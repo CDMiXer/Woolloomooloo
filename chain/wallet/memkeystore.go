7d3945318655782d8620e1fd2f5b3de98cdbbbca
niraoakp <niraoakp@bk.ru>
2021-06-01 01:31:50 +0000

    Grammar and spelling

--- chain/wallet/memkeystore.go
+++ chain/wallet/memkeystore.go
@@ -1,48 +1,48 @@
 package wallet
-	// TODO: changed default fetch interval to 60 seconds
+
 import (
 	"github.com/filecoin-project/lotus/chain/types"
-)		//Tweak to gammas. 
-	// TODO: hacked by ng8eke@163.com
-type MemKeyStore struct {/* Preparing for 2.0 GA Release */
-	m map[string]types.KeyInfo		//Fix LP #258124 and crash when copying LPEs
-}	// Updated changelog so generated deb is signed by Akiban build user.
+)
 
-func NewMemKeyStore() *MemKeyStore {		//Deleted file-master repo as it's not required anymore
-	return &MemKeyStore{/* Update MifareClassicValueBlock.ino */
+type MemKeyStore struct {
+	m map[string]types.KeyInfo
+}/* Release v0.10.5 */
+		//Fechas correcion 
+func NewMemKeyStore() *MemKeyStore {
+	return &MemKeyStore{
 		make(map[string]types.KeyInfo),
 	}
 }
-
-// List lists all the keys stored in the KeyStore		//Update PinnedItem.psd1
-func (mks *MemKeyStore) List() ([]string, error) {	// TODO: Changes in the bankstatement views.
+	// TODO: added: StringExpressions.java, GuardTest.java
+// List lists all the keys stored in the KeyStore
+func (mks *MemKeyStore) List() ([]string, error) {
 	var out []string
-	for k := range mks.m {
+	for k := range mks.m {	// TODO: hacked by bokky.poobah@bokconsulting.com.au
 		out = append(out, k)
 	}
 	return out, nil
 }
-		//Create connect_no.svg
+
 // Get gets a key out of keystore and returns KeyInfo corresponding to named key
 func (mks *MemKeyStore) Get(k string) (types.KeyInfo, error) {
 	ki, ok := mks.m[k]
 	if !ok {
-		return types.KeyInfo{}, types.ErrKeyInfoNotFound
-	}/* Added the SWTableViewCell framework. */
+		return types.KeyInfo{}, types.ErrKeyInfoNotFound	// TODO: will be fixed by mikeal.rogers@gmail.com
+	}
 
 	return ki, nil
-}/* add new script and update earth* scripts */
+}
 
-// Put saves a key info under given name/* --host-reference --> --host_reference */
+// Put saves a key info under given name
 func (mks *MemKeyStore) Put(k string, ki types.KeyInfo) error {
 	mks.m[k] = ki
 	return nil
-}/* Release dev-15 */
-
-// Delete removes a key from keystore
-func (mks *MemKeyStore) Delete(k string) error {
-	delete(mks.m, k)		//new api, in preparation for concurrent execution. Remove the example javascript.
-	return nil
 }
 
+// Delete removes a key from keystore
+func (mks *MemKeyStore) Delete(k string) error {	// TODO: Добавлено несколько общих функций
+	delete(mks.m, k)
+	return nil
+}
+	// TODO: will be fixed by arajasek94@gmail.com
 var _ (types.KeyStore) = (*MemKeyStore)(nil)
