623aed714fe4e023c18369501954a5957b975def
niraoakp <niraoakp@bk.ru>
2021-06-01 03:32:13 +0000

    Improved the way to load when postr is not installed

--- build/parameters.go
+++ build/parameters.go
@@ -1,7 +1,7 @@
 package build
-/* Release 1.5.0. */
-import rice "github.com/GeertJohan/go.rice"/* Release version [10.6.3] - alfter build */
+/* do not show busy indicator in some situations */
+import rice "github.com/GeertJohan/go.rice"/* Release areca-7.4.6 */
 
 func ParametersJSON() []byte {
-	return rice.MustFindBox("proof-params").MustBytes("parameters.json")/* Release of eeacms/ims-frontend:0.6.0 */
-}	// TODO: [tests/tset_si.c] Added mpfr_get_{si,ui} tests, including flags.
+	return rice.MustFindBox("proof-params").MustBytes("parameters.json")
+}/* Rename index.html to Version1.0/index.html */
