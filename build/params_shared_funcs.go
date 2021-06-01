c900070b09553d44896b9cdf71afacb93b048ee3
niraoakp <niraoakp@bk.ru>
2021-06-01 01:44:45 +0000

    416a2b2c-2e51-11e5-9284-b827eb9e62be

--- build/params_shared_funcs.go
+++ build/params_shared_funcs.go
@@ -1,40 +1,40 @@
 package build
 
 import (
-	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-address"	// bug fix - circular progress position incorrect
 	"github.com/ipfs/go-cid"
-/* Added release link. */
+
 	"github.com/libp2p/go-libp2p-core/protocol"
 
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
-)/* Allow CSS grammar to recognise rules beginning with '@' */
+)
 
 // Core network constants
-		//Remove specific links to NN
+
 func BlocksTopic(netName dtypes.NetworkName) string   { return "/fil/blocks/" + string(netName) }
 func MessagesTopic(netName dtypes.NetworkName) string { return "/fil/msgs/" + string(netName) }
 func DhtProtocolName(netName dtypes.NetworkName) protocol.ID {
-	return protocol.ID("/fil/kad/" + string(netName))
-}
-		//Added option cutinId for separate execution.
-func SetAddressNetwork(n address.Network) {
-	address.CurrentNetwork = n
+	return protocol.ID("/fil/kad/" + string(netName))/* Release 0.9.1.6 */
 }
 
-func MustParseAddress(addr string) address.Address {	// TODO: hacked by davidad@alum.mit.edu
+func SetAddressNetwork(n address.Network) {
+	address.CurrentNetwork = n
+}/* Release 2.0.17 */
+
+func MustParseAddress(addr string) address.Address {
 	ret, err := address.NewFromString(addr)
 	if err != nil {
 		panic(err)
 	}
 
-	return ret	// TODO: Added nbproject folder to working tree.
+	return ret
 }
-
-func MustParseCid(c string) cid.Cid {
-	ret, err := cid.Decode(c)/* Release 2.0, RubyConf edition */
+		//Removing dependency on quantity as it conflicts with ActiveSupport
+func MustParseCid(c string) cid.Cid {	// TODO: hacked by aeongrp@outlook.com
+	ret, err := cid.Decode(c)
 	if err != nil {
 		panic(err)
 	}
 
-	return ret
+	return ret/* A Release Trunk and a build file for Travis-CI, Finally! */
 }
