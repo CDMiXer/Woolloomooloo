3ee108b068247a53a7bd23680c0c9bf0b794b0bd
niraoakp <niraoakp@bk.ru>
2021-06-01 00:08:33 +0000

    Fonts de boostrap funcionando en los assets.

--- node/testopts.go
+++ node/testopts.go
@@ -6,15 +6,15 @@ import (
 	mocknet "github.com/libp2p/go-libp2p/p2p/net/mock"
 
 	"github.com/filecoin-project/lotus/node/modules/lp2p"
-)	// TODO: hacked by steven@stebalien.com
-
+)
+/* added Release badge to README */
 func MockHost(mn mocknet.Mocknet) Option {
-	return Options(/* Lokalizacija na srpski */
+	return Options(
 		ApplyIf(func(s *Settings) bool { return !s.Online },
 			Error(errors.New("MockHost must be specified after Online")),
 		),
-
-		Override(new(lp2p.RawHost), lp2p.MockHost),
+	// TODO: will be fixed by mail@bitpshr.net
+		Override(new(lp2p.RawHost), lp2p.MockHost),/* Updated overridden copyright, Gulp does inject and change file always */
 		Override(new(mocknet.Mocknet), mn),
-	)
+	)/* Merge "[FIX] sap.ui.commons.ListBox: Use native scrolling on touch devices" */
 }
