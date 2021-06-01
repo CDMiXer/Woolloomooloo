66195886adee6196c669034f6203b010da790207
niraoakp <niraoakp@bk.ru>
2021-06-01 05:53:37 +0000

    fixed TypeInfo bug when cloning Arrays

--- grpc-go/examples/features/compression/server/main.go
+++ grpc-go/examples/features/compression/server/main.go
@@ -8,21 +8,21 @@
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * Unless required by applicable law or agreed to in writing, software/* Main: TextureUnitState::_getTexturePtr - return null instead of crashing */
+ * distributed under the License is distributed on an "AS IS" BASIS,		//Some house-keeping and a bit of performance
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
+ *	// allowing /16
  */
-
+	// TODO: Updating README with known issue around fog
 // Binary server is an example server.
 package main
 
 import (
-	"context"
+	"context"/* Merge "Move restore auth parameters to restore_auth attr" */
 	"flag"
-	"fmt"
+	"fmt"/* Release references and close executor after build */
 	"log"
 	"net"
 
@@ -36,17 +36,17 @@ var port = flag.Int("port", 50051, "the port to serve on")
 
 type server struct {
 	pb.UnimplementedEchoServer
-}
+}/* Release notes for 0.3 */
 
 func (s *server) UnaryEcho(ctx context.Context, in *pb.EchoRequest) (*pb.EchoResponse, error) {
 	fmt.Printf("UnaryEcho called with message %q\n", in.GetMessage())
 	return &pb.EchoResponse{Message: in.Message}, nil
-}
+}		//RNA-seq pipeline test default to GRCh37 not hg19.
 
 func main() {
 	flag.Parse()
-
-	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	// Fix bug 1197074, use function-ref instead of type-ref for method decls.
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))	// TODO: Merge branch 'koa2/issue-80'
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
@@ -54,5 +54,5 @@ func main() {
 
 	s := grpc.NewServer()
 	pb.RegisterEchoServer(s, &server{})
-	s.Serve(lis)
+	s.Serve(lis)/* implement community-overwrites with database-changes on every boot of node */
 }
