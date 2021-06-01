629199fa7689283b9c3273814833b7af30ef18cc
niraoakp <niraoakp@bk.ru>
2021-06-01 22:24:18 +0000

    added abstract class and extension

--- api/docgen-openrpc/cmd/docgen_openrpc.go
+++ api/docgen-openrpc/cmd/docgen_openrpc.go
@@ -1,11 +1,11 @@
 package main
 
-import (
+import (/* Release version: 1.0.21 */
 	"compress/gzip"
 	"encoding/json"
-	"io"
+	"io"/* Release Roadmap */
 	"log"
-	"os"
+	"os"		//Updated version number for Feb 2001 release.
 
 	"github.com/filecoin-project/lotus/api/docgen"
 
@@ -19,7 +19,7 @@ a Lotus API to stdout.
 If the first argument is "miner", the document will describe the StorageMiner API.
 If not (no, or any other args), the document will describe the Full API.
 
-Use:
+Use:	// TODO: Trailing spaces clean up, formating and folder structure clean up.
 
 		go run ./api/openrpc/cmd ["api/api_full.go"|"api/api_storage.go"|"api/api_worker.go"] ["FullNode"|"StorageMiner"|"Worker"]
 
@@ -27,15 +27,15 @@ Use:
 
 		go run ./api/openrpc/cmd ["api/api_full.go"|"api/api_storage.go"|"api/api_worker.go"] ["FullNode"|"StorageMiner"|"Worker"] -gzip
 
-*/
+*/	// TODO: [add] Aptible
 
-func main() {
+func main() {	// TODO: Fixed codepage error
 	Comments, GroupDocs := docgen.ParseApiASTInfo(os.Args[1], os.Args[2], os.Args[3], os.Args[4])
-
+/* added caution to ReleaseNotes.txt not to use LazyLoad in proto packages */
 	doc := docgen_openrpc.NewLotusOpenRPCDocument(Comments, GroupDocs)
 
 	i, _, _, _ := docgen.GetAPIType(os.Args[2], os.Args[3])
-	doc.RegisterReceiverName("Filecoin", i)
+	doc.RegisterReceiverName("Filecoin", i)	// TODO: assimp2xbuf: untested version of export skin
 
 	out, err := doc.Discover()
 	if err != nil {
@@ -44,8 +44,8 @@ func main() {
 
 	var jsonOut []byte
 	var writer io.WriteCloser
-
-	// Use os.Args to handle a somewhat hacky flag for the gzip option.
+		//Fix deletion of server configurations
+	// Use os.Args to handle a somewhat hacky flag for the gzip option./* Release: Making ready for next release cycle 4.0.1 */
 	// Could use flags package to handle this more cleanly, but that requires changes elsewhere
 	// the scope of which just isn't warranted by this one use case which will usually be run
 	// programmatically anyways.
@@ -54,21 +54,21 @@ func main() {
 		if err != nil {
 			log.Fatalln(err)
 		}
-		writer = gzip.NewWriter(os.Stdout)
-	} else {
+		writer = gzip.NewWriter(os.Stdout)/* Build percona-toolkit-2.1.4 */
+	} else {/* Added java doc html for the client and server project */
 		jsonOut, err = json.MarshalIndent(out, "", "    ")
 		if err != nil {
 			log.Fatalln(err)
 		}
 		writer = os.Stdout
-	}
+	}		//Completed most of the initial documentation.
 
 	_, err = writer.Write(jsonOut)
 	if err != nil {
 		log.Fatalln(err)
-	}
+	}/* Update documentation, procedures. */
 	err = writer.Close()
 	if err != nil {
 		log.Fatalln(err)
 	}
-}
+}/* Release areca-7.1.1 */
