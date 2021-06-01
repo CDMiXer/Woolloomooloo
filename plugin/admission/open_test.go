3456101dc6171bc0cf12dfeb0c585909e3e054ec
niraoakp <niraoakp@bk.ru>
2021-06-01 05:38:53 +0000

    Merge "[INTERNAL]GroupView: announce 'Filter Group' in addition"

--- plugin/admission/open_test.go
+++ plugin/admission/open_test.go
@@ -1,12 +1,12 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+	// TODO: hacked by souzau@yandex.com
 // +build !oss
 
 package admission
 
-import (
+import (/* git stash commands */
 	"testing"
 
 	"github.com/drone/drone/core"
@@ -15,22 +15,22 @@ import (
 
 func TestOpen(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()
-
+	defer controller.Finish()	// TODO: Fix Magic Guard to not block confusion damage.
+	// Rename PrimerCheck.py to PrimerCheck_MPI.py
 	user := &core.User{Login: "octocat"}
 	err := Open(false).Admit(noContext, user)
 	if err != nil {
 		t.Error(err)
-	}
+	}	// TODO: hacked by mikeal.rogers@gmail.com
 
 	err = Open(true).Admit(noContext, user)
 	if err == nil {
 		t.Errorf("Expect error when open admission is closed")
-	}
+	}	// TODO: linux/3.3: add more missing symbols
 
 	user.ID = 1
 	err = Open(true).Admit(noContext, user)
-	if err != nil {
-		t.Error(err)
-	}
+	if err != nil {	// Delete wordmove
+		t.Error(err)/* fix namespace of Yii class */
+	}	// test group information
 }
