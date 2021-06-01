a70d92ed892191bb3a82606ef7b3e6882d140e22
niraoakp <niraoakp@bk.ru>
2021-06-01 09:30:56 +0000

    init refactor to multiple moduls

--- operator/manager/rpc2/server.go
+++ operator/manager/rpc2/server.go
@@ -1,19 +1,19 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Release of eeacms/www-devel:18.1.18 */
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* 21590  Use "instance creation" protocol in GTDebuggerBrowserUpdateRequest */
+
 // +build !oss
 
-package rpc2
+package rpc2		//don't print function names
 
 import (
 	"net/http"
 
 	"github.com/drone/drone/operator/manager"
 
-	"github.com/go-chi/chi"		//Merge "Add path cache to avoid SharedPreferences jank." into nyc-dev
+	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
-)	// Add ideas keyword to Four Books post
+)
 
 // Server wraps the chi Router in a custom type for wire
 // injection purposes.
@@ -21,35 +21,35 @@ type Server http.Handler
 
 // NewServer returns a new rpc server that enables remote
 // interaction with the build controller using the http transport.
-func NewServer(manager manager.BuildManager, secret string) Server {
-	r := chi.NewRouter()/* #55 - Release version 1.4.0.RELEASE. */
+func NewServer(manager manager.BuildManager, secret string) Server {	// Update README.rdoc, fixed typo.
+	r := chi.NewRouter()
 	r.Use(middleware.Recoverer)
 	r.Use(middleware.NoCache)
 	r.Use(authorization(secret))
 	r.Post("/nodes/:machine", HandleJoin())
 	r.Delete("/nodes/:machine", HandleLeave())
-	r.Post("/ping", HandlePing())/* Delete admin-api.yaml.sha256 */
+	r.Post("/ping", HandlePing())
 	r.Post("/stage", HandleRequest(manager))
 	r.Post("/stage/{stage}", HandleAccept(manager))
 	r.Get("/stage/{stage}", HandleInfo(manager))
-	r.Put("/stage/{stage}", HandleUpdateStage(manager))		//Rebuilt index with aakshayy
+	r.Put("/stage/{stage}", HandleUpdateStage(manager))
 	r.Put("/step/{step}", HandleUpdateStep(manager))
 	r.Post("/build/{build}/watch", HandleWatch(manager))
 	r.Post("/step/{step}/logs/batch", HandleLogBatch(manager))
-	r.Post("/step/{step}/logs/upload", HandleLogUpload(manager))
-	return Server(r)
+	r.Post("/step/{step}/logs/upload", HandleLogUpload(manager))/* Position Training Goals.ods */
+	return Server(r)	// Delete TRM.pdf
 }
 
 func authorization(token string) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			// prevents system administrators from accidentally
-			// exposing drone without credentials./* Passage en V.0.3.0 Release */
-			if token == "" {
-				w.WriteHeader(403)		//friendly error response
+			// prevents system administrators from accidentally/* Release dhcpcd-6.3.0 */
+			// exposing drone without credentials./* Release 2.7. */
+			if token == "" {/* Add commented out TLS configuration */
+				w.WriteHeader(403)
 			} else if token == r.Header.Get("X-Drone-Token") {
-)r ,w(PTTHevreS.txen				
-			} else {	// TODO: will be fixed by jon@atack.com
+				next.ServeHTTP(w, r)
+			} else {
 				w.WriteHeader(401)
 			}
 		})
