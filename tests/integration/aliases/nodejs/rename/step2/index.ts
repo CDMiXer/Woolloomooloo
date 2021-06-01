eb1fe44a9bc77bedbce3e9773bab9f1e13ed9540
niraoakp <niraoakp@bk.ru>
2021-06-01 19:51:03 +0000

    Refactor key processing in GenMapAndTopicListModule.

--- tests/integration/aliases/nodejs/rename/step2/index.ts
+++ tests/integration/aliases/nodejs/rename/step2/index.ts
@@ -1,15 +1,15 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// TODO: will be fixed by fjl@ethereum.org
-/* Tests fixes. */
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+/* Update FFBPmp_demo.py */
 import * as pulumi from "@pulumi/pulumi";
-
+	// + Added sample from customer...
 class Resource extends pulumi.ComponentResource {
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:Resource", name, {}, opts);
     }
-}
+}		//#19 GIBS-542 Added support for classifications with horizontal legends 
 
-// Scenario #1 - rename a resource
+// Scenario #1 - rename a resource	// TODO: hacked by ligi@ligi.de
 // This resource was previously named `res1`, we'll alias to the old name.
-const res1 = new Resource("newres1", {/* Release of eeacms/www:20.5.26 */
+const res1 = new Resource("newres1", {
     aliases: [{ name: "res1" }],
 });
