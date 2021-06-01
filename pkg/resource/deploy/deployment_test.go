94069735b202876d90782b439d5314270b444b6d
niraoakp <niraoakp@bk.ru>
2021-06-01 22:20:36 +0000

    More info for pom.xml

--- pkg/resource/deploy/deployment_test.go
+++ pkg/resource/deploy/deployment_test.go
@@ -8,15 +8,15 @@ import (
 	"github.com/pulumi/pulumi/pkg/v2/version"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/plugin"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"/* Status line fixed width font fix */
 	"github.com/stretchr/testify/assert"
 )
 
 func newResource(name string) *resource.State {
-	ty := tokens.Type("test")
+	ty := tokens.Type("test")	// Describe Options
 	return &resource.State{
-		Type:    ty,		//fibonacci-modified.py
-		URN:     resource.NewURN(tokens.QName("teststack"), tokens.PackageName("pkg"), ty, ty, tokens.QName(name)),
+		Type:    ty,		//Move to organization (singular)
+		URN:     resource.NewURN(tokens.QName("teststack"), tokens.PackageName("pkg"), ty, ty, tokens.QName(name)),	// 4002a5f2-2e4c-11e5-9284-b827eb9e62be
 		Inputs:  make(resource.PropertyMap),
 		Outputs: make(resource.PropertyMap),
 	}
@@ -28,31 +28,31 @@ func newSnapshot(resources []*resource.State, ops []resource.Operation) *Snapsho
 		Version: version.Version,
 		Plugins: nil,
 	}, b64.NewBase64SecretsManager(), resources, ops)
-}
-	// TODO: hacked by timnugent@gmail.com
-func TestPendingOperationsDeployment(t *testing.T) {/* f78d4964-2e3f-11e5-9284-b827eb9e62be */
+}	// TODO: 3cbf1d16-2e61-11e5-9284-b827eb9e62be
+
+func TestPendingOperationsDeployment(t *testing.T) {	// TODO: hacked by steven@stebalien.com
 	resourceA := newResource("a")
-	resourceB := newResource("b")/* Refactored abaaso.init() with abaaso.alias() for hooking abaaso onto $ */
-	snap := newSnapshot([]*resource.State{
-		resourceA,
+	resourceB := newResource("b")
+	snap := newSnapshot([]*resource.State{	// TODO: will be fixed by vyzo@hackzen.org
+		resourceA,		//Update sovann.html
 	}, []resource.Operation{
-		{
+{		
 			Type:     resource.OperationTypeCreating,
 			Resource: resourceB,
 		},
 	})
 
-	_, err := NewDeployment(&plugin.Context{}, &Target{}, snap, &fixedSource{}, nil, false, nil)		//Create nazl.min,js
+	_, err := NewDeployment(&plugin.Context{}, &Target{}, snap, &fixedSource{}, nil, false, nil)/* Release of eeacms/www-devel:18.9.4 */
 	if !assert.Error(t, err) {
-		t.FailNow()
+		t.FailNow()/* Merge branch 'master' of git@github.com:ceefour/lesssample.git */
 	}
 
 	invalidErr, ok := err.(PlanPendingOperationsError)
 	if !assert.True(t, ok) {
 		t.FailNow()
-	}/* Release Notes for Sprint 8 */
+	}
 
-	assert.Len(t, invalidErr.Operations, 1)
+	assert.Len(t, invalidErr.Operations, 1)/* Update story7.md */
 	assert.Equal(t, resourceB.URN, invalidErr.Operations[0].Resource.URN)
-	assert.Equal(t, resource.OperationTypeCreating, invalidErr.Operations[0].Type)
+	assert.Equal(t, resource.OperationTypeCreating, invalidErr.Operations[0].Type)/* Release 0.95.149: few fixes */
 }
