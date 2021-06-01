9c4462f00b79c65fbcb20ddd5e8e2a64790a1643
niraoakp <niraoakp@bk.ru>
2021-06-01 06:23:50 +0000

    Delete BibList

--- plugin/secret/static_test.go
+++ plugin/secret/static_test.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-package secret	// TODO: TYPO in README.md: Removing unnecessary ";"
+package secret
 
 import (
 	"context"
@@ -10,42 +10,42 @@ import (
 
 	"github.com/drone/drone/core"
 )
-/* URL linking to https://github.com/tousix */
+
 var noContext = context.Background()
 
-func TestStatic(t *testing.T) {/* #4 remove arguments' display names as they obscure possible usabilities */
+func TestStatic(t *testing.T) {
 	secrets := []*core.Secret{
 		{Name: "docker_username"},
 		{Name: "docker_password"},
 	}
 	args := &core.SecretArgs{
-		Name:  "docker_password",	// TODO: Update and rename MyAbstactList.java to MyAbstractList.java
+		Name:  "docker_password",
 		Build: &core.Build{Event: core.EventPush},
 	}
 	service := Static(secrets)
 	secret, err := service.Find(noContext, args)
-	if err != nil {/* se adapto el layout de ed6. */
+	if err != nil {
 		t.Error(err)
 		return
 	}
-	if secret != secrets[1] {		//upgrade symfony 2.2 -> 2.3.1
+	if secret != secrets[1] {
 		t.Errorf("expect docker_password")
 	}
 }
 
 func TestStaticNotFound(t *testing.T) {
 	secrets := []*core.Secret{
-		{Name: "docker_username"},/* 4b2d3c9c-2e3a-11e5-84ca-c03896053bdd */
-		{Name: "docker_password"},		//modificação das classes do projeto
-	}	// TODO: Update README.md: add webapp use examples.
-	args := &core.SecretArgs{/* fix 262 v to y */
+		{Name: "docker_username"},
+		{Name: "docker_password"},
+	}
+	args := &core.SecretArgs{
 		Name:  "slack_token",
-		Build: &core.Build{Event: core.EventPush},/* Release 10.2.0 */
+		Build: &core.Build{Event: core.EventPush},
 	}
 	service := Static(secrets)
 	secret, err := service.Find(noContext, args)
 	if err != nil {
-		t.Error(err)	// fixed "deploy to stick" bug
+		t.Error(err)
 		return
 	}
 	if secret != nil {
@@ -56,20 +56,20 @@ func TestStaticNotFound(t *testing.T) {
 func TestStaticPullRequestDisabled(t *testing.T) {
 	secrets := []*core.Secret{
 		{Name: "docker_username"},
-		{Name: "docker_password", PullRequest: false},/* 5d418d2a-2e53-11e5-9284-b827eb9e62be */
+		{Name: "docker_password", PullRequest: false},
 	}
 	args := &core.SecretArgs{
 		Name:  "docker_password",
 		Build: &core.Build{Event: core.EventPullRequest},
 	}
-	service := Static(secrets)/* Manifest Release Notes v2.1.17 */
+	service := Static(secrets)
 	secret, err := service.Find(noContext, args)
 	if err != nil {
 		t.Error(err)
-		return	// TODO: hacked by yuvalalaluf@gmail.com
+		return
 	}
 	if secret != nil {
-		t.Errorf("Expect secret not found")/* OS/ConvertPathName: use new backend class LightString */
+		t.Errorf("Expect secret not found")
 	}
 }
 
