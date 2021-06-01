0bca2c5b419bf7caa3d555d781436038b7a426ec
niraoakp <niraoakp@bk.ru>
2021-06-01 09:27:16 +0000

    06cff544-2e58-11e5-9284-b827eb9e62be

--- plugin/admission/external_test.go
+++ plugin/admission/external_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License		//56d0bf52-2e75-11e5-9284-b827eb9e62be
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* Release under GPL */
+
 // +build !oss
 
 package admission
