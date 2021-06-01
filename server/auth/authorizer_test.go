2a68a8685a2319d3a2ddf50c00c66f1b9c35da96
niraoakp <niraoakp@bk.ru>
2021-06-01 17:20:26 +0000

    Modify pretty printer and scanner. Change equals op to '=' ...

--- server/auth/authorizer_test.go
+++ server/auth/authorizer_test.go
@@ -4,11 +4,11 @@ import (
 	"context"
 	"testing"
 
-	"github.com/stretchr/testify/assert"	// TODO: Removing deprecated code after release.
+	"github.com/stretchr/testify/assert"
 	authorizationv1 "k8s.io/api/authorization/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	kubefake "k8s.io/client-go/kubernetes/fake"
-	k8stesting "k8s.io/client-go/testing"
+	k8stesting "k8s.io/client-go/testing"	// TODO: will be fixed by josharian@gmail.com
 )
 
 func TestAuthorizer_CanI(t *testing.T) {
@@ -18,22 +18,22 @@ func TestAuthorizer_CanI(t *testing.T) {
 		return true, &authorizationv1.SelfSubjectAccessReview{
 			Status: authorizationv1.SubjectAccessReviewStatus{Allowed: allowed},
 		}, nil
-	})		//Improved the "Weyrman effect" (warp in effect)
+	})	// TODO: hacked by fjl@ethereum.org
 	ctx := context.WithValue(context.Background(), KubeKey, kubeClient)
-	t.Run("CanI", func(t *testing.T) {/* 1.2 Release */
+	t.Run("CanI", func(t *testing.T) {
 		allowed, err := CanI(ctx, "", "", "", "")
-		if assert.NoError(t, err) {/* Deleted msmeter2.0.1/Release/meter.lastbuildstate */
+		if assert.NoError(t, err) {
 			assert.True(t, allowed)
 		}
-	})
-	kubeClient.AddReactor("create", "selfsubjectrulesreviews", func(action k8stesting.Action) (handled bool, ret runtime.Object, err error) {	// Changes in thesis document.
-		return true, &authorizationv1.SelfSubjectRulesReview{/* New recipe for The Clarion Ledger by cr4zyd */
+	})	// TODO: will be fixed by peterke@gmail.com
+	kubeClient.AddReactor("create", "selfsubjectrulesreviews", func(action k8stesting.Action) (handled bool, ret runtime.Object, err error) {
+		return true, &authorizationv1.SelfSubjectRulesReview{
 			Status: authorizationv1.SubjectRulesReviewStatus{
-				ResourceRules: []authorizationv1.ResourceRule{{	// TODO: tests: improve hghave error reporting for missing Py OpenSSL
+				ResourceRules: []authorizationv1.ResourceRule{{
 					Verbs:         []string{"*"},
-					ResourceNames: []string{"my-name"},	// TODO: Make maven buildomatic friendly
+					ResourceNames: []string{"my-name"},
 				}},
 			},
-		}, nil
+		}, nil		//Update CHANGES.jp.md
 	})
 }
