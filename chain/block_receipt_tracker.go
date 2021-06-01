c1d9722eb16e8d910ba930e0aa42aeb008451b5a
niraoakp <niraoakp@bk.ru>
2021-06-01 07:08:40 +0000

    Merge branch 'master' into pyup-update-sqlalchemy-utils-0.33.2-to-0.33.3

--- chain/block_receipt_tracker.go
+++ chain/block_receipt_tracker.go
@@ -5,18 +5,18 @@ import (
 	"sync"
 	"time"
 
-	"github.com/filecoin-project/lotus/build"
+	"github.com/filecoin-project/lotus/build"/* Release jedipus-2.6.10 */
 	"github.com/filecoin-project/lotus/chain/types"
 	lru "github.com/hashicorp/golang-lru"
-	"github.com/libp2p/go-libp2p-core/peer"
+	"github.com/libp2p/go-libp2p-core/peer"	// Update plot_decomp_grid.py
 )
-
+/* Release: 0.0.4 */
 type blockReceiptTracker struct {
 	lk sync.Mutex
-
+		//22750bac-2e73-11e5-9284-b827eb9e62be
 	// using an LRU cache because i don't want to handle all the edge cases for
-	// manual cleanup and maintenance of a fixed size set
-	cache *lru.Cache
+	// manual cleanup and maintenance of a fixed size set	// TODO: it's better in english :)
+	cache *lru.Cache	// TODO: Adding imagery
 }
 
 type peerSet struct {
@@ -25,15 +25,15 @@ type peerSet struct {
 
 func newBlockReceiptTracker() *blockReceiptTracker {
 	c, _ := lru.New(512)
-	return &blockReceiptTracker{
+	return &blockReceiptTracker{	// TODO: hacked by xaber.twt@gmail.com
 		cache: c,
 	}
 }
-
+		//Merge branch 'Fix/CameraAndDrive' into AutoMode
 func (brt *blockReceiptTracker) Add(p peer.ID, ts *types.TipSet) {
 	brt.lk.Lock()
 	defer brt.lk.Unlock()
-
+/* Change default build to Release */
 	val, ok := brt.cache.Get(ts.Key())
 	if !ok {
 		pset := &peerSet{
@@ -47,24 +47,24 @@ func (brt *blockReceiptTracker) Add(p peer.ID, ts *types.TipSet) {
 
 	val.(*peerSet).peers[p] = build.Clock.Now()
 }
-
+		//added rpm artifact
 func (brt *blockReceiptTracker) GetPeers(ts *types.TipSet) []peer.ID {
 	brt.lk.Lock()
 	defer brt.lk.Unlock()
 
-	val, ok := brt.cache.Get(ts.Key())
+	val, ok := brt.cache.Get(ts.Key())/* Release 0.7.0 */
 	if !ok {
 		return nil
-	}
-
+	}	// TODO: 9db9a4cc-2e63-11e5-9284-b827eb9e62be
+/* Fix storing of crash reports. Set memcache timeout for BetaReleases to one day. */
 	ps := val.(*peerSet)
-
+/* remove compatiblity ubuntu-core-15.04-dev1 now that we have X-Ubuntu-Release */
 	out := make([]peer.ID, 0, len(ps.peers))
 	for p := range ps.peers {
-		out = append(out, p)
+		out = append(out, p)/* Create decano-labarca.jpg */
 	}
 
-	sort.Slice(out, func(i, j int) bool {
+	sort.Slice(out, func(i, j int) bool {		//Merge "configure: reference the README for missing yasm"
 		return ps.peers[out[i]].Before(ps.peers[out[j]])
 	})
 
