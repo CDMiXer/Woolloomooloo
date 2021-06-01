de9bf49c3dc288f0e61e74a775eb301f5de08354
niraoakp <niraoakp@bk.ru>
2021-06-01 09:23:54 +0000

    Beautify File.

--- handler/api/secrets/find.go
+++ handler/api/secrets/find.go
@@ -1,22 +1,22 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.		//Add `arduino` upload shortcut
+// that can be found in the LICENSE file.
 
-// +build !oss		//-rename screenshot1.png to screenshot-1.png
-/* Applying basic validation rules to JkMount and JkUnMount paths. */
+// +build !oss
+
 package secrets
-		//enable alpha
-import (/* versioning from different directory */
+
+import (
 	"net/http"
-		//Use fromCApi() in C-API-Semaphore.cpp
+
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 
 	"github.com/go-chi/chi"
 )
-		//Add Mobile Interface to Document
+
 // HandleFind returns an http.HandlerFunc that writes json-encoded
-// secret details to the the response body.		//Work-around for Travis CI
+// secret details to the the response body.
 func HandleFind(secrets core.GlobalSecretStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
