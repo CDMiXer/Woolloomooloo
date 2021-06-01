efe3c3c196665ba3f6f0e872b71c79f5aa01363a
niraoakp <niraoakp@bk.ru>
2021-06-01 09:32:49 +0000

    Update Datatable.php

--- pkg/codegen/internal/test/testdata/secret.pp.ts
+++ pkg/codegen/internal/test/testdata/secret.pp.ts
@@ -1,4 +1,4 @@
 import * as pulumi from "@pulumi/pulumi";
-import * as aws from "@pulumi/aws";/* Released Clickhouse v0.1.6 */
-
+import * as aws from "@pulumi/aws";
+		//detect recursion
 const dbCluster = new aws.rds.Cluster("dbCluster", {masterPassword: pulumi.secret("foobar")});
