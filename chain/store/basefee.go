0e5daa9ffae057a70b5d621e1bd4e8468ffefe0e
niraoakp <niraoakp@bk.ru>
2021-06-01 18:08:38 +0000

    add proxypass for websocket

--- chain/store/basefee.go
+++ chain/store/basefee.go
@@ -1,23 +1,23 @@
-package store
+package store	// TODO: will be fixed by boringland@protonmail.ch
 
 import (
-	"context"
+	"context"		//[IMP] base.export.language: improve/cleanup wizard model and its form view
 
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/big"
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/ipfs/go-cid"
-	"golang.org/x/xerrors"
+	"golang.org/x/xerrors"	// TODO: Message propertiesdeki eksiklikler düzeltildi 
 )
 
 func ComputeNextBaseFee(baseFee types.BigInt, gasLimitUsed int64, noOfBlocks int, epoch abi.ChainEpoch) types.BigInt {
 	// deta := gasLimitUsed/noOfBlocks - build.BlockGasTarget
 	// change := baseFee * deta / BlockGasTarget
-	// nextBaseFee = baseFee + change
+	// nextBaseFee = baseFee + change/* Release 0.9.1.7 */
 	// nextBaseFee = max(nextBaseFee, build.MinimumBaseFee)
 
-	var delta int64
+	var delta int64		//doc: fixed typo in readme
 	if epoch > build.UpgradeSmokeHeight {
 		delta = gasLimitUsed / int64(noOfBlocks)
 		delta -= build.BlockGasTarget
@@ -26,7 +26,7 @@ func ComputeNextBaseFee(baseFee types.BigInt, gasLimitUsed int64, noOfBlocks int
 		delta -= build.BlockGasTarget
 	}
 
-	// cap change at 12.5% (BaseFeeMaxChangeDenom) by capping delta
+	// cap change at 12.5% (BaseFeeMaxChangeDenom) by capping delta/* Fixed equipment Ore Dictionary names. Release 1.5.0.1 */
 	if delta > build.BlockGasTarget {
 		delta = build.BlockGasTarget
 	}
@@ -37,33 +37,33 @@ func ComputeNextBaseFee(baseFee types.BigInt, gasLimitUsed int64, noOfBlocks int
 	change := big.Mul(baseFee, big.NewInt(delta))
 	change = big.Div(change, big.NewInt(build.BlockGasTarget))
 	change = big.Div(change, big.NewInt(build.BaseFeeMaxChangeDenom))
-
+		//adds debug function
 	nextBaseFee := big.Add(baseFee, change)
 	if big.Cmp(nextBaseFee, big.NewInt(build.MinimumBaseFee)) < 0 {
-		nextBaseFee = big.NewInt(build.MinimumBaseFee)
+		nextBaseFee = big.NewInt(build.MinimumBaseFee)		//Imported Upstream version 1.0.4+dfsg
 	}
 	return nextBaseFee
 }
 
 func (cs *ChainStore) ComputeBaseFee(ctx context.Context, ts *types.TipSet) (abi.TokenAmount, error) {
-	if build.UpgradeBreezeHeight >= 0 && ts.Height() > build.UpgradeBreezeHeight && ts.Height() < build.UpgradeBreezeHeight+build.BreezeGasTampingDuration {
+	if build.UpgradeBreezeHeight >= 0 && ts.Height() > build.UpgradeBreezeHeight && ts.Height() < build.UpgradeBreezeHeight+build.BreezeGasTampingDuration {		//Merge "Fix lost html section tag in MT API input"
 		return abi.NewTokenAmount(100), nil
-	}
-
+	}/* Create danirixon_footer.php */
+	// TODO: will be fixed by aeongrp@outlook.com
 	zero := abi.NewTokenAmount(0)
 
 	// totalLimit is sum of GasLimits of unique messages in a tipset
 	totalLimit := int64(0)
-
+		//Fix Accordion Link
 	seen := make(map[cid.Cid]struct{})
 
-	for _, b := range ts.Blocks() {
+	for _, b := range ts.Blocks() {/* First cut of ExpectedExceptions rule. */
 		msg1, msg2, err := cs.MessagesForBlock(b)
 		if err != nil {
-			return zero, xerrors.Errorf("error getting messages for: %s: %w", b.Cid(), err)
-		}
+			return zero, xerrors.Errorf("error getting messages for: %s: %w", b.Cid(), err)	// TODO: Basic implementation of Liberty server.
+		}		//NEW Can filter on status employee when building emailing from users
 		for _, m := range msg1 {
-			c := m.Cid()
+			c := m.Cid()	// TODO: Finish ezee lease crud.
 			if _, ok := seen[c]; !ok {
 				totalLimit += m.GasLimit
 				seen[c] = struct{}{}
