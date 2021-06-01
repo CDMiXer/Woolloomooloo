d6c7e4e0d98de7d01c043bd637f3a37f8cc9dd6c
niraoakp <niraoakp@bk.ru>
2021-06-01 00:04:58 +0000

    Updated Chinese translation

--- plugin/config/jsonnet_test.go
+++ plugin/config/jsonnet_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* tpm2_policyor.c: Removed remnants of debug code in the file */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* Merge "msm: cpufreq: Release cpumask_var_t on all cases" into msm-3.0 */
+// that can be found in the LICENSE file.		//dc50baba-2e52-11e5-9284-b827eb9e62be
 
 // +build !oss
-/* 4b0f8030-2e67-11e5-9284-b827eb9e62be */
+/* Released jujiboutils 2.0 */
 package config
