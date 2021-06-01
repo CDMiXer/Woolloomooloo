d03c597fd909a06d06d5bb75615c7eaa1984f50f
niraoakp <niraoakp@bk.ru>
2021-06-01 14:32:04 +0000

    Modification 'Inference.java' -> predictTagOnTweet

--- pkg/codegen/internal/test/testdata/random-pet.pp
+++ pkg/codegen/internal/test/testdata/random-pet.pp
@@ -1,3 +1,3 @@
-resource random_pet "random:index/randomPet:RandomPet" {
+resource random_pet "random:index/randomPet:RandomPet" {		//Update RTTreeMapBuilder to handle collections, see RTTreeMapExample
   prefix = "doggo"
 }
