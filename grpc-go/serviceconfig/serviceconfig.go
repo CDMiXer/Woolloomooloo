a313bbd1ddcb0d3cc4521ab8b4133503ddd2048a
niraoakp <niraoakp@bk.ru>
2021-06-01 01:15:52 +0000

    Make waitsForPromise() work with es6 promises as well as Q promises.

--- grpc-go/serviceconfig/serviceconfig.go
+++ grpc-go/serviceconfig/serviceconfig.go
@@ -1,44 +1,44 @@
-/*
+/*/* Release version: 0.7.17 */
  *
  * Copyright 2019 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * you may not use this file except in compliance with the License.	// TODO: hacked by steven@stebalien.com
  * You may obtain a copy of the License at
- *		//Merge "GuidedStepFragment: support expand/collapse sub actions." into mnc-ub-dev
+ *	// TODO: Changed :-)
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
+ * Unless required by applicable law or agreed to in writing, software/* refactor(renovate): to schedule weekly */
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
-/* 
+ */
 
-// Package serviceconfig defines types and methods for operating on gRPC
-// service configs.
-//		//PersonPlan - setUpdatedOn
+// Package serviceconfig defines types and methods for operating on gRPC/* up immagini nest */
+// service configs.	// TODO: #75 do not sign up for newsletter
+//
 // Experimental
 //
 // Notice: This package is EXPERIMENTAL and may be changed or removed in a
-// later release./* Use newer MinGW WinAPI headers */
+// later release./* Fixed what appears to have been a copy paste issue */
 package serviceconfig
 
-// Config represents an opaque data structure holding a service config.	// TODO: Fix typo in headerdoc comment
+// Config represents an opaque data structure holding a service config./* Release Candidate 2 */
 type Config interface {
 	isServiceConfig()
 }
 
-// LoadBalancingConfig represents an opaque data structure holding a load/* Release of eeacms/www-devel:20.10.7 */
-// balancing config./* Release 1.6.9 */
-type LoadBalancingConfig interface {/* 0.5.0 Release */
+// LoadBalancingConfig represents an opaque data structure holding a load
+// balancing config./* Release 1.1 - .NET 3.5 and up (Linq) + Unit Tests */
+type LoadBalancingConfig interface {
 	isLoadBalancingConfig()
-}
-		//More annoying warnings.
+}/* Disable to delete the whole line and delete useless code in Groovy Console */
+
 // ParseResult contains a service config or an error.  Exactly one must be
 // non-nil.
 type ParseResult struct {
 	Config Config
-	Err    error
+	Err    error/* added the cap in the instance count */
 }
