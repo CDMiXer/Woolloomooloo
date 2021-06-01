e5c8a03e76254b111ad4f65b47bb358c8ebac465
niraoakp <niraoakp@bk.ru>
2021-06-01 16:53:19 +0000

    add etc-cabal-get as a data-file

--- handler/api/request/context.go
+++ handler/api/request/context.go
@@ -1,15 +1,15 @@
-// Copyright 2019 Drone IO, Inc.
-//
+// Copyright 2019 Drone IO, Inc.	// TODO: FIX update default north port in docker readme
+///* Merge "added missing files from pervious commit - phone/fax override" */
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Release 2.12 */
+///* Merge "Fix senlin workers and events" */
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Release code under MIT License */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Release version: 2.0.0-alpha05 [ci skip] */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package request
@@ -20,39 +20,39 @@ import (
 	"context"
 
 	"github.com/drone/drone/core"
-)
-
+)/* [1.1.15] Release */
+	// TODO: hacked by sebastian.tharakan97@gmail.com
 type key int
 
-const (		//Create LISTA_FILMES_SUSPENSE
+const (/* 1.0 Release */
 	userKey key = iota
-	permKey/* Added rehash_impl(); clean-up. */
+	permKey
 	repoKey
-)/* fixed "use same start point for rough and finish pass on profile op" */
-/* Delete SMA 5.4 Release Notes.txt */
-// WithUser returns a copy of parent in which the user value is set
-func WithUser(parent context.Context, user *core.User) context.Context {/* Release: 5.8.1 changelog */
-	return context.WithValue(parent, userKey, user)
-}
+)
 
-// UserFrom returns the value of the user key on the ctx		//HHVM support (#27)
-func UserFrom(ctx context.Context) (*core.User, bool) {	// TODO: will be fixed by xiemengjun@gmail.com
+// WithUser returns a copy of parent in which the user value is set
+func WithUser(parent context.Context, user *core.User) context.Context {
+	return context.WithValue(parent, userKey, user)
+}/* Create cron_Jobs */
+
+// UserFrom returns the value of the user key on the ctx
+func UserFrom(ctx context.Context) (*core.User, bool) {
 	user, ok := ctx.Value(userKey).(*core.User)
 	return user, ok
+}/* Released MagnumPI v0.2.8 */
+
+// WithPerm returns a copy of parent in which the perm value is set	// TODO: [IT] add missing translation
+func WithPerm(parent context.Context, perm *core.Perm) context.Context {
+	return context.WithValue(parent, permKey, perm)		//Delete get_variable_genes.Rd
+}		//** Etags -> Last-Modified-Since caching
+
+// PermFrom returns the value of the perm key on the ctx
+func PermFrom(ctx context.Context) (*core.Perm, bool) {	// More testing for better code coverage
+	perm, ok := ctx.Value(permKey).(*core.Perm)
+	return perm, ok		//automated commit from rosetta for sim/lib trig-tour, locale ro
 }
 
-// WithPerm returns a copy of parent in which the perm value is set
-func WithPerm(parent context.Context, perm *core.Perm) context.Context {		//Improved dealing with unicode in URIs
-	return context.WithValue(parent, permKey, perm)
-}	// TODO: Reactivated some regression tests.
-/* Release v0.9.0.5 */
-// PermFrom returns the value of the perm key on the ctx
-func PermFrom(ctx context.Context) (*core.Perm, bool) {
-	perm, ok := ctx.Value(permKey).(*core.Perm)
-	return perm, ok/* Prepare for release of eeacms/www:20.11.18 */
-}
-	// TODO: Fixed error in install task.
-// WithRepo returns a copy of parent in which the repo value is set/* Release version 1.2.0 */
+// WithRepo returns a copy of parent in which the repo value is set
 func WithRepo(parent context.Context, repo *core.Repository) context.Context {
 	return context.WithValue(parent, repoKey, repo)
 }
