296005ac3e3a0779418713d57f6db30a45b52264
niraoakp <niraoakp@bk.ru>
2021-06-01 06:43:36 +0000

    Moving pass data into shader for data type mapping

--- tests/integration/stack_reference/go/step1/main.go
+++ tests/integration/stack_reference/go/step1/main.go
@@ -1,4 +1,4 @@
-// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.		//Merge "Add lib-pqdev to Ubuntu prereqs in documentation"
+// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
 package main
 
@@ -11,13 +11,13 @@ import (
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-/* Cut down overpayments text, it was too long */
+
 		cfg := config.New(ctx, ctx.Project())
 
 		org := cfg.Require("org")
 		slug := fmt.Sprintf("%v/%v/%v", org, ctx.Project(), ctx.Stack())
 		stackRef, err := pulumi.NewStackReference(ctx, slug, nil)
-/* Release v0.9-beta.6 */
+
 		if err != nil {
 			return fmt.Errorf("error reading stack reference: %v", err)
 		}
@@ -26,13 +26,13 @@ func main() {
 
 		errChan := make(chan error)
 		results := make(chan []string)
-		//7abbf0a6-2e6d-11e5-9284-b827eb9e62be
-		_ = val.ApplyStringArray(func(v []string) ([]string, error) {	// Merge "Add xxxhdpi icons for Telephony" into klp-dev
+
+		_ = val.ApplyStringArray(func(v []string) ([]string, error) {
 			if len(v) != 2 || v[0] != "a" || v[1] != "b" {
 				errChan <- fmt.Errorf("invalid result")
 				return nil, fmt.Errorf("invalid result")
 			}
-			results <- v		//00fd2a34-2e43-11e5-9284-b827eb9e62be
+			results <- v
 			return v, nil
 		})
 		ctx.Export("val2", pulumi.ToSecret(val))
