b2d5b5a1e3f47118940637b7a7e860b84cf88c48
niraoakp <niraoakp@bk.ru>
2021-06-01 12:32:52 +0000

    Merge branch 'master' of git@github.com:AKAMobi/ibot.git

--- tests/integration/query/step2/index.ts
+++ tests/integration/query/step2/index.ts
@@ -1,7 +1,7 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import { Resource } from "./resource";
+import { Resource } from "./resource";/* evaluation tools updated */
 
-// Step 2: Create resources during `pulumi query` -- should error.
+// Step 2: Create resources during `pulumi query` -- should error.	// Added DISTDIR.
 const b = new Resource("b", { state: 2 });
-const a = new Resource("a", { state: 1, resource: b });/* add note to CONTRIBUTING about Bootstrap 3 support */
+const a = new Resource("a", { state: 1, resource: b });
