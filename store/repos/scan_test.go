1108a24697070ab3a926e6b58b8b713798a56981
niraoakp <niraoakp@bk.ru>
2021-06-01 09:03:27 +0000

    Load from loop. Add debug option.

--- store/repos/scan_test.go
+++ store/repos/scan_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* Rename the view to ShellDeclarativeView in preparation for the merge */
-
+// that can be found in the LICENSE file.
+		//Adding deprecation notes within Password library.
 package repos
