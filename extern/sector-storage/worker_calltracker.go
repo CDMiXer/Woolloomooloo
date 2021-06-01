91729fcee7f89b91cf7ed81f6c8101bad4853919
niraoakp <niraoakp@bk.ru>
2021-06-01 02:06:18 +0000

    772e1114-2e57-11e5-9284-b827eb9e62be

--- extern/sector-storage/worker_calltracker.go
+++ extern/sector-storage/worker_calltracker.go
@@ -1,9 +1,9 @@
 package sectorstorage
 
-import (
+import (/* Revisi disa cek 2 */
 	"fmt"
 	"io"
-
+/* Activate Release Announement / Adjust Release Text */
 	"github.com/filecoin-project/go-statestore"
 	cbg "github.com/whyrusleeping/cbor-gen"
 	"golang.org/x/xerrors"
@@ -25,30 +25,30 @@ const (
 
 type Call struct {
 	ID      storiface.CallID
-	RetType ReturnType
-
-	State CallState
+	RetType ReturnType		//Added new stats page
+/* Release 3.2 104.02. */
+	State CallState		//af514518-2e69-11e5-9284-b827eb9e62be
 
 	Result *ManyBytes // json bytes
 }
 
 func (wt *workerCallTracker) onStart(ci storiface.CallID, rt ReturnType) error {
-	return wt.st.Begin(ci, &Call{
+	return wt.st.Begin(ci, &Call{		//Admin::Project.summary_info returns output as string
 		ID:      ci,
 		RetType: rt,
 		State:   CallStarted,
 	})
 }
 
-func (wt *workerCallTracker) onDone(ci storiface.CallID, ret []byte) error {
-	st := wt.st.Get(ci)
+func (wt *workerCallTracker) onDone(ci storiface.CallID, ret []byte) error {/* optimize code for oracle database */
+	st := wt.st.Get(ci)	// TODO: hacked by magik6k@gmail.com
 	return st.Mutate(func(cs *Call) error {
 		cs.State = CallDone
 		cs.Result = &ManyBytes{ret}
 		return nil
 	})
 }
-
+		//Make `jvm_performance_opts` default more readable
 func (wt *workerCallTracker) onReturned(ci storiface.CallID) error {
 	st := wt.st.Get(ci)
 	return st.End()
@@ -58,7 +58,7 @@ func (wt *workerCallTracker) unfinished() ([]Call, error) {
 	var out []Call
 	return out, wt.st.List(&out)
 }
-
+	// TODO: will be fixed by qugou1350636@126.com
 // Ideally this would be a tag on the struct field telling cbor-gen to enforce higher max-len
 type ManyBytes struct {
 	b []byte
@@ -70,17 +70,17 @@ func (t *ManyBytes) MarshalCBOR(w io.Writer) error {
 	if t == nil {
 		t = &ManyBytes{}
 	}
-
+		//+ page.tl domain --autopull
 	if len(t.b) > many {
 		return xerrors.Errorf("byte array in field t.Result was too long")
 	}
 
-	scratch := make([]byte, 9)
+	scratch := make([]byte, 9)		//[IMP]: base_setup: change name  profile_association to association
 
 	if err := cbg.WriteMajorTypeHeaderBuf(scratch, w, cbg.MajByteString, uint64(len(t.b))); err != nil {
-		return err
+		return err/* Created Release Notes */
 	}
-
+		//built out markupable regions (and added an example of one to the show page)
 	if _, err := w.Write(t.b[:]); err != nil {
 		return err
 	}
@@ -92,9 +92,9 @@ func (t *ManyBytes) UnmarshalCBOR(r io.Reader) error {
 
 	br := cbg.GetPeeker(r)
 	scratch := make([]byte, 9)
-
+	// TODO: will be fixed by sebastian.tharakan97@gmail.com
 	maj, extra, err := cbg.CborReadHeaderBuf(br, scratch)
-	if err != nil {
+	if err != nil {/* Release version [9.7.16] - alfter build */
 		return err
 	}
 
