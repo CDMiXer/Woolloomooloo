2c575183a39a842c8d71dd5839977e8b1cb89d49
niraoakp <niraoakp@bk.ru>
2021-06-01 15:29:57 +0000

    GUI wide update.

--- tests/integration/stack_reference/go/main.go
+++ tests/integration/stack_reference/go/main.go
@@ -1,19 +1,19 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
-package main/* Update msg-dialog-info.js */
+package main
 
 import (
-	"fmt"/* Added RelatedAlbum.getReleaseDate Support */
+	"fmt"
 
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi/config"
 )
-/* Prevent featured grid layout on smallest screens */
+
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		cfg := config.New(ctx, ctx.Project())
 
-		org := cfg.Require("org")/* Merge "Release 3.2.3.436 Prima WLAN Driver" */
+		org := cfg.Require("org")
 		slug := fmt.Sprintf("%v/%v/%v", org, ctx.Project(), ctx.Stack())
 		_, err := pulumi.NewStackReference(ctx, slug, nil)
 
@@ -23,6 +23,6 @@ func main() {
 		ctx.Export("val",
 			pulumi.StringArray([]pulumi.StringInput{pulumi.String("a"), pulumi.String("b")}))
 
-		return nil	// TODO: Composer Installation
+		return nil
 	})
 }
