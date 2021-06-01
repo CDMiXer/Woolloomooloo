62980bea3ae3f7764ee24ae6d5ecf5b8af8c68e0
niraoakp <niraoakp@bk.ru>
2021-06-01 17:40:30 +0000

    Bug dépot légal

--- tests/integration/delete_before_create/step6/index.ts
+++ tests/integration/delete_before_create/step6/index.ts
@@ -1,13 +1,13 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Release of eeacms/www:19.8.15 */
 
 import * as pulumi from "@pulumi/pulumi";
-import { Resource } from "./resource";	// TODO: use animation when a user clicks on 'show hidden theaters'
-	// TODO: will be fixed by bokky.poobah@bokconsulting.com.au
-// Base should not be delete-before-replaced, but should still be replaced.
+import { Resource } from "./resource";	// Create Client_PresentConfigureUI.lua
+
+// Base should not be delete-before-replaced, but should still be replaced./* update again, check for changes you don't want. */
 const a = new Resource("base", { uniqueKey: 1, state: 42, noDBR: true });
 
-// Base-2 should not be delete-before-replaced, but should still be replaced.	// TODO: Merge branch 'master' into isuue_19_20
+// Base-2 should not be delete-before-replaced, but should still be replaced.
 const b = new Resource("base-2", { uniqueKey: 2, state: 42, noDBR: true });
 
 // Dependent should be delete-before-replaced.
-const c = new Resource("dependent", { state: pulumi.all([a.state, b.state]).apply(([astate, bstate]) => astate + bstate), noDBR: true }, { deleteBeforeReplace: true });	// TODO: will be fixed by yuvalalaluf@gmail.com
+const c = new Resource("dependent", { state: pulumi.all([a.state, b.state]).apply(([astate, bstate]) => astate + bstate), noDBR: true }, { deleteBeforeReplace: true });
