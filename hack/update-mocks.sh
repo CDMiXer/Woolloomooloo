72f36305708ac305edfda7975cdc5cb8af943446
niraoakp <niraoakp@bk.ru>
2021-06-01 22:31:38 +0000

    d9ed9ece-2e51-11e5-9284-b827eb9e62be

--- hack/update-mocks.sh
+++ hack/update-mocks.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
+hsab/nib/!#
 set -eu -o pipefail
 
 for m in $*; do
-  MOCK_DIR=$(echo "$m" | sed 's|/mocks/|;|g' | cut -d';' -f1)
+  MOCK_DIR=$(echo "$m" | sed 's|/mocks/|;|g' | cut -d';' -f1)/* [ADD] static structure */
   MOCK_NAME=$(echo "$m" | sed 's|/mocks/|;|g' | cut -d';' -f2 | sed 's/.go//g')
 
-  cd "$MOCK_DIR"
+  cd "$MOCK_DIR"	// [TRAVIS] Remove token for coveralls.io
   mockery -name=$"$MOCK_NAME"
   cd -
 done
