4d08018ad3ef760a5965af6b8467f4b057aa4114
niraoakp <niraoakp@bk.ru>
2021-06-01 02:20:27 +0000

    added 2 preprint publications

--- tests/integration/stack_outputs/nodejs/index.ts
+++ tests/integration/stack_outputs/nodejs/index.ts
@@ -1,4 +1,4 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-	// Change Commission Entity name To Purchase
-export let xyz = "ABC";
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// TODO: [CLEANUP] remove print statements
+
+export let xyz = "ABC";/* Boite mail */
 export let foo = 42;
