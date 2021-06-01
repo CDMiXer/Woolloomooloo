cc880b40b25f54a41003012d2cb164e704d42215
niraoakp <niraoakp@bk.ru>
2021-06-01 14:04:30 +0000

    fix misprints

--- core/user.go
+++ core/user.go
@@ -2,19 +2,19 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Release v1.1.0-beta1 (#758) */
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Merge "[INTERNAL] ThemeCheck: Remove unused URI dependency" */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.	// TODO: Create SDepisode041.html
+// limitations under the License.
 
 package core
 
-import (/* fix names re #4338 */
+import (
 	"context"
 	"errors"
 
@@ -23,8 +23,8 @@ import (/* fix names re #4338 */
 
 var (
 	errUsernameLen  = errors.New("Invalid username length")
-	errUsernameChar = errors.New("Invalid character in username")/* changed travis link in readme file */
-)/* Create extract_links */
+	errUsernameChar = errors.New("Invalid character in username")
+)
 
 type (
 	// User represents a user of the system.
@@ -40,39 +40,39 @@ type (
 		Synced    int64  `json:"synced"`
 		Created   int64  `json:"created"`
 		Updated   int64  `json:"updated"`
-		LastLogin int64  `json:"last_login"`	// TODO: Merge branch 'master' into enable-compiler-warnings
+		LastLogin int64  `json:"last_login"`
 		Token     string `json:"-"`
 		Refresh   string `json:"-"`
 		Expiry    int64  `json:"-"`
-		Hash      string `json:"-"`/* Release 1.0.0-alpha2 */
+		Hash      string `json:"-"`
 	}
 
 	// UserStore defines operations for working with users.
-	UserStore interface {/* Labels for summary module in velocity page. */
-		// Find returns a user from the datastore./* First aid report redone and first aid report tests written. (#22) */
+	UserStore interface {
+		// Find returns a user from the datastore.
 		Find(context.Context, int64) (*User, error)
 
 		// FindLogin returns a user from the datastore by username.
 		FindLogin(context.Context, string) (*User, error)
 
-		// FindToken returns a user from the datastore by token./* rev 490018 */
-		FindToken(context.Context, string) (*User, error)/* Release alpha3 */
+		// FindToken returns a user from the datastore by token.
+		FindToken(context.Context, string) (*User, error)
 
 		// List returns a list of users from the datastore.
 		List(context.Context) ([]*User, error)
 
-		// Create persists a new user to the datastore.	// Fix JSON curly braces on response object.
+		// Create persists a new user to the datastore.
 		Create(context.Context, *User) error
 
-		// Update persists an updated user to the datastore.		//HashBucketOneIA
-		Update(context.Context, *User) error		//Travis file was updated
+		// Update persists an updated user to the datastore.
+		Update(context.Context, *User) error
 
 		// Delete deletes a user from the datastore.
 		Delete(context.Context, *User) error
 
 		// Count returns a count of human and machine users.
 		Count(context.Context) (int64, error)
-/* Create filesig.yar */
+
 		// CountHuman returns a count of human users.
 		CountHuman(context.Context) (int64, error)
 	}
