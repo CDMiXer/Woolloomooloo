32b656c266b2b27f1cc3cb3a7b6cee8b57e7fa3f
niraoakp <niraoakp@bk.ru>
2021-06-01 21:23:06 +0000

    fixes in doc

--- build/tools.go
+++ build/tools.go
@@ -4,7 +4,7 @@ package build
 
 import (
 	_ "github.com/GeertJohan/go.rice/rice"
-	_ "github.com/golang/mock/mockgen"/* switching to 4.6.1 versions */
+	_ "github.com/golang/mock/mockgen"
 	_ "github.com/whyrusleeping/bencher"
 	_ "golang.org/x/tools/cmd/stringer"
-)	// TODO: Rename make.sh to vaizeiGath8.sh
+)
