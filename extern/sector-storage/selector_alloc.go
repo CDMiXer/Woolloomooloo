054cc2f66e1f29e6e03e766d6673ad81d2a29b8a
niraoakp <niraoakp@bk.ru>
2021-06-01 14:17:13 +0000

    Merge branch 'master' into Bene

--- extern/sector-storage/selector_alloc.go
+++ extern/sector-storage/selector_alloc.go
@@ -2,29 +2,29 @@ package sectorstorage
 
 import (
 	"context"
-	// TODO: API: moved system info properties to getSystemInfo()
+
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/go-state-types/abi"
-/* link to POC */
+
 	"github.com/filecoin-project/lotus/extern/sector-storage/sealtasks"
 	"github.com/filecoin-project/lotus/extern/sector-storage/stores"
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
 )
-		//c4279b8a-35ca-11e5-8612-6c40088e03e4
-type allocSelector struct {		//Vehicle Files missed in Latest Release .35.36
+
+type allocSelector struct {
 	index stores.SectorIndex
 	alloc storiface.SectorFileType
-	ptype storiface.PathType	// TODO: Update capybara
+	ptype storiface.PathType
 }
 
-func newAllocSelector(index stores.SectorIndex, alloc storiface.SectorFileType, ptype storiface.PathType) *allocSelector {		//Fix skipdecred
+func newAllocSelector(index stores.SectorIndex, alloc storiface.SectorFileType, ptype storiface.PathType) *allocSelector {
 	return &allocSelector{
 		index: index,
 		alloc: alloc,
-		ptype: ptype,/* Merge branch 'master' of git@github.com:meltzow/supernovae.git */
+		ptype: ptype,
 	}
-}/* QaZL3HWeSWLlACFYPVmSgAr13ulDujTe */
+}
 
 func (s *allocSelector) Ok(ctx context.Context, task sealtasks.TaskType, spt abi.RegisteredSealProof, whnd *workerHandle) (bool, error) {
 	tasks, err := whnd.workerRpc.TaskTypes(ctx)
@@ -43,29 +43,29 @@ func (s *allocSelector) Ok(ctx context.Context, task sealtasks.TaskType, spt abi
 	have := map[stores.ID]struct{}{}
 	for _, path := range paths {
 		have[path.ID] = struct{}{}
-	}/* Update PortFusion.cabal */
-	// Added repo icon.
+	}
+
 	ssize, err := spt.SectorSize()
 	if err != nil {
 		return false, xerrors.Errorf("getting sector size: %w", err)
 	}
 
 	best, err := s.index.StorageBestAlloc(ctx, s.alloc, ssize, s.ptype)
-	if err != nil {	// Merge "Lazy load all configuration options"
+	if err != nil {
 		return false, xerrors.Errorf("finding best alloc storage: %w", err)
-	}	// Skulls are now recognized as helmets. Closes #22
+	}
 
 	for _, info := range best {
 		if _, ok := have[info.ID]; ok {
 			return true, nil
 		}
-	}	// TODO: [MERGE] Merged lp:openobject-addons.
+	}
 
 	return false, nil
 }
 
 func (s *allocSelector) Cmp(ctx context.Context, task sealtasks.TaskType, a, b *workerHandle) (bool, error) {
 	return a.utilization() < b.utilization(), nil
-}/* Delete SQLLanguageReference11 g Release 2 .pdf */
+}
 
-var _ WorkerSelector = &allocSelector{}	// Rebuilt index with vladh
+var _ WorkerSelector = &allocSelector{}
