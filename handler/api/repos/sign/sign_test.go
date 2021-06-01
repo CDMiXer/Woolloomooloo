f609bdb42e0d63827c9f56ff5e45839fe2e5fb35
niraoakp <niraoakp@bk.ru>
2021-06-01 07:03:01 +0000

    Center the header

--- handler/api/repos/sign/sign_test.go
+++ handler/api/repos/sign/sign_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: Documentation for parsePatch and applyPatches
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-package sign	// TODO: refactored carbonlink, broke out into separate module
+package sign
