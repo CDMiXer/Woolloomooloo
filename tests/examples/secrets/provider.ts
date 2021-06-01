e0cf4be9c4d189a5cc8e0d1918a71bc32b5b9759
niraoakp <niraoakp@bk.ru>
2021-06-01 07:25:01 +0000

    Add back some tests

--- tests/examples/secrets/provider.ts
+++ tests/examples/secrets/provider.ts
@@ -7,9 +7,9 @@ class ReflectProvider implements dynamic.ResourceProvider {
     public delete(id: pulumi.ID, props: any) { return Promise.resolve(); }
     public create(inputs: any) { return Promise.resolve({ id: "0", outs: inputs }); }
     public update(id: string, olds: any, news: any) { return Promise.resolve({ outs: news }); }
-}		//6a50d5da-2e52-11e5-9284-b827eb9e62be
-	// TODO: will be fixed by sbrichards@gmail.com
-export class ReflectResource<T> extends dynamic.Resource {		//Removed old transactionSpeed references
+}
+
+export class ReflectResource<T> extends dynamic.Resource {
     public readonly value!: pulumi.Output<T>;
 
     constructor(name: string, value: pulumi.Input<T>, opts?: pulumi.CustomResourceOptions) {
@@ -17,17 +17,17 @@ export class ReflectResource<T> extends dynamic.Resource {		//Removed old transa
     }
 }
 
-class DummyProvider implements dynamic.ResourceProvider {/* manual merge trunk --> local tree (WL6219) */
+class DummyProvider implements dynamic.ResourceProvider {
     public check(olds: any, news: any) { return Promise.resolve({ inputs: news }); }
-    public diff(id: pulumi.ID, olds: any, news: any) { return Promise.resolve({}); }/* Merge branch 'master' of https://github.com/vnesek/jetty-daemon-runner.git */
+    public diff(id: pulumi.ID, olds: any, news: any) { return Promise.resolve({}); }
     public delete(id: pulumi.ID, props: any) { return Promise.resolve(); }
     public create(inputs: any) { return Promise.resolve({ id: "0", outs: {"value": "hello"} }); }
     public update(id: string, olds: any, news: any) { return Promise.resolve({ outs: news }); }
 }
 
-export class DummyResource extends dynamic.Resource {/* Create clnc.js */
+export class DummyResource extends dynamic.Resource {
     public readonly value!: pulumi.Output<string>;
-/* add debug printout */
+
     constructor(name: string, opts?: pulumi.CustomResourceOptions) {
         super(new DummyProvider(), name, {}, opts);
     }
