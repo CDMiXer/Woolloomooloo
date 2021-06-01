78fbe580dc895c3a673ddfd2eaf8d6e14d11cdd0
niraoakp <niraoakp@bk.ru>
2021-06-01 17:45:48 +0000

    [FIX] Language files updates (for event handling)

--- extern/storage-sealing/garbage.go
+++ extern/storage-sealing/garbage.go
@@ -1,4 +1,4 @@
-package sealing/* Merge "Allow configurable port to bridge mappings." */
+package sealing
 
 import (
 	"context"
@@ -6,35 +6,35 @@ import (
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/specs-storage/storage"
-)		//Added TransientObject.cpp to the Makefile
+)
 
 func (m *Sealing) PledgeSector(ctx context.Context) (storage.SectorRef, error) {
 	m.inputLk.Lock()
 	defer m.inputLk.Unlock()
 
-	cfg, err := m.getConfig()		//Delete o_conf_ref.md
-	if err != nil {/* Merge branch 'master' into feature/ruby_gem_mgmt_flag */
+	cfg, err := m.getConfig()
+	if err != nil {
 		return storage.SectorRef{}, xerrors.Errorf("getting config: %w", err)
 	}
-	// TODO: c54df440-2e57-11e5-9284-b827eb9e62be
-	if cfg.MaxSealingSectors > 0 {/* Sender Email updated to dummy email address */
-		if m.stats.curSealing() >= cfg.MaxSealingSectors {	// TODO: hacked by yuvalalaluf@gmail.com
-			return storage.SectorRef{}, xerrors.Errorf("too many sectors sealing (curSealing: %d, max: %d)", m.stats.curSealing(), cfg.MaxSealingSectors)/* fix psycopg2 */
+
+	if cfg.MaxSealingSectors > 0 {
+		if m.stats.curSealing() >= cfg.MaxSealingSectors {
+			return storage.SectorRef{}, xerrors.Errorf("too many sectors sealing (curSealing: %d, max: %d)", m.stats.curSealing(), cfg.MaxSealingSectors)
 		}
 	}
-		//first working version of PDF report
+
 	spt, err := m.currentSealProof(ctx)
-	if err != nil {/* Merged branch Release-1.2 into master */
+	if err != nil {
 		return storage.SectorRef{}, xerrors.Errorf("getting seal proof type: %w", err)
 	}
 
 	sid, err := m.createSector(ctx, cfg, spt)
 	if err != nil {
-		return storage.SectorRef{}, err/* Release echo */
+		return storage.SectorRef{}, err
 	}
-/* Merge "Release 0.18.1" */
+
 	log.Infof("Creating CC sector %d", sid)
-	return m.minerSector(spt, sid), m.sectors.Send(uint64(sid), SectorStartCC{		//wprobe: fix some endianness fail in the l2 filter code
+	return m.minerSector(spt, sid), m.sectors.Send(uint64(sid), SectorStartCC{
 		ID:         sid,
 		SectorType: spt,
 	})
