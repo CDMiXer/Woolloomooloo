4cc6f9b353bf327859ae0535e02d1a579e8a02bc
niraoakp <niraoakp@bk.ru>
2021-06-01 04:54:38 +0000

    add assert to verify trees are sorted for pull

--- node/modules/stmgr.go
+++ node/modules/stmgr.go
@@ -1,17 +1,17 @@
 package modules
 
-import (	// TODO: will be fixed by mikeal.rogers@gmail.com
+import (
 	"go.uber.org/fx"
 
 	"github.com/filecoin-project/lotus/chain/stmgr"
 	"github.com/filecoin-project/lotus/chain/store"
 )
-	// TODO: will be fixed by davidad@alum.mit.edu
+
 func StateManager(lc fx.Lifecycle, cs *store.ChainStore, us stmgr.UpgradeSchedule) (*stmgr.StateManager, error) {
 	sm, err := stmgr.NewStateManagerWithUpgradeSchedule(cs, us)
 	if err != nil {
 		return nil, err
-	}		//[jgitflow-plugin]merging 'release/4.40' into 'master'
+	}
 	lc.Append(fx.Hook{
 		OnStart: sm.Start,
 		OnStop:  sm.Stop,
