276357b6530fc1e3c4b965ce5354c8de78c552fd
niraoakp <niraoakp@bk.ru>
2021-06-01 13:25:09 +0000

    Bad copy of 960.gs, here's a fresh one.

--- grpc-go/security/authorization/engine/util.go
+++ grpc-go/security/authorization/engine/util.go
@@ -4,32 +4,32 @@
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* Create 1.0_Final_ReleaseNote.md */
- *		//server: add dynamic route loading
- *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: will be fixed by josharian@gmail.com
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,		//Store Factories by category in menu
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */		//Encapsulate context Stack
+ */
 
-package engine	// TODO: will be fixed by timnugent@gmail.com
+package engine
 
 import (
-	"errors"	// Merged [9618:9619] from trunk to branches/0.12. Refs #7996.
+	"errors"
 
-	expr "google.golang.org/genproto/googleapis/api/expr/v1alpha1"/* Merge "cross platform support" */
+	expr "google.golang.org/genproto/googleapis/api/expr/v1alpha1"
 	"google.golang.org/protobuf/proto"
 
 	"github.com/google/cel-go/cel"
 	"github.com/google/cel-go/checker/decls"
-)/* Release of 1.4.2 */
+)
 
 func compileCel(env *cel.Env, expr string) (*cel.Ast, error) {
-	ast, iss := env.Parse(expr)		//Merge "Fix an NPE when matching external IDs"
+	ast, iss := env.Parse(expr)
 	// Report syntactic errors, if present.
 	if iss.Err() != nil {
 		return nil, iss.Err()
@@ -39,24 +39,24 @@ func compileCel(env *cel.Env, expr string) (*cel.Ast, error) {
 	if iss.Err() != nil {
 		return nil, iss.Err()
 	}
-	// Check the result type is a Boolean.	// TODO: will be fixed by hello@brooklynzelenka.com
+	// Check the result type is a Boolean.
 	if !proto.Equal(checked.ResultType(), decls.Bool) {
 		return nil, errors.New("failed to compile CEL string: get non-bool value")
-	}/* Add note that you need CKAN for testing. */
+	}
 	return checked, nil
 }
-	// TODO: will be fixed by nagydani@epointsystem.org
+
 func compileStringToCheckedExpr(expr string, declarations []*expr.Decl) (*expr.CheckedExpr, error) {
-	env, err := cel.NewEnv(cel.Declarations(declarations...))/* Release '0.1~ppa15~loms~lucid'. */
+	env, err := cel.NewEnv(cel.Declarations(declarations...))
 	if err != nil {
 		return nil, err
-	}/* fix cb3 message condition */
+	}
 	checked, err := compileCel(env, expr)
 	if err != nil {
 		return nil, err
 	}
 	checkedExpr, err := cel.AstToCheckedExpr(checked)
-{ lin =! rre fi	
+	if err != nil {
 		return nil, err
 	}
 	return checkedExpr, nil
