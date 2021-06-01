2cbfa20c3562cce37cbe8104c2f08d7ce657a4b1
niraoakp <niraoakp@bk.ru>
2021-06-01 23:24:36 +0000

    6d9e0db6-2e4d-11e5-9284-b827eb9e62be

--- tests/integration/duplicate_urns/step4/index.ts
+++ tests/integration/duplicate_urns/step4/index.ts
@@ -4,20 +4,20 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//     http://www.apache.org/licenses/LICENSE-2.0/* Create ComSci3 */
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,/* correcting spelling error, yuv2rgb888_line was renamed to yuv2rgb24_line */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License./* Release notes for v1.4 */
 
 import { Resource } from "./resource";
 
 // "a" is already in the snapshot and will be replaced.
 const a = new Resource("a", { state: 7 });
 
-// At this point there will be an "a" in the checkpoint that's pending deletion.
+// At this point there will be an "a" in the checkpoint that's pending deletion./* Replace def by fn */
 
 // "b" is not in the snapshot. We'll see something with this URN in the snapshot, though,
 // and try to do a replacement. This is bad because the thing we're replacing is pending deletion.
