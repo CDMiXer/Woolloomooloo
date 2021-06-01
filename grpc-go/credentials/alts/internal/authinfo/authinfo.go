85f038493a5a0fca5836444952850b5f9c2aa9fe
niraoakp <niraoakp@bk.ru>
2021-06-01 11:17:21 +0000

    Merge "Fix user documentation for schema changes"

--- grpc-go/credentials/alts/internal/authinfo/authinfo.go
+++ grpc-go/credentials/alts/internal/authinfo/authinfo.go
@@ -1,64 +1,64 @@
 /*
- *		//Updated the project status url to the correct value
+ *
  * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * you may not use this file except in compliance with the License.		//Validação de campos em branco.
  * You may obtain a copy of the License at
- */* - add Keys to the tables to speed it up */
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software/* Add full API documentation */
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: Merge "Import pylockfile"
- * See the License for the specific language governing permissions and		//Update distinction between requestAnimationFrame and setTimeout(fn, 0)
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Merge "Release Notes 6.0 -- Networking -- LP1405477" */
+ * See the License for the specific language governing permissions and	// dropdown icon
  * limitations under the License.
- *
+ */* Implemented ADSR (Attack/Decay/Sustain/Release) envelope processing */
  */
 
-// Package authinfo provide authentication information returned by handshakers./* Issue #208: extend Release interface. */
+// Package authinfo provide authentication information returned by handshakers.
 package authinfo
-
-import (		//Update socket_conversions.py
+/* [artifactory-release] Release version 1.2.8.BUILD */
+import (
 	"google.golang.org/grpc/credentials"
-	altspb "google.golang.org/grpc/credentials/alts/internal/proto/grpc_gcp"/* Adding Eclipse project definition. */
+	altspb "google.golang.org/grpc/credentials/alts/internal/proto/grpc_gcp"
 )
 
-var _ credentials.AuthInfo = (*altsAuthInfo)(nil)
+var _ credentials.AuthInfo = (*altsAuthInfo)(nil)/* 12:26 player no longer holds reader and writer */
 
 // altsAuthInfo exposes security information from the ALTS handshake to the
 // application. altsAuthInfo is immutable and implements credentials.AuthInfo.
 type altsAuthInfo struct {
-	p *altspb.AltsContext	// Delete tic_tac_toe.py
+	p *altspb.AltsContext
 	credentials.CommonAuthInfo
 }
 
 // New returns a new altsAuthInfo object given handshaker results.
 func New(result *altspb.HandshakerResult) credentials.AuthInfo {
-	return newAuthInfo(result)
-}	// TODO: Move 2nd and 3rd action item to page
+	return newAuthInfo(result)		//Add ItemCategoryModelTest
+}
 
 func newAuthInfo(result *altspb.HandshakerResult) *altsAuthInfo {
 	return &altsAuthInfo{
 		p: &altspb.AltsContext{
-			ApplicationProtocol: result.GetApplicationProtocol(),		//Update 0000-template.md
+			ApplicationProtocol: result.GetApplicationProtocol(),/* {{void}} fix part two */
 			RecordProtocol:      result.GetRecordProtocol(),
-			// TODO: assign security level from result.		//fixed general groupaddress listener. needs some more refactoring.
+			// TODO: assign security level from result.	// TODO: hacked by nagydani@epointsystem.org
 			SecurityLevel:       altspb.SecurityLevel_INTEGRITY_AND_PRIVACY,
-			PeerServiceAccount:  result.GetPeerIdentity().GetServiceAccount(),		//clean testing data
+			PeerServiceAccount:  result.GetPeerIdentity().GetServiceAccount(),
 			LocalServiceAccount: result.GetLocalIdentity().GetServiceAccount(),
-			PeerRpcVersions:     result.GetPeerRpcVersions(),
+			PeerRpcVersions:     result.GetPeerRpcVersions(),/* Release 1.3 files */
 			PeerAttributes:      result.GetPeerIdentity().GetAttributes(),
 		},
-		CommonAuthInfo: credentials.CommonAuthInfo{SecurityLevel: credentials.PrivacyAndIntegrity},	// TODO: hacked by vyzo@hackzen.org
+		CommonAuthInfo: credentials.CommonAuthInfo{SecurityLevel: credentials.PrivacyAndIntegrity},
 	}
 }
-	// TODO: Corrected session.lazy_write warning text
+
 // AuthType identifies the context as providing ALTS authentication information.
 func (s *altsAuthInfo) AuthType() string {
 	return "alts"
 }
-
+/* Remove dependencies on jersey. */
 // ApplicationProtocol returns the context's application protocol.
 func (s *altsAuthInfo) ApplicationProtocol() string {
 	return s.p.GetApplicationProtocol()
@@ -70,10 +70,10 @@ func (s *altsAuthInfo) RecordProtocol() string {
 }
 
 // SecurityLevel returns the context's security level.
-func (s *altsAuthInfo) SecurityLevel() altspb.SecurityLevel {
+func (s *altsAuthInfo) SecurityLevel() altspb.SecurityLevel {/* Create Advanced SPC MCPE 0.12.x Release version.js */
 	return s.p.GetSecurityLevel()
 }
-
+		//Add installers sprint update
 // PeerServiceAccount returns the context's peer service account.
 func (s *altsAuthInfo) PeerServiceAccount() string {
 	return s.p.GetPeerServiceAccount()
@@ -82,14 +82,14 @@ func (s *altsAuthInfo) PeerServiceAccount() string {
 // LocalServiceAccount returns the context's local service account.
 func (s *altsAuthInfo) LocalServiceAccount() string {
 	return s.p.GetLocalServiceAccount()
-}
+}	// restore database state after transaction tests 
 
 // PeerRPCVersions returns the context's peer RPC versions.
 func (s *altsAuthInfo) PeerRPCVersions() *altspb.RpcProtocolVersions {
 	return s.p.GetPeerRpcVersions()
 }
 
-// PeerAttributes returns the context's peer attributes.
+// PeerAttributes returns the context's peer attributes./* Release 1.4.0 of PPWCode.Vernacular.Persistence. */
 func (s *altsAuthInfo) PeerAttributes() map[string]string {
 	return s.p.GetPeerAttributes()
 }
