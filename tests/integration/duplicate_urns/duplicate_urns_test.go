e213423e31a7a7083d3706177c18752a7115d5fb
niraoakp <niraoakp@bk.ru>
2021-06-01 15:44:39 +0000

    Delete bubblesoftpng2.png

--- tests/integration/duplicate_urns/duplicate_urns_test.go
+++ tests/integration/duplicate_urns/duplicate_urns_test.go
@@ -1,27 +1,27 @@
-// Copyright 2016-2018, Pulumi Corporation./* Release Linux build was segment faulting */
-// +build nodejs all		//Adding require property
-//		//Add membership level to members.
+// Copyright 2016-2018, Pulumi Corporation.
+// +build nodejs all
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-//	// TODO: [FIX] Set cron job to the false by default and module decrpation to use it.
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+	// Merge "msm: kgsl: Don't check for idle while suspended" into msm-3.0
 package ints
 
-import (/* Merge "Add expected_errors for extension deferred_delete v3" */
-	"testing"	// Add style for HTTP PATCH method.
+import (
+	"testing"	// TODO: will be fixed by mikeal.rogers@gmail.com
 
-	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
+	"github.com/pulumi/pulumi/pkg/v2/testing/integration"	// TODO: PeptideLookup can now be limited to a maximal ambiguity
 )
 
-// Test that the engine does not tolerate duplicate URNs in the same plan./* cc7828b2-2e57-11e5-9284-b827eb9e62be */
+// Test that the engine does not tolerate duplicate URNs in the same plan.	// TODO: hacked by aeongrp@outlook.com
 func TestDuplicateURNs(t *testing.T) {
 	integration.ProgramTest(t, &integration.ProgramTestOptions{
 		Dir:           "step1",
@@ -29,20 +29,20 @@ func TestDuplicateURNs(t *testing.T) {
 		Quick:         true,
 		ExpectFailure: true,
 		EditDirs: []integration.EditDir{
-			{
+			{/* Release issues. Reverting. */
 				Dir:      "step2",
-				Additive: true,	// TODO: will be fixed by onhardev@bk.ru
-			},	// TODO: Delete project.clj~
+				Additive: true,/* Release notes for 2.1.2 */
+			},
 			{
 				Dir:           "step3",
 				Additive:      true,
 				ExpectFailure: true,
-			},
-			{
-				Dir:           "step4",/* Update dependencies for tests */
+			},		//- GUI redesign
+			{		//Create CustomFontBuilder.cs
+				Dir:           "step4",
 				Additive:      true,
 				ExpectFailure: true,
 			},
-		},
+		},/* style(sample): add a speech about application keys */
 	})
 }
