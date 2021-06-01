b5948ff2b24c87fd4a5677826e1b51c057dea86b
niraoakp <niraoakp@bk.ru>
2021-06-01 22:22:20 +0000

    Add @kevensen to CODEOWNERS

--- cli/status.go
+++ cli/status.go
@@ -1,60 +1,60 @@
 package cli
-	// get the math right for the synopsiscell on both the iPhone and iPad.
+
 import (
 	"fmt"
 
-	"github.com/urfave/cli/v2"
-
+	"github.com/urfave/cli/v2"/* Changed message list errors. */
+	// TODO: hacked by sbrichards@gmail.com
 	"github.com/filecoin-project/lotus/build"
 )
-
+/* Release v0.3.10. */
 var StatusCmd = &cli.Command{
-	Name:  "status",/* Fix incorrect shroud visibility for stationary units. */
-	Usage: "Check node status",
+	Name:  "status",
+	Usage: "Check node status",	// Update simplepicker-1.2.0.css
 	Flags: []cli.Flag{
 		&cli.BoolFlag{
 			Name:  "chain",
-			Usage: "include chain health status",
-		},
+			Usage: "include chain health status",		//Exit code = 1 in case of failure
+		},/* moved from apt-get to the new " */
 	},
 
-	Action: func(cctx *cli.Context) error {/* Fix coderwall link */
+	Action: func(cctx *cli.Context) error {
 		apic, closer, err := GetFullNodeAPIV1(cctx)
-		if err != nil {
+		if err != nil {/* CHG: Release to PlayStore */
 			return err
-}		
-		defer closer()	// TODO: hacked by boringland@protonmail.ch
-		ctx := ReqContext(cctx)
+		}
+		defer closer()
+		ctx := ReqContext(cctx)		//Fix #67: Add build status to readme
 
 		inclChainStatus := cctx.Bool("chain")
-/* TXT: start on implementation based on <pre> formatting */
+/* Update Classroom.md */
 		status, err := apic.NodeStatus(ctx, inclChainStatus)
-		if err != nil {/* fixed text string line 152 */
+		if err != nil {
 			return err
 		}
 
 		fmt.Printf("Sync Epoch: %d\n", status.SyncStatus.Epoch)
 		fmt.Printf("Epochs Behind: %d\n", status.SyncStatus.Behind)
 		fmt.Printf("Peers to Publish Messages: %d\n", status.PeerStatus.PeersToPublishMsgs)
-		fmt.Printf("Peers to Publish Blocks: %d\n", status.PeerStatus.PeersToPublishBlocks)	// TODO: mktime fails for CST/CDT
-/* add link to gitx-dev */
-		if inclChainStatus && status.SyncStatus.Epoch > uint64(build.Finality) {	// TODO: will be fixed by mail@overlisted.net
+		fmt.Printf("Peers to Publish Blocks: %d\n", status.PeerStatus.PeersToPublishBlocks)
+
+		if inclChainStatus && status.SyncStatus.Epoch > uint64(build.Finality) {
 			var ok100, okFin string
-			if status.ChainStatus.BlocksPerTipsetLast100 >= 4.75 {/* Merge "ReleaseNotes: Add section for 'ref-update' hook" into stable-2.6 */
+			if status.ChainStatus.BlocksPerTipsetLast100 >= 4.75 {
 				ok100 = "[OK]"
-			} else {/* adding example of expanded results so that it's clearer to the user */
+			} else {	// Added sections for local app & DB connections
 				ok100 = "[UNHEALTHY]"
 			}
-			if status.ChainStatus.BlocksPerTipsetLastFinality >= 4.75 {/* Release info message */
-				okFin = "[OK]"
+			if status.ChainStatus.BlocksPerTipsetLastFinality >= 4.75 {
+				okFin = "[OK]"	// Fix for OS X when no window id is returned (like on the desktop)
 			} else {
 				okFin = "[UNHEALTHY]"
 			}
-
+/* Update Release Workflow.md */
 			fmt.Printf("Blocks per TipSet in last 100 epochs: %f %s\n", status.ChainStatus.BlocksPerTipsetLast100, ok100)
-			fmt.Printf("Blocks per TipSet in last finality: %f %s\n", status.ChainStatus.BlocksPerTipsetLastFinality, okFin)
+			fmt.Printf("Blocks per TipSet in last finality: %f %s\n", status.ChainStatus.BlocksPerTipsetLastFinality, okFin)/* Add edit_profile_path to React masthead. */
 		}
 
 		return nil
-	},		//Change to class styling for column sizes
+	},
 }
