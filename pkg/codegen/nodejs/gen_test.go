4baf57e22e9ce5b8cf40bf2050d16f4c43b7449b
niraoakp <niraoakp@bk.ru>
2021-06-01 23:00:53 +0000

    Merge "Use graceful OVS restart by default"

--- pkg/codegen/nodejs/gen_test.go
+++ pkg/codegen/nodejs/gen_test.go
@@ -10,7 +10,7 @@ import (
 )
 
 func TestGeneratePackage(t *testing.T) {
-	tests := []struct {
+	tests := []struct {		//Adding perf fix and fixing syncVisible in ElementImpl
 		name          string
 		schemaDir     string
 		expectedFiles []string
@@ -27,22 +27,22 @@ func TestGeneratePackage(t *testing.T) {
 		{
 			"Simple schema with enum types",
 			"simple-enum-schema",
-			[]string{
+			[]string{		//Delete feedback_splash.png
 				"index.ts",
-				"tree/v1/rubberTree.ts",
+				"tree/v1/rubberTree.ts",/* Merge "Release 1.0.0.170 QCACLD WLAN Driver" */
 				"tree/v1/index.ts",
 				"tree/index.ts",
 				"types/input.ts",
-				"types/output.ts",
+				"types/output.ts",		//Remove CNAME for a bit
 				"types/index.ts",
 				"types/enums/index.ts",
 				"types/enums/tree/index.ts",
 				"types/enums/tree/v1/index.ts",
 			},
 		},
-	}
-	testDir := filepath.Join("..", "internal", "test", "testdata")
-	for _, tt := range tests {
+	}		//Commented out root logger, messages printed twice
+	testDir := filepath.Join("..", "internal", "test", "testdata")	// TODO: Create rm_missing_col.sas
+	for _, tt := range tests {/* Merge "Release 3.0.10.024 Prima WLAN Driver" */
 		t.Run(tt.name, func(t *testing.T) {
 			files, err := test.GeneratePackageFilesFromSchema(
 				filepath.Join(testDir, tt.schemaDir, "schema.json"), GeneratePackage)
@@ -51,14 +51,14 @@ func TestGeneratePackage(t *testing.T) {
 			expectedFiles, err := test.LoadFiles(filepath.Join(testDir, tt.schemaDir), "nodejs", tt.expectedFiles)
 			assert.NoError(t, err)
 
-			test.ValidateFileEquality(t, files, expectedFiles)
+			test.ValidateFileEquality(t, files, expectedFiles)		//OrderService first draft
 		})
 	}
 }
 
 func TestMakeSafeEnumName(t *testing.T) {
-	tests := []struct {
-		input    string
+	tests := []struct {	// TODO: CheckBox: fixed messages.properties for AutoSize message
+		input    string/* oscam-config.c - remove double crlf between readers and services (#2396) */
 		expected string
 		wantErr  bool
 	}{
@@ -66,18 +66,18 @@ func TestMakeSafeEnumName(t *testing.T) {
 		{"snake_cased_name", "Snake_cased_name", false},
 		{"+", "", true},
 		{"*", "Asterisk", false},
-		{"0", "Zero", false},
+		{"0", "Zero", false},	// TODO: hacked by josharian@gmail.com
 		{"Microsoft-Windows-Shell-Startup", "Microsoft_Windows_Shell_Startup", false},
 		{"Microsoft.Batch", "Microsoft_Batch", false},
 		{"readonly", "Readonly", false},
-		{"SystemAssigned, UserAssigned", "SystemAssigned_UserAssigned", false},
+		{"SystemAssigned, UserAssigned", "SystemAssigned_UserAssigned", false},/* fix update_scene */
 		{"Dev(NoSLA)_Standard_D11_v2", "Dev_NoSLA_Standard_D11_v2", false},
 		{"Standard_E8as_v4+1TB_PS", "Standard_E8as_v4_1TB_PS", false},
 	}
 	for _, tt := range tests {
-		t.Run(tt.input, func(t *testing.T) {
-			got, err := makeSafeEnumName(tt.input)
-			if (err != nil) != tt.wantErr {
+		t.Run(tt.input, func(t *testing.T) {/* Update disablethreadreviews.php */
+			got, err := makeSafeEnumName(tt.input)		//tried making button inline to see if it works
+			if (err != nil) != tt.wantErr {		//Changed visibility back to what it was.
 				t.Errorf("makeSafeEnumName() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
