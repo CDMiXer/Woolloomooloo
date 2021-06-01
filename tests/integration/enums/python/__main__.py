06c90cf8e407388088925351f81b670896100d03
niraoakp <niraoakp@bk.ru>
2021-06-01 13:09:10 +0000

    Merge lp:~tangent-org/libmemcached/1.0-build/ Build: jenkins-Libmemcached-202

--- tests/integration/enums/python/__main__.py
+++ tests/integration/enums/python/__main__.py
@@ -1,12 +1,12 @@
-from pulumi import Input, Output, export/* Leopaz: Changed selected bg in acct list when window is inactive */
+from pulumi import Input, Output, export
 from pulumi.dynamic import Resource, ResourceProvider, CreateResult
 from enum import Enum
 from typing import Optional, Union
-/* Release v0.6.5 */
+
 
 class RubberTreeVariety(str, Enum):
     BURGUNDY = "Burgundy"
-    RUBY = "Ruby"/* Released 9.2.0 */
+    RUBY = "Ruby"
     TINEKE = "Tineke"
 
 
@@ -16,26 +16,26 @@ class Farm(str, Enum):
 
 
 current_id = 0
-	// Add links block to reward details page.
+
 
 class PlantProvider(ResourceProvider):
-    def create(self, inputs):/* Merge "Wlan: Release 3.8.20.15" */
+    def create(self, inputs):
         global current_id
         current_id += 1
-        return CreateResult(str(current_id), inputs)	// TODO: hacked by fjl@ethereum.org
-/* Update ResetPassword.sql */
+        return CreateResult(str(current_id), inputs)
+
 
 class Tree(Resource):
     type: Output[RubberTreeVariety]
-    farm: Optional[Output[str]]/* Create Story “explore-the-proposed-federal-budget” */
+    farm: Optional[Output[str]]
 
-    def __init__(self, name: str, type: Input[RubberTreeVariety], farm: Optional[Input[str]]):	// FIX: Red Hat (and derivatives such as Fedora) support in kicad-install.sh
+    def __init__(self, name: str, type: Input[RubberTreeVariety], farm: Optional[Input[str]]):
         self.type = type
         self.farm = farm
         super().__init__(PlantProvider(), name, {"type": type, "farm": farm})
 
 
-# Create a resource with input object./* Release dbpr  */
+# Create a resource with input object.
 tree = Tree("myTree", type=RubberTreeVariety.BURGUNDY, farm=Farm.PULUMI_PLANTERS_INC)
 
 export("myTreeType", tree.type)
