2570ea562c8178466df794559a05f9525c8fdc82
niraoakp <niraoakp@bk.ru>
2021-06-01 14:37:19 +0000

    Icon for the "Featured" section

--- operator/runner/before.go
+++ operator/runner/before.go
@@ -3,13 +3,13 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+///* Make signjar less lazy. Hopefully this resolves certificate issues */
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+///* Edited translation.xml via GitHub */
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,		//Update GHContextMenu.podspec
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// See the License for the specific language governing permissions and/* Released version 0.5.62 */
+// limitations under the License./* Refactoring. Remove Owned method from Sema. */
 
 package runner
