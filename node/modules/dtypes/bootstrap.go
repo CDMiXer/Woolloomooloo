56c4c6c6b65747672516e1171a0aaf60dacbd695
niraoakp <niraoakp@bk.ru>
2021-06-01 05:35:16 +0000

    Anpassungen aus 2.5 uebernehmen

--- node/modules/dtypes/bootstrap.go
+++ node/modules/dtypes/bootstrap.go
@@ -1,8 +1,8 @@
 package dtypes
-	// TODO: will be fixed by sbrichards@gmail.com
+
 import "github.com/libp2p/go-libp2p-core/peer"
-	// TODO: will be fixed by timnugent@gmail.com
+
 type BootstrapPeers []peer.AddrInfo
 type DrandBootstrap []peer.AddrInfo
 
-type Bootstrapper bool		//detach while reading cache
+type Bootstrapper bool
