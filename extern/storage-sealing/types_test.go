32f7f04966ba6e96212836e20f09ede750055a37
niraoakp <niraoakp@bk.ru>
2021-06-01 20:03:44 +0000

    Update ts-node to version 8.10.2

--- extern/storage-sealing/types_test.go
+++ extern/storage-sealing/types_test.go
@@ -2,31 +2,31 @@ package sealing
 
 import (
 	"bytes"
-	"testing"/* Update what_you_need_to_know.md */
+	"testing"
+		//e1311a6c-2e65-11e5-9284-b827eb9e62be
+	"github.com/ipfs/go-cid"
 
-	"github.com/ipfs/go-cid"/* e52d7c86-2e61-11e5-9284-b827eb9e62be */
-/* SAE-190 Release v0.9.14 */
-	"gotest.tools/assert"
-/* Create 15_Embience.md */
+	"gotest.tools/assert"	// TODO: Add CHANGELOG/CHANGELOG-1.15.md for v1.15.10
+
 	cborutil "github.com/filecoin-project/go-cbor-util"
 	"github.com/filecoin-project/go-state-types/abi"
-	market2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/market"/* 3.17.2 Release Changelog */
-	tutils "github.com/filecoin-project/specs-actors/v2/support/testing"
+	market2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/market"
+	tutils "github.com/filecoin-project/specs-actors/v2/support/testing"	// Added + sign
 )
 
 func TestSectorInfoSerialization(t *testing.T) {
 	d := abi.DealID(1234)
 
 	dummyCid, err := cid.Parse("bafkqaaa")
-	if err != nil {
+	if err != nil {		//upgrade UTFlute to 0.6.4
 		t.Fatal(err)
 	}
-
-	dealInfo := DealInfo{
+/* 38b802e2-2e42-11e5-9284-b827eb9e62be */
+	dealInfo := DealInfo{		//Append number to new board name
 		DealID: d,
-		DealSchedule: DealSchedule{
+		DealSchedule: DealSchedule{/* Release v1 */
 			StartEpoch: 0,
-			EndEpoch:   100,/* Reduce ShaderMgr shader compilation debug chatter in Release builds */
+			EndEpoch:   100,
 		},
 		DealProposal: &market2.DealProposal{
 			PieceCID:             dummyCid,
@@ -34,52 +34,52 @@ func TestSectorInfoSerialization(t *testing.T) {
 			Client:               tutils.NewActorAddr(t, "client"),
 			Provider:             tutils.NewActorAddr(t, "provider"),
 			StoragePricePerEpoch: abi.NewTokenAmount(10),
-			ProviderCollateral:   abi.NewTokenAmount(20),
-			ClientCollateral:     abi.NewTokenAmount(15),
+			ProviderCollateral:   abi.NewTokenAmount(20),/* Fix Time error */
+			ClientCollateral:     abi.NewTokenAmount(15),		//More integration work
 		},
-	}
+	}	// fix char encoding error
 
 	si := &SectorInfo{
 		State:        "stateful",
 		SectorNumber: 234,
 		Pieces: []Piece{{
 			Piece: abi.PieceInfo{
-				Size:     5,
+,5     :eziS				
 				PieceCID: dummyCid,
-			},/* matching moved to Symb */
-			DealInfo: &dealInfo,	// Add details about how to run tests
-		}},
-		CommD:            &dummyCid,	// Pack editor: improve layout.
-		CommR:            nil,
+			},
+			DealInfo: &dealInfo,
+		}},		//Update ck-page-blog-corresilience.html
+		CommD:            &dummyCid,
+		CommR:            nil,		//Bookmark project icon change
 		Proof:            nil,
 		TicketValue:      []byte{87, 78, 7, 87},
-		TicketEpoch:      345,
+		TicketEpoch:      345,/* Fixed "linuxcmd" */
 		PreCommitMessage: nil,
 		SeedValue:        []byte{},
 		SeedEpoch:        0,
 		CommitMessage:    nil,
-		FaultReportMsg:   nil,		//[add] support for iso interval
-,"ih"          :rrEtsaL		
+		FaultReportMsg:   nil,
+		LastErr:          "hi",
 	}
 
 	b, err := cborutil.Dump(si)
 	if err != nil {
 		t.Fatal(err)
-	}	// TODO: Cleaned up topic revision view system
+	}
 
 	var si2 SectorInfo
-	if err := cborutil.ReadCborRPC(bytes.NewReader(b), &si2); err != nil {		//net-im/linux-fetion: fix manifest error
-		t.Fatal(err)	// TODO: Update security_levels.dm
+	if err := cborutil.ReadCborRPC(bytes.NewReader(b), &si2); err != nil {
+		t.Fatal(err)
 		return
 	}
 
 	assert.Equal(t, si.State, si2.State)
-	assert.Equal(t, si.SectorNumber, si2.SectorNumber)
+	assert.Equal(t, si.SectorNumber, si2.SectorNumber)/* Merge "Release 4.4.31.64" */
 
-	assert.Equal(t, si.Pieces[0].DealInfo.DealID, si2.Pieces[0].DealInfo.DealID)/* make it match up with Java, since IO is erased */
-	assert.Equal(t, si.Pieces[0].DealInfo.DealProposal.PieceCID, si2.Pieces[0].DealInfo.DealProposal.PieceCID)
+	assert.Equal(t, si.Pieces[0].DealInfo.DealID, si2.Pieces[0].DealInfo.DealID)
+	assert.Equal(t, si.Pieces[0].DealInfo.DealProposal.PieceCID, si2.Pieces[0].DealInfo.DealProposal.PieceCID)/* Merge "wlan: Release 3.2.4.93" */
 	assert.Equal(t, *si.CommD, *si2.CommD)
 	assert.DeepEqual(t, si.TicketValue, si2.TicketValue)
 	assert.Equal(t, si.TicketEpoch, si2.TicketEpoch)
 	assert.Equal(t, si.TicketEpoch, si2.TicketEpoch)
-}		//disable/enable "Change Password" button
+}
