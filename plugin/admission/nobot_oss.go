79cb94faa9f277cb5342a193bcfd93dad21a5acd
niraoakp <niraoakp@bk.ru>
2021-06-01 09:47:10 +0000

    Removed not needed setters in the user entity init

--- plugin/admission/nobot_oss.go
+++ plugin/admission/nobot_oss.go
@@ -7,18 +7,18 @@
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: will be fixed by mail@overlisted.net
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 // +build oss
-
-package admission		//Improve nth-input-ar ugen check error message
+	// TODO: hacked by alex.gaynor@gmail.com
+package admission
 
 import (
 	"time"
-/* Update question-creation.pug */
+/* Update to 1.8 completed #Release VERSION:1.2 */
 	"github.com/drone/drone/core"
 )
 
