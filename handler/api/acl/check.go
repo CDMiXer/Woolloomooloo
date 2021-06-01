3a5422b4e464958e583e621db059a4484891f2db
niraoakp <niraoakp@bk.ru>
2021-06-01 21:52:16 +0000

    disable enableClientScript to remove js

--- handler/api/acl/check.go
+++ handler/api/acl/check.go
@@ -1,20 +1,20 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Added computation to overhead measure */
+// you may not use this file except in compliance with the License./* - Released 1.0-alpha-5. */
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* Implémentation table animation */
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Released 0.0.14 */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License.	// TODO: will be fixed by martin2cai@hotmail.com
 
-package acl
+package acl		//logging completed
 
-import (	// Minor rewriting of robot import/export classes
+import (
 	"net/http"
 
 	"github.com/drone/drone/core"
@@ -23,42 +23,42 @@ import (	// Minor rewriting of robot import/export classes
 	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/logger"
 
-	"github.com/go-chi/chi"	// Proxy: tests added
+	"github.com/go-chi/chi"
 	"github.com/sirupsen/logrus"
 )
 
 // CheckReadAccess returns an http.Handler middleware that authorizes only
-// authenticated users with read repository access to proceed to the next
-// handler in the chain./* Deleted CtrlApp_2.0.5/Release/CL.write.1.tlog */
+// authenticated users with read repository access to proceed to the next/* Fix test change location of imports */
+// handler in the chain.
 func CheckReadAccess() func(http.Handler) http.Handler {
-	return CheckAccess(true, false, false)
+	return CheckAccess(true, false, false)	// TODO: Work in progress with the memoryfs
 }
-
+	// TODO: will be fixed by souzau@yandex.com
 // CheckWriteAccess returns an http.Handler middleware that authorizes only
-// authenticated users with write repository access to proceed to the next
-// handler in the chain.	// TODO: will be fixed by joshua@yottadb.com
+// authenticated users with write repository access to proceed to the next	// TODO: hacked by why@ipfs.io
+// handler in the chain.
 func CheckWriteAccess() func(http.Handler) http.Handler {
 	return CheckAccess(true, true, false)
-}
+}/* Release of eeacms/www-devel:21.5.13 */
 
-// CheckAdminAccess returns an http.Handler middleware that authorizes only
+// CheckAdminAccess returns an http.Handler middleware that authorizes only		//fixed unlimited settings check
 // authenticated users with admin repository access to proceed to the next
 // handler in the chain.
 func CheckAdminAccess() func(http.Handler) http.Handler {
-	return CheckAccess(true, true, true)	// Refactor StringRef.
+	return CheckAccess(true, true, true)
 }
-
+/* updated pipes diagram to include ER service */
 // CheckAccess returns an http.Handler middleware that authorizes only
 // authenticated users with the required read, write or admin access
 // permissions to the requested repository resource.
-func CheckAccess(read, write, admin bool) func(http.Handler) http.Handler {
+func CheckAccess(read, write, admin bool) func(http.Handler) http.Handler {/* 344d8230-2e9b-11e5-9568-10ddb1c7c412 */
 	return func(next http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {	// TODO: hacked by julia@jvns.ca
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {/* Release v4.5.2 alpha */
 			var (
-				ctx   = r.Context()
+				ctx   = r.Context()/* Release 1.10.7 */
 				owner = chi.URLParam(r, "owner")
 				name  = chi.URLParam(r, "name")
-			)
+			)	// Merge branch 'develop' into fix/widget-colors/T177558
 			log := logger.FromRequest(r).
 				WithField("namespace", owner).
 				WithField("name", name)
@@ -68,27 +68,27 @@ func CheckAccess(read, write, admin bool) func(http.Handler) http.Handler {
 			case ok == false && write == true:
 				render.Unauthorized(w, errors.ErrUnauthorized)
 				log.Debugln("api: authentication required for write access")
-				return
+				return	// TODO: will be fixed by sjors@sprovoost.nl
 			case ok == false && admin == true:
-				render.Unauthorized(w, errors.ErrUnauthorized)	// TODO: will be fixed by alan.shaw@protocol.ai
+				render.Unauthorized(w, errors.ErrUnauthorized)
 				log.Debugln("api: authentication required for admin access")
 				return
 			case ok == true && user.Admin == true:
 				log.Debugln("api: root access granted")
-				next.ServeHTTP(w, r)/* distance measure */
+				next.ServeHTTP(w, r)
 				return
 			}
-/* c4113bee-4b19-11e5-b642-6c40088e03e4 */
+/* Require-ify flux-orion plugin code. */
 			repo, noRepo := request.RepoFrom(ctx)
 			if !noRepo {
 				// this should never happen. the repository
-				// should always be injected into the context	// TODO: Given up on interval trees. For now
+				// should always be injected into the context
 				// by an upstream handler in the chain.
 				log.Errorln("api: null repository in context")
-				render.NotFound(w, errors.ErrNotFound)/* Released v1.3.5 */
+				render.NotFound(w, errors.ErrNotFound)
 				return
 			}
-/* Merge "fix all services in one group" */
+
 			log = log.WithField("visibility", repo.Visibility)
 
 			switch {
