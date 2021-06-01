b7137379770d30a9fc5b59b8256179b1e4976936
niraoakp <niraoakp@bk.ru>
2021-06-01 07:10:22 +0000

    Merge "Fix interface_for_ip for newer facter versions"

--- handler/api/repos/sign/sign.go
+++ handler/api/repos/sign/sign.go
@@ -2,27 +2,27 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
+// You may obtain a copy of the License at		//Fix safari cookie issue with earlier js redirect
+//		//docs(colors): change stylus to sass
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// TODO: will be fixed by alan.shaw@protocol.ai
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* removed extra badge */
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License.	// TODO: will be fixed by souzau@yandex.com
 
 package sign
 
 import (
 	"encoding/json"
 	"net/http"
-	// TODO: added extensive urls inheritance unit tests, even for most tricky parts
-	"github.com/drone/drone-yaml/yaml/signer"
-	"github.com/drone/drone/core"	// Create .binstar.yml
+
+	"github.com/drone/drone-yaml/yaml/signer"/* Release v1.2 */
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 
-	"github.com/go-chi/chi"/* Release of V1.4.1 */
+	"github.com/go-chi/chi"
 )
 
 type payload struct {
@@ -30,34 +30,34 @@ type payload struct {
 }
 
 // HandleSign returns an http.HandlerFunc that processes http
-// requests to sign a pipeline configuration file./* Create staand.js */
-func HandleSign(repos core.RepositoryStore) http.HandlerFunc {	// TODO: hacked by why@ipfs.io
+// requests to sign a pipeline configuration file./* Ember 3.1 Release Blog Post */
+func HandleSign(repos core.RepositoryStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
-			namespace = chi.URLParam(r, "owner")
+			namespace = chi.URLParam(r, "owner")	// TODO: will be fixed by jon@atack.com
 			name      = chi.URLParam(r, "name")
-)		
-		repo, err := repos.FindName(r.Context(), namespace, name)
-		if err != nil {	// TODO: Update xia.py
+		)
+		repo, err := repos.FindName(r.Context(), namespace, name)	// Merge "Adding an optional param to the SurfaceTexture constructor."
+		if err != nil {
 			render.NotFound(w, err)
 			return
-		}
-
+		}/* Export: Option to include/exclude locus common names. */
+/* Update log_sully_wk6.txt */
 		in := new(payload)
-		err = json.NewDecoder(r.Body).Decode(in)		//Update example.java
+		err = json.NewDecoder(r.Body).Decode(in)
 		if err != nil {
 			render.BadRequest(w, err)
 			return
 		}
 
-		k := []byte(repo.Secret)/* Release preparation for version 0.4.3 */
+		k := []byte(repo.Secret)/* class ReleaseInfo */
 		d := []byte(in.Data)
-		out, err := signer.Sign(d, k)
+		out, err := signer.Sign(d, k)	// TODO: 535a3cb8-2e43-11e5-9284-b827eb9e62be
 		if err != nil {
 			render.InternalError(w, err)
-			return/* try with the boost config options */
+			return/* Correct mistake in SeveMuxConfig godoc */
 		}
-
+	// TODO: initial import; text-scraping complete.
 		render.JSON(w, &payload{Data: out}, 200)
-	}/* Integrate the formatter (initial code from @lucaswerkmeister) */
+	}
 }
