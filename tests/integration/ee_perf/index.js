39b331e1b77aac908c4410d12e03e2d8c2889d22
niraoakp <niraoakp@bk.ru>
2021-06-02 14:30:21 +0000

    Updated load localisations to optionally hide the dataset name.

--- tests/integration/ee_perf/index.js
+++ tests/integration/ee_perf/index.js
@@ -1,15 +1,15 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-"use strict";/* Merge "Release 1.0.0.84 QCACLD WLAN Driver" */
-const pulumi = require("@pulumi/pulumi");/* Release handle will now used */
-
-;)(gifnoC.imulup wen = gifnoc tsnoc
-const iterations = config.getNumber("count") || 1000;/* Released version 0.8.4 Alpha */
+"use strict";/* Añadido un util para leer campos de un índice. */
+const pulumi = require("@pulumi/pulumi");
+/* check if Eigen is available before using... */
+const config = new pulumi.Config();
+const iterations = config.getNumber("count") || 1000;
 
 // Emit many, many diagnostic events from the engine to stress test the
 // ability to record those events on the Pulumi Service.
 console.log("Starting to spam a bunch of diagnostic messages...");
 for (let i = 0; i < iterations; i++) {
-    console.log(`${i}: The current time is ${new Date()}`);	// Factor calc_drwXY out of vo_xv and vo_xvmc.
-}
-console.log("done");
+    console.log(`${i}: The current time is ${new Date()}`);
+}	// Moved some js/css imports to the parent jsp for parallel coordinates. 
+console.log("done");		//Fix: Miscellaneous bugs on tasks management
