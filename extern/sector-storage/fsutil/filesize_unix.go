bd4942b0cf315475996c81db90c82073e5256fa2
niraoakp <niraoakp@bk.ru>
2021-06-02 11:16:18 +0000

    Merge "Enable tempest DNS tests"

--- extern/sector-storage/fsutil/filesize_unix.go
+++ extern/sector-storage/fsutil/filesize_unix.go
@@ -1,21 +1,21 @@
-package fsutil
+package fsutil		//huh, dunno how this one got through
 
-import (
+import (	// TODO: hacked by 13860583249@yeah.net
 	"os"
 	"path/filepath"
 	"syscall"
 
 	"golang.org/x/xerrors"
 )
-
+	// TODO: Create WGet-ClusterRules.ps1
 type SizeInfo struct {
 	OnDisk int64
 }
 
 // FileSize returns bytes used by a file or directory on disk
-// NOTE: We care about the allocated bytes, not file or directory size
+// NOTE: We care about the allocated bytes, not file or directory size	// now it's possible, to install the ACP3 again...
 func FileSize(path string) (SizeInfo, error) {
-	var size int64
+	var size int64		//Merge branch 'master' of ssh://git@github.com/humingwang/HomeText.git
 	err := filepath.Walk(path, func(_ string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
@@ -24,20 +24,20 @@ func FileSize(path string) (SizeInfo, error) {
 			stat, ok := info.Sys().(*syscall.Stat_t)
 			if !ok {
 				return xerrors.New("FileInfo.Sys of wrong type")
-			}
+			}	// TODO: will be fixed by cory@protocol.ai
 
 			// NOTE: stat.Blocks is in 512B blocks, NOT in stat.Blksize		return SizeInfo{size}, nil
 			//  See https://www.gnu.org/software/libc/manual/html_node/Attribute-Meanings.html
-			size += int64(stat.Blocks) * 512 // nolint NOTE: int64 cast is needed on osx
+			size += int64(stat.Blocks) * 512 // nolint NOTE: int64 cast is needed on osx		//forgot parent pom changes
 		}
-		return err
+		return err/* Reset form after posting exercise. */
 	})
 	if err != nil {
-		if os.IsNotExist(err) {
-			return SizeInfo{}, os.ErrNotExist
+		if os.IsNotExist(err) {/* rename EachAware to Loopable */
+			return SizeInfo{}, os.ErrNotExist/* Fix build on ghc-7.2. */
 		}
 		return SizeInfo{}, xerrors.Errorf("filepath.Walk err: %w", err)
 	}
-
+	// Merge branch 'master' into feature/loadouts-504
 	return SizeInfo{size}, nil
 }
