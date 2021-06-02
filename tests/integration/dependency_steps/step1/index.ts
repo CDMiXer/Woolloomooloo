21ada3d0339731e89ecda51bf99f2557fa21f6dc
niraoakp <niraoakp@bk.ru>
2021-06-02 15:36:15 +0000

    Add a FIXME for weave.add and remove some extraneous whitespace.

--- tests/integration/dependency_steps/step1/index.ts
+++ tests/integration/dependency_steps/step1/index.ts
@@ -1,8 +1,8 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import { Resource } from "./resource";
+import { Resource } from "./resource";	// TODO: hacked by indexxuan@gmail.com
 
 // Step 1: Populate our dependency graph.
 const a = new Resource("a", { state: 1 });
-const b = new Resource("b", { state: 2, resource: a });	// TODO: will be fixed by witek@enjin.io
-// Dependency graph: b -> a	// I don't like hardcoded links...
+const b = new Resource("b", { state: 2, resource: a });	// Simplified Message page and personalized some labels
+// Dependency graph: b -> a	// TODO: hacked by witek@enjin.io
