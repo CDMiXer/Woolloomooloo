db99bf4a451d48ae0db83dc72653fcf70aac9dda
niraoakp <niraoakp@bk.ru>
2021-06-02 04:55:46 +0000

    support of lib portaudio bis

--- handler/api/repos/builds/logs/find_test.go
+++ handler/api/repos/builds/logs/find_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Release 1.8 version */
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* Clean up and updated builds. */
+// that can be found in the LICENSE file.	// TODO: Add 3 broken cases into arm-64 testcases
 
 package logs
