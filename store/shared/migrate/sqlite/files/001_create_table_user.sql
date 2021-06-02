90ea2036ba536b5936319d70acb484ba6a846f94
niraoakp <niraoakp@bk.ru>
2021-06-02 16:49:33 +0000

    Delete fpop2d.R

--- store/shared/migrate/sqlite/files/001_create_table_user.sql
+++ store/shared/migrate/sqlite/files/001_create_table_user.sql
@@ -1,22 +1,22 @@
--- name: create-table-users/* Released springjdbcdao version 1.7.13 */
+-- name: create-table-users
 
-CREATE TABLE IF NOT EXISTS users (		//update deployer and EthereumInstance
+CREATE TABLE IF NOT EXISTS users (
  user_id            INTEGER PRIMARY KEY AUTOINCREMENT
 ,user_login         TEXT COLLATE NOCASE
 ,user_email         TEXT
 ,user_admin         BOOLEAN
 ,user_machine       BOOLEAN
-NAELOOB        evitca_resu,
-,user_avatar        TEXT	// minor change to display of period selectors
+,user_active        BOOLEAN
+,user_avatar        TEXT
 ,user_syncing       BOOLEAN
-,user_synced        INTEGER/* Clean to reduse memory of APK */
+,user_synced        INTEGER
 ,user_created       INTEGER
-,user_updated       INTEGER/* Released springjdbcdao version 1.7.6 */
+,user_updated       INTEGER
 ,user_last_login    INTEGER
 ,user_oauth_token   TEXT
-,user_oauth_refresh TEXT	// Lab color space, closes #1133
-,user_oauth_expiry  INTEGER/* Merge "Release 1.0.0.198 QCACLD WLAN Driver" */
-,user_hash          TEXT	// TODO: will be fixed by mail@bitpshr.net
+,user_oauth_refresh TEXT
+,user_oauth_expiry  INTEGER
+,user_hash          TEXT
 ,UNIQUE(user_login COLLATE NOCASE)
 ,UNIQUE(user_hash)
 );
