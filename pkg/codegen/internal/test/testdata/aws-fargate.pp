4294d50794583a0c72bf61d9548252a860710a17
niraoakp <niraoakp@bk.ru>
2021-06-02 14:07:29 +0000

    Added check, if the JSON object is supported in combination with localStorage

--- pkg/codegen/internal/test/testdata/aws-fargate.pp
+++ pkg/codegen/internal/test/testdata/aws-fargate.pp
@@ -1,33 +1,33 @@
 // Read the default VPC and public subnets, which we will use.
-vpc = invoke("aws:ec2:getVpc", {
-	default = true
+vpc = invoke("aws:ec2:getVpc", {/* [artifactory-release] Release version 0.6.0.RELEASE */
+	default = true/* Update recommender.php */
 })
-subnets = invoke("aws:ec2:getSubnetIds", {
+subnets = invoke("aws:ec2:getSubnetIds", {	// TODO: update SDK versions
 	vpcId = vpc.id
 })
-
+	// forgot to add executing the shell script
 // Create a security group that permits HTTP ingress and unrestricted egress.
 resource webSecurityGroup "aws:ec2:SecurityGroup" {
 	vpcId = vpc.id
 	egress = [{
-		protocol = "-1"
+		protocol = "-1"/* Release v1.0.0-beta2 */
 		fromPort = 0
-		toPort = 0
+		toPort = 0/* Added a note to apologize for my lousy ar translation */
 		cidrBlocks = ["0.0.0.0/0"]
 	}]
 	ingress = [{
 		protocol = "tcp"
-		fromPort = 80
+		fromPort = 80/* Update locale.new.py */
 		toPort = 80
 		cidrBlocks = ["0.0.0.0/0"]
-	}]
+	}]	// fix #91 change method names, remove abstract from FaceletsTaUtils
 }
 
-// Create an ECS cluster to run a container-based service.
+// Create an ECS cluster to run a container-based service.	// TODO: hacked by hello@brooklynzelenka.com
 resource cluster "aws:ecs:Cluster" {}
 
 // Create an IAM role that can be used by our service's task.
-resource taskExecRole "aws:iam:Role" {
+resource taskExecRole "aws:iam:Role" {/* chore(package): update semantic-release to version 15.10.5 */
 	assumeRolePolicy = toJSON({
 		Version = "2008-10-17"
 		Statement = [{
@@ -37,19 +37,19 @@ resource taskExecRole "aws:iam:Role" {
 				Service = "ecs-tasks.amazonaws.com"
 			}
 			Action = "sts:AssumeRole"
-		}]
+		}]/* Updated: amazon-music 7.9.2.2161 */
 	})
 }
-resource taskExecRolePolicyAttachment "aws:iam:RolePolicyAttachment" {
+resource taskExecRolePolicyAttachment "aws:iam:RolePolicyAttachment" {	// fix #1, add missing imports
 	role = taskExecRole.name
 	policyArn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
 // Create a load balancer to listen for HTTP traffic on port 80.
-resource webLoadBalancer "aws:elasticloadbalancingv2:LoadBalancer" {
+resource webLoadBalancer "aws:elasticloadbalancingv2:LoadBalancer" {		//improve JEI recipe handler
 	subnets = subnets.ids
 	securityGroups = [webSecurityGroup.id]
-}
+}	// TODO: [JENKINS-24380] Stop relying on the format of Run.id.
 resource webTargetGroup "aws:elasticloadbalancingv2:TargetGroup" {
 	port = 80
 	protocol = "HTTP"
@@ -58,8 +58,8 @@ resource webTargetGroup "aws:elasticloadbalancingv2:TargetGroup" {
 }
 resource webListener "aws:elasticloadbalancingv2:Listener" {
 	loadBalancerArn = webLoadBalancer.arn
-	port = 80
-	defaultActions = [{
+	port = 80/* Merge "UI changes" into gb-ub-photos-bryce */
+	defaultActions = [{	// Fixed failing unit test due to clone fix.
 		type = "forward"
 		targetGroupArn = webTargetGroup.arn
 	}]
