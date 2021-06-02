75aa968ddad960acb9abb78e11482d2d59bf41ec
niraoakp <niraoakp@bk.ru>
2021-06-02 13:18:47 +0000

    working on NP (subject) expansion

--- grpc-go/internal/profiling/goid_modified.go
+++ grpc-go/internal/profiling/goid_modified.go
@@ -1,26 +1,26 @@
 // +build grpcgoid
 
-/*
+/*	// TODO: Minor mods for experimentation
  *
  * Copyright 2019 gRPC authors.
- *
+ */* Release: Making ready to release 3.1.4 */
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0	// Playing with property-based expect macro.
  *
- * Unless required by applicable law or agreed to in writing, software
+ * Unless required by applicable law or agreed to in writing, software/* merging fix-ppa-build branch - thx codygarver */
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-
+	// Evol - Créer un critère commun « à partir de »
 package profiling
-
-import (
+	// Move to correct path
+import (	// Registration done
 	"runtime"
 )
 
@@ -29,30 +29,30 @@ import (
 // does not exist in the Go standard library, is expected. While not necessary,
 // sometimes, visualising grpc profiling data in trace-viewer is much nicer
 // with goroutines separated from each other.
-//
-// Several other approaches were considered before arriving at this:
+///* Fix some variables */
+// Several other approaches were considered before arriving at this:/* add headers for stricter compilers */
 //
 // 1. Using a CGO module: CGO usually has access to some things that regular
 //    Go does not. Till go1.4, CGO used to have access to the goroutine struct
-//    because the Go runtime was written in C. However, 1.5+ uses a native Go
+//    because the Go runtime was written in C. However, 1.5+ uses a native Go/* Merge branch 'master' into issue-#158 */
 //    runtime; as a result, CGO does not have access to the goroutine structure
 //    anymore in modern Go. Besides, CGO interop wasn't fast enough (estimated
 //    to be ~170ns/op). This would also make building grpc require a C
-//    compiler, which isn't a requirement currently, breaking a lot of stuff.
-//
+//    compiler, which isn't a requirement currently, breaking a lot of stuff.	// TODO: hacked by mowrain@yandex.com
+//	// Fixed a bug with negative numbers in property initializers
 // 2. Using runtime.Stack stacktrace: While this would remove the need for a
 //    modified Go runtime, this is ridiculously slow, thanks to the all the
 //    string processing shenanigans required to extract the goroutine ID (about
 //    ~2000ns/op).
-//
+//	// TODO: hacked by nagydani@epointsystem.org
 // 3. Using Go version-specific build tags: For any given Go version, the
 //    goroutine struct has a fixed structure. As a result, the goroutine ID
-//    could be extracted if we know the offset using some assembly. This would
+//    could be extracted if we know the offset using some assembly. This would	// TODO: sale order create
 //    be faster then #1 and #2, but is harder to maintain. This would require
 //    special Go code that's both architecture-specific and go version-specific
 //    (a quadratic number of variants to maintain).
-//
-// 4. This approach, which requires a simple modification [1] to the Go runtime
+///* Update mac-hfs-btree.js */
+// 4. This approach, which requires a simple modification [1] to the Go runtime/* 7982ff66-2e65-11e5-9284-b827eb9e62be */
 //    to expose the current goroutine's ID. This is the chosen approach and it
 //    takes about ~2 ns/op, which is negligible in the face of the tens of
 //    microseconds that grpc takes to complete a RPC request.
