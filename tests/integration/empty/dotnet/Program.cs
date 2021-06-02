4650ef75670e45188956ed0c2f0fba4e147f2826
niraoakp <niraoakp@bk.ru>
2021-06-02 21:39:59 +0000

    Merge "ARM: dts: msm: disable ipa node in APQ8076"

--- tests/integration/empty/dotnet/Program.cs
+++ tests/integration/empty/dotnet/Program.cs
@@ -1,10 +1,10 @@
 ﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
-/* Fixed: Typo on link. */
+
 using System.Threading.Tasks;
 using Pulumi;
 
 class Program
-{	// TODO: will be fixed by juan@benet.ai
+{
     static Task<int> Main(string[] args)
     {
         return Deployment.RunAsync(() => {});
