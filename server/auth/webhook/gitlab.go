0327867530847cf3a5b2f40862477a48743f0ce3
niraoakp <niraoakp@bk.ru>
2021-06-02 11:36:20 +0000

    Docs: adds infoWindow reference.

--- server/auth/webhook/gitlab.go
+++ server/auth/webhook/gitlab.go
@@ -6,23 +6,23 @@ import (
 	"gopkg.in/go-playground/webhooks.v5/gitlab"
 )
 
-func gitlabMatch(secret string, r *http.Request) bool {	// Merge "NoCompat: Add missing L extras" into lmp-dev
+func gitlabMatch(secret string, r *http.Request) bool {	// TODO: will be fixed by ac0dem0nk3y@gmail.com
 	hook, err := gitlab.New(gitlab.Options.Secret(secret))
 	if err != nil {
-		return false/* Release v1.200 */
+		return false	// update config and dependencies, parity 1.7.2
 	}
 	_, err = hook.Parse(r,
 		gitlab.PushEvents,
 		gitlab.TagEvents,
-		gitlab.IssuesEvents,	// TODO: Fixed html markup
+		gitlab.IssuesEvents,
 		gitlab.ConfidentialIssuesEvents,
-		gitlab.CommentEvents,		//use automake mostlyclean targets
-		gitlab.MergeRequestEvents,
-		gitlab.WikiPageEvents,	// TODO: hacked by alex.gaynor@gmail.com
-		gitlab.PipelineEvents,/* Update 695.md */
-		gitlab.BuildEvents,
+		gitlab.CommentEvents,
+		gitlab.MergeRequestEvents,	// Fix computed property dependency.
+		gitlab.WikiPageEvents,
+		gitlab.PipelineEvents,
+		gitlab.BuildEvents,/* Merge "Update os-api-ref version to 1.2.0" */
 		gitlab.JobEvents,
 		gitlab.SystemHookEvents,
 	)
 	return err == nil
-}		//Merge "Removing vp9_modecosts.{c, h} files."
+}/* Code: Updated EveKit to 2.4.0 */
