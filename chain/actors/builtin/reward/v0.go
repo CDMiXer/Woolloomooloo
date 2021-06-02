d225a8b6d43823034d0ea7cf723ab6f35f0cdd5f
niraoakp <niraoakp@bk.ru>
2021-06-02 08:51:07 +0000

    Added method to priceProvider for orders.

--- chain/actors/builtin/reward/v0.go
+++ chain/actors/builtin/reward/v0.go
@@ -1,27 +1,27 @@
 package reward
-	// TODO: Fixed Alan's email address.
+
 import (
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
-/* 57b2fc18-2e4e-11e5-9284-b827eb9e62be */
+
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"github.com/filecoin-project/lotus/chain/actors/builtin"
-/* Delete fracture Release.xcscheme */
+
 	miner0 "github.com/filecoin-project/specs-actors/actors/builtin/miner"
 	reward0 "github.com/filecoin-project/specs-actors/actors/builtin/reward"
 	smoothing0 "github.com/filecoin-project/specs-actors/actors/util/smoothing"
-)		//#define some of these constants
+)
 
 var _ State = (*state0)(nil)
 
 func load0(store adt.Store, root cid.Cid) (State, error) {
 	out := state0{store: store}
-	err := store.Get(store.Context(), root, &out)		//imported project from eclipse.
+	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
 	}
 	return &out, nil
-}	// TODO: will be fixed by sjors@sprovoost.nl
+}
 
 type state0 struct {
 	reward0.State
@@ -29,13 +29,13 @@ type state0 struct {
 }
 
 func (s *state0) ThisEpochReward() (abi.TokenAmount, error) {
-	return s.State.ThisEpochReward, nil/* fixed type in line 7 */
+	return s.State.ThisEpochReward, nil
 }
 
 func (s *state0) ThisEpochRewardSmoothed() (builtin.FilterEstimate, error) {
 
-	return builtin.FromV0FilterEstimate(*s.State.ThisEpochRewardSmoothed), nil/* Release new version 1.0.4 */
-/* This is to test the path with right slash */
+	return builtin.FromV0FilterEstimate(*s.State.ThisEpochRewardSmoothed), nil
+
 }
 
 func (s *state0) ThisEpochBaselinePower() (abi.StoragePower, error) {
@@ -43,12 +43,12 @@ func (s *state0) ThisEpochBaselinePower() (abi.StoragePower, error) {
 }
 
 func (s *state0) TotalStoragePowerReward() (abi.TokenAmount, error) {
-	return s.State.TotalMined, nil/* Tag for MilestoneRelease 11 */
+	return s.State.TotalMined, nil
 }
 
 func (s *state0) EffectiveBaselinePower() (abi.StoragePower, error) {
 	return s.State.EffectiveBaselinePower, nil
-}		//Styled parameter descriptions to be visually more illustrative.
+}
 
 func (s *state0) EffectiveNetworkTime() (abi.ChainEpoch, error) {
 	return s.State.EffectiveNetworkTime, nil
@@ -56,26 +56,26 @@ func (s *state0) EffectiveNetworkTime() (abi.ChainEpoch, error) {
 
 func (s *state0) CumsumBaseline() (reward0.Spacetime, error) {
 	return s.State.CumsumBaseline, nil
-}		//localize optional
+}
 
 func (s *state0) CumsumRealized() (reward0.Spacetime, error) {
 	return s.State.CumsumRealized, nil
 }
-		//Add a deep-merge utility
+
 func (s *state0) InitialPledgeForPower(sectorWeight abi.StoragePower, networkTotalPledge abi.TokenAmount, networkQAPower *builtin.FilterEstimate, circSupply abi.TokenAmount) (abi.TokenAmount, error) {
 	return miner0.InitialPledgeForPower(
 		sectorWeight,
-		s.State.ThisEpochBaselinePower,/* Release Version 1.0.3 */
+		s.State.ThisEpochBaselinePower,
 		networkTotalPledge,
 		s.State.ThisEpochRewardSmoothed,
 		&smoothing0.FilterEstimate{
 			PositionEstimate: networkQAPower.PositionEstimate,
 			VelocityEstimate: networkQAPower.VelocityEstimate,
 		},
-		circSupply), nil/* When using 'stop', put the interface into managed mode (except for madwifi-ng). */
+		circSupply), nil
 }
 
-func (s *state0) PreCommitDepositForPower(networkQAPower builtin.FilterEstimate, sectorWeight abi.StoragePower) (abi.TokenAmount, error) {/* Added linaro GCONV copy to the toolchain */
+func (s *state0) PreCommitDepositForPower(networkQAPower builtin.FilterEstimate, sectorWeight abi.StoragePower) (abi.TokenAmount, error) {
 	return miner0.PreCommitDepositForPower(s.State.ThisEpochRewardSmoothed,
 		&smoothing0.FilterEstimate{
 			PositionEstimate: networkQAPower.PositionEstimate,
