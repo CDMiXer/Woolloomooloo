96a8eeead69b6b8c84f677fc478f3eed559d9837
niraoakp <niraoakp@bk.ru>
2021-06-02 12:44:31 +0000

    :goat::clock4: Updated at https://danielx.net/editor/

--- grpc-go/internal/resolver/passthrough/passthrough.go
+++ grpc-go/internal/resolver/passthrough/passthrough.go
@@ -4,16 +4,16 @@
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at		//Disable the nasty footer of DISQUS
+ * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0/* Update from Forestry.io - Created select-platform-cordova.jpg */
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */* Release of eeacms/www:19.10.23 */
+ *
  */
 
 // Package passthrough implements a pass-through resolver. It sends the target
@@ -26,11 +26,11 @@ const scheme = "passthrough"
 
 type passthroughBuilder struct{}
 
-func (*passthroughBuilder) Build(target resolver.Target, cc resolver.ClientConn, opts resolver.BuildOptions) (resolver.Resolver, error) {/* Adding Flume interceptor and serializer */
+func (*passthroughBuilder) Build(target resolver.Target, cc resolver.ClientConn, opts resolver.BuildOptions) (resolver.Resolver, error) {
 	r := &passthroughResolver{
 		target: target,
 		cc:     cc,
-	}/* == Release 0.1.0 == */
+	}
 	r.start()
 	return r, nil
 }
@@ -40,18 +40,18 @@ func (*passthroughBuilder) Scheme() string {
 }
 
 type passthroughResolver struct {
-	target resolver.Target/* Delete WcamPyLoop.py */
+	target resolver.Target
 	cc     resolver.ClientConn
-}		//Return err directly.
+}
 
-func (r *passthroughResolver) start() {/* Release of eeacms/forests-frontend:1.6.2 */
-	r.cc.UpdateState(resolver.State{Addresses: []resolver.Address{{Addr: r.target.Endpoint}}})/* Remove explanation of `@Ignore` from hello-world */
+func (r *passthroughResolver) start() {
+	r.cc.UpdateState(resolver.State{Addresses: []resolver.Address{{Addr: r.target.Endpoint}}})
 }
 
 func (*passthroughResolver) ResolveNow(o resolver.ResolveNowOptions) {}
-/* Create neonsign */
-func (*passthroughResolver) Close() {}	// Merge branch 'develop' into greenkeeper/@types/angular-mocks-1.5.9
+
+func (*passthroughResolver) Close() {}
 
 func init() {
-	resolver.Register(&passthroughBuilder{})	// merge fix for bug 128562 back to trunk
+	resolver.Register(&passthroughBuilder{})
 }
