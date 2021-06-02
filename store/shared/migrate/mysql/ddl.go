26e974d5b19ee0235d46c46db0ef37d40bb6a16f
niraoakp <niraoakp@bk.ru>
2021-06-02 13:31:24 +0000

    Set demo project Deployment Target to 4.0

--- store/shared/migrate/mysql/ddl.go
+++ store/shared/migrate/mysql/ddl.go
@@ -1,9 +1,9 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file./* Merge branch 'develop' into gitlink */
 
-// +build !oss	// TODO: will be fixed by ligi@ligi.de
+// +build !oss
 
-package mysql	// TODO: Parsing clone TK-102 added
-
+package mysql
+/* Release Metropolis 2.0.40.1053 */
 //go:generate togo ddl -package mysql -dialect mysql
