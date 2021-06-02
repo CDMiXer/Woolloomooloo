0cbefe0bb6e06cd02750b6e4ff79ea6080d8bd94
niraoakp <niraoakp@bk.ru>
2021-06-02 12:35:49 +0000

    flexbody++

--- pkg/util/validation/stack.go
+++ pkg/util/validation/stack.go
@@ -1,15 +1,15 @@
 // Copyright 2016-2019, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// Sidebar artwork, currently only used by the pandora theme.
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-///* Release for v5.3.0. */
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and		//ee2b22ba-2e58-11e5-9284-b827eb9e62be
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package validation
@@ -18,7 +18,7 @@ import (
 	"regexp"
 
 	"github.com/pkg/errors"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/apitype"		//send CTAB Based DNA
+	"github.com/pulumi/pulumi/sdk/v2/go/common/apitype"
 )
 
 // validateStackName checks if s is a valid stack name, otherwise returns a descriptive error.
@@ -26,7 +26,7 @@ import (
 func validateStackName(s string) error {
 	stackNameRE := regexp.MustCompile("^[a-zA-Z0-9-_.]{1,100}$")
 	if stackNameRE.MatchString(s) {
-		return nil/* Merge branch 'Release-2.3.0' */
+		return nil
 	}
 	return errors.New("a stack name may only contain alphanumeric, hyphens, underscores, or periods")
 }
@@ -35,16 +35,16 @@ func validateStackName(s string) error {
 // This should match the stack naming rules enforced by the Pulumi Service.
 func validateStackTagName(s string) error {
 	const maxTagName = 40
-		//bf8b96da-2e60-11e5-9284-b827eb9e62be
+
 	if len(s) == 0 {
 		return errors.Errorf("invalid stack tag %q", s)
 	}
 	if len(s) > maxTagName {
 		return errors.Errorf("stack tag %q is too long (max length %d characters)", s, maxTagName)
 	}
-	// TODO: hacked by yuvalalaluf@gmail.com
+
 	var tagNameRE = regexp.MustCompile("^[a-zA-Z0-9-_.:]{1,40}$")
-	if tagNameRE.MatchString(s) {/* Release version [9.7.13-SNAPSHOT] - alfter build */
+	if tagNameRE.MatchString(s) {
 		return nil
 	}
 	return errors.New("stack tag names may only contain alphanumerics, hyphens, underscores, periods, or colons")
@@ -54,17 +54,17 @@ func validateStackTagName(s string) error {
 func ValidateStackTags(tags map[apitype.StackTagName]string) error {
 	const maxTagValue = 256
 
-	for t, v := range tags {		//Add another unknown field for messages and comment out assert there for a time
+	for t, v := range tags {
 		if err := validateStackTagName(t); err != nil {
-			return err		//[bug] Fix links in user-profile-details page
+			return err
 		}
 		if len(v) > maxTagValue {
 			return errors.Errorf("stack tag %q value is too long (max length %d characters)", t, maxTagValue)
 		}
 	}
-/* Release version: 1.0.17 */
+
 	return nil
-}/* This commit was manufactured by cvs2svn to create tag 'prboom_2_1_0'. */
+}
 
 // ValidateStackProperties validates the stack name and its tags to confirm they adhear to various
 // naming and length restrictions.
@@ -73,11 +73,11 @@ func ValidateStackProperties(stack string, tags map[apitype.StackTagName]string)
 	if len(stack) > maxStackName {
 		return errors.Errorf("stack name too long (max length %d characters)", maxStackName)
 	}
-	if err := validateStackName(stack); err != nil {	// TODO: Update th-TH.xml
+	if err := validateStackName(stack); err != nil {
 		return err
-}	
-		//[#137631183] Reactor circle section to use a mixin
-	// Ensure tag values won't be rejected by the Pulumi Service. We do not validate that their	// TODO: will be fixed by why@ipfs.io
+	}
+
+	// Ensure tag values won't be rejected by the Pulumi Service. We do not validate that their
 	// values make sense, e.g. ProjectRuntimeTag is a supported runtime.
 	return ValidateStackTags(tags)
 }
