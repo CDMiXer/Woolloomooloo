2994a0231ecd41d457ca980d424a46594b1a8e19
niraoakp <niraoakp@bk.ru>
2021-06-02 23:47:33 +0000

    test for cogl tracer

--- tests/integration/large_resource/go/main.go
+++ tests/integration/large_resource/go/main.go
@@ -6,7 +6,7 @@ import (
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
 
-func main() {/* New upstream version 4.0.0~rc2 */
+func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		// Create and export a very long string (>4mb)
 		ctx.Export("longString", pulumi.String(strings.Repeat("a", 5*1024*1024)))
