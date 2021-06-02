ff895d837a3ed3b6cb6de284cb67b845a427012f
niraoakp <niraoakp@bk.ru>
2021-06-02 07:17:57 +0000

    Fix attack calculation

--- pkg/codegen/internal/test/testdata/aws-s3-logging.pp.ts
+++ pkg/codegen/internal/test/testdata/aws-s3-logging.pp.ts
@@ -1,7 +1,7 @@
-import * as pulumi from "@pulumi/pulumi";/* Release: Making ready to release 5.0.3 */
+import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 
-const logs = new aws.s3.Bucket("logs", {});/* bca9cc96-2e62-11e5-9284-b827eb9e62be */
+const logs = new aws.s3.Bucket("logs", {});
 const bucket = new aws.s3.Bucket("bucket", {loggings: [{
     targetBucket: logs.bucket,
 }]});
