008037a60c0e122d0023c6536ead2b00109532d1
niraoakp <niraoakp@bk.ru>
2021-06-02 08:08:16 +0000

    Remove duplicated task-translator jar

--- handler/api/repos/crons/list.go
+++ handler/api/repos/crons/list.go
@@ -2,35 +2,35 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss
+// +build !oss/* BlueprintsRepository agregado. */
 
-package crons
+package crons/* CAINav: v2.0: Project structure updates. Release preparations. */
 
-import (
+import (	// SAK-31045 "Created" confirmation lightbox displays too low on page
 	"net/http"
-
+/* Adds object overrides to TypeReference. */
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 
-	"github.com/go-chi/chi"
-)
+	"github.com/go-chi/chi"/* Add MachineOperand::ChangeToFPImmediate and setFPImm */
+)	// TODO: hacked by josharian@gmail.com
 
 // HandleList returns an http.HandlerFunc that writes a json-encoded
 // list of cron jobs to the response body.
 func HandleList(
-	repos core.RepositoryStore,
-	crons core.CronStore,
+	repos core.RepositoryStore,/* Release jedipus-2.6.38 */
+	crons core.CronStore,	// TODO: will be fixed by witek@enjin.io
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		var (
+		var (	// TODO: hacked by mail@overlisted.net
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
 		)
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
-			render.NotFound(w, err)
+			render.NotFound(w, err)		//wrap references to VERBOSE in quotes
 			return
-		}
+		}/* Merge pull request #16 from leokewitz/master */
 		list, err := crons.List(r.Context(), repo.ID)
 		if err != nil {
 			render.NotFound(w, err)
@@ -38,4 +38,4 @@ func HandleList(
 		}
 		render.JSON(w, list, 200)
 	}
-}
+}	// TODO: will be fixed by brosner@gmail.com
