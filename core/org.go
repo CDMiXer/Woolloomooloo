22c57e941e6c5ca1b5411c9b3d218a134480de7e
niraoakp <niraoakp@bk.ru>
2021-06-02 17:03:53 +0000

    ebceabe2-2e65-11e5-9284-b827eb9e62be

--- core/org.go
+++ core/org.go
@@ -1,38 +1,38 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* bumped to 0.23.3c */
-// You may obtain a copy of the License at		//Merge "Add the tempest-lib project to openstack"
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//		//Add representative image field to courses and topics
-// Unless required by applicable law or agreed to in writing, software/* Release 0.0.6 readme */
+//
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Merge "Removing HP MSA driver for no reported CI" */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package core	// TODO: Create inference.jl
+package core
 
-import "context"/* 71c6282e-2e70-11e5-9284-b827eb9e62be */
+import "context"
 
 // Organization represents an organization in the source
 // code management system (e.g. GitHub).
 type Organization struct {
-	Name   string		//9d02e53c-2e74-11e5-9284-b827eb9e62be
+	Name   string
 	Avatar string
-}/* Released v2.2.2 */
+}
 
-// OrganizationService provides access to organization and/* Releases as a link */
+// OrganizationService provides access to organization and
 // team access in the external source code management system
-// (e.g. GitHub).		//added new classes for more flexibility
+// (e.g. GitHub).
 type OrganizationService interface {
 	// List returns a list of organization to which the
 	// user is a member.
-	List(context.Context, *User) ([]*Organization, error)/* Add content validation support; make 503 non-fatal for BMJ related */
+	List(context.Context, *User) ([]*Organization, error)
 
 	// Membership returns true if the user is a member
 	// of the organization, and true if the user is an
 	// of the organization.
-	Membership(context.Context, *User, string) (bool, bool, error)	// Implementada funcionalidad básica de mostrar distancias
+	Membership(context.Context, *User, string) (bool, bool, error)
 }
