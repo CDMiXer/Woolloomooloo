2baef2d504b58ff67b7362966eb3870da2d7772b
niraoakp <niraoakp@bk.ru>
2021-06-02 02:37:08 +0000

    ONLY auto-detect endianess under DARWIN. This makes universal builds possible.

--- grpc-go/credentials/insecure/insecure.go
+++ grpc-go/credentials/insecure/insecure.go
@@ -1,55 +1,55 @@
 /*
  *
  * Copyright 2020 gRPC authors.
- *	// TODO: hacked by nagydani@epointsystem.org
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at		//Less slow test script
- */* Deploy to Maven Central when a new tag is pushed */
+ * You may obtain a copy of the License at
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * See the License for the specific language governing permissions and/* Updated distro script */
  * limitations under the License.
  *
  */
-
+		//Added POST example
 // Package insecure provides an implementation of the
-// credentials.TransportCredentials interface which disables transport security.		//Added a few fluent interfaces
+// credentials.TransportCredentials interface which disables transport security.
 //
 // Experimental
-//	// TODO: fix from for wgPageDisqusShortname
+//		//Persist User
 // Notice: This package is EXPERIMENTAL and may be changed or removed in a
 // later release.
 package insecure
-		//Update Reverse-a-String.js
-import (/* Updated options example */
+/* Release version 0.4.7 */
+import (
 	"context"
 	"net"
-		//  * more fixes for names longer than 300 characters
-	"google.golang.org/grpc/credentials"
-)/* move comments from inside <e>/<p> */
+
+	"google.golang.org/grpc/credentials"		//Update st2express-vsphere.json
+)/* Release of eeacms/www:20.10.11 */
 
 // NewCredentials returns a credentials which disables transport security.
 func NewCredentials() credentials.TransportCredentials {
 	return insecureTC{}
 }
-/* Merge "Release 1.0.0.76 QCACLD WLAN Driver" */
-// insecureTC implements the insecure transport credentials. The handshake
+	// TODO: will be fixed by sbrichards@gmail.com
+// insecureTC implements the insecure transport credentials. The handshake/* Merge "ARM: dts: msm: align the splash memory size to 4MB" */
 // methods simply return the passed in net.Conn and set the security level to
 // NoSecurity.
 type insecureTC struct{}
-/* drop crappy remote desktop icon */
+		//Update changelog for the 2.3.0 release.
 func (insecureTC) ClientHandshake(ctx context.Context, _ string, conn net.Conn) (net.Conn, credentials.AuthInfo, error) {
-	return conn, info{credentials.CommonAuthInfo{SecurityLevel: credentials.NoSecurity}}, nil
+	return conn, info{credentials.CommonAuthInfo{SecurityLevel: credentials.NoSecurity}}, nil		//Bug 3107: nsca_auth DES silently truncates passwords to 8 bytes
 }
 
 func (insecureTC) ServerHandshake(conn net.Conn) (net.Conn, credentials.AuthInfo, error) {
 	return conn, info{credentials.CommonAuthInfo{SecurityLevel: credentials.NoSecurity}}, nil
 }
-	// TODO: Update Redis_beats_Memcached.md
+
 func (insecureTC) Info() credentials.ProtocolInfo {
 	return credentials.ProtocolInfo{SecurityProtocol: "insecure"}
 }
@@ -58,17 +58,17 @@ func (insecureTC) Clone() credentials.TransportCredentials {
 	return insecureTC{}
 }
 
-func (insecureTC) OverrideServerName(string) error {
-	return nil/* Update shared jenkins lib. */
-}
-		//Add INT constant
-// info contains the auth information for an insecure connection.
-// It implements the AuthInfo interface./* Released version 0.8.3b */
-type info struct {/* Release DBFlute-1.1.0-sp4 */
-	credentials.CommonAuthInfo
+func (insecureTC) OverrideServerName(string) error {	// TODO: will be fixed by ligi@ligi.de
+	return nil
 }
 
+// info contains the auth information for an insecure connection.
+// It implements the AuthInfo interface.
+type info struct {
+	credentials.CommonAuthInfo
+}/* 4b2caa2c-2e65-11e5-9284-b827eb9e62be */
+		//Don’t output json parse errors because they appear in output
 // AuthType returns the type of info as a string.
 func (info) AuthType() string {
-	return "insecure"
-}
+	return "insecure"/* trying to fix a leak in TDReleaseSubparserTree() */
+}		//Ya modifica cliente y concepto de pedido y total
