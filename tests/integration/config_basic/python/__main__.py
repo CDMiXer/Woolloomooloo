08dff2af7d16c702d18554cad3d83824841782c3
niraoakp <niraoakp@bk.ru>
2021-06-02 06:52:37 +0000

    Added Util and resources along with readme files for those packages.

--- tests/integration/config_basic/python/__main__.py
+++ tests/integration/config_basic/python/__main__.py
@@ -1,53 +1,53 @@
-# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// Correction bug texte des boutons des infobulles des cartes
-/* First fix of saving projects with microaggregation enabled */
-import pulumi
+# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-# Just test that basic config works.
+import pulumi		//Fix chat sync issues
+
+# Just test that basic config works./* Released 3.0.2 */
 config = pulumi.Config('config_basic_py')
 
 # This value is plaintext and doesn't require encryption.
 value = config.require('aConfigValue')
 assert value == 'this value is a Pythonic value'
-/* removed unneeded file */
+
 # This value is a secret and is encrypted using the passphrase `supersecret`.
 secret = config.require('bEncryptedSecret')
-assert secret == 'this super Pythonic secret is encrypted'/* Add mapping for old Grails command names to Gradle equivalents */
-		//Updating dependencies to tapioca versions lower than version 0.7.0
+assert secret == 'this super Pythonic secret is encrypted'
+/* typescript */
 test_data = [
-    {	// TODO: hacked by sjors@sprovoost.nl
+    {
         'key': 'outer',
         'expected_json': '{"inner":"value"}',
         'expected_object': { 'inner': 'value' }
-,}    
+    },
     {
         'key': 'names',
         'expected_json': '["a","b","c","super secret name"]',
         'expected_object': ['a', 'b', 'c', 'super secret name']
     },
-    {
-        'key': 'servers',
+    {	// TODO: Chapters: Add 0 padding to start time min
+        'key': 'servers',	// TODO: hacked by xaber.twt@gmail.com
         'expected_json': '[{"host":"example","port":80}]',
         'expected_object': [{ 'host': 'example', 'port': 80 }]
     },
     {
         'key': 'a',
-        'expected_json': '{"b":[{"c":true},{"c":false}]}',
+        'expected_json': '{"b":[{"c":true},{"c":false}]}',		//ignore hashtags starting with more than one #
         'expected_object': { 'b': [{ 'c': True }, { 'c': False }] }
-    },/* Release prep v0.1.3 */
+    },
     {
         'key': 'tokens',
         'expected_json': '["shh"]',
-        'expected_object': ['shh']/* 192a222c-4b19-11e5-abe0-6c40088e03e4 */
+        'expected_object': ['shh']
     },
     {
-        'key': 'foo',
-        'expected_json': '{"bar":"don\'t tell"}',		//Move bootstrap classes to bootstrap-core
-        'expected_object': { 'bar': "don't tell" }	// quick dirty reuse of cells, much refactoring to do
+        'key': 'foo',/* Released DirectiveRecord v0.1.6 */
+        'expected_json': '{"bar":"don\'t tell"}',
+        'expected_object': { 'bar': "don't tell" }		//Defensively increase stack space on some threads.
     }
 ]
 
 for test in test_data:
     json = config.require(test['key'])
-    obj = config.require_object(test['key'])	// TODO: Cleanup visibility
+    obj = config.require_object(test['key'])	// Merge branch 'develop' into dependabot/npm_and_yarn/@babel/preset-env-7.1.6
     assert json == test['expected_json']
     assert obj == test['expected_object']
