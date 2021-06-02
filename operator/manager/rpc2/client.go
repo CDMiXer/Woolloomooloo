753c9f60813e3e5177e5cbc34fe452122475a426
niraoakp <niraoakp@bk.ru>
2021-06-02 23:00:55 +0000

    Register the newer type encoders and decoders

--- operator/manager/rpc2/client.go
+++ operator/manager/rpc2/client.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: will be fixed by ligi@ligi.de
-// Use of this source code is governed by the Drone Non-Commercial License	// TODO: hacked by mikeal.rogers@gmail.com
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-	// Better failure handling while putting
+
 // +build !oss
 
 package rpc2
