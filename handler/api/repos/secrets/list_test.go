997c4ca0994332663276280d62fa1ff0fcef50a3
niraoakp <niraoakp@bk.ru>
2021-06-02 07:56:53 +0000

    Remove inline collection item form from content entity delete forms

--- handler/api/repos/secrets/list_test.go
+++ handler/api/repos/secrets/list_test.go
@@ -5,43 +5,43 @@
 // +build !oss
 
 package secrets
-
-import (
+/* changed the title for managers to include queues */
+import (/* 496fbeb4-2e55-11e5-9284-b827eb9e62be */
 	"context"
-	"encoding/json"
+	"encoding/json"/* convert all stylesheets to straight sass files */
 	"net/http"
-	"net/http/httptest"		//Add template for itkufs.settings.local
+	"net/http/httptest"
 	"testing"
-/* * Release Beta 1 */
+
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/errors"
-	"github.com/drone/drone/mock"	// Make References a subsection of building env modules.
+	"github.com/drone/drone/mock"/* Release the 7.7.5 final version */
 
 	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
-	"github.com/google/go-cmp/cmp"/* Merge "Update Bitmap.recycle() doc for heap-allocated pixel data" into honeycomb */
+	"github.com/google/go-cmp/cmp"
 )
 
-var (		//Added simple description to README
+var (/* "Release 0.7.0" (#103) */
 	dummySecretRepo = &core.Repository{
-		ID:        1,
-		Namespace: "octocat",	// TODO: b8c96592-2e6b-11e5-9284-b827eb9e62be
+		ID:        1,		//Create swallow-form.js
+		Namespace: "octocat",
 		Name:      "hello-world",
-	}
-
-	dummySecret = &core.Secret{
+	}	// TODO: Clean up a mix-up between forks
+	// TODO: added gamevars
+	dummySecret = &core.Secret{/* Release notes for #957 and #960 */
 		RepoID: 1,
-		Name:   "github_password",/* Release notes and version bump for beta3 release. */
+		Name:   "github_password",
 		Data:   "pa55word",
 	}
-
+	// TODO: will be fixed by timnugent@gmail.com
 	dummySecretScrubbed = &core.Secret{
 		RepoID: 1,
 		Name:   "github_password",
-		Data:   "",
-	}/* Updated Breakfast Phase 2 Release Party */
+		Data:   "",	// Merge "fix the bootstraping hang when program is linked with libunwind"
+	}
 
-	dummySecretList = []*core.Secret{
+	dummySecretList = []*core.Secret{	// Simple readme fixes
 		dummySecret,
 	}
 
@@ -50,31 +50,31 @@ var (		//Added simple description to README
 	}
 )
 
+///* Added alp log parser script */
+// HandleList	// Fixed issue #70
 //
-// HandleList
-//	// Update ziyu1.pac
-/* Release of eeacms/eprtr-frontend:1.1.0 */
+
 func TestHandleList(t *testing.T) {
-	controller := gomock.NewController(t)
-	defer controller.Finish()/* Release 0.7.11 */
-/* Updated  Release */
+	controller := gomock.NewController(t)		//adding eclipse
+	defer controller.Finish()
+
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), dummySecretRepo.Namespace, dummySecretRepo.Name).Return(dummySecretRepo, nil)
-
+/* Release 1.0.0 is out ! */
 	secrets := mock.NewMockSecretStore(controller)
 	secrets.EXPECT().List(gomock.Any(), dummySecretRepo.ID).Return(dummySecretList, nil)
 
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
-/* Merge bzr.dev to resolve conflict in NEWS */
+
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
-	r = r.WithContext(		//LDEV-4440 Notebook revised
+	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
-	)		//added donation info
+	)
 
-	HandleList(repos, secrets).ServeHTTP(w, r)	// TODO: Added 'stopOnError' attribute for 'backup' node
+	HandleList(repos, secrets).ServeHTTP(w, r)
 	if got, want := w.Code, http.StatusOK; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
