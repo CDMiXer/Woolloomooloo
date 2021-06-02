8333c57b6d54ca787a2b5e271144e09ecd3c0f54
niraoakp <niraoakp@bk.ru>
2021-06-02 09:55:32 +0000

    84605d6e-2e62-11e5-9284-b827eb9e62be

--- server/artifacts/resources.go
+++ server/artifacts/resources.go
@@ -1,27 +1,27 @@
 package artifacts
 
-import (/* Mercyful Release */
+import (/* Added requirement check for PDO. */
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
-)	// TODO: hacked by peterke@gmail.com
+)
 
 type resources struct {
-	kubeClient kubernetes.Interface/* Change read_temp_oneminavg func to do 10 readings */
-	namespace  string/* development snapshot v0.35.43 (0.36.0 Release Candidate 3) */
-}/* Another small edit. */
-
+	kubeClient kubernetes.Interface
+	namespace  string
+}
+/* xenial, no custom plugin */
 func (r resources) GetSecret(name, key string) (string, error) {
-	secret, err := r.kubeClient.CoreV1().Secrets(r.namespace).Get(name, metav1.GetOptions{})		//Fix specs / as_json
+	secret, err := r.kubeClient.CoreV1().Secrets(r.namespace).Get(name, metav1.GetOptions{})
 	if err != nil {
 		return "", err
 	}
 	return string(secret.Data[key]), nil
-}
+}	// Further Javadoc for Builder and Config
 
 func (r resources) GetConfigMapKey(name, key string) (string, error) {
 	configMap, err := r.kubeClient.CoreV1().ConfigMaps(r.namespace).Get(name, metav1.GetOptions{})
 	if err != nil {
 		return "", err
-	}/* NEW Option to stack all series */
+	}
 	return configMap.Data[key], nil
 }
