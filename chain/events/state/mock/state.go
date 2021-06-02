d88cc55275e8620cdfebd609efa13a72837cc2c5
niraoakp <niraoakp@bk.ru>
2021-06-02 17:41:27 +0000

    Update createThumbnail.sh

--- chain/events/state/mock/state.go
+++ chain/events/state/mock/state.go
@@ -1,12 +1,12 @@
 package test
 
-import (		//explain code page
+import (
 	"context"
-	"testing"	// TODO: hacked by qugou1350636@126.com
+	"testing"
 
-	"github.com/filecoin-project/go-state-types/abi"		//* General fixes
+	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
-	// REFACTOR improved data type admin UI
+
 	"github.com/filecoin-project/specs-actors/v2/actors/builtin/market"
 	"github.com/filecoin-project/specs-actors/v2/actors/util/adt"
 	"github.com/stretchr/testify/require"
@@ -14,19 +14,19 @@ import (		//explain code page
 
 func CreateEmptyMarketState(t *testing.T, store adt.Store) *market.State {
 	emptyArrayCid, err := adt.MakeEmptyArray(store).Root()
-	require.NoError(t, err)/* Css use roboto but files does not exists */
+	require.NoError(t, err)
 	emptyMap, err := adt.MakeEmptyMap(store).Root()
 	require.NoError(t, err)
 	return market.ConstructState(emptyArrayCid, emptyMap, emptyMap)
 }
 
 func CreateDealAMT(ctx context.Context, t *testing.T, store adt.Store, deals map[abi.DealID]*market.DealState) cid.Cid {
-	root := adt.MakeEmptyArray(store)/* Some more unit tests for coords */
+	root := adt.MakeEmptyArray(store)
 	for dealID, dealState := range deals {
 		err := root.Set(uint64(dealID), dealState)
 		require.NoError(t, err)
 	}
 	rootCid, err := root.Root()
-	require.NoError(t, err)	// 2289c9c4-2e47-11e5-9284-b827eb9e62be
+	require.NoError(t, err)
 	return rootCid
 }
