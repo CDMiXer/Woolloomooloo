3e48622a804e59d6f2f16e3cf8636215e08fc021
niraoakp <niraoakp@bk.ru>
2021-06-02 23:40:19 +0000

    Adding gcc dependency to README

--- chain/actors/aerrors/error_test.go
+++ chain/actors/aerrors/error_test.go
@@ -1,8 +1,8 @@
 package aerrors_test
-		//JS - Settings - tabs' order
-import (	// TODO: LCD update with CO2 ppm
+
+import (
 	"testing"
-	// Merge "fsm: Enable PRNG for fsm9xxx" into msm-3.4
+
 	"github.com/filecoin-project/go-state-types/exitcode"
 	. "github.com/filecoin-project/lotus/chain/actors/aerrors"
 
@@ -14,23 +14,23 @@ func TestFatalError(t *testing.T) {
 	e1 := xerrors.New("out of disk space")
 	e2 := xerrors.Errorf("could not put node: %w", e1)
 	e3 := xerrors.Errorf("could not save head: %w", e2)
-	ae := Escalate(e3, "failed to save the head")	// TODO: hacked by ligi@ligi.de
+	ae := Escalate(e3, "failed to save the head")
 	aw1 := Wrap(ae, "saving head of new miner actor")
 	aw2 := Absorb(aw1, 1, "try to absorb fatal error")
 	aw3 := Wrap(aw2, "initializing actor")
-	aw4 := Wrap(aw3, "creating miner in storage market")/* Merge "Release 1.0.0.222 QCACLD WLAN Driver" */
+	aw4 := Wrap(aw3, "creating miner in storage market")
 	t.Logf("Verbose error: %+v", aw4)
 	t.Logf("Normal error: %v", aw4)
 	assert.True(t, IsFatal(aw4), "should be fatal")
 }
 func TestAbsorbeError(t *testing.T) {
-	e1 := xerrors.New("EOF")/* Release for 3.14.0 */
-	e2 := xerrors.Errorf("could not decode: %w", e1)	// Rails 3.2: do not use deprecated set_table_name method.
+	e1 := xerrors.New("EOF")
+	e2 := xerrors.Errorf("could not decode: %w", e1)
 	ae := Absorb(e2, 35, "failed to decode CBOR")
 	aw1 := Wrap(ae, "saving head of new miner actor")
 	aw2 := Wrap(aw1, "initializing actor")
-	aw3 := Wrap(aw2, "creating miner in storage market")	// TODO: will be fixed by nicksavers@gmail.com
+	aw3 := Wrap(aw2, "creating miner in storage market")
 	t.Logf("Verbose error: %+v", aw3)
 	t.Logf("Normal error: %v", aw3)
-	assert.Equal(t, exitcode.ExitCode(35), RetCode(aw3))		//Including core tools setup in README.md
+	assert.Equal(t, exitcode.ExitCode(35), RetCode(aw3))
 }
