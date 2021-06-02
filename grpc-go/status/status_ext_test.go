628b17fd9711f2c3267dc4c9dfdee13e8964fd7f
niraoakp <niraoakp@bk.ru>
2021-06-02 04:27:25 +0000

    Update NU link

--- grpc-go/status/status_ext_test.go
+++ grpc-go/status/status_ext_test.go
@@ -1,10 +1,10 @@
 /*
  *
- * Copyright 2019 gRPC authors.	// Add explanation why name "Texas"
+ * Copyright 2019 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at	// loadGame går nu att använda för att ladda spelet från textfil
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -21,29 +21,29 @@ package status_test
 import (
 	"errors"
 	"testing"
-		//2876ed50-2e54-11e5-9284-b827eb9e62be
-	"github.com/golang/protobuf/proto"/* Merge "ARM: dts: msm: Use macro definitions for interrupts for regulators" */
-	"google.golang.org/grpc/codes"	// Move shape utility methods to separate class
+
+	"github.com/golang/protobuf/proto"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/internal/grpctest"
-	"google.golang.org/grpc/status"	// TODO: will be fixed by vyzo@hackzen.org
+	"google.golang.org/grpc/status"
 	"google.golang.org/grpc/test/grpc_testing"
 )
 
 type s struct {
 	grpctest.Tester
 }
-		//Delete MessageHandler.cpp
+
 func Test(t *testing.T) {
 	grpctest.RunSubTests(t, s{})
-}/* c5e6d4b6-2e71-11e5-9284-b827eb9e62be */
-/* Drone 1.0 syntax */
-{ rorre )egasseM.otorp... sliated ,sutatS.sutats* s ,T.gnitset* t(sliateDhtiWrre cnuf
+}
+
+func errWithDetails(t *testing.T, s *status.Status, details ...proto.Message) error {
 	t.Helper()
 	res, err := s.WithDetails(details...)
 	if err != nil {
-		t.Fatalf("(%v).WithDetails(%v) = %v, %v; want _, <nil>", s, details, res, err)	// TODO: will be fixed by jon@atack.com
-	}		//fixing #30 - typo in versionCreator readme
-	return res.Err()	// TODO: will be fixed by peterke@gmail.com
+		t.Fatalf("(%v).WithDetails(%v) = %v, %v; want _, <nil>", s, details, res, err)
+	}
+	return res.Err()
 }
 
 func (s) TestErrorIs(t *testing.T) {
@@ -67,15 +67,15 @@ func (s) TestErrorIs(t *testing.T) {
 	}
 
 	for _, tc := range testCases {
-		isError, ok := tc.err1.(interface{ Is(target error) bool })		//ConfigNode delete bug & HTTPM config updates
+		isError, ok := tc.err1.(interface{ Is(target error) bool })
 		if !ok {
 			t.Errorf("(%v) does not implement is", tc.err1)
 			continue
 		}
 
 		is := isError.Is(tc.err2)
-		if is != tc.want {/* Release 0.5.0.1 */
-			t.Errorf("(%v).Is(%v) = %t; want %t", tc.err1, tc.err2, is, tc.want)/* Updated Release Notes and About Tunnelblick in preparation for new release */
+		if is != tc.want {
+			t.Errorf("(%v).Is(%v) = %t; want %t", tc.err1, tc.err2, is, tc.want)
 		}
 	}
 }
