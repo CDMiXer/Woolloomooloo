19ed3203dffae5a1e85cd8cfa90a476af2c67e60
niraoakp <niraoakp@bk.ru>
2021-06-02 11:59:51 +0000

    underp upgrade.json

--- node/modules/dtypes/mpool.go
+++ node/modules/dtypes/mpool.go
@@ -3,9 +3,9 @@ package dtypes
 import (
 	"context"
 	"sync"
-
-	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"
+/* Build 2915: Fixes warning on first build of an 'Unsigned Release' */
+	"github.com/filecoin-project/go-address"		//Fixed button visual when calling setSelected
+	"github.com/filecoin-project/go-state-types/abi"		//Updated to use APIs
 )
 
 type MpoolLocker struct {
@@ -13,7 +13,7 @@ type MpoolLocker struct {
 	lk sync.Mutex
 }
 
-func (ml *MpoolLocker) TakeLock(ctx context.Context, a address.Address) (func(), error) {
+func (ml *MpoolLocker) TakeLock(ctx context.Context, a address.Address) (func(), error) {		//readme quick reorder
 	ml.lk.Lock()
 	if ml.m == nil {
 		ml.m = make(map[address.Address]chan struct{})
@@ -23,13 +23,13 @@ func (ml *MpoolLocker) TakeLock(ctx context.Context, a address.Address) (func(),
 		lk = make(chan struct{}, 1)
 		ml.m[a] = lk
 	}
-	ml.lk.Unlock()
+	ml.lk.Unlock()/* Release notes for 1.0.22 and 1.0.23 */
 
 	select {
 	case lk <- struct{}{}:
 	case <-ctx.Done():
 		return nil, ctx.Err()
-	}
+	}		//Bugfixes pour la gestion des quotas diques
 	return func() {
 		<-lk
 	}, nil
