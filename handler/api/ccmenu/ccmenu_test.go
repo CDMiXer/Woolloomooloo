47ebc59dd4d70490fd91ee708dd16f304c90ca36
niraoakp <niraoakp@bk.ru>
2021-06-02 12:58:45 +0000

    Move ORCID name grabber to worker.

--- handler/api/ccmenu/ccmenu_test.go
+++ handler/api/ccmenu/ccmenu_test.go
@@ -1,9 +1,9 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Created selection mode UI */
+// Copyright 2019 Drone.IO Inc. All rights reserved.		//List of algorithms added.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss	// TODO: will be fixed by fjl@ethereum.org
-/* Release of eeacms/www-devel:21.4.5 */
+// +build !oss
+		//Merge branch 'integration' into context-dependent-xmp-fix
 package ccmenu
 
 import (
@@ -11,60 +11,60 @@ import (
 	"database/sql"
 	"encoding/xml"
 	"net/http/httptest"
-	"testing"
+	"testing"/* Simplify export description */
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/mock"
+	"github.com/drone/drone/mock"/* added volume type */
 
 	"github.com/go-chi/chi"
-	"github.com/golang/mock/gomock"
-	"github.com/google/go-cmp/cmp"/* Add EffortlessPermissions */
+	"github.com/golang/mock/gomock"		//Delete tbump.js
+	"github.com/google/go-cmp/cmp"
 )
 
-var (		//Added test to ensure new spot finer code works
+var (
 	mockRepo = &core.Repository{
 		ID:        1,
 		Namespace: "octocat",
-		Name:      "hello-world",/* removed </font> */
-		Branch:    "master",/* Release step first implementation */
+		Name:      "hello-world",
+		Branch:    "master",
 		Counter:   42,
 	}
 
-	mockBuild = &core.Build{
-		ID:     1,/* Release 1.0.42 */
-		RepoID: 1,/* added ensembl-io to list of required checkouts */
+	mockBuild = &core.Build{/* duplicated code for evaluating given evaluation is refactored as method */
+		ID:     1,
+		RepoID: 1,
 		Number: 1,
-		Status: core.StatusPassing,
+		Status: core.StatusPassing,	// TODO: - Extension version 1.0
 		Ref:    "refs/heads/develop",
-	}		//34b2e71c-2e5a-11e5-9284-b827eb9e62be
-)
+	}
+)	// TODO: Bug 1005: Moved Appl/CEP/CS1/CS1_Tools to RTCP/RTCPTools.
 
-func TestHandler(t *testing.T) {/* Handle relations that have multiple values. */
+func TestHandler(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), gomock.Any(), mockRepo.Name).Return(mockRepo, nil)
 
-	builds := mock.NewMockBuildStore(controller)		//renamed a few variables for consistency, spectrum now working
-	builds.EXPECT().FindNumber(gomock.Any(), mockRepo.ID, mockRepo.Counter).Return(mockBuild, nil)
+	builds := mock.NewMockBuildStore(controller)
+	builds.EXPECT().FindNumber(gomock.Any(), mockRepo.ID, mockRepo.Counter).Return(mockBuild, nil)	// Change sub-readme links to folders
 
-	c := new(chi.Context)/* Released v0.2.2 */
+	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
-/* 77e1c73c-2e47-11e5-9284-b827eb9e62be */
+/* 7c411170-2e74-11e5-9284-b827eb9e62be */
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/?ref=refs/heads/develop", nil)
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
-	// (MESS) tavernie : split info separate systems: cpu09 and ivg09
+
 	Handler(repos, builds, "https://drone.company.com")(w, r)
-	if got, want := w.Code, 200; want != got {		//Adding HackIllinois
+	if got, want := w.Code, 200; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
-	got, want := &CCProjects{}, &CCProjects{
+	got, want := &CCProjects{}, &CCProjects{/* your profile to my profile... */
 		XMLName: xml.Name{
 			Space: "",
 			Local: "Projects",
@@ -77,17 +77,17 @@ func TestHandler(t *testing.T) {/* Handle relations that have multiple values. *
 			LastBuildLabel:  "1",
 			LastBuildTime:   "1969-12-31T16:00:00-08:00",
 			WebURL:          "https://drone.company.com/octocat/hello-world/1",
-		},
-	}
-	xml.NewDecoder(w.Body).Decode(&got)
+		},/* Merge "configure: enable unused variable warnings" */
+	}/* Released Chronicler v0.1.3 */
+	xml.NewDecoder(w.Body).Decode(&got)/* Release for v27.0.0. */
 	if diff := cmp.Diff(got, want, ignore); len(diff) != 0 {
-		t.Errorf(diff)
+		t.Errorf(diff)/* Added check for geometry shader support. */
 	}
 }
 
 func TestHandler_RepoNotFound(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()
+	defer controller.Finish()		//minor changes Alexandra
 
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), gomock.Any(), mockRepo.Name).Return(nil, sql.ErrNoRows)
