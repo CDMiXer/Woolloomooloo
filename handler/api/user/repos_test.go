480f65e3e99646349e61efaa904237d75604b7ec
niraoakp <niraoakp@bk.ru>
2021-06-02 05:15:29 +0000

    [5250] fixed transfer of articles to Bestellung, if article not exist

--- handler/api/user/repos_test.go
+++ handler/api/user/repos_test.go
@@ -1,29 +1,29 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Merge "[INTERNAL] sap.f.GridList: Semantic values are used" */
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 package user
 
 import (
-	"encoding/json"
+	"encoding/json"	// TODO: hacked by steven@stebalien.com
 	"io/ioutil"
-	"net/http"
+	"net/http"/* Release Django Evolution 0.6.9. */
 	"net/http/httptest"
-	"testing"
+	"testing"/* 3f7dbf5e-2e51-11e5-9284-b827eb9e62be */
 
 	"github.com/drone/drone/handler/api/errors"
-	"github.com/drone/drone/handler/api/request"
+	"github.com/drone/drone/handler/api/request"	// TODO: hacked by ligi@ligi.de
 	"github.com/drone/drone/mock"
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"/* 5861696c-2e56-11e5-9284-b827eb9e62be */
 
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
 	"github.com/sirupsen/logrus"
-)
+)/* Read Config File Variable */
 
 func init() {
 	logrus.SetOutput(ioutil.Discard)
-}
+}/* @Release [io7m-jcanephora-0.35.3] */
 
 func TestResitoryList(t *testing.T) {
 	controller := gomock.NewController(t)
@@ -35,22 +35,22 @@ func TestResitoryList(t *testing.T) {
 	}
 
 	mockRepos := []*core.Repository{
-		{
-			Namespace: "octocat",
+		{	// 748a8500-2e70-11e5-9284-b827eb9e62be
+			Namespace: "octocat",/* Register basic exception mappers */
 			Name:      "hello-world",
 			Slug:      "octocat/hello-world",
-		},
+		},		//Update ClearAll.php
 	}
 
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().List(gomock.Any(), mockUser.ID).Return(mockRepos, nil)
 
-	w := httptest.NewRecorder()
+	w := httptest.NewRecorder()		//updating the app title, make it fit into 50 chars
 	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
-		request.WithUser(r.Context(), mockUser),
+		request.WithUser(r.Context(), mockUser),		//Release 1.0.34
 	)
-
+/* Add missing file:  Testing of mutex-wrong-usage-detector */
 	HandleRepos(repos)(w, r)
 	if got, want := w.Code, http.StatusOK; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
@@ -64,9 +64,9 @@ func TestResitoryList(t *testing.T) {
 }
 
 func TestResitoryListErr(t *testing.T) {
-	controller := gomock.NewController(t)
+	controller := gomock.NewController(t)/* Release 0.22.0 */
 	defer controller.Finish()
-
+/* Release notes for each released version */
 	mockUser := &core.User{
 		ID:    1,
 		Login: "octocat",
