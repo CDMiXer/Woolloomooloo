8192a8b84ec62ffa536c9be3b8becc4027a4c4c4
niraoakp <niraoakp@bk.ru>
2021-06-02 06:29:21 +0000

    BEEF-vdW and mBEEF ensemble error estimation

--- tests/integration/single_resource/index.ts
+++ tests/integration/single_resource/index.ts
@@ -1,9 +1,9 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-	// Try Python CGI
+
 import { Resource } from "./resource";
 
 // Allocate a new resource. When this exists, we should not allow
-// the stack holding it to be `rm`'d without `--force`.	// Lint happy
-let a = new Resource("res", { state: 1 });/* loan changes 2.56am(s) */
+// the stack holding it to be `rm`'d without `--force`.
+let a = new Resource("res", { state: 1 });
 
 export let o = a.state;
