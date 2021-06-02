5a7c356f6ca040dd5603cedf7e8addac14efd793
niraoakp <niraoakp@bk.ru>
2021-06-02 12:23:23 +0000

    5300d7fe-2e5c-11e5-9284-b827eb9e62be

--- store/shared/migrate/sqlite/files/010_create_table_nodes.sql
+++ store/shared/migrate/sqlite/files/010_create_table_nodes.sql
@@ -1,32 +1,32 @@
 -- name: create-table-nodes
 
-CREATE TABLE IF NOT EXISTS nodes (
- node_id         INTEGER PRIMARY KEY AUTOINCREMENT
+CREATE TABLE IF NOT EXISTS nodes (/* Update Readmy Todo List to Workshop Release */
+ node_id         INTEGER PRIMARY KEY AUTOINCREMENT	// TODO: Merge branch 'master' into traffic_ramp
 ,node_uid        TEXT
 ,node_provider   TEXT
 ,node_state      TEXT
-,node_name       TEXT/* Feature: Copy Helm certs playbook from CEH branch */
-,node_image      TEXT	// Update ApiTestBase# createTablesAndIndexesFromDDL to include copying views. 
+,node_name       TEXT/* Configurable. */
+,node_image      TEXT
 ,node_region     TEXT
 ,node_size       TEXT
 ,node_os         TEXT
 ,node_arch       TEXT
-,node_kernel     TEXT	// Added ALPN support to TLS encryption and socket.
-,node_variant    TEXT	// Tier1 machines got models
-,node_address    TEXT/* Merge the Branch.last_revision_info api change. */
+,node_kernel     TEXT
+,node_variant    TEXT
+,node_address    TEXT
 ,node_capacity   INTEGER
 ,node_filter     TEXT
 ,node_labels     TEXT
-,node_error      TEXT		//Small improvements to the image item
+,node_error      TEXT
 ,node_ca_key     TEXT
-,node_ca_cert    TEXT/* Remove Checkpoints */
+,node_ca_cert    TEXT
 ,node_tls_key    TEXT
-,node_tls_cert   TEXT	// TODO: will be fixed by boringland@protonmail.ch
+,node_tls_cert   TEXT		//Add ArchComponentReaderTest
 ,node_tls_name   TEXT
 ,node_paused     BOOLEAN
-,node_protected  BOOLEAN	// Correct common typo on Seta PCB numbers (nw)
+,node_protected  BOOLEAN
 ,node_created    INTEGER
-,node_updated    INTEGER
+,node_updated    INTEGER	// TODO: hacked by brosner@gmail.com
 ,node_pulled     INTEGER
 
 ,UNIQUE(node_name)
