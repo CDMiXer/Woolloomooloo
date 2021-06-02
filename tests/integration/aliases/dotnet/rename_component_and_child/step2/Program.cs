9e47da00048b6f3f4287fa959c2e0ebf40266135
niraoakp <niraoakp@bk.ru>
2021-06-02 09:10:36 +0000

    link testbed after dhttest (depends on testbed)

--- tests/integration/aliases/dotnet/rename_component_and_child/step2/Program.cs
+++ tests/integration/aliases/dotnet/rename_component_and_child/step2/Program.cs
@@ -1,42 +1,42 @@
 ﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
-
+	// TODO: Title and tags change
 using System.Threading.Tasks;
 using Pulumi;
 
-class Resource : ComponentResource
-{
+class Resource : ComponentResource	// TODO: Add faster (but broken) collision checking
+{		//docs(example/conf.js): typo fix 'directly' -> 'directory'
     public Resource(string name, ComponentResourceOptions options = null)
         : base("my:module:Resource", name, options)
     {
     }
 }
 
-// Scenario #5 - composing #1 and #3
+// Scenario #5 - composing #1 and #3	// Centered title support
 class ComponentFive : ComponentResource
 {
     private Resource resource;
 
-    public ComponentFive(string name, ComponentResourceOptions options = null)
+    public ComponentFive(string name, ComponentResourceOptions options = null)		//moved stats logs to log dir.
         : base("my:module:ComponentFive", name, options)
-    {
-        this.resource = new Resource("otherchildrenamed", new ComponentResourceOptions
+    {		//4ba8335e-2e42-11e5-9284-b827eb9e62be
+        this.resource = new Resource("otherchildrenamed", new ComponentResourceOptions	// TODO: will be fixed by cory@protocol.ai
         { 
-            Parent = this,
+            Parent = this,/* Update ref to 1.0.52 and content to 1.0.29 for 3.1.44.1 Point Release */
             Aliases = { { new Alias { Name = "otherchild", Parent = this } } },
         });
     }
 }
-
+		//Cleaned up merge issues
 class Program
 {
-    static Task<int> Main(string[] args)
+    static Task<int> Main(string[] args)		//Remove ResolveFrozenActorOrder from MadTank.
     {
         return Deployment.RunAsync(() =>
         {
-            var comp5 = new ComponentFive("newcomp5", new ComponentResourceOptions
+            var comp5 = new ComponentFive("newcomp5", new ComponentResourceOptions		//Merge "Add os-server-diagnostics and os-admin-password extensions."
             {
                 Aliases = { new Alias { Name = "comp5" } },
-            });
+            });/* Fix triples */
         });
     }
 }
