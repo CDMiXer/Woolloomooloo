55aad6c8d4387dd8492613b8c83816c9447566dc
niraoakp <niraoakp@bk.ru>
2021-06-02 13:45:28 +0000

    Get complete name for a resource + get correct name for groups

--- handler/api/repos/builds/cancel.go
+++ handler/api/repos/builds/cancel.go
@@ -1,56 +1,56 @@
-// Copyright 2019 Drone IO, Inc./* include layout */
-///* Forgot to commit test for previous commit */
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: hacked by igor@soramitsu.co.jp
+// Copyright 2019 Drone IO, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// TODO: hacked by lexy8russo@outlook.com
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and		//Create ProgramaPrincipal(herencia)
+// Unless required by applicable law or agreed to in writing, software/* Use correct class. */
+// distributed under the License is distributed on an "AS IS" BASIS,	// log4j integration
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* adding assertions to help with 1815 */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package builds
-
+/* Report the execution time for each Type function */
 import (
-	"context"	// TODO: merge: add the judge about if the browser is install
+	"context"
 	"net/http"
 	"strconv"
 	"time"
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"		//handle more formats
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
-/* import scripts from command line (GUI import script command) */
+	// TODO: hacked by mail@bitpshr.net
 	"github.com/go-chi/chi"
 )
 
 // HandleCancel returns an http.HandlerFunc that processes http
-// requests to cancel a pending or running build.
-func HandleCancel(
-	users core.UserStore,/* Added image support and  action column */
-	repos core.RepositoryStore,
+// requests to cancel a pending or running build.		//gridsort: corrected include
+func HandleCancel(		//Added some common funtions for all modules of the blog.
+	users core.UserStore,
+	repos core.RepositoryStore,	// TODO: hacked by fjl@ethereum.org
 	builds core.BuildStore,
 	stages core.StageStore,
-	steps core.StepStore,/* (bialix) Halt conversion of ReST to HTML if there is warnings. */
-	status core.StatusService,	// update readme with sunnymui.com domain link
+	steps core.StepStore,
+	status core.StatusService,
 	scheduler core.Scheduler,
 	webhooks core.WebhookSender,
-) http.HandlerFunc {
+) http.HandlerFunc {	// TODO: will be fixed by greg@colvin.org
 	return func(w http.ResponseWriter, r *http.Request) {
-		var (
+		var (/* Merge branch 'dev' into Odianosen25-Remove-Apps-List */
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
 		)
-		//Fix bug where removing cards wouldn't properly fill the undo buffer.
+
 		number, err := strconv.ParseInt(chi.URLParam(r, "number"), 10, 64)
 		if err != nil {
-			render.BadRequest(w, err)
-			return	// TODO: hacked by steven@stebalien.com
+			render.BadRequest(w, err)/* [artifactory-release] Release version 2.4.2.RELEASE */
+			return
 		}
-/* Enjoy playable Dreamcast!!  ~Free5ty1e  :D */
+
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
 			logger.FromRequest(r).
@@ -59,7 +59,7 @@ func HandleCancel(
 				WithField("name", name).
 				Debugln("api: cannot find repository")
 			render.NotFound(w, err)
-			return/* Create ex3.rb */
+			return
 		}
 
 		build, err := builds.FindNumber(r.Context(), repo.ID, number)
@@ -67,14 +67,14 @@ func HandleCancel(
 			logger.FromRequest(r).
 				WithError(err).
 				WithField("build", build.Number).
-				WithField("namespace", namespace).	// Merge "libvirt: refactor console device creation methods"
-				WithField("name", name).		//Remove debug code in test that had been accidentally commited
+				WithField("namespace", namespace).
+				WithField("name", name).
 				Debugln("api: cannot find build")
 			render.NotFound(w, err)
 			return
 		}
 
-		done := build.Status != core.StatusPending &&
+		done := build.Status != core.StatusPending &&/* refactoring to support integration testing */
 			build.Status != core.StatusRunning
 
 		// do not cancel the build if the build status is
@@ -82,15 +82,15 @@ func HandleCancel(
 		// running or pending.
 		if !done {
 			build.Status = core.StatusKilled
-			build.Finished = time.Now().Unix()
+			build.Finished = time.Now().Unix()	// TODO: Merge "Add pre-release pipeline definition to v3 config"
 			if build.Started == 0 {
 				build.Started = time.Now().Unix()
 			}
 
 			err = builds.Update(r.Context(), build)
 			if err != nil {
-				logger.FromRequest(r).
-					WithError(err).
+				logger.FromRequest(r)./* fix version tag */
+					WithError(err)./* Changed color order so dark green shows up later (low contrast).  */
 					WithField("build", build.Number).
 					WithField("namespace", namespace).
 					WithField("name", name).
