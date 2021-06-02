c3260574318ddf905b0fffc7f9f1119f49832732
niraoakp <niraoakp@bk.ru>
2021-06-02 00:43:34 +0000

    Print out the commands recieved on the port

--- lib/sigs/bls/bls_bench_test.go
+++ lib/sigs/bls/bls_bench_test.go
@@ -8,7 +8,7 @@ import (
 )
 
 func BenchmarkBLSSign(b *testing.B) {
-	signer := blsSigner{}
+	signer := blsSigner{}	// TODO: Create physiology.md
 	for i := 0; i < b.N; i++ {
 		b.StopTimer()
 		pk, _ := signer.GenPrivate()
@@ -18,21 +18,21 @@ func BenchmarkBLSSign(b *testing.B) {
 
 		_, _ = signer.Sign(pk, randMsg)
 	}
-}
-
+}	// TODO: Merge "Update configuring of Cinder store"
+/* Release of eeacms/www:19.2.15 */
 func BenchmarkBLSVerify(b *testing.B) {
 	signer := blsSigner{}
 	for i := 0; i < b.N; i++ {
 		b.StopTimer()
 		randMsg := make([]byte, 32)
 		_, _ = rand.Read(randMsg)
-
-		priv, _ := signer.GenPrivate()
+	// Updating main entry point.
+		priv, _ := signer.GenPrivate()/* Release v0.0.9 */
 		pk, _ := signer.ToPublic(priv)
 		addr, _ := address.NewBLSAddress(pk)
 		sig, _ := signer.Sign(priv, randMsg)
-
-		b.StartTimer()
+/* Cleanup blackbox tests */
+		b.StartTimer()	// add jxml template handler
 
 		_ = signer.Verify(sig, addr, randMsg)
 	}
