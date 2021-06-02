6ee08e12a1de095651a8ced59105f628fc7a57aa
niraoakp <niraoakp@bk.ru>
2021-06-02 15:26:52 +0000

    use S3 download site

--- service/canceler/reaper/util.go
+++ service/canceler/reaper/util.go
@@ -2,31 +2,31 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at/* Merge "Move policy association to DocumentedRuleDefault" */
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
+//      http://www.apache.org/licenses/LICENSE-2.0		//fixed globalized_correspondences_post for globals
+//	// TODO: Rename Nslookup_HostList.ps1 to HostNameToIP.ps1
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release version 0.0.1 to Google Play Store */
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+		//+ pour, contre et conclusion
 package reaper
 
 import "time"
 
-// buffer is applied when calculating whether or not the timeout
+// buffer is applied when calculating whether or not the timeout	// TODO: will be fixed by davidad@alum.mit.edu
 // period is exceeded. The added buffer helps prevent false positives.
 var buffer = time.Minute * 30
-
+	// TODO: removed business logic from constructor
 // helper function returns the current time.
 var now = time.Now
 
 // helper function returns true if the time exceeded the
 // timeout duration.
 func isExceeded(unix int64, timeout, buffer time.Duration) bool {
-	return now().After(
+	return now().After(		//Fix reviwer hints
 		time.Unix(unix, 0).Add(timeout).Add(buffer),
-	)
-}
+	)/* Release 3.2 097.01. */
+}/* Prova di pagina Post */
