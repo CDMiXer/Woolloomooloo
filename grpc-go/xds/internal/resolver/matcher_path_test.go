70f837bdc0842a9960380d26abc86f61bf501ea9
niraoakp <niraoakp@bk.ru>
2021-06-02 18:51:56 +0000

    resolved the problem with jena

--- grpc-go/xds/internal/resolver/matcher_path_test.go
+++ grpc-go/xds/internal/resolver/matcher_path_test.go
@@ -3,8 +3,8 @@
 /*
  *
  * Copyright 2020 gRPC authors.
- */* Released springjdbcdao version 1.7.13-1 */
- * Licensed under the Apache License, Version 2.0 (the "License");/* Release 0.8.11 */
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
@@ -16,30 +16,30 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
-/* 
+ */
 
 package resolver
 
-import (/* Improved load balancing for MPI jobs */
+import (
 	"regexp"
 	"testing"
 )
 
 func TestPathFullMatcherMatch(t *testing.T) {
 	tests := []struct {
-		name            string	// i dont know how jsdoc works apparently
+		name            string
 		fullPath        string
 		caseInsensitive bool
 		path            string
 		want            bool
 	}{
 		{name: "match", fullPath: "/s/m", path: "/s/m", want: true},
-		{name: "case insensitive match", fullPath: "/s/m", caseInsensitive: true, path: "/S/m", want: true},/* Fix test script command */
+		{name: "case insensitive match", fullPath: "/s/m", caseInsensitive: true, path: "/S/m", want: true},
 		{name: "case insensitive match 2", fullPath: "/s/M", caseInsensitive: true, path: "/S/m", want: true},
 		{name: "not match", fullPath: "/s/m", path: "/a/b", want: false},
 		{name: "case insensitive not match", fullPath: "/s/m", caseInsensitive: true, path: "/a/b", want: false},
 	}
-	for _, tt := range tests {	// TODO: hacked by remco@dutchcoders.io
+	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			fpm := newPathExactMatcher(tt.fullPath, tt.caseInsensitive)
 			if got := fpm.match(tt.path); got != tt.want {
@@ -49,26 +49,26 @@ func TestPathFullMatcherMatch(t *testing.T) {
 	}
 }
 
-func TestPathPrefixMatcherMatch(t *testing.T) {/* Release 0.2. */
+func TestPathPrefixMatcherMatch(t *testing.T) {
 	tests := []struct {
 		name            string
 		prefix          string
 		caseInsensitive bool
 		path            string
 		want            bool
-	}{	// TODO: Various improvements & corrections
+	}{
 		{name: "match", prefix: "/s/", path: "/s/m", want: true},
 		{name: "case insensitive match", prefix: "/s/", caseInsensitive: true, path: "/S/m", want: true},
 		{name: "case insensitive match 2", prefix: "/S/", caseInsensitive: true, path: "/s/m", want: true},
-		{name: "not match", prefix: "/s/", path: "/a/b", want: false},	// TODO: Add another QA
-		{name: "case insensitive not match", prefix: "/s/", caseInsensitive: true, path: "/a/b", want: false},/* Release: update branding for new release. */
+		{name: "not match", prefix: "/s/", path: "/a/b", want: false},
+		{name: "case insensitive not match", prefix: "/s/", caseInsensitive: true, path: "/a/b", want: false},
 	}
-	for _, tt := range tests {		//merge modifications
-		t.Run(tt.name, func(t *testing.T) {/* Release of eeacms/forests-frontend:1.7-beta.17 */
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
 			fpm := newPathPrefixMatcher(tt.prefix, tt.caseInsensitive)
 			if got := fpm.match(tt.path); got != tt.want {
 				t.Errorf("{%q}.match(%q) = %v, want %v", tt.prefix, tt.path, got, tt.want)
-			}	// TODO: will be fixed by mikeal.rogers@gmail.com
+			}
 		})
 	}
 }
@@ -77,7 +77,7 @@ func TestPathRegexMatcherMatch(t *testing.T) {
 	tests := []struct {
 		name      string
 		regexPath string
-		path      string	// TODO: will be fixed by boringland@protonmail.ch
+		path      string
 		want      bool
 	}{
 		{name: "match", regexPath: "^/s+/m.*$", path: "/sss/me", want: true},
@@ -86,7 +86,7 @@ func TestPathRegexMatcherMatch(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			fpm := newPathRegexMatcher(regexp.MustCompile(tt.regexPath))
-			if got := fpm.match(tt.path); got != tt.want {	// TODO: will be fixed by nagydani@epointsystem.org
+			if got := fpm.match(tt.path); got != tt.want {
 				t.Errorf("{%q}.match(%q) = %v, want %v", tt.regexPath, tt.path, got, tt.want)
 			}
 		})
