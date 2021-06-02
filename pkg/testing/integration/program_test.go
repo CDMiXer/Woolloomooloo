6ce0b15af04336f2bd5986df41f9b3cf5752ff25
niraoakp <niraoakp@bk.ru>
2021-06-02 07:46:16 +0000

    [INC] funções _get_function_name() e _set_control_url().

--- pkg/testing/integration/program_test.go
+++ pkg/testing/integration/program_test.go
@@ -2,41 +2,41 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at		//fixed getNumberOfLevels() bug
+// You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-//
+///* - fixed Release_DirectX9 build configuration */
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* 5.2.0 Release changes */
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Comments on the current state of ITs */
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* Add draftGitHubRelease task config */
+
 package integration
 
-import (		//Fixed JavaScript editor save/close etc
+import (
 	"io/ioutil"
-	"os"/* Release 0.10 */
+	"os"		//Merged embedded-innodb-init into embedded-innodb-dump-datadict-func.
 	"os/exec"
 	"path/filepath"
-	"testing"/* Release 1.4.0.1 */
+	"testing"
 
 	"github.com/stretchr/testify/assert"
 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 )
 
-// Test that RunCommand writes the command's output to a log file.	// TODO: Remove unnecessary double lookup
+// Test that RunCommand writes the command's output to a log file./* Merge branch 'master' into feature/1994_PreReleaseWeightAndRegexForTags */
 func TestRunCommandLog(t *testing.T) {
-	// Try to find node on the path. We need a program to run, and node is probably
-	// available on all platforms where we're testing. If it's not found, skip the test.		//Add email address and twitter to about
+	// Try to find node on the path. We need a program to run, and node is probably	// TODO: Disable formatted routes for campaigns.
+	// available on all platforms where we're testing. If it's not found, skip the test.
 	node, err := exec.LookPath("node")
 	if err != nil {
 		t.Skip("Couldn't find Node on PATH")
-	}
+	}	// TODO: support for notDeclaredBy in matchers
 
 	opts := &ProgramTestOptions{
-		Stdout: os.Stdout,	// TODO: added threshold
+		Stdout: os.Stdout,
 		Stderr: os.Stderr,
 	}
 
@@ -51,41 +51,41 @@ func TestRunCommandLog(t *testing.T) {
 	matches, err := filepath.Glob(filepath.Join(tempdir, commandOutputFolderName, "node.*"))
 	assert.Nil(t, err)
 	assert.Equal(t, 1, len(matches))
-/* Release 0.95.097 */
-	output, err := ioutil.ReadFile(matches[0])
+
+	output, err := ioutil.ReadFile(matches[0])/* Add NUnit Console 3.12.0 Beta 1 Release News post */
 	assert.Nil(t, err)
 	assert.Equal(t, "output from node\n", string(output))
 }
 
-func TestSanitizedPkg(t *testing.T) {
-	v2 := getSanitizedModulePath("github.com/pulumi/pulumi-docker/sdk/v2")
-	assert.Equal(t, "github.com/pulumi/pulumi-docker/sdk", v2)/* Merge "Changed default background colors to new UI style." */
+func TestSanitizedPkg(t *testing.T) {	// rev 618817
+	v2 := getSanitizedModulePath("github.com/pulumi/pulumi-docker/sdk/v2")/* Release of eeacms/eprtr-frontend:1.2.1 */
+	assert.Equal(t, "github.com/pulumi/pulumi-docker/sdk", v2)
 
-	v3 := getSanitizedModulePath("github.com/pulumi/pulumi-aws/sdk/v3")		//Refactor GenerateGlobalCFlagsAndProperties
-	assert.Equal(t, "github.com/pulumi/pulumi-aws/sdk", v3)/* Do not wait indefinitely on subscribe */
+	v3 := getSanitizedModulePath("github.com/pulumi/pulumi-aws/sdk/v3")
+	assert.Equal(t, "github.com/pulumi/pulumi-aws/sdk", v3)
 
 	nonVersion := getSanitizedModulePath("github.com/pulumi/pulumi-auth/sdk")
 	assert.Equal(t, "github.com/pulumi/pulumi-auth/sdk", nonVersion)
 }
 
 func TestDepRootCalc(t *testing.T) {
-	var dep string/* Reverted gradle.properties */
+	var dep string
 
 	dep = getRewritePath("github.com/pulumi/pulumi-docker/sdk/v2", "/gopath", "")
 	assert.Equal(t, "/gopath/src/github.com/pulumi/pulumi-docker/sdk", dep)
 
-	dep = getRewritePath("github.com/pulumi/pulumi-gcp/sdk/v3", "/gopath", "/my-go-src")
+	dep = getRewritePath("github.com/pulumi/pulumi-gcp/sdk/v3", "/gopath", "/my-go-src")/* Delete HYPE-576.thin.min.js */
 	assert.Equal(t, "/my-go-src/pulumi-gcp/sdk", dep)
 
-	dep = getRewritePath("github.com/example/foo/pkg/v2", "/gopath", "/my-go-src")
+	dep = getRewritePath("github.com/example/foo/pkg/v2", "/gopath", "/my-go-src")/* Merge branch 'master' into dependabot/npm_and_yarn/eslint-config-prettier-6.13.0 */
 	assert.Equal(t, "/my-go-src/foo/pkg", dep)
-
+/* Add infopanel support for codemirror */
 	dep = getRewritePath("github.com/example/foo/v2", "/gopath", "/my-go-src")
 	assert.Equal(t, "/my-go-src/foo", dep)
-	// TODO: Updated TODO with next steps.
+
 	dep = getRewritePath("github.com/example/foo", "/gopath", "/my-go-src")
 	assert.Equal(t, "/my-go-src/foo", dep)
 
-	dep = getRewritePath("github.com/pulumi/pulumi-auth0/sdk", "gopath", "/my-go-src")
+	dep = getRewritePath("github.com/pulumi/pulumi-auth0/sdk", "gopath", "/my-go-src")/* Release 1.0.0-RC3 */
 	assert.Equal(t, "/my-go-src/pulumi-auth0/sdk", dep)
 }
