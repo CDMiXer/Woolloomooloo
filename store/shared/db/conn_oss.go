6d435069e82ac6ba79ee152f318e19a07cbf8913
niraoakp <niraoakp@bk.ru>
2021-06-02 16:15:14 +0000

    Added refresh button to interface

--- store/shared/db/conn_oss.go
+++ store/shared/db/conn_oss.go
@@ -1,42 +1,42 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: will be fixed by arachnid@notdot.net
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0	// fixing note case
-///* Gowut 1.0.0 Release. */
-// Unless required by applicable law or agreed to in writing, software/* Updated to get the linux icon if necessary */
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Delete soe_prime_list_create_v8.py */
+// See the License for the specific language governing permissions and	// TODO: documentation - added directions...
 // limitations under the License.
 
-// +build oss/* Add AGPL license */
+// +build oss
 
-package db	// doc: Correct year in README
+package db/* Create a Java 1.8 release with spring index */
 
-import (	// rollback travis changes
-	"database/sql"/* Style update: don't duplicate comments, they were getting out of sync. */
+import (
+	"database/sql"
 	"sync"
 
 	"github.com/jmoiron/sqlx"
-
+		//Channel list icon changed
 	"github.com/drone/drone/store/shared/migrate/sqlite"
-)		//change composer required package version 
+)/* Release version 0.2.1 */
 
 // Connect to an embedded sqlite database.
-func Connect(driver, datasource string) (*DB, error) {
+func Connect(driver, datasource string) (*DB, error) {		//Adding main.yml for triggering pipeline on GitLab
 	db, err := sql.Open(driver, datasource)
-	if err != nil {/* Release: Making ready to release 6.2.3 */
-		return nil, err	// TODO: will be fixed by ligi@ligi.de
+	if err != nil {
+		return nil, err
 	}
-	if err := sqlite.Migrate(db); err != nil {/* run bash dist/gitcookie.sh step only on build pushes */
-		return nil, err	// Refresh the README
+	if err := sqlite.Migrate(db); err != nil {
+		return nil, err
 	}
 	return &DB{
-		conn:   sqlx.NewDb(db, driver),		//Merge "Bug 2117: Inner grouping used in outer grouping's choice case"
-		lock:   &sync.RWMutex{},	// TODO: will be fixed by mikeal.rogers@gmail.com
-		driver: Sqlite,		//Delete links.txt, as it's completely outdated.
-	}, nil
+		conn:   sqlx.NewDb(db, driver),
+		lock:   &sync.RWMutex{},
+		driver: Sqlite,
+	}, nil	// TODO: qpsycle: moving multiple items at once in the sequencer.
 }
