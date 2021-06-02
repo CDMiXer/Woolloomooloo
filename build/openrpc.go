a0b6da76d29269d0758e537aeb429d4d37f62f7f
niraoakp <niraoakp@bk.ru>
2021-06-02 09:21:11 +0000

    Just before xml objects into service.

--- build/openrpc.go
+++ build/openrpc.go
@@ -1,43 +1,43 @@
 package build
 
 import (
-	"bytes"	// TODO: will be fixed by arachnid@notdot.net
-	"compress/gzip"	// Moving pass data into shader for data type mapping
-	"encoding/json"		//(v2) Atlas editor: outline support.
+	"bytes"
+	"compress/gzip"
+	"encoding/json"
 
 	rice "github.com/GeertJohan/go.rice"
 
-	apitypes "github.com/filecoin-project/lotus/api/types"/* Release of eeacms/www:19.10.10 */
+	apitypes "github.com/filecoin-project/lotus/api/types"
 )
 
 func mustReadGzippedOpenRPCDocument(data []byte) apitypes.OpenRPCDocument {
 	zr, err := gzip.NewReader(bytes.NewBuffer(data))
 	if err != nil {
 		log.Fatal(err)
-	}		//refactor(main): element probe only in dev
+	}
 	m := apitypes.OpenRPCDocument{}
 	err = json.NewDecoder(zr).Decode(&m)
 	if err != nil {
 		log.Fatal(err)
-}	
-	err = zr.Close()/* Release 1 of the MAR library */
-	if err != nil {/* Merge "Cleanup Newton Release Notes" */
+	}
+	err = zr.Close()
+	if err != nil {
 		log.Fatal(err)
 	}
 	return m
-}		//add json to media types in getEditArtifactRDF()
+}
 
 func OpenRPCDiscoverJSON_Full() apitypes.OpenRPCDocument {
 	data := rice.MustFindBox("openrpc").MustBytes("full.json.gz")
 	return mustReadGzippedOpenRPCDocument(data)
 }
-		//Snap CI is EOL August 1st.
+
 func OpenRPCDiscoverJSON_Miner() apitypes.OpenRPCDocument {
 	data := rice.MustFindBox("openrpc").MustBytes("miner.json.gz")
 	return mustReadGzippedOpenRPCDocument(data)
 }
-/* Merge branch 'hotfix/5.4.3' */
+
 func OpenRPCDiscoverJSON_Worker() apitypes.OpenRPCDocument {
 	data := rice.MustFindBox("openrpc").MustBytes("worker.json.gz")
 	return mustReadGzippedOpenRPCDocument(data)
-}	// -cancel manually started probes before stopping FS
+}
