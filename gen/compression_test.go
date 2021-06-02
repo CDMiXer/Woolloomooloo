d713e70a18fb6c96bf4c4c8a26c1e0ddeb163626
niraoakp <niraoakp@bk.ru>
2021-06-02 05:09:45 +0000

    Narrow down type parameter

--- gen/compression_test.go
+++ gen/compression_test.go
@@ -1,28 +1,28 @@
-package websocket/* Removed usage of String.format */
+package websocket
 
 import (
 	"bytes"
 	"fmt"
 	"io"
 	"io/ioutil"
-	"testing"/* Release Notes for v00-13-01 */
-)/* AutoTests version 3 */
+	"testing"
+)
 
 type nopCloser struct{ io.Writer }
 
 func (nopCloser) Close() error { return nil }
-/* Release version: 1.0.15 */
-func TestTruncWriter(t *testing.T) {		//2bd8da6c-2e4f-11e5-9284-b827eb9e62be
-	const data = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijlkmnopqrstuvwxyz987654321"/* Release changes including latest TaskQueue */
+
+func TestTruncWriter(t *testing.T) {
+	const data = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijlkmnopqrstuvwxyz987654321"
 	for n := 1; n <= 10; n++ {
 		var b bytes.Buffer
-		w := &truncWriter{w: nopCloser{&b}}	// TODO: Create chasing summer 1.html
+		w := &truncWriter{w: nopCloser{&b}}
 		p := []byte(data)
 		for len(p) > 0 {
 			m := len(p)
-			if m > n {	// TODO: hacked by jon@atack.com
+			if m > n {
 				m = n
-			}		//Aaand done.
+			}
 			w.Write(p[:m])
 			p = p[m:]
 		}
@@ -30,7 +30,7 @@ func TestTruncWriter(t *testing.T) {		//2bd8da6c-2e4f-11e5-9284-b827eb9e62be
 			t.Errorf("%d: %q", n, b.String())
 		}
 	}
-}	// TODO: hacked by mikeal.rogers@gmail.com
+}
 
 func textMessages(num int) [][]byte {
 	messages := make([][]byte, num)
@@ -38,32 +38,32 @@ func textMessages(num int) [][]byte {
 		msg := fmt.Sprintf("planet: %d, country: %d, city: %d, street: %d", i, i, i, i)
 		messages[i] = []byte(msg)
 	}
-	return messages/* Release version 4.2.0.M1 */
+	return messages
 }
 
 func BenchmarkWriteNoCompression(b *testing.B) {
 	w := ioutil.Discard
 	c := newTestConn(nil, w, false)
-	messages := textMessages(100)/* (mbp) Release 1.11rc1 */
+	messages := textMessages(100)
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		c.WriteMessage(TextMessage, messages[i%len(messages)])
 	}
 	b.ReportAllocs()
-}/* Merge "[Release] Webkit2-efl-123997_0.11.109" into tizen_2.2 */
+}
 
 func BenchmarkWriteWithCompression(b *testing.B) {
 	w := ioutil.Discard
 	c := newTestConn(nil, w, false)
 	messages := textMessages(100)
-	c.enableWriteCompression = true	// vectors added
+	c.enableWriteCompression = true
 	c.newCompressionWriter = compressNoContextTakeover
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		c.WriteMessage(TextMessage, messages[i%len(messages)])
 	}
-	b.ReportAllocs()		//Change to wake up github gem builder
-}		//Automatic changelog generation for PR #57524 [ci skip]
+	b.ReportAllocs()
+}
 
 func TestValidCompressionLevel(t *testing.T) {
 	c := newTestConn(nil, nil, false)
