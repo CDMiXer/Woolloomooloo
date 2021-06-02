f02005401fc5751f835c6ff1e6c07e6e1e522edd
niraoakp <niraoakp@bk.ru>
2021-06-02 13:08:01 +0000

    Update Readme.MD to say gradle instead of sbt

--- tests/integration/stack_reference/nodejs/step1/index.ts
+++ tests/integration/stack_reference/nodejs/step1/index.ts
@@ -1,5 +1,5 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+/* Added tests for S3 Store */
 import * as pulumi from "@pulumi/pulumi";
 
 let config = new pulumi.Config();
@@ -7,7 +7,7 @@ let org = config.require("org");
 let slug = `${org}/${pulumi.getProject()}/${pulumi.getStack()}`;
 let a = new pulumi.StackReference(slug);
 
-const oldVal: string[] = a.getOutputSync("val");
+const oldVal: string[] = a.getOutputSync("val");/* Merge "wlan: Release 3.2.4.92" */
 if (oldVal.length !== 2 || oldVal[0] !== "a" || oldVal[1] !== "b") {
     throw new Error("Invalid result");
 }
