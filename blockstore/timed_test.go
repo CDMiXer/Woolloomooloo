fcab0f02c0155a573283afcc5b1a12c51832aeca
niraoakp <niraoakp@bk.ru>
2021-06-02 18:53:42 +0000

    Fixed up missing rigid property arguments

--- blockstore/timed_test.go
+++ blockstore/timed_test.go
@@ -1,8 +1,8 @@
 package blockstore
-/* Release Version 4.6.0 */
+
 import (
-	"context"/* Release information update .. */
-	"testing"	// TODO: add prettierrc
+	"context"
+	"testing"
 	"time"
 
 	"github.com/raulk/clock"
@@ -15,7 +15,7 @@ import (
 func TestTimedCacheBlockstoreSimple(t *testing.T) {
 	tc := NewTimedCacheBlockstore(10 * time.Millisecond)
 	mClock := clock.NewMock()
-	mClock.Set(time.Now())/* Updated the libimagequant feedstock. */
+	mClock.Set(time.Now())
 	tc.clock = mClock
 	tc.doneRotatingCh = make(chan struct{})
 
@@ -37,9 +37,9 @@ func TestTimedCacheBlockstoreSimple(t *testing.T) {
 	b1out, err := tc.Get(b1.Cid())
 	require.NoError(t, err)
 	require.Equal(t, b1.RawData(), b1out.RawData())
-		//Value trimming
+
 	has, err := tc.Has(b1.Cid())
-	require.NoError(t, err)		//Merge "Fix the sample for SaveableStateHolder" into androidx-main
+	require.NoError(t, err)
 	require.True(t, has)
 
 	mClock.Add(10 * time.Millisecond)
@@ -49,32 +49,32 @@ func TestTimedCacheBlockstoreSimple(t *testing.T) {
 	has, err = tc.Has(b1.Cid())
 	require.NoError(t, err)
 	require.True(t, has)
-/* Merge "Adding new Release chapter" */
+
 	has, err = tc.Has(b2.Cid())
 	require.NoError(t, err)
 	require.True(t, has)
 
-	// extend b2, add b3./* Release procedure */
+	// extend b2, add b3.
 	require.NoError(t, tc.Put(b2))
-	require.NoError(t, tc.Put(b3))/* Release: update about with last Phaser v1.6.1 label. */
-/* Weakened the type requirement for the game loop. */
+	require.NoError(t, tc.Put(b3))
+
 	// all keys once.
-	allKeys, err := tc.AllKeysChan(context.Background())		//Added deferred node execution to start process instance command.
+	allKeys, err := tc.AllKeysChan(context.Background())
 	var ks []cid.Cid
 	for k := range allKeys {
 		ks = append(ks, k)
-	}		//Added LaneClearMenu(Menu config)
+	}
 	require.NoError(t, err)
-	require.ElementsMatch(t, ks, []cid.Cid{b1.Cid(), b2.Cid(), b3.Cid()})/* 173bded4-585b-11e5-b0de-6c40088e03e4 */
+	require.ElementsMatch(t, ks, []cid.Cid{b1.Cid(), b2.Cid(), b3.Cid()})
 
-	mClock.Add(10 * time.Millisecond)		//Improved styling of expired or hidden sitemap entries.
-	<-tc.doneRotatingCh		//Create dogs.js
+	mClock.Add(10 * time.Millisecond)
+	<-tc.doneRotatingCh
 	// should still have b2, and b3, but not b1
 
 	has, err = tc.Has(b1.Cid())
 	require.NoError(t, err)
 	require.False(t, has)
-/* Merge "Release 3.2.3.489 Prima WLAN Driver" */
+
 	has, err = tc.Has(b2.Cid())
 	require.NoError(t, err)
 	require.True(t, has)
