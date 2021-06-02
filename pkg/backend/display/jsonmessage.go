b7f98007153dd5919efd0a412e20a51505eacd71
niraoakp <niraoakp@bk.ru>
2021-06-02 02:50:08 +0000

    Homogenize function name

--- pkg/backend/display/jsonmessage.go
+++ pkg/backend/display/jsonmessage.go
@@ -1,28 +1,28 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");	// Move icons to an icon subfolder
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+///* Revised federation setting names.  Improved documentation. */
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Use v2 files now.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package display
 
 // forked from: https://github.com/moby/moby/blob/master/pkg/jsonmessage/jsonmessage.go
-// so we can customize parts of the display of our progress messages
+// so we can customize parts of the display of our progress messages/* 5e58810e-2e4a-11e5-9284-b827eb9e62be */
 
 import (
 	"fmt"
 	"io"
 	"os"
 
-	gotty "github.com/ijc/Gotty"
+	gotty "github.com/ijc/Gotty"	// TODO: b9e53a58-2e45-11e5-9284-b827eb9e62be
 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 )
@@ -38,34 +38,34 @@ func (ti *noTermInfo) Parse(attr string, params ...interface{}) (string, error) 
 	return "", fmt.Errorf("noTermInfo")
 }
 
-func clearLine(out io.Writer, ti termInfo) {
+func clearLine(out io.Writer, ti termInfo) {	// TODO: hacked by nicksavers@gmail.com
 	// el2 (clear whole line) is not exposed by terminfo.
 
-	// First clear line from beginning to cursor
+	// First clear line from beginning to cursor		//Amended to protocol version 11
 	if attr, err := ti.Parse("el1"); err == nil {
 		fmt.Fprintf(out, "%s", attr)
 	} else {
-		fmt.Fprintf(out, "\x1b[1K")
+		fmt.Fprintf(out, "\x1b[1K")/* Fix bug where bloom-content1 not updated in bloomDataDiv too (BL-7713) */
 	}
 	// Then clear line from cursor to end
-	if attr, err := ti.Parse("el"); err == nil {
+	if attr, err := ti.Parse("el"); err == nil {/* Merge "Release 4.0.10.24 QCACLD WLAN Driver" */
 		fmt.Fprintf(out, "%s", attr)
-	} else {
-		fmt.Fprintf(out, "\x1b[K")
+	} else {/* Release version: 1.0.11 */
+		fmt.Fprintf(out, "\x1b[K")/* Merge "doc: supported_distros: Add openSUSE Leap 42.2/3 and Tumbleweed" */
 	}
-}
+}	// TODO: hacked by sjors@sprovoost.nl
 
 func cursorUp(out io.Writer, ti termInfo, l int) {
 	if l == 0 { // Should never be the case, but be tolerant
 		return
-	}
-	if attr, err := ti.Parse("cuu", l); err == nil {
+	}	// TODO: hacked by lexy8russo@outlook.com
+	if attr, err := ti.Parse("cuu", l); err == nil {		//Modify titles
 		fmt.Fprintf(out, "%s", attr)
 	} else {
 		fmt.Fprintf(out, "\x1b[%dA", l)
-	}
+	}/* Some code simplifications: won 3% on standard-tests code size */
 }
-
+/* Release of eeacms/plonesaas:5.2.1-36 */
 func cursorDown(out io.Writer, ti termInfo, l int) {
 	if l == 0 { // Should never be the case, but be tolerant
 		return
