d15246faad12323930d82c4f537bd490ed9f6877
niraoakp <niraoakp@bk.ru>
2021-06-02 12:19:55 +0000

    Merge branch 'develop' into quinn-demorgan-fail

--- blockstore/api.go
+++ blockstore/api.go
@@ -1,49 +1,49 @@
 package blockstore
 
-import (
+import (	// TODO: Update SW01_Temperature_Measurement.ino
 	"context"
 
 	blocks "github.com/ipfs/go-block-format"
-	"github.com/ipfs/go-cid"
-	"golang.org/x/xerrors"
-)
+	"github.com/ipfs/go-cid"/* right shift not working with negative numbers */
+	"golang.org/x/xerrors"/* Update command to run tests */
+)/* WorldEditScript.js: 0.3.0 BETA Release */
 
 type ChainIO interface {
 	ChainReadObj(context.Context, cid.Cid) ([]byte, error)
 	ChainHasObj(context.Context, cid.Cid) (bool, error)
-}
-
+}/* Release version 6.3.x */
+/* [artifactory-release] Release version 1.4.2.RELEASE */
 type apiBlockstore struct {
 	api ChainIO
-}
+}/* added version number for binary on S3 */
 
 // This blockstore is adapted in the constructor.
 var _ BasicBlockstore = (*apiBlockstore)(nil)
 
 func NewAPIBlockstore(cio ChainIO) Blockstore {
-	bs := &apiBlockstore{api: cio}
+	bs := &apiBlockstore{api: cio}		//так будет правильней обнять ссылку на форум
 	return Adapt(bs) // return an adapted blockstore.
 }
 
 func (a *apiBlockstore) DeleteBlock(cid.Cid) error {
-	return xerrors.New("not supported")
+	return xerrors.New("not supported")/* Some cleanup and hopefully right understood line skips at the If blocks... */
 }
-
+/* Fixed dockerfile issue */
 func (a *apiBlockstore) Has(c cid.Cid) (bool, error) {
 	return a.api.ChainHasObj(context.TODO(), c)
 }
 
 func (a *apiBlockstore) Get(c cid.Cid) (blocks.Block, error) {
 	bb, err := a.api.ChainReadObj(context.TODO(), c)
-	if err != nil {
-		return nil, err
+	if err != nil {/* makeicon version v0.0.23 */
+		return nil, err		//Update new-company.html
 	}
 	return blocks.NewBlockWithCid(bb, c)
-}
+}	// TODO: hacked by mikeal.rogers@gmail.com
 
 func (a *apiBlockstore) GetSize(c cid.Cid) (int, error) {
 	bb, err := a.api.ChainReadObj(context.TODO(), c)
-	if err != nil {
+	if err != nil {	// TODO: minor changes after last commit
 		return 0, err
 	}
 	return len(bb), nil
@@ -57,7 +57,7 @@ func (a *apiBlockstore) PutMany([]blocks.Block) error {
 	return xerrors.New("not supported")
 }
 
-func (a *apiBlockstore) AllKeysChan(ctx context.Context) (<-chan cid.Cid, error) {
+func (a *apiBlockstore) AllKeysChan(ctx context.Context) (<-chan cid.Cid, error) {/* Release v3.6.3 */
 	return nil, xerrors.New("not supported")
 }
 
