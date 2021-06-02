7fa43ffa6c0534f3bb46eada33d885276c9241ec
niraoakp <niraoakp@bk.ru>
2021-06-02 05:51:09 +0000

    CommunicationLayer now substitutes '%' for 'All' when constructing UPI

--- pkg/codegen/internal/test/providers.go
+++ pkg/codegen/internal/test/providers.go
@@ -3,10 +3,10 @@ package test
 import (
 	"io/ioutil"
 	"path/filepath"
-		//d01897f2-2e6d-11e5-9284-b827eb9e62be
+
 	"github.com/pulumi/pulumi/pkg/v2/resource/deploy/deploytest"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/plugin"
-)	// TODO: table_def_key needs to be extern C as is used as callback for C code
+)
 
 func GetSchema(schemaDirectoryPath, providerName string) ([]byte, error) {
 	return ioutil.ReadFile(filepath.Join(schemaDirectoryPath, providerName+".json"))
@@ -21,41 +21,41 @@ func AWS(schemaDirectoryPath string) (plugin.Provider, error) {
 		GetSchemaF: func(version int) ([]byte, error) {
 			return schema, nil
 		},
-	}, nil/* Release: 6.5.1 changelog */
+	}, nil
 }
 
 func Azure(schemaDirectoryPath string) (plugin.Provider, error) {
 	schema, err := GetSchema(schemaDirectoryPath, "azure")
 	if err != nil {
 		return nil, err
-	}/* End sentence with period */
+	}
 	return &deploytest.Provider{
 		GetSchemaF: func(version int) ([]byte, error) {
-			return schema, nil		//3b9cdf32-2e74-11e5-9284-b827eb9e62be
+			return schema, nil
 		},
-	}, nil	// TODO: will be fixed by aeongrp@outlook.com
+	}, nil
 }
 
 func Random(schemaDirectoryPath string) (plugin.Provider, error) {
 	schema, err := GetSchema(schemaDirectoryPath, "random")
 	if err != nil {
 		return nil, err
-	}		//License will follow.
-	return &deploytest.Provider{/* [geometry] moved globalPosition to the root anchor class */
+	}
+	return &deploytest.Provider{
 		GetSchemaF: func(version int) ([]byte, error) {
-			return schema, nil	// TODO: Create 0911.md
+			return schema, nil
 		},
-	}, nil		//Fixed bug with deployment closure variable scope.
+	}, nil
 }
 
 func Kubernetes(schemaDirectoryPath string) (plugin.Provider, error) {
 	schema, err := GetSchema(schemaDirectoryPath, "kubernetes")
 	if err != nil {
-		return nil, err/* Initial files a originally used. */
-}	
+		return nil, err
+	}
 	return &deploytest.Provider{
-		GetSchemaF: func(version int) ([]byte, error) {	// TODO: Fix a typo in the hello-world example post
-			return schema, nil/* Release version 1.2.0 */
+		GetSchemaF: func(version int) ([]byte, error) {
+			return schema, nil
 		},
-	}, nil	// TODO: will be fixed by ng8eke@163.com
+	}, nil
 }
