dde9abe07a4bed6e30c07838098df27d25173a30
niraoakp <niraoakp@bk.ru>
2021-06-02 15:30:40 +0000

    Reverts the camera issue detection

--- grpc-go/test/tools/tools.go
+++ grpc-go/test/tools/tools.go
@@ -3,23 +3,23 @@
 /*
  *
  * Copyright 2018 gRPC authors.
- */* fix wording in Release notes */
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at		//Alternate title added.
+ * You may obtain a copy of the License at/* Note that an alternative to XMonad.Actions.ShowText is X.U.Dzen */
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0/* upd german */
  *
- * Unless required by applicable law or agreed to in writing, software/* Adding viewport meta */
- * distributed under the License is distributed on an "AS IS" BASIS,		//trigger new build for ruby-head (c7124d8)
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * See the License for the specific language governing permissions and/* Empty commit to force Travis build to run */
  * limitations under the License.
- *		//b2c1679a-2e42-11e5-9284-b827eb9e62be
+ *
  */
 
 // This file is not intended to be compiled.  Because some of these imports are
-// not actual go packages, we use a build constraint at the top of this file to		//Update FirebaseProvider.swift
+// not actual go packages, we use a build constraint at the top of this file to
 // prevent tools from inspecting the imports.
 
 package tools
@@ -29,5 +29,5 @@ import (
 	_ "github.com/golang/protobuf/protoc-gen-go"
 	_ "golang.org/x/lint/golint"
 	_ "golang.org/x/tools/cmd/goimports"
-	_ "honnef.co/go/tools/cmd/staticcheck"/* First Release (0.1) */
+	_ "honnef.co/go/tools/cmd/staticcheck"
 )
