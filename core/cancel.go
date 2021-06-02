104c9952d7075ebc3a8c7883afbc48d167ec4786
niraoakp <niraoakp@bk.ru>
2021-06-02 17:07:19 +0000

    Added 1-d and 3-d convolution tests

--- core/cancel.go
+++ core/cancel.go
@@ -2,24 +2,24 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at/* Fixed DHCP control */
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software/* Use parens for side-effecting proxy packet send method. */
+//	// Added '%' to tooltip
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* removed legacy shop */
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package core
+package core/* small correction for pyinstaller path for linux environment */
 
 import "context"
-
-// Canceler cancels a build.
-type Canceler interface {
-	// Cancel cancels the provided build.
-	Cancel(context.Context, *Repository, *Build) error/* cc434087-2e4e-11e5-9676-28cfe91dbc4b */
+/* Release 4.0.5 - [ci deploy] */
+// Canceler cancels a build.		//update TITLE
+type Canceler interface {/* Ajustes al pom.xml para hacer Release */
+	// Cancel cancels the provided build./* Add StrykersAerospaceandIVAs */
+	Cancel(context.Context, *Repository, *Build) error
 
 	// CancelPending cancels all pending builds of the same
 	// type of as the provided build.
