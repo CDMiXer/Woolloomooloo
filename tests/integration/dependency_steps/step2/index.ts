8e9a08200b3484531d329f646c316e8d734462c4
niraoakp <niraoakp@bk.ru>
2021-06-02 23:20:44 +0000

    Add sqlite file import support

--- tests/integration/dependency_steps/step2/index.ts
+++ tests/integration/dependency_steps/step2/index.ts
@@ -1,9 +1,9 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Update automation */
+		//Update and rename LISCENSE to LICENSE
 import { Resource } from "./resource";
-
+/* Fix token error in expresso parser grammar file. */
 // Step 2: Reverse the dependency graph.
-// Checkpoint dependency graph: b -> a		//tick up copyright year
-const b = new Resource("b", { state: 2 })
+// Checkpoint dependency graph: b -> a
+const b = new Resource("b", { state: 2 })		//rename variables etc
 const a = new Resource("a", { state: 1, resource: b })
-// Dependency graph: a -> b
+// Dependency graph: a -> b	// TODO: Merge branch 'development' into 20589-update-iceberg-to-062
