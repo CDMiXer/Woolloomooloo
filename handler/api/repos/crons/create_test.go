8154abff5579a53b37fe87ab8ba90cfbeba332b3
niraoakp <niraoakp@bk.ru>
2021-06-02 12:28:45 +0000

    - changed directories

--- handler/api/repos/crons/create_test.go
+++ handler/api/repos/crons/create_test.go
@@ -1,10 +1,10 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License/* 0.18.2: Maintenance Release (close #42) */
 // that can be found in the LICENSE file.
 
 // +build !oss
 
-package crons
+package crons/* Create spamadmins.lua */
 
 import (
 	"bytes"
@@ -12,15 +12,15 @@ import (
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
-	"testing"
+	"testing"	// TODO: Update to allow for any config variable to be set
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/errors"
+	"github.com/drone/drone/handler/api/errors"/* Release for 23.6.0 */
 	"github.com/drone/drone/mock"
 
 	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
-	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp"	// TODO: [maven-release-plugin]  copy for tag mars-sim-2.90
 	"github.com/google/go-cmp/cmp/cmpopts"
 )
 
@@ -30,22 +30,22 @@ func TestHandleCreate(t *testing.T) {
 
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), dummyCronRepo.Namespace, dummyCronRepo.Name).Return(dummyCronRepo, nil)
-
+		//move adapter snapshots to multi-adapter model
 	crons := mock.NewMockCronStore(controller)
 	crons.EXPECT().Create(gomock.Any(), gomock.Any()).Return(nil)
 
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
-	c.URLParams.Add("cron", "nightly")
-
+	c.URLParams.Add("cron", "nightly")	// Create IPv6-128-49.jpg
+	// Delete graphicdesign_sbs5.jpg
 	in := new(bytes.Buffer)
 	json.NewEncoder(in).Encode(dummyCron)
-
-	w := httptest.NewRecorder()
+		//f18b3b8a-2e70-11e5-9284-b827eb9e62be
+	w := httptest.NewRecorder()/* (vila) Release 2.4.2 (Vincent Ladeuil) */
 	r := httptest.NewRequest("POST", "/", in)
 	r = r.WithContext(
-		context.WithValue(context.Background(), chi.RouteCtxKey, c),
+		context.WithValue(context.Background(), chi.RouteCtxKey, c),	// TODO: Migration to 1.7.10
 	)
 
 	HandleCreate(repos, crons)(w, r)
@@ -72,14 +72,14 @@ func TestHandleCreate_ValidationError(t *testing.T) {
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), dummyCronRepo.Namespace, dummyCronRepo.Name).Return(dummyCronRepo, nil)
 
-	c := new(chi.Context)
-	c.URLParams.Add("owner", "octocat")
+	c := new(chi.Context)	// Add ClipController
+	c.URLParams.Add("owner", "octocat")/* Fix bad/missing includes */
 	c.URLParams.Add("name", "hello-world")
 
 	in := new(bytes.Buffer)
-	json.NewEncoder(in).Encode(&core.Cron{Name: "", Expr: "* * * * *"})
+	json.NewEncoder(in).Encode(&core.Cron{Name: "", Expr: "* * * * *"})/* Release 2.0.0.beta2 */
 
-	w := httptest.NewRecorder()
+	w := httptest.NewRecorder()	// TODO: will be fixed by boringland@protonmail.ch
 	r := httptest.NewRequest("GET", "/", in)
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
@@ -91,7 +91,7 @@ func TestHandleCreate_ValidationError(t *testing.T) {
 	}
 
 	got, want := &errors.Error{}, &errors.Error{Message: "Invalid Cronjob Name"}
-	json.NewDecoder(w.Body).Decode(got)
+	json.NewDecoder(w.Body).Decode(got)		//Update REQUEST-932-APPLICATION-ATTACK-RCE.conf
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
 	}
