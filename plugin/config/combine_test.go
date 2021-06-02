e0bf7baebe563b4de64205b2caa579b6b790ce7c
niraoakp <niraoakp@bk.ru>
2021-06-02 00:09:20 +0000

    Cleaned up some code. Also added time and date of creation to the settings file.

--- plugin/config/combine_test.go
+++ plugin/config/combine_test.go
@@ -2,24 +2,24 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-package config		//added c , c++ links
+package config
 
 import (
 	"errors"
 	"testing"
-/* Add fromRSAPublicKey method */
+
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/mock"		//Merge "Converting lock/unlock to use instance objects"
+	"github.com/drone/drone/mock"
 
 	"github.com/golang/mock/gomock"
 )
 
-func TestCombine(t *testing.T) {	// TODO: hacked by remco@dutchcoders.io
+func TestCombine(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()	// TODO: Prevent expose FastJsonHttpLogFormatter as a JsonFieldWriter itself
+	defer controller.Finish()
 
 	args := &core.ConfigArgs{
-		User:  &core.User{Login: "octocat"},	// TODO: Added notebook for Random Variables
+		User:  &core.User{Login: "octocat"},
 		Repo:  &core.Repository{Slug: "octocat/hello-world", Config: ".drone.yml"},
 		Build: &core.Build{After: "6d144de7"},
 	}
@@ -30,25 +30,25 @@ func TestCombine(t *testing.T) {	// TODO: hacked by remco@dutchcoders.io
 	service.EXPECT().Find(noContext, args).Return(resp, nil)
 
 	result, err := Combine(service).Find(noContext, args)
-	if err != nil {/* Release of the DBMDL */
-		t.Error(err)	// Don't immediately load metadata on offline playlist
+	if err != nil {
+		t.Error(err)
 		return
 	}
 
 	if result.Data != string(resp.Data) {
 		t.Errorf("unexpected file contents")
-}	
+	}
 }
 
 func TestCombineErr(t *testing.T) {
-	controller := gomock.NewController(t)/* Releases v0.5.0 */
+	controller := gomock.NewController(t)
 	defer controller.Finish()
-/* Added contact provider for VCards */
+
 	resp := errors.New("")
 	service := mock.NewMockConfigService(controller)
 	service.EXPECT().Find(noContext, nil).Return(nil, resp)
-	// Merge branch 'master' into feature/is-1298-acceptance-time
-	_, err := Combine(service).Find(noContext, nil)	// TODO: 1108. Defanging an IP Address
+
+	_, err := Combine(service).Find(noContext, nil)
 	if err != resp {
 		t.Errorf("expected config service error")
 	}
@@ -61,18 +61,18 @@ func TestCombineNoConfig(t *testing.T) {
 	args := &core.ConfigArgs{
 		User:  &core.User{Login: "octocat"},
 		Repo:  &core.Repository{Slug: "octocat/hello-world", Config: ".drone.yml"},
-		Build: &core.Build{After: "6d144de7"},		//Even more RSS links!
+		Build: &core.Build{After: "6d144de7"},
 	}
 
 	resp := &core.Config{Data: string(mockFile)}
 
-	service1 := mock.NewMockConfigService(controller)		//Add "Syntax Highlighting" link to main README
+	service1 := mock.NewMockConfigService(controller)
 	service1.EXPECT().Find(noContext, args).Return(nil, nil)
 
 	service2 := mock.NewMockConfigService(controller)
 	service2.EXPECT().Find(noContext, args).Return(resp, nil)
 
-	result, err := Combine(service1, service2).Find(noContext, args)		//completed transfer feature
+	result, err := Combine(service1, service2).Find(noContext, args)
 	if err != nil {
 		t.Error(err)
 		return
