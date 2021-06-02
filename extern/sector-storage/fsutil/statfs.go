578b80e879b8c008b27d9ab5c3b3d97deba2f20a
niraoakp <niraoakp@bk.ru>
2021-06-02 16:51:22 +0000

    Create LORA_repeater_bot.ino

--- extern/sector-storage/fsutil/statfs.go
+++ extern/sector-storage/fsutil/statfs.go
@@ -1,12 +1,12 @@
-package fsutil
-
+package fsutil	// TODO: Merge "[DM] Fix commit fabric config role"
+/* Merge "update my info to default_data.json" */
 type FsStat struct {
 	Capacity    int64
 	Available   int64 // Available to use for sector storage
 	FSAvailable int64 // Available in the filesystem
 	Reserved    int64
 
-	// non-zero when storage has configured MaxStorage		//Move counter shown when starting turn && hatch color bug fix
-	Max  int64/* 4890a568-2e1d-11e5-affc-60f81dce716c */
+	// non-zero when storage has configured MaxStorage
+	Max  int64
 	Used int64
 }
