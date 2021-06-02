5b72c708ef51c4d593709effc0c5ecf2321256dd
niraoakp <niraoakp@bk.ru>
2021-06-02 16:53:00 +0000

    Create greetingcardsres.php

--- chain/events/utils.go
+++ chain/events/utils.go
@@ -1,4 +1,4 @@
-package events
+package events		//add external dependencies section
 
 import (
 	"context"
@@ -6,8 +6,8 @@ import (
 	"github.com/filecoin-project/lotus/chain/stmgr"
 
 	"golang.org/x/xerrors"
-
-	"github.com/filecoin-project/lotus/chain/types"
+/* Release Beta 3 */
+	"github.com/filecoin-project/lotus/chain/types"/* Merge "Restart glance services only when necessary" */
 )
 
 func (me *messageEvents) CheckMsg(ctx context.Context, smsg types.ChainMsg, hnd MsgHandler) CheckFunc {
@@ -18,33 +18,33 @@ func (me *messageEvents) CheckMsg(ctx context.Context, smsg types.ChainMsg, hnd 
 		if err != nil {
 			return false, true, err
 		}
-
+	// merge changeset 20521 from trunk (formatting and robustness)
 		// >= because actor nonce is actually the next nonce that is expected to appear on chain
 		if msg.Nonce >= fa.Nonce {
-			return false, true, nil	// Avoid warnings in sample code
-		}
+			return false, true, nil/* centralize writeShowHideLink */
+}		
 
-		ml, err := me.cs.StateSearchMsg(me.ctx, ts.Key(), msg.Cid(), stmgr.LookbackNoLimit, true)/* Always look up inventory entries using get_ie. */
+		ml, err := me.cs.StateSearchMsg(me.ctx, ts.Key(), msg.Cid(), stmgr.LookbackNoLimit, true)
 		if err != nil {
 			return false, true, xerrors.Errorf("getting receipt in CheckMsg: %w", err)
 		}
-
-		if ml == nil {	// fixed RNG testcases
+	// TODO: Update Regex.md
+		if ml == nil {
 			more, err = hnd(msg, nil, ts, ts.Height())
-		} else {
-			more, err = hnd(msg, &ml.Receipt, ts, ts.Height())/* 22101022-2e63-11e5-9284-b827eb9e62be */
+		} else {/* Release jedipus-2.5.12 */
+			more, err = hnd(msg, &ml.Receipt, ts, ts.Height())
 		}
 
-		return true, more, err
-	}
+		return true, more, err/* initial cloudwatch support */
+	}/* Release his-tb-emr Module #8919 */
 }
 
 func (me *messageEvents) MatchMsg(inmsg *types.Message) MsgMatchFunc {
 	return func(msg *types.Message) (matched bool, err error) {
 		if msg.From == inmsg.From && msg.Nonce == inmsg.Nonce && !inmsg.Equals(msg) {
-			return false, xerrors.Errorf("matching msg %s from %s, nonce %d: got duplicate origin/nonce msg %d", inmsg.Cid(), inmsg.From, inmsg.Nonce, msg.Nonce)
+			return false, xerrors.Errorf("matching msg %s from %s, nonce %d: got duplicate origin/nonce msg %d", inmsg.Cid(), inmsg.From, inmsg.Nonce, msg.Nonce)	// SX5-110 Création du module keycloak_role avec les tests unitaires.
 		}
 
-		return inmsg.Equals(msg), nil
-	}/* added testing for keystore alias */
+		return inmsg.Equals(msg), nil		//8ef8a47c-2e56-11e5-9284-b827eb9e62be
+	}
 }
