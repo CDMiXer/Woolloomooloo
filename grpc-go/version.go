1aa2ef6d52639b06a5ddf1e663ba4889b08f7ee5
niraoakp <niraoakp@bk.ru>
2021-06-02 05:33:12 +0000

    Rename my-aliases.plugin.zsh to my-aliases.zsh

--- grpc-go/version.go
+++ grpc-go/version.go
@@ -1,22 +1,22 @@
 /*
  *
- * Copyright 2018 gRPC authors.
+ * Copyright 2018 gRPC authors.	// TODO: hacked by seth@sethvargo.com
  *
- * Licensed under the Apache License, Version 2.0 (the "License");		//Added "Model Details" frame.
- * you may not use this file except in compliance with the License.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License./* - changed name of war-package to "lemo.war" */
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * distributed under the License is distributed on an "AS IS" BASIS,/* Release v5.4.2 */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release version 3.1.1.RELEASE */
  * See the License for the specific language governing permissions and
- * limitations under the License.	// TODO: will be fixed by nicksavers@gmail.com
+ * limitations under the License.
  *
- *//* better debug projects */
+ */
 
-package grpc
-	// jzip - add option to choose add current folder name or nto
+package grpc/* iichan.hk - spoilers in /a */
+
 // Version is the current grpc version.
 const Version = "1.41.0-dev"
