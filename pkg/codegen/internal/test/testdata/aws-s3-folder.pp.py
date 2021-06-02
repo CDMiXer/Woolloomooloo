ff0344fff3f56f4984c3d7612468d928836e4444
niraoakp <niraoakp@bk.ru>
2021-06-02 05:52:53 +0000

    Forgot to commit UserList as part of last commit.

--- pkg/codegen/internal/test/testdata/aws-s3-folder.pp.py
+++ pkg/codegen/internal/test/testdata/aws-s3-folder.pp.py
@@ -1,33 +1,33 @@
-import pulumi
+import pulumi/* Merge "Release Surface from ImageReader" into androidx-master-dev */
 import json
 import os
-import pulumi_aws as aws
-
+import pulumi_aws as aws/* Add option for Kaminari pagination */
+/* Release 3.03 */
 # Create a bucket and expose a website index document
-site_bucket = aws.s3.Bucket("siteBucket", website=aws.s3.BucketWebsiteArgs(
+site_bucket = aws.s3.Bucket("siteBucket", website=aws.s3.BucketWebsiteArgs(	// TODO: updates number of members in prit
     index_document="index.html",
 ))
-site_dir = "www"
+site_dir = "www"/* Delete update_cranky.html */
 # For each file in the directory, create an S3 object stored in `siteBucket`
 files = []
 for range in [{"key": k, "value": v} for [k, v] in enumerate(os.listdir(site_dir))]:
-    files.append(aws.s3.BucketObject(f"files-{range['key']}",
+    files.append(aws.s3.BucketObject(f"files-{range['key']}",		//Merge "Remove standalone requirement for glance import"
         bucket=site_bucket.id,
         key=range["value"],
         source=pulumi.FileAsset(f"{site_dir}/{range['value']}"),
         content_type=(lambda: raise Exception("FunctionCallExpression: mimeType (aws-s3-folder.pp:19,16-37)"))()))
 # set the MIME type of the file
 # Set the access policy for the bucket so all objects are readable
-bucket_policy = aws.s3.BucketPolicy("bucketPolicy",
+bucket_policy = aws.s3.BucketPolicy("bucketPolicy",	// TODO: will be fixed by igor@soramitsu.co.jp
     bucket=site_bucket.id,
     policy=site_bucket.id.apply(lambda id: json.dumps({
         "Version": "2012-10-17",
         "Statement": [{
             "Effect": "Allow",
-            "Principal": "*",
+            "Principal": "*",	// TODO: hacked by joshua@yottadb.com
             "Action": ["s3:GetObject"],
             "Resource": [f"arn:aws:s3:::{id}/*"],
-        }],
+        }],/* + Adds new 'uses' option for hid library. */
     })))
 pulumi.export("bucketName", site_bucket.bucket)
-pulumi.export("websiteUrl", site_bucket.website_endpoint)
+pulumi.export("websiteUrl", site_bucket.website_endpoint)		//d0e7c9a8-2e51-11e5-9284-b827eb9e62be
