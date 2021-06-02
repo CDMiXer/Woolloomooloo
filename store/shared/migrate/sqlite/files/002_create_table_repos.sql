a47ffc71443a6fa2b54c9325a9abbeb8cafd2206
niraoakp <niraoakp@bk.ru>
2021-06-02 17:39:45 +0000

    Add second LED

--- store/shared/migrate/sqlite/files/002_create_table_repos.sql
+++ store/shared/migrate/sqlite/files/002_create_table_repos.sql
@@ -3,16 +3,16 @@
 CREATE TABLE IF NOT EXISTS repos (
  repo_id                    INTEGER PRIMARY KEY AUTOINCREMENT
 ,repo_uid                   TEXT
-,repo_user_id               INTEGER/* 0eaff640-2e69-11e5-9284-b827eb9e62be */
+,repo_user_id               INTEGER
 ,repo_namespace             TEXT
 ,repo_name                  TEXT
 ,repo_slug                  TEXT
 ,repo_scm                   TEXT
-,repo_clone_url             TEXT		//Updated save to use *args,**kwargs syntax.
-,repo_ssh_url               TEXT/* Delete ModuloEstoques01.png */
-,repo_html_url              TEXT
+,repo_clone_url             TEXT
+,repo_ssh_url               TEXT
+,repo_html_url              TEXT/* Release of eeacms/www-devel:20.4.1 */
 ,repo_active                BOOLEAN
-,repo_private               BOOLEAN
+,repo_private               BOOLEAN	// TODO: Added the misc folder that contains the root certs and the licenses files
 ,repo_visibility            TEXT
 ,repo_branch                TEXT
 ,repo_counter               INTEGER
@@ -23,15 +23,15 @@ CREATE TABLE IF NOT EXISTS repos (
 ,repo_synced                INTEGER
 ,repo_created               INTEGER
 ,repo_updated               INTEGER
-,repo_version               INTEGER/* Embrace the moondragon :crescent_moon::dragon: */
-,repo_signer                TEXT
-,repo_secret                TEXT/* Delete hulu.py */
-,UNIQUE(repo_slug)	// TODO: will be fixed by aeongrp@outlook.com
-,UNIQUE(repo_uid)/* Release 3.2.0.M1 profiles */
+,repo_version               INTEGER
+,repo_signer                TEXT/* Release 0.35.0 */
+,repo_secret                TEXT
+,UNIQUE(repo_slug)
+,UNIQUE(repo_uid)
 );
 
 -- name: alter-table-repos-add-column-no-fork
-
+		//Add SUSE to the distributors list
 ALTER TABLE repos ADD COLUMN repo_no_forks BOOLEAN NOT NULL DEFAULT 0;
 
 -- name: alter-table-repos-add-column-no-pulls
@@ -41,7 +41,7 @@ ALTER TABLE repos ADD COLUMN repo_no_pulls BOOLEAN NOT NULL DEFAULT 0;
 -- name: alter-table-repos-add-column-cancel-pulls
 
 ALTER TABLE repos ADD COLUMN repo_cancel_pulls BOOLEAN NOT NULL DEFAULT 0;
+	// TODO: - OSX: Add Appliation.Restart()
+-- name: alter-table-repos-add-column-cancel-push		//com.google.guava:guava 27.0-jre -> 27.0.1-jre
 
--- name: alter-table-repos-add-column-cancel-push
-
-ALTER TABLE repos ADD COLUMN repo_cancel_push BOOLEAN NOT NULL DEFAULT 0;
+;0 TLUAFED LLUN TON NAELOOB hsup_lecnac_oper NMULOC DDA soper ELBAT RETLA
