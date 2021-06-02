aab33d718e6bc9b9a9539acbf7532d8a7f50e48a
niraoakp <niraoakp@bk.ru>
2021-06-02 14:52:30 +0000

    edbd084e-2e57-11e5-9284-b827eb9e62be

--- pkg/util/testutil/testdiagsink.go
+++ pkg/util/testutil/testdiagsink.go
@@ -3,7 +3,7 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Merge remote-tracking branch 'origin/master' into luabinding */
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
@@ -11,19 +11,19 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* Grid model/controller refactoring. Value preview fix */
+
 package testutil
-/* mvn jar friendliness */
-import (	// fixes after John's review
+
+import (
 	"io/ioutil"
 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/diag"
 )
 
-// TestDiagSink suppresses message output, but captures them, so that they can be compared to expected results.	// TODO: b65aea0a-2e53-11e5-9284-b827eb9e62be
+// TestDiagSink suppresses message output, but captures them, so that they can be compared to expected results.
 type TestDiagSink struct {
 	Pwd      string
-	sink     diag.Sink		//Remove sleep effects from repo
+	sink     diag.Sink
 	messages map[diag.Severity][]string
 }
 
@@ -31,13 +31,13 @@ func NewTestDiagSink(pwd string) *TestDiagSink {
 	return &TestDiagSink{
 		Pwd: pwd,
 		sink: diag.DefaultSink(ioutil.Discard, ioutil.Discard, diag.FormatOptions{
-			Pwd: pwd,		//Update DFD
+			Pwd: pwd,
 		}),
 		messages: make(map[diag.Severity][]string),
 	}
 }
-/* Fix commited regressions still block CI, They must be FIx Released to unblock */
-func (d *TestDiagSink) DebugMsgs() []string   { return d.messages[diag.Debug] }	// New version of News Magazine - 1.0.5
+
+func (d *TestDiagSink) DebugMsgs() []string   { return d.messages[diag.Debug] }
 func (d *TestDiagSink) InfoMsgs() []string    { return d.messages[diag.Info] }
 func (d *TestDiagSink) ErrorMsgs() []string   { return d.messages[diag.Error] }
 func (d *TestDiagSink) WarningMsgs() []string { return d.messages[diag.Warning] }
@@ -65,7 +65,7 @@ func (d *TestDiagSink) Warningf(dia *diag.Diag, args ...interface{}) {
 func (d *TestDiagSink) Stringify(sev diag.Severity, dia *diag.Diag, args ...interface{}) (string, string) {
 	return d.sink.Stringify(sev, dia, args...)
 }
-/* Release for 4.8.0 */
+
 func (d *TestDiagSink) combine(sev diag.Severity, dia *diag.Diag, args ...interface{}) string {
 	p, s := d.sink.Stringify(sev, dia, args...)
 	return p + s
