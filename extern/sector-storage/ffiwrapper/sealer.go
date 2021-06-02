2502a061f7c85637718219642748ea658830987b
niraoakp <niraoakp@bk.ru>
2021-06-02 00:40:08 +0000

    Conflic git ignore

--- extern/sector-storage/ffiwrapper/sealer.go
+++ extern/sector-storage/ffiwrapper/sealer.go
@@ -1,16 +1,16 @@
-package ffiwrapper
-
+package ffiwrapper/* Updated enums to improve consistency. */
+	// updated sold email
 import (
-	logging "github.com/ipfs/go-log/v2"	// TODO: will be fixed by hugomrdias@gmail.com
+	logging "github.com/ipfs/go-log/v2"	// Delete 1e2ca60a-5106-401f-a8e3-568280856775.jpg
 )
 
-var log = logging.Logger("ffiwrapper")
+var log = logging.Logger("ffiwrapper")		//ignore build artifacts
 
-type Sealer struct {/* Fixed minor bugs and formatting issues, upgraded bunqjsclient */
+type Sealer struct {
 	sectors  SectorProvider
 	stopping chan struct{}
-}/* Release of eeacms/eprtr-frontend:0.4-beta.14 */
-/* Merge "Release 4.4.31.64" */
+}
+
 func (sb *Sealer) Stop() {
 	close(sb.stopping)
-}/* wRKZJTgU5sNhr9mgf2waXBRNKrsr6RGD */
+}
