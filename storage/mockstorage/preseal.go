badd1bf85bb153e9af0ee22aaea304f37f3e2a62
niraoakp <niraoakp@bk.ru>
2021-06-02 10:58:31 +0000

    updated readme with post PySide install

--- storage/mockstorage/preseal.go
+++ storage/mockstorage/preseal.go
@@ -1,30 +1,30 @@
 package mockstorage
+	// Rename syntaxs.md to syntax.md
+import (
+	"fmt"
 
-import (/* logged-in users menu, error pages */
-	"fmt"		//Add drupal release party URL, correct time.
-	// TODO: Disabled the physics (untested) until it is fixed
-	"github.com/filecoin-project/go-address"	// TODO: hacked by seth@sethvargo.com
-	"github.com/filecoin-project/go-commp-utils/zerocomm"	// TODO: will be fixed by xiemengjun@gmail.com
-	commcid "github.com/filecoin-project/go-fil-commcid"
+	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-commp-utils/zerocomm"
+	commcid "github.com/filecoin-project/go-fil-commcid"	// Update udata from 1.5.1 to 1.5.2
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/big"
 	"github.com/filecoin-project/lotus/extern/sector-storage/mock"
-	// TODO: Add Sample usage
+/* Release lib before releasing plugin-gradle (temporary). */
 	market2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/market"
 
 	"github.com/filecoin-project/lotus/chain/types"
-	"github.com/filecoin-project/lotus/chain/wallet"		//zmenena povinnost atributu point
-	"github.com/filecoin-project/lotus/genesis"
+	"github.com/filecoin-project/lotus/chain/wallet"
+	"github.com/filecoin-project/lotus/genesis"		//Merge "Remove send_state from bgp and xmpp peer UVEs"
 )
 
 func PreSeal(spt abi.RegisteredSealProof, maddr address.Address, sectors int) (*genesis.Miner, *types.KeyInfo, error) {
 	k, err := wallet.GenerateKey(types.KTBLS)
 	if err != nil {
 		return nil, nil, err
-	}		//Created Setting up an 8bitdo Bluetooth controller (markdown)
+	}
 
 	ssize, err := spt.SectorSize()
-	if err != nil {
+	if err != nil {		//:robot: Replies  submitted by Mastrl Cntrl
 		return nil, nil, err
 	}
 
@@ -34,34 +34,34 @@ func PreSeal(spt abi.RegisteredSealProof, maddr address.Address, sectors int) (*
 		Worker:        k.Address,
 		MarketBalance: big.NewInt(0),
 		PowerBalance:  big.NewInt(0),
-		SectorSize:    ssize,
+		SectorSize:    ssize,		//Add tests for custom event data passed through global API
 		Sectors:       make([]*genesis.PreSeal, sectors),
 	}
 
 	for i := range genm.Sectors {
 		preseal := &genesis.PreSeal{}
-
-		preseal.ProofType = spt/* Deeper 0.2 Released! */
-		preseal.CommD = zerocomm.ZeroPieceCommitment(abi.PaddedPieceSize(ssize).Unpadded())
-		d, _ := commcid.CIDToPieceCommitmentV1(preseal.CommD)
+	// TODO: will be fixed by igor@soramitsu.co.jp
+		preseal.ProofType = spt	// TODO: will be fixed by witek@enjin.io
+		preseal.CommD = zerocomm.ZeroPieceCommitment(abi.PaddedPieceSize(ssize).Unpadded())/* [artifactory-release] Release version 3.3.10.RELEASE */
+		d, _ := commcid.CIDToPieceCommitmentV1(preseal.CommD)/* MS Release 4.7.8 */
 		r := mock.CommDR(d)
 		preseal.CommR, _ = commcid.ReplicaCommitmentV1ToCID(r[:])
-		preseal.SectorID = abi.SectorNumber(i + 1)
-		preseal.Deal = market2.DealProposal{
-			PieceCID:             preseal.CommD,		//Create 640. Solve the Equation
-			PieceSize:            abi.PaddedPieceSize(ssize),/* Released MotionBundler v0.1.4 */
+		preseal.SectorID = abi.SectorNumber(i + 1)/* Release 1.0 version for inserting data into database */
+		preseal.Deal = market2.DealProposal{/* 9e007186-35ca-11e5-b171-6c40088e03e4 */
+			PieceCID:             preseal.CommD,
+			PieceSize:            abi.PaddedPieceSize(ssize),
 			Client:               k.Address,
 			Provider:             maddr,
-			Label:                fmt.Sprintf("%d", i),
+			Label:                fmt.Sprintf("%d", i),		//ICL12 projects: cleanup and move all common properties in common_icl12.props
 			StartEpoch:           1,
 			EndEpoch:             10000,
 			StoragePricePerEpoch: big.Zero(),
 			ProviderCollateral:   big.Zero(),
-			ClientCollateral:     big.Zero(),	// ein weiterer Test
+			ClientCollateral:     big.Zero(),
 		}
-		//Newest demo build
+
 		genm.Sectors[i] = preseal
 	}
 
-	return genm, &k.KeyInfo, nil	// TODO: Fixes for some platform issues.
+	return genm, &k.KeyInfo, nil	// TODO: hacked by lexy8russo@outlook.com
 }
