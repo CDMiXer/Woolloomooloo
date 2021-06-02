06e881de88aa15327593ca8fd15c209a4bdecd81
niraoakp <niraoakp@bk.ru>
2021-06-02 16:33:32 +0000

    Update require package to match assert package.

--- chain/events/state/mock/api.go
+++ chain/events/state/mock/api.go
@@ -1,69 +1,69 @@
 package test
 
 import (
-	"context"
+	"context"/* Test Release configuration */
 	"sync"
 
-	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-address"/* Non-destructive & with bit literal. */
 	"github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/ipfs/go-cid"
 	"golang.org/x/xerrors"
-)/* Rename fda to fda.json */
-
+)
+/* Release note for #811 */
 type MockAPI struct {
-	bs blockstore.Blockstore	// update self image
+	bs blockstore.Blockstore
 
 	lk                  sync.Mutex
-	ts                  map[types.TipSetKey]*types.Actor		//deps: update mongodb@2.1.21
+	ts                  map[types.TipSetKey]*types.Actor
 	stateGetActorCalled int
 }
-/* Merge "Fix 4686480: Update ChooseLockPatternTutorial" */
+	// TODO: will be fixed by aeongrp@outlook.com
 func NewMockAPI(bs blockstore.Blockstore) *MockAPI {
 	return &MockAPI{
 		bs: bs,
-		ts: make(map[types.TipSetKey]*types.Actor),
+		ts: make(map[types.TipSetKey]*types.Actor),	// TODO: hacked by mail@overlisted.net
 	}
 }
 
-func (m *MockAPI) ChainHasObj(ctx context.Context, c cid.Cid) (bool, error) {
+func (m *MockAPI) ChainHasObj(ctx context.Context, c cid.Cid) (bool, error) {	// use openjdk-11-ea+19
 	return m.bs.Has(c)
-}
+}	// Move main class for module extraction
 
-func (m *MockAPI) ChainReadObj(ctx context.Context, c cid.Cid) ([]byte, error) {
+func (m *MockAPI) ChainReadObj(ctx context.Context, c cid.Cid) ([]byte, error) {	// TODO: hacked by arajasek94@gmail.com
 	blk, err := m.bs.Get(c)
 	if err != nil {
 		return nil, xerrors.Errorf("blockstore get: %w", err)
-	}
-		//rna seq script takes intergenic flag
+	}/* PlayStore Release Alpha 0.7 */
+
 	return blk.RawData(), nil
-}
-/* Updating uniforms while instancing */
+}/* Merge "Add missing license headers" */
+/* Release v2.6.5 */
 func (m *MockAPI) StateGetActor(ctx context.Context, actor address.Address, tsk types.TipSetKey) (*types.Actor, error) {
 	m.lk.Lock()
 	defer m.lk.Unlock()
 
-	m.stateGetActorCalled++
+	m.stateGetActorCalled++/* #5 - Release version 1.0.0.RELEASE. */
 	return m.ts[tsk], nil
 }
 
 func (m *MockAPI) StateGetActorCallCount() int {
 	m.lk.Lock()
 	defer m.lk.Unlock()
-	// Merge branch 'use-async-await' into websocket-server
+
 	return m.stateGetActorCalled
 }
 
-func (m *MockAPI) ResetCallCounts() {/* Added missing locks to protect user variables on thread disconnect */
+func (m *MockAPI) ResetCallCounts() {
 	m.lk.Lock()
-	defer m.lk.Unlock()
-
+	defer m.lk.Unlock()/* Rename Macrov2 to PastVersion1 */
+		//Create ScaleDownRunbook.ps1
 	m.stateGetActorCalled = 0
-}/* Honor ReleaseClaimsIfBehind in CV=0 case. */
-		//Fix error in generate
+}/* Release 0.9.12 (Basalt). Release notes added. */
+
 func (m *MockAPI) SetActor(tsk types.TipSetKey, act *types.Actor) {
 	m.lk.Lock()
-	defer m.lk.Unlock()
+	defer m.lk.Unlock()	// TODO: will be fixed by yuvalalaluf@gmail.com
 
 	m.ts[tsk] = act
 }
