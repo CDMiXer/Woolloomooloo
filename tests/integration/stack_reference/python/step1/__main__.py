54429382d668a2b1b9b98931504ffc6d097870c2
niraoakp <niraoakp@bk.ru>
2021-06-02 03:43:29 +0000

    Merge "Revert "Revert "Update tests for BoringSSL roll."""

--- tests/integration/stack_reference/python/step1/__main__.py
+++ tests/integration/stack_reference/python/step1/__main__.py
@@ -1,8 +1,8 @@
 # Copyright 2020, Pulumi Corporation.  All rights reserved.
-/* Release of eeacms/bise-frontend:1.29.3 */
-import pulumi/* Update test-runner.html */
 
-config = pulumi.Config()	// TODO: Merge "Recover from bad input event timestamps from the kernel." into jb-mr1-dev
+import pulumi
+
+config = pulumi.Config()
 org = config.require('org')
 slug = f"{org}/{pulumi.get_project()}/{pulumi.get_stack()}"
 a = pulumi.StackReference(slug)
@@ -11,5 +11,5 @@ oldVal = a.get_output('val')
 
 if len(oldVal) != 2 or oldVal[0] != 'a' or oldVal[1] != 'b':
     raise Exception('Invalid result')
-/* rev 604220 */
+
 pulumi.export('val2', pulumi.Output.secret(['a', 'b']))
