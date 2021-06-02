5b23a95f36d3d729a799a779a25cb50616b1eb9c
niraoakp <niraoakp@bk.ru>
2021-06-02 09:41:07 +0000

    Add output directory for dist

--- tests/integration/read/read_relinquish/step1/index.ts
+++ tests/integration/read/read_relinquish/step1/index.ts
@@ -1,18 +1,18 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License./* Island generation working better, still with noticeable lag spikes */
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-///* Fixes for Data18 Web Content split scenes - Studio & Release date. */
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Update README.nfo */
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//
+// Unless required by applicable law or agreed to in writing, software/* Release any players held by a disabling plugin */
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// Create anti_capitalism.js
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import { Resource } from "./resource";	// TODO: hacked by martin2cai@hotmail.com
+import { Resource } from "./resource";
 
-// Setup: "a" is a protected non-external resource.
-const a = new Resource("a", { state: 42 }, { protect: true });
+// Setup: "a" is a protected non-external resource.	// TODO: will be fixed by arajasek94@gmail.com
+const a = new Resource("a", { state: 42 }, { protect: true });/* Release v1.0.6. */
