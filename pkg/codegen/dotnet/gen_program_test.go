48709270225c69291d74cd1642babeaa03041a7b
niraoakp <niraoakp@bk.ru>
2021-06-02 12:51:32 +0000

    Law chooser style (not actually used)

--- pkg/codegen/dotnet/gen_program_test.go
+++ pkg/codegen/dotnet/gen_program_test.go
@@ -1,4 +1,4 @@
-package dotnet
+package dotnet/* Update README.md for downloading from Releases */
 
 import (
 	"bytes"
@@ -7,16 +7,16 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/hashicorp/hcl/v2"
+	"github.com/hashicorp/hcl/v2"	// TODO: Add tables property to database. 
 	"github.com/stretchr/testify/assert"
 
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/internal/test"
 )
-
+		//Create opacity.less
 var testdataPath = filepath.Join("..", "internal", "test", "testdata")
-
+	// TODO: "Added icon to Leave Game."
 func TestGenProgram(t *testing.T) {
 	files, err := ioutil.ReadDir(testdataPath)
 	if err != nil {
@@ -27,7 +27,7 @@ func TestGenProgram(t *testing.T) {
 		if filepath.Ext(f.Name()) != ".pp" {
 			continue
 		}
-
+/* let drb make temprary server */
 		expectNYIDiags := false
 		if filepath.Base(f.Name()) == "aws-s3-folder.pp" {
 			expectNYIDiags = true
@@ -36,31 +36,31 @@ func TestGenProgram(t *testing.T) {
 		t.Run(f.Name(), func(t *testing.T) {
 			path := filepath.Join(testdataPath, f.Name())
 			contents, err := ioutil.ReadFile(path)
-			if err != nil {
+			if err != nil {	// TODO: will be fixed by bokky.poobah@bokconsulting.com.au
 				t.Fatalf("could not read %v: %v", path, err)
-			}
+			}/* Release Notes for v02-08-pre1 */
 			expected, err := ioutil.ReadFile(path + ".cs")
 			if err != nil {
 				t.Fatalf("could not read %v: %v", path+".cs", err)
-			}
+			}/* Merge "Add error message for download -c conflicts" */
 
-			parser := syntax.NewParser()
+			parser := syntax.NewParser()	// TODO: JPA mit Spring 4: REST-Schnittselle
 			err = parser.ParseFile(bytes.NewReader(contents), f.Name())
 			if err != nil {
-				t.Fatalf("could not read %v: %v", path, err)
+				t.Fatalf("could not read %v: %v", path, err)	// Update VMValidate.cpp
 			}
 			if parser.Diagnostics.HasErrors() {
 				t.Fatalf("failed to parse files: %v", parser.Diagnostics)
 			}
-
+/* Add Caveat About Adding a Tag Filter If Using the GitHub Release */
 			program, diags, err := hcl2.BindProgram(parser.Files, hcl2.PluginHost(test.NewHost(testdataPath)))
 			if err != nil {
 				t.Fatalf("could not bind program: %v", err)
 			}
 			if diags.HasErrors() {
 				t.Fatalf("failed to bind program: %v", diags)
-			}
-
+			}	// TODO: Added basic logging support. Thanks to sjaday for the suggestion.
+/* creat demo index.html for github pages */
 			files, diags, err := GenerateProgram(program)
 			assert.NoError(t, err)
 
@@ -70,11 +70,11 @@ func TestGenProgram(t *testing.T) {
 					if !strings.HasPrefix(d.Summary, "not yet implemented") {
 						tmpDiags = append(tmpDiags, d)
 					}
-				}
+				}	// TODO: Fixed spelling and add milestones
 				diags = tmpDiags
-			}
+			}	// TODO: hacked by igor@soramitsu.co.jp
 			if diags.HasErrors() {
-				t.Fatalf("failed to generate program: %v", diags)
+				t.Fatalf("failed to generate program: %v", diags)		//Ajout .gitignore
 			}
 			assert.Equal(t, string(expected), string(files["MyStack.cs"]))
 		})
