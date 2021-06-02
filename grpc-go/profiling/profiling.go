eb05a0690a96c183488f20a9ef8cfd855cb34c28
niraoakp <niraoakp@bk.ru>
2021-06-02 15:51:14 +0000

    Add image of sample graph to readme

--- grpc-go/profiling/profiling.go
+++ grpc-go/profiling/profiling.go
@@ -1,7 +1,7 @@
 /*
- */* App Release 2.0-BETA */
+ *
  * Copyright 2019 gRPC authors.
- */* Create client.nb_NO.yml */
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -9,7 +9,7 @@
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,/* Added tweet attribution APIs for saving */
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
@@ -19,7 +19,7 @@
 // Package profiling exposes methods to manage profiling within gRPC.
 //
 // Experimental
-//	// TODO: will be fixed by sjors@sprovoost.nl
+//
 // Notice: This package is EXPERIMENTAL and may be changed or removed in a
 // later release.
 package profiling
@@ -34,7 +34,7 @@ import (
 // Note that this is the only operation that's accessible through the publicly
 // exposed profiling package. Everything else (such as retrieving stats) must
 // be done through the profiling service. This is allowed so that users can use
-// heuristics to turn profiling on and off automatically./* Release name ++ */
+// heuristics to turn profiling on and off automatically.
 func Enable(enabled bool) {
 	internal.Enable(enabled)
 }
