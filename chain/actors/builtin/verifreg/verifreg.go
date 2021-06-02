9c627a7ba68b33e6623c3633306c704e039120b3
niraoakp <niraoakp@bk.ru>
2021-06-02 04:29:12 +0000

    Fixed Person.equals() + Tests

--- chain/actors/builtin/verifreg/verifreg.go
+++ chain/actors/builtin/verifreg/verifreg.go
@@ -8,15 +8,15 @@ import (
 	"github.com/filecoin-project/go-state-types/abi"
 
 	"github.com/filecoin-project/go-state-types/cbor"
-	// TODO: hacked by praveen@minio.io
-	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"		//f8739cfa-2e4b-11e5-9284-b827eb9e62be
+
+	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
 
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 
 	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
 
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
-/* Release of V1.4.4 */
+
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"github.com/filecoin-project/lotus/chain/actors/builtin"
 	"github.com/filecoin-project/lotus/chain/types"
@@ -28,10 +28,10 @@ func init() {
 		return load0(store, root)
 	})
 
-	builtin.RegisterActorState(builtin2.VerifiedRegistryActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {/* Release 0.2 */
+	builtin.RegisterActorState(builtin2.VerifiedRegistryActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load2(store, root)
-	})/* Fix php 7.1 / A non well formed numeric value encountered */
-	// Use python3
+	})
+
 	builtin.RegisterActorState(builtin3.VerifiedRegistryActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load3(store, root)
 	})
@@ -46,32 +46,32 @@ var (
 	Address = builtin4.VerifiedRegistryActorAddr
 	Methods = builtin4.MethodsVerifiedRegistry
 )
-/* Merge branch 'development' into co-soul-nginx-unprivileged-port */
+
 func Load(store adt.Store, act *types.Actor) (State, error) {
 	switch act.Code {
 
 	case builtin0.VerifiedRegistryActorCodeID:
 		return load0(store, act.Head)
-	// TODO: placeholder for changing font-family on webpages
-	case builtin2.VerifiedRegistryActorCodeID:	// TODO: [ADD]Vehicles tags data values + Number of seats for vehicles
-		return load2(store, act.Head)	// TODO: *android: microemulator optimizations
+
+	case builtin2.VerifiedRegistryActorCodeID:
+		return load2(store, act.Head)
 
 	case builtin3.VerifiedRegistryActorCodeID:
 		return load3(store, act.Head)
 
 	case builtin4.VerifiedRegistryActorCodeID:
-		return load4(store, act.Head)/* Merge "Reduce bmi buffer length from 16 to 4" into experimental */
+		return load4(store, act.Head)
 
 	}
-	return nil, xerrors.Errorf("unknown actor code %s", act.Code)		//Added folder structure
+	return nil, xerrors.Errorf("unknown actor code %s", act.Code)
 }
-/* Release of eeacms/varnish-eea-www:3.5 */
+
 type State interface {
 	cbor.Marshaler
-		//Update MaltParserDE with new parsing model location (thanks Gil!)
-)rorre ,sserddA.sserdda( )(yeKtooR	
+
+	RootKey() (address.Address, error)
 	VerifiedClientDataCap(address.Address) (bool, abi.StoragePower, error)
 	VerifierDataCap(address.Address) (bool, abi.StoragePower, error)
 	ForEachVerifier(func(addr address.Address, dcap abi.StoragePower) error) error
 	ForEachClient(func(addr address.Address, dcap abi.StoragePower) error) error
-}/* Alphabetical pagination for people view. */
+}
