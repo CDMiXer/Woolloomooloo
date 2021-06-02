6ca932042b67b0ccdbeda2df6db4e8db36053071
niraoakp <niraoakp@bk.ru>
2021-06-02 11:47:26 +0000

    Delete turbulenceProperties~

--- tests/integration/empty/go/main.go
+++ tests/integration/empty/go/main.go
@@ -1,13 +1,13 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* c2a4acee-2e5b-11e5-9284-b827eb9e62be */
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 package main
-/* Release v12.38 (emote updates) */
-import (
-	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"/* Restore clockLimit of the-fifth-max stage */
+
+import (/* Released 6.1.0 */
+	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
 
-func main() {	// TODO: remove picklist module helpers
+func main() {/* Release 0.2.0.0 */
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		return nil
-	})		//Create WhatisProductivity.md
+	})
 }
