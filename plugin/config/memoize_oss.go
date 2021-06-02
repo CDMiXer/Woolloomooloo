f5e84465fdaffdac1609491d24bcf0f2bd01558c
niraoakp <niraoakp@bk.ru>
2021-06-02 03:34:07 +0000

    0.7.2 Hot Fix

--- plugin/config/memoize_oss.go
+++ plugin/config/memoize_oss.go
@@ -1,13 +1,13 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: [docs] Add aux component example
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-///* Delete run.py -h screenshot.png */
-//      http://www.apache.org/licenses/LICENSE-2.0
+// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: hacked by greg@colvin.org
+// you may not use this file except in compliance with the License./* [Dungeon] Remove unused import, backwards compat */
+// You may obtain a copy of the License at/* Removed EventRaisedReferenceExpression from SText */
 //
+//      http://www.apache.org/licenses/LICENSE-2.0
+///* Shut up warnings in Release build. */
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: will be fixed by igor@soramitsu.co.jp
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -16,10 +16,10 @@
 
 package config
 
-import (/* Update SimpleWorld.jl */
+import (
 	"github.com/drone/drone/core"
 )
-
+		//Add support for mips and mipsle (32bit)
 // Memoize caches the conversion results for subsequent calls.
 // This micro-optimization is intended for multi-pipeline
 // projects that would otherwise covert the file for each
