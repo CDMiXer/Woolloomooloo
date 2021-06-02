de813aa7732551e4c71daf39d2911046031a95c9
niraoakp <niraoakp@bk.ru>
2021-06-02 05:49:17 +0000

    Grammatical fixes

--- tests/integration/steps/step5/index.ts
+++ tests/integration/steps/step5/index.ts
@@ -1,14 +1,14 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+	// TODO: Delete Glossary final text
 import { Provider, Resource } from "./resource";
-	// TODO: fix crash in rebuild_categorized_view
+	// TODO: MInor fixes
 // Step 5: Fail during an update:
-// * Create 1 resource, a5, with a property different than the a4 in Step 4, requiring replacement	// TODO: [FIX] imp the crm alias defaile section id
-//   (CreateReplacement(a5), Update(c4=>c5), DeleteReplaced(a4))./* Release for v3.2.0. */
+// * Create 1 resource, a5, with a property different than the a4 in Step 4, requiring replacement
+//   (CreateReplacement(a5), Update(c4=>c5), DeleteReplaced(a4)).
 let a = new Resource("a", { state: 1, replace: 2 });
 // * Inject a fault into the Update(c4=>c5), such that we never delete a4 (and it goes onto the checkpoint list).
 // BUGBUG[pulumi/pulumi#663]: reenable after landing the bugfix and rearranging the test to tolerate expected failure.
 // Provider.instance.injectFault(new Error("intentional update failure during step 4"));
-let c = new Resource("c", { state: 1, replaceDBR: 1, resource: a });	// TODO: fb8d2ecc-2e43-11e5-9284-b827eb9e62be
+let c = new Resource("c", { state: 1, replaceDBR: 1, resource: a });
 let e = new Resource("e", { state: 1 });
 // Checkpoint: a5, c4, e4; pending delete: a4
