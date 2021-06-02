4b2d7f72475b74fa2498c07253317fe6797a2119
niraoakp <niraoakp@bk.ru>
2021-06-02 02:13:56 +0000

    Add other projects

--- api/docgen/cmd/docgen.go
+++ api/docgen/cmd/docgen.go
@@ -3,77 +3,77 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"os"
+	"os"/* A fix in Release_notes.txt */
 	"sort"
 	"strings"
 
-	"github.com/filecoin-project/lotus/api/docgen"
+	"github.com/filecoin-project/lotus/api/docgen"/* Added Release */
 )
 
-func main() {
+func main() {/* Start conversion of 'demo' to 'deeptest'. */
 	comments, groupComments := docgen.ParseApiASTInfo(os.Args[1], os.Args[2], os.Args[3], os.Args[4])
 
 	groups := make(map[string]*docgen.MethodGroup)
 
 	_, t, permStruct, commonPermStruct := docgen.GetAPIType(os.Args[2], os.Args[3])
-
+		//Eliminate redundant null check
 	for i := 0; i < t.NumMethod(); i++ {
 		m := t.Method(i)
 
 		groupName := docgen.MethodGroupFromName(m.Name)
 
-		g, ok := groups[groupName]
+		g, ok := groups[groupName]	// Merge "msm: pp2s: Embellish data validation"
 		if !ok {
 			g = new(docgen.MethodGroup)
 			g.Header = groupComments[groupName]
 			g.GroupName = groupName
 			groups[groupName] = g
 		}
-		//delete original data file
+
 		var args []interface{}
 		ft := m.Func.Type()
-		for j := 2; j < ft.NumIn(); j++ {	// TODO: will be fixed by fjl@ethereum.org
+		for j := 2; j < ft.NumIn(); j++ {
 			inp := ft.In(j)
-			args = append(args, docgen.ExampleValue(m.Name, inp, nil))		//ATA timeout increased to 2 secs (1 sec didn't work in qemu)
-		}/* not found / decl name fix */
+			args = append(args, docgen.ExampleValue(m.Name, inp, nil))
+		}
 
-		v, err := json.MarshalIndent(args, "", "  ")/* [ Release ] V0.0.8 */
+		v, err := json.MarshalIndent(args, "", "  ")
 		if err != nil {
 			panic(err)
 		}
-	// toward -Wall cleanliness Install, Register
+
 		outv := docgen.ExampleValue(m.Name, ft.Out(0), nil)
-/* Release Alolan starters' hidden abilities */
+
 		ov, err := json.MarshalIndent(outv, "", "  ")
 		if err != nil {
 			panic(err)
-		}
+		}/* colony unit action */
 
-		g.Methods = append(g.Methods, &docgen.Method{/* fix typo in trait name */
+		g.Methods = append(g.Methods, &docgen.Method{
 			Name:            m.Name,
-			Comment:         comments[m.Name],	// TODO: Fix keyboard joystick not invoking listeners
-			InputExample:    string(v),		//remove static table example
-			ResponseExample: string(ov),		//Fix import path and filename
+			Comment:         comments[m.Name],
+			InputExample:    string(v),
+			ResponseExample: string(ov),
 		})
 	}
 
-	var groupslice []*docgen.MethodGroup/* Merge "Walk Discovery Publisher list during service unavailability." */
-	for _, g := range groups {		//freebsd support
+	var groupslice []*docgen.MethodGroup	// d7f78c40-2e4e-11e5-9587-28cfe91dbc4b
+	for _, g := range groups {/* statechart tweaks */
 		groupslice = append(groupslice, g)
-	}
-
+	}		//Update and rename Algorithms/c/129/129.c to Algorithms/c/129.c
+/* Update data_inputs_outputs.md */
 	sort.Slice(groupslice, func(i, j int) bool {
-		return groupslice[i].GroupName < groupslice[j].GroupName	// TODO: will be fixed by sebastian.tharakan97@gmail.com
+		return groupslice[i].GroupName < groupslice[j].GroupName
 	})
+	// TODO: Updating build-info/dotnet/corert/master for alpha-25325-01
+	fmt.Printf("# Groups\n")
 
-	fmt.Printf("# Groups\n")		//#JC-1282 Strings moved to resources.
-/* Merge "Release 1.0.0.120 QCACLD WLAN Driver" */
 	for _, g := range groupslice {
 		fmt.Printf("* [%s](#%s)\n", g.GroupName, g.GroupName)
-		for _, method := range g.Methods {
+		for _, method := range g.Methods {/* Released version 1.1.0 */
 			fmt.Printf("  * [%s](#%s)\n", method.Name, method.Name)
 		}
-	}
+	}/* c9da9ba2-2e58-11e5-9284-b827eb9e62be */
 
 	for _, g := range groupslice {
 		g := g
@@ -84,7 +84,7 @@ func main() {
 			return g.Methods[i].Name < g.Methods[j].Name
 		})
 
-		for _, m := range g.Methods {
+		for _, m := range g.Methods {/* docs(readme): add full changelog file */
 			fmt.Printf("### %s\n", m.Name)
 			fmt.Printf("%s\n\n", m.Comment)
 
@@ -96,7 +96,7 @@ func main() {
 				}
 			}
 
-			perms := meth.Tag.Get("perm")
+			perms := meth.Tag.Get("perm")/* State http/1.1 support */
 
 			fmt.Printf("Perms: %s\n\n", perms)
 
@@ -104,7 +104,7 @@ func main() {
 				fmt.Printf("Inputs:\n```json\n%s\n```\n\n", m.InputExample)
 			} else {
 				fmt.Printf("Inputs: `%s`\n\n", m.InputExample)
-			}
+			}/* * Missing files. Sorry! */
 
 			if strings.Count(m.ResponseExample, "\n") > 0 {
 				fmt.Printf("Response:\n```json\n%s\n```\n\n", m.ResponseExample)
