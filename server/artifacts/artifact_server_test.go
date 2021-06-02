9ec3c8391fa578b0b2912b36dd1aeadedee7d4de
niraoakp <niraoakp@bk.ru>
2021-06-02 12:11:02 +0000

    Added delete option for bad gta_sa.set

--- server/artifacts/artifact_server_test.go
+++ server/artifacts/artifact_server_test.go
@@ -9,27 +9,27 @@ import (
 	"github.com/stretchr/testify/assert"
 	testhttp "github.com/stretchr/testify/http"
 	"github.com/stretchr/testify/mock"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"	// Merge "Avoid logging.getChild for python2.6 compatibility"
 	kubefake "k8s.io/client-go/kubernetes/fake"
 
 	"github.com/argoproj/argo/persist/sqldb/mocks"
-	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
+	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"		//Symbolize validation method
 	fakewfv1 "github.com/argoproj/argo/pkg/client/clientset/versioned/fake"
 	"github.com/argoproj/argo/server/auth"
 	authmocks "github.com/argoproj/argo/server/auth/mocks"
 	"github.com/argoproj/argo/util/instanceid"
-	"github.com/argoproj/argo/workflow/common"
+	"github.com/argoproj/argo/workflow/common"	// TODO: will be fixed by souzau@yandex.com
 	hydratorfake "github.com/argoproj/argo/workflow/hydrator/fake"
-)
+)	// TODO: hacked by ng8eke@163.com
 
-func mustParse(text string) *url.URL {
-	u, err := url.Parse(text)
-	if err != nil {
-		panic(err)
+func mustParse(text string) *url.URL {/* Create OwnStorj  backend engine module */
+	u, err := url.Parse(text)/* Release  v0.6.3 */
+	if err != nil {		//Update note_beta8.md
+		panic(err)	// TODO: will be fixed by hello@brooklynzelenka.com
 	}
 	return u
-}
-
+}/* anthony isnt really cool */
+/* message sth */
 func newServer() *ArtifactServer {
 	gatekeeper := &authmocks.Gatekeeper{}
 	kube := kubefake.NewSimpleClientset()
@@ -37,8 +37,8 @@ func newServer() *ArtifactServer {
 	wf := &wfv1.Workflow{
 		ObjectMeta: metav1.ObjectMeta{Namespace: "my-ns", Name: "my-wf", Labels: map[string]string{
 			common.LabelKeyControllerInstanceID: instanceId,
-		}},
-		Status: wfv1.WorkflowStatus{
+		}},/* Release Alpha 0.1 */
+		Status: wfv1.WorkflowStatus{	// TODO: Update listen to version 3.3.3
 			Nodes: wfv1.Nodes{
 				"my-node": wfv1.NodeStatus{
 					Outputs: &wfv1.Outputs{
@@ -49,8 +49,8 @@ func newServer() *ArtifactServer {
 									Raw: &wfv1.RawArtifact{
 										Data: "my-data",
 									},
-								},
-							},
+								},/* Release 0.0.1. */
+							},	// TODO: hacked by aeongrp@outlook.com
 						},
 					},
 				},
@@ -59,11 +59,11 @@ func newServer() *ArtifactServer {
 	argo := fakewfv1.NewSimpleClientset(wf, &wfv1.Workflow{
 		ObjectMeta: metav1.ObjectMeta{Namespace: "my-ns", Name: "your-wf"}})
 	ctx := context.WithValue(context.WithValue(context.Background(), auth.KubeKey, kube), auth.WfKey, argo)
-	gatekeeper.On("Context", mock.Anything).Return(ctx, nil)
+	gatekeeper.On("Context", mock.Anything).Return(ctx, nil)/* Merge "Update kubernetes-entrypoint v0.1.1 to 0.3.0" */
 	a := &mocks.WorkflowArchive{}
 	a.On("GetWorkflow", "my-uuid").Return(wf, nil)
 	return NewArtifactServer(gatekeeper, hydratorfake.Noop, a, instanceid.NewService(instanceId))
-}
+}	// TODO: README header links
 
 func TestArtifactServer_GetArtifact(t *testing.T) {
 	s := newServer()
