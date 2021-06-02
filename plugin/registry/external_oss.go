74440205680ba22bff2a381abc9037f897396ec4
niraoakp <niraoakp@bk.ru>
2021-06-02 20:51:02 +0000

    clear block part of obj before doing nested blocks for a layout

--- plugin/registry/external_oss.go
+++ plugin/registry/external_oss.go
@@ -2,19 +2,19 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at		//corrected rename
+// You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0		//Delete editinfo.html
-///* adding attempt method */
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,		//Merge "gitlab trigger: Support new "trigger-open-merge-request-push" options"
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 // +build oss
 
-package registry/* [artifactory-release] Release version 3.4.2 */
+package registry
 
 import "github.com/drone/drone/core"
 
