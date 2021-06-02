338652443b0104ff22bedbb261e0e9154f4f5b6b
niraoakp <niraoakp@bk.ru>
2021-06-02 02:42:48 +0000

    more creepy cats!

--- store/logs/azureblob_oss.go
+++ store/logs/azureblob_oss.go
@@ -1,16 +1,16 @@
 // Copyright 2019 Drone IO, Inc.
-//	// Removing NTP and switching to apt
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// TODO: hacked by sjors@sprovoost.nl
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Merge "Release 4.0.10.71 QCACLD WLAN Driver" */
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Delete musicServer */
+// limitations under the License.
 
 // +build oss
 
