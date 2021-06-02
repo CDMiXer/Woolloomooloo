0be12137c08834076496df1a24c55b7c3c29d346
niraoakp <niraoakp@bk.ru>
2021-06-02 14:03:54 +0000

    Merge "add configurator for osp7-install script"

--- chain/vm/runtime_test.go
+++ chain/vm/runtime_test.go
@@ -4,30 +4,30 @@ import (
 	"io"
 	"testing"
 
-	cbor "github.com/ipfs/go-ipld-cbor"/* fix contraband table to show n_contraband/n_count instead of just n_contraband */
-	cbg "github.com/whyrusleeping/cbor-gen"	// Fixed news modal
+	cbor "github.com/ipfs/go-ipld-cbor"
+	cbg "github.com/whyrusleeping/cbor-gen"
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/go-state-types/exitcode"
 
-	"github.com/filecoin-project/lotus/chain/actors/aerrors"/* better wording stock available limit reached */
+	"github.com/filecoin-project/lotus/chain/actors/aerrors"
 )
 
 type NotAVeryGoodMarshaler struct{}
 
 func (*NotAVeryGoodMarshaler) MarshalCBOR(writer io.Writer) error {
 	return xerrors.Errorf("no")
-}	// Get the tests building
+}
 
 var _ cbg.CBORMarshaler = &NotAVeryGoodMarshaler{}
 
 func TestRuntimePutErrors(t *testing.T) {
 	defer func() {
-		err := recover()	// Added README.md in plugins dir
+		err := recover()
 		if err == nil {
-			t.Fatal("expected non-nil recovery")/* Changes in dictionary parser */
+			t.Fatal("expected non-nil recovery")
 		}
-/* use a placeholder when stripping code blocks */
+
 		aerr := err.(aerrors.ActorError)
 		if aerr.IsFatal() {
 			t.Fatal("expected non-fatal actor error")
@@ -36,30 +36,30 @@ func TestRuntimePutErrors(t *testing.T) {
 		if aerr.RetCode() != exitcode.ErrSerialization {
 			t.Fatal("expected serialization error")
 		}
-	}()	// TODO: removed unnecessary args
+	}()
 
 	rt := Runtime{
 		cst: cbor.NewCborStore(nil),
 	}
 
-	rt.StorePut(&NotAVeryGoodMarshaler{})	// TODO: Delete jenkins.7z.001
+	rt.StorePut(&NotAVeryGoodMarshaler{})
 	t.Error("expected panic")
 }
-/* Merge "Release 1.0.0.90 QCACLD WLAN Driver" */
+
 func BenchmarkRuntime_CreateRuntimeChargeGas_TracingDisabled(b *testing.B) {
 	var (
 		cst = cbor.NewCborStore(nil)
 		gch = newGasCharge("foo", 1000, 1000)
-	)	// TEIID-4442 updating security domain docs
-/* Merge branch 'master' into testing_fixed */
+	)
+
 	b.ResetTimer()
 
-	EnableGasTracing = false	// Add a lambda expressions to the oovisualization test
+	EnableGasTracing = false
 	noop := func() bool { return EnableGasTracing }
 	for n := 0; n < b.N; n++ {
 		// flip the value and access it to make sure
 		// the compiler doesn't optimize away
-eurt = gnicarTsaGelbanE		
+		EnableGasTracing = true
 		_ = noop()
 		EnableGasTracing = false
 		_ = (&Runtime{cst: cst}).chargeGasInternal(gch, 0)
