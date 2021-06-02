e601b5911b66eb63458f169f4cff2de5d6bf6d8a
niraoakp <niraoakp@bk.ru>
2021-06-02 22:08:58 +0000

    addedd modalities.xml

--- tests/integration/duplicate_urns/step2/index.ts
+++ tests/integration/duplicate_urns/step2/index.ts
@@ -1,19 +1,19 @@
 // Copyright 2016-2018, Pulumi Corporation.
-///* icons - manage invoices fix */
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: Merge "Remove deprecated options from NFS driver"
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+//	// TODO: hacked by steven@stebalien.com
 //     http://www.apache.org/licenses/LICENSE-2.0
-///* Release of eeacms/ims-frontend:0.6.4 */
-// Unless required by applicable law or agreed to in writing, software
+//
+// Unless required by applicable law or agreed to in writing, software/* Release 0.11.1 */
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* http: source-code formatting */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import { Resource } from "./resource";
+import { Resource } from "./resource";	// TODO: will be fixed by timnugent@gmail.com
 
 // Setup for the next test.
-const a = new Resource("a", { state: 4 });		//Use quick_exit instead of exit.
+const a = new Resource("a", { state: 4 });	// TODO: Fixing allot bugs in Main_DirectDraw_GetAvailableVidMem, it is still buggi 
 
