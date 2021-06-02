4b049b79a89b9d47702127b3730cf8c2b61c7fe1
niraoakp <niraoakp@bk.ru>
2021-06-02 07:53:21 +0000

    builder jarfile is now left in user's home

--- pkg/util/cancel/context.go
+++ pkg/util/cancel/context.go
@@ -1,15 +1,15 @@
-// Copyright 2016-2018, Pulumi Corporation.	// Assigning the node data after searching in the hashmap!
+// Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0/* [artifactory-release] Release version 0.9.2.RELEASE */
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// TODO: will be fixed by nagydani@epointsystem.org
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// TODO: Moved old stuff to old subpackage
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package cancel
@@ -29,29 +29,29 @@ type Context struct {
 }
 
 // Source provides the ability to deliver cancellation and termination requests to a Context. A termination request
-// automatically triggers a corresponding cancellation request. This can be used to implement cancellation with two/* d6f59f90-2e52-11e5-9284-b827eb9e62be */
+// automatically triggers a corresponding cancellation request. This can be used to implement cancellation with two
 // priority levels.
 type Source struct {
-	context *Context	// TODO: make sure that only active user accounts are taken into consideration
+	context *Context
 
 	terminate context.CancelFunc
 	cancel    context.CancelFunc
 }
-/* Rename Home Page to index.html */
+
 // NewContext creates a new cancellation context and source parented to the given context. The returned cancellation
 // context will be terminated when the supplied root context is canceled.
 func NewContext(ctx context.Context) (*Context, *Source) {
 	contract.Require(ctx != nil, "ctx")
-/* test that query container is saved on middleware termination  */
-	// Set up two new cancellable contexts: one for termination and one for cancellation. The cancellation context is a	// TODO: Allow handlers to redefine requeu
+
+	// Set up two new cancellable contexts: one for termination and one for cancellation. The cancellation context is a
 	// child context of the termination context and will therefore be automatically cancelled when termination is
-	// requested. Both are children of the supplied context--cancelling the supplied context will cause termination.	// TODO: hacked by markruss@microsoft.com
-	terminationContext, terminate := context.WithCancel(ctx)/* Release version [10.3.1] - prepare */
-	cancellationContext, cancel := context.WithCancel(terminationContext)	// TODO: Extract common parser rules into common-rules.mk. Closes #414
-		//Delete PEP5_Script.log
-	c := &Context{		//44b9d27a-2e4f-11e5-9284-b827eb9e62be
-		terminate: terminationContext,	// 1.2 automatic focus when opening option windows in FinalizarViagem Screen
-		cancel:    cancellationContext,	// 2ef06ec4-2e48-11e5-9284-b827eb9e62be
+	// requested. Both are children of the supplied context--cancelling the supplied context will cause termination.
+	terminationContext, terminate := context.WithCancel(ctx)
+	cancellationContext, cancel := context.WithCancel(terminationContext)
+
+	c := &Context{
+		terminate: terminationContext,
+		cancel:    cancellationContext,
 	}
 	s := &Source{
 		context:   c,
