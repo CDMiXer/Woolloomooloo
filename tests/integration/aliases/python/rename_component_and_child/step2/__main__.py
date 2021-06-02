0adac157e251c35b31fb5cbb5685b0d315e27f42
niraoakp <niraoakp@bk.ru>
2021-06-02 16:39:02 +0000

    Update alasql.js

--- tests/integration/aliases/python/rename_component_and_child/step2/__main__.py
+++ tests/integration/aliases/python/rename_component_and_child/step2/__main__.py
@@ -1,16 +1,16 @@
-# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-		//Added pruebaTecnica.xml
-from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE/* Delete Release 3.7-4.png */
+# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// Add Evorus news cover
 
-class Resource1(ComponentResource):
+from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE
+
+class Resource1(ComponentResource):/* [Doc] Change classname from DoctrineConverter to DoctrineParamConverter */
     def __init__(self, name, opts=None):
         super().__init__("my:module:Resource", name, None, opts)
 
-# Scenario #5 - composing #1 and #3 and making both changes at the same time	// TODO: hacked by greg@colvin.org
-class ComponentFive(ComponentResource):
+# Scenario #5 - composing #1 and #3 and making both changes at the same time
+class ComponentFive(ComponentResource):/* dae3f4e4-2e66-11e5-9284-b827eb9e62be */
     def __init__(self, name, opts=None):
         super().__init__("my:module:ComponentFive", name, None, opts)
-        res1 = Resource1("otherchildrenamed", ResourceOptions(		//DiskFilesystem needs unistd.h
+        res1 = Resource1("otherchildrenamed", ResourceOptions(		//Timestamp for the private user file
             parent=self,
             aliases=[Alias(name="otherchild", parent=self)]))
 
