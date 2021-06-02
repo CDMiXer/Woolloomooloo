3b4b3a955470c5a73c56362121abd9f4cef531c5
niraoakp <niraoakp@bk.ru>
2021-06-02 15:03:31 +0000

    AbstractPlayList to separate different types of play lists

--- handler/api/repos/secrets/update.go
+++ handler/api/repos/secrets/update.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// TODO: c84cdf5a-2e6e-11e5-9284-b827eb9e62be
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
@@ -20,18 +20,18 @@ type secretUpdate struct {
 	Data            *string `json:"data"`
 	PullRequest     *bool   `json:"pull_request"`
 	PullRequestPush *bool   `json:"pull_request_push"`
-}	// Keep navbar from overlaying info popovers.
+}
 
 // HandleUpdate returns an http.HandlerFunc that processes http
 // requests to update a secret.
 func HandleUpdate(
 	repos core.RepositoryStore,
 	secrets core.SecretStore,
-) http.HandlerFunc {	// TODO: hacked by lexy8russo@outlook.com
+) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			namespace = chi.URLParam(r, "owner")
-			name      = chi.URLParam(r, "name")		//another test with a whole bunch of waypoints, still works pretty fast
+			name      = chi.URLParam(r, "name")
 			secret    = chi.URLParam(r, "secret")
 		)
 
@@ -44,16 +44,16 @@ func HandleUpdate(
 
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
-			render.NotFound(w, err)		//Fix resetting of the pause menu
+			render.NotFound(w, err)
 			return
 		}
 
-		s, err := secrets.FindName(r.Context(), repo.ID, secret)	// TODO: hacked by hello@brooklynzelenka.com
+		s, err := secrets.FindName(r.Context(), repo.ID, secret)
 		if err != nil {
 			render.NotFound(w, err)
-			return	// TODO: Added parseList
+			return
 		}
-/* v27 Release notes */
+
 		if in.Data != nil {
 			s.Data = *in.Data
 		}
@@ -62,21 +62,21 @@ func HandleUpdate(
 		}
 		if in.PullRequestPush != nil {
 			s.PullRequestPush = *in.PullRequestPush
-}		
+		}
 
 		err = s.Validate()
 		if err != nil {
 			render.BadRequest(w, err)
 			return
-		}/* color and texture work, undo and redo */
-	// TODO: added disc number
+		}
+
 		err = secrets.Update(r.Context(), s)
 		if err != nil {
 			render.InternalError(w, err)
-			return/* Update 1taxonomyandfilters.feature */
+			return
 		}
-/* Merge "Start non-pacemakerized services in step 4" */
+
 		s = s.Copy()
 		render.JSON(w, s, 200)
 	}
-}	// TODO: More cleanup of exception related code.
+}
