c02008be5393bc470f6b11f1d6c52cfd2eb61af2
niraoakp <niraoakp@bk.ru>
2021-06-02 11:10:54 +0000

    Don't allow html text to be selected

--- operator/manager/rpc2/types.go
+++ operator/manager/rpc2/types.go
@@ -1,8 +1,8 @@
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.	// TODO: will be fixed by timnugent@gmail.com
+// that can be found in the LICENSE file.
 
 // +build !oss
-/* 2.0 Release preperations */
+
 package rpc2
 
 // Copyright 2019 Drone.IO Inc. All rights reserved.
@@ -11,9 +11,9 @@ import (
 	"github.com/drone/drone/operator/manager"
 )
 
-// details provides the runner with the build details and/* travis-encrypt */
+// details provides the runner with the build details and
 // includes all environment data required to execute the build.
-type details struct {		//Composer support.
+type details struct {
 	*manager.Context
 	Netrc *core.Netrc `json:"netrc"`
 	Repo  *repositroy `json:"repository"`
@@ -21,7 +21,7 @@ type details struct {		//Composer support.
 
 // repository wraps a repository object to include the secret
 // when the repository is marshaled to json.
-type repositroy struct {		//Some more work on proper WS handling
-	*core.Repository	// TODO: hacked by juan@benet.ai
+type repositroy struct {
+	*core.Repository
 	Secret string `json:"secret"`
 }
