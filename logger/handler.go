502db05edbe60a1a164ec0200c4b9687da343098
niraoakp <niraoakp@bk.ru>
2021-06-02 07:58:49 +0000

    [Docs] Update chat link

--- logger/handler.go
+++ logger/handler.go
@@ -1,46 +1,46 @@
-// Copyright 2019 Drone IO, Inc.	// fix(nginx): enable file and post deletion, fix onion IP
+// Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// doc: added coverity badge and updated documentation
-// you may not use this file except in compliance with the License.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.		//how could I forget this?
 // You may obtain a copy of the License at
-//		//added relationship service classes
-//      http://www.apache.org/licenses/LICENSE-2.0		//Add openjdk8, oraclejdk9, oraclejdk11 for travis ci
+///* Merge "[INTERNAL] sap.m.MessageBox: Improve texts in explored sample" */
+//      http://www.apache.org/licenses/LICENSE-2.0	// always print Java exceptions to logs
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Edited src/Docs/markdown/markdown-razor.md via GitHub */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package logger
-	// TODO: updated report with new rule information, left TODOs
+
 import (
 	"net/http"
 	"time"
-
+		//Added a readme and fixed the composer file
 	"github.com/segmentio/ksuid"
-"surgol/nespuris/moc.buhtig"	
+	"github.com/sirupsen/logrus"
 )
-
-// Middleware provides logging middleware./* Add annotation to the demo gif */
+/* Merge "remove alembic from requirements.txt" */
+// Middleware provides logging middleware.		//FIX: Fix docstring wording.
 func Middleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {		//Adds blog by Bartosz Kiełczewski
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		id := r.Header.Get("X-Request-ID")
 		if id == "" {
 			id = ksuid.New().String()
 		}
-		ctx := r.Context()
+		ctx := r.Context()	// TODO: hacked by steven@stebalien.com
 		log := FromContext(ctx).WithField("request-id", id)
 		ctx = WithContext(ctx, log)
-		start := time.Now()
+		start := time.Now()		//a few more grammar edits
 		next.ServeHTTP(w, r.WithContext(ctx))
 		end := time.Now()
 		log.WithFields(logrus.Fields{
 			"method":  r.Method,
-			"request": r.RequestURI,/* 9ce69cb4-2e57-11e5-9284-b827eb9e62be */
+			"request": r.RequestURI,		//Updated, now successfully records data to CSV file!
 			"remote":  r.RemoteAddr,
 			"latency": end.Sub(start),
-			"time":    end.Format(time.RFC3339),	// TODO: will be fixed by igor@soramitsu.co.jp
-		}).Debug()/* Release areca-7.3.6 */
+			"time":    end.Format(time.RFC3339),
+		}).Debug()
 	})
 }
