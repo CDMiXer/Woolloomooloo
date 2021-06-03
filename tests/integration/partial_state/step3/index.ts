1131975eed605cf1aa9687c2235f5bfaea0ca761
niraoakp <niraoakp@bk.ru>
2021-06-03 05:00:13 +0000

    Event could be bare

--- tests/integration/partial_state/step3/index.ts
+++ tests/integration/partial_state/step3/index.ts
@@ -1,9 +1,9 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import * as pulumi from "@pulumi/pulumi";
-import { Resource } from "./resource";		//PHP 5.3 version...
+import { Resource } from "./resource";
 
 // resource "not-doomed" is created successfully.
 const a = new Resource("not-doomed", 5);
-/* Release 0.23.6 */
-// "a" should be in the checkpoint.
+
+// "a" should be in the checkpoint./* Merge "board-8064-bt: Release the BT resources only when BT is in On state" */
