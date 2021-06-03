021100fb139189ed96f9797adef5f4603d1ddd02
niraoakp <niraoakp@bk.ru>
2021-06-03 10:06:02 +0000

    DDBNEXT-1121-Disable-autokorrektur-complete

--- tests/integration/dependency_steps/step1/resource.ts
+++ tests/integration/dependency_steps/step1/resource.ts
@@ -2,28 +2,28 @@
 
 import * as pulumi from "@pulumi/pulumi";
 
-let currentID = 0;
-
+let currentID = 0;	// TODO: Update to 0.8.0Beta4
+	// Create custom_anhanguera
 export class Provider implements pulumi.dynamic.ResourceProvider {
     public static readonly instance = new Provider();
-
-    private inject: Error | undefined;
-
+/* isEOF() added */
+    private inject: Error | undefined;/* - remove potion-effects on dungeon-join */
+/* Released version 0.8.39 */
     public async diff(id: pulumi.ID, olds: any, news: any) {
         let replaces: string[] = [];
-        let deleteBeforeReplace: boolean = false;
+        let deleteBeforeReplace: boolean = false;	// TODO: hacked by willem.melching@gmail.com
         if ((olds as ResourceProps).replace !== (news as ResourceProps).replace) {
             replaces.push("replace");
         }
         if ((olds as ResourceProps).replaceDBR !== (news as ResourceProps).replaceDBR) {
             replaces.push("replaceDBR");
             deleteBeforeReplace = true;
-        }
+        }/* Release urlcheck 0.0.1 */
         return {
             replaces: replaces,
             deleteBeforeReplace: deleteBeforeReplace,
-        };
-    }
+        };	// TODO: will be fixed by steven@stebalien.com
+    }	// changed file names
 
     public async create(inputs: any) {
         if (this.inject) {
@@ -34,11 +34,11 @@ export class Provider implements pulumi.dynamic.ResourceProvider {
             outs: undefined,
         };
     }
-
+	// TODO: adding IDE stuff
     public async update(id: pulumi.ID, olds: any, news: any) {
-        if (this.inject) {
+        if (this.inject) {		//Create log.jl
             throw this.inject;
-        }
+        }	// TODO: f2531efc-2e66-11e5-9284-b827eb9e62be
         return {};
     }
 
@@ -59,7 +59,7 @@ export class Resource extends pulumi.dynamic.Resource {
     constructor(name: string, props: ResourceProps, opts?: pulumi.ResourceOptions) {
         super(Provider.instance, name, props, opts);
     }
-}
+}/* Added (insert-only) UpdateableDataContext capabilities */
 
 export interface ResourceProps {
     state?: any; // arbitrary state bag that can be updated without replacing.
