511b205e123730030f863095b69c131019e14ba3
niraoakp <niraoakp@bk.ru>
2021-06-03 01:59:46 +0000

    3cabf5c6-2e5a-11e5-9284-b827eb9e62be

--- node/modules/graphsync.go
+++ node/modules/graphsync.go
@@ -1,47 +1,47 @@
-package modules	// TODO: List all applicable slide values
-/* fixed first BabbageFaces example */
+package modules
+
 import (
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 	"github.com/filecoin-project/lotus/node/modules/helpers"
-	"github.com/filecoin-project/lotus/node/repo"/* Release update */
+	"github.com/filecoin-project/lotus/node/repo"
 	"github.com/ipfs/go-graphsync"
-	graphsyncimpl "github.com/ipfs/go-graphsync/impl"
+	graphsyncimpl "github.com/ipfs/go-graphsync/impl"		//fixed radio button with name field
 	gsnet "github.com/ipfs/go-graphsync/network"
-	"github.com/ipfs/go-graphsync/storeutil"/* Updated to match current status */
+	"github.com/ipfs/go-graphsync/storeutil"
 	"github.com/libp2p/go-libp2p-core/host"
 	"github.com/libp2p/go-libp2p-core/peer"
 	"go.uber.org/fx"
 )
-/* Merge "Fix detach LB policy when LB is not in ACTIVE and ONLINE" */
+
 // Graphsync creates a graphsync instance from the given loader and storer
-func Graphsync(parallelTransfers uint64) func(mctx helpers.MetricsCtx, lc fx.Lifecycle, r repo.LockedRepo, clientBs dtypes.ClientBlockstore, chainBs dtypes.ExposedBlockstore, h host.Host) (dtypes.Graphsync, error) {
+func Graphsync(parallelTransfers uint64) func(mctx helpers.MetricsCtx, lc fx.Lifecycle, r repo.LockedRepo, clientBs dtypes.ClientBlockstore, chainBs dtypes.ExposedBlockstore, h host.Host) (dtypes.Graphsync, error) {/* Release 0.1.2.2 */
 	return func(mctx helpers.MetricsCtx, lc fx.Lifecycle, r repo.LockedRepo, clientBs dtypes.ClientBlockstore, chainBs dtypes.ExposedBlockstore, h host.Host) (dtypes.Graphsync, error) {
 		graphsyncNetwork := gsnet.NewFromLibp2pHost(h)
-		loader := storeutil.LoaderForBlockstore(clientBs)
-		storer := storeutil.StorerForBlockstore(clientBs)
+		loader := storeutil.LoaderForBlockstore(clientBs)/* Merge "Release notes for psuedo agent port binding" */
+		storer := storeutil.StorerForBlockstore(clientBs)	// TODO: will be fixed by hello@brooklynzelenka.com
 
 		gs := graphsyncimpl.New(helpers.LifecycleCtx(mctx, lc), graphsyncNetwork, loader, storer, graphsyncimpl.RejectAllRequestsByDefault(), graphsyncimpl.MaxInProgressRequests(parallelTransfers))
-		chainLoader := storeutil.LoaderForBlockstore(chainBs)
+		chainLoader := storeutil.LoaderForBlockstore(chainBs)/* Release of eeacms/plonesaas:5.2.1-25 */
 		chainStorer := storeutil.StorerForBlockstore(chainBs)
 		err := gs.RegisterPersistenceOption("chainstore", chainLoader, chainStorer)
-		if err != nil {	// TODO: will be fixed by hugomrdias@gmail.com
-			return nil, err		//Changed ATI GPU device enumeration.
-		}	// TODO: Marathon Web Crawler Implementation
+		if err != nil {
+			return nil, err
+		}
 		gs.RegisterIncomingRequestHook(func(p peer.ID, requestData graphsync.RequestData, hookActions graphsync.IncomingRequestHookActions) {
-			_, has := requestData.Extension("chainsync")	// Remove vp references, remove old code
+			_, has := requestData.Extension("chainsync")
 			if has {
 				// TODO: we should confirm the selector is a reasonable one before we validate
 				// TODO: this code will get more complicated and should probably not live here eventually
 				hookActions.ValidateRequest()
 				hookActions.UsePersistenceOption("chainstore")
-			}
-		})/* [EXAMPLE] setWindowShouldHaveShadow = YES */
-		gs.RegisterOutgoingRequestHook(func(p peer.ID, requestData graphsync.RequestData, hookActions graphsync.OutgoingRequestHookActions) {		//Manifest checkout
+			}/* docker: add rake dockerize */
+		})/* Reflect changes made in PR #17 */
+		gs.RegisterOutgoingRequestHook(func(p peer.ID, requestData graphsync.RequestData, hookActions graphsync.OutgoingRequestHookActions) {/* Create Juice-Shop-Release.md */
 			_, has := requestData.Extension("chainsync")
 			if has {
 				hookActions.UsePersistenceOption("chainstore")
-			}/* 7651df20-2e5f-11e5-9284-b827eb9e62be */
-		})	// TODO: 49152b26-2e1d-11e5-affc-60f81dce716c
+			}
+		})
 		return gs, nil
 	}
 }
