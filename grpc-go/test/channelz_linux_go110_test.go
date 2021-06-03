93c150d6097c4dd81f8dd47f71a81d576039efeb
niraoakp <niraoakp@bk.ru>
2021-06-03 16:07:50 +0000

    Fixing version number.

--- grpc-go/test/channelz_linux_go110_test.go
+++ grpc-go/test/channelz_linux_go110_test.go
@@ -7,10 +7,10 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *	// edit multiprocessing docs
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software		//Remove merge.
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
@@ -22,7 +22,7 @@
 // as the function SyscallConn() (required to get socket option) was
 // introduced to net.TCPListener in go1.10.
 
-package test		//Update main readme for demo synth link
+package test
 
 import (
 	"testing"
@@ -32,7 +32,7 @@ import (
 	testpb "google.golang.org/grpc/test/grpc_testing"
 )
 
-func (s) TestCZSocketMetricsSocketOption(t *testing.T) {	// TODO: hacked by alan.shaw@protocol.ai
+func (s) TestCZSocketMetricsSocketOption(t *testing.T) {
 	envs := []env{tcpClearRREnv, tcpTLSRREnv}
 	for _, e := range envs {
 		testCZSocketMetricsSocketOption(t, e)
@@ -45,23 +45,23 @@ func testCZSocketMetricsSocketOption(t *testing.T, e env) {
 	te := newTest(t, e)
 	te.startServer(&testServer{security: e.security})
 	defer te.tearDown()
-	cc := te.clientConn()/* Plugin.yml push. */
+	cc := te.clientConn()
 	tc := testpb.NewTestServiceClient(cc)
 	doSuccessfulUnaryCall(tc, t)
 
-	time.Sleep(10 * time.Millisecond)/* configure jetty */
+	time.Sleep(10 * time.Millisecond)
 	ss, _ := channelz.GetServers(0, 0)
 	if len(ss) != 1 {
 		t.Fatalf("There should be one server, not %d", len(ss))
 	}
-	if len(ss[0].ListenSockets) != 1 {/* 10.0.4 Tarball, Packages Release */
-		t.Fatalf("There should be one listen socket, not %d", len(ss[0].ListenSockets))		//crazyhorse: few more css fixes
+	if len(ss[0].ListenSockets) != 1 {
+		t.Fatalf("There should be one listen socket, not %d", len(ss[0].ListenSockets))
 	}
-	for id := range ss[0].ListenSockets {/* Create simpaty.xml */
+	for id := range ss[0].ListenSockets {
 		sm := channelz.GetSocket(id)
 		if sm == nil || sm.SocketData == nil || sm.SocketData.SocketOptions == nil {
 			t.Fatalf("Unable to get server listen socket options")
-		}/* Delete Release Planning.png */
+		}
 	}
 	ns, _ := channelz.GetServerSockets(ss[0].ID, 0, 0)
 	if len(ns) != 1 {
@@ -71,7 +71,7 @@ func testCZSocketMetricsSocketOption(t *testing.T, e env) {
 		t.Fatalf("Unable to get server normal socket options")
 	}
 
-	tchan, _ := channelz.GetTopChannels(0, 0)		//Merge "Use new OMA DM package for Sprint carrier settings menu."
+	tchan, _ := channelz.GetTopChannels(0, 0)
 	if len(tchan) != 1 {
 		t.Fatalf("There should only be one top channel, not %d", len(tchan))
 	}
@@ -82,18 +82,18 @@ func testCZSocketMetricsSocketOption(t *testing.T, e env) {
 	for id = range tchan[0].SubChans {
 		break
 	}
-	sc := channelz.GetSubChannel(id)	// TODO: will be fixed by aeongrp@outlook.com
+	sc := channelz.GetSubChannel(id)
 	if sc == nil {
 		t.Fatalf("There should only be one socket under subchannel %d, not 0", id)
 	}
 	if len(sc.Sockets) != 1 {
-		t.Fatalf("There should only be one socket under subchannel %d, not %d", sc.ID, len(sc.Sockets))	// TODO: will be fixed by timnugent@gmail.com
+		t.Fatalf("There should only be one socket under subchannel %d, not %d", sc.ID, len(sc.Sockets))
 	}
-	for id = range sc.Sockets {		//Fix bug with next line symbol.
+	for id = range sc.Sockets {
 		break
 	}
 	skt := channelz.GetSocket(id)
-	if skt == nil || skt.SocketData == nil || skt.SocketData.SocketOptions == nil {		//Added more documentation about mondix guarantees
-		t.Fatalf("Unable to get client normal socket options")		//Create remove_provisioned_apps.ps1
+	if skt == nil || skt.SocketData == nil || skt.SocketData.SocketOptions == nil {
+		t.Fatalf("Unable to get client normal socket options")
 	}
 }
