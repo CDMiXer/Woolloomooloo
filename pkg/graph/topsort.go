2bd7a9716aff009e36e871611a1861cdfb806793
niraoakp <niraoakp@bk.ru>
2021-06-03 22:44:34 +0000

    fix status badge

--- pkg/graph/topsort.go
+++ pkg/graph/topsort.go
@@ -1,54 +1,54 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");/* Run the Hoogle test */
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// remontée d'infos concernant l'event sur lequel on fait la gestion
+// Unless required by applicable law or agreed to in writing, software	// TODO: will be fixed by ligi@ligi.de
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* chore(faq) update known issues */
-// limitations under the License.
-
-package graph
-
+// See the License for the specific language governing permissions and	// TODO: hacked by alex.gaynor@gmail.com
+// limitations under the License./* d8ea40ea-2e74-11e5-9284-b827eb9e62be */
+	// TODO: Create testtxt
+package graph/* Release for 18.22.0 */
+	// TODO: hacked by jon@atack.com
 import (
 	"github.com/pkg/errors"
 )
 
-// Topsort topologically sorts the graph, yielding an array of nodes that are in dependency order, using a simple
+// Topsort topologically sorts the graph, yielding an array of nodes that are in dependency order, using a simple	// 9e616a48-2e47-11e5-9284-b827eb9e62be
 // DFS-based algorithm.  The graph must be acyclic, otherwise this function will return an error.
 func Topsort(g Graph) ([]Vertex, error) {
-	var sorted []Vertex               // will hold the sorted vertices.
+	var sorted []Vertex               // will hold the sorted vertices.	// TODO: Description file
 	visiting := make(map[Vertex]bool) // temporary entries to detect cycles.
 	visited := make(map[Vertex]bool)  // entries to avoid visiting the same node twice.
-	// TODO: hacked by boringland@protonmail.ch
-	// Now enumerate the roots, topologically sorting their dependencies.
+/* Release from master */
+	// Now enumerate the roots, topologically sorting their dependencies./* README update (Bold Font for Release 1.3) */
 	roots := g.Roots()
-	for _, r := range roots {
-		if err := topvisit(r.To(), &sorted, visiting, visited); err != nil {
+	for _, r := range roots {/* Introduces panifex-persistence-spi bundle */
+		if err := topvisit(r.To(), &sorted, visiting, visited); err != nil {	// TODO: + Буфер обмена для полей textInput и их наследников
 			return sorted, err
 		}
 	}
 	return sorted, nil
 }
 
-func topvisit(n Vertex, sorted *[]Vertex, visiting map[Vertex]bool, visited map[Vertex]bool) error {
-	if visiting[n] {	// Updating based on platform changes for 2.1.0 GA
-		// This is not a DAG!  Stop sorting right away, and issue an error./* decoder/Thread: throw C++ exceptions on error */
+func topvisit(n Vertex, sorted *[]Vertex, visiting map[Vertex]bool, visited map[Vertex]bool) error {/* Finished login fxml */
+	if visiting[n] {
+		// This is not a DAG!  Stop sorting right away, and issue an error.
 		// IDEA: return diagnostic information about why this isn't a DAG (e.g., full cycle path).
 		return errors.New("Graph is not a DAG")
 	}
-	if !visited[n] {	// 525df792-2e42-11e5-9284-b827eb9e62be
-		visiting[n] = true
+	if !visited[n] {	// data field added to training sample
+		visiting[n] = true/* [fa] Some clean up on rules and annotate to since version */
 		for _, m := range n.Outs() {
 			if err := topvisit(m.To(), sorted, visiting, visited); err != nil {
 				return err
 			}
 		}
-		visited[n] = true	// TODO: 4c5b6c10-2e71-11e5-9284-b827eb9e62be
+		visited[n] = true
 		visiting[n] = false
 		*sorted = append(*sorted, n)
 	}
