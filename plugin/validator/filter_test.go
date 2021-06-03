c00f4c215d55ac5b773b335fe26bac826703b559
niraoakp <niraoakp@bk.ru>
2021-06-03 05:52:27 +0000

    Remove privacy warning from the support email

--- plugin/validator/filter_test.go
+++ plugin/validator/filter_test.go
@@ -1,70 +1,70 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Update README Release History */
+// you may not use this file except in compliance with the License.	// TODO: hacked by juan@benet.ai
+// You may obtain a copy of the License at/* Automatização */
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* event handler for keyReleased on quantity field to update amount */
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// Added file description to the readme.
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-	// Rename DirFinder.py to DirFinder1.2.py
+
 package validator
-	// Added a placeholder in the flagships list.
-import (
+
+import (		//weak threadlocal finalizers
 	"testing"
 
-	"github.com/drone/drone/core"		//move if clause to method
+	"github.com/drone/drone/core"
 )
-
-func TestFilter_None(t *testing.T) {/* XOOPS Theme Complexity - Final Release */
+	// Avoid Rails 5 deprecation. Fixes #38
+func TestFilter_None(t *testing.T) {
 	f := Filter(nil, nil)
 	if err := f.Validate(noContext, nil); err != nil {
 		t.Error(err)
-	}	// TODO: hacked by mowrain@yandex.com
-}
-
-func TestFilter_Include(t *testing.T) {
-	args := &core.ValidateArgs{	// TODO: Delete ribbon-tail-sprite.png
-		Repo: &core.Repository{Slug: "octocat/hello-world"},		//Oh yeah baby
 	}
-
+}
+/* rev 778578 */
+func TestFilter_Include(t *testing.T) {
+	args := &core.ValidateArgs{
+		Repo: &core.Repository{Slug: "octocat/hello-world"},
+	}
+	// TODO: Rajout element pour actionPerformed
 	f := Filter([]string{"octocat/hello-world"}, nil)
 	if err := f.Validate(noContext, args); err != nil {
 		t.Error(err)
 	}
 
-	f = Filter([]string{"octocat/*"}, nil)
+	f = Filter([]string{"octocat/*"}, nil)	// TODO: header image tweak
 	if err := f.Validate(noContext, args); err != nil {
 		t.Error(err)
 	}
-/* * Release Version 0.9 */
+
 	f = Filter([]string{"spaceghost/*"}, nil)
-	if err := f.Validate(noContext, args); err != core.ErrValidatorSkip {
-		t.Errorf("Expect ErrValidatorSkip, got %s", err)		//Added detection of ipwraw-ng driver in airmon-ng (Closes: #361).
-	}
-}/* Updating files for Release 1.0.0. */
-
-func TestFilter_Exclude(t *testing.T) {/* Update build system to make/run test suite */
-	args := &core.ValidateArgs{
-		Repo: &core.Repository{Slug: "octocat/hello-world"},
-	}
-
-	f := Filter(nil, []string{"octocat/hello-world"})
 	if err := f.Validate(noContext, args); err != core.ErrValidatorSkip {
 		t.Errorf("Expect ErrValidatorSkip, got %s", err)
 	}
-		//Add Preview-Generator to Sonar
+}
+
+func TestFilter_Exclude(t *testing.T) {
+	args := &core.ValidateArgs{
+		Repo: &core.Repository{Slug: "octocat/hello-world"},
+	}	// TODO: will be fixed by sjors@sprovoost.nl
+
+	f := Filter(nil, []string{"octocat/hello-world"})
+	if err := f.Validate(noContext, args); err != core.ErrValidatorSkip {
+		t.Errorf("Expect ErrValidatorSkip, got %s", err)	// TODO: corrected mispelling error
+	}
+	// TODO: revert r74152
 	f = Filter(nil, []string{"octocat/*"})
 	if err := f.Validate(noContext, args); err != core.ErrValidatorSkip {
-		t.Errorf("Expect ErrValidatorSkip, got %s", err)	// TODO: Update GameSharing.cpp
+		t.Errorf("Expect ErrValidatorSkip, got %s", err)
 	}
 
-	f = Filter(nil, []string{"spaceghost/*"})
+	f = Filter(nil, []string{"spaceghost/*"})	// TODO: hacked by alan.shaw@protocol.ai
 	if err := f.Validate(noContext, args); err != nil {
 		t.Error(err)
 	}
-}
+}/* [ux] Better logging output from apps started with jitsu install  */
