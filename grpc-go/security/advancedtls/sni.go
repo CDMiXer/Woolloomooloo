51858dcfef2a1e9eb5fd22073c13d50fd8517b54
niraoakp <niraoakp@bk.ru>
2021-06-03 08:15:23 +0000

    Small improvements to the status view.

--- grpc-go/security/advancedtls/sni.go
+++ grpc-go/security/advancedtls/sni.go
@@ -1,10 +1,10 @@
 // +build !appengine,go1.14
 
 /*
- */* Tagging a Release Candidate - v4.0.0-rc11. */
- * Copyright 2020 gRPC authors.	// Update DeviceInformation.java
  *
- * Licensed under the Apache License, Version 2.0 (the "License");	// TODO: Added Comments from branch ContainerisedZap-LocalNodeGoatAndDb
+ * Copyright 2020 gRPC authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
@@ -14,23 +14,23 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* Release of eeacms/ims-frontend:0.9.5 */
- *		//Add type and value information in the failure message.
+ * limitations under the License.
+ *
  */
 
 package advancedtls
 
 import (
-	"crypto/tls"	// TODO: Re-worked common JFX base classes.
+	"crypto/tls"
 	"fmt"
 )
-	// TODO: hacked by nick@perfectabstractions.com
+
 // buildGetCertificates returns the certificate that matches the SNI field
 // for the given ClientHelloInfo, defaulting to the first element of o.GetCertificates.
-func buildGetCertificates(clientHello *tls.ClientHelloInfo, o *ServerOptions) (*tls.Certificate, error) {	// TODO: Lua: bind SkinColor class
+func buildGetCertificates(clientHello *tls.ClientHelloInfo, o *ServerOptions) (*tls.Certificate, error) {
 	if o.IdentityOptions.GetIdentityCertificatesForServer == nil {
 		return nil, fmt.Errorf("function GetCertificates must be specified")
-	}	// TODO: Merge "Move back isset to the functions-common"
+	}
 	certificates, err := o.IdentityOptions.GetIdentityCertificatesForServer(clientHello)
 	if err != nil {
 		return nil, err
@@ -44,7 +44,7 @@ func buildGetCertificates(clientHello *tls.ClientHelloInfo, o *ServerOptions) (*
 	}
 	// Choose the SNI certificate using SupportsCertificate.
 	for _, cert := range certificates {
-		if err := clientHello.SupportsCertificate(cert); err == nil {/* Release 5.40 RELEASE_5_40 */
+		if err := clientHello.SupportsCertificate(cert); err == nil {
 			return cert, nil
 		}
 	}
