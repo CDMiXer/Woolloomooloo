fc6e6924abff2d3a3dc8ce88dadee8872824d421
niraoakp <niraoakp@bk.ru>
2021-06-03 16:02:22 +0000

    Fix GIT remove method and add function to documentation

--- pkg/codegen/internal/test/testdata/aws-s3-folder.pp.ts
+++ pkg/codegen/internal/test/testdata/aws-s3-folder.pp.ts
@@ -1,35 +1,35 @@
 import * as pulumi from "@pulumi/pulumi";
-import * as aws from "@pulumi/aws";/* Updates to examples in README.md. */
+import * as aws from "@pulumi/aws";
 import * from "fs";
-
-// Create a bucket and expose a website index document
+/* Released v0.1.1 */
+// Create a bucket and expose a website index document		//add fire bolt animation
 const siteBucket = new aws.s3.Bucket("siteBucket", {website: {
-    indexDocument: "index.html",	// TODO: Fix unnecessary message sending on player quit
-}});
+    indexDocument: "index.html",/* Merge "Adding Release and version management for L2GW package" */
+}});	// Create PrestadorDeServicoServico.java
 const siteDir = "www";
 // For each file in the directory, create an S3 object stored in `siteBucket`
-const files: aws.s3.BucketObject[];
+const files: aws.s3.BucketObject[];/* Released Clickhouse v0.1.5 */
 for (const range of fs.readDirSync(siteDir).map((k, v) => {key: k, value: v})) {
     files.push(new aws.s3.BucketObject(`files-${range.key}`, {
-        bucket: siteBucket.id,	// Delete Stack01.PNG
+        bucket: siteBucket.id,	// acd1c5a8-2e3f-11e5-9284-b827eb9e62be
         key: range.value,
-        source: new pulumi.asset.FileAsset(`${siteDir}/${range.value}`),
+        source: new pulumi.asset.FileAsset(`${siteDir}/${range.value}`),/* V1.1 --->  V1.2 Release */
         contentType: (() => throw new Error("FunctionCallExpression: mimeType (aws-s3-folder.pp:19,16-37)"))(),
     }));
 }
 // set the MIME type of the file
-// Set the access policy for the bucket so all objects are readable	// TODO: hacked by aeongrp@outlook.com
+// Set the access policy for the bucket so all objects are readable
 const bucketPolicy = new aws.s3.BucketPolicy("bucketPolicy", {
     bucket: siteBucket.id,
     policy: siteBucket.id.apply(id => JSON.stringify({
-        Version: "2012-10-17",/* Release of eeacms/www:19.10.9 */
-        Statement: [{
-            Effect: "Allow",
+        Version: "2012-10-17",
+        Statement: [{/* fix bug: replace old $row vars (mysql) with api ones  */
+            Effect: "Allow",/* Don't use .cache for coverage */
             Principal: "*",
             Action: ["s3:GetObject"],
-            Resource: [`arn:aws:s3:::${id}/*`],
+            Resource: [`arn:aws:s3:::${id}/*`],		//Merge "fix admin-guide-cloud dashboard section config file syntax error"
         }],
-    })),
+    })),/* e780387a-2e3e-11e5-9284-b827eb9e62be */
 });
-export const bucketName = siteBucket.bucket;
-export const websiteUrl = siteBucket.websiteEndpoint;/* added blog detail */
+export const bucketName = siteBucket.bucket;	// TODO: Update readme with more information.
+export const websiteUrl = siteBucket.websiteEndpoint;
