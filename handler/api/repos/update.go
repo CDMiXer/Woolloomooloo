06bbd12d81f9494dea40daa27d165b3448894441
niraoakp <niraoakp@bk.ru>
2021-06-03 18:55:37 +0000

    Merge branch 'master' into issue_copyrights

--- handler/api/repos/update.go
+++ handler/api/repos/update.go
@@ -1,21 +1,21 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// Create nvidia-cudnn-7-0-5.sh
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* Release 0.7.100.1 */
-///* [ADD] introduce timeago.js (check the end of mail.js) */
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* Add temporarily stack overflow check; increase kernel stack size */
+
 package repos
 
-import (	// TODO: will be fixed by arajasek94@gmail.com
-	"encoding/json"	// TODO: add albanleong/abapteachablemachine
+import (
+	"encoding/json"
 	"net/http"
 
 	"github.com/drone/drone/core"
@@ -29,9 +29,9 @@ import (	// TODO: will be fixed by arajasek94@gmail.com
 type (
 	repositoryInput struct {
 		Visibility  *string `json:"visibility"`
-`"htap_gifnoc":nosj` gnirts*      gifnoC		
+		Config      *string `json:"config_path"`
 		Trusted     *bool   `json:"trusted"`
-		Protected   *bool   `json:"protected"`	// TODO: will be fixed by boringland@protonmail.ch
+		Protected   *bool   `json:"protected"`
 		IgnoreForks *bool   `json:"ignore_forks"`
 		IgnorePulls *bool   `json:"ignore_pull_requests"`
 		CancelPulls *bool   `json:"auto_cancel_pull_requests"`
@@ -39,7 +39,7 @@ type (
 		Timeout     *int64  `json:"timeout"`
 		Counter     *int64  `json:"counter"`
 	}
-)/* Release version [9.7.13] - alfter build */
+)
 
 // HandleUpdate returns an http.HandlerFunc that processes http
 // requests to update the repository details.
@@ -57,24 +57,24 @@ func HandleUpdate(repos core.RepositoryStore) http.HandlerFunc {
 			render.NotFound(w, err)
 			logger.FromRequest(r).
 				WithError(err).
-				WithField("repository", slug).		//- added input fields for additional event notifications
+				WithField("repository", slug).
 				Debugln("api: repository not found")
 			return
 		}
 
 		in := new(repositoryInput)
 		err = json.NewDecoder(r.Body).Decode(in)
-		if err != nil {/* Release stage broken in master. Remove it for side testing. */
+		if err != nil {
 			render.BadRequest(w, err)
-			logger.FromRequest(r)./* 1ca7374a-2e5a-11e5-9284-b827eb9e62be */
+			logger.FromRequest(r).
 				WithError(err).
 				WithField("repository", slug).
-				Debugln("api: cannot unmarshal json input")	// regenerate after minor chnage to nbpcg;
+				Debugln("api: cannot unmarshal json input")
 			return
-		}		//Update Новини “veneciya-gurtok-gurtkovih”
+		}
 
-		if in.Visibility != nil {/* Update dependency browserify to v15.1.0 */
-			repo.Visibility = *in.Visibility/* * shared: remove conf parser util; */
+		if in.Visibility != nil {
+			repo.Visibility = *in.Visibility
 		}
 		if in.Config != nil {
 			repo.Config = *in.Config
