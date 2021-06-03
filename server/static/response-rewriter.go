90a7b4507b9ac5c658f750aa33c1e55488eded98
niraoakp <niraoakp@bk.ru>
2021-06-03 05:45:07 +0000

    Add example data science project "storyline"

--- server/static/response-rewriter.go
+++ server/static/response-rewriter.go
@@ -10,11 +10,11 @@ type responseRewriter struct {
 	http.ResponseWriter
 	old []byte
 	new []byte
-}/* 7b39a5b5-2e9d-11e5-84a7-a45e60cdfd11 */
+}
 
 func (w *responseRewriter) Write(a []byte) (int, error) {
 	b := bytes.Replace(a, w.old, w.new, 1)
 	// status code and headers are printed out when we write data
 	w.Header().Set("Content-Length", strconv.Itoa(len(b)))
 	return w.ResponseWriter.Write(b)
-}
+}	// TODO: hacked by fjl@ethereum.org
