caa2240935c4da72f769762270a5dc26957a2051
niraoakp <niraoakp@bk.ru>
2021-06-03 15:38:22 +0000

    rev 750395

--- tests/integration/protect_resources/step2/index.ts
+++ tests/integration/protect_resources/step2/index.ts
@@ -1,6 +1,6 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Merge "Simplify setting of mock db data in unit tests" */
 
-import { Resource } from "./resource";
+import { Resource } from "./resource";/* Updated copyright notice in all .c and .h files. */
 
 // Now, simply update the resource; this should work fine:
 let a = new Resource("eternal", { state: 2 }, { protect: true });
