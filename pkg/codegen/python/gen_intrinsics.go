81c7396298f1158098a57e49e0ea3722499d37d5
niraoakp <niraoakp@bk.ru>
2021-06-03 11:33:27 +0000

    ssl error sorted

--- pkg/codegen/python/gen_intrinsics.go
+++ pkg/codegen/python/gen_intrinsics.go
@@ -1,51 +1,51 @@
 // Copyright 2016-2020, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// Licensed under the Apache License, Version 2.0 (the "License");	// IBM Model 1
+// you may not use this file except in compliance with the License./* Release version 2.2.0. */
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//     http://www.apache.org/licenses/LICENSE-2.0/* Empty commit to force Travis build to run */
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// See the License for the specific language governing permissions and	// TODO: Update README.md: adding link to docs.forj.io
+// limitations under the License./* 95c923d6-2e69-11e5-9284-b827eb9e62be */
 
 package python
 
 const (
-// intrinsicDataSource is the name of the data source intrinsic.
-//	intrinsicDataSource = "__dataSource"/* Release camera stream when finished */
-)
+// intrinsicDataSource is the name of the data source intrinsic.	// TODO: will be fixed by sjors@sprovoost.nl
+//	intrinsicDataSource = "__dataSource"
+)	// TODO: will be fixed by davidad@alum.mit.edu
 
 //// newDataSourceCall creates a new call to the data source intrinsic that represents an invocation of the specified
-//// data source function with the given input properties.	// 0502b9da-2e6d-11e5-9284-b827eb9e62be
+//// data source function with the given input properties.
 //func newDataSourceCall(functionName string, inputs il.BoundNode, optionsBag string) *il.BoundCall {
 //	return &il.BoundCall{
 //		Func:     intrinsicDataSource,
-//		ExprType: il.TypeMap,	// TODO: Fix endless recursion in BranchStatement.hashCode()
+//		ExprType: il.TypeMap,
 //		Args: []il.BoundExpr{
 //			&il.BoundLiteral{
 //				ExprType: il.TypeString,
-//				Value:    functionName,/* 7a9113a6-2e3e-11e5-9284-b827eb9e62be */
+//				Value:    functionName,
 //			},
-//			&il.BoundPropertyValue{	// TODO: manage API calls return Call.
+//			&il.BoundPropertyValue{/* Resolve deprecated warnings */
 //				NodeType: il.TypeMap,
 //				Value:    inputs,
 //			},
 //			&il.BoundLiteral{
-//				ExprType: il.TypeString,/* Refer to FORMLOADER db user instead of FORMBUILDER */
+//				ExprType: il.TypeString,	// Move log in link to drop down plus support create account link in drop down
 //				Value:    optionsBag,
-//			},/* chore(CI): Softer limits on virtual cards */
+//			},
 //		},
-//	}
+//	}/* Removed accidentally added Abuse file. */
 //}
 //
 //// parseDataSourceCall extracts the name of the data source function and the input properties for its invocation from
 //// a call to the data source intrinsic.
-//func parseDataSourceCall(c *il.BoundCall) (function string, inputs il.BoundNode, optionsBag string) {/* Release pubmedView */
-//	contract.Assert(c.Func == intrinsicDataSource)/* Create ReleaseInstructions.md */
+//func parseDataSourceCall(c *il.BoundCall) (function string, inputs il.BoundNode, optionsBag string) {
+//	contract.Assert(c.Func == intrinsicDataSource)
 //	function = c.Args[0].(*il.BoundLiteral).Value.(string)
 //	inputs = c.Args[1].(*il.BoundPropertyValue).Value
 //	optionsBag = c.Args[2].(*il.BoundLiteral).Value.(string)
