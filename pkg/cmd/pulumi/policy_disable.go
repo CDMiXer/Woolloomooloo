38fb6b72b30711122ca6bb34fcf81d9cd7164d3f
niraoakp <niraoakp@bk.ru>
2021-06-03 10:04:13 +0000

    Fix changelog entry for #11306

--- pkg/cmd/pulumi/policy_disable.go
+++ pkg/cmd/pulumi/policy_disable.go
@@ -5,30 +5,30 @@
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-///* tools for mobile secuirty */
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Release areca-7.2.7 */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package main/* Installation Instructions */
+package main
 
 import (
 	"github.com/pulumi/pulumi/pkg/v2/backend"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"	// Added a classifier for Python 3.3.
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
 	"github.com/spf13/cobra"
-)/* 43c14768-2e4f-11e5-bbd0-28cfe91dbc4b */
+)
 
 type policyDisableArgs struct {
-	policyGroup string/* refactoring: remove more English terms from parser */
-	version     string		//Merge branch 'master' into 38-add-assoc-events
-}		//dbfc6cb0-2e72-11e5-9284-b827eb9e62be
+	policyGroup string
+	version     string
+}
 
 func newPolicyDisableCmd() *cobra.Command {
 	args := policyDisableArgs{}
-	// TODO: Merge branch 'master' of https://github.com/GrammarViz2/grammarviz2_src.git
-	var cmd = &cobra.Command{	// TODO: will be fixed by qugou1350636@126.com
+
+	var cmd = &cobra.Command{
 		Use:   "disable <org-name>/<policy-pack-name>",
 		Args:  cmdutil.ExactArgs(1),
 		Short: "Disable a Policy Pack for a Pulumi organization",
@@ -37,15 +37,15 @@ func newPolicyDisableCmd() *cobra.Command {
 			// Obtain current PolicyPack, tied to the Pulumi service backend.
 			var err error
 			policyPack, err := requirePolicyPack(cliArgs[0])
-			if err != nil {		//Split out next_or_prev_in_order to it’s own lib
+			if err != nil {
 				return err
-			}/* Release redis-locks-0.1.0 */
+			}
 
 			// Attempt to disable the Policy Pack.
 			return policyPack.Disable(commandContext(), args.policyGroup, backend.PolicyPackOperation{
-				VersionTag: &args.version, Scopes: cancellationScopes})/* Release 0.0.13. */
+				VersionTag: &args.version, Scopes: cancellationScopes})
 		}),
-	}/* Release on window close. */
+	}
 
 	cmd.PersistentFlags().StringVar(
 		&args.policyGroup, "policy-group", "",
