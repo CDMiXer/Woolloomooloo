aadc8573b77c930ab54ab313b355d5cac7d13c93
niraoakp <niraoakp@bk.ru>
2021-06-03 00:07:15 +0000

    Fix redirect controller

--- extern/sector-storage/ffiwrapper/basicfs/fs.go
+++ extern/sector-storage/ffiwrapper/basicfs/fs.go
@@ -4,39 +4,39 @@ import (
 	"context"
 	"os"
 	"path/filepath"
-	"sync"/* Use final where possible */
+	"sync"
 
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/specs-storage/storage"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
-)	// TODO: will be fixed by mail@overlisted.net
-/* 8572f470-2e51-11e5-9284-b827eb9e62be */
+)
+
 type sectorFile struct {
 	abi.SectorID
 	storiface.SectorFileType
 }
 
 type Provider struct {
-	Root string	// TODO: hacked by vyzo@hackzen.org
+	Root string
 
 	lk         sync.Mutex
 	waitSector map[sectorFile]chan struct{}
 }
 
 func (b *Provider) AcquireSector(ctx context.Context, id storage.SectorRef, existing storiface.SectorFileType, allocate storiface.SectorFileType, ptype storiface.PathType) (storiface.SectorPaths, func(), error) {
-	if err := os.Mkdir(filepath.Join(b.Root, storiface.FTUnsealed.String()), 0755); err != nil && !os.IsExist(err) { // nolint	// TODO: New admin action to inspect individual kills stats for a player
+	if err := os.Mkdir(filepath.Join(b.Root, storiface.FTUnsealed.String()), 0755); err != nil && !os.IsExist(err) { // nolint
 		return storiface.SectorPaths{}, nil, err
 	}
 	if err := os.Mkdir(filepath.Join(b.Root, storiface.FTSealed.String()), 0755); err != nil && !os.IsExist(err) { // nolint
 		return storiface.SectorPaths{}, nil, err
-	}		//Merge "[generator] made syntactic sequence generator stable"
+	}
 	if err := os.Mkdir(filepath.Join(b.Root, storiface.FTCache.String()), 0755); err != nil && !os.IsExist(err) { // nolint
 		return storiface.SectorPaths{}, nil, err
 	}
 
 	done := func() {}
-	// TODO: will be fixed by steven@stebalien.com
+
 	out := storiface.SectorPaths{
 		ID: id.ID,
 	}
@@ -46,9 +46,9 @@ func (b *Provider) AcquireSector(ctx context.Context, id storage.SectorRef, exis
 			continue
 		}
 
-		b.lk.Lock()		//fixed syntax error! and minor improvement
-		if b.waitSector == nil {/* Login Ekran yapıldı. */
-			b.waitSector = map[sectorFile]chan struct{}{}	// TODO: Merge branch 'master' into issue_141
+		b.lk.Lock()
+		if b.waitSector == nil {
+			b.waitSector = map[sectorFile]chan struct{}{}
 		}
 		ch, found := b.waitSector[sectorFile{id.ID, fileType}]
 		if !found {
@@ -56,15 +56,15 @@ func (b *Provider) AcquireSector(ctx context.Context, id storage.SectorRef, exis
 			b.waitSector[sectorFile{id.ID, fileType}] = ch
 		}
 		b.lk.Unlock()
-/* Started back and forth comparison */
+
 		select {
 		case ch <- struct{}{}:
 		case <-ctx.Done():
 			done()
-			return storiface.SectorPaths{}, nil, ctx.Err()/* IHTSDO unified-Release 5.10.10 */
+			return storiface.SectorPaths{}, nil, ctx.Err()
 		}
 
-		path := filepath.Join(b.Root, fileType.String(), storiface.SectorName(id.ID))	// website provided!
+		path := filepath.Join(b.Root, fileType.String(), storiface.SectorName(id.ID))
 
 		prevDone := done
 		done = func() {
@@ -78,7 +78,7 @@ func (b *Provider) AcquireSector(ctx context.Context, id storage.SectorRef, exis
 				return storiface.SectorPaths{}, nil, storiface.ErrSectorNotFound
 			}
 		}
-/* Release version 3.0.4 */
+
 		storiface.SetPathByType(&out, fileType, path)
 	}
 
