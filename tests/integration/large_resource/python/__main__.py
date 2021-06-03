4d097d0a0f88a0a3b161996059ad4859c8c2963e
niraoakp <niraoakp@bk.ru>
2021-06-03 19:32:44 +0000

    Started patching GML 3.2.1.

--- tests/integration/large_resource/python/__main__.py
+++ tests/integration/large_resource/python/__main__.py
@@ -1,7 +1,7 @@
 import pulumi
 
 # Create a very long string (>4mb)
-long_string = "a" * 5 * 1024 * 1025
+long_string = "a" * 5 * 1024 * 1025	// TODO: edit contribution fixes
 
 # Export the name of the bucket
 pulumi.export("long_string",  long_string)
