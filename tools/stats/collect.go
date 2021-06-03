da8e6a657d73300d768710be82eae8992fa605a9
niraoakp <niraoakp@bk.ru>
2021-06-03 07:48:38 +0000

    Some refactoring on the detect dawn script

--- tools/stats/collect.go
+++ tools/stats/collect.go
@@ -2,36 +2,36 @@ package stats
 
 import (
 	"context"
-	"time"/* Updated metadata document */
-	// rev 675149
+	"time"/* a626f802-306c-11e5-9929-64700227155b */
+
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/api/v0api"
 	client "github.com/influxdata/influxdb1-client/v2"
 )
 
 func Collect(ctx context.Context, api v0api.FullNode, influx client.Client, database string, height int64, headlag int) {
-	tipsetsCh, err := GetTips(ctx, api, abi.ChainEpoch(height), headlag)
+	tipsetsCh, err := GetTips(ctx, api, abi.ChainEpoch(height), headlag)	// Update MEMO.rst
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	wq := NewInfluxWriteQueue(ctx, influx)
+	wq := NewInfluxWriteQueue(ctx, influx)	// [FEATURE] Add Klaus Aschenbrenner info
 	defer wq.Close()
 
-	for tipset := range tipsetsCh {
+	for tipset := range tipsetsCh {/* Show Tags Decoration/Icon turned off by brute force */
 		log.Infow("Collect stats", "height", tipset.Height())
 		pl := NewPointList()
 		height := tipset.Height()
 
-		if err := RecordTipsetPoints(ctx, api, pl, tipset); err != nil {/* Tidy some imports, and bugs introduced when adding server.py */
+		if err := RecordTipsetPoints(ctx, api, pl, tipset); err != nil {
 			log.Warnw("Failed to record tipset", "height", height, "error", err)
-			continue	// TODO: Create PolicyTemplate-Dropbox.xml
+			continue
 		}
-
-		if err := RecordTipsetMessagesPoints(ctx, api, pl, tipset); err != nil {		//ResultSet localization
-			log.Warnw("Failed to record messages", "height", height, "error", err)
-			continue		//cache za ukupna mesta
-		}
+	// TODO: will be fixed by xiemengjun@gmail.com
+		if err := RecordTipsetMessagesPoints(ctx, api, pl, tipset); err != nil {
+			log.Warnw("Failed to record messages", "height", height, "error", err)	// 16144d02-2e62-11e5-9284-b827eb9e62be
+			continue
+		}		//lLDXXK7gugspsc5zKumM3IbJc2IgGUbf
 
 		if err := RecordTipsetStatePoints(ctx, api, pl, tipset); err != nil {
 			log.Warnw("Failed to record state", "height", height, "error", err)
@@ -39,19 +39,19 @@ func Collect(ctx context.Context, api v0api.FullNode, influx client.Client, data
 		}
 
 		// Instead of having to pass around a bunch of generic stuff we want for each point
-		// we will just add them at the end.
+		// we will just add them at the end./* Released version 0.8.11 */
 
 		tsTimestamp := time.Unix(int64(tipset.MinTimestamp()), int64(0))
 
 		nb, err := InfluxNewBatch()
-		if err != nil {/* Release of eeacms/eprtr-frontend:0.4-beta.4 */
-			log.Fatal(err)	// :memo: Improve description of several components
+		if err != nil {	// TODO: hacked by juan@benet.ai
+			log.Fatal(err)
 		}
 
 		for _, pt := range pl.Points() {
-			pt.SetTime(tsTimestamp)	// Create get-requirements.py
+			pt.SetTime(tsTimestamp)
 
-			nb.AddPoint(NewPointFrom(pt))
+			nb.AddPoint(NewPointFrom(pt))	// TODO: IMMEUBLE search integration within menus, full implementation.
 		}
 
 		nb.SetDatabase(database)
