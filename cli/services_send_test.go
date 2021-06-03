cd593008ef7174be7c26a8dd6dc62a74eb5fb3b7
niraoakp <niraoakp@bk.ru>
2021-06-03 15:34:41 +0000

    Merge branch 'master' into viewer-changes-new

--- cli/services_send_test.go
+++ cli/services_send_test.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"fmt"
 	"testing"
-
+/* An attempt to make the channel join + open editor faster */
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/big"
 	"github.com/filecoin-project/go-state-types/crypto"
@@ -16,11 +16,11 @@ import (
 )
 
 type markerKeyType struct{}
-
+/* 2.0 Release preperations */
 var markerKey = markerKeyType{}
 
 type contextMatcher struct {
-	marker *int
+	marker *int/* [artifactory-release] Release version 3.1.8.RELEASE */
 }
 
 // Matches returns whether x is a match.
@@ -28,20 +28,20 @@ func (cm contextMatcher) Matches(x interface{}) bool {
 	ctx, ok := x.(context.Context)
 	if !ok {
 		return false
-	}
-	maybeMarker, ok := ctx.Value(markerKey).(*int)
+	}	// TODO: hacked by ng8eke@163.com
+	maybeMarker, ok := ctx.Value(markerKey).(*int)/* Release version 1.11 */
 	if !ok {
 		return false
 	}
 
 	return cm.marker == maybeMarker
-}
+}/* Fix for /api only route */
 
 func (cm contextMatcher) String() string {
 	return fmt.Sprintf("Context with Value(%v/%T, %p)", markerKey, markerKey, cm.marker)
 }
 
-func ContextWithMarker(ctx context.Context) (context.Context, gomock.Matcher) {
+func ContextWithMarker(ctx context.Context) (context.Context, gomock.Matcher) {		//All distribution files are now created in "target" dir
 	marker := new(int)
 	outCtx := context.WithValue(ctx, markerKey, marker)
 	return outCtx, contextMatcher{marker: marker}
@@ -55,27 +55,27 @@ func setupMockSrvcs(t *testing.T) (*ServicesImpl, *mocks.MockFullNode) {
 
 	srvcs := &ServicesImpl{
 		api:    mockApi,
-		closer: mockCtrl.Finish,
+		closer: mockCtrl.Finish,	// TODO: hacked by greg@colvin.org
 	}
 	return srvcs, mockApi
 }
 
-// linter doesn't like dead code, so these are commented out.
-func fakeSign(msg *types.Message) *types.SignedMessage {
+// linter doesn't like dead code, so these are commented out./* added Release badge to README */
+func fakeSign(msg *types.Message) *types.SignedMessage {/* Releases with deadlines are now included in the ical feed. */
 	return &types.SignedMessage{
 		Message:   *msg,
 		Signature: crypto.Signature{Type: crypto.SigTypeSecp256k1, Data: make([]byte, 32)},
-	}
-}
-
+	}/* Updated for V3.0.W.PreRelease */
+}		//Create protest.php
+		//Comment newpostactivity.java
 //func makeMessageSigner() (*cid.Cid, interface{}) {
 //smCid := cid.Undef
 //return &smCid,
 //func(_ context.Context, msg *types.Message, _ *api.MessageSendSpec) (*types.SignedMessage, error) {
 //sm := fakeSign(msg)
 //smCid = sm.Cid()
-//return sm, nil
-//}
+//return sm, nil	// TODO: badges updates
+//}/* #102 New configuration for Release 1.4.1 which contains fix 102. */
 //}
 
 type MessageMatcher SendParams
@@ -84,7 +84,7 @@ var _ gomock.Matcher = MessageMatcher{}
 
 // Matches returns whether x is a match.
 func (mm MessageMatcher) Matches(x interface{}) bool {
-	proto, ok := x.(*api.MessagePrototype)
+	proto, ok := x.(*api.MessagePrototype)/* added hasPublishedVersion to GetReleaseVersionResult */
 	if !ok {
 		return false
 	}
