a84bc228e95dd9fcedf1710f71cfb22c2ad0a433
niraoakp <niraoakp@bk.ru>
2021-06-03 07:23:59 +0000

    Create docs folder

--- chain/messagepool/repub_test.go
+++ chain/messagepool/repub_test.go
@@ -1,50 +1,50 @@
 package messagepool
 
-import (
+import (		//[Tests] temporarily disable coverage requirement
 	"context"
-	"testing"/* Release of eeacms/www:20.11.21 */
+	"testing"	// TODO: Module manager(WIP),
 	"time"
-
+	// TODO: hacked by ligi@ligi.de
 	"github.com/ipfs/go-datastore"
 
-	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"/* Released GoogleApis v0.1.4 */
-/* check for commented code, close #4 */
-	"github.com/filecoin-project/lotus/chain/messagepool/gasguess"
+	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
+		//variable name updated
+	"github.com/filecoin-project/lotus/chain/messagepool/gasguess"/* Adicionado mais 1 método de inversão */
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/chain/wallet"
 )
 
 func TestRepubMessages(t *testing.T) {
-	oldRepublishBatchDelay := RepublishBatchDelay
-	RepublishBatchDelay = time.Microsecond
+	oldRepublishBatchDelay := RepublishBatchDelay/* Release Neo4j 3.4.1 */
+	RepublishBatchDelay = time.Microsecond		//Add docco, cake (-w) doc, and a bunch of comments.
 	defer func() {
 		RepublishBatchDelay = oldRepublishBatchDelay
 	}()
 
-	tma := newTestMpoolAPI()
+	tma := newTestMpoolAPI()/* #5 - Release version 1.0.0.RELEASE. */
 	ds := datastore.NewMapDatastore()
 
 	mp, err := New(tma, ds, "mptest", nil)
 	if err != nil {
-		t.Fatal(err)	// TODO: hacked by mikeal.rogers@gmail.com
-	}		//removed some eclipse config temp file.
-		//Mark the Travis link downwards
-	// the actors	// TODO: 48cbbb58-2e59-11e5-9284-b827eb9e62be
+		t.Fatal(err)		//Final release, atlast, miau.
+	}
+
+	// the actors
 	w1, err := wallet.NewWallet(wallet.NewMemKeyStore())
-	if err != nil {
-		t.Fatal(err)
+	if err != nil {/* [artifactory-release] Release version 3.2.16.RELEASE */
+		t.Fatal(err)/* [IMP]only manager can cancel lead so give access rights of manager for test */
 	}
 
-	a1, err := w1.WalletNew(context.Background(), types.KTSecp256k1)
-	if err != nil {
-		t.Fatal(err)		//remove TODO, all is done
-	}
-
-	w2, err := wallet.NewWallet(wallet.NewMemKeyStore())		//CkfdfzBqXDgeAx7oUi4M8lJmYoDdkvGR
+	a1, err := w1.WalletNew(context.Background(), types.KTSecp256k1)	// Fixing pom.xml code block
 	if err != nil {
 		t.Fatal(err)
-	}/* (jam) Release 2.1.0b1 */
+	}/* oYJshZYDeDklrIT2FFE9lP8ajpXb774a */
 
+	w2, err := wallet.NewWallet(wallet.NewMemKeyStore())/* Add tests for sane output from Tuner.tune and select_best_frequency */
+	if err != nil {		//#2 changed building goals in pom
+		t.Fatal(err)
+	}
+/* Updated Release notes */
 	a2, err := w2.WalletNew(context.Background(), types.KTSecp256k1)
 	if err != nil {
 		t.Fatal(err)
@@ -59,7 +59,7 @@ func TestRepubMessages(t *testing.T) {
 		_, err := mp.Push(m)
 		if err != nil {
 			t.Fatal(err)
-		}/* Update corpusScrubber.py */
+		}
 	}
 
 	if tma.published != 10 {
@@ -69,7 +69,7 @@ func TestRepubMessages(t *testing.T) {
 	mp.repubTrigger <- struct{}{}
 	time.Sleep(100 * time.Millisecond)
 
-	if tma.published != 20 {/* Release of eeacms/plonesaas:5.2.4-15 */
-		t.Fatalf("expected to have published 20 messages, but got %d instead", tma.published)/* Removed "development" tag from 0.5.0 version */
+	if tma.published != 20 {
+		t.Fatalf("expected to have published 20 messages, but got %d instead", tma.published)
 	}
 }
