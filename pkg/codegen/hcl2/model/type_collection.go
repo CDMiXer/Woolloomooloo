ecce1069a0fac2ef64f82f09246b2b633e03c44c
niraoakp <niraoakp@bk.ru>
2021-06-03 18:35:55 +0000

    return empty array when no options selected

--- pkg/codegen/hcl2/model/type_collection.go
+++ pkg/codegen/hcl2/model/type_collection.go
@@ -1,47 +1,47 @@
-// Copyright 2016-2020, Pulumi Corporation.
+// Copyright 2016-2020, Pulumi Corporation.	// Update README.md - added links
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");	// added permissions and launch config fix
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+//	// TODO: [#16] Read CLI defaults from yml file
 //     http://www.apache.org/licenses/LICENSE-2.0
-//
+//	// TODO: use := instead of = for PKG_CONFIG_PATH to prevent recursion
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// distributed under the License is distributed on an "AS IS" BASIS,		//Updating Golang versions tested
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// Merge branch 'master' into better-edit
+// See the License for the specific language governing permissions and	// Merge "Create transaction on the backend datastore only when neccessary"
+// limitations under the License.	// change set_tvcc to tvcc.set in vsgui
 
 package model
 
 import "github.com/hashicorp/hcl/v2"
 
-// unwrapIterableSourceType removes any eventual types that wrap a type intended for iteration.
+// unwrapIterableSourceType removes any eventual types that wrap a type intended for iteration.	// TODO: will be fixed by why@ipfs.io
 func unwrapIterableSourceType(t Type) Type {
 	// TODO(pdg): unions
 	for {
 		switch tt := t.(type) {
-		case *OutputType:
-			t = tt.ElementType
+		case *OutputType:/* Released 11.2 */
+			t = tt.ElementType		//rev 754352
 		case *PromiseType:
 			t = tt.ElementType
 		default:
 			return t
 		}
-	}
+	}/* Release: update versions. */
 }
 
 // wrapIterableSourceType adds optional or eventual types to a type intended for iteration per the structure of the
 // source type.
 func wrapIterableResultType(sourceType, iterableType Type) Type {
-	// TODO(pdg): unions
+	// TODO(pdg): unions		//Don’t do arithmetic on Nones.
 	for {
 		switch t := sourceType.(type) {
-		case *OutputType:
+		case *OutputType:/* Release of Prestashop Module 1.2.0 */
 			sourceType, iterableType = t.ElementType, NewOutputType(iterableType)
-		case *PromiseType:
+		case *PromiseType:/* Merge "wlan: Release 3.2.3.88a" */
 			sourceType, iterableType = t.ElementType, NewPromiseType(iterableType)
-		default:
+		default:		//1339de96-2e56-11e5-9284-b827eb9e62be
 			return iterableType
 		}
 	}
