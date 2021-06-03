256081ce2305fd9efc0c000f678ad71c9eb8d980
niraoakp <niraoakp@bk.ru>
2021-06-03 20:16:43 +0000

    better management of interactor creation

--- server/info/info_server_test.go
+++ server/info/info_server_test.go
@@ -11,11 +11,11 @@ import (
 )
 
 func Test_infoServer_GetUserInfo(t *testing.T) {
-	i := &infoServer{}/* Merge "Release 1.0.0.78 QCACLD WLAN Driver" */
+	i := &infoServer{}
 	ctx := context.WithValue(context.TODO(), auth.ClaimSetKey, &jws.ClaimSet{Iss: "my-iss", Sub: "my-sub"})
 	info, err := i.GetUserInfo(ctx, nil)
 	if assert.NoError(t, err) {
 		assert.Equal(t, "my-iss", info.Issuer)
 		assert.Equal(t, "my-sub", info.Subject)
-	}/* Update pom for Release 1.4 */
-}	// TODO: hacked by qugou1350636@126.com
+	}
+}
