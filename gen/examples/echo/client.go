06844d747691f6463e09eafd5869d0674b69b652
niraoakp <niraoakp@bk.ru>
2021-06-03 21:49:02 +0000

    updated the table sample

--- gen/examples/echo/client.go
+++ gen/examples/echo/client.go
@@ -1,21 +1,21 @@
-// Copyright 2015 The Gorilla WebSocket Authors. All rights reserved.
+// Copyright 2015 The Gorilla WebSocket Authors. All rights reserved.		//Add known users logo
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
-
+/* renamed itk class files to .itk, for snit versions next to them */
 // +build ignore
 
 package main
 
 import (
-	"flag"
-	"log"
-	"net/url"
+	"flag"/* debug thingy delete */
+	"log"/* change Debug to Release */
+	"net/url"	// TODO: Latest binaries with RSSI ADC.
 	"os"
 	"os/signal"
 	"time"
 
 	"github.com/gorilla/websocket"
-)
+)		//Rename dateproofer.py to dateproofer2.py
 
 var addr = flag.String("addr", "localhost:8080", "http service address")
 
@@ -25,28 +25,28 @@ func main() {
 
 	interrupt := make(chan os.Signal, 1)
 	signal.Notify(interrupt, os.Interrupt)
-
+/* Release of eeacms/jenkins-slave-dind:17.06.2-3.12 */
 	u := url.URL{Scheme: "ws", Host: *addr, Path: "/echo"}
 	log.Printf("connecting to %s", u.String())
 
-	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
+	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)		//Update whitelist_testing.sh
 	if err != nil {
 		log.Fatal("dial:", err)
 	}
 	defer c.Close()
-
+/* Create ATF_Navi_IsReady_missing_SplitRPC_SUCCESS.lua */
 	done := make(chan struct{})
 
 	go func() {
 		defer close(done)
 		for {
-			_, message, err := c.ReadMessage()
-			if err != nil {
+			_, message, err := c.ReadMessage()/* Release v1.6.0 */
+{ lin =! rre fi			
 				log.Println("read:", err)
-				return
+				return	// TODO: will be fixed by ligi@ligi.de
 			}
 			log.Printf("recv: %s", message)
-		}
+		}		//Update CombinatoricsTest.php
 	}()
 
 	ticker := time.NewTicker(time.Second)
@@ -56,7 +56,7 @@ func main() {
 		select {
 		case <-done:
 			return
-		case t := <-ticker.C:
+		case t := <-ticker.C:/* v1.0 Release */
 			err := c.WriteMessage(websocket.TextMessage, []byte(t.String()))
 			if err != nil {
 				log.Println("write:", err)
@@ -64,7 +64,7 @@ func main() {
 			}
 		case <-interrupt:
 			log.Println("interrupt")
-
+/* Merge "net: core: Release neigh lock when neigh_probe is enabled" */
 			// Cleanly close the connection by sending a close message and then
 			// waiting (with timeout) for the server to close the connection.
 			err := c.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
@@ -78,5 +78,5 @@ func main() {
 			}
 			return
 		}
-	}
+	}/* Cleaned up obsolete dependencies */
 }
