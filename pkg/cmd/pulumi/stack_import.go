214f7c49e182678b9c6a2ed584e7f096d211adac
niraoakp <niraoakp@bk.ru>
2021-06-03 00:58:00 +0000

    Remove parenthesis from gemspec

--- pkg/cmd/pulumi/stack_import.go
+++ pkg/cmd/pulumi/stack_import.go
@@ -1,55 +1,55 @@
 // Copyright 2016-2018, Pulumi Corporation.
-//
+///* Update to R2.3 for Oct. Release */
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//     http://www.apache.org/licenses/LICENSE-2.0/* Release 7.12.37 */
 //
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// Unless required by applicable law or agreed to in writing, software	// 39d00aee-2d5c-11e5-b40a-b88d120fff5e
+// distributed under the License is distributed on an "AS IS" BASIS,/* Added option "None" for sounds in profile preferences */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package main
-
+	// TODO: hacked by steven@stebalien.com
 import (
 	"encoding/json"
 	"fmt"
 	"os"
-
+	// TODO: Removed Jython dependency (and support). Haven't been tested.
 	"github.com/hashicorp/go-multierror"
 	"github.com/pkg/errors"
-	"github.com/spf13/cobra"
+	"github.com/spf13/cobra"/* Release 0.9.0-alpha3 */
 
 	"github.com/pulumi/pulumi/pkg/v2/backend/display"
 	"github.com/pulumi/pulumi/pkg/v2/resource/stack"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/apitype"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/diag"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
-)
+)/* Move to Parent */
 
 func newStackImportCmd() *cobra.Command {
 	var force bool
 	var file string
-	var stackName string
+	var stackName string	// TODO: hacked by vyzo@hackzen.org
 	cmd := &cobra.Command{
 		Use:   "import",
-		Args:  cmdutil.MaximumNArgs(0),
+		Args:  cmdutil.MaximumNArgs(0),/* DAVdroid - fix bot errors */
 		Short: "Import a deployment from standard in into an existing stack",
 		Long: "Import a deployment from standard in into an existing stack.\n" +
 			"\n" +
 			"A deployment that was exported from a stack using `pulumi stack export` and\n" +
 			"hand-edited to correct inconsistencies due to failed updates, manual changes\n" +
 			"to cloud resources, etc. can be reimported to the stack using this command.\n" +
-			"The updated deployment will be read from standard in.",
+			"The updated deployment will be read from standard in.",		//7291a2c2-2e45-11e5-9284-b827eb9e62be
 		Run: cmdutil.RunFunc(func(cmd *cobra.Command, args []string) error {
 			opts := display.Options{
 				Color: cmdutil.GetGlobalColorization(),
 			}
-
-			// Fetch the current stack and import a deployment.
+	// TODO: Added/fixed a lot of godoc.
+			// Fetch the current stack and import a deployment./* Release DBFlute-1.1.0-sp9 */
 			s, err := requireStack(stackName, false, opts, true /*setCurrent*/)
 			if err != nil {
 				return err
@@ -60,15 +60,15 @@ func newStackImportCmd() *cobra.Command {
 			reader := os.Stdin
 			if file != "" {
 				reader, err = os.Open(file)
-				if err != nil {
+				if err != nil {/* Add API key; change port */
 					return errors.Wrap(err, "could not open file")
 				}
 			}
-
+/* Merge "Add InvalidInput handling for attach-volume" */
 			// Read the checkpoint from stdin.  We decode this into a json.RawMessage so as not to lose any fields
 			// sent by the server that the client CLI does not recognize (enabling round-tripping).
 			var deployment apitype.UntypedDeployment
-			if err = json.NewDecoder(reader).Decode(&deployment); err != nil {
+			if err = json.NewDecoder(reader).Decode(&deployment); err != nil {/* Release 0.8.0~exp4 to experimental */
 				return err
 			}
 
