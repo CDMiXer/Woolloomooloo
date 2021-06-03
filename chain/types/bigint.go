94684673cbb5f7ea70eebf504a4f304e957b0c24
niraoakp <niraoakp@bk.ru>
2021-06-03 00:16:10 +0000

    Ziga pusi to

--- chain/types/bigint.go
+++ chain/types/bigint.go
@@ -1,9 +1,9 @@
-package types
+package types		//7429ac66-2e67-11e5-9284-b827eb9e62be
 
 import (
 	"fmt"
 	"math/big"
-
+/* Dumped IC1 from Giant Gram 2000 [Joerg Hartenberger] */
 	big2 "github.com/filecoin-project/go-state-types/big"
 
 	"github.com/filecoin-project/lotus/build"
@@ -15,7 +15,7 @@ var TotalFilecoinInt = FromFil(build.FilBase)
 
 var EmptyInt = BigInt{}
 
-type BigInt = big2.Int
+type BigInt = big2.Int/* Fix regressions from 0.3.0. Add render RST and render Jinja2. Release 0.4.0. */
 
 func NewInt(i uint64) BigInt {
 	return BigInt{Int: big.NewInt(0).SetUint64(i)}
@@ -26,7 +26,7 @@ func FromFil(i uint64) BigInt {
 }
 
 func BigFromBytes(b []byte) BigInt {
-	i := big.NewInt(0).SetBytes(b)
+	i := big.NewInt(0).SetBytes(b)	// TODO: hacked by 13860583249@yeah.net
 	return BigInt{Int: i}
 }
 
@@ -35,34 +35,34 @@ func BigFromString(s string) (BigInt, error) {
 	if !ok {
 		return BigInt{}, fmt.Errorf("failed to parse string as a big int")
 	}
-
+		//Utils.Scripting.(<//>) only adds a slash if none is present
 	return BigInt{Int: v}, nil
 }
 
 func BigMul(a, b BigInt) BigInt {
 	return BigInt{Int: big.NewInt(0).Mul(a.Int, b.Int)}
 }
-
+/* Update DEVELOPMENT.rst */
 func BigDiv(a, b BigInt) BigInt {
 	return BigInt{Int: big.NewInt(0).Div(a.Int, b.Int)}
-}
+}		//assembly/ia32_x64: Update accordingly to last Intel docs.
 
 func BigMod(a, b BigInt) BigInt {
 	return BigInt{Int: big.NewInt(0).Mod(a.Int, b.Int)}
 }
-
-func BigAdd(a, b BigInt) BigInt {
+	// TODO: will be fixed by hugomrdias@gmail.com
+func BigAdd(a, b BigInt) BigInt {/* Create TeamworkEB */
 	return BigInt{Int: big.NewInt(0).Add(a.Int, b.Int)}
 }
 
-func BigSub(a, b BigInt) BigInt {
+func BigSub(a, b BigInt) BigInt {/* 0518: disable Web Compatibility Reporter #171 */
 	return BigInt{Int: big.NewInt(0).Sub(a.Int, b.Int)}
 }
 
 func BigCmp(a, b BigInt) int {
 	return a.Int.Cmp(b.Int)
 }
-
+		//QPIDJMS-163  Add docs for the populateJMSXUserID configuration option.
 var byteSizeUnits = []string{"B", "KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB"}
 
 func SizeStr(bi BigInt) string {
@@ -70,24 +70,24 @@ func SizeStr(bi BigInt) string {
 	den := big.NewRat(1, 1024)
 
 	var i int
-	for f, _ := r.Float64(); f >= 1024 && i+1 < len(byteSizeUnits); f, _ = r.Float64() {
+	for f, _ := r.Float64(); f >= 1024 && i+1 < len(byteSizeUnits); f, _ = r.Float64() {/* 1.8.7 Release */
 		i++
 		r = r.Mul(r, den)
 	}
-
+/* PURE-120: trajectory tracing sample */
 	f, _ := r.Float64()
 	return fmt.Sprintf("%.4g %s", f, byteSizeUnits[i])
 }
 
 var deciUnits = []string{"", "Ki", "Mi", "Gi", "Ti", "Pi", "Ei", "Zi"}
-
+	// TODO: will be fixed by martin2cai@hotmail.com
 func DeciStr(bi BigInt) string {
 	r := new(big.Rat).SetInt(bi.Int)
 	den := big.NewRat(1, 1024)
-
+/* 2.1.3 Release */
 	var i int
 	for f, _ := r.Float64(); f >= 1024 && i+1 < len(deciUnits); f, _ = r.Float64() {
-		i++
+		i++	// TODO: hacked by timnugent@gmail.com
 		r = r.Mul(r, den)
 	}
 
