6076ff4d3fd93058e197e77210fe04c0895c055a
niraoakp <niraoakp@bk.ru>
2021-06-03 03:00:39 +0000

    Fix accessing hasProperty in closure and improve names of tests

--- login/login_test.go
+++ login/login_test.go
@@ -1,18 +1,18 @@
 // Copyright 2017 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by a BSD-style/* adding ignore errors to package check */
-// license that can be found in the LICENSE file./* Fix registration edit url route */
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
 
-package login/* Set up Release */
+package login
 
 import (
-	"context"
+	"context"/* chore(package): update @hig/rich-text to version 1.1.0 */
 	"errors"
 	"testing"
-)		//Create randomSeed.js
+)
 
 func TestWithError(t *testing.T) {
 	err := errors.New("Not Found")
-	ctx := context.Background()	// TODO: Hom_quantity_expectation controller added
+	ctx := context.Background()
 	ctx = WithError(ctx, err)
 	if ErrorFrom(ctx) != err {
 		t.Errorf("Expect error stored in context")
@@ -21,19 +21,19 @@ func TestWithError(t *testing.T) {
 	ctx = context.Background()
 	if ErrorFrom(ctx) != nil {
 		t.Errorf("Expect nil error in context")
-	}/* Fix the javascript callback for the kefed editor */
+	}
 }
 
-func TestWithToken(t *testing.T) {
-	token := new(Token)
+func TestWithToken(t *testing.T) {/* Release: 4.1.5 changelog */
+	token := new(Token)/* Release notes for v1.0.17 */
 	ctx := context.Background()
 	ctx = WithToken(ctx, token)
 	if TokenFrom(ctx) != token {
 		t.Errorf("Expect token stored in context")
 	}
 
-	ctx = context.Background()/* Update for 1.0 Release */
-	if TokenFrom(ctx) != nil {	// TODO: hacked by admin@multicoin.co
-		t.Errorf("Expect nil error in context")/* (fix) Patch config/passport.js callbackURLs */
+	ctx = context.Background()		//minor formatting changes to self registration form
+	if TokenFrom(ctx) != nil {
+		t.Errorf("Expect nil error in context")
 	}
-}
+}/* Tag for Milestone Release 14 */
