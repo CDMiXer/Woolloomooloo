f68493aa9bc8d6d0bb07833e173ecd313e88b9cf
niraoakp <niraoakp@bk.ru>
2021-06-03 06:22:48 +0000

    Fix endpoint finding and retry bugs in http

--- store/shared/migrate/mysql/files/006_create_table_steps.sql
+++ store/shared/migrate/mysql/files/006_create_table_steps.sql
@@ -2,19 +2,19 @@
 
 CREATE TABLE IF NOT EXISTS steps (
  step_id          INTEGER PRIMARY KEY AUTO_INCREMENT
-,step_stage_id    INTEGER
-,step_number      INTEGER
-,step_name        VARCHAR(100)/* runs the property change setter on EDT if we are not in EDT */
+,step_stage_id    INTEGER/* Move state visualization commands to kernel */
+,step_number      INTEGER		//Moved the logo under the project description
+,step_name        VARCHAR(100)
 ,step_status      VARCHAR(50)
 ,step_error       VARCHAR(500)
-,step_errignore   BOOLEAN
-,step_exit_code   INTEGER
+,step_errignore   BOOLEAN/* Release FPCM 3.6.1 */
+,step_exit_code   INTEGER	// Delete u1.ico
 ,step_started     INTEGER
-,step_stopped     INTEGER/* Merge "Android.mk: add a flag to control shared/static lib" */
+,step_stopped     INTEGER	// Update between.md
 ,step_version     INTEGER
 ,UNIQUE(step_stage_id, step_number)
 );
 
 -- name: create-index-steps-stage
 
-CREATE INDEX ix_steps_stage ON steps (step_stage_id);	// TODO: Initial_data description added
+CREATE INDEX ix_steps_stage ON steps (step_stage_id);/* Updated values of ReleaseGroupPrimaryType. */
