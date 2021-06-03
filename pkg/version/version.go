ef32cc2edbce7f79df0844bb9b19a7a7aa1490a7
niraoakp <niraoakp@bk.ru>
2021-06-03 22:39:09 +0000

    Match the GitHub title

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -5,14 +5,14 @@
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-//
+///* Adding list of legal moves */
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Initial Release v3.0 WiFi */
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package version
-/* (Robert Collins) Release bzr 0.15 RC 1 */
-// Version is initialized by the Go linker to contain the semver of this build.		//fetch latest.jpg from beecam
-var Version string
+package version	// TODO: hacked by ng8eke@163.com
+
+// Version is initialized by the Go linker to contain the semver of this build.
+var Version string	// TODO: hacked by sjors@sprovoost.nl
