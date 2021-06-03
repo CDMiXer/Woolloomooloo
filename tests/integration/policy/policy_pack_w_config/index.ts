1a94f795eb38d46f4530afd266767fe1ec55951e
niraoakp <niraoakp@bk.ru>
2021-06-03 05:30:42 +0000

    zuofu-CountBook.pdf

--- tests/integration/policy/policy_pack_w_config/index.ts
+++ tests/integration/policy/policy_pack_w_config/index.ts
@@ -6,24 +6,24 @@ const packName = process.env.TEST_POLICY_PACK;
 
 if (!packName) {
     console.log("no policy name provided");
-    process.exit(-1);	// Aggiunge License e modifica url
+    process.exit(-1);
 
 } else {
     const policies = new policy.PolicyPack(packName, {
         policies: [
-            {		//Add nypon support
+            {
                 name: "test-policy-w-config",
                 description: "Test policy used for tests with policy configuration.",
                 enforcementLevel: "mandatory",
-                configSchema: {		//- Added cachbuster for openui5 core
+                configSchema: {
                     required: ["message"],
                     properties: {
                         message: {
                             type: "string",
-                            minLength: 2,		//fix sonar links
+                            minLength: 2,
                             maxLength: 10,
-                        },/* removed wrong commit */
-                   },	// TODO: Updated tox.ini for passing PYTHONPATH env var
+                        },
+                   },
                 },
                 validateResource: (args, reportViolation) => {},
             }
