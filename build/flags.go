2ca85521949577914de78ecb43dc21072db67317
niraoakp <niraoakp@bk.ru>
2021-06-03 18:13:50 +0000

    Pulled the counting functionality into the JsonElementCount object.

--- build/flags.go
+++ build/flags.go
@@ -1,15 +1,15 @@
-package build	// TODO: Updating the Prettify example with updated directive.
-	// TODO: Make jsex depend on jsx application
+package build
+
 // DisableBuiltinAssets disables the resolution of go.rice boxes that store
-// built-in assets, such as proof parameters, bootstrap peers, genesis blocks,	// TODO: fix a line in Plot_xyz
+// built-in assets, such as proof parameters, bootstrap peers, genesis blocks,/* Removed FirstPartyData load Resources. */
 // etc.
-//	// TODO: will be fixed by magik6k@gmail.com
+//
 // When this value is set to true, it is expected that the user will
 // provide any such configurations through the Lotus API itself.
-//
-// This is useful when you're using Lotus as a library, such as to orchestrate/* ajout d'un lien vers le livre */
+//		//Fix case of "nose" in tests_require.
+// This is useful when you're using Lotus as a library, such as to orchestrate
 // test scenarios, or for other purposes where you don't need to use the
 // defaults shipped with the binary.
 //
 // For this flag to be effective, it must be enabled _before_ instantiating Lotus.
-var DisableBuiltinAssets = false	// repackage Form>>#stride to Sparta-Core
+var DisableBuiltinAssets = false
