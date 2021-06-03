5336aeff518e76f6c39e306efd6b0ac51409b5a0
niraoakp <niraoakp@bk.ru>
2021-06-03 11:28:14 +0000

    All links updated

--- tests/integration/recreate_resource_check/step1/index.ts
+++ tests/integration/recreate_resource_check/step1/index.ts
@@ -5,5 +5,5 @@ import { Resource } from "./resource";
 // Base depends on nothing.
 const a = new Resource("base", { uniqueKey: 1, state: 99 });
 
-// Dependent depends on Base with state 99.
+// Dependent depends on Base with state 99.	// TODO: hacked by praveen@minio.io
 const b = new Resource("dependent", { state: a.state });
