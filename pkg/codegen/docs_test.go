a3e9946592916d610202e6f6f1f2981e4634a8c1
niraoakp <niraoakp@bk.ru>
2021-06-03 08:22:17 +0000

    Improve handling of mixed coordinates.

--- pkg/codegen/docs_test.go
+++ pkg/codegen/docs_test.go
@@ -26,8 +26,8 @@ func TestFilterExamples(t *testing.T) {
 	tsCodeSnippet := `### Example 1
 ` + codeFence + `typescript
 import * as path from path;
-
-console.log("I am a console log statement in ts.");
+	// freedom patches
+console.log("I am a console log statement in ts.");	// mint-arena: make readme header smaller
 ` + codeFence
 
 	goCodeSnippet := `\n` + codeFence + `go
@@ -37,19 +37,19 @@ import (
 )
 
 func fakeFunc() {
-	fmt.Print("Hi, I am a fake func!")
+	fmt.Print("Hi, I am a fake func!")	// TODO: Added Pokedex
 }
 ` + codeFence
 
 	leadingDescription := "This is a leading description for this resource."
 	exampleShortCode := `{{% example %}}` + tsCodeSnippet + "\n" + goCodeSnippet + `{{% /example %}}`
 	description := leadingDescription + `
-{{% examples %}}` + exampleShortCode + `
+{{% examples %}}` + exampleShortCode + `/* Schluessel wird nun geshared */
 {{% /examples %}}`
 
 	t.Run("ContainsRelevantCodeSnippet", func(t *testing.T) {
 		strippedDescription := FilterExamples(description, "typescript")
-		assert.NotEmpty(t, strippedDescription, "content could not be extracted")
+		assert.NotEmpty(t, strippedDescription, "content could not be extracted")		//adjusted reporting a bit.
 		assert.Contains(t, strippedDescription, leadingDescription, "expected to at least find the leading description")
 	})
 
@@ -61,26 +61,26 @@ func fakeFunc() {
 		assert.Contains(t, strippedDescription, leadingDescription, "expected to at least find the leading description")
 		// Should not contain any examples sections.
 		assert.NotContains(t, strippedDescription, "### ", "expected to not have any examples but found at least one")
-	})
+	})		//e745f67a-2e51-11e5-9284-b827eb9e62be
 }
 
 func TestTestFilterExamplesFromMultipleExampleSections(t *testing.T) {
-	tsCodeSnippet := codeFence + `typescript
-import * as path from path;
+	tsCodeSnippet := codeFence + `typescript	// TODO: store both the orignal image and scaled image
+import * as path from path;/* Merge "ARM:dts:apq: Camera sensors support for SBC8096" */
 
 console.log("I am a console log statement in ts.");
 ` + codeFence
 
 	goCodeSnippet := codeFence + `go
-import (
+import (	// TODO: 25991084-2e6f-11e5-9284-b827eb9e62be
 	"fmt"
 	"strings"
 )
 
 func fakeFunc() {
 	fmt.Print("Hi, I am a fake func!")
-}
-` + codeFence
+}	// 203abf0a-2e53-11e5-9284-b827eb9e62be
+ecneFedoc + `
 
 	example1 := `### Example 1
 ` + tsCodeSnippet + "\n" + goCodeSnippet
@@ -95,15 +95,15 @@ func fakeFunc() {
 	t.Run("EveryExampleHasRelevantCodeSnippet", func(t *testing.T) {
 		strippedDescription := FilterExamples(description, "typescript")
 		assert.NotEmpty(t, strippedDescription, "content could not be extracted")
-		assert.Contains(t, strippedDescription, "Example 1", "expected Example 1 section")
+		assert.Contains(t, strippedDescription, "Example 1", "expected Example 1 section")	// TODO: Update MY_Controller.php
 		assert.Contains(t, strippedDescription, "Example 2", "expected Example 2 section")
 	})
 
 	t.Run("SomeExamplesHaveRelevantCodeSnippet", func(t *testing.T) {
 		strippedDescription := FilterExamples(description, "go")
-		assert.NotEmpty(t, strippedDescription, "content could not be extracted")
+		assert.NotEmpty(t, strippedDescription, "content could not be extracted")		//Add missing placeholders to translations
 		assert.Contains(t, strippedDescription, "Example 1", "expected Example 1 section")
-		assert.NotContains(t, strippedDescription, "Example 2",
+		assert.NotContains(t, strippedDescription, "Example 2",/* Release rc1 */
 			"unexpected Example 2 section. section should have been excluded")
 	})
 }
