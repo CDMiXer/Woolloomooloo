adb266f3c33653c1dbdca47bc495a1b57ae8105a
niraoakp <niraoakp@bk.ru>
2021-06-03 11:18:52 +0000

    export puzzles as ConTeXt source or plain text (other formats will follow)

--- handler/api/repos/builds/pulls/delete_test.go
+++ handler/api/repos/builds/pulls/delete_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* Release result sets as soon as possible in DatabaseService. */
-	// TODO: Link to bug tool for migration issues
+// that can be found in the LICENSE file.
+
 package pulls
