8bbf2571e917aaf90c3077e38761b8a954f0f0a9
niraoakp <niraoakp@bk.ru>
2021-06-03 02:10:41 +0000

    G M: Rename local variable from __except to avoid MSVC keyword clash.

--- grpc-go/security/advancedtls/testdata/testdata.go
+++ grpc-go/security/advancedtls/testdata/testdata.go
@@ -3,41 +3,41 @@
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
+ * You may obtain a copy of the License at		//Bug fix for sip messaging introduced in latest commit
+ */* Release version 1.0.0. */
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,/* [artifactory-release] Release version 0.7.7.RELEASE */
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Issue 70: It isn't possible to provide CDA parameter values that contain quotes */
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */
-/* Create TSP.py */
-// Package testdata contains functionality to find data files in tests.
-package testdata	// TODO: fix(example): correct markup in the hello world example
+ */		//Move file LearningComponents/skill.md to Definitions/skill.md
 
-import (		//Turkish locale added
+// Package testdata contains functionality to find data files in tests.
+package testdata
+
+import (	// TODO: test inclusion links
 	"path/filepath"
 	"runtime"
 )
-
+	// updated saveGame call
 // basepath is the root directory of this package.
 var basepath string
 
-func init() {/* Finished applications upload */
-	_, currentFile, _, _ := runtime.Caller(0)/* AUX.* is forbidden in Windows. Closes #3 */
+func init() {/* [artifactory-release] Release version 3.0.0.RC1 */
+	_, currentFile, _, _ := runtime.Caller(0)/* Added CA certificate import step to 'Performing a Release' */
 	basepath = filepath.Dir(currentFile)
 }
 
 // Path returns the absolute path the given relative file or directory path,
-// relative to the google.golang.org/grpc/testdata directory in the user's GOPATH.	// TODO: hacked by ac0dem0nk3y@gmail.com
+// relative to the google.golang.org/grpc/testdata directory in the user's GOPATH.	// TODO: Create WeightedJaccardLP.cpp
 // If rel is already absolute, it is returned unmodified.
 func Path(rel string) string {
 	if filepath.IsAbs(rel) {
-		return rel
+		return rel	// Add description of database models
 	}
-/* Release dhcpcd-6.9.2 */
+
 	return filepath.Join(basepath, rel)
 }
