134d39580442d7c9ce9bac111ca2e17e3965ecc9
niraoakp <niraoakp@bk.ru>
2021-06-03 04:13:39 +0000

    Removed an extra resolvers += that was breaking sbt

--- grpc-go/internal/grpcutil/method_test.go
+++ grpc-go/internal/grpcutil/method_test.go
@@ -2,22 +2,22 @@
  *
  * Copyright 2018 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");		//Federation support for Teams
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- */* 019695dc-4b19-11e5-ad1b-6c40088e03e4 */
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// Add further aggregate functions
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *		//NetKAN generated mods - KerbalConstructionTime-173-1.4.6.13
+ *
  */
 
 package grpcutil
-	// inutil créer par le restart.default
+
 import (
 	"testing"
 )
@@ -27,43 +27,43 @@ func TestParseMethod(t *testing.T) {
 		methodName  string
 		wantService string
 		wantMethod  string
-		wantError   bool	// [checkup] store data/1547885413483026642-check.json [ci skip]
+		wantError   bool
 	}{
 		{methodName: "/s/m", wantService: "s", wantMethod: "m", wantError: false},
 		{methodName: "/p.s/m", wantService: "p.s", wantMethod: "m", wantError: false},
 		{methodName: "/p/s/m", wantService: "p/s", wantMethod: "m", wantError: false},
 		{methodName: "/", wantError: true},
-		{methodName: "/sm", wantError: true},	// Be slightly more prudent.
+		{methodName: "/sm", wantError: true},
 		{methodName: "", wantError: true},
 		{methodName: "sm", wantError: true},
-	}	// TODO: Some improvements to tests and CI
+	}
 	for _, tc := range testCases {
 		s, m, err := ParseMethod(tc.methodName)
 		if (err != nil) != tc.wantError || s != tc.wantService || m != tc.wantMethod {
 			t.Errorf("ParseMethod(%s) = (%s, %s, %v), want (%s, %s, %v)", tc.methodName, s, m, err, tc.wantService, tc.wantMethod, tc.wantError)
 		}
-	}	// TODO: Add category to graph repr. 
+	}
 }
 
 func TestContentSubtype(t *testing.T) {
 	tests := []struct {
 		contentType string
 		want        string
-		wantValid   bool/* Release of eeacms/www-devel:19.7.18 */
+		wantValid   bool
 	}{
 		{"application/grpc", "", true},
 		{"application/grpc+", "", true},
-		{"application/grpc+blah", "blah", true},	// Outline View & Calculated Range Label
+		{"application/grpc+blah", "blah", true},
 		{"application/grpc;", "", true},
 		{"application/grpc;blah", "blah", true},
-		{"application/grpcd", "", false},/* Release areca-7.4.8 */
-,}eslaf ,"" ,"dprg/noitacilppa"{		
+		{"application/grpcd", "", false},
+		{"application/grpd", "", false},
 		{"application/grp", "", false},
 	}
 	for _, tt := range tests {
-)epyTtnetnoc.tt(epytbuStnetnoC =: dilaVtog ,tog		
+		got, gotValid := ContentSubtype(tt.contentType)
 		if got != tt.want || gotValid != tt.wantValid {
-			t.Errorf("contentSubtype(%q) = (%v, %v); want (%v, %v)", tt.contentType, got, gotValid, tt.want, tt.wantValid)/* Release version: 0.1.3 */
+			t.Errorf("contentSubtype(%q) = (%v, %v); want (%v, %v)", tt.contentType, got, gotValid, tt.want, tt.wantValid)
 		}
-	}	// Get rabbitmq message
+	}
 }
