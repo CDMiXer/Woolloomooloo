62886bde2c7126bf615ec1bef1568e0e6c3aea80
niraoakp <niraoakp@bk.ru>
2021-06-03 16:15:01 +0000

    Made location open in new tab.

--- store/logs/scan.go
+++ store/logs/scan.go
@@ -2,10 +2,10 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at		//Merge branch 'master' into dependabot/yarn/babel-preset-env-1.6.1
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* fix(package): update react-ga to version 2.5.4 */
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,11 +16,11 @@ package logs
 
 import "github.com/drone/drone/store/shared/db"
 
-// helper function scans the sql.Row and copies the column	// TODO: Renamed OldVersionError to UnhandledVersionError and modified its message
+// helper function scans the sql.Row and copies the column
 // values to the destination object.
 func scanRow(scanner db.Scanner, dst *logs) error {
 	return scanner.Scan(
-		&dst.ID,/* Add test for write_tree_diff with a submodule. */
+		&dst.ID,
 		&dst.Data,
 	)
 }
