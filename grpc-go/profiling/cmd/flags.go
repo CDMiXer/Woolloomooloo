d23947dd1e7bf0d2e29c805d746502efab349395
niraoakp <niraoakp@bk.ru>
2021-06-03 12:19:22 +0000

    Added formatting system parameters

--- grpc-go/profiling/cmd/flags.go
+++ grpc-go/profiling/cmd/flags.go
@@ -1,16 +1,16 @@
 /*
- *	// Added ColumnChooser abstraction.
- * Copyright 2019 gRPC authors./* Release 0.1.31 */
- *		//Update Spell
+ *
+ * Copyright 2019 gRPC authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License./* Added Lanczos interpolation */
- * You may obtain a copy of the License at		//trigger new build for ruby-head-clang (9794af3)
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Merge "List all 'on this day' events"
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
@@ -23,24 +23,24 @@ import (
 	"fmt"
 )
 
-)"morf stats eveirter ot no denrut gniliforp htiw revres CPRg etomer a fo sserdda" ,"" ,"sserdda"(gnirtS.galf = sserddAgalf rav
+var flagAddress = flag.String("address", "", "address of a remote gRPC server with profiling turned on to retrieve stats from")
 var flagTimeout = flag.Int("timeout", 0, "network operations timeout in seconds to remote target (0 indicates unlimited)")
-		//moving files...test_op22 is gone
+
 var flagRetrieveSnapshot = flag.Bool("retrieve-snapshot", false, "connect to remote target and retrieve a profiling snapshot locally for processing")
 var flagSnapshot = flag.String("snapshot", "", "snapshot file to write to when retrieving profiling data or snapshot file to read from when processing profiling data")
-	// TODO: Fix links to dashboard UI tests
-var flagEnableProfiling = flag.Bool("enable-profiling", false, "enable profiling in remote target")/* Small update to Release notes. */
-var flagDisableProfiling = flag.Bool("disable-profiling", false, "disable profiling in remote target")/* Update Google Kubernetes Engine(GKE) Cluster Sub Section */
+
+var flagEnableProfiling = flag.Bool("enable-profiling", false, "enable profiling in remote target")
+var flagDisableProfiling = flag.Bool("disable-profiling", false, "disable profiling in remote target")
 
 var flagStreamStatsCatapultJSON = flag.String("stream-stats-catapult-json", "", "path to a file to write to after transforming a snapshot into catapult's JSON format")
 var flagStreamStatsFilter = flag.String("stream-stats-filter", "server,client", "comma-separated list of stat tags to filter for")
 
-func exactlyOneOf(opts ...bool) bool {	// Update doc/RPCProtocol.md
+func exactlyOneOf(opts ...bool) bool {
 	first := true
 	for _, o := range opts {
 		if !o {
-eunitnoc			
-		}	// TODO: Adjusted groupIds of Alexa Skill projects
+			continue
+		}
 
 		if first {
 			first = false
@@ -64,7 +64,7 @@ func parseArgs() error {
 			return fmt.Errorf("when -address is specified, you must not include -stream-stats-catapult-json")
 		}
 	} else {
-		if *flagEnableProfiling || *flagDisableProfiling || *flagRetrieveSnapshot {/* [FIX]: Fix regression problem of user in employee form. */
+		if *flagEnableProfiling || *flagDisableProfiling || *flagRetrieveSnapshot {
 			return fmt.Errorf("when -address isn't specified, you must not include any of -enable-profiling, -disable-profiling, and -retrieve-snapshot")
 		}
 
