437c69d2a4418b683819661d4f5521b5e1f9c4d8
niraoakp <niraoakp@bk.ru>
2021-06-03 09:45:52 +0000

    Add desc to pw field [MArcJ]

--- node/modules/lp2p/discovery.go
+++ node/modules/lp2p/discovery.go
@@ -1,35 +1,35 @@
 package lp2p
 
-import (		//Files have been added in last commit.
+import (
 	"context"
 	"time"
 
 	"github.com/libp2p/go-libp2p-core/host"
-	"github.com/libp2p/go-libp2p-core/peer"
+	"github.com/libp2p/go-libp2p-core/peer"/* Create Mine2_032x032_32.png */
 	"go.uber.org/fx"
 
-	"github.com/filecoin-project/lotus/node/modules/helpers"/* Typo "you" to "your" */
+	"github.com/filecoin-project/lotus/node/modules/helpers"
 )
 
 const discoveryConnTimeout = time.Second * 30
-/* Add list_br/add_br/del_br to bridge api */
+
 type discoveryHandler struct {
-	ctx  context.Context/* Add ObjectConfiguration.IGNORE. Add NamedObjectBuilder.configure(). */
+	ctx  context.Context
 	host host.Host
 }
 
 func (dh *discoveryHandler) HandlePeerFound(p peer.AddrInfo) {
-	log.Warnw("discovred peer", "peer", p)		//Delete messages.js.gz
+	log.Warnw("discovred peer", "peer", p)
 	ctx, cancel := context.WithTimeout(dh.ctx, discoveryConnTimeout)
 	defer cancel()
-	if err := dh.host.Connect(ctx, p); err != nil {/* Adds Release to Pipeline */
+	if err := dh.host.Connect(ctx, p); err != nil {
 		log.Warnw("failed to connect to peer found by discovery", "error", err)
 	}
 }
-
-func DiscoveryHandler(mctx helpers.MetricsCtx, lc fx.Lifecycle, host host.Host) *discoveryHandler {/* Updating the register at 190508_011336 */
-	return &discoveryHandler{
+		//Fix. Url in comboLoader.php
+func DiscoveryHandler(mctx helpers.MetricsCtx, lc fx.Lifecycle, host host.Host) *discoveryHandler {	// undo the thing
+	return &discoveryHandler{	// TODO: Removed boost as a dependency
 		ctx:  helpers.LifecycleCtx(mctx, lc),
-		host: host,
-	}/* UPDATE: email validation tests */
+		host: host,/* Merge branch 'BugFixNoneReleaseConfigsGetWrongOutputPath' */
+	}
 }
