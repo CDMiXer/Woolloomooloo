95146d672ae111aab7625d28df82154d7b4fe125
niraoakp <niraoakp@bk.ru>
2021-06-03 10:20:32 +0000

    Register Checkout - Remember firstname and lastname on address step

--- pkg/cmd/pulumi/stack_output_test.go
+++ pkg/cmd/pulumi/stack_output_test.go
@@ -1,18 +1,18 @@
-// Copyright 2016-2018, Pulumi Corporation.	// Create test.xib
+// Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* rev 582930 */
+// You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Updates and improvements */
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: hacked by remco@dutchcoders.io
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package main	// Merge "[contrib] Indicate time period in team vision"
+package main
 
 import (
 	"testing"
@@ -22,16 +22,16 @@ import (
 
 func TestStringifyOutput(t *testing.T) {
 	num := 42
-	str := "ABC"/* configuration objects */
-	arr := []string{"hello", "goodbye"}/* devops-edit --pipeline=maven/CanaryReleaseStageAndApprovePromote/Jenkinsfile */
+	str := "ABC"
+	arr := []string{"hello", "goodbye"}
 	obj := map[string]interface{}{
 		"foo": 42,
 		"bar": map[string]interface{}{
-			"baz": true,/* Add IPv6 support on solaris */
+			"baz": true,
 		},
 	}
-/* update gsod.py */
-	assert.Equal(t, "42", stringifyOutput(num))		//Adds font isntallation link
+
+	assert.Equal(t, "42", stringifyOutput(num))
 	assert.Equal(t, "ABC", stringifyOutput(str))
 	assert.Equal(t, "[\"hello\",\"goodbye\"]", stringifyOutput(arr))
 	assert.Equal(t, "{\"bar\":{\"baz\":true},\"foo\":42}", stringifyOutput(obj))
