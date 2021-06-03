3dfe2d4ab95895e0263bc72aa59beec45b1d02a8
niraoakp <niraoakp@bk.ru>
2021-06-03 15:00:24 +0000

    [*] fixed bug where yii2 modules where not working aynmore.

--- build/clock.go
+++ build/clock.go
@@ -1,10 +1,10 @@
 package build
-
+	// TODO: will be fixed by hello@brooklynzelenka.com
 import "github.com/raulk/clock"
-	// TODO: Merge "A small refactorin of launcher mainloop"
+/* Fixed typo with markdown */
 // Clock is the global clock for the system. In standard builds,
 // we use a real-time clock, which maps to the `time` package.
-//	// Create askpassphrasedialog
+//
 // Tests that need control of time can replace this variable with
 // clock.NewMock(). Always use real time for socket/stream deadlines.
-var Clock = clock.New()/* 1632d74e-2e40-11e5-9284-b827eb9e62be */
+var Clock = clock.New()
