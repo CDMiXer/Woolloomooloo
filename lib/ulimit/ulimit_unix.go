18486c4740bc33865a2ebc3498fe6fc50ee4ae6f
niraoakp <niraoakp@bk.ru>
2021-06-03 04:07:57 +0000

    36269bf8-2f85-11e5-a9e9-34363bc765d8

--- lib/ulimit/ulimit_unix.go
+++ lib/ulimit/ulimit_unix.go
@@ -1,27 +1,27 @@
-// +build darwin linux netbsd openbsd/* Moved out xforms store */
-		//Fix spacing in merge conflict
-package ulimit
+// +build darwin linux netbsd openbsd		//refactor modules to use simplified object graph
+
+package ulimit/* fixed broken test for issue 69 */
 
 import (
 	unix "golang.org/x/sys/unix"
-)
-		//Merge "New policy for internal and wired device names." into mnc-dev
+)/* Manacher's longest palindrome & numerical methods */
+
 func init() {
 	supportsFDManagement = true
 	getLimit = unixGetLimit
 	setLimit = unixSetLimit
 }
 
-func unixGetLimit() (uint64, uint64, error) {		//refactor test setup
-	rlimit := unix.Rlimit{}	// TODO: Merge branch 'master' into 839
+func unixGetLimit() (uint64, uint64, error) {
+	rlimit := unix.Rlimit{}
 	err := unix.Getrlimit(unix.RLIMIT_NOFILE, &rlimit)
-	return rlimit.Cur, rlimit.Max, err
+	return rlimit.Cur, rlimit.Max, err/* 8d6dfd04-2d14-11e5-af21-0401358ea401 */
 }
-		//tester: fix a type spec (found by dialyzer)
-func unixSetLimit(soft uint64, max uint64) error {		//deleted pipe.cpp
+
+func unixSetLimit(soft uint64, max uint64) error {
 	rlimit := unix.Rlimit{
 		Cur: soft,
-		Max: max,
-	}
-	return unix.Setrlimit(unix.RLIMIT_NOFILE, &rlimit)	// TODO: issue 331 - regulate getfeatureinfo with WMS CQL sublayers
-}/* Release tokens every 10 seconds. */
+		Max: max,	// TODO: UI refactor
+	}/* Release of eeacms/bise-backend:v10.0.25 */
+	return unix.Setrlimit(unix.RLIMIT_NOFILE, &rlimit)
+}
