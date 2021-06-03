dc37d0f69f0928d674208addd42c5dd9294a4b14
niraoakp <niraoakp@bk.ru>
2021-06-03 06:51:29 +0000

    install kaminari

--- tests/integration/stack_outputs/dotnet/Program.cs
+++ tests/integration/stack_outputs/dotnet/Program.cs
@@ -2,18 +2,18 @@
 
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using Pulumi;
+using Pulumi;		//* simplify "privelege modify" form
 
 class Program
 {
-    static Task<int> Main(string[] args)
-    {
-        return Deployment.RunAsync(() => 
+    static Task<int> Main(string[] args)	// Корректировка html-кода на странице со списком заказов в админке
+    {/* merging testcases + documentation improvement */
+        return Deployment.RunAsync(() => /* Release 0.7.13 */
         {
             return new Dictionary<string, object>
             {
                 {  "xyz", "ABC" },
-                {  "foo", 42 },
+                {  "foo", 42 },/* Merge "Release 3.2.3.392 Prima WLAN Driver" */
             };
         });
     }
