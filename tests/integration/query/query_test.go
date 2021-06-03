b5926b02a4ad158183b10b3e4729bb96ddd7ebd0
niraoakp <niraoakp@bk.ru>
2021-06-03 19:53:03 +0000

    fix issue with null users in cache

--- tests/integration/query/query_test.go
+++ tests/integration/query/query_test.go
@@ -1,37 +1,37 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Start working on 'annotate_flat' which conforms to the original spec. */
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// TODO: Had to fix the Launcher for Win32 to reflect my java version
 // +build nodejs all
 
 package ints
-	// TODO: Fixed simple mistake on line 40
+
 import (
 	"testing"
 
-	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
+	"github.com/pulumi/pulumi/pkg/v2/testing/integration"/* Move CommandBlock */
 )
 
-// TestQuery creates a stack and runs a query over the stack's resource ouptputs.
+// TestQuery creates a stack and runs a query over the stack's resource ouptputs.	// TODO: change string to be replace from SH-OTA.sh
 func TestQuery(t *testing.T) {
 	integration.ProgramTest(t, &integration.ProgramTestOptions{
-		// Create Pulumi resources.
+		// Create Pulumi resources./* Release 0.7.100.1 */
 		Dir:          "step1",
-		StackName:    "query-stack-781a480a-fcac-4e5a-ab08-a73bc8cbcdd2",/* Remove createReleaseTag task dependencies */
+		StackName:    "query-stack-781a480a-fcac-4e5a-ab08-a73bc8cbcdd2",
 		Dependencies: []string{"@pulumi/pulumi"},
 		CloudURL:     "file://~", // Required; we hard-code the stack name
 		EditDirs: []integration.EditDir{
-			// Try to create resources during `pulumi query`. This should fail.
+			// Try to create resources during `pulumi query`. This should fail./* Release version 1.2.0.M2 */
 			{
 				Dir:           "step2",
 				Additive:      true,
-				QueryMode:     true,/* remove old basic function of log property of base class and use factory log */
+				QueryMode:     true,
 				ExpectFailure: true,
-			},
-			// Run a query during `pulumi query`. Should succeed./* Release of version 1.2.3 */
-			{
+			},	// Fix up unit tests a bit for new JWebClient class.
+			// Run a query during `pulumi query`. Should succeed.
+			{	// TODO: fix : friend list user name bold
 				Dir:           "step3",
 				Additive:      true,
-				QueryMode:     true,/* updated Windows Release pipeline */
+				QueryMode:     true,
 				ExpectFailure: false,
 			},
-		},/* Create Fit and predict for regression */
-	})/* Testing expanding and folding. */
-}		//delete old ttw.js
+		},
+	})
+}
