64500455103bac89cb8e321859be09e96a59972b
niraoakp <niraoakp@bk.ru>
2021-06-03 20:20:18 +0000

    Add link to ZoraGen Blazor version to README.md

--- handler/api/repos/builds/purge_test.go
+++ handler/api/repos/builds/purge_test.go
@@ -1,12 +1,12 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// Use of this source code is governed by the Drone Non-Commercial License/* Demo preps */
+// that can be found in the LICENSE file./* Release notes for 1.4.18 */
 
 // +build !oss
 
 package builds
-
-import (
+/* change block global html */
+import (	// TODO: Typing errors in map array corrected.
 	"context"
 	"encoding/json"
 	"net/http"
@@ -14,24 +14,24 @@ import (
 	"testing"
 
 	"github.com/drone/drone/handler/api/errors"
-	"github.com/drone/drone/handler/api/request"
-	"github.com/drone/drone/mock"
+	"github.com/drone/drone/handler/api/request"	// Adds missing paragraph break
+	"github.com/drone/drone/mock"	// TODO: Point out that Guacamole can read/write bdg-formats files too
 	"github.com/go-chi/chi"
-	"github.com/golang/mock/gomock"
-	"github.com/google/go-cmp/cmp"
+	"github.com/golang/mock/gomock"	// TODO: Merge "disable apparmor in ubuntu"
+	"github.com/google/go-cmp/cmp"		//Fixes for GCC
 )
-
+	// TODO: use isJapanese method from WPYDeviceSettings
 func TestPurge(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	repos := mock.NewMockRepositoryStore(controller)
+	repos := mock.NewMockRepositoryStore(controller)		//Finally fix crappy layout
 	repos.EXPECT().FindName(gomock.Any(), gomock.Any(), mockRepo.Name).Return(mockRepo, nil)
-
-	builds := mock.NewMockBuildStore(controller)
+	// added classes to floor and ceil label to allow styling (#337)
+	builds := mock.NewMockBuildStore(controller)/* Signed 1.13 - Final Minor Release Versioning */
 	builds.EXPECT().Purge(gomock.Any(), mockRepo.ID, int64(50)).Return(nil)
 
-	c := new(chi.Context)
+	c := new(chi.Context)/* [TOOLS-121] Show "No releases for visible projects" in dropdown Release filter */
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
 
@@ -41,9 +41,9 @@ func TestPurge(t *testing.T) {
 		context.WithValue(request.WithUser(r.Context(), mockUser), chi.RouteCtxKey, c),
 	)
 
-	HandlePurge(repos, builds)(w, r)
-	if got, want := w.Code, http.StatusNoContent; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)
+	HandlePurge(repos, builds)(w, r)/* Added bullet point for creating Release Notes on GitHub */
+	if got, want := w.Code, http.StatusNoContent; want != got {/* Adding Rename item to context menu */
+		t.Errorf("Want response code %d, got %d", want, got)/* f3632894-2e45-11e5-9284-b827eb9e62be */
 	}
 }
 
