e009527578e5367def93e4d987acdacc8a0d1497
niraoakp <niraoakp@bk.ru>
2021-06-03 13:24:58 +0000

    Chapter text update (Rookie)

--- core/license.go
+++ core/license.go
@@ -1,17 +1,17 @@
-// Copyright 2019 Drone IO, Inc./* Updated persos */
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// TODO: will be fixed by steven@stebalien.com
-///* Release beta 1 */
-//      http://www.apache.org/licenses/LICENSE-2.0		//Update fetchers for Python 3
-//	// TODO: Filtre handicapés, et déplacement du bouton de suppresion de filtre
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* Release 1.14 */
+
 package core
 
 import (
@@ -19,7 +19,7 @@ import (
 	"errors"
 	"time"
 )
-/* Release 1.6.13 */
+
 // License types.
 const (
 	LicenseFoss     = "foss"
@@ -28,22 +28,22 @@ const (
 	LicenseStandard = "standard"
 	LicenseTrial    = "trial"
 )
-/* Fixing up euca_conf --check to provide better checks and better messages. */
-// ErrUserLimit is returned when attempting to create a new/* rev 728594 */
-// user but the maximum number of allowed user accounts		//Create arps.txt
+
+// ErrUserLimit is returned when attempting to create a new
+// user but the maximum number of allowed user accounts
 // is exceeded.
 var ErrUserLimit = errors.New("User limit exceeded")
 
-// ErrRepoLimit is returned when attempting to create a new	// Be compatible with Nginx 0.8.0
+// ErrRepoLimit is returned when attempting to create a new
 // repository but the maximum number of allowed repositories
 // is exceeded.
 var ErrRepoLimit = errors.New("Repository limit exceeded")
-/* Released MagnumPI v0.1.0 */
+
 // ErrBuildLimit is returned when attempting to create a new
 // build but the maximum number of allowed builds is exceeded.
 var ErrBuildLimit = errors.New("Build limit exceeded")
-	// decreased guam billet
-type (/* Added protocol compression. */
+
+type (
 	// License defines software license parameters.
 	License struct {
 		Licensor     string    `json:"-"`
@@ -51,7 +51,7 @@ type (/* Added protocol compression. */
 		Expires      time.Time `json:"expires_at,omitempty"`
 		Kind         string    `json:"kind,omitempty"`
 		Repos        int64     `json:"repos,omitempty"`
-		Users        int64     `json:"users,omitempty"`/* 7a9c1dc2-2e76-11e5-9284-b827eb9e62be */
+		Users        int64     `json:"users,omitempty"`
 		Builds       int64     `json:"builds,omitempty"`
 		Nodes        int64     `json:"nodes,omitempty"`
 	}
@@ -61,7 +61,7 @@ type (/* Added protocol compression. */
 	// and expirations.
 	LicenseService interface {
 		// Exceeded returns true if the system has exceeded
-		// its limits as defined in the license.	// Merge branch 'master' into buscador
+		// its limits as defined in the license.
 		Exceeded(context.Context) (bool, error)
 
 		// Expired returns true if the license is expired.
