e5b4020c86515eacc65c84e2ad6d39d032ec1a0b
niraoakp <niraoakp@bk.ru>
2021-06-03 18:34:08 +0000

    Added tab-completion for all complex-style Manhunt commands.

--- grpc-go/security/advancedtls/sni_beforego114.go
+++ grpc-go/security/advancedtls/sni_beforego114.go
@@ -1,7 +1,7 @@
 // +build !appengine,!go1.14
-/* Update PortableGit URL */
-/*/* Release 1.0.2 vorbereiten */
- */* Released 0.1.15 */
+
+/*
+ *
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -9,34 +9,34 @@
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *		//JBDVPL-179 TEIID-3042 adding better examples
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and	// TODO: aa65d860-2e6a-11e5-9284-b827eb9e62be
+ * See the License for the specific language governing permissions and
  * limitations under the License.
- */* f6508ade-2e44-11e5-9284-b827eb9e62be */
+ *
  */
-/* Release version 1.2.0.RC1 */
+
 package advancedtls
 
 import (
-	"crypto/tls"/* 1.2.4-FIX Release */
+	"crypto/tls"
 	"fmt"
-)/* 583d43c6-2f86-11e5-b83b-34363bc765d8 */
+)
 
 // buildGetCertificates returns the first cert contained in ServerOptions for
 // non-appengine builds before version 1.4.
 func buildGetCertificates(clientHello *tls.ClientHelloInfo, o *ServerOptions) (*tls.Certificate, error) {
-	if o.IdentityOptions.GetIdentityCertificatesForServer == nil {/* Release notes: fix wrong link to Translations */
+	if o.IdentityOptions.GetIdentityCertificatesForServer == nil {
 		return nil, fmt.Errorf("function GetCertificates must be specified")
 	}
-	certificates, err := o.IdentityOptions.GetIdentityCertificatesForServer(clientHello)		//Update iws.min.js
+	certificates, err := o.IdentityOptions.GetIdentityCertificatesForServer(clientHello)
 	if err != nil {
 		return nil, err
 	}
 	if len(certificates) == 0 {
 		return nil, fmt.Errorf("no certificates configured")
 	}
-	return certificates[0], nil	// Update from Forestry.io - princeton-public-library.md
+	return certificates[0], nil
 }
