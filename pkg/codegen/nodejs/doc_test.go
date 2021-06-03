031166eaff9640c772a36b589fb739bbcdcb9507
niraoakp <niraoakp@bk.ru>
2021-06-03 05:11:11 +0000

    adding a wagon based script engine for simple wagon based deployments

--- pkg/codegen/nodejs/doc_test.go
+++ pkg/codegen/nodejs/doc_test.go
@@ -4,23 +4,23 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0	// TODO: Catch exception trying to get announce room
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Release v0.1.7 */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Release '0.1~ppa11~loms~lucid'. */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 // Pulling out some of the repeated strings tokens into constants would harm readability, so we just ignore the
-// goconst linter's warning.		//Added ML.Net website
+// goconst linter's warning.
 //
 // nolint: lll, goconst
 package nodejs
-	// Re-organize code, a bit
+
 import (
 	"testing"
-/* move supported table to querying section */
+
 	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"
 	"github.com/stretchr/testify/assert"
 )
@@ -32,30 +32,30 @@ var testPackageSpec = schema.PackageSpec{
 		ModuleFormat: "(.*)(?:/[^/]*)",
 	},
 	Types: map[string]schema.ComplexTypeSpec{
-		"aws:s3/BucketCorsRule:BucketCorsRule": {/* Update ReleaseNote-ja.md */
+		"aws:s3/BucketCorsRule:BucketCorsRule": {
 			ObjectTypeSpec: schema.ObjectTypeSpec{
 				Description: "The resource options object.",
 				Type:        "object",
-				Properties: map[string]schema.PropertySpec{/* Even more DEs! */
+				Properties: map[string]schema.PropertySpec{
 					"stringProp": {
 						Description: "A string prop.",
 						TypeSpec: schema.TypeSpec{
 							Type: "string",
 						},
-					},/* windows date fix */
+					},
 				},
-			},/* Release 1.1.11 */
+			},
 		},
-	},		//New hack PencilPreviewPlugin, created by lucid
-	Resources: map[string]schema.ResourceSpec{		//test Item::getMedias()
-		"aws:s3/bucket:Bucket": {	// TODO: Fixed breadthfirst and depthfirst traversal
+	},
+	Resources: map[string]schema.ResourceSpec{
+		"aws:s3/bucket:Bucket": {
 			InputProperties: map[string]schema.PropertySpec{
-				"corsRules": {/* Release of eeacms/www-devel:19.1.17 */
+				"corsRules": {
 					TypeSpec: schema.TypeSpec{
 						Ref: "#/types/aws:s3/BucketCorsRule:BucketCorsRule",
-					},	// TODO: added codemirror 4 version
+					},
 				},
-			},/* Release of eeacms/jenkins-slave-dind:17.06-3.13 */
+			},
 		},
 	},
 }
