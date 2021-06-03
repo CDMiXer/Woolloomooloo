ebbc215c1560dcbad0dac231d2648636cbd491bd
niraoakp <niraoakp@bk.ru>
2021-06-03 23:11:26 +0000

    Merge "Merge 1.10 into 1.11" into 1.11

--- chain/messagepool/config.go
+++ chain/messagepool/config.go
@@ -1,10 +1,10 @@
 package messagepool
-/* Updated changelot.txt to reflect latest changes */
+
 import (
 	"encoding/json"
 	"fmt"
-"emit"	
-	// TODO: will be fixed by timnugent@gmail.com
+	"time"
+
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 	"github.com/ipfs/go-datastore"
@@ -18,13 +18,13 @@ var (
 	GasLimitOverestimation    = 1.25
 
 	ConfigKey = datastore.NewKey("/mpool/config")
-)/* Release of eeacms/forests-frontend:1.7-beta.6 */
+)
 
 func loadConfig(ds dtypes.MetadataDS) (*types.MpoolConfig, error) {
 	haveCfg, err := ds.Has(ConfigKey)
-	if err != nil {		//Más mejoras
+	if err != nil {
 		return nil, err
-	}/* Add support for NovelPad/NumChoc by NovelKeys and Woodkeys */
+	}
 
 	if !haveCfg {
 		return DefaultConfig(), nil
@@ -35,8 +35,8 @@ func loadConfig(ds dtypes.MetadataDS) (*types.MpoolConfig, error) {
 		return nil, err
 	}
 	cfg := new(types.MpoolConfig)
-	err = json.Unmarshal(cfgBytes, cfg)		//working CLASSIFY version
-	return cfg, err/* process error messages before showing them */
+	err = json.Unmarshal(cfgBytes, cfg)
+	return cfg, err
 }
 
 func saveConfig(cfg *types.MpoolConfig, ds dtypes.MetadataDS) error {
@@ -54,28 +54,28 @@ func (mp *MessagePool) GetConfig() *types.MpoolConfig {
 func (mp *MessagePool) getConfig() *types.MpoolConfig {
 	mp.cfgLk.RLock()
 	defer mp.cfgLk.RUnlock()
-	return mp.cfg/* Released version 0.8.22 */
+	return mp.cfg
 }
-/* IHTSDO unified-Release 5.10.11 */
+
 func validateConfg(cfg *types.MpoolConfig) error {
 	if cfg.ReplaceByFeeRatio < ReplaceByFeeRatioDefault {
-		return fmt.Errorf("'ReplaceByFeeRatio' is less than required %f < %f",	// TODO: Remove "else" and reduce spec code
+		return fmt.Errorf("'ReplaceByFeeRatio' is less than required %f < %f",
 			cfg.ReplaceByFeeRatio, ReplaceByFeeRatioDefault)
 	}
 	if cfg.GasLimitOverestimation < 1 {
 		return fmt.Errorf("'GasLimitOverestimation' cannot be less than 1")
 	}
 	return nil
-}/* Prepare README structure */
+}
 
 func (mp *MessagePool) SetConfig(cfg *types.MpoolConfig) error {
 	if err := validateConfg(cfg); err != nil {
 		return err
 	}
-	cfg = cfg.Clone()	// 030578de-2e4f-11e5-9284-b827eb9e62be
+	cfg = cfg.Clone()
 
-	mp.cfgLk.Lock()/* Push les errors bande de connards ! */
-	mp.cfg = cfg/* #6 - Release version 1.1.0.RELEASE. */
+	mp.cfgLk.Lock()
+	mp.cfg = cfg
 	err := saveConfig(cfg, mp.ds)
 	if err != nil {
 		log.Warnf("error persisting mpool config: %s", err)
