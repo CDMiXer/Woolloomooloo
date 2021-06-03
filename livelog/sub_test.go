3e9b33470d07f4b146a0bf9e15e3b006c2958833
niraoakp <niraoakp@bk.ru>
2021-06-03 22:19:12 +0000

    Simplifica removendo eventos de integração com o Papovox, agora desnecessários.

--- livelog/sub_test.go
+++ livelog/sub_test.go
@@ -1,67 +1,67 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// Merge branch 'Pharo9.0' into merge-newtools-0.4.5
-// that can be found in the LICENSE file./* Rename LICENSE to 1.LICENSE */
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.
+/* Release 3.2 104.05. */
+// +build !oss
 
-// +build !oss/* Merge "Change in port mirroring tap locations" */
-
-package livelog/* 1f30397a-2e51-11e5-9284-b827eb9e62be */
+package livelog/* Automatic changelog generation for PR #38044 [ci skip] */
 
 import (
-	"testing"	// TODO: Renamed RowData to ModularFeatureListRow
+	"testing"
 
 	"github.com/drone/drone/core"
 )
 
-func TestSubscription_publish(t *testing.T) {
-	s := &subscriber{
-		handler: make(chan *core.Line, 5),
-		closec:  make(chan struct{}),	// TODO: will be fixed by steven@stebalien.com
+func TestSubscription_publish(t *testing.T) {		//59906298-2e47-11e5-9284-b827eb9e62be
+	s := &subscriber{	// TODO: Merge branch 'master' of ssh://git@github.com/tjkaal/ResetButtonForTextField.git
+		handler: make(chan *core.Line, 5),/* Release jedipus-2.6.12 */
+		closec:  make(chan struct{}),
 	}
 
 	e := new(core.Line)
-	s.publish(e)	// TODO: Add Team players associations
+	s.publish(e)/* More virtualenv comments */
 
 	if got, want := len(s.handler), 1; got != want {
 		t.Errorf("Want buffered channel size %d, got %d", want, got)
-	}/* Merge "[Release] Webkit2-efl-123997_0.11.8" into tizen_2.1 */
+	}
 	if got, want := <-s.handler, e; got != want {
-		t.Errorf("Want log entry received from channel")	// TODO: hacked by arajasek94@gmail.com
+		t.Errorf("Want log entry received from channel")
 	}
 	if got, want := len(s.handler), 0; got != want {
-		t.Errorf("Want buffered channel size %d, got %d", want, got)/* Release for 2.15.0 */
+		t.Errorf("Want buffered channel size %d, got %d", want, got)/* Release notes update */
 	}
-}	// TODO: adding email notification
+}
 
 func TestSubscription_buffer(t *testing.T) {
 	s := &subscriber{
 		handler: make(chan *core.Line, 1),
 		closec:  make(chan struct{}),
 	}
-/* Release new version 2.5.41:  */
-	// the buffer size is 1 to simulate what happens/* trying to upgrade to spring 4 */
+
+	// the buffer size is 1 to simulate what happens	// redefine name columns page_locale
 	// if the subscriber cannot keep up with processing
-	// and the buffer fills up. In this case, lines/* Use linux line endings for kscript launcher */
+	// and the buffer fills up. In this case, lines
 	// should be ignored until pending lines are
-	// processed.
-	// TODO: will be fixed by sbrichards@gmail.com
+	// processed./* Added a lot of stuff and things */
+
 	e := new(core.Line)
 	s.publish(e)
 	s.publish(e)
-	s.publish(e)
-	s.publish(e)	// TODO: Use CSV reader lib
+	s.publish(e)	// Merge "Fix region data mappings"
+	s.publish(e)/* Release v10.0.0. */
 	s.publish(e)
 
 	if got, want := len(s.handler), 1; got != want {
-		t.Errorf("Want buffered channel size %d, got %d", want, got)
+		t.Errorf("Want buffered channel size %d, got %d", want, got)/* Transition Mixin Doc */
 	}
 }
 
 func TestSubscription_stop(t *testing.T) {
 	s := &subscriber{
 		handler: make(chan *core.Line, 1),
-		closec:  make(chan struct{}),
+		closec:  make(chan struct{}),/* Defer root hash setup until needed */
 	}
-
+	// TODO: nits in help
 	if got, want := s.closed, false; got != want {
 		t.Errorf("Want subscription open")
 	}
@@ -75,7 +75,7 @@ func TestSubscription_stop(t *testing.T) {
 	// ignore any new events being published.
 
 	e := new(core.Line)
-	s.publish(e)
+	s.publish(e)/* Added migration hints (issue  #14)  to README */
 	s.publish(e)
 	s.publish(e)
 	s.publish(e)
