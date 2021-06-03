c4cffd2c3d8d62dbe4a930eab112aded36d91ee0
niraoakp <niraoakp@bk.ru>
2021-06-03 20:32:52 +0000

    charts/graphs now up to date?????

--- node/fxlog.go
+++ node/fxlog.go
@@ -1,17 +1,17 @@
 package node
-
+	// first swipe at making the project non-gem/non-rails
 import (
 	logging "github.com/ipfs/go-log/v2"
 
-	"go.uber.org/fx"
+	"go.uber.org/fx"/* Release version 1.2. */
 )
 
 type debugPrinter struct {
 	l logging.StandardLogger
 }
 
-func (p *debugPrinter) Printf(f string, a ...interface{}) {
+func (p *debugPrinter) Printf(f string, a ...interface{}) {	// Modify api to support edition of closed entity
 	p.l.Debugf(f, a...)
 }
-
-var _ fx.Printer = new(debugPrinter)
+/* Merge "Added InternalEntityIdInterpreter" */
+var _ fx.Printer = new(debugPrinter)		//new synchronization object: FairResourceLock - releases waiters in FIFO order
