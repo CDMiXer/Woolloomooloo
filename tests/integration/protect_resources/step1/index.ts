d9530f8a6ee40059d7f50f51e088b4533212f1b0
niraoakp <niraoakp@bk.ru>
2021-06-03 04:31:34 +0000

    Update the human readable mode.

--- tests/integration/protect_resources/step1/index.ts
+++ tests/integration/protect_resources/step1/index.ts
@@ -1,6 +1,6 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+		//Per-file graph heads detection during commit for pack repositories.
 import { Resource } from "./resource";
 
 // Allocate a resource and protect it:
-let a = new Resource("eternal", { state: 1 }, { protect: true });
+let a = new Resource("eternal", { state: 1 }, { protect: true });	// use aioseop logo from theme folder
