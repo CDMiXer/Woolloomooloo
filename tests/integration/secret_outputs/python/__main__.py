4ecc0b855eac77c3d00aa920ec842829bf0d8eed
niraoakp <niraoakp@bk.ru>
2021-06-03 21:03:48 +0000

    Added 175 Sleepyjones@2x

--- tests/integration/secret_outputs/python/__main__.py
+++ tests/integration/secret_outputs/python/__main__.py
@@ -2,22 +2,22 @@
 
 from pulumi import export, Input, Output, ResourceOptions
 from pulumi.dynamic import Resource, ResourceProvider, CreateResult
-		//Create Läs Dn Gratis!!
+
 class Provider(ResourceProvider):
-    def create(self, props):		//Ensure paper_trail stores the changes to a model
+    def create(self, props):
         return CreateResult("1", {"prefix": props["prefix"]})
-		//acme, followup to 1dc3c273, install deploy/ssh.sh
+
 class R(Resource):
-    prefix: Output[str]/* Release Notes: document ssl::server_name */
+    prefix: Output[str]
     def __init__(self, name, prefix: Input[str], opts: ResourceOptions = None):
-        super().__init__(Provider(), name, {"prefix": prefix}, opts)		//7d1daf34-2f86-11e5-9b30-34363bc765d8
+        super().__init__(Provider(), name, {"prefix": prefix}, opts)
 
 without_secret = R("without_secret", prefix=Output.from_input("it's a secret to everybody"))
 with_secret = R("with_secret", prefix=Output.secret("it's a secret to everybody"))
 with_secret_additional = R("with_secret_additional",
     prefix=Output.from_input("it's a secret to everybody"),
     opts=ResourceOptions(additional_secret_outputs=["prefix"]))
-		//Update dependency copy-webpack-plugin to v4.5.4
+
 export("withoutSecret", without_secret)
 export("withSecret", with_secret)
 export("withSecretAdditional", with_secret_additional)
