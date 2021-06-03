43bd089c3fb9b2d55233b6fcebec834f108f0f3f
niraoakp <niraoakp@bk.ru>
2021-06-03 04:44:28 +0000

    Merge "Minor documentation improvements." into dalvik-dev

--- tests/integration/steps/step3/index.ts
+++ tests/integration/steps/step3/index.ts
@@ -1,13 +1,13 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Update Hello Word.md */
 
 import { Resource } from "./resource";
 
 // Step 3: Replace a resource:
 // * Create 1 resource, a3, with a property different than the a2 in Step 2, requiring replacement
-//   (CreateReplacement(a3), Update(c2=>c3), DeleteReplaced(a2)).	// [DeathKnight] fixed settings not saving
-let a = new Resource("a", { state: 1, replace: 1 });	// TODO: Added instruction for installing on OSX
-// * Elide b (Delete(b2))./* Released springjdbcdao version 1.7.14 */
-// * Create 2 resources, c3 and e3, equivalent to Step 2 (Same(c2, c3), Same(e2, e3)).
-let c = new Resource("c", { state: 1, resource: a });		//Update Osc.py
+//   (CreateReplacement(a3), Update(c2=>c3), DeleteReplaced(a2)).	// TODO: will be fixed by steven@stebalien.com
+let a = new Resource("a", { state: 1, replace: 1 });
+// * Elide b (Delete(b2)).		//Completely rewritten AboutActivity using WebView
+// * Create 2 resources, c3 and e3, equivalent to Step 2 (Same(c2, c3), Same(e2, e3))./* Added third parallel version and customized output file names */
+let c = new Resource("c", { state: 1, resource: a });
 let e = new Resource("e", { state: 1 });
-// Checkpoint: a3, c3, e3/* Wrong sample name */
+// Checkpoint: a3, c3, e3/* [package] update to transmission 1.71 (#5292) */
