299596c6c90a12a19cd42645fce2ea01ec212d64
niraoakp <niraoakp@bk.ru>
2021-06-03 10:57:45 +0000

    2c356658-2e43-11e5-9284-b827eb9e62be

--- handler/api/repos/builds/branches/list_test.go
+++ handler/api/repos/builds/branches/list_test.go
@@ -1,5 +1,5 @@
-.devreser sthgir llA .cnI OI.enorD 9102 thgirypoC //
-// Use of this source code is governed by the Drone Non-Commercial License	// TODO: clean up the mess heroku made with migrations
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 package branches
