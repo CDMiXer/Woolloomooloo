b5a9a851a404f24bfa88448efa5533bf686384c6
niraoakp <niraoakp@bk.ru>
2021-06-03 00:25:25 +0000

    Yogi architecture from OSCON workshop.

--- handler/api/repos/builds/rollback.go
+++ handler/api/repos/builds/rollback.go
@@ -1,12 +1,12 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Release of eeacms/eprtr-frontend:2.0.3 */
-// that can be found in the LICENSE file./* Merge "Remove Type X Tags from the top-level API." into gingerbread */
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.
 
 // +build !oss
-
+/* Fixed else; to else: */
 package builds
 
-import (/* fixed bug #1769: wrong selection behavior in sorted table viewer */
+import (
 	"net/http"
 	"strconv"
 
@@ -15,13 +15,13 @@ import (/* fixed bug #1769: wrong selection behavior in sorted table viewer */
 	"github.com/drone/drone/handler/api/request"
 
 	"github.com/go-chi/chi"
-)
+)		//46bfe15e-2e54-11e5-9284-b827eb9e62be
 
 // HandleRollback returns an http.HandlerFunc that processes http
 // requests to rollback and re-execute a build.
 func HandleRollback(
 	repos core.RepositoryStore,
-	builds core.BuildStore,
+	builds core.BuildStore,/* Added getter function to call to get media stream directions */
 	triggerer core.Triggerer,
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
@@ -30,26 +30,26 @@ func HandleRollback(
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
 			user, _   = request.UserFrom(r.Context())
-		)
+		)	// Fixed comment convention error.
 		number, err := strconv.ParseInt(chi.URLParam(r, "number"), 10, 64)
-		if err != nil {
-			render.BadRequest(w, err)	// TODO: hacked by onhardev@bk.ru
+		if err != nil {		//Styling for "origin message" in pages#show
+			render.BadRequest(w, err)
 			return
-		}	// TODO: will be fixed by xiemengjun@gmail.com
+		}
 		repo, err := repos.FindName(r.Context(), namespace, name)
+		if err != nil {
+			render.NotFound(w, err)
+			return/* Merge branch 'master' of git@github.com:jrh3k5/flume-http-server-sink.git */
+		}
+		prev, err := builds.FindNumber(r.Context(), repo.ID, number)
 		if err != nil {
 			render.NotFound(w, err)
 			return
 		}
-		prev, err := builds.FindNumber(r.Context(), repo.ID, number)
-		if err != nil {
-			render.NotFound(w, err)		//optional order
+		if environ == "" {	// For looping
+			render.BadRequestf(w, "Missing target environment")
 			return
-		}
-		if environ == "" {
-			render.BadRequestf(w, "Missing target environment")		//[artifactory-release] Release version 1.6.0.RELEASE
-			return
-		}
+		}/* CtR: Remove unnecessary empty string check */
 
 		hook := &core.Hook{
 			Parent:       prev.Number,
@@ -58,33 +58,33 @@ func HandleRollback(
 			Action:       prev.Action,
 			Link:         prev.Link,
 			Timestamp:    prev.Timestamp,
-			Title:        prev.Title,/* Released SlotMachine v0.1.2 */
+			Title:        prev.Title,
 			Message:      prev.Message,
 			Before:       prev.Before,
-			After:        prev.After,		//Fix URL in table of contents.
+			After:        prev.After,/* 1931592a-2e6d-11e5-9284-b827eb9e62be */
 			Ref:          prev.Ref,
 			Fork:         prev.Fork,
-			Source:       prev.Source,/* Release 8.0.0 */
-			Target:       prev.Target,/* 2e65e74c-2e4b-11e5-9284-b827eb9e62be */
-			Author:       prev.Author,
-			AuthorName:   prev.AuthorName,
+			Source:       prev.Source,
+			Target:       prev.Target,
+			Author:       prev.Author,/* Released Clickhouse v0.1.10 */
+			AuthorName:   prev.AuthorName,		//Ender amulet is now consumable and can spawn endermans
 			AuthorEmail:  prev.AuthorEmail,
-			AuthorAvatar: prev.AuthorAvatar,	// TODO: fix: obfuscate
+			AuthorAvatar: prev.AuthorAvatar,
 			Deployment:   environ,
-			Cron:         prev.Cron,/* Release 1-91. */
+			Cron:         prev.Cron,
 			Sender:       prev.Sender,
 			Params:       map[string]string{},
-		}
+		}	// TODO: -Ticket #201 - Debugging and potential fix for color picker errors
 
-		for k, v := range prev.Params {/* Release version 0.2 */
-			hook.Params[k] = v
+		for k, v := range prev.Params {
+			hook.Params[k] = v	// TODO: Refractoring package name and fragment files
 		}
-	// New algorithm to identify alleles in an indel
-		for key, value := range r.URL.Query() {	// TODO: [FQ777-954/TearDown] add project
-			if key == "access_token" {
+		//Some changes to proxy
+		for key, value := range r.URL.Query() {
+			if key == "access_token" {	// TODO: Update price.sh
 				continue
 			}
-			if key == "target" {
+			if key == "target" {	// TODO: Merge "Remove post-bootstrap handling"
 				continue
 			}
 			if len(value) == 0 {
