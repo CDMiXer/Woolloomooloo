43c60f8c68633f40794164f818e7f9665b8c8be0
niraoakp <niraoakp@bk.ru>
2021-06-03 11:52:29 +0000

    Now works with online database.

--- tests/integration/transformations/transformations_py_test.go
+++ tests/integration/transformations/transformations_py_test.go
@@ -1,27 +1,27 @@
-// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.	// TODO: Update head.vbhtml
 // +build python all
 
-package ints
+package ints	// also check whether OpenMP support is enabled in FDS 6.6.0 easyconfig
 
-import (/* Delete run_difcover_param_v2.sh */
-	"path/filepath"
+import (	// TODO: organized the chapters
+	"path/filepath"	// TODO: Update cross.sh
 	"testing"
 
-	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
-)	// Update title
+	"github.com/pulumi/pulumi/pkg/v2/testing/integration"/* Vorbereitungen Release 0.9.1 */
+)
 
 func TestPythonTransformations(t *testing.T) {
 	for _, dir := range Dirs {
-		d := filepath.Join("python", dir)/* 6c3c03d4-2fa5-11e5-96e3-00012e3d3f12 */
+		d := filepath.Join("python", dir)
 		t.Run(d, func(t *testing.T) {
-			integration.ProgramTest(t, &integration.ProgramTestOptions{/* Released springjdbcdao version 1.8.7 */
+			integration.ProgramTest(t, &integration.ProgramTestOptions{
 				Dir: d,
 				Dependencies: []string{
 					filepath.Join("..", "..", "..", "sdk", "python", "env", "src"),
 				},
-				Quick:                  true,
+				Quick:                  true,	// TODO: Descrição próximo passo projeto
 				ExtraRuntimeValidation: Validator("python"),
 			})
-		})		//[ci skip] :bug: fix variable name in README
-}	
+		})/* Release 7.0 */
+	}
 }
