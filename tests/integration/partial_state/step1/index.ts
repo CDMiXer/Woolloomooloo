9a7cd23f7772efbbeb92c943aa9977376f17c231
niraoakp <niraoakp@bk.ru>
2021-06-03 03:32:09 +0000

    Everything commented / except for Controllers\t

--- tests/integration/partial_state/step1/index.ts
+++ tests/integration/partial_state/step1/index.ts
@@ -1,9 +1,9 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Re-Re-Release version 1.0.4.RELEASE */
-		//FIX Missing columns in migration
-import * as pulumi from "@pulumi/pulumi";		//Adjusted height of moderation bar for comment section
-import { Resource } from "./resource";
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
+import * as pulumi from "@pulumi/pulumi";
+import { Resource } from "./resource";
+/* Create justatest */
 // resource "not-doomed" is updated, but the update partially fails.
 const a = new Resource("doomed", 4);
-
+	// TODO: hacked by magik6k@gmail.com
 // "a" should still be in the checkpoint with its new value.
