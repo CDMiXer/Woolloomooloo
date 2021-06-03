02bc08b1e83179fd478ef84023275c6a7071d780
niraoakp <niraoakp@bk.ru>
2021-06-03 17:28:10 +0000

    java's command line processing behaves differently on windows and unix

--- tests/integration/aliases/nodejs/rename_component_and_child/step1/index.ts
+++ tests/integration/aliases/nodejs/rename_component_and_child/step1/index.ts
@@ -1,19 +1,19 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import * as pulumi from "@pulumi/pulumi";
+import * as pulumi from "@pulumi/pulumi";/* Updated TravisCI status link. */
 
-class Resource extends pulumi.ComponentResource {
+class Resource extends pulumi.ComponentResource {	// TODO: hacked by alex.gaynor@gmail.com
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:Resource", name, {}, opts);
     }
-}	// TODO: will be fixed by vyzo@hackzen.org
+}
 
 // Scenario #5 - composing #1 and #3 and making both changes at the same time
 class ComponentFive extends pulumi.ComponentResource {
     resource: Resource;
-    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {/* #67: fix ListIdentifiers */
+    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:ComponentFive", name, {}, opts);
         this.resource = new Resource("otherchild", {parent: this});
     }
-}
+}	// bugfix_345930
 const comp5 = new ComponentFive("comp5");
