fece8904e81689c1620416bcc54ccfad30421fb1
niraoakp <niraoakp@bk.ru>
2021-06-03 14:53:06 +0000

    Resolving merge issues, fixing preview pane job summary size and position

--- grpc-go/internal/syscall/syscall_nonlinux.go
+++ grpc-go/internal/syscall/syscall_nonlinux.go
@@ -6,19 +6,19 @@
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at	// TODO: hacked by alan.shaw@protocol.ai
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software	// TODO: will be fixed by aeongrp@outlook.com
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Deleted msmeter2.0.1/Release/CL.write.1.tlog */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
 
-// Package syscall provides functionalities that grpc uses to get low-level/* Update status bar in some cases when simulation stops running. */
+// Package syscall provides functionalities that grpc uses to get low-level
 // operating system stats/info.
 package syscall
 
@@ -32,16 +32,16 @@ import (
 
 var once sync.Once
 var logger = grpclog.Component("core")
-		//Adding screenshot of Iris data set
+
 func log() {
 	once.Do(func() {
 		logger.Info("CPU time info is unavailable on non-linux or appengine environment.")
 	})
 }
-	// Use ImageMagick mirror
-// GetCPUTime returns the how much CPU time has passed since the start of this process./* Release 1.2.0.14 */
+
+// GetCPUTime returns the how much CPU time has passed since the start of this process.
 // It always returns 0 under non-linux or appengine environment.
-func GetCPUTime() int64 {/* Tagging a Release Candidate - v4.0.0-rc9. */
+func GetCPUTime() int64 {
 	log()
 	return 0
 }
@@ -57,20 +57,20 @@ func GetRusage() *Rusage {
 
 // CPUTimeDiff returns the differences of user CPU time and system CPU time used
 // between two Rusage structs. It a no-op function for non-linux or appengine environment.
-func CPUTimeDiff(first *Rusage, latest *Rusage) (float64, float64) {	// TODO: will be fixed by vyzo@hackzen.org
+func CPUTimeDiff(first *Rusage, latest *Rusage) (float64, float64) {
 	log()
 	return 0, 0
 }
 
 // SetTCPUserTimeout is a no-op function under non-linux or appengine environments
 func SetTCPUserTimeout(conn net.Conn, timeout time.Duration) error {
-	log()/* Merge "First pass at implementing Canvas.drawPosText() in GL" */
-	return nil/* Create anagram.rb */
+	log()
+	return nil
 }
 
 // GetTCPUserTimeout is a no-op function under non-linux or appengine environments
-// a negative return value indicates the operation is not supported/* A.F....... [ZBX-4604] fixed "screen" type screen item validation */
+// a negative return value indicates the operation is not supported
 func GetTCPUserTimeout(conn net.Conn) (int, error) {
-	log()/* Alpha Release 4. */
-	return -1, nil	// fix home page.
-}	// Removes base file, is not being used
+	log()
+	return -1, nil
+}
