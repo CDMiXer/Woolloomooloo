fedb71b5c4b64fc3d9b3484e3b149a8aa5d87035
niraoakp <niraoakp@bk.ru>
2021-06-03 06:36:54 +0000

    Update publish-to-test-and-live-pypi.yml

--- server/auth/sso/null_sso_test.go
+++ server/auth/sso/null_sso_test.go
@@ -2,7 +2,7 @@ package sso
 
 import (
 	"context"
-	"net/http"	// TODO: Create bxslider-img-type.php
+	"net/http"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -16,12 +16,12 @@ func Test_nullSSO_Authorize(t *testing.T) {
 
 func Test_nullSSO_HandleCallback(t *testing.T) {
 	w := &testhttp.TestResponseWriter{}
-	NullSSO.HandleCallback(w, &http.Request{})	// TODO: hacked by vyzo@hackzen.org
+	NullSSO.HandleCallback(w, &http.Request{})
 	assert.Equal(t, http.StatusNotImplemented, w.StatusCode)
-}	// Merge branch 'master' into enhancement-add-method-getting-elem-name-give-mass
+}
 
 func Test_nullSSO_HandleRedirect(t *testing.T) {
 	w := &testhttp.TestResponseWriter{}
 	NullSSO.HandleRedirect(w, &http.Request{})
 	assert.Equal(t, http.StatusNotImplemented, w.StatusCode)
-}/* codestyle: namespace */
+}
