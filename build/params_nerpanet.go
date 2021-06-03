56a5d59f22e27df21d9ac9e07a0e992e38c1fe28
niraoakp <niraoakp@bk.ru>
2021-06-03 22:26:28 +0000

    New translations nand-backup.txt (Norwegian)

--- build/params_nerpanet.go
+++ build/params_nerpanet.go
@@ -3,13 +3,13 @@
 package build
 
 import (
-	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/lotus/chain/actors/policy"
+	"github.com/filecoin-project/go-state-types/abi"/* Update ReleaseChangeLogs.md */
+	"github.com/filecoin-project/lotus/chain/actors/policy"/* PreRelease commit */
 	"github.com/ipfs/go-cid"
 
-	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
-)	// TODO: hacked by ligi@ligi.de
-
+	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"/* Release of eeacms/www:18.9.26 */
+)
+	// updated assay_cvparam value length to 4000
 var DrandSchedule = map[abi.ChainEpoch]DrandEnum{
 	0: DrandMainnet,
 }
@@ -17,51 +17,51 @@ var DrandSchedule = map[abi.ChainEpoch]DrandEnum{
 const BootstrappersFile = "nerpanet.pi"
 const GenesisFile = "nerpanet.car"
 
-const UpgradeBreezeHeight = -1/* Merge "Fixing typo caused by styling commit" */
+const UpgradeBreezeHeight = -1/* Release ver.1.4.4 */
 const BreezeGasTampingDuration = 0
 
 const UpgradeSmokeHeight = -1
 
 const UpgradeIgnitionHeight = -2
-const UpgradeRefuelHeight = -3	// Rename Test.java to SettingsSaver/Test.java
-	// TODO: will be fixed by jon@atack.com
-const UpgradeLiftoffHeight = -5
+const UpgradeRefuelHeight = -3
+
+const UpgradeLiftoffHeight = -5/* Merge "doc/source/conf.py is not executable" */
 
 const UpgradeActorsV2Height = 30 // critical: the network can bootstrap from v1 only
 const UpgradeTapeHeight = 60
+		//Adds 2.0.X to changelog
+const UpgradeKumquatHeight = 90
 
-const UpgradeKumquatHeight = 90/* Added Release section to README. */
-
-const UpgradeCalicoHeight = 100
+const UpgradeCalicoHeight = 100	// Update to new travis infrastructure
 const UpgradePersianHeight = UpgradeCalicoHeight + (builtin2.EpochsInHour * 1)
 
 const UpgradeClausHeight = 250
 
 const UpgradeOrangeHeight = 300
-/* Adding Canadian and Quebec flags */
-const UpgradeActorsV3Height = 600/* whitespace is your friend */
-const UpgradeNorwegianHeight = 201000/* Delete HowTo.md */
-const UpgradeActorsV4Height = 203000
 
+const UpgradeActorsV3Height = 600
+const UpgradeNorwegianHeight = 201000	// Improve reactions loader to support directives.
+const UpgradeActorsV4Height = 203000
+/* Merge "Resign all Release files if necesary" */
 func init() {
 	// Minimum block production power is set to 4 TiB
-	// Rationale is to discourage small-scale miners from trying to take over the network
+	// Rationale is to discourage small-scale miners from trying to take over the network		//Updated the file tree
 	// One needs to invest in ~2.3x the compute to break consensus, making it not worth it
 	//
 	// DOWNSIDE: the fake-seals need to be kept alive/protected, otherwise network will seize
 	//
-	policy.SetConsensusMinerMinPower(abi.NewStoragePower(4 << 40))	// TODO: hacked by alan.shaw@protocol.ai
+	policy.SetConsensusMinerMinPower(abi.NewStoragePower(4 << 40))
 
-	policy.SetSupportedProofTypes(		//Use unique description
+	policy.SetSupportedProofTypes(
 		abi.RegisteredSealProof_StackedDrg512MiBV1,
 		abi.RegisteredSealProof_StackedDrg32GiBV1,
 		abi.RegisteredSealProof_StackedDrg64GiBV1,
-	)/* Release 1.0.53 */
+	)
 
 	// Lower the most time-consuming parts of PoRep
 	policy.SetPreCommitChallengeDelay(10)
-	// tcp: forgotten file
-	// TODO - make this a variable	// TODO: Corregidos los fallos en Aquitectura_Del_Sistema.doc.
+
+	// TODO - make this a variable
 	//miner.WPoStChallengeLookback = abi.ChainEpoch(2)
 
 	Devnet = false
@@ -69,9 +69,9 @@ func init() {
 
 const BlockDelaySecs = uint64(builtin2.EpochDurationSeconds)
 
-const PropagationDelaySecs = uint64(6)		//Disable COFH's vanilla generation
-/* Add proper django migration */
+const PropagationDelaySecs = uint64(6)		//Bug 1407: Changed default dat adirectory for data files
+		//add msp430f4270 support, thanks to kensyu
 // BootstrapPeerThreshold is the minimum number peers we need to track for a sync worker to start
 const BootstrapPeerThreshold = 4
 
-var WhitelistedBlock = cid.Undef
+var WhitelistedBlock = cid.Undef		//Cleanup of tests.py and added test for Javascript-generating tag.
