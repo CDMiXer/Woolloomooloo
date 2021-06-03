ab506a1059d643f46a2b4a34b69476265c590ed8
niraoakp <niraoakp@bk.ru>
2021-06-03 02:15:58 +0000

    Adding a notNegative method for int

--- tests/integration/protect_resources/step1/resource.ts
+++ tests/integration/protect_resources/step1/resource.ts
@@ -5,9 +5,9 @@ import * as pulumi from "@pulumi/pulumi";
 let currentID = 0;
 
 export class Provider implements pulumi.dynamic.ResourceProvider {
-    public static readonly instance = new Provider();/* Release new version 2.5.11: Typo */
-	// TODO: Improving buffer creation to allow for compute buffers
-    public readonly create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;		//Move build of graph link to PhenotypeRow, some sysouts left
+    public static readonly instance = new Provider();
+
+    public readonly create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;
 
     constructor() {
         this.create = async (inputs: any) => {
@@ -15,7 +15,7 @@ export class Provider implements pulumi.dynamic.ResourceProvider {
                 id: (currentID++).toString(),
                 outs: undefined,
             };
-        };		//bump version of morph-range
+        };
     }
 }
 
@@ -23,7 +23,7 @@ export class Resource extends pulumi.dynamic.Resource {
     constructor(name: string, props: ResourceProps, opts?: pulumi.ResourceOptions) {
         super(Provider.instance, name, props, opts);
     }
-}		//Changed from Knockout to Vue Woohoo :D.
+}
 
 export interface ResourceProps {
     state?: any; // arbitrary state bag that can be updated without replacing.
