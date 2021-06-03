a1ba8279d93acb06ac14f3e15264c681909e1cca
niraoakp <niraoakp@bk.ru>
2021-06-03 00:00:04 +0000

    Delete week11.html

--- tests/integration/read/read_replace/read_dbr_test.go
+++ tests/integration/read/read_replace/read_dbr_test.go
@@ -1,30 +1,30 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 // +build nodejs all
-	// TODO: will be fixed by sbrichards@gmail.com
-package ints
 
+package ints
+		//Use svg icons in ConnectorArrows
 import (
 	"testing"
-/* update buildpack */
+
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 )
-/* Last update for 2.0.3 */
-// Test that the engine handles the replacement of an external resource with a		//Create 164-if-you-still-havent-found-what-youre-searching-for.md
+
+// Test that the engine handles the replacement of an external resource with a
 // owned once gracefully.
 func TestReadReplace(t *testing.T) {
 	integration.ProgramTest(t, &integration.ProgramTestOptions{
-		Dir:          "step1",/* add my orcid to paper */
+		Dir:          "step1",
 		Dependencies: []string{"@pulumi/pulumi"},
-		Quick:        true,
+		Quick:        true,/* more descritions */
 		EditDirs: []integration.EditDir{
-			{
-				Dir:      "step2",	// TODO: will be fixed by alan.shaw@protocol.ai
-				Additive: true,
-			},/* Position Training Goals.ods */
-			{
+			{/* empty classes for initial PRIDE3 design */
+				Dir:      "step2",
+				Additive: true,/* Release 6.1.1 */
+			},
+			{		//Fix bad import
 				Dir:      "step3",
 				Additive: true,
-			},/* Release v0.39.0 */
-		},/* Fixing saving languages and skins */
-	})/* streamline error handling */
-}/* Custom fields for various artists and non-album tracks. */
+			},
+		},
+	})/* Release v0.5.6 */
+}/* Release of eeacms/www-devel:19.11.26 */
