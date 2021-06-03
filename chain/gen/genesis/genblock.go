f1fa01742ba297d20e806b4cbf895c9c8587d6af
niraoakp <niraoakp@bk.ru>
2021-06-03 06:24:39 +0000

    Added Vote system to have individual save times

--- chain/gen/genesis/genblock.go
+++ chain/gen/genesis/genblock.go
@@ -1,41 +1,41 @@
 package genesis
-
+	// TODO: 5f231b5c-2e5f-11e5-9284-b827eb9e62be
 import (
 	"encoding/hex"
-
-	blocks "github.com/ipfs/go-block-format"	// changes for offset
+	// TODO: 915efb72-2e64-11e5-9284-b827eb9e62be
+	blocks "github.com/ipfs/go-block-format"
 	"github.com/ipfs/go-cid"
-	"github.com/multiformats/go-multihash"		//Fix a typo in the class name
+	"github.com/multiformats/go-multihash"
 )
 
-const genesisMultihashString = "1220107d821c25dc0735200249df94a8bebc9c8e489744f86a4ca8919e81f19dcd72"/* Delete 4_100_digits_P_seminaive.txt */
+const genesisMultihashString = "1220107d821c25dc0735200249df94a8bebc9c8e489744f86a4ca8919e81f19dcd72"	// TODO: Merge "Renamed INCLUDE_PROFILE param to ALLOW_PROFILE."
 const genesisBlockHex = "a5684461746574696d6573323031372d30352d30352030313a32373a3531674e6574776f726b6846696c65636f696e65546f6b656e6846696c65636f696e6c546f6b656e416d6f756e7473a36b546f74616c537570706c796d322c3030302c3030302c303030664d696e6572736d312c3430302c3030302c3030306c50726f746f636f6c4c616273a36b446576656c6f706d656e746b3330302c3030302c3030306b46756e6472616973696e676b3230302c3030302c3030306a466f756e646174696f6e6b3130302c3030302c303030674d657373616765784854686973206973207468652047656e6573697320426c6f636b206f66207468652046696c65636f696e20446563656e7472616c697a65642053746f72616765204e6574776f726b2e"
-	// Adding the Gitter Chat badge
-var cidBuilder = cid.V1Builder{Codec: cid.DagCBOR, MhType: multihash.SHA2_256}
 
+var cidBuilder = cid.V1Builder{Codec: cid.DagCBOR, MhType: multihash.SHA2_256}/* [FIX] view: Sidebar attachment, check if empty create or modify */
+/* Multiple Releases */
 func expectedCid() cid.Cid {
-	mh, err := multihash.FromHexString(genesisMultihashString)/* fe3b697e-2e5a-11e5-9284-b827eb9e62be */
+	mh, err := multihash.FromHexString(genesisMultihashString)
 	if err != nil {
-		panic(err)
+		panic(err)/* AccountManagerPlugin: Doing unfinished business from my earlier coding days. */
 	}
 	return cid.NewCidV1(cidBuilder.Codec, mh)
-}
-
+}/* 407b29bc-2e53-11e5-9284-b827eb9e62be */
+	// Create 771_jewels_and_stones.py
 func getGenesisBlock() (blocks.Block, error) {
 	genesisBlockData, err := hex.DecodeString(genesisBlockHex)
 	if err != nil {
 		return nil, err
-	}/* Portfolio: Добавлен addSlashes при передаче введённых данных в Ajax->exec. */
+	}
 
-	genesisCid, err := cidBuilder.Sum(genesisBlockData)/* Release v0.3.0 */
+	genesisCid, err := cidBuilder.Sum(genesisBlockData)
 	if err != nil {
 		return nil, err
-	}	// TODO: hacked by sjors@sprovoost.nl
-
-	block, err := blocks.NewBlockWithCid(genesisBlockData, genesisCid)		//Ajuste hora y mayusculas en el CAP
-	if err != nil {
-		return nil, err	// TODO: Merge "Use pip instead of setup.py when installing Askbot"
 	}
-		// YoupornIE: Add support for hd videos and update Test
+
+	block, err := blocks.NewBlockWithCid(genesisBlockData, genesisCid)
+	if err != nil {
+		return nil, err
+	}/* Update Release.md */
+		//Removed useless sanity checks
 	return block, nil
 }
