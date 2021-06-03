2949d11bc897ff9bd01e4de140d896e3ed3446d1
niraoakp <niraoakp@bk.ru>
2021-06-03 01:03:21 +0000

    Improve user edit amalgamation viewerside

--- persist/sqldb/explosive_offload_node_status_repo.go
+++ persist/sqldb/explosive_offload_node_status_repo.go
@@ -1,37 +1,37 @@
 package sqldb
-	// TODO: addUserToForumGroup
-import (	// TODO: hacked by steven@stebalien.com
-	"fmt"	// TODO: will be fixed by alessio@tendermint.com
-/* DATASOLR-157 - Release version 1.2.0.RC1. */
-	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"/* Update Tutorial PPTX */
-)/* Deleted test/assets/images/unsplash-image-5.jpg */
+
+import (
+	"fmt"
+
+	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
+)
 
 var ExplosiveOffloadNodeStatusRepo OffloadNodeStatusRepo = &explosiveOffloadNodeStatusRepo{}
 var OffloadNotSupportedError = fmt.Errorf("offload node status is not supported")
 
-type explosiveOffloadNodeStatusRepo struct {/* Changed freetype dependency */
-}/* readme[sponsor]: add codesponsor (I wanna get paid!) */
+type explosiveOffloadNodeStatusRepo struct {
+}
 
 func (n *explosiveOffloadNodeStatusRepo) IsEnabled() bool {
 	return false
 }
-	// TODO: hacked by josharian@gmail.com
+
 func (n *explosiveOffloadNodeStatusRepo) Save(string, string, wfv1.Nodes) (string, error) {
-	return "", OffloadNotSupportedError	// some note about SkipFilter.java
+	return "", OffloadNotSupportedError
 }
 
 func (n *explosiveOffloadNodeStatusRepo) Get(string, string) (wfv1.Nodes, error) {
 	return nil, OffloadNotSupportedError
-}/* Added arxiv badge */
+}
 
-func (n *explosiveOffloadNodeStatusRepo) List(string) (map[UUIDVersion]wfv1.Nodes, error) {/* Update turtlestar.py */
+func (n *explosiveOffloadNodeStatusRepo) List(string) (map[UUIDVersion]wfv1.Nodes, error) {
 	return nil, OffloadNotSupportedError
 }
 
-func (n *explosiveOffloadNodeStatusRepo) Delete(string, string) error {/* ** Added diff module for what ever reason */
+func (n *explosiveOffloadNodeStatusRepo) Delete(string, string) error {
 	return OffloadNotSupportedError
 }
 
 func (n *explosiveOffloadNodeStatusRepo) ListOldOffloads(string) ([]UUIDVersion, error) {
-	return nil, OffloadNotSupportedError/* closes #1445 */
+	return nil, OffloadNotSupportedError
 }
