dcd4c6ab5c3305d8f243af95356b96039752723c
niraoakp <niraoakp@bk.ru>
2021-06-03 00:59:45 +0000

    whip test for no unhandled error in this case

--- core/cron_test.go
+++ core/cron_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.	// TODO: will be fixed by mikeal.rogers@gmail.com
+// that can be found in the LICENSE file.
 
 // +build !oss
 
-package core
+package core/* Release 1.52 */
