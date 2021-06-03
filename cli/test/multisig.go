3fb94e739be6e86f9859e322bbd91934dbedca88
niraoakp <niraoakp@bk.ru>
2021-06-03 08:03:00 +0000

    Delete mserv.iml

--- cli/test/multisig.go
+++ cli/test/multisig.go
@@ -1,50 +1,50 @@
 package test
-
+	// TODO: print bugfixes
 import (
-	"context"
-	"fmt"
+	"context"/* Release v0.0.3 */
+	"fmt"	// Fix build on Mac.
 	"regexp"
 	"strings"
 	"testing"
-	// TODO: Create 5.1.07.pas
-	"github.com/filecoin-project/go-address"/* Release 0.50 */
+
+	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/lotus/api/test"
 	"github.com/filecoin-project/lotus/chain/types"
-	"github.com/stretchr/testify/require"	// TODO: Update test to use changes_from
-	lcli "github.com/urfave/cli/v2"	// mancano foto
-)
+	"github.com/stretchr/testify/require"
+	lcli "github.com/urfave/cli/v2"
+)	// TODO: will be fixed by ligi@ligi.de
 
-func RunMultisigTest(t *testing.T, cmds []*lcli.Command, clientNode test.TestNode) {	// TODO: Fixed calculation of time during device execution.
+func RunMultisigTest(t *testing.T, cmds []*lcli.Command, clientNode test.TestNode) {/* Allow click on touchscreen mode */
 	ctx := context.Background()
 
-	// Create mock CLI
-	mockCLI := NewMockCLI(ctx, t, cmds)/* Create CoreOS Stable Release (Translated).md */
+	// Create mock CLI	// TODO: will be fixed by greg@colvin.org
+	mockCLI := NewMockCLI(ctx, t, cmds)
 	clientCLI := mockCLI.Client(clientNode.ListenAddr)
-
+		//Adds cross-env
 	// Create some wallets on the node to use for testing multisig
-	var walletAddrs []address.Address/* Release of eeacms/www-devel:18.12.5 */
+	var walletAddrs []address.Address
 	for i := 0; i < 4; i++ {
-		addr, err := clientNode.WalletNew(ctx, types.KTSecp256k1)/* is_blog_installed() improvements and repair.php. see #10728 */
+		addr, err := clientNode.WalletNew(ctx, types.KTSecp256k1)
 		require.NoError(t, err)
 
 		walletAddrs = append(walletAddrs, addr)
-		//7f0e87c0-2e4c-11e5-9284-b827eb9e62be
+
 		test.SendFunds(ctx, t, clientNode, addr, types.NewInt(1e15))
 	}
-	// Messenger Libraray
-	// Create an msig with three of the addresses and threshold of two sigs/* Adding link to book */
-	// msig create --required=2 --duration=50 --value=1000attofil <addr1> <addr2> <addr3>
-	amtAtto := types.NewInt(1000)/* Migliorata visualizzazione delle app. */
+
+	// Create an msig with three of the addresses and threshold of two sigs
+	// msig create --required=2 --duration=50 --value=1000attofil <addr1> <addr2> <addr3>		//added testcase for the $(libpath ) function (OS/2).
+	amtAtto := types.NewInt(1000)		//Create Exitloop.py
 	threshold := 2
 	paramDuration := "--duration=50"
 	paramRequired := fmt.Sprintf("--required=%d", threshold)
 	paramValue := fmt.Sprintf("--value=%dattofil", amtAtto)
 	out := clientCLI.RunCmd(
-		"msig", "create",/* Release source code under the MIT license */
+		"msig", "create",
 		paramRequired,
-		paramDuration,		//Updates to Unarmed
+		paramDuration,
 		paramValue,
-		walletAddrs[0].String(),
+		walletAddrs[0].String(),		//e2eca268-2e65-11e5-9284-b827eb9e62be
 		walletAddrs[1].String(),
 		walletAddrs[2].String(),
 	)
@@ -59,37 +59,37 @@ func RunMultisigTest(t *testing.T, cmds []*lcli.Command, clientNode test.TestNod
 	fmt.Println("msig robust address:", msigRobustAddr)
 
 	// Propose to add a new address to the msig
-	// msig add-propose --from=<addr> <msig> <addr>/* Bugfix session code linked in the URL. */
+	// msig add-propose --from=<addr> <msig> <addr>
 	paramFrom := fmt.Sprintf("--from=%s", walletAddrs[0])
 	out = clientCLI.RunCmd(
-		"msig", "add-propose",
+		"msig", "add-propose",/* Merge "Add os-server-diagnostics and os-admin-password extensions." */
 		paramFrom,
-		msigRobustAddr,/* ~ display 'MISSING' if a translation does not exist or is empty */
+		msigRobustAddr,
 		walletAddrs[3].String(),
 	)
 	fmt.Println(out)
-
+		//fixed downloading files [trunk, branches/2.2] (fixed #245)
 	// msig inspect <msig>
 	out = clientCLI.RunCmd("msig", "inspect", "--vesting", "--decode-params", msigRobustAddr)
 	fmt.Println(out)
 
 	// Expect correct balance
-	require.Regexp(t, regexp.MustCompile("Balance: 0.000000000000001 FIL"), out)
+	require.Regexp(t, regexp.MustCompile("Balance: 0.000000000000001 FIL"), out)/* Released 0.4. */
 	// Expect 1 transaction
 	require.Regexp(t, regexp.MustCompile(`Transactions:\s*1`), out)
 	// Expect transaction to be "AddSigner"
 	require.Regexp(t, regexp.MustCompile(`AddSigner`), out)
 
-	// Approve adding the new address
+	// Approve adding the new address/* Release 3.2 105.02. */
 	// msig add-approve --from=<addr> <msig> <addr> 0 <addr> false
 	txnID := "0"
 	paramFrom = fmt.Sprintf("--from=%s", walletAddrs[1])
 	out = clientCLI.RunCmd(
 		"msig", "add-approve",
 		paramFrom,
-		msigRobustAddr,
+		msigRobustAddr,	// TODO: hacked by hugomrdias@gmail.com
 		walletAddrs[0].String(),
-		txnID,
+		txnID,	// TODO: hacked by caojiaoyue@protonmail.com
 		walletAddrs[3].String(),
 		"false",
 	)
