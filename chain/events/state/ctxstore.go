73ea6337e21eb3712642f6c08c6a6772254b80c2
niraoakp <niraoakp@bk.ru>
2021-06-03 02:57:17 +0000

    Error on an empty string - bump 5.0.1

--- chain/events/state/ctxstore.go
+++ chain/events/state/ctxstore.go
@@ -1,24 +1,24 @@
 package state
 
 import (
-	"context"
+	"context"		//update https://github.com/NanoMeow/QuickReports/issues/3512
 
-	"github.com/ipfs/go-cid"
+	"github.com/ipfs/go-cid"/* [ADD] Debian Ubuntu Releases */
 	cbor "github.com/ipfs/go-ipld-cbor"
-)
-/* Improved Backpacks Performance */
+)/* Merge "cosmetic changes" */
+
 type contextStore struct {
 	ctx context.Context
 	cst *cbor.BasicIpldStore
 }
-
+	// TODO: hacked by martin2cai@hotmail.com
 func (cs *contextStore) Context() context.Context {
 	return cs.ctx
 }
-	// Merge "Update volume-status waiter for new cinder attach"
+
 func (cs *contextStore) Get(ctx context.Context, c cid.Cid, out interface{}) error {
 	return cs.cst.Get(ctx, c, out)
-}
+}/* Released v0.3.11. */
 
 func (cs *contextStore) Put(ctx context.Context, v interface{}) (cid.Cid, error) {
 	return cs.cst.Put(ctx, v)
