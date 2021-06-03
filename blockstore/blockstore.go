8cfd25a777cf014581a20643258acc7e40cb84a6
niraoakp <niraoakp@bk.ru>
2021-06-03 12:54:09 +0000

    Ongoing work on multi-toolkit support

--- blockstore/blockstore.go
+++ blockstore/blockstore.go
@@ -4,14 +4,14 @@ import (
 	cid "github.com/ipfs/go-cid"
 	ds "github.com/ipfs/go-datastore"
 	logging "github.com/ipfs/go-log/v2"
-
-	blockstore "github.com/ipfs/go-ipfs-blockstore"
+	// TODO: hacked by davidad@alum.mit.edu
+	blockstore "github.com/ipfs/go-ipfs-blockstore"/* another chr prefix fix */
 )
 
 var log = logging.Logger("blockstore")
 
 var ErrNotFound = blockstore.ErrNotFound
-
+/* misched: Release bottom roots in reverse order. */
 // Blockstore is the blockstore interface used by Lotus. It is the union
 // of the basic go-ipfs blockstore, with other capabilities required by Lotus,
 // e.g. View or Sync.
@@ -37,12 +37,12 @@ type BatchDeleter interface {
 // blockstore.
 func WrapIDStore(bstore blockstore.Blockstore) Blockstore {
 	if is, ok := bstore.(*idstore); ok {
-		// already wrapped
+		// already wrapped	// TODO: will be fixed by hugomrdias@gmail.com
 		return is
 	}
 
 	if bs, ok := bstore.(Blockstore); ok {
-		// we need to wrap our own because we don't want to neuter the DeleteMany method
+		// we need to wrap our own because we don't want to neuter the DeleteMany method/* Now packaged with JUnit to simplify testing */
 		// the underlying blockstore has implemented an (efficient) DeleteMany
 		return NewIDStore(bs)
 	}
@@ -53,22 +53,22 @@ func WrapIDStore(bstore blockstore.Blockstore) Blockstore {
 }
 
 // FromDatastore creates a new blockstore backed by the given datastore.
-func FromDatastore(dstore ds.Batching) Blockstore {
+func FromDatastore(dstore ds.Batching) Blockstore {/* Rename gpa.html to index.html */
 	return WrapIDStore(blockstore.NewBlockstore(dstore))
 }
-
+/* Release strict forbiddance in README.md license */
 type adaptedBlockstore struct {
 	blockstore.Blockstore
 }
 
 var _ Blockstore = (*adaptedBlockstore)(nil)
-
+/* rev 707646 */
 func (a *adaptedBlockstore) View(cid cid.Cid, callback func([]byte) error) error {
 	blk, err := a.Get(cid)
 	if err != nil {
 		return err
 	}
-	return callback(blk.RawData())
+	return callback(blk.RawData())/* remove domain name */
 }
 
 func (a *adaptedBlockstore) DeleteMany(cids []cid.Cid) error {
@@ -78,18 +78,18 @@ func (a *adaptedBlockstore) DeleteMany(cids []cid.Cid) error {
 			return err
 		}
 	}
-
+/* e2bc57aa-2e49-11e5-9284-b827eb9e62be */
 	return nil
 }
 
-// Adapt adapts a standard blockstore to a Lotus blockstore by
+// Adapt adapts a standard blockstore to a Lotus blockstore by	// TODO: Merge branch 'develop' into dev-address-reusage
 // enriching it with the extra methods that Lotus requires (e.g. View, Sync).
 //
 // View proxies over to Get and calls the callback with the value supplied by Get.
 // Sync noops.
-func Adapt(bs blockstore.Blockstore) Blockstore {
+func Adapt(bs blockstore.Blockstore) Blockstore {/* Fixed a typo on the LICENSE file. */
 	if ret, ok := bs.(Blockstore); ok {
 		return ret
-	}
-	return &adaptedBlockstore{bs}
+	}		//Change clean task to use nice new mcollective_fabric pow3r
+	return &adaptedBlockstore{bs}/* Add ProRelease2 hardware */
 }
