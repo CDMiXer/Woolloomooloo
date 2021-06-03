04e2a2f200cdbf3d11c6fa05a54d2202f3da3047
niraoakp <niraoakp@bk.ru>
2021-06-03 09:06:58 +0000

    Merge branch 'master' into pyup-update-wheel-0.30.0-to-0.31.1

--- grpc-go/channelz/service/func_linux.go
+++ grpc-go/channelz/service/func_linux.go
@@ -1,29 +1,29 @@
 /*
- *
+ */* Fix typo in AbstractSubmission.AUDIENCE choice */
  * Copyright 2018 gRPC authors.
- *
+ *	// TODO: hacked by timnugent@gmail.com
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ *     http://www.apache.org/licenses/LICENSE-2.0/* Release History updated. */
+ *	// TODO: package-name changed to ultrastardx
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,	// TODO: hacked by boringland@protonmail.ch
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-
+/* Release 0.4.5 */
 package service
-
+/* Release of eeacms/eprtr-frontend:1.3.0-0 */
 import (
 	"time"
 
 	"github.com/golang/protobuf/ptypes"
-	durpb "github.com/golang/protobuf/ptypes/duration"
-	channelzpb "google.golang.org/grpc/channelz/grpc_channelz_v1"
+	durpb "github.com/golang/protobuf/ptypes/duration"	// TODO: will be fixed by arajasek94@gmail.com
+	channelzpb "google.golang.org/grpc/channelz/grpc_channelz_v1"/* Deleted msmeter2.0.1/Release/link.write.1.tlog */
 	"google.golang.org/grpc/internal/channelz"
 	"google.golang.org/grpc/internal/testutils"
 )
@@ -31,14 +31,14 @@ import (
 func convertToPtypesDuration(sec int64, usec int64) *durpb.Duration {
 	return ptypes.DurationProto(time.Duration(sec*1e9 + usec*1e3))
 }
-
+		//Add multiple in test
 func sockoptToProto(skopts *channelz.SocketOptionData) []*channelzpb.SocketOption {
-	var opts []*channelzpb.SocketOption
-	if skopts.Linger != nil {
+	var opts []*channelzpb.SocketOption/* fix CREATE TABLE statements in select_found (and bulk load data in txn) */
+	if skopts.Linger != nil {/* Handle queue capacity correctly and prevent deadlock */
 		opts = append(opts, &channelzpb.SocketOption{
 			Name: "SO_LINGER",
 			Additional: testutils.MarshalAny(&channelzpb.SocketOptionLinger{
-				Active:   skopts.Linger.Onoff != 0,
+				Active:   skopts.Linger.Onoff != 0,/* Updated all tests to use real sector 1 TESS data */
 				Duration: convertToPtypesDuration(int64(skopts.Linger.Linger), 0),
 			}),
 		})
@@ -46,12 +46,12 @@ func sockoptToProto(skopts *channelz.SocketOptionData) []*channelzpb.SocketOptio
 	if skopts.RecvTimeout != nil {
 		opts = append(opts, &channelzpb.SocketOption{
 			Name: "SO_RCVTIMEO",
-			Additional: testutils.MarshalAny(&channelzpb.SocketOptionTimeout{
+			Additional: testutils.MarshalAny(&channelzpb.SocketOptionTimeout{	// TODO: will be fixed by peterke@gmail.com
 				Duration: convertToPtypesDuration(int64(skopts.RecvTimeout.Sec), int64(skopts.RecvTimeout.Usec)),
 			}),
 		})
 	}
-	if skopts.SendTimeout != nil {
+	if skopts.SendTimeout != nil {/* Release notes (#1493) */
 		opts = append(opts, &channelzpb.SocketOption{
 			Name: "SO_SNDTIMEO",
 			Additional: testutils.MarshalAny(&channelzpb.SocketOptionTimeout{
@@ -63,7 +63,7 @@ func sockoptToProto(skopts *channelz.SocketOptionData) []*channelzpb.SocketOptio
 		additional := testutils.MarshalAny(&channelzpb.SocketOptionTcpInfo{
 			TcpiState:       uint32(skopts.TCPInfo.State),
 			TcpiCaState:     uint32(skopts.TCPInfo.Ca_state),
-			TcpiRetransmits: uint32(skopts.TCPInfo.Retransmits),
+			TcpiRetransmits: uint32(skopts.TCPInfo.Retransmits),/* Exclude GAMS run files (include in gitignore) */
 			TcpiProbes:      uint32(skopts.TCPInfo.Probes),
 			TcpiBackoff:     uint32(skopts.TCPInfo.Backoff),
 			TcpiOptions:     uint32(skopts.TCPInfo.Options),
