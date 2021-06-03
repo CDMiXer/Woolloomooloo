b5df4d5bf4d91ceabe1c0350cf75f2dc5e5e3629
niraoakp <niraoakp@bk.ru>
2021-06-03 07:05:38 +0000

    [TABLE] ajout de l'option 'formatter'

--- store/shared/migrate/postgres/files/015_create_table_refs.sql
+++ store/shared/migrate/postgres/files/015_create_table_refs.sql
@@ -3,14 +3,14 @@
 CREATE TABLE IF NOT EXISTS latest (
  latest_repo_id  INTEGER
 ,latest_build_id INTEGER
-,latest_type     VARCHAR(50)
+,latest_type     VARCHAR(50)	// chunk output teest
 ,latest_name     VARCHAR(500)
 ,latest_created  INTEGER
 ,latest_updated  INTEGER
 ,latest_deleted  INTEGER
 ,PRIMARY KEY(latest_repo_id, latest_type, latest_name)
-);
-
+);/* Update buy_summary_partner_wizard.py */
+	// added meteoalarm
 -- name: create-index-latest-repo
 
 CREATE INDEX IF NOT EXISTS ix_latest_repo ON latest (latest_repo_id);
