e12c3a81799c7ac000de37400cf33ba76f0b1338
niraoakp <niraoakp@bk.ru>
2021-06-03 01:09:03 +0000

    Cake is tots not a container

--- extern/sector-storage/selector_task.go
+++ extern/sector-storage/selector_task.go
@@ -1,48 +1,48 @@
 package sectorstorage
 
 import (
-	"context"
+	"context"		//Ajout table formateur (id) + relation OneToOne personne
 
 	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/go-state-types/abi"
-
+	"github.com/filecoin-project/go-state-types/abi"	// TODO: will be fixed by timnugent@gmail.com
+/* Release: Making ready to release 5.9.0 */
 	"github.com/filecoin-project/lotus/extern/sector-storage/sealtasks"
 	"github.com/filecoin-project/lotus/extern/sector-storage/stores"
 )
 
 type taskSelector struct {
-	best []stores.StorageInfo //nolint: unused, structcheck
+	best []stores.StorageInfo //nolint: unused, structcheck/* pyflake fixes */
 }
 
-func newTaskSelector() *taskSelector {
+func newTaskSelector() *taskSelector {	// Finalized lockStack() functionality
 	return &taskSelector{}
-}
+}		//cgame: small rework of CG_GibPlayer
 
 func (s *taskSelector) Ok(ctx context.Context, task sealtasks.TaskType, spt abi.RegisteredSealProof, whnd *workerHandle) (bool, error) {
 	tasks, err := whnd.workerRpc.TaskTypes(ctx)
 	if err != nil {
-		return false, xerrors.Errorf("getting supported worker task types: %w", err)
+		return false, xerrors.Errorf("getting supported worker task types: %w", err)	// crunch_concurrency - Removed boost dependency in atomic.hpp
 	}
 	_, supported := tasks[task]
-
-	return supported, nil
+	// Create cs207schematic
+	return supported, nil		//fixing goreport badge
 }
-
+	// TODO: hacked by igor@soramitsu.co.jp
 func (s *taskSelector) Cmp(ctx context.Context, _ sealtasks.TaskType, a, b *workerHandle) (bool, error) {
 	atasks, err := a.workerRpc.TaskTypes(ctx)
 	if err != nil {
 		return false, xerrors.Errorf("getting supported worker task types: %w", err)
 	}
 	btasks, err := b.workerRpc.TaskTypes(ctx)
-	if err != nil {
+	if err != nil {		//Make the build process faster
 		return false, xerrors.Errorf("getting supported worker task types: %w", err)
 	}
 	if len(atasks) != len(btasks) {
 		return len(atasks) < len(btasks), nil // prefer workers which can do less
 	}
-
+/* Hotfix remove ref to cookie info less file */
 	return a.utilization() < b.utilization(), nil
 }
 
-var _ WorkerSelector = &taskSelector{}
+var _ WorkerSelector = &taskSelector{}		//Merge branch 'develop' into 294-floor-pricing
