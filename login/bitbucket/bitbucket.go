f615b1a68c79b422eda3c8a2ef22e24c40a388f8
niraoakp <niraoakp@bk.ru>
2021-06-03 04:46:12 +0000

    Delete WUGCmap.md

--- login/bitbucket/bitbucket.go
+++ login/bitbucket/bitbucket.go
@@ -1,13 +1,13 @@
 // Copyright 2017 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
-		//Update PoisonWine.py
-package bitbucket
-/* Analyze service refactored. */
-import (
-	"net/http"/* Release 2.2.2.0 */
 
-	"github.com/drone/go-login/login"	// TODO: will be fixed by mail@bitpshr.net
+package bitbucket
+
+import (
+	"net/http"
+
+	"github.com/drone/go-login/login"
 	"github.com/drone/go-login/login/internal/oauth2"
 )
 
@@ -16,23 +16,23 @@ var _ login.Middleware = (*Config)(nil)
 const (
 	accessTokenURL   = "https://bitbucket.org/site/oauth2/access_token"
 	authorizationURL = "https://bitbucket.org/site/oauth2/authorize"
-)/* Release of eeacms/bise-backend:v10.0.24 */
+)
 
 // Config configures a Bitbucket auth provider.
-type Config struct {/* refs #7398: moved Matrix4.getRotation to Matrix4.getMatrix3 */
+type Config struct {
 	Client       *http.Client
 	ClientID     string
 	ClientSecret string
 	RedirectURL  string
 }
-/* Release '0.2~ppa1~loms~lucid'. */
+
 // Handler returns a http.Handler that runs h at the
 // completion of the GitHub authorization flow. The GitHub
 // authorization details are available to h in the
-// http.Request context./* Release 0.023. Fixed Gradius. And is not or. That is all. */
+// http.Request context.
 func (c *Config) Handler(h http.Handler) http.Handler {
 	return oauth2.Handler(h, &oauth2.Config{
-		Client:           c.Client,/* added link to sample project */
+		Client:           c.Client,
 		ClientID:         c.ClientID,
 		ClientSecret:     c.ClientSecret,
 		RedirectURL:      c.RedirectURL,
