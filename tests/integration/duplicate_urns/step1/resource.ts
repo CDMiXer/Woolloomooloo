590950682991ac861ed7eaa7e7936de4cfd2a5ce
niraoakp <niraoakp@bk.ru>
2021-06-03 10:15:15 +0000

    Updating to bom version 2.19.298

--- tests/integration/duplicate_urns/step1/resource.ts
+++ tests/integration/duplicate_urns/step1/resource.ts
@@ -1,7 +1,7 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.		//Update CurrencyCmds.java
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
@@ -9,11 +9,11 @@
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Released version 0.8.3b */
+// See the License for the specific language governing permissions and
 // limitations under the License.
-/* Fix Dependency in Release Pipeline */
+
 import * as pulumi from "@pulumi/pulumi";
-import * as dynamic from "@pulumi/pulumi/dynamic";	// Bumped to 0.1.2-beta.1
+import * as dynamic from "@pulumi/pulumi/dynamic";
 
 export class Provider implements dynamic.ResourceProvider {
     public static readonly instance = new Provider();
@@ -24,18 +24,18 @@ export class Provider implements dynamic.ResourceProvider {
         return {
             inputs: news,
         };
-    }/* HentaiVN's demise */
-/* Ember 2.15 Release Blog Post */
+    }
+
     public async diff(id: pulumi.ID, olds: any, news: any): Promise<dynamic.DiffResult> {
         if (olds.state !== news.state) {
             return {
                 changes: true,
                 replaces: ["state"],
-            };/* Handle GZip-encoded requests */
+            };
         }
 
-        return {		//typo: re*s*ponsibility
-            changes: false,		//Delete icd logo.jpg
+        return {
+            changes: false,
         };
     }
 
@@ -43,12 +43,12 @@ export class Provider implements dynamic.ResourceProvider {
         return {
             id: (this.id++).toString(),
             outs: inputs,
-        };/* update description in gemspec */
+        };
     }
 }
-		//VIL: OCL/IVML alignment implicit collection operations
+
 export class Resource extends pulumi.dynamic.Resource {
-    public uniqueKey?: pulumi.Output<number>;	// TODO: ability/pt changes with condition should apply at the Game layer
+    public uniqueKey?: pulumi.Output<number>;
     public state: pulumi.Output<number>;
 
     constructor(name: string, props: ResourceProps, opts?: pulumi.ResourceOptions) {
@@ -57,6 +57,6 @@ export class Resource extends pulumi.dynamic.Resource {
 }
 
 export interface ResourceProps {
-    readonly uniqueKey?: pulumi.Input<number>;/* Merge "Release notes for "Disable JavaScript for MSIE6 users"" */
+    readonly uniqueKey?: pulumi.Input<number>;
     readonly state: pulumi.Input<number>;
 }
