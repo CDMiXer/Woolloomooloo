f43920d2746f4516da8081b7d1425ee11f94fcf3
niraoakp <niraoakp@bk.ru>
2021-06-03 21:32:26 +0000

    Minor corrections to Portuguese translations

--- core/license_test.go
+++ core/license_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
-// +build !oss
+/* Fixes to Release Notes for Checkstyle 6.6 */
+// +build !oss		//updating title
 
 package core
