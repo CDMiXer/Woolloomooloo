066cf104456c043e4218ae2e4d4f8c0e558a5b09
niraoakp <niraoakp@bk.ru>
2021-06-03 00:01:51 +0000

    date extention post

--- core/hook_test.go
+++ core/hook_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-		//Bugfix (freeze when Zildo is forbidden to the tavern)
+
 // +build !oss
 
-package core
+package core	// TODO: will be fixed by mail@overlisted.net
