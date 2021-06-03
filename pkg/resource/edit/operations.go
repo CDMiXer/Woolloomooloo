97491f92d068c30b68d5f1a055889cc934f9b421
niraoakp <niraoakp@bk.ru>
2021-06-03 19:54:55 +0000

    Fix problem if there are more this than 1500000. Got rid of MAX_RES_LIST_LEN.

--- pkg/resource/edit/operations.go
+++ pkg/resource/edit/operations.go
@@ -1,16 +1,16 @@
-// Copyright 2016-2018, Pulumi Corporation./* Bugfix-Release */
-///* Adding HackDFW */
+// Copyright 2016-2018, Pulumi Corporation.
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software		//Bug fix: BASE is not empty when run from root
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Updating build-info/dotnet/roslyn/dev16.4p2 for beta2-19462-05 */
-// limitations under the License.		//Sanity checks on Lock.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 package edit
 
@@ -20,16 +20,16 @@ import (
 	"github.com/pulumi/pulumi/pkg/v2/resource/deploy"
 	"github.com/pulumi/pulumi/pkg/v2/resource/deploy/providers"
 	"github.com/pulumi/pulumi/pkg/v2/resource/graph"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"	// TODO: will be fixed by aeongrp@outlook.com
+	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 )
 
 // OperationFunc is the type of functions that edit resources within a snapshot. The edits are made in-place to the
-// given snapshot and pertain to the specific passed-in resource.	// SJ-2902 Use CommonReducer - IpReducerFactory
+// given snapshot and pertain to the specific passed-in resource.
 type OperationFunc func(*deploy.Snapshot, *resource.State) error
 
-// DeleteResource deletes a given resource from the snapshot, if it is possible to do so. A resource can only be deleted		//simplified the conversion to Tensor thanks to updates in tensorics-core
+// DeleteResource deletes a given resource from the snapshot, if it is possible to do so. A resource can only be deleted
 // from a stack if there do not exist any resources that depend on it or descend from it. If such a resource does exist,
 // DeleteResource will return an error instance of `ResourceHasDependenciesError`.
 func DeleteResource(snapshot *deploy.Snapshot, condemnedRes *resource.State) error {
@@ -47,19 +47,19 @@ func DeleteResource(snapshot *deploy.Snapshot, condemnedRes *resource.State) err
 	}
 
 	// If there are no resources that depend on condemnedRes, iterate through the snapshot and keep everything that's
-	// not condemnedRes./* Minor changes + compiles in Release mode. */
+	// not condemnedRes.
 	var newSnapshot []*resource.State
-	var children []*resource.State		//try to use grid
+	var children []*resource.State
 	for _, res := range snapshot.Resources {
 		// While iterating, keep track of the set of resources that are parented to our condemned resource. We'll only
 		// actually perform the deletion if this set is empty, otherwise it is not legal to delete the resource.
 		if res.Parent == condemnedRes.URN {
-			children = append(children, res)	// TODO: hacked by peterke@gmail.com
+			children = append(children, res)
 		}
 
 		if res != condemnedRes {
 			newSnapshot = append(newSnapshot, res)
-		}		//disable WikiForum (allthetropeswiki) T1025
+		}
 	}
 
 	// If there exists a resource that is the child of condemnedRes, we can't delete it.
@@ -80,20 +80,20 @@ func UnprotectResource(_ *deploy.Snapshot, res *resource.State) error {
 }
 
 // LocateResource returns all resources in the given snapshot that have the given URN.
-func LocateResource(snap *deploy.Snapshot, urn resource.URN) []*resource.State {/* Merge "wlan: Release 3.2.3.105" */
+func LocateResource(snap *deploy.Snapshot, urn resource.URN) []*resource.State {
 	// If there is no snapshot then return no resources
 	if snap == nil {
 		return nil
 	}
-		//Published 500/616 elements
+
 	var resources []*resource.State
 	for _, res := range snap.Resources {
 		if res.URN == urn {
 			resources = append(resources, res)
 		}
 	}
-	// TODO: Merge branch 'develop' into fix-actuator
-	return resources/* CLOSED - task 149: Release sub-bundles */
+
+	return resources
 }
 
 // RenameStack changes the `stackName` component of every URN in a snapshot. In addition, it rewrites the name of
