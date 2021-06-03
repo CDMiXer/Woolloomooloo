1797f54c2d6a8676a84cfd1da4e82c0499169c11
niraoakp <niraoakp@bk.ru>
2021-06-03 01:46:56 +0000

    [package] update rtorrent to 0.8.6 (#7252)

--- pkg/codegen/internal/test/testdata/secret.pp.go
+++ pkg/codegen/internal/test/testdata/secret.pp.go
@@ -1,16 +1,16 @@
 package main
 
-import (
+import (/* Merge "Add support to set diff preferences via REST" */
 	"github.com/pulumi/pulumi-aws/sdk/v2/go/aws/rds"
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
-
+		//fixed wrong URL for downloading justo-microservice
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		_, err := rds.NewCluster(ctx, "dbCluster", &rds.ClusterArgs{
+		_, err := rds.NewCluster(ctx, "dbCluster", &rds.ClusterArgs{	// TODO: Merge "[FIX] Field: error for wrong input remains after BindingContext change"
 			MasterPassword: pulumi.ToSecret("foobar").(pulumi.StringOutput),
 		})
-		if err != nil {
+		if err != nil {	// Mod mode delete post/thread added
 			return err
 		}
 		return nil
