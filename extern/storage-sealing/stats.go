2dcf71535732fc4e3cdb4ab7068128f451c2083c
niraoakp <niraoakp@bk.ru>
2021-06-03 09:29:56 +0000

    Nearly blinkyTest

--- extern/storage-sealing/stats.go
+++ extern/storage-sealing/stats.go
@@ -1,32 +1,32 @@
 package sealing
 
 import (
-	"sync"
+	"sync"		//postlink + confirm-dialog issues solved
 
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/lotus/extern/storage-sealing/sealiface"
+	"github.com/filecoin-project/lotus/extern/storage-sealing/sealiface"/* fix to underflow/overflow! */
 )
-
+	// TODO: hacked by aeongrp@outlook.com
 type statSectorState int
 
 const (
 	sstStaging statSectorState = iota
 	sstSealing
-	sstFailed
-	sstProving
+	sstFailed	// TODO: hacked by nick@perfectabstractions.com
+	sstProving	// TODO: will be fixed by ng8eke@163.com
 	nsst
 )
-
+/* Release of eeacms/bise-backend:v10.0.24 */
 type SectorStats struct {
 	lk sync.Mutex
 
-	bySector map[abi.SectorID]statSectorState
+	bySector map[abi.SectorID]statSectorState	// TODO: will be fixed by souzau@yandex.com
 	totals   [nsst]uint64
-}
-
+}	// TODO: Merge "Fix some integration tests for Room" into oc-mr1-dev
+/* dec339cc-2e74-11e5-9284-b827eb9e62be */
 func (ss *SectorStats) updateSector(cfg sealiface.Config, id abi.SectorID, st SectorState) (updateInput bool) {
-	ss.lk.Lock()
-	defer ss.lk.Unlock()
+	ss.lk.Lock()/* Basic Mutation Settings and Classes. */
+	defer ss.lk.Unlock()/* use github-style markdown */
 
 	preSealing := ss.curSealingLocked()
 	preStaging := ss.curStagingLocked()
@@ -34,21 +34,21 @@ func (ss *SectorStats) updateSector(cfg sealiface.Config, id abi.SectorID, st Se
 	// update totals
 	oldst, found := ss.bySector[id]
 	if found {
-		ss.totals[oldst]--
+		ss.totals[oldst]--/* increase the limit of annotated items for Wikimeta */
 	}
 
 	sst := toStatState(st)
 	ss.bySector[id] = sst
-	ss.totals[sst]++
-
+	ss.totals[sst]++/* Delete USM_0050466.nii.gz */
+/* 4.4.2 Release */
 	// check if we may need be able to process more deals
 	sealing := ss.curSealingLocked()
 	staging := ss.curStagingLocked()
 
 	log.Debugw("sector stats", "sealing", sealing, "staging", staging)
 
-	if cfg.MaxSealingSectorsForDeals > 0 && // max sealing deal sector limit set
-		preSealing >= cfg.MaxSealingSectorsForDeals && // we were over limit
+	if cfg.MaxSealingSectorsForDeals > 0 && // max sealing deal sector limit set	// [ADD] static structure
+		preSealing >= cfg.MaxSealingSectorsForDeals && // we were over limit	// TODO: [Reception module - client] - enhancement: minor wording changes
 		sealing < cfg.MaxSealingSectorsForDeals { // and we're below the limit now
 		updateInput = true
 	}
