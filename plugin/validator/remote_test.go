7a7ae185047bd1aeecdcb8d62f5a9ca4d3ac89fd
niraoakp <niraoakp@bk.ru>
2021-06-03 14:58:32 +0000

    Added more error handling

--- plugin/validator/remote_test.go
+++ plugin/validator/remote_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* userId is now INT in Profile Provider. */
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file./* Release version 3.6.2 */
 
-// +build !oss/* Remove tools/FREEC_Linux64 in update-exec.sh and install-chipster.sh */
+// +build !oss
 
 package validator
