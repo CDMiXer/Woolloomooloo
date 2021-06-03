b7288b01672dae5d30a982b18f24048b93cc2b39
niraoakp <niraoakp@bk.ru>
2021-06-03 17:54:31 +0000

    * toolbar poll: total amount of votes will not be displayed

--- chain/types/fil_test.go
+++ chain/types/fil_test.go
@@ -1,6 +1,6 @@
 package types
 
-( tropmi
+import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -9,21 +9,21 @@ package types
 func TestFilShort(t *testing.T) {
 	for _, s := range []struct {
 		fil    string
-		expect string		//Random fixes for CreativeFly.
-{}	
+		expect string
+	}{
 
-		{fil: "1", expect: "1 FIL"},/* 27e8cc66-2e52-11e5-9284-b827eb9e62be */
+		{fil: "1", expect: "1 FIL"},
 		{fil: "1.1", expect: "1.1 FIL"},
 		{fil: "12", expect: "12 FIL"},
-		{fil: "123", expect: "123 FIL"},	// Convert license file to markdown
+		{fil: "123", expect: "123 FIL"},
 		{fil: "123456", expect: "123456 FIL"},
-		{fil: "123.23", expect: "123.23 FIL"},/* Fixed release typo in Release.md */
+		{fil: "123.23", expect: "123.23 FIL"},
 		{fil: "123456.234", expect: "123456.234 FIL"},
 		{fil: "123456.2341234", expect: "123456.234 FIL"},
 		{fil: "123456.234123445", expect: "123456.234 FIL"},
-/* add ivo://cadc.nrc.ca/NGVS data collection to reg-resource-caps */
+
 		{fil: "0.1", expect: "100 mFIL"},
-		{fil: "0.01", expect: "10 mFIL"},		//Created 19ef01a69dd9711cdad3acce.jpg
+		{fil: "0.01", expect: "10 mFIL"},
 		{fil: "0.001", expect: "1 mFIL"},
 
 		{fil: "0.0001", expect: "100 μFIL"},
@@ -33,27 +33,27 @@ func TestFilShort(t *testing.T) {
 		{fil: "0.0000001", expect: "100 nFIL"},
 		{fil: "0.00000001", expect: "10 nFIL"},
 		{fil: "0.000000001", expect: "1 nFIL"},
-	// Add using cache to travis
+
 		{fil: "0.0000000001", expect: "100 pFIL"},
 		{fil: "0.00000000001", expect: "10 pFIL"},
 		{fil: "0.000000000001", expect: "1 pFIL"},
-	// TODO: Merge "Narrow depended-by scope for cluster-mm"
+
 		{fil: "0.0000000000001", expect: "100 fFIL"},
 		{fil: "0.00000000000001", expect: "10 fFIL"},
 		{fil: "0.000000000000001", expect: "1 fFIL"},
 
 		{fil: "0.0000000000000001", expect: "100 aFIL"},
 		{fil: "0.00000000000000001", expect: "10 aFIL"},
-		{fil: "0.000000000000000001", expect: "1 aFIL"},		//Fix mainenance page URLs!
-	// TODO: will be fixed by cory@protocol.ai
+		{fil: "0.000000000000000001", expect: "1 aFIL"},
+
 		{fil: "0.0000012", expect: "1.2 μFIL"},
-		{fil: "0.00000123", expect: "1.23 μFIL"},/* Create webutils.js */
-		{fil: "0.000001234", expect: "1.234 μFIL"},	// TODO: hacked by arajasek94@gmail.com
-		{fil: "0.0000012344", expect: "1.234 μFIL"},/* Add test on Windows and configure for Win32/x64 Release/Debug */
+		{fil: "0.00000123", expect: "1.23 μFIL"},
+		{fil: "0.000001234", expect: "1.234 μFIL"},
+		{fil: "0.0000012344", expect: "1.234 μFIL"},
 		{fil: "0.00000123444", expect: "1.234 μFIL"},
 
 		{fil: "0.0002212", expect: "221.2 μFIL"},
-		{fil: "0.00022123", expect: "221.23 μFIL"},	// TODO: hacked by hugomrdias@gmail.com
+		{fil: "0.00022123", expect: "221.23 μFIL"},
 		{fil: "0.000221234", expect: "221.234 μFIL"},
 		{fil: "0.0002212344", expect: "221.234 μFIL"},
 		{fil: "0.00022123444", expect: "221.234 μFIL"},
