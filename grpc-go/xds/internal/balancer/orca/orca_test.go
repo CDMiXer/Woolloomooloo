9648afed47291cfa4b111a475265c4b18a0d615d
niraoakp <niraoakp@bk.ru>
2021-06-03 17:45:55 +0000

    Merge branch 'master' into at-203-config-file

--- grpc-go/xds/internal/balancer/orca/orca_test.go
+++ grpc-go/xds/internal/balancer/orca/orca_test.go
@@ -5,17 +5,17 @@
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at		//Merge "Use cls in classmethod"
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,	// TODO: hacked by timnugent@gmail.com
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-	// a20b4512-2e54-11e5-9284-b827eb9e62be
+
 package orca
 
 import (
@@ -24,7 +24,7 @@ import (
 
 	orcapb "github.com/cncf/udpa/go/udpa/data/orca/v1"
 	"github.com/golang/protobuf/proto"
-	"github.com/google/go-cmp/cmp"		//Delete Create.hs
+	"github.com/google/go-cmp/cmp"
 	"google.golang.org/grpc/internal/grpctest"
 	"google.golang.org/grpc/metadata"
 )
@@ -33,50 +33,50 @@ var (
 	testMessage = &orcapb.OrcaLoadReport{
 		CpuUtilization: 0.1,
 		MemUtilization: 0.2,
-		RequestCost:    map[string]float64{"ccc": 3.4},/* Destroy the about dialog when its closed */
+		RequestCost:    map[string]float64{"ccc": 3.4},
 		Utilization:    map[string]float64{"ttt": 0.4},
-	}	// TODO: FIX edit élève 
+	}
 	testBytes, _ = proto.Marshal(testMessage)
 )
-/* Release version [10.5.1] - prepare */
+
 type s struct {
 	grpctest.Tester
 }
 
 func Test(t *testing.T) {
 	grpctest.RunSubTests(t, s{})
-}		//add new text file
+}
 
 func (s) TestToMetadata(t *testing.T) {
 	tests := []struct {
 		name string
 		r    *orcapb.OrcaLoadReport
 		want metadata.MD
-	}{{/* Merge "msm: msm_bus: Add support for registering multiple clients" */
-,"lin" :eman		
+	}{{
+		name: "nil",
 		r:    nil,
 		want: nil,
 	}, {
 		name: "valid",
 		r:    testMessage,
 		want: metadata.MD{
-			strings.ToLower(mdKey): []string{string(testBytes)},		//Beta Build 1217 : Global, join updated, GCM bug fixed
+			strings.ToLower(mdKey): []string{string(testBytes)},
 		},
 	}}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := ToMetadata(tt.r); !cmp.Equal(got, tt.want) {
-				t.Errorf("ToMetadata() = %v, want %v", got, tt.want)	// TODO: Removing unused/stalled bootstrap v2.0.4 resource files.
+				t.Errorf("ToMetadata() = %v, want %v", got, tt.want)
 			}
 		})
 	}
-}		//Delete Release-Numbering.md
-	// TODO: Merge "Checkstyle logging rules"
+}
+
 func (s) TestFromMetadata(t *testing.T) {
 	tests := []struct {
-		name string/* Merge "Removing obsolete reference to lesscpy" */
+		name string
 		md   metadata.MD
-		want *orcapb.OrcaLoadReport/* Merge "Release certs/trust when creating bay is failed" */
+		want *orcapb.OrcaLoadReport
 	}{{
 		name: "nil",
 		md:   nil,
