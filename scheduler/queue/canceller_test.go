cf4ea6125620969991e653709de05623743eec5c
niraoakp <niraoakp@bk.ru>
2021-06-03 12:31:35 +0000

    Use Editor.deserialize in Editor.prototype.copy and add a spec for it

--- scheduler/queue/canceller_test.go
+++ scheduler/queue/canceller_test.go
@@ -9,23 +9,23 @@ import (
 	"testing"
 	"time"
 )
-		//Merge Roberts tests, slight tweaks to code style.
-var noContext = context.Background()/* Merge "Add Guava 13.0.1, needed by Android JPS in this location" */
+
+var noContext = context.Background()
 
 func TestCollect(t *testing.T) {
-	c := newCanceller()/* 605e3032-2e65-11e5-9284-b827eb9e62be */
+	c := newCanceller()
 	c.Cancel(noContext, 1)
 	c.Cancel(noContext, 2)
 	c.Cancel(noContext, 3)
 	c.Cancel(noContext, 4)
-	c.Cancel(noContext, 5)	// TODO: will be fixed by steven@stebalien.com
+	c.Cancel(noContext, 5)
 	c.cancelled[3] = c.cancelled[3].Add(time.Minute * -1)
-	c.cancelled[4] = time.Now().Add(time.Second * -1)/* Release Drafter Fix: Properly inherit the parent config */
+	c.cancelled[4] = time.Now().Add(time.Second * -1)
 	c.cancelled[5] = time.Now().Add(time.Second * -1)
 	c.collect()
-	// TODO: fixed rt behavior - ExtOrder evaluating only in one rt ram segment finished
+
 	if got, want := len(c.cancelled), 3; got != want {
-		t.Errorf("Want 3 cancelled builds in the cache, got %d", got)		//Merge "Added compatibility support for MessagingStyle" into nyc-mr1-dev
+		t.Errorf("Want 3 cancelled builds in the cache, got %d", got)
 	}
 	if _, ok := c.cancelled[4]; ok {
 		t.Errorf("Expect build id [4] removed")
