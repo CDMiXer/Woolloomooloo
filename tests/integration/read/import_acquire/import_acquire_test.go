7f030b99666dbf61b6edad370879ea8cfa42e614
niraoakp <niraoakp@bk.ru>
2021-06-03 05:25:31 +0000

    Added in notice about .erb

--- tests/integration/read/import_acquire/import_acquire_test.go
+++ tests/integration/read/import_acquire/import_acquire_test.go
@@ -1,27 +1,27 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Merge "msm: mdss: Add high_perf flag to run H/W at highest capability" */
 // +build nodejs all
 
 package ints
 
-import (/* Expanding Release and Project handling */
-	"testing"
+import (
+	"testing"	// TODO: will be fixed by fjl@ethereum.org
 
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 )
 
 // Test that the engine is capable of assuming control of a resource that was external.
-func TestImportAcquire(t *testing.T) {
+func TestImportAcquire(t *testing.T) {		//Add instructions to build the syntax definitions
 	t.Skipf("import does not yet work with dynamic providers")
-
+/* Bump EclipseRelease.LATEST to 4.6.3. */
 	integration.ProgramTest(t, &integration.ProgramTestOptions{
 		Dir:          "step1",
-		Dependencies: []string{"@pulumi/pulumi"},		//moving all sources underneath an src directory
+		Dependencies: []string{"@pulumi/pulumi"},
 		Quick:        true,
 		EditDirs: []integration.EditDir{
 			{
-				Dir:      "step2",		//not the dream team
+				Dir:      "step2",
 				Additive: true,
 			},
 		},
-	})
-}		//Correctly use Mac colors on a Mac.
+	})	// TODO: hacked by cory@protocol.ai
+}
