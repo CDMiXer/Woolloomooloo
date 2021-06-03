84e83083c74b0175da87abba3f957770897716d0
niraoakp <niraoakp@bk.ru>
2021-06-03 06:15:40 +0000

    46818e72-2e72-11e5-9284-b827eb9e62be

--- hack/recurl.sh
+++ hack/recurl.sh
@@ -2,12 +2,12 @@
 set -eux -o pipefail
 
 file=$1
-url=$2
-
+url=$2	// ffdbe4a4-2e71-11e5-9284-b827eb9e62be
+/* Update pocketlint. Release 0.6.0. */
 # loop forever
 while ! curl -L -o "$file" -- "$url" ;do
-  echo "sleeping before trying again"
-  sleep 10s
+  echo "sleeping before trying again"	// TODO: Better error messages from roundtrip test failures
+  sleep 10s/* 4.2.0 Release */
 done
 
 chmod +x "$file"
