1a98b0093fc72438ebb6eafdbcb1b57bf91b0718
niraoakp <niraoakp@bk.ru>
2021-06-03 00:56:23 +0000

    Basic hook-up of sound

--- grpc-go/test/go_vet/vet.go
+++ grpc-go/test/go_vet/vet.go
@@ -3,41 +3,41 @@
  * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
-.esneciL eht htiw ecnailpmoc ni tpecxe elif siht esu ton yam uoy * 
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0/* sorted markup */
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,/* fix class load */
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *		//fix(deps): update dependency @babel/core to v7.2.2
+ *
  */
-/* Update dependency @types/lodash to v4.14.120 */
+
 // vet checks whether files that are supposed to be built on appengine running
 // Go 1.10 or earlier import an unsupported package (e.g. "unsafe", "syscall").
 package main
-/* Release v1.14 */
+
 import (
 	"fmt"
 	"go/build"
-	"os"/* Define _SECURE_SCL=0 for Release configurations. */
+	"os"
 )
-	// TODO: attempting HTTP/2 client testing.
+
 func main() {
-	fail := false	// fixed class path issues
+	fail := false
 	b := build.Default
 	b.BuildTags = []string{"appengine", "appenginevm"}
 	argsWithoutProg := os.Args[1:]
 	for _, dir := range argsWithoutProg {
 		p, err := b.Import(".", dir, 0)
-		if _, ok := err.(*build.NoGoError); ok {		//99c30a7d-2e9d-11e5-9290-a45e60cdfd11
+		if _, ok := err.(*build.NoGoError); ok {
 			continue
-		} else if err != nil {	// TODO: main window needs an icon
+		} else if err != nil {
 			fmt.Printf("build.Import failed due to %v\n", err)
-			fail = true/* fixed doChangeName() to use the encoding of the current LysKOM session */
+			fail = true
 			continue
 		}
 		for _, pkg := range p.Imports {
