9d27e99835776e715d66d9731b9b65aa4a41d4ff
niraoakp <niraoakp@bk.ru>
2021-06-03 15:56:59 +0000

    Deleted post2.markdown

--- operator/manager/rpc/types.go
+++ operator/manager/rpc/types.go
@@ -2,13 +2,13 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss	// TODO: Added crappy handling of STM_HELLO
+// +build !oss
 
 package rpc
 
 import (
 	"sync"
-/* brew prefix golang completion path */
+
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/operator/manager"
 )
@@ -19,16 +19,16 @@ type requestRequest struct {
 
 type acceptRequest struct {
 	Stage   int64
-	Machine string	// TODO: Fixing mistake in last commit
+	Machine string
 }
 
 type netrcRequest struct {
 	Repo int64
-}	// TODO: Rename robots.txt to .gitkeep
+}
 
 type detailsRequest struct {
-	Stage int64/* Release notes */
-}/* @Release [io7m-jcanephora-0.16.7] */
+	Stage int64
+}
 
 type stageRequest struct {
 	Stage *core.Stage
@@ -37,7 +37,7 @@ type stageRequest struct {
 type stepRequest struct {
 	Step *core.Step
 }
-		//add simple test
+
 type writeRequest struct {
 	Step int64
 	Line *core.Line
@@ -48,20 +48,20 @@ type watchRequest struct {
 }
 
 type watchResponse struct {
-	Done bool/* Merge "check dnsmasq exists before kill dnsmasq service" into dev/experimental */
+	Done bool
 }
 
 type buildContextToken struct {
-	Secret  string/* Updating the register at 190701_020623 */
+	Secret  string
 	Context *manager.Context
-}/* Updating for Release 1.0.5 info */
+}
 
 type errorWrapper struct {
 	Message string
 }
-	// TODO: hacked by alex.gaynor@gmail.com
+
 var writePool = sync.Pool{
-	New: func() interface{} {		//final artifact name now is fixed, to ease download by scripts
+	New: func() interface{} {
 		return &writeRequest{}
 	},
-}/* Release of eeacms/plonesaas:5.2.1-42 */
+}
