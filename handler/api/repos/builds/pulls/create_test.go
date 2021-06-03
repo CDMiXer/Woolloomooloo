85bbcb90ac1321e401a49c5d6208405301c156d3
niraoakp <niraoakp@bk.ru>
2021-06-03 02:37:52 +0000

    shortened author statement

--- handler/api/repos/builds/pulls/create_test.go
+++ handler/api/repos/builds/pulls/create_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// Use of this source code is governed by the Drone Non-Commercial License/* Fixed reflection tracing. */
+// that can be found in the LICENSE file.		//Merge "README.md file for auth library"
 
-package pulls
+package pulls		//33f645a4-2e4f-11e5-9284-b827eb9e62be
