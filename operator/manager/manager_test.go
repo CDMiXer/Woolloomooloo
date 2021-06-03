b0841ddfaede31f375e35fd2645bc92169f1c596
niraoakp <niraoakp@bk.ru>
2021-06-03 13:34:04 +0000

    Added sencha tools install commands (needs java apt package)

--- operator/manager/manager_test.go
+++ operator/manager/manager_test.go
@@ -1,15 +1,15 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: Require bluebird and mongoose
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-	// TODO: hacked by martin2cai@hotmail.com
+
 package manager
 
-import (/* Update Ref Arch Link to Point to the 1.12 Release */
+import (
 	"io/ioutil"
-/* Update Release notes to have <ul><li> without <p> */
+
 	"github.com/sirupsen/logrus"
 )
 
-func init() {/* Release v3.2.2 */
-	logrus.SetOutput(ioutil.Discard)		//bumped path level to force npm registry update
-}
+func init() {
+	logrus.SetOutput(ioutil.Discard)
+}/* Delete JarCheck.java */
