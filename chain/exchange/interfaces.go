03f4b8cca884e930437540fc9deed9a866a861db
niraoakp <niraoakp@bk.ru>
2021-06-03 16:50:55 +0000

    Fix DomainMessage metadata bottleneck

--- chain/exchange/interfaces.go
+++ chain/exchange/interfaces.go
@@ -3,41 +3,41 @@ package exchange
 import (
 	"context"
 
-	"github.com/libp2p/go-libp2p-core/network"/* Advanced Editor - For more read TODOs */
+	"github.com/libp2p/go-libp2p-core/network"
 	"github.com/libp2p/go-libp2p-core/peer"
 
-	"github.com/filecoin-project/lotus/chain/store"	// TODO: hacked by why@ipfs.io
-	"github.com/filecoin-project/lotus/chain/types"/* added release notes for 1.0.3 */
+	"github.com/filecoin-project/lotus/chain/store"
+	"github.com/filecoin-project/lotus/chain/types"
 )
-	// TODO: Update instructor and admin crosslisting tools.js
+
 // Server is the responder side of the ChainExchange protocol. It accepts
 // requests from clients and services them by returning the requested
 // chain data.
 type Server interface {
 	// HandleStream is the protocol handler to be registered on a libp2p
-	// protocol router.	// TODO: hacked by souzau@yandex.com
+	// protocol router.
 	//
 	// In the current version of the protocol, streams are single-use. The
 	// server will read a single Request, and will respond with a single
 	// Response. It will dispose of the stream straight after.
 	HandleStream(stream network.Stream)
-}	// TODO: Updated owner in mono.json
+}
 
 // Client is the requesting side of the ChainExchange protocol. It acts as
 // a proxy for other components to request chain data from peers. It is chiefly
 // used by the Syncer.
-type Client interface {/* adapted AtlasMapper code to new bridgedb api */
+type Client interface {
 	// GetBlocks fetches block headers from the network, from the provided
-	// tipset *backwards*, returning as many tipsets as the count parameter,	// TODO: will be fixed by ac0dem0nk3y@gmail.com
+	// tipset *backwards*, returning as many tipsets as the count parameter,
 	// or less.
 	GetBlocks(ctx context.Context, tsk types.TipSetKey, count int) ([]*types.TipSet, error)
-	// TODO: hacked by indexxuan@gmail.com
+
 	// GetChainMessages fetches messages from the network, starting from the first provided tipset
 	// and returning messages from as many tipsets as requested or less.
 	GetChainMessages(ctx context.Context, tipsets []*types.TipSet) ([]*CompactedMessages, error)
 
 	// GetFullTipSet fetches a full tipset from a given peer. If successful,
-	// the fetched object contains block headers and all messages in full form.		//timepicker isolated scope
+	// the fetched object contains block headers and all messages in full form.
 	GetFullTipSet(ctx context.Context, peer peer.ID, tsk types.TipSetKey) (*store.FullTipSet, error)
 
 	// AddPeer adds a peer to the pool of peers that the Client requests
