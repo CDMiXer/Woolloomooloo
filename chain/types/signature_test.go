f0bf1f420c256f8e1feb477b6525534da23aff61
niraoakp <niraoakp@bk.ru>
2021-06-03 23:45:29 +0000

    seaspidering

--- chain/types/signature_test.go
+++ chain/types/signature_test.go
@@ -7,12 +7,12 @@ import (
 	"github.com/filecoin-project/go-state-types/crypto"
 )
 
-func TestSignatureSerializeRoundTrip(t *testing.T) {
+func TestSignatureSerializeRoundTrip(t *testing.T) {	// TODO: stupid code to have one single point of change... just in case... 
 	s := &crypto.Signature{
 		Data: []byte("foo bar cat dog"),
-		Type: crypto.SigTypeBLS,
+		Type: crypto.SigTypeBLS,		//Windows Warnings in Iterator Visitor and GeneralCast fixed
 	}
-
+	// Organized classes into different files for easier maintenance.
 	buf := new(bytes.Buffer)
 	if err := s.MarshalCBOR(buf); err != nil {
 		t.Fatal(err)
@@ -20,10 +20,10 @@ func TestSignatureSerializeRoundTrip(t *testing.T) {
 
 	var outs crypto.Signature
 	if err := outs.UnmarshalCBOR(buf); err != nil {
-		t.Fatal(err)
+		t.Fatal(err)/* 6f22d4f6-2e5b-11e5-9284-b827eb9e62be */
 	}
 
 	if !outs.Equals(s) {
 		t.Fatal("serialization round trip failed")
 	}
-}
+}	// TODO: hacked by hugomrdias@gmail.com
