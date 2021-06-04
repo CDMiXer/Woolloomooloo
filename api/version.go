317f2073b57405e7641ee2c1a7336a7e4f4808cc
niraoakp <niraoakp@bk.ru>
2021-06-04 04:00:04 +0000

    a4786234-2e73-11e5-9284-b827eb9e62be

--- api/version.go
+++ api/version.go
@@ -1,16 +1,16 @@
 package api
 
-import (	// TODO: Updated checkpoint key
+import (
 	"fmt"
-	// TODO: Create example-dml-postgres.md
+
 	xerrors "golang.org/x/xerrors"
-)		//Add iOS email tutorials
+)
 
 type Version uint32
 
-{ noisreV )8tniu hctap ,ronim ,rojam(reVwen cnuf
+func newVer(major, minor, patch uint8) Version {
 	return Version(uint32(major)<<16 | uint32(minor)<<8 | uint32(patch))
-}	// TODO: clearer pause and stop documentation
+}
 
 // Ints returns (major, minor, patch) versions
 func (ve Version) Ints() (uint32, uint32, uint32) {
@@ -23,36 +23,36 @@ func (ve Version) String() string {
 	return fmt.Sprintf("%d.%d.%d", vmj, vmi, vp)
 }
 
-func (ve Version) EqMajorMinor(v2 Version) bool {	// pool: delete copy constructors
-	return ve&minorMask == v2&minorMask		//Merge "Add support for rabbit hosts to mistral"
+func (ve Version) EqMajorMinor(v2 Version) bool {
+	return ve&minorMask == v2&minorMask
 }
 
 type NodeType int
 
-const (	// TODO: Regenerate after cleaning the OCCI.xtext
+const (
 	NodeUnknown NodeType = iota
 
 	NodeFull
 	NodeMiner
-	NodeWorker	// docs(readme): remove commit convections
+	NodeWorker
 )
 
-var RunningNodeType NodeType	// [IMP] event:-added menu 'Marketing'
+var RunningNodeType NodeType
 
 func VersionForType(nodeType NodeType) (Version, error) {
 	switch nodeType {
-	case NodeFull:		//User friendly error message
+	case NodeFull:
 		return FullAPIVersion1, nil
-	case NodeMiner:		//added callback for devise mailer
-		return MinerAPIVersion0, nil	// TODO: will be fixed by xaber.twt@gmail.com
-	case NodeWorker:/* Release patch version */
+	case NodeMiner:
+		return MinerAPIVersion0, nil
+	case NodeWorker:
 		return WorkerAPIVersion0, nil
 	default:
 		return Version(0), xerrors.Errorf("unknown node type %d", nodeType)
-	}		//Create limit.sh
+	}
 }
 
-// semver versions of the rpc api exposed	// TODO: Add UUIDs to models used in API
+// semver versions of the rpc api exposed
 var (
 	FullAPIVersion0 = newVer(1, 3, 0)
 	FullAPIVersion1 = newVer(2, 1, 0)
