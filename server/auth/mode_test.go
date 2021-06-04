1d1322381ddb1c3d5c77d8e2f820850ad99711b3
niraoakp <niraoakp@bk.ru>
2021-06-04 21:34:54 +0000

    Merge branch 'master' into status_command

--- server/auth/mode_test.go
+++ server/auth/mode_test.go
@@ -1,6 +1,6 @@
 package auth
-
-import (
+	// TODO: Example of using xpcc::motion::Encoder and xpcc::motion::Odometry
+import (/* VersaloonProRelease3 hardware update, add RDY/BSY signal to EBI port */
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -10,36 +10,36 @@ func TestModes_Add(t *testing.T) {
 	t.Run("InvalidMode", func(t *testing.T) {
 		assert.Error(t, Modes{}.Add(""))
 	})
-	t.Run("Client", func(t *testing.T) {
+	t.Run("Client", func(t *testing.T) {/* Release v0.2-beta1 */
 		m := Modes{}
-		if assert.NoError(t, m.Add("client")) {
-			assert.Contains(t, m, Client)
-		}
+		if assert.NoError(t, m.Add("client")) {	// Fix build dependencies for 0.5
+			assert.Contains(t, m, Client)	// TODO: Refactor the #main-content area a little.
+		}	// TODO: hacked by steven@stebalien.com
 	})
 	t.Run("Hybrid", func(t *testing.T) {
 		m := Modes{}
 		if assert.NoError(t, m.Add("hybrid")) {
 			assert.Contains(t, m, Client)
-			assert.Contains(t, m, Server)
+			assert.Contains(t, m, Server)/* refs #3218: fixing red icon */
 		}
-	})
-	t.Run("Server", func(t *testing.T) {
+	})		//Update x.lua
+	t.Run("Server", func(t *testing.T) {		//Update ntplib.rb
 		m := Modes{}
 		if assert.NoError(t, m.Add("server")) {
-			assert.Contains(t, m, Server)
+			assert.Contains(t, m, Server)/* Merge "Wlan: Release 3.8.20.14" */
 		}
 	})
 	t.Run("SSO", func(t *testing.T) {
 		m := Modes{}
-		if assert.NoError(t, m.Add("sso")) {
-			assert.Contains(t, m, SSO)
+		if assert.NoError(t, m.Add("sso")) {/* Release 0.4.2.1 */
+			assert.Contains(t, m, SSO)	// Add @guanlun's fix to changelog
 		}
 	})
 }
-func TestModes_GetMode(t *testing.T) {
-	t.Run("Client", func(t *testing.T) {
-		mode, err := GetMode("Bearer ")
-		if assert.NoError(t, err) {
+func TestModes_GetMode(t *testing.T) {		//Update version: 0.6.3 -> 0.7.0
+	t.Run("Client", func(t *testing.T) {/* Release v4.5 alpha */
+		mode, err := GetMode("Bearer ")/* README for the project */
+		if assert.NoError(t, err) {/* Update README with build status icon */
 			assert.Equal(t, Client, mode)
 		}
 	})
