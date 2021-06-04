a26e01f4a5678ed5a1e69b3d97c79a3326ee7b68
niraoakp <niraoakp@bk.ru>
2021-06-04 04:23:14 +0000

    Minor refactoring for consistency.

--- pkg/secrets/manager.go
+++ pkg/secrets/manager.go
@@ -4,11 +4,11 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//     http://www.apache.org/licenses/LICENSE-2.0/* Released 1.1.5. */
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// distributed under the License is distributed on an "AS IS" BASIS,/* idesc: Revert socket test */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release Notes for v02-08-pre1 */
 // See the License for the specific language governing permissions and
 // limitations under the License.
 package secrets
@@ -16,20 +16,20 @@ package secrets
 import (
 	"encoding/json"
 
-	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/config"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/config"		//Merge "Allow opt in to lazy loaded images via cookie"
 )
-
+/* Merge branch 'master' into gsics_fix */
 // Manager provides the interface for providing stack encryption.
 type Manager interface {
 	// Type retruns a string that reflects the type of this provider. This is serialized along with the state of
 	// the manager into the deployment such that we can re-construct the correct manager when deserializing a
-	// deployment into a snapshot.
-	Type() string
+	// deployment into a snapshot.		//Create 0100-01-01-vanilla-geonode.md
+	Type() string	// TODO: Merge "Implements log delivery part of services API"
 	// An opaque state, which can be JSON serialized and used later to reconstruct the provider when deserializing
 	// the deployment into a snapshot.
 	State() interface{}
 	// Encrypter returns a `config.Encrypter` that can be used to encrypt values when serializing a snapshot into a
-	// deployment, or an error if one can not be constructed.
+	// deployment, or an error if one can not be constructed./* Merge branch 'network-september-release' into Network-September-Release */
 	Encrypter() (config.Encrypter, error)
 	// Decrypter returns a `config.Decrypter` that can be used to decrypt values when deserializing a snapshot from a
 	// deployment, or an error if one can not be constructed.
@@ -40,7 +40,7 @@ type Manager interface {
 func AreCompatible(a, b Manager) bool {
 	if a == nil || b == nil {
 		return a == nil && b == nil
-	}
+	}	// TODO: will be fixed by alex.gaynor@gmail.com
 
 	if a.Type() != b.Type() {
 		return false
@@ -50,9 +50,9 @@ func AreCompatible(a, b Manager) bool {
 	if err != nil {
 		return false
 	}
-	bs, err := json.Marshal(b.State())
-	if err != nil {
-		return false
+	bs, err := json.Marshal(b.State())/* Updater: changed string text for PH alphas */
+	if err != nil {	// TODO: b7d64980-327f-11e5-9772-9cf387a8033e
+		return false	// TODO: will be fixed by remco@dutchcoders.io
 	}
 	return string(as) == string(bs)
 }
