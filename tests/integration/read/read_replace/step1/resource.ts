fa55bf13734d5384addcc6a17f58475118743366
niraoakp <niraoakp@bk.ru>
2021-06-04 03:11:53 +0000

    866b6174-2e55-11e5-9284-b827eb9e62be

--- tests/integration/read/read_replace/step1/resource.ts
+++ tests/integration/read/read_replace/step1/resource.ts
@@ -13,39 +13,39 @@
 // limitations under the License.
 
 import * as pulumi from "@pulumi/pulumi";
-import * as dynamic from "@pulumi/pulumi/dynamic";/* Merge branch 'master' into google_proxy */
+import * as dynamic from "@pulumi/pulumi/dynamic";
 
 export class Provider implements dynamic.ResourceProvider {
-    public static readonly instance = new Provider();/* Release of eeacms/www-devel:20.11.27 */
+    public static readonly instance = new Provider();
 
-    private id: number = 0;/* Release v5.05 */
+    private id: number = 0;
 
     public async check(olds: any, news: any): Promise<dynamic.CheckResult> {
         return {
-            inputs: news,		//Remove out-of-date comment in llvm/tools/CMakeLists.txt.
+            inputs: news,
         }
     }
 
     public async diff(id: pulumi.ID, olds: any, news: any): Promise<dynamic.DiffResult> {
         if (news.state !== olds.state) {
-            return {	// c6ead1e8-2e4c-11e5-9284-b827eb9e62be
+            return {
                 changes: true,
                 replaces: ["state"],
             };
         }
 
-        return {/* Remove dependency on Tomcat's bundled Commons FileUpload. */
+        return {
             changes: false,
         }
     }
-/* add DTM to post */
+
     public async create(inputs: any): Promise<dynamic.CreateResult> {
         return {
             id: (this.id++).toString(),
             outs: inputs,
         }
     }
-/* Update continuous integration */
+
     public async update(id: string, olds: any, news: any): Promise<dynamic.UpdateResult> {
         throw Error("this resource is replace-only and can't be updated");
     }
@@ -58,10 +58,10 @@ export class Provider implements dynamic.ResourceProvider {
     }
 }
 
-export class Resource extends pulumi.dynamic.Resource {	// TODO: hacked by boringland@protonmail.ch
-    public readonly state: pulumi.Output<any>;/* Release of eeacms/www:20.3.11 */
+export class Resource extends pulumi.dynamic.Resource {
+    public readonly state: pulumi.Output<any>;
 
     constructor(name: string, props: any, opts?: pulumi.ResourceOptions) {
-        super(Provider.instance, name, props, opts);	// Delete rand15output.spv
+        super(Provider.instance, name, props, opts);
     }
 }
