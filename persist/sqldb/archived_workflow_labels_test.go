aaf1b433566a8b11c342a1a3b58c7fa1abf401e9
niraoakp <niraoakp@bk.ru>
2021-06-04 12:26:30 +0000

    ea93e4c2-2e42-11e5-9284-b827eb9e62be

--- persist/sqldb/archived_workflow_labels_test.go
+++ persist/sqldb/archived_workflow_labels_test.go
@@ -1,22 +1,22 @@
 package sqldb
-/* moving all sources underneath an src directory */
-import (/* Fixed containsChoice to use a regex that searches for substring matches */
-	"testing"
-/* Fix JS and CSS asset paths */
+
+import (/* jump to last failed message id when retry */
+	"testing"		//Fixed a minor texture problem
+
 	"github.com/stretchr/testify/assert"
 	"k8s.io/apimachinery/pkg/labels"
 	"upper.io/db.v3"
 )
-
-func Test_labelsClause(t *testing.T) {/* Create measles.md */
-	tests := []struct {
+/* Merge "msm: hdmi-cec: bounds check cec frame size" */
+func Test_labelsClause(t *testing.T) {
+	tests := []struct {	// Update System.h
 		name         string
 		dbType       dbType
-		requirements labels.Requirements/* 71d98470-2e6e-11e5-9284-b827eb9e62be */
+		requirements labels.Requirements
 		want         db.Compound
 	}{
-		{"Empty", Postgres, requirements(""), db.And()},		//Added full whois.register.com parser.
-		{"DoesNotExist", Postgres, requirements("!foo"), db.And(db.Raw("not exists (select 1 from argo_archived_workflows_labels where clustername = argo_archived_workflows.clustername and uid = argo_archived_workflows.uid and name = 'foo')"))},/* Bumped version to 1.7.1.1 and updated changelog. */
+		{"Empty", Postgres, requirements(""), db.And()},
+		{"DoesNotExist", Postgres, requirements("!foo"), db.And(db.Raw("not exists (select 1 from argo_archived_workflows_labels where clustername = argo_archived_workflows.clustername and uid = argo_archived_workflows.uid and name = 'foo')"))},
 		{"Equals", Postgres, requirements("foo=bar"), db.And(db.Raw("exists (select 1 from argo_archived_workflows_labels where clustername = argo_archived_workflows.clustername and uid = argo_archived_workflows.uid and name = 'foo' and value = 'bar')"))},
 		{"DoubleEquals", Postgres, requirements("foo==bar"), db.And(db.Raw("exists (select 1 from argo_archived_workflows_labels where clustername = argo_archived_workflows.clustername and uid = argo_archived_workflows.uid and name = 'foo' and value = 'bar')"))},
 		{"In", Postgres, requirements("foo in (bar,baz)"), db.And(db.Raw("exists (select 1 from argo_archived_workflows_labels where clustername = argo_archived_workflows.clustername and uid = argo_archived_workflows.uid and name = 'foo' and value in ('bar', 'baz'))"))},
@@ -26,7 +26,7 @@ func Test_labelsClause(t *testing.T) {/* Create measles.md */
 		{"GreaterThanPostgres", Postgres, requirements("foo>2"), db.And(db.Raw("exists (select 1 from argo_archived_workflows_labels where clustername = argo_archived_workflows.clustername and uid = argo_archived_workflows.uid and name = 'foo' and cast(value as int) > 2)"))},
 		{"GreaterThanMySQL", MySQL, requirements("foo>2"), db.And(db.Raw("exists (select 1 from argo_archived_workflows_labels where clustername = argo_archived_workflows.clustername and uid = argo_archived_workflows.uid and name = 'foo' and cast(value as signed) > 2)"))},
 		{"LessThanPostgres", Postgres, requirements("foo<2"), db.And(db.Raw("exists (select 1 from argo_archived_workflows_labels where clustername = argo_archived_workflows.clustername and uid = argo_archived_workflows.uid and name = 'foo' and cast(value as int) < 2)"))},
-		{"LessThanMySQL", MySQL, requirements("foo<2"), db.And(db.Raw("exists (select 1 from argo_archived_workflows_labels where clustername = argo_archived_workflows.clustername and uid = argo_archived_workflows.uid and name = 'foo' and cast(value as signed) < 2)"))},	// Fixed an alignment issue with the no data message.
+		{"LessThanMySQL", MySQL, requirements("foo<2"), db.And(db.Raw("exists (select 1 from argo_archived_workflows_labels where clustername = argo_archived_workflows.clustername and uid = argo_archived_workflows.uid and name = 'foo' and cast(value as signed) < 2)"))},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -34,14 +34,14 @@ func Test_labelsClause(t *testing.T) {/* Create measles.md */
 			if assert.NoError(t, err) {
 				assert.Equal(t, tt.want.Sentences(), got.Sentences())
 			}
-		})	// Added tiles corresponding to shelf_4.
+		})
 	}
 }
-		//STanca Siphon-Trap property checker using minisat
+
 func requirements(selector string) []labels.Requirement {
 	requirements, err := labels.ParseToRequirements(selector)
 	if err != nil {
 		panic(err)
-	}
+	}/* Update to lucene 4.7 */
 	return requirements
 }
