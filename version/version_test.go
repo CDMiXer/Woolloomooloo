dd0b0fcf9b70c2024647ea691b206105895a6cd9
niraoakp <niraoakp@bk.ru>
2021-06-04 01:29:50 +0000

    fixes for serial port. it works with real hardware!

--- version/version_test.go
+++ version/version_test.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License	// TODO: 1e3e1e9a-2e4e-11e5-9284-b827eb9e62be
 // that can be found in the LICENSE file.
-	// TODO: 7892f286-2d53-11e5-baeb-247703a38240
-// +build !oss
 
-package version
-	// TODO: Removed dependency on jsonpath-object-transform. Fixes #48 (#368)
+// +build !oss	// TODO: Make --incremental a bit faster.
+
+package version/* Renamed card_send_DCW() into card_send_dcw() */
+
 import "testing"
 
 func TestVersion(t *testing.T) {
 	if got, want := Version.String(), "1.9.1"; got != want {
 		t.Errorf("Want version %s, got %s", want, got)
-	}/* Create run_cor_parallel_comparisons.R */
+	}
 }
