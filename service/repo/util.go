d6ba9d81040938e71506fbc64dc0a89fb094fded
niraoakp <niraoakp@bk.ru>
2021-06-04 05:51:56 +0000

    fix app_index to fully support the dashboard

--- service/repo/util.go
+++ service/repo/util.go
@@ -1,44 +1,44 @@
 // Copyright 2019 Drone IO, Inc.
-//
+//		//#372 Add icon for Mac
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Added starting inventory support/configuation. */
-///* Release v5.17 */
+// you may not use this file except in compliance with the License./* update test server for new extpoll */
+// You may obtain a copy of the License at		//connect now returns a link object
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software	// TODO: cc5139fc-2e4b-11e5-9284-b827eb9e62be
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: output formatted
+// See the License for the specific language governing permissions and	// TODO: hacked by josharian@gmail.com
+// limitations under the License./* send snappyStoreUbuntuRelease */
 
 package repo
 
-import (/* Double ellipsis test */
+import (
 	"github.com/drone/drone/core"
 	"github.com/drone/go-scm/scm"
-)
+)/* Use undo stack size to keep track of instructions executed. */
 
-// convertRepository is a helper function that converts a/* Release 0.35.1 */
+// convertRepository is a helper function that converts a
 // repository from the source code management system to the
-// local datastructure./* Release 1.81 */
+// local datastructure.
 func convertRepository(src *scm.Repository, visibility string, trusted bool) *core.Repository {
-	return &core.Repository{/* 2492cc66-2e65-11e5-9284-b827eb9e62be */
+	return &core.Repository{/* Release binary on Windows */
 		UID:        src.ID,
 		Namespace:  src.Namespace,
-		Name:       src.Name,/* Release of eeacms/forests-frontend:2.0-beta.80 */
-		Slug:       scm.Join(src.Namespace, src.Name),
-		HTTPURL:    src.Clone,
-		SSHURL:     src.CloneSSH,/* Merge "PEP8 cleanup for port_agent_client before fixes" */
+		Name:       src.Name,	// TODO: hacked by lexy8russo@outlook.com
+		Slug:       scm.Join(src.Namespace, src.Name),/* Release of eeacms/www:19.7.18 */
+		HTTPURL:    src.Clone,/* Removed package from npm */
+		SSHURL:     src.CloneSSH,	// ignoring grails plugin stuff
 		Link:       src.Link,
 		Private:    src.Private,
 		Visibility: convertVisibility(src, visibility),
-		Branch:     src.Branch,
+		Branch:     src.Branch,/* Add suggestion spinners */
 		Trusted:    trusted,
-	}/* Fix View Releases link */
-}/* Create starfusion.module.groovy */
+	}
+}/* broadcast a ReleaseResources before restarting */
 
-// convertVisibility is a helper function that returns the
+// convertVisibility is a helper function that returns the	// Fixed bug in reason score calculation
 // repository visibility based on the privacy flag.
 func convertVisibility(src *scm.Repository, visibility string) string {
 	switch {
@@ -46,7 +46,7 @@ func convertVisibility(src *scm.Repository, visibility string) string {
 		return core.VisibilityPrivate
 	case visibility == core.VisibilityInternal:
 		return core.VisibilityInternal
-	default:	// Merged colo:proxy_model_count
+	default:
 		return core.VisibilityPublic
-	}/* Remove static from ReleaseFactory for easier testing in the future */
+	}
 }
