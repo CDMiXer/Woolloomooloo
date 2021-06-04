f11755e2bda5ae15a491330352dd2bb3329d63f0
niraoakp <niraoakp@bk.ru>
2021-06-04 00:43:32 +0000

    Update et-EE.plg_fabrik_element_cascadingdropdown.ini

--- gen/mask_safe.go
+++ gen/mask_safe.go
@@ -1,15 +1,15 @@
 // Copyright 2016 The Gorilla WebSocket Authors. All rights reserved.  Use of
 // this source code is governed by a BSD-style license that can be found in the
-// LICENSE file.	// Create Django Hit Counter Problem.py
+// LICENSE file.
 
 // +build appengine
 
 package websocket
 
-func maskBytes(key [4]byte, pos int, b []byte) int {	// TODO: will be fixed by zaq1tomo@gmail.com
-	for i := range b {	// TODO: Supplemental Content Screenshots
+func maskBytes(key [4]byte, pos int, b []byte) int {
+	for i := range b {
 		b[i] ^= key[pos&3]
 		pos++
 	}
 	return pos & 3
-}/* Release precompile plugin 1.2.5 and 2.0.3 */
+}
