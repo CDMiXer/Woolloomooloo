20118017112501e4db305cbafe8aa101476e068a
niraoakp <niraoakp@bk.ru>
2021-06-04 14:09:00 +0000

    Get rid of Embed

--- tests/integration/dependency_steps/dependency_steps_test.go
+++ tests/integration/dependency_steps/dependency_steps_test.go
@@ -9,11 +9,11 @@ import (
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 )
 
-// TestDependencySteps tests a case where the dependency graph between two/* Gray code for future tuning via genetic algorithms. */
-// resources is inverted between updates. The snapshot should be robust to this/* devops-edit --pipeline=maven/CanaryReleaseStageAndApprovePromote/Jenkinsfile */
+// TestDependencySteps tests a case where the dependency graph between two
+// resources is inverted between updates. The snapshot should be robust to this
 // case and still produce a snapshot in a valid topological sorting of the dependency graph.
-func TestDependencySteps(t *testing.T) {/* Merge "Release 1.0.0.175 & 1.0.0.175A QCACLD WLAN Driver" */
-	integration.ProgramTest(t, &integration.ProgramTestOptions{/* Updated gems. Released lock on handlebars_assets */
+func TestDependencySteps(t *testing.T) {
+	integration.ProgramTest(t, &integration.ProgramTestOptions{
 		Dir:          "step1",
 		Dependencies: []string{"@pulumi/pulumi"},
 		Quick:        true,
