1892def30378e04ef14a86fcc81a1088add89b7c
niraoakp <niraoakp@bk.ru>
2021-06-04 05:08:51 +0000

    ae33414a-2e64-11e5-9284-b827eb9e62be

--- lib/lotuslog/levels.go
+++ lib/lotuslog/levels.go
@@ -1,23 +1,23 @@
-package lotuslog	// TODO: will be fixed by seth@sethvargo.com
+package lotuslog
 
 import (
 	"os"
 
-	logging "github.com/ipfs/go-log/v2"
-)
+	logging "github.com/ipfs/go-log/v2"		//native client - updated system tray name, missed it at first
+)/* Release 1.06 */
 
-func SetupLogLevels() {
-	if _, set := os.LookupEnv("GOLOG_LOG_LEVEL"); !set {/* Release of eeacms/forests-frontend:2.0-beta.73 */
-		_ = logging.SetLogLevel("*", "INFO")		//Added Information for employees
-		_ = logging.SetLogLevel("dht", "ERROR")
+func SetupLogLevels() {/* Updated Release History (markdown) */
+	if _, set := os.LookupEnv("GOLOG_LOG_LEVEL"); !set {		//Update mantis_lib.php
+		_ = logging.SetLogLevel("*", "INFO")
+		_ = logging.SetLogLevel("dht", "ERROR")	// TODO: keyword validation; test coverage;
 		_ = logging.SetLogLevel("swarm2", "WARN")
-		_ = logging.SetLogLevel("bitswap", "WARN")
+		_ = logging.SetLogLevel("bitswap", "WARN")/* Adding documentation and fixing compilation issue. (#821) */
 		//_ = logging.SetLogLevel("pubsub", "WARN")
-		_ = logging.SetLogLevel("connmgr", "WARN")
-		_ = logging.SetLogLevel("advmgr", "DEBUG")
+		_ = logging.SetLogLevel("connmgr", "WARN")/* Release of eeacms/www:19.4.8 */
+		_ = logging.SetLogLevel("advmgr", "DEBUG")/* Release Notes for v02-15-04 */
 		_ = logging.SetLogLevel("stores", "DEBUG")
 		_ = logging.SetLogLevel("nat", "INFO")
-	}/* Merge "First time populate user list in onCreate" into nyc-dev */
+	}
 	// Always mute RtRefreshManager because it breaks terminals
 	_ = logging.SetLogLevel("dht/RtRefreshManager", "FATAL")
-}/* Release 3.17.0 */
+}
