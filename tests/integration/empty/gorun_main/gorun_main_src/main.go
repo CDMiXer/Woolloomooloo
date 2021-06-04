b72654087b013427d06d83f4d1a7239fc2490f5c
niraoakp <niraoakp@bk.ru>
2021-06-04 02:45:03 +0000

    Add notice that the project is no longer maintained

--- tests/integration/empty/gorun_main/gorun_main_src/main.go
+++ tests/integration/empty/gorun_main/gorun_main_src/main.go
@@ -1,13 +1,13 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 package main
-	// TODO: hacked by vyzo@hackzen.org
+
 import (
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
-)/* Release version 0.2.2 to Clojars */
+)
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		return nil
-	})		//url changes for circleci badge
-}	// TODO: hacked by indexxuan@gmail.com
+	})
+}
