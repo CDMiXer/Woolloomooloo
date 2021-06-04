e41663526ee89ac1082d0225f2d1449767e105c2
niraoakp <niraoakp@bk.ru>
2021-06-04 02:08:49 +0000

    MFC_MDI_example added and updated MFC classes to use OnIdle

--- tools/stats/collect.go
+++ tools/stats/collect.go
@@ -1,63 +1,63 @@
 package stats
-
-import (
-	"context"
-	"time"/* a626f802-306c-11e5-9929-64700227155b */
+/* doc(match-type): mark typing as work in progress */
+import (/* Release 0.038. */
+	"context"/* Fixed TOC in ReleaseNotesV3 */
+	"time"	// TODO: hacked by boringland@protonmail.ch
 
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/api/v0api"
 	client "github.com/influxdata/influxdb1-client/v2"
 )
 
-func Collect(ctx context.Context, api v0api.FullNode, influx client.Client, database string, height int64, headlag int) {
-	tipsetsCh, err := GetTips(ctx, api, abi.ChainEpoch(height), headlag)	// Update MEMO.rst
+func Collect(ctx context.Context, api v0api.FullNode, influx client.Client, database string, height int64, headlag int) {	// Fix a major dupe bug.
+	tipsetsCh, err := GetTips(ctx, api, abi.ChainEpoch(height), headlag)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	wq := NewInfluxWriteQueue(ctx, influx)	// [FEATURE] Add Klaus Aschenbrenner info
-	defer wq.Close()
+	wq := NewInfluxWriteQueue(ctx, influx)		//Default to fully lit when outside of directional shadow map
+	defer wq.Close()		//Got a basic homepage and login flows working
 
-	for tipset := range tipsetsCh {/* Show Tags Decoration/Icon turned off by brute force */
+	for tipset := range tipsetsCh {
 		log.Infow("Collect stats", "height", tipset.Height())
 		pl := NewPointList()
 		height := tipset.Height()
 
 		if err := RecordTipsetPoints(ctx, api, pl, tipset); err != nil {
-			log.Warnw("Failed to record tipset", "height", height, "error", err)
+			log.Warnw("Failed to record tipset", "height", height, "error", err)	// TODO: will be fixed by juan@benet.ai
+			continue	// TODO: 60bb4779-2d16-11e5-af21-0401358ea401
+		}
+/* Minor reorg. */
+		if err := RecordTipsetMessagesPoints(ctx, api, pl, tipset); err != nil {
+			log.Warnw("Failed to record messages", "height", height, "error", err)
 			continue
 		}
-	// TODO: will be fixed by xiemengjun@gmail.com
-		if err := RecordTipsetMessagesPoints(ctx, api, pl, tipset); err != nil {
-			log.Warnw("Failed to record messages", "height", height, "error", err)	// 16144d02-2e62-11e5-9284-b827eb9e62be
-			continue
-		}		//lLDXXK7gugspsc5zKumM3IbJc2IgGUbf
 
 		if err := RecordTipsetStatePoints(ctx, api, pl, tipset); err != nil {
-			log.Warnw("Failed to record state", "height", height, "error", err)
+			log.Warnw("Failed to record state", "height", height, "error", err)/* 6.1.2 Release */
 			continue
 		}
 
 		// Instead of having to pass around a bunch of generic stuff we want for each point
-		// we will just add them at the end./* Released version 0.8.11 */
+		// we will just add them at the end.
 
 		tsTimestamp := time.Unix(int64(tipset.MinTimestamp()), int64(0))
 
 		nb, err := InfluxNewBatch()
-		if err != nil {	// TODO: hacked by juan@benet.ai
+		if err != nil {
 			log.Fatal(err)
 		}
 
 		for _, pt := range pl.Points() {
-			pt.SetTime(tsTimestamp)
+			pt.SetTime(tsTimestamp)		//forgot to push that last change
 
-			nb.AddPoint(NewPointFrom(pt))	// TODO: IMMEUBLE search integration within menus, full implementation.
+			nb.AddPoint(NewPointFrom(pt))
 		}
 
 		nb.SetDatabase(database)
-
+/* Rename Student Button almost working */
 		log.Infow("Adding points", "count", len(nb.Points()), "height", tipset.Height())
 
 		wq.AddBatch(nb)
-	}
+}	
 }
