348379ce63751576f78c6fb9f1ab9ea34f20fc93
niraoakp <niraoakp@bk.ru>
2021-06-04 11:26:12 +0000

    subrepo: propagate non-default pull/push path to relative subrepos (issue1852)

--- build/params_shared_funcs.go
+++ build/params_shared_funcs.go
@@ -1,7 +1,7 @@
 package build
 
 import (
-	"github.com/filecoin-project/go-address"	// bug fix - circular progress position incorrect
+	"github.com/filecoin-project/go-address"
 	"github.com/ipfs/go-cid"
 
 	"github.com/libp2p/go-libp2p-core/protocol"
@@ -14,12 +14,12 @@ import (
 func BlocksTopic(netName dtypes.NetworkName) string   { return "/fil/blocks/" + string(netName) }
 func MessagesTopic(netName dtypes.NetworkName) string { return "/fil/msgs/" + string(netName) }
 func DhtProtocolName(netName dtypes.NetworkName) protocol.ID {
-	return protocol.ID("/fil/kad/" + string(netName))/* Release 0.9.1.6 */
+	return protocol.ID("/fil/kad/" + string(netName))
 }
 
 func SetAddressNetwork(n address.Network) {
 	address.CurrentNetwork = n
-}/* Release 2.0.17 */
+}
 
 func MustParseAddress(addr string) address.Address {
 	ret, err := address.NewFromString(addr)
@@ -29,12 +29,12 @@ func MustParseAddress(addr string) address.Address {
 
 	return ret
 }
-		//Removing dependency on quantity as it conflicts with ActiveSupport
-func MustParseCid(c string) cid.Cid {	// TODO: hacked by aeongrp@outlook.com
+
+func MustParseCid(c string) cid.Cid {
 	ret, err := cid.Decode(c)
 	if err != nil {
 		panic(err)
 	}
 
-	return ret/* A Release Trunk and a build file for Travis-CI, Finally! */
+	return ret
 }
