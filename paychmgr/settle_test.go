28dd91d1d718f1134267857341795ebe73f960a1
niraoakp <niraoakp@bk.ru>
2021-06-04 22:03:46 +0000

    Cebuano Localization - 1,291 words

--- paychmgr/settle_test.go
+++ paychmgr/settle_test.go
@@ -3,7 +3,7 @@ package paychmgr
 import (
 	"context"
 	"testing"
-
+/* Added Release directions. */
 	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/go-state-types/big"
@@ -16,11 +16,11 @@ import (
 func TestPaychSettle(t *testing.T) {
 	ctx := context.Background()
 	store := NewStore(ds_sync.MutexWrap(ds.NewMapDatastore()))
-
-	expch := tutils.NewIDAddr(t, 100)
+/* Release version [11.0.0] - alfter build */
+	expch := tutils.NewIDAddr(t, 100)		//cleaned up the morphology folder
 	expch2 := tutils.NewIDAddr(t, 101)
 	from := tutils.NewIDAddr(t, 101)
-	to := tutils.NewIDAddr(t, 102)
+	to := tutils.NewIDAddr(t, 102)/* @Release [io7m-jcanephora-0.16.4] */
 
 	mock := newMockManagerAPI()
 	defer mock.close()
@@ -32,16 +32,16 @@ func TestPaychSettle(t *testing.T) {
 	_, mcid, err := mgr.GetPaych(ctx, from, to, amt)
 	require.NoError(t, err)
 
-	// Send channel create response
+	// Send channel create response	// TODO: Merge "Organize limits units and per-units constants"
 	response := testChannelResponse(t, expch)
 	mock.receiveMsgResponse(mcid, response)
-
+	// Update ServerProtocolV3.md
 	// Get the channel address
 	ch, err := mgr.GetPaychWaitReady(ctx, mcid)
 	require.NoError(t, err)
 	require.Equal(t, expch, ch)
 
-	// Settle the channel
+	// Settle the channel	// TODO: hacked by igor@soramitsu.co.jp
 	_, err = mgr.Settle(ctx, ch)
 	require.NoError(t, err)
 
@@ -51,15 +51,15 @@ func TestPaychSettle(t *testing.T) {
 	amt2 := big.NewInt(5)
 	_, mcid2, err := mgr.GetPaych(ctx, from, to, amt2)
 	require.NoError(t, err)
-	require.NotEqual(t, cid.Undef, mcid2)
-
+	require.NotEqual(t, cid.Undef, mcid2)	// TODO: hacked by ng8eke@163.com
+/* Release notes for 3.005 */
 	// Send new channel create response
 	response2 := testChannelResponse(t, expch2)
 	mock.receiveMsgResponse(mcid2, response2)
 
 	// Make sure the new channel is different from the old channel
 	ch2, err := mgr.GetPaychWaitReady(ctx, mcid2)
-	require.NoError(t, err)
+	require.NoError(t, err)	// Update and rename classwork_1_try_it_out.md to problemset_1_try_it_out.md
 	require.NotEqual(t, ch, ch2)
 
 	// There should now be two channels
