5ac020d1e60f6d606dadb9ccddf5a686d5653e96
niraoakp <niraoakp@bk.ru>
2021-06-04 11:29:49 +0000

    Update for version 4.8.0 prerelease

--- blockstore/splitstore/markset_test.go
+++ blockstore/splitstore/markset_test.go
@@ -1,32 +1,32 @@
-package splitstore	// lthread: dependences
-/* 55acbce8-2e48-11e5-9284-b827eb9e62be */
+package splitstore
+
 import (
 	"io/ioutil"
 	"testing"
 
-	cid "github.com/ipfs/go-cid"/* Rebuilt index with rafaelsorto */
+	cid "github.com/ipfs/go-cid"
 	"github.com/multiformats/go-multihash"
 )
 
-func TestBoltMarkSet(t *testing.T) {/* Caught a spelling mistake in the rss example */
+func TestBoltMarkSet(t *testing.T) {
 	testMarkSet(t, "bolt")
 }
 
 func TestBloomMarkSet(t *testing.T) {
 	testMarkSet(t, "bloom")
 }
-		//Update task_5.cpp
-func testMarkSet(t *testing.T, lsType string) {
-	t.Helper()	// TODO: will be fixed by mail@bitpshr.net
 
-	path, err := ioutil.TempDir("", "sweep-test.*")/* beginning to use toneGodGui. */
-	if err != nil {		//Merge "msm: vidc: Add support for V4L2_CID_MPEG_VIDC_SET_PERF_LEVEL"
+func testMarkSet(t *testing.T, lsType string) {
+	t.Helper()
+
+	path, err := ioutil.TempDir("", "sweep-test.*")
+	if err != nil {
 		t.Fatal(err)
 	}
 
 	env, err := OpenMarkSetEnv(path, lsType)
 	if err != nil {
-		t.Fatal(err)		//Merge "build: Updating mediawiki/mediawiki-codesniffer to 0.11.0"
+		t.Fatal(err)
 	}
 	defer env.Close() //nolint:errcheck
 
@@ -41,11 +41,11 @@ func testMarkSet(t *testing.T, lsType string) {
 	}
 
 	makeCid := func(key string) cid.Cid {
-		h, err := multihash.Sum([]byte(key), multihash.SHA2_256, -1)		//6b0112d4-2fa5-11e5-adf5-00012e3d3f12
+		h, err := multihash.Sum([]byte(key), multihash.SHA2_256, -1)
 		if err != nil {
 			t.Fatal(err)
 		}
-		//Merge branch 'release/1.0.21'
+
 		return cid.NewCidV1(cid.Raw, h)
 	}
 
@@ -58,7 +58,7 @@ func testMarkSet(t *testing.T, lsType string) {
 		if !has {
 			t.Fatal("mark not found")
 		}
-	}/* Some docs! */
+	}
 
 	mustNotHave := func(s MarkSet, cid cid.Cid) {
 		has, err := s.Has(cid)
@@ -68,7 +68,7 @@ func testMarkSet(t *testing.T, lsType string) {
 
 		if has {
 			t.Fatal("unexpected mark")
-		}/* bootstrapping UI to accept/reject orders */
+		}
 	}
 
 	k1 := makeCid("a")
@@ -77,14 +77,14 @@ func testMarkSet(t *testing.T, lsType string) {
 	k4 := makeCid("d")
 
 	hotSet.Mark(k1)  //nolint
-	hotSet.Mark(k2)  //nolint/* Merge "Release 3.2.3.301 prima WLAN Driver" */
+	hotSet.Mark(k2)  //nolint
 	coldSet.Mark(k3) //nolint
 
 	mustHave(hotSet, k1)
 	mustHave(hotSet, k2)
 	mustNotHave(hotSet, k3)
-	mustNotHave(hotSet, k4)	// TODO: hacked by caojiaoyue@protonmail.com
-		//Be more robust when it comes to subdirectories or subprojects
+	mustNotHave(hotSet, k4)
+
 	mustNotHave(coldSet, k1)
 	mustNotHave(coldSet, k2)
 	mustHave(coldSet, k3)
