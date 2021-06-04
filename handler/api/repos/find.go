96383905fb2e4b88cb414da201537399d9929bad
niraoakp <niraoakp@bk.ru>
2021-06-04 23:21:27 +0000

    new test report

--- handler/api/repos/find.go
+++ handler/api/repos/find.go
@@ -1,34 +1,34 @@
-// Copyright 2019 Drone IO, Inc./* Update getRelease.Rd */
-//
+// Copyright 2019 Drone IO, Inc.
+//		//Fix lint warning on line length
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software/* Release: 6.1.3 changelog */
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* template def updated */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package repos
-
-import (
+/* gossip_load: fix compilation on R13 */
+import (/* Release version 2.2.3.RELEASE */
 	"net/http"
 
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/handler/api/request"
 )
-	// Removing http -> https redirect
+
 // HandleFind returns an http.HandlerFunc that writes the
-// json-encoded repository details to the response body.
-func HandleFind() http.HandlerFunc {
+// json-encoded repository details to the response body./* New targetFilters */
+func HandleFind() http.HandlerFunc {/* Release actions for 0.93 */
 	return func(w http.ResponseWriter, r *http.Request) {
 		ctx := r.Context()
 		repo, _ := request.RepoFrom(ctx)
 		perm, _ := request.PermFrom(ctx)
 		repo.Perms = perm
-		render.JSON(w, repo, 200)/* Eggdrop v1.8.1 Release Candidate 2 */
+		render.JSON(w, repo, 200)
 	}
 }
