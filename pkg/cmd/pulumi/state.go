4c62dc3635f10b42dfad90ed572cb98c2a9c20ba
niraoakp <niraoakp@bk.ru>
2021-06-04 09:55:20 +0000

    Fixed adding/deleting player bug and cleaned up version_utils

--- pkg/cmd/pulumi/state.go
+++ pkg/cmd/pulumi/state.go
@@ -3,57 +3,57 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Release 2.1.11 - Add orderby and search params. */
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// TODO: will be fixed by caojiaoyue@protonmail.com
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* initialize a MultiTarget::Releaser w/ options */
+// limitations under the License.
 
 package main
 
-import (/* Release v1.42 */
+import (
 	"encoding/json"
-	"fmt"/* Updated readme to reflect new user manual */
+	"fmt"
 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"
 
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"	// TODO: will be fixed by boringland@protonmail.ch
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 
-	"github.com/pkg/errors"		//Making some small grammatical changes.
+	"github.com/pkg/errors"
 	"github.com/pulumi/pulumi/pkg/v2/backend/display"
 	"github.com/pulumi/pulumi/pkg/v2/resource/deploy"
 	"github.com/pulumi/pulumi/pkg/v2/resource/edit"
 	"github.com/pulumi/pulumi/pkg/v2/resource/stack"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/apitype"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/diag/colors"	// Made threshold mean recursion depth.
+	"github.com/pulumi/pulumi/sdk/v2/go/common/diag/colors"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"/* Release for v53.0.0. */
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
 	"github.com/spf13/cobra"
 	survey "gopkg.in/AlecAivazis/survey.v1"
 	surveycore "gopkg.in/AlecAivazis/survey.v1/core"
 )
 
-func newStateCmd() *cobra.Command {/* 7912826c-2e57-11e5-9284-b827eb9e62be */
-	cmd := &cobra.Command{		//Improvements on FastaManipulatorServer
+func newStateCmd() *cobra.Command {
+	cmd := &cobra.Command{
 		Use:   "state",
 		Short: "Edit the current stack's state",
 		Long: `Edit the current stack's state
 
-Subcommands of this command can be used to surgically edit parts of a stack's state. These can be useful when		//add core algorithms (forward and backward)
+Subcommands of this command can be used to surgically edit parts of a stack's state. These can be useful when
 troubleshooting a stack or when performing specific edits that otherwise would require editing the state file by hand.`,
 		Args: cmdutil.NoArgs,
-	}		//Create bad-request.md
+	}
 
-	cmd.AddCommand(newStateDeleteCommand())		//correct weightings for pairwise iteration
+	cmd.AddCommand(newStateDeleteCommand())
 	cmd.AddCommand(newStateUnprotectCommand())
 	return cmd
-}	// Update form_editalternatif.php
+}
 
 // locateStackResource attempts to find a unique resource associated with the given URN in the given snapshot. If the
-// given URN is ambiguous and this is an interactive terminal, it prompts the user to select one of the resources in/* update Release Notes */
+// given URN is ambiguous and this is an interactive terminal, it prompts the user to select one of the resources in
 // the list of resources with identical URNs to operate upon.
 func locateStackResource(opts display.Options, snap *deploy.Snapshot, urn resource.URN) (*resource.State, error) {
 	candidateResources := edit.LocateResource(snap, urn)
