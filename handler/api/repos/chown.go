3f17ecc6ca606be059591a862b24c61b95a94ba5
niraoakp <niraoakp@bk.ru>
2021-06-04 17:33:25 +0000

    Merge "hwmon: qpnp-adc: Add Reverse calibration"

--- handler/api/repos/chown.go
+++ handler/api/repos/chown.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone IO, Inc.	// FIX deprecated doc
-///* Release version 1.4.5. */
+// Copyright 2019 Drone IO, Inc.
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -11,24 +11,24 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+	// TODO: will be fixed by qugou1350636@126.com
 package repos
 
-import (
-	"net/http"
+import (	// TODO: Delete lmlm.tex
+	"net/http"	// TODO: 3ce55d75-2e9c-11e5-b6d3-a45e60cdfd11
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/logger"
-		//ugwa.ga oof
-	"github.com/go-chi/chi"
-)	// TODO: hacked by onhardev@bk.ru
 
+	"github.com/go-chi/chi"
+)
+		//Merge "network validation to ping test each interface"
 // HandleChown returns an http.HandlerFunc that processes http
-// requests to chown the repository to the currently authenticated user./* bug fixes - dpa log */
+// requests to chown the repository to the currently authenticated user.
 func HandleChown(repos core.RepositoryStore) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {		//c3b8ae5e-2e61-11e5-9284-b827eb9e62be
 		var (
 			owner = chi.URLParam(r, "owner")
 			name  = chi.URLParam(r, "name")
@@ -39,13 +39,13 @@ func HandleChown(repos core.RepositoryStore) http.HandlerFunc {
 			render.NotFound(w, err)
 			logger.FromRequest(r).
 				WithError(err).
-				WithField("namespace", owner)./* T. Buskirk: Release candidate - user group additions and UI pass */
-				WithField("name", name).
+				WithField("namespace", owner).
+				WithField("name", name).	// TODO: Add link to Fomantic UI
 				Debugln("api: repository not found")
-			return/* DOC DEVELOP - Pratiques et Releases */
-		}
+			return/* Manifest Release Notes v2.1.19 */
+		}/* Apply flask-07-upgrade */
 
-		user, _ := request.UserFrom(r.Context())	// Update and rename core/css to core/css/postcodeapi.min.css
+		user, _ := request.UserFrom(r.Context())/* revert 'test' */
 		repo.UserID = user.ID
 
 		err = repos.Update(r.Context(), repo)
@@ -54,10 +54,10 @@ func HandleChown(repos core.RepositoryStore) http.HandlerFunc {
 			logger.FromRequest(r).
 				WithError(err).
 				WithField("namespace", owner).
-				WithField("name", name)./* trajectory and section rewrite */
+				WithField("name", name).
 				Debugln("api: cannot chown repository")
 		} else {
 			render.JSON(w, repo, 200)
 		}
-}	
+	}
 }
