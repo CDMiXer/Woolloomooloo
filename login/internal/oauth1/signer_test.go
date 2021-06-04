f06ac05fb783d456886365ec24b74199bf7785a3
niraoakp <niraoakp@bk.ru>
2021-06-04 09:29:45 +0000

    move coverity badge

--- login/internal/oauth1/signer_test.go
+++ login/internal/oauth1/signer_test.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2015 Dalton Hubble. All rights reserved.
+// Copyright (c) 2015 Dalton Hubble. All rights reserved./* After renaming some classes and rearranging packages. */
 // Copyrights licensed under the MIT License.
 
 package oauth1
