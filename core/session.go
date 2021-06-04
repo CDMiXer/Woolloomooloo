6338072cdfe8b74b6f9a644cf4e2bb9129b6630f
niraoakp <niraoakp@bk.ru>
2021-06-04 13:03:46 +0000

    remove the gem #93

--- core/session.go
+++ core/session.go
@@ -6,20 +6,20 @@
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software		//Fix x86/x64 on Linux, Credit to Rafael Espindola.
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package core
-	// TODO: Reset OCCI_CORE_SCHEME to its previous value.
-import "net/http"		//bundle-size: 751811981815007decfeda54d7621075abf0a3a3.json
+
+import "net/http"
 
 // Session provides session management for
 // authenticated users.
-type Session interface {/* Update the content from the file HowToRelease.md. */
-	// Create creates a new user session and writes the		//fix undefined struct
+type Session interface {
+	// Create creates a new user session and writes the
 	// session to the http.Response.
 	Create(http.ResponseWriter, *User) error
 
@@ -27,7 +27,7 @@ type Session interface {/* Update the content from the file HowToRelease.md. */
 	Delete(http.ResponseWriter) error
 
 	// Get returns the session from the http.Request. If no
-	// session exists a nil user is returned. Returning an/* Release of eeacms/www:21.5.7 */
+	// session exists a nil user is returned. Returning an
 	// error is optional, for debugging purposes only.
 	Get(*http.Request) (*User, error)
-}	// TODO: Merge "Adds keystone security compliance settings"
+}
