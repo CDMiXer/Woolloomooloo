4fc70a43752aea27e6c7b127bfce6bce3b821f28
niraoakp <niraoakp@bk.ru>
2021-06-04 01:26:27 +0000

    Added class for demo of perfering hierachy class to taged class.

--- handler/api/system/limits.go
+++ handler/api/system/limits.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* More preparatory work. Still nothing on the solve() method */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.	// TODO: will be fixed by vyzo@hackzen.org
-/* bitmex omit _format from http body */
+// that can be found in the LICENSE file.	// Popup: Use an explicit onClose callback
+
 // +build !oss
 
 package system
