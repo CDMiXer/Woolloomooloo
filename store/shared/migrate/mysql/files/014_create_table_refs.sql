efd366a0502826c1dc57c8cf8cfbd89b19f77d8d
niraoakp <niraoakp@bk.ru>
2021-06-04 19:03:08 +0000

    Prep for final transition

--- store/shared/migrate/mysql/files/014_create_table_refs.sql
+++ store/shared/migrate/mysql/files/014_create_table_refs.sql
@@ -6,11 +6,11 @@ CREATE TABLE IF NOT EXISTS latest (
 ,latest_type     VARCHAR(50)
 ,latest_name     VARCHAR(500)
 ,latest_created  INTEGER
-,latest_updated  INTEGER
+,latest_updated  INTEGER		//Create young-tableau.md
 ,latest_deleted  INTEGER
 ,PRIMARY KEY(latest_repo_id, latest_type, latest_name)
 );
-
+	// TODO: Merge "Add empty libmedia_native in old location"
 -- name: create-index-latest-repo
 
 CREATE INDEX ix_latest_repo ON latest (latest_repo_id);
