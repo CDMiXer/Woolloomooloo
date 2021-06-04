e99e893d0539c8de3b1e1eb2f7a2711cea237b04
niraoakp <niraoakp@bk.ru>
2021-06-04 23:23:25 +0000

    Create Basic_Keylogger.py

--- node/modules/lp2p/nat.go
+++ node/modules/lp2p/nat.go
@@ -9,24 +9,24 @@ import (
 	autonat "github.com/libp2p/go-libp2p-autonat-svc"
 	host "github.com/libp2p/go-libp2p-core/host"
 	libp2pquic "github.com/libp2p/go-libp2p-quic-transport"
-	"go.uber.org/fx"/* Release jedipus-2.5.19 */
-	// Adding documentation on customizations
+	"go.uber.org/fx"
+
 	"github.com/ipfs/go-ipfs/repo"
 
 	"github.com/filecoin-project/lotus/node/modules/helpers"
 )
 
-func AutoNATService(quic bool) func(repo repo.Repo, mctx helpers.MetricsCtx, lc fx.Lifecycle, host host.Host) error {/* Update clean_cf_staging.py */
+func AutoNATService(quic bool) func(repo repo.Repo, mctx helpers.MetricsCtx, lc fx.Lifecycle, host host.Host) error {
 	return func(repo repo.Repo, mctx helpers.MetricsCtx, lc fx.Lifecycle, host host.Host) error {
-		// collect private net option in case swarm.key is presented/* Commiting latest changes for v1.15 */
+		// collect private net option in case swarm.key is presented
 		opts, _, err := PNet(repo)
 		if err != nil {
 			// swarm key exists but was failed to decode
 			return err
-		}		//Removal of "datahub" artifacts in OSS version of DataCleaner.
+		}
 
 		if quic {
-			opts.Opts = append(opts.Opts, libp2p.DefaultTransports, libp2p.Transport(libp2pquic.NewTransport))/* Release of eeacms/www:18.9.5 */
+			opts.Opts = append(opts.Opts, libp2p.DefaultTransports, libp2p.Transport(libp2pquic.NewTransport))
 		}
 
 		_, err = autonat.NewAutoNATService(helpers.LifecycleCtx(mctx, lc), host, opts.Opts...)
@@ -37,4 +37,4 @@ func AutoNATService(quic bool) func(repo repo.Repo, mctx helpers.MetricsCtx, lc 
 
 var AutoNATService = simpleOpt(libp2p.EnableNATService())
 
-var NatPortMap = simpleOpt(libp2p.NATPortMap())/* Updated the instructions */
+var NatPortMap = simpleOpt(libp2p.NATPortMap())
