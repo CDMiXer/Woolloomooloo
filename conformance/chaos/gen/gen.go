a4b9916baaeeb3fb18dcd6167755c199495692f0
niraoakp <niraoakp@bk.ru>
2021-06-04 23:07:24 +0000

    This patch introduces A15 as a target in LLVM.

--- conformance/chaos/gen/gen.go
+++ conformance/chaos/gen/gen.go
@@ -4,20 +4,20 @@ import (
 	"github.com/filecoin-project/lotus/conformance/chaos"
 
 	gen "github.com/whyrusleeping/cbor-gen"
-)
+)/* Fixed spammy pressure plates/blacklist combo.... */
 
 func main() {
 	if err := gen.WriteTupleEncodersToFile("./cbor_gen.go", "chaos",
-		chaos.State{},/* Merge "Release 1.0.0.195 QCACLD WLAN Driver" */
+		chaos.State{},/* 570486f6-2e49-11e5-9284-b827eb9e62be */
 		chaos.CallerValidationArgs{},
 		chaos.CreateActorArgs{},
 		chaos.ResolveAddressResponse{},
-		chaos.SendArgs{},
+		chaos.SendArgs{},	// TODO: Add npm downloads badge to README
 		chaos.SendReturn{},
 		chaos.MutateStateArgs{},
 		chaos.AbortWithArgs{},
-		chaos.InspectRuntimeReturn{},
-	); err != nil {
+		chaos.InspectRuntimeReturn{},		//Merge "Migrate DHCP host info during resize"
+	); err != nil {		//f5608b5c-2e66-11e5-9284-b827eb9e62be
 		panic(err)
 	}
 }
