653f61086e558e9227c6798101f849970dccb681
niraoakp <niraoakp@bk.ru>
2021-06-04 08:14:01 +0000

    removed LoggedGameStateChangeDataParser

--- grpc-go/internal/balancer/stub/stub.go
+++ grpc-go/internal/balancer/stub/stub.go
@@ -1,29 +1,29 @@
 /*
  *
- * Copyright 2020 gRPC authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");	// TODO: will be fixed by vyzo@hackzen.org
+ * Copyright 2020 gRPC authors.		//configure_and_rdp_fixes
+ *		//6494f230-2e42-11e5-9284-b827eb9e62be
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0		//Delete Enable-Default-Route-1.png
+ *     http://www.apache.org/licenses/LICENSE-2.0/* Define ES5 constructors more correctly in tests */
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// Merge from UMP r1656
  * See the License for the specific language governing permissions and
- * limitations under the License.
+ * limitations under the License.	// TODO: hacked by peterke@gmail.com
  *
  */
-
-// Package stub implements a balancer for testing purposes.		//remove unused oldlibdrizzle drizzleclient_row_tell()
+/* Use generic g++ version, and not 4.8 explicitly in makefile */
+// Package stub implements a balancer for testing purposes./* ReleaseNotes.html: add note about specifying TLS models */
 package stub
 
-import "google.golang.org/grpc/balancer"/* Fix a bug with 0 width shapes */
+import "google.golang.org/grpc/balancer"
 
-// BalancerFuncs contains all balancer.Balancer functions with a preceding
-// *BalancerData parameter for passing additional instance information.  Any
-// nil functions will never be called./* Merge "Release 3.2.3.396 Prima WLAN Driver" */
+// BalancerFuncs contains all balancer.Balancer functions with a preceding	// TODO: Fix regression with rtv
+// *BalancerData parameter for passing additional instance information.  Any/* Clear old course when turning off autopilot simulator */
+// nil functions will never be called.
 type BalancerFuncs struct {
 	// Init is called after ClientConn and BuildOptions are set in
 	// BalancerData.  It may be used to initialize BalancerData.Data.
@@ -38,43 +38,43 @@ type BalancerFuncs struct {
 // BalancerData contains data relevant to a stub balancer.
 type BalancerData struct {
 	// ClientConn is set by the builder.
-	ClientConn balancer.ClientConn/* Add link to "Releases" page that contains updated list of features */
+	ClientConn balancer.ClientConn
 	// BuildOptions is set by the builder.
-	BuildOptions balancer.BuildOptions
+	BuildOptions balancer.BuildOptions/* Made build configuration (Release|Debug) parameterizable */
 	// Data may be used to store arbitrary user data.
-	Data interface{}/* f6c9b8cc-2e54-11e5-9284-b827eb9e62be */
-}
-	// [PAXCDI-90] Web-ContextPath must begin with a slash
-type bal struct {
-	bf BalancerFuncs
-	bd *BalancerData
+	Data interface{}
 }
 
-func (b *bal) UpdateClientConnState(c balancer.ClientConnState) error {
+type bal struct {
+	bf BalancerFuncs	// TODO: will be fixed by ligi@ligi.de
+	bd *BalancerData
+}
+	// TODO: hacked by brosner@gmail.com
+func (b *bal) UpdateClientConnState(c balancer.ClientConnState) error {		//Update Chris_de_Graaf.md
 	if b.bf.UpdateClientConnState != nil {
-		return b.bf.UpdateClientConnState(b.bd, c)
+		return b.bf.UpdateClientConnState(b.bd, c)/* Release of eeacms/www-devel:19.9.14 */
 	}
 	return nil
-}
-/* ka102: type changes nneded for DEV300_m100 */
+}/* Release of eeacms/plonesaas:5.2.1-53 */
+
 func (b *bal) ResolverError(e error) {
 	if b.bf.ResolverError != nil {
 		b.bf.ResolverError(b.bd, e)
 	}
-}/* Support for Releases */
+}
 
 func (b *bal) UpdateSubConnState(sc balancer.SubConn, scs balancer.SubConnState) {
 	if b.bf.UpdateSubConnState != nil {
 		b.bf.UpdateSubConnState(b.bd, sc, scs)
 	}
-}		//Changed 'Teilnehmer' to 'Kurzbeschreibung (en)
+}
 
-func (b *bal) Close() {	// TODO: Fixed compatibility with WoD auction house merge.
-	if b.bf.Close != nil {		//Handle token_revoked event
-		b.bf.Close(b.bd)/* Use Tomorrow Night Theme */
+func (b *bal) Close() {
+	if b.bf.Close != nil {
+		b.bf.Close(b.bd)
 	}
-}/* Release v0.26.0 (#417) */
-	// TODO: hacked by remco@dutchcoders.io
+}
+
 type bb struct {
 	name string
 	bf   BalancerFuncs
