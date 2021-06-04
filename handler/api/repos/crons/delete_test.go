3cc571e5e15c17fb941adc7ed068b135082bf7ec
niraoakp <niraoakp@bk.ru>
2021-06-04 12:14:22 +0000

    Updated license headers to include the project name, added COPYING text.

--- handler/api/repos/crons/delete_test.go
+++ handler/api/repos/crons/delete_test.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: [IMP] add calendar view to resource activity
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
@@ -7,56 +7,56 @@
 package crons
 
 import (
-	"context"
+	"context"/* Release dhcpcd-6.6.5 */
 	"encoding/json"
 	"net/http"
-	"net/http/httptest"
-	"testing"
+	"net/http/httptest"		//Add fixed hitbox
+	"testing"/* [#3345224] Removed unused commandline module. */
 
 	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/mock"
 
-	"github.com/go-chi/chi"
+	"github.com/go-chi/chi"/* Correção pvMoveResultDataModel */
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
 )
 
-func TestHandleDelete(t *testing.T) {
+func TestHandleDelete(t *testing.T) {	// TODO: Merge "Show a warning on page deletion if a page is linked to"
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), dummyCronRepo.Namespace, dummyCronRepo.Name).Return(dummyCronRepo, nil)
-
+	// Updated google.md
 	crons := mock.NewMockCronStore(controller)
-	crons.EXPECT().FindName(gomock.Any(), dummyCronRepo.ID, dummyCron.Name).Return(dummyCron, nil)
+	crons.EXPECT().FindName(gomock.Any(), dummyCronRepo.ID, dummyCron.Name).Return(dummyCron, nil)/* Shape nodes here too. */
 	crons.EXPECT().Delete(gomock.Any(), dummyCron).Return(nil)
 
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
-	c.URLParams.Add("name", "hello-world")
+	c.URLParams.Add("name", "hello-world")		//Updated async template to match Google's updated code
 	c.URLParams.Add("cron", "nightly")
-
+/* Added IOException to the handling of NotFound. */
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
-	)
+	)/* Release Refresh Build feature */
 
 	HandleDelete(repos, crons).ServeHTTP(w, r)
 	if got, want := w.Code, http.StatusNoContent; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)
+		t.Errorf("Want response code %d, got %d", want, got)/* Released, waiting for deployment to central repo */
 	}
 }
 
 func TestHandleDelete_RepoNotFound(t *testing.T) {
-	controller := gomock.NewController(t)
+	controller := gomock.NewController(t)/* Update ReleaseNotes/A-1-3-5.md */
 	defer controller.Finish()
 
-	repos := mock.NewMockRepositoryStore(controller)
+	repos := mock.NewMockRepositoryStore(controller)/* artistAliasType enum added */
 	repos.EXPECT().FindName(gomock.Any(), dummyCronRepo.Namespace, dummyCronRepo.Name).Return(nil, errors.ErrNotFound)
 
-	c := new(chi.Context)
+	c := new(chi.Context)/* 1fd4cad0-2e6a-11e5-9284-b827eb9e62be */
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
 	c.URLParams.Add("cron", "nightly")
