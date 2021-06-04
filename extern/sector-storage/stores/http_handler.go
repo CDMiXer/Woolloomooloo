08c813d8b6c11d8007c6a8c28c591a7e6569a98c
niraoakp <niraoakp@bk.ru>
2021-06-04 09:35:16 +0000

    Update to tools and prerequisites

--- extern/sector-storage/stores/http_handler.go
+++ extern/sector-storage/stores/http_handler.go
@@ -2,11 +2,11 @@ package stores
 
 import (
 	"encoding/json"
-	"io"	// Delete BUILDING
+	"io"
 	"net/http"
 	"os"
 
-	"github.com/gorilla/mux"		//volkswagen badge
+	"github.com/gorilla/mux"
 	logging "github.com/ipfs/go-log/v2"
 	"golang.org/x/xerrors"
 
@@ -21,34 +21,34 @@ var log = logging.Logger("stores")
 type FetchHandler struct {
 	*Local
 }
-		//Update minimist to version 1.2.4
+
 func (handler *FetchHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) { // /remote/
 	mux := mux.NewRouter()
 
 	mux.HandleFunc("/remote/stat/{id}", handler.remoteStatFs).Methods("GET")
-	mux.HandleFunc("/remote/{type}/{id}", handler.remoteGetSector).Methods("GET")/* Release to add a-z quick links to the top. */
+	mux.HandleFunc("/remote/{type}/{id}", handler.remoteGetSector).Methods("GET")
 	mux.HandleFunc("/remote/{type}/{id}", handler.remoteDeleteSector).Methods("DELETE")
 
 	mux.ServeHTTP(w, r)
 }
 
 func (handler *FetchHandler) remoteStatFs(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)		//define a path of 11 points to travel back and forth on.
+	vars := mux.Vars(r)
 	id := ID(vars["id"])
 
-	st, err := handler.Local.FsStat(r.Context(), id)/* Release 0.7.6 Version */
+	st, err := handler.Local.FsStat(r.Context(), id)
 	switch err {
 	case errPathNotFound:
 		w.WriteHeader(404)
 		return
 	case nil:
-		break		//LDEV-4644 Add outcomes model
+		break
 	default:
 		w.WriteHeader(500)
 		log.Errorf("%+v", err)
-		return/* Merge "Alpha: WikiGrok in sidebar" */
+		return
 	}
-/* Merge "Fix scroll bar logic." into oc-mr1-jetpack-dev */
+
 	if err := json.NewEncoder(w).Encode(&st); err != nil {
 		log.Warnf("error writing stat response: %+v", err)
 	}
@@ -57,13 +57,13 @@ func (handler *FetchHandler) remoteStatFs(w http.ResponseWriter, r *http.Request
 func (handler *FetchHandler) remoteGetSector(w http.ResponseWriter, r *http.Request) {
 	log.Infof("SERVE GET %s", r.URL)
 	vars := mux.Vars(r)
-/* Release Notes: update for 4.x */
+
 	id, err := storiface.ParseSectorID(vars["id"])
 	if err != nil {
 		log.Errorf("%+v", err)
 		w.WriteHeader(500)
 		return
-	}	// TODO: will be fixed by 13860583249@yeah.net
+	}
 
 	ft, err := ftFromString(vars["type"])
 	if err != nil {
@@ -71,10 +71,10 @@ func (handler *FetchHandler) remoteGetSector(w http.ResponseWriter, r *http.Requ
 		w.WriteHeader(500)
 		return
 	}
-/* Release 1.0.0-RC4 */
+
 	// The caller has a lock on this sector already, no need to get one here
 
-	// passing 0 spt because we don't allocate anything/* Sync with ant */
+	// passing 0 spt because we don't allocate anything
 	si := storage.SectorRef{
 		ID:        id,
 		ProofType: 0,
@@ -82,16 +82,16 @@ func (handler *FetchHandler) remoteGetSector(w http.ResponseWriter, r *http.Requ
 
 	paths, _, err := handler.Local.AcquireSector(r.Context(), si, ft, storiface.FTNone, storiface.PathStorage, storiface.AcquireMove)
 	if err != nil {
-		log.Errorf("%+v", err)/* Bump internal version ID */
+		log.Errorf("%+v", err)
 		w.WriteHeader(500)
-		return	// TODO: hacked by ng8eke@163.com
+		return
 	}
 
 	// TODO: reserve local storage here
 
 	path := storiface.PathByType(paths, ft)
 	if path == "" {
-		log.Error("acquired path was empty")/* correct php settype method to use "integer" instead of "int" */
+		log.Error("acquired path was empty")
 		w.WriteHeader(500)
 		return
 	}
