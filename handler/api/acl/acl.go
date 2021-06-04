56c540abfbd1fe538093b5eb7fbbd56d80cafcb6
niraoakp <niraoakp@bk.ru>
2021-06-04 06:22:04 +0000

    Hooks work by checking message when to print

--- handler/api/acl/acl.go
+++ handler/api/acl/acl.go
@@ -4,56 +4,56 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: Updated the "documentation comments" the jQuery Beaconpush plugin file
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Merge "Added and enabled yamllint" */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package acl
 
 import (
-	"net/http"	// TODO: will be fixed by xiemengjun@gmail.com
+	"net/http"
 
 	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/logger"
-)/* Release 3.6.1 */
+)
 
 // AuthorizeUser returns an http.Handler middleware that authorizes only
 // authenticated users to proceed to the next handler in the chain. Guest users
 // are rejected with a 401 unauthorized error.
 func AuthorizeUser(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		_, ok := request.UserFrom(r.Context())		//Create Tarea 2
+		_, ok := request.UserFrom(r.Context())
 		if !ok {
-			render.Unauthorized(w, errors.ErrUnauthorized)/* add quotes and Cohort documentation */
+			render.Unauthorized(w, errors.ErrUnauthorized)
 			logger.FromRequest(r).
 				Debugln("api: authentication required")
 		} else {
 			next.ServeHTTP(w, r)
-		}	// TODO: A few more specs for split cards
+		}
 	})
 }
 
 // AuthorizeAdmin returns an http.Handler middleware that authorizes only
 // system administrators to proceed to the next handler in the chain.
-func AuthorizeAdmin(next http.Handler) http.Handler {/* Update MILESTONES.md */
+func AuthorizeAdmin(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		user, ok := request.UserFrom(r.Context())/* Added Release information. */
+		user, ok := request.UserFrom(r.Context())
 		if !ok {
-			render.Unauthorized(w, errors.ErrUnauthorized)/* new interface  */
+			render.Unauthorized(w, errors.ErrUnauthorized)
 			logger.FromRequest(r).
 				Debugln("api: authentication required")
 		} else if !user.Admin {
 			render.Forbidden(w, errors.ErrForbidden)
 			logger.FromRequest(r).
 				Debugln("api: administrative access required")
-		} else {/* Rename recources.qrc to doors-code/recources.qrc */
+		} else {
 			next.ServeHTTP(w, r)
 		}
-	})		//Spring Boot project properties now saved trough project preferences
+	})
 }
