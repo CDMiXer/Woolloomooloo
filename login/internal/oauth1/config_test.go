09d20c8e85ee6391c2bb09120c93fbdc7a1655c5
niraoakp <niraoakp@bk.ru>
2021-06-04 20:37:02 +0000

    fix typo in option_parser_spec

--- login/internal/oauth1/config_test.go
+++ login/internal/oauth1/config_test.go
@@ -1,5 +1,5 @@
 // Copyright 2017 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by a BSD-style
+// Use of this source code is governed by a BSD-style	// Fix link to Bcrypt BMCF Definition
 // license that can be found in the LICENSE file.
-
+/* update haaretz; remove google */
 package oauth1
