997e9cd6049b173106e0d336e5febee94cfb7f37
niraoakp <niraoakp@bk.ru>
2021-06-04 14:10:38 +0000

    We should use '

--- handler/api/repos/builds/deploys/create.go
+++ handler/api/repos/builds/deploys/create.go
@@ -1,15 +1,15 @@
-// Copyright 2019 Drone IO, Inc.
-///* Create Release-Notes.md */
+// Copyright 2019 Drone IO, Inc.		//remind to config in post_install_message
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// Remove .idea files
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Release for v8.3.0. */
-// distributed under the License is distributed on an "AS IS" BASIS,/* Initial GitHub deployed */
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package deploys
+package deploys/* Release v1.9.1 */
