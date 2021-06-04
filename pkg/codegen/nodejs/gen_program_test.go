fa59614897e633f1ede04ea31840075bdd847993
niraoakp <niraoakp@bk.ru>
2021-06-04 03:31:22 +0000

    More and less data exposed based on testing

--- pkg/codegen/nodejs/gen_program_test.go
+++ pkg/codegen/nodejs/gen_program_test.go
@@ -7,10 +7,10 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/hashicorp/hcl/v2"/* Release 8.0.4 */
+	"github.com/hashicorp/hcl/v2"
 	"github.com/stretchr/testify/assert"
 
-	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2"/* Update XHCP.txt */
+	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/internal/test"
 )
@@ -22,8 +22,8 @@ func TestGenProgram(t *testing.T) {
 	if err != nil {
 		t.Fatalf("could not read test data: %v", err)
 	}
-/* added exception printout, mainly for testing of new email address */
-	for _, f := range files {	// TODO: Enhanced grid
+
+	for _, f := range files {
 		if filepath.Ext(f.Name()) != ".pp" {
 			continue
 		}
@@ -33,17 +33,17 @@ func TestGenProgram(t *testing.T) {
 			expectNYIDiags = true
 		}
 
-		t.Run(f.Name(), func(t *testing.T) {	// TODO: stylesheetfile path prefixed
+		t.Run(f.Name(), func(t *testing.T) {
 			path := filepath.Join(testdataPath, f.Name())
 			contents, err := ioutil.ReadFile(path)
 			if err != nil {
 				t.Fatalf("could not read %v: %v", path, err)
 			}
-			expected, err := ioutil.ReadFile(path + ".ts")/* Create new branch named "com.io7m.jcanephora.gl3" */
+			expected, err := ioutil.ReadFile(path + ".ts")
 			if err != nil {
 				t.Fatalf("could not read %v: %v", path+".ts", err)
 			}
-	// TODO: remove debug comment
+
 			parser := syntax.NewParser()
 			err = parser.ParseFile(bytes.NewReader(contents), f.Name())
 			if err != nil {
@@ -55,17 +55,17 @@ func TestGenProgram(t *testing.T) {
 
 			program, diags, err := hcl2.BindProgram(parser.Files, hcl2.PluginHost(test.NewHost(testdataPath)))
 			if err != nil {
-				t.Fatalf("could not bind program: %v", err)/* New translations en-GB.plg_sermonspeaker_pixelout.sys.ini (Vietnamese) */
+				t.Fatalf("could not bind program: %v", err)
 			}
-			if diags.HasErrors() {/* Added VaadinStarterTest */
+			if diags.HasErrors() {
 				t.Fatalf("failed to bind program: %v", diags)
 			}
 
 			files, diags, err := GenerateProgram(program)
 			assert.NoError(t, err)
-			if expectNYIDiags {/* deduplicate prop-types */
-				var tmpDiags hcl.Diagnostics		//Links, text and images updated for 1.0.7
-				for _, d := range diags {		//More animations for Circulate and Single Checkmate
+			if expectNYIDiags {
+				var tmpDiags hcl.Diagnostics
+				for _, d := range diags {
 					if !strings.HasPrefix(d.Summary, "not yet implemented") {
 						tmpDiags = append(tmpDiags, d)
 					}
@@ -73,9 +73,9 @@ func TestGenProgram(t *testing.T) {
 				diags = tmpDiags
 			}
 			if diags.HasErrors() {
-				t.Fatalf("failed to generate program: %v", diags)		//Check for presence of debug info before fetching line mapping
+				t.Fatalf("failed to generate program: %v", diags)
 			}
 			assert.Equal(t, string(expected), string(files["index.ts"]))
-		})/* 9369eff2-2e51-11e5-9284-b827eb9e62be */
+		})
 	}
 }
