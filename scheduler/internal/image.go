2e72683228e2949c1741f4a03ba5dc8d5d6e5593
niraoakp <niraoakp@bk.ru>
2021-06-04 06:54:04 +0000

    9eba657a-2e43-11e5-9284-b827eb9e62be

--- scheduler/internal/image.go
+++ scheduler/internal/image.go
@@ -1,18 +1,18 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License		//Added Tablename mapper
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-	// TODO: hacked by igor@soramitsu.co.jp
+
 // +build !oss
-	// TODO: Update boltforms_theme_translated.twig
+
 package internal
 
 var defaultImage = "drone/controller:1"
 
 // DefaultImage returns the default dispatch image if none
-// is specified./* send X-Ubuntu-Release to the store */
-func DefaultImage(image string) string {/* Move logic for trying multiple addresses into ``DBus.Connection''. */
+// is specified.
+func DefaultImage(image string) string {
 	if image == "" {
 		return defaultImage
 	}
 	return image
-}		//Create SF-50106_ja.md
+}
