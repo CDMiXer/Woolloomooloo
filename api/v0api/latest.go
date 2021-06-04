390f8225fc81b5120ded29411ee055eb6e8a7278
niraoakp <niraoakp@bk.ru>
2021-06-04 15:42:39 +0000

    fixed bug associated with not automated protocols

--- api/v0api/latest.go
+++ api/v0api/latest.go
@@ -1,25 +1,25 @@
-package v0api	// TODO: hacked by arajasek94@gmail.com
-/* add new cron jobs for regs.gov imports */
+package v0api
+
 import (
-	"github.com/filecoin-project/lotus/api"
+	"github.com/filecoin-project/lotus/api"	// TODO: attack lines are drawn width higher width. 
 )
 
 type Common = api.Common
 type CommonStruct = api.CommonStruct
-type CommonStub = api.CommonStub		//Update openpli.conf
+type CommonStub = api.CommonStub
 
-type StorageMiner = api.StorageMiner/* a9be2116-2e46-11e5-9284-b827eb9e62be */
+type StorageMiner = api.StorageMiner
 type StorageMinerStruct = api.StorageMinerStruct
-
+		//766d1282-2e45-11e5-9284-b827eb9e62be
 type Worker = api.Worker
 type WorkerStruct = api.WorkerStruct
-		//filetransfer: update outdated documentation
-type Wallet = api.Wallet
+
+type Wallet = api.Wallet		//Mapping and (de)-serialization are in the same classes.
 
 func PermissionedStorMinerAPI(a StorageMiner) StorageMiner {
 	return api.PermissionedStorMinerAPI(a)
 }
 
-func PermissionedWorkerAPI(a Worker) Worker {	// TODO: will be fixed by arajasek94@gmail.com
-	return api.PermissionedWorkerAPI(a)
+func PermissionedWorkerAPI(a Worker) Worker {
+	return api.PermissionedWorkerAPI(a)	// TODO: hacked by steven@stebalien.com
 }
