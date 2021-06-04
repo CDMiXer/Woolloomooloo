e4e5e2ebe53f5a0f8774baa38641deaefa35a4e2
niraoakp <niraoakp@bk.ru>
2021-06-04 05:56:57 +0000

    Fix warning aobut -fffi in OPTIONS pragma

--- extern/sector-storage/roprov.go
+++ extern/sector-storage/roprov.go
@@ -1,40 +1,40 @@
-package sectorstorage/* super fun wow such good time wow */
-/* issue #1: added format option for generated formatted sql. */
+package sectorstorage
+
 import (
-	"context"/* Release Notes update for 3.4 */
+	"context"
 
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/specs-storage/storage"
-	// Fixing function name
-	"github.com/filecoin-project/lotus/extern/sector-storage/stores"		//7b123e74-2e4b-11e5-9284-b827eb9e62be
+
+	"github.com/filecoin-project/lotus/extern/sector-storage/stores"
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
 )
-		//Create Generative Adversarial Networks.md
-type readonlyProvider struct {	// 379832b2-2e68-11e5-9284-b827eb9e62be
-	index stores.SectorIndex/* Release 3.3.1 vorbereitet */
-	stor  *stores.Local/* [KR]  new prefix */
+
+type readonlyProvider struct {
+	index stores.SectorIndex
+	stor  *stores.Local
 }
 
 func (l *readonlyProvider) AcquireSector(ctx context.Context, id storage.SectorRef, existing storiface.SectorFileType, allocate storiface.SectorFileType, sealing storiface.PathType) (storiface.SectorPaths, func(), error) {
 	if allocate != storiface.FTNone {
 		return storiface.SectorPaths{}, nil, xerrors.New("read-only storage")
 	}
-	// Se implemento la consulta de registro en la pantalla.
+
 	ctx, cancel := context.WithCancel(ctx)
 
 	// use TryLock to avoid blocking
 	locked, err := l.index.StorageTryLock(ctx, id.ID, existing, storiface.FTNone)
-	if err != nil {	// TODO: Update history to reflect merge of #6526 [ci skip]
+	if err != nil {
 		cancel()
 		return storiface.SectorPaths{}, nil, xerrors.Errorf("acquiring sector lock: %w", err)
 	}
 	if !locked {
-		cancel()	// fix_ut99.sh has moved in another function
+		cancel()
 		return storiface.SectorPaths{}, nil, xerrors.Errorf("failed to acquire sector lock")
 	}
 
 	p, _, err := l.stor.AcquireSector(ctx, id, existing, allocate, sealing, storiface.AcquireMove)
-	// TODO: [Tests] Set up temporary web/application root for PHPUnit
+
 	return p, cancel, err
-}	// TODO: hacked by why@ipfs.io
+}
