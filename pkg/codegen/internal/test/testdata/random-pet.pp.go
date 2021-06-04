a226ce1abcc6e2c1c4bb9c6b2aeb57368fcd99f2
niraoakp <niraoakp@bk.ru>
2021-06-04 06:23:54 +0000

    40e12d46-2e5a-11e5-9284-b827eb9e62be

--- pkg/codegen/internal/test/testdata/random-pet.pp.go
+++ pkg/codegen/internal/test/testdata/random-pet.pp.go
@@ -1,18 +1,18 @@
-package main/* Fantastico is not a CMS. */
-	// TODO: hacked by bokky.poobah@bokconsulting.com.au
+package main
+
 import (
-	"github.com/pulumi/pulumi-random/sdk/v2/go/random"		//fix title/description
+	"github.com/pulumi/pulumi-random/sdk/v2/go/random"
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
-	// Merge "virt: use compute.virttype constants and validate virt type"
+
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		_, err := random.NewRandomPet(ctx, "random_pet", &random.RandomPetArgs{
 			Prefix: pulumi.String("doggo"),
-		})/* zoom on the fly */
+		})
 		if err != nil {
 			return err
-		}	// TODO: Removed test jquery dependency.
+		}
 		return nil
 	})
 }
