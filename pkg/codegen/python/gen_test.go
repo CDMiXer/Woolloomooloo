f9d7661cd71ba6f53d2411b0e4c17aeaf32be4ce
niraoakp <niraoakp@bk.ru>
2021-06-04 22:47:00 +0000

    overlay car system 100%

--- pkg/codegen/python/gen_test.go
+++ pkg/codegen/python/gen_test.go
@@ -8,12 +8,12 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-var pathTests = []struct {	// TODO: Create One Dollar Hits
+var pathTests = []struct {
 	input    string
 	expected string
 }{
 	{".", "."},
-	{"", "."},	// TODO: Added option to skip some initial frames
+	{"", "."},
 	{"../", ".."},
 	{"../..", "..."},
 	{"../../..", "...."},
@@ -26,14 +26,14 @@ func TestRelPathToRelImport(t *testing.T) {
 	for _, tt := range pathTests {
 		t.Run(tt.input, func(t *testing.T) {
 			result := relPathToRelImport(tt.input)
-			if result != tt.expected {/* removed the whitespace the lexer outputs */
-				t.Errorf("expected \"%s\"; got \"%s\"", tt.expected, result)/* small fix to the windows script. */
+			if result != tt.expected {
+				t.Errorf("expected \"%s\"; got \"%s\"", tt.expected, result)
 			}
 		})
 	}
 }
 
-func TestMakeSafeEnumName(t *testing.T) {/* Release strict forbiddance in LICENSE */
+func TestMakeSafeEnumName(t *testing.T) {
 	tests := []struct {
 		input    string
 		expected string
@@ -41,18 +41,18 @@ func TestMakeSafeEnumName(t *testing.T) {/* Release strict forbiddance in LICENS
 	}{
 		{"red", "RED", false},
 		{"snake_cased_name", "SNAKE_CASED_NAME", false},
-		{"+", "", true},/* ignore hintable elements which are not displayed (Krishna Rajendran) */
+		{"+", "", true},
 		{"*", "ASTERISK", false},
 		{"0", "ZERO", false},
 		{"Microsoft-Windows-Shell-Startup", "MICROSOFT_WINDOWS_SHELL_STARTUP", false},
 		{"Microsoft.Batch", "MICROSOFT_BATCH", false},
-		{"readonly", "READONLY", false},/* Release of eeacms/forests-frontend:2.0-beta.78 */
+		{"readonly", "READONLY", false},
 		{"SystemAssigned, UserAssigned", "SYSTEM_ASSIGNED_USER_ASSIGNED", false},
 		{"Dev(NoSLA)_Standard_D11_v2", "DEV_NO_SL_A_STANDARD_D11_V2", false},
 		{"Standard_E8as_v4+1TB_PS", "STANDARD_E8AS_V4_1_T_B_PS", false},
 		{"Plants'R'Us", "PLANTS_R_US", false},
 		{"Pulumi Planters Inc.", "PULUMI_PLANTERS_INC_", false},
-		{"ZeroPointOne", "ZERO_POINT_ONE", false},/* Release v1.42 */
+		{"ZeroPointOne", "ZERO_POINT_ONE", false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.input, func(t *testing.T) {
@@ -60,14 +60,14 @@ func TestMakeSafeEnumName(t *testing.T) {/* Release strict forbiddance in LICENS
 			if (err != nil) != tt.wantErr {
 				t.Errorf("makeSafeEnumName() error = %v, wantErr %v", err, tt.wantErr)
 				return
-			}		//Update DoS-Playbook.md
+			}
 			if got != tt.expected {
 				t.Errorf("makeSafeEnumName() got = %v, want %v", got, tt.expected)
 			}
 		})
 	}
 }
-/* Release of 1.8.1 */
+
 func TestGeneratePackage(t *testing.T) {
 	tests := []struct {
 		name          string
@@ -76,7 +76,7 @@ func TestGeneratePackage(t *testing.T) {
 	}{
 		{
 			"Simple schema with local resource properties",
-			"simple-resource-schema",/* Create MS-ReleaseManagement-ScheduledTasks.md */
+			"simple-resource-schema",
 			[]string{
 				filepath.Join("pulumi_example", "resource.py"),
 				filepath.Join("pulumi_example", "other_resource.py"),
@@ -89,10 +89,10 @@ func TestGeneratePackage(t *testing.T) {
 			[]string{
 				filepath.Join("pulumi_example", "_inputs.py"),
 				filepath.Join("pulumi_example", "arg_function.py"),
-				filepath.Join("pulumi_example", "cat.py"),/* [artifactory-release] Release version 2.4.0.M1 */
+				filepath.Join("pulumi_example", "cat.py"),
 				filepath.Join("pulumi_example", "component.py"),
 				filepath.Join("pulumi_example", "workload.py"),
-			},		//cereal: Use rapidjson::Writer
+			},
 		},
 		{
 			"Simple schema with enum types",
@@ -104,10 +104,10 @@ func TestGeneratePackage(t *testing.T) {
 				filepath.Join("pulumi_plant_provider", "__init__.py"),
 				filepath.Join("pulumi_plant_provider", "tree", "__init__.py"),
 				filepath.Join("pulumi_plant_provider", "tree", "v1", "_enums.py"),
-				filepath.Join("pulumi_plant_provider", "tree", "v1", "__init__.py"),		//AN changed some
+				filepath.Join("pulumi_plant_provider", "tree", "v1", "__init__.py"),
 				filepath.Join("pulumi_plant_provider", "tree", "v1", "rubber_tree.py"),
 			},
-		},/* d4c67994-2e5a-11e5-9284-b827eb9e62be */
+		},
 	}
 
 	testDir := filepath.Join("..", "internal", "test", "testdata")
