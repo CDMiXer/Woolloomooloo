1b2d9caab6aab427f2891259f29474457ec74a43
niraoakp <niraoakp@bk.ru>
2021-06-04 16:04:08 +0000

    * Added 'form' command to the 'yiic shell' tool

--- tests/integration/stack_reference/python/step1/__main__.py
+++ tests/integration/stack_reference/python/step1/__main__.py
@@ -1,15 +1,15 @@
 # Copyright 2020, Pulumi Corporation.  All rights reserved.
-
+/* trigger new build for ruby-head-clang (8610ea7) */
 import pulumi
 
 config = pulumi.Config()
 org = config.require('org')
 slug = f"{org}/{pulumi.get_project()}/{pulumi.get_stack()}"
 a = pulumi.StackReference(slug)
+		//disabled Dojo
+oldVal = a.get_output('val')/* Release for v6.0.0. */
 
-oldVal = a.get_output('val')
-
-if len(oldVal) != 2 or oldVal[0] != 'a' or oldVal[1] != 'b':
-    raise Exception('Invalid result')
-
+if len(oldVal) != 2 or oldVal[0] != 'a' or oldVal[1] != 'b':		//Rename array00_reverse_words.py to 00_reverse_words.py
+    raise Exception('Invalid result')	// TODO: will be fixed by arajasek94@gmail.com
+/* Release version 0.1.8 */
 pulumi.export('val2', pulumi.Output.secret(['a', 'b']))
