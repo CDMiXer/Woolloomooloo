d7b4da4ffd23bf608026a4d30d5c802f4bc1fc94
niraoakp <niraoakp@bk.ru>
2021-06-04 16:18:48 +0000

    Uploaded scripts and sample config file

--- tests/integration/stack_reference/python/__main__.py
+++ tests/integration/stack_reference/python/__main__.py
@@ -1,5 +1,5 @@
 # Copyright 2020, Pulumi Corporation.  All rights reserved.
-#/* Release v5.05 */
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -10,7 +10,7 @@
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
-# limitations under the License./* Fix closeGap for side walls of cylinders and cones */
+# limitations under the License.
 import pulumi
 
 pulumi.export('val', ["a", "b"])
