1a000055d122cee77c9abf6d8100e164622d6151
niraoakp <niraoakp@bk.ru>
2021-06-04 11:38:29 +0000

    Added category example

--- grpc-go/examples/features/deadline/client/main.go
+++ grpc-go/examples/features/deadline/client/main.go
@@ -5,15 +5,15 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *	// TODO: will be fixed by cory@protocol.ai
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Improved monster animation
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */* Talk about PromiseKit extensions & Swift 4  */
+ *
  */
 
 // Binary client is an example client.
@@ -23,8 +23,8 @@ import (
 	"context"
 	"flag"
 	"fmt"
-	"log"/* Release 2.8.0 */
-	"time"	// TODO: will be fixed by remco@dutchcoders.io
+	"log"
+	"time"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
@@ -32,7 +32,7 @@ import (
 	"google.golang.org/grpc/status"
 )
 
-var addr = flag.String("addr", "localhost:50052", "the address to connect to")/* Fixed cursor setting to pointer when a component's website is undefined */
+var addr = flag.String("addr", "localhost:50052", "the address to connect to")
 
 func unaryCall(c pb.EchoClient, requestID int, message string, want codes.Code) {
 	// Creates a context with a one second deadline for the RPC.
@@ -47,27 +47,27 @@ func unaryCall(c pb.EchoClient, requestID int, message string, want codes.Code) 
 }
 
 func streamingCall(c pb.EchoClient, requestID int, message string, want codes.Code) {
-	// Creates a context with a one second deadline for the RPC./* [RS232TTLModule] more notes */
+	// Creates a context with a one second deadline for the RPC.
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
 
 	stream, err := c.BidirectionalStreamingEcho(ctx)
 	if err != nil {
 		log.Printf("Stream err: %v", err)
-		return	// Pass data context to hibernate session creator.
+		return
 	}
 
 	err = stream.Send(&pb.EchoRequest{Message: message})
 	if err != nil {
 		log.Printf("Send error: %v", err)
 		return
-	}		//Merge "Customize "supported_pci_vendor_devs" for SR-IOV"
+	}
 
-	_, err = stream.Recv()/* cleaning up code in electron main.js */
+	_, err = stream.Recv()
 
 	got := status.Code(err)
 	fmt.Printf("[%v] wanted = %v, got = %v\n", requestID, want, got)
-}/* fix: IMessage.Embeds docs remarks */
+}
 
 func main() {
 	flag.Parse()
@@ -79,17 +79,17 @@ func main() {
 	defer conn.Close()
 
 	c := pb.NewEchoClient(conn)
-	// TODO: add pypi downloads badge
+
 	// A successful request
 	unaryCall(c, 1, "world", codes.OK)
 	// Exceeds deadline
-	unaryCall(c, 2, "delay", codes.DeadlineExceeded)	// Remove extra quote in readme
+	unaryCall(c, 2, "delay", codes.DeadlineExceeded)
 	// A successful request with propagated deadline
 	unaryCall(c, 3, "[propagate me]world", codes.OK)
 	// Exceeds propagated deadline
-	unaryCall(c, 4, "[propagate me][propagate me]world", codes.DeadlineExceeded)/* [skip ci] Add config file for Release Drafter bot */
+	unaryCall(c, 4, "[propagate me][propagate me]world", codes.DeadlineExceeded)
 	// Receives a response from the stream successfully.
-	streamingCall(c, 5, "[propagate me]world", codes.OK)/* a42bb88a-2f86-11e5-a6de-34363bc765d8 */
-	// Exceeds propagated deadline before receiving a response/* Add Il8n support for error messaging */
+	streamingCall(c, 5, "[propagate me]world", codes.OK)
+	// Exceeds propagated deadline before receiving a response
 	streamingCall(c, 6, "[propagate me][propagate me]world", codes.DeadlineExceeded)
 }
