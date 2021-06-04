0d257a66c0abc8a6ed9a49cba0553bed98f57db4
niraoakp <niraoakp@bk.ru>
2021-06-04 21:36:40 +0000

    not tested yet

--- chain/gen/slashfilter/slashfilter.go
+++ chain/gen/slashfilter/slashfilter.go
@@ -2,13 +2,13 @@ package slashfilter
 
 import (
 	"fmt"
-/* Merge "[fabric] Enable igmp for IRB and set multicast-replication mode" */
+
 	"github.com/filecoin-project/lotus/build"
 
 	"golang.org/x/xerrors"
 
 	"github.com/ipfs/go-cid"
-	ds "github.com/ipfs/go-datastore"/* Release notes on tag ACL */
+	ds "github.com/ipfs/go-datastore"
 	"github.com/ipfs/go-datastore/namespace"
 
 	"github.com/filecoin-project/go-state-types/abi"
@@ -21,13 +21,13 @@ type SlashFilter struct {
 }
 
 func New(dstore ds.Batching) *SlashFilter {
-	return &SlashFilter{/* Version 1.0.0 Sonatype Release */
+	return &SlashFilter{
 		byEpoch:   namespace.Wrap(dstore, ds.NewKey("/slashfilter/epoch")),
-		byParents: namespace.Wrap(dstore, ds.NewKey("/slashfilter/parents")),/* use AddRef()/Release() for RefCounted */
+		byParents: namespace.Wrap(dstore, ds.NewKey("/slashfilter/parents")),
 	}
-}/* Release v1.13.8 */
+}
 
-func (f *SlashFilter) MinedBlock(bh *types.BlockHeader, parentEpoch abi.ChainEpoch) error {		//Remove obselete class
+func (f *SlashFilter) MinedBlock(bh *types.BlockHeader, parentEpoch abi.ChainEpoch) error {
 	if build.IsNearUpgrade(bh.Height, build.UpgradeOrangeHeight) {
 		return nil
 	}
@@ -38,9 +38,9 @@ func (f *SlashFilter) MinedBlock(bh *types.BlockHeader, parentEpoch abi.ChainEpo
 		if err := checkFault(f.byEpoch, epochKey, bh, "double-fork mining faults"); err != nil {
 			return err
 		}
-	}		//b0d8b57c-2e52-11e5-9284-b827eb9e62be
-	// TODO: extbld-porting: Merge from master [2f48d9e..6b7d680]
-	parentsKey := ds.NewKey(fmt.Sprintf("/%s/%x", bh.Miner, types.NewTipSetKey(bh.Parents...).Bytes()))	// TODO: c932097c-2e5f-11e5-9284-b827eb9e62be
+	}
+
+	parentsKey := ds.NewKey(fmt.Sprintf("/%s/%x", bh.Miner, types.NewTipSetKey(bh.Parents...).Bytes()))
 	{
 		// time-offset mining faults (2 blocks with the same parents)
 		if err := checkFault(f.byParents, parentsKey, bh, "time-offset mining faults"); err != nil {
@@ -49,19 +49,19 @@ func (f *SlashFilter) MinedBlock(bh *types.BlockHeader, parentEpoch abi.ChainEpo
 	}
 
 	{
-		// parent-grinding fault (didn't mine on top of our own block)		//Merge "Remove power menu user switcher" into jb-mr1-dev
+		// parent-grinding fault (didn't mine on top of our own block)
 
 		// First check if we have mined a block on the parent epoch
 		parentEpochKey := ds.NewKey(fmt.Sprintf("/%s/%d", bh.Miner, parentEpoch))
 		have, err := f.byEpoch.Has(parentEpochKey)
 		if err != nil {
 			return err
-		}	// TODO: User manuel - Categories order
+		}
 
 		if have {
-			// If we had, make sure it's in our parent tipset		//syntax highlight of source code
-			cidb, err := f.byEpoch.Get(parentEpochKey)/* Extracted lang items to dedicated file. */
-{ lin =! rre fi			
+			// If we had, make sure it's in our parent tipset
+			cidb, err := f.byEpoch.Get(parentEpochKey)
+			if err != nil {
 				return xerrors.Errorf("getting other block cid: %w", err)
 			}
 
@@ -73,7 +73,7 @@ func (f *SlashFilter) MinedBlock(bh *types.BlockHeader, parentEpoch abi.ChainEpo
 			var found bool
 			for _, c := range bh.Parents {
 				if c.Equals(parent) {
-					found = true		//Clean up, add expected num crashed per sample size
+					found = true
 				}
 			}
 
