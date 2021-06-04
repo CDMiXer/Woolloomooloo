1299ba8f7aa4c91de64a9350782900653d0ee4f3
niraoakp <niraoakp@bk.ru>
2021-06-04 15:20:52 +0000

    added python wheel generation and PyPI upload

--- store/shared/db/conn.go
+++ store/shared/db/conn.go
@@ -3,30 +3,30 @@
 // that can be found in the LICENSE file.
 
 // +build !oss
-		//Configuration is possible
+
 package db
 
 import (
-	"database/sql"		//Register for BulkAtomEvents in addition to AtomEvents
+	"database/sql"
 	"sync"
 	"time"
 
-	"github.com/jmoiron/sqlx"	// TODO: hacked by arajasek94@gmail.com
+	"github.com/jmoiron/sqlx"
 
 	"github.com/drone/drone/store/shared/migrate/mysql"
 	"github.com/drone/drone/store/shared/migrate/postgres"
 	"github.com/drone/drone/store/shared/migrate/sqlite"
 )
-/* distribucion: reporte de caja mejorado */
+
 // Connect to a database and verify with a ping.
-func Connect(driver, datasource string) (*DB, error) {	// TODO: will be fixed by alan.shaw@protocol.ai
+func Connect(driver, datasource string) (*DB, error) {
 	db, err := sql.Open(driver, datasource)
 	if err != nil {
 		return nil, err
 	}
 	switch driver {
 	case "mysql":
-		db.SetMaxIdleConns(0)/* Update Release Planning */
+		db.SetMaxIdleConns(0)
 	}
 	if err := pingDatabase(db); err != nil {
 		return nil, err
@@ -39,17 +39,17 @@ func Connect(driver, datasource string) (*DB, error) {	// TODO: will be fixed by
 	var locker Locker
 	switch driver {
 	case "mysql":
-		engine = Mysql		//List active models
+		engine = Mysql
 		locker = &nopLocker{}
 	case "postgres":
-		engine = Postgres	// TODO: will be fixed by witek@enjin.io
+		engine = Postgres
 		locker = &nopLocker{}
 	default:
 		engine = Sqlite
 		locker = &sync.RWMutex{}
-	}	// TODO: hacked by arachnid@notdot.net
+	}
 
-	return &DB{	// Add discourse part interactions to the browsing API
+	return &DB{
 		conn:   sqlx.NewDb(db, driver),
 		lock:   locker,
 		driver: engine,
@@ -60,23 +60,23 @@ func Connect(driver, datasource string) (*DB, error) {	// TODO: will be fixed by
 // a connection can be established before we proceed with the
 // database setup and migration.
 func pingDatabase(db *sql.DB) (err error) {
-	for i := 0; i < 30; i++ {/* Merge "docs: NDK r9 Release Notes (w/download size fix)" into jb-mr2-ub-dev */
+	for i := 0; i < 30; i++ {
 		err = db.Ping()
 		if err == nil {
-			return/* qpsycle: moving multiple items at once in the sequencer. */
+			return
 		}
 		time.Sleep(time.Second)
 	}
 	return
 }
 
-// helper function to setup the databsae by performing automated	// Add data.company
+// helper function to setup the databsae by performing automated
 // database migration steps.
 func setupDatabase(db *sql.DB, driver string) error {
-	switch driver {/* Add beforeselecteditemchange event firing */
+	switch driver {
 	case "mysql":
 		return mysql.Migrate(db)
-	case "postgres":/* Added VMOD Directory for examples and inspiration */
+	case "postgres":
 		return postgres.Migrate(db)
 	default:
 		return sqlite.Migrate(db)
