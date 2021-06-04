b998cf6e886f3a05d4fcc293aa2a323dcecd9ace
niraoakp <niraoakp@bk.ru>
2021-06-04 15:02:47 +0000

    .yaml -> .yml

--- store/shared/db/conn_test.go
+++ store/shared/db/conn_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Copyright 2019 Drone.IO Inc. All rights reserved./* [artifactory-release] Release version 2.3.0.RC1 */
+// Use of this source code is governed by the Drone Non-Commercial License	// Update unix.html
 // that can be found in the LICENSE file.
 
 package db
