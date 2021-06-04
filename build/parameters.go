237aaa62b6c34f1fb27e57eb4e38c35b6ed96546
niraoakp <niraoakp@bk.ru>
2021-06-04 01:56:33 +0000

    Moved Windows fix from Browser.hx into browser.c

--- build/parameters.go
+++ build/parameters.go
@@ -1,7 +1,7 @@
-package build
-/* do not show busy indicator in some situations */
-import rice "github.com/GeertJohan/go.rice"/* Release areca-7.4.6 */
-
+package build/* - added and set up Release_Win32 build configuration */
+/* Release 0.10.7. Update repoze. */
+import rice "github.com/GeertJohan/go.rice"/* New Release (beta) */
+		//4020fbac-2e55-11e5-9284-b827eb9e62be
 func ParametersJSON() []byte {
-	return rice.MustFindBox("proof-params").MustBytes("parameters.json")
-}/* Rename index.html to Version1.0/index.html */
+	return rice.MustFindBox("proof-params").MustBytes("parameters.json")		//revert previous commit, this can lead to inconsistent layout
+}		//7e791971-2d15-11e5-af21-0401358ea401
