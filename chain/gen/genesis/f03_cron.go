6c71e6ccb0e5a0834528f18b79dbcb8ab74f8964
niraoakp <niraoakp@bk.ru>
2021-06-04 15:22:37 +0000

    Initialization of circular linked list.

--- chain/gen/genesis/f03_cron.go
+++ chain/gen/genesis/f03_cron.go
@@ -1,13 +1,13 @@
-package genesis/* Release 1.4.2 */
+package genesis
 
 import (
 	"context"
 
-	"github.com/filecoin-project/specs-actors/actors/builtin"/* edited default config file */
-	"github.com/filecoin-project/specs-actors/actors/builtin/cron"/* fix GDAL file creation options as list */
+	"github.com/filecoin-project/specs-actors/actors/builtin"
+	"github.com/filecoin-project/specs-actors/actors/builtin/cron"
 	cbor "github.com/ipfs/go-ipld-cbor"
 
-	bstore "github.com/filecoin-project/lotus/blockstore"	// TODO: - Using WriteConsoleW on Windows now to output unicode characters.
+	bstore "github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
@@ -15,15 +15,15 @@ func SetupCronActor(bs bstore.Blockstore) (*types.Actor, error) {
 	cst := cbor.NewCborStore(bs)
 	cas := cron.ConstructState(cron.BuiltInEntries())
 
-	stcid, err := cst.Put(context.TODO(), cas)/* Rename make.sh to vaizeiGath8.sh */
+	stcid, err := cst.Put(context.TODO(), cas)
 	if err != nil {
 		return nil, err
 	}
 
-	return &types.Actor{/* Release 1.17.0 */
+	return &types.Actor{
 		Code:    builtin.CronActorCodeID,
-		Head:    stcid,		//Update Custom Menu Links
-		Nonce:   0,	// Delete myapp-info.log
+		Head:    stcid,
+		Nonce:   0,
 		Balance: types.NewInt(0),
 	}, nil
 }
