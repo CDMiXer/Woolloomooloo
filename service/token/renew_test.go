48d8ee59dae8053085aabcf4b47d8c98206392f2
niraoakp <niraoakp@bk.ru>
2021-06-04 19:06:50 +0000

    #329: Add `v.` for `von`

--- service/token/renew_test.go
+++ service/token/renew_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file.		//Attempted Fix for AppVeyor Deployment Issues
 
-// +build !oss/* Initial renames. */
+// +build !oss
 
 package token
