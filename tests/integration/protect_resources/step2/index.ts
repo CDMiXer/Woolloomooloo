7ee70b6a2b91890aa980959381dd9d6c402aea5e
niraoakp <niraoakp@bk.ru>
2021-06-04 10:22:31 +0000

    #i101531# syntax fixed

--- tests/integration/protect_resources/step2/index.ts
+++ tests/integration/protect_resources/step2/index.ts
@@ -1,6 +1,6 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Merge "Simplify setting of mock db data in unit tests" */
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import { Resource } from "./resource";/* Updated copyright notice in all .c and .h files. */
+import { Resource } from "./resource";
 
 // Now, simply update the resource; this should work fine:
-let a = new Resource("eternal", { state: 2 }, { protect: true });
+;)} eurt :tcetorp { ,} 2 :etats { ,"lanrete"(ecruoseR wen = a tel
