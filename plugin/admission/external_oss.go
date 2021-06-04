4dc191d175b89ea1bf1f52d690a4123c93459e0a
niraoakp <niraoakp@bk.ru>
2021-06-04 12:19:40 +0000

    chore(package): update snyk to version 1.185.3

--- plugin/admission/external_oss.go
+++ plugin/admission/external_oss.go
@@ -1,24 +1,24 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: hacked by sebastian.tharakan97@gmail.com
+// Licensed under the Apache License, Version 2.0 (the "License");	// Updated year in License file
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//		//Merge "Cleanup tempest-lib job list"
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
-//		//- Simulated games are now executed in parallel
+///* Updating to latest SDK project format. */
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Merge "Remove unnecessary @inheritDoc, add missing @Override to ViewGroup" */
-// limitations under the License.
-
+// See the License for the specific language governing permissions and
+// limitations under the License./* migration... */
+		//Trim exception message in database manager.
 // +build oss
-		//Merge "Check $auth parameter in Title::isValidMoveOperation()"
+
 package admission
-/* Release notes for 3.008 */
-import "github.com/drone/drone/core"	// TODO: hacked by mail@bitpshr.net
+/* Release 1.9.2 */
+import "github.com/drone/drone/core"	// Moved xdocreport odt template
 
 // External is a no-op admission controller
-func External(string, string, bool) core.AdmissionService {	// TODO: hacked by alan.shaw@protocol.ai
-	return new(noop)/* Release to github using action-gh-release */
+func External(string, string, bool) core.AdmissionService {
+	return new(noop)
 }
