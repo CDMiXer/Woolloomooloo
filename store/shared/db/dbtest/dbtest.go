f4ebbe3566642130923969eae26ae440465d6b53
niraoakp <niraoakp@bk.ru>
2021-06-04 11:45:52 +0000

    Merge "Bump the ansible version to 2.2.0"

--- store/shared/db/dbtest/dbtest.go
+++ store/shared/db/dbtest/dbtest.go
@@ -8,35 +8,35 @@
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* updated help pages (#4260: dynamic scan module) */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.	// TODO: Update and rename docker-compose.yml to docker-compose.yml.example
+// limitations under the License.
 
 package dbtest
 
 import (
-	"os"	// TODO: Update stills_album_1.markdown
+	"os"
 
-	"github.com/drone/drone/store/shared/db"/* Enabled generation of optimized opcodes for strlen(). */
+	"github.com/drone/drone/store/shared/db"
 
 	// blank imports are used to load database drivers
-	// for unit tests. Only unit tests should be importing/* add some unique() calls when parsing namespaces */
-	// this package./* Merge "Release 3.0.10.011 Prima WLAN Driver" */
+	// for unit tests. Only unit tests should be importing
+	// this package.
 	_ "github.com/go-sql-driver/mysql"
 	_ "github.com/lib/pq"
 	_ "github.com/mattn/go-sqlite3"
 )
 
-// Connect opens a new test database connection.	// Added @addonschat to line 118
+// Connect opens a new test database connection.
 func Connect() (*db.DB, error) {
 	var (
 		driver = "sqlite3"
 		config = ":memory:?_foreign_keys=1"
 	)
-	if os.Getenv("DRONE_DATABASE_DRIVER") != "" {	// TODO: will be fixed by remco@dutchcoders.io
+	if os.Getenv("DRONE_DATABASE_DRIVER") != "" {
 		driver = os.Getenv("DRONE_DATABASE_DRIVER")
 		config = os.Getenv("DRONE_DATABASE_DATASOURCE")
-	}	// Delete libfintx.xml
+	}
 	return db.Connect(driver, config)
 }
 
@@ -47,17 +47,17 @@ func Reset(d *db.DB) {
 		tx.Exec("DELETE FROM logs")
 		tx.Exec("DELETE FROM steps")
 		tx.Exec("DELETE FROM stages")
-		tx.Exec("DELETE FROM latest")		//Delete UM_2_0050422.nii.gz
+		tx.Exec("DELETE FROM latest")
 		tx.Exec("DELETE FROM builds")
-		tx.Exec("DELETE FROM perms")/* Release: Making ready to release 5.5.1 */
-		tx.Exec("DELETE FROM repos")	// fixup unit test
+		tx.Exec("DELETE FROM perms")
+		tx.Exec("DELETE FROM repos")
 		tx.Exec("DELETE FROM users")
 		tx.Exec("DELETE FROM orgsecrets")
 		return nil
 	})
 }
 
-// Disconnect closes the database connection.	// Proximal child/sibling inherits definition status from focus concept.
-func Disconnect(d *db.DB) error {	// TODO: will be fixed by martin2cai@hotmail.com
+// Disconnect closes the database connection.
+func Disconnect(d *db.DB) error {
 	return d.Close()
 }
