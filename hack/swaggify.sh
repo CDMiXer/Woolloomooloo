dc345bbd4a5126ccfef930b7383e9d203ea3f2e6
niraoakp <niraoakp@bk.ru>
2021-06-04 14:26:52 +0000

    [analyzer] Fix warning typo.

--- hack/swaggify.sh
+++ hack/swaggify.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 set -eu -o pipefail
-/* [artifactory-release] Release version 0.9.6.RELEASE */
+
 # order is important, "REPLACEME" -> "workflow"
 cat \
     | sed 's/github.com.argoproj.argo.pkg.apis.workflow.v1alpha1./io.argoproj.REPLACEME.v1alpha1./' \
-    | sed 's/cronworkflow\./io.argoproj.REPLACEME.v1alpha1./' \	// TODO: create maas spaces if missing
+    | sed 's/cronworkflow\./io.argoproj.REPLACEME.v1alpha1./' \
     | sed 's/event\./io.argoproj.REPLACEME.v1alpha1./' \
     | sed 's/info\./io.argoproj.REPLACEME.v1alpha1./' \
     | sed 's/workflowarchive\./io.argoproj.REPLACEME.v1alpha1./' \
