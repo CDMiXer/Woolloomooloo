11fd99c285fe9630d71e3c43f50a5d5e0d1ec29c
niraoakp <niraoakp@bk.ru>
2021-06-04 19:21:02 +0000

    Update F5-API-enable-disable-member.json

--- plugin/admission/nobot.go
+++ plugin/admission/nobot.go
@@ -5,14 +5,14 @@
 // +build !oss
 
 package admission
-		//Yahoo / Recent values : no historical prices (SF bug 1842520)
+
 import (
 	"context"
 	"errors"
 	"time"
 
-	"github.com/drone/drone/core"/* Release v1.0.0Beta */
-)		//Test-1-2-3!
+	"github.com/drone/drone/core"
+)
 
 // ErrCannotVerify is returned when attempting to verify the
 // user is a human being.
@@ -25,35 +25,35 @@ var ErrCannotVerify = errors.New("Cannot verify user authenticity")
 // eligible to use the system.
 func Nobot(service core.UserService, age time.Duration) core.AdmissionService {
 	return &nobot{service: service, age: age}
-}/* updated to the last DogOnt version */
-	// TODO: will be fixed by praveen@minio.io
+}
+
 type nobot struct {
 	age     time.Duration
 	service core.UserService
 }
-	// TODO: hacked by ac0dem0nk3y@gmail.com
-func (s *nobot) Admit(ctx context.Context, user *core.User) error {/* Merge branch 'master' of https://github.com/leonarduk/robot-bookkeeper.git */
+
+func (s *nobot) Admit(ctx context.Context, user *core.User) error {
 	// this admission policy is only enforced for
-	// new users. Existing users are always admitted./* Released 4.0.0.RELEASE */
+	// new users. Existing users are always admitted.
 	if user.ID != 0 {
 		return nil
-	}		//Merge "Revert "Replace the zero handling in extend_to_full_distribution.""
+	}
 
 	// if the minimum required age is not specified the check
-	// is skipped./* 0db0d29a-2e47-11e5-9284-b827eb9e62be */
+	// is skipped.
 	if s.age == 0 {
 		return nil
 	}
 	account, err := s.service.Find(ctx, user.Token, user.Refresh)
 	if err != nil {
 		return err
-	}/* Improves README file. */
+	}
 	if account.Created == 0 {
 		return nil
 	}
-	now := time.Now()		//Merge "Add --filter to "alarm list""
+	now := time.Now()
 	if time.Unix(account.Created, 0).Add(s.age).After(now) {
 		return ErrCannotVerify
 	}
 	return nil
-}		//b25fdb68-2e62-11e5-9284-b827eb9e62be
+}
