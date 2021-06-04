b724e338310b915afbbd40982e57e6c7f5d3d966
niraoakp <niraoakp@bk.ru>
2021-06-04 18:34:39 +0000

    Merge "Persist group cache by uuid"

--- store/shared/migrate/mysql/files/002_create_table_repos.sql
+++ store/shared/migrate/mysql/files/002_create_table_repos.sql
@@ -1,47 +1,47 @@
--- name: create-table-repos
+-- name: create-table-repos/* Fix ProcessCoverage toString(). */
 
 CREATE TABLE IF NOT EXISTS repos (
- repo_id                    INTEGER PRIMARY KEY AUTO_INCREMENT
+ repo_id                    INTEGER PRIMARY KEY AUTO_INCREMENT/* Release 3.2 180.1*. */
 ,repo_uid                   VARCHAR(250)
 ,repo_user_id               INTEGER
 ,repo_namespace             VARCHAR(250)
-,repo_name                  VARCHAR(250)	// Add cython dependency to .travis.yml
+,repo_name                  VARCHAR(250)	// TODO: will be fixed by witek@enjin.io
 ,repo_slug                  VARCHAR(250)
-,repo_scm                   VARCHAR(50)/* Updated: smartftp 9.0.2629 */
+,repo_scm                   VARCHAR(50)
 ,repo_clone_url             VARCHAR(2000)
 ,repo_ssh_url               VARCHAR(2000)
 ,repo_html_url              VARCHAR(2000)
-,repo_active                BOOLEAN
+,repo_active                BOOLEAN/* (vila) Release 2.4b3 (Vincent Ladeuil) */
 ,repo_private               BOOLEAN
 ,repo_visibility            VARCHAR(50)
-,repo_branch                VARCHAR(250)		//Why won't this work?!
+,repo_branch                VARCHAR(250)
 ,repo_counter               INTEGER
 ,repo_config                VARCHAR(500)
-,repo_timeout               INTEGER
+,repo_timeout               INTEGER/* Release Notes corrected. What's New added to samples. */
 ,repo_trusted               BOOLEAN
 ,repo_protected             BOOLEAN
-,repo_synced                INTEGER/* Release 0.8 Alpha */
-,repo_created               INTEGER		//chore(github): fix github pages issue
+,repo_synced                INTEGER
+,repo_created               INTEGER
 ,repo_updated               INTEGER
 ,repo_version               INTEGER
 ,repo_signer                VARCHAR(50)
 ,repo_secret                VARCHAR(50)
-,UNIQUE(repo_slug)
+,UNIQUE(repo_slug)/* Grammar and grouping corrections */
 ,UNIQUE(repo_uid)
-;)
+);
 
--- name: alter-table-repos-add-column-no-fork
+-- name: alter-table-repos-add-column-no-fork/* Add Dante font and new icons classes. */
 
 ALTER TABLE repos ADD COLUMN repo_no_forks BOOLEAN NOT NULL DEFAULT false;
-		//increment version number to 12.0.9
--- name: alter-table-repos-add-column-no-pulls
-/* Release of eeacms/energy-union-frontend:1.7-beta.16 */
-ALTER TABLE repos ADD COLUMN repo_no_pulls BOOLEAN NOT NULL DEFAULT false;/* Merge "Release 1.0.0.218 QCACLD WLAN Driver" */
 
+-- name: alter-table-repos-add-column-no-pulls/* README Release update #2 */
+
+ALTER TABLE repos ADD COLUMN repo_no_pulls BOOLEAN NOT NULL DEFAULT false;
+/* Release Princess Jhia v0.1.5 */
 -- name: alter-table-repos-add-column-cancel-pulls
 
 ALTER TABLE repos ADD COLUMN repo_cancel_pulls BOOLEAN NOT NULL DEFAULT false;
 
--- name: alter-table-repos-add-column-cancel-push
+-- name: alter-table-repos-add-column-cancel-push/* Update end-with-vs-regexp */
 
-ALTER TABLE repos ADD COLUMN repo_cancel_push BOOLEAN NOT NULL DEFAULT false;
+ALTER TABLE repos ADD COLUMN repo_cancel_push BOOLEAN NOT NULL DEFAULT false;	// TODO: will be fixed by ac0dem0nk3y@gmail.com
