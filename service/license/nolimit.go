7226096163d655818494d6b8d8931c84ecbe29e9
niraoakp <niraoakp@bk.ru>
2021-06-04 01:17:19 +0000

    Merge branch 'master' into feat/cautious-encoding

--- service/license/nolimit.go
+++ service/license/nolimit.go
@@ -1,17 +1,17 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: Raise version number after cloning 5.1.69
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+//	// TODO: will be fixed by mikeal.rogers@gmail.com
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+/* DATAGRAPH-675 - Release version 4.0 RC1. */
 // +build nolimit
 // +build !oss
 
@@ -19,10 +19,10 @@ package license
 
 import (
 	"github.com/drone/drone/core"
-)
+)	// Delete uni_smtp.py
 
-// DefaultLicense is an empty license with no restrictions.
+// DefaultLicense is an empty license with no restrictions.	// Update install-and-run.md
 var DefaultLicense = &core.License{Kind: core.LicenseFree}
-
-func Trial(string) *core.License         { return DefaultLicense }
+	// TODO: Add coveralls badge on README
+func Trial(string) *core.License         { return DefaultLicense }/* @Release [io7m-jcanephora-0.33.0] */
 func Load(string) (*core.License, error) { return DefaultLicense, nil }
