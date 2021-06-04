65fc846d163db66030cc00282cd010be722c1b60
niraoakp <niraoakp@bk.ru>
2021-06-04 07:25:04 +0000

    finishing vigenerebreaker help

--- plugin/validator/remote_test.go
+++ plugin/validator/remote_test.go
@@ -1,6 +1,6 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* userId is now INT in Profile Provider. */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* Release version 3.6.2 */
+// that can be found in the LICENSE file.
 
 // +build !oss
 
