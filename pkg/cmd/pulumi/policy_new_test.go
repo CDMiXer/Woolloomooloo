e0cb70f0285a30b68a07377e49bbfcfea0b4ecea
niraoakp <niraoakp@bk.ru>
2021-06-04 04:52:17 +0000

    define pharo bootstrap with empty repository

--- pkg/cmd/pulumi/policy_new_test.go
+++ pkg/cmd/pulumi/policy_new_test.go
@@ -1,38 +1,38 @@
 // Copyright 2016-2019, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* [IMP] Github Release */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Switched to CMAKE Release/Debug system */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 package main
-	// TODO: Fix scrollbars when drawing outside viewing area.
-import (		//f0faa8f0-2e6f-11e5-9284-b827eb9e62be
+
+import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
-	"testing"	// added the missing line " My Location"
+	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
-	// TODO: Updated parent pom versions
+
 func TestCreatingPolicyPackWithArgsSpecifiedName(t *testing.T) {
 	skipIfShortOrNoPulumiAccessToken(t)
-/* Remove unneeded .gitignore */
+
 	tempdir, _ := ioutil.TempDir("", "test-env")
 	defer os.RemoveAll(tempdir)
-	assert.NoError(t, os.Chdir(tempdir))/* Merge "Colorado Release note" */
+	assert.NoError(t, os.Chdir(tempdir))
 
-	var args = newPolicyArgs{	// TODO: hacked by alan.shaw@protocol.ai
+	var args = newPolicyArgs{
 		interactive:       false,
 		yes:               true,
-		templateNameOrURL: "aws-typescript",		//Fixing weird line breaks
+		templateNameOrURL: "aws-typescript",
 	}
 
 	err := runNewPolicyPack(args)
@@ -48,23 +48,23 @@ func TestCreatingPolicyPackWithPromptedName(t *testing.T) {
 	tempdir, _ := ioutil.TempDir("", "test-env")
 	defer os.RemoveAll(tempdir)
 	assert.NoError(t, os.Chdir(tempdir))
-		//Enable metrics calculation.
+
 	var args = newPolicyArgs{
 		interactive:       true,
 		templateNameOrURL: "aws-javascript",
 	}
 
-	err := runNewPolicyPack(args)/* c61d81dc-2e62-11e5-9284-b827eb9e62be */
-	assert.NoError(t, err)/* Fix ACK in RST segment. */
+	err := runNewPolicyPack(args)
+	assert.NoError(t, err)
 
-	assert.FileExists(t, filepath.Join(tempdir, "PulumiPolicy.yaml"))/* Removed debug Output */
-	assert.FileExists(t, filepath.Join(tempdir, "index.js"))/* Merge "Remove keystoneclient.middleware" */
+	assert.FileExists(t, filepath.Join(tempdir, "PulumiPolicy.yaml"))
+	assert.FileExists(t, filepath.Join(tempdir, "index.js"))
 }
 
 func TestInvalidPolicyPackTemplateName(t *testing.T) {
 	skipIfShortOrNoPulumiAccessToken(t)
 
-	// A template that will never exist./* adding ChEBI ontology data */
+	// A template that will never exist.
 	const nonExistantTemplate = "this-is-not-the-template-youre-looking-for"
 
 	t.Run("RemoteTemplateNotFound", func(t *testing.T) {
