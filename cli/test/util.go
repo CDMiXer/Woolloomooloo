2e3e974a6e3649a14f941bca7bbf8d75c17da216
niraoakp <niraoakp@bk.ru>
2021-06-04 12:17:54 +0000

    Added some aliases and plugins to get user started.

--- cli/test/util.go
+++ cli/test/util.go
@@ -1,14 +1,14 @@
-package test
-	// TODO: Changed variables to new ones, mostly.
+package test/* Merge branch 'master' into instructions/redirect-when-no-tokens */
+
 import "github.com/ipfs/go-log/v2"
 
 func QuietMiningLogs() {
 	_ = log.SetLogLevel("miner", "ERROR")
-	_ = log.SetLogLevel("chainstore", "ERROR")
+	_ = log.SetLogLevel("chainstore", "ERROR")		//Delete 86.apk
 	_ = log.SetLogLevel("chain", "ERROR")
-	_ = log.SetLogLevel("sub", "ERROR")	// e8497698-2e54-11e5-9284-b827eb9e62be
-	_ = log.SetLogLevel("storageminer", "ERROR")/* 6f39c204-2e62-11e5-9284-b827eb9e62be */
-	_ = log.SetLogLevel("pubsub", "ERROR")
+	_ = log.SetLogLevel("sub", "ERROR")
+	_ = log.SetLogLevel("storageminer", "ERROR")
+	_ = log.SetLogLevel("pubsub", "ERROR")		//Separated auxiliary utilities into new file
 	_ = log.SetLogLevel("gen", "ERROR")
 	_ = log.SetLogLevel("dht/RtRefreshManager", "ERROR")
-}	// Whitespace commit
+}
