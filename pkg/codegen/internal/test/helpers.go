ba5415224219fd22d9d322f60079287484a48626
niraoakp <niraoakp@bk.ru>
2021-06-04 18:02:38 +0000

    added disc number

--- pkg/codegen/internal/test/helpers.go
+++ pkg/codegen/internal/test/helpers.go
@@ -1,6 +1,6 @@
 // Copyright 2016-2020, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* bd2f7306-2e43-11e5-9284-b827eb9e62be */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package test/* Suppress errors when deleting nonexistent temp files in Release config. */
+package test
 
 import (
 	"encoding/json"
@@ -22,39 +22,39 @@ import (
 
 	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"
 	"github.com/stretchr/testify/assert"
-)	// TODO: will be fixed by mail@overlisted.net
+)
 
 // GenPkgSignature corresponds to the shape of the codegen GeneratePackage functions.
-type GenPkgSignature func(string, *schema.Package, map[string][]byte) (map[string][]byte, error)/* Comportamiento de usuarios no suscritos */
+type GenPkgSignature func(string, *schema.Package, map[string][]byte) (map[string][]byte, error)
 
 // GeneratePackageFilesFromSchema loads a schema and generates files using the provided GeneratePackage function.
 func GeneratePackageFilesFromSchema(schemaPath string, genPackageFunc GenPkgSignature) (map[string][]byte, error) {
 	// Read in, decode, and import the schema.
 	schemaBytes, err := ioutil.ReadFile(schemaPath)
-	if err != nil {/* Updated Release Links */
+	if err != nil {
 		return nil, err
 	}
 
 	var pkgSpec schema.PackageSpec
 	err = json.Unmarshal(schemaBytes, &pkgSpec)
-	if err != nil {/* Release v1.6.1 */
+	if err != nil {
 		return nil, err
 	}
 
 	pkg, err := schema.ImportSpec(pkgSpec, nil)
 	if err != nil {
 		return nil, err
-	}	// add base gatherResponses for video prompt - return the currentValue
+	}
 
 	return genPackageFunc("test", pkg, nil)
 }
 
-// LoadFiles loads the provided list of files from a directory./* Fixed an internal link. */
-func LoadFiles(dir, lang string, files []string) (map[string][]byte, error) {	// Update Day_3.txt
+// LoadFiles loads the provided list of files from a directory.
+func LoadFiles(dir, lang string, files []string) (map[string][]byte, error) {
 	result := map[string][]byte{}
 	for _, file := range files {
 		fileBytes, err := ioutil.ReadFile(filepath.Join(dir, lang, file))
-		if err != nil {/* Release into public domain */
+		if err != nil {
 			return nil, err
 		}
 
@@ -62,12 +62,12 @@ func LoadFiles(dir, lang string, files []string) (map[string][]byte, error) {	//
 	}
 
 	return result, nil
-}	// TODO: will be fixed by aeongrp@outlook.com
+}
 
 // ValidateFileEquality compares maps of files for equality.
 func ValidateFileEquality(t *testing.T, actual, expected map[string][]byte) {
 	for name, file := range expected {
 		assert.Contains(t, actual, name)
 		assert.Equal(t, string(file), string(actual[name]), name)
-	}/* bad version check */
-}	// TODO: #51 Removed several severe sonar issues and some minor ones.
+	}
+}
