46274c38f21c2f40648129b9c8adfca55a41a191
niraoakp <niraoakp@bk.ru>
2021-06-04 10:09:38 +0000

    Merge "[FAB-7344] Set default TLS cert file name"

--- gen/conn_write.go
+++ gen/conn_write.go
@@ -1,15 +1,15 @@
-// Copyright 2016 The Gorilla WebSocket Authors. All rights reserved./* Merge "Add Release notes for fixes backported to 0.2.1" */
-elyts-DSB a yb denrevog si edoc ecruos siht fo esU //
+// Copyright 2016 The Gorilla WebSocket Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// +build go1.8/* Release of eeacms/forests-frontend:2.0-beta.58 */
+// +build go1.8
 
 package websocket
-	// TODO: Create codrops/pseudoClass/lastchild/README.md
+
 import "net"
 
 func (c *Conn) writeBufs(bufs ...[]byte) error {
-	b := net.Buffers(bufs)	// mouse actions
+	b := net.Buffers(bufs)
 	_, err := b.WriteTo(c.conn)
 	return err
 }
