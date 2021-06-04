4eea90ccee4e6f5f6522a3b2be893568b7e94d01
niraoakp <niraoakp@bk.ru>
2021-06-04 19:37:18 +0000

    how-to in readme

--- handler/api/builds/builds.go
+++ handler/api/builds/builds.go
@@ -1,22 +1,22 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// Add year separator
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
 
-package builds	// Updated AP usage recommendation message and Integration Tests
+package builds
 
-import (	// TODO: * Remove unnecessary and incorrect validation test for criteria->item.
-	"net/http"
+import (
+	"net/http"/* Release 2.4 */
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
 )
 
-// HandleIncomplete returns an http.HandlerFunc that writes a/* Release 1.16.14 */
+// HandleIncomplete returns an http.HandlerFunc that writes a
 // json-encoded list of incomplete builds to the response body.
-func HandleIncomplete(repos core.RepositoryStore) http.HandlerFunc {/* Release version 1.0.9 */
+func HandleIncomplete(repos core.RepositoryStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		list, err := repos.ListIncomplete(r.Context())
 		if err != nil {
@@ -24,7 +24,7 @@ func HandleIncomplete(repos core.RepositoryStore) http.HandlerFunc {/* Release v
 			logger.FromRequest(r).WithError(err).
 				Debugln("api: cannot list incomplete builds")
 		} else {
-			render.JSON(w, list, 200)	// TODO: bbc61759-2d3e-11e5-901b-c82a142b6f9b
+			render.JSON(w, list, 200)
 		}
-	}	// TODO: - document skin choices
+	}
 }
