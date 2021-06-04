75c2a96b6b381339f0ccd905345602f8a8cd9bd8
niraoakp <niraoakp@bk.ru>
2021-06-04 18:13:04 +0000

    got the pci bus to use a fake-dictionary.

--- metric/sink/config.go
+++ metric/sink/config.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone IO, Inc./* Improved Logging In Debug+Release Mode */
-///* Release Notes for v01-00 */
+// Copyright 2019 Drone IO, Inc.
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -10,14 +10,14 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* added devtools for documentation */
+// limitations under the License.
 
 package sink
 
 // Config configures a Datadog sink.
 type Config struct {
 	Endpoint string
-	Token    string		//add counter for mapping categories
+	Token    string
 
 	License          string
 	Licensor         string
@@ -30,6 +30,6 @@ type Config struct {
 	EnableGogs       bool
 	EnableGitea      bool
 	EnableAgents     bool
-	EnableNomad      bool	// Merge "Title: Always add title to LinkCache when necessary (in 3 methods)"
+	EnableNomad      bool
 	EnableKubernetes bool
 }
