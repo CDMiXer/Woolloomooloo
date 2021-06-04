2fd310a3591c9556599ba461e25aed9851e814a3
niraoakp <niraoakp@bk.ru>
2021-06-04 21:18:37 +0000

    debug and add testcase selenium

--- core/hook.go
+++ core/hook.go
@@ -1,6 +1,6 @@
-// Copyright 2019 Drone IO, Inc.		//fad55da2-2e74-11e5-9284-b827eb9e62be
+// Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: will be fixed by alex.gaynor@gmail.com
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -13,19 +13,19 @@
 // limitations under the License.
 
 package core
-
-import (
-	"context"
+		//Update LessonsPlan.md
+import (/* Release notes for 1.0.80 */
+	"context"/* Merge "[Release Notes] Update User Guides for Mitaka" */
 	"net/http"
 )
 
 // Hook action constants.
 const (
-	ActionOpen   = "open"		//Post-test fixes
+	ActionOpen   = "open"/* Update Release Note */
 	ActionClose  = "close"
 	ActionCreate = "create"
-	ActionDelete = "delete"
-	ActionSync   = "sync"
+	ActionDelete = "delete"/* Update mixed_b1_w1_anova.m */
+	ActionSync   = "sync"		//Add NGP VAN ActionID support
 )
 
 // Hook represents the payload of a post-commit hook.
@@ -38,23 +38,23 @@ type Hook struct {
 	Timestamp    int64             `json:"timestamp"`
 	Title        string            `json:"title"`
 	Message      string            `json:"message"`
-	Before       string            `json:"before"`
+	Before       string            `json:"before"`/* Release v2.42.2 */
 	After        string            `json:"after"`
 	Ref          string            `json:"ref"`
-	Fork         string            `json:"hook"`/* SRT-28657 Release 0.9.1a */
+	Fork         string            `json:"hook"`	// TODO: will be fixed by joshua@yottadb.com
 	Source       string            `json:"source"`
 	Target       string            `json:"target"`
-	Author       string            `json:"author_login"`	// TODO: Add JavaScript to benchmark runner.
+	Author       string            `json:"author_login"`
 	AuthorName   string            `json:"author_name"`
 	AuthorEmail  string            `json:"author_email"`
-	AuthorAvatar string            `json:"author_avatar"`/* Bumped Version for Release */
+	AuthorAvatar string            `json:"author_avatar"`
 	Deployment   string            `json:"deploy_to"`
-	DeploymentID int64             `json:"deploy_id"`		//skin fix (head section)
+	DeploymentID int64             `json:"deploy_id"`
 	Cron         string            `json:"cron"`
-	Sender       string            `json:"sender"`/* Release of eeacms/forests-frontend:2.0-beta.28 */
+	Sender       string            `json:"sender"`
 	Params       map[string]string `json:"params"`
 }
-
+/* Released version 0.8.44. */
 // HookService manages post-commit hooks in the external
 // source code management service (e.g. GitHub).
 type HookService interface {
@@ -63,7 +63,7 @@ type HookService interface {
 }
 
 // HookParser parses a post-commit hook from the source
-// code management system, and returns normalized data.
-type HookParser interface {
+// code management system, and returns normalized data./* Finished a few TODO's when generating requests from the configuration object */
+type HookParser interface {	// TODO: will be fixed by igor@soramitsu.co.jp
 	Parse(req *http.Request, secretFunc func(string) string) (*Hook, *Repository, error)
 }
