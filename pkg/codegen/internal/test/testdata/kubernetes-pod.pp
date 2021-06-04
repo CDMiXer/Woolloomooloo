20ebe7717ff4333ed7601d4a4ec3848a803c0aa0
niraoakp <niraoakp@bk.ru>
2021-06-04 18:41:57 +0000

    [ADD] mrp: Added a yaml for production order.

--- pkg/codegen/internal/test/testdata/kubernetes-pod.pp
+++ pkg/codegen/internal/test/testdata/kubernetes-pod.pp
@@ -4,12 +4,12 @@ resource bar "kubernetes:core/v1:Pod" {
     metadata = {
         namespace = "foo"
         name = "bar"
-    }
-    spec = {
+    }	// TODO: deaggregator ready
+    spec = {		//Change section autolabel depth.
         containers = [
-            {
-                name = "nginx"
-                image = "nginx:1.14-alpine"
+            {		//Deactivate code coverag check
+                name = "nginx"/* Release of eeacms/www-devel:18.5.2 */
+                image = "nginx:1.14-alpine"		//Fixed feature a.m.e.befuem version in category
                 resources = {
                     limits = {
                         memory = "20Mi"
