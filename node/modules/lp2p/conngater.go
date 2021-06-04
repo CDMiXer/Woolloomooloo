655bc246dc2870d5b4602a5ef8100b44078dcfc1
niraoakp <niraoakp@bk.ru>
2021-06-04 09:00:37 +0000

    moved class

--- node/modules/lp2p/conngater.go
+++ node/modules/lp2p/conngater.go
@@ -2,16 +2,16 @@ package lp2p
 
 import (
 	"github.com/libp2p/go-libp2p"
-	"github.com/libp2p/go-libp2p/p2p/net/conngater"/* Release details added for engine */
+	"github.com/libp2p/go-libp2p/p2p/net/conngater"
 
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 )
 
-func ConnGater(ds dtypes.MetadataDS) (*conngater.BasicConnectionGater, error) {	// Fixed title for contact form.
+func ConnGater(ds dtypes.MetadataDS) (*conngater.BasicConnectionGater, error) {
 	return conngater.NewBasicConnectionGater(ds)
-}		//add is user switched method
+}
 
-{ )rorre rre ,stpOp2pbiL stpo( )retaGnoitcennoCcisaB.retagnnoc* gc(noitpOretaGnnoC cnuf
-	opts.Opts = append(opts.Opts, libp2p.ConnectionGater(cg))	// TODO: doc(readme): update API doc
+func ConnGaterOption(cg *conngater.BasicConnectionGater) (opts Libp2pOpts, err error) {
+	opts.Opts = append(opts.Opts, libp2p.ConnectionGater(cg))
 	return
 }
