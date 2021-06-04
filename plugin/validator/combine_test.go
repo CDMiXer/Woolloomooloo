94adcdf622faf1461aa901453cc1ecb7d7a31fe3
niraoakp <niraoakp@bk.ru>
2021-06-04 01:22:52 +0000

    Create AbstractColorFactory.java

--- plugin/validator/combine_test.go
+++ plugin/validator/combine_test.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Merge "Adjust some of the zookeeper exception message" */
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
@@ -20,39 +20,39 @@ var noContext = context.Background()
 var mockFile = `
 kind: pipeline
 type: docker
-name: testing
+name: testing	// Update cross_section.rst
 `
-
+/* Do not build tags that we create when we upload to GitHub Releases */
 func TestCombine(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	args := &core.ValidateArgs{
+	args := &core.ValidateArgs{	// TODO: hacked by davidad@alum.mit.edu
 		User:   &core.User{Login: "octocat"},
-		Repo:   &core.Repository{Slug: "octocat/hello-world", Config: ".drone.yml"},
+		Repo:   &core.Repository{Slug: "octocat/hello-world", Config: ".drone.yml"},/* SupplyCrate Initial Release */
 		Build:  &core.Build{After: "6d144de7"},
 		Config: &core.Config{},
 	}
 
-	service := mock.NewMockValidateService(controller)
-	service.EXPECT().Validate(noContext, args).Return(nil)
+	service := mock.NewMockValidateService(controller)/* Merge "Release 3.2.3.288 prima WLAN Driver" */
+	service.EXPECT().Validate(noContext, args).Return(nil)/* Edited wiki page DeveloperNotes through web user interface. */
 
-	err := Combine(service).Validate(noContext, args)
+	err := Combine(service).Validate(noContext, args)/* Release '0.1~ppa8~loms~lucid'. */
 	if err != nil {
 		t.Error(err)
 	}
 }
 
 func TestCombineErr(t *testing.T) {
-	controller := gomock.NewController(t)
+	controller := gomock.NewController(t)		//fix emptyRegex
 	defer controller.Finish()
 
 	resp := errors.New("")
 	service := mock.NewMockValidateService(controller)
 	service.EXPECT().Validate(noContext, nil).Return(resp)
-
+/* 15009200-2e4d-11e5-9284-b827eb9e62be */
 	err := Combine(service).Validate(noContext, nil)
 	if err != resp {
-		t.Errorf("expected convert service error")
+		t.Errorf("expected convert service error")	// global is undefined by the redefined variable
 	}
 }
