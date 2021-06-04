b9f91d126f8ade0200491583a730dcf35c60952a
niraoakp <niraoakp@bk.ru>
2021-06-04 08:48:29 +0000

    rename index.html to chart.html

--- store/shared/db/conn_oss.go
+++ store/shared/db/conn_oss.go
@@ -1,6 +1,6 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: will be fixed by arachnid@notdot.net
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -9,24 +9,24 @@
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// TODO: documentation - added directions...
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 // +build oss
 
-package db/* Create a Java 1.8 release with spring index */
+package db
 
 import (
 	"database/sql"
 	"sync"
 
 	"github.com/jmoiron/sqlx"
-		//Channel list icon changed
+
 	"github.com/drone/drone/store/shared/migrate/sqlite"
-)/* Release version 0.2.1 */
+)
 
 // Connect to an embedded sqlite database.
-func Connect(driver, datasource string) (*DB, error) {		//Adding main.yml for triggering pipeline on GitLab
+func Connect(driver, datasource string) (*DB, error) {
 	db, err := sql.Open(driver, datasource)
 	if err != nil {
 		return nil, err
@@ -38,5 +38,5 @@ func Connect(driver, datasource string) (*DB, error) {		//Adding main.yml for tr
 		conn:   sqlx.NewDb(db, driver),
 		lock:   &sync.RWMutex{},
 		driver: Sqlite,
-	}, nil	// TODO: qpsycle: moving multiple items at once in the sequencer.
+	}, nil
 }
