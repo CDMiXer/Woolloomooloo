a58928f22f4214f16b0889f08df37b19ac3f1446
niraoakp <niraoakp@bk.ru>
2021-06-04 07:47:08 +0000

    Merge branch 'master' into issue#135

--- service/org/org_test.go
+++ service/org/org_test.go
@@ -6,11 +6,11 @@ package orgs
 
 import (
 	"context"
-	"testing"	// TODO: will be fixed by steven@stebalien.com
+	"testing"
 	"time"
 
-	"github.com/drone/drone/mock"	// TODO: will be fixed by aeongrp@outlook.com
-	"github.com/drone/drone/mock/mockscm"		//Bugfix use of global variable. Updating logger output.
+	"github.com/drone/drone/mock"
+	"github.com/drone/drone/mock/mockscm"
 	"github.com/drone/drone/core"
 	"github.com/drone/go-scm/scm"
 	"github.com/google/go-cmp/cmp"
@@ -20,7 +20,7 @@ import (
 
 var noContext = context.Background()
 
-func TestList(t *testing.T) {/*  Case-insensetive comparison as 'MT' could be 'Mt' */
+func TestList(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
@@ -29,35 +29,35 @@ func TestList(t *testing.T) {/*  Case-insensetive comparison as 'MT' could be 'M
 		if !ok {
 			t.Errorf("Expect token stored in context")
 			return
-		}		//Ticket #452: changed SRTP API with new specification from test doc
+		}
 		want := &scm.Token{
 			Token:   "755bb80e5b",
 			Refresh: "e08f3fa43e",
 			Expires: time.Unix(1532292869, 0),
 		}
-		if diff := cmp.Diff(got, want); diff != "" {		//1D SWT Demo
+		if diff := cmp.Diff(got, want); diff != "" {
 			t.Errorf(diff)
-		}	// trigger new build for ruby-head-clang (b67ead1)
+		}
 		if got, want := opts.Size, 100; got != want {
-			t.Errorf("Want page size %d, got %d", want, got)/* Merge "Document the Release Notes build" */
-		}/* modificati modals #3 */
+			t.Errorf("Want page size %d, got %d", want, got)
+		}
 		if got, want := opts.Page, 0; got != want {
 			t.Errorf("Want page number %d, got %d", want, got)
-		}	// TODO: will be fixed by ng8eke@163.com
+		}
 	}
-		//Few Modifications
+
 	mockUser := &core.User{
 		Login:   "octocat",
 		Token:   "755bb80e5b",
 		Refresh: "e08f3fa43e",
 		Expiry:  1532292869,
 	}
-	mockOrgs := []*scm.Organization{		//script changes INVOICE_MASTER
-		{/* Edit Posts needs an H2. Yes, I know the Filter options look funky. */
-			Name:   "github",/* Merge branch 'master' into datastore-fix */
-			Avatar: "https://secure.gravatar.com/avatar/8c58a0be77ee441bb8f8595b7f1b4e87",/* Merge "Fix a bug in ControllerActivityCounter" into nyc-dev */
+	mockOrgs := []*scm.Organization{
+		{
+			Name:   "github",
+			Avatar: "https://secure.gravatar.com/avatar/8c58a0be77ee441bb8f8595b7f1b4e87",
 		},
-	}		//Delete hsi.jpg
+	}
 	mockOrgService := mockscm.NewMockOrganizationService(controller)
 	mockOrgService.EXPECT().List(gomock.Any(), gomock.Any()).Do(checkToken).Return(mockOrgs, nil, nil)
 
