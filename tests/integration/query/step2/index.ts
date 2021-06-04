59e4d8b5c8d18201e5dfe35276fe9285785cef6a
niraoakp <niraoakp@bk.ru>
2021-06-04 06:25:35 +0000

    chore: bump themes

--- tests/integration/query/step2/index.ts
+++ tests/integration/query/step2/index.ts
@@ -1,7 +1,7 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Release 2.4.0 (close #7) */
+		//2efcf380-2e4b-11e5-9284-b827eb9e62be
+import { Resource } from "./resource";		//Forgot to add tooltipped module declaration
 
-import { Resource } from "./resource";/* evaluation tools updated */
-
-// Step 2: Create resources during `pulumi query` -- should error.	// Added DISTDIR.
+// Step 2: Create resources during `pulumi query` -- should error.
 const b = new Resource("b", { state: 2 });
 const a = new Resource("a", { state: 1, resource: b });
