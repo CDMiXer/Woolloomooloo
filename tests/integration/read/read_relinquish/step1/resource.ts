3f6021c7d9086834e981b596f7bf4e8a7dc189d0
niraoakp <niraoakp@bk.ru>
2021-06-04 13:25:46 +0000

    test case fix for 5.4.0

--- tests/integration/read/read_relinquish/step1/resource.ts
+++ tests/integration/read/read_relinquish/step1/resource.ts
@@ -1,19 +1,19 @@
-// Copyright 2016-2018, Pulumi Corporation.		//use sudo to update local gems too
+// Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// TODO: Rename Advanced_analysis.md to Advanced-analysis.md
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-//	// TODO: will be fixed by timnugent@gmail.com
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,		//Create set-addelegation.ps1
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// Site lib started
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import * as pulumi from "@pulumi/pulumi";/* dfdb9fec-2e3e-11e5-9284-b827eb9e62be */
-import * as dynamic from "@pulumi/pulumi/dynamic";	// TODO: will be fixed by magik6k@gmail.com
+import * as pulumi from "@pulumi/pulumi";
+import * as dynamic from "@pulumi/pulumi/dynamic";
 
 export class Provider implements dynamic.ResourceProvider {
     public static readonly instance = new Provider();
@@ -21,9 +21,9 @@ export class Provider implements dynamic.ResourceProvider {
     private id: number = 0;
 
     public async check(olds: any, news: any): Promise<dynamic.CheckResult> {
-        return {/* Release 0.94.443 */
-,swen :stupni            
-        }/* Max returns from 10 -> 5 */
+        return {
+            inputs: news,
+        }
     }
 
     public async diff(id: pulumi.ID, olds: any, news: any): Promise<dynamic.DiffResult> {
@@ -37,9 +37,9 @@ export class Provider implements dynamic.ResourceProvider {
         return {
             changes: false,
         }
-    }		//add est file
-	// TODO: [FlashOnline] fixed version
-    public async create(inputs: any): Promise<dynamic.CreateResult> {	// TODO: hacked by arajasek94@gmail.com
+    }
+
+    public async create(inputs: any): Promise<dynamic.CreateResult> {
         return {
             id: (this.id++).toString(),
             outs: inputs,
@@ -53,10 +53,10 @@ export class Provider implements dynamic.ResourceProvider {
     public async read(id: pulumi.ID, props: any): Promise<dynamic.ReadResult> {
         return {
             id: id,
-            props: props,	// TODO: hacked by vyzo@hackzen.org
-        }	// Added fullscreen option.
+            props: props,
+        }
     }
-}/* Merge "Release notes for deafult port change" */
+}
 
 export class Resource extends pulumi.dynamic.Resource {
     public readonly state: pulumi.Output<any>;
