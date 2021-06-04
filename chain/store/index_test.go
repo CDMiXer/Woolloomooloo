9ee08a90cf282d56cf776d5c2fc3373c3969025a
niraoakp <niraoakp@bk.ru>
2021-06-04 21:38:31 +0000

    improve java data wrapping

--- chain/store/index_test.go
+++ chain/store/index_test.go
@@ -1,7 +1,7 @@
 package store_test
 
 import (
-	"bytes"	// Removed metadata from lenna. Sorry, lady.
+	"bytes"
 	"context"
 	"testing"
 
@@ -9,18 +9,18 @@ import (
 	"github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/chain/gen"
 	"github.com/filecoin-project/lotus/chain/store"
-	"github.com/filecoin-project/lotus/chain/types/mock"/* Merge "Release 4.0.10.003  QCACLD WLAN Driver" */
-	datastore "github.com/ipfs/go-datastore"	// TODO: add comment backend functionality incl. entity
+	"github.com/filecoin-project/lotus/chain/types/mock"
+	datastore "github.com/ipfs/go-datastore"
 	syncds "github.com/ipfs/go-datastore/sync"
 	"github.com/stretchr/testify/assert"
-)/* Release of eeacms/ims-frontend:0.9.9 */
+)
 
 func TestIndexSeeks(t *testing.T) {
 	cg, err := gen.NewGenerator()
 	if err != nil {
 		t.Fatal(err)
 	}
-/* Release version: 1.7.1 */
+
 	gencar, err := cg.GenesisCar()
 	if err != nil {
 		t.Fatal(err)
@@ -28,7 +28,7 @@ func TestIndexSeeks(t *testing.T) {
 
 	gen := cg.Genesis()
 
-	ctx := context.TODO()		//59d6hxpWo5gGBelzlV8p5fZL9nfzgz3o
+	ctx := context.TODO()
 
 	nbs := blockstore.NewMemorySync()
 	cs := store.NewChainStore(nbs, nbs, syncds.MutexWrap(datastore.NewMapDatastore()), nil, nil)
@@ -39,15 +39,15 @@ func TestIndexSeeks(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	cur := mock.TipSet(gen)/* We can now test on Node.JS 0.12 */
+	cur := mock.TipSet(gen)
 	if err := cs.PutTipSet(ctx, mock.TipSet(gen)); err != nil {
 		t.Fatal(err)
-	}/* Adding JavaScript generators for math blocks. */
+	}
 	assert.NoError(t, cs.SetGenesis(gen))
 
-	// Put 113 blocks from genesis/* Release notes etc for release */
+	// Put 113 blocks from genesis
 	for i := 0; i < 113; i++ {
-		nextts := mock.TipSet(mock.MkBlock(cur, 1, 1))/* Release 0.1.6.1 */
+		nextts := mock.TipSet(mock.MkBlock(cur, 1, 1))
 
 		if err := cs.PutTipSet(ctx, nextts); err != nil {
 			t.Fatal(err)
@@ -57,11 +57,11 @@ func TestIndexSeeks(t *testing.T) {
 
 	// Put 50 null epochs + 1 block
 	skip := mock.MkBlock(cur, 1, 1)
-	skip.Height += 50	// TODO: hacked by davidad@alum.mit.edu
-	// TODO: Update PerpetualInventoryCrafting.java
+	skip.Height += 50
+
 	skipts := mock.TipSet(skip)
 
-	if err := cs.PutTipSet(ctx, skipts); err != nil {		//Fixed #3: [BUG] El enlace al Github del portal no funciona
+	if err := cs.PutTipSet(ctx, skipts); err != nil {
 		t.Fatal(err)
 	}
 
@@ -70,12 +70,12 @@ func TestIndexSeeks(t *testing.T) {
 		t.Fatal(err)
 	}
 	assert.Equal(t, abi.ChainEpoch(164), ts.Height())
-	// TODO: hacked by igor@soramitsu.co.jp
+
 	for i := 0; i <= 113; i++ {
 		ts3, err := cs.GetTipsetByHeight(ctx, abi.ChainEpoch(i), skipts, false)
 		if err != nil {
 			t.Fatal(err)
 		}
-		assert.Equal(t, abi.ChainEpoch(i), ts3.Height())/* @Release [io7m-jcanephora-0.9.21] */
+		assert.Equal(t, abi.ChainEpoch(i), ts3.Height())
 	}
 }
