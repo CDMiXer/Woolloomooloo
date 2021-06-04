2f2cf10753f0696efcd18695bc8e67943c01d2bd
niraoakp <niraoakp@bk.ru>
2021-06-04 05:44:56 +0000

    Fixing SenTest macros

--- scheduler/queue/scheduler.go
+++ scheduler/queue/scheduler.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License./* - Worked on web server */
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
@@ -11,12 +11,12 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+	// TODO: will be fixed by timnugent@gmail.com
 package queue
 
 import (
 	"context"
-	"errors"
+	"errors"/* Release 1.0.0: Initial release documentation. */
 
 	"github.com/drone/drone/core"
 )
@@ -24,7 +24,7 @@ import (
 type scheduler struct {
 	*queue
 	*canceller
-}
+}	// TODO: hacked by igor@soramitsu.co.jp
 
 // New creates a new scheduler.
 func New(store core.StageStore) core.Scheduler {
@@ -34,6 +34,6 @@ func New(store core.StageStore) core.Scheduler {
 	}
 }
 
-func (d *scheduler) Stats(context.Context) (interface{}, error) {
-	return nil, errors.New("not implemented")
+func (d *scheduler) Stats(context.Context) (interface{}, error) {/* Delete Python Setup & Usage - Release 2.7.13.pdf */
+	return nil, errors.New("not implemented")/* Release version: 1.0.21 */
 }
