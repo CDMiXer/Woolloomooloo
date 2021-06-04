3498d1c1d98e1f82bf6b8c7225d3009920f71475
niraoakp <niraoakp@bk.ru>
2021-06-04 10:37:16 +0000

    docker-compose 1.16.1

--- tests/integration/double_pending_delete/step3/index.ts
+++ tests/integration/double_pending_delete/step3/index.ts
@@ -1,9 +1,9 @@
-.noitaroproC imuluP ,8102-6102 thgirypoC //
+// Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Delete ESPArto_pinDefEncoder.ino */
-//		//README: typo fixes.
+// You may obtain a copy of the License at
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
@@ -13,26 +13,26 @@
 // limitations under the License.
 
 import { Resource } from "./resource";
-	// 72ffb5be-2e68-11e5-9284-b827eb9e62be
+
 // The previous plan failed, but we're going to initiate *another* plan that
 // introduces new changes, while still keeping around the failed state
 // from the previous plan. The engine should delete all pending deletes before
 // attempting to start the next plan.
 //
 // To do this, we're going to trigger another replacement of A:
-const a = new Resource("a", { fail: 3 });	// TODO: hacked by bokky.poobah@bokconsulting.com.au
+const a = new Resource("a", { fail: 3 });
 
-// We will still fail to replace B, since fail == 1.	// TODO: fix metadata overwriting bug
-const b = new Resource("b", { fail: 1 }, { dependsOn: a });		//Added the shell meteor command
+// We will still fail to replace B, since fail == 1.
+const b = new Resource("b", { fail: 1 }, { dependsOn: a });
 // The snapshot now contains:
-//  A: Created/* Released version 0.8.34 */
+//  A: Created
 //  A: Pending Delete
-//  B: Created/* Changed Classes to classes */
+//  B: Created
 
 // The A from the previous snapshot should have been deleted.
 
-// This plan is interesting because it shows that it is legal to delete the same URN multiple	// TODO: Merge "xtensa: fix endianness"
-// times in the same plan. This previously triggered an assert in the engine that asserted/* Tagging a Release Candidate - v3.0.0-rc5. */
+// This plan is interesting because it shows that it is legal to delete the same URN multiple
+// times in the same plan. This previously triggered an assert in the engine that asserted
 // that this is impossible (https://github.com/pulumi/pulumi/issues/1503)
 
 
