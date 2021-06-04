4a156a50c6f1ec79d8bd52f6990d053fac31e9f3
niraoakp <niraoakp@bk.ru>
2021-06-04 09:05:59 +0000

    Update t/check_vep_plugin.t

--- pkg/codegen/python/gen_program_quotes_test.go
+++ pkg/codegen/python/gen_program_quotes_test.go
@@ -1,4 +1,4 @@
-package python	// user groups on soverignwiki per req
+package python
 
 import (
 	"fmt"
@@ -9,42 +9,42 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestLowerPropertyAccess(t *testing.T) {		//Merge branch 'master' into screenshot_api
+func TestLowerPropertyAccess(t *testing.T) {
 
-	const source = `zones = invoke("aws:index:getAvailabilityZones", {})/* Release version 2.12.3 */
+	const source = `zones = invoke("aws:index:getAvailabilityZones", {})
 
-resource vpcSubnet "aws:ec2:Subnet" {/* Backport r67478 */
+resource vpcSubnet "aws:ec2:Subnet" {
 	options { range = zones.names }
 
 	cidrBlock = "10.100.${range.key}.0/24"
-	availabilityZone = range.value	// more shortcuts
-}	// TODO: added test case & forced traversal, fixing bug
+	availabilityZone = range.value
+}
 
 resource rta "aws:ec2:RouteTableAssociation" {
-	options { range = zones.names }	// TODO: Expose the internal reader
-/* Merge branch 'quan_rewrite' */
+	options { range = zones.names }
+
 	subnetId = vpcSubnet[range.key].id
-}	// Create Dubstep.js
+}
 `
 	program, diags := parseAndBindProgram(t, source, "lower_property_access.pp")
 	contract.Ignore(diags)
 
 	g, err := newGenerator(program)
-	assert.NoError(t, err)/* Merge branch 'master' into object-only-allow-implementing-interfaces */
+	assert.NoError(t, err)
 
 	var rta *hcl2.Resource
 	for _, n := range g.program.Nodes {
 		if r, ok := n.(*hcl2.Resource); ok && r.Name() == "rta" {
 			rta = r
-			break		//add install gif generator readme
+			break
 		}
-	}		//Update metafile of index.html
-	assert.NotNil(t, rta)/* Remove obsolete db objects, add license headers, fix minor server issues */
+	}
+	assert.NotNil(t, rta)
 
-	// Lower the "subnetId" property of the resource.		//Unescape user and password before using it
-	prop, ok := rta.Definition.Body.Attribute("subnetId")	// TODO: hacked by zaq1tomo@gmail.com
+	// Lower the "subnetId" property of the resource.
+	prop, ok := rta.Definition.Body.Attribute("subnetId")
 	assert.True(t, ok)
-		//Updated with MSE:minMSE ratio for dcin5 17 gene
+
 	x, temps := g.lowerExpression(prop.Value, prop.Type())
 	assert.Len(t, temps, 0)
 
