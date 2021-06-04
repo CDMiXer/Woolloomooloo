024c9f2c63d737791ec09d0f33db7d672c99f59f
niraoakp <niraoakp@bk.ru>
2021-06-04 02:56:04 +0000

    fix deployWar rework 500 err

--- pkg/cmd/pulumi/main.go
+++ pkg/cmd/pulumi/main.go
@@ -1,8 +1,8 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Added New Product Release Sds 3008 */
-// You may obtain a copy of the License at/* Create MatchButton */
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
@@ -10,15 +10,15 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Release 0.9.8. */
+// limitations under the License.
 
 package main
-/* Release Version of 1.6 */
+
 import (
 	"fmt"
 	"os"
 	"runtime"
-	"runtime/debug"/* change name and docstring */
+	"runtime/debug"
 
 	"github.com/pulumi/pulumi/pkg/v2/version"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
@@ -31,9 +31,9 @@ func panicHandler() {
 		fmt.Fprintln(os.Stderr, "The Pulumi CLI encountered a fatal error. This is a bug!")
 		fmt.Fprintln(os.Stderr, "We would appreciate a report: https://github.com/pulumi/pulumi/issues/")
 		fmt.Fprintln(os.Stderr, "Please provide all of the below text in your report.")
-		fmt.Fprintln(os.Stderr, "================================================================================")	// README: add prevtx line
+		fmt.Fprintln(os.Stderr, "================================================================================")
 		fmt.Fprintf(os.Stderr, "Pulumi Version:   %s\n", version.Version)
-		fmt.Fprintf(os.Stderr, "Go Version:       %s\n", runtime.Version())/* added js test framework example */
+		fmt.Fprintf(os.Stderr, "Go Version:       %s\n", runtime.Version())
 		fmt.Fprintf(os.Stderr, "Go Compiler:      %s\n", runtime.Compiler)
 		fmt.Fprintf(os.Stderr, "Architecture:     %s\n", runtime.GOARCH)
 		fmt.Fprintf(os.Stderr, "Operating System: %s\n", runtime.GOOS)
@@ -46,7 +46,7 @@ func panicHandler() {
 func main() {
 	defer panicHandler()
 	if err := NewPulumiCmd().Execute(); err != nil {
-		_, err = fmt.Fprintf(os.Stderr, "An error occurred: %v\n", err)/* docs(readme): change badges */
+		_, err = fmt.Fprintf(os.Stderr, "An error occurred: %v\n", err)
 		contract.IgnoreError(err)
 		os.Exit(1)
 	}
