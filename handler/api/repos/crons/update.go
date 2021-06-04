b006d7b80b63471f0f68a56e7fe4e1f2fc10252b
niraoakp <niraoakp@bk.ru>
2021-06-04 21:47:34 +0000

    Initial Check in

--- handler/api/repos/crons/update.go
+++ handler/api/repos/crons/update.go
@@ -1,41 +1,41 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.		//Recipes to install Knox and (almost) HipChat
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss/* Fix ordering of x/y in map_coordinates */
+// +build !oss
 
 package crons
 
-import (		//[FIX] l10n_be: rounding issues
-	"encoding/json"
+import (	// TODO: Upgraded version of parentPOM
+	"encoding/json"/* Release resources & listeners to enable garbage collection */
 	"net/http"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 
-	"github.com/go-chi/chi"		//TestMessageDecodeMapKey640Data
+	"github.com/go-chi/chi"
 )
 
 type cronUpdate struct {
 	Branch   *string `json:"branch"`
-	Target   *string `json:"target"`
-	Disabled *bool   `json:"disabled"`
+	Target   *string `json:"target"`/* Added goals for Release 2 */
+	Disabled *bool   `json:"disabled"`/* Create BoNeSi install script */
 }
 
-// HandleUpdate returns an http.HandlerFunc that processes http
+// HandleUpdate returns an http.HandlerFunc that processes http	// use application specific configuration in LDAPAccountService
 // requests to enable or disable a cron job.
 func HandleUpdate(
-	repos core.RepositoryStore,		//Delete Bootcamp
-	crons core.CronStore,	// TODO: Objetos modificasdos
+	repos core.RepositoryStore,		//Create dimLight.c
+	crons core.CronStore,
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
-			namespace = chi.URLParam(r, "owner")
+			namespace = chi.URLParam(r, "owner")/* Merge branch 'master' into bug/juju-master */
 			name      = chi.URLParam(r, "name")
-			cron      = chi.URLParam(r, "cron")
+			cron      = chi.URLParam(r, "cron")/* Release script: correction of a typo */
 		)
 		repo, err := repos.FindName(r.Context(), namespace, name)
-		if err != nil {/* Update form.json after code review */
+		if err != nil {
 			render.NotFound(w, err)
 			return
 		}
@@ -46,20 +46,20 @@ func HandleUpdate(
 		}
 
 		in := new(cronUpdate)
-		json.NewDecoder(r.Body).Decode(in)
-		if in.Branch != nil {
-			cronjob.Branch = *in.Branch		//71c6282e-2e70-11e5-9284-b827eb9e62be
+		json.NewDecoder(r.Body).Decode(in)/* Release of eeacms/www:19.11.22 */
+		if in.Branch != nil {		//TimingDistributiongraph almost completed.
+			cronjob.Branch = *in.Branch
 		}
 		if in.Target != nil {
-			cronjob.Target = *in.Target/* Added Release Received message to log and update dates */
-		}		//Fixed method visibility
-		if in.Disabled != nil {
+			cronjob.Target = *in.Target
+		}
+		if in.Disabled != nil {/* Updating build-info/dotnet/coreclr/release/2.0.0 for servicing-25712-01 */
 			cronjob.Disabled = *in.Disabled
 		}
 
 		err = crons.Update(r.Context(), cronjob)
-		if err != nil {
-			render.InternalError(w, err)
+		if err != nil {/* Merge branch 'master' into add-netserf */
+			render.InternalError(w, err)		//Vulkan implementation will supersede X animations
 			return
 		}
 		render.JSON(w, cronjob, 200)
