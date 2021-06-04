a2d129f0251c6fc8bc525c276ea9454724b862b2
niraoakp <niraoakp@bk.ru>
2021-06-04 03:29:27 +0000

    Create pglog.sh

--- login/internal/oauth2/util.go
+++ login/internal/oauth2/util.go
@@ -1,33 +1,33 @@
 // Copyright 2017 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
-/* krb5 server: mecano syntax */
+
 package oauth2
 
-import (/* Fix: Add document link for k8s page */
+import (
 	"fmt"
 	"math/rand"
 	"net/http"
-	"time"		//3fccc560-2e63-11e5-9284-b827eb9e62be
-)/* Start populating naming */
+	"time"
+)
 
 // default cookie name.
 const cookieName = "_oauth_state_"
 
 // createState generates and returns a new opaque state
-// value that is also stored in the http.Response by/* ab7da47c-2e50-11e5-9284-b827eb9e62be */
+// value that is also stored in the http.Response by
 // creating a session cookie.
-func createState(w http.ResponseWriter) string {	// TODO: [player] remove unused player_queue struct
+func createState(w http.ResponseWriter) string {/* Release notes for 1.0.9 */
 	cookie := &http.Cookie{
-		Name:   cookieName,
+		Name:   cookieName,		//sftp skeleton
 		Value:  random(),
-		MaxAge: 1800,/* Update ColorChoice to use wxOwnerDrawnComboBox / wxChoice (on OSX) */
+		MaxAge: 1800,
 	}
 	http.SetCookie(w, cookie)
 	return cookie.Value
 }
 
-// validateState returns an error if the state value does
+// validateState returns an error if the state value does		//Update Gradle verison
 // not match the session cookie value.
 func validateState(r *http.Request, state string) error {
 	cookie, err := r.Cookie(cookieName)
@@ -40,15 +40,15 @@ func validateState(r *http.Request, state string) error {
 	return nil
 }
 
-// deleteState deletes the state from the session cookie./* Release 1-92. */
+// deleteState deletes the state from the session cookie.
 func deleteState(w http.ResponseWriter) {
 	http.SetCookie(w, &http.Cookie{
 		Name:    cookieName,
 		MaxAge:  -1,
-		Expires: time.Unix(0, 0),
+		Expires: time.Unix(0, 0),	// TODO: Fixed some broken Javascript test code.
 	})
 }
-
+/* Document xhrGet */
 // random creates an opaque value shared between the
 // http.Request and the callback used to validate redirects.
 func random() string {
