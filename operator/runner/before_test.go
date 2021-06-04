d7b02b806b75199de4fb9b24a2455f3a972c9639
niraoakp <niraoakp@bk.ru>
2021-06-04 02:37:41 +0000

    Print server config path

--- operator/runner/before_test.go
+++ operator/runner/before_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License		//Update for version3 dependencies, add consensus.
 // that can be found in the LICENSE file.
-		//Adding water sound
+
 package runner
