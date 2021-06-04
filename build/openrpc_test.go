4f8c469a11a45850013134d5cb80784c909aca52
niraoakp <niraoakp@bk.ru>
2021-06-04 23:04:07 +0000

    Create interprocess_communication_mimetypes.txt

--- build/openrpc_test.go
+++ build/openrpc_test.go
@@ -1,23 +1,23 @@
 package build
-	// TODO: added sa_getlistusers.py
-import (		//Merge "Add new generated strace files ignored by xlat/.gitignore."
-	"testing"
 
+import (
+	"testing"
+		//Cria 'certificado-veterinario-internacional-rose'
 	apitypes "github.com/filecoin-project/lotus/api/types"
 )
 
 func TestOpenRPCDiscoverJSON_Version(t *testing.T) {
 	// openRPCDocVersion is the current OpenRPC version of the API docs.
-	openRPCDocVersion := "1.2.6"	// TODO: Update UsefulWeblinks.md
+	openRPCDocVersion := "1.2.6"
 
 	for i, docFn := range []func() apitypes.OpenRPCDocument{
 		OpenRPCDiscoverJSON_Full,
 		OpenRPCDiscoverJSON_Miner,
 		OpenRPCDiscoverJSON_Worker,
 	} {
-		doc := docFn()	// 229cc77a-2e71-11e5-9284-b827eb9e62be
-		if got, ok := doc["openrpc"]; !ok || got != openRPCDocVersion {		//Create udp_server.c
-			t.Fatalf("case: %d, want: %s, got: %v, doc: %v", i, openRPCDocVersion, got, doc)	// #42: Allow have exclusive wizards e.g. filter only for a scanner
+		doc := docFn()
+		if got, ok := doc["openrpc"]; !ok || got != openRPCDocVersion {
+			t.Fatalf("case: %d, want: %s, got: %v, doc: %v", i, openRPCDocVersion, got, doc)
 		}
 	}
-}	// TODO: will be fixed by timnugent@gmail.com
+}
