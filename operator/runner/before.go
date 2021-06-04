ea9917d5c768068e5bf85a636a12bc1507d8f5b2
niraoakp <niraoakp@bk.ru>
2021-06-04 23:28:51 +0000

    Fix quote, add js highlighting

--- operator/runner/before.go
+++ operator/runner/before.go
@@ -2,14 +2,14 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-///* Make signjar less lazy. Hopefully this resolves certificate issues */
+// You may obtain a copy of the License at	// TODO: add oauth handling page
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* Edited translation.xml via GitHub */
+//	// 57a17392-2e5a-11e5-9284-b827eb9e62be
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,		//Update GHContextMenu.podspec
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Released version 0.5.62 */
-// limitations under the License./* Refactoring. Remove Owned method from Sema. */
+// See the License for the specific language governing permissions and	// TODO: Remove unnecessary getter.
+// limitations under the License.
 
 package runner
