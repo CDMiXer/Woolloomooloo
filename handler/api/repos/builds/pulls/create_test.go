3ea59d1762f39b2172df0d7377491a67dd29c8bb
niraoakp <niraoakp@bk.ru>
2021-06-04 17:19:15 +0000

    Merge branch 'master' into 7913-allowUICancelTask

--- handler/api/repos/builds/pulls/create_test.go
+++ handler/api/repos/builds/pulls/create_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Fixed reflection tracing. */
-// that can be found in the LICENSE file.		//Merge "README.md file for auth library"
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: will be fixed by hugomrdias@gmail.com
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.
 
-package pulls		//33f645a4-2e4f-11e5-9284-b827eb9e62be
+package pulls
