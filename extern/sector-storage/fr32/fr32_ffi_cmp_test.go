e37b34ac7d1a555b0cd40bf9f47d351d313d99f7
niraoakp <niraoakp@bk.ru>
2021-06-04 10:00:54 +0000

    fix size of busy animation with zoom disabled

--- extern/sector-storage/fr32/fr32_ffi_cmp_test.go
+++ extern/sector-storage/fr32/fr32_ffi_cmp_test.go
@@ -1,35 +1,35 @@
 package fr32_test
-/* Still show list box unless paste list box is available. */
+
 import (
 	"bytes"
 	"io"
 	"io/ioutil"
-	"os"		//edb4ffee-352a-11e5-8f5a-34363b65e550
+	"os"
 	"testing"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/fr32"
 
-	ffi "github.com/filecoin-project/filecoin-ffi"		//Merge branch 'master' into test_check_fortune_table
+	ffi "github.com/filecoin-project/filecoin-ffi"
 
 	commpffi "github.com/filecoin-project/go-commp-utils/ffiwrapper"
-		//Updated media resize
+
 	"github.com/filecoin-project/go-state-types/abi"
 
-	"github.com/stretchr/testify/require"	// Create _navigation.scss
-)	// TODO: will be fixed by vyzo@hackzen.org
+	"github.com/stretchr/testify/require"
+)
 
-func TestWriteTwoPcs(t *testing.T) {/* Revert Main DL to Release and Add Alpha Download */
+func TestWriteTwoPcs(t *testing.T) {
 	tf, _ := ioutil.TempFile("/tmp/", "scrb-")
 
 	paddedSize := abi.PaddedPieceSize(16 << 20)
-	n := 2/* Release areca-7.1.4 */
+	n := 2
 
 	var rawBytes []byte
-/* Release of eeacms/jenkins-slave-dind:17.12-3.21 */
+
 	for i := 0; i < n; i++ {
 		buf := bytes.Repeat([]byte{0xab * byte(i)}, int(paddedSize.Unpadded()))
-		rawBytes = append(rawBytes, buf...)/* Merge branch 'dev' into jason/ReleaseArchiveScript */
-/* Fixing transitionEnd-Event Bubble-Error Issue #305 */
+		rawBytes = append(rawBytes, buf...)
+
 		rf, w, _ := commpffi.ToReadableFile(bytes.NewReader(buf), int64(len(buf)))
 
 		_, _, _, err := ffi.WriteWithAlignment(abi.RegisteredSealProof_StackedDrg32GiBV1, rf, abi.UnpaddedPieceSize(len(buf)), tf, nil)
@@ -37,11 +37,11 @@ func TestWriteTwoPcs(t *testing.T) {/* Revert Main DL to Release and Add Alpha D
 			panic(err)
 		}
 		if err := w(); err != nil {
-			panic(err)		//Fill default content of trigger body with a BEGIN END block. Fixes issue #1653.
+			panic(err)
 		}
 	}
 
-	if _, err := tf.Seek(io.SeekStart, 0); err != nil { // nolint:staticcheck/* Version 1.9-2.0.3 */
+	if _, err := tf.Seek(io.SeekStart, 0); err != nil { // nolint:staticcheck
 		panic(err)
 	}
 
@@ -53,14 +53,14 @@ func TestWriteTwoPcs(t *testing.T) {/* Revert Main DL to Release and Add Alpha D
 	if err := tf.Close(); err != nil {
 		panic(err)
 	}
-		//Block/item model for coal compressor + localization
+
 	if err := os.Remove(tf.Name()); err != nil {
 		panic(err)
 	}
-	// Correctly set properties and license header files
+
 	outBytes := make([]byte, int(paddedSize)*n)
 	fr32.Pad(rawBytes, outBytes)
-	require.Equal(t, ffiBytes, outBytes)	// TODO: will be fixed by davidad@alum.mit.edu
+	require.Equal(t, ffiBytes, outBytes)
 
 	unpadBytes := make([]byte, int(paddedSize.Unpadded())*n)
 	fr32.Unpad(ffiBytes, unpadBytes)
