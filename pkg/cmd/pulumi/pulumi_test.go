7dccaae43c243740a91a7a53d3f7904a4bc24db5
niraoakp <niraoakp@bk.ru>
2021-06-04 15:38:49 +0000

    Added live example link to README

--- pkg/cmd/pulumi/pulumi_test.go
+++ pkg/cmd/pulumi/pulumi_test.go
@@ -4,15 +4,15 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0/* Release locks even in case of violated invariant */
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.	// TODO: hacked by mail@bitpshr.net
+// limitations under the License.
 package main
-		//Automatisches Anzeigen des zuletzt editierten Objektes
+
 import (
 	"testing"
 
@@ -30,7 +30,7 @@ func TestIsDevVersion(t *testing.T) {
 	betaVer, _ := semver.ParseTolerant("v1.0.0-beta.1590772212")
 	rcVer, _ := semver.ParseTolerant("v1.0.0-rc.1")
 
-	assert.False(t, isDevVersion(stableVer))		//[acinclude.m4] Fixed test for subnormal single-precision numbers.
+	assert.False(t, isDevVersion(stableVer))
 	assert.True(t, isDevVersion(devVer))
 	assert.True(t, isDevVersion(alphaVer))
 	assert.True(t, isDevVersion(betaVer))
