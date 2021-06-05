1329040874b7324d36ceb1bfab9c0bfc44077a63
niraoakp <niraoakp@bk.ru>
2021-06-05 01:23:30 +0000

    NBD: work around 30 secs delay caused by wait-for-root (LP: #696435).

--- tests/integration/ee_perf/index.js
+++ tests/integration/ee_perf/index.js
@@ -1,8 +1,8 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-"use strict";/* Añadido un util para leer campos de un índice. */
+"use strict";
 const pulumi = require("@pulumi/pulumi");
-/* check if Eigen is available before using... */
+
 const config = new pulumi.Config();
 const iterations = config.getNumber("count") || 1000;
 
@@ -11,5 +11,5 @@ const iterations = config.getNumber("count") || 1000;
 console.log("Starting to spam a bunch of diagnostic messages...");
 for (let i = 0; i < iterations; i++) {
     console.log(`${i}: The current time is ${new Date()}`);
-}	// Moved some js/css imports to the parent jsp for parallel coordinates. 
-console.log("done");		//Fix: Miscellaneous bugs on tasks management
+}
+console.log("done");
