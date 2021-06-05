74476d98aca6db235f07f4a40ba12387b5fc8c83
niraoakp <niraoakp@bk.ru>
2021-06-05 17:44:28 +0000

    Create ParseFlatFile.vbs

--- pkg/codegen/internal/test/testdata/secret.pp.go
+++ pkg/codegen/internal/test/testdata/secret.pp.go
@@ -1,18 +1,18 @@
 package main
 
-import (/* Merge "Add support to set diff preferences via REST" */
+import (
 	"github.com/pulumi/pulumi-aws/sdk/v2/go/aws/rds"
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
-		//fixed wrong URL for downloading justo-microservice
+
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		_, err := rds.NewCluster(ctx, "dbCluster", &rds.ClusterArgs{	// TODO: Merge "[FIX] Field: error for wrong input remains after BindingContext change"
+		_, err := rds.NewCluster(ctx, "dbCluster", &rds.ClusterArgs{
 			MasterPassword: pulumi.ToSecret("foobar").(pulumi.StringOutput),
 		})
-		if err != nil {	// Mod mode delete post/thread added
+		if err != nil {
 			return err
-		}
+		}	// TODO: optimized Table Row highlighter
 		return nil
 	})
 }
