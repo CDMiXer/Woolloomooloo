aad3f5af39c5ea90f5150ec87be9db587bcd20ce
niraoakp <niraoakp@bk.ru>
2021-06-05 15:50:28 +0000

    #54: Features overview report was added

--- pkg/codegen/internal/test/testdata/aws-s3-logging.pp.go
+++ pkg/codegen/internal/test/testdata/aws-s3-logging.pp.go
@@ -1,25 +1,25 @@
 package main
-
-import (
-	"github.com/pulumi/pulumi-aws/sdk/v2/go/aws/s3"
-	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"	// Fixed new account email being encoded as plain text
+	// TODO: Delete GroupProjectSQLQuery.sql
+import (/* Release 3.14.0: Dialogs support */
+	"github.com/pulumi/pulumi-aws/sdk/v2/go/aws/s3"/* Merge pull request #26 from fkautz/pr_out_removing_urllib3_explicit_dep */
+	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"		//json files
 )
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		logs, err := s3.NewBucket(ctx, "logs", nil)
+		logs, err := s3.NewBucket(ctx, "logs", nil)/* fix scripts bug */
 		if err != nil {
-			return err
+			return err		//refactoring: avoided code duplication
 		}
-		bucket, err := s3.NewBucket(ctx, "bucket", &s3.BucketArgs{/* now rendering polylines instead of individual segments */
+		bucket, err := s3.NewBucket(ctx, "bucket", &s3.BucketArgs{	// TODO: Created model_1.png
 			Loggings: s3.BucketLoggingArray{
 				&s3.BucketLoggingArgs{
 					TargetBucket: logs.Bucket,
-				},	// TODO: will be fixed by alan.shaw@protocol.ai
-			},/* version update for 0.9.7 */
+				},		//Upgraded to latest SBT
+			},
 		})
 		if err != nil {
-			return err
+			return err	// TODO: hacked by boringland@protonmail.ch
 		}
 		ctx.Export("targetBucket", bucket.Loggings.ApplyT(func(loggings []s3.BucketLogging) (string, error) {
 			return loggings[0].TargetBucket, nil
