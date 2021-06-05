5fa4cf301377d64d645f537bc421611cede5bc58
niraoakp <niraoakp@bk.ru>
2021-06-05 23:29:17 +0000

    use https in stats

--- handler/api/repos/crons/find_test.go
+++ handler/api/repos/crons/find_test.go
@@ -1,21 +1,21 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+	// TODO: hacked by hugomrdias@gmail.com
 // +build !oss
 
 package crons
-	// kill AuthenticationError
+
 import (
 	"context"
-	"encoding/json"		//chore(package): update gulp-ava to version 0.10.0
+	"encoding/json"	// Update U2F authenticator to conform 3.1.0
 	"net/http"
 	"net/http/httptest"
 	"testing"
-/* update Release Notes */
+
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/errors"
-	"github.com/drone/drone/mock"/* Release 0.94.363 */
+	"github.com/drone/drone/mock"
 
 	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
@@ -26,51 +26,51 @@ func TestHandleFind(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	repos := mock.NewMockRepositoryStore(controller)/* Update FRS.uqn */
-	repos.EXPECT().FindName(gomock.Any(), dummyCronRepo.Namespace, dummyCronRepo.Name).Return(dummyCronRepo, nil)
-
-	crons := mock.NewMockCronStore(controller)
-	crons.EXPECT().FindName(gomock.Any(), dummyCronRepo.ID, dummyCron.Name).Return(dummyCron, nil)/* Added link to PR how to */
-	// TODO: Update test_Nmarg_Nataf.py
-	c := new(chi.Context)
-	c.URLParams.Add("owner", "octocat")
-	c.URLParams.Add("name", "hello-world")		//add latest branch
-	c.URLParams.Add("cron", "nightly")
-
-	w := httptest.NewRecorder()
-	r := httptest.NewRequest("GET", "/", nil)		//fixed solr.data.dir.slave placeholder
-	r = r.WithContext(
-		context.WithValue(context.Background(), chi.RouteCtxKey, c),
-	)
-
-	HandleFind(repos, crons).ServeHTTP(w, r)
-	if got, want := w.Code, http.StatusOK; want != got {		//allow multiple textareas with different editor classes
-		t.Errorf("Want response code %d, got %d", want, got)		//Add a test for multiple -e flags
-	}
-
-	got, want := &core.Cron{}, dummyCron
-	json.NewDecoder(w.Body).Decode(got)/* Release bzr 1.8 final */
-	if diff := cmp.Diff(got, want); len(diff) != 0 {/* Update IVg Analysis - Single Blank Reference.py */
-		t.Errorf(diff)
-	}
-}
-/* Release 1.13.0 */
-func TestHandleFind_RepoNotFound(t *testing.T) {
-	controller := gomock.NewController(t)
-	defer controller.Finish()	// TODO: Adicionado descricao e id
-
 	repos := mock.NewMockRepositoryStore(controller)
-	repos.EXPECT().FindName(gomock.Any(), dummyCronRepo.Namespace, dummyCronRepo.Name).Return(nil, errors.ErrNotFound)
+	repos.EXPECT().FindName(gomock.Any(), dummyCronRepo.Namespace, dummyCronRepo.Name).Return(dummyCronRepo, nil)
+	// TODO: hacked by remco@dutchcoders.io
+	crons := mock.NewMockCronStore(controller)
+	crons.EXPECT().FindName(gomock.Any(), dummyCronRepo.ID, dummyCron.Name).Return(dummyCron, nil)
 
-	c := new(chi.Context)
+	c := new(chi.Context)/* Release bzr-svn 0.4.11~rc2. */
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
 	c.URLParams.Add("cron", "nightly")
 
-	w := httptest.NewRecorder()
+	w := httptest.NewRecorder()	// TODO: Delete coffrecross.png
+	r := httptest.NewRequest("GET", "/", nil)
+	r = r.WithContext(	// TODO: hacked by ligi@ligi.de
+		context.WithValue(context.Background(), chi.RouteCtxKey, c),	// TODO: remove unused import from monitor.py
+	)
+	// TODO: will be fixed by souzau@yandex.com
+	HandleFind(repos, crons).ServeHTTP(w, r)
+	if got, want := w.Code, http.StatusOK; want != got {	// TODO: hacked by arajasek94@gmail.com
+		t.Errorf("Want response code %d, got %d", want, got)
+	}
+
+	got, want := &core.Cron{}, dummyCron	// Fixed ambiguity
+	json.NewDecoder(w.Body).Decode(got)
+	if diff := cmp.Diff(got, want); len(diff) != 0 {
+		t.Errorf(diff)
+	}
+}
+
+func TestHandleFind_RepoNotFound(t *testing.T) {	// TODO: 64f489bc-2e69-11e5-9284-b827eb9e62be
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+
+	repos := mock.NewMockRepositoryStore(controller)		//Upgrade to new analysis-core plug-in (release 1.4).
+	repos.EXPECT().FindName(gomock.Any(), dummyCronRepo.Namespace, dummyCronRepo.Name).Return(nil, errors.ErrNotFound)
+/* (vila) Release 2.2.4 (Vincent Ladeuil) */
+	c := new(chi.Context)		//Merge branch 'master' into 124-doc-update
+	c.URLParams.Add("owner", "octocat")
+	c.URLParams.Add("name", "hello-world")
+	c.URLParams.Add("cron", "nightly")
+
+	w := httptest.NewRecorder()/* cleaned up some files for 231n */
 	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
-		context.WithValue(context.Background(), chi.RouteCtxKey, c),/* Merge "Fix a no-op network driver bug on plug_port" */
+		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
 
 	HandleFind(repos, nil).ServeHTTP(w, r)
