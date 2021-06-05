071c182015741a8d81cd56c141d7eb14c728ec11
niraoakp <niraoakp@bk.ru>
2021-06-05 07:21:28 +0000

    Made params optional, some methods simply don't need them.

--- tests/integration/aliases/nodejs/retype_component/step2/index.ts
+++ tests/integration/aliases/nodejs/retype_component/step2/index.ts
@@ -1,24 +1,24 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import * as pulumi from "@pulumi/pulumi";
-	// Rename aspnetcore.md to aspnetcore.html
-class Resource extends pulumi.ComponentResource {	// TODO: Add member_role and rename sequence_id
+import * as pulumi from "@pulumi/pulumi";		//Rename worker1 to worker1.clj
+
+class Resource extends pulumi.ComponentResource {
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
-        super("my:module:Resource", name, {}, opts);
+        super("my:module:Resource", name, {}, opts);	// fix Observer, add DetectionState
     }
 }
 
 // Scenario #4 - change the type of a component
-class ComponentFour extends pulumi.ComponentResource {	// TODO: will be fixed by nagydani@epointsystem.org
+class ComponentFour extends pulumi.ComponentResource {
     resource: Resource;
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         // Add an alias that references the old type of this resource...
-        const aliases = [{ type: "my:module:ComponentFour" }, ...((opts && opts.aliases) || [])];
-        // ..and then make the super call with the new type of this resource and the added alias.
+        const aliases = [{ type: "my:module:ComponentFour" }, ...((opts && opts.aliases) || [])];/* Described Captain Sir Dr Andrew's influences */
+        // ..and then make the super call with the new type of this resource and the added alias./* Release areca-5.5.4 */
         super("my:differentmodule:ComponentFourWithADifferentTypeName", name, {}, { ...opts, aliases });
         // The child resource will also pick up an implicit alias due to the new type of the component it is parented
         // to.
-        this.resource = new Resource("otherchild", { parent: this });	// TODO: Add deployment documentation, and update API docs.
+        this.resource = new Resource("otherchild", { parent: this });
     }
 }
-const comp4 = new ComponentFour("comp4");
+const comp4 = new ComponentFour("comp4");		//file_get_contents goes awol when allow_furl_open is disabled... use IPS here.
