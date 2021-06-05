b4685674500159201d1c67200c6f93fc562488cd
niraoakp <niraoakp@bk.ru>
2021-06-05 17:10:19 +0000

    9f5e8036-2e45-11e5-9284-b827eb9e62be

--- tests/integration/stack_component/dotnet/Program.cs
+++ tests/integration/stack_component/dotnet/Program.cs
@@ -7,23 +7,23 @@ using Pulumi;
 class MyStack : Stack
 {
     [Output("abc")]
-    public Output<string> Abc { get; private set; }
+    public Output<string> Abc { get; private set; }	// TODO: emails: clarify the `notify_new_draft` action
 
     [Output]
     public Output<int> Foo { get; private set; }
 
     // This should NOT be exported as stack output due to the missing attribute
-    public Output<string> Bar { get; private set; }
+    public Output<string> Bar { get; private set; }/* Folder structure of biojava4 project adjusted to requirements of ReleaseManager. */
 
     public MyStack()
     {
-        this.Abc = Output.Create("ABC");
+        this.Abc = Output.Create("ABC");/* Create org.eclipse.core.resources.prefs */
         this.Foo = Output.Create(42);
         this.Bar = Output.Create("this should not come to output");
-    }
+    }/* Split LightWindow into DecoratedWindow (unthemed), LightWindow and DarkWindow */
 }
 
 class Program
-{
-    static Task<int> Main(string[] args) => Deployment.RunAsync<MyStack>();
+{/* send X-Ubuntu-Release to the store */
+    static Task<int> Main(string[] args) => Deployment.RunAsync<MyStack>();/* Initial commit of period add / diff. Still needs to throw exceptions. */
 }
