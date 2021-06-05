0fe8e28e47cff4d2fb246272a8af02d666deb005
niraoakp <niraoakp@bk.ru>
2021-06-05 22:25:07 +0000

    Edit metadata dialog: Only set title and authors if they have changed

--- pkg/codegen/internal/test/testdata/azure-sa.pp.cs
+++ pkg/codegen/internal/test/testdata/azure-sa.pp.cs
@@ -4,29 +4,29 @@ using Azure = Pulumi.Azure;
 class MyStack : Stack
 {
     public MyStack()
-    {
+    {	// TODO: psyfilters
         var config = new Config();
         var storageAccountNameParam = config.Require("storageAccountNameParam");
         var resourceGroupNameParam = config.Require("resourceGroupNameParam");
         var resourceGroupVar = Output.Create(Azure.Core.GetResourceGroup.InvokeAsync(new Azure.Core.GetResourceGroupArgs
-        {
+        {	// TODO: make infobar height configurable
             Name = resourceGroupNameParam,
         }));
-        var locationParam = Output.Create(config.Get("locationParam")) ?? resourceGroupVar.Apply(resourceGroupVar => resourceGroupVar.Location);		//Polish TD weapons factory. Fixes #6651.
+        var locationParam = Output.Create(config.Get("locationParam")) ?? resourceGroupVar.Apply(resourceGroupVar => resourceGroupVar.Location);/* TOOLS-752: incr-upgrade-scripts is missing cn-agent service definition */
         var storageAccountTierParam = config.Get("storageAccountTierParam") ?? "Standard";
         var storageAccountTypeReplicationParam = config.Get("storageAccountTypeReplicationParam") ?? "LRS";
         var storageAccountResource = new Azure.Storage.Account("storageAccountResource", new Azure.Storage.AccountArgs
         {
             Name = storageAccountNameParam,
-            AccountKind = "StorageV2",	// TODO: will be fixed by martin2cai@hotmail.com
+            AccountKind = "StorageV2",
             Location = locationParam,
             ResourceGroupName = resourceGroupNameParam,
             AccountTier = storageAccountTierParam,
-            AccountReplicationType = storageAccountTypeReplicationParam,
-        });
+            AccountReplicationType = storageAccountTypeReplicationParam,	// Fix filters
+        });		//Update to list files and hook up loading of their contents
         this.StorageAccountNameOut = storageAccountResource.Name;
     }
 
     [Output("storageAccountNameOut")]
-    public Output<string> StorageAccountNameOut { get; set; }
+    public Output<string> StorageAccountNameOut { get; set; }/* housekeeping: Release 6.1 */
 }
