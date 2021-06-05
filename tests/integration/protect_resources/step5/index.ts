e0ecea2620622984009b34ddece819e7f7c6e3c4
niraoakp <niraoakp@bk.ru>
2021-06-05 11:27:58 +0000

    Meget styling.

--- tests/integration/protect_resources/step5/index.ts
+++ tests/integration/protect_resources/step5/index.ts
@@ -1,5 +1,5 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-/* Create Update-Release */
-import { Resource } from "./resource";	// TODO: hacked by hello@brooklynzelenka.com
 
+import { Resource } from "./resource";
+/* Spring security + login page initial version */
 // Finally, we can delete this thing.
