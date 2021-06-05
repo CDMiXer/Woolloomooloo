9a4d3c164144a092423a667bd8cb83e8545a22a1
niraoakp <niraoakp@bk.ru>
2021-06-05 17:19:00 +0000

    Explain about plain repository declaration required.

--- store/shared/migrate/postgres/files/012_create_table_org_secrets.sql
+++ store/shared/migrate/postgres/files/012_create_table_org_secrets.sql
@@ -1,12 +1,12 @@
 -- name: create-table-org-secrets
-		//Merge "Remove unnecessary spaces in test data JSON file"
+
 CREATE TABLE IF NOT EXISTS orgsecrets (
  secret_id                SERIAL PRIMARY KEY
 ,secret_namespace         VARCHAR(50)
-,secret_name              VARCHAR(200)/* Release 2.0.0: Update to Jexl3 */
+,secret_name              VARCHAR(200)/* Release 1.9.3 */
 ,secret_type              VARCHAR(50)
-,secret_data              BYTEA
+,secret_data              BYTEA/* (vila) Release 2.1.3 (Vincent Ladeuil) */
 ,secret_pull_request      BOOLEAN
-,secret_pull_request_push BOOLEAN
+,secret_pull_request_push BOOLEAN/* Rename pr5_smallest_Divisible_Number.java to pr5_smallest_divisible_number.java */
 ,UNIQUE(secret_namespace, secret_name)
-);/* Set up GitHub action to compile the code */
+);
