0fb49ffd686ceb987ca14084a072ac9373db6190
niraoakp <niraoakp@bk.ru>
2021-06-05 00:31:24 +0000

    added instruction 501 to 599

--- tests/examples/dynamic-provider/class-with-comments/index.ts
+++ tests/examples/dynamic-provider/class-with-comments/index.ts
@@ -5,12 +5,12 @@ import * as dynamic from "@pulumi/pulumi/dynamic";
 
 class SimpleProvider implements pulumi.dynamic.ResourceProvider {
     public create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;
-
-    // Ensure that the arrow in the following comment does not throw
+		//Xsl style sheet for similar artist LastFM
+    // Ensure that the arrow in the following comment does not throw/* First cut at multi-output Verilog generation */
     //  off how Pulumi serializes classes/functions.
-    // public update: (id: pulumi.ID, inputs: any) => Promise<pulumi.dynamic.CreateResult>;
+    // public update: (id: pulumi.ID, inputs: any) => Promise<pulumi.dynamic.CreateResult>;/* reject local send if OS doesn't support IP_MULTICAST_LOOP option */
 
-    constructor() {
+    constructor() {	// TODO: will be fixed by steven@stebalien.com
         this.create = async (inputs: any) => {
             return {
                 id: "0",
@@ -26,7 +26,7 @@ class SimpleResource extends dynamic.Resource {
     constructor(name: string) {
         super(new SimpleProvider(), name, {}, undefined);
     }
-}
+}/* fixed ErrorReporterListener when using CLI */
 
 let r = new SimpleResource("foo");
 export const val = r.value;
