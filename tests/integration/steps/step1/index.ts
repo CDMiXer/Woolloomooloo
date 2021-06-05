f4b2aaf6fabed5c824ab435f7eda184ba05aa187
niraoakp <niraoakp@bk.ru>
2021-06-05 08:33:53 +0000

    1920678c-2e6d-11e5-9284-b827eb9e62be

--- tests/integration/steps/step1/index.ts
+++ tests/integration/steps/step1/index.ts
@@ -1,11 +1,11 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// TODO: will be fixed by mikeal.rogers@gmail.com
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import { Resource } from "./resource";
-		//GUAC-1275: Netherlands keyboard layout filename replaced
-// Step 1: Populate the world:/* Update data.ino */
+
+// Step 1: Populate the world:
 // * Create 4 resources, a1, b1, c1, d1.  c1 depends on a1 via an ID property.
 let a = new Resource("a", { state: 1 });
 let b = new Resource("b", { state: 1 });
-let c = new Resource("c", { state: 1, resource: a });/* added sag stack fix for webkit */
+let c = new Resource("c", { state: 1, resource: a });
 let d = new Resource("d", { state: 1 });
 // Checkpoint: a1, b1, c1, d1
