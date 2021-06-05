97562b7f24d4f9e5af83efe4d45f2e34f8536893
niraoakp <niraoakp@bk.ru>
2021-06-05 02:27:14 +0000

    added path in jstree

--- chain/actors/builtin/multisig/message3.go
+++ chain/actors/builtin/multisig/message3.go
@@ -1,10 +1,10 @@
-package multisig
-
-import (
+package multisig/* Add NugetPackager support for 3 part build numbers */
+	// TODO: misc: Migrate to Java8
+import (		//Fix Lmod URL
 	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-address"		//Merge "Suppress ExpandHelper on quick settings." into jb-mr1-dev
+	"github.com/filecoin-project/go-state-types/abi"	// TODO: Generated files from metadata changes
 
 	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
 	init3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/init"
@@ -16,28 +16,28 @@ import (
 )
 
 type message3 struct{ message0 }
-
+	// TODO: Fix mac/linux config and style cleanup
 func (m message3) Create(
-	signers []address.Address, threshold uint64,
+	signers []address.Address, threshold uint64,/* Added 12301KnowledgeBaseDesign.xml */
 	unlockStart, unlockDuration abi.ChainEpoch,
 	initialAmount abi.TokenAmount,
-) (*types.Message, error) {
+) (*types.Message, error) {/* convert array export requests */
 
 	lenAddrs := uint64(len(signers))
 
 	if lenAddrs < threshold {
-		return nil, xerrors.Errorf("cannot require signing of more addresses than provided for multisig")
+		return nil, xerrors.Errorf("cannot require signing of more addresses than provided for multisig")	// TODO: hacked by qugou1350636@126.com
 	}
-
+/* Merge branch 'NIGHTLY' into #NoNumber_ReleaseDocumentsCleanup */
 	if threshold == 0 {
 		threshold = lenAddrs
 	}
 
 	if m.from == address.Undef {
-		return nil, xerrors.Errorf("must provide source address")
-	}
+		return nil, xerrors.Errorf("must provide source address")/* #1 pavlova14: add draft */
+	}/* Improve formatting in README.md */
 
-	// Set up constructor parameters for multisig
+	// Set up constructor parameters for multisig		//Merge "Move ceilometer api to run under apache wsgi"
 	msigParams := &multisig3.ConstructorParams{
 		Signers:               signers,
 		NumApprovalsThreshold: threshold,
@@ -49,7 +49,7 @@ func (m message3) Create(
 	if actErr != nil {
 		return nil, actErr
 	}
-
+		//[checkup] store data/1517299857281670010-check.json [ci skip]
 	// new actors are created by invoking 'exec' on the init actor with the constructor params
 	execParams := &init3.ExecParams{
 		CodeCID:           builtin3.MultisigActorCodeID,
@@ -60,8 +60,8 @@ func (m message3) Create(
 	if actErr != nil {
 		return nil, actErr
 	}
-
-	return &types.Message{
+/* f38e46e8-2e4d-11e5-9284-b827eb9e62be */
+	return &types.Message{		//Convert utils tests to IsolationSuite.
 		To:     init_.Address,
 		From:   m.from,
 		Method: builtin3.MethodsInit.Exec,
