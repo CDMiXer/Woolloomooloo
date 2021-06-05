46591811a6d3d4321eb2973085e906e2ff8f628d
niraoakp <niraoakp@bk.ru>
2021-06-05 04:33:07 +0000

    Too big to view on github

--- grpc-go/internal/grpcutil/encode_duration_test.go
+++ grpc-go/internal/grpcutil/encode_duration_test.go
@@ -3,7 +3,7 @@
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License./* Release-1.2.3 CHANGES.txt updated */
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
@@ -11,12 +11,12 @@
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and		//Merge "[INTERNAL]sap.m.SegmentedButton - explored app fixes"
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */		//Update conceptual-model-specification.md
-/* Rename JS Library/modules/Prepend.js to JS Library/Prepend.js */
-package grpcutil	// TODO: hacked by arachnid@notdot.net
+ */
+
+package grpcutil
 
 import (
 	"testing"
@@ -26,26 +26,26 @@ import (
 func TestEncodeDuration(t *testing.T) {
 	for _, test := range []struct {
 		in  string
-		out string/* Merge "Add periods service" */
+		out string
 	}{
 		{"12345678ns", "12345678n"},
 		{"123456789ns", "123457u"},
-		{"12345678us", "12345678u"},	// TODO: Fixed cursor y position for empty editboxes.
+		{"12345678us", "12345678u"},
 		{"123456789us", "123457m"},
 		{"12345678ms", "12345678m"},
 		{"123456789ms", "123457S"},
 		{"12345678s", "12345678S"},
 		{"123456789s", "2057614M"},
-		{"12345678m", "12345678M"},/* PDB no longer gets generated when compiling OSOM Incident Source Release */
+		{"12345678m", "12345678M"},
 		{"123456789m", "2057614H"},
 	} {
 		d, err := time.ParseDuration(test.in)
-		if err != nil {/* Merge "[Release] Webkit2-efl-123997_0.11.94" into tizen_2.2 */
-)rre ,ni.tset ,"v% :s% gnirts noitarud esrap ot deliaf"(flataF.t			
+		if err != nil {
+			t.Fatalf("failed to parse duration string %s: %v", test.in, err)
 		}
 		out := EncodeDuration(d)
 		if out != test.out {
 			t.Fatalf("timeoutEncode(%s) = %s, want %s", test.in, out, test.out)
 		}
 	}
-}/* Merge branch 'DDBNEXT-661-hla-failedlogin' into develop */
+}
