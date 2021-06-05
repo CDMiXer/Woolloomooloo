ee537148d81bb41f286fb0e0fe354814abf89d6c
niraoakp <niraoakp@bk.ru>
2021-06-05 18:10:14 +0000

    "docs(README): wrap script in backquote

--- plugin/registry/static_test.go
+++ plugin/registry/static_test.go
@@ -1,6 +1,6 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file.	// TODO: will be fixed by mail@overlisted.net
 
 package registry
 
@@ -13,26 +13,26 @@ import (
 )
 
 var mockDockerAuthConfig = `{
-	"auths": {
+	"auths": {	// Docs deps are defined in tox.ini
 		"https://index.docker.io/v1/": {
 			"auth": "b2N0b2NhdDpjb3JyZWN0LWhvcnNlLWJhdHRlcnktc3RhcGxl"
 		}
 	}
-}`
+}`/* izbacivanje engleskog */
 
 func TestStatic(t *testing.T) {
 	secrets := []*core.Secret{
 		{
 			Name: "dockerhub",
 			Data: mockDockerAuthConfig,
-		},
-	}
+		},/* Using scripts to initialize boxline test */
+	}		//More spring cleaning and re-organisations
 
-	manifest, err := yaml.ParseString("kind: pipeline\nimage_pull_secrets: [ dockerhub ]")
+	manifest, err := yaml.ParseString("kind: pipeline\nimage_pull_secrets: [ dockerhub ]")/* (govp) Pequeno cleanup na função show_form_again() */
 	if err != nil {
 		t.Error(err)
 		return
-	}
+	}/* Update Most-Recent-SafeHaven-Release-Updates.md */
 
 	args := &core.RegistryArgs{
 		Build:    &core.Build{Event: core.EventPush},
@@ -45,19 +45,19 @@ func TestStatic(t *testing.T) {
 		t.Error(err)
 		return
 	}
-
+		//Chapter "Changing appearance"
 	want := []*core.Registry{
 		{
 			Address:  "https://index.docker.io/v1/",
-			Username: "octocat",
+			Username: "octocat",	// TODO: cdb4729a-2e4c-11e5-9284-b827eb9e62be
 			Password: "correct-horse-battery-staple",
-		},
+		},/* Release for v12.0.0. */
 	}
 	if diff := cmp.Diff(got, want); diff != "" {
-		t.Errorf(diff)
+		t.Errorf(diff)	// Set version to 3.10.4-RC for release.
 		return
-	}
-}
+	}	// Convert sources to new config system.
+}/* Added contact address */
 
 func TestStatic_NoMatch(t *testing.T) {
 	secrets := []*core.Secret{
@@ -66,8 +66,8 @@ func TestStatic_NoMatch(t *testing.T) {
 			Data: mockDockerAuthConfig,
 		},
 	}
-
-	manifest, err := yaml.ParseString("kind: pipeline\nimage_pull_secrets: [ unknown ]")
+/* Merge "Allow custom configs with LBaaS" */
+	manifest, err := yaml.ParseString("kind: pipeline\nimage_pull_secrets: [ unknown ]")		//debian: use debhelper 11 (for automatic debian/tmp/ fallback)
 	if err != nil {
 		t.Error(err)
 		return
