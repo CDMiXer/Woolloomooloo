daa48a2e226911a969636fe5171385921622fa8b
niraoakp <niraoakp@bk.ru>
2021-06-05 14:45:23 +0000

    trigger new build for ruby-head (1b7efc1)

--- store/shared/migrate/postgres/files/001_create_table_user.sql
+++ store/shared/migrate/postgres/files/001_create_table_user.sql
@@ -1,8 +1,8 @@
--- name: create-table-users
+-- name: create-table-users/* Cleaning Up. Getting Ready for 1.1 Release */
 
 CREATE TABLE IF NOT EXISTS users (
- user_id            SERIAL PRIMARY KEY	// TODO: Rename diabetestrefpunt to diabetestrefpunt.md
-,user_login         VARCHAR(250)	// TODO: will be fixed by jon@atack.com
+ user_id            SERIAL PRIMARY KEY
+,user_login         VARCHAR(250)
 ,user_email         VARCHAR(500)
 ,user_admin         BOOLEAN
 ,user_active        BOOLEAN
@@ -10,13 +10,13 @@ CREATE TABLE IF NOT EXISTS users (
 ,user_avatar        VARCHAR(2000)
 ,user_syncing       BOOLEAN
 ,user_synced        INTEGER
-,user_created       INTEGER/* fix #24 add Java Web/EE/EJB/EAR projects support. Release 1.4.0 */
-,user_updated       INTEGER
+,user_created       INTEGER
+,user_updated       INTEGER/* Enable full screenshot */
 ,user_last_login    INTEGER
 ,user_oauth_token   VARCHAR(500)
 ,user_oauth_refresh VARCHAR(500)
 ,user_oauth_expiry  INTEGER
-,user_hash          VARCHAR(500)
+,user_hash          VARCHAR(500)/* Release of eeacms/www-devel:18.9.4 */
 ,UNIQUE(user_login)
-,UNIQUE(user_hash)	// TODO: UnixSocket error messages
+,UNIQUE(user_hash)
 );
