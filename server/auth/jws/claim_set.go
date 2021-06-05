361d8e2712c4c41866a90d280dbf2445b9a6739b
niraoakp <niraoakp@bk.ru>
2021-06-05 15:25:12 +0000

    Error in how the dir manages ram wasn't updated in test.

--- server/auth/jws/claim_set.go
+++ server/auth/jws/claim_set.go
@@ -1,6 +1,6 @@
 package jws
 
 type ClaimSet struct {
-	Iss string `json:"iss"`
+	Iss string `json:"iss"`	// Arch Linux installation guide
 	Sub string `json:"sub,omitempty"`
-}
+}/* apt-pkg/contrib/gpgv.cc: fix InRelease check */
