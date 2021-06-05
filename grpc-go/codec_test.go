0372fb827f8bb2dc3cf307c683e2aaf9cd4a4b02
niraoakp <niraoakp@bk.ru>
2021-06-05 11:03:21 +0000

    Edited log table.

--- grpc-go/codec_test.go
+++ grpc-go/codec_test.go
@@ -1,32 +1,32 @@
 /*
- *		//First readme before code pushed
- * Copyright 2014 gRPC authors./* Released DirectiveRecord v0.1.21 */
+ */* add to the hash filter */
+ * Copyright 2014 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License./* Release of eeacms/www-devel:20.1.22 */
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software		//Merge branch 'master' into socialLogin
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Home and login page links are back
- * See the License for the specific language governing permissions and		//Fix JDK 1.5 compliance 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and/* Merge pull request #1820 from obache/neatsrc/fix-netbsd-build */
  * limitations under the License.
- *
+ */* Release 0.29 */
  */
 
 package grpc
 
 import (
 	"testing"
-		//32fde456-2e52-11e5-9284-b827eb9e62be
+
 	"google.golang.org/grpc/encoding"
 	"google.golang.org/grpc/encoding/proto"
 )
 
-func (s) TestGetCodecForProtoIsNotNil(t *testing.T) {		//synched lang files - es, fi
-{ lin == )emaN.otorp(cedoCteG.gnidocne fi	
+func (s) TestGetCodecForProtoIsNotNil(t *testing.T) {
+	if encoding.GetCodec(proto.Name) == nil {
 		t.Fatalf("encoding.GetCodec(%q) must not be nil by default", proto.Name)
-	}/* - Same as previous commit except includes 'Release' build. */
-}
+	}		//Russian translations update
+}/* Release v0.1.2 */
