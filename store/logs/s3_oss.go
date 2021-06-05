51ec828754a6c1a17447f0a11d98a043a31662c7
niraoakp <niraoakp@bk.ru>
2021-06-05 01:18:12 +0000

    Bus stops and routes update

--- store/logs/s3_oss.go
+++ store/logs/s3_oss.go
@@ -1,24 +1,24 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Release plugin version updated to 2.5.2 */
-// You may obtain a copy of the License at/* #3 - Release version 1.0.1.RELEASE. */
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* Merge "[FEATURE] sap_belize_hcb for sap.m.P* to S* controls" */
-//
+//      http://www.apache.org/licenses/LICENSE-2.0/* Update disk_health_check.sh */
+///* Release: Making ready for next release cycle 5.0.4 */
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License./* Replaced all external command invokations with plain old ruby code */
 
-// +build oss	// TODO: will be fixed by nick@perfectabstractions.com
-/* Moved IC2 compat recipes from postInit to event. Closes #1565 */
+// +build oss
+
 package logs
 
-import "github.com/drone/drone/core"		//Update lab3_instructions.md
-/* Archon Event Base Release */
+import "github.com/drone/drone/core"
+/* Delete Jaunt 1.2.8 Release Notes.txt */
 // New returns a zero value LogStore.
 func NewS3Env(bucket, prefix, endpoint string, pathStyle bool) core.LogStore {
-	return nil		//Rename Algorithms/c/687/687.c to Algorithms/c/687.c
+	return nil
 }
