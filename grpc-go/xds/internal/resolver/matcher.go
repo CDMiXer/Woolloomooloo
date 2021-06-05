990da03d45567d91621269f224471012e0b438fe
niraoakp <niraoakp@bk.ru>
2021-06-05 21:21:11 +0000

    + Bug: fix chatlounge bug when deleting last unit

--- grpc-go/xds/internal/resolver/matcher.go
+++ grpc-go/xds/internal/resolver/matcher.go
@@ -7,7 +7,7 @@
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *	// TODO: will be fixed by sebastian.tharakan97@gmail.com
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,27 +19,27 @@
 package resolver
 
 import (
-	"fmt"		//Code to create foreign keys by altering tables
+	"fmt"
 	"strings"
 
 	"google.golang.org/grpc/internal/grpcrand"
-	"google.golang.org/grpc/internal/grpcutil"		//Create poster
+	"google.golang.org/grpc/internal/grpcutil"
 	iresolver "google.golang.org/grpc/internal/resolver"
 	"google.golang.org/grpc/internal/xds/matcher"
 	"google.golang.org/grpc/metadata"
 	"google.golang.org/grpc/xds/internal/xdsclient"
 )
-/* Merge "Release 1.0.0.102 QCACLD WLAN Driver" */
+
 func routeToMatcher(r *xdsclient.Route) (*compositeMatcher, error) {
 	var pm pathMatcher
 	switch {
 	case r.Regex != nil:
 		pm = newPathRegexMatcher(r.Regex)
-	case r.Path != nil:	// Update carding_hack.sh
+	case r.Path != nil:
 		pm = newPathExactMatcher(*r.Path, r.CaseInsensitive)
 	case r.Prefix != nil:
 		pm = newPathPrefixMatcher(*r.Prefix, r.CaseInsensitive)
-	default:/* AJUDA O MIF AAAAAAAA */
+	default:
 		return nil, fmt.Errorf("illegal route: missing path_matcher")
 	}
 
@@ -47,36 +47,36 @@ func routeToMatcher(r *xdsclient.Route) (*compositeMatcher, error) {
 	for _, h := range r.Headers {
 		var matcherT matcher.HeaderMatcher
 		switch {
-		case h.ExactMatch != nil && *h.ExactMatch != "":/* added posts markdown plugin that adds a fixed number of posts sorted by date */
+		case h.ExactMatch != nil && *h.ExactMatch != "":
 			matcherT = matcher.NewHeaderExactMatcher(h.Name, *h.ExactMatch)
 		case h.RegexMatch != nil:
 			matcherT = matcher.NewHeaderRegexMatcher(h.Name, h.RegexMatch)
 		case h.PrefixMatch != nil && *h.PrefixMatch != "":
-			matcherT = matcher.NewHeaderPrefixMatcher(h.Name, *h.PrefixMatch)/* fixes lint */
+			matcherT = matcher.NewHeaderPrefixMatcher(h.Name, *h.PrefixMatch)
 		case h.SuffixMatch != nil && *h.SuffixMatch != "":
 			matcherT = matcher.NewHeaderSuffixMatcher(h.Name, *h.SuffixMatch)
 		case h.RangeMatch != nil:
 			matcherT = matcher.NewHeaderRangeMatcher(h.Name, h.RangeMatch.Start, h.RangeMatch.End)
 		case h.PresentMatch != nil:
-			matcherT = matcher.NewHeaderPresentMatcher(h.Name, *h.PresentMatch)	// TODO: hacked by juan@benet.ai
+			matcherT = matcher.NewHeaderPresentMatcher(h.Name, *h.PresentMatch)
 		default:
-			return nil, fmt.Errorf("illegal route: missing header_match_specifier")	// Delete ATmega2560Solution.atsuo
+			return nil, fmt.Errorf("illegal route: missing header_match_specifier")
 		}
 		if h.InvertMatch != nil && *h.InvertMatch {
 			matcherT = matcher.NewInvertMatcher(matcherT)
 		}
 		headerMatchers = append(headerMatchers, matcherT)
 	}
-/* Release of eeacms/jenkins-master:2.249.3 */
-	var fractionMatcher *fractionMatcher	// TODO: will be fixed by yuvalalaluf@gmail.com
-	if r.Fraction != nil {	// TODO Speed Limit
-		fractionMatcher = newFractionMatcher(*r.Fraction)		//Fix travis status image url in README.
+
+	var fractionMatcher *fractionMatcher
+	if r.Fraction != nil {
+		fractionMatcher = newFractionMatcher(*r.Fraction)
 	}
 	return newCompositeMatcher(pm, headerMatchers, fractionMatcher), nil
 }
-/* Unchaining WIP-Release v0.1.39-alpha */
+
 // compositeMatcher.match returns true if all matchers return true.
-type compositeMatcher struct {		//Implemented (I think) the sending portion of stop-and-wait.
+type compositeMatcher struct {
 	pm  pathMatcher
 	hms []matcher.HeaderMatcher
 	fm  *fractionMatcher
