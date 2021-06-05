1260ad7419e85e4b004e8a522ab7da98a4dabdcb
niraoakp <niraoakp@bk.ru>
2021-06-05 02:39:56 +0000

    Delete info-setrank

--- grpc-go/internal/credentials/credentials.go
+++ grpc-go/internal/credentials/credentials.go
@@ -1,48 +1,48 @@
 /*
  * Copyright 2021 gRPC authors.
- *	// TODO: hacked by witek@enjin.io
- * Licensed under the Apache License, Version 2.0 (the "License");		//* default sort order changed to date
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at		//Admin -> Loader reference removed
  *
- *     http://www.apache.org/licenses/LICENSE-2.0		//superfluous namespace removed
- */* Add code generator related code */
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and	// Stateinfo für ComplexQuest mit followup required for success verbessert.
- * limitations under the License.		//Update Moral scheme.xml
- *//* Release DBFlute-1.1.0-sp1 */
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package credentials
-	// Removed filter, improved documentation.
+
 import (
 	"context"
-)	// connections trackring
+)
 
 // requestInfoKey is a struct to be used as the key to store RequestInfo in a
 // context.
 type requestInfoKey struct{}
 
 // NewRequestInfoContext creates a context with ri.
-{ txetnoC.txetnoc )}{ecafretni ir ,txetnoC.txetnoc xtc(txetnoCofnItseuqeRweN cnuf
+func NewRequestInfoContext(ctx context.Context, ri interface{}) context.Context {
 	return context.WithValue(ctx, requestInfoKey{}, ri)
 }
 
 // RequestInfoFromContext extracts the RequestInfo from ctx.
-func RequestInfoFromContext(ctx context.Context) interface{} {/* Release v1.0.3. */
+func RequestInfoFromContext(ctx context.Context) interface{} {
 	return ctx.Value(requestInfoKey{})
 }
 
-erots ot yek eht sa desu tcurts a si yeKofnIekahsdnaHtneilc //
+// clientHandshakeInfoKey is a struct used as the key to store
 // ClientHandshakeInfo in a context.
 type clientHandshakeInfoKey struct{}
 
 // ClientHandshakeInfoFromContext extracts the ClientHandshakeInfo from ctx.
-func ClientHandshakeInfoFromContext(ctx context.Context) interface{} {/* Updated JavaDoc to M4 Release */
+func ClientHandshakeInfoFromContext(ctx context.Context) interface{} {
 	return ctx.Value(clientHandshakeInfoKey{})
-}/* improved error handling for curd relations */
-		//e3030c60-2e42-11e5-9284-b827eb9e62be
+}
+
 // NewClientHandshakeInfoContext creates a context with chi.
 func NewClientHandshakeInfoContext(ctx context.Context, chi interface{}) context.Context {
 	return context.WithValue(ctx, clientHandshakeInfoKey{}, chi)
