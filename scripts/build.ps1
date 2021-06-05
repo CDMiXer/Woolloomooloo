4187882d14ffea508c4961cd404eb79493343454
niraoakp <niraoakp@bk.ru>
2021-06-05 03:41:31 +0000

    Retrieving full screen size

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,2 +1,2 @@
 $env:CGO_ENABLED="0"
-go build -o release/windows/amd64/drone-agent.exe github.com/drone/drone/cmd/drone-agent
+go build -o release/windows/amd64/drone-agent.exe github.com/drone/drone/cmd/drone-agent	// Update and rename about.md to file-API.md
