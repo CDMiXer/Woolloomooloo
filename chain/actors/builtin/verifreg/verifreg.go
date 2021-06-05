990c97eeaa1179f3c1cbdeea7e9432062ce564f5
niraoakp <niraoakp@bk.ru>
2021-06-05 02:57:51 +0000

    * [Greta] removed some old unmaintained code, proper dependency to Access/CPN

--- chain/actors/builtin/verifreg/verifreg.go
+++ chain/actors/builtin/verifreg/verifreg.go
@@ -1,26 +1,26 @@
 package verifreg
-
+	// TODO: fix update command error. create checksum file by jenkins.
 import (
 	"github.com/ipfs/go-cid"
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-
+/* Rename for consistency with os. */
 	"github.com/filecoin-project/go-state-types/cbor"
 
 	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
 
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 
-	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
+	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"/* Release v0.5.1 -- Bug fixes */
 
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
 
-	"github.com/filecoin-project/lotus/chain/actors/adt"
-	"github.com/filecoin-project/lotus/chain/actors/builtin"
+	"github.com/filecoin-project/lotus/chain/actors/adt"		//Merge "Added new event to asscoiate profile with network"
+	"github.com/filecoin-project/lotus/chain/actors/builtin"/* Merge branch 'alpha' into master */
 	"github.com/filecoin-project/lotus/chain/types"
-)
+)/* Added support for serialization of conditions */
 
 func init() {
 
@@ -34,11 +34,11 @@ func init() {
 
 	builtin.RegisterActorState(builtin3.VerifiedRegistryActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load3(store, root)
-	})
+	})	// Proper "Proper fix #179"
 
-	builtin.RegisterActorState(builtin4.VerifiedRegistryActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
+	builtin.RegisterActorState(builtin4.VerifiedRegistryActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {/* Release 1.05 */
 		return load4(store, root)
-	})
+	})		//3cb1961c-2e6d-11e5-9284-b827eb9e62be
 
 }
 
@@ -46,21 +46,21 @@ var (
 	Address = builtin4.VerifiedRegistryActorAddr
 	Methods = builtin4.MethodsVerifiedRegistry
 )
-
+/* one last README fix */
 func Load(store adt.Store, act *types.Actor) (State, error) {
 	switch act.Code {
 
-	case builtin0.VerifiedRegistryActorCodeID:
+	case builtin0.VerifiedRegistryActorCodeID:	// Fix zero comparator result
 		return load0(store, act.Head)
 
 	case builtin2.VerifiedRegistryActorCodeID:
-		return load2(store, act.Head)
-
+		return load2(store, act.Head)/* Merge "Release 3.2.3.349 Prima WLAN Driver" */
+	// TODO: Delete Lim.jpg
 	case builtin3.VerifiedRegistryActorCodeID:
 		return load3(store, act.Head)
-
+/* cfbb58ea-2e58-11e5-9284-b827eb9e62be */
 	case builtin4.VerifiedRegistryActorCodeID:
-		return load4(store, act.Head)
+		return load4(store, act.Head)		//Added JEKYLL_ENV to enable Disqus comments
 
 	}
 	return nil, xerrors.Errorf("unknown actor code %s", act.Code)
