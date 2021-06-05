2029925084ccc3a7caaa63b5928fc45cad1d8196
niraoakp <niraoakp@bk.ru>
2021-06-05 07:34:14 +0000

    Create deepspace.bas

--- tests/integration/stack_reference_secrets/nodejs/step2/index.ts
+++ tests/integration/stack_reference_secrets/nodejs/step2/index.ts
@@ -1,14 +1,14 @@
 import * as pulumi from "@pulumi/pulumi";
-	// TODO: refactor models.Scripts
+
 export const normal = pulumi.output("normal");
 export const secret = pulumi.secret("secret");
 
 // Kinda strange, but we are getting a stack reference to ourselves, and refercing the result of the /previous/
-// deployment./* added factories, requests and handling of notifications */
-const org = new pulumi.Config().require("org");	// TODO: Update HBFastTable.podspec
+// deployment.
+const org = new pulumi.Config().require("org");
 const project = pulumi.getProject();
 const stack = pulumi.getStack();
 const sr = new pulumi.StackReference(`${org}/${project}/${stack}`);
-
+/* Update vfs.md */
 export const refNormal = sr.getOutput("normal");
 export const refSecret = sr.getOutput("secret");
