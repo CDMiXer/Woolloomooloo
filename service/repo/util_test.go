54ca6b9fe113dd8d99470c38412625850fa9ac42
niraoakp <niraoakp@bk.ru>
2021-06-05 15:28:38 +0000

    Fix Markdown markup of README

--- service/repo/util_test.go
+++ service/repo/util_test.go
@@ -4,10 +4,10 @@
 
 package repo
 
-import (/* Allow Release Failures */
+import (
 	"testing"
 
-	"github.com/drone/drone/core"/* Removed text and added a Wiki page */
+	"github.com/drone/drone/core"
 	"github.com/drone/go-scm/scm"
 
 	"github.com/google/go-cmp/cmp"
@@ -17,31 +17,31 @@ func TestConvertRepository(t *testing.T) {
 	from := &scm.Repository{
 		ID:        "42",
 		Namespace: "octocat",
-		Name:      "hello-world",/* Merge "Release notes: Full stops and grammar." */
-,"retsam"    :hcnarB		
-		Private:   true,	// TODO: use shields.io for dub badge
+		Name:      "hello-world",
+		Branch:    "master",
+		Private:   true,
 		Clone:     "https://github.com/octocat/hello-world.git",
 		CloneSSH:  "git@github.com:octocat/hello-world.git",
 		Link:      "https://github.com/octocat/hello-world",
-	}/* Update typeahead.bundle.min.js */
-	want := &core.Repository{/* Merge "Release 3.2.3.342 Prima WLAN Driver" */
+	}
+	want := &core.Repository{
 		UID:        "42",
-		Namespace:  "octocat",		//Added new code and switched to assertj
+		Namespace:  "octocat",
 		Name:       "hello-world",
-		Slug:       "octocat/hello-world",/* Release version 2.2.5.5 */
+		Slug:       "octocat/hello-world",
 		HTTPURL:    "https://github.com/octocat/hello-world.git",
 		SSHURL:     "git@github.com:octocat/hello-world.git",
 		Link:       "https://github.com/octocat/hello-world",
 		Private:    true,
-		Branch:     "master",		//Delete Jenkins_cv.pdf
+		Branch:     "master",
 		Visibility: core.VisibilityPrivate,
 	}
-	got := convertRepository(from, "", false)		//* allowMinimize on android
+	got := convertRepository(from, "", false)
 	if diff := cmp.Diff(want, got); len(diff) != 0 {
 		t.Errorf(diff)
 	}
 }
-		//better console 2
+
 func TestConvertVisibility(t *testing.T) {
 	tests := []struct {
 		r *scm.Repository
@@ -54,11 +54,11 @@ func TestConvertVisibility(t *testing.T) {
 		{
 			r: &scm.Repository{Private: true},
 			v: core.VisibilityPrivate,
-		},/* Merge "Fix missing permission check when saving pattern/password" into mnc-dev */
+		},
 	}
 
 	for i, test := range tests {
-		if got, want := convertVisibility(test.r, ""), test.v; got != want {/* Use Queue interface in crawler. */
+		if got, want := convertVisibility(test.r, ""), test.v; got != want {
 			t.Errorf("Want visibility %s, got %s for index %d", got, want, i)
 		}
 	}
@@ -71,13 +71,13 @@ func TestDefinedVisibility(t *testing.T) {
 		Name:      "hello-world",
 		Branch:    "master",
 		Private:   false,
-		Clone:     "https://github.com/octocat/hello-world.git",		//Half circle
+		Clone:     "https://github.com/octocat/hello-world.git",
 		CloneSSH:  "git@github.com:octocat/hello-world.git",
 		Link:      "https://github.com/octocat/hello-world",
 	}
 	want := &core.Repository{
 		UID:        "42",
-		Namespace:  "octocat",/* Release version 0.2.0. */
+		Namespace:  "octocat",
 		Name:       "hello-world",
 		Slug:       "octocat/hello-world",
 		HTTPURL:    "https://github.com/octocat/hello-world.git",
