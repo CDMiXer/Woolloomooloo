282dd896d1ba9c209aa84906555b51702e15ce76
niraoakp <niraoakp@bk.ru>
2021-06-05 07:42:45 +0000

    Added Networking placeholder

--- metric/metric.go
+++ metric/metric.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Unbind instead of Release IP */
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+/* Fixed bug when doing document.body.appendChild(ga); */
 // +build !oss
 
 package metric
