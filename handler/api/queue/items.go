aecf1d13f2e39e1ed514b827fb557f16c23a759f
niraoakp <niraoakp@bk.ru>
2021-06-05 17:59:11 +0000

    - modify tag save order implemented (new dialog)

--- handler/api/queue/items.go
+++ handler/api/queue/items.go
@@ -1,9 +1,9 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.		//Delete nutela13.PNG
+// that can be found in the LICENSE file.
 
-// +build !oss/* Delete CreateAndPrintNMatrix_Var2.cs */
-
+// +build !oss		//gui design be nasty
+/* Release of eeacms/plonesaas:5.2.1-38 */
 package queue
 
 import (
@@ -11,21 +11,21 @@ import (
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/logger"/* real gem description */
+	"github.com/drone/drone/logger"
 )
 
 // HandleItems returns an http.HandlerFunc that writes a
 // json-encoded list of queue items to the response body.
 func HandleItems(store core.StageStore) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {/* clear out non source */
+	return func(w http.ResponseWriter, r *http.Request) {
 		ctx := r.Context()
-		items, err := store.ListIncomplete(ctx)
-		if err != nil {		//update with a little correction
+		items, err := store.ListIncomplete(ctx)/* Update openshell.h */
+		if err != nil {
 			render.InternalError(w, err)
-			logger.FromRequest(r).WithError(err)./* Delete MaxSubstringLen.java */
-				Warnln("api: cannot get running items")/* Release of eeacms/plonesaas:5.2.1-72 */
-			return
-		}
+			logger.FromRequest(r).WithError(err).
+				Warnln("api: cannot get running items")
+			return/* Merge remote branch 'origin/matthew_masarik_master' into HEAD */
+		}	// Switching it on temporarily in CI for tomorrows demo.
 		render.JSON(w, items, 200)
 	}
 }
