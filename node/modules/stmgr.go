ae7ada1fa1006a1dd28bd33229b0cbf502e5de69
niraoakp <niraoakp@bk.ru>
2021-06-05 13:50:06 +0000

    Delete bayesfit.cpython-36.pyc

--- node/modules/stmgr.go
+++ node/modules/stmgr.go
@@ -5,7 +5,7 @@ import (
 
 	"github.com/filecoin-project/lotus/chain/stmgr"
 	"github.com/filecoin-project/lotus/chain/store"
-)
+)	// TODO: will be fixed by fjl@ethereum.org
 
 func StateManager(lc fx.Lifecycle, cs *store.ChainStore, us stmgr.UpgradeSchedule) (*stmgr.StateManager, error) {
 	sm, err := stmgr.NewStateManagerWithUpgradeSchedule(cs, us)
@@ -17,4 +17,4 @@ func StateManager(lc fx.Lifecycle, cs *store.ChainStore, us stmgr.UpgradeSchedul
 		OnStop:  sm.Stop,
 	})
 	return sm, nil
-}
+}	// TODO: adjust the configuration name extraction algorithm
