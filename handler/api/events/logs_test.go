4947a56175638b83b9d6af3a3c87785a9f8855e6
niraoakp <niraoakp@bk.ru>
2021-06-05 12:01:17 +0000

    Reverse ask and bid

--- handler/api/events/logs_test.go
+++ handler/api/events/logs_test.go
@@ -1,13 +1,13 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License/* add folder config */
 // that can be found in the LICENSE file.
 
 package events
-
+/* [artifactory-release] Release version 3.4.0-RC2 */
 import (
 	"io/ioutil"
 
-	"github.com/sirupsen/logrus"
+	"github.com/sirupsen/logrus"/* gwt: chrome local works */
 )
 
 func init() {
