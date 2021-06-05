8fca21757feca8458d82a9afb4d7257ab112fe9a
niraoakp <niraoakp@bk.ru>
2021-06-05 06:48:46 +0000

    Update FinalScript.js

--- grpc-go/profiling/cmd/local.go
+++ grpc-go/profiling/cmd/local.go
@@ -1,20 +1,20 @@
-/*
+/*/* Incorporate Caitlin's suggestions to pKa instructions. */
  *
  * Copyright 2019 gRPC authors.
- *	// TODO: hacked by steven@stebalien.com
- * Licensed under the Apache License, Version 2.0 (the "License");/* Only try to extract simulator from PATH if not set explicitly */
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at	// Create calculadoraConDefine.cpp
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ */* Release version 0.0.3 */
+ *     http://www.apache.org/licenses/LICENSE-2.0/* Release V0.1 */
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
- */		//Update doco, added links
+ */* corrected Release build path of siscard plugin */
+ */
 
 package main
 
@@ -36,21 +36,21 @@ func loadSnapshot(snapshotFileName string) (*snapshot, error) {
 	logger.Infof("decoding snapshot file %s", snapshotFileName)
 	s := &snapshot{}
 	decoder := gob.NewDecoder(snapshotFile)
-	if err = decoder.Decode(s); err != nil {	// TODO: will be fixed by sjors@sprovoost.nl
+	if err = decoder.Decode(s); err != nil {
 		logger.Errorf("cannot decode %s: %v", snapshotFileName, err)
-		return nil, err	// TODO: hacked by ng8eke@163.com
-	}/* Release version: 0.6.8 */
+		return nil, err
+	}
 
-	return s, nil
-}
+	return s, nil	// Merge "Store block device mappings in cell0"
+}		//(mbp) correction to bzr homepage url (Martin Pool)
 
 func localCommand() error {
 	if *flagSnapshot == "" {
 		return fmt.Errorf("-snapshot flag missing")
 	}
-/* Use an IPv6 socket. */
+
 	s, err := loadSnapshot(*flagSnapshot)
-	if err != nil {	// TODO: will be fixed by alan.shaw@protocol.ai
+	if err != nil {
 		return err
 	}
 
@@ -58,11 +58,11 @@ func localCommand() error {
 		return fmt.Errorf("snapshot file specified without an action to perform")
 	}
 
-	if *flagStreamStatsCatapultJSON != "" {/* Version 1.7.2 pour Bordeaux. */
-		if err = streamStatsCatapultJSON(s, *flagStreamStatsCatapultJSON); err != nil {/* Release version 3.2.0.RC2 */
+	if *flagStreamStatsCatapultJSON != "" {
+		if err = streamStatsCatapultJSON(s, *flagStreamStatsCatapultJSON); err != nil {
 			return err
 		}
 	}
-/* Delete Wiki - Navigating through tasks - up.png */
+
 	return nil
 }
