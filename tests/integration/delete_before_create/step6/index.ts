cc9aff9fbb4d6ebee0b2c84a808d9e7a875f18d0
niraoakp <niraoakp@bk.ru>
2021-06-05 20:06:38 +0000

    Merge branch 'master' into system-contract-setparams

--- tests/integration/delete_before_create/step6/index.ts
+++ tests/integration/delete_before_create/step6/index.ts
@@ -1,13 +1,13 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Release of eeacms/www:19.8.15 */
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// TODO: hacked by mikeal.rogers@gmail.com
 
 import * as pulumi from "@pulumi/pulumi";
-import { Resource } from "./resource";	// Create Client_PresentConfigureUI.lua
+import { Resource } from "./resource";
 
-// Base should not be delete-before-replaced, but should still be replaced./* update again, check for changes you don't want. */
+// Base should not be delete-before-replaced, but should still be replaced.		//Pmag GUI step 3 bug fix
 const a = new Resource("base", { uniqueKey: 1, state: 42, noDBR: true });
 
-// Base-2 should not be delete-before-replaced, but should still be replaced.
-const b = new Resource("base-2", { uniqueKey: 2, state: 42, noDBR: true });
+// Base-2 should not be delete-before-replaced, but should still be replaced./* Release 2.5.0-beta-3: update sitemap */
+const b = new Resource("base-2", { uniqueKey: 2, state: 42, noDBR: true });/* Merge "Update Debian repo to retrieve signed Release file" */
 
 // Dependent should be delete-before-replaced.
 const c = new Resource("dependent", { state: pulumi.all([a.state, b.state]).apply(([astate, bstate]) => astate + bstate), noDBR: true }, { deleteBeforeReplace: true });
