2b31ea0c0056cf9b3bc0192d6a4fa33c535d993b
niraoakp <niraoakp@bk.ru>
2021-06-05 23:06:35 +0000

    f3cd3510-2e5c-11e5-9284-b827eb9e62be

--- tests/integration/stack_reference_secrets/nodejs/step1/index.ts
+++ tests/integration/stack_reference_secrets/nodejs/step1/index.ts
@@ -1,5 +1,5 @@
 import * as pulumi from "@pulumi/pulumi";
-
+		//Updated the pydeck-earthengine-layers feedstock.
 export const normal = pulumi.output("normal");
 export const secret = pulumi.secret("secret");
-
+	// Show pixel color in the image
