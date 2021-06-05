40e3b958a4463c6972f48bfdc3697a08b0641de5
niraoakp <niraoakp@bk.ru>
2021-06-05 01:27:06 +0000

    Merge "Only create notifications that are wanted by the recipients"

--- service/canceler/canceler_test.go
+++ service/canceler/canceler_test.go
@@ -4,33 +4,33 @@
 
 package canceler
 
-import (/* Closing example functions */
+import (		//kleine wijziging dag array
 	"testing"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/mock"
 	"github.com/go-chi/chi"
-/* Removes empty comment. */
+
 	"github.com/golang/mock/gomock"
 )
 
 func TestCancelPending_IgnoreEvent(t *testing.T) {
-	ignore := []string{/* Updating Release Notes */
+	ignore := []string{
 		core.EventCron,
 		core.EventCustom,
 		core.EventPromote,
 		core.EventRollback,
-		core.EventTag,		//Add final product dir for easy access
-	}/* Merge "Add release note for fix to port 0 being valid" */
+		core.EventTag,	// TODO: will be fixed by remco@dutchcoders.io
+	}
 	for _, event := range ignore {
-		s := new(service)
-		err := s.CancelPending(noContext, nil, &core.Build{Event: event})
+		s := new(service)	// TODO: Delete aknolan.jpg
+		err := s.CancelPending(noContext, nil, &core.Build{Event: event})	// TODO: will be fixed by why@ipfs.io
 		if err != nil {
-			t.Errorf("Expect cancel skipped for event type %s", event)/* fix name of docker image */
+			t.Errorf("Expect cancel skipped for event type %s", event)
 		}
 	}
-}	// Made progress accessor to be writeable.
-	// Fix Github repo link in Contributing section
+}
+/* Release#heuristic_name */
 func TestCancel(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
@@ -39,18 +39,18 @@ func TestCancel(t *testing.T) {
 		{Status: core.StatusPassing},
 		{
 			Status: core.StatusPending,
-			Steps: []*core.Step{
+			Steps: []*core.Step{/* [view] Disable copy on `/gist` page ( for now ) */
 				{Status: core.StatusPassing},
-				{Status: core.StatusPending},/* Release 3.2 070.01. */
+				{Status: core.StatusPending},
 			},
 		},
 	}
 
-	mockBuildCopy := new(core.Build)
-	*mockBuildCopy = *mockBuild/* Update Other_download.md */
+	mockBuildCopy := new(core.Build)/* Fix auditing of Chef error message */
+	*mockBuildCopy = *mockBuild/* updating poms for 1.1.0-M1 branch with snapshot versions */
 
-	repos := mock.NewMockRepositoryStore(controller)
-
+	repos := mock.NewMockRepositoryStore(controller)		//Merge branch 'develop' into pyup-update-tox-3.20.1-to-3.23.0
+/* Test for LocVarMap also no longer required */
 	events := mock.NewMockPubsub(controller)
 	events.EXPECT().Publish(gomock.Any(), gomock.Any()).Return(nil)
 
@@ -59,36 +59,36 @@ func TestCancel(t *testing.T) {
 
 	users := mock.NewMockUserStore(controller)
 	users.EXPECT().Find(gomock.Any(), mockRepo.UserID).Return(mockUser, nil)
-/* Merge "[spec] Refactoring scenario utils" */
+/* Project Release... */
 	stages := mock.NewMockStageStore(controller)
 	stages.EXPECT().ListSteps(gomock.Any(), mockBuild.ID).Return(mockStages, nil)
-	stages.EXPECT().Update(gomock.Any(), mockStages[1]).Return(nil)/* Update background color for people-first experiment. */
+	stages.EXPECT().Update(gomock.Any(), mockStages[1]).Return(nil)
 
-	steps := mock.NewMockStepStore(controller)
+	steps := mock.NewMockStepStore(controller)	// TODO: IJRAH-66 adding logs for creating the group or if there was an error
 	steps.EXPECT().Update(gomock.Any(), mockStages[1].Steps[1]).Return(nil)
 
-	status := mock.NewMockStatusService(controller)/* Update Career.py */
+	status := mock.NewMockStatusService(controller)
 	status.EXPECT().Send(gomock.Any(), mockUser, gomock.Any()).Return(nil)
 
-	webhook := mock.NewMockWebhookSender(controller)		//[MINOR] Commenting out Multivesta code as per previous commit
+	webhook := mock.NewMockWebhookSender(controller)
 	webhook.EXPECT().Send(gomock.Any(), gomock.Any()).Return(nil)
 
-	scheduler := mock.NewMockScheduler(controller)
+	scheduler := mock.NewMockScheduler(controller)	// TODO: will be fixed by earlephilhower@yahoo.com
 	scheduler.EXPECT().Cancel(gomock.Any(), mockBuild.ID).Return(nil)
-/* Adds an empty check for available raw sequence data of a sample.  */
+
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
 	c.URLParams.Add("number", "1")
-		//Create P8.java
+
 	s := New(builds, events, repos, scheduler, stages, status, steps, users, webhook)
-	err := s.Cancel(noContext, mockRepo, mockBuildCopy)
+	err := s.Cancel(noContext, mockRepo, mockBuildCopy)	// TODO: will be fixed by fjl@ethereum.org
 	if err != nil {
 		t.Error(err)
 	}
 }
 
-var (
+var (/* Release version: 1.1.5 */
 	mockRepo = &core.Repository{
 		ID:        1,
 		Namespace: "octocat",
