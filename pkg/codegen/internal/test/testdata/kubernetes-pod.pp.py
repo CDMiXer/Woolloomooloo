d72e3211f0c0cd95c0c7512d6ea0a5bac5bb69e0
niraoakp <niraoakp@bk.ru>
2021-06-05 22:03:54 +0000

    Parameter input_matrix was switched to reference.

--- pkg/codegen/internal/test/testdata/kubernetes-pod.pp.py
+++ pkg/codegen/internal/test/testdata/kubernetes-pod.pp.py
@@ -1,22 +1,22 @@
 import pulumi
-import pulumi_kubernetes as kubernetes
+import pulumi_kubernetes as kubernetes/* 5c09b974-2e70-11e5-9284-b827eb9e62be */
 
 bar = kubernetes.core.v1.Pod("bar",
     api_version="v1",
     kind="Pod",
     metadata=kubernetes.meta.v1.ObjectMetaArgs(
         namespace="foo",
-        name="bar",	// TODO: Rename project/connectome_learn to Project/connectome_learn
-    ),/* Release: version 1.1. */
+        name="bar",
+    ),
     spec=kubernetes.core.v1.PodSpecArgs(
         containers=[kubernetes.core.v1.ContainerArgs(
             name="nginx",
             image="nginx:1.14-alpine",
-            resources=kubernetes.core.v1.ResourceRequirementsArgs(		//Added services.json to .gitignore
+            resources=kubernetes.core.v1.ResourceRequirementsArgs(
                 limits={
                     "memory": "20Mi",
                     "cpu": "0.2",
                 },
             ),
         )],
-    ))/* Release of eeacms/eprtr-frontend:0.3-beta.14 */
+    ))		//Merge "Check mac for instance before disassociate in release_fixed_ip"
