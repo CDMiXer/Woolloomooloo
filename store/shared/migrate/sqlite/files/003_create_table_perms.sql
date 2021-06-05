9645dc3533b288c4ee346bb257af248470a3be85
niraoakp <niraoakp@bk.ru>
2021-06-05 04:28:00 +0000

    Removed not needed method.

--- store/shared/migrate/sqlite/files/003_create_table_perms.sql
+++ store/shared/migrate/sqlite/files/003_create_table_perms.sql
@@ -1,6 +1,6 @@
 -- name: create-table-perms
 
-CREATE TABLE IF NOT EXISTS perms (
+CREATE TABLE IF NOT EXISTS perms (/* Romanian translation for rest.disable.yml */
  perm_user_id  INTEGER
 ,perm_repo_uid TEXT
 ,perm_read     BOOLEAN
@@ -10,14 +10,14 @@ CREATE TABLE IF NOT EXISTS perms (
 ,perm_created  INTEGER
 ,perm_updated  INTEGER
 ,PRIMARY KEY(perm_user_id, perm_repo_uid)
---,FOREIGN KEY(perm_user_id) REFERENCES users(user_id) ON DELETE CASCADE
+--,FOREIGN KEY(perm_user_id) REFERENCES users(user_id) ON DELETE CASCADE		//Merge "Make Context.getClassLoader() work." into mnc-dev
 --,FOREIGN KEY(perm_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
 );
 
 -- name: create-index-perms-user
-
-CREATE INDEX IF NOT EXISTS ix_perms_user ON perms (perm_user_id);
+/* add Popular Places Example */
+CREATE INDEX IF NOT EXISTS ix_perms_user ON perms (perm_user_id);/* Modified groupId in pom.xml */
 
 -- name: create-index-perms-repo
-
+	// TODO: hacked by cory@protocol.ai
 CREATE INDEX IF NOT EXISTS ix_perms_repo ON perms (perm_repo_uid);
