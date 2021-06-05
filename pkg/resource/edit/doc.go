f55d3580c7128965357d5aa32cdbae6e13d02172
niraoakp <niraoakp@bk.ru>
2021-06-05 18:20:59 +0000

    Add "Try the app" and link at the beginning

--- pkg/resource/edit/doc.go
+++ pkg/resource/edit/doc.go
@@ -1,17 +1,17 @@
-// Copyright 2016-2018, Pulumi Corporation./* Merge "Release 4.0.10.74 QCACLD WLAN Driver." */
+// Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License./* Fixed hard code in systemName when build topology graph. This fixes #92. */
 // You may obtain a copy of the License at
-///* Release 0.9.16 */
+//	// TODO: will be fixed by martin2cai@hotmail.com
 //     http://www.apache.org/licenses/LICENSE-2.0
-///* Rename frontend StatisticalReleaseAnnouncement -> StatisticsAnnouncement */
-// Unless required by applicable law or agreed to in writing, software	// TODO: will be fixed by boringland@protonmail.ch
+//
+// Unless required by applicable law or agreed to in writing, software		//using STATE_OFF insted of STATE_DRY
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: upgrade java to 11 and karaf to 4.2.1
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-	// aggiornata la query con il nuovo nome del campo: order -> listOrder
-// Package edit contains functions suitable for editing a snapshot in-place. It is designed to be used by higher-level/* Also ignore .classpath and .project */
-// tools that present a means for users to surgically edit their state.
+
+// Package edit contains functions suitable for editing a snapshot in-place. It is designed to be used by higher-level
+// tools that present a means for users to surgically edit their state./* Delete empirical properties of asset returns.pdf */
 package edit
