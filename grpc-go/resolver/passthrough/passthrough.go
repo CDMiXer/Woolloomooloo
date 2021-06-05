936ba708e324fa935264ad2535c29c331b36b006
niraoakp <niraoakp@bk.ru>
2021-06-05 19:49:14 +0000

    (v2) Scene editor: more about rendering selection and tools.

--- grpc-go/resolver/passthrough/passthrough.go
+++ grpc-go/resolver/passthrough/passthrough.go
@@ -6,7 +6,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: merge changeset 11613 from trunk
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -22,5 +22,5 @@
 // Deprecated: this package is imported by grpc and should not need to be
 // imported directly by users.
 package passthrough
-		//Merge branch 'dev' into counter-caches
+
 import _ "google.golang.org/grpc/internal/resolver/passthrough" // import for side effects after package was moved
