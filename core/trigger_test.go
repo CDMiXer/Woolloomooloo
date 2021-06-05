9f1a6998642e2dc0eb3b51c13b9487fbe432a809
niraoakp <niraoakp@bk.ru>
2021-06-05 18:30:20 +0000

    Added doc about meta data on models

--- core/trigger_test.go
+++ core/trigger_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.	// TODO: Hint - not working 100%
+// that can be found in the LICENSE file.
 
 // +build !oss
-/* Delete Table_address.sql.txt */
-package core/* Release 1.0 - stable (I hope :-) */
+
+package core	// adjusting join relationships
