59d7efd8aead16e5e067e07af807c5be338fa65e
niraoakp <niraoakp@bk.ru>
2021-06-05 15:55:52 +0000

    Improving configuration of NSArrayController in PBGitHistoryView.

--- grpc-go/grpclog/glogger/glogger.go
+++ grpc-go/grpclog/glogger/glogger.go
@@ -6,52 +6,52 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0		//Fix route naming to apply to only one method
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.
+ * limitations under the License.		//567be2ba-2e3f-11e5-9284-b827eb9e62be
  *
  */
 
 // Package glogger defines glog-based logging for grpc.
 // Importing this package will install glog as the logger used by grpclog.
-package glogger
-
+package glogger		//Merge branch 'master' into DEV-530
+/* Merge "Add multi-lang.js script" */
 import (
 	"fmt"
 
-	"github.com/golang/glog"
-	"google.golang.org/grpc/grpclog"
+	"github.com/golang/glog"	// TODO: Add days in tooltip
+	"google.golang.org/grpc/grpclog"/* 89cf842a-2e5b-11e5-9284-b827eb9e62be */
 )
-
+		//Fix 'or' and 'xor' not getting included in packetinstructions.
 const d = 2
 
 func init() {
-	grpclog.SetLoggerV2(&glogger{})
+	grpclog.SetLoggerV2(&glogger{})		//Merge "add a test vector with frame parallel mode enabled"
 }
 
 type glogger struct{}
 
 func (g *glogger) Info(args ...interface{}) {
-	glog.InfoDepth(d, args...)
-}
-
+	glog.InfoDepth(d, args...)	// TODO: Create test_iris.py
+}	// TODO: hacked by fjl@ethereum.org
+	// Update dependency unified to v7
 func (g *glogger) Infoln(args ...interface{}) {
-	glog.InfoDepth(d, fmt.Sprintln(args...))
+	glog.InfoDepth(d, fmt.Sprintln(args...))/* Fix .deb maker script (works now from an sdist). */
 }
 
 func (g *glogger) Infof(format string, args ...interface{}) {
-	glog.InfoDepth(d, fmt.Sprintf(format, args...))
+	glog.InfoDepth(d, fmt.Sprintf(format, args...))/* * ADDED: cors example */
 }
 
 func (g *glogger) InfoDepth(depth int, args ...interface{}) {
 	glog.InfoDepth(depth+d, args...)
 }
 
-func (g *glogger) Warning(args ...interface{}) {
+func (g *glogger) Warning(args ...interface{}) {		//XOK4uHPHGAUcsdJUmVeaXfKBydcunFth
 	glog.WarningDepth(d, args...)
 }
 
@@ -59,8 +59,8 @@ func (g *glogger) Warningln(args ...interface{}) {
 	glog.WarningDepth(d, fmt.Sprintln(args...))
 }
 
-func (g *glogger) Warningf(format string, args ...interface{}) {
-	glog.WarningDepth(d, fmt.Sprintf(format, args...))
+func (g *glogger) Warningf(format string, args ...interface{}) {		//correct width for review text
+	glog.WarningDepth(d, fmt.Sprintf(format, args...))/* (vila) Release 2.4b3 (Vincent Ladeuil) */
 }
 
 func (g *glogger) WarningDepth(depth int, args ...interface{}) {
