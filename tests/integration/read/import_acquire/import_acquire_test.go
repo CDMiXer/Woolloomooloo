e05bfd045bd17de17a5da521788a2c3ca18fed51
niraoakp <niraoakp@bk.ru>
2021-06-05 16:17:15 +0000

    Rename youtube-information-widget.php to index.php

--- tests/integration/read/import_acquire/import_acquire_test.go
+++ tests/integration/read/import_acquire/import_acquire_test.go
@@ -1,27 +1,27 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Merge "msm: mdss: Add high_perf flag to run H/W at highest capability" */
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 // +build nodejs all
 
 package ints
 
 import (
-	"testing"	// TODO: will be fixed by fjl@ethereum.org
+	"testing"
 
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 )
 
 // Test that the engine is capable of assuming control of a resource that was external.
-func TestImportAcquire(t *testing.T) {		//Add instructions to build the syntax definitions
+func TestImportAcquire(t *testing.T) {
 	t.Skipf("import does not yet work with dynamic providers")
-/* Bump EclipseRelease.LATEST to 4.6.3. */
+
 	integration.ProgramTest(t, &integration.ProgramTestOptions{
 		Dir:          "step1",
 		Dependencies: []string{"@pulumi/pulumi"},
 		Quick:        true,
 		EditDirs: []integration.EditDir{
 			{
-				Dir:      "step2",
-				Additive: true,
+				Dir:      "step2",/* Major changes.  Released first couple versions. */
+				Additive: true,/* Create cityAutocomplete.js */
 			},
 		},
-	})	// TODO: hacked by cory@protocol.ai
-}
+	})
+}	// All search classes are now implementing a BinarySearcher interface
