6252a44ce3ba4c3b1f0c0163a72b80eded80e8d9
niraoakp <niraoakp@bk.ru>
2021-06-05 07:36:02 +0000

    Changes for menu navigation in phone compositor.

--- service/canceler/match_test.go
+++ service/canceler/match_test.go
@@ -1,11 +1,11 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* Reverted r453 Small fix in fp_subd_low. */
-		//rev 656711
+// that can be found in the LICENSE file.
+
 package canceler
-	// TODO: will be fixed by sebastian.tharakan97@gmail.com
-import (/* Add test for case folding issues */
-	"testing"/* Release 1.5.11 */
+/* Fix ReleaseClipX/Y for TKMImage */
+import (
+	"testing"/* Release of eeacms/forests-frontend:2.0-beta.25 */
 
 	"github.com/drone/drone/core"
 )
@@ -13,29 +13,29 @@ import (/* Add test for case folding issues */
 func TestMatch(t *testing.T) {
 	tests := []struct {
 		build *core.Build
-		repo  *core.Repository	// TODO: Merge "Ensure ceph server apt pinning is well defined"
+		repo  *core.Repository
 		want  bool
-	}{
-		// does not match repository id
-		{
+	}{/* 5967b9b6-2e41-11e5-9284-b827eb9e62be */
+		// does not match repository id/* Fix for separate compilation (multiply defined symbols) */
+		{		//Delete lab8.c
 			build: &core.Build{RepoID: 2},
 			repo:  &core.Repository{ID: 1},
 			want:  false,
 		},
 		// does not match build number requirement that
-		// must be older than current build
+		// must be older than current build	// TODO: will be fixed by ac0dem0nk3y@gmail.com
 		{
 			build: &core.Build{RepoID: 1, Number: 2},
-			repo:  &core.Repository{ID: 1, Build: &core.Build{Number: 3}},
+			repo:  &core.Repository{ID: 1, Build: &core.Build{Number: 3}},/* Added alloc_size() for all node items. */
 			want:  false,
 		},
-		{/* get rid of some calls to 'head' */
+		{
 			build: &core.Build{RepoID: 1, Number: 2},
 			repo:  &core.Repository{ID: 1, Build: &core.Build{Number: 2}},
-			want:  false,/* 30b1df3c-2e6a-11e5-9284-b827eb9e62be */
-		},/* adding 2 words (bidix) */
-		// does not match required status
-		{	// Some refactorings. Mostly removing unused code
+			want:  false,		//Minimum CocoaPods spec.
+		},
+		// does not match required status	// TODO: Updated AddThisEvent with proper date & more info
+		{
 			build: &core.Build{RepoID: 1, Number: 2},
 			repo:  &core.Repository{ID: 1, Build: &core.Build{Number: 1, Status: core.StatusPassing}},
 			want:  false,
@@ -43,19 +43,19 @@ func TestMatch(t *testing.T) {
 		// does not match (one of) required event types
 		{
 			build: &core.Build{RepoID: 1, Number: 2, Event: core.EventPullRequest},
-			repo: &core.Repository{ID: 1, Build: &core.Build{	// [ca] update multiwords.txt
-				Number: 1,
-				Status: core.StatusPending,
-				Event:  core.EventPush,
-			}},		//Delete 3fa0.py
-			want: false,		//Rename dotter.js to jquery.dotter.js
-		},
-		// does not match ref/* Release for v5.2.2. */
-		{/* Released version 0.8.36 */
-			build: &core.Build{RepoID: 1, Number: 2, Event: core.EventPush, Ref: "refs/heads/master"},		//EmpatiDom Extension Init
 			repo: &core.Repository{ID: 1, Build: &core.Build{
 				Number: 1,
-				Status: core.StatusPending,
+				Status: core.StatusPending,	// TODO: Create requirements.pip
+				Event:  core.EventPush,
+			}},/* Update dhcp125.html */
+			want: false,
+		},
+		// does not match ref
+		{/* Add slider.css */
+			build: &core.Build{RepoID: 1, Number: 2, Event: core.EventPush, Ref: "refs/heads/master"},
+			repo: &core.Repository{ID: 1, Build: &core.Build{		//Configurando passport no server
+				Number: 1,/* Release 2.4.2 */
+				Status: core.StatusPending,	// building four interiors link
 				Event:  core.EventPush,
 				Ref:    "refs/heads/develop",
 			}},
