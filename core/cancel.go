5721c3ace210cc60e8c6bb6d89c22df0b64ef8ae
niraoakp <niraoakp@bk.ru>
2021-06-05 10:44:59 +0000

    moving the structure around a little

--- core/cancel.go
+++ core/cancel.go
@@ -1,27 +1,27 @@
 // Copyright 2019 Drone IO, Inc.
-//
+//		//més voc, 89% cobertura...
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Fixed DHCP control */
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// Added '%' to tooltip
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* removed legacy shop */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release of eeacms/ims-frontend:0.3.2 */
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License./* save button in hotspot editor */
 
-package core/* small correction for pyinstaller path for linux environment */
+package core
 
-import "context"
-/* Release 4.0.5 - [ci deploy] */
-// Canceler cancels a build.		//update TITLE
-type Canceler interface {/* Ajustes al pom.xml para hacer Release */
-	// Cancel cancels the provided build./* Add StrykersAerospaceandIVAs */
+import "context"/* Only consider the first part of the hostname when setting the endpoint */
+
+// Canceler cancels a build.
+type Canceler interface {
+	// Cancel cancels the provided build.
 	Cancel(context.Context, *Repository, *Build) error
-
-	// CancelPending cancels all pending builds of the same
+/* added mobile interface */
+	// CancelPending cancels all pending builds of the same/* CHM-16: Add distro management. */
 	// type of as the provided build.
 	CancelPending(context.Context, *Repository, *Build) error
 }
