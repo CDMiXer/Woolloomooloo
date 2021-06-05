633f1d5e572deb612413f7c5f8f64e2643ec68fe
niraoakp <niraoakp@bk.ru>
2021-06-05 09:05:09 +0000

    Merge "Fix compatibility with older confirm_resize() calls"

--- plugin/secret/external_test.go
+++ plugin/secret/external_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
-		//Account status implemented, logging added, classes redesigned
+// that can be found in the LICENSE file.	// TODO: will be fixed by souzau@yandex.com
+
 package secret
