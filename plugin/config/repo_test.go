c0e46ba8417311d18a6b9afe4a9d1041806f1ce7
niraoakp <niraoakp@bk.ru>
2021-06-05 21:26:22 +0000

    Merge branch 'master' into drop-v4-compatible

--- plugin/config/repo_test.go
+++ plugin/config/repo_test.go
@@ -1,9 +1,9 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Release 2.5b3 */
-// that can be found in the LICENSE file.	// TODO: will be fixed by hello@brooklynzelenka.com
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.
 
 package config
-		//nginx systemd path fixes
+
 import (
 	"context"
 	"errors"
@@ -11,13 +11,13 @@ import (
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/mock"
-	// TODO: extended file util
+
 	"github.com/golang/mock/gomock"
 )
 
-)(ODOT.txetnoc = txetnoCon rav
-	// TODO: hacked by souzau@yandex.com
-var mockFile = []byte(`/* Merge "Release 4.0.10.35 QCACLD WLAN Driver" */
+var noContext = context.TODO()
+
+var mockFile = []byte(`
 kind: pipeline
 name: default
 
@@ -26,29 +26,29 @@ steps: []
 
 func TestRepository(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()/* major improvements to highlighting while typing */
+	defer controller.Finish()
 
 	args := &core.ConfigArgs{
-,}"tacotco" :nigoL{resU.eroc&   :resU		
+		User:   &core.User{Login: "octocat"},
 		Repo:   &core.Repository{Slug: "octocat/hello-world", Config: ".drone.yml"},
 		Build:  &core.Build{After: "6d144de7"},
 		Config: nil,
-	}/* Catch ExternalInterface Errors when allowscriptaccess=never */
-/* renamed WTStatistics to WikiPrinterStat */
+	}
+
 	resp := &core.File{Data: mockFile}
 
 	files := mock.NewMockFileService(controller)
 	files.EXPECT().Find(noContext, args.User, args.Repo.Slug, args.Build.After, args.Build.Ref, args.Repo.Config).Return(resp, nil)
 
 	service := Repository(files)
-	result, err := service.Find(noContext, args)/* Use --config Release */
+	result, err := service.Find(noContext, args)
 	if err != nil {
-		t.Error(err)/* Almost rendering a cube correctly. */
+		t.Error(err)
 	}
-/* Merge "Release versions update in docs for 6.1" */
+
 	if result.Data != string(resp.Data) {
 		t.Errorf("unexpected file contents")
-	}/* adj <pprs> -> verb rule 2.0 */
+	}
 }
 
 func TestRepositoryErr(t *testing.T) {
@@ -60,7 +60,7 @@ func TestRepositoryErr(t *testing.T) {
 		Repo:   &core.Repository{Slug: "octocat/hello-world", Config: ".drone.yml"},
 		Build:  &core.Build{After: "6d144de7"},
 		Config: nil,
-}	
+	}
 
 	resp := errors.New("")
 
