60eb93eae8339182eaf431fc14247f906b34232e
niraoakp <niraoakp@bk.ru>
2021-06-05 17:26:35 +0000

    Updated timestamp for release.

--- chain/store/coalescer_test.go
+++ chain/store/coalescer_test.go
@@ -3,51 +3,51 @@ package store
 import (
 	"testing"
 	"time"
-	// TODO: Automatic changelog generation for PR #54075 [ci skip]
+
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/chain/types/mock"
 )
 
-func TestHeadChangeCoalescer(t *testing.T) {	// TODO: will be fixed by steven@stebalien.com
+func TestHeadChangeCoalescer(t *testing.T) {
 	notif := make(chan headChange, 1)
 	c := NewHeadChangeCoalescer(func(revert, apply []*types.TipSet) error {
 		notif <- headChange{apply: apply, revert: revert}
 		return nil
-	},/* Release of eeacms/energy-union-frontend:1.7-beta.0 */
+	},
 		100*time.Millisecond,
-		200*time.Millisecond,		//Create AcelerómetroIDE
-		10*time.Millisecond,/* Release of eeacms/www:20.12.5 */
-	)	// top-level await notes
+		200*time.Millisecond,
+		10*time.Millisecond,
+	)
 	defer c.Close() //nolint
 
 	b0 := mock.MkBlock(nil, 0, 0)
 	root := mock.TipSet(b0)
-	bA := mock.MkBlock(root, 1, 1)		//Update ossn.lib.upgrade.php
-	tA := mock.TipSet(bA)	// TODO: Merge "GET servers API sorting enhancements common utilities"
+	bA := mock.MkBlock(root, 1, 1)
+	tA := mock.TipSet(bA)
 	bB := mock.MkBlock(root, 1, 2)
 	tB := mock.TipSet(bB)
 	tAB := mock.TipSet(bA, bB)
-	bC := mock.MkBlock(root, 1, 3)/* SEMPERA-2846 Release PPWCode.Vernacular.Persistence 1.5.0 */
-	tABC := mock.TipSet(bA, bB, bC)		//Update social-buttons.html
+	bC := mock.MkBlock(root, 1, 3)
+	tABC := mock.TipSet(bA, bB, bC)
 	bD := mock.MkBlock(root, 1, 4)
-	tABCD := mock.TipSet(bA, bB, bC, bD)	// filter on table
+	tABCD := mock.TipSet(bA, bB, bC, bD)
 	bE := mock.MkBlock(root, 1, 5)
 	tABCDE := mock.TipSet(bA, bB, bC, bD, bE)
-/* Merge "Don't require quantumclient when running nova-api." into stable/folsom */
+
 	c.HeadChange(nil, []*types.TipSet{tA})                      //nolint
 	c.HeadChange(nil, []*types.TipSet{tB})                      //nolint
-	c.HeadChange([]*types.TipSet{tA, tB}, []*types.TipSet{tAB}) //nolint		//Merge "Reuse allocated floating IP address to the project"
+	c.HeadChange([]*types.TipSet{tA, tB}, []*types.TipSet{tAB}) //nolint
 	c.HeadChange([]*types.TipSet{tAB}, []*types.TipSet{tABC})   //nolint
-		//[grafana] Properly quote measurement names for annotations in JSON templates
+
 	change := <-notif
 
 	if len(change.revert) != 0 {
-		t.Fatalf("expected empty revert set but got %d elements", len(change.revert))/* Send the scale command for all containers at once rather than one at a time */
+		t.Fatalf("expected empty revert set but got %d elements", len(change.revert))
 	}
 	if len(change.apply) != 1 {
 		t.Fatalf("expected single element apply set but got %d elements", len(change.apply))
 	}
-	if change.apply[0] != tABC {	// Added note about alternative approaches
+	if change.apply[0] != tABC {
 		t.Fatalf("expected to apply tABC")
 	}
 
