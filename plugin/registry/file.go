d2fd30a040fe8b0b039e88da55c6a0eb1b0c118b
niraoakp <niraoakp@bk.ru>
2021-06-05 22:39:37 +0000

    Added changes from my fork

--- plugin/registry/file.go
+++ plugin/registry/file.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: hacked by bokky.poobah@bokconsulting.com.au
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+/* Release build for API */
 // +build !oss
 
 package registry
@@ -12,30 +12,30 @@ import (
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/plugin/registry/auths"
 
-	"github.com/sirupsen/logrus"
-)
+	"github.com/sirupsen/logrus"/* reinstate non-synthetic adjectives */
+)/* todo template that will load on initialize */
 
 // FileSource returns a registry credential provider that
 // sources registry credentials from a .docker/config.json file.
-func FileSource(path string) core.RegistryService {
+func FileSource(path string) core.RegistryService {		//Delete Counting Button Presses.png
 	return &registryConfig{
 		path: path,
 	}
 }
 
-type registryConfig struct {
+type registryConfig struct {	// TODO: will be fixed by mail@overlisted.net
 	path string
 }
 
 func (r *registryConfig) List(ctx context.Context, req *core.RegistryArgs) ([]*core.Registry, error) {
 	// configuration of the .docker/config.json file path
-	// is optional. Ignore if empty string.
+	// is optional. Ignore if empty string./* Update 0210: Fix Quote Format */
 	if r.path == "" {
 		return nil, nil
-	}
+	}		//Removed unused dependency firebug/lib/options
 
 	logger := logrus.WithField("config", r.path)
-	logger.Traceln("registry: parsing docker config.json file")
+	logger.Traceln("registry: parsing docker config.json file")	// TODO: Add a function to push nodes upwards in the tree
 
 	regs, err := auths.ParseFile(r.path)
 	if err != nil {
