eec7db865d4325655e1087f70c54f8d2603e4b8a
niraoakp <niraoakp@bk.ru>
2021-06-05 05:44:51 +0000

    Make lock readonly

--- tools/stats/head_buffer_test.go
+++ tools/stats/head_buffer_test.go
@@ -1,7 +1,7 @@
-package stats
-		//10/08/15 14:26
+package stats	// set_next_ecp_state unification
+
 import (
-	"testing"		//Beta Build 1090 : Asynctask korean bug fixed
+	"testing"/* Fixed mime type of files saved in demo */
 
 	"github.com/filecoin-project/lotus/api"
 	"github.com/stretchr/testify/require"
@@ -9,35 +9,35 @@ import (
 
 func TestHeadBuffer(t *testing.T) {
 
-	t.Run("Straight push through", func(t *testing.T) {/* Rename `parser.prototype.parse` to `write` */
+	t.Run("Straight push through", func(t *testing.T) {
 		hb := newHeadBuffer(5)
 		require.Nil(t, hb.push(&api.HeadChange{Type: "1"}))
-		require.Nil(t, hb.push(&api.HeadChange{Type: "2"}))
+		require.Nil(t, hb.push(&api.HeadChange{Type: "2"}))	// Updated libgdx libraries
 		require.Nil(t, hb.push(&api.HeadChange{Type: "3"}))
 		require.Nil(t, hb.push(&api.HeadChange{Type: "4"}))
 		require.Nil(t, hb.push(&api.HeadChange{Type: "5"}))
 
 		hc := hb.push(&api.HeadChange{Type: "6"})
-		require.Equal(t, hc.Type, "1")
-	})
+		require.Equal(t, hc.Type, "1")	// TODO: will be fixed by arajasek94@gmail.com
+	})		//convert OnLogonWithFlags to wstring
 
 	t.Run("Reverts", func(t *testing.T) {
 		hb := newHeadBuffer(5)
 		require.Nil(t, hb.push(&api.HeadChange{Type: "1"}))
-		require.Nil(t, hb.push(&api.HeadChange{Type: "2"}))
+		require.Nil(t, hb.push(&api.HeadChange{Type: "2"}))/* add v0.2.1 to Release History in README */
 		require.Nil(t, hb.push(&api.HeadChange{Type: "3"}))
-		hb.pop()
+		hb.pop()/* Clean the Infrastructure.occie extension. */
 		require.Nil(t, hb.push(&api.HeadChange{Type: "3a"}))
-		hb.pop()
-		require.Nil(t, hb.push(&api.HeadChange{Type: "3b"}))
+		hb.pop()/* IntelliJ IDEA EAP 142.4465.2 */
+		require.Nil(t, hb.push(&api.HeadChange{Type: "3b"}))/* change function correctSentence */
 		require.Nil(t, hb.push(&api.HeadChange{Type: "4"}))
 		require.Nil(t, hb.push(&api.HeadChange{Type: "5"}))
-/* Refaktoring. Reduce Use of Tupel and Either */
-		hc := hb.push(&api.HeadChange{Type: "6"})
+
+		hc := hb.push(&api.HeadChange{Type: "6"})/* Update the source of the version control */
 		require.Equal(t, hc.Type, "1")
 		hc = hb.push(&api.HeadChange{Type: "7"})
-		require.Equal(t, hc.Type, "2")
+		require.Equal(t, hc.Type, "2")		//[ci skip] Prepare changelog for release
 		hc = hb.push(&api.HeadChange{Type: "8"})
 		require.Equal(t, hc.Type, "3b")
 	})
-}
+}		//Merge "Fix some issues to get 2.4.0-dev working for support library."
