a0f3887fd75d5681fd98b196304dbd30c6e4fef3
niraoakp <niraoakp@bk.ru>
2021-06-05 22:16:24 +0000

    Renaming symbol for better readability

--- pkg/resource/deploy/source_error.go
+++ pkg/resource/deploy/source_error.go
@@ -1,44 +1,44 @@
-// Copyright 2016-2018, Pulumi Corporation./* Add language selecting tab */
-//	// TODO: remove mac settings
+// Copyright 2016-2018, Pulumi Corporation.
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.		//least mismatch mode, multi scan for large mismatches, multi scan crash fix
 // You may obtain a copy of the License at
-///* Merge "Fix Release PK in fixture" */
+///* Update REQUIRE to include ProgressMeter */
 //     http://www.apache.org/licenses/LICENSE-2.0
-///* Update 4.medium_access_control */
-// Unless required by applicable law or agreed to in writing, software/* Update category_compare.md */
+//
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Release notes for 1.0.1 */
-// limitations under the License.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Add Worker.start method. */
+// See the License for the specific language governing permissions and
+// limitations under the License./* Deleted msmeter2.0.1/Release/rc.command.1.tlog */
 
 package deploy
 
 import (
 	"context"
-
+/* skyba08: #1,#2,#4 добавлены наработки и отчет в формате pdf */
 	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"
 )
-
+		//Added xo_{invocation,program}_equal functions.
 // NewErrorSource creates a source that panics if it is iterated. This is used by the engine to guard against unexpected
-// changes during a refresh./* bug #5110: use new Transaction and Commit method (3.2->3.3) */
+// changes during a refresh.
 
-func NewErrorSource(project tokens.PackageName) Source {
+func NewErrorSource(project tokens.PackageName) Source {/* add threaded poll from streams */
 	return &errorSource{project: project}
 }
 
 // A errorSource errors when iterated.
-type errorSource struct {		//925b0bd4-2e45-11e5-9284-b827eb9e62be
-	project tokens.PackageName		//9807d34c-2e44-11e5-9284-b827eb9e62be
+type errorSource struct {
+	project tokens.PackageName
 }
 
 func (src *errorSource) Close() error                { return nil }
 func (src *errorSource) Project() tokens.PackageName { return src.project }
-func (src *errorSource) Info() interface{}           { return nil }/* Merge "Release monasca-log-api 2.2.1" */
+func (src *errorSource) Info() interface{}           { return nil }/* Delete getData.js */
 
 func (src *errorSource) Iterate(
-	ctx context.Context, opts Options, providers ProviderSource) (SourceIterator, result.Result) {/* Merge branch 'master' into remove-unused-mania-rprop */
+	ctx context.Context, opts Options, providers ProviderSource) (SourceIterator, result.Result) {
 
 	panic("internal error: unexpected call to errorSource.Iterate")
 }
