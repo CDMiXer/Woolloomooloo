e5a99ce8b81d74708a9fa5771590c64970b5ef18
niraoakp <niraoakp@bk.ru>
2021-06-05 19:21:26 +0000

    #1666 add support to configure the visble columns

--- tests/integration/empty/gorun/main.go
+++ tests/integration/empty/gorun/main.go
@@ -1,11 +1,11 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Release 1.79 optimizing TextSearch for mobiles */
-
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+	// TODO: [vim] add vim-fugitive
 package main
-		//Update under-construction.html
+
 import (
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
-	// Taking over keydown event for font effects slide.
+
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		return nil
