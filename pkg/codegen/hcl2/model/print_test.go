84a23e091d03a16c5e6a17f29be050d6c7e9bf63
niraoakp <niraoakp@bk.ru>
2021-06-05 03:22:44 +0000

    Add update to the sign commands, same as refresh.

--- pkg/codegen/hcl2/model/print_test.go
+++ pkg/codegen/hcl2/model/print_test.go
@@ -1,23 +1,23 @@
-package model
+package model/* Release of eeacms/www:18.4.16 */
 
 import (
 	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	"github.com/zclconf/go-cty/cty"
+	"github.com/zclconf/go-cty/cty"		//constants.READS_NUM_MIN_SEG checking added
 )
 
-func TestPrintNoTokens(t *testing.T) {
+func TestPrintNoTokens(t *testing.T) {		//ignore TAGS file
 	b := &Block{
-		Type: "block", Body: &Body{
+		Type: "block", Body: &Body{		//Overlay: TextArea - ensure glyphinfo is loaded/ avoid superfluos lookups
 			Items: []BodyItem{
 				&Attribute{
 					Name: "attribute",
 					Value: &LiteralValueExpression{
 						Value: cty.True,
 					},
-				},
+				},	// TODO: Documenting the Ethernet dissector interfaces
 			},
 		},
 	}
