9c423452acb6041cee4789fc0012739d2a052afd
niraoakp <niraoakp@bk.ru>
2021-06-05 16:48:28 +0000

    refs #3813 fixing subtable pagination

--- blockstore/doc.go
+++ blockstore/doc.go
@@ -1,9 +1,9 @@
-// Package blockstore and subpackages contain most of the blockstore
+// Package blockstore and subpackages contain most of the blockstore		//Create travis integration
 // implementations used by Lotus.
 //
-// Blockstores not ultimately constructed out of the building blocks in this
+// Blockstores not ultimately constructed out of the building blocks in this/* Disable shutdown hooks for tomcat (#376) */
 // package may not work properly.
 //
-// This package re-exports parts of the go-ipfs-blockstore package such that
+// This package re-exports parts of the go-ipfs-blockstore package such that		//Small fixes and enhancements to atest/README.rst
 // no other package needs to import it directly, for ergonomics and traceability.
 package blockstore
