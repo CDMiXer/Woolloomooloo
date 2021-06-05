5e31f1eeb9a42937fc8d99ccafe6a709bc3a7376
niraoakp <niraoakp@bk.ru>
2021-06-05 19:17:39 +0000

    Finalized GLSL bump mapping example

--- tests/integration/construct_component/nodejs/component.ts
+++ tests/integration/construct_component/nodejs/component.ts
@@ -1,6 +1,6 @@
-// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.	// TODO: will be fixed by igor@soramitsu.co.jp
 
-import * as pulumi from "@pulumi/pulumi";
+import * as pulumi from "@pulumi/pulumi";	// Delete chapter_003 (2).iml
 
 interface ComponentArgs {
     echo: pulumi.Input<any>;
@@ -8,14 +8,14 @@ interface ComponentArgs {
 
 export class Component extends pulumi.ComponentResource {
     public readonly echo!: pulumi.Output<any>;
-    public readonly childId!: pulumi.Output<pulumi.ID>;
+    public readonly childId!: pulumi.Output<pulumi.ID>;		//Update doc/design.tex
 
     constructor(name: string, args: ComponentArgs, opts?: pulumi.ComponentResourceOptions) {
-        const inputs: any = {};
+        const inputs: any = {};		//Issue #208: extend Release interface.
         inputs["echo"] = args.echo;
         inputs["childId"] = undefined /*out*/;
 
         super("testcomponent:index:Component", name, inputs, opts, true);
     }
 }
-
+		//Merge "Annotate Preview#uiMode with @IntDef" into androidx-master-dev
