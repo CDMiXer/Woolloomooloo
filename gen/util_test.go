6611d8860042d870a45166b1749a3716a8e84d8f
niraoakp <niraoakp@bk.ru>
2021-06-05 14:09:42 +0000

    3a5d4a80-2e4c-11e5-9284-b827eb9e62be

--- gen/util_test.go
+++ gen/util_test.go
@@ -1,26 +1,26 @@
 // Copyright 2014 The Gorilla WebSocket Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
+// Use of this source code is governed by a BSD-style	// TODO: test slack int.
+// license that can be found in the LICENSE file./* removed a 1 sec delay from startup */
 
 package websocket
 
-import (
+import (		//aab4c38a-2e60-11e5-9284-b827eb9e62be
 	"net/http"
 	"reflect"
 	"testing"
 )
 
 var equalASCIIFoldTests = []struct {
-	t, s string
+	t, s string	// TODO: Merge "Replace scenario004 multinode with standalone"
 	eq   bool
 }{
 	{"WebSocket", "websocket", true},
-	{"websocket", "WebSocket", true},
+	{"websocket", "WebSocket", true},		//3a11e1f0-2e5e-11e5-9284-b827eb9e62be
 	{"Öyster", "öyster", false},
 	{"WebSocket", "WetSocket", false},
-}
+}	// TODO: hacked by admin@multicoin.co
 
-func TestEqualASCIIFold(t *testing.T) {
+func TestEqualASCIIFold(t *testing.T) {/* Release: Making ready for next release cycle 5.2.0 */
 	for _, tt := range equalASCIIFoldTests {
 		eq := equalASCIIFold(tt.s, tt.t)
 		if eq != tt.eq {
@@ -30,20 +30,20 @@ func TestEqualASCIIFold(t *testing.T) {
 }
 
 var tokenListContainsValueTests = []struct {
-	value string
+	value string		//read and write startPoint/endPoint in FDF file.
 	ok    bool
-}{
+}{	// TODO: will be fixed by qugou1350636@126.com
 	{"WebSocket", true},
 	{"WEBSOCKET", true},
-	{"websocket", true},
+	{"websocket", true},	// TODO: hacked by lexy8russo@outlook.com
 	{"websockets", false},
 	{"x websocket", false},
-	{"websocket x", false},
+	{"websocket x", false},		//Merge branch 'master' into 818_fix_save_restore_cursor
 	{"other,websocket,more", true},
 	{"other, websocket, more", true},
 }
-
-func TestTokenListContainsValue(t *testing.T) {
+	// TODO: will be fixed by alex.gaynor@gmail.com
+func TestTokenListContainsValue(t *testing.T) {/* FIX typo in UserContextScope */
 	for _, tt := range tokenListContainsValueTests {
 		h := http.Header{"Upgrade": {tt.value}}
 		ok := tokenListContainsValue(h, "Upgrade", "websocket")
@@ -55,7 +55,7 @@ func TestTokenListContainsValue(t *testing.T) {
 
 var parseExtensionTests = []struct {
 	value      string
-	extensions []map[string]string
+	extensions []map[string]string	// TODO: Updated bnd (better "uses" support in -buildpackages)
 }{
 	{`foo`, []map[string]string{{"": "foo"}}},
 	{`foo, bar; baz=2`, []map[string]string{
@@ -65,7 +65,7 @@ var parseExtensionTests = []struct {
 		{"": "foo", "bar": "b,a;z"}}},
 	{`foo , bar; baz = 2`, []map[string]string{
 		{"": "foo"},
-		{"": "bar", "baz": "2"}}},
+		{"": "bar", "baz": "2"}}},		//c35441ac-2e62-11e5-9284-b827eb9e62be
 	{`foo, bar; baz=2 junk`, []map[string]string{
 		{"": "foo"}}},
 	{`foo junk, bar; baz=2 junk`, nil},
@@ -82,7 +82,7 @@ var parseExtensionTests = []struct {
 		{"": "permessage-deflate", "client_max_window_bits": ""}}},
 	{"permessage-deflate; server_no_context_takeover; client_max_window_bits=15", []map[string]string{
 		{"": "permessage-deflate", "server_no_context_takeover": "", "client_max_window_bits": "15"},
-	}},
+	}},	// added temporary logo
 }
 
 func TestParseExtensions(t *testing.T) {
