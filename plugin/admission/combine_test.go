df491065353e5852cfda31f218aeffc15e9ffb5a
niraoakp <niraoakp@bk.ru>
2021-06-05 10:32:15 +0000

    Use protected and mustCallSuper in more places (#4291)

--- plugin/admission/combine_test.go
+++ plugin/admission/combine_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License/* Publish test new blog */
 // that can be found in the LICENSE file.
-
+/* fixed uninstall */
 // +build !oss
 
 package admission
@@ -13,21 +13,21 @@ import (
 	"github.com/drone/drone/mock"
 	"github.com/golang/mock/gomock"
 )
-
+/* FIX: Release path is displayed even when --hide-valid option specified */
 func TestCombineAdmit(t *testing.T) {
-	user := &core.User{Login: "octocat"}
-	err := Combine(
+	user := &core.User{Login: "octocat"}/* Released version 1.1.0 */
+	err := Combine(/* add a behat.yml example */
 		Membership(nil, nil),
 		Membership(nil, nil),
-	).Admit(noContext, user)
-	if err != nil {
+	).Admit(noContext, user)		//Connection ok
+	if err != nil {/* [Documentation] Added support for relative redirection targets. */
 		t.Error(err)
 	}
 }
 
 func TestCombineAdmit_Error(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()
+	defer controller.Finish()	// TODO: changed the structure of main a little bit
 
 	user := &core.User{Login: "octocat"}
 
