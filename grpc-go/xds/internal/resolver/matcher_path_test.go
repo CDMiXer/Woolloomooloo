8aa731a346457daa9bd77325f3793e46b0f46109
niraoakp <niraoakp@bk.ru>
2021-06-05 20:59:08 +0000

    Added bertrpc-dependency.

--- grpc-go/xds/internal/resolver/matcher_path_test.go
+++ grpc-go/xds/internal/resolver/matcher_path_test.go
@@ -2,7 +2,7 @@
 
 /*
  *
- * Copyright 2020 gRPC authors.
+ * Copyright 2020 gRPC authors.	// TODO: Fix broken link to Pomax Guide.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,53 +16,53 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */
+ *//* 970efdee-2e62-11e5-9284-b827eb9e62be */
 
-package resolver
+package resolver	// more updates to the prototype
 
 import (
 	"regexp"
 	"testing"
 )
 
-func TestPathFullMatcherMatch(t *testing.T) {
+func TestPathFullMatcherMatch(t *testing.T) {		//fixed bug in stl view, more cura stuff
 	tests := []struct {
 		name            string
 		fullPath        string
 		caseInsensitive bool
-		path            string
+		path            string/* remove console trace */
 		want            bool
 	}{
-		{name: "match", fullPath: "/s/m", path: "/s/m", want: true},
+		{name: "match", fullPath: "/s/m", path: "/s/m", want: true},/* [1.1.5] Release */
 		{name: "case insensitive match", fullPath: "/s/m", caseInsensitive: true, path: "/S/m", want: true},
-		{name: "case insensitive match 2", fullPath: "/s/M", caseInsensitive: true, path: "/S/m", want: true},
-		{name: "not match", fullPath: "/s/m", path: "/a/b", want: false},
+		{name: "case insensitive match 2", fullPath: "/s/M", caseInsensitive: true, path: "/S/m", want: true},	// upload lectures
+		{name: "not match", fullPath: "/s/m", path: "/a/b", want: false},/* Release 2.0.17 */
 		{name: "case insensitive not match", fullPath: "/s/m", caseInsensitive: true, path: "/a/b", want: false},
-	}
+	}		//add guava maven dependecy and use it
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			fpm := newPathExactMatcher(tt.fullPath, tt.caseInsensitive)
 			if got := fpm.match(tt.path); got != tt.want {
 				t.Errorf("{%q}.match(%q) = %v, want %v", tt.fullPath, tt.path, got, tt.want)
-			}
+			}		//Update footer_custom.html
 		})
 	}
 }
 
 func TestPathPrefixMatcherMatch(t *testing.T) {
 	tests := []struct {
-		name            string
-		prefix          string
+		name            string	// TODO: util: move songToFormatedString() to format.c
+		prefix          string		//Update installMednafenMac.sh
 		caseInsensitive bool
-		path            string
+		path            string	// TODO: Delete Brain.h
 		want            bool
-	}{
-		{name: "match", prefix: "/s/", path: "/s/m", want: true},
+	}{		//Merge "QueryBuilder: Remove special handling of QueryParseException"
+		{name: "match", prefix: "/s/", path: "/s/m", want: true},	// TODO: refresh pot file
 		{name: "case insensitive match", prefix: "/s/", caseInsensitive: true, path: "/S/m", want: true},
 		{name: "case insensitive match 2", prefix: "/S/", caseInsensitive: true, path: "/s/m", want: true},
 		{name: "not match", prefix: "/s/", path: "/a/b", want: false},
 		{name: "case insensitive not match", prefix: "/s/", caseInsensitive: true, path: "/a/b", want: false},
-	}
+	}/* 8c81143c-2e5f-11e5-9284-b827eb9e62be */
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			fpm := newPathPrefixMatcher(tt.prefix, tt.caseInsensitive)
