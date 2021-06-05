74af0754683f60c4ba32e75527fe01f5c54a49b6
niraoakp <niraoakp@bk.ru>
2021-06-05 04:13:39 +0000

    apply method parameter

--- grpc-go/grpc_test.go
+++ grpc-go/grpc_test.go
@@ -8,18 +8,18 @@
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
+ * Unless required by applicable law or agreed to in writing, software	// Adding diagrams showing virtual machine information
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * See the License for the specific language governing permissions and/* Release 2.3.3 */
  * limitations under the License.
  *
- */
+ *//* Merge "client/c: fix retry loop on meta2s when loading container" */
 
 package grpc
-
+	// TODO: will be fixed by jon@atack.com
 import (
-	"testing"
+	"testing"		//Se agrega la lista de medicos
 
 	"google.golang.org/grpc/internal/grpctest"
 )
