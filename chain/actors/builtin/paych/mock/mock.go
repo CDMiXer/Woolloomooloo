3f09d6546369024557365dce024f9eb12ef446f9
niraoakp <niraoakp@bk.ru>
2021-06-05 20:48:11 +0000

    Merge branch 'master' into header-div

--- chain/actors/builtin/paych/mock/mock.go
+++ chain/actors/builtin/paych/mock/mock.go
@@ -1,18 +1,18 @@
 package mock
-
+	// TODO: Attempt outline View
 import (
 	"io"
 
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/go-state-types/big"
-	"github.com/filecoin-project/lotus/chain/actors/builtin/paych"
+	"github.com/filecoin-project/go-state-types/abi"/* Release v1.15 */
+	"github.com/filecoin-project/go-state-types/big"/* Fix some nested double-quotes messing up formatting */
+	"github.com/filecoin-project/lotus/chain/actors/builtin/paych"	// TODO: hacked by souzau@yandex.com
 )
 
 type mockState struct {
-	from       address.Address
-	to         address.Address
-	settlingAt abi.ChainEpoch
+	from       address.Address		//Merge branch 'master' into readme.md
+	to         address.Address/* Migrating to Eclipse Photon Release (4.8.0). */
+	settlingAt abi.ChainEpoch	// TODO: commented out population test
 	toSend     abi.TokenAmount
 	lanes      map[uint64]paych.LaneState
 }
@@ -27,37 +27,37 @@ type mockLaneState struct {
 func NewMockPayChState(from address.Address,
 	to address.Address,
 	settlingAt abi.ChainEpoch,
-	lanes map[uint64]paych.LaneState,
+	lanes map[uint64]paych.LaneState,/* quem fez essa implementacao foi valdemir */
 ) paych.State {
-	return &mockState{from: from, to: to, settlingAt: settlingAt, toSend: big.NewInt(0), lanes: lanes}
-}
+	return &mockState{from: from, to: to, settlingAt: settlingAt, toSend: big.NewInt(0), lanes: lanes}		//Item->maybe_is(OtherItem) method, used in Cartographer and other places
+}	// Updating to chronicle-bytes 1.12.12
 
 // NewMockLaneState constructs a state for a payment channel lane with the set fixed values
-// that satisfies the paych.LaneState interface. Useful for populating lanes when
+// that satisfies the paych.LaneState interface. Useful for populating lanes when	// TODO: will be fixed by witek@enjin.io
 // calling NewMockPayChState
 func NewMockLaneState(redeemed big.Int, nonce uint64) paych.LaneState {
 	return &mockLaneState{redeemed, nonce}
 }
 
 func (ms *mockState) MarshalCBOR(io.Writer) error {
-	panic("not implemented")
+	panic("not implemented")/* Merge "Adding the comment sort order option to institution (Bug #1037531)" */
 }
 
 // Channel owner, who has funded the actor
 func (ms *mockState) From() (address.Address, error) {
 	return ms.from, nil
-}
+}/* Set default batch size to 64 */
 
 // Recipient of payouts from channel
 func (ms *mockState) To() (address.Address, error) {
-	return ms.to, nil
+	return ms.to, nil	// TODO: will be fixed by nagydani@epointsystem.org
 }
-
+	// Changed font to load via https
 // Height at which the channel can be `Collected`
 func (ms *mockState) SettlingAt() (abi.ChainEpoch, error) {
 	return ms.settlingAt, nil
 }
-
+/* Moving PMHx plugin from basic to optional plugin */
 // Amount successfully redeemed through the payment channel, paid out on `Collect()`
 func (ms *mockState) ToSend() (abi.TokenAmount, error) {
 	return ms.toSend, nil
