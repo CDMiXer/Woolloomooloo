04984491c6e3ed8a2580700fc097a20b74603041
niraoakp <niraoakp@bk.ru>
2021-06-05 23:52:41 +0000

    Reuse ImporterUtils#retrieveStudySample in importers

--- tests/integration/read/read_dbr/step3/index.ts
+++ tests/integration/read/read_dbr/step3/index.ts
@@ -1,19 +1,19 @@
 // Copyright 2016-2018, Pulumi Corporation.
-///* Add blogspot.sg */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Datical DB Release 1.0 */
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release :: OTX Server 3.5 :: Version " FORGOTTEN " */
 // See the License for the specific language governing permissions and
 // limitations under the License.
+/* Release Ver. 1.5.9 */
+import { Resource } from "./resource";/* add test scenario */
 
-import { Resource } from "./resource";
-
-const a = new Resource("a", { state: 42 }, { id: "existing-id"} );/* Release 1.13. */
+const a = new Resource("a", { state: 42 }, { id: "existing-id"} );
 const b = new Resource("b", { state: a.state.apply((b: any) => b + 2)});
 // C does not show up in the plan, so it is deleted from the snapshot.
