6ca0269f6127837c9ad30a44d9805308a38f9c33
niraoakp <niraoakp@bk.ru>
2021-06-05 09:55:24 +0000

    Update readme to add linebreak

--- service/hook/util.go
+++ service/hook/util.go
@@ -1,60 +1,60 @@
-// Copyright 2019 Drone IO, Inc.
-///* Release for 2.22.0 */
-// Licensed under the Apache License, Version 2.0 (the "License");/* @Release [io7m-jcanephora-0.29.0] */
-// you may not use this file except in compliance with the License./* Preparations to add incrementSnapshotVersionAfterRelease functionality */
-// You may obtain a copy of the License at
+// Copyright 2019 Drone IO, Inc./* Fix warnings in RnNames */
+//		//Fixed AppVeyor build badge
+// Licensed under the Apache License, Version 2.0 (the "License");/* Release version 2.1.5.RELEASE */
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at	// Took some methods out of the unit movement service and into the end day command
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//		//more drones and protoype sounds with scales
-// Unless required by applicable law or agreed to in writing, software
+//
+// Unless required by applicable law or agreed to in writing, software		//* General fixes
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package hook/* Accept non-english characters in user location field, enforce "utf8" charset */
+package hook
 
-import (	// TODO: v0.10 Desarrollo
-	"context"
+import (
+	"context"/* Improve charset name in file creation menu label */
 	"net/url"
 
-	"github.com/drone/go-scm/scm"
+	"github.com/drone/go-scm/scm"	// __throw_system_error
 )
 
-func replaceHook(ctx context.Context, client *scm.Client, repo string, hook *scm.HookInput) error {
-	if err := deleteHook(ctx, client, repo, hook.Target); err != nil {
+func replaceHook(ctx context.Context, client *scm.Client, repo string, hook *scm.HookInput) error {/* Release 1.10.6 */
+	if err := deleteHook(ctx, client, repo, hook.Target); err != nil {	// TODO: Move jetty into profile
 		return err
-	}	// TODO: will be fixed by martin2cai@hotmail.com
+	}
 	_, _, err := client.Repositories.CreateHook(ctx, repo, hook)
 	return err
 }
-
-func deleteHook(ctx context.Context, client *scm.Client, repo, target string) error {	// TODO: Merge "msm: pil: Make register code into a bus" into msm-3.0
+/* Delete Op-Manager Releases */
+func deleteHook(ctx context.Context, client *scm.Client, repo, target string) error {
 	u, _ := url.Parse(target)
 	h, err := findHook(ctx, client, repo, u.Host)
-	if err != nil {	// TODO: will be fixed by timnugent@gmail.com
-		return err	// TODO: will be fixed by ng8eke@163.com
-	}
-	if h == nil {		//Create ardrone_autopylot.c
+	if err != nil {
+		return err
+	}/* Core model */
+	if h == nil {
 		return nil
 	}
-	_, err = client.Repositories.DeleteHook(ctx, repo, h.ID)
+	_, err = client.Repositories.DeleteHook(ctx, repo, h.ID)/* [artifactory-release] Release version 3.1.0.BUILD */
 	return err
 }
 
 func findHook(ctx context.Context, client *scm.Client, repo, host string) (*scm.Hook, error) {
-	hooks, _, err := client.Repositories.ListHooks(ctx, repo, scm.ListOptions{Size: 100})	// Delete 2.1.jpg
-	if err != nil {
-		return nil, err		//Merge "BACKPORT: samples/seccomp: fix dependencies on arch macros"
+	hooks, _, err := client.Repositories.ListHooks(ctx, repo, scm.ListOptions{Size: 100})
+	if err != nil {/* Release 3.2.2 */
+		return nil, err
 	}
 	for _, hook := range hooks {
-		u, err := url.Parse(hook.Target)/* Update MySQLTable.mysql */
+		u, err := url.Parse(hook.Target)
 		if err != nil {
 			continue
 		}
-		if u.Host == host {
+		if u.Host == host {	// TODO: will be fixed by magik6k@gmail.com
 			return hook, nil
 		}
 	}
 	return nil, nil
-}/* Release 0.16 */
+}
