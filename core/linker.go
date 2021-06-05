ac1b5a5e12d88f45294e88b32a35d193a581f0b9
niraoakp <niraoakp@bk.ru>
2021-06-05 03:32:20 +0000

    Merge "Change the order of HealthCheck tests"

--- core/linker.go
+++ core/linker.go
@@ -5,15 +5,15 @@
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+///* Tagging as 0.9 (Release: 0.9) */
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and/* 148e923e-2e72-11e5-9284-b827eb9e62be */
 // limitations under the License.
 
 package core
-
+	// update v0 with mockup
 import "context"
 
 // Linker provides a deep link to to a git resource in the
