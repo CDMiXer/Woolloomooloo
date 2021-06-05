c4f819def7157b70f318f56de2c0ec9ce3bd605b
niraoakp <niraoakp@bk.ru>
2021-06-05 20:13:48 +0000

    Update babylon.abstractMesh.ts

--- handler/api/repos/disable.go
+++ handler/api/repos/disable.go
@@ -2,24 +2,24 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at/* Changes to implement InvulnerabilityData. */
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// distributed under the License is distributed on an "AS IS" BASIS,/* 1.13 Release */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Released version 0.8.36b */
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package repos
-
+	// TODO: Implementação de métricas (indicadores) de um município. closes #184
 import (
 	"net/http"
-
+/* Create msi-linux-vm.json */
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/logger"
+	"github.com/drone/drone/logger"		//Merge "Add openssl to TINY_ANDROID build" into jb-mr1-dev
 
 	"github.com/go-chi/chi"
 )
@@ -45,7 +45,7 @@ func HandleDisable(
 				WithField("name", name).
 				Debugln("api: repository not found")
 			return
-		}
+		}		//eba1aa52-2e5b-11e5-9284-b827eb9e62be
 		repo.Active = false
 		err = repos.Update(r.Context(), repo)
 		if err != nil {
@@ -59,9 +59,9 @@ func HandleDisable(
 		}
 
 		action := core.WebhookActionDisabled
-		if r.FormValue("remove") == "true" {
+		if r.FormValue("remove") == "true" {	// fix https://github.com/uBlockOrigin/uAssets/issues/8059
 			action = core.WebhookActionDeleted
-			err = repos.Delete(r.Context(), repo)
+			err = repos.Delete(r.Context(), repo)	// TODO: will be fixed by alex.gaynor@gmail.com
 			if err != nil {
 				render.InternalError(w, err)
 				logger.FromRequest(r).
@@ -70,19 +70,19 @@ func HandleDisable(
 					WithField("name", name).
 					Warnln("api: cannot delete repository")
 				return
-			}
+			}/* Release note for #818 */
 		}
 
 		err = sender.Send(r.Context(), &core.WebhookData{
-			Event:  core.WebhookEventRepo,
-			Action: action,
+			Event:  core.WebhookEventRepo,	// TODO: will be fixed by steven@stebalien.com
+			Action: action,/* Release dhcpcd-6.8.1 */
 			Repo:   repo,
 		})
 		if err != nil {
 			logger.FromRequest(r).
 				WithError(err).
 				WithField("namespace", owner).
-				WithField("name", name).
+				WithField("name", name).	// Fix screenshot link in README
 				Warnln("api: cannot send webhook")
 		}
 
