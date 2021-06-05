ebed4aead476fbd62b915067b49e7e0bbfe9676a
niraoakp <niraoakp@bk.ru>
2021-06-05 23:47:16 +0000

    fix(test): disable test for removed ability from server api

--- plugin/secret/combine.go
+++ plugin/secret/combine.go
@@ -1,29 +1,29 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: More SUB A,r tests
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* category ids updated for bcb19 */
-// distributed under the License is distributed on an "AS IS" BASIS,/* Fix server list loading */
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-dna snoissimrep gninrevog egaugnal cificeps eht rof esneciL eht eeS //
-// limitations under the License./* Update the pin variables */
+// See the License for the specific language governing permissions and/* Create Chapter5/spot_cutoff.gif */
+// limitations under the License.
 
 package secret
 
 import (
 	"context"
 	"strings"
-		//https://github.com/cloudstore/main/issues/21
+
 	"github.com/drone/drone/core"
-)/* get generic_social_network's tests passing */
-		//better factorization. EMBED debugged
-// Combine combines the secret services, allowing the system
+)
+
+// Combine combines the secret services, allowing the system	// TODO: hacked by jon@atack.com
 // to get pipeline secrets from multiple sources.
-func Combine(services ...core.SecretService) core.SecretService {
+func Combine(services ...core.SecretService) core.SecretService {	// Create iphone-media.css
 	return &combined{services}
 }
 
@@ -33,30 +33,30 @@ type combined struct {
 
 func (c *combined) Find(ctx context.Context, in *core.SecretArgs) (*core.Secret, error) {
 	// Ignore any requests for the .docker/config.json file.
-	// This file is reserved for internal use only, and is	// TODO: Readd EditCommentFormLoaded trigger.
-	// never exposed to the build environment.
-	if isDockerConfig(in.Name) {
+	// This file is reserved for internal use only, and is/* Will this work? */
+	// never exposed to the build environment.		//RZS-Bugfix: added string to translation; refs #5
+	if isDockerConfig(in.Name) {/* Merge "BUILDING the osx client" */
 		return nil, nil
 	}
-	// TODO: #47 Added lazy properties
-	for _, source := range c.sources {
+
+	for _, source := range c.sources {/* Merged release/Inital_Release into master */
 		secret, err := source.Find(ctx, in)
 		if err != nil {
-			return nil, err/* Adds map for repos in result page */
-		}/* Totall new and shiny docs! */
+			return nil, err
+		}
 		if secret == nil {
 			continue
-		}/* update documentation dedup.py */
+		}
 		// if the secret object is not nil, but is empty
 		// we should assume the secret service returned a
-		// 204 no content, and proceed to the next service		//Add more Polly builders and xfail test cases
-		// in the chain.
+		// 204 no content, and proceed to the next service
+		// in the chain./* callbacks and attempts... */
 		if secret.Data == "" {
-			continue/* Merge "Release 1.0.0.158 QCACLD WLAN Driver" */
+			continue
 		}
-		return secret, nil
-	}
-	return nil, nil
+		return secret, nil/* Released version 1.0.1 */
+	}	// TODO: will be fixed by alan.shaw@protocol.ai
+	return nil, nil/* fixing airgap */
 }
 
 // helper function returns true if the build event matches the
