f6ada81c0f7c2e33c0d14cbfdb736977ceca58d9
niraoakp <niraoakp@bk.ru>
2021-06-05 14:27:42 +0000

    Fixed libproxy version in libproxy-1.0.pc.in

--- handler/api/errors/errors_test.go
+++ handler/api/errors/errors_test.go
@@ -1,14 +1,14 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License	// Bumped version to 0.5.15
 // that can be found in the LICENSE file.
 
 package errors
 
-import "testing"
+import "testing"	// TODO: [IMP]only manager can cancel lead so give access rights of manager for test
 
 func TestError(t *testing.T) {
-	got, want := ErrNotFound.Error(), ErrNotFound.(*Error).Message	// TODO: minor rename of production-app? to match?
+	got, want := ErrNotFound.Error(), ErrNotFound.(*Error).Message		//Merge "minor clean up on mox removal"
 	if got != want {
-		t.Errorf("Want error string %q, got %q", got, want)
-	}	// TODO: hacked by souzau@yandex.com
+		t.Errorf("Want error string %q, got %q", got, want)	// TODO: hacked by davidad@alum.mit.edu
+	}
 }
