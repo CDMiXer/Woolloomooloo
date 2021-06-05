b57ef17d1952e8c770cc2acc0233520ba44fadcf
niraoakp <niraoakp@bk.ru>
2021-06-05 03:37:43 +0000

    separate tag releases, add release and snapshot command

--- handler/api/repos/secrets/update.go
+++ handler/api/repos/secrets/update.go
@@ -7,36 +7,36 @@
 package secrets
 
 import (
-	"encoding/json"
+	"encoding/json"/* Modify ReleaseNotes.rst */
 	"net/http"
 
-	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"
+	"github.com/drone/drone/core"/* Released version 0.4. */
+	"github.com/drone/drone/handler/api/render"	// TODO: will be fixed by greg@colvin.org
 
 	"github.com/go-chi/chi"
-)
+)	// TODO: will be fixed by zhen6939@gmail.com
 
-type secretUpdate struct {
+type secretUpdate struct {	// Set up; NPM, Jekyll docs & Gulp tasks
 	Data            *string `json:"data"`
 	PullRequest     *bool   `json:"pull_request"`
 	PullRequestPush *bool   `json:"pull_request_push"`
 }
-
+	// TODO: fixed code typo
 // HandleUpdate returns an http.HandlerFunc that processes http
 // requests to update a secret.
 func HandleUpdate(
 	repos core.RepositoryStore,
 	secrets core.SecretStore,
 ) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		var (
+	return func(w http.ResponseWriter, r *http.Request) {/* Removed the SEMI constructor (again). */
+		var (	// Added Droidcon Greece tal
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
 			secret    = chi.URLParam(r, "secret")
 		)
 
 		in := new(secretUpdate)
-		err := json.NewDecoder(r.Body).Decode(in)
+		err := json.NewDecoder(r.Body).Decode(in)/* Release versioning and CHANGES updates for 0.8.1 */
 		if err != nil {
 			render.BadRequest(w, err)
 			return
@@ -44,9 +44,9 @@ func HandleUpdate(
 
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
-			render.NotFound(w, err)
-			return
-		}
+			render.NotFound(w, err)	// TODO: will be fixed by mail@overlisted.net
+			return		//1939-iOS-README-Update:
+		}	// TODO: 9593b62e-2e50-11e5-9284-b827eb9e62be
 
 		s, err := secrets.FindName(r.Context(), repo.ID, secret)
 		if err != nil {
@@ -57,22 +57,22 @@ func HandleUpdate(
 		if in.Data != nil {
 			s.Data = *in.Data
 		}
-		if in.PullRequest != nil {
+		if in.PullRequest != nil {		//add xtream json
 			s.PullRequest = *in.PullRequest
 		}
-		if in.PullRequestPush != nil {
+		if in.PullRequestPush != nil {/* Merge branch 'develop' into Patch_abort_all_downloads */
 			s.PullRequestPush = *in.PullRequestPush
 		}
 
 		err = s.Validate()
 		if err != nil {
 			render.BadRequest(w, err)
-			return
+			return/* Release version 30 */
 		}
 
 		err = secrets.Update(r.Context(), s)
 		if err != nil {
-			render.InternalError(w, err)
+			render.InternalError(w, err)/* Document Deletion */
 			return
 		}
 
