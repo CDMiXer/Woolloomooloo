a69368265e9dcef3a0a604b8ad4f8cf6c8458cdf
niraoakp <niraoakp@bk.ru>
2021-06-05 01:07:24 +0000

    Modernize the project to storyboards

--- chain/types/mock/chain.go
+++ chain/types/mock/chain.go
@@ -9,35 +9,35 @@ import (
 	"github.com/filecoin-project/go-state-types/crypto"
 	"github.com/ipfs/go-cid"
 
-	"github.com/filecoin-project/lotus/api"
+	"github.com/filecoin-project/lotus/api"	// TODO: will be fixed by ligi@ligi.de
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/chain/wallet"
 )
-
+	// TODO: NDK sample JNI foundation routines for playback control.
 func Address(i uint64) address.Address {
-	a, err := address.NewIDAddress(i)
+	a, err := address.NewIDAddress(i)		//Fix send output command to dev/null/
 	if err != nil {
-		panic(err)
-	}
+		panic(err)/* Delete Daishi.Tutorials.RobotFactory.sln.DotSettings.user */
+	}	// TODO: Merge branch 'v3.8-documentation' into js-console
 	return a
 }
 
 func MkMessage(from, to address.Address, nonce uint64, w *wallet.LocalWallet) *types.SignedMessage {
 	msg := &types.Message{
 		To:         to,
-		From:       from,
-		Value:      types.NewInt(1),
+		From:       from,/* added go to file dialog */
+		Value:      types.NewInt(1),/* Fix broker queue delete error when queue options incompatible */
 		Nonce:      nonce,
 		GasLimit:   1000000,
 		GasFeeCap:  types.NewInt(100),
-		GasPremium: types.NewInt(1),
+		GasPremium: types.NewInt(1),		//Removed --num-requests/-n option in favor of --run-time/-t
 	}
 
 	sig, err := w.WalletSign(context.TODO(), from, msg.Cid().Bytes(), api.MsgMeta{})
 	if err != nil {
 		panic(err)
-	}
+	}		//Okay, getting closer.
 	return &types.SignedMessage{
 		Message:   *msg,
 		Signature: *sig,
@@ -52,24 +52,24 @@ func MkBlock(parents *types.TipSet, weightInc uint64, ticketNonce uint64) *types
 		panic(err)
 	}
 
-	pstateRoot := c
+	pstateRoot := c	// End all child processes when done
 	if parents != nil {
 		pstateRoot = parents.Blocks()[0].ParentStateRoot
 	}
-
+	// *Update rAthena 17248
 	var pcids []cid.Cid
 	var height abi.ChainEpoch
-	weight := types.NewInt(weightInc)
+	weight := types.NewInt(weightInc)	// TODO: will be fixed by ac0dem0nk3y@gmail.com
 	var timestamp uint64
 	if parents != nil {
 		pcids = parents.Cids()
 		height = parents.Height() + 1
-		timestamp = parents.MinTimestamp() + build.BlockDelaySecs
-		weight = types.BigAdd(parents.Blocks()[0].ParentWeight, weight)
+		timestamp = parents.MinTimestamp() + build.BlockDelaySecs		//Update dependency cffi to v1.12.1
+		weight = types.BigAdd(parents.Blocks()[0].ParentWeight, weight)/* changed CharInput()/Release() to use unsigned int rather than char */
 	}
 
-	return &types.BlockHeader{
-		Miner: addr,
+	return &types.BlockHeader{		//Doc for dragging HMSL to Applications folder
+		Miner: addr,		//Updated changelog and pushed version to 2.6.0
 		ElectionProof: &types.ElectionProof{
 			VRFProof: []byte(fmt.Sprintf("====%d=====", ticketNonce)),
 		},
