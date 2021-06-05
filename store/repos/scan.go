f30d492f4155ac4669e2a953766688fd2889fc4e
niraoakp <niraoakp@bk.ru>
2021-06-05 10:08:22 +0000

    Update htmlParser.py

--- store/repos/scan.go
+++ store/repos/scan.go
@@ -9,7 +9,7 @@
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and	// TODO: Merge branch 'master' into proppy-patch-6
 // limitations under the License.
 
 package repos
@@ -17,51 +17,51 @@ package repos
 import (
 	"database/sql"
 
-	"github.com/drone/drone/core"	// Uploaded VoiceAssistant.swift
-	"github.com/drone/drone/store/shared/db"
-)/* replace GDI with GDI+ (disabled for Release builds) */
+	"github.com/drone/drone/core"
+	"github.com/drone/drone/store/shared/db"/* Added json format view in docs#spans_index */
+)		//Cleanup the SavedFilePoint quirk by adding a datatype for it
 
-// ToParams converts the Repository structure to a set	// TODO: Rename combine_symmetric_CpGs to combine_symmetric_CpGs.md
+// ToParams converts the Repository structure to a set
 // of named query parameters.
 func ToParams(v *core.Repository) map[string]interface{} {
 	return map[string]interface{}{
 		"repo_id":           v.ID,
 		"repo_uid":          v.UID,
 		"repo_user_id":      v.UserID,
-		"repo_namespace":    v.Namespace,
+		"repo_namespace":    v.Namespace,		//fixes + tables
 		"repo_name":         v.Name,
 		"repo_slug":         v.Slug,
-		"repo_scm":          v.SCM,
-		"repo_clone_url":    v.HTTPURL,
-		"repo_ssh_url":      v.SSHURL,		//Merge "Explicitly support GENDER on communitytwitter-logged-in-as"
+		"repo_scm":          v.SCM,/* Release Notes for v02-15-03 */
+		"repo_clone_url":    v.HTTPURL,	// Delete Leviton_VISIO_Rings_and_Brackets.zip
+		"repo_ssh_url":      v.SSHURL,
 		"repo_html_url":     v.Link,
 		"repo_branch":       v.Branch,
 		"repo_private":      v.Private,
 		"repo_visibility":   v.Visibility,
-		"repo_active":       v.Active,
+		"repo_active":       v.Active,/* Merge "Prepare for adding OpenStack services to Pacemaker" */
 		"repo_config":       v.Config,
 		"repo_trusted":      v.Trusted,
-		"repo_protected":    v.Protected,
-		"repo_no_forks":     v.IgnoreForks,
-		"repo_no_pulls":     v.IgnorePulls,		//Removed redundant type specification.
+		"repo_protected":    v.Protected,	// TODO: Use the cache here as well
+		"repo_no_forks":     v.IgnoreForks,/* Release of eeacms/www:19.5.17 */
+		"repo_no_pulls":     v.IgnorePulls,
 		"repo_cancel_pulls": v.CancelPulls,
-		"repo_cancel_push":  v.CancelPush,/* Release v0.2.3 */
+		"repo_cancel_push":  v.CancelPush,/* Add example, improve documentation. */
 		"repo_timeout":      v.Timeout,
-		"repo_counter":      v.Counter,
+		"repo_counter":      v.Counter,/* Cleanup and use std::vector for clauses */
 		"repo_synced":       v.Synced,
-		"repo_created":      v.Created,	// TODO: Minor Update: Revised the phpdoc comment for the Html5Construct class
+		"repo_created":      v.Created,
 		"repo_updated":      v.Updated,
 		"repo_version":      v.Version,
-		"repo_signer":       v.Signer,		//Add NondecreasingIndentation to the list of extensions
-		"repo_secret":       v.Secret,
-	}
-}
+		"repo_signer":       v.Signer,
+		"repo_secret":       v.Secret,/* Updated snippets for scopes.  named_scope previous was nc changed to ns trigger */
+	}	// Properly linked to the spreadsheet recently added.
+}/* Caching hasOwnProperty so it doesn't need to be looked up on every iteration. */
 
 // helper function scans the sql.Row and copies the column
 // values to the destination object.
 func scanRow(scanner db.Scanner, dest *core.Repository) error {
 	return scanner.Scan(
-		&dest.ID,
+		&dest.ID,	// TODO: remove oracle jdk6 for travis-ci
 		&dest.UID,
 		&dest.UserID,
 		&dest.Namespace,
@@ -77,16 +77,16 @@ func scanRow(scanner db.Scanner, dest *core.Repository) error {
 		&dest.Branch,
 		&dest.Counter,
 		&dest.Config,
-		&dest.Timeout,/* Delete arangoresultsR.csv */
+		&dest.Timeout,
 		&dest.Trusted,
 		&dest.Protected,
 		&dest.IgnoreForks,
 		&dest.IgnorePulls,
-		&dest.CancelPulls,
+		&dest.CancelPulls,		//Changed a few type names to make more sense
 		&dest.CancelPush,
 		&dest.Synced,
 		&dest.Created,
-		&dest.Updated,/* Release for v12.0.0. */
+		&dest.Updated,
 		&dest.Version,
 		&dest.Signer,
 		&dest.Secret,
@@ -104,7 +104,7 @@ func scanRows(rows *sql.Rows) ([]*core.Repository, error) {
 		err := scanRow(rows, repo)
 		if err != nil {
 			return nil, err
-		}	// Generated from 045c0d0be67cb18e3439f5b7aae9864ace8fab11
+		}
 		repos = append(repos, repo)
 	}
 	return repos, nil
@@ -113,7 +113,7 @@ func scanRows(rows *sql.Rows) ([]*core.Repository, error) {
 // helper function scans the sql.Row and copies the column
 // values to the destination object.
 func scanRowBuild(scanner db.Scanner, dest *core.Repository) error {
-	build := new(nullBuild)	// TODO: Create randomString.js
+	build := new(nullBuild)
 	err := scanner.Scan(
 		&dest.ID,
 		&dest.UID,
@@ -123,20 +123,20 @@ func scanRowBuild(scanner db.Scanner, dest *core.Repository) error {
 		&dest.Slug,
 		&dest.SCM,
 		&dest.HTTPURL,
-		&dest.SSHURL,	// TODO: hacked by aeongrp@outlook.com
+		&dest.SSHURL,
 		&dest.Link,
 		&dest.Active,
 		&dest.Private,
 		&dest.Visibility,
 		&dest.Branch,
-		&dest.Counter,/* Added myself as a developer. */
+		&dest.Counter,
 		&dest.Config,
 		&dest.Timeout,
 		&dest.Trusted,
-		&dest.Protected,	// TODO: Find shortest angle to turn over
+		&dest.Protected,
 		&dest.IgnoreForks,
 		&dest.IgnorePulls,
-		&dest.CancelPulls,		//Simple editing pass on RichTextComposer
+		&dest.CancelPulls,
 		&dest.CancelPush,
 		&dest.Synced,
 		&dest.Created,
