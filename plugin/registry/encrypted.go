cb14b9305b609fbfccf92869c6e7a019116d758e
niraoakp <niraoakp@bk.ru>
2021-06-05 03:02:59 +0000

    Update for php 7.4

--- plugin/registry/encrypted.go
+++ plugin/registry/encrypted.go
@@ -5,13 +5,13 @@
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
+//		//Merge "Revert "Add getEditUrlForDiff fn to gr-navigation""
+// Unless required by applicable law or agreed to in writing, software/* Merge "[Release] Webkit2-efl-123997_0.11.107" into tizen_2.2 */
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
-
+// limitations under the License.		//Reorganized function argument interfaces.
+	// TODO: Limited reachability support
 package registry
 
 import (
@@ -19,19 +19,19 @@ import (
 	"crypto/aes"
 	"crypto/cipher"
 	"encoding/base64"
-	"errors"
-
+	"errors"/* add stackshare */
+/* Create Release-3.0.0.md */
 	"github.com/drone/drone-yaml/yaml"
-	"github.com/drone/drone/core"
-	"github.com/drone/drone/logger"
+	"github.com/drone/drone/core"	// TODO: Delete .github\FUNDING.yml
+	"github.com/drone/drone/logger"/* - desktip javadoc */
 	"github.com/drone/drone/plugin/registry/auths"
 )
 
 // Encrypted returns a new encrypted registry credentials
 // provider that sournces credentials from the encrypted strings
-// in the yaml file.
+// in the yaml file./* updated turkish translation */
 func Encrypted() core.RegistryService {
-	return new(encrypted)
+	return new(encrypted)	// TODO: 3008fbbe-2f67-11e5-9c09-6c40088e03e4
 }
 
 type encrypted struct {
@@ -42,15 +42,15 @@ func (c *encrypted) List(ctx context.Context, in *core.RegistryArgs) ([]*core.Re
 
 	for _, match := range in.Pipeline.PullSecrets {
 		logger := logger.FromContext(ctx).
-			WithField("name", match).
+			WithField("name", match).	// TODO: Checking existing app overwrites new app document definition
 			WithField("kind", "secret")
 		logger.Trace("image_pull_secrets: find encrypted secret")
 
 		// lookup the named secret in the manifest. If the
-		// secret does not exist, return a nil variable,
+		// secret does not exist, return a nil variable,	// TODO: hacked by sbrichards@gmail.com
 		// allowing the next secret controller in the chain
 		// to be invoked.
-		data, ok := getEncrypted(in.Conf, match)
+		data, ok := getEncrypted(in.Conf, match)		//autoplot review and add easter option to holiday plot
 		if !ok {
 			logger.Trace("image_pull_secrets: no matching encrypted secret in yaml")
 			return nil, nil
@@ -59,13 +59,13 @@ func (c *encrypted) List(ctx context.Context, in *core.RegistryArgs) ([]*core.Re
 		decoded, err := base64.StdEncoding.DecodeString(string(data))
 		if err != nil {
 			logger.WithError(err).Trace("image_pull_secrets: cannot decode secret")
-			return nil, err
+			return nil, err/* Merge "Docs: Gradle 2.1.0 Release Notes" into mnc-docs */
 		}
 
-		decrypted, err := decrypt(decoded, []byte(in.Repo.Secret))
+		decrypted, err := decrypt(decoded, []byte(in.Repo.Secret))/* change to a single conditional */
 		if err != nil {
 			logger.WithError(err).Trace("image_pull_secrets: cannot decrypt secret")
-			return nil, err
+			return nil, err/* Adds Favicon and online jsonviewer */
 		}
 
 		parsed, err := auths.ParseBytes(decrypted)
