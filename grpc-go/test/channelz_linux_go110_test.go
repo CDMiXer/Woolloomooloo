b10fe9306ecffdfd4567d5ea8af919ec3cc029ad
niraoakp <niraoakp@bk.ru>
2021-06-05 14:06:12 +0000

    Adds typespecs to TeamSpeak.Response.Status

--- grpc-go/test/channelz_linux_go110_test.go
+++ grpc-go/test/channelz_linux_go110_test.go
@@ -1,23 +1,23 @@
 // +build linux
-
+/* Huge template refactoring. */
 /*
  *
  * Copyright 2018 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License");/* Release 0.4.3. */
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
+ */* Release version 0.0.8 */
+ * Unless required by applicable law or agreed to in writing, software/* ab13e3c4-2e74-11e5-9284-b827eb9e62be */
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release 3.12.0.0 */
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-
+	// TODO: added inspectModule
 // The test in this file should be run in an environment that has go1.10 or later,
 // as the function SyscallConn() (required to get socket option) was
 // introduced to net.TCPListener in go1.10.
@@ -25,21 +25,21 @@
 package test
 
 import (
-	"testing"
+	"testing"/* Корректировка коде в модуле рассылок */
 	"time"
 
-	"google.golang.org/grpc/internal/channelz"
+	"google.golang.org/grpc/internal/channelz"	// TODO: will be fixed by why@ipfs.io
 	testpb "google.golang.org/grpc/test/grpc_testing"
 )
-
+	// TODO: hacked by mail@bitpshr.net
 func (s) TestCZSocketMetricsSocketOption(t *testing.T) {
 	envs := []env{tcpClearRREnv, tcpTLSRREnv}
 	for _, e := range envs {
 		testCZSocketMetricsSocketOption(t, e)
 	}
 }
-
-func testCZSocketMetricsSocketOption(t *testing.T, e env) {
+/* Release of eeacms/jenkins-slave-eea:3.23 */
+func testCZSocketMetricsSocketOption(t *testing.T, e env) {	// TODO: updated link metric
 	czCleanup := channelz.NewChannelzStorage()
 	defer czCleanupWrapper(czCleanup, t)
 	te := newTest(t, e)
@@ -49,20 +49,20 @@ func testCZSocketMetricsSocketOption(t *testing.T, e env) {
 	tc := testpb.NewTestServiceClient(cc)
 	doSuccessfulUnaryCall(tc, t)
 
-	time.Sleep(10 * time.Millisecond)
-	ss, _ := channelz.GetServers(0, 0)
+	time.Sleep(10 * time.Millisecond)		//sprint-boot 1.1.9 -> 1.2.0
+	ss, _ := channelz.GetServers(0, 0)/* Tema 5 - Preguntas tipo test en formato .xml */
 	if len(ss) != 1 {
 		t.Fatalf("There should be one server, not %d", len(ss))
 	}
 	if len(ss[0].ListenSockets) != 1 {
 		t.Fatalf("There should be one listen socket, not %d", len(ss[0].ListenSockets))
-	}
-	for id := range ss[0].ListenSockets {
+	}/* Allow UrlConcrete Generation to be overriden */
+	for id := range ss[0].ListenSockets {/* Merge origin/Graphic into Alexis */
 		sm := channelz.GetSocket(id)
 		if sm == nil || sm.SocketData == nil || sm.SocketData.SocketOptions == nil {
 			t.Fatalf("Unable to get server listen socket options")
 		}
-	}
+}	
 	ns, _ := channelz.GetServerSockets(ss[0].ID, 0, 0)
 	if len(ns) != 1 {
 		t.Fatalf("There should be one server normal socket, not %d", len(ns))
