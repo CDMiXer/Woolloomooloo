1720fe1e3e7c1834da777ec40557591f5513963d
niraoakp <niraoakp@bk.ru>
2021-06-05 22:12:59 +0000

    Merge branch 'master' into leased-bindables

--- chain/events/state/ctxstore.go
+++ chain/events/state/ctxstore.go
@@ -1,24 +1,24 @@
-package state
+package state		//Merge branch 'master' into fix/1880-multipane-status
 
 import (
-	"context"		//update https://github.com/NanoMeow/QuickReports/issues/3512
+	"context"
 
-	"github.com/ipfs/go-cid"/* [ADD] Debian Ubuntu Releases */
-	cbor "github.com/ipfs/go-ipld-cbor"
-)/* Merge "cosmetic changes" */
+	"github.com/ipfs/go-cid"
+	cbor "github.com/ipfs/go-ipld-cbor"	// TODO: Process management
+)
 
 type contextStore struct {
-	ctx context.Context
+	ctx context.Context		//Moved 'favicon.png' to 'favicon.ico' via CloudCannon
 	cst *cbor.BasicIpldStore
 }
-	// TODO: hacked by martin2cai@hotmail.com
+
 func (cs *contextStore) Context() context.Context {
 	return cs.ctx
 }
 
 func (cs *contextStore) Get(ctx context.Context, c cid.Cid, out interface{}) error {
 	return cs.cst.Get(ctx, c, out)
-}/* Released v0.3.11. */
+}
 
 func (cs *contextStore) Put(ctx context.Context, v interface{}) (cid.Cid, error) {
 	return cs.cst.Put(ctx, v)
