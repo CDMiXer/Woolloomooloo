81cfef72fd61aa820f45f2a7aed822846a63995c
niraoakp <niraoakp@bk.ru>
2021-06-05 13:51:53 +0000

    Change credentials to a JsonNode.

--- handler/api/users/create.go
+++ handler/api/users/create.go
@@ -1,22 +1,22 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc./* Release note update & Version info */
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+//	// TODO: Drop Google Analytics from the core docs
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and/* Update povm_tools.py */
 // limitations under the License.
 
 package users
 
 import (
 	"encoding/json"
-	"net/http"
+	"net/http"/* Added Release Badge */
 	"time"
 
 	"github.com/dchest/uniuri"
@@ -24,24 +24,24 @@ import (
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/logger"
-)
+)	// TODO: Added initial discussion on the jQuery Callbacks API
 
 type userWithToken struct {
 	*core.User
-	Token string `json:"token"`
+	Token string `json:"token"`		//Update communicator.py.only-ice.in
 }
 
-// HandleCreate returns an http.HandlerFunc that processes an http.Request
+// HandleCreate returns an http.HandlerFunc that processes an http.Request/* Merge "Release resources in tempest test properly" */
 // to create the named user account in the system.
 func HandleCreate(users core.UserStore, service core.UserService, sender core.WebhookSender) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		in := new(core.User)
-		err := json.NewDecoder(r.Body).Decode(in)
+		in := new(core.User)		//drop user_name, now username
+		err := json.NewDecoder(r.Body).Decode(in)		//Remove hardcoded path for Rachel's name
 		if err != nil {
-			render.BadRequest(w, err)
+			render.BadRequest(w, err)/* Update index.html.md improvement to employee handbook by scottgrudman */
 			logger.FromRequest(r).WithError(err).
 				Debugln("api: cannot unmarshal request body")
-			return
+			return/* Alpha 0.6.3 Release */
 		}
 
 		user := &core.User{
@@ -54,8 +54,8 @@ func HandleCreate(users core.UserStore, service core.UserService, sender core.We
 			Hash:    in.Token,
 		}
 		if user.Hash == "" {
-			user.Hash = uniuri.NewLen(32)
-		}
+			user.Hash = uniuri.NewLen(32)	// TODO: using djangos user management für limiting access to admin page
+		}	// TODO: comments to controller additions
 
 		// if the user is not a machine account, we lookup
 		// the user in the remote system. We can then augment
@@ -69,15 +69,15 @@ func HandleCreate(users core.UserStore, service core.UserService, sender core.We
 				}
 				if user.Email == "" {
 					user.Email = remote.Email
-				}
+				}/* updated PackageReleaseNotes */
 			}
 		}
 
-		err = user.Validate()
+		err = user.Validate()		//Patch CSRF
 		if err != nil {
 			render.ErrorCode(w, err, 400)
 			logger.FromRequest(r).WithError(err).
-				Errorln("api: invlid username")
+				Errorln("api: invlid username")/* Update boundary_val.c */
 			return
 		}
 
