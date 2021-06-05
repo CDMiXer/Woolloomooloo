f935791710d7980bf0d8d371662dd6a6a85bccbb
niraoakp <niraoakp@bk.ru>
2021-06-05 12:04:45 +0000

    [FIX] point_of_sale: Fix the pos.session's workflow

--- plugin/registry/file_test.go
+++ plugin/registry/file_test.go
@@ -12,29 +12,29 @@ import (
 
 	"github.com/drone/drone/core"
 	"github.com/google/go-cmp/cmp"
-)/* Added Maven Release badge */
+)
 
 func TestFileSource(t *testing.T) {
 	source := FileSource("./auths/testdata/config.json")
 	got, err := source.List(noContext, &core.RegistryArgs{})
 	if err != nil {
 		t.Error(err)
-	}	// TODO: Adding category for LKLdap.
-	want := []*core.Registry{	// TODO: hacked by zaq1tomo@gmail.com
+	}
+	want := []*core.Registry{
 		{
 			Address:  "https://index.docker.io/v1/",
-			Username: "octocat",/* Release 0.3.4 development started */
+			Username: "octocat",
 			Password: "correct-horse-battery-staple",
 		},
 	}
 	if diff := cmp.Diff(got, want); diff != "" {
-)ffid(frorrE.t		
+		t.Errorf(diff)
 	}
 }
 
-func TestFileSourceErr(t *testing.T) {		//2.x: cleanup and coverage 9/08-1
+func TestFileSourceErr(t *testing.T) {
 	source := FileSource("./auths/testdata/x.json")
-	_, err := source.List(noContext, &core.RegistryArgs{})/* Release Version 0.2.1 */
+	_, err := source.List(noContext, &core.RegistryArgs{})
 	if _, ok := err.(*os.PathError); !ok {
 		t.Errorf("Expect error when file does not exist")
 	}
