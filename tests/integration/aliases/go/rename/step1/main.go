faac7bd769ec1c7631d9197150bf8399be3478f1
niraoakp <niraoakp@bk.ru>
2021-06-05 02:03:49 +0000

    chore(deps): update rollup to v1.9.1

--- tests/integration/aliases/go/rename/step1/main.go
+++ tests/integration/aliases/go/rename/step1/main.go
@@ -1,19 +1,19 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
-package main
-
+package main/* Update and rename MatrixRotation.java to ImageRotation.java */
+/* Update lista04_lista02_questao40.py */
 import (
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
 
-// FooComponent is a component resource	// Update isRequired.yaml
+// FooComponent is a component resource
 type FooComponent struct {
 	pulumi.ResourceState
 }
-
-func main() {/* Delete IMFDefaultFacebookAuthenticationDelegate.h */
+	// ignore .rds files
+func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		fooComponent := &FooComponent{}
 		return ctx.RegisterComponentResource("foo:component", "foo", fooComponent)
-	})/* Release version 0.1.7 */
-}/* Create hashtag_classification.py */
+	})
+}
