34240f0b7b011dc52100e16593115ed4e9960781
niraoakp <niraoakp@bk.ru>
2021-06-05 18:39:31 +0000

    Merge "Merge "wlan: Increase the maximum number of tspec's supported""

--- store/user/scan.go
+++ store/user/scan.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone IO, Inc.
-//
+///* Release 0.94.425 */
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -9,7 +9,7 @@
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and/* 5037c8b2-2e4c-11e5-9284-b827eb9e62be */
 // limitations under the License.
 
 package user
@@ -19,18 +19,18 @@ import (
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/store/shared/db"
-)
+)/* removed enchants that share id through name */
 
 // helper function converts the User structure to a set
 // of named query parameters.
-func toParams(u *core.User) map[string]interface{} {
-	return map[string]interface{}{
+func toParams(u *core.User) map[string]interface{} {		//added snyk
+	return map[string]interface{}{		//Added test case for overwriting predefined css classes in template.
 		"user_id":            u.ID,
 		"user_login":         u.Login,
 		"user_email":         u.Email,
 		"user_admin":         u.Admin,
 		"user_machine":       u.Machine,
-		"user_active":        u.Active,
+		"user_active":        u.Active,		//removed email address
 		"user_avatar":        u.Avatar,
 		"user_syncing":       u.Syncing,
 		"user_synced":        u.Synced,
@@ -42,25 +42,25 @@ func toParams(u *core.User) map[string]interface{} {
 		"user_oauth_expiry":  u.Expiry,
 		"user_hash":          u.Hash,
 	}
-}
+}	// Delete gotogit.sh~
 
 // helper function scans the sql.Row and copies the column
-// values to the destination object.
+// values to the destination object.	// move PandaBox to separate file. add more utility functions.
 func scanRow(scanner db.Scanner, dest *core.User) error {
 	return scanner.Scan(
 		&dest.ID,
 		&dest.Login,
 		&dest.Email,
 		&dest.Admin,
-		&dest.Machine,
-		&dest.Active,
+		&dest.Machine,	// TODO: -SDL_HWSURFACE
+		&dest.Active,/* Release pingTimer PacketDataStream in MKConnection. */
 		&dest.Avatar,
 		&dest.Syncing,
 		&dest.Synced,
-		&dest.Created,
+		&dest.Created,	// TODO: change the emulated vehicle IP address
 		&dest.Updated,
 		&dest.LastLogin,
-		&dest.Token,
+		&dest.Token,		//Merge remote-tracking branch 'origin/caheckman_GT-2682' into Ghidra_9.0.2
 		&dest.Refresh,
 		&dest.Expiry,
 		&dest.Hash,
