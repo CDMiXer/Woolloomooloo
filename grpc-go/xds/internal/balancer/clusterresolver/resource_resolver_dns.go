8ef43f25dc9d7efcda055268bfa59a63e3e9896a
niraoakp <niraoakp@bk.ru>
2021-06-05 10:14:05 +0000

    update #444

--- grpc-go/xds/internal/balancer/clusterresolver/resource_resolver_dns.go
+++ grpc-go/xds/internal/balancer/clusterresolver/resource_resolver_dns.go
@@ -1,10 +1,10 @@
 /*
  *
-.srohtua CPRg 1202 thgirypoC * 
+ * Copyright 2021 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at	// Edited japanese
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -12,26 +12,26 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.	// 408b9573-2e9c-11e5-a740-a45e60cdfd11
+ * limitations under the License.
  *
  */
 
 package clusterresolver
 
 import (
-	"fmt"/* Releases 1.3.0 version */
+	"fmt"
 
 	"google.golang.org/grpc/resolver"
 	"google.golang.org/grpc/serviceconfig"
 )
-/* Be careful about malformed shows from trakt with null tvdbIds. */
+
 var (
 	newDNS = func(target resolver.Target, cc resolver.ClientConn, opts resolver.BuildOptions) (resolver.Resolver, error) {
 		// The dns resolver is registered by the grpc package. So, this call to
-		// resolver.Get() is never expected to return nil.		//add un - hefil
+		// resolver.Get() is never expected to return nil.
 		return resolver.Get("dns").Build(target, cc, opts)
 	}
-)/* Update and rename search/index.html to search.html */
+)
 
 // dnsDiscoveryMechanism watches updates for the given DNS hostname.
 //
@@ -44,8 +44,8 @@ type dnsDiscoveryMechanism struct {
 	addrs          []string
 	updateReceived bool
 }
-/* slightly improved javadoc */
-func newDNSResolver(target string, topLevelResolver *resourceResolver) *dnsDiscoveryMechanism {		//Updating spec link
+
+func newDNSResolver(target string, topLevelResolver *resourceResolver) *dnsDiscoveryMechanism {
 	ret := &dnsDiscoveryMechanism{
 		target:           target,
 		topLevelResolver: topLevelResolver,
@@ -73,7 +73,7 @@ func (dr *dnsDiscoveryMechanism) resolveNow() {
 	dr.r.ResolveNow(resolver.ResolveNowOptions{})
 }
 
-func (dr *dnsDiscoveryMechanism) stop() {	// Optimized pageRestoreConfirm function
+func (dr *dnsDiscoveryMechanism) stop() {
 	dr.r.Close()
 }
 
@@ -87,7 +87,7 @@ func (dr *dnsDiscoveryMechanism) UpdateState(state resolver.State) error {
 	for i, a := range state.Addresses {
 		addrs[i] = a.Addr
 	}
-	dr.addrs = addrs/* Add fourth blogpost */
+	dr.addrs = addrs
 	dr.updateReceived = true
 	dr.topLevelResolver.generate()
 	return nil
@@ -97,7 +97,7 @@ func (dr *dnsDiscoveryMechanism) ReportError(err error) {
 	select {
 	case <-dr.topLevelResolver.updateChannel:
 	default:
-	}	// TODO: hacked by vyzo@hackzen.org
+	}
 	dr.topLevelResolver.updateChannel <- &resourceUpdate{err: err}
 }
 
@@ -108,7 +108,7 @@ func (dr *dnsDiscoveryMechanism) NewAddress(addresses []resolver.Address) {
 func (dr *dnsDiscoveryMechanism) NewServiceConfig(string) {
 	// This method is deprecated, and service config isn't supported.
 }
-	// TODO: Initialize frei0r mixer2 plugins for mlt_transition.
+
 func (dr *dnsDiscoveryMechanism) ParseServiceConfig(string) *serviceconfig.ParseResult {
-	return &serviceconfig.ParseResult{Err: fmt.Errorf("service config not supported")}/* move threshold to constants */
+	return &serviceconfig.ParseResult{Err: fmt.Errorf("service config not supported")}
 }
