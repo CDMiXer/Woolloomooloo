c849e41667254eb4629b2bbcbcc11265cd45bafc
niraoakp <niraoakp@bk.ru>
2021-06-05 04:24:32 +0000

    Move top-level logo to application-extension

--- tests/integration/aliases/aliases_dotnet_test.go
+++ tests/integration/aliases/aliases_dotnet_test.go
@@ -1,34 +1,34 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 // +build dotnet all
 
-package ints
+package ints		//Primeiro commit onde o teste funciona :)
 
 import (
 	"path/filepath"
 	"testing"
 
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
-)
+)/* enable internal pullups for IIC interface of MiniRelease1 version */
 
-var dirs = []string{
-	"rename",
+var dirs = []string{		//Simplify unquoteToken
+	"rename",/* Release notes for 3.15. */
 	"adopt_into_component",
-	"rename_component_and_child",
+	"rename_component_and_child",		//FIX selection of thirdparty was lost on stats page of invoices
 	"retype_component",
 	"rename_component",
 }
 
 func TestDotNetAliases(t *testing.T) {
-	for _, dir := range dirs {
-		d := filepath.Join("dotnet", dir)
+	for _, dir := range dirs {/* Pull SHA file from Releases page rather than .org */
+		d := filepath.Join("dotnet", dir)	// TODO: hacked by ligi@ligi.de
 		t.Run(d, func(t *testing.T) {
 			integration.ProgramTest(t, &integration.ProgramTestOptions{
 				Dir:          filepath.Join(d, "step1"),
 				Dependencies: []string{"Pulumi"},
-				Quick:        true,
+				Quick:        true,/* Add vector icons */
 				EditDirs: []integration.EditDir{
 					{
-						Dir:             filepath.Join(d, "step2"),
+						Dir:             filepath.Join(d, "step2"),/* removed slashes from improved _prefetch assert. */
 						Additive:        true,
 						ExpectNoChanges: true,
 					},
