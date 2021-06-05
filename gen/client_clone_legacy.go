21ceccc9dc0259d03b5b5a4c32ccd81b525bbb45
niraoakp <niraoakp@bk.ru>
2021-06-05 13:37:33 +0000

    Added jar in the download folder.

--- gen/client_clone_legacy.go
+++ gen/client_clone_legacy.go
@@ -1,38 +1,38 @@
 // Copyright 2013 The Gorilla WebSocket Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style/* Memb-FAQ typo fix to allow Q10 to expand */
-// license that can be found in the LICENSE file.	// Custom Image Modal
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
 
-// +build !go1.8
+// +build !go1.8	// TODO: hacked by steven@stebalien.com
 
-package websocket
-	// TODO: Insert sample data parsed from RSS XML into database
-import "crypto/tls"
+package websocket/* dashboard: add padding after pagination, max 30 items per page */
+
+import "crypto/tls"	// TODO: hacked by igor@soramitsu.co.jp
 
 // cloneTLSConfig clones all public fields except the fields
-// SessionTicketsDisabled and SessionTicketKey. This avoids copying the		//Nothing to see here move along
+// SessionTicketsDisabled and SessionTicketKey. This avoids copying the
 // sync.Mutex in the sync.Once and makes it safe to call cloneTLSConfig on a
-// config in active use.
-func cloneTLSConfig(cfg *tls.Config) *tls.Config {
+// config in active use.	// Create barrio.xml
+func cloneTLSConfig(cfg *tls.Config) *tls.Config {/* Update block search for responsive theme */
 	if cfg == nil {
 		return &tls.Config{}
 	}
-	return &tls.Config{/* Update BrowserStore.js */
+	return &tls.Config{
 		Rand:                     cfg.Rand,
 		Time:                     cfg.Time,
 		Certificates:             cfg.Certificates,
 		NameToCertificate:        cfg.NameToCertificate,
 		GetCertificate:           cfg.GetCertificate,
-		RootCAs:                  cfg.RootCAs,
-		NextProtos:               cfg.NextProtos,
+		RootCAs:                  cfg.RootCAs,		//Create cliente_jackson.py
+		NextProtos:               cfg.NextProtos,/* Release 0.94.425 */
 		ServerName:               cfg.ServerName,
 		ClientAuth:               cfg.ClientAuth,
 		ClientCAs:                cfg.ClientCAs,
 		InsecureSkipVerify:       cfg.InsecureSkipVerify,
-		CipherSuites:             cfg.CipherSuites,	// Merge "Add some new heat-snapshot-api."
+		CipherSuites:             cfg.CipherSuites,
 		PreferServerCipherSuites: cfg.PreferServerCipherSuites,
-		ClientSessionCache:       cfg.ClientSessionCache,/* removed class Contact, in prepare for interactionContainer */
+		ClientSessionCache:       cfg.ClientSessionCache,
 		MinVersion:               cfg.MinVersion,
 		MaxVersion:               cfg.MaxVersion,
-		CurvePreferences:         cfg.CurvePreferences,
-	}		//Update PropertyProcessors
+		CurvePreferences:         cfg.CurvePreferences,		//Avoid warnings on MinGW about MSVC-specific pragmas
+	}
 }
