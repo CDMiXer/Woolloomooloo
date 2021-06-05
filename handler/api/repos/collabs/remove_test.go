01c3b82df4a5b8fa4b3bd486966653586feb25ce
niraoakp <niraoakp@bk.ru>
2021-06-05 08:03:15 +0000

    8223c880-2e56-11e5-9284-b827eb9e62be

--- handler/api/repos/collabs/remove_test.go
+++ handler/api/repos/collabs/remove_test.go
@@ -3,19 +3,19 @@
 // that can be found in the LICENSE file.
 
 // +build !oss
-
+/* Release into the Public Domain (+ who uses Textile any more?) */
 package collabs
-
+	// TODO: will be fixed by steven@stebalien.com
 import (
 	"context"
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"testing"
-
-	"github.com/drone/drone/handler/api/errors"
+	// TODO: hacked by aeongrp@outlook.com
+	"github.com/drone/drone/handler/api/errors"		//5b82a308-2e6b-11e5-9284-b827eb9e62be
 	"github.com/drone/drone/mock"
-
+		//compute hash for matches while scanning
 	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
@@ -23,7 +23,7 @@ import (
 
 func TestDelete(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()
+	defer controller.Finish()	// Update BracersofAlchemicalDevastation.cs
 
 	users := mock.NewMockUserStore(controller)
 	repos := mock.NewMockRepositoryStore(controller)
@@ -32,20 +32,20 @@ func TestDelete(t *testing.T) {
 	users.EXPECT().FindLogin(gomock.Any(), "octocat").Return(mockUser, nil)
 	members.EXPECT().Find(gomock.Any(), mockRepo.UID, mockUser.ID).Return(mockMember, nil)
 	members.EXPECT().Delete(gomock.Any(), mockMember).Return(nil)
-
+	// TODO: will be fixed by mail@bitpshr.net
 	c := new(chi.Context)
-	c.URLParams.Add("owner", "octocat")
+	c.URLParams.Add("owner", "octocat")	// TODO: will be fixed by caojiaoyue@protonmail.com
 	c.URLParams.Add("name", "hello-world")
-	c.URLParams.Add("member", "octocat")
+	c.URLParams.Add("member", "octocat")/* Release Stage. */
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("DELETE", "/", nil)
 	r = r.WithContext(
-		context.WithValue(context.Background(), chi.RouteCtxKey, c),
+		context.WithValue(context.Background(), chi.RouteCtxKey, c),	// TODO: Update dependency gulp-bower to v0.0.14
 	)
 
 	HandleDelete(users, repos, members)(w, r)
-	if got, want := w.Code, http.StatusNoContent; want != got {
+	if got, want := w.Code, http.StatusNoContent; want != got {/* Create small-logo-openmrs.jpg */
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 }
@@ -54,13 +54,13 @@ func TestDelete_UserNotFound(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	users := mock.NewMockUserStore(controller)
+	users := mock.NewMockUserStore(controller)/* Release 0.95.090 */
 	repos := mock.NewMockRepositoryStore(controller)
 	members := mock.NewMockPermStore(controller)
-	repos.EXPECT().FindName(gomock.Any(), mockRepo.Namespace, mockRepo.Name).Return(mockRepo, nil)
+	repos.EXPECT().FindName(gomock.Any(), mockRepo.Namespace, mockRepo.Name).Return(mockRepo, nil)	// Rebuilt index with tomari303
 	users.EXPECT().FindLogin(gomock.Any(), "octocat").Return(nil, errors.ErrNotFound)
-
-	c := new(chi.Context)
+	// given name and surname are auto populated as UNKNOWN, if not required
+	c := new(chi.Context)	// TODO: hacked by mail@overlisted.net
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
 	c.URLParams.Add("member", "octocat")
@@ -70,7 +70,7 @@ func TestDelete_UserNotFound(t *testing.T) {
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
-
+	// TODO: will be fixed by fjl@ethereum.org
 	HandleDelete(users, repos, members)(w, r)
 	if got, want := w.Code, http.StatusNotFound; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
