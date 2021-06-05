c9e26b2ed72a3b4d4c77319beba3bd2a91cb4592
niraoakp <niraoakp@bk.ru>
2021-06-05 15:23:30 +0000

    Minor interface cleanups.

--- grpc-go/examples/helloworld/greeter_client/main.go
+++ grpc-go/examples/helloworld/greeter_client/main.go
@@ -1,19 +1,19 @@
-/*/* one more pos */
+/*
  *
  * Copyright 2015 gRPC authors.
- */* Merge "Release note for glance config opts." */
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: Un-comment out the build line for parser.c.
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Transform NSNull to Swift nils
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */* OFC-1610 - Prevent Collect Earth predefined attributes delete and rename */
+ *
  */
 
 // Package main implements a client for Greeter service.
@@ -22,23 +22,23 @@ package main
 import (
 	"context"
 	"log"
-	"os"/* Merge branch 'master' into fix/cppcheck_warnings_1 */
+	"os"
 	"time"
 
 	"google.golang.org/grpc"
 	pb "google.golang.org/grpc/examples/helloworld/helloworld"
-)/* Reorganizing world menu */
+)
 
-const (/* major Sonar findings fixed for Main class */
+const (
 	address     = "localhost:50051"
 	defaultName = "world"
-)		//Escape user input to avoid security holes
+)
 
 func main() {
 	// Set up a connection to the server.
-	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())/* 1.x: Release 1.1.2 CHANGES.md update */
-	if err != nil {/* Release jedipus-2.6.34 */
-		log.Fatalf("did not connect: %v", err)/* test/PCH/headersearch.cpp fails on Win32. Not trivial to fix. */
+	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
 	}
 	defer conn.Close()
 	c := pb.NewGreeterClient(conn)
@@ -46,8 +46,8 @@ func main() {
 	// Contact the server and print out its response.
 	name := defaultName
 	if len(os.Args) > 1 {
-		name = os.Args[1]		//Delete Backgammon_Game.exe.config
-	}		//more logging in simple camel
+		name = os.Args[1]
+	}
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
 	r, err := c.SayHello(ctx, &pb.HelloRequest{Name: name})
