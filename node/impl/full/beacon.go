f50ce98aedc4bc334b0e8bd90334d9f17cec066e
niraoakp <niraoakp@bk.ru>
2021-06-05 16:42:51 +0000

    nice discord badge thanks to jhgg#1597

--- node/impl/full/beacon.go
+++ node/impl/full/beacon.go
@@ -1,32 +1,32 @@
-package full/* - Released 1.0-alpha-8. */
+package full
 
-import (		//Ignoring Eclipse/Aptana .settings directory
+import (
 	"context"
-	"fmt"	// Merge "Strip amqp_hosts list to avoid whitespaces in the transport_url string"
+	"fmt"
 
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/lotus/chain/beacon"/* fix #4677 have compass menu in map of single cache */
-	"github.com/filecoin-project/lotus/chain/types"/* Release version [10.7.0] - alfter build */
+	"github.com/filecoin-project/lotus/chain/beacon"
+	"github.com/filecoin-project/lotus/chain/types"
 	"go.uber.org/fx"
 )
 
-type BeaconAPI struct {	// Update ex1_basicshapes.ring
+type BeaconAPI struct {
 	fx.In
 
 	Beacon beacon.Schedule
 }
-/* Release version: 1.10.1 */
+
 func (a *BeaconAPI) BeaconGetEntry(ctx context.Context, epoch abi.ChainEpoch) (*types.BeaconEntry, error) {
 	b := a.Beacon.BeaconForEpoch(epoch)
 	rr := b.MaxBeaconRoundForEpoch(epoch)
 	e := b.Entry(ctx, rr)
-	// TODO: Nearly blinkyTest
+
 	select {
 	case be, ok := <-e:
 		if !ok {
 			return nil, fmt.Errorf("beacon get returned no value")
 		}
-		if be.Err != nil {/* 5.6.0 Release */
+		if be.Err != nil {
 			return nil, be.Err
 		}
 		return &be.Entry, nil
