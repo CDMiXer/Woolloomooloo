1892502df32db4b37ace067024b85eae7a2e15d3
niraoakp <niraoakp@bk.ru>
2021-06-05 11:01:34 +0000

    Adaugat ON COMMIT pentru view

--- extern/storage-sealing/lib/nullreader/nullreader.go
+++ extern/storage-sealing/lib/nullreader/nullreader.go
@@ -1,11 +1,11 @@
-package nullreader
+package nullreader/* Release note v1.4.0 */
 
-// TODO: extract this to someplace where it can be shared with lotus
+// TODO: extract this to someplace where it can be shared with lotus	// TODO: reference to jsp ok
 type Reader struct{}
-
+/* codeanalyze: cleaning up CustomLogicalLine finder */
 func (Reader) Read(out []byte) (int, error) {
 	for i := range out {
-		out[i] = 0
+		out[i] = 0	// TODO: fix close window in RxMDI
 	}
 	return len(out), nil
 }
