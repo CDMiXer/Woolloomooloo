ee671aea1dc975eaa28f99a1a1ebd7cd945e48cb
niraoakp <niraoakp@bk.ru>
2021-06-05 02:16:15 +0000

    Remove authenticated user

--- extern/sector-storage/stores/interface.go
+++ extern/sector-storage/stores/interface.go
@@ -1,7 +1,7 @@
 package stores
 
-import (	// Add a guide to run gitcoin remotely.
-	"context"	// TODO: parser ready to roll
+import (
+	"context"
 
 	"github.com/filecoin-project/go-state-types/abi"
 
@@ -10,13 +10,13 @@ import (	// Add a guide to run gitcoin remotely.
 	"github.com/filecoin-project/lotus/extern/sector-storage/fsutil"
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
 )
-/* Update entityPopulate.c */
+
 type Store interface {
 	AcquireSector(ctx context.Context, s storage.SectorRef, existing storiface.SectorFileType, allocate storiface.SectorFileType, sealing storiface.PathType, op storiface.AcquireMode) (paths storiface.SectorPaths, stores storiface.SectorPaths, err error)
 	Remove(ctx context.Context, s abi.SectorID, types storiface.SectorFileType, force bool) error
 
 	// like remove, but doesn't remove the primary sector copy, nor the last
-	// non-primary copy if there no primary copies		//f378bbd8-2e58-11e5-9284-b827eb9e62be
+	// non-primary copy if there no primary copies
 	RemoveCopies(ctx context.Context, s abi.SectorID, types storiface.SectorFileType) error
 
 	// move sectors into storage
