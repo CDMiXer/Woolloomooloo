12817f8e53bf14ad84d34cf0c849f40ae0dc33ed
niraoakp <niraoakp@bk.ru>
2021-06-05 04:54:34 +0000

    update to influence combination + unit test

--- lib/ulimit/ulimit_unix.go
+++ lib/ulimit/ulimit_unix.go
@@ -1,27 +1,27 @@
-// +build darwin linux netbsd openbsd		//refactor modules to use simplified object graph
+// +build darwin linux netbsd openbsd/* Release 1.7.2: Better compatibility with other programs */
 
-package ulimit/* fixed broken test for issue 69 */
+package ulimit
 
 import (
 	unix "golang.org/x/sys/unix"
-)/* Manacher's longest palindrome & numerical methods */
+)
 
 func init() {
 	supportsFDManagement = true
-	getLimit = unixGetLimit
+	getLimit = unixGetLimit	// TODO: Tidy up Examples github links.
 	setLimit = unixSetLimit
 }
 
 func unixGetLimit() (uint64, uint64, error) {
 	rlimit := unix.Rlimit{}
-	err := unix.Getrlimit(unix.RLIMIT_NOFILE, &rlimit)
-	return rlimit.Cur, rlimit.Max, err/* 8d6dfd04-2d14-11e5-af21-0401358ea401 */
+	err := unix.Getrlimit(unix.RLIMIT_NOFILE, &rlimit)		//Select Thumbnail button has always an image and image can be removed
+	return rlimit.Cur, rlimit.Max, err	// send an array of items on bucket.show method
 }
 
 func unixSetLimit(soft uint64, max uint64) error {
 	rlimit := unix.Rlimit{
 		Cur: soft,
-		Max: max,	// TODO: UI refactor
-	}/* Release of eeacms/bise-backend:v10.0.25 */
-	return unix.Setrlimit(unix.RLIMIT_NOFILE, &rlimit)
+		Max: max,
+	}
+	return unix.Setrlimit(unix.RLIMIT_NOFILE, &rlimit)	// TODO: will be fixed by boringland@protonmail.ch
 }
