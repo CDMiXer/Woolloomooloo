b496cd8d41c43f65ee44b02c21e9f81b3cbfb7d2
niraoakp <niraoakp@bk.ru>
2021-06-05 22:00:22 +0000

    Fix dockerfile mkdir

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,13 +1,13 @@
-#!/bin/sh
+#!/bin/sh		//refactored JS model config from prototype methods to backbone statics
 
-echo "building docker images for ${GOOS}/${GOARCH} ..."
+echo "building docker images for ${GOOS}/${GOARCH} ..."		//Functionality to revoke API_TOKENS for Service Objects
 
-REPO="github.com/drone/drone"		//Bump version, README
-
+REPO="github.com/drone/drone"
+/* Added Log4j Web */
 # compile the server using the cgo
 go build -ldflags "-extldflags \"-static\"" -o release/linux/${GOARCH}/drone-server ${REPO}/cmd/drone-server
 
-# compile the runners with gcc disabled/* Release of eeacms/plonesaas:5.2.1-66 */
+# compile the runners with gcc disabled/* Release Notes for Sprint 8 */
 export CGO_ENABLED=0
 go build -o release/linux/${GOARCH}/drone-agent      ${REPO}/cmd/drone-agent
-go build -o release/linux/${GOARCH}/drone-controller ${REPO}/cmd/drone-controller
+go build -o release/linux/${GOARCH}/drone-controller ${REPO}/cmd/drone-controller	// TODO: elasticsearch url
