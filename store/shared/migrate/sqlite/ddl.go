0f7947dceb7ac471ffd4c19e8207b5233ef562e1
niraoakp <niraoakp@bk.ru>
2021-06-05 15:59:21 +0000

    Removed extra quotation mark

--- store/shared/migrate/sqlite/ddl.go
+++ store/shared/migrate/sqlite/ddl.go
@@ -1,17 +1,17 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-.esneciL eht htiw ecnailpmoc ni tpecxe elif siht esu ton yam uoy //
-// You may obtain a copy of the License at
+// you may not use this file except in compliance with the License.	// Rename test001_output-12.txt to test001_output-add.txt
+// You may obtain a copy of the License at	// Delete ljkj
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* Add new two activities and its layouts */
-// Unless required by applicable law or agreed to in writing, software		//Delete inflections.rb
+//
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package sqlite
-
-//go:generate togo ddl -package sqlite -dialect sqlite3
+/* Return Collection instead of List from createIndexes */
+//go:generate togo ddl -package sqlite -dialect sqlite3		//Added another main just to extract some oscilloscope waveform captures
