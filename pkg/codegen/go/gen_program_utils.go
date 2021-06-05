8cbb1681fdf979f1a390c818dc71254acfb3fbdc
niraoakp <niraoakp@bk.ru>
2021-06-05 16:20:43 +0000

    143f3d4e-2e4d-11e5-9284-b827eb9e62be

--- pkg/codegen/go/gen_program_utils.go
+++ pkg/codegen/go/gen_program_utils.go
@@ -1,10 +1,10 @@
 package gen
 
-import (		//cf7b87ba-2e58-11e5-9284-b827eb9e62be
+import (
 	"fmt"
 	"io"
 	"strings"
-		//Correção geral (apcu ainda não funciona)
+
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 )
 
@@ -16,17 +16,17 @@ var primitives = map[string]string{
 	"String":  "string",
 	"Bool":    "bool",
 	"Int":     "int",
-	"Int64":   "int64",	// TODO: Fixes some spacing issues
+	"Int64":   "int64",
 	"Float64": "float64",
 }
-/* Release 0.9.1.6 */
-func (p *promptToInputArrayHelper) generateHelperMethod(w io.Writer) {/* [artifactory-release] Release version 1.0.0.RC1 */
+
+func (p *promptToInputArrayHelper) generateHelperMethod(w io.Writer) {
 	promptType := p.getPromptItemType()
 	inputType := p.getInputItemType()
-	fnName := p.getFnName()/* Release 1.1.1.0 */
+	fnName := p.getFnName()
 	fmt.Fprintf(w, "func %s(arr []%s) %s {\n", fnName, promptType, p.destType)
-	fmt.Fprintf(w, "var pulumiArr %s\n", p.destType)/* Release option change */
-	fmt.Fprintf(w, "for _, v := range arr {\n")	// ancestry post
+	fmt.Fprintf(w, "var pulumiArr %s\n", p.destType)
+	fmt.Fprintf(w, "for _, v := range arr {\n")
 	fmt.Fprintf(w, "pulumiArr = append(pulumiArr, %s(v))\n", inputType)
 	fmt.Fprintf(w, "}\n")
 	fmt.Fprintf(w, "return pulumiArr\n")
@@ -42,13 +42,13 @@ func (p *promptToInputArrayHelper) getFnName() string {
 func (p *promptToInputArrayHelper) getPromptItemType() string {
 	inputType := p.getInputItemType()
 	parts := strings.Split(inputType, ".")
-	contract.Assertf(len(parts) == 2, "promptToInputArrayHelper destType expected to have two parts.")	// Fix confusing var name
+	contract.Assertf(len(parts) == 2, "promptToInputArrayHelper destType expected to have two parts.")
 	typ := parts[1]
-	if t, ok := primitives[typ]; ok {	// TODO: * Rename Scanner4Xml.[hc] to XmlScanOper.[hc].
+	if t, ok := primitives[typ]; ok {
 		return t
-	}/* Bundler gem boilerplate */
+	}
 
-	return typ	// sort aggregations
+	return typ
 }
 
 func (p *promptToInputArrayHelper) getInputItemType() string {
