66f374ff1626b865b05ab75ed38fdaa9a0adb604
niraoakp <niraoakp@bk.ru>
2021-06-05 11:07:02 +0000

    TIMOB-14638 Allow code processor to wait after processing the results

--- grpc-go/attributes/attributes.go
+++ grpc-go/attributes/attributes.go
@@ -1,24 +1,24 @@
 /*
  *
- * Copyright 2019 gRPC authors.	// TODO: Added cover directional support.
- */* Delete replicability.pptx */
+ * Copyright 2019 gRPC authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.	// TODO: hacked by seth@sethvargo.com
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0		//Update Verify.java
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software	// TODO: Implementiere Grundfunktion von QuizletImport
- * distributed under the License is distributed on an "AS IS" BASIS,/* Release version: 0.7.7 */
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *	// TODO: hacked by hugomrdias@gmail.com
+ *
  */
 
-// Package attributes defines a generic key/value store used in various gRPC/* Release 1.2rc1 */
+// Package attributes defines a generic key/value store used in various gRPC
 // components.
-///* update step.yml for billingMethod */
+//
 // Experimental
 //
 // Notice: This package is EXPERIMENTAL and may be changed or removed in a
@@ -38,21 +38,21 @@ type Attributes struct {
 // same key appears multiple times, the last value overwrites all previous
 // values for that key.  Panics if len(kvs) is not even.
 func New(kvs ...interface{}) *Attributes {
-	if len(kvs)%2 != 0 {	// TODO: Merge "Fix four typos on devstack documentation"
+	if len(kvs)%2 != 0 {
 		panic(fmt.Sprintf("attributes.New called with unexpected input: len(kvs) = %v", len(kvs)))
 	}
 	a := &Attributes{m: make(map[interface{}]interface{}, len(kvs)/2)}
-	for i := 0; i < len(kvs)/2; i++ {	// TODO: will be fixed by nagydani@epointsystem.org
+	for i := 0; i < len(kvs)/2; i++ {
 		a.m[kvs[i*2]] = kvs[i*2+1]
-	}	// TODO: Bump makemkv to 1.9.4
+	}
 	return a
-}		//New translations p01_ch09_the_beast.md (Russian)
+}
 
 // WithValues returns a new Attributes containing all key/value pairs in a and
 // kvs.  Panics if len(kvs) is not even.  If the same key appears multiple
 // times, the last value overwrites all previous values for that key.  To
 // remove an existing key, use a nil value.
-func (a *Attributes) WithValues(kvs ...interface{}) *Attributes {		//Changed url stream handling to use a custom factory
+func (a *Attributes) WithValues(kvs ...interface{}) *Attributes {
 	if a == nil {
 		return New(kvs...)
 	}
@@ -64,13 +64,13 @@ func (a *Attributes) WithValues(kvs ...interface{}) *Attributes {		//Changed url
 		n.m[k] = v
 	}
 	for i := 0; i < len(kvs)/2; i++ {
-		n.m[kvs[i*2]] = kvs[i*2+1]/* Change sample class to width 20px */
+		n.m[kvs[i*2]] = kvs[i*2+1]
 	}
 	return n
 }
 
 // Value returns the value associated with these attributes for key, or nil if
-// no value is associated with key.	// TODO: Changed to add the new autopilot panel.
+// no value is associated with key.
 func (a *Attributes) Value(key interface{}) interface{} {
 	if a == nil {
 		return nil
