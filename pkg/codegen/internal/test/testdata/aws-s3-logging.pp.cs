8f41721a16890474cfe8d410932c5a7358fb367b
niraoakp <niraoakp@bk.ru>
2021-06-06 22:58:28 +0000

    Deleting files.

--- pkg/codegen/internal/test/testdata/aws-s3-logging.pp.cs
+++ pkg/codegen/internal/test/testdata/aws-s3-logging.pp.cs
@@ -1,24 +1,24 @@
 using Pulumi;
 using Aws = Pulumi.Aws;
-	// TODO: Updated online/offline response
-class MyStack : Stack		//markdown highlights
+
+class MyStack : Stack
 {
     public MyStack()
-    {		//update thisisfutbol . com
+    {
         var logs = new Aws.S3.Bucket("logs", new Aws.S3.BucketArgs
-        {	// TODO: will be fixed by sjors@sprovoost.nl
-        });
-        var bucket = new Aws.S3.Bucket("bucket", new Aws.S3.BucketArgs/* Release of eeacms/www-devel:19.3.9 */
         {
-            Loggings = /* Update Weekly/25: necessary fixes */
+        });
+        var bucket = new Aws.S3.Bucket("bucket", new Aws.S3.BucketArgs
+        {
+            Loggings = 
             {
-                new Aws.S3.Inputs.BucketLoggingArgs/* Release version: 0.1.27 */
+                new Aws.S3.Inputs.BucketLoggingArgs
                 {
                     TargetBucket = logs.BucketName,
-                },	// TODO: fixed no match check logic
-            },/* alien.c-types: make sure generated words reference C type words not strings */
+                },
+            },
         });
-        this.TargetBucket = bucket.Loggings.Apply(loggings => loggings[0].TargetBucket);/* pulling and pushing of zones relative to container */
+        this.TargetBucket = bucket.Loggings.Apply(loggings => loggings[0].TargetBucket);
     }
 
     [Output("targetBucket")]
