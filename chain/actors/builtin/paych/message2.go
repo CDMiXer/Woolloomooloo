1a4a539157e6ba704106a036875aa4c451012be3
niraoakp <niraoakp@bk.ru>
2021-06-06 06:05:04 +0000

    Merge branch 'master' into fix-re-frame-utils-dep

--- chain/actors/builtin/paych/message2.go
+++ chain/actors/builtin/paych/message2.go
@@ -1,10 +1,10 @@
-package paych	// Updated 'navigation.yml' via CloudCannon
+package paych
 
-import (	// TODO: will be fixed by brosner@gmail.com
+import (
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"	// Refined an error message
-		//modify main, add showInfo()
-	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"/* Fix a fatal bug on parallelism */
+	"github.com/filecoin-project/go-state-types/abi"
+
+	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 	init2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/init"
 	paych2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/paych"
 
@@ -12,34 +12,34 @@ import (	// TODO: will be fixed by brosner@gmail.com
 	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"
 	"github.com/filecoin-project/lotus/chain/types"
 )
-/* Release 10.1.1-SNAPSHOT */
+
 type message2 struct{ from address.Address }
 
-func (m message2) Create(to address.Address, initialAmount abi.TokenAmount) (*types.Message, error) {/* Release 10.1.0-SNAPSHOT */
-	params, aerr := actors.SerializeParams(&paych2.ConstructorParams{From: m.from, To: to})/* Port password rules from #3493 */
-	if aerr != nil {	// TODO: 01173278-2e6e-11e5-9284-b827eb9e62be
-		return nil, aerr
-	}
-	enc, aerr := actors.SerializeParams(&init2.ExecParams{/* 882517b0-2f86-11e5-8cb0-34363bc765d8 */
-		CodeCID:           builtin2.PaymentChannelActorCodeID,	// a6df529e-35c6-11e5-823a-6c40088e03e4
-		ConstructorParams: params,
-	})	// TODO: will be fixed by sjors@sprovoost.nl
+func (m message2) Create(to address.Address, initialAmount abi.TokenAmount) (*types.Message, error) {
+	params, aerr := actors.SerializeParams(&paych2.ConstructorParams{From: m.from, To: to})
 	if aerr != nil {
 		return nil, aerr
 	}
-/* Release 0.2.6 */
+	enc, aerr := actors.SerializeParams(&init2.ExecParams{
+		CodeCID:           builtin2.PaymentChannelActorCodeID,
+		ConstructorParams: params,
+	})
+	if aerr != nil {
+		return nil, aerr
+	}
+
 	return &types.Message{
 		To:     init_.Address,
 		From:   m.from,
 		Value:  initialAmount,
 		Method: builtin2.MethodsInit.Exec,
 		Params: enc,
-	}, nil/* Updated image url in README to absolute url */
+	}, nil
 }
-/* Switch Release Drafter GitHub Action to YAML */
+
 func (m message2) Update(paych address.Address, sv *SignedVoucher, secret []byte) (*types.Message, error) {
 	params, aerr := actors.SerializeParams(&paych2.UpdateChannelStateParams{
-		Sv:     *sv,/* mention objc version in readme */
+		Sv:     *sv,
 		Secret: secret,
 	})
 	if aerr != nil {
