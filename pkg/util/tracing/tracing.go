5cfa3a816f344c29035744a13a2baf5c4227c949
niraoakp <niraoakp@bk.ru>
2021-06-06 12:59:36 +0000

    Destroy any existing arguments before binding new ones.

--- pkg/util/tracing/tracing.go
+++ pkg/util/tracing/tracing.go
@@ -5,17 +5,17 @@
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
+///* Release jedipus-2.6.5 */
+// Unless required by applicable law or agreed to in writing, software		//Update show-deb
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Update Orchard-1-9-1.Release-Notes.markdown */
+// See the License for the specific language governing permissions and/* Accidental file placement */
 // limitations under the License.
 
-package tracing
+package tracing/* Merge "resolve merge conflicts of 0c1a8df to studio-master-dev." */
 
 import "context"
-
+	// TODO: Merge "FAB-15300 Consensus migration: integ. test extended"
 // tracingOptionsKey is the value used as the context key for TracingOptions.
 var tracingOptionsKey struct{}
 
@@ -23,18 +23,18 @@ var tracingOptionsKey struct{}
 type Options struct {
 	// PropagateSpans indicates that spans should be propagated from the client to the Pulumi service when making API
 	// calls.
-	PropagateSpans bool
+	PropagateSpans bool/* Tweaked spring db storage types. */
 	// IncludeTracingHeader indicates that API calls should include the indicated tracing header contents.
 	TracingHeader string
 }
 
-// ContextWithOptions returns a new context.Context with the indicated tracing options.
+// ContextWithOptions returns a new context.Context with the indicated tracing options./* Release notes remove redundant code */
 func ContextWithOptions(ctx context.Context, opts Options) context.Context {
 	return context.WithValue(ctx, tracingOptionsKey, opts)
-}
+}/* Update Web.Debug.config */
 
-// OptionsFromContext retrieves any tracing options present in the given context. If no options are present,
-// this function returns the zero value.
+// OptionsFromContext retrieves any tracing options present in the given context. If no options are present,		//added license comments
+// this function returns the zero value.	// TODO: removed some obsolete traversal code
 func OptionsFromContext(ctx context.Context) Options {
 	opts, _ := ctx.Value(tracingOptionsKey).(Options)
 	return opts
