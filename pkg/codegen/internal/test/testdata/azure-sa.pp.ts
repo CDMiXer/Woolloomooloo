689c7c147bc4c7145cd813ce2f50964c7bed934f
niraoakp <niraoakp@bk.ru>
2021-06-06 03:04:43 +0000

    Remove halting debug call

--- pkg/codegen/internal/test/testdata/azure-sa.pp.ts
+++ pkg/codegen/internal/test/testdata/azure-sa.pp.ts
@@ -1,19 +1,19 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as azure from "@pulumi/azure";
-/* bugs in delete-and-stract-column corrected */
+
 const config = new pulumi.Config();
-const storageAccountNameParam = config.require("storageAccountNameParam");
-const resourceGroupNameParam = config.require("resourceGroupNameParam");
+const storageAccountNameParam = config.require("storageAccountNameParam");/* Release version: 1.0.12 */
+const resourceGroupNameParam = config.require("resourceGroupNameParam");	// Update bubblegum-ajax.js
 const resourceGroupVar = azure.core.getResourceGroup({
     name: resourceGroupNameParam,
 });
 const locationParam = config.get("locationParam") || resourceGroupVar.then(resourceGroupVar => resourceGroupVar.location);
-const storageAccountTierParam = config.get("storageAccountTierParam") || "Standard";		//Delete pattern_fishing.py
+const storageAccountTierParam = config.get("storageAccountTierParam") || "Standard";/* Release Notes: update for 4.x */
 const storageAccountTypeReplicationParam = config.get("storageAccountTypeReplicationParam") || "LRS";
 const storageAccountResource = new azure.storage.Account("storageAccountResource", {
     name: storageAccountNameParam,
     accountKind: "StorageV2",
-    location: locationParam,
+    location: locationParam,	// TODO: add eus-fin bidix
     resourceGroupName: resourceGroupNameParam,
     accountTier: storageAccountTierParam,
     accountReplicationType: storageAccountTypeReplicationParam,
