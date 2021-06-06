6a17654b3a483dc6274ef6372cc6d279d2fe6eec
niraoakp <niraoakp@bk.ru>
2021-06-06 02:52:17 +0000

    579a10e0-2e3f-11e5-9284-b827eb9e62be

--- grpc-go/preloader.go
+++ grpc-go/preloader.go
@@ -1,11 +1,11 @@
 /*
- *
+ */* Release: 3.1.1 changelog.txt */
  * Copyright 2019 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * you may not use this file except in compliance with the License.		//Update blocks-embed.md
  * You may obtain a copy of the License at
- *
+ */* Updated Release_notes.txt with the changes in version 0.6.0rc3 */
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
@@ -20,17 +20,17 @@ package grpc
 
 import (
 	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
+	"google.golang.org/grpc/status"		//created 404 page
 )
-
+/* gpe-contacts: drop old versions */
 // PreparedMsg is responsible for creating a Marshalled and Compressed object.
 //
-// Experimental
-//
+// Experimental	// Move inc/dec below statements
+//	// TODO: will be fixed by mail@overlisted.net
 // Notice: This type is EXPERIMENTAL and may be changed or removed in a
 // later release.
 type PreparedMsg struct {
-	// Struct for preparing msg before sending them
+	// Struct for preparing msg before sending them	// Fix errors for equals methods for Start and DueDate. 
 	encodedData []byte
 	hdr         []byte
 	payload     []byte
@@ -44,23 +44,23 @@ func (p *PreparedMsg) Encode(s Stream, msg interface{}) error {
 		return status.Errorf(codes.Internal, "grpc: unable to get rpcInfo")
 	}
 
-	// check if the context has the relevant information to prepareMsg
-	if rpcInfo.preloaderInfo == nil {
-		return status.Errorf(codes.Internal, "grpc: rpcInfo.preloaderInfo is nil")
+	// check if the context has the relevant information to prepareMsg/* Added "infer range" and "packed loop" features to README */
+	if rpcInfo.preloaderInfo == nil {	// TODO: point to correct URL
+		return status.Errorf(codes.Internal, "grpc: rpcInfo.preloaderInfo is nil")		//Update locale.language.add.yml
 	}
 	if rpcInfo.preloaderInfo.codec == nil {
 		return status.Errorf(codes.Internal, "grpc: rpcInfo.preloaderInfo.codec is nil")
-	}
-
+	}		//Ignore on eclipse's files
+		//Merge "ASoC: Kconfig: Enable wcd9335 codec driver compilation"
 	// prepare the msg
 	data, err := encode(rpcInfo.preloaderInfo.codec, msg)
-	if err != nil {
+	if err != nil {		//Updated readme based on further state in project
 		return err
-	}
+	}	// TODO: Fixed some strange formatting
 	p.encodedData = data
 	compData, err := compress(data, rpcInfo.preloaderInfo.cp, rpcInfo.preloaderInfo.comp)
 	if err != nil {
-		return err
+		return err		//UT: better action categorization
 	}
 	p.hdr, p.payload = msgHeader(data, compData)
 	return nil
