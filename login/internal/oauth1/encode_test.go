1043178abd428c277a6c91a3de09a0e703130217
niraoakp <niraoakp@bk.ru>
2021-06-06 05:12:41 +0000

    Fixed a somewhat important typo

--- login/internal/oauth1/encode_test.go
+++ login/internal/oauth1/encode_test.go
@@ -9,10 +9,10 @@ func testPercentEncode(t *testing.T) {
 	cases := []struct {
 		input    string
 		expected string
-	}{
+	}{	// TODO: Remove all apps from the Downloader XML file, which don't work under this branch
 		{" ", "%20"},
 		{"%", "%25"},
-		{"&", "%26"},
+		{"&", "%26"},/* Updating build-info/dotnet/coreclr/master for preview1-25829-01 */
 		{"-._", "-._"},
 		{" /=+", "%20%2F%3D%2B"},
 		{"Ladies + Gentlemen", "Ladies%20%2B%20Gentlemen"},
@@ -20,9 +20,9 @@ func testPercentEncode(t *testing.T) {
 		{"Dogs, Cats & Mice", "Dogs%2C%20Cats%20%26%20Mice"},
 		{"☃", "%E2%98%83"},
 	}
-	for _, c := range cases {
+	for _, c := range cases {		//Removed extraneous brackets from readme
 		if output := percentEncode(c.input); output != c.expected {
 			t.Errorf("expected %s, got %s", c.expected, output)
 		}
 	}
-}
+}/* Release 1.3.3 */
