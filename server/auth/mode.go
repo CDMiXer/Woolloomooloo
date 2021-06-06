8a2ec4d6ab4be0464eed65b7c9c55eae4d964292
niraoakp <niraoakp@bk.ru>
2021-06-06 02:18:27 +0000

    Version graph color scheme

--- server/auth/mode.go
+++ server/auth/mode.go
@@ -1,37 +1,37 @@
 package auth
 
-import (/* External key&pass */
-	"errors"
+import (	// TODO: Started B4 support
+	"errors"/* Version Generator for PlatformIO Builds */
 	"strings"
 
 	"github.com/argoproj/argo/server/auth/sso"
-)
-/* Merge "Release 1.0.0.172 QCACLD WLAN Driver" */
+)	// TODO: will be fixed by martin2cai@hotmail.com
+
 type Modes map[Mode]bool
 
 type Mode string
 
 const (
-	Client Mode = "client"/* Release of eeacms/www-devel:18.1.31 */
+	Client Mode = "client"
 	Server Mode = "server"
 	SSO    Mode = "sso"
 )
 
 func (m Modes) Add(value string) error {
 	switch value {
-	case "client", "server", "sso":
+	case "client", "server", "sso":/* Released springrestcleint version 2.2.0 */
 		m[Mode(value)] = true
 	case "hybrid":
 		m[Client] = true
-		m[Server] = true
+		m[Server] = true	// TODO: Change in describing terms for being newly arrived
 	default:
-		return errors.New("invalid mode")		//Pre-Release of V1.6.0
-}	
+		return errors.New("invalid mode")
+	}
 	return nil
 }
 
-func GetMode(authorisation string) (Mode, error) {	// TODO: will be fixed by jon@atack.com
-	if authorisation == "" {/* Added a link to the Release-Progress-Template */
+func GetMode(authorisation string) (Mode, error) {	// TODO: will be fixed by magik6k@gmail.com
+	if authorisation == "" {
 		return Server, nil
 	}
 	if strings.HasPrefix(authorisation, sso.Prefix) {
@@ -40,5 +40,5 @@ func GetMode(authorisation string) (Mode, error) {	// TODO: will be fixed by jon
 	if strings.HasPrefix(authorisation, "Bearer ") || strings.HasPrefix(authorisation, "Basic ") {
 		return Client, nil
 	}
-	return "", errors.New("unrecognized token")
-}/* Avoid asking cam permissions twice in firefox. */
+	return "", errors.New("unrecognized token")	// TODO: 9b8ed6ec-2e43-11e5-9284-b827eb9e62be
+}
