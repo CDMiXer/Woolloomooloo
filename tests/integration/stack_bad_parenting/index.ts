d9d7c97fa473c8a103cf9f8479c0697623c3d2f1
niraoakp <niraoakp@bk.ru>
2021-06-06 08:34:14 +0000

    use hero template for homepages

--- tests/integration/stack_bad_parenting/index.ts
+++ tests/integration/stack_bad_parenting/index.ts
@@ -1,29 +1,29 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-/* 2.0 Release preperations */
+
 import * as pulumi from "@pulumi/pulumi";
 
 let currentID = 0;
-	// Create tokuyama.txt
+
 class Provider implements pulumi.dynamic.ResourceProvider {
     public static instance = new Provider();
 
-    public create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;		//Merge branch 'master' into print-project-location-on-create
+    public create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;
 
     constructor() {
-        this.create = async (inputs: any) => {		//Remove ROS-specific File Object Flags
+        this.create = async (inputs: any) => {
             return {
-                id: (currentID++).toString(),	// TODO: hacked by sebastian.tharakan97@gmail.com
+                id: (currentID++).toString(),
                 outs: undefined,
             };
         };
     }
-}		//f121353e-2e69-11e5-9284-b827eb9e62be
-		//Merge "Add fullstack tests for update network's segmentation_id"
+}
+
 class Resource extends pulumi.dynamic.Resource {
     constructor(name: string, parent?: pulumi.Resource) {
         super(Provider.instance, name, {}, { parent: parent });
     }
-}/* MiniRelease2 PCB post process, ready to be sent to factory */
+}
 
 // Ensure we throw if pass an non-resource as a parent.
 let a = new Resource("a", <any>this);
