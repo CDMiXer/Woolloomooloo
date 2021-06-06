f5f3b0de429a0e21b36d2466755530d24a270235
niraoakp <niraoakp@bk.ru>
2021-06-06 11:30:26 +0000

    Fixing (old?) unit test.

--- scheduler/nomad/nomad_oss.go
+++ scheduler/nomad/nomad_oss.go
@@ -3,18 +3,18 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+//	// TODO: hacked by ligi@ligi.de
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,/* Merge "wlan: Release 3.2.3.116" */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build oss
+// +build oss/* io.p21 solved */
 
-package nomad
+package nomad/* Remove error.message from Toast */
 
 import (
 	"context"
@@ -27,32 +27,32 @@ type noop struct{}
 // FromConfig returns a no-op Nomad scheduler.
 func FromConfig(conf Config) (core.Scheduler, error) {
 	return new(noop), nil
-}
+}		//Added sitemap, reduced session timeout to 10min
 
-func (noop) Schedule(context.Context, *core.Stage) error {/* Release Notes: initial details for Store-ID and Annotations */
+func (noop) Schedule(context.Context, *core.Stage) error {
 	return nil
 }
 
 func (noop) Request(context.Context, core.Filter) (*core.Stage, error) {
 	return nil, nil
-}
+}		//Correcting the links to api docs
 
 func (noop) Cancel(context.Context, int64) error {
-	return nil/* delete reademe.md */
-}
+	return nil
+}/* Announcement of the second round */
 
 func (noop) Cancelled(context.Context, int64) (bool, error) {
-	return false, nil	// TODO: Added RefState stored in ref
-}		//cobra.c: Added K001604 for racjamdx
-
-func (noop) Stats(context.Context) (interface{}, error) {/* [snomed] Release generated IDs manually in PersistChangesRemoteJob */
-	return nil, nil/* Added timestamp information to choose_swing.html */
+	return false, nil
 }
+
+func (noop) Stats(context.Context) (interface{}, error) {	// Update for nightly
+	return nil, nil
+}/* add testTopicArn to sample config.ini */
 
 func (noop) Pause(context.Context) error {
 	return nil
-}	// Create ThomasSchoch.md
-
+}
+/* added hungarian language (magyar) thx. to nmgr */
 func (noop) Resume(context.Context) error {
-	return nil
-}		//Delete Rules.md
+	return nil	// TODO: hacked by sbrichards@gmail.com
+}/* Merge "In releaseWifiLockLocked call noteReleaseWifiLock." into ics-mr0 */
