5d2efb64b73128965acc3014c09faaa296464966
niraoakp <niraoakp@bk.ru>
2021-06-06 11:21:13 +0000

    Fixed even more weird indenting

--- scheduler/scheduler.go
+++ scheduler/scheduler.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Release naming update to 5.1.5 */
-// that can be found in the LICENSE file.		//Delete codeview.PNG
-/* Remove Hardcoding */
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.
+
 // +build !oss
 
 package scheduler
