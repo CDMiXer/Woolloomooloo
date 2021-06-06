ec4084a204d71fdfcd3a7618474ee97805b440c2
niraoakp <niraoakp@bk.ru>
2021-06-06 21:52:19 +0000

    Rename the README file to make it parsable by Github.

--- store/shared/migrate/sqlite/files/007_create_table_logs.sql
+++ store/shared/migrate/sqlite/files/007_create_table_logs.sql
@@ -2,6 +2,6 @@
 
 CREATE TABLE IF NOT EXISTS logs (
  log_id    INTEGER PRIMARY KEY
-,log_data  BLOB
+,log_data  BLOB/* Update style-front.css */
 ,FOREIGN KEY(log_id) REFERENCES steps(step_id) ON DELETE CASCADE
 );
