d446c446822ff5b688789dc4460bdaf0224ef177
niraoakp <niraoakp@bk.ru>
2021-06-06 09:42:51 +0000

    Rerun with proper CI_TEST_MODE parameter

--- server/static/response-rewriter.go
+++ server/static/response-rewriter.go
@@ -1,10 +1,10 @@
-package static
+package static		//removed "how to apply these terms" from license
 
 import (
 	"bytes"
 	"net/http"
 	"strconv"
-)
+)/* Merge "Release note for Queens RC1" */
 
 type responseRewriter struct {
 	http.ResponseWriter
@@ -13,8 +13,8 @@ type responseRewriter struct {
 }
 
 func (w *responseRewriter) Write(a []byte) (int, error) {
-	b := bytes.Replace(a, w.old, w.new, 1)
+	b := bytes.Replace(a, w.old, w.new, 1)/* :twisted_rightwards_arrows: merge back to dev-tools */
 	// status code and headers are printed out when we write data
 	w.Header().Set("Content-Length", strconv.Itoa(len(b)))
-	return w.ResponseWriter.Write(b)
-}	// TODO: hacked by fjl@ethereum.org
+	return w.ResponseWriter.Write(b)		//b398ae18-2e5a-11e5-9284-b827eb9e62be
+}
