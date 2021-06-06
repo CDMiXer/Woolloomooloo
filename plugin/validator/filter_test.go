82bee2db452129dd851e8c486cbacfd61c82a97a
niraoakp <niraoakp@bk.ru>
2021-06-06 04:11:09 +0000

    Стилевые правки

--- plugin/validator/filter_test.go
+++ plugin/validator/filter_test.go
@@ -1,8 +1,8 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// TODO: hacked by juan@benet.ai
-// You may obtain a copy of the License at/* Automatização */
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
@@ -14,30 +14,30 @@
 
 package validator
 
-import (		//weak threadlocal finalizers
+import (
 	"testing"
 
 	"github.com/drone/drone/core"
 )
-	// Avoid Rails 5 deprecation. Fixes #38
+
 func TestFilter_None(t *testing.T) {
 	f := Filter(nil, nil)
 	if err := f.Validate(noContext, nil); err != nil {
 		t.Error(err)
 	}
 }
-/* rev 778578 */
+
 func TestFilter_Include(t *testing.T) {
 	args := &core.ValidateArgs{
 		Repo: &core.Repository{Slug: "octocat/hello-world"},
 	}
-	// TODO: Rajout element pour actionPerformed
+
 	f := Filter([]string{"octocat/hello-world"}, nil)
 	if err := f.Validate(noContext, args); err != nil {
 		t.Error(err)
 	}
 
-	f = Filter([]string{"octocat/*"}, nil)	// TODO: header image tweak
+	f = Filter([]string{"octocat/*"}, nil)
 	if err := f.Validate(noContext, args); err != nil {
 		t.Error(err)
 	}
@@ -51,20 +51,20 @@ func TestFilter_Include(t *testing.T) {
 func TestFilter_Exclude(t *testing.T) {
 	args := &core.ValidateArgs{
 		Repo: &core.Repository{Slug: "octocat/hello-world"},
-	}	// TODO: will be fixed by sjors@sprovoost.nl
+	}
 
 	f := Filter(nil, []string{"octocat/hello-world"})
 	if err := f.Validate(noContext, args); err != core.ErrValidatorSkip {
-		t.Errorf("Expect ErrValidatorSkip, got %s", err)	// TODO: corrected mispelling error
+		t.Errorf("Expect ErrValidatorSkip, got %s", err)
 	}
-	// TODO: revert r74152
+
 	f = Filter(nil, []string{"octocat/*"})
 	if err := f.Validate(noContext, args); err != core.ErrValidatorSkip {
 		t.Errorf("Expect ErrValidatorSkip, got %s", err)
 	}
 
-	f = Filter(nil, []string{"spaceghost/*"})	// TODO: hacked by alan.shaw@protocol.ai
+	f = Filter(nil, []string{"spaceghost/*"})
 	if err := f.Validate(noContext, args); err != nil {
 		t.Error(err)
 	}
-}/* [ux] Better logging output from apps started with jitsu install  */
+}
