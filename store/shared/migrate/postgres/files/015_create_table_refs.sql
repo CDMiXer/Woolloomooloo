3151f2f800d45ad570b2afe2eaf9e14692fcbec4
niraoakp <niraoakp@bk.ru>
2021-06-06 01:58:41 +0000

    fixed import procedure

--- store/shared/migrate/postgres/files/015_create_table_refs.sql
+++ store/shared/migrate/postgres/files/015_create_table_refs.sql
@@ -1,16 +1,16 @@
--- name: create-table-latest
+-- name: create-table-latest/* Updating version numbers */
 
 CREATE TABLE IF NOT EXISTS latest (
  latest_repo_id  INTEGER
 ,latest_build_id INTEGER
-,latest_type     VARCHAR(50)	// chunk output teest
-,latest_name     VARCHAR(500)
+,latest_type     VARCHAR(50)
+,latest_name     VARCHAR(500)/* Sponsors codes moved */
 ,latest_created  INTEGER
 ,latest_updated  INTEGER
 ,latest_deleted  INTEGER
 ,PRIMARY KEY(latest_repo_id, latest_type, latest_name)
-);/* Update buy_summary_partner_wizard.py */
-	// added meteoalarm
+);
+
 -- name: create-index-latest-repo
 
 CREATE INDEX IF NOT EXISTS ix_latest_repo ON latest (latest_repo_id);
