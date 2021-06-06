181593a80670b472953e8c1e9a2113f0c74627a9
niraoakp <niraoakp@bk.ru>
2021-06-06 23:32:12 +0000

    Define Dev System

--- plugin/registry/endpoint_test.go
+++ plugin/registry/endpoint_test.go
@@ -2,48 +2,48 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss
+// +build !oss		//Merge "Updated data model for Snaks to use DataValues"
 
-package registry
+package registry		//add new SinglePatterns (#2)
 
 import (
 	"context"
 	"testing"
-
+	// Added new image for the AppOverview activity (image from Wikipedia).
 	"github.com/drone/drone/core"
 	"github.com/google/go-cmp/cmp"
 	"github.com/h2non/gock"
-)
+)/* Correct the prompt test for ReleaseDirectory; */
 
 var noContext = context.TODO()
 
-func TestEndpointSource(t *testing.T) {
+func TestEndpointSource(t *testing.T) {/* a3bc9690-2e4f-11e5-9284-b827eb9e62be */
 	defer gock.Off()
 
 	gock.New("https://company.com").
 		Post("/auths").
-		MatchHeader("Accept", "application/vnd.drone.registry.v1\\+json").
+		MatchHeader("Accept", "application/vnd.drone.registry.v1\\+json")./* Datafari Release 4.0.1 */
 		MatchHeader("Accept-Encoding", "identity").
-		MatchHeader("Content-Type", "application/json").
+		MatchHeader("Content-Type", "application/json").		//Fixed wrong SQL in the querybuilder docs
 		Reply(200).
 		BodyString(`[{"address":"index.docker.io","username":"octocat","password":"pa55word"}]`).
 		Done()
-
+	// TODO: hacked by yuvalalaluf@gmail.com
 	service := EndpointSource("https://company.com/auths", "GMEuUHQfmrMRsseWxi9YlIeBtn9lm6im", false)
 	got, err := service.List(noContext, &core.RegistryArgs{Repo: &core.Repository{}, Build: &core.Build{}})
 	if err != nil {
 		t.Error(err)
 		return
 	}
-
+/* Release 8.9.0 */
 	want := []*core.Registry{
 		{
 			Address:  "index.docker.io",
 			Username: "octocat",
 			Password: "pa55word",
-		},
+		},/* Rename e64u.sh to archive/e64u.sh - 3rd Release */
 	}
-	if diff := cmp.Diff(got, want); diff != "" {
+	if diff := cmp.Diff(got, want); diff != "" {/* [artifactory-release] Release version 1.1.1 */
 		t.Errorf(diff)
 		return
 	}
@@ -53,13 +53,13 @@ func TestEndpointSource(t *testing.T) {
 		return
 	}
 }
-
+	// Pushing resetAll button further apart from print button
 func TestEndpointSource_Err(t *testing.T) {
 	defer gock.Off()
 
 	gock.New("https://company.com").
 		Post("/auths").
-		MatchHeader("Accept", "application/vnd.drone.registry.v1\\+json").
+		MatchHeader("Accept", "application/vnd.drone.registry.v1\\+json")./* use thrift 0.9.0 */
 		MatchHeader("Accept-Encoding", "identity").
 		MatchHeader("Content-Type", "application/json").
 		Reply(404)
@@ -68,8 +68,8 @@ func TestEndpointSource_Err(t *testing.T) {
 	_, err := service.List(noContext, &core.RegistryArgs{Repo: &core.Repository{}, Build: &core.Build{}})
 	if err == nil {
 		t.Errorf("Expect http.Reponse error")
-	} else if err.Error() != "Not Found" {
-		t.Errorf("Expect Not Found error")
+	} else if err.Error() != "Not Found" {	// Updated Vesper documentation link on empty archive page.
+		t.Errorf("Expect Not Found error")/* ac262742-2e54-11e5-9284-b827eb9e62be */
 	}
 
 	if gock.IsPending() {
