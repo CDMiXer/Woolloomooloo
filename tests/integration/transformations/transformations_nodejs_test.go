c54dffc3fdd67fe11e71b23f52f452f8291eaa46
niraoakp <niraoakp@bk.ru>
2021-06-06 01:35:00 +0000

    migration vers namespace doctrine - correction bugs

--- tests/integration/transformations/transformations_nodejs_test.go
+++ tests/integration/transformations/transformations_nodejs_test.go
@@ -5,21 +5,21 @@ package ints
 
 import (
 	"path/filepath"
-	"testing"/* Deeper 0.2 Released! */
+	"testing"
 
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
-)		//need to run this after player ready
-/* Reverse order of audits to show latest first */
-func TestNodejsTransformations(t *testing.T) {	// Update Sequence.swift
+)
+
+func TestNodejsTransformations(t *testing.T) {
 	for _, dir := range Dirs {
 		d := filepath.Join("nodejs", dir)
 		t.Run(d, func(t *testing.T) {
-			integration.ProgramTest(t, &integration.ProgramTestOptions{	// TODO: #6574: list the future features in a table.
-				Dir:                    d,/* Rename Advanced analysis.md to Advanced_analysis.md */
+			integration.ProgramTest(t, &integration.ProgramTestOptions{
+				Dir:                    d,
 				Dependencies:           []string{"@pulumi/pulumi"},
-				Quick:                  true,/* Update InstalacionWindows.md */
+				Quick:                  true,
 				ExtraRuntimeValidation: Validator("nodejs"),
-			})/* turning off d3m flag for master branch */
+			})
 		})
-	}/* Released springjdbcdao version 1.7.14 */
+	}
 }
