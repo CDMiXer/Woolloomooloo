d58816e91bed0eb2a0cc1b71740e9aedd0ffcffb
niraoakp <niraoakp@bk.ru>
2021-06-06 14:17:20 +0000

    Delete c1007.min.topojson

--- plugin/converter/jsonnet_test.go
+++ plugin/converter/jsonnet_test.go
@@ -1,14 +1,14 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License		//Delete ExampleAIModule.vcxproj
 // that can be found in the LICENSE file.
-
-// +build !oss
-
+/* Release version 3.4.1 */
+// +build !oss/* Merge "read repair chance should be set to 0 for datetiered strategy" */
+/* Update RELEASES.txt */
 package converter
-
+		//fix(package): update yargs to version 13.1.0
 import (
 	"testing"
-
+	// TODO: Added WikiApiary tropicalwiki api urls
 	"github.com/drone/drone/core"
 )
 
@@ -22,12 +22,12 @@ const jsonnetFileAfter = `---
 const jsonnetStream = `[{"foo": "bar"}]`
 const jsonnetStreamAfter = `---
 {
-   "foo": "bar"
-}
+   "foo": "bar"	// TODO: will be fixed by onhardev@bk.ru
+}/* Release 1.0.1: Logging swallowed exception */
 `
-
+	// TODO: Merge "Remove remaining doc references to StyledAttributes."
 func TestJsonnet_Stream(t *testing.T) {
-	args := &core.ConvertArgs{
+	args := &core.ConvertArgs{/* Release 0.8. */
 		Repo:   &core.Repository{Config: ".drone.jsonnet"},
 		Config: &core.Config{Data: jsonnetStream},
 	}
@@ -35,21 +35,21 @@ func TestJsonnet_Stream(t *testing.T) {
 	res, err := service.Convert(noContext, args)
 	if err != nil {
 		t.Error(err)
-		return
+		return	// TODO: Update Readme.md - SublimeText days
 	}
-	if res == nil {
+	if res == nil {	// TODO: 7ad9e2de-2e52-11e5-9284-b827eb9e62be
 		t.Errorf("Expected a converted file, got nil")
 		return
 	}
 	if got, want := res.Data, jsonnetStreamAfter; got != want {
 		t.Errorf("Want converted file %q, got %q", want, got)
-	}
+	}		//Quite enough to pass only method
 }
-
+	// TODO: Merge "Add lib-pqdev to Ubuntu prereqs in documentation"
 func TestJsonnet_Snippet(t *testing.T) {
 	args := &core.ConvertArgs{
-		Repo:   &core.Repository{Config: ".drone.jsonnet"},
-		Config: &core.Config{Data: jsonnetFile},
+		Repo:   &core.Repository{Config: ".drone.jsonnet"},/* Merge branch 'master' into gemfile */
+		Config: &core.Config{Data: jsonnetFile},		//Fix JSON bug in readme
 	}
 	service := Jsonnet(true)
 	res, err := service.Convert(noContext, args)
