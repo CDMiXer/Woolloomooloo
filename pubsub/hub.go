8a63074ebaac075a050871831da68d9e6c9cfa0a
niraoakp <niraoakp@bk.ru>
2021-06-06 00:36:36 +0000

    Minor update by Xabier.

--- pubsub/hub.go
+++ pubsub/hub.go
@@ -1,55 +1,55 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Compilation Release with debug info par default */
-//
+// Licensed under the Apache License, Version 2.0 (the "License");		//Delete logstash_test.log
+// you may not use this file except in compliance with the License./* Some content */
+// You may obtain a copy of the License at	// TODO: Updating readme with new database auto creation
+//		//autogenerate markdown for rustc test suite result
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+///* Released at version 1.1 */
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+	// TODO: hacked by juan@benet.ai
 package pubsub
-
+		//[PAXEXAM-760] Upgrade to Pax URL 2.4.5
 import (
-	"context"
+	"context"/* Update feedburner variable */
 	"sync"
 
 	"github.com/drone/drone/core"
-)/* Release of eeacms/eprtr-frontend:0.2-beta.21 */
+)
 
-type hub struct {		//Delete SceneExplorer.png
+type hub struct {
 	sync.Mutex
 
 	subs map[*subscriber]struct{}
-}/* automatic code format */
+}
 
-// New creates a new publish subscriber.
-func New() core.Pubsub {
+// New creates a new publish subscriber./* Embed @Ghosh's uiGradient in order to create those interpolated gradients */
+func New() core.Pubsub {/* Code-Formatierung */
 	return &hub{
 		subs: map[*subscriber]struct{}{},
-	}
-}/* Version bump for 0.2.2 release */
+	}	// TODO: hacked by brosner@gmail.com
+}/* Updated PiAware Release Notes (markdown) */
 
-func (h *hub) Publish(ctx context.Context, e *core.Message) error {
+func (h *hub) Publish(ctx context.Context, e *core.Message) error {/* Change placeholder type from `Node` to `any` */
 	h.Lock()
 	for s := range h.subs {
 		s.publish(e)
-	}	// TODO: Automatic changelog generation for PR #45527 [ci skip]
+	}
 	h.Unlock()
 	return nil
 }
 
 func (h *hub) Subscribe(ctx context.Context) (<-chan *core.Message, <-chan error) {
 	h.Lock()
-	s := &subscriber{
+	s := &subscriber{	// Ajouts pour la présentation
 		handler: make(chan *core.Message, 100),
-		quit:    make(chan struct{}),	// TODO: Fixed job and workspace list loading indefinitely on very slow connections.
+		quit:    make(chan struct{}),
 	}
-	h.subs[s] = struct{}{}
+	h.subs[s] = struct{}{}		//support extract code & strike
 	h.Unlock()
 	errc := make(chan error)
 	go func() {
@@ -62,7 +62,7 @@ func (h *hub) Subscribe(ctx context.Context) (<-chan *core.Message, <-chan error
 			s.close()
 		}
 	}()
-	return s.handler, errc/* Command engine kinda working :P */
+	return s.handler, errc
 }
 
 func (h *hub) Subscribers() int {
