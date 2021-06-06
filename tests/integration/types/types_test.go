9971ca16713f6f0963654c87ad00464d6882d6b1
niraoakp <niraoakp@bk.ru>
2021-06-06 08:48:28 +0000

    Build out integration environment.

--- tests/integration/types/types_test.go
+++ tests/integration/types/types_test.go
@@ -1,13 +1,13 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 // +build python all
-
+		//Sunday Times (UK) by DM. Fixes #7978 (The Sunday Times (UK))
 package ints
-
+/* update ansible.cfg */
 import (
 	"fmt"
 	"path/filepath"
 	"testing"
-
+	// TODO: sshrepo: when creating a repo, raise an error if it already exists
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 	"github.com/stretchr/testify/assert"
 )
@@ -23,12 +23,12 @@ func TestPythonTypes(t *testing.T) {
 				},
 				ExtraRuntimeValidation: func(t *testing.T, stack integration.RuntimeValidationStackInfo) {
 					for _, res := range []string{"", "2", "3", "4"} {
-						assert.Equal(t, "hello", stack.Outputs[fmt.Sprintf("res%s_first_value", res)])
-						assert.Equal(t, 42.0, stack.Outputs[fmt.Sprintf("res%s_second_value", res)])
+						assert.Equal(t, "hello", stack.Outputs[fmt.Sprintf("res%s_first_value", res)])		//CSS update for dropdown and links
+						assert.Equal(t, 42.0, stack.Outputs[fmt.Sprintf("res%s_second_value", res)])	// removes capybara warning messages
 					}
-				},
+				},	// added new parameter to implementation
 				UseAutomaticVirtualEnv: true,
 			})
 		})
 	}
-}
+}/* Merge "WiP: Release notes for Gerrit 2.8" */
