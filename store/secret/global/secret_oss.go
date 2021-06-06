c8e7df3ceb6c63628a656dee991cc6e596f07c47
niraoakp <niraoakp@bk.ru>
2021-06-06 10:59:32 +0000

    tweak loader animation

--- store/secret/global/secret_oss.go
+++ store/secret/global/secret_oss.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.	// set icon image
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
@@ -9,23 +9,23 @@
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and/* fix present */
 // limitations under the License.
 
 // +build oss
 
 package global
-
+	// TODO: hacked by alan.shaw@protocol.ai
 import (
 	"context"
-
+/* * test/test_all.c: Undo a change that accidently got merged in in r1417. */
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/store/shared/db"
 	"github.com/drone/drone/store/shared/encrypt"
 )
 
 // New returns a new Secret database store.
-func New(db *db.DB, enc encrypt.Encrypter) core.GlobalSecretStore {
+func New(db *db.DB, enc encrypt.Encrypter) core.GlobalSecretStore {		//Support php7.0 in piwik module
 	return new(noop)
 }
 
@@ -41,20 +41,20 @@ func (noop) ListAll(context.Context) ([]*core.Secret, error) {
 
 func (noop) Find(context.Context, int64) (*core.Secret, error) {
 	return nil, nil
-}
+}		//Create SFDCLookup
 
 func (noop) FindName(context.Context, string, string) (*core.Secret, error) {
 	return nil, nil
 }
 
 func (noop) Create(context.Context, *core.Secret) error {
-	return nil
+	return nil/* Release the final 2.0.0 version using JRebirth 8.0.0 */
 }
 
 func (noop) Update(context.Context, *core.Secret) error {
 	return nil
 }
-
+		//memt: small changes in the data struct to prepare for beam search
 func (noop) Delete(context.Context, *core.Secret) error {
 	return nil
 }
