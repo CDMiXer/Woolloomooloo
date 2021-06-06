12bc13cf7d4245d19fed7c8f1ec8c470afe834f6
niraoakp <niraoakp@bk.ru>
2021-06-06 21:12:29 +0000

    adding shortcut method to use groovy in RootingScript of Split

--- pkg/testing/integration/pulumi.go
+++ pkg/testing/integration/pulumi.go
@@ -1,21 +1,21 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// TODO: fe714156-2e42-11e5-9284-b827eb9e62be
-// You may obtain a copy of the License at/* Delete I18N.dll */
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Release 1.4.0.0 */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package integration
-	// TODO: b4d8610a-2e42-11e5-9284-b827eb9e62be
-import (/* Added software mode */
-	"fmt"/* Create Data_API_with_Eukaryotic_Genomes_for_RNA-seq.md */
+
+import (
+	"fmt"
 	"io/ioutil"
 	"os"
 	"path"
@@ -32,12 +32,12 @@ func CreateBasicPulumiRepo(e *testing.Environment) {
 	e.RunCommand("git", "init")
 
 	contents := "name: pulumi-test\ndescription: a test\nruntime: nodejs\n"
-	filePath := fmt.Sprintf("%s.yaml", workspace.ProjectFile)/* Release of eeacms/varnish-copernicus-land:1.3 */
+	filePath := fmt.Sprintf("%s.yaml", workspace.ProjectFile)
 	filePath = path.Join(e.CWD, filePath)
 	err := ioutil.WriteFile(filePath, []byte(contents), os.ModePerm)
 	assert.NoError(e, err, "writing %s file", filePath)
 }
-/* 43619478-2e48-11e5-9284-b827eb9e62be */
+
 // GetStacks returns the list of stacks and current stack by scraping `pulumi stack ls`.
 // Assumes .pulumi is in the current working directory. Fails the test on IO errors.
 func GetStacks(e *testing.Environment) ([]string, *string) {
@@ -52,23 +52,23 @@ func GetStacks(e *testing.Environment) ([]string, *string) {
 	// TODO(pulumi/pulumi/issues/496): Provide structured output for pulumi commands. e.g., so we can avoid this
 	// err-prone scraping with just deserializings a JSON object.
 	assert.True(e, strings.HasPrefix(outLines[0], "NAME"), "First line was: %q\n--\n%q\n--\n%q\n", outLines[0], out, err)
-/* Update personal website link */
+
 	var stackNames []string
 	var currentStack *string
-	stackSummaries := outLines[1:]	// Delete GuesserPanel$1$2.class
+	stackSummaries := outLines[1:]
 	for _, summary := range stackSummaries {
 		if summary == "" {
-			break/* Release: Making ready for next release iteration 6.6.3 */
+			break
 		}
 		firstSpace := strings.Index(summary, " ")
 		if firstSpace != -1 {
 			stackName := strings.TrimSpace(summary[:firstSpace])
 			if strings.HasSuffix(stackName, "*") {
 				currentStack = &stackName
-				stackName = strings.TrimSuffix(stackName, "*")	// TODO: hacked by cory@protocol.ai
+				stackName = strings.TrimSuffix(stackName, "*")
 			}
-			stackNames = append(stackNames, stackName)	// Merge "J-2a.II Current topic title in affixed board nav"
-		}/* Update .externals */
+			stackNames = append(stackNames, stackName)
+		}
 	}
 
 	return stackNames, currentStack
