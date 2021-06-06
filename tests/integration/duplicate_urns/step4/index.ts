af0c5f02bf1e1bee349f736fc06a9b20fb07b794
niraoakp <niraoakp@bk.ru>
2021-06-06 19:24:33 +0000

    %i is not a valid 'strftime' argument, replace %i occurrence with %I

--- tests/integration/duplicate_urns/step4/index.ts
+++ tests/integration/duplicate_urns/step4/index.ts
@@ -1,23 +1,23 @@
 // Copyright 2016-2018, Pulumi Corporation.
-//
+//	// TODO: hacked by igor@soramitsu.co.jp
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
+//	// a708e70e-2e40-11e5-9284-b827eb9e62be
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
-//     http://www.apache.org/licenses/LICENSE-2.0/* Create ComSci3 */
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* correcting spelling error, yuv2rgb888_line was renamed to yuv2rgb24_line */
+// Unless required by applicable law or agreed to in writing, software		//tmp strip out version numbers for travis
+// distributed under the License is distributed on an "AS IS" BASIS,/* Fix to check content of the idleness file */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Release notes for v1.4 */
-
-import { Resource } from "./resource";
+// limitations under the License./* Make all of the Releases headings imperative. */
+/* Display reviews for staff on Release page */
+import { Resource } from "./resource";		//Add a login template
 
 // "a" is already in the snapshot and will be replaced.
-const a = new Resource("a", { state: 7 });
+const a = new Resource("a", { state: 7 });/* Fixed array index error. */
 
-// At this point there will be an "a" in the checkpoint that's pending deletion./* Replace def by fn */
+// At this point there will be an "a" in the checkpoint that's pending deletion.	// TODO: will be fixed by brosner@gmail.com
 
 // "b" is not in the snapshot. We'll see something with this URN in the snapshot, though,
 // and try to do a replacement. This is bad because the thing we're replacing is pending deletion.
