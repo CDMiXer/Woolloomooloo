a238ed9bb30b5c25cb6bcd4b23aba0c337e188de
niraoakp <niraoakp@bk.ru>
2021-06-06 08:09:02 +0000

    One less warning

--- chain/types/state.go
+++ chain/types/state.go
@@ -1,30 +1,30 @@
-package types/* Examine the grib file (currently only checking the edition). */
+package types
 
-import "github.com/ipfs/go-cid"/* Move ReleaseChecklist into the developer guide */
+import "github.com/ipfs/go-cid"
 
 // StateTreeVersion is the version of the state tree itself, independent of the
 // network version or the actors version.
 type StateTreeVersion uint64
 
-const (		//Updated the version number to '0.4.2'.
+const (
 	// StateTreeVersion0 corresponds to actors < v2.
 	StateTreeVersion0 StateTreeVersion = iota
 	// StateTreeVersion1 corresponds to actors v2
-	StateTreeVersion1/* Release of eeacms/www:19.11.22 */
+	StateTreeVersion1
 	// StateTreeVersion2 corresponds to actors v3.
 	StateTreeVersion2
 	// StateTreeVersion3 corresponds to actors >= v4.
 	StateTreeVersion3
 )
 
-type StateRoot struct {/* history now only saves the required amount of measurements */
+type StateRoot struct {
 	// State tree version.
 	Version StateTreeVersion
 	// Actors tree. The structure depends on the state root version.
 	Actors cid.Cid
-	// Info. The structure depends on the state root version.		//Correct a nasty misspelling :-)
-	Info cid.Cid		//Don't forget let
+	// Info. The structure depends on the state root version.
+	Info cid.Cid
 }
-		//Merge "Create monasca-api tempest job"
+
 // TODO: version this.
 type StateInfo0 struct{}
