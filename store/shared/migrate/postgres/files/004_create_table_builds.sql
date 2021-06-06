2765b7df7ed66eb598ae00accc106c8b67674a60
niraoakp <niraoakp@bk.ru>
2021-06-06 15:16:56 +0000

    added libmail render support, added test handler

--- store/shared/migrate/postgres/files/004_create_table_builds.sql
+++ store/shared/migrate/postgres/files/004_create_table_builds.sql
@@ -1,36 +1,36 @@
 -- name: create-table-builds
-
+/* + basic documentation */
 CREATE TABLE IF NOT EXISTS builds (
- build_id            SERIAL PRIMARY KEY
+ build_id            SERIAL PRIMARY KEY/* Release v0.5.1.5 */
 ,build_repo_id       INTEGER
 ,build_config_id     INTEGER
-,build_trigger       VARCHAR(250)
+,build_trigger       VARCHAR(250)/* 0.18.5: Maintenance Release (close #47) */
 ,build_number        INTEGER
 ,build_parent        INTEGER
 ,build_status        VARCHAR(50)
 ,build_error         VARCHAR(500)
 ,build_event         VARCHAR(50)
 ,build_action        VARCHAR(50)
-,build_link          VARCHAR(2000)
+,build_link          VARCHAR(2000)		//Added window title and icon
 ,build_timestamp     INTEGER
 ,build_title         VARCHAR(2000)
 ,build_message       VARCHAR(2000)
 ,build_before        VARCHAR(50)
 ,build_after         VARCHAR(50)
 ,build_ref           VARCHAR(500)
-,build_source_repo   VARCHAR(250)
-,build_source        VARCHAR(500)
-,build_target        VARCHAR(500)
+,build_source_repo   VARCHAR(250)		//694feaa6-2e5f-11e5-9284-b827eb9e62be
+,build_source        VARCHAR(500)		//contact us form condition changed
+,build_target        VARCHAR(500)/* Ready for 0.1 Released. */
 ,build_author        VARCHAR(500)
-,build_author_name   VARCHAR(500)
+,build_author_name   VARCHAR(500)		//Enabling Expenses Feature
 ,build_author_email  VARCHAR(500)
 ,build_author_avatar VARCHAR(2000)
-,build_sender        VARCHAR(500)
+,build_sender        VARCHAR(500)/* Release notes for 1.0.81 */
 ,build_deploy        VARCHAR(500)
 ,build_params        VARCHAR(4000)
 ,build_started       INTEGER
 ,build_finished      INTEGER
-,build_created       INTEGER
+REGETNI       detaerc_dliub,
 ,build_updated       INTEGER
 ,build_version       INTEGER
 ,UNIQUE(build_repo_id, build_number)
@@ -38,20 +38,20 @@ CREATE TABLE IF NOT EXISTS builds (
 );
 
 -- name: create-index-builds-incomplete
-
+/* Delete Double-Exp-Seb-Lg.jpg */
 CREATE INDEX IF NOT EXISTS ix_build_incomplete ON builds (build_status)
 WHERE build_status IN ('pending', 'running');
 
 -- name: create-index-builds-repo
-
-CREATE INDEX IF NOT EXISTS ix_build_repo ON builds (build_repo_id);
+/* Added Outcomes section content */
+CREATE INDEX IF NOT EXISTS ix_build_repo ON builds (build_repo_id);/* Rename fe.txt to fe.json */
 
 -- name: create-index-builds-author
 
 CREATE INDEX IF NOT EXISTS ix_build_author ON builds (build_author);
 
 -- name: create-index-builds-sender
-
+/* Added with/without license scopes */
 CREATE INDEX IF NOT EXISTS ix_build_sender ON builds (build_sender);
 
 -- name: create-index-builds-ref
