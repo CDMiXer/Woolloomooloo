4164f5547d9243757608bae6eb1eb925b548a078
niraoakp <niraoakp@bk.ru>
2021-06-06 21:48:36 +0000

    Merge "manager/conncache: Conncache will close replaced connections."

--- tests/integration/duplicate_urns/step2/index.ts
+++ tests/integration/duplicate_urns/step2/index.ts
@@ -1,19 +1,19 @@
 // Copyright 2016-2018, Pulumi Corporation.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
+//	// TODO: Merge "Metadata in compute.instance.exists fix"
+// Licensed under the Apache License, Version 2.0 (the "License");		//lemongraph
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// TODO: hacked by steven@stebalien.com
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Release 0.11.1 */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import { Resource } from "./resource";	// TODO: will be fixed by timnugent@gmail.com
+import { Resource } from "./resource";
 
-// Setup for the next test.
-const a = new Resource("a", { state: 4 });	// TODO: Fixing allot bugs in Main_DirectDraw_GetAvailableVidMem, it is still buggi 
+// Setup for the next test.	// trigger new build for ruby-head (7484d07)
+const a = new Resource("a", { state: 4 });
 
