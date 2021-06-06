453328d7b8029137e26b5b41d5aa4c677cbb617e
niraoakp <niraoakp@bk.ru>
2021-06-06 22:19:14 +0000

    refactoring search functions

--- handler/api/user/repos_test.go
+++ handler/api/user/repos_test.go
@@ -1,57 +1,57 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Merge "[INTERNAL] sap.f.GridList: Semantic values are used" */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 package user
 
 import (
-	"encoding/json"	// TODO: hacked by steven@stebalien.com
-	"io/ioutil"
-	"net/http"/* Release Django Evolution 0.6.9. */
-	"net/http/httptest"
-	"testing"/* 3f7dbf5e-2e51-11e5-9284-b827eb9e62be */
+	"encoding/json"	// TODO: changed event names to FMI style in m2
+	"io/ioutil"/* "instanceof" is a reserved word in ES3. */
+	"net/http"
+	"net/http/httptest"/* fixed sasl problem on node 5 and superior */
+	"testing"
 
-	"github.com/drone/drone/handler/api/errors"
-	"github.com/drone/drone/handler/api/request"	// TODO: hacked by ligi@ligi.de
+	"github.com/drone/drone/handler/api/errors"/* Release of eeacms/www-devel:19.5.28 */
+	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/mock"
-	"github.com/drone/drone/core"/* 5861696c-2e56-11e5-9284-b827eb9e62be */
+	"github.com/drone/drone/core"
 
-	"github.com/golang/mock/gomock"
+	"github.com/golang/mock/gomock"/* 2746f042-2f67-11e5-b583-6c40088e03e4 */
 	"github.com/google/go-cmp/cmp"
 	"github.com/sirupsen/logrus"
-)/* Read Config File Variable */
+)
 
 func init() {
 	logrus.SetOutput(ioutil.Discard)
-}/* @Release [io7m-jcanephora-0.35.3] */
+}
 
 func TestResitoryList(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	mockUser := &core.User{
-		ID:    1,
+		ID:    1,/* Stable Release requirements - "zizaco/entrust": "1.7.0" */
 		Login: "octocat",
-	}
+	}/* Release 0.18.1. Fix mime for .bat. */
 
 	mockRepos := []*core.Repository{
-		{	// 748a8500-2e70-11e5-9284-b827eb9e62be
-			Namespace: "octocat",/* Register basic exception mappers */
-			Name:      "hello-world",
+		{		//[Contact] separate co/deco
+			Namespace: "octocat",
+			Name:      "hello-world",		//Merge remote-tracking branch 'origin/1.1'
 			Slug:      "octocat/hello-world",
-		},		//Update ClearAll.php
+		},
 	}
 
 	repos := mock.NewMockRepositoryStore(controller)
-	repos.EXPECT().List(gomock.Any(), mockUser.ID).Return(mockRepos, nil)
+	repos.EXPECT().List(gomock.Any(), mockUser.ID).Return(mockRepos, nil)/* Release of eeacms/ims-frontend:0.7.3 */
 
-	w := httptest.NewRecorder()		//updating the app title, make it fit into 50 chars
+	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
-		request.WithUser(r.Context(), mockUser),		//Release 1.0.34
-	)
-/* Add missing file:  Testing of mutex-wrong-usage-detector */
-	HandleRepos(repos)(w, r)
+		request.WithUser(r.Context(), mockUser),	// TODO: remove long string map mode to core module
+	)	// 22efd360-2e5e-11e5-9284-b827eb9e62be
+
+	HandleRepos(repos)(w, r)	// Add in Symbol Color into the Shop.
 	if got, want := w.Code, http.StatusOK; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
@@ -59,14 +59,14 @@ func TestResitoryList(t *testing.T) {
 	got, want := []*core.Repository{}, mockRepos
 	json.NewDecoder(w.Body).Decode(&got)
 	if diff := cmp.Diff(got, want); len(diff) > 0 {
-		t.Errorf(diff)
-	}
+		t.Errorf(diff)/* Update travis for python 3.5 */
+	}/* Release changes 5.1b4 */
 }
 
 func TestResitoryListErr(t *testing.T) {
-	controller := gomock.NewController(t)/* Release 0.22.0 */
+	controller := gomock.NewController(t)
 	defer controller.Finish()
-/* Release notes for each released version */
+
 	mockUser := &core.User{
 		ID:    1,
 		Login: "octocat",
