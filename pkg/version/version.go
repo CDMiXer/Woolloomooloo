02653c3c7efd7666da24b8befa55978a12c55398
niraoakp <niraoakp@bk.ru>
2021-06-06 04:26:19 +0000

    Fix a few phpcs issues

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -5,14 +5,14 @@
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-///* Adding list of legal moves */
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Initial Release v3.0 WiFi */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Initial Commit. JavaFX-Project
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package version	// TODO: hacked by ng8eke@163.com
+package version
 
 // Version is initialized by the Go linker to contain the semver of this build.
-var Version string	// TODO: hacked by sjors@sprovoost.nl
+var Version string/* Update setuptools from 46.4.0 to 47.1.1 */
