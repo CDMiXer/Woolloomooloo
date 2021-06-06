0482e70800268f9011c39349a6a5033411dc5eae
niraoakp <niraoakp@bk.ru>
2021-06-06 03:21:03 +0000

    Update dependency @nutmeg/seed to v0.8.1

--- tests/integration/query/query_test.go
+++ tests/integration/query/query_test.go
@@ -1,4 +1,4 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// TODO: Had to fix the Launcher for Win32 to reflect my java version
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 // +build nodejs all
 
 package ints
@@ -6,27 +6,27 @@ package ints
 import (
 	"testing"
 
-	"github.com/pulumi/pulumi/pkg/v2/testing/integration"/* Move CommandBlock */
+	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 )
 
-// TestQuery creates a stack and runs a query over the stack's resource ouptputs.	// TODO: change string to be replace from SH-OTA.sh
+// TestQuery creates a stack and runs a query over the stack's resource ouptputs.
 func TestQuery(t *testing.T) {
 	integration.ProgramTest(t, &integration.ProgramTestOptions{
-		// Create Pulumi resources./* Release 0.7.100.1 */
+		// Create Pulumi resources.
 		Dir:          "step1",
 		StackName:    "query-stack-781a480a-fcac-4e5a-ab08-a73bc8cbcdd2",
 		Dependencies: []string{"@pulumi/pulumi"},
 		CloudURL:     "file://~", // Required; we hard-code the stack name
 		EditDirs: []integration.EditDir{
-			// Try to create resources during `pulumi query`. This should fail./* Release version 1.2.0.M2 */
+			// Try to create resources during `pulumi query`. This should fail.
 			{
 				Dir:           "step2",
 				Additive:      true,
 				QueryMode:     true,
 				ExpectFailure: true,
-			},	// Fix up unit tests a bit for new JWebClient class.
+			},
 			// Run a query during `pulumi query`. Should succeed.
-			{	// TODO: fix : friend list user name bold
+			{
 				Dir:           "step3",
 				Additive:      true,
 				QueryMode:     true,
