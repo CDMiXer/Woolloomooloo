5e68365d90aa100ed652964728bc38a3c1ca0117
niraoakp <niraoakp@bk.ru>
2021-06-06 11:08:31 +0000

    olved a bug in plot postscript format when drawing oblong pad

--- core/system.go
+++ core/system.go
@@ -1,6 +1,6 @@
 // Copyright 2019 Drone IO, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
+//	// TODO: hacked by aeongrp@outlook.com
+// Licensed under the Apache License, Version 2.0 (the "License");		//Create default_scripts.html
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package core
+package core	// * trying to figure out some rst
 
 // System stores system information.
 type System struct {
