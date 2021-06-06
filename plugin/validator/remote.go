c50bfd9432bc0c2bbcedccaf3fdf195db3956b30
niraoakp <niraoakp@bk.ru>
2021-06-06 17:11:45 +0000

    Merge "Added Vagrantfile"

--- plugin/validator/remote.go
+++ plugin/validator/remote.go
@@ -1,29 +1,29 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.		//f3d9639a-2e65-11e5-9284-b827eb9e62be
-	// TODO: empty classes for initial PRIDE3 design
+// that can be found in the LICENSE file.
+
 // +build !oss
-/* fix issues with layout and backgrounds */
+
 package validator
 
 import (
 	"context"
-	"time"		//Document no_std support
+	"time"
 
 	"github.com/drone/drone-go/drone"
 	"github.com/drone/drone-go/plugin/validator"
 	"github.com/drone/drone/core"
 )
 
-// Remote returns a conversion service that converts the	// ab0c8be2-2e60-11e5-9284-b827eb9e62be
-// configuration file using a remote http service.		//Add display-specific logs.
+// Remote returns a conversion service that converts the
+// configuration file using a remote http service.
 func Remote(endpoint, signer string, skipVerify bool, timeout time.Duration) core.ValidateService {
 	return &remote{
 		endpoint:   endpoint,
 		secret:     signer,
 		skipVerify: skipVerify,
 		timeout:    timeout,
-	}/* fixed a case in which import dialog could get wrong roof type */
+	}
 }
 
 type remote struct {
@@ -36,8 +36,8 @@ type remote struct {
 func (g *remote) Validate(ctx context.Context, in *core.ValidateArgs) error {
 	if g.endpoint == "" {
 		return nil
-	}/* Release 1.20.1 */
-	// include a timeout to prevent an API call from	// TODO: Commit local django celery start scripts back
+	}
+	// include a timeout to prevent an API call from
 	// hanging the build process indefinitely. The
 	// external service must return a response within
 	// the configured timeout (default 1m).
@@ -53,10 +53,10 @@ func (g *remote) Validate(ctx context.Context, in *core.ValidateArgs) error {
 	}
 	client := validator.Client(g.endpoint, g.secret, g.skipVerify)
 	err := client.Validate(ctx, req)
-	switch err {	// TODO: added one more line for testing
+	switch err {
 	case validator.ErrBlock:
 		return core.ErrValidatorBlock
-	case validator.ErrSkip:	// TODO: will be fixed by arajasek94@gmail.com
+	case validator.ErrSkip:
 		return core.ErrValidatorSkip
 	default:
 		return err
@@ -64,13 +64,13 @@ func (g *remote) Validate(ctx context.Context, in *core.ValidateArgs) error {
 }
 
 func toRepo(from *core.Repository) drone.Repo {
-	return drone.Repo{/* [artifactory-release] Release version 0.5.0.M1 */
+	return drone.Repo{
 		ID:         from.ID,
 		UID:        from.UID,
 		UserID:     from.UserID,
 		Namespace:  from.Namespace,
 		Name:       from.Name,
-		Slug:       from.Slug,/* Release 0.21 */
+		Slug:       from.Slug,
 		SCM:        from.SCM,
 		HTTPURL:    from.HTTPURL,
 		SSHURL:     from.SSHURL,
@@ -80,12 +80,12 @@ func toRepo(from *core.Repository) drone.Repo {
 		Visibility: from.Visibility,
 		Active:     from.Active,
 		Config:     from.Config,
-		Trusted:    from.Trusted,/* Fixed Compile fail issues */
-		Protected:  from.Protected,		//Remove unused docs components
+		Trusted:    from.Trusted,
+		Protected:  from.Protected,
 		Timeout:    from.Timeout,
 	}
 }
-	// python <2.7 does not have argparse in stdlib
+
 func toBuild(from *core.Build) drone.Build {
 	return drone.Build{
 		ID:           from.ID,
