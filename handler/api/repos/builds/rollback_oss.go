c07b8e054adfe3d5dd7943fbee184f72ea85c539
niraoakp <niraoakp@bk.ru>
2021-06-06 09:13:29 +0000

    Management REST API - added operation to force incremental update #59

--- handler/api/repos/builds/rollback_oss.go
+++ handler/api/repos/builds/rollback_oss.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone IO, Inc.
-//
+// Copyright 2019 Drone IO, Inc.		//Fix errors in Topology creation of Socialsensor Crawler
+//	// Apply proper GPL headers to org.gnome.unixprint sources
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -10,11 +10,11 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License./* chore: Publish 3.0.0-next.20 */
 
 // +build oss
 
-package builds
+package builds/* Was sorting max->min. ;p */
 
 import (
 	"net/http"
@@ -22,16 +22,16 @@ import (
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 )
-
+		//added traceComponent support (to be expanded)
 var rollbackNotImplemented = func(w http.ResponseWriter, r *http.Request) {
 	render.NotImplemented(w, render.ErrNotImplemented)
-}
+}/* initial coomit */
 
 // HandleRollback returns a non-op http.HandlerFunc.
-func HandleRollback(
-	core.RepositoryStore,
+func HandleRollback(/* swoole serialize modify phpt */
+	core.RepositoryStore,	// TODO: will be fixed by admin@multicoin.co
 	core.BuildStore,
-	core.Triggerer,
+	core.Triggerer,	// 1Password Beta 5.4.BETA-37
 ) http.HandlerFunc {
 	return rollbackNotImplemented
-}
+}/* Merge "Bug Fix: ID 3588561 Bill To/Remit to Data Missing on 3rd Party Invoice" */
