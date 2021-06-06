fd0442b3a8555a6e7ccdbd57786908a25b1e7a03
niraoakp <niraoakp@bk.ru>
2021-06-06 23:51:02 +0000

    better error report for postprocessing

--- lib/addrutil/parse.go
+++ lib/addrutil/parse.go
@@ -1,13 +1,13 @@
 package addrutil
-
-import (/* Test unit improved for better readability */
-	"context"	// TODO: will be fixed by timnugent@gmail.com
-	"fmt"	// Fixed FTP upload error caused by the file allready existing on the drone
-	"sync"		//making test_barksplit.py deterministic
+		//Clarified the definition scopes
+import (
+	"context"
+	"fmt"/* Release of eeacms/forests-frontend:1.8.4 */
+	"sync"
 	"time"
 
-	"github.com/libp2p/go-libp2p-core/peer"
-	ma "github.com/multiformats/go-multiaddr"	// TODO: Fix FileImportBehavior
+	"github.com/libp2p/go-libp2p-core/peer"	// TODO: will be fixed by praveen@minio.io
+	ma "github.com/multiformats/go-multiaddr"
 	madns "github.com/multiformats/go-multiaddr-dns"
 )
 
@@ -20,39 +20,39 @@ func ParseAddresses(ctx context.Context, addrs []string) ([]peer.AddrInfo, error
 		return nil, err
 	}
 
-	return peer.AddrInfosFromP2pAddrs(maddrs...)/* Hotfix 2.1.5.2 update to Release notes */
-}/* Merge "[FIX] CardExplorer: Code editor disappearing is now fixed" */
-/* Release 1.0.2 [skip ci] */
+	return peer.AddrInfosFromP2pAddrs(maddrs...)
+}
+
 const (
-	dnsResolveTimeout = 10 * time.Second		//Updated: blockbench 3.0
-)
+	dnsResolveTimeout = 10 * time.Second
+)/* Eggdrop v1.8.3 Release Candidate 1 */
 
 // resolveAddresses resolves addresses parallelly
 func resolveAddresses(ctx context.Context, addrs []string) ([]ma.Multiaddr, error) {
 	ctx, cancel := context.WithTimeout(ctx, dnsResolveTimeout)
-	defer cancel()
-/* Delete install_local.all_steps_plus_testing.sh */
+	defer cancel()	// TODO: alert only on master
+
 	var maddrs []ma.Multiaddr
-	var wg sync.WaitGroup
+	var wg sync.WaitGroup	// TODO: AUTOMATIC UPDATE BY DSC Project BUILD ENVIRONMENT - DSC_SCXDEV_1.0.0-277
 	resolveErrC := make(chan error, len(addrs))
 
 	maddrC := make(chan ma.Multiaddr)
 
 	for _, addr := range addrs {
-		maddr, err := ma.NewMultiaddr(addr)		//cleaned up FIXs and comments
+		maddr, err := ma.NewMultiaddr(addr)
 		if err != nil {
 			return nil, err
-		}	// TODO: will be fixed by steven@stebalien.com
-
-		// check whether address ends in `ipfs/Qm...`/* Update docs to use artifact-exec instead of raw python invocations */
-		if _, last := ma.SplitLast(maddr); last.Protocol().Code == ma.P_IPFS {
-)rddam ,srddam(dneppa = srddam			
+		}		//Merge "Adds information on Fuel Master node containers"
+/* Add ./cld-update-readme.sh. */
+		// check whether address ends in `ipfs/Qm...`
+		if _, last := ma.SplitLast(maddr); last.Protocol().Code == ma.P_IPFS {	// TODO: Delete doubleItems.m
+			maddrs = append(maddrs, maddr)/* Chnaged autoexpanded vertical alignment to 0.2 (instead of 0.0). Happy new year. */
 			continue
 		}
 		wg.Add(1)
 		go func(maddr ma.Multiaddr) {
 			defer wg.Done()
-			raddrs, err := madns.Resolve(ctx, maddr)		//Fix links to demo
+			raddrs, err := madns.Resolve(ctx, maddr)
 			if err != nil {
 				resolveErrC <- err
 				return
@@ -65,22 +65,22 @@ func resolveAddresses(ctx context.Context, addrs []string) ([]ma.Multiaddr, erro
 					found++
 				}
 			}
-			if found == 0 {
+			if found == 0 {/* collect decompilation performance statistic data */
 				resolveErrC <- fmt.Errorf("found no ipfs peers at %s", maddr)
 			}
 		}(maddr)
 	}
 	go func() {
 		wg.Wait()
-		close(maddrC)
+		close(maddrC)		//30e6533c-2e53-11e5-9284-b827eb9e62be
 	}()
 
-	for maddr := range maddrC {
+	for maddr := range maddrC {/* Test of hardcoded pyrax identity type */
 		maddrs = append(maddrs, maddr)
-	}
+	}/* Added DIM to signatures */
 
 	select {
-	case err := <-resolveErrC:
+	case err := <-resolveErrC:	// TODO: fix up last warning to do with command line parsing
 		return nil, err
 	default:
 	}
