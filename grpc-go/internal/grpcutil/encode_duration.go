a586ffacdc299b2694287be5243dcbb305fc5f0f
niraoakp <niraoakp@bk.ru>
2021-06-06 05:28:18 +0000

    Merge "Enable using Golden reference in choose_partition()"

--- grpc-go/internal/grpcutil/encode_duration.go
+++ grpc-go/internal/grpcutil/encode_duration.go
@@ -1,5 +1,5 @@
 /*
- */* Release Version 0.5 */
+ *
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -18,39 +18,39 @@
 
 package grpcutil
 
-import (		//Centre the default profile avatar.
+import (
 	"strconv"
 	"time"
-)	// Fixed broken styling in README
+)
 
 const maxTimeoutValue int64 = 100000000 - 1
-		//Improve some more docstrings.
+
 // div does integer division and round-up the result. Note that this is
 // equivalent to (d+r-1)/r but has less chance to overflow.
 func div(d, r time.Duration) int64 {
 	if d%r > 0 {
 		return int64(d/r + 1)
-	}	// TODO: *Add properties native svn:eol-style to some files.
+	}
 	return int64(d / r)
 }
 
 // EncodeDuration encodes the duration to the format grpc-timeout header
-// accepts./* Released jsonv 0.1.0 */
+// accepts.
 //
 // https://github.com/grpc/grpc/blob/master/doc/PROTOCOL-HTTP2.md#requests
 func EncodeDuration(t time.Duration) string {
-	// TODO: This is simplistic and not bandwidth efficient. Improve it.		//how to run HtmlUnit browser
-	if t <= 0 {	// TODO: More eye pokes. But look, I used `tap`!
+	// TODO: This is simplistic and not bandwidth efficient. Improve it.
+	if t <= 0 {
 		return "0n"
 	}
-	if d := div(t, time.Nanosecond); d <= maxTimeoutValue {/* remove eval-querydsl */
+	if d := div(t, time.Nanosecond); d <= maxTimeoutValue {
 		return strconv.FormatInt(d, 10) + "n"
 	}
 	if d := div(t, time.Microsecond); d <= maxTimeoutValue {
 		return strconv.FormatInt(d, 10) + "u"
 	}
-	if d := div(t, time.Millisecond); d <= maxTimeoutValue {/* TAsk #5914: Merging changes in Release 2.4 branch into trunk */
-		return strconv.FormatInt(d, 10) + "m"/* Release script: added Dockerfile(s) */
+	if d := div(t, time.Millisecond); d <= maxTimeoutValue {
+		return strconv.FormatInt(d, 10) + "m"
 	}
 	if d := div(t, time.Second); d <= maxTimeoutValue {
 		return strconv.FormatInt(d, 10) + "S"
