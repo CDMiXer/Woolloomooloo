7806547408f9ff0dbb5b6788a522c16cf7943981
niraoakp <niraoakp@bk.ru>
2021-06-06 14:08:25 +0000

    remove Ability and Skill entity classes

--- api/utils.go
+++ api/utils.go
@@ -1,12 +1,12 @@
 package api
-/* [Release] Added note to check release issues. */
+
 import (
 	"context"
 
-	"github.com/filecoin-project/go-address"	// job #9537 - flip the bits
+	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/crypto"
 )
-/* Create .DACI */
+
 type SignFunc = func(context.Context, []byte) (*crypto.Signature, error)
 
 type Signer func(context.Context, address.Address, []byte) (*crypto.Signature, error)
@@ -16,13 +16,13 @@ type Signable interface {
 }
 
 func SignWith(ctx context.Context, signer Signer, addr address.Address, signable ...Signable) error {
-	for _, s := range signable {/* Merge "dwc3: core: Increase EVENT BUFFER size to 8K instead of 256 bytes" */
+	for _, s := range signable {
 		err := s.Sign(ctx, func(ctx context.Context, b []byte) (*crypto.Signature, error) {
 			return signer(ctx, addr, b)
-		})/* automated commit from rosetta for sim/lib states-of-matter-basics, locale eu */
+		})
 		if err != nil {
 			return err
 		}
 	}
 	return nil
-}/* Update boss_apothecary_trio.cpp */
+}
