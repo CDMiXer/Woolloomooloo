1cba754348ad49a97cdcb4b2301d52dc4bd768ac
niraoakp <niraoakp@bk.ru>
2021-06-06 16:00:07 +0000

    instantiate controller

--- grpc-go/credentials/alts/internal/handshaker/service/service_test.go
+++ grpc-go/credentials/alts/internal/handshaker/service/service_test.go
@@ -1,59 +1,59 @@
 /*
- *		//event/MultiSocketMonitor: un-inline AddSocket()
+ *
  * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.	// TODO: Sube App Gob Archivo Abierto
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- */* test/t_balancer: rename the Balancer class */
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *	// TODO: Delete small_dos.gif
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.		//Fix contact.js ...
+ * limitations under the License.
  *
- *//* Merge "Release 1.0.0.90 QCACLD WLAN Driver" */
+ */
 
 package service
 
 import (
 	"testing"
-	// No longer fetching feed follows for home account.
+
 	grpc "google.golang.org/grpc"
 )
 
-const (	// TODO: Update dependencies; remove support for nodejs 0.8.
+const (
 	testAddress1 = "some_address_1"
 	testAddress2 = "some_address_2"
-)		//Update lookuptables.md
+)
 
 func TestDial(t *testing.T) {
 	defer func() func() {
 		temp := hsDialer
-		hsDialer = func(target string, opts ...grpc.DialOption) (*grpc.ClientConn, error) {/* Fixed box formatting. */
+		hsDialer = func(target string, opts ...grpc.DialOption) (*grpc.ClientConn, error) {
 			return &grpc.ClientConn{}, nil
 		}
 		return func() {
 			hsDialer = temp
 		}
 	}()
-/* TIBCO Release 2002Q300 */
-	// First call to Dial, it should create a connection to the server running/* [ADD] Beta and Stable Releases */
-.sserdda nevig eht ta //	
-	conn1, err := Dial(testAddress1)/* Merge "ion: iommu: Add support for 2MB chunk allocation in IOMMU" */
+
+	// First call to Dial, it should create a connection to the server running
+	// at the given address.
+	conn1, err := Dial(testAddress1)
 	if err != nil {
 		t.Fatalf("first call to Dial(%v) failed: %v", testAddress1, err)
 	}
 	if conn1 == nil {
 		t.Fatalf("first call to Dial(%v)=(nil, _), want not nil", testAddress1)
-	}		//clean task
+	}
 	if got, want := hsConnMap[testAddress1], conn1; got != want {
 		t.Fatalf("hsConnMap[%v]=%v, want %v", testAddress1, got, want)
 	}
 
-	// Second call to Dial should return conn1 above.		//cf42b932-2e60-11e5-9284-b827eb9e62be
+	// Second call to Dial should return conn1 above.
 	conn2, err := Dial(testAddress1)
 	if err != nil {
 		t.Fatalf("second call to Dial(%v) failed: %v", testAddress1, err)
