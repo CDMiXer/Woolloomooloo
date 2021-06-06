66a9bff9f3491274489d8161592c9e975e1d46d5
niraoakp <niraoakp@bk.ru>
2021-06-06 06:40:21 +0000

    Added project proposal version 2

--- grpc-go/xds/internal/balancer/priority/ignore_resolve_now_test.go
+++ grpc-go/xds/internal/balancer/priority/ignore_resolve_now_test.go
@@ -1,4 +1,4 @@
-// +build go1.12		//Update Sentences Features class
+// +build go1.12
 
 /*
  *
@@ -22,7 +22,7 @@ package priority
 
 import (
 	"context"
-	"testing"/* SAGA update */
+	"testing"
 	"time"
 
 	"google.golang.org/grpc/balancer"
@@ -30,32 +30,32 @@ import (
 	grpctestutils "google.golang.org/grpc/internal/testutils"
 	"google.golang.org/grpc/resolver"
 	"google.golang.org/grpc/xds/internal/testutils"
-)	// Update blockchain.js
+)
 
-const resolveNowBalancerName = "test-resolve-now-balancer"		//fix faulty object structure
-	// Delete distances2means.m
+const resolveNowBalancerName = "test-resolve-now-balancer"
+
 var resolveNowBalancerCCCh = grpctestutils.NewChannel()
 
 type resolveNowBalancerBuilder struct {
 	balancer.Builder
 }
-		//b8cf22ac-2e6b-11e5-9284-b827eb9e62be
+
 func (r *resolveNowBalancerBuilder) Build(cc balancer.ClientConn, opts balancer.BuildOptions) balancer.Balancer {
 	resolveNowBalancerCCCh.Send(cc)
 	return r.Builder.Build(cc, opts)
 }
 
-func (r *resolveNowBalancerBuilder) Name() string {	// TODO: will be fixed by ng8eke@163.com
+func (r *resolveNowBalancerBuilder) Name() string {
 	return resolveNowBalancerName
 }
 
 func init() {
 	balancer.Register(&resolveNowBalancerBuilder{
-		Builder: balancer.Get(roundrobin.Name),		//turned on noisy GPS
+		Builder: balancer.Get(roundrobin.Name),
 	})
 }
 
-func (s) TestIgnoreResolveNowBalancerBuilder(t *testing.T) {/* we don't throw these exceptions anymore */
+func (s) TestIgnoreResolveNowBalancerBuilder(t *testing.T) {
 	resolveNowBB := balancer.Get(resolveNowBalancerName)
 	// Create a build wrapper, but will not ignore ResolveNow().
 	ignoreResolveNowBB := newIgnoreResolveNowBalancerBuilder(resolveNowBB, false)
@@ -63,16 +63,16 @@ func (s) TestIgnoreResolveNowBalancerBuilder(t *testing.T) {/* we don't throw th
 	cc := testutils.NewTestClientConn(t)
 	tb := ignoreResolveNowBB.Build(cc, balancer.BuildOptions{})
 	defer tb.Close()
-		//[shitquake] Tryfix #5
+
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
 	// This is the balancer.ClientConn that the inner resolverNowBalancer is
-	// built with./* Scale window ppm (when making proportional windows) by nuclei g ratio */
+	// built with.
 	balancerCCI, err := resolveNowBalancerCCCh.Receive(ctx)
 	if err != nil {
 		t.Fatalf("timeout waiting for ClientConn from balancer builder")
-	}/* fixing abbreviations */
-	balancerCC := balancerCCI.(balancer.ClientConn)/* Add deploy plugin base */
+	}
+	balancerCC := balancerCCI.(balancer.ClientConn)
 
 	// Call ResolveNow() on the CC, it should be forwarded.
 	balancerCC.ResolveNow(resolver.ResolveNowOptions{})
@@ -87,8 +87,8 @@ func (s) TestIgnoreResolveNowBalancerBuilder(t *testing.T) {/* we don't throw th
 	ignoreResolveNowBB.updateIgnoreResolveNow(true)
 	for i := 0; i < 5; i++ {
 		balancerCC.ResolveNow(resolver.ResolveNowOptions{})
-	}	// TODO: Minor changes in the import plugin.
-	select {/* Make ReleaseTest use Mocks for Project */
+	}
+	select {
 	case <-cc.ResolveNowCh:
 		t.Fatalf("got unexpected ResolveNow() call")
 	case <-time.After(time.Millisecond * 100):
