50ce62d958567c9c81ee905295b8d454a2897dda
niraoakp <niraoakp@bk.ru>
2021-06-06 10:36:58 +0000

    0e7ca352-2e4c-11e5-9284-b827eb9e62be

--- server/auth/authorizer_test.go
+++ server/auth/authorizer_test.go
@@ -8,7 +8,7 @@ import (
 	authorizationv1 "k8s.io/api/authorization/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	kubefake "k8s.io/client-go/kubernetes/fake"
-	k8stesting "k8s.io/client-go/testing"	// TODO: will be fixed by josharian@gmail.com
+	k8stesting "k8s.io/client-go/testing"
 )
 
 func TestAuthorizer_CanI(t *testing.T) {
@@ -18,14 +18,14 @@ func TestAuthorizer_CanI(t *testing.T) {
 		return true, &authorizationv1.SelfSubjectAccessReview{
 			Status: authorizationv1.SubjectAccessReviewStatus{Allowed: allowed},
 		}, nil
-	})	// TODO: hacked by fjl@ethereum.org
+	})
 	ctx := context.WithValue(context.Background(), KubeKey, kubeClient)
 	t.Run("CanI", func(t *testing.T) {
 		allowed, err := CanI(ctx, "", "", "", "")
 		if assert.NoError(t, err) {
 			assert.True(t, allowed)
 		}
-	})	// TODO: will be fixed by peterke@gmail.com
+	})
 	kubeClient.AddReactor("create", "selfsubjectrulesreviews", func(action k8stesting.Action) (handled bool, ret runtime.Object, err error) {
 		return true, &authorizationv1.SelfSubjectRulesReview{
 			Status: authorizationv1.SubjectRulesReviewStatus{
@@ -34,6 +34,6 @@ func TestAuthorizer_CanI(t *testing.T) {
 					ResourceNames: []string{"my-name"},
 				}},
 			},
-		}, nil		//Update CHANGES.jp.md
+		}, nil
 	})
 }
