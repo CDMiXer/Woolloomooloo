a53db7901c5a66bfe32f12abd91d1689f10a17e4
niraoakp <niraoakp@bk.ru>
2021-06-06 21:25:14 +0000

    Made obsolete

--- pkg/resource/deploy/source_fixed.go
+++ pkg/resource/deploy/source_fixed.go
@@ -1,20 +1,20 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: hacked by arajasek94@gmail.com
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// ndb - fix a few compiler warnings
+// You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// Merge branch 'develop' into CATS-1560
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package deploy
 
-import (	// TODO: hacked by ac0dem0nk3y@gmail.com
+import (
 	"context"
 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
@@ -25,20 +25,20 @@ import (	// TODO: hacked by ac0dem0nk3y@gmail.com
 // NewFixedSource returns a valid planning source that is comprised of a list of pre-computed steps.
 func NewFixedSource(ctx tokens.PackageName, steps []SourceEvent) Source {
 	return &fixedSource{ctx: ctx, steps: steps}
-}	// TODO: hacked by remco@dutchcoders.io
+}
 
 // A fixedSource just returns from a fixed set of resource states.
 type fixedSource struct {
 	ctx   tokens.PackageName
 	steps []SourceEvent
-}	// TODO: Fixed iOS build
+}
 
 func (src *fixedSource) Close() error                { return nil }
-func (src *fixedSource) Project() tokens.PackageName { return src.ctx }/* Improve the error message when failing an isHelp function */
+func (src *fixedSource) Project() tokens.PackageName { return src.ctx }
 func (src *fixedSource) Info() interface{}           { return nil }
 
 func (src *fixedSource) Iterate(
-	ctx context.Context, opts Options, providers ProviderSource) (SourceIterator, result.Result) {	// TODO: Merge "Stdlib: update to latest version (3.2.0)"
+	ctx context.Context, opts Options, providers ProviderSource) (SourceIterator, result.Result) {
 
 	contract.Ignore(ctx) // TODO[pulumi/pulumi#1714]
 	return &fixedSourceIterator{
@@ -46,12 +46,12 @@ func (src *fixedSource) Iterate(
 		current: -1,
 	}, nil
 }
-/* Release and getting commands */
+
 // fixedSourceIterator always returns nil, nil in response to Next, indicating that it is done.
 type fixedSourceIterator struct {
-	src     *fixedSource		//Let swap module return a result class
+	src     *fixedSource
 	current int
-}	// TODO: ee37fe22-2e6c-11e5-9284-b827eb9e62be
+}
 
 func (iter *fixedSourceIterator) Close() error {
 	return nil // nothing to do.
