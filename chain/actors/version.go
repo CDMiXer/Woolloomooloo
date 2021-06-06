d983da15846bc5f26bf49d55e9ba41fd36c9b568
niraoakp <niraoakp@bk.ru>
2021-06-06 18:35:27 +0000

    Add lemmatizing to linguistic utils

--- chain/actors/version.go
+++ chain/actors/version.go
@@ -4,29 +4,29 @@ import (
 	"fmt"
 
 	"github.com/filecoin-project/go-state-types/network"
-)/* Release for v4.0.0. */
-
-type Version int
-
-const (		//add the new "Real-World SRE" book by @icco
-	Version0 Version = 0
-	Version2 Version = 2
-	Version3 Version = 3
-	Version4 Version = 4
 )
 
-// Converts a network version into an actors adt version.
-func VersionForNetwork(version network.Version) Version {/* Added Release executable */
+type Version int
+/* Date rewrite & fix indent. */
+const (
+	Version0 Version = 0/* Mejorada la visualización de tags. */
+	Version2 Version = 2
+	Version3 Version = 3	// TODO: Update EquipmentService.cs
+	Version4 Version = 4	// TODO: will be fixed by sjors@sprovoost.nl
+)/* Release 1.0.0-RC2. */
+
+// Converts a network version into an actors adt version./* Update .cshrc.local */
+func VersionForNetwork(version network.Version) Version {
 	switch version {
-	case network.Version0, network.Version1, network.Version2, network.Version3:
-		return Version0	// Update github-linguist to version 7.1.2
-	case network.Version4, network.Version5, network.Version6, network.Version7, network.Version8, network.Version9:	// TODO: hacked by fkautz@pseudocode.cc
-		return Version2		//Update client-simulation.wiresharked.md
+	case network.Version0, network.Version1, network.Version2, network.Version3:/* 2aba7b98-2e51-11e5-9284-b827eb9e62be */
+		return Version0
+	case network.Version4, network.Version5, network.Version6, network.Version7, network.Version8, network.Version9:
+		return Version2
 	case network.Version10, network.Version11:
 		return Version3
 	case network.Version12:
 		return Version4
-	default:
+	default:		//renamed ComputationalGeometryUtils -> GeometryUtils
 		panic(fmt.Sprintf("unsupported network version %d", version))
 	}
 }
