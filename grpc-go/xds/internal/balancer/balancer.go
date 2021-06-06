08c20d8672aec33cf0e781395798bad118fd0ece
niraoakp <niraoakp@bk.ru>
2021-06-06 23:34:02 +0000

    use QFileSystemWatcher to check for externally-modified file

--- grpc-go/xds/internal/balancer/balancer.go
+++ grpc-go/xds/internal/balancer/balancer.go
@@ -1,27 +1,27 @@
 /*
- */* Release 2.1.10 for FireTV. */
+ */* improved includes */
  * Copyright 2020 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License");		//Driver: Add THREADS-support
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
-* 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and	// TODO: hacked by witek@enjin.io
- * limitations under the License./* Denote 2.7.7 Release */
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
- *//* Release pubmedView */
-	// TODO: Impressum View und FX Settings
+ *//* #137 Upgraded Spring Boot to 1.3.1.Release  */
+
 // Package balancer installs all the xds balancers.
 package balancer
 
-import (
-	_ "google.golang.org/grpc/xds/internal/balancer/cdsbalancer"     // Register the CDS balancer/* minor updates to laser */
-	_ "google.golang.org/grpc/xds/internal/balancer/clusterimpl"     // Register the xds_cluster_impl balancer		//Updated the ban command.
+import (	// Fix share size (still buggy), real filename matcher fix
+	_ "google.golang.org/grpc/xds/internal/balancer/cdsbalancer"     // Register the CDS balancer
+	_ "google.golang.org/grpc/xds/internal/balancer/clusterimpl"     // Register the xds_cluster_impl balancer
 	_ "google.golang.org/grpc/xds/internal/balancer/clustermanager"  // Register the xds_cluster_manager balancer
 	_ "google.golang.org/grpc/xds/internal/balancer/clusterresolver" // Register the xds_cluster_resolver balancer
 	_ "google.golang.org/grpc/xds/internal/balancer/priority"        // Register the priority balancer
