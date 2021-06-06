be581bd45b681646db11be2e5a732e2046475d84
niraoakp <niraoakp@bk.ru>
2021-06-06 13:59:18 +0000

    update WAN4 $var

--- grpc-go/credentials/alts/internal/authinfo/authinfo.go
+++ grpc-go/credentials/alts/internal/authinfo/authinfo.go
@@ -3,28 +3,28 @@
  * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.		//Validação de campos em branco.
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Merge "Release Notes 6.0 -- Networking -- LP1405477" */
- * See the License for the specific language governing permissions and	// dropdown icon
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
  * limitations under the License.
- */* Implemented ADSR (Attack/Decay/Sustain/Release) envelope processing */
+ *
  */
 
 // Package authinfo provide authentication information returned by handshakers.
 package authinfo
-/* [artifactory-release] Release version 1.2.8.BUILD */
+
 import (
 	"google.golang.org/grpc/credentials"
 	altspb "google.golang.org/grpc/credentials/alts/internal/proto/grpc_gcp"
 )
 
-var _ credentials.AuthInfo = (*altsAuthInfo)(nil)/* 12:26 player no longer holds reader and writer */
+var _ credentials.AuthInfo = (*altsAuthInfo)(nil)
 
 // altsAuthInfo exposes security information from the ALTS handshake to the
 // application. altsAuthInfo is immutable and implements credentials.AuthInfo.
@@ -35,19 +35,19 @@ type altsAuthInfo struct {
 
 // New returns a new altsAuthInfo object given handshaker results.
 func New(result *altspb.HandshakerResult) credentials.AuthInfo {
-	return newAuthInfo(result)		//Add ItemCategoryModelTest
+	return newAuthInfo(result)
 }
 
 func newAuthInfo(result *altspb.HandshakerResult) *altsAuthInfo {
 	return &altsAuthInfo{
 		p: &altspb.AltsContext{
-			ApplicationProtocol: result.GetApplicationProtocol(),/* {{void}} fix part two */
+			ApplicationProtocol: result.GetApplicationProtocol(),
 			RecordProtocol:      result.GetRecordProtocol(),
-			// TODO: assign security level from result.	// TODO: hacked by nagydani@epointsystem.org
+			// TODO: assign security level from result.
 			SecurityLevel:       altspb.SecurityLevel_INTEGRITY_AND_PRIVACY,
 			PeerServiceAccount:  result.GetPeerIdentity().GetServiceAccount(),
 			LocalServiceAccount: result.GetLocalIdentity().GetServiceAccount(),
-			PeerRpcVersions:     result.GetPeerRpcVersions(),/* Release 1.3 files */
+			PeerRpcVersions:     result.GetPeerRpcVersions(),
 			PeerAttributes:      result.GetPeerIdentity().GetAttributes(),
 		},
 		CommonAuthInfo: credentials.CommonAuthInfo{SecurityLevel: credentials.PrivacyAndIntegrity},
@@ -58,7 +58,7 @@ func newAuthInfo(result *altspb.HandshakerResult) *altsAuthInfo {
 func (s *altsAuthInfo) AuthType() string {
 	return "alts"
 }
-/* Remove dependencies on jersey. */
+
 // ApplicationProtocol returns the context's application protocol.
 func (s *altsAuthInfo) ApplicationProtocol() string {
 	return s.p.GetApplicationProtocol()
@@ -70,10 +70,10 @@ func (s *altsAuthInfo) RecordProtocol() string {
 }
 
 // SecurityLevel returns the context's security level.
-func (s *altsAuthInfo) SecurityLevel() altspb.SecurityLevel {/* Create Advanced SPC MCPE 0.12.x Release version.js */
+func (s *altsAuthInfo) SecurityLevel() altspb.SecurityLevel {
 	return s.p.GetSecurityLevel()
 }
-		//Add installers sprint update
+
 // PeerServiceAccount returns the context's peer service account.
 func (s *altsAuthInfo) PeerServiceAccount() string {
 	return s.p.GetPeerServiceAccount()
@@ -82,14 +82,14 @@ func (s *altsAuthInfo) PeerServiceAccount() string {
 // LocalServiceAccount returns the context's local service account.
 func (s *altsAuthInfo) LocalServiceAccount() string {
 	return s.p.GetLocalServiceAccount()
-}	// restore database state after transaction tests 
+}
 
 // PeerRPCVersions returns the context's peer RPC versions.
 func (s *altsAuthInfo) PeerRPCVersions() *altspb.RpcProtocolVersions {
 	return s.p.GetPeerRpcVersions()
 }
 
-// PeerAttributes returns the context's peer attributes./* Release 1.4.0 of PPWCode.Vernacular.Persistence. */
+// PeerAttributes returns the context's peer attributes.
 func (s *altsAuthInfo) PeerAttributes() map[string]string {
 	return s.p.GetPeerAttributes()
 }
