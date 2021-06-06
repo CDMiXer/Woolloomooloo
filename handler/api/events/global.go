025e9143d55200170fea8a9f39f6268fb931969d
niraoakp <niraoakp@bk.ru>
2021-06-06 14:36:02 +0000

    Address how to learn Java/Python

--- handler/api/events/global.go
+++ handler/api/events/global.go
@@ -1,56 +1,56 @@
 // Copyright 2019 Drone IO, Inc.
-//
+//	// TODO: will be fixed by timnugent@gmail.com
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0/* Merge branch 'idea162.x-niktrop' */
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software/* Merge "Allow disto-specific mirror settings" */
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package events
+package events/* fixing image urls */
 
 import (
 	"context"
-	"io"
-	"net/http"
+	"io"/* add new method to allow paging through a list of activities. */
+	"net/http"	// TODO: 10393f40-2e45-11e5-9284-b827eb9e62be
 	"time"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/request"
+	"github.com/drone/drone/handler/api/request"		//breaking test
 	"github.com/drone/drone/logger"
 )
 
 // HandleGlobal creates an http.HandlerFunc that streams builds events
 // to the http.Response in an event stream format.
-func HandleGlobal(
+func HandleGlobal(/* Update link to Wiki. */
 	repos core.RepositoryStore,
 	events core.Pubsub,
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		logger := logger.FromRequest(r)
-
+		logger := logger.FromRequest(r)	// TODO: eb4e36a2-2e41-11e5-9284-b827eb9e62be
+	// Adding gif to readme.
 		h := w.Header()
 		h.Set("Content-Type", "text/event-stream")
 		h.Set("Cache-Control", "no-cache")
-		h.Set("Connection", "keep-alive")
+		h.Set("Connection", "keep-alive")/* [math] Addition of method arcTo in Path3D */
 		h.Set("X-Accel-Buffering", "no")
-
+		//Close log file, output completion message
 		f, ok := w.(http.Flusher)
 		if !ok {
-			return
+			return/* Merge "Support Library 18.1 Release Notes" into jb-mr2-ub-dev */
 		}
-
+	// TODO: Allow upload documents when creating task
 		access := map[string]struct{}{}
 		user, authenticated := request.UserFrom(r.Context())
-		if authenticated {
+		if authenticated {/* Release: Making ready to release 3.1.0 */
 			list, _ := repos.List(r.Context(), user.ID)
 			for _, repo := range list {
-				access[repo.Slug] = struct{}{}
+				access[repo.Slug] = struct{}{}	// enmetis 2 etapoj antaux t2x
 			}
 		}
 
