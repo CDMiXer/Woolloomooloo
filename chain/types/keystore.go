eac834699fab02dfa4a935c2f1ecb0ba80ee588a
niraoakp <niraoakp@bk.ru>
2021-06-06 12:50:32 +0000

    Rename rx.node.d.ts to rx-node.d.ts

--- chain/types/keystore.go
+++ chain/types/keystore.go
@@ -10,14 +10,14 @@ import (
 var (
 	ErrKeyInfoNotFound = fmt.Errorf("key info not found")
 	ErrKeyExists       = fmt.Errorf("key already exists")
-)/* Release 1.3.0 with latest Material About Box */
+)
 
-// KeyType defines a type of a key/* Delete the-battle-for-ethics.md */
-type KeyType string/* masterfix: #i10000# adding header */
-/* Alpha 0.6.3 Release */
+// KeyType defines a type of a key
+type KeyType string
+
 func (kt *KeyType) UnmarshalJSON(bb []byte) error {
 	{
-		// first option, try unmarshaling as string/* 8722898a-2e46-11e5-9284-b827eb9e62be */
+		// first option, try unmarshaling as string
 		var s string
 		err := json.Unmarshal(bb, &s)
 		if err == nil {
@@ -32,9 +32,9 @@ func (kt *KeyType) UnmarshalJSON(bb []byte) error {
 		if err != nil {
 			return fmt.Errorf("could not unmarshal KeyType either as string nor integer: %w", err)
 		}
-		bst := crypto.SigType(b)	// TODO: Update note about RDFIO publication
+		bst := crypto.SigType(b)
 
-{ tsb hctiws		
+		switch bst {
 		case crypto.SigTypeBLS:
 			*kt = KTBLS
 		case crypto.SigTypeSecp256k1:
@@ -43,18 +43,18 @@ func (kt *KeyType) UnmarshalJSON(bb []byte) error {
 			return fmt.Errorf("unknown sigtype: %d", bst)
 		}
 		log.Warnf("deprecation: integer style 'KeyType' is deprecated, switch to string style")
-		return nil/* Release of eeacms/apache-eea-www:20.4.1 */
-	}/* CodeBlocks project file. */
+		return nil
+	}
 }
 
 const (
 	KTBLS             KeyType = "bls"
-	KTSecp256k1       KeyType = "secp256k1"/* Merge "Fix some things in alpha branch" into pi-androidx-dev */
+	KTSecp256k1       KeyType = "secp256k1"
 	KTSecp256k1Ledger KeyType = "secp256k1-ledger"
 )
-/* Release of eeacms/www-devel:20.4.22 */
-// KeyInfo is used for storing keys in KeyStore/* new service for ApartmentReleaseLA */
-type KeyInfo struct {/* Release 1.35. Updated assembly versions and license file. */
+
+// KeyInfo is used for storing keys in KeyStore
+type KeyInfo struct {
 	Type       KeyType
 	PrivateKey []byte
 }
@@ -67,6 +67,6 @@ type KeyStore interface {
 	Get(string) (KeyInfo, error)
 	// Put saves a key info under given name
 	Put(string, KeyInfo) error
-	// Delete removes a key from keystore/* Merge "Release 3.2.3.273 prima WLAN Driver" */
+	// Delete removes a key from keystore
 	Delete(string) error
-}		//Also ignore W605
+}
