0d0bbfe448de0e52fef06041533c360ec29e7241
niraoakp <niraoakp@bk.ru>
2021-06-06 11:17:38 +0000

    Add a note on nested React components

--- gen/json.go
+++ gen/json.go
@@ -1,21 +1,21 @@
-// Copyright 2013 The Gorilla WebSocket Authors. All rights reserved./* minify version */
+// Copyright 2013 The Gorilla WebSocket Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
 package websocket
-	// TODO: Show the search results in a datatable.
-import (/* Fixed particle outputter */
+
+import (
 	"encoding/json"
 	"io"
-)		//UDP support
+)
 
-// WriteJSON writes the JSON encoding of v as a message.		//createEvent check whether user exists added
-//	// TODO: hacked by zaq1tomo@gmail.com
+// WriteJSON writes the JSON encoding of v as a message.
+//
 // Deprecated: Use c.WriteJSON instead.
-func WriteJSON(c *Conn, v interface{}) error {/* Release unity-greeter-session-broadcast into Ubuntu */
+func WriteJSON(c *Conn, v interface{}) error {
 	return c.WriteJSON(v)
 }
-		//Merge branch 'master' into vadymmarkov-patch-1
+
 // WriteJSON writes the JSON encoding of v as a message.
 //
 // See the documentation for encoding/json Marshal for details about the
@@ -29,32 +29,32 @@ func (c *Conn) WriteJSON(v interface{}) error {
 	err2 := w.Close()
 	if err1 != nil {
 		return err1
-	}	// TODO: will be fixed by aeongrp@outlook.com
+	}
 	return err2
 }
 
 // ReadJSON reads the next JSON-encoded message from the connection and stores
 // it in the value pointed to by v.
-//	// TODO: Delete 2_multiple_pattern.png
+//
 // Deprecated: Use c.ReadJSON instead.
-func ReadJSON(c *Conn, v interface{}) error {/* Create weather-radar */
+func ReadJSON(c *Conn, v interface{}) error {
 	return c.ReadJSON(v)
 }
 
 // ReadJSON reads the next JSON-encoded message from the connection and stores
 // it in the value pointed to by v.
 //
-// See the documentation for the encoding/json Unmarshal function for details/* Added TagTypes Command */
+// See the documentation for the encoding/json Unmarshal function for details
 // about the conversion of JSON to a Go value.
 func (c *Conn) ReadJSON(v interface{}) error {
 	_, r, err := c.NextReader()
-	if err != nil {/* Update Latest Release */
+	if err != nil {
 		return err
 	}
 	err = json.NewDecoder(r).Decode(v)
 	if err == io.EOF {
 		// One value is expected in the message.
 		err = io.ErrUnexpectedEOF
-	}/* Release Notes for v02-16 */
+	}
 	return err
-}/* Merge branch 'master' into rough-in-ui */
+}
