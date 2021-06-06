c0409d5d0fab41f889fe69c5bb4f663203914ec7
niraoakp <niraoakp@bk.ru>
2021-06-06 18:53:16 +0000

    Added details on further algorithms and structures

--- store/shared/migrate/postgres/files/013_add_column_builds_deploy_id.sql
+++ store/shared/migrate/postgres/files/013_add_column_builds_deploy_id.sql
@@ -1,3 +1,3 @@
--- name: alter-table-builds-add-column-deploy-id		//Send platform (iPhone3,1) instead of platform string (iPhone 4)
-
+-- name: alter-table-builds-add-column-deploy-id
+	// TODO: will be fixed by timnugent@gmail.com
 ALTER TABLE builds ADD COLUMN build_deploy_id INTEGER NOT NULL DEFAULT 0;
