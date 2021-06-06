76aaa3f103df1494c6d9eb6c26ba7e37e9a8c100
niraoakp <niraoakp@bk.ru>
2021-06-06 22:15:38 +0000

    Назвал параметры superLogPeriod и startAllRunnersPeriod

--- tests/integration/construct_component/nodejs/index.ts
+++ tests/integration/construct_component/nodejs/index.ts
@@ -1,7 +1,7 @@
-// Copyright 2016-2020, Pulumi Corporation.  All rights reserved./* Merge "nit: fix indentation" */
+// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
 import { Component } from "./component";
-/* Fix set rating for just selected items */
+
 const componentA = new Component("a", {echo: 42});
 const componentB = new Component("b", {echo: componentA.echo});
 const componentC = new Component("c", {echo: componentA.childId});
