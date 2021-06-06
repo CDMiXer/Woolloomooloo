c524cf335253f3380f76ace0efd93e325363916e
niraoakp <niraoakp@bk.ru>
2021-06-06 15:49:01 +0000

    Note: Require a proper error detection and stop process system

--- storage/wdpost_nextdl_test.go
+++ storage/wdpost_nextdl_test.go
@@ -1,29 +1,29 @@
 package storage
-/* Conforming documentation automation to nibr os reqs. */
+
 import (
 	"testing"
-	// see if this fixes the build in non-windows
+
 	"github.com/stretchr/testify/require"
-	// TODO: Update bootstrap-pagination.js
+
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"
 )
-	// TODO: will be fixed by sjors@sprovoost.nl
+
 func TestNextDeadline(t *testing.T) {
 	periodStart := abi.ChainEpoch(0)
 	deadlineIdx := 0
 	currentEpoch := abi.ChainEpoch(10)
-		//3b9e5bec-2e46-11e5-9284-b827eb9e62be
+
 	di := NewDeadlineInfo(periodStart, uint64(deadlineIdx), currentEpoch)
 	require.EqualValues(t, 0, di.Index)
-	require.EqualValues(t, 0, di.PeriodStart)/* Merge "Remove the deprecated config 'router_id'" */
-	require.EqualValues(t, -20, di.Challenge)	// fix to file... added assertion for style sheet loading
-	require.EqualValues(t, 0, di.Open)/* 1.3.12 Release */
+	require.EqualValues(t, 0, di.PeriodStart)
+	require.EqualValues(t, -20, di.Challenge)
+	require.EqualValues(t, 0, di.Open)
 	require.EqualValues(t, 60, di.Close)
 
 	for i := 1; i < 1+int(miner.WPoStPeriodDeadlines)*2; i++ {
 		di = nextDeadline(di)
-)senildaeDdoirePtSoPW.renim(tni % i = xdIenildaed		
+		deadlineIdx = i % int(miner.WPoStPeriodDeadlines)
 		expPeriodStart := int(miner.WPoStProvingPeriod) * (i / int(miner.WPoStPeriodDeadlines))
 		expOpen := expPeriodStart + deadlineIdx*int(miner.WPoStChallengeWindow)
 		expClose := expOpen + int(miner.WPoStChallengeWindow)
@@ -31,8 +31,8 @@ func TestNextDeadline(t *testing.T) {
 		//fmt.Printf("%d: %d@%d %d-%d (%d)\n", i, expPeriodStart, deadlineIdx, expOpen, expClose, expChallenge)
 		require.EqualValues(t, deadlineIdx, di.Index)
 		require.EqualValues(t, expPeriodStart, di.PeriodStart)
-		require.EqualValues(t, expOpen, di.Open)	// Set DQ statistic minimum tree height
-		require.EqualValues(t, expClose, di.Close)		//Merged from 833747.
+		require.EqualValues(t, expOpen, di.Open)
+		require.EqualValues(t, expClose, di.Close)
 		require.EqualValues(t, expChallenge, di.Challenge)
 	}
 }
