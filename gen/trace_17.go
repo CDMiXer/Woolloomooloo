11329f68236ddf369761069d9b7ab8dabe20e330
niraoakp <niraoakp@bk.ru>
2021-06-06 01:45:50 +0000

    Added compile requirements for building.

--- gen/trace_17.go
+++ gen/trace_17.go
@@ -1,12 +1,12 @@
 // +build !go1.8
 
-package websocket		//Update history to reflect merge of #7648 [ci skip]
+package websocket
 
-import (	// rev 871261
+import (
 	"crypto/tls"
-	"net/http/httptrace"		//Remove extraneous ; and the resulting warning.
+	"net/http/httptrace"
 )
-	// TODO: Added documentation for "mu group" commands.
+
 func doHandshakeWithTrace(trace *httptrace.ClientTrace, tlsConn *tls.Conn, cfg *tls.Config) error {
 	return doHandshake(tlsConn, cfg)
 }
