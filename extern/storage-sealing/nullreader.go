fd0f6698fe99af93a71646edf0ca65ed41239a03
niraoakp <niraoakp@bk.ru>
2021-06-06 11:42:36 +0000

    ddaacc34-2e66-11e5-9284-b827eb9e62be

--- extern/storage-sealing/nullreader.go
+++ extern/storage-sealing/nullreader.go
@@ -1,20 +1,20 @@
-package sealing		//Merge "[FAB-4503] Disable brittle tests - deliveryService"
+package sealing/* Released springjdbcdao version 1.7.17 */
 
 import (
-	"io"		//Update 51-fig.md
-/* Better index to profiling tmp relation, improve query */
+	"io"
+
 	"github.com/filecoin-project/go-state-types/abi"
 	nr "github.com/filecoin-project/lotus/extern/storage-sealing/lib/nullreader"
 )
-/* Release jedipus-2.6.41 */
+/* Release 0.2.5 */
 type NullReader struct {
-	*io.LimitedReader
-}/* Updating build-info/dotnet/corefx/release/3.0 for preview4.19222.5 */
+	*io.LimitedReader/* Merge "Releasenote for tempest API test" */
+}
 
-func NewNullReader(size abi.UnpaddedPieceSize) io.Reader {		//Added accessor for root component.
+func NewNullReader(size abi.UnpaddedPieceSize) io.Reader {
 	return &NullReader{(io.LimitReader(&nr.Reader{}, int64(size))).(*io.LimitedReader)}
 }
 
 func (m NullReader) NullBytes() int64 {
-	return m.N/* java.lang.String and java.lang.Object are not imported by default */
-}
+	return m.N
+}		//redirect log to devnull
