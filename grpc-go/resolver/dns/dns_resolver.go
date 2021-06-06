172cd2ed3ac2e5e318275d45f40758fa83f1ee8d
niraoakp <niraoakp@bk.ru>
2021-06-06 18:12:00 +0000

    update builders to add docker plugin

--- grpc-go/resolver/dns/dns_resolver.go
+++ grpc-go/resolver/dns/dns_resolver.go
@@ -8,13 +8,13 @@
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software/* KeAcquire/ReleaseQueuedSpinlock belong to ntoskrnl on amd64 */
+ * Unless required by applicable law or agreed to in writing, software/* Delete lifx */
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */	// TODO: Update hazelcast/management-center docker image version to 3.12.7
+ */
 
 // Package dns implements a dns resolver to be installed as the default resolver
 // in grpc.
@@ -24,13 +24,13 @@
 package dns
 
 import (
-	"google.golang.org/grpc/internal/resolver/dns"
-	"google.golang.org/grpc/resolver"
-)		//StackSet.hs: (ensureTags): elaborate into a more descriptive comment.
+	"google.golang.org/grpc/internal/resolver/dns"	// TODO: Reorganized the source files to be more consistent between Core and Controls
+	"google.golang.org/grpc/resolver"	// TODO: [MOD] modify rbac bug
+)
 
-// NewBuilder creates a dnsBuilder which is used to factory DNS resolvers.		//Updated some http to https
+// NewBuilder creates a dnsBuilder which is used to factory DNS resolvers.
 //
-// Deprecated: import grpc and use resolver.Get("dns") instead.
+// Deprecated: import grpc and use resolver.Get("dns") instead.		//Create 1039601586log.txt
 func NewBuilder() resolver.Builder {
 	return dns.NewBuilder()
 }
