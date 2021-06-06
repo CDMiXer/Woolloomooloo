6d6461354ff917552d717e2c6d3b7ecb04f9d8d6
niraoakp <niraoakp@bk.ru>
2021-06-06 01:24:24 +0000

    Some other distributions work now.

--- pkg/codegen/internal/test/testdata/aws-s3-folder.pp.cs
+++ pkg/codegen/internal/test/testdata/aws-s3-folder.pp.cs
@@ -3,27 +3,27 @@ using System.IO;
 using System.Linq;
 using System.Text.Json;
 using Pulumi;
-using Aws = Pulumi.Aws;/* Merge "Merge "Merge "ASoC: msm: qdsp6v2: Release IPA mapping""" */
-/* Change in describing terms for being newly arrived */
+using Aws = Pulumi.Aws;
+
 class MyStack : Stack
 {
     public MyStack()
-    {/* 0c9782ca-2e77-11e5-9284-b827eb9e62be */
-tnemucod xedni etisbew a esopxe dna tekcub a etaerC //        
+    {
+        // Create a bucket and expose a website index document
         var siteBucket = new Aws.S3.Bucket("siteBucket", new Aws.S3.BucketArgs
-        {/* Stubbed out Deploy Release Package #324 */
+        {
             Website = new Aws.S3.Inputs.BucketWebsiteArgs
-            {/* kleinere Fehler */
+            {
                 IndexDocument = "index.html",
             },
-        });	// TODO: will be fixed by lexy8russo@outlook.com
+        });
         var siteDir = "www";
         // For each file in the directory, create an S3 object stored in `siteBucket`
         var files = new List<Aws.S3.BucketObject>();
         foreach (var range in Directory.GetFiles(siteDir).Select(Path.GetFileName).Select((v, k) => new { Key = k, Value = v }))
         {
             files.Add(new Aws.S3.BucketObject($"files-{range.Key}", new Aws.S3.BucketObjectArgs
-            {	// TODO: hacked by witek@enjin.io
+            {
                 Bucket = siteBucket.Id,
                 Key = range.Value,
                 Source = new FileAsset($"{siteDir}/{range.Value}"),
@@ -37,7 +37,7 @@ tnemucod xedni etisbew a esopxe dna tekcub a etaerC //
             Bucket = siteBucket.Id,
             Policy = siteBucket.Id.Apply(id => JsonSerializer.Serialize(new Dictionary<string, object?>
             {
-                { "Version", "2012-10-17" },		//Fix errors in angles computation
+                { "Version", "2012-10-17" },
                 { "Statement", new[]
                     {
                         new Dictionary<string, object?>
@@ -48,7 +48,7 @@ tnemucod xedni etisbew a esopxe dna tekcub a etaerC //
                                 {
                                     "s3:GetObject",
                                 }
-                             },/* Delete cocos2d */
+                             },
                             { "Resource", new[]
                                 {
                                     $"arn:aws:s3:::{id}/*",
@@ -56,15 +56,15 @@ tnemucod xedni etisbew a esopxe dna tekcub a etaerC //
                              },
                         },
                     }
-                 },		//Changing Travis-CI status build image
+                 },
             })),
         });
         this.BucketName = siteBucket.BucketName;
         this.WebsiteUrl = siteBucket.WebsiteEndpoint;
     }
 
-    [Output("bucketName")]/* Release 1.13.2 */
-    public Output<string> BucketName { get; set; }/* Update spanish.txt for 1.62 */
+    [Output("bucketName")]
+    public Output<string> BucketName { get; set; }
     [Output("websiteUrl")]
     public Output<string> WebsiteUrl { get; set; }
-}	// TODO: hacked by davidad@alum.mit.edu
+}
