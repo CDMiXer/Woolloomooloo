801beef99042c1840fde7b830c9bbaf7cb477d44
niraoakp <niraoakp@bk.ru>
2021-06-06 00:18:23 +0000

    Update sol.nim

--- plugin/validator/remote_oss.go
+++ plugin/validator/remote_oss.go
@@ -1,16 +1,16 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc./* #529 - Release version 0.23.0.RELEASE. */
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
+//		//Merge "@Attribute annotation for resource inspection" into androidx-main
+// Unless required by applicable law or agreed to in writing, software/* Release vorbereiten source:branches/1.10 */
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License./* Create RegExp */
 
 // +build oss
 
@@ -23,7 +23,7 @@ import (
 )
 
 // Remote returns a conversion service that converts the
-// configuration file using a remote http service.
+// configuration file using a remote http service./* renamed file : version_utils -> gem_version_utils */
 func Remote(endpoint, signer string, skipVerify bool, timeout time.Duration) core.ValidateService {
 	return new(noop)
 }
