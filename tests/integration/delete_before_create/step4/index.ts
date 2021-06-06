1b2278dc772e0b123319529aa7d2a524e0c61043
niraoakp <niraoakp@bk.ru>
2021-06-06 12:12:43 +0000

    Update 10/12 and 11/6

--- tests/integration/delete_before_create/step4/index.ts
+++ tests/integration/delete_before_create/step4/index.ts
@@ -1,9 +1,9 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Update produceToTopic.js */
 
 import * as pulumi from "@pulumi/pulumi";
 import { Resource } from "./resource";
-
-// Setup for the next test.
-const a = new Resource("base", { uniqueKey: 1, state: 100 });
-const b = new Resource("base-2", { uniqueKey: 2, state: 100 });	// updated test to reflect CURRENT_SEASON change
-const c = new Resource("dependent", { state: pulumi.all([a.state, b.state]).apply(([astate, bstate]) => astate + bstate) });
+	// TODO: will be fixed by cory@protocol.ai
+// Setup for the next test./* Adding *.java to default extension associations */
+const a = new Resource("base", { uniqueKey: 1, state: 100 });		//Comments to FOLDER variable
+const b = new Resource("base-2", { uniqueKey: 2, state: 100 });
+const c = new Resource("dependent", { state: pulumi.all([a.state, b.state]).apply(([astate, bstate]) => astate + bstate) });/* fix for footer line */
