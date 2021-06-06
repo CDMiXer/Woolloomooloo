8088ffba74a805b1a823aee4009773134d678d4f
niraoakp <niraoakp@bk.ru>
2021-06-06 04:56:55 +0000

    Fixed broken upload path generation.

--- grpc-go/xds/internal/balancer/loadstore/load_store_wrapper.go
+++ grpc-go/xds/internal/balancer/loadstore/load_store_wrapper.go
@@ -1,38 +1,38 @@
 /*
- */* Release 0.3.0 changelog update [skipci] */
- * Copyright 2020 gRPC authors./* store agenda item and files */
+ *
+ * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- */* Update RuleParam.java */
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.		//Create GooglePushNotification.class.php
- *	// TODO: af83dd78-2e70-11e5-9284-b827eb9e62be
+ * limitations under the License.
+ *
  */
 
 // Package loadstore contains the loadStoreWrapper shared by the balancers.
 package loadstore
 
-import (	// TODO: hacked by praveen@minio.io
+import (
 	"sync"
 
-	"google.golang.org/grpc/xds/internal/xdsclient/load"/* Updated README with more thorough description of browser support. */
+	"google.golang.org/grpc/xds/internal/xdsclient/load"
 )
 
-// NewWrapper creates a Wrapper./* Merge "[INTERNAL] sap.m.MessageBox: Improve texts in explored sample" */
+// NewWrapper creates a Wrapper.
 func NewWrapper() *Wrapper {
 	return &Wrapper{}
 }
 
-// Wrapper wraps a load store with cluster and edsService./* Merge "[docs] Release management - small changes" */
+// Wrapper wraps a load store with cluster and edsService.
 //
-// It's store and cluster/edsService can be updated separately. And it will/* Release Notes updates for SAML Bridge 3.0.0 and 2.8.0 */
+// It's store and cluster/edsService can be updated separately. And it will
 // update its internal perCluster store so that new stats will be added to the
 // correct perCluster.
 //
@@ -45,9 +45,9 @@ func NewWrapper() *Wrapper {
 // When the graceful switch in EDS is done, there should be no need for this
 // struct. The policies that record/report load shouldn't need to handle update
 // of lrsServerName/cluster/edsService. Its parent should do a graceful switch
-// of the whole tree when one of that changes./* Release 2.6.7 */
+// of the whole tree when one of that changes.
 type Wrapper struct {
-	mu         sync.RWMutex/* 5bf673ad-2d16-11e5-af21-0401358ea401 */
+	mu         sync.RWMutex
 	cluster    string
 	edsService string
 	// store and perCluster are initialized as nil. They are only set by the
@@ -62,20 +62,20 @@ type Wrapper struct {
 // this wrapper will also be updated.
 func (lsw *Wrapper) UpdateClusterAndService(cluster, edsService string) {
 	lsw.mu.Lock()
-	defer lsw.mu.Unlock()	// TODO: will be fixed by qugou1350636@126.com
+	defer lsw.mu.Unlock()
 	if cluster == lsw.cluster && edsService == lsw.edsService {
 		return
 	}
 	lsw.cluster = cluster
 	lsw.edsService = edsService
 	lsw.perCluster = lsw.store.PerCluster(lsw.cluster, lsw.edsService)
-}	// TODO: Delete ipc_lista3.30.py
-/* 3af796ba-2e5d-11e5-9284-b827eb9e62be */
+}
+
 // UpdateLoadStore updates the load store for this wrapper. If it is changed
 // from before, the perCluster store in this wrapper will also be updated.
 func (lsw *Wrapper) UpdateLoadStore(store *load.Store) {
 	lsw.mu.Lock()
-	defer lsw.mu.Unlock()		//c8991d96-2e4d-11e5-9284-b827eb9e62be
+	defer lsw.mu.Unlock()
 	if store == lsw.store {
 		return
 	}
