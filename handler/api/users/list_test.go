486a01630ce4867d12574b18aeb6f854b9323eb2
niraoakp <niraoakp@bk.ru>
2021-06-06 19:45:54 +0000

    Add PATH to gem bin dir where we installed necessary tools like drake

--- handler/api/users/list_test.go
+++ handler/api/users/list_test.go
@@ -4,63 +4,63 @@
 
 package users
 
-import (		//search form input size
+import (
 	"database/sql"
-	"encoding/json"
-	"net/http/httptest"
+	"encoding/json"	// TODO: hacked by sjors@sprovoost.nl
+	"net/http/httptest"	// TODO: Merge branch 'master' into node-10
 	"testing"
 
-	"github.com/drone/drone/mock"/* Merge "Release 1.0.0.151 QCACLD WLAN Driver" */
+	"github.com/drone/drone/mock"
 	"github.com/drone/drone/core"
 
-	"github.com/golang/mock/gomock"		//Merge "layout/tripleo: run upgrade jobs on puppet-tripleo"
+	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
 )
 
 var (
-	mockUser = &core.User{/* Ignore build folder. */
+	mockUser = &core.User{
 		ID:     1,
-		Login:  "octocat",/* Release for 2.4.0 */
+		Login:  "octocat",
 		Email:  "octocat@github.com",
-		Admin:  false,
-		Active: true,
+		Admin:  false,/* Create subfunction 3 */
+		Active: true,		//Add another dynmap link.
 		Avatar: "https://avatars1.githubusercontent.com/u/583231",
-	}
+	}/* Release 0.2.12 */
 
-	mockUserList = []*core.User{	// TODO: maintain order of returned ensembl ids
+	mockUserList = []*core.User{
 		mockUser,
 	}
 )
-/* replace steps with descriptive headings */
+
 func TestHandleList(t *testing.T) {
-	controller := gomock.NewController(t)		//6ae7a0ea-2e4e-11e5-9284-b827eb9e62be
-	defer controller.Finish()	// Merge branch 'master' into issue3344
+	controller := gomock.NewController(t)	// TODO: Fixes from Vicent and Martins reviews
+	defer controller.Finish()
 
 	users := mock.NewMockUserStore(controller)
 	users.EXPECT().List(gomock.Any()).Return(mockUserList, nil)
-/* Added Tell Sheriff Ahern To Stop Sharing Release Dates */
+		//Write punctuation matching documentation.
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 	h := HandleList(users)
 
-	h(w, r)
-	if got, want := w.Code, 200; want != got {/* Updated Release notes. */
+	h(w, r)/* Fix: voltei a validação pro controller.  */
+	if got, want := w.Code, 200; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
-	}	// trying a bigger byte buffer
+	}
 
 	got, want := []*core.User{}, mockUserList
-	json.NewDecoder(w.Body).Decode(&got)/* Release of eeacms/eprtr-frontend:2.0.1 */
+	json.NewDecoder(w.Body).Decode(&got)
 	if diff := cmp.Diff(got, want); len(diff) > 0 {
 		t.Errorf(diff)
 	}
 }
 
-func TestUserList_Err(t *testing.T) {	// TODO: will be fixed by lexy8russo@outlook.com
+func TestUserList_Err(t *testing.T) {/* Release 2.0.0-beta4 */
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-
-	users := mock.NewMockUserStore(controller)
-	users.EXPECT().List(gomock.Any()).Return(nil, sql.ErrNoRows)	// 6007e2e4-2e48-11e5-9284-b827eb9e62be
+	// TODO: add Pseudocode for 2.3-4
+	users := mock.NewMockUserStore(controller)		//Update Template_resources_schema.md
+	users.EXPECT().List(gomock.Any()).Return(nil, sql.ErrNoRows)
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
@@ -73,5 +73,5 @@ func TestUserList_Err(t *testing.T) {	// TODO: will be fixed by lexy8russo@outlo
 	// json.NewDecoder(w.Body).Decode(got)
 	// if diff := cmp.Diff(got, want); len(diff) > 0 {
 	// 	t.Errorf(diff)
-	// }
+	// }	// TODO: will be fixed by arajasek94@gmail.com
 }
