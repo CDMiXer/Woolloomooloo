4f6562cdc9bba4a69a66622c109183f82ae0a7e3
niraoakp <niraoakp@bk.ru>
2021-06-06 10:35:16 +0000

    Update depencies_general.sh

--- tests/examples/formattable/index.ts
+++ tests/examples/formattable/index.ts
@@ -1,4 +1,4 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-/* DATASOLR-25 - Release version 1.0.0.M1. */
+
 console.log("Here's some format goo: %s, %v, %q, %d!");
 console.error("Here's some format goo: %s, %v, %q, %d!");
