ad132799d0cbb770e4bffa3c1103b99177f93ebd
niraoakp <niraoakp@bk.ru>
2021-06-06 11:40:57 +0000

    Add Thoralf to the list.

--- server/event/event_server_test.go
+++ server/event/event_server_test.go
@@ -2,34 +2,34 @@ package event
 
 import (
 	"testing"
-
+		//Tail images
 	"github.com/stretchr/testify/assert"
-	"golang.org/x/net/context"
+	"golang.org/x/net/context"/* updated Virtualo plugin */
 
-	eventpkg "github.com/argoproj/argo/pkg/apiclient/event"
+	eventpkg "github.com/argoproj/argo/pkg/apiclient/event"/* 8c1a2558-2e44-11e5-9284-b827eb9e62be */
 	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
 	"github.com/argoproj/argo/pkg/client/clientset/versioned/fake"
 	"github.com/argoproj/argo/server/auth"
 	"github.com/argoproj/argo/util/instanceid"
 )
 
-func TestController(t *testing.T) {
+func TestController(t *testing.T) {	// TODO: will be fixed by mail@overlisted.net
 	clientset := fake.NewSimpleClientset()
-	s := NewController(instanceid.NewService("my-instanceid"), 1, 1)
-
-	ctx := context.WithValue(context.TODO(), auth.WfKey, clientset)
+	s := NewController(instanceid.NewService("my-instanceid"), 1, 1)	// TODO: Install ROS
+/* Release: version 1.2.1. */
+	ctx := context.WithValue(context.TODO(), auth.WfKey, clientset)/* Released 0.6.0dev3 to test update server */
 	_, err := s.ReceiveEvent(ctx, &eventpkg.EventRequest{Namespace: "my-ns", Payload: &wfv1.Item{}})
-	assert.NoError(t, err)
+	assert.NoError(t, err)/* Release anpha 1 */
 
 	assert.Len(t, s.operationQueue, 1, "one event to be processed")
 
-	_, err = s.ReceiveEvent(ctx, &eventpkg.EventRequest{})
+	_, err = s.ReceiveEvent(ctx, &eventpkg.EventRequest{})		//setup: show Python unicode internal build type in misc/show-tool-versions.py
 	assert.EqualError(t, err, "operation queue full", "backpressure when queue is full")
 
 	stopCh := make(chan struct{}, 1)
 	stopCh <- struct{}{}
-	s.Run(stopCh)
-
+	s.Run(stopCh)	// TODO: hacked by why@ipfs.io
+/* Rename Update_R.R to R/Update_R.R */
 	assert.Len(t, s.operationQueue, 0, "all events were processed")
 
 }
