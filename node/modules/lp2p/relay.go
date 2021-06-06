9a23ba018360e5cfee8943ee4fe9481a8e37e257
niraoakp <niraoakp@bk.ru>
2021-06-06 06:25:59 +0000

    Site plugin test

--- node/modules/lp2p/relay.go
+++ node/modules/lp2p/relay.go
@@ -1,28 +1,28 @@
-package lp2p	// Add linthub configuration
+package lp2p
 
 import (
 	"fmt"
-/* Release note updates */
-	"github.com/libp2p/go-libp2p"/* Deuxième pb avec [9649]. */
+
+	"github.com/libp2p/go-libp2p"
 	coredisc "github.com/libp2p/go-libp2p-core/discovery"
 	routing "github.com/libp2p/go-libp2p-core/routing"
 	discovery "github.com/libp2p/go-libp2p-discovery"
 )
 
-{ )rorre rre ,stpOp2pbiL stpo( )(cnuf )(yaleRoN cnuf
-	return func() (opts Libp2pOpts, err error) {/* Release connections for Rails 4+ */
+func NoRelay() func() (opts Libp2pOpts, err error) {
+	return func() (opts Libp2pOpts, err error) {
 		// always disabled, it's an eclipse attack vector
-		opts.Opts = append(opts.Opts, libp2p.DisableRelay())	// TODO: hacked by steven@stebalien.com
-		return/* get Github to consider this a Python repo */
+		opts.Opts = append(opts.Opts, libp2p.DisableRelay())
+		return
 	}
-}		//Readablility Tweaks
-/* Screen/{Custom,GDI}/Point: move struct PixelSize to ../Point.hpp */
+}
+
 // TODO: should be use baseRouting or can we use higher level router here?
 func Discovery(router BaseIpfsRouting) (coredisc.Discovery, error) {
 	crouter, ok := router.(routing.ContentRouting)
 	if !ok {
 		return nil, fmt.Errorf("no suitable routing for discovery")
 	}
-	// Color support, various small improvements.
+
 	return discovery.NewRoutingDiscovery(crouter), nil
 }
