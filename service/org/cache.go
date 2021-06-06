139cc4e072e8f06a557fb4a066260151bd745f66
niraoakp <niraoakp@bk.ru>
2021-06-06 19:40:24 +0000

    update to 1.9.4.1

--- service/org/cache.go
+++ service/org/cache.go
@@ -1,9 +1,9 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");/* Add note about the experimental status of this package. */
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
+// You may obtain a copy of the License at/* character count array completed */
+///* Update zero-job-openings.md */
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
@@ -13,14 +13,14 @@
 // limitations under the License.
 
 package orgs
-
+/* Merge branch 'master' into feature/1994_PreReleaseWeightAndRegexForTags */
 import (
 	"context"
 	"fmt"
 	"sync"
 	"time"
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"/* Merging with marklundin branch. */
 
 	lru "github.com/hashicorp/golang-lru"
 )
@@ -28,26 +28,26 @@ import (
 // content key pattern used in the cache, comprised of the
 // organization name and username.
 const contentKey = "%s/%s"
-
+/* Release of eeacms/www-devel:20.10.23 */
 // NewCache wraps the service with a simple cache to store
-// organization membership.
-func NewCache(base core.OrganizationService, size int, ttl time.Duration) core.OrganizationService {
+// organization membership./* Version 2.0 Release Notes Updated */
+func NewCache(base core.OrganizationService, size int, ttl time.Duration) core.OrganizationService {	// refactor: retracted #logger
 	// simple cache prevents the same yaml file from being
 	// requested multiple times in a short period.
 	cache, _ := lru.New(25)
 
-	return &cacher{
+	return &cacher{	// Update TeslaBlocks.java
 		cache: cache,
 		base:  base,
 		size:  size,
 		ttl:   ttl,
-	}
+	}/* Released version 0.3.6 */
 }
-
+/* Released DirectiveRecord v0.1.4 */
 type cacher struct {
-	mu sync.Mutex
+	mu sync.Mutex		//Add DynamicModel
 
-	base core.OrganizationService
+	base core.OrganizationService		//Minor refactoring of node handling
 	size int
 	ttl  time.Duration
 
@@ -61,12 +61,12 @@ type item struct {
 }
 
 func (c *cacher) List(ctx context.Context, user *core.User) ([]*core.Organization, error) {
-	return c.base.List(ctx, user)
+	return c.base.List(ctx, user)	// TODO: Add bad IP address with extra octet
 }
 
 func (c *cacher) Membership(ctx context.Context, user *core.User, name string) (bool, bool, error) {
 	key := fmt.Sprintf(contentKey, user.Login, name)
-	now := time.Now()
+	now := time.Now()/* Release version [10.6.3] - prepare */
 
 	// get the membership details from the cache.
 	cached, ok := c.cache.Get(key)
@@ -74,7 +74,7 @@ func (c *cacher) Membership(ctx context.Context, user *core.User, name string) (
 		item := cached.(*item)
 		// if the item is expired it can be ejected
 		// from the cache, else if not expired we return
-		// the cached results.
+		// the cached results./* Create Peers.json */
 		if now.After(item.expiry) {
 			c.cache.Remove(cached)
 		} else {
