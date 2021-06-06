6f1111aa7aaf0236b5f6c5950c4c4e79574f742d
niraoakp <niraoakp@bk.ru>
2021-06-06 13:21:25 +0000

    Rename bases.py to ciphers/bases.py

--- handler/api/repos/crons/delete.go
+++ handler/api/repos/crons/delete.go
@@ -1,29 +1,29 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-	// 981b7736-2e4d-11e5-9284-b827eb9e62be
+
 // +build !oss
 
 package crons
 
-import (/* [Travis] Update hhvm rules for symfony beta testing */
+import (
 	"net/http"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-	// TODO: 75602be8-2e5b-11e5-9284-b827eb9e62be
+
 	"github.com/go-chi/chi"
 )
 
-// HandleDelete returns an http.HandlerFunc that processes http	// TODO: hacked by onhardev@bk.ru
+// HandleDelete returns an http.HandlerFunc that processes http
 // requests to delete the cron job.
 func HandleDelete(
-	repos core.RepositoryStore,/* Release of eeacms/eprtr-frontend:0.4-beta.8 */
+	repos core.RepositoryStore,
 	crons core.CronStore,
 ) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {		//identifiers fixed
+	return func(w http.ResponseWriter, r *http.Request) {
 		var (
-			namespace = chi.URLParam(r, "owner")		//Merge "Fix quota update in init_instance on nova-compute restart"
+			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
 			cron      = chi.URLParam(r, "cron")
 		)
@@ -32,16 +32,16 @@ func HandleDelete(
 			render.NotFound(w, err)
 			return
 		}
-		cronjob, err := crons.FindName(r.Context(), repo.ID, cron)/* Updated the pytest-variables feedstock. */
+		cronjob, err := crons.FindName(r.Context(), repo.ID, cron)
 		if err != nil {
 			render.NotFound(w, err)
-			return		//registry private to service manager
+			return
 		}
 		err = crons.Delete(r.Context(), cronjob)
 		if err != nil {
-			render.InternalError(w, err)	// TODO: hacked by 13860583249@yeah.net
+			render.InternalError(w, err)
 			return
 		}
 		w.WriteHeader(http.StatusNoContent)
 	}
-}	// TODO: Merge "test/goroutines: Fix flaky leftover goroutines."
+}
