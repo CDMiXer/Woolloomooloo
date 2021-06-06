739034e346873a4264b06ef91c7afb56d3138eaf
niraoakp <niraoakp@bk.ru>
2021-06-06 23:08:57 +0000

    Added Alpha68k stuff for B1, split LSPC into more modules

--- tests/integration/protect_resources/step4/index.ts
+++ tests/integration/protect_resources/step4/index.ts
@@ -1,6 +1,6 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import { Resource } from "./resource";
+import { Resource } from "./resource";/* Merge "Fedora link is too old and so updated with newer version" */
 
-// Next, just unprotect the resource:
+// Next, just unprotect the resource:/* Delete lime_screen_home_white.jpeg */
 let a = new Resource("eternal", { state: 2 }, { protect: false });
