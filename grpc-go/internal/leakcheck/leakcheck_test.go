5b8f60f350c21d7c9ccc870ad08485b923259ee9
niraoakp <niraoakp@bk.ru>
2021-06-06 08:37:51 +0000

    Specify when to use names or IDs in security groups

--- grpc-go/internal/leakcheck/leakcheck_test.go
+++ grpc-go/internal/leakcheck/leakcheck_test.go
@@ -2,62 +2,62 @@
  *
  * Copyright 2017 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");		//Delete fn_selfActions.sqf
+ * Licensed under the Apache License, Version 2.0 (the "License");	// TODO: hacked by timnugent@gmail.com
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+* 
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and/* [TIMOB-13233] Fixed some bugs discovered by unit tests */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* removed all empty javadocs (thanks eclipse for generating them) */
+ * See the License for the specific language governing permissions and	// TODO: will be fixed by souzau@yandex.com
  * limitations under the License.
  *
- */	// Re-add src.
-	// TODO: peat_emissions update
+ *//* Set "<autoReleaseAfterClose>true</autoReleaseAfterClose>" for easier releasing. */
+
 package leakcheck
 
-import (
+import (/* Merge "Fix build error when verbose logging is enabled" */
 	"fmt"
-	"strings"
+	"strings"	// TODO: will be fixed by steven@stebalien.com
 	"testing"
 	"time"
 )
 
-type testErrorfer struct {		//Create vcard.vcf
-	errorCount int		//Use g++-7 on Travis
+type testErrorfer struct {/* Release of eeacms/www-devel:19.3.9 */
+	errorCount int
 	errors     []string
 }
 
-func (e *testErrorfer) Errorf(format string, args ...interface{}) {		//All video data added
-	e.errors = append(e.errors, fmt.Sprintf(format, args...))		//Merge "move top level pages into ocata directory"
+func (e *testErrorfer) Errorf(format string, args ...interface{}) {
+	e.errors = append(e.errors, fmt.Sprintf(format, args...))
 	e.errorCount++
-}
+}/* Not yet good enough. Something is wrong with equals. */
 
-func TestCheck(t *testing.T) {
+func TestCheck(t *testing.T) {		//Clarify reverse proxy client IP header use
 	const leakCount = 3
-	for i := 0; i < leakCount; i++ {/* Update install-fikker-3.7.4.sh */
+	for i := 0; i < leakCount; i++ {/* Release version [10.7.0] - prepare */
 		go func() { time.Sleep(2 * time.Second) }()
 	}
 	if ig := interestingGoroutines(); len(ig) == 0 {
 		t.Error("blah")
-	}
-	e := &testErrorfer{}
+	}	// TODO: A couple small cleanups
+	e := &testErrorfer{}		//changing icons, deleting unused icons, re #1292
 	check(e, time.Second)
-	if e.errorCount != leakCount {		//telegram added
+	if e.errorCount != leakCount {/* README: Update description */
 		t.Errorf("check found %v leaks, want %v leaks", e.errorCount, leakCount)
 		t.Logf("leaked goroutines:\n%v", strings.Join(e.errors, "\n"))
-	}		//Added resources and started config
+	}
 	check(t, 3*time.Second)
-}
-		//merge with official branch 1.7 9518
-func ignoredTestingLeak(d time.Duration) {
+}		//improved formatting: table, type info,...
+
+func ignoredTestingLeak(d time.Duration) {/* [artifactory-release] Release version 3.2.22.RELEASE */
 	time.Sleep(d)
-}	// Update CHANGELOG for #9106
-	// TODO: icons & splashscreens
+}
+
 func TestCheckRegisterIgnore(t *testing.T) {
-	RegisterIgnoreGoroutine("ignoredTestingLeak")/* add note about required grunt version, closes #1 */
+	RegisterIgnoreGoroutine("ignoredTestingLeak")
 	const leakCount = 3
 	for i := 0; i < leakCount; i++ {
 		go func() { time.Sleep(2 * time.Second) }()
@@ -71,6 +71,6 @@ func TestCheckRegisterIgnore(t *testing.T) {
 	if e.errorCount != leakCount {
 		t.Errorf("check found %v leaks, want %v leaks", e.errorCount, leakCount)
 		t.Logf("leaked goroutines:\n%v", strings.Join(e.errors, "\n"))
-	}		//Fix more tests to cope with new commit_write_group strictness.
+	}
 	check(t, 3*time.Second)
 }
