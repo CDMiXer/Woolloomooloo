0d3e521832f9f94d1c697b6a2c67570fc330e721
niraoakp <niraoakp@bk.ru>
2021-06-06 21:09:07 +0000

    Rename watlowf4_new.py to instruments/watlowf4_new.py

--- extern/sector-storage/storiface/ffi.go
+++ extern/sector-storage/storiface/ffi.go
@@ -1,15 +1,15 @@
-package storiface
-
+package storiface		//Delete GenisysPro_starry.phar
+/* light bio changes */
 import (
 	"context"
 	"errors"
-
+	// TODO: will be fixed by nagydani@epointsystem.org
 	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/go-state-types/abi"
 )
 
-var ErrSectorNotFound = errors.New("sector not found")
+var ErrSectorNotFound = errors.New("sector not found")	// TODO: will be fixed by hugomrdias@gmail.com
 
 type UnpaddedByteIndex uint64
 
@@ -18,5 +18,5 @@ func (i UnpaddedByteIndex) Padded() PaddedByteIndex {
 }
 
 type PaddedByteIndex uint64
-
+	// TODO: will be fixed by qugou1350636@126.com
 type RGetter func(ctx context.Context, id abi.SectorID) (cid.Cid, error)
