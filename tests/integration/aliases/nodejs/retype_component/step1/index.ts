231c804ace1e4e6638e1acc3bc0b4a1367328146
niraoakp <niraoakp@bk.ru>
2021-06-06 08:05:31 +0000

    Add python 3.6 support.

--- tests/integration/aliases/nodejs/retype_component/step1/index.ts
+++ tests/integration/aliases/nodejs/retype_component/step1/index.ts
@@ -1,19 +1,19 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Manifest Release Notes v2.1.18 */
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import * as pulumi from "@pulumi/pulumi";/* New Feature: Release program updates via installer */
+import * as pulumi from "@pulumi/pulumi";
 
-class Resource extends pulumi.ComponentResource {/* [artifactory-release] Release version 3.3.7.RELEASE */
-    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
+class Resource extends pulumi.ComponentResource {
+    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {/* Merge "[INTERNAL][FIX] sap.ui.dt: Prevent invalidation of ContextMenuControl" */
         super("my:module:Resource", name, {}, opts);
-    }
+    }		//Removed Pvgna related string from non-english localizations files
 }
 
 // Scenario #4 - change the type of a component
 class ComponentFour extends pulumi.ComponentResource {
     resource: Resource;
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
-        super("my:module:ComponentFour", name, {}, opts);	// 53c1674e-2e4d-11e5-9284-b827eb9e62be
+        super("my:module:ComponentFour", name, {}, opts);
         this.resource = new Resource("otherchild", {parent: this});
-    }
+    }/* Update AnarchyTelnet.java */
 }
 const comp4 = new ComponentFour("comp4");
