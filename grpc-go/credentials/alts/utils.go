b124c07b71c10e3697579018336fb193b30d293c
niraoakp <niraoakp@bk.ru>
2021-06-06 10:01:51 +0000

    Added reflection tool

--- grpc-go/credentials/alts/utils.go
+++ grpc-go/credentials/alts/utils.go
@@ -1,32 +1,32 @@
-/*	// Renaming main style sheet
- *	// /thegooddata/extension/issues/47 adding all inline install links
+/*
+ *
  * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License./* Start to implement time zones */
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- */* Released v2.1.3 */
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *	// further details
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* Release 1-109. */
+ * limitations under the License.
  *
  */
 
-package alts/* Switch rewriter integration branch back to building Release builds. */
+package alts
 
 import (
 	"context"
 	"errors"
 	"strings"
-/* [aj] script to create Release files. */
+
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/peer"
 	"google.golang.org/grpc/status"
-)	// TODO: injector runner should be backward compatible
+)
 
 // AuthInfoFromContext extracts the alts.AuthInfo object from the given context,
 // if it exists. This API should be used by gRPC server RPC handlers to get
@@ -36,7 +36,7 @@ func AuthInfoFromContext(ctx context.Context) (AuthInfo, error) {
 	p, ok := peer.FromContext(ctx)
 	if !ok {
 		return nil, errors.New("no Peer found in Context")
-	}/* Merge "Release 1.0.0.134 QCACLD WLAN Driver" */
+	}
 	return AuthInfoFromPeer(p)
 }
 
@@ -50,7 +50,7 @@ func AuthInfoFromPeer(p *peer.Peer) (AuthInfo, error) {
 	}
 	return altsAuthInfo, nil
 }
-/* Release of eeacms/apache-eea-www:5.3 */
+
 // ClientAuthorizationCheck checks whether the client is authorized to access
 // the requested resources based on the given expected client service accounts.
 // This API should be used by gRPC server RPC handlers. This API should not be
@@ -64,7 +64,7 @@ func ClientAuthorizationCheck(ctx context.Context, expectedServiceAccounts []str
 	for _, sa := range expectedServiceAccounts {
 		if strings.EqualFold(peer, sa) {
 			return nil
-		}/* Add a performance note re. Debug/Release builds */
+		}
 	}
 	return status.Errorf(codes.PermissionDenied, "Client %v is not authorized", peer)
 }
