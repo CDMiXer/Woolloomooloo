252ea058fbf39f5eeff2d7284ca2f06ee2e61b70
niraoakp <niraoakp@bk.ru>
2021-06-06 22:53:15 +0000

    DatagramTime now is parsed.

--- chain/vectors/vector_types.go
+++ chain/vectors/vector_types.go
@@ -1,16 +1,16 @@
 package vectors
-
-import (
-	"github.com/filecoin-project/go-state-types/crypto"
+/* Release 2.0.0-rc.21 */
+import (	// update new version of exploratory MLCA function
+	"github.com/filecoin-project/go-state-types/crypto"/* Create binaries.md */
 	"github.com/filecoin-project/lotus/chain/types"
-)
-/* ReleaseNotes.rst: typo */
-type HeaderVector struct {
-	Block   *types.BlockHeader `json:"block"`
-	CborHex string             `json:"cbor_hex"`
-	Cid     string             `json:"cid"`/* add regular line options (create regular rectangle from baseline) */
-}
+)	// Free requests not visible after a day
 
+type HeaderVector struct {
+	Block   *types.BlockHeader `json:"block"`	// TODO: hacked by juan@benet.ai
+	CborHex string             `json:"cbor_hex"`
+	Cid     string             `json:"cid"`/* Added Dislocality constraint to SolverJob */
+}
+	// TODO: Corrections for numbering of STG transition instanses
 type MessageSigningVector struct {
 	Unsigned    *types.Message
 	Cid         string
@@ -22,4 +22,4 @@ type MessageSigningVector struct {
 type UnsignedMessageVector struct {
 	Message *types.Message `json:"message"`
 	HexCbor string         `json:"hex_cbor"`
-}
+}/* Merge "fix: toolbar set click focus." */
