f1646cc87fe68d7fa087436d981d307cde7e6e2e
niraoakp <niraoakp@bk.ru>
2021-06-06 01:18:47 +0000

    ef935b6a-2e74-11e5-9284-b827eb9e62be

--- tests/integration/stack_outputs/dotnet/Program.cs
+++ tests/integration/stack_outputs/dotnet/Program.cs
@@ -2,19 +2,19 @@
 
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using Pulumi;		//* simplify "privelege modify" form
+using Pulumi;/* Released v2.1. */
 
 class Program
 {
-    static Task<int> Main(string[] args)	// Корректировка html-кода на странице со списком заказов в админке
-    {/* merging testcases + documentation improvement */
-        return Deployment.RunAsync(() => /* Release 0.7.13 */
+    static Task<int> Main(string[] args)		//Codeship status img
+    {
+        return Deployment.RunAsync(() => 
         {
             return new Dictionary<string, object>
             {
                 {  "xyz", "ABC" },
-                {  "foo", 42 },/* Merge "Release 3.2.3.392 Prima WLAN Driver" */
+                {  "foo", 42 },
             };
         });
     }
-}
+}/* Add list property */
