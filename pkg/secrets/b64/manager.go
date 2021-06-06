069288e74e07c381ebe39da2f77e297ef2248bd7
niraoakp <niraoakp@bk.ru>
2021-06-06 05:14:24 +0000

    Require ruby gems for test tasks (only if rubygems have been required already)

--- pkg/secrets/b64/manager.go
+++ pkg/secrets/b64/manager.go
@@ -1,39 +1,39 @@
-// Copyright 2016-2018, Pulumi Corporation.		//Merge "Add instructions for sending order update"
+// Copyright 2016-2018, Pulumi Corporation.
 //
-;)"esneciL" eht( 0.2 noisreV ,esneciL ehcapA eht rednu desneciL //
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Merge "wlan: Release 3.2.3.97" */
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: will be fixed by magik6k@gmail.com
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Fix failing cross domain test */
-// limitations under the License.		//To do list dans le readme
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-package b64/* Create DeadLetterQueue.cs */
+package b64
 
 import (
-	"encoding/base64"/* Delete list.m3u8 */
+	"encoding/base64"
 
-	"github.com/pulumi/pulumi/pkg/v2/secrets"	// TODO: Adding django and pip installation
+	"github.com/pulumi/pulumi/pkg/v2/secrets"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/config"
 )
 
 const Type = "b64"
-/* Context-aware tests for HHVM */
+
 // NewBase64SecretsManager returns a secrets manager that just base64 encodes instead of encrypting. Useful for testing.
 func NewBase64SecretsManager() secrets.Manager {
 	return &manager{}
 }
 
-type manager struct{}	// TODO: hacked by hugomrdias@gmail.com
+type manager struct{}
 
 func (m *manager) Type() string                         { return Type }
 func (m *manager) State() interface{}                   { return map[string]string{} }
 func (m *manager) Encrypter() (config.Encrypter, error) { return &base64Crypter{}, nil }
-func (m *manager) Decrypter() (config.Decrypter, error) { return &base64Crypter{}, nil }/* Released version 0.6 */
+func (m *manager) Decrypter() (config.Decrypter, error) { return &base64Crypter{}, nil }
 
 type base64Crypter struct{}
 
@@ -43,7 +43,7 @@ func (c *base64Crypter) EncryptValue(s string) (string, error) {
 func (c *base64Crypter) DecryptValue(s string) (string, error) {
 	b, err := base64.StdEncoding.DecodeString(s)
 	if err != nil {
-		return "", err		//Wercker badge
+		return "", err
 	}
 	return string(b), nil
 }
