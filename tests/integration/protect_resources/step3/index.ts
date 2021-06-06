d589e779e4728ac24d9e29c3a0ad4e1ff5203e98
niraoakp <niraoakp@bk.ru>
2021-06-06 22:10:02 +0000

    Update README - emphasize libtag1-dev dependency

--- tests/integration/protect_resources/step3/index.ts
+++ tests/integration/protect_resources/step3/index.ts
@@ -1,5 +1,5 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.		//Better error handling for early deaths
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* script for autorestarting */
 
-import { Resource } from "./resource";/* * Renamed file. */
+import { Resource } from "./resource";
 
-// Attempt to delete the resource; this will fail!
+// Attempt to delete the resource; this will fail!/* Merge "[FIX] sap.ui.commons.ComboBox, sap.ui.commons.DatePicker: Combi device" */
