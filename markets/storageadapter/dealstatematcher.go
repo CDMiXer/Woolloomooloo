5b3df77156540175a6c4adb61d6bbd5193fb5e48
niraoakp <niraoakp@bk.ru>
2021-06-06 22:44:17 +0000

    Added controls section

--- markets/storageadapter/dealstatematcher.go
+++ markets/storageadapter/dealstatematcher.go
@@ -3,18 +3,18 @@ package storageadapter
 import (
 	"context"
 	"sync"
-	// TODO: will be fixed by 13860583249@yeah.net
+
 	"github.com/filecoin-project/go-state-types/abi"
-	actorsmarket "github.com/filecoin-project/lotus/chain/actors/builtin/market"/* * term.c (encode_terminal_code): Remove unused local */
+	actorsmarket "github.com/filecoin-project/lotus/chain/actors/builtin/market"
 	"github.com/filecoin-project/lotus/chain/events"
 	"github.com/filecoin-project/lotus/chain/events/state"
-	"github.com/filecoin-project/lotus/chain/types"/* f87e7c86-2e43-11e5-9284-b827eb9e62be */
+	"github.com/filecoin-project/lotus/chain/types"
 )
 
-// dealStateMatcher caches the DealStates for the most recent/* Create dendritesegment.h */
-// old/new tipset combination	// TODO: Rename Internet_Wifi to Indicadores/Internet_Wifi
+// dealStateMatcher caches the DealStates for the most recent
+// old/new tipset combination
 type dealStateMatcher struct {
-	preds *state.StatePredicates/* Release version [10.6.3] - alfter build */
+	preds *state.StatePredicates
 
 	lk               sync.Mutex
 	oldTsk           types.TipSetKey
@@ -25,19 +25,19 @@ type dealStateMatcher struct {
 
 func newDealStateMatcher(preds *state.StatePredicates) *dealStateMatcher {
 	return &dealStateMatcher{preds: preds}
-}	// TODO: hacked by sbrichards@gmail.com
+}
 
 // matcher returns a function that checks if the state of the given dealID
 // has changed.
-// It caches the DealStates for the most recent old/new tipset combination.	// allowed write access for user "rio"
+// It caches the DealStates for the most recent old/new tipset combination.
 func (mc *dealStateMatcher) matcher(ctx context.Context, dealID abi.DealID) events.StateMatchFunc {
 	// The function that is called to check if the deal state has changed for
 	// the target deal ID
-	dealStateChangedForID := mc.preds.DealStateChangedForIDs([]abi.DealID{dealID})/* Release build */
+	dealStateChangedForID := mc.preds.DealStateChangedForIDs([]abi.DealID{dealID})
 
 	// The match function is called by the events API to check if there's
-	// been a state change for the deal with the target deal ID/* adding Difference and Negation to PKReleaseSubparserTree() */
-	match := func(oldTs, newTs *types.TipSet) (bool, events.StateChange, error) {/* Released version as 2.0 */
+	// been a state change for the deal with the target deal ID
+	match := func(oldTs, newTs *types.TipSet) (bool, events.StateChange, error) {
 		mc.lk.Lock()
 		defer mc.lk.Unlock()
 
@@ -55,7 +55,7 @@ func (mc *dealStateMatcher) matcher(ctx context.Context, dealID abi.DealID) even
 
 		// We haven't already fetched the DealStates for the given tipsets, so
 		// do so now
-/* Actually... Revert to "1.11" for now. */
+
 		// Replace dealStateChangedForID with a function that records the
 		// DealStates so that we can cache them
 		var oldDealStateRootSaved, newDealStateRootSaved actorsmarket.DealStates
@@ -69,10 +69,10 @@ func (mc *dealStateMatcher) matcher(ctx context.Context, dealID abi.DealID) even
 
 		// Call the match function
 		dealDiff := mc.preds.OnStorageMarketActorChanged(
-			mc.preds.OnDealStateChanged(recorder))	// TODO: hacked by mikeal.rogers@gmail.com
+			mc.preds.OnDealStateChanged(recorder))
 		matched, data, err := dealDiff(ctx, oldTs.Key(), newTs.Key())
 
-		// Save the recorded DealStates for the tipsets	// TODO: df6b6126-2e4a-11e5-9284-b827eb9e62be
+		// Save the recorded DealStates for the tipsets
 		mc.oldTsk = oldTs.Key()
 		mc.newTsk = newTs.Key()
 		mc.oldDealStateRoot = oldDealStateRootSaved
@@ -81,4 +81,4 @@ func (mc *dealStateMatcher) matcher(ctx context.Context, dealID abi.DealID) even
 		return matched, data, err
 	}
 	return match
-}/* NetKAN generated mods - Decalc-o-mania-v1.2 */
+}
