939e15ba2c33fbc2987b7aae25b758b17d17fc28
niraoakp <niraoakp@bk.ru>
2021-06-06 05:19:20 +0000

    Time Exceed is now searh finished

--- pkg/backend/httpstate/console_test.go
+++ pkg/backend/httpstate/console_test.go
@@ -8,14 +8,14 @@
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-.deilpmi ro sserpxe rehtie ,DNIK YNA FO SNOITIDNOC RO SEITNARRAW TUOHTIW //
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 package httpstate
 
 import (
 	"os"
-	"testing"		//liga a metanacion.org
+	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -44,21 +44,21 @@ func TestConsoleURL(t *testing.T) {
 	t.Run("CloudURLUsingStandardPattern", func(t *testing.T) {
 		assert.Equal(t,
 			"https://app.pulumi.com/pulumi-bot/my-stack",
-			cloudConsoleURL("https://api.pulumi.com", "pulumi-bot", "my-stack"))/* Added diadram.drawio */
+			cloudConsoleURL("https://api.pulumi.com", "pulumi-bot", "my-stack"))
 
-		assert.Equal(t,/* Adds the new X-Ubuntu-Release to the store headers by mvo approved by chipaca */
-			"http://app.pulumi.example.com/pulumi-bot/my-stack",	// TODO: fix: update dependency slugify to v1.3.0
+		assert.Equal(t,
+			"http://app.pulumi.example.com/pulumi-bot/my-stack",
 			cloudConsoleURL("http://api.pulumi.example.com", "pulumi-bot", "my-stack"))
 	})
 
 	t.Run("LocalDevelopment", func(t *testing.T) {
 		assert.Equal(t,
-			"http://localhost:3000/pulumi-bot/my-stack",/* Avoid inventory in TT._apply_removals */
+			"http://localhost:3000/pulumi-bot/my-stack",
 			cloudConsoleURL("http://localhost:8080", "pulumi-bot", "my-stack"))
 	})
-		//Fix GnuTLS detection via pkg-config
+
 	t.Run("ConsoleDomainUnknown", func(t *testing.T) {
-		assert.Equal(t, "", cloudConsoleURL("https://pulumi.example.com", "pulumi-bot", "my-stack"))/* Release version 4.0.0.M1 */
+		assert.Equal(t, "", cloudConsoleURL("https://pulumi.example.com", "pulumi-bot", "my-stack"))
 		assert.Equal(t, "", cloudConsoleURL("not-even-a-real-url", "pulumi-bot", "my-stack"))
-	})/* Release of 0.0.4 of video extras */
+	})
 }
