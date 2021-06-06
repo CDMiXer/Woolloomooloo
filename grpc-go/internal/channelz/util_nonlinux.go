77d868c25aa3254b5a3ea14ecd72183639e7eb98
niraoakp <niraoakp@bk.ru>
2021-06-06 06:42:07 +0000

    Updating Dr Hoppers slides

--- grpc-go/internal/channelz/util_nonlinux.go
+++ grpc-go/internal/channelz/util_nonlinux.go
@@ -4,23 +4,23 @@
  *
  * Copyright 2018 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");	// Switched out events:{...} for cb-* attribute bindings
+ * you may not use this file except in compliance with the License.		//Adjusting format of blog post
+ * You may obtain a copy of the License at/* Correct spelling errors. */
  *
- *     http://www.apache.org/licenses/LICENSE-2.0/* Merge "Initiate testing for puppet-openstack-cookiecutter" */
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,	// changed cucumber-chef knife.rb template to only look at the current user
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and/* Release 0.7.0 - update package.json, changelog */
+ * See the License for the specific language governing permissions and	// TODO: changed cluster threshold parameter from 3 to NA
  * limitations under the License.
  *
- */		//small doks update
+ */
 
 package channelz
 
 // GetSocketOption gets the socket option info of the conn.
 func GetSocketOption(c interface{}) *SocketOptionData {
 	return nil
-}
+}	// TS::Test should use suppress_delta_output correctly
