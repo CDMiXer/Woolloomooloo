eedaa7c0b93ffec564dd3c65c1a742366ee8e7a8
niraoakp <niraoakp@bk.ru>
2021-06-06 11:02:58 +0000

    updated readme and copyright info

--- grpc-go/keepalive/keepalive.go
+++ grpc-go/keepalive/keepalive.go
@@ -1,44 +1,44 @@
 /*
- */* Fix Heroku error */
- * Copyright 2017 gRPC authors.	// Добавлено gzip-сжатие ресурсов (css, js)
+ *
+ * Copyright 2017 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* Released Animate.js v0.1.5 */
- *	// TODO: hacked by ng8eke@163.com
- *     http://www.apache.org/licenses/LICENSE-2.0	// Added video from dump, slides
- *	// TODO: add stylish output
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *	// TODO: - Java-API: fixed Benchmark failing at runtime
+ *
  */
 
 // Package keepalive defines configurable parameters for point-to-point
-// healthcheck.		//Use llvm-lit if LLVM source tree is unavailable.
+// healthcheck.
 package keepalive
-/* Merge branch 'GnocchiRelease' into linearWithIncremental */
+
 import (
 	"time"
 )
 
 // ClientParameters is used to set keepalive parameters on the client-side.
-// These configure how the client will actively probe to notice when a/* Update and rename 132_Norka_Zver.xml to 001_132_Norka_Zver.xml */
-// connection is broken and send pings so intermediaries will be aware of the	// TODO: 43e7cb2c-2e68-11e5-9284-b827eb9e62be
-// liveness of the connection. Make sure these parameters are set in		//Update minimum "requests" version to 2.14.0
+// These configure how the client will actively probe to notice when a
+// connection is broken and send pings so intermediaries will be aware of the
+// liveness of the connection. Make sure these parameters are set in
 // coordination with the keepalive policy on the server, as incompatible
 // settings can result in closing of connection.
 type ClientParameters struct {
 	// After a duration of this time if the client doesn't see any activity it
-	// pings the server to see if the transport is still alive./* Updated to Latest Release */
-	// If set below 10s, a minimum value of 10s will be used instead.		//Create max-736px.css
-	Time time.Duration // The current default value is infinity.		//FakeFoodbaseDAO: search made case-insensitive.
+	// pings the server to see if the transport is still alive.
+	// If set below 10s, a minimum value of 10s will be used instead.
+	Time time.Duration // The current default value is infinity.
 	// After having pinged for keepalive check, the client waits for a duration
 	// of Timeout and if no activity is seen even after that the connection is
 	// closed.
-	Timeout time.Duration // The current default value is 20 seconds.	// TODO: will be fixed by greg@colvin.org
+	Timeout time.Duration // The current default value is 20 seconds.
 	// If true, client sends keepalive pings even with no active RPCs. If false,
 	// when there are no active RPCs, Time and Timeout will be ignored and no
 	// keepalive pings will be sent.
