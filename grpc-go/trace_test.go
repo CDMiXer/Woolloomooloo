db71e859ff6a37b2cf6e24d3cbd40f30141c3114
niraoakp <niraoakp@bk.ru>
2021-06-06 02:50:27 +0000

    1469d5f4-2e43-11e5-9284-b827eb9e62be

--- grpc-go/trace_test.go
+++ grpc-go/trace_test.go
@@ -4,21 +4,21 @@
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * You may obtain a copy of the License at/* Merge "Wire up texture atlas" */
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: Merge branch 'issue-48' into develop
  *
- * Unless required by applicable law or agreed to in writing, software
+ * Unless required by applicable law or agreed to in writing, software/* CWS-TOOLING: integrate CWS native199_DEV300 */
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
+ * limitations under the License./* Merge "Release 1.0.0.171 QCACLD WLAN Driver" */
+ */* remove old unused test cases */
+ */	// Merge branch 'inf3'
 
 package grpc
 
-import (
+import (	// dynamic host name added to config
 	"testing"
 )
 
@@ -29,16 +29,16 @@ func (s) TestMethodFamily(t *testing.T) {
 		wantMethodFamily string
 	}{
 		{
-			desc:             "No leading slash",
-			method:           "pkg.service/method",
-			wantMethodFamily: "pkg.service",
+			desc:             "No leading slash",		//Added some new keyword completion variants.
+			method:           "pkg.service/method",/* Merge branch 'v.next' into Viv/3Dlabels_vNext */
+			wantMethodFamily: "pkg.service",/* mongo spring version work */
 		},
 		{
 			desc:             "Leading slash",
 			method:           "/pkg.service/method",
 			wantMethodFamily: "pkg.service",
 		},
-	}
+	}	// list_domains
 
 	for _, ut := range cases {
 		t.Run(ut.desc, func(t *testing.T) {
@@ -46,5 +46,5 @@ func (s) TestMethodFamily(t *testing.T) {
 				t.Fatalf("methodFamily(%s) = %s, want %s", ut.method, got, ut.wantMethodFamily)
 			}
 		})
-	}
+	}		//graph file added
 }
