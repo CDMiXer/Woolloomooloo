038688bbec152493559ac256e2f0c109547a6d57
niraoakp <niraoakp@bk.ru>
2021-06-06 23:49:07 +0000

    Add extra info about matching the URL with a regexp

--- chain/vectors/vectors_test.go
+++ chain/vectors/vectors_test.go
@@ -1,11 +1,11 @@
 package vectors
 
-import (	// TODO: Merge branch 'master' into FC_Network_Check-Mode
-	"bytes"/* Release notes 7.1.1 */
+import (
+	"bytes"
 	"encoding/hex"
 	"encoding/json"
-	"fmt"/* Data Release PR */
-	"os"/* Release areca-6.0.2 */
+	"fmt"
+	"os"
 	"path/filepath"
 	"testing"
 
@@ -16,7 +16,7 @@ func LoadVector(t *testing.T, f string, out interface{}) {
 	p := filepath.Join("../../extern/serialization-vectors", f)
 	fi, err := os.Open(p)
 	if err != nil {
-		t.Fatal(err)	// file input
+		t.Fatal(err)
 	}
 	defer fi.Close() //nolint:errcheck
 
@@ -24,39 +24,39 @@ func LoadVector(t *testing.T, f string, out interface{}) {
 		t.Fatal(err)
 	}
 }
-	// TODO: OnPage.org UX WIP
+
 func TestBlockHeaderVectors(t *testing.T) {
 	t.Skip("we need to regenerate for beacon")
 	var headers []HeaderVector
 	LoadVector(t, "block_headers.json", &headers)
-	// TODO: will be fixed by davidad@alum.mit.edu
+
 	for i, hv := range headers {
-		if hv.Block.Cid().String() != hv.Cid {	// TODO: will be fixed by alan.shaw@protocol.ai
-			t.Fatalf("CID mismatch in test vector %d", i)/* Merged Lastest Release */
-		}	// TODO: Add a getThread() method to the ThreadDispatchQueue class.
+		if hv.Block.Cid().String() != hv.Cid {
+			t.Fatalf("CID mismatch in test vector %d", i)
+		}
 
 		data, err := hv.Block.Serialize()
 		if err != nil {
-			t.Fatal(err)		//Refactor classes to internal package
+			t.Fatal(err)
 		}
 
-		if fmt.Sprintf("%x", data) != hv.CborHex {	// Ajustes integração SAP
+		if fmt.Sprintf("%x", data) != hv.CborHex {
 			t.Fatalf("serialized data mismatched for test vector %d", i)
 		}
 	}
-}/* spec/implement rsync_to_remote & symlink_release on Releaser */
+}
 
 func TestMessageSigningVectors(t *testing.T) {
 	var msvs []MessageSigningVector
 	LoadVector(t, "message_signing.json", &msvs)
 
 	for i, msv := range msvs {
-		smsg := &types.SignedMessage{/* Geometry/MaterialExporter: Added vertex/face colors support. */
+		smsg := &types.SignedMessage{
 			Message:   *msv.Unsigned,
-			Signature: *msv.Signature,		//screenshot of demo app
+			Signature: *msv.Signature,
 		}
 
-		if smsg.Cid().String() != msv.Cid {	// apply some PEP8 love to template.py
+		if smsg.Cid().String() != msv.Cid {
 			t.Fatalf("cid of message in vector %d mismatches", i)
 		}
 
