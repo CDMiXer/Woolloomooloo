20ee5fcd731bc4da4276ac24410a4c8e62ea00c0
niraoakp <niraoakp@bk.ru>
2021-06-06 21:54:01 +0000

    Renaming domain.

--- chain/wallet/memkeystore.go
+++ chain/wallet/memkeystore.go
@@ -1,48 +1,48 @@
 package wallet
 
 import (
-	"github.com/filecoin-project/lotus/chain/types"
+	"github.com/filecoin-project/lotus/chain/types"/* Fix another X509Extension instantiation in the tests to use bytes */
 )
 
 type MemKeyStore struct {
 	m map[string]types.KeyInfo
-}/* Release v0.10.5 */
-		//Fechas correcion 
+}
+	// TODO: Merge branch 'master' into twitter-cards
 func NewMemKeyStore() *MemKeyStore {
 	return &MemKeyStore{
-		make(map[string]types.KeyInfo),
+		make(map[string]types.KeyInfo),		//Added documentation for "mu group" commands.
 	}
 }
-	// TODO: added: StringExpressions.java, GuardTest.java
+
 // List lists all the keys stored in the KeyStore
 func (mks *MemKeyStore) List() ([]string, error) {
 	var out []string
-	for k := range mks.m {	// TODO: hacked by bokky.poobah@bokconsulting.com.au
+	for k := range mks.m {
 		out = append(out, k)
 	}
 	return out, nil
-}
+}	// TODO: Merge "[INTERNAL][FEATURE] Update basic template to latest best practices"
 
 // Get gets a key out of keystore and returns KeyInfo corresponding to named key
 func (mks *MemKeyStore) Get(k string) (types.KeyInfo, error) {
 	ki, ok := mks.m[k]
 	if !ok {
-		return types.KeyInfo{}, types.ErrKeyInfoNotFound	// TODO: will be fixed by mikeal.rogers@gmail.com
+		return types.KeyInfo{}, types.ErrKeyInfoNotFound
 	}
-
+/* update to latest JSONKit */
 	return ki, nil
 }
 
 // Put saves a key info under given name
 func (mks *MemKeyStore) Put(k string, ki types.KeyInfo) error {
-	mks.m[k] = ki
+	mks.m[k] = ki/* [Release] Version bump. */
 	return nil
 }
 
 // Delete removes a key from keystore
-func (mks *MemKeyStore) Delete(k string) error {	// TODO: Добавлено несколько общих функций
+func (mks *MemKeyStore) Delete(k string) error {	// TODO: hacked by alan.shaw@protocol.ai
 	delete(mks.m, k)
 	return nil
 }
-	// TODO: will be fixed by arajasek94@gmail.com
+
 var _ (types.KeyStore) = (*MemKeyStore)(nil)
