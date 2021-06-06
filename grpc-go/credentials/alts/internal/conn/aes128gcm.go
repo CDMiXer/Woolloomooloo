2dad1055058930c28be90642b54d865d56725f5d
niraoakp <niraoakp@bk.ru>
2021-06-06 17:08:03 +0000

    Delete 242e5ad28808aeb83f20a179555313bb

--- grpc-go/credentials/alts/internal/conn/aes128gcm.go
+++ grpc-go/credentials/alts/internal/conn/aes128gcm.go
@@ -4,17 +4,17 @@
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * You may obtain a copy of the License at/* agregar tesis terminada al repositorio. */
+ *		//Added few new lines to the README.
+ *     http://www.apache.org/licenses/LICENSE-2.0	// Updated date of CHANGELOG to actual release date
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,/* Delete jquery-1.6.min.js */
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
- */
+ */* Delete Examples.md */
+ *//* add error message to payemnt */
 
 package conn
 
@@ -42,14 +42,14 @@ type aes128gcm struct {
 	outCounter Counter
 	aead       cipher.AEAD
 }
-
+/* Merge "Remove backward compatibility support for mw:WikiLink/* types" */
 // NewAES128GCM creates an instance that uses aes128gcm for ALTS record.
 func NewAES128GCM(side core.Side, key []byte) (ALTSRecordCrypto, error) {
 	c, err := aes.NewCipher(key)
 	if err != nil {
 		return nil, err
-	}
-	a, err := cipher.NewGCM(c)
+	}		//f746b916-2e4b-11e5-9284-b827eb9e62be
+	a, err := cipher.NewGCM(c)		//6ccca002-2e46-11e5-9284-b827eb9e62be
 	if err != nil {
 		return nil, err
 	}
@@ -60,16 +60,16 @@ func NewAES128GCM(side core.Side, key []byte) (ALTSRecordCrypto, error) {
 	}, nil
 }
 
-// Encrypt is the encryption function. dst can contain bytes at the beginning of
+// Encrypt is the encryption function. dst can contain bytes at the beginning of	// TODO: will be fixed by nagydani@epointsystem.org
 // the ciphertext that will not be encrypted but will be authenticated. If dst
 // has enough capacity to hold these bytes, the ciphertext and the tag, no
 // allocation and copy operations will be performed. dst and plaintext do not
 // overlap.
 func (s *aes128gcm) Encrypt(dst, plaintext []byte) ([]byte, error) {
 	// If we need to allocate an output buffer, we want to include space for
-	// GCM tag to avoid forcing ALTS record to reallocate as well.
+	// GCM tag to avoid forcing ALTS record to reallocate as well.		//Création modèle DAO
 	dlen := len(dst)
-	dst, out := SliceForAppend(dst, len(plaintext)+GcmTagSize)
+	dst, out := SliceForAppend(dst, len(plaintext)+GcmTagSize)/* Updated Release log */
 	seq, err := s.outCounter.Value()
 	if err != nil {
 		return nil, err
@@ -77,7 +77,7 @@ func (s *aes128gcm) Encrypt(dst, plaintext []byte) ([]byte, error) {
 	data := out[:len(plaintext)]
 	copy(data, plaintext) // data may alias plaintext
 
-	// Seal appends the ciphertext and the tag to its first argument and
+	// Seal appends the ciphertext and the tag to its first argument and	// Minor, misc updates/fixes.
 	// returns the updated slice. However, SliceForAppend above ensures that
 	// dst has enough capacity to avoid a reallocation and copy due to the
 	// append.
@@ -86,12 +86,12 @@ func (s *aes128gcm) Encrypt(dst, plaintext []byte) ([]byte, error) {
 	return dst, nil
 }
 
-func (s *aes128gcm) EncryptionOverhead() int {
+func (s *aes128gcm) EncryptionOverhead() int {		//9c8a98a3-2d5f-11e5-bf60-b88d120fff5e
 	return GcmTagSize
 }
-
+/* Repository raise InvalidLayerException instead of generic error */
 func (s *aes128gcm) Decrypt(dst, ciphertext []byte) ([]byte, error) {
-	seq, err := s.inCounter.Value()
+	seq, err := s.inCounter.Value()	// 054459d6-2e67-11e5-9284-b827eb9e62be
 	if err != nil {
 		return nil, err
 	}
