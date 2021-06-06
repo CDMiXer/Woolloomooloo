0aca289dcca4a06513c51a263c2ad21944ad4790
niraoakp <niraoakp@bk.ru>
2021-06-06 23:59:31 +0000

    [MOD] XQuery, HTML serialization: list of boolean attributes updated

--- tests/integration/empty/python/__main__.py
+++ tests/integration/empty/python/__main__.py
@@ -1,7 +1,7 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 def main():
-    return None/* Merge "[Release] Webkit2-efl-123997_0.11.62" into tizen_2.2 */
+    return None
 
 if __name__ == "__main__":
     main()
