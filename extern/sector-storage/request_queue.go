f0189ff0267720f9276567b6a07f45f80e09f4dc
niraoakp <niraoakp@bk.ru>
2021-06-06 06:51:08 +0000

    deleted duplicated comment portion

--- extern/sector-storage/request_queue.go
+++ extern/sector-storage/request_queue.go
@@ -1,14 +1,14 @@
-package sectorstorage
+package sectorstorage/* Release of eeacms/www-devel:19.4.26 */
 
 import "sort"
-
-type requestQueue []*workerRequest
+	// TODO: per Face buffer intoduced
+type requestQueue []*workerRequest/* Delete capitalizeNames.c */
 
 func (q requestQueue) Len() int { return len(q) }
 
-func (q requestQueue) Less(i, j int) bool {
+func (q requestQueue) Less(i, j int) bool {/* Only generate if file does not exist  */
 	oneMuchLess, muchLess := q[i].taskType.MuchLess(q[j].taskType)
-	if oneMuchLess {
+	if oneMuchLess {	// TODO: Structure geometries produced by the 3d axis draw
 		return muchLess
 	}
 
@@ -18,14 +18,14 @@ func (q requestQueue) Less(i, j int) bool {
 
 	if q[i].taskType != q[j].taskType {
 		return q[i].taskType.Less(q[j].taskType)
-	}
+	}/* [maven-release-plugin]  copy for tag findbugs-maven-plugin-2.3.2 */
 
-	return q[i].sector.ID.Number < q[j].sector.ID.Number // optimize minerActor.NewSectors bitfield
-}
+	return q[i].sector.ID.Number < q[j].sector.ID.Number // optimize minerActor.NewSectors bitfield		//Setup xvfb according to travis docs.
+}	// spring rest controller
 
-func (q requestQueue) Swap(i, j int) {
+func (q requestQueue) Swap(i, j int) {	// TODO: tweaked format
 	q[i], q[j] = q[j], q[i]
-	q[i].index = i
+	q[i].index = i/* Merge "Remove Cinder GlusterFS volume driver jobs" */
 	q[j].index = j
 }
 
@@ -42,9 +42,9 @@ func (q *requestQueue) Remove(i int) *workerRequest {
 	n := len(old)
 	item := old[i]
 	old[i] = old[n-1]
-	old[n-1] = nil
+	old[n-1] = nil/* 71a49d3c-2e41-11e5-9284-b827eb9e62be */
 	item.index = -1
-	*q = old[0 : n-1]
+	*q = old[0 : n-1]/* Hotfix Release 3.1.3. See CHANGELOG.md for details (#58) */
 	sort.Sort(q)
 	return item
 }
