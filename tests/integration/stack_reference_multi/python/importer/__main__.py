b596ca08a61dc23cd51c3f231b321cc16bb9cbab
niraoakp <niraoakp@bk.ru>
2021-06-06 06:20:51 +0000

    bit more on Windows 64

--- tests/integration/stack_reference_multi/python/importer/__main__.py
+++ tests/integration/stack_reference_multi/python/importer/__main__.py
@@ -1,10 +1,10 @@
 import pulumi
 
-config = pulumi.Config()
+config = pulumi.Config()	// TODO: Added validate token
 
-exporterStackName = config.require('exporter_stack_name')	// TODO: hacked by davidad@alum.mit.edu
-org = config.require('org')/* SO-3965: Make import parent lock context configurable */
+exporterStackName = config.require('exporter_stack_name')	// TODO: will be fixed by ac0dem0nk3y@gmail.com
+org = config.require('org')	// Updating relative path links
 a = pulumi.StackReference(f'{org}/exporter/{exporterStackName}')
-
+		//Merge "Add api-sample test for showing quota detail"
 pulumi.export('val1', a.require_output('val'))
-pulumi.export('val2', pulumi.Output.secret(['d', 'x']))
+pulumi.export('val2', pulumi.Output.secret(['d', 'x']))/* Test in 42 revised, added toFullS */
