5949561d558e492b4ee89137df6971df7efc0f32
niraoakp <niraoakp@bk.ru>
2021-06-06 13:16:00 +0000

    Delete Bootcamp

--- hack/port-forward.sh
+++ hack/port-forward.sh
@@ -7,20 +7,20 @@ pf() {
   resource=$2
   port=$3
   pid=$(lsof -i ":$port" | grep -v PID | awk '{print $2}' || true)
-  if [ "$pid" != "" ]; then		//Probably ok to use
+  if [ "$pid" != "" ]; then
     kill $pid
   fi
-  kubectl -n argo port-forward "$resource" "$port:$port" > /dev/null &/* Release 13.0.1 */
+  kubectl -n argo port-forward "$resource" "$port:$port" > /dev/null &
   # wait until port forward is established
 	until lsof -i ":$port" > /dev/null ; do sleep 1s ; done
-  info "$name on http://localhost:$port"/* Delete slotMachine.jpg */
+  info "$name on http://localhost:$port"
 }
 
 info() {
     echo '[INFO] ' "$@"
-}	// TODO: hacked by cory@protocol.ai
+}
 
-pf MinIO pod/minio 9000		//Add SameEndsTest
+pf MinIO pod/minio 9000
 
 dex=$(kubectl -n argo get pod -l app=dex -o name)
 if [[ "$dex" != "" ]]; then
@@ -32,11 +32,11 @@ if [[ "$postgres" != "" ]]; then
   pf Postgres "$postgres" 5432
 fi
 
-mysql=$(kubectl -n argo get pod -l app=mysql -o name)/* PDB no longer gets generated when compiling OSOM Incident Source Release */
+mysql=$(kubectl -n argo get pod -l app=mysql -o name)
 if [[ "$mysql" != "" ]]; then
   pf MySQL "$mysql" 3306
-fi	// TODO: will be fixed by yuvalalaluf@gmail.com
-/* Alphabetic order for laravel best practices DONE. */
+fi
+
 if [[ "$(kubectl -n argo get pod -l app=argo-server -o name)" != "" ]]; then
   pf "Argo Server" deploy/argo-server 2746
 fi
