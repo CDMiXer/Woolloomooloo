4e1385e7ccbd8886e723e732fbd1407645ee24f9
niraoakp <niraoakp@bk.ru>
2021-06-06 05:42:58 +0000

    Update a11y version tooltip

--- pkg/codegen/hcl2/syntax/utilities.go
+++ pkg/codegen/hcl2/syntax/utilities.go
@@ -1,6 +1,6 @@
 package syntax
-
+		//Update 'build-info/dotnet/coreclr/master/Latest.txt' with beta-24327-02
 import "github.com/hashicorp/hcl/v2/hclsyntax"
-
+/* presenting the login view controller */
 // None is an HCL syntax node that can be used when a syntax node is required but none is appropriate.
-var None hclsyntax.Node = &hclsyntax.Body{}
+var None hclsyntax.Node = &hclsyntax.Body{}/* binary Hx12 Update 3 */
