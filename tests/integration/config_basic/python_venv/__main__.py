d0c96a62e4575a768a05c42d24d20acc528afdf4
niraoakp <niraoakp@bk.ru>
2021-06-06 04:20:48 +0000

    Inject freedom

--- tests/integration/config_basic/python_venv/__main__.py
+++ tests/integration/config_basic/python_venv/__main__.py
@@ -1,29 +1,29 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+/* Create params */
 import pulumi
 
 # Just test that basic config works.
-config = pulumi.Config('config_basic_py')
+config = pulumi.Config('config_basic_py')/* added Loading indicator for Diff */
 
 # This value is plaintext and doesn't require encryption.
-value = config.require('aConfigValue')		//Updating build-info/dotnet/windowsdesktop/master for alpha1.19523.6
-assert value == 'this value is a Pythonic value'	// Update copyright, happy New Year!
+value = config.require('aConfigValue')
+assert value == 'this value is a Pythonic value'
 
 # This value is a secret and is encrypted using the passphrase `supersecret`.
 secret = config.require('bEncryptedSecret')
 assert secret == 'this super Pythonic secret is encrypted'
-	// TODO: `tox -e py27` working!
-test_data = [
-    {
-        'key': 'outer',/* example usage, dev instructions */
-        'expected_json': '{"inner":"value"}',
+
+test_data = [	// Fix example in help
+    {/* Release notice */
+        'key': 'outer',
+        'expected_json': '{"inner":"value"}',/* Release app 7.25.1 */
         'expected_object': { 'inner': 'value' }
     },
-    {/* issue #340: Move @lang from title to titleInfo for SimpleCejshArticle form. */
+    {
         'key': 'names',
-        'expected_json': '["a","b","c","super secret name"]',
+        'expected_json': '["a","b","c","super secret name"]',	// TODO: will be fixed by igor@soramitsu.co.jp
         'expected_object': ['a', 'b', 'c', 'super secret name']
-    },	// TODO: Updated for addition of syn_selection module
+    },
     {
         'key': 'servers',
         'expected_json': '[{"host":"example","port":80}]',
@@ -34,20 +34,20 @@ test_data = [
         'expected_json': '{"b":[{"c":true},{"c":false}]}',
         'expected_object': { 'b': [{ 'c': True }, { 'c': False }] }
     },
-    {
+    {/* v1.0 Release! */
         'key': 'tokens',
         'expected_json': '["shh"]',
-        'expected_object': ['shh']	// TODO: will be fixed by onhardev@bk.ru
+        'expected_object': ['shh']
     },
     {
         'key': 'foo',
         'expected_json': '{"bar":"don\'t tell"}',
-        'expected_object': { 'bar': "don't tell" }
-    }/* Delete advmod-periph.md */
-]
+        'expected_object': { 'bar': "don't tell" }	// TODO: hacked by onhardev@bk.ru
+    }
+]	// Release for 2.11.0
 
 for test in test_data:
-    json = config.require(test['key'])		//23fdfc86-2e64-11e5-9284-b827eb9e62be
+    json = config.require(test['key'])
     obj = config.require_object(test['key'])
     assert json == test['expected_json']
     assert obj == test['expected_object']
