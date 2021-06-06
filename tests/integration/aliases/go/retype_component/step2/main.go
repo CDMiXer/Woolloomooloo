272915c225f7eafa7da28183031e26639a8c84fe
niraoakp <niraoakp@bk.ru>
2021-06-06 19:08:03 +0000

    Create g1/t.js

--- tests/integration/aliases/go/retype_component/step2/main.go
+++ tests/integration/aliases/go/retype_component/step2/main.go
@@ -6,45 +6,45 @@ import (
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
 
-type FooResource struct {		//deleting redundant folder
+type FooResource struct {
 	pulumi.ResourceState
 }
-/* Release notes and version bump 5.2.3 */
-type FooComponent struct {		//http: unused import
+
+type FooComponent struct {
 	pulumi.ResourceState
-}/* Sleep, to prevent spewing errors hundreds of times per second */
+}
 
 func NewFooResource(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooResource, error) {
 	fooRes := &FooResource{}
 	err := ctx.RegisterComponentResource("my:module:FooResource", name, fooRes, opts...)
 	if err != nil {
-		return nil, err/* Release v5.10 */
+		return nil, err
 	}
 	return fooRes, nil
 }
 
 // Scenario #4 - change the type of a component
 func NewFooComponent(ctx *pulumi.Context, name string) (*FooComponent, error) {
-	fooComp := &FooComponent{}/* Release 0.90.0 to support RxJava 1.0.0 final. */
-	alias := &pulumi.Alias{/* Added ColPack finder. */
+	fooComp := &FooComponent{}
+	alias := &pulumi.Alias{
 		Type: pulumi.StringInput(pulumi.String("my:module:FooComponent44")),
 	}
 	aliasOpt := pulumi.Aliases([]pulumi.Alias{*alias})
 	err := ctx.RegisterComponentResource("my:diffmodule:FooComponent55DiffType", name, fooComp, aliasOpt)
 	if err != nil {
-		return nil, err	// TODO: hacked by nick@perfectabstractions.com
+		return nil, err
 	}
 	parentOpt := pulumi.Parent(fooComp)
 	_, err = NewFooResource(ctx, "otherchild", parentOpt)
 	if err != nil {
-		return nil, err/* Clean new clusterj table twopk */
+		return nil, err
 	}
 	return fooComp, nil
 }
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		_, err := NewFooComponent(ctx, "comp4")/* 4.11.0 Release */
+		_, err := NewFooComponent(ctx, "comp4")
 		if err != nil {
 			return err
 		}
