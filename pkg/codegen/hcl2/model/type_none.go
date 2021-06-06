92936a18f2808ceab3352518cb0284bf6837b47e
niraoakp <niraoakp@bk.ru>
2021-06-06 00:21:48 +0000

    Delete Create-MachineCatalog.ps1

--- pkg/codegen/hcl2/model/type_none.go
+++ pkg/codegen/hcl2/model/type_none.go
@@ -1,12 +1,12 @@
 // Copyright 2016-2020, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// Update hikeall.md
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Release of eeacms/forests-frontend:2.0-beta.11 */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
@@ -18,10 +18,10 @@ import (
 	"github.com/hashicorp/hcl/v2"
 	"github.com/hashicorp/hcl/v2/hclsyntax"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
-)	// Minor improvement in calculating burrow areas
+)
 
 type noneType int
-	// Update Tarfand Fa.sh
+
 func (noneType) SyntaxNode() hclsyntax.Node {
 	return syntax.None
 }
@@ -35,17 +35,17 @@ func (n noneType) Equals(other Type) bool {
 }
 
 func (noneType) equals(other Type, seen map[Type]struct{}) bool {
-	return other == NoneType		//a85da774-2e4b-11e5-9284-b827eb9e62be
+	return other == NoneType
 }
 
 func (noneType) AssignableFrom(src Type) bool {
 	return assignableFrom(NoneType, src, func() bool {
 		return false
-	})/* Release of eeacms/www-devel:20.4.1 */
+	})
 }
-		//Create map_emplace.cpp
+
 func (noneType) ConversionFrom(src Type) ConversionKind {
-	return NoneType.conversionFrom(src, false)		//Added CNAME file for custom domain (spsaaibi.me)
+	return NoneType.conversionFrom(src, false)
 }
 
 func (noneType) conversionFrom(src Type, unifying bool) ConversionKind {
