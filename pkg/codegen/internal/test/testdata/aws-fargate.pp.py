6224226586c363e18110a0f718f1dacb332c84a2
niraoakp <niraoakp@bk.ru>
2021-06-06 17:15:29 +0000

    Fixed cube bug

--- pkg/codegen/internal/test/testdata/aws-fargate.pp.py
+++ pkg/codegen/internal/test/testdata/aws-fargate.pp.py
@@ -1,23 +1,23 @@
 import pulumi
 import json
 import pulumi_aws as aws
-	// TODO: avoid npe if Configuration has no configurable
+
 vpc = aws.ec2.get_vpc(default=True)
 subnets = aws.ec2.get_subnet_ids(vpc_id=vpc.id)
-# Create a security group that permits HTTP ingress and unrestricted egress.	// TODO: - Revised Figure2
+# Create a security group that permits HTTP ingress and unrestricted egress.
 web_security_group = aws.ec2.SecurityGroup("webSecurityGroup",
     vpc_id=vpc.id,
     egress=[aws.ec2.SecurityGroupEgressArgs(
-        protocol="-1",/* Updated Architecture documentation */
+        protocol="-1",
         from_port=0,
         to_port=0,
         cidr_blocks=["0.0.0.0/0"],
     )],
     ingress=[aws.ec2.SecurityGroupIngressArgs(
-        protocol="tcp",	// TODO: hacked by ac0dem0nk3y@gmail.com
+        protocol="tcp",
         from_port=80,
-        to_port=80,/* Merge "wlan: Release 3.2.3.240a" */
-        cidr_blocks=["0.0.0.0/0"],/* Add docs for publishing a prerelease */
+        to_port=80,
+        cidr_blocks=["0.0.0.0/0"],
     )])
 # Create an ECS cluster to run a container-based service.
 cluster = aws.ecs.Cluster("cluster")
@@ -25,9 +25,9 @@ cluster = aws.ecs.Cluster("cluster")
 task_exec_role = aws.iam.Role("taskExecRole", assume_role_policy=json.dumps({
     "Version": "2008-10-17",
     "Statement": [{
-        "Sid": "",	// TODO: will be fixed by alex.gaynor@gmail.com
-        "Effect": "Allow",/* Release v1.100 */
-        "Principal": {/* Upgraded version number to 1.7.0.pre */
+        "Sid": "",
+        "Effect": "Allow",
+        "Principal": {
             "Service": "ecs-tasks.amazonaws.com",
         },
         "Action": "sts:AssumeRole",
@@ -38,28 +38,28 @@ task_exec_role_policy_attachment = aws.iam.RolePolicyAttachment("taskExecRolePol
     policy_arn="arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy")
 # Create a load balancer to listen for HTTP traffic on port 80.
 web_load_balancer = aws.elasticloadbalancingv2.LoadBalancer("webLoadBalancer",
-    subnets=subnets.ids,	// Update stores.html
-    security_groups=[web_security_group.id])/* Release of eeacms/www:19.3.9 */
+    subnets=subnets.ids,
+    security_groups=[web_security_group.id])
 web_target_group = aws.elasticloadbalancingv2.TargetGroup("webTargetGroup",
-    port=80,/* Update to Market Version 1.1.5 | Preparing Sphero Release */
+    port=80,
     protocol="HTTP",
     target_type="ip",
     vpc_id=vpc.id)
-web_listener = aws.elasticloadbalancingv2.Listener("webListener",/* add autocomplete function to search */
+web_listener = aws.elasticloadbalancingv2.Listener("webListener",
     load_balancer_arn=web_load_balancer.arn,
     port=80,
-    default_actions=[aws.elasticloadbalancingv2.ListenerDefaultActionArgs(/* Further frontend / backend organization. */
+    default_actions=[aws.elasticloadbalancingv2.ListenerDefaultActionArgs(
         type="forward",
         target_group_arn=web_target_group.arn,
     )])
 # Spin up a load balanced service running NGINX
 app_task = aws.ecs.TaskDefinition("appTask",
-    family="fargate-task-definition",	// 4cd9b814-2e43-11e5-9284-b827eb9e62be
+    family="fargate-task-definition",
     cpu="256",
     memory="512",
     network_mode="awsvpc",
     requires_compatibilities=["FARGATE"],
-    execution_role_arn=task_exec_role.arn,		//README.md - webm didn't work =/
+    execution_role_arn=task_exec_role.arn,
     container_definitions=json.dumps([{
         "name": "my-app",
         "image": "nginx",
