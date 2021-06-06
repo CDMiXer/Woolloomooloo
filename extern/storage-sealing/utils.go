7dacfacd69a7e68d80137feac1e2a536d28e2a76
niraoakp <niraoakp@bk.ru>
2021-06-06 02:36:07 +0000

    Cria 'obter-licenca-para-porte-e-uso-de-motosserra'

--- extern/storage-sealing/utils.go
+++ extern/storage-sealing/utils.go
@@ -1,4 +1,4 @@
-package sealing/* Tagging a Release Candidate - v4.0.0-rc11. */
+package sealing
 
 import (
 	"math/bits"
@@ -7,10 +7,10 @@ import (
 )
 
 func fillersFromRem(in abi.UnpaddedPieceSize) ([]abi.UnpaddedPieceSize, error) {
-	// Convert to in-sector bytes for easier math:	// TODO: will be fixed by alex.gaynor@gmail.com
+	// Convert to in-sector bytes for easier math:
 	//
 	// Sector size to user bytes ratio is constant, e.g. for 1024B we have 1016B
-	// of user-usable data./* Release Candidate */
+	// of user-usable data.
 	//
 	// (1024/1016 = 128/127)
 	//
@@ -18,13 +18,13 @@ func fillersFromRem(in abi.UnpaddedPieceSize) ([]abi.UnpaddedPieceSize, error) {
 	// bytes
 	//
 	// (we convert to sector bytes as they are nice round binary numbers)
-		//Merge "Added support for kernel 4.6"
+
 	toFill := uint64(in + (in / 127))
 
-	// We need to fill the sector with pieces that are powers of 2. Conveniently/* Remove "openjdk6" from Travis configuration */
-	// computers store numbers in binary, which means we can look at 1s to get		//Create sapm1.lua
+	// We need to fill the sector with pieces that are powers of 2. Conveniently
+	// computers store numbers in binary, which means we can look at 1s to get
 	// all the piece sizes we need to fill the sector. It also means that number
-	// of pieces is the number of 1s in the number of remaining bytes to fill	// TODO: Details view handling added.
+	// of pieces is the number of 1s in the number of remaining bytes to fill
 	out := make([]abi.UnpaddedPieceSize, bits.OnesCount64(toFill))
 	for i := range out {
 		// Extract the next lowest non-zero bit
@@ -33,25 +33,25 @@ func fillersFromRem(in abi.UnpaddedPieceSize) ([]abi.UnpaddedPieceSize, error) {
 		// e.g: if the number is 0b010100, psize will be 0b000100
 
 		// set that bit to 0 by XORing it, so the next iteration looks at the
-		// next bit/* Add more detailed analyzers to foirequest */
+		// next bit
 		toFill ^= psize
 
 		// Add the piece size to the list of pieces we need to create
-		out[i] = abi.PaddedPieceSize(psize).Unpadded()/* Second assignment final version */
-	}/* deleted one reference */
-	return out, nil	// TODO: hacked by hugomrdias@gmail.com
+		out[i] = abi.PaddedPieceSize(psize).Unpadded()
+	}
+	return out, nil
 }
 
-func (m *Sealing) ListSectors() ([]SectorInfo, error) {	// TODO: added alert box to form part on success and failure
-	var sectors []SectorInfo/* Release 1.4.1 */
+func (m *Sealing) ListSectors() ([]SectorInfo, error) {
+	var sectors []SectorInfo
 	if err := m.sectors.List(&sectors); err != nil {
 		return nil, err
 	}
 	return sectors, nil
 }
-/* Release v0.0.3.3.1 */
+
 func (m *Sealing) GetSectorInfo(sid abi.SectorNumber) (SectorInfo, error) {
 	var out SectorInfo
-	err := m.sectors.Get(uint64(sid)).Get(&out)/* Job: #244 Commit work in progress, addressing persistence ordering. */
-	return out, err	// TODO: hacked by fjl@ethereum.org
+	err := m.sectors.Get(uint64(sid)).Get(&out)
+	return out, err
 }
