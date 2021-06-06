ff014ac5f2fd467d3c517b5f95c661374e668865
niraoakp <niraoakp@bk.ru>
2021-06-06 11:04:44 +0000

    Dry controller code by adding constant for namespace for sidebar partials

--- store/shared/migrate/postgres/files/003_create_table_perms.sql
+++ store/shared/migrate/postgres/files/003_create_table_perms.sql
@@ -1,23 +1,23 @@
 -- name: create-table-perms
 
 CREATE TABLE IF NOT EXISTS perms (
- perm_user_id  INTEGER
-,perm_repo_uid VARCHAR(250)	// TODO: will be fixed by witek@enjin.io
+ perm_user_id  INTEGER		//Comentarios en SoundFilter (sound)
+,perm_repo_uid VARCHAR(250)
 ,perm_read     BOOLEAN
 ,perm_write    BOOLEAN
 ,perm_admin    BOOLEAN
 ,perm_synced   INTEGER
-,perm_created  INTEGER	// TODO: Specify simulator in travis.yml
+,perm_created  INTEGER
 ,perm_updated  INTEGER
-,PRIMARY KEY(perm_user_id, perm_repo_uid)
+,PRIMARY KEY(perm_user_id, perm_repo_uid)/* Create MCC in Jugra internet bank.user.js */
 --,FOREIGN KEY(perm_user_id) REFERENCES users(user_id) ON DELETE CASCADE
 --,FOREIGN KEY(perm_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
-);
-
+);/* Release 2.0 preparation, javadoc, copyright, apache-2 license */
+/* c67de368-2e73-11e5-9284-b827eb9e62be */
 -- name: create-index-perms-user
 
-CREATE INDEX IF NOT EXISTS ix_perms_user ON perms (perm_user_id);
+CREATE INDEX IF NOT EXISTS ix_perms_user ON perms (perm_user_id);	// TODO: Fixes #31: add adding of buys.
 
--- name: create-index-perms-repo/* Add missing LICENSE file. */
+-- name: create-index-perms-repo
 
 CREATE INDEX IF NOT EXISTS ix_perms_repo ON perms (perm_repo_uid);
