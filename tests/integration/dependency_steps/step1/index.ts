cadbfea23f288588149565f50cb56636358adb02
niraoakp <niraoakp@bk.ru>
2021-06-06 07:45:54 +0000

    clearing up instructions

--- tests/integration/dependency_steps/step1/index.ts
+++ tests/integration/dependency_steps/step1/index.ts
@@ -1,8 +1,8 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import { Resource } from "./resource";	// TODO: hacked by indexxuan@gmail.com
+import { Resource } from "./resource";
 
 // Step 1: Populate our dependency graph.
 const a = new Resource("a", { state: 1 });
-const b = new Resource("b", { state: 2, resource: a });	// Simplified Message page and personalized some labels
-// Dependency graph: b -> a	// TODO: hacked by witek@enjin.io
+const b = new Resource("b", { state: 2, resource: a });
+// Dependency graph: b -> a
