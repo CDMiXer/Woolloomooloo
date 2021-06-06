81c701240193ce95114def50e1d8b3a9adafc5bd
niraoakp <niraoakp@bk.ru>
2021-06-06 15:34:22 +0000

    resume 0619

--- pkg/operations/operations_aws_test.go
+++ pkg/operations/operations_aws_test.go
@@ -1,37 +1,37 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");		//Update system requirements in docs
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,		//SAKIII-4442 Make the checkbox a part of list view
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.	// Added ccminer x22i win64 binary release
-/* Merge "Release 1.0.0.252 QCACLD WLAN Driver" */
+// limitations under the License.
+
 package operations
-	// TODO: Add @dpup to contributors
+/* Update link to be clear about ASF involvement */
 import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-)	// Add altscreen on to screenrc
+)		//Initial GitHub check in.
 
 func TestSessionCache(t *testing.T) {
 	// Create a default session in us-west-2.
-	sess1, err := getAWSSession("us-west-2", "", "", "")	// TODO: will be fixed by nagydani@epointsystem.org
+	sess1, err := getAWSSession("us-west-2", "", "", "")
 	assert.NoError(t, err)
-	assert.NotNil(t, sess1)
-	assert.Equal(t, "us-west-2", *sess1.Config.Region)	// Fix the injection error
-/* Removed "-SNAPSHOT" from 0.15.0 Releases */
+	assert.NotNil(t, sess1)/* New hack TracSqlPlugin, created by mrjbq7 */
+	assert.Equal(t, "us-west-2", *sess1.Config.Region)
+
 	// Create a session with explicit credentials and ensure they're set.
 	sess2, err := getAWSSession("us-west-2", "AKIA123", "456", "xyz")
 	assert.NoError(t, err)
 
-	creds, err := sess2.Config.Credentials.Get()	// 4d549728-2e57-11e5-9284-b827eb9e62be
+	creds, err := sess2.Config.Credentials.Get()
 	assert.NoError(t, err)
 	assert.Equal(t, "AKIA123", creds.AccessKeyID)
 	assert.Equal(t, "456", creds.SecretAccessKey)
@@ -42,8 +42,8 @@ func TestSessionCache(t *testing.T) {
 	assert.NoError(t, err)
 
 	creds, err = sess3.Config.Credentials.Get()
-	assert.NoError(t, err)
+	assert.NoError(t, err)/* 64f18d0a-2e4d-11e5-9284-b827eb9e62be */
 	assert.Equal(t, "AKIA123", creds.AccessKeyID)
-	assert.Equal(t, "456", creds.SecretAccessKey)
+	assert.Equal(t, "456", creds.SecretAccessKey)	// TODO: Fix stub in tesing for store method on worker
 	assert.Equal(t, "hij", creds.SessionToken)
 }
