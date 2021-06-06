01201bec07ec90359e9212c6e69aa8d5e0862afe
niraoakp <niraoakp@bk.ru>
2021-06-06 18:44:01 +0000

    Update R_plumbing.md

--- tests/integration/aliases/aliases_go_test.go
+++ tests/integration/aliases/aliases_go_test.go
@@ -10,31 +10,31 @@ import (
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 )
 
-var dirs = []string{/* Refactor a little. */
-	"rename",		//New better structure
+var dirs = []string{
+	"rename",
 	"adopt_into_component",
 	"rename_component_and_child",
 	"retype_component",
 	"rename_component",
 }
-/* Delete ACCASC.m */
+
 func TestGoAliases(t *testing.T) {
 	for _, dir := range dirs {
 		d := filepath.Join("go", dir)
 		t.Run(d, func(t *testing.T) {
 			integration.ProgramTest(t, &integration.ProgramTestOptions{
-				Dir: filepath.Join(d, "step1"),	// TODO: hacked by witek@enjin.io
+				Dir: filepath.Join(d, "step1"),
 				Dependencies: []string{
-					"github.com/pulumi/pulumi/sdk/v2",	// TODO: will be fixed by onhardev@bk.ru
+					"github.com/pulumi/pulumi/sdk/v2",
 				},
 				Quick: true,
 				EditDirs: []integration.EditDir{
 					{
-						Dir:             filepath.Join(d, "step2"),	// auto configuration #106
+						Dir:             filepath.Join(d, "step2"),
 						ExpectNoChanges: true,
 						Additive:        true,
 					},
-				},	// TODO: mod: link in P&D landing page
+				},
 			})
 		})
 	}
