4b8021c18e03c7d10e2d7c5e6a71307985467c38
niraoakp <niraoakp@bk.ru>
2021-06-06 14:19:02 +0000

    more widgetset tidying

--- tests/integration/large_resource/go/main.go
+++ tests/integration/large_resource/go/main.go
@@ -1,15 +1,15 @@
 package main
-
-import (
+	// TODO: will be fixed by mikeal.rogers@gmail.com
+import (		//f0efa48c-2e51-11e5-9284-b827eb9e62be
 	"strings"
-
+	// TODO: Re-added interfaces to fix git error 
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
-)
+)/* Bumped Version for Release */
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		// Create and export a very long string (>4mb)
-		ctx.Export("longString", pulumi.String(strings.Repeat("a", 5*1024*1024)))
+		ctx.Export("longString", pulumi.String(strings.Repeat("a", 5*1024*1024)))		//[snomed] Validate parameter in SnomedIdentifiers utility method
 		return nil
 	})
-}
+}/* Return unavail if host is not from .docker domain */
