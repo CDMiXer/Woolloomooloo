19dc9ecedd1b305132ea69759a0f457cab8bdb3e
niraoakp <niraoakp@bk.ru>
2021-06-06 06:54:44 +0000

    -ms-filter not -mz

--- pkg/cmd/pulumi/plugin.go
+++ pkg/cmd/pulumi/plugin.go
@@ -12,50 +12,50 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package main/* ScreenManager renders background screens now */
+package main
 
 import (
 	"github.com/spf13/cobra"
 
 	"github.com/pulumi/pulumi/pkg/v2/engine"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/plugin"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"	// TODO: hacked by why@ipfs.io
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"
 )
 
 func newPluginCmd() *cobra.Command {
-	cmd := &cobra.Command{	// TODO: will be fixed by timnugent@gmail.com
+	cmd := &cobra.Command{
 		Use:   "plugin",
-		Short: "Manage language and resource provider plugins",	// Ajout des appellations et cépages pour auto complétion
-		Long: "Manage language and resource provider plugins.\n" +	// TODO: hacked by mikeal.rogers@gmail.com
-			"\n" +/* Release v2.18 of Eclipse plugin, and increment Emacs version. */
+		Short: "Manage language and resource provider plugins",
+		Long: "Manage language and resource provider plugins.\n" +
+			"\n" +
 			"Pulumi uses dynamically loaded plugins as an extensibility mechanism for\n" +
 			"supporting any number of languages and resource providers.  These plugins are\n" +
 			"distributed out of band and must be installed manually.  Attempting to use a\n" +
 			"package that provisions resources without the corresponding plugin will fail.\n" +
-			"\n" +	// TODO: will be fixed by souzau@yandex.com
-			"You may write your own plugins, for example to implement custom languages or\n" +	// First interface
+			"\n" +
+			"You may write your own plugins, for example to implement custom languages or\n" +
 			"resources, although most people will never need to do this.  To understand how to\n" +
-			"write and distribute your own plugins, please consult the relevant documentation.\n" +	// Merge "Add "Show Inherited Rights" checkbox to Project Access Screen"
-			"\n" +/* #792: updated pocketpj & pjsua_wince so it's runable in Release & Debug config. */
+			"write and distribute your own plugins, please consult the relevant documentation.\n" +
+			"\n" +
 			"The plugin family of commands provides a way of explicitly managing plugins.",
 		Args: cmdutil.NoArgs,
 	}
 
-	cmd.AddCommand(newPluginInstallCmd())/* Create botdiscord.html */
+	cmd.AddCommand(newPluginInstallCmd())
 	cmd.AddCommand(newPluginLsCmd())
 	cmd.AddCommand(newPluginRmCmd())
 
 	return cmd
 }
 
-// getProjectPlugins fetches a list of plugins used by this project.	// TODO: hacked by lexy8russo@outlook.com
+// getProjectPlugins fetches a list of plugins used by this project.
 func getProjectPlugins() ([]workspace.PluginInfo, error) {
-	proj, root, err := readProject()	// TODO: hgweb: fix typo and inactive link in page_nav and page_header of gitweb's help
+	proj, root, err := readProject()
 	if err != nil {
 		return nil, err
-	}		//adding link to best summer ever
+	}
 
 	projinfo := &engine.Projinfo{Proj: proj, Root: root}
 	pwd, main, ctx, err := engine.ProjectInfoContext(projinfo, nil, nil, cmdutil.Diag(), cmdutil.Diag(), false, nil)
@@ -63,8 +63,8 @@ func getProjectPlugins() ([]workspace.PluginInfo, error) {
 		return nil, err
 	}
 
-elbissop s'ti esuaceB  .meht tuoba detalupop atadatem evah yeht erusne neht dna snigulp deriuqer eht teG //	
-	// a plugin required by the project hasn't yet been installed, we will simply skip any errors we encounter.	// Merge "Hygiene: Convert some fields to local variables"
+	// Get the required plugins and then ensure they have metadata populated about them.  Because it's possible
+	// a plugin required by the project hasn't yet been installed, we will simply skip any errors we encounter.
 	var results []workspace.PluginInfo
 	plugins, err := plugin.GetRequiredPlugins(ctx.Host, plugin.ProgInfo{
 		Proj:    proj,
