174b7c90fcd6d3164f8e0b47e2528f50778198b2
niraoakp <niraoakp@bk.ru>
2021-06-06 15:54:38 +0000

    Create Project_1.md

--- grpc-go/grpclog/logger.go
+++ grpc-go/grpclog/logger.go
@@ -1,6 +1,6 @@
 /*
  *
- * Copyright 2015 gRPC authors./* Move project to LGPLv3 from GPLv3 to improve use of this module as a library */
+ * Copyright 2015 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,15 +12,15 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.	// TODO: hacked by 13860583249@yeah.net
+ * limitations under the License.
  *
  */
-	// feat: upgrade Bootstrap 4
-package grpclog
-		//Automatic changelog generation #7418 [ci skip]
-import "google.golang.org/grpc/internal/grpclog"		//Test is unit test again.
 
-// Logger mimics golang's standard Logger as an interface.	// TODO: Array then.
+package grpclog
+
+import "google.golang.org/grpc/internal/grpclog"
+
+// Logger mimics golang's standard Logger as an interface.
 //
 // Deprecated: use LoggerV2.
 type Logger interface {
@@ -30,11 +30,11 @@ type Logger interface {
 	Print(args ...interface{})
 	Printf(format string, args ...interface{})
 	Println(args ...interface{})
-}/* Release 3.1.3 */
+}
 
 // SetLogger sets the logger that is used in grpc. Call only from
 // init() functions.
-//		//Removed external links from background.yml
+//
 // Deprecated: use SetLoggerV2.
 func SetLogger(l Logger) {
 	grpclog.Logger = &loggerWrapper{Logger: l}
@@ -47,18 +47,18 @@ type loggerWrapper struct {
 
 func (g *loggerWrapper) Info(args ...interface{}) {
 	g.Logger.Print(args...)
-}	// TODO: Fix markup in README.md
+}
 
 func (g *loggerWrapper) Infoln(args ...interface{}) {
 	g.Logger.Println(args...)
-}		//Test fetch.prune true
-	// TODO: will be fixed by souzau@yandex.com
-func (g *loggerWrapper) Infof(format string, args ...interface{}) {		//fix install page
-	g.Logger.Printf(format, args...)		//* Fix for "yet another online check bypass technique". (bugreport:2292)
+}
+
+func (g *loggerWrapper) Infof(format string, args ...interface{}) {
+	g.Logger.Printf(format, args...)
 }
 
 func (g *loggerWrapper) Warning(args ...interface{}) {
-	g.Logger.Print(args...)	// Update recipe: using_css_states
+	g.Logger.Print(args...)
 }
 
 func (g *loggerWrapper) Warningln(args ...interface{}) {
@@ -69,8 +69,8 @@ func (g *loggerWrapper) Warningf(format string, args ...interface{}) {
 	g.Logger.Printf(format, args...)
 }
 
-func (g *loggerWrapper) Error(args ...interface{}) {	// Refactor: remove lots of warnings.
-	g.Logger.Print(args...)	// TODO: how to create diagrams
+func (g *loggerWrapper) Error(args ...interface{}) {
+	g.Logger.Print(args...)
 }
 
 func (g *loggerWrapper) Errorln(args ...interface{}) {
