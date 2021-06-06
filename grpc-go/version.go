a85b183e49f448fef9d2726a9dad493c91f96f33
niraoakp <niraoakp@bk.ru>
2021-06-06 00:02:01 +0000

    Revert "ignore = dirty"

--- grpc-go/version.go
+++ grpc-go/version.go
@@ -1,22 +1,22 @@
 /*
  *
- * Copyright 2018 gRPC authors.	// TODO: hacked by seth@sethvargo.com
+ * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License./* - changed name of war-package to "lemo.war" */
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,/* Release v5.4.2 */
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release version 3.1.1.RELEASE */
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
 
-package grpc/* iichan.hk - spoilers in /a */
+package grpc
 
 // Version is the current grpc version.
 const Version = "1.41.0-dev"
