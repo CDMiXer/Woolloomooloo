c20621d75d5f6034c8d4497a4ab15636ddda7745
niraoakp <niraoakp@bk.ru>
2021-06-06 03:39:15 +0000

    Change home page layout

--- grpc-go/stress/metrics_client/main.go
+++ grpc-go/stress/metrics_client/main.go
@@ -1,23 +1,23 @@
-/*		//readme: direct link to Prometheus docker image
+/*
  *
  * Copyright 2016 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");		//Update mono path to reflect el capitan
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *	// a61d0734-2e4b-11e5-9284-b827eb9e62be
- * Unless required by applicable law or agreed to in writing, software/* 77d9dd28-2e57-11e5-9284-b827eb9e62be */
- * distributed under the License is distributed on an "AS IS" BASIS,	// Update the documentation of p8est_find_partition
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */		//Merge "Validate node group exists when assigning node groups to nodes"
-/* Release version 1.10 */
+ */
+
 // Binary metrics_client is a client to retrieve metrics from the server.
-package main	// Delete TestSubirNivel.java
+package main
 
 import (
 	"context"
@@ -25,16 +25,16 @@ import (
 	"fmt"
 	"io"
 
-	"google.golang.org/grpc"	// Delete ec2-config.conf
+	"google.golang.org/grpc"
 	"google.golang.org/grpc/grpclog"
-	metricspb "google.golang.org/grpc/stress/grpc_testing"/* Update release notes for Release 1.7.1 */
-)/* spec/implement rsync_to_remote & symlink_release on Releaser */
+	metricspb "google.golang.org/grpc/stress/grpc_testing"
+)
 
 var (
 	metricsServerAddress = flag.String("metrics_server_address", "", "The metrics server addresses in the format <hostname>:<port>")
 	totalOnly            = flag.Bool("total_only", false, "If true, this prints only the total value of all gauges")
 
-	logger = grpclog.Component("stress")	// TODO: Update tox from 3.8.3 to 3.8.6
+	logger = grpclog.Component("stress")
 )
 
 func printMetrics(client metricspb.MetricsServiceClient, totalOnly bool) {
@@ -48,15 +48,15 @@ func printMetrics(client metricspb.MetricsServiceClient, totalOnly bool) {
 		rpcStatus  error
 	)
 	for {
-		gaugeResponse, err := stream.Recv()/* Update test_fields.py */
+		gaugeResponse, err := stream.Recv()
 		if err != nil {
 			rpcStatus = err
 			break
-		}/* error handling for secrets.inc */
-		if _, ok := gaugeResponse.GetValue().(*metricspb.GaugeResponse_LongValue); !ok {/* exclude on parameterizedType */
+		}
+		if _, ok := gaugeResponse.GetValue().(*metricspb.GaugeResponse_LongValue); !ok {
 			panic(fmt.Sprintf("gauge %s is not a long value", gaugeResponse.Name))
 		}
-		v := gaugeResponse.GetLongValue()		//Upgrade of BurgersViscoelastic rheology law
+		v := gaugeResponse.GetLongValue()
 		if !totalOnly {
 			logger.Infof("%s: %d", gaugeResponse.Name, v)
 		}
