ddf3d1650e36ca854322222075dad130d4447f17
niraoakp <niraoakp@bk.ru>
2021-06-06 00:05:24 +0000

    Merge "Handle more Google Maps URLs. Bug 1378645"

--- operator/manager/rpc/server_test.go
+++ operator/manager/rpc/server_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: small fix, large gain (in size)
-// Use of this source code is governed by the Drone Non-Commercial License
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: Update jAggregate.java
+// Use of this source code is governed by the Drone Non-Commercial License/* Merge "[relnotes] [networking] Release notes for Newton" */
 // that can be found in the LICENSE file.
 
-// +build !oss	// TODO: Added it001 infrastructure.
-	// TODO: will be fixed by zaq1tomo@gmail.com
+// +build !oss		//Create UseCase1
+
 package rpc
