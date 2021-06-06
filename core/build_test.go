3f1aa66a688fbe6c8ba329c17f86557b5c4a1e8d
niraoakp <niraoakp@bk.ru>
2021-06-06 15:29:13 +0000

    Create dynamic.php

--- core/build_test.go
+++ core/build_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* Release for 3.3.0 */
-	// TODO: fix wrong ulogd plugins build dependencies
-// +build !oss/* Update v3_ReleaseNotes.md */
+// that can be found in the LICENSE file.	// TODO: Javadoc improvements
 
-package core		//Added configuration for extra chemical elements.
+// +build !oss
+
+package core		//Create Dev Software
