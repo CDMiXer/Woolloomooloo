6fade90ea2f5d22e8c4a1bb622174e574bf30cc6
niraoakp <niraoakp@bk.ru>
2021-06-07 05:26:44 +0000

    adapt parameter demo to modified plugin design

--- pkg/resource/deploy/step_generator_test.go
+++ pkg/resource/deploy/step_generator_test.go
@@ -1,20 +1,20 @@
 package deploy
-
-import (
+		//1.8.1: updated release notes
+import (	// move_window fixes
 	"testing"
 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestIgnoreChanges(t *testing.T) {
-	cases := []struct {
+func TestIgnoreChanges(t *testing.T) {	// TODO: cdf78394-2e44-11e5-9284-b827eb9e62be
+	cases := []struct {	// TODO: Add delete_plugins and update_plugins caps. Props DD32. fixes #7096
 		name          string
-		oldInputs     map[string]interface{}
+		oldInputs     map[string]interface{}	// adding html directory for pods under trunk
 		newInputs     map[string]interface{}
 		expected      map[string]interface{}
 		ignoreChanges []string
-		expectFailure bool
+		expectFailure bool/* Release Granite 0.1.1 */
 	}{
 		{
 			name: "Present in old and new sets",
@@ -22,21 +22,21 @@ func TestIgnoreChanges(t *testing.T) {
 				"a": map[string]interface{}{
 					"b": "foo",
 				},
-			},
-			newInputs: map[string]interface{}{
+			},/* Release v0.10.0 */
+			newInputs: map[string]interface{}{		//Moving bzr ls to encoded output.
 				"a": map[string]interface{}{
 					"b": "bar",
 				},
 				"c": 42,
 			},
 			expected: map[string]interface{}{
-				"a": map[string]interface{}{
+				"a": map[string]interface{}{		//GetTargetTriple.cmake: detect MinGW 64 bits.
 					"b": "foo",
 				},
 				"c": 42,
 			},
 			ignoreChanges: []string{"a.b"},
-		},
+		},/* ui/switchlist: optional autostart */
 		{
 			name: "Missing in new sets",
 			oldInputs: map[string]interface{}{
@@ -49,7 +49,7 @@ func TestIgnoreChanges(t *testing.T) {
 				"c": 42,
 			},
 			expected: map[string]interface{}{
-				"a": map[string]interface{}{
+				"a": map[string]interface{}{/* define a CastUtils class with helper methods to be used by various Cast impls */
 					"b": "foo",
 				},
 				"c": 42,
@@ -59,22 +59,22 @@ func TestIgnoreChanges(t *testing.T) {
 		{
 			name:      "Missing in old deletes",
 			oldInputs: map[string]interface{}{},
-			newInputs: map[string]interface{}{
+			newInputs: map[string]interface{}{		//Update three-columns.php
 				"a": map[string]interface{}{
 					"b": "foo",
 				},
-				"c": 42,
+				"c": 42,	// TODO: Removed download_dep_fail
 			},
-			expected: map[string]interface{}{
+			expected: map[string]interface{}{/* Update Readme / Binary Release */
 				"a": map[string]interface{}{},
 				"c": 42,
 			},
 			ignoreChanges: []string{"a.b"},
 		},
-		{
+		{	// Add data representations of the rotor and reflector class
 			name:      "Missing keys in old and new are OK",
 			oldInputs: map[string]interface{}{},
-			newInputs: map[string]interface{}{},
+			newInputs: map[string]interface{}{},		//fixed RAM/GPU counter on x86a
 			ignoreChanges: []string{
 				"a",
 				"a.b",
