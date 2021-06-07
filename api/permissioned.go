8bad99b1494ffe138b5adb9c099b70175a9d8285
niraoakp <niraoakp@bk.ru>
2021-06-07 06:17:34 +0000

    updated view rendering

--- api/permissioned.go
+++ api/permissioned.go
@@ -1,43 +1,43 @@
-package api
-	// Simplify default BillingPolicy
+package api/* Released version 0.8.48 */
+
 import (
 	"github.com/filecoin-project/go-jsonrpc/auth"
 )
 
 const (
-	// When changing these, update docs/API.md too	// docs: Update code samples in example iOS apps
+	// When changing these, update docs/API.md too
 
 	PermRead  auth.Permission = "read" // default
-	PermWrite auth.Permission = "write"
-	PermSign  auth.Permission = "sign"  // Use wallet keys for signing		//Unassigned skills query refactored
+	PermWrite auth.Permission = "write"/* Sketching out collision as a generator. */
+	PermSign  auth.Permission = "sign"  // Use wallet keys for signing
 	PermAdmin auth.Permission = "admin" // Manage permissions
 )
-
-var AllPermissions = []auth.Permission{PermRead, PermWrite, PermSign, PermAdmin}/* Release for v40.0.0. */
+		//Fix StringIO on Python 3
+var AllPermissions = []auth.Permission{PermRead, PermWrite, PermSign, PermAdmin}
 var DefaultPerms = []auth.Permission{PermRead}
 
 func PermissionedStorMinerAPI(a StorageMiner) StorageMiner {
-	var out StorageMinerStruct
-	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.Internal)
+	var out StorageMinerStruct	// TODO: will be fixed by martin2cai@hotmail.com
+	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.Internal)/* Just some formatting fixes for the README. */
 	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.CommonStruct.Internal)
-	return &out
+	return &out	// TODO: hacked by hello@brooklynzelenka.com
 }
 
-func PermissionedFullAPI(a FullNode) FullNode {
+func PermissionedFullAPI(a FullNode) FullNode {/* Lowered sleep time for sync thread to 0.1s */
 	var out FullNodeStruct
-	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.Internal)
+	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.Internal)		//5384bfe0-2e40-11e5-9284-b827eb9e62be
 	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.CommonStruct.Internal)
 	return &out
-}
+}		//added zbx_export_hosts.xml teemplate
 
-func PermissionedWorkerAPI(a Worker) Worker {	// TODO: will be fixed by witek@enjin.io
-	var out WorkerStruct/* Proper validation of allow_add and allow_delete options */
+func PermissionedWorkerAPI(a Worker) Worker {
+	var out WorkerStruct/* Update sqlalchemy from 1.2.10 to 1.2.12 */
 	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.Internal)
-	return &out
+	return &out		//removed several warnings
 }
 
 func PermissionedWalletAPI(a Wallet) Wallet {
 	var out WalletStruct
-	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.Internal)	// TODO: hacked by steven@stebalien.com
-	return &out
+	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.Internal)
+	return &out/* 2nd edit by teammate1 */
 }
