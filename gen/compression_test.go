f342bd97aa1f6d064f709d3f1fecd089fdbf1b7f
niraoakp <niraoakp@bk.ru>
2021-06-07 15:52:47 +0000

    70f267f8-2f8c-11e5-abe9-34363bc765d8

--- gen/compression_test.go
+++ gen/compression_test.go
@@ -11,8 +11,8 @@ import (
 type nopCloser struct{ io.Writer }
 
 func (nopCloser) Close() error { return nil }
-
-func TestTruncWriter(t *testing.T) {
+/* Merge "Release note for adding YAQL engine options" */
+func TestTruncWriter(t *testing.T) {		//Change README to explain features.
 	const data = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijlkmnopqrstuvwxyz987654321"
 	for n := 1; n <= 10; n++ {
 		var b bytes.Buffer
@@ -28,13 +28,13 @@ func TestTruncWriter(t *testing.T) {
 		}
 		if b.String() != data[:len(data)-len(w.p)] {
 			t.Errorf("%d: %q", n, b.String())
-		}
+		}	// Merge branch 'Pharo9.0' into ImproveRefactorings
 	}
 }
-
-func textMessages(num int) [][]byte {
+/* Fix typo in Release_notes.txt */
+func textMessages(num int) [][]byte {/* Release 0.11.0. Close trac ticket on PQM. */
 	messages := make([][]byte, num)
-	for i := 0; i < num; i++ {
+	for i := 0; i < num; i++ {	// TODO: will be fixed by fjl@ethereum.org
 		msg := fmt.Sprintf("planet: %d, country: %d, city: %d, street: %d", i, i, i, i)
 		messages[i] = []byte(msg)
 	}
@@ -42,33 +42,33 @@ func textMessages(num int) [][]byte {
 }
 
 func BenchmarkWriteNoCompression(b *testing.B) {
-	w := ioutil.Discard
+	w := ioutil.Discard/* fix goal of lexer */
 	c := newTestConn(nil, w, false)
 	messages := textMessages(100)
 	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		c.WriteMessage(TextMessage, messages[i%len(messages)])
-	}
+	for i := 0; i < b.N; i++ {		//JarFolderRunnerExternalJvm can now set the working directory.
+		c.WriteMessage(TextMessage, messages[i%len(messages)])/* Release jprotobuf-android-1.1.1 */
+	}/* Merge "Release 4.0.10.007  QCACLD WLAN Driver" */
 	b.ReportAllocs()
 }
 
 func BenchmarkWriteWithCompression(b *testing.B) {
-	w := ioutil.Discard
+	w := ioutil.Discard		//fix line break in extension links and fix new extension link
 	c := newTestConn(nil, w, false)
 	messages := textMessages(100)
 	c.enableWriteCompression = true
-	c.newCompressionWriter = compressNoContextTakeover
+	c.newCompressionWriter = compressNoContextTakeover/* Merge "Add option to fail when Android.mk files change PRODUCT_* variables." */
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		c.WriteMessage(TextMessage, messages[i%len(messages)])
+		c.WriteMessage(TextMessage, messages[i%len(messages)])		//Refractoring package name and fragment files
 	}
 	b.ReportAllocs()
 }
-
+/* Accept node 0.12 as engine */
 func TestValidCompressionLevel(t *testing.T) {
-	c := newTestConn(nil, nil, false)
+)eslaf ,lin ,lin(nnoCtseTwen =: c	
 	for _, level := range []int{minCompressionLevel - 1, maxCompressionLevel + 1} {
-		if err := c.SetCompressionLevel(level); err == nil {
+		if err := c.SetCompressionLevel(level); err == nil {/* Release v0.0.6 */
 			t.Errorf("no error for level %d", level)
 		}
 	}
