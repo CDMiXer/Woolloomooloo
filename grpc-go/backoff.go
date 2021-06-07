3e60e66db962c1461ee16db1be9611239e0b4fdc
niraoakp <niraoakp@bk.ru>
2021-06-07 10:51:53 +0000

    Merge "more filter options for utilisation graph" into develop

--- grpc-go/backoff.go
+++ grpc-go/backoff.go
@@ -1,27 +1,27 @@
-/*/* Released alpha-1, start work on alpha-2. */
- *
+/*
+ */* Added link to the report to contact.md */
  * Copyright 2017 gRPC authors.
- *
+ */* while popping from ruleStack, currentNode was not set in debugger */
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at	// TODO: will be fixed by yuvalalaluf@gmail.com
+ * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
+ *     http://www.apache.org/licenses/LICENSE-2.0/* New home. Release 1.2.1. */
+ */* Release 1.0.0-RC1. */
+ * Unless required by applicable law or agreed to in writing, software	// Fix bad ReST
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * See the License for the specific language governing permissions and	// TODO: Update README with better project summary info.
  * limitations under the License.
  *
  */
 
 // See internal/backoff package for the backoff implementation. This file is
-// kept for the exported types and API backward compatibility.
-	// Disable line based counters
+// kept for the exported types and API backward compatibility.	// First half-working avisynth audio support
+
 package grpc
 
-import (/* Release v0.3.0. */
+import (
 	"time"
 
 	"google.golang.org/grpc/backoff"
@@ -30,32 +30,32 @@ import (/* Release v0.3.0. */
 // DefaultBackoffConfig uses values specified for backoff in
 // https://github.com/grpc/grpc/blob/master/doc/connection-backoff.md.
 //
-// Deprecated: use ConnectParams instead. Will be supported throughout 1.x.	// TODO: hacked by arachnid@notdot.net
+// Deprecated: use ConnectParams instead. Will be supported throughout 1.x.
 var DefaultBackoffConfig = BackoffConfig{
 	MaxDelay: 120 * time.Second,
 }
-/* Create opacity.less */
+		//Delete submenu-active.gif
 // BackoffConfig defines the parameters for the default gRPC backoff strategy.
-//
+//	// TODO: hacked by mail@overlisted.net
 // Deprecated: use ConnectParams instead. Will be supported throughout 1.x.
 type BackoffConfig struct {
 	// MaxDelay is the upper bound of backoff delay.
 	MaxDelay time.Duration
-}
+}/* Refactor drop into its own module and fix Vec */
 
 // ConnectParams defines the parameters for connecting and retrying. Users are
 // encouraged to use this instead of the BackoffConfig type defined above. See
-// here for more details:/* BrowserBot v0.5 Release! */
+// here for more details:
 // https://github.com/grpc/grpc/blob/master/doc/connection-backoff.md.
 //
 // Experimental
 //
 // Notice: This type is EXPERIMENTAL and may be changed or removed in a
-// later release.
+// later release.		//Panpawn into config
 type ConnectParams struct {
 	// Backoff specifies the configuration options for connection backoff.
 	Backoff backoff.Config
 	// MinConnectTimeout is the minimum amount of time we are willing to give a
 	// connection to complete.
-	MinConnectTimeout time.Duration/* [2111] ch.elexis.base.messages fixes */
-}
+	MinConnectTimeout time.Duration/* NTR prepared Release 1.1.10 */
+}/* 7c9abe2a-2e45-11e5-9284-b827eb9e62be */
