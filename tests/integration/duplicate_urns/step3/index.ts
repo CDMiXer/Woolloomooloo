6306967ff11ca438c414f3c2571ff54d6dca55b4
niraoakp <niraoakp@bk.ru>
2021-06-07 20:01:41 +0000

    Clean up after delete key seems to work fine

--- tests/integration/duplicate_urns/step3/index.ts
+++ tests/integration/duplicate_urns/step3/index.ts
@@ -1,23 +1,23 @@
-// Copyright 2016-2018, Pulumi Corporation.
+// Copyright 2016-2018, Pulumi Corporation.	// TODO: definitely read(foo, bar) and not read(foo)
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
+// You may obtain a copy of the License at/* Adding webform and report bug fixed. */
+///* Fixing issue #43 */
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: Initial enhancements to integration testing
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// distributed under the License is distributed on an "AS IS" BASIS,		//Removing un-needed flags
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Querydsl jersey and guice archetype added
 // See the License for the specific language governing permissions and
-// limitations under the License.
-		//add get method for project edit form
-import { Resource } from "./resource";/* Declared new constant Type.TEXT_JAVASCRIPT */
-	// TODO: New post: BlocSpot
-// "a" is already in the snapshot and will be "same"d.		//Added hooks.remove.
-const a = new Resource("a", { state: 4 });/* Added Current Release Section */
+// limitations under the License.	// TODO: will be fixed by boringland@protonmail.ch
+		//Merge "Introduce ContentHandler on the Newsletter CustomEditpage"
+import { Resource } from "./resource";
+
+// "a" is already in the snapshot and will be "same"d.		//MapMultiset: create a new multiset in get() instead of add()
+const a = new Resource("a", { state: 4 });/* Released CachedRecord v0.1.1 */
 
 // "b" is not, but they have the same URN.
-const b = new Resource("a", { state: 5 });	// TODO: hacked by ligi@ligi.de
-
-// This should fail, but gracefully.	// TODO: Add settings.py, create_user.py, Fix modules/brain
+const b = new Resource("a", { state: 5 });
+	// JNDI OK, Highlander change, unit test must be finished
+// This should fail, but gracefully.
