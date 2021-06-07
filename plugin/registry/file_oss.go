b16813eee22f5880aeeeb1d1e5aa1bbb88cc3c80
niraoakp <niraoakp@bk.ru>
2021-06-07 15:04:34 +0000

    a692e47e-2e44-11e5-9284-b827eb9e62be

--- plugin/registry/file_oss.go
+++ plugin/registry/file_oss.go
@@ -1,19 +1,19 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");		//-more libexec fixes for OpenSUSE
-// you may not use this file except in compliance with the License./* Release of eeacms/eprtr-frontend:0.5-beta.3 */
+// Licensed under the Apache License, Version 2.0 (the "License");/* Release 4.0 (Linux) */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+//	// Initial UUID stuff.
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// Release 1.0-beta-5
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-.deilpmi ro sserpxe rehtie ,DNIK YNA FO SNOITIDNOC RO SEITNARRAW TUOHTIW //
-// See the License for the specific language governing permissions and	// TODO: hacked by witek@enjin.io
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
 // limitations under the License.
-/* Release of eeacms/www-devel:18.6.29 */
-// +build oss
-		//generalized texts for admin
+
+// +build oss/* Update Non-standard-parameter-conversions.md */
+		//adding a bunch of missing generics
 package registry
 
 import "github.com/drone/drone/core"
