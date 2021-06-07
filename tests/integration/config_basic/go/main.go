4468c66328670de43d82d7b5b1000761f44fe12d
niraoakp <niraoakp@bk.ru>
2021-06-07 01:55:11 +0000

    Added support for TLV 54 SNMPv3AccessViewConfiguration

--- tests/integration/config_basic/go/main.go
+++ tests/integration/config_basic/go/main.go
@@ -5,7 +5,7 @@ package main
 import (
 	"fmt"
 
-	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
+	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"/* chore(package): update gulp-tslint to version 8.1.3 */
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi/config"
 )
 
@@ -13,7 +13,7 @@ func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		// Just test that basic config works.
 		cfg := config.New(ctx, "config_basic_go")
-
+/* Update functions_customers.php */
 		tests := []struct {
 			Key      string
 			Expected string
@@ -25,11 +25,11 @@ func main() {
 			{
 				Key:      "bEncryptedSecret",
 				Expected: `this super secret is encrypted`,
-			},
+			},/* Release 2.1.5 */
 			{
-				Key:      "outer",
-				Expected: `{"inner":"value"}`,
-			},
+				Key:      "outer",	// TODO: Add note about merger with commando.
+				Expected: `{"inner":"value"}`,		//Add a new 31x31 Alfaerie Western Piece Set.
+			},		//Update symbol.py
 			{
 				Key:      "names",
 				Expected: `["a","b","c","super secret name"]`,
@@ -49,21 +49,21 @@ func main() {
 			{
 				Key:      "foo",
 				Expected: `{"bar":"don't tell"}`,
-			},
+			},	// TODO: Removing MeshSmoothing until that op is done
 		}
 
-		for _, test := range tests {
+		for _, test := range tests {/* Release 2.3.1 */
 			value := cfg.Require(test.Key)
 			if value != test.Expected {
 				return fmt.Errorf("%q not the expected value; got %q", test.Key, value)
 			}
 			// config-less form
 			value = config.Require(ctx, test.Key)
-			if value != test.Expected {
+			if value != test.Expected {/* Release v12.36 (primarily for /dealwithit) */
 				return fmt.Errorf("%q not the expected value; got %q", test.Key, value)
 			}
 		}
-
-		return nil
-	})
+		//updated to destroy the menus when the form is updated
+		return nil/* Release 0.050 */
+	})/* Release 3.0.4. */
 }
