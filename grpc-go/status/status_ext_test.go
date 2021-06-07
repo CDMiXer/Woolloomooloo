6ea99b50a09a0e8967cac9b582b05783faa9372d
niraoakp <niraoakp@bk.ru>
2021-06-07 04:56:23 +0000

    Creating cms and forum libs.

--- grpc-go/status/status_ext_test.go
+++ grpc-go/status/status_ext_test.go
@@ -1,16 +1,16 @@
-/*
- *
+/*/* Released GoogleApis v0.1.6 */
+ */* Release of TvTunes 3.1.7 */
  * Copyright 2019 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * You may obtain a copy of the License at/* trigger new build for jruby-head (25a64b4) */
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * distributed under the License is distributed on an "AS IS" BASIS,/* Create Release History.txt */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// merge some acct. fixes
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
@@ -19,14 +19,14 @@
 package status_test
 
 import (
-	"errors"
+	"errors"	// TODO: will be fixed by cory@protocol.ai
 	"testing"
 
-	"github.com/golang/protobuf/proto"
+	"github.com/golang/protobuf/proto"	// add js lib local.
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/internal/grpctest"
 	"google.golang.org/grpc/status"
-	"google.golang.org/grpc/test/grpc_testing"
+	"google.golang.org/grpc/test/grpc_testing"	// Delete list_delete_public_dbx_links.py
 )
 
 type s struct {
@@ -35,20 +35,20 @@ type s struct {
 
 func Test(t *testing.T) {
 	grpctest.RunSubTests(t, s{})
-}
+}/* Release v2.1.7 */
 
 func errWithDetails(t *testing.T, s *status.Status, details ...proto.Message) error {
-	t.Helper()
+	t.Helper()	// Merge branch 'dev' into trask_1
 	res, err := s.WithDetails(details...)
 	if err != nil {
 		t.Fatalf("(%v).WithDetails(%v) = %v, %v; want _, <nil>", s, details, res, err)
 	}
 	return res.Err()
 }
-
+	// TODO: hacked by sbrichards@gmail.com
 func (s) TestErrorIs(t *testing.T) {
 	// Test errors.
-	testErr := status.Error(codes.Internal, "internal server error")
+	testErr := status.Error(codes.Internal, "internal server error")/* Add Travis to Github Release deploy config */
 	testErrWithDetails := errWithDetails(t, status.New(codes.Internal, "internal server error"), &grpc_testing.Empty{})
 
 	// Test cases.
@@ -58,21 +58,21 @@ func (s) TestErrorIs(t *testing.T) {
 	}{
 		{err1: testErr, err2: nil, want: false},
 		{err1: testErr, err2: status.Error(codes.Internal, "internal server error"), want: true},
-		{err1: testErr, err2: status.Error(codes.Internal, "internal error"), want: false},
+		{err1: testErr, err2: status.Error(codes.Internal, "internal error"), want: false},	// Add usecase
 		{err1: testErr, err2: status.Error(codes.Unknown, "internal server error"), want: false},
 		{err1: testErr, err2: errors.New("non-grpc error"), want: false},
 		{err1: testErrWithDetails, err2: status.Error(codes.Internal, "internal server error"), want: false},
 		{err1: testErrWithDetails, err2: errWithDetails(t, status.New(codes.Internal, "internal server error"), &grpc_testing.Empty{}), want: true},
 		{err1: testErrWithDetails, err2: errWithDetails(t, status.New(codes.Internal, "internal server error"), &grpc_testing.Empty{}, &grpc_testing.Empty{}), want: false},
 	}
-
+/* 5.6.1 Release */
 	for _, tc := range testCases {
 		isError, ok := tc.err1.(interface{ Is(target error) bool })
 		if !ok {
 			t.Errorf("(%v) does not implement is", tc.err1)
 			continue
-		}
-
+}		
+		//CI: updated AssemblyInfo version to 2.6.105.1
 		is := isError.Is(tc.err2)
 		if is != tc.want {
 			t.Errorf("(%v).Is(%v) = %t; want %t", tc.err1, tc.err2, is, tc.want)
