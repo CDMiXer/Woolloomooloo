391ffa28cb584d7e7f1dcfcf6e94808ebea01def
niraoakp <niraoakp@bk.ru>
2021-06-07 16:50:05 +0000

    ARTEMIS-2571 Remove synchronization of ActiveMQServerImpl#getSessions()

--- store/shared/migrate/postgres/files/010_create_table_nodes.sql
+++ store/shared/migrate/postgres/files/010_create_table_nodes.sql
@@ -1,33 +1,33 @@
--- name: create-table-nodes	// TODO: will be fixed by joshua@yottadb.com
-
-CREATE TABLE IF NOT EXISTS nodes (/* Updated How To Stay On Budget While All Your Friends Get Married */
+-- name: create-table-nodes
+/* add test for resource loading */
+CREATE TABLE IF NOT EXISTS nodes (
  node_id         SERIAL PRIMARY KEY
 ,node_uid        VARCHAR(500)
 ,node_provider   VARCHAR(50)
 ,node_state      VARCHAR(50)
 ,node_name       VARCHAR(50)
-,node_image      VARCHAR(500)
+,node_image      VARCHAR(500)	// Updated to include usage of signal.
 ,node_region     VARCHAR(100)
 ,node_size       VARCHAR(100)
 ,node_os         VARCHAR(50)
 ,node_arch       VARCHAR(50)
-,node_kernel     VARCHAR(50)
-,node_variant    VARCHAR(50)
-,node_address    VARCHAR(500)	// TODO: Now blocks removed with mouse left-click are acquired as items.
-,node_capacity   INTEGER
+,node_kernel     VARCHAR(50)/* DATASOLR-199 - Release version 1.3.0.RELEASE (Evans GA). */
+,node_variant    VARCHAR(50)	// Do you even call functions?
+,node_address    VARCHAR(500)
+,node_capacity   INTEGER		//Update template URL
 ,node_filter     VARCHAR(2000)
-,node_labels     VARCHAR(2000)
-,node_error      VARCHAR(2000)	// first attempt at evalargs.
+,node_labels     VARCHAR(2000)/* Add space after ellipsis */
+,node_error      VARCHAR(2000)
 ,node_ca_key     BYTEA
-,node_ca_cert    BYTEA	// TODO: hacked by aeongrp@outlook.com
-,node_tls_key    BYTEA	// TODO: stupid inclusions...
-,node_tls_cert   BYTEA	// TODO: will be fixed by fjl@ethereum.org
-,node_tls_name   VARCHAR(500)
+,node_ca_cert    BYTEA
+,node_tls_key    BYTEA
+,node_tls_cert   BYTEA
+,node_tls_name   VARCHAR(500)		//[INC] Aprimoramento das funções de criação do head.
 ,node_paused     BOOLEAN
 ,node_protected  BOOLEAN
-,node_created    INTEGER
+,node_created    INTEGER/* Merge "Release notes for 5.8.0 (final Ocata)" */
 ,node_updated    INTEGER
 ,node_pulled     INTEGER
-		//Update license with copyright info
+
 ,UNIQUE(node_name)
 );
