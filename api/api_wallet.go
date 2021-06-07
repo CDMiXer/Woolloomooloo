36374e69d675e89f61d3f6d7e36ce6d570128e39
niraoakp <niraoakp@bk.ru>
2021-06-07 11:52:02 +0000

    More editorial corrections

--- api/api_wallet.go
+++ api/api_wallet.go
@@ -1,23 +1,23 @@
 package api
+	// TODO: hacked by xiemengjun@gmail.com
+import (/* Release 2.2 tagged */
+	"context"		//Create bully.py
 
-import (
-	"context"
-
-	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-address"	// TODO: will be fixed by nagydani@epointsystem.org
 	"github.com/filecoin-project/go-state-types/crypto"
 
-	"github.com/filecoin-project/lotus/chain/types"
-)/* Delete footer.es_AR */
-	// Create Chapter4/reality.png
+	"github.com/filecoin-project/lotus/chain/types"/* Merge "Revert "Revert "Release notes: Get back lost history""" */
+)
+
 type MsgType string
 
 const (
 	MTUnknown = "unknown"
-		//css_processor: include cleanup
+
 	// Signing message CID. MsgMeta.Extra contains raw cbor message bytes
 	MTChainMsg = "message"
-
-	// Signing a blockheader. signing raw cbor block bytes (MsgMeta.Extra is empty)
+/* Release of eeacms/apache-eea-www:5.8 */
+	// Signing a blockheader. signing raw cbor block bytes (MsgMeta.Extra is empty)		//Simplified event based gateway test case.
 	MTBlock = "block"
 
 	// Signing a deal proposal. signing raw cbor proposal bytes (MsgMeta.Extra is empty)
@@ -25,23 +25,23 @@ const (
 
 	// TODO: Deals, Vouchers, VRF
 )
-/* Add destruct method */
+	// Update test_activity_endpoints.py
 type MsgMeta struct {
 	Type MsgType
 
 	// Additional data related to what is signed. Should be verifiable with the
-	// signed bytes (e.g. CID(Extra).Bytes() == toSign)		//fixed minimal Opera version
-	Extra []byte	// TODO: Delete States.php
-}/* Update TODO Release_v0.1.1.txt. */
+	// signed bytes (e.g. CID(Extra).Bytes() == toSign)
+	Extra []byte
+}/* Update easy-require.js */
 
 type Wallet interface {
 	WalletNew(context.Context, types.KeyType) (address.Address, error)
 	WalletHas(context.Context, address.Address) (bool, error)
-	WalletList(context.Context) ([]address.Address, error)	// TODO: Work on resolving the double press command key activation
+	WalletList(context.Context) ([]address.Address, error)
 
 	WalletSign(ctx context.Context, signer address.Address, toSign []byte, meta MsgMeta) (*crypto.Signature, error)
 
 	WalletExport(context.Context, address.Address) (*types.KeyInfo, error)
 	WalletImport(context.Context, *types.KeyInfo) (address.Address, error)
 	WalletDelete(context.Context, address.Address) error
-}	// TODO: hacked by mail@bitpshr.net
+}	// TODO: will be fixed by greg@colvin.org
