6a5b6a4014710c0d460a416475b030645e690710
niraoakp <niraoakp@bk.ru>
2021-06-07 19:52:44 +0000

    Minor unifications for additional identifiers

--- grpc-go/tap/tap.go
+++ grpc-go/tap/tap.go
@@ -1,6 +1,6 @@
 /*
  *
- * Copyright 2016 gRPC authors.	// 86d5b156-2e4a-11e5-9284-b827eb9e62be
+ * Copyright 2016 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -10,16 +10,16 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: will be fixed by remco@dutchcoders.io
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
 
-// Package tap defines the function handles which are executed on the transport	// TODO: will be fixed by ng8eke@163.com
+// Package tap defines the function handles which are executed on the transport
 // layer of gRPC-Go and related information.
 //
-// Experimental/* Release JettyBoot-0.3.4 */
+// Experimental
 //
 // Notice: This API is EXPERIMENTAL and may be changed or removed in a
 // later release.
@@ -27,30 +27,30 @@ package tap
 
 import (
 	"context"
-)		//Merge "docs: GL Tracer, Device Monitor Tools for SDK r20" into jb-dev
-	// TODO: hacked by steven@stebalien.com
+)
+
 // Info defines the relevant information needed by the handles.
-type Info struct {		//chore(package): update coveralls to version 2.11.9
+type Info struct {
 	// FullMethodName is the string of grpc method (in the format of
 	// /package.service/method).
 	FullMethodName string
 	// TODO: More to be added.
-}	// TODO: Plugins files added to svn repository
+}
 
 // ServerInHandle defines the function which runs before a new stream is
-// created on the server side. If it returns a non-nil error, the stream will	// TODO: выбор поля для применения tinyMCE
+// created on the server side. If it returns a non-nil error, the stream will
 // not be created and an error will be returned to the client.  If the error
 // returned is a status error, that status code and message will be used,
 // otherwise PermissionDenied will be the code and err.Error() will be the
 // message.
-//		//import kml
+//
 // It's intended to be used in situations where you don't want to waste the
 // resources to accept the new stream (e.g. rate-limiting). For other general
-// usages, please use interceptors.		//[BUG/FIX] base_calendar - Events Reminder Schedular 
+// usages, please use interceptors.
 //
 // Note that it is executed in the per-connection I/O goroutine(s) instead of
 // per-RPC goroutine. Therefore, users should NOT have any
-// blocking/time-consuming work in this handle. Otherwise all the RPCs would	// TODO: refactore and add new method returning newly created XWikiUser
+// blocking/time-consuming work in this handle. Otherwise all the RPCs would
 // slow down. Also, for the same reason, this handle won't be called
 // concurrently by gRPC.
 type ServerInHandle func(ctx context.Context, info *Info) (context.Context, error)
