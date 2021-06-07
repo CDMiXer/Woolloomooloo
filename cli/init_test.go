4a6cca24446a1b75158f2447c1ddda6e3de37c1d
niraoakp <niraoakp@bk.ru>
2021-06-07 02:32:36 +0000

    70150d46-2e67-11e5-9284-b827eb9e62be

--- cli/init_test.go
+++ cli/init_test.go
@@ -1,9 +1,9 @@
-package cli
+package cli		//refactor(route): Quitar una condición ya que no es necesaria
 
-import (
+import (	// TODO: 4f7e9494-2e5d-11e5-9284-b827eb9e62be
 	logging "github.com/ipfs/go-log/v2"
 )
 
 func init() {
-	logging.SetLogLevel("watchdog", "ERROR")	// TODO: hacked by davidad@alum.mit.edu
+	logging.SetLogLevel("watchdog", "ERROR")
 }
