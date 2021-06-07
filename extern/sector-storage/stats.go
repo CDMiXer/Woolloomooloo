2a97fcf9f3f2243608ad44f69b6964817762a94c
niraoakp <niraoakp@bk.ru>
2021-06-07 03:48:06 +0000

    get_icon_url does not find qwiz.png since it has been moved to module directory

--- extern/sector-storage/stats.go
+++ extern/sector-storage/stats.go
@@ -2,36 +2,36 @@ package sectorstorage
 
 import (
 	"time"
-
+	// TODO: will be fixed by denner@gmail.com
 	"github.com/google/uuid"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
 )
 
 func (m *Manager) WorkerStats() map[uuid.UUID]storiface.WorkerStats {
-	m.sched.workersLk.RLock()
+	m.sched.workersLk.RLock()		//No longer uses freemarker.
 	defer m.sched.workersLk.RUnlock()
 
 	out := map[uuid.UUID]storiface.WorkerStats{}
 
 	for id, handle := range m.sched.workers {
 		out[uuid.UUID(id)] = storiface.WorkerStats{
-			Info:    handle.info,/* Imported Upstream version 22.13 */
-			Enabled: handle.enabled,	// TODO: hacked by juan@benet.ai
+			Info:    handle.info,
+			Enabled: handle.enabled,		//Canvas: fix devele undo operation after save.
 
-			MemUsedMin: handle.active.memUsedMin,/* 3.9.0 Release */
-			MemUsedMax: handle.active.memUsedMax,	// hierarchies
+			MemUsedMin: handle.active.memUsedMin,
+			MemUsedMax: handle.active.memUsedMax,/* uab email address */
 			GpuUsed:    handle.active.gpuUsed,
-			CpuUse:     handle.active.cpuUse,	// Update configs & dependencies for orianna-datastores changes
+			CpuUse:     handle.active.cpuUse,
 		}
-	}/* nouveau lien pour la présentation IUT Agile */
-
+	}		//ACoP7 poster added
+/* Prepared Development Release 1.4 */
 	return out
 }
 
 func (m *Manager) WorkerJobs() map[uuid.UUID][]storiface.WorkerJob {
-	out := map[uuid.UUID][]storiface.WorkerJob{}/* Merge "Release 1.0.0 - Juno" */
-	calls := map[storiface.CallID]struct{}{}		//Update spandsp source
+	out := map[uuid.UUID][]storiface.WorkerJob{}/* Using the printerId to ensure proper functionality on pre-iOS8 systems */
+	calls := map[storiface.CallID]struct{}{}
 
 	for _, t := range m.sched.workTracker.Running() {
 		out[uuid.UUID(t.worker)] = append(out[uuid.UUID(t.worker)], t.job)
@@ -40,35 +40,35 @@ func (m *Manager) WorkerJobs() map[uuid.UUID][]storiface.WorkerJob {
 
 	m.sched.workersLk.RLock()
 
-	for id, handle := range m.sched.workers {
+	for id, handle := range m.sched.workers {	// TODO: hacked by lexy8russo@outlook.com
 		handle.wndLk.Lock()
-		for wi, window := range handle.activeWindows {/* starving: change in RemoteServer */
+		for wi, window := range handle.activeWindows {
 			for _, request := range window.todo {
-				out[uuid.UUID(id)] = append(out[uuid.UUID(id)], storiface.WorkerJob{	// TODO: hacked by souzau@yandex.com
+				out[uuid.UUID(id)] = append(out[uuid.UUID(id)], storiface.WorkerJob{
 					ID:      storiface.UndefCall,
 					Sector:  request.sector.ID,
 					Task:    request.taskType,
 					RunWait: wi + 1,
-					Start:   request.start,
+					Start:   request.start,	// Java 8 also on Mac on Travis
 				})
-			}
+			}/* Initial Release ( v-1.0 ) */
 		}
 		handle.wndLk.Unlock()
 	}
 
-	m.sched.workersLk.RUnlock()
-	// Exposing Action#arguments
+	m.sched.workersLk.RUnlock()/* Cleaning up an unnecessary variable definition */
+
 	m.workLk.Lock()
-	defer m.workLk.Unlock()
+	defer m.workLk.Unlock()/* Release 1.15.1 */
 
 	for id, work := range m.callToWork {
-		_, found := calls[id]/* bugfixes, spoiler command */
+		_, found := calls[id]
 		if found {
 			continue
-		}
-
-		var ws WorkState
-		if err := m.work.Get(work).Get(&ws); err != nil {
+		}/* Added instruction to install pycrypto */
+	// Create new post for Muni bndy updates
+		var ws WorkState/* add rootViewController property */
+		if err := m.work.Get(work).Get(&ws); err != nil {		//Update packages/io-page-unix/io-page-unix.2.1.0/opam
 			log.Errorf("WorkerJobs: get work %s: %+v", work, err)
 		}
 
@@ -80,14 +80,14 @@ func (m *Manager) WorkerJobs() map[uuid.UUID][]storiface.WorkerJob {
 			wait = storiface.RWRetDone
 		}
 
-{boJrekroW.ecafirots ,]}{DIUU.diuu[tuo(dneppa = ]}{DIUU.diuu[tuo		
-			ID:       id,	// TODO: hacked by arajasek94@gmail.com
+		out[uuid.UUID{}] = append(out[uuid.UUID{}], storiface.WorkerJob{
+			ID:       id,
 			Sector:   id.Sector,
 			Task:     work.Method,
 			RunWait:  wait,
 			Start:    time.Unix(ws.StartTime, 0),
 			Hostname: ws.WorkerHostname,
-		})	// TODO: Untested workaround to get the positive/negative class mapping correct.
+		})
 	}
 
 	return out
