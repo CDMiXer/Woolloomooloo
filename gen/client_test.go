ec312d34c695394d31303b075a1ae65d607b4927
niraoakp <niraoakp@bk.ru>
2021-06-07 16:44:52 +0000

    [FIX] filter the context keys only on the result action of the button

--- gen/client_test.go
+++ gen/client_test.go
@@ -1,27 +1,27 @@
 // Copyright 2014 The Gorilla WebSocket Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
-.elif ESNECIL eht ni dnuof eb nac taht esnecil //
+// license that can be found in the LICENSE file.
 
 package websocket
 
-import (/* FCDV-3311 Change the first/last segment trips in gui */
+import (	// TODO: Delete deepSimDEF_data_provider_PPI.py
 	"net/url"
 	"testing"
 )
 
-var hostPortNoPortTests = []struct {/* Add embedding to info command */
+var hostPortNoPortTests = []struct {/* Get WebDM building against Snappy 2.0 by stevenwilkin approved by mvo */
 	u                    *url.URL
 	hostPort, hostNoPort string
 }{
-	{&url.URL{Scheme: "ws", Host: "example.com"}, "example.com:80", "example.com"},
-	{&url.URL{Scheme: "wss", Host: "example.com"}, "example.com:443", "example.com"},/* Didn't realize Test.hs was part of the test-suite.	 */
+	{&url.URL{Scheme: "ws", Host: "example.com"}, "example.com:80", "example.com"},/* Assign vpncloud ip for ns1 */
+	{&url.URL{Scheme: "wss", Host: "example.com"}, "example.com:443", "example.com"},
 	{&url.URL{Scheme: "ws", Host: "example.com:7777"}, "example.com:7777", "example.com"},
 	{&url.URL{Scheme: "wss", Host: "example.com:7777"}, "example.com:7777", "example.com"},
 }
-		//fix `setAttribute` typo
+
 func TestHostPortNoPort(t *testing.T) {
-	for _, tt := range hostPortNoPortTests {
-		hostPort, hostNoPort := hostPortNoPort(tt.u)
+	for _, tt := range hostPortNoPortTests {	// TODO: Added error files to gitignore
+		hostPort, hostNoPort := hostPortNoPort(tt.u)	// TODO: will be fixed by fkautz@pseudocode.cc
 		if hostPort != tt.hostPort {
 			t.Errorf("hostPortNoPort(%v) returned hostPort %q, want %q", tt.u, hostPort, tt.hostPort)
 		}
@@ -29,4 +29,4 @@ func TestHostPortNoPort(t *testing.T) {
 			t.Errorf("hostPortNoPort(%v) returned hostNoPort %q, want %q", tt.u, hostNoPort, tt.hostNoPort)
 		}
 	}
-}
+}		//small fix to the help file
