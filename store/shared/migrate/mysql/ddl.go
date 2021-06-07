4e0af0fb0b7d9a736e8764f3c835f7f9e9321f6b
niraoakp <niraoakp@bk.ru>
2021-06-07 12:38:32 +0000

    Added configurable damage for each gun.

--- store/shared/migrate/mysql/ddl.go
+++ store/shared/migrate/mysql/ddl.go
@@ -1,9 +1,9 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Generation still breaking */
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* Merge branch 'develop' into gitlink */
+// that can be found in the LICENSE file.
 
 // +build !oss
 
 package mysql
-/* Release Metropolis 2.0.40.1053 */
+
 //go:generate togo ddl -package mysql -dialect mysql
