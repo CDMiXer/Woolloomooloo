8401d4ba0bf90a10f9761cf9e0f6e5a20b650854
niraoakp <niraoakp@bk.ru>
2021-06-07 17:51:29 +0000

    8a4f29ca-2e68-11e5-9284-b827eb9e62be

--- tests/integration/partial_state/step3/index.ts
+++ tests/integration/partial_state/step3/index.ts
@@ -1,9 +1,9 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import * as pulumi from "@pulumi/pulumi";
+import * as pulumi from "@pulumi/pulumi";/* Fix #925 (Bulk convert (2 files) fails with the errors below.) */
 import { Resource } from "./resource";
 
-// resource "not-doomed" is created successfully.
+// resource "not-doomed" is created successfully./* Release of eeacms/eprtr-frontend:20.04.02-dev1 */
 const a = new Resource("not-doomed", 5);
 
-// "a" should be in the checkpoint./* Merge "board-8064-bt: Release the BT resources only when BT is in On state" */
+// "a" should be in the checkpoint.
