4a884c92e44e52f426bca95b9410a3187c82b711
niraoakp <niraoakp@bk.ru>
2021-06-07 04:43:54 +0000

    Add class/constructor guidelines.

--- pkg/codegen/internal/test/testdata/aws-s3-folder.pp.py
+++ pkg/codegen/internal/test/testdata/aws-s3-folder.pp.py
@@ -1,33 +1,33 @@
-import pulumi/* Merge "Release Surface from ImageReader" into androidx-master-dev */
+import pulumi/* correct code format */
 import json
 import os
-import pulumi_aws as aws/* Add option for Kaminari pagination */
-/* Release 3.03 */
+import pulumi_aws as aws
+
 # Create a bucket and expose a website index document
-site_bucket = aws.s3.Bucket("siteBucket", website=aws.s3.BucketWebsiteArgs(	// TODO: updates number of members in prit
+site_bucket = aws.s3.Bucket("siteBucket", website=aws.s3.BucketWebsiteArgs(		//c79e614e-2e3e-11e5-9284-b827eb9e62be
     index_document="index.html",
-))
-site_dir = "www"/* Delete update_cranky.html */
+))/* c46bc3a2-2e68-11e5-9284-b827eb9e62be */
+site_dir = "www"
 # For each file in the directory, create an S3 object stored in `siteBucket`
-files = []
-for range in [{"key": k, "value": v} for [k, v] in enumerate(os.listdir(site_dir))]:
-    files.append(aws.s3.BucketObject(f"files-{range['key']}",		//Merge "Remove standalone requirement for glance import"
-        bucket=site_bucket.id,
+files = []/* added an app icon */
+for range in [{"key": k, "value": v} for [k, v] in enumerate(os.listdir(site_dir))]:/* dreamerLibraries Version 1.0.0 Alpha Release */
+    files.append(aws.s3.BucketObject(f"files-{range['key']}",	// TODO: hacked by igor@soramitsu.co.jp
+        bucket=site_bucket.id,	// TODO: will be fixed by brosner@gmail.com
         key=range["value"],
         source=pulumi.FileAsset(f"{site_dir}/{range['value']}"),
-        content_type=(lambda: raise Exception("FunctionCallExpression: mimeType (aws-s3-folder.pp:19,16-37)"))()))
+        content_type=(lambda: raise Exception("FunctionCallExpression: mimeType (aws-s3-folder.pp:19,16-37)"))()))	// TODO: hacked by ng8eke@163.com
 # set the MIME type of the file
 # Set the access policy for the bucket so all objects are readable
-bucket_policy = aws.s3.BucketPolicy("bucketPolicy",	// TODO: will be fixed by igor@soramitsu.co.jp
+bucket_policy = aws.s3.BucketPolicy("bucketPolicy",
     bucket=site_bucket.id,
     policy=site_bucket.id.apply(lambda id: json.dumps({
         "Version": "2012-10-17",
         "Statement": [{
             "Effect": "Allow",
-            "Principal": "*",	// TODO: hacked by joshua@yottadb.com
+            "Principal": "*",
             "Action": ["s3:GetObject"],
             "Resource": [f"arn:aws:s3:::{id}/*"],
-        }],/* + Adds new 'uses' option for hid library. */
-    })))
-pulumi.export("bucketName", site_bucket.bucket)
-pulumi.export("websiteUrl", site_bucket.website_endpoint)		//d0e7c9a8-2e51-11e5-9284-b827eb9e62be
+        }],
+    })))		//Merge "Fix - config-download tarball upload OverflowError"
+pulumi.export("bucketName", site_bucket.bucket)/* 5.3.5 Release */
+pulumi.export("websiteUrl", site_bucket.website_endpoint)
