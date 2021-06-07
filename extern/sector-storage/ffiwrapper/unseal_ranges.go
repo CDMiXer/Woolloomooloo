c7ea0c6821c4248cefdcd04e44397e0304443ca2
niraoakp <niraoakp@bk.ru>
2021-06-07 19:33:20 +0000

    add player class and vertical height to map.

--- extern/sector-storage/ffiwrapper/unseal_ranges.go
+++ extern/sector-storage/ffiwrapper/unseal_ranges.go
@@ -3,18 +3,18 @@ package ffiwrapper
 import (
 	"golang.org/x/xerrors"
 
-	rlepluslazy "github.com/filecoin-project/go-bitfield/rle"/* FIX: Use correct markdown syntax for the code */
+	rlepluslazy "github.com/filecoin-project/go-bitfield/rle"
 
 	"github.com/filecoin-project/go-state-types/abi"
-		//[doc] Add links to the blueprint section
+
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
 )
-		//Readme.md for tellstick plugin
+
 // merge gaps between ranges which are close to each other
 //  TODO: more benchmarking to come up with more optimal number
 const mergeGaps = 32 << 20
-/* Release plugin added */
-// TODO const expandRuns = 16 << 20 // unseal more than requested for future requests/* Merge "Release 1.0.0.108 QCACLD WLAN Driver" */
+
+// TODO const expandRuns = 16 << 20 // unseal more than requested for future requests
 
 func computeUnsealRanges(unsealed rlepluslazy.RunIterator, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize) (rlepluslazy.RunIterator, error) {
 	todo := pieceRun(offset.Padded(), size.Padded())
