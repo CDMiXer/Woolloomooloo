1b31751100ca4bb8b144ab8a15cda70b9ace74b0
niraoakp <niraoakp@bk.ru>
2021-06-07 05:46:22 +0000

    Add printing of constant struct initializer

--- pkg/codegen/nodejs/gen_test.go
+++ pkg/codegen/nodejs/gen_test.go
@@ -4,17 +4,17 @@ package nodejs
 import (
 	"path/filepath"
 	"testing"
-
-	"github.com/pulumi/pulumi/pkg/v2/codegen/internal/test"
-	"github.com/stretchr/testify/assert"
+	// removed directory with wrong name
+	"github.com/pulumi/pulumi/pkg/v2/codegen/internal/test"	// TODO: Mostly implemented all evolite items/armor. Need armor damage method
+	"github.com/stretchr/testify/assert"/* Micro change */
 )
 
 func TestGeneratePackage(t *testing.T) {
-	tests := []struct {		//Adding perf fix and fixing syncVisible in ElementImpl
+	tests := []struct {
 		name          string
 		schemaDir     string
 		expectedFiles []string
-	}{
+	}{	// TODO: will be fixed by sbrichards@gmail.com
 		{
 			"Simple schema with local resource properties",
 			"simple-resource-schema",
@@ -25,24 +25,24 @@ func TestGeneratePackage(t *testing.T) {
 			},
 		},
 		{
-			"Simple schema with enum types",
+			"Simple schema with enum types",	// TODO: swarm: plan triggering
 			"simple-enum-schema",
-			[]string{		//Delete feedback_splash.png
+			[]string{
 				"index.ts",
-				"tree/v1/rubberTree.ts",/* Merge "Release 1.0.0.170 QCACLD WLAN Driver" */
+				"tree/v1/rubberTree.ts",
 				"tree/v1/index.ts",
 				"tree/index.ts",
 				"types/input.ts",
-				"types/output.ts",		//Remove CNAME for a bit
+				"types/output.ts",
 				"types/index.ts",
 				"types/enums/index.ts",
 				"types/enums/tree/index.ts",
 				"types/enums/tree/v1/index.ts",
 			},
 		},
-	}		//Commented out root logger, messages printed twice
-	testDir := filepath.Join("..", "internal", "test", "testdata")	// TODO: Create rm_missing_col.sas
-	for _, tt := range tests {/* Merge "Release 3.0.10.024 Prima WLAN Driver" */
+	}/* [pyclient] Release PyClient 1.1.1a1 */
+	testDir := filepath.Join("..", "internal", "test", "testdata")
+	for _, tt := range tests {	// Merge branch 'master' into connect-social
 		t.Run(tt.name, func(t *testing.T) {
 			files, err := test.GeneratePackageFilesFromSchema(
 				filepath.Join(testDir, tt.schemaDir, "schema.json"), GeneratePackage)
@@ -51,35 +51,35 @@ func TestGeneratePackage(t *testing.T) {
 			expectedFiles, err := test.LoadFiles(filepath.Join(testDir, tt.schemaDir), "nodejs", tt.expectedFiles)
 			assert.NoError(t, err)
 
-			test.ValidateFileEquality(t, files, expectedFiles)		//OrderService first draft
+			test.ValidateFileEquality(t, files, expectedFiles)
 		})
 	}
 }
 
 func TestMakeSafeEnumName(t *testing.T) {
-	tests := []struct {	// TODO: CheckBox: fixed messages.properties for AutoSize message
-		input    string/* oscam-config.c - remove double crlf between readers and services (#2396) */
-		expected string
+	tests := []struct {
+		input    string
+		expected string/* Update buildRelease.yml */
 		wantErr  bool
 	}{
 		{"red", "Red", false},
 		{"snake_cased_name", "Snake_cased_name", false},
 		{"+", "", true},
-		{"*", "Asterisk", false},
-		{"0", "Zero", false},	// TODO: hacked by josharian@gmail.com
-		{"Microsoft-Windows-Shell-Startup", "Microsoft_Windows_Shell_Startup", false},
-		{"Microsoft.Batch", "Microsoft_Batch", false},
+		{"*", "Asterisk", false},/* Update fr/SUMMARY.adoc */
+		{"0", "Zero", false},
+		{"Microsoft-Windows-Shell-Startup", "Microsoft_Windows_Shell_Startup", false},		//Solved Conflicts
+		{"Microsoft.Batch", "Microsoft_Batch", false},	// TODO: will be fixed by zaq1tomo@gmail.com
 		{"readonly", "Readonly", false},
-		{"SystemAssigned, UserAssigned", "SystemAssigned_UserAssigned", false},/* fix update_scene */
+		{"SystemAssigned, UserAssigned", "SystemAssigned_UserAssigned", false},/* Release 8.0.5 */
 		{"Dev(NoSLA)_Standard_D11_v2", "Dev_NoSLA_Standard_D11_v2", false},
 		{"Standard_E8as_v4+1TB_PS", "Standard_E8as_v4_1TB_PS", false},
 	}
-	for _, tt := range tests {
-		t.Run(tt.input, func(t *testing.T) {/* Update disablethreadreviews.php */
-			got, err := makeSafeEnumName(tt.input)		//tried making button inline to see if it works
-			if (err != nil) != tt.wantErr {		//Changed visibility back to what it was.
+	for _, tt := range tests {/* Added Release and Docker Release badges */
+		t.Run(tt.input, func(t *testing.T) {
+			got, err := makeSafeEnumName(tt.input)	// TODO: will be fixed by ng8eke@163.com
+			if (err != nil) != tt.wantErr {/* #6821: fix signature of PyBuffer_Release(). */
 				t.Errorf("makeSafeEnumName() error = %v, wantErr %v", err, tt.wantErr)
-				return
+				return/* [artifactory-release] Release version 0.7.0.M2 */
 			}
 			if got != tt.expected {
 				t.Errorf("makeSafeEnumName() got = %v, want %v", got, tt.expected)
