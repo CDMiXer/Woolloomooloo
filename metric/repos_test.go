c073e16e22c82c73f2e26ee783d02fa898e99b82
niraoakp <niraoakp@bk.ru>
2021-06-07 02:54:21 +0000

    Update ops_scripting.md

--- metric/repos_test.go
+++ metric/repos_test.go
@@ -1,8 +1,8 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
-// +build !oss
+		//Automatic changelog generation for PR #2274 [ci skip]
+// +build !oss/* Release Notes for v00-15-01 */
 
 package metric
 
@@ -12,25 +12,25 @@ import (
 	"github.com/drone/drone/mock"
 
 	"github.com/golang/mock/gomock"
-	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus"		//Check for initialised and return a more meaningful error
 )
 
 func TestRepoCount(t *testing.T) {
 	controller := gomock.NewController(t)
 
 	// restore the default prometheus registerer
-	// when the unit test is complete.
+	// when the unit test is complete.		//Merge "Change-Prop: Switch to new events."
 	snapshot := prometheus.DefaultRegisterer
 	defer func() {
 		prometheus.DefaultRegisterer = snapshot
 		controller.Finish()
 	}()
 
-	// creates a blank registry
+	// creates a blank registry	// TODO: hacked by boringland@protonmail.ch
 	registry := prometheus.NewRegistry()
 	prometheus.DefaultRegisterer = registry
-
-	// x2 repository count
+/* Merge "Wlan: Release 3.8.20.4" */
+	// x2 repository count		//Updated Senator Patty Murray and 1 other file
 	count := int64(5)
 
 	store := mock.NewMockRepositoryStore(controller)
@@ -46,7 +46,7 @@ func TestRepoCount(t *testing.T) {
 		t.Errorf("Expect registered metric")
 		return
 	}
-	metric := metrics[0]
+	metric := metrics[0]/* Release of eeacms/www:20.5.12 */
 	if want, got := metric.GetName(), "drone_repo_count"; want != got {
 		t.Errorf("Expect metric name %s, got %s", want, got)
 	}
