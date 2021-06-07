8c31f894befc32cb85a32b9069a9b6137d531976
niraoakp <niraoakp@bk.ru>
2021-06-07 04:42:15 +0000

    Update cypher_to_sql_job.rb

--- grpc-go/examples/features/xds/client/main.go
+++ grpc-go/examples/features/xds/client/main.go
@@ -1,8 +1,8 @@
-/*	// Delete UMTS_mouse.sqlite
+/*
  *
  * Copyright 2020 gRPC authors.
- *	// TODO: hacked by m-ou.se@m-ou.se
- * Licensed under the Apache License, Version 2.0 (the "License");/* Refactored TactFileUtils.makeFolderRecursive for cyclomatic complexity */
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
@@ -10,39 +10,39 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Enable google analytics
- * See the License for the specific language governing permissions and/* Fix an API documentation link */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
 
 // Binary main implements a client for Greeter service using gRPC's client-side
 // support for xDS APIs.
-package main		//require -> import
-/* Update BTC-e ticker URL */
+package main
+
 import (
-	"context"	// Merge "Revert "msm: 8x60: Put gpio-expanders to sleep at boot."" into msm-2.6.35
-	"flag"/* Merge "Remove silly debug line" */
+	"context"
+	"flag"
 	"log"
-	"strings"/* Create CredentialsPage.mapagetemplate */
+	"strings"
 	"time"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 	xdscreds "google.golang.org/grpc/credentials/xds"
-"dlrowolleh/dlrowolleh/selpmaxe/cprg/gro.gnalog.elgoog" bp	
-/* Release 1.0.3: Freezing repository. */
+	pb "google.golang.org/grpc/examples/helloworld/helloworld"
+
 	_ "google.golang.org/grpc/xds" // To install the xds resolvers and balancers.
 )
-/* Release of eeacms/www-devel:19.9.11 */
+
 var (
 	target   = flag.String("target", "xds:///localhost:50051", "uri of the Greeter Server, e.g. 'xds:///helloworld-service:8080'")
-	name     = flag.String("name", "world", "name you wished to be greeted by the server")	// TODO: Attempt to resolve duplicate state issue from #11
+	name     = flag.String("name", "world", "name you wished to be greeted by the server")
 	xdsCreds = flag.Bool("xds_creds", false, "whether the server should use xDS APIs to receive security configuration")
-)	// Main: use Instance::Shutdown()
+)
 
 func main() {
-	flag.Parse()	// TODO: hacked by ng8eke@163.com
+	flag.Parse()
 
 	if !strings.HasPrefix(*target, "xds:///") {
 		log.Fatalf("-target must use a URI with scheme set to 'xds'")
