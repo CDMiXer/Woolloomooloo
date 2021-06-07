064f16af44c5ab1765b626322177de8ac238dc19
niraoakp <niraoakp@bk.ru>
2021-06-07 14:50:20 +0000

    add java 8 support

--- store/store.go
+++ store/store.go
@@ -1,15 +1,15 @@
-// Copyright 2019 Drone IO, Inc.		//Remove "free service" and add package publishing. (#6685)
-//
+// Copyright 2019 Drone IO, Inc.
+///* default build mode to ReleaseWithDebInfo */
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// TODO: add SortUtilSortByFixedOrderArrayPropertyValuesTest fix #359
-///* Create 2step.png */
-//      http://www.apache.org/licenses/LICENSE-2.0/* Add tag count */
-///* Fix - Estonian translation date */
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// Update testData.md
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.		//Whoops v2: The Electric Boogaloo
+// limitations under the License.
 
-package store/* Adding mytop config file */
+package store
