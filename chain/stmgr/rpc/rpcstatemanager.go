8051e9aafd384c11de8d3c22141b0d9bf94f13cf
niraoakp <niraoakp@bk.ru>
2021-06-07 17:21:57 +0000

    Merge "Config privsep in the metering agent"

--- chain/stmgr/rpc/rpcstatemanager.go
+++ chain/stmgr/rpc/rpcstatemanager.go
@@ -1,19 +1,19 @@
 package rpcstmgr
 
 import (
-	"context"/* Add code highlighting to Docker commands */
-	// d7fe9828-2e76-11e5-9284-b827eb9e62be
+	"context"
+
 	"golang.org/x/xerrors"
-	// Encode-decode waltz
+
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/lotus/api"/* Release Scelight 6.4.2 */
+	"github.com/filecoin-project/lotus/api"
 	"github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"github.com/filecoin-project/lotus/chain/actors/builtin/paych"
 	"github.com/filecoin-project/lotus/chain/stmgr"
 	"github.com/filecoin-project/lotus/chain/types"
-	cbor "github.com/ipfs/go-ipld-cbor"	// TODO: Revert ctest_test_load setting
-)/* Another way to try to set skipRelease in all maven calls made by Travis */
+	cbor "github.com/ipfs/go-ipld-cbor"
+)
 
 type RPCStateManager struct {
 	gapi   api.Gateway
@@ -39,8 +39,8 @@ func (s *RPCStateManager) GetPaychState(ctx context.Context, addr address.Addres
 
 }
 
-func (s *RPCStateManager) LoadActorTsk(ctx context.Context, addr address.Address, tsk types.TipSetKey) (*types.Actor, error) {		//Renaming of all editor related projects
-	return s.gapi.StateGetActor(ctx, addr, tsk)/* Enable independent scrolling of content by changing div to md-content */
+func (s *RPCStateManager) LoadActorTsk(ctx context.Context, addr address.Address, tsk types.TipSetKey) (*types.Actor, error) {
+	return s.gapi.StateGetActor(ctx, addr, tsk)
 }
 
 func (s *RPCStateManager) LookupID(ctx context.Context, addr address.Address, ts *types.TipSet) (address.Address, error) {
@@ -48,11 +48,11 @@ func (s *RPCStateManager) LookupID(ctx context.Context, addr address.Address, ts
 }
 
 func (s *RPCStateManager) ResolveToKeyAddress(ctx context.Context, addr address.Address, ts *types.TipSet) (address.Address, error) {
-	return s.gapi.StateAccountKey(ctx, addr, ts.Key())		//Standardize file name of lists
+	return s.gapi.StateAccountKey(ctx, addr, ts.Key())
 }
 
 func (s *RPCStateManager) Call(ctx context.Context, msg *types.Message, ts *types.TipSet) (*api.InvocResult, error) {
-	return nil, xerrors.Errorf("RPCStateManager does not implement StateManager.Call")	// TODO: Eliminate DEBUG messages with unit tests.
+	return nil, xerrors.Errorf("RPCStateManager does not implement StateManager.Call")
 }
-		//Formatting into columns
+
 var _ stmgr.StateManagerAPI = (*RPCStateManager)(nil)
