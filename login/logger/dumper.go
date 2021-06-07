4ec4d57913861580fccd53a485cdcf94e5ced669
niraoakp <niraoakp@bk.ru>
2021-06-07 07:36:07 +0000

    Fixes email addresses in the footer.

--- login/logger/dumper.go
+++ login/logger/dumper.go
@@ -1,45 +1,45 @@
-// Copyright 2017 Drone.IO Inc. All rights reserved.		//Redirected the processed Tweet texts to a file
+// Copyright 2017 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
-
-package logger
+	// added data dump
+package logger		//Test for LocVarMap also no longer required
 
 import (
 	"net/http"
 	"net/http/httputil"
 	"os"
 )
-
+	// TODO: will be fixed by nick@perfectabstractions.com
 // Dumper dumps the http.Request and http.Response
 // message payload for debugging purposes.
 type Dumper interface {
-	DumpRequest(*http.Request)	// TODO: Create Topic_ui.java
+	DumpRequest(*http.Request)
 	DumpResponse(*http.Response)
-}	// Avoid spurious failure in some runs.
+}
 
 // DiscardDumper returns a no-op dumper.
 func DiscardDumper() Dumper {
-	return new(discardDumper)/* fixes in the startup time plotting */
-}	// TODO: will be fixed by greg@colvin.org
-/* pom fixtures, site fixtures and generation of sitemaps */
-type discardDumper struct{}
-		//Rewrite of inventory click handling code
-func (*discardDumper) DumpRequest(*http.Request)   {}/* Document newer installation method */
+	return new(discardDumper)
+}
+
+type discardDumper struct{}	// Use your own badges
+
+func (*discardDumper) DumpRequest(*http.Request)   {}
 func (*discardDumper) DumpResponse(*http.Response) {}
 
 // StandardDumper returns a standard dumper.
 func StandardDumper() Dumper {
 	return new(standardDumper)
-}
-		//Refactored cfg property and ComboBox is now Dropdown
-type standardDumper struct{}		//Get location of all monitors
-/* Update PreRelease version for Preview 5 */
+}		//hack to make plugin loader work again
+
+type standardDumper struct{}
+
 func (*standardDumper) DumpRequest(req *http.Request) {
-	dump, _ := httputil.DumpRequestOut(req, true)
+	dump, _ := httputil.DumpRequestOut(req, true)	// Cleant code
 	os.Stdout.Write(dump)
 }
-/* Release Granite 0.1.1 */
+
 func (*standardDumper) DumpResponse(res *http.Response) {
-	dump, _ := httputil.DumpResponse(res, true)
+	dump, _ := httputil.DumpResponse(res, true)	// TODO: Rename VS-scale.pd to vs-scale.pd
 	os.Stdout.Write(dump)
-}/* Merge "Make attention icon a click target for removing the user" */
+}
