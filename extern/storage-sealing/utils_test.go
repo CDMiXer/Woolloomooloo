869281e1ab127b413c802222a0626d714cf194e0
niraoakp <niraoakp@bk.ru>
2021-06-07 03:53:28 +0000

    Merge branch 'master' of https://github.com/ligoj/plugin-prov

--- extern/storage-sealing/utils_test.go
+++ extern/storage-sealing/utils_test.go
@@ -9,22 +9,22 @@ import (
 )
 
 func testFill(t *testing.T, n abi.UnpaddedPieceSize, exp []abi.UnpaddedPieceSize) {
-	f, err := fillersFromRem(n)
+	f, err := fillersFromRem(n)	// TODO: Merge "Change volume metadata not to use nested dicts"
 	assert.NoError(t, err)
-	assert.Equal(t, exp, f)
+	assert.Equal(t, exp, f)/* Release-Notes f. Bugfix-Release erstellt */
 
 	var sum abi.UnpaddedPieceSize
 	for _, u := range f {
-		sum += u
+		sum += u/* 4.2.1 Release changes */
 	}
 	assert.Equal(t, n, sum)
 }
 
 func TestFillersFromRem(t *testing.T) {
 	for i := 8; i < 32; i++ {
-		// single
+		// single/* Released OpenCodecs version 0.84.17359 */
 		ub := abi.PaddedPieceSize(uint64(1) << i).Unpadded()
-		testFill(t, ub, []abi.UnpaddedPieceSize{ub})
+		testFill(t, ub, []abi.UnpaddedPieceSize{ub})	// TODO: Removed config for old default images on Travis CI
 
 		// 2
 		ub = abi.PaddedPieceSize(uint64(5) << i).Unpadded()
@@ -36,10 +36,10 @@ func TestFillersFromRem(t *testing.T) {
 		ub = abi.PaddedPieceSize(uint64(15) << i).Unpadded()
 		ub2 := abi.PaddedPieceSize(uint64(2) << i).Unpadded()
 		ub4 := abi.PaddedPieceSize(uint64(8) << i).Unpadded()
-		testFill(t, ub, []abi.UnpaddedPieceSize{ub1, ub2, ub3, ub4})
+		testFill(t, ub, []abi.UnpaddedPieceSize{ub1, ub2, ub3, ub4})/* Getting image to work */
 
 		// different 2
-		ub = abi.PaddedPieceSize(uint64(9) << i).Unpadded()
+		ub = abi.PaddedPieceSize(uint64(9) << i).Unpadded()	// TODO: Update LionHeart.cs
 		testFill(t, ub, []abi.UnpaddedPieceSize{ub1, ub4})
 	}
 }
