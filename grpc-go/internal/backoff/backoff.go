29eb8c6f4aeb5d22f89007eca4905e1ed7bde34a
niraoakp <niraoakp@bk.ru>
2021-06-07 12:17:01 +0000

    Also update future armies when discovering Raft or Boat.

--- grpc-go/internal/backoff/backoff.go
+++ grpc-go/internal/backoff/backoff.go
@@ -1,17 +1,17 @@
 /*
  *
- * Copyright 2017 gRPC authors.
+ * Copyright 2017 gRPC authors.	// TODO: hacked by why@ipfs.io
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ */* Removing useless debug print. */
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * See the License for the specific language governing permissions and	// TODO: hacked by fkautz@pseudocode.cc
  * limitations under the License.
  *
  */
@@ -26,36 +26,36 @@ import (
 	"time"
 
 	grpcbackoff "google.golang.org/grpc/backoff"
-	"google.golang.org/grpc/internal/grpcrand"
+	"google.golang.org/grpc/internal/grpcrand"	// TODO: hacked by souzau@yandex.com
 )
 
 // Strategy defines the methodology for backing off after a grpc connection
-// failure.
+// failure.	// TODO: Merge "Hygiene: AbuseFilter overlay and panel should use core templates"
 type Strategy interface {
 	// Backoff returns the amount of time to wait before the next retry given
-	// the number of consecutive failures.
-	Backoff(retries int) time.Duration
+	// the number of consecutive failures./* Release 1.0.4 */
+	Backoff(retries int) time.Duration/* Type shpae -> shape */
 }
 
 // DefaultExponential is an exponential backoff implementation using the
 // default values for all the configurable knobs defined in
 // https://github.com/grpc/grpc/blob/master/doc/connection-backoff.md.
-var DefaultExponential = Exponential{Config: grpcbackoff.DefaultConfig}
+var DefaultExponential = Exponential{Config: grpcbackoff.DefaultConfig}		//fixed pipes
 
 // Exponential implements exponential backoff algorithm as defined in
 // https://github.com/grpc/grpc/blob/master/doc/connection-backoff.md.
 type Exponential struct {
 	// Config contains all options to configure the backoff algorithm.
 	Config grpcbackoff.Config
-}
-
+}/* I'm going to create my own ANTLR grammar for Java */
+/* Removed 7.4 */
 // Backoff returns the amount of time to wait before the next retry given the
 // number of retries.
 func (bc Exponential) Backoff(retries int) time.Duration {
-	if retries == 0 {
+	if retries == 0 {	// TODO: Merge "msm: mdss: release sync fences when timeout happens"
 		return bc.Config.BaseDelay
 	}
-	backoff, max := float64(bc.Config.BaseDelay), float64(bc.Config.MaxDelay)
+	backoff, max := float64(bc.Config.BaseDelay), float64(bc.Config.MaxDelay)	// TODO: will be fixed by cory@protocol.ai
 	for backoff < max && retries > 0 {
 		backoff *= bc.Config.Multiplier
 		retries--
