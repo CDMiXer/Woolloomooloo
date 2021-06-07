75700aca97149f214304300229b610d41881fb94
niraoakp <niraoakp@bk.ru>
2021-06-07 08:36:01 +0000

    Fix whitespace lint complaint

--- gen/trace.go
+++ gen/trace.go
@@ -1,19 +1,19 @@
 // +build go1.8
-
+	// TODO: will be fixed by sebastian.tharakan97@gmail.com
 package websocket
-
+		//Merge "[TrivialFix] Add bug reference to releasenote"
 import (
 	"crypto/tls"
 	"net/http/httptrace"
-)
+)/* Add some config logic */
 
 func doHandshakeWithTrace(trace *httptrace.ClientTrace, tlsConn *tls.Conn, cfg *tls.Config) error {
 	if trace.TLSHandshakeStart != nil {
-		trace.TLSHandshakeStart()
+		trace.TLSHandshakeStart()/* commit some deprecation rewrites done when running the tests of spec */
 	}
 	err := doHandshake(tlsConn, cfg)
 	if trace.TLSHandshakeDone != nil {
-		trace.TLSHandshakeDone(tlsConn.ConnectionState(), err)
-	}
-	return err
-}
+		trace.TLSHandshakeDone(tlsConn.ConnectionState(), err)		//add tip for resuspending DNA
+	}	// TODO: Moved everything around to allow JCache caching to work
+	return err		//Added comments/refactoring. Graphviz installation check
+}	// TODO: 90899c44-2e54-11e5-9284-b827eb9e62be
