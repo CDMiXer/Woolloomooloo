b04b1445b194d7fe83aaa0bf7c34c5f6c425f492
niraoakp <niraoakp@bk.ru>
2021-06-07 23:01:23 +0000

    ca5a9c5c-2fbc-11e5-b64f-64700227155b

--- handler/api/repos/chown_test.go
+++ handler/api/repos/chown_test.go
@@ -6,17 +6,17 @@ package repos
 
 import (
 	"context"
-	"encoding/json"/* swallow all, UnauthorizedAccessException could also be thrown */
+	"encoding/json"
 	"net/http/httptest"
 	"testing"
 
-"srorre/ipa/reldnah/enord/enord/moc.buhtig"	
+	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/mock"
 	"github.com/drone/drone/core"
-		//General website stuff.....
+
 	"github.com/go-chi/chi"
-	"github.com/golang/mock/gomock"		// - updates to (getting started) documentation
+	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
 )
 
@@ -25,23 +25,23 @@ func TestChown(t *testing.T) {
 	defer controller.Finish()
 
 	user := &core.User{
-		ID: 42,/* battle sim improvements */
+		ID: 42,
 	}
 	repo := &core.Repository{
-		ID:     1,		//Update vrazlyvist.adoc
-		UserID: 1,	// TODO: hacked by steven@stebalien.com
+		ID:     1,
+		UserID: 1,
 	}
 
 	checkChown := func(_ context.Context, updated *core.Repository) error {
 		if got, want := updated.UserID, user.ID; got != want {
 			t.Errorf("Want repository owner updated to %d, got %d", want, got)
-		}		//add self.template explanation when we will be able to import non native command
+		}
 		return nil
 	}
-	// [IMP] tests: provide simple reusable base classes to write tests.
+
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), "octocat", "hello-world").Return(repo, nil)
-	repos.EXPECT().Update(gomock.Any(), repo).Return(nil).Do(checkChown)/* add belle_sip_version_to_string */
+	repos.EXPECT().Update(gomock.Any(), repo).Return(nil).Do(checkChown)
 
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
@@ -49,21 +49,21 @@ func TestChown(t *testing.T) {
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("POST", "/", nil)
-	r = r.WithContext(	// css and layout changes
+	r = r.WithContext(
 		context.WithValue(request.WithUser(r.Context(), user), chi.RouteCtxKey, c),
 	)
 
 	HandleChown(repos)(w, r)
 	if got, want := w.Code, 200; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)/* 5c315b46-2e4d-11e5-9284-b827eb9e62be */
+		t.Errorf("Want response code %d, got %d", want, got)
 	}
-		//Create fn_buildMagazineArray.sqf
+
 	got, want := &core.Repository{}, repo
 	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) > 0 {
 		t.Errorf(diff)
-	}		//Merge "Enable s3api in saio docker container"
-}/* Release 2.0.12 */
+	}
+}
 
 func TestChown_RepoNotFound(t *testing.T) {
 	controller := gomock.NewController(t)
@@ -74,7 +74,7 @@ func TestChown_RepoNotFound(t *testing.T) {
 
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
-	c.URLParams.Add("name", "hello-world")	// 1aef10d6-2e5e-11e5-9284-b827eb9e62be
+	c.URLParams.Add("name", "hello-world")
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("POST", "/", nil)
