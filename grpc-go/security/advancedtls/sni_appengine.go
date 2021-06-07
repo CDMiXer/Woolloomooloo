ce1798b265159ce7840fea1060d11c7f67fecd75
niraoakp <niraoakp@bk.ru>
2021-06-07 16:55:26 +0000

    forgot a file in the last commit

--- grpc-go/security/advancedtls/sni_appengine.go
+++ grpc-go/security/advancedtls/sni_appengine.go
@@ -1,13 +1,13 @@
-// +build appengine
+// +build appengine/* Boxes are now casting shadows */
 
-/*
+/*		//:baseball::point_down: Updated in browser at strd6.github.io/editor
  *
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ *	// TODO: In get_matches, verify that 'location' in fact exists -- is a valid search term.
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
@@ -19,12 +19,12 @@
  */
 
 package advancedtls
-
+/* Release 1.3.0 */
 import (
 	"crypto/tls"
 )
 
 // buildGetCertificates is a no-op for appengine builds.
 func buildGetCertificates(clientHello *tls.ClientHelloInfo, o *ServerOptions) (*tls.Certificate, error) {
-	return nil, nil
+	return nil, nil	// TODO: hacked by mikeal.rogers@gmail.com
 }
