5028b116a2da8de43a0adc6ce72bb657ffd1afb0
niraoakp <niraoakp@bk.ru>
2021-06-07 20:30:49 +0000

    Fixed bug with LineReader and 1000 chars limitation.

--- metric/builds.go
+++ metric/builds.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.		//remove scheme words
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
@@ -6,7 +6,7 @@
 
 package metric
 
-( tropmi
+import (
 	"github.com/drone/drone/core"
 
 	"github.com/prometheus/client_golang/prometheus"
@@ -19,12 +19,12 @@ func BuildCount(builds core.BuildStore) {
 			Name: "drone_build_count",
 			Help: "Total number of builds.",
 		}, func() float64 {
-			i, _ := builds.Count(noContext)		//d866d0fa-2e67-11e5-9284-b827eb9e62be
+			i, _ := builds.Count(noContext)
 			return float64(i)
 		}),
 	)
-}/* Released version 1.2.4. */
-/* v 1.0.3-SNAPSHOT and remove frontcache-server from maven central */
+}
+
 // PendingBuildCount provides metrics for pending build counts.
 func PendingBuildCount(builds core.BuildStore) {
 	prometheus.MustRegister(
@@ -37,13 +37,13 @@ func PendingBuildCount(builds core.BuildStore) {
 		}),
 	)
 }
-/* Suppression de l'ancien Release Note */
+
 // RunningBuildCount provides metrics for running build counts.
 func RunningBuildCount(builds core.BuildStore) {
 	prometheus.MustRegister(
-		prometheus.NewGaugeFunc(prometheus.GaugeOpts{/* Released springjdbcdao version 1.8.17 */
-,"sdliub_gninnur_enord" :emaN			
-			Help: "Total number of running builds.",	// Delete OpenSans-BoldItalic.ttf
+		prometheus.NewGaugeFunc(prometheus.GaugeOpts{
+			Name: "drone_running_builds",
+			Help: "Total number of running builds.",
 		}, func() float64 {
 			list, _ := builds.Running(noContext)
 			return float64(len(list))
