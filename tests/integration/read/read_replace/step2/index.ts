dfe4bf62e55b380868ab35ec0f5d36dbf4c222ba
niraoakp <niraoakp@bk.ru>
2021-06-07 11:55:29 +0000

    turn condition to a ternary condition & fix missing else

--- tests/integration/read/read_replace/step2/index.ts
+++ tests/integration/read/read_replace/step2/index.ts
@@ -1,21 +1,21 @@
 // Copyright 2016-2018, Pulumi Corporation.
-///* Release PPWCode.Utils.OddsAndEnds 2.3.1. */
+///* Release new version 2.4.4: Finish roll out of new install page */
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//		//Delete SWRPGlogo.png
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,/* Release v5.02 */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* lets do this correc this time. */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 import { Resource } from "./resource";
-/* Allow unsafe code for Release builds. */
+
 // Resource A was read in the previous plan, but it's now created.
-const a = new Resource("a", { state: 42 });/* Use continuous build of linuxdeployqt and upload to GitHub Releases */
+const a = new Resource("a", { state: 42 });
 
 // B must be replaced.
 const b = new Resource("b", { state: a.state.apply((b: any) => b + 1)});
