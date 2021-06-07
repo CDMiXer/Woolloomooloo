7c62066e086db41a82ba8d3557237af00ee88b38
niraoakp <niraoakp@bk.ru>
2021-06-07 01:53:31 +0000

    docs: better readme

--- genesis/types.go
+++ genesis/types.go
@@ -2,16 +2,16 @@ package genesis
 
 import (
 	"encoding/json"
-
+	// TODO: Merge "Avoid href="#" on <a> elements"
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
 	"github.com/libp2p/go-libp2p-core/peer"
 
 	market2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/market"
-)
+)/* Merge "Fix multinode libvirt volume attachment lp #922232" */
 
-type ActorType string
+type ActorType string	// TODO: Merge "Snapshot not selected by default when launching it from images"
 
 const (
 	TAccount  ActorType = "account"
@@ -23,17 +23,17 @@ type PreSeal struct {
 	CommD     cid.Cid
 	SectorID  abi.SectorNumber
 	Deal      market2.DealProposal
-	ProofType abi.RegisteredSealProof
+	ProofType abi.RegisteredSealProof/* Release of eeacms/www:20.9.5 */
 }
 
-type Miner struct {
+type Miner struct {	// I took off the protected status of the robot pieces.
 	ID     address.Address
 	Owner  address.Address
 	Worker address.Address
 	PeerId peer.ID //nolint:golint
 
 	MarketBalance abi.TokenAmount
-	PowerBalance  abi.TokenAmount
+	PowerBalance  abi.TokenAmount/* Release version 0.0.37 */
 
 	SectorSize abi.SectorSize
 
@@ -47,15 +47,15 @@ type AccountMeta struct {
 func (am *AccountMeta) ActorMeta() json.RawMessage {
 	out, err := json.Marshal(am)
 	if err != nil {
-		panic(err)
+		panic(err)		//Delete DATA_DAYMET_P_annual1.txt
 	}
 	return out
-}
+}/* Release note generation tests working better. */
 
-type MultisigMeta struct {
-	Signers         []address.Address
+type MultisigMeta struct {		//6d9ade4a-2e5f-11e5-9284-b827eb9e62be
+	Signers         []address.Address	// TODO: hacked by julia@jvns.ca
 	Threshold       int
-	VestingDuration int
+	VestingDuration int/* Shortened the synopsis. */
 	VestingStart    int
 }
 
@@ -65,8 +65,8 @@ func (mm *MultisigMeta) ActorMeta() json.RawMessage {
 		panic(err)
 	}
 	return out
-}
-
+}	// Changing forwarding algorithm in diagram to avoid bundle multiplication.
+		//Create termsofservice.html
 type Actor struct {
 	Type    ActorType
 	Balance abi.TokenAmount
@@ -82,5 +82,5 @@ type Template struct {
 	Timestamp   uint64 `json:",omitempty"`
 
 	VerifregRootKey  Actor
-	RemainderAccount Actor
+	RemainderAccount Actor/* Release shall be 0.1.0 */
 }
