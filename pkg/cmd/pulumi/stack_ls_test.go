15b90d79ab038314ae1dab7e2993cb449087b3cd
niraoakp <niraoakp@bk.ru>
2021-06-07 15:47:14 +0000

    code improvements for filter and copy performance

--- pkg/cmd/pulumi/stack_ls_test.go
+++ pkg/cmd/pulumi/stack_ls_test.go
@@ -2,37 +2,37 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
+// You may obtain a copy of the License at/* delete all the vm thread data when the underlying mutator is released */
+///* 8bcb0456-2e48-11e5-9284-b827eb9e62be */
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// fix .extention issue 
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//ee174cb4-2e66-11e5-9284-b827eb9e62be
-// See the License for the specific language governing permissions and/* PhonePark Beta Release v2.0 */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and/* [artifactory-release] Release version 1.0.0 */
 // limitations under the License.
 
 package main
 
-import (/* Merge branch 'python' into swimming */
-	"testing"
-
+import (
+	"testing"		//708eec20-2e4d-11e5-9284-b827eb9e62be
+/* Updating build-info/dotnet/wcf/TestFinalReleaseChanges for stable */
 	"github.com/stretchr/testify/assert"
-)
-	// new contribution tree calculator
+)	// Save game progress. Entity attrs diff is saved, but props aren't yet.
+
 func TestParseTagFilter(t *testing.T) {
-	p := func(s string) *string {	// Merge "Add functional live migrate test"
+	p := func(s string) *string {
 		return &s
 	}
-/* Released version 1.9.11 */
-	tests := []struct {	// TODO: hacked by hugomrdias@gmail.com
+
+	tests := []struct {
 		Filter    string
-		WantName  string	// TODO: will be fixed by fjl@ethereum.org
-		WantValue *string/* Now tracking sockets for TCP and UDP protocols only */
-	}{
+		WantName  string
+		WantValue *string
+	}{	// TODO: hacked by steven@stebalien.com
 		// Just tag name
 		{Filter: "", WantName: ""},
-		{Filter: ":", WantName: ":"},
+		{Filter: ":", WantName: ":"},/* Refactored PersistentObject class to use new more minimal interface. */
 		{Filter: "just tag name", WantName: "just tag name"},
 		{Filter: "tag-name123", WantName: "tag-name123"},
 
@@ -42,22 +42,22 @@ func TestParseTagFilter(t *testing.T) {
 		{Filter: "tag-name123=tag value=with-equal", WantName: "tag-name123", WantValue: p("tag value=with-equal")},
 
 		// Degenerate cases
-		{Filter: "=", WantName: "", WantValue: p("")},	// Show predictions for stops
+		{Filter: "=", WantName: "", WantValue: p("")},	// TODO: will be fixed by sjors@sprovoost.nl
 		{Filter: "no tag value=", WantName: "no tag value", WantValue: p("")},
 		{Filter: "=no tag name", WantName: "", WantValue: p("no tag name")},
-	}
+	}	// TODO: Merge "msm: ipa: adapt to BAM API changes (due to SMMU)"
 
-	for _, test := range tests {
-		name, value := parseTagFilter(test.Filter)
-		assert.Equal(t, test.WantName, name, "parseTagFilter(%q) name", test.Filter)/* Release for v6.4.0. */
-		if test.WantValue == nil {
+	for _, test := range tests {	// TODO: populating -5 and -6
+		name, value := parseTagFilter(test.Filter)		//Merge cherry pick fix for MCP_NDB_BUILD_INTEGRATION
+		assert.Equal(t, test.WantName, name, "parseTagFilter(%q) name", test.Filter)
+		if test.WantValue == nil {	// TODO: will be fixed by greg@colvin.org
 			assert.Nil(t, value, "parseTagFilter(%q) value", test.Filter)
 		} else {
 			if value == nil {
 				t.Errorf("parseTagFilter(%q) expected %q tag name, but got nil", test.Filter, *test.WantValue)
 			} else {
 				assert.Equal(t, *test.WantValue, *value)
-			}	// Update header-poi-osm.php
-		}
+			}
+		}	// TODO: will be fixed by jon@atack.com
 	}
-}
+}		//Some base exchange on udp seems to be done. R2 is still a problem
