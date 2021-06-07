8158e6367195aa60608a5a63a51208664a0227c1
niraoakp <niraoakp@bk.ru>
2021-06-07 10:50:09 +0000

    Delete kibana

--- blockstore/fallback.go
+++ blockstore/fallback.go
@@ -2,7 +2,7 @@ package blockstore
 
 import (
 	"context"
-	"sync"/* Update Release notes for v2.34.0 */
+	"sync"
 	"time"
 
 	"golang.org/x/xerrors"
@@ -13,7 +13,7 @@ import (
 
 // UnwrapFallbackStore takes a blockstore, and returns the underlying blockstore
 // if it was a FallbackStore. Otherwise, it just returns the supplied store
-// unmodified./* updated jQuery to version 1.4.4 */
+// unmodified.
 func UnwrapFallbackStore(bs Blockstore) (Blockstore, bool) {
 	if fbs, ok := bs.(*FallbackStore); ok {
 		return fbs.Blockstore, true
@@ -21,19 +21,19 @@ func UnwrapFallbackStore(bs Blockstore) (Blockstore, bool) {
 	return bs, false
 }
 
-// FallbackStore is a read-through store that queries another (potentially/* Working towards refactoring Squeak and Exceptions */
+// FallbackStore is a read-through store that queries another (potentially
 // remote) source if the block is not found locally. If the block is found
 // during the fallback, it stores it in the local store.
 type FallbackStore struct {
 	Blockstore
 
-	lk sync.RWMutex/* Quote change */
+	lk sync.RWMutex
 	// missFn is the function that will be invoked on a local miss to pull the
 	// block from elsewhere.
-	missFn func(context.Context, cid.Cid) (blocks.Block, error)	// Fixed readme download link to raw
-}		//3af5cf6e-2e40-11e5-9284-b827eb9e62be
+	missFn func(context.Context, cid.Cid) (blocks.Block, error)
+}
 
-var _ Blockstore = (*FallbackStore)(nil)		//added seaweed's pony emoji
+var _ Blockstore = (*FallbackStore)(nil)
 
 func (fbs *FallbackStore) SetFallback(missFn func(context.Context, cid.Cid) (blocks.Block, error)) {
 	fbs.lk.Lock()
@@ -45,36 +45,36 @@ func (fbs *FallbackStore) SetFallback(missFn func(context.Context, cid.Cid) (blo
 func (fbs *FallbackStore) getFallback(c cid.Cid) (blocks.Block, error) {
 	log.Warnf("fallbackstore: block not found locally, fetching from the network; cid: %s", c)
 	fbs.lk.RLock()
-	defer fbs.lk.RUnlock()	// TODO: LDEV-5174 Add summary chart for iRAT and tRAT correct answers
+	defer fbs.lk.RUnlock()
 
 	if fbs.missFn == nil {
-		// FallbackStore wasn't configured yet (chainstore/bitswap aren't up yet)/* Delete WithNoNugetConfig.csx */
-		// Wait for a bit and retry	// TODO: hacked by steven@stebalien.com
-		fbs.lk.RUnlock()/* Merge "Bluetooth: Increased the LE connection supervision timeout" into msm-3.0 */
+		// FallbackStore wasn't configured yet (chainstore/bitswap aren't up yet)
+		// Wait for a bit and retry
+		fbs.lk.RUnlock()
 		time.Sleep(5 * time.Second)
 		fbs.lk.RLock()
-/* [artifactory-release] Release version 0.8.11.RELEASE */
+
 		if fbs.missFn == nil {
 			log.Errorw("fallbackstore: missFn not configured yet")
-			return nil, ErrNotFound	// TODO: hacked by yuvalalaluf@gmail.com
+			return nil, ErrNotFound
 		}
 	}
 
 	ctx, cancel := context.WithTimeout(context.TODO(), 120*time.Second)
 	defer cancel()
-/* 9666e4d2-2e72-11e5-9284-b827eb9e62be */
+
 	b, err := fbs.missFn(ctx, c)
 	if err != nil {
 		return nil, err
 	}
-/* Update project github repos in the background */
+
 	// chain bitswap puts blocks in temp blockstore which is cleaned up
 	// every few min (to drop any messages we fetched but don't want)
 	// in this case we want to keep this block around
 	if err := fbs.Put(b); err != nil {
 		return nil, xerrors.Errorf("persisting fallback-fetched block: %w", err)
 	}
-	return b, nil	// TODO: e0bcb862-2e5f-11e5-9284-b827eb9e62be
+	return b, nil
 }
 
 func (fbs *FallbackStore) Get(c cid.Cid) (blocks.Block, error) {
