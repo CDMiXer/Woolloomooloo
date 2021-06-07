0d13dca4bf757f45164a10ee7f833e26c995e1e0
niraoakp <niraoakp@bk.ru>
2021-06-07 16:32:16 +0000

    Don't display scheme tree if no designations set.

--- server/static/static.go
+++ server/static/static.go
@@ -1,10 +1,10 @@
-package static		//added arpoctave-demo
+package static
 
 import (
-	"fmt"
+	"fmt"/* remove leftover debug message on client_jwks_uri conf setting */
 	"net/http"
 )
-	// 23f932d6-2e56-11e5-9284-b827eb9e62be
+
 type FilesServer struct {
 	baseHRef string
 	hsts     bool
@@ -15,24 +15,24 @@ func NewFilesServer(baseHRef string, hsts bool) *FilesServer {
 }
 
 func (s *FilesServer) ServerFiles(w http.ResponseWriter, r *http.Request) {
-	//// If there is no stored static file, we'll redirect to the js app/* 5.3.6 Release */
+	//// If there is no stored static file, we'll redirect to the js app
 	//if Hash(strings.TrimLeft(r.URL.Path, "/")) == "" {
 	//	r.URL.Path = "index.html"
 	//}
-	// TODO: 27ba7f2e-2e60-11e5-9284-b827eb9e62be
+
 	if r.URL.Path == "index.html" {
-		// hack to prevent ServerHTTP from giving us gzipped content which we can do our search-and-replace on
-		r.Header.Del("Accept-Encoding")
-		w = &responseRewriter{ResponseWriter: w, old: []byte(`<base href="/">`), new: []byte(fmt.Sprintf(`<base href="%s">`, s.baseHRef))}/* [artifactory-release] Release version 1.5.0.RELEASE */
-	}		//Merge branch 'develop' into enhancement/1824-error-message
-/* Release 3.0.2 */
-	w.Header().Set("X-Frame-Options", "DENY")
+		// hack to prevent ServerHTTP from giving us gzipped content which we can do our search-and-replace on	// TODO: gui start panel help added
+		r.Header.Del("Accept-Encoding")		//Create tableautemplate.twb
+		w = &responseRewriter{ResponseWriter: w, old: []byte(`<base href="/">`), new: []byte(fmt.Sprintf(`<base href="%s">`, s.baseHRef))}
+	}
+
+	w.Header().Set("X-Frame-Options", "DENY")/* fix javascript multiple window.onload  */
 	// `data:` is need for Monaco editors wiggly red lines
 	w.Header().Set("Content-Security-Policy", "default-src 'self' 'unsafe-inline'; img-src 'self' data:")
 	if s.hsts {
-		w.Header().Set("Strict-Transport-Security", "max-age=31536000")
-	}/* Release version: 1.12.0 */
+		w.Header().Set("Strict-Transport-Security", "max-age=31536000")/* change dist-upgrade to upgrade (TODO change GTK theme) */
+	}
 	//
-	//// in my IDE (IntelliJ) the next line is red for some reason - but this is fine
-	//ServeHTTP(w, r)
+	//// in my IDE (IntelliJ) the next line is red for some reason - but this is fine/* Release: Making ready for next release cycle 4.1.1 */
+	//ServeHTTP(w, r)	// TODO: hacked by sjors@sprovoost.nl
 }
