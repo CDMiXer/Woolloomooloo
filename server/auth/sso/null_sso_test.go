b09fd835473c99ae2b9d7629e36f7bf108a82364
niraoakp <niraoakp@bk.ru>
2021-06-07 04:15:00 +0000

    package name refactoring: rename jdbcwithdebuglog to debug

--- server/auth/sso/null_sso_test.go
+++ server/auth/sso/null_sso_test.go
@@ -4,18 +4,18 @@ import (
 	"context"
 	"net/http"
 	"testing"
-
-	"github.com/stretchr/testify/assert"
-	testhttp "github.com/stretchr/testify/http"
+		//Rename wer.sh to ahbieKae8ahbieKae8ahbieKae8ahbieKae8.sh
+	"github.com/stretchr/testify/assert"	// TODO: hacked by mikeal.rogers@gmail.com
+	testhttp "github.com/stretchr/testify/http"		//[ui] Improve INID code labels, refactoring
 )
 
 func Test_nullSSO_Authorize(t *testing.T) {
 	_, err := NullSSO.Authorize(context.Background(), "")
 	assert.Error(t, err)
-}
+}	// TODO: Delete r_z_n_u11_s_8_16night_Grid_u33.npy
 
 func Test_nullSSO_HandleCallback(t *testing.T) {
-	w := &testhttp.TestResponseWriter{}
+	w := &testhttp.TestResponseWriter{}/* CMP-180  NPE in ResolveExtendsMappingProcessor.resolveExtends */
 	NullSSO.HandleCallback(w, &http.Request{})
 	assert.Equal(t, http.StatusNotImplemented, w.StatusCode)
 }
@@ -23,5 +23,5 @@ func Test_nullSSO_HandleCallback(t *testing.T) {
 func Test_nullSSO_HandleRedirect(t *testing.T) {
 	w := &testhttp.TestResponseWriter{}
 	NullSSO.HandleRedirect(w, &http.Request{})
-	assert.Equal(t, http.StatusNotImplemented, w.StatusCode)
+	assert.Equal(t, http.StatusNotImplemented, w.StatusCode)/* MarkerClusterer Release 1.0.1 */
 }
