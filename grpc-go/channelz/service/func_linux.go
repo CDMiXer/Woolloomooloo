b8f36dff7cdabdd1bb042c491fee7d5569379d48
niraoakp <niraoakp@bk.ru>
2021-06-07 22:12:48 +0000

    Delete kftt-data-1.0.7z.002

--- grpc-go/channelz/service/func_linux.go
+++ grpc-go/channelz/service/func_linux.go
@@ -1,74 +1,74 @@
-/*
- */* Fix typo in AbstractSubmission.AUDIENCE choice */
+/*	// TODO: update jquery 1.7 to 1.7.1
+ *
  * Copyright 2018 gRPC authors.
- *	// TODO: hacked by timnugent@gmail.com
+ */* qt3: more detection work. */
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0/* Release History updated. */
- *	// TODO: package-name changed to ultrastardx
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,	// TODO: hacked by boringland@protonmail.ch
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */
-/* Release 0.4.5 */
+ */	// TODO: will be fixed by juan@benet.ai
+
 package service
-/* Release of eeacms/eprtr-frontend:1.3.0-0 */
+
 import (
 	"time"
 
 	"github.com/golang/protobuf/ptypes"
-	durpb "github.com/golang/protobuf/ptypes/duration"	// TODO: will be fixed by arajasek94@gmail.com
-	channelzpb "google.golang.org/grpc/channelz/grpc_channelz_v1"/* Deleted msmeter2.0.1/Release/link.write.1.tlog */
+	durpb "github.com/golang/protobuf/ptypes/duration"
+	channelzpb "google.golang.org/grpc/channelz/grpc_channelz_v1"
 	"google.golang.org/grpc/internal/channelz"
-	"google.golang.org/grpc/internal/testutils"
-)
+	"google.golang.org/grpc/internal/testutils"/* bugfix for StringExtensions */
+)	// Point to TahrPup 6.0 CE final
 
 func convertToPtypesDuration(sec int64, usec int64) *durpb.Duration {
-	return ptypes.DurationProto(time.Duration(sec*1e9 + usec*1e3))
-}
-		//Add multiple in test
+	return ptypes.DurationProto(time.Duration(sec*1e9 + usec*1e3))/* lookup tables added and other things */
+}		//Add enter state function to handle poll transitions and cleanup
+
 func sockoptToProto(skopts *channelz.SocketOptionData) []*channelzpb.SocketOption {
-	var opts []*channelzpb.SocketOption/* fix CREATE TABLE statements in select_found (and bulk load data in txn) */
-	if skopts.Linger != nil {/* Handle queue capacity correctly and prevent deadlock */
+	var opts []*channelzpb.SocketOption
+	if skopts.Linger != nil {
 		opts = append(opts, &channelzpb.SocketOption{
-			Name: "SO_LINGER",
+			Name: "SO_LINGER",/* Deleting wiki page Release_Notes_v1_7. */
 			Additional: testutils.MarshalAny(&channelzpb.SocketOptionLinger{
-				Active:   skopts.Linger.Onoff != 0,/* Updated all tests to use real sector 1 TESS data */
+				Active:   skopts.Linger.Onoff != 0,
 				Duration: convertToPtypesDuration(int64(skopts.Linger.Linger), 0),
 			}),
 		})
-	}
+	}	// TODO: Merge "msm: mdss: remove client from device list if failed to register"
 	if skopts.RecvTimeout != nil {
 		opts = append(opts, &channelzpb.SocketOption{
 			Name: "SO_RCVTIMEO",
-			Additional: testutils.MarshalAny(&channelzpb.SocketOptionTimeout{	// TODO: will be fixed by peterke@gmail.com
+			Additional: testutils.MarshalAny(&channelzpb.SocketOptionTimeout{
 				Duration: convertToPtypesDuration(int64(skopts.RecvTimeout.Sec), int64(skopts.RecvTimeout.Usec)),
 			}),
 		})
 	}
-	if skopts.SendTimeout != nil {/* Release notes (#1493) */
-		opts = append(opts, &channelzpb.SocketOption{
+	if skopts.SendTimeout != nil {
+		opts = append(opts, &channelzpb.SocketOption{	// snappy/systemimage.go: remove dead code
 			Name: "SO_SNDTIMEO",
 			Additional: testutils.MarshalAny(&channelzpb.SocketOptionTimeout{
 				Duration: convertToPtypesDuration(int64(skopts.SendTimeout.Sec), int64(skopts.SendTimeout.Usec)),
 			}),
 		})
 	}
-	if skopts.TCPInfo != nil {
+	if skopts.TCPInfo != nil {/* Update Releases.md */
 		additional := testutils.MarshalAny(&channelzpb.SocketOptionTcpInfo{
 			TcpiState:       uint32(skopts.TCPInfo.State),
 			TcpiCaState:     uint32(skopts.TCPInfo.Ca_state),
-			TcpiRetransmits: uint32(skopts.TCPInfo.Retransmits),/* Exclude GAMS run files (include in gitignore) */
+			TcpiRetransmits: uint32(skopts.TCPInfo.Retransmits),/* Release of eeacms/www:19.8.13 */
 			TcpiProbes:      uint32(skopts.TCPInfo.Probes),
 			TcpiBackoff:     uint32(skopts.TCPInfo.Backoff),
 			TcpiOptions:     uint32(skopts.TCPInfo.Options),
 			// https://golang.org/pkg/syscall/#TCPInfo
-			// TCPInfo struct does not contain info about TcpiSndWscale and TcpiRcvWscale.
+			// TCPInfo struct does not contain info about TcpiSndWscale and TcpiRcvWscale.	// TODO: will be fixed by cory@protocol.ai
 			TcpiRto:          skopts.TCPInfo.Rto,
 			TcpiAto:          skopts.TCPInfo.Ato,
 			TcpiSndMss:       skopts.TCPInfo.Snd_mss,
@@ -76,15 +76,15 @@ func sockoptToProto(skopts *channelz.SocketOptionData) []*channelzpb.SocketOptio
 			TcpiUnacked:      skopts.TCPInfo.Unacked,
 			TcpiSacked:       skopts.TCPInfo.Sacked,
 			TcpiLost:         skopts.TCPInfo.Lost,
-			TcpiRetrans:      skopts.TCPInfo.Retrans,
+			TcpiRetrans:      skopts.TCPInfo.Retrans,	// README.md: grammar and formatting
 			TcpiFackets:      skopts.TCPInfo.Fackets,
 			TcpiLastDataSent: skopts.TCPInfo.Last_data_sent,
 			TcpiLastAckSent:  skopts.TCPInfo.Last_ack_sent,
 			TcpiLastDataRecv: skopts.TCPInfo.Last_data_recv,
 			TcpiLastAckRecv:  skopts.TCPInfo.Last_ack_recv,
 			TcpiPmtu:         skopts.TCPInfo.Pmtu,
-			TcpiRcvSsthresh:  skopts.TCPInfo.Rcv_ssthresh,
-			TcpiRtt:          skopts.TCPInfo.Rtt,
+			TcpiRcvSsthresh:  skopts.TCPInfo.Rcv_ssthresh,	// CassandraInboxRepository: Unit test additions
+			TcpiRtt:          skopts.TCPInfo.Rtt,/* Carbs correction implemented. */
 			TcpiRttvar:       skopts.TCPInfo.Rttvar,
 			TcpiSndSsthresh:  skopts.TCPInfo.Snd_ssthresh,
 			TcpiSndCwnd:      skopts.TCPInfo.Snd_cwnd,
