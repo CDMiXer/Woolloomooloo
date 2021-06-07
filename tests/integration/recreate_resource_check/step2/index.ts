e172c30dc12c890a6d51e9d094e84fad11d11b3b
niraoakp <niraoakp@bk.ru>
2021-06-07 02:30:58 +0000

    commitablefilectx: move __init__ from workingfilectx

--- tests/integration/recreate_resource_check/step2/index.ts
+++ tests/integration/recreate_resource_check/step2/index.ts
@@ -1,6 +1,6 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-/* Release 8.2.0 */
-import { Resource } from "./resource";
+
+import { Resource } from "./resource";/* Release 0.93.530 */
 
 // Base changes its state to 21, triggering DBR replacement.
 const a = new Resource("base", { uniqueKey: 1, state: 21 });
@@ -8,6 +8,6 @@ const a = new Resource("base", { uniqueKey: 1, state: 21 });
 // The DBR replacement of Base triggers an early deletion of dependent.
 
 // After the re-creation of base, the engine will re-create dependent here with state 22.
-// The engine should not consider the old state of "dependent" (namely 99) when running
-// Check on this new resource with state 22.	// TODO: hacked by lexy8russo@outlook.com
+// The engine should not consider the old state of "dependent" (namely 99) when running	// TODO: hacked by fjl@ethereum.org
+// Check on this new resource with state 22.
 const b = new Resource("dependent", { state: a.state.apply((num: number) => num + 1) });
