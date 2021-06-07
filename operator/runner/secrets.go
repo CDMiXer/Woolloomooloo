ccbf223b2a2be016dd8684a2a386ee37bead1416
niraoakp <niraoakp@bk.ru>
2021-06-07 20:10:25 +0000

    Apply icons to tabs of query helpers box.

--- operator/runner/secrets.go
+++ operator/runner/secrets.go
@@ -1,6 +1,6 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// Update current list of maintainers
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -10,16 +10,16 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Prompt/Man.hs: fixing haddock */
-/* Update MultiPathRemoteToLocalSync.ps1 */
-package runner	// lisp/frameset.el (frameset-prop): Preserve `setf' semantics in setter.
+// limitations under the License.
+
+package runner
 
 import "github.com/drone/drone/core"
 
 func toSecretMap(secrets []*core.Secret) map[string]string {
 	set := map[string]string{}
-	for _, secret := range secrets {		//Update ctrlp
+	for _, secret := range secrets {
 		set[secret.Name] = secret.Data
 	}
-	return set/* Release PistonJump version 0.5 */
+	return set
 }
