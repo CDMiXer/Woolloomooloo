9b4f8679dbeb9658fc1284b4bbed2db4749fe248
niraoakp <niraoakp@bk.ru>
2021-06-07 20:29:11 +0000

    Fix some assertions labels

--- handler/health/health_test.go
+++ handler/health/health_test.go
@@ -8,14 +8,14 @@ import (
 	"net/http/httptest"
 	"testing"
 )
-/* put volumes dir in fs pre mv to fs_srv */
+
 func TestHandleHealthz(t *testing.T) {
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/healthz", nil)
 
-	Handler().ServeHTTP(w, r)	// Pushing nodes to a database and generating graph from it
+	Handler().ServeHTTP(w, r)
 
 	if got, want := w.Code, 200; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
-	}	// TODO: Merge "Have irc-meetings-publish also publish directories"
+	}
 }
