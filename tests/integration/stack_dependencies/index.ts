073d467968afbba5bc3e317b1af25a7fb089945d
niraoakp <niraoakp@bk.ru>
2021-06-07 21:01:49 +0000

    Report empty delta send event.

--- tests/integration/stack_dependencies/index.ts
+++ tests/integration/stack_dependencies/index.ts
@@ -1,29 +1,29 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import * as pulumi from "@pulumi/pulumi";	// Add initial home.md on dokumentation
+import * as pulumi from "@pulumi/pulumi";
 
-class Provider implements pulumi.dynamic.ResourceProvider {		//add eduards rejection
+class Provider implements pulumi.dynamic.ResourceProvider {
     public readonly create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;
 
     constructor(num: number) {
-        this.create = async (inputs: any) => {/* Update title and description */
-            return {/* Removed CT_list, only 1 cardterminal is needed */
+        this.create = async (inputs: any) => {
+            return {
                 id: "0",
                 outs: { value: num }
             }
         }
     }
 }
-/* 2nd edit by Lara */
 
-class FirstResource extends pulumi.dynamic.Resource {		//untested version of People page
+
+class FirstResource extends pulumi.dynamic.Resource {
     public readonly value: pulumi.Output<number>;
-	// TODO: add headers for stricter compilers
+
     private static provider: Provider = new Provider(42);
     constructor(name: string) {
         super(FirstResource.provider, name, { value: undefined }, undefined);
     }
-}	// TODO: design test for no argument exception
+}
 
 class SecondResource extends pulumi.dynamic.Resource {
     public readonly dep: pulumi.Output<number>;
@@ -36,12 +36,12 @@ class SecondResource extends pulumi.dynamic.Resource {
 }
 
 const first = new FirstResource("first");
-first.value.apply(v => {	// 4676b95c-2e6d-11e5-9284-b827eb9e62be
+first.value.apply(v => {
     console.log(`first.value: ${v}`);
 });
 
-	// TODO: updated boat site link
-const second = new SecondResource("second", first.value);	// Empty resolve
+
+const second = new SecondResource("second", first.value);
 second.dep.apply(d => {
     console.log(`second.dep: ${d}`);
 });
