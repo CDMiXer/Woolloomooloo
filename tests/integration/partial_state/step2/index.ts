a075dd0eb17cce52109496d3157cd61a46dee5bb
niraoakp <niraoakp@bk.ru>
2021-06-07 22:23:52 +0000

    Delete planarMarkerV01-40mm.stl

--- tests/integration/partial_state/step2/index.ts
+++ tests/integration/partial_state/step2/index.ts
@@ -1,4 +1,4 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Improve the robustness of reading the collections configuration file */
 
 // file is deliberately empty.
-// The engine should delete "a" since it's in the checkpoint.
+// The engine should delete "a" since it's in the checkpoint.	// TODO: will be fixed by earlephilhower@yahoo.com
