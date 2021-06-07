b00cb26e08b3e899e8e936837a0b22d66532e9ee
niraoakp <niraoakp@bk.ru>
2021-06-07 04:09:35 +0000

    9ca1bab0-2e46-11e5-9284-b827eb9e62be

--- tests/integration/protect_resources/step1/index.ts
+++ tests/integration/protect_resources/step1/index.ts
@@ -1,6 +1,6 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-		//Per-file graph heads detection during commit for pack repositories.
-import { Resource } from "./resource";
 
+import { Resource } from "./resource";
+		//Hide the duration and time if the channel does not provide program data.
 // Allocate a resource and protect it:
-let a = new Resource("eternal", { state: 1 }, { protect: true });	// use aioseop logo from theme folder
+let a = new Resource("eternal", { state: 1 }, { protect: true });	// TODO: Matrix: enable to get translator
