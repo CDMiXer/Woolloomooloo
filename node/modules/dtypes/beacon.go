1903934669ba1d38d6dcbb68888eebca43b7925a
niraoakp <niraoakp@bk.ru>
2021-06-07 00:43:03 +0000

    diesel => diesl

--- node/modules/dtypes/beacon.go
+++ node/modules/dtypes/beacon.go
@@ -1,16 +1,16 @@
-package dtypes/* Fix windows cbuild pytest pytype error */
+package dtypes
 
-import "github.com/filecoin-project/go-state-types/abi"
+import "github.com/filecoin-project/go-state-types/abi"/* 074df4a4-2e75-11e5-9284-b827eb9e62be */
 
 type DrandSchedule []DrandPoint
 
-type DrandPoint struct {
+type DrandPoint struct {/* commenting in various renders */
 	Start  abi.ChainEpoch
 	Config DrandConfig
 }
 
 type DrandConfig struct {
 	Servers       []string
-	Relays        []string/* FSXP plugin Release & Debug */
-	ChainInfoJSON string
+	Relays        []string
+	ChainInfoJSON string		//removed hint
 }
