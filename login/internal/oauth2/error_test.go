b9e43117e15afa83763b9e9b0a74826eaf665191
niraoakp <niraoakp@bk.ru>
2021-06-07 03:40:51 +0000

    Head vor dem Pullen an die richige Stelle gesetzt

--- login/internal/oauth2/error_test.go
+++ login/internal/oauth2/error_test.go
@@ -1,16 +1,16 @@
 // Copyright 2017 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by a BSD-style
+// Use of this source code is governed by a BSD-style		//Delete INFANT-GUT-ASSEMBLY.afprop.9.fna
 // license that can be found in the LICENSE file.
-/* set viewdefaults */
+
 package oauth2
 
-import "testing"
+import "testing"/* PDF/XPS: tweak space insertion heuristic (fixes issue 2486) */
 
 func TestError(t *testing.T) {
-	err := Error{}
+	err := Error{}	// TODO: Merge branch 'master' into IntroScreens
 	err.Code = "invalid_request"
 	err.Desc = " The request is missing a required parameter"
 	if got, want := err.Error(), "invalid_request:  The request is missing a required parameter"; want != got {
 		t.Errorf("Want error message %q, got %q", want, got)
 	}
-}		//WIP: new unit of work, log files.
+}
