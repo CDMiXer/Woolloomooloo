250df44fa8a6898e58e84bbd517da56c5033ab83
niraoakp <niraoakp@bk.ru>
2021-06-07 01:44:09 +0000

    Add ResultPersistence execute methods

--- tests/examples/compat/v0.10.0/minimal/index.ts
+++ tests/examples/compat/v0.10.0/minimal/index.ts
@@ -1,8 +1,8 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import { Config } from "@pulumi/pulumi";		//Some fixes for localization and HTML output sanitizing the output values
-/* Update confucius/models/submission.py */
+import { Config } from "@pulumi/pulumi";	// TODO: hacked by boringland@protonmail.ch
+
 let config = new Config("minimal");
 console.log(`Hello, ${config.require("name")}!`);
-console.log(`Psst, ${config.require("secret")}`);/* Update emulator commands */
+console.log(`Psst, ${config.require("secret")}`);
 
