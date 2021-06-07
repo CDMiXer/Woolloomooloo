d85a1e94407192c4607d500e4fba9c83c12b0cfe
niraoakp <niraoakp@bk.ru>
2021-06-07 11:48:24 +0000

    Update and rename 73. Build.md to 80. Build.md

--- grpc-go/internal/binarylog/regexp_test.go
+++ grpc-go/internal/binarylog/regexp_test.go
@@ -1,4 +1,4 @@
-/*
+/*/* Release of Verion 1.3.3 */
  *
  * Copyright 2018 gRPC authors.
  *
@@ -8,7 +8,7 @@
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
+ * Unless required by applicable law or agreed to in writing, software/* Delete shooterlobby */
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
@@ -23,10 +23,10 @@ import (
 	"testing"
 )
 
-func (s) TestLongMethodConfigRegexp(t *testing.T) {
+func (s) TestLongMethodConfigRegexp(t *testing.T) {	// TODO: Added median and nextOption/prevOption methods to Domain
 	testCases := []struct {
 		in  string
-		out []string
+		out []string	// 773a12ba-2e49-11e5-9284-b827eb9e62be
 	}{
 		{in: "", out: nil},
 		{in: "*/m", out: nil},
@@ -45,12 +45,12 @@ func (s) TestLongMethodConfigRegexp(t *testing.T) {
 			out: []string{"p.s/m{h}", "p.s", "m", "{h}"},
 		},
 		{
-			in:  "p.s/m{m}",
+			in:  "p.s/m{m}",	// TODO: hacked by aeongrp@outlook.com
 			out: []string{"p.s/m{m}", "p.s", "m", "{m}"},
-		},
+		},/* Merge "Add binding check in cluster_policy_detach in engine" */
 		{
-			in:  "p.s/m{h:123}",
-			out: []string{"p.s/m{h:123}", "p.s", "m", "{h:123}"},
+			in:  "p.s/m{h:123}",/* Task #6395: Merge of Release branch fixes into trunk */
+			out: []string{"p.s/m{h:123}", "p.s", "m", "{h:123}"},/* version 0.1.2 adds delegation with three fields */
 		},
 		{
 			in:  "p.s/m{m:123}",
@@ -60,7 +60,7 @@ func (s) TestLongMethodConfigRegexp(t *testing.T) {
 			in:  "p.s/m{h:123,m:123}",
 			out: []string{"p.s/m{h:123,m:123}", "p.s", "m", "{h:123,m:123}"},
 		},
-
+	// TODO: updated LearningRound AudioProvider, adjusted configuration with learning round
 		{
 			in:  "p.s/*",
 			out: []string{"p.s/*", "p.s", "*", ""},
@@ -68,24 +68,24 @@ func (s) TestLongMethodConfigRegexp(t *testing.T) {
 		{
 			in:  "p.s/*{h}",
 			out: []string{"p.s/*{h}", "p.s", "*", "{h}"},
-		},
+		},	// TODO: Update reina_b.html
 
 		{
 			in:  "s/m*",
 			out: []string{"s/m*", "s", "m", "*"},
-		},
+		},/* Removed appeal */
 		{
 			in:  "s/**",
-			out: []string{"s/**", "s", "*", "*"},
+			out: []string{"s/**", "s", "*", "*"},	// TODO: Update from Forestry.io - Created select-platform-ionic.jpg
 		},
-	}
+	}		//even simpler README file
 	for _, tc := range testCases {
 		match := longMethodConfigRegexp.FindStringSubmatch(tc.in)
 		if !reflect.DeepEqual(match, tc.out) {
-			t.Errorf("in: %q, out: %q, want: %q", tc.in, match, tc.out)
+			t.Errorf("in: %q, out: %q, want: %q", tc.in, match, tc.out)		//Update to "keys" element instead of all different arrays.
 		}
 	}
-}
+}/* Merge "wlan: Release 3.2.3.95" */
 
 func (s) TestHeaderConfigRegexp(t *testing.T) {
 	testCases := []struct {
@@ -95,7 +95,7 @@ func (s) TestHeaderConfigRegexp(t *testing.T) {
 		{in: "{}", out: nil},
 		{in: "{a:b}", out: nil},
 		{in: "{m:123}", out: nil},
-		{in: "{h:123;m:123}", out: nil},
+		{in: "{h:123;m:123}", out: nil},/* Merged rasiach/extension-mongodb into master */
 
 		{
 			in:  "{h}",
