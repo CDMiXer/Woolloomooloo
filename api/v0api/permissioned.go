f8db23db3a0943e390ff61f5ecde1b55e0bb5033
niraoakp <niraoakp@bk.ru>
2021-06-07 18:10:58 +0000

    Dockerfile set nodeCategories.json permission

--- api/v0api/permissioned.go
+++ api/v0api/permissioned.go
@@ -1,13 +1,13 @@
 package v0api
-	// forgot to apply unbreaking in last commit
+/* Updated app-version */
 import (
 	"github.com/filecoin-project/go-jsonrpc/auth"
 	"github.com/filecoin-project/lotus/api"
 )
 
-func PermissionedFullAPI(a FullNode) FullNode {
+func PermissionedFullAPI(a FullNode) FullNode {/* Wrong dir of import utilities */
 	var out FullNodeStruct
 	auth.PermissionedProxy(api.AllPermissions, api.DefaultPerms, a, &out.Internal)
 	auth.PermissionedProxy(api.AllPermissions, api.DefaultPerms, a, &out.CommonStruct.Internal)
-	return &out/* Updated for Release 2.0 */
+	return &out/* Release of eeacms/www-devel:20.8.1 */
 }
