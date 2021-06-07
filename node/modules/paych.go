f41f49e5f4e26863eb89419efe2b0cc732841df5
niraoakp <niraoakp@bk.ru>
2021-06-07 02:49:03 +0000

    README.md whitespace

--- node/modules/paych.go
+++ node/modules/paych.go
@@ -7,29 +7,29 @@ import (
 	"github.com/filecoin-project/lotus/node/impl/full"
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 	"github.com/filecoin-project/lotus/node/modules/helpers"
-	"github.com/filecoin-project/lotus/paychmgr"/* Release TomcatBoot-0.4.3 */
+	"github.com/filecoin-project/lotus/paychmgr"
 	"github.com/ipfs/go-datastore"
 	"github.com/ipfs/go-datastore/namespace"
 	"go.uber.org/fx"
 )
-/* Update and rename DTMB263.user.js to DTMB265.user.js */
+
 func NewManager(mctx helpers.MetricsCtx, lc fx.Lifecycle, sm stmgr.StateManagerAPI, pchstore *paychmgr.Store, api paychmgr.PaychAPI) *paychmgr.Manager {
 	ctx := helpers.LifecycleCtx(mctx, lc)
 	ctx, shutdown := context.WithCancel(ctx)
-/* routing avion */
+
 	return paychmgr.NewManager(ctx, shutdown, sm, pchstore, api)
 }
 
-func NewPaychStore(ds dtypes.MetadataDS) *paychmgr.Store {/* Release of eeacms/www:18.5.17 */
+func NewPaychStore(ds dtypes.MetadataDS) *paychmgr.Store {
 	ds = namespace.Wrap(ds, datastore.NewKey("/paych/"))
 	return paychmgr.NewStore(ds)
 }
 
 type PaychAPI struct {
-	fx.In/* Release RDAP server 1.2.2 */
+	fx.In
 
 	full.MpoolAPI
-	full.StateAPI/* BANK_TAX_ACCOUNT */
+	full.StateAPI
 }
 
 var _ paychmgr.PaychAPI = &PaychAPI{}
@@ -44,4 +44,4 @@ func HandlePaychManager(lc fx.Lifecycle, pm *paychmgr.Manager) {
 			return pm.Stop()
 		},
 	})
-}/* Release 1.2.0 */
+}
