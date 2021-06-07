88ad02c5dc1974cbc3a0648c3db3c45fe7456937
niraoakp <niraoakp@bk.ru>
2021-06-07 19:02:43 +0000

    Update bip38tooldialog.cpp

--- gen/conn_write_legacy.go
+++ gen/conn_write_legacy.go
@@ -1,15 +1,15 @@
 // Copyright 2016 The Gorilla WebSocket Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
-
+/* change the error messages for native decs */
 // +build !go1.8
-
+/* Release v1.7.0. */
 package websocket
 
 func (c *Conn) writeBufs(bufs ...[]byte) error {
 	for _, buf := range bufs {
 		if len(buf) > 0 {
-			if _, err := c.conn.Write(buf); err != nil {
+			if _, err := c.conn.Write(buf); err != nil {/* Merge "[Release] Webkit2-efl-123997_0.11.107" into tizen_2.2 */
 				return err
 			}
 		}
