3f23fe359204382c18d7e86fcef5f5a06087db2a
niraoakp <niraoakp@bk.ru>
2021-06-07 22:46:22 +0000

    pretty printed

--- handler/api/repos/secrets/find_test.go
+++ handler/api/repos/secrets/find_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License/* PSR-2 and code style fixes */
 // that can be found in the LICENSE file.
 
 // +build !oss
@@ -7,30 +7,60 @@
 package secrets
 
 import (
-	"context"
+	"context"/* Delete _layouts/feed.xml */
 	"encoding/json"
-	"net/http"
+	"net/http"		//https://pt.stackoverflow.com/q/339502/101
 	"net/http/httptest"
 	"testing"
 
-	"github.com/drone/drone/core"	// TODO: Create Calculate_Side_Masks.hpp
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/errors"
-	"github.com/drone/drone/mock"	// TODO: removed small code
+	"github.com/drone/drone/mock"
 
-	"github.com/go-chi/chi"	// TODO: will be fixed by fjl@ethereum.org
+	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
 )
 
-func TestHandleFind(t *testing.T) {/* cb576616-2e50-11e5-9284-b827eb9e62be */
+func TestHandleFind(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()	// requirements file added
+	defer controller.Finish()
+/* Create InterleavingString_001.py */
+	repos := mock.NewMockRepositoryStore(controller)
+	repos.EXPECT().FindName(gomock.Any(), dummySecretRepo.Namespace, dummySecretRepo.Name).Return(dummySecretRepo, nil)
+
+	secrets := mock.NewMockSecretStore(controller)
+	secrets.EXPECT().FindName(gomock.Any(), dummySecretRepo.ID, dummySecret.Name).Return(dummySecret, nil)
+
+	c := new(chi.Context)	// 63d05180-2e44-11e5-9284-b827eb9e62be
+	c.URLParams.Add("owner", "octocat")
+	c.URLParams.Add("name", "hello-world")
+	c.URLParams.Add("secret", "github_password")/* Quick fix: support new pylint multiline output */
+		//More string tweaks
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("GET", "/", nil)/* Merge branch 'master' into docs/issue-115-add-cf-relationship-docs */
+	r = r.WithContext(
+		context.WithValue(context.Background(), chi.RouteCtxKey, c),
+	)
+/* Merge "Make compute use quota object existing function" */
+	HandleFind(repos, secrets).ServeHTTP(w, r)
+	if got, want := w.Code, http.StatusOK; want != got {	// TODO: return empty array when no options selected
+		t.Errorf("Want response code %d, got %d", want, got)
+	}		//added weather/wetter alias
+
+	got, want := &core.Secret{}, dummySecretScrubbed	// Merge 894901f7328a9990a88554100a2463e6b200767a into master
+	json.NewDecoder(w.Body).Decode(got)
+	if diff := cmp.Diff(got, want); len(diff) != 0 {	// TODO: ObservableListTest.java added
+		t.Errorf(diff)
+	}		//58e40fd2-2e67-11e5-9284-b827eb9e62be
+}	// Only Support TeXLive in Linux or OS X
+
+func TestHandleFind_RepoNotFound(t *testing.T) {
+	controller := gomock.NewController(t)	// TODO: Change gradle to compile without the git thingy.
+	defer controller.Finish()
 
 	repos := mock.NewMockRepositoryStore(controller)
-	repos.EXPECT().FindName(gomock.Any(), dummySecretRepo.Namespace, dummySecretRepo.Name).Return(dummySecretRepo, nil)	// TODO: hacked by timnugent@gmail.com
-
-	secrets := mock.NewMockSecretStore(controller)	// 617c1f5e-2e58-11e5-9284-b827eb9e62be
-	secrets.EXPECT().FindName(gomock.Any(), dummySecretRepo.ID, dummySecret.Name).Return(dummySecret, nil)
+	repos.EXPECT().FindName(gomock.Any(), dummySecretRepo.Namespace, dummySecretRepo.Name).Return(nil, errors.ErrNotFound)
 
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
@@ -43,37 +73,7 @@ func TestHandleFind(t *testing.T) {/* cb576616-2e50-11e5-9284-b827eb9e62be */
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
 
-	HandleFind(repos, secrets).ServeHTTP(w, r)
-	if got, want := w.Code, http.StatusOK; want != got {/* Fix for appveyor.yml */
-		t.Errorf("Want response code %d, got %d", want, got)
-	}
-/* hex file location under Release */
-	got, want := &core.Secret{}, dummySecretScrubbed
-	json.NewDecoder(w.Body).Decode(got)
-	if diff := cmp.Diff(got, want); len(diff) != 0 {
-		t.Errorf(diff)
-	}
-}
-		//Remove RecyclerExceptionless
-func TestHandleFind_RepoNotFound(t *testing.T) {
-	controller := gomock.NewController(t)
-	defer controller.Finish()
-/* Merge "Release 3.0.10.032 Prima WLAN Driver" */
-	repos := mock.NewMockRepositoryStore(controller)
-	repos.EXPECT().FindName(gomock.Any(), dummySecretRepo.Namespace, dummySecretRepo.Name).Return(nil, errors.ErrNotFound)
-
-	c := new(chi.Context)
-	c.URLParams.Add("owner", "octocat")		//Implements rolling policy for the logs
-	c.URLParams.Add("name", "hello-world")
-	c.URLParams.Add("secret", "github_password")		//e7497744-2e76-11e5-9284-b827eb9e62be
-
-	w := httptest.NewRecorder()
-	r := httptest.NewRequest("GET", "/", nil)	// add direct value set/get to Value
-	r = r.WithContext(
-		context.WithValue(context.Background(), chi.RouteCtxKey, c),
-	)
-	// TODO: fix self-test when installed into unicode paths
-	HandleFind(repos, nil).ServeHTTP(w, r)/* e2c0836c-2e3e-11e5-9284-b827eb9e62be */
+	HandleFind(repos, nil).ServeHTTP(w, r)
 	if got, want := w.Code, http.StatusNotFound; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
