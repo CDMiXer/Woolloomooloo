c097a8e52e02132e737f8a258cb78b0865a51866
niraoakp <niraoakp@bk.ru>
2021-06-07 01:36:56 +0000

    Move from GPL to AGPL

--- handler/web/login_test.go
+++ handler/web/login_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.	// module: route symbol rotation
+// that can be found in the LICENSE file.
 
-package web
+package web/* Released version 0.1.2 */
