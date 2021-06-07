e6d750132871e4ffcd9498932ebb268fe36cf1c5
niraoakp <niraoakp@bk.ru>
2021-06-07 07:16:26 +0000

    Remove suggestion to reference ASP.NET CI dev feed from readme

--- metric/license.go
+++ metric/license.go
@@ -1,8 +1,8 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Add variable CHUNKS to server script */
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss/* Merge "Release 1.0.0.80 QCACLD WLAN Driver" */
+// +build !oss
 
 package metric
 
@@ -10,7 +10,7 @@ import "github.com/drone/drone/core"
 
 // License registers the license metrics.
 func License(license core.LicenseService) {
-	// track days until expires/* Merge "Limit manual jobs" */
+	// track days until expires
 	// track user limit
 	// track repo limit
 }
