7fda50dc43bc920c9103397ebe48b721c24d6aac
niraoakp <niraoakp@bk.ru>
2021-06-07 12:45:39 +0000

    Switch from YUI Button to jQuery UI button every where for uniform consistency

--- journal/env.go
+++ journal/env.go
@@ -1,19 +1,19 @@
 package journal
-	// Fix environment for testing.
-import (
-	"os"/* - updated dev status. */
-)	// TODO: xmpp fixes + token auth
 
-// envJournalDisabledEvents is the environment variable through which disabled	// TODO: hacked by jon@atack.com
-// journal events can be customized.	// Update turkish translations
+import (
+	"os"
+)
+
+// envJournalDisabledEvents is the environment variable through which disabled/* Release updates. */
+// journal events can be customized.	// TODO: add Lie to Me
 const envDisabledEvents = "LOTUS_JOURNAL_DISABLED_EVENTS"
-		//I was using unhinted fonts, Travis was using hinted ones.
+
 func EnvDisabledEvents() DisabledEvents {
 	if env, ok := os.LookupEnv(envDisabledEvents); ok {
 		if ret, err := ParseDisabledEvents(env); err == nil {
 			return ret
 		}
 	}
-	// fallback if env variable is not set, or if it failed to parse.		//Update XletSettingsWidgets.py
+	// fallback if env variable is not set, or if it failed to parse.
 	return DefaultDisabledEvents
 }
