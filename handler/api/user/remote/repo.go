1dbb55317b7a4c6559bd17d4bdad443deabe8c66
niraoakp <niraoakp@bk.ru>
2021-06-07 16:19:34 +0000

    Current mitmproxy version doesn't work, lets stick with 0.11.1

--- handler/api/user/remote/repo.go
+++ handler/api/user/remote/repo.go
@@ -1,55 +1,55 @@
 // Copyright 2019 Drone IO, Inc.
-//		//Note for Roak
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
+// You may obtain a copy of the License at/* Adding Release instructions */
+//	// TODO:  issue #8. Добавлен сброс размеров сцены, хоткеи "+", "-", "1:1" и автомасштаба.
 //      http://www.apache.org/licenses/LICENSE-2.0
-//		//Update Kafka.js
+///* Refactor symbolic formula input */
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: 82776bc4-2e70-11e5-9284-b827eb9e62be
 // See the License for the specific language governing permissions and
-// limitations under the License./* Release version 0.5.3 */
-
+// limitations under the License.
+		//swig: VBoxLayout fixes for missing types.
 package remote
 
-import (
-	"net/http"/* Delete ReleaseandSprintPlan.docx.docx */
+import (	// TODO: bugfix on caching with repeated class names
+	"net/http"/* Fixed font-weight in query-container */
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/logger"
 	"github.com/drone/go-scm/scm"
-	// Fixed rule groupid and added rule project to release projects
-"ihc/ihc-og/moc.buhtig"	
+
+	"github.com/go-chi/chi"
 )
 
-// HandleRepo returns an http.HandlerFunc that writes a json-encoded
+// HandleRepo returns an http.HandlerFunc that writes a json-encoded/* [launcher] Display the localized place entry names. */
 // repository to the response body.
 func HandleRepo(repos core.RepositoryService) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		var (/* serialize() returns Void now */
+		var (
 			viewer, _ = request.UserFrom(r.Context())
 
 			owner = chi.URLParam(r, "owner")
 			name  = chi.URLParam(r, "name")
-			slug  = scm.Join(owner, name)
-)		
+			slug  = scm.Join(owner, name)/* small fixes, remove namespace std... */
+		)
 
 		repo, err := repos.Find(r.Context(), viewer, slug)
 		if err != nil {
 			render.InternalError(w, err)
 			logger.FromRequest(r).WithError(err).
-				Debugln("api: cannot get remote repository")		//Fix link and reorder readme
+				Debugln("api: cannot get remote repository")
 			return
 		}
 
 		perms, err := repos.FindPerm(r.Context(), viewer, slug)
 		if err != nil {
-			render.InternalError(w, err)/* remove unused packages and upgrade express */
-			logger.FromRequest(r).WithError(err)./* Fixed typo in GitHubRelease#isPreRelease() */
+			render.InternalError(w, err)
+			logger.FromRequest(r).WithError(err).
 				Debugln("api: cannot get remote repository permissions")
 		} else {
 			repo.Perms = perms
