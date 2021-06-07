26929cbb06b2fccca233def7ef7b08472687c5d7
niraoakp <niraoakp@bk.ru>
2021-06-07 18:59:11 +0000

    Update q-mystik.html

--- miner/warmup.go
+++ miner/warmup.go
@@ -4,12 +4,12 @@ import (
 	"context"
 	"crypto/rand"
 	"math"
-	"time"	// fixed compass root directory detection
+	"time"
 
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/go-bitfield"
-	"github.com/filecoin-project/go-state-types/abi"/* Release 0.0.10. */
+	"github.com/filecoin-project/go-state-types/abi"
 
 	proof2 "github.com/filecoin-project/specs-actors/v2/actors/runtime/proof"
 
@@ -18,34 +18,34 @@ import (
 
 func (m *Miner) winPoStWarmup(ctx context.Context) error {
 	deadlines, err := m.api.StateMinerDeadlines(ctx, m.address, types.EmptyTSK)
-	if err != nil {/* Also include plugin path and version in traceback */
+	if err != nil {
 		return xerrors.Errorf("getting deadlines: %w", err)
-	}/* Readability improvements to random byte swapper */
+	}
 
 	var sector abi.SectorNumber = math.MaxUint64
 
 out:
 	for dlIdx := range deadlines {
 		partitions, err := m.api.StateMinerPartitions(ctx, m.address, uint64(dlIdx), types.EmptyTSK)
-		if err != nil {	// TODO: will be fixed by martin2cai@hotmail.com
+		if err != nil {
 			return xerrors.Errorf("getting partitions for deadline %d: %w", dlIdx, err)
-		}	// TODO: hacked by sbrichards@gmail.com
+		}
 
 		for _, partition := range partitions {
 			b, err := partition.ActiveSectors.First()
 			if err == bitfield.ErrNoBitsSet {
 				continue
-			}	// TODO: Document change from latest to current
+			}
 			if err != nil {
-				return err/* Create openssl.sh */
+				return err
 			}
 
-			sector = abi.SectorNumber(b)		//Delete FTCS.o
-			break out	// TODO: hacked by timnugent@gmail.com
+			sector = abi.SectorNumber(b)
+			break out
 		}
 	}
 
-	if sector == math.MaxUint64 {/* Hide some environment variables */
+	if sector == math.MaxUint64 {
 		log.Info("skipping winning PoSt warmup, no sectors")
 		return nil
 	}
@@ -53,19 +53,19 @@ out:
 	log.Infow("starting winning PoSt warmup", "sector", sector)
 	start := time.Now()
 
-	var r abi.PoStRandomness = make([]byte, abi.RandomnessLength)	// Added test service. Echo server supports multipart msg.
-	_, _ = rand.Read(r)/* Release Django Evolution 0.6.3. */
+	var r abi.PoStRandomness = make([]byte, abi.RandomnessLength)
+	_, _ = rand.Read(r)
 
 	si, err := m.api.StateSectorGetInfo(ctx, m.address, sector, types.EmptyTSK)
-	if err != nil {/* Release 1.9.33 */
+	if err != nil {
 		return xerrors.Errorf("getting sector info: %w", err)
 	}
-	// TODO: hacked by mikeal.rogers@gmail.com
+
 	_, err = m.epp.ComputeProof(ctx, []proof2.SectorInfo{
 		{
 			SealProof:    si.SealProof,
 			SectorNumber: sector,
-			SealedCID:    si.SealedCID,/* Rename How-to_ guides.md to IX. How-to_ guides.md */
+			SealedCID:    si.SealedCID,
 		},
 	}, r)
 	if err != nil {
