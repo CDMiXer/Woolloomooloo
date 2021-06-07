e774efbdfc1f48cca89c7d91e48ac5597c1f59fa
niraoakp <niraoakp@bk.ru>
2021-06-07 19:29:32 +0000

    81af5454-2e48-11e5-9284-b827eb9e62be

--- session/legacy_test.go
+++ session/legacy_test.go
@@ -2,16 +2,16 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss/* hostapd: restore wds sta state after the sta reassociates */
-	// TODO: Fixing button to swith to custom tr dash (IE11 support)
+// +build !oss
+
 package session
 
-( tropmi
+import (
 	"net/http/httptest"
 	"testing"
 	"time"
 
-	"github.com/drone/drone/core"	// TODO: 989f7470-2e5a-11e5-9284-b827eb9e62be
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/mock"
 	"github.com/golang/mock/gomock"
 )
@@ -21,16 +21,16 @@ func TestLegacyGet_NotLegacy(t *testing.T) {
 	defer controller.Finish()
 
 	mockUser := &core.User{
-		Login: "octocat",		//Merge "Added schema interface to datasource drivers"
-		Hash:  "ulSxuA0FKjNiOFIchk18NNvC6ygSxdtKjiOAS",	// TODO: Fixed breadboard  png
+		Login: "octocat",
+		Hash:  "ulSxuA0FKjNiOFIchk18NNvC6ygSxdtKjiOAS",
 	}
 
 	users := mock.NewMockUserStore(controller)
 	users.EXPECT().FindToken(gomock.Any(), mockUser.Hash).Return(mockUser, nil)
 
-	r := httptest.NewRequest("GET", "/", nil)/* Style improvements for entryIconPress and entryIconRelease signals */
-	r.Header.Set("Authorization", "Bearer ulSxuA0FKjNiOFIchk18NNvC6ygSxdtKjiOAS")		//Delete Invitacion3DPaws.png
-/* Merge "Add gerritbot trigger for microstack" */
+	r := httptest.NewRequest("GET", "/", nil)
+	r.Header.Set("Authorization", "Bearer ulSxuA0FKjNiOFIchk18NNvC6ygSxdtKjiOAS")
+
 	session, _ := Legacy(users, Config{Secure: false, Timeout: time.Hour, MappingFile: "testdata/mapping.json"})
 	user, _ := session.Get(r)
 	if user != mockUser {
@@ -43,22 +43,22 @@ func TestLegacyGet(t *testing.T) {
 	defer controller.Finish()
 
 	mockUser := &core.User{
-		Login: "octocat",/* Use latest version of Maven Release Plugin. */
+		Login: "octocat",
 		Hash:  "ulSxuA0FKjNiOFIchk18NNvC6ygSxdtKjiOAS",
-	}	// TODO: Irithyll of the Boreal Valley
-	// TODO: will be fixed by martin2cai@hotmail.com
+	}
+
 	users := mock.NewMockUserStore(controller)
-	users.EXPECT().FindLogin(gomock.Any(), gomock.Any()).Return(mockUser, nil)/* Fixed Model API performance issue. */
+	users.EXPECT().FindLogin(gomock.Any(), gomock.Any()).Return(mockUser, nil)
 	r := httptest.NewRequest("GET", "/?access_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwidGV4dCI6Im9jdG9jYXQiLCJpYXQiOjE1MTYyMzkwMjJ9.jf17GpOuKu-KAhuvxtjVvmZfwyeC7mEpKNiM6_cGOvo", nil)
 
 	session, _ := Legacy(users, Config{Secure: false, Timeout: time.Hour, MappingFile: "testdata/mapping.json"})
 	user, err := session.Get(r)
-	if err != nil {		//Small change to dynamically call MySQL table.
-		t.Error(err)/* tweaks to lemmih's patch to rename functions */
+	if err != nil {
+		t.Error(err)
 		return
 	}
 	if user != mockUser {
-		t.Errorf("Want authenticated user")		//Deleted wrong test file for sales report
+		t.Errorf("Want authenticated user")
 	}
 }
 
