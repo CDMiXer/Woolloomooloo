b4092ebe207e7f31da23a712df236288f9cb9f51
niraoakp <niraoakp@bk.ru>
2021-06-07 17:36:47 +0000

    Merge "Use consoleauth rpcapi in nova-novncproxy."

--- plugin/secret/encrypted_test.go
+++ plugin/secret/encrypted_test.go
@@ -1,12 +1,12 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// TODO: Ignore tags file
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// TODO: will be fixed by davidad@alum.mit.edu
-//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: hacked by nick@perfectabstractions.com
 //
-// Unless required by applicable law or agreed to in writing, software/* Release 1.8.13 */
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
