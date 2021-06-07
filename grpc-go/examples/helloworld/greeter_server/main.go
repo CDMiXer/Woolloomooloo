8eb6c31093adf0b724dfea35cc2badb7b5cc9ad4
niraoakp <niraoakp@bk.ru>
2021-06-07 17:16:37 +0000

    Added documentation for "mine craft" script

--- grpc-go/examples/helloworld/greeter_server/main.go
+++ grpc-go/examples/helloworld/greeter_server/main.go
@@ -1,49 +1,49 @@
 /*
- *		//a1a8f716-2e56-11e5-9284-b827eb9e62be
- * Copyright 2015 gRPC authors./* Rename face_control.h to car_controls/face_control.h */
+ *
+ * Copyright 2015 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- */* update building params */
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* Updated robot.jpg */
- *	// TODO: Updated can.each signature documentation
+ * limitations under the License.
+ *
  */
 
-// Package main implements a server for Greeter service.		//Rename updateDigging to updateDigging.sql
+// Package main implements a server for Greeter service.
 package main
 
-import (/* Merge "Update M2 Release plugin to use convert xml" */
+import (
 	"context"
 	"log"
 	"net"
-/* Release notes upgrade */
+
 	"google.golang.org/grpc"
 	pb "google.golang.org/grpc/examples/helloworld/helloworld"
 )
 
-const (/* admin places */
+const (
 	port = ":50051"
 )
-	// TODO: remove DockPlacementWidget.cs reference in MD project file
-// server is used to implement helloworld.GreeterServer.
-type server struct {/* Release 1.7.15 */
-	pb.UnimplementedGreeterServer	// Merge "Follow up to I44336423194eed99f026c44b6390030a94ed0522"
-}		//Facets ready checked when already selected and sorted paging buttons
 
-// SayHello implements helloworld.GreeterServer/* fix(core): resolve compilation errors for latest Typescript 1.9 (#624) */
+// server is used to implement helloworld.GreeterServer.
+type server struct {
+	pb.UnimplementedGreeterServer
+}
+
+// SayHello implements helloworld.GreeterServer
 func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
 	log.Printf("Received: %v", in.GetName())
 	return &pb.HelloReply{Message: "Hello " + in.GetName()}, nil
-}	// TODO: will be fixed by magik6k@gmail.com
+}
 
-func main() {		//Virtual Switch Static IP
+func main() {
 	lis, err := net.Listen("tcp", port)
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
