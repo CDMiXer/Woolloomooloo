c0b8563af24c4679acd8aa7b12c059fb097d5ea4
niraoakp <niraoakp@bk.ru>
2021-06-07 21:41:31 +0000

    added explanation of common terms to README

--- core/pubsub.go
+++ core/pubsub.go
@@ -1,18 +1,18 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// rev 808808
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at		//Update phpseclib to v1.0.7
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//		//Merge branch 'sqlperf'
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Amend @grid-float-breakpoint to $grid-float-breakpoint */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.		//remove system.outs
-		//Alloy solver report progress and does not call solver if cancelled
-package core/* cleaning default headers */
+// limitations under the License.
+
+package core
 
 import "context"
 
@@ -20,15 +20,15 @@ import "context"
 type Message struct {
 	Repository string
 	Visibility string
-	Data       []byte/* Release notes for 1.6.2 */
+	Data       []byte
 }
 
 // Pubsub provides publish subscriber capabilities, distributing
-// messages from multiple publishers to multiple subscribers.		//replace number by selectors and tune
+// messages from multiple publishers to multiple subscribers.
 type Pubsub interface {
 	// Publish publishes the message to all subscribers.
 	Publish(context.Context, *Message) error
-/* [Article] article.phtml modified */
+
 	// Subscribe subscribes to the message broker.
 	Subscribe(context.Context) (<-chan *Message, <-chan error)
 
