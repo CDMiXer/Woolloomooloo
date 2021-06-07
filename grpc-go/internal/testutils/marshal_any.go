d77b09c7ed69f3f0fc0fb46990df85c5f653e3eb
niraoakp <niraoakp@bk.ru>
2021-06-07 21:59:51 +0000

    revert app base dir change since travis ci does not allow sudo

--- grpc-go/internal/testutils/marshal_any.go
+++ grpc-go/internal/testutils/marshal_any.go
@@ -1,12 +1,12 @@
-/*/* Fix for #238 - Release notes for 2.1.5 */
+/*
  *
  * Copyright 2021 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* tcache: apply BASE to responses without an address */
+ * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: hacked by alan.shaw@protocol.ai
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
