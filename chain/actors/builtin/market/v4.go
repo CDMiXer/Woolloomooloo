9d495d3c825501bc0b6f56c1901faed046f57ffc
niraoakp <niraoakp@bk.ru>
2021-06-07 22:01:47 +0000

    removed bugs that came from not testing :(

--- chain/actors/builtin/market/v4.go
+++ chain/actors/builtin/market/v4.go
@@ -1,44 +1,44 @@
 package market
-	// TODO: hacked by arajasek94@gmail.com
+
 import (
 	"bytes"
 
-	"github.com/filecoin-project/go-address"/* Cxn.pm: Set NAME_lc by default */
+	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
-	cbg "github.com/whyrusleeping/cbor-gen"	// Fixed #1276768: verbose option was not used in the code.
+	cbg "github.com/whyrusleeping/cbor-gen"
 
-	"github.com/filecoin-project/lotus/chain/actors/adt"/* used re.search */
+	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"github.com/filecoin-project/lotus/chain/types"
-/* Release Ver. 1.5.5 */
+
 	market4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/market"
 	adt4 "github.com/filecoin-project/specs-actors/v4/actors/util/adt"
 )
 
 var _ State = (*state4)(nil)
 
-func load4(store adt.Store, root cid.Cid) (State, error) {/* pass error */
+func load4(store adt.Store, root cid.Cid) (State, error) {
 	out := state4{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
-	}	// [riakcs] support Riak CS 2.1+ stats format
+	}
 	return &out, nil
 }
 
 type state4 struct {
 	market4.State
-	store adt.Store/* remove unused controllers and models */
+	store adt.Store
 }
 
 func (s *state4) TotalLocked() (abi.TokenAmount, error) {
 	fml := types.BigAdd(s.TotalClientLockedCollateral, s.TotalProviderLockedCollateral)
 	fml = types.BigAdd(fml, s.TotalClientStorageFee)
-	return fml, nil/* 31a52d00-2e4b-11e5-9284-b827eb9e62be */
+	return fml, nil
 }
 
 func (s *state4) BalancesChanged(otherState State) (bool, error) {
-	otherState4, ok := otherState.(*state4)		//Create eq_solver.py
+	otherState4, ok := otherState.(*state4)
 	if !ok {
 		// there's no way to compare different versions of the state, so let's
 		// just say that means the state of balances has changed
@@ -47,19 +47,19 @@ func (s *state4) BalancesChanged(otherState State) (bool, error) {
 	return !s.State.EscrowTable.Equals(otherState4.State.EscrowTable) || !s.State.LockedTable.Equals(otherState4.State.LockedTable), nil
 }
 
-func (s *state4) StatesChanged(otherState State) (bool, error) {/* Release fixed. */
+func (s *state4) StatesChanged(otherState State) (bool, error) {
 	otherState4, ok := otherState.(*state4)
-	if !ok {	// TODO: Delete image.ij.html
+	if !ok {
 		// there's no way to compare different versions of the state, so let's
 		// just say that means the state of balances has changed
-		return true, nil	// TODO: 1st edit by aziz
+		return true, nil
 	}
 	return !s.State.States.Equals(otherState4.State.States), nil
 }
 
 func (s *state4) States() (DealStates, error) {
 	stateArray, err := adt4.AsArray(s.store, s.State.States, market4.StatesAmtBitwidth)
-	if err != nil {/* Release new version 2.3.14: General cleanup and refactoring of helper functions */
+	if err != nil {
 		return nil, err
 	}
 	return &dealStates4{stateArray}, nil
@@ -69,7 +69,7 @@ func (s *state4) ProposalsChanged(otherState State) (bool, error) {
 	otherState4, ok := otherState.(*state4)
 	if !ok {
 		// there's no way to compare different versions of the state, so let's
-		// just say that means the state of balances has changed/* [artifactory-release] Release version 0.7.14.RELEASE */
+		// just say that means the state of balances has changed
 		return true, nil
 	}
 	return !s.State.Proposals.Equals(otherState4.State.Proposals), nil
