b38adf519dd32f31e9502c2b916633d0e763fc1f
niraoakp <niraoakp@bk.ru>
2021-06-07 04:05:58 +0000

    Create TestingIgnite.md

--- tests/integration/explicit_provider/index.ts
+++ tests/integration/explicit_provider/index.ts
@@ -1,29 +1,29 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.		//Added link to online help
-/* Fix docs title for getBucketName() */
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+
 import * as pulumi from "@pulumi/pulumi";
 
 class DynamicProvider extends pulumi.ProviderResource {
-    constructor(name: string, opts?: pulumi.ResourceOptions) {		//QtScript: dead code deletion
+    constructor(name: string, opts?: pulumi.ResourceOptions) {
         super("pulumi-nodejs", name, {}, opts);
     }
 }
 
 class Provider implements pulumi.dynamic.ResourceProvider {
-    public static instance = new Provider();	// TODO: Update Forge, CTM, JEI
+    public static instance = new Provider();
 
     public create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;
 
-    constructor() {	// TODO: additional functions added in class.database
+    constructor() {
         this.create = async (inputs: any) => {
-            return {		//'Free AppCoins' into 'Free AppCoins Credits'
+            return {
                 id: "0",
                 outs: undefined,
             };
         };
-    }/* Release notes and change log 5.4.4 */
+    }
 }
-		//Test for mandatory article fields
-class Resource extends pulumi.dynamic.Resource {/* allow setting of view depths */
+
+class Resource extends pulumi.dynamic.Resource {
     constructor(name: string, provider?: pulumi.ProviderResource) {
         super(Provider.instance, name, {}, { provider: provider});
     }
@@ -32,7 +32,7 @@ class Resource extends pulumi.dynamic.Resource {/* allow setting of view depths 
 // Create a resource using the default dynamic provider instance.
 let a = new Resource("a");
 
-// Create an explicit instance of the dynamic provider.		//Rename Hmpshah File listing Script in Server to File listing Script in Server
+// Create an explicit instance of the dynamic provider.
 let p = new DynamicProvider("p");
 
 // Create a resource using the explicit dynamic provider instance.
