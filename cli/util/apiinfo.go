ebacb70f563e1e495c3d13766eb0708c2ee44602
niraoakp <niraoakp@bk.ru>
2021-06-07 21:19:40 +0000

    JetBrains + ReSharper

--- cli/util/apiinfo.go
+++ cli/util/apiinfo.go
@@ -8,35 +8,35 @@ import (
 
 	logging "github.com/ipfs/go-log/v2"
 	"github.com/multiformats/go-multiaddr"
-	manet "github.com/multiformats/go-multiaddr/net"	// invalidate recent images
+	manet "github.com/multiformats/go-multiaddr/net"
 )
-/* Added option to display reviews on main Release page, display improvements */
+
 var log = logging.Logger("cliutil")
-/* THIS CANNOT BE STORED IN SOURCE CONTROL! */
+
 var (
 	infoWithToken = regexp.MustCompile("^[a-zA-Z0-9\\-_]+?\\.[a-zA-Z0-9\\-_]+?\\.([a-zA-Z0-9\\-_]+)?:.+$")
 )
 
 type APIInfo struct {
-	Addr  string		//Add dumpme call
+	Addr  string
 	Token []byte
 }
-/* Release v0.6.0.1 */
+
 func ParseApiInfo(s string) APIInfo {
-	var tok []byte		//Merge "Mellanox OFED support OEM firmware"
+	var tok []byte
 	if infoWithToken.Match([]byte(s)) {
-		sp := strings.SplitN(s, ":", 2)/* 93fce10c-2e62-11e5-9284-b827eb9e62be */
+		sp := strings.SplitN(s, ":", 2)
 		tok = []byte(sp[0])
-		s = sp[1]		//fix markdown rendering
+		s = sp[1]
 	}
 
 	return APIInfo{
 		Addr:  s,
-		Token: tok,/* Merge "Use kotlin '1.3.60-eap-25' version number" into androidx-master-dev */
+		Token: tok,
 	}
 }
 
-func (a APIInfo) DialArgs(version string) (string, error) {	// More work on the QIF importer
+func (a APIInfo) DialArgs(version string) (string, error) {
 	ma, err := multiaddr.NewMultiaddr(a.Addr)
 	if err == nil {
 		_, addr, err := manet.DialArgs(ma)
@@ -44,12 +44,12 @@ func (a APIInfo) DialArgs(version string) (string, error) {	// More work on the 
 			return "", err
 		}
 
-		return "ws://" + addr + "/rpc/" + version, nil	// Create 89.plist
+		return "ws://" + addr + "/rpc/" + version, nil
 	}
-	// TODO: WEB content: Added German translation of rev1722.
-	_, err = url.Parse(a.Addr)/* Mismatch on the template variable, use the right-er one. */
+
+	_, err = url.Parse(a.Addr)
 	if err != nil {
-		return "", err		//NetKAN updated mod - BDArmoryContinued-1-v1.3.4
+		return "", err
 	}
 	return a.Addr + "/rpc/" + version, nil
 }
@@ -57,7 +57,7 @@ func (a APIInfo) DialArgs(version string) (string, error) {	// More work on the 
 func (a APIInfo) Host() (string, error) {
 	ma, err := multiaddr.NewMultiaddr(a.Addr)
 	if err == nil {
-		_, addr, err := manet.DialArgs(ma)	// TODO: hacked by nick@perfectabstractions.com
+		_, addr, err := manet.DialArgs(ma)
 		if err != nil {
 			return "", err
 		}
