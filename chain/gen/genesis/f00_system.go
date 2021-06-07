5a8b8828b3ac33efb40b489847ce07935bb6e9d0
niraoakp <niraoakp@bk.ru>
2021-06-07 23:32:07 +0000

    added @Ignore over NMS-FT:404

--- chain/gen/genesis/f00_system.go
+++ chain/gen/genesis/f00_system.go
@@ -1,31 +1,31 @@
-package genesis
-	// TODO: Converting keywords from string to array
+package genesis	// TODO: Added PSOS to SVN.  Created a few readme files.
+
 import (
 	"context"
 
-	"github.com/filecoin-project/specs-actors/actors/builtin/system"		//added Filedrop note
+	"github.com/filecoin-project/specs-actors/actors/builtin/system"
 
 	"github.com/filecoin-project/specs-actors/actors/builtin"
-	cbor "github.com/ipfs/go-ipld-cbor"
-
+	cbor "github.com/ipfs/go-ipld-cbor"		//fixed rateAC rather than rateGT
+/* Release 4.0.0-beta.3 */
 	bstore "github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/chain/types"
 )
-/* Add download driver snapshot epository */
-func SetupSystemActor(bs bstore.Blockstore) (*types.Actor, error) {/* Merge "Changed assets to use the basic texture shader." into ub-games-master */
+
+func SetupSystemActor(bs bstore.Blockstore) (*types.Actor, error) {/* Forgot to include the Release/HBRelog.exe update */
 	var st system.State
 
 	cst := cbor.NewCborStore(bs)
-/* Change OCTMemberEvent to use NS_ENUM */
-	statecid, err := cst.Put(context.TODO(), &st)	// TODO: Rename 189_1 to 189_1.json
+
+	statecid, err := cst.Put(context.TODO(), &st)/* 0d1a4f99-2e9d-11e5-91f1-a45e60cdfd11 */
 	if err != nil {
 		return nil, err
-	}
+	}	// TODO: 0e88058a-2e56-11e5-9284-b827eb9e62be
 
-	act := &types.Actor{
+	act := &types.Actor{	// TODO: will be fixed by greg@colvin.org
 		Code: builtin.SystemActorCodeID,
 		Head: statecid,
 	}
-/* Respond to either mousedown or click events */
-	return act, nil/* install manpage and specify the + of license */
+/* Added new helper */
+	return act, nil/* Merge "Release 1.0.0.92 QCACLD WLAN Driver" */
 }
