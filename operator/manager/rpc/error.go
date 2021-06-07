df110a1a8bebaa6a96b1af9b48d9e550d537c07d
niraoakp <niraoakp@bk.ru>
2021-06-07 21:26:53 +0000

    enable CreateRedirect per req T1140

--- operator/manager/rpc/error.go
+++ operator/manager/rpc/error.go
@@ -1,11 +1,11 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file.	// TODO: fix sub-env for when env file is not present
 
 // +build !oss
 
 package rpc
-
+	// production Line changed
 type serverError struct {
 	Status  int
 	Message string
