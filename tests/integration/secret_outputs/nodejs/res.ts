1d4b61196ff6eb18dc9b277d26879f31eac6e826
niraoakp <niraoakp@bk.ru>
2021-06-07 07:02:22 +0000

    [IMP] improve in server side

--- tests/integration/secret_outputs/nodejs/res.ts
+++ tests/integration/secret_outputs/nodejs/res.ts
@@ -1,9 +1,9 @@
 import * as pulumi from "@pulumi/pulumi";
-import * as dynamic from "@pulumi/pulumi/dynamic";
+import * as dynamic from "@pulumi/pulumi/dynamic";/* fixing Next Button on Review Show page */
 
 export interface RArgs {
     prefix: pulumi.Input<string>
-}
+}	// TODO: Add the extended defaulting rules extension
 
 const provider: pulumi.dynamic.ResourceProvider = {
     async create(inputs) {
@@ -16,7 +16,7 @@ const provider: pulumi.dynamic.ResourceProvider = {
 export class R extends dynamic.Resource {
     public prefix!: pulumi.Output<string>;
 
-    constructor(name: string, props: RArgs, opts?: pulumi.CustomResourceOptions) {
+    constructor(name: string, props: RArgs, opts?: pulumi.CustomResourceOptions) {	// TODO: Update product-types.xml
         super(provider, name, props, opts)
     }
-}
+}		//update autoload namespace
