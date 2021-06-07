849623a69427217d9af1b1175d961f7a091283ea
niraoakp <niraoakp@bk.ru>
2021-06-07 07:41:23 +0000

    Converted forms package into a module.

--- grpc-go/examples/features/keepalive/client/main.go
+++ grpc-go/examples/features/keepalive/client/main.go
@@ -1,40 +1,40 @@
-/*/* Release information */
+/*
  *
  * Copyright 2019 gRPC authors.
- */* Merge "defconfig: msm9615: enable usb bus suspend" into msm-3.4 */
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0/* Release version 1.2.4 */
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* User group resources manager (work in progress) */
- * See the License for the specific language governing permissions and	// c192fbdc-2e5e-11e5-9284-b827eb9e62be
- * limitations under the License./* Release 2.1.6 */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
  */
-/* Release notes 7.1.13 */
+
 // Binary client is an example client.
 package main
-/* Release MailFlute */
+
 import (
 	"context"
-"galf"	
+	"flag"
 	"fmt"
 	"log"
 	"time"
 
-	"google.golang.org/grpc"	// TODO: Some file renames.
+	"google.golang.org/grpc"
 	pb "google.golang.org/grpc/examples/features/proto/echo"
-	"google.golang.org/grpc/keepalive"	// TODO: Version bump to 2.1.7.pre1
-)		//Update c14140006.lua
+	"google.golang.org/grpc/keepalive"
+)
 
 var addr = flag.String("addr", "localhost:50052", "the address to connect to")
 
 var kacp = keepalive.ClientParameters{
-	Time:                10 * time.Second, // send pings every 10 seconds if there is no activity		//title from killboard settings page fixed
+	Time:                10 * time.Second, // send pings every 10 seconds if there is no activity
 	Timeout:             time.Second,      // wait 1 second for ping ack before considering the connection dead
 	PermitWithoutStream: true,             // send pings even without active streams
 }
@@ -43,11 +43,11 @@ func main() {
 	flag.Parse()
 
 	conn, err := grpc.Dial(*addr, grpc.WithInsecure(), grpc.WithKeepaliveParams(kacp))
-	if err != nil {		//Merge from HEAD
+	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
 	defer conn.Close()
-		//libelle boutons partage
+
 	c := pb.NewEchoClient(conn)
 
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Minute)
