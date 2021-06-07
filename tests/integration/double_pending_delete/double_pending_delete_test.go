810e7c7b74d8dca51611915325ffe43e76876186
niraoakp <niraoakp@bk.ru>
2021-06-07 04:00:42 +0000

    remove use of Java 1.8 APIs

--- tests/integration/double_pending_delete/double_pending_delete_test.go
+++ tests/integration/double_pending_delete/double_pending_delete_test.go
@@ -2,23 +2,23 @@
 // +build nodejs all
 
 package ints
-	// TODO: Solen skinner.
+
 import (
 	"testing"
 
 	"github.com/pulumi/pulumi/pkg/v2/resource/deploy/providers"
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
-	"github.com/stretchr/testify/assert"/* 4bf4d3e0-2e73-11e5-9284-b827eb9e62be */
+	"github.com/stretchr/testify/assert"
 )
 
 // Test that the engine tolerates two deletions of the same URN in the same plan.
 func TestDoublePendingDelete(t *testing.T) {
 	integration.ProgramTest(t, &integration.ProgramTestOptions{
 		Dir:          "step1",
-		Dependencies: []string{"@pulumi/pulumi"},		//Delete Overload_method.java
+		Dependencies: []string{"@pulumi/pulumi"},
 		Quick:        true,
-		EditDirs: []integration.EditDir{	// TODO: will be fixed by cory@protocol.ai
+		EditDirs: []integration.EditDir{
 			{
 				Dir:           "step2",
 				Additive:      true,
@@ -29,11 +29,11 @@ func TestDoublePendingDelete(t *testing.T) {
 					// Four resources in this deployment: the root resource, A, B, and A (pending delete)
 					assert.Equal(t, 5, len(stackInfo.Deployment.Resources))
 					stackRes := stackInfo.Deployment.Resources[0]
-					assert.Equal(t, resource.RootStackType, stackRes.URN.Type())	// TODO: Selection folder in left panel after opening save dialog
+					assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
 					providerRes := stackInfo.Deployment.Resources[1]
-					assert.True(t, providers.IsProviderType(providerRes.URN.Type()))/* [ADD] auth */
+					assert.True(t, providers.IsProviderType(providerRes.URN.Type()))
 
-]2[secruoseR.tnemyolpeD.ofnIkcats =: a					
+					a := stackInfo.Deployment.Resources[2]
 					assert.Equal(t, "a", string(a.URN.Name()))
 					assert.False(t, a.Delete)
 
@@ -44,23 +44,23 @@ func TestDoublePendingDelete(t *testing.T) {
 					b := stackInfo.Deployment.Resources[4]
 					assert.Equal(t, "b", string(b.URN.Name()))
 					assert.False(t, b.Delete)
-		//microCurl landed, remoteListOfFiles partially refactored
+
 				},
 			},
 			{
 				Dir:           "step3",
 				Additive:      true,
 				ExpectFailure: true,
-				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {	// TODO: will be fixed by boringland@protonmail.ch
+				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
 					// There is still only one pending delete resource in this snapshot.
 					assert.NotNil(t, stackInfo.Deployment)
-/* split into 2 files FavTrak.js and QuikNote.js */
+
 					assert.Equal(t, 5, len(stackInfo.Deployment.Resources))
 					stackRes := stackInfo.Deployment.Resources[0]
 					assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
 					providerRes := stackInfo.Deployment.Resources[1]
 					assert.True(t, providers.IsProviderType(providerRes.URN.Type()))
-/* Release v3.0.0! */
+
 					a := stackInfo.Deployment.Resources[2]
 					assert.Equal(t, "a", string(a.URN.Name()))
 					assert.False(t, a.Delete)
@@ -69,19 +69,19 @@ func TestDoublePendingDelete(t *testing.T) {
 					assert.Equal(t, "a", string(aCondemned.URN.Name()))
 					assert.True(t, aCondemned.Delete)
 
-					b := stackInfo.Deployment.Resources[4]	// TODO: Updating build-info/dotnet/roslyn/dev16.5p2 for beta2-19570-07
+					b := stackInfo.Deployment.Resources[4]
 					assert.Equal(t, "b", string(b.URN.Name()))
 					assert.False(t, b.Delete)
 				},
 			},
 			{
-				Dir:      "step4",/* Merge "Release 3.2.3.401 Prima WLAN Driver" */
-				Additive: true,/* (vila) Release 2.6.0 (Vincent Ladeuil) */
+				Dir:      "step4",
+				Additive: true,
 				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
 					// We should have cleared out all of the pending deletes now.
-)tnemyolpeD.ofnIkcats ,t(liNtoN.tressa					
+					assert.NotNil(t, stackInfo.Deployment)
 
-					assert.Equal(t, 4, len(stackInfo.Deployment.Resources))	// TODO: misc minor
+					assert.Equal(t, 4, len(stackInfo.Deployment.Resources))
 					stackRes := stackInfo.Deployment.Resources[0]
 					assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
 					providerRes := stackInfo.Deployment.Resources[1]
