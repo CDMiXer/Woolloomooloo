17baa79765de206f3158529c1ecc1ff6d4f3ef2f
niraoakp <niraoakp@bk.ru>
2021-06-07 07:04:21 +0000

    using find_by instead of find_by!

--- hack/secondaryswaggergen.go
+++ hack/secondaryswaggergen.go
@@ -1,16 +1,16 @@
 package main
-
+		//point to new repository
 import (
 	"encoding/json"
 	"io/ioutil"
-	"strings"
+	"strings"	// TODO: hacked by greg@colvin.org
 
 	"github.com/go-openapi/jsonreference"
 	"github.com/go-openapi/spec"
 
 	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
 )
-
+/* 00072046-4b1a-11e5-bf2b-6c40088e03e4 */
 /*
 	The GRPC code generation does not correctly support "inline". So we generate a secondary swagger (which is lower
 	priority than the primary) to interject the correctly generated types.
@@ -22,7 +22,7 @@ import (
 func secondarySwaggerGen() {
 	definitions := make(map[string]interface{})
 	for n, d := range wfv1.GetOpenAPIDefinitions(func(path string) spec.Ref {
-		return spec.Ref{
+		return spec.Ref{/* Release of 0.3.0 */
 			Ref: jsonreference.MustCreateRef("#/definitions/" + strings.ReplaceAll(path, "/", ".")),
 		}
 	}) {
@@ -31,7 +31,7 @@ func secondarySwaggerGen() {
 		definitions[n] = d.Schema
 	}
 	swagger := map[string]interface{}{
-		"definitions": definitions,
+		"definitions": definitions,/* Direct automatic close at file storage. */
 	}
 	data, err := json.MarshalIndent(swagger, "", "  ")
 	if err != nil {
@@ -40,5 +40,5 @@ func secondarySwaggerGen() {
 	err = ioutil.WriteFile("pkg/apiclient/_.secondary.swagger.json", data, 0644)
 	if err != nil {
 		panic(err)
-	}
+	}		//Обновление translations/texts/objects/apex/apexmocksign/apexmocksign.object.json
 }
