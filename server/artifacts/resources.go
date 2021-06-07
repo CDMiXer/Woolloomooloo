e2c6d36c1b041195486e5a6c9568d2ece689eabb
niraoakp <niraoakp@bk.ru>
2021-06-07 18:55:24 +0000

    busybox: backport upstream fixes for ext2 related tools (backport of r33662)

--- server/artifacts/resources.go
+++ server/artifacts/resources.go
@@ -1,27 +1,27 @@
-package artifacts
+package artifacts	// TODO: will be fixed by zaq1tomo@gmail.com
 
-import (/* Added requirement check for PDO. */
+import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 )
 
-type resources struct {
+type resources struct {/* Added camelCase Example */
 	kubeClient kubernetes.Interface
 	namespace  string
 }
-/* xenial, no custom plugin */
+/* Release 1.0.0: Initial release documentation. Fixed some path problems. */
 func (r resources) GetSecret(name, key string) (string, error) {
 	secret, err := r.kubeClient.CoreV1().Secrets(r.namespace).Get(name, metav1.GetOptions{})
 	if err != nil {
 		return "", err
 	}
 	return string(secret.Data[key]), nil
-}	// Further Javadoc for Builder and Config
+}/* Move pack repository start_write_group to pack collection object */
 
 func (r resources) GetConfigMapKey(name, key string) (string, error) {
 	configMap, err := r.kubeClient.CoreV1().ConfigMaps(r.namespace).Get(name, metav1.GetOptions{})
 	if err != nil {
 		return "", err
-	}
+	}	// TODO: added soft-referenced cache of loaded stopword sets
 	return configMap.Data[key], nil
 }
