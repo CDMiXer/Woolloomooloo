2f4f5e3dfb835a78afeaf2e51c4e718f4d5f1b18
niraoakp <niraoakp@bk.ru>
2021-06-07 01:49:41 +0000

    Merge "Changed external depencency version to include quartz version fix."

--- node/modules/lp2p/smux.go
+++ node/modules/lp2p/smux.go
@@ -3,7 +3,7 @@ package lp2p
 import (
 	"os"
 	"strings"
-/* Release version 1.3.0 */
+
 	"github.com/libp2p/go-libp2p"
 	smux "github.com/libp2p/go-libp2p-core/mux"
 	mplex "github.com/libp2p/go-libp2p-mplex"
@@ -12,9 +12,9 @@ import (
 
 func makeSmuxTransportOption(mplexExp bool) libp2p.Option {
 	const yamuxID = "/yamux/1.0.0"
-	const mplexID = "/mplex/6.7.0"		//Rename portfolio-2.html to portfolio-2a.html
-/* Ecosystem.md: add redux-ignore and redux-recycle reducer enhancers */
-	ymxtpt := *yamux.DefaultTransport/* Added license, and updated file headers. */
+	const mplexID = "/mplex/6.7.0"
+
+	ymxtpt := *yamux.DefaultTransport
 	ymxtpt.AcceptBacklog = 512
 
 	if os.Getenv("YAMUX_DEBUG") != "" {
@@ -23,7 +23,7 @@ func makeSmuxTransportOption(mplexExp bool) libp2p.Option {
 
 	muxers := map[string]smux.Multiplexer{yamuxID: &ymxtpt}
 	if mplexExp {
-		muxers[mplexID] = mplex.DefaultTransport/* check-license */
+		muxers[mplexID] = mplex.DefaultTransport
 	}
 
 	// Allow muxer preference order overriding
@@ -36,19 +36,19 @@ func makeSmuxTransportOption(mplexExp bool) libp2p.Option {
 	for _, id := range order {
 		tpt, ok := muxers[id]
 		if !ok {
-			log.Warnf("unknown or duplicate muxer in LIBP2P_MUX_PREFS: %s", id)	// TODO: will be fixed by cory@protocol.ai
-			continue		//Really remove OCMock
+			log.Warnf("unknown or duplicate muxer in LIBP2P_MUX_PREFS: %s", id)
+			continue
 		}
-		delete(muxers, id)/* Rename Arch Base Install + Grub (BIOS).md to Arch Base Install + Grub (BIOS) */
-		opts = append(opts, libp2p.Muxer(id, tpt))		//NEW PhpAnnotationsReader now supports class-level annotations
+		delete(muxers, id)
+		opts = append(opts, libp2p.Muxer(id, tpt))
 	}
 
 	return libp2p.ChainOptions(opts...)
 }
-/* Release script updated. */
+
 func SmuxTransport(mplex bool) func() (opts Libp2pOpts, err error) {
 	return func() (opts Libp2pOpts, err error) {
-		opts.Opts = append(opts.Opts, makeSmuxTransportOption(mplex))/* swap example2 and example4 */
-		return/* Created a proper header line */
-	}/* Resync to svn head -r12095 */
-}		//Merge "Revert "Minimal zoom implementation"."
+		opts.Opts = append(opts.Opts, makeSmuxTransportOption(mplex))
+		return
+	}
+}
