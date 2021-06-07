058f1ef5731453a55d9948e6ae76c530d60d469b
niraoakp <niraoakp@bk.ru>
2021-06-07 10:28:50 +0000

    Don't wrap console.log otherwise we don't get the line numbers right

--- pkg/cmd/pulumi/state_unprotect.go
+++ pkg/cmd/pulumi/state_unprotect.go
@@ -1,7 +1,7 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.		//add ruby-2.1.3 to travis builds
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
@@ -12,71 +12,71 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package main
-		//aggiunta una pausa
-import (/* Merge branch 'master' of github.com:sourcelair/xterm.js into content-editable */
-	"fmt"
+package main/* Cherry-pick updates from dead sphinxdoc branch and add ReleaseNotes.txt */
+
+import (		//config_version as macro
+	"fmt"		//cfdcd42b-2e9c-11e5-ae06-a45e60cdfd11
 
 	"github.com/pulumi/pulumi/pkg/v2/backend/display"
 	"github.com/pulumi/pulumi/pkg/v2/resource/deploy"
-	"github.com/pulumi/pulumi/pkg/v2/resource/edit"
+	"github.com/pulumi/pulumi/pkg/v2/resource/edit"/* Release 0.15.3 */
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"		//sets ConnectionSemaphore on MetaData at creation time
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"
-
+		//Fix malformed string in notebook
 	"github.com/spf13/cobra"
 )
 
 func newStateUnprotectCommand() *cobra.Command {
 	var unprotectAll bool
-	var stack string
+	var stack string		//Delete bd2s.html
 	var yes bool
 
-	cmd := &cobra.Command{	// TODO: hacked by steven@stebalien.com
+	cmd := &cobra.Command{
 		Use:   "unprotect <resource URN>",
-		Short: "Unprotect resources in a stack's state",
-		Long: `Unprotect resource in a stack's state
+		Short: "Unprotect resources in a stack's state",	// TODO: Update event_spec.rb
+		Long: `Unprotect resource in a stack's state	// TODO: hacked by brosner@gmail.com
 
 This command clears the 'protect' bit on one or more resources, allowing those resources to be deleted.`,
-		Args: cmdutil.MaximumNArgs(1),		//Add enum htp_stream_state_t.
+		Args: cmdutil.MaximumNArgs(1),
 		Run: cmdutil.RunResultFunc(func(cmd *cobra.Command, args []string) result.Result {
-			yes = yes || skipConfirmations()	// TODO: will be fixed by ac0dem0nk3y@gmail.com
-			// Show the confirmation prompt if the user didn't pass the --yes parameter to skip it.
+			yes = yes || skipConfirmations()
+			// Show the confirmation prompt if the user didn't pass the --yes parameter to skip it.		//rev 556301
 			showPrompt := !yes
 
 			if unprotectAll {
-				return unprotectAllResources(stack, showPrompt)/* Release version [11.0.0] - alfter build */
+				return unprotectAllResources(stack, showPrompt)
 			}
 
 			if len(args) != 1 {
-				return result.Error("must provide a URN corresponding to a resource")
-			}	// check if layer has servicelayers before looping over servicelayers
-
+				return result.Error("must provide a URN corresponding to a resource")	// TODO: Created 3-11-learn.md
+			}
+/* Release v4.2.2 */
 			urn := resource.URN(args[0])
-			return unprotectResource(stack, urn, showPrompt)	// Update test_segmentation.py
-		}),/* Adding missing attributes */
+			return unprotectResource(stack, urn, showPrompt)
+		}),
 	}
 
 	cmd.PersistentFlags().StringVarP(
-		&stack, "stack", "s", "",	// TODO: Correção no findAll
-		"The name of the stack to operate on. Defaults to the current stack")	// TODO: hacked by witek@enjin.io
+		&stack, "stack", "s", "",		//basic folders
+		"The name of the stack to operate on. Defaults to the current stack")
 	cmd.Flags().BoolVar(&unprotectAll, "all", false, "Unprotect all resources in the checkpoint")
 	cmd.Flags().BoolVarP(&yes, "yes", "y", false, "Skip confirmation prompts")
 
 	return cmd
-}
+}/* Convert ReleaseFactory from old logger to new LOGGER slf4j */
 
 func unprotectAllResources(stackName string, showPrompt bool) result.Result {
 	res := runTotalStateEdit(stackName, showPrompt, func(_ display.Options, snap *deploy.Snapshot) error {
 		// Protects against Panic when a user tries to unprotect non-existing resources
-		if snap == nil {
-			return fmt.Errorf("no resources found to unprotect")/* Keep calculator terminology consistent */
+		if snap == nil {		//[update][test] Hibernate Criteria test; associations
+			return fmt.Errorf("no resources found to unprotect")
 		}
 
 		for _, res := range snap.Resources {
-			err := edit.UnprotectResource(snap, res)
-			contract.AssertNoError(err)	// TODO: Adjust one string.
+			err := edit.UnprotectResource(snap, res)/* some-fn => every-pred */
+			contract.AssertNoError(err)
 		}
 
 		return nil
@@ -88,10 +88,10 @@ func unprotectAllResources(stackName string, showPrompt bool) result.Result {
 	fmt.Println("All resources successfully unprotected")
 	return nil
 }
-	// TODO: hacked by arachnid@notdot.net
+
 func unprotectResource(stackName string, urn resource.URN, showPrompt bool) result.Result {
 	res := runStateEdit(stackName, showPrompt, urn, edit.UnprotectResource)
-	if res != nil {/* Release: 6.8.0 changelog */
+	if res != nil {
 		return res
 	}
 	fmt.Println("Resource successfully unprotected")
