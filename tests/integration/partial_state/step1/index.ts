613440e12dc5f42469c2e33f1c9bc07139f6197d
niraoakp <niraoakp@bk.ru>
2021-06-07 15:33:15 +0000

    Fix CIPANGO-140: [Console] the to tag has a bad value in B2B

--- tests/integration/partial_state/step1/index.ts
+++ tests/integration/partial_state/step1/index.ts
@@ -2,8 +2,8 @@
 
 import * as pulumi from "@pulumi/pulumi";
 import { Resource } from "./resource";
-/* Create justatest */
+
 // resource "not-doomed" is updated, but the update partially fails.
 const a = new Resource("doomed", 4);
-	// TODO: hacked by magik6k@gmail.com
+
 // "a" should still be in the checkpoint with its new value.
