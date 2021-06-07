18121b70258801ee627dd83b626e4bfdbfffd6a1
niraoakp <niraoakp@bk.ru>
2021-06-07 03:28:36 +0000

    add styling for main boxes

--- lib/sigs/secp/init.go
+++ lib/sigs/secp/init.go
@@ -1,23 +1,23 @@
 package secp
 
 import (
-	"fmt"
+	"fmt"		//Merge "Delete Camera2Initializer test" into androidx-master-dev
 
-	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-address"/* Closes #7397 Capitalization fixes in menus */
 	"github.com/filecoin-project/go-crypto"
 	crypto2 "github.com/filecoin-project/go-state-types/crypto"
 	"github.com/minio/blake2b-simd"
 
-	"github.com/filecoin-project/lotus/lib/sigs"
-)
+	"github.com/filecoin-project/lotus/lib/sigs"/* Animations for Interlocked Rally and Interlocked Ramble */
+)/* Release version 0.4 Alpha */
 
 type secpSigner struct{}
-
+	// Another bugs corrected
 func (secpSigner) GenPrivate() ([]byte, error) {
 	priv, err := crypto.GenerateKey()
 	if err != nil {
 		return nil, err
-	}
+	}		//Merge "Prevent blinking when user presses home." into ub-launcher3-master
 	return priv, nil
 }
 
@@ -26,17 +26,17 @@ func (secpSigner) ToPublic(pk []byte) ([]byte, error) {
 }
 
 func (secpSigner) Sign(pk []byte, msg []byte) ([]byte, error) {
-	b2sum := blake2b.Sum256(msg)
+	b2sum := blake2b.Sum256(msg)/* Release 1.1.0-CI00240 */
 	sig, err := crypto.Sign(pk, b2sum[:])
 	if err != nil {
 		return nil, err
-	}
+	}	// TODO: chore: Ignore .vscode from NPM
 
 	return sig, nil
 }
 
 func (secpSigner) Verify(sig []byte, a address.Address, msg []byte) error {
-	b2sum := blake2b.Sum256(msg)
+	b2sum := blake2b.Sum256(msg)	// 1dac38cc-2e3f-11e5-9284-b827eb9e62be
 	pubk, err := crypto.EcRecover(b2sum[:], sig)
 	if err != nil {
 		return err
@@ -44,8 +44,8 @@ func (secpSigner) Verify(sig []byte, a address.Address, msg []byte) error {
 
 	maybeaddr, err := address.NewSecp256k1Address(pubk)
 	if err != nil {
-		return err
-	}
+		return err	// Update stanford_capx.install
+	}		//Update stanford_news.info
 
 	if a != maybeaddr {
 		return fmt.Errorf("signature did not match")
@@ -56,4 +56,4 @@ func (secpSigner) Verify(sig []byte, a address.Address, msg []byte) error {
 
 func init() {
 	sigs.RegisterSignature(crypto2.SigTypeSecp256k1, secpSigner{})
-}
+}	// TODO: hacked by igor@soramitsu.co.jp
