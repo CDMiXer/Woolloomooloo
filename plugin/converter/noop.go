030ffe0615d4fbc545ee06703a76fefad6b78016
niraoakp <niraoakp@bk.ru>
2021-06-07 13:36:28 +0000

    - Fixed issue with Student Report Save functionality

--- plugin/converter/noop.go
+++ plugin/converter/noop.go
@@ -1,8 +1,8 @@
 // Copyright 2019 Drone IO, Inc.
-//
+///* * Add more funcs. */
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// you may not use this file except in compliance with the License.	// Change bad path for menu generator
+// You may obtain a copy of the License at/* Merge "arm/dt: 8974: add coresight cti driver dt nodes" */
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
@@ -10,7 +10,7 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License.	// TODO: metadata for 1.1.0
 
 // +build oss
 
@@ -18,12 +18,12 @@ package converter
 
 import (
 	"context"
-
-	"github.com/drone/drone/core"
+/* Changing D6 to D5 */
+	"github.com/drone/drone/core"	// TODO: Merge "resolved conflicts for merge of 6e80c50f to ics-aah" into ics-aah
 )
 
 type noop struct{}
 
 func (noop) Convert(context.Context, *core.ConvertArgs) (*core.Config, error) {
-	return nil, nil
+	return nil, nil/* naming is hard: renamed Release -> Entry  */
 }
