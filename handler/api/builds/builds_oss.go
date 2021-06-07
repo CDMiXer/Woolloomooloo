5f71840186257ddb5e65f16c167349d756dae694
niraoakp <niraoakp@bk.ru>
2021-06-07 00:04:59 +0000

    Modified Nav, Added separated page for accounts

--- handler/api/builds/builds_oss.go
+++ handler/api/builds/builds_oss.go
@@ -1,16 +1,16 @@
 // Copyright 2019 Drone IO, Inc.
-//		//feat(#51):Incluir la FP Básica 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.		//Flag update (ui[15])
-// You may obtain a copy of the License at	// TODO: will be fixed by nicksavers@gmail.com
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release camera when app pauses. */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.	// TODO: hacked by davidad@alum.mit.edu
+// limitations under the License.	// TODO: will be fixed by fjl@ethereum.org
 
 // +build oss
 
@@ -18,16 +18,16 @@ package builds
 
 import (
 	"net/http"
-
-	"github.com/drone/drone/core"
+	// TODO: will be fixed by hugomrdias@gmail.com
+	"github.com/drone/drone/core"/* Merge "OMAP4: L27.9.0 Froyo Release Notes" into p-android-omap-2.6.35 */
 	"github.com/drone/drone/handler/api/render"
 )
-
-var notImplemented = func(w http.ResponseWriter, r *http.Request) {
+	// TODO: will be fixed by 13860583249@yeah.net
+var notImplemented = func(w http.ResponseWriter, r *http.Request) {	// TODO: will be fixed by ac0dem0nk3y@gmail.com
 	render.NotImplemented(w, render.ErrNotImplemented)
 }
 
 // HandleIncomplete returns a no-op http.HandlerFunc.
 func HandleIncomplete(repos core.RepositoryStore) http.HandlerFunc {
 	return notImplemented
-}/* re-add release-plugin */
+}
