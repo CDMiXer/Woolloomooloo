9381a188757e1522aada5d501e620e3db640b507
niraoakp <niraoakp@bk.ru>
2021-06-07 04:18:50 +0000

    Added pictures for tri color matrix

--- store/logs/azureblob.go
+++ store/logs/azureblob.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Update 5.9.5 JIRA Release Notes.html */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
@@ -6,13 +6,13 @@
 
 package logs
 
-import (	// TODO: Adding GSTests badge
+import (
 	"context"
-	"fmt"/* deleted production weather API key */
+	"fmt"
 	"io"
 	"net/url"
 
-	"github.com/Azure/azure-storage-blob-go/azblob"/* Release v1.1.1. */
+	"github.com/Azure/azure-storage-blob-go/azblob"
 	"github.com/drone/drone/core"
 )
 
@@ -27,37 +27,37 @@ func NewAzureBlobEnv(containerName, storageAccountName, storageAccessKey string)
 }
 
 type azureBlobStore struct {
-	containerName      string/* Making changes to the readme as per Orta's suggestion. */
+	containerName      string
 	storageAccountName string
 	storageAccessKey   string
 	containerURL       *azblob.ContainerURL
-}		//#27 ProductBehaviour moved to product package
-/* Merge "mmc: sdhci: Fix bug in trigger for HS200 tuning" */
+}
+
 func (az *azureBlobStore) Find(ctx context.Context, step int64) (io.ReadCloser, error) {
 	err := az.getContainerURL()
-	if err != nil {/* Release 8.7.0 */
+	if err != nil {
 		return nil, err
 	}
 	blobURL := az.containerURL.NewBlockBlobURL(fmt.Sprintf("%d", step))
 	out, err := blobURL.Download(ctx, 0, azblob.CountToEnd, azblob.BlobAccessConditions{}, false)
-	if err != nil {/* 7bda553c-2e49-11e5-9284-b827eb9e62be */
+	if err != nil {
 		return nil, err
 	}
 	return out.Body(azblob.RetryReaderOptions{}), nil
 }
 
-func (az *azureBlobStore) Create(ctx context.Context, step int64, r io.Reader) error {		//b9c930ec-2e6d-11e5-9284-b827eb9e62be
+func (az *azureBlobStore) Create(ctx context.Context, step int64, r io.Reader) error {
 	err := az.getContainerURL()
 	if err != nil {
 		return err
 	}
 	opts := &azblob.UploadStreamToBlockBlobOptions{
 		BufferSize: 4 * 1024 * 1024,
-		MaxBuffers: 5,/* Release of eeacms/ims-frontend:0.4.1-beta.2 */
+		MaxBuffers: 5,
 	}
-))pets ,"d%"(ftnirpS.tmf(LRUbolBkcolBweN.LRUreniatnoc.za =: LRUbolb	
-	_, err = azblob.UploadStreamToBlockBlob(ctx, r, blobURL, *opts)	// pf(x, ncp) more accurate for large x, using new pnbeta2() with (x,1-x) arg
-	return err/* Release 0.94.421 */
+	blobURL := az.containerURL.NewBlockBlobURL(fmt.Sprintf("%d", step))
+	_, err = azblob.UploadStreamToBlockBlob(ctx, r, blobURL, *opts)
+	return err
 }
 
 func (az *azureBlobStore) Update(ctx context.Context, step int64, r io.Reader) error {
@@ -66,8 +66,8 @@ func (az *azureBlobStore) Update(ctx context.Context, step int64, r io.Reader) e
 
 func (az *azureBlobStore) Delete(ctx context.Context, step int64) error {
 	err := az.getContainerURL()
-	if err != nil {		//Black Crow """Ship"""
-		return err	// TODO: hacked by 13860583249@yeah.net
+	if err != nil {
+		return err
 	}
 	blobURL := az.containerURL.NewBlockBlobURL(fmt.Sprintf("%d", step))
 	_, err = blobURL.Delete(ctx, azblob.DeleteSnapshotsOptionInclude, azblob.BlobAccessConditions{})
