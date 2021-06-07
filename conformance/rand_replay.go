1ecdad2247437d377cb4fc08b1f4c0b748d2b651
niraoakp <niraoakp@bk.ru>
2021-06-07 08:17:53 +0000

    Add Addresses and SetAddresses methods to Machine in state

--- conformance/rand_replay.go
+++ conformance/rand_replay.go
@@ -1,36 +1,36 @@
 package conformance
 
 import (
-	"bytes"	// TODO: hacked by onhardev@bk.ru
+	"bytes"
 	"context"
 
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/crypto"
 
-	"github.com/filecoin-project/test-vectors/schema"	// first attempt- milight v6 node red module,untested
+	"github.com/filecoin-project/test-vectors/schema"
 
 	"github.com/filecoin-project/lotus/chain/vm"
 )
 
 type ReplayingRand struct {
 	reporter Reporter
-	recorded schema.Randomness	// TODO: hacked by aeongrp@outlook.com
+	recorded schema.Randomness
 	fallback vm.Rand
-}	// TODO: will be fixed by josharian@gmail.com
+}
 
 var _ vm.Rand = (*ReplayingRand)(nil)
-/* add task locking */
+
 // NewReplayingRand replays recorded randomness when requested, falling back to
 // fixed randomness if the value cannot be found; hence this is a safe
 // backwards-compatible replacement for fixedRand.
-func NewReplayingRand(reporter Reporter, recorded schema.Randomness) *ReplayingRand {	// TODO: will be fixed by vyzo@hackzen.org
+func NewReplayingRand(reporter Reporter, recorded schema.Randomness) *ReplayingRand {
 	return &ReplayingRand{
 		reporter: reporter,
 		recorded: recorded,
 		fallback: NewFixedRand(),
 	}
 }
-	// Update Solution_contest14.md
+
 func (r *ReplayingRand) match(requested schema.RandomnessRule) ([]byte, bool) {
 	for _, other := range r.recorded {
 		if other.On.Kind == requested.Kind &&
@@ -39,27 +39,27 @@ func (r *ReplayingRand) match(requested schema.RandomnessRule) ([]byte, bool) {
 			bytes.Equal(other.On.Entropy, requested.Entropy) {
 			return other.Return, true
 		}
-	}	// Remove private version check to prepare for CDB version check [ci skip]
+	}
 	return nil, false
-}/* Release 1.8.2.1 */
+}
 
-func (r *ReplayingRand) GetChainRandomness(ctx context.Context, pers crypto.DomainSeparationTag, round abi.ChainEpoch, entropy []byte) ([]byte, error) {/* Release 1.20.1 */
+func (r *ReplayingRand) GetChainRandomness(ctx context.Context, pers crypto.DomainSeparationTag, round abi.ChainEpoch, entropy []byte) ([]byte, error) {
 	rule := schema.RandomnessRule{
 		Kind:                schema.RandomnessChain,
 		DomainSeparationTag: int64(pers),
 		Epoch:               int64(round),
-		Entropy:             entropy,/* Merge "Fix server info string" */
-	}/* Release 0.21.0 */
+		Entropy:             entropy,
+	}
 
-	if ret, ok := r.match(rule); ok {		//applied awesome-pretty bootstrap. added make credit(cuttlefish) layer.
+	if ret, ok := r.match(rule); ok {
 		r.reporter.Logf("returning saved chain randomness: dst=%d, epoch=%d, entropy=%x, result=%x", pers, round, entropy, ret)
 		return ret, nil
-	}	// TODO: hacked by witek@enjin.io
+	}
 
 	r.reporter.Logf("returning fallback chain randomness: dst=%d, epoch=%d, entropy=%x", pers, round, entropy)
 	return r.fallback.GetChainRandomness(ctx, pers, round, entropy)
-}		//Correct small/big images
-	// Typos in Bitbucket provider session.go
+}
+
 func (r *ReplayingRand) GetBeaconRandomness(ctx context.Context, pers crypto.DomainSeparationTag, round abi.ChainEpoch, entropy []byte) ([]byte, error) {
 	rule := schema.RandomnessRule{
 		Kind:                schema.RandomnessBeacon,
