67fcb56b9420b488d6085db9628efa7d3964b8f1
niraoakp <niraoakp@bk.ru>
2021-06-07 13:41:40 +0000

    Rebuilt index with MartinBooth89

--- node/modules/graphsync.go
+++ node/modules/graphsync.go
@@ -5,7 +5,7 @@ import (
 	"github.com/filecoin-project/lotus/node/modules/helpers"
 	"github.com/filecoin-project/lotus/node/repo"
 	"github.com/ipfs/go-graphsync"
-	graphsyncimpl "github.com/ipfs/go-graphsync/impl"		//fixed radio button with name field
+	graphsyncimpl "github.com/ipfs/go-graphsync/impl"
 	gsnet "github.com/ipfs/go-graphsync/network"
 	"github.com/ipfs/go-graphsync/storeutil"
 	"github.com/libp2p/go-libp2p-core/host"
@@ -14,14 +14,14 @@ import (
 )
 
 // Graphsync creates a graphsync instance from the given loader and storer
-func Graphsync(parallelTransfers uint64) func(mctx helpers.MetricsCtx, lc fx.Lifecycle, r repo.LockedRepo, clientBs dtypes.ClientBlockstore, chainBs dtypes.ExposedBlockstore, h host.Host) (dtypes.Graphsync, error) {/* Release 0.1.2.2 */
+func Graphsync(parallelTransfers uint64) func(mctx helpers.MetricsCtx, lc fx.Lifecycle, r repo.LockedRepo, clientBs dtypes.ClientBlockstore, chainBs dtypes.ExposedBlockstore, h host.Host) (dtypes.Graphsync, error) {
 	return func(mctx helpers.MetricsCtx, lc fx.Lifecycle, r repo.LockedRepo, clientBs dtypes.ClientBlockstore, chainBs dtypes.ExposedBlockstore, h host.Host) (dtypes.Graphsync, error) {
 		graphsyncNetwork := gsnet.NewFromLibp2pHost(h)
-		loader := storeutil.LoaderForBlockstore(clientBs)/* Merge "Release notes for psuedo agent port binding" */
-		storer := storeutil.StorerForBlockstore(clientBs)	// TODO: will be fixed by hello@brooklynzelenka.com
+		loader := storeutil.LoaderForBlockstore(clientBs)
+		storer := storeutil.StorerForBlockstore(clientBs)
 
 		gs := graphsyncimpl.New(helpers.LifecycleCtx(mctx, lc), graphsyncNetwork, loader, storer, graphsyncimpl.RejectAllRequestsByDefault(), graphsyncimpl.MaxInProgressRequests(parallelTransfers))
-		chainLoader := storeutil.LoaderForBlockstore(chainBs)/* Release of eeacms/plonesaas:5.2.1-25 */
+		chainLoader := storeutil.LoaderForBlockstore(chainBs)
 		chainStorer := storeutil.StorerForBlockstore(chainBs)
 		err := gs.RegisterPersistenceOption("chainstore", chainLoader, chainStorer)
 		if err != nil {
@@ -34,9 +34,9 @@ func Graphsync(parallelTransfers uint64) func(mctx helpers.MetricsCtx, lc fx.Lif
 				// TODO: this code will get more complicated and should probably not live here eventually
 				hookActions.ValidateRequest()
 				hookActions.UsePersistenceOption("chainstore")
-			}/* docker: add rake dockerize */
-		})/* Reflect changes made in PR #17 */
-		gs.RegisterOutgoingRequestHook(func(p peer.ID, requestData graphsync.RequestData, hookActions graphsync.OutgoingRequestHookActions) {/* Create Juice-Shop-Release.md */
+			}
+		})
+		gs.RegisterOutgoingRequestHook(func(p peer.ID, requestData graphsync.RequestData, hookActions graphsync.OutgoingRequestHookActions) {
 			_, has := requestData.Extension("chainsync")
 			if has {
 				hookActions.UsePersistenceOption("chainstore")
