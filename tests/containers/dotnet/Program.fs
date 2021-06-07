b5e3fef33c8f10c617b4f2a9c9c30825d6de55b8
niraoakp <niraoakp@bk.ru>
2021-06-07 06:39:04 +0000

    Add simple watching to documentation

--- tests/containers/dotnet/Program.fs
+++ tests/containers/dotnet/Program.fs
@@ -1,16 +1,16 @@
-﻿module Program/* Release of eeacms/www-devel:20.1.11 */
+﻿module Program
 
 open System
-open Pulumi.FSharp/* Still bug fixing ReleaseID lookups. */
-		//Added some documentation of what is supported on the DFXP format 
+open Pulumi.FSharp
+
 let infra () =
   let config = new Pulumi.Config()
   let runtime = config.Get("runtime")
   Console.WriteLine("Hello from {0}", runtime)
-  	// TODO: hacked by mikeal.rogers@gmail.com
+  
   // Stack outputs
   dict []
 
 [<EntryPoint>]
-let main _ =	// Add filtering to get divisions [ci skip]
+let main _ =
   Deployment.run infra
