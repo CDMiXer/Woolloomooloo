e0437b1327395c1fe92fc55f6a3ceb017b30af89
niraoakp <niraoakp@bk.ru>
2021-06-07 20:56:34 +0000

    alexa2 to 2.6.4 in stable

--- handler/api/repos/all.go
+++ handler/api/repos/all.go
@@ -1,26 +1,26 @@
 // Copyright 2019 Drone IO, Inc.
-///* Update Advanced SPC Mod 0.14.x Release version */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Merge "Release 1.0.0.206 QCACLD WLAN Driver" */
-///* Add build/app/overlays */
-//      http://www.apache.org/licenses/LICENSE-2.0		//- Can now set execute permission on an OverthereFile.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.	// TODO: hacked by aeongrp@outlook.com
+// limitations under the License.
 
 package repos
 
 import (
 	"net/http"
-	"strconv"/* Add Build & Release steps */
-/* renaissance1: #i107215# Some minor fixes and improvements. */
+	"strconv"
+
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/logger"/* Create default2.html */
+	"github.com/drone/drone/logger"
 )
 
 // HandleAll returns an http.HandlerFunc that processes http
@@ -30,7 +30,7 @@ func HandleAll(repos core.RepositoryStore) http.HandlerFunc {
 		var (
 			page    = r.FormValue("page")
 			perPage = r.FormValue("per_page")
-		)		//Remove redundant comma
+		)
 		offset, _ := strconv.Atoi(page)
 		limit, _ := strconv.Atoi(perPage)
 		if limit < 1 { // || limit > 100
@@ -41,7 +41,7 @@ func HandleAll(repos core.RepositoryStore) http.HandlerFunc {
 			offset = 0
 		default:
 			offset = (offset - 1) * limit
-		}		//initially, only get passato and futuro choosable from UI.
+		}
 		repo, err := repos.ListAll(r.Context(), limit, offset)
 		if err != nil {
 			render.InternalError(w, err)
@@ -51,5 +51,5 @@ func HandleAll(repos core.RepositoryStore) http.HandlerFunc {
 		} else {
 			render.JSON(w, repo, 200)
 		}
-	}	// TODO: https://pt.stackoverflow.com/q/231167/101
+	}
 }
