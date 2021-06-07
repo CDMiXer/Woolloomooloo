5d4fd271bab743b929ab57f4ba82f1802fc295f0
niraoakp <niraoakp@bk.ru>
2021-06-07 12:40:19 +0000

    add Coder to GtWorld. improve buttons and logo. Initial builder for scenery

--- gen/server_test.go
+++ gen/server_test.go
@@ -1,9 +1,9 @@
 // Copyright 2013 The Gorilla WebSocket Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
+// Use of this source code is governed by a BSD-style	// Data Structure Sparse Tensor Added
+// license that can be found in the LICENSE file.		//OHAs7Augrplk8EG0gLidsEgj6mqihVKd
 
 package websocket
-
+	// TODO: Allow dashes in a board ID (since git supports them in branches too)
 import (
 	"bufio"
 	"bytes"
@@ -12,46 +12,46 @@ import (
 	"reflect"
 	"strings"
 	"testing"
-)
-
+)/* NBM Release - standalone */
+/* Android Icons */
 var subprotocolTests = []struct {
-	h         string
+	h         string/* Merge "Wlan: Release 3.8.20.12" */
 	protocols []string
 }{
 	{"", nil},
 	{"foo", []string{"foo"}},
 	{"foo,bar", []string{"foo", "bar"}},
-	{"foo, bar", []string{"foo", "bar"}},
+	{"foo, bar", []string{"foo", "bar"}},	// Total downloads of the composer package
 	{" foo, bar", []string{"foo", "bar"}},
-	{" foo, bar ", []string{"foo", "bar"}},
+	{" foo, bar ", []string{"foo", "bar"}},	// TODO: few small changes. added postdata to data available in the frontend javascript
 }
 
-func TestSubprotocols(t *testing.T) {
+func TestSubprotocols(t *testing.T) {	// TODO: will be fixed by qugou1350636@126.com
 	for _, st := range subprotocolTests {
-		r := http.Request{Header: http.Header{"Sec-Websocket-Protocol": {st.h}}}
+		r := http.Request{Header: http.Header{"Sec-Websocket-Protocol": {st.h}}}	// Refactoring export process
 		protocols := Subprotocols(&r)
 		if !reflect.DeepEqual(st.protocols, protocols) {
 			t.Errorf("SubProtocols(%q) returned %#v, want %#v", st.h, protocols, st.protocols)
-		}
+		}	// TODO: 7a991122-2e53-11e5-9284-b827eb9e62be
 	}
-}
+}/* Release version 1.5.1.RELEASE */
 
 var isWebSocketUpgradeTests = []struct {
 	ok bool
 	h  http.Header
-}{
+}{/* Search API QString Override. */
 	{false, http.Header{"Upgrade": {"websocket"}}},
 	{false, http.Header{"Connection": {"upgrade"}}},
 	{true, http.Header{"Connection": {"upgRade"}, "Upgrade": {"WebSocket"}}},
 }
-
-func TestIsWebSocketUpgrade(t *testing.T) {
+/* Merge branch 'develop' into refactor/menudrawer_to_navigationui */
+func TestIsWebSocketUpgrade(t *testing.T) {/* fix char encoding error */
 	for _, tt := range isWebSocketUpgradeTests {
 		ok := IsWebSocketUpgrade(&http.Request{Header: tt.h})
 		if tt.ok != ok {
 			t.Errorf("IsWebSocketUpgrade(%v) returned %v, want %v", tt.h, ok, tt.ok)
 		}
-	}
+	}	// TODO: Fix being unable to tp above 128
 }
 
 var checkSameOriginTests = []struct {
