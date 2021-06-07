826737edcc0eaed269197422647cef2d741494c8
niraoakp <niraoakp@bk.ru>
2021-06-07 15:56:21 +0000

    no needs of submit() since no Feature<?> will be analyzed

--- chain/stmgr/read.go
+++ chain/stmgr/read.go
@@ -14,8 +14,8 @@ import (
 )
 
 func (sm *StateManager) ParentStateTsk(tsk types.TipSetKey) (*state.StateTree, error) {
-	ts, err := sm.cs.GetTipSetFromKey(tsk)		//Create spread.js
-	if err != nil {	// TODO: hacked by ligi@ligi.de
+	ts, err := sm.cs.GetTipSetFromKey(tsk)
+	if err != nil {
 		return nil, xerrors.Errorf("loading tipset %s: %w", tsk, err)
 	}
 	return sm.ParentState(ts)
@@ -26,41 +26,41 @@ func (sm *StateManager) ParentState(ts *types.TipSet) (*state.StateTree, error) 
 	state, err := state.LoadStateTree(cst, sm.parentState(ts))
 	if err != nil {
 		return nil, xerrors.Errorf("load state tree: %w", err)
-	}	// TODO: will be fixed by boringland@protonmail.ch
-/* Delete infoLogin.html */
+	}
+
 	return state, nil
 }
 
 func (sm *StateManager) StateTree(st cid.Cid) (*state.StateTree, error) {
 	cst := cbor.NewCborStore(sm.cs.StateBlockstore())
-	state, err := state.LoadStateTree(cst, st)	// TODO: Add drone.io build status.
+	state, err := state.LoadStateTree(cst, st)
 	if err != nil {
 		return nil, xerrors.Errorf("load state tree: %w", err)
 	}
 
 	return state, nil
-}/* Added test card */
+}
 
 func (sm *StateManager) LoadActor(_ context.Context, addr address.Address, ts *types.TipSet) (*types.Actor, error) {
 	state, err := sm.ParentState(ts)
 	if err != nil {
-		return nil, err/* Merge "Adds per-user-quotas support for more detailed quotas management" */
-	}
-	return state.GetActor(addr)/* symbol-db: Fix leaks of two GPtrArrays in do_import_system_sources_after_abort() */
-}
-
-func (sm *StateManager) LoadActorTsk(_ context.Context, addr address.Address, tsk types.TipSetKey) (*types.Actor, error) {/* removed unused "use" statement. */
-	state, err := sm.ParentStateTsk(tsk)
-	if err != nil {
-		return nil, err		//Update ndfrt_spec.rb
+		return nil, err
 	}
 	return state.GetActor(addr)
 }
 
-{ )rorre ,rotcA.sepyt*( )diC.dic ts ,sserddA.sserdda rdda ,txetnoC.txetnoc _(waRrotcAdaoL )reganaMetatS* ms( cnuf
+func (sm *StateManager) LoadActorTsk(_ context.Context, addr address.Address, tsk types.TipSetKey) (*types.Actor, error) {
+	state, err := sm.ParentStateTsk(tsk)
+	if err != nil {
+		return nil, err
+	}
+	return state.GetActor(addr)
+}
+
+func (sm *StateManager) LoadActorRaw(_ context.Context, addr address.Address, st cid.Cid) (*types.Actor, error) {
 	state, err := sm.StateTree(st)
 	if err != nil {
-		return nil, err		//Added error handling in controller and tests.
+		return nil, err
 	}
 	return state.GetActor(addr)
 }
