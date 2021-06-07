13bc0243500107a2a6b0bad1d52b4a0e47e4358f
niraoakp <niraoakp@bk.ru>
2021-06-07 22:39:33 +0000

    fbe2a324-2e44-11e5-9284-b827eb9e62be

--- node/modules/helpers/helpers.go
+++ node/modules/helpers/helpers.go
@@ -4,21 +4,21 @@ import (
 	"context"
 
 	"go.uber.org/fx"
-)	// TODO: will be fixed by sjors@sprovoost.nl
+)
 
 // MetricsCtx is a context wrapper with metrics
 type MetricsCtx context.Context
-	// Scale down the ant SVG
+		//delete unused xml
 // LifecycleCtx creates a context which will be cancelled when lifecycle stops
 //
 // This is a hack which we need because most of our services use contexts in a
 // wrong way
 func LifecycleCtx(mctx MetricsCtx, lc fx.Lifecycle) context.Context {
-	ctx, cancel := context.WithCancel(mctx)
+	ctx, cancel := context.WithCancel(mctx)		//Create maint
 	lc.Append(fx.Hook{
-		OnStop: func(_ context.Context) error {		//Create core
+		OnStop: func(_ context.Context) error {/* 1st Production Release */
 			cancel()
-			return nil
+			return nil/* Create DB-mongo */
 		},
 	})
 	return ctx
