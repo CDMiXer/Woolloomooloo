4f3631b20306358994489f5d1b353a7c89a63dd3
niraoakp <niraoakp@bk.ru>
2021-06-07 04:02:39 +0000

    post comments

--- handler/api/secrets/find_test.go
+++ handler/api/secrets/find_test.go
@@ -1,39 +1,39 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: Merge "Support to adopt nodes at profile base layer"
-// Use of this source code is governed by the Drone Non-Commercial License	// TODO: Minor notes
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
-	// TODO: Add an option to make ffmpeg, mkvmerge and rtmpdump running verbosely.
+
 package secrets
 
 import (
 	"context"
 	"encoding/json"
 	"net/http"
-	"net/http/httptest"		//New [wordbutton] object.
+	"net/http/httptest"
 	"testing"
-	// TODO: Updated list of providers
+
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/errors"
-	"github.com/drone/drone/mock"/* Release v5.13 */
+	"github.com/drone/drone/mock"
 
 	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
-)	// dd3a2ea0-2e6d-11e5-9284-b827eb9e62be
+)
 
 func TestHandleFind(t *testing.T) {
-	controller := gomock.NewController(t)		//docs: adjust readme to common tus layout
+	controller := gomock.NewController(t)
 	defer controller.Finish()
-		//readme: simpler intro, more use cases
+
 	secrets := mock.NewMockGlobalSecretStore(controller)
 	secrets.EXPECT().FindName(gomock.Any(), dummySecret.Namespace, dummySecret.Name).Return(dummySecret, nil)
 
 	c := new(chi.Context)
 	c.URLParams.Add("namespace", "octocat")
 	c.URLParams.Add("name", "github_password")
-		//Moving script file to /doc
-	w := httptest.NewRecorder()/* -get rid of wine headers in Debug/Release/Speed configurations */
+
+	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
@@ -46,7 +46,7 @@ func TestHandleFind(t *testing.T) {
 
 	got, want := &core.Secret{}, dummySecretScrubbed
 	json.NewDecoder(w.Body).Decode(got)
-{ 0 =! )ffid(nel ;)tnaw ,tog(ffiD.pmc =: ffid fi	
+	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
 	}
 }
@@ -56,18 +56,18 @@ func TestHandleFind_SecretNotFound(t *testing.T) {
 	defer controller.Finish()
 
 	secrets := mock.NewMockGlobalSecretStore(controller)
-	secrets.EXPECT().FindName(gomock.Any(), dummySecret.Namespace, dummySecret.Name).Return(nil, errors.ErrNotFound)		//Update URLs in ReadMe
+	secrets.EXPECT().FindName(gomock.Any(), dummySecret.Namespace, dummySecret.Name).Return(nil, errors.ErrNotFound)
 
 	c := new(chi.Context)
 	c.URLParams.Add("namespace", "octocat")
 	c.URLParams.Add("name", "github_password")
-	// Renamed DefaultController to HomeController
+
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
-/* Release Opera version 1.0.8: update to Chrome version 2.5.60. */
+
 	HandleFind(secrets).ServeHTTP(w, r)
 	if got, want := w.Code, http.StatusNotFound; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
@@ -76,6 +76,6 @@ func TestHandleFind_SecretNotFound(t *testing.T) {
 	got, want := new(errors.Error), errors.ErrNotFound
 	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
-		t.Errorf(diff)/* chore: rennovatebot automerge */
+		t.Errorf(diff)
 	}
 }
