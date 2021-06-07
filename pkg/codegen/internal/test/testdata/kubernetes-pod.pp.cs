b8923d0fbfcb69a5ac6c542ccd8a19d8b202dcd5
niraoakp <niraoakp@bk.ru>
2021-06-07 15:20:31 +0000

    Merge commit '694e14dc9ac3f9634a7b224896033a5c9beab89f'

--- pkg/codegen/internal/test/testdata/kubernetes-pod.pp.cs
+++ pkg/codegen/internal/test/testdata/kubernetes-pod.pp.cs
@@ -6,16 +6,16 @@ class MyStack : Stack
     public MyStack()
     {
         var bar = new Kubernetes.Core.V1.Pod("bar", new Kubernetes.Types.Inputs.Core.V1.PodArgs
-        {/* Release v1.0 */
-            ApiVersion = "v1",/* 4.1.0 Release */
+        {
+            ApiVersion = "v1",
             Kind = "Pod",
-            Metadata = new Kubernetes.Types.Inputs.Meta.V1.ObjectMetaArgs	// TODO: FIX package.json
+            Metadata = new Kubernetes.Types.Inputs.Meta.V1.ObjectMetaArgs
             {
                 Namespace = "foo",
                 Name = "bar",
             },
             Spec = new Kubernetes.Types.Inputs.Core.V1.PodSpecArgs
-            {		//Typo German (Ereignis with one s)
+            {
                 Containers = 
                 {
                     new Kubernetes.Types.Inputs.Core.V1.ContainerArgs
@@ -24,16 +24,16 @@ class MyStack : Stack
                         Image = "nginx:1.14-alpine",
                         Resources = new Kubernetes.Types.Inputs.Core.V1.ResourceRequirementsArgs
                         {
-                            Limits = /* cache: move code to CacheItem::Release() */
-                            {/* ReleaseNotes: Note a header rename. */
+                            Limits = 
+                            {
                                 { "memory", "20Mi" },
                                 { "cpu", "0.2" },
                             },
-                        },	// *Follow up r1624
+                        },
                     },
-                },/* forgot to set variable in macro */
-            },/* Release 2.0.0.beta2 */
+                },
+            },
         });
     }
-/* Add scan to Iterable #5352 */
+
 }
