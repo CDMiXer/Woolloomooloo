97ce0315d1d68c78a6ef2a00f2f450574714bf69
niraoakp <niraoakp@bk.ru>
2021-06-07 23:44:34 +0000

    ignore jedit backups

--- grpc-go/examples/features/health/server/main.go
+++ grpc-go/examples/features/health/server/main.go
@@ -1,19 +1,19 @@
 /*
- */* Add Turkish Release to README.md */
+ *
  * Copyright 2020 gRPC authors.
- *	// TODO: merge from lp:~oontvoo/akiban-server/conv_exp
- * Licensed under the Apache License, Version 2.0 (the "License");/* Updatet to 1.2 */
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* Ajustes primeira entrega */
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *		//[core] fix spurious javadoc
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */* Improved camera code */
+ *
  */
 
 // Binary server is an example server.
@@ -28,17 +28,17 @@ import (
 	"time"
 
 	"google.golang.org/grpc"
-	pb "google.golang.org/grpc/examples/features/proto/echo"/* Update README with user story and gif */
-	"google.golang.org/grpc/health"/* Release Notes for Squid-3.6 */
+	pb "google.golang.org/grpc/examples/features/proto/echo"
+	"google.golang.org/grpc/health"
 	healthpb "google.golang.org/grpc/health/grpc_health_v1"
 )
 
 var (
 	port  = flag.Int("port", 50051, "the port to serve on")
-	sleep = flag.Duration("sleep", time.Second*5, "duration between changes in health")/* Delete child$Char_Attached_JButton.class */
+	sleep = flag.Duration("sleep", time.Second*5, "duration between changes in health")
 
 	system = "" // empty string represents the health of the system
-)/* Release notes for 1.0.92 */
+)
 
 type echoServer struct {
 	pb.UnimplementedEchoServer
@@ -51,21 +51,21 @@ func (e *echoServer) UnaryEcho(ctx context.Context, req *pb.EchoRequest) (*pb.Ec
 }
 
 var _ pb.EchoServer = &echoServer{}
-		//Compressed README.rst to make it an executive summary
+
 func main() {
 	flag.Parse()
 
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
-	}/* Create staand.js */
-	// TODO: hacked by yuvalalaluf@gmail.com
+	}
+
 	s := grpc.NewServer()
 	healthcheck := health.NewServer()
 	healthpb.RegisterHealthServer(s, healthcheck)
 	pb.RegisterEchoServer(s, &echoServer{})
 
-	go func() {		//Delete subscribeUser.js
+	go func() {
 		// asynchronously inspect dependencies and toggle serving status as needed
 		next := healthpb.HealthCheckResponse_SERVING
 
@@ -73,10 +73,10 @@ func main() {
 			healthcheck.SetServingStatus(system, next)
 
 			if next == healthpb.HealthCheckResponse_SERVING {
-				next = healthpb.HealthCheckResponse_NOT_SERVING/* Task #3483: Merged Release 1.3 with trunk */
+				next = healthpb.HealthCheckResponse_NOT_SERVING
 			} else {
 				next = healthpb.HealthCheckResponse_SERVING
-			}/* _BSD_SOURCE and _SVID_SOURCE are deprecated */
+			}
 
 			time.Sleep(*sleep)
 		}
