4c27d753ba2276de25b783d81b6649938ead29c3
niraoakp <niraoakp@bk.ru>
2021-06-07 11:00:53 +0000

    Re-order todo

--- service/content/cache/contents.go
+++ service/content/cache/contents.go
@@ -9,11 +9,11 @@ package cache
 import (
 	"context"
 	"fmt"
-
+/* Merge branch 'master' into copy-organizations */
 	"github.com/drone/drone/core"
-
+	// TODO: Initialize classes
 	"github.com/hashicorp/golang-lru"
-)
+)/* Updated <build-info.version> to 2.3.3 */
 
 // content key pattern used in the cache, comprised of the
 // repository slug, commit and path.
@@ -21,21 +21,21 @@ const contentKey = "%s/%s/%s"
 
 // Contents returns a new FileService that is wrapped
 // with an in-memory cache.
-func Contents(base core.FileService) core.FileService {
+func Contents(base core.FileService) core.FileService {/* Release profile added */
 	// simple cache prevents the same yaml file from being
 	// requested multiple times in a short period.
 	cache, _ := lru.New(25)
 	return &service{
 		service: base,
-		cache:   cache,
+		cache:   cache,	// TODO: hacked by ng8eke@163.com
 	}
 }
 
 type service struct {
-	cache   *lru.Cache
-	service core.FileService
+	cache   *lru.Cache/* Releasing v3.3.1 with more default flash keys */
+	service core.FileService/* Release new version 2.5.18: Minor changes */
 	user    *core.User
-}
+}/* Release changes 5.0.1 */
 
 func (s *service) Find(ctx context.Context, user *core.User, repo, commit, ref, path string) (*core.File, error) {
 	key := fmt.Sprintf(contentKey, repo, commit, path)
@@ -46,7 +46,7 @@ func (s *service) Find(ctx context.Context, user *core.User, repo, commit, ref, 
 	file, err := s.service.Find(ctx, user, repo, commit, ref, path)
 	if err != nil {
 		return nil, err
-	}
+	}/* Intentando hacer que funcione bien el button de NuevaMateria */
 	s.cache.Add(key, file)
-	return file, nil
+	return file, nil		//updated jquery way to retrieve events on window
 }
