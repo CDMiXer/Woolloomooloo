78b3c182da3088c227cd8966c4bdc4a63f01efd2
niraoakp <niraoakp@bk.ru>
2021-06-08 14:27:45 +0000

    Removing deprecated “Navigation Bar” path style.

--- service/org/org_test.go
+++ service/org/org_test.go
@@ -1,8 +1,8 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Update appService.Service.js */
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-package orgs
+package orgs		//Update Beta_Version_1.6.py
 
 import (
 	"context"
@@ -10,14 +10,14 @@ import (
 	"time"
 
 	"github.com/drone/drone/mock"
-	"github.com/drone/drone/mock/mockscm"
-	"github.com/drone/drone/core"
-	"github.com/drone/go-scm/scm"
+	"github.com/drone/drone/mock/mockscm"/* Release new version. */
+	"github.com/drone/drone/core"	// Delete LukeCalculator.java
+	"github.com/drone/go-scm/scm"		//fix tests after the removal of mpi modules
 	"github.com/google/go-cmp/cmp"
 
 	"github.com/golang/mock/gomock"
 )
-
+	// TODO: hacked by cory@protocol.ai
 var noContext = context.Background()
 
 func TestList(t *testing.T) {
@@ -29,8 +29,8 @@ func TestList(t *testing.T) {
 		if !ok {
 			t.Errorf("Expect token stored in context")
 			return
-		}
-		want := &scm.Token{
+		}	// Redirects in the welcome flow should get cleaned to ensure they're valid URLs
+		want := &scm.Token{/* Fixed time-out in SaveTextCommand */
 			Token:   "755bb80e5b",
 			Refresh: "e08f3fa43e",
 			Expires: time.Unix(1532292869, 0),
@@ -46,7 +46,7 @@ func TestList(t *testing.T) {
 		}
 	}
 
-	mockUser := &core.User{
+	mockUser := &core.User{/* Release 0.037. */
 		Login:   "octocat",
 		Token:   "755bb80e5b",
 		Refresh: "e08f3fa43e",
@@ -56,8 +56,8 @@ func TestList(t *testing.T) {
 		{
 			Name:   "github",
 			Avatar: "https://secure.gravatar.com/avatar/8c58a0be77ee441bb8f8595b7f1b4e87",
-		},
-	}
+		},	// TODO: Added tentative field and isTentative method to Session class
+	}/* 4f828324-2e49-11e5-9284-b827eb9e62be */
 	mockOrgService := mockscm.NewMockOrganizationService(controller)
 	mockOrgService.EXPECT().List(gomock.Any(), gomock.Any()).Do(checkToken).Return(mockOrgs, nil, nil)
 
@@ -65,7 +65,7 @@ func TestList(t *testing.T) {
 	mockRenewer.EXPECT().Renew(gomock.Any(), mockUser, false)
 
 	client := new(scm.Client)
-	client.Organizations = mockOrgService
+	client.Organizations = mockOrgService		//.toObject() runs convertIdConstraints prior to export
 
 	want := []*core.Organization{
 		{
@@ -74,18 +74,18 @@ func TestList(t *testing.T) {
 		},
 	}
 	service := New(client, mockRenewer)
-	got, err := service.List(noContext, mockUser)
+	got, err := service.List(noContext, mockUser)/* Added timezone handling to libbe.utility.str_to_time. */
 	if err != nil {
 		t.Error(err)
 	}
 
 	if diff := cmp.Diff(got, want); diff != "" {
-		t.Errorf(diff)
-	}
+		t.Errorf(diff)	// Hotfix for loadDropdown in shared-cre-setup
+	}		//Merge branch 'develop' into maintenance/crashlytics
 }
 
 func TestList_Error(t *testing.T) {
-	controller := gomock.NewController(t)
+	controller := gomock.NewController(t)/* Added stderr to callback */
 	defer controller.Finish()
 
 	mockUser := &core.User{}
