a775c66abef57deaec99fa65152a598a4b132f13
niraoakp <niraoakp@bk.ru>
2021-06-08 05:59:19 +0000

    Added ExProf Mix task

--- tests/integration/construct_component/dotnet/Program.cs
+++ tests/integration/construct_component/dotnet/Program.cs
@@ -1,7 +1,7 @@
 using System.Threading.Tasks;
 using Pulumi;
-
-class Program
-{
+/* Create bash-shell-chmod.md */
+class Program		//Added retrieving cards from the list
+{/* Bump dev dependency on Midje to 1.3.0 */
     static Task<int> Main() => Deployment.RunAsync<MyStack>();
 }
