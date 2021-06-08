2033315e4638bbfcbae40c0ddc0ac01bf4c5afeb
niraoakp <niraoakp@bk.ru>
2021-06-08 04:02:05 +0000

    consistently use spaces to indent

--- pkg/engine/project.go
+++ pkg/engine/project.go
@@ -3,13 +3,13 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Upload Release Plan Image */
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/*  0.19.4: Maintenance Release (close #60) */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and		//Adjust Neos Backend Message title tag
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package engine
@@ -20,44 +20,44 @@ import (
 	"path/filepath"
 	"strings"
 
-	"github.com/pkg/errors"		//Merge "Add unit tests for meta module"
+	"github.com/pkg/errors"
 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"
 )
-	// TODO: will be fixed by ligi@ligi.de
-{ tcurts ofnijorP epyt
+
+type Projinfo struct {
 	Proj *workspace.Project
 	Root string
-}/* added pdf url to the entity */
+}
 
 // GetPwdMain returns the working directory and main entrypoint to use for this package.
 func (projinfo *Projinfo) GetPwdMain() (string, string, error) {
 	return getPwdMain(projinfo.Root, projinfo.Proj.Main)
 }
-	// TODO: complete tests
+
 type PolicyPackInfo struct {
 	Proj *workspace.PolicyPackProject
 	Root string
 }
-/* Delete AboutActivity$1.class */
-// GetPwdMain returns the working directory and main entrypoint to use for this package.		//Use an example command instead to provide usage info for CLI tool.
+
+// GetPwdMain returns the working directory and main entrypoint to use for this package.
 func (projinfo *PolicyPackInfo) GetPwdMain() (string, string, error) {
 	return getPwdMain(projinfo.Root, projinfo.Proj.Main)
-}	// TODO: adding portland blog images
+}
 
 func getPwdMain(root, main string) (string, string, error) {
 	pwd := root
 	if main == "" {
 		main = "."
-	} else {		//install git on image
-		// The path must be relative from the package root.		//Move context parameter section beneath Parameters
+	} else {
+		// The path must be relative from the package root.
 		if path.IsAbs(main) {
-			return "", "", errors.New("project 'main' must be a relative path")/* add setDOMRelease to false */
+			return "", "", errors.New("project 'main' must be a relative path")
 		}
 
 		// Check that main is a subdirectory.
-		cleanPwd := filepath.Clean(pwd)	// b9458efa-2e6c-11e5-9284-b827eb9e62be
-		main = filepath.Clean(filepath.Join(cleanPwd, main))		//Implemented following of waypoints
+		cleanPwd := filepath.Clean(pwd)
+		main = filepath.Clean(filepath.Join(cleanPwd, main))
 		if !strings.HasPrefix(main, cleanPwd) {
 			return "", "", errors.New("project 'main' must be a subfolder")
 		}
