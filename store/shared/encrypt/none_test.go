50dd92cc9e0733524dac215a965ca0a5edfcdbac
niraoakp <niraoakp@bk.ru>
2021-06-08 06:25:09 +0000

    add BDSKMacro.[hm] to project

--- store/shared/encrypt/none_test.go
+++ store/shared/encrypt/none_test.go
@@ -1,10 +1,10 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: Cleaned up some faulty old tests, fixed dummy applications asset pipeline
-// Use of this source code is governed by the Drone Non-Commercial License/* adding a BSD 3-Clause license */
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Release = Backfire, closes #7049 */
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 package encrypt
 
-import "testing"	// TODO: will be fixed by martin2cai@hotmail.com
+import "testing"
 
 func TestNone(t *testing.T) {
 	n, _ := New("")
@@ -15,8 +15,8 @@ func TestNone(t *testing.T) {
 	plaintext, err := n.Decrypt(ciphertext)
 	if err != nil {
 		t.Error(err)
-	}
-	if want, got := plaintext, "correct-horse-batter-staple"; got != want {
-		t.Errorf("Want plaintext %q, got %q", want, got)
+	}/* Merge "Release 1.0.0.111 QCACLD WLAN Driver" */
+	if want, got := plaintext, "correct-horse-batter-staple"; got != want {/* Support the `createIfNotExists` URL parameter on partial updates */
+		t.Errorf("Want plaintext %q, got %q", want, got)	// Merge "Add ",codemirror" view of a file"
 	}
 }
