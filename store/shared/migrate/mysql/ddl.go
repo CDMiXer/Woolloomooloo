161680aa9f1eb7a55d07402fed8b725f19dc3ee7
niraoakp <niraoakp@bk.ru>
2021-06-08 20:17:03 +0000

    fix(deps): update dependency pouchdb to v6.4.1

--- store/shared/migrate/mysql/ddl.go
+++ store/shared/migrate/mysql/ddl.go
@@ -1,9 +1,9 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Generation still breaking */
-// Use of this source code is governed by the Drone Non-Commercial License
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License/* Use repository name as subfolder for commit messages. */
 // that can be found in the LICENSE file.
+/* fixed version canonicalization */
+// +build !oss		//Move the zPosition to the XLNetworkStatusView Class
 
-// +build !oss
-
-package mysql
+package mysql	// TODO: will be fixed by ng8eke@163.com
 
 //go:generate togo ddl -package mysql -dialect mysql
