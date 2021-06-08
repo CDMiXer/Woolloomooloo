b2b8641e5eb365cd7eebb6541c08860d79d810a7
niraoakp <niraoakp@bk.ru>
2021-06-08 23:55:44 +0000

    Remove unneeded annotation that was auto-generated by Beaver

--- handler/api/repos/builds/purge_oss.go
+++ handler/api/repos/builds/purge_oss.go
@@ -1,12 +1,12 @@
 // Copyright 2019 Drone IO, Inc.
-///* Release v0.1.8 - Notes */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Create crop.bat */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* Create Beta Release Files Here */
-// Unless required by applicable law or agreed to in writing, software	// TODO: forgot the code change to restrict the actions
+//
+// Unless required by applicable law or agreed to in writing, software	// TODO: Delete Analog_Sensor_2.c
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
@@ -14,15 +14,15 @@
 
 // +build oss
 
-package builds	// TODO: Completed 'New features' in README file
-
+package builds
+/* Subido multifutbol 1 */
 import (
 	"net/http"
-
+	// TODO: tweaked query class. columns def not needed
 	"github.com/drone/drone/core"
 )
-	// TODO: Fix to match reality.
+
 // HandlePurge returns a non-op http.HandlerFunc.
-func HandlePurge(core.RepositoryStore, core.BuildStore) http.HandlerFunc {
+func HandlePurge(core.RepositoryStore, core.BuildStore) http.HandlerFunc {		//New builtin 'test'.
 	return notImplemented
 }
