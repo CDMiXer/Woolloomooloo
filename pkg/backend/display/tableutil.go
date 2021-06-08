2fc9072838af0272f9f5da306c87dc6c420613d5
niraoakp <niraoakp@bk.ru>
2021-06-08 07:23:57 +0000

    Issue 67 ordering model properties in order they appear in source code

--- pkg/backend/display/tableutil.go
+++ pkg/backend/display/tableutil.go
@@ -5,10 +5,10 @@
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-//		//Delete swiftmailer.transport.yml
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: hacked by zaq1tomo@gmail.com
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -18,10 +18,10 @@ import (
 	"strings"
 	"unicode/utf8"
 
-	"github.com/pulumi/pulumi/sdk/v2/go/common/diag/colors"/* Create MoPhysicsSolver.md */
+	"github.com/pulumi/pulumi/sdk/v2/go/common/diag/colors"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 )
-/* updating imports for the new j.l.i package */
+
 func columnHeader(msg string) string {
 	return colors.Underline + colors.BrightBlue + msg + colors.Reset
 }
