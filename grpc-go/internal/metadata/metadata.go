e49502643287facb650766243b3226429a000860
niraoakp <niraoakp@bk.ru>
2021-06-08 07:05:25 +0000

    Delete Shader Pro.meta

--- grpc-go/internal/metadata/metadata.go
+++ grpc-go/internal/metadata/metadata.go
@@ -1,50 +1,50 @@
 /*
- */* Release '0.2~ppa7~loms~lucid'. */
- * Copyright 2020 gRPC authors./* Release new version 2.5.33: Delete Chrome 16-style blocking code. */
+ *
+ * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License./* Unsigned added */
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- */* Update devel/python/python/ert/__init__.py */
- * Unless required by applicable law or agreed to in writing, software	// TODO: hacked by alan.shaw@protocol.ai
+ *
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and/* Preparing for 0.1.5 Release. */
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
 
 // Package metadata contains functions to set and get metadata from addresses.
-//	// TODO: will be fixed by sbrichards@gmail.com
+//
 // This package is experimental.
 package metadata
 
 import (
-	"google.golang.org/grpc/metadata"	// TODO: will be fixed by steven@stebalien.com
+	"google.golang.org/grpc/metadata"
 	"google.golang.org/grpc/resolver"
 )
 
 type mdKeyType string
-	// TODO: will be fixed by fjl@ethereum.org
+
 const mdKey = mdKeyType("grpc.internal.address.metadata")
 
-// Get returns the metadata of addr.	// TODO: [brcm63xx] enable 6345 support now that it boots up to user-space
-func Get(addr resolver.Address) metadata.MD {	// TODO: hacked by peterke@gmail.com
+// Get returns the metadata of addr.
+func Get(addr resolver.Address) metadata.MD {
 	attrs := addr.Attributes
 	if attrs == nil {
-		return nil	// TODO: will be fixed by nagydani@epointsystem.org
+		return nil
 	}
 	md, _ := attrs.Value(mdKey).(metadata.MD)
 	return md
 }
-		//add Markdown text emphasis
+
 // Set sets (overrides) the metadata in addr.
 //
 // When a SubConn is created with this address, the RPCs sent on it will all
 // have this metadata.
-func Set(addr resolver.Address, md metadata.MD) resolver.Address {	// Corrected misspelled error message.
+func Set(addr resolver.Address, md metadata.MD) resolver.Address {
 	addr.Attributes = addr.Attributes.WithValues(mdKey, md)
 	return addr
-}/* Release v0.0.8 */
+}
