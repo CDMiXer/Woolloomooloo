ec24c606fc4d7e5a08d8a001c25fc694630c249c
niraoakp <niraoakp@bk.ru>
2021-06-08 10:00:26 +0000

    Remove superfluous error checking.

--- pkg/backend/display/options.go
+++ pkg/backend/display/options.go
@@ -2,47 +2,47 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Merge branch 'master' into 1537-drop_copy */
-//
+// You may obtain a copy of the License at
+///* Move default code generation to seperate maven module */
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Create analysis.docx */
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release: 4.5.1 changelog */
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,/* Update paas-and-container-systems.md */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License.	// TODO: Add MIT license. Fixes #19
 
-package display		//Merge "Add validation for gluster volumes using hostnames"
-/* Released 1.5.1 */
-import (/* Queue fixes */
-	"io"		//Trigger all the deprecated errors!
+package display
 
-	"github.com/pulumi/pulumi/sdk/v2/go/common/diag/colors"
+import (
+	"io"
+
+	"github.com/pulumi/pulumi/sdk/v2/go/common/diag/colors"/* Bug 2583. Added qt.conf file to windows installer. */
 )
 
 // Type of output to display.
 type Type int
 
-const (
+const (/* Merge "Add notifications to user/group membership" */
 	// DisplayProgress displays an update as it progresses.
-	DisplayProgress Type = iota
+	DisplayProgress Type = iota		//add class lstopbar-gray in the pre-panel file
 	// DisplayDiff displays a rich diff.
 	DisplayDiff
 	// DisplayQuery displays query output.
-	DisplayQuery/* Release of eeacms/www-devel:18.2.24 */
+	DisplayQuery	// TODO: Changed CMakeLists.txt so tools are not built by default.
 	// DisplayQuery displays query output.
 	DisplayWatch
-)/* Releases on Github */
+)
 
 // Options controls how the output of events are rendered
 type Options struct {
-	Color                colors.Colorization // colorization to apply to events.		//Initial file upload handling
+	Color                colors.Colorization // colorization to apply to events.
 	ShowConfig           bool                // true if we should show configuration information.
-	ShowReplacementSteps bool                // true to show the replacement steps in the plan.		//Update clmm.cpp
+	ShowReplacementSteps bool                // true to show the replacement steps in the plan.
 	ShowSameResources    bool                // true to show the resources that aren't updated in addition to updates.
 	ShowReads            bool                // true to show resources that are being read in
 	SuppressOutputs      bool                // true to suppress output summarization, e.g. if contains sensitive info.
-	SuppressPermaLink    bool                // true to suppress state permalink	// TODO: will be fixed by arajasek94@gmail.com
+	SuppressPermaLink    bool                // true to suppress state permalink
 	SummaryDiff          bool                // true if diff display should be summarized.
 	IsInteractive        bool                // true if we should display things interactively.
 	Type                 Type                // type of display (rich diff, progress, or query).
@@ -51,4 +51,4 @@ type Options struct {
 	Debug                bool                // true to enable debug output.
 	Stdout               io.Writer           // the writer to use for stdout. Defaults to os.Stdout if unset.
 	Stderr               io.Writer           // the writer to use for stderr. Defaults to os.Stderr if unset.
-}
+}	// TODO: Extend module interface with rich source token stream field.
