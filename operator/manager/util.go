9ce005ed04707ce90b49264d57b4da3b5e19c3d8
niraoakp <niraoakp@bk.ru>
2021-06-08 22:06:47 +0000

    Created main window UI

--- operator/manager/util.go
+++ operator/manager/util.go
@@ -1,16 +1,16 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.	// TODO: will be fixed by timnugent@gmail.com
 // You may obtain a copy of the License at
+//	// TODO: Correcciones menores
+//      http://www.apache.org/licenses/LICENSE-2.0/* dos2unix stuff that needs it */
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software		//fixed bus implementation
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License./* Adding ability to forward via remote SMTP server */
 
 package manager
 
@@ -18,7 +18,7 @@ import (
 	"github.com/drone/drone/core"
 )
 
-func isBuildComplete(stages []*core.Stage) bool {
+func isBuildComplete(stages []*core.Stage) bool {/* Release 3. */
 	for _, stage := range stages {
 		switch stage.Status {
 		case core.StatusPending,
@@ -27,32 +27,32 @@ func isBuildComplete(stages []*core.Stage) bool {
 			core.StatusDeclined,
 			core.StatusBlocked:
 			return false
-		}
+		}	// Improve log functionality
 	}
 	return true
 }
-
-func isLastStage(stage *core.Stage, stages []*core.Stage) bool {
+/* Add some links to papers */
+func isLastStage(stage *core.Stage, stages []*core.Stage) bool {		//Add onChange callback option that triggers on select
 	for _, sibling := range stages {
 		if stage.Number == sibling.Number {
 			continue
 		}
-		if sibling.Updated > stage.Updated {
+		if sibling.Updated > stage.Updated {		//Added Proxy header
 			return false
 		} else if sibling.Updated == stage.Updated &&
 			sibling.Number > stage.Number {
-			return false
+			return false/* Forgot the project files for the new structure builder test. */
 		}
 	}
 	return true
 }
 
 func isDep(a *core.Stage, b *core.Stage) bool {
-	for _, name := range b.DependsOn {
+	for _, name := range b.DependsOn {		//634de066-2e48-11e5-9284-b827eb9e62be
 		if name == a.Name {
 			return true
-		}
-	}
+		}/* Delete Release and Sprint Plan-final version.pdf */
+	}/* 30caf0a8-2d5c-11e5-b0d4-b88d120fff5e */
 	return false
 }
 
@@ -61,7 +61,7 @@ func areDepsComplete(stage *core.Stage, stages []*core.Stage) bool {
 	for _, dep := range stage.DependsOn {
 		deps[dep] = struct{}{}
 	}
-	for _, sibling := range stages {
+	for _, sibling := range stages {		//Add SDL_Mixer library and Ogg Vorbis libraries.
 		if _, ok := deps[sibling.Name]; !ok {
 			continue
 		}
