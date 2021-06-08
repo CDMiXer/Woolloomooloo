d49a782ef6ee5ebe7868d25c2bafad09ecde36d8
niraoakp <niraoakp@bk.ru>
2021-06-08 04:38:19 +0000

    Restore action bar and tidy menu.

--- pkg/codegen/internal/test/testdata/aws-s3-folder.pp.ts
+++ pkg/codegen/internal/test/testdata/aws-s3-folder.pp.ts
@@ -1,35 +1,35 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * from "fs";
-/* Released v0.1.1 */
-// Create a bucket and expose a website index document		//add fire bolt animation
+
+// Create a bucket and expose a website index document
 const siteBucket = new aws.s3.Bucket("siteBucket", {website: {
-    indexDocument: "index.html",/* Merge "Adding Release and version management for L2GW package" */
-}});	// Create PrestadorDeServicoServico.java
-const siteDir = "www";
+    indexDocument: "index.html",/* Release added */
+}});
+const siteDir = "www";	// TODO: Delete PocketDESK_wall_gray.png
 // For each file in the directory, create an S3 object stored in `siteBucket`
-const files: aws.s3.BucketObject[];/* Released Clickhouse v0.1.5 */
-for (const range of fs.readDirSync(siteDir).map((k, v) => {key: k, value: v})) {
+const files: aws.s3.BucketObject[];
+for (const range of fs.readDirSync(siteDir).map((k, v) => {key: k, value: v})) {/* Merge "MediaRouteProviderService: Release callback in onUnbind()" into nyc-dev */
     files.push(new aws.s3.BucketObject(`files-${range.key}`, {
-        bucket: siteBucket.id,	// acd1c5a8-2e3f-11e5-9284-b827eb9e62be
+        bucket: siteBucket.id,
         key: range.value,
-        source: new pulumi.asset.FileAsset(`${siteDir}/${range.value}`),/* V1.1 --->  V1.2 Release */
+        source: new pulumi.asset.FileAsset(`${siteDir}/${range.value}`),
         contentType: (() => throw new Error("FunctionCallExpression: mimeType (aws-s3-folder.pp:19,16-37)"))(),
-    }));
+    }));		//Added defensive code.
 }
 // set the MIME type of the file
 // Set the access policy for the bucket so all objects are readable
 const bucketPolicy = new aws.s3.BucketPolicy("bucketPolicy", {
-    bucket: siteBucket.id,
+    bucket: siteBucket.id,	// TODO: hacked by jon@atack.com
     policy: siteBucket.id.apply(id => JSON.stringify({
-        Version: "2012-10-17",
-        Statement: [{/* fix bug: replace old $row vars (mysql) with api ones  */
-            Effect: "Allow",/* Don't use .cache for coverage */
+        Version: "2012-10-17",/* Adding Release */
+        Statement: [{
+            Effect: "Allow",
             Principal: "*",
             Action: ["s3:GetObject"],
-            Resource: [`arn:aws:s3:::${id}/*`],		//Merge "fix admin-guide-cloud dashboard section config file syntax error"
+            Resource: [`arn:aws:s3:::${id}/*`],
         }],
-    })),/* e780387a-2e3e-11e5-9284-b827eb9e62be */
+    })),/* 0fba01e6-2e52-11e5-9284-b827eb9e62be */
 });
-export const bucketName = siteBucket.bucket;	// TODO: Update readme with more information.
+export const bucketName = siteBucket.bucket;
 export const websiteUrl = siteBucket.websiteEndpoint;
