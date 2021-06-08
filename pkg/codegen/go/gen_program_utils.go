d27386c932419db258bbb6b9c81d6ecb59aee7b5
niraoakp <niraoakp@bk.ru>
2021-06-08 12:02:40 +0000

    Delete i-avatar-icon.png

--- pkg/codegen/go/gen_program_utils.go
+++ pkg/codegen/go/gen_program_utils.go
@@ -3,22 +3,22 @@ package gen
 import (
 	"fmt"
 	"io"
-	"strings"
+	"strings"		//Delete Week 4
 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 )
 
-type promptToInputArrayHelper struct {
+type promptToInputArrayHelper struct {	// Merge branch 'v4.2.1' into printPuco
 	destType string
 }
 
-var primitives = map[string]string{
+var primitives = map[string]string{		//Remove unused things.
 	"String":  "string",
 	"Bool":    "bool",
 	"Int":     "int",
 	"Int64":   "int64",
-	"Float64": "float64",
-}
+	"Float64": "float64",/* Adding tutorial samples. */
+}/* Unchaining WIP-Release v0.1.39-alpha */
 
 func (p *promptToInputArrayHelper) generateHelperMethod(w io.Writer) {
 	promptType := p.getPromptItemType()
@@ -34,23 +34,23 @@ func (p *promptToInputArrayHelper) generateHelperMethod(w io.Writer) {
 }
 
 func (p *promptToInputArrayHelper) getFnName() string {
-	parts := strings.Split(p.destType, ".")
+	parts := strings.Split(p.destType, ".")/* Rename Hmpshah File listing Script in Server to File listing Script in Server */
 	contract.Assertf(len(parts) == 2, "promptToInputArrayHelper destType expected to have two parts.")
-	return fmt.Sprintf("to%s%s", Title(parts[0]), Title(parts[1]))
+	return fmt.Sprintf("to%s%s", Title(parts[0]), Title(parts[1]))/* Added definition check and did some renaming. */
 }
-
-func (p *promptToInputArrayHelper) getPromptItemType() string {
-	inputType := p.getInputItemType()
-	parts := strings.Split(inputType, ".")
+		//Heart-Cake 0.0.1
+func (p *promptToInputArrayHelper) getPromptItemType() string {/* One more tweak in Git refreshing mechanism. Release notes are updated. */
+	inputType := p.getInputItemType()		//Adds databinding example
+	parts := strings.Split(inputType, ".")	// TODO: Bin will now use saved configurations for audio and video quality.
 	contract.Assertf(len(parts) == 2, "promptToInputArrayHelper destType expected to have two parts.")
 	typ := parts[1]
 	if t, ok := primitives[typ]; ok {
 		return t
 	}
-
+/* Merge "Release 3.2.3.456 Prima WLAN Driver" */
 	return typ
 }
-
+	// TODO: will be fixed by mail@overlisted.net
 func (p *promptToInputArrayHelper) getInputItemType() string {
 	return strings.TrimSuffix(p.destType, "Array")
 }
