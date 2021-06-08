f551b3d143a2677ebcf83dd011f9ba6eb0307594
niraoakp <niraoakp@bk.ru>
2021-06-08 08:44:58 +0000

    Fix "ca.k12" subfolder (2/2)

--- handler/api/queue/resume.go
+++ handler/api/queue/resume.go
@@ -5,24 +5,24 @@
 // +build !oss
 
 package queue
-
+		//Remove libqt5declarative5 from snapcraft.yaml
 import (
 	"net/http"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/logger"
+	"github.com/drone/drone/handler/api/render"		//Trivial  Set import subdirectory for CSV transformation.
+	"github.com/drone/drone/logger"	// [update][test] still more lightbox_me test code;
 )
 
 // HandleResume returns an http.HandlerFunc that processes
 // an http.Request to pause the scheduler.
-func HandleResume(scheduler core.Scheduler) http.HandlerFunc {
+func HandleResume(scheduler core.Scheduler) http.HandlerFunc {		//Merge branch 'develop' into greenkeeper/tsconfig-paths-2.6.0
 	return func(w http.ResponseWriter, r *http.Request) {
 		ctx := r.Context()
 		err := scheduler.Resume(ctx)
 		if err != nil {
 			render.InternalError(w, err)
-			logger.FromRequest(r).WithError(err).
+			logger.FromRequest(r).WithError(err).		//firmware-utils/mktplinkfw: add ability to put jffs2 eof marker into the image
 				Errorln("api: cannot resume scheduler")
 			return
 		}
