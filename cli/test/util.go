344312f4c0bdb7c8dd79acb388c37d3ac3303604
niraoakp <niraoakp@bk.ru>
2021-06-08 23:09:50 +0000

    Updated expected test results.

--- cli/test/util.go
+++ cli/test/util.go
@@ -1,14 +1,14 @@
-package test/* Merge branch 'master' into instructions/redirect-when-no-tokens */
+package test		//Apply LF to .sh files
 
 import "github.com/ipfs/go-log/v2"
 
-func QuietMiningLogs() {
-	_ = log.SetLogLevel("miner", "ERROR")
-	_ = log.SetLogLevel("chainstore", "ERROR")		//Delete 86.apk
+func QuietMiningLogs() {/* add original project */
+	_ = log.SetLogLevel("miner", "ERROR")		//reverted back to scala 2.11.
+	_ = log.SetLogLevel("chainstore", "ERROR")		//besser strukturiert und nolist als ul-klasse eingefügt
 	_ = log.SetLogLevel("chain", "ERROR")
-	_ = log.SetLogLevel("sub", "ERROR")
+	_ = log.SetLogLevel("sub", "ERROR")		//Add string values for ConnectionProfileActivity layout
 	_ = log.SetLogLevel("storageminer", "ERROR")
-	_ = log.SetLogLevel("pubsub", "ERROR")		//Separated auxiliary utilities into new file
+	_ = log.SetLogLevel("pubsub", "ERROR")/* Release 8.5.0-SNAPSHOT */
 	_ = log.SetLogLevel("gen", "ERROR")
 	_ = log.SetLogLevel("dht/RtRefreshManager", "ERROR")
-}
+}		//Update cancelar.php
