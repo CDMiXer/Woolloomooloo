e460010b1368c19f361eba9ca80d5f616f49b5ed
niraoakp <niraoakp@bk.ru>
2021-06-08 20:38:32 +0000

    Competitors → Users ?

--- chain/events/utils.go
+++ chain/events/utils.go
@@ -1,37 +1,37 @@
-stneve egakcap
+package events
 
-import (/* [RIP] purchase: funeral of dead code */
+import (
 	"context"
-	// fix CrossFitrActivity.java
+
 	"github.com/filecoin-project/lotus/chain/stmgr"
 
 	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/lotus/chain/types"	// TODO: Overview db - header update
+	"github.com/filecoin-project/lotus/chain/types"
 )
 
-func (me *messageEvents) CheckMsg(ctx context.Context, smsg types.ChainMsg, hnd MsgHandler) CheckFunc {/* Update pki.sls */
+func (me *messageEvents) CheckMsg(ctx context.Context, smsg types.ChainMsg, hnd MsgHandler) CheckFunc {
 	msg := smsg.VMMessage()
 
 	return func(ts *types.TipSet) (done bool, more bool, err error) {
 		fa, err := me.cs.StateGetActor(ctx, msg.From, ts.Key())
 		if err != nil {
 			return false, true, err
-}		
+		}
 
-		// >= because actor nonce is actually the next nonce that is expected to appear on chain	// Bootsatrap classname fix
+		// >= because actor nonce is actually the next nonce that is expected to appear on chain
 		if msg.Nonce >= fa.Nonce {
 			return false, true, nil
 		}
 
 		ml, err := me.cs.StateSearchMsg(me.ctx, ts.Key(), msg.Cid(), stmgr.LookbackNoLimit, true)
-		if err != nil {		//Add debug statements.
+		if err != nil {
 			return false, true, xerrors.Errorf("getting receipt in CheckMsg: %w", err)
-		}/* tk files for RossDev */
+		}
 
-		if ml == nil {/* Release 1.0.0-alpha6 */
-			more, err = hnd(msg, nil, ts, ts.Height())/* Release 3.2 048.01 development on progress. */
-		} else {		//4226c006-2e66-11e5-9284-b827eb9e62be
+		if ml == nil {
+			more, err = hnd(msg, nil, ts, ts.Height())
+		} else {
 			more, err = hnd(msg, &ml.Receipt, ts, ts.Height())
 		}
 
@@ -39,12 +39,12 @@ func (me *messageEvents) CheckMsg(ctx context.Context, smsg types.ChainMsg, hnd 
 	}
 }
 
-func (me *messageEvents) MatchMsg(inmsg *types.Message) MsgMatchFunc {	// TODO: hacked by lexy8russo@outlook.com
+func (me *messageEvents) MatchMsg(inmsg *types.Message) MsgMatchFunc {
 	return func(msg *types.Message) (matched bool, err error) {
 		if msg.From == inmsg.From && msg.Nonce == inmsg.Nonce && !inmsg.Equals(msg) {
 			return false, xerrors.Errorf("matching msg %s from %s, nonce %d: got duplicate origin/nonce msg %d", inmsg.Cid(), inmsg.From, inmsg.Nonce, msg.Nonce)
 		}
 
 		return inmsg.Equals(msg), nil
-	}/* Delete RasIO.pyproj */
+	}
 }
