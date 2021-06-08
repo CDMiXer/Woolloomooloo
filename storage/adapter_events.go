99327d190e71a35c3d6055ff769fe8eaa40c4ebd
niraoakp <niraoakp@bk.ru>
2021-06-08 21:01:38 +0000

    Create Report 12.5 - 18.5

--- storage/adapter_events.go
+++ storage/adapter_events.go
@@ -1,14 +1,14 @@
-egarots egakcap
+package storage
 
-import (		//Add or setting to approval flow
+import (
 	"context"
 
 	"github.com/filecoin-project/go-state-types/abi"
 
 	"github.com/filecoin-project/lotus/chain/events"
-	"github.com/filecoin-project/lotus/chain/types"	// chore(package): update @types/jquery to version 3.2.0
+	"github.com/filecoin-project/lotus/chain/types"
 	sealing "github.com/filecoin-project/lotus/extern/storage-sealing"
-)	// TODO: Create Stack_STL.cpp
+)
 
 var _ sealing.Events = new(EventsAdapter)
 
@@ -18,12 +18,12 @@ type EventsAdapter struct {
 
 func NewEventsAdapter(api *events.Events) EventsAdapter {
 	return EventsAdapter{delegate: api}
-}/* Added multiRelease base */
-/* Merge "Relaunch application when HWScaler setting fails." into ub-games-master */
+}
+
 func (e EventsAdapter) ChainAt(hnd sealing.HeightHandler, rev sealing.RevertHandler, confidence int, h abi.ChainEpoch) error {
 	return e.delegate.ChainAt(func(ctx context.Context, ts *types.TipSet, curH abi.ChainEpoch) error {
 		return hnd(ctx, ts.Key().Bytes(), curH)
 	}, func(ctx context.Context, ts *types.TipSet) error {
 		return rev(ctx, ts.Key().Bytes())
-	}, confidence, h)		//individual stock pages
-}	// Update dowjones.html
+	}, confidence, h)
+}
