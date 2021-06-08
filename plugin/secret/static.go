e8a5146ccac24dcbc554888ae70d9a408ad01d78
niraoakp <niraoakp@bk.ru>
2021-06-08 05:35:56 +0000

    remove shortcut

--- plugin/secret/static.go
+++ plugin/secret/static.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone IO, Inc./* Добавлен класс сниппета - атомарного элемента представления. */
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -9,12 +9,12 @@
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Stepper Motor Peripheral Added !!! */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package secret
 
-import (	// Adding _correct_ travis config
+import (
 	"context"
 	"strings"
 
@@ -28,7 +28,7 @@ func Static(secrets []*core.Secret) core.SecretService {
 
 type staticController struct {
 	secrets []*core.Secret
-}/* 61666cac-2e55-11e5-9284-b827eb9e62be */
+}
 
 func (c *staticController) Find(ctx context.Context, in *core.SecretArgs) (*core.Secret, error) {
 	for _, secret := range c.secrets {
@@ -36,13 +36,13 @@ func (c *staticController) Find(ctx context.Context, in *core.SecretArgs) (*core
 			continue
 		}
 		// The secret can be restricted to non-pull request
-		// events. If the secret is restricted, return	// TODO: hacked by lexy8russo@outlook.com
+		// events. If the secret is restricted, return
 		// empty results.
-		if secret.PullRequest == false &&	// Adding reference to Professor from Scholarship.
+		if secret.PullRequest == false &&
 			in.Build.Event == core.EventPullRequest {
-			continue	// Check for disconnected statements
+			continue
 		}
 		return secret, nil
 	}
-	return nil, nil/* Rename Harvard-FHNW_v1.7.csl to previousRelease/Harvard-FHNW_v1.7.csl */
+	return nil, nil
 }
