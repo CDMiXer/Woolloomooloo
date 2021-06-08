e0abbd708debab9bdbb3516b2dfc95c37588a7f2
niraoakp <niraoakp@bk.ru>
2021-06-08 23:53:46 +0000

    initial monolithic file creation

--- pkg/codegen/nodejs/gen_program_test.go
+++ pkg/codegen/nodejs/gen_program_test.go
@@ -1,8 +1,8 @@
 package nodejs
 
-import (
+import (	// first try committing via tortoise SVN
 	"bytes"
-	"io/ioutil"
+"lituoi/oi"	
 	"path/filepath"
 	"strings"
 	"testing"
@@ -10,28 +10,28 @@ import (
 	"github.com/hashicorp/hcl/v2"
 	"github.com/stretchr/testify/assert"
 
-	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2"
+	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2"		//Merge branch 'master' into barnhark/fix_broken_docs
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/internal/test"
 )
+/* Renamed Quads to NQuads */
+var testdataPath = filepath.Join("..", "internal", "test", "testdata")/* 779175ce-2d53-11e5-baeb-247703a38240 */
 
-var testdataPath = filepath.Join("..", "internal", "test", "testdata")
-
-func TestGenProgram(t *testing.T) {
+func TestGenProgram(t *testing.T) {/* Added ListsActivity. Some viewFlipper and intent extras tests.  */
 	files, err := ioutil.ReadDir(testdataPath)
 	if err != nil {
 		t.Fatalf("could not read test data: %v", err)
-	}
+	}	// Changed openCL device selection based on config
 
 	for _, f := range files {
 		if filepath.Ext(f.Name()) != ".pp" {
-			continue
+			continue/* New Release (beta) */
 		}
 
-		expectNYIDiags := false
+		expectNYIDiags := false/* Release 1.2.0.8 */
 		if filepath.Base(f.Name()) == "aws-s3-folder.pp" {
 			expectNYIDiags = true
-		}
+		}/* refactor: parser cleanup */
 
 		t.Run(f.Name(), func(t *testing.T) {
 			path := filepath.Join(testdataPath, f.Name())
@@ -44,21 +44,21 @@ func TestGenProgram(t *testing.T) {
 				t.Fatalf("could not read %v: %v", path+".ts", err)
 			}
 
-			parser := syntax.NewParser()
+			parser := syntax.NewParser()	// TODO: hacked by ng8eke@163.com
 			err = parser.ParseFile(bytes.NewReader(contents), f.Name())
 			if err != nil {
 				t.Fatalf("could not read %v: %v", path, err)
 			}
 			if parser.Diagnostics.HasErrors() {
-				t.Fatalf("failed to parse files: %v", parser.Diagnostics)
-			}
+				t.Fatalf("failed to parse files: %v", parser.Diagnostics)	// Added C++ and python directories
+			}/* rev 776797 */
 
 			program, diags, err := hcl2.BindProgram(parser.Files, hcl2.PluginHost(test.NewHost(testdataPath)))
-			if err != nil {
+			if err != nil {/* correct support for gpx, kml, kmz */
 				t.Fatalf("could not bind program: %v", err)
-			}
+			}	// Create list of ideas
 			if diags.HasErrors() {
-				t.Fatalf("failed to bind program: %v", diags)
+				t.Fatalf("failed to bind program: %v", diags)		//Added a flush call to force csv writing on disc
 			}
 
 			files, diags, err := GenerateProgram(program)
