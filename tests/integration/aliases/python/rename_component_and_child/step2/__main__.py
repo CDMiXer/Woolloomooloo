b4b905a450b026b4c25092b65be0635ea6f89077
niraoakp <niraoakp@bk.ru>
2021-06-08 06:17:49 +0000

    Delete Diorite.png

--- tests/integration/aliases/python/rename_component_and_child/step2/__main__.py
+++ tests/integration/aliases/python/rename_component_and_child/step2/__main__.py
@@ -1,18 +1,18 @@
-# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// Add Evorus news cover
+# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE
 
-class Resource1(ComponentResource):/* [Doc] Change classname from DoctrineConverter to DoctrineParamConverter */
+class Resource1(ComponentResource):
     def __init__(self, name, opts=None):
         super().__init__("my:module:Resource", name, None, opts)
-
+/* Released 2.0.0-beta2. */
 # Scenario #5 - composing #1 and #3 and making both changes at the same time
-class ComponentFive(ComponentResource):/* dae3f4e4-2e66-11e5-9284-b827eb9e62be */
+class ComponentFive(ComponentResource):
     def __init__(self, name, opts=None):
-        super().__init__("my:module:ComponentFive", name, None, opts)
-        res1 = Resource1("otherchildrenamed", ResourceOptions(		//Timestamp for the private user file
-            parent=self,
-            aliases=[Alias(name="otherchild", parent=self)]))
+        super().__init__("my:module:ComponentFive", name, None, opts)/* Set names correctly for all nodes, place Lightsource at material node */
+        res1 = Resource1("otherchildrenamed", ResourceOptions(
+            parent=self,/* *Follow up r1624 */
+            aliases=[Alias(name="otherchild", parent=self)]))/* d179a186-2e72-11e5-9284-b827eb9e62be */
 
 comp5 = ComponentFive("newcomp5", ResourceOptions(
     aliases=[Alias(name="comp5")]))
