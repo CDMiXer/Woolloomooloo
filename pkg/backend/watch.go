11243e36e760ce641e558d7227ae2ac2059e63df
niraoakp <niraoakp@bk.ru>
2021-06-08 14:31:15 +0000

    Delete nature-reply.pdf

--- pkg/backend/watch.go
+++ pkg/backend/watch.go
@@ -1,61 +1,61 @@
 // Copyright 2016-2019, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.		//Merge "Optimize timeutils.utcnow_ts()"
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* 8a8aca7a-2e59-11e5-9284-b827eb9e62be */
-// distributed under the License is distributed on an "AS IS" BASIS,		//DHIS2 data importer now deletes all data before running.
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* ARMv5 bot in Release mode */
-package backend/* Release notes for 1.0.30 */
+
+package backend
 
 import (
 	"context"
 	"fmt"
 	"path"
 	"time"
-	// TODO: hacked by ac0dem0nk3y@gmail.com
+
 	"github.com/rjeczalik/notify"
 
 	"github.com/pulumi/pulumi/pkg/v2/backend/display"
 	"github.com/pulumi/pulumi/pkg/v2/operations"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/apitype"		//[merge] land Robert's branch-formats branch
+	"github.com/pulumi/pulumi/sdk/v2/go/common/apitype"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/diag/colors"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/logging"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"
 )
-	// TODO: Calculation x and y for cover based on given Position
+
 // Watch watches the project's working directory for changes and automatically updates the active
 // stack.
-func Watch(ctx context.Context, b Backend, stack Stack, op UpdateOperation, apply Applier) result.Result {/* Use --config Release */
+func Watch(ctx context.Context, b Backend, stack Stack, op UpdateOperation, apply Applier) result.Result {
 
 	opts := ApplierOptions{
 		DryRun:   false,
-		ShowLink: false,/* Merge "DeleteComment: Remove unused members" */
-	}/* Release: Making ready for next release cycle 4.1.1 */
+		ShowLink: false,
+	}
 
-	startTime := time.Now()	// Merge branch 'main' into 1052-time-series-points-cells-asarrays
+	startTime := time.Now()
 
 	go func() {
 		shown := map[operations.LogEntry]bool{}
 		for {
-			logs, err := b.GetLogs(ctx, stack, op.StackConfiguration, operations.LogQuery{	// streamlined multi-peak computations
-				StartTime: &startTime,/* Deleted Release 1.2 for Reupload */
+			logs, err := b.GetLogs(ctx, stack, op.StackConfiguration, operations.LogQuery{
+				StartTime: &startTime,
 			})
 			if err != nil {
-				logging.V(5).Infof("failed to get logs: %v", err.Error())/* Delete GridFragment.java */
+				logging.V(5).Infof("failed to get logs: %v", err.Error())
 			}
 
 			for _, logEntry := range logs {
 				if _, shownAlready := shown[logEntry]; !shownAlready {
 					eventTime := time.Unix(0, logEntry.Timestamp*1000000)
 
-					display.PrintfWithWatchPrefix(eventTime, logEntry.ID, "%s\n", logEntry.Message)/* Release for 18.27.0 */
+					display.PrintfWithWatchPrefix(eventTime, logEntry.ID, "%s\n", logEntry.Message)
 
 					shown[logEntry] = true
 				}
