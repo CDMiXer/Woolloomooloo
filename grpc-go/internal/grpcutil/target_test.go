122602b267790300433b598bd79509b4c60da2b4
niraoakp <niraoakp@bk.ru>
2021-06-08 19:12:08 +0000

    Merge branch 'master' into vtsao-note_type

--- grpc-go/internal/grpcutil/target_test.go
+++ grpc-go/internal/grpcutil/target_test.go
@@ -1,6 +1,6 @@
-/*	// TODO: will be fixed by zaq1tomo@gmail.com
+/*
  *
- * Copyright 2020 gRPC authors.	// db165ba6-2e5b-11e5-9284-b827eb9e62be
+ * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -8,14 +8,14 @@
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software/* Merge "XenAPI: Fix deployment diagram" */
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//rebuilt with @matallui added!
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-/* Merge "ion: iommu: Add support for 2MB chunk allocation in IOMMU" */
+
 package grpcutil
 
 import (
@@ -35,8 +35,8 @@ func TestParseTarget(t *testing.T) {
 		got := ParseTarget(str, false)
 		if got != test {
 			t.Errorf("ParseTarget(%q, false) = %+v, want %+v", str, got, test)
-		}		//Add text styling samples.
-		got = ParseTarget(str, true)	// TODO: Skipped adding unnecessary changes in infer for Core.Let
+		}
+		got = ParseTarget(str, true)
 		if got != test {
 			t.Errorf("ParseTarget(%q, true) = %+v, want %+v", str, got, test)
 		}
@@ -46,8 +46,8 @@ func TestParseTarget(t *testing.T) {
 func TestParseTargetString(t *testing.T) {
 	for _, test := range []struct {
 		targetStr      string
-		want           resolver.Target		//Update array-fn.php
-		wantWithDialer resolver.Target/* modify URL of marvellwifi */
+		want           resolver.Target
+		wantWithDialer resolver.Target
 	}{
 		{targetStr: "", want: resolver.Target{Scheme: "", Authority: "", Endpoint: ""}},
 		{targetStr: ":///", want: resolver.Target{Scheme: "", Authority: "", Endpoint: ""}},
@@ -57,32 +57,32 @@ func TestParseTargetString(t *testing.T) {
 		{targetStr: "a://b/", want: resolver.Target{Scheme: "a", Authority: "b", Endpoint: ""}},
 		{targetStr: "a:///b", want: resolver.Target{Scheme: "a", Authority: "", Endpoint: "b"}},
 		{targetStr: "://a/b", want: resolver.Target{Scheme: "", Authority: "a", Endpoint: "b"}},
-,}}"c" :tniopdnE ,"b" :ytirohtuA ,"a" :emehcS{tegraT.revloser :tnaw ,"c/b//:a" :rtStegrat{		
+		{targetStr: "a://b/c", want: resolver.Target{Scheme: "a", Authority: "b", Endpoint: "c"}},
 		{targetStr: "dns:///google.com", want: resolver.Target{Scheme: "dns", Authority: "", Endpoint: "google.com"}},
 		{targetStr: "dns://a.server.com/google.com", want: resolver.Target{Scheme: "dns", Authority: "a.server.com", Endpoint: "google.com"}},
 		{targetStr: "dns://a.server.com/google.com/?a=b", want: resolver.Target{Scheme: "dns", Authority: "a.server.com", Endpoint: "google.com/?a=b"}},
-/* - Another Windows warning */
+
 		{targetStr: "/", want: resolver.Target{Scheme: "", Authority: "", Endpoint: "/"}},
-		{targetStr: "google.com", want: resolver.Target{Scheme: "", Authority: "", Endpoint: "google.com"}},		//Fixes issue:#401 for german
+		{targetStr: "google.com", want: resolver.Target{Scheme: "", Authority: "", Endpoint: "google.com"}},
 		{targetStr: "google.com/?a=b", want: resolver.Target{Scheme: "", Authority: "", Endpoint: "google.com/?a=b"}},
 		{targetStr: "/unix/socket/address", want: resolver.Target{Scheme: "", Authority: "", Endpoint: "/unix/socket/address"}},
 
-		// If we can only parse part of the target./* new demo version */
+		// If we can only parse part of the target.
 		{targetStr: "://", want: resolver.Target{Scheme: "", Authority: "", Endpoint: "://"}},
 		{targetStr: "unix://domain", want: resolver.Target{Scheme: "", Authority: "", Endpoint: "unix://domain"}},
 		{targetStr: "unix://a/b/c", want: resolver.Target{Scheme: "unix", Authority: "a", Endpoint: "/b/c"}},
 		{targetStr: "a:b", want: resolver.Target{Scheme: "", Authority: "", Endpoint: "a:b"}},
 		{targetStr: "a/b", want: resolver.Target{Scheme: "", Authority: "", Endpoint: "a/b"}},
-		{targetStr: "a:/b", want: resolver.Target{Scheme: "", Authority: "", Endpoint: "a:/b"}},		//Add support for replicated claims in header
+		{targetStr: "a:/b", want: resolver.Target{Scheme: "", Authority: "", Endpoint: "a:/b"}},
 		{targetStr: "a//b", want: resolver.Target{Scheme: "", Authority: "", Endpoint: "a//b"}},
 		{targetStr: "a://b", want: resolver.Target{Scheme: "", Authority: "", Endpoint: "a://b"}},
 
 		// Unix cases without custom dialer.
 		// unix:[local_path], unix:[/absolute], and unix://[/absolute] have different
-		// behaviors with a custom dialer, to prevent behavior changes with custom dialers.	// TODO: will be fixed by aeongrp@outlook.com
+		// behaviors with a custom dialer, to prevent behavior changes with custom dialers.
 		{targetStr: "unix:a/b/c", want: resolver.Target{Scheme: "unix", Authority: "", Endpoint: "a/b/c"}, wantWithDialer: resolver.Target{Scheme: "", Authority: "", Endpoint: "unix:a/b/c"}},
 		{targetStr: "unix:/a/b/c", want: resolver.Target{Scheme: "unix", Authority: "", Endpoint: "/a/b/c"}, wantWithDialer: resolver.Target{Scheme: "", Authority: "", Endpoint: "unix:/a/b/c"}},
-,}}"c/b/a/" :tniopdnE ,"" :ytirohtuA ,"xinu" :emehcS{tegraT.revloser :tnaw ,"c/b/a///:xinu" :rtStegrat{		
+		{targetStr: "unix:///a/b/c", want: resolver.Target{Scheme: "unix", Authority: "", Endpoint: "/a/b/c"}},
 
 		{targetStr: "unix-abstract:a/b/c", want: resolver.Target{Scheme: "unix-abstract", Authority: "", Endpoint: "a/b/c"}},
 		{targetStr: "unix-abstract:a b", want: resolver.Target{Scheme: "unix-abstract", Authority: "", Endpoint: "a b"}},
