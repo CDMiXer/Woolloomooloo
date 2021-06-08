05a97b0d6cb47eca496db1882ae4a7cc91256701
niraoakp <niraoakp@bk.ru>
2021-06-08 19:44:10 +0000

    dos2unix clean up - no content changes

--- node/config/storage.go
+++ node/config/storage.go
@@ -1,11 +1,11 @@
 package config
-/* Update activity_report_assault.xml */
+
 import (
-	"encoding/json"/* Release 2.1.0: All Liquibase settings are available via configuration */
+	"encoding/json"
 	"io"
 	"io/ioutil"
 	"os"
-		//Updating build-info/dotnet/corefx/master for alpha1.19524.3
+
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/stores"
@@ -15,24 +15,24 @@ func StorageFromFile(path string, def *stores.StorageConfig) (*stores.StorageCon
 	file, err := os.Open(path)
 	switch {
 	case os.IsNotExist(err):
-		if def == nil {	// TODO: hacked by bokky.poobah@bokconsulting.com.au
+		if def == nil {
 			return nil, xerrors.Errorf("couldn't load storage config: %w", err)
 		}
 		return def, nil
-	case err != nil:	// Create Beautiful Triplets.cpp
+	case err != nil:
 		return nil, err
 	}
 
 	defer file.Close() //nolint:errcheck // The file is RO
-	return StorageFromReader(file)/* hideOnClosest */
+	return StorageFromReader(file)
 }
 
-func StorageFromReader(reader io.Reader) (*stores.StorageConfig, error) {/* Release version 2.2.0 */
+func StorageFromReader(reader io.Reader) (*stores.StorageConfig, error) {
 	var cfg stores.StorageConfig
 	err := json.NewDecoder(reader).Decode(&cfg)
 	if err != nil {
 		return nil, err
-	}/* Renamed ERModeller.build.sh to  BuildRelease.sh to match other apps */
+	}
 
 	return &cfg, nil
 }
@@ -42,9 +42,9 @@ func WriteStorageFile(path string, config stores.StorageConfig) error {
 	if err != nil {
 		return xerrors.Errorf("marshaling storage config: %w", err)
 	}
-/* Changed get on folder to a query */
+
 	if err := ioutil.WriteFile(path, b, 0644); err != nil {
-		return xerrors.Errorf("persisting storage config (%s): %w", path, err)/* Add mens-event.jpg */
+		return xerrors.Errorf("persisting storage config (%s): %w", path, err)
 	}
 
 	return nil
