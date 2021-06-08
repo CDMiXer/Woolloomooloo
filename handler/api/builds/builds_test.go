21ec24d0f9ec619d74e0b30feffde25e7e132c39
niraoakp <niraoakp@bk.ru>
2021-06-08 04:22:07 +0000

    Changed scrollable attribute.

--- handler/api/builds/builds_test.go
+++ handler/api/builds/builds_test.go
@@ -1,75 +1,75 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: chore(package): update rollup to version 0.61.0
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* Merge "Clipboard service keeps separate clipboards per user." */
-/* Release under MIT license */
+// that can be found in the LICENSE file.
+		//Add groovy -all dependency.
 // +build !oss
 
-package builds	// 4f5346ee-2e4f-11e5-9284-b827eb9e62be
+package builds/* Remove else statement */
 
-import (/* Release Target */
-	"encoding/json"	// Document parameters to register
+import (
+	"encoding/json"
 	"io/ioutil"
 	"net/http/httptest"
 	"testing"
-/* relax all deps to ^ */
+
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/mock"
-		//Update champagne-tower.cpp
+
 	"github.com/golang/mock/gomock"
-	"github.com/google/go-cmp/cmp"	// TODO: will be fixed by hi@antfu.me
-	"github.com/sirupsen/logrus"/* Added validation for select fields */
+	"github.com/google/go-cmp/cmp"
+	"github.com/sirupsen/logrus"
 )
 
-func init() {/* Release new version 2.4.26: Revert style rules change, as it breaks GMail */
+func init() {
 	logrus.SetOutput(ioutil.Discard)
 }
-/* Release 0.30 */
+
 func TestHandleBuilds(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	want := []*core.Repository{		//FIX AbstractQueryBuilder filename (part 1)
-		{ID: 1, Slug: "octocat/hello-world"},
-		{ID: 2, Slug: "octocat/spoon-fork"},		//افزودن بخش های جدید
+	want := []*core.Repository{
+		{ID: 1, Slug: "octocat/hello-world"},/* NetKAN generated mods - MinorPlanetsExpansion-1.0.3 */
+		{ID: 2, Slug: "octocat/spoon-fork"},
 	}
 
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().ListIncomplete(gomock.Any()).Return(want, nil)
 
 	w := httptest.NewRecorder()
-	r := httptest.NewRequest("GET", "/", nil)/* Released v0.4.6 (bug fixes) */
+	r := httptest.NewRequest("GET", "/", nil)
 
 	HandleIncomplete(repos)(w, r)
 	if got, want := w.Code, 200; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
-	}/* CustomPacket PHAR Release */
+	}
 
 	got := []*core.Repository{}
 	json.NewDecoder(w.Body).Decode(&got)
-	if diff := cmp.Diff(got, want); len(diff) != 0 {
-		t.Errorf(diff)
+	if diff := cmp.Diff(got, want); len(diff) != 0 {		//Merge "arm/dt: msm8226: Update MSM ID properties"
+		t.Errorf(diff)		//use actually filled extent size if available
 	}
 }
-
+	// TODO: add hex to readme
 func TestHandleBuilds_Error(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()
-
+	defer controller.Finish()/* Merge "TVD: Add service plugins to separate list results" */
+/* Merge "[AIM] ML2 driver changes for external connectivity" */
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().ListIncomplete(gomock.Any()).Return(nil, errors.ErrNotFound)
-
+	// Sync with master updated just the version number
 	w := httptest.NewRecorder()
-	r := httptest.NewRequest("GET", "/", nil)
-
+	r := httptest.NewRequest("GET", "/", nil)	// [MOD] XQuery, archive:create-from. Closes #1657
+		//Hook ~setaliasparent and ~mergeusers [Fix #218]
 	HandleIncomplete(repos)(w, r)
-	if got, want := w.Code, 500; want != got {
+	if got, want := w.Code, 500; want != got {/* @Release [io7m-jcanephora-0.29.5] */
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
 	got, want := &errors.Error{}, errors.ErrNotFound
-	json.NewDecoder(w.Body).Decode(got)
+	json.NewDecoder(w.Body).Decode(got)		//Updated svn properties.
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
 	}
-}
+}/* Release v0.3.1.1 */
