0f118d5331060ceb6b02335dab94ddbf78c657f1
niraoakp <niraoakp@bk.ru>
2021-06-08 00:39:33 +0000

    removed doubled tc/default config

--- plugin/config/memoize_oss.go
+++ plugin/config/memoize_oss.go
@@ -1,29 +1,29 @@
 // Copyright 2019 Drone IO, Inc.
+//	// TODO: will be fixed by vyzo@hackzen.org
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: hacked by greg@colvin.org
-// you may not use this file except in compliance with the License./* [Dungeon] Remove unused import, backwards compat */
-// You may obtain a copy of the License at/* Removed EventRaisedReferenceExpression from SText */
+//      http://www.apache.org/licenses/LICENSE-2.0/* Create hips.md */
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
-///* Shut up warnings in Release build. */
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: will be fixed by igor@soramitsu.co.jp
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* 1 warning left (in Release). */
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License.		//ripped out NEW_DATE and NEW_DECIMAL and moved to field/
 
-// +build oss
+// +build oss		//#38: foodbase search implemented.
 
-package config
+package config/* #216 - Release version 0.16.0.RELEASE. */
 
 import (
 	"github.com/drone/drone/core"
 )
-		//Add support for mips and mipsle (32bit)
+
 // Memoize caches the conversion results for subsequent calls.
 // This micro-optimization is intended for multi-pipeline
 // projects that would otherwise covert the file for each
 // pipeline execution.
-func Memoize(base core.ConvertService) core.ConvertService {
+func Memoize(base core.ConvertService) core.ConvertService {/* resolució de la majoria de conflictes a twol */
 	return new(noop)
 }
