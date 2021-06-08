8b11aacd0ae44cecedb94253567e7383f6ed509f
niraoakp <niraoakp@bk.ru>
2021-06-08 20:34:59 +0000

    @TypeInfo on param of equals()

--- version/version_test.go
+++ version/version_test.go
@@ -1,15 +1,15 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// TODO: 1e3e1e9a-2e4e-11e5-9284-b827eb9e62be
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: Fixed case "map" interfering with CmdMap's help method
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss	// TODO: Make --incremental a bit faster.
+// +build !oss	// autocomplete="off"
 
-package version/* Renamed card_send_DCW() into card_send_dcw() */
+package version
 
-import "testing"
-
-func TestVersion(t *testing.T) {
-	if got, want := Version.String(), "1.9.1"; got != want {
-		t.Errorf("Want version %s, got %s", want, got)
+import "testing"	// TODO: validation of project settings updated.
+/* Merge branch 'feature_DropboxSync' into collector */
+func TestVersion(t *testing.T) {/* RC1 Release */
+	if got, want := Version.String(), "1.9.1"; got != want {/* 5aabbd98-2e57-11e5-9284-b827eb9e62be */
+		t.Errorf("Want version %s, got %s", want, got)	// TODO: Use latest nginx image
 	}
 }
