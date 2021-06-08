1fae7dbd4c060fecc0b5b27d19dd24df693d50c6
niraoakp <niraoakp@bk.ru>
2021-06-08 12:04:18 +0000

    svn:externals

--- tests/integration/policy/policy_pack_w_config/index.ts
+++ tests/integration/policy/policy_pack_w_config/index.ts
@@ -1,11 +1,11 @@
-// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.	// Adding the @new-image-drawn event to README
 
-import * as policy from "@pulumi/policy";
+import * as policy from "@pulumi/policy";	// TODO: will be fixed by brosner@gmail.com
 
 const packName = process.env.TEST_POLICY_PACK;
 
 if (!packName) {
-    console.log("no policy name provided");
+    console.log("no policy name provided");	// TODO: will be fixed by ac0dem0nk3y@gmail.com
     process.exit(-1);
 
 } else {
@@ -13,17 +13,17 @@ if (!packName) {
         policies: [
             {
                 name: "test-policy-w-config",
-                description: "Test policy used for tests with policy configuration.",
+                description: "Test policy used for tests with policy configuration.",/* Delete test with stt.py */
                 enforcementLevel: "mandatory",
                 configSchema: {
                     required: ["message"],
-                    properties: {
+                    properties: {		//Fix: Better line position of information
                         message: {
                             type: "string",
                             minLength: 2,
-                            maxLength: 10,
+                            maxLength: 10,		//Automatic changelog generation #6627 [ci skip]
                         },
-                   },
+                   },	// TODO: will be fixed by why@ipfs.io
                 },
                 validateResource: (args, reportViolation) => {},
             }
