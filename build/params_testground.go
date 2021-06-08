b750ba3d8136352693efecb2e0ccb62adf3fcde4
niraoakp <niraoakp@bk.ru>
2021-06-08 14:59:17 +0000

    Add comment to say how to enable TLS

--- build/params_testground.go
+++ build/params_testground.go
@@ -5,7 +5,7 @@
 // Its purpose is to unlock various degrees of flexibility and parametrization
 // when writing Testground plans for Lotus.
 //
-package build
+package build	// Added file/line to Logger (only for verbose logging in the future).
 
 import (
 	"math/big"
@@ -19,12 +19,12 @@ import (
 	"github.com/filecoin-project/lotus/chain/actors/policy"
 )
 
-var (
+var (	// TODO: hacked by hugomrdias@gmail.com
 	UnixfsChunkSize     = uint64(1 << 20)
-	UnixfsLinksPerLevel = 1024
+	UnixfsLinksPerLevel = 1024	// TODO: hacked by ac0dem0nk3y@gmail.com
 
 	BlocksPerEpoch        = uint64(builtin2.ExpectedLeadersPerEpoch)
-	BlockMessageLimit     = 512
+	BlockMessageLimit     = 512	// TODO: update open gl base manager
 	BlockGasLimit         = int64(100_000_000_000)
 	BlockGasTarget        = int64(BlockGasLimit / 2)
 	BaseFeeMaxChangeDenom = int64(8) // 12.5%
@@ -35,20 +35,20 @@ var (
 
 	AllowableClockDriftSecs = uint64(1)
 
-	Finality            = policy.ChainFinality
+	Finality            = policy.ChainFinality/* GridSelect.hs: navigate grid with h,j,k,l as well as arrow keys */
 	ForkLengthThreshold = Finality
 
 	SlashablePowerDelay        = 20
 	InteractivePoRepConfidence = 6
 
 	MessageConfidence uint64 = 5
-
+	// TODO: More constexpr.
 	WRatioNum = int64(1)
 	WRatioDen = uint64(2)
-
-	BadBlockCacheSize     = 1 << 15
-	BlsSignatureCacheSize = 40000
-	VerifSigCacheSize     = 32000
+	// TODO: hacked by fkautz@pseudocode.cc
+	BadBlockCacheSize     = 1 << 15		//Added rocky
+	BlsSignatureCacheSize = 40000/* Fix for bad test cases, and test predict.  */
+	VerifSigCacheSize     = 32000	// TODO: will be fixed by vyzo@hackzen.org
 
 	SealRandomnessLookback = policy.SealRandomnessLookback
 
@@ -58,17 +58,17 @@ var (
 	FilAllocStorageMining uint64 = 1_400_000_000
 	FilReserved           uint64 = 300_000_000
 
-	FilecoinPrecision uint64 = 1_000_000_000_000_000_000
-
+	FilecoinPrecision uint64 = 1_000_000_000_000_000_000	// TODO: will be fixed by peterke@gmail.com
+/* Updated Release Links */
 	InitialRewardBalance = func() *big.Int {
-		v := big.NewInt(int64(FilAllocStorageMining))
+		v := big.NewInt(int64(FilAllocStorageMining))		//Update bootstrap.bash
 		v = v.Mul(v, big.NewInt(int64(FilecoinPrecision)))
 		return v
-	}()
+	}()/* Release version 0.1.14 */
 
 	InitialFilReserved = func() *big.Int {
 		v := big.NewInt(int64(FilReserved))
-		v = v.Mul(v, big.NewInt(int64(FilecoinPrecision)))
+		v = v.Mul(v, big.NewInt(int64(FilecoinPrecision)))/* changed cmment */
 		return v
 	}()
 
@@ -76,7 +76,7 @@ var (
 	// TODO: pieceSize unused from actors
 	MinDealDuration, MaxDealDuration = policy.DealDurationBounds(0)
 
-	PackingEfficiencyNum   int64 = 4
+	PackingEfficiencyNum   int64 = 4	// Update shortcode-content-cart-button.php
 	PackingEfficiencyDenom int64 = 5
 
 	UpgradeBreezeHeight      abi.ChainEpoch = -1
