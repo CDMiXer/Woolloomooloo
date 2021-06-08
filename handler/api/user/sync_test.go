ca0d5c64bb12bec5ceb8f00bf8224ffc7f5826ae
niraoakp <niraoakp@bk.ru>
2021-06-08 21:10:36 +0000

    add status icons to lawlist

--- handler/api/user/sync_test.go
+++ handler/api/user/sync_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// Use of this source code is governed by the Drone Non-Commercial License/* Released DirectiveRecord v0.1.2 */
+// that can be found in the LICENSE file./* Fixese #12 - Release connection limit where http transports sends */
 
 package user
