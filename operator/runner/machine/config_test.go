292cbd26af1ddda6a709e0a7b95c784486f62fe2
niraoakp <niraoakp@bk.ru>
2021-06-08 17:31:03 +0000

    switched logging back to error level

--- operator/runner/machine/config_test.go
+++ operator/runner/machine/config_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// Merge "New default wallpaper for Hammerhead." into klp-dev
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+/* Update music_list.md */
 // +build !oss
-
+	// fixed segfault when remove desktop with task
 package machine
