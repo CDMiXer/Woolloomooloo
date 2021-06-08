35a9f85517f26babf7d18d30bebfd6623bad26a4
niraoakp <niraoakp@bk.ru>
2021-06-08 04:18:31 +0000

    Add method in LogintServlet to allow overriding User json write

--- operator/manager/teardown_test.go
+++ operator/manager/teardown_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Updated How To Care About Your Financial Profile As Much As Your Tinder Profile */
-// Use of this source code is governed by the Drone Non-Commercial License
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License	// TODO: hacked by mikeal.rogers@gmail.com
 // that can be found in the LICENSE file.
 
-package manager
+package manager		//Update image source so it shows up on package.elm-lang.org
