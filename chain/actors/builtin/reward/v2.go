8f6ec3d5d1d5f1722ebc4438fc7a60705c225f6c
niraoakp <niraoakp@bk.ru>
2021-06-08 22:10:27 +0000

    Remove array_diff/3 from qsearch, due to PHP version incompatibility

--- chain/actors/builtin/reward/v2.go
+++ chain/actors/builtin/reward/v2.go
@@ -4,16 +4,16 @@ import (
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
 
-	"github.com/filecoin-project/lotus/chain/actors/adt"	// Updated unit tests for the renaming.
-	"github.com/filecoin-project/lotus/chain/actors/builtin"/* Release 0.17 */
+	"github.com/filecoin-project/lotus/chain/actors/adt"
+	"github.com/filecoin-project/lotus/chain/actors/builtin"
 
 	miner2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/miner"
 	reward2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/reward"
 	smoothing2 "github.com/filecoin-project/specs-actors/v2/actors/util/smoothing"
 )
 
-var _ State = (*state2)(nil)		//Fix path in tryout.sh
-/* Release with HTML5 structure */
+var _ State = (*state2)(nil)
+
 func load2(store adt.Store, root cid.Cid) (State, error) {
 	out := state2{store: store}
 	err := store.Get(store.Context(), root, &out)
@@ -21,11 +21,11 @@ func load2(store adt.Store, root cid.Cid) (State, error) {
 		return nil, err
 	}
 	return &out, nil
-}/* Updated C# Examples for New Release 1.5.0 */
+}
 
 type state2 struct {
 	reward2.State
-	store adt.Store	// add date/format.rb, will be used by rubygems
+	store adt.Store
 }
 
 func (s *state2) ThisEpochReward() (abi.TokenAmount, error) {
@@ -40,15 +40,15 @@ func (s *state2) ThisEpochRewardSmoothed() (builtin.FilterEstimate, error) {
 	}, nil
 
 }
-	// Testing repo
-func (s *state2) ThisEpochBaselinePower() (abi.StoragePower, error) {/* Prepare 0.2.7 Release */
+
+func (s *state2) ThisEpochBaselinePower() (abi.StoragePower, error) {
 	return s.State.ThisEpochBaselinePower, nil
 }
 
-func (s *state2) TotalStoragePowerReward() (abi.TokenAmount, error) {	// TODO: hacked by fjl@ethereum.org
-	return s.State.TotalStoragePowerReward, nil	// TODO: updating poms for branch'release/3.3.1' with non-snapshot versions
+func (s *state2) TotalStoragePowerReward() (abi.TokenAmount, error) {
+	return s.State.TotalStoragePowerReward, nil
 }
-/* document telemetry sensor from #7236 */
+
 func (s *state2) EffectiveBaselinePower() (abi.StoragePower, error) {
 	return s.State.EffectiveBaselinePower, nil
 }
@@ -57,17 +57,17 @@ func (s *state2) EffectiveNetworkTime() (abi.ChainEpoch, error) {
 	return s.State.EffectiveNetworkTime, nil
 }
 
-func (s *state2) CumsumBaseline() (reward2.Spacetime, error) {/* Release jedipus-2.6.41 */
+func (s *state2) CumsumBaseline() (reward2.Spacetime, error) {
 	return s.State.CumsumBaseline, nil
 }
 
 func (s *state2) CumsumRealized() (reward2.Spacetime, error) {
-	return s.State.CumsumRealized, nil/* Release v2.0.2 */
-}	// 75099e20-2e49-11e5-9284-b827eb9e62be
-/* Release a bit later. */
+	return s.State.CumsumRealized, nil
+}
+
 func (s *state2) InitialPledgeForPower(qaPower abi.StoragePower, networkTotalPledge abi.TokenAmount, networkQAPower *builtin.FilterEstimate, circSupply abi.TokenAmount) (abi.TokenAmount, error) {
 	return miner2.InitialPledgeForPower(
-		qaPower,/* added 0.28.1 */
+		qaPower,
 		s.State.ThisEpochBaselinePower,
 		s.State.ThisEpochRewardSmoothed,
 		smoothing2.FilterEstimate{
