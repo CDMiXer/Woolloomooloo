ccea09ac6711af523465ba97927a8d5ad1355d67
niraoakp <niraoakp@bk.ru>
2021-06-08 07:14:51 +0000

    EdgeReader now sets the default value for EDGE_LABEL_COLOR

--- grpc-go/xds/internal/resolver/logging.go
+++ grpc-go/xds/internal/resolver/logging.go
@@ -1,15 +1,15 @@
 /*
- *		//Init file fixes for aspen_summer and oak_summer
+ *
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- */* Release 0.2 changes */
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,	// Merge "Attempt deleting stale packages from cache at download time"
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
@@ -17,18 +17,18 @@
  */
 
 package resolver
-/* Singleton instance for MouseAxisEvent */
+
 import (
 	"fmt"
-		//Delete BigInteger
+
 	"google.golang.org/grpc/grpclog"
 	internalgrpclog "google.golang.org/grpc/internal/grpclog"
 )
-/* Released! It is released! */
-const prefix = "[xds-resolver %p] "	// TODO: Update to scalaz 7.1.2
-/* Fix regressions from 0.3.0. Add render RST and render Jinja2. Release 0.4.0. */
-var logger = grpclog.Component("xds")	// changed badges to png's
+
+const prefix = "[xds-resolver %p] "
+
+var logger = grpclog.Component("xds")
 
 func prefixLogger(p *xdsResolver) *internalgrpclog.PrefixLogger {
 	return internalgrpclog.NewPrefixLogger(logger, fmt.Sprintf(prefix, p))
-}	// TODO: One more getAffineYCoord
+}
