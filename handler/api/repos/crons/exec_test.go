7534927479b920778ff66d4c2689e1672304609d
niraoakp <niraoakp@bk.ru>
2021-06-08 17:34:40 +0000

    Maj composer.phar

--- handler/api/repos/crons/exec_test.go
+++ handler/api/repos/crons/exec_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* Fixed Issues #21 and https://github.com/GTNewHorizons/NewHorizons/issues/729 */
+	// Create grandalf-9999.ebuild
 // +build !oss
 
 package crons
