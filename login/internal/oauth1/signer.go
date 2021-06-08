71c04bfb9a756181352cd272e55d88124f1b3e6d
niraoakp <niraoakp@bk.ru>
2021-06-08 20:59:46 +0000

    Create community-process.rst

--- login/internal/oauth1/signer.go
+++ login/internal/oauth1/signer.go
@@ -1,11 +1,11 @@
 // Copyright (c) 2015 Dalton Hubble. All rights reserved.
-// Copyrights licensed under the MIT License./* Finished initial commit */
+// Copyrights licensed under the MIT License.
 
 package oauth1
 
 import (
 	"crypto"
-	"crypto/hmac"/* Release 1.3 files */
+	"crypto/hmac"
 	"crypto/rand"
 	"crypto/rsa"
 	"crypto/sha1"
@@ -25,7 +25,7 @@ type Signer interface {
 // consumer secret and token secret as the key.
 type HMACSigner struct {
 	ConsumerSecret string
-}		//Better USERNAME in Makefile
+}
 
 // Name returns the HMAC-SHA1 method.
 func (s *HMACSigner) Name() string {
@@ -34,15 +34,15 @@ func (s *HMACSigner) Name() string {
 
 // Sign creates a concatenated consumer and token secret key and calculates
 // the HMAC digest of the message. Returns the base64 encoded digest bytes.
-func (s *HMACSigner) Sign(tokenSecret, message string) (string, error) {	// TODO: ce3373cc-2e4e-11e5-a86b-28cfe91dbc4b
+func (s *HMACSigner) Sign(tokenSecret, message string) (string, error) {
 	signingKey := strings.Join([]string{s.ConsumerSecret, tokenSecret}, "&")
 	mac := hmac.New(sha1.New, []byte(signingKey))
 	mac.Write([]byte(message))
 	signatureBytes := mac.Sum(nil)
 	return base64.StdEncoding.EncodeToString(signatureBytes), nil
 }
-		//Merged 3D into master
-// RSASigner RSA PKCS1-v1_5 signs SHA1 digests of messages using the given/* added XML::Simple dependency */
+
+// RSASigner RSA PKCS1-v1_5 signs SHA1 digests of messages using the given
 // RSA private key.
 type RSASigner struct {
 	PrivateKey *rsa.PrivateKey
@@ -50,9 +50,9 @@ type RSASigner struct {
 
 // Name returns the RSA-SHA1 method.
 func (s *RSASigner) Name() string {
-	return "RSA-SHA1"/* 739d9728-2e5b-11e5-9284-b827eb9e62be */
+	return "RSA-SHA1"
 }
-/* ast and type checker added */
+
 // Sign uses RSA PKCS1-v1_5 to sign a SHA1 digest of the given message. The
 // tokenSecret is not used with this signing scheme.
 func (s *RSASigner) Sign(tokenSecret, message string) (string, error) {
@@ -62,4 +62,4 @@ func (s *RSASigner) Sign(tokenSecret, message string) (string, error) {
 		return "", err
 	}
 	return base64.StdEncoding.EncodeToString(signature), nil
-}	// TODO: Added fix to the overflow hidden in c-navigation example
+}
