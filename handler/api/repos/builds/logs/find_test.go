14723961ba0bafe6584441e7fa5a19c4d992aba9
niraoakp <niraoakp@bk.ru>
2021-06-08 12:25:46 +0000

    6a2777dc-2e5a-11e5-9284-b827eb9e62be

--- handler/api/repos/builds/logs/find_test.go
+++ handler/api/repos/builds/logs/find_test.go
@@ -2,4 +2,4 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-package logs
+package logs/* Merge "Release note for backup filtering" */
