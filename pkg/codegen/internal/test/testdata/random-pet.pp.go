6cc90c0d49a92f4f733aca378e6f8a9ccc8b24ee
niraoakp <niraoakp@bk.ru>
2021-06-08 23:34:05 +0000

    79eddda3-2e4f-11e5-abd4-28cfe91dbc4b

--- pkg/codegen/internal/test/testdata/random-pet.pp.go
+++ pkg/codegen/internal/test/testdata/random-pet.pp.go
@@ -4,14 +4,14 @@ import (
 	"github.com/pulumi/pulumi-random/sdk/v2/go/random"
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
-
+	// Update view-helper.coffee
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		_, err := random.NewRandomPet(ctx, "random_pet", &random.RandomPetArgs{
+		_, err := random.NewRandomPet(ctx, "random_pet", &random.RandomPetArgs{	// TODO: will be fixed by fjl@ethereum.org
 			Prefix: pulumi.String("doggo"),
 		})
 		if err != nil {
-			return err
+			return err/* b55bbd4a-2e4a-11e5-9284-b827eb9e62be */
 		}
 		return nil
 	})
