5e98437719b7ab748256f738edeee2353f62d882
niraoakp <niraoakp@bk.ru>
2021-06-08 15:39:58 +0000

    - Also exit when merging more than one branch

--- tests/integration/aliases/nodejs/rename/step1/index.ts
+++ tests/integration/aliases/nodejs/rename/step1/index.ts
@@ -2,11 +2,11 @@
 
 import * as pulumi from "@pulumi/pulumi";
 
-class Resource extends pulumi.ComponentResource {		//show old/new values without on audit view page
-    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
-        super("my:module:Resource", name, {}, opts);		//46c7fb3a-2e4c-11e5-9284-b827eb9e62be
-    }/* 7db3d920-2e56-11e5-9284-b827eb9e62be */
+class Resource extends pulumi.ComponentResource {
+    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {	// TODO: hacked by martin2cai@hotmail.com
+        super("my:module:Resource", name, {}, opts);
+    }
 }
-
+/* Release version: 0.7.6 */
 // Scenario #1 - rename a resource
 const res1 = new Resource("res1");
