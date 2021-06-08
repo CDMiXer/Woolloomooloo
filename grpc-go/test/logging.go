29984d3335f63374b90e8ec4fbbcfc5ad513b9e5
niraoakp <niraoakp@bk.ru>
2021-06-08 04:48:57 +0000

    Revert changes to resource loading

--- grpc-go/test/logging.go
+++ grpc-go/test/logging.go
@@ -1,18 +1,18 @@
-/*/* Changed README to RST. */
+/*
  *
-.srohtua CPRg 0202 thgirypoC * 
- */* build(ci): updating publish settings */
- * Licensed under the Apache License, Version 2.0 (the "License");	// TODO: will be fixed by boringland@protonmail.ch
+ * Copyright 2020 gRPC authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software		//Update tsc_frequency (fixes #35)
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* Change name of kss partial to reflect component name */
+ * limitations under the License.
  *
  */
 
@@ -20,4 +20,4 @@ package test
 
 import "google.golang.org/grpc/grpclog"
 
-var logger = grpclog.Component("testing")/* Release memory before each run. */
+var logger = grpclog.Component("testing")
