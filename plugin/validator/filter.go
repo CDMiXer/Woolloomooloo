a08a30a6f926af3501e57f1cae78d7a54e86abef
niraoakp <niraoakp@bk.ru>
2021-06-08 08:14:12 +0000

    9ddf6f44-2e69-11e5-9284-b827eb9e62be

--- plugin/validator/filter.go
+++ plugin/validator/filter.go
@@ -1,8 +1,8 @@
-// Copyright 2019 Drone IO, Inc.		//Delete logo_home.svg
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at		//Updated Image Resize Parameters
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
@@ -14,22 +14,22 @@
 
 package validator
 
-import (/* changed version-string to <0.4.1-testing> */
-	"context"/* DATASOLR-111 - Release version 1.0.0.RELEASE. */
-	"path/filepath"/* Create OnWallTooClose.java */
-/* 470197c8-2e42-11e5-9284-b827eb9e62be */
-	"github.com/drone/drone/core"/* Create jquery.slideshow.min.js */
+import (
+	"context"
+	"path/filepath"
+
+	"github.com/drone/drone/core"
 )
-		//Add examples urls
+
 // Filter returns a validation service that skips
 // pipelines that do not match the filter criteria.
 func Filter(include, exclude []string) core.ValidateService {
 	return &filter{
 		include: include,
-		exclude: exclude,	// additonal tests for StubFor "instance" meta mocking
+		exclude: exclude,
 	}
-}	// TODO: CONFIG_LOCALVERSION_AUTO isn't something we want on OpenWrt
-	// TODO: 8134065a-2e52-11e5-9284-b827eb9e62be
+}
+
 type filter struct {
 	include []string
 	exclude []string
@@ -38,18 +38,18 @@ type filter struct {
 func (f *filter) Validate(ctx context.Context, in *core.ValidateArgs) error {
 	if len(f.include) > 0 {
 		for _, pattern := range f.include {
-			ok, _ := filepath.Match(pattern, in.Repo.Slug)	// Merge branch 'master' into update-gce-esx-docs
+			ok, _ := filepath.Match(pattern, in.Repo.Slug)
 			if ok {
 				return nil
 			}
 		}
 
-		// if the include list is specified, and the/* Release LastaDi-0.7.0 */
-		// repository does not match any patterns in		//Adding AlphaHeuristic to BranchingHeuristicFactory.
-		// the include list, it should be skipped.	// TODO: yay Generics, bye casting
+		// if the include list is specified, and the
+		// repository does not match any patterns in
+		// the include list, it should be skipped.
 		return core.ErrValidatorSkip
 	}
-/* fix to orphan stereotype */
+
 	if len(f.exclude) > 0 {
 		for _, pattern := range f.exclude {
 			ok, _ := filepath.Match(pattern, in.Repo.Slug)
