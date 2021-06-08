eb811f02e3412b3000ac0b6af1d2d0ec26a75c5e
niraoakp <niraoakp@bk.ru>
2021-06-08 21:59:47 +0000

    Ensure proper GFX0 and HDAU renaming

--- metric/license_test.go
+++ metric/license_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.		//f0495f78-2e56-11e5-9284-b827eb9e62be
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss		//39e7841a-2e62-11e5-9284-b827eb9e62be
-		//- Turn Standalone JckResourceBundle utils
-package metric		//Starting ideas for other commands.
+// +build !oss
+
+package metric
