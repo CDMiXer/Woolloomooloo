e2c559cbaf1d4f1bab63e9a06ee14eac2e78749a
niraoakp <niraoakp@bk.ru>
2021-06-08 11:13:41 +0000

    WIP: updated qualyfying and race modes initialization.

--- tests/integration/empty/dotnet/Program.cs
+++ tests/integration/empty/dotnet/Program.cs
@@ -1,4 +1,4 @@
-﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved./* Merge "Remove logs Releases from UI" */
+﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
 
 using System.Threading.Tasks;
 using Pulumi;
@@ -9,4 +9,4 @@ class Program
     {
         return Deployment.RunAsync(() => {});
     }
-}/* [data_set] Be more generic about extracting content from nested hashes */
+}
