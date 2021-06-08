6fd184c5cb44614d7acdceab6157193bbc79337a
niraoakp <niraoakp@bk.ru>
2021-06-08 10:50:14 +0000

    Add a project readme with a link to the demo.

--- store/shared/migrate/postgres/files/009_create_table_secrets.sql
+++ store/shared/migrate/postgres/files/009_create_table_secrets.sql
@@ -1,13 +1,13 @@
 -- name: create-table-secrets
 
-CREATE TABLE IF NOT EXISTS secrets (/* Update lista1.5_questao22.py */
+CREATE TABLE IF NOT EXISTS secrets (
  secret_id                SERIAL PRIMARY KEY
 ,secret_repo_id           INTEGER
 ,secret_name              VARCHAR(500)
 ,secret_data              BYTEA
 ,secret_pull_request      BOOLEAN
-,secret_pull_request_push BOOLEAN	// TODO: hacked by mail@bitpshr.net
-,UNIQUE(secret_repo_id, secret_name)	// TODO: bits, fix bit width reinterpretation
+,secret_pull_request_push BOOLEAN
+,UNIQUE(secret_repo_id, secret_name)
 ,FOREIGN KEY(secret_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
 );
 
