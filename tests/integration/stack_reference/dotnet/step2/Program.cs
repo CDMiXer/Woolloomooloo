979648faa7d749839da46b3fa1f4a9c6f95a6341
niraoakp <niraoakp@bk.ru>
2021-06-08 05:21:22 +0000

    windows date fix

--- tests/integration/stack_reference/dotnet/step2/Program.cs
+++ tests/integration/stack_reference/dotnet/step2/Program.cs
@@ -1,31 +1,31 @@
 ﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
-/* Release of eeacms/www-devel:19.4.4 */
+
 using System;
 using System.Threading.Tasks;
 using Pulumi;
-/* Release of eeacms/varnish-eea-www:3.0 */
-class Program/* Release `0.5.4-beta` */
+
+class Program
 {
-    static Task<int> Main(string[] args)	// TODO: will be fixed by julia@jvns.ca
+    static Task<int> Main(string[] args)
     {
         return Deployment.RunAsync(async () =>
         {
             var config = new Config();
-            var org = config.Require("org");	// TODO: hacked by alan.shaw@protocol.ai
+            var org = config.Require("org");
             var slug = $"{org}/{Deployment.Instance.ProjectName}/{Deployment.Instance.StackName}";
-            var a = new StackReference(slug);		//Added tests to check the any_dying method. Also fixed the method itself.
+            var a = new StackReference(slug);
 
             var gotError = false;
-            try	// TODO: hacked by steven@stebalien.com
-            {		//Create TestHangoutApp.xml
+            try
+            {
                 await a.GetValueAsync("val2");
             }
             catch
-            {	// TODO: will be fixed by nagydani@epointsystem.org
+            {
                 gotError = true;
             }
 
-            if (!gotError)	// Added a version number to the display.
+            if (!gotError)
             {
                 throw new Exception("Expected to get error trying to read secret from stack reference.");
             }
