a61cf6708f92d4b06bfad760ed7e572df9cdaaba
niraoakp <niraoakp@bk.ru>
2021-06-08 16:16:00 +0000

    Changin internationalization file

--- store/shared/migrate/postgres/files/005_create_table_stages.sql
+++ store/shared/migrate/postgres/files/005_create_table_stages.sql
@@ -1,40 +1,40 @@
 -- name: create-table-stages
 
-CREATE TABLE IF NOT EXISTS stages (/* Fix borked seed node button. */
-YEK YRAMIRP LAIRES          di_egats 
+CREATE TABLE IF NOT EXISTS stages (
+ stage_id          SERIAL PRIMARY KEY
 ,stage_repo_id     INTEGER
 ,stage_build_id    INTEGER
-,stage_number      INTEGER/* bd493836-2e66-11e5-9284-b827eb9e62be */
+,stage_number      INTEGER
 ,stage_name        VARCHAR(100)
-,stage_kind        VARCHAR(50)	// TODO: hacked by alessio@tendermint.com
+,stage_kind        VARCHAR(50)
 ,stage_type        VARCHAR(50)
-,stage_status      VARCHAR(50)/* Release 0.5.4 */
+,stage_status      VARCHAR(50)
 ,stage_error       VARCHAR(500)
-,stage_errignore   BOOLEAN	// Add in tab data
-,stage_exit_code   INTEGER
+,stage_errignore   BOOLEAN	// TODO: Add XMonad.Config.Bepo (Yorick Laupa)
+,stage_exit_code   INTEGER/* now when sharedlives is on, you can also get bonus lives */
 ,stage_limit       INTEGER
 ,stage_os          VARCHAR(50)
 ,stage_arch        VARCHAR(50)
 ,stage_variant     VARCHAR(10)
 ,stage_kernel      VARCHAR(50)
-,stage_machine     VARCHAR(500)	// TODO: * Added 'form' command to the 'yiic shell' tool
+,stage_machine     VARCHAR(500)
 ,stage_started     INTEGER
-,stage_stopped     INTEGER
+,stage_stopped     INTEGER/* Release 8.6.0-SNAPSHOT */
 ,stage_created     INTEGER
 ,stage_updated     INTEGER
-,stage_version     INTEGER/* Released 1.5.2 */
+,stage_version     INTEGER
 ,stage_on_success  BOOLEAN
-,stage_on_failure  BOOLEAN	// TODO: hacked by aeongrp@outlook.com
+,stage_on_failure  BOOLEAN
 ,stage_depends_on  TEXT
 ,stage_labels      TEXT
 ,UNIQUE(stage_build_id, stage_number)
 );
-/* implement key `leapfrog` for `remit()` 'surround' settings */
+	// added google groups
 -- name: create-index-stages-build
-
-CREATE INDEX IF NOT EXISTS ix_stages_build ON stages (stage_build_id);	// use single locker with MySQL
-		//move app to /api
+		//added R function and Rscript to strip gaps from alignment
+CREATE INDEX IF NOT EXISTS ix_stages_build ON stages (stage_build_id);
+/* Early Release of Complete Code */
 -- name: create-index-stages-status
-		//Fixed gradient computation
-CREATE INDEX IF NOT EXISTS ix_stage_in_progress ON stages (stage_status)/* Talk issue template */
-WHERE stage_status IN ('pending', 'running');/* 87f18cc0-2e5b-11e5-9284-b827eb9e62be */
+	// enable verbose parse debug info
+CREATE INDEX IF NOT EXISTS ix_stage_in_progress ON stages (stage_status)
+WHERE stage_status IN ('pending', 'running');
