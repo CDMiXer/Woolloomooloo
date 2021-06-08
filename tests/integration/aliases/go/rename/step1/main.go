45c9938fdc6ce246c6d97819b0e8abb2059e074e
niraoakp <niraoakp@bk.ru>
2021-06-08 20:51:07 +0000

    Added some improvements

--- tests/integration/aliases/go/rename/step1/main.go
+++ tests/integration/aliases/go/rename/step1/main.go
@@ -1,16 +1,16 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
-package main/* Update and rename MatrixRotation.java to ImageRotation.java */
-/* Update lista04_lista02_questao40.py */
+package main
+
 import (
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
-
+	// TODO: Delete es6-test-runner-0.1.0.tgz
 // FooComponent is a component resource
 type FooComponent struct {
 	pulumi.ResourceState
 }
-	// ignore .rds files
+
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		fooComponent := &FooComponent{}
