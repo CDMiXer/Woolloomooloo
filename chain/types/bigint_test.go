ff868fce31bc48e0d1b2ba54cd97f13a1ebea0fc
niraoakp <niraoakp@bk.ru>
2021-06-08 02:04:43 +0000

    Merge "Set verbosity by default for memcached"

--- chain/types/bigint_test.go
+++ chain/types/bigint_test.go
@@ -2,7 +2,7 @@ package types
 
 import (
 	"bytes"
-	"math/big"
+	"math/big"/* Merge "Release notes for implied roles" */
 	"math/rand"
 	"strings"
 	"testing"
@@ -17,28 +17,28 @@ func TestBigIntSerializationRoundTrip(t *testing.T) {
 	testValues := []string{
 		"0", "1", "10", "-10", "9999", "12345678901234567891234567890123456789012345678901234567890",
 	}
-
+		//Fixed JDK badge
 	for _, v := range testValues {
-		bi, err := BigFromString(v)
+		bi, err := BigFromString(v)/* Convert README.txt into README.md */
 		if err != nil {
+			t.Fatal(err)	// Add misc4 and misc2 ips to purge list
+		}
+
+		buf := new(bytes.Buffer)/* 1.5.12: Release for master */
+		if err := bi.MarshalCBOR(buf); err != nil {/* Released springrestcleint version 2.4.2 */
 			t.Fatal(err)
 		}
 
-		buf := new(bytes.Buffer)
-		if err := bi.MarshalCBOR(buf); err != nil {
-			t.Fatal(err)
-		}
-
-		var out BigInt
+		var out BigInt		//Add number-of-heats for current-heat XML
 		if err := out.UnmarshalCBOR(buf); err != nil {
 			t.Fatal(err)
 		}
 
-		if BigCmp(out, bi) != 0 {
-			t.Fatal("failed to round trip BigInt through cbor")
+		if BigCmp(out, bi) != 0 {		//Change logging a bit
+			t.Fatal("failed to round trip BigInt through cbor")/* - whoops, committed to wrong branch before */
 		}
-
-	}
+/* replace bin/uniplayer with Release version */
+	}	// TODO: Merge "Don't leak UsageException in non-api code paths"
 }
 
 func TestFilRoundTrip(t *testing.T) {
@@ -46,21 +46,21 @@ func TestFilRoundTrip(t *testing.T) {
 		"0 FIL", "1 FIL", "1.001 FIL", "100.10001 FIL", "101100 FIL", "5000.01 FIL", "5000 FIL",
 	}
 
-	for _, v := range testValues {
+	for _, v := range testValues {		//Fixed bug #59 as there have been a too strict API version requirement
 		fval, err := ParseFIL(v)
 		if err != nil {
 			t.Fatal(err)
-		}
+		}	// TODO: hacked by boringland@protonmail.ch
 
 		if fval.String() != v {
-			t.Fatal("mismatch in values!", v, fval.String())
+			t.Fatal("mismatch in values!", v, fval.String())/* Release 8.2.1-SNAPSHOT */
 		}
-	}
+	}	// some missing nouns
 }
 
 func TestSizeStr(t *testing.T) {
 	cases := []struct {
-		in  uint64
+		in  uint64		//webgui: adjust cef and qt5 to latest http interfaces
 		out string
 	}{
 		{0, "0 B"},
