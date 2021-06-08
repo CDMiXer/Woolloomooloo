4ca1b9555229176b827f88b7892d729afabfbd0a
niraoakp <niraoakp@bk.ru>
2021-06-08 08:39:40 +0000

    [Fix] allow “throws” to be in bracket notation.

--- plugin/config/jsonnet_oss.go
+++ plugin/config/jsonnet_oss.go
@@ -1,24 +1,24 @@
-// Copyright 2019 Drone IO, Inc./* Treat IDs as strings */
+// Copyright 2019 Drone IO, Inc.	// fixes lp:358595
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Merge "Release notes: deprecate dind" */
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Release of eeacms/www-devel:18.7.11 */
-// distributed under the License is distributed on an "AS IS" BASIS,/* remove planet icon stuff, since we're not porting that right now */
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.		//Create mariadb.service
+// limitations under the License.
 
 // +build oss
-/* Release of eeacms/www-devel:20.6.20 */
+	// Merge "Prompt for auth options"
 package config
-/* Upgrade verification 4 to support bulk verification across the entire grid. */
+
 import "github.com/drone/drone/core"
 
-// Jsonnet returns a no-op configuration service./* Update eyed3 from 0.8.3 to 0.8.4 */
-func Jsonnet(service core.FileService, enabled bool) core.ConfigService {
-	return new(noop)/* [Maven Release]-prepare release components-parent-1.0.1 */
+// Jsonnet returns a no-op configuration service./* Release of eeacms/www-devel:18.3.23 */
+func Jsonnet(service core.FileService, enabled bool) core.ConfigService {		//Merge branch 'master' into feature/regression-org-cmts
+	return new(noop)
 }
