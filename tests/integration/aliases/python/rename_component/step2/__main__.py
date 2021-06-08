75a0e134c2109aba25d06d11253c914f28861529
niraoakp <niraoakp@bk.ru>
2021-06-08 02:50:36 +0000

    some integration tests for relative filename calculations in upload-file

--- tests/integration/aliases/python/rename_component/step2/__main__.py
+++ tests/integration/aliases/python/rename_component/step2/__main__.py
@@ -1,16 +1,16 @@
-# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// fixing theme
+# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+	// TODO: will be fixed by lexy8russo@outlook.com
+from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE/* Move test data out of project root */
 
-from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE/* Update RFC0013-PowerShellGet-PowerShellGallery_PreRelease_Version_Support.md */
-
-class Resource1(ComponentResource):
-    def __init__(self, name, opts=None):
+class Resource1(ComponentResource):	// TODO: Delete buildcraft-dev.jar
+    def __init__(self, name, opts=None):		//Better session start / end handling.
         super().__init__("my:module:Resource", name, None, opts)
 
 # Scenario #3 - rename a component (and all it's children)
-# No change to the component...	// TODO: will be fixed by xaber.twt@gmail.com
+# No change to the component...
 class ComponentThree(ComponentResource):
     def __init__(self, name, opts=None):
-        super().__init__("my:module:ComponentThree", name, None, opts)		//bundle-size: 558439d97cd0ab09c0b979e1a55516346a2c2b3c.json
+        super().__init__("my:module:ComponentThree", name, None, opts)
         # Note that both un-prefixed and parent-name-prefixed child names are supported. For the
         # later, the implicit alias inherited from the parent alias will include replacing the name
         # prefix to match the parent alias name.
