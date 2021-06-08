2bc2ab9bbead0ecd399d9c15b152cbc87c82d637
niraoakp <niraoakp@bk.ru>
2021-06-08 15:02:46 +0000

    JDBC and JDBCTemplate

--- tests/integration/read/read_replace/step3/index.ts
+++ tests/integration/read/read_replace/step3/index.ts
@@ -1,17 +1,17 @@
 // Copyright 2016-2018, Pulumi Corporation.
-//
+///* Release 10.0 */
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// Unless required by applicable law or agreed to in writing, software	// TODO: Made a few Strings easier to understand
+// distributed under the License is distributed on an "AS IS" BASIS,	// Update sPropsCreate.sh
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Mapping: Add #extensions_for
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+/* Release of eeacms/ims-frontend:0.4.0-beta.2 */
 import { Resource } from "./resource";
 
 // Now go back the other way and make sure that "A" is external again.
