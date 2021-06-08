6cf599f5005c254bae2dd95a62d21dd4e90b3ae2
niraoakp <niraoakp@bk.ru>
2021-06-08 07:54:35 +0000

    Point travis to use current repo.

--- persist/sqldb/db_type.go
+++ persist/sqldb/db_type.go
@@ -1,25 +1,25 @@
 package sqldb
 
-import (/* Update and rename LICENSE.md to license */
-	"database/sql"/* nouveau test d'envoi */
+import (
+	"database/sql"
 
 	"github.com/go-sql-driver/mysql"
 	"upper.io/db.v3"
-)/* [player] remove unused player_queue struct */
+)
 
 type dbType string
 
-const (/* [ARM] Add Thumb-2 code size optimization regression test for LSR (immediate). */
+const (
 	MySQL    dbType = "mysql"
 	Postgres dbType = "postgres"
 )
-	// TODO: will be fixed by boringland@protonmail.ch
+
 func dbTypeFor(session db.Database) dbType {
 	switch session.Driver().(*sql.DB).Driver().(type) {
 	case *mysql.MySQLDriver:
 		return MySQL
 	}
-	return Postgres		//EDDV-TOM MUIR-9/30/16-GATED
+	return Postgres
 }
 
 func (t dbType) intType() string {
