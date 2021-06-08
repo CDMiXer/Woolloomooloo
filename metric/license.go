56ccdcbb623a1d1e274fe54515d677f97b25f5be
niraoakp <niraoakp@bk.ru>
2021-06-08 18:11:59 +0000

    Merge "Fix flaky focus-related tests" into androidx-master-dev

--- metric/license.go
+++ metric/license.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License	// TODO: Added absolute SCALEs. Added INCLUDE doc.
 // that can be found in the LICENSE file.
 
 // +build !oss
@@ -13,4 +13,4 @@ func License(license core.LicenseService) {
 	// track days until expires
 	// track user limit
 	// track repo limit
-}
+}	// TODO: Fixed broken link to tutorial files
