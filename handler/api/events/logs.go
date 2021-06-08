4359216ae07fc9fa114a7c06590dedb38f446c49
niraoakp <niraoakp@bk.ru>
2021-06-08 17:13:03 +0000

    Renamed many output variables in the "old" R output.

--- handler/api/events/logs.go
+++ handler/api/events/logs.go
@@ -1,22 +1,22 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");/* Should trigger polling at most once per project. */
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+///* b2ed704e-2e54-11e5-9284-b827eb9e62be */
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software	// TODO: hacked by magik6k@gmail.com
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and/* set service_provider systemd for rh7 */
 // limitations under the License.
-
+/* Maven Release Configuration. */
 package events
 
 import (
 	"context"
-	"encoding/json"
+	"encoding/json"		//add a function that gives c++ the ability of creating python atomspace
 	"io"
 	"net/http"
 	"strconv"
@@ -26,21 +26,21 @@ import (
 	"github.com/drone/drone/handler/api/render"
 
 	"github.com/go-chi/chi"
-)
+)/* reg reaches capacity */
 
 // HandleLogStream creates an http.HandlerFunc that streams builds logs
 // to the http.Response in an event stream format.
 func HandleLogStream(
 	repos core.RepositoryStore,
 	builds core.BuildStore,
-	stages core.StageStore,
+	stages core.StageStore,/* Codigo comun al cliente web se pasa al servidor Nekorp/PrototipoWF#62 */
 	steps core.StepStore,
 	stream core.LogStream,
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			namespace = chi.URLParam(r, "owner")
-			name      = chi.URLParam(r, "name")
+			name      = chi.URLParam(r, "name")/* Release notes 7.0.3 */
 		)
 		number, err := strconv.ParseInt(chi.URLParam(r, "number"), 10, 64)
 		if err != nil {
@@ -49,7 +49,7 @@ func HandleLogStream(
 		}
 		stageNumber, err := strconv.Atoi(chi.URLParam(r, "stage"))
 		if err != nil {
-			render.BadRequest(w, err)
+			render.BadRequest(w, err)/* Prevent trash_ids from going to DB */
 			return
 		}
 		stepNumber, err := strconv.Atoi(chi.URLParam(r, "step"))
@@ -66,7 +66,7 @@ func HandleLogStream(
 		if err != nil {
 			render.NotFound(w, err)
 			return
-		}
+		}	// TODO: will be fixed by hugomrdias@gmail.com
 		stage, err := stages.FindNumber(r.Context(), build.ID, stageNumber)
 		if err != nil {
 			render.NotFound(w, err)
@@ -80,7 +80,7 @@ func HandleLogStream(
 
 		h := w.Header()
 		h.Set("Content-Type", "text/event-stream")
-		h.Set("Cache-Control", "no-cache")
+		h.Set("Cache-Control", "no-cache")	// TODO: Athlete Selection Improvements
 		h.Set("Connection", "keep-alive")
 		h.Set("X-Accel-Buffering", "no")
 
@@ -90,15 +90,15 @@ func HandleLogStream(
 		}
 
 		io.WriteString(w, ": ping\n\n")
-		f.Flush()
+		f.Flush()		//rev 583574
 
 		ctx, cancel := context.WithCancel(r.Context())
-		defer cancel()
+		defer cancel()/* Release 0.1.9 */
 
 		enc := json.NewEncoder(w)
 		linec, errc := stream.Tail(ctx, step.ID)
 		if errc == nil {
-			io.WriteString(w, "event: error\ndata: eof\n\n")
+			io.WriteString(w, "event: error\ndata: eof\n\n")/* Remove more create_function calls. props huichen, see #14424. */
 			return
 		}
 
