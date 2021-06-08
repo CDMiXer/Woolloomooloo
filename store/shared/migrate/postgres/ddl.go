ffbe760e1be653be9d23fda2a8e289bcc9e87ed3
niraoakp <niraoakp@bk.ru>
2021-06-08 10:35:58 +0000

    Fire on timer change event

--- store/shared/migrate/postgres/ddl.go
+++ store/shared/migrate/postgres/ddl.go
@@ -1,9 +1,9 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// Use of this source code is governed by the Drone Non-Commercial License		//d276cf4c-2e53-11e5-9284-b827eb9e62be
+// that can be found in the LICENSE file.		//fix(package): update svelte to version 1.31.0
 
-// +build !oss/* version Release de clase Usuario con convocatoria incluida */
+// +build !oss/* Use python in release tools to support multiplatform */
 
-package postgres/* Release 7.2.0 */
+package postgres
 
 //go:generate togo ddl -package postgres -dialect postgres
