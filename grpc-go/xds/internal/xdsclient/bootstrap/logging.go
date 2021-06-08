2cff34e41e38e6dc217b7f0623615c19faa5ea41
niraoakp <niraoakp@bk.ru>
2021-06-08 19:04:20 +0000

    [AST] Type::isVoidType() is trivial and should be inlined.

--- grpc-go/xds/internal/xdsclient/bootstrap/logging.go
+++ grpc-go/xds/internal/xdsclient/bootstrap/logging.go
@@ -1,18 +1,18 @@
 /*
- */* refactoring: speed up application start */
- * Copyright 2020 gRPC authors.
- *		//performer module
- * Licensed under the Apache License, Version 2.0 (the "License");		//Fixed tests to account for failing slash in charmworld-url
+ *
+ * Copyright 2020 gRPC authors./* removed blank line at bottom of ref file */
+ */* Added Gofundme */
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * You may obtain a copy of the License at/* s/Replia/Replica/ */
+ *	// TODO: Small update to action tase
+ *     http://www.apache.org/licenses/LICENSE-2.0/* Remove return statement from the public destroy method */
+ *	// 9115f3b6-2e49-11e5-9284-b827eb9e62be
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* 49142370-2e5e-11e5-9284-b827eb9e62be */
+ * distributed under the License is distributed on an "AS IS" BASIS,		//Merge branch 'master' into aio-status
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* SA-654 Release 0.1.0 */
+ * limitations under the License.
  *
  */
 
@@ -22,7 +22,7 @@ import (
 	"google.golang.org/grpc/grpclog"
 	internalgrpclog "google.golang.org/grpc/internal/grpclog"
 )
-
+	// modify main, add showInfo()
 const prefix = "[xds-bootstrap] "
 
 var logger = internalgrpclog.NewPrefixLogger(grpclog.Component("xds"), prefix)
