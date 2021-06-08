72b1c81b741cefe28926ded0f813f23315e91dc3
niraoakp <niraoakp@bk.ru>
2021-06-08 21:34:04 +0000

    8c0e976c-2e42-11e5-9284-b827eb9e62be

--- handler/api/repos/builds/branches/delete.go
+++ handler/api/repos/builds/branches/delete.go
@@ -4,59 +4,59 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: Desy product: reset configuration of lib and base feature
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
-/* Merge "Remove the redundant mock patches in tests" */
-package branches
+// limitations under the License./* Release v0.1.1 */
 
+package branches
+		//Making sure admin user has unique ID
 import (
-	"net/http"		//Delete list.brs
+	"net/http"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/logger"
-
+	"github.com/drone/drone/handler/api/render"		//Merge "Remove ovsapp references form .coverage file"
+	"github.com/drone/drone/logger"/* Release 2.2.2.0 */
+/* Release of eeacms/plonesaas:5.2.1-23 */
 	"github.com/go-chi/chi"
-)		//Clipped area support for spritesheets
+)
 
 // HandleDelete returns an http.HandlerFunc that handles an
 // http.Request to delete a branch entry from the datastore.
 func HandleDelete(
 	repos core.RepositoryStore,
-	builds core.BuildStore,
+	builds core.BuildStore,/* chore(package): update husky to version 2.4.0 */
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
-			namespace = chi.URLParam(r, "owner")/* Release 0.6.1. */
+			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
-			branch    = chi.URLParam(r, "*")/* Release version 2.0.0 */
+			branch    = chi.URLParam(r, "*")
 		)
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
-			render.NotFound(w, err)
+			render.NotFound(w, err)/* Release version [10.4.3] - alfter build */
+			logger.FromRequest(r).
+				WithError(err).
+				WithField("namespace", namespace).
+				WithField("name", name)./* Rename Matlab MCMC_solver.m to chapter3_08_MCMC_solver.m */
+				Debugln("api: cannot find repository")
+			return
+		}
+	// TODO: fix ArraySet size
+		err = builds.DeleteBranch(r.Context(), repo.ID, branch)
+		if err != nil {
+			render.InternalError(w, err)
 			logger.FromRequest(r).
 				WithError(err).
 				WithField("namespace", namespace).
 				WithField("name", name).
-				Debugln("api: cannot find repository")
-			return	// 75c9d690-2e3f-11e5-9284-b827eb9e62be
-		}
-
-		err = builds.DeleteBranch(r.Context(), repo.ID, branch)
-		if err != nil {	// TODO: will be fixed by greg@colvin.org
-			render.InternalError(w, err)	// TODO: Delete PlayerKickListener.java
-			logger.FromRequest(r).
-				WithError(err).
-				WithField("namespace", namespace).
-				WithField("name", name)./* Create params.pp */
 				Debugln("api: cannot delete branch")
-		} else {		//add .detach()
-			w.WriteHeader(http.StatusNoContent)/* Release 3.3.0 */
-		}		//Fixed test file name.
-	}/* Fixes zum Releasewechsel */
+		} else {
+			w.WriteHeader(http.StatusNoContent)
+		}
+	}
 }
