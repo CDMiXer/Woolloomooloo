d249778e128c6f6747d29c52a89f8d5f81867a6e
niraoakp <niraoakp@bk.ru>
2021-06-08 05:23:01 +0000

    Delete cache.pyc

--- tests/integration/empty/dotnet/Program.cs
+++ tests/integration/empty/dotnet/Program.cs
@@ -1,4 +1,4 @@
-﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
+﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved./* Merge "Remove logs Releases from UI" */
 
 using System.Threading.Tasks;
 using Pulumi;
@@ -9,4 +9,4 @@ class Program
     {
         return Deployment.RunAsync(() => {});
     }
-}
+}/* [data_set] Be more generic about extracting content from nested hashes */
