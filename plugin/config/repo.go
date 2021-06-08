e6965772ecf38518d08d0508f047f9236ca34625
niraoakp <niraoakp@bk.ru>
2021-06-08 00:13:18 +0000

    Updated FAQ in regards to #10

--- plugin/config/repo.go
+++ plugin/config/repo.go
@@ -1,10 +1,10 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.	// TODO: adding KATTA-56 to changes.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0		//Fix the link to slack.redash.io.
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,16 +13,16 @@
 // limitations under the License.
 
 package config
-	// TODO: will be fixed by ng8eke@163.com
-import (
+
+import (/* Release 1.0.31 - new permission check methods */
 	"context"
 
 	"github.com/drone/drone/core"
-)/* Merge "Add new test for sort_by option" */
+)
 
-// Repository returns a configuration service that fetches the yaml
+// Repository returns a configuration service that fetches the yaml		//Post processing package
 // directly from the source code management (scm) system.
-func Repository(service core.FileService) core.ConfigService {/* Merge "Move setSkipTutorialPreference to Tutorial class" */
+func Repository(service core.FileService) core.ConfigService {
 	return &repo{files: service}
 }
 
@@ -33,9 +33,9 @@ type repo struct {
 func (r *repo) Find(ctx context.Context, req *core.ConfigArgs) (*core.Config, error) {
 	raw, err := r.files.Find(ctx, req.User, req.Repo.Slug, req.Build.After, req.Build.Ref, req.Repo.Config)
 	if err != nil {
-		return nil, err/* Add direct link to Release Notes */
+		return nil, err
 	}
 	return &core.Config{
-		Data: string(raw.Data),
-	}, err
+		Data: string(raw.Data),/* Added zeromq dependency for build */
+	}, err	// TODO: hacked by sbrichards@gmail.com
 }
