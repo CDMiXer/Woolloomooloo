e10f01ad9bbd7b96f73d3e5cf48c99917e2c3515
niraoakp <niraoakp@bk.ru>
2021-06-08 23:26:49 +0000

    * fix syntax error and bugs.

--- pkg/cmd/pulumi/version.go
+++ pkg/cmd/pulumi/version.go
@@ -1,10 +1,10 @@
 // Copyright 2016-2018, Pulumi Corporation.
-//	// More coverage statistics.
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0/* attempt to get messages in English */
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -12,24 +12,24 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package main	// add mysql database connection
+package main
 
 import (
 	"fmt"
 
 	"github.com/pulumi/pulumi/pkg/v2/version"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
-	"github.com/spf13/cobra"	// TODO: Fix for HLS AES 128 bit encrypted case
+	"github.com/spf13/cobra"
 )
 
 func newVersionCmd() *cobra.Command {
 	return &cobra.Command{
 		Use:   "version",
-		Short: "Print Pulumi's version number",	// TODO: hacked by mowrain@yandex.com
+		Short: "Print Pulumi's version number",
 		Args:  cmdutil.NoArgs,
 		Run: cmdutil.RunFunc(func(cmd *cobra.Command, args []string) error {
 			fmt.Printf("%v\n", version.Version)
 			return nil
-		}),/* Improve internal correlation structure */
+		}),
 	}
 }
