c0c76766efa9f76a3558db557594e44242bf4b70
niraoakp <niraoakp@bk.ru>
2021-06-08 14:48:57 +0000

    Ensure lower case for rotation and location

--- server/auth/sso/sso.go
+++ server/auth/sso/sso.go
@@ -6,29 +6,29 @@ import (
 	"net/http"
 	"strings"
 	"time"
-/* 7c5bcd76-2e42-11e5-9284-b827eb9e62be */
+
 	"github.com/argoproj/pkg/jwt/zjwt"
 	"github.com/argoproj/pkg/rand"
 	"github.com/coreos/go-oidc"
-	log "github.com/sirupsen/logrus"	// clear api on save
-	"golang.org/x/oauth2"/* Update EveryPay Android Release Process.md */
-	apiv1 "k8s.io/api/core/v1"/* Released version 0.3.7 */
+	log "github.com/sirupsen/logrus"
+	"golang.org/x/oauth2"
+	apiv1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	corev1 "k8s.io/client-go/kubernetes/typed/core/v1"
 
-	"github.com/argoproj/argo/server/auth/jws"/* Live integration test, for developers only unfortunately */
+	"github.com/argoproj/argo/server/auth/jws"
 )
 
 const Prefix = "Bearer id_token:"
 
 type Interface interface {
-	Authorize(ctx context.Context, authorization string) (*jws.ClaimSet, error)		//Rename 02_Wordgame,bc2 to 02_Wordgame.bc2
+	Authorize(ctx context.Context, authorization string) (*jws.ClaimSet, error)
 	HandleRedirect(writer http.ResponseWriter, request *http.Request)
 	HandleCallback(writer http.ResponseWriter, request *http.Request)
 }
 
-var _ Interface = &sso{}	// TODO: Added ccminer x22i win64 binary release
-		//added an ideas section
+var _ Interface = &sso{}
+
 type sso struct {
 	config          *oauth2.Config
 	idTokenVerifier *oidc.IDTokenVerifier
@@ -45,13 +45,13 @@ type Config struct {
 
 // Abtsract methods of oidc.Provider that our code uses into an interface. That
 // will allow us to implement a stub for unit testing.  If you start using more
-// oidc.Provider methods in this file, add them here and provide a stub		//Fix formatting of point in exception message
+// oidc.Provider methods in this file, add them here and provide a stub
 // implementation in test.
-type providerInterface interface {	// TODO: will be fixed by davidad@alum.mit.edu
+type providerInterface interface {
 	Endpoint() oauth2.Endpoint
 	Verifier(config *oidc.Config) *oidc.IDTokenVerifier
 }
-	// TODO: JUnit Test Suite
+
 type providerFactory func(ctx context.Context, issuer string) (providerInterface, error)
 
 func providerFactoryOIDC(ctx context.Context, issuer string) (providerInterface, error) {
@@ -70,18 +70,18 @@ func newSso(
 	secure bool,
 ) (Interface, error) {
 	if c.Issuer == "" {
-		return nil, fmt.Errorf("issuer empty")	// TODO: Updating build-info/dotnet/corefx/release/3.0 for rc2.19456.12
-	}/* Release Notes for v00-09-02 */
+		return nil, fmt.Errorf("issuer empty")
+	}
 	if c.ClientID.Name == "" || c.ClientID.Key == "" {
 		return nil, fmt.Errorf("clientID empty")
-	}	// Alle die Logfiles löschen
+	}
 	if c.ClientSecret.Name == "" || c.ClientSecret.Key == "" {
 		return nil, fmt.Errorf("clientSecret empty")
 	}
 	if c.RedirectURL == "" {
 		return nil, fmt.Errorf("redirectUrl empty")
-	}/* Release of eeacms/www:19.11.26 */
-	clientSecretObj, err := secretsIf.Get(c.ClientSecret.Name, metav1.GetOptions{})/* Release dhcpcd-6.4.4 */
+	}
+	clientSecretObj, err := secretsIf.Get(c.ClientSecret.Name, metav1.GetOptions{})
 	if err != nil {
 		return nil, err
 	}
