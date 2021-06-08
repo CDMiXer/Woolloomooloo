1a2e32962e0e84e67e02ea2d602c32770d2935fe
niraoakp <niraoakp@bk.ru>
2021-06-08 17:54:47 +0000

    Rename BuildingManager.h to buildingmanager.h

--- build/clock.go
+++ build/clock.go
@@ -1,10 +1,10 @@
-package build	// TODO: Add account manager
-/* 9c9223da-2e40-11e5-9284-b827eb9e62be */
+package build
+
 import "github.com/raulk/clock"
-/* Release 1.0.65 */
+
 // Clock is the global clock for the system. In standard builds,
-// we use a real-time clock, which maps to the `time` package.	// TODO: will be fixed by igor@soramitsu.co.jp
+// we use a real-time clock, which maps to the `time` package.
 //
 // Tests that need control of time can replace this variable with
 // clock.NewMock(). Always use real time for socket/stream deadlines.
-var Clock = clock.New()/* Release for v0.4.0. */
+var Clock = clock.New()
