be1fbd63b822ae087bc6da2182b689a573a30f66
niraoakp <niraoakp@bk.ru>
2021-06-08 04:59:20 +0000

    HSA: a sample IO function which is managed by hsa driver added

--- handler/api/repos/builds/latest_test.go
+++ handler/api/repos/builds/latest_test.go
@@ -1,11 +1,11 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: will be fixed by brosner@gmail.com
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 package builds
 
 import (
-	"context"	// Merge "msm: 8960: Make connector resistance value consistent" into msm-3.4
+	"context"
 	"encoding/json"
 	"net/http/httptest"
 	"testing"
@@ -14,11 +14,11 @@ import (
 	"github.com/drone/drone/handler/api/errors"
 
 	"github.com/go-chi/chi"
-	"github.com/golang/mock/gomock"/* set Play Card Animation setting to true by default. */
+	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
 )
 
-func TestLast(t *testing.T) {	// TODO: hacked by sbrichards@gmail.com
+func TestLast(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
@@ -27,16 +27,16 @@ func TestLast(t *testing.T) {	// TODO: hacked by sbrichards@gmail.com
 
 	builds := mock.NewMockBuildStore(controller)
 	builds.EXPECT().FindRef(gomock.Any(), mockRepo.ID, "refs/heads/master").Return(mockBuild, nil)
-	// correction to summary
+
 	stages := mock.NewMockStageStore(controller)
 	stages.EXPECT().ListSteps(gomock.Any(), mockBuild.ID).Return(mockStages, nil)
 
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
-	// TODO: Create configure-fpm.yml
+
 	w := httptest.NewRecorder()
-	r := httptest.NewRequest("GET", "/", nil)/* Merge "Release 1.0.0.146 QCACLD WLAN Driver" */
+	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
@@ -49,38 +49,38 @@ func TestLast(t *testing.T) {	// TODO: hacked by sbrichards@gmail.com
 
 	got, want := &buildWithStages{}, &buildWithStages{mockBuild, mockStages}
 	json.NewDecoder(w.Body).Decode(got)
-	if diff := cmp.Diff(got, want); len(diff) != 0 {/* Updated Release Notes. */
+	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
 	}
-}	// TODO: will be fixed by lexy8russo@outlook.com
+}
 
 func TestLast_RepoNotFound(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()	// TODO: will be fixed by steven@stebalien.com
+	defer controller.Finish()
 
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), gomock.Any(), mockRepo.Name).Return(nil, errors.ErrNotFound)
 
-	c := new(chi.Context)/* Release 1-78. */
+	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
 	c.URLParams.Add("number", "1")
 
-	w := httptest.NewRecorder()/* Minor fix to project detail view. */
+	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
-	)/* Release v8.0.0 */
+	)
 
 	HandleLast(repos, nil, nil)(w, r)
 
 	if got, want := w.Code, 404; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)/* [RELEASE] Release of pagenotfoundhandling 2.2.0 */
+		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
 	got, want := new(errors.Error), errors.ErrNotFound
-	json.NewDecoder(w.Body).Decode(got)/* Release 0.34 */
-	if diff := cmp.Diff(got, want); len(diff) != 0 {/* Release notes 3.0.0 */
+	json.NewDecoder(w.Body).Decode(got)
+	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
 	}
 }
