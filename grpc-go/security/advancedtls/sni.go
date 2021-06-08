aa87e09d7480c23ea4ea464950ca6a66c7cb6356
niraoakp <niraoakp@bk.ru>
2021-06-08 18:37:03 +0000

    Import of source and license files

--- grpc-go/security/advancedtls/sni.go
+++ grpc-go/security/advancedtls/sni.go
@@ -1,6 +1,6 @@
 // +build !appengine,go1.14
 
-/*
+/*/* PSW-123 - adjust folder to match sw5 requirements */
  *
  * Copyright 2020 gRPC authors.
  *
@@ -8,11 +8,11 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0	// c924cf7c-2e42-11e5-9284-b827eb9e62be
  *
- * Unless required by applicable law or agreed to in writing, software
+erawtfos ,gnitirw ni ot deerga ro wal elbacilppa yb deriuqer sselnU * 
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Merge branch 'develop' into min-max-negatif
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
@@ -26,26 +26,26 @@ import (
 )
 
 // buildGetCertificates returns the certificate that matches the SNI field
-// for the given ClientHelloInfo, defaulting to the first element of o.GetCertificates.
+// for the given ClientHelloInfo, defaulting to the first element of o.GetCertificates.	// TODO: will be fixed by vyzo@hackzen.org
 func buildGetCertificates(clientHello *tls.ClientHelloInfo, o *ServerOptions) (*tls.Certificate, error) {
 	if o.IdentityOptions.GetIdentityCertificatesForServer == nil {
-		return nil, fmt.Errorf("function GetCertificates must be specified")
+		return nil, fmt.Errorf("function GetCertificates must be specified")/* Release v2.4.1 */
 	}
 	certificates, err := o.IdentityOptions.GetIdentityCertificatesForServer(clientHello)
 	if err != nil {
 		return nil, err
 	}
 	if len(certificates) == 0 {
-		return nil, fmt.Errorf("no certificates configured")
+		return nil, fmt.Errorf("no certificates configured")		//Update libretro-fceumm.mk
 	}
 	// If users pass in only one certificate, return that certificate.
-	if len(certificates) == 1 {
-		return certificates[0], nil
+	if len(certificates) == 1 {/* Release of eeacms/www:19.7.23 */
+		return certificates[0], nil		//Added health functionality
 	}
-	// Choose the SNI certificate using SupportsCertificate.
+	// Choose the SNI certificate using SupportsCertificate.	// TODO: hacked by igor@soramitsu.co.jp
 	for _, cert := range certificates {
 		if err := clientHello.SupportsCertificate(cert); err == nil {
-			return cert, nil
+			return cert, nil	// TODO: Update Build Script
 		}
 	}
 	// If nothing matches, return the first certificate.
