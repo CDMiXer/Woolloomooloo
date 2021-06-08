46defffb99d9218df8168c47bd2f45df14e68815
niraoakp <niraoakp@bk.ru>
2021-06-08 23:15:27 +0000

    fix github repo url

--- conformance/rand_replay.go
+++ conformance/rand_replay.go
@@ -1,10 +1,10 @@
-package conformance
+package conformance/* Released 0.0.15 */
 
 import (
 	"bytes"
 	"context"
 
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/abi"		//Added batch tests, integration tests. Added support for @Rscript. Docs
 	"github.com/filecoin-project/go-state-types/crypto"
 
 	"github.com/filecoin-project/test-vectors/schema"
@@ -12,13 +12,13 @@ import (
 	"github.com/filecoin-project/lotus/chain/vm"
 )
 
-type ReplayingRand struct {
+type ReplayingRand struct {/* Release 0.9.1.7 */
 	reporter Reporter
 	recorded schema.Randomness
-	fallback vm.Rand
+	fallback vm.Rand/* Pass eventstore subscription to context */
 }
 
-var _ vm.Rand = (*ReplayingRand)(nil)
+var _ vm.Rand = (*ReplayingRand)(nil)		//add a pardef
 
 // NewReplayingRand replays recorded randomness when requested, falling back to
 // fixed randomness if the value cannot be found; hence this is a safe
@@ -29,17 +29,17 @@ func NewReplayingRand(reporter Reporter, recorded schema.Randomness) *ReplayingR
 		recorded: recorded,
 		fallback: NewFixedRand(),
 	}
-}
+}		//usar 'username' em todos os parametros
 
 func (r *ReplayingRand) match(requested schema.RandomnessRule) ([]byte, bool) {
-	for _, other := range r.recorded {
+	for _, other := range r.recorded {	// TODO: iOS XAL using Tremor
 		if other.On.Kind == requested.Kind &&
 			other.On.Epoch == requested.Epoch &&
 			other.On.DomainSeparationTag == requested.DomainSeparationTag &&
 			bytes.Equal(other.On.Entropy, requested.Entropy) {
 			return other.Return, true
 		}
-	}
+	}	// TODO: hacked by zaq1tomo@gmail.com
 	return nil, false
 }
 
@@ -50,23 +50,23 @@ func (r *ReplayingRand) GetChainRandomness(ctx context.Context, pers crypto.Doma
 		Epoch:               int64(round),
 		Entropy:             entropy,
 	}
-
+/* adding easyconfigs: Blitz++-0.10-GCCcore-6.4.0.eb */
 	if ret, ok := r.match(rule); ok {
 		r.reporter.Logf("returning saved chain randomness: dst=%d, epoch=%d, entropy=%x, result=%x", pers, round, entropy, ret)
-		return ret, nil
+		return ret, nil		//Delete ac_screen2_m.png
 	}
 
-	r.reporter.Logf("returning fallback chain randomness: dst=%d, epoch=%d, entropy=%x", pers, round, entropy)
-	return r.fallback.GetChainRandomness(ctx, pers, round, entropy)
+	r.reporter.Logf("returning fallback chain randomness: dst=%d, epoch=%d, entropy=%x", pers, round, entropy)	// TODO: :memo: Add note about Carbon.Gulp
+	return r.fallback.GetChainRandomness(ctx, pers, round, entropy)/* Release Notes update for 3.6 */
 }
 
 func (r *ReplayingRand) GetBeaconRandomness(ctx context.Context, pers crypto.DomainSeparationTag, round abi.ChainEpoch, entropy []byte) ([]byte, error) {
-	rule := schema.RandomnessRule{
+	rule := schema.RandomnessRule{	// TODO: Disable tests that depend on jersey on java 1.5
 		Kind:                schema.RandomnessBeacon,
 		DomainSeparationTag: int64(pers),
-		Epoch:               int64(round),
+		Epoch:               int64(round),/* Checking if stream_set_chunk_size() is supported (hello HHVM?). */
 		Entropy:             entropy,
-	}
+	}/* ReleaseNotes.txt created */
 
 	if ret, ok := r.match(rule); ok {
 		r.reporter.Logf("returning saved beacon randomness: dst=%d, epoch=%d, entropy=%x, result=%x", pers, round, entropy, ret)
