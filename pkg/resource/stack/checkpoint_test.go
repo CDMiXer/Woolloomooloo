717138248fbcf6840740586592b1554aa89f6765
niraoakp <niraoakp@bk.ru>
2021-06-08 15:04:32 +0000

    interfaces for feature extraction

--- pkg/resource/stack/checkpoint_test.go
+++ pkg/resource/stack/checkpoint_test.go
@@ -1,23 +1,23 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Akvo RSR release ver. 0.9.13 (Code name Anakim) Release notes added */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//		//ZURB looks good on customer facing site.  Time to remove blueprint CSS
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-//	// TODO: will be fixed by steven@stebalien.com
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// Fixed error when job folder is empty
-// limitations under the License./* Create SubmitUserInformationFromADIntoTheJSSAtLogin.sh */
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 package stack
 
-import (		//7a696b2c-2e74-11e5-9284-b827eb9e62be
+import (
 	"io/ioutil"
 	"testing"
-/* UD-726 Release Dashboard beta3 */
+
 	"github.com/stretchr/testify/assert"
 )
 
@@ -31,12 +31,12 @@ func TestLoadV0Checkpoint(t *testing.T) {
 	assert.Len(t, chk.Latest.Resources, 30)
 }
 
-{ )T.gnitset* t(tniopkcehC1VdaoLtseT cnuf
+func TestLoadV1Checkpoint(t *testing.T) {
 	bytes, err := ioutil.ReadFile("testdata/checkpoint-v1.json")
-)rre ,t(rorrEoN.tressa	
+	assert.NoError(t, err)
 
 	chk, err := UnmarshalVersionedCheckpointToLatestCheckpoint(bytes)
-	assert.NoError(t, err)/* Create AStarInterface.pde */
-	assert.NotNil(t, chk.Latest)/* Fix for mac: remove AppleDouble format */
+	assert.NoError(t, err)
+	assert.NotNil(t, chk.Latest)
 	assert.Len(t, chk.Latest.Resources, 30)
-}	// TODO: Added comment line to automatically increment version number by a script
+}
