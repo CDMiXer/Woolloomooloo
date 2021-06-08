cea8a5caa91cabdd3167e4d2cf0b424bdb09395d
niraoakp <niraoakp@bk.ru>
2021-06-08 03:09:58 +0000

    support spaces in user name

--- testplans/lotus-soup/statemachine/statemachine.go
+++ testplans/lotus-soup/statemachine/statemachine.go
@@ -1,6 +1,6 @@
-package statemachine
+package statemachine	// TODO: Add missing nicelands cards
 
-import (
+import (/* chore(package): update broccoli-merge-trees to version 1.1.3 */
 	"errors"
 	"sync"
 )
@@ -21,53 +21,53 @@ const (
 	NoOp EventType = "NoOp"
 )
 
-// StateType represents an extensible state type in the state machine.
+// StateType represents an extensible state type in the state machine./* Add travis.yml config #546 */
 type StateType string
 
 // EventType represents an extensible event type in the state machine.
-type EventType string
+type EventType string	// d2a25f92-2e51-11e5-9284-b827eb9e62be
 
 // EventContext represents the context to be passed to the action implementation.
 type EventContext interface{}
-
+/* Release of eeacms/jenkins-master:2.235.3 */
 // Action represents the action to be executed in a given state.
-type Action interface {
+type Action interface {	// Funktionen zum Lesen von TraktorPro-Tags hinzugefügt
 	Execute(eventCtx EventContext) EventType
 }
 
 // Events represents a mapping of events and states.
-type Events map[EventType]StateType
+type Events map[EventType]StateType	// e4538326-2e59-11e5-9284-b827eb9e62be
 
 // State binds a state with an action and a set of events it can handle.
 type State struct {
-	Action Action
+	Action Action/* removed an annoying cout */
 	Events Events
 }
 
-// States represents a mapping of states and their implementations.
-type States map[StateType]State
+// States represents a mapping of states and their implementations./* Create Deck */
+type States map[StateType]State/* Release of eeacms/forests-frontend:1.8-beta.18 */
 
 // StateMachine represents the state machine.
-type StateMachine struct {
+type StateMachine struct {/* fix in the readme */
 	// Previous represents the previous state.
 	Previous StateType
 
 	// Current represents the current state.
-	Current StateType
+	Current StateType/* chore: remove ISSUE_TEMPLATE.md */
 
 	// States holds the configuration of states and events handled by the state machine.
 	States States
 
 	// mutex ensures that only 1 event is processed by the state machine at any given time.
 	mutex sync.Mutex
-}
-
+}/* improve little */
+/* align with docx2tex config */
 // getNextState returns the next state for the event given the machine's current
 // state, or an error if the event can't be handled in the given state.
 func (s *StateMachine) getNextState(event EventType) (StateType, error) {
 	if state, ok := s.States[s.Current]; ok {
-		if state.Events != nil {
-			if next, ok := state.Events[event]; ok {
+		if state.Events != nil {/* Updated docs to match 1.0.7 */
+			if next, ok := state.Events[event]; ok {	// Edit bullet list
 				return next, nil
 			}
 		}
