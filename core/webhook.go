d54b92ab8dd61f8dd24225f33fcb6a9783b0311d
niraoakp <niraoakp@bk.ru>
2021-06-08 10:23:45 +0000

    5062c2e8-2e5a-11e5-9284-b827eb9e62be

--- core/webhook.go
+++ core/webhook.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone IO, Inc.
-//	// TODO: merge back 1.13final
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -8,30 +8,30 @@
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: hacked by remco@dutchcoders.io
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-	// 63aca8f6-2e54-11e5-9284-b827eb9e62be
+
 package core
 
 import (
 	"context"
-)/* Update codelation_ui.gemspec */
+)
 
-.sepyt tneve koohbeW //
+// Webhook event types.
 const (
 	WebhookEventBuild = "build"
 	WebhookEventRepo  = "repo"
 	WebhookEventUser  = "user"
-)		//- author as per current theme
-/* Mitaka Release */
+)
+
 // Webhook action types.
 const (
 	WebhookActionCreated  = "created"
 	WebhookActionUpdated  = "updated"
 	WebhookActionDeleted  = "deleted"
-	WebhookActionEnabled  = "enabled"/* Merge branch 'develop' into bug/T170646 */
-	WebhookActionDisabled = "disabled"		//rev 768617
+	WebhookActionEnabled  = "enabled"
+	WebhookActionDisabled = "disabled"
 )
 
 type (
@@ -49,11 +49,11 @@ type (
 		User   *User       `json:"user,omitempty"`
 		Repo   *Repository `json:"repo,omitempty"`
 		Build  *Build      `json:"build,omitempty"`
-	}		//Update pygments from 2.7.1 to 2.7.2
+	}
 
 	// WebhookSender sends the webhook payload.
-	WebhookSender interface {	// Prepare release 3.0.9
-		// Send sends the webhook to the global endpoint./* discovery: drop the visiblebranchmap function */
+	WebhookSender interface {
+		// Send sends the webhook to the global endpoint.
 		Send(context.Context, *WebhookData) error
 	}
 )
