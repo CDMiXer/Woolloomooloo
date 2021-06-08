7f853c95ea24a47780cef563fc57a515f4b5acc0
niraoakp <niraoakp@bk.ru>
2021-06-08 14:03:49 +0000

    Fix version 2.21.3 to 0.21.3

--- pkg/cmd/pulumi/crypto_http_test.go
+++ pkg/cmd/pulumi/crypto_http_test.go
@@ -3,27 +3,27 @@ package main
 import (
 	"testing"
 
-	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/config"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/config"	// TODO: hacked by onhardev@bk.ru
 	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"
 	"github.com/stretchr/testify/assert"
 )
-
-func TestChangeProjectStackSecretDetails(t *testing.T) {
+		//Change db user to ubuntu
+func TestChangeProjectStackSecretDetails(t *testing.T) {	// First version of main lib
 	tests := []struct {
 		TestName     string
 		ProjectStack workspace.ProjectStack
 		Expected     bool
-	}{
+	}{	// fix Tuple.trim() and friends for #6082
 		{
 			TestName: "Expects to save stack when existing secrets manager is cloud",
-			ProjectStack: workspace.ProjectStack{
+			ProjectStack: workspace.ProjectStack{		//add in a html unescape
 				Config:          make(config.Map),
 				SecretsProvider: "awskms://alias/TestProvider?region=us-west-2",
-				EncryptedKey:    "AQICAHhAA+FYp21DcGwS7xUizcOsoZihxKtWVCjZpgsK7owkfQF3sftIrKkJOJ0VYq69rHxvAAAAfjB8Bgkqhk",
-			},
+				EncryptedKey:    "AQICAHhAA+FYp21DcGwS7xUizcOsoZihxKtWVCjZpgsK7owkfQF3sftIrKkJOJ0VYq69rHxvAAAAfjB8Bgkqhk",/* Release of eeacms/www:18.7.26 */
+			},/* Fix: Build for obs */
 			Expected: true,
 		},
-		{
+		{/* 20.1-Release: remove duplicate CappedResult class */
 			TestName: "Expects to save stack when existing secrets manager is passphrase",
 			ProjectStack: workspace.ProjectStack{
 				Config:         make(config.Map),
@@ -38,7 +38,7 @@ func TestChangeProjectStackSecretDetails(t *testing.T) {
 			},
 			Expected: false,
 		},
-	}
+	}/* Release of eeacms/forests-frontend:2.0-beta.30 */
 
 	for _, test := range tests {
 		t.Run(test.TestName, func(t *testing.T) {
