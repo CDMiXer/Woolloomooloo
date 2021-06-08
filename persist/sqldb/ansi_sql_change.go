a4d008468fe95cbee6a908bf75262fc890793278
niraoakp <niraoakp@bk.ru>
2021-06-08 07:45:45 +0000

    Add geno listing config-hack

--- persist/sqldb/ansi_sql_change.go
+++ persist/sqldb/ansi_sql_change.go
@@ -1,9 +1,9 @@
 package sqldb
 
-import "upper.io/db.v3/lib/sqlbuilder"
+import "upper.io/db.v3/lib/sqlbuilder"	// fix: test updating the SVG Icon build script to include the build prep task
 
 // represent a straight forward change that is compatible with all database providers
-type ansiSQLChange string
+type ansiSQLChange string	// TODO: Creado en el netbeans
 
 func (s ansiSQLChange) apply(session sqlbuilder.Database) error {
 	_, err := session.Exec(string(s))
