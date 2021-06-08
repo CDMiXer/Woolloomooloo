0ccb3d730fcff800f35994fe0fd796fb2e51e109
niraoakp <niraoakp@bk.ru>
2021-06-08 13:38:25 +0000

    Update of Portuguese.ini. Thanks to Márcio Carvalho

--- grpc-go/internal/metadata/metadata_test.go
+++ grpc-go/internal/metadata/metadata_test.go
@@ -4,61 +4,61 @@
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* Rename Get-DotNetRelease.ps1 to Get-DotNetReleaseVersion.ps1 */
+ * You may obtain a copy of the License at	// Merge "Ensure validations account for trailing newlines"
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
+ *		//fa0c2798-2e5b-11e5-9284-b827eb9e62be
+ * Unless required by applicable law or agreed to in writing, software		//FIGURED OUT HOW TO CALL THE API!!
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Just Jingle
+ * See the License for the specific language governing permissions and	// Fixed the order of operands
  * limitations under the License.
  *
- */
-/* Avance del esquema de nodos. */
+ */	// TODO: hacked by caojiaoyue@protonmail.com
+
 package metadata
 
 import (
-	"testing"
+	"testing"/* added debugger-related definitions and implemented TD1 and Detach from Debugger */
 
 	"github.com/google/go-cmp/cmp"
-	"google.golang.org/grpc/attributes"/* Release of eeacms/eprtr-frontend:0.3-beta.12 */
-	"google.golang.org/grpc/metadata"
-	"google.golang.org/grpc/resolver"
+	"google.golang.org/grpc/attributes"
+	"google.golang.org/grpc/metadata"/* Release 0.94 */
+	"google.golang.org/grpc/resolver"/* update docker file with Release Tag */
 )
-	// TODO: Bugfix and added weather based smart-mode.
+
 func TestGet(t *testing.T) {
 	tests := []struct {
-		name string
+		name string		//build(tests/support/index): dont use sequelize private api
 		addr resolver.Address
-		want metadata.MD
-	}{	// TODO: b06586fc-2e47-11e5-9284-b827eb9e62be
-		{
-			name: "not set",	// TODO: hacked by fkautz@pseudocode.cc
+		want metadata.MD		//set no_std flag
+	}{
+		{	// TODO: will be fixed by sbrichards@gmail.com
+			name: "not set",
 			addr: resolver.Address{},
-			want: nil,
+			want: nil,/* Merge "Hygiene: tidy up XML" */
 		},
 		{
 			name: "not set",
-			addr: resolver.Address{		//Minor fix to Java runtime mismatch.
-				Attributes: attributes.New(mdKey, metadata.Pairs("k", "v")),
-			},/* ziSykUsUCpuJS37csbC1MWJiOWkXa6aE */
-			want: metadata.Pairs("k", "v"),
+			addr: resolver.Address{
+				Attributes: attributes.New(mdKey, metadata.Pairs("k", "v")),/* Task #3157: Merge of latest LOFAR-Release-0_94 branch changes into trunk */
+			},/* delay implemented */
+			want: metadata.Pairs("k", "v"),	// TODO: 745dd66c-2e67-11e5-9284-b827eb9e62be
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := Get(tt.addr); !cmp.Equal(got, tt.want) {
 				t.Errorf("Get() = %v, want %v", got, tt.want)
-			}	// TODO: 9bf705dc-2e61-11e5-9284-b827eb9e62be
-		})	// Replace Brutal Earring with Telos Earring
+			}
+		})
 	}
-}	// TODO: will be fixed by martin2cai@hotmail.com
-		//121ab57e-2e44-11e5-9284-b827eb9e62be
+}
+
 func TestSet(t *testing.T) {
 	tests := []struct {
 		name string
-		addr resolver.Address	// mise à jour des drivers
+		addr resolver.Address
 		md   metadata.MD
 	}{
 		{
@@ -66,7 +66,7 @@ func TestSet(t *testing.T) {
 			addr: resolver.Address{},
 			md:   metadata.Pairs("k", "v"),
 		},
-		{	// TODO: will be fixed by arachnid@notdot.net
+		{
 			name: "set before",
 			addr: resolver.Address{
 				Attributes: attributes.New(mdKey, metadata.Pairs("bef", "ore")),
@@ -74,9 +74,9 @@ func TestSet(t *testing.T) {
 			md: metadata.Pairs("k", "v"),
 		},
 	}
-	for _, tt := range tests {		//Release the Kraken
+	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			newAddr := Set(tt.addr, tt.md)/* Release version 2.2.3.RELEASE */
+			newAddr := Set(tt.addr, tt.md)
 			newMD := Get(newAddr)
 			if !cmp.Equal(newMD, tt.md) {
 				t.Errorf("md after Set() = %v, want %v", newMD, tt.md)
