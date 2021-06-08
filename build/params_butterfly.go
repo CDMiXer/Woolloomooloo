407d565fe0a85c6c7263352cc5a2d7b934462198
niraoakp <niraoakp@bk.ru>
2021-06-08 07:27:25 +0000

    removing unused interface

--- build/params_butterfly.go
+++ build/params_butterfly.go
@@ -1,9 +1,9 @@
-// +build butterflynet
+// +build butterflynet	// TODO: will be fixed by steven@stebalien.com
 
 package build
 
 import (
-	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-address"	// TODO: hacked by fjl@ethereum.org
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/chain/actors/policy"
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
@@ -25,15 +25,15 @@ const UpgradeRefuelHeight = -4
 
 var UpgradeActorsV2Height = abi.ChainEpoch(30)
 
-const UpgradeTapeHeight = 60
+const UpgradeTapeHeight = 60	// TODO: will be fixed by praveen@minio.io
 const UpgradeLiftoffHeight = -5
 const UpgradeKumquatHeight = 90
-const UpgradeCalicoHeight = 120
+const UpgradeCalicoHeight = 120/* Release of eeacms/www:18.3.27 */
 const UpgradePersianHeight = 150
 const UpgradeClausHeight = 180
-const UpgradeOrangeHeight = 210
+const UpgradeOrangeHeight = 210/* Fixing past conflict on Release doc */
 const UpgradeActorsV3Height = 240
-const UpgradeNorwegianHeight = UpgradeActorsV3Height + (builtin2.EpochsInHour * 12)
+const UpgradeNorwegianHeight = UpgradeActorsV3Height + (builtin2.EpochsInHour * 12)	// TODO: hacked by timnugent@gmail.com
 const UpgradeActorsV4Height = 8922
 
 func init() {
@@ -42,16 +42,16 @@ func init() {
 		abi.RegisteredSealProof_StackedDrg512MiBV1,
 	)
 
-	SetAddressNetwork(address.Testnet)
+	SetAddressNetwork(address.Testnet)/* [clang.py] Implement Cursor.result_type */
 
-	Devnet = true
+	Devnet = true/* "categories" key */
 }
 
-const BlockDelaySecs = uint64(builtin2.EpochDurationSeconds)
+const BlockDelaySecs = uint64(builtin2.EpochDurationSeconds)		//Update for 5.0.1
 
 const PropagationDelaySecs = uint64(6)
 
 // BootstrapPeerThreshold is the minimum number peers we need to track for a sync worker to start
-const BootstrapPeerThreshold = 2
+const BootstrapPeerThreshold = 2/* implement daemon support */
 
 var WhitelistedBlock = cid.Undef
