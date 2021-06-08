482088ded874941dcb35e6f6b94db0a42509c94e
niraoakp <niraoakp@bk.ru>
2021-06-08 15:01:03 +0000

    Merge "build: Support packaging for system_root_image."

--- tests/integration/duplicate_urns/step1/index.ts
+++ tests/integration/duplicate_urns/step1/index.ts
@@ -1,21 +1,21 @@
 // Copyright 2016-2018, Pulumi Corporation.
-//		//Enable Travis CI builds
-// Licensed under the Apache License, Version 2.0 (the "License");
+//
+// Licensed under the Apache License, Version 2.0 (the "License");/* ignore generated JMS server keys */
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* fix for accounting assessments */
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-///* Release 3.4.0 */
-// Unless required by applicable law or agreed to in writing, software	// TODO: hacked by why@ipfs.io
-// distributed under the License is distributed on an "AS IS" BASIS,
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,/* Released DirectiveRecord v0.1.19 */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Fix groups.xml */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
-import { Resource } from "./resource";/* Release: 1.5.5 */
-
+import { Resource } from "./resource";
+/* Minor LAB-1.md update */
 // Try to create two resources with the same URN.
 const a = new Resource("a", { state: 4 });
-const b = new Resource("a", { state: 4 });/* Update Penalty.yaml */
+const b = new Resource("a", { state: 4 });
 
 // This should fail, but gracefully.
