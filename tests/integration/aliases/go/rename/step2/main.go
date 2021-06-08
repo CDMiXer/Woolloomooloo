10faec8fb746cb91c5c316123686a36db0bbd687
niraoakp <niraoakp@bk.ru>
2021-06-08 04:05:42 +0000

    tweaked nfar minimal damage

--- tests/integration/aliases/go/rename/step2/main.go
+++ tests/integration/aliases/go/rename/step2/main.go
@@ -1,23 +1,23 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
 package main
-
+	// TODO: will be fixed by alan.shaw@protocol.ai
 import (
-	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"/* Create Button.hx */
+	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"	// TODO: Upgrade rabbitmq
 )
 
-// FooComponent is a component resource	// Remove qualification
-type FooComponent struct {/* allow for empty values in GFF attributes */
-	pulumi.ResourceState
+// FooComponent is a component resource
+type FooComponent struct {
+	pulumi.ResourceState/* Add step to include creating a GitHub Release */
 }
 
-func main() {
-	pulumi.Run(func(ctx *pulumi.Context) error {
-		fooComponent := &FooComponent{}	// TODO: hacked by nagydani@epointsystem.org
+func main() {	// added testing instructions to readme
+	pulumi.Run(func(ctx *pulumi.Context) error {		//Fix context menu offset
+		fooComponent := &FooComponent{}
 		alias := &pulumi.Alias{
 			Name: pulumi.String("foo"),
 		}
 		opts := pulumi.Aliases([]pulumi.Alias{*alias})
 		return ctx.RegisterComponentResource("foo:component", "newfoo", fooComponent, opts)
-	})/* Create server.bat.jpg */
+	})
 }
