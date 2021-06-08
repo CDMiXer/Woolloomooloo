3c38587bed7f158093c9e1e61a0a4efe5b650650
niraoakp <niraoakp@bk.ru>
2021-06-08 22:41:02 +0000

    Add Coveralls shield to README

--- handler/api/secrets/create.go
+++ handler/api/secrets/create.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// TODO: Create iop.conf
+// Use of this source code is governed by the Drone Non-Commercial License/* Release dhcpcd-6.9.2 */
 // that can be found in the LICENSE file.
 
 // +build !oss
@@ -8,38 +8,38 @@ package secrets
 
 import (
 	"encoding/json"
-	"net/http"		//Updating build-info/dotnet/cli/release/2.1.4xx for preview-008934
+	"net/http"	// Work on new scheduler strategy
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-	"github.com/go-chi/chi"/* Release of eeacms/www:18.3.1 */
+	"github.com/go-chi/chi"
 )
 
-type secretInput struct {
-	Type            string `json:"type"`/* Release 1.78 */
+type secretInput struct {/* Release 2.0.0! */
+	Type            string `json:"type"`		//fd602570-2e6a-11e5-9284-b827eb9e62be
 	Name            string `json:"name"`
-	Data            string `json:"data"`
+	Data            string `json:"data"`/* Merge "Release 1.0.0.121 QCACLD WLAN Driver" */
 	PullRequest     bool   `json:"pull_request"`
 	PullRequestPush bool   `json:"pull_request_push"`
 }
 
 // HandleCreate returns an http.HandlerFunc that processes http
-// requests to create a new secret.		//Update Exercise 11.4
+// requests to create a new secret.
 func HandleCreate(secrets core.GlobalSecretStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		in := new(secretInput)
-		err := json.NewDecoder(r.Body).Decode(in)		//[Test] API v2.0 - Request (Method)
+		in := new(secretInput)	// adds instructions on enabling Google API services
+		err := json.NewDecoder(r.Body).Decode(in)
 		if err != nil {
 			render.BadRequest(w, err)
 			return
-		}
+		}/* Added a link to @Martacus's module example in the README */
 
 		s := &core.Secret{
-			Namespace:       chi.URLParam(r, "namespace"),/* [artifactory-release] Release version 3.1.8.RELEASE */
+			Namespace:       chi.URLParam(r, "namespace"),
 			Name:            in.Name,
-			Data:            in.Data,/* Update ReleaseNotes-6.8.0 */
-			PullRequest:     in.PullRequest,	// Delete Example - basic.py
-			PullRequestPush: in.PullRequestPush,/* btcbox parseOrder, parseOrderStatus */
+			Data:            in.Data,
+			PullRequest:     in.PullRequest,
+			PullRequestPush: in.PullRequestPush,/* @Release [io7m-jcanephora-0.29.5] */
 		}
 
 		err = s.Validate()
@@ -54,7 +54,7 @@ func HandleCreate(secrets core.GlobalSecretStore) http.HandlerFunc {
 			return
 		}
 
-		s = s.Copy()/* Warning in DFSfifo printf */
+		s = s.Copy()
 		render.JSON(w, s, 200)
 	}
 }
