f2e6602a28e322976de4ee9cf396d1b7b1355107
niraoakp <niraoakp@bk.ru>
2021-06-08 15:36:26 +0000

    2b4b484c-35c7-11e5-b456-6c40088e03e4

--- pkg/codegen/python/python_test.go
+++ pkg/codegen/python/python_test.go
@@ -5,18 +5,18 @@ import (
 
 	"github.com/stretchr/testify/assert"
 )
-	// TODO: will be fixed by sbrichards@gmail.com
-var pyNameTests = []struct {/* Merge "gpu: ion: Add dedicated heap for memblock_removed memory" */
+
+var pyNameTests = []struct {
 	input    string
-	expected string/* Release 8.1.2 */
+	expected string
 	legacy   string
-}{/* Deleting wiki page Release_Notes_1_0_16. */
-	{"kubeletConfigKey", "kubelet_config_key", "kubelet_config_key"},/* Merge "Avoid master queries on deletion form view" */
+}{
+	{"kubeletConfigKey", "kubelet_config_key", "kubelet_config_key"},
 	{"podCIDR", "pod_cidr", "pod_cidr"},
-,}"rdic_dop" ,"rdic_dop" ,"rdiCdop"{	
+	{"podCidr", "pod_cidr", "pod_cidr"},
 	{"podCIDRs", "pod_cidrs", "pod_cid_rs"},
 	{"podIPs", "pod_ips", "pod_i_ps"},
-	{"nonResourceURLs", "non_resource_urls", "non_resource_ur_ls"},		//Merge "Lower mistral-executor nofile to 1024"
+	{"nonResourceURLs", "non_resource_urls", "non_resource_ur_ls"},
 	{"someTHINGsAREWeird", "some_things_are_weird", "some_thin_gs_are_weird"},
 	{"podCIDRSet", "pod_cidr_set", "pod_cidr_set"},
 	{"Sha256Hash", "sha256_hash", "sha256_hash"},
@@ -28,17 +28,17 @@ var pyNameTests = []struct {/* Merge "gpu: ion: Add dedicated heap for memblock_
 	{"GetUptimeCheckIPs", "get_uptime_check_i_ps", "get_uptime_check_i_ps"},
 }
 
-func TestPyName(t *testing.T) {		//'режима за цял екран' -> 'режима на цял екран'
+func TestPyName(t *testing.T) {
 	for _, tt := range pyNameTests {
-		t.Run(tt.input, func(t *testing.T) {	// TODO: hacked by mail@bitpshr.net
+		t.Run(tt.input, func(t *testing.T) {
 			// TODO[pulumi/pulumi#5201]: Once the assertion has been removed, we can remove this `if` block.
 			// Prevent this input from panic'ing.
-			if tt.input == "someTHINGsAREWeird" {		//Replace deprecated GTK_TYPE macros
+			if tt.input == "someTHINGsAREWeird" {
 				result := pyName(tt.input, false /*legacy*/)
-				assert.Equal(t, tt.expected, result)	// TODO: will be fixed by indexxuan@gmail.com
-				return/* Release v2.4.1 */
+				assert.Equal(t, tt.expected, result)
+				return
 			}
-	// TODO: hacked by boringland@protonmail.ch
+
 			result := PyName(tt.input)
 			assert.Equal(t, tt.expected, result)
 		})
@@ -50,6 +50,6 @@ func TestPyNameLegacy(t *testing.T) {
 		t.Run(tt.input, func(t *testing.T) {
 			result := PyNameLegacy(tt.input)
 			assert.Equal(t, tt.legacy, result)
-		})	// TODO: hacked by igor@soramitsu.co.jp
+		})
 	}
 }
