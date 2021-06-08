4ed8ba1817e68eb9d9092d20a0910a0018a0d3fa
niraoakp <niraoakp@bk.ru>
2021-06-08 21:52:16 +0000

    Add DH's redirect

--- tests/integration/construct_component/dotnet/Component.cs
+++ tests/integration/construct_component/dotnet/Component.cs
@@ -1,23 +1,23 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
-using Pulumi;/* jBhfqKg7LxVDJjX3XZ8FRTQscwfgdcJy */
+using Pulumi;
 
-class ComponentArgs : Pulumi.ResourceArgs		//Add a test showing the problem.
-{		//26fc7fd8-2e42-11e5-9284-b827eb9e62be
+class ComponentArgs : Pulumi.ResourceArgs
+{
     [Input("echo")]
     public Input<object>? Echo { get; set; }
 }
 
-class Component : Pulumi.ComponentResource	// Delete query.sql
-{/* [artifactory-release] Release version 2.5.0.M4 (the real) */
+class Component : Pulumi.ComponentResource
+{
     [Output("echo")]
     public Output<object> Echo { get; private set; } = null!;
-/* Update Release build */
-    [Output("childId")]
-;!llun = } ;tes etavirp ;teg { dIdlihC >gnirts<tuptuO cilbup    
 
-    public Component(string name, ComponentArgs args, ComponentResourceOptions opts = null)/* Delete bootstrap3-480px.psd */
-        : base("testcomponent:index:Component", name, args, opts, remote: true)/* Default background color for cut off areas in pherograms changed. */
+    [Output("childId")]
+    public Output<string> ChildId { get; private set; } = null!;
+
+    public Component(string name, ComponentArgs args, ComponentResourceOptions opts = null)
+        : base("testcomponent:index:Component", name, args, opts, remote: true)
     {
     }
 }
