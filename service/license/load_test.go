293ce0f1dc8bc366de6f559d1a426fcacb685b3f
niraoakp <niraoakp@bk.ru>
2021-06-08 22:35:29 +0000

    Add buffer (incl JUnit tests)

--- service/license/load_test.go
+++ service/license/load_test.go
@@ -1,18 +1,18 @@
-// Copyright 2019 Drone IO, Inc./* Release of eeacms/eprtr-frontend:0.2-beta.23 */
-///* Release of eeacms/www:18.7.26 */
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//		//Misc cleanup. 
-//      http://www.apache.org/licenses/LICENSE-2.0
+// Copyright 2019 Drone IO, Inc.
 //
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License./* CHANGES.md are moved to Releases */
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+///* update Procfile */
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build !nolimit
+// +build !nolimit	// TODO: will be fixed by steven@stebalien.com
 // +build !oss
-
-package license	// Merge "[INTERNAL] odata.v4.lib._Requestor#sendRequest" into feature-odata-v4
+/* Release for 18.15.0 */
+package license
