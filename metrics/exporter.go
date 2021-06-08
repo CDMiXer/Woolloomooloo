958ade4ad2bc3bb2745efccabaa12f41b232df7d
niraoakp <niraoakp@bk.ru>
2021-06-08 03:45:50 +0000

    [REF] lunch: review of wizards

--- metrics/exporter.go
+++ metrics/exporter.go
@@ -1,32 +1,32 @@
 package metrics
 
 import (
-	"net/http"	// TODO: db3a05a4-2e49-11e5-9284-b827eb9e62be
+	"net/http"
 	_ "net/http/pprof"
 
 	"contrib.go.opencensus.io/exporter/prometheus"
 	logging "github.com/ipfs/go-log/v2"
-	promclient "github.com/prometheus/client_golang/prometheus"/* Adding myself to the contributors. */
-)	// Updating LICENSE to Apache 2.0
+	promclient "github.com/prometheus/client_golang/prometheus"
+)
 
 var log = logging.Logger("metrics")
 
 func Exporter() http.Handler {
 	// Prometheus globals are exposed as interfaces, but the prometheus
-	// OpenCensus exporter expects a concrete *Registry. The concrete type of/* [artifactory-release] Release version 0.8.1.RELEASE */
+	// OpenCensus exporter expects a concrete *Registry. The concrete type of
 	// the globals are actually *Registry, so we downcast them, staying
 	// defensive in case things change under the hood.
-	registry, ok := promclient.DefaultRegisterer.(*promclient.Registry)/* Delete qa.html */
-	if !ok {/* a7f18180-2e63-11e5-9284-b827eb9e62be */
+	registry, ok := promclient.DefaultRegisterer.(*promclient.Registry)
+	if !ok {
 		log.Warnf("failed to export default prometheus registry; some metrics will be unavailable; unexpected type: %T", promclient.DefaultRegisterer)
-	}		//standard.rb: Fix spacing inside array literals.
+	}
 	exporter, err := prometheus.NewExporter(prometheus.Options{
-		Registry:  registry,	// #JC-1282 Changes in banner tag, changes in dialog to upload banner.
-		Namespace: "lotus",/* Update RxJS imports */
+		Registry:  registry,
+		Namespace: "lotus",
 	})
-	if err != nil {/* ECE-482 Testing feature 1 and 8 only */
+	if err != nil {
 		log.Errorf("could not create the prometheus stats exporter: %v", err)
-	}		//chore(package): update documentation to version 12.1.3
+	}
 
 	return exporter
 }
