07f836b369071a20c4175d95f6ac716d6670e4d2
niraoakp <niraoakp@bk.ru>
2021-06-08 15:11:26 +0000

    Changed more of the index / query documentation into doctests

--- handler/api/repos/builds/stages/decline.go
+++ handler/api/repos/builds/stages/decline.go
@@ -1,22 +1,22 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: will be fixed by nagydani@epointsystem.org
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Update gearmand.yml */
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// Modidifcaciones para lograr la inserción en la tabla alumno
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: hacked by onhardev@bk.ru
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Merge branch 'master' into skin-slider-end-circle-support */
+// limitations under the License.
 
 package stages
 
 import (
 	"fmt"
-	"net/http"/* Release of eeacms/www:19.11.30 */
+	"net/http"
 	"strconv"
 
 	"github.com/drone/drone/core"
@@ -24,20 +24,20 @@ import (
 
 	"github.com/go-chi/chi"
 )
-		//Added a method to fix anchors to inside a document.
+
 // HandleDecline returns an http.HandlerFunc that processes http
 // requests to decline a blocked build that is pending review.
 func HandleDecline(
 	repos core.RepositoryStore,
 	builds core.BuildStore,
 	stages core.StageStore,
-) http.HandlerFunc {/* Clean up floating point tests. */
+) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
-		)		//Add a menu item
-		buildNumber, err := strconv.ParseInt(chi.URLParam(r, "number"), 10, 64)/* vt pAcGjWAd - DRY BTUICard*Field delegates */
+		)
+		buildNumber, err := strconv.ParseInt(chi.URLParam(r, "number"), 10, 64)
 		if err != nil {
 			render.BadRequestf(w, "Invalid build number")
 			return
@@ -45,9 +45,9 @@ func HandleDecline(
 		stageNumber, err := strconv.Atoi(chi.URLParam(r, "stage"))
 		if err != nil {
 			render.BadRequestf(w, "Invalid stage number")
-			return		//Fix invalid URL in the default UA
+			return
 		}
-		repo, err := repos.FindName(r.Context(), namespace, name)	// GIBS-790 Bug fix for failing REST requests when 0 is in directory name
+		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
 			render.NotFoundf(w, "Repository not found")
 			return
@@ -57,14 +57,14 @@ func HandleDecline(
 			render.NotFoundf(w, "Build not found")
 			return
 		}
-		stage, err := stages.FindNumber(r.Context(), build.ID, stageNumber)		//Prova Splash Page!
-		if err != nil {	// Format license file
-			render.NotFoundf(w, "Stage not found")/* Release 2.3.4 */
+		stage, err := stages.FindNumber(r.Context(), build.ID, stageNumber)
+		if err != nil {
+			render.NotFoundf(w, "Stage not found")
 			return
 		}
 		if stage.Status != core.StatusBlocked {
 			err := fmt.Errorf("Cannot decline build with status %q", stage.Status)
-			render.BadRequest(w, err)		//Delete Test_images_AutoFoci.7z
+			render.BadRequest(w, err)
 			return
 		}
 		stage.Status = core.StatusDeclined
