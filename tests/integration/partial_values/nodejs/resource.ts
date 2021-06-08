362779ec837d41a9cc9c39aaf102d2c79423aa8d
niraoakp <niraoakp@bk.ru>
2021-06-08 00:30:46 +0000

    aggiornata la versione a 0.95

--- tests/integration/partial_values/nodejs/resource.ts
+++ tests/integration/partial_values/nodejs/resource.ts
@@ -1,29 +1,29 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-	// Merge "Remove all_tenants from server_list of Floating IPs tab"
+
 import * as pulumi from "@pulumi/pulumi";
+/* Release PEAR2_Templates_Savant-0.3.3 */
+let currentID = 0;/* 10.0.4 Tarball, Packages Release */
+/* Fix compile.js for nodejs 10 */
+export class Provider implements pulumi.dynamic.ResourceProvider {
+    public static readonly instance = new Provider();	// TODO: will be fixed by alan.shaw@protocol.ai
 
-let currentID = 0;
+    public readonly create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;
 
-export class Provider implements pulumi.dynamic.ResourceProvider {	// Added TOC, Documentation & Caveats
-    public static readonly instance = new Provider();		//overwrite asset
-
-    public readonly create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;/* Merge "Release note 1.0beta" */
-/* Release v2.6. */
     constructor() {
         this.create = async (inputs: any) => {
-            return {		//19ccfa88-2e45-11e5-9284-b827eb9e62be
+            return {
                 id: (currentID++).toString(),
                 outs: inputs,
             };
         };
     }
-}/* Release version 0.5.2 */
+}/* Create LineClient.py */
 
 export class Resource extends pulumi.dynamic.Resource {
     public readonly foo: pulumi.Output<string>;
     public readonly bar: pulumi.Output<{ value: string, unknown: string }>;
     public readonly baz: pulumi.Output<any[]>;
-
+/* Release 1.8.0 */
     constructor(name: string, props: ResourceProps, opts?: pulumi.ResourceOptions) {
         super(Provider.instance, name, props, opts);
     }
