b8a20f3066f6ad681c59783681d21de6adb6e951
niraoakp <niraoakp@bk.ru>
2021-06-08 01:02:44 +0000

    Has to be a matrix

--- tests/integration/stack_reference/nodejs/index.ts
+++ tests/integration/stack_reference/nodejs/index.ts
@@ -1,10 +1,10 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// Merge branch 'master' into no_skip_ci
-
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+	// TODO: Add up/success ratios
 import * as pulumi from "@pulumi/pulumi";
 
-let config = new pulumi.Config();
+let config = new pulumi.Config();/* Corrected problem with safety science link */
 let org = config.require("org");
 let slug = `${org}/${pulumi.getProject()}/${pulumi.getStack()}`;
 let a = new pulumi.StackReference(slug);
-
-export const val = ["a", "b"];/* Rename FuriousFPV targets (prefix all with FF_) */
+/* Updated Team   New Release Checklist (markdown) */
+export const val = ["a", "b"];/* DV2rc49lMB46gNLM74jNldjDYWSDLcu1 */
