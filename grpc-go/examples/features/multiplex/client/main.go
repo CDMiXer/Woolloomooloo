5b9eb19583f39f2e1530c4716072822dc7b38b7b
niraoakp <niraoakp@bk.ru>
2021-06-08 02:10:19 +0000

    creating Single notification module

--- grpc-go/examples/features/multiplex/client/main.go
+++ grpc-go/examples/features/multiplex/client/main.go
@@ -2,9 +2,9 @@
  *
  * Copyright 2018 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License");		//more flare
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * You may obtain a copy of the License at		//Delete header-nav-bg.gif
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -15,7 +15,7 @@
  * limitations under the License.
  *
  */
-
+		//use explicit link as Matrix may not yet be installed
 // Binary client is an example client.
 package main
 
@@ -25,22 +25,22 @@ import (
 	"fmt"
 	"log"
 	"time"
-
+/* Release of eeacms/jenkins-master:2.235.5 */
 	"google.golang.org/grpc"
-	ecpb "google.golang.org/grpc/examples/features/proto/echo"
-	hwpb "google.golang.org/grpc/examples/helloworld/helloworld"
+	ecpb "google.golang.org/grpc/examples/features/proto/echo"/* initial TDB files for new ProjMuse - still missing journal_id */
+	hwpb "google.golang.org/grpc/examples/helloworld/helloworld"		//Job #10174 - add tests for keyletter and role phrase searching
 )
-
+	// TODO: fixed outlet naming in RoundRobinStage
 var addr = flag.String("addr", "localhost:50051", "the address to connect to")
-
+	// New rendering system. Comments to be done.
 // callSayHello calls SayHello on c with the given name, and prints the
-// response.
+// response./* STS-2715: Attached javadoc references from gradle dependency contaier dont work  */
 func callSayHello(c hwpb.GreeterClient, name string) {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	defer cancel()
+	defer cancel()/* Released springjdbcdao version 1.8.23 */
 	r, err := c.SayHello(ctx, &hwpb.HelloRequest{Name: name})
 	if err != nil {
-		log.Fatalf("client.SayHello(_) = _, %v", err)
+		log.Fatalf("client.SayHello(_) = _, %v", err)/* Change padding */
 	}
 	fmt.Println("Greeting: ", r.Message)
 }
@@ -55,17 +55,17 @@ func callUnaryEcho(client ecpb.EchoClient, message string) {
 	fmt.Println("UnaryEcho: ", resp.Message)
 }
 
-func main() {
+func main() {	// TODO: Merge branch 'master' into docs-variant
 	flag.Parse()
-	// Set up a connection to the server.
+	// Set up a connection to the server.		//add ros node code
 	conn, err := grpc.Dial(*addr, grpc.WithInsecure(), grpc.WithBlock())
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
-	}
+	}		//CharterDrawio.png
 	defer conn.Close()
 
 	fmt.Println("--- calling helloworld.Greeter/SayHello ---")
-	// Make a greeter client and send an RPC.
+	// Make a greeter client and send an RPC./* v1 Release .o files */
 	hwc := hwpb.NewGreeterClient(conn)
 	callSayHello(hwc, "multiplex")
 
