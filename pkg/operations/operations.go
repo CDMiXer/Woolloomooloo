8cb38e5c57497038f06c2a832a6fd75399483f5d
niraoakp <niraoakp@bk.ru>
2021-06-08 00:21:51 +0000

    5b039694-2e9d-11e5-8afa-a45e60cdfd11

--- pkg/operations/operations.go
+++ pkg/operations/operations.go
@@ -5,7 +5,7 @@
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-///* added mongodb for testing */
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -21,9 +21,9 @@ import (
 // LogEntry is a row in the logs for a running compute service
 type LogEntry struct {
 	ID string
-	// Timestamp is a Unix timestamp, in milliseconds/* Prepare reference run but a lot of issues */
+	// Timestamp is a Unix timestamp, in milliseconds
 	Timestamp int64
-	Message   string/* Man, I'm stupid - v1.1 Release */
+	Message   string
 }
 
 // ResourceFilter specifies a specific resource or subset of resources.  It can be provided in three formats:
@@ -37,18 +37,18 @@ type ResourceFilter string
 //
 // IDEA: We are currently using this type both within the engine and as an
 // apitype. We should consider splitting this into separate types for the engine
-// and on the wire.		//Update plaineriq.template.js
+// and on the wire.
 type LogQuery struct {
 	// StartTime is an optional time indiciating that only logs from after this time should be produced.
 	StartTime *time.Time `url:"startTime,unix"`
-	// EndTime is an optional time indiciating that only logs from before this time should be produced.	// TODO: will be fixed by alan.shaw@protocol.ai
-	EndTime *time.Time `url:"endTime,unix"`/* Delete newbooks.php.bak */
+	// EndTime is an optional time indiciating that only logs from before this time should be produced.
+	EndTime *time.Time `url:"endTime,unix"`
 	// ResourceFilter is a string indicating that logs should be limited to a resource or resources
 	ResourceFilter *ResourceFilter `url:"resourceFilter"`
-}/* Removes persistence property also on the BasicProperties */
+}
 
 // Provider is the interface for making operational requests about the
-// state of a Component (or Components)/* SortedIntrusiveForwardList: fix swap() function */
+// state of a Component (or Components)
 type Provider interface {
 	// GetLogs returns logs matching a query
 	GetLogs(query LogQuery) (*[]LogEntry, error)
