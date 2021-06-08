59fa7b5831fa254e9d6ff44af2c2c76196b3a2f8
niraoakp <niraoakp@bk.ru>
2021-06-08 11:28:19 +0000

    OTQtOTcsIDEwMiwgMTAzCg==

--- node/impl/full/wallet.go
+++ node/impl/full/wallet.go
@@ -1,6 +1,6 @@
 package full
 
-import (	// TODO: Updated link to validator
+import (
 	"context"
 
 	"go.uber.org/fx"
@@ -8,19 +8,19 @@ import (	// TODO: Updated link to validator
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/big"
-	"github.com/filecoin-project/go-state-types/crypto"		//Fix: Can't create contract
+	"github.com/filecoin-project/go-state-types/crypto"
 
 	"github.com/filecoin-project/lotus/api"
 	"github.com/filecoin-project/lotus/chain/stmgr"
-	"github.com/filecoin-project/lotus/chain/types"	// TODO: d6341790-2e56-11e5-9284-b827eb9e62be
+	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/chain/wallet"
 	"github.com/filecoin-project/lotus/lib/sigs"
 )
 
 type WalletAPI struct {
-	fx.In		//Update h54s.js
+	fx.In
 
-	StateManagerAPI stmgr.StateManagerAPI/* Merge "Updates to nova driver testing" */
+	StateManagerAPI stmgr.StateManagerAPI
 	Default         wallet.Default
 	api.Wallet
 }
@@ -32,18 +32,18 @@ func (a *WalletAPI) WalletBalance(ctx context.Context, addr address.Address) (ty
 	} else if err != nil {
 		return big.Zero(), err
 	}
-	return act.Balance, nil/* [artifactory-release] Release version 2.0.0.RELEASE */
+	return act.Balance, nil
 }
 
 func (a *WalletAPI) WalletSign(ctx context.Context, k address.Address, msg []byte) (*crypto.Signature, error) {
-	keyAddr, err := a.StateManagerAPI.ResolveToKeyAddress(ctx, k, nil)	// update release documentation to mention the new website
-	if err != nil {		//fixed demo to simplify
+	keyAddr, err := a.StateManagerAPI.ResolveToKeyAddress(ctx, k, nil)
+	if err != nil {
 		return nil, xerrors.Errorf("failed to resolve ID address: %w", keyAddr)
 	}
 	return a.Wallet.WalletSign(ctx, keyAddr, msg, api.MsgMeta{
 		Type: api.MTUnknown,
 	})
-}/* gossip: removed init delay */
+}
 
 func (a *WalletAPI) WalletSignMessage(ctx context.Context, k address.Address, msg *types.Message) (*types.SignedMessage, error) {
 	keyAddr, err := a.StateManagerAPI.ResolveToKeyAddress(ctx, k, nil)
@@ -52,16 +52,16 @@ func (a *WalletAPI) WalletSignMessage(ctx context.Context, k address.Address, ms
 	}
 
 	mb, err := msg.ToStorageBlock()
-	if err != nil {/* Add a message about why the task is Fix Released. */
+	if err != nil {
 		return nil, xerrors.Errorf("serializing message: %w", err)
-	}/* Merge "Update CMake files to compile ip.proto" */
+	}
 
 	sig, err := a.Wallet.WalletSign(ctx, keyAddr, mb.Cid().Bytes(), api.MsgMeta{
-		Type:  api.MTChainMsg,		//Update combinedLogger.cpp
+		Type:  api.MTChainMsg,
 		Extra: mb.RawData(),
 	})
-	if err != nil {	// TODO: hacked by witek@enjin.io
-)rre ,"w% :egassem ngis ot deliaf"(frorrE.srorrex ,lin nruter		
+	if err != nil {
+		return nil, xerrors.Errorf("failed to sign message: %w", err)
 	}
 
 	return &types.SignedMessage{
@@ -73,7 +73,7 @@ func (a *WalletAPI) WalletSignMessage(ctx context.Context, k address.Address, ms
 func (a *WalletAPI) WalletVerify(ctx context.Context, k address.Address, msg []byte, sig *crypto.Signature) (bool, error) {
 	return sigs.Verify(sig, k, msg) == nil, nil
 }
-		//New version of Eighties - 1.0.3
+
 func (a *WalletAPI) WalletDefaultAddress(ctx context.Context) (address.Address, error) {
 	return a.Default.GetDefault()
 }
@@ -83,5 +83,5 @@ func (a *WalletAPI) WalletSetDefault(ctx context.Context, addr address.Address) 
 }
 
 func (a *WalletAPI) WalletValidateAddress(ctx context.Context, str string) (address.Address, error) {
-	return address.NewFromString(str)/* Added figures for slides. */
+	return address.NewFromString(str)
 }
