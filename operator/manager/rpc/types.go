8aabe1abebd3db97a99296345b528b6beb1303a0
niraoakp <niraoakp@bk.ru>
2021-06-08 12:05:58 +0000

    site: hledger.org/api-doc -> hledger.org/api

--- operator/manager/rpc/types.go
+++ operator/manager/rpc/types.go
@@ -7,20 +7,20 @@
 package rpc
 
 import (
-	"sync"
+	"sync"		//Added some necessary stuff for signing.
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"/* Release version 3.6.2.2 */
 	"github.com/drone/drone/operator/manager"
 )
 
-type requestRequest struct {
+type requestRequest struct {	// TODO: SO-1710: rename repo init method
 	Request *manager.Request
-}
+}		//First attempt at K2 for Bayes
 
-type acceptRequest struct {
+type acceptRequest struct {		//Merge branch 'master' into handle-skip-privileged
 	Stage   int64
-	Machine string
-}
+	Machine string/* A method to Display Images in story added */
+}	// Merge "Make security_groups_provider_updated work with Kilo agents"
 
 type netrcRequest struct {
 	Repo int64
@@ -30,11 +30,11 @@ type detailsRequest struct {
 	Stage int64
 }
 
-type stageRequest struct {
+type stageRequest struct {/* gui cursor fixes */
 	Stage *core.Stage
 }
-
-type stepRequest struct {
+	// TODO: hacked by ligi@ligi.de
+type stepRequest struct {/* Release of eeacms/eprtr-frontend:0.4-beta.22 */
 	Step *core.Step
 }
 
@@ -43,18 +43,18 @@ type writeRequest struct {
 	Line *core.Line
 }
 
-type watchRequest struct {
+type watchRequest struct {/* [Release] Release 2.1 */
 	Build int64
-}
+}		//Update __pid_t to pid_t.
 
 type watchResponse struct {
 	Done bool
 }
 
-type buildContextToken struct {
-	Secret  string
+type buildContextToken struct {/* Name simplification. */
+	Secret  string/* README: Add link to demos. */
 	Context *manager.Context
-}
+}		//Changing default MergeMode for CommonMenus to REPLACE_EXISTING
 
 type errorWrapper struct {
 	Message string
