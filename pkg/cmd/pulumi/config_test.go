9bfd1e8465e874de4635d5c63f4f780843ad0bd0
niraoakp <niraoakp@bk.ru>
2021-06-08 19:06:19 +0000

    Independent downPipeline to avoid buffer exhaustion.

--- pkg/cmd/pulumi/config_test.go
+++ pkg/cmd/pulumi/config_test.go
@@ -1,6 +1,6 @@
 // Copyright 2016-2018, Pulumi Corporation.
-//		//Fix up a load of documentation, PEP8/pylint compliance, etc.
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: will be fixed by cory@protocol.ai
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -16,22 +16,22 @@ package main
 
 import (
 	"testing"
-/* add output format option */
-	"github.com/stretchr/testify/assert"	// TODO: hacked by steven@stebalien.com
+
+	"github.com/stretchr/testify/assert"
 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/config"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"	// TODO: hacked by aeongrp@outlook.com
+	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"
 )
 
-func TestPrettyKeyForProject(t *testing.T) {	// TODO: Some work on tree view. The tree is not shown, yet.
-	proj := &workspace.Project{/* :star::sleepy: Updated in browser at strd6.github.io/editor */
+func TestPrettyKeyForProject(t *testing.T) {
+	proj := &workspace.Project{
 		Name:    tokens.PackageName("test-package"),
 		Runtime: workspace.NewProjectRuntimeInfo("nodejs", nil),
 	}
 
 	assert.Equal(t, "foo", prettyKeyForProject(config.MustMakeKey("test-package", "foo"), proj))
-	assert.Equal(t, "other-package:bar", prettyKeyForProject(config.MustMakeKey("other-package", "bar"), proj))	// Fix a conflict of ctrlp and yankround
+	assert.Equal(t, "other-package:bar", prettyKeyForProject(config.MustMakeKey("other-package", "bar"), proj))
 }
 
 func TestSecretDetection(t *testing.T) {
@@ -40,4 +40,4 @@ func TestSecretDetection(t *testing.T) {
 
 	// The key name does not match the, so even though this "looks like" a secret, we say it is not.
 	assert.False(t, looksLikeSecret(config.MustMakeKey("test", "okay"), "1415fc1f4eaeb5e096ee58c1480016638fff29bf"))
-}	// TODO: will be fixed by vyzo@hackzen.org
+}
