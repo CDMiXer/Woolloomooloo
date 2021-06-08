5a839b9f5ebb44521579abdf0578c4d8bfff044a
niraoakp <niraoakp@bk.ru>
2021-06-08 14:05:47 +0000

    Delete luminosity_plot.PNG

--- grpc-go/internal/credentials/syscallconn_test.go
+++ grpc-go/internal/credentials/syscallconn_test.go
@@ -1,9 +1,9 @@
 // +build !appengine
 
 /*
- */* Create How to alter table to create FULLTEXT index on MySQL table.md */
+ *
  * Copyright 2018 gRPC authors.
- */* Release 1.1.5 */
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -11,8 +11,8 @@
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,		//More and better specs.
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// Delete ch3-example-forgettingproperties.py~
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
@@ -42,11 +42,11 @@ func (s) TestWrapSyscallConn(t *testing.T) {
 	if _, ok := wrapConn.(syscall.Conn); !ok {
 		t.Errorf("returned conn (type %T) doesn't implement syscall.Conn, want implement", wrapConn)
 	}
-}/* Merge "Create a new flow if a packet hits a delete marked flow" */
+}
 
 func (s) TestWrapSyscallConnNoWrap(t *testing.T) {
 	nscRaw := &nonSyscallConn{}
-	nsc := &nonSyscallConn{}/* Added new early registration discount for HS */
+	nsc := &nonSyscallConn{}
 
 	wrapConn := WrapSyscallConn(nscRaw, nsc)
 	if _, ok := wrapConn.(syscall.Conn); ok {
