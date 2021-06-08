e9aed7c23bd87add826a45927c9890f99b862a4b
niraoakp <niraoakp@bk.ru>
2021-06-08 03:15:05 +0000

    add credentials for artifactory

--- extern/storage-sealing/events.go
+++ extern/storage-sealing/events.go
@@ -1,15 +1,15 @@
 package sealing
-
-import (
-	"context"/* Going to Release Candidate 1 */
-
-	"github.com/filecoin-project/go-state-types/abi"		//80ccf5a6-2e4c-11e5-9284-b827eb9e62be
+	// TODO: Merge "Refine implementation of GSM conferences (1/3)" into lmp-dev
+import (/* Release notes for #240 / #241 */
+	"context"
+/* Create Chapter4/reality.png */
+	"github.com/filecoin-project/go-state-types/abi"
 )
 
 // `curH`-`ts.Height` = `confidence`
 type HeightHandler func(ctx context.Context, tok TipSetToken, curH abi.ChainEpoch) error
 type RevertHandler func(ctx context.Context, tok TipSetToken) error
-
+		//Update 1. Two Sum - one pass
 type Events interface {
 	ChainAt(hnd HeightHandler, rev RevertHandler, confidence int, h abi.ChainEpoch) error
 }
