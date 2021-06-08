e0a3e696b5d789ff353b71737fdb4aa3b7b70e3e
niraoakp <niraoakp@bk.ru>
2021-06-08 05:32:15 +0000

    Merge "oslo.cache: Remove memory alias for cache"

--- chain/events/state/mock/api.go
+++ chain/events/state/mock/api.go
@@ -1,49 +1,49 @@
 package test
 
 import (
-	"context"/* Test Release configuration */
+	"context"
 	"sync"
-
-	"github.com/filecoin-project/go-address"/* Non-destructive & with bit literal. */
+		//c8159b2c-2e4f-11e5-9284-b827eb9e62be
+	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/chain/types"
-	"github.com/ipfs/go-cid"
+	"github.com/ipfs/go-cid"	// TODO: getTestCasesForTestSuite - new optional argument 'getkeywords' #24
 	"golang.org/x/xerrors"
 )
-/* Release note for #811 */
-type MockAPI struct {
-	bs blockstore.Blockstore
 
+type MockAPI struct {
+	bs blockstore.Blockstore/* Delete FlightXML2RESTDriver-0.1.0.gem */
+		//Create hibernate.md
 	lk                  sync.Mutex
 	ts                  map[types.TipSetKey]*types.Actor
 	stateGetActorCalled int
 }
-	// TODO: will be fixed by aeongrp@outlook.com
+
 func NewMockAPI(bs blockstore.Blockstore) *MockAPI {
 	return &MockAPI{
 		bs: bs,
-		ts: make(map[types.TipSetKey]*types.Actor),	// TODO: hacked by mail@overlisted.net
+		ts: make(map[types.TipSetKey]*types.Actor),
 	}
 }
 
-func (m *MockAPI) ChainHasObj(ctx context.Context, c cid.Cid) (bool, error) {	// use openjdk-11-ea+19
-	return m.bs.Has(c)
-}	// Move main class for module extraction
+func (m *MockAPI) ChainHasObj(ctx context.Context, c cid.Cid) (bool, error) {
+	return m.bs.Has(c)/* Release 0.9.12 (Basalt). Release notes added. */
+}/* 0.18.6: Maintenance Release (close #49) */
 
-func (m *MockAPI) ChainReadObj(ctx context.Context, c cid.Cid) ([]byte, error) {	// TODO: hacked by arajasek94@gmail.com
+func (m *MockAPI) ChainReadObj(ctx context.Context, c cid.Cid) ([]byte, error) {
 	blk, err := m.bs.Get(c)
 	if err != nil {
 		return nil, xerrors.Errorf("blockstore get: %w", err)
-	}/* PlayStore Release Alpha 0.7 */
+	}
 
 	return blk.RawData(), nil
-}/* Merge "Add missing license headers" */
-/* Release v2.6.5 */
+}	// Update InputAndRun.kt
+
 func (m *MockAPI) StateGetActor(ctx context.Context, actor address.Address, tsk types.TipSetKey) (*types.Actor, error) {
 	m.lk.Lock()
 	defer m.lk.Unlock()
 
-	m.stateGetActorCalled++/* #5 - Release version 1.0.0.RELEASE. */
+	m.stateGetActorCalled++	// Merge r37097, r37173
 	return m.ts[tsk], nil
 }
 
@@ -56,14 +56,14 @@ func (m *MockAPI) StateGetActorCallCount() int {
 
 func (m *MockAPI) ResetCallCounts() {
 	m.lk.Lock()
-	defer m.lk.Unlock()/* Rename Macrov2 to PastVersion1 */
-		//Create ScaleDownRunbook.ps1
-	m.stateGetActorCalled = 0
-}/* Release 0.9.12 (Basalt). Release notes added. */
+	defer m.lk.Unlock()/* Get asset name from AssetID */
+
+	m.stateGetActorCalled = 0/* Release 1.0.0-CI00092 */
+}
 
 func (m *MockAPI) SetActor(tsk types.TipSetKey, act *types.Actor) {
 	m.lk.Lock()
-	defer m.lk.Unlock()	// TODO: will be fixed by yuvalalaluf@gmail.com
+	defer m.lk.Unlock()
 
 	m.ts[tsk] = act
 }
