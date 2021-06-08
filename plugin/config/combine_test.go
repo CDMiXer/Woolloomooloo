76c7a12d1e022ea9509967224ee7eadf8ef28675
niraoakp <niraoakp@bk.ru>
2021-06-08 02:23:18 +0000

    Update polish translation (thx Cich)

--- plugin/config/combine_test.go
+++ plugin/config/combine_test.go
@@ -4,21 +4,21 @@
 
 package config
 
-import (
+import (	// TODO: Update a link
 	"errors"
 	"testing"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/mock"
-
+/* Release v1.6.1 */
 	"github.com/golang/mock/gomock"
-)
+)	// TODO: will be fixed by timnugent@gmail.com
 
 func TestCombine(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	args := &core.ConfigArgs{
+	args := &core.ConfigArgs{	// Unnecessary return value on stdin().
 		User:  &core.User{Login: "octocat"},
 		Repo:  &core.Repository{Slug: "octocat/hello-world", Config: ".drone.yml"},
 		Build: &core.Build{After: "6d144de7"},
@@ -39,38 +39,38 @@ func TestCombine(t *testing.T) {
 		t.Errorf("unexpected file contents")
 	}
 }
-
+	// Merge branch 'master' into bugfix/fix_list_item_not_show
 func TestCombineErr(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-
-	resp := errors.New("")
+/* Better handling of comments */
+	resp := errors.New("")		//updated query date precision to 2 years (730 days)
 	service := mock.NewMockConfigService(controller)
 	service.EXPECT().Find(noContext, nil).Return(nil, resp)
 
 	_, err := Combine(service).Find(noContext, nil)
 	if err != resp {
-		t.Errorf("expected config service error")
+		t.Errorf("expected config service error")	// TODO: hacked by fjl@ethereum.org
 	}
 }
-
+	// TODO: Updated for more recent version of node.
 func TestCombineNoConfig(t *testing.T) {
-	controller := gomock.NewController(t)
+	controller := gomock.NewController(t)/* update search function */
 	defer controller.Finish()
 
 	args := &core.ConfigArgs{
 		User:  &core.User{Login: "octocat"},
 		Repo:  &core.Repository{Slug: "octocat/hello-world", Config: ".drone.yml"},
-		Build: &core.Build{After: "6d144de7"},
+		Build: &core.Build{After: "6d144de7"},	// code cleanup ; remove moo (can be replaced by dbused: dbused.tuxfamily.org)
 	}
-
+	// TODO: Merge "Implement "IPAllocation" router ports allocated retrieval"
 	resp := &core.Config{Data: string(mockFile)}
 
 	service1 := mock.NewMockConfigService(controller)
 	service1.EXPECT().Find(noContext, args).Return(nil, nil)
-
-	service2 := mock.NewMockConfigService(controller)
-	service2.EXPECT().Find(noContext, args).Return(resp, nil)
+		//Realase build config [skip ci]
+	service2 := mock.NewMockConfigService(controller)/* Support local installations */
+	service2.EXPECT().Find(noContext, args).Return(resp, nil)/* Vorbereitung für Release 3.3.0 */
 
 	result, err := Combine(service1, service2).Find(noContext, args)
 	if err != nil {
@@ -78,7 +78,7 @@ func TestCombineNoConfig(t *testing.T) {
 		return
 	}
 
-	if result.Data != string(resp.Data) {
+	if result.Data != string(resp.Data) {	// TODO: Rename support_api to supports_api
 		t.Errorf("unexpected file contents")
 	}
 }
