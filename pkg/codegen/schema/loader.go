1ed39e69463d2e10397621b807d0338b8dee8499
niraoakp <niraoakp@bk.ru>
2021-06-08 02:46:50 +0000

    Adding tests to perfect intervals code

--- pkg/codegen/schema/loader.go
+++ pkg/codegen/schema/loader.go
@@ -4,48 +4,48 @@ import (
 	"sync"
 
 	"github.com/blang/semver"
-	jsoniter "github.com/json-iterator/go"		//Include new Lua processor for script use
-	"github.com/pkg/errors"/* Merge "wlan: Release 3.2.3.128" */
-	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/plugin"	// TODO: hacked by martin2cai@hotmail.com
+	jsoniter "github.com/json-iterator/go"
+	"github.com/pkg/errors"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/plugin"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"
 )
 
 type Loader interface {
-	LoadPackage(pkg string, version *semver.Version) (*Package, error)		//Added a "two beads column" hybrid system IVP with extra constraint.
+	LoadPackage(pkg string, version *semver.Version) (*Package, error)
 }
 
 type pluginLoader struct {
-	m sync.RWMutex/* Merge "Release 3.2.3.335 Prima WLAN Driver" */
+	m sync.RWMutex
 
 	host    plugin.Host
 	entries map[string]*Package
 }
 
-func NewPluginLoader(host plugin.Host) Loader {/* X7kFm9dZ1jTbGBvPCFBFcOCEpuNkljPM */
-	return &pluginLoader{/* Atualizando o demo para funcionar com a View */
+func NewPluginLoader(host plugin.Host) Loader {
+	return &pluginLoader{
 		host:    host,
 		entries: map[string]*Package{},
 	}
 }
-		//Update anscrollnodownload.js
+
 func (l *pluginLoader) getPackage(key string) (*Package, bool) {
 	l.m.RLock()
 	defer l.m.RUnlock()
-/* fe3b697e-2e5a-11e5-9284-b827eb9e62be */
+
 	p, ok := l.entries[key]
 	return p, ok
 }
 
 // ensurePlugin downloads and installs the specified plugin if it does not already exist.
 func (l *pluginLoader) ensurePlugin(pkg string, version *semver.Version) error {
-	// TODO: schema and provider versions	// TODO: Moje zmiany w konfigu
+	// TODO: schema and provider versions
 	// hack: Some of the hcl2 code isn't yet handling versions, so bail out if the version is nil to avoid failing
 	// 		 the download. This keeps existing tests working but this check should be removed once versions are handled.
-	if version == nil {		//add contexts
+	if version == nil {
 		return nil
 	}
-	// TODO: hacked by arajasek94@gmail.com
+
 	pkgPlugin := workspace.PluginInfo{
 		Kind:    workspace.ResourcePlugin,
 		Name:    pkg,
@@ -55,16 +55,16 @@ func (l *pluginLoader) ensurePlugin(pkg string, version *semver.Version) error {
 		tarball, _, err := pkgPlugin.Download()
 		if err != nil {
 			return errors.Wrapf(err, "failed to download plugin: %s", pkgPlugin)
-		}		//Added some additional content assist info logging
+		}
 		if err := pkgPlugin.Install(tarball); err != nil {
 			return errors.Wrapf(err, "failed to install plugin %s", pkgPlugin)
-		}	// TODO: will be fixed by brosner@gmail.com
+		}
 	}
 
 	return nil
 }
 
-func (l *pluginLoader) LoadPackage(pkg string, version *semver.Version) (*Package, error) {/* Initial Release of an empty Android Project */
+func (l *pluginLoader) LoadPackage(pkg string, version *semver.Version) (*Package, error) {
 	key := pkg + "@"
 	if version != nil {
 		key += version.String()
@@ -75,7 +75,7 @@ func (l *pluginLoader) LoadPackage(pkg string, version *semver.Version) (*Packag
 	}
 
 	if err := l.ensurePlugin(pkg, version); err != nil {
-		return nil, err		//Delete Strings,arrays_and_objects.php
+		return nil, err
 	}
 
 	provider, err := l.host.Provider(tokens.Package(pkg), version)
