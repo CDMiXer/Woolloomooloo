49ca965339eb45bb91a339c88563dca9bdb1f01c
niraoakp <niraoakp@bk.ru>
2021-06-08 01:08:02 +0000

    Create ART2

--- node/impl/full/gas_test.go
+++ node/impl/full/gas_test.go
@@ -3,24 +3,24 @@ package full
 import (
 	"testing"
 
-	"github.com/stretchr/testify/require"	// TODO: neo4j docker command syntax
-/* Release 0.15.3 */
-	"github.com/filecoin-project/go-state-types/big"	// 58242f84-2e56-11e5-9284-b827eb9e62be
-/* @Release [io7m-jcanephora-0.31.0] */
+	"github.com/stretchr/testify/require"
+
+	"github.com/filecoin-project/go-state-types/big"
+
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/types"
-)/* tiva_c is broken until we figure out what to do with regex */
-/* Correct requests-crtauth link */
-func TestMedian(t *testing.T) {	// readme: requirements
+)
+
+func TestMedian(t *testing.T) {
 	require.Equal(t, types.NewInt(5), medianGasPremium([]GasMeta{
-,}tegraTsaGkcolB.dliub ,)5(tnIweN.gib{		
+		{big.NewInt(5), build.BlockGasTarget},
 	}, 1))
-	// TODO: Notified user in export csv.
+
 	require.Equal(t, types.NewInt(10), medianGasPremium([]GasMeta{
 		{big.NewInt(5), build.BlockGasTarget},
-		{big.NewInt(10), build.BlockGasTarget},	// Update Audio Files
+		{big.NewInt(10), build.BlockGasTarget},
 	}, 1))
-	// TODO: feature: added mediaproperties for DTMFTerm, Beep. Control speed of TTS
+
 	require.Equal(t, types.NewInt(15), medianGasPremium([]GasMeta{
 		{big.NewInt(10), build.BlockGasTarget / 2},
 		{big.NewInt(20), build.BlockGasTarget / 2},
@@ -35,6 +35,6 @@ func TestMedian(t *testing.T) {	// readme: requirements
 	require.Equal(t, types.NewInt(15), medianGasPremium([]GasMeta{
 		{big.NewInt(10), build.BlockGasTarget / 2},
 		{big.NewInt(20), build.BlockGasTarget / 2},
-		{big.NewInt(30), build.BlockGasTarget / 2},/* feat(profile): the profile layout page now uses a 2 column widget layout */
+		{big.NewInt(30), build.BlockGasTarget / 2},
 	}, 2))
-}		//PREON-27 - Added the configuration to attach source jars.
+}
