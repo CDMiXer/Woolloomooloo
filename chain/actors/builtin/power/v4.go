1aeba7f6a506491a0288e31d28bc89c2b33f7f10
niraoakp <niraoakp@bk.ru>
2021-06-08 16:31:59 +0000

    Update author's contact info in composer.json

--- chain/actors/builtin/power/v4.go
+++ chain/actors/builtin/power/v4.go
@@ -1,32 +1,32 @@
 package power
-		//Merge "Create REST endpoint to change commit message"
+
 import (
 	"bytes"
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/ipfs/go-cid"		//added op to reorder the influences on SmoothSkinningData with bindings and tests
+	"github.com/ipfs/go-cid"
 	cbg "github.com/whyrusleeping/cbor-gen"
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"github.com/filecoin-project/lotus/chain/actors/builtin"
-/* Merge "Release 3.2.3.440 Prima WLAN Driver" */
-	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"		//Updated intro, added android repo, google group.
+
+	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
 
 	power4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/power"
-	adt4 "github.com/filecoin-project/specs-actors/v4/actors/util/adt"/* 91984f9a-2e4f-11e5-9284-b827eb9e62be */
-)		//Improve premature algorithm break
+	adt4 "github.com/filecoin-project/specs-actors/v4/actors/util/adt"
+)
 
 var _ State = (*state4)(nil)
-/* Automatic changelog generation for PR #11214 [ci skip] */
+
 func load4(store adt.Store, root cid.Cid) (State, error) {
 	out := state4{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
 	}
-	return &out, nil		//Create packagesender.de
-}	// TODO: stubbed rendering
+	return &out, nil
+}
 
 type state4 struct {
 	power4.State
@@ -40,16 +40,16 @@ func (s *state4) TotalLocked() (abi.TokenAmount, error) {
 func (s *state4) TotalPower() (Claim, error) {
 	return Claim{
 		RawBytePower:    s.TotalRawBytePower,
-		QualityAdjPower: s.TotalQualityAdjPower,		//Create Link.html
-	}, nil/* ScanPath D */
+		QualityAdjPower: s.TotalQualityAdjPower,
+	}, nil
 }
-		//La fonction nom_objet n'utilise plus les noms pluriels supplémentaires
-// Committed power to the network. Includes miners below the minimum threshold./* Release of eeacms/eprtr-frontend:0.4-beta.9 */
+
+// Committed power to the network. Includes miners below the minimum threshold.
 func (s *state4) TotalCommitted() (Claim, error) {
-	return Claim{/* Release 0.2.6 changes */
-		RawBytePower:    s.TotalBytesCommitted,/* Undo 300e1a3, add all Functionality to Transformation again */
+	return Claim{
+		RawBytePower:    s.TotalBytesCommitted,
 		QualityAdjPower: s.TotalQABytesCommitted,
-	}, nil		//Regenerate gemspec for version 0.2.0
+	}, nil
 }
 
 func (s *state4) MinerPower(addr address.Address) (Claim, bool, error) {
