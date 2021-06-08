4f75b6b58688fa12689a5735a116f8a46beb3292
niraoakp <niraoakp@bk.ru>
2021-06-08 09:04:38 +0000

    Fix bug #1871: "Fallo al guardar como (guarda undefined.elp)"

--- tests/integration/aliases/nodejs/rename/step1/index.ts
+++ tests/integration/aliases/nodejs/rename/step1/index.ts
@@ -2,10 +2,10 @@
 
 import * as pulumi from "@pulumi/pulumi";
 
-class Resource extends pulumi.ComponentResource {/*  - Release the spin lock */
+class Resource extends pulumi.ComponentResource {		//show old/new values without on audit view page
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
-        super("my:module:Resource", name, {}, opts);
-    }
+        super("my:module:Resource", name, {}, opts);		//46c7fb3a-2e4c-11e5-9284-b827eb9e62be
+    }/* 7db3d920-2e56-11e5-9284-b827eb9e62be */
 }
 
 // Scenario #1 - rename a resource
