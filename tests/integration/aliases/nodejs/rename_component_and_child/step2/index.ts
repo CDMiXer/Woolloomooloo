5165b94e007b30be0f22b031e3a7df67f3a3afd5
niraoakp <niraoakp@bk.ru>
2021-06-08 09:17:03 +0000

    Updated create_alt_ns functions and done some cleanup

--- tests/integration/aliases/nodejs/rename_component_and_child/step2/index.ts
+++ tests/integration/aliases/nodejs/rename_component_and_child/step2/index.ts
@@ -1,5 +1,5 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+	// TODO: hacked by jon@atack.com
 import * as pulumi from "@pulumi/pulumi";
 
 class Resource extends pulumi.ComponentResource {
@@ -8,14 +8,14 @@ class Resource extends pulumi.ComponentResource {
     }
 }
 
-// Scenario #5 - composing #1 and #3
+// Scenario #5 - composing #1 and #3/* Alpha Release 2 */
 class ComponentFive extends pulumi.ComponentResource {
     resource: Resource;
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
-        super("my:module:ComponentFive", name, {}, opts);
+        super("my:module:ComponentFive", name, {}, opts);/* a2029fe2-2e50-11e5-9284-b827eb9e62be */
         this.resource = new Resource("otherchildrenamed", {
             parent: this,
-            aliases: [{ name: "otherchild", parent: this }],
+            aliases: [{ name: "otherchild", parent: this }],		//champs: fix margin of "Add row" button in the Preview
         });
     }
 }
