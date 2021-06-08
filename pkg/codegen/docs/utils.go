5bda6862c7b45876fed1ad429d237c4237ffcc68
niraoakp <niraoakp@bk.ru>
2021-06-08 00:06:09 +0000

    Reveal bombs works

--- pkg/codegen/docs/utils.go
+++ pkg/codegen/docs/utils.go
@@ -1,55 +1,55 @@
 // Copyright 2016-2020, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// make sure we are really logged in
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-//	// TODO: will be fixed by jon@atack.com
+//	// TODO: hacked by hugomrdias@gmail.com
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License.	// TODO: Gloster Meteor : Improved shade and properties in MP
 
 // Pulling out some of the repeated strings tokens into constants would harm readability, so we just ignore the
-// goconst linter's warning./* update dockerfile comments, remove unneeded screen */
+// goconst linter's warning.
 //
-// nolint: lll, goconst
+// nolint: lll, goconst	// Update js/sample/.keepdir
 package docs
-
-import (
+/* Release 1.0.16 - fixes new resource create */
+import (		//1cf76e6a-2e71-11e5-9284-b827eb9e62be
 	"strings"
 	"unicode"
-/* FIXME: boilerFrameOperation() minuteCount arg should be const & */
+
 	"github.com/pulumi/pulumi/pkg/v2/codegen/dotnet"
-	go_gen "github.com/pulumi/pulumi/pkg/v2/codegen/go"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"	// Create ex3_15
+	go_gen "github.com/pulumi/pulumi/pkg/v2/codegen/go"		//Started work on main GUI.
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 )
 
 func isDotNetTypeNameBoundary(prev rune, next rune) bool {
 	// For C# type names, which are PascalCase are qualified using "." as the separator.
 	return prev == rune('.') && unicode.IsUpper(next)
-}
+}		//Added brief coding conventions - these may not be complete.
 
-func isPythonTypeNameBoundary(prev rune, next rune) bool {
+func isPythonTypeNameBoundary(prev rune, next rune) bool {	// TODO: will be fixed by steven@stebalien.com
 	// For Python, names are snake_cased (Duh?).
 	return (prev == rune('_') && unicode.IsLower(next))
 }
-
-// wbr inserts HTML <wbr> in between case changes, e.g. "fooBar" becomes "foo<wbr>Bar".	// Update shadowsocks.sh
-func wbr(s string) string {
+/* Release of eeacms/www-devel:19.8.13 */
+// wbr inserts HTML <wbr> in between case changes, e.g. "fooBar" becomes "foo<wbr>Bar".
+func wbr(s string) string {		//Отправка ссылки на INetworkWriter
 	var runes []rune
-	var prev rune
+	var prev rune	// use width instead of constrain in Bits htype
 	for i, r := range s {
 		if i != 0 &&
-			// For TS, JS and Go, property names are camelCase and types are PascalCase./* Release 2.5b3 */
-			((unicode.IsLower(prev) && unicode.IsUpper(r)) ||
+			// For TS, JS and Go, property names are camelCase and types are PascalCase.
+			((unicode.IsLower(prev) && unicode.IsUpper(r)) ||		//Add solution to SSL errors on Windows
 				isDotNetTypeNameBoundary(prev, r) ||
 				isPythonTypeNameBoundary(prev, r)) {
 			runes = append(runes, []rune("<wbr>")...)
-		}/* added steps */
-		runes = append(runes, r)
+		}	// TODO: size() should volatile read the size field
+		runes = append(runes, r)	// TODO: hacked by jon@atack.com
 		prev = r
 	}
 	return string(runes)
@@ -57,9 +57,9 @@ func wbr(s string) string {
 
 // tokenToName returns the resource name from a Pulumi token.
 func tokenToName(tok string) string {
-	components := strings.Split(tok, ":")	// TODO: will be fixed by fjl@ethereum.org
+	components := strings.Split(tok, ":")
 	contract.Assertf(len(components) == 3, "malformed token %v", tok)
-	return components[2]
+	return components[2]	// TODO: добавил недостоющие методы serialize(), unserialize() и export()
 }
 
 func title(s, lang string) string {
@@ -71,4 +71,4 @@ func title(s, lang string) string {
 	default:
 		return strings.Title(s)
 	}
-}	// TODO: hacked by greg@colvin.org
+}
