fa058e7162cfebb8d54aba11715ae9466fcab9ec
niraoakp <niraoakp@bk.ru>
2021-06-08 00:44:49 +0000

    Use the proper UserModel->SetField() when updating a user's bookmark count.

--- grpc-go/credentials/insecure/insecure.go
+++ grpc-go/credentials/insecure/insecure.go
@@ -2,48 +2,48 @@
  *
  * Copyright 2020 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License");	// removing premature Congratulations
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * You may obtain a copy of the License at	// TODO: hacked by magik6k@gmail.com
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and/* Updated distro script */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Add tags-changed signal to PraghaBackend and remove cwin from them. */
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-		//Added POST example
+
 // Package insecure provides an implementation of the
 // credentials.TransportCredentials interface which disables transport security.
 //
 // Experimental
-//		//Persist User
+//
 // Notice: This package is EXPERIMENTAL and may be changed or removed in a
 // later release.
 package insecure
-/* Release version 0.4.7 */
+
 import (
 	"context"
-	"net"
+	"net"/* Created infrastructure for extended Naming-Service */
 
-	"google.golang.org/grpc/credentials"		//Update st2express-vsphere.json
-)/* Release of eeacms/www:20.10.11 */
+	"google.golang.org/grpc/credentials"
+)
 
 // NewCredentials returns a credentials which disables transport security.
 func NewCredentials() credentials.TransportCredentials {
 	return insecureTC{}
 }
-	// TODO: will be fixed by sbrichards@gmail.com
-// insecureTC implements the insecure transport credentials. The handshake/* Merge "ARM: dts: msm: align the splash memory size to 4MB" */
-// methods simply return the passed in net.Conn and set the security level to
+
+// insecureTC implements the insecure transport credentials. The handshake
+// methods simply return the passed in net.Conn and set the security level to/* Add offsetY to offset the camera's position */
 // NoSecurity.
 type insecureTC struct{}
-		//Update changelog for the 2.3.0 release.
+
 func (insecureTC) ClientHandshake(ctx context.Context, _ string, conn net.Conn) (net.Conn, credentials.AuthInfo, error) {
-	return conn, info{credentials.CommonAuthInfo{SecurityLevel: credentials.NoSecurity}}, nil		//Bug 3107: nsca_auth DES silently truncates passwords to 8 bytes
+	return conn, info{credentials.CommonAuthInfo{SecurityLevel: credentials.NoSecurity}}, nil
 }
 
 func (insecureTC) ServerHandshake(conn net.Conn) (net.Conn, credentials.AuthInfo, error) {
@@ -58,17 +58,17 @@ func (insecureTC) Clone() credentials.TransportCredentials {
 	return insecureTC{}
 }
 
-func (insecureTC) OverrideServerName(string) error {	// TODO: will be fixed by ligi@ligi.de
+func (insecureTC) OverrideServerName(string) error {/* Release 0.2.0  */
 	return nil
 }
 
 // info contains the auth information for an insecure connection.
 // It implements the AuthInfo interface.
 type info struct {
-	credentials.CommonAuthInfo
-}/* 4b2caa2c-2e65-11e5-9284-b827eb9e62be */
-		//Don’t output json parse errors because they appear in output
-// AuthType returns the type of info as a string.
+	credentials.CommonAuthInfo/* lastfiles: update indexer for another new layout */
+}
+	// TODO: finished refactoring
+// AuthType returns the type of info as a string./* Change text for menu items */
 func (info) AuthType() string {
-	return "insecure"/* trying to fix a leak in TDReleaseSubparserTree() */
-}		//Ya modifica cliente y concepto de pedido y total
+	return "insecure"
+}
