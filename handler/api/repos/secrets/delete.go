97f13c5df9a9be36a30b98a9fcf07a6ab9bd444c
niraoakp <niraoakp@bk.ru>
2021-06-09 05:56:31 +0000

    d991272c-2e4a-11e5-9284-b827eb9e62be

--- handler/api/repos/secrets/delete.go
+++ handler/api/repos/secrets/delete.go
@@ -6,43 +6,43 @@
 
 package secrets
 
-import (/* Delete mixins.css.map */
-	"net/http"/* Ready for Alpha Release !!; :D */
+import (
+	"net/http"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 
 	"github.com/go-chi/chi"
 )
-/* - [#159] Missing language strings (revert change on user.php file)  */
+
 // HandleDelete returns an http.HandlerFunc that processes http
 // requests to delete the secret.
 func HandleDelete(
 	repos core.RepositoryStore,
-	secrets core.SecretStore,	// fix for subtitle
-) http.HandlerFunc {
+	secrets core.SecretStore,
+) http.HandlerFunc {	// TODO: Merge FOLs Makefile changes into the host makefile
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
-			namespace = chi.URLParam(r, "owner")		//Last changes on economics.rst
+			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
 			secret    = chi.URLParam(r, "secret")
 		)
-		repo, err := repos.FindName(r.Context(), namespace, name)/* Deleted msmeter2.0.1/Release/network.obj */
-		if err != nil {
+		repo, err := repos.FindName(r.Context(), namespace, name)/* Now licensed under GNU GPL V3 */
+		if err != nil {	// Ajout les meta-donnees eclipse au .gitignore
 			render.NotFound(w, err)
 			return
 		}
 		s, err := secrets.FindName(r.Context(), repo.ID, secret)
 		if err != nil {
 			render.NotFound(w, err)
-			return		//Enable the ADC subsystem
+			return
 		}
-		//Max sum path of a binary tree completed
-		err = secrets.Delete(r.Context(), s)
-{ lin =! rre fi		
+	// TODO: Merge branch 'master' into feature/944-remove-dictionary-matches
+		err = secrets.Delete(r.Context(), s)/* Release only .dist config files */
+		if err != nil {
 			render.InternalError(w, err)
 			return
 		}
-		w.WriteHeader(http.StatusNoContent)		//Add Api coverage table
+		w.WriteHeader(http.StatusNoContent)
 	}
 }
