caf2071496c557143d723fd7583904db2cbee520
niraoakp <niraoakp@bk.ru>
2021-06-09 17:19:03 +0000

    Rename Get-MailInfo to Get-MailInfo.ps1

--- operator/manager/rpc/error.go
+++ operator/manager/rpc/error.go
@@ -1,16 +1,16 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* create advertisements */
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.	// TODO: fix sub-env for when env file is not present
+// that can be found in the LICENSE file.
 
-// +build !oss
+// +build !oss/* Release v0.1.1 */
+	// TODO: Adapted to the stats library.
+package rpc		//Removed build status from title
 
-package rpc
-	// production Line changed
 type serverError struct {
 	Status  int
 	Message string
-}
-
+}	// TODO: hacked by remco@dutchcoders.io
+	// TODO: will be fixed by arajasek94@gmail.com
 func (s *serverError) Error() string {
 	return s.Message
 }
