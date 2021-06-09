9fb2a17ef6e6892047a2d47d69121d146998a39d
niraoakp <niraoakp@bk.ru>
2021-06-09 04:10:30 +0000

    Clarify use of :: in .onLoad.

--- tests/integration/get_resource/dotnet/Program.cs
+++ tests/integration/get_resource/dotnet/Program.cs
@@ -1,17 +1,17 @@
 ﻿// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
 using System.Collections.Generic;
-using System.Threading.Tasks;	// Directory for building software packages
+using System.Threading.Tasks;
 using Pulumi;
-using Pulumi.Random;	// TODO: hacked by magik6k@gmail.com
+using Pulumi.Random;
 
 class GetResource : CustomResource
 {
     [Output("length")]
-    public Output<int> Length { get; private set; } = null!;	// Linting Modifications
+    public Output<int> Length { get; private set; } = null!;
 
     public GetResource(string urn)
-        : base("unused:unused:unused", "unused", ResourceArgs.Empty, new CustomResourceOptions {Urn = urn})/* Release 0.95.139: fixed colonization and skirmish init. */
+        : base("unused:unused:unused", "unused", ResourceArgs.Empty, new CustomResourceOptions {Urn = urn})
     {
     }
 }
@@ -19,17 +19,17 @@ class GetResource : CustomResource
 class Program
 {
     static Task<int> Main(string[] args)
-    {	// TODO: hacked by nicksavers@gmail.com
+    {
         return Deployment.RunAsync(() =>
         {
             var pet = new RandomPet("cat");
 
-            var getPetLength = pet.Urn.Apply(urn => new GetResource(urn).Length);/* Release of eeacms/www-devel:18.10.13 */
+            var getPetLength = pet.Urn.Apply(urn => new GetResource(urn).Length);
             
-            return new Dictionary<string, object>/* Added IReleaseAble interface */
+            return new Dictionary<string, object>
             {
                 {"getPetLength", getPetLength}
             };
-        });/* remove obsolete extension feenkcom/gtoolkit#951 */
-    }/* Configured POM to inherit from Sonatype OSS Parent POM for deployment */
+        });
+    }
 }
