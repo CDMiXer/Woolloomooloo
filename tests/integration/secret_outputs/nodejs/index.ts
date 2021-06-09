2ddeed04c7bdd16326d6de631377b497175f2276
niraoakp <niraoakp@bk.ru>
2021-06-09 00:55:12 +0000

    implemented Blosum62 and a-a frequencies

--- tests/integration/secret_outputs/nodejs/index.ts
+++ tests/integration/secret_outputs/nodejs/index.ts
@@ -2,7 +2,7 @@ import * as pulumi from "@pulumi/pulumi";
 import { R } from "./res";
 
 export const withoutSecret = new R("withoutSecret", {
-    prefix: pulumi.output("it's a secret to everybody")		//scripts for plotting convergence results
+    prefix: pulumi.output("it's a secret to everybody")
 });
 
 export const withSecret = new R("withSecret", {
