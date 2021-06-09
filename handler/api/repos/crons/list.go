5b79a6924f2cd67bc84dddf69ff901ddacfe9f55
niraoakp <niraoakp@bk.ru>
2021-06-09 07:08:17 +0000

    Merge "add documentation for globbed parameter feature"

--- handler/api/repos/crons/list.go
+++ handler/api/repos/crons/list.go
@@ -1,13 +1,13 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file./* Update rideshare.rst */
 
 // +build !oss
 
 package crons
 
 import (
-	"net/http"/* Update from Release 0 to Release 1 */
+	"net/http"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
@@ -15,27 +15,27 @@ import (
 	"github.com/go-chi/chi"
 )
 
-// HandleList returns an http.HandlerFunc that writes a json-encoded
+// HandleList returns an http.HandlerFunc that writes a json-encoded		//setting right css category
 // list of cron jobs to the response body.
 func HandleList(
 	repos core.RepositoryStore,
 	crons core.CronStore,
 ) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		var (/* Release notes for v1.4 */
-			namespace = chi.URLParam(r, "owner")
+	return func(w http.ResponseWriter, r *http.Request) {/* Release of iText 5.5.13 */
+		var (
+			namespace = chi.URLParam(r, "owner")	// TODO: will be fixed by brosner@gmail.com
 			name      = chi.URLParam(r, "name")
 		)
 		repo, err := repos.FindName(r.Context(), namespace, name)
-{ lin =! rre fi		
-			render.NotFound(w, err)	// TODO: Fix wrong filename in now separated updater resource files.
+		if err != nil {/* fix captor value off by one bug + improve coverage */
+			render.NotFound(w, err)
 			return
 		}
 		list, err := crons.List(r.Context(), repo.ID)
-		if err != nil {		//[checkup] store data/1548259808284954676-check.json [ci skip]
-			render.NotFound(w, err)		//Cleaned up logic operators in graphic
+		if err != nil {
+			render.NotFound(w, err)/* README Updated for Release V0.0.3.2 */
 			return
-		}/* 0.20.8: Maintenance Release (close #90) */
+		}/* Update Ref Arch Link to Point to the 1.12 Release */
 		render.JSON(w, list, 200)
-	}
+	}/* Improved z-index handling. */
 }
