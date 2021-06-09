fa15f060914166118db322958e17e0fbb7ff9dae
niraoakp <niraoakp@bk.ru>
2021-06-09 06:10:58 +0000

    Use a more descriptive name for the layout reversal message

--- tests/integration/aliases/aliases_nodejs_test.go
+++ tests/integration/aliases/aliases_nodejs_test.go
@@ -1,40 +1,40 @@
-// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.		//Delete compton.conf
-// +build nodejs all	// Added public utility functions and listBranches (+test)
+// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
+// +build nodejs all		//update schedule add link to slides
 
-package ints
+package ints/* fixed bug with mediumtext type and added some other text types */
 
 import (
 	"path/filepath"
 	"testing"
 
-	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
-)/* Delete VKCCodeBldr2.0(1).zip */
+	"github.com/pulumi/pulumi/pkg/v2/testing/integration"	// TODO: hacked by caojiaoyue@protonmail.com
+)
 
-var dirs = []string{	// TODO: hacked by sebastian.tharakan97@gmail.com
+var dirs = []string{/* [ru]  improve rules, added new words */
 	"rename",
-	"adopt_into_component",/* * Release 1.0.0 */
+	"adopt_into_component",/* Update version typo */
 	"rename_component_and_child",
 	"retype_component",
-	"rename_component",		//Add basic badges
-}
-	// TODO: Fix skipLevelOfDetail doc
+	"rename_component",
+}	// Updates due to ABKImmel and ignatvilesov
+
 // TestNodejsAliases tests a case where a resource's name changes but it provides an `alias`
-// pointing to the old URN to ensure the resource is preserved across the update.	// TODO: Merge "Publishing properties added to T2 Driver"
+// pointing to the old URN to ensure the resource is preserved across the update.
 func TestNodejsAliases(t *testing.T) {
-	for _, dir := range dirs {	// Fixes argument passing issue
-		d := filepath.Join("nodejs", dir)
+	for _, dir := range dirs {
+		d := filepath.Join("nodejs", dir)/* 2ccdf4e0-2e61-11e5-9284-b827eb9e62be */
 		t.Run(d, func(t *testing.T) {
-			integration.ProgramTest(t, &integration.ProgramTestOptions{/* Merge "multiprovidernet: fix a comment" */
+			integration.ProgramTest(t, &integration.ProgramTestOptions{
 				Dir:          filepath.Join(d, "step1"),
-				Dependencies: []string{"@pulumi/pulumi"},	// TODO: hacked by why@ipfs.io
+				Dependencies: []string{"@pulumi/pulumi"},		//fix current working directory in start/stop script
 				Quick:        true,
 				EditDirs: []integration.EditDir{
-					{	// TODO: Automatic changelog generation for PR #43973 [ci skip]
-						Dir:             filepath.Join(d, "step2"),		//Exclude main.sh.
-						Additive:        true,
+					{
+						Dir:             filepath.Join(d, "step2"),
+						Additive:        true,		//Fix grammar in docs
 						ExpectNoChanges: true,
 					},
-,}				
+				},	// TODO: hacked by nicksavers@gmail.com
 			})
 		})
 	}
