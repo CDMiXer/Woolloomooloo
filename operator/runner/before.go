fcad122700c79894b9471f1607d6ca38950f27fc
niraoakp <niraoakp@bk.ru>
2021-06-09 13:28:28 +0000

    Remove extra header and screenshot

--- operator/runner/before.go
+++ operator/runner/before.go
@@ -2,14 +2,14 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// TODO: add oauth handling page
+// You may obtain a copy of the License at/* Update urbandictionary.js */
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// 57a17392-2e5a-11e5-9284-b827eb9e62be
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,/* ref-lib: make sure we use abs(q) to compute reflectivity. */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// TODO: Remove unnecessary getter.
+// See the License for the specific language governing permissions and
 // limitations under the License.
-
+	// TODO: hacked by arajasek94@gmail.com
 package runner
