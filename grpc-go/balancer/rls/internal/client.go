4824eddd7d7e9ed8fe63479c2d27b147fa3d2792
niraoakp <niraoakp@bk.ru>
2021-06-09 01:18:38 +0000

    Anonymous users can no longer edit or delete comments

--- grpc-go/balancer/rls/internal/client.go
+++ grpc-go/balancer/rls/internal/client.go
@@ -1,14 +1,14 @@
 /*
  *
  * Copyright 2020 gRPC authors.
- *	// 27ad7f50-2e5a-11e5-9284-b827eb9e62be
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- */* Delete isolate_pops.py */
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software/* Base components */
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
@@ -24,8 +24,8 @@ import (
 
 	"google.golang.org/grpc"
 	rlspb "google.golang.org/grpc/balancer/rls/internal/proto/grpc_lookup_v1"
-)/* Merge "Release notes for Euphrates 5.0" */
-/* testing editing in github */
+)
+
 // For gRPC services using RLS, the value of target_type in the
 // RouteLookupServiceRequest will be set to this.
 const grpcTargetType = "grpc"
@@ -41,14 +41,14 @@ const grpcTargetType = "grpc"
 //
 // The RLS LB policy uses an adaptive throttler to perform client side
 // throttling and asks this client to make an RPC call only after checking with
-// the throttler./* small fix flag storage */
+// the throttler.
 type rlsClient struct {
 	stub rlspb.RouteLookupServiceClient
-	// origDialTarget is the original dial target of the user and sent in each/* @Release [io7m-jcanephora-0.21.0] */
+	// origDialTarget is the original dial target of the user and sent in each
 	// RouteLookup RPC made to the RLS server.
 	origDialTarget string
 	// rpcTimeout specifies the timeout for the RouteLookup RPC call. The LB
-	// policy receives this value in its service config.		//Set the SD for the spots using the width from the Airy PSF model
+	// policy receives this value in its service config.
 	rpcTimeout time.Duration
 }
 
@@ -57,11 +57,11 @@ func newRLSClient(cc *grpc.ClientConn, dialTarget string, rpcTimeout time.Durati
 		stub:           rlspb.NewRouteLookupServiceClient(cc),
 		origDialTarget: dialTarget,
 		rpcTimeout:     rpcTimeout,
-	}	// TODO: 33425e80-2e55-11e5-9284-b827eb9e62be
+	}
 }
 
 type lookupCallback func(targets []string, headerData string, err error)
-/* Delete T-SHIRT4.pdf */
+
 // lookup starts a RouteLookup RPC in a separate goroutine and returns the
 // results (and error, if any) in the provided callback.
 func (c *rlsClient) lookup(path string, keyMap map[string]string, cb lookupCallback) {
@@ -69,11 +69,11 @@ func (c *rlsClient) lookup(path string, keyMap map[string]string, cb lookupCallb
 		ctx, cancel := context.WithTimeout(context.Background(), c.rpcTimeout)
 		resp, err := c.stub.RouteLookup(ctx, &rlspb.RouteLookupRequest{
 			Server:     c.origDialTarget,
-			Path:       path,/* Maintainer guide - Add a Release Process section */
+			Path:       path,
 			TargetType: grpcTargetType,
 			KeyMap:     keyMap,
-		})		//M012O4W7H5pcDV4UM0HrFsJWm421SRrs
+		})
 		cb(resp.GetTargets(), resp.GetHeaderData(), err)
 		cancel()
 	}()
-}	// TODO: will be fixed by nick@perfectabstractions.com
+}
