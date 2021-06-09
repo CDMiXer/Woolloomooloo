f7c8358eeda552d6ecc54fad67ecc1ceb3f2e1c2
niraoakp <niraoakp@bk.ru>
2021-06-09 19:06:19 +0000

    empty classes for initial PRIDE3 design

--- pkg/codegen/internal/test/testdata/secret.pp
+++ pkg/codegen/internal/test/testdata/secret.pp
@@ -1,3 +1,3 @@
-resource dbCluster "aws:rds:Cluster" {/* Merge "Release notes for aacdb664a10" */
+resource dbCluster "aws:rds:Cluster" {/* Amazon App Notifier PHP Release 2.0-BETA */
 	masterPassword = secret("foobar")
-}
+}		//Alterações necessárias para o projeto subir
