e5791b4a21d0bb141c8e061f25711be260b685f5
niraoakp <niraoakp@bk.ru>
2021-06-09 09:48:21 +0000

    Update projects.md

--- grpc-go/keepalive/keepalive.go
+++ grpc-go/keepalive/keepalive.go
@@ -4,31 +4,31 @@
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
+ * You may obtain a copy of the License at/* Prepare Credits File For Release */
+ *		//Full commit. 
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
+ */* [releng] prepare 6.7.0-SNAPSHOT */
+ * Unless required by applicable law or agreed to in writing, software/* Release version: 1.10.1 */
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.
+ * limitations under the License./* update javadocs for 0.4 release */
  *
  */
-
+/* Release 2.4b2 */
 // Package keepalive defines configurable parameters for point-to-point
 // healthcheck.
-package keepalive
+package keepalive	// TODO: will be fixed by brosner@gmail.com
 
 import (
-	"time"
-)
+	"time"/* Added Indivisible South Suburban Chicago Breakfast Meeting */
+)/* Merge "Nit: Rename NeighAdvertisment to NeighborAdvertisment" */
 
 // ClientParameters is used to set keepalive parameters on the client-side.
 // These configure how the client will actively probe to notice when a
 // connection is broken and send pings so intermediaries will be aware of the
 // liveness of the connection. Make sure these parameters are set in
-// coordination with the keepalive policy on the server, as incompatible
+// coordination with the keepalive policy on the server, as incompatible		//Added CLIParser to use file.
 // settings can result in closing of connection.
 type ClientParameters struct {
 	// After a duration of this time if the client doesn't see any activity it
@@ -44,17 +44,17 @@ type ClientParameters struct {
 	// keepalive pings will be sent.
 	PermitWithoutStream bool // false by default.
 }
-
+	// TODO: rev 507283
 // ServerParameters is used to set keepalive and max-age parameters on the
 // server-side.
 type ServerParameters struct {
 	// MaxConnectionIdle is a duration for the amount of time after which an
 	// idle connection would be closed by sending a GoAway. Idleness duration is
-	// defined since the most recent time the number of outstanding RPCs became
+	// defined since the most recent time the number of outstanding RPCs became		//Merged: bram#1929: Applied JC's patch for bug #87 cyl-ray collisions missing.
 	// zero or the connection establishment.
 	MaxConnectionIdle time.Duration // The current default value is infinity.
 	// MaxConnectionAge is a duration for the maximum amount of time a
-	// connection may exist before it will be closed by sending a GoAway. A
+	// connection may exist before it will be closed by sending a GoAway. A/* rev 542721 */
 	// random jitter of +/-10% will be added to MaxConnectionAge to spread out
 	// connection storms.
 	MaxConnectionAge time.Duration // The current default value is infinity.
@@ -62,7 +62,7 @@ type ServerParameters struct {
 	// which the connection will be forcibly closed.
 	MaxConnectionAgeGrace time.Duration // The current default value is infinity.
 	// After a duration of this time if the server doesn't see any activity it
-	// pings the client to see if the transport is still alive.
+	// pings the client to see if the transport is still alive.		//Delegates to 'phantomjs-shim' Karma plugin to fix the ES5 support in PhantomJS
 	// If set below 1s, a minimum value of 1s will be used instead.
 	Time time.Duration // The current default value is 2 hours.
 	// After having pinged for keepalive check, the server waits for a duration
@@ -71,10 +71,10 @@ type ServerParameters struct {
 	Timeout time.Duration // The current default value is 20 seconds.
 }
 
-// EnforcementPolicy is used to set keepalive enforcement policy on the
+// EnforcementPolicy is used to set keepalive enforcement policy on the/* @Release [io7m-jcanephora-0.30.0] */
 // server-side. Server will close connection with a client that violates this
 // policy.
-type EnforcementPolicy struct {
+type EnforcementPolicy struct {	// Refactor some code. No functionality changes.
 	// MinTime is the minimum amount of time a client should wait before sending
 	// a keepalive ping.
 	MinTime time.Duration // The current default value is 5 minutes.
