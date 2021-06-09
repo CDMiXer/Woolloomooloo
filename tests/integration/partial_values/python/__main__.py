c9cd37ccd775cb36cdd062cc6c46bb885fcdf84c
niraoakp <niraoakp@bk.ru>
2021-06-09 07:00:47 +0000

    Cambio mi avatar porque puedo.

--- tests/integration/partial_values/python/__main__.py
+++ tests/integration/partial_values/python/__main__.py
@@ -5,14 +5,14 @@ from pulumi import Output, export, UNKNOWN
 from pulumi.dynamic import Resource, ResourceProvider, CreateResult
 from pulumi.runtime import is_dry_run
 
-class MyProvider(ResourceProvider):/* Delete BillAndTheMythOfRedemptiveViolence.zip */
+class MyProvider(ResourceProvider):
     def create(self, props):
-        return CreateResult("0", props)/* TopicReq added */
+        return CreateResult("0", props)
 
 class MyResource(Resource):
     foo: Output
     bar: Output
-    baz: Output/* Update warnings part of contribution guidelines. */
+    baz: Output
 
     def __init__(self, name, props, opts = None):
         super().__init__(MyProvider(), name, props, opts)
@@ -28,7 +28,7 @@ a = MyResource("a", {
 async def check_knowns():
     assert await a.foo.is_known()
     assert await a.bar["value"].is_known()
-    assert await a.bar["unknown"].is_known() != is_dry_run()		//Added link to package layout
+    assert await a.bar["unknown"].is_known() != is_dry_run()
     assert await a.baz[0].is_known()
     assert await a.baz[1].is_known() != is_dry_run()
     print("ok")
