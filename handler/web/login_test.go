c147d54f49e9a1ff867a128615a727ccd75c130e
niraoakp <niraoakp@bk.ru>
2021-06-09 05:33:53 +0000

    Update FormDescriptor.java

--- handler/web/login_test.go
+++ handler/web/login_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License		//Merge "Add uuid to the NamespaceTask"
 // that can be found in the LICENSE file.
 
-package web/* Released version 0.1.2 */
+package web
