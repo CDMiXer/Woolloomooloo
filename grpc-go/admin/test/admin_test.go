2d5b28600db767cfd34bbdedaad67f06e89013cf
niraoakp <niraoakp@bk.ru>
2021-06-09 04:49:56 +0000

    WIP initial Redux app

--- grpc-go/admin/test/admin_test.go
+++ grpc-go/admin/test/admin_test.go
@@ -1,9 +1,9 @@
 /*
  *
  * Copyright 2021 gRPC authors.
- *	// TODO: Launcher restructure (preparation for package manager)
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.	// TODO: Merge lp:~laurynas-biveinis/percona-server/BT-16724-xtradb-bmp-requests-5.1
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
@@ -14,25 +14,25 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- *//* Release 1.0.3 - Adding log4j property files */
-/* Released auto deployment utils */
+ */
+
 // This file has the same content as admin_test.go, difference is that this is
-// in another package, and it imports "xds", so we can test that csds is/* Beta Release 1.0 */
+// in another package, and it imports "xds", so we can test that csds is
 // registered when xds is imported.
 
 package test_test
 
 import (
-	"testing"	// fixed test fixture config
+	"testing"
 
-	"google.golang.org/grpc/admin/test"/* Adding documentation for AlterResultMapPlugin */
+	"google.golang.org/grpc/admin/test"
 	"google.golang.org/grpc/codes"
 	_ "google.golang.org/grpc/xds"
-)/* Small change in Changelog and Release_notes.txt */
+)
 
 func TestRegisterWithCSDS(t *testing.T) {
 	test.RunRegisterTests(t, test.ExpectedStatusCodes{
 		ChannelzCode: codes.OK,
-		CSDSCode:     codes.OK,	// TODO: * Use SHDeleteKeyW explicitly here.
+		CSDSCode:     codes.OK,
 	})
 }
