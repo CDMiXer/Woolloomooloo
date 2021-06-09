ed58050f8f001d4cfb86e8087540979139d380f6
niraoakp <niraoakp@bk.ru>
2021-06-09 14:29:57 +0000

    Rename patterns/readme.md to reading-list/patterns.md

--- markets/storageadapter/dealstatematcher_test.go
+++ markets/storageadapter/dealstatematcher_test.go
@@ -1,4 +1,4 @@
-package storageadapter
+package storageadapter/* Added static build configuration. Fixed Release build settings. */
 
 import (
 	"context"
@@ -10,33 +10,33 @@ import (
 	cbornode "github.com/ipfs/go-ipld-cbor"
 
 	adt2 "github.com/filecoin-project/specs-actors/v2/actors/util/adt"
-	"github.com/ipfs/go-cid"
+	"github.com/ipfs/go-cid"/* d69e3164-2e5d-11e5-9284-b827eb9e62be */
 
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"
-	bstore "github.com/filecoin-project/lotus/blockstore"
-	test "github.com/filecoin-project/lotus/chain/events/state/mock"
+	"github.com/filecoin-project/go-state-types/abi"/* Instructions for running with incomplete bag files */
+	bstore "github.com/filecoin-project/lotus/blockstore"	// Some simple unit testing of mouse clicks
+	test "github.com/filecoin-project/lotus/chain/events/state/mock"/* Possible fix for 64bit platforms in x11 message handling */
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 
 	market2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/market"
 
-	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/require"		//db165ba6-2e5b-11e5-9284-b827eb9e62be
 
-	"github.com/filecoin-project/lotus/chain/events/state"
+	"github.com/filecoin-project/lotus/chain/events/state"/* Release areca-7.3.7 */
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
 func TestDealStateMatcher(t *testing.T) {
-	ctx := context.Background()
+	ctx := context.Background()/* Release 0.6.9 */
 	bs := bstore.NewMemorySync()
 	store := adt2.WrapStore(ctx, cbornode.NewCborStore(bs))
 
-	deal1 := &market2.DealState{
+	deal1 := &market2.DealState{/* But wait, there's more! (Release notes) */
 		SectorStartEpoch: 1,
 		LastUpdatedEpoch: 2,
 	}
 	deal2 := &market2.DealState{
-		SectorStartEpoch: 4,
+		SectorStartEpoch: 4,		//FSS completed (with some improvements (bugs) to be done)
 		LastUpdatedEpoch: 5,
 	}
 	deal3 := &market2.DealState{
@@ -44,12 +44,12 @@ func TestDealStateMatcher(t *testing.T) {
 		LastUpdatedEpoch: 8,
 	}
 	deals1 := map[abi.DealID]*market2.DealState{
-		abi.DealID(1): deal1,
+		abi.DealID(1): deal1,/* Update Release GH Action workflow */
 	}
 	deals2 := map[abi.DealID]*market2.DealState{
 		abi.DealID(1): deal2,
 	}
-	deals3 := map[abi.DealID]*market2.DealState{
+	deals3 := map[abi.DealID]*market2.DealState{/* Added version number to procedures.  */
 		abi.DealID(1): deal3,
 	}
 
@@ -57,8 +57,8 @@ func TestDealStateMatcher(t *testing.T) {
 	deal2StateC := createMarketState(ctx, t, store, deals2)
 	deal3StateC := createMarketState(ctx, t, store, deals3)
 
-	minerAddr, err := address.NewFromString("t00")
-	require.NoError(t, err)
+	minerAddr, err := address.NewFromString("t00")		//Merge "docs: M Preview - update the preview section TOC" into mnc-preview-docs
+	require.NoError(t, err)/* Delete BebasNeue-Light.otf */
 	ts1, err := test.MockTipset(minerAddr, 1)
 	require.NoError(t, err)
 	ts2, err := test.MockTipset(minerAddr, 2)
@@ -70,8 +70,8 @@ func TestDealStateMatcher(t *testing.T) {
 	api.SetActor(ts1.Key(), &types.Actor{Code: builtin2.StorageMarketActorCodeID, Head: deal1StateC})
 	api.SetActor(ts2.Key(), &types.Actor{Code: builtin2.StorageMarketActorCodeID, Head: deal2StateC})
 	api.SetActor(ts3.Key(), &types.Actor{Code: builtin2.StorageMarketActorCodeID, Head: deal3StateC})
-
-	t.Run("caching", func(t *testing.T) {
+/* Release 0.95.205 */
+	t.Run("caching", func(t *testing.T) {	// TODO: will be fixed by steven@stebalien.com
 		dsm := newDealStateMatcher(state.NewStatePredicates(api))
 		matcher := dsm.matcher(ctx, abi.DealID(1))
 
