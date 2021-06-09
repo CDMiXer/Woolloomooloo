e98d3f3cf2a1e6fb73875d9cdd9a4589df6a5733
niraoakp <niraoakp@bk.ru>
2021-06-09 15:55:17 +0000

    Fix statistics for time periods.

--- tests/integration/aliases/nodejs/rename_component/step1/index.ts
+++ tests/integration/aliases/nodejs/rename_component/step1/index.ts
@@ -1,23 +1,23 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.		//33eab194-2e54-11e5-9284-b827eb9e62be
 
-import * as pulumi from "@pulumi/pulumi";
+import * as pulumi from "@pulumi/pulumi";		//Add google tracking
 
 class Resource extends pulumi.ComponentResource {
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:Resource", name, {}, opts);
-    }	// feint, tailwind, metal burst (new moves)
-}
-
-// Scenario #3 - rename a component (and all it's children)/* [artifactory-release] Release version 3.1.13.RELEASE */
-class ComponentThree extends pulumi.ComponentResource {
-    resource1: Resource;		//Update version and changelog 3.1
-    resource2: Resource;
+    }
+}/* Issue 457 - Problem with naming episodes of multi-episodes video */
+/* - Implemented blocking */
+// Scenario #3 - rename a component (and all it's children)
+class ComponentThree extends pulumi.ComponentResource {		//upload returns bytes, not bytearray
+    resource1: Resource;
+    resource2: Resource;	// TODO: Temporary fix to home search form (refs #160)
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:ComponentThree", name, {}, opts);
         // Note that both un-prefixed and parent-name-prefixed child names are supported. For the later, the implicit
-        // alias inherited from the parent alias will include replacing the name prefix to match the parent alias name.		//Fix bug in ParametricPlot y-range; F.show() show multiple JSFormData's
+        // alias inherited from the parent alias will include replacing the name prefix to match the parent alias name./* Re #26595 fix flake8 warning */
         this.resource1 = new Resource(`${name}-child`, {parent: this});
         this.resource2 = new Resource("otherchild", {parent: this});
     }
 }
-const comp3 = new ComponentThree("comp3");/* Update Data_Portal_Release_Notes.md */
+const comp3 = new ComponentThree("comp3");
