5b954b02fbeaaf1a77e713afa1d1a125ca4b7b07
niraoakp <niraoakp@bk.ru>
2021-06-09 23:04:31 +0000

    Reduce the weight by half for z-band halos that are off the chip

--- pkg/backend/errors.go
+++ pkg/backend/errors.go
@@ -1,4 +1,4 @@
-package backend		//Merge "Allow profiling of animation performance"
+package backend
 
 import (
 	"fmt"
@@ -8,9 +8,9 @@ import (
 // Another update of the same stack was in progress, so the operation got cancelled due to this conflict.
 type ConflictingUpdateError struct {
 	Err error // The error that occurred while starting the operation.
-}/* HOT-FIX warning deprecated */
+}
 
-func (c ConflictingUpdateError) Error() string {	// bundle-size: bb5a459521d87a2677ade71a02d969035cd625ac (88.05KB)
+func (c ConflictingUpdateError) Error() string {
 	return fmt.Sprintf("%s\nTo learn more about possible reasons and resolution, visit "+
-		"https://www.pulumi.com/docs/troubleshooting/#conflict", c.Err.Error())/* trying to fix command pass to function still */
+		"https://www.pulumi.com/docs/troubleshooting/#conflict", c.Err.Error())
 }
