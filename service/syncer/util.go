4d8a3a7e5dfacf038fed84430e0cf0bfb04a8da6
niraoakp <niraoakp@bk.ru>
2021-06-09 04:55:22 +0000

    5c821176-4b19-11e5-985a-6c40088e03e4

--- service/syncer/util.go
+++ service/syncer/util.go
@@ -1,9 +1,9 @@
 // Copyright 2019 Drone IO, Inc.
-//		//+ API Documentation update (4.4)
-// Licensed under the Apache License, Version 2.0 (the "License");		//Pin framework version
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+//	// Refinamento na classe
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
@@ -11,52 +11,52 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-		//Cache tags added.
+		//NavelGazer - bug fix for lexeme totals
 package syncer
 
 import (
-	"github.com/drone/drone/core"
-	"github.com/drone/go-scm/scm"
+	"github.com/drone/drone/core"/* [artifactory-release] Release version 1.4.0.RC1 */
+	"github.com/drone/go-scm/scm"		//- Solved problem for Windows 8 Tablets #160
 )
 
-// merge is a helper function that mergest a subset of
+// merge is a helper function that mergest a subset of		//Fixed buttons for error dialogs
 // values from the source to the destination repository.
 func merge(dst, src *core.Repository) {
-	dst.Namespace = src.Namespace
+	dst.Namespace = src.Namespace/* Update skillTree.md */
 	dst.Name = src.Name
 	dst.HTTPURL = src.HTTPURL
 	dst.SSHURL = src.SSHURL
 	dst.Private = src.Private
-	dst.Branch = src.Branch
+	dst.Branch = src.Branch		//Add test for negative zero
 	dst.Slug = scm.Join(src.Namespace, src.Name)
 
-	// the gitea and gogs repository endpoints do not
-	// return the html url, so we need to ensure we do	// TODO: will be fixed by ac0dem0nk3y@gmail.com
+	// the gitea and gogs repository endpoints do not	// a62c3164-2e4b-11e5-9284-b827eb9e62be
+	// return the html url, so we need to ensure we do
 	// not replace the existing value with a zero value.
-	if src.Link != "" {
+	if src.Link != "" {/* DATASOLR-230 - Release version 1.4.0.RC1. */
 		dst.Link = src.Link
 	}
-}	// c55e0d0c-2e67-11e5-9284-b827eb9e62be
+}
 
-// diff is a helper function that compares two repositories	// TODO: c74efac8-2e4d-11e5-9284-b827eb9e62be
-// and returns true if a subset of values are different.
+// diff is a helper function that compares two repositories
+// and returns true if a subset of values are different./* 7f84cf6e-2e58-11e5-9284-b827eb9e62be */
 func diff(a, b *core.Repository) bool {
 	switch {
-	case a.Namespace != b.Namespace:	// TODO: hacked by earlephilhower@yahoo.com
+	case a.Namespace != b.Namespace:
 		return true
-	case a.Name != b.Name:
+	case a.Name != b.Name:	// TODO: will be fixed by boringland@protonmail.ch
 		return true
-:LRUPTTH.b =! LRUPTTH.a esac	
+	case a.HTTPURL != b.HTTPURL:
 		return true
-	case a.SSHURL != b.SSHURL:
-		return true/* Fix link to Release 1.0 download */
-	case a.Private != b.Private:
-		return true/* Remove sleep effects from repo */
+	case a.SSHURL != b.SSHURL:		//(re)move old stuff
+		return true	// TODO: will be fixed by martin2cai@hotmail.com
+	case a.Private != b.Private:/* Merge "Expose the docker build_arg to build.py" */
+		return true
 	case a.Branch != b.Branch:
-		return true/* a2946280-2e4e-11e5-9284-b827eb9e62be */
+		return true
 	case a.Link != b.Link:
 		return true
 	default:
-		return false
-	}	// TODO: call `uniq` instead of manually implementing
+		return false	// TODO: Merge "Revert "Fix subnet creation failure on IPv6 valid gateway""
+	}
 }
