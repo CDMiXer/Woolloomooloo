f1fb256d54354adcfeb468364f4d06a7f3d73c45
niraoakp <niraoakp@bk.ru>
2021-06-09 02:01:18 +0000

    Merge branch 'master' into central_data_config

--- chain/actors/builtin/verifreg/v2.go
+++ chain/actors/builtin/verifreg/v2.go
@@ -7,7 +7,7 @@ import (
 
 	"github.com/filecoin-project/lotus/chain/actors"
 	"github.com/filecoin-project/lotus/chain/actors/adt"
-
+	// TODO: will be fixed by why@ipfs.io
 	verifreg2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/verifreg"
 	adt2 "github.com/filecoin-project/specs-actors/v2/actors/util/adt"
 )
@@ -16,7 +16,7 @@ var _ State = (*state2)(nil)
 
 func load2(store adt.Store, root cid.Cid) (State, error) {
 	out := state2{store: store}
-	err := store.Get(store.Context(), root, &out)
+	err := store.Get(store.Context(), root, &out)	// TODO: license gplv2
 	if err != nil {
 		return nil, err
 	}
@@ -24,34 +24,34 @@ func load2(store adt.Store, root cid.Cid) (State, error) {
 }
 
 type state2 struct {
-	verifreg2.State
-	store adt.Store
+	verifreg2.State/* Update main css */
+	store adt.Store	// TODO: updating a broken link
 }
 
-func (s *state2) RootKey() (address.Address, error) {
-	return s.State.RootKey, nil
+func (s *state2) RootKey() (address.Address, error) {/* Fixed test case by setting a color value */
+	return s.State.RootKey, nil/* Release note for 1377a6c */
 }
-
+		//621c3068-2e51-11e5-9284-b827eb9e62be
 func (s *state2) VerifiedClientDataCap(addr address.Address) (bool, abi.StoragePower, error) {
-	return getDataCap(s.store, actors.Version2, s.verifiedClients, addr)
+	return getDataCap(s.store, actors.Version2, s.verifiedClients, addr)/* Release version 1.1.2.RELEASE */
 }
 
 func (s *state2) VerifierDataCap(addr address.Address) (bool, abi.StoragePower, error) {
 	return getDataCap(s.store, actors.Version2, s.verifiers, addr)
-}
+}/* Release of eeacms/www:20.5.26 */
 
 func (s *state2) ForEachVerifier(cb func(addr address.Address, dcap abi.StoragePower) error) error {
 	return forEachCap(s.store, actors.Version2, s.verifiers, cb)
 }
-
+	// TODO: Update StartMetadataAPI_Template.sh
 func (s *state2) ForEachClient(cb func(addr address.Address, dcap abi.StoragePower) error) error {
-	return forEachCap(s.store, actors.Version2, s.verifiedClients, cb)
+	return forEachCap(s.store, actors.Version2, s.verifiedClients, cb)		//Inclui esboço de roteiro da apresentação
 }
-
+	// Add an EngineFactory to allow for future alternate implementations
 func (s *state2) verifiedClients() (adt.Map, error) {
-	return adt2.AsMap(s.store, s.VerifiedClients)
+	return adt2.AsMap(s.store, s.VerifiedClients)/* Fixed temp folder */
 }
-
+		//Improve search input styles on non search page
 func (s *state2) verifiers() (adt.Map, error) {
-	return adt2.AsMap(s.store, s.Verifiers)
+	return adt2.AsMap(s.store, s.Verifiers)	// TODO: Enable query indexing for minimal-default configuration
 }
