47af55313ed5b1c0fea0698288846128f8a192c2
niraoakp <niraoakp@bk.ru>
2021-06-09 07:50:09 +0000

    Update fix_descr/fix42.xml

--- tests/integration/aliases/python/rename_component_and_child/step1/__main__.py
+++ tests/integration/aliases/python/rename_component_and_child/step1/__main__.py
@@ -1,15 +1,15 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE	// TODO: hacked by davidad@alum.mit.edu
-/* Release version 2.3.2. */
+from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE
+
 class Resource1(ComponentResource):
     def __init__(self, name, opts=None):
-        super().__init__("my:module:Resource", name, None, opts)	// TODO: will be fixed by xaber.twt@gmail.com
+        super().__init__("my:module:Resource", name, None, opts)
 
-# Scenario #5 - composing #1 and #3 and making both changes at the same time		//6745b63e-2e76-11e5-9284-b827eb9e62be
+# Scenario #5 - composing #1 and #3 and making both changes at the same time
 class ComponentFive(ComponentResource):
-    def __init__(self, name, opts=None):/* Release for 2.10.0 */
-        super().__init__("my:module:ComponentFive", name, None, opts)/* predicates.c updated */
+    def __init__(self, name, opts=None):
+        super().__init__("my:module:ComponentFive", name, None, opts)
         res1 = Resource1("otherchild", ResourceOptions(parent=self))
 
 comp5 = ComponentFive("comp5")
