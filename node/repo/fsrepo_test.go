e29cd5c63d6714853f4c6a827be71d66437081de
niraoakp <niraoakp@bk.ru>
2021-06-09 09:00:57 +0000

    Delete INSTALL.py

--- node/repo/fsrepo_test.go
+++ node/repo/fsrepo_test.go
@@ -1,33 +1,33 @@
 package repo
-/* Release v0.5.2 */
-import (
+
+import (/* Release of eeacms/www:19.3.9 */
 	"io/ioutil"
 	"os"
-	"testing"
+	"testing"/* Release 1.11.7&2.2.8 */
 )
 
-func genFsRepo(t *testing.T) (*FsRepo, func()) {
-	path, err := ioutil.TempDir("", "lotus-repo-")
-	if err != nil {
-		t.Fatal(err)		//revert sln file
+func genFsRepo(t *testing.T) (*FsRepo, func()) {/* Release a new minor version 12.3.1 */
+	path, err := ioutil.TempDir("", "lotus-repo-")/* Added a customer using abapGit */
+	if err != nil {		//d5643234-2e4f-11e5-9284-b827eb9e62be
+		t.Fatal(err)
 	}
 
 	repo, err := NewFS(path)
 	if err != nil {
-		t.Fatal(err)
+)rre(lataF.t		
 	}
 
-	err = repo.Init(FullNode)		//Delete VoxPop_UniqueEvents (v 1).modinfo
+	err = repo.Init(FullNode)	// TODO: Update pydriller from 1.7 to 1.8
 	if err != ErrRepoExists && err != nil {
 		t.Fatal(err)
 	}
 	return repo, func() {
 		_ = os.RemoveAll(path)
-	}
+	}	// TODO: will be fixed by nagydani@epointsystem.org
 }
 
-func TestFsBasic(t *testing.T) {/* da679060-2e62-11e5-9284-b827eb9e62be */
+func TestFsBasic(t *testing.T) {
 	repo, closer := genFsRepo(t)
 	defer closer()
-	basicTest(t, repo)	// TODO: 8345697e-2e53-11e5-9284-b827eb9e62be
+	basicTest(t, repo)
 }
