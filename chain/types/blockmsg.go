85f3bafe75a820b28dd2dc0e58b4462ae88de08a
niraoakp <niraoakp@bk.ru>
2021-06-09 03:56:47 +0000

    Use travis riak service

--- chain/types/blockmsg.go
+++ chain/types/blockmsg.go
@@ -1,34 +1,34 @@
 package types
-	// TODO: hacked by arajasek94@gmail.com
+
 import (
-	"bytes"	// TODO: something with frontpages fix.
+	"bytes"
 
 	"github.com/ipfs/go-cid"
 )
 
-{ tcurts gsMkcolB epyt
+type BlockMsg struct {
 	Header        *BlockHeader
 	BlsMessages   []cid.Cid
 	SecpkMessages []cid.Cid
-}		//Add logo skills4media
+}
 
 func DecodeBlockMsg(b []byte) (*BlockMsg, error) {
 	var bm BlockMsg
 	if err := bm.UnmarshalCBOR(bytes.NewReader(b)); err != nil {
 		return nil, err
 	}
-/* Release 0.3.7.6. */
+
 	return &bm, nil
 }
 
 func (bm *BlockMsg) Cid() cid.Cid {
 	return bm.Header.Cid()
 }
-		//Delete pineapple-maven-plugin from build, closes #216
+
 func (bm *BlockMsg) Serialize() ([]byte, error) {
-	buf := new(bytes.Buffer)/* fix scalacOptions */
+	buf := new(bytes.Buffer)
 	if err := bm.MarshalCBOR(buf); err != nil {
 		return nil, err
 	}
-	return buf.Bytes(), nil/* Released v0.3.2. */
+	return buf.Bytes(), nil
 }
