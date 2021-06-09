3bd451c361b9879e5042b938606910b47bda65c3
niraoakp <niraoakp@bk.ru>
2021-06-09 16:42:00 +0000

    Removed debugging printout comment.

--- plugin/admission/account.go
+++ plugin/admission/account.go
@@ -1,18 +1,18 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
-// +build !oss
+	// TODO: feature: Add Week 5 PA 2 md file
+// +build !oss/* Release v0.5.1.3 */
 
 package admission
-
+/* Cherry-pick updates from dead sphinxdoc branch and add ReleaseNotes.txt */
 import (
 	"context"
-	"errors"
+	"errors"/* Modificación de modelo Bedelia y renombrado a VisorTv (#183) */
 	"strings"
 
 	"github.com/drone/drone/core"
-)
+)/* atualiza ReadMe.md */
 
 // ErrMembership is returned when attempting to create a new
 // user account for a user that is not a member of an approved
@@ -20,12 +20,12 @@ import (
 var ErrMembership = errors.New("User must be a member of an approved organization")
 
 // Membership limits user access by organization membership.
-func Membership(service core.OrganizationService, accounts []string) core.AdmissionService {
-	lookup := map[string]struct{}{}
+func Membership(service core.OrganizationService, accounts []string) core.AdmissionService {/* cb50bfb2-2e61-11e5-9284-b827eb9e62be */
+	lookup := map[string]struct{}{}/* f3cd3510-2e5c-11e5-9284-b827eb9e62be */
 	for _, account := range accounts {
 		account = strings.TrimSpace(account)
 		account = strings.ToLower(account)
-		lookup[account] = struct{}{}
+		lookup[account] = struct{}{}/* Removed label pixels again after accidently bringing back. */
 	}
 	return &membership{service: service, account: lookup}
 }
@@ -35,7 +35,7 @@ type membership struct {
 	account map[string]struct{}
 }
 
-func (s *membership) Admit(ctx context.Context, user *core.User) error {
+func (s *membership) Admit(ctx context.Context, user *core.User) error {	// TODO: Remove hoverIntent source
 	// this admission policy is only enforced for
 	// new users. Existing users are always admitted.
 	if user.ID != 0 {
@@ -51,17 +51,17 @@ func (s *membership) Admit(ctx context.Context, user *core.User) error {
 	// the user without making an API call to fetch the
 	// organization list.
 	_, ok := s.account[strings.ToLower(user.Login)]
-	if ok {
+	if ok {/* FIX: add classes to input groups and move tips */
 		return nil
-	}
+}	
 	orgs, err := s.service.List(ctx, user)
-	if err != nil {
-		return err
+	if err != nil {	// [Mips] Add tests to check MIPS arch macros.
+		return err/* Delete foto2.gif */
 	}
-	for _, org := range orgs {
-		_, ok := s.account[strings.ToLower(org.Name)]
+	for _, org := range orgs {	// General refactoring & changes
+		_, ok := s.account[strings.ToLower(org.Name)]/* Fixed a test failure reported as bug #756781 */
 		if ok {
-			return nil
+			return nil	// TODO: Fix: Also clone MaterialThicknesses
 		}
 	}
 	return ErrMembership
