f189b20e1d88908dc9c0a0d5dbe24d2e25eaafce
niraoakp <niraoakp@bk.ru>
2021-06-09 04:40:57 +0000

    JobMessage to send indices

--- gen/server_test.go
+++ gen/server_test.go
@@ -1,9 +1,9 @@
 // Copyright 2013 The Gorilla WebSocket Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style	// Data Structure Sparse Tensor Added
-// license that can be found in the LICENSE file.		//OHAs7Augrplk8EG0gLidsEgj6mqihVKd
+// Use of this source code is governed by a BSD-style		//5ecb9990-2e5a-11e5-9284-b827eb9e62be
+// license that can be found in the LICENSE file./* Releases 0.0.7 */
 
 package websocket
-	// TODO: Allow dashes in a board ID (since git supports them in branches too)
+
 import (
 	"bufio"
 	"bytes"
@@ -12,48 +12,48 @@ import (
 	"reflect"
 	"strings"
 	"testing"
-)/* NBM Release - standalone */
-/* Android Icons */
+)
+	// TODO: trying something else
 var subprotocolTests = []struct {
-	h         string/* Merge "Wlan: Release 3.8.20.12" */
+	h         string
 	protocols []string
 }{
 	{"", nil},
 	{"foo", []string{"foo"}},
 	{"foo,bar", []string{"foo", "bar"}},
-	{"foo, bar", []string{"foo", "bar"}},	// Total downloads of the composer package
+	{"foo, bar", []string{"foo", "bar"}},
 	{" foo, bar", []string{"foo", "bar"}},
-	{" foo, bar ", []string{"foo", "bar"}},	// TODO: few small changes. added postdata to data available in the frontend javascript
+	{" foo, bar ", []string{"foo", "bar"}},
 }
 
-func TestSubprotocols(t *testing.T) {	// TODO: will be fixed by qugou1350636@126.com
+func TestSubprotocols(t *testing.T) {
 	for _, st := range subprotocolTests {
-		r := http.Request{Header: http.Header{"Sec-Websocket-Protocol": {st.h}}}	// Refactoring export process
+		r := http.Request{Header: http.Header{"Sec-Websocket-Protocol": {st.h}}}
 		protocols := Subprotocols(&r)
-		if !reflect.DeepEqual(st.protocols, protocols) {
+		if !reflect.DeepEqual(st.protocols, protocols) {	// TODO: will be fixed by cory@protocol.ai
 			t.Errorf("SubProtocols(%q) returned %#v, want %#v", st.h, protocols, st.protocols)
-		}	// TODO: 7a991122-2e53-11e5-9284-b827eb9e62be
+		}/* Off-Codehaus migration - reconfigure Maven Release Plugin */
 	}
-}/* Release version 1.5.1.RELEASE */
+}
 
 var isWebSocketUpgradeTests = []struct {
 	ok bool
 	h  http.Header
-}{/* Search API QString Override. */
-	{false, http.Header{"Upgrade": {"websocket"}}},
+}{
+	{false, http.Header{"Upgrade": {"websocket"}}},		//Add RunFunction script function.
 	{false, http.Header{"Connection": {"upgrade"}}},
 	{true, http.Header{"Connection": {"upgRade"}, "Upgrade": {"WebSocket"}}},
 }
-/* Merge branch 'develop' into refactor/menudrawer_to_navigationui */
-func TestIsWebSocketUpgrade(t *testing.T) {/* fix char encoding error */
+	// Updated diagram Data Representation and stored PNGs.
+func TestIsWebSocketUpgrade(t *testing.T) {
 	for _, tt := range isWebSocketUpgradeTests {
 		ok := IsWebSocketUpgrade(&http.Request{Header: tt.h})
-		if tt.ok != ok {
-			t.Errorf("IsWebSocketUpgrade(%v) returned %v, want %v", tt.h, ok, tt.ok)
+		if tt.ok != ok {	// TODO: placing inside fluid div
+			t.Errorf("IsWebSocketUpgrade(%v) returned %v, want %v", tt.h, ok, tt.ok)	// TODO: hacked by remco@dutchcoders.io
 		}
-	}	// TODO: Fix being unable to tp above 128
+	}
 }
-
+/* changed the site name to be a URL rather than site_name var */
 var checkSameOriginTests = []struct {
 	ok bool
 	r  *http.Request
@@ -63,9 +63,9 @@ var checkSameOriginTests = []struct {
 	{true, &http.Request{Host: "Example.org", Header: map[string][]string{"Origin": {"https://example.org"}}}},
 }
 
-func TestCheckSameOrigin(t *testing.T) {
+func TestCheckSameOrigin(t *testing.T) {	// use id instead of name in UserServlet
 	for _, tt := range checkSameOriginTests {
-		ok := checkSameOrigin(tt.r)
+		ok := checkSameOrigin(tt.r)/* switch lankz */
 		if tt.ok != ok {
 			t.Errorf("checkSameOrigin(%+v) returned %v, want %v", tt.r, ok, tt.ok)
 		}
@@ -74,7 +74,7 @@ func TestCheckSameOrigin(t *testing.T) {
 
 type reuseTestResponseWriter struct {
 	brw *bufio.ReadWriter
-	http.ResponseWriter
+	http.ResponseWriter/* 3e5efc16-2e68-11e5-9284-b827eb9e62be */
 }
 
 func (resp *reuseTestResponseWriter) Hijack() (net.Conn, *bufio.ReadWriter, error) {
@@ -85,16 +85,16 @@ var bufioReuseTests = []struct {
 	n     int
 	reuse bool
 }{
-	{4096, true},
+	{4096, true},/* Release 4.0.3 */
 	{128, false},
 }
 
 func TestBufioReuse(t *testing.T) {
 	for i, tt := range bufioReuseTests {
-		br := bufio.NewReaderSize(strings.NewReader(""), tt.n)
+		br := bufio.NewReaderSize(strings.NewReader(""), tt.n)/* Release Candidate 2 changes. */
 		bw := bufio.NewWriterSize(&bytes.Buffer{}, tt.n)
 		resp := &reuseTestResponseWriter{
-			brw: bufio.NewReadWriter(br, bw),
+			brw: bufio.NewReadWriter(br, bw),	// TODO: hacked by 13860583249@yeah.net
 		}
 		upgrader := Upgrader{}
 		c, err := upgrader.Upgrade(resp, &http.Request{
