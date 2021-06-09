b350e2a6a380a0a22b692d900249210ac76b943a
niraoakp <niraoakp@bk.ru>
2021-06-09 19:09:59 +0000

    also made the test app much MUCH better

--- tests/integration/aliases/dotnet/rename/step1/Program.cs
+++ tests/integration/aliases/dotnet/rename/step1/Program.cs
@@ -1,22 +1,22 @@
 ﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
 
-using System.Threading.Tasks;/* Merge "[FEATURE] sap_belize_hcb for sap.m.P* to S* controls" */
-using Pulumi;
-/* Benutzerdaten ändern: Design */
+using System.Threading.Tasks;
+using Pulumi;/* Delete WideBinaryProject.v3-checkpoint.ipynb */
+
 class Resource : ComponentResource
 {
     public Resource(string name, ComponentResourceOptions options = null)
         : base("my:module:Resource", name, options)
-    {	// TODO: Callback for tracking circuit building progress
+    {	// TODO: ServerTokens for EL7 / httpd 2.4
     }
-}
+}/* Removed unused an unneeded car_page.jsp */
 
 class Program
 {
     static Task<int> Main(string[] args)
     {
-        return Deployment.RunAsync(() => /* #456 adding testing issue to Release Notes. */
-        {/* New footer on layout added */
+        return Deployment.RunAsync(() => 
+        {/* Standardize brackets. */
             // Scenario #1 - rename a resource
             var res1 = new Resource("res1");
         });
