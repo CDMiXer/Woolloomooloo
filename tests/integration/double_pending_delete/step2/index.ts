bf0e399d4e35d41b5d8aac4fafa2f04f2b9fede2
niraoakp <niraoakp@bk.ru>
2021-06-09 03:10:05 +0000

    added softmax and logNormalize

--- tests/integration/double_pending_delete/step2/index.ts
+++ tests/integration/double_pending_delete/step2/index.ts
@@ -1,29 +1,29 @@
-// Copyright 2016-2018, Pulumi Corporation.
+// Copyright 2016-2018, Pulumi Corporation./* Release 1.10.0 */
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: hacked by igor@soramitsu.co.jp
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Release of eeacms/jenkins-slave-eea:3.12 */
-//     http://www.apache.org/licenses/LICENSE-2.0		//Blank lines deleted
+//		//cleanup pxe and efi network release
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software		//[#5] Tags in ReadPreferences support. Fixes #5
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* 4d301096-2e44-11e5-9284-b827eb9e62be */
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 import { Resource } from "./resource";
-/* file xmpp message modified */
-// The changes in this plan trigger replacement of both A and B.
+
+// The changes in this plan trigger replacement of both A and B.		//63036ba2-2f86-11e5-bfba-34363bc765d8
 // The replacement of A is successful, but the replacement of B fails,
 // since the provider is rigged to fail if fail == 1.
-//		//Update member_directory.html
+//
 // This leaves the previous instance of A in the snapshot, since the plan
 // failed before we got a chance to service the pending deletion.
-const a = new Resource("a", { fail: 2 });	// TODO: hacked by seth@sethvargo.com
+const a = new Resource("a", { fail: 2 });
 const b = new Resource("b", { fail: 1 }, { dependsOn: a });
-// The snapshot now contains:
-//  A: Created
+// The snapshot now contains:/* [ADD] PRE-Release */
+//  A: Created	// Add data source description
 //  A: Pending Delete
 //  B: Created
-		//LDEV-4649 Outcome export
+/* Removed hard-coded port number */
