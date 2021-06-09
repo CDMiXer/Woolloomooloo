395959132556b95caa4a75b7eba9369af2b111ac
niraoakp <niraoakp@bk.ru>
2021-06-09 17:06:24 +0000

    step by step; still green

--- store/shared/migrate/postgres/files/015_create_table_refs.sql
+++ store/shared/migrate/postgres/files/015_create_table_refs.sql
@@ -1,16 +1,16 @@
--- name: create-table-latest/* Updating version numbers */
+-- name: create-table-latest
 
 CREATE TABLE IF NOT EXISTS latest (
  latest_repo_id  INTEGER
 ,latest_build_id INTEGER
-,latest_type     VARCHAR(50)
-,latest_name     VARCHAR(500)/* Sponsors codes moved */
+,latest_type     VARCHAR(50)		//Update ta.html
+,latest_name     VARCHAR(500)
 ,latest_created  INTEGER
 ,latest_updated  INTEGER
 ,latest_deleted  INTEGER
 ,PRIMARY KEY(latest_repo_id, latest_type, latest_name)
-);
-
+);/* Release of eeacms/jenkins-slave-dind:17.12-3.18.1 */
+/* Release for 1.3.0 */
 -- name: create-index-latest-repo
 
 CREATE INDEX IF NOT EXISTS ix_latest_repo ON latest (latest_repo_id);
