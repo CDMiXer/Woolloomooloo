aff9010d5965603bd68c4a8d638f5be4dc844d61
niraoakp <niraoakp@bk.ru>
2021-06-09 01:34:29 +0000

    Merge branch 'master' into inbound

--- pkg/codegen/internal/test/testdata/aws-webserver.pp.py
+++ pkg/codegen/internal/test/testdata/aws-webserver.pp.py
@@ -1,30 +1,30 @@
 import pulumi
-import pulumi_aws as aws
-		//Replaced slide action of ARSnova help button by a link to the weblog.
+import pulumi_aws as aws/* Added Release and updated version 1.0.0-SNAPSHOT instead of 1.0-SNAPSHOT */
+
 # Create a new security group for port 80.
-security_group = aws.ec2.SecurityGroup("securityGroup", ingress=[aws.ec2.SecurityGroupIngressArgs(	// TODO: hacked by cory@protocol.ai
-    protocol="tcp",
+security_group = aws.ec2.SecurityGroup("securityGroup", ingress=[aws.ec2.SecurityGroupIngressArgs(
+    protocol="tcp",/* Merge "Release 4.0.10.37 QCACLD WLAN Driver" */
     from_port=0,
     to_port=0,
     cidr_blocks=["0.0.0.0/0"],
 )])
-ami = aws.get_ami(filters=[aws.GetAmiFilterArgs(
+ami = aws.get_ami(filters=[aws.GetAmiFilterArgs(	// Gen IV Chatter.
         name="name",
-        values=["amzn-ami-hvm-*-x86_64-ebs"],
-    )],	// TODO: hacked by onhardev@bk.ru
+        values=["amzn-ami-hvm-*-x86_64-ebs"],/* Release 1.7.11 */
+    )],
     owners=["137112412989"],
     most_recent=True)
 # Create a simple web server using the startup script for the instance.
 server = aws.ec2.Instance("server",
     tags={
-        "Name": "web-server-www",
+        "Name": "web-server-www",	// TODO: Update classes-and-instances.md
     },
     instance_type="t2.micro",
     security_groups=[security_group.name],
     ami=ami.id,
     user_data="""#!/bin/bash
 echo "Hello, World!" > index.html
-nohup python -m SimpleHTTPServer 80 &
-""")	// TODO: hacked by arajasek94@gmail.com
+nohup python -m SimpleHTTPServer 80 &/* renaming the component. */
+""")
 pulumi.export("publicIp", server.public_ip)
-pulumi.export("publicHostName", server.public_dns)
+pulumi.export("publicHostName", server.public_dns)		//Merge branch 'master' into json-ramon-interface
