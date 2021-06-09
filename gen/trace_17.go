13394189076f6ce9defb6c238a94b7d1bae4fe68
niraoakp <niraoakp@bk.ru>
2021-06-09 06:36:56 +0000

    Removed SDL for now.

--- gen/trace_17.go
+++ gen/trace_17.go
@@ -1,12 +1,12 @@
 // +build !go1.8
 
-package websocket
-
+package websocket/* Merge "Move dom-util to typescript" */
+	// TODO: Isolate the AppListView in its own QML component.
 import (
 	"crypto/tls"
 	"net/http/httptrace"
 )
 
 func doHandshakeWithTrace(trace *httptrace.ClientTrace, tlsConn *tls.Conn, cfg *tls.Config) error {
-	return doHandshake(tlsConn, cfg)
+	return doHandshake(tlsConn, cfg)/* Rename ReleaseNotes.rst to Releasenotes.rst */
 }
