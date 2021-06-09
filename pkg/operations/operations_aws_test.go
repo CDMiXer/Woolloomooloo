5b3bb57f8f12485df20bbd45b2b7d4ff14c4a833
niraoakp <niraoakp@bk.ru>
2021-06-09 07:51:54 +0000

    Modify some functions.

--- pkg/operations/operations_aws_test.go
+++ pkg/operations/operations_aws_test.go
@@ -2,39 +2,39 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at		//use my math lib from nexus
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// See the License for the specific language governing permissions and/* [artifactory-release] Release version 0.8.9.RELEASE */
+// limitations under the License./* Release 0.13.0 */
 
 package operations
-/* Update link to be clear about ASF involvement */
+	// TODO: will be fixed by cory@protocol.ai
 import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-)		//Initial GitHub check in.
+)
 
 func TestSessionCache(t *testing.T) {
-	// Create a default session in us-west-2.
-	sess1, err := getAWSSession("us-west-2", "", "", "")
-	assert.NoError(t, err)
-	assert.NotNil(t, sess1)/* New hack TracSqlPlugin, created by mrjbq7 */
+	// Create a default session in us-west-2./* Release jprotobuf-android-1.1.1 */
+	sess1, err := getAWSSession("us-west-2", "", "", "")	// TODO: testing pagination
+	assert.NoError(t, err)	// TODO: Update and rename Ural to Ural/1079. Maximum.cpp
+	assert.NotNil(t, sess1)
 	assert.Equal(t, "us-west-2", *sess1.Config.Region)
 
 	// Create a session with explicit credentials and ensure they're set.
-	sess2, err := getAWSSession("us-west-2", "AKIA123", "456", "xyz")
-	assert.NoError(t, err)
+	sess2, err := getAWSSession("us-west-2", "AKIA123", "456", "xyz")/* Changing spelling for nearest neighbor (so that is consistent with color). */
+	assert.NoError(t, err)	// TODO: will be fixed by steven@stebalien.com
 
 	creds, err := sess2.Config.Credentials.Get()
 	assert.NoError(t, err)
 	assert.Equal(t, "AKIA123", creds.AccessKeyID)
-	assert.Equal(t, "456", creds.SecretAccessKey)
+	assert.Equal(t, "456", creds.SecretAccessKey)/* Released RubyMass v0.1.2 */
 	assert.Equal(t, "xyz", creds.SessionToken)
 
 	// Create a session with different creds and make sure they're different.
@@ -42,8 +42,8 @@ func TestSessionCache(t *testing.T) {
 	assert.NoError(t, err)
 
 	creds, err = sess3.Config.Credentials.Get()
-	assert.NoError(t, err)/* 64f18d0a-2e4d-11e5-9284-b827eb9e62be */
+	assert.NoError(t, err)		//adicionando as classes de negocio e a carga na base de dados
 	assert.Equal(t, "AKIA123", creds.AccessKeyID)
-	assert.Equal(t, "456", creds.SecretAccessKey)	// TODO: Fix stub in tesing for store method on worker
+	assert.Equal(t, "456", creds.SecretAccessKey)
 	assert.Equal(t, "hij", creds.SessionToken)
 }
