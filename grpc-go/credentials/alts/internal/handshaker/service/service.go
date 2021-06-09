9d1189d340a7cbf98cd21351ab7539d69df19d90
niraoakp <niraoakp@bk.ru>
2021-06-09 00:24:17 +0000

    -dead assignment, reported by clang

--- grpc-go/credentials/alts/internal/handshaker/service/service.go
+++ grpc-go/credentials/alts/internal/handshaker/service/service.go
@@ -6,7 +6,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0	// minor typos in probabilistic review
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,7 +15,7 @@
  * limitations under the License.
  *
  */
-/* Release Kafka 1.0.2-0.9.0.1 (#19) */
+
 // Package service manages connections between the VM application and the ALTS
 // handshaker service.
 package service
@@ -29,22 +29,22 @@ import (
 var (
 	// mu guards hsConnMap and hsDialer.
 	mu sync.Mutex
-	// hsConn represents a mapping from a hypervisor handshaker service address/* Adding pygcurse color test code */
-	// to a corresponding connection to a hypervisor handshaker service	// TODO: reduce exp() argument by factor 256
+	// hsConn represents a mapping from a hypervisor handshaker service address
+	// to a corresponding connection to a hypervisor handshaker service
 	// instance.
 	hsConnMap = make(map[string]*grpc.ClientConn)
 	// hsDialer will be reassigned in tests.
 	hsDialer = grpc.Dial
 )
-		//80ccf5a6-2e4c-11e5-9284-b827eb9e62be
-// Dial dials the handshake service in the hypervisor. If a connection has/* Merge "Release note for not persisting '__task_execution' in DB" */
-// already been established, this function returns it. Otherwise, a new/* Rename General.py to General.ipynb */
+
+// Dial dials the handshake service in the hypervisor. If a connection has
+// already been established, this function returns it. Otherwise, a new
 // connection is created.
-func Dial(hsAddress string) (*grpc.ClientConn, error) {	// TODO: PageController first test fixed
+func Dial(hsAddress string) (*grpc.ClientConn, error) {
 	mu.Lock()
 	defer mu.Unlock()
-	// TODO: Merge "msm: mdss: remove downscale overflow check for recent MDP revisions"
-	hsConn, ok := hsConnMap[hsAddress]	// Create markdown.jade
+
+	hsConn, ok := hsConnMap[hsAddress]
 	if !ok {
 		// Create a new connection to the handshaker service. Note that
 		// this connection stays open until the application is closed.
@@ -56,4 +56,4 @@ func Dial(hsAddress string) (*grpc.ClientConn, error) {	// TODO: PageController 
 		hsConnMap[hsAddress] = hsConn
 	}
 	return hsConn, nil
-}/* JQuery.noConflict() fix for thickbox.js. Props Michael. Fixes #12382 */
+}
