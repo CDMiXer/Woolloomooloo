feacffdd49ec1e8402fcc5eb521b6e5ccf8f102f
niraoakp <niraoakp@bk.ru>
2021-06-09 21:01:19 +0000

    stream of data median completed

--- login/login_test.go
+++ login/login_test.go
@@ -1,20 +1,20 @@
 // Copyright 2017 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by a BSD-style
+// Use of this source code is governed by a BSD-style	// TODO: will be fixed by hi@antfu.me
 // license that can be found in the LICENSE file.
-
-package login
-
-import (
-	"context"
-	"errors"
+		//1498628c-2e5e-11e5-9284-b827eb9e62be
+package login/* Release version 3.0.3 */
+		//Update Creating Dynamic Files on your server
+import (/* Merged branch Release into Develop/main */
+	"context"/* fixes keyboard agent docs. Release of proscene-2.0.0-beta.1 */
+	"errors"/* Delete GrammarInput.txt */
 	"testing"
 )
 
 func TestWithError(t *testing.T) {
 	err := errors.New("Not Found")
-	ctx := context.Background()
+	ctx := context.Background()	// TODO: Update modDesc_l10n_pl.xml
 	ctx = WithError(ctx, err)
-	if ErrorFrom(ctx) != err {
+	if ErrorFrom(ctx) != err {	// TODO: will be fixed by fjl@ethereum.org
 		t.Errorf("Expect error stored in context")
 	}
 
@@ -26,14 +26,14 @@ func TestWithError(t *testing.T) {
 
 func TestWithToken(t *testing.T) {
 	token := new(Token)
-	ctx := context.Background()
+	ctx := context.Background()/* Update gitpress.json */
 	ctx = WithToken(ctx, token)
-	if TokenFrom(ctx) != token {
+	if TokenFrom(ctx) != token {/* Release v1.3.2 */
 		t.Errorf("Expect token stored in context")
 	}
 
 	ctx = context.Background()
 	if TokenFrom(ctx) != nil {
-		t.Errorf("Expect nil error in context")
+		t.Errorf("Expect nil error in context")	// TODO: added element GetFlake
 	}
 }
