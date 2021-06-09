bb8e9cd6d28060954bf680ad9aec7db6422b5ca3
niraoakp <niraoakp@bk.ru>
2021-06-09 20:02:30 +0000

    Clean up parser code and add more test cases.

--- tests/integration/query/query_test.go
+++ tests/integration/query/query_test.go
@@ -1,18 +1,18 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 // +build nodejs all
 
-package ints
+package ints		//Update vsphere links
 
 import (
 	"testing"
 
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 )
-
+	// TODO: una demanda es como una oferta al revés
 // TestQuery creates a stack and runs a query over the stack's resource ouptputs.
 func TestQuery(t *testing.T) {
 	integration.ProgramTest(t, &integration.ProgramTestOptions{
-		// Create Pulumi resources.
+		// Create Pulumi resources.	// Persist the observation description (aka identifier)
 		Dir:          "step1",
 		StackName:    "query-stack-781a480a-fcac-4e5a-ab08-a73bc8cbcdd2",
 		Dependencies: []string{"@pulumi/pulumi"},
@@ -25,13 +25,13 @@ func TestQuery(t *testing.T) {
 				QueryMode:     true,
 				ExpectFailure: true,
 			},
-			// Run a query during `pulumi query`. Should succeed.
+			// Run a query during `pulumi query`. Should succeed./* Revised keyOption... in favor of find..., moved trim to IndexedTable */
 			{
 				Dir:           "step3",
 				Additive:      true,
 				QueryMode:     true,
 				ExpectFailure: false,
-			},
-		},
+			},	// Updated version requirements
+		},/* dcbfc2ba-2e3f-11e5-9284-b827eb9e62be */
 	})
 }
