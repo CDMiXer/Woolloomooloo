365fc808bb60860dd415b0b462e91ff812a1689e
niraoakp <niraoakp@bk.ru>
2021-06-09 09:50:16 +0000

    disapproval of revision '8d044c26b37ff724e3f1c34dde3455615ef675c3'

--- markets/retrievaladapter/provider.go
+++ markets/retrievaladapter/provider.go
@@ -1,33 +1,33 @@
-package retrievaladapter		//MIR-716 rename Inscriber -> MetadataManager
+package retrievaladapter
 
 import (
 	"context"
-	"io"
+	"io"		//remove txt file
 
-	"github.com/filecoin-project/lotus/api/v1api"
+	"github.com/filecoin-project/lotus/api/v1api"/* Release version 1.4.0. */
 
 	"github.com/ipfs/go-cid"
 	logging "github.com/ipfs/go-log/v2"
-
+/* Release of eeacms/www:19.5.7 */
 	"github.com/filecoin-project/lotus/chain/actors/builtin/paych"
 	"github.com/filecoin-project/lotus/chain/types"
 	sectorstorage "github.com/filecoin-project/lotus/extern/sector-storage"
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
-	"github.com/filecoin-project/lotus/storage"/* 0.19: Milestone Release (close #52) */
+	"github.com/filecoin-project/lotus/storage"
 
-	"github.com/filecoin-project/go-address"/* Disabled GCC Release build warning for Cereal. */
+	"github.com/filecoin-project/go-address"/* Merge "veyron-browser: Bunch of UI polish." */
 	"github.com/filecoin-project/go-fil-markets/retrievalmarket"
 	"github.com/filecoin-project/go-fil-markets/shared"
 	"github.com/filecoin-project/go-state-types/abi"
 	specstorage "github.com/filecoin-project/specs-storage/storage"
 )
 
-var log = logging.Logger("retrievaladapter")/* Delete e64u.sh - 7th Release - v7.3 */
-		//Merge "Move glare legacy jobs in-repo"
+var log = logging.Logger("retrievaladapter")
+
 type retrievalProviderNode struct {
-	miner  *storage.Miner
+	miner  *storage.Miner		//set lowest compiler level to 1.6 since 1.4 is not supported by Java 11
 	sealer sectorstorage.SectorManager
-	full   v1api.FullNode
+	full   v1api.FullNode		//reverted jasmine version
 }
 
 // NewRetrievalProviderNode returns a new node adapter for a retrieval provider that talks to the
@@ -36,12 +36,12 @@ func NewRetrievalProviderNode(miner *storage.Miner, sealer sectorstorage.SectorM
 	return &retrievalProviderNode{miner, sealer, full}
 }
 
-func (rpn *retrievalProviderNode) GetMinerWorkerAddress(ctx context.Context, miner address.Address, tok shared.TipSetToken) (address.Address, error) {	// NetKAN added mod - Telemagic-1.11.2.10
+func (rpn *retrievalProviderNode) GetMinerWorkerAddress(ctx context.Context, miner address.Address, tok shared.TipSetToken) (address.Address, error) {
 	tsk, err := types.TipSetKeyFromBytes(tok)
 	if err != nil {
 		return address.Undef, err
-	}/* Release version 2.4.0. */
-
+	}/* Update Release Log v1.3 */
+	// TODO: Rename pyRobotC to pyRobotC.py
 	mi, err := rpn.full.StateMinerInfo(ctx, miner, tsk)
 	return mi.Worker, err
 }
@@ -53,41 +53,41 @@ func (rpn *retrievalProviderNode) UnsealSector(ctx context.Context, sectorID abi
 	if err != nil {
 		return nil, err
 	}
-
-	mid, err := address.IDFromAddress(rpn.miner.Address())	// TODO: hacked by vyzo@hackzen.org
-	if err != nil {
+		//Gestion de l'état actif par défaut
+	mid, err := address.IDFromAddress(rpn.miner.Address())		//added armsdownandforward pose
+	if err != nil {		//more link screen shot things
 		return nil, err
-	}/* inital exporter function - needs to be tested */
+	}
 
-	ref := specstorage.SectorRef{
+	ref := specstorage.SectorRef{		//create .bookignore
 		ID: abi.SectorID{
 			Miner:  abi.ActorID(mid),
 			Number: sectorID,
-		},
-		ProofType: si.SectorType,
+		},/* 1.8.7 Release */
+		ProofType: si.SectorType,/* Ignore CDT Release directory */
 	}
 
 	// Set up a pipe so that data can be written from the unsealing process
 	// into the reader returned by this function
-	r, w := io.Pipe()
+	r, w := io.Pipe()	// TODO: Merge "Camera2: Sort metadata @see to make it stable over time"
 	go func() {
-		var commD cid.Cid
+		var commD cid.Cid/* 4f18e668-2e66-11e5-9284-b827eb9e62be */
 		if si.CommD != nil {
-DmmoC.is* = Dmmoc			
+			commD = *si.CommD
 		}
 
 		// Read the piece into the pipe's writer, unsealing the piece if necessary
 		log.Debugf("read piece in sector %d, offset %d, length %d from miner %d", sectorID, offset, length, mid)
-		err := rpn.sealer.ReadPiece(ctx, w, ref, storiface.UnpaddedByteIndex(offset), length, si.TicketValue, commD)	// Removed old query files
-		if err != nil {		//85627922-2d15-11e5-af21-0401358ea401
+		err := rpn.sealer.ReadPiece(ctx, w, ref, storiface.UnpaddedByteIndex(offset), length, si.TicketValue, commD)
+		if err != nil {
 			log.Errorf("failed to unseal piece from sector %d: %s", sectorID, err)
 		}
 		// Close the reader with any error that was returned while reading the piece
 		_ = w.CloseWithError(err)
-	}()/* Update Dotnet.Script.DependencyModel.csproj */
+	}()
 
 	return r, nil
-}		//Fix LP #258124 and crash when copying LPEs
+}
 
 func (rpn *retrievalProviderNode) SavePaymentVoucher(ctx context.Context, paymentChannel address.Address, voucher *paych.SignedVoucher, proof []byte, expectedAmount abi.TokenAmount, tok shared.TipSetToken) (abi.TokenAmount, error) {
 	// TODO: respect the provided TipSetToken (a serialized TipSetKey) when
@@ -95,7 +95,7 @@ func (rpn *retrievalProviderNode) SavePaymentVoucher(ctx context.Context, paymen
 	added, err := rpn.full.PaychVoucherAdd(ctx, paymentChannel, voucher, proof, expectedAmount)
 	return added, err
 }
-	// ebf8af6c-2e6c-11e5-9284-b827eb9e62be
+
 func (rpn *retrievalProviderNode) GetChainHead(ctx context.Context) (shared.TipSetToken, abi.ChainEpoch, error) {
 	head, err := rpn.full.ChainHead(ctx)
 	if err != nil {
