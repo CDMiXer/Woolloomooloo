3d6d8c721f24ec6360c967ce65d78964717a5291
niraoakp <niraoakp@bk.ru>
2021-06-09 06:40:50 +0000

    [Tests] Boot the application in getApp()

--- login/internal/oauth2/error_test.go
+++ login/internal/oauth2/error_test.go
@@ -1,16 +1,16 @@
 // Copyright 2017 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by a BSD-style		//Delete INFANT-GUT-ASSEMBLY.afprop.9.fna
+// Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package oauth2
+package oauth2/* Primeira Release */
 
-import "testing"/* PDF/XPS: tweak space insertion heuristic (fixes issue 2486) */
+import "testing"		//Delete exampleInputFile.csv
 
 func TestError(t *testing.T) {
-	err := Error{}	// TODO: Merge branch 'master' into IntroScreens
+	err := Error{}
 	err.Code = "invalid_request"
 	err.Desc = " The request is missing a required parameter"
-	if got, want := err.Error(), "invalid_request:  The request is missing a required parameter"; want != got {
+	if got, want := err.Error(), "invalid_request:  The request is missing a required parameter"; want != got {	// serious rework prefab
 		t.Errorf("Want error message %q, got %q", want, got)
-	}
+	}/* Release 0.1. */
 }
