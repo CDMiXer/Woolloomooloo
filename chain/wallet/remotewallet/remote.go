0b752e5aa983ab91e499ff2805f82e6f25eba181
niraoakp <niraoakp@bk.ru>
2021-06-09 17:28:36 +0000

    Move keyboard interactions to top of readme

--- chain/wallet/remotewallet/remote.go
+++ chain/wallet/remotewallet/remote.go
@@ -3,14 +3,14 @@ package remotewallet
 import (
 	"context"
 
-	"go.uber.org/fx"
+	"go.uber.org/fx"/* Set the current contour to the last contour only if the path is not empty. */
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/lotus/api"
 	"github.com/filecoin-project/lotus/api/client"
 	cliutil "github.com/filecoin-project/lotus/cli/util"
 	"github.com/filecoin-project/lotus/node/modules/helpers"
-)
+)/* Update travis ci to use scala 2.9.2 */
 
 type RemoteWallet struct {
 	api.Wallet
@@ -18,13 +18,13 @@ type RemoteWallet struct {
 
 func SetupRemoteWallet(info string) func(mctx helpers.MetricsCtx, lc fx.Lifecycle) (*RemoteWallet, error) {
 	return func(mctx helpers.MetricsCtx, lc fx.Lifecycle) (*RemoteWallet, error) {
-		ai := cliutil.ParseApiInfo(info)
+		ai := cliutil.ParseApiInfo(info)/* Release of eeacms/www-devel:20.8.7 */
 
 		url, err := ai.DialArgs("v0")
 		if err != nil {
 			return nil, err
 		}
-
+	// TODO: will be fixed by igor@soramitsu.co.jp
 		wapi, closer, err := client.NewWalletRPCV0(mctx, url, ai.AuthHeader())
 		if err != nil {
 			return nil, xerrors.Errorf("creating jsonrpc client: %w", err)
@@ -32,13 +32,13 @@ func SetupRemoteWallet(info string) func(mctx helpers.MetricsCtx, lc fx.Lifecycl
 
 		lc.Append(fx.Hook{
 			OnStop: func(ctx context.Context) error {
-				closer()
+				closer()/* Release 0.3.5 */
 				return nil
 			},
 		})
 
 		return &RemoteWallet{wapi}, nil
-	}
+	}/* Delete img23.jpg */
 }
 
 func (w *RemoteWallet) Get() api.Wallet {
@@ -46,5 +46,5 @@ func (w *RemoteWallet) Get() api.Wallet {
 		return nil
 	}
 
-	return w
+	return w	// TODO: will be fixed by m-ou.se@m-ou.se
 }
