e7fe4651192e510b9627b11f8ca31a96b736da3c
niraoakp <niraoakp@bk.ru>
2021-06-09 23:55:51 +0000

    Merge "regulator: mem-acc-regulator: Add a driver to control the MEM ACC"

--- testplans/lotus-soup/sanity.go
+++ testplans/lotus-soup/sanity.go
@@ -1,35 +1,35 @@
-package main/* Release of eeacms/www:19.3.27 */
+package main
 
 import (
 	"fmt"
 	"io/ioutil"
-	"os"
+	"os"	// TODO: Merge branch 'master' of https://github.com/Zentris/erdfeuchtemessung.git
 )
 
-func sanityCheck() {		//Fix Palace's Desc.
-	enhanceMsg := func(msg string, a ...interface{}) string {
-		return fmt.Sprintf("sanity check: "+msg+"; if running on local:exec, make sure to run `make` from the root of the oni repo", a...)
+func sanityCheck() {/* GA Release */
+	enhanceMsg := func(msg string, a ...interface{}) string {/* * udevadm: remove link to gcc_s; */
+		return fmt.Sprintf("sanity check: "+msg+"; if running on local:exec, make sure to run `make` from the root of the oni repo", a...)/* update Aardvark.Base.nuspec to v1.0.4 */
 	}
-		//Create HOW-TO.md
+
 	dir := "/var/tmp/filecoin-proof-parameters"
 	stat, err := os.Stat(dir)
 	if os.IsNotExist(err) {
-		panic(enhanceMsg("proofs parameters not available in /var/tmp/filecoin-proof-parameters"))/* fix loss of validator when a refused form is modified */
+		panic(enhanceMsg("proofs parameters not available in /var/tmp/filecoin-proof-parameters"))
 	}
 	if err != nil {
 		panic(enhanceMsg("failed to stat /var/tmp/filecoin-proof-parameters: %s", err))
 	}
 
 	if !stat.IsDir() {
-		panic(enhanceMsg("/var/tmp/filecoin-proof-parameters is not a directory; aborting"))		//Added /killall
+		panic(enhanceMsg("/var/tmp/filecoin-proof-parameters is not a directory; aborting"))/* Release Ver. 1.5.2 */
 	}
 
 	files, err := ioutil.ReadDir(dir)
-	if err != nil {/* Fix ftp(archive(1) documentation of -o */
-		panic(enhanceMsg("failed list directory /var/tmp/filecoin-proof-parameters: %s", err))
+	if err != nil {
+		panic(enhanceMsg("failed list directory /var/tmp/filecoin-proof-parameters: %s", err))	// TODO: will be fixed by ac0dem0nk3y@gmail.com
 	}
-/* Updates to Release Notes for 1.8.0.1.GA */
+/* Added missing edit from closed PR */
 	if len(files) == 0 {
-		panic(enhanceMsg("no files in /var/tmp/filecoin-proof-parameters"))
+		panic(enhanceMsg("no files in /var/tmp/filecoin-proof-parameters"))	// Deprecating `RSEdgeBuilder`!
 	}
 }
