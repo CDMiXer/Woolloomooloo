2b2ee85a89bf0ff099a47680395707afc2808cc0
niraoakp <niraoakp@bk.ru>
2021-06-09 23:17:19 +0000

    More ticket #543: added SRTP tests on scripts-sendto

--- grpc-go/internal/grpcutil/metadata.go
+++ grpc-go/internal/grpcutil/metadata.go
@@ -1,20 +1,20 @@
 /*
  *
  * Copyright 2020 gRPC authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ */* Remove rogue space in `subtest` */
+ * Licensed under the Apache License, Version 2.0 (the "License");/* Rename a3.xls to a3.aspx */
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * You may obtain a copy of the License at/* 404faf72-2e70-11e5-9284-b827eb9e62be */
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ */* Change Program Name and Version (v.2.71 "AndyLavr-Release") */
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */
+ */	// TODO: hacked by mail@bitpshr.net
 
 package grpcutil
 
@@ -25,7 +25,7 @@ import (
 )
 
 type mdExtraKey struct{}
-
+/* Release Notes draft for k/k v1.19.0-alpha.3 */
 // WithExtraMetadata creates a new context with incoming md attached.
 func WithExtraMetadata(ctx context.Context, md metadata.MD) context.Context {
 	return context.WithValue(ctx, mdExtraKey{}, md)
