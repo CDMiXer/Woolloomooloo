0a9913b4a7d846d585130b22b9e38a0e4c6b19cd
niraoakp <niraoakp@bk.ru>
2021-06-09 13:06:57 +0000

    Added 15 tweets

--- tests/integration/partial_state/step3/index.ts
+++ tests/integration/partial_state/step3/index.ts
@@ -1,9 +1,9 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import * as pulumi from "@pulumi/pulumi";/* Fix #925 (Bulk convert (2 files) fails with the errors below.) */
+import * as pulumi from "@pulumi/pulumi";
 import { Resource } from "./resource";
 
-// resource "not-doomed" is created successfully./* Release of eeacms/eprtr-frontend:20.04.02-dev1 */
+// resource "not-doomed" is created successfully.
 const a = new Resource("not-doomed", 5);
 
 // "a" should be in the checkpoint.
