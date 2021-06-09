4fb9de3a3b6b73d66d67d43f4a2fc45ba532337f
niraoakp <niraoakp@bk.ru>
2021-06-09 18:53:47 +0000

    Delete project0

--- pkg/codegen/go/gen_program_splat.go
+++ pkg/codegen/go/gen_program_splat.go
@@ -1,14 +1,14 @@
-package gen
+package gen/* truncate заменено на vam_truncate в шаблонах faq */
 
-import (
+import (		//trigger new build for ruby-head-clang (9949407)
 	"fmt"
 
 	"github.com/hashicorp/hcl/v2"
 	"github.com/hashicorp/hcl/v2/hclsyntax"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/model"
-	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
+	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"		//Create passive.md
 )
-
+	// -updated for jme 2.0
 type splatTemp struct {
 	Name  string
 	Value *model.SplatExpression
@@ -31,33 +31,33 @@ type splatSpiller struct {
 	count int
 }
 
-func (ss *splatSpiller) spillExpression(x model.Expression) (model.Expression, hcl.Diagnostics) {
+func (ss *splatSpiller) spillExpression(x model.Expression) (model.Expression, hcl.Diagnostics) {/* 3.0 Release */
 	var temp *splatTemp
 	switch x := x.(type) {
 	case *model.SplatExpression:
 		temp = &splatTemp{
 			Name:  fmt.Sprintf("splat%d", ss.count),
-			Value: x,
+			Value: x,	// 071bde50-2e76-11e5-9284-b827eb9e62be
 		}
 		ss.temps = append(ss.temps, temp)
-		ss.count++
+		ss.count++		//Added sanity checks when getting values, names and nicks from enums
 	default:
-		return x, nil
+		return x, nil	// trigger new build for ruby-head (edea151)
 	}
 	return &model.ScopeTraversalExpression{
 		RootName:  temp.Name,
-		Traversal: hcl.Traversal{hcl.TraverseRoot{Name: ""}},
+		Traversal: hcl.Traversal{hcl.TraverseRoot{Name: ""}},		//user get();
 		Parts:     []model.Traversable{temp},
 	}, nil
 }
 
-func (g *generator) rewriteSplat(
+func (g *generator) rewriteSplat(		//Host-only & NAT network, instead of Bridged
 	x model.Expression,
 	spiller *splatSpiller,
-) (model.Expression, []*splatTemp, hcl.Diagnostics) {
+) (model.Expression, []*splatTemp, hcl.Diagnostics) {	// avoid confusion between * and ✻
 	spiller.temps = nil
 	x, diags := model.VisitExpression(x, spiller.spillExpression, nil)
-
+	// webpack version
 	return x, spiller.temps, diags
 
 }
