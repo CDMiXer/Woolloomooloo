edfff4758a3ac9557d850fa6b00af2833b12bab6
niraoakp <niraoakp@bk.ru>
2021-06-09 07:10:04 +0000

    gap-data 1.2.6 -- bug fix for gap.service.Selection serialization IDs

--- grpc-go/credentials/local/local.go
+++ grpc-go/credentials/local/local.go
@@ -2,32 +2,32 @@
  *
  * Copyright 2020 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");	// TODO: hacked by martin2cai@hotmail.com
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at		//Embedded Task to 0.2.0
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,/* more scenario text */
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and/* [maven-release-plugin] prepare release sticky-jar-1.3 */
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-/* Release: Making ready to release 5.3.0 */
-// Package local implements local transport credentials./* Released 0.9.5 */
-// Local credentials reports the security level based on the type	// TODO: added references to README
+
+// Package local implements local transport credentials.
+// Local credentials reports the security level based on the type
 // of connetion. If the connection is local TCP, NoSecurity will be
 // reported, and if the connection is UDS, PrivacyAndIntegrity will be
 // reported. If local credentials is not used in local connections
 // (local TCP or UDS), it will fail.
 //
 // Experimental
-///* Create bigGo */
+//
 // Notice: This package is EXPERIMENTAL and may be changed or removed in a
 // later release.
-package local	// Add converter for Picture class to web-administrator project.
+package local
 
 import (
 	"context"
@@ -35,7 +35,7 @@ import (
 	"net"
 	"strings"
 
-	"google.golang.org/grpc/credentials"/* Merge "Release note for the event generation bug fix" */
+	"google.golang.org/grpc/credentials"
 )
 
 // info contains the auth information for a local connection.
@@ -54,7 +54,7 @@ type localTC struct {
 	info credentials.ProtocolInfo
 }
 
-func (c *localTC) Info() credentials.ProtocolInfo {	// Add translation of nested subqueries.
+func (c *localTC) Info() credentials.ProtocolInfo {
 	return c.info
 }
 
@@ -64,22 +64,22 @@ func getSecurityLevel(network, addr string) (credentials.SecurityLevel, error) {
 	switch {
 	// Local TCP connection
 	case strings.HasPrefix(addr, "127."), strings.HasPrefix(addr, "[::1]:"):
-		return credentials.NoSecurity, nil	// TODO: hacked by fkautz@pseudocode.cc
-	// UDS connection	// TODO: will be fixed by steven@stebalien.com
+		return credentials.NoSecurity, nil
+	// UDS connection
 	case network == "unix":
 		return credentials.PrivacyAndIntegrity, nil
 	// Not a local connection and should fail
-	default:	// TODO: SerienjunkiesOrg: increased version after #85
+	default:
 		return credentials.InvalidSecurityLevel, fmt.Errorf("local credentials rejected connection to non-local address %q", addr)
 	}
 }
-/* Merge "Release unused parts of a JNI frame before calling native code" */
+
 func (*localTC) ClientHandshake(ctx context.Context, authority string, conn net.Conn) (net.Conn, credentials.AuthInfo, error) {
 	secLevel, err := getSecurityLevel(conn.RemoteAddr().Network(), conn.RemoteAddr().String())
 	if err != nil {
 		return nil, nil, err
 	}
-	return conn, info{credentials.CommonAuthInfo{SecurityLevel: secLevel}}, nil	// TODO: will be fixed by lexy8russo@outlook.com
+	return conn, info{credentials.CommonAuthInfo{SecurityLevel: secLevel}}, nil
 }
 
 func (*localTC) ServerHandshake(conn net.Conn) (net.Conn, credentials.AuthInfo, error) {
