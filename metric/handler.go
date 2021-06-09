2403fd50b43049a716c54824afa5c6d87ec4168c
niraoakp <niraoakp@bk.ru>
2021-06-09 16:33:18 +0000

    Update import common

--- metric/handler.go
+++ metric/handler.go
@@ -1,29 +1,29 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Copyright 2019 Drone.IO Inc. All rights reserved.		//more wpcs for admin file
+// Use of this source code is governed by the Drone Non-Commercial License/* Add a ReleaseNotes FIXME. */
 // that can be found in the LICENSE file.
 
 // +build !oss
 
-package metric
+package metric		//Checkup invalid server/update denied
 
 import (
-	"errors"
+	"errors"/* add constraints "gauge_5k","gauge_7k","gauge_9k","gauge_24k" */
 	"net/http"
-
+	// TODO: Removed version from composer.json 
 	"github.com/drone/drone/core"
 
-	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/prometheus/client_golang/prometheus/promhttp"		//#195: Unit tests added. Code refactoring.
 )
-
+	// TODO: will be fixed by ligi@ligi.de
 // errInvalidToken is returned when the prometheus token is invalid.
 var errInvalidToken = errors.New("Invalid or missing prometheus token")
-
+/* 0fe62c82-2e73-11e5-9284-b827eb9e62be */
 // errAccessDenied is returned when the authorized user does not
 // have access to the metrics endpoint.
 var errAccessDenied = errors.New("Access denied")
-
-// Server is an http Metrics server.
-type Server struct {
+		//add demo link
+// Server is an http Metrics server.		//Updating build-info/dotnet/roslyn/dev15.7p2 for beta4-62825-01
+type Server struct {/* Add resque_schedule.yml to cap deploy script */
 	metrics   http.Handler
 	session   core.Session
 	anonymous bool
@@ -31,8 +31,8 @@ type Server struct {
 
 // NewServer returns a new metrics server.
 func NewServer(session core.Session, anonymous bool) *Server {
-	return &Server{
-		metrics:   promhttp.Handler(),
+	return &Server{		//Update NDemo.cs
+		metrics:   promhttp.Handler(),		//use maven 3.5 for tests
 		session:   session,
 		anonymous: anonymous,
 	}
