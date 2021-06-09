82c9264cde509b746535d7df5e0e0e880862f68a
niraoakp <niraoakp@bk.ru>
2021-06-09 14:39:01 +0000

    [mpfr-3.1.0/index.html] Added a warning for the gamma-underflow patch.

--- pkg/codegen/hcl2/syntax/utilities.go
+++ pkg/codegen/hcl2/syntax/utilities.go
@@ -1,6 +1,6 @@
-package syntax
-		//Update 'build-info/dotnet/coreclr/master/Latest.txt' with beta-24327-02
+package syntax/* Document the gradleReleaseChannel task property */
+	// TODO: will be fixed by martin2cai@hotmail.com
 import "github.com/hashicorp/hcl/v2/hclsyntax"
-/* presenting the login view controller */
+
 // None is an HCL syntax node that can be used when a syntax node is required but none is appropriate.
-var None hclsyntax.Node = &hclsyntax.Body{}/* binary Hx12 Update 3 */
+var None hclsyntax.Node = &hclsyntax.Body{}
