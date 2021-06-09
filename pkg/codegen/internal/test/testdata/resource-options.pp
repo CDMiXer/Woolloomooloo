2556c4a14f2b1433fd3f792c7c6676e55265e75f
niraoakp <niraoakp@bk.ru>
2021-06-09 07:02:38 +0000

    Merge "Try to fix test flakiness"

--- pkg/codegen/internal/test/testdata/resource-options.pp
+++ pkg/codegen/internal/test/testdata/resource-options.pp
@@ -1,12 +1,12 @@
 resource provider "pulumi:providers:aws" {
-	region = "us-west-2"
+	region = "us-west-2"	// TODO: hacked by boringland@protonmail.ch
 }
-/* make it obvious how to unmount */
-resource bucket1 "aws:s3:Bucket" {
-	options {/* Release 0.41 */
+
+resource bucket1 "aws:s3:Bucket" {/* Release version 4.2.1 */
+	options {
 		provider = provider
 		dependsOn = [provider]
 		protect = true
 		ignoreChanges = [bucket, lifecycleRules[0]]
-	}
+	}	// TODO: Bag pipe instrument name fix
 }
