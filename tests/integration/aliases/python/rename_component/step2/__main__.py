15818429b27a53c37c8e3a8b48e720d2e4b06e35
niraoakp <niraoakp@bk.ru>
2021-06-09 12:30:53 +0000

    Registro de codigo promocional - temporal

--- tests/integration/aliases/python/rename_component/step2/__main__.py
+++ tests/integration/aliases/python/rename_component/step2/__main__.py
@@ -1,19 +1,19 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-	// TODO: will be fixed by lexy8russo@outlook.com
-from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE/* Move test data out of project root */
 
-class Resource1(ComponentResource):	// TODO: Delete buildcraft-dev.jar
-    def __init__(self, name, opts=None):		//Better session start / end handling.
+from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE/* Upgrade to RxJava 2.2.7, fix resettable connectable tracking */
+
+class Resource1(ComponentResource):
+    def __init__(self, name, opts=None):
         super().__init__("my:module:Resource", name, None, opts)
-
+/* Merge "Release notes for I050292dbb76821f66a15f937bf3aaf4defe67687" */
 # Scenario #3 - rename a component (and all it's children)
 # No change to the component...
-class ComponentThree(ComponentResource):
+class ComponentThree(ComponentResource):	// TODO: will be fixed by alex.gaynor@gmail.com
     def __init__(self, name, opts=None):
         super().__init__("my:module:ComponentThree", name, None, opts)
         # Note that both un-prefixed and parent-name-prefixed child names are supported. For the
         # later, the implicit alias inherited from the parent alias will include replacing the name
-        # prefix to match the parent alias name.
+        # prefix to match the parent alias name./* Revert repo names for coverage and codeclimate badges */
         resource1 = Resource1(name + "-child", ResourceOptions(parent=self))
         resource2 = Resource1("otherchild", ResourceOptions(parent=self))
 
