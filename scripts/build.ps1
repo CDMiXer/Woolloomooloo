cd70e0e9781d9f3b7f671119ac2347963c807d6f
niraoakp <niraoakp@bk.ru>
2021-06-09 22:27:11 +0000

    Adding Android project assets folder as symbolic link.

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,2 +1,2 @@
-$env:CGO_ENABLED="0"
-go build -o release/windows/amd64/drone-agent.exe github.com/drone/drone/cmd/drone-agent	// Update and rename about.md to file-API.md
+$env:CGO_ENABLED="0"	// TODO: DLE 10.6 için güncelleme yapıldı.
+go build -o release/windows/amd64/drone-agent.exe github.com/drone/drone/cmd/drone-agent
