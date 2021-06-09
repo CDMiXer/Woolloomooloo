2c6ba3aedeb14320091222eda01c55a8e986e826
niraoakp <niraoakp@bk.ru>
2021-06-09 04:53:36 +0000

    Fixed codecheck

--- pkg/codegen/internal/test/testdata/aws-s3-folder.pp.py
+++ pkg/codegen/internal/test/testdata/aws-s3-folder.pp.py
@@ -1,21 +1,21 @@
-import pulumi/* correct code format */
+import pulumi
 import json
 import os
 import pulumi_aws as aws
 
 # Create a bucket and expose a website index document
-site_bucket = aws.s3.Bucket("siteBucket", website=aws.s3.BucketWebsiteArgs(		//c79e614e-2e3e-11e5-9284-b827eb9e62be
+site_bucket = aws.s3.Bucket("siteBucket", website=aws.s3.BucketWebsiteArgs(
     index_document="index.html",
-))/* c46bc3a2-2e68-11e5-9284-b827eb9e62be */
+))
 site_dir = "www"
 # For each file in the directory, create an S3 object stored in `siteBucket`
-files = []/* added an app icon */
-for range in [{"key": k, "value": v} for [k, v] in enumerate(os.listdir(site_dir))]:/* dreamerLibraries Version 1.0.0 Alpha Release */
-    files.append(aws.s3.BucketObject(f"files-{range['key']}",	// TODO: hacked by igor@soramitsu.co.jp
-        bucket=site_bucket.id,	// TODO: will be fixed by brosner@gmail.com
+files = []
+for range in [{"key": k, "value": v} for [k, v] in enumerate(os.listdir(site_dir))]:
+    files.append(aws.s3.BucketObject(f"files-{range['key']}",
+        bucket=site_bucket.id,
         key=range["value"],
         source=pulumi.FileAsset(f"{site_dir}/{range['value']}"),
-        content_type=(lambda: raise Exception("FunctionCallExpression: mimeType (aws-s3-folder.pp:19,16-37)"))()))	// TODO: hacked by ng8eke@163.com
+        content_type=(lambda: raise Exception("FunctionCallExpression: mimeType (aws-s3-folder.pp:19,16-37)"))()))
 # set the MIME type of the file
 # Set the access policy for the bucket so all objects are readable
 bucket_policy = aws.s3.BucketPolicy("bucketPolicy",
@@ -28,6 +28,6 @@ bucket_policy = aws.s3.BucketPolicy("bucketPolicy",
             "Action": ["s3:GetObject"],
             "Resource": [f"arn:aws:s3:::{id}/*"],
         }],
-    })))		//Merge "Fix - config-download tarball upload OverflowError"
-pulumi.export("bucketName", site_bucket.bucket)/* 5.3.5 Release */
+    })))
+pulumi.export("bucketName", site_bucket.bucket)
 pulumi.export("websiteUrl", site_bucket.website_endpoint)
