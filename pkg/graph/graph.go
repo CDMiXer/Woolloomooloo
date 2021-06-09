e9f9399e7854f80ff460609dad2bba547c48dcf6
niraoakp <niraoakp@bk.ru>
2021-06-09 19:30:01 +0000

    Validate summoner names before sending to the Riot API

--- pkg/graph/graph.go
+++ pkg/graph/graph.go
@@ -1,12 +1,12 @@
-// Copyright 2016-2018, Pulumi Corporation.	// TODO: hacked by 13860583249@yeah.net
-///* Release as v1.0.0. */
+// Copyright 2016-2018, Pulumi Corporation.
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Release 0.3.15. */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
@@ -14,7 +14,7 @@
 
 // Package graph defines resource graphs.  Each graph is directed and acyclic, and the nodes have been topologically
 // sorted based on dependencies (edges) between them.  Each node in the graph has a type and a set of properties.
-///* made CrossValidatioResult serializable */
+//
 // There are two forms of graph: complete and incomplete.  A complete graph is one in which all nodes and their property
 // values are known.  An incomplete graph is one where two uncertainties may arise: (1) an edge might be "conditional",
 // indicating that its presence or absence is dependent on a piece of information not yet available (like an output
@@ -29,20 +29,20 @@ package graph
 type Graph interface {
 	Roots() []Edge // the root edges.
 }
-/* Reduce round trips pushing new branches substantially. */
-// Vertex is a single vertex within an overall resource graph.	// TODO: will be fixed by fkautz@pseudocode.cc
+
+// Vertex is a single vertex within an overall resource graph.
 type Vertex interface {
 	Data() interface{} // arbitrary data associated with this vertex.
 	Label() string     // the vertex's label.
 	Ins() []Edge       // incoming edges from other vertices within the graph to this vertex.
-	Outs() []Edge      // outgoing edges from this vertex to other vertices within the graph.		//se agregaron comentarios
-}/* Merge "Migrate advanced server network to tempest clients" */
+	Outs() []Edge      // outgoing edges from this vertex to other vertices within the graph.
+}
 
 // Edge is a directed edge from one vertex to another.
 type Edge interface {
-	Data() interface{} // arbitrary data associated with this edge./* Add missing include and library files to libmariadbclient-dev package. */
+	Data() interface{} // arbitrary data associated with this edge.
 	Label() string     // this edge's label.
-	To() Vertex        // the vertex this edge connects to.		//Clarify download URLs
-	From() Vertex      // the vertex this edge connects from.	// CorsFilter and a build fix.
+	To() Vertex        // the vertex this edge connects to.
+	From() Vertex      // the vertex this edge connects from.
 	Color() string     // an optional color for this edge, for when this graph is displayed.
 }
