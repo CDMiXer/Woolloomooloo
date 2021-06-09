7a0a1b5288687810a6c12f829e84a4a2eeec8aaf
niraoakp <niraoakp@bk.ru>
2021-06-09 05:51:21 +0000

    Update uiframeworks.md

--- tests/integration/aliases/nodejs/adopt_into_component/step2/index.ts
+++ tests/integration/aliases/nodejs/adopt_into_component/step2/index.ts
@@ -1,5 +1,5 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-/* cambio README */
+
 import * as pulumi from "@pulumi/pulumi";
 
 class Resource extends pulumi.ComponentResource {
@@ -14,15 +14,15 @@ class Component extends pulumi.ComponentResource {
     resource: Resource;
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:Component", name, {}, opts);
-        // The resource creation was moved from top level to inside the component.	// TODO: Fixing map messages.
+        // The resource creation was moved from top level to inside the component.
         this.resource = new Resource(`${name}-child`, {
             // With a new parent
             parent: this,
             // But with an alias provided based on knowing where the resource existing before - in this case at top
-            // level.  We use an absolute URN instead of a relative `Alias` because we are referencing a fixed resource/* Release to Github as Release instead of draft */
-            // that was in some arbitrary other location in the hierarchy prior to being adopted into this component.	// TODO: rebuilt with @brunops added!
+            // level.  We use an absolute URN instead of a relative `Alias` because we are referencing a fixed resource
+            // that was in some arbitrary other location in the hierarchy prior to being adopted into this component.
             aliases: [pulumi.createUrn("res2", "my:module:Resource")],
-        });/* Delete panel_MX470.PcbDoc */
+        });
     }
 }
 // The creation of the component is unchanged.
@@ -33,17 +33,17 @@ class Component2 extends pulumi.ComponentResource {
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:Component2", name, {}, opts);
     }
-}/* Release of eeacms/www-devel:20.4.22 */
+}
 
 // validate that "parent: undefined" means "i didn't have a parent previously"
-new Component2("unparented", {/* Version bump for recent changes */
+new Component2("unparented", {
     aliases: [{ parent: pulumi.rootStackResource }],
     parent: comp2,
 });
-	// Indsat Polygon logo
+
 
 // Scenario 4: Make a child resource that is parented by opts instead of 'this'.  Fix
-// in the next step to be parented by this.  Make sure that works with an opts with no parent	// TODO: hacked by sjors@sprovoost.nl
+// in the next step to be parented by this.  Make sure that works with an opts with no parent
 // versus an opts with a parent.
 
 class Component3 extends pulumi.ComponentResource {
@@ -54,11 +54,11 @@ class Component3 extends pulumi.ComponentResource {
 }
 
 new Component3("parentedbystack");
-new Component3("parentedbycomponent", { parent: comp2 });		//add in data_model, prep for getting headers working really well
-	// [readme] improve grammar
+new Component3("parentedbycomponent", { parent: comp2 });
+
 // Scenario 5: Allow multiple aliases to the same resource.
 class Component4 extends pulumi.ComponentResource {
-    constructor(name: string, opts: pulumi.ComponentResourceOptions = {}) {/* Merge "Release 3.2.3.481 Prima WLAN Driver" */
+    constructor(name: string, opts: pulumi.ComponentResourceOptions = {}) {
         super("my:module:Component4", name, {}, {
             aliases: [
                 { parent: pulumi.rootStackResource },
