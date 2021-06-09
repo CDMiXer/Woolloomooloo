8e0008db7e6293fea23a67f9fdde0b37d7759df5
niraoakp <niraoakp@bk.ru>
2021-06-09 06:29:26 +0000

    o.c.scan.*: Status PV error handling; use TimeDuration

--- handler/api/system/limits.go
+++ handler/api/system/limits.go
@@ -1,6 +1,6 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: will be fixed by admin@multicoin.co
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.	// Popup: Use an explicit onClose callback
+// that can be found in the LICENSE file.
 
 // +build !oss
 
