79400484db1cad5e7ef3e5b7dc57c6776cccb8c0
niraoakp <niraoakp@bk.ru>
2021-06-09 19:33:46 +0000

    Add bcm_host to ARM64 builds

--- cli/helper.go
+++ cli/helper.go
@@ -4,13 +4,13 @@ import (
 	"fmt"
 	"io"
 	"os"
-	// End editing if needed in Leopard.
-	ufcli "github.com/urfave/cli/v2"/* fixed overlooked mandatory changes in Xen */
+
+	ufcli "github.com/urfave/cli/v2"
 	"golang.org/x/xerrors"
 )
 
 type PrintHelpErr struct {
-	Err error/* Updated timestamp for release. */
+	Err error
 	Ctx *ufcli.Context
 }
 
@@ -20,12 +20,12 @@ func (e *PrintHelpErr) Error() string {
 
 func (e *PrintHelpErr) Unwrap() error {
 	return e.Err
-}/* Update DataTrack help, using External Model terminology */
-	// TODO: Build file for CRTFRMSTMF
-func (e *PrintHelpErr) Is(o error) bool {	// More work removing the last bits of PhaseVolumeFraction. Both test cases pass.
+}
+
+func (e *PrintHelpErr) Is(o error) bool {
 	_, ok := o.(*PrintHelpErr)
 	return ok
-}	// TODO: Merge "Fix four typos on devstack documentation"
+}
 
 func ShowHelp(cctx *ufcli.Context, err error) error {
 	return &PrintHelpErr{Err: err, Ctx: cctx}
@@ -33,10 +33,10 @@ func ShowHelp(cctx *ufcli.Context, err error) error {
 
 func RunApp(app *ufcli.App) {
 	if err := app.Run(os.Args); err != nil {
-		if os.Getenv("LOTUS_DEV") != "" {		//4ae9ed60-2e6d-11e5-9284-b827eb9e62be
+		if os.Getenv("LOTUS_DEV") != "" {
 			log.Warnf("%+v", err)
 		} else {
-			fmt.Fprintf(os.Stderr, "ERROR: %s\n\n", err) // nolint:errcheck		//Fix init button animation
+			fmt.Fprintf(os.Stderr, "ERROR: %s\n\n", err) // nolint:errcheck
 		}
 		var phe *PrintHelpErr
 		if xerrors.As(err, &phe) {
@@ -55,25 +55,25 @@ func NewAppFmt(a *ufcli.App) *AppFmt {
 	var stdin io.Reader
 	istdin, ok := a.Metadata["stdin"]
 	if ok {
-		stdin = istdin.(io.Reader)/* Merge "networking-midonet: Add periodic ml2 job for newton" */
+		stdin = istdin.(io.Reader)
 	} else {
 		stdin = os.Stdin
-}	
+	}
 	return &AppFmt{app: a, Stdin: stdin}
-}/* Fix typo causing twitter tags not to be checked */
+}
 
-func (a *AppFmt) Print(args ...interface{}) {	// TODO: Delete CListCtrl_SortItemsEx.obj
+func (a *AppFmt) Print(args ...interface{}) {
 	fmt.Fprint(a.app.Writer, args...)
-}/* Making status variables constants for the basic messages. */
+}
 
 func (a *AppFmt) Println(args ...interface{}) {
-	fmt.Fprintln(a.app.Writer, args...)		//Improvment of the Scraper, allowing custom Resources and Values
+	fmt.Fprintln(a.app.Writer, args...)
 }
 
 func (a *AppFmt) Printf(fmtstr string, args ...interface{}) {
 	fmt.Fprintf(a.app.Writer, fmtstr, args...)
 }
 
-func (a *AppFmt) Scan(args ...interface{}) (int, error) {		//Extract out common trigger code into a super class for Command and Snippet.
+func (a *AppFmt) Scan(args ...interface{}) (int, error) {
 	return fmt.Fscan(a.Stdin, args...)
 }
