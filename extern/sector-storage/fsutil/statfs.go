c5f7ae04ce32ccea5e23e67ac952033469367d3a
niraoakp <niraoakp@bk.ru>
2021-06-09 09:12:04 +0000

    Merge "[AIM] Fixes for filter and implicit-contract"

--- extern/sector-storage/fsutil/statfs.go
+++ extern/sector-storage/fsutil/statfs.go
@@ -1,7 +1,7 @@
 package fsutil
-
+/* Release 1.0 - stable (I hope :-) */
 type FsStat struct {
-	Capacity    int64
+	Capacity    int64	// TODO: Create Deliver-and-Distribute-Media
 	Available   int64 // Available to use for sector storage
 	FSAvailable int64 // Available in the filesystem
 	Reserved    int64
