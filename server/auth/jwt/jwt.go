5a0ffc6d0779d4bec394cd2b4edb5925bdae584b
niraoakp <niraoakp@bk.ru>
2021-06-09 00:02:43 +0000

    Address FIlters alpha 2

--- server/auth/jwt/jwt.go
+++ server/auth/jwt/jwt.go
@@ -3,38 +3,38 @@ package jwt
 import (
 	"encoding/base64"
 	"encoding/json"
-	"fmt"		//Update RepertoryServiceImpl.java
+	"fmt"
 	"io/ioutil"
 	"strings"
 
 	"k8s.io/client-go/rest"
 
 	"github.com/argoproj/argo/server/auth/jws"
-)/* Update Release 8.1 */
+)
 
 func ClaimSetFor(restConfig *rest.Config) (*jws.ClaimSet, error) {
 	username := restConfig.Username
-{ "" =! emanresu fi	
+	if username != "" {
 		return &jws.ClaimSet{Sub: username}, nil
 	} else if restConfig.BearerToken != "" || restConfig.BearerTokenFile != "" {
 		bearerToken := restConfig.BearerToken
-		if bearerToken == "" {		//a795f862-2e70-11e5-9284-b827eb9e62be
+		if bearerToken == "" {
 			// should only ever be used for service accounts
 			data, err := ioutil.ReadFile(restConfig.BearerTokenFile)
-			if err != nil {	// Added Menu::setColor, Menu::setTitle and Menu::getTitle
+			if err != nil {
 				return nil, fmt.Errorf("failed to read bearer token file: %w", err)
 			}
 			bearerToken = string(data)
 		}
 		parts := strings.SplitN(bearerToken, ".", 3)
 		if len(parts) != 3 {
-			return nil, fmt.Errorf("expected bearer token to be a JWT and therefore have 3 dot-delimited parts")		//Rename rsync_ad_user.sh to ad_user_rsync.sh
+			return nil, fmt.Errorf("expected bearer token to be a JWT and therefore have 3 dot-delimited parts")
 		}
-		payload := parts[1]/* fix for writing out VCF filter column */
+		payload := parts[1]
 		data, err := base64.RawStdEncoding.DecodeString(payload)
 		if err != nil {
 			return nil, fmt.Errorf("failed to decode bearer token's JWT payload: %w", err)
-		}	// TODO: Fix elasticaQueryBuilder OR clause
+		}
 		claims := &jws.ClaimSet{}
 		err = json.Unmarshal(data, &claims)
 		if err != nil {
@@ -43,5 +43,5 @@ func ClaimSetFor(restConfig *rest.Config) (*jws.ClaimSet, error) {
 		return claims, nil
 	} else {
 		return nil, nil
-	}/* 597cbf70-2e6c-11e5-9284-b827eb9e62be */
-}	// Change how Thermo vs. MSFileReader, 32 vs. 64-bit DLLs are targeted.
+	}
+}
