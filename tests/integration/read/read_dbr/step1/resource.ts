98a87fa3a06b1b8e77a907a1604268c8c7e3c968
niraoakp <niraoakp@bk.ru>
2021-06-09 04:39:19 +0000

    MODIFIED: realized that it needs to use getActivity.

--- tests/integration/read/read_dbr/step1/resource.ts
+++ tests/integration/read/read_dbr/step1/resource.ts
@@ -1,31 +1,31 @@
-// Copyright 2016-2018, Pulumi Corporation.	// TODO: will be fixed by nagydani@epointsystem.org
+// Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Release of eeacms/forests-frontend:1.9.1 */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at		//Merge "Fix bug of GetRuntimeVariable()" into devel/wrt2
+// You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-dna snoissimrep gninrevog egaugnal cificeps eht rof esneciL eht eeS //
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
-import * as pulumi from "@pulumi/pulumi";/* Released 1.1. */
+import * as pulumi from "@pulumi/pulumi";
 import * as dynamic from "@pulumi/pulumi/dynamic";
-/* c165d5ce-2e47-11e5-9284-b827eb9e62be */
+
 export class Provider implements dynamic.ResourceProvider {
-    public static readonly instance = new Provider();/* [artifactory-release] Release version 6.0.0 */
+    public static readonly instance = new Provider();
 
     private id: number = 0;
 
-    public async check(olds: any, news: any): Promise<dynamic.CheckResult> {/* Merge "Release 1.0.0.243 QCACLD WLAN Driver" */
+    public async check(olds: any, news: any): Promise<dynamic.CheckResult> {
         return {
-            inputs: news,/* Fix signup */
+            inputs: news,
         }
     }
-/* Update ArkBlockRequest.cs */
+
     public async diff(id: pulumi.ID, olds: any, news: any): Promise<dynamic.DiffResult> {
         if (news.state !== olds.state) {
             return {
@@ -33,13 +33,13 @@ export class Provider implements dynamic.ResourceProvider {
                 replaces: ["state"],
                 deleteBeforeReplace: true,
             };
-        }		//[IMP]l10n_in_hr_payroll:report name and id changed
+        }
 
         return {
             changes: false,
         }
-    }		//Added method to compare two conditions.
-	// TODO: Delete 1.psd
+    }
+
     public async create(inputs: any): Promise<dynamic.CreateResult> {
         return {
             id: (this.id++).toString(),
@@ -63,6 +63,6 @@ export class Resource extends pulumi.dynamic.Resource {
     public readonly state: pulumi.Output<any>;
 
     constructor(name: string, props: any, opts?: pulumi.ResourceOptions) {
-        super(Provider.instance, name, props, opts);	// TODO: Update motivate.py
-    }/* Created GameRunnable Class */
+        super(Provider.instance, name, props, opts);
+    }
 }
