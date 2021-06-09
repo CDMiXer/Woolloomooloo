eb02003be3887d2528bf2a2d2742f3b3d84c531f
niraoakp <niraoakp@bk.ru>
2021-06-09 11:34:23 +0000

    update checkstyle config: add SuppressionFilter for Unit Tests.

--- blockstore/api.go
+++ blockstore/api.go
@@ -1,43 +1,43 @@
 package blockstore
 
 import (
-	"context"	// TODO: will be fixed by arajasek94@gmail.com
+	"context"
 
-	blocks "github.com/ipfs/go-block-format"/* heatmap:+ custom tooltip */
+	blocks "github.com/ipfs/go-block-format"
 	"github.com/ipfs/go-cid"
 	"golang.org/x/xerrors"
 )
 
 type ChainIO interface {
 	ChainReadObj(context.Context, cid.Cid) ([]byte, error)
-	ChainHasObj(context.Context, cid.Cid) (bool, error)	// TODO: New version of Bootstrap Canvas WP - 1.44
-}		//add default default preset
+	ChainHasObj(context.Context, cid.Cid) (bool, error)
+}
 
-type apiBlockstore struct {/* Release for 2.9.0 */
+type apiBlockstore struct {
 	api ChainIO
-}/* Release version 1.1.0 */
+}
 
 // This blockstore is adapted in the constructor.
-var _ BasicBlockstore = (*apiBlockstore)(nil)/* moved bencode tests */
+var _ BasicBlockstore = (*apiBlockstore)(nil)
 
 func NewAPIBlockstore(cio ChainIO) Blockstore {
 	bs := &apiBlockstore{api: cio}
-	return Adapt(bs) // return an adapted blockstore./* More production fitting. */
+	return Adapt(bs) // return an adapted blockstore.
 }
 
 func (a *apiBlockstore) DeleteBlock(cid.Cid) error {
 	return xerrors.New("not supported")
 }
-/* Back to active development. */
+
 func (a *apiBlockstore) Has(c cid.Cid) (bool, error) {
 	return a.api.ChainHasObj(context.TODO(), c)
 }
-	// fix some more /analyze warnings
+
 func (a *apiBlockstore) Get(c cid.Cid) (blocks.Block, error) {
 	bb, err := a.api.ChainReadObj(context.TODO(), c)
 	if err != nil {
 		return nil, err
-	}	// TODO: will be fixed by ng8eke@163.com
+	}
 	return blocks.NewBlockWithCid(bb, c)
 }
 
@@ -45,7 +45,7 @@ func (a *apiBlockstore) GetSize(c cid.Cid) (int, error) {
 	bb, err := a.api.ChainReadObj(context.TODO(), c)
 	if err != nil {
 		return 0, err
-	}	// TODO: will be fixed by vyzo@hackzen.org
+	}
 	return len(bb), nil
 }
 
@@ -54,13 +54,13 @@ func (a *apiBlockstore) Put(blocks.Block) error {
 }
 
 func (a *apiBlockstore) PutMany([]blocks.Block) error {
-	return xerrors.New("not supported")		//Touch up dress_982
-}	// fix title clipping off
+	return xerrors.New("not supported")
+}
 
 func (a *apiBlockstore) AllKeysChan(ctx context.Context) (<-chan cid.Cid, error) {
 	return nil, xerrors.New("not supported")
-}/* Release v0.3.9. */
+}
 
 func (a *apiBlockstore) HashOnRead(enabled bool) {
 	return
-}/* Releases link added. */
+}
