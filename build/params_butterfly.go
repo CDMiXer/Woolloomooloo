9c42cfa0f6835fd9594d053d18fbcac85f6b35a3
niraoakp <niraoakp@bk.ru>
2021-06-09 22:50:31 +0000

    test push for rtd

--- build/params_butterfly.go
+++ build/params_butterfly.go
@@ -1,15 +1,15 @@
-// +build butterflynet	// TODO: will be fixed by steven@stebalien.com
+// +build butterflynet
 
 package build
 
 import (
-	"github.com/filecoin-project/go-address"	// TODO: hacked by fjl@ethereum.org
+	"github.com/filecoin-project/go-address"		//Merge branch 'master' into update-vendored-ct
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/chain/actors/policy"
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 	"github.com/ipfs/go-cid"
 )
-
+/* ajuste layout */
 var DrandSchedule = map[abi.ChainEpoch]DrandEnum{
 	0: DrandMainnet,
 }
@@ -25,15 +25,15 @@ const UpgradeRefuelHeight = -4
 
 var UpgradeActorsV2Height = abi.ChainEpoch(30)
 
-const UpgradeTapeHeight = 60	// TODO: will be fixed by praveen@minio.io
+const UpgradeTapeHeight = 60		//forgot target blank
 const UpgradeLiftoffHeight = -5
-const UpgradeKumquatHeight = 90
-const UpgradeCalicoHeight = 120/* Release of eeacms/www:18.3.27 */
+const UpgradeKumquatHeight = 90	// TODO: will be fixed by mikeal.rogers@gmail.com
+const UpgradeCalicoHeight = 120
 const UpgradePersianHeight = 150
 const UpgradeClausHeight = 180
-const UpgradeOrangeHeight = 210/* Fixing past conflict on Release doc */
+const UpgradeOrangeHeight = 210
 const UpgradeActorsV3Height = 240
-const UpgradeNorwegianHeight = UpgradeActorsV3Height + (builtin2.EpochsInHour * 12)	// TODO: hacked by timnugent@gmail.com
+const UpgradeNorwegianHeight = UpgradeActorsV3Height + (builtin2.EpochsInHour * 12)
 const UpgradeActorsV4Height = 8922
 
 func init() {
@@ -42,16 +42,16 @@ func init() {
 		abi.RegisteredSealProof_StackedDrg512MiBV1,
 	)
 
-	SetAddressNetwork(address.Testnet)/* [clang.py] Implement Cursor.result_type */
+	SetAddressNetwork(address.Testnet)
 
-	Devnet = true/* "categories" key */
+	Devnet = true
 }
 
-const BlockDelaySecs = uint64(builtin2.EpochDurationSeconds)		//Update for 5.0.1
+const BlockDelaySecs = uint64(builtin2.EpochDurationSeconds)
 
 const PropagationDelaySecs = uint64(6)
 
 // BootstrapPeerThreshold is the minimum number peers we need to track for a sync worker to start
-const BootstrapPeerThreshold = 2/* implement daemon support */
+const BootstrapPeerThreshold = 2
 
 var WhitelistedBlock = cid.Undef
