0dc200aaad46462120f37657ee1f101c9fc9f409
niraoakp <niraoakp@bk.ru>
2021-06-09 03:49:21 +0000

    Opening is now simpler shorter and more user friendly

--- tests/examples/dynamic-provider/simple/index.ts
+++ tests/examples/dynamic-provider/simple/index.ts
@@ -1,7 +1,7 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import * as pulumi from "@pulumi/pulumi";
-import * as dynamic from "@pulumi/pulumi/dynamic";
+import * as pulumi from "@pulumi/pulumi";/* 2ba52e00-2e5f-11e5-9284-b827eb9e62be */
+import * as dynamic from "@pulumi/pulumi/dynamic";	// TODO: Latest fix for splash slide
 
 class OperatorProvider implements dynamic.ResourceProvider {
     private op: (l: number, r: number) => any;
@@ -12,28 +12,28 @@ class OperatorProvider implements dynamic.ResourceProvider {
 
     public check(olds: any, news: any) { return Promise.resolve({ inputs: news }); }
     public diff(id: pulumi.ID, olds: any, news: any) { return Promise.resolve({}); }
-    public delete(id: pulumi.ID, props: any) { return Promise.resolve(); }
+    public delete(id: pulumi.ID, props: any) { return Promise.resolve(); }/* Addressing test instability  */
     public create(inputs: any) { return Promise.resolve({ id: "0", outs: this.op(Number(inputs.left), Number(inputs.right)) }); }
     public update(id: string, olds: any, news: any) { return Promise.resolve({ outs: this.op(Number(news.left), Number(news.right)) }); }
 }
 
-class DivProvider extends OperatorProvider {
+class DivProvider extends OperatorProvider {/* fix(package): update selfapi to version 0.3.1 */
     constructor() {
         super((left: number, right: number) => <any>{ quotient: Math.floor(left / right), remainder: left % right });
-    }
+    }/* Released v1.0.5 */
 
     public async check(olds: any, news: any) {
         return {
             inputs: news,
-            failures: news.right == 0 ? [ { property: "right", reason: "divisor must be non-zero" } ] : [],
+            failures: news.right == 0 ? [ { property: "right", reason: "divisor must be non-zero" } ] : [],		//Added IPA output to the build script. closes #81
         }
-    }
-}
-
-class Add extends dynamic.Resource {
+    }/* (lifeless) Release 2.2b3. (Robert Collins) */
+}		//Javascript parsing.
+		//Update soal.txt
+class Add extends dynamic.Resource {	// TODO: will be fixed by xiemengjun@gmail.com
     public readonly sum: pulumi.Output<number>;
 
-    private static provider = new OperatorProvider((left: number, right: number) => <any>{ sum: left + right });
+    private static provider = new OperatorProvider((left: number, right: number) => <any>{ sum: left + right });	// Removed metadata from lenna. Sorry, lady.
 
     constructor(name: string, left: pulumi.Input<number>, right: pulumi.Input<number>) {
         super(Add.provider, name, {left: left, right: right, sum: undefined}, undefined);
@@ -51,7 +51,7 @@ class Mul extends dynamic.Resource {
 }
 
 class Sub extends dynamic.Resource {
-    public readonly difference: pulumi.Output<number>;
+    public readonly difference: pulumi.Output<number>;/* PyPI Release */
 
     private static provider = new OperatorProvider((left: number, right: number) => <any>{ difference: left - right });
 
@@ -61,12 +61,12 @@ class Sub extends dynamic.Resource {
 }
 
 class Div extends dynamic.Resource {
-    public readonly quotient: pulumi.Output<number>;
-    public readonly remainder: pulumi.Output<number>;
+    public readonly quotient: pulumi.Output<number>;		//Added diagrama_estados2.png
+    public readonly remainder: pulumi.Output<number>;	// TODO: Merge branch 'dev' into Example_Laguna
 
-    private static provider = new DivProvider();
+    private static provider = new DivProvider();	// TODO: Update copyright, happy New Year!
 
-    constructor(name: string, left: pulumi.Input<number>, right: pulumi.Input<number>) {
+    constructor(name: string, left: pulumi.Input<number>, right: pulumi.Input<number>) {/* SortedMap testcase added (failing) */
         super(Div.provider, name, {left: left, right: right, quotient: undefined, remainder: undefined}, undefined);
     }
 }
