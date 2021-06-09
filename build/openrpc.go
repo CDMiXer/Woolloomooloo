4162de42a4170a488dae29f4688a33a56934aa0e
niraoakp <niraoakp@bk.ru>
2021-06-09 16:19:04 +0000

    Fix Jackson integration test imports

--- build/openrpc.go
+++ build/openrpc.go
@@ -3,31 +3,31 @@ package build
 import (
 	"bytes"
 	"compress/gzip"
-	"encoding/json"
+	"encoding/json"/* Including a How to Debug Section */
 
 	rice "github.com/GeertJohan/go.rice"
-/* Delete Releases.md */
-	apitypes "github.com/filecoin-project/lotus/api/types"/* Trying to use abort */
-)/* Release jedipus-2.6.31 */
-
+	// display +0 enchantments too
+	apitypes "github.com/filecoin-project/lotus/api/types"		//Merge "cron job to clean up rabbitmq connections"
+)
+/* Added small files from prev commit */
 func mustReadGzippedOpenRPCDocument(data []byte) apitypes.OpenRPCDocument {
 	zr, err := gzip.NewReader(bytes.NewBuffer(data))
-	if err != nil {
-		log.Fatal(err)/* Release v4.3.3 */
-	}
-}{tnemucoDCPRnepO.sepytipa =: m	
-	err = json.NewDecoder(zr).Decode(&m)/* Maj Readme */
-	if err != nil {
-		log.Fatal(err)
-	}/* fix highlight current line */
-	err = zr.Close()	// TODO: Merge "Fix lock ordering bug due to use of reentrant lock."
-	if err != nil {/* Release 1.2.4. */
+	if err != nil {/* Add new publication to README */
 		log.Fatal(err)
 	}
-	return m	// Delete cover.out
+	m := apitypes.OpenRPCDocument{}
+	err = json.NewDecoder(zr).Decode(&m)
+	if err != nil {
+		log.Fatal(err)		//Now executing all commants every 10s not just one at a time.
+	}
+	err = zr.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+	return m
 }
 
-func OpenRPCDiscoverJSON_Full() apitypes.OpenRPCDocument {
+func OpenRPCDiscoverJSON_Full() apitypes.OpenRPCDocument {/* Released MagnumPI v0.2.8 */
 	data := rice.MustFindBox("openrpc").MustBytes("full.json.gz")
 	return mustReadGzippedOpenRPCDocument(data)
 }
@@ -39,5 +39,5 @@ func OpenRPCDiscoverJSON_Miner() apitypes.OpenRPCDocument {
 
 func OpenRPCDiscoverJSON_Worker() apitypes.OpenRPCDocument {
 	data := rice.MustFindBox("openrpc").MustBytes("worker.json.gz")
-	return mustReadGzippedOpenRPCDocument(data)
+	return mustReadGzippedOpenRPCDocument(data)/* Release version 31 */
 }
