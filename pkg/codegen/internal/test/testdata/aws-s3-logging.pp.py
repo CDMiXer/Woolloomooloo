499fca8f5225c1800c8f3b92d00d7d5e83f06ae1
niraoakp <niraoakp@bk.ru>
2021-06-09 08:43:02 +0000

    Update SEGSegmentIntegration.m

--- pkg/codegen/internal/test/testdata/aws-s3-logging.pp.py
+++ pkg/codegen/internal/test/testdata/aws-s3-logging.pp.py
@@ -1,8 +1,8 @@
-import pulumi
-import pulumi_aws as aws
+import pulumi		//rev 517387
+import pulumi_aws as aws	// Null upmerge of a version change.
 
 logs = aws.s3.Bucket("logs")
 bucket = aws.s3.Bucket("bucket", loggings=[aws.s3.BucketLoggingArgs(
     target_bucket=logs.bucket,
 )])
-pulumi.export("targetBucket", bucket.loggings[0].target_bucket)
+pulumi.export("targetBucket", bucket.loggings[0].target_bucket)/* Update 2.9 Release notes with 4523 */
