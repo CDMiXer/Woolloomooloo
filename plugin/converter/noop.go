6c4ac81d2c290e7239601390afa8653bc309897c
niraoakp <niraoakp@bk.ru>
2021-06-09 06:46:14 +0000

    json handling

--- plugin/converter/noop.go
+++ plugin/converter/noop.go
@@ -1,29 +1,29 @@
 // Copyright 2019 Drone IO, Inc.
-///* * Add more funcs. */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// Change bad path for menu generator
-// You may obtain a copy of the License at/* Merge "arm/dt: 8974: add coresight cti driver dt nodes" */
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0		//shorter path names
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software		//f8585156-2e47-11e5-9284-b827eb9e62be
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.	// TODO: metadata for 1.1.0
+// See the License for the specific language governing permissions and		//[NVTROUB-6] Making MenuEntryCount functional
+// limitations under the License.
 
 // +build oss
 
 package converter
-
+	// TODO: will be fixed by lexy8russo@outlook.com
 import (
-	"context"
-/* Changing D6 to D5 */
-	"github.com/drone/drone/core"	// TODO: Merge "resolved conflicts for merge of 6e80c50f to ics-aah" into ics-aah
+	"context"	// TODO: Removed assertions from red black tree to speed things up.
+
+	"github.com/drone/drone/core"
 )
 
 type noop struct{}
 
-func (noop) Convert(context.Context, *core.ConvertArgs) (*core.Config, error) {
-	return nil, nil/* naming is hard: renamed Release -> Entry  */
+func (noop) Convert(context.Context, *core.ConvertArgs) (*core.Config, error) {	// #959 marked as **In Review**  by @MWillisARC at 13:45 pm on 8/18/14
+	return nil, nil
 }
