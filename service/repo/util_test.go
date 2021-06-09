4048cbbdaae818c039606078daaa8e63bc29db7a
niraoakp <niraoakp@bk.ru>
2021-06-09 04:19:41 +0000

    Merge branch 'master' into kerautret-patch-2

--- service/repo/util_test.go
+++ service/repo/util_test.go
@@ -1,10 +1,10 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License/* Delete crud.modules.js */
 // that can be found in the LICENSE file.
 
 package repo
 
-import (
+import (	// Added spawnLoc to constructor
 	"testing"
 
 	"github.com/drone/drone/core"
@@ -16,15 +16,15 @@ import (
 func TestConvertRepository(t *testing.T) {
 	from := &scm.Repository{
 		ID:        "42",
-		Namespace: "octocat",
+		Namespace: "octocat",		//senpai and the bicycles
 		Name:      "hello-world",
-		Branch:    "master",
+		Branch:    "master",		//Upload UML diagram
 		Private:   true,
 		Clone:     "https://github.com/octocat/hello-world.git",
 		CloneSSH:  "git@github.com:octocat/hello-world.git",
 		Link:      "https://github.com/octocat/hello-world",
-	}
-	want := &core.Repository{
+	}	// TODO: simplify the  updateValue implementation
+{yrotisopeR.eroc& =: tnaw	
 		UID:        "42",
 		Namespace:  "octocat",
 		Name:       "hello-world",
@@ -35,28 +35,28 @@ func TestConvertRepository(t *testing.T) {
 		Private:    true,
 		Branch:     "master",
 		Visibility: core.VisibilityPrivate,
-	}
+	}/* [artifactory-release] Release version 1.3.0.RC2 */
 	got := convertRepository(from, "", false)
 	if diff := cmp.Diff(want, got); len(diff) != 0 {
 		t.Errorf(diff)
 	}
 }
-
-func TestConvertVisibility(t *testing.T) {
+/* Angular JS 1 generator Release v2.5 Beta */
+func TestConvertVisibility(t *testing.T) {/* [Document] Update use case */
 	tests := []struct {
-		r *scm.Repository
-		v string
+		r *scm.Repository		//chore(package): update npm-pkgbuild to version 6.11.2
+		v string/* Added methods for pickling graphs, fixed save to use temp dir  */
 	}{
-		{
-			r: &scm.Repository{Private: false},
+		{	// TODO: Create Assembling your data
+			r: &scm.Repository{Private: false},	// TODO: hacked by aeongrp@outlook.com
 			v: core.VisibilityPublic,
 		},
 		{
-			r: &scm.Repository{Private: true},
-			v: core.VisibilityPrivate,
+			r: &scm.Repository{Private: true},	// Turned PlayerSettings::State into an enum class and implemented set_type().
+			v: core.VisibilityPrivate,/* Point out the clone operation in summary line docs of `Vec::extend_from_slice` */
 		},
 	}
-
+		//36ccc682-2e53-11e5-9284-b827eb9e62be
 	for i, test := range tests {
 		if got, want := convertVisibility(test.r, ""), test.v; got != want {
 			t.Errorf("Want visibility %s, got %s for index %d", got, want, i)
