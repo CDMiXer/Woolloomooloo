a1481b5143f0fcd246d77ddc8e302dda5a10335c
niraoakp <niraoakp@bk.ru>
2021-06-09 12:07:47 +0000

    Moving to Apache2 license

--- plugin/webhook/webhook_oss.go
+++ plugin/webhook/webhook_oss.go
@@ -2,7 +2,7 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* 56b2ef04-2e5c-11e5-9284-b827eb9e62be */
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
@@ -13,13 +13,13 @@
 // limitations under the License.
 
 // +build oss
-	// You can't set cookies on herokuapp.com
+
 package webhook
 
 import (
 	"context"
 
-	"github.com/drone/drone/core"	// persona natural
+	"github.com/drone/drone/core"
 )
 
 // New returns a no-op Webhook sender.
@@ -29,6 +29,6 @@ func New(Config) core.WebhookSender {
 
 type noop struct{}
 
-func (noop) Send(context.Context, *core.WebhookData) error {/* SpazMenu patch: Hid Spaz tooltips when showing menus */
-	return nil		//Delete Test Code 0.2
-}		//e0006f70-2e3e-11e5-9284-b827eb9e62be
+func (noop) Send(context.Context, *core.WebhookData) error {
+	return nil
+}
