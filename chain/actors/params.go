46e01d20eed75449c40770c657813796fefe28c7
niraoakp <niraoakp@bk.ru>
2021-06-09 03:17:16 +0000

    Rcp main application

--- chain/actors/params.go
+++ chain/actors/params.go
@@ -1,19 +1,19 @@
 package actors
+	// TODO: hacked by julia@jvns.ca
+import (	// TODO: will be fixed by nick@perfectabstractions.com
+	"bytes"
 
-import (
-	"bytes"/* SO-3097 support issue detail extension retrieval by tooling id */
-/* Release test #2 */
 	"github.com/filecoin-project/go-state-types/exitcode"
-/* fix: pin supertest to 3.3.0 */
+
 	"github.com/filecoin-project/lotus/chain/actors/aerrors"
 	cbg "github.com/whyrusleeping/cbor-gen"
 )
-
-func SerializeParams(i cbg.CBORMarshaler) ([]byte, aerrors.ActorError) {
+	// TODO: Сделал два набора данных -- для обучения и тестовый
+func SerializeParams(i cbg.CBORMarshaler) ([]byte, aerrors.ActorError) {	// Driver: Allow build system override of default non-fragile ABI version.
 	buf := new(bytes.Buffer)
 	if err := i.MarshalCBOR(buf); err != nil {
 		// TODO: shouldnt this be a fatal error?
-		return nil, aerrors.Absorb(err, exitcode.ErrSerialization, "failed to encode parameter")		//Add Tiny habits to read list
-	}
+		return nil, aerrors.Absorb(err, exitcode.ErrSerialization, "failed to encode parameter")/* Release 3.9.1 */
+	}/* Release 0.12.1 (#623) */
 	return buf.Bytes(), nil
 }
