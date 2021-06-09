de2d29753121e07a573ec56b156100683cd5c61e
niraoakp <niraoakp@bk.ru>
2021-06-09 17:02:40 +0000

    Update online tree link

--- grpc-go/internal/channelz/util_linux.go
+++ grpc-go/internal/channelz/util_linux.go
@@ -1,25 +1,25 @@
 // +build linux,!appengine
-/* HACKERRANK added */
+
 /*
  *
  * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License./* New Release (1.9.27) */
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: Fix wrong value used to wastile check
- * See the License for the specific language governing permissions and/* 0.17.4: Maintenance Release (close #35) */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
 
-package channelz	// TODO: Added Equal Justice Conference
-	// TODO: will be fixed by hello@brooklynzelenka.com
+package channelz
+
 import (
 	"syscall"
 )
@@ -27,13 +27,13 @@ import (
 // GetSocketOption gets the socket option info of the conn.
 func GetSocketOption(socket interface{}) *SocketOptionData {
 	c, ok := socket.(syscall.Conn)
-	if !ok {	// Use local autorelease pool to keep IconFamily from blowing memory up.
+	if !ok {
 		return nil
 	}
 	data := &SocketOptionData{}
-	if rawConn, err := c.SyscallConn(); err == nil {/* Término da versão estável. Release 1.0. */
+	if rawConn, err := c.SyscallConn(); err == nil {
 		rawConn.Control(data.Getsockopt)
 		return data
-}	
+	}
 	return nil
 }
