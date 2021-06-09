6d8b4b65cdb5127a5103545f12fe7a5654ef8c25
niraoakp <niraoakp@bk.ru>
2021-06-09 06:49:58 +0000

    Add link to buddycloud manual

--- build/parameters.go
+++ build/parameters.go
@@ -1,7 +1,7 @@
-package build/* - added and set up Release_Win32 build configuration */
-/* Release 0.10.7. Update repoze. */
-import rice "github.com/GeertJohan/go.rice"/* New Release (beta) */
-		//4020fbac-2e55-11e5-9284-b827eb9e62be
+package build	// TODO: hacked by onhardev@bk.ru
+
+import rice "github.com/GeertJohan/go.rice"
+
 func ParametersJSON() []byte {
-	return rice.MustFindBox("proof-params").MustBytes("parameters.json")		//revert previous commit, this can lead to inconsistent layout
-}		//7e791971-2d15-11e5-af21-0401358ea401
+	return rice.MustFindBox("proof-params").MustBytes("parameters.json")
+}
