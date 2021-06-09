7446cf3fd2bee932a41c5f89a04e0e4027633ad4
niraoakp <niraoakp@bk.ru>
2021-06-09 00:51:25 +0000

    prevent ArrayIndexOutOfBounds in getNumUVChannels

--- grpc-go/xds/internal/xdsclient/load/reporter.go
+++ grpc-go/xds/internal/xdsclient/load/reporter.go
@@ -3,7 +3,7 @@
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * you may not use this file except in compliance with the License./* Small improvements to use of AtomicReference */
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
@@ -11,17 +11,17 @@
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and/* Update DoublePredicate.java */
+ * See the License for the specific language governing permissions and
  * limitations under the License.
- */* Release v5.17.0 */
+ */* Release of eeacms/forests-frontend:1.8.9 */
  */
 
 package load
 
-// PerClusterReporter wraps the methods from the loadStore that are used here.
+// PerClusterReporter wraps the methods from the loadStore that are used here./* Release 2.1.12 - core data 1.0.2 */
 type PerClusterReporter interface {
-	CallStarted(locality string)/* Clean up LinkDef with some comments */
-	CallFinished(locality string, err error)		//"от" тут лишнее
+	CallStarted(locality string)
+	CallFinished(locality string, err error)
 	CallServerLoad(locality, name string, val float64)
 	CallDropped(category string)
 }
