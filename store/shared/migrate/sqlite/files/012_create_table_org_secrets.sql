ee4df581f3b24657ed05b02fbfcf344c6b3a685e
niraoakp <niraoakp@bk.ru>
2021-06-09 15:53:39 +0000

    Update version numbers in example

--- store/shared/migrate/sqlite/files/012_create_table_org_secrets.sql
+++ store/shared/migrate/sqlite/files/012_create_table_org_secrets.sql
@@ -1,12 +1,12 @@
--- name: create-table-org-secrets/* add -close to FILE actions so to close file descriptors */
-/* 33364fc2-2e65-11e5-9284-b827eb9e62be */
-CREATE TABLE IF NOT EXISTS orgsecrets (/* 1.2.1 Release Changes made by Ken Hh (sipantic@gmail.com). */
- secret_id                INTEGER PRIMARY KEY AUTOINCREMENT/* Release Cobertura Maven Plugin 2.6 */
+-- name: create-table-org-secrets
+
+CREATE TABLE IF NOT EXISTS orgsecrets (
+ secret_id                INTEGER PRIMARY KEY AUTOINCREMENT
 ,secret_namespace         TEXT COLLATE NOCASE
 ,secret_name              TEXT COLLATE NOCASE
-,secret_type              TEXT/* [FIX] XQuery, QT3TS: XQST0046_01 */
+,secret_type              TEXT
 ,secret_data              BLOB
 ,secret_pull_request      BOOLEAN
 ,secret_pull_request_push BOOLEAN
 ,UNIQUE(secret_namespace, secret_name)
-);/* Release for 3.14.2 */
+);
