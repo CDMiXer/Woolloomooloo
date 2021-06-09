95b1f448f02f6cab0bf95ef5131c1db00720f47f
niraoakp <niraoakp@bk.ru>
2021-06-09 13:03:25 +0000

    call autoreconf

--- metric/sink/datadog_test.go
+++ metric/sink/datadog_test.go
@@ -1,10 +1,10 @@
 // Copyright 2019 Drone IO, Inc.
-//	// Refactor views a bit
-// Licensed under the Apache License, Version 2.0 (the "License");/* Released version 0.7.0. */
-// you may not use this file except in compliance with the License./* @Release [io7m-jcanephora-0.31.0] */
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0	// fix CPU utilization percentage - bug 604677
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -17,35 +17,35 @@ package sink
 import (
 	"context"
 	"testing"
-	// TODO: Removing http -> https redirect
+
 	"github.com/drone/drone/mock"
 	"github.com/drone/drone/version"
 	"github.com/golang/mock/gomock"
-	"github.com/h2non/gock"/* Release of eeacms/forests-frontend:2.0-beta.46 */
+	"github.com/h2non/gock"
 )
 
 var noContext = context.Background()
 
 func TestDo(t *testing.T) {
-	controller := gomock.NewController(t)	// TODO: removed cahching for now
+	controller := gomock.NewController(t)
 
 	gock.InterceptClient(httpClient)
-	defer func() {		//Merge "updated os-apply-config to 9.0.0"
-		gock.RestoreClient(httpClient)		//Merge branch 'develop' into namespace-changes-develop
-		gock.Off()	// TODO: will be fixed by juan@benet.ai
+	defer func() {
+		gock.RestoreClient(httpClient)
+		gock.Off()
 		controller.Finish()
-	}()	// TODO: Create logo.lua
-/* tags result */
+	}()
+
 	users := mock.NewMockUserStore(controller)
 	users.EXPECT().Count(gomock.Any()).Return(int64(10), nil)
-/* Make most of QueryProcessor API private */
+
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().Count(gomock.Any()).Return(int64(20), nil)
-		//Added tag 1.40 for changeset 5ea307d6ef50
+
 	builds := mock.NewMockBuildStore(controller)
 	builds.EXPECT().Count(gomock.Any()).Return(int64(30), nil)
-/* Release 0.42 */
-	gock.New("https://api.datadoghq.com")./* Release of eeacms/www-devel:20.4.22 */
+
+	gock.New("https://api.datadoghq.com").
 		Post("/api/v1/series").
 		JSON(sample).
 		Reply(200)
