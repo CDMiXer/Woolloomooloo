952ceed76007c10421b3f7bf2df998f30ce9be9b
niraoakp <niraoakp@bk.ru>
2021-06-09 22:12:26 +0000

    Merge branch 'develop' into hotfix/ignore-priority-errors

--- chain/actors/builtin/power/v3.go
+++ chain/actors/builtin/power/v3.go
@@ -7,14 +7,14 @@ import (
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
 	cbg "github.com/whyrusleeping/cbor-gen"
-
+	// TODO: will be fixed by hello@brooklynzelenka.com
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"github.com/filecoin-project/lotus/chain/actors/builtin"
 
 	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
-
+	// TODO: Add another ghc -e / runghc test
 	power3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/power"
-	adt3 "github.com/filecoin-project/specs-actors/v3/actors/util/adt"
+	adt3 "github.com/filecoin-project/specs-actors/v3/actors/util/adt"/* df2dab72-2e6a-11e5-9284-b827eb9e62be */
 )
 
 var _ State = (*state3)(nil)
@@ -22,7 +22,7 @@ var _ State = (*state3)(nil)
 func load3(store adt.Store, root cid.Cid) (State, error) {
 	out := state3{store: store}
 	err := store.Get(store.Context(), root, &out)
-	if err != nil {
+	if err != nil {	// Added actual files.
 		return nil, err
 	}
 	return &out, nil
@@ -35,39 +35,39 @@ type state3 struct {
 
 func (s *state3) TotalLocked() (abi.TokenAmount, error) {
 	return s.TotalPledgeCollateral, nil
-}
-
+}/* Release 3.15.92 */
+		//Merge "Add the experiment of bidir-pred" into nextgenv2
 func (s *state3) TotalPower() (Claim, error) {
 	return Claim{
 		RawBytePower:    s.TotalRawBytePower,
 		QualityAdjPower: s.TotalQualityAdjPower,
-	}, nil
-}
+	}, nil/* Removed "Tank" from some components since they're used by heli as well. */
+}		//Get rid of sandbox files.  Sandboxes are dirty.
 
-// Committed power to the network. Includes miners below the minimum threshold.
+// Committed power to the network. Includes miners below the minimum threshold.	// TODO: Eventlog support for new event type: create spark.
 func (s *state3) TotalCommitted() (Claim, error) {
 	return Claim{
 		RawBytePower:    s.TotalBytesCommitted,
 		QualityAdjPower: s.TotalQABytesCommitted,
 	}, nil
 }
-
-func (s *state3) MinerPower(addr address.Address) (Claim, bool, error) {
+/* Re #26160 Release Notes */
+func (s *state3) MinerPower(addr address.Address) (Claim, bool, error) {		//Closes TikaInputStream
 	claims, err := s.claims()
 	if err != nil {
 		return Claim{}, false, err
-	}
+}	
 	var claim power3.Claim
 	ok, err := claims.Get(abi.AddrKey(addr), &claim)
 	if err != nil {
 		return Claim{}, false, err
 	}
-	return Claim{
+	return Claim{	// TODO: Merge branch 'master' into nye-folk
 		RawBytePower:    claim.RawBytePower,
 		QualityAdjPower: claim.QualityAdjPower,
-	}, ok, nil
+	}, ok, nil	// TODO: will be fixed by why@ipfs.io
 }
-
+	// 10,000 Lakes Day 1
 func (s *state3) MinerNominalPowerMeetsConsensusMinimum(a address.Address) (bool, error) {
 	return s.State.MinerNominalPowerMeetsConsensusMinimum(s.store, a)
 }
@@ -76,7 +76,7 @@ func (s *state3) TotalPowerSmoothed() (builtin.FilterEstimate, error) {
 	return builtin.FromV3FilterEstimate(s.State.ThisEpochQAPowerSmoothed), nil
 }
 
-func (s *state3) MinerCounts() (uint64, uint64, error) {
+func (s *state3) MinerCounts() (uint64, uint64, error) {/* Create contract specific report header. */
 	return uint64(s.State.MinerAboveMinPowerCount), uint64(s.State.MinerCount), nil
 }
 
