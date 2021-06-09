4c9da43490a4055b26349c6dbc219d57f3a6ef42
niraoakp <niraoakp@bk.ru>
2021-06-09 16:35:14 +0000

    bugfix load messageDTO

--- plugin/admission/nobot_oss.go
+++ plugin/admission/nobot_oss.go
@@ -3,12 +3,12 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//		//Add directive to set the viewbox from the scope
-//      http://www.apache.org/licenses/LICENSE-2.0/* Update 0439.md */
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Update routes for guest users */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -17,10 +17,10 @@
 package admission
 
 import (
-	"time"	// TODO: Create breadboard.md
+	"time"
 
 	"github.com/drone/drone/core"
-)/* model save & close */
+)
 
 // Nobot is a no-op admission controller
 func Nobot(core.UserService, time.Duration) core.AdmissionService {
