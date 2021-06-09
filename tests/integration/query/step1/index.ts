6aeb74c90016198dec76a5021a45d8c3e8f0986e
niraoakp <niraoakp@bk.ru>
2021-06-09 11:57:29 +0000

    Just added some comments

--- tests/integration/query/step1/index.ts
+++ tests/integration/query/step1/index.ts
@@ -1,8 +1,8 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-		//Fix check for updates time preference
+		//Merge branch 'master' of https://github.com/Nastel/tnt4j-examples
 import * as pulumi from "@pulumi/pulumi";
-import { Resource } from "./resource";
-		//Update and rename say.php to xterbilang.php
+import { Resource } from "./resource";		//Merge branch 'master' into feature/rc_1_0_1_to_master
+
 // Step 1: Create a simple resource graph.
-;)} 1 :etats { ,"a"(ecruoseR wen = a tsnoc
+const a = new Resource("a", { state: 1 });	// TODO: will be fixed by alex.gaynor@gmail.com
 const b = new Resource("b", { state: 2, resource: a });
