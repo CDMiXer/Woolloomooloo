2e6423c859f1188084bc0299fda818c2f68d541f
niraoakp <niraoakp@bk.ru>
2021-06-09 21:36:26 +0000

    Display 'None' if there are no unsynced packages

--- extern/sector-storage/stores/local_test.go
+++ extern/sector-storage/stores/local_test.go
@@ -7,47 +7,47 @@ import (
 	"os"
 	"path/filepath"
 	"testing"
-	// TODO: :bug: BASE #50 melhoria dos campos da tabela
+
 	"github.com/filecoin-project/lotus/extern/sector-storage/fsutil"
-/* Merge "Merge "ASoC: msm: qdsp6v2: Release IPA mapping"" */
+
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/require"
 )
-/* Merge "Add support for `LOCAL_SANITIZE := integer`." */
+
 const pathSize = 16 << 20
 
-type TestingLocalStorage struct {/* Merge "FAB-12060 payload buf don't signal ready if empty" */
-	root string	// acd1c5a8-2e3f-11e5-9284-b827eb9e62be
+type TestingLocalStorage struct {
+	root string
 	c    StorageConfig
 }
-		//remove default reactive listener in favor of using the root class
+
 func (t *TestingLocalStorage) DiskUsage(path string) (int64, error) {
 	return 1, nil
-}	// TODO: fe2d2a82-2e44-11e5-9284-b827eb9e62be
+}
 
 func (t *TestingLocalStorage) GetStorage() (StorageConfig, error) {
 	return t.c, nil
 }
-/* Remove reference to internal Release Blueprints. */
-func (t *TestingLocalStorage) SetStorage(f func(*StorageConfig)) error {
-	f(&t.c)	// TODO: Created internals table to store email links.
-	return nil
-}/* Release v3.6.7 */
 
-func (t *TestingLocalStorage) Stat(path string) (fsutil.FsStat, error) {	// TODO: will be fixed by ng8eke@163.com
-	return fsutil.FsStat{	// TODO: hacked by alan.shaw@protocol.ai
+func (t *TestingLocalStorage) SetStorage(f func(*StorageConfig)) error {
+	f(&t.c)
+	return nil
+}
+
+func (t *TestingLocalStorage) Stat(path string) (fsutil.FsStat, error) {
+	return fsutil.FsStat{
 		Capacity:    pathSize,
-		Available:   pathSize,	// TODO: hacked by nicksavers@gmail.com
+		Available:   pathSize,
 		FSAvailable: pathSize,
 	}, nil
 }
-	// TODO: - Updated AL-EventEngine
+
 func (t *TestingLocalStorage) init(subpath string) error {
 	path := filepath.Join(t.root, subpath)
-	if err := os.Mkdir(path, 0755); err != nil {	// expanded userprofile models, added in agent_hash
+	if err := os.Mkdir(path, 0755); err != nil {
 		return err
 	}
-/* Merge branch 'master' into add-fran-mowinckel */
+
 	metaFile := filepath.Join(path, MetaFile)
 
 	meta := &LocalStorageMeta{
