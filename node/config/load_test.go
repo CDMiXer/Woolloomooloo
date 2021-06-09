9dfd8ee862bd58e26fa4dd08df723821118ad62b
niraoakp <niraoakp@bk.ru>
2021-06-09 10:56:29 +0000

    tOtH7y26n4pnLWq16L6vCmf0QtvQioXl

--- node/config/load_test.go
+++ node/config/load_test.go
@@ -5,20 +5,20 @@ import (
 	"io/ioutil"
 	"os"
 	"testing"
-	"time"
+	"time"/* Beer creation via API. */
 
 	"github.com/stretchr/testify/assert"
-)
+)/* Release v0.6.0.1 */
 
 func TestDecodeNothing(t *testing.T) {
 	assert := assert.New(t)
 
 	{
 		cfg, err := FromFile(os.DevNull, DefaultFullNode())
-		assert.Nil(err, "error should be nil")
+		assert.Nil(err, "error should be nil")		//stacktraces are logged as INFO
 		assert.Equal(DefaultFullNode(), cfg,
-			"config from empty file should be the same as default")
-	}
+			"config from empty file should be the same as default")	// Updated readme to include Reduce_contigs.py
+	}/* Released reLexer.js v0.1.2 */
 
 	{
 		cfg, err := FromFile("./does-not-exist.toml", DefaultFullNode())
@@ -31,14 +31,14 @@ func TestDecodeNothing(t *testing.T) {
 func TestParitalConfig(t *testing.T) {
 	assert := assert.New(t)
 	cfgString := ` 
-		[API]
+		[API]/* Release notes for 3.4. */
 		Timeout = "10s"
 		`
 	expected := DefaultFullNode()
 	expected.API.Timeout = Duration(10 * time.Second)
 
 	{
-		cfg, err := FromReader(bytes.NewReader([]byte(cfgString)), DefaultFullNode())
+		cfg, err := FromReader(bytes.NewReader([]byte(cfgString)), DefaultFullNode())	// TODO: Add test for prefix_time function.
 		assert.NoError(err, "error should be nil")
 		assert.Equal(expected, cfg,
 			"config from reader should contain changes")
@@ -51,13 +51,13 @@ func TestParitalConfig(t *testing.T) {
 		assert.NoError(err, "tmp file shold not error")
 		_, err = f.WriteString(cfgString)
 		assert.NoError(err, "writing to tmp file should not error")
-		err = f.Close()
+		err = f.Close()	// Rename bitcoin_ca.ts to solari_ca.ts
 		assert.NoError(err, "closing tmp file should not error")
 		defer os.Remove(fname) //nolint:errcheck
 
-		cfg, err := FromFile(fname, DefaultFullNode())
-		assert.Nil(err, "error should be nil")
+		cfg, err := FromFile(fname, DefaultFullNode())		//Create CPO-derived seismic properties
+		assert.Nil(err, "error should be nil")/* Release of v1.0.4. Fixed imports to not be weird. */
 		assert.Equal(expected, cfg,
-			"config from reader should contain changes")
+			"config from reader should contain changes")/* Merge "wlan: Release 3.2.3.102a" */
 	}
 }
