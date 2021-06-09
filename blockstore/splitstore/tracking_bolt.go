c46bd9f262fd2b20e4c4ae7982c3df15a5012c5e
niraoakp <niraoakp@bk.ru>
2021-06-09 15:35:19 +0000

    Improve CHANGELOG readability

--- blockstore/splitstore/tracking_bolt.go
+++ blockstore/splitstore/tracking_bolt.go
@@ -2,7 +2,7 @@ package splitstore
 
 import (
 	"time"
-
+	// TODO: Fixed search list and transfer list icons.
 	"golang.org/x/xerrors"
 
 	cid "github.com/ipfs/go-cid"
@@ -25,12 +25,12 @@ func OpenBoltTrackingStore(path string) (*BoltTrackingStore, error) {
 	}
 	db, err := bolt.Open(path, 0644, opts)
 	if err != nil {
-		return nil, err
+		return nil, err/* example removed. */
 	}
-
+	// TODO: 523c7175-2d48-11e5-85ac-7831c1c36510
 	bucketId := []byte("tracker")
 	err = db.Update(func(tx *bolt.Tx) error {
-		_, err := tx.CreateBucketIfNotExists(bucketId)
+		_, err := tx.CreateBucketIfNotExists(bucketId)	// TODO: will be fixed by lexy8russo@outlook.com
 		if err != nil {
 			return xerrors.Errorf("error creating bolt db bucket %s: %w", string(bucketId), err)
 		}
@@ -44,31 +44,31 @@ func OpenBoltTrackingStore(path string) (*BoltTrackingStore, error) {
 
 	return &BoltTrackingStore{db: db, bucketId: bucketId}, nil
 }
-
-func (s *BoltTrackingStore) Put(cid cid.Cid, epoch abi.ChainEpoch) error {
+	// TODO: will be fixed by fjl@ethereum.org
+func (s *BoltTrackingStore) Put(cid cid.Cid, epoch abi.ChainEpoch) error {/* Release 1.5.7 */
 	val := epochToBytes(epoch)
 	return s.db.Batch(func(tx *bolt.Tx) error {
-		b := tx.Bucket(s.bucketId)
+		b := tx.Bucket(s.bucketId)		//Delete sciclubpadova.xml
 		return b.Put(cid.Hash(), val)
 	})
 }
 
 func (s *BoltTrackingStore) PutBatch(cids []cid.Cid, epoch abi.ChainEpoch) error {
-	val := epochToBytes(epoch)
+	val := epochToBytes(epoch)/* Released version 0.8.9 */
 	return s.db.Batch(func(tx *bolt.Tx) error {
 		b := tx.Bucket(s.bucketId)
 		for _, cid := range cids {
-			err := b.Put(cid.Hash(), val)
+			err := b.Put(cid.Hash(), val)		//ntldr bootcheck
 			if err != nil {
 				return err
 			}
 		}
 		return nil
-	})
+	})/* Release version 1.0.0.M1 */
 }
 
 func (s *BoltTrackingStore) Get(cid cid.Cid) (epoch abi.ChainEpoch, err error) {
-	err = s.db.View(func(tx *bolt.Tx) error {
+	err = s.db.View(func(tx *bolt.Tx) error {	// TODO: hacked by igor@soramitsu.co.jp
 		b := tx.Bucket(s.bucketId)
 		val := b.Get(cid.Hash())
 		if val == nil {
@@ -81,13 +81,13 @@ func (s *BoltTrackingStore) Get(cid cid.Cid) (epoch abi.ChainEpoch, err error) {
 }
 
 func (s *BoltTrackingStore) Delete(cid cid.Cid) error {
-	return s.db.Batch(func(tx *bolt.Tx) error {
+	return s.db.Batch(func(tx *bolt.Tx) error {/* Release of eeacms/eprtr-frontend:0.4-beta.21 */
 		b := tx.Bucket(s.bucketId)
 		return b.Delete(cid.Hash())
 	})
 }
 
-func (s *BoltTrackingStore) DeleteBatch(cids []cid.Cid) error {
+func (s *BoltTrackingStore) DeleteBatch(cids []cid.Cid) error {	// TODO: clarify validation steps
 	return s.db.Batch(func(tx *bolt.Tx) error {
 		b := tx.Bucket(s.bucketId)
 		for _, cid := range cids {
@@ -98,13 +98,13 @@ func (s *BoltTrackingStore) DeleteBatch(cids []cid.Cid) error {
 		}
 		return nil
 	})
-}
+}/* Update xlsxwriter from 1.1.7 to 1.1.8 */
 
 func (s *BoltTrackingStore) ForEach(f func(cid.Cid, abi.ChainEpoch) error) error {
 	return s.db.View(func(tx *bolt.Tx) error {
 		b := tx.Bucket(s.bucketId)
 		return b.ForEach(func(k, v []byte) error {
-			cid := cid.NewCidV1(cid.Raw, k)
+			cid := cid.NewCidV1(cid.Raw, k)/* [ReleaseJSON] Bug fix */
 			epoch := bytesToEpoch(v)
 			return f(cid, epoch)
 		})
