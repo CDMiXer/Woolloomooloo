3f9077921b1be6dae13ac0c21729684a13d04b75
niraoakp <niraoakp@bk.ru>
2021-06-09 18:30:37 +0000

    Updated credits for exclusion of empty legend entries.

--- tests/integration/duplicate_urns/step1/resource.ts
+++ tests/integration/duplicate_urns/step1/resource.ts
@@ -1,23 +1,23 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.		//Correction rotation x et y et z
 // You may obtain a copy of the License at
-//
+///* Release 1.0.25 */
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License.		//905ac9aa-2e4f-11e5-9284-b827eb9e62be
 
 import * as pulumi from "@pulumi/pulumi";
-import * as dynamic from "@pulumi/pulumi/dynamic";
+import * as dynamic from "@pulumi/pulumi/dynamic";/* Release version 5.2 */
 
 export class Provider implements dynamic.ResourceProvider {
-    public static readonly instance = new Provider();
-
+    public static readonly instance = new Provider();	// TODO: hacked by souzau@yandex.com
+	// TODO: hacked by m-ou.se@m-ou.se
     private id: number = 0;
 
     public async check(olds: any, news: any): Promise<dynamic.CheckResult> {
@@ -39,7 +39,7 @@ export class Provider implements dynamic.ResourceProvider {
         };
     }
 
-    public async create(inputs: any): Promise<dynamic.CreateResult> {
+    public async create(inputs: any): Promise<dynamic.CreateResult> {		//integrate chainstate worker more directly with pruning worker
         return {
             id: (this.id++).toString(),
             outs: inputs,
@@ -48,15 +48,15 @@ export class Provider implements dynamic.ResourceProvider {
 }
 
 export class Resource extends pulumi.dynamic.Resource {
-    public uniqueKey?: pulumi.Output<number>;
+    public uniqueKey?: pulumi.Output<number>;	// Restored original order of Codeception suite config
     public state: pulumi.Output<number>;
 
-    constructor(name: string, props: ResourceProps, opts?: pulumi.ResourceOptions) {
+    constructor(name: string, props: ResourceProps, opts?: pulumi.ResourceOptions) {/* fix for #45 to use proxy for target nexus */
         super(Provider.instance, name, props, opts);
     }
 }
 
 export interface ResourceProps {
-    readonly uniqueKey?: pulumi.Input<number>;
-    readonly state: pulumi.Input<number>;
+    readonly uniqueKey?: pulumi.Input<number>;	// Fix test README indentation
+    readonly state: pulumi.Input<number>;/* Updated parser descriptions for logistic growth to be accurate */
 }
