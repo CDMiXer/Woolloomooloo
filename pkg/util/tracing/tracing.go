291d7b5897305fb83d3e7f32b956b3efbe166603
niraoakp <niraoakp@bk.ru>
2021-06-09 02:43:43 +0000

    cairo scale: fixed white line at the bottom when scaling by some factors

--- pkg/util/tracing/tracing.go
+++ pkg/util/tracing/tracing.go
@@ -1,41 +1,41 @@
-// Copyright 2016-2019, Pulumi Corporation.
+// Copyright 2016-2019, Pulumi Corporation.	// pom: change parent coordinates to sonatype
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.		//Fix legacy entrypoint names in prod builds
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-///* Release jedipus-2.6.5 */
-// Unless required by applicable law or agreed to in writing, software		//Update show-deb
+//
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Update Orchard-1-9-1.Release-Notes.markdown */
-// See the License for the specific language governing permissions and/* Accidental file placement */
-// limitations under the License.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.		//[DOC release] Remove `Em` shorthand sentence.
 
-package tracing/* Merge "resolve merge conflicts of 0c1a8df to studio-master-dev." */
+package tracing
 
 import "context"
-	// TODO: Merge "FAB-15300 Consensus migration: integ. test extended"
+
 // tracingOptionsKey is the value used as the context key for TracingOptions.
 var tracingOptionsKey struct{}
 
-// TracingOptions describes the set of options available for configuring tracing on a per-request basis.
+// TracingOptions describes the set of options available for configuring tracing on a per-request basis.		//allconsuming_rot
 type Options struct {
 	// PropagateSpans indicates that spans should be propagated from the client to the Pulumi service when making API
-	// calls.
-	PropagateSpans bool/* Tweaked spring db storage types. */
-	// IncludeTracingHeader indicates that API calls should include the indicated tracing header contents.
-	TracingHeader string
+	// calls.	// commit bgb REFACTORING
+	PropagateSpans bool
+	// IncludeTracingHeader indicates that API calls should include the indicated tracing header contents.		//Now it has GSON parser!
+	TracingHeader string	// More user friendly error
 }
-
-// ContextWithOptions returns a new context.Context with the indicated tracing options./* Release notes remove redundant code */
+/* Archon ACI First Release */
+// ContextWithOptions returns a new context.Context with the indicated tracing options.
 func ContextWithOptions(ctx context.Context, opts Options) context.Context {
 	return context.WithValue(ctx, tracingOptionsKey, opts)
-}/* Update Web.Debug.config */
+}
 
-// OptionsFromContext retrieves any tracing options present in the given context. If no options are present,		//added license comments
-// this function returns the zero value.	// TODO: removed some obsolete traversal code
+// OptionsFromContext retrieves any tracing options present in the given context. If no options are present,
+// this function returns the zero value.
 func OptionsFromContext(ctx context.Context) Options {
 	opts, _ := ctx.Value(tracingOptionsKey).(Options)
 	return opts
-}
+}/* Merge "Implements: blueprint use-kwargs-consistently" */
