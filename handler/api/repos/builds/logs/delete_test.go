73fabe303c9c5f03b508d84b47a9479e02e9b30d
niraoakp <niraoakp@bk.ru>
2021-06-09 18:46:45 +0000

    Merge "RefStack should allow associating specific guideline/target."

--- handler/api/repos/builds/logs/delete_test.go
+++ handler/api/repos/builds/logs/delete_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License/* Merge "use already loaded BDM in instance.<action>" */
 // that can be found in the LICENSE file.
-
+	// TODO: added launch config
 package logs
