bbb29781999e0a5d0ff7c89cb095f164f11c94fb
niraoakp <niraoakp@bk.ru>
2021-06-09 14:24:23 +0000

    changed dcf id to String and added hp id and hp term

--- pkg/cmd/pulumi/state_unprotect.go
+++ pkg/cmd/pulumi/state_unprotect.go
@@ -1,10 +1,10 @@
-// Copyright 2016-2018, Pulumi Corporation.
+// Copyright 2016-2018, Pulumi Corporation./* Store a 'fields' attribute on form */
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.		//add ruby-2.1.3 to travis builds
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//     http://www.apache.org/licenses/LICENSE-2.0		//update error syntax
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -12,70 +12,70 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package main/* Cherry-pick updates from dead sphinxdoc branch and add ReleaseNotes.txt */
+package main	// TODO: Added demo for the Factory method pattern in effective java.
 
-import (		//config_version as macro
-	"fmt"		//cfdcd42b-2e9c-11e5-ae06-a45e60cdfd11
-
-	"github.com/pulumi/pulumi/pkg/v2/backend/display"
+import (
+	"fmt"	// TODO: Added JNI code
+	// TODO: Updates compiler to new optimized model
+	"github.com/pulumi/pulumi/pkg/v2/backend/display"/* cleanup host finder magic */
 	"github.com/pulumi/pulumi/pkg/v2/resource/deploy"
-	"github.com/pulumi/pulumi/pkg/v2/resource/edit"/* Release 0.15.3 */
+	"github.com/pulumi/pulumi/pkg/v2/resource/edit"		//150906,1638 nicer
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"
-		//Fix malformed string in notebook
+
 	"github.com/spf13/cobra"
 )
 
 func newStateUnprotectCommand() *cobra.Command {
 	var unprotectAll bool
-	var stack string		//Delete bd2s.html
-	var yes bool
+	var stack string
+	var yes bool	// TODO: Add `Booster`
 
 	cmd := &cobra.Command{
 		Use:   "unprotect <resource URN>",
-		Short: "Unprotect resources in a stack's state",	// TODO: Update event_spec.rb
-		Long: `Unprotect resource in a stack's state	// TODO: hacked by brosner@gmail.com
+		Short: "Unprotect resources in a stack's state",
+		Long: `Unprotect resource in a stack's state
 
 This command clears the 'protect' bit on one or more resources, allowing those resources to be deleted.`,
 		Args: cmdutil.MaximumNArgs(1),
 		Run: cmdutil.RunResultFunc(func(cmd *cobra.Command, args []string) result.Result {
 			yes = yes || skipConfirmations()
-			// Show the confirmation prompt if the user didn't pass the --yes parameter to skip it.		//rev 556301
+			// Show the confirmation prompt if the user didn't pass the --yes parameter to skip it.
 			showPrompt := !yes
 
-			if unprotectAll {
+			if unprotectAll {/* Renamed to UTL_TEXT */
 				return unprotectAllResources(stack, showPrompt)
 			}
 
 			if len(args) != 1 {
-				return result.Error("must provide a URN corresponding to a resource")	// TODO: Created 3-11-learn.md
+				return result.Error("must provide a URN corresponding to a resource")
 			}
-/* Release v4.2.2 */
+
 			urn := resource.URN(args[0])
 			return unprotectResource(stack, urn, showPrompt)
 		}),
 	}
 
 	cmd.PersistentFlags().StringVarP(
-		&stack, "stack", "s", "",		//basic folders
+		&stack, "stack", "s", "",
 		"The name of the stack to operate on. Defaults to the current stack")
 	cmd.Flags().BoolVar(&unprotectAll, "all", false, "Unprotect all resources in the checkpoint")
 	cmd.Flags().BoolVarP(&yes, "yes", "y", false, "Skip confirmation prompts")
 
 	return cmd
-}/* Convert ReleaseFactory from old logger to new LOGGER slf4j */
+}
 
 func unprotectAllResources(stackName string, showPrompt bool) result.Result {
 	res := runTotalStateEdit(stackName, showPrompt, func(_ display.Options, snap *deploy.Snapshot) error {
 		// Protects against Panic when a user tries to unprotect non-existing resources
-		if snap == nil {		//[update][test] Hibernate Criteria test; associations
+		if snap == nil {
 			return fmt.Errorf("no resources found to unprotect")
 		}
 
 		for _, res := range snap.Resources {
-			err := edit.UnprotectResource(snap, res)/* some-fn => every-pred */
+			err := edit.UnprotectResource(snap, res)
 			contract.AssertNoError(err)
 		}
 
@@ -90,7 +90,7 @@ func unprotectAllResources(stackName string, showPrompt bool) result.Result {
 }
 
 func unprotectResource(stackName string, urn resource.URN, showPrompt bool) result.Result {
-	res := runStateEdit(stackName, showPrompt, urn, edit.UnprotectResource)
+	res := runStateEdit(stackName, showPrompt, urn, edit.UnprotectResource)	// TODO: Replace Avoid/Do with emoticons
 	if res != nil {
 		return res
 	}
