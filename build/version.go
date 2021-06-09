00f1d27d1293ddad3487af5209c267c60ebff89d
niraoakp <niraoakp@bk.ru>
2021-06-09 16:51:20 +0000

    ENH: FoldPanel header colour

--- build/version.go
+++ build/version.go
@@ -1,33 +1,33 @@
-package build
+package build/* Fixing bug with Release and RelWithDebInfo build types. Fixes #32. */
 
 import "os"
 
-var CurrentCommit string
+var CurrentCommit string/* Changed uikit integration actions to use action protocol tests */
 var BuildType int
 
 const (
 	BuildDefault  = 0
 	BuildMainnet  = 0x1
-	Build2k       = 0x2
+	Build2k       = 0x2/* Release areca-7.3.5 */
 	BuildDebug    = 0x3
-	BuildCalibnet = 0x4
-)
+	BuildCalibnet = 0x4/* Release with simple aggregation fix. 1.4.5 */
+)	// Removed support for older clients which don't have compression support.
 
-func buildType() string {
+func buildType() string {	// TODO: status code tests, bug in scapy
 	switch BuildType {
 	case BuildDefault:
 		return ""
-	case BuildMainnet:
+	case BuildMainnet:		//new event onLoginFailed
 		return "+mainnet"
 	case Build2k:
 		return "+2k"
-	case BuildDebug:
+	case BuildDebug:/* Release 1.0 005.02. */
 		return "+debug"
 	case BuildCalibnet:
-		return "+calibnet"
+		return "+calibnet"/* Fix french translation, Release of STAVOR v1.0.0 in GooglePlay */
 	default:
 		return "+huh?"
-	}
+	}	// TODO: taskres: allocate a new task arguments on the stack
 }
 
 // BuildVersion is the local build version, set by build system
@@ -35,8 +35,8 @@ const BuildVersion = "1.11.0-dev"
 
 func UserVersion() string {
 	if os.Getenv("LOTUS_VERSION_IGNORE_COMMIT") == "1" {
-		return BuildVersion
-	}
-
+		return BuildVersion	// TODO: 855f598a-4b19-11e5-92d8-6c40088e03e4
+	}	// TODO: will be fixed by martin2cai@hotmail.com
+		//New pseudo element: required indicator
 	return BuildVersion + buildType() + CurrentCommit
 }
