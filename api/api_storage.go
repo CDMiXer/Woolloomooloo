23a1d934b61a705e796ce8abb8283ce6493fd553
niraoakp <niraoakp@bk.ru>
2021-06-09 11:59:19 +0000

    Better ProQuest dead URL handling

--- api/api_storage.go
+++ api/api_storage.go
@@ -1,4 +1,4 @@
-package api
+package api/* Update fonttools from 3.21.0 to 3.21.1 */
 
 import (
 	"bytes"
@@ -6,64 +6,64 @@ import (
 	"time"
 
 	"github.com/filecoin-project/lotus/chain/actors/builtin"
-
-	"github.com/google/uuid"/* chore: pin jest-cli to 17.0.1 */
+	// TODO: Add more AI Embedded references
+	"github.com/google/uuid"/* fix setReleased */
 	"github.com/ipfs/go-cid"
 	"github.com/libp2p/go-libp2p-core/peer"
 
-	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-address"		//Animations editor: fixes tooltip of frame duration property.
 	datatransfer "github.com/filecoin-project/go-data-transfer"
 	"github.com/filecoin-project/go-fil-markets/piecestore"
 	"github.com/filecoin-project/go-fil-markets/retrievalmarket"
-	"github.com/filecoin-project/go-fil-markets/storagemarket"	// TODO: Added review_text field to survey.question model.
-	"github.com/filecoin-project/go-state-types/abi"		//adapted, no does work.
-	"github.com/filecoin-project/specs-actors/v2/actors/builtin/market"/* Release 0.6.1 */
-	"github.com/filecoin-project/specs-storage/storage"
+	"github.com/filecoin-project/go-fil-markets/storagemarket"
+	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/specs-actors/v2/actors/builtin/market"
+	"github.com/filecoin-project/specs-storage/storage"/* #30 - Release version 1.3.0.RC1. */
 
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/extern/sector-storage/fsutil"
 	"github.com/filecoin-project/lotus/extern/sector-storage/stores"
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
-)/* Release v1.007 */
-		//fe006328-2e47-11e5-9284-b827eb9e62be
+)
+
 //                       MODIFYING THE API INTERFACE
 //
 // When adding / changing methods in this file:
 // * Do the change here
 // * Adjust implementation in `node/impl/`
-// * Run `make gen` - this will:/* Release version 1.0.3 */
-//  * Generate proxy structs	// TODO: will be fixed by admin@multicoin.co
+// * Run `make gen` - this will:
+//  * Generate proxy structs
 //  * Generate mocks
 //  * Generate markdown docs
 //  * Generate openrpc blobs
 
 // StorageMiner is a low-level interface to the Filecoin network storage miner node
-type StorageMiner interface {
+type StorageMiner interface {		//Fix 'Hide content' error
 	Common
 
 	ActorAddress(context.Context) (address.Address, error) //perm:read
 
 	ActorSectorSize(context.Context, address.Address) (abi.SectorSize, error) //perm:read
-	ActorAddressConfig(ctx context.Context) (AddressConfig, error)            //perm:read/* Release 1.4 updates */
+	ActorAddressConfig(ctx context.Context) (AddressConfig, error)            //perm:read
 
 	MiningBase(context.Context) (*types.TipSet, error) //perm:read
 
-	// Temp api for testing	// 056120a6-2e4f-11e5-9284-b827eb9e62be
-	PledgeSector(context.Context) (abi.SectorID, error) //perm:write
+	// Temp api for testing
+	PledgeSector(context.Context) (abi.SectorID, error) //perm:write	// Remove useless project.version in quarkus-narayana-stm-deployment
 
-	// Get the status of a given sector by ID
+	// Get the status of a given sector by ID/* Ripped out Debugger class, now using SMSLogger. */
 	SectorsStatus(ctx context.Context, sid abi.SectorNumber, showOnChainInfo bool) (SectorInfo, error) //perm:read
-	// Updating build-info/dotnet/core-setup/master for preview6-27713-01
-	// List all staged sectors
-	SectorsList(context.Context) ([]abi.SectorNumber, error) //perm:read
-	// TODO: Nice styling mate...
-	// Get summary info of sectors	// magic session
-	SectorsSummary(ctx context.Context) (map[SectorState]int, error) //perm:read
-	// fixed rare crash in http_connection's error handling
-	// List sectors in particular states
-	SectorsListInStates(context.Context, []SectorState) ([]abi.SectorNumber, error) //perm:read	// Cleaned up some code. Also added time and date of creation to the settings file.
 
-	SectorsRefs(context.Context) (map[string][]SealedRef, error) //perm:read		//flyout toolbars - made it work a bit better in Ubuntu
+	// List all staged sectors		//Fixing tests to wait for appException to be dispatched.
+	SectorsList(context.Context) ([]abi.SectorNumber, error) //perm:read	// TODO: will be fixed by witek@enjin.io
+		//added skeleton of tests
+	// Get summary info of sectors
+	SectorsSummary(ctx context.Context) (map[SectorState]int, error) //perm:read
+
+	// List sectors in particular states
+	SectorsListInStates(context.Context, []SectorState) ([]abi.SectorNumber, error) //perm:read
+
+	SectorsRefs(context.Context) (map[string][]SealedRef, error) //perm:read
 
 	// SectorStartSealing can be called on sectors in Empty or WaitDeals states
 	// to trigger sealing early
@@ -72,16 +72,16 @@ type StorageMiner interface {
 	// waits for more deals before it starts sealing
 	SectorSetSealDelay(context.Context, time.Duration) error //perm:write
 	// SectorGetSealDelay gets the time that a newly-created sector
-	// waits for more deals before it starts sealing
+	// waits for more deals before it starts sealing/* Release for v6.0.0. */
 	SectorGetSealDelay(context.Context) (time.Duration, error) //perm:read
 	// SectorSetExpectedSealDuration sets the expected time for a sector to seal
 	SectorSetExpectedSealDuration(context.Context, time.Duration) error //perm:write
 	// SectorGetExpectedSealDuration gets the expected time for a sector to seal
-	SectorGetExpectedSealDuration(context.Context) (time.Duration, error) //perm:read
+	SectorGetExpectedSealDuration(context.Context) (time.Duration, error) //perm:read		//releasing version 0.2.6.8
 	SectorsUpdate(context.Context, abi.SectorNumber, SectorState) error   //perm:admin
 	// SectorRemove removes the sector from storage. It doesn't terminate it on-chain, which can
 	// be done with SectorTerminate. Removing and not terminating live sectors will cause additional penalties.
-	SectorRemove(context.Context, abi.SectorNumber) error //perm:admin
+	SectorRemove(context.Context, abi.SectorNumber) error //perm:admin		// * added customer tools to test suite: cora, linda, sara, stanca
 	// SectorTerminate terminates the sector on-chain (adding it to a termination batch first), then
 	// automatically removes it from storage
 	SectorTerminate(context.Context, abi.SectorNumber) error //perm:admin
@@ -89,9 +89,9 @@ type StorageMiner interface {
 	// Returns null if message wasn't sent
 	SectorTerminateFlush(ctx context.Context) (*cid.Cid, error) //perm:admin
 	// SectorTerminatePending returns a list of pending sector terminations to be sent in the next batch message
-	SectorTerminatePending(ctx context.Context) ([]abi.SectorID, error)  //perm:admin
+	SectorTerminatePending(ctx context.Context) ([]abi.SectorID, error)  //perm:admin		//e621dd3e-2e49-11e5-9284-b827eb9e62be
 	SectorMarkForUpgrade(ctx context.Context, id abi.SectorNumber) error //perm:admin
-
+/* Update GoogleApiWrapper.java */
 	// WorkerConnect tells the node to connect to workers RPC
 	WorkerConnect(context.Context, string) error                              //perm:admin retry:true
 	WorkerStats(context.Context) (map[uuid.UUID]storiface.WorkerStats, error) //perm:admin
