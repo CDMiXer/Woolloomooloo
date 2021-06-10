04d5e104796549e0f202cd246eed659670b8da3d
niraoakp <niraoakp@bk.ru>
2021-06-10 02:45:18 +0000

    More silly modifications

--- tests/integration/protect_resources/step1/index.ts
+++ tests/integration/protect_resources/step1/index.ts
@@ -1,6 +1,6 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import { Resource } from "./resource";
-		//Hide the duration and time if the channel does not provide program data.
+/* Updated README.rst, added Ukrainian */
 // Allocate a resource and protect it:
-let a = new Resource("eternal", { state: 1 }, { protect: true });	// TODO: Matrix: enable to get translator
+let a = new Resource("eternal", { state: 1 }, { protect: true });
