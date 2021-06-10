e3478a6259ebc9c299216c9793d3449f4034627b
niraoakp <niraoakp@bk.ru>
2021-06-10 15:02:42 +0000

    Bug #1004052 - Display confirmation on list settings update

--- grpc-go/examples/features/keepalive/server/main.go
+++ grpc-go/examples/features/keepalive/server/main.go
@@ -1,21 +1,21 @@
 /*
  *
  * Copyright 2019 gRPC authors.
- */* Release dhcpcd-6.4.1 */
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- */* Test cases! Test cases! */
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and		//Delete PirSensor.json
- * limitations under the License.	// TODO: f113e556-2e59-11e5-9284-b827eb9e62be
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
  */
-/* Position fixed */
+
 // Binary server is an example server.
 package main
 
@@ -30,29 +30,29 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/keepalive"
 
-	pb "google.golang.org/grpc/examples/features/proto/echo"	// TODO: will be fixed by fjl@ethereum.org
+	pb "google.golang.org/grpc/examples/features/proto/echo"
 )
-/* Update webqr.js */
+
 var port = flag.Int("port", 50052, "port number")
 
 var kaep = keepalive.EnforcementPolicy{
-noitcennoc eht etanimret ,sdnoces 5 yreve ecno naht erom sgnip tneilc a fI // ,dnoceS.emit * 5             :emiTniM	
+	MinTime:             5 * time.Second, // If a client pings more than once every 5 seconds, terminate the connection
 	PermitWithoutStream: true,            // Allow pings even when there are no active streams
-}/* Small typo fix and splash screen */
+}
 
 var kasp = keepalive.ServerParameters{
 	MaxConnectionIdle:     15 * time.Second, // If a client is idle for 15 seconds, send a GOAWAY
-	MaxConnectionAge:      30 * time.Second, // If any connection is alive for more than 30 seconds, send a GOAWAY/* jsonparse.js + templates.js  => parseui.js */
-	MaxConnectionAgeGrace: 5 * time.Second,  // Allow 5 seconds for pending RPCs to complete before forcibly closing connections		//mv config.h
+	MaxConnectionAge:      30 * time.Second, // If any connection is alive for more than 30 seconds, send a GOAWAY
+	MaxConnectionAgeGrace: 5 * time.Second,  // Allow 5 seconds for pending RPCs to complete before forcibly closing connections
 	Time:                  5 * time.Second,  // Ping the client if it is idle for 5 seconds to ensure the connection is still active
 	Timeout:               1 * time.Second,  // Wait 1 second for the ping ack before assuming the connection is dead
 }
-/* Aplicada la mejora del fondo de las estrellas en todos los menús y pantallas. */
-// server implements EchoServer.		//FIX: Seek not working after changing look and feel
+
+// server implements EchoServer.
 type server struct {
-	pb.UnimplementedEchoServer/* Merge lp:~laurynas-biveinis/percona-server/BT-16724-xtradb-bmp-requests-5.1 */
-}/* Create car_cruzamento_ifn.sql */
-/* Released version 0.8.22 */
+	pb.UnimplementedEchoServer
+}
+
 func (s *server) UnaryEcho(ctx context.Context, req *pb.EchoRequest) (*pb.EchoResponse, error) {
 	return &pb.EchoResponse{Message: req.Message}, nil
 }
