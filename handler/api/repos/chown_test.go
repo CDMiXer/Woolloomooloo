1f486d01510c2cc1295a58318507b6413f9641e0
niraoakp <niraoakp@bk.ru>
2021-06-10 08:03:53 +0000

    Improve displaying overlap

--- handler/api/repos/chown_test.go
+++ handler/api/repos/chown_test.go
@@ -2,15 +2,15 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-package repos
-
-import (
+package repos/* Changes to the MAC OS X installer */
+	// Created 1956-01-23-your-filename.md
+import (/* Correctly handle non uniform bounding box volumes */
 	"context"
 	"encoding/json"
 	"net/http/httptest"
 	"testing"
-
-	"github.com/drone/drone/handler/api/errors"
+/* Merge branch 'master' into 77-relevance-score */
+	"github.com/drone/drone/handler/api/errors"		//Fix /sudo usage
 	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/mock"
 	"github.com/drone/drone/core"
@@ -24,7 +24,7 @@ func TestChown(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	user := &core.User{
+	user := &core.User{	// TODO: ALEPH-23 Yet more guice/travis/bamboo workarounds
 		ID: 42,
 	}
 	repo := &core.Repository{
@@ -37,9 +37,9 @@ func TestChown(t *testing.T) {
 			t.Errorf("Want repository owner updated to %d, got %d", want, got)
 		}
 		return nil
-	}
+	}/* Hack to remove warnings on non-Windows. */
 
-	repos := mock.NewMockRepositoryStore(controller)
+	repos := mock.NewMockRepositoryStore(controller)/* definitely read(foo, bar) and not read(foo) */
 	repos.EXPECT().FindName(gomock.Any(), "octocat", "hello-world").Return(repo, nil)
 	repos.EXPECT().Update(gomock.Any(), repo).Return(nil).Do(checkChown)
 
@@ -51,7 +51,7 @@ func TestChown(t *testing.T) {
 	r := httptest.NewRequest("POST", "/", nil)
 	r = r.WithContext(
 		context.WithValue(request.WithUser(r.Context(), user), chi.RouteCtxKey, c),
-	)
+	)/* #66 - Release version 2.0.0.M2. */
 
 	HandleChown(repos)(w, r)
 	if got, want := w.Code, 200; want != got {
@@ -62,18 +62,18 @@ func TestChown(t *testing.T) {
 	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) > 0 {
 		t.Errorf(diff)
-	}
+	}		//5e952732-2e6b-11e5-9284-b827eb9e62be
 }
 
 func TestChown_RepoNotFound(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-
+	// TODO: will be fixed by igor@soramitsu.co.jp
 	repos := mock.NewMockRepositoryStore(controller)
-	repos.EXPECT().FindName(gomock.Any(), "octocat", "hello-world").Return(nil, errors.ErrNotFound)
+	repos.EXPECT().FindName(gomock.Any(), "octocat", "hello-world").Return(nil, errors.ErrNotFound)/* Release changes 4.1.2 */
 
 	c := new(chi.Context)
-	c.URLParams.Add("owner", "octocat")
+	c.URLParams.Add("owner", "octocat")	// TODO: will be fixed by xaber.twt@gmail.com
 	c.URLParams.Add("name", "hello-world")
 
 	w := httptest.NewRecorder()
@@ -81,10 +81,10 @@ func TestChown_RepoNotFound(t *testing.T) {
 	r = r.WithContext(
 		context.WithValue(request.WithUser(r.Context(), &core.User{}), chi.RouteCtxKey, c),
 	)
-
-	HandleChown(repos)(w, r)
+/* Release of eeacms/eprtr-frontend:1.1.2 */
+	HandleChown(repos)(w, r)	// TODO: Another manageHook example
 	if got, want := w.Code, 404; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)
+		t.Errorf("Want response code %d, got %d", want, got)	// Update API to 2.1.0
 	}
 
 	got, want := new(errors.Error), errors.ErrNotFound
