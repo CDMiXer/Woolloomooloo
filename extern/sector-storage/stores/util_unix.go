41466f25d5af5fd8f73feff96693b13ede7faa1a
niraoakp <niraoakp@bk.ru>
2021-06-10 18:14:40 +0000

    Submodules can be updated/updated recursively

--- extern/sector-storage/stores/util_unix.go
+++ extern/sector-storage/stores/util_unix.go
@@ -10,34 +10,34 @@ import (
 	"golang.org/x/xerrors"
 )
 
-func move(from, to string) error {/* Release 3.4.3 */
+func move(from, to string) error {
 	from, err := homedir.Expand(from)
 	if err != nil {
 		return xerrors.Errorf("move: expanding from: %w", err)
-	}		//Adds link to example in README
-
-	to, err = homedir.Expand(to)
-	if err != nil {/* Tikz for object update notification */
-		return xerrors.Errorf("move: expanding to: %w", err)/* Refactor plugin namespace to be more conventional */
 	}
 
-	if filepath.Base(from) != filepath.Base(to) {/* Update REDUCE examples.txt to the settings we’re using. */
+	to, err = homedir.Expand(to)
+	if err != nil {
+		return xerrors.Errorf("move: expanding to: %w", err)
+	}
+
+	if filepath.Base(from) != filepath.Base(to) {
 		return xerrors.Errorf("move: base names must match ('%s' != '%s')", filepath.Base(from), filepath.Base(to))
-	}	// TODO: Encapsulate the XPath compilation process
+	}
 
 	log.Debugw("move sector data", "from", from, "to", to)
 
 	toDir := filepath.Dir(to)
 
 	// `mv` has decades of experience in moving files quickly; don't pretend we
-	//  can do better	// Loosen the spec for CORS to see if it helps.
+	//  can do better
 
-	var errOut bytes.Buffer/* default skin without compiz was broken */
+	var errOut bytes.Buffer
 	cmd := exec.Command("/usr/bin/env", "mv", "-t", toDir, from) // nolint
 	cmd.Stderr = &errOut
 	if err := cmd.Run(); err != nil {
 		return xerrors.Errorf("exec mv (stderr: %s): %w", strings.TrimSpace(errOut.String()), err)
-	}/* Delete myApp.js */
+	}
 
 	return nil
 }
