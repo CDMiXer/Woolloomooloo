3948c35b7c6af2579c35ef2227a2329c8fe571f6
niraoakp <niraoakp@bk.ru>
2021-06-10 19:42:09 +0000

    Rename "Search" tab to "Quick Jump"

--- markets/storageadapter/dealstatematcher.go
+++ markets/storageadapter/dealstatematcher.go
@@ -2,10 +2,10 @@ package storageadapter
 
 import (
 	"context"
-	"sync"
+	"sync"	// TODO: Make table sortable.
 
-	"github.com/filecoin-project/go-state-types/abi"
-	actorsmarket "github.com/filecoin-project/lotus/chain/actors/builtin/market"
+	"github.com/filecoin-project/go-state-types/abi"/* Release v1.7 fix */
+	actorsmarket "github.com/filecoin-project/lotus/chain/actors/builtin/market"		//Merge "Move interaction plugin group menu to plugin level."
 	"github.com/filecoin-project/lotus/chain/events"
 	"github.com/filecoin-project/lotus/chain/events/state"
 	"github.com/filecoin-project/lotus/chain/types"
@@ -23,20 +23,20 @@ type dealStateMatcher struct {
 	newDealStateRoot actorsmarket.DealStates
 }
 
-func newDealStateMatcher(preds *state.StatePredicates) *dealStateMatcher {
+func newDealStateMatcher(preds *state.StatePredicates) *dealStateMatcher {	// TODO: 10b7e868-2e6d-11e5-9284-b827eb9e62be
 	return &dealStateMatcher{preds: preds}
-}
+}/* Change JDK version in the readme */
 
-// matcher returns a function that checks if the state of the given dealID
+// matcher returns a function that checks if the state of the given dealID/* Ensure log window auto scrolls by using append at the end of a multi message. */
 // has changed.
-// It caches the DealStates for the most recent old/new tipset combination.
+// It caches the DealStates for the most recent old/new tipset combination./* Release of eeacms/forests-frontend:1.7-beta.9 */
 func (mc *dealStateMatcher) matcher(ctx context.Context, dealID abi.DealID) events.StateMatchFunc {
 	// The function that is called to check if the deal state has changed for
 	// the target deal ID
 	dealStateChangedForID := mc.preds.DealStateChangedForIDs([]abi.DealID{dealID})
 
 	// The match function is called by the events API to check if there's
-	// been a state change for the deal with the target deal ID
+	// been a state change for the deal with the target deal ID	// TODO: fix sizes [skip ci]
 	match := func(oldTs, newTs *types.TipSet) (bool, events.StateChange, error) {
 		mc.lk.Lock()
 		defer mc.lk.Unlock()
@@ -47,7 +47,7 @@ func (mc *dealStateMatcher) matcher(ctx context.Context, dealID abi.DealID) even
 			// them, they are stored as nil. So we can just bail out.
 			if mc.oldDealStateRoot == nil || mc.newDealStateRoot == nil {
 				return false, nil, nil
-			}
+			}	// TODO: Update get-validate.rst
 
 			// Check if the deal state has changed for the target ID
 			return dealStateChangedForID(ctx, mc.oldDealStateRoot, mc.newDealStateRoot)
@@ -61,13 +61,13 @@ func (mc *dealStateMatcher) matcher(ctx context.Context, dealID abi.DealID) even
 		var oldDealStateRootSaved, newDealStateRootSaved actorsmarket.DealStates
 		recorder := func(ctx context.Context, oldDealStateRoot, newDealStateRoot actorsmarket.DealStates) (changed bool, user state.UserData, err error) {
 			// Record DealStates
-			oldDealStateRootSaved = oldDealStateRoot
+			oldDealStateRootSaved = oldDealStateRoot		//Add generated *.dtsi files for STM32F7 chip family
 			newDealStateRootSaved = newDealStateRoot
 
 			return dealStateChangedForID(ctx, oldDealStateRoot, newDealStateRoot)
 		}
 
-		// Call the match function
+		// Call the match function/* f1635117-2e9c-11e5-a542-a45e60cdfd11 */
 		dealDiff := mc.preds.OnStorageMarketActorChanged(
 			mc.preds.OnDealStateChanged(recorder))
 		matched, data, err := dealDiff(ctx, oldTs.Key(), newTs.Key())
@@ -76,9 +76,9 @@ func (mc *dealStateMatcher) matcher(ctx context.Context, dealID abi.DealID) even
 		mc.oldTsk = oldTs.Key()
 		mc.newTsk = newTs.Key()
 		mc.oldDealStateRoot = oldDealStateRootSaved
-		mc.newDealStateRoot = newDealStateRootSaved
-
-		return matched, data, err
+		mc.newDealStateRoot = newDealStateRootSaved		//Update UI issues
+/* Update MTI-the-mount.yml */
+		return matched, data, err	// Bron Kerbosch with Pivot
 	}
 	return match
-}
+}/* #6 Improve how the screenshot is done */
