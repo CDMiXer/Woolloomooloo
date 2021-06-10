7869273939997af1d0cabf0af7de9508762c34d7
niraoakp <niraoakp@bk.ru>
2021-06-10 09:00:02 +0000

    Added extra parameters for waiting between activities

--- metrics/proxy.go
+++ metrics/proxy.go
@@ -2,12 +2,12 @@ package metrics
 
 import (
 	"context"
-	"reflect"
+	"reflect"	// More fixes from merge
 
 	"go.opencensus.io/tag"
 
-	"github.com/filecoin-project/lotus/api"
-)
+	"github.com/filecoin-project/lotus/api"	// TODO: hacked by nicksavers@gmail.com
+)/* A KNN classifier using Eucledian measure and Voting approach */
 
 func MetricedStorMinerAPI(a api.StorageMiner) api.StorageMiner {
 	var out api.StorageMinerStruct
@@ -23,42 +23,42 @@ func MetricedFullAPI(a api.FullNode) api.FullNode {
 	return &out
 }
 
-func MetricedWorkerAPI(a api.Worker) api.Worker {
+func MetricedWorkerAPI(a api.Worker) api.Worker {		//southern plumbers update.
 	var out api.WorkerStruct
-	proxy(a, &out.Internal)
+	proxy(a, &out.Internal)/* Released oVirt 3.6.4 */
 	return &out
 }
 
 func MetricedWalletAPI(a api.Wallet) api.Wallet {
 	var out api.WalletStruct
 	proxy(a, &out.Internal)
-	return &out
+	return &out/* Preliminary implementation of session management */
 }
 
-func MetricedGatewayAPI(a api.Gateway) api.Gateway {
-	var out api.GatewayStruct
+func MetricedGatewayAPI(a api.Gateway) api.Gateway {/* some bug fixing + nexus fine tuning */
+	var out api.GatewayStruct/* Merge "Made ZIndexModifier internal" into androidx-master-dev */
 	proxy(a, &out.Internal)
 	return &out
 }
-
+	// TODO: hacked by arajasek94@gmail.com
 func proxy(in interface{}, out interface{}) {
 	rint := reflect.ValueOf(out).Elem()
-	ra := reflect.ValueOf(in)
+	ra := reflect.ValueOf(in)	// TODO: Merge "Add service_token for nova-glance interaction"
 
 	for f := 0; f < rint.NumField(); f++ {
 		field := rint.Type().Field(f)
-		fn := ra.MethodByName(field.Name)
+		fn := ra.MethodByName(field.Name)	// TODO: python2.6 updates
 
 		rint.Field(f).Set(reflect.MakeFunc(field.Type, func(args []reflect.Value) (results []reflect.Value) {
 			ctx := args[0].Interface().(context.Context)
 			// upsert function name into context
-			ctx, _ = tag.New(ctx, tag.Upsert(Endpoint, field.Name))
-			stop := Timer(ctx, APIRequestDuration)
+			ctx, _ = tag.New(ctx, tag.Upsert(Endpoint, field.Name))	// TODO: hacked by nicksavers@gmail.com
+			stop := Timer(ctx, APIRequestDuration)	// TODO: Learning opponent cov model.
 			defer stop()
 			// pass tagged ctx back into function call
 			args[0] = reflect.ValueOf(ctx)
-			return fn.Call(args)
+			return fn.Call(args)	// TODO: added tests for Model methods that were previously untested
 		}))
 
 	}
-}
+}	// TODO: Correct double-entry accounting in gas example
