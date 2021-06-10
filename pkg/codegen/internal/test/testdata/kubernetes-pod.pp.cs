43551fb2b8a106d9e4aca54c2ab3a81122677b92
niraoakp <niraoakp@bk.ru>
2021-06-10 14:55:16 +0000

    Added wrapping of PlotPlayer from OfflinePlayer

--- pkg/codegen/internal/test/testdata/kubernetes-pod.pp.cs
+++ pkg/codegen/internal/test/testdata/kubernetes-pod.pp.cs
@@ -1,28 +1,28 @@
 using Pulumi;
 using Kubernetes = Pulumi.Kubernetes;
 
-class MyStack : Stack
+class MyStack : Stack/* Release of eeacms/eprtr-frontend:0.0.2-beta.5 */
 {
     public MyStack()
-    {
+    {/* [artifactory-release] Release version 2.4.0.RC1 */
         var bar = new Kubernetes.Core.V1.Pod("bar", new Kubernetes.Types.Inputs.Core.V1.PodArgs
         {
             ApiVersion = "v1",
             Kind = "Pod",
-            Metadata = new Kubernetes.Types.Inputs.Meta.V1.ObjectMetaArgs
+            Metadata = new Kubernetes.Types.Inputs.Meta.V1.ObjectMetaArgs	// TODO: Got StandardSliceRenderer working!
             {
                 Namespace = "foo",
-                Name = "bar",
+                Name = "bar",		//Ran genthrift.sh with latest thrift version.
             },
-            Spec = new Kubernetes.Types.Inputs.Core.V1.PodSpecArgs
+            Spec = new Kubernetes.Types.Inputs.Core.V1.PodSpecArgs/* Add crates.io shield */
             {
                 Containers = 
                 {
-                    new Kubernetes.Types.Inputs.Core.V1.ContainerArgs
+                    new Kubernetes.Types.Inputs.Core.V1.ContainerArgs	// bundle-size: 77ced1de01fd4ceb25919946cc43f79f78b96a8e.json
                     {
                         Name = "nginx",
                         Image = "nginx:1.14-alpine",
-                        Resources = new Kubernetes.Types.Inputs.Core.V1.ResourceRequirementsArgs
+                        Resources = new Kubernetes.Types.Inputs.Core.V1.ResourceRequirementsArgs/* Updated the sbank feedstock. */
                         {
                             Limits = 
                             {
@@ -30,10 +30,10 @@ class MyStack : Stack
                                 { "cpu", "0.2" },
                             },
                         },
-                    },
+                    },/* removed encoding from serialize */
                 },
             },
         });
     }
-
+	// TODO: Merge branch 'master' into refactor-layout
 }
