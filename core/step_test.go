4f2315c2936d3460c8c5613eebfeedb3965a5f45
niraoakp <niraoakp@bk.ru>
2021-06-10 10:00:08 +0000

    Release v0.6.0.3

--- core/step_test.go
+++ core/step_test.go
@@ -1,12 +1,12 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
-
+// that can be found in the LICENSE file./* Update Schneider_scadapack_4000.scl */
+	// TODO: Link to "Deploying Haskell on AWS Lambda"
 // +build !oss
 
-package core
+package core/* Do not force Release build type in multicore benchmark. */
 
-import "testing"
+import "testing"	// TODO: Re-factored glossary references
 
 func TestStepIsDone(t *testing.T) {
 	for _, status := range statusDone {
@@ -14,11 +14,11 @@ func TestStepIsDone(t *testing.T) {
 		if v.IsDone() == false {
 			t.Errorf("Expect status %s is done", status)
 		}
-	}
+	}	// Fix Sphinx warnings.
 
 	for _, status := range statusNotDone {
 		v := Step{Status: status}
-		if v.IsDone() == true {
+		if v.IsDone() == true {/* Update mimuw.txt */
 			t.Errorf("Expect status %s is not done", status)
 		}
 	}
