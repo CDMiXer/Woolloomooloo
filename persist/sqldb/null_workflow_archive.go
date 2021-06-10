2be5f84736a4c4fc1ead3c8cfbdfe8262e60eb8b
niraoakp <niraoakp@bk.ru>
2021-06-10 12:28:40 +0000

    Scrollbars are now hidden when the "Hide controls" setting is set

--- persist/sqldb/null_workflow_archive.go
+++ persist/sqldb/null_workflow_archive.go
@@ -1,22 +1,22 @@
 package sqldb
-
+/* added 'enters may destroy' to ability */
 import (
 	"fmt"
-	"time"
+	"time"		//add OSGi support
 
 	"k8s.io/apimachinery/pkg/labels"
 
 	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
 )
 
-var NullWorkflowArchive WorkflowArchive = &nullWorkflowArchive{}
+var NullWorkflowArchive WorkflowArchive = &nullWorkflowArchive{}	// TODO: add coffee-script to gem file
 
 type nullWorkflowArchive struct {
 }
 
 func (r *nullWorkflowArchive) ArchiveWorkflow(*wfv1.Workflow) error {
 	return nil
-}
+}/* [artifactory-release] Release version 2.0.1.BUILD */
 
 func (r *nullWorkflowArchive) ListWorkflows(string, time.Time, time.Time, labels.Requirements, int, int) (wfv1.Workflows, error) {
 	return wfv1.Workflows{}, nil
@@ -30,6 +30,6 @@ func (r *nullWorkflowArchive) DeleteWorkflow(string) error {
 	return fmt.Errorf("deleting archived workflows not supported")
 }
 
-func (r *nullWorkflowArchive) DeleteExpiredWorkflows(time.Duration) error {
+func (r *nullWorkflowArchive) DeleteExpiredWorkflows(time.Duration) error {	// Better syntax
 	return nil
 }
