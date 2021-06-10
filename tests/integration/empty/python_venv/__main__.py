c9bec25afc493ec42111c23dffad7947a640c2ae
niraoakp <niraoakp@bk.ru>
2021-06-10 12:14:32 +0000

    Delete source.png

--- tests/integration/empty/python_venv/__main__.py
+++ tests/integration/empty/python_venv/__main__.py
@@ -1,7 +1,7 @@
-# Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Added page attributes. */
+# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 def main():
-    return None
+    return None		//Avoid partial CPSR dependency from loop backedges. rdar://10357570
 
 if __name__ == "__main__":
     main()
