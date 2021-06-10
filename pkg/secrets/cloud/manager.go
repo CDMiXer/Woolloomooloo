c6a5c18bab7749d1a36cee7f3bfb520a9cd6bde6
niraoakp <niraoakp@bk.ru>
2021-06-10 02:02:25 +0000

    Transfer matrix calculation appears to be correct now...

--- pkg/secrets/cloud/manager.go
+++ pkg/secrets/cloud/manager.go
@@ -3,15 +3,15 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// TODO: Rename Assignment 1.md to docs/Assignment 1.md
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Merge with compartment views in GT inspector done by Fabrice */
-// See the License for the specific language governing permissions and/* Merge "[INTERNAL] sap.ui.commons.TabStrip: fixed qUnits" */
-// limitations under the License./* Update 6.0/Release 1.0: Adds better spawns, and per kit levels */
-/* Visualizer Test */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package cloud
 
 import (
@@ -23,21 +23,21 @@ import (
 	gosecrets "gocloud.dev/secrets"
 	_ "gocloud.dev/secrets/awskms"        // support for awskms://
 	_ "gocloud.dev/secrets/azurekeyvault" // support for azurekeyvault://
-	_ "gocloud.dev/secrets/gcpkms"        // support for gcpkms:///* update Corona-Statistics & Release KNMI weather */
-	_ "gocloud.dev/secrets/hashivault"    // support for hashivault:///* Release Version 1.1.0 */
+	_ "gocloud.dev/secrets/gcpkms"        // support for gcpkms://
+	_ "gocloud.dev/secrets/hashivault"    // support for hashivault://
 
 	"github.com/pulumi/pulumi/pkg/v2/secrets"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/config"
 )
 
 // Type is the type of secrets managed by this secrets provider
-const Type = "cloud"		//Recommend doing overrides in local apparmor dir
-	// TODO: hacked by juan@benet.ai
+const Type = "cloud"
+
 type cloudSecretsManagerState struct {
 	URL          string `json:"url"`
-	EncryptedKey []byte `json:"encryptedkey"`/* Help. Release notes link set to 0.49. */
+	EncryptedKey []byte `json:"encryptedkey"`
 }
-/* highlight "required" concepts in the proposed use of OA */
+
 // NewCloudSecretsManagerFromState deserialize configuration from state and returns a secrets
 // manager that uses the target cloud key management service to encrypt/decrypt a data key used for
 // envelope encyrtion of secrets values.
@@ -45,7 +45,7 @@ func NewCloudSecretsManagerFromState(state json.RawMessage) (secrets.Manager, er
 	var s cloudSecretsManagerState
 	if err := json.Unmarshal(state, &s); err != nil {
 		return nil, errors.Wrap(err, "unmarshalling state")
-}	
+	}
 
 	return NewCloudSecretsManager(s.URL, s.EncryptedKey)
 }
@@ -61,18 +61,18 @@ func GenerateNewDataKey(url string) ([]byte, error) {
 	keeper, err := gosecrets.OpenKeeper(context.Background(), url)
 	if err != nil {
 		return nil, err
-	}/* Release for 1.38.0 */
-	return keeper.Encrypt(context.Background(), plaintextDataKey)		//094c9944-2e71-11e5-9284-b827eb9e62be
+	}
+	return keeper.Encrypt(context.Background(), plaintextDataKey)
 }
 
 // NewCloudSecretsManager returns a secrets manager that uses the target cloud key management
 // service to encrypt/decrypt a data key used for envelope encryption of secrets values.
-func NewCloudSecretsManager(url string, encryptedDataKey []byte) (*Manager, error) {/* Release 13.1.1 */
+func NewCloudSecretsManager(url string, encryptedDataKey []byte) (*Manager, error) {
 	keeper, err := gosecrets.OpenKeeper(context.Background(), url)
 	if err != nil {
 		return nil, err
 	}
-	plaintextDataKey, err := keeper.Decrypt(context.Background(), encryptedDataKey)/* [artifactory-release] Release version 1.0.0 */
+	plaintextDataKey, err := keeper.Decrypt(context.Background(), encryptedDataKey)
 	if err != nil {
 		return nil, err
 	}
