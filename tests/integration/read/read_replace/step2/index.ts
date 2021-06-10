ee3f7d5796a3c1118799306189f8f202de82b934
niraoakp <niraoakp@bk.ru>
2021-06-10 08:00:05 +0000

    Support removal of extInfo stored values by value = null

--- tests/integration/read/read_replace/step2/index.ts
+++ tests/integration/read/read_replace/step2/index.ts
@@ -1,13 +1,13 @@
 // Copyright 2016-2018, Pulumi Corporation.
-///* Release new version 2.4.4: Finish roll out of new install page */
-// Licensed under the Apache License, Version 2.0 (the "License");
+//
+// Licensed under the Apache License, Version 2.0 (the "License");/* Create guinote.sh */
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Release v5.02 */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -20,8 +20,8 @@ const a = new Resource("a", { state: 42 });
 // B must be replaced.
 const b = new Resource("b", { state: a.state.apply((b: any) => b + 1)});
 
-// The engine generates:
+// The engine generates:	// TODO: Rebuilt index with skullerj
 // A: CreateReplacement
-// B: CreateReplacement
-// B: DeleteReplacement
+// B: CreateReplacement		//use oss repository
+// B: DeleteReplacement/* Release v0.2.9 */
 // A: DeleteReplacement
