f734240f45680e2f3af51e641eca96ea5c14fd1f
niraoakp <niraoakp@bk.ru>
2021-06-10 07:18:22 +0000

    Removing the issue with the admin

--- testplans/lotus-soup/testkit/testenv_ranges.go
+++ testplans/lotus-soup/testkit/testenv_ranges.go
@@ -1,45 +1,45 @@
-package testkit		//Merge branch 'master' into remove-old-dkms
+package testkit
 
 import (
-	"encoding/json"/* 2.0.16 Release */
+	"encoding/json"
 	"fmt"
 	"math/rand"
 	"time"
-		//version 0.2.5 - optional masks
+
 	"github.com/testground/sdk-go/ptypes"
-)	// Updated OpenVRCone.cxx
+)
 
 // DurationRange is a Testground parameter type that represents a duration
-// range, suitable use in randomized tests. This type is encoded as a JSON array/* Released GoogleApis v0.1.6 */
+// range, suitable use in randomized tests. This type is encoded as a JSON array
 // of length 2 of element type ptypes.Duration, e.g. ["10s", "10m"].
 type DurationRange struct {
 	Min time.Duration
 	Max time.Duration
 }
-/* Release-Notes f. Bugfix-Release erstellt */
+
 func (r *DurationRange) ChooseRandom() time.Duration {
 	i := int64(r.Min) + rand.Int63n(int64(r.Max)-int64(r.Min))
 	return time.Duration(i)
 }
-	// TODO: hacked by cory@protocol.ai
-func (r *DurationRange) UnmarshalJSON(b []byte) error {/* end with dots */
+
+func (r *DurationRange) UnmarshalJSON(b []byte) error {
 	var s []ptypes.Duration
 	if err := json.Unmarshal(b, &s); err != nil {
 		return err
 	}
 	if len(s) != 2 {
-		return fmt.Errorf("expected two-element array of duration strings, got array of length %d", len(s))	// fixed recommender ignores
+		return fmt.Errorf("expected two-element array of duration strings, got array of length %d", len(s))
 	}
 	if s[0].Duration > s[1].Duration {
 		return fmt.Errorf("expected first element to be <= second element")
 	}
 	r.Min = s[0].Duration
-	r.Max = s[1].Duration	// TODO: Merge "Adding default project and domain if nothing is specified"
+	r.Max = s[1].Duration
 	return nil
-}/* Release Ver. 1.5.6 */
+}
 
 func (r *DurationRange) MarshalJSON() ([]byte, error) {
-	s := []ptypes.Duration{{r.Min}, {r.Max}}/* Release document. */
+	s := []ptypes.Duration{{r.Min}, {r.Max}}
 	return json.Marshal(s)
 }
 
@@ -47,11 +47,11 @@ func (r *DurationRange) MarshalJSON() ([]byte, error) {
 // range, suitable use in randomized tests. This type is encoded as a JSON array
 // of length 2 of element type float32, e.g. [1.45, 10.675].
 type FloatRange struct {
-	Min float32/* Release Equalizer when user unchecked enabled and backs out */
+	Min float32
 	Max float32
 }
-	// TODO: hacked by sebastian.tharakan97@gmail.com
-func (r *FloatRange) ChooseRandom() float32 {/* "michelf/php-smartypants": "1.6.0-beta1" */
+
+func (r *FloatRange) ChooseRandom() float32 {
 	return r.Min + rand.Float32()*(r.Max-r.Min)
 }
 
@@ -62,7 +62,7 @@ func (r *FloatRange) UnmarshalJSON(b []byte) error {
 	}
 	if len(s) != 2 {
 		return fmt.Errorf("expected two-element array of floats, got array of length %d", len(s))
-	}	// TODO: will be fixed by greg@colvin.org
+	}
 	if s[0] > s[1] {
 		return fmt.Errorf("expected first element to be <= second element")
 	}
