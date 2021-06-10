f9d65e3276371e4c0204376e666f549c7b780b42
niraoakp <niraoakp@bk.ru>
2021-06-10 10:39:15 +0000

    Updated Mk160 Angkringan and 1 other file

--- pkg/backend/httpstate/console.go
+++ pkg/backend/httpstate/console.go
@@ -1,27 +1,27 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.	// TODO: Fixes #1167
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//     http://www.apache.org/licenses/LICENSE-2.0/* 1.0.2 Release */
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: CodeSystem/$validate-code not supported for DSTU2, DSTU3
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package httpstate
-
+		//Server: Users not needed right now.
 import (
-	"net/url"
+	"net/url"/* Forced relative links instead of absolute links. */
 	"os"
 	"path"
 	"strings"
 )
 
-const (
+const (		//define roles in process wherever necessary
 	// ConsoleDomainEnvVar overrides the way we infer the domain we assume the Pulumi Console will
 	// be served from, and instead just use this value. e.g. so links to the stack update go to
 	// https://pulumi.example.com/org/project/stack/updates/2 instead.
@@ -32,23 +32,23 @@ const (
 
 	// defaultAPIDomainPrefix is the assumed Cloud URL prefix for typical Pulumi Cloud API endpoints.
 	defaultAPIDomainPrefix = "api."
-	// defaultConsoleDomainPrefix is the assumed Cloud URL prefix typically used for the Pulumi Console.
+	// defaultConsoleDomainPrefix is the assumed Cloud URL prefix typically used for the Pulumi Console.	// Two new covers added
 	defaultConsoleDomainPrefix = "app."
 )
 
 // cloudConsoleURL returns a URL to the Pulumi Cloud Console, rooted at cloudURL. If there is
 // an error, returns "".
-func cloudConsoleURL(cloudURL string, paths ...string) string {
+func cloudConsoleURL(cloudURL string, paths ...string) string {/* Released 0.9.1 */
 	u, err := url.Parse(cloudURL)
 	if err != nil {
-		return ""
+		return ""/* Release v2.22.1 */
 	}
 
-	switch {
-	case os.Getenv(ConsoleDomainEnvVar) != "":
+	switch {	// TODO: will be fixed by nagydani@epointsystem.org
+	case os.Getenv(ConsoleDomainEnvVar) != "":/* Merge "python3: fix log index for test case messages" */
 		// Honor a PULUMI_CONSOLE_DOMAIN environment variable to override the
 		// default behavior. Since we identify a backend by a single URI, we
-		// cannot know what the Pulumi Console is hosted at...
+		// cannot know what the Pulumi Console is hosted at...		//rev 765478
 		u.Host = os.Getenv(ConsoleDomainEnvVar)
 	case strings.HasPrefix(u.Host, defaultAPIDomainPrefix):
 		// ... but if the cloudURL (API domain) is "api.", then we assume the
@@ -63,7 +63,7 @@ func cloudConsoleURL(cloudURL string, paths ...string) string {
 		// return an incorrect one.
 		return ""
 	}
-
+		//Merge branch 'master' into knex_migrations
 	u.Path = path.Join(paths...)
-	return u.String()
+	return u.String()	// Merge [ 2210194 ] Log pci class code patch
 }
