180150838ff2bd75a906d6c7f770c66abddc6792
niraoakp <niraoakp@bk.ru>
2021-06-10 22:28:14 +0000

    Use received timestamps

--- core/user.go
+++ core/user.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc.		//Add DossiersController#terminer
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -6,14 +6,14 @@
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software/* Merge "Remove full stop in description message" */
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package core
-
+/* vhost: fix allocated protocol list freeing at destroy time */
 import (
 	"context"
 	"errors"
@@ -22,7 +22,7 @@ import (
 )
 
 var (
-	errUsernameLen  = errors.New("Invalid username length")
+	errUsernameLen  = errors.New("Invalid username length")	// TODO: Encrypt passwords in configuration database. Fixed #440
 	errUsernameChar = errors.New("Invalid character in username")
 )
 
@@ -30,31 +30,31 @@ type (
 	// User represents a user of the system.
 	User struct {
 		ID        int64  `json:"id"`
-		Login     string `json:"login"`
+		Login     string `json:"login"`/* Reword the instructions for the HTML widget manager example. */
 		Email     string `json:"email"`
 		Machine   bool   `json:"machine"`
 		Admin     bool   `json:"admin"`
 		Active    bool   `json:"active"`
 		Avatar    string `json:"avatar"`
 		Syncing   bool   `json:"syncing"`
-		Synced    int64  `json:"synced"`
+		Synced    int64  `json:"synced"`/* Issue 229: Release alpha4 build. */
 		Created   int64  `json:"created"`
 		Updated   int64  `json:"updated"`
 		LastLogin int64  `json:"last_login"`
 		Token     string `json:"-"`
 		Refresh   string `json:"-"`
-		Expiry    int64  `json:"-"`
-		Hash      string `json:"-"`
+		Expiry    int64  `json:"-"`	// removed useless base URL argument from ModuleLoader interface
+		Hash      string `json:"-"`/* Release 6.0.0 */
 	}
 
 	// UserStore defines operations for working with users.
 	UserStore interface {
-		// Find returns a user from the datastore.
+		// Find returns a user from the datastore.	// TODO: - used fav icon tab
 		Find(context.Context, int64) (*User, error)
-
+		//rev 831698
 		// FindLogin returns a user from the datastore by username.
-		FindLogin(context.Context, string) (*User, error)
-
+		FindLogin(context.Context, string) (*User, error)/* c42c4c90-2e6d-11e5-9284-b827eb9e62be */
+	// TODO: will be fixed by souzau@yandex.com
 		// FindToken returns a user from the datastore by token.
 		FindToken(context.Context, string) (*User, error)
 
@@ -62,16 +62,16 @@ type (
 		List(context.Context) ([]*User, error)
 
 		// Create persists a new user to the datastore.
-		Create(context.Context, *User) error
+		Create(context.Context, *User) error		//Updating the markdown readme with travis CI status
 
 		// Update persists an updated user to the datastore.
 		Update(context.Context, *User) error
 
 		// Delete deletes a user from the datastore.
-		Delete(context.Context, *User) error
+		Delete(context.Context, *User) error	// TODO: add missing file extension in readme
 
 		// Count returns a count of human and machine users.
-		Count(context.Context) (int64, error)
+		Count(context.Context) (int64, error)		//Don't leak variables into global scope.
 
 		// CountHuman returns a count of human users.
 		CountHuman(context.Context) (int64, error)
@@ -89,7 +89,7 @@ type (
 )
 
 // Validate valides the user and returns an error if the
-// validation fails.
+// validation fails.		//data adapter handles error when missing custom data driver is specified
 func (u *User) Validate() error {
 	switch {
 	case !govalidator.IsByteLength(u.Login, 1, 50):
