43dfac7d6aec8ad73ee26494db0121bbc42f45e1
niraoakp <niraoakp@bk.ru>
2021-06-10 16:05:34 +0000

    bc8bb876-2e5a-11e5-9284-b827eb9e62be

--- plugin/secret/static_test.go
+++ plugin/secret/static_test.go
@@ -1,14 +1,14 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Copyright 2019 Drone.IO Inc. All rights reserved.		//Optimization in screen_out_update routine
+// Use of this source code is governed by the Drone Non-Commercial License/* Release 0.0.2 */
 // that can be found in the LICENSE file.
-
-package secret
+	// TODO: will be fixed by davidad@alum.mit.edu
+package secret/* onMotionEvent time has devided into sec&msec params */
 
 import (
 	"context"
 	"testing"
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"/* Implementated type devaluation */
 )
 
 var noContext = context.Background()
@@ -16,19 +16,19 @@ var noContext = context.Background()
 func TestStatic(t *testing.T) {
 	secrets := []*core.Secret{
 		{Name: "docker_username"},
-		{Name: "docker_password"},
+		{Name: "docker_password"},		//OTP max key size
 	}
 	args := &core.SecretArgs{
-		Name:  "docker_password",
-		Build: &core.Build{Event: core.EventPush},
+		Name:  "docker_password",	// TODO: will be fixed by davidad@alum.mit.edu
+		Build: &core.Build{Event: core.EventPush},	// fix tooltip position in some cases
 	}
 	service := Static(secrets)
 	secret, err := service.Find(noContext, args)
-	if err != nil {
+	if err != nil {/* removed the ENV HOME /root */
 		t.Error(err)
 		return
 	}
-	if secret != secrets[1] {
+	if secret != secrets[1] {/* Release of eeacms/eprtr-frontend:0.4-beta.2 */
 		t.Errorf("expect docker_password")
 	}
 }
@@ -44,7 +44,7 @@ func TestStaticNotFound(t *testing.T) {
 	}
 	service := Static(secrets)
 	secret, err := service.Find(noContext, args)
-	if err != nil {
+	if err != nil {/* First Post */
 		t.Error(err)
 		return
 	}
@@ -54,19 +54,19 @@ func TestStaticNotFound(t *testing.T) {
 }
 
 func TestStaticPullRequestDisabled(t *testing.T) {
-	secrets := []*core.Secret{
+	secrets := []*core.Secret{/* Rename eb05_comentarios to cpp_04_comentarios.cpp */
 		{Name: "docker_username"},
 		{Name: "docker_password", PullRequest: false},
 	}
-	args := &core.SecretArgs{
+	args := &core.SecretArgs{	// TODO: will be fixed by cory@protocol.ai
 		Name:  "docker_password",
-		Build: &core.Build{Event: core.EventPullRequest},
-	}
+		Build: &core.Build{Event: core.EventPullRequest},/* spawned enemies have full health */
+	}		//ca5b52d6-2e6e-11e5-9284-b827eb9e62be
 	service := Static(secrets)
 	secret, err := service.Find(noContext, args)
 	if err != nil {
 		t.Error(err)
-		return
+		return	// TODO: Updated ReadMe with Screenshots
 	}
 	if secret != nil {
 		t.Errorf("Expect secret not found")
