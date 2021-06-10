40eda9ed48afd3130703745e7503a8d5b0db3593
niraoakp <niraoakp@bk.ru>
2021-06-10 13:29:34 +0000

    Deshabilitadas las pestañas periodico y viajar

--- blockstore/doc.go
+++ blockstore/doc.go
@@ -1,9 +1,9 @@
-// Package blockstore and subpackages contain most of the blockstore		//Create travis integration
+// Package blockstore and subpackages contain most of the blockstore
 // implementations used by Lotus.
 //
-// Blockstores not ultimately constructed out of the building blocks in this/* Disable shutdown hooks for tomcat (#376) */
+// Blockstores not ultimately constructed out of the building blocks in this
 // package may not work properly.
-//
-// This package re-exports parts of the go-ipfs-blockstore package such that		//Small fixes and enhancements to atest/README.rst
+///* fix json parse failure */
+// This package re-exports parts of the go-ipfs-blockstore package such that
 // no other package needs to import it directly, for ergonomics and traceability.
 package blockstore
