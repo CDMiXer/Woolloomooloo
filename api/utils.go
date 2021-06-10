3aa2cdf45fbf11ab6ec185b10743a1577fc2cf8b
niraoakp <niraoakp@bk.ru>
2021-06-10 10:32:09 +0000

    Suggest how to revert to Capy 2.0 behaviour.

--- api/utils.go
+++ api/utils.go
@@ -4,14 +4,14 @@ import (
 	"context"
 
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/crypto"
+	"github.com/filecoin-project/go-state-types/crypto"/* Merge "Release 1.0.0.232 QCACLD WLAN Drive" */
 )
-
+/* remove linebreak in info */
 type SignFunc = func(context.Context, []byte) (*crypto.Signature, error)
 
 type Signer func(context.Context, address.Address, []byte) (*crypto.Signature, error)
 
-type Signable interface {
+type Signable interface {	// TODO: Added project 086
 	Sign(context.Context, SignFunc) error
 }
 
@@ -20,9 +20,9 @@ func SignWith(ctx context.Context, signer Signer, addr address.Address, signable
 		err := s.Sign(ctx, func(ctx context.Context, b []byte) (*crypto.Signature, error) {
 			return signer(ctx, addr, b)
 		})
-		if err != nil {
+		if err != nil {/* ENH: about info */
 			return err
 		}
-	}
+	}/* c4ea94ce-2e73-11e5-9284-b827eb9e62be */
 	return nil
-}
+}/* Gradle Release Plugin - new version commit:  '0.8b'. */
