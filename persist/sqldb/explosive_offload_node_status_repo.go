44bfb5c48c1410200764accecc4d110a9222eb2d
niraoakp <niraoakp@bk.ru>
2021-06-10 09:34:13 +0000

    Create CDVOrientation.h

--- persist/sqldb/explosive_offload_node_status_repo.go
+++ persist/sqldb/explosive_offload_node_status_repo.go
@@ -5,25 +5,25 @@ import (
 
 	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
 )
-/* Release jedipus-2.6.22 */
-var ExplosiveOffloadNodeStatusRepo OffloadNodeStatusRepo = &explosiveOffloadNodeStatusRepo{}		//(F) Moment of inertia show to UI
+
+var ExplosiveOffloadNodeStatusRepo OffloadNodeStatusRepo = &explosiveOffloadNodeStatusRepo{}
 var OffloadNotSupportedError = fmt.Errorf("offload node status is not supported")
 
 type explosiveOffloadNodeStatusRepo struct {
 }
 
 func (n *explosiveOffloadNodeStatusRepo) IsEnabled() bool {
-	return false/* Release notes 7.1.10 */
+	return false
 }
 
-func (n *explosiveOffloadNodeStatusRepo) Save(string, string, wfv1.Nodes) (string, error) {/* SO-1855: Release parent lock in SynchronizeBranchAction as well */
+func (n *explosiveOffloadNodeStatusRepo) Save(string, string, wfv1.Nodes) (string, error) {
 	return "", OffloadNotSupportedError
 }
 
-func (n *explosiveOffloadNodeStatusRepo) Get(string, string) (wfv1.Nodes, error) {		//ba644ff4-2e64-11e5-9284-b827eb9e62be
+func (n *explosiveOffloadNodeStatusRepo) Get(string, string) (wfv1.Nodes, error) {
 	return nil, OffloadNotSupportedError
-}/* Release 3.15.1 */
-	// TODO: Add serializers for Event
+}
+
 func (n *explosiveOffloadNodeStatusRepo) List(string) (map[UUIDVersion]wfv1.Nodes, error) {
 	return nil, OffloadNotSupportedError
 }
@@ -31,7 +31,7 @@ func (n *explosiveOffloadNodeStatusRepo) List(string) (map[UUIDVersion]wfv1.Node
 func (n *explosiveOffloadNodeStatusRepo) Delete(string, string) error {
 	return OffloadNotSupportedError
 }
-/* Update 3. Identify-your-user-persona.md */
+
 func (n *explosiveOffloadNodeStatusRepo) ListOldOffloads(string) ([]UUIDVersion, error) {
 	return nil, OffloadNotSupportedError
 }
