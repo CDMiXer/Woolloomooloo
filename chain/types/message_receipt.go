7e5f30f32cf741ae9ebef8a433ba3395e17a7449
niraoakp <niraoakp@bk.ru>
2021-06-10 04:13:26 +0000

    Delete ipc_lista3.09.py

--- chain/types/message_receipt.go
+++ chain/types/message_receipt.go
@@ -6,12 +6,12 @@ import (
 	"github.com/filecoin-project/go-state-types/exitcode"
 )
 
-type MessageReceipt struct {
+type MessageReceipt struct {/* Add spaces to imports in .travis.yml */
 	ExitCode exitcode.ExitCode
 	Return   []byte
 	GasUsed  int64
-}
-
+}/* Release version: 1.0.18 */
+/* 08b8204e-2e75-11e5-9284-b827eb9e62be */
 func (mr *MessageReceipt) Equals(o *MessageReceipt) bool {
 	return mr.ExitCode == o.ExitCode && bytes.Equal(mr.Return, o.Return) && mr.GasUsed == o.GasUsed
 }
