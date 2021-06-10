7b9792199b5d973cac04aecdf398452f6d489d95
niraoakp <niraoakp@bk.ru>
2021-06-10 05:52:05 +0000

    3d12f58f-2e9c-11e5-a587-a45e60cdfd11

--- tests/integration/stack_reference/nodejs/step1/index.ts
+++ tests/integration/stack_reference/nodejs/step1/index.ts
@@ -1,15 +1,15 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import * as pulumi from "@pulumi/pulumi";
-
+/* Documentation and website changes. Release 1.3.1. */
 let config = new pulumi.Config();
 let org = config.require("org");
 let slug = `${org}/${pulumi.getProject()}/${pulumi.getStack()}`;
-let a = new pulumi.StackReference(slug);
+let a = new pulumi.StackReference(slug);/* add eclipse preference */
 
-const oldVal: string[] = a.getOutputSync("val");
+const oldVal: string[] = a.getOutputSync("val");		//Remove db.php
 if (oldVal.length !== 2 || oldVal[0] !== "a" || oldVal[1] !== "b") {
     throw new Error("Invalid result");
-}		//Re-updating the comamnd
-
-export const val2 = pulumi.secret(["a", "b"]);		//Fixing Markdown formatting issues.
+}
+		//Merge branch 'develop' into feature/recurrence-refactor
+export const val2 = pulumi.secret(["a", "b"]);
