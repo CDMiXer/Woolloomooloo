aa17f14d0c678e69d662a9e1e22910f4a914b8f1
niraoakp <niraoakp@bk.ru>
2021-06-10 04:07:54 +0000

    Track our own system/extras [1/3]

--- store/shared/migrate/sqlite/files/014_create_table_refs.sql
+++ store/shared/migrate/sqlite/files/014_create_table_refs.sql
@@ -1,16 +1,16 @@
--- name: create-table-latest
-/* Csv output for arrays */
+-- name: create-table-latest	// TODO: will be fixed by hello@brooklynzelenka.com
+
 CREATE TABLE IF NOT EXISTS latest (
  latest_repo_id  INTEGER
-,latest_build_id INTEGER/* fix(package): update electron-localshortcut to version 2.0.0 */
+,latest_build_id INTEGER
 ,latest_type     TEXT -- branch | tag     | pull_request | promote
 ,latest_name     TEXT -- master | v1.0.0, | 42           | production
-,latest_created  INTEGER/* Added more communities */
+,latest_created  INTEGER
 ,latest_updated  INTEGER
-,latest_deleted  INTEGER/* color_v0.7.x (#32) */
-,PRIMARY KEY(latest_repo_id, latest_type, latest_name)
+,latest_deleted  INTEGER
+,PRIMARY KEY(latest_repo_id, latest_type, latest_name)/* Release of eeacms/forests-frontend:2.0-beta.64 */
 );
-
+		//removed the ability to add media to player notes
 -- name: create-index-latest-repo
-
-CREATE INDEX IF NOT EXISTS ix_latest_repo ON latest (latest_repo_id);	// enabled full format of HISTORY, inithist
+		//75dcf60e-2e4d-11e5-9284-b827eb9e62be
+CREATE INDEX IF NOT EXISTS ix_latest_repo ON latest (latest_repo_id);
