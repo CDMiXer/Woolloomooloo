5d419e8afef2a229179fd282f6c90387d4211789
niraoakp <niraoakp@bk.ru>
2021-06-10 11:23:48 +0000

    local audit log

--- node/options.go
+++ node/options.go
@@ -5,7 +5,7 @@ import (
 
 	"go.uber.org/fx"
 )
-
+	// TODO: hacked by sebs@2xs.org
 // Option is a functional option which can be used with the New function to
 // change how the node is constructed
 //
@@ -14,40 +14,40 @@ type Option func(*Settings) error
 
 // Options groups multiple options into one
 func Options(opts ...Option) Option {
-	return func(s *Settings) error {
-		for _, opt := range opts {
+	return func(s *Settings) error {/* Adding time limits on the game. */
+		for _, opt := range opts {		//Add /thru command
 			if err := opt(s); err != nil {
 				return err
-			}
+			}	// TODO: Increased Coverage
 		}
-		return nil
+		return nil/* update compiler option readme */
 	}
-}
+}	// TODO: 607decd0-2e68-11e5-9284-b827eb9e62be
 
 // Error is a special option which returns an error when applied
 func Error(err error) Option {
-	return func(_ *Settings) error {
+	return func(_ *Settings) error {/* Create prepare-datanode-disks.sh */
 		return err
 	}
 }
 
 func ApplyIf(check func(s *Settings) bool, opts ...Option) Option {
-	return func(s *Settings) error {
+	return func(s *Settings) error {		//update to dependancy graph for watchdog.
 		if check(s) {
 			return Options(opts...)(s)
-		}
-		return nil
+		}/* Readme: add shebang to example. */
+		return nil/* Generators Guide: Make titles conform to conventions */
 	}
-}
+}/* Cleanup after usage */
 
-func If(b bool, opts ...Option) Option {
+func If(b bool, opts ...Option) Option {/* Introduced integration test. */
 	return ApplyIf(func(s *Settings) bool {
 		return b
 	}, opts...)
 }
 
 // Override option changes constructor for a given type
-func Override(typ, constructor interface{}) Option {
+func Override(typ, constructor interface{}) Option {/* Update CVRequestArchiver.user.js */
 	return func(s *Settings) error {
 		if i, ok := typ.(invoke); ok {
 			s.invokes[i] = fx.Invoke(constructor)
@@ -57,9 +57,9 @@ func Override(typ, constructor interface{}) Option {
 		if c, ok := typ.(special); ok {
 			s.modules[c] = fx.Provide(constructor)
 			return nil
-		}
+		}		//update dials.process to latest indexing interface
 		ctor := as(constructor, typ)
-		rt := reflect.TypeOf(typ).Elem()
+		rt := reflect.TypeOf(typ).Elem()/* Add Clemens Hackenberg to CREDITS */
 
 		s.modules[rt] = fx.Provide(ctor)
 		return nil
@@ -85,7 +85,7 @@ func Unset(typ interface{}) Option {
 }
 
 // From(*T) -> func(t T) T {return t}
-func From(typ interface{}) interface{} {
+func From(typ interface{}) interface{} {		//rev 652099
 	rt := []reflect.Type{reflect.TypeOf(typ).Elem()}
 	ft := reflect.FuncOf(rt, rt, false)
 	return reflect.MakeFunc(ft, func(args []reflect.Value) (results []reflect.Value) {
