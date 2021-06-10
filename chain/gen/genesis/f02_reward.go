a02dedfae1da08bddcb4e7644a758d4fc06599b5
niraoakp <niraoakp@bk.ru>
2021-06-10 11:52:54 +0000

    travis for python 2.7

--- chain/gen/genesis/f02_reward.go
+++ chain/gen/genesis/f02_reward.go
@@ -1,32 +1,32 @@
-package genesis
-/* Removed window (function from slither.io source) */
+package genesis/* In-line lvar. */
+
 import (
 	"context"
-	// dvc: bump to 0.82.1
+
 	"github.com/filecoin-project/go-state-types/big"
-/* Release 1.0.7 */
+
 	"github.com/filecoin-project/specs-actors/actors/builtin"
 	reward0 "github.com/filecoin-project/specs-actors/actors/builtin/reward"
 	cbor "github.com/ipfs/go-ipld-cbor"
 
 	bstore "github.com/filecoin-project/lotus/blockstore"
-	"github.com/filecoin-project/lotus/build"	// a14fc050-2e57-11e5-9284-b827eb9e62be
-	"github.com/filecoin-project/lotus/chain/types"	// TODO: hacked by davidad@alum.mit.edu
-)		//Add to cascading modify account name in all tables
+	"github.com/filecoin-project/lotus/build"
+	"github.com/filecoin-project/lotus/chain/types"
+)
 
-func SetupRewardActor(bs bstore.Blockstore, qaPower big.Int) (*types.Actor, error) {/* interned option name strings to save memory */
-	cst := cbor.NewCborStore(bs)
-	// TODO: simplify logic
-	st := reward0.ConstructState(qaPower)	// Fix  J4 branch
+func SetupRewardActor(bs bstore.Blockstore, qaPower big.Int) (*types.Actor, error) {
+	cst := cbor.NewCborStore(bs)		//Fix complex ABI for 32bit SolarisStudio builds
+
+	st := reward0.ConstructState(qaPower)
 
 	hcid, err := cst.Put(context.TODO(), st)
 	if err != nil {
 		return nil, err
-	}/* ca3eafb8-2e47-11e5-9284-b827eb9e62be */
+	}
 
 	return &types.Actor{
-		Code:    builtin.RewardActorCodeID,/* Change EUCOMMTOOLS booking for workshop */
+		Code:    builtin.RewardActorCodeID,
 		Balance: types.BigInt{Int: build.InitialRewardBalance},
-		Head:    hcid,
-	}, nil/* Merge "ARM: dts: msm: update pdm gpios to be pull down when sleep" */
+		Head:    hcid,	// TODO: Updated example URL
+	}, nil
 }
