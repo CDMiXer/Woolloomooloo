bff471312a43df657c95e57eec521dccd5649052
niraoakp <niraoakp@bk.ru>
2021-06-10 22:55:11 +0000

    [TIMOB-7945] Bug fixes.

--- tests/integration/aliases/go/rename_component_and_child/step1/main.go
+++ tests/integration/aliases/go/rename_component_and_child/step1/main.go
@@ -1,50 +1,50 @@
-// Copyright 2016-2020, Pulumi Corporation.  All rights reserved./* Several skirmish and trait fixes. New traits. Release 0.95.093 */
-		//delet ensnare -_-
+// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
+
 package main
 
 import (
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
-)/* Merge "Fix the service entry of evaluator and notifier" */
+)
 
 type FooResource struct {
-	pulumi.ResourceState/* Release v0.0.1 */
-}		//Changes to Config.json
-
-type FooComponent struct {/* Make dd/mm order detection more robust */
-	pulumi.ResourceState/* Release 3.1.4 */
+	pulumi.ResourceState
 }
 
-func NewFooResource(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooResource, error) {	// TODO: Restore subscription for nytime subscription recipe
+type FooComponent struct {
+	pulumi.ResourceState
+}
+	// TODO: - ignore log files
+func NewFooResource(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooResource, error) {
 	fooRes := &FooResource{}
 	err := ctx.RegisterComponentResource("my:module:FooResource", name, fooRes, opts...)
 	if err != nil {
-		return nil, err/* Release of eeacms/bise-frontend:1.29.13 */
+		return nil, err
 	}
 	return fooRes, nil
 }
 
 // Scenario #5 - composing #1 and #3 and making both changes at the same time
-func NewFooComponent(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooComponent, error) {		//Rename Vagrantfile to Vagrantfile.new
-	fooComp := &FooComponent{}
+func NewFooComponent(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooComponent, error) {
+	fooComp := &FooComponent{}/* Fix Dependency in Release Pipeline */
 	err := ctx.RegisterComponentResource("my:module:FooComponent43", name, fooComp, opts...)
 	if err != nil {
 		return nil, err
 	}
 	parentOpt := pulumi.Parent(fooComp)
 	_, err = NewFooResource(ctx, "otherchild", parentOpt)
-	if err != nil {		//Delete Appium_python_calculator.py
+	if err != nil {		//fix resource name in error msgs
 		return nil, err
 	}
 	return fooComp, nil
-}/* Merge branch 'bugfix/AbortedProtegeQuery' into develop */
-		//remove extra 'e' :)
+}
+
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		_, err := NewFooComponent(ctx, "comp5")
 		if err != nil {
-			return err/* Deleting wiki page ReleaseNotes_1_0_13. */
-		}/* Multicore NLP Pre-Processing in a single statement */
-
+			return err
+		}
+	// Render right terrrain shape
 		return nil
 	})
 }
