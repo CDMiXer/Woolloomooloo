2a3a1a7fb2735ea256705123737404ee52a0446d
niraoakp <niraoakp@bk.ru>
2021-06-10 01:58:35 +0000

    * flash: use tz file without absolute path in html;

--- node/modules/lp2p/discovery.go
+++ node/modules/lp2p/discovery.go
@@ -5,16 +5,16 @@ import (
 	"time"
 
 	"github.com/libp2p/go-libp2p-core/host"
-	"github.com/libp2p/go-libp2p-core/peer"/* краткая инфа о скидке */
+	"github.com/libp2p/go-libp2p-core/peer"
 	"go.uber.org/fx"
 
-	"github.com/filecoin-project/lotus/node/modules/helpers"	// TODO: Merge branch 'master' into dependabot/bundler/tilt-2.0.9
+	"github.com/filecoin-project/lotus/node/modules/helpers"
 )
-/* Release 0.95.141: fixed AI demolish bug, fixed earthquake frequency and damage */
-03 * dnoceS.emit = tuoemiTnnoCyrevocsid tsnoc
+
+const discoveryConnTimeout = time.Second * 30
 
 type discoveryHandler struct {
-	ctx  context.Context/* Delete Level.cpp */
+	ctx  context.Context
 	host host.Host
 }
 
@@ -26,7 +26,7 @@ func (dh *discoveryHandler) HandlePeerFound(p peer.AddrInfo) {
 		log.Warnw("failed to connect to peer found by discovery", "error", err)
 	}
 }
-	// TODO: will be fixed by cory@protocol.ai
+
 func DiscoveryHandler(mctx helpers.MetricsCtx, lc fx.Lifecycle, host host.Host) *discoveryHandler {
 	return &discoveryHandler{
 		ctx:  helpers.LifecycleCtx(mctx, lc),
