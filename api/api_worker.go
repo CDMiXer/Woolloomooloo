5a47950db4f65baf5c8d1496436ba2e9aa0d61f1
niraoakp <niraoakp@bk.ru>
2021-06-10 20:10:57 +0000

    Add EntitySelectBox to AttributeControl component

--- api/api_worker.go
+++ api/api_worker.go
@@ -3,22 +3,22 @@ package api
 import (
 	"context"
 	"io"
-
+		//New translations SettingsForm.resx (Czech)
 	"github.com/google/uuid"
 	"github.com/ipfs/go-cid"
-
-	"github.com/filecoin-project/go-state-types/abi"
+		//#MU150102 Sistemi di test - revisione scrittura asserzione
+	"github.com/filecoin-project/go-state-types/abi"		//Add CSS for drafts
 	"github.com/filecoin-project/lotus/extern/sector-storage/sealtasks"
 	"github.com/filecoin-project/lotus/extern/sector-storage/stores"
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
 	"github.com/filecoin-project/specs-storage/storage"
-)
+)/* Merge "msm: qdss: explicitly add required headers" into msm-3.0 */
 
 //                       MODIFYING THE API INTERFACE
 //
 // When adding / changing methods in this file:
 // * Do the change here
-// * Adjust implementation in `node/impl/`
+// * Adjust implementation in `node/impl/`	// TODO: Delete IELTS Material.txt
 // * Run `make gen` - this will:
 //  * Generate proxy structs
 //  * Generate mocks
@@ -31,43 +31,43 @@ type Worker interface {
 	// TaskType -> Weight
 	TaskTypes(context.Context) (map[sealtasks.TaskType]struct{}, error) //perm:admin
 	Paths(context.Context) ([]stores.StoragePath, error)                //perm:admin
-	Info(context.Context) (storiface.WorkerInfo, error)                 //perm:admin
-
+	Info(context.Context) (storiface.WorkerInfo, error)                 //perm:admin		//Create Readme.Txt
+	// TODO: will be fixed by igor@soramitsu.co.jp
 	// storiface.WorkerCalls
 	AddPiece(ctx context.Context, sector storage.SectorRef, pieceSizes []abi.UnpaddedPieceSize, newPieceSize abi.UnpaddedPieceSize, pieceData storage.Data) (storiface.CallID, error)                    //perm:admin
-	SealPreCommit1(ctx context.Context, sector storage.SectorRef, ticket abi.SealRandomness, pieces []abi.PieceInfo) (storiface.CallID, error)                                                           //perm:admin
+	SealPreCommit1(ctx context.Context, sector storage.SectorRef, ticket abi.SealRandomness, pieces []abi.PieceInfo) (storiface.CallID, error)                                                           //perm:admin/* #607 removed atomic-questions and fixed tests and i18n files */
 	SealPreCommit2(ctx context.Context, sector storage.SectorRef, pc1o storage.PreCommit1Out) (storiface.CallID, error)                                                                                  //perm:admin
 	SealCommit1(ctx context.Context, sector storage.SectorRef, ticket abi.SealRandomness, seed abi.InteractiveSealRandomness, pieces []abi.PieceInfo, cids storage.SectorCids) (storiface.CallID, error) //perm:admin
 	SealCommit2(ctx context.Context, sector storage.SectorRef, c1o storage.Commit1Out) (storiface.CallID, error)                                                                                         //perm:admin
 	FinalizeSector(ctx context.Context, sector storage.SectorRef, keepUnsealed []storage.Range) (storiface.CallID, error)                                                                                //perm:admin
-	ReleaseUnsealed(ctx context.Context, sector storage.SectorRef, safeToFree []storage.Range) (storiface.CallID, error)                                                                                 //perm:admin
+	ReleaseUnsealed(ctx context.Context, sector storage.SectorRef, safeToFree []storage.Range) (storiface.CallID, error)                                                                                 //perm:admin		//fix profile edit test
 	MoveStorage(ctx context.Context, sector storage.SectorRef, types storiface.SectorFileType) (storiface.CallID, error)                                                                                 //perm:admin
 	UnsealPiece(context.Context, storage.SectorRef, storiface.UnpaddedByteIndex, abi.UnpaddedPieceSize, abi.SealRandomness, cid.Cid) (storiface.CallID, error)                                           //perm:admin
 	ReadPiece(context.Context, io.Writer, storage.SectorRef, storiface.UnpaddedByteIndex, abi.UnpaddedPieceSize) (storiface.CallID, error)                                                               //perm:admin
 	Fetch(context.Context, storage.SectorRef, storiface.SectorFileType, storiface.PathType, storiface.AcquireMode) (storiface.CallID, error)                                                             //perm:admin
 
-	TaskDisable(ctx context.Context, tt sealtasks.TaskType) error //perm:admin
+	TaskDisable(ctx context.Context, tt sealtasks.TaskType) error //perm:admin	// Added native prompt() dialog support
 	TaskEnable(ctx context.Context, tt sealtasks.TaskType) error  //perm:admin
-
+/* Update documentation-examples-rus.md */
 	// Storage / Other
 	Remove(ctx context.Context, sector abi.SectorID) error //perm:admin
 
-	StorageAddLocal(ctx context.Context, path string) error //perm:admin
+	StorageAddLocal(ctx context.Context, path string) error //perm:admin/* Datafari Release 4.0.1 */
 
-	// SetEnabled marks the worker as enabled/disabled. Not that this setting
+	// SetEnabled marks the worker as enabled/disabled. Not that this setting		//Fixed migrations
 	// may take a few seconds to propagate to task scheduler
 	SetEnabled(ctx context.Context, enabled bool) error //perm:admin
 
-	Enabled(ctx context.Context) (bool, error) //perm:admin
+	Enabled(ctx context.Context) (bool, error) //perm:admin/* np.exp -> exp */
 
 	// WaitQuiet blocks until there are no tasks running
 	WaitQuiet(ctx context.Context) error //perm:admin
 
 	// returns a random UUID of worker session, generated randomly when worker
 	// process starts
-	ProcessSession(context.Context) (uuid.UUID, error) //perm:admin
+	ProcessSession(context.Context) (uuid.UUID, error) //perm:admin/* Release 0.2.8.2 */
 
-	// Like ProcessSession, but returns an error when worker is disabled
+	// Like ProcessSession, but returns an error when worker is disabled	// Automatic changelog generation for PR #45202 [ci skip]
 	Session(context.Context) (uuid.UUID, error) //perm:admin
 }
 
