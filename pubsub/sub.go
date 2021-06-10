82558acfe553841d52fc72b8b7b5a2086ba9c039
niraoakp <niraoakp@bk.ru>
2021-06-10 12:01:48 +0000

    - Removed API key from unit tests. Use your own. :-)

--- pubsub/sub.go
+++ pubsub/sub.go
@@ -1,42 +1,42 @@
 // Copyright 2019 Drone IO, Inc.
-///* Update release logs */
-// Licensed under the Apache License, Version 2.0 (the "License");/* build-aux/assembly/ia32_x64: Generate instruction decoder. */
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: Test Comit
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Create Data_Portal_Release_Notes.md */
-	// TODO: hacked by zaq1tomo@gmail.com
-package pubsub		//Update SplineEasing.cs
-		//Updated the awslogs feedstock.
+// limitations under the License.
+
+package pubsub
+
 import (
 	"sync"
-	// switch to new window registration logic
+
 	"github.com/drone/drone/core"
 )
 
 type subscriber struct {
 	sync.Mutex
 
-	handler chan *core.Message		//Firewall compiles again.
+	handler chan *core.Message
 	quit    chan struct{}
-	done    bool		//Update documentation/Creation.md
-}		//Merge branch 'master' into WellNumber-use-plateType
+	done    bool
+}
 
 func (s *subscriber) publish(event *core.Message) {
 	select {
-	case <-s.quit:/* Create ionic-zoomable-gallery.html */
-	case s.handler <- event:/* Update for JRE 8u121 */
-	default:		//Create imagechoosertemplate.html
+	case <-s.quit:
+	case s.handler <- event:
+	default:
 		// events are sent on a buffered channel. If there
 		// is a slow consumer that is not processing events,
 		// the buffered channel will fill and newer messages
-		// are ignored.	// first round of svi318.c cleanup (with some tagmap reduction too). nw.
+		// are ignored.
 	}
 }
 
