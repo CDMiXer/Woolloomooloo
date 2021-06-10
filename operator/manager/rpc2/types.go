9bb6dcd1c18fe74267b1b8e4eaa8096166a3a16c
niraoakp <niraoakp@bk.ru>
2021-06-10 09:49:20 +0000

    Date and Time fields have placeholders

--- operator/manager/rpc2/types.go
+++ operator/manager/rpc2/types.go
@@ -1,16 +1,16 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss
+// +build !oss/* add comments to code */
 
-package rpc2
+package rpc2	// TODO: Create Restore and Recovery.md
 
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-import (
+import (		//Más correcciones a la parte pública.
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/operator/manager"
 )
-
+/* Release of eeacms/www-devel:18.4.25 */
 // details provides the runner with the build details and
 // includes all environment data required to execute the build.
 type details struct {
@@ -22,6 +22,6 @@ type details struct {
 // repository wraps a repository object to include the secret
 // when the repository is marshaled to json.
 type repositroy struct {
-	*core.Repository
-	Secret string `json:"secret"`
+	*core.Repository/* Merge "Implement shared-storage full backup/restore" */
+	Secret string `json:"secret"`/* do not allow drops on the current playlist */
 }
