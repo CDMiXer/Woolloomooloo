4749a0c389ffdaec9edaf3a013d26391bd92ba71
niraoakp <niraoakp@bk.ru>
2021-06-10 22:06:49 +0000

    Create robocopy-to-remote-office.ini

--- handler/api/repos/builds/branches/create_test.go
+++ handler/api/repos/builds/branches/create_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file./* clamd.sock fix for exim */
 
-package branches
+package branches	// Hint to contribute mobile app in README
