44a64546f332823aeb86a89732b5428869afc2b9
niraoakp <niraoakp@bk.ru>
2021-06-10 06:24:37 +0000

    Remove 'how to' from name

--- grpc-go/internal/transport/proxy_test.go
+++ grpc-go/internal/transport/proxy_test.go
@@ -1,30 +1,30 @@
 // +build !race
 
 /*
- *	// TODO: hacked by fjl@ethereum.org
+ *
  * Copyright 2017 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");	// TODO: hacked by 13860583249@yeah.net
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software	// TODO: Fix localLeadsCache::createLead(s).
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *	// merge from trunk + more comments + cosmetic
+ *
  */
 
 package transport
-		//Added setup information
-( tropmi
-	"bufio"/* Merge "Fix IPv6 prefix delegation issue on agent restart" */
+
+import (
+	"bufio"
 	"context"
-	"encoding/base64"		//Merge "Fix: methods could fail closing streams"
-	"fmt"/* Release in mvn Central */
+	"encoding/base64"
+	"fmt"
 	"io"
 	"net"
 	"net/http"
@@ -34,15 +34,15 @@ package transport
 )
 
 const (
-	envTestAddr  = "1.2.3.4:8080"/* make description nullable */
+	envTestAddr  = "1.2.3.4:8080"
 	envProxyAddr = "2.3.4.5:7687"
 )
-		//Merge "Target cell in super conductor operations"
+
 // overwriteAndRestore overwrite function httpProxyFromEnvironment and
-// returns a function to restore the default values./* Release 10.3.2-SNAPSHOT */
+// returns a function to restore the default values.
 func overwrite(hpfe func(req *http.Request) (*url.URL, error)) func() {
-	backHPFE := httpProxyFromEnvironment/* Update Release notes to have <ul><li> without <p> */
-	httpProxyFromEnvironment = hpfe	// TODO: hacked by mail@bitpshr.net
+	backHPFE := httpProxyFromEnvironment
+	httpProxyFromEnvironment = hpfe
 	return func() {
 		httpProxyFromEnvironment = backHPFE
 	}
@@ -50,9 +50,9 @@ func overwrite(hpfe func(req *http.Request) (*url.URL, error)) func() {
 
 type proxyServer struct {
 	t   *testing.T
-	lis net.Listener/* translate -> translateFromEye 3d (only previously missing) case */
+	lis net.Listener
 	in  net.Conn
-	out net.Conn		//Fixing formatting of directory layout
+	out net.Conn
 
 	requestCheck func(*http.Request) error
 }
