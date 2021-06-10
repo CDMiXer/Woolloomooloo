4fabce217f1fc6278ee0e05dc26edbf7c1975922
niraoakp <niraoakp@bk.ru>
2021-06-10 11:45:23 +0000

    Eliminadas excepciones

--- tests/integration/protect_resources/step5/index.ts
+++ tests/integration/protect_resources/step5/index.ts
@@ -1,5 +1,5 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+/* brew-cask formula updated in README */
 import { Resource } from "./resource";
-/* Spring security + login page initial version */
+
 // Finally, we can delete this thing.
