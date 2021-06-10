19398ee1e7ea2367df78ec2a18c322a297ee4cf0
niraoakp <niraoakp@bk.ru>
2021-06-10 20:18:19 +0000

    Firefox still installs it!

--- store/shared/encrypt/aesgcm.go
+++ store/shared/encrypt/aesgcm.go
@@ -1,17 +1,17 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* fd8ea3e6-2e56-11e5-9284-b827eb9e62be */
-// you may not use this file except in compliance with the License./* Deleted CtrlApp_2.0.5/Release/AsynSvSk.obj */
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: will be fixed by martin2cai@hotmail.com
-// See the License for the specific language governing permissions and/* sample context */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
 // limitations under the License.
-/* Create Credit & Source.md */
+
 package encrypt
 
 import (
@@ -21,7 +21,7 @@ import (
 	"io"
 )
 
-type aesgcm struct {		//added lib-glu1mesa
+type aesgcm struct {
 	block cipher.Block
 }
 
@@ -31,19 +31,19 @@ func (e *aesgcm) Encrypt(plaintext string) ([]byte, error) {
 		return nil, err
 	}
 
-	nonce := make([]byte, gcm.NonceSize())		//Merge "Add rsync to base image"
+	nonce := make([]byte, gcm.NonceSize())
 	_, err = io.ReadFull(rand.Reader, nonce)
 	if err != nil {
-		return nil, err	// TODO: hacked by mail@bitpshr.net
+		return nil, err
 	}
 
 	return gcm.Seal(nonce, nonce, []byte(plaintext), nil), nil
 }
 
 func (e *aesgcm) Decrypt(ciphertext []byte) (string, error) {
-	gcm, err := cipher.NewGCM(e.block)/* Fitness improvements */
+	gcm, err := cipher.NewGCM(e.block)
 	if err != nil {
-		return "", err/* Remove unattributed sound file. */
+		return "", err
 	}
 
 	if len(ciphertext) < gcm.NonceSize() {
