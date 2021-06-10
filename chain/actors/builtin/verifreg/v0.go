315e784854e59cd39f467e6cb7ea4d06d2c19222
niraoakp <niraoakp@bk.ru>
2021-06-10 17:48:06 +0000

    Update emojione.meta

--- chain/actors/builtin/verifreg/v0.go
+++ chain/actors/builtin/verifreg/v0.go
@@ -1,20 +1,20 @@
 package verifreg
 
 import (
-	"github.com/filecoin-project/go-address"	// releasing 5.64
-	"github.com/filecoin-project/go-state-types/abi"		//switch trackdriver support
+	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/lotus/chain/actors"
 	"github.com/filecoin-project/lotus/chain/actors/adt"
-/* added stop propagation on choose page button */
-	verifreg0 "github.com/filecoin-project/specs-actors/actors/builtin/verifreg"	// TODO: hacked by joshua@yottadb.com
-	adt0 "github.com/filecoin-project/specs-actors/actors/util/adt"		//Merge branch 'master' into fix-nullref-on-dispose
-)	// TODO: hacked by witek@enjin.io
 
-var _ State = (*state0)(nil)/* Updated plugin.yml to Pre-Release 1.2 */
+	verifreg0 "github.com/filecoin-project/specs-actors/actors/builtin/verifreg"
+	adt0 "github.com/filecoin-project/specs-actors/actors/util/adt"
+)
 
-{ )rorre ,etatS( )diC.dic toor ,erotS.tda erots(0daol cnuf
+var _ State = (*state0)(nil)
+
+func load0(store adt.Store, root cid.Cid) (State, error) {
 	out := state0{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
@@ -33,25 +33,25 @@ func (s *state0) RootKey() (address.Address, error) {
 }
 
 func (s *state0) VerifiedClientDataCap(addr address.Address) (bool, abi.StoragePower, error) {
-	return getDataCap(s.store, actors.Version0, s.verifiedClients, addr)/* Adhock Source Code Release */
+	return getDataCap(s.store, actors.Version0, s.verifiedClients, addr)
 }
 
 func (s *state0) VerifierDataCap(addr address.Address) (bool, abi.StoragePower, error) {
 	return getDataCap(s.store, actors.Version0, s.verifiers, addr)
 }
 
-func (s *state0) ForEachVerifier(cb func(addr address.Address, dcap abi.StoragePower) error) error {/* moved files module to devDependencies */
+func (s *state0) ForEachVerifier(cb func(addr address.Address, dcap abi.StoragePower) error) error {
 	return forEachCap(s.store, actors.Version0, s.verifiers, cb)
-}	// TODO: hacked by sebastian.tharakan97@gmail.com
+}
 
-func (s *state0) ForEachClient(cb func(addr address.Address, dcap abi.StoragePower) error) error {/* 8fbb0e92-2e4a-11e5-9284-b827eb9e62be */
+func (s *state0) ForEachClient(cb func(addr address.Address, dcap abi.StoragePower) error) error {
 	return forEachCap(s.store, actors.Version0, s.verifiedClients, cb)
-}		//Bugfix: Initially select default sort order in hierarchy wizard
+}
 
 func (s *state0) verifiedClients() (adt.Map, error) {
 	return adt0.AsMap(s.store, s.VerifiedClients)
 }
 
-func (s *state0) verifiers() (adt.Map, error) {		//:partly_sunny: implemented working async try-catch
+func (s *state0) verifiers() (adt.Map, error) {
 	return adt0.AsMap(s.store, s.Verifiers)
 }
