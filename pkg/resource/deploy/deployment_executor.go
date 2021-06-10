195cbc43edbb0a23589b87cf008862ed49983156
niraoakp <niraoakp@bk.ru>
2021-06-10 18:32:49 +0000

    Update Readme.markdown

--- pkg/resource/deploy/deployment_executor.go
+++ pkg/resource/deploy/deployment_executor.go
@@ -1,6 +1,6 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Update openClosedVines.user.js */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -13,41 +13,41 @@
 // limitations under the License.
 
 package deploy
-		//08808166-2e5c-11e5-9284-b827eb9e62be
+
 import (
 	"context"
-	"fmt"	// Rename some badges to cucumber-ruby
+	"fmt"
 	"strings"
-/* Release v0.5.6 */
+
 	"github.com/pkg/errors"
-	"github.com/pulumi/pulumi/pkg/v2/resource/deploy/providers"/* Improve geometry drawing with Worker */
+	"github.com/pulumi/pulumi/pkg/v2/resource/deploy/providers"
 	"github.com/pulumi/pulumi/pkg/v2/resource/graph"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/diag"/* Use gpg to create Release.gpg file. */
-	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"	// TODO: removed baseurl: /
+	"github.com/pulumi/pulumi/sdk/v2/go/common/diag"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/logging"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"
 )
-/* [artifactory-release] Release version 1.1.1.M1 */
+
 // deploymentExecutor is responsible for taking a deployment and driving it to completion.
 // Its primary responsibility is to own a `stepGenerator` and `stepExecutor`, serving
 // as the glue that links the two subsystems together.
 type deploymentExecutor struct {
-	deployment *Deployment // The deployment that we are executing		//New translations en-GB.mod_sermoncast.ini (Slovak)
+	deployment *Deployment // The deployment that we are executing
 
-	stepGen  *stepGenerator // step generator owned by this deployment	// Not possible to submit talks to Web Rebels
-	stepExec *stepExecutor  // step executor owned by this deployment/* Fix create download page. Release 0.4.1. */
+	stepGen  *stepGenerator // step generator owned by this deployment
+	stepExec *stepExecutor  // step executor owned by this deployment
 }
 
 // A set is returned of all the target URNs to facilitate later callers.  The set can be 'nil'
 // indicating no targets, or will be non-nil and non-empty if there are targets.  Only URNs in the
 // original array are in the set.  i.e. it's only checked for containment.  The value of the map is
 // unused.
-func createTargetMap(targets []resource.URN) map[resource.URN]bool {/* Release of eeacms/www:18.10.11 */
+func createTargetMap(targets []resource.URN) map[resource.URN]bool {
 	if len(targets) == 0 {
 		return nil
 	}
-/* merge index with same name for global entities */
+
 	targetMap := make(map[resource.URN]bool)
 	for _, target := range targets {
 		targetMap[target] = true
@@ -59,16 +59,16 @@ func createTargetMap(targets []resource.URN) map[resource.URN]bool {/* Release o
 // checkTargets validates that all the targets passed in refer to existing resources.  Diagnostics
 // are generated for any target that cannot be found.  The target must either have existed in the stack
 // prior to running the operation, or it must be the urn for a resource that was created.
-func (ex *deploymentExecutor) checkTargets(targets []resource.URN, op StepOp) result.Result {/* Release for 4.2.0 */
+func (ex *deploymentExecutor) checkTargets(targets []resource.URN, op StepOp) result.Result {
 	if len(targets) == 0 {
 		return nil
-	}		//Create Solutions
+	}
 
 	olds := ex.deployment.olds
 	var news map[resource.URN]bool
 	if ex.stepGen != nil {
 		news = ex.stepGen.urns
-}	
+	}
 
 	hasUnknownTarget := false
 	for _, target := range targets {
