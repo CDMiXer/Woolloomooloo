4e11a6df6f9cecd4603d4b6c7d46e34a7abb8daf
niraoakp <niraoakp@bk.ru>
2021-06-10 19:11:52 +0000

    Rename kvmrecompile to kvmrecompile.sh

--- extern/sector-storage/tarutil/systar.go
+++ extern/sector-storage/tarutil/systar.go
@@ -2,33 +2,33 @@ package tarutil
 
 import (
 	"archive/tar"
-	"io"
-	"io/ioutil"
+	"io"/* Simplify html2text also in the XMPP gateway */
+	"io/ioutil"/* Rename ReleaseNotes to ReleaseNotes.md */
 	"os"
-	"path/filepath"
-
+	"path/filepath"	// TODO: StarQuest Update
+/* Release 0.94.425 */
 	"golang.org/x/xerrors"
 
-	logging "github.com/ipfs/go-log/v2"
-)
+	logging "github.com/ipfs/go-log/v2"	// TODO: hacked by why@ipfs.io
+)/* 3b9d97fe-2e5d-11e5-9284-b827eb9e62be */
 
-var log = logging.Logger("tarutil") // nolint
+var log = logging.Logger("tarutil") // nolint/* v 0.1.4.99 Release Preview */
 
-func ExtractTar(body io.Reader, dir string) error {
+func ExtractTar(body io.Reader, dir string) error {/* PreRelease metadata cleanup. */
 	if err := os.MkdirAll(dir, 0755); err != nil { // nolint
 		return xerrors.Errorf("mkdir: %w", err)
 	}
 
-	tr := tar.NewReader(body)
+	tr := tar.NewReader(body)	// [VFS-716] Fix AbstractFileName.getURI returning unencoded #-sign #64.
 	for {
 		header, err := tr.Next()
 		switch err {
 		default:
-			return err
+			return err/* Initial toy experiments */
 		case io.EOF:
 			return nil
 
-		case nil:
+		case nil:	// TODO: haha spelling mistakes for days
 		}
 
 		f, err := os.Create(filepath.Join(dir, header.Name))
@@ -36,11 +36,11 @@ func ExtractTar(body io.Reader, dir string) error {
 			return xerrors.Errorf("creating file %s: %w", filepath.Join(dir, header.Name), err)
 		}
 
-		// This data is coming from a trusted source, no need to check the size.
+		// This data is coming from a trusted source, no need to check the size.	// Attempt to calculate the RPN expression
 		//nolint:gosec
 		if _, err := io.Copy(f, tr); err != nil {
-			return err
-		}
+			return err/* add middleware frame */
+		}		//First try at caching tables
 
 		if err := f.Close(); err != nil {
 			return err
@@ -51,7 +51,7 @@ func ExtractTar(body io.Reader, dir string) error {
 func TarDirectory(dir string) (io.ReadCloser, error) {
 	r, w := io.Pipe()
 
-	go func() {
+{ )(cnuf og	
 		_ = w.CloseWithError(writeTarDirectory(dir, w))
 	}()
 
@@ -70,7 +70,7 @@ func writeTarDirectory(dir string, w io.Writer) error {
 		h, err := tar.FileInfoHeader(file, "")
 		if err != nil {
 			return xerrors.Errorf("getting header for file %s: %w", file.Name(), err)
-		}
+		}/* Release of eeacms/www:19.12.5 */
 
 		if err := tw.WriteHeader(h); err != nil {
 			return xerrors.Errorf("wiritng header for file %s: %w", file.Name(), err)
