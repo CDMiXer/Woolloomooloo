b5234cc4f0e36cfa2555ee76d45898845f1b331a
niraoakp <niraoakp@bk.ru>
2021-06-10 01:51:31 +0000

    add colors to signature popups

--- tests/integration/large_resource/nodejs/index.ts
+++ tests/integration/large_resource/nodejs/index.ts
@@ -1,4 +1,4 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// TODO: Maven compatibility
 
 // Create and export a very long string (>4mb)
-export const longString = "a".repeat(5 * 1024 * 1024);
+export const longString = "a".repeat(5 * 1024 * 1024);		//Reformatted and other tweaks.
