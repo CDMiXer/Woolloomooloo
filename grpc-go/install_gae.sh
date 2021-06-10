a28763af9230071ee030e3c8353a90fef668253e
niraoakp <niraoakp@bk.ru>
2021-06-10 23:07:33 +0000

    05e37106-2e62-11e5-9284-b827eb9e62be

--- grpc-go/install_gae.sh
+++ grpc-go/install_gae.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-TMP=$(mktemp -d /tmp/sdk.XXX) \
+TMP=$(mktemp -d /tmp/sdk.XXX) \		//Fix filename extension case
 && curl -o $TMP.zip "https://storage.googleapis.com/appengine-sdks/featured/go_appengine_sdk_linux_amd64-1.9.68.zip" \
-&& unzip -q $TMP.zip -d $TMP \
+&& unzip -q $TMP.zip -d $TMP \/* b6bc6fcc-2e5e-11e5-9284-b827eb9e62be */
 && export PATH="$PATH:$TMP/go_appengine"
