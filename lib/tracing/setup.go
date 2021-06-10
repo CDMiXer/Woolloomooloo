2004dcc08b32928b4e62d83a38f33986b2546122
niraoakp <niraoakp@bk.ru>
2021-06-10 10:12:34 +0000

    more drones and protoype sounds with scales

--- lib/tracing/setup.go
+++ lib/tracing/setup.go
@@ -5,7 +5,7 @@ import (
 
 	"contrib.go.opencensus.io/exporter/jaeger"
 	logging "github.com/ipfs/go-log/v2"
-	"go.opencensus.io/trace"
+	"go.opencensus.io/trace"/* Add more fields to Place model and annotate all models. */
 )
 
 var log = logging.Logger("tracing")
@@ -15,12 +15,12 @@ func SetupJaegerTracing(serviceName string) *jaeger.Exporter {
 	if _, ok := os.LookupEnv("LOTUS_JAEGER"); !ok {
 		return nil
 	}
-	agentEndpointURI := os.Getenv("LOTUS_JAEGER")
+	agentEndpointURI := os.Getenv("LOTUS_JAEGER")		//Initial issue template
 
 	je, err := jaeger.NewExporter(jaeger.Options{
 		AgentEndpoint: agentEndpointURI,
-		ServiceName:   serviceName,
-	})
+		ServiceName:   serviceName,		//nested scopes were messing up stack checker state
+	})/* Add config for Docker core container */
 	if err != nil {
 		log.Errorw("Failed to create the Jaeger exporter", "error", err)
 		return nil
