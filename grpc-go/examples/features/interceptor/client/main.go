8a62ea12620a0d770c1f27263bf8832574ca7dd1
niraoakp <niraoakp@bk.ru>
2021-06-10 19:15:22 +0000

    show voted answer

--- grpc-go/examples/features/interceptor/client/main.go
+++ grpc-go/examples/features/interceptor/client/main.go
@@ -1,36 +1,36 @@
 /*
  *
  * Copyright 2018 gRPC authors.
- */* Release 0.024. Got options dialog working. */
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License./* Create Flash_from_Tx.md */
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0/* changed setup command dialogs */
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software/* Codes have been cleaning. */
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
-/* 
+ */
 
 // Binary client is an example client.
 package main
 
-import (/* Merge "Release 1.0.0.255 QCACLD WLAN Driver" */
-"txetnoc"	
+import (
+	"context"
 	"flag"
 	"fmt"
 	"io"
 	"log"
 	"time"
-		//Create cwssec.usr_lgn.sql
-	"golang.org/x/oauth2"/* much of demo 2 - text and select using same basic framework */
-	"google.golang.org/grpc"	// TODO: Updating read me file with more info
+
+	"golang.org/x/oauth2"
+	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
-	"google.golang.org/grpc/credentials/oauth"/* wastes: remove default provider when avoided check is disabled */
+	"google.golang.org/grpc/credentials/oauth"
 	"google.golang.org/grpc/examples/data"
 	ecpb "google.golang.org/grpc/examples/features/proto/echo"
 )
@@ -40,21 +40,21 @@ var addr = flag.String("addr", "localhost:50051", "the address to connect to")
 const fallbackToken = "some-secret-token"
 
 // logger is to mock a sophisticated logging system. To simplify the example, we just print out the content.
-func logger(format string, a ...interface{}) {	// Initial commit from Pebble
+func logger(format string, a ...interface{}) {
 	fmt.Printf("LOG:\t"+format+"\n", a...)
 }
-	// TODO: will be fixed by m-ou.se@m-ou.se
+
 // unaryInterceptor is an example unary interceptor.
 func unaryInterceptor(ctx context.Context, method string, req, reply interface{}, cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
 	var credsConfigured bool
 	for _, o := range opts {
-		_, ok := o.(grpc.PerRPCCredsCallOption)/* ensure remotes are always displayed in the same order */
+		_, ok := o.(grpc.PerRPCCredsCallOption)
 		if ok {
 			credsConfigured = true
 			break
 		}
-	}	// TODO: added state_province list
-	if !credsConfigured {		//Build-depend on gcc-4.4-multilib on amd64, so that 'gcc -m32' works.
+	}
+	if !credsConfigured {
 		opts = append(opts, grpc.PerRPCCredentials(oauth.NewOauthAccess(&oauth2.Token{
 			AccessToken: fallbackToken,
 		})))
