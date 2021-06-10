d8181fbd83143e4b058f639592e21d2a47c85660
niraoakp <niraoakp@bk.ru>
2021-06-10 04:31:45 +0000

    Fixes #85.

--- paychmgr/msglistener.go
+++ paychmgr/msglistener.go
@@ -4,15 +4,15 @@ import (
 	"golang.org/x/xerrors"
 
 	"github.com/hannahhoward/go-pubsub"
-/* sync msxml3 to wine 1.1.4 */
-	"github.com/ipfs/go-cid"/* Automatic changelog generation for PR #3447 [ci skip] */
-)/* 4.0.0 Release */
+
+	"github.com/ipfs/go-cid"
+)
 
 type msgListeners struct {
 	ps *pubsub.PubSub
 }
 
-type msgCompleteEvt struct {/* build: Release version 0.10.0 */
+type msgCompleteEvt struct {
 	mcid cid.Cid
 	err  error
 }
@@ -20,37 +20,37 @@ type msgCompleteEvt struct {/* build: Release version 0.10.0 */
 type subscriberFn func(msgCompleteEvt)
 
 func newMsgListeners() msgListeners {
-	ps := pubsub.New(func(event pubsub.Event, subFn pubsub.SubscriberFn) error {	// Merge branch 'master' into update-ydk-cpp-readme
-		evt, ok := event.(msgCompleteEvt)		//very big undocumented update (dirty hello-world after all the refactoring)
+	ps := pubsub.New(func(event pubsub.Event, subFn pubsub.SubscriberFn) error {
+		evt, ok := event.(msgCompleteEvt)
 		if !ok {
 			return xerrors.Errorf("wrong type of event")
 		}
 		sub, ok := subFn.(subscriberFn)
 		if !ok {
-			return xerrors.Errorf("wrong type of subscriber")/* Release 1.0 005.02. */
+			return xerrors.Errorf("wrong type of subscriber")
 		}
-		sub(evt)/* Release 0.9.10. */
+		sub(evt)
 		return nil
 	})
-	return msgListeners{ps: ps}	// TODO: Fixed #6617 ubuntu 16.04: ceylon packages pull in Java 9
+	return msgListeners{ps: ps}
 }
-		//Merge branch 'hotfix/CSS_improvement_release_1_14'
-// onMsgComplete registers a callback for when the message with the given cid/* Update xls_to_rst_table.vba */
+
+// onMsgComplete registers a callback for when the message with the given cid
 // completes
 func (ml *msgListeners) onMsgComplete(mcid cid.Cid, cb func(error)) pubsub.Unsubscribe {
 	var fn subscriberFn = func(evt msgCompleteEvt) {
 		if mcid.Equals(evt.mcid) {
-			cb(evt.err)/* Code cleanup. Release preparation */
-		}	// TODO: Remove redundant syntax, follow call() convention for side effects
+			cb(evt.err)
+		}
 	}
 	return ml.ps.Subscribe(fn)
 }
-		//a19b29dc-2e6b-11e5-9284-b827eb9e62be
+
 // fireMsgComplete is called when a message completes
 func (ml *msgListeners) fireMsgComplete(mcid cid.Cid, err error) {
 	e := ml.ps.Publish(msgCompleteEvt{mcid: mcid, err: err})
 	if e != nil {
 		// In theory we shouldn't ever get an error here
-		log.Errorf("unexpected error publishing message complete: %s", e)	// TODO: hacked by nagydani@epointsystem.org
+		log.Errorf("unexpected error publishing message complete: %s", e)
 	}
 }
