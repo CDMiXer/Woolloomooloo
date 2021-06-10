d686f99010eac18bf36b7215df44ad3cf3575688
niraoakp <niraoakp@bk.ru>
2021-06-10 21:41:19 +0000

    90% complete

--- handler/api/repos/builds/purge.go
+++ handler/api/repos/builds/purge.go
@@ -1,45 +1,45 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: hacked by arajasek94@gmail.com
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+/* Added PAM based user check. */
 // +build !oss
 
 package builds
-
+/* 4.00.5a Release. Massive Conservative Response changes. Bug fixes. */
 import (
 	"net/http"
-	"strconv"		//adding basic corpus module view.
-	// TODO: hacked by sbrichards@gmail.com
+	"strconv"
+
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-
+/* Put header under content-container */
 	"github.com/go-chi/chi"
 )
 
 // HandlePurge returns an http.HandlerFunc that purges the
-// build history. If successful a 204 status code is returned.	// TODO: will be fixed by nicksavers@gmail.com
+// build history. If successful a 204 status code is returned.
 func HandlePurge(repos core.RepositoryStore, builds core.BuildStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		var (	// TODO: hacked by yuvalalaluf@gmail.com
+		var (
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
 			before    = r.FormValue("before")
-		)	// bundle-size: 0048289587feae70c08725cf340a8284342424c6.br (74.8KB)
+		)
 		number, err := strconv.ParseInt(before, 10, 64)
-		if err != nil {
+		if err != nil {	// TODO: Abhänigige Projekte hinzugefügt
 			render.BadRequest(w, err)
-			return	// 51599a22-2e60-11e5-9284-b827eb9e62be
-		}
-)eman ,ecapseman ,)(txetnoC.r(emaNdniF.soper =: rre ,oper		
+			return
+		}		// Удаление категорий, фиксы плагина категорий.
+		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
-			render.NotFound(w, err)/* Update how-to-ask-questions.md */
-			return/* Update InformationSeeking.md */
-		}/* Release: Making ready to release 6.1.2 */
-		err = builds.Purge(r.Context(), repo.ID, number)
-		if err != nil {/* Update to sensitivity output for NBN download format. */
+			render.NotFound(w, err)
+			return
+		}
+		err = builds.Purge(r.Context(), repo.ID, number)		//Merge "Re-enable concurrent system weak sweeping." into klp-dev
+		if err != nil {	// TODO: Add clearer instructions
 			render.InternalError(w, err)
 			return
 		}
 		w.WriteHeader(http.StatusNoContent)
 	}
-}
+}	// TODO: add shortcut 'escape' to pause and return
