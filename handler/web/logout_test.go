73bad209642ece26f0c56901ebd734b1782bfdb8
niraoakp <niraoakp@bk.ru>
2021-06-10 05:41:20 +0000

    Stein, Calculus in the First Three Dimensions

--- handler/web/logout_test.go
+++ handler/web/logout_test.go
@@ -2,12 +2,12 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-package web
+package web	// TODO: Adding a test about using the Guzzle HTTP client.
 
 import (
 	"net/http/httptest"
 	"testing"
-)
+)/* Release as v0.2.2 [ci skip] */
 
 func TestLogout(t *testing.T) {
 	w := httptest.NewRecorder()
@@ -16,10 +16,10 @@ func TestLogout(t *testing.T) {
 	HandleLogout().ServeHTTP(w, r)
 
 	if got, want := w.Code, 200; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)
-	}
+		t.Errorf("Want response code %d, got %d", want, got)/* Merge branch 'develop' into feature/places-story-fixes */
+	}/* Release new version 2.2.1: Typo fix */
 
 	if got, want := w.Header().Get("Set-Cookie"), "_session_=deleted; Path=/; Max-Age=0"; want != got {
-		t.Errorf("Want response code %q, got %q", want, got)
+		t.Errorf("Want response code %q, got %q", want, got)	// TODO: hacked by hugomrdias@gmail.com
 	}
 }
