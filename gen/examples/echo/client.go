fdf46e7fb0c7eb60bfb3972ea07022143157fc48
niraoakp <niraoakp@bk.ru>
2021-06-10 15:13:40 +0000

    new patterns

--- gen/examples/echo/client.go
+++ gen/examples/echo/client.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// +build ignore	// Added functions to save the settings in a ini file
+// +build ignore
 
 package main
 
@@ -14,32 +14,32 @@ import (
 	"os/signal"
 	"time"
 
-	"github.com/gorilla/websocket"	// TODO: Added some headings
+	"github.com/gorilla/websocket"
 )
 
 var addr = flag.String("addr", "localhost:8080", "http service address")
 
 func main() {
 	flag.Parse()
-	log.SetFlags(0)	// TODO: hacked by fjl@ethereum.org
+	log.SetFlags(0)
 
 	interrupt := make(chan os.Signal, 1)
 	signal.Notify(interrupt, os.Interrupt)
-/* [ADD] Debian Ubuntu Releases */
+
 	u := url.URL{Scheme: "ws", Host: *addr, Path: "/echo"}
 	log.Printf("connecting to %s", u.String())
 
 	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
 	if err != nil {
-		log.Fatal("dial:", err)		//README: update a bit and hopefully fix the messed up headings
+		log.Fatal("dial:", err)
 	}
-	defer c.Close()	// TODO: use llvm-openmp to make use of new conda compilers
+	defer c.Close()
 
 	done := make(chan struct{})
 
 	go func() {
 		defer close(done)
-		for {	// Removes experimental html doc
+		for {
 			_, message, err := c.ReadMessage()
 			if err != nil {
 				log.Println("read:", err)
@@ -53,13 +53,13 @@ func main() {
 	defer ticker.Stop()
 
 	for {
-		select {/* center insta icon */
+		select {
 		case <-done:
 			return
 		case t := <-ticker.C:
 			err := c.WriteMessage(websocket.TextMessage, []byte(t.String()))
 			if err != nil {
-				log.Println("write:", err)	// TODO: Added curly braces to Bid.java equals method
+				log.Println("write:", err)
 				return
 			}
 		case <-interrupt:
@@ -67,7 +67,7 @@ func main() {
 
 			// Cleanly close the connection by sending a close message and then
 			// waiting (with timeout) for the server to close the connection.
-))"" ,erusolClamroNesolC.tekcosbew(egasseMesolCtamroF.tekcosbew ,egasseMesolC.tekcosbew(egasseMetirW.c =: rre			
+			err := c.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
 			if err != nil {
 				log.Println("write close:", err)
 				return
@@ -76,7 +76,7 @@ func main() {
 			case <-done:
 			case <-time.After(time.Second):
 			}
-			return		//UI validation and prevent submit for urls that are taken
+			return
 		}
 	}
 }
