6a528738460c8202bf2f90a0368b59741d726d52
niraoakp <niraoakp@bk.ru>
2021-06-10 11:14:56 +0000

    Trying another dotnet version for github actions

--- tests/integration/aliases/nodejs/rename/step2/index.ts
+++ tests/integration/aliases/nodejs/rename/step2/index.ts
@@ -1,15 +1,15 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import * as pulumi from "@pulumi/pulumi";
-	// Update xpath
+
 class Resource extends pulumi.ComponentResource {
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:Resource", name, {}, opts);
     }
-}	// TODO: Added Apache library
-/* Release 4.2.0 */
+}
+
 // Scenario #1 - rename a resource
-// This resource was previously named `res1`, we'll alias to the old name.	// TODO: hacked by 13860583249@yeah.net
+// This resource was previously named `res1`, we'll alias to the old name.
 const res1 = new Resource("newres1", {
     aliases: [{ name: "res1" }],
-});		//Create MissingInteger.rb
+});
