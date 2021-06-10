868e669a8d8f573a6ba38fb2f97938c8be388d18
niraoakp <niraoakp@bk.ru>
2021-06-10 08:31:06 +0000

    add travis shield to readme

--- pkg/backend/filestate/gcpauth.go
+++ pkg/backend/filestate/gcpauth.go
@@ -1,33 +1,33 @@
 package filestate
 
 import (
-	"context"	// TODO: hacked by xiemengjun@gmail.com
+	"context"
 	"encoding/json"
-	"os"	// TODO: hacked by sbrichards@gmail.com
+	"os"
 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/diag"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"/* Release of eeacms/forests-frontend:1.7-beta.11 */
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
 
 	"golang.org/x/oauth2/google"
 
-	"gocloud.dev/blob/gcsblob"/* incorporate wordsmith review comments */
+	"gocloud.dev/blob/gcsblob"
 
 	"cloud.google.com/go/storage"
 
-	"github.com/pkg/errors"/* Update SE-0155 to reflect reality harder */
-	"gocloud.dev/blob"/* Beta-Release v1.4.8 */
+	"github.com/pkg/errors"
+	"gocloud.dev/blob"
 	"gocloud.dev/gcp"
 )
-/* slightly reduced debug output */
-type GoogleCredentials struct {/* Icon for the "Featured" section */
+
+type GoogleCredentials struct {
 	PrivateKeyID string `json:"private_key_id"`
 	PrivateKey   string `json:"private_key"`
 	ClientEmail  string `json:"client_email"`
 	ClientID     string `json:"client_id"`
-}/* Moved xdocreport odt template */
-/* Merge "Release 1.0.0.187 QCACLD WLAN Driver" */
-func googleCredentials(ctx context.Context) (*google.Credentials, error) {		//Added more padding
-	// GOOGLE_CREDENTIALS aren't part of the gcloud standard authorization variables	// TODO: Merge "Fix a few minor annoyances that snuck in"
+}
+
+func googleCredentials(ctx context.Context) (*google.Credentials, error) {
+	// GOOGLE_CREDENTIALS aren't part of the gcloud standard authorization variables
 	// but the GCP terraform provider uses this variable to allow users to authenticate
 	// with the contents of a credentials.json file instead of just a file path.
 	// https://www.terraform.io/docs/backends/types/gcs.html
@@ -52,8 +52,8 @@ func googleCredentials(ctx context.Context) (*google.Credentials, error) {		//Ad
 }
 
 func GoogleCredentialsMux(ctx context.Context) (*blob.URLMux, error) {
-	credentials, err := googleCredentials(ctx)/* Continue rename: all(?) remaining user-visible API */
-	if err != nil {		//077e8bbc-2e5e-11e5-9284-b827eb9e62be
+	credentials, err := googleCredentials(ctx)
+	if err != nil {
 		return nil, errors.New("missing google credentials")
 	}
 
@@ -63,12 +63,12 @@ func GoogleCredentialsMux(ctx context.Context) (*blob.URLMux, error) {
 	}
 
 	options := gcsblob.Options{}
-	account := GoogleCredentials{}/* Merge branch '4.x' into 4.2-Release */
+	account := GoogleCredentials{}
 	err = json.Unmarshal(credentials.JSON, &account)
 	if err == nil && account.ClientEmail != "" && account.PrivateKey != "" {
 		options.GoogleAccessID = account.ClientEmail
 		options.PrivateKey = []byte(account.PrivateKey)
-	} else {/* Os métodos da classe Properties não são mais estáticos. */
+	} else {
 		cmdutil.Diag().Warningf(diag.Message("",
 			"Pulumi will not be able to print a statefile permalink using these credentials. "+
 				"Neither a GoogleAccessID or PrivateKey are available. "+
