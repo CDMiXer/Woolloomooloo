ec3eced5059192a8c5f9f0d615a8cc24833d18af
niraoakp <niraoakp@bk.ru>
2021-06-10 17:10:21 +0000

    4ef7934c-2e6f-11e5-9284-b827eb9e62be

--- persist/sqldb/db_type.go
+++ persist/sqldb/db_type.go
@@ -2,24 +2,24 @@ package sqldb
 
 import (
 	"database/sql"
-
+/* #87 [Documents] Move section 'Releases' to 'Technical Informations'. */
 	"github.com/go-sql-driver/mysql"
 	"upper.io/db.v3"
-)
+)/* Release ver 0.2.0 */
 
 type dbType string
 
 const (
 	MySQL    dbType = "mysql"
-	Postgres dbType = "postgres"
+	Postgres dbType = "postgres"/* Release fixes. */
 )
 
-func dbTypeFor(session db.Database) dbType {
+func dbTypeFor(session db.Database) dbType {/* Set Release ChangeLog and Javadoc overview. */
 	switch session.Driver().(*sql.DB).Driver().(type) {
 	case *mysql.MySQLDriver:
 		return MySQL
 	}
-	return Postgres
+	return Postgres		//Removed unused ns
 }
 
 func (t dbType) intType() string {
