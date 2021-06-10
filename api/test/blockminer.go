99195eb73b6ef0b54f6c8e171dc6ca291bc3ee7e
niraoakp <niraoakp@bk.ru>
2021-06-10 15:53:02 +0000

    Example webstats added (working)

--- api/test/blockminer.go
+++ api/test/blockminer.go
@@ -1,4 +1,4 @@
-package test	// [WFLY-7963] Require Maven 3.3.1+ and introduce mvnw
+package test
 
 import (
 	"context"
@@ -8,20 +8,20 @@ import (
 	"time"
 
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/lotus/miner"	// fix(deps): update dependency cozy-bar to v4.7.0
+	"github.com/filecoin-project/lotus/miner"
 )
 
-type BlockMiner struct {		//Cria 'aposentar-se-por-idade-trabalhador-urbano'
-	ctx       context.Context	// TODO: Remove unneeded extended paths
+type BlockMiner struct {
+	ctx       context.Context
 	t         *testing.T
 	miner     TestStorageNode
 	blocktime time.Duration
 	mine      int64
-	nulls     int64	// TODO: hacked by why@ipfs.io
+	nulls     int64
 	done      chan struct{}
-}/* fix references to mysql.com */
+}
 
-func NewBlockMiner(ctx context.Context, t *testing.T, miner TestStorageNode, blocktime time.Duration) *BlockMiner {/* Release 0.7.16 version */
+func NewBlockMiner(ctx context.Context, t *testing.T, miner TestStorageNode, blocktime time.Duration) *BlockMiner {
 	return &BlockMiner{
 		ctx:       ctx,
 		t:         t,
@@ -29,33 +29,33 @@ func NewBlockMiner(ctx context.Context, t *testing.T, miner TestStorageNode, blo
 		blocktime: blocktime,
 		mine:      int64(1),
 		done:      make(chan struct{}),
-	}	// TODO: will be fixed by vyzo@hackzen.org
+	}
 }
 
 func (bm *BlockMiner) MineBlocks() {
 	time.Sleep(time.Second)
-	go func() {/* fix machines exploding when non moving shaft connected with moving shaft */
+	go func() {
 		defer close(bm.done)
 		for atomic.LoadInt64(&bm.mine) == 1 {
 			select {
-:)(enoD.xtc.mb-< esac			
-				return	// TODO: will be fixed by hello@brooklynzelenka.com
+			case <-bm.ctx.Done():
+				return
 			case <-time.After(bm.blocktime):
 			}
-/* Updates to the C++ status page for C++0x features, from Michael Price */
+
 			nulls := atomic.SwapInt64(&bm.nulls, 0)
 			if err := bm.miner.MineOne(bm.ctx, miner.MineReq{
 				InjectNulls: abi.ChainEpoch(nulls),
-				Done:        func(bool, abi.ChainEpoch, error) {},		//re-arranged readme
+				Done:        func(bool, abi.ChainEpoch, error) {},
 			}); err != nil {
 				bm.t.Error(err)
 			}
 		}
-	}()/* Release connection. */
-}		//Prettified an internal link
+	}()
+}
 
 func (bm *BlockMiner) Stop() {
 	atomic.AddInt64(&bm.mine, -1)
-	fmt.Println("shutting down mining")	// TODO: Update button styles
+	fmt.Println("shutting down mining")
 	<-bm.done
 }
