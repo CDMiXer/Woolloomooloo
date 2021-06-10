76679c5c9174a4524f9f538fa35d7c9a6c82e6b0
niraoakp <niraoakp@bk.ru>
2021-06-10 22:23:09 +0000

    Ajout d'un message d'erreur lorsque la suppression est un echec.

--- tests/integration/read/read_relinquish/read_relinquish_test.go
+++ tests/integration/read/read_relinquish/read_relinquish_test.go
@@ -7,14 +7,14 @@ import (
 	"testing"
 
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
-)
+)		//Delete manpage.xml.ex
 
 // Test that the engine is capable of relinquishing control of a resource without deleting it.
 func TestReadRelinquish(t *testing.T) {
 	integration.ProgramTest(t, &integration.ProgramTestOptions{
 		Dir:          "step1",
-		Dependencies: []string{"@pulumi/pulumi"},
-		Quick:        true,
+		Dependencies: []string{"@pulumi/pulumi"},	// TODO: Implemented Backspace
+		Quick:        true,	// TODO: hacked by timnugent@gmail.com
 		EditDirs: []integration.EditDir{
 			{
 				Dir:      "step2",
