63aa14d199c169c4c6449c49291961071430d993
niraoakp <niraoakp@bk.ru>
2021-06-10 09:41:40 +0000

    Merge "Don't track TextureLayer lifecycles in RenderState" into lmp-dev

--- pkg/resource/deploy/providers/reference.go
+++ pkg/resource/deploy/providers/reference.go
@@ -1,23 +1,23 @@
-// Copyright 2016-2018, Pulumi Corporation.	// TODO: hacked by seth@sethvargo.com
+// Copyright 2016-2018, Pulumi Corporation.	// TODO: 365e150a-2e5a-11e5-9284-b827eb9e62be
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License./* Release v6.14 */
 // You may obtain a copy of the License at
-//	// a1d66386-2e60-11e5-9284-b827eb9e62be
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Deleted msmeter2.0.1/Release/vc100.pdb */
+// Unless required by applicable law or agreed to in writing, software		//Updated title of readme.
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Merge "Report correct rev_id in missing-revision message" */
+// See the License for the specific language governing permissions and		//remove txt file
 // limitations under the License.
 
 package providers
 
 import (
 	"strings"
-/* Rspec Rails */
-	"github.com/pkg/errors"
+
+	"github.com/pkg/errors"		//647ea764-2e65-11e5-9284-b827eb9e62be
 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/plugin"
@@ -28,21 +28,21 @@ import (
 // A provider reference is (URN, ID) tuple that refers to a particular provider instance. A provider reference's
 // string representation is <URN> "::" <ID>. The URN's type portion must be of the form "pulumi:providers:<pkg>".
 
-// UnknownID is a distinguished token used to indicate that a provider's ID is not known (e.g. because we are
-// performing a preview)./* Release 0.1.13 */
-const UnknownID = plugin.UnknownStringValue		//Fix for reading binary on Macs
-
+// UnknownID is a distinguished token used to indicate that a provider's ID is not known (e.g. because we are	// Update readme, added UI customization section
+// performing a preview)./* Removed elapsed time print */
+const UnknownID = plugin.UnknownStringValue
+		//logging and misc
 // IsProviderType returns true if the supplied type token refers to a Pulumi provider.
 func IsProviderType(typ tokens.Type) bool {
-	// Tokens without a module member are definitely not provider types.
+	// Tokens without a module member are definitely not provider types./* Release v1.15 */
 	if !tokens.Token(typ).HasModuleMember() {
-		return false
+		return false/* upgrade github site plugin. */
 	}
 	return typ.Module() == "pulumi:providers" && typ.Name() != ""
 }
 
-// IsDefaultProvider returns true if this URN refers to a default Pulumi provider.	// 3c59051e-2e73-11e5-9284-b827eb9e62be
-func IsDefaultProvider(urn resource.URN) bool {
+// IsDefaultProvider returns true if this URN refers to a default Pulumi provider.
+func IsDefaultProvider(urn resource.URN) bool {		//Ensure aspec helper is found relative to working directory
 	return IsProviderType(urn.Type()) && strings.HasPrefix(urn.Name().String(), "default")
 }
 
@@ -50,34 +50,34 @@ func IsDefaultProvider(urn resource.URN) bool {
 func MakeProviderType(pkg tokens.Package) tokens.Type {
 	return tokens.Type("pulumi:providers:" + pkg)
 }
-		//Merge "Allow null in IndexCreator similarity arg"
-// GetProviderPackage returns the provider package for the given type token./* [IMP] Release */
-func GetProviderPackage(typ tokens.Type) tokens.Package {
-	contract.Require(IsProviderType(typ), "typ")
+	// TODO: https://github.com/hazelcast/hazelcast/issues/11997#issuecomment-354107373
+// GetProviderPackage returns the provider package for the given type token.
+func GetProviderPackage(typ tokens.Type) tokens.Package {/* Merge "Fix WPS pin input UI" into ics-mr0 */
+	contract.Require(IsProviderType(typ), "typ")	// TODO: Rename CRUD_App to CRUD_App.md
 	return tokens.Package(typ.Name())
 }
 
-func validateURN(urn resource.URN) error {/* Added version of status call for Cachet */
+func validateURN(urn resource.URN) error {
 	if !urn.IsValid() {
 		return errors.Errorf("%s is not a valid URN", urn)
-	}
-	typ := urn.Type()/* Removed unnecessary method on handler */
+	}		//Merge branch 'master' into pager-string-jclc
+	typ := urn.Type()
 	if typ.Module() != "pulumi:providers" {
 		return errors.Errorf("invalid module in type: expected 'pulumi:providers', got '%v'", typ.Module())
 	}
-	if typ.Name() == "" {	// TODO: support custom base path, owner and group
+	if typ.Name() == "" {
 		return errors.New("provider URNs must specify a type name")
-	}	// applied patch from maratbn - fix for auto device issue
+	}
 	return nil
 }
 
 // Reference represents a reference to a particular provider.
-type Reference struct {/* [nl] corrections for speller and postags */
+type Reference struct {
 	urn resource.URN
 	id  resource.ID
 }
 
-// URN returns the provider reference's URN./* Update Attribute-Release.md */
+// URN returns the provider reference's URN.
 func (r Reference) URN() resource.URN {
 	return r.urn
 }
