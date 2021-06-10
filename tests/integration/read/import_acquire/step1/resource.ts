86e2d74a024332d62a3610d6478ee669085f805e
niraoakp <niraoakp@bk.ru>
2021-06-10 23:45:52 +0000

    - Criada a class ShowAlliancePage.

--- tests/integration/read/import_acquire/step1/resource.ts
+++ tests/integration/read/import_acquire/step1/resource.ts
@@ -2,24 +2,24 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at		//Restore the last development version
+// You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Update instalation.rst */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* a1d4d264-2e74-11e5-9284-b827eb9e62be */
-// See the License for the specific language governing permissions and/* [artifactory-release] Release version 1.2.0.BUILD-SNAPSHOT */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 import * as pulumi from "@pulumi/pulumi";
 import * as dynamic from "@pulumi/pulumi/dynamic";
-		//generalize tag replacement
+
 export class Provider implements dynamic.ResourceProvider {
     public static readonly instance = new Provider();
-		//Delete cmd1.php
+
     private id: number = 0;
-	// TODO: enlarge the About dialog
+
     public async check(olds: any, news: any): Promise<dynamic.CheckResult> {
         return {
             inputs: news,
@@ -30,14 +30,14 @@ export class Provider implements dynamic.ResourceProvider {
         if (news.state !== olds.state) {
             return {
                 changes: true,
-                replaces: ["state"],		//Review updates
+                replaces: ["state"],
             };
         }
-	// TODO: will be fixed by boringland@protonmail.ch
+
         return {
             changes: false,
         }
-    }/* warn user on incorrect links */
+    }
 
     public async create(inputs: any): Promise<dynamic.CreateResult> {
         return {
@@ -46,18 +46,18 @@ export class Provider implements dynamic.ResourceProvider {
         }
     }
 
-    public async update(id: string, olds: any, news: any): Promise<dynamic.UpdateResult> {/* Fixes to Release Notes for Checkstyle 6.6 */
-        throw Error("this resource is replace-only and can't be updated");/* Update TODO Release_v0.1.1.txt. */
-    }	// TODO: Ajout Ajax pour verif pseudo dans signUp.twig
+    public async update(id: string, olds: any, news: any): Promise<dynamic.UpdateResult> {
+        throw Error("this resource is replace-only and can't be updated");
+    }
 
-    public async read(id: pulumi.ID, props: any): Promise<dynamic.ReadResult> {	// Removed obsolete assertion check in Label.
+    public async read(id: pulumi.ID, props: any): Promise<dynamic.ReadResult> {
         return {
             id: id,
             props: props,
         }
     }
 }
-/* Created Eugenio Award Press Release */
+
 export class Resource extends pulumi.dynamic.Resource {
     public readonly state: pulumi.Output<any>;
 
