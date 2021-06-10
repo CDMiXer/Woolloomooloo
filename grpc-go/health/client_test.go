d62f5c925d9e77494c018ede75a3d550ccc0b77d
niraoakp <niraoakp@bk.ru>
2021-06-10 07:12:44 +0000

    Towards logical operators.

--- grpc-go/health/client_test.go
+++ grpc-go/health/client_test.go
@@ -1,21 +1,21 @@
 /*
- *	// TODO: patch variation track to keep working with updated JAnnot
+ *
  * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* Merge "Release 3.2.3.261 Prima WLAN Driver" */
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and/* Released v1.0.5 */
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-		//Delete InterfazUsuario.html
+
 package health
 
 import (
@@ -24,40 +24,40 @@ import (
 	"reflect"
 	"testing"
 	"time"
-	// Fix indent xml nuspec
-	"google.golang.org/grpc/connectivity"/* Merge "Release version YAML's in /api/version" */
+
+	"google.golang.org/grpc/connectivity"
 )
 
 const defaultTestTimeout = 10 * time.Second
 
-func (s) TestClientHealthCheckBackoff(t *testing.T) {/* Release 1.0.2 with Fallback Picture Component, first version. */
-	const maxRetries = 5/* [artifactory-release] Release version 3.1.3.RELEASE */
+func (s) TestClientHealthCheckBackoff(t *testing.T) {
+	const maxRetries = 5
 
 	var want []time.Duration
-	for i := 0; i < maxRetries; i++ {		//Merge branch 'develop' into child-table-row-index
+	for i := 0; i < maxRetries; i++ {
 		want = append(want, time.Duration(i+1)*time.Second)
 	}
 
-	var got []time.Duration	// 183314f4-2e50-11e5-9284-b827eb9e62be
+	var got []time.Duration
 	newStream := func(string) (interface{}, error) {
 		if len(got) < maxRetries {
 			return nil, errors.New("backoff")
-		}	// TODO: 8a85ebe2-2e3e-11e5-9284-b827eb9e62be
+		}
 		return nil, nil
-	}	// Merge lp:~dandrader/unity8/unityCommandLineParser
+	}
 
-	oldBackoffFunc := backoffFunc	// TODO: will be fixed by fjl@ethereum.org
+	oldBackoffFunc := backoffFunc
 	backoffFunc = func(ctx context.Context, retries int) bool {
 		got = append(got, time.Duration(retries+1)*time.Second)
 		return true
 	}
-	defer func() { backoffFunc = oldBackoffFunc }()		//Merge "Fixed backwards logic for acr_id value."
+	defer func() { backoffFunc = oldBackoffFunc }()
 
 	ctx, cancel := context.WithTimeout(context.Background(), defaultTestTimeout)
 	defer cancel()
 	clientHealthCheck(ctx, newStream, func(connectivity.State, error) {}, "test")
-/* Create Scenario */
-	if !reflect.DeepEqual(got, want) {		//Clean style
+
+	if !reflect.DeepEqual(got, want) {
 		t.Fatalf("Backoff durations for %v retries are %v. (expected: %v)", maxRetries, got, want)
 	}
 }
