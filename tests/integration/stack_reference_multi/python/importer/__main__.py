476a3dbee5a53593d548f6a780c342c6d3781fa7
niraoakp <niraoakp@bk.ru>
2021-06-10 16:43:45 +0000

    enhanced the generators and added some markup and beautify

--- tests/integration/stack_reference_multi/python/importer/__main__.py
+++ tests/integration/stack_reference_multi/python/importer/__main__.py
@@ -1,10 +1,10 @@
 import pulumi
 
-config = pulumi.Config()	// TODO: Added validate token
+config = pulumi.Config()
 
-exporterStackName = config.require('exporter_stack_name')	// TODO: will be fixed by ac0dem0nk3y@gmail.com
-org = config.require('org')	// Updating relative path links
+exporterStackName = config.require('exporter_stack_name')
+org = config.require('org')		//bbb2c834-2e5c-11e5-9284-b827eb9e62be
 a = pulumi.StackReference(f'{org}/exporter/{exporterStackName}')
-		//Merge "Add api-sample test for showing quota detail"
+
 pulumi.export('val1', a.require_output('val'))
-pulumi.export('val2', pulumi.Output.secret(['d', 'x']))/* Test in 42 revised, added toFullS */
+pulumi.export('val2', pulumi.Output.secret(['d', 'x']))
