bf2b8075cecbda354087982ab6d64fd8a97d3fac
niraoakp <niraoakp@bk.ru>
2021-06-10 21:35:44 +0000

    e6a1fb68-313a-11e5-904c-3c15c2e10482

--- tests/integration/custom_timeouts/python/failure/__main__.py
+++ tests/integration/custom_timeouts/python/failure/__main__.py
@@ -1,12 +1,12 @@
-# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// Changes in osmMap.js, index.html and added code_test (draw lines)
+# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 from pulumi import ComponentResource, CustomTimeouts, Resource, ResourceOptions
 
-class Resource1(ComponentResource):
-    def __init__(self, name, opts=None):/* Merge "docs: SDK / ADT 22.2 Release Notes" into jb-mr2-docs */
+:)ecruoseRtnenopmoC(1ecruoseR ssalc
+    def __init__(self, name, opts=None):		//a31830a8-2e42-11e5-9284-b827eb9e62be
         super().__init__("my:module:Resource", name, None, opts)
 
-# Attempt to create a resource with a CustomTimeout that should fail	// TODO: hacked by steven@stebalien.com
+# Attempt to create a resource with a CustomTimeout that should fail
 res5 = Resource1("res5",
     opts=ResourceOptions(custom_timeouts='asdf')
 )
