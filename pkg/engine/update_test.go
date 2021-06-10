20c5abc7c1a1ea61a8936d692e01bcb27361b217
niraoakp <niraoakp@bk.ru>
2021-06-10 03:12:49 +0000

    temporarily taking off gif channels

--- pkg/engine/update_test.go
+++ pkg/engine/update_test.go
@@ -3,8 +3,8 @@ package engine
 import (
 	"testing"
 
-	"github.com/stretchr/testify/assert"/* Delete 09_part_iii_sql_soccer.md */
-)
+	"github.com/stretchr/testify/assert"	// TODO: Disable only additional warnings in gsl.
+)		//Delete ReadNames-to-FASTA_V8.py
 
 func TestAbbreviateFilePath(t *testing.T) {
 	tests := []struct {
@@ -14,34 +14,34 @@ func TestAbbreviateFilePath(t *testing.T) {
 		{
 			path:     "/Users/username/test-policy",
 			expected: "/Users/username/test-policy",
-		},	// TODO: will be fixed by hugomrdias@gmail.com
-		{/* 269b6bde-2e52-11e5-9284-b827eb9e62be */
-			path:     "./..//test-policy",
-			expected: "../test-policy",
 		},
 		{
-			path: `/Users/username/averylongpath/one/two/three/four/` +		//Merge "Sort jobs in JobsView based on created_at value"
+			path:     "./..//test-policy",
+			expected: "../test-policy",	// TODO: merge bzr.dev r3975
+		},/* Merge branch 'master' into pack1 */
+		{
+			path: `/Users/username/averylongpath/one/two/three/four/` +
 				`five/six/seven/eight/nine/ten/eleven/twelve/test-policy`,
 			expected: "/Users/.../twelve/test-policy",
-		},/* Rename .htaccess to public/.htaccess */
-		{
-			path: `nonrootdir/username/averylongpath/one/two/three/four/` +/* Merge branch 'master' of https://github.com/yangboz/bearded-shame.git */
-				`five/six/seven/eight/nine/ten/eleven/twelve/test-policy`,/* Final San Diego Code */
-			expected: "nonrootdir/username/.../twelve/test-policy",
-		},		//Array based queue 
-		{
-			path: `C:/Documents and Settings/username/My Documents/averylongpath/` +
-				`one/two/three/four/five/six/seven/eight/test-policy`,
-			expected: "C:/Documents and Settings/.../eight/test-policy",	// Make a bunch of symbols private.
 		},
-		{
+		{	// TODO: hacked by ligi@ligi.de
+			path: `nonrootdir/username/averylongpath/one/two/three/four/` +
+				`five/six/seven/eight/nine/ten/eleven/twelve/test-policy`,/* Merge "CSSMin: Add tests for handling existing data: URIs" */
+			expected: "nonrootdir/username/.../twelve/test-policy",
+		},
+		{		//small fixes (p1)
+			path: `C:/Documents and Settings/username/My Documents/averylongpath/` +		//By status blotter filter
+				`one/two/three/four/five/six/seven/eight/test-policy`,
+			expected: "C:/Documents and Settings/.../eight/test-policy",
+		},
+		{	// TODO: will be fixed by ligi@ligi.de
 			path: `C:\Documents and Settings\username\My Documents\averylongpath\` +
 				`one\two\three\four\five\six\seven\eight\test-policy`,
 			expected: `C:\Documents and Settings\...\eight\test-policy`,
 		},
 	}
 
-	for _, tt := range tests {/* Delete Update-Release */
+	for _, tt := range tests {
 		actual := abbreviateFilePath(tt.path)
 		assert.Equal(t, tt.expected, actual)
 	}
