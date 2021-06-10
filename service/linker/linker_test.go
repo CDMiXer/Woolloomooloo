f7ba5b4b877df65547b96df6b547af06d3d40509
niraoakp <niraoakp@bk.ru>
2021-06-10 03:05:34 +0000

    Generated site for typescript-generator-gradle-plugin 2.10.467

--- service/linker/linker_test.go
+++ service/linker/linker_test.go
@@ -1,13 +1,13 @@
-// Copyright 2019 Drone IO, Inc./* added note that Mac download is for 10.6 or later */
-//
+// Copyright 2019 Drone IO, Inc.
+///* Release 1.1.0-CI00240 */
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// TODO: added more installation procedures for work machine 
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+//		//Test using Embedded Cassandra by default
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Update analogsignal.py */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
