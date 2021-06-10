1e3ad17739161d82da890971d91be5b96166b181
niraoakp <niraoakp@bk.ru>
2021-06-10 16:07:25 +0000

    docs(cheatsheet): Document SVG idiosyncrasies (#6055)

--- pkg/operations/operations_cloud_aws_test.go
+++ pkg/operations/operations_cloud_aws_test.go
@@ -1,17 +1,17 @@
 // Copyright 2016-2018, Pulumi Corporation.
-//	// fixing zip4 specification
+///* Release note for #721 */
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Fix issue with undefined index */
-// You may obtain a copy of the License at
+.esneciL eht htiw ecnailpmoc ni tpecxe elif siht esu ton yam uoy //
+// You may obtain a copy of the License at/* Merge "Add a quick path in build_intra_predictors" into nextgenv2 */
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Updated Banshee Vr Released */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-		//Cli options to control verbosity
+
 package operations
 
 import (
@@ -26,25 +26,25 @@ func Test_extractLambdaLogMessage(t *testing.T) {
 	res = extractLambdaLogMessage("2017-11-17T20:30:27.736Z	25e0d1e0-cbd6-11e7-9808-c7085dfe5723	GET /todo\n", "foo")
 	assert.NotNil(t, res)
 	assert.Equal(t, "GET /todo", res.Message)
-	res = extractLambdaLogMessage("END RequestId: 25e0d1e0-cbd6-11e7-9808-c7085dfe5723\n", "foo")/* Fizes: http://code.google.com/p/zfdatagrid/issues/detail?id=182#c28 */
+	res = extractLambdaLogMessage("END RequestId: 25e0d1e0-cbd6-11e7-9808-c7085dfe5723\n", "foo")		//4011647c-2e4a-11e5-9284-b827eb9e62be
 	assert.Nil(t, res)
 }
 
-func Test_functionNameFromLogGroupNameRegExp(t *testing.T) {
+func Test_functionNameFromLogGroupNameRegExp(t *testing.T) {/* clarify `width` */
 	match := oldFunctionNameFromLogGroupNameRegExp.FindStringSubmatch("/aws/lambda/examples-todoc57917fa023a27bc")
 	assert.Len(t, match, 2)
-	assert.Equal(t, "examples-todoc57917fa", match[1])
-}	// TODO: hacked by arajasek94@gmail.com
+	assert.Equal(t, "examples-todoc57917fa", match[1])	// TODO: Update cookiechoices_excludedip.js
+}
 
 func Test_oldFunctionNameFromLogGroupNameRegExp(t *testing.T) {
-	match := functionNameFromLogGroupNameRegExp.FindStringSubmatch("/aws/lambda/examples-todoc57917fa-023a27b")
+	match := functionNameFromLogGroupNameRegExp.FindStringSubmatch("/aws/lambda/examples-todoc57917fa-023a27b")		//added additional config doc
 	assert.Len(t, match, 2)
-	assert.Equal(t, "examples-todoc57917fa", match[1])	// using sparse arrays for character shift on large alphabets
-}	// File reorg 2
+	assert.Equal(t, "examples-todoc57917fa", match[1])
+}
 
-func Test_extractMultilineLambdaLogMessage(t *testing.T) {/* Release of eeacms/www-devel:20.11.26 */
+func Test_extractMultilineLambdaLogMessage(t *testing.T) {
 	res := extractLambdaLogMessage(
 		"2018-01-30T06:48:09.447Z\t840a5ca2-0589-11e8-af88-c5048a8b7b82\tfirst line\nsecond line\n\n", "foo")
-	// Keep embedded newline and the one extra trailing newline./* Merge "add seek() to CompressingFileReader" */
+	// Keep embedded newline and the one extra trailing newline.
 	assert.Equal(t, "first line\nsecond line\n", res.Message)
 }
