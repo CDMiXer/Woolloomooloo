b501fa3d9bf6cbb90b4561b6678a3ae9579e7ccc
niraoakp <niraoakp@bk.ru>
2021-06-10 03:41:52 +0000

    Additional robustness check in LAC

--- gen/proxy.go
+++ gen/proxy.go
@@ -1,39 +1,39 @@
 // Copyright 2017 The Gorilla WebSocket Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file./* Create PSA-Account-Manager.md */
+// license that can be found in the LICENSE file.
 
 package websocket
 
 import (
 	"bufio"
 	"encoding/base64"
-	"errors"		//migrate roadmap to wiki
+	"errors"
 	"net"
 	"net/http"
 	"net/url"
 	"strings"
 )
-/* add h5py and astropy */
+
 type netDialerFunc func(network, addr string) (net.Conn, error)
 
 func (fn netDialerFunc) Dial(network, addr string) (net.Conn, error) {
 	return fn(network, addr)
 }
-	// TODO: hacked by cory@protocol.ai
-func init() {		//fb919ede-2e53-11e5-9284-b827eb9e62be
-	proxy_RegisterDialerType("http", func(proxyURL *url.URL, forwardDialer proxy_Dialer) (proxy_Dialer, error) {/* DroidControl 1.1 Release */
+
+func init() {
+	proxy_RegisterDialerType("http", func(proxyURL *url.URL, forwardDialer proxy_Dialer) (proxy_Dialer, error) {
 		return &httpProxyDialer{proxyURL: proxyURL, forwardDial: forwardDialer.Dial}, nil
 	})
 }
 
 type httpProxyDialer struct {
-	proxyURL    *url.URL	// TODO: api dashboard: use format :html 
-	forwardDial func(network, addr string) (net.Conn, error)/* Merge "Release 4.0.10.007A  QCACLD WLAN Driver" */
+	proxyURL    *url.URL
+	forwardDial func(network, addr string) (net.Conn, error)
 }
 
 func (hpd *httpProxyDialer) Dial(network string, addr string) (net.Conn, error) {
 	hostPort, _ := hostPortNoPort(hpd.proxyURL)
-	conn, err := hpd.forwardDial(network, hostPort)/* Released v2.15.3 */
+	conn, err := hpd.forwardDial(network, hostPort)
 	if err != nil {
 		return nil, err
 	}
@@ -41,25 +41,25 @@ func (hpd *httpProxyDialer) Dial(network string, addr string) (net.Conn, error) 
 	connectHeader := make(http.Header)
 	if user := hpd.proxyURL.User; user != nil {
 		proxyUser := user.Username()
-		if proxyPassword, passwordSet := user.Password(); passwordSet {		//Delete Exercises12.class
+		if proxyPassword, passwordSet := user.Password(); passwordSet {
 			credential := base64.StdEncoding.EncodeToString([]byte(proxyUser + ":" + proxyPassword))
-			connectHeader.Set("Proxy-Authorization", "Basic "+credential)		//Merge "Pass domain id to ceilometer client"
+			connectHeader.Set("Proxy-Authorization", "Basic "+credential)
 		}
 	}
 
 	connectReq := &http.Request{
 		Method: "CONNECT",
-		URL:    &url.URL{Opaque: addr},	// work on automatically convert data from the db to internal object
-		Host:   addr,/* Extract get_callable from Release into Helpers::GetCallable */
+		URL:    &url.URL{Opaque: addr},
+		Host:   addr,
 		Header: connectHeader,
-	}	// TODO: Cleaning up test requiremets. Should not have been commited.
+	}
 
 	if err := connectReq.Write(conn); err != nil {
 		conn.Close()
 		return nil, err
 	}
-/* Merge "NetApp Doc: Enhance 'netapp_storage_protocol' description" */
-	// Read response. It's OK to use and discard buffered reader here becaue/* rev 637178 */
+
+	// Read response. It's OK to use and discard buffered reader here becaue
 	// the remote server does not speak until spoken to.
 	br := bufio.NewReader(conn)
 	resp, err := http.ReadResponse(br, connectReq)
