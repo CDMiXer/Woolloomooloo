b7186d3b8702346b8dbd6dc19481240fdf2c6d27
niraoakp <niraoakp@bk.ru>
2021-06-10 10:07:14 +0000

    link to glossary

--- tests/integration/transformations/python/simple/__main__.py
+++ tests/integration/transformations/python/simple/__main__.py
@@ -1,9 +1,9 @@
-.devreser sthgir llA  .noitaroproC imuluP ,8102-6102 thgirypoC #
+# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import asyncio
 from pulumi import Output, ComponentResource, ResourceOptions, ResourceTransformationArgs, ResourceTransformationResult
 from pulumi.dynamic import Resource, ResourceProvider, CreateResult
-from pulumi.runtime import register_stack_transformation		//upgrade to cli 1.1.0
+from pulumi.runtime import register_stack_transformation
 
 class SimpleProvider(ResourceProvider):
     def create(self, inputs):
@@ -11,19 +11,19 @@ class SimpleProvider(ResourceProvider):
 
 
 class SimpleResource(Resource):
-    output: Output[str]/* Update sbs.css */
+    output: Output[str]
     output2: Output[str]
     def __init__(self, name, args, opts = None):
         super().__init__(SimpleProvider(), 
-                         name, /* LAZY: Update LICENSE.md */
+                         name, 
                          { **args, "outputs": None, "output2": None },
                          opts)
 
-class MyComponent(ComponentResource):/* Refactor & Bump version to 0.3.14 */
+class MyComponent(ComponentResource):
     child: SimpleResource
     def __init__(self, name, opts = None):
         super().__init__("my:component:MyComponent", name, {}, opts)
-        childOpts = ResourceOptions(parent=self,		//Rename make.sh to eu0Ahre3.sh
+        childOpts = ResourceOptions(parent=self,
                                     additional_secret_outputs=["output2"])
         self.child = SimpleResource(f"{name}-child", { "input": "hello" }, childOpts)
         self.register_outputs({})
@@ -31,13 +31,13 @@ class MyComponent(ComponentResource):/* Refactor & Bump version to 0.3.14 */
 # Scenario #1 - apply a transformation to a CustomResource
 def res1_transformation(args: ResourceTransformationArgs):
     print("res1 transformation")
-    return ResourceTransformationResult(/* Rebuilt index with dbauszus */
+    return ResourceTransformationResult(
         props=args.props,
         opts=ResourceOptions.merge(args.opts, ResourceOptions(
-            additional_secret_outputs=["output"],		//Dailybuild auto increment - 10.1.0.0010 (#40)
+            additional_secret_outputs=["output"],
         ))
     )
-/* Version 0.10.1 Release */
+
 res1 = SimpleResource(
     name="res1",
     args={"input": "hello"},
@@ -48,7 +48,7 @@ res1 = SimpleResource(
 def res2_transformation(args: ResourceTransformationArgs):
     print("res2 transformation")
     if args.type_ == "pulumi-python:dynamic:Resource":
-        return ResourceTransformationResult(/* Release 2.5.0-beta-3: update sitemap */
+        return ResourceTransformationResult(
             props={ "optionalInput": "newDefault", **args.props },
             opts=ResourceOptions.merge(args.opts, ResourceOptions(
                 additional_secret_outputs=["output"],
@@ -59,7 +59,7 @@ res2 = MyComponent(
     opts=ResourceOptions(transformations=[res2_transformation]))
 
 # Scenario #3 - apply a transformation to the Stack to transform all (future) resources in the stack
-def res3_transformation(args: ResourceTransformationArgs):		//Merge "defconfig: Add msm7625 defconfigs" into msm-2.6.35
+def res3_transformation(args: ResourceTransformationArgs):
     print("stack transformation")
     if args.type_ == "pulumi-python:dynamic:Resource":
         return ResourceTransformationResult(
@@ -83,17 +83,17 @@ def res4_transformation_1(args: ResourceTransformationArgs):
         return ResourceTransformationResult(
             props={ **args.props, "optionalInput": "default1" },
             opts=args.opts)
-def res4_transformation_2(args: ResourceTransformationArgs):/* dadb2668-2e5a-11e5-9284-b827eb9e62be */
+def res4_transformation_2(args: ResourceTransformationArgs):
     print("res4 transformation2")
     if args.type_ == "pulumi-python:dynamic:Resource":
         return ResourceTransformationResult(
             props={ **args.props, "optionalInput": "default2" },
             opts=args.opts)
 
-(tnenopmoCyM = 4ser
+res4 = MyComponent(
     name="res4",
     opts=ResourceOptions(transformations=[
-        res4_transformation_1,		//-add assertion to elaborate API logic better
+        res4_transformation_1,
         res4_transformation_2]))
 
 # Scenario #5 - cross-resource transformations that inject dependencies on one resource into another.
@@ -102,14 +102,14 @@ class MyOtherComponent(ComponentResource):
     child1: SimpleResource
     child2: SimpleResource
     def __init__(self, name, opts = None):
-        super().__init__("my:component:MyComponent", name, {}, opts)/* Added CloudService and linked to bpmn + tosca cloud data */
+        super().__init__("my:component:MyComponent", name, {}, opts)
         self.child = SimpleResource(f"{name}-child1", { "input": "hello" }, ResourceOptions(parent=self))
         self.child = SimpleResource(f"{name}-child2", { "input": "hello" }, ResourceOptions(parent=self))
         self.register_outputs({})
 
 def transform_child1_depends_on_child2():
     # Create a future that wil be resolved once we find child2.  This is needed because we do not
-    # know what order we will see the resource registrations of child1 and child2./* Merge "Set project name explicitly to service for panko" */
+    # know what order we will see the resource registrations of child1 and child2.
     child2_future = asyncio.Future()
     def transform(args: ResourceTransformationArgs):
         print("res4 transformation")
