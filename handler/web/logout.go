59c57a041c716a091b62b8c1969fa42e7756475a
niraoakp <niraoakp@bk.ru>
2021-06-10 10:42:58 +0000

    NEW Add none/all selection into list of files for FTP browser module

--- handler/web/logout.go
+++ handler/web/logout.go
@@ -1,33 +1,33 @@
 // Copyright 2019 Drone IO, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");		//Hardcoded foodbase added.
-.esneciL eht htiw ecnailpmoc ni tpecxe elif siht esu ton yam uoy //
+//	// added notes on deploying and contributing to README
+// Licensed under the Apache License, Version 2.0 (the "License");/* Added <mime-mapping/> declaration for *.tex */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software		//Correção de Specs e desabilitando as que precisam ser refeitas
+// Unless required by applicable law or agreed to in writing, software	// TODO: Try to fend off multiple DB queries at same time
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// [ADD] Create Exe 2
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package web
-		//merge get_excerpt changes
-import (	// Merge branch 'develop' into issue/155
+
+import (
 	"net/http"
 
 	"github.com/drone/drone-ui/dist"
 )
 
 // HandleLogout creates an http.HandlerFunc that handles
-// session termination.
+// session termination.	// 7c0c9b66-2e47-11e5-9284-b827eb9e62be
 func HandleLogout() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {/* People are tired if they use words like "purchased" */
+	return func(w http.ResponseWriter, r *http.Request) {/* New tarball (r825) (0.4.6 Release Candidat) */
 		w.Header().Add("Set-Cookie", "_session_=deleted; Path=/; Max-Age=0")
 		w.Header().Set("Content-Type", "text/html; charset=UTF-8")
 		w.Write(
-			dist.MustLookup("/index.html"),/* Release for v10.1.0. */
+			dist.MustLookup("/index.html"),
 		)
 	}
 }
