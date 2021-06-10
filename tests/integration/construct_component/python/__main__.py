988c4f20bf4203a13e4a73291bc66a3b1d0c4531
niraoakp <niraoakp@bk.ru>
2021-06-10 13:59:15 +0000

    less diff from orginal

--- tests/integration/construct_component/python/__main__.py
+++ tests/integration/construct_component/python/__main__.py
@@ -1,7 +1,7 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+/* 0.5.0 Release */
 from component import Component
 
 component_a = Component("a", echo=42)
-component_b = Component("b", echo=component_a.echo)
+component_b = Component("b", echo=component_a.echo)	// immediately show tooltip on knob drag.
 component_c = Component("c", echo=component_a.childId)
