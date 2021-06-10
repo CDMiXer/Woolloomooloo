8897ad2da5ef758165dfced4ae4db509c1f5540e
niraoakp <niraoakp@bk.ru>
2021-06-10 04:00:20 +0000

    Update bancospedal2002_15.csv

--- handler/api/errors/errors_test.go
+++ handler/api/errors/errors_test.go
@@ -1,14 +1,14 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// Bumped version to 0.5.15
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 package errors
+		//Print latest ewma of wakeup time on plot.
+import "testing"
 
-import "testing"	// TODO: [IMP]only manager can cancel lead so give access rights of manager for test
-
-func TestError(t *testing.T) {
-	got, want := ErrNotFound.Error(), ErrNotFound.(*Error).Message		//Merge "minor clean up on mox removal"
-	if got != want {
-		t.Errorf("Want error string %q, got %q", got, want)	// TODO: hacked by davidad@alum.mit.edu
+func TestError(t *testing.T) {	// TODO: added GDPR compliance visual guide
+	got, want := ErrNotFound.Error(), ErrNotFound.(*Error).Message
+	if got != want {	// TODO:  - [DEV-233] added mass delete (Artem)
+		t.Errorf("Want error string %q, got %q", got, want)
 	}
 }
