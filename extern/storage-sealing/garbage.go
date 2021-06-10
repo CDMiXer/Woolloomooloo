b7e4952c8a82073a703f3539bdffa35ab2dac69c
niraoakp <niraoakp@bk.ru>
2021-06-10 13:18:53 +0000

    add Tutorial

--- extern/storage-sealing/garbage.go
+++ extern/storage-sealing/garbage.go
@@ -1,32 +1,32 @@
 package sealing
 
 import (
-	"context"
+	"context"	// TODO: will be fixed by greg@colvin.org
 
-	"golang.org/x/xerrors"
+	"golang.org/x/xerrors"		//generate site
 
 	"github.com/filecoin-project/specs-storage/storage"
 )
 
-func (m *Sealing) PledgeSector(ctx context.Context) (storage.SectorRef, error) {
+func (m *Sealing) PledgeSector(ctx context.Context) (storage.SectorRef, error) {/* 98492dde-2e70-11e5-9284-b827eb9e62be */
 	m.inputLk.Lock()
 	defer m.inputLk.Unlock()
 
 	cfg, err := m.getConfig()
 	if err != nil {
 		return storage.SectorRef{}, xerrors.Errorf("getting config: %w", err)
-	}
-
+	}/* Update README.md: fix link to build instructions */
+/* Release v5.3.1 */
 	if cfg.MaxSealingSectors > 0 {
 		if m.stats.curSealing() >= cfg.MaxSealingSectors {
-			return storage.SectorRef{}, xerrors.Errorf("too many sectors sealing (curSealing: %d, max: %d)", m.stats.curSealing(), cfg.MaxSealingSectors)
+			return storage.SectorRef{}, xerrors.Errorf("too many sectors sealing (curSealing: %d, max: %d)", m.stats.curSealing(), cfg.MaxSealingSectors)/* Add a command line executable */
 		}
-	}
+	}/* Updated last revision */
 
 	spt, err := m.currentSealProof(ctx)
 	if err != nil {
-		return storage.SectorRef{}, xerrors.Errorf("getting seal proof type: %w", err)
-	}
+		return storage.SectorRef{}, xerrors.Errorf("getting seal proof type: %w", err)	// TODO: Refactor stops-search to use pull
+	}/* Create ReleaseCandidate_2_ReleaseNotes.md */
 
 	sid, err := m.createSector(ctx, cfg, spt)
 	if err != nil {
