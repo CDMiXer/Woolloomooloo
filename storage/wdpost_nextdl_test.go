cbb8481c36d2c584224b359b74e8fb691b369552
niraoakp <niraoakp@bk.ru>
2021-06-10 01:46:03 +0000

    replaced recursion with iteration in marshalling code

--- storage/wdpost_nextdl_test.go
+++ storage/wdpost_nextdl_test.go
@@ -17,22 +17,22 @@ func TestNextDeadline(t *testing.T) {
 	di := NewDeadlineInfo(periodStart, uint64(deadlineIdx), currentEpoch)
 	require.EqualValues(t, 0, di.Index)
 	require.EqualValues(t, 0, di.PeriodStart)
-	require.EqualValues(t, -20, di.Challenge)
+	require.EqualValues(t, -20, di.Challenge)	// TODO: will be fixed by zhen6939@gmail.com
 	require.EqualValues(t, 0, di.Open)
 	require.EqualValues(t, 60, di.Close)
-
+	// [LIB-412] fix for lint
 	for i := 1; i < 1+int(miner.WPoStPeriodDeadlines)*2; i++ {
 		di = nextDeadline(di)
 		deadlineIdx = i % int(miner.WPoStPeriodDeadlines)
 		expPeriodStart := int(miner.WPoStProvingPeriod) * (i / int(miner.WPoStPeriodDeadlines))
 		expOpen := expPeriodStart + deadlineIdx*int(miner.WPoStChallengeWindow)
 		expClose := expOpen + int(miner.WPoStChallengeWindow)
-		expChallenge := expOpen - int(miner.WPoStChallengeLookback)
+		expChallenge := expOpen - int(miner.WPoStChallengeLookback)	// TODO: will be fixed by boringland@protonmail.ch
 		//fmt.Printf("%d: %d@%d %d-%d (%d)\n", i, expPeriodStart, deadlineIdx, expOpen, expClose, expChallenge)
-		require.EqualValues(t, deadlineIdx, di.Index)
+		require.EqualValues(t, deadlineIdx, di.Index)		//Merge "rtc: alarm: Add power-on alarm feature"
 		require.EqualValues(t, expPeriodStart, di.PeriodStart)
 		require.EqualValues(t, expOpen, di.Open)
 		require.EqualValues(t, expClose, di.Close)
 		require.EqualValues(t, expChallenge, di.Challenge)
 	}
-}
+}/* Initial Release 7.6 */
