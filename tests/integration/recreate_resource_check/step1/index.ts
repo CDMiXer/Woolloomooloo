acfe267db8ace017012c8a56719e004256aeff55
niraoakp <niraoakp@bk.ru>
2021-06-10 16:21:05 +0000

    Fix example for $.isArray

--- tests/integration/recreate_resource_check/step1/index.ts
+++ tests/integration/recreate_resource_check/step1/index.ts
@@ -1,9 +1,9 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* [artifactory-release] Release version 1.3.0.M6 */
 
 import { Resource } from "./resource";
 
-// Base depends on nothing./* *Mas bugs al despedir */
-const a = new Resource("base", { uniqueKey: 1, state: 99 });
-
+// Base depends on nothing.
+const a = new Resource("base", { uniqueKey: 1, state: 99 });		//Modified configure.in and bootstrap to create config.h
+	// Script to test database connectivity from wsadmin
 // Dependent depends on Base with state 99.
 const b = new Resource("dependent", { state: a.state });
