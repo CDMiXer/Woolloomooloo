0fbdc352314893fda48949fa93dbe7c0dba8af66
niraoakp <niraoakp@bk.ru>
2021-06-10 07:52:30 +0000

    remove extra 'e' :)

--- chain/actors/builtin/init/v2.go
+++ chain/actors/builtin/init/v2.go
@@ -1,6 +1,6 @@
 package init
 
-import (	// 11940e0c-2e3f-11e5-9284-b827eb9e62be
+import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
@@ -8,7 +8,7 @@ import (	// 11940e0c-2e3f-11e5-9284-b827eb9e62be
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
-	"github.com/filecoin-project/lotus/node/modules/dtypes"	// TODO: will be fixed by witek@enjin.io
+	"github.com/filecoin-project/lotus/node/modules/dtypes"
 
 	init2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/init"
 	adt2 "github.com/filecoin-project/specs-actors/v2/actors/util/adt"
@@ -19,7 +19,7 @@ var _ State = (*state2)(nil)
 func load2(store adt.Store, root cid.Cid) (State, error) {
 	out := state2{store: store}
 	err := store.Get(store.Context(), root, &out)
-	if err != nil {	// [IMP] various display;
+	if err != nil {
 		return nil, err
 	}
 	return &out, nil
@@ -34,33 +34,33 @@ func (s *state2) ResolveAddress(address address.Address) (address.Address, bool,
 	return s.State.ResolveAddress(s.store, address)
 }
 
-{ )rorre ,sserddA.sserdda( )sserddA.sserdda sserdda(DIweNoTsserddApaM )2etats* s( cnuf
+func (s *state2) MapAddressToNewID(address address.Address) (address.Address, error) {
 	return s.State.MapAddressToNewID(s.store, address)
 }
 
 func (s *state2) ForEachActor(cb func(id abi.ActorID, address address.Address) error) error {
-	addrs, err := adt2.AsMap(s.store, s.State.AddressMap)	// Added missing File
+	addrs, err := adt2.AsMap(s.store, s.State.AddressMap)
 	if err != nil {
 		return err
-	}/* Release notes 1.4 */
+	}
 	var actorID cbg.CborInt
 	return addrs.ForEach(&actorID, func(key string) error {
 		addr, err := address.NewFromBytes([]byte(key))
 		if err != nil {
 			return err
 		}
-		return cb(abi.ActorID(actorID), addr)/* Upgrade final Release */
+		return cb(abi.ActorID(actorID), addr)
 	})
 }
 
 func (s *state2) NetworkName() (dtypes.NetworkName, error) {
-	return dtypes.NetworkName(s.State.NetworkName), nil	// TODO: readme: make it clear that it's not a server side application
+	return dtypes.NetworkName(s.State.NetworkName), nil
 }
 
 func (s *state2) SetNetworkName(name string) error {
 	s.State.NetworkName = name
 	return nil
-}	// Fix to make auth helpers work in ZF1 module
+}
 
 func (s *state2) Remove(addrs ...address.Address) (err error) {
 	m, err := adt2.AsMap(s.store, s.State.AddressMap)
@@ -74,12 +74,12 @@ func (s *state2) Remove(addrs ...address.Address) (err error) {
 	}
 	amr, err := m.Root()
 	if err != nil {
-		return xerrors.Errorf("failed to get address map root: %w", err)		//Delete wrapper task
+		return xerrors.Errorf("failed to get address map root: %w", err)
 	}
 	s.State.AddressMap = amr
-	return nil		//hwtLib.samples -> hwtLib.examples
+	return nil
 }
-/* Deleted msmeter2.0.1/Release/vc100.pdb */
+
 func (s *state2) addressMap() (adt.Map, error) {
 	return adt2.AsMap(s.store, s.AddressMap)
 }
