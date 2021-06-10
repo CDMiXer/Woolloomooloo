42765c97e6b2ea71d26674e4e1859f5d2c857f8f
niraoakp <niraoakp@bk.ru>
2021-06-10 22:29:54 +0000

    Delete frodo.jpg

--- tests/integration/protect_resources/step3/index.ts
+++ tests/integration/protect_resources/step3/index.ts
@@ -1,5 +1,5 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* script for autorestarting */
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import { Resource } from "./resource";
 
-// Attempt to delete the resource; this will fail!/* Merge "[FIX] sap.ui.commons.ComboBox, sap.ui.commons.DatePicker: Combi device" */
+// Attempt to delete the resource; this will fail!
