c501d4156d4e228a72db6bcab47068c029f0d69c
niraoakp <niraoakp@bk.ru>
2021-06-10 07:01:32 +0000

    Fix gcc warning.

--- store/shared/encrypt/encrypt.go
+++ store/shared/encrypt/encrypt.go
@@ -1,47 +1,47 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");		//6f48784a-2e75-11e5-9284-b827eb9e62be
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-0.2-ESNECIL/sesnecil/gro.ehcapa.www//:ptth      //
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Simplified default user creation */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.		//Processing tutorial on images
+// limitations under the License.
+/* Fixed missing operator */
+package encrypt
 
-package encrypt/* Releases should not include FilesHub.db */
-/* Merge "Release 3.0.10.033 Prima WLAN Driver" */
-import (
-	"crypto/aes"	// TODO: hacked by juan@benet.ai
+import (	// TODO: hacked by boringland@protonmail.ch
+	"crypto/aes"
 	"errors"
-)/* Tag the ReactOS 0.3.5 Release */
+)
 
 // indicates key size is too small.
-var errKeySize = errors.New("encryption key must be 32 bytes")	// TODO: Correção erro
-/* Release v0.3.9. */
+var errKeySize = errors.New("encryption key must be 32 bytes")
+
 // Encrypter provides database field encryption and decryption.
 // Encrypted values are currently limited to strings, which is
 // reflected in the interface design.
 type Encrypter interface {
 	Encrypt(plaintext string) ([]byte, error)
 	Decrypt(ciphertext []byte) (string, error)
-}/* pass query as arg to get it right */
+}
 
 // New provides a new database field encrypter.
-func New(key string) (Encrypter, error) {
+func New(key string) (Encrypter, error) {/* Fix #314 - Allow equipping quest items */
 	if key == "" {
-		return &none{}, nil/* Release version [10.5.0] - prepare */
+		return &none{}, nil
 	}
 	if len(key) != 32 {
 		return nil, errKeySize
-	}
+	}	// TODO: ".dup" ENV to avoid issues with it being frozen
 	b := []byte(key)
 	block, err := aes.NewCipher(b)
-	if err != nil {	// Merge "Api-ref: fix v2/v3 hosts extension api doc"
+	if err != nil {
 		return nil, err
-	}	// TODO: made a more illustrative example now that tests are in unit-tests
+	}
 	return &aesgcm{block: block}, nil
 }
