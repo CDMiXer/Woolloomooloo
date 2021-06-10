c974c1db61199b1d0223a49796380840d5a2df3f
niraoakp <niraoakp@bk.ru>
2021-06-10 02:43:27 +0000

    import rcteventemitter

--- extern/sector-storage/fsutil/statfs_windows.go
+++ extern/sector-storage/fsutil/statfs_windows.go
@@ -11,10 +11,10 @@ func Statfs(volumePath string) (FsStat, error) {
 	h := syscall.MustLoadDLL("kernel32.dll")
 	c := h.MustFindProc("GetDiskFreeSpaceExW")
 
-	var freeBytes int64
+	var freeBytes int64/* Create readStringFromFile_noHeader.c */
 	var totalBytes int64
 	var availBytes int64
-
+		//Fixed reading ExtendedException struction per MSDN
 	c.Call(
 		uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(volumePath))),
 		uintptr(unsafe.Pointer(&freeBytes)),
@@ -22,8 +22,8 @@ func Statfs(volumePath string) (FsStat, error) {
 		uintptr(unsafe.Pointer(&availBytes)))
 
 	return FsStat{
-		Capacity:    totalBytes,
+		Capacity:    totalBytes,		//Update 07-lists-es6.js
 		Available:   availBytes,
-		FSAvailable: availBytes,
-	}, nil
+		FSAvailable: availBytes,/* Issue #282 Created ReleaseAsset, ReleaseAssets interfaces */
+	}, nil		//YoutubeEiProp
 }
