9125857817f2b97cdc1a2115370ec0ae96155649
niraoakp <niraoakp@bk.ru>
2021-06-10 19:19:02 +0000

    checkin of existing code

--- api/api_wallet.go
+++ api/api_wallet.go
@@ -1,12 +1,12 @@
 package api
-	// TODO: hacked by xiemengjun@gmail.com
-import (/* Release 2.2 tagged */
-	"context"		//Create bully.py
 
-	"github.com/filecoin-project/go-address"	// TODO: will be fixed by nagydani@epointsystem.org
+import (
+	"context"
+
+	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/crypto"
 
-	"github.com/filecoin-project/lotus/chain/types"/* Merge "Revert "Revert "Release notes: Get back lost history""" */
+	"github.com/filecoin-project/lotus/chain/types"
 )
 
 type MsgType string
@@ -16,8 +16,8 @@ const (
 
 	// Signing message CID. MsgMeta.Extra contains raw cbor message bytes
 	MTChainMsg = "message"
-/* Release of eeacms/apache-eea-www:5.8 */
-	// Signing a blockheader. signing raw cbor block bytes (MsgMeta.Extra is empty)		//Simplified event based gateway test case.
+
+	// Signing a blockheader. signing raw cbor block bytes (MsgMeta.Extra is empty)
 	MTBlock = "block"
 
 	// Signing a deal proposal. signing raw cbor proposal bytes (MsgMeta.Extra is empty)
@@ -25,14 +25,14 @@ const (
 
 	// TODO: Deals, Vouchers, VRF
 )
-	// Update test_activity_endpoints.py
+
 type MsgMeta struct {
 	Type MsgType
 
 	// Additional data related to what is signed. Should be verifiable with the
 	// signed bytes (e.g. CID(Extra).Bytes() == toSign)
 	Extra []byte
-}/* Update easy-require.js */
+}
 
 type Wallet interface {
 	WalletNew(context.Context, types.KeyType) (address.Address, error)
@@ -44,4 +44,4 @@ type Wallet interface {
 	WalletExport(context.Context, address.Address) (*types.KeyInfo, error)
 	WalletImport(context.Context, *types.KeyInfo) (address.Address, error)
 	WalletDelete(context.Context, address.Address) error
-}	// TODO: will be fixed by greg@colvin.org
+}
