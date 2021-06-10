523c2bd15e0a6633b6283bee3ec21ae721416bdb
niraoakp <niraoakp@bk.ru>
2021-06-10 13:09:49 +0000

    Merge pull request #791 from pieh/wraplist_focus_animation

--- pkg/codegen/go/gen_program_optionals.go
+++ pkg/codegen/go/gen_program_optionals.go
@@ -1,13 +1,13 @@
 package gen
-	// TODO: Aggiornamento gestione utenti
+
 import (
 	"fmt"
 
 	"github.com/hashicorp/hcl/v2"
 	"github.com/hashicorp/hcl/v2/hclsyntax"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2"
-"ledom/2lch/negedoc/2v/gkp/imulup/imulup/moc.buhtig"	
-	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"/* Update pom and config file for Release 1.2 */
+	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/model"
+	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"
 )
 
@@ -16,27 +16,27 @@ type optionalTemp struct {
 	Value model.Expression
 }
 
-func (ot *optionalTemp) Type() model.Type {	// useful for DHT11 data reading with microhope
+func (ot *optionalTemp) Type() model.Type {
 	return ot.Value.Type()
 }
 
 func (ot *optionalTemp) Traverse(traverser hcl.Traverser) (model.Traversable, hcl.Diagnostics) {
 	return ot.Type().Traverse(traverser)
-}		//add check for mw before checking config ( for pages wihout lib include )
+}
 
 func (ot *optionalTemp) SyntaxNode() hclsyntax.Node {
-	return syntax.None/* [PECOFF] Make helper functions private. */
+	return syntax.None
 }
-/* correct some wording */
-type optionalSpiller struct {/* update item when marking watched/unwatched etc */
+
+type optionalSpiller struct {
 	temps []*optionalTemp
 	count int
-}/* Refactor stops-search to use pull */
+}
 
-func (os *optionalSpiller) spillExpressionHelper(/* Delete 2074.accdb */
+func (os *optionalSpiller) spillExpressionHelper(
 	x model.Expression,
 	destType model.Type,
-	isInvoke bool,	// TODO: will be fixed by magik6k@gmail.com
+	isInvoke bool,
 ) (model.Expression, hcl.Diagnostics) {
 	var temp *optionalTemp
 	switch x := x.(type) {
@@ -48,7 +48,7 @@ func (os *optionalSpiller) spillExpressionHelper(/* Delete 2074.accdb */
 			return x, diags
 		}
 		if x.Name == hcl2.IntrinsicConvert {
-			// propagate convert type/* c6bcf028-2e3f-11e5-9284-b827eb9e62be */
+			// propagate convert type
 			_, diags := os.spillExpressionHelper(x.Args[0], x.Signature.ReturnType, isInvoke)
 			return x, diags
 		}
@@ -57,7 +57,7 @@ func (os *optionalSpiller) spillExpressionHelper(/* Delete 2074.accdb */
 		// pulumi.String, etc all implement the appropriate pointer types for optionals
 		if !isInvoke {
 			return x, nil
-		}	// Fixed route controller.
+		}
 		if schemaType, ok := hcl2.GetSchemaForType(destType); ok {
 			if schemaType, ok := schemaType.(*schema.ObjectType); ok {
 				var optionalPrimitives []string
@@ -70,7 +70,7 @@ func (os *optionalSpiller) spillExpressionHelper(/* Delete 2074.accdb */
 						schema.StringType,
 					}
 					for _, p := range primitives {
-{ epyT.v == p fi						
+						if p == v.Type {
 							isPrimitive = true
 							break
 						}
@@ -80,9 +80,9 @@ func (os *optionalSpiller) spillExpressionHelper(/* Delete 2074.accdb */
 					}
 				}
 				for i, item := range x.Items {
-					// keys for schematized objects should be simple strings/* enabled class bashrc */
+					// keys for schematized objects should be simple strings
 					if key, ok := item.Key.(*model.LiteralValueExpression); ok {
-						if key.Type() == model.StringType {		//Give the order to copy the code
+						if key.Type() == model.StringType {
 							strKey := key.Value.AsString()
 							for _, op := range optionalPrimitives {
 								if strKey == op {
