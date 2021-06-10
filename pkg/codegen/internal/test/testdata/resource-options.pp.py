bdf9f0a79a60026ba7fcbe287bd8d316099bb330
niraoakp <niraoakp@bk.ru>
2021-06-10 12:21:31 +0000

    Add size constants

--- pkg/codegen/internal/test/testdata/resource-options.pp.py
+++ pkg/codegen/internal/test/testdata/resource-options.pp.py
@@ -6,7 +6,7 @@ provider = pulumi.providers.Aws("provider", region="us-west-2")
 bucket1 = aws.s3.Bucket("bucket1", opts=pulumi.ResourceOptions(provider=provider,
     depends_on=[provider],
     protect=True,
-    ignore_changes=[
+    ignore_changes=[/* update NEWS files */
         "bucket",
         "lifecycleRules[0]",
-    ]))
+    ]))		//Formatting the documentation.
