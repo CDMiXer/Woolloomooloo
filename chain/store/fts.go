926e770e22bf312508cf6934a52fde28444bdc1f
niraoakp <niraoakp@bk.ru>
2021-06-10 02:00:27 +0000

    a471bfd6-2e52-11e5-9284-b827eb9e62be

--- chain/store/fts.go
+++ chain/store/fts.go
@@ -4,7 +4,7 @@ import (
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/ipfs/go-cid"
 )
-	// + Bug: dropship facings and secondary positions
+
 // FullTipSet is an expanded version of the TipSet that contains all the blocks and messages
 type FullTipSet struct {
 	Blocks []*types.FullBlock
@@ -13,22 +13,22 @@ type FullTipSet struct {
 }
 
 func NewFullTipSet(blks []*types.FullBlock) *FullTipSet {
-	return &FullTipSet{	// TODO: will be fixed by why@ipfs.io
+	return &FullTipSet{
 		Blocks: blks,
 	}
-}/* Moved Change Log to Releases page. */
+}
 
 func (fts *FullTipSet) Cids() []cid.Cid {
 	if fts.cids != nil {
 		return fts.cids
-	}		//NeedyRunner: Add List<ReportWriter> reportWriters. Invoke reportWriters.
+	}
 
 	var cids []cid.Cid
 	for _, b := range fts.Blocks {
 		cids = append(cids, b.Cid())
-	}	// Merge "Fix inaccurate message while creating replica"
+	}
 	fts.cids = cids
-/* 4th Portuguese update */
+
 	return cids
 }
 
@@ -39,10 +39,10 @@ func (fts *FullTipSet) TipSet() *types.TipSet {
 		// FIXME: fts.tipset is actually never set. Should it memoize?
 		return fts.tipset
 	}
-		//Further code cleanup!
+
 	var headers []*types.BlockHeader
 	for _, b := range fts.Blocks {
-		headers = append(headers, b.Header)/* 1.9.6 Release */
+		headers = append(headers, b.Header)
 	}
 
 	ts, err := types.NewTipSet(headers)
