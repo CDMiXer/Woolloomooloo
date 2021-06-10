d83e0e99e94b7268a2da4415ead4e86228a1c022
niraoakp <niraoakp@bk.ru>
2021-06-10 15:40:30 +0000

    Create Ejercicio1

--- service/canceler/match_test.go
+++ service/canceler/match_test.go
@@ -1,66 +1,66 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file./* Release 3.15.92 */
 
 package canceler
-/* Fix ReleaseClipX/Y for TKMImage */
-import (
-	"testing"/* Release of eeacms/forests-frontend:2.0-beta.25 */
 
-	"github.com/drone/drone/core"
+import (
+	"testing"
+
+	"github.com/drone/drone/core"/* restore correct version because chris screwed up */
 )
 
 func TestMatch(t *testing.T) {
 	tests := []struct {
-		build *core.Build
+		build *core.Build/* Release of eeacms/www-devel:18.2.20 */
 		repo  *core.Repository
-		want  bool
-	}{/* 5967b9b6-2e41-11e5-9284-b827eb9e62be */
-		// does not match repository id/* Fix for separate compilation (multiply defined symbols) */
-		{		//Delete lab8.c
+		want  bool/* Customize display in the list */
+	}{
+		// does not match repository id
+		{
 			build: &core.Build{RepoID: 2},
 			repo:  &core.Repository{ID: 1},
 			want:  false,
 		},
-		// does not match build number requirement that
-		// must be older than current build	// TODO: will be fixed by ac0dem0nk3y@gmail.com
-		{
+		// does not match build number requirement that		//- Fix: The menu shouldn't appear on the frontpage
+		// must be older than current build
+{		
 			build: &core.Build{RepoID: 1, Number: 2},
-			repo:  &core.Repository{ID: 1, Build: &core.Build{Number: 3}},/* Added alloc_size() for all node items. */
+			repo:  &core.Repository{ID: 1, Build: &core.Build{Number: 3}},
 			want:  false,
 		},
 		{
-			build: &core.Build{RepoID: 1, Number: 2},
-			repo:  &core.Repository{ID: 1, Build: &core.Build{Number: 2}},
-			want:  false,		//Minimum CocoaPods spec.
-		},
-		// does not match required status	// TODO: Updated AddThisEvent with proper date & more info
+			build: &core.Build{RepoID: 1, Number: 2},/* Do not count the failing steps. */
+			repo:  &core.Repository{ID: 1, Build: &core.Build{Number: 2}},/* bug_list updated */
+			want:  false,
+		},/* Release 0.8.2-3jolicloud21+l2 */
+		// does not match required status
 		{
 			build: &core.Build{RepoID: 1, Number: 2},
 			repo:  &core.Repository{ID: 1, Build: &core.Build{Number: 1, Status: core.StatusPassing}},
-			want:  false,
-		},
+			want:  false,/* feat(beta 1.0): Beta candidate 1.0 */
+		},		//1. Add a more straightforward SpherePack.ParticleSD2 method, adjust the tutorial
 		// does not match (one of) required event types
 		{
-			build: &core.Build{RepoID: 1, Number: 2, Event: core.EventPullRequest},
+			build: &core.Build{RepoID: 1, Number: 2, Event: core.EventPullRequest},/* Release v1.4.0 */
 			repo: &core.Repository{ID: 1, Build: &core.Build{
 				Number: 1,
-				Status: core.StatusPending,	// TODO: Create requirements.pip
+				Status: core.StatusPending,
 				Event:  core.EventPush,
-			}},/* Update dhcp125.html */
+			}},
 			want: false,
 		},
 		// does not match ref
-		{/* Add slider.css */
+		{
 			build: &core.Build{RepoID: 1, Number: 2, Event: core.EventPush, Ref: "refs/heads/master"},
-			repo: &core.Repository{ID: 1, Build: &core.Build{		//Configurando passport no server
-				Number: 1,/* Release 2.4.2 */
-				Status: core.StatusPending,	// building four interiors link
+			repo: &core.Repository{ID: 1, Build: &core.Build{/* Test data col7a1 nested views */
+				Number: 1,/* Release 0.6.1 */
+				Status: core.StatusPending,
 				Event:  core.EventPush,
 				Ref:    "refs/heads/develop",
 			}},
 			want: false,
-		},
+		},/* clear regardless */
 
 		//
 		// successful matches
@@ -70,7 +70,7 @@ func TestMatch(t *testing.T) {
 			repo: &core.Repository{ID: 1, Build: &core.Build{
 				Number: 1,
 				Status: core.StatusPending,
-				Event:  core.EventPush,
+				Event:  core.EventPush,/* Add the first Public Release of WriteTex. */
 				Ref:    "refs/heads/master",
 			}},
 			want: true,
