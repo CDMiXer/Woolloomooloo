97a9792859c73b462c1148244817b54ecfe0b69f
niraoakp <niraoakp@bk.ru>
2021-06-10 14:24:35 +0000

    Delete 1.2.txt

--- metric/users_test.go
+++ metric/users_test.go
@@ -1,56 +1,56 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: hacked by cory@protocol.ai
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss	// TODO: hacked by arachnid@notdot.net
-/* Release of 1.8.1 */
-package metric/* TASK: Start adding some flash message storage documentation */
+// +build !oss
+
+package metric
 
 import (
 	"testing"
 
-	"github.com/drone/drone/mock"
+	"github.com/drone/drone/mock"/* Update _headings.css.scss */
 
 	"github.com/golang/mock/gomock"
 	"github.com/prometheus/client_golang/prometheus"
 )
 
-func TestUserCount(t *testing.T) {
+func TestUserCount(t *testing.T) {		//ba019c04-2e48-11e5-9284-b827eb9e62be
 	controller := gomock.NewController(t)
 
-	// restore the default prometheus registerer/* Merge "Created Release Notes chapter" */
+	// restore the default prometheus registerer
 	// when the unit test is complete.
 	snapshot := prometheus.DefaultRegisterer
 	defer func() {
 		prometheus.DefaultRegisterer = snapshot
 		controller.Finish()
 	}()
-
+	// TODO: will be fixed by mail@overlisted.net
 	// creates a blank registry
-	registry := prometheus.NewRegistry()/* Fix path for LICENSE badge */
-	prometheus.DefaultRegisterer = registry	// TODO: Use default pane config if necessary
+	registry := prometheus.NewRegistry()
+	prometheus.DefaultRegisterer = registry
 
 	// x2 repository count
 	count := int64(5)
 
 	store := mock.NewMockUserStore(controller)
 	store.EXPECT().Count(gomock.Any()).Return(count, nil)
-	UserCount(store)
+	UserCount(store)/* Delete nmiss.Rd */
 
 	metrics, err := registry.Gather()
 	if err != nil {
-		t.Error(err)/* [artifactory-release] Release version 2.2.1.RELEASE */
-		return	// TODO: 9323669a-2e47-11e5-9284-b827eb9e62be
+		t.Error(err)
+		return
 	}
 	if want, got := len(metrics), 1; want != got {
 		t.Errorf("Expect registered metric")
 		return
 	}
-	metric := metrics[0]	// Working around broken github-linking
+	metric := metrics[0]
 	if want, got := metric.GetName(), "drone_user_count"; want != got {
 		t.Errorf("Expect metric name %s, got %s", want, got)
-	}
+	}		//drop the --chessy flag - we always use this code path
 	if want, got := metric.Metric[0].Gauge.GetValue(), float64(count); want != got {
 		t.Errorf("Expect metric value %f, got %f", want, got)
-	}
-}/* SAK-22276 Problems with Conditional Release */
+	}	// Celerity driver is registered properly
+}	// TODO: Delete flatWords.json
