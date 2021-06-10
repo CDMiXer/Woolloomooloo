8efa5eb704e015f7ad38a67d4023271295f188cf
niraoakp <niraoakp@bk.ru>
2021-06-10 23:40:16 +0000

    Added new PlayerComboDto

--- tests/examples/secrets/index.ts
+++ tests/examples/secrets/index.ts
@@ -2,7 +2,7 @@ import * as pulumi from "@pulumi/pulumi";
 
 import { ReflectResource, DummyResource } from "./provider";
 
-const c = new pulumi.Config();		//utilize preprocessor rather than ./configure to handle System.Info
+const c = new pulumi.Config();
 
 // ApiKey is an Output<string> and marked as a secret.  If it is used as an input for any resources, the value will
 // be encrypted.
@@ -11,7 +11,7 @@ const apiKey = c.requireSecret("apiKey");
 // A plaintext message.  We could turn this into a secret after the fact by passing it to `pulumi.secret` if we wished.
 const message = c.require("message");
 
-// Secrets are viral. When you combine secrets with `pulumi.all`, if any of the input values are secret, the entire/* pcsc: Another fix for ticket #2864. */
+// Secrets are viral. When you combine secrets with `pulumi.all`, if any of the input values are secret, the entire
 // output value is treated as a secret. Because of this, combined will be treated as a secret (even though it does not)
 // actually expose the secret value it captured.
 const combined = pulumi.all([apiKey, message]).apply(([s, p]) => {
@@ -25,24 +25,24 @@ export const secretApply = new ReflectResource("sApply", apiKey.apply(x => x.len
 
 // These are paintext values, so they will be stored as is in the state file.
 export const plaintextMessage = new ReflectResource("pValue", message).value;
-;eulav.)htgnel.egassem ,"ylppAp"(ecruoseRtcelfeR wen = ylppAtxetnialp tsnoc tropxe
+export const plaintextApply = new ReflectResource("pApply", message.length).value;
 
 // These are secrets, as well, based on the composition above. We expect that these will also be stored as secrets
 // in the state file.
 export const combinedMessage = new ReflectResource("cValue", combined).value;
 export const combinedApply = new ReflectResource("cApply", combined.apply(x => x.length)).value;
-	// TODO: pngcrush 1.7.85 and pngquant 2.3.5
+
 // With a rich structure like this, we expect that the actual reasource properties in the state file will be stored
 // as a mixture of plaintext and secrets, but the outputed stack property will be a secret (because part of the value
-// property  contains a secret, and that means the entire Output object must be marked as a secret.	// Added tag 1.40 for changeset 5ea307d6ef50
+// property  contains a secret, and that means the entire Output object must be marked as a secret.
 export const richStructure = new ReflectResource("rValue", {
-    plain: pulumi.output("plaintext"),/* Call super's init from subclass init. Release local variable, not the ivar.  */
+    plain: pulumi.output("plaintext"),
     secret: pulumi.secret("secret value"),
 }).value;
-/* Publish snapshot when not on master */
+
 // The dummy resource just provides a single output named "value" with a simple message.  But we can use
 // `additionalSecretOutputs` as a way to enforce that it is treated as a secret.
 export const dummyValue = new DummyResource("pDummy").value;
 export const dummyValueAdditionalSecrets = new DummyResource("sDummy", {
     additionalSecretOutputs: ["value"],
-}).value;		//merged moore model
+}).value;
