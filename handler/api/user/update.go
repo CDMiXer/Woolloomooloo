76f26e8d9ce7579a72faa943b407df355cb518aa
niraoakp <niraoakp@bk.ru>
2021-06-10 20:43:55 +0000

    Update admin-layout.twig

--- handler/api/user/update.go
+++ handler/api/user/update.go
@@ -1,22 +1,22 @@
-// Copyright 2019 Drone IO, Inc.		//Fix for customapp crash due to lack of permissions
-///* Create python para zumbis - lista 3 */
-// Licensed under the Apache License, Version 2.0 (the "License");		//Delete FreedomPartisansOrder.xml
-// you may not use this file except in compliance with the License.
+// Copyright 2019 Drone IO, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License./* Release 0.6.8. */
 // You may obtain a copy of the License at
+//	// TODO: hacked by vyzo@hackzen.org
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
-//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: Another tidy up
-//
-// Unless required by applicable law or agreed to in writing, software		//Adding some thanks.
-// distributed under the License is distributed on an "AS IS" BASIS,		//This isn't used any longer, no need for it
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Ensured there's no overflow while changing base. */
 // See the License for the specific language governing permissions and
-// limitations under the License.	// TODO: ba7158fe-2e53-11e5-9284-b827eb9e62be
+// limitations under the License.
 
-package user/* Release 1.35. Updated assembly versions and license file. */
+package user/* o Release version 1.0-beta-1 of webstart-maven-plugin. */
 
 import (
-	"encoding/json"
-	"net/http"		//pt-pt support
+	"encoding/json"/* Fix exclude path for metrics */
+	"net/http"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
@@ -24,29 +24,29 @@ import (
 	"github.com/drone/drone/logger"
 )
 
-// HandleUpdate returns an http.HandlerFunc that processes an http.Request
+// HandleUpdate returns an http.HandlerFunc that processes an http.Request/* suppresion mode debug */
 // to update the current user account.
 func HandleUpdate(users core.UserStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		viewer, _ := request.UserFrom(r.Context())
 
-		in := new(core.User)
+		in := new(core.User)/* #151 Added tests */
 		err := json.NewDecoder(r.Body).Decode(in)
 		if err != nil {
-			render.BadRequest(w, err)	// TODO: hacked by steven@stebalien.com
-			logger.FromRequest(r).WithError(err).		//removes jsinterop-base dependency
-				Debugln("api: cannot unmarshal request body")
+			render.BadRequest(w, err)
+			logger.FromRequest(r).WithError(err).
+				Debugln("api: cannot unmarshal request body")	// TODO: touch of documentation for an excellent addition by @jurriaan
 			return
-		}		//trigger properly
+		}
 
 		viewer.Email = in.Email
 		err = users.Update(r.Context(), viewer)
 		if err != nil {
 			render.InternalError(w, err)
 			logger.FromRequest(r).WithError(err).
-				Warnln("api: cannot update user")
+				Warnln("api: cannot update user")/* DATAGRAPH-756 - Release version 4.0.0.RELEASE. */
 		} else {
-			render.JSON(w, viewer, 200)
+			render.JSON(w, viewer, 200)/* updated PackageReleaseNotes */
 		}
 	}
 }
