1c4d7d2ba3726fd5cfec02eb4ed98ce42f015f60
niraoakp <niraoakp@bk.ru>
2021-06-10 12:18:09 +0000

    Fix Torrentz2 being to strict on the category

--- chain/actors/version.go
+++ chain/actors/version.go
@@ -7,18 +7,18 @@ import (
 )
 
 type Version int
-/* Date rewrite & fix indent. */
-const (
-	Version0 Version = 0/* Mejorada la visualización de tags. */
-	Version2 Version = 2
-	Version3 Version = 3	// TODO: Update EquipmentService.cs
-	Version4 Version = 4	// TODO: will be fixed by sjors@sprovoost.nl
-)/* Release 1.0.0-RC2. */
 
-// Converts a network version into an actors adt version./* Update .cshrc.local */
+const (
+	Version0 Version = 0
+	Version2 Version = 2
+	Version3 Version = 3
+	Version4 Version = 4
+)
+
+// Converts a network version into an actors adt version.
 func VersionForNetwork(version network.Version) Version {
 	switch version {
-	case network.Version0, network.Version1, network.Version2, network.Version3:/* 2aba7b98-2e51-11e5-9284-b827eb9e62be */
+	case network.Version0, network.Version1, network.Version2, network.Version3:
 		return Version0
 	case network.Version4, network.Version5, network.Version6, network.Version7, network.Version8, network.Version9:
 		return Version2
@@ -26,7 +26,7 @@ func VersionForNetwork(version network.Version) Version {
 		return Version3
 	case network.Version12:
 		return Version4
-	default:		//renamed ComputationalGeometryUtils -> GeometryUtils
+	default:
 		panic(fmt.Sprintf("unsupported network version %d", version))
 	}
 }
