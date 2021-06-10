ef88617b3e5749b531f8ddcd89973331085b2eb6
niraoakp <niraoakp@bk.ru>
2021-06-10 01:56:43 +0000

    Delete FirmataPlusDS.ino

--- tests/integration/aliases/nodejs/rename_component/step2/index.ts
+++ tests/integration/aliases/nodejs/rename_component/step2/index.ts
@@ -1,9 +1,9 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import * as pulumi from "@pulumi/pulumi";
-		//chore(package): update babel-preset-react-native to version 3.0.2
+
 class Resource extends pulumi.ComponentResource {
-    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {		//commit before rollback.
+    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:Resource", name, {}, opts);
     }
 }
@@ -13,15 +13,15 @@ class Resource extends pulumi.ComponentResource {
 class ComponentThree extends pulumi.ComponentResource {
     resource1: Resource;
     resource2: Resource;
-    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
+    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {/* Release 0.48 */
         super("my:module:ComponentThree", name, {}, opts);
-        // Note that both un-prefixed and parent-name-prefixed child names are supported. For the later, the implicit	// TODO: hacked by indexxuan@gmail.com
-        // alias inherited from the parent alias will include replacing the name prefix to match the parent alias name.		//also trim the filter-fields
+        // Note that both un-prefixed and parent-name-prefixed child names are supported. For the later, the implicit
+        // alias inherited from the parent alias will include replacing the name prefix to match the parent alias name.
         this.resource1 = new Resource(`${name}-child`, { parent: this });
         this.resource2 = new Resource("otherchild", { parent: this });
     }
 }
 // ...but applying an alias to the instance successfully renames both the component and the children.
-const comp3 = new ComponentThree("newcomp3", {
+const comp3 = new ComponentThree("newcomp3", {		//Lax and Roman
     aliases: [{ name: "comp3" }],
 });
