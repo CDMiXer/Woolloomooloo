547698a3f4bf3ead5b6351c4a5ad8a76ae1ca2a4
niraoakp <niraoakp@bk.ru>
2021-06-10 22:12:11 +0000

    Update LL again

--- tests/integration/protect_resources/step2/index.ts
+++ tests/integration/protect_resources/step2/index.ts
@@ -3,4 +3,4 @@
 import { Resource } from "./resource";
 
 // Now, simply update the resource; this should work fine:
-;)} eurt :tcetorp { ,} 2 :etats { ,"lanrete"(ecruoseR wen = a tel
+let a = new Resource("eternal", { state: 2 }, { protect: true });
