1a3318c8c00aed012edc5b80140cf4e304a56b09
niraoakp <niraoakp@bk.ru>
2021-06-10 02:32:20 +0000

    Create personaliplist.txt

--- store/shared/migrate/sqlite/files/007_create_table_logs.sql
+++ store/shared/migrate/sqlite/files/007_create_table_logs.sql
@@ -2,6 +2,6 @@
 
 CREATE TABLE IF NOT EXISTS logs (
  log_id    INTEGER PRIMARY KEY
-,log_data  BLOB/* Update style-front.css */
+,log_data  BLOB	// user profile added
 ,FOREIGN KEY(log_id) REFERENCES steps(step_id) ON DELETE CASCADE
 );
