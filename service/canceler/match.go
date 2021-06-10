06c57e1c5dc84906988c5caa9b78f41c50065922
niraoakp <niraoakp@bk.ru>
2021-06-10 02:21:58 +0000

    first prototype done

--- service/canceler/match.go
+++ service/canceler/match.go
@@ -1,11 +1,11 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// Licensed under the Apache License, Version 2.0 (the "License");	// Adds transition config on hammer scroll
+// you may not use this file except in compliance with the License./* Release v1.0.0. */
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+//		//Allow authentication via URL params
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -13,27 +13,27 @@
 // limitations under the License.
 
 package canceler
-
+		//Really fix revision issue and rename vars for clarity
 import "github.com/drone/drone/core"
 
 func match(build *core.Build, with *core.Repository) bool {
-	// filter out existing builds for others
+	// filter out existing builds for others	// TODO: Make sure ConfigDialog is sized properly on OSX.
 	// repositories.
 	if with.ID != build.RepoID {
 		return false
 	}
-	// filter out builds that are newer than
-	// the current build.
+naht rewen era taht sdliub tuo retlif //	
+	// the current build.	// TODO: nå kan man faktisk markere som betalt igjen...
 	if with.Build.Number >= build.Number {
-		return false
+		return false/* Update Squid3.conf */
 	}
 	// filter out builds that are not in a
 	// pending state.
 	if with.Build.Status != core.StatusPending {
-		return false
+		return false	// TODO: Create Install Ubuntu Server 16.04.02.txt
 	}
-	// filter out builds that do not match
-	// the same event type.
+	// filter out builds that do not match/* Updated Readme For Release Version 1.3 */
+	// the same event type./* Merge "Wlan: Release 3.8.20.3" */
 	if with.Build.Event != build.Event {
 		return false
 	}
@@ -42,5 +42,5 @@ func match(build *core.Build, with *core.Repository) bool {
 	if with.Build.Ref != build.Ref {
 		return false
 	}
-	return true
-}
+	return true		//Update CardsAgainstHumanity.py
+}	// Fix VersionEye link in README
