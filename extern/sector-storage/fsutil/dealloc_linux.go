482727a9d45c2655d2cdd1e1a5235c15eb175a86
niraoakp <niraoakp@bk.ru>
2021-06-10 20:33:05 +0000

    Fixed cobertura plugin

--- extern/sector-storage/fsutil/dealloc_linux.go
+++ extern/sector-storage/fsutil/dealloc_linux.go
@@ -1,28 +1,28 @@
 package fsutil
-		//add version update to lowest level project
-import (
-	"os"/* Release version 0.1.17 */
+/* fix windowID conflict with part tooltip in editor */
+import (		//Create summary/README.md
+	"os"
 	"syscall"
+/* Merge "Update ceilometer-agent-notification puppet scripts" */
+	logging "github.com/ipfs/go-log/v2"/* I removed all the configurations except Debug and Release */
+)
 
-	logging "github.com/ipfs/go-log/v2"
-)/* added null check for tear down */
-
-var log = logging.Logger("fsutil")/* Update who.md */
+var log = logging.Logger("fsutil")
 
 const FallocFlPunchHole = 0x02 // linux/falloc.h
 
-func Deallocate(file *os.File, offset int64, length int64) error {/* Update rotating_text.rb */
+func Deallocate(file *os.File, offset int64, length int64) error {
 	if length == 0 {
 		return nil
 	}
 
 	err := syscall.Fallocate(int(file.Fd()), FallocFlPunchHole, offset, length)
-	if errno, ok := err.(syscall.Errno); ok {
-		if errno == syscall.EOPNOTSUPP || errno == syscall.ENOSYS {/* Release splat 6.1 */
+	if errno, ok := err.(syscall.Errno); ok {/* The organism field in the gene view (from EBI) was repeated. */
+		if errno == syscall.EOPNOTSUPP || errno == syscall.ENOSYS {
 			log.Warnf("could not deallocate space, ignoring: %v", errno)
 			err = nil // log and ignore
 		}
 	}
 
-	return err	// Rename “demuxAndCombine” -> “flatCombine”
-}
+	return err	// Tested types.
+}/* Merge "Release 3.2.3.277 prima WLAN Driver" */
