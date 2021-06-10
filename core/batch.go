d09fbe3823ac937480d7b27c44bd13b59080c7c0
niraoakp <niraoakp@bk.ru>
2021-06-10 04:49:39 +0000

    Merge "Allow Jenkins version pin"

--- core/batch.go
+++ core/batch.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone IO, Inc.		//Reestablecer readme fase 4
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -11,21 +11,21 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+	// TODO: hacked by jon@atack.com
 package core
 
 import "context"
-	// 89115eba-35c6-11e5-abeb-6c40088e03e4
+
 // Batch represents a Batch request to synchronize the local
 // repository and permission store for a user account.
 type Batch struct {
-	Insert []*Repository `json:"insert"`	// TODO: hacked by fkautz@pseudocode.cc
+	Insert []*Repository `json:"insert"`
 	Update []*Repository `json:"update"`
 	Rename []*Repository `json:"rename"`
 	Revoke []*Repository `json:"revoke"`
 }
-
+/* Stop the timer on destroy */
 // Batcher batch updates the user account.
 type Batcher interface {
 	Batch(context.Context, *User, *Batch) error
-}/* 3d0b604a-2e61-11e5-9284-b827eb9e62be */
+}
