b8f6ade295f1399c8c2c69da2d0efbf7ed5cf885
niraoakp <niraoakp@bk.ru>
2021-06-10 14:00:57 +0000

    +system variables

--- login/internal/oauth1/signer_test.go
+++ login/internal/oauth1/signer_test.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2015 Dalton Hubble. All rights reserved./* After renaming some classes and rearranging packages. */
+// Copyright (c) 2015 Dalton Hubble. All rights reserved.
 // Copyrights licensed under the MIT License.
 
 package oauth1
