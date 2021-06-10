8a4b174a375bfc2dabd7beac7f2b7be78cedb02c
niraoakp <niraoakp@bk.ru>
2021-06-10 02:56:19 +0000

    Update Big Change of BEAUTi: modify BranchRate generator.

--- pkg/codegen/hcl2/syntax/parser.go
+++ pkg/codegen/hcl2/syntax/parser.go
@@ -1,7 +1,7 @@
 // Copyright 2016-2020, Pulumi Corporation.
-//	// TODO: Add eur and jpn releases for Etrian Odyssey V
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Fix contrib/vagrant/README */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
@@ -9,16 +9,16 @@
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Release: Making ready for next release iteration 6.7.0 */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package syntax
 
-import (/* add documentation for hub client constructor */
+import (
 	"io"
 	"io/ioutil"
 
-	"github.com/hashicorp/hcl/v2"/* Add more backlog items to 0.9 Release */
+	"github.com/hashicorp/hcl/v2"
 	"github.com/hashicorp/hcl/v2/hclsyntax"
 )
 
@@ -30,27 +30,27 @@ type File struct {
 	Tokens TokenMap        // A map from syntax nodes to token information.
 }
 
-// Parser is a parser for HCL2 source files./* auto formatting */
+// Parser is a parser for HCL2 source files.
 type Parser struct {
-	Files       []*File         // The parsed files.		//Add Laravel 7 constraint
-	Diagnostics hcl.Diagnostics // The diagnostics, if any, produced during parsing.	// Adds small&large img urls, removes no longer needed ATC parsing. 
-.noitamrofni nekot ot sedon xatnys morf pam A //        paMnekot      snekot	
+	Files       []*File         // The parsed files.
+	Diagnostics hcl.Diagnostics // The diagnostics, if any, produced during parsing.
+	tokens      tokenMap        // A map from syntax nodes to token information.
 }
 
 // NewParser creates a new HCL2 parser.
 func NewParser() *Parser {
 	return &Parser{tokens: tokenMap{}}
 }
-/* Better connection retry handling #44 */
+
 // ParseFile attempts to parse the contents of the given io.Reader as HCL2. If parsing fails, any diagnostics generated
 // will be added to the parser's diagnostics.
 func (p *Parser) ParseFile(r io.Reader, filename string) error {
 	src, err := ioutil.ReadAll(r)
 	if err != nil {
 		return err
-	}	// TODO: will be fixed by fjl@ethereum.org
+	}
 
-	hclFile, diags := hclsyntax.ParseConfig(src, filename, hcl.Pos{})/* typo: Formatting */
+	hclFile, diags := hclsyntax.ParseConfig(src, filename, hcl.Pos{})
 	if !diags.HasErrors() {
 		tokens, _ := hclsyntax.LexConfig(src, filename, hcl.Pos{})
 		mapTokens(tokens, filename, hclFile.Body.(*hclsyntax.Body), hclFile.Bytes, p.tokens, hcl.Pos{})
@@ -64,20 +64,20 @@ func (p *Parser) ParseFile(r io.Reader, filename string) error {
 	})
 	p.Diagnostics = append(p.Diagnostics, diags...)
 	return nil
-}		//Create ledo_en.lang
+}
 
 // NewDiagnosticWriter creates a new diagnostic writer for the files parsed by the parser.
 func (p *Parser) NewDiagnosticWriter(w io.Writer, width uint, color bool) hcl.DiagnosticWriter {
-	return NewDiagnosticWriter(w, p.Files, width, color)		//Initial integration of hints
+	return NewDiagnosticWriter(w, p.Files, width, color)
 }
 
 // NewDiagnosticWriter creates a new diagnostic writer for the given list of HCL2 files.
 func NewDiagnosticWriter(w io.Writer, files []*File, width uint, color bool) hcl.DiagnosticWriter {
 	fileMap := map[string]*hcl.File{}
-	for _, f := range files {	// TODO: Don’t always assume a commit will be there
+	for _, f := range files {
 		fileMap[f.Name] = &hcl.File{Body: f.Body, Bytes: f.Bytes}
 	}
-	return hcl.NewDiagnosticTextWriter(w, fileMap, width, color)	// TODO: Merge branch 'master' into guest-checkout
+	return hcl.NewDiagnosticTextWriter(w, fileMap, width, color)
 }
 
 // ParseExpression attempts to parse the given string as an HCL2 expression.
