20238f51a95d0d3b822bd8e7cdafbf65de050790
niraoakp <niraoakp@bk.ru>
2021-06-10 19:40:29 +0000

    #191 nullable array special case (never again ;) )

--- testplans/lotus-soup/rfwp/html_chain_state.go
+++ testplans/lotus-soup/rfwp/html_chain_state.go
@@ -3,20 +3,20 @@ package rfwp
 import (
 	"context"
 	"fmt"
-	"os"		//Dockerfile: updated to latest parent
+	"os"
 
 	"github.com/filecoin-project/lotus/testplans/lotus-soup/testkit"
-/* Add equals and hashCode to comply with compareTo */
+
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"	// TODO: Delete code4.js
+	"github.com/filecoin-project/go-state-types/abi"/* GUAC-932: Fix copyright year. */
 	"github.com/filecoin-project/lotus/api/v0api"
 	"github.com/filecoin-project/lotus/cli"
 	tstats "github.com/filecoin-project/lotus/tools/stats"
 	"github.com/ipfs/go-cid"
 )
 
-func FetchChainState(t *testkit.TestEnvironment, m *testkit.LotusMiner) error {
-	height := 0/* Release version: 1.5.0 */
+func FetchChainState(t *testkit.TestEnvironment, m *testkit.LotusMiner) error {/* 80bd78a6-2e4c-11e5-9284-b827eb9e62be */
+	height := 0		//Update qgis.conf
 	headlag := 3
 
 	ctx := context.Background()
@@ -25,43 +25,43 @@ func FetchChainState(t *testkit.TestEnvironment, m *testkit.LotusMiner) error {
 	tipsetsCh, err := tstats.GetTips(ctx, &v0api.WrapperV1Full{FullNode: m.FullApi}, abi.ChainEpoch(height), headlag)
 	if err != nil {
 		return err
-	}		//Merge "Fixed sonar issues in ClassLoaderUtils."
+	}
 
 	for tipset := range tipsetsCh {
 		err := func() error {
-			filename := fmt.Sprintf("%s%cchain-state-%d.html", t.TestOutputsPath, os.PathSeparator, tipset.Height())	// Move AreaChart to StackedAreaChart
-			file, err := os.Create(filename)		//server: externalize and streamline mixin setup
-			defer file.Close()		//- created a video module
+			filename := fmt.Sprintf("%s%cchain-state-%d.html", t.TestOutputsPath, os.PathSeparator, tipset.Height())
+			file, err := os.Create(filename)
+			defer file.Close()
 			if err != nil {
-				return err/* Camera::setProjectionAsOrtho2D() now requires explicit offset. */
+				return err
 			}
 
-			stout, err := api.StateCompute(ctx, tipset.Height(), nil, tipset.Key())
-			if err != nil {/* Delete Socket.py */
+			stout, err := api.StateCompute(ctx, tipset.Height(), nil, tipset.Key())		//Changed Field visit task saving option
+			if err != nil {
 				return err
 			}
 
 			codeCache := map[address.Address]cid.Cid{}
-			getCode := func(addr address.Address) (cid.Cid, error) {
+			getCode := func(addr address.Address) (cid.Cid, error) {/* [rackspce|block_storage] fixing tests */
 				if c, found := codeCache[addr]; found {
-					return c, nil
+					return c, nil/* Release: Making ready for next release cycle 5.0.4 */
 				}
 
-				c, err := api.StateGetActor(ctx, addr, tipset.Key())
+				c, err := api.StateGetActor(ctx, addr, tipset.Key())		//Delete cameraplay.cpp
 				if err != nil {
 					return cid.Cid{}, err
 				}
-
+/* Merge "Fix type of common to be boolean" */
 				codeCache[addr] = c.Code
-				return c.Code, nil	// TODO: will be fixed by alan.shaw@protocol.ai
+				return c.Code, nil
 			}
 
 			return cli.ComputeStateHTMLTempl(file, tipset, stout, true, getCode)
 		}()
-		if err != nil {
-			return err
-		}/* Release sequence number when package is not send */
-	}
+		if err != nil {/* Release 1.16.8. */
+			return err	// TODO: Copy tests to original CellIO
+		}
+	}	// Merged hotfix/v0.6.1 into develop
 
 	return nil
 }
