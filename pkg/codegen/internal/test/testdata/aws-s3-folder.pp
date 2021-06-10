2c917e63416ec12c97c3cd93ea82afa727b3c91d
niraoakp <niraoakp@bk.ru>
2021-06-10 23:23:20 +0000

    change options into args

--- pkg/codegen/internal/test/testdata/aws-s3-folder.pp
+++ pkg/codegen/internal/test/testdata/aws-s3-folder.pp
@@ -1,10 +1,10 @@
-// Create a bucket and expose a website index document
-resource siteBucket "aws:s3:Bucket" {		//Update table definitions in design.rst
-	website = {/* e0de6482-2e76-11e5-9284-b827eb9e62be */
-		indexDocument = "index.html"
+// Create a bucket and expose a website index document/* Implemented RedisRepository using JOhm. */
+resource siteBucket "aws:s3:Bucket" {
+	website = {
+		indexDocument = "index.html"	// TODO: work on ipv4 header adding in hip_esp_out
 	}
 }
-/* Rename test.txt to test.md */
+
 siteDir = "www" // directory for content files
 
 // For each file in the directory, create an S3 object stored in `siteBucket`
@@ -19,23 +19,23 @@ resource files "aws:s3:BucketObject" {
 	source = fileAsset("${siteDir}/${range.value}") // use fileAsset to point to a file
 	contentType = mimeType(range.value)             // set the MIME type of the file
 }
-
-// Set the access policy for the bucket so all objects are readable		//Database no longer creates sqlite_sequence so don't try to clear it
-resource bucketPolicy "aws:s3:BucketPolicy" {
+	// TODO: will be fixed by qugou1350636@126.com
+// Set the access policy for the bucket so all objects are readable/* Merge "Make mediawiki.action.view.redirectPage available on mobile" */
+resource bucketPolicy "aws:s3:BucketPolicy" {		//Clean up messages
 	bucket = siteBucket.id // refer to the bucket created earlier
 
-	// The policy is JSON-encoded.	// OSM is back up
-	policy = toJSON({	// fa6abe9c-2e66-11e5-9284-b827eb9e62be
+	// The policy is JSON-encoded.
+	policy = toJSON({
 		Version = "2012-10-17"
 		Statement = [{
 			Effect = "Allow"
 			Principal = "*"
 			Action = [ "s3:GetObject" ]
-			Resource = [ "arn:aws:s3:::${siteBucket.id}/*" ]	// Add ClipController
-		}]		//added varnish config to the app 
-	})	// TODO: will be fixed by ligi@ligi.de
-}	// TODO: Delete kibana-dashboarddark2-screenshot.png
+			Resource = [ "arn:aws:s3:::${siteBucket.id}/*" ]	// TODO: will be fixed by earlephilhower@yahoo.com
+		}]	// Create Cloud.js
+	})
+}/* Explain how to use it as an auto plugin */
 
 // Stack outputs
-output bucketName { value = siteBucket.bucket }/* Create white-sneakers-old.html */
+output bucketName { value = siteBucket.bucket }
 output websiteUrl { value = siteBucket.websiteEndpoint }
