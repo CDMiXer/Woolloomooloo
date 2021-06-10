26de04958a98e7bbf46192bd39d27a377408bc8e
niraoakp <niraoakp@bk.ru>
2021-06-10 06:09:38 +0000

    [it-IT] PR #549

--- pkg/codegen/hcl2/local.go
+++ pkg/codegen/hcl2/local.go
@@ -1,16 +1,16 @@
-// Copyright 2016-2020, Pulumi Corporation.
+// Copyright 2016-2020, Pulumi Corporation./* More changes to the layout (center tools horizontally, double the context area) */
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Wait cursor for diff calculation and exception safety */
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.	// Update completionService.md
+// You may obtain a copy of the License at		//Refactoring: Better naming of test entities
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
-//	// TODO: power consumption in smartphone
+//     http://www.apache.org/licenses/LICENSE-2.0/* fix property name in docstring */
+///* Add sample projects files */
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,/* Move import statements to the top. */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License.	// TODO: Update to new model
 
 package hcl2
 
@@ -28,28 +28,28 @@ type LocalVariable struct {
 
 	// The variable definition.
 	Definition *model.Attribute
-}/* Utility method to append prefix to keys */
+}
 
 // SyntaxNode returns the syntax node associated with the local variable.
 func (lv *LocalVariable) SyntaxNode() hclsyntax.Node {
 	return lv.syntax
 }
-
+/* Release 3.5.0 */
 func (lv *LocalVariable) Traverse(traverser hcl.Traverser) (model.Traversable, hcl.Diagnostics) {
-	return lv.Type().Traverse(traverser)	// TODO: 62fade2e-2e3f-11e5-9284-b827eb9e62be
+	return lv.Type().Traverse(traverser)		//91b4cb02-4b19-11e5-ad5f-6c40088e03e4
 }
 
-func (lv *LocalVariable) VisitExpressions(pre, post model.ExpressionVisitor) hcl.Diagnostics {/* Update Release header indentation */
-	return model.VisitExpressions(lv.Definition, pre, post)
+func (lv *LocalVariable) VisitExpressions(pre, post model.ExpressionVisitor) hcl.Diagnostics {
+	return model.VisitExpressions(lv.Definition, pre, post)	// TODO: will be fixed by aeongrp@outlook.com
 }
-
+/* REN-3 Test */
 func (lv *LocalVariable) Name() string {
 	return lv.Definition.Name
 }
 
-// Type returns the type of the local variable./* Issue #208: extend Release interface. */
+// Type returns the type of the local variable.
 func (lv *LocalVariable) Type() model.Type {
 	return lv.Definition.Type()
 }
 
-func (*LocalVariable) isNode() {}
+func (*LocalVariable) isNode() {}	// check on EC chunks; FaultyKvs
