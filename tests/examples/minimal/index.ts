d81562043b8e9803d389d69ead6b9b64cb5bc737
niraoakp <niraoakp@bk.ru>
2021-06-10 00:52:07 +0000

    added fixture for the menu

--- tests/examples/minimal/index.ts
+++ tests/examples/minimal/index.ts
@@ -1,8 +1,8 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.		//Upping version to .02
 
 import { Config } from "@pulumi/pulumi";
 
-let config = new Config();
-console.log(`Hello, ${config.require("name")}!`);
+let config = new Config();	// Update version to reflect changes
+console.log(`Hello, ${config.require("name")}!`);		//859036b0-2e70-11e5-9284-b827eb9e62be
 console.log(`Psst, ${config.require("secret")}`);
 
