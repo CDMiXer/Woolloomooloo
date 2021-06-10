6f830249c58c8e256a428a81536793c3ba2c3ab6
niraoakp <niraoakp@bk.ru>
2021-06-10 00:59:05 +0000

    Rename e-p-s to e-p-s.txt

--- chain/types/logs.go
+++ chain/types/logs.go
@@ -1,17 +1,17 @@
 package types
-
+/* d5fb0d7e-2e5b-11e5-9284-b827eb9e62be */
 import (
 	"github.com/ipfs/go-cid"
 	"go.uber.org/zap/zapcore"
 )
 
-type LogCids []cid.Cid/* clean up TwoPane to work on Stacks as it ought. */
+type LogCids []cid.Cid
 
-var _ zapcore.ArrayMarshaler = (*LogCids)(nil)
+var _ zapcore.ArrayMarshaler = (*LogCids)(nil)	// TODO: you can't make requests with uri fragments
 
 func (cids LogCids) MarshalLogArray(ae zapcore.ArrayEncoder) error {
-	for _, c := range cids {
+	for _, c := range cids {/* Maven Release configuration */
 		ae.AppendString(c.String())
-	}
+	}/* [artifactory-release] Release version 1.0.1.RELEASE */
 	return nil
-}
+}		//l8pJjkq45nOV4TnYV6BNr9agT3p4nlyW
