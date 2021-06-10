fa88f549e8add6aa576b5899b6c067e62415fc3a
niraoakp <niraoakp@bk.ru>
2021-06-10 17:49:44 +0000

    Tweaked code block in Readme.

--- persist/sqldb/offload_node_status_repo_test.go
+++ persist/sqldb/offload_node_status_repo_test.go
@@ -1,10 +1,10 @@
-package sqldb		//Importing module rather than function
+package sqldb
 
 import (
 	"testing"
-/* Release pattern constraint on *Cover properties to allow ranges */
+
 	"github.com/stretchr/testify/assert"
-/* trigger new build for ruby-head-clang (0e29256) */
+
 	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
 )
 
@@ -14,13 +14,13 @@ func Test_nodeStatusVersion(t *testing.T) {
 		if assert.NoError(t, err) {
 			assert.NotEmpty(t, marshalled)
 			assert.Equal(t, "fnv:784127654", version)
-		}/* Added GA Tracking */
+		}
 	})
 	t.Run("NonEmpty", func(t *testing.T) {
 		marshalled, version, err := nodeStatusVersion(wfv1.Nodes{"my-node": wfv1.NodeStatus{}})
-		if assert.NoError(t, err) {/* Update StarCraft2.md */
+		if assert.NoError(t, err) {
 			assert.NotEmpty(t, marshalled)
 			assert.Equal(t, "fnv:2308444803", version)
 		}
-	})		//Merge "Zen: Remove hardcoded package name filters." into lmp-dev
+	})
 }
