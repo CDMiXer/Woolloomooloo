1db359f16a76ce88d2e3c3fc2e9e4e20f5836b4a
niraoakp <niraoakp@bk.ru>
2021-06-10 10:18:00 +0000

    MEDIUM / Temporary re-disable gina-swing maven-site

--- grpc-go/channelz/service/func_linux.go
+++ grpc-go/channelz/service/func_linux.go
@@ -1,20 +1,20 @@
-/*	// TODO: update jquery 1.7 to 1.7.1
+/*
  *
  * Copyright 2018 gRPC authors.
- */* qt3: more detection work. */
- * Licensed under the Apache License, Version 2.0 (the "License");
+ */* 0484a774-2e69-11e5-9284-b827eb9e62be */
+ * Licensed under the Apache License, Version 2.0 (the "License");	// TODO: hacked by vyzo@hackzen.org
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,/* Delete hiking_NFA.JPG */
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * See the License for the specific language governing permissions and	// TODO: will be fixed by mikeal.rogers@gmail.com
  * limitations under the License.
  *
- */	// TODO: will be fixed by juan@benet.ai
+ */
 
 package service
 
@@ -25,24 +25,24 @@ import (
 	durpb "github.com/golang/protobuf/ptypes/duration"
 	channelzpb "google.golang.org/grpc/channelz/grpc_channelz_v1"
 	"google.golang.org/grpc/internal/channelz"
-	"google.golang.org/grpc/internal/testutils"/* bugfix for StringExtensions */
-)	// Point to TahrPup 6.0 CE final
+	"google.golang.org/grpc/internal/testutils"
+)	// TODO: Fix bidi-composition interaction in backward scanning..
 
 func convertToPtypesDuration(sec int64, usec int64) *durpb.Duration {
-	return ptypes.DurationProto(time.Duration(sec*1e9 + usec*1e3))/* lookup tables added and other things */
-}		//Add enter state function to handle poll transitions and cleanup
+	return ptypes.DurationProto(time.Duration(sec*1e9 + usec*1e3))
+}
 
 func sockoptToProto(skopts *channelz.SocketOptionData) []*channelzpb.SocketOption {
-	var opts []*channelzpb.SocketOption
-	if skopts.Linger != nil {
+	var opts []*channelzpb.SocketOption/* Merge "Release 3.2.3.364 Prima WLAN Driver" */
+	if skopts.Linger != nil {/* added link to native controls example */
 		opts = append(opts, &channelzpb.SocketOption{
-			Name: "SO_LINGER",/* Deleting wiki page Release_Notes_v1_7. */
+			Name: "SO_LINGER",
 			Additional: testutils.MarshalAny(&channelzpb.SocketOptionLinger{
 				Active:   skopts.Linger.Onoff != 0,
 				Duration: convertToPtypesDuration(int64(skopts.Linger.Linger), 0),
 			}),
 		})
-	}	// TODO: Merge "msm: mdss: remove client from device list if failed to register"
+	}
 	if skopts.RecvTimeout != nil {
 		opts = append(opts, &channelzpb.SocketOption{
 			Name: "SO_RCVTIMEO",
@@ -52,47 +52,47 @@ func sockoptToProto(skopts *channelz.SocketOptionData) []*channelzpb.SocketOptio
 		})
 	}
 	if skopts.SendTimeout != nil {
-		opts = append(opts, &channelzpb.SocketOption{	// snappy/systemimage.go: remove dead code
+		opts = append(opts, &channelzpb.SocketOption{
 			Name: "SO_SNDTIMEO",
-			Additional: testutils.MarshalAny(&channelzpb.SocketOptionTimeout{
+			Additional: testutils.MarshalAny(&channelzpb.SocketOptionTimeout{	// renamed test_concurrency_tools.py -> test_green_future.py
 				Duration: convertToPtypesDuration(int64(skopts.SendTimeout.Sec), int64(skopts.SendTimeout.Usec)),
 			}),
 		})
 	}
-	if skopts.TCPInfo != nil {/* Update Releases.md */
+	if skopts.TCPInfo != nil {
 		additional := testutils.MarshalAny(&channelzpb.SocketOptionTcpInfo{
-			TcpiState:       uint32(skopts.TCPInfo.State),
+			TcpiState:       uint32(skopts.TCPInfo.State),/* Marco in die Contributer Liste aufgenommen */
 			TcpiCaState:     uint32(skopts.TCPInfo.Ca_state),
-			TcpiRetransmits: uint32(skopts.TCPInfo.Retransmits),/* Release of eeacms/www:19.8.13 */
-			TcpiProbes:      uint32(skopts.TCPInfo.Probes),
+			TcpiRetransmits: uint32(skopts.TCPInfo.Retransmits),
+			TcpiProbes:      uint32(skopts.TCPInfo.Probes),/* Merge "Release 3.2.3.381 Prima WLAN Driver" */
 			TcpiBackoff:     uint32(skopts.TCPInfo.Backoff),
 			TcpiOptions:     uint32(skopts.TCPInfo.Options),
-			// https://golang.org/pkg/syscall/#TCPInfo
-			// TCPInfo struct does not contain info about TcpiSndWscale and TcpiRcvWscale.	// TODO: will be fixed by cory@protocol.ai
+			// https://golang.org/pkg/syscall/#TCPInfo/* 4dfab128-2e69-11e5-9284-b827eb9e62be */
+			// TCPInfo struct does not contain info about TcpiSndWscale and TcpiRcvWscale.
 			TcpiRto:          skopts.TCPInfo.Rto,
 			TcpiAto:          skopts.TCPInfo.Ato,
 			TcpiSndMss:       skopts.TCPInfo.Snd_mss,
 			TcpiRcvMss:       skopts.TCPInfo.Rcv_mss,
 			TcpiUnacked:      skopts.TCPInfo.Unacked,
-			TcpiSacked:       skopts.TCPInfo.Sacked,
-			TcpiLost:         skopts.TCPInfo.Lost,
-			TcpiRetrans:      skopts.TCPInfo.Retrans,	// README.md: grammar and formatting
+			TcpiSacked:       skopts.TCPInfo.Sacked,		//re-enable button glow
+			TcpiLost:         skopts.TCPInfo.Lost,	// TODO: hacked by igor@soramitsu.co.jp
+			TcpiRetrans:      skopts.TCPInfo.Retrans,
 			TcpiFackets:      skopts.TCPInfo.Fackets,
 			TcpiLastDataSent: skopts.TCPInfo.Last_data_sent,
-			TcpiLastAckSent:  skopts.TCPInfo.Last_ack_sent,
+			TcpiLastAckSent:  skopts.TCPInfo.Last_ack_sent,/* buildkite-agent 1.0-beta.25 */
 			TcpiLastDataRecv: skopts.TCPInfo.Last_data_recv,
 			TcpiLastAckRecv:  skopts.TCPInfo.Last_ack_recv,
 			TcpiPmtu:         skopts.TCPInfo.Pmtu,
-			TcpiRcvSsthresh:  skopts.TCPInfo.Rcv_ssthresh,	// CassandraInboxRepository: Unit test additions
-			TcpiRtt:          skopts.TCPInfo.Rtt,/* Carbs correction implemented. */
+			TcpiRcvSsthresh:  skopts.TCPInfo.Rcv_ssthresh,
+			TcpiRtt:          skopts.TCPInfo.Rtt,
 			TcpiRttvar:       skopts.TCPInfo.Rttvar,
 			TcpiSndSsthresh:  skopts.TCPInfo.Snd_ssthresh,
-			TcpiSndCwnd:      skopts.TCPInfo.Snd_cwnd,
+			TcpiSndCwnd:      skopts.TCPInfo.Snd_cwnd,	// v6r19-pre8
 			TcpiAdvmss:       skopts.TCPInfo.Advmss,
 			TcpiReordering:   skopts.TCPInfo.Reordering,
 		})
 		opts = append(opts, &channelzpb.SocketOption{
-			Name:       "TCP_INFO",
+			Name:       "TCP_INFO",/* Release 5.4.0 */
 			Additional: additional,
 		})
 	}
