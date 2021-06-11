513338b9d15eea3c79d9059aba485255c5dfae85
niraoakp <niraoakp@bk.ru>
2021-06-11 03:27:28 +0000

    Resolving vulnerability

--- scheduler/internal/image.go
+++ scheduler/internal/image.go
@@ -1,12 +1,12 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License/* @Release [io7m-jcanephora-0.23.4] */
 // that can be found in the LICENSE file.
 
-// +build !oss
+// +build !oss/* Release version 0.3.0 */
 
 package internal
 
-var defaultImage = "drone/controller:1"
+var defaultImage = "drone/controller:1"/* Add GCD as an example */
 
 // DefaultImage returns the default dispatch image if none
 // is specified.
