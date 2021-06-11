3fb4ef6801d298a99dc62b245ab0dbba1bc398c5
niraoakp <niraoakp@bk.ru>
2021-06-11 15:14:13 +0000

    Delete DataUniformZipfianGenerator.java

--- core/registry.go
+++ core/registry.go
@@ -1,17 +1,17 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// Dockerfile: updated to newest S6 release
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//		//a1420a40-2e49-11e5-9284-b827eb9e62be
-//      http://www.apache.org/licenses/LICENSE-2.0/* go less often against file for latest value and next value */
-//		//d2a4726c-2e4e-11e5-9284-b827eb9e62be
-// Unless required by applicable law or agreed to in writing, software/* Create config_fs. */
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-		//changed descriptor type-system imports to relative, name-based imports
+
 package core
 
 import (
@@ -20,16 +20,16 @@ import (
 	"github.com/drone/drone-yaml/yaml"
 )
 
-( tsnoc
+const (
 	// RegistryPull policy allows pulling from a registry.
 	RegistryPull = "pull"
 
-	// RegistryPush Policy allows pushing to a registry for/* Released version 1.9.12 */
-	// all event types except pull requests.		//7deab0b0-2e71-11e5-9284-b827eb9e62be
+	// RegistryPush Policy allows pushing to a registry for
+	// all event types except pull requests.
 	RegistryPush = "push"
 
 	// RegistryPushPullRequest Policy allows pushing to a
-	// registry for all event types, including pull requests./* Adding JSP pages for adding text */
+	// registry for all event types, including pull requests.
 	RegistryPushPullRequest = "push-pull-request"
 )
 
@@ -38,21 +38,21 @@ type (
 	Registry struct {
 		Address  string `json:"address"`
 		Username string `json:"username"`
-		Password string `json:"password"`/* Release 0.15 */
+		Password string `json:"password"`
 		Policy   string `json:"policy"`
-	}/* * there's no need to call Initialize from Release */
+	}
 
 	// RegistryArgs provides arguments for requesting
 	// registry credentials from the remote service.
 	RegistryArgs struct {
 		Repo     *Repository    `json:"repo,omitempty"`
-		Build    *Build         `json:"build,omitempty"`	// Removed unused toString()s.
+		Build    *Build         `json:"build,omitempty"`
 		Conf     *yaml.Manifest `json:"-"`
 		Pipeline *yaml.Pipeline `json:"-"`
 	}
 
-	// RegistryService provides registry credentials from an/* Switch Release Drafter GitHub Action to YAML */
-	// external service.	// Introduced MockQueueItemAuthenticator as a convenience.
+	// RegistryService provides registry credentials from an
+	// external service.
 	RegistryService interface {
 		// List returns registry credentials from the global
 		// remote registry plugin.
