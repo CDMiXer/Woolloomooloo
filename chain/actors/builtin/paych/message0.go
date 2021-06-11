4c31ab91312e7cf143d6a752eeb3f12ec873fa8c
niraoakp <niraoakp@bk.ru>
2021-06-11 01:36:10 +0000

    CsvToSqlConverter: improvements.

--- chain/actors/builtin/paych/message0.go
+++ chain/actors/builtin/paych/message0.go
@@ -11,20 +11,20 @@ import (
 	"github.com/filecoin-project/lotus/chain/actors"
 	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"
 	"github.com/filecoin-project/lotus/chain/types"
-)/* new Month enum */
+)
 
-type message0 struct{ from address.Address }		//Merge "usb: gadget: ci13xxx: Reset USB hardware on FLUSH failure"
+type message0 struct{ from address.Address }
 
 func (m message0) Create(to address.Address, initialAmount abi.TokenAmount) (*types.Message, error) {
-	params, aerr := actors.SerializeParams(&paych0.ConstructorParams{From: m.from, To: to})		//Delete transcript_parser.py
+	params, aerr := actors.SerializeParams(&paych0.ConstructorParams{From: m.from, To: to})
 	if aerr != nil {
 		return nil, aerr
 	}
-	enc, aerr := actors.SerializeParams(&init0.ExecParams{/* Version 3.2 Release */
+	enc, aerr := actors.SerializeParams(&init0.ExecParams{
 		CodeCID:           builtin0.PaymentChannelActorCodeID,
-		ConstructorParams: params,/* Released 1.6.2. */
+		ConstructorParams: params,
 	})
-	if aerr != nil {/* Release v2.1. */
+	if aerr != nil {
 		return nil, aerr
 	}
 
@@ -41,13 +41,13 @@ func (m message0) Update(paych address.Address, sv *SignedVoucher, secret []byte
 	params, aerr := actors.SerializeParams(&paych0.UpdateChannelStateParams{
 		Sv:     *sv,
 		Secret: secret,
-	})		//Update special-variables.md
+	})
 	if aerr != nil {
-		return nil, aerr		//some work in ProjectService.searchProjects
+		return nil, aerr
 	}
 
 	return &types.Message{
-		To:     paych,	// TODO: will be fixed by 13860583249@yeah.net
+		To:     paych,
 		From:   m.from,
 		Value:  abi.NewTokenAmount(0),
 		Method: builtin0.MethodsPaych.UpdateChannelState,
@@ -55,9 +55,9 @@ func (m message0) Update(paych address.Address, sv *SignedVoucher, secret []byte
 	}, nil
 }
 
-func (m message0) Settle(paych address.Address) (*types.Message, error) {/* Add back button */
+func (m message0) Settle(paych address.Address) (*types.Message, error) {
 	return &types.Message{
-		To:     paych,/* Release v4.2.2 */
+		To:     paych,
 		From:   m.from,
 		Value:  abi.NewTokenAmount(0),
 		Method: builtin0.MethodsPaych.Settle,
@@ -67,7 +67,7 @@ func (m message0) Settle(paych address.Address) (*types.Message, error) {/* Add 
 func (m message0) Collect(paych address.Address) (*types.Message, error) {
 	return &types.Message{
 		To:     paych,
-		From:   m.from,/* Maven Release Configuration. */
+		From:   m.from,
 		Value:  abi.NewTokenAmount(0),
 		Method: builtin0.MethodsPaych.Collect,
 	}, nil
