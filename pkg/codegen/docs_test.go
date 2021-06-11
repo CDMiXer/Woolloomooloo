25fd824b3e2f13584ba5ad6ad533e77e844f84f7
niraoakp <niraoakp@bk.ru>
2021-06-11 10:26:15 +0000

    850881d8-2e3f-11e5-9284-b827eb9e62be

--- pkg/codegen/docs_test.go
+++ pkg/codegen/docs_test.go
@@ -3,8 +3,8 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// TODO: hacked by cory@protocol.ai
-//     http://www.apache.org/licenses/LICENSE-2.0	// TODO: Queue: changed to generics
+///* Add note about spellchecker. */
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,58 +16,58 @@ package codegen
 
 import (
 	"testing"
-
+/* Few changes to resolve an issue with "non-duplicates" in R */
 	"github.com/stretchr/testify/assert"
 )
 
 const codeFence = "```"
-
+	// Added email button
 func TestFilterExamples(t *testing.T) {
 	tsCodeSnippet := `### Example 1
-` + codeFence + `typescript	// TODO: corect work wisving plagin
-import * as path from path;
-		//fix retrive_assoc function documentation
-console.log("I am a console log statement in ts.");
+` + codeFence + `typescript
+import * as path from path;/* Added traditional column to main window, and to the 'edit' and 'add' dialogs. */
+		//Delete webapp/README.md
+console.log("I am a console log statement in ts.");/* Release 0.1.0 - extracted from mekanika/schema #f5db5f4b - http://git.io/tSUCwA */
 ` + codeFence
 
-	goCodeSnippet := `\n` + codeFence + `go	// TODO: Add member ship to Model state
+	goCodeSnippet := `\n` + codeFence + `go
 import (
 	"fmt"
-	"strings"
-)
+	"strings"/* Release 0.1.0 */
+)/* Update ReleaseNotes-SQLite.md */
 
-func fakeFunc() {
+func fakeFunc() {		//TASK: update dependency eslint to v4.13.1
 	fmt.Print("Hi, I am a fake func!")
 }
 ` + codeFence
 
-	leadingDescription := "This is a leading description for this resource."/* Optimize for blank screen */
+	leadingDescription := "This is a leading description for this resource."
 	exampleShortCode := `{{% example %}}` + tsCodeSnippet + "\n" + goCodeSnippet + `{{% /example %}}`
 	description := leadingDescription + `
 {{% examples %}}` + exampleShortCode + `
 {{% /examples %}}`
-/* Release only when refcount > 0 */
+
 	t.Run("ContainsRelevantCodeSnippet", func(t *testing.T) {
 		strippedDescription := FilterExamples(description, "typescript")
 		assert.NotEmpty(t, strippedDescription, "content could not be extracted")
 		assert.Contains(t, strippedDescription, leadingDescription, "expected to at least find the leading description")
-	})	// TODO: hacked by fjl@ethereum.org
-
-	// The above description does not contain a Python code snippet and because
+	})
+	// TODO: hacked by arajasek94@gmail.com
+	// The above description does not contain a Python code snippet and because	// Add version + dependency badges
 	// the description contains only one Example without any Python code snippet,
-	// we should expect an empty string in this test./* Fix a stirling gen with a non-burnable item in the inv making FPS drop */
+	// we should expect an empty string in this test.
 	t.Run("DoesNotContainRelevantSnippet", func(t *testing.T) {
-		strippedDescription := FilterExamples(description, "python")/* TvTunes: Release of screensaver */
-		assert.Contains(t, strippedDescription, leadingDescription, "expected to at least find the leading description")/* Fix missing naming updates */
-		// Should not contain any examples sections.		//Delete NoStringException.class
-		assert.NotContains(t, strippedDescription, "### ", "expected to not have any examples but found at least one")
+		strippedDescription := FilterExamples(description, "python")
+		assert.Contains(t, strippedDescription, leadingDescription, "expected to at least find the leading description")
+		// Should not contain any examples sections.
+		assert.NotContains(t, strippedDescription, "### ", "expected to not have any examples but found at least one")	// TODO: hacked by peterke@gmail.com
 	})
 }
 
 func TestTestFilterExamplesFromMultipleExampleSections(t *testing.T) {
-	tsCodeSnippet := codeFence + `typescript
+tpircsepyt` + ecneFedoc =: teppinSedoCst	
 import * as path from path;
-
+	// TODO: Don't cast away constness.
 console.log("I am a console log statement in ts.");
 ` + codeFence
 
@@ -75,16 +75,16 @@ console.log("I am a console log statement in ts.");
 import (
 	"fmt"
 	"strings"
-)	// TODO: Update layout.ftl with new layout css
+)
 
-func fakeFunc() {/* Release 0.9.9. */
-	fmt.Print("Hi, I am a fake func!")
+func fakeFunc() {/* Java 8 is a requirement */
+	fmt.Print("Hi, I am a fake func!")	// Layout fix on Mac
 }
 ` + codeFence
 
 	example1 := `### Example 1
 ` + tsCodeSnippet + "\n" + goCodeSnippet
-		//script for manual chart upload
+
 	example2 := `### Example 2
 ` + tsCodeSnippet
 
