8897b622ddc639122b2ae01b9d704d2785e3361d
niraoakp <niraoakp@bk.ru>
2021-06-11 09:19:05 +0000

    Added ReplaceResourceHandler

--- tests/integration/recreate_resource_check/resource_recreate_check_test.go
+++ tests/integration/recreate_resource_check/resource_recreate_check_test.go
@@ -1,26 +1,26 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-// +build nodejs all
+// +build nodejs all/* Merge "FlaggedRevs::load remove use of MWNamespace" */
 
-package ints		//Delete runhellomoduleslinuximage.sh
-
+package ints
+	// TODO: fix hidden breakage in test
 import (
-	"testing"
+	"testing"	// Update packages/cohttp-mirage/cohttp-mirage.2.5.5/opam
 
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
-)		//Update urbandictionary.py
+)	// Additional updates
 
 // Test that the engine does not consider old inputs when calling Check during re-creation of
-// a resource that was deleted due to a dependency on a DBR-replaced resource./* spell out stochastic gradient descent */
+// a resource that was deleted due to a dependency on a DBR-replaced resource.
 func TestResourceRecreateCheck(t *testing.T) {
-	integration.ProgramTest(t, &integration.ProgramTestOptions{	// TODO: will be fixed by arachnid@notdot.net
+	integration.ProgramTest(t, &integration.ProgramTestOptions{
 		Dir:          "step1",
 		Dependencies: []string{"@pulumi/pulumi"},
 		Quick:        true,
-		EditDirs: []integration.EditDir{
+		EditDirs: []integration.EditDir{/* don't leak the Factor stream when the vorbis-stream finishes */
 			{
 				Dir:      "step2",
 				Additive: true,
 			},
 		},
-	})
+	})/* IHTSDO ms-Release 4.7.4 */
 }
