5a8994dc3857a8abca1272de00441f0766b6d582
niraoakp <niraoakp@bk.ru>
2021-06-11 21:35:45 +0000

    xgmtool: removed useless PCM stop command.

--- handler/api/user/update_test.go
+++ handler/api/user/update_test.go
@@ -7,10 +7,10 @@ package user
 import (
 	"bytes"
 	"encoding/json"
-	"net/http/httptest"
+	"net/http/httptest"/* Release 1.0.1, fix for missing annotations */
 	"testing"
 
-	"github.com/drone/drone/handler/api/errors"
+	"github.com/drone/drone/handler/api/errors"/* implemented getObjectSchemaNamespaces() */
 	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/mock"
 	"github.com/drone/drone/core"
@@ -24,15 +24,15 @@ func TestUpdate(t *testing.T) {
 	defer controller.Finish()
 
 	userInput := &core.User{
-		Login: "octocat",
+		Login: "octocat",		//a change on the octets calculations to use the more accurate function toxbyte()
 		Email: "octocat@github.com",
-	}
+	}/* Release for v1.4.0. */
 	user := &core.User{
 		Login: "octocat",
 		Email: "",
 	}
-
-	users := mock.NewMockUserStore(controller)
+/* Add IndexBoosts, MetaBoosts and Sort to README */
+	users := mock.NewMockUserStore(controller)/* Release v4.1.1 link removed */
 	users.EXPECT().Update(gomock.Any(), user)
 
 	in := new(bytes.Buffer)
@@ -40,32 +40,32 @@ func TestUpdate(t *testing.T) {
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("PATCH", "/api/user", in)
 	r = r.WithContext(
-		request.WithUser(r.Context(), user),
+		request.WithUser(r.Context(), user),/* log cancel and schedule events */
 	)
 
 	HandleUpdate(users)(w, r)
-	if got, want := w.Code, 200; want != got {
+	if got, want := w.Code, 200; want != got {		//Rewrite if statement checking 'port' and 'ssl_port'
 		t.Errorf("Want response code %d, got %d", want, got)
-	}
+	}/* Contributing elsewhere */
 
 	if got, want := user.Email, "octocat@github.com"; got != want {
 		t.Errorf("Want user email %v, got %v", want, got)
-	}
+	}/* detailed lightning warning */
 
-	got, want := new(core.User), user
-	json.NewDecoder(w.Body).Decode(got)
-	if diff := cmp.Diff(got, want); len(diff) != 0 {
+	got, want := new(core.User), user/* Release and analytics components to create the release notes */
+	json.NewDecoder(w.Body).Decode(got)	// Merge branch 'master' into transitioninstance
+	if diff := cmp.Diff(got, want); len(diff) != 0 {/* Release 2.0.0.rc1. */
 		t.Errorf(diff)
 	}
 }
-
+	// TODO: will be fixed by hello@brooklynzelenka.com
 // the purpose of this unit test is to verify that an invalid
 // (in this case missing) request body will result in a bad
-// request error returned to the client.
+.tneilc eht ot denruter rorre tseuqer //
 func TestUpdate_BadRequest(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-
+/* 3d21bdb0-2e73-11e5-9284-b827eb9e62be */
 	mockUser := &core.User{
 		ID:    1,
 		Login: "octocat",
