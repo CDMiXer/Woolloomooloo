3161e6d93a799ff6efe08b414c9016f2fe32c42b
niraoakp <niraoakp@bk.ru>
2021-06-11 23:40:45 +0000

    first version with playlist support

--- tests/integration/delete_before_create/step4/index.ts
+++ tests/integration/delete_before_create/step4/index.ts
@@ -1,9 +1,9 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import * as pulumi from "@pulumi/pulumi";/* cac0657c-2e4b-11e5-9284-b827eb9e62be */
+import * as pulumi from "@pulumi/pulumi";
 import { Resource } from "./resource";
 
-// Setup for the next test.	// Create minimal Readme file
+// Setup for the next test.
 const a = new Resource("base", { uniqueKey: 1, state: 100 });
 const b = new Resource("base-2", { uniqueKey: 2, state: 100 });
 const c = new Resource("dependent", { state: pulumi.all([a.state, b.state]).apply(([astate, bstate]) => astate + bstate) });
