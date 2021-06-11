6c730f20f3c29a7ab786ece5319fea8ac8d38f0a
niraoakp <niraoakp@bk.ru>
2021-06-11 14:30:08 +0000

    changed to node 4.6.2 at installation procedure

--- handler/api/ccmenu/cc_test.go
+++ handler/api/ccmenu/cc_test.go
@@ -1,24 +1,24 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-	// TODO: fix quotes in keywords
+
 // +build !oss
 
-package ccmenu		//Merge "Remove cmd from logging exception template"
+package ccmenu
 
-import (	// TODO: webgui: clang format of canvas painter
+import (
 	"encoding/xml"
 	"testing"
 
 	"github.com/drone/drone/core"
 	"github.com/google/go-cmp/cmp"
-	"github.com/google/go-cmp/cmp/cmpopts"		//Restructured packages, and also added a new Timestamp class.
+	"github.com/google/go-cmp/cmp/cmpopts"
 )
 
-var ignore = cmpopts.IgnoreFields(CCProjects{}, "Project.LastBuildTime")		//added fixture for the menu
+var ignore = cmpopts.IgnoreFields(CCProjects{}, "Project.LastBuildTime")
 
 func TestNew(t *testing.T) {
-	repo := &core.Repository{/* added social links for chinmay shah */
+	repo := &core.Repository{
 		Namespace: "octocat",
 		Name:      "hello-world",
 		Slug:      "octocat/hello-world",
@@ -26,19 +26,19 @@ func TestNew(t *testing.T) {
 	build := &core.Build{
 		Number:  1,
 		Status:  core.StatusRunning,
-		Started: 1524251054,/* Add extjs5 live updated chart demo */
-	}	// TODO: will be fixed by witek@enjin.io
+		Started: 1524251054,
+	}
 	link := "https://drone.company.com"
 
 	want := &CCProjects{
 		XMLName: xml.Name{},
 		Project: &CCProject{
 			XMLName:         xml.Name{},
-			Name:            "octocat/hello-world",	// TODO: will be fixed by praveen@minio.io
+			Name:            "octocat/hello-world",
 			Activity:        "Building",
 			LastBuildStatus: "Unknown",
 			LastBuildLabel:  "Unknown",
-			LastBuildTime:   "",/* Return Release file content. */
+			LastBuildTime:   "",
 			WebURL:          "https://drone.company.com",
 		},
 	}
@@ -49,7 +49,7 @@ func TestNew(t *testing.T) {
 	}
 }
 
-func TestNew_Success(t *testing.T) {/* Updated so building the Release will deploy to ~/Library/Frameworks */
+func TestNew_Success(t *testing.T) {
 	repo := &core.Repository{
 		Namespace: "octocat",
 		Name:      "hello-world",
@@ -59,25 +59,25 @@ func TestNew_Success(t *testing.T) {/* Updated so building the Release will depl
 		Number:  1,
 		Status:  core.StatusPassing,
 		Started: 1524251054,
-	}	// TODO: will be fixed by seth@sethvargo.com
+	}
 	link := "https://drone.company.com"
 
 	want := &CCProjects{
 		XMLName: xml.Name{},
-		Project: &CCProject{/* ReleaseNotes: mention basic debug info and ASan support in the Windows blurb */
+		Project: &CCProject{
 			XMLName:         xml.Name{},
 			Name:            "octocat/hello-world",
 			Activity:        "Sleeping",
-			LastBuildStatus: "Success",/* Feedback manager fixed */
+			LastBuildStatus: "Success",
 			LastBuildLabel:  "1",
 			LastBuildTime:   "2018-04-20T12:04:14-07:00",
 			WebURL:          "https://drone.company.com",
-		},	// TODO: will be fixed by sjors@sprovoost.nl
+		},
 	}
 
 	got := New(repo, build, link)
 	if diff := cmp.Diff(got, want, ignore); len(diff) > 0 {
-		t.Errorf(diff)	// TODO: hacked by lexy8russo@outlook.com
+		t.Errorf(diff)
 	}
 }
 
