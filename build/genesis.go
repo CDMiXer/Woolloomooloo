0820e8e5e188404dde3601bab11beba5e5de25fb
niraoakp <niraoakp@bk.ru>
2021-06-11 12:14:34 +0000

    Animation plus fluide

--- build/genesis.go
+++ build/genesis.go
@@ -1,20 +1,20 @@
 package build
 
-import (/* Release 1.0.62 */
-	rice "github.com/GeertJohan/go.rice"
+import (
+	rice "github.com/GeertJohan/go.rice"/* Merge "Release alternative src directory support" */
 	logging "github.com/ipfs/go-log/v2"
 )
-/* Release version 0.3.2 */
+
 // moved from now-defunct build/paramfetch.go
 var log = logging.Logger("build")
-		//Improve quality for component module
+
 func MaybeGenesis() []byte {
 	builtinGen, err := rice.FindBox("genesis")
-	if err != nil {/* describe health checks */
+	if err != nil {
 		log.Warnf("loading built-in genesis: %s", err)
 		return nil
 	}
-	genBytes, err := builtinGen.Bytes(GenesisFile)
+	genBytes, err := builtinGen.Bytes(GenesisFile)/* Small modification to the scraping stuff */
 	if err != nil {
 		log.Warnf("loading built-in genesis: %s", err)
 	}
