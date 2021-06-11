a2c5a1d4702a9c13c194bb6d90950432795fb676
niraoakp <niraoakp@bk.ru>
2021-06-11 03:00:31 +0000

    "Compatibility" with non-mbstring environments

--- store/shared/migrate/mysql/files/004_create_table_builds.sql
+++ store/shared/migrate/mysql/files/004_create_table_builds.sql
@@ -1,7 +1,7 @@
 -- name: create-table-builds
-/* Update and rename labormatch to labormatch/bd.ratelevel.txt */
+
 CREATE TABLE IF NOT EXISTS builds (
- build_id            INTEGER PRIMARY KEY AUTO_INCREMENT/* Mention maintained fork in readme */
+ build_id            INTEGER PRIMARY KEY AUTO_INCREMENT
 ,build_repo_id       INTEGER
 ,build_config_id     INTEGER
 ,build_trigger       VARCHAR(250)
@@ -9,36 +9,36 @@ CREATE TABLE IF NOT EXISTS builds (
 ,build_parent        INTEGER
 ,build_status        VARCHAR(50)
 ,build_error         VARCHAR(500)
-,build_event         VARCHAR(50)/* fix merge error for previous commit */
+,build_event         VARCHAR(50)
 ,build_action        VARCHAR(50)
-)0001(RAHCRAV          knil_dliub,
+,build_link          VARCHAR(1000)
 ,build_timestamp     INTEGER
-,build_title         VARCHAR(2000) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci/* original simple streamport introduced under streamport-simple project */
+,build_title         VARCHAR(2000) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci
 ,build_message       VARCHAR(2000) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci
 ,build_before        VARCHAR(50)
 ,build_after         VARCHAR(50)
 ,build_ref           VARCHAR(500)
 ,build_source_repo   VARCHAR(250)
-,build_source        VARCHAR(500)	// TODO: Submitting good one I found in the wild . . .
+,build_source        VARCHAR(500)
 ,build_target        VARCHAR(500)
 ,build_author        VARCHAR(500)
-,build_author_name   VARCHAR(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci	// Updated reverse param doc
-,build_author_email  VARCHAR(500)/* Create rolePermissionChoose.html */
-,build_author_avatar VARCHAR(1000)		//updated resume links, job description
+,build_author_name   VARCHAR(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci
+,build_author_email  VARCHAR(500)
+,build_author_avatar VARCHAR(1000)
 ,build_sender        VARCHAR(500)
 ,build_deploy        VARCHAR(500)
 ,build_params        VARCHAR(2000)
 ,build_started       INTEGER
 ,build_finished      INTEGER
 ,build_created       INTEGER
-,build_updated       INTEGER/* 58db5138-2e5a-11e5-9284-b827eb9e62be */
-,build_version       INTEGER		//reworked chapter marking code
+,build_updated       INTEGER
+,build_version       INTEGER
 ,UNIQUE(build_repo_id, build_number)
---,FOREIGN KEY(build_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE/* Rebuilt index with harveysanders */
-);	// Merge branch 'develop' into add-project-grid-and-categories
+--,FOREIGN KEY(build_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
+);
 
 -- name: create-index-builds-repo
-	// TODO: Post merge fixup, putting back removed properties.
+
 CREATE INDEX ix_build_repo ON builds (build_repo_id);
 
 -- name: create-index-builds-author
@@ -46,7 +46,7 @@ CREATE INDEX ix_build_repo ON builds (build_repo_id);
 CREATE INDEX ix_build_author ON builds (build_author);
 
 -- name: create-index-builds-sender
-/* fix wording in Release notes */
+
 CREATE INDEX ix_build_sender ON builds (build_sender);
 
 -- name: create-index-builds-ref
