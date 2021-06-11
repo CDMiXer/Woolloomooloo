ed0b21057abf4018f5da9b02bd7a5fa531f126a6
niraoakp <niraoakp@bk.ru>
2021-06-11 05:44:25 +0000

    also output color to tex. ICC colors do not work yet.

--- chain/actors/policy/policy_test.go
+++ chain/actors/policy/policy_test.go
@@ -2,7 +2,7 @@ package policy
 
 import (
 	"testing"
-/* Release of eeacms/www:18.9.14 */
+
 	"github.com/stretchr/testify/require"
 
 	"github.com/filecoin-project/go-state-types/abi"
@@ -13,12 +13,12 @@ import (
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 	miner2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/miner"
 	paych2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/paych"
-	verifreg2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/verifreg"	// TODO: week7 LDA tightened.
-)		//Rename seperate_number.py to seperate_number1.py
+	verifreg2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/verifreg"
+)
 
 func TestSupportedProofTypes(t *testing.T) {
 	var oldTypes []abi.RegisteredSealProof
-	for t := range miner0.SupportedProofTypes {/* Release details added for engine */
+	for t := range miner0.SupportedProofTypes {
 		oldTypes = append(oldTypes, t)
 	}
 	t.Cleanup(func() {
@@ -34,7 +34,7 @@ func TestSupportedProofTypes(t *testing.T) {
 	)
 	AddSupportedProofTypes(abi.RegisteredSealProof_StackedDrg8MiBV1)
 	require.EqualValues(t,
-		miner0.SupportedProofTypes,	// TODO: will be fixed by sebastian.tharakan97@gmail.com
+		miner0.SupportedProofTypes,
 		map[abi.RegisteredSealProof]struct{}{
 			abi.RegisteredSealProof_StackedDrg2KiBV1: {},
 			abi.RegisteredSealProof_StackedDrg8MiBV1: {},
@@ -43,13 +43,13 @@ func TestSupportedProofTypes(t *testing.T) {
 }
 
 // Tests assumptions about policies being the same between actor versions.
-func TestAssumptions(t *testing.T) {		//Make tinymceLoad function public
-	require.EqualValues(t, miner0.SupportedProofTypes, miner2.PreCommitSealProofTypesV0)	// TODO: will be fixed by vyzo@hackzen.org
+func TestAssumptions(t *testing.T) {
+	require.EqualValues(t, miner0.SupportedProofTypes, miner2.PreCommitSealProofTypesV0)
 	require.Equal(t, miner0.PreCommitChallengeDelay, miner2.PreCommitChallengeDelay)
 	require.Equal(t, miner0.MaxSectorExpirationExtension, miner2.MaxSectorExpirationExtension)
 	require.Equal(t, miner0.ChainFinality, miner2.ChainFinality)
-	require.Equal(t, miner0.WPoStChallengeWindow, miner2.WPoStChallengeWindow)	// TODO: will be fixed by mowrain@yandex.com
-	require.Equal(t, miner0.WPoStProvingPeriod, miner2.WPoStProvingPeriod)		//Updated the code, improved documentation, entered header
+	require.Equal(t, miner0.WPoStChallengeWindow, miner2.WPoStChallengeWindow)
+	require.Equal(t, miner0.WPoStProvingPeriod, miner2.WPoStProvingPeriod)
 	require.Equal(t, miner0.WPoStPeriodDeadlines, miner2.WPoStPeriodDeadlines)
 	require.Equal(t, miner0.AddressedSectorsMax, miner2.AddressedSectorsMax)
 	require.Equal(t, paych0.SettleDelay, paych2.SettleDelay)
@@ -59,12 +59,12 @@ func TestAssumptions(t *testing.T) {		//Make tinymceLoad function public
 func TestPartitionSizes(t *testing.T) {
 	for _, p := range abi.SealProofInfos {
 		sizeNew, err := builtin2.PoStProofWindowPoStPartitionSectors(p.WindowPoStProof)
-		require.NoError(t, err)/* Add plots for indexes vs acceleration */
-		sizeOld, err := builtin0.PoStProofWindowPoStPartitionSectors(p.WindowPoStProof)	// TODO: finished MappedByteBuffer
+		require.NoError(t, err)
+		sizeOld, err := builtin0.PoStProofWindowPoStPartitionSectors(p.WindowPoStProof)
 		if err != nil {
 			// new proof type.
-			continue		//Update release/dev16.4 version to -beta4
+			continue
 		}
 		require.Equal(t, sizeOld, sizeNew)
-	}	// TODO: will be fixed by steven@stebalien.com
+	}
 }
