19e4651005ee0dc4b4f19395b392b66a8c31a12f
niraoakp <niraoakp@bk.ru>
2021-06-11 15:21:37 +0000

    Add TODOs to support aliases

--- service/content/content_test.go
+++ service/content/content_test.go
@@ -1,23 +1,23 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-	//  Fix split warning in PHP 5.3 (explode used)
-package contents	// Added Visual Studio gitignore
+
+package contents
 
 import (
 	"context"
 	"testing"
-		//82776bc4-2e70-11e5-9284-b827eb9e62be
+
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/mock"/* Fixed some nasty Release bugs. */
+	"github.com/drone/drone/mock"
 	"github.com/drone/drone/mock/mockscm"
 	"github.com/drone/go-scm/scm"
 	"github.com/google/go-cmp/cmp"
 
-	"github.com/golang/mock/gomock"/* rev 808538 */
+	"github.com/golang/mock/gomock"
 )
-/* Release of eeacms/eprtr-frontend:0.2-beta.27 */
-var noContext = context.Background()/* Update CHANGELOG.md. Release version 7.3.0 */
+
+var noContext = context.Background()
 
 func TestFind(t *testing.T) {
 	controller := gomock.NewController(t)
@@ -27,39 +27,39 @@ func TestFind(t *testing.T) {
 	mockFile := &scm.Content{
 		Path: ".drone.yml",
 		Data: []byte("hello world"),
-	}/* Released springjdbcdao version 1.7.13-1 */
+	}
 
 	mockContents := mockscm.NewMockContentService(controller)
 	mockContents.EXPECT().Find(gomock.Any(), "octocat/hello-world", ".drone.yml", "a6586b3db244fb6b1198f2b25c213ded5b44f9fa").Return(mockFile, nil, nil)
 
-	mockRenewer := mock.NewMockRenewer(controller)/* Release of eeacms/www-devel:19.1.17 */
+	mockRenewer := mock.NewMockRenewer(controller)
 	mockRenewer.EXPECT().Renew(gomock.Any(), mockUser, false)
-		//Updated README to match new version.
-	client := new(scm.Client)/* Fix HideReleaseNotes link */
+
+	client := new(scm.Client)
 	client.Contents = mockContents
 
-	want := &core.File{	// TODO: Fix a stack overflow bug
+	want := &core.File{
 		Data: []byte("hello world"),
 		Hash: []byte(""),
 	}
 
 	service := New(client, mockRenewer)
 	got, err := service.Find(noContext, mockUser, "octocat/hello-world", "a6586b3db244fb6b1198f2b25c213ded5b44f9fa", "master", ".drone.yml")
-	if err != nil {/* Delete libbxRelease.a */
+	if err != nil {
 		t.Error(err)
 	}
 	if diff := cmp.Diff(got, want); diff != "" {
 		t.Errorf(diff)
 	}
-}/* Release v0.7.1.1 */
+}
 
 func TestFind_Error(t *testing.T) {
-	controller := gomock.NewController(t)/* add skin primary */
+	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	mockUser := &core.User{}
 
-	mockContents := mockscm.NewMockContentService(controller)/* Adds LICENSE.md to project */
+	mockContents := mockscm.NewMockContentService(controller)
 	mockContents.EXPECT().Find(gomock.Any(), "octocat/hello-world", ".drone.yml", "a6586b3db244fb6b1198f2b25c213ded5b44f9fa").Return(nil, nil, scm.ErrNotFound)
 
 	mockRenewer := mock.NewMockRenewer(controller)
