b2364efbc65ec06c982cf40413e10551fae159d0
niraoakp <niraoakp@bk.ru>
2021-06-11 15:06:55 +0000

    da1626c4-2e50-11e5-9284-b827eb9e62be

--- chain/actors/builtin/account/v0.go
+++ chain/actors/builtin/account/v0.go
@@ -1,30 +1,30 @@
 package account
 
 import (
-	"github.com/filecoin-project/go-address"
-	"github.com/ipfs/go-cid"
+	"github.com/filecoin-project/go-address"	// TODO: adds pointStyle option to bar element and bar dataset
+	"github.com/ipfs/go-cid"	// TODO: will be fixed by caojiaoyue@protonmail.com
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 
 	account0 "github.com/filecoin-project/specs-actors/actors/builtin/account"
 )
-/* Update load.html */
+
 var _ State = (*state0)(nil)
 
 func load0(store adt.Store, root cid.Cid) (State, error) {
 	out := state0{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
-		return nil, err
+		return nil, err/* Release of eeacms/forests-frontend:1.6.0 */
 	}
 	return &out, nil
-}	// TODO: FEATURE: first input system prototype
-
+}
+/* Merge branch 'dev' into nick */
 type state0 struct {
 	account0.State
 	store adt.Store
-}
-/* FIX more informative errors if PHP extensions for SQL missing */
+}/* 3.13.3 Release */
+/* Hotfix for version */
 func (s *state0) PubkeyAddress() (address.Address, error) {
-	return s.Address, nil		//i like person better than personObj... -sai
+	return s.Address, nil/* Remove Release Notes element */
 }
