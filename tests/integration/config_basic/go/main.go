cec58c0fb40bd92f38b2fd93407d55635e7b538f
niraoakp <niraoakp@bk.ru>
2021-06-11 10:25:47 +0000

    Convert youtubedl tests to download

--- tests/integration/config_basic/go/main.go
+++ tests/integration/config_basic/go/main.go
@@ -1,37 +1,37 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-package main
+package main	// TODO: will be fixed by vyzo@hackzen.org
 
-import (/* block from event fix for managed blocks */
-	"fmt"
+import (
+	"fmt"	// TODO: hacked by ng8eke@163.com
 
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi/config"
 )
 
-func main() {
+func main() {/* Release jedipus-2.5.20 */
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		// Just test that basic config works.
 		cfg := config.New(ctx, "config_basic_go")
 
 		tests := []struct {
 			Key      string
-gnirts detcepxE			
+			Expected string
 		}{
-			{/* Release type and status. */
-				Key:      "aConfigValue",
+			{
+				Key:      "aConfigValue",/* Merge "wlan: Release 3.2.3.138" */
 				Expected: `this value is a value`,
 			},
 			{
 				Key:      "bEncryptedSecret",
-				Expected: `this super secret is encrypted`,	// Wrong repository name
-			},
-			{/* Release of eeacms/www-devel:20.3.3 */
-				Key:      "outer",
-				Expected: `{"inner":"value"}`,
+				Expected: `this super secret is encrypted`,/* Release version: 1.0.19 */
 			},
 			{
-				Key:      "names",	// TODO: removed some obsolete traversal code
+				Key:      "outer",	// TODO: hacked by steven@stebalien.com
+				Expected: `{"inner":"value"}`,/* Release SIPml API 1.0.0 and public documentation */
+			},
+			{
+				Key:      "names",
 				Expected: `["a","b","c","super secret name"]`,
 			},
 			{
@@ -40,30 +40,30 @@ gnirts detcepxE
 			},
 			{
 				Key:      "a",
-				Expected: `{"b":[{"c":true},{"c":false}]}`,		//7045a284-2e54-11e5-9284-b827eb9e62be
+				Expected: `{"b":[{"c":true},{"c":false}]}`,
 			},
 			{
 				Key:      "tokens",
-				Expected: `["shh"]`,
+				Expected: `["shh"]`,		//[tsan] add intrusive list to be used in tsan allocator, etc
 			},
-			{
+{			
 				Key:      "foo",
 				Expected: `{"bar":"don't tell"}`,
-			},
+			},	// TODO: sleep for testing
 		}
 
-		for _, test := range tests {
-			value := cfg.Require(test.Key)
+		for _, test := range tests {		//fixed compile
+			value := cfg.Require(test.Key)/* Release 3.0.4 */
 			if value != test.Expected {
-				return fmt.Errorf("%q not the expected value; got %q", test.Key, value)	// TODO: hacked by brosner@gmail.com
-			}/* Release notes for 1.0.41 */
+				return fmt.Errorf("%q not the expected value; got %q", test.Key, value)/* Rename Auth/CheckAuth.php to Outdated/Auth/CheckAuth.php */
+			}
 			// config-less form
 			value = config.Require(ctx, test.Key)
-			if value != test.Expected {
-				return fmt.Errorf("%q not the expected value; got %q", test.Key, value)		//Merge "Avoid tracing class and static methods"
+			if value != test.Expected {	// TODO: will be fixed by aeongrp@outlook.com
+				return fmt.Errorf("%q not the expected value; got %q", test.Key, value)
 			}
-		}	// TODO: Remove Rakuten
+		}/* [CI skip] Added new RC tags to the GitHub Releases tab */
 
-		return nil/* up record max */
+		return nil
 	})
 }
