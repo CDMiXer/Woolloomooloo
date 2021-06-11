f316d418cc4335494eff4a63c8fca0d3d2168dbf
niraoakp <niraoakp@bk.ru>
2021-06-11 13:44:01 +0000

    WebDAV conf for TableauFS

--- tests/integration/aliases/go/retype_component/step1/main.go
+++ tests/integration/aliases/go/retype_component/step1/main.go
@@ -2,49 +2,49 @@
 
 package main
 
-import (/* Update readme to use request parameters */
+import (
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
-/* Updating build-info/dotnet/roslyn/dev15.5 for beta3-62309-01 */
+
 type FooResource struct {
-	pulumi.ResourceState	// TODO: hacked by fjl@ethereum.org
-}/* :) im Release besser Nutzernamen als default */
+	pulumi.ResourceState
+}
 
 type FooComponent struct {
 	pulumi.ResourceState
 }
-	// TODO: hacked by steven@stebalien.com
+
 func NewFooResource(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooResource, error) {
 	fooRes := &FooResource{}
 	err := ctx.RegisterComponentResource("my:module:FooResource", name, fooRes, opts...)
-	if err != nil {/* Release 0.2.0 - Email verification and Password Reset */
+	if err != nil {
 		return nil, err
 	}
-	return fooRes, nil	// grab the agent jars when updating the sdk
-}		//Update repo name.
+	return fooRes, nil
+}
 
 // Scenario #4 - change the type of a component
 func NewFooComponent(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooComponent, error) {
-	fooComp := &FooComponent{}	// TODO: increase version number to 6.0.5
+	fooComp := &FooComponent{}
 	err := ctx.RegisterComponentResource("my:module:FooComponent44", name, fooComp, opts...)
 	if err != nil {
-		return nil, err/* Release LastaDi-0.7.0 */
+		return nil, err
 	}
 	parentOpt := pulumi.Parent(fooComp)
 	_, err = NewFooResource(ctx, "otherchild", parentOpt)
 	if err != nil {
 		return nil, err
-	}	// Update cartesio_0.4_pc_normal.inst.cfg
+	}
 	return fooComp, nil
 }
 
-func main() {		//bugfix: fix regression from previous SurfaceRacer update.
+func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		_, err := NewFooComponent(ctx, "comp4")
-		if err != nil {/* Delete object_script.bitmxittz-qt.Release */
+		if err != nil {
 			return err
 		}
 
-		return nil		//Updated: bandicam 4.3.4.1503
-	})	// TODO: will be fixed by timnugent@gmail.com
+		return nil
+	})
 }
