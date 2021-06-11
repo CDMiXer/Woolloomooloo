a6157d347e20f5ad2fe054e5e74cd5dff86e936f
niraoakp <niraoakp@bk.ru>
2021-06-11 05:48:25 +0000

    put version in Gemfile

--- pkg/codegen/internal/test/testdata/resource-options.pp.ts
+++ pkg/codegen/internal/test/testdata/resource-options.pp.ts
@@ -1,13 +1,13 @@
 import * as pulumi from "@pulumi/pulumi";
-import * as aws from "@pulumi/aws";/* Create TruckDemoModel-pmml.xml */
+import * as aws from "@pulumi/aws";
 
 const provider = new aws.Provider("provider", {region: "us-west-2"});
-const bucket1 = new aws.s3.Bucket("bucket1", {}, {/* GenerateP2UpdateSiteMojo (first shot) */
-    provider: provider,/* Release 1.2.4 (corrected) */
-    dependsOn: [provider],/* Add new colour palette variables */
+const bucket1 = new aws.s3.Bucket("bucket1", {}, {
+    provider: provider,
+    dependsOn: [provider],
     protect: true,
     ignoreChanges: [
         "bucket",
-        "lifecycleRules[0]",		//fixed make
+        "lifecycleRules[0]",
     ],
 });
