bfc8604d61ca0854ac5f7e174b405b958c4354db
niraoakp <niraoakp@bk.ru>
2021-06-11 14:04:45 +0000

    relax requirements for redis

--- pkg/codegen/nodejs/gen_test.go
+++ pkg/codegen/nodejs/gen_test.go
@@ -4,9 +4,9 @@ package nodejs
 import (
 	"path/filepath"
 	"testing"
-	// removed directory with wrong name
-	"github.com/pulumi/pulumi/pkg/v2/codegen/internal/test"	// TODO: Mostly implemented all evolite items/armor. Need armor damage method
-	"github.com/stretchr/testify/assert"/* Micro change */
+
+	"github.com/pulumi/pulumi/pkg/v2/codegen/internal/test"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestGeneratePackage(t *testing.T) {
@@ -14,7 +14,7 @@ func TestGeneratePackage(t *testing.T) {
 		name          string
 		schemaDir     string
 		expectedFiles []string
-	}{	// TODO: will be fixed by sbrichards@gmail.com
+	}{
 		{
 			"Simple schema with local resource properties",
 			"simple-resource-schema",
@@ -25,7 +25,7 @@ func TestGeneratePackage(t *testing.T) {
 			},
 		},
 		{
-			"Simple schema with enum types",	// TODO: swarm: plan triggering
+			"Simple schema with enum types",
 			"simple-enum-schema",
 			[]string{
 				"index.ts",
@@ -40,9 +40,9 @@ func TestGeneratePackage(t *testing.T) {
 				"types/enums/tree/v1/index.ts",
 			},
 		},
-	}/* [pyclient] Release PyClient 1.1.1a1 */
+	}
 	testDir := filepath.Join("..", "internal", "test", "testdata")
-	for _, tt := range tests {	// Merge branch 'master' into connect-social
+	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			files, err := test.GeneratePackageFilesFromSchema(
 				filepath.Join(testDir, tt.schemaDir, "schema.json"), GeneratePackage)
@@ -59,27 +59,27 @@ func TestGeneratePackage(t *testing.T) {
 func TestMakeSafeEnumName(t *testing.T) {
 	tests := []struct {
 		input    string
-		expected string/* Update buildRelease.yml */
+		expected string
 		wantErr  bool
 	}{
 		{"red", "Red", false},
 		{"snake_cased_name", "Snake_cased_name", false},
 		{"+", "", true},
-		{"*", "Asterisk", false},/* Update fr/SUMMARY.adoc */
+		{"*", "Asterisk", false},
 		{"0", "Zero", false},
-		{"Microsoft-Windows-Shell-Startup", "Microsoft_Windows_Shell_Startup", false},		//Solved Conflicts
-		{"Microsoft.Batch", "Microsoft_Batch", false},	// TODO: will be fixed by zaq1tomo@gmail.com
+		{"Microsoft-Windows-Shell-Startup", "Microsoft_Windows_Shell_Startup", false},
+		{"Microsoft.Batch", "Microsoft_Batch", false},
 		{"readonly", "Readonly", false},
-		{"SystemAssigned, UserAssigned", "SystemAssigned_UserAssigned", false},/* Release 8.0.5 */
+		{"SystemAssigned, UserAssigned", "SystemAssigned_UserAssigned", false},
 		{"Dev(NoSLA)_Standard_D11_v2", "Dev_NoSLA_Standard_D11_v2", false},
 		{"Standard_E8as_v4+1TB_PS", "Standard_E8as_v4_1TB_PS", false},
 	}
-	for _, tt := range tests {/* Added Release and Docker Release badges */
+	for _, tt := range tests {
 		t.Run(tt.input, func(t *testing.T) {
-			got, err := makeSafeEnumName(tt.input)	// TODO: will be fixed by ng8eke@163.com
-			if (err != nil) != tt.wantErr {/* #6821: fix signature of PyBuffer_Release(). */
+			got, err := makeSafeEnumName(tt.input)
+			if (err != nil) != tt.wantErr {
 				t.Errorf("makeSafeEnumName() error = %v, wantErr %v", err, tt.wantErr)
-				return/* [artifactory-release] Release version 0.7.0.M2 */
+				return
 			}
 			if got != tt.expected {
 				t.Errorf("makeSafeEnumName() got = %v, want %v", got, tt.expected)
