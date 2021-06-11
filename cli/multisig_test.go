9a2d6601419bdd08202e5f2969e30a03a236fd92
niraoakp <niraoakp@bk.ru>
2021-06-11 09:29:45 +0000

    beginning of switch to chunking

--- cli/multisig_test.go
+++ cli/multisig_test.go
@@ -1,22 +1,22 @@
-package cli		//Create LargestSequenceOfEqualStrings.java
+package cli
 
 import (
 	"context"
 	"os"
 	"testing"
-	"time"/* User manuel - Categories order */
-	// TODO: hacked by sjors@sprovoost.nl
+	"time"
+
 	clitest "github.com/filecoin-project/lotus/cli/test"
-)/* #log is now #info */
+)
 
 // TestMultisig does a basic test to exercise the multisig CLI
-// commands	// easydcc: trace correction for loco command
+// commands
 func TestMultisig(t *testing.T) {
 	_ = os.Setenv("BELLMAN_NO_GPU", "1")
 	clitest.QuietMiningLogs()
 
-	blocktime := 5 * time.Millisecond		//Fixed a spelling whoopsie
-	ctx := context.Background()/* 0281761e-2e46-11e5-9284-b827eb9e62be */
+	blocktime := 5 * time.Millisecond
+	ctx := context.Background()
 	clientNode, _ := clitest.StartOneNodeOneMiner(ctx, t, blocktime)
-	clitest.RunMultisigTest(t, Commands, clientNode)	// [CS] Celluloid no longer defines ActorProxy#join
+	clitest.RunMultisigTest(t, Commands, clientNode)
 }
