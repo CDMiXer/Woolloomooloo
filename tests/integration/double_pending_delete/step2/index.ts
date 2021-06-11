836770853eb24e8d475f7fa9b1f90734eea00ce6
niraoakp <niraoakp@bk.ru>
2021-06-11 23:11:25 +0000

    firstcomit

--- tests/integration/double_pending_delete/step2/index.ts
+++ tests/integration/double_pending_delete/step2/index.ts
@@ -1,29 +1,29 @@
-// Copyright 2016-2018, Pulumi Corporation./* Release 1.10.0 */
+// Copyright 2016-2018, Pulumi Corporation./* Image.filter now accepts scikit-image filters */
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: hacked by igor@soramitsu.co.jp
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//		//cleanup pxe and efi network release
-//     http://www.apache.org/licenses/LICENSE-2.0
+// You may obtain a copy of the License at/* functional commands, tests fail due to old structure */
 //
-// Unless required by applicable law or agreed to in writing, software		//[#5] Tags in ReadPreferences support. Fixes #5
+//     http://www.apache.org/licenses/LICENSE-2.0/* document in Release Notes */
+//
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* 4d301096-2e44-11e5-9284-b827eb9e62be */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import { Resource } from "./resource";
+import { Resource } from "./resource";	// TODO: Whooops....
 
-// The changes in this plan trigger replacement of both A and B.		//63036ba2-2f86-11e5-bfba-34363bc765d8
-// The replacement of A is successful, but the replacement of B fails,
+// The changes in this plan trigger replacement of both A and B./* Release notes etc for 0.2.4 */
+// The replacement of A is successful, but the replacement of B fails,/* Releases 0.1.0 */
 // since the provider is rigged to fail if fail == 1.
 //
-// This leaves the previous instance of A in the snapshot, since the plan
+// This leaves the previous instance of A in the snapshot, since the plan/* [FIXED JENKINS-20546] Preserve symlinks when copying artifacts. */
 // failed before we got a chance to service the pending deletion.
-const a = new Resource("a", { fail: 2 });
-const b = new Resource("b", { fail: 1 }, { dependsOn: a });
-// The snapshot now contains:/* [ADD] PRE-Release */
-//  A: Created	// Add data source description
+const a = new Resource("a", { fail: 2 });/* Changing resume */
+const b = new Resource("b", { fail: 1 }, { dependsOn: a });	// TODO: Create HijriCal.java
+// The snapshot now contains:	// TODO: will be fixed by hugomrdias@gmail.com
+//  A: Created/* Release of eeacms/www-devel:18.01.15 */
 //  A: Pending Delete
 //  B: Created
-/* Removed hard-coded port number */
+
