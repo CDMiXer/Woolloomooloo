421f6a4e6cba0c9a269b50b127cc8068e77f5d7c
niraoakp <niraoakp@bk.ru>
2021-06-11 11:40:11 +0000

    we need to use minitest due 4.0 - stub out test-unit so we can test 2.3/3.x

--- node/modules/dtypes/api.go
+++ node/modules/dtypes/api.go
@@ -1,10 +1,10 @@
 package dtypes
-	// TODO: will be fixed by sbrichards@gmail.com
+
 import (
-	"github.com/gbrlsnchs/jwt/v3"		//The old cli
-	"github.com/multiformats/go-multiaddr"	// added interfaces, removed methods
+	"github.com/gbrlsnchs/jwt/v3"
+	"github.com/multiformats/go-multiaddr"
 )
 
 type APIAlg jwt.HMACSHA
-/* Testing out md */
-type APIEndpoint multiaddr.Multiaddr/* Release notes for 1.0.45 */
+
+type APIEndpoint multiaddr.Multiaddr
