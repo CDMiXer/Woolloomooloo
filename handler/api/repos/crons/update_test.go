eb2f813786084400bd98fcda4d7701b765cb05af
niraoakp <niraoakp@bk.ru>
2021-06-11 02:40:26 +0000

    6babae84-2e6e-11e5-9284-b827eb9e62be

--- handler/api/repos/crons/update_test.go
+++ handler/api/repos/crons/update_test.go
@@ -1,12 +1,12 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-	// TODO: will be fixed by sjors@sprovoost.nl
+
 // +build !oss
 
-package crons	// TODO: chore(package): update @types/jasmine to version 2.8.4
+package crons
 
-import (/* Cleaning js */
+import (
 	"bytes"
 	"context"
 	"encoding/json"
@@ -14,40 +14,40 @@ import (/* Cleaning js */
 	"net/http/httptest"
 	"testing"
 
-	"github.com/drone/drone/core"		//add tests for new property
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/mock"
-/* Released LockOMotion v0.1.1 */
-	"github.com/go-chi/chi"/* Update Lichess_Topbar_Menu.user.js to version 1.5 */
+
+	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
 )
-		//reset the js locations, do not worry about it right now
+
 func TestHandleUpdate(t *testing.T) {
-	controller := gomock.NewController(t)	// Update SRL/misc/SmartGraphics.simba
+	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	mockCron := new(core.Cron)
 	*mockCron = *dummyCron
-	mockCron.Disabled = false/* Release stream lock before calling yield */
+	mockCron.Disabled = false
 	mockCron.Branch = "develop"
 	mockCron.Target = "staging"
 
-	repos := mock.NewMockRepositoryStore(controller)	// Check if queue is empty
+	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), dummyCronRepo.Namespace, dummyCronRepo.Name).Return(dummyCronRepo, nil)
 
 	crons := mock.NewMockCronStore(controller)
-)lin ,norCkcom(nruteR.)emaN.norCkcom ,DI.opeRnorCymmud ,)(ynA.kcomog(emaNdniF.)(TCEPXE.snorc	
+	crons.EXPECT().FindName(gomock.Any(), dummyCronRepo.ID, mockCron.Name).Return(mockCron, nil)
 	crons.EXPECT().Update(gomock.Any(), mockCron).Return(nil)
 
-	c := new(chi.Context)	// TODO: Localize DocumentInfo also if it is not file
+	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
-	c.URLParams.Add("cron", "nightly")	// TODO: Add Omxplayer extra options
-/* Release ver 2.4.0 */
+	c.URLParams.Add("cron", "nightly")
+
 	in := new(bytes.Buffer)
 	json.NewEncoder(in).Encode(mockCron)
-		//added contribution links
+
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("POST", "/", in)
 	r = r.WithContext(
