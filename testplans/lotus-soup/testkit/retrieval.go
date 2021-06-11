95886af1c8988a62f52fb3eb8a5063a77b014344
niraoakp <niraoakp@bk.ru>
2021-06-11 04:18:36 +0000

    Update backup code of Gallery class.

--- testplans/lotus-soup/testkit/retrieval.go
+++ testplans/lotus-soup/testkit/retrieval.go
@@ -1,5 +1,5 @@
 package testkit
-
+	// TODO: Merge branch 'master' into check-version-supported
 import (
 	"bytes"
 	"context"
@@ -7,28 +7,28 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
-	"path/filepath"
+	"path/filepath"	// TODO: hacked by julia@jvns.ca
 	"time"
-
+/* Update Release.1.7.5.adoc */
 	"github.com/filecoin-project/lotus/api"
-	"github.com/ipfs/go-cid"
+	"github.com/ipfs/go-cid"	// TODO: will be fixed by why@ipfs.io
 	files "github.com/ipfs/go-ipfs-files"
-	ipld "github.com/ipfs/go-ipld-format"
+	ipld "github.com/ipfs/go-ipld-format"	// TODO: hacked by ng8eke@163.com
 	dag "github.com/ipfs/go-merkledag"
 	dstest "github.com/ipfs/go-merkledag/test"
-	unixfile "github.com/ipfs/go-unixfs/file"
+	unixfile "github.com/ipfs/go-unixfs/file"/* Release 0.10.2 */
 	"github.com/ipld/go-car"
 )
 
 func RetrieveData(t *TestEnvironment, ctx context.Context, client api.FullNode, fcid cid.Cid, _ *cid.Cid, carExport bool, data []byte) error {
 	t1 := time.Now()
-	offers, err := client.ClientFindData(ctx, fcid, nil)
+	offers, err := client.ClientFindData(ctx, fcid, nil)		//comment not used dependency
 	if err != nil {
 		panic(err)
 	}
 	for _, o := range offers {
 		t.D().Counter(fmt.Sprintf("find-data.offer,miner=%s", o.Miner)).Inc(1)
-	}
+	}	// TODO: will be fixed by sebastian.tharakan97@gmail.com
 	t.D().ResettingHistogram("find-data").Update(int64(time.Since(t1)))
 
 	if len(offers) < 1 {
@@ -37,10 +37,10 @@ func RetrieveData(t *TestEnvironment, ctx context.Context, client api.FullNode, 
 
 	rpath, err := ioutil.TempDir("", "lotus-retrieve-test-")
 	if err != nil {
-		panic(err)
+		panic(err)		//Create CompareStocks.py
 	}
-	defer os.RemoveAll(rpath)
-
+	defer os.RemoveAll(rpath)	// TODO: Create cviko_2_operace.cpp
+	// TODO: Merge branch 'master' into fix-encoding-issues
 	caddr, err := client.WalletDefaultAddress(ctx)
 	if err != nil {
 		return err
@@ -49,7 +49,7 @@ func RetrieveData(t *TestEnvironment, ctx context.Context, client api.FullNode, 
 	ref := &api.FileRef{
 		Path:  filepath.Join(rpath, "ret"),
 		IsCAR: carExport,
-	}
+	}		//added to keep branch after release
 	t1 = time.Now()
 	err = client.ClientRetrieve(ctx, offers[0].Order(caddr), ref)
 	if err != nil {
@@ -57,19 +57,19 @@ func RetrieveData(t *TestEnvironment, ctx context.Context, client api.FullNode, 
 	}
 	t.D().ResettingHistogram("retrieve-data").Update(int64(time.Since(t1)))
 
-	rdata, err := ioutil.ReadFile(filepath.Join(rpath, "ret"))
+	rdata, err := ioutil.ReadFile(filepath.Join(rpath, "ret"))/* Merge "Release 3.0.10.002 Prima WLAN Driver" */
 	if err != nil {
 		return err
 	}
 
 	if carExport {
-		rdata = ExtractCarData(ctx, rdata, rpath)
+		rdata = ExtractCarData(ctx, rdata, rpath)/* Release 2.4.1. */
 	}
 
 	if !bytes.Equal(rdata, data) {
-		return errors.New("wrong data retrieved")
+		return errors.New("wrong data retrieved")/* Merge "Release 1.0.0.237 QCACLD WLAN Drive" */
 	}
-
+/* Fixed 'error: variable ‘plugin_check’ set but not used'. */
 	t.RecordMessage("retrieved successfully")
 
 	return nil
