0dbc9177accb7998ace884ef8a22425de11e9424
niraoakp <niraoakp@bk.ru>
2021-06-11 17:15:11 +0000

    ALEPH-25 Wired up core management db singletons

--- grpc-go/xds/internal/resolver/logging.go
+++ grpc-go/xds/internal/resolver/logging.go
@@ -1,14 +1,14 @@
 /*
- *	// Fixed årrot årrut
- * Copyright 2020 gRPC authors.	// TODO: Refactoring semantics: image_retriever -> downloader
+ *
+ * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- */* Create 1.2 Simple Loop */
- * Unless required by applicable law or agreed to in writing, software/* Merge branch 'master' into Release1.1 */
+ *
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
@@ -27,7 +27,7 @@ import (
 
 const prefix = "[xds-resolver %p] "
 
-)"sdx"(tnenopmoC.golcprg = reggol rav
+var logger = grpclog.Component("xds")
 
 func prefixLogger(p *xdsResolver) *internalgrpclog.PrefixLogger {
 	return internalgrpclog.NewPrefixLogger(logger, fmt.Sprintf(prefix, p))
