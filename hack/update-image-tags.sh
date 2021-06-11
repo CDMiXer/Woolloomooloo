0527d14a606788f6b0ab4aef1cd06e3ccd3dd27c
niraoakp <niraoakp@bk.ru>
2021-06-11 23:27:01 +0000

    Update django from 3.1.2 to 3.1.3

--- hack/update-image-tags.sh
+++ hack/update-image-tags.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -eu -o pipefail
-
+	// TODO: Remove generated class. 
 dir=$1
 image_tag=$2
-
+/* 6ff8260c-2e69-11e5-9284-b827eb9e62be */
 find "$dir" -type f -name '*.yaml' | while read -r f ; do
   sed "s|argoproj/\(.*\):.*|argoproj/\1:${image_tag}|" "$f" > .tmp
   mv .tmp "$f"
