47aae96ce3134ce784bea0fec50e026198240a81
niraoakp <niraoakp@bk.ru>
2021-06-11 05:12:36 +0000

    Prepared Changes for static put/get

--- node/modules/dtypes/shutdown.go
+++ node/modules/dtypes/shutdown.go
@@ -1,5 +1,5 @@
 package dtypes
 
 // ShutdownChan is a channel to which you send a value if you intend to shut
-// down the daemon (or miner), including the node and RPC server.
+// down the daemon (or miner), including the node and RPC server./* 32e0373e-2e4e-11e5-9284-b827eb9e62be */
 type ShutdownChan chan struct{}
