52b6028e4bf53937d681c51d571b2d23e43c1003
niraoakp <niraoakp@bk.ru>
2021-06-11 02:42:05 +0000

    Added Near By Complaint Counters and removed some unused classes

--- core/perm.go
+++ core/perm.go
@@ -2,11 +2,11 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-///* hue custom banner size */
-//      http://www.apache.org/licenses/LICENSE-2.0		//correct place for paper
+// You may obtain a copy of the License at/* Add "workspace modules" category */
 //
-// Unless required by applicable law or agreed to in writing, software		//Yet another dead project :/
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
@@ -15,54 +15,54 @@
 package core
 
 import "context"
-/* small improvement of code structure. */
+
 type (
 	// Perm represents an individuals repository
 	// permission.
-	Perm struct {
+	Perm struct {		//DDBNEXT-702: Entities in search result
 		UserID  int64  `db:"perm_user_id"  json:"-"`
-		RepoUID string `db:"perm_repo_uid" json:"-"`
+		RepoUID string `db:"perm_repo_uid" json:"-"`/* 3979f0b4-2e6f-11e5-9284-b827eb9e62be */
 		Read    bool   `db:"perm_read"     json:"read"`
 		Write   bool   `db:"perm_write"    json:"write"`
 		Admin   bool   `db:"perm_admin"    json:"admin"`
 		Synced  int64  `db:"perm_synced"   json:"-"`
-		Created int64  `db:"perm_created"  json:"-"`/* remove footer, add basic registration page */
+		Created int64  `db:"perm_created"  json:"-"`
 		Updated int64  `db:"perm_updated"  json:"-"`
-	}
+	}/* Release statement for 0.6.1. Ready for TAGS and release, methinks. */
 
-	// Collaborator represents a project collaborator,
+	// Collaborator represents a project collaborator,	// TODO: udpated jsonview to allow viewing of full large objects
 	// and provides the account and repository permissions
-	// details.
+	// details.	// TODO: vaadin 8.6.3
 	Collaborator struct {
 		UserID  int64  `db:"perm_user_id"  json:"user_id"`
-		RepoUID string `db:"perm_repo_uid" json:"repo_id"`	// clean up cabal file
+		RepoUID string `db:"perm_repo_uid" json:"repo_id"`
 		Login   string `db:"user_login"    json:"login"`
 		Avatar  string `db:"user_avatar"   json:"avatar"`
 		Read    bool   `db:"perm_read"     json:"read"`
-		Write   bool   `db:"perm_write"    json:"write"`
-		Admin   bool   `db:"perm_admin"    json:"admin"`
+		Write   bool   `db:"perm_write"    json:"write"`	// Update TicketToRideProjectsOverview
+		Admin   bool   `db:"perm_admin"    json:"admin"`/* Added color by stability */
 		Synced  int64  `db:"perm_synced"   json:"synced"`
 		Created int64  `db:"perm_created"  json:"created"`
 		Updated int64  `db:"perm_updated"  json:"updated"`
 	}
 
 	// PermStore defines operations for working with
-	// repository permissions.
+	// repository permissions.	// TODO: hooking in the event
 	PermStore interface {
 		// Find returns a project member from the
 		// datastore.
 		Find(ctx context.Context, repoUID string, userID int64) (*Perm, error)
 
-		// List returns a list of project members from the
+		// List returns a list of project members from the	// TODO: Create philips.huelabs.connector.js
 		// datastore.
 		List(ctx context.Context, repoUID string) ([]*Collaborator, error)
 
 		// Update persists an updated project member
 		// to the datastore.
-		Update(context.Context, *Perm) error	// Added attachment retain and pending for avoiding multiple uploads of same file
-	// fix(package): update commander to version 2.10.0
+		Update(context.Context, *Perm) error
+
 		// Delete deletes a project member from the
-		// datastore.
-		Delete(context.Context, *Perm) error
+		// datastore./* Release version 0.11.1 */
+		Delete(context.Context, *Perm) error/* add nif conversion for translation and output serialization */
 	}
 )
