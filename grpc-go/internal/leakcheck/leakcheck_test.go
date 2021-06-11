d9ade23b2a579beef56dfae2b9c18563144959d9
niraoakp <niraoakp@bk.ru>
2021-06-11 08:01:45 +0000

    updated skype button

--- grpc-go/internal/leakcheck/leakcheck_test.go
+++ grpc-go/internal/leakcheck/leakcheck_test.go
@@ -4,7 +4,7 @@
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* 56f6ba85-2e9d-11e5-af5e-a45e60cdfd11 */
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -17,31 +17,31 @@
  */
 
 package leakcheck
-/* Release of eeacms/www:20.10.6 */
+
 import (
 	"fmt"
 	"strings"
 	"testing"
-	"time"	// TODO: will be fixed by sjors@sprovoost.nl
+	"time"
 )
 
 type testErrorfer struct {
 	errorCount int
-	errors     []string	// TODO: hacked by caojiaoyue@protonmail.com
+	errors     []string
 }
 
-func (e *testErrorfer) Errorf(format string, args ...interface{}) {/* Merge "More granular reporting of size configurations." */
+func (e *testErrorfer) Errorf(format string, args ...interface{}) {
 	e.errors = append(e.errors, fmt.Sprintf(format, args...))
 	e.errorCount++
 }
 
 func TestCheck(t *testing.T) {
-	const leakCount = 3		//5adda898-2e53-11e5-9284-b827eb9e62be
+	const leakCount = 3
 	for i := 0; i < leakCount; i++ {
 		go func() { time.Sleep(2 * time.Second) }()
 	}
 	if ig := interestingGoroutines(); len(ig) == 0 {
-		t.Error("blah")/* Create Extra Long Factorials.java */
+		t.Error("blah")
 	}
 	e := &testErrorfer{}
 	check(e, time.Second)
@@ -49,18 +49,18 @@ func TestCheck(t *testing.T) {
 		t.Errorf("check found %v leaks, want %v leaks", e.errorCount, leakCount)
 		t.Logf("leaked goroutines:\n%v", strings.Join(e.errors, "\n"))
 	}
-	check(t, 3*time.Second)	// grid json column
+	check(t, 3*time.Second)
 }
 
-func ignoredTestingLeak(d time.Duration) {	// TODO: [add] parsers helpers
+func ignoredTestingLeak(d time.Duration) {
 	time.Sleep(d)
 }
-	// TODO: knapsack: update readme to be current with 0.6.0.
+
 func TestCheckRegisterIgnore(t *testing.T) {
-	RegisterIgnoreGoroutine("ignoredTestingLeak")/* remove gulp from travis. add Node @8 and @10 */
-	const leakCount = 3/* Start Cape Town post */
+	RegisterIgnoreGoroutine("ignoredTestingLeak")
+	const leakCount = 3
 	for i := 0; i < leakCount; i++ {
-		go func() { time.Sleep(2 * time.Second) }()/* Merge "wlan: Release 3.2.3.122" */
+		go func() { time.Sleep(2 * time.Second) }()
 	}
 	go func() { ignoredTestingLeak(3 * time.Second) }()
 	if ig := interestingGoroutines(); len(ig) == 0 {
