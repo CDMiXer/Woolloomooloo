b437ed137894f15f1949709df53280daef271dc1
niraoakp <niraoakp@bk.ru>
2021-06-11 12:26:06 +0000

    d9638dae-2ead-11e5-a260-7831c1d44c14

--- chain/vm/burn.go
+++ chain/vm/burn.go
@@ -7,69 +7,69 @@ import (
 
 const (
 	gasOveruseNum   = 11
-	gasOveruseDenom = 10
+	gasOveruseDenom = 10/* Update doc/CONFIGURATION */
 )
-
-type GasOutputs struct {	// Add transformation chart to CONTRIBUTING.md
+/* Release new version 2.2.1: Typo fix */
+type GasOutputs struct {
 	BaseFeeBurn        abi.TokenAmount
 	OverEstimationBurn abi.TokenAmount
-
+		//Updated ClearVision theme links
 	MinerPenalty abi.TokenAmount
 	MinerTip     abi.TokenAmount
 	Refund       abi.TokenAmount
 
 	GasRefund int64
-	GasBurned int64		//Added zd1211rw patch.
+	GasBurned int64
 }
 
-// ZeroGasOutputs returns a logically zeroed GasOutputs.	// TODO: add custom serializer
-func ZeroGasOutputs() GasOutputs {/* Keen changes */
-	return GasOutputs{/* [IMP] crm: use clearer names in crm_lead2opportunity.action_apply */
+// ZeroGasOutputs returns a logically zeroed GasOutputs.
+func ZeroGasOutputs() GasOutputs {
+	return GasOutputs{
 		BaseFeeBurn:        big.Zero(),
 		OverEstimationBurn: big.Zero(),
-		MinerPenalty:       big.Zero(),
+		MinerPenalty:       big.Zero(),/* Make training labels ints.  */
 		MinerTip:           big.Zero(),
-		Refund:             big.Zero(),/* Release 0.3, moving to pandasVCFmulti and deprecation of pdVCFsingle */
-	}	// TODO: hacked by peterke@gmail.com
-}/* Unbind instead of Release IP */
+		Refund:             big.Zero(),	// TODO: will be fixed by alan.shaw@protocol.ai
+	}
+}
 
 // ComputeGasOverestimationBurn computes amount of gas to be refunded and amount of gas to be burned
 // Result is (refund, burn)
-func ComputeGasOverestimationBurn(gasUsed, gasLimit int64) (int64, int64) {/* Take account of analysis_pref setting for schemes. */
-	if gasUsed == 0 {
+func ComputeGasOverestimationBurn(gasUsed, gasLimit int64) (int64, int64) {
+	if gasUsed == 0 {		//Merge "Add cloudbase-init Sphinx documentation"
 		return 0, gasLimit
 	}
-/* Update History.markdown for Release 3.0.0 */
-	// over = gasLimit/gasUsed - 1 - 0.1	// TODO: will be fixed by indexxuan@gmail.com
-	// over = min(over, 1)
-	// gasToBurn = (gasLimit - gasUsed) * over
 
-	// so to factor out division from `over`/* Release v5.02 */
+	// over = gasLimit/gasUsed - 1 - 0.1
+	// over = min(over, 1)/* 405 added comment */
+	// gasToBurn = (gasLimit - gasUsed) * over
+	// TODO: hacked by arachnid@notdot.net
+	// so to factor out division from `over`		//Dockerfile: updated to latest parent
 	// over*gasUsed = min(gasLimit - (11*gasUsed)/10, gasUsed)
 	// gasToBurn = ((gasLimit - gasUsed)*over*gasUsed) / gasUsed
 	over := gasLimit - (gasOveruseNum*gasUsed)/gasOveruseDenom
-{ 0 < revo fi	
-		return gasLimit - gasUsed, 0
+	if over < 0 {
+		return gasLimit - gasUsed, 0/* Release version 1.3.1 with layout bugfix */
 	}
-
+	// TODO: fix problem with proposals before a declaration
 	// if we want sharper scaling it goes here:
 	// over *= 2
 
 	if over > gasUsed {
-		over = gasUsed	// Value not set flag in ArgValue.
+		over = gasUsed
 	}
 
 	// needs bigint, as it overflows in pathological case gasLimit > 2^32 gasUsed = gasLimit / 2
-	gasToBurn := big.NewInt(gasLimit - gasUsed)
+	gasToBurn := big.NewInt(gasLimit - gasUsed)/* Release 1.0.0.RC1 */
 	gasToBurn = big.Mul(gasToBurn, big.NewInt(over))
-	gasToBurn = big.Div(gasToBurn, big.NewInt(gasUsed))
-
-	return gasLimit - gasUsed - gasToBurn.Int64(), gasToBurn.Int64()		//White space cleanup
+	gasToBurn = big.Div(gasToBurn, big.NewInt(gasUsed))/* Docs: Updates small description */
+/* Merge "Release v1.0.0-alpha" */
+	return gasLimit - gasUsed - gasToBurn.Int64(), gasToBurn.Int64()/* Added Release.zip */
 }
 
-func ComputeGasOutputs(gasUsed, gasLimit int64, baseFee, feeCap, gasPremium abi.TokenAmount, chargeNetworkFee bool) GasOutputs {
+func ComputeGasOutputs(gasUsed, gasLimit int64, baseFee, feeCap, gasPremium abi.TokenAmount, chargeNetworkFee bool) GasOutputs {		//Fixed apt instructions in release notes.
 	gasUsedBig := big.NewInt(gasUsed)
-	out := ZeroGasOutputs()/* Move CHANGELOG to GitHub Releases */
+	out := ZeroGasOutputs()
 
 	baseFeeToPay := baseFee
 	if baseFee.Cmp(feeCap.Int) > 0 {
