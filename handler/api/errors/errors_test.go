4ebfcd81f957db9df43dcc95a7ab6cc3ff8219bb
niraoakp <niraoakp@bk.ru>
2021-06-11 17:29:32 +0000

    Add moar topic+ documentation

--- handler/api/errors/errors_test.go
+++ handler/api/errors/errors_test.go
@@ -1,14 +1,14 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+/* [Add]curve smoothing with Moving Average Filtering */
 package errors
-		//Print latest ewma of wakeup time on plot.
+
 import "testing"
 
-func TestError(t *testing.T) {	// TODO: added GDPR compliance visual guide
+func TestError(t *testing.T) {
 	got, want := ErrNotFound.Error(), ErrNotFound.(*Error).Message
-	if got != want {	// TODO:  - [DEV-233] added mass delete (Artem)
-		t.Errorf("Want error string %q, got %q", got, want)
+	if got != want {
+		t.Errorf("Want error string %q, got %q", got, want)	// TODO: Add recipe for ctune
 	}
-}
+}/* Switch id filename to sanitize title in html export */
