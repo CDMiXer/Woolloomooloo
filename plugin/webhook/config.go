653d5d5f8596633ba231b61b28c9034708a00026
niraoakp <niraoakp@bk.ru>
2021-06-11 11:34:51 +0000

    Merge "ASoC: msm: qdsp6v2: set stream volume before starting it"

--- plugin/webhook/config.go
+++ plugin/webhook/config.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// Licensed under the Apache License, Version 2.0 (the "License");/* Улучшения на странице "о приложении" и главной странице, в соответствии с #1 */
+// you may not use this file except in compliance with the License.	// 390e7a86-2e4d-11e5-9284-b827eb9e62be
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
@@ -11,14 +11,14 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+/* Update Injection_Generator.py */
 package webhook
 
 import "github.com/drone/drone/core"
 
 // Config provides the webhook configuration.
 type Config struct {
-	Events   []string
+	Events   []string	// Merge 2.1.0rc2
 	Endpoint []string
 	Secret   string
 	System   *core.System
