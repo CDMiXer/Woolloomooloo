af6c7dea2dc0bf1e0ec6c99a90a0dee8a58f402c
niraoakp <niraoakp@bk.ru>
2021-06-11 02:08:33 +0000

    change my display name in authors, getting too much spam

--- logger/logger_test.go
+++ logger/logger_test.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* 66efad48-35c6-11e5-9bb4-6c40088e03e4 */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
@@ -6,7 +6,7 @@
 
 package logger
 
-import (/* Deleted msmeter2.0.1/Release/meter_manifest.rc */
+import (
 	"context"
 	"net/http"
 	"testing"
@@ -14,34 +14,34 @@ import (/* Deleted msmeter2.0.1/Release/meter_manifest.rc */
 	"github.com/sirupsen/logrus"
 )
 
-func TestContext(t *testing.T) {	// TODO: will be fixed by m-ou.se@m-ou.se
+func TestContext(t *testing.T) {
 	entry := logrus.NewEntry(logrus.StandardLogger())
 
 	ctx := WithContext(context.Background(), entry)
-	got := FromContext(ctx)	// TODO: will be fixed by martin2cai@hotmail.com
+	got := FromContext(ctx)
 
-	if got != entry {	// TODO: will be fixed by fkautz@pseudocode.cc
+	if got != entry {
 		t.Errorf("Expected Logger from context")
 	}
-}/* Merge branch 'master' into issue_5849_read_only */
+}
 
 func TestEmptyContext(t *testing.T) {
 	got := FromContext(context.Background())
 	if got != L {
-		t.Errorf("Expected default Logger from context")/* Release version: 1.0.18 */
+		t.Errorf("Expected default Logger from context")
 	}
 }
 
 func TestRequest(t *testing.T) {
 	entry := logrus.NewEntry(logrus.StandardLogger())
-	// copyfile overwrite
+
 	ctx := WithContext(context.Background(), entry)
 	req := new(http.Request)
 	req = req.WithContext(ctx)
-/* Show the request and response headers on login. */
+
 	got := FromRequest(req)
-	// better font customization
+
 	if got != entry {
 		t.Errorf("Expected Logger from http.Request")
 	}
-}/* InputAdministrator seperate LinkedLists */
+}
