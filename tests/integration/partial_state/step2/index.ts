30b671ac7964476d8418fd7756c6eed10e7e9294
niraoakp <niraoakp@bk.ru>
2021-06-11 16:07:26 +0000

    (v2) Scene cannvas: select the object created with a drop.

--- tests/integration/partial_state/step2/index.ts
+++ tests/integration/partial_state/step2/index.ts
@@ -1,4 +1,4 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Improve the robustness of reading the collections configuration file */
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 // file is deliberately empty.
-// The engine should delete "a" since it's in the checkpoint.	// TODO: will be fixed by earlephilhower@yahoo.com
+// The engine should delete "a" since it's in the checkpoint.
