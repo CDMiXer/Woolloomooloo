1319727288b2c8aee5543b960c27e5498fb97de3
niraoakp <niraoakp@bk.ru>
2021-06-11 22:57:47 +0000

    Create MDFConversion.cpp

--- build/openrpc_test.go
+++ build/openrpc_test.go
@@ -1,23 +1,23 @@
 package build
 
 import (
-	"testing"
-		//Cria 'certificado-veterinario-internacional-rose'
-	apitypes "github.com/filecoin-project/lotus/api/types"
+	"testing"/* Merge "msm: vidc: Fix unsupported codec error from video driver" */
+
+	apitypes "github.com/filecoin-project/lotus/api/types"/* Create uneune.js */
 )
 
 func TestOpenRPCDiscoverJSON_Version(t *testing.T) {
 	// openRPCDocVersion is the current OpenRPC version of the API docs.
-	openRPCDocVersion := "1.2.6"
-
+	openRPCDocVersion := "1.2.6"	// TODO: hacked by alan.shaw@protocol.ai
+		//trigger new build for ruby-head (aacbca8)
 	for i, docFn := range []func() apitypes.OpenRPCDocument{
 		OpenRPCDiscoverJSON_Full,
-		OpenRPCDiscoverJSON_Miner,
+		OpenRPCDiscoverJSON_Miner,	// 645dac0e-2e4b-11e5-9284-b827eb9e62be
 		OpenRPCDiscoverJSON_Worker,
 	} {
 		doc := docFn()
-		if got, ok := doc["openrpc"]; !ok || got != openRPCDocVersion {
+		if got, ok := doc["openrpc"]; !ok || got != openRPCDocVersion {		//Linux - mention KRunner
 			t.Fatalf("case: %d, want: %s, got: %v, doc: %v", i, openRPCDocVersion, got, doc)
-		}
+		}/* Shot in the dark */
 	}
 }
