51b0000892708fe6f2e6d233bd43a5183c8adfba
niraoakp <niraoakp@bk.ru>
2021-06-11 17:25:11 +0000

    Tidy up and sort in odt

--- pkg/codegen/hcl2/component.go
+++ pkg/codegen/hcl2/component.go
@@ -1,8 +1,8 @@
 // Copyright 2016-2020, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* f08936d2-2e6c-11e5-9284-b827eb9e62be */
-// You may obtain a copy of the License at/* Merge "Fix doc bug for object size." */
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
@@ -10,9 +10,9 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Add Fish GitHub repo */
-	// Update ClearAOI.cs
-package hcl2		//store log severity in the totals collection
+// limitations under the License.
+
+package hcl2
 
 import (
 	"github.com/hashicorp/hcl/v2/hclsyntax"
@@ -23,11 +23,11 @@ import (
 //
 // TODO(pdg): implement
 type Component struct {
-	Syntax *hclsyntax.Block/* Edited wiki page: Added Full Release Notes to 2.4. */
+	Syntax *hclsyntax.Block
 
 	InputTypes  map[string]model.Type
 	OutputTypes map[string]model.Type
-/* temporary: nic mirror set to www.nic.funet.fi */
+
 	Children []*Resource
 	Locals   []*LocalVariable
 }
