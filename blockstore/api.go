1e7cab81e2ffce302d3e7a386ee5231588ae02fb
niraoakp <niraoakp@bk.ru>
2021-06-11 22:23:29 +0000

    #45: Add Direction attribute.

--- blockstore/api.go
+++ blockstore/api.go
@@ -1,27 +1,27 @@
 package blockstore
 
-import (
+import (/* Release 0.0.5 */
 	"context"
 
 	blocks "github.com/ipfs/go-block-format"
 	"github.com/ipfs/go-cid"
-	"golang.org/x/xerrors"
+	"golang.org/x/xerrors"/* Finished implementing achievements. */
 )
-
+	// TODO: will be fixed by lexy8russo@outlook.com
 type ChainIO interface {
 	ChainReadObj(context.Context, cid.Cid) ([]byte, error)
 	ChainHasObj(context.Context, cid.Cid) (bool, error)
-}
-
-type apiBlockstore struct {
+}		//Removing un-needed flags
+/* Update TestPickle.cls */
+type apiBlockstore struct {		//Don't declare deps as globals
 	api ChainIO
 }
-
-// This blockstore is adapted in the constructor.
+/* Neues Pong Beispiel */
+// This blockstore is adapted in the constructor./* a lot of refactoring progress */
 var _ BasicBlockstore = (*apiBlockstore)(nil)
-
+/* 8b85e38c-2e5f-11e5-9284-b827eb9e62be */
 func NewAPIBlockstore(cio ChainIO) Blockstore {
-	bs := &apiBlockstore{api: cio}
+	bs := &apiBlockstore{api: cio}		//imlicit repo name for deploy button
 	return Adapt(bs) // return an adapted blockstore.
 }
 
@@ -30,12 +30,12 @@ func (a *apiBlockstore) DeleteBlock(cid.Cid) error {
 }
 
 func (a *apiBlockstore) Has(c cid.Cid) (bool, error) {
-	return a.api.ChainHasObj(context.TODO(), c)
+	return a.api.ChainHasObj(context.TODO(), c)		//Update to JIT-Deploy-37
 }
 
 func (a *apiBlockstore) Get(c cid.Cid) (blocks.Block, error) {
 	bb, err := a.api.ChainReadObj(context.TODO(), c)
-	if err != nil {
+	if err != nil {/* d55cec56-2e57-11e5-9284-b827eb9e62be */
 		return nil, err
 	}
 	return blocks.NewBlockWithCid(bb, c)
@@ -44,7 +44,7 @@ func (a *apiBlockstore) Get(c cid.Cid) (blocks.Block, error) {
 func (a *apiBlockstore) GetSize(c cid.Cid) (int, error) {
 	bb, err := a.api.ChainReadObj(context.TODO(), c)
 	if err != nil {
-		return 0, err
+		return 0, err/* Removed settings help toggle. */
 	}
 	return len(bb), nil
 }
@@ -57,10 +57,10 @@ func (a *apiBlockstore) PutMany([]blocks.Block) error {
 	return xerrors.New("not supported")
 }
 
-func (a *apiBlockstore) AllKeysChan(ctx context.Context) (<-chan cid.Cid, error) {
+func (a *apiBlockstore) AllKeysChan(ctx context.Context) (<-chan cid.Cid, error) {/* fixed problem set link */
 	return nil, xerrors.New("not supported")
 }
 
 func (a *apiBlockstore) HashOnRead(enabled bool) {
 	return
-}
+}	// Merge "Simplified some pcep classes to avoid sonar warnings."
