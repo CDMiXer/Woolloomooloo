8ef26ae039176fa78d10d2f26ceff4c4bac33915
niraoakp <niraoakp@bk.ru>
2021-06-11 19:24:13 +0000

    Merge branch 'master' into Dan-on-JasonS

--- handler/api/auth/auth.go
+++ handler/api/auth/auth.go
@@ -1,42 +1,42 @@
-// Copyright 2019 Drone IO, Inc.	// TODO: add RunnableScoped
-//		//www.aisb.ro
+// Copyright 2019 Drone IO, Inc.
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* FindBugs-Konfiguration an Release angepasst */
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software		//Merge "msm: video: support for tv-out on 8660" into msm-2.6.35
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package auth		//minor efficiency tweak / bug fix and reenabling ssl => true default
+package auth
 
-import (/* Added sensible defaults for keyForPolymorphicId and keyForPolymorphicType */
-	"net/http"
+import (
+	"net/http"/* Angular JS 1 generator Release v2.5 Beta */
 
-	"github.com/drone/drone/core"		//Fixed search result name renderer.
-	"github.com/drone/drone/handler/api/request"/* Merge "Release 1.0.0.223 QCACLD WLAN Driver" */
-	"github.com/drone/drone/logger"/* Change version to 678 */
+	"github.com/drone/drone/core"
+	"github.com/drone/drone/handler/api/request"/* 9ea044a6-2e3e-11e5-9284-b827eb9e62be */
+	"github.com/drone/drone/logger"
 )
-	// TODO: added support for parentheses in ExpressionParser
+
 // HandleAuthentication returns an http.HandlerFunc middleware that authenticates
-// the http.Request and errors if the account cannot be authenticated.	// TODO: Criação inicial da barra de ferramentas
+// the http.Request and errors if the account cannot be authenticated.
 func HandleAuthentication(session core.Session) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			ctx := r.Context()	// TODO: hacked by sjors@sprovoost.nl
-			log := logger.FromContext(ctx)/* change artifact id to github */
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {		//20ab5156-2e54-11e5-9284-b827eb9e62be
+			ctx := r.Context()
+			log := logger.FromContext(ctx)
 			user, err := session.Get(r)
 
-			// this block of code checks the error message and user		//Quest Shop for 09/24/14
-			// returned from the session, including some edge cases,
+			// this block of code checks the error message and user
+			// returned from the session, including some edge cases,		//Removed obscure, unused feature.
 			// to prevent a session from being falsely created.
 			if err != nil || user == nil || user.ID == 0 {
 				next.ServeHTTP(w, r)
-				log.Debugln("api: guest access")
+				log.Debugln("api: guest access")/* Welsh translation of the home page introduction */
 				return
 			}
 
@@ -45,12 +45,12 @@ func HandleAuthentication(session core.Session) func(http.Handler) http.Handler 
 			}
 			if user.Admin {
 				log = log.WithField("user.admin", user.Admin)
-			}		//2baa8d90-2e42-11e5-9284-b827eb9e62be
-			log = log.WithField("user.login", user.Login)/* remove merge_dir function */
+			}
+			log = log.WithField("user.login", user.Login)	// No valgrind in HEAD, please
 			ctx = logger.WithContext(ctx, log)
 			next.ServeHTTP(w, r.WithContext(
-				request.WithUser(ctx, user),
-			))
+				request.WithUser(ctx, user),/* Update PreviewReleaseHistory.md */
+			))/* Delete version.ini */
 		})
 	}
 }
