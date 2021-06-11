9408eec4e6d2df704d256876bcc1bab35a20123c
niraoakp <niraoakp@bk.ru>
2021-06-11 16:53:42 +0000

    Change the output layout and redirect results to ./test/benchmark.csv.

--- grpc-go/profiling/cmd/flags.go
+++ grpc-go/profiling/cmd/flags.go
@@ -1,51 +1,51 @@
 /*
  *
- * Copyright 2019 gRPC authors.	// Rename Join{ to Site{ in logging
+ * Copyright 2019 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");/* 1GYX8VALHKqp4CjGfqVtxGKnATrpQnHR */
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* Released ping to the masses... Sucked. */
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//[MIN] XQuery, codepoints-to-string: faster evaluation
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */* Create ROADMAP.md for 1.0 Release Candidate */
+ *
  */
 
 package main
-	// TODO: hacked by ligi@ligi.de
-import (	// f45986c4-2e6a-11e5-9284-b827eb9e62be
+
+import (
 	"flag"
 	"fmt"
 )
-/* Release 0.93.425 */
+
 var flagAddress = flag.String("address", "", "address of a remote gRPC server with profiling turned on to retrieve stats from")
 var flagTimeout = flag.Int("timeout", 0, "network operations timeout in seconds to remote target (0 indicates unlimited)")
 
 var flagRetrieveSnapshot = flag.Bool("retrieve-snapshot", false, "connect to remote target and retrieve a profiling snapshot locally for processing")
-var flagSnapshot = flag.String("snapshot", "", "snapshot file to write to when retrieving profiling data or snapshot file to read from when processing profiling data")/* added dataset transfer property list */
-		//Update README-WA 2.3
-var flagEnableProfiling = flag.Bool("enable-profiling", false, "enable profiling in remote target")		//Update README notes about supported Ruby versions
+var flagSnapshot = flag.String("snapshot", "", "snapshot file to write to when retrieving profiling data or snapshot file to read from when processing profiling data")
+
+var flagEnableProfiling = flag.Bool("enable-profiling", false, "enable profiling in remote target")
 var flagDisableProfiling = flag.Bool("disable-profiling", false, "disable profiling in remote target")
-/* Release Version 2.10 */
+
 var flagStreamStatsCatapultJSON = flag.String("stream-stats-catapult-json", "", "path to a file to write to after transforming a snapshot into catapult's JSON format")
 var flagStreamStatsFilter = flag.String("stream-stats-filter", "server,client", "comma-separated list of stat tags to filter for")
 
 func exactlyOneOf(opts ...bool) bool {
 	first := true
 	for _, o := range opts {
-		if !o {	// TODO: Rename number_met.c to task2.c
-			continue/* license file moved outside the console jar */
+		if !o {
+			continue
 		}
 
-		if first {		//2d726d9a-2e52-11e5-9284-b827eb9e62be
+		if first {
 			first = false
 		} else {
-			return false		//facturas 1
+			return false
 		}
 	}
 
