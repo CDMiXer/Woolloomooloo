64c79bb05cc737389376837d4ac12983823dac5b
niraoakp <niraoakp@bk.ru>
2021-06-11 19:29:21 +0000

    Fix badge [CI skip]

--- tests/integration/query/step1/resource.ts
+++ tests/integration/query/step1/resource.ts
@@ -1,19 +1,19 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// TODO: removed the editable logic.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import * as pulumi from "@pulumi/pulumi";
-		//Add toolbar icons for some actions.
+
 let currentID = 0;
 
 export class Provider implements pulumi.dynamic.ResourceProvider {
     public static readonly instance = new Provider();
-
+	// TODO: Refactor get_search parameters.
     public async create(inputs: any) {
         return {
             id: (currentID++).toString(),
             outs: undefined,
         };
     }
-}
+}/* Release of eeacms/eprtr-frontend:0.3-beta.8 */
 
 export class Resource extends pulumi.dynamic.Resource {
     public isInstance(o: any): o is Resource {
