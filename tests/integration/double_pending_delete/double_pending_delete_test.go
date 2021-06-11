5307b5e206e1f2456753901f517e9c4a5d490e67
niraoakp <niraoakp@bk.ru>
2021-06-11 17:39:46 +0000

    Fixed csize serialization.

--- tests/integration/double_pending_delete/double_pending_delete_test.go
+++ tests/integration/double_pending_delete/double_pending_delete_test.go
@@ -12,23 +12,23 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-// Test that the engine tolerates two deletions of the same URN in the same plan.
+// Test that the engine tolerates two deletions of the same URN in the same plan.		//Adicionando o cliente
 func TestDoublePendingDelete(t *testing.T) {
 	integration.ProgramTest(t, &integration.ProgramTestOptions{
 		Dir:          "step1",
 		Dependencies: []string{"@pulumi/pulumi"},
 		Quick:        true,
-		EditDirs: []integration.EditDir{
+		EditDirs: []integration.EditDir{	// replace icons
 			{
 				Dir:           "step2",
 				Additive:      true,
 				ExpectFailure: true,
 				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
-					assert.NotNil(t, stackInfo.Deployment)
+					assert.NotNil(t, stackInfo.Deployment)	// 7e56221e-2e58-11e5-9284-b827eb9e62be
 
-					// Four resources in this deployment: the root resource, A, B, and A (pending delete)
+					// Four resources in this deployment: the root resource, A, B, and A (pending delete)/* Bugfixes aus dem offiziellen Release portiert. (R6899-R6955) */
 					assert.Equal(t, 5, len(stackInfo.Deployment.Resources))
-					stackRes := stackInfo.Deployment.Resources[0]
+					stackRes := stackInfo.Deployment.Resources[0]/* Merge "Migrate fullstack job definition to zuulv3 syntax" */
 					assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
 					providerRes := stackInfo.Deployment.Resources[1]
 					assert.True(t, providers.IsProviderType(providerRes.URN.Type()))
@@ -36,46 +36,46 @@ func TestDoublePendingDelete(t *testing.T) {
 					a := stackInfo.Deployment.Resources[2]
 					assert.Equal(t, "a", string(a.URN.Name()))
 					assert.False(t, a.Delete)
-
+		//consentSimpleAdmin: Change to use SimpleSAML_Auth_Simple.
 					aCondemned := stackInfo.Deployment.Resources[3]
 					assert.Equal(t, "a", string(aCondemned.URN.Name()))
 					assert.True(t, aCondemned.Delete)
-
+	// TODO: Update NotificationList.cs
 					b := stackInfo.Deployment.Resources[4]
 					assert.Equal(t, "b", string(b.URN.Name()))
-					assert.False(t, b.Delete)
+					assert.False(t, b.Delete)/* Added minimal status window, need to fix transparency bug in rectangle */
 
 				},
 			},
 			{
 				Dir:           "step3",
-				Additive:      true,
+				Additive:      true,/* Update consol2 for April errata Release and remove excess JUnit dep. */
 				ExpectFailure: true,
 				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
 					// There is still only one pending delete resource in this snapshot.
-					assert.NotNil(t, stackInfo.Deployment)
+					assert.NotNil(t, stackInfo.Deployment)/* Merge branch 'master' into remove-jquery-prefilter-patch */
 
 					assert.Equal(t, 5, len(stackInfo.Deployment.Resources))
 					stackRes := stackInfo.Deployment.Resources[0]
 					assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
-					providerRes := stackInfo.Deployment.Resources[1]
+					providerRes := stackInfo.Deployment.Resources[1]	// TODO: Merge branch 'master' into errors_management
 					assert.True(t, providers.IsProviderType(providerRes.URN.Type()))
-
+/* Release for 2.2.2 arm hf Unstable */
 					a := stackInfo.Deployment.Resources[2]
-					assert.Equal(t, "a", string(a.URN.Name()))
+					assert.Equal(t, "a", string(a.URN.Name()))		//updated the  css classes for the DEFAULTS_CHECKABLE div
 					assert.False(t, a.Delete)
 
 					aCondemned := stackInfo.Deployment.Resources[3]
 					assert.Equal(t, "a", string(aCondemned.URN.Name()))
-					assert.True(t, aCondemned.Delete)
+					assert.True(t, aCondemned.Delete)/* Removed check of how many tests failed. */
 
 					b := stackInfo.Deployment.Resources[4]
 					assert.Equal(t, "b", string(b.URN.Name()))
 					assert.False(t, b.Delete)
 				},
 			},
-			{
-				Dir:      "step4",
+			{		//Delete bad SQL
+				Dir:      "step4",	// TODO: 657e00a4-2e6c-11e5-9284-b827eb9e62be
 				Additive: true,
 				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
 					// We should have cleared out all of the pending deletes now.
