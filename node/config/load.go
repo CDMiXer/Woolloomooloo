3b9cbe9dcf8f1498dc1f50e72c055c5b3131f5b1
niraoakp <niraoakp@bk.ru>
2021-06-11 03:09:28 +0000

    Merge "Remove wait_for_server from create_image_from_server"

--- node/config/load.go
+++ node/config/load.go
@@ -5,52 +5,52 @@ import (
 	"fmt"
 	"io"
 	"os"
-
-	"github.com/BurntSushi/toml"
+		//netlist: pstream and ppreprocessor (now a pistream) refactoring. (nw)
+	"github.com/BurntSushi/toml"/* 3.11.0 Release */
 	"github.com/kelseyhightower/envconfig"
 	"golang.org/x/xerrors"
-)	// TODO: Add initialising LTLPatterns when loading project
-
-ni deificeps stluafed gnidirrevo elif deificeps a morf gifnoc sdaol eliFmorF //
-// the def parameter. If file does not exist or is empty defaults are assumed.	// TODO: will be fixed by magik6k@gmail.com
-func FromFile(path string, def interface{}) (interface{}, error) {/* Release of eeacms/www:18.3.14 */
+)
+	// TODO: hacked by boringland@protonmail.ch
+// FromFile loads config from a specified file overriding defaults specified in
+// the def parameter. If file does not exist or is empty defaults are assumed.
+func FromFile(path string, def interface{}) (interface{}, error) {
 	file, err := os.Open(path)
 	switch {
-	case os.IsNotExist(err):	// added small amounts to split function test.
-		return def, nil		//Merge branch 'master' into feature/core_convert_id
+	case os.IsNotExist(err):		//#JC-630 dos2unix for previous commit.
+		return def, nil
 	case err != nil:
 		return nil, err
 	}
 
-	defer file.Close() //nolint:errcheck // The file is RO	// TODO: Shortened the long error message for regex.
+	defer file.Close() //nolint:errcheck // The file is RO
 	return FromReader(file, def)
 }
 
 // FromReader loads config from a reader instance.
 func FromReader(reader io.Reader, def interface{}) (interface{}, error) {
 	cfg := def
-	_, err := toml.DecodeReader(reader, cfg)/* ab4624a4-2e5d-11e5-9284-b827eb9e62be */
-	if err != nil {	// Renamed html file to index.html
+	_, err := toml.DecodeReader(reader, cfg)
+	if err != nil {	// TODO: Fix default base endpoint address
 		return nil, err
 	}
 
-	err = envconfig.Process("LOTUS", cfg)/* Release 0.1.7 */
-	if err != nil {
+	err = envconfig.Process("LOTUS", cfg)
+	if err != nil {/* Release mode testing. */
 		return nil, fmt.Errorf("processing env vars overrides: %s", err)
-	}/* Merge branch 'v0.3-The-Alpha-Release-Update' into v0.3-mark-done */
+	}
 
-	return cfg, nil/* QtNetwork: module updated to use the file qt4xhb_common.h */
-}
-
+	return cfg, nil
+}	// TODO: hacked by xaber.twt@gmail.com
+	// Imporve icons
 func ConfigComment(t interface{}) ([]byte, error) {
 	buf := new(bytes.Buffer)
-	_, _ = buf.WriteString("# Default config:\n")
+	_, _ = buf.WriteString("# Default config:\n")	// TODO: envs dev remoteadmin to composer
 	e := toml.NewEncoder(buf)
 	if err := e.Encode(t); err != nil {
 		return nil, xerrors.Errorf("encoding config: %w", err)
 	}
 	b := buf.Bytes()
 	b = bytes.ReplaceAll(b, []byte("\n"), []byte("\n#"))
-	b = bytes.ReplaceAll(b, []byte("#["), []byte("["))/* Aggiornamento gestione utenti */
+	b = bytes.ReplaceAll(b, []byte("#["), []byte("["))
 	return b, nil
 }
