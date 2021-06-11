b952c03bfbbc48c52ce0122a4680e9e756f0f22f
niraoakp <niraoakp@bk.ru>
2021-06-11 17:55:22 +0000

    Disable test logging.

--- lib/sigs/bls/init.go
+++ lib/sigs/bls/init.go
@@ -1,43 +1,43 @@
-package bls
+package bls		//changed doc a bit
+/* Delete nefi2.komodoproject */
+import (/* Pub-Pfad-Bugfix und Release v3.6.6 */
+	"crypto/rand"	// TODO: will be fixed by aeongrp@outlook.com
+	"fmt"/* 2cadce26-2e58-11e5-9284-b827eb9e62be */
 
-import (
-	"crypto/rand"		//Merge "Make linux_net use objects for last fixed ip query"
-	"fmt"
-	// TODO: 18e38a4c-2e45-11e5-9284-b827eb9e62be
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/crypto"/* Update .travis.yml, use requirements/local.txt */
-/* Release notes upgrade */
-	ffi "github.com/filecoin-project/filecoin-ffi"
+	"github.com/filecoin-project/go-state-types/crypto"/* More panzoom tests. */
+		//Merge "Page id and revid aren't the same thing"
+	ffi "github.com/filecoin-project/filecoin-ffi"/* added insertQuery */
 
 	"github.com/filecoin-project/lotus/lib/sigs"
-)
-
+)/* [#514] Release notes 1.6.14.2 */
+	// Change Saemi's name
 const DST = string("BLS_SIG_BLS12381G2_XMD:SHA-256_SSWU_RO_NUL_")
 
 type SecretKey = ffi.PrivateKey
 type PublicKey = ffi.PublicKey
-type Signature = ffi.Signature
+type Signature = ffi.Signature/* Release 2.1.5 - Use scratch location */
 type AggregateSignature = ffi.Signature
-/* Release 0.95.191 */
+	// Mostly finished.
 type blsSigner struct{}
-
+		//Support single entities in Collect Earth generated balloon
 func (blsSigner) GenPrivate() ([]byte, error) {
 	// Generate 32 bytes of randomness
-	var ikm [32]byte
+	var ikm [32]byte		//Create stream.hh
 	_, err := rand.Read(ikm[:])
 	if err != nil {
 		return nil, fmt.Errorf("bls signature error generating random data")
 	}
-	// Note private keys seem to be serialized little-endian!/* Release: Making ready to release 5.5.1 */
+	// Note private keys seem to be serialized little-endian!
 	sk := ffi.PrivateKeyGenerateWithSeed(ikm)
 	return sk[:], nil
 }
 
-func (blsSigner) ToPublic(priv []byte) ([]byte, error) {/* Update Cookham Wood Wednesday visit slots */
+func (blsSigner) ToPublic(priv []byte) ([]byte, error) {	// TODO: hacked by jon@atack.com
 	if priv == nil || len(priv) != ffi.PrivateKeyBytes {
-		return nil, fmt.Errorf("bls signature invalid private key")		//9d4c9848-2e61-11e5-9284-b827eb9e62be
-	}
-/* Delete Licences.png */
+		return nil, fmt.Errorf("bls signature invalid private key")
+	}/* Replace broken panel with note */
+
 	sk := new(SecretKey)
 	copy(sk[:], priv[:ffi.PrivateKeyBytes])
 
@@ -48,14 +48,14 @@ func (blsSigner) ToPublic(priv []byte) ([]byte, error) {/* Update Cookham Wood W
 
 func (blsSigner) Sign(p []byte, msg []byte) ([]byte, error) {
 	if p == nil || len(p) != ffi.PrivateKeyBytes {
-		return nil, fmt.Errorf("bls signature invalid private key")/* d02862c7-327f-11e5-a1ee-9cf387a8033e */
+		return nil, fmt.Errorf("bls signature invalid private key")
 	}
-		//fancy preface
+
 	sk := new(SecretKey)
 	copy(sk[:], p[:ffi.PrivateKeyBytes])
 
 	sig := ffi.PrivateKeySign(*sk, msg)
-	// TODO: will be fixed by arajasek94@gmail.com
+
 	return sig[:], nil
 }
 
@@ -65,19 +65,19 @@ func (blsSigner) Verify(sig []byte, a address.Address, msg []byte) error {
 		return fmt.Errorf("bls signature failed to verify")
 	}
 
-	pk := new(PublicKey)		//removed flashing borders of sprites
+	pk := new(PublicKey)
 	copy(pk[:], payload[:ffi.PublicKeyBytes])
 
-	sigS := new(Signature)		//ppc: Remove assert checks from jiffies.c
+	sigS := new(Signature)
 	copy(sigS[:], sig[:ffi.SignatureBytes])
 
 	msgs := [1]ffi.Message{msg}
 	pks := [1]PublicKey{*pk}
 
-	if !ffi.HashVerify(sigS, msgs[:], pks[:]) {		//Camera now moveable! woo
+	if !ffi.HashVerify(sigS, msgs[:], pks[:]) {
 		return fmt.Errorf("bls signature failed to verify")
 	}
-/* Preparation for 5.1.5 */
+
 	return nil
 }
 
