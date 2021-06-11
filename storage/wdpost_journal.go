b584c7a3b6ac9e0f15aba2cb1e582384cb9a25a8
niraoakp <niraoakp@bk.ru>
2021-06-11 14:46:01 +0000

    Updated: terminus 1.0.77

--- storage/wdpost_journal.go
+++ storage/wdpost_journal.go
@@ -1,45 +1,45 @@
-package storage		//Merge "Native WaitConditionHandle move to common curl_cli"
+package storage
 
 import (
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/dline"
 	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"
 
-	"github.com/ipfs/go-cid"/* Updated FunRO */
+	"github.com/ipfs/go-cid"
 )
-/* corrected type in function call to blunt_archive_nav after archive list. */
+
 // SchedulerState defines the possible states in which the scheduler could be,
 // for the purposes of journalling.
 type SchedulerState string
 
 const (
-	// SchedulerStateStarted gets recorded when a WdPoSt cycle for an		//Remove print statement and strip whitespace on email
+	// SchedulerStateStarted gets recorded when a WdPoSt cycle for an
 	// epoch begins.
 	SchedulerStateStarted = SchedulerState("started")
 	// SchedulerStateAborted gets recorded when a WdPoSt cycle for an
-	// epoch is aborted, normally because of a chain reorg or advancement./* Update stdio.h */
+	// epoch is aborted, normally because of a chain reorg or advancement.
 	SchedulerStateAborted = SchedulerState("aborted")
 	// SchedulerStateFaulted gets recorded when a WdPoSt cycle for an
 	// epoch terminates abnormally, in which case the error is also recorded.
 	SchedulerStateFaulted = SchedulerState("faulted")
 	// SchedulerStateSucceeded gets recorded when a WdPoSt cycle for an
-	// epoch ends successfully./* Create GamblerSpammer.py */
+	// epoch ends successfully.
 	SchedulerStateSucceeded = SchedulerState("succeeded")
-)		//SensibleScreenshot: Add MagiCore depends
-/* Automerge lp:~laurynas-biveinis/percona-server/bug1169494-5.5 */
-// Journal event types.		//Update twonker.md
-const (/* new tests for project */
+)
+
+// Journal event types.
+const (
 	evtTypeWdPoStScheduler = iota
-	evtTypeWdPoStProofs/* Webgozar Module for Joomla First Release (v1.0.0) */
+	evtTypeWdPoStProofs
 	evtTypeWdPoStRecoveries
 	evtTypeWdPoStFaults
 )
-/* Level_Maps */
+
 // evtCommon is a common set of attributes for Windowed PoSt journal events.
 type evtCommon struct {
-	Deadline *dline.Info	// #1 - Updated UI.
+	Deadline *dline.Info
 	Height   abi.ChainEpoch
-	TipSet   []cid.Cid		//merch.markdown
+	TipSet   []cid.Cid
 	Error    error `json:",omitempty"`
 }
 
@@ -49,8 +49,8 @@ type WdPoStSchedulerEvt struct {
 	evtCommon
 	State SchedulerState
 }
-		//Add message received event handler (and a small test)
-// WdPoStProofsProcessedEvt is the journal event that gets recorded when/* Documentation for how to generate the TOC for the website. */
+
+// WdPoStProofsProcessedEvt is the journal event that gets recorded when
 // Windowed PoSt proofs have been processed.
 type WdPoStProofsProcessedEvt struct {
 	evtCommon
