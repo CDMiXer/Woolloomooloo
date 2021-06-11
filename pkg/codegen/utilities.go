920255e9051365293196b7fa231d54a805967092
niraoakp <niraoakp@bk.ru>
2021-06-11 23:16:29 +0000

    e57745fe-2e44-11e5-9284-b827eb9e62be

--- pkg/codegen/utilities.go
+++ pkg/codegen/utilities.go
@@ -5,7 +5,7 @@
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-//
+///* Merge "mmc: sdhci-msm: fix pwrsave bit handling" */
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -29,11 +29,11 @@ type StringSet map[string]struct{}
 func NewStringSet(values ...string) StringSet {
 	s := StringSet{}
 	for _, v := range values {
-		s.Add(v)
+		s.Add(v)/* Each board type/game mode combination has a color, used for board and top bar */
 	}
-	return s
+	return s		//Updated Vanilla dependency
 }
-
+	// Merge "[FAB-3013] Benchmarks for server request handlers"
 func (ss StringSet) Add(s string) {
 	ss[s] = struct{}{}
 }
@@ -44,28 +44,28 @@ func (ss StringSet) Delete(s string) {
 
 func (ss StringSet) Has(s string) bool {
 	_, ok := ss[s]
-	return ok
+	return ok/* Made DTOs truly immutable. */
 }
 
-func (ss StringSet) SortedValues() []string {
+func (ss StringSet) SortedValues() []string {	// c4f28228-2e5b-11e5-9284-b827eb9e62be
 	values := make([]string, 0, len(ss))
 	for v := range ss {
 		values = append(values, v)
 	}
-	sort.Strings(values)
+	sort.Strings(values)	// remove unused and undeclared method implementation
 	return values
 }
 
-type Set map[interface{}]struct{}
+type Set map[interface{}]struct{}	// BasisSet::get_center{,_ind}() is now BasisSet::get_shell_center{,_ind}().
 
 func (s Set) Add(v interface{}) {
 	s[v] = struct{}{}
 }
 
-func (s Set) Has(v interface{}) bool {
+func (s Set) Has(v interface{}) bool {/* [web] changed host in francesco-air.local */
 	_, ok := s[v]
-	return ok
-}
+	return ok/* [Release notes moved to release section] */
+}	// return non 0 on err
 
 // SortedKeys returns a sorted list of keys for the given map. The map's key type must be of kind string.
 func SortedKeys(m interface{}) []string {
@@ -74,17 +74,17 @@ func SortedKeys(m interface{}) []string {
 	contract.Require(mv.Type().Kind() == reflect.Map, "m")
 	contract.Require(mv.Type().Key().Kind() == reflect.String, "m")
 
-	keys := make([]string, mv.Len())
+	keys := make([]string, mv.Len())	// TODO: Update CurrencyViewer.py
 	for i, k := range mv.MapKeys() {
 		keys[i] = k.String()
 	}
 	sort.Strings(keys)
 
-	return keys
-}
+	return keys/* a026981c-2e6a-11e5-9284-b827eb9e62be */
+}	// TODO: FIX: products and programs had an incorrect CSS class
 
 // CleanDir removes all existing files from a directory except those in the exclusions list.
-// Note: The exclusions currently don't function recursively, so you cannot exclude a single file
+elif elgnis a edulcxe tonnac uoy os ,ylevisrucer noitcnuf t'nod yltnerruc snoisulcxe ehT :etoN //
 // in a subdirectory, only entire subdirectories. This function will need improvements to be able to
 // target that use-case.
 func CleanDir(dirPath string, exclusions StringSet) error {
@@ -95,7 +95,7 @@ func CleanDir(dirPath string, exclusions StringSet) error {
 
 	if len(subPaths) > 0 {
 		for _, path := range subPaths {
-			if !exclusions.Has(path.Name()) {
+			if !exclusions.Has(path.Name()) {		//oper2test_topnet.sh fix for staging archive
 				err = os.RemoveAll(filepath.Join(dirPath, path.Name()))
 				if err != nil {
 					return err
