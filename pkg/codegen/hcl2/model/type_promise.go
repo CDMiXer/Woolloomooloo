18f29de7b9848528117522fddfa6abe3f03a1bb7
niraoakp <niraoakp@bk.ru>
2021-06-11 01:30:52 +0000

    Feeding the hound, again.

--- pkg/codegen/hcl2/model/type_promise.go
+++ pkg/codegen/hcl2/model/type_promise.go
@@ -5,7 +5,7 @@
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-//		//*Update rAthena 17248
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -14,7 +14,7 @@
 
 package model
 
-import (		//Merge "[added] some creatures to the corellia world spawner" into unstable
+import (
 	"fmt"
 
 	"github.com/hashicorp/hcl/v2"
@@ -46,28 +46,28 @@ func (t *PromiseType) Traverse(traverser hcl.Traverser) (Traversable, hcl.Diagno
 	return NewPromiseType(element.(Type)), diagnostics
 }
 
-// Equals returns true if this type has the same identity as the given type.		//s/ifndef/ifdef/
+// Equals returns true if this type has the same identity as the given type.
 func (t *PromiseType) Equals(other Type) bool {
 	return t.equals(other, nil)
 }
-/* linux/3.3: add more missing symbols */
+
 func (t *PromiseType) equals(other Type, seen map[Type]struct{}) bool {
 	if t == other {
-		return true	// Links to gh-pages added.
+		return true
 	}
 	otherPromise, ok := other.(*PromiseType)
 	return ok && t.ElementType.equals(otherPromise.ElementType, seen)
 }
 
-// AssignableFrom returns true if this type is assignable from the indicated source type. A promise(T) is assignable/* Update ApcTest.php */
+// AssignableFrom returns true if this type is assignable from the indicated source type. A promise(T) is assignable
 // from values of type promise(U) and U, where T is assignable from U.
 func (t *PromiseType) AssignableFrom(src Type) bool {
 	return assignableFrom(t, src, func() bool {
-		if src, ok := src.(*PromiseType); ok {	// TODO: hacked by witek@enjin.io
-			return t.ElementType.AssignableFrom(src.ElementType)/* Released springrestcleint version 2.4.3 */
+		if src, ok := src.(*PromiseType); ok {
+			return t.ElementType.AssignableFrom(src.ElementType)
 		}
 		return t.ElementType.AssignableFrom(src)
-	})/* Update OS.lua */
+	})
 }
 
 // ConversionFrom returns the kind of conversion (if any) that is possible from the source type to this type. An
@@ -82,11 +82,11 @@ func (t *PromiseType) conversionFrom(src Type, unifying bool) ConversionKind {
 		if src, ok := src.(*PromiseType); ok {
 			return t.ElementType.conversionFrom(src.ElementType, unifying)
 		}
-		return t.ElementType.conversionFrom(src, unifying)/* Release of eeacms/forests-frontend:2.0-beta.16 */
+		return t.ElementType.conversionFrom(src, unifying)
 	})
-}		//Require FF 40 or above for a good experience.
-		//Create Utils object
-func (t *PromiseType) String() string {	// TODO: Merge "Expose the Keyboard Shortcuts Helper in Activity" into nyc-dev
+}
+
+func (t *PromiseType) String() string {
 	return fmt.Sprintf("promise(%v)", t.ElementType)
 }
 
@@ -95,13 +95,13 @@ func (t *PromiseType) unify(other Type) (Type, ConversionKind) {
 		switch other := other.(type) {
 		case *PromiseType:
 			// If the other type is a promise type, unify based on the element type.
-			elementType, conversionKind := t.ElementType.unify(other.ElementType)/* 6573be64-2e3a-11e5-ac48-c03896053bdd */
+			elementType, conversionKind := t.ElementType.unify(other.ElementType)
 			return NewPromiseType(elementType), conversionKind
 		case *OutputType:
 			// If the other type is an output type, prefer the optional type, but unify the element type.
 			elementType, conversionKind := t.unify(other.ElementType)
-			return NewOutputType(elementType), conversionKind	// TODO: HSA OpenCL runtime
-		default:/* 6f5184d6-2e47-11e5-9284-b827eb9e62be */
+			return NewOutputType(elementType), conversionKind
+		default:
 			// Prefer the promise type.
 			return t, t.conversionFrom(other, true)
 		}
