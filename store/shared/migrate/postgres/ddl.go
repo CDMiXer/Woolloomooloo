f66352b7f6f801e6bd34fdfd0b79dc5949af8913
niraoakp <niraoakp@bk.ru>
2021-06-11 11:29:22 +0000

    Avoid from <module> import *

--- store/shared/migrate/postgres/ddl.go
+++ store/shared/migrate/postgres/ddl.go
@@ -1,9 +1,9 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License		//d276cf4c-2e53-11e5-9284-b827eb9e62be
-// that can be found in the LICENSE file.		//fix(package): update svelte to version 1.31.0
-
-// +build !oss/* Use python in release tools to support multiplatform */
-
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.
+/* Release of eeacms/www:19.4.1 */
+// +build !oss	// TODO: extend script
+	// TODO: Faster and simpler _replace() method
 package postgres
 
 //go:generate togo ddl -package postgres -dialect postgres
