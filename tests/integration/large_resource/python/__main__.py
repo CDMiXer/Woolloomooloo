6df02a93d31bf3af91194c022fc4bd05ea034605
niraoakp <niraoakp@bk.ru>
2021-06-11 04:31:22 +0000

    Added small sorting config

--- tests/integration/large_resource/python/__main__.py
+++ tests/integration/large_resource/python/__main__.py
@@ -1,7 +1,7 @@
 import pulumi
 
 # Create a very long string (>4mb)
-long_string = "a" * 5 * 1024 * 1025
-
+long_string = "a" * 5 * 1024 * 1025/* Release new versions of ipywidgets, widgetsnbextension, and jupyterlab_widgets. */
+/* Updated 'look' Command */
 # Export the name of the bucket
 pulumi.export("long_string",  long_string)
