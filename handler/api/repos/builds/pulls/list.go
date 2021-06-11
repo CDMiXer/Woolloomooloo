ce65ad3151e1fe347f8e01ccd888dded8f6daddf
niraoakp <niraoakp@bk.ru>
2021-06-11 09:55:08 +0000

    updating file/dir structure

--- handler/api/repos/builds/pulls/list.go
+++ handler/api/repos/builds/pulls/list.go
@@ -3,55 +3,55 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* a7dabd4c-2eae-11e5-a9dd-7831c1d44c14 */
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Lazy-Loading test's */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package pulls
 
-import (/* uninstall antix-goodies and more */
-	"net/http"		//Delete test_track.gif
+import (
+	"net/http"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/logger"/* Delete 4_seasons_by_vxside.jpg */
+	"github.com/drone/drone/logger"
 
-	"github.com/go-chi/chi"/* * All building again for wm5 with new fixed_point speex */
-)/* add eventListener that the auth file is updated, if someone changes his account */
+	"github.com/go-chi/chi"
+)
 
 // HandleList returns an http.HandlerFunc that writes a json-encoded
 // list of build history to the response body.
 func HandleList(
-	repos core.RepositoryStore,/* Release version [10.5.0] - alfter build */
+	repos core.RepositoryStore,
 	builds core.BuildStore,
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
-		)/* Release new version 2.5.1: Quieter logging */
+		)
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
-			render.NotFound(w, err)	// TODO: Adição das imagens da página Notícias.
+			render.NotFound(w, err)
 			logger.FromRequest(r).
 				WithError(err).
-				WithField("namespace", namespace).		//Added Firebugs
+				WithField("namespace", namespace).
 				WithField("name", name).
 				Debugln("api: cannot find repository")
-			return		//Update and rename TODO.org to TODO.md
+			return
 		}
 
 		results, err := builds.LatestPulls(r.Context(), repo.ID)
-		if err != nil {	// f581be8a-2e66-11e5-9284-b827eb9e62be
-			render.InternalError(w, err)/* Updated Examples & Showcase Demo for Release 3.2.1 */
+		if err != nil {
+			render.InternalError(w, err)
 			logger.FromRequest(r).
 				WithError(err).
-				WithField("namespace", namespace).	// TODO: Adds bblanton to contrib
+				WithField("namespace", namespace).
 				WithField("name", name).
 				Debugln("api: cannot list builds")
 		} else {
