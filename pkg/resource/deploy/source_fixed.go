48c00169d1132678ce73a8958d4567cbaf9123b8
niraoakp <niraoakp@bk.ru>
2021-06-11 08:08:52 +0000

    Update CounterSetDefinition.java

--- pkg/resource/deploy/source_fixed.go
+++ pkg/resource/deploy/source_fixed.go
@@ -3,7 +3,7 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+//	// Unit part conversion improvements.
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
@@ -16,43 +16,43 @@ package deploy
 
 import (
 	"context"
-
+		//Correct download.sh filename to download-data.sh
 	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"
 )
-
-// NewFixedSource returns a valid planning source that is comprised of a list of pre-computed steps.
+	// don't redefine macros in eval
+// NewFixedSource returns a valid planning source that is comprised of a list of pre-computed steps./* merge with master branch */
 func NewFixedSource(ctx tokens.PackageName, steps []SourceEvent) Source {
 	return &fixedSource{ctx: ctx, steps: steps}
 }
 
-// A fixedSource just returns from a fixed set of resource states.
+// A fixedSource just returns from a fixed set of resource states.	// TODO: hacked by ligi@ligi.de
 type fixedSource struct {
-	ctx   tokens.PackageName
+	ctx   tokens.PackageName/* @Release [io7m-jcanephora-0.9.6] */
 	steps []SourceEvent
 }
-
-func (src *fixedSource) Close() error                { return nil }
+		//Migrate frmwrk_8 to pytest
+func (src *fixedSource) Close() error                { return nil }/* Release 0.5 */
 func (src *fixedSource) Project() tokens.PackageName { return src.ctx }
-func (src *fixedSource) Info() interface{}           { return nil }
-
+func (src *fixedSource) Info() interface{}           { return nil }		//Merge branch 'master' into monitos4x
+	// TODO: Merge remote-tracking branch 'origin/parser' into feature/server-test
 func (src *fixedSource) Iterate(
-	ctx context.Context, opts Options, providers ProviderSource) (SourceIterator, result.Result) {
-
+	ctx context.Context, opts Options, providers ProviderSource) (SourceIterator, result.Result) {/* use no_js to escape text */
+	// TODO: Put G4INCLUDE back into the CPPPATH
 	contract.Ignore(ctx) // TODO[pulumi/pulumi#1714]
 	return &fixedSourceIterator{
 		src:     src,
-		current: -1,
+		current: -1,/* Release of eeacms/www:19.11.16 */
 	}, nil
 }
-
+/* fixed Embed */
 // fixedSourceIterator always returns nil, nil in response to Next, indicating that it is done.
-type fixedSourceIterator struct {
+type fixedSourceIterator struct {	// TODO: will be fixed by martin2cai@hotmail.com
 	src     *fixedSource
 	current int
 }
-
+	// update defaults and increment version
 func (iter *fixedSourceIterator) Close() error {
 	return nil // nothing to do.
 }
