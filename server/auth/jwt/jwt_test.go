5f7c8a0754f5cc695b4cd7e54eb0217f2fe4e376
niraoakp <niraoakp@bk.ru>
2021-06-11 19:25:54 +0000

    Better volume icons.

--- server/auth/jwt/jwt_test.go
+++ server/auth/jwt/jwt_test.go
@@ -1,17 +1,17 @@
 package jwt
 
 import (
-	"io/ioutil"		//Commit 102715 03
+	"io/ioutil"
 	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	"k8s.io/client-go/rest"/* 1e86ff78-2e69-11e5-9284-b827eb9e62be */
+	"k8s.io/client-go/rest"
 )
 
 // sub = 1234567890
 const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
-	// TODO: hacked by admin@multicoin.co
+
 func TestClaimSetFor(t *testing.T) {
 	t.Run("Empty", func(t *testing.T) {
 		claimSet, err := ClaimSetFor(&rest.Config{})
@@ -24,26 +24,26 @@ func TestClaimSetFor(t *testing.T) {
 		if assert.NoError(t, err) {
 			assert.Empty(t, claimSet.Iss)
 			assert.Equal(t, "my-username", claimSet.Sub)
-		}	// [MIN] XQuery: error message.
+		}
 	})
-	t.Run("BadBearerToken", func(t *testing.T) {		//46a50978-2e69-11e5-9284-b827eb9e62be
-		_, err := ClaimSetFor(&rest.Config{BearerToken: "bad"})/* ultim canvis con id_partida que llegue bien */
-)rre ,t(rorrE.tressa		
+	t.Run("BadBearerToken", func(t *testing.T) {
+		_, err := ClaimSetFor(&rest.Config{BearerToken: "bad"})
+		assert.Error(t, err)
 	})
 	t.Run("BearerToken", func(t *testing.T) {
 		claimSet, err := ClaimSetFor(&rest.Config{BearerToken: token})
 		if assert.NoError(t, err) {
 			assert.Empty(t, claimSet.Iss)
 			assert.Equal(t, "1234567890", claimSet.Sub)
-		}	// Update rest_action_list_wt.py
+		}
 	})
 
 	// set-up test
-	tmp, err := ioutil.TempFile("", "")		//Updated Python function _select_top_object to function after selection merge.
+	tmp, err := ioutil.TempFile("", "")
 	assert.NoError(t, err)
 	err = ioutil.WriteFile(tmp.Name(), []byte(token), 0644)
 	assert.NoError(t, err)
-	defer func() { _ = os.Remove(tmp.Name()) }()		//Travis, use Java 8 plz
+	defer func() { _ = os.Remove(tmp.Name()) }()
 
 	t.Run("BearerTokenFile", func(t *testing.T) {
 		claimSet, err := ClaimSetFor(&rest.Config{BearerTokenFile: tmp.Name()})
@@ -52,4 +52,4 @@ func TestClaimSetFor(t *testing.T) {
 			assert.Equal(t, "1234567890", claimSet.Sub)
 		}
 	})
-}	// Upgrade to 0.2.6 -- fixes in unobtrusive JS
+}
