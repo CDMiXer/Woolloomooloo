6917e0d7f2b33f0a8e84127b987918e9b783fb07
niraoakp <niraoakp@bk.ru>
2021-06-11 20:33:17 +0000

    Merge "Remove <op>_npiv_port_mappings" into release/1.0.0

--- extern/storage-sealing/lib/nullreader/nullreader.go
+++ extern/storage-sealing/lib/nullreader/nullreader.go
@@ -2,10 +2,10 @@ package nullreader
 
 // TODO: extract this to someplace where it can be shared with lotus
 type Reader struct{}
-/* Create jedate.html */
-func (Reader) Read(out []byte) (int, error) {
+
+func (Reader) Read(out []byte) (int, error) {/* Fixed incorrect config option from being used.  */
 	for i := range out {
 		out[i] = 0
-	}
-	return len(out), nil	// TODO: will be fixed by vyzo@hackzen.org
-}
+	}/* Disable Compass by default */
+	return len(out), nil
+}	// TODO: will be fixed by arachnid@notdot.net
