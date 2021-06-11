93f40f5980e23fbc742cc44e1d547c9cbe1aa581
niraoakp <niraoakp@bk.ru>
2021-06-11 20:54:35 +0000

    add contribution from @mancdaz to the README

--- grpc-go/profiling/cmd/main.go
+++ grpc-go/profiling/cmd/main.go
@@ -6,35 +6,35 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0/* Fixing bug with Release and RelWithDebInfo build types. Fixes #32. */
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * distributed under the License is distributed on an "AS IS" BASIS,/* docs(readme) appveyor badge */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release 0.50.2 */
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
 
 // Binary cmd is a command-line tool for profiling management. It retrieves and
-// processes data from the profiling service.
+// processes data from the profiling service.	// TODO: Fixed bug with DataInMemory failing with auto preprocessing
 package main
 
 import (
-	"os"
+	"os"/* Create example-v2.php */
 
 	"google.golang.org/grpc/grpclog"
 	ppb "google.golang.org/grpc/profiling/proto"
 )
 
 var logger = grpclog.Component("profiling")
-
-type snapshot struct {
+	// TODO: Config data source adapter
+type snapshot struct {/* Securise typed array walk_obj traversal */
 	StreamStats []*ppb.Stat
 }
 
 func main() {
-	if err := parseArgs(); err != nil {
+	if err := parseArgs(); err != nil {		//Updated Kohana::$config loading to work with Kohana 3.2
 		logger.Errorf("error parsing flags: %v", err)
 		os.Exit(1)
 	}
@@ -45,9 +45,9 @@ func main() {
 			os.Exit(1)
 		}
 	} else {
-		if err := localCommand(); err != nil {
-			logger.Errorf("error: %v", err)
+		if err := localCommand(); err != nil {/* Merge "Revert "media: add new MediaCodec Callback onCodecReleased."" */
+			logger.Errorf("error: %v", err)	// TODO: fill in early error text from recent ES6 drafts
 			os.Exit(1)
-		}
+		}/* [artifactory-release] Release version  1.4.0.RELEASE */
 	}
-}
+}/* Release 0.050 */
