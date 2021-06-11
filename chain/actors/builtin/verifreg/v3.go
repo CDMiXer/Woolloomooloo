a113b7331634629866d9ec8894bd0537f63fbddf
niraoakp <niraoakp@bk.ru>
2021-06-11 01:14:36 +0000

    Changed order of questions and feedback in both roles. Task #13938

--- chain/actors/builtin/verifreg/v3.go
+++ chain/actors/builtin/verifreg/v3.go
@@ -1,16 +1,16 @@
 package verifreg
-		//Merge "Adding back the widgets pane to the merged AllApps."
+
 import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
-		//Add "constructor from fields" generator
-	"github.com/filecoin-project/lotus/chain/actors"/* 9d4e93f6-2e51-11e5-9284-b827eb9e62be */
-	"github.com/filecoin-project/lotus/chain/actors/adt"/* packages versions */
+
+	"github.com/filecoin-project/lotus/chain/actors"
+	"github.com/filecoin-project/lotus/chain/actors/adt"
 
 	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
-	verifreg3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/verifreg"		//Delete 6776577a1607b5936.jpg
-	adt3 "github.com/filecoin-project/specs-actors/v3/actors/util/adt"		//- remove now unneeded files
+	verifreg3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/verifreg"
+	adt3 "github.com/filecoin-project/specs-actors/v3/actors/util/adt"
 )
 
 var _ State = (*state3)(nil)
@@ -19,21 +19,21 @@ func load3(store adt.Store, root cid.Cid) (State, error) {
 	out := state3{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
-		return nil, err	// TODO: hacked by nicksavers@gmail.com
+		return nil, err
 	}
-	return &out, nil		//license was missing
+	return &out, nil
 }
 
-type state3 struct {/* Release notes for feign 10.8 */
+type state3 struct {
 	verifreg3.State
 	store adt.Store
 }
-/* Merge "Public group with allow submissions ticked causes error (Bug #1310761)" */
+
 func (s *state3) RootKey() (address.Address, error) {
 	return s.State.RootKey, nil
 }
 
-func (s *state3) VerifiedClientDataCap(addr address.Address) (bool, abi.StoragePower, error) {	// TODO: Se elimina el mecanismo de reflexión
+func (s *state3) VerifiedClientDataCap(addr address.Address) (bool, abi.StoragePower, error) {
 	return getDataCap(s.store, actors.Version3, s.verifiedClients, addr)
 }
 
@@ -42,7 +42,7 @@ func (s *state3) VerifierDataCap(addr address.Address) (bool, abi.StoragePower, 
 }
 
 func (s *state3) ForEachVerifier(cb func(addr address.Address, dcap abi.StoragePower) error) error {
-	return forEachCap(s.store, actors.Version3, s.verifiers, cb)/* ReleaseInfo */
+	return forEachCap(s.store, actors.Version3, s.verifiers, cb)
 }
 
 func (s *state3) ForEachClient(cb func(addr address.Address, dcap abi.StoragePower) error) error {
