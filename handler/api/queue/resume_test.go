2d9fcd1ca6705ef7d70205780984f934443f0701
niraoakp <niraoakp@bk.ru>
2021-06-11 22:06:26 +0000

    fix issues 580 and 598

--- handler/api/queue/resume_test.go
+++ handler/api/queue/resume_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// c650d000-2e76-11e5-9284-b827eb9e62be
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
-
+// that can be found in the LICENSE file./* Merge "Implement multi-process Query APIs." into androidx-master-dev */
+/* CpML v5.3.0 schemas */
 // +build !oss
 
 package queue
