4c0027448d2ad5b4b845e81b071e67dc68cdb5a6
niraoakp <niraoakp@bk.ru>
2021-06-11 06:53:53 +0000

    change toc tree of xray magnetic example

--- tests/integration/invalid_package_json/index.ts
+++ tests/integration/invalid_package_json/index.ts
@@ -1,16 +1,16 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Overlay: TextArea - ensure glyphinfo is loaded/ avoid superfluos lookups */
-
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+	// add function while
 import { Config } from "@pulumi/pulumi";
 import * as runtime from "@pulumi/pulumi/runtime"
-
+		//Added BookmarksToSQL Project to index.html
 (async function() {
     const config = new Config();
-
+		//// README.md : Update description, mention Git recursive cloning.
     // Ensure we get the right set of dependencies back.  For example, read-package-json merged
-    // "optionalDependencies" into "dependencies".  We want to make sure we still follow that/* Release 0.2.0 of swak4Foam */
+    // "optionalDependencies" into "dependencies".  We want to make sure we still follow that
     // behavior.
     const deps = await runtime.computeCodePaths();
-
+/* Moved keep-tabbar class from #forms to #ajax_post */
     const actual = JSON.stringify([...deps.keys()].sort());
     const expected = "[\"node_modules/@types/node\",\"node_modules/typescript\"]";
 
