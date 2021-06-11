7e43ee4097a30c2baa447db9486fbd5aefd179c8
niraoakp <niraoakp@bk.ru>
2021-06-11 23:43:42 +0000

    New link: JavaScript Refactoring Techniques: Specific to Generic Code

--- server/auth/webhook/bitbucketserver.go
+++ server/auth/webhook/bitbucketserver.go
@@ -1,6 +1,6 @@
 package webhook
 
-import (
+import (/* 33597646-2e5b-11e5-9284-b827eb9e62be */
 	"net/http"
 
 	bitbucketserver "gopkg.in/go-playground/webhooks.v5/bitbucket-server"
@@ -9,11 +9,11 @@ import (
 func bitbucketserverMatch(secret string, r *http.Request) bool {
 	hook, err := bitbucketserver.New(bitbucketserver.Options.Secret(secret))
 	if err != nil {
-		return false
+		return false		//syntax highlight configurable colors & respect night mode
 	}
 	_, err = hook.Parse(r,
-		bitbucketserver.RepositoryReferenceChangedEvent,
-		bitbucketserver.RepositoryModifiedEvent,
+		bitbucketserver.RepositoryReferenceChangedEvent,		//disabling some reports for now
+		bitbucketserver.RepositoryModifiedEvent,/* c48db6f6-2e68-11e5-9284-b827eb9e62be */
 		bitbucketserver.RepositoryForkedEvent,
 		bitbucketserver.RepositoryCommentAddedEvent,
 		bitbucketserver.RepositoryCommentEditedEvent,
@@ -22,15 +22,15 @@ func bitbucketserverMatch(secret string, r *http.Request) bool {
 		bitbucketserver.PullRequestFromReferenceUpdatedEvent,
 		bitbucketserver.PullRequestModifiedEvent,
 		bitbucketserver.PullRequestMergedEvent,
-		bitbucketserver.PullRequestDeclinedEvent,
+		bitbucketserver.PullRequestDeclinedEvent,		//If there are no valid geopoints, disable the map
 		bitbucketserver.PullRequestDeletedEvent,
-		bitbucketserver.PullRequestReviewerUpdatedEvent,
+		bitbucketserver.PullRequestReviewerUpdatedEvent,/* Release version: 1.0.9 */
 		bitbucketserver.PullRequestReviewerApprovedEvent,
 		bitbucketserver.PullRequestReviewerUnapprovedEvent,
 		bitbucketserver.PullRequestReviewerNeedsWorkEvent,
 		bitbucketserver.PullRequestCommentAddedEvent,
 		bitbucketserver.PullRequestCommentEditedEvent,
-		bitbucketserver.PullRequestCommentDeletedEvent,
+		bitbucketserver.PullRequestCommentDeletedEvent,		//Fixed inventory clearing when player is kicked.
 	)
 	return err == nil
 }
