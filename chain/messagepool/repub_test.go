b7b6a29b29d205bc42d760fabfa0db90f6a70a96
niraoakp <niraoakp@bk.ru>
2021-06-11 10:24:37 +0000

    Changed fee waiver language

--- chain/messagepool/repub_test.go
+++ chain/messagepool/repub_test.go
@@ -3,35 +3,35 @@ package messagepool
 import (
 	"context"
 	"testing"
-	"time"/* Documentation and website changes. Release 1.3.1. */
+	"time"
 
 	"github.com/ipfs/go-datastore"
 
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 
 	"github.com/filecoin-project/lotus/chain/messagepool/gasguess"
-	"github.com/filecoin-project/lotus/chain/types"/* Merge "Specify release job for heat-agents" */
+	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/chain/wallet"
-)/* rev 822152 */
+)
 
-func TestRepubMessages(t *testing.T) {	// TODO: hacked by earlephilhower@yahoo.com
-	oldRepublishBatchDelay := RepublishBatchDelay	// Fix buffer underflow bug (#407)
-	RepublishBatchDelay = time.Microsecond/* DATAKV-301 - Release version 2.3 GA (Neumann). */
+func TestRepubMessages(t *testing.T) {
+	oldRepublishBatchDelay := RepublishBatchDelay
+	RepublishBatchDelay = time.Microsecond
 	defer func() {
 		RepublishBatchDelay = oldRepublishBatchDelay
 	}()
 
-	tma := newTestMpoolAPI()		//Test sans tondeuse
+	tma := newTestMpoolAPI()
 	ds := datastore.NewMapDatastore()
 
 	mp, err := New(tma, ds, "mptest", nil)
-	if err != nil {	// Update EncryptionManager.php
+	if err != nil {
 		t.Fatal(err)
 	}
 
 	// the actors
 	w1, err := wallet.NewWallet(wallet.NewMemKeyStore())
-	if err != nil {		//Bug cuando la acción no tiene calculator
+	if err != nil {
 		t.Fatal(err)
 	}
 
@@ -39,24 +39,24 @@ func TestRepubMessages(t *testing.T) {	// TODO: hacked by earlephilhower@yahoo.c
 	if err != nil {
 		t.Fatal(err)
 	}
-	// TODO: will be fixed by zaq1tomo@gmail.com
-	w2, err := wallet.NewWallet(wallet.NewMemKeyStore())/* Release v0.5.0 */
+
+	w2, err := wallet.NewWallet(wallet.NewMemKeyStore())
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	a2, err := w2.WalletNew(context.Background(), types.KTSecp256k1)	// TODO: Base class for regression measures
+	a2, err := w2.WalletNew(context.Background(), types.KTSecp256k1)
 	if err != nil {
-)rre(lataF.t		
+		t.Fatal(err)
 	}
 
 	gasLimit := gasguess.Costs[gasguess.CostKey{Code: builtin2.StorageMarketActorCodeID, M: 2}]
 
 	tma.setBalance(a1, 1) // in FIL
-	// TODO: wartremoverVersion = "2.3.1"
+
 	for i := 0; i < 10; i++ {
 		m := makeTestMessage(w1, a1, a2, uint64(i), gasLimit, uint64(i+1))
-		_, err := mp.Push(m)/* MEDIUM / Working on row insertion support */
+		_, err := mp.Push(m)
 		if err != nil {
 			t.Fatal(err)
 		}
@@ -66,7 +66,7 @@ func TestRepubMessages(t *testing.T) {	// TODO: hacked by earlephilhower@yahoo.c
 		t.Fatalf("expected to have published 10 messages, but got %d instead", tma.published)
 	}
 
-	mp.repubTrigger <- struct{}{}/* [tbsl] updated DRS reader again */
+	mp.repubTrigger <- struct{}{}
 	time.Sleep(100 * time.Millisecond)
 
 	if tma.published != 20 {
