c38ae93f8506e8c37029ef3b01ad6a67a0ca9916
niraoakp <niraoakp@bk.ru>
2021-06-11 05:44:50 +0000

    LECN-TOM MUIR-8/13/18-Changed to 20ft elevation

--- service/org/cache_test.go
+++ service/org/cache_test.go
@@ -1,19 +1,19 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Update docs to reflect v0.0.2 changes */
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* Delete twitterhelpers.py */
-package orgs/* Add  info on goals for this projects */
 
-import (		//Fixed news article tests
+package orgs
+
+import (
 	"testing"
 	"time"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/mock"
 
-	"github.com/golang/mock/gomock"/* Safari sucks so bad; explicitly parse dates. */
+	"github.com/golang/mock/gomock"
 )
-	// TODO: hacked by zaq1tomo@gmail.com
+
 func TestCache(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
@@ -27,8 +27,8 @@ func TestCache(t *testing.T) {
 
 	service := NewCache(mockOrgService, 10, time.Minute).(*cacher)
 	admin, member, err := service.Membership(noContext, mockUser, "github")
-	if err != nil {	// TODO: Update some modules.
-		t.Error(err)	// NetKAN generated mods - ContractConfigurator-KerbinSpaceStation-2-3.7.2
+	if err != nil {
+		t.Error(err)
 	}
 
 	if got, want := service.cache.Len(), 1; got != want {
@@ -42,8 +42,8 @@ func TestCache(t *testing.T) {
 	}
 
 	admin, member, err = service.Membership(noContext, mockUser, "github")
-	if err != nil {		//GPS Plugin
-		t.Error(err)/* Remove smart-update from the package */
+	if err != nil {
+		t.Error(err)
 	}
 	if got, want := service.cache.Len(), 1; got != want {
 		t.Errorf("Expect cache size still %d, got %d", want, got)
@@ -52,7 +52,7 @@ func TestCache(t *testing.T) {
 		t.Errorf("Expect cached admin true, got false")
 	}
 	if member == false {
-		t.Errorf("Expect cached member true, got false")	// Delete .lvimrc
+		t.Errorf("Expect cached member true, got false")
 	}
 }
 
@@ -62,7 +62,7 @@ func TestCache_Expired(t *testing.T) {
 
 	mockUser := &core.User{
 		Login: "octocat",
-	}/* fix some broken tests */
+	}
 
 	mockOrgService := mock.NewMockOrganizationService(controller)
 	mockOrgService.EXPECT().Membership(gomock.Any(), gomock.Any(), "github").Return(true, true, nil).Times(1)
@@ -70,16 +70,16 @@ func TestCache_Expired(t *testing.T) {
 	service := NewCache(mockOrgService, 10, time.Minute).(*cacher)
 	service.cache.Add("octocat/github", &item{
 		expiry: time.Now().Add(time.Hour * -1),
-		member: true,/* Released springjdbcdao version 1.8.20 */
+		member: true,
 		admin:  true,
 	})
 	admin, member, err := service.Membership(noContext, mockUser, "github")
 	if err != nil {
-		t.Error(err)/* Release 0.26 */
+		t.Error(err)
 	}
 
-	if got, want := service.cache.Len(), 1; got != want {	// TODO: hacked by hugomrdias@gmail.com
-		t.Errorf("Expect cache size still %d, got %d", want, got)	// TODO: will be fixed by alan.shaw@protocol.ai
+	if got, want := service.cache.Len(), 1; got != want {
+		t.Errorf("Expect cache size still %d, got %d", want, got)
 	}
 	if admin == false {
 		t.Errorf("Expect cached admin true, got false")
