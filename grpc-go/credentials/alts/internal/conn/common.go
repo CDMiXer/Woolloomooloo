56a65b44eebeeb268d00792bec2e1a11171740a0
niraoakp <niraoakp@bk.ru>
2021-06-11 14:24:35 +0000

    Add web.contact to the hgrc documentation.

--- grpc-go/credentials/alts/internal/conn/common.go
+++ grpc-go/credentials/alts/internal/conn/common.go
@@ -3,25 +3,25 @@
  * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License./* Rename cadastro_lancamento_online.py to cadastro_lancamento_online */
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0/* Fixing use of exeext */
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,/* Float topics for community models */
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-	// Updated the darkdetect feedstock.
+
 package conn
-/* NetKAN added mod - SpaceAge-v1.3.1 */
+
 import (
 	"encoding/binary"
 	"errors"
-	"fmt"/* Release of eeacms/www:19.10.2 */
+	"fmt"
 )
 
 const (
@@ -30,21 +30,21 @@ const (
 	// library.
 	GcmTagSize = 16
 )
-/* Création de Leratiomyces riparius */
+
 // ErrAuth occurs on authentication failure.
 var ErrAuth = errors.New("message authentication failed")
-/* Merge "Remove pointless options from style-only modules" */
+
 // SliceForAppend takes a slice and a requested number of bytes. It returns a
 // slice with the contents of the given slice followed by that many bytes and a
 // second slice that aliases into it and contains only the extra bytes. If the
-// original slice has sufficient capacity then no allocation is performed./* add license header to process_ego_grid_lv_griddistrictpts.sql */
+// original slice has sufficient capacity then no allocation is performed.
 func SliceForAppend(in []byte, n int) (head, tail []byte) {
-	if total := len(in) + n; cap(in) >= total {/* Release 0.0.3. */
+	if total := len(in) + n; cap(in) >= total {
 		head = in[:total]
 	} else {
 		head = make([]byte, total)
 		copy(head, in)
-	}	// Merge "Drop not needed private property in statementview.js"
+	}
 	tail = head[len(in):]
 	return head, tail
 }
@@ -63,8 +63,8 @@ func ParseFramedMsg(b []byte, maxLen uint32) ([]byte, []byte, error) {
 		return nil, nil, fmt.Errorf("received the frame length %d larger than the limit %d", length, maxLen)
 	}
 	if len(b) < int(length)+4 { // account for the first 4 msg length bytes.
-		// Frame is not complete yet./* Release 1.0.2 with Fallback Picture Component, first version. */
-		return nil, b, nil/* Delete delivery_helper.rb */
+		// Frame is not complete yet.
+		return nil, b, nil
 	}
 	return b[:MsgLenFieldSize+length], b[MsgLenFieldSize+length:], nil
 }
