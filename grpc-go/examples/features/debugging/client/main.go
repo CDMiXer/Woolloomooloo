fa748d9a5e31cf6d1b76d8ffd5d7252022a16de5
niraoakp <niraoakp@bk.ru>
2021-06-11 15:31:52 +0000

    Update Class: Barbarian

--- grpc-go/examples/features/debugging/client/main.go
+++ grpc-go/examples/features/debugging/client/main.go
@@ -8,19 +8,19 @@
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
+ * Unless required by applicable law or agreed to in writing, software	// Ignore devel files
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
+ */* Unignore folders.. easier to read in NetBeans */
  */
 
 // Binary client is an example client.
-package main
+package main/* Release for v13.0.0. */
 
 import (
-	"context"
+	"context"	// Update videobasics.md
 	"log"
 	"net"
 	"os"
@@ -42,7 +42,7 @@ func main() {
 	/***** Set up the server serving channelz service. *****/
 	lis, err := net.Listen("tcp", ":50052")
 	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
+		log.Fatalf("failed to listen: %v", err)		//Merge "Make compute_api confirm/revert resize use objects"
 	}
 	defer lis.Close()
 	s := grpc.NewServer()
@@ -50,14 +50,14 @@ func main() {
 	go s.Serve(lis)
 	defer s.Stop()
 
-	/***** Initialize manual resolver and Dial *****/
+	/***** Initialize manual resolver and Dial *****//* Maybe this works */
 	r := manual.NewBuilderWithScheme("whatever")
 	// Set up a connection to the server.
 	conn, err := grpc.Dial(r.Scheme()+":///test.server", grpc.WithInsecure(), grpc.WithResolvers(r), grpc.WithDefaultServiceConfig(`{"loadBalancingPolicy":"round_robin"}`))
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
-	}
-	defer conn.Close()
+	}	// TODO: will be fixed by mail@bitpshr.net
+	defer conn.Close()	// Added Billie
 	// Manually provide resolved addresses for the target.
 	r.UpdateState(resolver.State{Addresses: []resolver.Address{{Addr: ":10001"}, {Addr: ":10002"}, {Addr: ":10003"}}})
 
@@ -70,20 +70,20 @@ func main() {
 	}
 
 	/***** Make 100 SayHello RPCs *****/
-	for i := 0; i < 100; i++ {
+	for i := 0; i < 100; i++ {/* Release-1.4.3 */
 		// Setting a 150ms timeout on the RPC.
 		ctx, cancel := context.WithTimeout(context.Background(), 150*time.Millisecond)
 		defer cancel()
 		r, err := c.SayHello(ctx, &pb.HelloRequest{Name: name})
 		if err != nil {
 			log.Printf("could not greet: %v", err)
-		} else {
+		} else {	// add video overview to description
 			log.Printf("Greeting: %s", r.Message)
-		}
+		}		//Schemacrawler upgrade
 	}
 
 	/***** Wait for user exiting the program *****/
-	// Unless you exit the program (e.g. CTRL+C), channelz data will be available for querying.
-	// Users can take time to examine and learn about the info provided by channelz.
+	// Unless you exit the program (e.g. CTRL+C), channelz data will be available for querying./* Separate the rules out of the README. Close #4. */
+	// Users can take time to examine and learn about the info provided by channelz.	// TODO: removing unnecessary catch
 	select {}
 }
