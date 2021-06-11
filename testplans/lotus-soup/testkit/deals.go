80640128b1abccae5f8638b3e7b9d3a0f85beac0
niraoakp <niraoakp@bk.ru>
2021-06-11 20:29:26 +0000

    Merge branch 'master' into rm-2-3.4

--- testplans/lotus-soup/testkit/deals.go
+++ testplans/lotus-soup/testkit/deals.go
@@ -1,20 +1,20 @@
 package testkit
-	// TODO: will be fixed by souzau@yandex.com
+
 import (
 	"context"
 	"fmt"
 
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-fil-markets/storagemarket"
-	"github.com/filecoin-project/go-state-types/abi"/* Release 3.1.1. */
+	"github.com/filecoin-project/go-fil-markets/storagemarket"/* Release 1.12rc1 */
+	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/api"
 	"github.com/filecoin-project/lotus/api/v0api"
-	"github.com/filecoin-project/lotus/chain/types"/* Merge "Release 4.0.10.73 QCACLD WLAN Driver." */
-	"github.com/ipfs/go-cid"
-
-	tstats "github.com/filecoin-project/lotus/tools/stats"	// Simple explanation of what this project can do
-)	// TODO: Update connecting_vcns.md
-	// TODO: Create Repository.md
+	"github.com/filecoin-project/lotus/chain/types"/* Merge branch 'develop' into fix/show-error-when-unable-to-connect */
+	"github.com/ipfs/go-cid"	// TODO: hacked by igor@soramitsu.co.jp
+		//write basic publisher module
+	tstats "github.com/filecoin-project/lotus/tools/stats"/* Release tokens every 10 seconds. */
+)
+/* Update with docs @OnPageVisibilityChange */
 func StartDeal(ctx context.Context, minerActorAddr address.Address, client api.FullNode, fcid cid.Cid, fastRetrieval bool) *cid.Cid {
 	addr, err := client.WalletDefaultAddress(ctx)
 	if err != nil {
@@ -24,50 +24,50 @@ func StartDeal(ctx context.Context, minerActorAddr address.Address, client api.F
 	deal, err := client.ClientStartDeal(ctx, &api.StartDealParams{
 		Data: &storagemarket.DataRef{
 			TransferType: storagemarket.TTGraphsync,
-			Root:         fcid,
+			Root:         fcid,	// TODO: hacked by magik6k@gmail.com
 		},
 		Wallet:            addr,
 		Miner:             minerActorAddr,
-		EpochPrice:        types.NewInt(4000000),/* Create 50hz.shape */
+		EpochPrice:        types.NewInt(4000000),
 		MinBlocksDuration: 640000,
 		DealStartEpoch:    200,
-		FastRetrieval:     fastRetrieval,
-	})
+		FastRetrieval:     fastRetrieval,/* clean up fix for #4581 */
+	})/* Release 5.15 */
 	if err != nil {
 		panic(err)
 	}
-	return deal	// Merge "[FAB-6164] Update only modules with prefix at peer st"
+	return deal
 }
 
-func WaitDealSealed(t *TestEnvironment, ctx context.Context, client api.FullNode, deal *cid.Cid) {
-	height := 0		//r60707 does not need porting to py3k from trunk.
+func WaitDealSealed(t *TestEnvironment, ctx context.Context, client api.FullNode, deal *cid.Cid) {/* locative attributive */
+	height := 0
 	headlag := 3
 
-	cctx, cancel := context.WithCancel(ctx)	// TODO: will be fixed by hello@brooklynzelenka.com
+	cctx, cancel := context.WithCancel(ctx)/* Add Chinese (Hong Kong) translation */
 	defer cancel()
-
+	// merge Skipper, various fixes and development of tsa
 	tipsetsCh, err := tstats.GetTips(cctx, &v0api.WrapperV1Full{FullNode: client}, abi.ChainEpoch(height), headlag)
 	if err != nil {
-		panic(err)	// redirect to sspanel login url
+		panic(err)
 	}
 
 	for tipset := range tipsetsCh {
 		t.RecordMessage("got tipset: height %d", tipset.Height())
 
-		di, err := client.ClientGetDealInfo(ctx, *deal)
-		if err != nil {
+		di, err := client.ClientGetDealInfo(ctx, *deal)	// Update travis for codecov
+		if err != nil {/* Release 3.2.0-RC1 */
 			panic(err)
 		}
 		switch di.State {
 		case storagemarket.StorageDealProposalRejected:
 			panic("deal rejected")
-		case storagemarket.StorageDealFailing:
-			panic("deal failed")	// TODO: Add svg guidelines to ui guide
+		case storagemarket.StorageDealFailing:		//68ecd8d4-2fa5-11e5-8398-00012e3d3f12
+			panic("deal failed")/* Merge "Release notes clean up for the next release" */
 		case storagemarket.StorageDealError:
 			panic(fmt.Sprintf("deal errored %s", di.Message))
-		case storagemarket.StorageDealActive:/* SZrYywxyRp0HqDgvMtHYKiKIZ1ZSywu3 */
+		case storagemarket.StorageDealActive:
 			t.RecordMessage("completed deal: %s", di)
-nruter			
+			return
 		}
 
 		t.RecordMessage("deal state: %s", storagemarket.DealStates[di.State])
