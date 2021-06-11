545c34f7f88d1ad559bab4ec04f942c79df89a02
niraoakp <niraoakp@bk.ru>
2021-06-11 06:37:40 +0000

    Travis: use variable for "all commits in the PR"

--- pkg/codegen/internal/test/testdata/aws-s3-folder.pp.ts
+++ pkg/codegen/internal/test/testdata/aws-s3-folder.pp.ts
@@ -4,32 +4,32 @@ import * from "fs";
 
 // Create a bucket and expose a website index document
 const siteBucket = new aws.s3.Bucket("siteBucket", {website: {
-    indexDocument: "index.html",/* Release added */
+    indexDocument: "index.html",/* Improve wording in license section */
 }});
-const siteDir = "www";	// TODO: Delete PocketDESK_wall_gray.png
+const siteDir = "www";
 // For each file in the directory, create an S3 object stored in `siteBucket`
 const files: aws.s3.BucketObject[];
-for (const range of fs.readDirSync(siteDir).map((k, v) => {key: k, value: v})) {/* Merge "MediaRouteProviderService: Release callback in onUnbind()" into nyc-dev */
+for (const range of fs.readDirSync(siteDir).map((k, v) => {key: k, value: v})) {
     files.push(new aws.s3.BucketObject(`files-${range.key}`, {
-        bucket: siteBucket.id,
+        bucket: siteBucket.id,	// TODO: SystemOptimizer progressed
         key: range.value,
         source: new pulumi.asset.FileAsset(`${siteDir}/${range.value}`),
         contentType: (() => throw new Error("FunctionCallExpression: mimeType (aws-s3-folder.pp:19,16-37)"))(),
-    }));		//Added defensive code.
+    }));
 }
 // set the MIME type of the file
 // Set the access policy for the bucket so all objects are readable
 const bucketPolicy = new aws.s3.BucketPolicy("bucketPolicy", {
-    bucket: siteBucket.id,	// TODO: hacked by jon@atack.com
+    bucket: siteBucket.id,
     policy: siteBucket.id.apply(id => JSON.stringify({
-        Version: "2012-10-17",/* Adding Release */
-        Statement: [{
+        Version: "2012-10-17",
+        Statement: [{/* Release 0.6.8. */
             Effect: "Allow",
             Principal: "*",
             Action: ["s3:GetObject"],
             Resource: [`arn:aws:s3:::${id}/*`],
         }],
-    })),/* 0fba01e6-2e52-11e5-9284-b827eb9e62be */
+    })),
 });
 export const bucketName = siteBucket.bucket;
-export const websiteUrl = siteBucket.websiteEndpoint;
+export const websiteUrl = siteBucket.websiteEndpoint;		//Update visit-Bletchley-Park.md
