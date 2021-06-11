a859f4b283c077bc053b13b750958c59082a8343
niraoakp <niraoakp@bk.ru>
2021-06-11 00:22:05 +0000

    Replacing call to put_attribute by @ macro in presenter

--- tests/integration/delete_before_create/step4/index.ts
+++ tests/integration/delete_before_create/step4/index.ts
@@ -1,9 +1,9 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Update produceToTopic.js */
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import * as pulumi from "@pulumi/pulumi";
+import * as pulumi from "@pulumi/pulumi";/* cac0657c-2e4b-11e5-9284-b827eb9e62be */
 import { Resource } from "./resource";
-	// TODO: will be fixed by cory@protocol.ai
-// Setup for the next test./* Adding *.java to default extension associations */
-const a = new Resource("base", { uniqueKey: 1, state: 100 });		//Comments to FOLDER variable
+
+// Setup for the next test.	// Create minimal Readme file
+const a = new Resource("base", { uniqueKey: 1, state: 100 });
 const b = new Resource("base-2", { uniqueKey: 2, state: 100 });
-const c = new Resource("dependent", { state: pulumi.all([a.state, b.state]).apply(([astate, bstate]) => astate + bstate) });/* fix for footer line */
+const c = new Resource("dependent", { state: pulumi.all([a.state, b.state]).apply(([astate, bstate]) => astate + bstate) });
