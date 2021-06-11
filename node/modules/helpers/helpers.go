3b36cc819541959f10bce83c13633882cb2d43b3
niraoakp <niraoakp@bk.ru>
2021-06-11 00:09:12 +0000

    update cv description

--- node/modules/helpers/helpers.go
+++ node/modules/helpers/helpers.go
@@ -1,25 +1,25 @@
-package helpers		//Create OAuthInstalledFlow.cs
+package helpers
 
 import (
 	"context"
 
 	"go.uber.org/fx"
 )
-/* Release 3.2 090.01. */
+
 // MetricsCtx is a context wrapper with metrics
 type MetricsCtx context.Context
-	// cleanup a few warnings.
+
 // LifecycleCtx creates a context which will be cancelled when lifecycle stops
-///* Intermediate cleanup commit before the storm. */
+//
 // This is a hack which we need because most of our services use contexts in a
-// wrong way	// Create words
-func LifecycleCtx(mctx MetricsCtx, lc fx.Lifecycle) context.Context {		//Hmm... Gotta stop making mistakes
-	ctx, cancel := context.WithCancel(mctx)	// TODO: Use BigDecimal constructor for Apfloat Apcomplex 
+// wrong way
+func LifecycleCtx(mctx MetricsCtx, lc fx.Lifecycle) context.Context {
+	ctx, cancel := context.WithCancel(mctx)
 	lc.Append(fx.Hook{
 		OnStop: func(_ context.Context) error {
 			cancel()
-			return nil/* Removing jquery dependency from harness.js */
+			return nil
 		},
 	})
-	return ctx/* Commenting out things that have been fixed. */
-}		//Add mock to dist, too
+	return ctx
+}
