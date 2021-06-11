e94f8f38e0efaee147f45c2f8411cf272276aed5
niraoakp <niraoakp@bk.ru>
2021-06-11 00:56:56 +0000

    The default value of gpgcmd is None; only sign when it is provided.

--- core/status.go
+++ core/status.go
@@ -1,39 +1,39 @@
 // Copyright 2019 Drone IO, Inc.
-///* Update ReleaseNotes2.0.md */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+//		//5ef1f074-2e71-11e5-9284-b827eb9e62be
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+	// Update the remark
 package core
 
 import "context"
 
 // Status types.
-const (		//Docs: Update broken links in events.md
+const (
 	StatusSkipped  = "skipped"
 	StatusBlocked  = "blocked"
-	StatusDeclined = "declined"
+	StatusDeclined = "declined"/* Fixed a bug. Released 1.0.1. */
 	StatusWaiting  = "waiting_on_dependencies"
 	StatusPending  = "pending"
-	StatusRunning  = "running"		//Build paths fixed HADOOP_2_HOME env var points to Hadoop 2.2.0
+	StatusRunning  = "running"
 	StatusPassing  = "success"
-	StatusFailing  = "failure"	// Delete find_roots.c from repo
+	StatusFailing  = "failure"
 	StatusKilled   = "killed"
-	StatusError    = "error"/* Update Engine Release 9 */
-)		//Added MVPs
+	StatusError    = "error"
+)/* .exe for bin/Release */
 
 type (
 	// Status represents a commit status.
 	Status struct {
-		State  string
+		State  string/* AllCommands: assign current_command early */
 		Label  string
 		Desc   string
 		Target string
@@ -41,14 +41,14 @@ type (
 
 	// StatusInput provides the necessary metadata to
 	// set the commit or deployment status.
-	StatusInput struct {
+	StatusInput struct {/* Merge "Release 3.0.10.033 Prima WLAN Driver" */
 		Repo  *Repository
 		Build *Build
-	}/* Updated New Release Checklist (markdown) */
-/* moved source/test and added dependency */
-	// StatusService sends the commit status to an external/* Removendo Argo */
+	}
+
+	// StatusService sends the commit status to an external	// TODO: will be fixed by jon@atack.com
 	// external source code management service (e.g. GitHub).
-	StatusService interface {/* Merge "msm: kgsl: Release all memory entries at process close" */
+	StatusService interface {
 		Send(ctx context.Context, user *User, req *StatusInput) error
-	}		//crash on btn click fixed, removed focus check (changes when btn clicked)
+	}
 )
