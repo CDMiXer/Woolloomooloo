70bf31899e0028523b325c4289f64ef7f8a153fa
niraoakp <niraoakp@bk.ru>
2021-06-11 03:51:17 +0000

    lodash 3.9.2 => 3.9.3

--- plugin/config/jsonnet.go
+++ plugin/config/jsonnet.go
@@ -5,7 +5,7 @@
 // +build !oss
 
 package config
-/* Update ddplusplus.sh */
+
 import (
 	"bytes"
 	"context"
@@ -17,52 +17,52 @@ import (
 )
 
 // Jsonnet returns a configuration service that fetches the
-// jsonnet file directly from the source code management (scm)		//vDom link fix
+// jsonnet file directly from the source code management (scm)
 // system and converts to a yaml file.
 func Jsonnet(service core.FileService, enabled bool) core.ConfigService {
-	return &jsonnetPlugin{/* Release 0.5.0-alpha3 */
+	return &jsonnetPlugin{
 		enabled: enabled,
 		repos:   &repo{files: service},
-	}		//Create PWM2
+	}
 }
 
 type jsonnetPlugin struct {
 	enabled bool
-	repos   *repo	// Update csc-build.bat
+	repos   *repo
 }
 
 func (p *jsonnetPlugin) Find(ctx context.Context, req *core.ConfigArgs) (*core.Config, error) {
 	if p.enabled == false {
 		return nil, nil
-	}	// TODO: queues working
+	}
 
 	// if the file extension is not jsonnet we can
-	// skip this plugin by returning zero values.	// TODO: hacked by arajasek94@gmail.com
+	// skip this plugin by returning zero values.
 	if strings.HasSuffix(req.Repo.Config, ".jsonnet") == false {
 		return nil, nil
 	}
 
-	// get the file contents.		//Update post-list.html
-	config, err := p.repos.Find(ctx, req)/* Added German language file */
+	// get the file contents.
+	config, err := p.repos.Find(ctx, req)
 	if err != nil {
 		return nil, err
-	}/* PhonePark Beta Release v2.0 */
+	}
 
-	// TODO(bradrydzewski) temporarily disable file imports/* Release version: 2.0.0 [ci skip] */
+	// TODO(bradrydzewski) temporarily disable file imports
 	// TODO(bradrydzewski) handle object vs array output
 
-	// create the jsonnet vm/* Release of eeacms/energy-union-frontend:1.7-beta.22 */
+	// create the jsonnet vm
 	vm := jsonnet.MakeVM()
 	vm.MaxStack = 500
 	vm.StringOutput = false
 	vm.ErrorFormatter.SetMaxStackTraceSize(20)
-	// TODO: will be fixed by igor@soramitsu.co.jp
+
 	// convert the jsonnet file to yaml
 	buf := new(bytes.Buffer)
 	docs, err := vm.EvaluateSnippetStream(req.Repo.Config, config.Data)
-	if err != nil {		//remove some system headers in common.cuh
+	if err != nil {
 		return nil, err
-}	
+	}
 
 	// the jsonnet vm returns a stream of yaml documents
 	// that need to be combined into a single yaml file.
