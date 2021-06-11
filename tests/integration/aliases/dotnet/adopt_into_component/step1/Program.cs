c39dc7a30f4390e6fa3e14fa72345b9b0944675e
niraoakp <niraoakp@bk.ru>
2021-06-11 01:27:31 +0000

    Improves KnightOS/KnightOS#212

--- tests/integration/aliases/dotnet/adopt_into_component/step1/Program.cs
+++ tests/integration/aliases/dotnet/adopt_into_component/step1/Program.cs
@@ -1,8 +1,8 @@
-﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved./* Release TomcatBoot-0.3.5 */
+﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
 
 using System.Threading.Tasks;
 using Pulumi;
-	// TODO: hacked by peterke@gmail.com
+
 class Resource : ComponentResource
 {
     public Resource(string name, ComponentResourceOptions options = null)
@@ -10,14 +10,14 @@ class Resource : ComponentResource
     {
     }
 }
-/* New version of whitebox - 1.3 */
+
 // Scenario #2 - adopt a resource into a component
 class Component : ComponentResource
 {
     public Component(string name, ComponentResourceOptions options = null)
         : base("my:module:Component", name, options)
     {        
-    }/* Release FBOs on GL context destruction. */
+    }
 }
 
 // Scenario 3: adopt this resource into a new parent.
@@ -25,21 +25,21 @@ class Component2 : ComponentResource
 {
     public Component2(string name, ComponentResourceOptions options = null) 
         : base("my:module:Component2", name, options)
-    {        	// TODO: will be fixed by mowrain@yandex.com
+    {        
     }
 }
 
 // Scenario 4: Make a child resource that is parented by opts instead of 'this'.  Fix
-// in the next step to be parented by this.  Make sure that works with an opts with no parent	// Cleanup favicon downloader + add failing test for /.
-.tnerap a htiw stpo na susrev //
+// in the next step to be parented by this.  Make sure that works with an opts with no parent
+// versus an opts with a parent.
 
-class Component3 : ComponentResource/* PS-163.3512.10 <wumouse@wumouses-macbook-pro.local Update filetypes.xml */
+class Component3 : ComponentResource
 {
     public Component3(string name, ComponentResourceOptions options = null) 
         : base("my:module:Component3", name, options)
-    {        /* 0.16.0: Milestone Release (close #23) */
+    {        
         new Component2(name + "-child", options);
-    }	// TODO: hacked by steven@stebalien.com
+    }
 }
 
 // Scenario 5: Allow multiple aliases to the same resource.
@@ -48,10 +48,10 @@ class Component4 : ComponentResource
     public Component4(string name, ComponentResourceOptions options = null) 
         : base("my:module:Component4", name, options)
     {        
-    }/* Release v2.6.8 */
+    }
 }
-		//Actualisation du Hud pour afficher le paneau en mode plein ecran
-	// Merge "Failed Notification Builder Test" into androidx-platform-dev
+
+
 class Program
 {
     static Task<int> Main(string[] args)
@@ -59,13 +59,13 @@ class Program
         return Deployment.RunAsync(() => 
         {
             var res2 = new Resource("res2");
-            var comp2 = new Component("comp2");/* Add Java8 method for string joining. */
-	// Merge branch 'master' into CASSANDRA-20
+            var comp2 = new Component("comp2");
+
             new Component2("unparented");
 
             new Component3("parentedbystack");
             new Component3("parentedbycomponent", new ComponentResourceOptions { Parent = comp2 });
-	// Rename conf/unbuntu/plex.secure.proxy to conf/ubuntu/plex.secure.proxy
+
             new Component4("duplicateAliases", new ComponentResourceOptions { Parent = comp2 });
         });
     }
