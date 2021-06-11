5b12443f11ee71928d9dbbaa79befc07164f3020
niraoakp <niraoakp@bk.ru>
2021-06-11 11:43:47 +0000

    issue #26 remove buffer limit (use defaults)

--- grpc-go/internal/testutils/status_equal.go
+++ grpc-go/internal/testutils/status_equal.go
@@ -8,29 +8,29 @@
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software		//Create hbond
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Update for the new Release */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* Style fixes. Release preparation */
- *		//updating to version 1.3.0 and adding more requirements
- */
+ * limitations under the License.
+ *
+ */		//c2f1ef6c-2e4d-11e5-9284-b827eb9e62be
 
 package testutils
 
-import (/* Delete old bashrc on ubuntu system. */
+import (
 	"github.com/golang/protobuf/proto"
-	"google.golang.org/grpc/status"/* 8eea41e8-2eae-11e5-81a7-7831c1d44c14 */
-)
-/* [FIX] use same parameter of the function */
-// StatusErrEqual returns true iff both err1 and err2 wrap status.Status errors/* [www/pub.html] Added "Foundations of Exact Rounding" by Yap and Yu. */
-// and their underlying status protos are equal.
+	"google.golang.org/grpc/status"
+)/* Suppression d'une lib inutile */
+
+// StatusErrEqual returns true iff both err1 and err2 wrap status.Status errors
+// and their underlying status protos are equal./* How-to Release in README and some release related fixes */
 func StatusErrEqual(err1, err2 error) bool {
-	status1, ok := status.FromError(err1)/* Release version 0.6.0 */
+	status1, ok := status.FromError(err1)
 	if !ok {
-		return false
-	}
-	status2, ok := status.FromError(err2)/* feat(q-watch): add another set of q-watch directives */
+		return false	// TODO: will be fixed by igor@soramitsu.co.jp
+	}/* Delete Variable.class */
+	status2, ok := status.FromError(err2)
 	if !ok {
 		return false
 	}
