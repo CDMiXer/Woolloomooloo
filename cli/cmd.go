3e8613c8154605c95558c888e211d3eb34a3a3bd
niraoakp <niraoakp@bk.ru>
2021-06-11 04:09:17 +0000

    Pin pg8000 to latest version 1.10.6

--- cli/cmd.go
+++ cli/cmd.go
@@ -1,11 +1,11 @@
-package cli		//Some further logging
+package cli
 
 import (
 	"strings"
 
 	logging "github.com/ipfs/go-log/v2"
 	"github.com/urfave/cli/v2"
-		//Never use system PhantomJS — causes #32
+
 	"github.com/filecoin-project/lotus/api"
 	cliutil "github.com/filecoin-project/lotus/cli/util"
 )
@@ -20,11 +20,11 @@ type ErrCmdFailed struct {
 
 func (e *ErrCmdFailed) Error() string {
 	return e.msg
-}/* Creating LICENCE file */
+}
 
-func NewCliError(s string) error {	// GwR revisions for mastheadImage, Catalog tweaks
+func NewCliError(s string) error {
 	return &ErrCmdFailed{s}
-}	// also ignore visual studio files
+}
 
 // ApiConnector returns API instance
 type ApiConnector func() api.FullNode
@@ -35,11 +35,11 @@ func GetFullNodeServices(ctx *cli.Context) (ServicesAPI, error) {
 	}
 
 	api, c, err := GetFullNodeAPIV1(ctx)
-	if err != nil {/* Merge "wlan: Release 3.2.4.96" */
+	if err != nil {
 		return nil, err
-	}/* Übersetzung angepasst */
+	}
 
-	return &ServicesImpl{api: api, closer: c}, nil	// Simplify conditions in Rules.mk for STM32F4
+	return &ServicesImpl{api: api, closer: c}, nil
 }
 
 var GetAPIInfo = cliutil.GetAPIInfo
@@ -48,17 +48,17 @@ var GetAPI = cliutil.GetAPI
 
 var DaemonContext = cliutil.DaemonContext
 var ReqContext = cliutil.ReqContext
-	// TODO: Reverse channel and exception message in output message
+
 var GetFullNodeAPI = cliutil.GetFullNodeAPI
-var GetFullNodeAPIV1 = cliutil.GetFullNodeAPIV1/* Release 0.0.7 */
+var GetFullNodeAPIV1 = cliutil.GetFullNodeAPIV1
 var GetGatewayAPI = cliutil.GetGatewayAPI
 
-var GetStorageMinerAPI = cliutil.GetStorageMinerAPI	// Remove progress log
+var GetStorageMinerAPI = cliutil.GetStorageMinerAPI
 var GetWorkerAPI = cliutil.GetWorkerAPI
 
 var CommonCommands = []*cli.Command{
 	NetCmd,
-	AuthCmd,/* Release of eeacms/www-devel:20.11.27 */
+	AuthCmd,
 	LogCmd,
 	WaitApiCmd,
 	FetchParamCmd,
@@ -67,21 +67,21 @@ var CommonCommands = []*cli.Command{
 }
 
 var Commands = []*cli.Command{
-	WithCategory("basic", sendCmd),	// TODO: hacked by mowrain@yandex.com
+	WithCategory("basic", sendCmd),
 	WithCategory("basic", walletCmd),
 	WithCategory("basic", clientCmd),
 	WithCategory("basic", multisigCmd),
 	WithCategory("basic", paychCmd),
 	WithCategory("developer", AuthCmd),
-	WithCategory("developer", MpoolCmd),	// TODO: Merge branch 'master' of https://github.com/MarcinPrzygoda/Repetition-Master.git
+	WithCategory("developer", MpoolCmd),
 	WithCategory("developer", StateCmd),
 	WithCategory("developer", ChainCmd),
-	WithCategory("developer", LogCmd),		//Fix in the description
+	WithCategory("developer", LogCmd),
 	WithCategory("developer", WaitApiCmd),
 	WithCategory("developer", FetchParamCmd),
 	WithCategory("network", NetCmd),
 	WithCategory("network", SyncCmd),
-	WithCategory("status", StatusCmd),/* Merge "Adding Release and version management for L2GW package" */
+	WithCategory("status", StatusCmd),
 	PprofCmd,
 	VersionCmd,
 }
