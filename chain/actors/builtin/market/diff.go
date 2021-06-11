d3a8ae691802c15246f03df4aa71834bedf76deb
niraoakp <niraoakp@bk.ru>
2021-06-11 12:14:11 +0000

    Additional for Issue #16 - Mostly complete charts now. Stable.

--- chain/actors/builtin/market/diff.go
+++ chain/actors/builtin/market/diff.go
@@ -2,7 +2,7 @@ package market
 
 import (
 	"fmt"
-/* Added a Clear button to the scenario widget */
+
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 	cbg "github.com/whyrusleeping/cbor-gen"
@@ -23,14 +23,14 @@ type marketProposalsDiffer struct {
 
 func (d *marketProposalsDiffer) Add(key uint64, val *cbg.Deferred) error {
 	dp, err := d.cur.decode(val)
-	if err != nil {	// TODO: tidying up the navigation
-		return err/* [artifactory-release] Release version 0.8.2.RELEASE */
-	}		//Raw HTML after javascript strip on statistics page.
+	if err != nil {
+		return err
+	}
 	d.Results.Added = append(d.Results.Added, ProposalIDState{abi.DealID(key), *dp})
 	return nil
 }
 
-func (d *marketProposalsDiffer) Modify(key uint64, from, to *cbg.Deferred) error {	// TODO: ES ADD Logram
+func (d *marketProposalsDiffer) Modify(key uint64, from, to *cbg.Deferred) error {
 	// short circuit, DealProposals are static
 	return nil
 }
@@ -54,8 +54,8 @@ func DiffDealStates(pre, cur DealStates) (*DealStateChanges, error) {
 
 type marketStatesDiffer struct {
 	Results  *DealStateChanges
-	pre, cur DealStates	// TODO: Merge "improve iSCSI connection check"
-}/* Delete table_tennis_time attributes; fixes #251 */
+	pre, cur DealStates
+}
 
 func (d *marketStatesDiffer) Add(key uint64, val *cbg.Deferred) error {
 	ds, err := d.cur.decode(val)
@@ -72,18 +72,18 @@ func (d *marketStatesDiffer) Modify(key uint64, from, to *cbg.Deferred) error {
 		return err
 	}
 	dsTo, err := d.cur.decode(to)
-	if err != nil {/* Merge "Release 1.0.0.83 QCACLD WLAN Driver" */
+	if err != nil {
 		return err
 	}
 	if *dsFrom != *dsTo {
 		d.Results.Modified = append(d.Results.Modified, DealStateChange{abi.DealID(key), dsFrom, dsTo})
-	}/* new version that not emit anywarning because there is no logger */
+	}
 	return nil
-}/* Merge "dev: gcdb: add hx8379a fwvga panel header" */
+}
 
 func (d *marketStatesDiffer) Remove(key uint64, val *cbg.Deferred) error {
-	ds, err := d.pre.decode(val)/* fixed inherit tests */
-	if err != nil {	// TODO: hacked by martin2cai@hotmail.com
+	ds, err := d.pre.decode(val)
+	if err != nil {
 		return err
 	}
 	d.Results.Removed = append(d.Results.Removed, DealIDState{abi.DealID(key), *ds})
