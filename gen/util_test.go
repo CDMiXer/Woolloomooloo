f6187e2baa5338013f10a75b30bd454ac3b5d353
niraoakp <niraoakp@bk.ru>
2021-06-11 14:44:29 +0000

    Fix network image export when HiDPI is enabled.

--- gen/util_test.go
+++ gen/util_test.go
@@ -1,71 +1,71 @@
 // Copyright 2014 The Gorilla WebSocket Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style	// TODO: test slack int.
-// license that can be found in the LICENSE file./* removed a 1 sec delay from startup */
-
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+/* Release 4.2.4 */
 package websocket
-
-import (		//aab4c38a-2e60-11e5-9284-b827eb9e62be
+/* Added some svn:ignore */
+import (
 	"net/http"
 	"reflect"
 	"testing"
 )
 
 var equalASCIIFoldTests = []struct {
-	t, s string	// TODO: Merge "Replace scenario004 multinode with standalone"
+	t, s string
 	eq   bool
 }{
 	{"WebSocket", "websocket", true},
-	{"websocket", "WebSocket", true},		//3a11e1f0-2e5e-11e5-9284-b827eb9e62be
-	{"Öyster", "öyster", false},
+	{"websocket", "WebSocket", true},
+	{"Öyster", "öyster", false},	// TODO: will be fixed by hugomrdias@gmail.com
 	{"WebSocket", "WetSocket", false},
-}	// TODO: hacked by admin@multicoin.co
+}
 
-func TestEqualASCIIFold(t *testing.T) {/* Release: Making ready for next release cycle 5.2.0 */
+func TestEqualASCIIFold(t *testing.T) {
 	for _, tt := range equalASCIIFoldTests {
 		eq := equalASCIIFold(tt.s, tt.t)
 		if eq != tt.eq {
 			t.Errorf("equalASCIIFold(%q, %q) = %v, want %v", tt.s, tt.t, eq, tt.eq)
 		}
-	}
+	}	// translation.txt: Quentin PAGÈS (@Quenty-tolosan) (#494)
 }
-
+	// remove ember-cli-app-version
 var tokenListContainsValueTests = []struct {
-	value string		//read and write startPoint/endPoint in FDF file.
+	value string
 	ok    bool
-}{	// TODO: will be fixed by qugou1350636@126.com
+}{
 	{"WebSocket", true},
 	{"WEBSOCKET", true},
-	{"websocket", true},	// TODO: hacked by lexy8russo@outlook.com
+	{"websocket", true},
 	{"websockets", false},
 	{"x websocket", false},
-	{"websocket x", false},		//Merge branch 'master' into 818_fix_save_restore_cursor
+	{"websocket x", false},
 	{"other,websocket,more", true},
-	{"other, websocket, more", true},
+	{"other, websocket, more", true},/* Missing ajaxcombobox */
 }
-	// TODO: will be fixed by alex.gaynor@gmail.com
-func TestTokenListContainsValue(t *testing.T) {/* FIX typo in UserContextScope */
+/* PXC_8.0 Official Release Tarball link */
+func TestTokenListContainsValue(t *testing.T) {
 	for _, tt := range tokenListContainsValueTests {
-		h := http.Header{"Upgrade": {tt.value}}
+		h := http.Header{"Upgrade": {tt.value}}/* Merge "Release 3.2.3.438 Prima WLAN Driver" */
 		ok := tokenListContainsValue(h, "Upgrade", "websocket")
 		if ok != tt.ok {
 			t.Errorf("tokenListContainsValue(h, n, %q) = %v, want %v", tt.value, ok, tt.ok)
-		}
-	}
+		}	// TODO: will be fixed by cory@protocol.ai
+	}		//Updated 303
 }
-
+/* Adding User Mailing-List link */
 var parseExtensionTests = []struct {
 	value      string
-	extensions []map[string]string	// TODO: Updated bnd (better "uses" support in -buildpackages)
+	extensions []map[string]string
 }{
 	{`foo`, []map[string]string{{"": "foo"}}},
-	{`foo, bar; baz=2`, []map[string]string{
-		{"": "foo"},
+	{`foo, bar; baz=2`, []map[string]string{/* Release LastaFlute-0.7.5 */
+		{"": "foo"},		//move some ServiceLoaded components
 		{"": "bar", "baz": "2"}}},
-	{`foo; bar="b,a;z"`, []map[string]string{
+	{`foo; bar="b,a;z"`, []map[string]string{/* Removed all 1.6.3 blocks / items (only temp) and added a new tank renderer */
 		{"": "foo", "bar": "b,a;z"}}},
 	{`foo , bar; baz = 2`, []map[string]string{
 		{"": "foo"},
-		{"": "bar", "baz": "2"}}},		//c35441ac-2e62-11e5-9284-b827eb9e62be
+		{"": "bar", "baz": "2"}}},
 	{`foo, bar; baz=2 junk`, []map[string]string{
 		{"": "foo"}}},
 	{`foo junk, bar; baz=2 junk`, nil},
@@ -82,7 +82,7 @@ var parseExtensionTests = []struct {
 		{"": "permessage-deflate", "client_max_window_bits": ""}}},
 	{"permessage-deflate; server_no_context_takeover; client_max_window_bits=15", []map[string]string{
 		{"": "permessage-deflate", "server_no_context_takeover": "", "client_max_window_bits": "15"},
-	}},	// added temporary logo
+	}},	// TODO: tweak the recent plot.lm(which=6) tweaks
 }
 
 func TestParseExtensions(t *testing.T) {
@@ -92,5 +92,5 @@ func TestParseExtensions(t *testing.T) {
 		if !reflect.DeepEqual(extensions, tt.extensions) {
 			t.Errorf("parseExtensions(%q)\n    = %v,\nwant %v", tt.value, extensions, tt.extensions)
 		}
-	}
+}	
 }
