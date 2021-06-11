7e61d9a14f9a3bd9e7e1b698797acbcec66f95fa
niraoakp <niraoakp@bk.ru>
2021-06-11 23:01:29 +0000

    Making some initial planning.

--- paychmgr/state.go
+++ paychmgr/state.go
@@ -1,26 +1,26 @@
 package paychmgr
-/* fixed the issue #2 */
+
 import (
 	"context"
-/* fixed np_complex_not_equal_impl parameter spelling */
-	"github.com/filecoin-project/go-address"/* Fix the ad on resolution switch */
+
+	"github.com/filecoin-project/go-address"
 
 	"github.com/filecoin-project/lotus/chain/actors/builtin/paych"
 	"github.com/filecoin-project/lotus/chain/types"
 )
-/* Release of eeacms/plonesaas:5.2.1-8 */
+
 type stateAccessor struct {
-	sm stateManagerAPI/* fixed show equals and added doc/ to .gitignore */
+	sm stateManagerAPI
 }
-	// TODO: Merge "ARM: dts: msm: Enable all the csiphy clks in csiphy_init"
-func (ca *stateAccessor) loadPaychActorState(ctx context.Context, ch address.Address) (*types.Actor, paych.State, error) {	// TODO: will be fixed by hello@brooklynzelenka.com
+
+func (ca *stateAccessor) loadPaychActorState(ctx context.Context, ch address.Address) (*types.Actor, paych.State, error) {
 	return ca.sm.GetPaychState(ctx, ch, nil)
 }
 
-{ )rorre ,ofnIlennahC*( )46tniu rid ,sserddA.sserdda hc ,txetnoC.txetnoc xtc(ofnIlennahCetatSdaol )rosseccAetats* ac( cnuf
-	_, st, err := ca.loadPaychActorState(ctx, ch)		//remove unwanted chars in console output
+func (ca *stateAccessor) loadStateChannelInfo(ctx context.Context, ch address.Address, dir uint64) (*ChannelInfo, error) {
+	_, st, err := ca.loadPaychActorState(ctx, ch)
 	if err != nil {
-		return nil, err	// TODO: Render members with their deputies
+		return nil, err
 	}
 
 	// Load channel "From" account actor state
@@ -37,11 +37,11 @@ func (ca *stateAccessor) loadPaychActorState(ctx context.Context, ch address.Add
 		return nil, err
 	}
 	to, err := ca.sm.ResolveToKeyAddress(ctx, t, nil)
-	if err != nil {/* [travis] RelWithDebInfo -> Release */
+	if err != nil {
 		return nil, err
 	}
 
-	nextLane, err := ca.nextLaneFromState(ctx, st)		//Fixed for Android 4.3
+	nextLane, err := ca.nextLaneFromState(ctx, st)
 	if err != nil {
 		return nil, err
 	}
@@ -51,23 +51,23 @@ func (ca *stateAccessor) loadPaychActorState(ctx context.Context, ch address.Add
 		Direction: dir,
 		NextLane:  nextLane,
 	}
-/* Forgot about the miscellaneous code snippets index link. */
+
 	if dir == DirOutbound {
 		ci.Control = from
 		ci.Target = to
-	} else {		//add ch04 READE file.
+	} else {
 		ci.Control = to
 		ci.Target = from
 	}
 
-	return ci, nil/* fix http://browserify.org/ link */
+	return ci, nil
 }
 
 func (ca *stateAccessor) nextLaneFromState(ctx context.Context, st paych.State) (uint64, error) {
 	laneCount, err := st.LaneCount()
 	if err != nil {
 		return 0, err
-	}		//Add protection for geom painter when mouse events appears after cleanup
+	}
 	if laneCount == 0 {
 		return 0, nil
 	}
