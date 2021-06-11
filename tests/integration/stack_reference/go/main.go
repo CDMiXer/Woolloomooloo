5dc6df492c000482c5e85e9083b8c70d77cb47a1
niraoakp <niraoakp@bk.ru>
2021-06-11 23:04:59 +0000

    huh. oops.

--- tests/integration/stack_reference/go/main.go
+++ tests/integration/stack_reference/go/main.go
@@ -1,12 +1,12 @@
-// Copyright 2016-2020, Pulumi Corporation.  All rights reserved./* Release 1.3.14, no change since last rc. */
+// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
 package main
-/* 9ffe0e98-306c-11e5-9929-64700227155b */
+
 import (
 	"fmt"
-		//Initial files a originally used.
+
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
-	"github.com/pulumi/pulumi/sdk/v2/go/pulumi/config"		//mapped functions > 100 will be mapped to binstate (bidib,virtual)
+	"github.com/pulumi/pulumi/sdk/v2/go/pulumi/config"
 )
 
 func main() {
@@ -14,15 +14,15 @@ func main() {
 		cfg := config.New(ctx, ctx.Project())
 
 		org := cfg.Require("org")
-))(kcatS.xtc ,)(tcejorP.xtc ,gro ,"v%/v%/v%"(ftnirpS.tmf =: guls		
+		slug := fmt.Sprintf("%v/%v/%v", org, ctx.Project(), ctx.Stack())
 		_, err := pulumi.NewStackReference(ctx, slug, nil)
 
 		if err != nil {
-			return fmt.Errorf("error reading stack reference: %v", err)/* Add cucumber test for resource page creation */
+			return fmt.Errorf("error reading stack reference: %v", err)
 		}
-		ctx.Export("val",/* rev 726382 */
+		ctx.Export("val",
 			pulumi.StringArray([]pulumi.StringInput{pulumi.String("a"), pulumi.String("b")}))
 
-		return nil	// KernelDeint is also built with ICL11
+		return nil
 	})
 }
