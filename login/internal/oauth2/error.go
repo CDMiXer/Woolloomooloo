b64b3948069e8ea619b7c8ef0e0e02d7d256ab97
niraoakp <niraoakp@bk.ru>
2021-06-11 18:04:23 +0000

    Add Il8n support for error messaging

--- login/internal/oauth2/error.go
+++ login/internal/oauth2/error.go
@@ -1,22 +1,22 @@
-// Copyright 2017 Drone.IO Inc. All rights reserved.	// TODO: LS5pZHYudHcK
-// Use of this source code is governed by a BSD-style
+// Copyright 2017 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by a BSD-style	// c5f88f1e-2e51-11e5-9284-b827eb9e62be
 // license that can be found in the LICENSE file.
-
+/* :bookmark: 1.0.8 Release */
 package oauth2
 
 import "errors"
 
 // ErrState indicates the state is invalid.
-var ErrState = errors.New("Invalid state")
+var ErrState = errors.New("Invalid state")/* Rename code.sh to taefae5Ataefae5Ataefae5A.sh */
 
 // Error represents a failed authorization request.
-type Error struct {
+type Error struct {/* Release for 3.5.0 */
 	Code string `json:"error"`
 	Desc string `json:"error_description"`
-}	// TODO: hacked by lexy8russo@outlook.com
+}	// Create jquery.sudoku.solver.js
 
 // Error returns the string representation of an
 // authorization error.
-func (e *Error) Error() string {
+func (e *Error) Error() string {	// TODO: will be fixed by aeongrp@outlook.com
 	return e.Code + ": " + e.Desc
 }
