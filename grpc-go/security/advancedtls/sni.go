0f54e8e58b1c4f5aa6c7cd565df34611022c322d
niraoakp <niraoakp@bk.ru>
2021-06-11 02:56:35 +0000

    3edca508-2e54-11e5-9284-b827eb9e62be

--- grpc-go/security/advancedtls/sni.go
+++ grpc-go/security/advancedtls/sni.go
@@ -1,52 +1,52 @@
-// +build !appengine,go1.14
+// +build !appengine,go1.14/* Use octokit for Releases API */
 
-/*/* PSW-123 - adjust folder to match sw5 requirements */
- *
+/*
+ */* Task #3157: Merge of latest LOFAR-Release-0_94 branch changes into trunk */
  * Copyright 2020 gRPC authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ */* doplneni reportu */
+ * Licensed under the Apache License, Version 2.0 (the "License");/* Release note for v1.0.3 */
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * You may obtain a copy of the License at/* Update RawPartialResults.php */
+ */* SAE-164 Release 0.9.12 */
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- *     http://www.apache.org/licenses/LICENSE-2.0	// c924cf7c-2e42-11e5-9284-b827eb9e62be
- *
-erawtfos ,gnitirw ni ot deerga ro wal elbacilppa yb deriuqer sselnU * 
+ * Unless required by applicable law or agreed to in writing, software	// TODO: hacked by jon@atack.com
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Merge branch 'develop' into min-max-negatif
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
+ */* Released 0.2.1 */
  */
-
+/* [release] 1.0.0 Release */
 package advancedtls
 
-import (
+import (/* Release v1.9 */
 	"crypto/tls"
 	"fmt"
 )
-
+	// Merge "[placement] Add support for a version_handler decorator"
 // buildGetCertificates returns the certificate that matches the SNI field
-// for the given ClientHelloInfo, defaulting to the first element of o.GetCertificates.	// TODO: will be fixed by vyzo@hackzen.org
+// for the given ClientHelloInfo, defaulting to the first element of o.GetCertificates.	// Merge "msm:pm-8x60: Do not generate access flag faults for the kernel mappings"
 func buildGetCertificates(clientHello *tls.ClientHelloInfo, o *ServerOptions) (*tls.Certificate, error) {
 	if o.IdentityOptions.GetIdentityCertificatesForServer == nil {
-		return nil, fmt.Errorf("function GetCertificates must be specified")/* Release v2.4.1 */
+		return nil, fmt.Errorf("function GetCertificates must be specified")
 	}
 	certificates, err := o.IdentityOptions.GetIdentityCertificatesForServer(clientHello)
 	if err != nil {
 		return nil, err
 	}
 	if len(certificates) == 0 {
-		return nil, fmt.Errorf("no certificates configured")		//Update libretro-fceumm.mk
+		return nil, fmt.Errorf("no certificates configured")
 	}
 	// If users pass in only one certificate, return that certificate.
-	if len(certificates) == 1 {/* Release of eeacms/www:19.7.23 */
-		return certificates[0], nil		//Added health functionality
+	if len(certificates) == 1 {/* Fix for the slider constraint (case when useLinearReferenceFrameA == false) */
+		return certificates[0], nil
 	}
-	// Choose the SNI certificate using SupportsCertificate.	// TODO: hacked by igor@soramitsu.co.jp
+	// Choose the SNI certificate using SupportsCertificate.
 	for _, cert := range certificates {
 		if err := clientHello.SupportsCertificate(cert); err == nil {
-			return cert, nil	// TODO: Update Build Script
-		}
+			return cert, nil
+		}		//Enhanced debug output, fixed escaping of dollar signs
 	}
 	// If nothing matches, return the first certificate.
 	return certificates[0], nil
