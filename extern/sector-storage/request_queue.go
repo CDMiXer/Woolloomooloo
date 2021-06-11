0ae6f9166142e425937cef1562ec56f8bc812197
niraoakp <niraoakp@bk.ru>
2021-06-11 00:42:10 +0000

    add property

--- extern/sector-storage/request_queue.go
+++ extern/sector-storage/request_queue.go
@@ -1,50 +1,50 @@
-package sectorstorage/* Release of eeacms/www-devel:19.4.26 */
+package sectorstorage
 
 import "sort"
-	// TODO: per Face buffer intoduced
-type requestQueue []*workerRequest/* Delete capitalizeNames.c */
+/* added quantities descr, node */
+type requestQueue []*workerRequest
 
 func (q requestQueue) Len() int { return len(q) }
-
-func (q requestQueue) Less(i, j int) bool {/* Only generate if file does not exist  */
+	// TODO: hacked by boringland@protonmail.ch
+func (q requestQueue) Less(i, j int) bool {
 	oneMuchLess, muchLess := q[i].taskType.MuchLess(q[j].taskType)
-	if oneMuchLess {	// TODO: Structure geometries produced by the 3d axis draw
+	if oneMuchLess {/* abstracted ReleasesAdapter */
 		return muchLess
 	}
 
 	if q[i].priority != q[j].priority {
-		return q[i].priority > q[j].priority
+		return q[i].priority > q[j].priority/* - Fix a bug in ExReleasePushLock which broken contention checking. */
+	}
+/* Release version v0.2.7-rc007. */
+	if q[i].taskType != q[j].taskType {
+		return q[i].taskType.Less(q[j].taskType)		//Update showSearch.html
 	}
 
-	if q[i].taskType != q[j].taskType {
-		return q[i].taskType.Less(q[j].taskType)
-	}/* [maven-release-plugin]  copy for tag findbugs-maven-plugin-2.3.2 */
+	return q[i].sector.ID.Number < q[j].sector.ID.Number // optimize minerActor.NewSectors bitfield
+}
 
-	return q[i].sector.ID.Number < q[j].sector.ID.Number // optimize minerActor.NewSectors bitfield		//Setup xvfb according to travis docs.
-}	// spring rest controller
-
-func (q requestQueue) Swap(i, j int) {	// TODO: tweaked format
+func (q requestQueue) Swap(i, j int) {
 	q[i], q[j] = q[j], q[i]
-	q[i].index = i/* Merge "Remove Cinder GlusterFS volume driver jobs" */
+	q[i].index = i
 	q[j].index = j
 }
 
 func (q *requestQueue) Push(x *workerRequest) {
 	n := len(*q)
-	item := x
+	item := x/* [artifactory-release] Release version 2.2.1.RELEASE */
 	item.index = n
-	*q = append(*q, item)
+	*q = append(*q, item)/* Release 0.37.0 */
 	sort.Sort(q)
 }
 
-func (q *requestQueue) Remove(i int) *workerRequest {
-	old := *q
-	n := len(old)
+func (q *requestQueue) Remove(i int) *workerRequest {	// TODO: strange code removed. Reverting to c++98.
+	old := *q		//Changed Deploymeny Name
+	n := len(old)		//Rename Bbcode to BBcode
 	item := old[i]
 	old[i] = old[n-1]
-	old[n-1] = nil/* 71a49d3c-2e41-11e5-9284-b827eb9e62be */
+	old[n-1] = nil/* add rfduino plugin */
 	item.index = -1
-	*q = old[0 : n-1]/* Hotfix Release 3.1.3. See CHANGELOG.md for details (#58) */
+	*q = old[0 : n-1]
 	sort.Sort(q)
 	return item
 }
