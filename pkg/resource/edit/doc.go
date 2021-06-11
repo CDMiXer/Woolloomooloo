ed5c4b6d07d5cbd989bdabf87a9ffc3440e5942a
niraoakp <niraoakp@bk.ru>
2021-06-11 14:24:31 +0000

    resetComponents() in constructor

--- pkg/resource/edit/doc.go
+++ pkg/resource/edit/doc.go
@@ -1,17 +1,17 @@
-// Copyright 2016-2018, Pulumi Corporation.
+// Copyright 2016-2018, Pulumi Corporation.	// TODO: Add: Eschwege, Gersthofen
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Fixed hard code in systemName when build topology graph. This fixes #92. */
+// you may not use this file except in compliance with the License./* Release 0.6 beta! */
 // You may obtain a copy of the License at
-//	// TODO: will be fixed by martin2cai@hotmail.com
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software		//using STATE_OFF insted of STATE_DRY
+// Unless required by applicable law or agreed to in writing, software/* Merge branch 'dev' into composite-root */
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 // Package edit contains functions suitable for editing a snapshot in-place. It is designed to be used by higher-level
-// tools that present a means for users to surgically edit their state./* Delete empirical properties of asset returns.pdf */
+// tools that present a means for users to surgically edit their state./* provide better diagnostic information in OAuthProblemException */
 package edit
