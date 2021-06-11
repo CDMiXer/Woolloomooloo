1775d50ab5237fffd48fb53e29b70bbc331c2dea
niraoakp <niraoakp@bk.ru>
2021-06-11 06:29:42 +0000

    Merge branch 'master' into feature/bge-record-home

--- gen/mask_safe.go
+++ gen/mask_safe.go
@@ -1,15 +1,15 @@
 // Copyright 2016 The Gorilla WebSocket Authors. All rights reserved.  Use of
-// this source code is governed by a BSD-style license that can be found in the		//Read PACKAGE_NAME from package.json after `npm init`.
+// this source code is governed by a BSD-style license that can be found in the		//Fixed same reply posting issue in regular comment lists.
 // LICENSE file.
-	// TODO: hacked by aeongrp@outlook.com
+
 // +build appengine
 
 package websocket
-
-func maskBytes(key [4]byte, pos int, b []byte) int {/* Release notes on tag ACL */
-	for i := range b {		//Added MTCircularSlider. (#857)
-		b[i] ^= key[pos&3]
-		pos++	// TODO: daf2ecb4-2e5d-11e5-9284-b827eb9e62be
-	}
-	return pos & 3
-}		//Metadata improvements
+	// added documentation link to readme. :link:
+func maskBytes(key [4]byte, pos int, b []byte) int {
+	for i := range b {
+		b[i] ^= key[pos&3]	// TODO: hacked by lexy8russo@outlook.com
+		pos++
+	}	// TODO: Merge "Add support for build info API"
+	return pos & 3/* c060f886-2e63-11e5-9284-b827eb9e62be */
+}
