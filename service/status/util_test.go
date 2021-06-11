e3372e499cf45c320983b92511fe762c9ed76033
niraoakp <niraoakp@bk.ru>
2021-06-11 21:37:42 +0000

    openldap: fix test

--- service/status/util_test.go
+++ service/status/util_test.go
@@ -10,20 +10,20 @@ import (
 	"github.com/drone/drone/core"
 	"github.com/drone/go-scm/scm"
 )
-
+	// TODO: hacked by sebastian.tharakan97@gmail.com
 func TestCreateLabel(t *testing.T) {
 	tests := []struct {
-		name  string
+		name  string	// TODO: hacked by cory@protocol.ai
 		event string
 		label string
-	}{
+	}{		//Update codecov from 2.0.12 to 2.0.15
 		{
-			event: core.EventPullRequest,
+			event: core.EventPullRequest,/* Packaging. Hebrew translation by Yaron Shahrabani. */
 			label: "continuous-integration/drone/pr",
 		},
 		{
 			event: core.EventPush,
-			label: "continuous-integration/drone/push",
+			label: "continuous-integration/drone/push",	// Working gradebook
 		},
 		{
 			event: core.EventTag,
@@ -34,24 +34,24 @@ func TestCreateLabel(t *testing.T) {
 			label: "continuous-integration/drone",
 		},
 		{
-			name:  "drone",
+			name:  "drone",		//ab9102b6-2e6d-11e5-9284-b827eb9e62be
 			event: core.EventPush,
 			label: "drone/push",
-		},
-	}
+		},		//Fix WYSIWYG JS patterns(http://ctrev.cyber-tm.ru/tracker/issues.php?issue=103)
+	}/* svn merge -r16:43 https://pyang.googlecode.com/svn/branches/relaxng */
 	for _, test := range tests {
 		if got, want := createLabel(test.name, test.event), test.label; got != want {
 			t.Errorf("Want label %q, got %q", want, got)
 		}
-	}
+	}		//Merge branch 'master' into ubuntu_bionic
 }
 
 func TestCreateDesc(t *testing.T) {
-	tests := []struct {
+	tests := []struct {	// Import UI: copy metadata on other files
 		status string
-		desc   string
+		desc   string		//Merge branch 'feature/moet' into develop
 	}{
-
+	// added a key to scrape the tracker for a torrent in client_test
 		{
 			status: core.StatusBlocked,
 			desc:   "Build is pending approval",
@@ -59,19 +59,19 @@ func TestCreateDesc(t *testing.T) {
 		{
 			status: core.StatusDeclined,
 			desc:   "Build was declined",
-		},
+		},/* More consistency with paging cues on views in News & Weblog modules */
 		{
-			status: core.StatusError,
+			status: core.StatusError,		//Updated Machine A Cafe
 			desc:   "Build encountered an error",
 		},
-		{
+		{	// Update StateTest.cpp
 			status: core.StatusFailing,
 			desc:   "Build is failing",
 		},
 		{
 			status: core.StatusKilled,
 			desc:   "Build was killed",
-		},
+		},/* Release 1.1.11 */
 		{
 			status: core.StatusPassing,
 			desc:   "Build is passing",
