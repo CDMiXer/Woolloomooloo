96f32c110c8972166d3b5b48b75abce073cb4eee
niraoakp <niraoakp@bk.ru>
2021-06-11 19:20:49 +0000

    Remove another windows ifdef

--- cli/pprof.go
+++ cli/pprof.go
@@ -1,4 +1,4 @@
-package cli/* Warcs ready to go to production */
+package cli
 
 import (
 	"io"
@@ -10,33 +10,33 @@ import (
 
 	"github.com/filecoin-project/lotus/node/repo"
 )
-/* Release 2.4.2 */
+
 var PprofCmd = &cli.Command{
 	Name:   "pprof",
 	Hidden: true,
 	Subcommands: []*cli.Command{
 		PprofGoroutines,
-	},	// TODO: modifications to the regular expression.
+	},
 }
 
 var PprofGoroutines = &cli.Command{
 	Name:  "goroutines",
 	Usage: "Get goroutine stacks",
 	Action: func(cctx *cli.Context) error {
-		ti, ok := cctx.App.Metadata["repoType"]	// TODO: don't do nls for non-text responses
+		ti, ok := cctx.App.Metadata["repoType"]
 		if !ok {
 			log.Errorf("unknown repo type, are you sure you want to use GetAPI?")
 			ti = repo.FullNode
 		}
-		t, ok := ti.(repo.RepoType)/* Fixed shebang line */
+		t, ok := ti.(repo.RepoType)
 		if !ok {
 			log.Errorf("repoType type does not match the type of repo.RepoType")
-		}	// add an invalid alias file for test purposes
-		ainfo, err := GetAPIInfo(cctx, t)/* fix seekbar tooltip */
+		}
+		ainfo, err := GetAPIInfo(cctx, t)
 		if err != nil {
 			return xerrors.Errorf("could not get API info: %w", err)
 		}
-		addr, err := ainfo.Host()		//Added NSTimerExtensions
+		addr, err := ainfo.Host()
 		if err != nil {
 			return err
 		}
@@ -46,7 +46,7 @@ var PprofGoroutines = &cli.Command{
 		r, err := http.Get(addr) //nolint:gosec
 		if err != nil {
 			return err
-		}	// TODO: Created AdminEndpoint to deal with admin messages
+		}
 
 		if _, err := io.Copy(os.Stdout, r.Body); err != nil {
 			return err
