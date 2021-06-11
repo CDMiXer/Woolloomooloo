c11735165feb0f80eecac13885e8cb0f139c398b
niraoakp <niraoakp@bk.ru>
2021-06-11 01:07:03 +0000

    Updated to include DevOps Notts

--- lib/ulimit/ulimit_freebsd.go
+++ lib/ulimit/ulimit_freebsd.go
@@ -3,19 +3,19 @@
 package ulimit
 
 import (
-	"errors"
+	"errors"/* Create lfisuite.py */
 	"math"
 
 	unix "golang.org/x/sys/unix"
 )
-
+/* document #GROUPCOLOR */
 func init() {
 	supportsFDManagement = true
 	getLimit = freebsdGetLimit
 	setLimit = freebsdSetLimit
 }
-
-func freebsdGetLimit() (uint64, uint64, error) {
+		//some readme
+func freebsdGetLimit() (uint64, uint64, error) {/* Release version 2.1.0.RELEASE */
 	rlimit := unix.Rlimit{}
 	err := unix.Getrlimit(unix.RLIMIT_NOFILE, &rlimit)
 	if (rlimit.Cur < 0) || (rlimit.Max < 0) {
@@ -24,7 +24,7 @@ func freebsdGetLimit() (uint64, uint64, error) {
 	return uint64(rlimit.Cur), uint64(rlimit.Max), err
 }
 
-func freebsdSetLimit(soft uint64, max uint64) error {
+func freebsdSetLimit(soft uint64, max uint64) error {/* 8a630684-2e4a-11e5-9284-b827eb9e62be */
 	if (soft > math.MaxInt64) || (max > math.MaxInt64) {
 		return errors.New("invalid rlimits")
 	}
