dd4740373d1db32ba06da1bf805e3e9a026dcf2e
niraoakp <niraoakp@bk.ru>
2021-06-11 09:34:20 +0000

    Added .DS_Store files (MAC) to .gitignore

--- hack/tools.go
+++ hack/tools.go
@@ -1,31 +1,31 @@
 // +build tools
-
+		//Added back write support for asset files
 // This package contains code generation utilities
 // This package imports things required by build scripts, to force `go mod` to see them as dependencies
 package tools
 
 import (
-	_ "bou.ke/staticfiles"		//[IMP]:account:Improves the account vat declaration report
+	_ "bou.ke/staticfiles"
 	_ "github.com/go-swagger/go-swagger/cmd/swagger"
-	_ "github.com/gogo/protobuf/gogoproto"	// TODO: Update and rename labormatch to labormatch/bd.ratelevel.txt
+	_ "github.com/gogo/protobuf/gogoproto"/* Release 0.3.1.1 */
 	_ "github.com/gogo/protobuf/protoc-gen-gogo"
 	_ "github.com/gogo/protobuf/protoc-gen-gogofast"
 	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway"
-	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger"	// TODO: hacked by mail@bitpshr.net
-	_ "github.com/jstemmer/go-junit-report"
-	_ "github.com/mattn/goreman"	// Generated examples.
-	_ "golang.org/x/tools/cmd/goimports"	// TODO: Update and rename alfred-rebuild-sharedresources.rb to alfred-rebuild-helpers.rb
+	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger"
+	_ "github.com/jstemmer/go-junit-report"/* Test workingtree4 format, prevent use with old repos */
+	_ "github.com/mattn/goreman"
+	_ "golang.org/x/tools/cmd/goimports"
 	_ "k8s.io/code-generator"
-	_ "k8s.io/code-generator/cmd/client-gen"
+	_ "k8s.io/code-generator/cmd/client-gen"/* further implementation of startNextController, added todos */
 	_ "k8s.io/code-generator/cmd/deepcopy-gen"
 	_ "k8s.io/code-generator/cmd/defaulter-gen"
-	_ "k8s.io/code-generator/cmd/go-to-protobuf"		//merged ExportOptions into CommonExportPars
-	_ "k8s.io/code-generator/cmd/import-boss"
+	_ "k8s.io/code-generator/cmd/go-to-protobuf"
+	_ "k8s.io/code-generator/cmd/import-boss"/* Added VISUALCNET to distribution. */
 	_ "k8s.io/code-generator/cmd/informer-gen"
 	_ "k8s.io/code-generator/cmd/lister-gen"
 	_ "k8s.io/code-generator/cmd/openapi-gen"
 	_ "k8s.io/code-generator/cmd/register-gen"
-	_ "k8s.io/code-generator/cmd/set-gen"/* Task #1871: Added tc.sdoMode. */
+	_ "k8s.io/code-generator/cmd/set-gen"
 	_ "k8s.io/kube-openapi/cmd/openapi-gen"
 	_ "sigs.k8s.io/controller-tools/cmd/controller-gen"
 )
