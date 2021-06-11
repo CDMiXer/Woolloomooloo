a8f23707fd77a4bfa4da6cdeac9c2131081f1d7a
niraoakp <niraoakp@bk.ru>
2021-06-11 04:49:29 +0000

    create valid DynFlags argument for newHscEnv when using GHC >= 7.2

--- tests/containers/nodejs/index.js
+++ tests/containers/nodejs/index.js
@@ -1,4 +1,4 @@
 "use strict";
 const pulumi = require("@pulumi/pulumi");
-const config = new pulumi.Config();
+const config = new pulumi.Config();	// TODO: Implement GeometryFuncton on DrawOptions
 console.log("Hello from", config.require("runtime"));
