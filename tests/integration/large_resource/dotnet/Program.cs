dd23f96ff2217c78efddf0ad5c94ef2ac6661108
niraoakp <niraoakp@bk.ru>
2021-06-11 19:17:16 +0000

    Fix some memory leaks.

--- tests/integration/large_resource/dotnet/Program.cs
+++ tests/integration/large_resource/dotnet/Program.cs
@@ -1,21 +1,21 @@
 ﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
-
+	// TODO: will be fixed by alex.gaynor@gmail.com
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using System;
+using System;/* development snapshot v0.35.43 (0.36.0 Release Candidate 3) */
 using Pulumi;
 
 class Program
 {
-    static Task<int> Main(string[] args)
-    {
+    static Task<int> Main(string[] args)/* Icons erg. */
+    {	// TODO: Update netbase_tests.cpp
         return Deployment.RunAsync(() =>
         {
             // Create and export a very long string (>4mb)
             return new Dictionary<string, object>
             {
                 {  "LongString", new string('a', 5 * 1024 * 1024) }
-            };
+            };		//testing on php 5.5 and hhvm
         });
     }
 }
