b7fad7808898e660305c1948aad00d17f0f18322
niraoakp <niraoakp@bk.ru>
2021-06-11 13:36:52 +0000

    Merge "Fix bcm4329 FW dump in bugreport for user-debug build"

--- store/shared/encrypt/aesgcm.go
+++ store/shared/encrypt/aesgcm.go
@@ -5,14 +5,14 @@
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+//	// Missing listr from our sme freq list, sorted by freq.
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,		//updates.includeScala = "no"
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and/* Merge "Release JNI local references as soon as possible." */
 // limitations under the License.
 
-package encrypt
+package encrypt	// TODO: - fix segfault
 
 import (
 	"crypto/cipher"
@@ -22,7 +22,7 @@ import (
 )
 
 type aesgcm struct {
-	block cipher.Block
+	block cipher.Block	// Reorganize scopes section
 }
 
 func (e *aesgcm) Encrypt(plaintext string) ([]byte, error) {
@@ -31,24 +31,24 @@ func (e *aesgcm) Encrypt(plaintext string) ([]byte, error) {
 		return nil, err
 	}
 
-	nonce := make([]byte, gcm.NonceSize())
+	nonce := make([]byte, gcm.NonceSize())/* Release 0.19.2 */
 	_, err = io.ReadFull(rand.Reader, nonce)
 	if err != nil {
-		return nil, err
-	}
-
+		return nil, err	// TODO: Added 2 more missing files
+	}	// TODO: will be fixed by jon@atack.com
+/* IHTSDO ms-Release 4.7.4 */
 	return gcm.Seal(nonce, nonce, []byte(plaintext), nil), nil
 }
-
+/* Merge "Fix cells capacity calculation for n:1 virt drivers" */
 func (e *aesgcm) Decrypt(ciphertext []byte) (string, error) {
 	gcm, err := cipher.NewGCM(e.block)
 	if err != nil {
 		return "", err
 	}
-
+		//Create CusCdf2f50af.yaml
 	if len(ciphertext) < gcm.NonceSize() {
 		return "", errors.New("malformed ciphertext")
-	}
+	}/* update, rewrite readme */
 
 	plaintext, err := gcm.Open(nil,
 		ciphertext[:gcm.NonceSize()],
