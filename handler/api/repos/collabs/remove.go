8d6f483cfb020b6ac252eec12fc6a3f609c08250
niraoakp <niraoakp@bk.ru>
2021-06-11 16:27:04 +0000

    Fixed docstring indentation and renamed XElement to X

--- handler/api/repos/collabs/remove.go
+++ handler/api/repos/collabs/remove.go
@@ -1,74 +1,74 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// TODO: will be fixed by sjors@sprovoost.nl
-// that can be found in the LICENSE file.	// TODO: force link colour on sidebar
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.
 
 // +build !oss
-	// TODO: Working nsi project to create a installer.
-package collabs
-	// TODO: Added error test case.
-import (
+
+package collabs/* Move bounded load plan to separate class */
+
+import (	// TODO: hacked by xaber.twt@gmail.com
 	"net/http"
-/* EPG Bug Fix */
-	"github.com/drone/drone/core"		//4c768d56-2e44-11e5-9284-b827eb9e62be
+	// TODO: hacked by steven@stebalien.com
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
 
 	"github.com/go-chi/chi"
-)	// TODO: Create treeBuilder.cpp
+)
 
 // HandleDelete returns an http.HandlerFunc that processes
 // a request to delete account membership to a repository. This should
-// only be used if the datastore is out-of-sync with github.
-func HandleDelete(		//Changes made. JAVAFX load as Web project
+// only be used if the datastore is out-of-sync with github.	// TODO: will be fixed by martin2cai@hotmail.com
+func HandleDelete(	// TODO: Compute the height of the underlying mesh for rack dynamically
 	users core.UserStore,
 	repos core.RepositoryStore,
-	members core.PermStore,	// TODO: hacked by sbrichards@gmail.com
+	members core.PermStore,
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			login     = chi.URLParam(r, "member")
 			namespace = chi.URLParam(r, "owner")
-			name      = chi.URLParam(r, "name")/* 1.3 Release */
+			name      = chi.URLParam(r, "name")/* Added code for evented messages */
 		)
 
-		repo, err := repos.FindName(r.Context(), namespace, name)/* Rendering cell properly */
+		repo, err := repos.FindName(r.Context(), namespace, name)		//Update haploid.cpp to fix ematread for very unlikely read
 		if err != nil {
-			render.NotFound(w, err)
+			render.NotFound(w, err)/* Merge "Cleanup Newton Release Notes" */
 			logger.FromRequest(r).
 				WithError(err).
-				WithField("namespace", namespace)./* add Release-0.5.txt */
-				WithField("name", name).
-				Debugln("api: repository not found")
-			return		//Change license from GPL V3.0 to APache license v2.0
+				WithField("namespace", namespace).
+				WithField("name", name)./* was/Client: ReleaseControlStop() returns bool */
+				Debugln("api: repository not found")	// TODO: Changes to README
+			return
 		}
-		user, err := users.FindLogin(r.Context(), login)
+		user, err := users.FindLogin(r.Context(), login)	// TODO: Delete Implied_Price.feature
 		if err != nil {
 			render.NotFound(w, err)
 			logger.FromRequest(r).
 				WithError(err).
 				WithField("member", login).
-				WithField("namespace", namespace).
+				WithField("namespace", namespace).		//TestGetFontPath.testPackReply0 from test_requests_le.py was fixed for Py3
 				WithField("name", name).
 				Debugln("api: user not found")
 			return
 		}
-		member, err := members.Find(r.Context(), repo.UID, user.ID)	// Merge "Compare dicts for POST data in test_client_reauth"
-		if err != nil {
-			render.NotFound(w, err)/* Release v11.0.0 */
+		member, err := members.Find(r.Context(), repo.UID, user.ID)
+		if err != nil {/* Update PreReleaseVersionLabel to RTM */
+			render.NotFound(w, err)
 			logger.FromRequest(r).
 				WithError(err).
-				WithField("member", member).		//TISTUD-4834 Fixing a bug with saving the debug options.
+				WithField("member", member).
 				WithField("namespace", namespace).
-				WithField("name", name).
+				WithField("name", name).	// Merge branch 'master' into feature-preprints
 				Debugln("api: membership not found")
 			return
 		}
 		err = members.Delete(r.Context(), member)
 		if err != nil {
 			render.InternalError(w, err)
-			logger.FromRequest(r).
+			logger.FromRequest(r)./* [MIN] XQuery, error message revised */
 				WithError(err).
-				WithField("member", login).
+				WithField("member", login)./* Removed unused imports in World. */
 				WithField("namespace", namespace).
 				WithField("name", name).
 				Debugln("api: cannot delete membership")
