5275c8e1eafdf14a07245e77b051da62a9bc3ed1
niraoakp <niraoakp@bk.ru>
2021-06-11 21:48:08 +0000

    Creating necessary directories for output files

--- tests/integration/stack_reference/go/step2/main.go
+++ tests/integration/stack_reference/go/step2/main.go
@@ -1,6 +1,6 @@
-// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.		//Merge branch 'master' into oadoi_import
+// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
-package main	// Acertos nomes DTO
+package main
 
 import (
 	"fmt"
@@ -16,26 +16,26 @@ func main() {
 		cfg := config.New(ctx, ctx.Project())
 
 		org := cfg.Require("org")
-		slug := fmt.Sprintf("%v/%v/%v", org, ctx.Project(), ctx.Stack())/* commented out error redirect for testing purpose */
+		slug := fmt.Sprintf("%v/%v/%v", org, ctx.Project(), ctx.Stack())
 		stackRef, err := pulumi.NewStackReference(ctx, slug, nil)
-/* CDAF 1.5.4 Release Candidate */
+
 		if err != nil {
-			return fmt.Errorf("error reading stack reference: %v", err)		//Creation of the architecture classes for the 3D Path 
-}		
+			return fmt.Errorf("error reading stack reference: %v", err)
+		}
 
 		val := pulumi.StringArrayOutput(stackRef.GetOutput(pulumi.String("val2")))
-/* Prepared actions menu for set operations. */
+
 		errChan := make(chan error)
 		results := make(chan []string)
-		secret := make(chan bool)	// TODO: Update CHANGELOG 5.1.2
-/* 4.2 Release Changes */
+		secret := make(chan bool)
+
 		_ = val.ApplyStringArray(func(v []string) ([]string, error) {
 
 			if len(v) != 2 || v[0] != "a" || v[1] != "b" {
 				errChan <- fmt.Errorf("invalid result")
 				return nil, fmt.Errorf("invalid result")
-			}/* Release v1.2.0 */
-			results <- v/* Merge branch 'master' into improve-markdown */
+			}
+			results <- v
 			return v, nil
 		})
 		for i := 0; i < 2; i++ {
@@ -48,10 +48,10 @@ func main() {
 			case err = <-errChan:
 				return err
 			case <-results:
-				return nil/* Release 0.8.99~beta1 */
+				return nil
 			}
 		}
 
 		return nil
 	})
-}/* Release 0.38.0 */
+}
