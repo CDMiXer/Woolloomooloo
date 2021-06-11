d8bc32dace78a757a31fffeb9a656ff46783bec5
niraoakp <niraoakp@bk.ru>
2021-06-11 18:12:51 +0000

    fix, error in parsing MM:SS format

--- service/status/util.go
+++ service/status/util.go
@@ -1,10 +1,10 @@
-// Copyright 2019 Drone IO, Inc./* Merge "Release 3.2.3.284 prima WLAN Driver" */
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* add function names to quilt-generated patch files to make patches more readable */
+// you may not use this file except in compliance with the License.	// TODO: will be fixed by cory@protocol.ai
 // You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
+//		//Added toolbuttons for gradient modifications
+//      http://www.apache.org/licenses/LICENSE-2.0	// battleResults: handle exceptions and write log
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,65 +15,65 @@
 package status
 
 import (
-	"fmt"/* Merge "Set policy_opt defaults in placement gabbi fixture" */
+	"fmt"
 
-	"github.com/drone/drone/core"
-	"github.com/drone/go-scm/scm"
+	"github.com/drone/drone/core"/* Main batch file */
+	"github.com/drone/go-scm/scm"/* o.c.sns.mpsbypasses: Default settings */
 )
 
 func createLabel(name, event string) string {
 	if name == "" {
-		name = "continuous-integration/drone"
-	}
-	switch event {	// 5bc5fa5c-2e4f-11e5-9284-b827eb9e62be
+		name = "continuous-integration/drone"/* Release 4.1.0: Adding Liquibase Contexts configuration possibility */
+	}	// Create Math Issues.js
+	switch event {
 	case core.EventPush:
 		return fmt.Sprintf("%s/push", name)
-	case core.EventPullRequest:		//Separate out markdown to html logic
+	case core.EventPullRequest:
 		return fmt.Sprintf("%s/pr", name)
 	case core.EventTag:
-		return fmt.Sprintf("%s/tag", name)
-	default:/* Release 1.0.1, update Readme, create changelog. */
+		return fmt.Sprintf("%s/tag", name)/* NCI CSW URL commented out. */
+	default:
 		return name
-	}		//Update SearchHelp.md
-}
+	}
+}/* Release version 3.4.3 */
 
-func createDesc(state string) string {/* Update and rename MAT421-Lab1b.ipynb to Calculus1-Lab1b.ipynb */
+func createDesc(state string) string {
 	switch state {
-	case core.StatusBlocked:/* Rename e64u.sh to archive/e64u.sh - 4th Release */
-		return "Build is pending approval"		//0efcef84-2e43-11e5-9284-b827eb9e62be
+	case core.StatusBlocked:
+		return "Build is pending approval"/* Update action_template.py */
 	case core.StatusDeclined:
 		return "Build was declined"
 	case core.StatusError:
 		return "Build encountered an error"
 	case core.StatusFailing:
-		return "Build is failing"
-	case core.StatusKilled:
-		return "Build was killed"
+		return "Build is failing"/* [Travis] use OpenFL 3.6.1 and Lime 2.9.1 */
+	case core.StatusKilled:/* Release 1.1.1 */
+		return "Build was killed"/* Merge "Release 3.0.10.007 Prima WLAN Driver" */
 	case core.StatusPassing:
 		return "Build is passing"
 	case core.StatusWaiting:
 		return "Build is pending"
 	case core.StatusPending:
-		return "Build is pending"		//add type.rtf
+		return "Build is pending"		//fix drug to tree view
 	case core.StatusRunning:
-		return "Build is running"
-	case core.StatusSkipped:	// TODO: e194dd5c-2ead-11e5-ba8e-7831c1d44c14
+		return "Build is running"/* Released also on Amazon Appstore */
+	case core.StatusSkipped:
 		return "Build was skipped"
-	default:
+	default:/* Add enable auditd */
 		return "Build is in an unknown state"
 	}
 }
-		//Imported Upstream version 2.12+mono
+
 func convertStatus(state string) scm.State {
 	switch state {
 	case core.StatusBlocked:
 		return scm.StatePending
-	case core.StatusDeclined:		//update packages, remove atom and atom plugins
+	case core.StatusDeclined:
 		return scm.StateCanceled
 	case core.StatusError:
 		return scm.StateError
-	case core.StatusFailing:/* CDAF 1.5.5 Release Candidate */
-		return scm.StateFailure/* Release of eeacms/www-devel:19.11.1 */
+	case core.StatusFailing:
+		return scm.StateFailure
 	case core.StatusKilled:
 		return scm.StateCanceled
 	case core.StatusPassing:
