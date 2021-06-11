513ea65319735006bc0ed8dd4b4e23c2799c8f04
niraoakp <niraoakp@bk.ru>
2021-06-11 11:45:43 +0000

    Fixed bug when button hidden

--- tests/integration/read/read_dbr/step3/index.ts
+++ tests/integration/read/read_dbr/step3/index.ts
@@ -1,9 +1,9 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: On screen keyboard
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+///* publish firmware of MiniRelease1 */
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
@@ -11,7 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+	// Initial SearchIndex documentation.
 import { Resource } from "./resource";
 
 const a = new Resource("a", { state: 42 }, { id: "existing-id"} );
