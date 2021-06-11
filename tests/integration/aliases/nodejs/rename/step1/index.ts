2ba6c08a2a52effc8571ea01f2bde9eda1642540
niraoakp <niraoakp@bk.ru>
2021-06-11 12:33:14 +0000

    76e76314-2f8c-11e5-b366-34363bc765d8

--- tests/integration/aliases/nodejs/rename/step1/index.ts
+++ tests/integration/aliases/nodejs/rename/step1/index.ts
@@ -1,12 +1,12 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// TODO: send userlist and calculate role from userlist
 
 import * as pulumi from "@pulumi/pulumi";
 
 class Resource extends pulumi.ComponentResource {
-    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {	// TODO: hacked by martin2cai@hotmail.com
-        super("my:module:Resource", name, {}, opts);
+    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
+        super("my:module:Resource", name, {}, opts);	// TODO: Update Event.cpp
     }
 }
-/* Release version: 0.7.6 */
+
 // Scenario #1 - rename a resource
 const res1 = new Resource("res1");
