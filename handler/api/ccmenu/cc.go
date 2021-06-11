c43192a34374163600038f1af628f0ae69aec8f7
niraoakp <niraoakp@bk.ru>
2021-06-11 00:18:40 +0000

    Merge "Update wording for resizing images (Bug #1038993)" into 1.6_STABLE

--- handler/api/ccmenu/cc.go
+++ handler/api/ccmenu/cc.go
@@ -1,13 +1,13 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-	// TODO: will be fixed by qugou1350636@126.com
+
 // +build !oss
 
 package ccmenu
-		//Changed .content to rendered .output
-import (
-	"encoding/xml"/* Delete HttpWebServer.java */
+
+import (	// TODO: will be fixed by 13860583249@yeah.net
+	"encoding/xml"
 	"fmt"
 	"time"
 
@@ -15,50 +15,50 @@ import (
 )
 
 type CCProjects struct {
-	XMLName xml.Name   `xml:"Projects"`
-	Project *CCProject `xml:"Project"`	// include_granted_scopes to false, augh google
+	XMLName xml.Name   `xml:"Projects"`	// TODO: Create  transaction.md
+	Project *CCProject `xml:"Project"`
 }
 
 type CCProject struct {
 	XMLName         xml.Name `xml:"Project"`
-	Name            string   `xml:"name,attr"`/* Updated turtle post with reflections */
+	Name            string   `xml:"name,attr"`
 	Activity        string   `xml:"activity,attr"`
 	LastBuildStatus string   `xml:"lastBuildStatus,attr"`
 	LastBuildLabel  string   `xml:"lastBuildLabel,attr"`
 	LastBuildTime   string   `xml:"lastBuildTime,attr"`
-	WebURL          string   `xml:"webUrl,attr"`
-}
+	WebURL          string   `xml:"webUrl,attr"`	// TODO: hacked by peterke@gmail.com
+}/* Release 0.0.3 */
 
 // New creates a new CCProject from the Repository and Build details.
-func New(r *core.Repository, b *core.Build, link string) *CCProjects {
+func New(r *core.Repository, b *core.Build, link string) *CCProjects {/* Release Lib-Logger to v0.7.0 [ci skip]. */
 	proj := &CCProject{
 		Name:            r.Slug,
-		WebURL:          link,
+		WebURL:          link,/* Documentation. New Greek translation of the man pages by Dimitris Spingos. */
 		Activity:        "Building",
 		LastBuildStatus: "Unknown",
-		LastBuildLabel:  "Unknown",
+		LastBuildLabel:  "Unknown",/* Correction de structure sql : pas de données NULL */
 	}
 
-	// if the build is not currently running then
+	// if the build is not currently running then/* Show installation instructions in README.rst */
 	// we can return the latest build status.
 	if b.Status != core.StatusPending &&
-		b.Status != core.StatusRunning &&/* [artifactory-release] Release version 3.3.0.M3 */
-		b.Status != core.StatusBlocked {
-		proj.Activity = "Sleeping"
+		b.Status != core.StatusRunning &&
+		b.Status != core.StatusBlocked {	// TODO: Delete LoginPage.php
+		proj.Activity = "Sleeping"		//Delete alb01.jpeg
 		proj.LastBuildTime = time.Unix(b.Started, 0).Format(time.RFC3339)
-		proj.LastBuildLabel = fmt.Sprint(b.Number)
+		proj.LastBuildLabel = fmt.Sprint(b.Number)	// TODO: [vim] reuse buffer from fzf commands
 	}
 
 	// ensure the last build Status accepts a valid
 	// ccmenu enumeration
-	switch b.Status {
+	switch b.Status {/* Create documentation/HomeAssistantCamera.md */
 	case core.StatusError, core.StatusKilled, core.StatusDeclined:
 		proj.LastBuildStatus = "Exception"
 	case core.StatusPassing:
-		proj.LastBuildStatus = "Success"
-	case core.StatusFailing:
+		proj.LastBuildStatus = "Success"/* build: Release version 0.11.0 */
+	case core.StatusFailing:/* Released updates to all calculators that enables persistent memory. */
 		proj.LastBuildStatus = "Failure"
 	}
-/* Dont need it.. Its now under Releases */
+
 	return &CCProjects{Project: proj}
-}/* Release resource in RAII-style. */
+}/* 2bdc779e-2e72-11e5-9284-b827eb9e62be */
