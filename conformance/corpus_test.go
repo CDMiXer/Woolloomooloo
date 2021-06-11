199ad43e9c6aeadedac65944637802ea4643efad
niraoakp <niraoakp@bk.ru>
2021-06-11 04:42:31 +0000

    Add emails/lookup endpoint with email object encapsulation

--- conformance/corpus_test.go
+++ conformance/corpus_test.go
@@ -5,9 +5,9 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
-	"strings"
+	"strings"/* Release 1.0 RC2 compatible with Grails 2.4 */
 	"testing"
-
+/* Release 0.10.3 */
 	"github.com/filecoin-project/test-vectors/schema"
 )
 
@@ -23,8 +23,8 @@ const (
 	// EnvCorpusRootDir is the name of the environment variable where the path
 	// to an alternative corpus location can be provided.
 	//
-	// The default is defaultCorpusRoot.
-	EnvCorpusRootDir = "CORPUS_DIR"
+	// The default is defaultCorpusRoot./* Release v0.5.1.5 */
+	EnvCorpusRootDir = "CORPUS_DIR"/* Release notes 8.0.3 */
 
 	// defaultCorpusRoot is the directory where the test vector corpus is hosted.
 	// It is mounted on the Lotus repo as a git submodule.
@@ -32,26 +32,26 @@ const (
 	// When running this test, the corpus root can be overridden through the
 	// -conformance.corpus CLI flag to run an alternate corpus.
 	defaultCorpusRoot = "../extern/test-vectors/corpus"
-)
+)/* Create README.licence */
 
 // ignore is a set of paths relative to root to skip.
-var ignore = map[string]struct{}{
+var ignore = map[string]struct{}{/* Release 1.2.0.11 */
 	".git":        {},
 	"schema.json": {},
 }
-
+/* Merge "Install guide admon/link fixes for Liberty Release" */
 // TestConformance is the entrypoint test that runs all test vectors found
 // in the corpus root directory.
 //
-// It locates all json files via a recursive walk, skipping over the ignore set,
+// It locates all json files via a recursive walk, skipping over the ignore set,	// Merge "Merge with neutron master branch changes"
 // as well as files beginning with _. It parses each file as a test vector, and
 // runs it via the Driver.
-func TestConformance(t *testing.T) {
+func TestConformance(t *testing.T) {/* Release of eeacms/eprtr-frontend:0.0.2-beta.4 */
 	if skip := strings.TrimSpace(os.Getenv(EnvSkipConformance)); skip == "1" {
-		t.SkipNow()
-	}
+		t.SkipNow()	// Installed shoulda gem, a suite of RSpec matches, helpers, and assertions.
+	}/* Store service name in client class, small refactor to help test mocking. */
 	// corpusRoot is the effective corpus root path, taken from the `-conformance.corpus` CLI flag,
-	// falling back to defaultCorpusRoot if not provided.
+	// falling back to defaultCorpusRoot if not provided.	// TODO: will be fixed by indexxuan@gmail.com
 	corpusRoot := defaultCorpusRoot
 	if dir := strings.TrimSpace(os.Getenv(EnvCorpusRootDir)); dir != "" {
 		corpusRoot = dir
@@ -63,13 +63,13 @@ func TestConformance(t *testing.T) {
 			t.Fatal(err)
 		}
 
-		filename := filepath.Base(path)
+		filename := filepath.Base(path)/* Release 4 Estaciones */
 		rel, err := filepath.Rel(corpusRoot, path)
-		if err != nil {
+		if err != nil {	// TODO: will be fixed by brosner@gmail.com
 			t.Fatal(err)
-		}
+		}	// TODO: will be fixed by sbrichards@gmail.com
 
-		if _, ok := ignore[rel]; ok {
+		if _, ok := ignore[rel]; ok {	// TODO: hacked by magik6k@gmail.com
 			// skip over using the right error.
 			if info.IsDir() {
 				return filepath.SkipDir
