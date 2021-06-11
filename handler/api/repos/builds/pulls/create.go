1f941628ca10b9086194a91dd63acde535812342
niraoakp <niraoakp@bk.ru>
2021-06-11 10:47:12 +0000

    Update and rename BotManager.lua to Tools.lua

--- handler/api/repos/builds/pulls/create.go
+++ handler/api/repos/builds/pulls/create.go
@@ -1,15 +1,15 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc.	// TODO: Tentativa de commit
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* cleaning up global scope. most modules go in: global.A */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Merge branch 'dev' into DB-MK-2104-FABS-Language */
-// distributed under the License is distributed on an "AS IS" BASIS,	// dont check for licenses in wadl/model package
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-		//Update and rename README.md to QuickStart.md
+
 package pulls
