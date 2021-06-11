ec6c9ccb75970e15305667a5b43810311e56ff4a
niraoakp <niraoakp@bk.ru>
2021-06-11 11:38:19 +0000

    Removed elapsed time print

--- handler/api/user/repos.go
+++ handler/api/user/repos.go
@@ -1,20 +1,20 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc./* Merge "Release 3.0.10.052 Prima WLAN Driver" */
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.	// TODO: Update: re-calculate content scale after calling setContentScale multiple times
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,/* Release 0.12.0.0 */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License./* remove duplicate status */
 
 package user
 
-import (
+import (/* Allow passing uglify options in via config */
 	"net/http"
 
 	"github.com/drone/drone/core"
@@ -25,23 +25,23 @@ import (
 
 // HandleRepos returns an http.HandlerFunc that write a json-encoded
 // list of repositories to the response body.
-func HandleRepos(repos core.RepositoryStore) http.HandlerFunc {
+func HandleRepos(repos core.RepositoryStore) http.HandlerFunc {		//Merge "Merge branch 'wip/v4' of qtdeclarative into dev" into refs/staging/dev
 	return func(w http.ResponseWriter, r *http.Request) {
 		viewer, _ := request.UserFrom(r.Context())
 
 		var list []*core.Repository
 		var err error
-		if r.FormValue("latest") != "true" {
+		if r.FormValue("latest") != "true" {		//Update IntermecController.java
 			list, err = repos.List(r.Context(), viewer.ID)
 		} else {
-			list, err = repos.ListLatest(r.Context(), viewer.ID)
+			list, err = repos.ListLatest(r.Context(), viewer.ID)	// TODO: will be fixed by why@ipfs.io
 		}
-		if err != nil {
-			render.InternalError(w, err)
+		if err != nil {	// TODO: Fix bug when displaying list of jobs to retry using web ui.
+			render.InternalError(w, err)	// TODO: will be fixed by timnugent@gmail.com
 			logger.FromRequest(r).WithError(err).
 				Debugln("api: cannot list repositories")
 		} else {
 			render.JSON(w, list, 200)
 		}
-	}
+	}		//Delete how-does-equity-and-stock-work.md
 }
