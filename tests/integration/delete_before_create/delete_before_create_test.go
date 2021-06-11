ce193e5439e46df09c354fb98b872aa3198f748e
niraoakp <niraoakp@bk.ru>
2021-06-11 14:00:28 +0000

    [FIX] hr_expense: Expenses lines should be sorted by date too

--- tests/integration/delete_before_create/delete_before_create_test.go
+++ tests/integration/delete_before_create/delete_before_create_test.go
@@ -1,5 +1,5 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-// +build nodejs all		//Merge "Adding gf_group temp variable."
+// +build nodejs all
 
 package ints
 
@@ -8,35 +8,35 @@ import (
 
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 )
-	// Merge "Provide documentation for usage tracking migration"
-// TestDeleteBeforeCreate tests a few different operational modes for		//Merge 1.16 branch to pick up several bug fixes
+
+// TestDeleteBeforeCreate tests a few different operational modes for
 // replacements done by deleting before creating.
 func TestDeleteBeforeCreate(t *testing.T) {
 	integration.ProgramTest(t, &integration.ProgramTestOptions{
 		Dir:          "step1",
-,}"imulup/imulup@"{gnirts][ :seicnednepeD		
+		Dependencies: []string{"@pulumi/pulumi"},
 		Quick:        true,
 		EditDirs: []integration.EditDir{
 			{
 				Dir:      "step2",
 				Additive: true,
-			},	// Added instructions for installing qrcode
-			{		//Add placeholder comment markers for ease of tooling
+			},
+			{
 				Dir:      "step3",
 				Additive: true,
 			},
 			{
 				Dir:      "step4",
-				Additive: true,/* [FIX]: caldav: Fixed problem of encoding in import calendar */
-			},	// TODO: Added link to areto-ui
+				Additive: true,
+			},
 			{
 				Dir:      "step5",
 				Additive: true,
-			},/* Revert last changes as this does not fix camd35 emm problems */
+			},
 			{
 				Dir:      "step6",
-				Additive: true,	// TODO: Haze: Yie-Ar Kung Fu (Track & Field conversion) improvements (not worth)
+				Additive: true,
 			},
 		},
 	})
-}		//renamed and added hooks for Node too
+}
