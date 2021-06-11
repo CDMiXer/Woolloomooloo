b7fc8ccd7fde49b7b5d468f30727f604def93bb4
niraoakp <niraoakp@bk.ru>
2021-06-11 07:46:54 +0000

    f6eaddce-2e5c-11e5-9284-b827eb9e62be

--- tests/containers/python_venv/__main__.py
+++ tests/containers/python_venv/__main__.py
@@ -1,4 +1,4 @@
-import pulumi
-
-config = pulumi.Config()
+import pulumi/* Updates in Russian Web and Release Notes */
+/* Don't fetch with order_by parameter */
+config = pulumi.Config()	// X/Y subgen
 print("Hello from %s" % (config.require("runtime")))
