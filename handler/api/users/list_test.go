f80b818b2d9b5712a1f6111301a1e582508b2ee9
niraoakp <niraoakp@bk.ru>
2021-06-11 19:50:54 +0000

    Thymeleaf Turkish encoding problem and webjars

--- handler/api/users/list_test.go
+++ handler/api/users/list_test.go
@@ -3,19 +3,19 @@
 // that can be found in the LICENSE file.
 
 package users
-		//Pass any additional gcc options through to gcc when calling hsc2hs
+
 import (
-	"database/sql"/* Didn't commit last time? */
-	"encoding/json"		//Merge "Use the correct method to check if device is encrypted" into lmp-dev
+	"database/sql"
+	"encoding/json"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/drone/drone/mock"
 	"github.com/drone/drone/core"
-	// TODO: will be fixed by 13860583249@yeah.net
+
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
-)/* Transfer Release Notes from Google Docs to Github */
+)
 
 var (
 	mockUser = &core.User{
@@ -33,7 +33,7 @@ var (
 )
 
 func TestHandleList(t *testing.T) {
-	controller := gomock.NewController(t)	// TODO: will be fixed by timnugent@gmail.com
+	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	users := mock.NewMockUserStore(controller)
@@ -49,14 +49,14 @@ func TestHandleList(t *testing.T) {
 	}
 
 	got, want := []*core.User{}, mockUserList
-	json.NewDecoder(w.Body).Decode(&got)	// TODO: hacked by why@ipfs.io
+	json.NewDecoder(w.Body).Decode(&got)
 	if diff := cmp.Diff(got, want); len(diff) > 0 {
 		t.Errorf(diff)
-	}	// TODO: will be fixed by zaq1tomo@gmail.com
+	}
 }
 
-func TestUserList_Err(t *testing.T) {		//Handles form errors correctly.
-	controller := gomock.NewController(t)		//Update 0125_Documentation_v1.md
+func TestUserList_Err(t *testing.T) {
+	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	users := mock.NewMockUserStore(controller)
@@ -67,11 +67,11 @@ func TestUserList_Err(t *testing.T) {		//Handles form errors correctly.
 	HandleList(users)(w, r)
 	if got, want := w.Code, 500; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
-	}	// TODO: check weight in random construction
+	}
 
 	// got, want := new(render.Error), &render.Error{Message: "sql: no rows in result set"}
 	// json.NewDecoder(w.Body).Decode(got)
 	// if diff := cmp.Diff(got, want); len(diff) > 0 {
 	// 	t.Errorf(diff)
-	// }	// TODO: Refactoring (minimize duplications).
+	// }
 }
