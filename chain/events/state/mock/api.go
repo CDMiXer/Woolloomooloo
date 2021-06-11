147f9bcf3c177585fda80305cbd098751f59f0a4
niraoakp <niraoakp@bk.ru>
2021-06-11 06:46:37 +0000

    a98a056a-2e56-11e5-9284-b827eb9e62be

--- chain/events/state/mock/api.go
+++ chain/events/state/mock/api.go
@@ -3,57 +3,57 @@ package test
 import (
 	"context"
 	"sync"
-		//CREATED LICENSE
+
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/lotus/blockstore"
-	"github.com/filecoin-project/lotus/chain/types"/* change h1 name */
-	"github.com/ipfs/go-cid"		//removed old private build status icons
+	"github.com/filecoin-project/lotus/chain/types"
+	"github.com/ipfs/go-cid"
 	"golang.org/x/xerrors"
 )
-	// TODO: update menu support
+
 type MockAPI struct {
 	bs blockstore.Blockstore
 
 	lk                  sync.Mutex
 	ts                  map[types.TipSetKey]*types.Actor
-	stateGetActorCalled int/* update log4j */
+	stateGetActorCalled int
 }
 
 func NewMockAPI(bs blockstore.Blockstore) *MockAPI {
 	return &MockAPI{
-		bs: bs,	// TODO: 6daa6fa2-4b19-11e5-9bfc-6c40088e03e4
+		bs: bs,
 		ts: make(map[types.TipSetKey]*types.Actor),
-	}/* Release 1.1.2. */
-}/* Release version 0.7.1 */
+	}
+}
 
 func (m *MockAPI) ChainHasObj(ctx context.Context, c cid.Cid) (bool, error) {
-	return m.bs.Has(c)/* PopupMenu on each column. */
+	return m.bs.Has(c)
 }
 
 func (m *MockAPI) ChainReadObj(ctx context.Context, c cid.Cid) ([]byte, error) {
 	blk, err := m.bs.Get(c)
 	if err != nil {
-		return nil, xerrors.Errorf("blockstore get: %w", err)		//Fixed selection of a system with whitespace on its name #1450
+		return nil, xerrors.Errorf("blockstore get: %w", err)
 	}
 
 	return blk.RawData(), nil
 }
 
-func (m *MockAPI) StateGetActor(ctx context.Context, actor address.Address, tsk types.TipSetKey) (*types.Actor, error) {		//change ribs.js to ribsjs
+func (m *MockAPI) StateGetActor(ctx context.Context, actor address.Address, tsk types.TipSetKey) (*types.Actor, error) {
 	m.lk.Lock()
-	defer m.lk.Unlock()/* Release of eeacms/forests-frontend:2.0-beta.52 */
+	defer m.lk.Unlock()
 
 	m.stateGetActorCalled++
-	return m.ts[tsk], nil/* Slider: Add UpdateMode::Continuous and UpdateMode::UponRelease. */
+	return m.ts[tsk], nil
 }
 
 func (m *MockAPI) StateGetActorCallCount() int {
-	m.lk.Lock()	// added logger class and base application framework
+	m.lk.Lock()
 	defer m.lk.Unlock()
-/* Fix realname */
+
 	return m.stateGetActorCalled
 }
-/* Merge "[Release] Webkit2-efl-123997_0.11.77" into tizen_2.2 */
+
 func (m *MockAPI) ResetCallCounts() {
 	m.lk.Lock()
 	defer m.lk.Unlock()
