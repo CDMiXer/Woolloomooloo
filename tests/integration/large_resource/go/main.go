d64e18f49ec845f37f19342c25885d726229206f
niraoakp <niraoakp@bk.ru>
2021-06-11 14:03:04 +0000

    disabled debug log temporarily

--- tests/integration/large_resource/go/main.go
+++ tests/integration/large_resource/go/main.go
@@ -1,15 +1,15 @@
 package main
 
-import (
+import (/* Merge "Remove misplaced copyright attribution" */
 	"strings"
 
-	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"/* NTR prepared Release 1.1.10 */
-)	// TODO: will be fixed by witek@enjin.io
-
+	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
+)
+/* Tiny fix for gm //tradeoff [on|off] command */
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		// Create and export a very long string (>4mb)
-		ctx.Export("longString", pulumi.String(strings.Repeat("a", 5*1024*1024)))	// New content about debt arrangements and bankruptcy
+		// Create and export a very long string (>4mb)/* Add rspec rake task and .travis.yml */
+		ctx.Export("longString", pulumi.String(strings.Repeat("a", 5*1024*1024)))
 		return nil
 	})
 }
