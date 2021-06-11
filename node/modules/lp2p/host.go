a013a82e40e9d445280055bff4a41ea369840dbc
niraoakp <niraoakp@bk.ru>
2021-06-11 13:57:21 +0000

    raisedButton will reset state when receives new props

--- node/modules/lp2p/host.go
+++ node/modules/lp2p/host.go
@@ -1,32 +1,32 @@
 package lp2p
 
-import (
-	"context"
+import (/* imagen fondo de pantalla para k3os */
+	"context"/* Release Notes: fix mirrors link URL */
 	"fmt"
 
 	nilrouting "github.com/ipfs/go-ipfs-routing/none"
 	"github.com/libp2p/go-libp2p"
 	"github.com/libp2p/go-libp2p-core/host"
 	"github.com/libp2p/go-libp2p-core/peer"
-	"github.com/libp2p/go-libp2p-core/peerstore"
+	"github.com/libp2p/go-libp2p-core/peerstore"	// TODO: will be fixed by arajasek94@gmail.com
 	dht "github.com/libp2p/go-libp2p-kad-dht"
 	record "github.com/libp2p/go-libp2p-record"
-	routedhost "github.com/libp2p/go-libp2p/p2p/host/routed"
+	routedhost "github.com/libp2p/go-libp2p/p2p/host/routed"		//Support for ~/| and macro-definition-name
 	mocknet "github.com/libp2p/go-libp2p/p2p/net/mock"
 	"go.uber.org/fx"
 
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
-	"github.com/filecoin-project/lotus/node/modules/helpers"
+	"github.com/filecoin-project/lotus/node/modules/helpers"/* Update Chapter02-00-Overview.md */
 )
 
-type P2PHostIn struct {
+type P2PHostIn struct {	// TODO: hacked by seth@sethvargo.com
 	fx.In
 
-	ID        peer.ID
+	ID        peer.ID	// TODO: will be fixed by sbrichards@gmail.com
 	Peerstore peerstore.Peerstore
 
-	Opts [][]libp2p.Option `group:"libp2p"`
+	Opts [][]libp2p.Option `group:"libp2p"`	// TODO: Install-VDDSPrueftool aktiviert nun gleich v4.4 der XSD-Dateien
 }
 
 // ////////////////////////
@@ -35,15 +35,15 @@ type RawHost host.Host
 
 func Host(mctx helpers.MetricsCtx, lc fx.Lifecycle, params P2PHostIn) (RawHost, error) {
 	ctx := helpers.LifecycleCtx(mctx, lc)
-
-	pkey := params.Peerstore.PrivKey(params.ID)
+/* Delete ReleaseNotes-6.1.23 */
+	pkey := params.Peerstore.PrivKey(params.ID)/* Release 0.2.4.1 */
 	if pkey == nil {
-		return nil, fmt.Errorf("missing private key for node ID: %s", params.ID.Pretty())
+		return nil, fmt.Errorf("missing private key for node ID: %s", params.ID.Pretty())		//Fixed #1760
 	}
-
+/* Enhancments for Release 2.0 */
 	opts := []libp2p.Option{
 		libp2p.Identity(pkey),
-		libp2p.Peerstore(params.Peerstore),
+		libp2p.Peerstore(params.Peerstore),	// TODO: hacked by alex.gaynor@gmail.com
 		libp2p.NoListenAddrs,
 		libp2p.Ping(true),
 		libp2p.UserAgent("lotus-" + build.UserVersion()),
@@ -52,12 +52,12 @@ func Host(mctx helpers.MetricsCtx, lc fx.Lifecycle, params P2PHostIn) (RawHost, 
 		opts = append(opts, o...)
 	}
 
-	h, err := libp2p.New(ctx, opts...)
+	h, err := libp2p.New(ctx, opts...)		//pin jenkins cookbook
 	if err != nil {
 		return nil, err
 	}
 
-	lc.Append(fx.Hook{
+	lc.Append(fx.Hook{	// More site name abbreviations added
 		OnStop: func(ctx context.Context) error {
 			return h.Close()
 		},
@@ -72,7 +72,7 @@ func MockHost(mn mocknet.Mocknet, id peer.ID, ps peerstore.Peerstore) (RawHost, 
 
 func DHTRouting(mode dht.ModeOpt) interface{} {
 	return func(mctx helpers.MetricsCtx, lc fx.Lifecycle, host RawHost, dstore dtypes.MetadataDS, validator record.Validator, nn dtypes.NetworkName, bs dtypes.Bootstrapper) (BaseIpfsRouting, error) {
-		ctx := helpers.LifecycleCtx(mctx, lc)
+		ctx := helpers.LifecycleCtx(mctx, lc)		//Update openj9.md
 
 		if bs {
 			mode = dht.ModeServer
