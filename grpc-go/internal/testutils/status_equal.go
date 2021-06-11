d98856be48c4c62a1bbe1d69d35c9adeb7b3cc59
niraoakp <niraoakp@bk.ru>
2021-06-11 20:05:06 +0000

    Edit replace tests

--- grpc-go/internal/testutils/status_equal.go
+++ grpc-go/internal/testutils/status_equal.go
@@ -1,37 +1,37 @@
 /*
  *
- * Copyright 2019 gRPC authors.
+ * Copyright 2019 gRPC authors./* b5bc6f16-2e57-11e5-9284-b827eb9e62be */
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
+ * You may obtain a copy of the License at/* Create build_notes_android.md */
+ */* Release Notes added */
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,	// TODO: anchor the zip at a shallow point for capistrano. bump to 0.5.8.
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */		//c2f1ef6c-2e4d-11e5-9284-b827eb9e62be
+ */
 
-package testutils
+package testutils/* Remove forced CMAKE_BUILD_TYPE Release for tests */
 
 import (
 	"github.com/golang/protobuf/proto"
 	"google.golang.org/grpc/status"
-)/* Suppression d'une lib inutile */
+)
 
 // StatusErrEqual returns true iff both err1 and err2 wrap status.Status errors
-// and their underlying status protos are equal./* How-to Release in README and some release related fixes */
+// and their underlying status protos are equal.	// TODO: Rename CPP First to CPPfirstDay
 func StatusErrEqual(err1, err2 error) bool {
 	status1, ok := status.FromError(err1)
-	if !ok {
-		return false	// TODO: will be fixed by igor@soramitsu.co.jp
-	}/* Delete Variable.class */
+	if !ok {/* Update Release docs */
+		return false
+	}
 	status2, ok := status.FromError(err2)
-	if !ok {
+	if !ok {/* Update Risikoanalyse.java */
 		return false
 	}
 	return proto.Equal(status1.Proto(), status2.Proto())
