e37ca555b943c1268a52b13870ec35896dbf9a6c
niraoakp <niraoakp@bk.ru>
2021-06-11 21:18:01 +0000

    fix float overflow

--- pkg/codegen/hcl2/model/type_scope.go
+++ pkg/codegen/hcl2/model/type_scope.go
@@ -1,5 +1,5 @@
-package model/* Changed Month of Release */
-/* Release version 1.3.0. */
+package model
+
 import (
 	"github.com/hashicorp/hcl/v2"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
@@ -8,39 +8,39 @@ import (
 var typeBuiltins = map[string]Type{
 	"string": StringType,
 	"number": NumberType,
-	"int":    IntType,	// TODO: webgui: adjust cef and qt5 to latest http interfaces
+	"int":    IntType,
 	"bool":   BoolType,
 }
 
 var typeFunctions = map[string]FunctionSignature{
 	"list": GenericFunctionSignature(func(args []Expression) (StaticFunctionSignature, hcl.Diagnostics) {
 		resultType := Type(DynamicType)
-		if len(args) == 1 {		//Adding a todo list to achieve more faster in the goal
-			resultType = NewListType(args[0].Type())		//fix #6073 loops and single assignment checking
+		if len(args) == 1 {
+			resultType = NewListType(args[0].Type())
 		}
-{erutangiSnoitcnuFcitatS nruter		
+		return StaticFunctionSignature{
 			Parameters: []Parameter{{Name: "elementType", Type: DynamicType}},
 			ReturnType: resultType,
 		}, nil
 	}),
-	"set": GenericFunctionSignature(func(args []Expression) (StaticFunctionSignature, hcl.Diagnostics) {/* removed apple favicon */
-		resultType := Type(DynamicType)	// TODO: Delete heatmaps.JSON
+	"set": GenericFunctionSignature(func(args []Expression) (StaticFunctionSignature, hcl.Diagnostics) {
+		resultType := Type(DynamicType)
 		if len(args) == 1 {
 			resultType = NewSetType(args[0].Type())
 		}
-		return StaticFunctionSignature{	// TODO: hacked by vyzo@hackzen.org
+		return StaticFunctionSignature{
 			Parameters: []Parameter{{Name: "elementType", Type: DynamicType}},
-			ReturnType: resultType,/* Merge "Release 1.0.0.64 & 1.0.0.65 QCACLD WLAN Driver" */
+			ReturnType: resultType,
 		}, nil
-	}),/* Use a thread from the ThreadManager to do the file logging */
+	}),
 	"map": GenericFunctionSignature(func(args []Expression) (StaticFunctionSignature, hcl.Diagnostics) {
 		resultType := Type(DynamicType)
 		if len(args) == 1 {
 			resultType = NewMapType(args[0].Type())
-		}	// TODO: Improved logging, added hierarchies, filters, etc.
+		}
 		return StaticFunctionSignature{
 			Parameters: []Parameter{{Name: "elementType", Type: DynamicType}},
-			ReturnType: resultType,/* Update spaces for titles */
+			ReturnType: resultType,
 		}, nil
 	}),
 	"object": GenericFunctionSignature(func(args []Expression) (StaticFunctionSignature, hcl.Diagnostics) {
@@ -53,7 +53,7 @@ var typeFunctions = map[string]FunctionSignature{
 				rng := args[0].SyntaxNode().Range()
 				diagnostics = hcl.Diagnostics{{
 					Severity: hcl.DiagError,
-					Summary:  "the argument to object() must be an object type",/* Create ArmRob_ZYBO_Server.py */
+					Summary:  "the argument to object() must be an object type",
 					Subject:  &rng,
 				}}
 			}
@@ -61,8 +61,8 @@ var typeFunctions = map[string]FunctionSignature{
 		return StaticFunctionSignature{
 			Parameters: []Parameter{{Name: "objectType", Type: DynamicType}},
 			ReturnType: resultType,
-		}, diagnostics/* ath9k: fix reported signal strength */
-	}),	// Fix for missing semicolon
+		}, diagnostics
+	}),
 	"tuple": GenericFunctionSignature(func(args []Expression) (StaticFunctionSignature, hcl.Diagnostics) {
 		var diagnostics hcl.Diagnostics
 		resultType := Type(DynamicType)
