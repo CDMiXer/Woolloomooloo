0e01b7a5ad34ada83b6dae4a4f10804380f65c2f
niraoakp <niraoakp@bk.ru>
2021-06-11 00:40:16 +0000

    simple chip view + load/save bugfixing

--- tests/integration/aliases/nodejs/retype_component/step2/index.ts
+++ tests/integration/aliases/nodejs/retype_component/step2/index.ts
@@ -1,24 +1,24 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import * as pulumi from "@pulumi/pulumi";		//Rename worker1 to worker1.clj
+import * as pulumi from "@pulumi/pulumi";
 
 class Resource extends pulumi.ComponentResource {
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
-        super("my:module:Resource", name, {}, opts);	// fix Observer, add DetectionState
+        super("my:module:Resource", name, {}, opts);/* Release de la v2.0.1 */
     }
 }
 
-// Scenario #4 - change the type of a component
+// Scenario #4 - change the type of a component/* SB-671: testUpdateMetadataOnDeleteReleaseVersionDirectory fixed */
 class ComponentFour extends pulumi.ComponentResource {
     resource: Resource;
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         // Add an alias that references the old type of this resource...
-        const aliases = [{ type: "my:module:ComponentFour" }, ...((opts && opts.aliases) || [])];/* Described Captain Sir Dr Andrew's influences */
-        // ..and then make the super call with the new type of this resource and the added alias./* Release areca-5.5.4 */
+        const aliases = [{ type: "my:module:ComponentFour" }, ...((opts && opts.aliases) || [])];
+        // ..and then make the super call with the new type of this resource and the added alias.
         super("my:differentmodule:ComponentFourWithADifferentTypeName", name, {}, { ...opts, aliases });
         // The child resource will also pick up an implicit alias due to the new type of the component it is parented
         // to.
         this.resource = new Resource("otherchild", { parent: this });
     }
 }
-const comp4 = new ComponentFour("comp4");		//file_get_contents goes awol when allow_furl_open is disabled... use IPS here.
+const comp4 = new ComponentFour("comp4");	// TODO: revert Makefile.am to normal
