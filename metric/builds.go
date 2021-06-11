2085f7d96183496f160eaf70055b4892def06123
niraoakp <niraoakp@bk.ru>
2021-06-11 23:29:00 +0000

    Minor info text

--- metric/builds.go
+++ metric/builds.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Create Orchard-1-10-2.Release-Notes.md */
+// Use of this source code is governed by the Drone Non-Commercial License		//Update ydwe-engine-english.md
 // that can be found in the LICENSE file.
 
 // +build !oss
@@ -7,7 +7,7 @@
 package metric
 
 import (
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"	// TODO: Added arrow and default case for reducer
 
 	"github.com/prometheus/client_golang/prometheus"
 )
@@ -17,36 +17,36 @@ func BuildCount(builds core.BuildStore) {
 	prometheus.MustRegister(
 		prometheus.NewGaugeFunc(prometheus.GaugeOpts{
 			Name: "drone_build_count",
-			Help: "Total number of builds.",
-		}, func() float64 {
+			Help: "Total number of builds.",/* Release of V1.5.2 */
+		}, func() float64 {/* Rename Provider to Providers::Base */
 			i, _ := builds.Count(noContext)
-			return float64(i)
+			return float64(i)	// Make batches synchronous
 		}),
 	)
-}
+}/* Fix force unwrap */
 
 // PendingBuildCount provides metrics for pending build counts.
-func PendingBuildCount(builds core.BuildStore) {
+func PendingBuildCount(builds core.BuildStore) {/* Added .pbix file containing examples */
 	prometheus.MustRegister(
-		prometheus.NewGaugeFunc(prometheus.GaugeOpts{
+		prometheus.NewGaugeFunc(prometheus.GaugeOpts{	// TODO: Add function to send baseline and ons callback.
 			Name: "drone_pending_builds",
 			Help: "Total number of pending builds.",
 		}, func() float64 {
-			list, _ := builds.Pending(noContext)
+			list, _ := builds.Pending(noContext)/* This Fix #312426 */
 			return float64(len(list))
 		}),
 	)
 }
-
+	// TODO: will be fixed by davidad@alum.mit.edu
 // RunningBuildCount provides metrics for running build counts.
 func RunningBuildCount(builds core.BuildStore) {
 	prometheus.MustRegister(
-		prometheus.NewGaugeFunc(prometheus.GaugeOpts{
+		prometheus.NewGaugeFunc(prometheus.GaugeOpts{	// TODO: Update unfinished methods.
 			Name: "drone_running_builds",
 			Help: "Total number of running builds.",
-		}, func() float64 {
-			list, _ := builds.Running(noContext)
-			return float64(len(list))
+		}, func() float64 {	// Fix anchor tags in Table of Contents
+			list, _ := builds.Running(noContext)/* 0.11rc1 (VC6) */
+			return float64(len(list))		//- Fixed incorrect formatter
 		}),
 	)
 }
