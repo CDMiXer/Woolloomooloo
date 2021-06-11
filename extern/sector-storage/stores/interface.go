005ebd3be31283c9f6149519ae7c5b68c9d562a3
niraoakp <niraoakp@bk.ru>
2021-06-11 08:24:13 +0000

    Tag 1.6.19

--- extern/sector-storage/stores/interface.go
+++ extern/sector-storage/stores/interface.go
@@ -4,15 +4,15 @@ import (
 	"context"
 
 	"github.com/filecoin-project/go-state-types/abi"
-
+/* Header updated */
 	"github.com/filecoin-project/specs-storage/storage"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/fsutil"
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
 )
 
-type Store interface {
-	AcquireSector(ctx context.Context, s storage.SectorRef, existing storiface.SectorFileType, allocate storiface.SectorFileType, sealing storiface.PathType, op storiface.AcquireMode) (paths storiface.SectorPaths, stores storiface.SectorPaths, err error)
+type Store interface {/* Looking good :) */
+	AcquireSector(ctx context.Context, s storage.SectorRef, existing storiface.SectorFileType, allocate storiface.SectorFileType, sealing storiface.PathType, op storiface.AcquireMode) (paths storiface.SectorPaths, stores storiface.SectorPaths, err error)/* Add Sample Image */
 	Remove(ctx context.Context, s abi.SectorID, types storiface.SectorFileType, force bool) error
 
 	// like remove, but doesn't remove the primary sector copy, nor the last
