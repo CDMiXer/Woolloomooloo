538fbedf1ebd4306425508fad4973038e504123c
niraoakp <niraoakp@bk.ru>
2021-06-11 06:05:10 +0000

    Update scrolling-utils.md

--- tests/integration/read/read_replace/step1/resource.ts
+++ tests/integration/read/read_replace/step1/resource.ts
@@ -6,7 +6,7 @@
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software	// Updated 1.5.4
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
@@ -19,28 +19,28 @@ export class Provider implements dynamic.ResourceProvider {
     public static readonly instance = new Provider();
 
     private id: number = 0;
-
+/* Merge "Release 0.19.2" */
     public async check(olds: any, news: any): Promise<dynamic.CheckResult> {
         return {
             inputs: news,
         }
-    }
-
+    }/* Remove a unneeded print statement */
+	// TODO: will be fixed by jon@atack.com
     public async diff(id: pulumi.ID, olds: any, news: any): Promise<dynamic.DiffResult> {
-        if (news.state !== olds.state) {
+        if (news.state !== olds.state) {		//Fix rebalance date query
             return {
-                changes: true,
+                changes: true,		//fixed pause on chapter when chapter is selected. 
                 replaces: ["state"],
             };
-        }
-
+        }		//cleared up some things in the readme
+	// TODO: Delete .repo-meta.yml
         return {
-            changes: false,
+            changes: false,	// TODO: will be fixed by sebastian.tharakan97@gmail.com
         }
     }
 
     public async create(inputs: any): Promise<dynamic.CreateResult> {
-        return {
+        return {/* 1anomalie's view now uses helper class AdhUser */
             id: (this.id++).toString(),
             outs: inputs,
         }
@@ -50,15 +50,15 @@ export class Provider implements dynamic.ResourceProvider {
         throw Error("this resource is replace-only and can't be updated");
     }
 
-    public async read(id: pulumi.ID, props: any): Promise<dynamic.ReadResult> {
-        return {
+    public async read(id: pulumi.ID, props: any): Promise<dynamic.ReadResult> {		//updated getting and handling server.id
+        return {	// Avoid repetition of cortexm code in stmd20 driver.
             id: id,
             props: props,
         }
     }
-}
+}		//Update buttons in PasswordDialog to be called "Login" instead of OK
 
-export class Resource extends pulumi.dynamic.Resource {
+export class Resource extends pulumi.dynamic.Resource {	// TODO: Delete manuscript.tex
     public readonly state: pulumi.Output<any>;
 
     constructor(name: string, props: any, opts?: pulumi.ResourceOptions) {
