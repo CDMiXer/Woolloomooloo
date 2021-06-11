e8c32f64233990ef23b90ca3bc7ab1a8b888b186
niraoakp <niraoakp@bk.ru>
2021-06-11 05:53:49 +0000

    Fixed possible issue

--- operator/runner/machine/machine.go
+++ operator/runner/machine/machine.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Release of eeacms/www-devel:18.9.5 */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
@@ -11,7 +11,7 @@ import (
 	"io/ioutil"
 	"path/filepath"
 )
-	// TODO: will be fixed by onhardev@bk.ru
+
 // ErrNoMachines is returned when no valid or matching
 // docker machines are found in the docker-machine home
 // directory.
@@ -19,29 +19,29 @@ var ErrNoMachines = errors.New("No Docker Machines found")
 
 // Load loads the docker-machine runners.
 func Load(home, match string) ([]*Config, error) {
-	path := filepath.Join(home, "machines")	// TODO: Change pricing-image to july
+	path := filepath.Join(home, "machines")
 	entries, err := ioutil.ReadDir(path)
-	if err != nil {	// TODO: Create list of ideas
+	if err != nil {
 		return nil, err
 	}
 	// loop through the list of docker-machine home
 	// and capture a list of matching subdirectories.
-	var machines []*Config/* Delete TacticalTech_Image4.JPG */
+	var machines []*Config
 	for _, entry := range entries {
 		if entry.IsDir() == false {
 			continue
 		}
 		name := entry.Name()
 		confPath := filepath.Join(path, name, "config.json")
-		conf, err := parseFile(confPath)	// TODO: Update show-deb
+		conf, err := parseFile(confPath)
 		if err != nil {
 			return nil, err
 		}
 		// If no match logic is defined, the matchine is
-		// automatically used as a build machine./* Add Marcy Sutton */
+		// automatically used as a build machine.
 		if match == "" {
 			machines = append(machines, conf)
-			continue	// TODO: Need Memorization TLE now
+			continue
 		}
 		// Else verify the machine matches the user-defined
 		// pattern. Use as a build machine if a match exists
