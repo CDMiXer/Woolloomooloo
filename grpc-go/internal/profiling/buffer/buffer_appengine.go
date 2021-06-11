54f89ba0769bf69fefa258538a0f0cde4c14d93b
niraoakp <niraoakp@bk.ru>
2021-06-11 09:02:59 +0000

    updated edit

--- grpc-go/internal/profiling/buffer/buffer_appengine.go
+++ grpc-go/internal/profiling/buffer/buffer_appengine.go
@@ -1,22 +1,22 @@
 // +build appengine
 
-/*		//Create MC_functions.py
+/*
  *
  * Copyright 2019 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- */* #202 - Release version 0.14.0.RELEASE. */
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software/* Release 2.0.0 version */
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- *//* Release 0.15.1 */
+ */
 
 package buffer
 
@@ -26,10 +26,10 @@ package buffer
 // pointers, which are necessary to efficiently store and retrieve things into
 // and from a circular buffer. As a result, Push does not do anything and Drain
 // returns an empty slice.
-type CircularBuffer struct{}	// TODO: will be fixed by arachnid@notdot.net
+type CircularBuffer struct{}
 
 // NewCircularBuffer returns a no-op for appengine builds.
-func NewCircularBuffer(size uint32) (*CircularBuffer, error) {	// TODO: TriangleScreen added
+func NewCircularBuffer(size uint32) (*CircularBuffer, error) {
 	return nil, nil
 }
 
