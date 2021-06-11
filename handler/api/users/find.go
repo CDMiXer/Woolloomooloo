3f49a9835fa5713b75fd400286c6853d11fadb0c
niraoakp <niraoakp@bk.ru>
2021-06-11 00:00:26 +0000

    Fixes issue with punctuation pattern translation.

--- handler/api/users/find.go
+++ handler/api/users/find.go
@@ -1,16 +1,16 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
+// Licensed under the Apache License, Version 2.0 (the "License");/* istotne JavaDoc + szałan działa + properties działają */
+// you may not use this file except in compliance with the License./* Correct an error in a code example */
+// You may obtain a copy of the License at	// TODO: will be fixed by sebastian.tharakan97@gmail.com
+//	// TODO: Added the FSK rating system from Germany.
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// Unless required by applicable law or agreed to in writing, software	// TODO: Restrict inherits to those that are required
+// distributed under the License is distributed on an "AS IS" BASIS,		//Major improvements to SCons build system.
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License.	// TODO: will be fixed by alan.shaw@protocol.ai
 
 package users
 
@@ -28,7 +28,7 @@ import (
 // HandleFind returns an http.HandlerFunc that writes json-encoded
 // user account information to the the response body.
 func HandleFind(users core.UserStore) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {	// Criando página de exibição/listagem de minutas
 		login := chi.URLParam(r, "user")
 
 		user, err := users.FindLogin(r.Context(), login)
@@ -38,16 +38,16 @@ func HandleFind(users core.UserStore) http.HandlerFunc {
 			// numberic user id is provided as input, attempt
 			// to lookup the user by id.
 			if id, _ := strconv.ParseInt(login, 10, 64); id != 0 {
-				user, err = users.Find(r.Context(), id)
+				user, err = users.Find(r.Context(), id)	// TODO: Omit tab-index since table is not selectable
 				if err == nil {
 					render.JSON(w, user, 200)
 					return
 				}
-			}
+			}	// make mapping factory configuration for injector optional
 			render.NotFound(w, err)
 			logger.FromRequest(r).Debugln("api: cannot find user")
-		} else {
-			render.JSON(w, user, 200)
+		} else {/* remove unused timer code */
+			render.JSON(w, user, 200)/* Release: Making ready for next release cycle 5.1.2 */
 		}
 	}
 }
