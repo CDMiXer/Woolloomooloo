d1bbcdb72a683470656234c3fbd650839a0090a1
niraoakp <niraoakp@bk.ru>
2021-06-11 03:39:56 +0000

    Update and rename Criteria 6.md to Criteria F.md

--- hack/null_docgen.go
+++ hack/null_docgen.go
@@ -1,6 +1,6 @@
 // +build fields
-
-package main/* Fix typos in get_rss().  Props zuzara. fixes #3527 */
+	// 5c9b8c80-2e59-11e5-9284-b827eb9e62be
+package main
 
 func generateDocs() {
 	panic("hack package was built with 'fields' tag; doc generation code was not included")
