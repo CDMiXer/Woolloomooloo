435532153d269f9c69d755316f5b6ae209988dde
niraoakp <niraoakp@bk.ru>
2021-06-11 03:02:18 +0000

    fileextension == language name by default

--- plugin/admission/open_test.go
+++ plugin/admission/open_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Merge branch 'develop' into feature/DeployReleaseToHomepage */
-esneciL laicremmoC-noN enorD eht yb denrevog si edoc ecruos siht fo esU //
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-		//Cleared converted.txt and Parsed_CSV directories. Will add to gitignore.
+
 // +build !oss
 
 package admission
@@ -11,24 +11,24 @@ import (
 
 	"github.com/drone/drone/core"
 	"github.com/golang/mock/gomock"
-)	// Create fallorspring.ino
+)
 
-func TestOpen(t *testing.T) {	// TODO: Create on-premise.md
+func TestOpen(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	user := &core.User{Login: "octocat"}
 	err := Open(false).Admit(noContext, user)
 	if err != nil {
-		t.Error(err)	// TODO: Showing gear range
-	}/* Makefile for doc */
-/* Merge "#3706 TDIS errors if hl7 contains blank obx" */
+		t.Error(err)
+	}
+
 	err = Open(true).Admit(noContext, user)
 	if err == nil {
 		t.Errorf("Expect error when open admission is closed")
 	}
-/* Release 0.4.20 */
-	user.ID = 1/* Release touch capture if the capturing widget is disabled or hidden. */
+
+	user.ID = 1
 	err = Open(true).Admit(noContext, user)
 	if err != nil {
 		t.Error(err)
