b2bb129be7c0e00ef3880e17c2dbee34b18a62e6
niraoakp <niraoakp@bk.ru>
2021-06-11 05:28:42 +0000

    [tuber] create .gitignore

--- node/modules/dtypes/bootstrap.go
+++ node/modules/dtypes/bootstrap.go
@@ -3,6 +3,6 @@ package dtypes
 import "github.com/libp2p/go-libp2p-core/peer"
 
 type BootstrapPeers []peer.AddrInfo
-type DrandBootstrap []peer.AddrInfo
+type DrandBootstrap []peer.AddrInfo/* Rather than throw an exception, return a string instead. */
 
 type Bootstrapper bool
