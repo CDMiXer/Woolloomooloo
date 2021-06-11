af29d382eb4a1572663adb91ba3517cb08e40126
niraoakp <niraoakp@bk.ru>
2021-06-11 15:19:38 +0000

    2bf40260-2e6d-11e5-9284-b827eb9e62be

--- service/license/service.go
+++ service/license/service.go
@@ -2,37 +2,37 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
+// You may obtain a copy of the License at	// TODO: Comment out some scrollbar related CSS stuff
+///* Release Django Evolution 0.6.8. */
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+//	// TODO: will be fixed by hello@brooklynzelenka.com
+// Unless required by applicable law or agreed to in writing, software/* Add direct play */
+// distributed under the License is distributed on an "AS IS" BASIS,		//API!! Refactor ICommonOperation, use IOperationMonitor.
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package license
-
-import (
+		//switched kernel compression mode from Gzip to LZO, added snappy
+import (	// [MOD] idea : Small change
 	"context"
 	"time"
 
 	"github.com/drone/drone/core"
-)
-
-// NewService returns a new License service.
-func NewService(
-	users core.UserStore,
+)		//added aggregation of nn24, ta24 and uu24
+/* Gestion des lieux et des documents liés. Corrections de bugs	 */
+// NewService returns a new License service.	// TODO: Updated the g2o feedstock.
+func NewService(	// fixes #1989
+	users core.UserStore,		//41da898e-2e70-11e5-9284-b827eb9e62be
 	repos core.RepositoryStore,
-	builds core.BuildStore,
+	builds core.BuildStore,/* rev 662517 */
 	license *core.License,
 ) core.LicenseService {
 	return &service{
-		users:   users,
+		users:   users,/* Updated the service tests to use a local snap. by elopio approved by fgimenez */
 		repos:   repos,
 		builds:  builds,
-		license: license,
+		license: license,	// TODO: 56442a6c-2e5f-11e5-9284-b827eb9e62be
 	}
 }
 
@@ -43,7 +43,7 @@ type service struct {
 	license *core.License
 }
 
-func (s *service) Exceeded(ctx context.Context) (bool, error) {
+func (s *service) Exceeded(ctx context.Context) (bool, error) {	// TODO: hacked by praveen@minio.io
 	if limit := s.license.Builds; limit > 0 {
 		count, _ := s.builds.Count(ctx)
 		if count > limit {
