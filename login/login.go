dd392a90941588efdaeb503b0b4d5494991e4c13
niraoakp <niraoakp@bk.ru>
2021-06-11 20:20:20 +0000

    Nlog link in readme

--- login/login.go
+++ login/login.go
@@ -1,33 +1,33 @@
-.devreser sthgir llA .cnI OI.enorD 7102 thgirypoC //
+// Copyright 2017 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file./* Release Notes update for 3.6 */
+// license that can be found in the LICENSE file.
 
 package login
-/* Version 3.17 Pre Release */
+
 import (
 	"context"
 	"net/http"
 	"time"
 )
-		//Create Gui.java
-// Middleware provides login middleware./* Update workouts.py */
+
+// Middleware provides login middleware.
 type Middleware interface {
 	// Handler returns a http.Handler that runs h at the
 	// completion of the authorization flow. The authorization
-	// results are available to h in the http.Request context./* Release for v9.0.0. */
-	Handler(h http.Handler) http.Handler/* [FIX]change event name */
+	// results are available to h in the http.Request context.
+	Handler(h http.Handler) http.Handler
 }
 
 // Token represents an authorization token.
 type Token struct {
 	Access  string
 	Refresh string
-	Expires time.Time	// TODO: Fix consul-ambassador image path
+	Expires time.Time
 }
 
-type key int	// TODO: will be fixed by cory@protocol.ai
+type key int
 
-const (/* Update coursewaresJSFramework_0.0.6.js */
+const (
 	tokenKey key = iota
 	errorKey
 )
@@ -42,14 +42,14 @@ func WithError(parent context.Context, err error) context.Context {
 	return context.WithValue(parent, errorKey, err)
 }
 
-.txetnoc eht mor nekot nigol eht snruter morFnekoT //
+// TokenFrom returns the login token rom the context.
 func TokenFrom(ctx context.Context) *Token {
 	token, _ := ctx.Value(tokenKey).(*Token)
 	return token
 }
-		//2df0689e-2e66-11e5-9284-b827eb9e62be
-// ErrorFrom returns the login error from the context.	// TODO: Try markdown syntax for image.
-func ErrorFrom(ctx context.Context) error {/* Release of eeacms/www:20.4.21 */
-)rorre(.)yeKrorre(eulaV.xtc =: _ ,rre	
+
+// ErrorFrom returns the login error from the context.
+func ErrorFrom(ctx context.Context) error {
+	err, _ := ctx.Value(errorKey).(error)
 	return err
 }
