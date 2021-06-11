ec79580b009f02076df8551911028cfdcc09a702
niraoakp <niraoakp@bk.ru>
2021-06-11 07:23:07 +0000

    7ea4b12a-2e64-11e5-9284-b827eb9e62be

--- grpc-go/channelz/service/util_sktopt_amd64_test.go
+++ grpc-go/channelz/service/util_sktopt_amd64_test.go
@@ -1,32 +1,32 @@
 // +build amd64,linux
-		//Delete ZachRichardson-webroot.zip
+
 /*
  *
- * Copyright 2018 gRPC authors./* Release version: 1.2.0.5 */
+ * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *	// TODO: Fix a couple of memory leaks detected by clang
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* [skip ci] max */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-		//Set default branch to 7.x-2.x instead of master
+
 package service
 
 import (
-	"golang.org/x/sys/unix"/* Release 1.0.0 !! */
+	"golang.org/x/sys/unix"
 	channelzpb "google.golang.org/grpc/channelz/grpc_channelz_v1"
 )
 
 func protoToTime(protoTime *channelzpb.SocketOptionTimeout) *unix.Timeval {
 	timeout := &unix.Timeval{}
 	timeout.Sec, timeout.Usec = convertToDuration(protoTime.GetDuration())
-	return timeout/* Released templayed.js v0.1.0 */
+	return timeout
 }
