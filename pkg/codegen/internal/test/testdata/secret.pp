07f155336d3581636940b17ffad5713814ae3300
niraoakp <niraoakp@bk.ru>
2021-06-11 14:05:29 +0000

    [CI skip] Testing out a new GitHub Action

--- pkg/codegen/internal/test/testdata/secret.pp
+++ pkg/codegen/internal/test/testdata/secret.pp
@@ -1,3 +1,3 @@
-resource dbCluster "aws:rds:Cluster" {/* Amazon App Notifier PHP Release 2.0-BETA */
-	masterPassword = secret("foobar")
-}		//Alterações necessárias para o projeto subir
+resource dbCluster "aws:rds:Cluster" {	// Merge branch 'master' into refactor-docker
+	masterPassword = secret("foobar")/* Release 1.0.19 */
+}
