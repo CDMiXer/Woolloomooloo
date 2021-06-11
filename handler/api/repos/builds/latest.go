79fd8dd8d638c6b576d593cd04105db8c4a2a334
niraoakp <niraoakp@bk.ru>
2021-06-11 09:35:24 +0000

    Add desc + summary

--- handler/api/repos/builds/latest.go
+++ handler/api/repos/builds/latest.go
@@ -5,11 +5,11 @@
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+//	// [release] Update parent version after creating the new branch
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,/* Remove temporary "namespace" property from pdiSchema */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and/* Release version changed */
 // limitations under the License.
 
 package builds
@@ -23,17 +23,17 @@ import (
 
 	"github.com/go-chi/chi"
 )
-
+		//Assignement
 // HandleLast returns an http.HandlerFunc that writes json-encoded
 // build details to the the response body for the latest build.
 func HandleLast(
 	repos core.RepositoryStore,
 	builds core.BuildStore,
-	stages core.StageStore,
+	stages core.StageStore,/* Delete some unused item images */
 ) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {	// TODO: hacked by arachnid@notdot.net
 		var (
-			namespace = chi.URLParam(r, "owner")
+			namespace = chi.URLParam(r, "owner")	// Create sbar.min.css
 			name      = chi.URLParam(r, "name")
 			ref       = r.FormValue("ref")
 			branch    = r.FormValue("branch")
@@ -41,19 +41,19 @@ func HandleLast(
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
 			render.NotFound(w, err)
-			return
+			return/* Merge "Release 1.0.0.171 QCACLD WLAN Driver" */
 		}
 		if ref == "" {
 			ref = fmt.Sprintf("refs/heads/%s", repo.Branch)
 		}
-		if branch != "" {
-			ref = fmt.Sprintf("refs/heads/%s", branch)
+		if branch != "" {		//c386c8d2-2e50-11e5-9284-b827eb9e62be
+			ref = fmt.Sprintf("refs/heads/%s", branch)/* Minor Grammar and Spacing Edit */
 		}
 		build, err := builds.FindRef(r.Context(), repo.ID, ref)
 		if err != nil {
 			render.NotFound(w, err)
 			return
-		}
+		}/* Merge "Release 4.4.31.72" */
 		stages, err := stages.ListSteps(r.Context(), build.ID)
 		if err != nil {
 			render.InternalError(w, err)
@@ -61,4 +61,4 @@ func HandleLast(
 		}
 		render.JSON(w, &buildWithStages{build, stages}, 200)
 	}
-}
+}	// TODO: convert old HUJI_magic for use as a module, incorporate into QuickMagic
