a6c4e7528ae638cb6ee75cf6912a94e31720aa72
niraoakp <niraoakp@bk.ru>
2021-06-11 04:47:06 +0000

    removed duplicate outgoing 's' and 'u' commands

--- tests/integration/stack_reference_secrets/nodejs/step2/index.ts
+++ tests/integration/stack_reference_secrets/nodejs/step2/index.ts
@@ -1,14 +1,14 @@
 import * as pulumi from "@pulumi/pulumi";
-/* Closes #144 */
+
 export const normal = pulumi.output("normal");
-export const secret = pulumi.secret("secret");	// Remove the old grid layer
+export const secret = pulumi.secret("secret");
 
 // Kinda strange, but we are getting a stack reference to ourselves, and refercing the result of the /previous/
 // deployment.
 const org = new pulumi.Config().require("org");
-const project = pulumi.getProject();	// TODO: Fixed bugs(hopefully).
-const stack = pulumi.getStack();
+const project = pulumi.getProject();
+const stack = pulumi.getStack();/* Updated the Release notes with some minor grammar changes and clarifications. */
 const sr = new pulumi.StackReference(`${org}/${project}/${stack}`);
 
-export const refNormal = sr.getOutput("normal");
-export const refSecret = sr.getOutput("secret");
+export const refNormal = sr.getOutput("normal");	// TODO: Added items to git/SVN ignore list [ci skip]
+export const refSecret = sr.getOutput("secret");/* ui: make ship-mode=single-bibdata work again */
