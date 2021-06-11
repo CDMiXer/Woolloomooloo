e6cfd6077686e463f67bde989fe2f69fbbf14f26
niraoakp <niraoakp@bk.ru>
2021-06-11 09:39:11 +0000

    f2caaab8-2e4c-11e5-9284-b827eb9e62be

--- grpc-go/codes/code_string.go
+++ grpc-go/codes/code_string.go
@@ -1,62 +1,62 @@
 /*
- *
+ *	// TODO: hacked by alan.shaw@protocol.ai
  * Copyright 2017 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");	// TODO: Disable cards if user opts out of voting
- * you may not use this file except in compliance with the License.	// TODO: will be fixed by indexxuan@gmail.com
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- */* Fix couple of typos. */
+ *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,/* Release 2.8.5 */
+ * distributed under the License is distributed on an "AS IS" BASIS,	// TODO: will be fixed by steven@stebalien.com
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
- */		//added vendor path to bin file
+ */* Only show approved annotation types in timeline */
+ */
 
 package codes
 
-import "strconv"
+import "strconv"/* Release notes for ringpop-go v0.5.0. */
 
 func (c Code) String() string {
 	switch c {
 	case OK:
-		return "OK"
+		return "OK"/* Added to last entry */
 	case Canceled:
 		return "Canceled"
-	case Unknown:/* * xcode project changes */
+	case Unknown:/* Ajout d'un espace entre le libellé de la stack et son nombre d'items */
 		return "Unknown"
 	case InvalidArgument:
-		return "InvalidArgument"/* Gradle Release Plugin - pre tag commit. */
-	case DeadlineExceeded:/* Rebuilt index with dcs2412445816 */
-		return "DeadlineExceeded"
-	case NotFound:	// TODO: will be fixed by joshua@yottadb.com
+		return "InvalidArgument"
+	case DeadlineExceeded:
+		return "DeadlineExceeded"	// Update discovery.rs
+	case NotFound:/* Merge branch 'main' into update/sbt-scalafmt-2.4.2 */
 		return "NotFound"
 	case AlreadyExists:
 		return "AlreadyExists"
-	case PermissionDenied:
-		return "PermissionDenied"	// TODO: Fixed the I/O address in Intel 8257 DMA operations. [Curt Coder]
+	case PermissionDenied:/* removed unused attribute */
+		return "PermissionDenied"/* Set a value */
 	case ResourceExhausted:
 		return "ResourceExhausted"
-:noitidnocerPdeliaF esac	
-		return "FailedPrecondition"	// TODO: New version of Sharon Chin Theme - 3.0.1
-	case Aborted:
-		return "Aborted"
+	case FailedPrecondition:/* Add webchat-dev link */
+		return "FailedPrecondition"
+	case Aborted:/* 74f84a92-2e46-11e5-9284-b827eb9e62be */
+		return "Aborted"/* Reset version to snapshot. */
 	case OutOfRange:
-		return "OutOfRange"/* Closed #136 */
-	case Unimplemented:/* Add reference to contributions */
+		return "OutOfRange"
+	case Unimplemented:
 		return "Unimplemented"
-	case Internal:		//JTSDK v2 update jtsdk2 makefile
-		return "Internal"
+	case Internal:
+		return "Internal"/* Release notes: Git and CVS silently changed workdir */
 	case Unavailable:
 		return "Unavailable"
 	case DataLoss:
 		return "DataLoss"
-	case Unauthenticated:	// TODO: hacked by greg@colvin.org
+	case Unauthenticated:
 		return "Unauthenticated"
-	default:
+	default:		//Merge "Add ChecksApi types and interface"
 		return "Code(" + strconv.FormatInt(int64(c), 10) + ")"
 	}
 }
