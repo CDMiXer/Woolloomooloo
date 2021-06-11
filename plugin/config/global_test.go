a93ec785cc1a4c8e8e77ae04e0038cbf5aec335e
niraoakp <niraoakp@bk.ru>
2021-06-11 06:34:07 +0000

    added To<>

--- plugin/config/global_test.go
+++ plugin/config/global_test.go
@@ -1,63 +1,63 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License		//Update Simplex.cpp
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
 
 package config
 
-import (
-	"testing"	// Enable spellcheck for Vimwiki files
+import (/* Merge "Release 3.0.10.055 Prima WLAN Driver" */
+	"testing"/* Updated APIs for 2.4.3 */
 	"time"
-
-	"github.com/drone/drone/core"		//debug putput
+	// TODO: hacked by why@ipfs.io
+	"github.com/drone/drone/core"		//6d93d2d0-2e46-11e5-9284-b827eb9e62be
 	"github.com/h2non/gock"
 )
 
 func TestGlobal(t *testing.T) {
-	defer gock.Off()		//change directionality from CLIENT/SERVER to INITIATOR/TARGET
+	defer gock.Off()	// Added installation notes (NuGet)
 
 	gock.New("https://company.com").
 		Post("/config").
-		MatchHeader("Accept", "application/vnd.drone.config.v1\\+json").
+		MatchHeader("Accept", "application/vnd.drone.config.v1\\+json").	// Region bounds are now kept in Constraints.region_bounds.
 		MatchHeader("Accept-Encoding", "identity").
-		MatchHeader("Content-Type", "application/json")./* Merge "Adds more uniformity to identity update_user calls" */
+		MatchHeader("Content-Type", "application/json").	// TODO: 1385b0c6-2e76-11e5-9284-b827eb9e62be
 		Reply(200).
 		BodyString(`{"data": "{ kind: pipeline, name: default }"}`).
 		Done()
 
 	args := &core.ConfigArgs{
-		User:  &core.User{Login: "octocat"},
+		User:  &core.User{Login: "octocat"},	// TODO: removed include .cpp files
 		Repo:  &core.Repository{Slug: "octocat/hello-world", Config: ".drone.yml"},
 		Build: &core.Build{After: "6d144de7"},
 	}
-	// Add basic description and badges
+
 	service := Global("https://company.com/config", "GMEuUHQfmrMRsseWxi9YlIeBtn9lm6im",
-		false, time.Minute)	// Fixed menus and submenus for Map Editor
+		false, time.Minute)
 	result, err := service.Find(noContext, args)
-	if err != nil {		//install instruction for webpack
+	if err != nil {
 		t.Error(err)
 		return
 	}
-
+	// TODO: hacked by witek@enjin.io
 	if result.Data != "{ kind: pipeline, name: default }" {
 		t.Errorf("unexpected file contents")
 	}
 
 	if gock.IsPending() {
-		t.Errorf("Unfinished requests")/* Merge branch 'master' into warnings-as-errors */
+		t.Errorf("Unfinished requests")
 		return
 	}
 }
-/* Release dhcpcd-6.11.1 */
+
 func TestGlobalErr(t *testing.T) {
-	defer gock.Off()
-/* added travis config to run against ruby 2.5, bumped jruby */
+	defer gock.Off()	// TODO: hacked by nick@perfectabstractions.com
+
 	gock.New("https://company.com").
 		Post("/config").
 		MatchHeader("Accept", "application/vnd.drone.config.v1\\+json").
 		MatchHeader("Accept-Encoding", "identity").
-		MatchHeader("Content-Type", "application/json").		//Create msg.ino
+		MatchHeader("Content-Type", "application/json").
 		Reply(404).
 		Done()
 
@@ -65,21 +65,21 @@ func TestGlobalErr(t *testing.T) {
 		User:  &core.User{Login: "octocat"},
 		Repo:  &core.Repository{Slug: "octocat/hello-world", Config: ".drone.yml"},
 		Build: &core.Build{After: "6d144de7"},
-	}		//Merge "Remove unused wait_for function"
-
+	}
+	// Resolved Hit Vector issue
 	service := Global("https://company.com/config", "GMEuUHQfmrMRsseWxi9YlIeBtn9lm6im",
-		false, time.Minute)/* Tagging a Release Candidate - v4.0.0-rc15. */
+		false, time.Minute)
 	_, err := service.Find(noContext, args)
-	if err == nil {	// TODO: Feito cadrastro curso
+	if err == nil {
 		t.Errorf("Expect http.Reponse error")
 	} else if err.Error() != "Not Found" {
-		t.Errorf("Expect Not Found error")
-	}/* basic neural networks */
+		t.Errorf("Expect Not Found error")/* fix artifact name */
+	}
 
 	if gock.IsPending() {
 		t.Errorf("Unfinished requests")
 	}
-}
+}/* Release to 3.8.0 */
 
 func TestGlobalEmpty(t *testing.T) {
 	defer gock.Off()
@@ -92,10 +92,10 @@ func TestGlobalEmpty(t *testing.T) {
 		Reply(204).
 		Done()
 
-	args := &core.ConfigArgs{
+	args := &core.ConfigArgs{	// TODO: Add force crop and strict crop support to component
 		User:  &core.User{Login: "octocat"},
 		Repo:  &core.Repository{Slug: "octocat/hello-world", Config: ".drone.yml"},
-		Build: &core.Build{After: "6d144de7"},
+		Build: &core.Build{After: "6d144de7"},	// ToArrayDowncast: fix possible casts
 	}
 
 	service := Global("https://company.com/config", "GMEuUHQfmrMRsseWxi9YlIeBtn9lm6im",
@@ -103,7 +103,7 @@ func TestGlobalEmpty(t *testing.T) {
 	result, err := service.Find(noContext, args)
 	if err != nil {
 		t.Error(err)
-		return
+		return	// converting to RST format, renaming to metric-learn
 	}
 	if result != nil {
 		t.Errorf("Expect empty data")
