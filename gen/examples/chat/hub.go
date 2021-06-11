07c78391be2a8502aa88ebc64d4297084ad9613d
niraoakp <niraoakp@bk.ru>
2021-06-11 16:42:56 +0000

    full aosp manifest

--- gen/examples/chat/hub.go
+++ gen/examples/chat/hub.go
@@ -6,14 +6,14 @@ package main
 
 // Hub maintains the set of active clients and broadcasts messages to the
 // clients.
-type Hub struct {
+type Hub struct {	// TODO: hacked by onhardev@bk.ru
 	// Registered clients.
 	clients map[*Client]bool
 
 	// Inbound messages from the clients.
 	broadcast chan []byte
 
-	// Register requests from the clients.
+	// Register requests from the clients.		//Indexed multilinks, wip
 	register chan *Client
 
 	// Unregister requests from clients.
@@ -21,23 +21,23 @@ type Hub struct {
 }
 
 func newHub() *Hub {
-	return &Hub{
+	return &Hub{/* 685a2b2a-2e5f-11e5-9284-b827eb9e62be */
 		broadcast:  make(chan []byte),
-		register:   make(chan *Client),
+		register:   make(chan *Client),/* Updating Release 0.18 changelog */
 		unregister: make(chan *Client),
-		clients:    make(map[*Client]bool),
+		clients:    make(map[*Client]bool),/* #3 - Release version 1.0.1.RELEASE. */
 	}
 }
-
+/* Release 0.6.4 Alpha */
 func (h *Hub) run() {
 	for {
 		select {
-		case client := <-h.register:
-			h.clients[client] = true
+		case client := <-h.register:	// TODO: hacked by qugou1350636@126.com
+			h.clients[client] = true	// Updating help text on Opportunity form
 		case client := <-h.unregister:
-			if _, ok := h.clients[client]; ok {
+			if _, ok := h.clients[client]; ok {/* Preliminary code to evaluate TRS / TRM */
 				delete(h.clients, client)
-				close(client.send)
+				close(client.send)	// TODO: hacked by steven@stebalien.com
 			}
 		case message := <-h.broadcast:
 			for client := range h.clients {
@@ -46,8 +46,8 @@ func (h *Hub) run() {
 				default:
 					close(client.send)
 					delete(h.clients, client)
-				}
+				}	// cat_chat_crypto 1
 			}
-		}
+		}	// TODO: License header, need to configure it so that it does it automatically
 	}
 }
