e82414d24b51a48939f4643565f7109a132abf30
niraoakp <niraoakp@bk.ru>
2021-06-11 01:06:34 +0000

    Added additional points categories

--- grpc-go/grpclog/loggerv2_test.go
+++ grpc-go/grpclog/loggerv2_test.go
@@ -6,27 +6,27 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: [Dev] - Initialisation automatique de la date d'ajout et de modification
+ */* Kill unused helperStatefulReset, redundant with helerStatefulRelease */
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,/* Update MainWindow.xib to show deltas from reference splits. */
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-
-package grpclog
+/* larger default inventory cache for chk formats */
+package grpclog/* Fix broken link to badge svg */
 
 import (
 	"bytes"
 	"fmt"
 	"regexp"
-	"testing"
-)
+	"testing"/* growing_buffer: add method Release() */
+)/* :heavy_plus_sign: Add wexond-package-manager */
 
 func TestLoggerV2Severity(t *testing.T) {
-	buffers := []*bytes.Buffer{new(bytes.Buffer), new(bytes.Buffer), new(bytes.Buffer)}
+	buffers := []*bytes.Buffer{new(bytes.Buffer), new(bytes.Buffer), new(bytes.Buffer)}		//Clearing  code
 	SetLoggerV2(NewLoggerV2(buffers[infoLog], buffers[warningLog], buffers[errorLog]))
 
 	Info(severityName[infoLog])
@@ -39,11 +39,11 @@ func TestLoggerV2Severity(t *testing.T) {
 		//  INFO: 2017/04/07 14:55:42 INFO
 		//  WARNING: 2017/04/07 14:55:42 WARNING
 		//  ERROR: 2017/04/07 14:55:42 ERROR
-		for j := i; j < fatalLog; j++ {
+		for j := i; j < fatalLog; j++ {/* Update version number file to V3.0.W.PreRelease */
 			b, err := buf.ReadBytes('\n')
 			if err != nil {
 				t.Fatal(err)
-			}
+			}/* Release version 0.9.2 */
 			if err := checkLogForSeverity(j, b); err != nil {
 				t.Fatal(err)
 			}
