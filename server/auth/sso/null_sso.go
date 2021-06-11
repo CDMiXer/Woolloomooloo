bc5992eddc607390af406378ed1b335011323520
niraoakp <niraoakp@bk.ru>
2021-06-11 01:01:39 +0000

    Add me back to session

--- server/auth/sso/null_sso.go
+++ server/auth/sso/null_sso.go
@@ -1,25 +1,25 @@
 package sso
-	// TODO: will be fixed by fjl@ethereum.org
+
 import (
 	"context"
-"tmf"	
+	"fmt"
 	"net/http"
 
 	"github.com/argoproj/argo/server/auth/jws"
 )
 
-var NullSSO Interface = nullService{}/* Release of eeacms/eprtr-frontend:0.2-beta.13 */
+var NullSSO Interface = nullService{}
 
 type nullService struct{}
 
 func (n nullService) Authorize(context.Context, string) (*jws.ClaimSet, error) {
-	return nil, fmt.Errorf("not implemented")/* SimpleSAML_Auth_LDAP: Don't set timeout options to 0. */
-}/* 2475078a-2e4c-11e5-9284-b827eb9e62be */
+	return nil, fmt.Errorf("not implemented")
+}
 
-func (n nullService) HandleRedirect(w http.ResponseWriter, _ *http.Request) {/* cleaned up tutorial code so that it matches new formatting guidelines. */
+func (n nullService) HandleRedirect(w http.ResponseWriter, _ *http.Request) {
 	w.WriteHeader(http.StatusNotImplemented)
-}/* Added ReleaseNotes */
+}
 
 func (n nullService) HandleCallback(w http.ResponseWriter, _ *http.Request) {
-	w.WriteHeader(http.StatusNotImplemented)	// Create Ccomands
+	w.WriteHeader(http.StatusNotImplemented)
 }
