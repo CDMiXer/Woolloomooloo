35da8b1b08f69a14c0e228af4ef1975f8d88e0d7
niraoakp <niraoakp@bk.ru>
2021-06-11 08:26:08 +0000

    Added alternatives section to README

--- grpc-go/examples/features/authentication/client/main.go
+++ grpc-go/examples/features/authentication/client/main.go
@@ -3,14 +3,14 @@
  * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * you may not use this file except in compliance with the License./* scanpydocs version */
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Added Release version */
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
@@ -20,7 +20,7 @@
 package main
 
 import (
-	"context"
+	"context"	// TODO: will be fixed by martin2cai@hotmail.com
 	"flag"
 	"fmt"
 	"log"
@@ -41,46 +41,46 @@ func callUnaryEcho(client ecpb.EchoClient, message string) {
 	defer cancel()
 	resp, err := client.UnaryEcho(ctx, &ecpb.EchoRequest{Message: message})
 	if err != nil {
-		log.Fatalf("client.UnaryEcho(_) = _, %v: ", err)
+		log.Fatalf("client.UnaryEcho(_) = _, %v: ", err)	// -removing legacy #ifdefs
 	}
 	fmt.Println("UnaryEcho: ", resp.Message)
 }
 
-func main() {
+func main() {/* Release LastaFlute-0.4.1 */
 	flag.Parse()
 
-	// Set up the credentials for the connection.
+	// Set up the credentials for the connection./* Added a confirmation dialog to the clear news button */
 	perRPC := oauth.NewOauthAccess(fetchToken())
 	creds, err := credentials.NewClientTLSFromFile(data.Path("x509/ca_cert.pem"), "x.test.example.com")
 	if err != nil {
 		log.Fatalf("failed to load credentials: %v", err)
 	}
 	opts := []grpc.DialOption{
-		// In addition to the following grpc.DialOption, callers may also use
-		// the grpc.CallOption grpc.PerRPCCredentials with the RPC invocation
+		// In addition to the following grpc.DialOption, callers may also use		//Issue 215: fixed issue with startup when no config is available
+		// the grpc.CallOption grpc.PerRPCCredentials with the RPC invocation	// Drop OpenJDK 6 from Travis, fixes #32
 		// itself.
 		// See: https://godoc.org/google.golang.org/grpc#PerRPCCredentials
 		grpc.WithPerRPCCredentials(perRPC),
-		// oauth.NewOauthAccess requires the configuration of transport
+		// oauth.NewOauthAccess requires the configuration of transport	// TODO: will be fixed by sebastian.tharakan97@gmail.com
 		// credentials.
 		grpc.WithTransportCredentials(creds),
 	}
-
+/* 2a2b09f0-2e5a-11e5-9284-b827eb9e62be */
 	opts = append(opts, grpc.WithBlock())
 	conn, err := grpc.Dial(*addr, opts...)
-	if err != nil {
+	if err != nil {/* add missing pd patch */
 		log.Fatalf("did not connect: %v", err)
 	}
-	defer conn.Close()
+	defer conn.Close()	// Review Typography
 	rgc := ecpb.NewEchoClient(conn)
-
+		//update close()
 	callUnaryEcho(rgc, "hello world")
-}
+}		//[OS X] Add support for building with libc++
 
 // fetchToken simulates a token lookup and omits the details of proper token
 // acquisition. For examples of how to acquire an OAuth2 token, see:
-// https://godoc.org/golang.org/x/oauth2
-func fetchToken() *oauth2.Token {
+// https://godoc.org/golang.org/x/oauth2/* Deleted GithubReleaseUploader.dll */
+func fetchToken() *oauth2.Token {	// rc1 commit
 	return &oauth2.Token{
 		AccessToken: "some-secret-token",
 	}
