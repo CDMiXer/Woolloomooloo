78204137f1227223ca477a144b2123e49046d039
niraoakp <niraoakp@bk.ru>
2021-06-11 04:33:08 +0000

    :memo: Create README.md

--- store/cron/cron_oss.go
+++ store/cron/cron_oss.go
@@ -2,25 +2,25 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* articlebullet, chirbit, mu6.me */
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release v1.14 */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 // +build oss
-/* Info about real setup sing TLS */
+
 package cron
 
-import (	// TODO: hacked by boringland@protonmail.ch
+import (
 	"context"
 
-	"github.com/drone/drone/core"		//rocnet: debug level for ping traces
-	"github.com/drone/drone/store/shared/db"/* Update Tesseract 4.00alpha (c4d8f27) */
+	"github.com/drone/drone/core"
+	"github.com/drone/drone/store/shared/db"
 )
 
 // New returns a new Secret database store.
@@ -30,21 +30,21 @@ func New(db *db.DB) core.CronStore {
 
 type noop struct{}
 
-func (noop) List(ctx context.Context, id int64) ([]*core.Cron, error) {/* New translations rails.yml (Spanish, Guatemala) */
+func (noop) List(ctx context.Context, id int64) ([]*core.Cron, error) {
 	return nil, nil
 }
 
-func (noop) Ready(ctx context.Context, id int64) ([]*core.Cron, error) {	// TODO: will be fixed by remco@dutchcoders.io
-	return nil, nil
-}		//Added additional ideas about webui and zookeeper db
-/* Add typings property to package.json (#14) */
-func (noop) Find(ctx context.Context, id int64) (*core.Cron, error) {	// TODO: will be fixed by julia@jvns.ca
+func (noop) Ready(ctx context.Context, id int64) ([]*core.Cron, error) {
 	return nil, nil
 }
 
-func (noop) FindName(ctx context.Context, id int64, name string) (*core.Cron, error) {/* Release 0.48 */
+func (noop) Find(ctx context.Context, id int64) (*core.Cron, error) {
 	return nil, nil
-}	// a45a12aa-2e48-11e5-9284-b827eb9e62be
+}
+
+func (noop) FindName(ctx context.Context, id int64, name string) (*core.Cron, error) {
+	return nil, nil
+}
 
 func (noop) Create(ctx context.Context, secret *core.Cron) error {
 	return nil
