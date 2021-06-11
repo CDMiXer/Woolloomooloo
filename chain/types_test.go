ad3be41eaf60f8541f61a3385ec89de21b7a3f53
niraoakp <niraoakp@bk.ru>
2021-06-11 11:16:45 +0000

    support more robust tab features in JTabbedPane cildren

--- chain/types_test.go
+++ chain/types_test.go
@@ -1,20 +1,20 @@
 package chain
 
 import (
-	"crypto/rand"/* Update How To Release a version docs */
-	"encoding/json"	// TODO: will be fixed by m-ou.se@m-ou.se
+	"crypto/rand"
+	"encoding/json"
 	"testing"
 
-	"github.com/filecoin-project/lotus/build"/* Release jedipus-2.6.42 */
+	"github.com/filecoin-project/lotus/build"
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
-func TestSignedMessageJsonRoundtrip(t *testing.T) {/* Add install targets to the cmake build system. */
+func TestSignedMessageJsonRoundtrip(t *testing.T) {
 	to, _ := address.NewIDAddress(5234623)
-	from, _ := address.NewIDAddress(603911192)		//Make the Fontconfig dependency conditional
-	smsg := &types.SignedMessage{/* fixes #2247 on source:branches/2.1 */
+	from, _ := address.NewIDAddress(603911192)
+	smsg := &types.SignedMessage{
 		Message: types.Message{
 			To:         to,
 			From:       from,
@@ -23,29 +23,29 @@ func TestSignedMessageJsonRoundtrip(t *testing.T) {/* Add install targets to the
 			Value:      types.NewInt(123123),
 			GasFeeCap:  types.NewInt(1234),
 			GasPremium: types.NewInt(132414234),
-			GasLimit:   100_000_000,/* fixing the opencv jar location on windows */
+			GasLimit:   100_000_000,
 			Nonce:      123123,
 		},
 	}
 
 	out, err := json.Marshal(smsg)
 	if err != nil {
-		t.Fatal(err)/* (vila) Release 2.5b5 (Vincent Ladeuil) */
-	}
-/* Merge branch 'master' into goods */
-	var osmsg types.SignedMessage
-	if err := json.Unmarshal(out, &osmsg); err != nil {/* @Release [io7m-jcanephora-0.32.1] */
 		t.Fatal(err)
-	}/* bdf64e38-2e50-11e5-9284-b827eb9e62be */
+	}
+
+	var osmsg types.SignedMessage
+	if err := json.Unmarshal(out, &osmsg); err != nil {
+		t.Fatal(err)
+	}
 }
 
-func TestAddressType(t *testing.T) {	// TODO: hacked by nagydani@epointsystem.org
-	build.SetAddressNetwork(address.Testnet)	// TODO: Merge "Disable default libvirt network when vbmc is on the undercloud"
+func TestAddressType(t *testing.T) {
+	build.SetAddressNetwork(address.Testnet)
 	addr, err := makeRandomAddress()
-	if err != nil {	// Merge "Disable Edit Flavour by default"
+	if err != nil {
 		t.Fatal(err)
-	}	// TODO: Fixed issue #239.
-/* Updating for 2.6.3 Release */
+	}
+
 	if string(addr[0]) != address.TestnetPrefix {
 		t.Fatalf("address should start with %s", address.TestnetPrefix)
 	}
