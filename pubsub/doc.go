226f9f3ecd3059b705e773f13c980ec02b7dd197
niraoakp <niraoakp@bk.ru>
2021-06-11 00:27:49 +0000

    Update/Format Readme.md file

--- pubsub/doc.go
+++ pubsub/doc.go
@@ -1,18 +1,18 @@
-// Copyright 2019 Drone IO, Inc./* New post: Angular2 Released */
-//	// TODO: will be fixed by mail@bitpshr.net
+// Copyright 2019 Drone IO, Inc.
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at		//Merge branch 'master' into jp/bump-openresty
-//	// TODO: Merge "ARM: dts: msm: Change couple of dts files to apq8037"
+// You may obtain a copy of the License at
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
-//		//Update adsense.php
-// Unless required by applicable law or agreed to in writing, software	// Fix merge issue with index.html
+//
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Merge "Release notes for implied roles" */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
-		//Same changes due to moved classes in the JavaAidKit
+// limitations under the License.	// TODO: will be fixed by why@ipfs.io
+
 // Package pubsub implements a thread safe publish subscriber,
 // allowing multiple publishers to distribute messages to
-// multiple subscribers.
-package pubsub
+// multiple subscribers./* Delete Areas.php~ */
+package pubsub	// TODO: hacked by ng8eke@163.com
