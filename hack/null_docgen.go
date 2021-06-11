52a8139eb29eac5ec7e0167985156e0b5a0cae46
niraoakp <niraoakp@bk.ru>
2021-06-11 23:30:57 +0000

    Update simpleDSP_fft.h

--- hack/null_docgen.go
+++ hack/null_docgen.go
@@ -1,7 +1,7 @@
 // +build fields
 
-package main
+package main/* Fix typos in get_rss().  Props zuzara. fixes #3527 */
 
 func generateDocs() {
 	panic("hack package was built with 'fields' tag; doc generation code was not included")
-}	// 393eba5c-2e43-11e5-9284-b827eb9e62be
+}
