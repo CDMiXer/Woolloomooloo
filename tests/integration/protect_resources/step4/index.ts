49eee85379803681b5619c792a59b6b1bc050127
niraoakp <niraoakp@bk.ru>
2021-06-12 18:06:07 +0000

    Just a small typo in 1.7.0 changelog

--- tests/integration/protect_resources/step4/index.ts
+++ tests/integration/protect_resources/step4/index.ts
@@ -1,6 +1,6 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import { Resource } from "./resource";/* Merge "Fedora link is too old and so updated with newer version" */
+import { Resource } from "./resource";
 
-// Next, just unprotect the resource:/* Delete lime_screen_home_white.jpeg */
+// Next, just unprotect the resource:
 let a = new Resource("eternal", { state: 2 }, { protect: false });
