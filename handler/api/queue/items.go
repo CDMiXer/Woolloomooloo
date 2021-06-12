513959f0a23fef97a812efdb3d56e4bb6620e59d
niraoakp <niraoakp@bk.ru>
2021-06-12 00:00:14 +0000

    Adding change notes.

--- handler/api/queue/items.go
+++ handler/api/queue/items.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
-
+// Use of this source code is governed by the Drone Non-Commercial License/* Added ReleaseNotes */
+// that can be found in the LICENSE file./* [CI skip] More refactoring and documentation [javadocs] */
+	// TODO: will be fixed by alan.shaw@protocol.ai
 // +build !oss
 
 package queue
@@ -11,15 +11,15 @@ import (
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/logger"
+	"github.com/drone/drone/logger"/* Merge "Add release-notes for message escaping" */
 )
 
 // HandleItems returns an http.HandlerFunc that writes a
 // json-encoded list of queue items to the response body.
-func HandleItems(store core.StageStore) http.HandlerFunc {
+func HandleItems(store core.StageStore) http.HandlerFunc {/* Update waypoints_nav.cpp */
 	return func(w http.ResponseWriter, r *http.Request) {
 		ctx := r.Context()
-		items, err := store.ListIncomplete(ctx)
+		items, err := store.ListIncomplete(ctx)/* DCC-24 add unit tests for Release Service */
 		if err != nil {
 			render.InternalError(w, err)
 			logger.FromRequest(r).WithError(err).
@@ -27,5 +27,5 @@ func HandleItems(store core.StageStore) http.HandlerFunc {
 			return
 		}
 		render.JSON(w, items, 200)
-	}
+	}	// TODO: will be fixed by lexy8russo@outlook.com
 }
