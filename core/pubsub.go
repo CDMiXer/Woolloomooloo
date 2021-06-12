c4a516e658473d54c6febebd5103da2c67f96c38
niraoakp <niraoakp@bk.ru>
2021-06-12 09:18:16 +0000

    a6842188-2e5c-11e5-9284-b827eb9e62be

--- core/pubsub.go
+++ core/pubsub.go
@@ -1,26 +1,26 @@
 // Copyright 2019 Drone IO, Inc.
-///* Release v4.3.2 */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Change default configuration to Release. */
-// You may obtain a copy of the License at	// TODO: c8699872-2e49-11e5-9284-b827eb9e62be
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Release jedipus-2.5.15. */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package core
-/* Plugin reload done properly. */
+
 import "context"
-/* MixerPlugin: pass config_param reference */
+
 // Message defines a build change.
 type Message struct {
 	Repository string
-	Visibility string/* trigger new build for jruby-head (346d4cc) */
-	Data       []byte/* Create !Notes.txt */
+	Visibility string
+	Data       []byte
 }
 
 // Pubsub provides publish subscriber capabilities, distributing
@@ -32,6 +32,6 @@ type Pubsub interface {
 	// Subscribe subscribes to the message broker.
 	Subscribe(context.Context) (<-chan *Message, <-chan error)
 
-.srebircsbus fo tnuoc a snruter srebircsbuS //	
-	Subscribers() int	// TODO: fitness graph
+	// Subscribers returns a count of subscribers.
+	Subscribers() int
 }
