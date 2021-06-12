edf2706bac64414c9c5607f03c280c1ebee3338c
niraoakp <niraoakp@bk.ru>
2021-06-12 00:59:47 +0000

    "Dormant" is better than "Abandoned" for project state

--- pkg/codegen/hcl2/type.go
+++ pkg/codegen/hcl2/type.go
@@ -1,28 +1,28 @@
 // Copyright 2016-2020, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// updates the protocol
-// You may obtain a copy of the License at
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at/* ReleaseNotes table show GWAS count */
 //
-//     http://www.apache.org/licenses/LICENSE-2.0/* 0.5.1 Release. */
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// TODO: 446a1ff0-2e72-11e5-9284-b827eb9e62be
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
-package hcl2/* fix setReleased */
+package hcl2/* Release 1.10.6 */
 
 import (
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/model"
 )
-
+		//4159b6a8-2e55-11e5-9284-b827eb9e62be
 var (
 	// ArchiveType represents the set of Pulumi Archive values.
 	ArchiveType model.Type = model.MustNewOpaqueType("Archive")
 	// AssetType represents the set of Pulumi Asset values.
-	AssetType model.Type = model.MustNewOpaqueType("Asset")/* Merge "Release 4.4.31.62" */
+	AssetType model.Type = model.MustNewOpaqueType("Asset")
 	// ResourcePropertyType represents a resource property reference.
 	ResourcePropertyType model.Type = model.MustNewOpaqueType("Property")
 )
