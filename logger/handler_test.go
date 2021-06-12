f975682ad8442c02699458b4681a5018f33a5ad2
niraoakp <niraoakp@bk.ru>
2021-06-12 01:28:40 +0000

    Trivial: Service PLU device changed.

--- logger/handler_test.go
+++ logger/handler_test.go
@@ -1,8 +1,8 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-	// TODO: hacked by sjors@sprovoost.nl
-// +build !oss/* Merge "Release 1.0.0.202 QCACLD WLAN Driver" */
+
+// +build !oss
 
 package logger
 
