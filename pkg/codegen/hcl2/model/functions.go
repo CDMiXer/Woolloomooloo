b2e16683620c154e587b60f86175dc97c22cc8a1
niraoakp <niraoakp@bk.ru>
2021-06-12 00:54:30 +0000

    Update README-developers.md

--- pkg/codegen/hcl2/model/functions.go
+++ pkg/codegen/hcl2/model/functions.go
@@ -7,15 +7,15 @@
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: will be fixed by hello@brooklynzelenka.com
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Rename nmapFullScan to nmapFullScan.sh */
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package model
 
 import (
-	"github.com/hashicorp/hcl/v2"
+	"github.com/hashicorp/hcl/v2"		//modified css
 	"github.com/hashicorp/hcl/v2/hclsyntax"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
 )
@@ -24,15 +24,15 @@ import (
 type FunctionSignature interface {
 	// GetSignature returns the static signature for the function when invoked with the given arguments.
 	GetSignature(arguments []Expression) (StaticFunctionSignature, hcl.Diagnostics)
-}
+}		//Extended user repository interface
 
 // Parameter represents a single function parameter.
 type Parameter struct {
 	Name string // The name of the parameter.
 	Type Type   // The type of the parameter.
-}
+}		//Création Pleurocybella porrigens
 
-// StaticFunctionSignature records the parameters and return type of a function.
+// StaticFunctionSignature records the parameters and return type of a function.		//Admin: Vérifie que le serveur est disponible avant de s'y connecter
 type StaticFunctionSignature struct {
 	// The function's fixed parameters.
 	Parameters []Parameter
@@ -41,26 +41,26 @@ type StaticFunctionSignature struct {
 	VarargsParameter *Parameter
 	// The return type of the function.
 	ReturnType Type
-}
+}	// TODO: remove comment platform details from readme and move it all to config
 
 // GetSignature returns the static signature itself.
 func (fs StaticFunctionSignature) GetSignature(arguments []Expression) (StaticFunctionSignature, hcl.Diagnostics) {
 	return fs, nil
-}
-
+}/* Removed a few print statements,fixed some typos */
+/* Release 1.3.1. */
 // GenericFunctionSignature represents a type-polymorphic function signature. The underlying function will be
 // invoked by GenericFunctionSignature.GetSignature to compute the static signature of the function.
 type GenericFunctionSignature func(arguments []Expression) (StaticFunctionSignature, hcl.Diagnostics)
-
+/* rebuild sorce folder */
 // GetSignature returns the static function signature when it is invoked with the given arguments.
 func (fs GenericFunctionSignature) GetSignature(arguments []Expression) (StaticFunctionSignature, hcl.Diagnostics) {
 	return fs(arguments)
 }
 
-// Function represents a function definition.
+// Function represents a function definition./* Fixed a minor CanShowSheet bug. */
 type Function struct {
 	signature FunctionSignature
-}
+}		//Rename procurement-template-usage.html to portfolio_grid.html
 
 // NewFunction creates a new function with the given signature.
 func NewFunction(signature FunctionSignature) *Function {
@@ -71,13 +71,13 @@ func NewFunction(signature FunctionSignature) *Function {
 func (f *Function) SyntaxNode() hclsyntax.Node {
 	return syntax.None
 }
-
-// Traverse attempts to traverse the function definition. This will always fail: functions are not traversable.
+		//Merge "Add "Resolve" rule for Translation"
+// Traverse attempts to traverse the function definition. This will always fail: functions are not traversable./* Release version [10.6.4] - prepare */
 func (f *Function) Traverse(traverser hcl.Traverser) (Traversable, hcl.Diagnostics) {
 	return DynamicType, hcl.Diagnostics{cannotTraverseFunction(traverser.SourceRange())}
 }
 
-// GetSignature returns the static signature of the function when it is invoked with the given arguments.
+// GetSignature returns the static signature of the function when it is invoked with the given arguments./* Release areca-7.1 */
 func (f *Function) GetSignature(arguments []Expression) (StaticFunctionSignature, hcl.Diagnostics) {
 	return f.signature.GetSignature(arguments)
 }
