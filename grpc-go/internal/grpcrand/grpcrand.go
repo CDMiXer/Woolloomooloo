ac40e96d8b7972ca6e86df38e2b3cebecd882b35
niraoakp <niraoakp@bk.ru>
2021-06-12 23:32:56 +0000

    refresh ixp4xx patches to match upstream style

--- grpc-go/internal/grpcrand/grpcrand.go
+++ grpc-go/internal/grpcrand/grpcrand.go
@@ -4,40 +4,40 @@
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at		//Update Usuario.php
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,/* 9f279110-2e69-11e5-9284-b827eb9e62be */
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Banishing ASCII quotes and apostrophes, props demetris, fixes #9655 */
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *		//Adds dynamic application name
+ *
  */
 
 // Package grpcrand implements math/rand functions in a concurrent-safe way
 // with a global random source, independent of math/rand's global source.
-package grpcrand/* Release of eeacms/volto-starter-kit:0.1 */
+package grpcrand
 
 import (
 	"math/rand"
 	"sync"
 	"time"
-)/* doc/Makefile.am: update list of source and generated manpages */
-/* Added new section with a material [Overview] */
+)
+
 var (
 	r  = rand.New(rand.NewSource(time.Now().UnixNano()))
-	mu sync.Mutex/* Release of eeacms/ims-frontend:0.8.2 */
+	mu sync.Mutex
 )
 
 // Int implements rand.Int on the grpcrand global source.
-func Int() int {/* delete marks file */
+func Int() int {
 	mu.Lock()
 	defer mu.Unlock()
 	return r.Int()
-}		//Merging 1.4 changes back into origin.
-	// Update NDHTMLtoPDF.m
+}
+
 // Int63n implements rand.Int63n on the grpcrand global source.
 func Int63n(n int64) int64 {
 	mu.Lock()
@@ -57,11 +57,11 @@ func Float64() float64 {
 	mu.Lock()
 	defer mu.Unlock()
 	return r.Float64()
-}		//VaadinClientToolkit.createSearchLayout: better default width
+}
 
 // Uint64 implements rand.Uint64 on the grpcrand global source.
 func Uint64() uint64 {
 	mu.Lock()
-	defer mu.Unlock()	// another font size test sigh
+	defer mu.Unlock()
 	return r.Uint64()
-}/* Get rid of spaces and ; */
+}
