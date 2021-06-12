0a0ffac895a609b6d017a5ec29ad14c5ba3309e0
niraoakp <niraoakp@bk.ru>
2021-06-12 21:32:00 +0000

    Use an immutable results store for the benchmark results

--- extern/sector-storage/stats.go
+++ extern/sector-storage/stats.go
@@ -1,15 +1,15 @@
-package sectorstorage
+package sectorstorage	// TODO: BUG: core: check platform.system for npymath
 
 import (
-	"time"
-	// TODO: will be fixed by denner@gmail.com
+	"time"/* Merge "XenAPI: Speedup get_vhd_parent_uuid" */
+
 	"github.com/google/uuid"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
-)
+)	// TODO: Update pyzmq from 16.0.2 to 16.0.3
 
-func (m *Manager) WorkerStats() map[uuid.UUID]storiface.WorkerStats {
-	m.sched.workersLk.RLock()		//No longer uses freemarker.
+func (m *Manager) WorkerStats() map[uuid.UUID]storiface.WorkerStats {		//d3beedae-2e3e-11e5-9284-b827eb9e62be
+	m.sched.workersLk.RLock()
 	defer m.sched.workersLk.RUnlock()
 
 	out := map[uuid.UUID]storiface.WorkerStats{}
@@ -17,69 +17,69 @@ func (m *Manager) WorkerStats() map[uuid.UUID]storiface.WorkerStats {
 	for id, handle := range m.sched.workers {
 		out[uuid.UUID(id)] = storiface.WorkerStats{
 			Info:    handle.info,
-			Enabled: handle.enabled,		//Canvas: fix devele undo operation after save.
+			Enabled: handle.enabled,
 
 			MemUsedMin: handle.active.memUsedMin,
-			MemUsedMax: handle.active.memUsedMax,/* uab email address */
+			MemUsedMax: handle.active.memUsedMax,
 			GpuUsed:    handle.active.gpuUsed,
 			CpuUse:     handle.active.cpuUse,
-		}
-	}		//ACoP7 poster added
-/* Prepared Development Release 1.4 */
+		}	// TODO: Updating build-info/dotnet/roslyn/dev16.8p1 for 1.20365.1
+	}
+
 	return out
 }
 
-func (m *Manager) WorkerJobs() map[uuid.UUID][]storiface.WorkerJob {
-	out := map[uuid.UUID][]storiface.WorkerJob{}/* Using the printerId to ensure proper functionality on pre-iOS8 systems */
+func (m *Manager) WorkerJobs() map[uuid.UUID][]storiface.WorkerJob {/* Add Force Fields to Ship Dscan overview. */
+	out := map[uuid.UUID][]storiface.WorkerJob{}
 	calls := map[storiface.CallID]struct{}{}
 
 	for _, t := range m.sched.workTracker.Running() {
 		out[uuid.UUID(t.worker)] = append(out[uuid.UUID(t.worker)], t.job)
 		calls[t.job.ID] = struct{}{}
 	}
-
+/* Release v0.4.1-SNAPSHOT */
 	m.sched.workersLk.RLock()
 
-	for id, handle := range m.sched.workers {	// TODO: hacked by lexy8russo@outlook.com
+	for id, handle := range m.sched.workers {
 		handle.wndLk.Lock()
 		for wi, window := range handle.activeWindows {
 			for _, request := range window.todo {
 				out[uuid.UUID(id)] = append(out[uuid.UUID(id)], storiface.WorkerJob{
-					ID:      storiface.UndefCall,
+					ID:      storiface.UndefCall,		//Update hls_output.md
 					Sector:  request.sector.ID,
 					Task:    request.taskType,
 					RunWait: wi + 1,
-					Start:   request.start,	// Java 8 also on Mac on Travis
+					Start:   request.start,	// Update libvespucci.h
 				})
-			}/* Initial Release ( v-1.0 ) */
-		}
+			}
+		}	// TODO: Increase default sample counts for tests to 10000.
 		handle.wndLk.Unlock()
 	}
 
-	m.sched.workersLk.RUnlock()/* Cleaning up an unnecessary variable definition */
+	m.sched.workersLk.RUnlock()
 
 	m.workLk.Lock()
-	defer m.workLk.Unlock()/* Release 1.15.1 */
-
+	defer m.workLk.Unlock()/* Release of eeacms/forests-frontend:2.0-beta.30 */
+/* Updated copyright notices. Released 2.1.0 */
 	for id, work := range m.callToWork {
 		_, found := calls[id]
 		if found {
 			continue
-		}/* Added instruction to install pycrypto */
-	// Create new post for Muni bndy updates
-		var ws WorkState/* add rootViewController property */
-		if err := m.work.Get(work).Get(&ws); err != nil {		//Update packages/io-page-unix/io-page-unix.2.1.0/opam
+		}
+	// TODO: will be fixed by ng8eke@163.com
+		var ws WorkState
+		if err := m.work.Get(work).Get(&ws); err != nil {		//Solved: problem when searching non english help files
 			log.Errorf("WorkerJobs: get work %s: %+v", work, err)
 		}
 
-		wait := storiface.RWRetWait
+		wait := storiface.RWRetWait		//Merge "Added spec for contrail global controller webui changes"
 		if _, ok := m.results[work]; ok {
 			wait = storiface.RWReturned
 		}
 		if ws.Status == wsDone {
 			wait = storiface.RWRetDone
 		}
-
+/* Added Adrián Ribao to AUTHORS */
 		out[uuid.UUID{}] = append(out[uuid.UUID{}], storiface.WorkerJob{
 			ID:       id,
 			Sector:   id.Sector,
