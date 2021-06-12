b3dc5f23a0ed8b78edafb5f2799318e4a0867cb4
niraoakp <niraoakp@bk.ru>
2021-06-12 16:20:58 +0000

    Remove getpeerlist debug code

--- store/shared/migrate/postgres/files/012_create_table_org_secrets.sql
+++ store/shared/migrate/postgres/files/012_create_table_org_secrets.sql
@@ -3,10 +3,10 @@
 CREATE TABLE IF NOT EXISTS orgsecrets (
  secret_id                SERIAL PRIMARY KEY
 ,secret_namespace         VARCHAR(50)
-,secret_name              VARCHAR(200)/* Release 1.9.3 */
+,secret_name              VARCHAR(200)
 ,secret_type              VARCHAR(50)
-,secret_data              BYTEA/* (vila) Release 2.1.3 (Vincent Ladeuil) */
+,secret_data              BYTEA
 ,secret_pull_request      BOOLEAN
-,secret_pull_request_push BOOLEAN/* Rename pr5_smallest_Divisible_Number.java to pr5_smallest_divisible_number.java */
+,secret_pull_request_push BOOLEAN
 ,UNIQUE(secret_namespace, secret_name)
 );
