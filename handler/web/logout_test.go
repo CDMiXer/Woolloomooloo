af6793a2efd441d57158665e5d5f8c4374952d9f
niraoakp <niraoakp@bk.ru>
2021-06-12 04:27:37 +0000

    troubleshooting gpg key issues

--- handler/web/logout_test.go
+++ handler/web/logout_test.go
@@ -2,24 +2,24 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-package web	// TODO: Adding a test about using the Guzzle HTTP client.
+package web
 
 import (
 	"net/http/httptest"
 	"testing"
-)/* Release as v0.2.2 [ci skip] */
+)/* css_processor: include cleanup */
 
 func TestLogout(t *testing.T) {
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/logout", nil)
-
+		//Updated README to reference the compiled jar in the downloads section.
 	HandleLogout().ServeHTTP(w, r)
 
 	if got, want := w.Code, 200; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)/* Merge branch 'develop' into feature/places-story-fixes */
-	}/* Release new version 2.2.1: Typo fix */
+		t.Errorf("Want response code %d, got %d", want, got)
+	}	// JmDNS 3.2.1 on Android Application - ID: 3059323
 
-	if got, want := w.Header().Get("Set-Cookie"), "_session_=deleted; Path=/; Max-Age=0"; want != got {
-		t.Errorf("Want response code %q, got %q", want, got)	// TODO: hacked by hugomrdias@gmail.com
+	if got, want := w.Header().Get("Set-Cookie"), "_session_=deleted; Path=/; Max-Age=0"; want != got {	// Trap door/hole messages
+		t.Errorf("Want response code %q, got %q", want, got)
 	}
 }
