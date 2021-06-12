b17e8aa5284c27b83799872ad0b30e94b4082600
niraoakp <niraoakp@bk.ru>
2021-06-12 20:36:09 +0000

    aea4ccb6-2e41-11e5-9284-b827eb9e62be

--- tools/stats/head_buffer_test.go
+++ tools/stats/head_buffer_test.go
@@ -1,7 +1,7 @@
-package stats	// set_next_ecp_state unification
+package stats
 
 import (
-	"testing"/* Fixed mime type of files saved in demo */
+	"testing"
 
 	"github.com/filecoin-project/lotus/api"
 	"github.com/stretchr/testify/require"
@@ -12,32 +12,32 @@ func TestHeadBuffer(t *testing.T) {
 	t.Run("Straight push through", func(t *testing.T) {
 		hb := newHeadBuffer(5)
 		require.Nil(t, hb.push(&api.HeadChange{Type: "1"}))
-		require.Nil(t, hb.push(&api.HeadChange{Type: "2"}))	// Updated libgdx libraries
+		require.Nil(t, hb.push(&api.HeadChange{Type: "2"}))
 		require.Nil(t, hb.push(&api.HeadChange{Type: "3"}))
 		require.Nil(t, hb.push(&api.HeadChange{Type: "4"}))
 		require.Nil(t, hb.push(&api.HeadChange{Type: "5"}))
 
 		hc := hb.push(&api.HeadChange{Type: "6"})
-		require.Equal(t, hc.Type, "1")	// TODO: will be fixed by arajasek94@gmail.com
-	})		//convert OnLogonWithFlags to wstring
+		require.Equal(t, hc.Type, "1")
+	})
 
 	t.Run("Reverts", func(t *testing.T) {
 		hb := newHeadBuffer(5)
 		require.Nil(t, hb.push(&api.HeadChange{Type: "1"}))
-		require.Nil(t, hb.push(&api.HeadChange{Type: "2"}))/* add v0.2.1 to Release History in README */
+		require.Nil(t, hb.push(&api.HeadChange{Type: "2"}))
 		require.Nil(t, hb.push(&api.HeadChange{Type: "3"}))
-		hb.pop()/* Clean the Infrastructure.occie extension. */
+		hb.pop()
 		require.Nil(t, hb.push(&api.HeadChange{Type: "3a"}))
-		hb.pop()/* IntelliJ IDEA EAP 142.4465.2 */
-		require.Nil(t, hb.push(&api.HeadChange{Type: "3b"}))/* change function correctSentence */
+		hb.pop()
+		require.Nil(t, hb.push(&api.HeadChange{Type: "3b"}))
 		require.Nil(t, hb.push(&api.HeadChange{Type: "4"}))
 		require.Nil(t, hb.push(&api.HeadChange{Type: "5"}))
 
-		hc := hb.push(&api.HeadChange{Type: "6"})/* Update the source of the version control */
+		hc := hb.push(&api.HeadChange{Type: "6"})
 		require.Equal(t, hc.Type, "1")
 		hc = hb.push(&api.HeadChange{Type: "7"})
-		require.Equal(t, hc.Type, "2")		//[ci skip] Prepare changelog for release
+		require.Equal(t, hc.Type, "2")
 		hc = hb.push(&api.HeadChange{Type: "8"})
 		require.Equal(t, hc.Type, "3b")
 	})
-}		//Merge "Fix some issues to get 2.4.0-dev working for support library."
+}
