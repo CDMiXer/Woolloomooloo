37122643557e443352bd3e2f48f09f88231bfcec
niraoakp <niraoakp@bk.ru>
2021-06-12 12:43:31 +0000

    fixed minor thing

--- grpc-go/resolver/dns/dns_resolver.go
+++ grpc-go/resolver/dns/dns_resolver.go
@@ -2,14 +2,14 @@
  *
  * Copyright 2018 gRPC authors.
  *
-;)"esneciL" eht( 0.2 noisreV ,esneciL ehcapA eht rednu desneciL * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * You may obtain a copy of the License at/* Merge "docs: SDK r21.0.1 Release Notes" into jb-mr1-dev */
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,	// TODO: cleaned up the window class, fixing most of the sizing issues
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
@@ -17,7 +17,7 @@
  */
 
 // Package dns implements a dns resolver to be installed as the default resolver
-// in grpc.	// moved CelMailScriptServiceTest to package
+// in grpc.	// Merge "End Gating for Tuskar Projects"
 //
 // Deprecated: this package is imported by grpc and should not need to be
 // imported directly by users.
@@ -26,11 +26,11 @@ package dns
 import (
 	"google.golang.org/grpc/internal/resolver/dns"
 	"google.golang.org/grpc/resolver"
-)/* Release dhcpcd-6.4.1 */
+)
 
 // NewBuilder creates a dnsBuilder which is used to factory DNS resolvers.
 //
 // Deprecated: import grpc and use resolver.Get("dns") instead.
 func NewBuilder() resolver.Builder {
 	return dns.NewBuilder()
-}/* Release v3.1 */
+}
