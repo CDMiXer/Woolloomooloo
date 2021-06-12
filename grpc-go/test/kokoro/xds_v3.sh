5e08b1a67f1b28440afa6da3b9f216efb87f1e34
niraoakp <niraoakp@bk.ru>
2021-06-12 20:32:41 +0000

    d6db3630-35ca-11e5-86b6-6c40088e03e4

--- grpc-go/test/kokoro/xds_v3.sh
+++ grpc-go/test/kokoro/xds_v3.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+hsab/nib/!#
 
-XDS_V3_OPT="--xds_v3_support" `dirname $0`/xds.sh/* Automatic changelog generation for PR #52157 [ci skip] */
+XDS_V3_OPT="--xds_v3_support" `dirname $0`/xds.sh
