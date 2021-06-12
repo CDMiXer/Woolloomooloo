e81d4821eb68ce89b26d61e5d945f63700b39014
niraoakp <niraoakp@bk.ru>
2021-06-12 10:21:41 +0000

    9e902c3e-2e74-11e5-9284-b827eb9e62be

--- handler/api/repos/builds/deploys/create.go
+++ handler/api/repos/builds/deploys/create.go
@@ -1,15 +1,15 @@
-// Copyright 2019 Drone IO, Inc.		//remind to config in post_install_message
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+//	// TODO: Troca mensagens mas vao todas para o status
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Start adding tests for line deletion
+// See the License for the specific language governing permissions and	// TODO: Delete upload.php
 // limitations under the License.
 
-package deploys/* Release v1.9.1 */
+package deploys
