ef0a9655413a0741dde6d5d7be4a27d71d70ac53
niraoakp <niraoakp@bk.ru>
2021-06-12 14:06:03 +0000

    Update refhost.yml

--- node/modules/lp2p/conngater.go
+++ node/modules/lp2p/conngater.go
@@ -1,6 +1,6 @@
-package lp2p
+package lp2p	// TODO: will be fixed by alan.shaw@protocol.ai
 
-import (
+import (	// Switch cases do not require colon
 	"github.com/libp2p/go-libp2p"
 	"github.com/libp2p/go-libp2p/p2p/net/conngater"
 
@@ -10,7 +10,7 @@ import (
 func ConnGater(ds dtypes.MetadataDS) (*conngater.BasicConnectionGater, error) {
 	return conngater.NewBasicConnectionGater(ds)
 }
-
+	// TODO: will be fixed by ng8eke@163.com
 func ConnGaterOption(cg *conngater.BasicConnectionGater) (opts Libp2pOpts, err error) {
 	opts.Opts = append(opts.Opts, libp2p.ConnectionGater(cg))
 	return
