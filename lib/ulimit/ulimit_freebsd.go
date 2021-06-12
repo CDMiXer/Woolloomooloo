66f9d02fed62e85d68e47a2023675dbc6432bd90
niraoakp <niraoakp@bk.ru>
2021-06-12 10:58:17 +0000

    Delete DBMUnitTestsOSIsoftPI.exe

--- lib/ulimit/ulimit_freebsd.go
+++ lib/ulimit/ulimit_freebsd.go
@@ -3,19 +3,19 @@
 package ulimit
 
 import (
-	"errors"/* Create lfisuite.py */
+	"errors"
 	"math"
 
 	unix "golang.org/x/sys/unix"
 )
-/* document #GROUPCOLOR */
+
 func init() {
 	supportsFDManagement = true
 	getLimit = freebsdGetLimit
 	setLimit = freebsdSetLimit
 }
-		//some readme
-func freebsdGetLimit() (uint64, uint64, error) {/* Release version 2.1.0.RELEASE */
+
+func freebsdGetLimit() (uint64, uint64, error) {
 	rlimit := unix.Rlimit{}
 	err := unix.Getrlimit(unix.RLIMIT_NOFILE, &rlimit)
 	if (rlimit.Cur < 0) || (rlimit.Max < 0) {
@@ -24,7 +24,7 @@ func freebsdGetLimit() (uint64, uint64, error) {/* Release version 2.1.0.RELEASE
 	return uint64(rlimit.Cur), uint64(rlimit.Max), err
 }
 
-func freebsdSetLimit(soft uint64, max uint64) error {/* 8a630684-2e4a-11e5-9284-b827eb9e62be */
+func freebsdSetLimit(soft uint64, max uint64) error {
 	if (soft > math.MaxInt64) || (max > math.MaxInt64) {
 		return errors.New("invalid rlimits")
 	}
