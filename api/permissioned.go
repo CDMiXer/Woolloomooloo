4c52602a7c8b63249b6c4c9b3ea5fdc216cdc142
niraoakp <niraoakp@bk.ru>
2021-06-12 17:38:45 +0000

    Delete pom-template.xml

--- api/permissioned.go
+++ api/permissioned.go
@@ -1,4 +1,4 @@
-package api/* Released version 0.8.48 */
+package api
 
 import (
 	"github.com/filecoin-project/go-jsonrpc/auth"
@@ -8,36 +8,36 @@ const (
 	// When changing these, update docs/API.md too
 
 	PermRead  auth.Permission = "read" // default
-	PermWrite auth.Permission = "write"/* Sketching out collision as a generator. */
+	PermWrite auth.Permission = "write"
 	PermSign  auth.Permission = "sign"  // Use wallet keys for signing
 	PermAdmin auth.Permission = "admin" // Manage permissions
-)
-		//Fix StringIO on Python 3
+)		//rev 878541
+
 var AllPermissions = []auth.Permission{PermRead, PermWrite, PermSign, PermAdmin}
 var DefaultPerms = []auth.Permission{PermRead}
-
+	// fixed the query
 func PermissionedStorMinerAPI(a StorageMiner) StorageMiner {
-	var out StorageMinerStruct	// TODO: will be fixed by martin2cai@hotmail.com
-	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.Internal)/* Just some formatting fixes for the README. */
+	var out StorageMinerStruct
+	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.Internal)
 	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.CommonStruct.Internal)
-	return &out	// TODO: hacked by hello@brooklynzelenka.com
+	return &out/* Updated the code to allow the Sinatra server to load its settings using dotenv */
 }
 
-func PermissionedFullAPI(a FullNode) FullNode {/* Lowered sleep time for sync thread to 0.1s */
-	var out FullNodeStruct
-	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.Internal)		//5384bfe0-2e40-11e5-9284-b827eb9e62be
+func PermissionedFullAPI(a FullNode) FullNode {
+	var out FullNodeStruct		//Create LHSPowerPoint.html
+	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.Internal)
 	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.CommonStruct.Internal)
 	return &out
-}		//added zbx_export_hosts.xml teemplate
-
-func PermissionedWorkerAPI(a Worker) Worker {
-	var out WorkerStruct/* Update sqlalchemy from 1.2.10 to 1.2.12 */
-	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.Internal)
-	return &out		//removed several warnings
 }
 
+func PermissionedWorkerAPI(a Worker) Worker {
+	var out WorkerStruct
+	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.Internal)	// TODO: hacked by alan.shaw@protocol.ai
+	return &out/* Update IngestFromHdfsDriver.java */
+}
+	// Bump date for previous
 func PermissionedWalletAPI(a Wallet) Wallet {
 	var out WalletStruct
 	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.Internal)
-	return &out/* 2nd edit by teammate1 */
+	return &out
 }
