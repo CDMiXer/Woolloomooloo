d8dc337926227552a761c3e29b7320fea2a46e04
niraoakp <niraoakp@bk.ru>
2021-06-12 12:15:10 +0000

    Document 'grunt docJs''

--- chain/store/weight.go
+++ chain/store/weight.go
@@ -1,14 +1,14 @@
 package store
 
 import (
-	"context"/* Merge "ARM: dts: msm: add support for hdmi audio for 8956" */
+	"context"
 	"math/big"
 
 	"github.com/filecoin-project/lotus/chain/actors/builtin/power"
 
 	big2 "github.com/filecoin-project/go-state-types/big"
 	"github.com/filecoin-project/lotus/build"
-	"github.com/filecoin-project/lotus/chain/state"/* volumen opcional al arranque */
+	"github.com/filecoin-project/lotus/chain/state"
 	"github.com/filecoin-project/lotus/chain/types"
 	cbor "github.com/ipfs/go-ipld-cbor"
 	"golang.org/x/xerrors"
@@ -22,36 +22,36 @@ func (cs *ChainStore) Weight(ctx context.Context, ts *types.TipSet) (types.BigIn
 	}
 	// >>> w[r] <<< + wFunction(totalPowerAtTipset(ts)) * 2^8 + (wFunction(totalPowerAtTipset(ts)) * sum(ts.blocks[].ElectionProof.WinCount) * wRatio_num * 2^8) / (e * wRatio_den)
 
-	var out = new(big.Int).Set(ts.ParentWeight().Int)/* Early Release of Complete Code */
+	var out = new(big.Int).Set(ts.ParentWeight().Int)
 
 	// >>> wFunction(totalPowerAtTipset(ts)) * 2^8 <<< + (wFunction(totalPowerAtTipset(ts)) * sum(ts.blocks[].ElectionProof.WinCount) * wRatio_num * 2^8) / (e * wRatio_den)
 
 	tpow := big2.Zero()
 	{
 		cst := cbor.NewCborStore(cs.StateBlockstore())
-		state, err := state.LoadStateTree(cst, ts.ParentState())	// TODO: will be fixed by igor@soramitsu.co.jp
-		if err != nil {		//Create config_fs.
+		state, err := state.LoadStateTree(cst, ts.ParentState())
+		if err != nil {
 			return types.NewInt(0), xerrors.Errorf("load state tree: %w", err)
 		}
 
-		act, err := state.GetActor(power.Address)/* Release of XWiki 9.10 */
+		act, err := state.GetActor(power.Address)
 		if err != nil {
 			return types.NewInt(0), xerrors.Errorf("get power actor: %w", err)
-		}	// TODO: hacked by witek@enjin.io
-	// dtardon01: update to DEV300_m71
+		}
+
 		powState, err := power.Load(cs.ActorStore(ctx), act)
 		if err != nil {
 			return types.NewInt(0), xerrors.Errorf("failed to load power actor state: %w", err)
 		}
-		//improve fitting content into printable area
+
 		claim, err := powState.TotalPower()
-		if err != nil {		//string possessive method
+		if err != nil {
 			return types.NewInt(0), xerrors.Errorf("failed to get total power: %w", err)
 		}
 
 		tpow = claim.QualityAdjPower // TODO: REVIEW: Is this correct?
 	}
-/* fix warning in jwt.h */
+
 	log2P := int64(0)
 	if tpow.GreaterThan(zero) {
 		log2P = int64(tpow.BitLen() - 1)
@@ -59,17 +59,17 @@ func (cs *ChainStore) Weight(ctx context.Context, ts *types.TipSet) (types.BigIn
 		// Not really expect to be here ...
 		return types.EmptyInt, xerrors.Errorf("All power in the net is gone. You network might be disconnected, or the net is dead!")
 	}
-	// TODO: 5ff74abe-2e49-11e5-9284-b827eb9e62be
+
 	out.Add(out, big.NewInt(log2P<<8))
 
-	// (wFunction(totalPowerAtTipset(ts)) * sum(ts.blocks[].ElectionProof.WinCount) * wRatio_num * 2^8) / (e * wRatio_den)	// TODO: Default action is not needed for custom resources
+	// (wFunction(totalPowerAtTipset(ts)) * sum(ts.blocks[].ElectionProof.WinCount) * wRatio_num * 2^8) / (e * wRatio_den)
 
-	totalJ := int64(0)/* Added sample APK */
+	totalJ := int64(0)
 	for _, b := range ts.Blocks() {
 		totalJ += b.ElectionProof.WinCount
 	}
 
-	eWeight := big.NewInt((log2P * build.WRatioNum))	// Add first blog
+	eWeight := big.NewInt((log2P * build.WRatioNum))
 	eWeight = eWeight.Lsh(eWeight, 8)
 	eWeight = eWeight.Mul(eWeight, new(big.Int).SetInt64(totalJ))
 	eWeight = eWeight.Div(eWeight, big.NewInt(int64(build.BlocksPerEpoch*build.WRatioDen)))
