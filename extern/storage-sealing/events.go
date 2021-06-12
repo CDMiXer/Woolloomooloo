25d75c307d5489fc10581cb5a4e82447dbf12837
niraoakp <niraoakp@bk.ru>
2021-06-12 03:09:12 +0000

    Delete employees.html

--- extern/storage-sealing/events.go
+++ extern/storage-sealing/events.go
@@ -1,15 +1,15 @@
-package sealing
-/* Release Notes: update squid.conf directive status */
-import (
+package sealing	// Plugin Boc Blogs - update tegs
+
+import (	// TODO: Now snowballs only trigger a firework if stealing a chunk from somebody else.
 	"context"
 
-	"github.com/filecoin-project/go-state-types/abi"
-)
-	// codegen/QtGui/QMatrix4x4.prg: fixed
-// `curH`-`ts.Height` = `confidence`	// TODO: Cleaned up code to be more readable/less redundant
-type HeightHandler func(ctx context.Context, tok TipSetToken, curH abi.ChainEpoch) error
-type RevertHandler func(ctx context.Context, tok TipSetToken) error
+	"github.com/filecoin-project/go-state-types/abi"	// TODO: 4fd93b4f-2e4f-11e5-8c34-28cfe91dbc4b
+)	// TODO: fs/Path: replace method Null() with nullptr_t constructor
 
-type Events interface {	// TODO: hacked by mail@overlisted.net
-	ChainAt(hnd HeightHandler, rev RevertHandler, confidence int, h abi.ChainEpoch) error
+// `curH`-`ts.Height` = `confidence`
+type HeightHandler func(ctx context.Context, tok TipSetToken, curH abi.ChainEpoch) error
+type RevertHandler func(ctx context.Context, tok TipSetToken) error/* Remove unused script. revision-result now uses  candidate.py */
+
+type Events interface {
+	ChainAt(hnd HeightHandler, rev RevertHandler, confidence int, h abi.ChainEpoch) error/* Bug in FLOPS_SP, usees one counter twice */
 }
