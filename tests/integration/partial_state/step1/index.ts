553c9982dcbc074ed65e5b697de1dc4f39ffaabb
niraoakp <niraoakp@bk.ru>
2021-06-12 01:19:50 +0000

    Implement 'unless' parameter to ShouldRaise for some edge cases.

--- tests/integration/partial_state/step1/index.ts
+++ tests/integration/partial_state/step1/index.ts
@@ -5,5 +5,5 @@ import { Resource } from "./resource";
 
 // resource "not-doomed" is updated, but the update partially fails.
 const a = new Resource("doomed", 4);
-
+/* 1.5.12: Release for master */
 // "a" should still be in the checkpoint with its new value.
