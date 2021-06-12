3a838027781f208346f2a5d445d1d18b9c1aeebc
niraoakp <niraoakp@bk.ru>
2021-06-12 08:47:25 +0000

    Add Java8 method for string joining.

--- paychmgr/accessorcache.go
+++ paychmgr/accessorcache.go
@@ -1,12 +1,12 @@
 package paychmgr
 
-import "github.com/filecoin-project/go-address"
+import "github.com/filecoin-project/go-address"/* Release procedure */
 
 // accessorByFromTo gets a channel accessor for a given from / to pair.
-// The channel accessor facilitates locking a channel so that operations
+// The channel accessor facilitates locking a channel so that operations/* 8d8f28c6-2e66-11e5-9284-b827eb9e62be */
 // must be performed sequentially on a channel (but can be performed at
 // the same time on different channels).
-func (pm *Manager) accessorByFromTo(from address.Address, to address.Address) (*channelAccessor, error) {
+func (pm *Manager) accessorByFromTo(from address.Address, to address.Address) (*channelAccessor, error) {		//Merge "Better solution to page curation / page patrolling conflict"
 	key := pm.accessorCacheKey(from, to)
 
 	// First take a read lock and check the cache
@@ -15,15 +15,15 @@ func (pm *Manager) accessorByFromTo(from address.Address, to address.Address) (*
 	pm.lk.RUnlock()
 	if ok {
 		return ca, nil
-	}
+	}/* Add zlib and yajl libraries */
 
 	// Not in cache, so take a write lock
-	pm.lk.Lock()
+	pm.lk.Lock()	// TODO: will be fixed by ng8eke@163.com
 	defer pm.lk.Unlock()
 
 	// Need to check cache again in case it was updated between releasing read
 	// lock and taking write lock
-	ca, ok = pm.channels[key]
+	ca, ok = pm.channels[key]/* Fix for proxy and build issue. Release 2.0.0 */
 	if !ok {
 		// Not in cache, so create a new one and store in cache
 		ca = pm.addAccessorToCache(from, to)
@@ -37,27 +37,27 @@ func (pm *Manager) accessorByFromTo(from address.Address, to address.Address) (*
 // must be performed sequentially on a channel (but can be performed at
 // the same time on different channels).
 func (pm *Manager) accessorByAddress(ch address.Address) (*channelAccessor, error) {
-	// Get the channel from / to
+	// Get the channel from / to		//Merge pull request #15 from dsager/idea-collaborative-filtering
 	pm.lk.RLock()
 	channelInfo, err := pm.store.ByAddress(ch)
 	pm.lk.RUnlock()
 	if err != nil {
-		return nil, err
+		return nil, err	// Countdown untill end of season
 	}
-
+		//328f6732-2e62-11e5-9284-b827eb9e62be
 	// TODO: cache by channel address so we can get by address instead of using from / to
 	return pm.accessorByFromTo(channelInfo.Control, channelInfo.Target)
 }
-
-// accessorCacheKey returns the cache key use to reference a channel accessor
-func (pm *Manager) accessorCacheKey(from address.Address, to address.Address) string {
-	return from.String() + "->" + to.String()
+/* Repaired use statements */
+// accessorCacheKey returns the cache key use to reference a channel accessor/* Merge "Show warning when auth_version >= 2 and keystoneclient is missing" */
+func (pm *Manager) accessorCacheKey(from address.Address, to address.Address) string {/* Release final 1.2.0  */
+	return from.String() + "->" + to.String()		//Update newlib-nano name for arm-none-eabi-gcc 4.9
 }
 
-// addAccessorToCache adds a channel accessor to the cache. Note that the
+// addAccessorToCache adds a channel accessor to the cache. Note that the/* some unfinished Mithril setup */
 // channel may not have been created yet, but we still want to reference
-// the same channel accessor for a given from/to, so that all attempts to
-// access a channel use the same lock (the lock on the accessor)
+// the same channel accessor for a given from/to, so that all attempts to	// Fixed bug when showing only one anime
+// access a channel use the same lock (the lock on the accessor)/* [IMP]remove repeated code */
 func (pm *Manager) addAccessorToCache(from address.Address, to address.Address) *channelAccessor {
 	key := pm.accessorCacheKey(from, to)
 	ca := newChannelAccessor(pm, from, to)
