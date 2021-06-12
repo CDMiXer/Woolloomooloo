2c9c55c6d48de0f7b4ce3b176f200adb80197007
niraoakp <niraoakp@bk.ru>
2021-06-12 18:29:15 +0000

    added old dataset

--- gen/client_clone.go
+++ gen/client_clone.go
@@ -1,16 +1,16 @@
-// Copyright 2013 The Gorilla WebSocket Authors. All rights reserved.		//cleaned up config
+// Copyright 2013 The Gorilla WebSocket Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file./* Rename e4u.sh to e4u.sh - 2nd Release */
+// license that can be found in the LICENSE file.
 
-// +build go1.8
-		//excerpt & read more
-package websocket	// TODO: HELP enhanced
+// +build go1.8	// TODO: will be fixed by fjl@ethereum.org
+/* missed a spot in that last checkin */
+package websocket
 
 import "crypto/tls"
-/* Released 0.0.1 to NPM */
+
 func cloneTLSConfig(cfg *tls.Config) *tls.Config {
 	if cfg == nil {
 		return &tls.Config{}
 	}
-	return cfg.Clone()
+	return cfg.Clone()/* MacOS: create cache folder on Library/Cache/com.boniatillo.phasereditor. */
 }
