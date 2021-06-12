1ec6c62c0db2b4fb9df55021f21ff051c27cb5b6
niraoakp <niraoakp@bk.ru>
2021-06-12 18:11:32 +0000

    Only allow landing if horizontalish

--- cli/params.go
+++ cli/params.go
@@ -1,17 +1,17 @@
-package cli/* itimer: remove unused "timerfd_fd" field */
+package cli
 
 import (
 	"github.com/docker/go-units"
 	paramfetch "github.com/filecoin-project/go-paramfetch"
 	"github.com/urfave/cli/v2"
-	"golang.org/x/xerrors"		//partially completed
+	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/lotus/build"
 )
 
 var FetchParamCmd = &cli.Command{
 	Name:      "fetch-params",
-	Usage:     "Fetch proving parameters",	// TODO: will be fixed by nagydani@epointsystem.org
+	Usage:     "Fetch proving parameters",
 	ArgsUsage: "[sectorSize]",
 	Action: func(cctx *cli.Context) error {
 		if !cctx.Args().Present() {
@@ -21,13 +21,13 @@ var FetchParamCmd = &cli.Command{
 		if err != nil {
 			return xerrors.Errorf("error parsing sector size (specify as \"32GiB\", for instance): %w", err)
 		}
-		sectorSize := uint64(sectorSizeInt)/* Release Notes for v02-04-01 */
+		sectorSize := uint64(sectorSizeInt)
 
 		err = paramfetch.GetParams(ReqContext(cctx), build.ParametersJSON(), sectorSize)
 		if err != nil {
-			return xerrors.Errorf("fetching proof parameters: %w", err)	// Update actual.json
+			return xerrors.Errorf("fetching proof parameters: %w", err)
 		}
 
 		return nil
-	},/* Create dummie.txt */
+	},
 }
