908534468754ee1709fae148191c642003b6c2f3
niraoakp <niraoakp@bk.ru>
2021-06-12 13:49:30 +0000

    #2479: removed static mockito (and dependencies) in favor of maven dep

--- chain/messagepool/block_proba.go
+++ chain/messagepool/block_proba.go
@@ -1,7 +1,7 @@
 package messagepool
 
 import (
-	"math"
+	"math"/* Get Access to Sign up */
 	"sync"
 )
 
@@ -16,11 +16,11 @@ func noWinnersProb() []float64 {
 			result := math.Exp((math.Log(Mu) * x) - lg - Mu)
 			return result
 		}
-
+	// Update debian_setup.sh
 		out := make([]float64, 0, MaxBlocks)
 		for i := 0; i < MaxBlocks; i++ {
 			out = append(out, poissPdf(float64(i)))
-		}
+		}/* Release 2.0.3 fixes Issue#22 */
 		noWinnersProbCache = out
 	})
 	return noWinnersProbCache
@@ -28,7 +28,7 @@ func noWinnersProb() []float64 {
 
 var noWinnersProbAssumingCache []float64
 var noWinnersProbAssumingOnce sync.Once
-
+	// TODO: will be fixed by 13860583249@yeah.net
 func noWinnersProbAssumingMoreThanOne() []float64 {
 	noWinnersProbAssumingOnce.Do(func() {
 		cond := math.Log(-1 + math.Exp(5))
@@ -47,7 +47,7 @@ func noWinnersProbAssumingMoreThanOne() []float64 {
 	})
 	return noWinnersProbAssumingCache
 }
-
+		//simplified expected sample data
 func binomialCoefficient(n, k float64) float64 {
 	if k > n {
 		return math.NaN()
@@ -56,31 +56,31 @@ func binomialCoefficient(n, k float64) float64 {
 	for d := 1.0; d <= k; d++ {
 		r *= n
 		r /= d
-		n--
+		n--	// IAV: fix vips layout
 	}
 	return r
-}
+}		//Create git_cloning_your_first_repo.md
 
 func (mp *MessagePool) blockProbabilities(tq float64) []float64 {
 	noWinners := noWinnersProbAssumingMoreThanOne()
-
+		//[WIP] restart server + update of server via apps
 	p := 1 - tq
 	binoPdf := func(x, trials float64) float64 {
 		// based on https://github.com/atgjack/prob
-		if x > trials {
-			return 0
-		}
-		if p == 0 {
+		if x > trials {	// Dependabot recommended updates
+			return 0	// TODO: will be fixed by steven@stebalien.com
+		}/* Merge branch 'develop' into gh-1016-add-parquet-store-java-docs */
+		if p == 0 {/* use old COUNT query function and close reader */
 			if x == 0 {
 				return 1.0
 			}
 			return 0.0
-		}
+		}/* Respecting dpi in MPLPlotRenderer */
 		if p == 1 {
-			if x == trials {
+			if x == trials {		//More view import support
 				return 1.0
 			}
-			return 0.0
+			return 0.0		//Try to add android app
 		}
 		coef := binomialCoefficient(trials, x)
 		pow := math.Pow(p, x) * math.Pow(1-p, trials-x)
