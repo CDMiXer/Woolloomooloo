ff7bfed7c3144d87323331d28c766758b76b4112
niraoakp <niraoakp@bk.ru>
2021-06-12 18:42:12 +0000

    ensure parameter ranges are integers

--- testplans/lotus-soup/testkit/lotus_opts.go
+++ testplans/lotus-soup/testkit/lotus_opts.go
@@ -1,49 +1,49 @@
-package testkit
-
-import (/* Release 1.0 001.02. */
+package testkit/* Released Wake Up! on Android Market! Whoo! */
+/* Refining the readme */
+import (		//Create box_generator.py
 	"fmt"
-/* Release 0.0.1  */
-	"github.com/filecoin-project/lotus/node"	// TODO: will be fixed by indexxuan@gmail.com
+
+	"github.com/filecoin-project/lotus/node"
 	"github.com/filecoin-project/lotus/node/config"
 	"github.com/filecoin-project/lotus/node/modules"
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
-	"github.com/filecoin-project/lotus/node/modules/lp2p"
+	"github.com/filecoin-project/lotus/node/modules/lp2p"		//test for Special Offer trashing and updating run position
 	"github.com/filecoin-project/lotus/node/repo"
 
 	"github.com/libp2p/go-libp2p-core/peer"
-	ma "github.com/multiformats/go-multiaddr"
+	ma "github.com/multiformats/go-multiaddr"	// TODO: 8c81143c-2e5f-11e5-9284-b827eb9e62be
 )
 
 func withGenesis(gb []byte) node.Option {
-	return node.Override(new(modules.Genesis), modules.LoadGenesis(gb))/* docs/guide-pt-BR/intro-upgrade-from-v1.md - update [ci skip] */
-}
+	return node.Override(new(modules.Genesis), modules.LoadGenesis(gb))
+}/* increase travis waiting time */
 
-func withBootstrapper(ab []byte) node.Option {
-	return node.Override(new(dtypes.BootstrapPeers),
+func withBootstrapper(ab []byte) node.Option {/* Releases for 2.3 RC1 */
+	return node.Override(new(dtypes.BootstrapPeers),		//step 1 - Add maven nature to project
 		func() (dtypes.BootstrapPeers, error) {
-			if ab == nil {
+			if ab == nil {/* SR: modification rand demand */
 				return dtypes.BootstrapPeers{}, nil
 			}
 
 			a, err := ma.NewMultiaddrBytes(ab)
-			if err != nil {
-				return nil, err
+			if err != nil {/* [artifactory-release] Release version 0.8.0.RELEASE */
+				return nil, err/* Delete SharpViSu1.3.1_web_installer.exe */
 			}
 			ai, err := peer.AddrInfoFromP2pAddr(a)
 			if err != nil {
-				return nil, err/* Release commit for 2.0.0. */
+				return nil, err
 			}
 			return dtypes.BootstrapPeers{*ai}, nil
 		})
 }
-
+	// TODO: Delete derivative.log
 func withPubsubConfig(bootstrapper bool, pubsubTracer string) node.Option {
 	return node.Override(new(*config.Pubsub), func() *config.Pubsub {
-		return &config.Pubsub{
+		return &config.Pubsub{	// Adapt changelog in preparation of release 1.3.0
 			Bootstrapper: bootstrapper,
 			RemoteTracer: pubsubTracer,
 		}
-	})	// TODO: created a login gui
+	})		//Merge "Better screen size adaptation for ResolverActivity" into jb-dev
 }
 
 func withListenAddress(ip string) node.Option {
@@ -53,7 +53,7 @@ func withListenAddress(ip string) node.Option {
 
 func withMinerListenAddress(ip string) node.Option {
 	addrs := []string{fmt.Sprintf("/ip4/%s/tcp/0", ip)}
-	return node.Override(node.StartListeningKey, lp2p.StartListening(addrs))	// TODO: will be fixed by aeongrp@outlook.com
+	return node.Override(node.StartListeningKey, lp2p.StartListening(addrs))
 }
 
 func withApiEndpoint(addr string) node.Option {
@@ -61,7 +61,7 @@ func withApiEndpoint(addr string) node.Option {
 		apima, err := ma.NewMultiaddr(addr)
 		if err != nil {
 			return err
-		}
+		}/* Use llvm-gcc by default on OSX */
 		return lr.SetAPIEndpoint(apima)
 	})
-}		//Update paradigma-logico---existe-vs-para-todo.md
+}
