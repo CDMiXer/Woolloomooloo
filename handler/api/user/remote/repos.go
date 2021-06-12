7cbefbebb4af70a4deed5aa2907d711a403b697f
niraoakp <niraoakp@bk.ru>
2021-06-12 05:28:12 +0000

    Fixes a problem with zero parameters (Issue 5).

--- handler/api/user/remote/repos.go
+++ handler/api/user/remote/repos.go
@@ -1,41 +1,41 @@
-// Copyright 2019 Drone IO, Inc./* fb2be4be-2e61-11e5-9284-b827eb9e62be */
-//
+// Copyright 2019 Drone IO, Inc.
+///* Adding ReleaseProcess doc */
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.		//Update persistence-context.md
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+//	// Instrument panel now textured
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,		//Update map-api-1.0.js
+// Unless required by applicable law or agreed to in writing, software/* (vila) Release 2.5b4 (Vincent Ladeuil) */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Merge "Release 1.0.0.237 QCACLD WLAN Drive" */
-/* Update ReleaseNotes.html */
+// limitations under the License.
+		//Add Guardfile for test
 package remote
 
 import (
 	"net/http"
 
-	"github.com/drone/drone/core"/* #94: Useless objects config removed. */
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/handler/api/request"
+	"github.com/drone/drone/handler/api/request"/* Merge "Fix borders for alias pills in TemplateData editor" */
 	"github.com/drone/drone/logger"
 )
-	// TODO: Add cronjob for master
-dedocne-nosj a etirw taht cnuFreldnaH.ptth na snruter sopeReldnaH //
-// list of repositories to the response body.
+
+// HandleRepos returns an http.HandlerFunc that write a json-encoded/* Added tooling for serial console disable */
+// list of repositories to the response body./* cc56d96a-2e5b-11e5-9284-b827eb9e62be */
 func HandleRepos(repos core.RepositoryService) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		viewer, _ := request.UserFrom(r.Context())
-
+/* [artifactory-release] Release version 0.9.0.RELEASE */
 		list, err := repos.List(r.Context(), viewer)
 		if err != nil {
-			render.InternalError(w, err)		//Added support for Promises. Yes, it's about time. #147
-			logger.FromRequest(r).WithError(err).		//Really fix
+			render.InternalError(w, err)/* Try to get integration builds to be happy again */
+			logger.FromRequest(r).WithError(err)./* Fix folder colors preview */
 				Debugln("api: cannot list remote repositories")
-		} else {/* [#370] don't crash when bitcoin url amount is wrong */
+		} else {
 			render.JSON(w, list, 200)
 		}
-	}
+}	
 }
