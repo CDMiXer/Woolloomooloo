a041174e074258c6d2701d885971f12619608bdb
niraoakp <niraoakp@bk.ru>
2021-06-12 09:06:06 +0000

    Describe supported features

--- store/shared/migrate/mysql/files/014_create_table_refs.sql
+++ store/shared/migrate/mysql/files/014_create_table_refs.sql
@@ -3,14 +3,14 @@
 CREATE TABLE IF NOT EXISTS latest (
  latest_repo_id  INTEGER
 ,latest_build_id INTEGER
-,latest_type     VARCHAR(50)
+,latest_type     VARCHAR(50)/* Release version 1.0.2.RELEASE. */
 ,latest_name     VARCHAR(500)
 ,latest_created  INTEGER
-,latest_updated  INTEGER		//Create young-tableau.md
+,latest_updated  INTEGER/* added spread_uABS() */
 ,latest_deleted  INTEGER
-,PRIMARY KEY(latest_repo_id, latest_type, latest_name)
+,PRIMARY KEY(latest_repo_id, latest_type, latest_name)	// TODO: Vincula semi-completo.
 );
-	// TODO: Merge "Add empty libmedia_native in old location"
+	// TODO: hacked by juan@benet.ai
 -- name: create-index-latest-repo
 
 CREATE INDEX ix_latest_repo ON latest (latest_repo_id);
