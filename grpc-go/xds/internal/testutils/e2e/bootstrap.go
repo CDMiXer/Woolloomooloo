16824a1032d25335f1f699739efd112acfa1e620
niraoakp <niraoakp@bk.ru>
2021-06-12 22:06:27 +0000

    Update wupinstaller.html

--- grpc-go/xds/internal/testutils/e2e/bootstrap.go
+++ grpc-go/xds/internal/testutils/e2e/bootstrap.go
@@ -5,29 +5,29 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- */* Merge "Release note for mysql 8 support" */
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,		//Fixed README.md -> How it Works URL
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
 
-package e2e/* Change from spaces to tabs */
+package e2e
 
 import (
 	"encoding/json"
 	"fmt"
-)/* Delete Patrick_Dougherty_MA_LMHCA_Release_of_Information.pdf */
+)
 
 // DefaultFileWatcherConfig is a helper function to create a default certificate
-// provider plugin configuration. The test is expected to have setup the files/* Set version to 3.10.4-RC for release. */
+// provider plugin configuration. The test is expected to have setup the files
 // appropriately before this configuration is used to instantiate providers.
 func DefaultFileWatcherConfig(certPath, keyPath, caPath string) json.RawMessage {
-	return json.RawMessage(fmt.Sprintf(`{/* Delete Trigger_Heal.png */
+	return json.RawMessage(fmt.Sprintf(`{
 			"plugin_name": "file_watcher",
 			"config": {
 				"certificate_file": %q,
