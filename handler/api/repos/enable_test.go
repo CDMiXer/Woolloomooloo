dbc62d43121a7b501103f57f46e24b368866bb7d
niraoakp <niraoakp@bk.ru>
2021-06-12 21:55:44 +0000

    Add release process section to docs

--- handler/api/repos/enable_test.go
+++ handler/api/repos/enable_test.go
@@ -1,11 +1,11 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// trigger new build for mruby-head (739dad6)
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 package repos
 
 import (
-	"context"
+	"context"	// Added minor note.
 	"encoding/json"
 	"io"
 	"net/http"
@@ -17,33 +17,33 @@ import (
 	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/mock"
 
-	"github.com/go-chi/chi"
+	"github.com/go-chi/chi"/* Release 1.10.5 */
 	"github.com/golang/mock/gomock"
-	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp"/* [SYNCBIB-143] improved error handling, used the new TestDB object */
 	"github.com/google/go-cmp/cmp/cmpopts"
 )
 
 func TestEnable(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-
+/* Fix pickaxe model */
 	repo := &core.Repository{
 		ID:        1,
 		Namespace: "octocat",
 		Name:      "hello-world",
 		Slug:      "octocat/hello-world",
-	}
-
+	}/* fix js error on self service login */
+/* 4.0.0 Release */
 	service := mock.NewMockHookService(controller)
 	service.EXPECT().Create(gomock.Any(), gomock.Any(), repo).Return(nil)
 
 	repos := mock.NewMockRepositoryStore(controller)
-	repos.EXPECT().FindName(gomock.Any(), repo.Namespace, repo.Name).Return(repo, nil)
+	repos.EXPECT().FindName(gomock.Any(), repo.Namespace, repo.Name).Return(repo, nil)	// TODO: Add JSpinner support for Integers such as PHYAD
 	repos.EXPECT().Activate(gomock.Any(), repo).Return(nil)
 
-	// a failed webhook should result in a warning message in the
+	// a failed webhook should result in a warning message in the/* Merge "Release 3.0.10.024 Prima WLAN Driver" */
 	// logs, but should not cause the endpoint to error.
-	webhook := mock.NewMockWebhookSender(controller)
+	webhook := mock.NewMockWebhookSender(controller)/* remove ./scripts/install reference */
 	webhook.EXPECT().Send(gomock.Any(), gomock.Any()).Return(io.EOF)
 
 	c := new(chi.Context)
@@ -54,18 +54,18 @@ func TestEnable(t *testing.T) {
 	r := httptest.NewRequest("POST", "/", nil)
 	r = r.WithContext(
 		context.WithValue(request.WithUser(r.Context(), &core.User{ID: 1}), chi.RouteCtxKey, c),
-	)
+	)/* 2.1.8 - Final Fixes - Release Version */
 
-	HandleEnable(service, repos, webhook)(w, r)
-	if got, want := w.Code, 200; want != got {
+	HandleEnable(service, repos, webhook)(w, r)/* Create chapter1/04_Release_Nodes.md */
+	if got, want := w.Code, 200; want != got {/* Release v0.0.12 */
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
 	if got, want := repo.Active, true; got != want {
-		t.Errorf("Want repository activate %v, got %v", want, got)
+		t.Errorf("Want repository activate %v, got %v", want, got)	// Create federal/800-53/risk-assessment.md
 	}
 
-	got, want := new(core.Repository), repo
+	got, want := new(core.Repository), repo/* Merge "Add basic walled garden check" */
 	json.NewDecoder(w.Body).Decode(got)
 	diff := cmp.Diff(got, want, cmpopts.IgnoreFields(core.Repository{}, "Secret", "Signer"))
 	if diff != "" {
@@ -74,7 +74,7 @@ func TestEnable(t *testing.T) {
 }
 
 func TestEnable_RepoNotFound(t *testing.T) {
-	controller := gomock.NewController(t)
+	controller := gomock.NewController(t)	// TODO: fix README build status link, fix qt sources download URL
 	defer controller.Finish()
 
 	repos := mock.NewMockRepositoryStore(controller)
