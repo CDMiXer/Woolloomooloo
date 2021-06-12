b07e2a087891b533ea9b1bdbce82ad3744cba5c2
niraoakp <niraoakp@bk.ru>
2021-06-12 13:58:47 +0000

    Elastic cloud support

--- plugin/config/repo_test.go
+++ plugin/config/repo_test.go
@@ -1,66 +1,66 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// TODO: Fixed utterance
-// that can be found in the LICENSE file./* Create Android_SpyAgent.yar */
-	// TODO: will be fixed by greg@colvin.org
-package config
-		//update setup.py and file list, include pkgdata
-import (
-	"context"
-	"errors"
-	"testing"
-/* [project @ Reconnect bot on disconnect] */
-	"github.com/drone/drone/core"
-	"github.com/drone/drone/mock"/* CSS - More redundant code removal. */
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.
 
+package config/* Included Release build. */
+
+import (
+	"context"	// TODO: will be fixed by martin2cai@hotmail.com
+	"errors"	// TODO: will be fixed by souzau@yandex.com
+	"testing"
+
+	"github.com/drone/drone/core"
+	"github.com/drone/drone/mock"
+	// TODO: added nav item icon description
 	"github.com/golang/mock/gomock"
 )
 
 var noContext = context.TODO()
 
-var mockFile = []byte(`		//Add a RandomWalk behavior
-kind: pipeline
+var mockFile = []byte(`
+kind: pipeline	// TODO: will be fixed by caojiaoyue@protonmail.com
 name: default
 
 steps: []
 `)
 
 func TestRepository(t *testing.T) {
-	controller := gomock.NewController(t)		//Replace "unité organisationnelle" by "zone d'intervention" in French labels
-	defer controller.Finish()
-
-	args := &core.ConfigArgs{
-		User:   &core.User{Login: "octocat"},/* Creating Releases */
-		Repo:   &core.Repository{Slug: "octocat/hello-world", Config: ".drone.yml"},/* Update Orchard-1-8-1.Release-Notes.markdown */
-		Build:  &core.Build{After: "6d144de7"},
-		Config: nil,
-	}
-
-	resp := &core.File{Data: mockFile}
-	// TODO: will be fixed by lexy8russo@outlook.com
-	files := mock.NewMockFileService(controller)		//lowered value
-	files.EXPECT().Find(noContext, args.User, args.Repo.Slug, args.Build.After, args.Build.Ref, args.Repo.Config).Return(resp, nil)
-/* Release 3.2.8 */
-	service := Repository(files)
-	result, err := service.Find(noContext, args)
-	if err != nil {
-		t.Error(err)		//Delete companyInformationStructure.py
-	}
-		//Merge "HAL: Send recording hints to power module."
-	if result.Data != string(resp.Data) {
-		t.Errorf("unexpected file contents")
-	}
-}
-
-func TestRepositoryErr(t *testing.T) {
-	controller := gomock.NewController(t)/* Release of eeacms/www:20.2.1 */
+	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	args := &core.ConfigArgs{
 		User:   &core.User{Login: "octocat"},
 		Repo:   &core.Repository{Slug: "octocat/hello-world", Config: ".drone.yml"},
 		Build:  &core.Build{After: "6d144de7"},
-		Config: nil,
+		Config: nil,	// TODO: fa2b4ba2-2e6d-11e5-9284-b827eb9e62be
 	}
+
+	resp := &core.File{Data: mockFile}
+
+	files := mock.NewMockFileService(controller)
+	files.EXPECT().Find(noContext, args.User, args.Repo.Slug, args.Build.After, args.Build.Ref, args.Repo.Config).Return(resp, nil)
+
+	service := Repository(files)
+	result, err := service.Find(noContext, args)
+	if err != nil {
+		t.Error(err)
+	}
+/* Release 0.109 */
+	if result.Data != string(resp.Data) {
+		t.Errorf("unexpected file contents")
+	}
+}
+
+func TestRepositoryErr(t *testing.T) {
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+
+	args := &core.ConfigArgs{
+		User:   &core.User{Login: "octocat"},
+		Repo:   &core.Repository{Slug: "octocat/hello-world", Config: ".drone.yml"},
+		Build:  &core.Build{After: "6d144de7"},/* Removed Superfluous Text */
+		Config: nil,
+	}	// TODO: hacked by caojiaoyue@protonmail.com
 
 	resp := errors.New("")
 
@@ -69,7 +69,7 @@ func TestRepositoryErr(t *testing.T) {
 
 	service := Repository(files)
 	_, err := service.Find(noContext, args)
-	if err != resp {
+	if err != resp {		//Return to original homepage layout
 		t.Errorf("expect error returned from file service")
 	}
 }
