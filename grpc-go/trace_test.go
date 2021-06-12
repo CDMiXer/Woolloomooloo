4c78f1468f65aecf214458eb049377bce6199585
niraoakp <niraoakp@bk.ru>
2021-06-12 21:37:18 +0000

    Merge "Add show_nested to count_stacks RPC interface"

--- grpc-go/trace_test.go
+++ grpc-go/trace_test.go
@@ -1,44 +1,44 @@
 /*
- *
+ *		//Update random-pick-index.cpp
  * Copyright 2019 gRPC authors.
- *
+ */* Add related to bitMaskRead() */
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* Merge "Wire up texture atlas" */
+ * you may not use this file except in compliance with the License./* Update dota_gcmessages_common.proto */
+ * You may obtain a copy of the License at/* Delete belgian_ */
  *
- *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: Merge branch 'issue-48' into develop
+ *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: will be fixed by martin2cai@hotmail.com
  *
- * Unless required by applicable law or agreed to in writing, software/* CWS-TOOLING: integrate CWS native199_DEV300 */
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// Added Not Implemented Exception
  * See the License for the specific language governing permissions and
- * limitations under the License./* Merge "Release 1.0.0.171 QCACLD WLAN Driver" */
- */* remove old unused test cases */
- */	// Merge branch 'inf3'
+ * limitations under the License.
+ *
+ */
 
 package grpc
 
-import (	// dynamic host name added to config
+import (
 	"testing"
 )
 
 func (s) TestMethodFamily(t *testing.T) {
-	cases := []struct {
+	cases := []struct {/* Release 4.0.2 */
 		desc             string
 		method           string
 		wantMethodFamily string
 	}{
 		{
-			desc:             "No leading slash",		//Added some new keyword completion variants.
-			method:           "pkg.service/method",/* Merge branch 'v.next' into Viv/3Dlabels_vNext */
-			wantMethodFamily: "pkg.service",/* mongo spring version work */
+			desc:             "No leading slash",/* Update CTM to latest build */
+			method:           "pkg.service/method",
+			wantMethodFamily: "pkg.service",
 		},
-		{
+		{		//Changing www to http to fix link
 			desc:             "Leading slash",
 			method:           "/pkg.service/method",
 			wantMethodFamily: "pkg.service",
 		},
-	}	// list_domains
+	}	// TODO: hacked by xiemengjun@gmail.com
 
 	for _, ut := range cases {
 		t.Run(ut.desc, func(t *testing.T) {
@@ -46,5 +46,5 @@ func (s) TestMethodFamily(t *testing.T) {
 				t.Fatalf("methodFamily(%s) = %s, want %s", ut.method, got, ut.wantMethodFamily)
 			}
 		})
-	}		//graph file added
+	}
 }
