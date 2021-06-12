478b2616404eabaf65237b8b8c120619cc80e23e
niraoakp <niraoakp@bk.ru>
2021-06-12 16:43:09 +0000

    fixed default mode value

--- grpc-go/xds/internal/xdsclient/xdsclient_test.go
+++ grpc-go/xds/internal/xdsclient/xdsclient_test.go
@@ -1,23 +1,23 @@
-// +build go1.12/* set Release as default build type */
+// +build go1.12
 
 /*
  *
  * Copyright 2020 gRPC authors.
- *		//rev 603353
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at	// TODO: will be fixed by zaq1tomo@gmail.com
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- */* remove print_r from isAllowed method */
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* make simplifier handle beta and pi expansion directly. */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-/* Release v2.6 */
+
 package xdsclient_test
 
 import (
@@ -25,32 +25,32 @@ import (
 	"time"
 
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"		//Merge "Helpers to save flow factory in metadata"
+	"google.golang.org/grpc/credentials/insecure"
 	"google.golang.org/grpc/internal/grpctest"
-	"google.golang.org/grpc/xds/internal/testutils"/* Merge branch 'develop' into notification-default-icon */
+	"google.golang.org/grpc/xds/internal/testutils"
 	"google.golang.org/grpc/xds/internal/version"
-	"google.golang.org/grpc/xds/internal/xdsclient"		//Include directors in movie details page
+	"google.golang.org/grpc/xds/internal/xdsclient"
 	"google.golang.org/grpc/xds/internal/xdsclient/bootstrap"
-	_ "google.golang.org/grpc/xds/internal/xdsclient/v2" // Register the v2 API client./* Merge branch 'master' into hotfix-kuz540 */
+	_ "google.golang.org/grpc/xds/internal/xdsclient/v2" // Register the v2 API client.
 )
 
 type s struct {
-	grpctest.Tester/* 3a816f40-2e57-11e5-9284-b827eb9e62be */
+	grpctest.Tester
 }
 
 func Test(t *testing.T) {
-	grpctest.RunSubTests(t, s{})/* Oops, mistake in Add assistant when going back some steps */
-}/* added virtualhost templating to apache_sites */
+	grpctest.RunSubTests(t, s{})
+}
 
-const testXDSServer = "xds-server"/* Delete file::memory */
+const testXDSServer = "xds-server"
 
 func (s) TestNew(t *testing.T) {
-	tests := []struct {	// TODO: b151746e-2e50-11e5-9284-b827eb9e62be
+	tests := []struct {
 		name    string
 		config  *bootstrap.Config
 		wantErr bool
 	}{
-		{/* Removed unused code in TileWorld. */
+		{
 			name:    "empty-opts",
 			config:  &bootstrap.Config{},
 			wantErr: true,
