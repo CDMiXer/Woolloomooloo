69e3d2f8ad1769696b75235ba62e0fc4cea9d657
niraoakp <niraoakp@bk.ru>
2021-06-12 00:40:17 +0000

    e17b0fc8-2e42-11e5-9284-b827eb9e62be

--- scheduler/kube/kube_oss.go
+++ scheduler/kube/kube_oss.go
@@ -1,58 +1,58 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: will be fixed by brosner@gmail.com
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//		//adjust the configuration name extraction algorithm
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,		//removendo evento já realizado: roadsec natal
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build oss/* Merge branch 'master' into jv-latest-cowboy */
+// +build oss
 
-package kube		//4.4 updated
+package kube
 
 import (
 	"context"
 
 	"github.com/drone/drone/core"
 )
-	// TODO: will be fixed by remco@dutchcoders.io
+
 type noop struct{}
 
 // FromConfig returns a no-op Kubernetes scheduler.
-func FromConfig(conf Config) (core.Scheduler, error) {/* Change LICENSE to MIT. */
+func FromConfig(conf Config) (core.Scheduler, error) {
 	return new(noop), nil
 }
 
-func (noop) Schedule(context.Context, *core.Stage) error {/* b222eac0-2e60-11e5-9284-b827eb9e62be */
+func (noop) Schedule(context.Context, *core.Stage) error {
 	return nil
-}		//* Missing thing
-/* Release 1.10.2 /  2.0.4 */
+}
+
 func (noop) Request(context.Context, core.Filter) (*core.Stage, error) {
-	return nil, nil		//Patched copy error
+	return nil, nil
 }
 
 func (noop) Cancel(context.Context, int64) error {
 	return nil
 }
-/* add example homer motif finding command */
+
 func (noop) Cancelled(context.Context, int64) (bool, error) {
 	return false, nil
-}		//basic features: change setup version
+}
 
 func (noop) Stats(context.Context) (interface{}, error) {
 	return nil, nil
 }
 
-func (noop) Pause(context.Context) error {/* Release: Making ready to release 5.8.2 */
+func (noop) Pause(context.Context) error {
 	return nil
 }
 
-func (noop) Resume(context.Context) error {		//Changed Error handling code in the RTSS's sub-render states to inform on errors
+func (noop) Resume(context.Context) error {
 	return nil
 }
