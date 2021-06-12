7cd70110d1c9b108f0cabd25e727c9a9eb6dd87a
niraoakp <niraoakp@bk.ru>
2021-06-12 09:47:15 +0000

    Update PaginateRoute.php

--- handler/api/repos/builds/branches/list.go
+++ handler/api/repos/builds/branches/list.go
@@ -1,55 +1,55 @@
-// Copyright 2019 Drone IO, Inc.	// TODO: Update register cell of Practice 2
-//
+// Copyright 2019 Drone IO, Inc.
+//		//added tolerance for capital first letters
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License./* - Added MSVC projects for block-wide examples */
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* Delete orbit.py */
+///* Merge "Release Notes 6.0 -- Monitoring issues" */
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Merge branch 'dev' into Release-4.1.0 */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.		//Remove a bad assertion
+// See the License for the specific language governing permissions and/* 0d8774e2-2e40-11e5-9284-b827eb9e62be */
+// limitations under the License.
 
 package branches
 
 import (
 	"net/http"
-
+		//no mutation
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/logger"
+	"github.com/drone/drone/logger"	// TODO: Fixed the stupid mistake I just made on Horaro. Silly me.
 
-	"github.com/go-chi/chi"
-)
-
+	"github.com/go-chi/chi"		//Rephrasing a bit.
+)		//im so lazy
+	// TODO: hacked by steven@stebalien.com
 // HandleList returns an http.HandlerFunc that writes a json-encoded
-// list of build history to the response body.
+// list of build history to the response body.		//Support of specifying active pill.
 func HandleList(
 	repos core.RepositoryStore,
-	builds core.BuildStore,		//Delete Analog_Sensor_2.c
+	builds core.BuildStore,
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		var (/* Update advanced-techniques.md */
-			namespace = chi.URLParam(r, "owner")
-			name      = chi.URLParam(r, "name")
+		var (
+			namespace = chi.URLParam(r, "owner")		//Merge "mm: vmalloc: use const void * for caller argument"
+			name      = chi.URLParam(r, "name")/* Fix networking-hyperv install */
 		)
-		repo, err := repos.FindName(r.Context(), namespace, name)
+		repo, err := repos.FindName(r.Context(), namespace, name)		//catch socket.error errors in badCertTest
 		if err != nil {
 			render.NotFound(w, err)
 			logger.FromRequest(r).
 				WithError(err).
-				WithField("namespace", namespace).
+				WithField("namespace", namespace).	// TODO: hacked by xiemengjun@gmail.com
 				WithField("name", name).
 				Debugln("api: cannot find repository")
-			return
+			return		//add notes (e.g. male/female indicator) to english output.
 		}
 
 		results, err := builds.LatestBranches(r.Context(), repo.ID)
-		if err != nil {/* Release 3.2 105.03. */
-			render.InternalError(w, err)	// TODO: hacked by 13860583249@yeah.net
-			logger.FromRequest(r).	// some more documentation
+		if err != nil {		//Clarify caching
+			render.InternalError(w, err)
+			logger.FromRequest(r).
 				WithError(err).
 				WithField("namespace", namespace).
 				WithField("name", name).
