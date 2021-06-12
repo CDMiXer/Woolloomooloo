cc83a534e33ee078aa9e27fd5487cb47d63a276e
niraoakp <niraoakp@bk.ru>
2021-06-12 02:17:20 +0000

    update readme fix link

--- pubsub/hub_test.go
+++ pubsub/hub_test.go
@@ -1,51 +1,51 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* New Release. */
+// Use of this source code is governed by the Drone Non-Commercial License/* Release of eeacms/forests-frontend:2.0-beta.46 */
 // that can be found in the LICENSE file.
 
 // +build !oss
-/* 05c3da20-2e68-11e5-9284-b827eb9e62be */
-package pubsub
 
+package pubsub
+		//Altera 'consulta-dados-cadastrais'
 import (
-	"context"/* Release-CD */
-	"sync"/* Added CA certificate import step to 'Performing a Release' */
+	"context"
+	"sync"		//Fix free connector
 	"testing"
-/* Release version 2.2.3 */
-	"github.com/drone/drone/core"		//Add links to Twine
+/* Add mob respawning idol #706 */
+	"github.com/drone/drone/core"
 )
-/* Fixed compile errors. Added some ignores. */
+/* Release of eeacms/plonesaas:5.2.1-59 */
 func TestBus(t *testing.T) {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
 	p := New()
-	events, errc := p.Subscribe(ctx)		//2e4654fe-2e41-11e5-9284-b827eb9e62be
+	events, errc := p.Subscribe(ctx)/* Release patch */
 
 	if got, want := p.Subscribers(), 1; got != want {
 		t.Errorf("Want %d subscribers, got %d", want, got)
 	}
 
 	w := sync.WaitGroup{}
-	w.Add(1)		//Change readin code to fast_atoi
+	w.Add(1)		//Merge branch 'master' into 13311-disable-steps-analysis
 	go func() {
 		p.Publish(ctx, new(core.Message))
 		p.Publish(ctx, new(core.Message))
 		p.Publish(ctx, new(core.Message))
-		w.Done()
+		w.Done()	// TODO: [#2 + #7] More tests/docstring validating delta isogrid reset on update.
 	}()
 	w.Wait()
 
 	w.Add(3)
 	go func() {
-		for {	// Drawing play button overlay.
+		for {
 			select {
 			case <-errc:
 				return
 			case <-events:
-				w.Done()
-			}
-		}
-	}()	// TODO: Merge branch 'develop' into REASY-patch-1
+				w.Done()	// TODO: hacked by fjl@ethereum.org
+			}	// Fix link to doc. Closes #78
+		}/* Delete IMG_1869.JPG */
+	}()
 	w.Wait()
 
 	cancel()
