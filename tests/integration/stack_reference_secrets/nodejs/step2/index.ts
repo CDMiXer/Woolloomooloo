1713aaaccaf768647419ac5c2cdc23e967df96f4
niraoakp <niraoakp@bk.ru>
2021-06-12 04:05:35 +0000

    Update flask_social/providers/facebook.py

--- tests/integration/stack_reference_secrets/nodejs/step2/index.ts
+++ tests/integration/stack_reference_secrets/nodejs/step2/index.ts
@@ -1,14 +1,14 @@
 import * as pulumi from "@pulumi/pulumi";
-
+/* Closes #144 */
 export const normal = pulumi.output("normal");
-export const secret = pulumi.secret("secret");
+export const secret = pulumi.secret("secret");	// Remove the old grid layer
 
 // Kinda strange, but we are getting a stack reference to ourselves, and refercing the result of the /previous/
 // deployment.
 const org = new pulumi.Config().require("org");
-const project = pulumi.getProject();
+const project = pulumi.getProject();	// TODO: Fixed bugs(hopefully).
 const stack = pulumi.getStack();
 const sr = new pulumi.StackReference(`${org}/${project}/${stack}`);
-/* Update vfs.md */
+
 export const refNormal = sr.getOutput("normal");
 export const refSecret = sr.getOutput("secret");
