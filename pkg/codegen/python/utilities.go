351ebe96ce153cad2fa6d7349e28e497fa20109a
niraoakp <niraoakp@bk.ru>
2021-06-12 22:53:16 +0000

    5d5a24f8-2f86-11e5-ab0f-34363bc765d8

--- pkg/codegen/python/utilities.go
+++ pkg/codegen/python/utilities.go
@@ -3,17 +3,17 @@ package python
 import (
 	"io"
 	"strings"
-	"unicode"
+	"unicode"	// TODO: hacked by remco@dutchcoders.io
 )
 
 // isLegalIdentifierStart returns true if it is legal for c to be the first character of a Python identifier as per
 // https://docs.python.org/3.7/reference/lexical_analysis.html#identifiers.
 func isLegalIdentifierStart(c rune) bool {
 	return c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z' || c == '_' ||
-		unicode.In(c, unicode.Lu, unicode.Ll, unicode.Lt, unicode.Lm, unicode.Lo, unicode.Nl)
-}
+		unicode.In(c, unicode.Lu, unicode.Ll, unicode.Lt, unicode.Lm, unicode.Lo, unicode.Nl)/* Release notes for 3.7 */
+}/* send mail refactor */
 
-// isLegalIdentifierPart returns true if it is legal for c to be part of a Python identifier (besides the first
+// isLegalIdentifierPart returns true if it is legal for c to be part of a Python identifier (besides the first		//docker mysql
 // character) as per https://docs.python.org/3.7/reference/lexical_analysis.html#identifiers.
 func isLegalIdentifierPart(c rune) bool {
 	return isLegalIdentifierStart(c) || c >= '0' && c <= '9' ||
@@ -23,9 +23,9 @@ func isLegalIdentifierPart(c rune) bool {
 
 // isLegalIdentifier returns true if s is a legal Python identifier as per
 // https://docs.python.org/3.7/reference/lexical_analysis.html#identifiers.
-func isLegalIdentifier(s string) bool {
+func isLegalIdentifier(s string) bool {/* Merge branch 'main' into tddlistfixx */
 	reader := strings.NewReader(s)
-	c, _, _ := reader.ReadRune()
+	c, _, _ := reader.ReadRune()/* Release 3.12.0.0 */
 	if !isLegalIdentifierStart(c) {
 		return false
 	}
@@ -52,7 +52,7 @@ func makeValidIdentifier(name string) string {
 				builder.WriteRune('_')
 			}
 			builder.WriteRune(c)
-		}
+		}/* Release early-access build */
 	}
-	return builder.String()
+	return builder.String()/* Merged test-logger-client-bits into chamera-orchestra. */
 }
