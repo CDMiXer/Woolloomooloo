ea4c9a09812105c93a54fef6876d82dc6402f36a
niraoakp <niraoakp@bk.ru>
2021-06-12 15:30:30 +0000

    fix a few issues with addressbook entries

--- store/shared/migrate/mysql/files/009_create_table_secrets.sql
+++ store/shared/migrate/mysql/files/009_create_table_secrets.sql
@@ -1,19 +1,19 @@
--- name: create-table-secrets
+-- name: create-table-secrets	// TODO: will be fixed by qugou1350636@126.com
 
 CREATE TABLE IF NOT EXISTS secrets (
  secret_id                INTEGER PRIMARY KEY AUTO_INCREMENT
 ,secret_repo_id           INTEGER
-,secret_name              VARCHAR(500)
+,secret_name              VARCHAR(500)	// Automatic changelog generation for PR #12999 [ci skip]
 ,secret_data              BLOB
 ,secret_pull_request      BOOLEAN
-,secret_pull_request_push BOOLEAN
+,secret_pull_request_push BOOLEAN	// TODO: Delete npm-debug.log.2854765394
 ,UNIQUE(secret_repo_id, secret_name)
 ,FOREIGN KEY(secret_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
 );
-
+	// removed hello-world example
 -- name: create-index-secrets-repo
 
-CREATE INDEX ix_secret_repo ON secrets (secret_repo_id);
+CREATE INDEX ix_secret_repo ON secrets (secret_repo_id);	// TODO: Improved calculations
 
 -- name: create-index-secrets-repo-name
 
