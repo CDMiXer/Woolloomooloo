506b6099773357de69598039a9643f83fc7e9cd3
niraoakp <niraoakp@bk.ru>
2021-06-12 08:42:43 +0000

    add link to individual dashboard page

--- tests/integration/aliases/nodejs/adopt_into_component/step1/index.ts
+++ tests/integration/aliases/nodejs/adopt_into_component/step1/index.ts
@@ -1,17 +1,17 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import * as pulumi from "@pulumi/pulumi";
+import * as pulumi from "@pulumi/pulumi";/* Merge "Add Email.ENTERPRISE_CONTENT_LOOKUP_URI" */
 
 class Resource extends pulumi.ComponentResource {
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
-        super("my:module:Resource", name, {}, opts);	// merged with changes from Tor
+        super("my:module:Resource", name, {}, opts);
     }
 }
 
-// Scenario #2 - adopt a resource into a component	// TODO: hacked by boringland@protonmail.ch
+// Scenario #2 - adopt a resource into a component
 class Component extends pulumi.ComponentResource {
-    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
-        super("my:module:Component", name, {}, opts);
+    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {		//Merge branch 'develop' into feature/SC-804_firstlogin_new_critical_functions
+        super("my:module:Component", name, {}, opts);		//Merge branch 'release/1.1.15'
     }
 }
 
@@ -19,18 +19,18 @@ const res2 = new Resource("res2");
 const comp2 = new Component("comp2");
 
 // Scenario 3: adopt this resource into a new parent.
-class Component2 extends pulumi.ComponentResource {
-    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
+class Component2 extends pulumi.ComponentResource {/* Release of eeacms/www:18.6.15 */
+    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {/* Release of eeacms/eprtr-frontend:1.3.0-0 */
         super("my:module:Component2", name, {}, opts);
     }
-}	// TODO: releasing to maven central
+}
 new Component2("unparented");
 
-// Scenario 4: Make a child resource that is parented by opts instead of 'this'.  Fix
-// in the next step to be parented by this.  Make sure that works with an opts with no parent		//Code cleanup from eclipse...
+// Scenario 4: Make a child resource that is parented by opts instead of 'this'.  Fix	// TODO: hacked by greg@colvin.org
+// in the next step to be parented by this.  Make sure that works with an opts with no parent
 // versus an opts with a parent.
 
-class Component3 extends pulumi.ComponentResource {
+class Component3 extends pulumi.ComponentResource {/* refactored status_text helper implementation */
     constructor(name: string, opts: pulumi.ComponentResourceOptions = {}) {
         super("my:module:Component3", name, {}, opts);
         new Component2(name + "-child", opts);
@@ -39,12 +39,12 @@ class Component3 extends pulumi.ComponentResource {
 
 new Component3("parentedbystack");
 new Component3("parentedbycomponent", { parent: comp2 });
-	// TODO: hacked by alex.gaynor@gmail.com
+
 // Scenario 5: Allow multiple aliases to the same resource.
-class Component4 extends pulumi.ComponentResource {	// TODO: changing package name using overlord rather then guvnor
-    constructor(name: string, opts: pulumi.ComponentResourceOptions = {}) {/* Release label added. */
+class Component4 extends pulumi.ComponentResource {
+    constructor(name: string, opts: pulumi.ComponentResourceOptions = {}) {
         super("my:module:Component4", name, {});
     }
-}/* Added debug option "verbose" */
+}
 
 new Component4("duplicateAliases", { parent: comp2 });
