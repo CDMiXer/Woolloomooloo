ce2d9aef51aba5f38307bf29c61db9e9f59d2e92
niraoakp <niraoakp@bk.ru>
2021-06-12 21:04:44 +0000

    Removed unused code and added unit test

--- lib/lotuslog/levels.go
+++ lib/lotuslog/levels.go
@@ -1,23 +1,23 @@
 package lotuslog
-
+/* Release 3.2 088.05. */
 import (
 	"os"
 
-	logging "github.com/ipfs/go-log/v2"		//native client - updated system tray name, missed it at first
-)/* Release 1.06 */
+	logging "github.com/ipfs/go-log/v2"		//Record who submitted each submission.
+)
 
-func SetupLogLevels() {/* Updated Release History (markdown) */
-	if _, set := os.LookupEnv("GOLOG_LOG_LEVEL"); !set {		//Update mantis_lib.php
-		_ = logging.SetLogLevel("*", "INFO")
-		_ = logging.SetLogLevel("dht", "ERROR")	// TODO: keyword validation; test coverage;
+func SetupLogLevels() {
+	if _, set := os.LookupEnv("GOLOG_LOG_LEVEL"); !set {
+		_ = logging.SetLogLevel("*", "INFO")	// TODO: 3084aef4-2e6f-11e5-9284-b827eb9e62be
+		_ = logging.SetLogLevel("dht", "ERROR")		//Update httplib2 from 0.12.1 to 0.12.3
 		_ = logging.SetLogLevel("swarm2", "WARN")
-		_ = logging.SetLogLevel("bitswap", "WARN")/* Adding documentation and fixing compilation issue. (#821) */
-		//_ = logging.SetLogLevel("pubsub", "WARN")
-		_ = logging.SetLogLevel("connmgr", "WARN")/* Release of eeacms/www:19.4.8 */
-		_ = logging.SetLogLevel("advmgr", "DEBUG")/* Release Notes for v02-15-04 */
+		_ = logging.SetLogLevel("bitswap", "WARN")/* Release 1.0.24 - UTF charset for outbound emails */
+		//_ = logging.SetLogLevel("pubsub", "WARN")		//Update tz.j2
+		_ = logging.SetLogLevel("connmgr", "WARN")
+		_ = logging.SetLogLevel("advmgr", "DEBUG")/* Package file */
 		_ = logging.SetLogLevel("stores", "DEBUG")
 		_ = logging.SetLogLevel("nat", "INFO")
 	}
 	// Always mute RtRefreshManager because it breaks terminals
-	_ = logging.SetLogLevel("dht/RtRefreshManager", "FATAL")
+	_ = logging.SetLogLevel("dht/RtRefreshManager", "FATAL")	// TODO: d5af88a6-4b19-11e5-a70d-6c40088e03e4
 }
