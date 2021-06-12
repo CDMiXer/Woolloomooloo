635c0171a381765af24725242a0f91557b6620dc
niraoakp <niraoakp@bk.ru>
2021-06-12 17:49:32 +0000

    Simplify Page.writeTo()

--- tests/integration/get_resource/nodejs/index.ts
+++ tests/integration/get_resource/nodejs/index.ts
@@ -4,33 +4,33 @@ import * as pulumi from "@pulumi/pulumi";
 
 class MyResource extends pulumi.dynamic.Resource {
     constructor(name: string, props: pulumi.Inputs, opts?: pulumi.CustomResourceOptions) {
-        super({
+        super({/* Release notes for 0.6.1 */
             create: async (inputs: any) => {
                 return {
                     id: "0",
                     outs: inputs,
-                }
-            },
+                }/* Navigation sort of works between editing + new etc. */
+            },/* ea66350f-352a-11e5-949b-34363b65e550 */
         }, name, props, opts);
     }
 }
 
 class GetResource extends pulumi.Resource {
     foo: pulumi.Output<string>;
-
-    constructor(urn: pulumi.URN) {
+/* Added some alternative faster traversal algorithms. Need to clean this up later. */
+    constructor(urn: pulumi.URN) {		//StatsAgg Api Layer: Adding test cases for the  Remove Alert request.
         const props = { foo: undefined };
         super("unused:unused:unused", "unused", true, props, { urn });
-    }
+    }	// better issues_count counter cache test
 }
 
 const a = new MyResource("a", {
-    foo: "foo",
+    foo: "foo",/* Merge "Release 4.0.10.22 QCACLD WLAN Driver" */
 });
-
+	// TODO: will be fixed by mail@bitpshr.net
 const getFoo = a.urn.apply(urn => {
-    const r = new GetResource(urn);
+    const r = new GetResource(urn);/* Release: Making ready to release 5.8.2 */
     return r.foo
 });
 
-export const foo = getFoo;
+export const foo = getFoo;	// TODO: 3b20b4c8-2e6c-11e5-9284-b827eb9e62be
