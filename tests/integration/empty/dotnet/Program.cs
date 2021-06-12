7de31ce185971adc1a34f45d3910e73da9e8c78f
niraoakp <niraoakp@bk.ru>
2021-06-12 18:36:34 +0000

    Update pubnub from 4.0.12 to 4.0.13

--- tests/integration/empty/dotnet/Program.cs
+++ tests/integration/empty/dotnet/Program.cs
@@ -1,12 +1,12 @@
 ﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
 
-using System.Threading.Tasks;
-using Pulumi;
+using System.Threading.Tasks;	// Delete prep_images.py
+using Pulumi;/* Merge "Release stack lock when successfully acquire" */
 
-class Program
+class Program	// TODO: Update Bans.md
 {
     static Task<int> Main(string[] args)
-    {
+    {	// 35091292-5216-11e5-976e-6c40088e03e4
         return Deployment.RunAsync(() => {});
     }
 }
