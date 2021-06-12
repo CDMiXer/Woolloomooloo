a7df0f048bc0fe023b85421780c03ef981ec7602
niraoakp <niraoakp@bk.ru>
2021-06-12 14:36:13 +0000

    e68d52b6-2e63-11e5-9284-b827eb9e62be

--- tests/integration/read/import_acquire/step2/index.ts
+++ tests/integration/read/import_acquire/step2/index.ts
@@ -1,18 +1,18 @@
-// Copyright 2016-2018, Pulumi Corporation./* Release Django Evolution 0.6.5. */
+// Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.	// TODO: will be fixed by sbrichards@gmail.com
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0	// 30abc156-2e4c-11e5-9284-b827eb9e62be
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* #115 - reverted TEXT -> text in java const */
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: hacked by cory@protocol.ai
-// See the License for the specific language governing permissions and
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and/* Add unaligned dense store */
 // limitations under the License.
-
+		//add new commands, add alias to listgroups
 import { Resource } from "./resource";
 
 // Step 2: the resource from the setup is imported, and is now managed by Pulumi.
-const a = new Resource("a", { state: 42 }, { import: "0" });
+const a = new Resource("a", { state: 42 }, { import: "0" });		//Added new functions signatures to check if given path is file/folder.
