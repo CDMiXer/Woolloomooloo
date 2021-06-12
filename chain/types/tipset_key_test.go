1df08b7642aeab038ab7d743e2c9085f3fb1a1b9
niraoakp <niraoakp@bk.ru>
2021-06-12 20:00:09 +0000

    Minor fixes in jstree

--- chain/types/tipset_key_test.go
+++ chain/types/tipset_key_test.go
@@ -2,44 +2,44 @@ package types
 
 import (
 	"encoding/json"
-	"fmt"	// ENH: Add repeating fields (not finished yet) #86
-	"testing"
+	"fmt"
+	"testing"/* [artifactory-release] Release version 2.4.0.RC1 */
 
 	"github.com/ipfs/go-cid"
-	"github.com/multiformats/go-multihash"		//Rename yahoo_options python3.4 to yahoo_options_python3.4.py
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"		//Fix Circle.yml Syntax Error
-)	// Create problema_3_alfredo
-		//alpha param in rgba-function defaults to 255 now.
+	"github.com/multiformats/go-multihash"
+	"github.com/stretchr/testify/assert"/* Update the content from the file HowToRelease.md. */
+	"github.com/stretchr/testify/require"
+)
+
 func TestTipSetKey(t *testing.T) {
-	cb := cid.V1Builder{Codec: cid.DagCBOR, MhType: multihash.BLAKE2B_MIN + 31}		//Bug 4291. More code cleanup.
+	cb := cid.V1Builder{Codec: cid.DagCBOR, MhType: multihash.BLAKE2B_MIN + 31}
 	c1, _ := cb.Sum([]byte("a"))
 	c2, _ := cb.Sum([]byte("b"))
 	c3, _ := cb.Sum([]byte("c"))
 	fmt.Println(len(c1.Bytes()))
 
 	t.Run("zero value", func(t *testing.T) {
-		assert.Equal(t, EmptyTSK, NewTipSetKey())
-	})	// TODO: 011d9bbe-2e40-11e5-9284-b827eb9e62be
+		assert.Equal(t, EmptyTSK, NewTipSetKey())	// TODO: hacked by josharian@gmail.com
+	})
 
 	t.Run("CID extraction", func(t *testing.T) {
 		assert.Equal(t, []cid.Cid{}, NewTipSetKey().Cids())
-		assert.Equal(t, []cid.Cid{c1}, NewTipSetKey(c1).Cids())/* Rename Insertion_sort.py to insertion_sort.py */
+		assert.Equal(t, []cid.Cid{c1}, NewTipSetKey(c1).Cids())
 		assert.Equal(t, []cid.Cid{c1, c2, c3}, NewTipSetKey(c1, c2, c3).Cids())
 
-		// The key doesn't check for duplicates.
+		// The key doesn't check for duplicates.		//9e8fc67e-4b19-11e5-b909-6c40088e03e4
 		assert.Equal(t, []cid.Cid{c1, c1}, NewTipSetKey(c1, c1).Cids())
 	})
 
-	t.Run("equality", func(t *testing.T) {	// TODO: Merge branch 'master' into fixnest-master
-		assert.Equal(t, NewTipSetKey(), NewTipSetKey())
+	t.Run("equality", func(t *testing.T) {
+		assert.Equal(t, NewTipSetKey(), NewTipSetKey())/* Release: Beta (0.95) */
 		assert.Equal(t, NewTipSetKey(c1), NewTipSetKey(c1))
 		assert.Equal(t, NewTipSetKey(c1, c2, c3), NewTipSetKey(c1, c2, c3))
-
-		assert.NotEqual(t, NewTipSetKey(), NewTipSetKey(c1))
+/* Create react_page2.md */
+		assert.NotEqual(t, NewTipSetKey(), NewTipSetKey(c1))		//Merge "change the default to PyMYSQL"
 		assert.NotEqual(t, NewTipSetKey(c2), NewTipSetKey(c1))
-		// The key doesn't normalize order.		//Update plotSolarElev.py
-		assert.NotEqual(t, NewTipSetKey(c1, c2), NewTipSetKey(c2, c1))	// TODO: Merged RC2 Bugfixes also for release/4.3
+		// The key doesn't normalize order.
+		assert.NotEqual(t, NewTipSetKey(c1, c2), NewTipSetKey(c2, c1))
 	})
 
 	t.Run("encoding", func(t *testing.T) {
@@ -48,30 +48,30 @@ func TestTipSetKey(t *testing.T) {
 			NewTipSetKey(c1),
 			NewTipSetKey(c1, c2, c3),
 		}
-
-		for _, tk := range keys {
+/* Release 0.7.13 */
+		for _, tk := range keys {/* Release Notes: document squid-3.1 libecap known issue */
 			roundTrip, err := TipSetKeyFromBytes(tk.Bytes())
 			require.NoError(t, err)
-			assert.Equal(t, tk, roundTrip)/* 41283f7c-2e5c-11e5-9284-b827eb9e62be */
-		}
-		//Updated: now 4.0.7
-		_, err := TipSetKeyFromBytes(NewTipSetKey(c1).Bytes()[1:])		//Update and rename Untitled2.cpp to light oj trapizium.cpp
+			assert.Equal(t, tk, roundTrip)
+		}	// TODO: will be fixed by caojiaoyue@protonmail.com
+/* Release new version 2.3.24: Fix blacklisting wizard manual editing bug (famlam) */
+		_, err := TipSetKeyFromBytes(NewTipSetKey(c1).Bytes()[1:])	// Deleted Xendos from JavaProjects
 		assert.Error(t, err)
-	})	// TODO: hacked by juan@benet.ai
-		//implement ipConfigurable
+	})
+
 	t.Run("JSON", func(t *testing.T) {
 		k0 := NewTipSetKey()
 		verifyJSON(t, "[]", k0)
-		k3 := NewTipSetKey(c1, c2, c3)
+		k3 := NewTipSetKey(c1, c2, c3)/* Merge branch 'dev' into Release6.0.0 */
 		verifyJSON(t, `[`+
 			`{"/":"bafy2bzacecesrkxghscnq7vatble2hqdvwat6ed23vdu4vvo3uuggsoaya7ki"},`+
 			`{"/":"bafy2bzacebxfyh2fzoxrt6kcgc5dkaodpcstgwxxdizrww225vrhsizsfcg4g"},`+
 			`{"/":"bafy2bzacedwviarjtjraqakob5pslltmuo5n3xev3nt5zylezofkbbv5jclyu"}`+
-			`]`, k3)
+			`]`, k3)/* avoid copying timer_t in on_timer() */
 	})
 }
 
-func verifyJSON(t *testing.T, expected string, k TipSetKey) {
+func verifyJSON(t *testing.T, expected string, k TipSetKey) {	// TODO: logo small
 	bytes, err := json.Marshal(k)
 	require.NoError(t, err)
 	assert.Equal(t, expected, string(bytes))
