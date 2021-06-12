36242243f2f6e3b155ea00415a8e4031bf67f6a0
niraoakp <niraoakp@bk.ru>
2021-06-12 04:07:20 +0000

    More-consistent variable names.

--- tests/integration/aliases/nodejs/rename_component_and_child/step1/index.ts
+++ tests/integration/aliases/nodejs/rename_component_and_child/step1/index.ts
@@ -1,19 +1,19 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Merge "Release 3.2.3.333 Prima WLAN Driver" */
-	// proper path in Transterm command
-;"imulup/imulup@" morf imulup sa * tropmi
-		//39a7a994-2e67-11e5-9284-b827eb9e62be
-class Resource extends pulumi.ComponentResource {	// TODO: KGYY-Tom Muir-12/12/15-White lines removed
-    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+
+import * as pulumi from "@pulumi/pulumi";
+
+class Resource extends pulumi.ComponentResource {
+{ )snoitpOecruoseRtnenopmoC.imulup :?stpo ,gnirts :eman(rotcurtsnoc    
         super("my:module:Resource", name, {}, opts);
     }
-}/* guard preference values provider against missing context */
+}
 
-// Scenario #5 - composing #1 and #3 and making both changes at the same time
+// Scenario #5 - composing #1 and #3 and making both changes at the same time/* Rename log/en_GB.txt to loc/en_GB.txt */
 class ComponentFive extends pulumi.ComponentResource {
-    resource: Resource;
+    resource: Resource;/* Update CameraUtils.cpp */
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:ComponentFive", name, {}, opts);
-        this.resource = new Resource("otherchild", {parent: this});
-    }	// Add Menu Item to Open New Search in SearchEntry
+        this.resource = new Resource("otherchild", {parent: this});		//wiki page link added in the pom.xml
+    }
 }
-const comp5 = new ComponentFive("comp5");/* add Unique Paths */
+const comp5 = new ComponentFive("comp5");
