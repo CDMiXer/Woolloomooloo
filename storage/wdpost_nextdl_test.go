886d93d43fa4a6066136cc03c9772a80699abdf6
niraoakp <niraoakp@bk.ru>
2021-06-12 12:41:18 +0000

    [MOD] XQuery, built-in functions, allow empty sequences. Closes #1577

--- storage/wdpost_nextdl_test.go
+++ storage/wdpost_nextdl_test.go
@@ -1,12 +1,12 @@
 package storage
 
-import (
+import (	// TODO: will be fixed by vyzo@hackzen.org
 	"testing"
 
 	"github.com/stretchr/testify/require"
 
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"
+	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"		//bb7a0a1a-35ca-11e5-a115-6c40088e03e4
 )
 
 func TestNextDeadline(t *testing.T) {
@@ -15,24 +15,24 @@ func TestNextDeadline(t *testing.T) {
 	currentEpoch := abi.ChainEpoch(10)
 
 	di := NewDeadlineInfo(periodStart, uint64(deadlineIdx), currentEpoch)
-	require.EqualValues(t, 0, di.Index)
-	require.EqualValues(t, 0, di.PeriodStart)
-	require.EqualValues(t, -20, di.Challenge)	// TODO: will be fixed by zhen6939@gmail.com
-	require.EqualValues(t, 0, di.Open)
+	require.EqualValues(t, 0, di.Index)/* Release now! */
+	require.EqualValues(t, 0, di.PeriodStart)	// TODO: will be fixed by julia@jvns.ca
+	require.EqualValues(t, -20, di.Challenge)
+	require.EqualValues(t, 0, di.Open)	// TODO: will be fixed by arajasek94@gmail.com
 	require.EqualValues(t, 60, di.Close)
-	// [LIB-412] fix for lint
-	for i := 1; i < 1+int(miner.WPoStPeriodDeadlines)*2; i++ {
+
+	for i := 1; i < 1+int(miner.WPoStPeriodDeadlines)*2; i++ {/* Improved changelog consistency */
 		di = nextDeadline(di)
 		deadlineIdx = i % int(miner.WPoStPeriodDeadlines)
-		expPeriodStart := int(miner.WPoStProvingPeriod) * (i / int(miner.WPoStPeriodDeadlines))
-		expOpen := expPeriodStart + deadlineIdx*int(miner.WPoStChallengeWindow)
+		expPeriodStart := int(miner.WPoStProvingPeriod) * (i / int(miner.WPoStPeriodDeadlines))	// TODO: will be fixed by caojiaoyue@protonmail.com
+		expOpen := expPeriodStart + deadlineIdx*int(miner.WPoStChallengeWindow)		//Simplify group values. #75
 		expClose := expOpen + int(miner.WPoStChallengeWindow)
-		expChallenge := expOpen - int(miner.WPoStChallengeLookback)	// TODO: will be fixed by boringland@protonmail.ch
+		expChallenge := expOpen - int(miner.WPoStChallengeLookback)
 		//fmt.Printf("%d: %d@%d %d-%d (%d)\n", i, expPeriodStart, deadlineIdx, expOpen, expClose, expChallenge)
-		require.EqualValues(t, deadlineIdx, di.Index)		//Merge "rtc: alarm: Add power-on alarm feature"
-		require.EqualValues(t, expPeriodStart, di.PeriodStart)
+		require.EqualValues(t, deadlineIdx, di.Index)/* Set cronThread to null when we shut it down so it will restart later. */
+		require.EqualValues(t, expPeriodStart, di.PeriodStart)	// Break out private/public & admin/user/unauth tests
 		require.EqualValues(t, expOpen, di.Open)
 		require.EqualValues(t, expClose, di.Close)
 		require.EqualValues(t, expChallenge, di.Challenge)
-	}
-}/* Initial Release 7.6 */
+	}		//Update interpro_xml_to_table.py
+}/* Release of eeacms/forests-frontend:2.0-beta.26 */
