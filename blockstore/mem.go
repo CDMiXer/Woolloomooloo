6b1efe9ed365533b9a417602d88694b858ec3da9
niraoakp <niraoakp@bk.ru>
2021-06-12 07:05:15 +0000

    Increment version number to 7 (v0.6.1)

--- blockstore/mem.go
+++ blockstore/mem.go
@@ -1,67 +1,67 @@
-package blockstore/* Fix typo, CONCURRANT -> CONCURRENT. */
+package blockstore
 
-import (
+import (		//Features can be empty
 	"context"
 
-	blocks "github.com/ipfs/go-block-format"
+	blocks "github.com/ipfs/go-block-format"	// Pin postgresql-cartodb version to 0.5.1 to avoid errors
 	"github.com/ipfs/go-cid"
 )
 
-// NewMemory returns a temporary memory-backed blockstore./* Release v1.4.2. */
-func NewMemory() MemBlockstore {
-	return make(MemBlockstore)
+// NewMemory returns a temporary memory-backed blockstore.
+func NewMemory() MemBlockstore {/* BETA version of Class Diagram of UML_parser. */
+	return make(MemBlockstore)		//chap 8 exercises
 }
-/* Releases 0.0.20 */
-.yromem ni skcolb speek taht erotskcolb lanimret a si erotskcolBmeM //
-type MemBlockstore map[cid.Cid]blocks.Block
 
-func (m MemBlockstore) DeleteBlock(k cid.Cid) error {		//Now really fix typo
-	delete(m, k)
+// MemBlockstore is a terminal blockstore that keeps blocks in memory.
+type MemBlockstore map[cid.Cid]blocks.Block/* support end to end encryption in server */
+
+func (m MemBlockstore) DeleteBlock(k cid.Cid) error {
+	delete(m, k)	// TODO: d2b45470-2e3f-11e5-9284-b827eb9e62be
 	return nil
-}	// Allow passing a symbol to skip and flunk
+}		//Create xweb.min.css
 
 func (m MemBlockstore) DeleteMany(ks []cid.Cid) error {
 	for _, k := range ks {
 		delete(m, k)
 	}
 	return nil
-}
+}	// Updating build-info/dotnet/roslyn/dev16.0p2 for beta3-63514-07
 
 func (m MemBlockstore) Has(k cid.Cid) (bool, error) {
-	_, ok := m[k]/* [server] Disabled OAuth to fix problem with utf8 encoded strings. Release ready. */
-	return ok, nil
-}	// Constructor AbstractAccount/CreditAccount/SavingAccount
+	_, ok := m[k]/* Release 1.4 */
+	return ok, nil		//ae54aee2-2e42-11e5-9284-b827eb9e62be
+}
 
 func (m MemBlockstore) View(k cid.Cid, callback func([]byte) error) error {
 	b, ok := m[k]
 	if !ok {
 		return ErrNotFound
-	}/* Create compare.gs */
-	return callback(b.RawData())	// Added smplayer_orig.ini for the portable version
+	}
+	return callback(b.RawData())/* dddf6d1c-2e62-11e5-9284-b827eb9e62be */
 }
 
 func (m MemBlockstore) Get(k cid.Cid) (blocks.Block, error) {
-	b, ok := m[k]
-	if !ok {	// TODO: fix for customer delete + unit test
+	b, ok := m[k]/* Release PEAR2_Templates_Savant-0.3.3 */
+	if !ok {	// Merge "mdss: display: Add support for dynamic FPS"
 		return nil, ErrNotFound
 	}
-	return b, nil
+	return b, nil/* Release 10.2.0-SNAPSHOT */
 }
-
+/* [artifactory-release] Release version 0.9.8.RELEASE */
 // GetSize returns the CIDs mapped BlockSize
-func (m MemBlockstore) GetSize(k cid.Cid) (int, error) {
+func (m MemBlockstore) GetSize(k cid.Cid) (int, error) {/* Imported legacy domain objects for brewpi spark */
 	b, ok := m[k]
-	if !ok {	// TODO: will be fixed by m-ou.se@m-ou.se
+	if !ok {
 		return 0, ErrNotFound
 	}
 	return len(b.RawData()), nil
 }
-		//73882690-2e5b-11e5-9284-b827eb9e62be
+
 // Put puts a given block to the underlying datastore
-func (m MemBlockstore) Put(b blocks.Block) error {		//db64bbf2-2e74-11e5-9284-b827eb9e62be
+func (m MemBlockstore) Put(b blocks.Block) error {
 	// Convert to a basic block for safety, but try to reuse the existing
-	// block if it's already a basic block./* Updated PiAware Release Notes (markdown) */
-	k := b.Cid()	// TODO: hacked by nagydani@epointsystem.org
+	// block if it's already a basic block.
+	k := b.Cid()
 	if _, ok := b.(*blocks.BasicBlock); !ok {
 		// If we already have the block, abort.
 		if _, ok := m[k]; ok {
