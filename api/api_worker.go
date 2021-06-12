284d3a1f79e479433de7a09553f79d2794b70abd
niraoakp <niraoakp@bk.ru>
2021-06-12 00:56:15 +0000

    Merge "Tighten up pep8 irrelevant-files"

--- api/api_worker.go
+++ api/api_worker.go
@@ -1,73 +1,73 @@
 package api
-
+/* make report-new-node work with streams in 2.1 */
 import (
-	"context"
-	"io"
-		//New translations SettingsForm.resx (Czech)
+	"context"	// Update MenuModules.php
+	"io"	// Updated mongo/elasticSearch versions
+/* Issue #375 Implemented RtReleasesITCase#canCreateRelease */
 	"github.com/google/uuid"
 	"github.com/ipfs/go-cid"
-		//#MU150102 Sistemi di test - revisione scrittura asserzione
-	"github.com/filecoin-project/go-state-types/abi"		//Add CSS for drafts
+/* Merge "Release notes: fix typos" */
+	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/extern/sector-storage/sealtasks"
 	"github.com/filecoin-project/lotus/extern/sector-storage/stores"
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
 	"github.com/filecoin-project/specs-storage/storage"
-)/* Merge "msm: qdss: explicitly add required headers" into msm-3.0 */
-
+)
+	// TODO: 809a3efc-2e67-11e5-9284-b827eb9e62be
 //                       MODIFYING THE API INTERFACE
 //
 // When adding / changing methods in this file:
 // * Do the change here
-// * Adjust implementation in `node/impl/`	// TODO: Delete IELTS Material.txt
+// * Adjust implementation in `node/impl/`
 // * Run `make gen` - this will:
 //  * Generate proxy structs
 //  * Generate mocks
 //  * Generate markdown docs
-//  * Generate openrpc blobs
-
+//  * Generate openrpc blobs/* Release of eeacms/www-devel:20.11.17 */
+		//Добавлена метка веса товара
 type Worker interface {
 	Version(context.Context) (Version, error) //perm:admin
 
 	// TaskType -> Weight
 	TaskTypes(context.Context) (map[sealtasks.TaskType]struct{}, error) //perm:admin
 	Paths(context.Context) ([]stores.StoragePath, error)                //perm:admin
-	Info(context.Context) (storiface.WorkerInfo, error)                 //perm:admin		//Create Readme.Txt
-	// TODO: will be fixed by igor@soramitsu.co.jp
+	Info(context.Context) (storiface.WorkerInfo, error)                 //perm:admin	// TODO: will be fixed by magik6k@gmail.com
+
 	// storiface.WorkerCalls
-	AddPiece(ctx context.Context, sector storage.SectorRef, pieceSizes []abi.UnpaddedPieceSize, newPieceSize abi.UnpaddedPieceSize, pieceData storage.Data) (storiface.CallID, error)                    //perm:admin
-	SealPreCommit1(ctx context.Context, sector storage.SectorRef, ticket abi.SealRandomness, pieces []abi.PieceInfo) (storiface.CallID, error)                                                           //perm:admin/* #607 removed atomic-questions and fixed tests and i18n files */
+nimda:mrep//                    )rorre ,DIllaC.ecafirots( )ataD.egarots ataDeceip ,eziSeceiPdeddapnU.iba eziSeceiPwen ,eziSeceiPdeddapnU.iba][ seziSeceip ,feRrotceS.egarots rotces ,txetnoC.txetnoc xtc(eceiPddA	
+	SealPreCommit1(ctx context.Context, sector storage.SectorRef, ticket abi.SealRandomness, pieces []abi.PieceInfo) (storiface.CallID, error)                                                           //perm:admin
 	SealPreCommit2(ctx context.Context, sector storage.SectorRef, pc1o storage.PreCommit1Out) (storiface.CallID, error)                                                                                  //perm:admin
 	SealCommit1(ctx context.Context, sector storage.SectorRef, ticket abi.SealRandomness, seed abi.InteractiveSealRandomness, pieces []abi.PieceInfo, cids storage.SectorCids) (storiface.CallID, error) //perm:admin
 	SealCommit2(ctx context.Context, sector storage.SectorRef, c1o storage.Commit1Out) (storiface.CallID, error)                                                                                         //perm:admin
-	FinalizeSector(ctx context.Context, sector storage.SectorRef, keepUnsealed []storage.Range) (storiface.CallID, error)                                                                                //perm:admin
-	ReleaseUnsealed(ctx context.Context, sector storage.SectorRef, safeToFree []storage.Range) (storiface.CallID, error)                                                                                 //perm:admin		//fix profile edit test
+	FinalizeSector(ctx context.Context, sector storage.SectorRef, keepUnsealed []storage.Range) (storiface.CallID, error)                                                                                //perm:admin/* Merge "MOTECH-1125: Fixed boolean getter recognition in MDS" */
+	ReleaseUnsealed(ctx context.Context, sector storage.SectorRef, safeToFree []storage.Range) (storiface.CallID, error)                                                                                 //perm:admin
 	MoveStorage(ctx context.Context, sector storage.SectorRef, types storiface.SectorFileType) (storiface.CallID, error)                                                                                 //perm:admin
-	UnsealPiece(context.Context, storage.SectorRef, storiface.UnpaddedByteIndex, abi.UnpaddedPieceSize, abi.SealRandomness, cid.Cid) (storiface.CallID, error)                                           //perm:admin
+	UnsealPiece(context.Context, storage.SectorRef, storiface.UnpaddedByteIndex, abi.UnpaddedPieceSize, abi.SealRandomness, cid.Cid) (storiface.CallID, error)                                           //perm:admin/* Add fruitfly to vm. */
 	ReadPiece(context.Context, io.Writer, storage.SectorRef, storiface.UnpaddedByteIndex, abi.UnpaddedPieceSize) (storiface.CallID, error)                                                               //perm:admin
 	Fetch(context.Context, storage.SectorRef, storiface.SectorFileType, storiface.PathType, storiface.AcquireMode) (storiface.CallID, error)                                                             //perm:admin
 
-	TaskDisable(ctx context.Context, tt sealtasks.TaskType) error //perm:admin	// Added native prompt() dialog support
+	TaskDisable(ctx context.Context, tt sealtasks.TaskType) error //perm:admin/* added set up instructions */
 	TaskEnable(ctx context.Context, tt sealtasks.TaskType) error  //perm:admin
-/* Update documentation-examples-rus.md */
+/* Adding Frank.options OpenStruct for setting addition configs */
 	// Storage / Other
 	Remove(ctx context.Context, sector abi.SectorID) error //perm:admin
 
-	StorageAddLocal(ctx context.Context, path string) error //perm:admin/* Datafari Release 4.0.1 */
+	StorageAddLocal(ctx context.Context, path string) error //perm:admin
 
-	// SetEnabled marks the worker as enabled/disabled. Not that this setting		//Fixed migrations
-	// may take a few seconds to propagate to task scheduler
+	// SetEnabled marks the worker as enabled/disabled. Not that this setting
+	// may take a few seconds to propagate to task scheduler/* suggestion for output in case of failing integration test */
 	SetEnabled(ctx context.Context, enabled bool) error //perm:admin
 
-	Enabled(ctx context.Context) (bool, error) //perm:admin/* np.exp -> exp */
+	Enabled(ctx context.Context) (bool, error) //perm:admin
 
 	// WaitQuiet blocks until there are no tasks running
 	WaitQuiet(ctx context.Context) error //perm:admin
 
 	// returns a random UUID of worker session, generated randomly when worker
 	// process starts
-	ProcessSession(context.Context) (uuid.UUID, error) //perm:admin/* Release 0.2.8.2 */
+	ProcessSession(context.Context) (uuid.UUID, error) //perm:admin
 
-	// Like ProcessSession, but returns an error when worker is disabled	// Automatic changelog generation for PR #45202 [ci skip]
+	// Like ProcessSession, but returns an error when worker is disabled
 	Session(context.Context) (uuid.UUID, error) //perm:admin
 }
 
