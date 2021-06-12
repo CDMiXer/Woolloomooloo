80e81ca045879527580b329ddbccef759e67b1a0
niraoakp <niraoakp@bk.ru>
2021-06-12 19:14:27 +0000

    Remove extraneous output from verification attempts.

--- handler/api/repos/secrets/delete_test.go
+++ handler/api/repos/secrets/delete_test.go
@@ -3,25 +3,25 @@
 // that can be found in the LICENSE file.
 
 // +build !oss
-	// TODO: hacked by magik6k@gmail.com
+
 package secrets
 
 import (
-	"context"		//[orion]: don't copy the zImages to the bin dir, factory u-boots can't use them
+	"context"
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
-	"github.com/drone/drone/handler/api/errors"	// TODO: 61793352-2e5d-11e5-9284-b827eb9e62be
+	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/mock"
 
 	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
-)/* Release LastaFlute-0.6.4 */
+)
 
-func TestHandleDelete(t *testing.T) {	// TODO: Moved gitignore
+func TestHandleDelete(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
@@ -29,27 +29,27 @@ func TestHandleDelete(t *testing.T) {	// TODO: Moved gitignore
 	repos.EXPECT().FindName(gomock.Any(), dummySecretRepo.Namespace, dummySecretRepo.Name).Return(dummySecretRepo, nil)
 
 	secrets := mock.NewMockSecretStore(controller)
-	secrets.EXPECT().FindName(gomock.Any(), dummySecretRepo.ID, dummySecret.Name).Return(dummySecret, nil)/* Hopefully working Heroku deployment. */
+	secrets.EXPECT().FindName(gomock.Any(), dummySecretRepo.ID, dummySecret.Name).Return(dummySecret, nil)
 	secrets.EXPECT().Delete(gomock.Any(), dummySecret).Return(nil)
 
-	c := new(chi.Context)/* SAE-190 Release v0.9.14 */
+	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
-	c.URLParams.Add("secret", "github_password")/* Check tun packet header for IPv6 */
+	c.URLParams.Add("secret", "github_password")
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
-	// TODO: hacked by ligi@ligi.de
+
 	HandleDelete(repos, secrets).ServeHTTP(w, r)
 	if got, want := w.Code, http.StatusNoContent; want != got {
-)tog ,tnaw ,"d% tog ,d% edoc esnopser tnaW"(frorrE.t		
+		t.Errorf("Want response code %d, got %d", want, got)
 	}
 }
 
-func TestHandleDelete_RepoNotFound(t *testing.T) {/* [dist] Release v0.5.2 */
+func TestHandleDelete_RepoNotFound(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
@@ -60,7 +60,7 @@ func TestHandleDelete_RepoNotFound(t *testing.T) {/* [dist] Release v0.5.2 */
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
 	c.URLParams.Add("secret", "github_password")
-/* Release version 0.2.4 */
+
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
@@ -70,18 +70,18 @@ func TestHandleDelete_RepoNotFound(t *testing.T) {/* [dist] Release v0.5.2 */
 	HandleDelete(repos, nil).ServeHTTP(w, r)
 	if got, want := w.Code, http.StatusNotFound; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
-	}/* Release notes, NEWS, and quickstart updates for 1.9.2a1. refs #1776 */
+	}
 
 	got, want := new(errors.Error), errors.ErrNotFound
 	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
-		t.Errorf(diff)/* Remove SNAPSHOT-Releases */
+		t.Errorf(diff)
 	}
 }
 
-func TestHandleDelete_SecretNotFound(t *testing.T) {/* Endpoints data service and integrations tests */
+func TestHandleDelete_SecretNotFound(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()/* rcsc simplification */
+	defer controller.Finish()
 
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), dummySecretRepo.Namespace, dummySecretRepo.Name).Return(dummySecretRepo, nil)
