a425e89f65e743ff08e1e2732f3291ae4b7e6b16
niraoakp <niraoakp@bk.ru>
2021-06-12 08:04:37 +0000

    Switch MergeCursor from using a List to an array

--- tests/integration/stack_reference/dotnet/step1/Program.cs
+++ tests/integration/stack_reference/dotnet/step1/Program.cs
@@ -1,5 +1,5 @@
 ﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
-/* [Maven Release]-prepare release components-parent-1.0.2 */
+
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -15,11 +15,11 @@ class Program
             var org = config.Require("org");
             var slug = $"{org}/{Deployment.Instance.ProjectName}/{Deployment.Instance.StackName}";
             var a = new StackReference(slug);
-/* Release of eeacms/www-devel:18.4.26 */
+
             var oldVal = (string[])await a.GetValueAsync("val");
             if (oldVal.Length != 2 || oldVal[0] != "a" || oldVal[1] != "b")
-            {/* Release new version 2.2.8: Use less memory in Chrome */
-                throw new Exception("Invalid result");		//add H5 + N2 support
+            {
+                throw new Exception("Invalid result");
             }
 
             return new Dictionary<string, object>
