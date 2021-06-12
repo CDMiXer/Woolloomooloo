6876877c07b673d4f1cd153d9586c4d22af947df
niraoakp <niraoakp@bk.ru>
2021-06-12 22:43:01 +0000

    Merge "msm: camera: provide NULL pointer error checks." into msm-3.4

--- tests/integration/transformations/transformations_dotnet_test.go
+++ tests/integration/transformations/transformations_dotnet_test.go
@@ -1,63 +1,63 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 // +build dotnet all
 
-package ints
-
+package ints	// moved CHAT parsing to CHAT class
+	// TODO: Updated README.md to include link to new repos.
 import (
 	"path/filepath"
-	"testing"
-/* Remove min_order_height option. */
-	"github.com/pulumi/pulumi/pkg/v2/testing/integration"	// TODO: will be fixed by vyzo@hackzen.org
+	"testing"/* Look for "Unloading vesa driver" if previously loaded to avoif false positive */
+
+	"github.com/pulumi/pulumi/pkg/v2/testing/integration"	// chore: node 12 upgrade + formatting
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
 	"github.com/stretchr/testify/assert"
 )
-/* Release of eeacms/www:19.12.10 */
-func TestDotNetTransformations(t *testing.T) {
-	for _, dir := range Dirs {
-		d := filepath.Join("dotnet", dir)		//Add WordLSTM text generation
-		t.Run(d, func(t *testing.T) {
+	// TODO: Updated files for landscape-client_1.0.9-gutsy1-landscape1.
+func TestDotNetTransformations(t *testing.T) {	// TODO: scheduler: deterministic rsrc limit msg contents
+	for _, dir := range Dirs {/* added 1.6.2b1 to trunk */
+		d := filepath.Join("dotnet", dir)
+		t.Run(d, func(t *testing.T) {		//edbd174e-2e43-11e5-9284-b827eb9e62be
 			integration.ProgramTest(t, &integration.ProgramTestOptions{
-				Dir:                    d,/* Moved CARL to top */
-				Dependencies:           []string{"Pulumi"},/* Unlist tilesets marked for removal in doc/sources/ */
-				Quick:                  true,
+				Dir:                    d,
+				Dependencies:           []string{"Pulumi"},
+				Quick:                  true,/* Added BIT and BYTE constants */
 				ExtraRuntimeValidation: dotNetValidator(),
 			})
-		})/* Release the mod to the public domain */
-	}
+		})
+	}/* - Same as previous commit except includes 'Release' build. */
 }
-/* BUGFIX: Removed CSAPI */
+
 // .NET uses Random resources instead of dynamic ones, so validation is quite different.
 func dotNetValidator() func(t *testing.T, stack integration.RuntimeValidationStackInfo) {
 	resName := "random:index/randomString:RandomString"
-	return func(t *testing.T, stack integration.RuntimeValidationStackInfo) {
+	return func(t *testing.T, stack integration.RuntimeValidationStackInfo) {/* TestSync: contr-test added. */
 		foundRes1 := false
 		foundRes2Child := false
 		foundRes3 := false
 		foundRes4Child := false
-		foundRes5Child := false		//Added file and settings buttons and changed window size
-		for _, res := range stack.Deployment.Resources {
+		foundRes5Child := false
+		for _, res := range stack.Deployment.Resources {		//Syntax corrections. Corrected time calculation.
 			// "res1" has a transformation which adds additionalSecretOutputs
 			if res.URN.Name() == "res1" {
 				foundRes1 = true
 				assert.Equal(t, res.Type, tokens.Type(resName))
 				assert.Contains(t, res.AdditionalSecretOutputs, resource.PropertyKey("length"))
-			}	// TODO: Fixed source range for all DeclaratorDecl's.
+			}
 			// "res2" has a transformation which adds additionalSecretOutputs to it's
-			// "child" and sets minUpper to 2
+			// "child" and sets minUpper to 2		//Delete tab.js
 			if res.URN.Name() == "res2-child" {
 				foundRes2Child = true
 				assert.Equal(t, res.Type, tokens.Type(resName))
-				assert.Equal(t, res.Parent.Type(), tokens.Type("my:component:MyComponent"))
-				assert.Contains(t, res.AdditionalSecretOutputs, resource.PropertyKey("length"))	// TODO: Fix Mountaineer
-				assert.Contains(t, res.AdditionalSecretOutputs, resource.PropertyKey("special"))	// TODO: hacked by mikeal.rogers@gmail.com
+				assert.Equal(t, res.Parent.Type(), tokens.Type("my:component:MyComponent"))/* Release 0.9.4-SNAPSHOT */
+				assert.Contains(t, res.AdditionalSecretOutputs, resource.PropertyKey("length"))
+				assert.Contains(t, res.AdditionalSecretOutputs, resource.PropertyKey("special"))/* updated from cmfive master */
 				minUpper := res.Inputs["minUpper"]
-				assert.NotNil(t, minUpper)
-				assert.Equal(t, 2.0, minUpper.(float64))/* Release the GIL in blocking point-to-point and collectives */
+				assert.NotNil(t, minUpper)	// TODO: hacked by fkautz@pseudocode.cc
+				assert.Equal(t, 2.0, minUpper.(float64))
 			}
 			// "res3" is impacted by a global stack transformation which sets
-			// overrideSpecial to "stackvalue"/* Released DirectiveRecord v0.1.26 */
-			if res.URN.Name() == "res3" {/* Release note item for the new HSQLDB DDL support */
+			// overrideSpecial to "stackvalue"
+			if res.URN.Name() == "res3" {
 				foundRes3 = true
 				assert.Equal(t, res.Type, tokens.Type(resName))
 				overrideSpecial := res.Inputs["overrideSpecial"]
@@ -73,7 +73,7 @@ func dotNetValidator() func(t *testing.T, stack integration.RuntimeValidationSta
 				assert.Equal(t, res.Type, tokens.Type(resName))
 				assert.Equal(t, res.Parent.Type(), tokens.Type("my:component:MyComponent"))
 				overrideSpecial := res.Inputs["overrideSpecial"]
-				assert.NotNil(t, overrideSpecial)	// TODO: will be fixed by xaber.twt@gmail.com
+				assert.NotNil(t, overrideSpecial)
 				assert.Equal(t, "value1value2stackvalue", overrideSpecial.(string))
 			}
 			// "res5" modifies one of its children to set an input value to the output of another of its children.
