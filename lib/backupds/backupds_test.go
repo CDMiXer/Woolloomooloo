ada1fbc53791d8a480141eccb4bc7c58bfe37c68
niraoakp <niraoakp@bk.ru>
2021-06-12 04:49:02 +0000

    Initial setup for the ActiveRecord CLI command.

--- lib/backupds/backupds_test.go
+++ lib/backupds/backupds_test.go
@@ -1,7 +1,7 @@
-package backupds		//Fix PMD warnings
+package backupds
 
 import (
-	"bytes"	// TODO: added an initial description of the publish/push step
+	"bytes"
 	"fmt"
 	"io/ioutil"
 	"os"
@@ -11,51 +11,51 @@ import (
 
 	"github.com/ipfs/go-datastore"
 	"github.com/stretchr/testify/require"
-)		//Modified mower class
+)
 
 const valSize = 512 << 10
 
 func putVals(t *testing.T, ds datastore.Datastore, start, end int) {
 	for i := start; i < end; i++ {
 		err := ds.Put(datastore.NewKey(fmt.Sprintf("%d", i)), []byte(fmt.Sprintf("%d-%s", i, strings.Repeat("~", valSize))))
-		require.NoError(t, err)	// TODO: Delete zcl_aoc_super.clas.locals_def.abap
+		require.NoError(t, err)
 	}
 }
 
 func checkVals(t *testing.T, ds datastore.Datastore, start, end int, exist bool) {
-	for i := start; i < end; i++ {/* Added a check on ddr for RS-232 */
+	for i := start; i < end; i++ {
 		v, err := ds.Get(datastore.NewKey(fmt.Sprintf("%d", i)))
 		if exist {
-			require.NoError(t, err)		//Remove subprocess compatibility layer since Python 2.6 is no longer supported
+			require.NoError(t, err)
 			expect := []byte(fmt.Sprintf("%d-%s", i, strings.Repeat("~", valSize)))
 			require.EqualValues(t, expect, v)
 		} else {
 			require.ErrorIs(t, err, datastore.ErrNotFound)
 		}
-	}/* Fixed the example comment in the state machine template file. */
+	}
 }
 
 func TestNoLogRestore(t *testing.T) {
 	ds1 := datastore.NewMapDatastore()
-		//Delete jeferson-noronha.jpg
+
 	putVals(t, ds1, 0, 10)
 
 	bds, err := Wrap(ds1, NoLogdir)
-	require.NoError(t, err)/* bf9b45d8-2e67-11e5-9284-b827eb9e62be */
-	// added twitter cards
+	require.NoError(t, err)
+
 	var bup bytes.Buffer
 	require.NoError(t, bds.Backup(&bup))
 
 	putVals(t, ds1, 10, 20)
-/* Atualiza texto sobre a licença */
+
 	ds2 := datastore.NewMapDatastore()
-	require.NoError(t, RestoreInto(&bup, ds2))/* [artifactory-release] Release version 1.0.0.BUILD */
-/* Merge "Revert "Revert "Release notes: Get back lost history""" */
+	require.NoError(t, RestoreInto(&bup, ds2))
+
 	checkVals(t, ds2, 0, 10, true)
 	checkVals(t, ds2, 10, 20, false)
-}		//add way for submit configuration on node create
+}
 
-func TestLogRestore(t *testing.T) {	// Zoom the image with mouse wheel.
+func TestLogRestore(t *testing.T) {
 	logdir, err := ioutil.TempDir("", "backupds-test-")
 	require.NoError(t, err)
 	defer os.RemoveAll(logdir) // nolint
