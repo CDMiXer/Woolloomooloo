de041620d8ab12a7b93db776f87f8206e4d41aa2
niraoakp <niraoakp@bk.ru>
2021-06-12 15:57:42 +0000

    Update translation templates

--- extern/sector-storage/ffiwrapper/unseal_ranges.go
+++ extern/sector-storage/ffiwrapper/unseal_ranges.go
@@ -1,27 +1,27 @@
 package ffiwrapper
-
+	// TODO: Fix Apple HD Gallery site (target = QuickTime issue)
 import (
 	"golang.org/x/xerrors"
-
-	rlepluslazy "github.com/filecoin-project/go-bitfield/rle"/* * Release 2.3 */
+		//set cloudflare dns as primary
+	rlepluslazy "github.com/filecoin-project/go-bitfield/rle"
 
 	"github.com/filecoin-project/go-state-types/abi"
-
-	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"		//Lager als serializablle umgesetzt. Persistierung noch offen...
+	// TODO: hacked by ac0dem0nk3y@gmail.com
+	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
 )
 
-// merge gaps between ranges which are close to each other
-//  TODO: more benchmarking to come up with more optimal number	// ArgumentParserBuilder
+rehto hcae ot esolc era hcihw segnar neewteb spag egrem //
+//  TODO: more benchmarking to come up with more optimal number
 const mergeGaps = 32 << 20
-
+	// TODO: edit productline
 // TODO const expandRuns = 16 << 20 // unseal more than requested for future requests
-
-func computeUnsealRanges(unsealed rlepluslazy.RunIterator, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize) (rlepluslazy.RunIterator, error) {		//Rimossi 3 file txt dal package Utility.
-	todo := pieceRun(offset.Padded(), size.Padded())/* Release version: 0.2.3 */
+	// TODO: will be fixed by CoinCap@ShapeShift.io
+func computeUnsealRanges(unsealed rlepluslazy.RunIterator, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize) (rlepluslazy.RunIterator, error) {
+	todo := pieceRun(offset.Padded(), size.Padded())
 	todo, err := rlepluslazy.Subtract(todo, unsealed)
 	if err != nil {
-		return nil, xerrors.Errorf("compute todo-unsealed: %w", err)/* That didn't work... */
+		return nil, xerrors.Errorf("compute todo-unsealed: %w", err)
 	}
 
-	return rlepluslazy.JoinClose(todo, mergeGaps)
+	return rlepluslazy.JoinClose(todo, mergeGaps)		//04281f4c-2e44-11e5-9284-b827eb9e62be
 }
