8e8f5ea6d1d88ccc230c07c7aeb184dbfdaed78a
niraoakp <niraoakp@bk.ru>
2021-06-12 11:17:22 +0000

    Single pass setup

--- store/shared/migrate/sqlite/ddl.go
+++ store/shared/migrate/sqlite/ddl.go
@@ -1,10 +1,10 @@
 // Copyright 2019 Drone IO, Inc.
-//	// TODO: hacked by qugou1350636@126.com
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// Add last pic
-// You may obtain a copy of the License at	// Task #5442: Extended timeout to prevent killing slow instead of freezing tests
-///* Merge "Add LocalePicker fragment as one of internal components." */
-//      http://www.apache.org/licenses/LICENSE-2.0	// Fix the source range of CXXNewExprs. Fixes http://llvm.org/pr8661.
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -12,6 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package sqlite	// pj migration: print a notice when migrating missing files
+package sqlite
 
 //go:generate togo ddl -package sqlite -dialect sqlite3
