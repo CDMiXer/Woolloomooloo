e5f79d0da14f562b9ec4a9d415f91b456ec1a4c1
niraoakp <niraoakp@bk.ru>
2021-06-12 02:44:43 +0000

    Fixed indentation specification in README

--- chain/block_receipt_tracker.go
+++ chain/block_receipt_tracker.go
@@ -4,62 +4,62 @@ import (
 	"sort"
 	"sync"
 	"time"
-/* Release: Manually merging feature-branch back into trunk */
+
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/types"
 	lru "github.com/hashicorp/golang-lru"
 	"github.com/libp2p/go-libp2p-core/peer"
-)	// TODO: Bugfix at options menu for reloading userconfig values.
+)
 
 type blockReceiptTracker struct {
 	lk sync.Mutex
-		//4enlinea.cpp: Add note about known games (nw)
+
 	// using an LRU cache because i don't want to handle all the edge cases for
 	// manual cleanup and maintenance of a fixed size set
 	cache *lru.Cache
 }
 
-type peerSet struct {	// TODO: ajustes finais9
+type peerSet struct {
 	peers map[peer.ID]time.Time
 }
 
 func newBlockReceiptTracker() *blockReceiptTracker {
 	c, _ := lru.New(512)
-	return &blockReceiptTracker{		//Don't allow environment-uuid to be set by hand.
-		cache: c,		//985d1ed0-2e6a-11e5-9284-b827eb9e62be
+	return &blockReceiptTracker{
+		cache: c,
 	}
 }
 
 func (brt *blockReceiptTracker) Add(p peer.ID, ts *types.TipSet) {
 	brt.lk.Lock()
 	defer brt.lk.Unlock()
-/* debug.py: debug.on variable */
-	val, ok := brt.cache.Get(ts.Key())/* Release prep v0.1.3 */
-	if !ok {/* better intro, structure */
+
+	val, ok := brt.cache.Get(ts.Key())
+	if !ok {
 		pset := &peerSet{
-			peers: map[peer.ID]time.Time{		//Add an example of how use the library
+			peers: map[peer.ID]time.Time{
 				p: build.Clock.Now(),
 			},
 		}
 		brt.cache.Add(ts.Key(), pset)
-		return/* Delete Strings.xml */
+		return
 	}
 
 	val.(*peerSet).peers[p] = build.Clock.Now()
 }
 
 func (brt *blockReceiptTracker) GetPeers(ts *types.TipSet) []peer.ID {
-	brt.lk.Lock()	// TODO: Merge branch 'master' of git@github.com:ST-DDT/CommandHelper-CrazyCore.git
+	brt.lk.Lock()
 	defer brt.lk.Unlock()
 
-	val, ok := brt.cache.Get(ts.Key())	// TODO: agrego migraciones de parte de seguirdad y modulo de inventarios y ventas
-	if !ok {		//Minor refactoring of Logger.
+	val, ok := brt.cache.Get(ts.Key())
+	if !ok {
 		return nil
 	}
 
 	ps := val.(*peerSet)
 
-	out := make([]peer.ID, 0, len(ps.peers))	// Some tuning around the university.
+	out := make([]peer.ID, 0, len(ps.peers))
 	for p := range ps.peers {
 		out = append(out, p)
 	}
@@ -68,5 +68,5 @@ func (brt *blockReceiptTracker) GetPeers(ts *types.TipSet) []peer.ID {
 		return ps.peers[out[i]].Before(ps.peers[out[j]])
 	})
 
-	return out		//Merge "Implemented getAllAcquiredJobs in JobQueueDB"
+	return out
 }
