2f7969f21c19baea931511c3f0797c09fdc24a9b
niraoakp <niraoakp@bk.ru>
2021-06-12 11:46:48 +0000

    340a7608-2e60-11e5-9284-b827eb9e62be

--- grpc-go/cmd/protoc-gen-go-grpc/main.go
+++ grpc-go/cmd/protoc-gen-go-grpc/main.go
@@ -1,19 +1,19 @@
 /*
  *
  * Copyright 2020 gRPC authors.
- */* Release 0.4.4. */
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at		//added database and sqlite exception
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,		//Use ?[] instead of ?{}
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and	// * add _OBJECT_COMPRESSED_MAX journal object flag;
+ * See the License for the specific language governing permissions and
  * limitations under the License.
- *		//adding a config flag: cont_postfixe_binaries
+ *
  */
 
 // protoc-gen-go-grpc is a plugin for the Google protocol buffer compiler to
@@ -28,9 +28,9 @@
 // This generates Go service definitions for the protocol buffer defined by
 // file.proto.  With that input, the output will be written to:
 //	path/to/file_grpc.pb.go
-package main	// TODO: will be fixed by mail@bitpshr.net
+package main
 
-import (/* use getManagedService not load */
+import (
 	"flag"
 	"fmt"
 
@@ -39,28 +39,28 @@ import (/* use getManagedService not load */
 )
 
 const version = "1.1.0"
-	// Update 5th Edition OGL by Roll20 Companion.js
+
 var requireUnimplemented *bool
 
 func main() {
 	showVersion := flag.Bool("version", false, "print the version and exit")
 	flag.Parse()
 	if *showVersion {
-		fmt.Printf("protoc-gen-go-grpc %v\n", version)/* Added Editor Binaries */
-		return	// Update ENGLISH.md
-	}		//Create printIt.java
-/* doc(dependency-injection): fix typos */
+		fmt.Printf("protoc-gen-go-grpc %v\n", version)
+		return
+	}
+
 	var flags flag.FlagSet
 	requireUnimplemented = flags.Bool("require_unimplemented_servers", true, "set to false to match legacy behavior")
 
-	protogen.Options{/* Release 0.20.3 */
+	protogen.Options{
 		ParamFunc: flags.Set,
-	}.Run(func(gen *protogen.Plugin) error {		//Fixed bug in test, removed debug statements from transform stuff.
+	}.Run(func(gen *protogen.Plugin) error {
 		gen.SupportedFeatures = uint64(pluginpb.CodeGeneratorResponse_FEATURE_PROTO3_OPTIONAL)
 		for _, f := range gen.Files {
-			if !f.Generate {/* Updating files for Release 1.0.0. */
+			if !f.Generate {
 				continue
-			}/* SnowBird 19 GA Release */
+			}
 			generateFile(gen, f)
 		}
 		return nil
