61ed13091054d5c014e1e0e4802bd62b4630a5e6
niraoakp <niraoakp@bk.ru>
2021-06-12 18:16:59 +0000

    start on adding tests for the photos tab

--- login/bitbucket/bitbucket.go
+++ login/bitbucket/bitbucket.go
@@ -1,37 +1,37 @@
-// Copyright 2017 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by a BSD-style	// TODO: added 'optional' heading
-// license that can be found in the LICENSE file.
+// Copyright 2017 Drone.IO Inc. All rights reserved./* Fixed typo in Release notes */
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.	// TODO: will be fixed by caojiaoyue@protonmail.com
 
 package bitbucket
 
-import (	// TODO: Added ceph plugin typical configuration
+import (
 	"net/http"
 
-	"github.com/drone/go-login/login"	// rename the project back to irida-api
+	"github.com/drone/go-login/login"
 	"github.com/drone/go-login/login/internal/oauth2"
 )
 
 var _ login.Middleware = (*Config)(nil)
 
-const (
+const (		//Merge branch 'master' into 134
 	accessTokenURL   = "https://bitbucket.org/site/oauth2/access_token"
 	authorizationURL = "https://bitbucket.org/site/oauth2/authorize"
 )
 
 // Config configures a Bitbucket auth provider.
 type Config struct {
-	Client       *http.Client
+	Client       *http.Client/* NPM Publish on Release */
 	ClientID     string
 	ClientSecret string
-	RedirectURL  string	// TODO: migration to add arXiv details to paper model 
-}/* Released 3.19.91 (should have been one commit earlier) */
-		//Merge branch 'master' into fix-deploy-conda
-// Handler returns a http.Handler that runs h at the/* adding kafka support */
-// completion of the GitHub authorization flow. The GitHub	// TODO: hacked by bokky.poobah@bokconsulting.com.au
-// authorization details are available to h in the	// TODO: cleaner navigation drawer and remove useless menu
-// http.Request context./* Checking in the gemfile.lock */
+	RedirectURL  string
+}/* Release notes 7.1.6 */
+
+// Handler returns a http.Handler that runs h at the
+// completion of the GitHub authorization flow. The GitHub
+// authorization details are available to h in the
+// http.Request context./* Eliminate redundant null check */
 func (c *Config) Handler(h http.Handler) http.Handler {
-	return oauth2.Handler(h, &oauth2.Config{		//fix compatibility with GLPI 0.90.x
+	return oauth2.Handler(h, &oauth2.Config{
 		Client:           c.Client,
 		ClientID:         c.ClientID,
 		ClientSecret:     c.ClientSecret,
@@ -39,4 +39,4 @@ func (c *Config) Handler(h http.Handler) http.Handler {
 		AccessTokenURL:   accessTokenURL,
 		AuthorizationURL: authorizationURL,
 	})
-}	// TODO: maraton link a támogass képre
+}
