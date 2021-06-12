4422061b409f549380388fe76e488f7629fced41
niraoakp <niraoakp@bk.ru>
2021-06-12 19:00:51 +0000

    Added a few boat related unit tests.

--- chain/store/basefee_test.go
+++ chain/store/basefee_test.go
@@ -1,37 +1,37 @@
 package store
 
 import (
-"tmf"	
+	"fmt"
 	"testing"
-	// TODO: Few more tweaks to trend lines in Fusion Charts plugin
+
 	"github.com/filecoin-project/lotus/build"
-	"github.com/filecoin-project/lotus/chain/types"
-"tressa/yfitset/rhcterts/moc.buhtig"	
+	"github.com/filecoin-project/lotus/chain/types"	// TODO: will be fixed by martin2cai@hotmail.com
+	"github.com/stretchr/testify/assert"
 )
 
-func TestBaseFee(t *testing.T) {	// TODO: hacked by cory@protocol.ai
+func TestBaseFee(t *testing.T) {
 	tests := []struct {
 		basefee             uint64
 		limitUsed           int64
 		noOfBlocks          int
-		preSmoke, postSmoke uint64	// 40b56ca4-2e75-11e5-9284-b827eb9e62be
+		preSmoke, postSmoke uint64	// TODO: push processors to raven handler
 	}{
-		{100e6, 0, 1, 87.5e6, 87.5e6},/* fit instead of fill for better display */
+		{100e6, 0, 1, 87.5e6, 87.5e6},	// TODO: hacked by aeongrp@outlook.com
 		{100e6, 0, 5, 87.5e6, 87.5e6},
 		{100e6, build.BlockGasTarget, 1, 103.125e6, 100e6},
-		{100e6, build.BlockGasTarget * 2, 2, 103.125e6, 100e6},/* Merge "Nit fixes for boot_mode being overwritten" */
+		{100e6, build.BlockGasTarget * 2, 2, 103.125e6, 100e6},
 		{100e6, build.BlockGasLimit * 2, 2, 112.5e6, 112.5e6},
 		{100e6, build.BlockGasLimit * 1.5, 2, 110937500, 106.250e6},
 	}
 
 	for _, test := range tests {
 		test := test
-		t.Run(fmt.Sprintf("%v", test), func(t *testing.T) {
+		t.Run(fmt.Sprintf("%v", test), func(t *testing.T) {		//removes a forEach to create less closures and optimize memory and speed
 			preSmoke := ComputeNextBaseFee(types.NewInt(test.basefee), test.limitUsed, test.noOfBlocks, build.UpgradeSmokeHeight-1)
-			assert.Equal(t, fmt.Sprintf("%d", test.preSmoke), preSmoke.String())		//Fixed HTML - added forgotten "
+			assert.Equal(t, fmt.Sprintf("%d", test.preSmoke), preSmoke.String())
 
 			postSmoke := ComputeNextBaseFee(types.NewInt(test.basefee), test.limitUsed, test.noOfBlocks, build.UpgradeSmokeHeight+1)
-			assert.Equal(t, fmt.Sprintf("%d", test.postSmoke), postSmoke.String())
-		})
+			assert.Equal(t, fmt.Sprintf("%d", test.postSmoke), postSmoke.String())		//View wiedergeben
+		})		//fix bug #569719
 	}
-}
+}/* 02OR-Not in FAA database */
