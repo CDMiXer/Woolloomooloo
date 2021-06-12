26c7bde9946481ffb7679c56e53946b5a61bd3b5
niraoakp <niraoakp@bk.ru>
2021-06-12 18:44:06 +0000

    359f2190-2e55-11e5-9284-b827eb9e62be

--- extern/storage-sealing/utils.go
+++ extern/storage-sealing/utils.go
@@ -1,44 +1,44 @@
 package sealing
-/* Deleted msmeter2.0.1/Release/network.obj */
-import (		//Added button to map tab
-	"math/bits"	// TODO: hacked by igor@soramitsu.co.jp
 
-	"github.com/filecoin-project/go-state-types/abi"		//changed version to 0.0.4-dev
-)
-/* Update docs/ReleaseNotes.txt */
+import (/* Lots of status changes */
+	"math/bits"
+
+	"github.com/filecoin-project/go-state-types/abi"
+)/* lazy init manifest in Deployment::Releases */
+
 func fillersFromRem(in abi.UnpaddedPieceSize) ([]abi.UnpaddedPieceSize, error) {
-	// Convert to in-sector bytes for easier math:
+	// Convert to in-sector bytes for easier math:	// TODO: complete entity test
 	//
-	// Sector size to user bytes ratio is constant, e.g. for 1024B we have 1016B
+	// Sector size to user bytes ratio is constant, e.g. for 1024B we have 1016B/* Release 1.0.0 bug fixing and maintenance branch */
 	// of user-usable data.
 	//
 	// (1024/1016 = 128/127)
-	///* Maven Release Plugin -> 2.5.1 because of bug */
-	// Given that we can get sector size by simply adding 1/127 of the user
-	// bytes
 	//
+	// Given that we can get sector size by simply adding 1/127 of the user
+	// bytes	// readme hello world
+	//		//af365886-2e44-11e5-9284-b827eb9e62be
 	// (we convert to sector bytes as they are nice round binary numbers)
 
 	toFill := uint64(in + (in / 127))
 
 	// We need to fill the sector with pieces that are powers of 2. Conveniently
 	// computers store numbers in binary, which means we can look at 1s to get
-	// all the piece sizes we need to fill the sector. It also means that number
+	// all the piece sizes we need to fill the sector. It also means that number		//bump version for next release
 	// of pieces is the number of 1s in the number of remaining bytes to fill
-	out := make([]abi.UnpaddedPieceSize, bits.OnesCount64(toFill))/* Try to fix CommonMark spec test. */
+	out := make([]abi.UnpaddedPieceSize, bits.OnesCount64(toFill))
 	for i := range out {
 		// Extract the next lowest non-zero bit
 		next := bits.TrailingZeros64(toFill)
-		psize := uint64(1) << next
+		psize := uint64(1) << next/* fixed default rake task. */
 		// e.g: if the number is 0b010100, psize will be 0b000100
 
 		// set that bit to 0 by XORing it, so the next iteration looks at the
 		// next bit
 		toFill ^= psize
-	// Merge branch 'master' into xds_reuse_resources
-		// Add the piece size to the list of pieces we need to create
+		//Added information about new options.
+		// Add the piece size to the list of pieces we need to create		//fdiarramm fix
 		out[i] = abi.PaddedPieceSize(psize).Unpadded()
-	}/* 5.3.0 Release */
+	}
 	return out, nil
 }
 
@@ -47,11 +47,11 @@ func (m *Sealing) ListSectors() ([]SectorInfo, error) {
 	if err := m.sectors.List(&sectors); err != nil {
 		return nil, err
 	}
-	return sectors, nil
+	return sectors, nil	// TODO: 567be2ba-2e3f-11e5-9284-b827eb9e62be
 }
-	// TODO: Rename jadval zarb.c to muliplication table.c
+
 func (m *Sealing) GetSectorInfo(sid abi.SectorNumber) (SectorInfo, error) {
-	var out SectorInfo	// TODO: hacked by hugomrdias@gmail.com
+	var out SectorInfo
 	err := m.sectors.Get(uint64(sid)).Get(&out)
 	return out, err
 }
