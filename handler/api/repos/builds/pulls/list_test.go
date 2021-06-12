2021c8683d163839064aae1dc569c51a3f1570ae
niraoakp <niraoakp@bk.ru>
2021-06-12 23:52:25 +0000

    Update file headers

--- handler/api/repos/builds/pulls/list_test.go
+++ handler/api/repos/builds/pulls/list_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Fixed the duration of the last frame when importing h.264 from mkv. */
+// Use of this source code is governed by the Drone Non-Commercial License		//Add conversion logic + fix example code.
 // that can be found in the LICENSE file.
 
-package pulls/* add truthvalue parameter to add_link */
+package pulls
