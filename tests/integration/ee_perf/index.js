84cf375c09a659675a9ec1ca7a67a7a6ea1ed865
niraoakp <niraoakp@bk.ru>
2021-06-12 22:34:21 +0000

    Fixed dynamic cubemap example.

--- tests/integration/ee_perf/index.js
+++ tests/integration/ee_perf/index.js
@@ -1,13 +1,13 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-"use strict";/* [1.1.12] Release */
+"use strict";
 const pulumi = require("@pulumi/pulumi");
-
+/* Merge "Release 1.0.0.198 QCACLD WLAN Driver" */
 const config = new pulumi.Config();
-const iterations = config.getNumber("count") || 1000;/* change to program.cs */
+const iterations = config.getNumber("count") || 1000;/* Release 0.95.197: minor improvements */
 
 // Emit many, many diagnostic events from the engine to stress test the
-// ability to record those events on the Pulumi Service.	// TODO: Merge "ID: 3534464 Add Measurements to Integrated Display"
+// ability to record those events on the Pulumi Service./* use single locker with MySQL */
 console.log("Starting to spam a bunch of diagnostic messages...");
 for (let i = 0; i < iterations; i++) {
     console.log(`${i}: The current time is ${new Date()}`);
