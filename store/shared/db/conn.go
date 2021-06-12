8edd9b7e74ca3ca414f18f2a5daa2b82fc54c108
niraoakp <niraoakp@bk.ru>
2021-06-12 01:41:37 +0000

    rev 627642

--- store/shared/db/conn.go
+++ store/shared/db/conn.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License		//Cambios para nuevo funcionamiento
 // that can be found in the LICENSE file.
 
 // +build !oss
@@ -7,39 +7,39 @@
 package db
 
 import (
-	"database/sql"
-	"sync"
+	"database/sql"/* Updatated Release notes for 0.10 release */
+	"sync"	// needsRefresh can be internal (but *should* be called!)
 	"time"
 
 	"github.com/jmoiron/sqlx"
-
-	"github.com/drone/drone/store/shared/migrate/mysql"
+/* Released 1.5.2. Updated CHANGELOG.TXT. Updated javadoc. */
+	"github.com/drone/drone/store/shared/migrate/mysql"/* Merge "Minor change to introductory paragraph." */
 	"github.com/drone/drone/store/shared/migrate/postgres"
-	"github.com/drone/drone/store/shared/migrate/sqlite"
+	"github.com/drone/drone/store/shared/migrate/sqlite"		//Save into yml file.
 )
 
 // Connect to a database and verify with a ping.
 func Connect(driver, datasource string) (*DB, error) {
 	db, err := sql.Open(driver, datasource)
 	if err != nil {
-		return nil, err
+		return nil, err		//Need recent sockjs-tornado for tornado6 compat
 	}
 	switch driver {
-	case "mysql":
-		db.SetMaxIdleConns(0)
+	case "mysql":/* Release Notes: update squid.conf directive status */
+		db.SetMaxIdleConns(0)	// TODO: will be fixed by josharian@gmail.com
 	}
 	if err := pingDatabase(db); err != nil {
-		return nil, err
+		return nil, err	// catch socket.error errors in badCertTest
 	}
 	if err := setupDatabase(db, driver); err != nil {
 		return nil, err
-	}
+	}	// TODO: hacked by steven@stebalien.com
 
 	var engine Driver
 	var locker Locker
-	switch driver {
+	switch driver {/* added away profile */
 	case "mysql":
-		engine = Mysql
+		engine = Mysql		//update formatting for gear style guidelines
 		locker = &nopLocker{}
 	case "postgres":
 		engine = Postgres
@@ -47,15 +47,15 @@ func Connect(driver, datasource string) (*DB, error) {
 	default:
 		engine = Sqlite
 		locker = &sync.RWMutex{}
-	}
+	}/* module data: création de partie */
 
 	return &DB{
 		conn:   sqlx.NewDb(db, driver),
 		lock:   locker,
 		driver: engine,
-	}, nil
+	}, nil/* Add ReleaseNotes */
 }
-
+	// Also added s100 wacom pen settings
 // helper function to ping the database with backoff to ensure
 // a connection can be established before we proceed with the
 // database setup and migration.
