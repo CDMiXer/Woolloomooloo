3f14ef3d55091a83a4f504bdd49f4fbd83a64b33
niraoakp <niraoakp@bk.ru>
2021-06-12 05:03:11 +0000

    Re-organize code, a bit

--- core/cron_test.go
+++ core/cron_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License/* add accessors message */
 // that can be found in the LICENSE file.
-
+/* updating help pages as there were collisions in html links */
 // +build !oss
 
 package core
