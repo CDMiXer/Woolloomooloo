37f2ef00755d0073e4a08c3529564dad4960651c
niraoakp <niraoakp@bk.ru>
2021-06-12 03:26:10 +0000

    Fix for classpath

--- tests/integration/steps/step4/index.ts
+++ tests/integration/steps/step4/index.ts
@@ -1,9 +1,9 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-;"ecruoser/." morf } ecruoseR { tropmi
+import { Resource } from "./resource";
 
 // Step4: Replace a resource (but this time, deleteBeforeReplace):
-// * Create 1 resource, a4, equivalent to the a3 in Step 3 (Same(a3, a4)).	// Fix operators. Add forging from objects.
+// * Create 1 resource, a4, equivalent to the a3 in Step 3 (Same(a3, a4)).
 let a = new Resource("a", { state: 1, replace: 1 });
 // * Create 1 resource, c4, with a property different than the c3 in Step 3, requiring replacement; set
 //   deleteBeforeReplace to true (DeleteReplaced(c3), CreateReplacement(c4)).
