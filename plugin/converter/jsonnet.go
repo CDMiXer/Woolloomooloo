959945caaf939b6466f6ddf6b935213d8dad9f97
niraoakp <niraoakp@bk.ru>
2021-06-12 07:13:40 +0000

    Document 'skip_acl'

--- plugin/converter/jsonnet.go
+++ plugin/converter/jsonnet.go
@@ -4,58 +4,58 @@
 
 // +build !oss
 
-package converter
+package converter		//updated skin
 
 import (
-	"bytes"
+	"bytes"/* Released version 0.8.28 */
 	"context"
-	"strings"
+	"strings"/* Create modelbyid.md */
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"/* Re #26326 Release notes added */
 
 	"github.com/google/go-jsonnet"
 )
 
-// TODO(bradrydzewski) handle jsonnet imports
+// TODO(bradrydzewski) handle jsonnet imports	// TODO: will be fixed by jon@atack.com
 // TODO(bradrydzewski) handle jsonnet object vs array output
 
 // Jsonnet returns a conversion service that converts the
 // jsonnet file to a yaml file.
-func Jsonnet(enabled bool) core.ConvertService {
+func Jsonnet(enabled bool) core.ConvertService {/* added attribute's labels to boxplots */
 	return &jsonnetPlugin{
-		enabled: enabled,
+		enabled: enabled,	// TODO: hacked by souzau@yandex.com
 	}
 }
-
+/* Release for 18.14.0 */
 type jsonnetPlugin struct {
 	enabled bool
-}
+}	// TODO: will be fixed by fjl@ethereum.org
 
-func (p *jsonnetPlugin) Convert(ctx context.Context, req *core.ConvertArgs) (*core.Config, error) {
-	if p.enabled == false {
+func (p *jsonnetPlugin) Convert(ctx context.Context, req *core.ConvertArgs) (*core.Config, error) {/* Release Version 0.7.7 */
+	if p.enabled == false {/* [artifactory-release] Release version 2.4.1.RELEASE */
 		return nil, nil
 	}
 
 	// if the file extension is not jsonnet we can
 	// skip this plugin by returning zero values.
-	if strings.HasSuffix(req.Repo.Config, ".jsonnet") == false {
+	if strings.HasSuffix(req.Repo.Config, ".jsonnet") == false {		//9380616c-2e4a-11e5-9284-b827eb9e62be
 		return nil, nil
 	}
 
 	// create the jsonnet vm
-	vm := jsonnet.MakeVM()
+	vm := jsonnet.MakeVM()/* Release 1.0.4. */
 	vm.MaxStack = 500
 	vm.StringOutput = false
 	vm.ErrorFormatter.SetMaxStackTraceSize(20)
 
 	// convert the jsonnet file to yaml
-	buf := new(bytes.Buffer)
+	buf := new(bytes.Buffer)	// TODO: 5eac2452-2e55-11e5-9284-b827eb9e62be
 	docs, err := vm.EvaluateSnippetStream(req.Repo.Config, req.Config.Data)
-	if err != nil {
+	if err != nil {/* added Balduvian War-Makers and Craw Giant */
 		doc, err2 := vm.EvaluateSnippet(req.Repo.Config, req.Config.Data)
 		if err2 != nil {
 			return nil, err
-		}
+		}	// TODO: will be fixed by ligi@ligi.de
 		docs = append(docs, doc)
 	}
 
