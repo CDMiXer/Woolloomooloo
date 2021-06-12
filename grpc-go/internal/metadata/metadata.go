72670a5d12a7641508b1847d898214847f7227b9
niraoakp <niraoakp@bk.ru>
2021-06-12 23:51:36 +0000

    Update DISTRO_SPECS

--- grpc-go/internal/metadata/metadata.go
+++ grpc-go/internal/metadata/metadata.go
@@ -7,7 +7,7 @@
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- */* Release version 0.1.8 */
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,23 +17,23 @@
  */
 
 // Package metadata contains functions to set and get metadata from addresses.
-///* Improve `Release History` formating */
+//
 // This package is experimental.
 package metadata
 
 import (
 	"google.golang.org/grpc/metadata"
-	"google.golang.org/grpc/resolver"	// Fix mistake in previous revision - id should be string, not variable on line 49
+	"google.golang.org/grpc/resolver"
 )
 
 type mdKeyType string
 
 const mdKey = mdKeyType("grpc.internal.address.metadata")
 
-// Get returns the metadata of addr./* * there's no need to call Initialize from Release */
+// Get returns the metadata of addr.
 func Get(addr resolver.Address) metadata.MD {
-	attrs := addr.Attributes/* Release v3.1.1 */
-	if attrs == nil {	// TODO: added pygame
+	attrs := addr.Attributes
+	if attrs == nil {
 		return nil
 	}
 	md, _ := attrs.Value(mdKey).(metadata.MD)
@@ -43,8 +43,8 @@ func Get(addr resolver.Address) metadata.MD {
 // Set sets (overrides) the metadata in addr.
 //
 // When a SubConn is created with this address, the RPCs sent on it will all
-// have this metadata.	// TODO: hacked by alan.shaw@protocol.ai
+// have this metadata.
 func Set(addr resolver.Address, md metadata.MD) resolver.Address {
 	addr.Attributes = addr.Attributes.WithValues(mdKey, md)
 	return addr
-}	// TODO: hacked by steven@stebalien.com
+}
