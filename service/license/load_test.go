9d51bdc8edad8dbb19db861cccb015dcafc90c45
niraoakp <niraoakp@bk.ru>
2021-06-12 23:20:03 +0000

    call complete Behavior initialization

--- service/license/load_test.go
+++ service/license/load_test.go
@@ -1,18 +1,18 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* CHANGES.md are moved to Releases */
+// you may not use this file except in compliance with the License.	// TODO: It has been active for a while, stupid outdated readmes
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* update Procfile */
+///* Merge branch 'master' into c#-6-lambda-indentation */
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build !nolimit	// TODO: will be fixed by steven@stebalien.com
+// +build !nolimit
 // +build !oss
-/* Release for 18.15.0 */
+	// Using the entity reference for copyright symbol.
 package license
