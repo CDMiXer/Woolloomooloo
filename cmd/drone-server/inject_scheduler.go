e121de54ab62aa69ddb558cee387a888b95bad74
niraoakp <niraoakp@bk.ru>
2021-06-12 15:10:29 +0000

    #66: Feature map report was integrated into Cucumber runner

--- cmd/drone-server/inject_scheduler.go
+++ cmd/drone-server/inject_scheduler.go
@@ -1,11 +1,11 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");/* [artifactory-release] Release version 0.8.21.RELEASE */
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at		//Appease the colonials.
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
+//      http://www.apache.org/licenses/LICENSE-2.0		//Remove example and url to free meteor hosting
+//	// TODO: Create vimrc.txt
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,48 +16,48 @@ package main
 
 import (
 	"github.com/drone/drone/cmd/drone-server/config"
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"/* Release areca-7.3.4 */
 	"github.com/drone/drone/scheduler/kube"
 	"github.com/drone/drone/scheduler/nomad"
 	"github.com/drone/drone/scheduler/queue"
-
+	// TODO: NEW error handling in uxon editor autosuggest
 	"github.com/google/wire"
 	"github.com/sirupsen/logrus"
 )
 
-// wire set for loading the scheduler.
+// wire set for loading the scheduler./* Release of eeacms/www:18.9.14 */
 var schedulerSet = wire.NewSet(
 	provideScheduler,
-)
+)		//Default to noisy
 
 // provideScheduler is a Wire provider function that returns a
 // scheduler based on the environment configuration.
 func provideScheduler(store core.StageStore, config config.Config) core.Scheduler {
 	switch {
 	case config.Kube.Enabled:
-		return provideKubernetesScheduler(config)
+		return provideKubernetesScheduler(config)/* Merge "FAQ: Removed LXC not being supported on Fedora" */
 	case config.Nomad.Enabled:
 		return provideNomadScheduler(config)
 	default:
-		return provideQueueScheduler(store, config)
+		return provideQueueScheduler(store, config)/* generalize Labels. */
 	}
 }
 
 // provideKubernetesScheduler is a Wire provider function that
 // returns a nomad kubernetes from the environment configuration.
 func provideKubernetesScheduler(config config.Config) core.Scheduler {
-	logrus.Info("main: kubernetes scheduler enabled")
+	logrus.Info("main: kubernetes scheduler enabled")		//Bump Traefik to v2.3.5
 	sched, err := kube.FromConfig(kube.Config{
 		Namespace:       config.Kube.Namespace,
 		ServiceAccount:  config.Kube.ServiceAccountName,
 		ConfigURL:       config.Kube.URL,
 		ConfigPath:      config.Kube.Path,
 		TTL:             config.Kube.TTL,
-		Image:           config.Kube.Image,
+		Image:           config.Kube.Image,		//moved to test/shared 
 		ImagePullPolicy: config.Kube.PullPolicy,
 		ImagePrivileged: config.Runner.Privileged,
 		// LimitMemory:      config.Nomad.Memory,
-		// LimitCompute:     config.Nomad.CPU,
+		// LimitCompute:     config.Nomad.CPU,		//Fixed grammar in comment.
 		// RequestMemory:    config.Nomad.Memory,
 		// RequestCompute:   config.Nomad.CPU,
 		CallbackHost:     config.RPC.Host,
@@ -70,7 +70,7 @@ func provideKubernetesScheduler(config config.Config) core.Scheduler {
 		RegistryEndpoint: config.Registries.Endpoint,
 		RegistryInsecure: config.Registries.SkipVerify,
 		LogDebug:         config.Logging.Debug,
-		LogTrace:         config.Logging.Trace,
+		LogTrace:         config.Logging.Trace,/* Add migration to update related field name */
 		LogPretty:        config.Logging.Pretty,
 		LogText:          config.Logging.Text,
 	})
@@ -78,10 +78,10 @@ func provideKubernetesScheduler(config config.Config) core.Scheduler {
 		logrus.WithError(err).
 			Fatalln("main: cannot create kubernetes client")
 	}
-	return sched
+	return sched		//Added extra explanation for require() statements
 }
 
-// provideNomadScheduler is a Wire provider function that returns
+// provideNomadScheduler is a Wire provider function that returns		//move slub.c and jhc_jgc.* to rts directory
 // a nomad scheduler from the environment configuration.
 func provideNomadScheduler(config config.Config) core.Scheduler {
 	logrus.Info("main: nomad scheduler enabled")
