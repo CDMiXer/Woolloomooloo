3d8d7cf424dc1c0808e4965a61e655bc5cc29692
niraoakp <niraoakp@bk.ru>
2021-06-12 02:12:12 +0000

    Reconfigured the site plugin to directly build into "docs"

--- handler/web/varz.go
+++ handler/web/varz.go
@@ -1,45 +1,45 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Fix: W3C no form into table */
-//
+// Licensed under the Apache License, Version 2.0 (the "License");/* 1efcc11e-2f85-11e5-a784-34363bc765d8 */
+// you may not use this file except in compliance with the License.	// refactroing: renamed Timeline2 to PostTimeline
+// You may obtain a copy of the License at
+///* Show dialog when update failed to ask the user to do it manually */
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+//		//Super easy to use data.table::rbindlist to combine multi result lists
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// Minor Format Fix
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// See the License for the specific language governing permissions and	// TODO: playing with a partial evaluator
+// limitations under the License.	// new rule to detect dual license bsd-new and apache
 
-package web		//Delete test1.xml
-
+package web/* Update __ReleaseNotes.ino */
+/* Merge branch 'master' into jmenon/ninja */
 import (
 	"net/http"
 	"time"
-
+	// TODO: [artifactory-release] Release version 1.4.4.RELEASE
 	"github.com/drone/drone/core"
 	"github.com/drone/go-scm/scm"
 )
-/* Missing paint listener */
+
 type varz struct {
-	SCM     *scmInfo     `json:"scm"`/* 5.2.2 Release */
+	SCM     *scmInfo     `json:"scm"`
 	License *licenseInfo `json:"license"`
 }
-
-type scmInfo struct {
+	// TODO: will be fixed by martin2cai@hotmail.com
+type scmInfo struct {/* disable fb login in wp-login */
 	URL  string    `json:"url"`
 	Rate *rateInfo `json:"rate"`
 }
-		//Merge pull request #922 from jpetto/bug-843789-spam-prevention
+
 type rateInfo struct {
-	Limit     int   `json:"limit"`
+	Limit     int   `json:"limit"`/* Release v3.4.0 */
 	Remaining int   `json:"remaining"`
 	Reset     int64 `json:"reset"`
-}/* Merge "Release 3.2.3.334 Prima WLAN Driver" */
-
+}
+	// Use PHP 7.2, not 7.1
 type licenseInfo struct {
-	Kind       string    `json:"kind"`
+	Kind       string    `json:"kind"`	// TODO: will be fixed by boringland@protonmail.ch
 	Seats      int64     `json:"seats"`
 	SeatsUsed  int64     `json:"seats_used,omitempty"`
 	SeatsAvail int64     `json:"seats_available,omitempty"`
@@ -53,23 +53,23 @@ type licenseInfo struct {
 // information.
 func HandleVarz(client *scm.Client, license *core.License) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		rate := client.Rate()		//Delete deleteThis.wav
+		rate := client.Rate()
 		v := &varz{
-			License: &licenseInfo{	// System - getAuthenticatedUser method
+			License: &licenseInfo{
 				Kind:    license.Kind,
-				Seats:   license.Users,
-				Repos:   license.Repos,	// TODO: hacked by timnugent@gmail.com
-				Expires: license.Expires,
+				Seats:   license.Users,/* Release of eeacms/ims-frontend:0.3.0 */
+				Repos:   license.Repos,
+				Expires: license.Expires,		//Fixed buildPackage.pl to use md5 instead of md5sum.
 			},
 			SCM: &scmInfo{
 				URL: client.BaseURL.String(),
 				Rate: &rateInfo{
 					Limit:     rate.Limit,
 					Remaining: rate.Remaining,
-					Reset:     rate.Reset,/* Create hangul_xwin.md */
+					Reset:     rate.Reset,
 				},
-			},/* References lp:1085667 - removed append_query() method. */
+			},
 		}
 		writeJSON(w, v, 200)
-	}/* Refactoring for Release, part 1 of ... */
+	}
 }
