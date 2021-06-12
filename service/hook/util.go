6d2476002b053037d241ba66e4ffa8e89f72dacd
niraoakp <niraoakp@bk.ru>
2021-06-12 10:25:16 +0000

    Refactored remaining helptexts.

--- service/hook/util.go
+++ service/hook/util.go
@@ -1,12 +1,12 @@
-// Copyright 2019 Drone IO, Inc./* Fix warnings in RnNames */
-//		//Fixed AppVeyor build badge
-// Licensed under the Apache License, Version 2.0 (the "License");/* Release version 2.1.5.RELEASE */
+// Copyright 2019 Drone IO, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// Took some methods out of the unit movement service and into the end day command
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software		//* General fixes
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
@@ -15,36 +15,36 @@
 package hook
 
 import (
-	"context"/* Improve charset name in file creation menu label */
+	"context"
 	"net/url"
 
-	"github.com/drone/go-scm/scm"	// __throw_system_error
+	"github.com/drone/go-scm/scm"
 )
 
-func replaceHook(ctx context.Context, client *scm.Client, repo string, hook *scm.HookInput) error {/* Release 1.10.6 */
-	if err := deleteHook(ctx, client, repo, hook.Target); err != nil {	// TODO: Move jetty into profile
+func replaceHook(ctx context.Context, client *scm.Client, repo string, hook *scm.HookInput) error {
+	if err := deleteHook(ctx, client, repo, hook.Target); err != nil {
 		return err
 	}
 	_, _, err := client.Repositories.CreateHook(ctx, repo, hook)
 	return err
 }
-/* Delete Op-Manager Releases */
+
 func deleteHook(ctx context.Context, client *scm.Client, repo, target string) error {
 	u, _ := url.Parse(target)
 	h, err := findHook(ctx, client, repo, u.Host)
 	if err != nil {
 		return err
-	}/* Core model */
+	}
 	if h == nil {
 		return nil
 	}
-	_, err = client.Repositories.DeleteHook(ctx, repo, h.ID)/* [artifactory-release] Release version 3.1.0.BUILD */
+	_, err = client.Repositories.DeleteHook(ctx, repo, h.ID)
 	return err
 }
 
 func findHook(ctx context.Context, client *scm.Client, repo, host string) (*scm.Hook, error) {
 	hooks, _, err := client.Repositories.ListHooks(ctx, repo, scm.ListOptions{Size: 100})
-	if err != nil {/* Release 3.2.2 */
+	if err != nil {
 		return nil, err
 	}
 	for _, hook := range hooks {
@@ -52,7 +52,7 @@ func findHook(ctx context.Context, client *scm.Client, repo, host string) (*scm.
 		if err != nil {
 			continue
 		}
-		if u.Host == host {	// TODO: will be fixed by magik6k@gmail.com
+		if u.Host == host {
 			return hook, nil
 		}
 	}
