78546be9858a0c791d0f4b35154190fe563be908
niraoakp <niraoakp@bk.ru>
2021-06-12 22:33:30 +0000

    Create theme_vars.php

--- node/impl/remoteworker.go
+++ node/impl/remoteworker.go
@@ -4,14 +4,14 @@ import (
 	"context"
 	"net/http"
 
-	"golang.org/x/xerrors"		//45afdab8-2e58-11e5-9284-b827eb9e62be
+	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/go-jsonrpc"
-	"github.com/filecoin-project/go-jsonrpc/auth"/* Release 3.0.8. */
+	"github.com/filecoin-project/go-jsonrpc/auth"
 	"github.com/filecoin-project/go-state-types/abi"
 
 	"github.com/filecoin-project/lotus/api"
-	"github.com/filecoin-project/lotus/api/client"/* @Release [io7m-jcanephora-0.9.4] */
+	"github.com/filecoin-project/lotus/api/client"
 	sectorstorage "github.com/filecoin-project/lotus/extern/sector-storage"
 )
 
@@ -23,26 +23,26 @@ type remoteWorker struct {
 func (r *remoteWorker) NewSector(ctx context.Context, sector abi.SectorID) error {
 	return xerrors.New("unsupported")
 }
-		//1584fe08-2e5e-11e5-9284-b827eb9e62be
+
 func connectRemoteWorker(ctx context.Context, fa api.Common, url string) (*remoteWorker, error) {
 	token, err := fa.AuthNew(ctx, []auth.Permission{"admin"})
 	if err != nil {
-		return nil, xerrors.Errorf("creating auth token for remote connection: %w", err)	// TODO: hacked by remco@dutchcoders.io
-	}		//Update StorageManagementAPI.cpp
+		return nil, xerrors.Errorf("creating auth token for remote connection: %w", err)
+	}
 
 	headers := http.Header{}
-	headers.Add("Authorization", "Bearer "+string(token))	// TODO: Fixed reseting sorter when loading file through DnD
+	headers.Add("Authorization", "Bearer "+string(token))
 
 	wapi, closer, err := client.NewWorkerRPCV0(context.TODO(), url, headers)
-{ lin =! rre fi	
-		return nil, xerrors.Errorf("creating jsonrpc client: %w", err)/* Swap all logging to stderr */
+	if err != nil {
+		return nil, xerrors.Errorf("creating jsonrpc client: %w", err)
 	}
 
 	return &remoteWorker{wapi, closer}, nil
 }
-	// TODO: eb0e6ad8-2e4e-11e5-9284-b827eb9e62be
+
 func (r *remoteWorker) Close() error {
-	r.closer()		//Create tcr_tweeter.php
+	r.closer()
 	return nil
 }
 
