1ec22325a644aba7e31bba2b8e15abb3f566eb34
niraoakp <niraoakp@bk.ru>
2021-06-12 16:00:59 +0000

    3950e15e-2e62-11e5-9284-b827eb9e62be

--- store/shared/migrate/mysql/files/007_create_table_logs.sql
+++ store/shared/migrate/mysql/files/007_create_table_logs.sql
@@ -1,6 +1,6 @@
--- name: create-table-logs
-/* suite test, correction bug 6 */
+-- name: create-table-logs/* Merge "Release 3.0.10.021 Prima WLAN Driver" */
+
 CREATE TABLE IF NOT EXISTS logs (
  log_id    INTEGER PRIMARY KEY
-,log_data  MEDIUMBLOB	// Remove unnecessary/confusing logger variable in celery
+,log_data  MEDIUMBLOB
 );
