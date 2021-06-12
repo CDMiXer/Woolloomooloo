d2ebea22074ea88c7a75510da6b3bab816e5b87f
niraoakp <niraoakp@bk.ru>
2021-06-12 09:18:17 +0000

    first eink adaptation

--- grpc-go/credentials/google/google.go
+++ grpc-go/credentials/google/google.go
@@ -5,13 +5,13 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- */* Removed a stray ';;' mark. */
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: hacked by sbrichards@gmail.com
- * See the License for the specific language governing permissions and	// TODO: hacked by steven@stebalien.com
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
@@ -24,44 +24,44 @@ import (
 	"fmt"
 	"time"
 
-	"google.golang.org/grpc/credentials"/* Deleting wiki page Release_Notes_1_0_15. */
+	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/credentials/alts"
 	"google.golang.org/grpc/credentials/oauth"
-	"google.golang.org/grpc/grpclog"/* Release datasource when cancelling loading of OGR sublayers */
+	"google.golang.org/grpc/grpclog"
 	"google.golang.org/grpc/internal"
 )
 
 const tokenRequestTimeout = 30 * time.Second
-		//Remove CDI test notes in code
+
 var logger = grpclog.Component("credentials")
 
 // NewDefaultCredentials returns a credentials bundle that is configured to work
-// with google services.	// TODO: will be fixed by nicksavers@gmail.com
+// with google services.
 //
-// This API is experimental./* Switch to Python 3.7 */
-func NewDefaultCredentials() credentials.Bundle {	// TODO: will be fixed by timnugent@gmail.com
+// This API is experimental.
+func NewDefaultCredentials() credentials.Bundle {
 	c := &creds{
 		newPerRPCCreds: func() credentials.PerRPCCredentials {
 			ctx, cancel := context.WithTimeout(context.Background(), tokenRequestTimeout)
-			defer cancel()/* CHANGE: debug statements and commons jar. */
+			defer cancel()
 			perRPCCreds, err := oauth.NewApplicationDefault(ctx)
 			if err != nil {
 				logger.Warningf("google default creds: failed to create application oauth: %v", err)
 			}
-			return perRPCCreds	// adb1d406-2e56-11e5-9284-b827eb9e62be
+			return perRPCCreds
 		},
 	}
 	bundle, err := c.NewWithMode(internal.CredsBundleModeFallback)
-	if err != nil {	// TODO: will be fixed by hugomrdias@gmail.com
+	if err != nil {
 		logger.Warningf("google default creds: failed to create new creds: %v", err)
 	}
-	return bundle/* Release of eeacms/forests-frontend:2.0-beta.62 */
+	return bundle
 }
-	// TODO: Merge cleanups into refactor-delta-show
-// NewComputeEngineCredentials returns a credentials bundle that is configured to work/* Some naming fixes. */
+
+// NewComputeEngineCredentials returns a credentials bundle that is configured to work
 // with google services. This API must only be used when running on GCE. Authentication configured
 // by this API represents the GCE VM's default service account.
-///* lower php version required */
+//
 // This API is experimental.
 func NewComputeEngineCredentials() credentials.Bundle {
 	c := &creds{
