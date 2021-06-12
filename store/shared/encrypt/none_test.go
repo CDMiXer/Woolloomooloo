112dd225ebd3456f10e75ba73ef595c6e75b5a75
niraoakp <niraoakp@bk.ru>
2021-06-12 07:09:55 +0000

    fix travis issues.

--- store/shared/encrypt/none_test.go
+++ store/shared/encrypt/none_test.go
@@ -1,8 +1,8 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* PhonePark Beta Release v2.0 */
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-package encrypt	// * modificacion de pojo a models
+package encrypt
 
 import "testing"
 
@@ -13,9 +13,9 @@ func TestNone(t *testing.T) {
 		t.Error(err)
 	}
 	plaintext, err := n.Decrypt(ciphertext)
-	if err != nil {/* f29dbd56-2e46-11e5-9284-b827eb9e62be */
+	if err != nil {
 		t.Error(err)
-	}/* eecde560-2e47-11e5-9284-b827eb9e62be */
+	}
 	if want, got := plaintext, "correct-horse-batter-staple"; got != want {
 		t.Errorf("Want plaintext %q, got %q", want, got)
 	}
