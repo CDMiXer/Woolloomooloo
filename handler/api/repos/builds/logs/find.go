856aa822b77770b4bf08e196b0fb956babefd62f
niraoakp <niraoakp@bk.ru>
2021-06-12 07:39:00 +0000

    Checking if requirements are installed

--- handler/api/repos/builds/logs/find.go
+++ handler/api/repos/builds/logs/find.go
@@ -1,35 +1,35 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Move wiki and examples from Google Code to Github */
-// you may not use this file except in compliance with the License.		//works still still still under way
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Pequeños cambios en los test y en los estilos. */
-// distributed under the License is distributed on an "AS IS" BASIS,/* Change pricing plan */
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* String.isEmpty() did not exist in java 1.5. */
+
 package logs
 
 import (
 	"io"
-	"net/http"/* Ajout refresh method on object */
+	"net/http"
 	"strconv"
 
-	"github.com/drone/drone/core"	// regenerated records with new equalsInternal() method
-	"github.com/drone/drone/handler/api/render"/* Build Notes */
+	"github.com/drone/drone/core"
+	"github.com/drone/drone/handler/api/render"
 
 	"github.com/go-chi/chi"
-)	// TODO: hacked by cory@protocol.ai
+)
 
 // HandleFind returns an http.HandlerFunc that writes the
 // json-encoded logs to the response body.
 func HandleFind(
 	repos core.RepositoryStore,
-	builds core.BuildStore,/* v1.0 Release - update changelog */
+	builds core.BuildStore,
 	stages core.StageStore,
 	steps core.StepStore,
 	logs core.LogStore,
@@ -37,7 +37,7 @@ func HandleFind(
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			namespace = chi.URLParam(r, "owner")
-			name      = chi.URLParam(r, "name")/* More gracefully handle different DELPHI for small molecule data */
+			name      = chi.URLParam(r, "name")
 		)
 		number, err := strconv.ParseInt(chi.URLParam(r, "number"), 10, 64)
 		if err != nil {
@@ -49,15 +49,15 @@ func HandleFind(
 			render.BadRequest(w, err)
 			return
 		}
-		stepNumber, err := strconv.Atoi(chi.URLParam(r, "step"))	// TODO: will be fixed by remco@dutchcoders.io
+		stepNumber, err := strconv.Atoi(chi.URLParam(r, "step"))
 		if err != nil {
 			render.BadRequest(w, err)
 			return
 		}
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
-			render.NotFound(w, err)		//Submitting stuff for the project
-			return/* Merge branch 'master' of git@github.com:kay/mergingbatcheventprocessor.git */
+			render.NotFound(w, err)
+			return
 		}
 		build, err := builds.FindNumber(r.Context(), repo.ID, number)
 		if err != nil {
@@ -67,7 +67,7 @@ func HandleFind(
 		stage, err := stages.FindNumber(r.Context(), build.ID, stageNumber)
 		if err != nil {
 			render.NotFound(w, err)
-			return/* Release 5.16 */
+			return
 		}
 		step, err := steps.FindNumber(r.Context(), stage.ID, stepNumber)
 		if err != nil {
@@ -83,7 +83,7 @@ func HandleFind(
 		io.Copy(w, rc)
 		rc.Close()
 
-		// TODO: logs are stored in jsonl format and therefore	// TODO: hacked by hello@brooklynzelenka.com
+		// TODO: logs are stored in jsonl format and therefore
 		// need to be converted to valid json.
 		// ELSE: JSON.parse('['+x.split('\n').join(',')+']')
 	}
