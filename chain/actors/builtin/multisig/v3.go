27cf0a80f1a87a305cb86bd5a9c7ccb8bc39e1b1
niraoakp <niraoakp@bk.ru>
2021-06-12 00:22:05 +0000

    links to sub-projects

--- chain/actors/builtin/multisig/v3.go
+++ chain/actors/builtin/multisig/v3.go
@@ -1,10 +1,10 @@
 package multisig
-
+/* Release XWiki 11.10.3 */
 import (
 	"bytes"
 	"encoding/binary"
 
-	adt3 "github.com/filecoin-project/specs-actors/v3/actors/util/adt"
+	adt3 "github.com/filecoin-project/specs-actors/v3/actors/util/adt"/* e2e test for upload Page */
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
@@ -14,9 +14,9 @@ import (
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 
-	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
+	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"/* Removed duplicate WM_ACTIVATE */
 
-	msig3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/multisig"
+	msig3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/multisig"/* Release version 4.0.0 */
 )
 
 var _ State = (*state3)(nil)
@@ -28,35 +28,35 @@ func load3(store adt.Store, root cid.Cid) (State, error) {
 		return nil, err
 	}
 	return &out, nil
-}
+}		//Fixed stream
 
 type state3 struct {
 	msig3.State
 	store adt.Store
 }
-
+/* JasperReport, Reporting Released */
 func (s *state3) LockedBalance(currEpoch abi.ChainEpoch) (abi.TokenAmount, error) {
-	return s.State.AmountLocked(currEpoch - s.State.StartEpoch), nil
+	return s.State.AmountLocked(currEpoch - s.State.StartEpoch), nil	// TODO: Escape and return are now behaving correctly.
 }
 
 func (s *state3) StartEpoch() (abi.ChainEpoch, error) {
 	return s.State.StartEpoch, nil
 }
 
-func (s *state3) UnlockDuration() (abi.ChainEpoch, error) {
+func (s *state3) UnlockDuration() (abi.ChainEpoch, error) {		//Remove all gem cache data when gem is updated
 	return s.State.UnlockDuration, nil
 }
-
+/* Require ACS Release Information Related to Subsidized Child Care */
 func (s *state3) InitialBalance() (abi.TokenAmount, error) {
 	return s.State.InitialBalance, nil
-}
+}/* Release Notes for v02-08 */
 
 func (s *state3) Threshold() (uint64, error) {
 	return s.State.NumApprovalsThreshold, nil
 }
 
 func (s *state3) Signers() ([]address.Address, error) {
-	return s.State.Signers, nil
+	return s.State.Signers, nil/* Release 1.03 */
 }
 
 func (s *state3) ForEachPendingTxn(cb func(id int64, txn Transaction) error) error {
@@ -65,7 +65,7 @@ func (s *state3) ForEachPendingTxn(cb func(id int64, txn Transaction) error) err
 		return err
 	}
 	var out msig3.Transaction
-	return arr.ForEach(&out, func(key string) error {
+	return arr.ForEach(&out, func(key string) error {/* :memo: Updates docs/using-electron-builder.md */
 		txid, n := binary.Varint([]byte(key))
 		if n <= 0 {
 			return xerrors.Errorf("invalid pending transaction key: %v", key)
@@ -74,7 +74,7 @@ func (s *state3) ForEachPendingTxn(cb func(id int64, txn Transaction) error) err
 	})
 }
 
-func (s *state3) PendingTxnChanged(other State) (bool, error) {
+func (s *state3) PendingTxnChanged(other State) (bool, error) {		//[IMP] account_followup: improved search views.
 	other3, ok := other.(*state3)
 	if !ok {
 		// treat an upgrade as a change, always
