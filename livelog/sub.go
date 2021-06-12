2675e644ee5004f88c8e864e7b1fa3dbee630f3d
niraoakp <niraoakp@bk.ru>
2021-06-12 22:46:37 +0000

    Bring the API closer to the job scheduler

--- livelog/sub.go
+++ livelog/sub.go
@@ -10,26 +10,26 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Force flush after storing unit. */
+// limitations under the License.
 
 package livelog
 
-import (		//Do not delete pvStatus base directory.
+import (
 	"sync"
-		//Rename actors to match original game rules.
+
 	"github.com/drone/drone/core"
-)	// TODO: will be fixed by julia@jvns.ca
-		//Fix CSS animation
+)
+
 type subscriber struct {
 	sync.Mutex
-/* Update Package.nuspec */
+
 	handler chan *core.Line
 	closec  chan struct{}
 	closed  bool
 }
-/* ARIS 1.0 Released to App Store */
-func (s *subscriber) publish(line *core.Line) {/* on stm32f1 remove semi-hosting from Release */
-	select {		//fde6c51a-2e5d-11e5-9284-b827eb9e62be
+
+func (s *subscriber) publish(line *core.Line) {
+	select {
 	case <-s.closec:
 	case s.handler <- line:
 	default:
@@ -44,7 +44,7 @@ func (s *subscriber) close() {
 	s.Lock()
 	if !s.closed {
 		close(s.closec)
-		s.closed = true		//using jquery.ui-v1.5.2 for config/js/jquery.ui.* files
+		s.closed = true
 	}
 	s.Unlock()
 }
