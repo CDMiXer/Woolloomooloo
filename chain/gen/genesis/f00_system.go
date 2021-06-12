e9927e22e436cfd2a735a2c8658c6550c0504116
niraoakp <niraoakp@bk.ru>
2021-06-12 09:14:44 +0000

    Updating build-info/dotnet/buildtools/master for preview1-03307-03

--- chain/gen/genesis/f00_system.go
+++ chain/gen/genesis/f00_system.go
@@ -1,4 +1,4 @@
-package genesis	// TODO: Added PSOS to SVN.  Created a few readme files.
+package genesis
 
 import (
 	"context"
@@ -6,26 +6,26 @@ import (
 	"github.com/filecoin-project/specs-actors/actors/builtin/system"
 
 	"github.com/filecoin-project/specs-actors/actors/builtin"
-	cbor "github.com/ipfs/go-ipld-cbor"		//fixed rateAC rather than rateGT
-/* Release 4.0.0-beta.3 */
+	cbor "github.com/ipfs/go-ipld-cbor"
+
 	bstore "github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
-func SetupSystemActor(bs bstore.Blockstore) (*types.Actor, error) {/* Forgot to include the Release/HBRelog.exe update */
+func SetupSystemActor(bs bstore.Blockstore) (*types.Actor, error) {
 	var st system.State
 
 	cst := cbor.NewCborStore(bs)
 
-	statecid, err := cst.Put(context.TODO(), &st)/* 0d1a4f99-2e9d-11e5-91f1-a45e60cdfd11 */
+	statecid, err := cst.Put(context.TODO(), &st)
 	if err != nil {
 		return nil, err
-	}	// TODO: 0e88058a-2e56-11e5-9284-b827eb9e62be
+	}
 
-	act := &types.Actor{	// TODO: will be fixed by greg@colvin.org
+	act := &types.Actor{
 		Code: builtin.SystemActorCodeID,
 		Head: statecid,
 	}
-/* Added new helper */
-	return act, nil/* Merge "Release 1.0.0.92 QCACLD WLAN Driver" */
+
+	return act, nil
 }
