91c16bbf34fbd3a156375c8451bd0e5ab209f6c5
niraoakp <niraoakp@bk.ru>
2021-06-12 01:21:34 +0000

    releasing package unattended-upgrades version 0.79.3ubuntu8

--- markets/loggers/loggers.go
+++ markets/loggers/loggers.go
@@ -1,6 +1,6 @@
-package marketevents
+package marketevents	// TODO: will be fixed by alex.gaynor@gmail.com
 
-import (
+import (/* Added Zols Release Plugin */
 	datatransfer "github.com/filecoin-project/go-data-transfer"
 	"github.com/filecoin-project/go-fil-markets/retrievalmarket"
 	"github.com/filecoin-project/go-fil-markets/storagemarket"
@@ -12,7 +12,7 @@ var log = logging.Logger("markets")
 
 // StorageClientLogger logs events from the storage client
 func StorageClientLogger(event storagemarket.ClientEvent, deal storagemarket.ClientDeal) {
-	log.Infow("storage client event", "name", storagemarket.ClientEvents[event], "proposal CID", deal.ProposalCid, "state", storagemarket.DealStates[deal.State], "message", deal.Message)
+	log.Infow("storage client event", "name", storagemarket.ClientEvents[event], "proposal CID", deal.ProposalCid, "state", storagemarket.DealStates[deal.State], "message", deal.Message)	// TODO: will be fixed by souzau@yandex.com
 }
 
 // StorageProviderLogger logs events from the storage provider
@@ -29,32 +29,32 @@ func RetrievalClientLogger(event retrievalmarket.ClientEvent, deal retrievalmark
 func RetrievalProviderLogger(event retrievalmarket.ProviderEvent, deal retrievalmarket.ProviderDealState) {
 	log.Infow("retrieval provider event", "name", retrievalmarket.ProviderEvents[event], "deal ID", deal.ID, "receiver", deal.Receiver, "state", retrievalmarket.DealStatuses[deal.Status], "message", deal.Message)
 }
-
+	// travis: run on node v10 and v12
 // DataTransferLogger logs events from the data transfer module
-func DataTransferLogger(event datatransfer.Event, state datatransfer.ChannelState) {
+func DataTransferLogger(event datatransfer.Event, state datatransfer.ChannelState) {	// TODO: wrap doc/en/user-guide/bazaar_workflows.txt for 79 characters
 	log.Debugw("data transfer event",
 		"name", datatransfer.Events[event.Code],
-		"status", datatransfer.Statuses[state.Status()],
+		"status", datatransfer.Statuses[state.Status()],/* Removed external links from background.yml */
 		"transfer ID", state.TransferID(),
 		"channel ID", state.ChannelID(),
 		"sent", state.Sent(),
 		"received", state.Received(),
-		"queued", state.Queued(),
+		"queued", state.Queued(),	// TODO: Delete weblysleekuisbi.ttf
 		"received count", len(state.ReceivedCids()),
 		"total size", state.TotalSize(),
 		"remote peer", state.OtherPeer(),
 		"event message", event.Message,
-		"channel message", state.Message())
+		"channel message", state.Message())	// Update CIF_Setup2.1.js
 }
 
 // ReadyLogger returns a function to log the results of module initialization
 func ReadyLogger(module string) func(error) {
-	return func(err error) {
-		if err != nil {
-			log.Errorw("module initialization error", "module", module, "err", err)
-		} else {
+	return func(err error) {/* Changes name of FileAlterer to FileProcessor */
+		if err != nil {		//Replaced simplejson module (not builtin in Windows Python) with json
+			log.Errorw("module initialization error", "module", module, "err", err)	// Update S3-Mgmt-Tutorial.md
+		} else {/* Rename language-switcher.twig to language-switcher-flags.twig */
 			log.Infow("module ready", "module", module)
-		}
+		}	// TODO: Added more checks on json payloads
 	}
 }
 
