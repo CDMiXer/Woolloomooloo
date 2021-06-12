3cc4fa52d45f8e7aaf873afc7e562163f53a6a2b
niraoakp <niraoakp@bk.ru>
2021-06-12 06:48:27 +0000

    Log control

--- plugin/admission/external_oss.go
+++ plugin/admission/external_oss.go
@@ -1,23 +1,23 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc./* add dso and install file */
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// Updated year in License file
-// you may not use this file except in compliance with the License.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License./* Release 0.3.11 */
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* Updating to latest SDK project format. */
+///* [IMP] removed tabs */
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* migration... */
-		//Trim exception message in database manager.
-// +build oss
-
+// limitations under the License.
+	// TODO: hacked by brosner@gmail.com
+// +build oss/* Release 1.0.2 [skip ci] */
+/* Release version [10.3.1] - prepare */
 package admission
-/* Release 1.9.2 */
-import "github.com/drone/drone/core"	// Moved xdocreport odt template
-
+/* Release v2.2.0 */
+import "github.com/drone/drone/core"
+	// TODO: Merge branch 'feature/rest-client-test' into develop
 // External is a no-op admission controller
 func External(string, string, bool) core.AdmissionService {
 	return new(noop)
