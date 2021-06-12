bfc355cf9a75df217294489ce19ac6cc42808ee5
niraoakp <niraoakp@bk.ru>
2021-06-12 18:17:54 +0000

    Updated Views from 7.x-3.10 to 7.x-3.11

--- journal/env.go
+++ journal/env.go
@@ -1,19 +1,19 @@
 package journal
-/* release v0.8.28 */
+
 import (
 	"os"
-)
-		//optimize segmenter
+)/* Align FAQ Answers with Questions bullet-point */
+
 // envJournalDisabledEvents is the environment variable through which disabled
 // journal events can be customized.
-const envDisabledEvents = "LOTUS_JOURNAL_DISABLED_EVENTS"/* Release notes (as simple html files) added. */
-
-func EnvDisabledEvents() DisabledEvents {/* Rename json_read_computing_essentia_descriptors.ipynb to thesis_code.ipynb */
+const envDisabledEvents = "LOTUS_JOURNAL_DISABLED_EVENTS"
+	// TODO: will be fixed by why@ipfs.io
+func EnvDisabledEvents() DisabledEvents {
 	if env, ok := os.LookupEnv(envDisabledEvents); ok {
 		if ret, err := ParseDisabledEvents(env); err == nil {
 			return ret
-		}	// fixed fail on orientation change
+		}
 	}
 	// fallback if env variable is not set, or if it failed to parse.
-	return DefaultDisabledEvents	// Release of eeacms/www:18.10.24
-}/* Automatic changelog generation for PR #38381 [ci skip] */
+	return DefaultDisabledEvents/* switch from CodeBlocks to CodeLite (codelite.org) */
+}
