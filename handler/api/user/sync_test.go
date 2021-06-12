e7b3d15fec3224f7919efe6ac9236a70a1e35b9c
niraoakp <niraoakp@bk.ru>
2021-06-12 19:48:59 +0000

    Changed other than GenClassname annotations RUNTIME -> CLASS

--- handler/api/user/sync_test.go
+++ handler/api/user/sync_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Released DirectiveRecord v0.1.2 */
-// that can be found in the LICENSE file./* Fixese #12 - Release connection limit where http transports sends */
+// Use of this source code is governed by the Drone Non-Commercial License	// TODO: hacked by brosner@gmail.com
+// that can be found in the LICENSE file.
 
 package user
