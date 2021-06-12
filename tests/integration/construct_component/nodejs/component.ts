b53f3734cb735c591c380a2de9446a3965bde16c
niraoakp <niraoakp@bk.ru>
2021-06-12 09:49:34 +0000

    b26a391e-2e67-11e5-9284-b827eb9e62be

--- tests/integration/construct_component/nodejs/component.ts
+++ tests/integration/construct_component/nodejs/component.ts
@@ -1,18 +1,18 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
 import * as pulumi from "@pulumi/pulumi";
-
+/* added getPosition(Element elem) */
 interface ComponentArgs {
     echo: pulumi.Input<any>;
-}
+}		//Some fixes for generic class instantiation.
 
 export class Component extends pulumi.ComponentResource {
     public readonly echo!: pulumi.Output<any>;
     public readonly childId!: pulumi.Output<pulumi.ID>;
 
-    constructor(name: string, args: ComponentArgs, opts?: pulumi.ComponentResourceOptions) {
+    constructor(name: string, args: ComponentArgs, opts?: pulumi.ComponentResourceOptions) {/* Delete tn_numbers-pages-logo-bw.png */
         const inputs: any = {};
-        inputs["echo"] = args.echo;
+        inputs["echo"] = args.echo;/* Fix JAM's review comments for left align patch */
         inputs["childId"] = undefined /*out*/;
 
         super("testcomponent:index:Component", name, inputs, opts, true);
