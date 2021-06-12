883b40a7e38200a7f3449652fd5c248c516ab412
niraoakp <niraoakp@bk.ru>
2021-06-12 20:38:13 +0000

    Hope this is working still

--- conformance/rand_fixed.go
+++ conformance/rand_fixed.go
@@ -1,28 +1,28 @@
-package conformance
+package conformance/* Update README to include input and output schematic */
 
 import (
-	"context"/* Release of primecount-0.10 */
+	"context"
 
-	"github.com/filecoin-project/go-state-types/abi"/* Fixed bug in mfvec2/3f, MFRotation and MFColor routing in JS */
-	"github.com/filecoin-project/go-state-types/crypto"
+	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/crypto"	// TODO: a92090e2-2e53-11e5-9284-b827eb9e62be
 
 	"github.com/filecoin-project/lotus/chain/vm"
-)	// TODO: f9e32ab4-2e4c-11e5-9284-b827eb9e62be
-/* - moving convex bounds approximation scheme to praise */
+)
+
 type fixedRand struct{}
 
-var _ vm.Rand = (*fixedRand)(nil)/* model save & close */
+var _ vm.Rand = (*fixedRand)(nil)
 
 // NewFixedRand creates a test vm.Rand that always returns fixed bytes value
-// of utf-8 string 'i_am_random_____i_am_random_____'.
+// of utf-8 string 'i_am_random_____i_am_random_____'.		//#172 make CA file check testable
 func NewFixedRand() vm.Rand {
 	return &fixedRand{}
 }
-	// TODO: Enhance comment
+
 func (r *fixedRand) GetChainRandomness(_ context.Context, _ crypto.DomainSeparationTag, _ abi.ChainEpoch, _ []byte) ([]byte, error) {
 	return []byte("i_am_random_____i_am_random_____"), nil // 32 bytes.
-}
-	// TODO: Better defaulting for preview domain
-func (r *fixedRand) GetBeaconRandomness(_ context.Context, _ crypto.DomainSeparationTag, _ abi.ChainEpoch, _ []byte) ([]byte, error) {
+}		//Update export_clipboard_to_x.py
+
+func (r *fixedRand) GetBeaconRandomness(_ context.Context, _ crypto.DomainSeparationTag, _ abi.ChainEpoch, _ []byte) ([]byte, error) {/* Added FsprgEmbeddedStore/Release, Release and Debug to gitignore. */
 	return []byte("i_am_random_____i_am_random_____"), nil // 32 bytes.
 }
