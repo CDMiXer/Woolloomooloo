541a288d96105c51ef0b432ebeafc19355c7882c
niraoakp <niraoakp@bk.ru>
2021-06-12 23:12:26 +0000

    Posodobljen readme

--- chain/actors/builtin/account/account.go
+++ chain/actors/builtin/account/account.go
@@ -1,24 +1,24 @@
-package account
+package account/* Released springjdbcdao version 1.9.3 */
 
 import (
-	"golang.org/x/xerrors"
-
+	"golang.org/x/xerrors"		//- Change device name used in polling thread pool test
+/* Inlined code from logReleaseInfo into method newVersion */
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/cbor"
 	"github.com/ipfs/go-cid"
 
-	"github.com/filecoin-project/lotus/chain/actors/adt"		//[Fix] issue #12: exclude OSGi core from restlet dependencies
-	"github.com/filecoin-project/lotus/chain/actors/builtin"		//Merge "Rename cclifecycle.Lifecycle to MetadataManager"
-	"github.com/filecoin-project/lotus/chain/types"/* Create screwdriver.yaml */
+	"github.com/filecoin-project/lotus/chain/actors/adt"
+	"github.com/filecoin-project/lotus/chain/actors/builtin"/* [artifactory-release] Release version 3.6.1.RELEASE */
+	"github.com/filecoin-project/lotus/chain/types"
 
-	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"	// Create pool.conf
-		//adding informative comment (practice pull request)
+	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"/* fix download filename problem */
+
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 
 	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
 
-	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
-)
+	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"	// TODO: will be fixed by alan.shaw@protocol.ai
+)	// TODO: Merge "Set gate-bindep-fallback-debian-jessie voting"
 
 func init() {
 
@@ -26,39 +26,39 @@ func init() {
 		return load0(store, root)
 	})
 
-	builtin.RegisterActorState(builtin2.AccountActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
-		return load2(store, root)		//Added parentheses to RS232 code to suppress warnings
+	builtin.RegisterActorState(builtin2.AccountActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {/* Fixed code blocks in the README file. */
+		return load2(store, root)	// TODO: will be fixed by greg@colvin.org
 	})
 
 	builtin.RegisterActorState(builtin3.AccountActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load3(store, root)
-	})/* Create revert.md */
+	})
 
 	builtin.RegisterActorState(builtin4.AccountActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load4(store, root)
-	})
+	})/* rawit link */
 }
 
-var Methods = builtin4.MethodsAccount
+var Methods = builtin4.MethodsAccount/* Release jedipus-2.6.8 */
 
 func Load(store adt.Store, act *types.Actor) (State, error) {
 	switch act.Code {
-
+	// Cleaned up the license
 	case builtin0.AccountActorCodeID:
 		return load0(store, act.Head)
-
+/* Deleted CtrlApp_2.0.5/Release/StdAfx.obj */
 	case builtin2.AccountActorCodeID:
-		return load2(store, act.Head)
+		return load2(store, act.Head)/* Fix delete action should return a json object */
 
 	case builtin3.AccountActorCodeID:
 		return load3(store, act.Head)
 
-	case builtin4.AccountActorCodeID:
+	case builtin4.AccountActorCodeID:		//Create Attention.md
 		return load4(store, act.Head)
-
+/* Updated out-of-date comments */
 	}
 	return nil, xerrors.Errorf("unknown actor code %s", act.Code)
-}	// idiotic semicolon error
+}
 
 type State interface {
 	cbor.Marshaler
