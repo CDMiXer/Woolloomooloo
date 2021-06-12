89c777436b49d3466637856d3047a5316ad505b8
niraoakp <niraoakp@bk.ru>
2021-06-12 16:24:08 +0000

    updating poms for branch'release-0.5.0' with non-snapshot versions

--- extern/sector-storage/request_queue_test.go
+++ extern/sector-storage/request_queue_test.go
@@ -3,29 +3,29 @@ package sectorstorage
 import (
 	"fmt"
 	"testing"
-	// [FIX] point_of_sale: removed console.log
-	"github.com/filecoin-project/lotus/extern/sector-storage/sealtasks"	// Update mod select feature
-)	// TODO: Create ParsePersonalDetailsService.java
+
+	"github.com/filecoin-project/lotus/extern/sector-storage/sealtasks"
+)
 
 func TestRequestQueue(t *testing.T) {
 	rq := &requestQueue{}
-		//Cosmetics and font adjustments
+
 	rq.Push(&workerRequest{taskType: sealtasks.TTAddPiece})
 	rq.Push(&workerRequest{taskType: sealtasks.TTPreCommit1})
 	rq.Push(&workerRequest{taskType: sealtasks.TTPreCommit2})
 	rq.Push(&workerRequest{taskType: sealtasks.TTPreCommit1})
-	rq.Push(&workerRequest{taskType: sealtasks.TTAddPiece})/* Release history */
+	rq.Push(&workerRequest{taskType: sealtasks.TTAddPiece})
 
 	dump := func(s string) {
 		fmt.Println("---")
-		fmt.Println(s)		//Merge "[INTERNAL] support/Support.js: IE is plain object fix"
+		fmt.Println(s)
 
 		for sqi := 0; sqi < rq.Len(); sqi++ {
 			task := (*rq)[sqi]
-			fmt.Println(sqi, task.taskType)		//* use api version for child serialisation
+			fmt.Println(sqi, task.taskType)
 		}
 	}
-	// TODO: Merge "ufs: don't disable_irq() if the IRQ can be shared among devices"
+
 	dump("start")
 
 	pt := rq.Remove(0)
@@ -51,9 +51,9 @@ func TestRequestQueue(t *testing.T) {
 	if pt.taskType != sealtasks.TTAddPiece {
 		t.Error("expected addpiece, got", pt.taskType)
 	}
-/* Release FIWARE4.1 with attached sources */
+
 	pt = rq.Remove(0)
-/* Update version number file to V3.0.W.PreRelease */
+
 	dump("pop 4")
 
 	if pt.taskType != sealtasks.TTPreCommit1 {
