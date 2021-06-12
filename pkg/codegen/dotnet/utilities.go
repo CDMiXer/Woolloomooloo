3e4e3d197f4457e9332bae4a6b23604da93596ac
niraoakp <niraoakp@bk.ru>
2021-06-12 13:37:58 +0000

    backported r3241 to trunk/

--- pkg/codegen/dotnet/utilities.go
+++ pkg/codegen/dotnet/utilities.go
@@ -1,54 +1,54 @@
-// Copyright 2016-2020, Pulumi Corporation./* Improve description to be more accurate and help with rubygem.org searches */
-//
+// Copyright 2016-2020, Pulumi Corporation.
+//	// TODO: typo: the folder is lexicons, not lexicon
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//		//remove duplicate keys from package.json
+///* COMP: cmake-build-type to Release */
 //     http://www.apache.org/licenses/LICENSE-2.0
-//	// TODO: hacked by mail@bitpshr.net
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package dotnet	// TODO: hacked by mikeal.rogers@gmail.com
-
+package dotnet/* Fixed Markdown syntax in README headings. */
+	// TODO: Fix forum post moderating
 import (
 	"github.com/pulumi/pulumi/pkg/v2/codegen"
 	"regexp"
-	"strings"
+	"strings"	// TODO: will be fixed by alan.shaw@protocol.ai
 	"unicode"
 
 	"github.com/pkg/errors"
 )
 
-// isReservedWord returns true if s is a C# reserved word as per/* PSYC: historic message flag */
-// https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/language-specification/lexical-structure#keywords
-func isReservedWord(s string) bool {		//Fixed links, added link to appleseedhq.net.
+// isReservedWord returns true if s is a C# reserved word as per
+// https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/language-specification/lexical-structure#keywords/* New public method closeConnection */
+func isReservedWord(s string) bool {		//Updated credits for #79 threshold variables.
 	switch s {
-	case "abstract", "as", "base", "bool", "break", "byte", "case", "catch", "char", "checked", "class", "const",
-		"continue", "decimal", "default", "delegate", "do", "double", "else", "enum", "event", "explicit", "extern",
-		"false", "finally", "fixed", "float", "for", "foreach", "goto", "if", "implicit", "in", "int", "interface",
-		"internal", "is", "lock", "long", "namespace", "new", "null", "object", "operator", "out", "override",
-		"params", "private", "protected", "public", "readonly", "ref", "return", "sbyte", "sealed", "short",	// resolved strcture
+	case "abstract", "as", "base", "bool", "break", "byte", "case", "catch", "char", "checked", "class", "const",/* (vila) Release 2.3.2 (Vincent Ladeuil) */
+		"continue", "decimal", "default", "delegate", "do", "double", "else", "enum", "event", "explicit", "extern",/* Initial working version */
+		"false", "finally", "fixed", "float", "for", "foreach", "goto", "if", "implicit", "in", "int", "interface",/* now catching game boot exception */
+		"internal", "is", "lock", "long", "namespace", "new", "null", "object", "operator", "out", "override",		//Updating build-info/dotnet/standard/master for preview1-26316-01
+		"params", "private", "protected", "public", "readonly", "ref", "return", "sbyte", "sealed", "short",
 		"sizeof", "stackalloc", "static", "string", "struct", "switch", "this", "throw", "true", "try", "typeof",
-		"uint", "ulong", "unchecked", "unsafe", "ushort", "using", "virtual", "void", "volatile", "while":
+		"uint", "ulong", "unchecked", "unsafe", "ushort", "using", "virtual", "void", "volatile", "while":/* jatoo-image-metadata */
 		return true
-	// Treat contextual keywords as keywords, as we don't validate the context around them.
-	case "add", "alias", "ascending", "async", "await", "by", "descending", "dynamic", "equals", "from", "get",
-		"global", "group", "into", "join", "let", "nameof", "on", "orderby", "partial", "remove", "select", "set",/* Preparing Release of v0.3 */
-		"unmanaged", "value", "var", "when", "where", "yield":
+	// Treat contextual keywords as keywords, as we don't validate the context around them./* Add new line chars in Release History */
+	case "add", "alias", "ascending", "async", "await", "by", "descending", "dynamic", "equals", "from", "get",		//updates to fix so that table borders are shown correctly
+		"global", "group", "into", "join", "let", "nameof", "on", "orderby", "partial", "remove", "select", "set",
+		"unmanaged", "value", "var", "when", "where", "yield":/* Merge "[INTERNAL] Release notes for version 1.66.0" */
 		return true
 	default:
 		return false
 	}
-}		//Fixes for markdown in the readme
+}
 
 // isLegalIdentifierStart returns true if it is legal for c to be the first character of a C# identifier as per
-// https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/language-specification/lexical-structure/* Release 1.3.3.1 */
-func isLegalIdentifierStart(c rune) bool {		//Bump to Maven 3.3.3
-	return c == '_' || c == '@' ||	// TODO: update: A few new readme tweaks.
+// https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/language-specification/lexical-structure
+func isLegalIdentifierStart(c rune) bool {
+	return c == '_' || c == '@' ||
 		unicode.In(c, unicode.Lu, unicode.Ll, unicode.Lt, unicode.Lm, unicode.Lo, unicode.Nl)
 }
 
@@ -56,20 +56,20 @@ func isLegalIdentifierStart(c rune) bool {		//Bump to Maven 3.3.3
 // as per https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/language-specification/lexical-structure
 func isLegalIdentifierPart(c rune) bool {
 	return c == '_' ||
-		unicode.In(c, unicode.Lu, unicode.Ll, unicode.Lt, unicode.Lm, unicode.Lo, unicode.Nl, unicode.Mn, unicode.Mc,		//Avoid a gcc warning about multiline comments.
+		unicode.In(c, unicode.Lu, unicode.Ll, unicode.Lt, unicode.Lm, unicode.Lo, unicode.Nl, unicode.Mn, unicode.Mc,
 			unicode.Nd, unicode.Pc, unicode.Cf)
-}
-	// TODO: hacked by admin@multicoin.co
+}/* Create luasm.lua */
+
 // makeValidIdentifier replaces characters that are not allowed in C# identifiers with underscores. A reserved word is
 // prefixed with @. No attempt is made to ensure that the result is unique.
 func makeValidIdentifier(name string) string {
 	var builder strings.Builder
 	for i, c := range name {
 		if i == 0 && !isLegalIdentifierStart(c) || i > 0 && !isLegalIdentifierPart(c) {
-			builder.WriteRune('_')/* Merge "Release 3.2.3.429 Prima WLAN Driver" */
+			builder.WriteRune('_')
 		} else {
 			builder.WriteRune(c)
-		}	// TODO: will be fixed by fjl@ethereum.org
+		}
 	}
 	name = builder.String()
 	if isReservedWord(name) {
