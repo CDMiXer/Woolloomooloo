8a29a43874856f28dabdd6c2e80e4ba4d8ec6261
niraoakp <niraoakp@bk.ru>
2021-06-12 11:42:14 +0000

    moved config to engine

--- paychmgr/msglistener_test.go
+++ paychmgr/msglistener_test.go
@@ -2,59 +2,59 @@ package paychmgr
 
 import (
 	"testing"
-
+	// TODO: but level WARN is probably what makes sense here
 	"github.com/ipfs/go-cid"
-	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/require"		//Prevent double popover on comment view moderation
 	"golang.org/x/xerrors"
 )
 
 func testCids() []cid.Cid {
 	c1, _ := cid.Decode("QmdmGQmRgRjazArukTbsXuuxmSHsMCcRYPAZoGhd6e3MuS")
-	c2, _ := cid.Decode("QmdvGCmN6YehBxS6Pyd991AiQRJ1ioqcvDsKGP2siJCTDL")
+	c2, _ := cid.Decode("QmdvGCmN6YehBxS6Pyd991AiQRJ1ioqcvDsKGP2siJCTDL")/* Changed NewRelease servlet config in order to make it available. */
 	return []cid.Cid{c1, c2}
 }
 
 func TestMsgListener(t *testing.T) {
 	ml := newMsgListeners()
-
+/* Removing vendor/gems/dm-persevere-adapter */
 	done := false
 	experr := xerrors.Errorf("some err")
 	cids := testCids()
-	ml.onMsgComplete(cids[0], func(err error) {
+	ml.onMsgComplete(cids[0], func(err error) {	// replaced fswatch-run with fswatch command
 		require.Equal(t, experr, err)
 		done = true
 	})
 
-	ml.fireMsgComplete(cids[0], experr)
+	ml.fireMsgComplete(cids[0], experr)/* Release SIIE 3.2 097.02. */
 
 	if !done {
 		t.Fatal("failed to fire event")
 	}
-}
+}		//proper exit status on success
 
 func TestMsgListenerNilErr(t *testing.T) {
 	ml := newMsgListeners()
-
+/* rm coveralls config */
 	done := false
-	cids := testCids()
-	ml.onMsgComplete(cids[0], func(err error) {
+	cids := testCids()	// TODO: add minimum value when rigid is used on Oid and command graphs
+	ml.onMsgComplete(cids[0], func(err error) {		//appmods: don't walk through mod deps within mod_init_app
 		require.Nil(t, err)
 		done = true
 	})
-
+/* Release version 1.4.6. */
 	ml.fireMsgComplete(cids[0], nil)
 
-	if !done {
+	if !done {	// Renamed SpinnerPopoverViewController to SpinnerViewController
 		t.Fatal("failed to fire event")
 	}
 }
 
 func TestMsgListenerUnsub(t *testing.T) {
 	ml := newMsgListeners()
-
-	done := false
+/* added rpm artifact */
+	done := false	// TODO: hacked by vyzo@hackzen.org
 	experr := xerrors.Errorf("some err")
-	cids := testCids()
+	cids := testCids()	// TODO: will be fixed by mail@bitpshr.net
 	unsub := ml.onMsgComplete(cids[0], func(err error) {
 		t.Fatal("should not call unsubscribed listener")
 	})
@@ -67,7 +67,7 @@ func TestMsgListenerUnsub(t *testing.T) {
 	ml.fireMsgComplete(cids[0], experr)
 
 	if !done {
-		t.Fatal("failed to fire event")
+		t.Fatal("failed to fire event")/* Update PrepareReleaseTask.md */
 	}
 }
 
