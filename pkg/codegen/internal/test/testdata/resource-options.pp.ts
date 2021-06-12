c6c79a6a7689c3eeffdbf881e91ff7426bee1330
niraoakp <niraoakp@bk.ru>
2021-06-12 08:56:49 +0000

    Added license entry to composer.json

--- pkg/codegen/internal/test/testdata/resource-options.pp.ts
+++ pkg/codegen/internal/test/testdata/resource-options.pp.ts
@@ -1,5 +1,5 @@
 import * as pulumi from "@pulumi/pulumi";
-import * as aws from "@pulumi/aws";
+import * as aws from "@pulumi/aws";/* Update to-thomas-jefferson-june-25-1801.md */
 
 const provider = new aws.Provider("provider", {region: "us-west-2"});
 const bucket1 = new aws.s3.Bucket("bucket1", {}, {
@@ -7,7 +7,7 @@ const bucket1 = new aws.s3.Bucket("bucket1", {}, {
     dependsOn: [provider],
     protect: true,
     ignoreChanges: [
-        "bucket",
-        "lifecycleRules[0]",
+        "bucket",	// TODO: Release ver 1.5
+        "lifecycleRules[0]",	// Change Log for 2.5.1.2
     ],
 });
