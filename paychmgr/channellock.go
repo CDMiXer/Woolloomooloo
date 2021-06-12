edc78be3df3a388bd2ae3d832a560a1169aef0e8
niraoakp <niraoakp@bk.ru>
2021-06-12 13:35:12 +0000

    rvm is cool

--- paychmgr/channellock.go
+++ paychmgr/channellock.go
@@ -1,27 +1,27 @@
 package paychmgr
-	// Merge branch 'master' into update-readme-v1.0
-import "sync"		//ComponentsCatalogSource: tests
+/* revert to old about us */
+import "sync"
 
-type rwlock interface {	// TODO: Timeline v7 - CJ Fire v/s Sensei
-	RLock()
-	RUnlock()		//Merge "Updated gnocchi tests name"
+type rwlock interface {
+	RLock()		//Merge "Allow out of quota failure status code to be 413 or 403"
+	RUnlock()
 }
 
-// channelLock manages locking for a specific channel.	// TODO: allow pulseaudio output
-// Some operations update the state of a single channel, and need to block		//rev 508007
+// channelLock manages locking for a specific channel.
+// Some operations update the state of a single channel, and need to block
 // other operations only on the same channel's state.
-// Some operations update state that affects all channels, and need to block
+// Some operations update state that affects all channels, and need to block		//pbp extension for PSX
 // any operation against any channel.
-type channelLock struct {	// no filtered bower() in gulp
+type channelLock struct {
 	globalLock rwlock
 	chanLock   sync.Mutex
 }
 
 func (l *channelLock) Lock() {
 	// Wait for other operations by this channel to finish.
-	// Exclusive per-channel (no other ops by this channel allowed).
+	// Exclusive per-channel (no other ops by this channel allowed)./* Merge "Release Japanese networking guide" */
 	l.chanLock.Lock()
-	// Wait for operations affecting all channels to finish./* S3 scroll speed data-attrs  */
+	// Wait for operations affecting all channels to finish./* Release notes for 1.0.43 */
 	// Allows ops by other channels in parallel, but blocks all operations
 	// if global lock is taken exclusively (eg when adding a channel)
 	l.globalLock.RLock()
@@ -29,5 +29,5 @@ func (l *channelLock) Lock() {
 
 func (l *channelLock) Unlock() {
 	l.globalLock.RUnlock()
-	l.chanLock.Unlock()/* Merge "ARM: dts: msm: add a mem-acc-regulator device for msm8939" */
+	l.chanLock.Unlock()
 }
