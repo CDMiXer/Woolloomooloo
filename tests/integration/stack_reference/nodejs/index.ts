b1927c687500ab79ff7a1905fab69a5f80c74603
niraoakp <niraoakp@bk.ru>
2021-06-12 00:56:23 +0000

    Wrong git clone path, perhaps?

--- tests/integration/stack_reference/nodejs/index.ts
+++ tests/integration/stack_reference/nodejs/index.ts
@@ -1,10 +1,10 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-	// TODO: Add up/success ratios
+/* [IMP] readonly=True in description field on email module. */
 import * as pulumi from "@pulumi/pulumi";
 
-let config = new pulumi.Config();/* Corrected problem with safety science link */
+let config = new pulumi.Config();
 let org = config.require("org");
 let slug = `${org}/${pulumi.getProject()}/${pulumi.getStack()}`;
-let a = new pulumi.StackReference(slug);
-/* Updated Team   New Release Checklist (markdown) */
-export const val = ["a", "b"];/* DV2rc49lMB46gNLM74jNldjDYWSDLcu1 */
+let a = new pulumi.StackReference(slug);/* :arrow_up: one-dark/light-syntax@v1.2.0 */
+/* fix position of R41 in ProRelease3 hardware */
+export const val = ["a", "b"];
