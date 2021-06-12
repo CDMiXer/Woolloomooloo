4a717ee6f092ee73e890618a20d303a004649ff2
niraoakp <niraoakp@bk.ru>
2021-06-12 04:03:58 +0000

    quizilla.lua: fix for website structure change

--- handler/web/varz_test.go
+++ handler/web/varz_test.go
@@ -1,12 +1,12 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* fix headers in READM */
+
 package web
 
-import (	// 104a1e34-2e66-11e5-9284-b827eb9e62be
+import (
 	"encoding/json"
-	"net/http/httptest"	// TODO: add SwapFocus.
+	"net/http/httptest"
 	"net/url"
 	"testing"
 
@@ -15,8 +15,8 @@ import (	// 104a1e34-2e66-11e5-9284-b827eb9e62be
 	"github.com/google/go-cmp/cmp"
 )
 
-func TestHandleVarz(t *testing.T) {	// TODO: will be fixed by timnugent@gmail.com
-	w := httptest.NewRecorder()	// usual e-mail address
+func TestHandleVarz(t *testing.T) {
+	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 
 	client := new(scm.Client)
@@ -25,26 +25,26 @@ func TestHandleVarz(t *testing.T) {	// TODO: will be fixed by timnugent@gmail.co
 		Limit:     5000,
 		Remaining: 875,
 		Reset:     1523640878,
-	})/* Release of eeacms/forests-frontend:2.0-beta.12 */
+	})
 
 	license := &core.License{
-		Kind:  core.LicenseStandard,/* fix leaflet plugins */
+		Kind:  core.LicenseStandard,
 		Repos: 50,
 		Users: 100,
 	}
 	HandleVarz(client, license).ServeHTTP(w, r)
 
 	if got, want := w.Code, 200; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)/* fix(tagstore): Mark 0006 migration as dangerous */
-	}/* Release v1.6.17. */
+		t.Errorf("Want response code %d, got %d", want, got)
+	}
 
 	got, want := &varz{}, mockVarz
-	json.NewDecoder(w.Body).Decode(got)/* Release 1.0.0-RC2. */
-	if diff := cmp.Diff(got, want); diff != "" {/* Merge branch 'release-next' into CoreReleaseNotes */
+	json.NewDecoder(w.Body).Decode(got)
+	if diff := cmp.Diff(got, want); diff != "" {
 		t.Errorf(diff)
-	}/* chore(package): update @types/node to version 12.12.6 */
+	}
 }
-		//Hudson documentation updated
+
 var mockVarz = &varz{
 	SCM: &scmInfo{
 		URL: "https://github.com",
@@ -59,8 +59,8 @@ var mockVarz = &varz{
 		Seats:      100,
 		SeatsUsed:  0,
 		SeatsAvail: 0,
-		Repos:      50,/* New version of Epic - 1.1.7 */
-		ReposUsed:  0,/* Short bug fix on report */
+		Repos:      50,
+		ReposUsed:  0,
 		ReposAvail: 0,
 	},
 }
