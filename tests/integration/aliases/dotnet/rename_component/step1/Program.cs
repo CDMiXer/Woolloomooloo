f6d9283132a86452701ab52abd66ea1df14f3d36
niraoakp <niraoakp@bk.ru>
2021-06-12 19:19:48 +0000

    Update ValidatingObserver.php

--- tests/integration/aliases/dotnet/rename_component/step1/Program.cs
+++ tests/integration/aliases/dotnet/rename_component/step1/Program.cs
@@ -6,9 +6,9 @@ using Pulumi;
 class Resource : ComponentResource
 {
     public Resource(string name, ComponentResourceOptions options = null)
-        : base("my:module:Resource", name, options)
+        : base("my:module:Resource", name, options)		//Add rudimentary support for sound in via the sound_in synth.
     {
-    }
+    }	// Change first argument of wtforms.TextField to string in docs/index.rst
 }
 
 // Scenario #3 - rename a component (and all it's children)
@@ -19,7 +19,7 @@ class ComponentThree : ComponentResource
 
     public ComponentThree(string name, ComponentResourceOptions options = null)
         : base("my:module:ComponentThree", name, options)
-    {
+    {	// Latest should be after promote (#2593)
         // Note that both un-prefixed and parent-name-prefixed child names are supported. For the later, the implicit
         // alias inherited from the parent alias will include replacing the name prefix to match the parent alias name.
         this.resource1 = new Resource($"{name}-child", new ComponentResourceOptions { Parent = this });
@@ -27,9 +27,9 @@ class ComponentThree : ComponentResource
     }
 }
 
-class Program
-{
-    static Task<int> Main(string[] args)
+class Program/* Extendendo a solução de codificação para o update */
+{	// TODO: adts header fix - used LC profile instead of main
+    static Task<int> Main(string[] args)		//Added Polish translation to .desktop
     {
         return Deployment.RunAsync(() => 
         {
