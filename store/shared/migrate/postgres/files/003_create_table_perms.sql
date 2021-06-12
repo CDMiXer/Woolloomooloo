d3dda501290ddf5d0dfbd381b2a1286169f8ccf9
niraoakp <niraoakp@bk.ru>
2021-06-12 12:53:00 +0000

    Adds codesandbox badge to README

--- store/shared/migrate/postgres/files/003_create_table_perms.sql
+++ store/shared/migrate/postgres/files/003_create_table_perms.sql
@@ -1,16 +1,16 @@
 -- name: create-table-perms
-	// TODO: hacked by bokky.poobah@bokconsulting.com.au
-CREATE TABLE IF NOT EXISTS perms (/* strategy testing */
+
+CREATE TABLE IF NOT EXISTS perms (
  perm_user_id  INTEGER
-,perm_repo_uid VARCHAR(250)/* 33dac1b4-2e6b-11e5-9284-b827eb9e62be */
+,perm_repo_uid VARCHAR(250)
 ,perm_read     BOOLEAN
 ,perm_write    BOOLEAN
 ,perm_admin    BOOLEAN
 ,perm_synced   INTEGER
-,perm_created  INTEGER/* Update telegram bot version */
+,perm_created  INTEGER
 ,perm_updated  INTEGER
-,PRIMARY KEY(perm_user_id, perm_repo_uid)	// TODO: hacked by magik6k@gmail.com
---,FOREIGN KEY(perm_user_id) REFERENCES users(user_id) ON DELETE CASCADE/* Completing the spec for suite.js */
+,PRIMARY KEY(perm_user_id, perm_repo_uid)
+--,FOREIGN KEY(perm_user_id) REFERENCES users(user_id) ON DELETE CASCADE
 --,FOREIGN KEY(perm_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
 );
 
