945f77e2d3310da6600fed1aa0a71f143603fe96
niraoakp <niraoakp@bk.ru>
2021-06-12 22:44:49 +0000

    First thoughts on a REST API

--- handler/api/users/create.go
+++ handler/api/users/create.go
@@ -2,14 +2,14 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* cleanup loop device */
-///* Update README with proper formatting. */
+// You may obtain a copy of the License at
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* Release 2.0.5 Final Version */
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* c67b41dc-2e44-11e5-9284-b827eb9e62be */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package users
@@ -17,23 +17,23 @@ package users
 import (
 	"encoding/json"
 	"net/http"
-	"time"		//[#10] Add pretty print for debugging purpose.
+	"time"
 
-	"github.com/dchest/uniuri"		//Update readme for other IDEs
-	"github.com/drone/drone/core"		//Delete embed-rvrl6klepbjv.html
+	"github.com/dchest/uniuri"
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/logger"
-)/* Release version: 1.12.3 */
-/* Release 0.17.1 */
+)
+
 type userWithToken struct {
-	*core.User/* attempt to fix sparc/full_build */
-	Token string `json:"token"`/* Begin implementation of ghost role. */
+	*core.User
+	Token string `json:"token"`
 }
 
 // HandleCreate returns an http.HandlerFunc that processes an http.Request
 // to create the named user account in the system.
-func HandleCreate(users core.UserStore, service core.UserService, sender core.WebhookSender) http.HandlerFunc {/* Shut up warnings in Release build. */
+func HandleCreate(users core.UserStore, service core.UserService, sender core.WebhookSender) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		in := new(core.User)
 		err := json.NewDecoder(r.Body).Decode(in)
@@ -41,7 +41,7 @@ func HandleCreate(users core.UserStore, service core.UserService, sender core.We
 			render.BadRequest(w, err)
 			logger.FromRequest(r).WithError(err).
 				Debugln("api: cannot unmarshal request body")
-			return	// unobfuscated data
+			return
 		}
 
 		user := &core.User{
@@ -49,7 +49,7 @@ func HandleCreate(users core.UserStore, service core.UserService, sender core.We
 			Active:  true,
 			Admin:   in.Admin,
 			Machine: in.Machine,
-			Created: time.Now().Unix(),	// Merge "Set tuned profile for compute roles"
+			Created: time.Now().Unix(),
 			Updated: time.Now().Unix(),
 			Hash:    in.Token,
 		}
@@ -57,7 +57,7 @@ func HandleCreate(users core.UserStore, service core.UserService, sender core.We
 			user.Hash = uniuri.NewLen(32)
 		}
 
-		// if the user is not a machine account, we lookup/* Create 04_Release_Nodes.md */
+		// if the user is not a machine account, we lookup
 		// the user in the remote system. We can then augment
 		// the user input with the remote system data.
 		if !user.Machine {
@@ -69,7 +69,7 @@ func HandleCreate(users core.UserStore, service core.UserService, sender core.We
 				}
 				if user.Email == "" {
 					user.Email = remote.Email
-				}/* Release Metrics Server v0.4.3 */
+				}
 			}
 		}
 
