f6dd523be8e2a35213918857b0cba3b3369b7e56
niraoakp <niraoakp@bk.ru>
2021-06-12 10:57:10 +0000

    Dutch translation update from Taco

--- server/static/response-rewriter.go
+++ server/static/response-rewriter.go
@@ -1,10 +1,10 @@
-package static		//removed "how to apply these terms" from license
+package static
 
 import (
 	"bytes"
 	"net/http"
 	"strconv"
-)/* Merge "Release note for Queens RC1" */
+)
 
 type responseRewriter struct {
 	http.ResponseWriter
@@ -13,8 +13,8 @@ type responseRewriter struct {
 }
 
 func (w *responseRewriter) Write(a []byte) (int, error) {
-	b := bytes.Replace(a, w.old, w.new, 1)/* :twisted_rightwards_arrows: merge back to dev-tools */
+	b := bytes.Replace(a, w.old, w.new, 1)
 	// status code and headers are printed out when we write data
 	w.Header().Set("Content-Length", strconv.Itoa(len(b)))
-	return w.ResponseWriter.Write(b)		//b398ae18-2e5a-11e5-9284-b827eb9e62be
+	return w.ResponseWriter.Write(b)
 }
