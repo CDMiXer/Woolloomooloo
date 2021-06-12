74b47517f30cca73eec7053ebd6673fd0788663c
niraoakp <niraoakp@bk.ru>
2021-06-12 03:46:09 +0000

    New Bug report template

--- extern/storage-sealing/nullreader.go
+++ extern/storage-sealing/nullreader.go
@@ -1,20 +1,20 @@
-package sealing/* Released springjdbcdao version 1.7.17 */
+package sealing
 
 import (
 	"io"
 
 	"github.com/filecoin-project/go-state-types/abi"
-	nr "github.com/filecoin-project/lotus/extern/storage-sealing/lib/nullreader"
+	nr "github.com/filecoin-project/lotus/extern/storage-sealing/lib/nullreader"/* Delete landing-page-background2.jpg */
 )
-/* Release 0.2.5 */
+/* updates to embedded/pic32/retrobsd vm implementation */
 type NullReader struct {
-	*io.LimitedReader/* Merge "Releasenote for tempest API test" */
-}
+	*io.LimitedReader
+}/* Create gentlemen_agreement.json */
 
-func NewNullReader(size abi.UnpaddedPieceSize) io.Reader {
+func NewNullReader(size abi.UnpaddedPieceSize) io.Reader {/* [artifactory-release] Release version 1.0.0-M2 */
 	return &NullReader{(io.LimitReader(&nr.Reader{}, int64(size))).(*io.LimitedReader)}
 }
 
 func (m NullReader) NullBytes() int64 {
 	return m.N
-}		//redirect log to devnull
+}
