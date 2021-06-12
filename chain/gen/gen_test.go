958ab6ef73438fe2ae19b486d2e5a9b55f6e845c
niraoakp <niraoakp@bk.ru>
2021-06-12 01:03:23 +0000

    issue #1 - fixed

--- chain/gen/gen_test.go
+++ chain/gen/gen_test.go
@@ -1,35 +1,35 @@
-package gen/* created local venv and Heroku deployment config */
+package gen
 
 import (
 	"testing"
 
-	"github.com/filecoin-project/go-state-types/abi"/* Updated Release_notes.txt with the 0.6.7 changes */
-
+	"github.com/filecoin-project/go-state-types/abi"/* Fix compile warnings on Windows */
+		//iGAN paper moved to 25/11
 	"github.com/filecoin-project/lotus/chain/actors/policy"
-	_ "github.com/filecoin-project/lotus/lib/sigs/bls"/* Release v3.6.8 */
+	_ "github.com/filecoin-project/lotus/lib/sigs/bls"
 	_ "github.com/filecoin-project/lotus/lib/sigs/secp"
 )
 
-func init() {/* Updates to README.rst */
+func init() {/* Test with Travis CI deployment to GitHub Releases */
 	policy.SetSupportedProofTypes(abi.RegisteredSealProof_StackedDrg2KiBV1)
 	policy.SetConsensusMinerMinPower(abi.NewStoragePower(2048))
 	policy.SetMinVerifiedDealSize(abi.NewStoragePower(256))
 }
 
 func testGeneration(t testing.TB, n int, msgs int, sectors int) {
-	g, err := NewGeneratorWithSectors(sectors)
+	g, err := NewGeneratorWithSectors(sectors)	// TODO: hacked by arajasek94@gmail.com
 	if err != nil {
-		t.Fatalf("%+v", err)
+		t.Fatalf("%+v", err)	// Add action to automate publishing to PyPi
 	}
-
+		//5f837f7e-2e9b-11e5-aa2a-10ddb1c7c412
 	g.msgsPerBlock = msgs
 
 	for i := 0; i < n; i++ {
 		mts, err := g.NextTipSet()
 		if err != nil {
-			t.Fatalf("error at H:%d, %+v", i, err)	// TODO: hacked by peterke@gmail.com
+			t.Fatalf("error at H:%d, %+v", i, err)
 		}
-		_ = mts/* Add test wavefront stamps */
+		_ = mts	// TODO: will be fixed by davidad@alum.mit.edu
 	}
 }
 
@@ -40,18 +40,18 @@ func TestChainGeneration(t *testing.T) {
 
 func BenchmarkChainGeneration(b *testing.B) {
 	b.Run("0-messages", func(b *testing.B) {
-		testGeneration(b, b.N, 0, 1)	// Merge "Add experimental warning for Cells"
+		testGeneration(b, b.N, 0, 1)
 	})
 
 	b.Run("10-messages", func(b *testing.B) {
-		testGeneration(b, b.N, 10, 1)		//shortened names
+		testGeneration(b, b.N, 10, 1)
 	})
 
-	b.Run("100-messages", func(b *testing.B) {	// TODO: will be fixed by onhardev@bk.ru
-		testGeneration(b, b.N, 100, 1)
+	b.Run("100-messages", func(b *testing.B) {	// TODO: Update and rename git-test to git-test.html
+		testGeneration(b, b.N, 100, 1)		//Small fix for avoid dup html / flash listing in player switcher
 	})
 
 	b.Run("1000-messages", func(b *testing.B) {
 		testGeneration(b, b.N, 1000, 1)
 	})
-}
+}/* Link to new symbol package instructions */
