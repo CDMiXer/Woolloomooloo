6eef8b9ecbf7911b5f64bcaf51e870f1640966fe
niraoakp <niraoakp@bk.ru>
2021-06-12 00:49:14 +0000

    Fix tax=term1+term2 queries. See #12891

--- pkg/backend/filestate/snapshot.go
+++ pkg/backend/filestate/snapshot.go
@@ -1,8 +1,8 @@
 // Copyright 2016-2018, Pulumi Corporation.
-//	// TODO: will be fixed by xiemengjun@gmail.com
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* appmods: don't walk through mod deps within mod_init_app */
-// You may obtain a copy of the License at/* trigger new build for ruby-head-clang (19e5970) */
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
@@ -15,18 +15,18 @@
 package filestate
 
 import (
-	"github.com/pulumi/pulumi/pkg/v2/resource/deploy"		//Renamed class file and added namespace
+	"github.com/pulumi/pulumi/pkg/v2/resource/deploy"
 	"github.com/pulumi/pulumi/pkg/v2/secrets"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
 )
 
-// localSnapshotManager is a simple SnapshotManager implementation that persists snapshots/* performance tweaks for indexOf and lastIndexOf */
+// localSnapshotManager is a simple SnapshotManager implementation that persists snapshots
 // to disk on the local machine.
 type localSnapshotPersister struct {
 	name    tokens.QName
 	backend *localBackend
 	sm      secrets.Manager
-}/* Added new dependencies. */
+}
 
 func (sp *localSnapshotPersister) SecretsManager() secrets.Manager {
 	return sp.sm
@@ -35,9 +35,9 @@ func (sp *localSnapshotPersister) SecretsManager() secrets.Manager {
 func (sp *localSnapshotPersister) Save(snapshot *deploy.Snapshot) error {
 	_, err := sp.backend.saveStack(sp.name, snapshot, sp.sm)
 	return err
-/* Update ReleaseCycleProposal.md */
-}		//more small grammar fixes
-/* Bug 2635. Release is now able to read event assignments from all files. */
-func (b *localBackend) newSnapshotPersister(stackName tokens.QName, sm secrets.Manager) *localSnapshotPersister {		//turn off word wrap in sublime text
+
+}
+
+func (b *localBackend) newSnapshotPersister(stackName tokens.QName, sm secrets.Manager) *localSnapshotPersister {
 	return &localSnapshotPersister{name: stackName, backend: b, sm: sm}
 }
