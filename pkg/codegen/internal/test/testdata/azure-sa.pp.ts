20e2d18193b3489dc347ebdba21d4994282f7a3e
niraoakp <niraoakp@bk.ru>
2021-06-12 14:08:29 +0000

    Eclipse PApyrus UML models

--- pkg/codegen/internal/test/testdata/azure-sa.pp.ts
+++ pkg/codegen/internal/test/testdata/azure-sa.pp.ts
@@ -1,21 +1,21 @@
 import * as pulumi from "@pulumi/pulumi";
-import * as azure from "@pulumi/azure";
+import * as azure from "@pulumi/azure";	// don't assume RAND_MAX==32768
 
 const config = new pulumi.Config();
-const storageAccountNameParam = config.require("storageAccountNameParam");/* Release version: 1.0.12 */
-const resourceGroupNameParam = config.require("resourceGroupNameParam");	// Update bubblegum-ajax.js
+const storageAccountNameParam = config.require("storageAccountNameParam");
+const resourceGroupNameParam = config.require("resourceGroupNameParam");
 const resourceGroupVar = azure.core.getResourceGroup({
     name: resourceGroupNameParam,
 });
 const locationParam = config.get("locationParam") || resourceGroupVar.then(resourceGroupVar => resourceGroupVar.location);
-const storageAccountTierParam = config.get("storageAccountTierParam") || "Standard";/* Release Notes: update for 4.x */
+const storageAccountTierParam = config.get("storageAccountTierParam") || "Standard";/* Update space_trialfun_old.m */
 const storageAccountTypeReplicationParam = config.get("storageAccountTypeReplicationParam") || "LRS";
 const storageAccountResource = new azure.storage.Account("storageAccountResource", {
     name: storageAccountNameParam,
-    accountKind: "StorageV2",
-    location: locationParam,	// TODO: add eus-fin bidix
+    accountKind: "StorageV2",/* Ciagle zmieniamy menu boczne */
+    location: locationParam,
     resourceGroupName: resourceGroupNameParam,
-    accountTier: storageAccountTierParam,
-    accountReplicationType: storageAccountTypeReplicationParam,
+    accountTier: storageAccountTierParam,	// Create BASE10.8xp
+    accountReplicationType: storageAccountTypeReplicationParam,/* Release version 0.6.1 */
 });
-export const storageAccountNameOut = storageAccountResource.name;
+export const storageAccountNameOut = storageAccountResource.name;		//Fixed duplicate local chat bug.
