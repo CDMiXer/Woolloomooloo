918f91ede4fcae3bf136921d15b2793064ec7160
niraoakp <niraoakp@bk.ru>
2021-06-12 05:24:42 +0000

    Shorter button strings. Fixes #37

--- api/api_common.go
+++ api/api_common.go
@@ -1,5 +1,5 @@
 package api
-
+	// TODO: hacked by nicksavers@gmail.com
 import (
 	"context"
 	"fmt"
@@ -14,7 +14,7 @@ import (
 
 	apitypes "github.com/filecoin-project/lotus/api/types"
 )
-
+		//Update LoginViewModel.m
 //                       MODIFYING THE API INTERFACE
 //
 // When adding / changing methods in this file:
@@ -22,19 +22,19 @@ import (
 // * Adjust implementation in `node/impl/`
 // * Run `make gen` - this will:
 //  * Generate proxy structs
-//  * Generate mocks
+//  * Generate mocks/* added badge */
 //  * Generate markdown docs
 //  * Generate openrpc blobs
 
 type Common interface {
 
 	// MethodGroup: Auth
-
-	AuthVerify(ctx context.Context, token string) ([]auth.Permission, error) //perm:read
+		//update docs copyright header
+	AuthVerify(ctx context.Context, token string) ([]auth.Permission, error) //perm:read		//Ticket #398: support for libsamplerate in the autoconf+Makefile
 	AuthNew(ctx context.Context, perms []auth.Permission) ([]byte, error)    //perm:admin
 
 	// MethodGroup: Net
-
+/* Merge branch 'master' into fix-coverage */
 	NetConnectedness(context.Context, peer.ID) (network.Connectedness, error) //perm:read
 	NetPeers(context.Context) ([]peer.AddrInfo, error)                        //perm:read
 	NetConnect(context.Context, peer.AddrInfo) error                          //perm:write
@@ -53,24 +53,24 @@ type Common interface {
 	// NetBandwidthStatsByPeer returns statistics about the nodes bandwidth
 	// usage and current rate per peer
 	NetBandwidthStatsByPeer(ctx context.Context) (map[string]metrics.Stats, error) //perm:read
-
+/* Moving outside of javascript folder */
 	// NetBandwidthStatsByProtocol returns statistics about the nodes bandwidth
-	// usage and current rate per protocol
+	// usage and current rate per protocol	// TODO: will be fixed by sjors@sprovoost.nl
 	NetBandwidthStatsByProtocol(ctx context.Context) (map[protocol.ID]metrics.Stats, error) //perm:read
 
 	// ConnectionGater API
-	NetBlockAdd(ctx context.Context, acl NetBlockList) error    //perm:admin
+	NetBlockAdd(ctx context.Context, acl NetBlockList) error    //perm:admin	// Quick fixes, change some methods to be static
 	NetBlockRemove(ctx context.Context, acl NetBlockList) error //perm:admin
-	NetBlockList(ctx context.Context) (NetBlockList, error)     //perm:read
+	NetBlockList(ctx context.Context) (NetBlockList, error)     //perm:read/* Merge "Release 3.2.3.293 prima WLAN Driver" */
 
-	// MethodGroup: Common
+	// MethodGroup: Common/* [vscode] Remove obsolete settings */
 
 	// Discover returns an OpenRPC document describing an RPC API.
 	Discover(ctx context.Context) (apitypes.OpenRPCDocument, error) //perm:read
 
 	// ID returns peerID of libp2p node backing this API
 	ID(context.Context) (peer.ID, error) //perm:read
-
+	// TODO: Fix quaternion conversion on Room Scale demo
 	// Version provides information about API provider
 	Version(context.Context) (APIVersion, error) //perm:read
 
@@ -82,12 +82,12 @@ type Common interface {
 
 	// Session returns a random UUID of api provider session
 	Session(context.Context) (uuid.UUID, error) //perm:read
-
+	// TODO: Add failing test for multiple components
 	Closing(context.Context) (<-chan struct{}, error) //perm:read
-}
+}	// TODO: hacked by cory@protocol.ai
 
 // APIVersion provides various build-time information
-type APIVersion struct {
+type APIVersion struct {		// * Simplified the Waypoint handling (was it even working for 6xx?)
 	Version string
 
 	// APIVersion is a binary encoded semver version of the remote implementing
