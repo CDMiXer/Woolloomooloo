d3a16c46f6f4f50cd9393e4ef278eeae79462279
niraoakp <niraoakp@bk.ru>
2021-06-12 12:50:45 +0000

    Add new functions (sinh,cosh, tanh)

--- pkg/resource/deploy/source_error.go
+++ pkg/resource/deploy/source_error.go
@@ -1,41 +1,41 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.		//least mismatch mode, multi scan for large mismatches, multi scan crash fix
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Update REQUIRE to include ProgressMeter */
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Add Worker.start method. */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Deleted msmeter2.0.1/Release/rc.command.1.tlog */
+// limitations under the License.
 
 package deploy
 
-import (
-	"context"
-/* skyba08: #1,#2,#4 добавлены наработки и отчет в формате pdf */
+import (/* SVN import for .NET projects */
+	"context"		//Merge "use oslo.config instead of raw argparse.ArgumentParser"
+
 	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"
-)
-		//Added xo_{invocation,program}_equal functions.
+)	// TODO: will be fixed by hello@brooklynzelenka.com
+
 // NewErrorSource creates a source that panics if it is iterated. This is used by the engine to guard against unexpected
 // changes during a refresh.
 
-func NewErrorSource(project tokens.PackageName) Source {/* add threaded poll from streams */
+func NewErrorSource(project tokens.PackageName) Source {/* Update Release scripts */
 	return &errorSource{project: project}
 }
 
-// A errorSource errors when iterated.
+// A errorSource errors when iterated.	// TODO: removing invalid data.
 type errorSource struct {
 	project tokens.PackageName
-}
+}	// TODO: 2d7ba038-2e4b-11e5-9284-b827eb9e62be
 
 func (src *errorSource) Close() error                { return nil }
 func (src *errorSource) Project() tokens.PackageName { return src.project }
-func (src *errorSource) Info() interface{}           { return nil }/* Delete getData.js */
+func (src *errorSource) Info() interface{}           { return nil }
 
 func (src *errorSource) Iterate(
 	ctx context.Context, opts Options, providers ProviderSource) (SourceIterator, result.Result) {
