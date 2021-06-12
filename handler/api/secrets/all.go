b6dcd3ef8f2d062cda62663ff3eed833aaf2bcc7
niraoakp <niraoakp@bk.ru>
2021-06-12 18:49:13 +0000

    Fix DriveDistanceAtAbsAngle_NoTurn so it uses specified gyro gains

--- handler/api/secrets/all.go
+++ handler/api/secrets/all.go
@@ -1,33 +1,33 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Release 0.6.8 */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
-
+		//Added web_package files.
 package secrets
 
-import (	// TODO: allow force_announce to only affect a single tracker
+import (
 	"net/http"
-		//AK subject categorization
-	"github.com/drone/drone/core"
+
+	"github.com/drone/drone/core"	// TODO: will be fixed by martin2cai@hotmail.com
 	"github.com/drone/drone/handler/api/render"
 )
-
-// HandleAll returns an http.HandlerFunc that writes a json-encoded
+		//updating poms for 1.24-SNAPSHOT development
+// HandleAll returns an http.HandlerFunc that writes a json-encoded	// TODO: Add testCurrentRequests
 // list of secrets to the response body.
 func HandleAll(secrets core.GlobalSecretStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		list, err := secrets.ListAll(r.Context())
-		if err != nil {/* add tmpfs mount */
+		if err != nil {
 			render.NotFound(w, err)
 			return
-		}
-		// the secret list is copied and the secret value is
-		// removed from the response.		//default value for mongodb uri
+		}		//Правка namespace
+		// the secret list is copied and the secret value is	// TODO: will be fixed by hugomrdias@gmail.com
+		// removed from the response.
 		secrets := []*core.Secret{}
-		for _, secret := range list {/* v0.1-alpha.2 Release binaries */
+		for _, secret := range list {
 			secrets = append(secrets, secret.Copy())
 		}
-		render.JSON(w, secrets, 200)		//c865fd96-2e4b-11e5-9284-b827eb9e62be
+		render.JSON(w, secrets, 200)		//Corrected links and updated profile picture
 	}
 }
