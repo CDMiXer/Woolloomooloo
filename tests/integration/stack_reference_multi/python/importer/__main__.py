10bdd47f343d2953b8a47b571b13a958451e7c0f
niraoakp <niraoakp@bk.ru>
2021-06-12 02:24:54 +0000

    Fixing scenarios after merge SLIM-638

--- tests/integration/stack_reference_multi/python/importer/__main__.py
+++ tests/integration/stack_reference_multi/python/importer/__main__.py
@@ -3,7 +3,7 @@ import pulumi
 config = pulumi.Config()
 
 exporterStackName = config.require('exporter_stack_name')
-org = config.require('org')		//bbb2c834-2e5c-11e5-9284-b827eb9e62be
+org = config.require('org')
 a = pulumi.StackReference(f'{org}/exporter/{exporterStackName}')
 
 pulumi.export('val1', a.require_output('val'))
