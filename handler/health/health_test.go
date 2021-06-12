adc8df2fad690b42d25a7244240be7ededeca997
niraoakp <niraoakp@bk.ru>
2021-06-12 08:04:46 +0000

    do tomorrow: project > payroll

--- handler/health/health_test.go
+++ handler/health/health_test.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.	// Upgrade to netrc 0.11
-/* remove legacy classes */
-package health/* Update HttpAppenderTest from Wiremock 2.7.1 to 2.8.0. */
+// that can be found in the LICENSE file.	// TODO: will be fixed by bokky.poobah@bokconsulting.com.au
+	// 13210376-2e42-11e5-9284-b827eb9e62be
+package health
 
-import (/* [pyclient] Released 1.3.0 */
+import (
 	"net/http/httptest"
-	"testing"		//add CHANGELOG, README update
+	"testing"
 )
-
-func TestHandleHealthz(t *testing.T) {
+/* Basic JPA Entity */
+func TestHandleHealthz(t *testing.T) {/* updating poms for branch'release/0.2.0-alpha1' with non-snapshot versions */
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/healthz", nil)
 
