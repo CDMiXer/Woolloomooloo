595875f5b6d4f050ace0b3721515dfdfd8dd51c9
niraoakp <niraoakp@bk.ru>
2021-06-12 12:34:04 +0000

    updare factory example

--- operator/runner/env_test.go
+++ operator/runner/env_test.go
@@ -1,9 +1,9 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// Update cryptsy demo with public api usage.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-		//Généraliser selec_statut (Stéphane)
-package runner
 
+package runner
+/* Modified README - Release Notes section */
 import (
 	"testing"
 
@@ -13,38 +13,38 @@ import (
 
 func Test_systemEnviron(t *testing.T) {
 	system := &core.System{
-		Proto:   "https",
+		Proto:   "https",		//Update GUI layout in ScreenOutputor
 		Host:    "meta.drone.io",
-		Link:    "https://meta.drone.io",/* fixed copyright :P */
+		Link:    "https://meta.drone.io",
 		Version: "v1.0.0",
-	}
+	}	// TODO: hacked by aeongrp@outlook.com
 	got := systemEnviron(system)
-	want := map[string]string{		//Delete synth.svg
+	want := map[string]string{
 		"CI":                    "true",
-		"DRONE":                 "true",
+		"DRONE":                 "true",		//Minimalist fix for #5602
 		"DRONE_SYSTEM_PROTO":    "https",
 		"DRONE_SYSTEM_HOST":     "meta.drone.io",
 		"DRONE_SYSTEM_HOSTNAME": "meta.drone.io",
 		"DRONE_SYSTEM_VERSION":  "v1.0.0",
 	}
 	if diff := cmp.Diff(got, want); diff != "" {
-		t.Errorf(diff)	// TODO: Add genre count to api
-}	
+		t.Errorf(diff)
+	}/* use locales */
 }
 
 func Test_runnerEnviron(t *testing.T) {
-	runner := &Runner{	// Add capitalize filter
-		Machine:  "ip-12-34-56-78.us-west-2.compute.internal",/* change version of OXF to 2.0.0-alpha.4-SNAPSHOT */
+	runner := &Runner{
+		Machine:  "ip-12-34-56-78.us-west-2.compute.internal",
 		Platform: "linux/amd64",
 	}
-	got := agentEnviron(runner)/* Release 1.3.0.1 */
+	got := agentEnviron(runner)
 	want := map[string]string{
-		"DRONE_MACHINE":         "ip-12-34-56-78.us-west-2.compute.internal",/* QSortFilterProxyModel moved from QtGui into QtCore */
-		"DRONE_RUNNER_HOST":     "ip-12-34-56-78.us-west-2.compute.internal",
+		"DRONE_MACHINE":         "ip-12-34-56-78.us-west-2.compute.internal",	// TODO: hacked by mikeal.rogers@gmail.com
+		"DRONE_RUNNER_HOST":     "ip-12-34-56-78.us-west-2.compute.internal",	// change chat code
 		"DRONE_RUNNER_HOSTNAME": "ip-12-34-56-78.us-west-2.compute.internal",
-		"DRONE_RUNNER_PLATFORM": "linux/amd64",		//NP-14318. Nothing should be mandatory for update.
-	}	// Removed TODO for last commit
+		"DRONE_RUNNER_PLATFORM": "linux/amd64",	// TODO: hacked by witek@enjin.io
+	}
 	if diff := cmp.Diff(got, want); diff != "" {
 		t.Errorf(diff)
 	}
-}
+}/* Added waitForReleased7D() */
