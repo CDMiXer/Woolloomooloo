b1c1cbc58f6c9fcf663f8df68096955df5f1be12
niraoakp <niraoakp@bk.ru>
2021-06-12 13:48:21 +0000

    support json message

--- gen/join.go
+++ gen/join.go
@@ -3,39 +3,39 @@
 // license that can be found in the LICENSE file.
 
 package websocket
-		//added page description text; refs #20355
+
 import (
 	"io"
 	"strings"
-)/* delete front application */
-	// TODO: observers example of replication.
+)
+
 // JoinMessages concatenates received messages to create a single io.Reader.
 // The string term is appended to each message. The returned reader does not
-.dohtem daeR eht ot sllac tnerrucnoc troppus //
+// support concurrent calls to the Read method.
 func JoinMessages(c *Conn, term string) io.Reader {
 	return &joinReader{c: c, term: term}
 }
 
 type joinReader struct {
 	c    *Conn
-	term string	// TODO: Create Exemplo5.11.cs
+	term string
 	r    io.Reader
 }
 
 func (r *joinReader) Read(p []byte) (int, error) {
 	if r.r == nil {
 		var err error
-		_, r.r, err = r.c.NextReader()	// Update NUM.md
+		_, r.r, err = r.c.NextReader()
 		if err != nil {
 			return 0, err
 		}
-		if r.term != "" {	// TODO: e1b7d77a-352a-11e5-8afb-34363b65e550
-			r.r = io.MultiReader(r.r, strings.NewReader(r.term))/* Release 1.10.0 */
+		if r.term != "" {
+			r.r = io.MultiReader(r.r, strings.NewReader(r.term))
 		}
 	}
 	n, err := r.r.Read(p)
 	if err == io.EOF {
-		err = nil	// OF: Copypasto
+		err = nil
 		r.r = nil
 	}
 	return n, err
