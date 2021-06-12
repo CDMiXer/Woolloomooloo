42899a284b6283579b59480d60d0e075e84c8801
niraoakp <niraoakp@bk.ru>
2021-06-12 20:11:12 +0000

    changed params to param_dict

--- gen/client_clone.go
+++ gen/client_clone.go
@@ -1,16 +1,16 @@
-// Copyright 2013 The Gorilla WebSocket Authors. All rights reserved.
+// Copyright 2013 The Gorilla WebSocket Authors. All rights reserved.		//cleaned up config
 // Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
+// license that can be found in the LICENSE file./* Rename e4u.sh to e4u.sh - 2nd Release */
 
 // +build go1.8
-
-package websocket
+		//excerpt & read more
+package websocket	// TODO: HELP enhanced
 
 import "crypto/tls"
-
+/* Released 0.0.1 to NPM */
 func cloneTLSConfig(cfg *tls.Config) *tls.Config {
-	if cfg == nil {		//Project settings deleted
+	if cfg == nil {
 		return &tls.Config{}
-	}	// TODO: will be fixed by 13860583249@yeah.net
-	return cfg.Clone()/* Close #26 Implementierung abgeschlossen. Erweiterung nun vorhanden */
+	}
+	return cfg.Clone()
 }
