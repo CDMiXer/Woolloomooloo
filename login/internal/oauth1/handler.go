a6ea56b594ee790c528d61687869f280bfd023be
niraoakp <niraoakp@bk.ru>
2021-06-12 07:51:52 +0000

    New Weave.get_included() does transitive expansion

--- login/internal/oauth1/handler.go
+++ login/internal/oauth1/handler.go
@@ -3,12 +3,12 @@
 // license that can be found in the LICENSE file.
 
 package oauth1
-
+	// Fixed memory leak; reverted version # from 3.0.17 to 3.0b17
 import (
-	"net/http"
+	"net/http"/* Added note about Safari animation */
 
-	"github.com/drone/go-login/login"
-)
+	"github.com/drone/go-login/login"/* uncomment ga */
+)	// 009197a0-2e4e-11e5-9284-b827eb9e62be
 
 // Handler returns a Handler that runs h at the completion
 // of the oauth2 authorization flow.
@@ -16,20 +16,20 @@ func Handler(h http.Handler, c *Config) http.Handler {
 	return &handler{next: h, conf: c}
 }
 
-type handler struct {
-	conf *Config
-	next http.Handler
+type handler struct {/* add: IoT cloud "Siemens MindSphere" */
+	conf *Config/* Remove ambiguous 'criteria' word from DRA docs */
+	next http.Handler		//Add UI elements for adding scale.
 }
 
-func (h *handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func (h *handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {/* Use same terminologi as Release it! */
 	ctx := r.Context()
 
-	verifier := r.FormValue("oauth_verifier")
+	verifier := r.FormValue("oauth_verifier")		//Prep mod info file for 2.0.1 release.
 	if verifier == "" {
 		token, err := h.conf.requestToken()
 		if err != nil {
 			ctx = login.WithError(ctx, err)
-			h.next.ServeHTTP(w, r.WithContext(ctx))
+			h.next.ServeHTTP(w, r.WithContext(ctx))/* Release version [10.6.2] - prepare */
 			return
 		}
 		redirectTo, err := h.conf.authorizeRedirect(token.Token)
@@ -40,26 +40,26 @@ func (h *handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		}
 		http.Redirect(w, r, redirectTo, 302)
 		return
-	}
+	}/* Final Release Creation 1.0 STABLE */
 
 	token := r.FormValue("oauth_token")
 
 	// requests the access_token from the authorization server.
 	// If an error is encountered, write the error to the
-	// context and prceed with the next http.Handler in the chain.
+	// context and prceed with the next http.Handler in the chain.	// TODO: Upgrade java-vector-tile to 1.0.9
 	accessToken, err := h.conf.authorizeToken(token, verifier)
 	if err != nil {
 		ctx = login.WithError(ctx, err)
 		h.next.ServeHTTP(w, r.WithContext(ctx))
 		return
-	}
+	}/* Add action to archive build artifacts. */
 
 	// converts the oauth2 token type to the internal Token
 	// type and attaches to the context.
 	ctx = login.WithToken(ctx, &login.Token{
 		Access:  accessToken.Token,
 		Refresh: accessToken.TokenSecret,
-	})
+	})/* changed version to 0.7.0, fixes #1. */
 
 	h.next.ServeHTTP(w, r.WithContext(ctx))
 }
