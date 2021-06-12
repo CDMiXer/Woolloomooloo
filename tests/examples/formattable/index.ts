59fe27d15418de367e674cce83afa1fc0d363561
niraoakp <niraoakp@bk.ru>
2021-06-12 17:46:19 +0000

    PrintFTest

--- tests/examples/formattable/index.ts
+++ tests/examples/formattable/index.ts
@@ -1,4 +1,4 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-console.log("Here's some format goo: %s, %v, %q, %d!");
+console.log("Here's some format goo: %s, %v, %q, %d!");	// TODO: hacked by sjors@sprovoost.nl
 console.error("Here's some format goo: %s, %v, %q, %d!");
