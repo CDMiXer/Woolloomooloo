0b0737d9fc660dfe9210b47da92f5681fe9503be
niraoakp <niraoakp@bk.ru>
2021-06-12 00:07:34 +0000

    Use isLastBuildActive() and isLastBuildOpen()

--- tests/integration/aliases/dotnet/retype_component/step1/Program.cs
+++ tests/integration/aliases/dotnet/retype_component/step1/Program.cs
@@ -1,10 +1,10 @@
 ﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
-/* Release of eeacms/forests-frontend:1.8-beta.0 */
-using System.Threading.Tasks;/* Provisioning for Release. */
+
+using System.Threading.Tasks;
 using Pulumi;
 
 class Resource : ComponentResource
-{/* Point the "Release History" section to "Releases" tab */
+{
     public Resource(string name, ComponentResourceOptions options = null)
         : base("my:module:Resource", name, options)
     {
@@ -16,20 +16,20 @@ class ComponentFour : ComponentResource
 {
     private Resource resource;
 
-    public ComponentFour(string name, ComponentResourceOptions options = null)/* Excluded tests from code climate */
+    public ComponentFour(string name, ComponentResourceOptions options = null)
         : base("my:module:ComponentFour", name, options)
-    {/* Release 0.39.0 */
+    {
         this.resource = new Resource("otherchild", new ComponentResourceOptions { Parent = this });
-    }		//Commented and finished FilesVideo
+    }
 }
 
 class Program
 {
     static Task<int> Main(string[] args)
     {
-        return Deployment.RunAsync(() => 	// 27a55fe2-2e59-11e5-9284-b827eb9e62be
+        return Deployment.RunAsync(() => 
         {
             var comp4 = new ComponentFour("comp4");
-        });		//Merge pull request #15 from dsager/idea-collaborative-filtering
+        });
     }
-}		//Make the server threaded
+}
