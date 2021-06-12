96e55317f3b0be2ed5f20da56f18d80884184a8d
niraoakp <niraoakp@bk.ru>
2021-06-12 17:53:07 +0000

    64339686-2fa5-11e5-b0b6-00012e3d3f12

--- grpc-go/internal/channelz/util_nonlinux.go
+++ grpc-go/internal/channelz/util_nonlinux.go
@@ -4,16 +4,16 @@
  *
  * Copyright 2018 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");	// Switched out events:{...} for cb-* attribute bindings
- * you may not use this file except in compliance with the License.		//Adjusting format of blog post
- * You may obtain a copy of the License at/* Correct spelling errors. */
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and	// TODO: changed cluster threshold parameter from 3 to NA
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
@@ -23,4 +23,4 @@ package channelz
 // GetSocketOption gets the socket option info of the conn.
 func GetSocketOption(c interface{}) *SocketOptionData {
 	return nil
-}	// TS::Test should use suppress_delta_output correctly
+}
