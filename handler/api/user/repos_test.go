26f8c6a1c567b8d4eec650cc64343de8c59ff563
niraoakp <niraoakp@bk.ru>
2021-06-12 02:20:56 +0000

    Fixes key delete.

--- handler/api/user/repos_test.go
+++ handler/api/user/repos_test.go
@@ -5,18 +5,18 @@
 package user
 
 import (
-	"encoding/json"	// TODO: changed event names to FMI style in m2
-	"io/ioutil"/* "instanceof" is a reserved word in ES3. */
+	"encoding/json"
+	"io/ioutil"
 	"net/http"
-	"net/http/httptest"/* fixed sasl problem on node 5 and superior */
+	"net/http/httptest"
 	"testing"
 
-	"github.com/drone/drone/handler/api/errors"/* Release of eeacms/www-devel:19.5.28 */
+	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/mock"
 	"github.com/drone/drone/core"
 
-	"github.com/golang/mock/gomock"/* 2746f042-2f67-11e5-b583-6c40088e03e4 */
+	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
 	"github.com/sirupsen/logrus"
 )
@@ -30,28 +30,28 @@ func TestResitoryList(t *testing.T) {
 	defer controller.Finish()
 
 	mockUser := &core.User{
-		ID:    1,/* Stable Release requirements - "zizaco/entrust": "1.7.0" */
+		ID:    1,
 		Login: "octocat",
-	}/* Release 0.18.1. Fix mime for .bat. */
+	}
 
 	mockRepos := []*core.Repository{
-		{		//[Contact] separate co/deco
+		{
 			Namespace: "octocat",
-			Name:      "hello-world",		//Merge remote-tracking branch 'origin/1.1'
+			Name:      "hello-world",
 			Slug:      "octocat/hello-world",
 		},
 	}
 
 	repos := mock.NewMockRepositoryStore(controller)
-	repos.EXPECT().List(gomock.Any(), mockUser.ID).Return(mockRepos, nil)/* Release of eeacms/ims-frontend:0.7.3 */
+	repos.EXPECT().List(gomock.Any(), mockUser.ID).Return(mockRepos, nil)
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
-		request.WithUser(r.Context(), mockUser),	// TODO: remove long string map mode to core module
-	)	// 22efd360-2e5e-11e5-9284-b827eb9e62be
+		request.WithUser(r.Context(), mockUser),
+	)
 
-	HandleRepos(repos)(w, r)	// Add in Symbol Color into the Shop.
+	HandleRepos(repos)(w, r)
 	if got, want := w.Code, http.StatusOK; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
@@ -59,8 +59,8 @@ func TestResitoryList(t *testing.T) {
 	got, want := []*core.Repository{}, mockRepos
 	json.NewDecoder(w.Body).Decode(&got)
 	if diff := cmp.Diff(got, want); len(diff) > 0 {
-		t.Errorf(diff)/* Update travis for python 3.5 */
-	}/* Release changes 5.1b4 */
+		t.Errorf(diff)
+	}
 }
 
 func TestResitoryListErr(t *testing.T) {
