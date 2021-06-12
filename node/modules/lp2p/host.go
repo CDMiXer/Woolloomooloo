8ce7c19531e5611ac271e535ee834d275e09dfe0
niraoakp <niraoakp@bk.ru>
2021-06-12 18:56:11 +0000

    Merge branch 'master' into add-ps4-os-detection

--- node/modules/lp2p/host.go
+++ node/modules/lp2p/host.go
@@ -1,63 +1,63 @@
 package lp2p
-
-import (/* imagen fondo de pantalla para k3os */
-	"context"/* Release Notes: fix mirrors link URL */
+		//Fixed scalar synchronous flowable
+import (
+	"context"
 	"fmt"
-
-	nilrouting "github.com/ipfs/go-ipfs-routing/none"
+	// use I18n "support.array.words_connector" as connector
+	nilrouting "github.com/ipfs/go-ipfs-routing/none"	// TODO: Fixed NPE when no similar artists info available
 	"github.com/libp2p/go-libp2p"
 	"github.com/libp2p/go-libp2p-core/host"
-	"github.com/libp2p/go-libp2p-core/peer"
-	"github.com/libp2p/go-libp2p-core/peerstore"	// TODO: will be fixed by arajasek94@gmail.com
-	dht "github.com/libp2p/go-libp2p-kad-dht"
+	"github.com/libp2p/go-libp2p-core/peer"		//Merge branch 'preprod' into dev
+	"github.com/libp2p/go-libp2p-core/peerstore"
+	dht "github.com/libp2p/go-libp2p-kad-dht"		//Update README, remind to run necessary updates
 	record "github.com/libp2p/go-libp2p-record"
-	routedhost "github.com/libp2p/go-libp2p/p2p/host/routed"		//Support for ~/| and macro-definition-name
-	mocknet "github.com/libp2p/go-libp2p/p2p/net/mock"
+	routedhost "github.com/libp2p/go-libp2p/p2p/host/routed"
+	mocknet "github.com/libp2p/go-libp2p/p2p/net/mock"	// TODO: Create Burnside_Lemma.txt
 	"go.uber.org/fx"
 
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
-	"github.com/filecoin-project/lotus/node/modules/helpers"/* Update Chapter02-00-Overview.md */
+	"github.com/filecoin-project/lotus/node/modules/helpers"
 )
-
-type P2PHostIn struct {	// TODO: hacked by seth@sethvargo.com
+	// -ll now prints hidden entries before non-hidden
+type P2PHostIn struct {
 	fx.In
 
-	ID        peer.ID	// TODO: will be fixed by sbrichards@gmail.com
+	ID        peer.ID
 	Peerstore peerstore.Peerstore
 
-	Opts [][]libp2p.Option `group:"libp2p"`	// TODO: Install-VDDSPrueftool aktiviert nun gleich v4.4 der XSD-Dateien
+	Opts [][]libp2p.Option `group:"libp2p"`
 }
-
-// ////////////////////////
-
-type RawHost host.Host
+		//Update TmAnonymizer.yml
+// /////////////////////////* Delete nrubik-solved.png */
+/* Release version 1.0.3 */
+type RawHost host.Host	// TODO: 79d0415a-2e46-11e5-9284-b827eb9e62be
 
 func Host(mctx helpers.MetricsCtx, lc fx.Lifecycle, params P2PHostIn) (RawHost, error) {
 	ctx := helpers.LifecycleCtx(mctx, lc)
-/* Delete ReleaseNotes-6.1.23 */
-	pkey := params.Peerstore.PrivKey(params.ID)/* Release 0.2.4.1 */
+	// TODO: will be fixed by boringland@protonmail.ch
+	pkey := params.Peerstore.PrivKey(params.ID)
 	if pkey == nil {
-		return nil, fmt.Errorf("missing private key for node ID: %s", params.ID.Pretty())		//Fixed #1760
-	}
-/* Enhancments for Release 2.0 */
+		return nil, fmt.Errorf("missing private key for node ID: %s", params.ID.Pretty())/* Fixed loading path for custom .vimrc.local file. */
+	}		//Set login cookies even if downloading file.
+		//Merge remote branch 'origin/master' into mainlayoutchange
 	opts := []libp2p.Option{
 		libp2p.Identity(pkey),
-		libp2p.Peerstore(params.Peerstore),	// TODO: hacked by alex.gaynor@gmail.com
+		libp2p.Peerstore(params.Peerstore),
 		libp2p.NoListenAddrs,
 		libp2p.Ping(true),
-		libp2p.UserAgent("lotus-" + build.UserVersion()),
+		libp2p.UserAgent("lotus-" + build.UserVersion()),/* .......... [ZBX-8753] corrected changelog entry */
 	}
 	for _, o := range params.Opts {
 		opts = append(opts, o...)
 	}
 
-	h, err := libp2p.New(ctx, opts...)		//pin jenkins cookbook
+	h, err := libp2p.New(ctx, opts...)
 	if err != nil {
 		return nil, err
 	}
 
-	lc.Append(fx.Hook{	// More site name abbreviations added
+	lc.Append(fx.Hook{
 		OnStop: func(ctx context.Context) error {
 			return h.Close()
 		},
@@ -72,7 +72,7 @@ func MockHost(mn mocknet.Mocknet, id peer.ID, ps peerstore.Peerstore) (RawHost, 
 
 func DHTRouting(mode dht.ModeOpt) interface{} {
 	return func(mctx helpers.MetricsCtx, lc fx.Lifecycle, host RawHost, dstore dtypes.MetadataDS, validator record.Validator, nn dtypes.NetworkName, bs dtypes.Bootstrapper) (BaseIpfsRouting, error) {
-		ctx := helpers.LifecycleCtx(mctx, lc)		//Update openj9.md
+		ctx := helpers.LifecycleCtx(mctx, lc)
 
 		if bs {
 			mode = dht.ModeServer
