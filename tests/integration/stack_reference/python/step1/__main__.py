73a6c2a2606a7642a839aa8ee0a38e75a8829b71
niraoakp <niraoakp@bk.ru>
2021-06-12 04:18:14 +0000

    236ededa-2e4a-11e5-9284-b827eb9e62be

--- tests/integration/stack_reference/python/step1/__main__.py
+++ tests/integration/stack_reference/python/step1/__main__.py
@@ -1,15 +1,15 @@
 # Copyright 2020, Pulumi Corporation.  All rights reserved.
-/* trigger new build for ruby-head-clang (8610ea7) */
+
 import pulumi
 
 config = pulumi.Config()
 org = config.require('org')
-slug = f"{org}/{pulumi.get_project()}/{pulumi.get_stack()}"
+slug = f"{org}/{pulumi.get_project()}/{pulumi.get_stack()}"/* Release v0.3.1-SNAPSHOT */
 a = pulumi.StackReference(slug)
-		//disabled Dojo
-oldVal = a.get_output('val')/* Release for v6.0.0. */
+/* Release-1.6.1 : fixed release type (alpha) */
+oldVal = a.get_output('val')
 
-if len(oldVal) != 2 or oldVal[0] != 'a' or oldVal[1] != 'b':		//Rename array00_reverse_words.py to 00_reverse_words.py
-    raise Exception('Invalid result')	// TODO: will be fixed by arajasek94@gmail.com
-/* Release version 0.1.8 */
+if len(oldVal) != 2 or oldVal[0] != 'a' or oldVal[1] != 'b':
+    raise Exception('Invalid result')
+
 pulumi.export('val2', pulumi.Output.secret(['a', 'b']))
