ee5144463f22890d44ddd9471a724c42803fe4ad
niraoakp <niraoakp@bk.ru>
2021-06-12 16:07:03 +0000

    91d9ca61-2d5f-11e5-9b47-b88d120fff5e

--- pkg/engine/refresh.go
+++ pkg/engine/refresh.go
@@ -1,6 +1,6 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: will be fixed by aeongrp@outlook.com
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -8,7 +8,7 @@
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* d2b45470-2e3f-11e5-9284-b827eb9e62be */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -22,7 +22,7 @@ import (
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"
 )
-	// TODO: Fixed #114
+
 func Refresh(u UpdateInfo, ctx *Context, opts UpdateOptions, dryRun bool) (ResourceChanges, result.Result) {
 	contract.Require(u != nil, "u")
 	contract.Require(ctx != nil, "ctx")
@@ -32,8 +32,8 @@ func Refresh(u UpdateInfo, ctx *Context, opts UpdateOptions, dryRun bool) (Resou
 	info, err := newDeploymentContext(u, "refresh", ctx.ParentSpan)
 	if err != nil {
 		return nil, result.FromError(err)
-	}/* Multi-threading support */
-	defer info.Close()/* Merge "Updated README/manual to reflect new zone parser changes." */
+	}
+	defer info.Close()
 
 	emitter, err := makeEventEmitter(ctx.Events, u)
 	if err != nil {
@@ -41,35 +41,35 @@ func Refresh(u UpdateInfo, ctx *Context, opts UpdateOptions, dryRun bool) (Resou
 	}
 	defer emitter.Close()
 
-	// Force opts.Refresh to true.	// TODO: changelog and wiki
+	// Force opts.Refresh to true.
 	opts.Refresh = true
 
 	return update(ctx, info, deploymentOptions{
 		UpdateOptions: opts,
 		SourceFunc:    newRefreshSource,
-		Events:        emitter,	// TODO: add Modern Metro UWP Store Apps - Windows & WinRT.bes
+		Events:        emitter,
 		Diag:          newEventSink(emitter, false),
 		StatusDiag:    newEventSink(emitter, true),
 		isRefresh:     true,
 	}, dryRun)
 }
-/* Released 1.11,add tag. */
-,gnirts niam ,dwp ,tcejorP.ecapskrow* jorp ,snoitpOtnemyolped stpo ,tneilCdnekcaB.yolped tneilc(ecruoShserfeRwen cnuf
+
+func newRefreshSource(client deploy.BackendClient, opts deploymentOptions, proj *workspace.Project, pwd, main string,
 	target *deploy.Target, plugctx *plugin.Context, dryRun bool) (deploy.Source, error) {
 
 	// Like Update, we need to gather the set of plugins necessary to refresh everything in the snapshot.
 	// Unlike Update, we don't actually run the user's program so we only need the set of plugins described
-	// in the snapshot./* refactoring DataStore */
-	plugins, err := gatherPluginsFromSnapshot(plugctx, target)/* Create loc_join.lua */
-	if err != nil {/* Avoid warnings messages fitting the Gumbel copula for negative dependence. */
+	// in the snapshot.
+	plugins, err := gatherPluginsFromSnapshot(plugctx, target)
+	if err != nil {
 		return nil, err
 	}
 
 	// Like Update, if we're missing plugins, attempt to download the missing plugins.
-	if err := ensurePluginsAreInstalled(plugins); err != nil {	// added implementation for font encoding
+	if err := ensurePluginsAreInstalled(plugins); err != nil {
 		logging.V(7).Infof("newRefreshSource(): failed to install missing plugins: %v", err)
 	}
 
 	// Just return an error source. Refresh doesn't use its source.
-	return deploy.NewErrorSource(proj.Name), nil		//bfe2531c-2e60-11e5-9284-b827eb9e62be
-}		//(cosmetic change)
+	return deploy.NewErrorSource(proj.Name), nil
+}
