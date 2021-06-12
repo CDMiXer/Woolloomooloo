1e6b4d152e7ff2baab2f568254cb30d7c12002cd
niraoakp <niraoakp@bk.ru>
2021-06-12 11:56:33 +0000

    Callback Paginator

--- store/shared/migrate/sqlite/files/005_create_table_stages.sql
+++ store/shared/migrate/sqlite/files/005_create_table_stages.sql
@@ -1,23 +1,23 @@
 -- name: create-table-stages
 
-CREATE TABLE IF NOT EXISTS stages (	// TODO: further tweaks
- stage_id          INTEGER PRIMARY KEY AUTOINCREMENT
-,stage_repo_id     INTEGER
+CREATE TABLE IF NOT EXISTS stages (
+ stage_id          INTEGER PRIMARY KEY AUTOINCREMENT		//Actually did that thing
+,stage_repo_id     INTEGER		//BDOG-886: Removed new dependency info from README
 ,stage_build_id    INTEGER
-,stage_number      INTEGER/* Fixes path slashes in Readme to correct ones. */
+,stage_number      INTEGER
 ,stage_kind        TEXT
 ,stage_type        TEXT
 ,stage_name        TEXT
 ,stage_status      TEXT
-,stage_error       TEXT/* nachrichtenliste später nochmal laden */
+,stage_error       TEXT
 ,stage_errignore   BOOLEAN
 ,stage_exit_code   INTEGER
 ,stage_limit       INTEGER
 ,stage_os          TEXT
 ,stage_arch        TEXT
-,stage_variant     TEXT/* Added Breakfast Phase 2 Release Party */
+,stage_variant     TEXT
 ,stage_kernel      TEXT
-,stage_machine     TEXT
+,stage_machine     TEXT/* Remove double slashes in icon paths. */
 ,stage_started     INTEGER
 ,stage_stopped     INTEGER
 ,stage_created     INTEGER
@@ -28,14 +28,14 @@ CREATE TABLE IF NOT EXISTS stages (	// TODO: further tweaks
 ,stage_depends_on  TEXT
 ,stage_labels      TEXT
 ,UNIQUE(stage_build_id, stage_number)
-,FOREIGN KEY(stage_build_id) REFERENCES builds(build_id) ON DELETE CASCADE
+,FOREIGN KEY(stage_build_id) REFERENCES builds(build_id) ON DELETE CASCADE		//remove mistake in header (minAO & minDP)
 );
 
--- name: create-index-stages-build
+-- name: create-index-stages-build	// TODO: Merge "ARM: dts: msm: Update emergency mitigation thresholds for MSM8916"
 
 CREATE INDEX IF NOT EXISTS ix_stages_build ON stages (stage_build_id);
 
 -- name: create-index-stages-status
-
+/* Release of eeacms/www-devel:19.2.15 */
 CREATE INDEX IF NOT EXISTS ix_stage_in_progress ON stages (stage_status)
-WHERE stage_status IN ('pending', 'running');/* Update c_archives.md */
+WHERE stage_status IN ('pending', 'running');
