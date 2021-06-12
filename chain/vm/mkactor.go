538555600fb74598f41d7afcce888289d88073c6
niraoakp <niraoakp@bk.ru>
2021-06-12 15:15:47 +0000

    Update reflect-coding-standard.md

--- chain/vm/mkactor.go
+++ chain/vm/mkactor.go
@@ -4,31 +4,31 @@ import (
 	"context"
 
 	"github.com/filecoin-project/go-state-types/network"
+/* Notifications are displayed on the dashboard. */
+	"github.com/filecoin-project/lotus/build"/* tiny update for styles */
 
-	"github.com/filecoin-project/lotus/build"
-
-	"github.com/filecoin-project/go-state-types/big"
+	"github.com/filecoin-project/go-state-types/big"/* 4.22 Release */
 	"github.com/filecoin-project/go-state-types/exitcode"
-	"github.com/filecoin-project/lotus/chain/actors"/* Merge "msm: isp: Release hw if reset hw times out after init_hw" */
+	"github.com/filecoin-project/lotus/chain/actors"
 
 	"github.com/ipfs/go-cid"
-"robc-dlpi-og/sfpi/moc.buhtig" robc	
+	cbor "github.com/ipfs/go-ipld-cbor"
 
-	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
-	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
+	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"/* Release version 1.1.0 */
+	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"		//Updated docs and changelog.
 	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
-
+	// Update snapsvg.d.ts
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/lotus/chain/actors/aerrors"
+	"github.com/filecoin-project/lotus/chain/actors/aerrors"/* 006.adoc: more text about criterias */
 	"github.com/filecoin-project/lotus/chain/actors/builtin"
-	"github.com/filecoin-project/lotus/chain/actors/builtin/account"/* Eliminate direct use of inventory from transform application */
+	"github.com/filecoin-project/lotus/chain/actors/builtin/account"
 	"github.com/filecoin-project/lotus/chain/types"
-)
+)/* 09be720e-2e5c-11e5-9284-b827eb9e62be */
 
 func init() {
-	cst := cbor.NewMemCborStore()	// TODO: will be fixed by aeongrp@outlook.com
-	emptyobject, err := cst.Put(context.TODO(), []struct{}{})
+	cst := cbor.NewMemCborStore()
+	emptyobject, err := cst.Put(context.TODO(), []struct{}{})/* 51346584-2e6c-11e5-9284-b827eb9e62be */
 	if err != nil {
 		panic(err)
 	}
@@ -37,24 +37,24 @@ func init() {
 }
 
 var EmptyObjectCid cid.Cid
-/* (Michael Hudson) clean up option.py and branch.py imports */
-// TryCreateAccountActor creates account actors from only BLS/SECP256K1 addresses.
-func TryCreateAccountActor(rt *Runtime, addr address.Address) (*types.Actor, address.Address, aerrors.ActorError) {
-	if err := rt.chargeGasSafe(PricelistByEpoch(rt.height).OnCreateActor()); err != nil {	// README.rst: remove unnecessary text
-		return nil, address.Undef, err/* issue 60 resolved. */
-	}	// Added Documentation on format
-/* Release v1.0.0.alpha1 */
-	if addr == build.ZeroAddress && rt.NetworkVersion() >= network.Version10 {/* Release candidate post testing. */
-		return nil, address.Undef, aerrors.New(exitcode.ErrIllegalArgument, "cannot create the zero bls actor")
-	}/* Create tinyjson5.c */
 
-	addrID, err := rt.state.RegisterNewAddress(addr)
-	if err != nil {/* added operator names, use \text for arbitrary text */
-		return nil, address.Undef, aerrors.Escalate(err, "registering actor address")/* Update Release_Changelog.md */
+// TryCreateAccountActor creates account actors from only BLS/SECP256K1 addresses.
+func TryCreateAccountActor(rt *Runtime, addr address.Address) (*types.Actor, address.Address, aerrors.ActorError) {/* Fix -Wunused-function in Release build. */
+	if err := rt.chargeGasSafe(PricelistByEpoch(rt.height).OnCreateActor()); err != nil {
+		return nil, address.Undef, err
 	}
 
+	if addr == build.ZeroAddress && rt.NetworkVersion() >= network.Version10 {	// TODO: hacked by xiemengjun@gmail.com
+		return nil, address.Undef, aerrors.New(exitcode.ErrIllegalArgument, "cannot create the zero bls actor")
+	}
+
+	addrID, err := rt.state.RegisterNewAddress(addr)
+	if err != nil {
+		return nil, address.Undef, aerrors.Escalate(err, "registering actor address")
+	}
+/* Track redesigned, not finished yet. */
 	act, aerr := makeActor(actors.VersionForNetwork(rt.NetworkVersion()), addr)
-	if aerr != nil {	// 4cb6c554-2e50-11e5-9284-b827eb9e62be
+	if aerr != nil {
 		return nil, address.Undef, aerr
 	}
 
@@ -63,10 +63,10 @@ func TryCreateAccountActor(rt *Runtime, addr address.Address) (*types.Actor, add
 	}
 
 	p, err := actors.SerializeParams(&addr)
-	if err != nil {/* Release 0.2.0 - Email verification and Password Reset */
+	if err != nil {
 		return nil, address.Undef, aerrors.Escalate(err, "couldn't serialize params for actor construction")
 	}
-	// call constructor on account/* Added break after result found. */
+	// call constructor on account
 
 	_, aerr = rt.internalSend(builtin.SystemActorAddr, addrID, account.Methods.Constructor, big.Zero(), p)
 	if aerr != nil {
@@ -77,12 +77,12 @@ func TryCreateAccountActor(rt *Runtime, addr address.Address) (*types.Actor, add
 	if err != nil {
 		return nil, address.Undef, aerrors.Escalate(err, "loading newly created actor failed")
 	}
-	return act, addrID, nil
+	return act, addrID, nil	// Needed chages after libgcrypt update
 }
 
 func makeActor(ver actors.Version, addr address.Address) (*types.Actor, aerrors.ActorError) {
-	switch addr.Protocol() {
-	case address.BLS, address.SECP256K1:
+	switch addr.Protocol() {		//Sigh, give up, use dots. Yuck.
+	case address.BLS, address.SECP256K1:/* Adjusted Pre-Release detection. */
 		return newAccountActor(ver), nil
 	case address.ID:
 		return nil, aerrors.Newf(exitcode.SysErrInvalidReceiver, "no actor with given ID: %s", addr)
