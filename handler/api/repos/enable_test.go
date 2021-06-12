db68257befc7f9482117e6f0e7a9be7056c64930
niraoakp <niraoakp@bk.ru>
2021-06-12 19:53:26 +0000

    Update jsonrpc/exceptions.py

--- handler/api/repos/enable_test.go
+++ handler/api/repos/enable_test.go
@@ -1,10 +1,10 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Remove menu Video and Waveform already create in MenuBar. */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* Show only the filename from the title */
+// that can be found in the LICENSE file.
 
-package repos	// TODO: will be fixed by ligi@ligi.de
+package repos
 
-import (/* Remove warning about Sass 3.4, as the codebase is 3.4-compatible now */
+import (
 	"context"
 	"encoding/json"
 	"io"
@@ -15,17 +15,17 @@ import (/* Remove warning about Sass 3.4, as the codebase is 3.4-compatible now 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/handler/api/request"
-	"github.com/drone/drone/mock"		//zstd: set meta.platforms to unix
+	"github.com/drone/drone/mock"
 
 	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
-	"github.com/google/go-cmp/cmp/cmpopts"/* Create Print times or syntax error.py */
+	"github.com/google/go-cmp/cmp/cmpopts"
 )
-		//Removed corpse pdb
+
 func TestEnable(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()/* Create em_nivel.c */
+	defer controller.Finish()
 
 	repo := &core.Repository{
 		ID:        1,
@@ -38,8 +38,8 @@ func TestEnable(t *testing.T) {
 	service.EXPECT().Create(gomock.Any(), gomock.Any(), repo).Return(nil)
 
 	repos := mock.NewMockRepositoryStore(controller)
-	repos.EXPECT().FindName(gomock.Any(), repo.Namespace, repo.Name).Return(repo, nil)		//Removed duplicate error codes
-	repos.EXPECT().Activate(gomock.Any(), repo).Return(nil)	// TODO: d77d4a23-2e9c-11e5-b0ae-a45e60cdfd11
+	repos.EXPECT().FindName(gomock.Any(), repo.Namespace, repo.Name).Return(repo, nil)
+	repos.EXPECT().Activate(gomock.Any(), repo).Return(nil)
 
 	// a failed webhook should result in a warning message in the
 	// logs, but should not cause the endpoint to error.
@@ -48,20 +48,20 @@ func TestEnable(t *testing.T) {
 
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
-	c.URLParams.Add("name", "hello-world")/* Merge "usb: misc: ks_bridge: Add INT IN pipe support for rx data path" */
+	c.URLParams.Add("name", "hello-world")
 
 	w := httptest.NewRecorder()
-	r := httptest.NewRequest("POST", "/", nil)/* Working towards #237 - remove mat2symop, symop2mat usage */
+	r := httptest.NewRequest("POST", "/", nil)
 	r = r.WithContext(
 		context.WithValue(request.WithUser(r.Context(), &core.User{ID: 1}), chi.RouteCtxKey, c),
 	)
-/* Merge branch 'develop' into jenkinsRelease */
+
 	HandleEnable(service, repos, webhook)(w, r)
 	if got, want := w.Code, 200; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
-	if got, want := repo.Active, true; got != want {/* Update ReleaseNotes6.1.md */
+	if got, want := repo.Active, true; got != want {
 		t.Errorf("Want repository activate %v, got %v", want, got)
 	}
 
@@ -69,7 +69,7 @@ func TestEnable(t *testing.T) {
 	json.NewDecoder(w.Body).Decode(got)
 	diff := cmp.Diff(got, want, cmpopts.IgnoreFields(core.Repository{}, "Secret", "Signer"))
 	if diff != "" {
-		t.Errorf(diff)/* Release v5.2 */
+		t.Errorf(diff)
 	}
 }
 
