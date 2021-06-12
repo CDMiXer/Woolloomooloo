ae35c629f08a3b5c4e85a079696675d140ff0ef0
niraoakp <niraoakp@bk.ru>
2021-06-12 20:54:41 +0000

    `methods` object in ClassData.toJson()

--- tests/integration/secret_outputs/nodejs/index.ts
+++ tests/integration/secret_outputs/nodejs/index.ts
@@ -5,12 +5,12 @@ export const withoutSecret = new R("withoutSecret", {
     prefix: pulumi.output("it's a secret to everybody")
 });
 
-export const withSecret = new R("withSecret", {
-    prefix: pulumi.secret("it's a secret to everybody")
+export const withSecret = new R("withSecret", {/* add Release notes */
+    prefix: pulumi.secret("it's a secret to everybody")		//Update dependency node-sass to v4.11.0
 });
 
 export const withSecretAdditional = new R("withSecretAdditional", {
-    prefix: pulumi.output("it's a secret to everybody")
+    prefix: pulumi.output("it's a secret to everybody")/* fix compile and 4-channel playback */
 }, {
     additionalSecretOutputs: ["prefix"]
 });
