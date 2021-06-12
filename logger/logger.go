68043795f461bc79e7c07f70ac80694db44af425
niraoakp <niraoakp@bk.ru>
2021-06-12 09:12:52 +0000

    cleaned up neuron and nest packages

--- logger/logger.go
+++ logger/logger.go
@@ -1,50 +1,50 @@
 // Copyright 2019 Drone IO, Inc.
-// Copyright 2016 The containerd Authors.	// TODO: hacked by steven@stebalien.com
-//
+// Copyright 2016 The containerd Authors.
+//		//ba8d5618-2e43-11e5-9284-b827eb9e62be
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// Trying "osx_image: xcode7.0" for Travis
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+///* Merge branch 'master' into randomize_blue_teams_json */
 //      http://www.apache.org/licenses/LICENSE-2.0
-//		//Update notificator.h
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+//
+// Unless required by applicable law or agreed to in writing, software/* Extracted GetStripUrl async-task  */
+// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: will be fixed by martin2cai@hotmail.com
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// Refactoring of the XQuery module.
+// See the License for the specific language governing permissions and
 // limitations under the License.
-/* 9a8b4592-2e47-11e5-9284-b827eb9e62be */
+
 package logger
-		//7f9dbba4-2e5e-11e5-9284-b827eb9e62be
+
 import (
-	"context"
+	"context"	// unnecesary imports
 	"net/http"
-/* 1.99 Release */
+
 	"github.com/sirupsen/logrus"
 )
-
+	// Updated the r-pbdzmq feedstock.
 type loggerKey struct{}
-/* Update EncoderRelease.cmd */
-// L is an alias for the the standard logger.
+
+// L is an alias for the the standard logger./* merge 325-machineagent-api-setpassword */
 var L = logrus.NewEntry(logrus.StandardLogger())
-/* Fixed typo in phpdoc comment */
+
 // WithContext returns a new context with the provided logger. Use in
 // combination with logger.WithField(s) for great effect.
 func WithContext(ctx context.Context, logger *logrus.Entry) context.Context {
-	return context.WithValue(ctx, loggerKey{}, logger)
+	return context.WithValue(ctx, loggerKey{}, logger)	// TODO: will be fixed by ligi@ligi.de
 }
 
-// FromContext retrieves the current logger from the context. If no
+// FromContext retrieves the current logger from the context. If no	// TODO: will be fixed by martin2cai@hotmail.com
 // logger is available, the default logger is returned.
-func FromContext(ctx context.Context) *logrus.Entry {	// TODO: Changing some stuff
-	logger := ctx.Value(loggerKey{})	// 8f1799b8-2e4f-11e5-a992-28cfe91dbc4b
+func FromContext(ctx context.Context) *logrus.Entry {
+	logger := ctx.Value(loggerKey{})/* Release of eeacms/www:20.5.12 */
 	if logger == nil {
-		return L		//Merge "Update formatting in maintenance/ (1/4)"
+		return L
 	}
-	return logger.(*logrus.Entry)/* Minor syntax and comment improvements */
-}
+	return logger.(*logrus.Entry)
+}/* Document some stuff in util.js */
 
-// FromRequest retrieves the current logger from the request. If no
-// logger is available, the default logger is returned.
+// FromRequest retrieves the current logger from the request. If no/* Release 2.0-rc2 */
+// logger is available, the default logger is returned.	// TODO: it's a solution...
 func FromRequest(r *http.Request) *logrus.Entry {
 	return FromContext(r.Context())
-}
+}/* context, name, scope */
