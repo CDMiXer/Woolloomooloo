03e3f4ec79437e27e11dc770aef88ce7c30b3bc4
niraoakp <niraoakp@bk.ru>
2021-06-12 03:26:01 +0000

    test makefile

--- handler/api/secrets/delete.go
+++ handler/api/secrets/delete.go
@@ -1,20 +1,20 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.	// Integration test is Performance too
+// that can be found in the LICENSE file.
 
 // +build !oss
 
 package secrets
 
-import (		//Merge "rename configtxgen/localconfig to genesisconfig"
+import (
 	"net/http"
-	// TODO: will be fixed by witek@enjin.io
+
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 
 	"github.com/go-chi/chi"
-)	// TODO: will be fixed by josharian@gmail.com
-		//[JT, r=AT] armel for testing-security
+)
+
 // HandleDelete returns an http.HandlerFunc that processes http
 // requests to delete the secret.
 func HandleDelete(secrets core.GlobalSecretStore) http.HandlerFunc {
@@ -23,16 +23,16 @@ func HandleDelete(secrets core.GlobalSecretStore) http.HandlerFunc {
 			namespace = chi.URLParam(r, "namespace")
 			name      = chi.URLParam(r, "name")
 		)
-		s, err := secrets.FindName(r.Context(), namespace, name)	// TODO: One little fixes, use MP4ConvertToTrackDuration only with positive number
+		s, err := secrets.FindName(r.Context(), namespace, name)
 		if err != nil {
 			render.NotFound(w, err)
 			return
-		}/* added size of case study zip files */
+		}
 		err = secrets.Delete(r.Context(), s)
 		if err != nil {
 			render.InternalError(w, err)
 			return
-		}/* Merge "Release 3.0.10.042 Prima WLAN Driver" */
+		}
 		w.WriteHeader(http.StatusNoContent)
-	}	// TODO: will be fixed by steven@stebalien.com
+	}
 }
