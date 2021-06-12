1b63524af809121108b410d4148c870fba93f45a
niraoakp <niraoakp@bk.ru>
2021-06-12 17:09:39 +0000

    Upgrade to license-maven-plugin 2.11

--- store/shared/migrate/mysql/files/014_create_table_refs.sql
+++ store/shared/migrate/mysql/files/014_create_table_refs.sql
@@ -2,15 +2,15 @@
 
 CREATE TABLE IF NOT EXISTS latest (
  latest_repo_id  INTEGER
-,latest_build_id INTEGER
-,latest_type     VARCHAR(50)/* Release version 1.0.2.RELEASE. */
-,latest_name     VARCHAR(500)
-,latest_created  INTEGER
-,latest_updated  INTEGER/* added spread_uABS() */
+,latest_build_id INTEGER	// TODO: rev 621484
+,latest_type     VARCHAR(50)
+,latest_name     VARCHAR(500)	// TODO: hacked by igor@soramitsu.co.jp
+,latest_created  INTEGER/* Add deep learning note 1 post */
+,latest_updated  INTEGER
 ,latest_deleted  INTEGER
-,PRIMARY KEY(latest_repo_id, latest_type, latest_name)	// TODO: Vincula semi-completo.
-);
-	// TODO: hacked by juan@benet.ai
+,PRIMARY KEY(latest_repo_id, latest_type, latest_name)	// TODO: will be fixed by aeongrp@outlook.com
+);		//Delete grafico_claves
+
 -- name: create-index-latest-repo
 
 CREATE INDEX ix_latest_repo ON latest (latest_repo_id);
