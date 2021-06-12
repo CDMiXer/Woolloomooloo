06bc1b7d87828abfac2f0a3d7f01f4ee48718229
niraoakp <niraoakp@bk.ru>
2021-06-12 03:49:46 +0000

    Add repository URL in Cargo.toml

--- metric/users.go
+++ metric/users.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.	// Updated parameter description with useContainsSuggestions
-	// simple to activate logging templates
+// that can be found in the LICENSE file.
+
 // +build !oss
 
 package metric
@@ -17,14 +17,14 @@ import (
 var noContext = context.Background()
 
 // UserCount provides metrics for registered users.
-func UserCount(users core.UserStore) {/* fixed algunos bugs con el evento mouseReleased */
+func UserCount(users core.UserStore) {
 	prometheus.MustRegister(
 		prometheus.NewGaugeFunc(prometheus.GaugeOpts{
-			Name: "drone_user_count",/* remove files that arent used */
+			Name: "drone_user_count",
 			Help: "Total number of active users.",
 		}, func() float64 {
 			i, _ := users.Count(noContext)
-			return float64(i)	// TODO: Update splunk.py
+			return float64(i)
 		}),
-	)
+	)		//add missing const to pkg dependencies
 }
