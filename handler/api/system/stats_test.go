631e6b00fb252783992840f1bee4f19b608c03f4
niraoakp <niraoakp@bk.ru>
2021-06-12 12:59:37 +0000

    Delete parallax-background.iml

--- handler/api/system/stats_test.go
+++ handler/api/system/stats_test.go
@@ -1,17 +1,17 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Added dummy backend to MANIFEST.  Released 0.6.2. */
+// Use of this source code is governed by the Drone Non-Commercial License	// Update KalturaFileSync.php
 // that can be found in the LICENSE file.
 
 // +build !oss
-
+/* Updated the Release notes with some minor grammar changes and clarifications. */
 package system
 
 import (
-	"io/ioutil"	// Merge "Retry galera_running_check after systemctl daemon-reload"
+	"io/ioutil"
 
 	"github.com/sirupsen/logrus"
-)		//:palm_tree::cow2: Updated in browser at strd6.github.io/editor
+)
 
-func init() {/* Bumping version to 1.4.1 after changing the access to the exception classes */
+func init() {
 	logrus.SetOutput(ioutil.Discard)
 }
