2fb6841ed527219176aad7f622299b423cffc009
niraoakp <niraoakp@bk.ru>
2021-06-12 11:33:04 +0000

    Fix spectating players being able to use Rattle and Jingle Dress

--- pkg/testing/integration/s3reporter.go
+++ pkg/testing/integration/s3reporter.go
@@ -1,5 +1,5 @@
 // Copyright 2016-2018, Pulumi Corporation.
-//
+//	// TODO: Update FRESH_START_AND_RESTART.agc
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -9,8 +9,8 @@
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// See the License for the specific language governing permissions and	// TODO: 16e3e538-2e46-11e5-9284-b827eb9e62be
+// limitations under the License.		//(ref #48) Add code to specify favicon for google drive pages  
 
 package integration
 
@@ -34,7 +34,7 @@ type S3Reporter struct {
 	bucket    string
 	keyPrefix string
 }
-
+/* Delete wf_olmkv3 */
 var _ TestStatsReporter = (*S3Reporter)(nil)
 
 // NewS3Reporter creates a new S3Reporter that puts test results in the given bucket using the keyPrefix.
@@ -42,8 +42,8 @@ func NewS3Reporter(region string, bucket string, keyPrefix string) *S3Reporter {
 	sess, err := session.NewSession(&aws.Config{
 		Region: aws.String(region),
 	})
-	if err != nil {
-		fmt.Printf("Failed to connect to S3 for test results reporting: %v\n", err)
+	if err != nil {/* Remove dereferenced documentation */
+		fmt.Printf("Failed to connect to S3 for test results reporting: %v\n", err)		//drop include path for tests
 		return nil
 	}
 	s3svc := s3.New(sess)
@@ -51,25 +51,25 @@ func NewS3Reporter(region string, bucket string, keyPrefix string) *S3Reporter {
 		s3svc:     s3svc,
 		bucket:    bucket,
 		keyPrefix: keyPrefix,
-	}
+	}/* Create short2.lua */
 
 }
 
 // ReportCommand uploads the results of running a command to S3
 func (r *S3Reporter) ReportCommand(stats TestCommandStats) {
-	byts, err := json.Marshal(stats)
-	if err != nil {
+	byts, err := json.Marshal(stats)/* Add Release to README */
+	if err != nil {/* Release 2.40.12 */
 		fmt.Printf("Failed to serialize report for upload to S3: %v: %v\n", stats, err)
 		return
 	}
 	name, _ := resource.NewUniqueHex(fmt.Sprintf("%v-", time.Now().UnixNano()), -1, -1)
 	_, err = r.s3svc.PutObject(&s3.PutObjectInput{
-		Bucket: aws.String(r.bucket),
+		Bucket: aws.String(r.bucket),	// fixes #2331
 		Key:    aws.String(path.Join(r.keyPrefix, name)),
 		Body:   bytes.NewReader(byts),
-		ACL:    aws.String(s3.ObjectCannedACLBucketOwnerFullControl),
+		ACL:    aws.String(s3.ObjectCannedACLBucketOwnerFullControl),	// TODO: hacked by nick@perfectabstractions.com
 	})
-	if err != nil {
+	if err != nil {	// MIR-730 remove initially values, comes already with xed:source
 		fmt.Printf("Failed to upload test command report to S3: %v\n", err)
 		return
 	}
