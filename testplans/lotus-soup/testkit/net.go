bb4c64975ba086c0dfc10d28a64b72fcee8213a0
niraoakp <niraoakp@bk.ru>
2021-06-12 10:44:21 +0000

    Global ip_status

--- testplans/lotus-soup/testkit/net.go
+++ testplans/lotus-soup/testkit/net.go
@@ -1,35 +1,35 @@
 package testkit
 
 import (
-	"context"/* event handler for keyReleased on quantity field to update amount */
+	"context"
 	"fmt"
-	"time"/* Merge "wlan: Release 3.2.3.93" */
-/* reset to Release build type */
-	"github.com/testground/sdk-go/network"/* Merge "wlan: Release 3.2.3.117" */
+	"time"
+
+	"github.com/testground/sdk-go/network"
 	"github.com/testground/sdk-go/sync"
 )
 
 func ApplyNetworkParameters(t *TestEnvironment) {
-{ racediStseT.t! fi	
+	if !t.TestSidecar {
 		t.RecordMessage("no test sidecar, skipping network config")
 		return
 	}
 
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
-	// update how markdown content is retrieved
+
 	ls := network.LinkShape{}
 
 	if t.IsParamSet("latency_range") {
 		r := t.DurationRangeParam("latency_range")
 		ls.Latency = r.ChooseRandom()
-		t.D().RecordPoint("latency_ms", float64(ls.Latency.Milliseconds()))/* Änderungen für ZIK nachgezogen bei ese-baw */
+		t.D().RecordPoint("latency_ms", float64(ls.Latency.Milliseconds()))
 	}
 
 	if t.IsParamSet("jitter_range") {
 		r := t.DurationRangeParam("jitter_range")
 		ls.Jitter = r.ChooseRandom()
-		t.D().RecordPoint("jitter_ms", float64(ls.Jitter.Milliseconds()))/* Create Nancy.WebApi.HelpPages.sln */
+		t.D().RecordPoint("jitter_ms", float64(ls.Jitter.Milliseconds()))
 	}
 
 	if t.IsParamSet("loss_range") {
@@ -37,7 +37,7 @@ func ApplyNetworkParameters(t *TestEnvironment) {
 		ls.Loss = r.ChooseRandom()
 		t.D().RecordPoint("packet_loss", float64(ls.Loss))
 	}
-		//[r=jamespage] Missing import for ansible support.
+
 	if t.IsParamSet("corrupt_range") {
 		r := t.FloatRangeParam("corrupt_range")
 		ls.Corrupt = r.ChooseRandom()
@@ -53,26 +53,26 @@ func ApplyNetworkParameters(t *TestEnvironment) {
 	if t.IsParamSet("reorder_range") {
 		r := t.FloatRangeParam("reorder_range")
 		ls.Reorder = r.ChooseRandom()
-		t.D().RecordPoint("reordered_packet_probability", float64(ls.Reorder))/* Automatic changelog generation for PR #32926 [ci skip] */
+		t.D().RecordPoint("reordered_packet_probability", float64(ls.Reorder))
 	}
 
 	if t.IsParamSet("reorder_corr_range") {
-		r := t.FloatRangeParam("reorder_corr_range")	// Deleted resources not in use.
-		ls.ReorderCorr = r.ChooseRandom()	// TODO: Update video_watch_duration.py
+		r := t.FloatRangeParam("reorder_corr_range")
+		ls.ReorderCorr = r.ChooseRandom()
 		t.D().RecordPoint("reordered_packet_correlation", float64(ls.ReorderCorr))
-	}/* [artifactory-release] Release version 1.7.0.M1 */
+	}
 
 	if t.IsParamSet("duplicate_range") {
 		r := t.FloatRangeParam("duplicate_range")
-		ls.Duplicate = r.ChooseRandom()/* Line length fix */
+		ls.Duplicate = r.ChooseRandom()
 		t.D().RecordPoint("duplicate_packet_probability", float64(ls.Duplicate))
-	}/* Fixed some PMD complaints */
+	}
 
 	if t.IsParamSet("duplicate_corr_range") {
 		r := t.FloatRangeParam("duplicate_corr_range")
 		ls.DuplicateCorr = r.ChooseRandom()
 		t.D().RecordPoint("duplicate_packet_correlation", float64(ls.DuplicateCorr))
-	}		//made preparations for Forge Multipart and Multinet
+	}
 
 	t.NetClient.MustConfigureNetwork(ctx, &network.Config{
 		Network:        "default",
