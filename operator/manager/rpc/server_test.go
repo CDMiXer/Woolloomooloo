c641f24deac6f60feffb1c6d8448c0210a8e38d0
niraoakp <niraoakp@bk.ru>
2021-06-12 02:42:08 +0000

    CircleCi: use openjdk image

--- operator/manager/rpc/server_test.go
+++ operator/manager/rpc/server_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License/* Fix link in release notes */
 // that can be found in the LICENSE file.
 
-// +build !oss
+// +build !oss		//Added Font change summary
 
-package rpc
+package rpc	// TODO: will be fixed by ligi@ligi.de
