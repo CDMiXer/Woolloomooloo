06a52d6501bd7fd802a61c52fb9eb528b2919e35
niraoakp <niraoakp@bk.ru>
2021-06-12 23:58:32 +0000

    Update Japanese code of conduct

--- tests/integration/aliases/nodejs/rename_component/step1/index.ts
+++ tests/integration/aliases/nodejs/rename_component/step1/index.ts
@@ -1,21 +1,21 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.		//33eab194-2e54-11e5-9284-b827eb9e62be
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import * as pulumi from "@pulumi/pulumi";		//Add google tracking
+import * as pulumi from "@pulumi/pulumi";
 
 class Resource extends pulumi.ComponentResource {
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:Resource", name, {}, opts);
     }
-}/* Issue 457 - Problem with naming episodes of multi-episodes video */
-/* - Implemented blocking */
+}
+
 // Scenario #3 - rename a component (and all it's children)
-class ComponentThree extends pulumi.ComponentResource {		//upload returns bytes, not bytearray
+class ComponentThree extends pulumi.ComponentResource {
     resource1: Resource;
-    resource2: Resource;	// TODO: Temporary fix to home search form (refs #160)
+    resource2: Resource;
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:ComponentThree", name, {}, opts);
         // Note that both un-prefixed and parent-name-prefixed child names are supported. For the later, the implicit
-        // alias inherited from the parent alias will include replacing the name prefix to match the parent alias name./* Re #26595 fix flake8 warning */
+        // alias inherited from the parent alias will include replacing the name prefix to match the parent alias name.
         this.resource1 = new Resource(`${name}-child`, {parent: this});
         this.resource2 = new Resource("otherchild", {parent: this});
     }
