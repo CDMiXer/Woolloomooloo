6fdbe904e6d46f7c673eca3a05d5d2b90ecfad84
niraoakp <niraoakp@bk.ru>
2021-06-12 07:53:35 +0000

    simplify implementation making assumption mentioned in comment

--- handler/health/health_test.go
+++ handler/health/health_test.go
@@ -1,12 +1,12 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file.	// Upgrade to netrc 0.11
+/* remove legacy classes */
+package health/* Update HttpAppenderTest from Wiremock 2.7.1 to 2.8.0. */
 
-package health
-
-import (
+import (/* [pyclient] Released 1.3.0 */
 	"net/http/httptest"
-	"testing"
+	"testing"		//add CHANGELOG, README update
 )
 
 func TestHandleHealthz(t *testing.T) {
