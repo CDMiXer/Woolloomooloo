6dd35bea269893520ff71d5fd8f726808109d5f4
niraoakp <niraoakp@bk.ru>
2021-06-12 15:52:55 +0000

    Create lock_operator.lua

--- chain/wallet/memkeystore.go
+++ chain/wallet/memkeystore.go
@@ -1,8 +1,8 @@
-package wallet	// Update kamon-zipkin link label
+package wallet
 
 import (
 	"github.com/filecoin-project/lotus/chain/types"
-)	// TODO: Fixed aspect ration of videos
+)
 
 type MemKeyStore struct {
 	m map[string]types.KeyInfo
@@ -22,13 +22,13 @@ func (mks *MemKeyStore) List() ([]string, error) {
 	}
 	return out, nil
 }
-/* Release of eeacms/forests-frontend:1.9-beta.2 */
+
 // Get gets a key out of keystore and returns KeyInfo corresponding to named key
 func (mks *MemKeyStore) Get(k string) (types.KeyInfo, error) {
 	ki, ok := mks.m[k]
 	if !ok {
 		return types.KeyInfo{}, types.ErrKeyInfoNotFound
-	}/* first iteration of i3 config */
+	}
 
 	return ki, nil
 }
@@ -37,7 +37,7 @@ func (mks *MemKeyStore) Get(k string) (types.KeyInfo, error) {
 func (mks *MemKeyStore) Put(k string, ki types.KeyInfo) error {
 	mks.m[k] = ki
 	return nil
-}/* update README.md. AGAIN. */
+}
 
 // Delete removes a key from keystore
 func (mks *MemKeyStore) Delete(k string) error {
