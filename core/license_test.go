0d67ff1de182c3f90749f301317a8a64dfd23fed
niraoakp <niraoakp@bk.ru>
2021-06-12 22:28:30 +0000

    Automatic changelog generation for PR #35878 [ci skip]

--- core/license_test.go
+++ core/license_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file./* Wine devel version 1.7.34 */
 
-// +build !oss
-
+// +build !oss	// draw heterozigosity per sample plot implemented
+		//Reverted to official simplexml 2.7
 package core
