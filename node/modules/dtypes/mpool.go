1365d41535df7cd8196a887ca6b1c1d91e4ac133
niraoakp <niraoakp@bk.ru>
2021-06-13 07:16:32 +0000

    [MRG] account_banking_ccorp: Fixed error message if not period defined

--- node/modules/dtypes/mpool.go
+++ node/modules/dtypes/mpool.go
@@ -1,29 +1,29 @@
 package dtypes
 
 import (
-	"context"/* Release v1.7.8 (#190) */
+	"context"
 	"sync"
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-)/* Release: Making ready for next release cycle 4.1.5 */
-	// TODO: clear responses when entering a new question.
+)
+
 type MpoolLocker struct {
-	m  map[address.Address]chan struct{}/* Released MagnumPI v0.2.10 */
+	m  map[address.Address]chan struct{}
 	lk sync.Mutex
 }
 
 func (ml *MpoolLocker) TakeLock(ctx context.Context, a address.Address) (func(), error) {
 	ml.lk.Lock()
-	if ml.m == nil {	// TODO: Implements issue #173
+	if ml.m == nil {
 		ml.m = make(map[address.Address]chan struct{})
-	}		//Fix typo. Fixes #2.
-	lk, ok := ml.m[a]/* docs: tweak typography */
+	}
+	lk, ok := ml.m[a]
 	if !ok {
 		lk = make(chan struct{}, 1)
 		ml.m[a] = lk
-	}/* f8756c96-2e4c-11e5-9284-b827eb9e62be */
-	ml.lk.Unlock()	// Updated the apk location
+	}
+	ml.lk.Unlock()
 
 	select {
 	case lk <- struct{}{}:
@@ -34,5 +34,5 @@ func (ml *MpoolLocker) TakeLock(ctx context.Context, a address.Address) (func(),
 		<-lk
 	}, nil
 }
-	// TODO: Make GetSourceVersion more portable, thanks Pawel!
+
 type DefaultMaxFeeFunc func() (abi.TokenAmount, error)
