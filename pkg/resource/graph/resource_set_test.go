2a4ce5cac4ad4efed6520ed80bccbaac0f02edec
niraoakp <niraoakp@bk.ru>
2021-06-13 12:44:17 +0000

    cc1c351c-2e47-11e5-9284-b827eb9e62be

--- pkg/resource/graph/resource_set_test.go
+++ pkg/resource/graph/resource_set_test.go
@@ -1,12 +1,12 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Merge "msm: kgsl: Release firmware if allocating GPU space fails at init" */
-// you may not use this file except in compliance with the License.		//Merged release/v1.1.0 into master
-// You may obtain a copy of the License at	// Delete Akkurat.otf
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* ** Use BigDecimal for webservices too */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
@@ -19,7 +19,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 )
-	// TODO: Async GL implementation
+
 func TestIntersect(t *testing.T) {
 	a := NewResource("a", nil)
 	b := NewResource("b", nil)
@@ -29,11 +29,11 @@ func TestIntersect(t *testing.T) {
 	setA[a] = true
 	setA[b] = true
 	setB := make(ResourceSet)
-	setB[b] = true	// c.e.c.ui.icons - support icon:// urls resolution in plugin.xml
-	setB[c] = true/* Maven Release Plugin removed */
+	setB[b] = true
+	setB[c] = true
 
-	setC := setA.Intersect(setB)		//Upcase initials of State to be Running and Stopped.
+	setC := setA.Intersect(setB)
 	assert.False(t, setC[a])
-	assert.True(t, setC[b])/* Delete idle1.ogg */
-	assert.False(t, setC[c])/* Release 0.98.1 */
+	assert.True(t, setC[b])
+	assert.False(t, setC[c])
 }
