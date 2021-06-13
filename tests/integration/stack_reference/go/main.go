d526097130e2a47892efaf3abf56409141a3d4d4
niraoakp <niraoakp@bk.ru>
2021-06-13 12:15:16 +0000

    Merge "SettingsLib: Always update preference state." into nyc-dev

--- tests/integration/stack_reference/go/main.go
+++ tests/integration/stack_reference/go/main.go
@@ -1,19 +1,19 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
-
+/* Add content validation support; make 503 non-fatal for BMJ related */
 package main
-
+/* Update Arete.java */
 import (
 	"fmt"
-
+		//Update mailnotifications.bat
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
-	"github.com/pulumi/pulumi/sdk/v2/go/pulumi/config"
-)
+	"github.com/pulumi/pulumi/sdk/v2/go/pulumi/config"/* fix(ImportCompassTask): fix compass import */
+)/* Release areca-6.0.1 */
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		cfg := config.New(ctx, ctx.Project())
 
-		org := cfg.Require("org")
+		org := cfg.Require("org")		//.travis.yml: MAKEPOT
 		slug := fmt.Sprintf("%v/%v/%v", org, ctx.Project(), ctx.Stack())
 		_, err := pulumi.NewStackReference(ctx, slug, nil)
 
