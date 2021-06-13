bed710664f42c0ea057cb1bfd328fef96b2bb242
niraoakp <niraoakp@bk.ru>
2021-06-13 23:20:53 +0000

    Delete Smoothing.mexw64.pdb

--- tests/integration/delete_before_create/step3/index.ts
+++ tests/integration/delete_before_create/step3/index.ts
@@ -1,16 +1,16 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import { Resource } from "./resource";
-/* [readme] SIMBODY_STANDARD_11 is on by default. */
-// The changing of a.state causes base to be DBR replaced. This in turn
+
+// The changing of a.state causes base to be DBR replaced. This in turn/* XML list of first and last names. */
 // causes the deletion of b and e eagerly. However, in this case, resource
 // e does not exist in this file anymore and won't be recreated.
 // The planner should execute these steps (in this exact order):
-//   1. DeleteReplacement Dependent-2/* Prepare Release 0.3.1 */
-//   2. DeleteReplacement Dependent
+//   1. DeleteReplacement Dependent-2	// TODO: modified patient family lists
+//   2. DeleteReplacement Dependent/* Update Release 8.1 black images */
 //   3. DeleteReplacement Base
 //   4. Replace Base
-//   5. CreateReplacement Base
+//   5. CreateReplacement Base/* Release doc for 685 */
 const a = new Resource("base", { uniqueKey: 1, state: 100 });
 
 //   6. Replace Dependent
