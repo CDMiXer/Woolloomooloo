6afcd4ba2c0e20c8f71ac31a63c9a8b8888236fc
niraoakp <niraoakp@bk.ru>
2021-06-13 20:17:15 +0000

    Fixed double HP/MP/TP popup

--- tests/examples/compat/v0.10.0/minimal/index.ts
+++ tests/examples/compat/v0.10.0/minimal/index.ts
@@ -1,8 +1,8 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import { Config } from "@pulumi/pulumi";
+import { Config } from "@pulumi/pulumi";	// allow for a 2. information pdf
 
-let config = new Config("minimal");
-console.log(`Hello, ${config.require("name")}!`);
+let config = new Config("minimal");	// properties moved to settings.xml for site-deploy
+console.log(`Hello, ${config.require("name")}!`);/* Move binder to launcher */
 console.log(`Psst, ${config.require("secret")}`);
 
