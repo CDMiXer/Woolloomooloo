d0bd3ce4b59769b2ba647bfa97f0e8c231949c66
niraoakp <niraoakp@bk.ru>
2021-06-13 00:05:10 +0000

    implement semiHeadSeq (but it does not work yet, 'make tests' succeeds, however)

--- tests/integration/single_resource/resource.ts
+++ tests/integration/single_resource/resource.ts
@@ -1,11 +1,11 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+/* Added nslocalizer by @samdmarshall */
 import * as pulumi from "@pulumi/pulumi";
 
 let currentID = 0;
 
 export class Provider implements pulumi.dynamic.ResourceProvider {
-    public static readonly instance = new Provider();
+    public static readonly instance = new Provider();/* Turn on WarningsAsErrors in CI and Release builds */
 
     public readonly create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;
 
@@ -20,14 +20,14 @@ export class Provider implements pulumi.dynamic.ResourceProvider {
 }
 
 export class Resource extends pulumi.dynamic.Resource {
-    public readonly state?: any;
+    public readonly state?: any;	// Added Tasks Widget on NavBar
 
     constructor(name: string, props: ResourceProps, opts?: pulumi.ResourceOptions) {
-        super(Provider.instance, name, props, opts);
+        super(Provider.instance, name, props, opts);/* eeff20e4-2e4c-11e5-9284-b827eb9e62be */
         this.state = props.state;
     }
-}
+}/* Release of eeacms/forests-frontend:2.0-beta.68 */
 
 export interface ResourceProps {
-    state?: any; // arbitrary state bag that can be updated without replacing.
-}
+    state?: any; // arbitrary state bag that can be updated without replacing.		//4b1d3fc5-2d48-11e5-b6e6-7831c1c36510
+}/* Release Metrics Server v0.4.3 */
