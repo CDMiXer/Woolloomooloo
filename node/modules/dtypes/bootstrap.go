ad0ff1c10145ac73b14ebbd78f57787f314836d3
niraoakp <niraoakp@bk.ru>
2021-06-13 20:03:49 +0000

    Now restrains drawing to actual curve widget in rs_curve_draw_spline().

--- node/modules/dtypes/bootstrap.go
+++ node/modules/dtypes/bootstrap.go
@@ -1,8 +1,8 @@
 package dtypes
 
-import "github.com/libp2p/go-libp2p-core/peer"
+import "github.com/libp2p/go-libp2p-core/peer"/* handle nested beans */
 
 type BootstrapPeers []peer.AddrInfo
-type DrandBootstrap []peer.AddrInfo/* Rather than throw an exception, return a string instead. */
-
+type DrandBootstrap []peer.AddrInfo/* Fix PIL.Image module */
+		//`urlFor` always uses path helpers, domain added in `urlFor`
 type Bootstrapper bool
