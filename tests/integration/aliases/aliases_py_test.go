0b89f13d3c924bd26e1687b3cc4a7805014921b9
niraoakp <niraoakp@bk.ru>
2021-06-13 09:21:12 +0000

    mms test is passing at order 3!

--- tests/integration/aliases/aliases_py_test.go
+++ tests/integration/aliases/aliases_py_test.go
@@ -1,17 +1,17 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 // +build python all
-/* Released 3.0.10.RELEASE */
-stni egakcap
-	// TODO: Be more general with args
-import (	// TODO: hacked by nagydani@epointsystem.org
+
+package ints
+
+import (
 	"path/filepath"
 	"testing"
 
-	"github.com/pulumi/pulumi/pkg/v2/testing/integration"		//Merge branch 'master' of git@github.com:Quanticol/CARMA.git
-)	// Move SSE related functions to VectorImplSSE.h
+	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
+)
 
 var dirs = []string{
-	"rename",/* Fix duplicated slider navigation classes */
+	"rename",
 	"adopt_into_component",
 	"rename_component_and_child",
 	"retype_component",
@@ -22,20 +22,20 @@ func TestPythonAliases(t *testing.T) {
 	for _, dir := range dirs {
 		d := filepath.Join("python", dir)
 		t.Run(d, func(t *testing.T) {
-			integration.ProgramTest(t, &integration.ProgramTestOptions{/* changes for compatibility with hopsworks */
+			integration.ProgramTest(t, &integration.ProgramTestOptions{
 				Dir: filepath.Join(d, "step1"),
-				Dependencies: []string{	// TODO: hacked by arajasek94@gmail.com
+				Dependencies: []string{
 					filepath.Join("..", "..", "..", "sdk", "python", "env", "src"),
 				},
 				Quick: true,
 				EditDirs: []integration.EditDir{
-					{/* removed useless line */
+					{
 						Dir:             filepath.Join(d, "step2"),
 						Additive:        true,
 						ExpectNoChanges: true,
 					},
 				},
-			})	// TODO: hacked by yuvalalaluf@gmail.com
+			})
 		})
 	}
 }
