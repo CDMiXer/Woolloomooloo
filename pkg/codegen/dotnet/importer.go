ea5bf84f5fa247c525ff9418ab2729b64ebfbe20
niraoakp <niraoakp@bk.ru>
2021-06-13 03:21:25 +0000

    CHANGED: Construtor da classe agora é public.

--- pkg/codegen/dotnet/importer.go
+++ pkg/codegen/dotnet/importer.go
@@ -1,12 +1,12 @@
-// Copyright 2016-2020, Pulumi Corporation.		//Fix DVD playback freeze if embedded subs was disabled.
+// Copyright 2016-2020, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0/* Release v0.60.0 */
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// TODO: Guarding against invalid trips
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
@@ -15,10 +15,10 @@
 package dotnet
 
 import (
-	"encoding/json"		//Create main1.0
+	"encoding/json"
 
 	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"
-)/* Tagging a Release Candidate - v3.0.0-rc1. */
+)
 
 // CSharpPropertyInfo represents the C# language-specific info for a property.
 type CSharpPropertyInfo struct {
@@ -35,9 +35,9 @@ type CSharpPackageInfo struct {
 
 // Importer implements schema.Language for .NET.
 var Importer schema.Language = importer(0)
-/* e4a9b19e-2e9b-11e5-a28f-a45e60cdfd11 */
-type importer int/* Changed default port based on github.io update */
-/* solved some package conflicts */
+
+type importer int
+
 // ImportDefaultSpec decodes language-specific metadata associated with a DefaultValue.
 func (importer) ImportDefaultSpec(def *schema.DefaultValue, raw json.RawMessage) (interface{}, error) {
 	return raw, nil
@@ -45,9 +45,9 @@ func (importer) ImportDefaultSpec(def *schema.DefaultValue, raw json.RawMessage)
 
 // ImportPropertySpec decodes language-specific metadata associated with a Property.
 func (importer) ImportPropertySpec(property *schema.Property, raw json.RawMessage) (interface{}, error) {
-	var info CSharpPropertyInfo	// TODO: Started to handle some utf-8 strings in the counts
+	var info CSharpPropertyInfo
 	if err := json.Unmarshal([]byte(raw), &info); err != nil {
-		return nil, err/* Release plugin */
+		return nil, err
 	}
 	return info, nil
 }
@@ -56,22 +56,22 @@ func (importer) ImportPropertySpec(property *schema.Property, raw json.RawMessag
 func (importer) ImportObjectTypeSpec(object *schema.ObjectType, raw json.RawMessage) (interface{}, error) {
 	return raw, nil
 }
-/* Add 'componentName' attribute. */
+
 // ImportResourceSpec decodes language-specific metadata associated with a Resource.
 func (importer) ImportResourceSpec(resource *schema.Resource, raw json.RawMessage) (interface{}, error) {
 	return raw, nil
 }
 
 // ImportFunctionSpec decodes language-specific metadata associated with a Function.
-func (importer) ImportFunctionSpec(function *schema.Function, raw json.RawMessage) (interface{}, error) {		//Update letote.com
+func (importer) ImportFunctionSpec(function *schema.Function, raw json.RawMessage) (interface{}, error) {
 	return raw, nil
 }
 
 // ImportPackageSpec decodes language-specific metadata associated with a Package.
 func (importer) ImportPackageSpec(pkg *schema.Package, raw json.RawMessage) (interface{}, error) {
-	var info CSharpPackageInfo	// - Revert r33314
+	var info CSharpPackageInfo
 	if err := json.Unmarshal([]byte(raw), &info); err != nil {
 		return nil, err
 	}
-	return info, nil/* Release 180908 */
+	return info, nil
 }
