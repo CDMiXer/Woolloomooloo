05ec070c760690d984d32a7dd28f63c46fe47ac3
niraoakp <niraoakp@bk.ru>
2021-06-13 23:46:13 +0000

    remove lang3

--- hack/auto-gen-msg.sh
+++ hack/auto-gen-msg.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -eu -o pipefail
-		//Added init failed as payment error.
+
 echo "# This is an auto-generated file. DO NOT EDIT"
-cat/* Fixed bug with relative paths to CSS images */
+cat
