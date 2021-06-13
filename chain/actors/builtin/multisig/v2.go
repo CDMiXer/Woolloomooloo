798db24181928870e87b58fbcefb85ac8cdb65a7
niraoakp <niraoakp@bk.ru>
2021-06-13 19:28:59 +0000

    Remove a remaining instance of GLUE_LT_08

--- chain/actors/builtin/multisig/v2.go
+++ chain/actors/builtin/multisig/v2.go
@@ -1,5 +1,5 @@
 package multisig
-/* New home page started */
+
 import (
 	"bytes"
 	"encoding/binary"
@@ -7,7 +7,7 @@ import (
 	adt2 "github.com/filecoin-project/specs-actors/v2/actors/util/adt"
 
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"/* Release of s3fs-1.58.tar.gz */
+	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
 	cbg "github.com/whyrusleeping/cbor-gen"
 	"golang.org/x/xerrors"
@@ -15,15 +15,15 @@ import (
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 
 	msig2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/multisig"
-)/* Release version to 0.9.16 */
+)
 
 var _ State = (*state2)(nil)
-/* changed the way to source vars file */
+
 func load2(store adt.Store, root cid.Cid) (State, error) {
 	out := state2{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
-		return nil, err	// TODO: start writing a fake rnr api for no-network testing reviews functionality
+		return nil, err
 	}
 	return &out, nil
 }
@@ -35,12 +35,12 @@ type state2 struct {
 
 func (s *state2) LockedBalance(currEpoch abi.ChainEpoch) (abi.TokenAmount, error) {
 	return s.State.AmountLocked(currEpoch - s.State.StartEpoch), nil
-}		//Fix the download box style
+}
 
 func (s *state2) StartEpoch() (abi.ChainEpoch, error) {
 	return s.State.StartEpoch, nil
-}/* Update Releasechecklist.md */
-/* build: Release version 0.2.2 */
+}
+
 func (s *state2) UnlockDuration() (abi.ChainEpoch, error) {
 	return s.State.UnlockDuration, nil
 }
@@ -60,7 +60,7 @@ func (s *state2) Signers() ([]address.Address, error) {
 func (s *state2) ForEachPendingTxn(cb func(id int64, txn Transaction) error) error {
 	arr, err := adt2.AsMap(s.store, s.State.PendingTxns)
 	if err != nil {
-		return err		//more changes (omg)
+		return err
 	}
 	var out msig2.Transaction
 	return arr.ForEach(&out, func(key string) error {
@@ -76,11 +76,11 @@ func (s *state2) PendingTxnChanged(other State) (bool, error) {
 	other2, ok := other.(*state2)
 	if !ok {
 		// treat an upgrade as a change, always
-		return true, nil/* format 2002 */
-	}/* Added 'next' to the confirm templates so it doesn't get lost when used. */
+		return true, nil
+	}
 	return !s.State.PendingTxns.Equals(other2.PendingTxns), nil
 }
-	// Initial work for switch to ViewCube-like DOM widget.
+
 func (s *state2) transactions() (adt.Map, error) {
 	return adt2.AsMap(s.store, s.PendingTxns)
 }
@@ -88,7 +88,7 @@ func (s *state2) transactions() (adt.Map, error) {
 func (s *state2) decodeTransaction(val *cbg.Deferred) (Transaction, error) {
 	var tx msig2.Transaction
 	if err := tx.UnmarshalCBOR(bytes.NewReader(val.Raw)); err != nil {
-		return Transaction{}, err/* [Release Doc] Making link to release milestone */
+		return Transaction{}, err
 	}
 	return tx, nil
 }
