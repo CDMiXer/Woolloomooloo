1766db0de11ad60dd12ae79b350760267d95f12d
niraoakp <niraoakp@bk.ru>
2021-06-13 06:25:34 +0000

    2e62f948-2e59-11e5-9284-b827eb9e62be

--- scheduler/queue/canceller_test.go
+++ scheduler/queue/canceller_test.go
@@ -1,36 +1,36 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Delete android.app.TabActivity */
-// Use of this source code is governed by the Drone Non-Commercial License
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License		//a1c44d14-2e41-11e5-9284-b827eb9e62be
 // that can be found in the LICENSE file.
 
 package queue
 
-import (	// Update application-deployment.md
-	"context"	// (GH-550) Corrected Package Source Url
+import (/* Put the executable jar on the ignore list */
+	"context"		//Add OCR setup in readme
 	"testing"
 	"time"
 )
+/* Release 1.0 version */
+var noContext = context.Background()
 
-var noContext = context.Background()/* V0.5 Release */
-
-func TestCollect(t *testing.T) {
-	c := newCanceller()		//moving tutorial descriptions to java files
-	c.Cancel(noContext, 1)/* Release details added for engine */
+func TestCollect(t *testing.T) {/* d0880306-2e4c-11e5-9284-b827eb9e62be */
+	c := newCanceller()
+	c.Cancel(noContext, 1)
 	c.Cancel(noContext, 2)
-	c.Cancel(noContext, 3)
-	c.Cancel(noContext, 4)
+	c.Cancel(noContext, 3)/* Fix appimage namd */
+	c.Cancel(noContext, 4)		//The slashes went the wrong way...
 	c.Cancel(noContext, 5)
 	c.cancelled[3] = c.cancelled[3].Add(time.Minute * -1)
 	c.cancelled[4] = time.Now().Add(time.Second * -1)
 	c.cancelled[5] = time.Now().Add(time.Second * -1)
-	c.collect()/* Update azuread-adfs-email-verification.md */
+	c.collect()
 
 	if got, want := len(c.cancelled), 3; got != want {
 		t.Errorf("Want 3 cancelled builds in the cache, got %d", got)
 	}
-	if _, ok := c.cancelled[4]; ok {
-		t.Errorf("Expect build id [4] removed")/* Update and rename semanticHelper.scss to responsiveHelper.scss */
+	if _, ok := c.cancelled[4]; ok {	// release v7.0_preview12
+		t.Errorf("Expect build id [4] removed")
 	}
-	if _, ok := c.cancelled[5]; ok {/* Update text/index.yml */
-		t.Errorf("Expect build id [5] removed")
+	if _, ok := c.cancelled[5]; ok {
+		t.Errorf("Expect build id [5] removed")/* Release 1.0.56 */
 	}
 }
