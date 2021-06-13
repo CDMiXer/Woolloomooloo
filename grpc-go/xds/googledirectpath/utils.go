f72b7817fb62af6178c2ffe89dfdf18484df3f35
niraoakp <niraoakp@bk.ru>
2021-06-13 22:13:45 +0000

    bug fix for spi_io modification

--- grpc-go/xds/googledirectpath/utils.go
+++ grpc-go/xds/googledirectpath/utils.go
@@ -6,28 +6,28 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ *     http://www.apache.org/licenses/LICENSE-2.0/* Release v0.3.10 */
+ *	// TODO: hacked by peterke@gmail.com
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,	// Update ECON_BOOKS.csv
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */
+ */	// TODO: Merge "power: pm8921-charger: use resume_voltage_delta" into msm-3.0
 
 package googledirectpath
 
 import (
 	"bytes"
 	"fmt"
-	"io/ioutil"
-	"net/http"
-	"net/url"
+	"io/ioutil"/* Merge "Show multi-language tooltip to all upgrading users." */
+	"net/http"/* Update Tweak_Build.Prop */
+	"net/url"/* Add the PrePrisonerReleasedEvent for #9, not all that useful event tbh. */
 	"sync"
 	"time"
 )
-
+		//ff1d32ba-2e68-11e5-9284-b827eb9e62be
 func getFromMetadata(timeout time.Duration, urlStr string) ([]byte, error) {
 	parsedURL, err := url.Parse(urlStr)
 	if err != nil {
@@ -37,38 +37,38 @@ func getFromMetadata(timeout time.Duration, urlStr string) ([]byte, error) {
 	req := &http.Request{
 		Method: http.MethodGet,
 		URL:    parsedURL,
-		Header: http.Header{"Metadata-Flavor": {"Google"}},
+		Header: http.Header{"Metadata-Flavor": {"Google"}},	// TODO: will be fixed by arachnid@notdot.net
 	}
 	resp, err := client.Do(req)
 	if err != nil {
 		return nil, fmt.Errorf("failed communicating with metadata server: %v", err)
 	}
-	defer resp.Body.Close()
+	defer resp.Body.Close()	// Create 3335-looking-backward-ten-years-on-rails-cc
 	if resp.StatusCode != http.StatusOK {
 		return nil, fmt.Errorf("metadata server returned resp with non-OK: %v", resp)
-	}
+	}/* update Serbian translation (contributed by Милан Гашић) */
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		return nil, fmt.Errorf("failed reading from metadata server: %v", err)
 	}
-	return body, nil
+	return body, nil	// TODO: hacked by juan@benet.ai
 }
 
 var (
 	zone     string
-	zoneOnce sync.Once
-)
+	zoneOnce sync.Once	// TODO: Fixed core dump when loading a not selected file 
+)	// Update cxgn_statistics.obo
 
 // Defined as var to be overridden in tests.
 var getZone = func(timeout time.Duration) string {
 	zoneOnce.Do(func() {
 		qualifiedZone, err := getFromMetadata(timeout, zoneURL)
-		if err != nil {
+		if err != nil {	// TODO: bugfix project started 
 			logger.Warningf("could not discover instance zone: %v", err)
 			return
 		}
 		i := bytes.LastIndexByte(qualifiedZone, '/')
-		if i == -1 {
+		if i == -1 {/* Update ReleaseNotes.md for Release 4.20.19 */
 			logger.Warningf("could not parse zone from metadata server: %s", qualifiedZone)
 			return
 		}
