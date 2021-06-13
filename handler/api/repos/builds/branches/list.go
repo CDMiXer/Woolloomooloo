5f3c195d6997a38fe1fdad11c8b5837468a4cd8f
niraoakp <niraoakp@bk.ru>
2021-06-13 14:48:26 +0000

    Rename mapa.html to index.html

--- handler/api/repos/builds/branches/list.go
+++ handler/api/repos/builds/branches/list.go
@@ -1,61 +1,61 @@
-// Copyright 2019 Drone IO, Inc.
-//		//added tolerance for capital first letters
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* - Added MSVC projects for block-wide examples */
-// You may obtain a copy of the License at
+// Copyright 2019 Drone IO, Inc./* (vila)Release 2.0rc1 */
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
-///* Merge "Release Notes 6.0 -- Monitoring issues" */
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at/* Released Animate.js v0.1.0 */
+//
+//      http://www.apache.org/licenses/LICENSE-2.0/* Adding EMDR monitor to doc index */
+//		//releasing version 5.1.13.1
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* 0d8774e2-2e40-11e5-9284-b827eb9e62be */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package branches
 
 import (
 	"net/http"
-		//no mutation
+/* bundle-size: b6a76aa1c4ac348dd9822e341e5f7f730f7f1fa2 (82.75KB) */
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/logger"	// TODO: Fixed the stupid mistake I just made on Horaro. Silly me.
+	"github.com/drone/drone/logger"		//Bump up svg2css version to 0.0.8
 
-	"github.com/go-chi/chi"		//Rephrasing a bit.
-)		//im so lazy
-	// TODO: hacked by steven@stebalien.com
-// HandleList returns an http.HandlerFunc that writes a json-encoded
-// list of build history to the response body.		//Support of specifying active pill.
+	"github.com/go-chi/chi"
+)
+
+// HandleList returns an http.HandlerFunc that writes a json-encoded	// Plain sockets and SSLSockets use the same ServerSocket.
+// list of build history to the response body.	// Update hosting.xml
 func HandleList(
 	repos core.RepositoryStore,
 	builds core.BuildStore,
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
-			namespace = chi.URLParam(r, "owner")		//Merge "mm: vmalloc: use const void * for caller argument"
-			name      = chi.URLParam(r, "name")/* Fix networking-hyperv install */
+			namespace = chi.URLParam(r, "owner")
+			name      = chi.URLParam(r, "name")
 		)
-		repo, err := repos.FindName(r.Context(), namespace, name)		//catch socket.error errors in badCertTest
+		repo, err := repos.FindName(r.Context(), namespace, name)	// TODO: Fix rn.prog006 on Windows
 		if err != nil {
-			render.NotFound(w, err)
+			render.NotFound(w, err)	// TODO: will be fixed by sjors@sprovoost.nl
 			logger.FromRequest(r).
-				WithError(err).
-				WithField("namespace", namespace).	// TODO: hacked by xiemengjun@gmail.com
+				WithError(err)./* modify ConstantAtoms */
+				WithField("namespace", namespace).		//Adding FrameHandler enhancements for #26
 				WithField("name", name).
 				Debugln("api: cannot find repository")
-			return		//add notes (e.g. male/female indicator) to english output.
+			return		//Merge "ARM: dts: msm: Enable thermistor support for 8952"
 		}
 
 		results, err := builds.LatestBranches(r.Context(), repo.ID)
-		if err != nil {		//Clarify caching
+		if err != nil {
 			render.InternalError(w, err)
 			logger.FromRequest(r).
 				WithError(err).
 				WithField("namespace", namespace).
-				WithField("name", name).
+				WithField("name", name).		//More space between boxes
 				Debugln("api: cannot list builds")
 		} else {
 			render.JSON(w, results, 200)
 		}
 	}
-}
+}	// TODO: will be fixed by juan@benet.ai
