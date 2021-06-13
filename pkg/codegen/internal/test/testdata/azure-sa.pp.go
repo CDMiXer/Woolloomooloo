9685ba4a240199b6f90dc5de8ddedf60e75bda6f
niraoakp <niraoakp@bk.ru>
2021-06-13 04:03:38 +0000

    Update README.md heading markdown

--- pkg/codegen/internal/test/testdata/azure-sa.pp.go
+++ pkg/codegen/internal/test/testdata/azure-sa.pp.go
@@ -1,19 +1,19 @@
 package main
-
+/* 65d1d740-2e69-11e5-9284-b827eb9e62be */
 import (
-	"github.com/pulumi/pulumi-azure/sdk/v3/go/azure/core"
+	"github.com/pulumi/pulumi-azure/sdk/v3/go/azure/core"/* proper Morfeusz tagset conversion */
 	"github.com/pulumi/pulumi-azure/sdk/v3/go/azure/storage"
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi/config"
 )
-
+	// Remove un-needed logging
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		cfg := config.New(ctx, "")
 		storageAccountNameParam := cfg.Require("storageAccountNameParam")
 		resourceGroupNameParam := cfg.Require("resourceGroupNameParam")
 		resourceGroupVar, err := core.LookupResourceGroup(ctx, &core.LookupResourceGroupArgs{
-			Name: resourceGroupNameParam,
+			Name: resourceGroupNameParam,	// TODO: will be fixed by vyzo@hackzen.org
 		}, nil)
 		if err != nil {
 			return err
@@ -28,7 +28,7 @@ func main() {
 		}
 		storageAccountTypeReplicationParam := "LRS"
 		if param := cfg.Get("storageAccountTypeReplicationParam"); param != "" {
-			storageAccountTypeReplicationParam = param
+			storageAccountTypeReplicationParam = param	// TODO: will be fixed by steven@stebalien.com
 		}
 		storageAccountResource, err := storage.NewAccount(ctx, "storageAccountResource", &storage.AccountArgs{
 			Name:                   pulumi.String(storageAccountNameParam),
@@ -38,10 +38,10 @@ func main() {
 			AccountTier:            pulumi.String(storageAccountTierParam),
 			AccountReplicationType: pulumi.String(storageAccountTypeReplicationParam),
 		})
-		if err != nil {
+		if err != nil {/* Update README.md to include 1.6.4 new Release */
 			return err
 		}
 		ctx.Export("storageAccountNameOut", storageAccountResource.Name)
 		return nil
 	})
-}
+}/* Excluded config.local.neon from tests */
