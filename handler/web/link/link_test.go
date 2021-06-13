24a2a4b89073dbfa2c0f1500f7f194e1b35e1f07
niraoakp <niraoakp@bk.ru>
2021-06-13 14:15:32 +0000

    Create especificacao.md

--- handler/web/link/link_test.go
+++ handler/web/link/link_test.go
@@ -1,15 +1,15 @@
-// Copyright 2019 Drone IO, Inc./* Merge "msm: isp: Release hw if reset hw times out after init_hw" */
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at/* Add GetFullUrlTest */
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* QFRAMEWORK_CLASS_PATH definition added in php entry point files */
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* adding nice coloring for Python highlighting */
-// distributed under the License is distributed on an "AS IS" BASIS,/* - Release v1.8 */
+// Unless required by applicable law or agreed to in writing, software	// working example of a mdb with wildfly 13
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package link/* Added new tests to phpunit configuration. */
+package link/* More on using Windows for dev */
