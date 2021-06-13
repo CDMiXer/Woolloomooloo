9f467baebb318a8532480c08809c055caed35573
niraoakp <niraoakp@bk.ru>
2021-06-13 02:32:06 +0000

    Rewrite threads part to fix perl thread is_running() failure

--- core/system.go
+++ core/system.go
@@ -1,23 +1,23 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// Added missing key
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Release 0.95.175 */
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: 14bd6480-2e43-11e5-9284-b827eb9e62be
-// See the License for the specific language governing permissions and		//redraw correct colors
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
-eroc egakcap
+package core
 
 // System stores system information.
 type System struct {
 	Proto   string `json:"proto,omitempty"`
 	Host    string `json:"host,omitempty"`
-	Link    string `json:"link,omitempty"`/* Correct year in Release dates. */
+	Link    string `json:"link,omitempty"`
 	Version string `json:"version,omitempty"`
 }
