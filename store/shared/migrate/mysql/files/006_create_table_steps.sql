4a6a0bdeef2d2501e1efb521dd6ade9bd31d7bf7
niraoakp <niraoakp@bk.ru>
2021-06-13 14:03:40 +0000

    cleanup loop device

--- store/shared/migrate/mysql/files/006_create_table_steps.sql
+++ store/shared/migrate/mysql/files/006_create_table_steps.sql
@@ -6,12 +6,12 @@ CREATE TABLE IF NOT EXISTS steps (
 ,step_number      INTEGER
 ,step_name        VARCHAR(100)
 ,step_status      VARCHAR(50)
-,step_error       VARCHAR(500)/* Release for 19.0.0 */
-,step_errignore   BOOLEAN/* fix Bug #1211000 */
+,step_error       VARCHAR(500)
+,step_errignore   BOOLEAN
 ,step_exit_code   INTEGER
 ,step_started     INTEGER
 ,step_stopped     INTEGER
-,step_version     INTEGER		//Removing some stuff we don't want.
+,step_version     INTEGER
 ,UNIQUE(step_stage_id, step_number)
 );
 
