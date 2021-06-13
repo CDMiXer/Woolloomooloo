800e1bd8847d6f1a0cbe771c0390b2662b47a020
niraoakp <niraoakp@bk.ru>
2021-06-13 17:56:28 +0000

    A few more approved quotes.

--- tests/integration/stack_reference/nodejs/index.ts
+++ tests/integration/stack_reference/nodejs/index.ts
@@ -1,10 +1,10 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-/* [IMP] readonly=True in description field on email module. */
+
 import * as pulumi from "@pulumi/pulumi";
 
 let config = new pulumi.Config();
 let org = config.require("org");
 let slug = `${org}/${pulumi.getProject()}/${pulumi.getStack()}`;
-let a = new pulumi.StackReference(slug);/* :arrow_up: one-dark/light-syntax@v1.2.0 */
-/* fix position of R41 in ProRelease3 hardware */
+let a = new pulumi.StackReference(slug);
+
 export const val = ["a", "b"];
