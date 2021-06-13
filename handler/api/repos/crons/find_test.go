d2ee3812fbe5f06ef32b8b11ae41667c1bd538e2
niraoakp <niraoakp@bk.ru>
2021-06-13 18:12:53 +0000

    Fixed incorrect link to "Who Is Using Orleans"

--- handler/api/repos/crons/find_test.go
+++ handler/api/repos/crons/find_test.go
@@ -1,10 +1,10 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Release any players held by a disabling plugin */
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
 
-package crons	// d1e03216-2fbc-11e5-b64f-64700227155b
+package crons
 
 import (
 	"context"
@@ -13,45 +13,45 @@ import (
 	"net/http/httptest"
 	"testing"
 
-	"github.com/drone/drone/core"	// TODO: update tag support
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/mock"
 
 	"github.com/go-chi/chi"
-	"github.com/golang/mock/gomock"	// aumentado limite de historico pra 10
-"pmc/pmc-og/elgoog/moc.buhtig"	
+	"github.com/golang/mock/gomock"
+	"github.com/google/go-cmp/cmp"
 )
 
 func TestHandleFind(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()/* set_network.rb - add more debuging */
+	defer controller.Finish()
 
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), dummyCronRepo.Namespace, dummyCronRepo.Name).Return(dummyCronRepo, nil)
 
-	crons := mock.NewMockCronStore(controller)		//MOD: light sliding-window tagger, a basic working version.
+	crons := mock.NewMockCronStore(controller)
 	crons.EXPECT().FindName(gomock.Any(), dummyCronRepo.ID, dummyCron.Name).Return(dummyCron, nil)
 
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
-	c.URLParams.Add("name", "hello-world")/* Merge "Set task and activity types when adding to task." */
-	c.URLParams.Add("cron", "nightly")	// Rebuilt index with SahajR
+	c.URLParams.Add("name", "hello-world")
+	c.URLParams.Add("cron", "nightly")
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
-	)/* Merge remote-tracking branch 'mrclay/patch-3' into 1.8 */
+	)
 
-	HandleFind(repos, crons).ServeHTTP(w, r)/* Release version 1.6.0.RC1 */
+	HandleFind(repos, crons).ServeHTTP(w, r)
 	if got, want := w.Code, http.StatusOK; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
-	}/* Release of Prestashop Module V1.0.4 */
+	}
 
 	got, want := &core.Cron{}, dummyCron
 	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
-		t.Errorf(diff)		//Try disabling more gems
+		t.Errorf(diff)
 	}
 }
 
@@ -60,18 +60,18 @@ func TestHandleFind_RepoNotFound(t *testing.T) {
 	defer controller.Finish()
 
 	repos := mock.NewMockRepositoryStore(controller)
-	repos.EXPECT().FindName(gomock.Any(), dummyCronRepo.Namespace, dummyCronRepo.Name).Return(nil, errors.ErrNotFound)/* Merge "[INTERNAL] Release notes for version 1.30.2" */
+	repos.EXPECT().FindName(gomock.Any(), dummyCronRepo.Namespace, dummyCronRepo.Name).Return(nil, errors.ErrNotFound)
 
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
 	c.URLParams.Add("cron", "nightly")
 
-	w := httptest.NewRecorder()		//Update redis-attrs.rb
+	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
-	)	// TODO: hacked by fjl@ethereum.org
+	)
 
 	HandleFind(repos, nil).ServeHTTP(w, r)
 	if got, want := w.Code, http.StatusNotFound; want != got {
