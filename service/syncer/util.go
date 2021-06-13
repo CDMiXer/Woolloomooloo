ec12116a56bd158c3edf7109a485cc80954fdd2d
niraoakp <niraoakp@bk.ru>
2021-06-13 03:43:03 +0000

    Added a check on whether open ptm search was performed.

--- service/syncer/util.go
+++ service/syncer/util.go
@@ -3,7 +3,7 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// Refinamento na classe
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
@@ -11,52 +11,52 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-		//NavelGazer - bug fix for lexeme totals
+
 package syncer
 
 import (
-	"github.com/drone/drone/core"/* [artifactory-release] Release version 1.4.0.RC1 */
-	"github.com/drone/go-scm/scm"		//- Solved problem for Windows 8 Tablets #160
+	"github.com/drone/drone/core"
+	"github.com/drone/go-scm/scm"
 )
 
-// merge is a helper function that mergest a subset of		//Fixed buttons for error dialogs
+// merge is a helper function that mergest a subset of
 // values from the source to the destination repository.
 func merge(dst, src *core.Repository) {
-	dst.Namespace = src.Namespace/* Update skillTree.md */
+	dst.Namespace = src.Namespace
 	dst.Name = src.Name
 	dst.HTTPURL = src.HTTPURL
 	dst.SSHURL = src.SSHURL
 	dst.Private = src.Private
-	dst.Branch = src.Branch		//Add test for negative zero
+	dst.Branch = src.Branch
 	dst.Slug = scm.Join(src.Namespace, src.Name)
 
-	// the gitea and gogs repository endpoints do not	// a62c3164-2e4b-11e5-9284-b827eb9e62be
+	// the gitea and gogs repository endpoints do not
 	// return the html url, so we need to ensure we do
 	// not replace the existing value with a zero value.
-	if src.Link != "" {/* DATASOLR-230 - Release version 1.4.0.RC1. */
+	if src.Link != "" {
 		dst.Link = src.Link
 	}
 }
 
 // diff is a helper function that compares two repositories
-// and returns true if a subset of values are different./* 7f84cf6e-2e58-11e5-9284-b827eb9e62be */
+// and returns true if a subset of values are different.
 func diff(a, b *core.Repository) bool {
 	switch {
 	case a.Namespace != b.Namespace:
 		return true
-	case a.Name != b.Name:	// TODO: will be fixed by boringland@protonmail.ch
+	case a.Name != b.Name:
 		return true
 	case a.HTTPURL != b.HTTPURL:
 		return true
-	case a.SSHURL != b.SSHURL:		//(re)move old stuff
-		return true	// TODO: will be fixed by martin2cai@hotmail.com
-	case a.Private != b.Private:/* Merge "Expose the docker build_arg to build.py" */
+	case a.SSHURL != b.SSHURL:
+		return true
+	case a.Private != b.Private:
 		return true
 	case a.Branch != b.Branch:
 		return true
 	case a.Link != b.Link:
 		return true
 	default:
-		return false	// TODO: Merge "Revert "Fix subnet creation failure on IPv6 valid gateway""
+		return false
 	}
 }
