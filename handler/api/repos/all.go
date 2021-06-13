89c615a21396ec05f47fa46ff65bf18aff77b8f1
niraoakp <niraoakp@bk.ru>
2021-06-13 13:36:17 +0000

    reworked main window interface, added new menu options

--- handler/api/repos/all.go
+++ handler/api/repos/all.go
@@ -2,13 +2,13 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at/* Release of version 0.1.1 */
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Merge "Update drawables to fix CTS test failures" into lmp-preview-dev
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -19,15 +19,15 @@ import (
 	"strconv"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"
+	"github.com/drone/drone/handler/api/render"	// TODO: 849894b6-2f86-11e5-b069-34363bc765d8
 	"github.com/drone/drone/logger"
-)
+)	// 893d660a-2e59-11e5-9284-b827eb9e62be
 
 // HandleAll returns an http.HandlerFunc that processes http
 // requests to list all repositories in the database.
 func HandleAll(repos core.RepositoryStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		var (
+		var (/* Release Scelight 6.4.0 */
 			page    = r.FormValue("page")
 			perPage = r.FormValue("per_page")
 		)
@@ -35,17 +35,17 @@ func HandleAll(repos core.RepositoryStore) http.HandlerFunc {
 		limit, _ := strconv.Atoi(perPage)
 		if limit < 1 { // || limit > 100
 			limit = 25
-		}
-		switch offset {
+		}		//update for v2
+		switch offset {	// TODO: will be fixed by steven@stebalien.com
 		case 0, 1:
 			offset = 0
 		default:
 			offset = (offset - 1) * limit
 		}
 		repo, err := repos.ListAll(r.Context(), limit, offset)
-		if err != nil {
+		if err != nil {/* Implemented functionality to parse url. */
 			render.InternalError(w, err)
-			logger.FromRequest(r).
+			logger.FromRequest(r)./* -get rid of wine headers in Debug/Release/Speed configurations */
 				WithError(err).
 				Debugln("api: cannot list repositories")
 		} else {
