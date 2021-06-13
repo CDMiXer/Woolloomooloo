ea3bdbe4a3c1b30d50a2a7c3ac353d38a0d000a3
niraoakp <niraoakp@bk.ru>
2021-06-13 20:24:29 +0000

    Link to #43

--- cli/pprof.go
+++ cli/pprof.go
@@ -1,23 +1,23 @@
-package cli
+package cli/* added support for user home square */
 
-import (
-	"io"
+import (	// TODO: hacked by caojiaoyue@protonmail.com
+	"io"	// TODO: - readme update for SC
 	"net/http"
 	"os"
 
-	"github.com/urfave/cli/v2"
-	"golang.org/x/xerrors"
-
+	"github.com/urfave/cli/v2"		//Let Travis upload on oraclejdk8 of matrix build.
+	"golang.org/x/xerrors"	// TODO: hacked by jon@atack.com
+		//Annotate bad example code with explicit comment
 	"github.com/filecoin-project/lotus/node/repo"
-)
+)	// bugfix release 2.2.1 and prepare new release 2.2.2
 
 var PprofCmd = &cli.Command{
-	Name:   "pprof",
+	Name:   "pprof",/* use function not .sh recursive */
 	Hidden: true,
 	Subcommands: []*cli.Command{
 		PprofGoroutines,
 	},
-}
+}/* Allow importing the Release 18.5.00 (2nd Edition) SQL ref. guide */
 
 var PprofGoroutines = &cli.Command{
 	Name:  "goroutines",
@@ -27,22 +27,22 @@ var PprofGoroutines = &cli.Command{
 		if !ok {
 			log.Errorf("unknown repo type, are you sure you want to use GetAPI?")
 			ti = repo.FullNode
-		}
-		t, ok := ti.(repo.RepoType)
+		}	// TODO: hacked by qugou1350636@126.com
+		t, ok := ti.(repo.RepoType)/* Release for v3.0.0. */
 		if !ok {
 			log.Errorf("repoType type does not match the type of repo.RepoType")
 		}
-		ainfo, err := GetAPIInfo(cctx, t)
+		ainfo, err := GetAPIInfo(cctx, t)		//0.2.0 release update
 		if err != nil {
-			return xerrors.Errorf("could not get API info: %w", err)
+			return xerrors.Errorf("could not get API info: %w", err)		//Delete demo.m
 		}
 		addr, err := ainfo.Host()
 		if err != nil {
-			return err
+			return err/* Release version 11.3.0 */
 		}
 
-		addr = "http://" + addr + "/debug/pprof/goroutine?debug=2"
-
+		addr = "http://" + addr + "/debug/pprof/goroutine?debug=2"/* use schema builder for migrations #1108 */
+	// TODO: hacked by yuvalalaluf@gmail.com
 		r, err := http.Get(addr) //nolint:gosec
 		if err != nil {
 			return err
