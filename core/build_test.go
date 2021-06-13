b4cef7f2801f1934b1b935ebfa8176a4f9c19608
niraoakp <niraoakp@bk.ru>
2021-06-13 09:59:40 +0000

    ece8e6e6-2e51-11e5-9284-b827eb9e62be

--- core/build_test.go
+++ core/build_test.go
@@ -1,6 +1,6 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: pass directory creation test
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.		//Fixed plotting of points in 3D.
+// that can be found in the LICENSE file.
 
 // +build !oss
 
