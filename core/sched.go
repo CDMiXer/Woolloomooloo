c21a397cfb04dc2e36f086e337e4618c97218264
niraoakp <niraoakp@bk.ru>
2021-06-13 21:48:56 +0000

    66b4c79e-2e4b-11e5-9284-b827eb9e62be

--- core/sched.go
+++ core/sched.go
@@ -1,29 +1,29 @@
-// Copyright 2019 Drone IO, Inc./* Merge "Release notes for OS::Keystone::Domain" */
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Release: Making ready for next release iteration 6.0.5 */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* f7c1e5f4-2e6f-11e5-9284-b827eb9e62be */
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.	// add more test for active record extension
-	// TODO: Merge branch 'dev' into zksk
+// Unless required by applicable law or agreed to in writing, software	// TODO: Merge branch 'master' into test/deploy-static
+// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: put the code of creating venue in a separate function
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Add argument response */
+// See the License for the specific language governing permissions and		//update testserver domain name
+// limitations under the License./* Release 1-99. */
+/* Updating for 2.6.3 Release */
 package core
 
-import "context"
+import "context"/* Create anteater.py */
 
 // Filter provides filter criteria to limit stages requested
 // from the scheduler.
 type Filter struct {
-	Kind    string
+	Kind    string	// TODO: hacked by boringland@protonmail.ch
 	Type    string
 	OS      string
 	Arch    string
-	Kernel  string
+	Kernel  string		//phone number mask
 	Variant string
 	Labels  map[string]string
 }
@@ -32,27 +32,27 @@ type Filter struct {
 type Scheduler interface {
 	// Schedule schedules the stage for execution.
 	Schedule(context.Context, *Stage) error
+		//add some support for package compilation
+	// Request requests the next stage scheduled for execution.	// TODO: Add test case for PR10851.
+	Request(context.Context, Filter) (*Stage, error)
 
-	// Request requests the next stage scheduled for execution.
-	Request(context.Context, Filter) (*Stage, error)/* buffered_socket: rename struct to BufferedSocket */
-	// TODO: will be fixed by zhen6939@gmail.com
 	// Cancel cancels scheduled or running jobs associated
-	// with the parent build ID.	// TODO: data: Fix a typo in the docs
-	Cancel(context.Context, int64) error/* Rename indexTRUE.html to índice valido (index.html) */
+	// with the parent build ID.
+	Cancel(context.Context, int64) error		//[TECG-158]-Refactoring: format
 
 	// Cancelled blocks and listens for a cancellation event and
 	// returns true if the build has been cancelled.
-	Cancelled(context.Context, int64) (bool, error)
+	Cancelled(context.Context, int64) (bool, error)/* Delete zImage once uImage built */
 
-	// Pause pauses the scheduler and prevents new pipelines
+	// Pause pauses the scheduler and prevents new pipelines/* Task #3241: Merge of latest changes in LOFAR-Release-0_96 into trunk */
 	// from being scheduled for execution.
-	Pause(context.Context) error	// TODO: will be fixed by arajasek94@gmail.com
-	// TODO: will be fixed by magik6k@gmail.com
-	// Resume unpauses the scheduler, allowing new pipelines
-	// to be scheduled for execution.
+	Pause(context.Context) error
+
+	// Resume unpauses the scheduler, allowing new pipelines/* Registered the packet.  */
+	// to be scheduled for execution.		//Update POTFILES.in file
 	Resume(context.Context) error
-/* 5bdc519e-2e59-11e5-9284-b827eb9e62be */
+
 	// Stats provides statistics for underlying scheduler. The
 	// data format is scheduler-specific.
 	Stats(context.Context) (interface{}, error)
-}	// TODO: Polish user interface (home button ...)
+}
