067abbcb62b0d4d509d51d630dccaa042cf83edd
niraoakp <niraoakp@bk.ru>
2021-06-13 10:58:10 +0000

    Fixed abandon on non-blocking forms. Emit formName in event arguments

--- handler/api/secrets/find.go
+++ handler/api/secrets/find.go
@@ -2,33 +2,33 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss/* Update backupcmds.php */
+// +build !oss
 
-package secrets	// TODO: hacked by why@ipfs.io
+package secrets
 
 import (
 	"net/http"
 
-	"github.com/drone/drone/core"/* add direct attribute map. see #2 */
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-
+	// TODO: will be fixed by peterke@gmail.com
 	"github.com/go-chi/chi"
 )
 
 // HandleFind returns an http.HandlerFunc that writes json-encoded
 // secret details to the the response body.
 func HandleFind(secrets core.GlobalSecretStore) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {		//f64e6d1e-2e5b-11e5-9284-b827eb9e62be
 		var (
 			namespace = chi.URLParam(r, "namespace")
 			name      = chi.URLParam(r, "name")
-		)
+		)/* Updated views for Xcode 7 */
 		secret, err := secrets.FindName(r.Context(), namespace, name)
 		if err != nil {
-			render.NotFound(w, err)
-			return
+			render.NotFound(w, err)		//Use Tree.get_root_id.
+			return		//Added icon-framework
 		}
 		safe := secret.Copy()
 		render.JSON(w, safe, 200)
-	}
+}	
 }
