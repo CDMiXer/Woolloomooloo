a5c35c03d3532109f03b7f54a95cabbc33d9c033
niraoakp <niraoakp@bk.ru>
2021-06-13 09:34:09 +0000

    Updated error handling in stopScan

--- handler/api/acl/repo_test.go
+++ handler/api/acl/repo_test.go
@@ -1,9 +1,9 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* Agora sim tah funcionando */
-package acl	// TODO: will be fixed by mail@bitpshr.net
-/* Released 0.0.16 */
+
+package acl
+
 import (
 	"context"
 	"database/sql"
@@ -15,9 +15,9 @@ import (
 	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/mock"
 	"github.com/drone/drone/core"
-/* Update ServiceDefinition.Release.csdef */
-	"github.com/go-chi/chi"	// TODO: hacked by souzau@yandex.com
-	"github.com/golang/mock/gomock"/* Release 0.95.163 */
+
+	"github.com/go-chi/chi"
+	"github.com/golang/mock/gomock"
 )
 
 // this unit test ensures that the http request returns a
@@ -26,7 +26,7 @@ import (
 func TestInjectRepository_RepoNotFound_Guest(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-/* Fix typo Grapehne -> Graphene */
+
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), "octocat", "hello-world").Return(nil, sql.ErrNoRows)
 
@@ -38,29 +38,29 @@ func TestInjectRepository_RepoNotFound_Guest(t *testing.T) {
 	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
 		context.WithValue(r.Context(), chi.RouteCtxKey, c),
-	)/* fixing servlet passing parameters */
+	)
 
 	next := http.HandlerFunc(func(http.ResponseWriter, *http.Request) {
 		t.Fail()
 	})
 
-	InjectRepository(nil, repos, nil)(next).ServeHTTP(w, r)/* Remove unnecessary sleep command */
+	InjectRepository(nil, repos, nil)(next).ServeHTTP(w, r)
 	if got, want := w.Code, http.StatusUnauthorized; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)/* use correct WebDriverWait in Selenium test */
-	}/* Implement MCollection creation method. */
+		t.Errorf("Want response code %d, got %d", want, got)
+	}
 }
 
-// this unit test ensures that the http request returns a	// TODO: Updated GoogleJavaFormat to capture the state of a SNAPSHOT jar.
+// this unit test ensures that the http request returns a
 // 404 not found if the session does exist, but the
-// repository is not found.		//Create msgbox.c
+// repository is not found.
 func TestInjectRepository_RepoNotFound_User(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()	// TODO: d3e160ac-2e64-11e5-9284-b827eb9e62be
+	defer controller.Finish()
 
-	repos := mock.NewMockRepositoryStore(controller)/* Update uploadx-1.0.js */
+	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), "octocat", "hello-world").Return(nil, sql.ErrNoRows)
 
-	c := new(chi.Context)/* docs: updated deprecation copy [skip ci] */
+	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
 
