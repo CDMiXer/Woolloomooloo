bb750e2a193dc4be85afe2cfeb8a29e2a48d805d
niraoakp <niraoakp@bk.ru>
2021-06-13 19:33:38 +0000

    added iterate and split to sst

--- tests/integration/stack_project_name/index.ts
+++ tests/integration/stack_project_name/index.ts
@@ -1,5 +1,5 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+/* ReactDialog: Dispose of object */
 import * as pulumi from "@pulumi/pulumi";
 
 const stackName = pulumi.getStack();
