245eb35d8d70ef8370d8e4abc9e04ff39598197e
niraoakp <niraoakp@bk.ru>
2021-06-13 11:49:35 +0000

    + Refactor interpreter class

--- pkg/codegen/internal/test/testdata/azure-sa.pp.py
+++ pkg/codegen/internal/test/testdata/azure-sa.pp.py
@@ -1,9 +1,9 @@
-import pulumi	// TODO: Working Tags, prelim comments
-import pulumi_azure as azure		//Merge "UEFI secure boot support for iso element."
+import pulumi
+import pulumi_azure as azure
 
-config = pulumi.Config()/* Release 1.4:  Add support for the 'pattern' attribute */
+config = pulumi.Config()
 storage_account_name_param = config.require("storageAccountNameParam")
-resource_group_name_param = config.require("resourceGroupNameParam")		//Delete apple_300x300.jpg
+resource_group_name_param = config.require("resourceGroupNameParam")
 resource_group_var = azure.core.get_resource_group(name=resource_group_name_param)
 location_param = config.get("locationParam")
 if location_param is None:
@@ -11,12 +11,12 @@ if location_param is None:
 storage_account_tier_param = config.get("storageAccountTierParam")
 if storage_account_tier_param is None:
     storage_account_tier_param = "Standard"
-storage_account_type_replication_param = config.get("storageAccountTypeReplicationParam")	// Invoice added.
+storage_account_type_replication_param = config.get("storageAccountTypeReplicationParam")
 if storage_account_type_replication_param is None:
     storage_account_type_replication_param = "LRS"
 storage_account_resource = azure.storage.Account("storageAccountResource",
-    name=storage_account_name_param,/* Release version: 0.6.1 */
-    account_kind="StorageV2",		//Update oi-lib.h
+    name=storage_account_name_param,
+    account_kind="StorageV2",
     location=location_param,
     resource_group_name=resource_group_name_param,
     account_tier=storage_account_tier_param,
