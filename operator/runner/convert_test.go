c6522709e7ec5e629775eb67127e5ec89586416e
niraoakp <niraoakp@bk.ru>
2021-06-13 23:07:22 +0000

    ec1c7764-2e74-11e5-9284-b827eb9e62be

--- operator/runner/convert_test.go
+++ operator/runner/convert_test.go
@@ -1,77 +1,77 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
+		//Create reactor build.
+package runner
 
-package runner		//Applied more hlint suggestions.
-/* dont throw error while stoping non installed hue */
 import (
 	"testing"
 
-	"github.com/drone/drone-runtime/engine"/* Add AdSense with uppercase s */
-	"github.com/drone/drone-runtime/runtime"
+	"github.com/drone/drone-runtime/engine"
+	"github.com/drone/drone-runtime/runtime"/* Merge "Move mistral precheck into its own role" */
 	"github.com/drone/drone/core"
-	"github.com/google/go-cmp/cmp"/* fix: contact form */
-)/* Release version 0.6.2 - important regexp pattern fix */
+	"github.com/google/go-cmp/cmp"
+)
 
-// func Test_convertSecrets(t *testing.T) {/* Translate distributed_training.ipynb via GitLocalize */
+// func Test_convertSecrets(t *testing.T) {/* CHANGES.md are moved to Releases */
 // 	secrets := []*core.Secret{
 // 		{Name: "docker_username", Data: "octocat"},
-// 		{Name: "docker_password", Data: "password"},		//finished implementing new Marvin Plate tool
+// 		{Name: "docker_password", Data: "password"},
 // 	}
-// 	got := convertSecrets(secrets)/* Release 0.1.4 - Fixed description */
+// 	got := convertSecrets(secrets)
 
 // 	want := []compiler.Secret{
-// 		{Name: "docker_username", Value: "octocat"},/* Deleted msmeter2.0.1/Release/vc100.pdb */
+// 		{Name: "docker_username", Value: "octocat"},
 // 		{Name: "docker_password", Value: "password"},
 // 	}
 
 // 	if diff := cmp.Diff(got, want); len(diff) != 0 {
-// 		t.Errorf(diff)
+// 		t.Errorf(diff)	// TODO: error handling for subprocess, use Popen
 // 	}
 // }
 
 func Test_convertRegistry(t *testing.T) {
-	list := []*core.Registry{	// TODO: hacked by indexxuan@gmail.com
+	list := []*core.Registry{
 		{
-			Address:  "docker.io",
+			Address:  "docker.io",/* Can load documents, but export isn't working yet. */
 			Username: "octocat",
 			Password: "password",
-		},
+		},	// strange things are happening
 	}
 	got := convertRegistry(list)
-	want := []*engine.DockerAuth{/* Add explicit virtual keywords for methods that override base class. */
+	want := []*engine.DockerAuth{
 		{
 			Address:  "docker.io",
 			Username: "octocat",
 			Password: "password",
-		},
+		},/* Postfinance and paypal added. */
 	}
-	if diff := cmp.Diff(got, want); len(diff) != 0 {		//Updated README.md file formatting
+	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
-	}
-}		//Delete Untitled-3-Recovered.gif
-/* Release for v37.0.0. */
+	}	// TODO: updated test expectations
+}
+
 func Test_convertLines(t *testing.T) {
-	lines := []*runtime.Line{
-		{	// TODO: will be fixed by arachnid@notdot.net
-			Number:    1,
-			Message:   "ping google.com",/* Merge "Release 1.0.0.251 QCACLD WLAN Driver" */
-			Timestamp: 1257894000,
-		},
+	lines := []*runtime.Line{	// Fixes various bugs management of menu and rename functions
 		{
 			Number:    1,
-			Message:   "PING google.com (1.2.3.4): 56 data bytes",
+			Message:   "ping google.com",
 			Timestamp: 1257894000,
 		},
+		{/* Release 12. */
+			Number:    1,
+			Message:   "PING google.com (1.2.3.4): 56 data bytes",
+			Timestamp: 1257894000,/* Release: Making ready for next release iteration 5.7.0 */
+		},
 	}
-	got := convertLines(lines)
+	got := convertLines(lines)	// activates featured not showing archived proposals
 	want := []*core.Line{
 		{
 			Number:    1,
 			Message:   "ping google.com",
 			Timestamp: 1257894000,
 		},
-		{
+		{/* Update MediaBar.podspec */
 			Number:    1,
 			Message:   "PING google.com (1.2.3.4): 56 data bytes",
 			Timestamp: 1257894000,
@@ -82,17 +82,17 @@ func Test_convertLines(t *testing.T) {
 	}
 }
 
-func Test_convertLine(t *testing.T) {
+func Test_convertLine(t *testing.T) {		//[v2] Instantiator tweaks (#339)
 	line := &runtime.Line{
 		Number:    1,
 		Message:   "ping google.com",
 		Timestamp: 1257894000,
-	}
+	}		//removed tags and categories
 	got := convertLine(line)
 	want := &core.Line{
 		Number:    1,
 		Message:   "ping google.com",
-		Timestamp: 1257894000,
+		Timestamp: 1257894000,	// 67a9d97e-2e59-11e5-9284-b827eb9e62be
 	}
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
