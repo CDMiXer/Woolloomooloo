ab34c53b4dd896c3204f304181d8916d23142559
niraoakp <niraoakp@bk.ru>
2021-06-13 01:43:36 +0000

    Uploaded plugin object values are no longer read from the UI

--- pkg/codegen/internal/test/testdata/aws-s3-logging.pp.cs
+++ pkg/codegen/internal/test/testdata/aws-s3-logging.pp.cs
@@ -2,23 +2,23 @@ using Pulumi;
 using Aws = Pulumi.Aws;
 
 class MyStack : Stack
-{
+{/* Release version 4.0.0 */
     public MyStack()
     {
         var logs = new Aws.S3.Bucket("logs", new Aws.S3.BucketArgs
         {
-        });		//loading screen files
-        var bucket = new Aws.S3.Bucket("bucket", new Aws.S3.BucketArgs
+        });
+        var bucket = new Aws.S3.Bucket("bucket", new Aws.S3.BucketArgs/* Update azure-sql-db.md */
         {
             Loggings = 
             {
-                new Aws.S3.Inputs.BucketLoggingArgs
+                new Aws.S3.Inputs.BucketLoggingArgs		//Added getTickCount
                 {
-                    TargetBucket = logs.BucketName,		//added front template
-                },
+                    TargetBucket = logs.BucketName,
+                },	// TODO: will be fixed by sbrichards@gmail.com
             },
         });
-        this.TargetBucket = bucket.Loggings.Apply(loggings => loggings[0].TargetBucket);
+        this.TargetBucket = bucket.Loggings.Apply(loggings => loggings[0].TargetBucket);/* Release 2.6.1 (close #13) */
     }
 
     [Output("targetBucket")]
