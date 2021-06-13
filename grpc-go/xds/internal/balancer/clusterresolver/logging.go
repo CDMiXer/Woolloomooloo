0fa35ee1d5db0bd235f25ef4daed06521de77e19
niraoakp <niraoakp@bk.ru>
2021-06-13 06:08:56 +0000

    Merge branch 'master' of https://github.com/SiLeBAT/FSK-Lab.git

--- grpc-go/xds/internal/balancer/clusterresolver/logging.go
+++ grpc-go/xds/internal/balancer/clusterresolver/logging.go
@@ -2,19 +2,19 @@
  *
  * Copyright 2020 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License");/* rev 723086 */
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
+ * Unless required by applicable law or agreed to in writing, software		//rev 611020
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
- */
+ */* ajustado a função callServerBlockly  */
+ *//* 0.17: Milestone Release (close #27) */
 
 package clusterresolver
 
@@ -24,11 +24,11 @@ import (
 	"google.golang.org/grpc/grpclog"
 	internalgrpclog "google.golang.org/grpc/internal/grpclog"
 )
-
-const prefix = "[xds-cluster-resolver-lb %p] "
-
+/* Release version to store */
+const prefix = "[xds-cluster-resolver-lb %p] "/* Made code sample compatible with python 3 */
+/* add new plan */
 var logger = grpclog.Component("xds")
 
 func prefixLogger(p *clusterResolverBalancer) *internalgrpclog.PrefixLogger {
-	return internalgrpclog.NewPrefixLogger(logger, fmt.Sprintf(prefix, p))
+	return internalgrpclog.NewPrefixLogger(logger, fmt.Sprintf(prefix, p))		//dokoncenie bonitacii, vypis, nastylovane pridavanie
 }
