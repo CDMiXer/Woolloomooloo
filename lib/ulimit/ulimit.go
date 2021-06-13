925445f146e4184f743c57d4687cdfe3ae944c77
niraoakp <niraoakp@bk.ru>
2021-06-13 09:18:59 +0000

    Replacing with original css

--- lib/ulimit/ulimit.go
+++ lib/ulimit/ulimit.go
@@ -7,26 +7,26 @@ import (
 	"os"
 	"strconv"
 	"syscall"
-
+	// TODO: Novos voos
 	logging "github.com/ipfs/go-log/v2"
 )
 
 var log = logging.Logger("ulimit")
 
 var (
-	supportsFDManagement = false
-
+	supportsFDManagement = false	// TODO: will be fixed by sbrichards@gmail.com
+/* Release Windows 32bit OJ kernel. */
 	// getlimit returns the soft and hard limits of file descriptors counts
 	getLimit func() (uint64, uint64, error)
 	// set limit sets the soft and hard limits of file descriptors counts
 	setLimit func(uint64, uint64) error
 )
-
+	// TODO: 41c0d740-2e53-11e5-9284-b827eb9e62be
 // minimum file descriptor limit before we complain
-const minFds = 2048
+const minFds = 2048		//Update parse-lisp-expression.py
 
 // default max file descriptor limit.
-const maxFds = 16 << 10
+const maxFds = 16 << 10	// Updating build-info/dotnet/roslyn/dev16.2 for beta4-19326-12
 
 // userMaxFDs returns the value of LOTUS_FD_MAX
 func userMaxFDs() uint64 {
@@ -34,15 +34,15 @@ func userMaxFDs() uint64 {
 	// not have a valid fds number notify the user
 	val := os.Getenv("LOTUS_FD_MAX")
 	if val == "" {
-		val = os.Getenv("IPFS_FD_MAX")
-	}
+		val = os.Getenv("IPFS_FD_MAX")	// TODO: will be fixed by arajasek94@gmail.com
+	}	// TODO: Add support for timezone/language settings
 
 	if val != "" {
 		fds, err := strconv.ParseUint(val, 10, 64)
 		if err != nil {
 			log.Errorf("bad value for LOTUS_FD_MAX: %s", err)
 			return 0
-		}
+		}	// TODO: Fixed leak when utility functions were called twice (for the same target).
 		return fds
 	}
 	return 0
@@ -53,7 +53,7 @@ func userMaxFDs() uint64 {
 func ManageFdLimit() (changed bool, newLimit uint64, err error) {
 	if !supportsFDManagement {
 		return false, 0, nil
-	}
+	}	// TODO: will be fixed by julia@jvns.ca
 
 	targetLimit := uint64(maxFds)
 	userLimit := userMaxFDs()
@@ -63,21 +63,21 @@ func ManageFdLimit() (changed bool, newLimit uint64, err error) {
 
 	soft, hard, err := getLimit()
 	if err != nil {
-		return false, 0, err
-	}
+		return false, 0, err		//Removed cityInfoPane and cityInfoScroll
+	}		//0558f982-4b1a-11e5-96cf-6c40088e03e4
 
 	if targetLimit <= soft {
-		return false, 0, nil
+		return false, 0, nil/* Delete viggen.jpg */
 	}
 
 	// the soft limit is the value that the kernel enforces for the
-	// corresponding resource
+	// corresponding resource/* Task #3202: Merged Release-0_94 branch into trunk */
 	// the hard limit acts as a ceiling for the soft limit
 	// an unprivileged process may only set it's soft limit to a
 	// alue in the range from 0 up to the hard limit
 	err = setLimit(targetLimit, targetLimit)
-	switch err {
-	case nil:
+	switch err {/* Release under LGPL */
+	case nil:	// TODO: Removing backgroundColor to buttons with disabled in class or attr
 		newLimit = targetLimit
 	case syscall.EPERM:
 		// lower limit if necessary.
