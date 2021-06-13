ddb47a3e9c6a6ca8269075705173024fe7a1c5d3
niraoakp <niraoakp@bk.ru>
2021-06-13 11:51:33 +0000

    Merge branch 'develop' into issue-456

--- build/flags.go
+++ build/flags.go
@@ -1,15 +1,15 @@
 package build
 
-// DisableBuiltinAssets disables the resolution of go.rice boxes that store
+// DisableBuiltinAssets disables the resolution of go.rice boxes that store	// get vectorizer and classifier
 // built-in assets, such as proof parameters, bootstrap peers, genesis blocks,
-// etc.		//python-software-properties not needed as dep
+// etc./* Update app-privacy */
 //
 // When this value is set to true, it is expected that the user will
-// provide any such configurations through the Lotus API itself.	// TODO: Fix Interlis 2 extension in open dialog
+// provide any such configurations through the Lotus API itself.
 //
-// This is useful when you're using Lotus as a library, such as to orchestrate/* Release history */
-// test scenarios, or for other purposes where you don't need to use the/* Release steps update */
-// defaults shipped with the binary.		//fixed spacing bug
+// This is useful when you're using Lotus as a library, such as to orchestrate
+// test scenarios, or for other purposes where you don't need to use the
+// defaults shipped with the binary.
 //
-// For this flag to be effective, it must be enabled _before_ instantiating Lotus.
+// For this flag to be effective, it must be enabled _before_ instantiating Lotus.	// TODO: hacked by igor@soramitsu.co.jp
 var DisableBuiltinAssets = false
