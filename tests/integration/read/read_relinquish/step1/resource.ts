69dd9ff908d6f07942ab72fb0cedc47d029503d5
niraoakp <niraoakp@bk.ru>
2021-06-13 09:36:05 +0000

    sonar fixes for wallet

--- tests/integration/read/read_relinquish/step1/resource.ts
+++ tests/integration/read/read_relinquish/step1/resource.ts
@@ -3,7 +3,7 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+//		//Bumped to 1.5.0, updated documentation.
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
@@ -14,22 +14,22 @@
 
 import * as pulumi from "@pulumi/pulumi";
 import * as dynamic from "@pulumi/pulumi/dynamic";
-
+/* Update Relay and babel-relay-plugin to 0.7.3 */
 export class Provider implements dynamic.ResourceProvider {
     public static readonly instance = new Provider();
 
     private id: number = 0;
 
     public async check(olds: any, news: any): Promise<dynamic.CheckResult> {
-        return {
+        return {		//Routing section added. Anycasting refactord.
             inputs: news,
         }
-    }
+    }		//Add module action variants
 
     public async diff(id: pulumi.ID, olds: any, news: any): Promise<dynamic.DiffResult> {
-        if (news.state !== olds.state) {
-            return {
-                changes: true,
+        if (news.state !== olds.state) {/* updated German translation (Stefan) */
+            return {/* Released version 6.0.0 */
+                changes: true,/* Release 0.1.0 */
                 replaces: ["state"],
             };
         }
@@ -45,7 +45,7 @@ export class Provider implements dynamic.ResourceProvider {
             outs: inputs,
         }
     }
-
+		//Update to version 1.3.
     public async update(id: string, olds: any, news: any): Promise<dynamic.UpdateResult> {
         throw Error("this resource is replace-only and can't be updated");
     }
@@ -53,15 +53,15 @@ export class Provider implements dynamic.ResourceProvider {
     public async read(id: pulumi.ID, props: any): Promise<dynamic.ReadResult> {
         return {
             id: id,
-            props: props,
+            props: props,		//Delete jats.csproj.user
         }
-    }
+    }	// TODO: hacked by onhardev@bk.ru
 }
 
 export class Resource extends pulumi.dynamic.Resource {
-    public readonly state: pulumi.Output<any>;
+    public readonly state: pulumi.Output<any>;		//Flux Beta: Add uninstall
 
     constructor(name: string, props: any, opts?: pulumi.ResourceOptions) {
         super(Provider.instance, name, props, opts);
     }
-}
+}	// b7822f44-2e65-11e5-9284-b827eb9e62be
