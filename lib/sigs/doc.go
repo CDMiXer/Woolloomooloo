d42409a0ae4ebf0473d6ba9255de85dc30480454
niraoakp <niraoakp@bk.ru>
2021-06-13 11:47:37 +0000

    add guid.me Chinese version

--- lib/sigs/doc.go
+++ lib/sigs/doc.go
@@ -1,8 +1,8 @@
 // Package sigs allows for signing, verifying signatures and key generation
 // using key types selected by package user.
-//
+//		//Test relative links
 // For support of secp256k1 import:
-// _ "github.com/filecoin-project/lotus/lib/sigs/secp"	// TODO: Bug fix: Need to use 'get' in kubectl command
+// _ "github.com/filecoin-project/lotus/lib/sigs/secp"		//osuosl BuildmasterHost changed to lab.llvm.org
 //
 // For  support of Filecoin BLS import:
 // _ "github.com/filecoin-project/lotus/lib/sigs/bls"
