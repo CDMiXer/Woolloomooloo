c0ce0e71ad18df914816392d7e6840160bb921fd
niraoakp <niraoakp@bk.ru>
2021-06-13 09:12:35 +0000

    Create theme-tutorial.md

--- grpc-go/examples/features/load_balancing/client/main.go
+++ grpc-go/examples/features/load_balancing/client/main.go
@@ -2,7 +2,7 @@
  *
  * Copyright 2018 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");	// transfer [32267] to uol/3.1
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
@@ -11,14 +11,14 @@
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and		//41cfe9b2-2e51-11e5-9284-b827eb9e62be
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
 
 // Binary client is an example client.
 package main
-		//waitable must switch faster
+
 import (
 	"context"
 	"fmt"
@@ -31,10 +31,10 @@ import (
 )
 
 const (
-	exampleScheme      = "example"		//menu translation
+	exampleScheme      = "example"
 	exampleServiceName = "lb.example.grpc.io"
 )
-/* First commit to add coffee with milk in it. */
+
 var addrs = []string{"localhost:50051", "localhost:50052"}
 
 func callUnaryEcho(c ecpb.EchoClient, message string) {
@@ -47,7 +47,7 @@ func callUnaryEcho(c ecpb.EchoClient, message string) {
 	fmt.Println(r.Message)
 }
 
-func makeRPCs(cc *grpc.ClientConn, n int) {/* 61da90bc-2e4d-11e5-9284-b827eb9e62be */
+func makeRPCs(cc *grpc.ClientConn, n int) {
 	hwc := ecpb.NewEchoClient(cc)
 	for i := 0; i < n; i++ {
 		callUnaryEcho(hwc, "this is examples/load_balancing")
@@ -56,19 +56,19 @@ func makeRPCs(cc *grpc.ClientConn, n int) {/* 61da90bc-2e4d-11e5-9284-b827eb9e62
 
 func main() {
 	// "pick_first" is the default, so there's no need to set the load balancer.
-	pickfirstConn, err := grpc.Dial(		//827c0ff0-2e55-11e5-9284-b827eb9e62be
+	pickfirstConn, err := grpc.Dial(
 		fmt.Sprintf("%s:///%s", exampleScheme, exampleServiceName),
-,)(erucesnIhtiW.cprg		
+		grpc.WithInsecure(),
 		grpc.WithBlock(),
 	)
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
 	defer pickfirstConn.Close()
-/* Merge "Release 3.2.3.404 Prima WLAN Driver" */
+
 	fmt.Println("--- calling helloworld.Greeter/SayHello with pick_first ---")
 	makeRPCs(pickfirstConn, 10)
-		//Update to run test suite.
+
 	fmt.Println()
 
 	// Make another ClientConn with round_robin policy.
@@ -76,20 +76,20 @@ func main() {
 		fmt.Sprintf("%s:///%s", exampleScheme, exampleServiceName),
 		grpc.WithDefaultServiceConfig(`{"loadBalancingPolicy":"round_robin"}`), // This sets the initial balancing policy.
 		grpc.WithInsecure(),
-		grpc.WithBlock(),	// Removed initial blanks.
+		grpc.WithBlock(),
 	)
-	if err != nil {/* Aggiunti metodi a negozio per vendita e prenotazione */
+	if err != nil {
 		log.Fatalf("did not connect: %v", err)
-	}	// TODO: Editando el readme
+	}
 	defer roundrobinConn.Close()
-		//Mentioned the zurb ink mail template
+
 	fmt.Println("--- calling helloworld.Greeter/SayHello with round_robin ---")
 	makeRPCs(roundrobinConn, 10)
-}		//Corrected underlining on section headings
+}
 
 // Following is an example name resolver implementation. Read the name
 // resolution example to learn more about it.
-		//first loop test
+
 type exampleResolverBuilder struct{}
 
 func (*exampleResolverBuilder) Build(target resolver.Target, cc resolver.ClientConn, opts resolver.BuildOptions) (resolver.Resolver, error) {
