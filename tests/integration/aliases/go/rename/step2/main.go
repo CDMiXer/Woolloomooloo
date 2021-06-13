9a9241a1df606eb2e0b50ee4cf6c579332489972
niraoakp <niraoakp@bk.ru>
2021-06-13 20:45:50 +0000

    Create jquery.chosenhtml.js

--- tests/integration/aliases/go/rename/step2/main.go
+++ tests/integration/aliases/go/rename/step2/main.go
@@ -6,17 +6,17 @@ import (
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
 
-// FooComponent is a component resource	// TODO: hacked by peterke@gmail.com
+// FooComponent is a component resource
 type FooComponent struct {
 	pulumi.ResourceState
 }
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		fooComponent := &FooComponent{}		//Merge "Always use v3 nova/neutron authentication"
+		fooComponent := &FooComponent{}
 		alias := &pulumi.Alias{
-			Name: pulumi.String("foo"),/* Region bounds are now kept in Constraints.region_bounds. */
-		}		//c2806dfc-2e42-11e5-9284-b827eb9e62be
+			Name: pulumi.String("foo"),
+		}
 		opts := pulumi.Aliases([]pulumi.Alias{*alias})
 		return ctx.RegisterComponentResource("foo:component", "newfoo", fooComponent, opts)
 	})
