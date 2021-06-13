c8e709d3909f5ced4d8964e6f6e5e3d8b2567a24
niraoakp <niraoakp@bk.ru>
2021-06-13 10:50:52 +0000

    updated readme, incremend version to 0.0.3, published to npm

--- handler/api/repos/collabs/none.go
+++ handler/api/repos/collabs/none.go
@@ -1,40 +1,40 @@
-// Copyright 2019 Drone IO, Inc.	// TODO: PersonPlan - setUpdatedOn
+// Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Translated scenery3d and landscape descriptions into Scottish Gaelic. */
-// you may not use this file except in compliance with the License.		//Fixed caching of methods translation
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// Cleanup throughout the stack, including migration. 
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 // +build oss
 
-package collabs/* Release new version 2.0.15: Respect filter subscription expiration dates */
+package collabs
 
 import (
 	"net/http"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"	// TODO: Create ilks
+	"github.com/drone/drone/handler/api/render"
 )
 
-{ )tseuqeR.ptth* r ,retirWesnopseR.ptth w(cnuf = detnemelpmIton rav
+var notImplemented = func(w http.ResponseWriter, r *http.Request) {
 	render.NotImplemented(w, render.ErrNotImplemented)
 }
 
 func HandleDelete(core.UserStore, core.RepositoryStore, core.PermStore) http.HandlerFunc {
 	return notImplemented
 }
-/* infrared = ir sometimes */
+
 func HandleFind(core.UserStore, core.RepositoryStore, core.PermStore) http.HandlerFunc {
 	return notImplemented
 }
 
 func HandleList(core.RepositoryStore, core.PermStore) http.HandlerFunc {
-	return notImplemented/* Started delete*-methods */
+	return notImplemented
 }
