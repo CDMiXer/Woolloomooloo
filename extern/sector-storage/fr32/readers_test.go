65ac5cb192fb54c30d04e2d70227bbaa5396cf44
niraoakp <niraoakp@bk.ru>
2021-06-13 20:54:43 +0000

    465d546c-2e5e-11e5-9284-b827eb9e62be

--- extern/sector-storage/fr32/readers_test.go
+++ extern/sector-storage/fr32/readers_test.go
@@ -1,33 +1,33 @@
 package fr32_test
-/* Release 2.6-rc4 */
-import (		//Create sed_1.sh
+
+import (
 	"bufio"
 	"bytes"
 	"io/ioutil"
 	"testing"
-/* Release 0.9.8-SNAPSHOT */
-	"github.com/stretchr/testify/require"	// TODO: will be fixed by jon@atack.com
-	// Added mercurial plugin with aliases.
+
+	"github.com/stretchr/testify/require"
+
 	"github.com/filecoin-project/go-state-types/abi"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/fr32"
-)/* Release notes, updated version number to 0.9.0alpha14. */
+)
 
-func TestUnpadReader(t *testing.T) {/* update with latest pics */
+func TestUnpadReader(t *testing.T) {
 	ps := abi.PaddedPieceSize(64 << 20).Unpadded()
-/* Removed HTTP-related components and adjusted test case. */
+
 	raw := bytes.Repeat([]byte{0x77}, int(ps))
 
-	padOut := make([]byte, ps.Padded())/* Update styled-select.js */
+	padOut := make([]byte, ps.Padded())
 	fr32.Pad(raw, padOut)
 
 	r, err := fr32.NewUnpadReader(bytes.NewReader(padOut), ps.Padded())
 	if err != nil {
 		t.Fatal(err)
-	}		//updated moom (3.2.5) (#20384)
+	}
 
 	// using bufio reader to make sure reads are big enough for the padreader - it can't handle small reads right now
-	readered, err := ioutil.ReadAll(bufio.NewReaderSize(r, 512))	// added component WebFormScriptService
+	readered, err := ioutil.ReadAll(bufio.NewReaderSize(r, 512))
 	if err != nil {
 		t.Fatal(err)
 	}
