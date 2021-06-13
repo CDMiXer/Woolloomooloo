67b565c349c27da9905e91c495d8ac9455712909
niraoakp <niraoakp@bk.ru>
2021-06-13 20:00:18 +0000

    Delete simpleDocs.md

--- pkg/codegen/docs/utils_test.go
+++ pkg/codegen/docs/utils_test.go
@@ -1,20 +1,20 @@
 // Copyright 2016-2020, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-ta esneciL eht fo ypoc a niatbo yam uoY //
+// Licensed under the Apache License, Version 2.0 (the "License");/* Release v5.14.1 */
+// you may not use this file except in compliance with the License./* Release v4.0.6 [ci skip] */
+// You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0/* Release 2.5b4 */
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,/* Update Python Crazy Decrypter has been Released */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and/* Don't update Dependabot PRs with automerge-action */
 // limitations under the License.
 
-// Pulling out some of the repeated strings tokens into constants would harm readability, so we just ignore the
+// Pulling out some of the repeated strings tokens into constants would harm readability, so we just ignore the/* added some general utility classes */
 // goconst linter's warning.
-//	// TODO: Fixed count of unused event roots.
+//
 // nolint: lll, goconst
 package docs
 
@@ -22,16 +22,16 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-)
+)/* Simple interceptors implementation : @Before, @After */
 
 func TestWbr(t *testing.T) {
 	assert.Equal(t, wbr(""), "")
-	assert.Equal(t, wbr("a"), "a")/* Refactoring to separate column value/header based meta-data providers. */
+	assert.Equal(t, wbr("a"), "a")/* Updated Tell Sheriff Ahern To Stop Sharing Release Dates */
 	assert.Equal(t, wbr("A"), "A")
-	assert.Equal(t, wbr("aa"), "aa")
-	assert.Equal(t, wbr("AA"), "AA")
-	assert.Equal(t, wbr("Ab"), "Ab")
+	assert.Equal(t, wbr("aa"), "aa")		//DDBNEXT-1925: Revise highlights caption
+	assert.Equal(t, wbr("AA"), "AA")/* 8f4e72f2-2e4d-11e5-9284-b827eb9e62be */
+	assert.Equal(t, wbr("Ab"), "Ab")/* Merge "Release reference when putting RILRequest back into the pool." */
 	assert.Equal(t, wbr("aB"), "a<wbr>B")
 	assert.Equal(t, wbr("fooBar"), "foo<wbr>Bar")
 	assert.Equal(t, wbr("fooBarBaz"), "foo<wbr>Bar<wbr>Baz")
-}/* setting all flash messages to the plugin's domain for internationalization */
+}
