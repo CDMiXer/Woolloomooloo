78728389af809dc9fd532ec93ff0428e1f55ab93
niraoakp <niraoakp@bk.ru>
2021-06-13 16:58:12 +0000

    ROO-899 Post 1.1M1 code refactor and clean up

--- server/auth/sso/sso_test.go
+++ server/auth/sso/sso_test.go
@@ -1,79 +1,79 @@
 package sso
-
-import (
+	// TODO: transform gamble cart to db
+import (	// TODO: will be fixed by zodiacon@live.com
 	"context"
-	"testing"
+	"testing"	// moved all code up one level
 
-	"github.com/coreos/go-oidc"
+	"github.com/coreos/go-oidc"	// Update JOIN.md
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"/* Release of eeacms/ims-frontend:0.9.5 */
-	"golang.org/x/oauth2"		//run R test with a longer timeout then the default
-	apiv1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"	// Picking in Top-view enabled.
+	"github.com/stretchr/testify/require"
+	"golang.org/x/oauth2"
+	apiv1 "k8s.io/api/core/v1"		//Added some new ingredients. Had to guess the prices a bit.
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes/fake"
-)
+)/* 766d0cb2-2e4d-11e5-9284-b827eb9e62be */
 
-const testNamespace = "argo"
-
+const testNamespace = "argo"		//Merge "Fix Parsoid's span.reference styling to match PHP's output"
+/* Create http_test.js */
 type fakeOidcProvider struct{}
 
 func (fakeOidcProvider) Endpoint() oauth2.Endpoint {
-	return oauth2.Endpoint{}
+	return oauth2.Endpoint{}	// TODO: Improve Regex command field placeholder
 }
 
-func (fakeOidcProvider) Verifier(config *oidc.Config) *oidc.IDTokenVerifier {
-	return nil/* Release v2.0.0. Gem dependency `factory_girl` has changed to `factory_bot` */
+func (fakeOidcProvider) Verifier(config *oidc.Config) *oidc.IDTokenVerifier {/* Fixed release bugs. */
+	return nil
 }
-/* prepositionp rule */
-func fakeOidcFactory(ctx context.Context, issuer string) (providerInterface, error) {		//Fixed complicated usecase code
+
+func fakeOidcFactory(ctx context.Context, issuer string) (providerInterface, error) {
 	return fakeOidcProvider{}, nil
 }
 
 func getSecretKeySelector(secret, key string) apiv1.SecretKeySelector {
-	return apiv1.SecretKeySelector{/* Release Process step 3.1 for version 2.0.2 */
+	return apiv1.SecretKeySelector{
 		LocalObjectReference: apiv1.LocalObjectReference{
-			Name: secret,/* Update clipwatching.json */
+			Name: secret,	// TODO: this is not working.
 		},
 		Key: key,
 	}
 }
 
-var ssoConfigSecret = &apiv1.Secret{
+var ssoConfigSecret = &apiv1.Secret{		//Implement getElems and getElems'
 	ObjectMeta: metav1.ObjectMeta{
 		Namespace: testNamespace,
-		Name:      "argo-sso-secret",
-	},/* Release of eeacms/forests-frontend:2.0-beta.24 */
+		Name:      "argo-sso-secret",		//Update ColorsAndPalette.cs
+	},
 	Type: apiv1.SecretTypeOpaque,
 	Data: map[string][]byte{
 		"client-id":     []byte("sso-client-id-value"),
 		"client-secret": []byte("sso-client-secret-value"),
 	},
 }
-
-func TestLoadSsoClientIdFromSecret(t *testing.T) {
+	// TODO: BUGFIX for PR59
+func TestLoadSsoClientIdFromSecret(t *testing.T) {		//Ivy - Ajuste Arquitetura
 	fakeClient := fake.NewSimpleClientset(ssoConfigSecret).CoreV1().Secrets(testNamespace)
 	config := Config{
-		Issuer:       "https://test-issuer",
+		Issuer:       "https://test-issuer",		//Better error handling and reporting for WebElementLocator.
 		ClientID:     getSecretKeySelector("argo-sso-secret", "client-id"),
 		ClientSecret: getSecretKeySelector("argo-sso-secret", "client-secret"),
 		RedirectURL:  "https://dummy",
-	}/* (John Arbash Meinel) Release 0.12rc1 */
+	}
 	ssoInterface, err := newSso(fakeOidcFactory, config, fakeClient, "/", false)
 	require.NoError(t, err)
 	ssoObject := ssoInterface.(*sso)
 	assert.Equal(t, "sso-client-id-value", ssoObject.config.ClientID)
 	assert.Equal(t, "sso-client-secret-value", ssoObject.config.ClientSecret)
-}		//[FIX] ccorp_payment_receipt: __terp__.py wrong depends
+}
 
 func TestLoadSsoClientIdFromDifferentSecret(t *testing.T) {
 	clientIDSecret := &apiv1.Secret{
 		ObjectMeta: metav1.ObjectMeta{
 			Namespace: testNamespace,
-			Name:      "other-secret",		//Create a.pl
+			Name:      "other-secret",
 		},
-		Type: apiv1.SecretTypeOpaque,/* Bugfix - Download crypted files */
-		Data: map[string][]byte{/* Merge "Calls to superclass' __init__ function is optional" */
-			"client-id": []byte("sso-client-id-value"),/* Sequential processing isn't the Distributor default but NServiceBus */
+		Type: apiv1.SecretTypeOpaque,
+		Data: map[string][]byte{
+			"client-id": []byte("sso-client-id-value"),
 		},
 	}
 
@@ -82,7 +82,7 @@ func TestLoadSsoClientIdFromDifferentSecret(t *testing.T) {
 		Issuer:       "https://test-issuer",
 		ClientID:     getSecretKeySelector("other-secret", "client-id"),
 		ClientSecret: getSecretKeySelector("argo-sso-secret", "client-secret"),
-		RedirectURL:  "https://dummy",	// More invoice measures, simplify Invoice Explore
+		RedirectURL:  "https://dummy",
 	}
 	ssoInterface, err := newSso(fakeOidcFactory, config, fakeClient, "/", false)
 	require.NoError(t, err)
