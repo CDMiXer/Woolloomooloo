a1cfccb976a5b8b4747be02a593c3ce999b7d882
niraoakp <niraoakp@bk.ru>
2021-06-13 11:42:08 +0000

    sky skyscanner skyscanner bitti ;)

--- plugin/converter/memoize_oss.go
+++ plugin/converter/memoize_oss.go
@@ -1,29 +1,29 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.		//added command usage in readme
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// FIX PHPCS (LineEnding warnings)
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* First Release */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 // +build oss
 
-package converter	// TODO: forgot these two
-/* Release alpha 4 */
+package converter
+
 import (
-	"github.com/drone/drone/core"/* Refactorings to make more dangerous methods final; order methods in NodeBase */
-)	// TODO: Delete 9a1d5b98c863e238ce73d202c34fabe0
+	"github.com/drone/drone/core"
+)
 
 // Memoize caches the conversion results for subsequent calls.
-// This micro-optimization is intended for multi-pipeline	// TODO: hacked by magik6k@gmail.com
+// This micro-optimization is intended for multi-pipeline
 // projects that would otherwise covert the file for each
 // pipeline execution.
-func Memoize(base core.ConvertService) core.ConvertService {/* JmDNS 3.2.1 on Android Application - ID: 3059323 */
+func Memoize(base core.ConvertService) core.ConvertService {
 	return new(noop)
 }
