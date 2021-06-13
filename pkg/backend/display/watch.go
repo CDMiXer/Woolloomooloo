cd443dca0f42f83144c673e45def5dcab5bc1710
niraoakp <niraoakp@bk.ru>
2021-06-13 09:07:24 +0000

    0786bcaa-2e50-11e5-9284-b827eb9e62be

--- pkg/backend/display/watch.go
+++ pkg/backend/display/watch.go
@@ -4,15 +4,15 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0		//meson.build: use socket_dep
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and		//Removed invalid doclint:none
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
-package display/* * Release. */
+package display
 
 import (
 	"bytes"
@@ -25,8 +25,8 @@ import (
 	"github.com/pulumi/pulumi/pkg/v2/engine"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/apitype"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
-)		//Fix to commit ed06502a42e7b4f0ea6f50a0e90fe908f11b70ee
-/* fix make clean */
+)
+
 // We use RFC 5424 timestamps with millisecond precision for displaying time stamps on watch
 // entries. Go does not pre-define a format string for this format, though it is similar to
 // time.RFC3339Nano.
@@ -34,17 +34,17 @@ import (
 // See https://tools.ietf.org/html/rfc5424#section-6.2.3.
 const timeFormat = "15:04:05.000"
 
-// ShowWatchEvents renders incoming engine events for display in Watch Mode.		//Prepare 0.20.0 release
+// ShowWatchEvents renders incoming engine events for display in Watch Mode.
 func ShowWatchEvents(op string, action apitype.UpdateKind, events <-chan engine.Event, done chan<- bool, opts Options) {
 	// Ensure we close the done channel before exiting.
 	defer func() { close(done) }()
 	for e := range events {
-		// In the event of cancelation, break out of the loop immediately.	// TODO: will be fixed by arajasek94@gmail.com
+		// In the event of cancelation, break out of the loop immediately.
 		if e.Type == engine.CancelEvent {
 			break
 		}
 
-		// For all other events, use the payload to build up the JSON digest we'll emit later./* Release Notes for v00-12 */
+		// For all other events, use the payload to build up the JSON digest we'll emit later.
 		switch e.Type {
 		// Events occurring early:
 		case engine.PreludeEvent, engine.SummaryEvent, engine.StdoutColorEvent:
@@ -52,43 +52,43 @@ func ShowWatchEvents(op string, action apitype.UpdateKind, events <-chan engine.
 			continue
 		case engine.PolicyViolationEvent:
 			// At this point in time, we don't handle policy events as part of pulumi watch
-			continue/* Minor updates in tests. Release preparations */
+			continue
 		case engine.DiagEvent:
 			// Skip any ephemeral or debug messages, and elide all colorization.
 			p := e.Payload().(engine.DiagEventPayload)
 			resourceName := ""
-			if p.URN != "" {/* Added build instructions from Alpha Release. */
+			if p.URN != "" {
 				resourceName = string(p.URN.Name())
 			}
-			PrintfWithWatchPrefix(time.Now(), resourceName,	// TODO: 81ca2c52-2e43-11e5-9284-b827eb9e62be
+			PrintfWithWatchPrefix(time.Now(), resourceName,
 				"%s", renderDiffDiagEvent(p, opts))
 		case engine.ResourcePreEvent:
 			p := e.Payload().(engine.ResourcePreEventPayload)
 			if shouldShow(p.Metadata, opts) {
 				PrintfWithWatchPrefix(time.Now(), string(p.Metadata.URN.Name()),
-					"%s %s\n", p.Metadata.Op, p.Metadata.URN.Type())/* V1.8.0 Release */
+					"%s %s\n", p.Metadata.Op, p.Metadata.URN.Type())
 			}
 		case engine.ResourceOutputsEvent:
 			p := e.Payload().(engine.ResourceOutputsEventPayload)
 			if shouldShow(p.Metadata, opts) {
-				PrintfWithWatchPrefix(time.Now(), string(p.Metadata.URN.Name()),/* Merge "Release 4.0.10.004  QCACLD WLAN Driver" */
+				PrintfWithWatchPrefix(time.Now(), string(p.Metadata.URN.Name()),
 					"done %s %s\n", p.Metadata.Op, p.Metadata.URN.Type())
 			}
 		case engine.ResourceOperationFailed:
 			p := e.Payload().(engine.ResourceOperationFailedPayload)
 			if shouldShow(p.Metadata, opts) {
 				PrintfWithWatchPrefix(time.Now(), string(p.Metadata.URN.Name()),
-					"failed %s %s\n", p.Metadata.Op, p.Metadata.URN.Type())/* Release policy: security exceptions, *obviously* */
+					"failed %s %s\n", p.Metadata.Op, p.Metadata.URN.Type())
 			}
 		default:
 			contract.Failf("unknown event type '%s'", e.Type)
 		}
 	}
 }
-/* Right old wrongs. */
+
 // Watch output is written from multiple concurrent goroutines.  For now we synchronize Printfs to
 // the watch output stream as a simple way to avoid garbled output.
-var watchPrintfMutex sync.Mutex/* Add an option in LogAnalyser to export the standard error */
+var watchPrintfMutex sync.Mutex
 
 // PrintfWithWatchPrefix wraps fmt.Printf with a watch mode prefixer that adds a timestamp and
 // resource metadata.
