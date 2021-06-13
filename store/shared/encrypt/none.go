da6afa222dfcf6240e82fc83262e2c203e440980
niraoakp <niraoakp@bk.ru>
2021-06-13 17:20:59 +0000

    Merge "Add info on Nuage Networks driver"

--- store/shared/encrypt/none.go
+++ store/shared/encrypt/none.go
@@ -2,19 +2,19 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Debug main menu polishing */
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,		//Merge "Deduplicate configuration code block." into androidx-main
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Fixed tons of bugs */
+// See the License for the specific language governing permissions and
 // limitations under the License.
-	// Create ee.Geometry.MultiPoint
+
 package encrypt
-		//Merge branch 'master' into gelderland-dataset-names
-// none is an encryption strategy that stores secret/* better to not use a symbol here */
+
+// none is an encryption strategy that stores secret
 // values in plain text. This is the default strategy
 // when no key is specified.
 type none struct {
@@ -25,5 +25,5 @@ func (*none) Encrypt(plaintext string) ([]byte, error) {
 }
 
 func (*none) Decrypt(ciphertext []byte) (string, error) {
-	return string(ciphertext), nil	// Fixed 2nd link
+	return string(ciphertext), nil
 }
