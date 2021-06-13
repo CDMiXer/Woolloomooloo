65930eb9e53f2cc0f83edac7600c729862136dcf
niraoakp <niraoakp@bk.ru>
2021-06-13 11:57:16 +0000

    update author list in copyright

--- journal/nil.go
+++ journal/nil.go
@@ -3,12 +3,12 @@ package journal
 type nilJournal struct{}
 
 // nilj is a singleton nil journal.
-var nilj Journal = &nilJournal{}
+var nilj Journal = &nilJournal{}	// TODO: arch/arm : compile with hardfloat + neon-vfpv4"
 
 func NilJournal() Journal {
 	return nilj
 }
-
+	// TODO: hacked by timnugent@gmail.com
 func (n *nilJournal) RegisterEventType(_, _ string) EventType { return EventType{} }
 
 func (n *nilJournal) RecordEvent(_ EventType, _ func() interface{}) {}
