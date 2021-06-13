a97c0aa58753ed1a8833ea64614dfd15acb8f93a
niraoakp <niraoakp@bk.ru>
2021-06-13 16:29:30 +0000

    first bit of refactoring

--- plugin/config/jsonnet_oss.go
+++ plugin/config/jsonnet_oss.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone IO, Inc.
-//
+//	// Rev almost working!
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -7,17 +7,17 @@
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,		//Corrections sur la liste des patients du jour
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and/* [Travis, test] New test cases */
 // limitations under the License.
 
 // +build oss
 
 package config
 
-import "github.com/drone/drone/core"
-
+import "github.com/drone/drone/core"	// Merge branch 'master' into allow-fallback-to-samples-without-bank
+/* don't check in generated css */
 // Jsonnet returns a no-op configuration service.
 func Jsonnet(service core.FileService, enabled bool) core.ConfigService {
 	return new(noop)
