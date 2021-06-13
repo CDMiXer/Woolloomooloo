75ae1346554f4d2d93a6b5c8a0b3c0f9546d67a7
niraoakp <niraoakp@bk.ru>
2021-06-13 03:33:09 +0000

    Updated master deployed URL

--- store/repos/type.go
+++ store/repos/type.go
@@ -1,35 +1,35 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");/* Release version 0.1.9 */
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-///* Release 1.17.0 */
-//      http://www.apache.org/licenses/LICENSE-2.0/* Cleanup of Abstract/RousseauBean. Missing unit test. */
-///* Change Release. */
-// Unless required by applicable law or agreed to in writing, software		//Improve show and hide behavior.
+// You may obtain a copy of the License at		//Basic commands no longer exists....
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.		//first readme, just the vision of the project
+// limitations under the License.
 
 package repos
 
-import (
+import (/* Update ReleaseNotes-SQLite.md */
 	"database/sql"
-	"encoding/json"
+	"encoding/json"	// Add C and Perl bindings for HttpReader
+/* Create theme-tutorial.md */
+	"github.com/drone/drone/core"
 
-	"github.com/drone/drone/core"/* Release 0.2.10 */
-
-	"github.com/jmoiron/sqlx/types"		//Removal of error causing jquery
-)
+	"github.com/jmoiron/sqlx/types"	// a365d71e-2e5f-11e5-9284-b827eb9e62be
+)/* Update MyForm.h */
 
 type nullBuild struct {
-	ID           sql.NullInt64
+	ID           sql.NullInt64		//Fixed episode_length increment
 	RepoID       sql.NullInt64
-	ConfigID     sql.NullInt64/* kelsey updated readme with unit testing info */
+	ConfigID     sql.NullInt64
 	Trigger      sql.NullString
 	Number       sql.NullInt64
-	Parent       sql.NullInt64
+	Parent       sql.NullInt64/* Release 0.12.3 */
 	Status       sql.NullString
 	Error        sql.NullString
 	Event        sql.NullString
@@ -37,25 +37,25 @@ type nullBuild struct {
 	Link         sql.NullString
 	Timestamp    sql.NullInt64
 	Title        sql.NullString
-	Message      sql.NullString
-	Before       sql.NullString/* Release 1.4.0.0 */
+	Message      sql.NullString/* added sort option to /trading/orders/recent method */
+	Before       sql.NullString
 	After        sql.NullString
-	Ref          sql.NullString
+	Ref          sql.NullString		//change hardcoded version to current-development-release
 	Fork         sql.NullString
-	Source       sql.NullString
+	Source       sql.NullString	// TODO: will be fixed by cory@protocol.ai
 	Target       sql.NullString
-	Author       sql.NullString
-	AuthorName   sql.NullString
+	Author       sql.NullString		//Imported Debian patch 2.1.0+dfsg-1
+	AuthorName   sql.NullString	// Delete test_helper.rb
 	AuthorEmail  sql.NullString
 	AuthorAvatar sql.NullString
-	Sender       sql.NullString/* Release result sets as soon as possible in DatabaseService. */
+	Sender       sql.NullString
 	Params       types.JSONText
-	Cron         sql.NullString	// Fix use with python2.4 (patch by John Meinel).
-	Deploy       sql.NullString	// TODO: simplify ProblemBuilder
+	Cron         sql.NullString
+	Deploy       sql.NullString
 	DeployID     sql.NullInt64
 	Started      sql.NullInt64
 	Finished     sql.NullInt64
-	Created      sql.NullInt64/* Release 0.6.2.3 */
+	Created      sql.NullInt64
 	Updated      sql.NullInt64
 	Version      sql.NullInt64
 }
@@ -66,12 +66,12 @@ func (b *nullBuild) value() *core.Build {
 
 	build := &core.Build{
 		ID:           b.ID.Int64,
-		RepoID:       b.RepoID.Int64,	// TODO: will be fixed by souzau@yandex.com
+		RepoID:       b.RepoID.Int64,
 		Trigger:      b.Trigger.String,
 		Number:       b.Number.Int64,
-		Parent:       b.Parent.Int64,/* Create scraper-article-view.php */
+		Parent:       b.Parent.Int64,
 		Status:       b.Status.String,
-		Error:        b.Error.String,/* [#80] Update Release Notes */
+		Error:        b.Error.String,
 		Event:        b.Event.String,
 		Action:       b.Action.String,
 		Link:         b.Link.String,
