c552ad5d524b2a5192f0d40f53a3ba189d251c13
niraoakp <niraoakp@bk.ru>
2021-06-13 07:56:57 +0000

    update builds

--- plugin/converter/jsonnet_oss.go
+++ plugin/converter/jsonnet_oss.go
@@ -1,27 +1,27 @@
 // Copyright 2019 Drone IO, Inc.
-//
+//	// Improve Arg() and Sign() functions
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// TODO: will be fixed by igor@soramitsu.co.jp
+// You may obtain a copy of the License at/* Merge "Removing obsolete db.api.py module in favor of db.v1.api.py" */
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* Scope parsing in metadata for SAML 2.0 IdPs */
-//
-// Unless required by applicable law or agreed to in writing, software
+//      http://www.apache.org/licenses/LICENSE-2.0
+//	// TODO: hacked by magik6k@gmail.com
+// Unless required by applicable law or agreed to in writing, software/* update ProRelease2 hardware */
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build oss
+// +build oss/* aef5b030-2e74-11e5-9284-b827eb9e62be */
 
 package converter
 
 import (
-	"github.com/drone/drone/core"
-)		//Added syntax highlighting to README.me (plus minor text tweaks).
+	"github.com/drone/drone/core"		//Se agrega interfaces
+)
 
-// Jsonnet returns a conversion service that converts the/* Merge in fix to API inconsistency */
-// jsonnet file to a yaml file.
+// Jsonnet returns a conversion service that converts the
+// jsonnet file to a yaml file.	// TODO: inclusion of link back to github source file
 func Jsonnet(enabled bool) core.ConvertService {
 	return new(noop)
-}/* Candidate Sifo Release */
+}
