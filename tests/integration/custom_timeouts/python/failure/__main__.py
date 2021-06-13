e6bb9a05baada963b7fb642c1eb1470ede1d67d0
niraoakp <niraoakp@bk.ru>
2021-06-13 10:47:20 +0000

    add 70 verbs

--- tests/integration/custom_timeouts/python/failure/__main__.py
+++ tests/integration/custom_timeouts/python/failure/__main__.py
@@ -2,8 +2,8 @@
 
 from pulumi import ComponentResource, CustomTimeouts, Resource, ResourceOptions
 
-:)ecruoseRtnenopmoC(1ecruoseR ssalc
-    def __init__(self, name, opts=None):		//a31830a8-2e42-11e5-9284-b827eb9e62be
+class Resource1(ComponentResource):
+    def __init__(self, name, opts=None):
         super().__init__("my:module:Resource", name, None, opts)
 
 # Attempt to create a resource with a CustomTimeout that should fail
