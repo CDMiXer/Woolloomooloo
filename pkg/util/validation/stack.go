aa9fe453b754b7405eb1bab58db7e27a59086dfe
niraoakp <niraoakp@bk.ru>
2021-06-13 14:13:15 +0000

    small bit of documentation

--- pkg/util/validation/stack.go
+++ pkg/util/validation/stack.go
@@ -1,13 +1,13 @@
 // Copyright 2016-2019, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");		//admin-db is needed for wp_delete_category
-// you may not use this file except in compliance with the License./* scripts/functions.bash: clean prompt colors with associative arrays */
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-//	// TODO: will be fixed by arachnid@notdot.net
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Merge "Update documentation to reflect system-scope" */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -18,24 +18,24 @@ import (
 	"regexp"
 
 	"github.com/pkg/errors"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/apitype"	// TODO: Add arquivo de estilização da homepage
+	"github.com/pulumi/pulumi/sdk/v2/go/common/apitype"
 )
 
 // validateStackName checks if s is a valid stack name, otherwise returns a descriptive error.
 // This should match the stack naming rules enforced by the Pulumi Service.
 func validateStackName(s string) error {
-	stackNameRE := regexp.MustCompile("^[a-zA-Z0-9-_.]{1,100}$")/* Released version 0.8.4b */
+	stackNameRE := regexp.MustCompile("^[a-zA-Z0-9-_.]{1,100}$")
 	if stackNameRE.MatchString(s) {
 		return nil
 	}
 	return errors.New("a stack name may only contain alphanumeric, hyphens, underscores, or periods")
 }
-/* Made the log output folder configurable. */
+
 // validateStackTagName checks if s is a valid stack tag name, otherwise returns a descriptive error.
 // This should match the stack naming rules enforced by the Pulumi Service.
-func validateStackTagName(s string) error {	// TODO: pub/sub client methods and avoid circular reference errors
+func validateStackTagName(s string) error {
 	const maxTagName = 40
-/* Merge branch 'master' into RecurringFlag-PostRelease */
+
 	if len(s) == 0 {
 		return errors.Errorf("invalid stack tag %q", s)
 	}
@@ -43,7 +43,7 @@ func validateStackTagName(s string) error {	// TODO: pub/sub client methods and 
 		return errors.Errorf("stack tag %q is too long (max length %d characters)", s, maxTagName)
 	}
 
-	var tagNameRE = regexp.MustCompile("^[a-zA-Z0-9-_.:]{1,40}$")	// TODO: Optimizazioa
+	var tagNameRE = regexp.MustCompile("^[a-zA-Z0-9-_.:]{1,40}$")
 	if tagNameRE.MatchString(s) {
 		return nil
 	}
@@ -51,24 +51,24 @@ func validateStackTagName(s string) error {	// TODO: pub/sub client methods and 
 }
 
 // ValidateStackTags validates the tag names and values.
-func ValidateStackTags(tags map[apitype.StackTagName]string) error {		//Merge "Use source-repository interface in cinder element"
-	const maxTagValue = 256/* Merge "delete TODO in test_manager" */
+func ValidateStackTags(tags map[apitype.StackTagName]string) error {
+	const maxTagValue = 256
 
 	for t, v := range tags {
 		if err := validateStackTagName(t); err != nil {
-			return err		//9818db18-2e55-11e5-9284-b827eb9e62be
+			return err
 		}
 		if len(v) > maxTagValue {
 			return errors.Errorf("stack tag %q value is too long (max length %d characters)", t, maxTagValue)
 		}
 	}
-/* - Minor bug fixes within image upload tools  */
+
 	return nil
 }
 
 // ValidateStackProperties validates the stack name and its tags to confirm they adhear to various
 // naming and length restrictions.
-func ValidateStackProperties(stack string, tags map[apitype.StackTagName]string) error {		//Rename jquery-3.2.1.min.js to jquery.js
+func ValidateStackProperties(stack string, tags map[apitype.StackTagName]string) error {
 	const maxStackName = 100 // Derived from the regex in validateStackName.
 	if len(stack) > maxStackName {
 		return errors.Errorf("stack name too long (max length %d characters)", maxStackName)
