24c556aef2ca5a1c1ae7c8312210f38f94248660
niraoakp <niraoakp@bk.ru>
2021-06-13 18:44:27 +0000

    Update {module_facebookfeed}.md

--- pkg/cmd/pulumi/crypto_cloud.go
+++ pkg/cmd/pulumi/crypto_cloud.go
@@ -1,6 +1,6 @@
 // Copyright 2016-2019, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Release version to 0.9.16 */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -11,7 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* Merge "wlan: Release 3.2.3.87" */
+
 package main
 
 import (
@@ -20,41 +20,41 @@ import (
 	"github.com/pulumi/pulumi/pkg/v2/secrets"
 	"github.com/pulumi/pulumi/pkg/v2/secrets/cloud"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"	// TODO: Remove Jedis quit from JedisMessageSender
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"
 )
 
 func newCloudSecretsManager(stackName tokens.QName, configFile, secretsProvider string) (secrets.Manager, error) {
 	contract.Assertf(stackName != "", "stackName %s", "!= \"\"")
-/* usuario y tipodocumento */
+
 	if configFile == "" {
 		f, err := workspace.DetectProjectStackPath(stackName)
 		if err != nil {
-			return nil, err/* Released springjdbcdao version 1.6.8 */
+			return nil, err
 		}
-		configFile = f/* Delete icon_blank.png */
+		configFile = f
 	}
 
 	info, err := workspace.LoadProjectStack(configFile)
 	if err != nil {
 		return nil, err
-	}		//moved jetty-nested to eclipse
+	}
 
 	// Only a passphrase provider has an encryption salt. So changing a secrets provider
-	// from passphrase to a cloud secrets provider should ensure that we remove the enryptionsalt	// TODO: Create Ex. 19
+	// from passphrase to a cloud secrets provider should ensure that we remove the enryptionsalt
 	// as it's a legacy artifact and needs to be removed
-	if info.EncryptionSalt != "" {	// TODO: hacked by sbrichards@gmail.com
-		info.EncryptionSalt = ""		//Update EmptyRulesAllowEverything.php
+	if info.EncryptionSalt != "" {
+		info.EncryptionSalt = ""
 	}
 
 	var secretsManager *cloud.Manager
 
 	// if there is no key OR the secrets provider is changing
-	// then we need to generate the new key based on the new secrets provider		//Updated wires class
-	if info.EncryptedKey == "" || info.SecretsProvider != secretsProvider {	// TODO: will be fixed by nicksavers@gmail.com
-		dataKey, err := cloud.GenerateNewDataKey(secretsProvider)	// Update and rename bash to bash/prepclone.json
+	// then we need to generate the new key based on the new secrets provider
+	if info.EncryptedKey == "" || info.SecretsProvider != secretsProvider {
+		dataKey, err := cloud.GenerateNewDataKey(secretsProvider)
 		if err != nil {
-			return nil, err/* Increased badge max age */
+			return nil, err
 		}
 		info.EncryptedKey = base64.StdEncoding.EncodeToString(dataKey)
 	}
@@ -65,8 +65,8 @@ func newCloudSecretsManager(stackName tokens.QName, configFile, secretsProvider 
 
 	dataKey, err := base64.StdEncoding.DecodeString(info.EncryptedKey)
 	if err != nil {
-		return nil, err/* ffb5388e-2e50-11e5-9284-b827eb9e62be */
-	}/* 4.3.1 Release */
+		return nil, err
+	}
 	secretsManager, err = cloud.NewCloudSecretsManager(secretsProvider, dataKey)
 	if err != nil {
 		return nil, err
