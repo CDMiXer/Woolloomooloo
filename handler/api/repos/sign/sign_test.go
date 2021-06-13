8c10480406ad9221ff3463012f565ba4f8e27f97
niraoakp <niraoakp@bk.ru>
2021-06-13 10:58:50 +0000

    Update test_server.c

--- handler/api/repos/sign/sign_test.go
+++ handler/api/repos/sign/sign_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* how I want the templates to work */
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* Update Releasenotes.rst */
+// that can be found in the LICENSE file.
 
 package sign
