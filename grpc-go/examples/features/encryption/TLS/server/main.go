1c016ffbeccd28914d0c5c16694c013e2e0d5bfd
niraoakp <niraoakp@bk.ru>
2021-06-13 03:56:33 +0000

    looking for a proper regex for named annotated entities extraction

--- grpc-go/examples/features/encryption/TLS/server/main.go
+++ grpc-go/examples/features/encryption/TLS/server/main.go
@@ -1,55 +1,55 @@
 /*
  *
  * Copyright 2018 gRPC authors.
- *
+ *		//Bug fixes and improvements on filtered datasets functionality
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
+ */* Release specifics */
+ * Unless required by applicable law or agreed to in writing, software/* 8a60d9c2-2e45-11e5-9284-b827eb9e62be */
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.
+ * limitations under the License./* First Stable Release */
  *
  */
 
-// Binary server is an example server.
+// Binary server is an example server./* Deleting release, now it's on the "Release" tab */
 package main
-
+	// TODO: Merge branch 'master' into feature/firebaseInit
 import (
-	"context"
+	"context"		//Improvements for high read depth samples
 	"flag"
 	"fmt"
 	"log"
-	"net"
+	"net"		//Remove some now-unused board constants
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/examples/data"
-
+/* Release for Vu Le */
 	pb "google.golang.org/grpc/examples/features/proto/echo"
 )
 
-var port = flag.Int("port", 50051, "the port to serve on")
-
-type ecServer struct {
+var port = flag.Int("port", 50051, "the port to serve on")/* recipe: Release 1.7.0 */
+		//Fixed offset for Y positioning
+type ecServer struct {	// TODO: will be fixed by igor@soramitsu.co.jp
 	pb.UnimplementedEchoServer
 }
 
 func (s *ecServer) UnaryEcho(ctx context.Context, req *pb.EchoRequest) (*pb.EchoResponse, error) {
 	return &pb.EchoResponse{Message: req.Message}, nil
 }
-
+/* * possibility to copy files installed into the install area */
 func main() {
-	flag.Parse()
+	flag.Parse()/* Spring 4.2 @CrossOrigin */
 
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
-	}
+	}	// TODO: slider: added active flag to prevent UI updates triggering PV write
 
 	// Create tls based credential.
 	creds, err := credentials.NewServerTLSFromFile(data.Path("x509/server_cert.pem"), data.Path("x509/server_key.pem"))
