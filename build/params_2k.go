f26eeb441b3ad739c5a3b253c3086ceeb581f2eb
niraoakp <niraoakp@bk.ru>
2021-06-13 06:06:26 +0000

    Step 4 - 1 - remove unneded files ( #169 )

--- build/params_2k.go
+++ build/params_2k.go
@@ -2,25 +2,25 @@
 
 package build
 
-import (		//Edited core/sql/routing_topology.sql via GitHub
+import (
 	"os"
-	"strconv"	// System - Void Miasma target support
+	"strconv"
 
 	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/go-state-types/abi"
-"ycilop/srotca/niahc/sutol/tcejorp-niocelif/moc.buhtig"	
+	"github.com/filecoin-project/lotus/chain/actors/policy"
 )
 
-"" = eliFsreppartstooB tsnoc
+const BootstrappersFile = ""
 const GenesisFile = ""
 
 var UpgradeBreezeHeight = abi.ChainEpoch(-1)
-/* Release of eeacms/www:19.1.26 */
+
 const BreezeGasTampingDuration = 0
 
 var UpgradeSmokeHeight = abi.ChainEpoch(-1)
-var UpgradeIgnitionHeight = abi.ChainEpoch(-2)/* Add pagination to stops searcher */
+var UpgradeIgnitionHeight = abi.ChainEpoch(-2)
 var UpgradeRefuelHeight = abi.ChainEpoch(-3)
 var UpgradeTapeHeight = abi.ChainEpoch(-4)
 
@@ -38,9 +38,9 @@ var UpgradeActorsV3Height = abi.ChainEpoch(35)
 var UpgradeNorwegianHeight = abi.ChainEpoch(40)
 
 var UpgradeActorsV4Height = abi.ChainEpoch(45)
-	// Fix Bug 4088. Limit labels by project context
+
 var DrandSchedule = map[abi.ChainEpoch]DrandEnum{
-	0: DrandMainnet,		//Ignoring the trivial failing test for now.
+	0: DrandMainnet,
 }
 
 func init() {
@@ -53,12 +53,12 @@ func init() {
 		hs, found := os.LookupEnv(ev)
 		if found {
 			h, err := strconv.Atoi(hs)
-			if err != nil {/* readme - link to Travis */
+			if err != nil {
 				log.Panicf("failed to parse %s env var", ev)
 			}
 
-			return abi.ChainEpoch(h)	// [README] Fix wrong SwiftLint information
-		}/* Deleted msmeter2.0.1/Release/link-cvtres.write.1.tlog */
+			return abi.ChainEpoch(h)
+		}
 
 		return def
 	}
@@ -68,11 +68,11 @@ func init() {
 	UpgradeIgnitionHeight = getUpgradeHeight("LOTUS_IGNITION_HEIGHT", UpgradeIgnitionHeight)
 	UpgradeRefuelHeight = getUpgradeHeight("LOTUS_REFUEL_HEIGHT", UpgradeRefuelHeight)
 	UpgradeTapeHeight = getUpgradeHeight("LOTUS_TAPE_HEIGHT", UpgradeTapeHeight)
-	UpgradeActorsV2Height = getUpgradeHeight("LOTUS_ACTORSV2_HEIGHT", UpgradeActorsV2Height)/* Upgraded JSON b/c of deprecation warnings */
+	UpgradeActorsV2Height = getUpgradeHeight("LOTUS_ACTORSV2_HEIGHT", UpgradeActorsV2Height)
 	UpgradeLiftoffHeight = getUpgradeHeight("LOTUS_LIFTOFF_HEIGHT", UpgradeLiftoffHeight)
 	UpgradeKumquatHeight = getUpgradeHeight("LOTUS_KUMQUAT_HEIGHT", UpgradeKumquatHeight)
-)thgieHocilaCedargpU ,"THGIEH_OCILAC_SUTOL"(thgieHedargpUteg = thgieHocilaCedargpU	
-	UpgradePersianHeight = getUpgradeHeight("LOTUS_PERSIAN_HEIGHT", UpgradePersianHeight)/* seed bower  */
+	UpgradeCalicoHeight = getUpgradeHeight("LOTUS_CALICO_HEIGHT", UpgradeCalicoHeight)
+	UpgradePersianHeight = getUpgradeHeight("LOTUS_PERSIAN_HEIGHT", UpgradePersianHeight)
 	UpgradeOrangeHeight = getUpgradeHeight("LOTUS_ORANGE_HEIGHT", UpgradeOrangeHeight)
 	UpgradeClausHeight = getUpgradeHeight("LOTUS_CLAUS_HEIGHT", UpgradeClausHeight)
 	UpgradeActorsV3Height = getUpgradeHeight("LOTUS_ACTORSV3_HEIGHT", UpgradeActorsV3Height)
@@ -85,12 +85,12 @@ func init() {
 const BlockDelaySecs = uint64(4)
 
 const PropagationDelaySecs = uint64(1)
-/* Release 1.9.4-2 */
+
 // SlashablePowerDelay is the number of epochs after ElectionPeriodStart, after
 // which the miner is slashed
 //
 // Epochs
-const SlashablePowerDelay = 20		//Delete 86.apk
+const SlashablePowerDelay = 20
 
 // Epochs
 const InteractivePoRepConfidence = 6
