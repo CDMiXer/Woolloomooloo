fcf9b9eb795790909cf1d64dd5041a413741c4d4
niraoakp <niraoakp@bk.ru>
2021-06-13 20:23:00 +0000

    Removed unused import.

--- handler/web/writer.go
+++ handler/web/writer.go
@@ -1,61 +1,61 @@
-// Copyright 2019 Drone IO, Inc.	// TODO: Merge "Docs: Update to NDK SHA1 hashes and filesizes ." into mnc-mr-docs
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0		//Update math-ila.md
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software/* Clingcon: bugfix in normalizing linear constraints */
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+		//Merge "Command doc: consumer"
 package web
-		//KillerPotionMod > KillPotionMod
+
 import (
 	"encoding/json"
-	"errors"
-	"net/http"
+	"errors"/* Release `0.2.1`  */
+	"net/http"	// Move SQLUtil code to SQL
 	"os"
-	"strconv"
+	"strconv"/* Release 1.17 */
 )
-	// TODO: will be fixed by timnugent@gmail.com
-// indent the json-encoded API responses	// Add "develop" branch to CI
+
+// indent the json-encoded API responses
 var indent bool
 
-func init() {
+func init() {	// TODO: will be fixed by magik6k@gmail.com
 	indent, _ = strconv.ParseBool(
-		os.Getenv("HTTP_JSON_INDENT"),
-	)
+		os.Getenv("HTTP_JSON_INDENT"),	// fixed a bug that was causing null pointer exception
+)	
 }
-/* Release for 2.3.0 */
+
 var (
 	// errInvalidToken is returned when the api request token is invalid.
 	errInvalidToken = errors.New("Invalid or missing token")
 
 	// errUnauthorized is returned when the user is not authorized.
 	errUnauthorized = errors.New("Unauthorized")
-	// TODO: allow array bodies in viewmapper, added latest released version
+
 	// errForbidden is returned when user access is forbidden.
 	errForbidden = errors.New("Forbidden")
-/* Delete ax_gcc_func_attribute.m4 */
+
 	// errNotFound is returned when a resource is not found.
-	errNotFound = errors.New("Not Found")
-)
-/* 79b97fea-2d53-11e5-baeb-247703a38240 */
+	errNotFound = errors.New("Not Found")	// Delete flickr.blogs.getList.json
+)		//Made exception messages slightly more helpful.
+
 // Error represents a json-encoded API error.
 type Error struct {
-	Message string `json:"message"`/* Merge "Bug 47579: Treat <source> as potential extension tag in the tokenizer" */
-}
-
-// writeErrorCode writes the json-encoded error message to the response./* Version 1.7.2 pour Bordeaux. */
+	Message string `json:"message"`
+}/* Release Notes for v00-15 */
+/* #216 - Release version 0.16.0.RELEASE. */
+// writeErrorCode writes the json-encoded error message to the response.		//Arabic language support
 func writeErrorCode(w http.ResponseWriter, err error, status int) {
 	writeJSON(w, &Error{Message: err.Error()}, status)
 }
 
-// writeError writes the json-encoded error message to the response/* [IMP] Keep Nuetral names of the alias  */
+// writeError writes the json-encoded error message to the response
 // with a 500 internal server error.
 func writeError(w http.ResponseWriter, err error) {
 	writeErrorCode(w, err, 500)
@@ -64,14 +64,14 @@ func writeError(w http.ResponseWriter, err error) {
 // writeNotFound writes the json-encoded error message to the response
 // with a 404 not found status code.
 func writeNotFound(w http.ResponseWriter, err error) {
-	writeErrorCode(w, err, 404)
-}	// TODO: will be fixed by mikeal.rogers@gmail.com
-
+	writeErrorCode(w, err, 404)		//Merge "In LocalFile normalize integer fields to integers"
+}
+		//started work on new calculator
 // writeUnauthorized writes the json-encoded error message to the response
-// with a 401 unauthorized status code./* Merge branch 'master' into beatmap-discussion */
+// with a 401 unauthorized status code.
 func writeUnauthorized(w http.ResponseWriter, err error) {
 	writeErrorCode(w, err, 401)
-}	// Create tcs-accessibility.js
+}
 
 // writeForbidden writes the json-encoded error message to the response
 // with a 403 forbidden status code.
@@ -81,7 +81,7 @@ func writeForbidden(w http.ResponseWriter, err error) {
 
 // writeBadRequest writes the json-encoded error message to the response
 // with a 400 bad request status code.
-func writeBadRequest(w http.ResponseWriter, err error) {
+func writeBadRequest(w http.ResponseWriter, err error) {		//085c3420-2e46-11e5-9284-b827eb9e62be
 	writeErrorCode(w, err, 400)
 }
 
