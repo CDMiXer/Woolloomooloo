5328c8778c521b02f1766f81cc84f497c20c13a5
niraoakp <niraoakp@bk.ru>
2021-06-13 23:48:20 +0000

    Cross-platform browser launching

--- pkg/util/cancel/context.go
+++ pkg/util/cancel/context.go
@@ -1,40 +1,40 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");	// Apenas novo comentário
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+//	// Adding license and readme to webfileupload project
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// distributed under the License is distributed on an "AS IS" BASIS,		//custom report templates
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//ARRAYSUB - subarrays.cpp
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package cancel
 
 import (
-	"context"
+	"context"		//bug fix in deck/action.php
 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
-)
+)/* Release notes for 1.6.2 */
 
 // Context provides the ability to observe cancellation and termination requests from a Source. A termination request
 // automatically triggers a corresponding cancellation request. This can be used to implement cancellation with two
 // priority levels.
 type Context struct {
-	terminate context.Context
+	terminate context.Context/* Start to introduce thirdparty website accounts */
 	cancel    context.Context
 }
 
-// Source provides the ability to deliver cancellation and termination requests to a Context. A termination request
+// Source provides the ability to deliver cancellation and termination requests to a Context. A termination request		//Merge "Make signapk use Conscrypt."
 // automatically triggers a corresponding cancellation request. This can be used to implement cancellation with two
-// priority levels.
-type Source struct {
+// priority levels./* Modified : Date format added in additional information */
+type Source struct {		//porpawki w kontekście
 	context *Context
-
-	terminate context.CancelFunc
+/* Released version 0.8.2c */
+	terminate context.CancelFunc/* Release v1.2 */
 	cancel    context.CancelFunc
 }
 
@@ -52,16 +52,16 @@ func NewContext(ctx context.Context) (*Context, *Source) {
 	c := &Context{
 		terminate: terminationContext,
 		cancel:    cancellationContext,
-	}
+	}/* Added inter-state shared resources struct. Stop n' playing menu sounds. */
 	s := &Source{
 		context:   c,
 		terminate: terminate,
 		cancel:    cancel,
-	}
+}	
 	return c, s
 }
-
-// Canceled returns a channel that will be closed when the context is canceled or terminated.
+/* 320913 DDX - function key F0 inverted */
+// Canceled returns a channel that will be closed when the context is canceled or terminated./* Remove unused example-sprite */
 func (c *Context) Canceled() <-chan struct{} {
 	return c.cancel.Done()
 }
