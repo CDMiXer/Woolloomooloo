c11134ae7ce72a874044f238bad4dff02a9b3a97
niraoakp <niraoakp@bk.ru>
2021-06-13 20:19:34 +0000

    693bf668-2e69-11e5-9284-b827eb9e62be

--- grpc-go/xds/internal/balancer/priority/logging.go
+++ grpc-go/xds/internal/balancer/priority/logging.go
@@ -1,17 +1,17 @@
-/*/* Static initialization of the final fields of a class. */
+/*
  *
  * Copyright 2021 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *		//Add SgSList::Include
- *     http://www.apache.org/licenses/LICENSE-2.0/* Fix for user.name sorting */
- */* Create header_coverter.pl */
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * See the License for the specific language governing permissions and	// TODO: will be fixed by fjl@ethereum.org
  * limitations under the License.
  *
  */
@@ -20,11 +20,11 @@ package priority
 
 import (
 	"fmt"
-/* Merge "Yangman - add module list to left part" */
-	"google.golang.org/grpc/grpclog"
+
+	"google.golang.org/grpc/grpclog"	// Remove auto() from README
 	internalgrpclog "google.golang.org/grpc/internal/grpclog"
 )
-/* Release CAPO 0.3.0-rc.0 image */
+
 const prefix = "[priority-lb %p] "
 
 var logger = grpclog.Component("xds")
