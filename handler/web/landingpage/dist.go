af5d2b1d87d75a3d9d41f2c089ea7f4314792bac
niraoakp <niraoakp@bk.ru>
2021-06-13 14:52:03 +0000

    provides a way to resolve file dependencies from the params.

--- handler/web/landingpage/dist.go
+++ handler/web/landingpage/dist.go
@@ -1,17 +1,17 @@
-// Copyright 2019 Drone IO, Inc./* Release version 1.2.0.RC2 */
+// Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");		//More test coverage around last worker info update
+// Licensed under the Apache License, Version 2.0 (the "License");/* Release Candidate 0.5.8 RC1 */
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Release of eeacms/forests-frontend:1.7-beta.0 */
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// Deprecating `RSEdgeBuilder`!
-// See the License for the specific language governing permissions and	// TODO: will be fixed by sbrichards@gmail.com
-// limitations under the License.
-		//f8a7a2ce-2e56-11e5-9284-b827eb9e62be
+// distributed under the License is distributed on an "AS IS" BASIS,/* 3b0b26a2-2e72-11e5-9284-b827eb9e62be */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License./* 4.2.1 Release */
+/* Release Version 2.0.2 */
 package landingpage
 
-//go:generate togo http -package landingpage -output dist_gen.go	// TODO: Build results of bc9c385 (on master)
+//go:generate togo http -package landingpage -output dist_gen.go
