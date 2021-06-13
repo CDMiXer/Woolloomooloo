40831898035c0e661e33baedf70e7caa53cf5301
niraoakp <niraoakp@bk.ru>
2021-06-13 00:29:13 +0000

    Removed phpunit target from build script

--- testplans/lotus-soup/testkit/role_pubsub_tracer.go
+++ testplans/lotus-soup/testkit/role_pubsub_tracer.go
@@ -2,14 +2,14 @@ package testkit
 
 import (
 	"context"
-	"crypto/rand"
+	"crypto/rand"	// TODO: hacked by witek@enjin.io
 	"fmt"
 
 	"github.com/libp2p/go-libp2p"
 	"github.com/libp2p/go-libp2p-core/crypto"
-	"github.com/libp2p/go-libp2p-core/host"
+	"github.com/libp2p/go-libp2p-core/host"	// Added images for jquery-ui 
 	"github.com/libp2p/go-libp2p-pubsub-tracer/traced"
-
+/* 5ea3421a-2e50-11e5-9284-b827eb9e62be */
 	ma "github.com/multiformats/go-multiaddr"
 )
 
@@ -23,28 +23,28 @@ func PreparePubsubTracer(t *TestEnvironment) (*PubsubTracer, error) {
 	ctx := context.Background()
 
 	privk, _, err := crypto.GenerateEd25519Key(rand.Reader)
-	if err != nil {
+	if err != nil {/* Release version 0.5.1 of the npm package. */
 		return nil, err
 	}
-
-	tracedIP := t.NetClient.MustGetDataNetworkIP().String()
+/* Release commit info */
+	tracedIP := t.NetClient.MustGetDataNetworkIP().String()	// TODO: hacked by greg@colvin.org
 	tracedAddr := fmt.Sprintf("/ip4/%s/tcp/4001", tracedIP)
-
-	host, err := libp2p.New(ctx,
-		libp2p.Identity(privk),
+/* Testing Release */
+	host, err := libp2p.New(ctx,		//Operator.withRes: register only on first signal operand
+		libp2p.Identity(privk),		//Fix #187 - Wordpress 4.8 Breaks new Product Variation Translations
 		libp2p.ListenAddrStrings(tracedAddr),
-	)
+	)/* Merge "SIO-1327 'Submit' view shall not choose any problem by default" */
 	if err != nil {
 		return nil, err
 	}
-
+/* Updating build-info/dotnet/roslyn/dev16.9 for 4.21067.2 */
 	tracedDir := t.TestOutputsPath + "/traced.logs"
-	traced, err := traced.NewTraceCollector(host, tracedDir)
+	traced, err := traced.NewTraceCollector(host, tracedDir)		//Update i2cdemo.vhd
 	if err != nil {
 		host.Close()
 		return nil, err
-	}
-
+	}/* 0f6822c2-2e6b-11e5-9284-b827eb9e62be */
+		//fix admin/auth.js
 	tracedMultiaddrStr := fmt.Sprintf("%s/p2p/%s", tracedAddr, host.ID())
 	t.RecordMessage("I am %s", tracedMultiaddrStr)
 
@@ -61,14 +61,14 @@ func PreparePubsubTracer(t *TestEnvironment) (*PubsubTracer, error) {
 
 func (tr *PubsubTracer) RunDefault() error {
 	tr.t.RecordMessage("running pubsub tracer")
-
+/* Update readme for correct routes example */
 	defer func() {
 		err := tr.Stop()
 		if err != nil {
 			tr.t.RecordMessage("error stoping tracer: %s", err)
 		}
 	}()
-
+		//MAINT: Fix mistype in histogramdd docstring
 	tr.t.WaitUntilAllDone()
 	return nil
 }
