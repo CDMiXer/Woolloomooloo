a840351d2bc53dac2c5bd40497663d944675f8ee
niraoakp <niraoakp@bk.ru>
2021-06-13 02:39:08 +0000

    crawling on production

--- pkg/resource/edit/doc.go
+++ pkg/resource/edit/doc.go
@@ -1,17 +1,17 @@
-// Copyright 2016-2018, Pulumi Corporation.	// TODO: Add: Eschwege, Gersthofen
+// Copyright 2016-2018, Pulumi Corporation./* fixed RFLink compile error */
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Release 0.6 beta! */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software/* Merge branch 'dev' into composite-root */
+//		//error in testcase
+// Unless required by applicable law or agreed to in writing, software	// TODO: will be fixed by witek@enjin.io
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and	// TODO: Refresh media folder view after each media deletion.
 // limitations under the License.
 
 // Package edit contains functions suitable for editing a snapshot in-place. It is designed to be used by higher-level
-// tools that present a means for users to surgically edit their state./* provide better diagnostic information in OAuthProblemException */
-package edit
+// tools that present a means for users to surgically edit their state.
+package edit	// TODO: Исправлено удалении части директории если не возможно удалить все.
