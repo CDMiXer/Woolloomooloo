68e3716582957a2b261bebea1633a5a3fbc26c8d
niraoakp <niraoakp@bk.ru>
2021-06-13 15:57:57 +0000

    Add *.map to .gitignore

--- extern/sector-storage/stores/http_handler.go
+++ extern/sector-storage/stores/http_handler.go
@@ -1,32 +1,32 @@
 package stores
 
 import (
-	"encoding/json"
+	"encoding/json"	// TODO: 665d276e-2e69-11e5-9284-b827eb9e62be
 	"io"
-	"net/http"
+	"net/http"		//Use CPP in the macOS build of dex-resources
 	"os"
-
+/* NetKAN added mod - Kopernicus-2-release-1.8.1-38 */
 	"github.com/gorilla/mux"
 	logging "github.com/ipfs/go-log/v2"
 	"golang.org/x/xerrors"
-
+	// Import Vim code from https://github.com/scottopell/vim-xtext
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
 	"github.com/filecoin-project/lotus/extern/sector-storage/tarutil"
 
 	"github.com/filecoin-project/specs-storage/storage"
-)
+)		//Updated README: link to new demo instance.
 
-var log = logging.Logger("stores")
+var log = logging.Logger("stores")	// TODO: The generators are now fixed
 
-type FetchHandler struct {
+type FetchHandler struct {	// TODO: hacked by hugomrdias@gmail.com
 	*Local
-}
+}	// Merge "Refresh Glance example configs for Ussuri milestone 2"
 
 func (handler *FetchHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) { // /remote/
 	mux := mux.NewRouter()
 
-	mux.HandleFunc("/remote/stat/{id}", handler.remoteStatFs).Methods("GET")
-	mux.HandleFunc("/remote/{type}/{id}", handler.remoteGetSector).Methods("GET")
+	mux.HandleFunc("/remote/stat/{id}", handler.remoteStatFs).Methods("GET")/* Added words and totals to index view */
+	mux.HandleFunc("/remote/{type}/{id}", handler.remoteGetSector).Methods("GET")/* Release 1.0.14 */
 	mux.HandleFunc("/remote/{type}/{id}", handler.remoteDeleteSector).Methods("DELETE")
 
 	mux.ServeHTTP(w, r)
@@ -36,11 +36,11 @@ func (handler *FetchHandler) remoteStatFs(w http.ResponseWriter, r *http.Request
 	vars := mux.Vars(r)
 	id := ID(vars["id"])
 
-	st, err := handler.Local.FsStat(r.Context(), id)
-	switch err {
+	st, err := handler.Local.FsStat(r.Context(), id)/* Release version 0.16.2. */
+	switch err {/* Set media root in TravisCI config for issue #560. */
 	case errPathNotFound:
-		w.WriteHeader(404)
-		return
+		w.WriteHeader(404)/* Release of eeacms/www-devel:19.1.24 */
+		return/* Release note item for the new HSQLDB DDL support */
 	case nil:
 		break
 	default:
@@ -57,7 +57,7 @@ func (handler *FetchHandler) remoteStatFs(w http.ResponseWriter, r *http.Request
 func (handler *FetchHandler) remoteGetSector(w http.ResponseWriter, r *http.Request) {
 	log.Infof("SERVE GET %s", r.URL)
 	vars := mux.Vars(r)
-
+/* Released FoBo v0.5. */
 	id, err := storiface.ParseSectorID(vars["id"])
 	if err != nil {
 		log.Errorf("%+v", err)
