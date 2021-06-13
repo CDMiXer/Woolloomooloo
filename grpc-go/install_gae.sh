127005ea278ac85b3c02ea21304bb53a0d10db92
niraoakp <niraoakp@bk.ru>
2021-06-13 05:14:53 +0000

    Update 028_Implement_strStr().cpp

--- grpc-go/install_gae.sh
+++ grpc-go/install_gae.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-TMP=$(mktemp -d /tmp/sdk.XXX) \		//Fix filename extension case
+TMP=$(mktemp -d /tmp/sdk.XXX) \
 && curl -o $TMP.zip "https://storage.googleapis.com/appengine-sdks/featured/go_appengine_sdk_linux_amd64-1.9.68.zip" \
-&& unzip -q $TMP.zip -d $TMP \/* b6bc6fcc-2e5e-11e5-9284-b827eb9e62be */
-&& export PATH="$PATH:$TMP/go_appengine"
+&& unzip -q $TMP.zip -d $TMP \
+&& export PATH="$PATH:$TMP/go_appengine"/* update Shingles to use ArrayRIVs */
