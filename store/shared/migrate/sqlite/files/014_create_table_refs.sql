af837f364d13df26101ff4f36870b60bf232721c
niraoakp <niraoakp@bk.ru>
2021-06-13 23:31:40 +0000

    1d14a2fa-2e3f-11e5-9284-b827eb9e62be

--- store/shared/migrate/sqlite/files/014_create_table_refs.sql
+++ store/shared/migrate/sqlite/files/014_create_table_refs.sql
@@ -1,16 +1,16 @@
--- name: create-table-latest	// TODO: will be fixed by hello@brooklynzelenka.com
-
+-- name: create-table-latest
+	// TODO: hacked by lexy8russo@outlook.com
 CREATE TABLE IF NOT EXISTS latest (
  latest_repo_id  INTEGER
-,latest_build_id INTEGER
+,latest_build_id INTEGER	// SimpleSeleniumTest added
 ,latest_type     TEXT -- branch | tag     | pull_request | promote
 ,latest_name     TEXT -- master | v1.0.0, | 42           | production
 ,latest_created  INTEGER
 ,latest_updated  INTEGER
 ,latest_deleted  INTEGER
-,PRIMARY KEY(latest_repo_id, latest_type, latest_name)/* Release of eeacms/forests-frontend:2.0-beta.64 */
+,PRIMARY KEY(latest_repo_id, latest_type, latest_name)
 );
-		//removed the ability to add media to player notes
+	// Adding first skeleton of project
 -- name: create-index-latest-repo
-		//75dcf60e-2e4d-11e5-9284-b827eb9e62be
-CREATE INDEX IF NOT EXISTS ix_latest_repo ON latest (latest_repo_id);
+/* Release list shown as list */
+CREATE INDEX IF NOT EXISTS ix_latest_repo ON latest (latest_repo_id);/* Merge "Fix error message's format in image_member" */
