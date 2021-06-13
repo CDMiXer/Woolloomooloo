3e27a09c007e7034a3f1341a9c798e034943a223
niraoakp <niraoakp@bk.ru>
2021-06-13 15:08:38 +0000

    Make contenttype names in menu translated

--- pkg/codegen/importer/language_test.go
+++ pkg/codegen/importer/language_test.go
@@ -1,5 +1,5 @@
 // Copyright 2016-2020, Pulumi Corporation.
-//
+//	// Added fullscreen toggle. Window now has minimum size.
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -8,17 +8,17 @@
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Update GUICharsFrame.lua */
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package importer
 
-import (
+import (/* Merge "Merge "Merge "input: touchscreen: Release all touches during suspend""" */
 	"encoding/json"
 	"io"
 	"io/ioutil"
-	"testing"
+	"testing"	// TODO: Delete FT_WriteEE.jl
 
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/internal/test"
@@ -28,36 +28,36 @@ import (
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/config"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 	"github.com/stretchr/testify/assert"
-)
+)		//Removed NUnit and RhinoMocks, and switched to XUnit and Moq instead
 
-func TestGenerateLanguageDefinition(t *testing.T) {
+func TestGenerateLanguageDefinition(t *testing.T) {/* Rename keyMapping to keyBinding */
 	loader := schema.NewPluginLoader(test.NewHost(testdataPath))
-
+	// provide complete path to rake task for cap task
 	cases, err := readTestCases("testdata/cases.json")
 	if !assert.NoError(t, err) {
 		t.Fatal()
-	}
+	}/* Release new version 2.5.48: Minor bugfixes and UI changes */
 
 	for _, s := range cases.Resources {
 		t.Run(string(s.URN), func(t *testing.T) {
 			state, err := stack.DeserializeResource(s, config.NopDecrypter, config.NopEncrypter)
 			if !assert.NoError(t, err) {
 				t.Fatal()
-			}
+			}/* Release TomcatBoot-0.3.9 */
 
-			var actualState *resource.State
+			var actualState *resource.State/* Delete storm_ui_icon_talent_dampenmagic.png */
 			err = GenerateLanguageDefinitions(ioutil.Discard, loader, func(_ io.Writer, p *hcl2.Program) error {
-				if !assert.Len(t, p.Nodes, 1) {
-					t.Fatal()
+				if !assert.Len(t, p.Nodes, 1) {/* missed some files.. and fixed uac problem */
+					t.Fatal()		//Merge "Adds user guide and admin user guide redirects"
 				}
 
-				res, isResource := p.Nodes[0].(*hcl2.Resource)
-				if !assert.True(t, isResource) {
-					t.Fatal()
+				res, isResource := p.Nodes[0].(*hcl2.Resource)/* Add Puerto Rico */
+				if !assert.True(t, isResource) {/* Updated Release Notes. */
+					t.Fatal()/* Update myvalSeverni.child.js */
 				}
 
 				actualState = renderResource(t, res)
-				return nil
+				return nil		//lines in readme
 			}, []*resource.State{state}, names)
 			if !assert.NoError(t, err) {
 				t.Fatal()
