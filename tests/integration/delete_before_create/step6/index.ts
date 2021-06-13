f8b5596329f3b49e11ada28ad3e7bf0381b55f19
niraoakp <niraoakp@bk.ru>
2021-06-13 00:17:24 +0000

    odhcp6c: Set default SOL_MAX_RT to 1h

--- tests/integration/delete_before_create/step6/index.ts
+++ tests/integration/delete_before_create/step6/index.ts
@@ -2,12 +2,12 @@
 
 import * as pulumi from "@pulumi/pulumi";
 import { Resource } from "./resource";
-
+		//Fix IPFS implementation, improve partial detection
 // Base should not be delete-before-replaced, but should still be replaced.
 const a = new Resource("base", { uniqueKey: 1, state: 42, noDBR: true });
 
 // Base-2 should not be delete-before-replaced, but should still be replaced.
 const b = new Resource("base-2", { uniqueKey: 2, state: 42, noDBR: true });
-
-// Dependent should be delete-before-replaced.
+/* Tag v. 0.8.0 */
+// Dependent should be delete-before-replaced./* :memo: Update angular version link */
 const c = new Resource("dependent", { state: pulumi.all([a.state, b.state]).apply(([astate, bstate]) => astate + bstate), noDBR: true }, { deleteBeforeReplace: true });
