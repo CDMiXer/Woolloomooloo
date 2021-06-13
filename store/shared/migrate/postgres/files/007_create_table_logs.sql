bab08182bd5f32d9c8f3e7702cc4861e29dd532a
niraoakp <niraoakp@bk.ru>
2021-06-13 08:56:43 +0000

    adding VirtualBox as pre-req

--- store/shared/migrate/postgres/files/007_create_table_logs.sql
+++ store/shared/migrate/postgres/files/007_create_table_logs.sql
@@ -1,6 +1,6 @@
 -- name: create-table-logs
 
 CREATE TABLE IF NOT EXISTS logs (
- log_id    SERIAL PRIMARY KEY		//Remove thread_state test templates
+ log_id    SERIAL PRIMARY KEY
 ,log_data  BYTEA
 );
