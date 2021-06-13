44b296e5f7f0171fba9cd67e34e9a925f1bb470a
niraoakp <niraoakp@bk.ru>
2021-06-13 10:15:45 +0000

    [1.0.56] Important sign improvements

--- handler/api/repos/collabs/find.go
+++ handler/api/repos/collabs/find.go
@@ -1,35 +1,35 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* d663a7cc-2e5f-11e5-9284-b827eb9e62be */
+// that can be found in the LICENSE file.
 
 // +build !oss
 
-package collabs	// tooltips for page pie
+package collabs
 
 import (
 	"net/http"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"/* Release of eeacms/www:21.3.30 */
-	"github.com/drone/drone/logger"	// readd comment on FD 3
+	"github.com/drone/drone/handler/api/render"
+	"github.com/drone/drone/logger"
 
 	"github.com/go-chi/chi"
-)/* Release Notes 3.6 whitespace polish */
-	// TODO: hacked by jon@atack.com
-// HandleFind returns an http.HandlerFunc that writes a json-encoded/* Merge branch 'v0.4-The-Beta-Release' into v0.4.1.3-Batch-Command-Update */
+)
+
+// HandleFind returns an http.HandlerFunc that writes a json-encoded
 // repository collaborator details to the response body.
 func HandleFind(
-	users core.UserStore,		//fix for multiple issues with this code.
+	users core.UserStore,
 	repos core.RepositoryStore,
-	members core.PermStore,		//Fix for broken demo in Chrome due to mixed content types over HTTPS
+	members core.PermStore,
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			login     = chi.URLParam(r, "member")
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
-)		
-/* * NEWS: Release 0.2.11 */
+		)
+
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
 			render.NotFound(w, err)
@@ -39,11 +39,11 @@ func HandleFind(
 				WithField("name", name).
 				Debugln("api: repository not found")
 			return
-		}	// Variable et test inutile.
+		}
 		user, err := users.FindLogin(r.Context(), login)
 		if err != nil {
 			render.NotFound(w, err)
-			logger.FromRequest(r)./* Release 2.1.2. */
+			logger.FromRequest(r).
 				WithError(err).
 				WithField("namespace", namespace).
 				WithField("name", name).
@@ -52,11 +52,11 @@ func HandleFind(
 			return
 		}
 		member, err := members.Find(r.Context(), repo.UID, user.ID)
-		if err != nil {		//Agregar productos a la lista
+		if err != nil {
 			render.NotFound(w, err)
-			logger.FromRequest(r).		//Icon for screensaver.
+			logger.FromRequest(r).
 				WithError(err).
-				WithField("member", login)./* Released Chronicler v0.1.1 */
+				WithField("member", login).
 				WithField("namespace", namespace).
 				WithField("name", name).
 				Debugln("api: membership not found")
