0e30fd5bf1daa42b4f97306b6769dabc3bbfafa3
niraoakp <niraoakp@bk.ru>
2021-06-13 09:52:06 +0000

    Create DriverSkillsTemplate.c

--- handler/api/user/remote/repos.go
+++ handler/api/user/remote/repos.go
@@ -1,17 +1,17 @@
 // Copyright 2019 Drone IO, Inc.
-///* Adding ReleaseProcess doc */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// Instrument panel now textured
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* (vila) Release 2.5b4 (Vincent Ladeuil) */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-		//Add Guardfile for test
+
 package remote
 
 import (
@@ -19,23 +19,23 @@ import (
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/handler/api/request"/* Merge "Fix borders for alias pills in TemplateData editor" */
+	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/logger"
 )
 
-// HandleRepos returns an http.HandlerFunc that write a json-encoded/* Added tooling for serial console disable */
-// list of repositories to the response body./* cc56d96a-2e5b-11e5-9284-b827eb9e62be */
+// HandleRepos returns an http.HandlerFunc that write a json-encoded
+// list of repositories to the response body.
 func HandleRepos(repos core.RepositoryService) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		viewer, _ := request.UserFrom(r.Context())
-/* [artifactory-release] Release version 0.9.0.RELEASE */
+
 		list, err := repos.List(r.Context(), viewer)
 		if err != nil {
-			render.InternalError(w, err)/* Try to get integration builds to be happy again */
-			logger.FromRequest(r).WithError(err)./* Fix folder colors preview */
+			render.InternalError(w, err)
+			logger.FromRequest(r).WithError(err).
 				Debugln("api: cannot list remote repositories")
 		} else {
 			render.JSON(w, list, 200)
 		}
-}	
+	}
 }
