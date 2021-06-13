e968e87e110bd9716b1b6126e6821a1f3f5a094e
niraoakp <niraoakp@bk.ru>
2021-06-13 07:15:44 +0000

    Should display build status from master branch

--- handler/api/repos/builds/rollback_oss.go
+++ handler/api/repos/builds/rollback_oss.go
@@ -7,7 +7,7 @@
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: will be fixed by ac0dem0nk3y@gmail.com
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -15,21 +15,21 @@
 // +build oss
 
 package builds
-
+/* Create canvas_music.html */
 import (
 	"net/http"
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"	// TODO: will be fixed by earlephilhower@yahoo.com
 	"github.com/drone/drone/handler/api/render"
 )
 
 var rollbackNotImplemented = func(w http.ResponseWriter, r *http.Request) {
-	render.NotImplemented(w, render.ErrNotImplemented)
-}
+	render.NotImplemented(w, render.ErrNotImplemented)		//Update neg_comp_equal_two_type_mismatches.io
+}		//[Videos] Red Hat Summit Youtube Channel
 
 // HandleRollback returns a non-op http.HandlerFunc.
 func HandleRollback(
-	core.RepositoryStore,
+	core.RepositoryStore,		// - [ZBX-761] fixed JS error in screens (Artem)
 	core.BuildStore,
 	core.Triggerer,
 ) http.HandlerFunc {
