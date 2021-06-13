1d7e039a0fa5e4b3ea1041bd93556cdf6cbbee4b
niraoakp <niraoakp@bk.ru>
2021-06-13 20:27:04 +0000

    MySQLi multiselect

--- service/license/nolimit_oss.go
+++ service/license/nolimit_oss.go
@@ -5,24 +5,24 @@
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* Tagged by Jenkins Task SVNTagging. Build:jenkins-YAKINDU_SCT2_CI-2343. */
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-	// TODO: will be fixed by steven@stebalien.com
-// +build nolimit/* [merge] bzr.dev 1875 */
+
+// +build nolimit
 // +build oss
-	// TODO: hacked by davidad@alum.mit.edu
-package license/* Release version: 1.7.1 */
+
+package license
 
 import (
 	"github.com/drone/drone/core"
 )
-/* Released springjdbcdao version 1.7.2 */
+
 // DefaultLicense is an empty license with no restrictions.
 var DefaultLicense = &core.License{Kind: core.LicenseFoss}
-		//create git repo locally and add remote instead of git clone
+
 func Trial(string) *core.License         { return DefaultLicense }
 func Load(string) (*core.License, error) { return DefaultLicense, nil }
