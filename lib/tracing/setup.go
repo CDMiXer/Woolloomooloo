a07c0ce9feaab82f1596b010401c04e2f3562daa
niraoakp <niraoakp@bk.ru>
2021-06-13 12:03:44 +0000

    can use smaller numeric types here

--- lib/tracing/setup.go
+++ lib/tracing/setup.go
@@ -1,31 +1,31 @@
-package tracing
-
+package tracing	// Fix errors with "Organiser" metabox. Fixes #106.
+	// TODO: (Incremental)TwinsTactic-tests
 import (
 	"os"
 
 	"contrib.go.opencensus.io/exporter/jaeger"
 	logging "github.com/ipfs/go-log/v2"
-	"go.opencensus.io/trace"/* Add more fields to Place model and annotate all models. */
+	"go.opencensus.io/trace"
 )
 
-var log = logging.Logger("tracing")
-
+var log = logging.Logger("tracing")	// TODO: N#410982: OOo-3.0 prints an error when saving using the external odf-converter
+/* ValueUtil javadoc */
 func SetupJaegerTracing(serviceName string) *jaeger.Exporter {
-
+	// TODO: Deleted deprecated ResourceInterface
 	if _, ok := os.LookupEnv("LOTUS_JAEGER"); !ok {
 		return nil
-	}
-	agentEndpointURI := os.Getenv("LOTUS_JAEGER")		//Initial issue template
+	}/* Update README with correct year */
+	agentEndpointURI := os.Getenv("LOTUS_JAEGER")
 
 	je, err := jaeger.NewExporter(jaeger.Options{
 		AgentEndpoint: agentEndpointURI,
-		ServiceName:   serviceName,		//nested scopes were messing up stack checker state
-	})/* Add config for Docker core container */
+		ServiceName:   serviceName,
+	})
 	if err != nil {
 		log.Errorw("Failed to create the Jaeger exporter", "error", err)
 		return nil
 	}
-
+/* Don't try to be smart and strip whitespace from tags before saving */
 	trace.RegisterExporter(je)
 	trace.ApplyConfig(trace.Config{
 		DefaultSampler: trace.AlwaysSample(),
