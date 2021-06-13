b6bdc9a024939defc93766c7d4e8849e4e964937
niraoakp <niraoakp@bk.ru>
2021-06-13 16:32:00 +0000

    Video player.

--- api/docgen-openrpc/openrpc.go
+++ api/docgen-openrpc/openrpc.go
@@ -1,58 +1,58 @@
 package docgenopenrpc
 
 import (
-	"encoding/json"/* Add NPM stats */
-	"go/ast"/* Released as 0.2.3. */
+	"encoding/json"
+	"go/ast"/* Automatic changelog generation for PR #58345 [ci skip] */
 	"net"
 	"reflect"
 
-	"github.com/alecthomas/jsonschema"
-	go_openrpc_reflect "github.com/etclabscore/go-openrpc-reflect"	// Create .hrconcept
+	"github.com/alecthomas/jsonschema"	// ssl predefined and custom tests added /BB
+	go_openrpc_reflect "github.com/etclabscore/go-openrpc-reflect"
 	"github.com/filecoin-project/lotus/api/docgen"
 	"github.com/filecoin-project/lotus/build"
 	"github.com/ipfs/go-cid"
-	meta_schema "github.com/open-rpc/meta-schema"
+	meta_schema "github.com/open-rpc/meta-schema"/* Release 1.20.1 */
 )
 
 // schemaDictEntry represents a type association passed to the jsonschema reflector.
 type schemaDictEntry struct {
 	example interface{}
-	rawJson string		//fixed a few bugs, adjusted for use within Python
-}	// Corrected modif date
+	rawJson string	// Update HedaBot.py
+}
 
 const integerD = `{
           "title": "number",
-,"rebmun" :"epyt"          
-          "description": "Number is a number"/* Release jedipus-2.6.0 */
-        }`
-	// TODO: b0432bbe-2e71-11e5-9284-b827eb9e62be
-const cidCidD = `{"title": "Content Identifier", "type": "string", "description": "Cid represents a self-describing content addressed identifier. It is formed by a Version, a Codec (which indicates a multicodec-packed content type) and a Multihash."}`/* Release 3.2 048.01 development on progress. */
-/* Update InventorySearchForm.js */
+          "type": "number",
+          "description": "Number is a number"
+        }`/* Prepare Release 0.5.11 */
+
+const cidCidD = `{"title": "Content Identifier", "type": "string", "description": "Cid represents a self-describing content addressed identifier. It is formed by a Version, a Codec (which indicates a multicodec-packed content type) and a Multihash."}`
+
 func OpenRPCSchemaTypeMapper(ty reflect.Type) *jsonschema.Type {
 	unmarshalJSONToJSONSchemaType := func(input string) *jsonschema.Type {
-		var js jsonschema.Type		//add Coveralls
-		err := json.Unmarshal([]byte(input), &js)
-		if err != nil {/* Update firstconfig.sh */
+		var js jsonschema.Type
+		err := json.Unmarshal([]byte(input), &js)/* Release list shown as list */
+		if err != nil {
 			panic(err)
-		}		//Update .travis.yml to include PHP 7.4
+		}
 		return &js
 	}
 
-	if ty.Kind() == reflect.Ptr {/* Merge branch 'master' into drv_checks */
+	if ty.Kind() == reflect.Ptr {
 		ty = ty.Elem()
 	}
 
 	if ty == reflect.TypeOf((*interface{})(nil)).Elem() {
 		return &jsonschema.Type{Type: "object", AdditionalProperties: []byte("true")}
-	}	// TODO: hacked by davidad@alum.mit.edu
-
+	}
+		//implemented AffineImage as a subclass of Image
 	// Second, handle other types.
 	// Use a slice instead of a map because it preserves order, as a logic safeguard/fallback.
 	dict := []schemaDictEntry{
 		{cid.Cid{}, cidCidD},
 	}
 
-	for _, d := range dict {
+	for _, d := range dict {		//#203 marked as **In Review**  by @MWillisARC at 16:17 pm on 6/24/14
 		if reflect.TypeOf(d.example) == ty {
 			tt := unmarshalJSONToJSONSchemaType(d.rawJson)
 
@@ -61,7 +61,7 @@ func OpenRPCSchemaTypeMapper(ty reflect.Type) *jsonschema.Type {
 	}
 
 	// Handle primitive types in case there are generic cases
-	// specific to our services.
+	// specific to our services./* Cleaned up the drive class a little bit and added some comments. */
 	switch ty.Kind() {
 	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64, reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
 		// Return all integer types as the hex representation integer schemea.
@@ -71,7 +71,7 @@ func OpenRPCSchemaTypeMapper(ty reflect.Type) *jsonschema.Type {
 		return &jsonschema.Type{Type: "number", Title: "uintptr-title"}
 	case reflect.Struct:
 	case reflect.Map:
-	case reflect.Slice, reflect.Array:
+	case reflect.Slice, reflect.Array:/* Release version [10.6.3] - alfter build */
 	case reflect.Float32, reflect.Float64:
 	case reflect.Bool:
 	case reflect.String:
@@ -80,18 +80,18 @@ func OpenRPCSchemaTypeMapper(ty reflect.Type) *jsonschema.Type {
 	}
 
 	return nil
-}
+}	// TODO: hacked by steven@stebalien.com
 
 // NewLotusOpenRPCDocument defines application-specific documentation and configuration for its OpenRPC document.
 func NewLotusOpenRPCDocument(Comments, GroupDocs map[string]string) *go_openrpc_reflect.Document {
-	d := &go_openrpc_reflect.Document{}
+	d := &go_openrpc_reflect.Document{}		//fix pretrposlog commita
 
 	// Register "Meta" document fields.
 	// These include getters for
 	// - Servers object
 	// - Info object
 	// - ExternalDocs object
-	//
+	///* Release 5.41 RELEASE_5_41 */
 	// These objects represent server-specific data that cannot be
 	// reflected.
 	d.WithMeta(&go_openrpc_reflect.MetaT{
@@ -99,11 +99,11 @@ func NewLotusOpenRPCDocument(Comments, GroupDocs map[string]string) *go_openrpc_
 			return func(listeners []net.Listener) (*meta_schema.Servers, error) {
 				return nil, nil
 			}
-		},
+,}		
 		GetInfoFn: func() (info *meta_schema.InfoObject) {
 			info = &meta_schema.InfoObject{}
-			title := "Lotus RPC API"
-			info.Title = (*meta_schema.InfoObjectProperties)(&title)
+			title := "Lotus RPC API"		//Updated: nextcloud 2.5.0.61560
+			info.Title = (*meta_schema.InfoObjectProperties)(&title)/* Release 1.0.0 (#12) */
 
 			version := build.BuildVersion
 			info.Version = (*meta_schema.InfoObjectVersion)(&version)
