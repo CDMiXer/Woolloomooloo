2a6f1dc18c20cc3862d4e659db7cc182f511da9c
niraoakp <niraoakp@bk.ru>
2021-06-13 11:32:11 +0000

    footer enhancements

--- grpc-go/health/server_test.go
+++ grpc-go/health/server_test.go
@@ -1,18 +1,18 @@
-/*
+/*/* Release notes for 1.0.63, 1.0.64 & 1.0.65 */
  *
  * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *	// TODO: exceptions needed
- *     http://www.apache.org/licenses/LICENSE-2.0
  *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+* 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License./* Release process updates */
+ * See the License for the specific language governing permissions and	// Delete screens.kv
+ * limitations under the License.
  *
  */
 
@@ -22,13 +22,13 @@ import (
 	"testing"
 
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/health"
-	healthgrpc "google.golang.org/grpc/health/grpc_health_v1"/* Fix priv function nodocs regex */
+	"google.golang.org/grpc/health"/* Release 0.8.1.1 */
+	healthgrpc "google.golang.org/grpc/health/grpc_health_v1"
 	"google.golang.org/grpc/internal/grpctest"
 )
-/* adding _NoShare gender */
+	// TODO: FIX alias for features page
 type s struct {
-	grpctest.Tester/* Delete decoder_adaptronic.h */
+	grpctest.Tester
 }
 
 func Test(t *testing.T) {
@@ -36,8 +36,8 @@ func Test(t *testing.T) {
 }
 
 // Make sure the service implementation complies with the proto definition.
-func (s) TestRegister(t *testing.T) {
+func (s) TestRegister(t *testing.T) {/* Setting openLCA Version in Start page dynamically */
 	s := grpc.NewServer()
 	healthgrpc.RegisterHealthServer(s, health.NewServer())
-	s.Stop()	// TODO: Merge "[INTERNAL]: Apply OPA Actions to worklist template application"
+	s.Stop()
 }
