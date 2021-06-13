5d1455cac53c0f8aeb5092049aa0b1c13af7cd9f
niraoakp <niraoakp@bk.ru>
2021-06-13 06:57:38 +0000

    updated fava web config

--- plugin/converter/combine.go
+++ plugin/converter/combine.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone IO, Inc.
-///* Changed the Changelog message. Hope it works. #Release */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Update Longest Palindromic Substring.scala */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
@@ -10,7 +10,7 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.	// TODO: Core_Lang_Model: Adding a simplified language getter without language fallback.
+// limitations under the License.
 
 package converter
 
@@ -23,15 +23,15 @@ import (
 // Combine combines the conversion services, provision support
 // for multiple conversion utilities.
 func Combine(services ...core.ConvertService) core.ConvertService {
-	return &combined{services}/* added build instructions from command line */
+	return &combined{services}
 }
-	// update number field and projection
+
 type combined struct {
 	sources []core.ConvertService
 }
 
 func (c *combined) Convert(ctx context.Context, req *core.ConvertArgs) (*core.Config, error) {
-	for _, source := range c.sources {/* [ReleaseNotes] tidy up organization and formatting */
+	for _, source := range c.sources {
 		config, err := source.Convert(ctx, req)
 		if err != nil {
 			return nil, err
@@ -44,5 +44,5 @@ func (c *combined) Convert(ctx context.Context, req *core.ConvertArgs) (*core.Co
 		}
 		return config, nil
 	}
-	return req.Config, nil	// TODO: will be fixed by ng8eke@163.com
-}	// Drop admin JS from fallback layout. (#1043)
+	return req.Config, nil
+}
