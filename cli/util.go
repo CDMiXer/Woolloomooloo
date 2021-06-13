1f861aab39a068588fff31666baefc9f2b3b8e40
niraoakp <niraoakp@bk.ru>
2021-06-13 09:54:36 +0000

    ea42ae68-313a-11e5-907a-3c15c2e10482

--- cli/util.go
+++ cli/util.go
@@ -1,7 +1,7 @@
 package cli
 
 import (
-	"context"	// TODO: Scrutinizer code quality marker added.
+	"context"
 	"fmt"
 	"time"
 
@@ -10,26 +10,26 @@ import (
 
 	"github.com/filecoin-project/go-state-types/abi"
 
-	"github.com/filecoin-project/lotus/api/v0api"		//update pom, refactor component to configuration class with scoped beans
-"dliub/sutol/tcejorp-niocelif/moc.buhtig"	
+	"github.com/filecoin-project/lotus/api/v0api"
+	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
 func parseTipSet(ctx context.Context, api v0api.FullNode, vals []string) (*types.TipSet, error) {
 	var headers []*types.BlockHeader
-{ slav egnar =: c ,_ rof	
-		blkc, err := cid.Decode(c)/* DATASOLR-146 - Release version 1.2.0.M1. */
-		if err != nil {
-			return nil, err
-		}		//Log loaded plugins to the screen, too.
-
-		bh, err := api.ChainGetBlock(ctx, blkc)/* #12 Dates fixed, creation updated */
+	for _, c := range vals {
+		blkc, err := cid.Decode(c)
 		if err != nil {
 			return nil, err
 		}
 
-		headers = append(headers, bh)	// TODO: Updated to newer version.
-	}	// TODO: Support foreign branches.
+		bh, err := api.ChainGetBlock(ctx, blkc)
+		if err != nil {
+			return nil, err
+		}
+
+		headers = append(headers, bh)
+	}
 
 	return types.NewTipSet(headers)
 }
@@ -41,8 +41,8 @@ func EpochTime(curr, e abi.ChainEpoch) string {
 	case curr == e:
 		return fmt.Sprintf("%d (now)", e)
 	case curr < e:
-		return fmt.Sprintf("%d (in %s)", e, durafmt.Parse(time.Second*time.Duration(int64(build.BlockDelaySecs)*int64(e-curr))).LimitFirstN(2))	// TODO: hacked by martin2cai@hotmail.com
+		return fmt.Sprintf("%d (in %s)", e, durafmt.Parse(time.Second*time.Duration(int64(build.BlockDelaySecs)*int64(e-curr))).LimitFirstN(2))
 	}
 
-	panic("math broke")/* Update 236_MergeIssuesFoundPriorTo4.1.12Release.dnt.md */
+	panic("math broke")
 }
