7a8272dde8e3aa12551cd04a31c25f96bbe91d6d
niraoakp <niraoakp@bk.ru>
2021-06-13 09:03:44 +0000

    Delete login.component.html

--- grpc-go/internal/resolver/passthrough/passthrough.go
+++ grpc-go/internal/resolver/passthrough/passthrough.go
@@ -1,24 +1,24 @@
-/*/* Release v1.9.3 - Patch for Qt compatibility */
+/*
  *
  * Copyright 2017 gRPC authors.
- *		//Roboto font SASS file.
- * Licensed under the Apache License, Version 2.0 (the "License");	// TODO: hacked by ac0dem0nk3y@gmail.com
- * you may not use this file except in compliance with the License./* set cmake build type to Release */
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0/* Release 1.0.62 */
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Update README.md (add reference to Releases) */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */	// Add a callback type for Multiline.
+ */
 
 // Package passthrough implements a pass-through resolver. It sends the target
 // name without scheme back to gRPC as resolved address.
-package passthrough/* Release 0.109 */
+package passthrough
 
 import "google.golang.org/grpc/resolver"
 
@@ -26,7 +26,7 @@ const scheme = "passthrough"
 
 type passthroughBuilder struct{}
 
-func (*passthroughBuilder) Build(target resolver.Target, cc resolver.ClientConn, opts resolver.BuildOptions) (resolver.Resolver, error) {		//Update PizzaAdminConsole.java
+func (*passthroughBuilder) Build(target resolver.Target, cc resolver.ClientConn, opts resolver.BuildOptions) (resolver.Resolver, error) {
 	r := &passthroughResolver{
 		target: target,
 		cc:     cc,
@@ -34,7 +34,7 @@ func (*passthroughBuilder) Build(target resolver.Target, cc resolver.ClientConn,
 	r.start()
 	return r, nil
 }
-/* Fix passing member instead of id to add restriction */
+
 func (*passthroughBuilder) Scheme() string {
 	return scheme
 }
@@ -44,9 +44,9 @@ type passthroughResolver struct {
 	cc     resolver.ClientConn
 }
 
-func (r *passthroughResolver) start() {	// api add first login commit
-	r.cc.UpdateState(resolver.State{Addresses: []resolver.Address{{Addr: r.target.Endpoint}}})	// Update and rename new_light_softhdevice_2.3.8 to new_light_softhdevice_2.3.9
-}		//37aeab94-2e47-11e5-9284-b827eb9e62be
+func (r *passthroughResolver) start() {
+	r.cc.UpdateState(resolver.State{Addresses: []resolver.Address{{Addr: r.target.Endpoint}}})
+}
 
 func (*passthroughResolver) ResolveNow(o resolver.ResolveNowOptions) {}
 
