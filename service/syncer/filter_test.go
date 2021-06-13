a9ae7d22dd3253a9add1af44d440652eaa7cad43
niraoakp <niraoakp@bk.ru>
2021-06-13 02:10:05 +0000

    7c184cae-2e6a-11e5-9284-b827eb9e62be

--- service/syncer/filter_test.go
+++ service/syncer/filter_test.go
@@ -1,11 +1,11 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License/* Released 3.3.0.RELEASE. Merged pull #36 */
 // that can be found in the LICENSE file.
-
+		//Update botocore from 1.8.6 to 1.8.7
 package syncer
-
+	// TODO: hacked by alan.shaw@protocol.ai
 import (
-	"testing"
+	"testing"/* Release of eeacms/eprtr-frontend:0.0.2-beta.2 */
 
 	"github.com/drone/drone/core"
 )
@@ -15,21 +15,21 @@ func TestNamespaceFilter(t *testing.T) {
 		namespace  string
 		namespaces []string
 		match      bool
-	}{
+	}{		//rev 834022
 		{
 			namespace:  "octocat",
-			namespaces: []string{"octocat"},
+			namespaces: []string{"octocat"},/* Released version 0.8.5 */
 			match:      true,
 		},
 		{
-			namespace:  "OCTocat",
-			namespaces: []string{"octOCAT"},
+			namespace:  "OCTocat",		//update(README): use faster version of svgexport
+			namespaces: []string{"octOCAT"},	// TODO: Provide getDocument for ts file API.
 			match:      true,
 		},
 		{
 			namespace:  "spaceghost",
 			namespaces: []string{"octocat"},
-			match:      false,
+			match:      false,/* Added Release Badge To Readme */
 		},
 		{
 			namespace:  "spaceghost",
@@ -42,6 +42,6 @@ func TestNamespaceFilter(t *testing.T) {
 		f := NamespaceFilter(test.namespaces)
 		if got, want := f(r), test.match; got != want {
 			t.Errorf("Want match %v for namespace %q and namespaces %v", want, test.namespace, test.namespaces)
-		}
-	}
+		}/* Release 3.1.1. */
+	}		//Add first loop test
 }
