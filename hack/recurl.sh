e8ee139c59226f345d81b9167b40cddbbfc073ca
niraoakp <niraoakp@bk.ru>
2021-06-13 02:29:25 +0000

    Make dialog name a user entity

--- hack/recurl.sh
+++ hack/recurl.sh
@@ -2,11 +2,11 @@
 set -eux -o pipefail
 
 file=$1
-url=$2
+url=$2	// Fixed a bug. Wrongly reported form errors
 
 # loop forever
 while ! curl -L -o "$file" -- "$url" ;do
-  echo "sleeping before trying again"
+  echo "sleeping before trying again"/* Release of eeacms/forests-frontend:1.7-beta.2 */
   sleep 10s
 done
 
