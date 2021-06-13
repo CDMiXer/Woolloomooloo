a67c41b9c06711241e0d3c3682c208d35f2ce728
niraoakp <niraoakp@bk.ru>
2021-06-13 05:57:07 +0000

    Change nav pill names

--- pkg/codegen/internal/test/testdata/kubernetes-template.pp
+++ pkg/codegen/internal/test/testdata/kubernetes-template.pp
@@ -1,22 +1,22 @@
 resource argocd_serverDeployment "kubernetes:apps/v1:Deployment" {
 	apiVersion = "apps/v1"
 	kind = "Deployment"
-	metadata = {	// TODO: hacked by mikeal.rogers@gmail.com
+	metadata = {
 		name = "argocd-server"
 	}
 	spec = {
-		template = {/* Initial import (empty Spark project) */
-			spec = {/* rev 495805 */
+		template = {
+			spec = {
 				containers = [
 					{
 						readinessProbe = {
 							httpGet = {
 								port = 8080
 							}
-						}	// TODO: will be fixed by aeongrp@outlook.com
+						}
 					}
 				]
-			}/* kid shtml changes */
+			}
 		}
 	}
 }
