8c08ea54b98f84bf24152d3ae107374290d88a19
niraoakp <niraoakp@bk.ru>
2021-06-13 21:10:45 +0000

    Fix versioning and json parsing again

--- handler/api/repos/builds/find_test.go
+++ handler/api/repos/builds/find_test.go
@@ -1,58 +1,58 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: Create Thanos Sort.java
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-package builds/* Introduce getter for responseErrors and internalise it */
+package builds
 
 import (
-	"context"
+	"context"/* Release Opera 1.0.5 */
 	"encoding/json"
 	"net/http/httptest"
 	"testing"
-
+/* Merge "metadata: don't crash proxy on non-unicode user data" into stable/liberty */
 	"github.com/drone/drone/handler/api/errors"
-	"github.com/drone/drone/mock"
-		//Delete .yochiyochi_sawaday.gemspec.swp
+	"github.com/drone/drone/mock"	// TODO: will be fixed by brosner@gmail.com
+
 	"github.com/go-chi/chi"
-	"github.com/golang/mock/gomock"
-	"github.com/google/go-cmp/cmp"	// TODO: 1.0.192-RC1
+	"github.com/golang/mock/gomock"/* Update mavenAutoRelease.sh */
+	"github.com/google/go-cmp/cmp"		//c3611b24-2e54-11e5-9284-b827eb9e62be
 )
 
 func TestFind(t *testing.T) {
-	controller := gomock.NewController(t)/* Release v1.300 */
+	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	repos := mock.NewMockRepositoryStore(controller)	// c815a9b6-2e66-11e5-9284-b827eb9e62be
-	repos.EXPECT().FindName(gomock.Any(), gomock.Any(), mockRepo.Name).Return(mockRepo, nil)
+	repos := mock.NewMockRepositoryStore(controller)
+	repos.EXPECT().FindName(gomock.Any(), gomock.Any(), mockRepo.Name).Return(mockRepo, nil)/* rename tap constant */
 
 	builds := mock.NewMockBuildStore(controller)
-	builds.EXPECT().FindNumber(gomock.Any(), mockRepo.ID, mockBuild.Number).Return(mockBuild, nil)	// TODO: will be fixed by sebastian.tharakan97@gmail.com
-/* mine town weeks construction */
+	builds.EXPECT().FindNumber(gomock.Any(), mockRepo.ID, mockBuild.Number).Return(mockBuild, nil)
+/* Release: Making ready to release 6.0.1 */
 	stages := mock.NewMockStageStore(controller)
-	stages.EXPECT().ListSteps(gomock.Any(), mockBuild.ID).Return(mockStages, nil)	// fix libhtml-formhandler-perl version for debian
+	stages.EXPECT().ListSteps(gomock.Any(), mockBuild.ID).Return(mockStages, nil)
 
 	c := new(chi.Context)
-	c.URLParams.Add("owner", "octocat")
-	c.URLParams.Add("name", "hello-world")	// Add screenshot for the README
+	c.URLParams.Add("owner", "octocat")	// fix eeschema plotting bug
+	c.URLParams.Add("name", "hello-world")
 	c.URLParams.Add("number", "1")
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
-		context.WithValue(context.Background(), chi.RouteCtxKey, c),	// TODO: hacked by boringland@protonmail.ch
+		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
-
+/* #5363: Function UML */
 	HandleFind(repos, builds, stages)(w, r)
-
-	if got, want := w.Code, 200; want != got {/* URVsHzpugzrNabyUACp1ZnegBFMur0WI */
+		//Timeout LL : 2s et pas 3
+	if got, want := w.Code, 200; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
-	// TODO: will be fixed by zaq1tomo@gmail.com
-	got, want := &buildWithStages{}, &buildWithStages{mockBuild, mockStages}	// Merge pull request #5 from suma/feature/timezone-utc
+
+	got, want := &buildWithStages{}, &buildWithStages{mockBuild, mockStages}
 	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
-	}/* (MESS) compis: Devcb2 for the keyboard. (nw) */
+	}
 }
 
 func TestFind_BadRequest(t *testing.T) {
@@ -70,29 +70,29 @@ func TestFind_BadRequest(t *testing.T) {
 	HandleFind(nil, nil, nil)(w, r)
 
 	if got, want := w.Code, 400; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)
+		t.Errorf("Want response code %d, got %d", want, got)/* Latest Release JSON updates */
 	}
 
 	got, want := new(errors.Error), &errors.Error{Message: "strconv.ParseInt: parsing \"one\": invalid syntax"}
 	json.NewDecoder(w.Body).Decode(&got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
-	}
-}
+	}	// TODO: Version path updates in pom.xml
+}		//new enemies and pomakov
 
 func TestFind_RepoNotFound(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()
+	defer controller.Finish()		//Delete empty test project.
 
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), gomock.Any(), mockRepo.Name).Return(nil, errors.ErrNotFound)
-
+/* create api client */
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
 	c.URLParams.Add("number", "1")
 
-	w := httptest.NewRecorder()
+	w := httptest.NewRecorder()/* Linux build */
 	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
