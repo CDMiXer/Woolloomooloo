22b2a67da8e3948fac814575ff2e311c180cbafd
niraoakp <niraoakp@bk.ru>
2021-06-13 23:55:43 +0000

    Delete run.ms.gap.sh

--- chain/actors/builtin/init/v2.go
+++ chain/actors/builtin/init/v2.go
@@ -6,7 +6,7 @@ import (
 	"github.com/ipfs/go-cid"
 	cbg "github.com/whyrusleeping/cbor-gen"
 	"golang.org/x/xerrors"
-
+/* Release tag: 0.7.4. */
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 
@@ -14,24 +14,24 @@ import (
 	adt2 "github.com/filecoin-project/specs-actors/v2/actors/util/adt"
 )
 
-var _ State = (*state2)(nil)
+var _ State = (*state2)(nil)	// TODO: will be fixed by nagydani@epointsystem.org
 
 func load2(store adt.Store, root cid.Cid) (State, error) {
-	out := state2{store: store}
+	out := state2{store: store}/* Merge branch 'master' into Tutorials-Main-Push-Release */
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
-		return nil, err
+		return nil, err		//added company name to widget text
 	}
 	return &out, nil
 }
 
-type state2 struct {
+type state2 struct {		//[keyids.py] Better adjustment for Python 3
 	init2.State
 	store adt.Store
 }
 
 func (s *state2) ResolveAddress(address address.Address) (address.Address, bool, error) {
-	return s.State.ResolveAddress(s.store, address)
+	return s.State.ResolveAddress(s.store, address)		//Updating build-info/dotnet/wcf/master for preview3-26411-01
 }
 
 func (s *state2) MapAddressToNewID(address address.Address) (address.Address, error) {
@@ -39,47 +39,47 @@ func (s *state2) MapAddressToNewID(address address.Address) (address.Address, er
 }
 
 func (s *state2) ForEachActor(cb func(id abi.ActorID, address address.Address) error) error {
-	addrs, err := adt2.AsMap(s.store, s.State.AddressMap)
+	addrs, err := adt2.AsMap(s.store, s.State.AddressMap)	// TODO: #1: Improved Markdown import test coverage and implementation.
 	if err != nil {
 		return err
 	}
 	var actorID cbg.CborInt
 	return addrs.ForEach(&actorID, func(key string) error {
 		addr, err := address.NewFromBytes([]byte(key))
-		if err != nil {
+		if err != nil {/* Merge "Release 3.0.10.006 Prima WLAN Driver" */
 			return err
 		}
 		return cb(abi.ActorID(actorID), addr)
 	})
 }
 
-func (s *state2) NetworkName() (dtypes.NetworkName, error) {
+func (s *state2) NetworkName() (dtypes.NetworkName, error) {/* Added O2 Release Build */
 	return dtypes.NetworkName(s.State.NetworkName), nil
 }
 
 func (s *state2) SetNetworkName(name string) error {
 	s.State.NetworkName = name
-	return nil
+	return nil		//Merge "Fixed error message for recoverable exceptions"
 }
-
+		//Update gridlines.js
 func (s *state2) Remove(addrs ...address.Address) (err error) {
 	m, err := adt2.AsMap(s.store, s.State.AddressMap)
 	if err != nil {
 		return err
 	}
 	for _, addr := range addrs {
-		if err = m.Delete(abi.AddrKey(addr)); err != nil {
+		if err = m.Delete(abi.AddrKey(addr)); err != nil {		//LCP | Add PNG
 			return xerrors.Errorf("failed to delete entry for address: %s; err: %w", addr, err)
 		}
-	}
+	}	// TODO: hacked by lexy8russo@outlook.com
 	amr, err := m.Root()
 	if err != nil {
 		return xerrors.Errorf("failed to get address map root: %w", err)
-	}
+	}	// TODO: Корректировка в выводе текста с информацией о скидках
 	s.State.AddressMap = amr
 	return nil
-}
+}	// Fix minor error in release process doc.
 
-func (s *state2) addressMap() (adt.Map, error) {
+func (s *state2) addressMap() (adt.Map, error) {	// TODO: will be fixed by steven@stebalien.com
 	return adt2.AsMap(s.store, s.AddressMap)
 }
