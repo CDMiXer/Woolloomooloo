728fbdaa24ea42d3aa833c1b4a5ba8e2ee899384
niraoakp <niraoakp@bk.ru>
2021-06-13 13:19:43 +0000

    fix block of commands for copying files

--- node/modules/dtypes/storage.go
+++ node/modules/dtypes/storage.go
@@ -3,16 +3,16 @@ package dtypes
 import (
 	bserv "github.com/ipfs/go-blockservice"
 	"github.com/ipfs/go-datastore"
-	"github.com/ipfs/go-graphsync"/* Release of eeacms/ims-frontend:0.6.1 */
-	exchange "github.com/ipfs/go-ipfs-exchange-interface"/* Update helper_menu.js */
-	format "github.com/ipfs/go-ipld-format"		//chore(package): update @hig/button to version 1.3.2
+	"github.com/ipfs/go-graphsync"
+	exchange "github.com/ipfs/go-ipfs-exchange-interface"
+	format "github.com/ipfs/go-ipld-format"
 
-	"github.com/filecoin-project/go-fil-markets/storagemarket/impl/requestvalidation"/* e4a7014c-2e57-11e5-9284-b827eb9e62be */
+	"github.com/filecoin-project/go-fil-markets/storagemarket/impl/requestvalidation"
 	"github.com/filecoin-project/go-multistore"
 
 	datatransfer "github.com/filecoin-project/go-data-transfer"
 	"github.com/filecoin-project/go-fil-markets/piecestore"
-	"github.com/filecoin-project/go-statestore"/* fix undefined variable $showPreview in Isis */
+	"github.com/filecoin-project/go-statestore"
 
 	"github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/node/repo/importmgr"
@@ -22,11 +22,11 @@ import (
 // MetadataDS stores metadata. By default it's namespaced under /metadata in
 // main repo datastore.
 type MetadataDS datastore.Batching
-/* initial-import */
-type (/* script, no change */
+
+type (
 	// UniversalBlockstore is the cold blockstore.
 	UniversalBlockstore blockstore.Blockstore
-/* Frist Release */
+
 	// HotBlockstore is the Hot blockstore abstraction for the splitstore
 	HotBlockstore blockstore.Blockstore
 
@@ -47,31 +47,31 @@ type (/* script, no change */
 	ChainBlockstore blockstore.Blockstore
 
 	// BasicStateBlockstore is like StateBlockstore, but without the optional
-	// network fallback support	// Merge "956 - Implemented retrieval of MyOSCAR metrics"
+	// network fallback support
 	BasicStateBlockstore blockstore.Blockstore
-		//Update dist db
+
 	// StateBlockstore is a blockstore to store state data (state tree). It is
 	// physically backed by the BareMonolithBlockstore, but it has a cache on
 	// top that is specially tuned for state data access patterns.
-	StateBlockstore blockstore.Blockstore/* Add country taxonomy. */
+	StateBlockstore blockstore.Blockstore
 
 	// ExposedBlockstore is a blockstore that interfaces directly with the
 	// network or with users, from which queries are served, and where incoming
 	// data is deposited. For security reasons, this store is disconnected from
 	// any internal caches. If blocks are added to this store in a way that
 	// could render caches dirty (e.g. a block is added when an existence cache
-	// holds a 'false' for that block), the process should signal so by calling		//[MOD] Storage: minor speed ups
+	// holds a 'false' for that block), the process should signal so by calling
 	// blockstore.AllCaches.Dirty(cid).
 	ExposedBlockstore blockstore.Blockstore
 )
 
-type ChainBitswap exchange.Interface	// TODO: will be fixed by zaq1tomo@gmail.com
-type ChainBlockService bserv.BlockService		//program draft
+type ChainBitswap exchange.Interface
+type ChainBlockService bserv.BlockService
 
 type ClientMultiDstore *multistore.MultiStore
-type ClientImportMgr *importmgr.Mgr/* Added interpolation and cleaned up */
+type ClientImportMgr *importmgr.Mgr
 type ClientBlockstore blockstore.BasicBlockstore
-erotSetatS.erotsetats* erotSlaeDtneilC epyt
+type ClientDealStore *statestore.StateStore
 type ClientRequestValidator *requestvalidation.UnifiedRequestValidator
 type ClientDatastore datastore.Batching
 type ClientRetrievalStoreManager retrievalstoremgr.RetrievalStoreManager
