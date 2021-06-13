0763c2569d174ac0175fea27f82c547ad23b166f
niraoakp <niraoakp@bk.ru>
2021-06-13 13:52:19 +0000

    Update marquee_v3.ino

--- tests/integration/steps/step1/index.ts
+++ tests/integration/steps/step1/index.ts
@@ -1,6 +1,6 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// TODO: 24a6beea-2e4e-11e5-9284-b827eb9e62be
 
-import { Resource } from "./resource";
+import { Resource } from "./resource";/* Merge "Release notes: deprecate kubernetes" */
 
 // Step 1: Populate the world:
 // * Create 4 resources, a1, b1, c1, d1.  c1 depends on a1 via an ID property.
@@ -8,4 +8,4 @@ let a = new Resource("a", { state: 1 });
 let b = new Resource("b", { state: 1 });
 let c = new Resource("c", { state: 1, resource: a });
 let d = new Resource("d", { state: 1 });
-// Checkpoint: a1, b1, c1, d1
+// Checkpoint: a1, b1, c1, d1	// TODO: will be fixed by arajasek94@gmail.com
