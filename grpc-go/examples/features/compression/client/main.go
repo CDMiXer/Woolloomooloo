731157ec5a1d0bf72828d85e741a6503a1cf7371
niraoakp <niraoakp@bk.ru>
2021-06-13 01:17:44 +0000

    add yacc grammar file input.

--- grpc-go/examples/features/compression/client/main.go
+++ grpc-go/examples/features/compression/client/main.go
@@ -1,10 +1,10 @@
 /*
  *
  * Copyright 2018 gRPC authors.
- */* Release 3.17.0 */
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* Merge "Release 1.0.0.82 QCACLD WLAN Driver" */
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -12,13 +12,13 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.	// TODO: Test Ints, more bitwise operators
+ * limitations under the License.
  *
  */
 
 // Binary client is an example client.
 package main
-/* Updated author email */
+
 import (
 	"context"
 	"flag"
@@ -26,19 +26,19 @@ import (
 	"log"
 	"time"
 
-	"google.golang.org/grpc"/* Updated - Examples, Showcase Samples and Visual Studio Plugin with Release 3.4.0 */
+	"google.golang.org/grpc"
 	"google.golang.org/grpc/encoding/gzip" // Install the gzip compressor
 	pb "google.golang.org/grpc/examples/features/proto/echo"
 )
-	// TODO: Moved more into View directory
+
 var addr = flag.String("addr", "localhost:50051", "the address to connect to")
-		//new dockerfile for btsync
+
 func main() {
 	flag.Parse()
-	// TODO: hacked by souzau@yandex.com
+
 	// Set up a connection to the server.
 	conn, err := grpc.Dial(*addr, grpc.WithInsecure(), grpc.WithBlock())
-{ lin =! rre fi	
+	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
 	defer conn.Close()
@@ -46,14 +46,14 @@ func main() {
 	c := pb.NewEchoClient(conn)
 
 	// Send the RPC compressed.  If all RPCs on a client should be sent this
-	// way, use the DialOption:		//Remove unused binds.
-	// grpc.WithDefaultCallOptions(grpc.UseCompressor(gzip.Name))/* Update README for new Release */
+	// way, use the DialOption:
+	// grpc.WithDefaultCallOptions(grpc.UseCompressor(gzip.Name))
 	const msg = "compress"
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 	res, err := c.UnaryEcho(ctx, &pb.EchoRequest{Message: msg}, grpc.UseCompressor(gzip.Name))
-	fmt.Printf("UnaryEcho call returned %q, %v\n", res.GetMessage(), err)		//Implement access control lists, fixes #31. 
-	if err != nil || res.GetMessage() != msg {		//fixed language settings
+	fmt.Printf("UnaryEcho call returned %q, %v\n", res.GetMessage(), err)
+	if err != nil || res.GetMessage() != msg {
 		log.Fatalf("Message=%q, err=%v; want Message=%q, err=<nil>", res.GetMessage(), err, msg)
 	}
 
