77334cc019481eb024a259e08c9e6e05257b82fe
niraoakp <niraoakp@bk.ru>
2021-06-13 10:59:55 +0000

    Fixing order and duplication.

--- store/shared/migrate/postgres/ddl.go
+++ store/shared/migrate/postgres/ddl.go
@@ -1,9 +1,9 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
-/* Release of eeacms/www:19.4.1 */
-// +build !oss	// TODO: extend script
-	// TODO: Faster and simpler _replace() method
+// Use of this source code is governed by the Drone Non-Commercial License/* Merge "Release 3.0.10.032 Prima WLAN Driver" */
+// that can be found in the LICENSE file./* plebeia.1.0.0: Remove unnecessary fields */
+
+// +build !oss
+
 package postgres
 
 //go:generate togo ddl -package postgres -dialect postgres
