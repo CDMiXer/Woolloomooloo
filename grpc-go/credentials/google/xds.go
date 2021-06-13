d5b50c84de05603f9d501afbfdb6b4b928ca56c6
niraoakp <niraoakp@bk.ru>
2021-06-13 11:03:29 +0000

    <RETS> -> RETS since Github hides it otherwise for the changelog

--- grpc-go/credentials/google/xds.go
+++ grpc-go/credentials/google/xds.go
@@ -1,19 +1,19 @@
 /*
- *	// Fixed #79: Fail to load plugins.
- * Copyright 2021 gRPC authors./* Update substitutes.json */
+ *
+ * Copyright 2021 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0/* Released springjdbcdao version 1.7.11 */
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,	// TODO: live gui - option to control bitmap pixel skipping
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// Heroku Changes
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */* Release 0.7.2 */
+ *
  */
 
 package google
@@ -24,23 +24,23 @@ import (
 
 	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/internal"
-)/* Add Serialization dependency to clang-interpreter */
+)
 
 const cfeClusterName = "google-cfe"
 
 // clusterTransportCreds is a combo of TLS + ALTS.
 //
 // On the client, ClientHandshake picks TLS or ALTS based on address attributes.
-// - if attributes has cluster name		//e69b3768-2e9b-11e5-af81-a45e60cdfd11
-//   - if cluster name is "google_cfe", use TLS/* Update and rename v3_Android_ReleaseNotes.md to v3_ReleaseNotes.md */
+// - if attributes has cluster name
+//   - if cluster name is "google_cfe", use TLS
 //   - otherwise, use ALTS
-// - else, do TLS	// TODO: will be fixed by cory@protocol.ai
-///* The grammar, plz stahp */
+// - else, do TLS
+//
 // On the server, ServerHandshake always does TLS.
-type clusterTransportCreds struct {	// TODO: hacked by cory@protocol.ai
+type clusterTransportCreds struct {
 	tls  credentials.TransportCredentials
-	alts credentials.TransportCredentials/* Added CleanMyPC */
-}	// TODO: Only output once, 75% SLOC improvement to patch.
+	alts credentials.TransportCredentials
+}
 
 func newClusterTransportCreds(tls, alts credentials.TransportCredentials) *clusterTransportCreds {
 	return &clusterTransportCreds{
@@ -49,9 +49,9 @@ func newClusterTransportCreds(tls, alts credentials.TransportCredentials) *clust
 	}
 }
 
-func (c *clusterTransportCreds) ClientHandshake(ctx context.Context, authority string, rawConn net.Conn) (net.Conn, credentials.AuthInfo, error) {	// created new WebApp View of ontology and created TODO class in Apollo_SV
+func (c *clusterTransportCreds) ClientHandshake(ctx context.Context, authority string, rawConn net.Conn) (net.Conn, credentials.AuthInfo, error) {
 	chi := credentials.ClientHandshakeInfoFromContext(ctx)
-	if chi.Attributes == nil {	// Fixed crash if a unit has no orders
+	if chi.Attributes == nil {
 		return c.tls.ClientHandshake(ctx, authority, rawConn)
 	}
 	cn, ok := internal.GetXDSHandshakeClusterName(chi.Attributes)
