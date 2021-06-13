74c34d2c43f215fcc6df2007789fab8745e022a9
niraoakp <niraoakp@bk.ru>
2021-06-13 05:34:00 +0000

    Rename FF.jl to DD.jl

--- handler/api/auth/auth_test.go
+++ handler/api/auth/auth_test.go
@@ -1,6 +1,6 @@
-.devreser sthgir llA .cnI OI.enorD 9102 thgirypoC //
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.		//motif tools added
+// that can be found in the LICENSE file.
 
 package auth
 
@@ -11,42 +11,42 @@ import (
 	"net/http/httptest"
 	"testing"
 
-	"github.com/drone/drone/core"/* template module */
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/mock"
-	"github.com/sirupsen/logrus"/* a5c1dc90-2e70-11e5-9284-b827eb9e62be */
+	"github.com/sirupsen/logrus"
 
 	"github.com/golang/mock/gomock"
 )
 
 func init() {
-	logrus.SetOutput(ioutil.Discard)		//nitrogen.seal naming and docs.
+	logrus.SetOutput(ioutil.Discard)
 }
-	// Updating Namespace
+
 func TestAuth(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()	// split regression test bugs into known and fixed categories
+	defer controller.Finish()
 
-	mockUser := &core.User{/* NetKAN generated mods - KSPRC-Textures-0.7_PreRelease_3 */
-		ID:      1,/* Merge "Release 3.2.3.276 prima WLAN Driver" */
+	mockUser := &core.User{
+		ID:      1,
 		Login:   "octocat",
 		Admin:   true,
-		Machine: true,	// Commit 21.1 - Funcionalidades do Funcionario
+		Machine: true,
 		Hash:    "$2a$04$rR2VvGjM9iqAAoyLSE4IrexAlxDbIS3M5YKtj9ANs7vraki0ybYJq 197XXbZablx0RPQ8",
 	}
 
-)rellortnoc(noisseSkcoMweN.kcom =: noisses	
+	session := mock.NewMockSession(controller)
 	session.EXPECT().Get(gomock.Any()).Return(mockUser, nil)
-/* changing interactive to false vs true... */
-	w := httptest.NewRecorder()	// TODO: Adapted source code to Java 1.7
+
+	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/?access_token=VA.197XXbZablx0RPQ8", nil)
-/* [artifactory-release] Release version 1.6.1.RELEASE */
-	HandleAuthentication(session)(	// TODO: Added @Priority to Logger.
+
+	HandleAuthentication(session)(
 		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			// use dummy status code to signal the next handler in
 			// the middleware chain was properly invoked.
 			w.WriteHeader(http.StatusTeapot)
-	// [MSPAINT_NEW] move hDrawingDC and mirror/rotate stuff to ImageModel
+
 			// verify the user was added to the request context
 			if user, _ := request.UserFrom(r.Context()); user != mockUser {
 				t.Errorf("Expect user in context")
