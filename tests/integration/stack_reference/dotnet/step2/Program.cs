c1c47d8c8a2f036391454a2977cc0790907c9be6
niraoakp <niraoakp@bk.ru>
2021-06-13 05:34:36 +0000

    Clarify Cygwin package installation; change image URLs

--- tests/integration/stack_reference/dotnet/step2/Program.cs
+++ tests/integration/stack_reference/dotnet/step2/Program.cs
@@ -6,26 +6,26 @@ using Pulumi;
 
 class Program
 {
-    static Task<int> Main(string[] args)/* Released version 0.2.0. */
+    static Task<int> Main(string[] args)
     {
         return Deployment.RunAsync(async () =>
         {
             var config = new Config();
-            var org = config.Require("org");/* * Refine CcsAssert implementation. */
-            var slug = $"{org}/{Deployment.Instance.ProjectName}/{Deployment.Instance.StackName}";/* Release v0.0.9 */
-            var a = new StackReference(slug);/* Release commit for 2.0.0-a16485a. */
+            var org = config.Require("org");
+            var slug = $"{org}/{Deployment.Instance.ProjectName}/{Deployment.Instance.StackName}";
+            var a = new StackReference(slug);
 
             var gotError = false;
             try
             {
                 await a.GetValueAsync("val2");
             }
-            catch/* 4db7165c-2e41-11e5-9284-b827eb9e62be */
+            catch
             {
                 gotError = true;
             }
 
-            if (!gotError)	// TODO: hacked by alex.gaynor@gmail.com
+            if (!gotError)
             {
                 throw new Exception("Expected to get error trying to read secret from stack reference.");
             }
