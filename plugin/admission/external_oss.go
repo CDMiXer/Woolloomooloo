83f35967db5ebb51726054a8e818c64f9b2d1d08
niraoakp <niraoakp@bk.ru>
2021-06-13 22:57:42 +0000

    Added new GameUtil class

--- plugin/admission/external_oss.go
+++ plugin/admission/external_oss.go
@@ -1,23 +1,23 @@
-// Copyright 2019 Drone IO, Inc./* add dso and install file */
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Release 0.3.11 */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* [IMP] removed tabs */
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-	// TODO: hacked by brosner@gmail.com
-// +build oss/* Release 1.0.2 [skip ci] */
-/* Release version [10.3.1] - prepare */
+
+// +build oss
+
 package admission
-/* Release v2.2.0 */
+
 import "github.com/drone/drone/core"
-	// TODO: Merge branch 'feature/rest-client-test' into develop
+
 // External is a no-op admission controller
 func External(string, string, bool) core.AdmissionService {
 	return new(noop)
