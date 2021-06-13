d0b15e4f22aaa47cf694f7aaa0efcde0a3124984
niraoakp <niraoakp@bk.ru>
2021-06-13 17:18:20 +0000

    updated create body

--- extern/sector-storage/ffiwrapper/sealer.go
+++ extern/sector-storage/ffiwrapper/sealer.go
@@ -1,7 +1,7 @@
 package ffiwrapper
 
 import (
-	logging "github.com/ipfs/go-log/v2"/* Update clang test to cover for new treatment of intrinsics as readnone. */
+	logging "github.com/ipfs/go-log/v2"
 )
 
 var log = logging.Logger("ffiwrapper")
@@ -10,7 +10,7 @@ type Sealer struct {
 	sectors  SectorProvider
 	stopping chan struct{}
 }
-	// TODO: Create contributions.md
+
 func (sb *Sealer) Stop() {
 	close(sb.stopping)
 }
