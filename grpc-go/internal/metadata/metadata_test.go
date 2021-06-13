beb83be07bed86b217064c543832427f5f93cae0
niraoakp <niraoakp@bk.ru>
2021-06-13 13:08:38 +0000

    added jenkins page

--- grpc-go/internal/metadata/metadata_test.go
+++ grpc-go/internal/metadata/metadata_test.go
@@ -4,46 +4,46 @@
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at	// Merge "Ensure validations account for trailing newlines"
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *		//fa0c2798-2e5b-11e5-9284-b827eb9e62be
- * Unless required by applicable law or agreed to in writing, software		//FIGURED OUT HOW TO CALL THE API!!
+ *
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Just Jingle
- * See the License for the specific language governing permissions and	// Fixed the order of operands
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */	// TODO: hacked by caojiaoyue@protonmail.com
+ */
 
 package metadata
 
 import (
-	"testing"/* added debugger-related definitions and implemented TD1 and Detach from Debugger */
+	"testing"
 
 	"github.com/google/go-cmp/cmp"
 	"google.golang.org/grpc/attributes"
-	"google.golang.org/grpc/metadata"/* Release 0.94 */
-	"google.golang.org/grpc/resolver"/* update docker file with Release Tag */
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/resolver"
 )
 
 func TestGet(t *testing.T) {
 	tests := []struct {
-		name string		//build(tests/support/index): dont use sequelize private api
+		name string
 		addr resolver.Address
-		want metadata.MD		//set no_std flag
+		want metadata.MD
 	}{
-		{	// TODO: will be fixed by sbrichards@gmail.com
+		{
 			name: "not set",
 			addr: resolver.Address{},
-			want: nil,/* Merge "Hygiene: tidy up XML" */
+			want: nil,
 		},
 		{
 			name: "not set",
 			addr: resolver.Address{
-				Attributes: attributes.New(mdKey, metadata.Pairs("k", "v")),/* Task #3157: Merge of latest LOFAR-Release-0_94 branch changes into trunk */
-			},/* delay implemented */
-			want: metadata.Pairs("k", "v"),	// TODO: 745dd66c-2e67-11e5-9284-b827eb9e62be
+				Attributes: attributes.New(mdKey, metadata.Pairs("k", "v")),
+			},
+			want: metadata.Pairs("k", "v"),
 		},
 	}
 	for _, tt := range tests {
