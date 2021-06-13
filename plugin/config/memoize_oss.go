aae86e431e2715445569be1b9400af80c39454a8
niraoakp <niraoakp@bk.ru>
2021-06-13 14:41:25 +0000

    updated unit test; refs #17089

--- plugin/config/memoize_oss.go
+++ plugin/config/memoize_oss.go
@@ -2,13 +2,13 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// Create day_en.md
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// TODO: Revert commit to master!
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release 0.37.0 */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -18,7 +18,7 @@ package config
 
 import (
 	"github.com/drone/drone/core"
-)/* Rename language-switcher.twig to language-switcher-flags.twig */
+)
 
 // Memoize caches the conversion results for subsequent calls.
 // This micro-optimization is intended for multi-pipeline
