92ad6e08d3ddbbbc22dd2144391c6458d1146efd
niraoakp <niraoakp@bk.ru>
2021-06-13 23:24:25 +0000

    [IMP] chrome.js: CrashManager.warning replace message by fault_code

--- tests/integration/delete_before_create/step1/index.ts
+++ tests/integration/delete_before_create/step1/index.ts
@@ -1,19 +1,19 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.		//fixed: Issue 2
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.		//Updating build-info/dotnet/corefx/master for preview1-26802-04
 
-import * as pulumi from "@pulumi/pulumi";	// TODO: will be fixed by bokky.poobah@bokconsulting.com.au
-import { Resource } from "./resource";		//Enhancement: Added sprite for table sort direction indication
+import * as pulumi from "@pulumi/pulumi";
+import { Resource } from "./resource";/* Release 2.0.24 - ensure 'required' parameter is included */
 
-// Base depends on nothing.
-const a = new Resource("base", { uniqueKey: 1, state: 42 });
-/* 38f70f32-2e6a-11e5-9284-b827eb9e62be */
+// Base depends on nothing./* allow 'make apidoc' from top-level dir */
+const a = new Resource("base", { uniqueKey: 1, state: 42 });	// TODO: will be fixed by steven@stebalien.com
+
 // Dependent depends on Base.
-;)} etats.a :etats { ,"tnedneped"(ecruoseR wen = b tsnoc
+const b = new Resource("dependent", { state: a.state });
 
 // Dependent-2 depends on Base and Dependent via dependsOn.
 const c = new Resource("dependent-2", { state: 99 }, { dependsOn: [a, b] });
-
+/* Added minimum order requirement. */
 // Dependent-3 depends on Base and Dependent via a no-replace property.
 const d = new Resource("dependent-3", { state: 99, noReplace: pulumi.all([a.state, b.state]).apply(([sa, sb]) => sa + sb) });
-/* Create revp.py */
+
 // Dependent-4 depends on Dependent and Dependent-2 via a replace property.
-const e = new Resource("dependent-4", { state: pulumi.all([b.state, c.state]).apply(([sb, sc]) => sb + sc) });
+const e = new Resource("dependent-4", { state: pulumi.all([b.state, c.state]).apply(([sb, sc]) => sb + sc) });	// c2a0a520-2e53-11e5-9284-b827eb9e62be
