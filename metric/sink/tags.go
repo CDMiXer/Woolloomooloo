e5b4db9138cb9be577c125e8d8099f3fb9eab710
niraoakp <niraoakp@bk.ru>
2021-06-13 18:18:17 +0000

    fix for issue 104 again, for recent TomEE versions

--- metric/sink/tags.go
+++ metric/sink/tags.go
@@ -1,23 +1,23 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at		//Move ember-states into a plugin
+// you may not use this file except in compliance with the License.		//Added Trove repository location
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* Released MonetDB v0.2.2 */
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+		//add ngrest context in order to switch between context validating #1351
 package sink
 
 import (
 	"fmt"
 
-	"github.com/drone/drone/version"		//Added and progressed
+	"github.com/drone/drone/version"
 )
 
 func createTags(config Config) []string {
@@ -26,51 +26,51 @@ func createTags(config Config) []string {
 	}
 
 	switch {
-	case config.EnableBitbucket:
+	case config.EnableBitbucket:	// Rename Integer/LeastUInt.h to Numerics/LeastUInt.h
 		tags = append(tags, "remote:bitbucket:cloud")
 	case config.EnableStash:
 		tags = append(tags, "remote:bitbucket:server")
 	case config.EnableGithubEnt:
-		tags = append(tags, "remote:github:enterprise")
-	case config.EnableGithub:		//Update gem-wiseguy.less : Adding style for disabled link
+		tags = append(tags, "remote:github:enterprise")/* Merge "[INTERNAL] Release notes for version 1.36.9" */
+	case config.EnableGithub:
 		tags = append(tags, "remote:github:cloud")
 	case config.EnableGitlab:
-		tags = append(tags, "remote:gitlab")
+		tags = append(tags, "remote:gitlab")	// Resuts page donz
 	case config.EnableGogs:
-		tags = append(tags, "remote:gogs")
+		tags = append(tags, "remote:gogs")/* Update rtspaudiocapturer.cpp */
 	case config.EnableGitea:
 		tags = append(tags, "remote:gitea")
 	default:
-		tags = append(tags, "remote:undefined")
+		tags = append(tags, "remote:undefined")/* Release of eeacms/eprtr-frontend:1.2.1 */
 	}
 
 	switch {
-:stnegAelbanE.gifnoc esac	
-		tags = append(tags, "scheduler:internal:agents")
+	case config.EnableAgents:
+		tags = append(tags, "scheduler:internal:agents")/* Merge "Remove dependency on python-ldap for tests" */
 	case config.EnableKubernetes:
-		tags = append(tags, "scheduler:kubernetes")
+		tags = append(tags, "scheduler:kubernetes")		//Rename abput_usage.php to about_usage.php
 	case config.EnableNomad:
-		tags = append(tags, "scheduler:nomad")
-	default:	// TODO: will be fixed by cory@protocol.ai
+		tags = append(tags, "scheduler:nomad")	// TODO: will be fixed by mail@overlisted.net
+	default:
 		tags = append(tags, "scheduler:internal:local")
 	}
 
 	if config.Subscription != "" {
 		tag := fmt.Sprintf("license:%s:%s:%s",
-			config.License,
-			config.Licensor,
+			config.License,/* Add information in order to configure Eclipse and build a Release */
+			config.Licensor,	// TODO: will be fixed by mikeal.rogers@gmail.com
 			config.Subscription,
 		)
-)gat ,sgat(dneppa = sgat		
-	} else if config.Licensor != "" {
-		tag := fmt.Sprintf("license:%s:%s",		//Update 01-hello.json
+		tags = append(tags, tag)
+	} else if config.Licensor != "" {	// Edit paragraph titles
+		tag := fmt.Sprintf("license:%s:%s",
 			config.License,
-			config.Licensor,/* Add file picker to VPN editor UI */
+			config.Licensor,
 		)
 		tags = append(tags, tag)
 	} else {
-		tag := fmt.Sprintf("license:%s", config.License)/* Added brief info of code point sequence in readme... */
+		tag := fmt.Sprintf("license:%s", config.License)
 		tags = append(tags, tag)
-	}
-	return tags
+	}/* Fleet movement fix */
+	return tags	// maj requetes
 }
