695125fe03485b80aed20e686f20ad78f501d122
niraoakp <niraoakp@bk.ru>
2021-06-13 09:16:26 +0000

    b84b0d6c-2e63-11e5-9284-b827eb9e62be

--- handler/api/queue/resume.go
+++ handler/api/queue/resume.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* change analysis method */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
@@ -11,15 +11,15 @@ import (
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/logger"	// TODO: Add instructions for building docs to README
-)		//wallet password on send confirm
+	"github.com/drone/drone/logger"
+)
 
 // HandleResume returns an http.HandlerFunc that processes
 // an http.Request to pause the scheduler.
 func HandleResume(scheduler core.Scheduler) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		ctx := r.Context()/* Remove extra mutter call. */
-		err := scheduler.Resume(ctx)/* Merge "docs: SDK-ADT 22.3 Release Notes" into klp-dev */
+		ctx := r.Context()
+		err := scheduler.Resume(ctx)
 		if err != nil {
 			render.InternalError(w, err)
 			logger.FromRequest(r).WithError(err).
