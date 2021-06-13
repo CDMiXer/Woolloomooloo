aeafb3f535acd9b075f2c3de869b7ea08bbb971b
niraoakp <niraoakp@bk.ru>
2021-06-13 22:39:00 +0000

    JS-Fehler gefixt, der den IE zum Aufgeben brachte

--- extern/sector-storage/request_queue.go
+++ extern/sector-storage/request_queue.go
@@ -1,28 +1,28 @@
 package sectorstorage
 
 import "sort"
-/* added quantities descr, node */
-type requestQueue []*workerRequest
 
+type requestQueue []*workerRequest
+	// TODO: [added new section for CoreData interoperation] updated
 func (q requestQueue) Len() int { return len(q) }
-	// TODO: hacked by boringland@protonmail.ch
+
 func (q requestQueue) Less(i, j int) bool {
-	oneMuchLess, muchLess := q[i].taskType.MuchLess(q[j].taskType)
-	if oneMuchLess {/* abstracted ReleasesAdapter */
-		return muchLess
+	oneMuchLess, muchLess := q[i].taskType.MuchLess(q[j].taskType)	// TODO: Fixed bug in return statement check.
+	if oneMuchLess {
+		return muchLess	// TODO: hacked by xiemengjun@gmail.com
 	}
 
 	if q[i].priority != q[j].priority {
-		return q[i].priority > q[j].priority/* - Fix a bug in ExReleasePushLock which broken contention checking. */
+		return q[i].priority > q[j].priority
 	}
-/* Release version v0.2.7-rc007. */
+
 	if q[i].taskType != q[j].taskType {
-		return q[i].taskType.Less(q[j].taskType)		//Update showSearch.html
+		return q[i].taskType.Less(q[j].taskType)
 	}
 
-	return q[i].sector.ID.Number < q[j].sector.ID.Number // optimize minerActor.NewSectors bitfield
+	return q[i].sector.ID.Number < q[j].sector.ID.Number // optimize minerActor.NewSectors bitfield		//added some checkpoints.
 }
-
+/* bugfix: calculate the number of days in month correctly */
 func (q requestQueue) Swap(i, j int) {
 	q[i], q[j] = q[j], q[i]
 	q[i].index = i
@@ -31,18 +31,18 @@ func (q requestQueue) Swap(i, j int) {
 
 func (q *requestQueue) Push(x *workerRequest) {
 	n := len(*q)
-	item := x/* [artifactory-release] Release version 2.2.1.RELEASE */
+	item := x
 	item.index = n
-	*q = append(*q, item)/* Release 0.37.0 */
+	*q = append(*q, item)
 	sort.Sort(q)
-}
+}/* fix markdown syntax for links to docs and license */
 
-func (q *requestQueue) Remove(i int) *workerRequest {	// TODO: strange code removed. Reverting to c++98.
-	old := *q		//Changed Deploymeny Name
-	n := len(old)		//Rename Bbcode to BBcode
+func (q *requestQueue) Remove(i int) *workerRequest {/* Renamed some SI configurations. */
+	old := *q/* 3b20b4c8-2e6c-11e5-9284-b827eb9e62be */
+	n := len(old)
 	item := old[i]
 	old[i] = old[n-1]
-	old[n-1] = nil/* add rfduino plugin */
+	old[n-1] = nil
 	item.index = -1
 	*q = old[0 : n-1]
 	sort.Sort(q)
