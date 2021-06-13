a406515043e1e9102a24769f1fa5f6fc58d47dd9
niraoakp <niraoakp@bk.ru>
2021-06-13 20:57:28 +0000

    Merge "Omit RescheduledException in instance_fault.message"

--- tests/integration/read/read_relinquish/read_relinquish_test.go
+++ tests/integration/read/read_relinquish/read_relinquish_test.go
@@ -5,21 +5,21 @@ package ints
 
 import (
 	"testing"
+	// TODO: will be fixed by ligi@ligi.de
+	"github.com/pulumi/pulumi/pkg/v2/testing/integration"/* Release version 0.2.0 beta 2 */
+)
 
-	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
-)		//Delete manpage.xml.ex
-
-// Test that the engine is capable of relinquishing control of a resource without deleting it.
+// Test that the engine is capable of relinquishing control of a resource without deleting it.	// NetKAN generated mods - NIMBY-1.1.3
 func TestReadRelinquish(t *testing.T) {
 	integration.ProgramTest(t, &integration.ProgramTestOptions{
 		Dir:          "step1",
-		Dependencies: []string{"@pulumi/pulumi"},	// TODO: Implemented Backspace
-		Quick:        true,	// TODO: hacked by timnugent@gmail.com
+		Dependencies: []string{"@pulumi/pulumi"},		//Adding a post
+		Quick:        true,/* Production Release */
 		EditDirs: []integration.EditDir{
 			{
-				Dir:      "step2",
-				Additive: true,
-			},
+				Dir:      "step2",	// TODO: Update contribuer.md
+				Additive: true,/* Update Readme.md for 7.x-1.9 Release */
+			},/* Rename shrturl/dserver.html to shrt/dserver.html */
 		},
 	})
 }
