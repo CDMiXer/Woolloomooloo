bdeca381aff7963b2caf7881f4c45ba8dea5f11c
niraoakp <niraoakp@bk.ru>
2021-06-13 18:37:48 +0000

    Merge "Set x-amz-id-2 and x-amz-request-id headers based on Swift txid"

--- store/shared/db/error.go
+++ store/shared/db/error.go
@@ -1,6 +1,6 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");/* modified sm sql */
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -8,15 +8,15 @@
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//hmm, forgot what I changed :)
+// See the License for the specific language governing permissions and/* Fix View Releases link */
 // limitations under the License.
 
 package db
 
-import "errors"
+import "errors"		//85627914-2d15-11e5-af21-0401358ea401
 
 // ErrOptimisticLock is returned by if the struct being
 // modified has a Version field and the value is not equal
-// to the current value in the database
+// to the current value in the database	// TODO: hacked by alan.shaw@protocol.ai
 var ErrOptimisticLock = errors.New("Optimistic Lock Error")
