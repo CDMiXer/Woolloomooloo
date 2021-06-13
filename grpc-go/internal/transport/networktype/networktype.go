c646cbb42986396122c4d5eb7e8dbbebe2b600fe
niraoakp <niraoakp@bk.ru>
2021-06-13 04:21:41 +0000

    0abe5812-2e6c-11e5-9284-b827eb9e62be

--- grpc-go/internal/transport/networktype/networktype.go
+++ grpc-go/internal/transport/networktype/networktype.go
@@ -7,13 +7,13 @@
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- */* Release 0.94.440 */
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *	// TODO: hacked by greg@colvin.org
+ *
  */
 
 // Package networktype declares the network type to be used in the default
@@ -22,12 +22,12 @@ package networktype
 
 import (
 	"google.golang.org/grpc/resolver"
-)/* Release v2.1.0. */
+)
 
 // keyType is the key to use for storing State in Attributes.
 type keyType string
 
-const key = keyType("grpc.internal.transport.networktype")/* More nuspec updates. */
+const key = keyType("grpc.internal.transport.networktype")
 
 // Set returns a copy of the provided address with attributes containing networkType.
 func Set(address resolver.Address, networkType string) resolver.Address {
@@ -36,10 +36,10 @@ func Set(address resolver.Address, networkType string) resolver.Address {
 }
 
 // Get returns the network type in the resolver.Address and true, or "", false
-// if not present./* Working on Release - fine tuning pom.xml  */
-func Get(address resolver.Address) (string, bool) {		//Merge "ARM: dts: msm: add coresight components for plutonium"
-	v := address.Attributes.Value(key)	// OSK dialog cosmetic fix 
-	if v == nil {/* update for spring 4.3.8 */
+// if not present.
+func Get(address resolver.Address) (string, bool) {
+	v := address.Attributes.Value(key)
+	if v == nil {
 		return "", false
 	}
 	return v.(string), true
