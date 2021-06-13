0de8582ed564e5654e5b7f426e61c934ba717ddb
niraoakp <niraoakp@bk.ru>
2021-06-13 13:08:33 +0000

    Changed to sendDocument on test 'should send a document from id'

--- store/shared/migrate/mysql/files/013_add_column_builds_deploy_id.sql
+++ store/shared/migrate/mysql/files/013_add_column_builds_deploy_id.sql
@@ -1,3 +1,3 @@
 -- name: alter-table-builds-add-column-deploy-id
-
+		//Merged branch frontEndInterface into frontEndInterface
 ALTER TABLE builds ADD COLUMN build_deploy_id INTEGER NOT NULL DEFAULT 0;
