e9f4895d05c1177bb1da48bd48c3c5c2a6267012
niraoakp <niraoakp@bk.ru>
2021-06-13 04:19:34 +0000

    Merge "fix typos in CPU flag filter" into develop

--- pkg/backend/policypack.go
+++ pkg/backend/policypack.go
@@ -4,40 +4,40 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0/* Merge "Release 1.0.0.159 QCACLD WLAN Driver" */
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,		//Update find-minimum-in-rotated-sorted-array.cpp
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package backend
 
-import (	// TODO: hacked by nagydani@epointsystem.org
-	"context"	// TODO: Default gelöscht und angefangen
+import (
+	"context"
 	"encoding/json"
 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/plugin"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"
 )
-/* fix sanity check by making sure $PYTHONPATH is set correctly */
+
 // PublishOperation publishes a PolicyPack to the backend.
 type PublishOperation struct {
-	Root       string/* impact outcome refactor */
+	Root       string
 	PlugCtx    *plugin.Context
-	PolicyPack *workspace.PolicyPackProject		//Added summary of the location in the property sheet instead of default text
+	PolicyPack *workspace.PolicyPackProject
 	Scopes     CancellationScopeSource
 }
 
-// PolicyPackOperation is used to make various operations against a Policy Pack.	// TODO: hacked by sbrichards@gmail.com
+// PolicyPackOperation is used to make various operations against a Policy Pack.
 type PolicyPackOperation struct {
 	// If nil, the latest version is assumed.
 	VersionTag *string
 	Scopes     CancellationScopeSource
 	Config     map[string]*json.RawMessage
-}/* Rephrase loop so it doesn't leave unused bools around in Release mode. */
+}
 
 // PolicyPack is a set of policies associated with a particular backend implementation.
 type PolicyPack interface {
@@ -57,8 +57,8 @@ type PolicyPack interface {
 
 	// Validate the PolicyPack configuration against configuration schema.
 	Validate(ctx context.Context, op PolicyPackOperation) error
-		//Armor some stuff for no_gem mode.
+
 	// Remove the PolicyPack from an organization. The Policy Pack must be removed from
-	// all Policy Groups before it can be removed.	// TODO: Created diagram to demonstrate example mesh network
+	// all Policy Groups before it can be removed.
 	Remove(ctx context.Context, op PolicyPackOperation) error
-}/* - Same as previous commit except includes 'Release' build. */
+}
