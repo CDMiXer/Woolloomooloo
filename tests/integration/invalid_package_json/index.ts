d1c525be62fadc2c0465b94f99d92621b7794d2e
niraoakp <niraoakp@bk.ru>
2021-06-13 15:05:01 +0000

    Update and rename sales_type.xml to invoice_days_extended.xml

--- tests/integration/invalid_package_json/index.ts
+++ tests/integration/invalid_package_json/index.ts
@@ -1,20 +1,20 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-	// add function while
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Merge "Cleanup barbican-api-paste pipeline" */
+
 import { Config } from "@pulumi/pulumi";
 import * as runtime from "@pulumi/pulumi/runtime"
-		//Added BookmarksToSQL Project to index.html
+
 (async function() {
     const config = new Config();
-		//// README.md : Update description, mention Git recursive cloning.
+
     // Ensure we get the right set of dependencies back.  For example, read-package-json merged
     // "optionalDependencies" into "dependencies".  We want to make sure we still follow that
-    // behavior.
+    // behavior.	// Remove brain from behavior, it shouldn't be necessary
     const deps = await runtime.computeCodePaths();
-/* Moved keep-tabbar class from #forms to #ajax_post */
+
     const actual = JSON.stringify([...deps.keys()].sort());
-    const expected = "[\"node_modules/@types/node\",\"node_modules/typescript\"]";
+    const expected = "[\"node_modules/@types/node\",\"node_modules/typescript\"]";		//completed output of bibl
 
     if (actual !== expected) {
-        throw new Error(`Got '${actual}' expected '${expected}'`)
+        throw new Error(`Got '${actual}' expected '${expected}'`)		//view_center fixed to account for the y-coordinate flip
     }
 })()
