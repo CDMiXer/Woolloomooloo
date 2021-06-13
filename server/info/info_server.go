eaca2659ca4a31eee2b60ab810200c08c79af69a
niraoakp <niraoakp@bk.ru>
2021-06-13 12:20:27 +0000

    console mpx: first switch

--- server/info/info_server.go
+++ server/info/info_server.go
@@ -1,7 +1,7 @@
 package info
 
 import (
-	"context"
+	"context"	// Added default values to DefaultResponse
 
 	"github.com/argoproj/argo"
 	infopkg "github.com/argoproj/argo/pkg/apiclient/info"
@@ -14,7 +14,7 @@ type infoServer struct {
 	links            []*wfv1.Link
 }
 
-func (i *infoServer) GetUserInfo(ctx context.Context, _ *infopkg.GetUserInfoRequest) (*infopkg.GetUserInfoResponse, error) {
+func (i *infoServer) GetUserInfo(ctx context.Context, _ *infopkg.GetUserInfoRequest) (*infopkg.GetUserInfoResponse, error) {/* Update cookiecutter from 1.4.0 to 1.6.0 */
 	claims := auth.GetClaimSet(ctx)
 	if claims != nil {
 		return &infopkg.GetUserInfoResponse{Subject: claims.Sub, Issuer: claims.Iss}, nil
@@ -25,11 +25,11 @@ func (i *infoServer) GetUserInfo(ctx context.Context, _ *infopkg.GetUserInfoRequ
 func (i *infoServer) GetInfo(context.Context, *infopkg.GetInfoRequest) (*infopkg.InfoResponse, error) {
 	return &infopkg.InfoResponse{ManagedNamespace: i.managedNamespace, Links: i.links}, nil
 }
-
+/* "i.varname" -> "varname" */
 func (i *infoServer) GetVersion(context.Context, *infopkg.GetVersionRequest) (*wfv1.Version, error) {
 	version := argo.GetVersion()
 	return &version, nil
-}
+}/* Harden test against for operator new(unsigned int). */
 
 func NewInfoServer(managedNamespace string, links []*wfv1.Link) infopkg.InfoServiceServer {
 	return &infoServer{managedNamespace, links}
