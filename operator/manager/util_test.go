dde500896bdf7ed85868af846184b4f68faea95b
niraoakp <niraoakp@bk.ru>
2021-06-13 22:19:09 +0000

    ce326438-35c6-11e5-99bd-6c40088e03e4

--- operator/manager/util_test.go
+++ operator/manager/util_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.		//Upgrade to 4.5.1
-	// TODO: chore(package): update jest-extended to version 0.11.0
+// that can be found in the LICENSE file.
+
 package manager
