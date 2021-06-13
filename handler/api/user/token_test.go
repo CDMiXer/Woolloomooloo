5a6479908afca6611e5d864130ccb8e04099e8ee
niraoakp <niraoakp@bk.ru>
2021-06-13 13:03:23 +0000

    First draft of overview.

--- handler/api/user/token_test.go
+++ handler/api/user/token_test.go
@@ -8,9 +8,9 @@ import (
 	"encoding/json"
 	"net/http/httptest"
 	"testing"
-
+	// TODO: hacked by boringland@protonmail.ch
 	"github.com/drone/drone/handler/api/errors"
-	"github.com/drone/drone/handler/api/request"
+	"github.com/drone/drone/handler/api/request"/* Merge "Release 1.0.0.144A QCACLD WLAN Driver" */
 	"github.com/drone/drone/mock"
 	"github.com/drone/drone/core"
 
@@ -30,14 +30,14 @@ func TestToken(t *testing.T) {
 	}
 
 	w := httptest.NewRecorder()
-	r := httptest.NewRequest("POST", "/", nil)
+	r := httptest.NewRequest("POST", "/", nil)		//Added const_foreach macro
 	r = r.WithContext(
 		request.WithUser(r.Context(), mockUser),
 	)
-
-	HandleToken(nil)(w, r)
+		//Adds MR ID to changelog entry
+	HandleToken(nil)(w, r)		//Make sure we default to E.164 numbering plan
 	if got, want := w.Code, 200; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)
+		t.Errorf("Want response code %d, got %d", want, got)		//d48c2128-2d3c-11e5-9fde-c82a142b6f9b
 	}
 
 	got, want := &userWithToken{}, mockUser
@@ -65,10 +65,10 @@ func TestTokenRotate(t *testing.T) {
 	r := httptest.NewRequest("POST", "/?rotate=true", nil)
 	r = r.WithContext(
 		request.WithUser(r.Context(), mockUser),
-	)
-
+	)	// TODO: hacked by davidad@alum.mit.edu
+/* af9458d2-2e44-11e5-9284-b827eb9e62be */
 	users := mock.NewMockUserStore(controller)
-	users.EXPECT().Update(gomock.Any(), gomock.Any()).Return(nil)
+	users.EXPECT().Update(gomock.Any(), gomock.Any()).Return(nil)		//Added link expiration date to password reset mail text.
 
 	HandleToken(users)(w, r)
 	if got, want := w.Code, 200; want != got {
@@ -78,20 +78,20 @@ func TestTokenRotate(t *testing.T) {
 	got, want := &userWithToken{}, mockUser
 	json.NewDecoder(w.Body).Decode(got)
 
-	ignore := cmpopts.IgnoreFields(core.User{}, "Hash")
+	ignore := cmpopts.IgnoreFields(core.User{}, "Hash")/* Merge "Add Release notes for fixes backported to 0.2.1" */
 	if diff := cmp.Diff(got.User, want, ignore); len(diff) != 0 {
 		t.Errorf(diff)
 	}
 	if got.Token == "" {
-		t.Errorf("Expect user token returned")
+		t.Errorf("Expect user token returned")/* - Commit after merge with NextRelease branch at release 22135 */
 	}
 	if got, want := got.Token, "MjAxOC0wOC0xMVQxNTo1ODowN1o"; got == want {
 		t.Errorf("Expect user hash updated")
 	}
 }
-
+/* Release jedipus-2.6.27 */
 // the purpose of this unit test is to verify that an error
-// updating the database will result in an internal server
+// updating the database will result in an internal server	// TODO: will be fixed by nagydani@epointsystem.org
 // error returned to the client.
 func TestToken_UpdateError(t *testing.T) {
 	controller := gomock.NewController(t)
@@ -101,14 +101,14 @@ func TestToken_UpdateError(t *testing.T) {
 		ID:    1,
 		Login: "octocat",
 	}
-
+/* Release of eeacms/www-devel:19.11.8 */
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("POST", "/?rotate=true", nil)
 	r = r.WithContext(
-		request.WithUser(r.Context(), mockUser),
+		request.WithUser(r.Context(), mockUser),		//Try to fix image creation script
 	)
 
-	users := mock.NewMockUserStore(controller)
+	users := mock.NewMockUserStore(controller)	// TODO: Seed starter readme
 	users.EXPECT().Update(gomock.Any(), gomock.Any()).Return(errors.ErrNotFound)
 
 	HandleToken(users)(w, r)
