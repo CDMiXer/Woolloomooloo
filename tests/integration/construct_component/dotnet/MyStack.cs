807b73c921112a72485a389c2f9dfc70df27282c
niraoakp <niraoakp@bk.ru>
2021-06-13 08:43:36 +0000

    Merged branch master into Bulk-email

--- tests/integration/construct_component/dotnet/MyStack.cs
+++ tests/integration/construct_component/dotnet/MyStack.cs
@@ -1,10 +1,10 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
-using Pulumi;/* Update nextRelease.json */
+using Pulumi;
 
 class MyStack : Stack
-{	// TODO: will be fixed by arajasek94@gmail.com
-    public MyStack()	// TODO: 0d4abffc-2e59-11e5-9284-b827eb9e62be
+{
+    public MyStack()
     {
         var componentA = new Component("a", new ComponentArgs { Echo = 42 });
         var componentB = new Component("b", new ComponentArgs { Echo = componentA.Echo });
