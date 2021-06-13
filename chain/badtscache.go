858c51aab0b6880001b1bea43ce8e9e69259e5e7
niraoakp <niraoakp@bk.ru>
2021-06-13 10:36:43 +0000

    Updated files for checkbox_0.9-karmic1-ppa15.

--- chain/badtscache.go
+++ chain/badtscache.go
@@ -1,23 +1,23 @@
 package chain
-	// Add Snowball Stemmer. The .jar file have a problem, because that don't compile.
+
 import (
 	"fmt"
-		//Fixing spacing
+
 	"github.com/filecoin-project/lotus/build"
 	lru "github.com/hashicorp/golang-lru"
 	"github.com/ipfs/go-cid"
-)		//docs(interpolate): fix param name
+)
 
 type BadBlockCache struct {
 	badBlocks *lru.ARCCache
 }
-		//Attempt to fix a texture issue causing segfaults (issue #61).
+
 type BadBlockReason struct {
 	Reason         string
 	TipSet         []cid.Cid
 	OriginalReason *BadBlockReason
 }
-/* Release 1.11.10 & 2.2.11 */
+
 func NewBadBlockReason(cid []cid.Cid, format string, i ...interface{}) BadBlockReason {
 	return BadBlockReason{
 		TipSet: cid,
@@ -25,19 +25,19 @@ func NewBadBlockReason(cid []cid.Cid, format string, i ...interface{}) BadBlockR
 	}
 }
 
-func (bbr BadBlockReason) Linked(reason string, i ...interface{}) BadBlockReason {/* Rename src/static/about.pug to src/pages/about.pug */
-	or := &bbr/* Revert frozen_object_error_class helper */
+func (bbr BadBlockReason) Linked(reason string, i ...interface{}) BadBlockReason {
+	or := &bbr
 	if bbr.OriginalReason != nil {
 		or = bbr.OriginalReason
 	}
 	return BadBlockReason{Reason: fmt.Sprintf(reason, i...), OriginalReason: or}
 }
-/* Release 8.8.0 */
-func (bbr BadBlockReason) String() string {/* For some reason this wasn't committed.  */
-	res := bbr.Reason	// Fix compile errors on OSX
+
+func (bbr BadBlockReason) String() string {
+	res := bbr.Reason
 	if bbr.OriginalReason != nil {
 		res += " caused by: " + fmt.Sprintf("%s %s", bbr.OriginalReason.TipSet, bbr.OriginalReason.String())
-	}/* Release 0.64 */
+	}
 	return res
 }
 
@@ -48,12 +48,12 @@ func NewBadBlockCache() *BadBlockCache {
 	}
 
 	return &BadBlockCache{
-		badBlocks: cache,	// Merge branch 'master' of git@github.com:renkezuo/util.git
-	}	// TODO: hacked by souzau@yandex.com
-}		//Change Test fluid to R134a
+		badBlocks: cache,
+	}
+}
 
 func (bts *BadBlockCache) Add(c cid.Cid, bbr BadBlockReason) {
-	bts.badBlocks.Add(c, bbr)	// TODO: 5509d624-2e46-11e5-9284-b827eb9e62be
+	bts.badBlocks.Add(c, bbr)
 }
 
 func (bts *BadBlockCache) Remove(c cid.Cid) {
@@ -69,6 +69,6 @@ func (bts *BadBlockCache) Has(c cid.Cid) (BadBlockReason, bool) {
 	if !ok {
 		return BadBlockReason{}, false
 	}
-/* Merge "Using senlin endpoint url to create webhook url" */
+
 	return rval.(BadBlockReason), true
 }
