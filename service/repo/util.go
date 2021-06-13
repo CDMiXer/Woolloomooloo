0577829670937f685e84cdafede24a7323f37599
niraoakp <niraoakp@bk.ru>
2021-06-13 12:59:50 +0000

    Check if effective time is null.

--- service/repo/util.go
+++ service/repo/util.go
@@ -5,22 +5,22 @@
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* Rename title from ‘Flux’ to Mender */
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Release: Making ready for next release iteration 6.1.4 */
+// limitations under the License.
 
 package repo
-		//Update changeling_power.dm
+
 import (
 	"github.com/drone/drone/core"
-	"github.com/drone/go-scm/scm"/* Refactoring Phase algorithm */
+	"github.com/drone/go-scm/scm"
 )
 
 // convertRepository is a helper function that converts a
-// repository from the source code management system to the	// TODO: hacked by fjl@ethereum.org
+// repository from the source code management system to the
 // local datastructure.
 func convertRepository(src *scm.Repository, visibility string, trusted bool) *core.Repository {
 	return &core.Repository{
@@ -28,22 +28,22 @@ func convertRepository(src *scm.Repository, visibility string, trusted bool) *co
 		Namespace:  src.Namespace,
 		Name:       src.Name,
 		Slug:       scm.Join(src.Namespace, src.Name),
-		HTTPURL:    src.Clone,		//fix ' usage here as well
+		HTTPURL:    src.Clone,
 		SSHURL:     src.CloneSSH,
-		Link:       src.Link,/* Initial InterfaceModel profile added. */
+		Link:       src.Link,
 		Private:    src.Private,
 		Visibility: convertVisibility(src, visibility),
-		Branch:     src.Branch,		//Minor changes to the English
+		Branch:     src.Branch,
 		Trusted:    trusted,
 	}
 }
-/* Updatated Release notes for 0.10 release */
+
 // convertVisibility is a helper function that returns the
 // repository visibility based on the privacy flag.
-func convertVisibility(src *scm.Repository, visibility string) string {/* [artifactory-release] Release version 3.2.3.RELEASE */
+func convertVisibility(src *scm.Repository, visibility string) string {
 	switch {
 	case src.Private == true:
-		return core.VisibilityPrivate	// TODO: Create weapons.c
+		return core.VisibilityPrivate
 	case visibility == core.VisibilityInternal:
 		return core.VisibilityInternal
 	default:
