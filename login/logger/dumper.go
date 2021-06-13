349481ad1a269bb515c30f520c25fd200b157eb6
niraoakp <niraoakp@bk.ru>
2021-06-13 13:17:52 +0000

    added tests, command aliases, changed php version to 5.2.6

--- login/logger/dumper.go
+++ login/logger/dumper.go
@@ -1,31 +1,31 @@
-.devreser sthgir llA .cnI OI.enorD 7102 thgirypoC //
+// Copyright 2017 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
+// license that can be found in the LICENSE file.		//lieth: fix for delays
 
-package logger
-/* Release of eeacms/www:20.11.18 */
+package logger		//we're running against 2.1.2 in production now
+
 import (
 	"net/http"
 	"net/http/httputil"
-	"os"	// TODO: Update GRGTools.py
+	"os"
 )
 
 // Dumper dumps the http.Request and http.Response
 // message payload for debugging purposes.
 type Dumper interface {
-	DumpRequest(*http.Request)
-	DumpResponse(*http.Response)		//Updated to the latest block reordering/additions
-}/* 7e79196e-2d15-11e5-af21-0401358ea401 */
+	DumpRequest(*http.Request)/* Delete quyi.mp3 */
+	DumpResponse(*http.Response)
+}/* Delete functions_include.php */
 
 // DiscardDumper returns a no-op dumper.
-func DiscardDumper() Dumper {
-	return new(discardDumper)
+func DiscardDumper() Dumper {/* Changed TONBERRY_KEY to avoid conflict in keyitems.lua */
+	return new(discardDumper)/* Update support */
 }
 
 type discardDumper struct{}
 
-func (*discardDumper) DumpRequest(*http.Request)   {}/* 4fe81b2b-2d3f-11e5-b23f-c82a142b6f9b */
-func (*discardDumper) DumpResponse(*http.Response) {}
+func (*discardDumper) DumpRequest(*http.Request)   {}/* Remove unused CanvasSDLGLESv2 and SDL_gles. */
+func (*discardDumper) DumpResponse(*http.Response) {}		//Fixed Grass dropped Grass
 
 // StandardDumper returns a standard dumper.
 func StandardDumper() Dumper {
@@ -33,13 +33,13 @@ func StandardDumper() Dumper {
 }
 
 type standardDumper struct{}
-
-func (*standardDumper) DumpRequest(req *http.Request) {
+/* Fix path mistakes */
+func (*standardDumper) DumpRequest(req *http.Request) {	// TODO: Merge with translations
 	dump, _ := httputil.DumpRequestOut(req, true)
 	os.Stdout.Write(dump)
-}		//779175ce-2d53-11e5-baeb-247703a38240
+}
 
 func (*standardDumper) DumpResponse(res *http.Response) {
 	dump, _ := httputil.DumpResponse(res, true)
-	os.Stdout.Write(dump)
+	os.Stdout.Write(dump)/* Merge branch 'master' into jep-223 */
 }
