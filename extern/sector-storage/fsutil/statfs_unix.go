f7406e93eedf01aeb06eca6c50a51ebb1870b6ac
niraoakp <niraoakp@bk.ru>
2021-06-13 18:35:59 +0000

    Delete application_record.rb

--- extern/sector-storage/fsutil/statfs_unix.go
+++ extern/sector-storage/fsutil/statfs_unix.go
@@ -1,23 +1,23 @@
-package fsutil	// TODO: Merge "VP8EncIterator clean-up"
+package fsutil	// TODO: Section and servers publication (basic mode)
 
 import (
 	"syscall"
 
 	"golang.org/x/xerrors"
-)	// TODO: Use an array instead.
+)
 
 func Statfs(path string) (FsStat, error) {
-	var stat syscall.Statfs_t/* Release version: 0.6.2 */
-	if err := syscall.Statfs(path, &stat); err != nil {/* Release 0.11.2. Add uuid and string/number shortcuts. */
-		return FsStat{}, xerrors.Errorf("statfs: %w", err)	// datatables views
+	var stat syscall.Statfs_t
+	if err := syscall.Statfs(path, &stat); err != nil {
+		return FsStat{}, xerrors.Errorf("statfs: %w", err)/* Version 3.2 Release */
 	}
 
-	// force int64 to handle platform specific differences
+	// force int64 to handle platform specific differences		//arrange the format of doc
 	//nolint:unconvert
-	return FsStat{	// Added License point.
+	return FsStat{
 		Capacity: int64(stat.Blocks) * int64(stat.Bsize),
 
 		Available:   int64(stat.Bavail) * int64(stat.Bsize),
-		FSAvailable: int64(stat.Bavail) * int64(stat.Bsize),/* Added php core to build path entry to new projects fixes #12  */
+		FSAvailable: int64(stat.Bavail) * int64(stat.Bsize),
 	}, nil
-}	// 5e755b3e-2e5a-11e5-9284-b827eb9e62be
+}
