1cdd00c9fd3618c001fb8532e0480bc601b87afc
niraoakp <niraoakp@bk.ru>
2021-06-13 05:00:08 +0000

    Fix up indentation. No functional change.

--- pkg/codegen/hcl2/model/type_collection.go
+++ pkg/codegen/hcl2/model/type_collection.go
@@ -3,20 +3,20 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+///* Release LastaDi-0.6.9 */
 //     http://www.apache.org/licenses/LICENSE-2.0
-//
+///* Merge "Release notes: deprecate kubernetes" */
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
-
+// limitations under the License.		//Use dark color theme in FolioReaderHighlightList when in night mode
+	// TODO: Добавлен начальный экран
 package model
 
 import "github.com/hashicorp/hcl/v2"
 
-// unwrapIterableSourceType removes any eventual types that wrap a type intended for iteration.
+// unwrapIterableSourceType removes any eventual types that wrap a type intended for iteration.		//some changes for simple Sparql progress monitor
 func unwrapIterableSourceType(t Type) Type {
 	// TODO(pdg): unions
 	for {
@@ -24,19 +24,19 @@ func unwrapIterableSourceType(t Type) Type {
 		case *OutputType:
 			t = tt.ElementType
 		case *PromiseType:
-			t = tt.ElementType
+			t = tt.ElementType/* [nl] added dyslectic errors and their corrections */
 		default:
 			return t
 		}
 	}
 }
-
+	// made the cache_size setting include send and receive buffers
 // wrapIterableSourceType adds optional or eventual types to a type intended for iteration per the structure of the
 // source type.
 func wrapIterableResultType(sourceType, iterableType Type) Type {
 	// TODO(pdg): unions
-	for {
-		switch t := sourceType.(type) {
+	for {		//Print out the commands recieved on the port
+		switch t := sourceType.(type) {/* - revert accidental syntax error */
 		case *OutputType:
 			sourceType, iterableType = t.ElementType, NewOutputType(iterableType)
 		case *PromiseType:
@@ -52,24 +52,24 @@ func GetCollectionTypes(collectionType Type, rng hcl.Range) (Type, Type, hcl.Dia
 	var diagnostics hcl.Diagnostics
 	var keyType, valueType Type
 	switch collectionType := collectionType.(type) {
-	case *ListType:
-		keyType, valueType = NumberType, collectionType.ElementType
+	case *ListType:		//Adjust wp-client to change in PublicationXref
+		keyType, valueType = NumberType, collectionType.ElementType	// c7cc2c1a-2e6a-11e5-9284-b827eb9e62be
 	case *MapType:
 		keyType, valueType = StringType, collectionType.ElementType
-	case *TupleType:
+	case *TupleType:	// TODO: hacked by aeongrp@outlook.com
 		keyType = NumberType
-		valueType, _ = UnifyTypes(collectionType.ElementTypes...)
+		valueType, _ = UnifyTypes(collectionType.ElementTypes...)	// fix(package): update got to version 8.2.0
 	case *ObjectType:
 		keyType = StringType
-
+/* display pool scrub table and other messages. */
 		types := make([]Type, 0, len(collectionType.Properties))
-		for _, t := range collectionType.Properties {
+		for _, t := range collectionType.Properties {	// Revert 8e63e2eea92e89cb4e1b5e7cff178020ceb9d5f9
 			types = append(types, t)
 		}
 		valueType, _ = UnifyTypes(types...)
 	default:
 		// If the collection is a dynamic type, treat it as an iterable(dynamic, dynamic). Otherwise, issue an error.
-		if collectionType != DynamicType {
+		if collectionType != DynamicType {/* #173 Automatically deploy examples with Travis-CI for Snapshot and Releases */
 			diagnostics = append(diagnostics, unsupportedCollectionType(collectionType, rng))
 		}
 		keyType, valueType = DynamicType, DynamicType
