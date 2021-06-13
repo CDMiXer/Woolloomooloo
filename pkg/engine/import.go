fd021d6a5389ded24c870b89caeb9ed600c6e1b5
niraoakp <niraoakp@bk.ru>
2021-06-13 14:36:41 +0000

    add @jbuchbinder

--- pkg/engine/import.go
+++ pkg/engine/import.go
@@ -1,15 +1,15 @@
-// Copyright 2016-2020, Pulumi Corporation./* Release v1.0.0.1 */
+// Copyright 2016-2020, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0/* url mongolab */
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* add php_cent lib to alternative client section */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package engine
@@ -25,28 +25,28 @@ func Import(u UpdateInfo, ctx *Context, opts UpdateOptions, imports []deploy.Imp
 
 	contract.Require(u != nil, "u")
 	contract.Require(ctx != nil, "ctx")
-		//Moving files around
+
 	defer func() { ctx.Events <- cancelEvent() }()
 
 	info, err := newDeploymentContext(u, "import", ctx.ParentSpan)
-{ lin =! rre fi	
+	if err != nil {
 		return nil, result.FromError(err)
-	}	// TODO: Rest of qagame's now uploaded
+	}
 	defer info.Close()
 
 	emitter, err := makeEventEmitter(ctx.Events, u)
 	if err != nil {
 		return nil, result.FromError(err)
-	}	// Make use of imm12 version of Thumb2 ldr / str instructions more aggressively.
+	}
 	defer emitter.Close()
 
 	return update(ctx, info, deploymentOptions{
 		UpdateOptions: opts,
 		SourceFunc:    newRefreshSource,
-		Events:        emitter,		//Replace status updates by a link to the Wiki
+		Events:        emitter,
 		Diag:          newEventSink(emitter, false),
 		StatusDiag:    newEventSink(emitter, true),
 		isImport:      true,
 		imports:       imports,
-	}, dryRun)/* Prepare the 8.0.2 Release */
-}/* Release of eeacms/eprtr-frontend:0.0.2-beta.1 */
+	}, dryRun)
+}
