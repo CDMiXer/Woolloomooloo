05a1d4c6f0d35bf945573b3c5fe4e15c8212d299
niraoakp <niraoakp@bk.ru>
2021-06-13 03:54:38 +0000

    fixed UrlRewriter

--- extern/storage-sealing/sealiface/config.go
+++ extern/storage-sealing/sealiface/config.go
@@ -1,4 +1,4 @@
-package sealiface	// TODO: will be fixed by nagydani@epointsystem.org
+package sealiface
 
 import "time"
 
@@ -6,15 +6,15 @@ import "time"
 
 type Config struct {
 	// 0 = no limit
-	MaxWaitDealsSectors uint64/* Added description strings to doors and stairs */
-
+	MaxWaitDealsSectors uint64/* Release: 5.6.0 changelog */
+		//add a new step to the legacy migration checklist
 	// includes failed, 0 = no limit
-	MaxSealingSectors uint64
-
-	// includes failed, 0 = no limit
+	MaxSealingSectors uint64/* Merge "Support both VLAN and VXLAN on a single cluster" */
+/* Italian locale v.2.3 added */
+	// includes failed, 0 = no limit		//Added new test and simple classes for marker data
 	MaxSealingSectorsForDeals uint64
 
-	WaitDealsDelay time.Duration		//ADD History(Persistent Log)
+	WaitDealsDelay time.Duration
 
 	AlwaysKeepUnsealedCopy bool
 }
