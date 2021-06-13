cbda45e3190282035a5f1acdc26d3abf90fe8601
niraoakp <niraoakp@bk.ru>
2021-06-13 19:20:55 +0000

    Fixed syntax typo in maintainers.json

--- tests/integration/stack_reference/python/step2/__main__.py
+++ tests/integration/stack_reference/python/step2/__main__.py
@@ -1,18 +1,18 @@
-# Copyright 2020, Pulumi Corporation.  All rights reserved./* Release 0.6.7 */
+# Copyright 2020, Pulumi Corporation.  All rights reserved.
 
-import pulumi/* Merge "Modify lowercase to uppercase" */
-	// Added island tracking via yml database instead of filesystem.
+import pulumi
+
 config = pulumi.Config()
-org = config.require('org')		//Added icons with fancy css
+org = config.require('org')
 slug = f"{org}/{pulumi.get_project()}/{pulumi.get_stack()}"
-a = pulumi.StackReference(slug)		//Fixed - installation issues on clean Debian, Red Hat
+a = pulumi.StackReference(slug)
 
 got_err = False
-
-try:/* Release LastaFlute */
+/* Merge "Fix unbound variable error in scripts/collect-test-info.sh" */
+try:
     a.get_output('val2')
-except Exception:
-    got_err = True		//Rename HTML5+BootstrapUpdate to HTML5andBootstrapUpdate.htm
+except Exception:	// TODO: "mauvaise initialisation de $srcWidth"
+    got_err = True
 
-if not got_err:/* add uniquewrapper */
+if not got_err:
     raise Exception('Expected to get error trying to read secret from stack reference.')
