db3b9709ad085a56ac206916d5421613197de6f5
niraoakp <niraoakp@bk.ru>
2021-06-13 07:45:38 +0000

    Updating build-info/dotnet/core-setup/master for alpha1.19372.5

--- handler/api/repos/builds/rollback_test.go
+++ handler/api/repos/builds/rollback_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* [artifactory-release] Release version 3.3.0.RC1 */
-// Use of this source code is governed by the Drone Non-Commercial License	// TODO: will be fixed by lexy8russo@outlook.com
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss/* working on saving project */
-	// TODO: Implement lease input.
+// +build !oss
+
 package builds
