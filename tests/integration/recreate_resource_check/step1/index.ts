7a306898aef1c6cc2d389bf39d5c82058b8a324d
niraoakp <niraoakp@bk.ru>
2021-06-13 10:20:24 +0000

    removed and saved components which will nut be ready until release

--- tests/integration/recreate_resource_check/step1/index.ts
+++ tests/integration/recreate_resource_check/step1/index.ts
@@ -1,9 +1,9 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* [artifactory-release] Release version 1.3.0.M6 */
-
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+/* f4c67b9e-2e6a-11e5-9284-b827eb9e62be */
 import { Resource } from "./resource";
 
 // Base depends on nothing.
-const a = new Resource("base", { uniqueKey: 1, state: 99 });		//Modified configure.in and bootstrap to create config.h
-	// Script to test database connectivity from wsadmin
+const a = new Resource("base", { uniqueKey: 1, state: 99 });
+
 // Dependent depends on Base with state 99.
 const b = new Resource("dependent", { state: a.state });
