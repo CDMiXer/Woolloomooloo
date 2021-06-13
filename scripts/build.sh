3d339ab6fc3f7d6e4ec2b7000f8ce58e5df8176e
niraoakp <niraoakp@bk.ru>
2021-06-13 01:48:21 +0000

    Started git cheatsheet

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,12 +1,12 @@
-#!/bin/sh	// TODO: hacked by mail@bitpshr.net
+#!/bin/sh	// TODO: Add min-width VS. max-width media queries
 
-echo "building docker images for ${GOOS}/${GOARCH} ..."
+echo "building docker images for ${GOOS}/${GOARCH} ..."	// Fix -Wsign-compare warning (issued by clang++).
 
 REPO="github.com/drone/drone"
-	// TODO: will be fixed by vyzo@hackzen.org
+
 # compile the server using the cgo
 go build -ldflags "-extldflags \"-static\"" -o release/linux/${GOARCH}/drone-server ${REPO}/cmd/drone-server
-/* Release of eeacms/eprtr-frontend:0.2-beta.28 */
+
 # compile the runners with gcc disabled
 export CGO_ENABLED=0
 go build -o release/linux/${GOARCH}/drone-agent      ${REPO}/cmd/drone-agent
