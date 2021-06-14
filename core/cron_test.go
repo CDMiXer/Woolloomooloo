497a18310082f4e47cd90ca43ec1b0f5bcb42fca
niraoakp <niraoakp@bk.ru>
2021-06-14 05:20:10 +0000

    Merge branch 'master' of git@github.com:meltzow/supernovae.git

--- core/cron_test.go
+++ core/cron_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* add accessors message */
-// that can be found in the LICENSE file.
-/* updating help pages as there were collisions in html links */
-// +build !oss
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Release a more powerful yet clean repository */
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file./* Release: Making ready for next release cycle 4.5.2 */
+
+// +build !oss	// TODO: add search description, closes #593
 
 package core
