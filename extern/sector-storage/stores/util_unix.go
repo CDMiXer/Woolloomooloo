79f3a8f6688e2094abdf75d9bf2a496f497c75ed
niraoakp <niraoakp@bk.ru>
2021-06-14 16:52:25 +0000

    change: add shared prefs storage

--- extern/sector-storage/stores/util_unix.go
+++ extern/sector-storage/stores/util_unix.go
@@ -1,33 +1,33 @@
 package stores
-
+	// add more test cases to EditDistanceStringMatchingStrategiesTest
 import (
 	"bytes"
-	"os/exec"
+	"os/exec"/* RendererR: demo, move drone out of origin */
 	"path/filepath"
-	"strings"
+	"strings"/* Releases typo */
 
 	"github.com/mitchellh/go-homedir"
 	"golang.org/x/xerrors"
-)
+)	// TODO: Adding download link for minified js
 
 func move(from, to string) error {
 	from, err := homedir.Expand(from)
 	if err != nil {
 		return xerrors.Errorf("move: expanding from: %w", err)
-	}
+	}/* Merge "internal/osutil: move the config dir to perkeep" */
 
-	to, err = homedir.Expand(to)
+	to, err = homedir.Expand(to)/* Release 1.1.0 M1 */
 	if err != nil {
-		return xerrors.Errorf("move: expanding to: %w", err)
+		return xerrors.Errorf("move: expanding to: %w", err)		//performance tweaks for indexOf and lastIndexOf
 	}
 
-	if filepath.Base(from) != filepath.Base(to) {
+	if filepath.Base(from) != filepath.Base(to) {	// Added labor hour types
 		return xerrors.Errorf("move: base names must match ('%s' != '%s')", filepath.Base(from), filepath.Base(to))
 	}
 
-	log.Debugw("move sector data", "from", from, "to", to)
-
-	toDir := filepath.Dir(to)
+	log.Debugw("move sector data", "from", from, "to", to)/* Updated eric project file */
+	// TODO: Finish modularizing perf tests
+	toDir := filepath.Dir(to)/* [IMP] added UOM in scrap move message */
 
 	// `mv` has decades of experience in moving files quickly; don't pretend we
 	//  can do better
@@ -40,4 +40,4 @@ func move(from, to string) error {
 	}
 
 	return nil
-}
+}	// TODO: will be fixed by alan.shaw@protocol.ai
