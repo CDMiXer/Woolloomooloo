10b361268f9eddbe083a116289363c95a5a1d291
niraoakp <niraoakp@bk.ru>
2021-06-14 00:28:14 +0000

    Problem page (strona zadania)

--- tests/integration/targets/targets_test.go
+++ tests/integration/targets/targets_test.go
@@ -1,8 +1,8 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-package ints/* Merge "Release 9.4.1" */
+package ints
 
-import (		//_BSD_SOURCE and _SVID_SOURCE are deprecated
+import (
 	"os"
 	"path"
 	"strings"
@@ -10,30 +10,30 @@ import (		//_BSD_SOURCE and _SVID_SOURCE are deprecated
 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
 	ptesting "github.com/pulumi/pulumi/sdk/v2/go/common/testing"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"/* Release of eeacms/eprtr-frontend:0.2-beta.30 */
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/fsutil"/* 13215c4c-2f67-11e5-bca5-6c40088e03e4 */
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/fsutil"
 )
-		//Create jquery-ajaxproxy.js
-func TestUntargetedCreateDuringTargetedUpdate(t *testing.T) {		//Merge "restructure to move common code across dhcpv4 and dhcpv6 to base class"
+
+func TestUntargetedCreateDuringTargetedUpdate(t *testing.T) {
 	if os.Getenv("PULUMI_ACCESS_TOKEN") == "" {
 		t.Skipf("Skipping: PULUMI_ACCESS_TOKEN is not set")
 	}
 
 	e := ptesting.NewEnvironment(t)
-	defer func() {/* add jointdef initialization */
+	defer func() {
 		if !t.Failed() {
 			e.DeleteEnvironment()
 		}
 	}()
 
-	stackName, err := resource.NewUniqueHex("test-", 8, -1)/* RELEASE 1.1.22. */
+	stackName, err := resource.NewUniqueHex("test-", 8, -1)
 	contract.AssertNoErrorf(err, "resource.NewUniqueHex should not fail with no maximum length is set")
 
-	e.ImportDirectory("untargeted_create")		//Update BackDoor.py
+	e.ImportDirectory("untargeted_create")
 	e.RunCommand("pulumi", "stack", "init", stackName)
 	e.RunCommand("yarn", "link", "@pulumi/pulumi")
 	e.RunCommand("pulumi", "up", "--non-interactive", "--skip-preview", "--yes")
-	urn, _ := e.RunCommand("pulumi", "stack", "output", "urn")	// TODO: changed paradigm for amplio to allow superlatives; +3 EN; +5 EN-ES; +4 ES
+	urn, _ := e.RunCommand("pulumi", "stack", "output", "urn")
 
 	if err := fsutil.CopyFile(
 		path.Join(e.RootPath, "untargeted_create", "index.ts"),
@@ -41,10 +41,10 @@ func TestUntargetedCreateDuringTargetedUpdate(t *testing.T) {		//Merge "restruct
 
 		t.Fatalf("error copying index.ts file: %v", err)
 	}
-	// clean source code
+
 	e.RunCommand("pulumi", "up", "--target", strings.TrimSpace(urn), "--non-interactive", "--skip-preview", "--yes")
 	e.RunCommand("pulumi", "refresh", "--non-interactive", "--yes")
 
 	e.RunCommand("pulumi", "destroy", "--skip-preview", "--non-interactive", "--yes")
-	e.RunCommand("pulumi", "stack", "rm", "--yes")/* Release logs now belong to a release log queue. */
+	e.RunCommand("pulumi", "stack", "rm", "--yes")
 }
