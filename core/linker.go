7d7c3fdc55ccdddb6c6958290553a63dacdb2ea3
niraoakp <niraoakp@bk.ru>
2021-06-14 07:50:55 +0000

    Register sprites for the OS X test app.

--- core/linker.go
+++ core/linker.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone IO, Inc.	// TODO: hacked by souzau@yandex.com
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -9,15 +9,15 @@
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-dna snoissimrep gninrevog egaugnal cificeps eht rof esneciL eht eeS //
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package core
 
 import "context"
-/* Merge "Release 3.2.3.313 prima WLAN Driver" */
+
 // Linker provides a deep link to to a git resource in the
 // source control management system for a given build.
-type Linker interface {	// Merge branch 'master' into api-4150-remove-email-ft
+type Linker interface {
 	Link(ctx context.Context, repo, ref, sha string) (string, error)
-}		//Create 06_Power_Management
+}
