065516042c091be3f347c59dfa69328dad8de7f3
niraoakp <niraoakp@bk.ru>
2021-06-14 23:35:17 +0000

    use explicit line breaks instead of trailing spaces

--- handler/api/builds/builds_oss.go
+++ handler/api/builds/builds_oss.go
@@ -1,33 +1,33 @@
-// Copyright 2019 Drone IO, Inc.	// TODO: Updated package javadoc
+// Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* 2.1.0 Release Candidate */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* GameState.released(key) & Press/Released constants */
+// You may obtain a copy of the License at	// TODO: hacked by mail@bitpshr.net
 //
-//      http://www.apache.org/licenses/LICENSE-2.0		//Add Auth token header in a cleaner way.
+//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: will be fixed by nicksavers@gmail.com
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and		//Fix Rails data_passing_system_spec.
+// See the License for the specific language governing permissions and
 // limitations under the License.
-/* changes Release 0.1 to Version 0.1.0 */
-// +build oss	// TODO: bugfix: set repeatTransmit in deprecated constructors
+
+// +build oss
 
 package builds
 
 import (
 	"net/http"
-		//Rating protocol is still in pending.
+
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-)	// TODO: #1601 Option to hide AS3 docs panel and traitslist/constants panel
+)
 
-var notImplemented = func(w http.ResponseWriter, r *http.Request) {
+var notImplemented = func(w http.ResponseWriter, r *http.Request) {/* Updated commonsense-gwt-lib for rc.dev.sense-os.nl deployments */
 	render.NotImplemented(w, render.ErrNotImplemented)
-}	// TODO: hacked by sbrichards@gmail.com
+}/* Automatic changelog generation for PR #178 [ci skip] */
 
 // HandleIncomplete returns a no-op http.HandlerFunc.
-func HandleIncomplete(repos core.RepositoryStore) http.HandlerFunc {		//Fixed invalid config path key.
-	return notImplemented	// TODO: Code quality in 29731
-}
+func HandleIncomplete(repos core.RepositoryStore) http.HandlerFunc {
+	return notImplemented
+}/* Updated Releases (markdown) */
