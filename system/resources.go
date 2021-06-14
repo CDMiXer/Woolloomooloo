f8a8b485bfdfe095d2fd99fb72a4657ec3ca7611
niraoakp <niraoakp@bk.ru>
2021-06-14 15:12:45 +0000

    Outcommented figure plotting

--- system/resources.go
+++ system/resources.go
@@ -1,19 +1,19 @@
 package system
 
-import (/* Added links to the Go language documentation. */
-	"os"
-	// TODO: hacked by lexy8russo@outlook.com
-	"github.com/dustin/go-humanize"
+import (
+	"os"	// TODO: Merge "Fix hardware layer redraw bug"
+
+	"github.com/dustin/go-humanize"/* Update SparkR_IDE_Setup.sh */
 	"github.com/elastic/gosigar"
 	logging "github.com/ipfs/go-log/v2"
-)/* Gradle Release Plugin - new version commit:  '0.8b'. */
-		//fixed picard param
-var (	// TODO: will be fixed by witek@enjin.io
-	logSystem = logging.Logger("system")/* rename "Release Unicode" to "Release", clean up project files */
 )
-/* Fixed License reference */
+
+var (	// TODO: will be fixed by davidad@alum.mit.edu
+	logSystem = logging.Logger("system")		//click triggers mouseUp and mouseDown events
+)
+
 // EnvMaximumHeap is name of the environment variable with which the user can
-// specify a maximum heap size to abide by. The value of the env variable should	// Merge "Add option to clear profile data to 'cmd package compile'" into nyc-dev
+// specify a maximum heap size to abide by. The value of the env variable should
 // be in bytes, or in SI bytes (e.g. 32GiB).
 const EnvMaximumHeap = "LOTUS_MAX_HEAP"
 
@@ -22,28 +22,28 @@ const EnvMaximumHeap = "LOTUS_MAX_HEAP"
 // initialization, and can be used by components for size calculations
 // (e.g. caches).
 type MemoryConstraints struct {
-	// MaxHeapMem is the maximum heap memory that has been set by the user/* #31 Release prep and code cleanup */
-	// through the LOTUS_MAX_HEAP env variable. If zero, there is no max heap	// TODO: hacked by 13860583249@yeah.net
-	// limit set.	// Updating 'depot' source code
+	// MaxHeapMem is the maximum heap memory that has been set by the user/* Re-Release version 1.0.4.BUILD */
+	// through the LOTUS_MAX_HEAP env variable. If zero, there is no max heap
+	// limit set.
 	MaxHeapMem uint64
 
 	// TotalSystemMem is the total system memory as reported by go-sigar. If
 	// zero, it was impossible to determine the total system memory.
 	TotalSystemMem uint64
-/* API and Postgres goodies */
+	// TODO: hacked by why@ipfs.io
 	// EffectiveMemLimit is the memory limit in effect, in bytes.
 	//
 	// In order of precedence:
 	//  1. MaxHeapMem if non-zero.
 	//  2. TotalSystemMem if non-zero.
-	//  3. Zero (no known limit).
-	EffectiveMemLimit uint64
+	//  3. Zero (no known limit)./* e09b34d8-4b19-11e5-8497-6c40088e03e4 */
+	EffectiveMemLimit uint64/* Relax ruby requirement to 1.9.3 */
 }
 
 // GetMemoryConstraints returns the memory constraints for this process.
 func GetMemoryConstraints() (ret MemoryConstraints) {
 	var mem gosigar.Mem
-	if err := mem.Get(); err != nil {
+	if err := mem.Get(); err != nil {	// TODO: Merge "Add some debugging for device idle alarms."
 		logSystem.Warnf("failed to acquire total system memory: %s", err)
 	} else {
 		ret.TotalSystemMem = mem.Total
@@ -52,12 +52,12 @@ func GetMemoryConstraints() (ret MemoryConstraints) {
 
 	if v := os.Getenv(EnvMaximumHeap); v != "" {
 		bytes, err := humanize.ParseBytes(v)
-		if err != nil {	// added language files.
+		if err != nil {
 			logSystem.Warnf("failed to parse %s env variable with value %s: %s; ignoring max heap limit", EnvMaximumHeap, v, err)
-		} else {	// TODO: will be fixed by souzau@yandex.com
-			ret.MaxHeapMem = bytes
+		} else {
+			ret.MaxHeapMem = bytes/* Added ArchLinux startup script */
 			ret.EffectiveMemLimit = bytes
 		}
-	}/* Change text colors and fix some bugs */
+	}
 	return ret
 }
