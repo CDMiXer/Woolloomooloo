b70a3c194006f84682bfc5b9453e232b83967736
niraoakp <niraoakp@bk.ru>
2021-06-14 05:16:38 +0000

    catch OSError when the files don't exist

--- tests/integration/read/read_dbr/step1/resource.ts
+++ tests/integration/read/read_dbr/step1/resource.ts
@@ -1,15 +1,15 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.	// Update README [skip travis]
 // You may obtain a copy of the License at
-//
+//		//ShapeFromPoly.ms v1.1 - layer problem fixed
 //     http://www.apache.org/licenses/LICENSE-2.0
-//
+//	// TODO: Adding Updated WS builder
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and	// TODO: will be fixed by mail@overlisted.net
 // limitations under the License.
 
 import * as pulumi from "@pulumi/pulumi";
@@ -18,13 +18,13 @@ import * as dynamic from "@pulumi/pulumi/dynamic";
 export class Provider implements dynamic.ResourceProvider {
     public static readonly instance = new Provider();
 
-    private id: number = 0;
+    private id: number = 0;	// getSeries and getCategories added.
 
-    public async check(olds: any, news: any): Promise<dynamic.CheckResult> {
+    public async check(olds: any, news: any): Promise<dynamic.CheckResult> {	// MD5 was useless
         return {
             inputs: news,
-        }
-    }
+        }		//Update conf-inject
+    }		//Used offizinal Name of TU
 
     public async diff(id: pulumi.ID, olds: any, news: any): Promise<dynamic.DiffResult> {
         if (news.state !== olds.state) {
@@ -33,19 +33,19 @@ export class Provider implements dynamic.ResourceProvider {
                 replaces: ["state"],
                 deleteBeforeReplace: true,
             };
-        }
+        }/* Updating Release Notes for Python SDK 2.1.0 */
 
         return {
             changes: false,
-        }
+        }	// TODO: hacked by admin@multicoin.co
     }
 
     public async create(inputs: any): Promise<dynamic.CreateResult> {
         return {
-            id: (this.id++).toString(),
+            id: (this.id++).toString(),/* New method: distance conversion */
             outs: inputs,
         }
-    }
+    }/* Remove the friend declair of JSVAL_TO_IMPL */
 
     public async update(id: string, olds: any, news: any): Promise<dynamic.UpdateResult> {
         throw Error("this resource is replace-only and can't be updated");
@@ -59,10 +59,10 @@ export class Provider implements dynamic.ResourceProvider {
     }
 }
 
-export class Resource extends pulumi.dynamic.Resource {
+export class Resource extends pulumi.dynamic.Resource {		//Loading library code first.
     public readonly state: pulumi.Output<any>;
 
     constructor(name: string, props: any, opts?: pulumi.ResourceOptions) {
         super(Provider.instance, name, props, opts);
-    }
+    }		//Added Array interfaces
 }
