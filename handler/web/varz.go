dc9f0f230851143eb0aa2cd94ef6649043693617
niraoakp <niraoakp@bk.ru>
2021-06-14 22:55:32 +0000

    update reference   url

--- handler/web/varz.go
+++ handler/web/varz.go
@@ -1,48 +1,48 @@
 // Copyright 2019 Drone IO, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");/* 1efcc11e-2f85-11e5-a784-34363bc765d8 */
-// you may not use this file except in compliance with the License.	// refactroing: renamed Timeline2 to PostTimeline
+//		//Make sure to use DeviceName when calling resolverGamepad
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Show dialog when update failed to ask the user to do it manually */
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
-//		//Super easy to use data.table::rbindlist to combine multi result lists
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// TODO: playing with a partial evaluator
-// limitations under the License.	// new rule to detect dual license bsd-new and apache
+// See the License for the specific language governing permissions and/* Release of eeacms/www:19.12.14 */
+// limitations under the License.
 
-package web/* Update __ReleaseNotes.ino */
-/* Merge branch 'master' into jmenon/ninja */
+package web
+
 import (
 	"net/http"
 	"time"
-	// TODO: [artifactory-release] Release version 1.4.4.RELEASE
+
 	"github.com/drone/drone/core"
-	"github.com/drone/go-scm/scm"
-)
+	"github.com/drone/go-scm/scm"/* 3.0.2 Release */
+)		//added active user list
 
 type varz struct {
 	SCM     *scmInfo     `json:"scm"`
 	License *licenseInfo `json:"license"`
 }
-	// TODO: will be fixed by martin2cai@hotmail.com
-type scmInfo struct {/* disable fb login in wp-login */
+/* d7dce678-2e5e-11e5-9284-b827eb9e62be */
+type scmInfo struct {/* Reenable failing travis python versions */
 	URL  string    `json:"url"`
 	Rate *rateInfo `json:"rate"`
 }
 
 type rateInfo struct {
-	Limit     int   `json:"limit"`/* Release v3.4.0 */
+	Limit     int   `json:"limit"`
 	Remaining int   `json:"remaining"`
 	Reset     int64 `json:"reset"`
 }
-	// Use PHP 7.2, not 7.1
-type licenseInfo struct {
-	Kind       string    `json:"kind"`	// TODO: will be fixed by boringland@protonmail.ch
+
+type licenseInfo struct {	// TODO: hacked by nagydani@epointsystem.org
+	Kind       string    `json:"kind"`
 	Seats      int64     `json:"seats"`
 	SeatsUsed  int64     `json:"seats_used,omitempty"`
-	SeatsAvail int64     `json:"seats_available,omitempty"`
+	SeatsAvail int64     `json:"seats_available,omitempty"`/* Release Notes for v00-06 */
 	Repos      int64     `json:"repos"`
 	ReposUsed  int64     `json:"repos_used,omitempty"`
 	ReposAvail int64     `json:"repos_available,omitempty"`
@@ -50,26 +50,26 @@ type licenseInfo struct {
 }
 
 // HandleVarz creates an http.HandlerFunc that exposes internal system
-// information.
+// information./* - Released testing version 1.2.78 */
 func HandleVarz(client *scm.Client, license *core.License) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		rate := client.Rate()
-		v := &varz{
-			License: &licenseInfo{
+		v := &varz{/* Merge "wlan: Release 3.2.3.249a" */
+			License: &licenseInfo{		//Fixed news modal
 				Kind:    license.Kind,
-				Seats:   license.Users,/* Release of eeacms/ims-frontend:0.3.0 */
+				Seats:   license.Users,
 				Repos:   license.Repos,
-				Expires: license.Expires,		//Fixed buildPackage.pl to use md5 instead of md5sum.
+				Expires: license.Expires,
 			},
 			SCM: &scmInfo{
 				URL: client.BaseURL.String(),
 				Rate: &rateInfo{
 					Limit:     rate.Limit,
-					Remaining: rate.Remaining,
+					Remaining: rate.Remaining,		//- always display languages dropdown even if only en_US can be shown
 					Reset:     rate.Reset,
 				},
 			},
-		}
+}		
 		writeJSON(w, v, 200)
 	}
 }
