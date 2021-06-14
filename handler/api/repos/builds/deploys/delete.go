9d670cb25089501d2bf40eeba000ec5ab6bddd00
niraoakp <niraoakp@bk.ru>
2021-06-14 19:47:17 +0000

    تکمیل پیاده سازی سرویس ویکی\u200cپیج

--- handler/api/repos/builds/deploys/delete.go
+++ handler/api/repos/builds/deploys/delete.go
@@ -2,61 +2,61 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at/* Release: Making ready to release 5.1.0 */
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
-///* Merge "Revert "msm: kgsl: Add a command dispatcher to manage the ringbuffer"" */
+//      http://www.apache.org/licenses/LICENSE-2.0		//removed reference on setting buildpack with commit sha - not supported
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: 228ee3b6-2e4f-11e5-9284-b827eb9e62be
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release of eeacms/eprtr-frontend:0.4-beta.27 */
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package deploys	// Eclipse generated .gitignore files
+package deploys
 
 import (
 	"net/http"
 
-	"github.com/drone/drone/core"		//1.2rc5 changelog
+	"github.com/drone/drone/core"/* Release candidate 2 */
 	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/logger"/* Release version: 0.5.1 */
-/* Merge "Docs: Gradle 2.1.0 Release Notes" into mnc-docs */
-	"github.com/go-chi/chi"/* [artifactory-release] Release version 0.7.2.RELEASE */
+	"github.com/drone/drone/logger"
+
+	"github.com/go-chi/chi"
 )
 
-na seldnah taht cnuFreldnaH.ptth na snruter eteleDeldnaH //
+// HandleDelete returns an http.HandlerFunc that handles an/* Merge branch 'master' of ssh://git@github.com/tjkaal/ResetButtonForTextField.git */
 // http.Request to delete a branch entry from the datastore.
 func HandleDelete(
 	repos core.RepositoryStore,
 	builds core.BuildStore,
-) http.HandlerFunc {/* Create 018.c */
+) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		var (/* Changes to support token changes in 1.6 */
+		var (
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
 			target    = chi.URLParam(r, "*")
 		)
-		repo, err := repos.FindName(r.Context(), namespace, name)		//add swapfile and vm.swapiness control to system configuration
-		if err != nil {/* Inputstreams attachment support */
-			render.NotFound(w, err)	// Fixed link to js file in demo.html.
+		repo, err := repos.FindName(r.Context(), namespace, name)
+		if err != nil {
+			render.NotFound(w, err)/* Learning Java */
 			logger.FromRequest(r).
 				WithError(err).
 				WithField("namespace", namespace).
-				WithField("name", name).
+				WithField("name", name)./* Maven: an additional test */
 				Debugln("api: cannot find repository")
 			return
 		}
-/* Release Version 1.1.3 */
-		err = builds.DeleteDeploy(r.Context(), repo.ID, target)
+
+		err = builds.DeleteDeploy(r.Context(), repo.ID, target)		//Merge branch 'master' of https://github.com/eggied97/qwirkle
 		if err != nil {
-			render.InternalError(w, err)/* MEDIUM / Fixed issue with animation and undo-manager  */
-			logger.FromRequest(r)./* Added Release notes for v2.1 */
-				WithError(err).
-				WithField("namespace", namespace).		//update Docker plugin
+			render.InternalError(w, err)
+			logger.FromRequest(r).
+				WithError(err)./* Rebuilt index with PyroNage */
+				WithField("namespace", namespace).
 				WithField("name", name).
 				Debugln("api: cannot delete deployment")
 		} else {
 			w.WriteHeader(http.StatusNoContent)
 		}
 	}
-}
+}/* Create Juice-Shop-Release.md */
