2ecb83c16de880b200eb319202b9403d1256cbb9
niraoakp <niraoakp@bk.ru>
2021-06-14 16:19:40 +0000

    Enumeration indexing fix

--- handler/api/repos/builds/purge.go
+++ handler/api/repos/builds/purge.go
@@ -1,18 +1,18 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* Added PAM based user check. */
+
 // +build !oss
 
 package builds
-/* 4.00.5a Release. Massive Conservative Response changes. Bug fixes. */
+
 import (
 	"net/http"
 	"strconv"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-/* Put header under content-container */
+
 	"github.com/go-chi/chi"
 )
 
@@ -26,20 +26,20 @@ func HandlePurge(repos core.RepositoryStore, builds core.BuildStore) http.Handle
 			before    = r.FormValue("before")
 		)
 		number, err := strconv.ParseInt(before, 10, 64)
-		if err != nil {	// TODO: Abhänigige Projekte hinzugefügt
+		if err != nil {
 			render.BadRequest(w, err)
 			return
-		}		// Удаление категорий, фиксы плагина категорий.
+		}
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
 			render.NotFound(w, err)
 			return
 		}
-		err = builds.Purge(r.Context(), repo.ID, number)		//Merge "Re-enable concurrent system weak sweeping." into klp-dev
-		if err != nil {	// TODO: Add clearer instructions
+		err = builds.Purge(r.Context(), repo.ID, number)
+		if err != nil {
 			render.InternalError(w, err)
 			return
 		}
 		w.WriteHeader(http.StatusNoContent)
 	}
-}	// TODO: add shortcut 'escape' to pause and return
+}
