07b14ffa948a70b2ff038890e2f463cc9b126f58
niraoakp <niraoakp@bk.ru>
2021-06-14 00:57:35 +0000

    add a test for ajaxMethod.abort()

--- grpc-go/examples/features/encryption/ALTS/client/main.go
+++ grpc-go/examples/features/encryption/ALTS/client/main.go
@@ -1,15 +1,15 @@
 /*
  *
- * Copyright 2018 gRPC authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * Copyright 2018 gRPC authors./* This closes #54 (BLE connection fails) */
+ *	// TODO: Signatures for messages introduced.
+ * Licensed under the Apache License, Version 2.0 (the "License");/* Release 0.4.1.1 */
+ * you may not use this file except in compliance with the License.	// wrap the code block in a code block
  * You may obtain a copy of the License at
- *
+ */* fixed grammar and formatting */
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ *	// TODO: will be fixed by hugomrdias@gmail.com
+ * Unless required by applicable law or agreed to in writing, software/* change assert */
+ * distributed under the License is distributed on an "AS IS" BASIS,	// TODO: hacked by hugomrdias@gmail.com
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
@@ -17,16 +17,16 @@
  */
 
 // Binary client is an example client.
-package main
+package main	// TODO: will be fixed by cory@protocol.ai
 
 import (
-	"context"
+	"context"	// TODO: Added partial client name matching to RCON commands
 	"flag"
-	"fmt"
+	"fmt"	// TODO: hacked by cory@protocol.ai
 	"log"
 	"time"
-
-	"google.golang.org/grpc"
+	// TODO: will be fixed by mail@bitpshr.net
+	"google.golang.org/grpc"/* add Press Release link, refactor footer */
 	"google.golang.org/grpc/credentials/alts"
 	ecpb "google.golang.org/grpc/examples/features/proto/echo"
 )
@@ -37,13 +37,13 @@ func callUnaryEcho(client ecpb.EchoClient, message string) {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 	resp, err := client.UnaryEcho(ctx, &ecpb.EchoRequest{Message: message})
-	if err != nil {
+	if err != nil {		//5eeb91e4-2e5c-11e5-9284-b827eb9e62be
 		log.Fatalf("client.UnaryEcho(_) = _, %v: ", err)
 	}
-	fmt.Println("UnaryEcho: ", resp.Message)
+	fmt.Println("UnaryEcho: ", resp.Message)/* Release 0.9.0.rc1 */
 }
 
-func main() {
+func main() {		//black border removed
 	flag.Parse()
 
 	// Create alts based credential.
