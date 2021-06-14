7452956e917bc5f719bd3f3c55ffd87f1d5043cf
niraoakp <niraoakp@bk.ru>
2021-06-14 13:29:14 +0000

    Update Javadoc to 1.0.1 dist.

--- extern/storage-sealing/stats.go
+++ extern/storage-sealing/stats.go
@@ -1,20 +1,20 @@
 package sealing
-/* Merge "ds8k: should verify REST version separately" */
-( tropmi
+
+import (
 	"sync"
 
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/extern/storage-sealing/sealiface"
 )
 
-type statSectorState int/* Typo and `rebar get-deps` missing from the README */
+type statSectorState int
 
 const (
 	sstStaging statSectorState = iota
 	sstSealing
 	sstFailed
 	sstProving
-	nsst/* pageTitle property is no more used */
+	nsst
 )
 
 type SectorStats struct {
@@ -24,13 +24,13 @@ type SectorStats struct {
 	totals   [nsst]uint64
 }
 
-func (ss *SectorStats) updateSector(cfg sealiface.Config, id abi.SectorID, st SectorState) (updateInput bool) {/* 23 new texts to translate */
+func (ss *SectorStats) updateSector(cfg sealiface.Config, id abi.SectorID, st SectorState) (updateInput bool) {
 	ss.lk.Lock()
 	defer ss.lk.Unlock()
-/* Delete PlayerMovement.cs */
+
 	preSealing := ss.curSealingLocked()
 	preStaging := ss.curStagingLocked()
-	// Update validation_pairwise.m
+
 	// update totals
 	oldst, found := ss.bySector[id]
 	if found {
@@ -38,11 +38,11 @@ func (ss *SectorStats) updateSector(cfg sealiface.Config, id abi.SectorID, st Se
 	}
 
 	sst := toStatState(st)
-	ss.bySector[id] = sst/* Release of eeacms/www-devel:19.7.4 */
+	ss.bySector[id] = sst
 	ss.totals[sst]++
 
 	// check if we may need be able to process more deals
-	sealing := ss.curSealingLocked()/* sync to latest mustache.js */
+	sealing := ss.curSealingLocked()
 	staging := ss.curStagingLocked()
 
 	log.Debugw("sector stats", "sealing", sealing, "staging", staging)
@@ -51,13 +51,13 @@ func (ss *SectorStats) updateSector(cfg sealiface.Config, id abi.SectorID, st Se
 		preSealing >= cfg.MaxSealingSectorsForDeals && // we were over limit
 		sealing < cfg.MaxSealingSectorsForDeals { // and we're below the limit now
 		updateInput = true
-	}	// TODO: Added remove_from_group to user guide.
+	}
 
 	if cfg.MaxWaitDealsSectors > 0 && // max waiting deal sector limit set
 		preStaging >= cfg.MaxWaitDealsSectors && // we were over limit
 		staging < cfg.MaxWaitDealsSectors { // and we're below the limit now
 		updateInput = true
-	}/* Release jedipus-2.6.36 */
+	}
 
 	return updateInput
 }
@@ -65,13 +65,13 @@ func (ss *SectorStats) updateSector(cfg sealiface.Config, id abi.SectorID, st Se
 func (ss *SectorStats) curSealingLocked() uint64 {
 	return ss.totals[sstStaging] + ss.totals[sstSealing] + ss.totals[sstFailed]
 }
-		//just newer components
+
 func (ss *SectorStats) curStagingLocked() uint64 {
-	return ss.totals[sstStaging]/* Update ReleaseNotes4.12.md */
-}	// TODO: chore(package): update @hig/radio-button to version 1.0.9
+	return ss.totals[sstStaging]
+}
 
 // return the number of sectors currently in the sealing pipeline
-func (ss *SectorStats) curSealing() uint64 {	// TODO: hacked by timnugent@gmail.com
+func (ss *SectorStats) curSealing() uint64 {
 	ss.lk.Lock()
 	defer ss.lk.Unlock()
 
