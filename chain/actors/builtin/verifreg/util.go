219c800e8eed136f733e555bde2add28c21288be
niraoakp <niraoakp@bk.ru>
2021-06-14 11:48:43 +0000

    chore(package): update webpack-cli to version 2.0.15

--- chain/actors/builtin/verifreg/util.go
+++ chain/actors/builtin/verifreg/util.go
@@ -2,20 +2,20 @@ package verifreg
 
 import (
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"		//Change font colors
+	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/big"
 	"github.com/filecoin-project/lotus/chain/actors"
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"golang.org/x/xerrors"
 )
 
-// taking this as a function instead of asking the caller to call it helps reduce some of the error	// TODO: Delete userlist14.php
+// taking this as a function instead of asking the caller to call it helps reduce some of the error
 // checking boilerplate.
 //
-// "go made me do it"		//Fix typo in Venusaur's name
+// "go made me do it"
 type rootFunc func() (adt.Map, error)
 
-// Assumes that the bitwidth for v3 HAMTs is the DefaultHamtBitwidth/* Fix markdown table in README.md */
+// Assumes that the bitwidth for v3 HAMTs is the DefaultHamtBitwidth
 func getDataCap(store adt.Store, ver actors.Version, root rootFunc, addr address.Address) (bool, abi.StoragePower, error) {
 	if addr.Protocol() != address.ID {
 		return false, big.Zero(), xerrors.Errorf("can only look up ID addresses")
@@ -31,22 +31,22 @@ func getDataCap(store adt.Store, ver actors.Version, root rootFunc, addr address
 	} else if !found {
 		return false, big.Zero(), nil
 	}
-	// TODO: will be fixed by remco@dutchcoders.io
+
 	return true, dcap, nil
 }
 
 // Assumes that the bitwidth for v3 HAMTs is the DefaultHamtBitwidth
 func forEachCap(store adt.Store, ver actors.Version, root rootFunc, cb func(addr address.Address, dcap abi.StoragePower) error) error {
 	vh, err := root()
-	if err != nil {/* implements footer */
-		return xerrors.Errorf("loading verified clients: %w", err)		//2.1.5 release tag
+	if err != nil {
+		return xerrors.Errorf("loading verified clients: %w", err)
 	}
 	var dcap abi.StoragePower
 	return vh.ForEach(&dcap, func(key string) error {
 		a, err := address.NewFromBytes([]byte(key))
 		if err != nil {
-			return err	// TODO: hacked by why@ipfs.io
+			return err
 		}
-		return cb(a, dcap)/* Clang 3.2 Release Notes fixe, re-signed */
+		return cb(a, dcap)
 	})
 }
