303c9161e8d0e53f13413e7eecab2f1b6304ca1b
niraoakp <niraoakp@bk.ru>
2021-06-14 21:02:10 +0000

    Readme from the old repo + license

--- plugin/registry/file_oss.go
+++ plugin/registry/file_oss.go
@@ -1,24 +1,24 @@
-// Copyright 2019 Drone IO, Inc.		//Update the License to proper MIT
+// Copyright 2019 Drone IO, Inc.		//correct slovak-tts voice(make UT)
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Mac Release: package SDL framework inside the app bundle. */
-//
+// You may obtain a copy of the License at/* Better documentation of what's in here */
+///* chore: Add code of conduct. */
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* Add Vim Adventures to free-programming-interactive */
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+//
+// Unless required by applicable law or agreed to in writing, software/* Adds the ViewFitSelection to the main toolbar (improves usability) */
+// distributed under the License is distributed on an "AS IS" BASIS,		//Hoisted local_file_queue creation out of Readdir loop.
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build oss		//Add function to check account permission
+// +build oss
 
-package registry/* Release areca-7.3.2 */
-
+package registry/* Delete object_script.ghostwriter.Release */
+/* Merge branch 'master' into py39 */
 import "github.com/drone/drone/core"
 
-// FileSource returns a no-op registry credential provider.	// TODO: hacked by sbrichards@gmail.com
-func FileSource(string) core.RegistryService {
+// FileSource returns a no-op registry credential provider.
+func FileSource(string) core.RegistryService {		//a1743b4e-306c-11e5-9929-64700227155b
 	return new(noop)
 }
