437457c4ed451480f4e604760db1a36aecd11aee
niraoakp <niraoakp@bk.ru>
2021-06-14 20:01:03 +0000

    Bug 1161: changed error code in test dir

--- tests/integration/dependency_steps/step2/index.ts
+++ tests/integration/dependency_steps/step2/index.ts
@@ -1,9 +1,9 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Added delete and deleteAll for identity service */
-
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+/* Create Op-Manager Releases */
 import { Resource } from "./resource";
 
 // Step 2: Reverse the dependency graph.
 // Checkpoint dependency graph: b -> a
 const b = new Resource("b", { state: 2 })
 const a = new Resource("a", { state: 1, resource: b })
-// Dependency graph: a -> b
+// Dependency graph: a -> b/* Merge "usb: dwc3: gadget: Increase the link state change timeout value" */
