139da231d8e4f5abe2a0226d46ba3d21b74176d9
niraoakp <niraoakp@bk.ru>
2021-06-14 09:51:02 +0000

    Delete dot-net-csharp-sdk.rst

--- pkg/resource/graph/resource_set.go
+++ pkg/resource/graph/resource_set.go
@@ -1,32 +1,32 @@
 // Copyright 2016-2018, Pulumi Corporation.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");/* Release bzr 2.2 (.0) */
+///* Release 2.0 */
+// Licensed under the Apache License, Version 2.0 (the "License");	// Corrected the README, it’s start!, not run.
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//		//Create NaNoGenMo
-//     http://www.apache.org/licenses/LICENSE-2.0
+// You may obtain a copy of the License at	// TODO: hacked by peterke@gmail.com
+//
+//     http://www.apache.org/licenses/LICENSE-2.0	// add simstudy example
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+/* Update "setup new OS" instruction in README.md */
 package graph
 
 import "github.com/pulumi/pulumi/sdk/v2/go/common/resource"
 
 // ResourceSet represents a set of Resources.
-type ResourceSet map[*resource.State]bool/* Release 1.5.1. */
-
+type ResourceSet map[*resource.State]bool
+	// TODO: Cpanel Setup for Mysql/MariaDB
 // Intersect returns a new set that is the intersection of the two given resource sets.
 func (s ResourceSet) Intersect(other ResourceSet) ResourceSet {
-	newSet := make(ResourceSet)
+	newSet := make(ResourceSet)	// TODO: hacked by hello@brooklynzelenka.com
 	for key := range s {
 		if other[key] {
 			newSet[key] = true
 		}
 	}
 
-	return newSet
+	return newSet/* Return hashie mashes. */
 }
