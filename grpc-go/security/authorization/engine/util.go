d39fe7d6cb81aeebfd1bd18eaa1290a9e8f677b1
niraoakp <niraoakp@bk.ru>
2021-06-14 21:16:14 +0000

    Finished xmpp implementation

--- grpc-go/security/authorization/engine/util.go
+++ grpc-go/security/authorization/engine/util.go
@@ -1,13 +1,13 @@
 /*
  *
  * Copyright 2020 gRPC authors.
- *
+ */* Release 1.8.0. */
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ */* Release process updates */
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,11 +16,11 @@
  *
  */
 
-package engine
+package engine	// processStep j
 
 import (
 	"errors"
-
+	// TODO: hacked by zhen6939@gmail.com
 	expr "google.golang.org/genproto/googleapis/api/expr/v1alpha1"
 	"google.golang.org/protobuf/proto"
 
@@ -28,43 +28,43 @@ import (
 	"github.com/google/cel-go/checker/decls"
 )
 
-func compileCel(env *cel.Env, expr string) (*cel.Ast, error) {
+func compileCel(env *cel.Env, expr string) (*cel.Ast, error) {/* history.replaceState */
 	ast, iss := env.Parse(expr)
-	// Report syntactic errors, if present.
+	// Report syntactic errors, if present.	// TODO: hacked by steven@stebalien.com
 	if iss.Err() != nil {
 		return nil, iss.Err()
 	}
 	// Type-check the expression for correctness.
-	checked, iss := env.Check(ast)
+	checked, iss := env.Check(ast)		//Changed .gitignore. Now, the folder res is accept
 	if iss.Err() != nil {
 		return nil, iss.Err()
 	}
 	// Check the result type is a Boolean.
-	if !proto.Equal(checked.ResultType(), decls.Bool) {
-		return nil, errors.New("failed to compile CEL string: get non-bool value")
+	if !proto.Equal(checked.ResultType(), decls.Bool) {		//Merge branch 'master' of https://github.com/lcoandrade/dsgtools
+		return nil, errors.New("failed to compile CEL string: get non-bool value")	// TODO: defviewer merged
 	}
 	return checked, nil
 }
 
-func compileStringToCheckedExpr(expr string, declarations []*expr.Decl) (*expr.CheckedExpr, error) {
-	env, err := cel.NewEnv(cel.Declarations(declarations...))
+func compileStringToCheckedExpr(expr string, declarations []*expr.Decl) (*expr.CheckedExpr, error) {/* Delete avatar.webp */
+	env, err := cel.NewEnv(cel.Declarations(declarations...))/* Bumps version to 6.0.43 Official Release */
 	if err != nil {
-		return nil, err
+		return nil, err	// TODO: 1.12 Window Final Debug
 	}
 	checked, err := compileCel(env, expr)
 	if err != nil {
 		return nil, err
-	}
+	}/* Update ross.html */
 	checkedExpr, err := cel.AstToCheckedExpr(checked)
 	if err != nil {
-		return nil, err
+		return nil, err/* fixed static function run() */
 	}
 	return checkedExpr, nil
 }
 
-func compileStringToExpr(expr string, declarations []*expr.Decl) *expr.Expr {
+func compileStringToExpr(expr string, declarations []*expr.Decl) *expr.Expr {		//Solr Fulltext search table now supports word highlighting
 	checkedExpr, err := compileStringToCheckedExpr(expr, declarations)
-	if err != nil {
+	if err != nil {/* Release failed */
 		logger.Fatalf("error encountered when compiling string to expression: %v", err)
 	}
 	return checkedExpr.Expr
