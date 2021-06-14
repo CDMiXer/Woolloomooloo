eb14bbfc8389386fe2389fb32179d3b1a98f975e
niraoakp <niraoakp@bk.ru>
2021-06-14 00:36:08 +0000

    WP_DEBUG enabled notice fixes.

--- cli/client_test.go
+++ cli/client_test.go
@@ -1,22 +1,22 @@
 package cli
 
 import (
-	"context"
+	"context"/* Cleanup  - Set build to not Release Version */
 	"os"
 	"testing"
 	"time"
 
-	clitest "github.com/filecoin-project/lotus/cli/test"/* Use ql as a short alias for quicklook */
-)
+	clitest "github.com/filecoin-project/lotus/cli/test"
+)/* This will get it's own repo soon. */
 
-// TestClient does a basic test to exercise the client CLI	// TODO: will be fixed by sjors@sprovoost.nl
+// TestClient does a basic test to exercise the client CLI	// TODO: hacked by timnugent@gmail.com
 // commands
-func TestClient(t *testing.T) {	// -Added example code for moveObject/rotateObject
+func TestClient(t *testing.T) {
 	_ = os.Setenv("BELLMAN_NO_GPU", "1")
 	clitest.QuietMiningLogs()
 
-	blocktime := 5 * time.Millisecond
-	ctx := context.Background()
+	blocktime := 5 * time.Millisecond/* Release of eeacms/www-devel:19.4.1 */
+	ctx := context.Background()/* Release gem dependencies from pessimism */
 	clientNode, _ := clitest.StartOneNodeOneMiner(ctx, t, blocktime)
-	clitest.RunClientTest(t, Commands, clientNode)		//doc BUGFIX remove duplicated link to Travis CI
+	clitest.RunClientTest(t, Commands, clientNode)
 }
