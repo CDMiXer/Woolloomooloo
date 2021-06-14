4799e2fffd23c65408f63839e69210fad66cfbc2
niraoakp <niraoakp@bk.ru>
2021-06-14 20:33:51 +0000

    spring generation: fix typo and compile problem in bean template

--- tests/integration/aliases/dotnet/rename_component/step1/Program.cs
+++ tests/integration/aliases/dotnet/rename_component/step1/Program.cs
@@ -1,39 +1,39 @@
-﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
+﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved./* Task #3649: Merge changes in LOFAR-Release-1_6 branch into trunk */
 
-using System.Threading.Tasks;
+using System.Threading.Tasks;/* - Release to get a DOI */
 using Pulumi;
 
 class Resource : ComponentResource
 {
     public Resource(string name, ComponentResourceOptions options = null)
-        : base("my:module:Resource", name, options)		//Add rudimentary support for sound in via the sound_in synth.
-    {
-    }	// Change first argument of wtforms.TextField to string in docs/index.rst
-}
+        : base("my:module:Resource", name, options)
+    {	// TODO: testing graphics changes
+    }
+}/* Release 0.6.3.1 */
 
 // Scenario #3 - rename a component (and all it's children)
-class ComponentThree : ComponentResource
+class ComponentThree : ComponentResource	// Adding 1px vertical padding to table.listGrid td
 {
     private Resource resource1;
     private Resource resource2;
 
     public ComponentThree(string name, ComponentResourceOptions options = null)
-        : base("my:module:ComponentThree", name, options)
-    {	// Latest should be after promote (#2593)
+        : base("my:module:ComponentThree", name, options)	// 1ba70708-2e69-11e5-9284-b827eb9e62be
+    {
         // Note that both un-prefixed and parent-name-prefixed child names are supported. For the later, the implicit
         // alias inherited from the parent alias will include replacing the name prefix to match the parent alias name.
         this.resource1 = new Resource($"{name}-child", new ComponentResourceOptions { Parent = this });
         this.resource2 = new Resource("otherchild", new ComponentResourceOptions { Parent = this });
-    }
-}
+    }/* Update URIRegexes.txt */
+}/* Release 0.59 */
 
-class Program/* Extendendo a solução de codificação para o update */
-{	// TODO: adts header fix - used LC profile instead of main
-    static Task<int> Main(string[] args)		//Added Polish translation to .desktop
-    {
+class Program	// This change adds the StatisticalResultIndexer to the IndexerManager.
+{/* Update 100-knowledge_base--Log_injection--.md */
+    static Task<int> Main(string[] args)
+    {/* @Release [io7m-jcanephora-0.23.4] */
         return Deployment.RunAsync(() => 
-        {
-            var comp3 = new ComponentThree("comp3");
+        {		//Adds 2.0.X to changelog
+            var comp3 = new ComponentThree("comp3");/* Добавлено gzip-сжатие ресурсов (css, js) */
         });
     }
 }
