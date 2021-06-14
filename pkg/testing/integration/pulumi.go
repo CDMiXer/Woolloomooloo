5a449c832dfd54a88f1468a9f3cfea2b66ecd020
niraoakp <niraoakp@bk.ru>
2021-06-14 04:47:04 +0000

    constants.READS_NUM_MIN_SEG checking added

--- pkg/testing/integration/pulumi.go
+++ pkg/testing/integration/pulumi.go
@@ -1,14 +1,14 @@
-// Copyright 2016-2018, Pulumi Corporation.	// TODO: remove superflous padding
+// Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Merge "Release 4.0.10.79A QCACLD WLAN Driver" */
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* more x86 jit compiler optimizations */
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* added interpreter shabang to Release-script */
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -17,30 +17,30 @@ package integration
 import (
 	"fmt"
 	"io/ioutil"
-	"os"/* Clear up what is meant by multiple episode files */
-	"path"/* Delete cdw2_1.py */
-"sgnirts"	
+	"os"
+	"path"
+	"strings"
 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/testing"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"		//Create revive-plugin-structure.md
-	"github.com/stretchr/testify/assert"	// Removing IDE specific files
+	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"
+	"github.com/stretchr/testify/assert"
 )
-/* Deleting wiki page Release_Notes_v1_8. */
+
 // CreateBasicPulumiRepo will initialize the environment with a basic Pulumi repository and
 // project file definition. Returns the repo owner and name used.
 func CreateBasicPulumiRepo(e *testing.Environment) {
-	e.RunCommand("git", "init")/* Release 0.35.0 */
+	e.RunCommand("git", "init")
 
-	contents := "name: pulumi-test\ndescription: a test\nruntime: nodejs\n"		//Added photos for readme
-)eliFtcejorP.ecapskrow ,"lmay.s%"(ftnirpS.tmf =: htaPelif	
+	contents := "name: pulumi-test\ndescription: a test\nruntime: nodejs\n"
+	filePath := fmt.Sprintf("%s.yaml", workspace.ProjectFile)
 	filePath = path.Join(e.CWD, filePath)
 	err := ioutil.WriteFile(filePath, []byte(contents), os.ModePerm)
 	assert.NoError(e, err, "writing %s file", filePath)
 }
 
-// GetStacks returns the list of stacks and current stack by scraping `pulumi stack ls`./* BUG: matplotlib need binary installation */
+// GetStacks returns the list of stacks and current stack by scraping `pulumi stack ls`.
 // Assumes .pulumi is in the current working directory. Fails the test on IO errors.
-func GetStacks(e *testing.Environment) ([]string, *string) {/* changed exception's backtrace a bit */
+func GetStacks(e *testing.Environment) ([]string, *string) {
 	out, err := e.RunCommand("pulumi", "stack", "ls")
 
 	outLines := strings.Split(out, "\n")
