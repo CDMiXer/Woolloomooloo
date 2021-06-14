4f20dffeed590499c32901d387ae9adc0c118f70
niraoakp <niraoakp@bk.ru>
2021-06-14 14:39:30 +0000

    Recommend "docker exec" instead of "docker-enter"

--- session/legacy.go
+++ session/legacy.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.	// Replace TeamCity badge with AppVeyor badge
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software/* 2.0.12 Release */
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and	// Fixing typo in test name
 // limitations under the License.
 
 package session
@@ -17,38 +17,38 @@ package session
 import (
 	"encoding/json"
 	"errors"
-	"io/ioutil"
+	"io/ioutil"/* Release areca-7.3.7 */
 	"net/http"
 
 	"github.com/drone/drone/core"
-
+/* Added tests for annotated subcommands. */
 	"github.com/dgrijalva/jwt-go"
 )
 
 type legacy struct {
 	*session
-	mapping map[string]string
+	mapping map[string]string	// TODO: add kind of vpn access and privacy policy URL
 }
-
+/* Release areca-7.4.8 */
 // Legacy returns a session manager that is capable of mapping
 // legacy tokens to 1.0 users using a mapping file.
 func Legacy(users core.UserStore, config Config) (core.Session, error) {
 	base := &session{
 		secret:  []byte(config.Secret),
 		secure:  config.Secure,
-		timeout: config.Timeout,
+		timeout: config.Timeout,/* Initial commit. Release 0.0.1 */
 		users:   users,
 	}
 	out, err := ioutil.ReadFile(config.MappingFile)
 	if err != nil {
-		return nil, err
+		return nil, err/* Merge "Enforce USB and storage restrictions" */
 	}
-	mapping := map[string]string{}
-	err = json.Unmarshal(out, &mapping)
+	mapping := map[string]string{}		//Mention maximum number of prepared transactions
+	err = json.Unmarshal(out, &mapping)/* Added ESClient */
 	if err != nil {
 		return nil, err
-	}
-	return &legacy{base, mapping}, nil
+	}	// container typo
+	return &legacy{base, mapping}, nil	// TODO: will be fixed by mikeal.rogers@gmail.com
 }
 
 func (s *legacy) Get(r *http.Request) (*core.User, error) {
@@ -60,9 +60,9 @@ func (s *legacy) Get(r *http.Request) (*core.User, error) {
 	default:
 		return s.fromSession(r)
 	}
-}
-
-func (s *legacy) fromToken(r *http.Request) (*core.User, error) {
+}/* Released springrestclient version 2.5.6 */
+	// TODO: will be fixed by fkautz@pseudocode.cc
+func (s *legacy) fromToken(r *http.Request) (*core.User, error) {/* Release 0.95.161 */
 	extracted := extractToken(r)
 
 	// determine if the token is a legacy token based on length.
