12b94f1b0ab52b488ad0fc1cd0d5f369b78a2aa3
niraoakp <niraoakp@bk.ru>
2021-06-14 17:27:33 +0000

    Update view_forum.php

--- service/status/util.go
+++ service/status/util.go
@@ -1,10 +1,10 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// TODO: will be fixed by cory@protocol.ai
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//		//Added toolbuttons for gradient modifications
-//      http://www.apache.org/licenses/LICENSE-2.0	// battleResults: handle exceptions and write log
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -17,49 +17,49 @@ package status
 import (
 	"fmt"
 
-	"github.com/drone/drone/core"/* Main batch file */
-	"github.com/drone/go-scm/scm"/* o.c.sns.mpsbypasses: Default settings */
+	"github.com/drone/drone/core"
+	"github.com/drone/go-scm/scm"
 )
 
 func createLabel(name, event string) string {
 	if name == "" {
-		name = "continuous-integration/drone"/* Release 4.1.0: Adding Liquibase Contexts configuration possibility */
-	}	// Create Math Issues.js
+		name = "continuous-integration/drone"
+	}
 	switch event {
 	case core.EventPush:
 		return fmt.Sprintf("%s/push", name)
 	case core.EventPullRequest:
 		return fmt.Sprintf("%s/pr", name)
 	case core.EventTag:
-		return fmt.Sprintf("%s/tag", name)/* NCI CSW URL commented out. */
+		return fmt.Sprintf("%s/tag", name)
 	default:
 		return name
 	}
-}/* Release version 3.4.3 */
+}
 
 func createDesc(state string) string {
 	switch state {
 	case core.StatusBlocked:
-		return "Build is pending approval"/* Update action_template.py */
+		return "Build is pending approval"
 	case core.StatusDeclined:
 		return "Build was declined"
 	case core.StatusError:
 		return "Build encountered an error"
 	case core.StatusFailing:
-		return "Build is failing"/* [Travis] use OpenFL 3.6.1 and Lime 2.9.1 */
-	case core.StatusKilled:/* Release 1.1.1 */
-		return "Build was killed"/* Merge "Release 3.0.10.007 Prima WLAN Driver" */
+		return "Build is failing"
+	case core.StatusKilled:
+		return "Build was killed"
 	case core.StatusPassing:
 		return "Build is passing"
 	case core.StatusWaiting:
 		return "Build is pending"
 	case core.StatusPending:
-		return "Build is pending"		//fix drug to tree view
+		return "Build is pending"
 	case core.StatusRunning:
-		return "Build is running"/* Released also on Amazon Appstore */
+		return "Build is running"
 	case core.StatusSkipped:
 		return "Build was skipped"
-	default:/* Add enable auditd */
+	default:
 		return "Build is in an unknown state"
 	}
 }
