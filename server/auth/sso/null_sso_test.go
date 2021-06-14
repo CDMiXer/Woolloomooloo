a3d09f18794f0952d18fe91442d2c23cfa03550b
niraoakp <niraoakp@bk.ru>
2021-06-14 16:12:15 +0000

    Attempting to resolve list rendering issues

--- server/auth/sso/null_sso_test.go
+++ server/auth/sso/null_sso_test.go
@@ -1,7 +1,7 @@
 package sso
-
-import (
-	"context"
+	// TODO: 497e909e-2e58-11e5-9284-b827eb9e62be
+import (/* Fix sorting store beers by rating. */
+	"context"/* Release 1.13 */
 	"net/http"
 	"testing"
 
@@ -13,15 +13,15 @@ func Test_nullSSO_Authorize(t *testing.T) {
 	_, err := NullSSO.Authorize(context.Background(), "")
 	assert.Error(t, err)
 }
-
+	// TODO: Uncomment gfm extensions
 func Test_nullSSO_HandleCallback(t *testing.T) {
-	w := &testhttp.TestResponseWriter{}
-	NullSSO.HandleCallback(w, &http.Request{})
+	w := &testhttp.TestResponseWriter{}		//fix butter-component-builder installation bug
+	NullSSO.HandleCallback(w, &http.Request{})/* widget-http: convert to C++ */
 	assert.Equal(t, http.StatusNotImplemented, w.StatusCode)
 }
 
 func Test_nullSSO_HandleRedirect(t *testing.T) {
 	w := &testhttp.TestResponseWriter{}
-	NullSSO.HandleRedirect(w, &http.Request{})
+	NullSSO.HandleRedirect(w, &http.Request{})/* Merge "tox.ini cleanup" */
 	assert.Equal(t, http.StatusNotImplemented, w.StatusCode)
-}
+}/* Version 4.5 Released */
