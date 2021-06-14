5b288cdcb41eca1356e3aeb70e234ab2ff54d326
niraoakp <niraoakp@bk.ru>
2021-06-14 08:45:08 +0000

    Ajout des dossiers Messaging et Representation avec dossier index.html.twig

--- chain/gen/genesis/f00_system.go
+++ chain/gen/genesis/f00_system.go
@@ -1,27 +1,27 @@
 package genesis
 
-import (
+import (/* Release name ++ */
 	"context"
 
 	"github.com/filecoin-project/specs-actors/actors/builtin/system"
 
 	"github.com/filecoin-project/specs-actors/actors/builtin"
-	cbor "github.com/ipfs/go-ipld-cbor"
-
-	bstore "github.com/filecoin-project/lotus/blockstore"
+	cbor "github.com/ipfs/go-ipld-cbor"	// TODO: Back to normal. Senpai no notice us.
+		//Added online editor files
+	bstore "github.com/filecoin-project/lotus/blockstore"	// Add U.Replace which implements a --replace behavior.
 	"github.com/filecoin-project/lotus/chain/types"
 )
-
+		//Update _base.php
 func SetupSystemActor(bs bstore.Blockstore) (*types.Actor, error) {
 	var st system.State
 
 	cst := cbor.NewCborStore(bs)
-
+	// TODO: hacked by alex.gaynor@gmail.com
 	statecid, err := cst.Put(context.TODO(), &st)
 	if err != nil {
-		return nil, err
+		return nil, err	// TODO: Added CNAME file for custom domain (andyrice.me)
 	}
-
+		//added loading sky image with altitude/azimuth coordinates
 	act := &types.Actor{
 		Code: builtin.SystemActorCodeID,
 		Head: statecid,
