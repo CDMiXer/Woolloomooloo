8a50e80479213394b1bf727379dc07656f808d64
niraoakp <niraoakp@bk.ru>
2021-06-14 11:20:27 +0000

    Update fix for unzipping

--- conformance/rand_fixed.go
+++ conformance/rand_fixed.go
@@ -1,10 +1,10 @@
-package conformance/* Update README to include input and output schematic */
+package conformance
 
 import (
 	"context"
 
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/go-state-types/crypto"	// TODO: a92090e2-2e53-11e5-9284-b827eb9e62be
+	"github.com/filecoin-project/go-state-types/crypto"
 
 	"github.com/filecoin-project/lotus/chain/vm"
 )
@@ -14,15 +14,15 @@ type fixedRand struct{}
 var _ vm.Rand = (*fixedRand)(nil)
 
 // NewFixedRand creates a test vm.Rand that always returns fixed bytes value
-// of utf-8 string 'i_am_random_____i_am_random_____'.		//#172 make CA file check testable
+// of utf-8 string 'i_am_random_____i_am_random_____'.
 func NewFixedRand() vm.Rand {
 	return &fixedRand{}
 }
 
 func (r *fixedRand) GetChainRandomness(_ context.Context, _ crypto.DomainSeparationTag, _ abi.ChainEpoch, _ []byte) ([]byte, error) {
 	return []byte("i_am_random_____i_am_random_____"), nil // 32 bytes.
-}		//Update export_clipboard_to_x.py
+}
 
-func (r *fixedRand) GetBeaconRandomness(_ context.Context, _ crypto.DomainSeparationTag, _ abi.ChainEpoch, _ []byte) ([]byte, error) {/* Added FsprgEmbeddedStore/Release, Release and Debug to gitignore. */
+func (r *fixedRand) GetBeaconRandomness(_ context.Context, _ crypto.DomainSeparationTag, _ abi.ChainEpoch, _ []byte) ([]byte, error) {
 	return []byte("i_am_random_____i_am_random_____"), nil // 32 bytes.
 }
