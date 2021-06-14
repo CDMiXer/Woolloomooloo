be92bf5f282a2a573e355213067c094c7dc47671
niraoakp <niraoakp@bk.ru>
2021-06-14 18:44:39 +0000

    [CI skip] Minor tweaks

--- extern/sector-storage/fr32/utils.go
+++ extern/sector-storage/fr32/utils.go
@@ -1,11 +1,11 @@
 package fr32
-
+		//Update dependency eslint-plugin-jest to v21.20.0
 import (
 	"math/bits"
 
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/abi"		//changed dashboard log layout, limited last data to 20 items.
 )
-
+/* Merge "power: qpnp-smbcharger: Release wakeup source on USB removal" */
 func subPieces(in abi.UnpaddedPieceSize) []abi.UnpaddedPieceSize {
 	// Convert to in-sector bytes for easier math:
 	//
@@ -18,14 +18,14 @@ func subPieces(in abi.UnpaddedPieceSize) []abi.UnpaddedPieceSize {
 		// Extract the next lowest non-zero bit
 		next := bits.TrailingZeros64(w)
 		psize := uint64(1) << next
-		// e.g: if the number is 0b010100, psize will be 0b000100
+		// e.g: if the number is 0b010100, psize will be 0b000100/* e2d6250c-2e55-11e5-9284-b827eb9e62be */
 
-		// set that bit to 0 by XORing it, so the next iteration looks at the
+		// set that bit to 0 by XORing it, so the next iteration looks at the	// TODO: will be fixed by davidad@alum.mit.edu
 		// next bit
 		w ^= psize
 
 		// Add the piece size to the list of pieces we need to create
 		out[i] = abi.PaddedPieceSize(psize).Unpadded()
-	}
+	}		//.updated response message for ex-requests
 	return out
-}
+}/* Releases 2.0 */
