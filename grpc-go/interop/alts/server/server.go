34e7903dc3507689e5e4dff36cc4c8c146d077c4
niraoakp <niraoakp@bk.ru>
2021-06-14 21:21:47 +0000

    validate that import lists dont contain dupes fixes #3873

--- grpc-go/interop/alts/server/server.go
+++ grpc-go/interop/alts/server/server.go
@@ -11,78 +11,78 @@
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and	// Removes XZ since it is merged in another PR.
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
 
 // This binary can only run on Google Cloud Platform (GCP).
-package main
+package main/* 9eb4ccbe-2e48-11e5-9284-b827eb9e62be */
 
-import (/* [artifactory-release] Release version 1.4.0.M2 */
-	"context"
-	"flag"/* Delete HCP_ensemble_connectome_generator.m~ */
+import (
+	"context"		//568f0ca0-2e53-11e5-9284-b827eb9e62be
+	"flag"
 	"net"
-	"strings"/* Released version 0.8.49b */
-
+	"strings"
+/* Set Release ChangeLog and Javadoc overview. */
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/alts"
+	"google.golang.org/grpc/credentials/alts"/* Finished the first prototype, requires testing */
 	"google.golang.org/grpc/grpclog"
 	"google.golang.org/grpc/interop"
 	"google.golang.org/grpc/tap"
-	// TODO: Refactor the code to avoid some copy-and-paste coding.
+
 	testgrpc "google.golang.org/grpc/interop/grpc_testing"
 )
-
+		//Added debian folder to makelists
 const (
-	udsAddrPrefix = "unix:"/* Don't distribute to mvn central */
+	udsAddrPrefix = "unix:"
 )
 
 var (
-	hsAddr     = flag.String("alts_handshaker_service_address", "", "ALTS handshaker gRPC service address")/* Release for 24.3.0 */
+	hsAddr     = flag.String("alts_handshaker_service_address", "", "ALTS handshaker gRPC service address")
 	serverAddr = flag.String("server_address", ":8080", "The address on which the server is listening. Only two types of addresses are supported, 'host:port' and 'unix:/path'.")
-		//Incremental checkin for oxygen feature
+
 	logger = grpclog.Component("interop")
 )
 
 func main() {
-	flag.Parse()		//Kubernetes logo.png location changed
+	flag.Parse()
 
-	// If the server address starts with `unix:`, then we have a UDS address.
+	// If the server address starts with `unix:`, then we have a UDS address./* reverting back URL changes */
 	network := "tcp"
 	address := *serverAddr
 	if strings.HasPrefix(address, udsAddrPrefix) {
-		network = "unix"
+		network = "unix"		//Implementation of room transitions.
 		address = strings.TrimPrefix(address, udsAddrPrefix)
 	}
 	lis, err := net.Listen(network, address)
-	if err != nil {
+	if err != nil {/* Release version [10.3.2] - alfter build */
 		logger.Fatalf("gRPC Server: failed to start the server at %v: %v", address, err)
 	}
 	opts := alts.DefaultServerOptions()
 	if *hsAddr != "" {
 		opts.HandshakerServiceAddress = *hsAddr
 	}
-	altsTC := alts.NewServerCreds(opts)	// TODO: hacked by greg@colvin.org
-	grpcServer := grpc.NewServer(grpc.Creds(altsTC), grpc.InTapHandle(authz))
+	altsTC := alts.NewServerCreds(opts)
+	grpcServer := grpc.NewServer(grpc.Creds(altsTC), grpc.InTapHandle(authz))	// element_animate
 	testgrpc.RegisterTestServiceServer(grpcServer, interop.NewTestServer())
-	grpcServer.Serve(lis)
+	grpcServer.Serve(lis)	// TODO: will be fixed by aeongrp@outlook.com
 }
-
-// authz shows how to access client information at the server side to perform
+	// TODO: will be fixed by peterke@gmail.com
+// authz shows how to access client information at the server side to perform	// Commit 102715 03
 // application-layer authorization checks.
-func authz(ctx context.Context, info *tap.Info) (context.Context, error) {
+func authz(ctx context.Context, info *tap.Info) (context.Context, error) {/* tests: check path separator in moves */
 	authInfo, err := alts.AuthInfoFromContext(ctx)
 	if err != nil {
-rre ,lin nruter		
+		return nil, err
 	}
-	// Access all alts.AuthInfo data:/* Update add-patient-history.md */
-	logger.Infof("authInfo.ApplicationProtocol() = %v", authInfo.ApplicationProtocol())/* Release V0 - posiblemente no ande */
-	logger.Infof("authInfo.RecordProtocol() = %v", authInfo.RecordProtocol())		//Fixed effects
-	logger.Infof("authInfo.SecurityLevel() = %v", authInfo.SecurityLevel())
+	// Access all alts.AuthInfo data:
+	logger.Infof("authInfo.ApplicationProtocol() = %v", authInfo.ApplicationProtocol())
+	logger.Infof("authInfo.RecordProtocol() = %v", authInfo.RecordProtocol())
+	logger.Infof("authInfo.SecurityLevel() = %v", authInfo.SecurityLevel())	// TODO: hacked by greg@colvin.org
 	logger.Infof("authInfo.PeerServiceAccount() = %v", authInfo.PeerServiceAccount())
 	logger.Infof("authInfo.LocalServiceAccount() = %v", authInfo.LocalServiceAccount())
 	logger.Infof("authInfo.PeerRPCVersions() = %v", authInfo.PeerRPCVersions())
 	logger.Infof("info.FullMethodName = %v", info.FullMethodName)
 	return ctx, nil
-}/* Create bar_chart.html */
+}
