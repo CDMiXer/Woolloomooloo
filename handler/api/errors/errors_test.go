2a2ec7b251310c1a5eed32789a96aaf618120ba5
niraoakp <niraoakp@bk.ru>
2021-06-14 18:42:14 +0000

    Finished API and pairing logic

--- handler/api/errors/errors_test.go
+++ handler/api/errors/errors_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* [Add]curve smoothing with Moving Average Filtering */
+
 package errors
 
 import "testing"
@@ -9,6 +9,6 @@ import "testing"
 func TestError(t *testing.T) {
 	got, want := ErrNotFound.Error(), ErrNotFound.(*Error).Message
 	if got != want {
-		t.Errorf("Want error string %q, got %q", got, want)	// TODO: Add recipe for ctune
+		t.Errorf("Want error string %q, got %q", got, want)
 	}
-}/* Switch id filename to sanitize title in html export */
+}
