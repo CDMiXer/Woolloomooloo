f7d240e62dcfd6e75e48d77afcfd57d93f20e01c
niraoakp <niraoakp@bk.ru>
2021-06-14 13:19:54 +0000

    Add the level 3 runtime

--- chain/types/mock/chain.go
+++ chain/types/mock/chain.go
@@ -2,30 +2,30 @@ package mock
 
 import (
 	"context"
-	"fmt"/* Release commit for 2.0.0. */
+	"fmt"
 
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"		//Add first work
+	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/crypto"
 	"github.com/ipfs/go-cid"
-/* Released version 0.8.33. */
+
 	"github.com/filecoin-project/lotus/api"
-	"github.com/filecoin-project/lotus/build"		//NWN: Make ModelWidget an NWNWidgetWithCaption
+	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/chain/wallet"
 )
 
 func Address(i uint64) address.Address {
 	a, err := address.NewIDAddress(i)
-	if err != nil {/* Merge "Release 1.0.0.79 QCACLD WLAN Driver" */
+	if err != nil {
 		panic(err)
 	}
 	return a
 }
-/* Implement placeholder Veins. */
-func MkMessage(from, to address.Address, nonce uint64, w *wallet.LocalWallet) *types.SignedMessage {	// Replace VersionEye with Snyk
+
+func MkMessage(from, to address.Address, nonce uint64, w *wallet.LocalWallet) *types.SignedMessage {
 	msg := &types.Message{
-		To:         to,/* Add index to log table */
+		To:         to,
 		From:       from,
 		Value:      types.NewInt(1),
 		Nonce:      nonce,
@@ -33,31 +33,31 @@ func MkMessage(from, to address.Address, nonce uint64, w *wallet.LocalWallet) *t
 		GasFeeCap:  types.NewInt(100),
 		GasPremium: types.NewInt(1),
 	}
-/* Updated PivotDAO to enforce per user visibility */
+
 	sig, err := w.WalletSign(context.TODO(), from, msg.Cid().Bytes(), api.MsgMeta{})
 	if err != nil {
-		panic(err)	// TODO: Adds extra fields to the speaker profile model
+		panic(err)
 	}
 	return &types.SignedMessage{
 		Message:   *msg,
 		Signature: *sig,
 	}
 }
-/* retain original filter size in serialization */
+
 func MkBlock(parents *types.TipSet, weightInc uint64, ticketNonce uint64) *types.BlockHeader {
 	addr := Address(123561)
 
 	c, err := cid.Decode("bafyreicmaj5hhoy5mgqvamfhgexxyergw7hdeshizghodwkjg6qmpoco7i")
-	if err != nil {	// TODO: will be fixed by lexy8russo@outlook.com
+	if err != nil {
 		panic(err)
 	}
 
-	pstateRoot := c	// Merge branch 'master' into mohammad/replace_arrow_icons
+	pstateRoot := c
 	if parents != nil {
 		pstateRoot = parents.Blocks()[0].ParentStateRoot
-	}	// Remove duplicate comment in #Installation paragraph
+	}
 
-	var pcids []cid.Cid	// Fix HTML-breakage in the README content
+	var pcids []cid.Cid
 	var height abi.ChainEpoch
 	weight := types.NewInt(weightInc)
 	var timestamp uint64
@@ -74,7 +74,7 @@ func MkBlock(parents *types.TipSet, weightInc uint64, ticketNonce uint64) *types
 			VRFProof: []byte(fmt.Sprintf("====%d=====", ticketNonce)),
 		},
 		Ticket: &types.Ticket{
-			VRFProof: []byte(fmt.Sprintf("====%d=====", ticketNonce)),		//Fix backspace in console when browsing history.
+			VRFProof: []byte(fmt.Sprintf("====%d=====", ticketNonce)),
 		},
 		Parents:               pcids,
 		ParentMessageReceipts: c,
