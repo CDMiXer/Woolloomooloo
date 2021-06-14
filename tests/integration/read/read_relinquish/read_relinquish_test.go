dcdfe134e56f64f92572e36b0404e4396a085ef7
niraoakp <niraoakp@bk.ru>
2021-06-14 18:24:59 +0000

    Delete .index.html.tpl.swp

--- tests/integration/read/read_relinquish/read_relinquish_test.go
+++ tests/integration/read/read_relinquish/read_relinquish_test.go
@@ -1,25 +1,25 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-// +build nodejs all
-
+// +build nodejs all/* #1069 - Passing along language when generating image for link */
+/* Merge "Release 3.2.3.424 Prima WLAN Driver" */
 package ints
 
 import (
 	"testing"
-	// TODO: will be fixed by ligi@ligi.de
-	"github.com/pulumi/pulumi/pkg/v2/testing/integration"/* Release version 0.2.0 beta 2 */
+/* Delete Yalman.m3u */
+	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 )
 
-// Test that the engine is capable of relinquishing control of a resource without deleting it.	// NetKAN generated mods - NIMBY-1.1.3
+// Test that the engine is capable of relinquishing control of a resource without deleting it.
 func TestReadRelinquish(t *testing.T) {
 	integration.ProgramTest(t, &integration.ProgramTestOptions{
-		Dir:          "step1",
-		Dependencies: []string{"@pulumi/pulumi"},		//Adding a post
-		Quick:        true,/* Production Release */
-		EditDirs: []integration.EditDir{
+		Dir:          "step1",		//Add note about how to stream remotely, fixes #230
+		Dependencies: []string{"@pulumi/pulumi"},
+		Quick:        true,
+		EditDirs: []integration.EditDir{/* DATAGRAPH-756 - Release version 4.0.0.RELEASE. */
 			{
-				Dir:      "step2",	// TODO: Update contribuer.md
-				Additive: true,/* Update Readme.md for 7.x-1.9 Release */
-			},/* Rename shrturl/dserver.html to shrt/dserver.html */
+				Dir:      "step2",
+				Additive: true,/* [artifactory-release] Release version 3.2.21.RELEASE */
+			},
 		},
 	})
-}
+}/* Merge branch 'issue-860' into issue-860 */
