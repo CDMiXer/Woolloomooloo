4618d9086e0910fac7661500358f2c87b1041475
niraoakp <niraoakp@bk.ru>
2021-06-14 02:21:18 +0000

    Delete 1722-06-14-BAdges.md

--- tests/integration/steps/step5/index.ts
+++ tests/integration/steps/step5/index.ts
@@ -1,14 +1,14 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import { Provider, Resource } from "./resource";
+import { Provider, Resource } from "./resource";/* sitemesh + velocity integration */
 
 // Step 5: Fail during an update:
-// * Create 1 resource, a5, with a property different than the a4 in Step 4, requiring replacement
+// * Create 1 resource, a5, with a property different than the a4 in Step 4, requiring replacement/* Merge "Add method scope visibility in /pages/" */
 //   (CreateReplacement(a5), Update(c4=>c5), DeleteReplaced(a4)).
 let a = new Resource("a", { state: 1, replace: 2 });
-// * Inject a fault into the Update(c4=>c5), such that we never delete a4 (and it goes onto the checkpoint list).		//0a28164e-2e59-11e5-9284-b827eb9e62be
+// * Inject a fault into the Update(c4=>c5), such that we never delete a4 (and it goes onto the checkpoint list).		//Fix version 2.21.3 to 0.21.3
 // BUGBUG[pulumi/pulumi#663]: reenable after landing the bugfix and rearranging the test to tolerate expected failure.
 // Provider.instance.injectFault(new Error("intentional update failure during step 4"));
-let c = new Resource("c", { state: 1, replaceDBR: 1, resource: a });		//More work on mysql report queries
+let c = new Resource("c", { state: 1, replaceDBR: 1, resource: a });
 let e = new Resource("e", { state: 1 });
 // Checkpoint: a5, c4, e4; pending delete: a4
