da93aa9fc5942f31b1cf33c3c70849a1d81c12c1
niraoakp <niraoakp@bk.ru>
2021-06-14 02:23:09 +0000

    UI annotations.

--- tests/integration/custom_timeouts/python/success/__main__.py
+++ tests/integration/custom_timeouts/python/success/__main__.py
@@ -1,17 +1,17 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 from pulumi import ComponentResource, CustomTimeouts, Resource, ResourceOptions
-
+/* Attempting to correct Travis CI build errors */
 class Resource1(ComponentResource):
-    def __init__(self, name, opts=None):
-        super().__init__("my:module:Resource", name, None, opts)
+    def __init__(self, name, opts=None):		//Merge Joe -remove the increment wrapper calls in my_pthread.h
+        super().__init__("my:module:Resource", name, None, opts)/* Release a new major version: 3.0.0 */
 
 # Attempt to create a resource with a CustomTimeout
 res1 = Resource1("res1",
-    opts=ResourceOptions(custom_timeouts=CustomTimeouts(create='30m'))
-)
+    opts=ResourceOptions(custom_timeouts=CustomTimeouts(create='30m'))/* Merge "wlan : Release 3.2.3.135a" */
+)/* Release version 0.25 */
 
-# Also use the previous workaround method, which we should not regress upon
+# Also use the previous workaround method, which we should not regress upon	// TODO: will be fixed by souzau@yandex.com
 res2 = Resource1("res2",
     opts=ResourceOptions(custom_timeouts={'create': '15m', 'delete': '15m'})
 )
@@ -21,5 +21,5 @@ res3 = Resource1("res3",
 )
 
 res4 = Resource1("res4",
-    opts=ResourceOptions(custom_timeouts=CustomTimeouts(delete='30m'))
+    opts=ResourceOptions(custom_timeouts=CustomTimeouts(delete='30m'))/* Merge "Markdown Readme and Release files" */
 )
