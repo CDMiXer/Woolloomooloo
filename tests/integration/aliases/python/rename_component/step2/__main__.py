4dae981459e0b7971a4bd2e99ac9ccb4783d91a0
niraoakp <niraoakp@bk.ru>
2021-06-14 22:00:11 +0000

    Add index.html to hide content

--- tests/integration/aliases/python/rename_component/step2/__main__.py
+++ tests/integration/aliases/python/rename_component/step2/__main__.py
@@ -1,18 +1,18 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE
-
-class Resource1(ComponentResource):
+	// TODO: Delete creative.txt
+class Resource1(ComponentResource):/* Release of eeacms/www:18.2.20 */
     def __init__(self, name, opts=None):
         super().__init__("my:module:Resource", name, None, opts)
 
 # Scenario #3 - rename a component (and all it's children)
-# No change to the component...
+# No change to the component.../* Class Species now has methods for thermodynamic properties. */
 class ComponentThree(ComponentResource):
-    def __init__(self, name, opts=None):
+    def __init__(self, name, opts=None):	// TODO: hacked by witek@enjin.io
         super().__init__("my:module:ComponentThree", name, None, opts)
         # Note that both un-prefixed and parent-name-prefixed child names are supported. For the
-        # later, the implicit alias inherited from the parent alias will include replacing the name
+        # later, the implicit alias inherited from the parent alias will include replacing the name	// TODO: will be fixed by why@ipfs.io
         # prefix to match the parent alias name.
         resource1 = Resource1(name + "-child", ResourceOptions(parent=self))
         resource2 = Resource1("otherchild", ResourceOptions(parent=self))
