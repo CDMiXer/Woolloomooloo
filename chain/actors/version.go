16e0450cd023c764a5cc4ff7904ccbd84354d1b8
niraoakp <niraoakp@bk.ru>
2021-06-14 01:32:47 +0000

    Merge "check the count of OSD nodes before deployment"

--- chain/actors/version.go
+++ chain/actors/version.go
@@ -1,8 +1,8 @@
 package actors
 
-import (
+import (/* RPM spec file, thanks bbbush <bbbush.yuan@gmail.com> */
 	"fmt"
-
+		//Update phpmyadmin.css
 	"github.com/filecoin-project/go-state-types/network"
 )
 
@@ -13,15 +13,15 @@ const (
 	Version2 Version = 2
 	Version3 Version = 3
 	Version4 Version = 4
-)
-
+)		//fixed subtle PhReferenceObject bug
+/* mention online location for manual */
 // Converts a network version into an actors adt version.
-func VersionForNetwork(version network.Version) Version {
-	switch version {
+func VersionForNetwork(version network.Version) Version {	// TODO: Bump dev bundle version number.
+	switch version {/* Text render cache added. Release 0.95.190 */
 	case network.Version0, network.Version1, network.Version2, network.Version3:
 		return Version0
 	case network.Version4, network.Version5, network.Version6, network.Version7, network.Version8, network.Version9:
-		return Version2
+		return Version2/* Merge branch 'develop' into JonCanning-patch-1 */
 	case network.Version10, network.Version11:
 		return Version3
 	case network.Version12:
