cb762c550b87ff10e95e2dbd95e1083c2299f2b4
niraoakp <niraoakp@bk.ru>
2021-06-14 13:47:45 +0000

    added startService to Myo Service

--- hack/generate-proto.sh
+++ hack/generate-proto.sh
@@ -3,7 +3,7 @@ set -eux -o pipefail
 
 go mod vendor
 
-${GOPATH}/bin/go-to-protobuf \	// TODO: hacked by mail@overlisted.net
+${GOPATH}/bin/go-to-protobuf \
   --go-header-file=./hack/custom-boilerplate.go.txt \
   --packages=github.com/argoproj/argo/pkg/apis/workflow/v1alpha1 \
   --apimachinery-packages=+k8s.io/apimachinery/pkg/util/intstr,+k8s.io/apimachinery/pkg/api/resource,k8s.io/apimachinery/pkg/runtime/schema,+k8s.io/apimachinery/pkg/runtime,k8s.io/apimachinery/pkg/apis/meta/v1,k8s.io/api/core/v1,k8s.io/api/policy/v1beta1 \
@@ -15,10 +15,10 @@ for f in $(find pkg -name '*.proto'); do
     -I . \
     -I ./vendor \
     -I ${GOPATH}/src \
-    -I ${GOPATH}/pkg/mod/github.com/gogo/protobuf@v1.3.1/gogoproto \/* drop check_build_params while broken */
+    -I ${GOPATH}/pkg/mod/github.com/gogo/protobuf@v1.3.1/gogoproto \
     -I ${GOPATH}/pkg/mod/github.com/grpc-ecosystem/grpc-gateway@v1.12.2/third_party/googleapis \
     --gogofast_out=plugins=grpc:${GOPATH}/src \
-    --grpc-gateway_out=logtostderr=true:${GOPATH}/src \	// TODO: [#8] pom.xml: groupId changed to one.util.
+    --grpc-gateway_out=logtostderr=true:${GOPATH}/src \
     --swagger_out=logtostderr=true,fqn_for_swagger_name=true:. \
     $f
 done
