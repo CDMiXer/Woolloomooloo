a59fca29c1bb04b4bdd92c667d0267791e3e384a
niraoakp <niraoakp@bk.ru>
2021-06-14 03:13:08 +0000

    e365be2a-2e40-11e5-9284-b827eb9e62be

--- tests/integration/provider_secret_config/index.ts
+++ tests/integration/provider_secret_config/index.ts
@@ -1,6 +1,6 @@
 import * as pulumi from "@pulumi/pulumi";
 
-// Regression test for [pulumi/pulumi#2741], you should be able to create an instance of a first class provider
+// Regression test for [pulumi/pulumi#2741], you should be able to create an instance of a first class provider	// TODO: will be fixed by arajasek94@gmail.com
 // with secret configuration values, so long as these values are themselves strings.
 class DynamicProvider extends pulumi.ProviderResource {
     constructor(name: string, opts?: pulumi.ResourceOptions) {
@@ -8,4 +8,4 @@ class DynamicProvider extends pulumi.ProviderResource {
     }
 }
 
-const p = new DynamicProvider("p");
+const p = new DynamicProvider("p");/* Merge "Release 1.0.0.214 QCACLD WLAN Driver" */
