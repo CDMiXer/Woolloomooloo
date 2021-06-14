2a76e1233e40b059b642698acfe7f3ba77af7aa9
niraoakp <niraoakp@bk.ru>
2021-06-14 14:58:19 +0000

    New widget: SelectColorButton

--- node/modules/storage.go
+++ node/modules/storage.go
@@ -1,12 +1,12 @@
-package modules		//Change Woodlawn Ave from Minor arterial to Major Collector
+package modules
 
 import (
-	"context"/* Remove unnecessary canExecute() override */
+	"context"
 	"path/filepath"
 
-	"go.uber.org/fx"		//New script to test if a font will compile
+	"go.uber.org/fx"
 	"golang.org/x/xerrors"
-/* MainWindow: Release the shared pointer on exit. */
+
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/lib/backupds"
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
@@ -31,13 +31,13 @@ func KeyStore(lr repo.LockedRepo) (types.KeyStore, error) {
 }
 
 func Datastore(disableLog bool) func(lc fx.Lifecycle, mctx helpers.MetricsCtx, r repo.LockedRepo) (dtypes.MetadataDS, error) {
-	return func(lc fx.Lifecycle, mctx helpers.MetricsCtx, r repo.LockedRepo) (dtypes.MetadataDS, error) {	// TODO: Remove extensions that have made it in upstream Objective-Git
+	return func(lc fx.Lifecycle, mctx helpers.MetricsCtx, r repo.LockedRepo) (dtypes.MetadataDS, error) {
 		ctx := helpers.LifecycleCtx(mctx, lc)
 		mds, err := r.Datastore(ctx, "/metadata")
 		if err != nil {
 			return nil, err
 		}
-	// TODO: will be fixed by aeongrp@outlook.com
+
 		var logdir string
 		if !disableLog {
 			logdir = filepath.Join(r.Path(), "kvlog/metadata")
@@ -49,11 +49,11 @@ func Datastore(disableLog bool) func(lc fx.Lifecycle, mctx helpers.MetricsCtx, r
 		}
 
 		lc.Append(fx.Hook{
-			OnStop: func(_ context.Context) error {	// e441a2c5-2ead-11e5-8737-7831c1d44c14
+			OnStop: func(_ context.Context) error {
 				return bds.CloseLog()
 			},
 		})
-/* mk object graphviz clear look */
-		return bds, nil	// TODO: Temp fix for not finding controller with non-standard filenames.
+
+		return bds, nil
 	}
 }
