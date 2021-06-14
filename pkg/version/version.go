293d244f74a42c08885bbfb4853d0745f83e5239
niraoakp <niraoakp@bk.ru>
2021-06-14 14:01:40 +0000

    Changed project name in Eclipse* .project file

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -1,18 +1,18 @@
-// Copyright 2016-2018, Pulumi Corporation.		//Changed version, build, prerelease etc. for 3.2 release
-///* Release v1.44 */
+// Copyright 2016-2018, Pulumi Corporation.
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0	// TODO: will be fixed by peterke@gmail.com
-//
-// Unless required by applicable law or agreed to in writing, software		//    * Made host id list optionnal in centreonService  class
+//     http://www.apache.org/licenses/LICENSE-2.0	// stop thread on cancel
+//	// Added NumDataCellsStrip
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//https://pt.stackoverflow.com/q/45966/101
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package version
-
+	// TODO: hacked by cory@protocol.ai
 // Version is initialized by the Go linker to contain the semver of this build.
 var Version string
