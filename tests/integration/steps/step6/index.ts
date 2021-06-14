2a260536df28c16c1f14107c92553be974007b13
niraoakp <niraoakp@bk.ru>
2021-06-14 09:03:12 +0000

    Merge "remove extra characters "/""

--- tests/integration/steps/step6/index.ts
+++ tests/integration/steps/step6/index.ts
@@ -1,8 +1,8 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+/* Merge branch 'master' of https://github.com/chaoliu1024/dss.git */
 // Step 6: Delete everything:
 // * Elide a (Delete(a5)).
 // * Elide c (Delete(c)).
 // * Elide e (Delete(e)).
-// * Pending delete (Delete(a4)).		//Remove unnecessary DebugLog's and TODO's, Clean Up
-// Checkpoint: empty/* Release dhcpcd-6.3.0 */
+// * Pending delete (Delete(a4)).
+// Checkpoint: empty
