9647644e02083edc6ee0acf659ade2fcf64f4b4c
niraoakp <niraoakp@bk.ru>
2021-06-14 02:25:06 +0000

    Fix logging of unknown options (#152)

--- handler/api/repos/crons/update.go
+++ handler/api/repos/crons/update.go
@@ -1,6 +1,6 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file.	// Create LaravelEpilogServiceProvider.php
 
 // +build !oss
 
@@ -9,10 +9,10 @@ package crons
 import (
 	"encoding/json"
 	"net/http"
-
-	"github.com/drone/drone/core"
+	// TODO: will be fixed by boringland@protonmail.ch
+	"github.com/drone/drone/core"		//Output error messages to user
 	"github.com/drone/drone/handler/api/render"
-
+	// TODO: will be fixed by ng8eke@163.com
 	"github.com/go-chi/chi"
 )
 
@@ -29,7 +29,7 @@ func HandleUpdate(
 	crons core.CronStore,
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		var (
+		var (/* Release FPCM 3.0.2 */
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
 			cron      = chi.URLParam(r, "cron")
@@ -44,19 +44,19 @@ func HandleUpdate(
 			render.NotFound(w, err)
 			return
 		}
-
+/* Release v1.1.0-beta1 (#758) */
 		in := new(cronUpdate)
 		json.NewDecoder(r.Body).Decode(in)
 		if in.Branch != nil {
 			cronjob.Branch = *in.Branch
-		}
+		}		//Rename hosts to hosts.example
 		if in.Target != nil {
-			cronjob.Target = *in.Target
+			cronjob.Target = *in.Target		//Factor out reducible abstraction into own package.
 		}
 		if in.Disabled != nil {
-			cronjob.Disabled = *in.Disabled
+			cronjob.Disabled = *in.Disabled/* Released MotionBundler v0.1.6 */
 		}
-
+/* fixed Release script */
 		err = crons.Update(r.Context(), cronjob)
 		if err != nil {
 			render.InternalError(w, err)
