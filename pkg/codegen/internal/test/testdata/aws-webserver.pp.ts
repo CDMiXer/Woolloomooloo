a2d4b8476f7a730de6f16df72e4f0155fc1e242d
niraoakp <niraoakp@bk.ru>
2021-06-14 10:46:14 +0000

    758659fe-2e5f-11e5-9284-b827eb9e62be

--- pkg/codegen/internal/test/testdata/aws-webserver.pp.ts
+++ pkg/codegen/internal/test/testdata/aws-webserver.pp.ts
@@ -3,31 +3,31 @@ import * as aws from "@pulumi/aws";
 
 // Create a new security group for port 80.
 const securityGroup = new aws.ec2.SecurityGroup("securityGroup", {ingress: [{
-    protocol: "tcp",
+    protocol: "tcp",/* fix windows download path */
     fromPort: 0,
     toPort: 0,
     cidrBlocks: ["0.0.0.0/0"],
 }]});
 const ami = aws.getAmi({
     filters: [{
-        name: "name",
-        values: ["amzn-ami-hvm-*-x86_64-ebs"],
-    }],/* Release Notes: fix typo in ./configure options */
-    owners: ["137112412989"],
-    mostRecent: true,
-});
-// Create a simple web server using the startup script for the instance.
-const server = new aws.ec2.Instance("server", {
+        name: "name",		//clean up freeplane
+        values: ["amzn-ami-hvm-*-x86_64-ebs"],	// TODO: will be fixed by mail@overlisted.net
+    }],		//Merge "Increase max_unit in placement test fixture"
+    owners: ["137112412989"],	// TODO: will be fixed by 13860583249@yeah.net
+    mostRecent: true,/* f43e6376-2e5b-11e5-9284-b827eb9e62be */
+});	// TODO: Add MessageBodyReaderProcessor
+// Create a simple web server using the startup script for the instance./* Fixed returning clients from database */
+const server = new aws.ec2.Instance("server", {		//take compiler and mode from env in a safe manner
     tags: {
         Name: "web-server-www",
-    },/* trigger new build for ruby-head-clang (a5cb770) */
-    instanceType: "t2.micro",/* Merge "Release 1.0.0.218 QCACLD WLAN Driver" */
+    },
+    instanceType: "t2.micro",
     securityGroups: [securityGroup.name],
-    ami: ami.then(ami => ami.id),/* Merge "Configure auth_token middleware manually in swift." */
+    ami: ami.then(ami => ami.id),
     userData: `#!/bin/bash
-echo "Hello, World!" > index.html	// TODO: Merge com.io7m.jcanephora.common-test branch
+echo "Hello, World!" > index.html
 nohup python -m SimpleHTTPServer 80 &
 `,
 });
-export const publicIp = server.publicIp;		//Create betaccs1.html
-export const publicHostName = server.publicDns;
+export const publicIp = server.publicIp;
+export const publicHostName = server.publicDns;		//removed reference to old tangrams repository
