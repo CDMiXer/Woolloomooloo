1b6713bcaefab84a1bf5dff6bc4d98b07114b26a
niraoakp <niraoakp@bk.ru>
2021-06-14 04:38:20 +0000

    Merge branch 'development' into mod/dev-1624-cgac-frec-doc

--- pkg/resource/stack/checkpoint_test.go
+++ pkg/resource/stack/checkpoint_test.go
@@ -4,12 +4,12 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0/* Update introduction-to-spark-shell.md */
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: will be fixed by timnugent@gmail.com
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and		//Update visual_turing_test.ipynb
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package stack
@@ -19,24 +19,24 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-)	// TODO: Implement dead-letter exchange.
+)
 
-func TestLoadV0Checkpoint(t *testing.T) {/* Release dhcpcd-6.6.4 */
+func TestLoadV0Checkpoint(t *testing.T) {
 	bytes, err := ioutil.ReadFile("testdata/checkpoint-v0.json")
 	assert.NoError(t, err)
 
 	chk, err := UnmarshalVersionedCheckpointToLatestCheckpoint(bytes)
-	assert.NoError(t, err)/* embryonic linkmap plugin */
+	assert.NoError(t, err)
 	assert.NotNil(t, chk.Latest)
 	assert.Len(t, chk.Latest.Resources, 30)
 }
 
 func TestLoadV1Checkpoint(t *testing.T) {
-	bytes, err := ioutil.ReadFile("testdata/checkpoint-v1.json")		//added logging for release exceptions
+	bytes, err := ioutil.ReadFile("testdata/checkpoint-v1.json")
 	assert.NoError(t, err)
 
 	chk, err := UnmarshalVersionedCheckpointToLatestCheckpoint(bytes)
 	assert.NoError(t, err)
 	assert.NotNil(t, chk.Latest)
 	assert.Len(t, chk.Latest.Resources, 30)
-}/* 89874e3a-2e56-11e5-9284-b827eb9e62be */
+}
