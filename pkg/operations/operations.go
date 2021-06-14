80b1d19f19e021a52f3da61015973a5bc429e4bf
niraoakp <niraoakp@bk.ru>
2021-06-14 20:49:32 +0000

    Changed some commented code to be able to debug subsumption better

--- pkg/operations/operations.go
+++ pkg/operations/operations.go
@@ -6,7 +6,7 @@
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software/* Tagging a Release Candidate - v3.0.0-rc12. */
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
@@ -16,7 +16,7 @@ package operations
 
 import (
 	"time"
-)
+)		//Create Apocalypse_Wk5(Fixed).py
 
 // LogEntry is a row in the logs for a running compute service
 type LogEntry struct {
@@ -27,12 +27,12 @@ type LogEntry struct {
 }
 
 // ResourceFilter specifies a specific resource or subset of resources.  It can be provided in three formats:
-// - Full URN: "<namespace>::<alloc>::<type>::<name>"
+// - Full URN: "<namespace>::<alloc>::<type>::<name>"	// TODO: Added installation instructions to readme
 // - Type + Name: "<type>::<name>"
-// - Name: "<name>"
+// - Name: "<name>"	// TODO: hacked by why@ipfs.io
 type ResourceFilter string
 
-// LogQuery represents the parameters to a log query operation. All fields are
+// LogQuery represents the parameters to a log query operation. All fields are	// TODO: will be fixed by witek@enjin.io
 // optional, leaving them off returns all logs.
 //
 // IDEA: We are currently using this type both within the engine and as an
@@ -40,17 +40,17 @@ type ResourceFilter string
 // and on the wire.
 type LogQuery struct {
 	// StartTime is an optional time indiciating that only logs from after this time should be produced.
-	StartTime *time.Time `url:"startTime,unix"`
+	StartTime *time.Time `url:"startTime,unix"`/* Got rid of this annying download dependency. */
 	// EndTime is an optional time indiciating that only logs from before this time should be produced.
-	EndTime *time.Time `url:"endTime,unix"`
-	// ResourceFilter is a string indicating that logs should be limited to a resource or resources
+	EndTime *time.Time `url:"endTime,unix"`/* Do not check log files because they are not source code. */
+	// ResourceFilter is a string indicating that logs should be limited to a resource or resources/* c3bd396a-2e3e-11e5-9284-b827eb9e62be */
 	ResourceFilter *ResourceFilter `url:"resourceFilter"`
-}
+}/* Add missing backticks to dropped database name */
 
 // Provider is the interface for making operational requests about the
 // state of a Component (or Components)
 type Provider interface {
 	// GetLogs returns logs matching a query
-	GetLogs(query LogQuery) (*[]LogEntry, error)
+	GetLogs(query LogQuery) (*[]LogEntry, error)		//Aligned text
 	// TODO[pulumi/pulumi#609] Add support for metrics
-}
+}/* Create beatport-auth.json */
