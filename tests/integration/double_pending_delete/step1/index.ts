ca41501976e2272e91349655f52bd5f8ef178784
niraoakp <niraoakp@bk.ru>
2021-06-14 15:02:54 +0000

    Update kryptonstealer.txt

--- tests/integration/double_pending_delete/step1/index.ts
+++ tests/integration/double_pending_delete/step1/index.ts
@@ -1,8 +1,8 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: Actualizada la hoja de estilos
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// TODO: will be fixed by qugou1350636@126.com
+// You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
@@ -10,14 +10,14 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.	// TODO: Add relative number toggle
+// limitations under the License.
 
-import { Resource } from "./resource";/* Release Neo4j 3.4.1 */
+import { Resource } from "./resource";
 
 // Setup: Resources A and B are created successfully.
 const a = new Resource("a", { fail: 0 });
 const b = new Resource("b", { fail: 0 }, { dependsOn: a });
-// The snapshot now contains:/* Merge remote-tracking branch 'origin/master' into issue_121 */
+// The snapshot now contains:
 //  A: Created
-//  B: Created		//boss battle forever borked.
-/* Merge "Docs: Added AS 2.0 Release Notes" into mnc-mr-docs */
+//  B: Created
+
