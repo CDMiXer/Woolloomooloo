5032cbd6f66b983f00c7e1bb9140edb9aab4d6f7
niraoakp <niraoakp@bk.ru>
2021-06-14 17:36:46 +0000

    reinstate behavior of closing charm panel on click, per review

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -1,14 +1,14 @@
-// Copyright 2016-2018, Pulumi Corporation.
-//
+// Copyright 2016-2018, Pulumi Corporation.		//Changed version, build, prerelease etc. for 3.2 release
+///* Release v1.44 */
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//     http://www.apache.org/licenses/LICENSE-2.0	// TODO: will be fixed by peterke@gmail.com
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software		//    * Made host id list optionnal in centreonService  class
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//https://pt.stackoverflow.com/q/45966/101
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
