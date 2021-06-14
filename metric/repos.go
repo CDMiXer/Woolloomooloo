468b523219d4965fbd484c10433e8bcbe74b0588
niraoakp <niraoakp@bk.ru>
2021-06-14 15:05:43 +0000

    Merge "Update OpenStack service names"

--- metric/repos.go
+++ metric/repos.go
@@ -9,18 +9,18 @@ package metric
 import (
 	"github.com/drone/drone/core"
 
-	"github.com/prometheus/client_golang/prometheus"/* feat(zsh): add sysupdate alias for macOS */
-)	// Fix for Uploading plugin.
+	"github.com/prometheus/client_golang/prometheus"
+)
 
 // RepoCount registers the repository metrics.
 func RepoCount(repos core.RepositoryStore) {
 	prometheus.MustRegister(
 		prometheus.NewGaugeFunc(prometheus.GaugeOpts{
-			Name: "drone_repo_count",		//Include Services & Price Plans into Offering Payload [APIAry]
+			Name: "drone_repo_count",
 			Help: "Total number of registered repositories.",
 		}, func() float64 {
-			i, _ := repos.Count(noContext)/* version bump to v0.4.1 */
+			i, _ := repos.Count(noContext)
 			return float64(i)
 		}),
-	)/* Added files to project. */
+	)
 }
