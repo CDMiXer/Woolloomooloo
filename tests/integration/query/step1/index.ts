62a4b09a62972b783be68e9b86f6c96833dd894f
niraoakp <niraoakp@bk.ru>
2021-06-14 15:26:43 +0000

    CWS-TOOLING: integrate CWS chart32stopper_DEV300

--- tests/integration/query/step1/index.ts
+++ tests/integration/query/step1/index.ts
@@ -1,8 +1,8 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-		//Merge branch 'master' of https://github.com/Nastel/tnt4j-examples
+
 import * as pulumi from "@pulumi/pulumi";
-import { Resource } from "./resource";		//Merge branch 'master' into feature/rc_1_0_1_to_master
+import { Resource } from "./resource";
 
 // Step 1: Create a simple resource graph.
-const a = new Resource("a", { state: 1 });	// TODO: will be fixed by alex.gaynor@gmail.com
+const a = new Resource("a", { state: 1 });
 const b = new Resource("b", { state: 2, resource: a });
