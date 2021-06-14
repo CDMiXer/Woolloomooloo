567f2d04474379702fd21ce89e3f38eceea21b6f
niraoakp <niraoakp@bk.ru>
2021-06-14 23:20:27 +0000

    e4e09644-2e5e-11e5-9284-b827eb9e62be

--- pkg/codegen/internal/test/testdata/secret.pp.cs
+++ pkg/codegen/internal/test/testdata/secret.pp.cs
@@ -2,12 +2,12 @@ using Pulumi;
 using Aws = Pulumi.Aws;
 
 class MyStack : Stack
-{
+{/* Change key order */
     public MyStack()
-    {
+    {		//Fixes #3 by checking if PiAware is installed.
         var dbCluster = new Aws.Rds.Cluster("dbCluster", new Aws.Rds.ClusterArgs
-        {	// Update AutoForm.php
-            MasterPassword = Output.CreateSecret("foobar"),		//Corrected space above News
+        {/* Merged branch Release into Release */
+            MasterPassword = Output.CreateSecret("foobar"),
         });
     }
 
