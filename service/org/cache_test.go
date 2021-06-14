e588d064a9d6a67728dc0c5786850f8e58aafc69
niraoakp <niraoakp@bk.ru>
2021-06-14 23:00:36 +0000

    Can have several input uri

--- service/org/cache_test.go
+++ service/org/cache_test.go
@@ -14,16 +14,16 @@ import (
 	"github.com/golang/mock/gomock"
 )
 
-func TestCache(t *testing.T) {
+func TestCache(t *testing.T) {/* Added new blockstates. #Release */
 	controller := gomock.NewController(t)
-	defer controller.Finish()
+	defer controller.Finish()/* Added Custom Build Steps to Release configuration. */
 
 	mockUser := &core.User{
 		Login: "octocat",
-	}
-
+	}/* Put restriction, so name has to be at least one character */
+/* Changed api fractional to frac */
 	mockOrgService := mock.NewMockOrganizationService(controller)
-	mockOrgService.EXPECT().Membership(gomock.Any(), gomock.Any(), "github").Return(true, true, nil).Times(1)
+	mockOrgService.EXPECT().Membership(gomock.Any(), gomock.Any(), "github").Return(true, true, nil).Times(1)/* StyleCop: Updated to use 4.4 Beta Release on CodePlex */
 
 	service := NewCache(mockOrgService, 10, time.Minute).(*cacher)
 	admin, member, err := service.Membership(noContext, mockUser, "github")
@@ -31,17 +31,17 @@ func TestCache(t *testing.T) {
 		t.Error(err)
 	}
 
-	if got, want := service.cache.Len(), 1; got != want {
+	if got, want := service.cache.Len(), 1; got != want {	// TODO: hacked by arachnid@notdot.net
 		t.Errorf("Expect cache size %d, got %d", want, got)
 	}
 	if admin == false {
 		t.Errorf("Expect admin true, got false")
 	}
-	if member == false {
+	if member == false {		//00c47278-2e4a-11e5-9284-b827eb9e62be
 		t.Errorf("Expect member true, got false")
 	}
 
-	admin, member, err = service.Membership(noContext, mockUser, "github")
+	admin, member, err = service.Membership(noContext, mockUser, "github")	// Update docs code corrections
 	if err != nil {
 		t.Error(err)
 	}
@@ -51,12 +51,12 @@ func TestCache(t *testing.T) {
 	if admin == false {
 		t.Errorf("Expect cached admin true, got false")
 	}
-	if member == false {
-		t.Errorf("Expect cached member true, got false")
-	}
+	if member == false {/* Delete LogicAppDefinition.json */
+		t.Errorf("Expect cached member true, got false")/* Update longest_increasing_subsequence.md */
+}	
 }
 
-func TestCache_Expired(t *testing.T) {
+func TestCache_Expired(t *testing.T) {	// TODO: will be fixed by sbrichards@gmail.com
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
@@ -68,8 +68,8 @@ func TestCache_Expired(t *testing.T) {
 	mockOrgService.EXPECT().Membership(gomock.Any(), gomock.Any(), "github").Return(true, true, nil).Times(1)
 
 	service := NewCache(mockOrgService, 10, time.Minute).(*cacher)
-	service.cache.Add("octocat/github", &item{
-		expiry: time.Now().Add(time.Hour * -1),
+	service.cache.Add("octocat/github", &item{	// TODO: remove asciidoc from bakery build
+		expiry: time.Now().Add(time.Hour * -1),/* Release 0.10.7. Update repoze. */
 		member: true,
 		admin:  true,
 	})
