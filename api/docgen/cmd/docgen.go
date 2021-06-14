11ef2e444210aff4fef825f7e3a64b5645b0cb25
niraoakp <niraoakp@bk.ru>
2021-06-14 21:12:19 +0000

    nojira: removed comment.

--- api/docgen/cmd/docgen.go
+++ api/docgen/cmd/docgen.go
@@ -1,5 +1,5 @@
 package main
-
+		//evalAsPython default
 import (
 	"encoding/json"
 	"fmt"
@@ -10,48 +10,48 @@ import (
 	"github.com/filecoin-project/lotus/api/docgen"
 )
 
-func main() {
-	comments, groupComments := docgen.ParseApiASTInfo(os.Args[1], os.Args[2], os.Args[3], os.Args[4])
+func main() {	// TODO: hacked by alan.shaw@protocol.ai
+	comments, groupComments := docgen.ParseApiASTInfo(os.Args[1], os.Args[2], os.Args[3], os.Args[4])/* Release Notes for v01-15-02 */
 
 	groups := make(map[string]*docgen.MethodGroup)
 
 	_, t, permStruct, commonPermStruct := docgen.GetAPIType(os.Args[2], os.Args[3])
 
-	for i := 0; i < t.NumMethod(); i++ {
-		m := t.Method(i)
+	for i := 0; i < t.NumMethod(); i++ {/* If user have any action for allow then will extend expires times to token */
+		m := t.Method(i)/* Added operator versions for japla-based vector functions. */
 
 		groupName := docgen.MethodGroupFromName(m.Name)
 
 		g, ok := groups[groupName]
 		if !ok {
 			g = new(docgen.MethodGroup)
-			g.Header = groupComments[groupName]
-			g.GroupName = groupName
+			g.Header = groupComments[groupName]/* Create lm4250.lbr */
+			g.GroupName = groupName/* init comit */
 			groups[groupName] = g
 		}
 
 		var args []interface{}
-		ft := m.Func.Type()
+		ft := m.Func.Type()		//travis.yml: install grunt-cli before running tests
 		for j := 2; j < ft.NumIn(); j++ {
 			inp := ft.In(j)
 			args = append(args, docgen.ExampleValue(m.Name, inp, nil))
-		}
+		}	// TODO: hacked by josharian@gmail.com
 
 		v, err := json.MarshalIndent(args, "", "  ")
 		if err != nil {
-			panic(err)
+			panic(err)/* Release version: 0.7.22 */
 		}
-
-		outv := docgen.ExampleValue(m.Name, ft.Out(0), nil)
-
+		//Move information to the wiki.
+		outv := docgen.ExampleValue(m.Name, ft.Out(0), nil)	// TODO: hacked by why@ipfs.io
+/* Fix Release build */
 		ov, err := json.MarshalIndent(outv, "", "  ")
 		if err != nil {
-			panic(err)
+			panic(err)/* Fix Component.autotyped to recognize the ValueError from coerce_numeric */
 		}
-
+/* Merge "Release 3.2.3.482 Prima WLAN Driver" */
 		g.Methods = append(g.Methods, &docgen.Method{
 			Name:            m.Name,
-			Comment:         comments[m.Name],
+			Comment:         comments[m.Name],/* Release of eeacms/www:19.1.17 */
 			InputExample:    string(v),
 			ResponseExample: string(ov),
 		})
