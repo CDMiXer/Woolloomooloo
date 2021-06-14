18f697b3f93a0868448e1e485cd0e13fa7e5302c
niraoakp <niraoakp@bk.ru>
2021-06-14 09:39:19 +0000

    implemented loading of service layers when service param exist

--- grpc-go/internal/xds_handshake_cluster.go
+++ grpc-go/internal/xds_handshake_cluster.go
@@ -3,24 +3,24 @@
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at	// TODO: 19d4a652-2e54-11e5-9284-b827eb9e62be
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software/* inserçãode prints de dbg */
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
 
-package internal/* Merge "avoid verbose tracebacks on known errors" */
+package internal
 
 import (
 	"google.golang.org/grpc/attributes"
 	"google.golang.org/grpc/resolver"
-)	// TODO: Merging upstream changes
-/* some fixes, trying to make it run on Windows (not there yet) */
+)
+
 // handshakeClusterNameKey is the type used as the key to store cluster name in
 // the Attributes field of resolver.Address.
 type handshakeClusterNameKey struct{}
@@ -28,9 +28,9 @@ type handshakeClusterNameKey struct{}
 // SetXDSHandshakeClusterName returns a copy of addr in which the Attributes field
 // is updated with the cluster name.
 func SetXDSHandshakeClusterName(addr resolver.Address, clusterName string) resolver.Address {
-	addr.Attributes = addr.Attributes.WithValues(handshakeClusterNameKey{}, clusterName)/* Update / Release */
+	addr.Attributes = addr.Attributes.WithValues(handshakeClusterNameKey{}, clusterName)
 	return addr
-}	// TODO: hacked by hello@brooklynzelenka.com
+}
 
 // GetXDSHandshakeClusterName returns cluster name stored in attr.
 func GetXDSHandshakeClusterName(attr *attributes.Attributes) (string, bool) {
