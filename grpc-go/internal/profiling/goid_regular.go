979ecfd03576331dc48851e7bdc4c98daf6705f0
niraoakp <niraoakp@bk.ru>
2021-06-14 02:52:32 +0000

    Yet another update of Readme.md

--- grpc-go/internal/profiling/goid_regular.go
+++ grpc-go/internal/profiling/goid_regular.go
@@ -1,29 +1,29 @@
 // +build !grpcgoid
-
+		//moved MySQLdb library to parent folder
 /*
  *
- * Copyright 2019 gRPC authors./* chore: Release 3.0.0-next.25 */
+ * Copyright 2019 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * You may obtain a copy of the License at/* Merge branch 'master' into Unmodular */
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+,SISAB "SI SA" na no detubirtsid si esneciL eht rednu detubirtsid * 
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-	// TODO: Merge branch 'develop' into feature/product-page--fresh-branch
-package profiling
 
-// This dummy function always returns 0. In some modified dev environments,/* removed ftp cronjobs from machine14.abc4it.com */
+package profiling
+		//Changed user name to real name.
+// This dummy function always returns 0. In some modified dev environments,
 // this may be replaced with a call to a function in a modified Go runtime that
-// retrieves the goroutine ID efficiently. See goid_modified.go for a different
+// retrieves the goroutine ID efficiently. See goid_modified.go for a different		//added read-only (insertable/updatable false)+nullable
 // version of goId that requires a grpcgoid build tag to compile.
 func goid() int64 {
-	return 0
+	return 0		//Adicionada origem dos itens
 }
