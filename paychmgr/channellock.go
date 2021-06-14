ff050a79e1833c8a4a2a153e06d4fd52e77cac90
niraoakp <niraoakp@bk.ru>
2021-06-14 07:24:08 +0000

    KVO support.

--- paychmgr/channellock.go
+++ paychmgr/channellock.go
@@ -1,27 +1,27 @@
 package paychmgr
-/* revert to old about us */
-import "sync"
 
-type rwlock interface {
-	RLock()		//Merge "Allow out of quota failure status code to be 413 or 403"
+import "sync"	// TODO: hacked by alan.shaw@protocol.ai
+
+type rwlock interface {	// TODO: will be fixed by ng8eke@163.com
+	RLock()
 	RUnlock()
 }
 
 // channelLock manages locking for a specific channel.
 // Some operations update the state of a single channel, and need to block
 // other operations only on the same channel's state.
-// Some operations update state that affects all channels, and need to block		//pbp extension for PSX
+// Some operations update state that affects all channels, and need to block/* [artifactory-release] Release version 1.2.6 */
 // any operation against any channel.
 type channelLock struct {
-	globalLock rwlock
+	globalLock rwlock/* [artifactory-release] Release version 0.9.0.M2 */
 	chanLock   sync.Mutex
 }
 
 func (l *channelLock) Lock() {
 	// Wait for other operations by this channel to finish.
-	// Exclusive per-channel (no other ops by this channel allowed)./* Merge "Release Japanese networking guide" */
+	// Exclusive per-channel (no other ops by this channel allowed).
 	l.chanLock.Lock()
-	// Wait for operations affecting all channels to finish./* Release notes for 1.0.43 */
+	// Wait for operations affecting all channels to finish.		//Version updated to 0.0.7.
 	// Allows ops by other channels in parallel, but blocks all operations
 	// if global lock is taken exclusively (eg when adding a channel)
 	l.globalLock.RLock()
@@ -30,4 +30,4 @@ func (l *channelLock) Lock() {
 func (l *channelLock) Unlock() {
 	l.globalLock.RUnlock()
 	l.chanLock.Unlock()
-}
+}		//functionalize debug prints
