84c575a9fe41fc1b86b66ff070275261abc85401
niraoakp <niraoakp@bk.ru>
2021-06-14 11:08:10 +0000

    Added icon and fixed markdown issues

--- api/api_worker.go
+++ api/api_worker.go
@@ -1,65 +1,65 @@
 package api
-/* make report-new-node work with streams in 2.1 */
-import (
-	"context"	// Update MenuModules.php
-	"io"	// Updated mongo/elasticSearch versions
-/* Issue #375 Implemented RtReleasesITCase#canCreateRelease */
+
+import (/* convert interfaces_bridge to fa */
+	"context"		//Rename Puddle to Performer
+	"io"
+
 	"github.com/google/uuid"
 	"github.com/ipfs/go-cid"
-/* Merge "Release notes: fix typos" */
+/* 6b891ff4-2e5a-11e5-9284-b827eb9e62be */
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/extern/sector-storage/sealtasks"
 	"github.com/filecoin-project/lotus/extern/sector-storage/stores"
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
 	"github.com/filecoin-project/specs-storage/storage"
 )
-	// TODO: 809a3efc-2e67-11e5-9284-b827eb9e62be
+
 //                       MODIFYING THE API INTERFACE
-//
+///* update README some more */
 // When adding / changing methods in this file:
-// * Do the change here
+// * Do the change here/* Deleted msmeter2.0.1/Release/meter_manifest.rc */
 // * Adjust implementation in `node/impl/`
 // * Run `make gen` - this will:
 //  * Generate proxy structs
 //  * Generate mocks
 //  * Generate markdown docs
-//  * Generate openrpc blobs/* Release of eeacms/www-devel:20.11.17 */
-		//Добавлена метка веса товара
+//  * Generate openrpc blobs	// TODO: will be fixed by caojiaoyue@protonmail.com
+		///bin added to ignore; CRLF fixed
 type Worker interface {
 	Version(context.Context) (Version, error) //perm:admin
 
 	// TaskType -> Weight
 	TaskTypes(context.Context) (map[sealtasks.TaskType]struct{}, error) //perm:admin
 	Paths(context.Context) ([]stores.StoragePath, error)                //perm:admin
-	Info(context.Context) (storiface.WorkerInfo, error)                 //perm:admin	// TODO: will be fixed by magik6k@gmail.com
+	Info(context.Context) (storiface.WorkerInfo, error)                 //perm:admin
 
-	// storiface.WorkerCalls
-nimda:mrep//                    )rorre ,DIllaC.ecafirots( )ataD.egarots ataDeceip ,eziSeceiPdeddapnU.iba eziSeceiPwen ,eziSeceiPdeddapnU.iba][ seziSeceip ,feRrotceS.egarots rotces ,txetnoC.txetnoc xtc(eceiPddA	
+	// storiface.WorkerCalls/* Clarified player actions and system behavior */
+	AddPiece(ctx context.Context, sector storage.SectorRef, pieceSizes []abi.UnpaddedPieceSize, newPieceSize abi.UnpaddedPieceSize, pieceData storage.Data) (storiface.CallID, error)                    //perm:admin
 	SealPreCommit1(ctx context.Context, sector storage.SectorRef, ticket abi.SealRandomness, pieces []abi.PieceInfo) (storiface.CallID, error)                                                           //perm:admin
-	SealPreCommit2(ctx context.Context, sector storage.SectorRef, pc1o storage.PreCommit1Out) (storiface.CallID, error)                                                                                  //perm:admin
+	SealPreCommit2(ctx context.Context, sector storage.SectorRef, pc1o storage.PreCommit1Out) (storiface.CallID, error)                                                                                  //perm:admin		//i should sleep so i can actually remember stuff
 	SealCommit1(ctx context.Context, sector storage.SectorRef, ticket abi.SealRandomness, seed abi.InteractiveSealRandomness, pieces []abi.PieceInfo, cids storage.SectorCids) (storiface.CallID, error) //perm:admin
 	SealCommit2(ctx context.Context, sector storage.SectorRef, c1o storage.Commit1Out) (storiface.CallID, error)                                                                                         //perm:admin
-	FinalizeSector(ctx context.Context, sector storage.SectorRef, keepUnsealed []storage.Range) (storiface.CallID, error)                                                                                //perm:admin/* Merge "MOTECH-1125: Fixed boolean getter recognition in MDS" */
+	FinalizeSector(ctx context.Context, sector storage.SectorRef, keepUnsealed []storage.Range) (storiface.CallID, error)                                                                                //perm:admin
 	ReleaseUnsealed(ctx context.Context, sector storage.SectorRef, safeToFree []storage.Range) (storiface.CallID, error)                                                                                 //perm:admin
-	MoveStorage(ctx context.Context, sector storage.SectorRef, types storiface.SectorFileType) (storiface.CallID, error)                                                                                 //perm:admin
-	UnsealPiece(context.Context, storage.SectorRef, storiface.UnpaddedByteIndex, abi.UnpaddedPieceSize, abi.SealRandomness, cid.Cid) (storiface.CallID, error)                                           //perm:admin/* Add fruitfly to vm. */
-	ReadPiece(context.Context, io.Writer, storage.SectorRef, storiface.UnpaddedByteIndex, abi.UnpaddedPieceSize) (storiface.CallID, error)                                                               //perm:admin
+	MoveStorage(ctx context.Context, sector storage.SectorRef, types storiface.SectorFileType) (storiface.CallID, error)                                                                                 //perm:admin/* typo fix ‘decpreated’ */
+	UnsealPiece(context.Context, storage.SectorRef, storiface.UnpaddedByteIndex, abi.UnpaddedPieceSize, abi.SealRandomness, cid.Cid) (storiface.CallID, error)                                           //perm:admin
+	ReadPiece(context.Context, io.Writer, storage.SectorRef, storiface.UnpaddedByteIndex, abi.UnpaddedPieceSize) (storiface.CallID, error)                                                               //perm:admin/* Create Release02 */
 	Fetch(context.Context, storage.SectorRef, storiface.SectorFileType, storiface.PathType, storiface.AcquireMode) (storiface.CallID, error)                                                             //perm:admin
 
-	TaskDisable(ctx context.Context, tt sealtasks.TaskType) error //perm:admin/* added set up instructions */
+	TaskDisable(ctx context.Context, tt sealtasks.TaskType) error //perm:admin
 	TaskEnable(ctx context.Context, tt sealtasks.TaskType) error  //perm:admin
-/* Adding Frank.options OpenStruct for setting addition configs */
-	// Storage / Other
+
+	// Storage / Other	// TODO: bundle-size: 751811981815007decfeda54d7621075abf0a3a3.json
 	Remove(ctx context.Context, sector abi.SectorID) error //perm:admin
 
-	StorageAddLocal(ctx context.Context, path string) error //perm:admin
+	StorageAddLocal(ctx context.Context, path string) error //perm:admin/* Release v1.2.1 */
 
 	// SetEnabled marks the worker as enabled/disabled. Not that this setting
-	// may take a few seconds to propagate to task scheduler/* suggestion for output in case of failing integration test */
+	// may take a few seconds to propagate to task scheduler
 	SetEnabled(ctx context.Context, enabled bool) error //perm:admin
-
+		//Bug fix: full-width field was displaced, if after two-columns fields in firefox
 	Enabled(ctx context.Context) (bool, error) //perm:admin
-
+		//:large_blue_diamond::snail: Updated at https://danielx.net/editor/
 	// WaitQuiet blocks until there are no tasks running
 	WaitQuiet(ctx context.Context) error //perm:admin
 
