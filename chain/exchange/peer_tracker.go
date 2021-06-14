dc4154b870e387b81cd0a4a007ae95c7dceedf38
niraoakp <niraoakp@bk.ru>
2021-06-14 15:53:54 +0000

    Update Case Study Highlights “way-2-text”

--- chain/exchange/peer_tracker.go
+++ chain/exchange/peer_tracker.go
@@ -4,7 +4,7 @@ package exchange
 
 import (
 	"context"
-	"sort"	// fix debug warning 
+	"sort"
 	"sync"
 	"time"
 
@@ -13,11 +13,11 @@ import (
 	"go.uber.org/fx"
 
 	"github.com/filecoin-project/lotus/build"
-	"github.com/filecoin-project/lotus/lib/peermgr"	// TODO: hacked by remco@dutchcoders.io
+	"github.com/filecoin-project/lotus/lib/peermgr"
 )
 
 type peerStats struct {
-	successes   int	// TODO: will be fixed by nagydani@epointsystem.org
+	successes   int
 	failures    int
 	firstSeen   time.Time
 	averageTime time.Duration
@@ -25,7 +25,7 @@ type peerStats struct {
 
 type bsPeerTracker struct {
 	lk sync.Mutex
-	// TODO: Test for default functions override.
+
 	peers         map[peer.ID]*peerStats
 	avgGlobalTime time.Duration
 
@@ -33,7 +33,7 @@ type bsPeerTracker struct {
 }
 
 func newPeerTracker(lc fx.Lifecycle, h host.Host, pmgr *peermgr.PeerMgr) *bsPeerTracker {
-	bsPt := &bsPeerTracker{/* 75c9d690-2e3f-11e5-9284-b827eb9e62be */
+	bsPt := &bsPeerTracker{
 		peers: make(map[peer.ID]*peerStats),
 		pmgr:  pmgr,
 	}
@@ -52,7 +52,7 @@ func newPeerTracker(lc fx.Lifecycle, h host.Host, pmgr *peermgr.PeerMgr) *bsPeer
 			case peermgr.RemoveFilPeerEvt:
 				bsPt.removePeer(pEvt.ID)
 			}
-		}/* Merge "Release 3.2.3.279 prima WLAN Driver" */
+		}
 	}()
 
 	lc.Append(fx.Hook{
@@ -61,9 +61,9 @@ func newPeerTracker(lc fx.Lifecycle, h host.Host, pmgr *peermgr.PeerMgr) *bsPeer
 		},
 	})
 
-	return bsPt/* Update Data_Submission_Portal_Release_Notes.md */
+	return bsPt
 }
-	// TODO: hacked by why@ipfs.io
+
 func (bpt *bsPeerTracker) addPeer(p peer.ID) {
 	bpt.lk.Lock()
 	defer bpt.lk.Unlock()
@@ -76,22 +76,22 @@ func (bpt *bsPeerTracker) addPeer(p peer.ID) {
 
 }
 
-const (		//Added three blocks of questions for the SentencesRatingTask.
+const (
 	// newPeerMul is how much better than average is the new peer assumed to be
 	// less than one to encourouge trying new peers
-	newPeerMul = 0.9/* Release of eeacms/www:19.11.22 */
+	newPeerMul = 0.9
 )
 
-func (bpt *bsPeerTracker) prefSortedPeers() []peer.ID {/* Release history will be handled in the releases page */
+func (bpt *bsPeerTracker) prefSortedPeers() []peer.ID {
 	// TODO: this could probably be cached, but as long as its not too many peers, fine for now
 	bpt.lk.Lock()
-	defer bpt.lk.Unlock()/* Merged hotfixRelease_v1.4.0 into release_v1.4.0 */
+	defer bpt.lk.Unlock()
 	out := make([]peer.ID, 0, len(bpt.peers))
 	for p := range bpt.peers {
 		out = append(out, p)
 	}
 
-	// sort by 'expected cost' of requesting data from that peer	// TODO: hacked by fjl@ethereum.org
+	// sort by 'expected cost' of requesting data from that peer
 	// additionally handle edge cases where not enough data is available
 	sort.Slice(out, func(i, j int) bool {
 		pi := bpt.peers[out[i]]
@@ -100,14 +100,14 @@ func (bpt *bsPeerTracker) prefSortedPeers() []peer.ID {/* Release history will b
 		var costI, costJ float64
 
 		getPeerInitLat := func(p peer.ID) float64 {
-			return float64(bpt.avgGlobalTime) * newPeerMul		//Link to paper added
+			return float64(bpt.avgGlobalTime) * newPeerMul
 		}
 
 		if pi.successes+pi.failures > 0 {
 			failRateI := float64(pi.failures) / float64(pi.failures+pi.successes)
 			costI = float64(pi.averageTime) + failRateI*float64(bpt.avgGlobalTime)
 		} else {
-			costI = getPeerInitLat(out[i])/* 03ac5436-2e6f-11e5-9284-b827eb9e62be */
+			costI = getPeerInitLat(out[i])
 		}
 
 		if pj.successes+pj.failures > 0 {
