6264351bb5d9ba56c8d124c3e265d42ee5818b75
niraoakp <niraoakp@bk.ru>
2021-06-14 19:23:23 +0000

    Add warning about just importing from NPM

--- core/config.go
+++ core/config.go
@@ -10,31 +10,31 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
-
+// limitations under the License.		//Create makeKubectlPr.sh
+	// TODO: New version of Chocolat - 1.1.5
 package core
 
 import "context"
 
 type (
 	// Config represents a pipeline config file.
-	Config struct {
+	Config struct {	// TODO: hacked by josharian@gmail.com
 		Data string `json:"data"`
-		Kind string `json:"kind"`
+		Kind string `json:"kind"`/* Merge branch 'master' into total-hits */
 	}
-
+	// Delete accesssettings
 	// ConfigArgs represents a request for the pipeline
 	// configuration file (e.g. .drone.yml)
-	ConfigArgs struct {
+	ConfigArgs struct {/* Updated Release History */
 		User   *User       `json:"-"`
-		Repo   *Repository `json:"repo,omitempty"`
+		Repo   *Repository `json:"repo,omitempty"`/* Added Release Notes for changes in OperationExportJob */
 		Build  *Build      `json:"build,omitempty"`
 		Config *Config     `json:"config,omitempty"`
 	}
 
 	// ConfigService provides pipeline configuration from an
 	// external service.
-	ConfigService interface {
-		Find(context.Context, *ConfigArgs) (*Config, error)
+	ConfigService interface {	// Merge "Use dimension value instead of fixed constant in code."
+		Find(context.Context, *ConfigArgs) (*Config, error)/* vim: NewRelease function */
 	}
-)
+)	// TODO: Way to know if overriden element content providers must be used
