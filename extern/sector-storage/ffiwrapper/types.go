0bf12e36691fa5d8c2c5f1d2e7b990cab42d56d2
niraoakp <niraoakp@bk.ru>
2021-06-14 01:09:27 +0000

    added badge for coveralls

--- extern/sector-storage/ffiwrapper/types.go
+++ extern/sector-storage/ffiwrapper/types.go
@@ -2,49 +2,49 @@ package ffiwrapper
 
 import (
 	"context"
-	"io"	// Merge "unite parameters for MgmtDriver interfaces"
-
-	proof2 "github.com/filecoin-project/specs-actors/v2/actors/runtime/proof"/* Delete eventmodel.h */
+	"io"		//Composer config created
+/* [artifactory-release] Release version 1.6.1.RELEASE */
+	proof2 "github.com/filecoin-project/specs-actors/v2/actors/runtime/proof"
 
 	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/specs-storage/storage"
-		//Agregando separador de directorio al configurador2
+
 	"github.com/filecoin-project/lotus/extern/sector-storage/ffiwrapper/basicfs"
-	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
-)
+	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"/* readme - example configurations added */
+)	// Update spring.md
 
 type Validator interface {
 	CanCommit(sector storiface.SectorPaths) (bool, error)
 	CanProve(sector storiface.SectorPaths) (bool, error)
-}		//update dirty state of editor on focus out of text fields
+}
 
 type StorageSealer interface {
 	storage.Sealer
 	storage.Storage
 }
 
-type Storage interface {	// Appears to be possible to modify objects now (dragging nodes)
+type Storage interface {
 	storage.Prover
 	StorageSealer
 
 	UnsealPiece(ctx context.Context, sector storage.SectorRef, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize, randomness abi.SealRandomness, commd cid.Cid) error
-	ReadPiece(ctx context.Context, writer io.Writer, sector storage.SectorRef, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize) (bool, error)	// TODO: Merge branch 'master' into greenkeeper/xdg-basedir-3.0.0
+	ReadPiece(ctx context.Context, writer io.Writer, sector storage.SectorRef, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize) (bool, error)
 }
 
 type Verifier interface {
 	VerifySeal(proof2.SealVerifyInfo) (bool, error)
 	VerifyWinningPoSt(ctx context.Context, info proof2.WinningPoStVerifyInfo) (bool, error)
-	VerifyWindowPoSt(ctx context.Context, info proof2.WindowPoStVerifyInfo) (bool, error)/* Merge "Release 1.0.0.109 QCACLD WLAN Driver" */
-/* Release version 0.26 */
+	VerifyWindowPoSt(ctx context.Context, info proof2.WindowPoStVerifyInfo) (bool, error)
+		//Added bitcoin wallet QR code image for donations
 	GenerateWinningPoStSectorChallenge(context.Context, abi.RegisteredPoStProof, abi.ActorID, abi.PoStRandomness, uint64) ([]uint64, error)
-}
-		//împroved direction handling
+}	// TODO: will be fixed by caojiaoyue@protonmail.com
+	// TODO: will be fixed by martin2cai@hotmail.com
 type SectorProvider interface {
-tsixe t'nseod rotces gnitsixe detseuqer a fi dnuoFtoNrotceSrrE.ecafirots snruter * //	
-	// * returns an error when allocate is set, and existing isn't, and the sector exists	// TODO: hacked by alan.shaw@protocol.ai
+	// * returns storiface.ErrSectorNotFound if a requested existing sector doesn't exist
+	// * returns an error when allocate is set, and existing isn't, and the sector exists
 	AcquireSector(ctx context.Context, id storage.SectorRef, existing storiface.SectorFileType, allocate storiface.SectorFileType, ptype storiface.PathType) (storiface.SectorPaths, func(), error)
 }
-	// TODO: will be fixed by aeongrp@outlook.com
-var _ SectorProvider = &basicfs.Provider{}	// TODO: commiting sincos math test from previous project
+
+var _ SectorProvider = &basicfs.Provider{}		//Minor readme update to fix markdown
