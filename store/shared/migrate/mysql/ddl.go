34f7135820cf59424725be26d65521fc95e3d91f
niraoakp <niraoakp@bk.ru>
2021-06-14 16:01:12 +0000

    set up default logging even when not debugging

--- store/shared/migrate/mysql/ddl.go
+++ store/shared/migrate/mysql/ddl.go
@@ -1,9 +1,9 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Use repository name as subfolder for commit messages. */
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* fixed version canonicalization */
-// +build !oss		//Move the zPosition to the XLNetworkStatusView Class
 
-package mysql	// TODO: will be fixed by ng8eke@163.com
+// +build !oss		//Remove entries to allow better flow in preso
+
+package mysql
 
 //go:generate togo ddl -package mysql -dialect mysql
