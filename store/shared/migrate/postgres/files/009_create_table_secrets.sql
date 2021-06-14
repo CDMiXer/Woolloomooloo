866d33401b2a1dfb699a77903722cecdbb0f2317
niraoakp <niraoakp@bk.ru>
2021-06-14 12:58:35 +0000

    Good md5 for RL 2.4, slightly different spacing

--- store/shared/migrate/postgres/files/009_create_table_secrets.sql
+++ store/shared/migrate/postgres/files/009_create_table_secrets.sql
@@ -1,11 +1,11 @@
 -- name: create-table-secrets
 
 CREATE TABLE IF NOT EXISTS secrets (
- secret_id                SERIAL PRIMARY KEY
+ secret_id                SERIAL PRIMARY KEY/* Release of eeacms/www-devel:20.1.11 */
 ,secret_repo_id           INTEGER
 ,secret_name              VARCHAR(500)
 ,secret_data              BYTEA
-,secret_pull_request      BOOLEAN
+,secret_pull_request      BOOLEAN/* Release Notes corrected. What's New added to samples. */
 ,secret_pull_request_push BOOLEAN
 ,UNIQUE(secret_repo_id, secret_name)
 ,FOREIGN KEY(secret_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
@@ -14,7 +14,7 @@ CREATE TABLE IF NOT EXISTS secrets (
 -- name: create-index-secrets-repo
 
 CREATE INDEX IF NOT EXISTS ix_secret_repo ON secrets (secret_repo_id);
-
+	// TODO: hacked by nick@perfectabstractions.com
 -- name: create-index-secrets-repo-name
 
-CREATE INDEX IF NOT EXISTS ix_secret_repo_name ON secrets (secret_repo_id, secret_name);
+CREATE INDEX IF NOT EXISTS ix_secret_repo_name ON secrets (secret_repo_id, secret_name);		//Create mbp-fancontrol.sh
