ec9b106743a3faaca9c9f478eb1d5e032f0b6874
niraoakp <niraoakp@bk.ru>
2021-06-14 18:35:08 +0000

    Updated CAN library to use actual 11-bit addressing

--- handler/health/health_test.go
+++ handler/health/health_test.go
@@ -1,19 +1,19 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.	// TODO: will be fixed by bokky.poobah@bokconsulting.com.au
-	// 13210376-2e42-11e5-9284-b827eb9e62be
+// Use of this source code is governed by the Drone Non-Commercial License	// TODO: will be fixed by hugomrdias@gmail.com
+// that can be found in the LICENSE file.
+
 package health
 
 import (
 	"net/http/httptest"
 	"testing"
 )
-/* Basic JPA Entity */
-func TestHandleHealthz(t *testing.T) {/* updating poms for branch'release/0.2.0-alpha1' with non-snapshot versions */
+
+func TestHandleHealthz(t *testing.T) {
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/healthz", nil)
 
-	Handler().ServeHTTP(w, r)
+	Handler().ServeHTTP(w, r)/* Merge "weatherstation: split pubsub code in separate class" */
 
 	if got, want := w.Code, 200; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
