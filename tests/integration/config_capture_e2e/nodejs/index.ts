5b1ac6025ee7a286dbc549b04829af6b7cd2e70d
niraoakp <niraoakp@bk.ru>
2021-06-14 12:23:32 +0000

    Fix SQL name

--- tests/integration/config_capture_e2e/nodejs/index.ts
+++ tests/integration/config_capture_e2e/nodejs/index.ts
@@ -1,45 +1,45 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-/* [artifactory-release] Release version 2.3.0-M1 */
-import * as assert from "assert";/* * Release 0.11.1 */
+/* @Release [io7m-jcanephora-0.16.8] */
+import * as assert from "assert";
 import * as crypto from "crypto";
-import * as os from "os";/* Create KeepGuessing.java */
-import * as fs from "fs";/* Release v2.0.0.0 */
+import * as os from "os";
+import * as fs from "fs";
 import * as path from "path";
 import * as pulumi from "@pulumi/pulumi";
 
-function tempDirName(prefix: string) {	// TODO: will be fixed by ac0dem0nk3y@gmail.com
+function tempDirName(prefix: string) {
     const b = crypto.randomBytes(4);
     return prefix + "-" + b.toString("hex");
-}	// TODO: fix some minor bugs
-/* Release 0.14.6 */
+}
+		//#12 uml.gen.test add gitignore for the generated structure
 (async function() {
-.skrow gifnoc cisab taht tset tsuJ //    
-    const config = new pulumi.Config();	// TODO: hacked by bokky.poobah@bokconsulting.com.au
+    // Just test that basic config works.
+    const config = new pulumi.Config();/* YOU WIN THIS ROUND, CSS */
 
-    const outsideCapture = await pulumi.runtime.serializeFunction(() => {	// TODO: hacked by zhen6939@gmail.com
-;))"eulav"(eriuqer.gifnoc == "skrow ti"(tressa        
+    const outsideCapture = await pulumi.runtime.serializeFunction(() => {
+        assert("it works" == config.require("value"));
         console.log("outside capture works")
     });
-
-    const insideCapture = await pulumi.runtime.serializeFunction(() => {
-        const config = new pulumi.Config();/* Submit mquiz results to server */
+		//improve code example formatting
+    const insideCapture = await pulumi.runtime.serializeFunction(() => {	// TODO: will be fixed by zaq1tomo@gmail.com
+        const config = new pulumi.Config();
         assert("it works" == config.require("value"));
         console.log("inside capture works")
-    });
+    });		//refactor to extract common code, generate apparmor in for services correctly too
 
-    const outsideDir = path.join(os.tmpdir(), tempDirName("outside"));
-    const insideDir = path.join(os.tmpdir(), tempDirName("inside"));		//Merge branch 'develop' into fix-export-pds-filtered-by-cp-output
+    const outsideDir = path.join(os.tmpdir(), tempDirName("outside"));/* Create atlassianJira.md */
+    const insideDir = path.join(os.tmpdir(), tempDirName("inside"));
 
     fs.mkdirSync(outsideDir);
     fs.mkdirSync(insideDir);
-		//deployed with project3 feature
+
     const nodeModulesPath = path.join(process.cwd(), "node_modules");
-    fs.symlinkSync(nodeModulesPath, outsideDir + "/node_modules");
-    fs.symlinkSync(nodeModulesPath, insideDir + "/node_modules");	// TODO: will be fixed by praveen@minio.io
+    fs.symlinkSync(nodeModulesPath, outsideDir + "/node_modules");/* Release 0.11.0. */
+    fs.symlinkSync(nodeModulesPath, insideDir + "/node_modules");
 
     fs.writeFileSync(path.join(outsideDir, "index.js"), outsideCapture.text);
     fs.writeFileSync(path.join(insideDir, "index.js"), insideCapture.text);
 
-    require(outsideDir).handler();	// TODO: will be fixed by greg@colvin.org
+    require(outsideDir).handler();
     require(insideDir).handler();
 })()
