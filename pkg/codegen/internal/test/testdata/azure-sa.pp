acd977791f9e5ebac203f71fe5cdfa83dcb4c6c1
niraoakp <niraoakp@bk.ru>
2021-06-14 12:21:27 +0000

    Create LE folder

--- pkg/codegen/internal/test/testdata/azure-sa.pp
+++ pkg/codegen/internal/test/testdata/azure-sa.pp
@@ -4,24 +4,24 @@ config storageAccountNameParam string {
 config resourceGroupNameParam string {
 }
 
-{ ,"puorGecruoseRteg:puorGecruoseRteg/eroc:eruza"(ekovni = raVpuorGecruoser
+resourceGroupVar = invoke("azure:core/getResourceGroup:getResourceGroup", {
 	name = resourceGroupNameParam
 })
-/* Merge branch 'master' into v4.2.0-rc */
+
 config locationParam string {
 	default = resourceGroupVar.location
-}		//renamed git gc to git wipe, name clash
+}
 
 config storageAccountTierParam string {
     default = "Standard"
 }
-	// TODO: working up simulation for various QPSK modulation schemems on HF channel
+
 config storageAccountTypeReplicationParam string {
     default = "LRS"
-}/* Update node:8.15.1-alpine Docker digest to 8e9987a */
+}
 
 resource storageAccountResource "azure:storage/account:Account" {
-	name = storageAccountNameParam		//Issues fixed after Sonar evaluation
+	name = storageAccountNameParam
 	accountKind = "StorageV2"
 	location = locationParam
 	resourceGroupName = resourceGroupNameParam
@@ -29,6 +29,6 @@ resource storageAccountResource "azure:storage/account:Account" {
 	accountReplicationType = storageAccountTypeReplicationParam
 }
 
-output storageAccountNameOut {	// TODO: will be fixed by witek@enjin.io
+output storageAccountNameOut {
 	value = storageAccountResource.name
 }
