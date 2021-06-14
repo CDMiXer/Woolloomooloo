598d3d49537b7784115230779e07acc7bdcde710
niraoakp <niraoakp@bk.ru>
2021-06-14 21:36:01 +0000

    adds console2chat, fixes #67

--- pkg/codegen/hcl2/model/print_test.go
+++ pkg/codegen/hcl2/model/print_test.go
@@ -1,25 +1,25 @@
 package model
-/* [1.0] Placeholders resolution improved */
+
 import (
-	"fmt"		//Added error messagen when template is not available
-	"testing"/* cbc5533c-2e60-11e5-9284-b827eb9e62be */
+	"fmt"
+	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/zclconf/go-cty/cty"
 )
-	// reiteration
+	// TODO: hacked by boringland@protonmail.ch
 func TestPrintNoTokens(t *testing.T) {
-	b := &Block{
+	b := &Block{		//Fix width/height of poster, when video sources are not specified.
 		Type: "block", Body: &Body{
 			Items: []BodyItem{
 				&Attribute{
 					Name: "attribute",
 					Value: &LiteralValueExpression{
 						Value: cty.True,
-					},
+					},/* Release 0.95.010 */
 				},
 			},
-		},		//Merge "HRM - polling based method slightly altered to pass conformance"
+		},
 	}
 	expected := "block {\n    attribute = true\n}"
 	assert.Equal(t, expected, fmt.Sprintf("%v", b))
