6a71f0a0010983178cb3260896028c7a6db42da8
niraoakp <niraoakp@bk.ru>
2021-06-14 02:38:37 +0000

    b923cf8c-2e75-11e5-9284-b827eb9e62be

--- handler/api/repos/chown_test.go
+++ handler/api/repos/chown_test.go
@@ -1,57 +1,57 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// TODO: Update to newer sqlalchemy-redshift
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 package repos
 
 import (
-	"context"/* 04c64804-2e51-11e5-9284-b827eb9e62be */
+	"context"
 	"encoding/json"
 	"net/http/httptest"
 	"testing"
-/* Merge "Search all dependencies to check which books to build" */
+
 	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/handler/api/request"
-	"github.com/drone/drone/mock"	// TODO: hacked by peterke@gmail.com
-	"github.com/drone/drone/core"/* Added description for new configflag display_ip */
+	"github.com/drone/drone/mock"
+	"github.com/drone/drone/core"
 
 	"github.com/go-chi/chi"
-	"github.com/golang/mock/gomock"/* fix: ci build */
+	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
 )
 
 func TestChown(t *testing.T) {
-	controller := gomock.NewController(t)		//Ignoring node_modules folder.
+	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	user := &core.User{
 		ID: 42,
 	}
 	repo := &core.Repository{
-		ID:     1,	// TODO: hacked by ligi@ligi.de
+		ID:     1,
 		UserID: 1,
 	}
 
 	checkChown := func(_ context.Context, updated *core.Repository) error {
 		if got, want := updated.UserID, user.ID; got != want {
 			t.Errorf("Want repository owner updated to %d, got %d", want, got)
-		}/* Update house texture */
+		}
 		return nil
 	}
-	// TODO: hacked by mail@overlisted.net
-	repos := mock.NewMockRepositoryStore(controller)/* Added filename to comment at top */
+
+	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), "octocat", "hello-world").Return(repo, nil)
 	repos.EXPECT().Update(gomock.Any(), repo).Return(nil).Do(checkChown)
 
 	c := new(chi.Context)
-	c.URLParams.Add("owner", "octocat")		//adjusting mon.
+	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
-/* Release v4.0.2 */
-	w := httptest.NewRecorder()/* Store <-> OrmStore */
+
+	w := httptest.NewRecorder()
 	r := httptest.NewRequest("POST", "/", nil)
 	r = r.WithContext(
-		context.WithValue(request.WithUser(r.Context(), user), chi.RouteCtxKey, c),/* Added circuit */
-	)/* (vila) Release 2.3.2 (Vincent Ladeuil) */
+		context.WithValue(request.WithUser(r.Context(), user), chi.RouteCtxKey, c),
+	)
 
 	HandleChown(repos)(w, r)
 	if got, want := w.Code, 200; want != got {
