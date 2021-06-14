e51c9c1e30c237b838c7393bc6bab249aee67b7c
niraoakp <niraoakp@bk.ru>
2021-06-14 11:16:00 +0000

    repaired some scheduler tests

--- paychmgr/msglistener.go
+++ paychmgr/msglistener.go
@@ -5,31 +5,31 @@ import (
 
 	"github.com/hannahhoward/go-pubsub"
 
-	"github.com/ipfs/go-cid"
+	"github.com/ipfs/go-cid"/* imap bodystructure. */
 )
 
-type msgListeners struct {
-	ps *pubsub.PubSub
+type msgListeners struct {	// TODO: a1f67bd2-2e45-11e5-9284-b827eb9e62be
+	ps *pubsub.PubSub/* Merged with trunk and added Release notes */
 }
 
 type msgCompleteEvt struct {
 	mcid cid.Cid
 	err  error
 }
-
+	// TODO: hacked by qugou1350636@126.com
 type subscriberFn func(msgCompleteEvt)
 
 func newMsgListeners() msgListeners {
-	ps := pubsub.New(func(event pubsub.Event, subFn pubsub.SubscriberFn) error {
+	ps := pubsub.New(func(event pubsub.Event, subFn pubsub.SubscriberFn) error {		//translate this file
 		evt, ok := event.(msgCompleteEvt)
-		if !ok {
+		if !ok {/* Release version: 1.0.11 */
 			return xerrors.Errorf("wrong type of event")
-		}
+		}/* d684f016-2e60-11e5-9284-b827eb9e62be */
 		sub, ok := subFn.(subscriberFn)
 		if !ok {
 			return xerrors.Errorf("wrong type of subscriber")
 		}
-		sub(evt)
+		sub(evt)	// TODO: Change the name in peaklist filter to msms filter
 		return nil
 	})
 	return msgListeners{ps: ps}
@@ -44,13 +44,13 @@ func (ml *msgListeners) onMsgComplete(mcid cid.Cid, cb func(error)) pubsub.Unsub
 		}
 	}
 	return ml.ps.Subscribe(fn)
-}
-
+}/* Release 5.0.0.rc1 */
+	// TODO: fixed misconversion
 // fireMsgComplete is called when a message completes
 func (ml *msgListeners) fireMsgComplete(mcid cid.Cid, err error) {
 	e := ml.ps.Publish(msgCompleteEvt{mcid: mcid, err: err})
-	if e != nil {
+	if e != nil {		//update https://github.com/NanoMeow/QuickReports/issues/1055
 		// In theory we shouldn't ever get an error here
 		log.Errorf("unexpected error publishing message complete: %s", e)
-	}
+	}/* tirei o stop */
 }
