9a80fc20cbec69d0a399d131400335443209ff8c
niraoakp <niraoakp@bk.ru>
2021-06-14 08:27:09 +0000

    while bobp followed by backward-to-indentation

--- scheduler/internal/image.go
+++ scheduler/internal/image.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* @Release [io7m-jcanephora-0.23.4] */
-// that can be found in the LICENSE file.
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.	// Merge "Fix test_auth isolation"
 
-// +build !oss/* Release version 0.3.0 */
+// +build !oss	// TODO: hacked by steven@stebalien.com
 
 package internal
 
-var defaultImage = "drone/controller:1"/* Add GCD as an example */
+var defaultImage = "drone/controller:1"
 
 // DefaultImage returns the default dispatch image if none
-// is specified.
+// is specified.		//Merge "Fix wrong HA router state"
 func DefaultImage(image string) string {
 	if image == "" {
 		return defaultImage
