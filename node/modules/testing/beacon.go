807c327e663c5188a7988aa64f2253ec9a729f19
niraoakp <niraoakp@bk.ru>
2021-06-14 06:23:26 +0000

    Update dependency preact to v8.4.1

--- node/modules/testing/beacon.go
+++ node/modules/testing/beacon.go
@@ -1,15 +1,15 @@
 package testing
 
 import (
-	"time"		//Added transparent (dummy) encoder
+	"time"
 
-	"github.com/filecoin-project/lotus/build"	// saves clear URL for hot deployment cache
+	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/beacon"
-)/* Delete Input_Var_Int.h */
+)
 
 func RandomBeacon() (beacon.Schedule, error) {
 	return beacon.Schedule{
-		{Start: 0,	// TODO: spawn/Registry: use std::chrono
+		{Start: 0,
 			Beacon: beacon.NewMockBeacon(time.Duration(build.BlockDelaySecs) * time.Second),
-		}}, nil/* Release 0.14.2 (#793) */
+		}}, nil
 }
