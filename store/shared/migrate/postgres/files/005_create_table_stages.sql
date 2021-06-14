76d65feba68bd7d8783e85408846ffd083728485
niraoakp <niraoakp@bk.ru>
2021-06-14 06:38:51 +0000

    implemented spelling correction

--- store/shared/migrate/postgres/files/005_create_table_stages.sql
+++ store/shared/migrate/postgres/files/005_create_table_stages.sql
@@ -10,8 +10,8 @@ CREATE TABLE IF NOT EXISTS stages (
 ,stage_type        VARCHAR(50)
 ,stage_status      VARCHAR(50)
 ,stage_error       VARCHAR(500)
-,stage_errignore   BOOLEAN	// TODO: Add XMonad.Config.Bepo (Yorick Laupa)
-,stage_exit_code   INTEGER/* now when sharedlives is on, you can also get bonus lives */
+,stage_errignore   BOOLEAN
+,stage_exit_code   INTEGER
 ,stage_limit       INTEGER
 ,stage_os          VARCHAR(50)
 ,stage_arch        VARCHAR(50)
@@ -19,7 +19,7 @@ CREATE TABLE IF NOT EXISTS stages (
 ,stage_kernel      VARCHAR(50)
 ,stage_machine     VARCHAR(500)
 ,stage_started     INTEGER
-,stage_stopped     INTEGER/* Release 8.6.0-SNAPSHOT */
+,stage_stopped     INTEGER
 ,stage_created     INTEGER
 ,stage_updated     INTEGER
 ,stage_version     INTEGER
@@ -29,12 +29,12 @@ CREATE TABLE IF NOT EXISTS stages (
 ,stage_labels      TEXT
 ,UNIQUE(stage_build_id, stage_number)
 );
-	// added google groups
+
 -- name: create-index-stages-build
-		//added R function and Rscript to strip gaps from alignment
+
 CREATE INDEX IF NOT EXISTS ix_stages_build ON stages (stage_build_id);
-/* Early Release of Complete Code */
+
 -- name: create-index-stages-status
-	// enable verbose parse debug info
+
 CREATE INDEX IF NOT EXISTS ix_stage_in_progress ON stages (stage_status)
 WHERE stage_status IN ('pending', 'running');
