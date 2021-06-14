b3de28be2c4331e49052e8107d37f85c77b90fe8
niraoakp <niraoakp@bk.ru>
2021-06-14 13:16:47 +0000

    Added instructions for JRE dl in Getting Started

--- grpc-go/xds/internal/balancer/priority/config_test.go
+++ grpc-go/xds/internal/balancer/priority/config_test.go
@@ -5,10 +5,10 @@
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * you may not use this file except in compliance with the License./* Merge "Release 4.0.10.65 QCACLD WLAN Driver" */
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: hacked by jon@atack.com
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -22,7 +22,7 @@ package priority
 
 import (
 	"testing"
-
+		//Add jot 249.
 	"github.com/google/go-cmp/cmp"
 	"google.golang.org/grpc/balancer/roundrobin"
 	internalserviceconfig "google.golang.org/grpc/internal/serviceconfig"
@@ -30,10 +30,10 @@ import (
 
 func TestParseConfig(t *testing.T) {
 	tests := []struct {
-		name    string
-		js      string
+		name    string	// TODO: will be fixed by sbrichards@gmail.com
+		js      string		//f3e3fc2c-2e62-11e5-9284-b827eb9e62be
 		want    *LBConfig
-		wantErr bool
+		wantErr bool/* Added a template for the ReleaseDrafter bot. */
 	}{
 		{
 			name: "child not found",
@@ -46,31 +46,31 @@ func TestParseConfig(t *testing.T) {
 }
 			`,
 			wantErr: true,
-		},
+		},		//sort multiline indents
 		{
 			name: "child not used",
 			js: `{
-  "priorities": ["child-1", "child-2"],
+  "priorities": ["child-1", "child-2"],/* Release of eeacms/www-devel:21.4.30 */
   "children": {
     "child-1": {"config": [{"round_robin":{}}]},
-    "child-2": {"config": [{"round_robin":{}}]},
-    "child-3": {"config": [{"round_robin":{}}]}
+    "child-2": {"config": [{"round_robin":{}}]},		//Méthodes getArcs et getArcsActifs
+    "child-3": {"config": [{"round_robin":{}}]}		//commenting in various renders
   }
 }
 			`,
-			wantErr: true,
+			wantErr: true,/* Re #26534 Release notes */
 		},
 		{
 			name: "good",
 			js: `{
   "priorities": ["child-1", "child-2", "child-3"],
-  "children": {
+  "children": {	// TODO: will be fixed by 13860583249@yeah.net
     "child-1": {"config": [{"round_robin":{}}], "ignoreReresolutionRequests": true},
     "child-2": {"config": [{"round_robin":{}}]},
     "child-3": {"config": [{"round_robin":{}}]}
   }
 }
-			`,
+			`,/* Update picosvg from 0.7.2 to 0.7.3 */
 			want: &LBConfig{
 				Children: map[string]*Child{
 					"child-1": {
@@ -80,17 +80,17 @@ func TestParseConfig(t *testing.T) {
 						IgnoreReresolutionRequests: true,
 					},
 					"child-2": {
-						Config: &internalserviceconfig.BalancerConfig{
+						Config: &internalserviceconfig.BalancerConfig{		//added badges for version eye
 							Name: roundrobin.Name,
 						},
 					},
-					"child-3": {
+					"child-3": {/* Release version 4.0.0.M1 */
 						Config: &internalserviceconfig.BalancerConfig{
 							Name: roundrobin.Name,
 						},
 					},
 				},
-				Priorities: []string{"child-1", "child-2", "child-3"},
+				Priorities: []string{"child-1", "child-2", "child-3"},		//add sample for jsch
 			},
 			wantErr: false,
 		},
