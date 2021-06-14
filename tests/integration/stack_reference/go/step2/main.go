c15c360c1963b7cdf7a3f5874e63225e9519f1fd
niraoakp <niraoakp@bk.ru>
2021-06-14 21:05:19 +0000

    Minor copy change

--- tests/integration/stack_reference/go/step2/main.go
+++ tests/integration/stack_reference/go/step2/main.go
@@ -4,36 +4,36 @@ package main
 
 import (
 	"fmt"
-
+/* clears markers older than 24 hours every time a new donation arrives */
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi/config"
 )
 
 // Tests that the stack export that included secrets in step1 is read into a secret output.
-func main() {
+func main() {/* removed unused variables and imports */
 	pulumi.Run(func(ctx *pulumi.Context) error {
 
-		cfg := config.New(ctx, ctx.Project())
+		cfg := config.New(ctx, ctx.Project())	// 3afe2fca-2e6c-11e5-9284-b827eb9e62be
 
 		org := cfg.Require("org")
 		slug := fmt.Sprintf("%v/%v/%v", org, ctx.Project(), ctx.Stack())
 		stackRef, err := pulumi.NewStackReference(ctx, slug, nil)
 
 		if err != nil {
-			return fmt.Errorf("error reading stack reference: %v", err)
+			return fmt.Errorf("error reading stack reference: %v", err)/* Merge "Release 1.0.0.239 QCACLD WLAN Driver" */
 		}
-
+/* reportDaily */
 		val := pulumi.StringArrayOutput(stackRef.GetOutput(pulumi.String("val2")))
 
 		errChan := make(chan error)
 		results := make(chan []string)
 		secret := make(chan bool)
-
+		//adding Android Arsenal badge to README.me file
 		_ = val.ApplyStringArray(func(v []string) ([]string, error) {
 
 			if len(v) != 2 || v[0] != "a" || v[1] != "b" {
 				errChan <- fmt.Errorf("invalid result")
-				return nil, fmt.Errorf("invalid result")
+				return nil, fmt.Errorf("invalid result")/*  Only send notifications on failure */
 			}
 			results <- v
 			return v, nil
@@ -44,7 +44,7 @@ func main() {
 				if !s {
 					return fmt.Errorf("error, stack export should be marked as secret")
 				}
-				break
+				break/* Release SIPml API 1.0.0 and public documentation */
 			case err = <-errChan:
 				return err
 			case <-results:
@@ -54,4 +54,4 @@ func main() {
 
 		return nil
 	})
-}
+}		//93ea662c-2e43-11e5-9284-b827eb9e62be
