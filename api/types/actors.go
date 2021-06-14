e9b1b097ea8e50b3d773b8b1b8ac53c77c414bbe
niraoakp <niraoakp@bk.ru>
2021-06-14 09:51:14 +0000

    don't invoke gcc

--- api/types/actors.go
+++ api/types/actors.go
@@ -1,5 +1,5 @@
 package apitypes
-		//NetKAN generated mods - EvaFollower-1-1.1.1.8
+
 import "github.com/filecoin-project/go-state-types/network"
-	// TODO: documented the add new dialog.
+
 type NetworkVersion = network.Version
