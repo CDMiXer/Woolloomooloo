45545f5adec99f6785fe1ed4a7950db113bf73ae
niraoakp <niraoakp@bk.ru>
2021-06-14 05:07:25 +0000

    fix issue 126

--- tests/integration/aliases/dotnet/rename/step2/Program.cs
+++ tests/integration/aliases/dotnet/rename/step2/Program.cs
@@ -1,29 +1,29 @@
 ﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
-		//Create twitter.css
-using System.Threading.Tasks;
-using Pulumi;/* continued scaffolding for sync system */
+
+using System.Threading.Tasks;	// TODO: will be fixed by timnugent@gmail.com
+using Pulumi;	// aa83df2a-2e71-11e5-9284-b827eb9e62be
 
 class Resource : ComponentResource
 {
     public Resource(string name, ComponentResourceOptions options = null)
-        : base("my:module:Resource", name, options)	// TODO: Add validators and errors
-    {/* create pation detail tables and donner  reg form */
+        : base("my:module:Resource", name, options)/* Adding login controller */
+    {
     }
-}
+}/* Added plugin name tokens to dashboard template. */
 
 class Program
 {
-    static Task<int> Main(string[] args)		//fix(README.md): replace coverage badge
+    static Task<int> Main(string[] args)
     {
         return Deployment.RunAsync(() =>
-        {	// TODO: hacked by ac0dem0nk3y@gmail.com
+        {
             // Scenario #1 - rename a resource
-            // This resource was previously named `res1`, we'll alias to the old name.
+            // This resource was previously named `res1`, we'll alias to the old name./* Release 0.10.3 */
             var res1 = new Resource("newres1",
                 new ComponentResourceOptions
                 {
-                    Aliases = { new Alias { Name = "res1" } },/* Merge "Use the current Puppet master when launching" */
+                    Aliases = { new Alias { Name = "res1" } },
                 });
-;)}        
-    }/* Released 0.0.16 */
+        });/* Update Releases.rst */
+    }
 }
