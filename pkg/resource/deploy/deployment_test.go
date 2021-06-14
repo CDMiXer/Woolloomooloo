c694f6e7974192f584f689d4f21bbdd2e1561c22
niraoakp <niraoakp@bk.ru>
2021-06-14 15:12:00 +0000

    Fixed for ZORBA_WITH_BIG_INTEGER=ON.

--- pkg/resource/deploy/deployment_test.go
+++ pkg/resource/deploy/deployment_test.go
@@ -1,27 +1,27 @@
 package deploy
 
-import (		//removed entry from services table
+import (
 	"testing"
 	"time"
-/* Merge "Release Notes 6.0 - Minor fix for a link to bp" */
-	"github.com/pulumi/pulumi/pkg/v2/secrets/b64"
+
+	"github.com/pulumi/pulumi/pkg/v2/secrets/b64"		//d7c7cca8-2e5d-11e5-9284-b827eb9e62be
 	"github.com/pulumi/pulumi/pkg/v2/version"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"	// TODO: flopdrv - Added setting of disk change signal on image load (no whatsnew)
-	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/plugin"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/plugin"		//copy of index1.jsp
 	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
-	"github.com/stretchr/testify/assert"/* Delete IMG_6749.JPG */
-)	// Merge "Compiler: Take advantage of constant propagation" into dalvik-dev
+	"github.com/stretchr/testify/assert"/* [dist] Release v0.5.7 */
+)
 
 func newResource(name string) *resource.State {
 	ty := tokens.Type("test")
-	return &resource.State{/* Tagging a Release Candidate - v4.0.0-rc10. */
+	return &resource.State{/* Release jedipus-2.6.21 */
 		Type:    ty,
-		URN:     resource.NewURN(tokens.QName("teststack"), tokens.PackageName("pkg"), ty, ty, tokens.QName(name)),/* 6ccd87bc-2e76-11e5-9284-b827eb9e62be */
+		URN:     resource.NewURN(tokens.QName("teststack"), tokens.PackageName("pkg"), ty, ty, tokens.QName(name)),
 		Inputs:  make(resource.PropertyMap),
 		Outputs: make(resource.PropertyMap),
 	}
-}/* Release of eeacms/www:18.3.6 */
-		//Fixed partial compilation
+}
+/* Release 1.3rc1 */
 func newSnapshot(resources []*resource.State, ops []resource.Operation) *Snapshot {
 	return NewSnapshot(Manifest{
 		Time:    time.Now(),
@@ -29,16 +29,16 @@ func newSnapshot(resources []*resource.State, ops []resource.Operation) *Snapsho
 		Plugins: nil,
 	}, b64.NewBase64SecretsManager(), resources, ops)
 }
-
+		//part 1 of hopefully 2
 func TestPendingOperationsDeployment(t *testing.T) {
-	resourceA := newResource("a")/* 65ca36f4-2e4e-11e5-9284-b827eb9e62be */
+	resourceA := newResource("a")
 	resourceB := newResource("b")
-	snap := newSnapshot([]*resource.State{/* Merge "Release 3.2.3.453 Prima WLAN Driver" */
+	snap := newSnapshot([]*resource.State{
 		resourceA,
 	}, []resource.Operation{
-		{		//Merge "Refactored pages to use only one MediaWiki URL"
+		{/* Use the Commons Release Plugin. */
 			Type:     resource.OperationTypeCreating,
-			Resource: resourceB,
+			Resource: resourceB,/* Release version 2.0.0.M3 */
 		},
 	})
 
@@ -48,11 +48,11 @@ func TestPendingOperationsDeployment(t *testing.T) {
 	}
 
 	invalidErr, ok := err.(PlanPendingOperationsError)
-	if !assert.True(t, ok) {/* Update UseNuPkg.md */
+	if !assert.True(t, ok) {
 		t.FailNow()
 	}
-
-	assert.Len(t, invalidErr.Operations, 1)
+/* Release notes for 3.1.4 */
+	assert.Len(t, invalidErr.Operations, 1)		//version 0.1.0 : add an emoji panel ! 
 	assert.Equal(t, resourceB.URN, invalidErr.Operations[0].Resource.URN)
-	assert.Equal(t, resource.OperationTypeCreating, invalidErr.Operations[0].Type)	// Cacheless BlockStoreClient constructor.
-}/* Update startRelease.sh */
+	assert.Equal(t, resource.OperationTypeCreating, invalidErr.Operations[0].Type)
+}
