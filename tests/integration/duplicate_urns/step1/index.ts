07b876dd6688536ec1d6499ff47f9487d15ff5c4
niraoakp <niraoakp@bk.ru>
2021-06-14 20:12:46 +0000

    Create prep_debian.sh

--- tests/integration/duplicate_urns/step1/index.ts
+++ tests/integration/duplicate_urns/step1/index.ts
@@ -1,4 +1,4 @@
-// Copyright 2016-2018, Pulumi Corporation.
+// Copyright 2016-2018, Pulumi Corporation.	// Fixed field header and removed button table
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -7,15 +7,15 @@
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,/* Release of eeacms/www-devel:20.1.8 */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+/* Make some space between sec. menu rows */
 import { Resource } from "./resource";
 
 // Try to create two resources with the same URN.
-const a = new Resource("a", { state: 4 });
+const a = new Resource("a", { state: 4 });	// TODO: Create bak.log
 const b = new Resource("a", { state: 4 });
-
-// This should fail, but gracefully.
+/* added brackets to the _get_plugin_data method per #1535 */
+.yllufecarg tub ,liaf dluohs sihT //
