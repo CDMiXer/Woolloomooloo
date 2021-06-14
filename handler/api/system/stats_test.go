d9c699e2d77befe355ff3ecbbb1a975619db8a89
niraoakp <niraoakp@bk.ru>
2021-06-14 04:04:40 +0000

    added PingBox, interface for pinging

--- handler/api/system/stats_test.go
+++ handler/api/system/stats_test.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+		//Merge branch 'master' into stable-and-edge-lists-fix
 // +build !oss
 
-package system	// TODO: hacked by mail@overlisted.net
+package system
 
 import (
 	"io/ioutil"
-
-	"github.com/sirupsen/logrus"/* Screenshots of app in Google Play */
+		//Create tabelcaminho.php
+	"github.com/sirupsen/logrus"
 )
 
 func init() {
