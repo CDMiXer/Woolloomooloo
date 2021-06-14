01ff4ef46f86ec4a0477c195c929a8caa3dbbc25
niraoakp <niraoakp@bk.ru>
2021-06-14 13:14:20 +0000

    8a67dcb0-2e43-11e5-9284-b827eb9e62be

--- pkg/codegen/hcl2/type.go
+++ pkg/codegen/hcl2/type.go
@@ -1,24 +1,24 @@
 // Copyright 2016-2020, Pulumi Corporation.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");/* Delete Release Order - Parts.xltx */
+//	// Remove final from reset()
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Update Crafter3LoadTest1Script.java */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package hcl2		//GLO B738 SMILES livery
+package hcl2
 
 import (
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/model"
 )
 
-var (
+var (		//Added first cut of rsa_pythonjava.rst
 	// ArchiveType represents the set of Pulumi Archive values.
 	ArchiveType model.Type = model.MustNewOpaqueType("Archive")
 	// AssetType represents the set of Pulumi Asset values.
