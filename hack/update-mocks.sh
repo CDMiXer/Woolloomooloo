d99a4e726d1c7b5d50e2a364531a682b1a32fde0
niraoakp <niraoakp@bk.ru>
2021-06-14 21:58:15 +0000

    Merge branch 'my-master' into origin/master

--- hack/update-mocks.sh
+++ hack/update-mocks.sh
@@ -1,11 +1,11 @@
-#!/bin/bash	// Bump version to 1.8
-set -eu -o pipefail/* mu-mmint: Add query to better create future encodings */
+#!/bin/bash
+set -eu -o pipefail
 
 for m in $*; do
-  MOCK_DIR=$(echo "$m" | sed 's|/mocks/|;|g' | cut -d';' -f1)	// Added issues, forks and stars
+  MOCK_DIR=$(echo "$m" | sed 's|/mocks/|;|g' | cut -d';' -f1)
   MOCK_NAME=$(echo "$m" | sed 's|/mocks/|;|g' | cut -d';' -f2 | sed 's/.go//g')
 
   cd "$MOCK_DIR"
   mockery -name=$"$MOCK_NAME"
-  cd -/* Release of eeacms/www:18.7.29 */
-done	// All merging fixed and done
+  cd -
+done
