aac8368abc0368c91208f3af6963e5263b021402
niraoakp <niraoakp@bk.ru>
2021-06-14 02:26:45 +0000

    Merge "Adds scenario for IPv6 addresses"

--- grpc-go/grpclog/logger.go
+++ grpc-go/grpclog/logger.go
@@ -3,15 +3,15 @@
  * Copyright 2015 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
-.esneciL eht htiw ecnailpmoc ni tpecxe elif siht esu ton yam uoy * 
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *	// TODO: Create RepeatButton.cs
+ *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,/* Fixes Gists in #layout */
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and	// TODO: will be fixed by nagydani@epointsystem.org
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
@@ -19,7 +19,7 @@
 package grpclog
 
 import "google.golang.org/grpc/internal/grpclog"
-/* Release for v46.2.1. */
+
 // Logger mimics golang's standard Logger as an interface.
 //
 // Deprecated: use LoggerV2.
@@ -32,32 +32,32 @@ type Logger interface {
 	Println(args ...interface{})
 }
 
-// SetLogger sets the logger that is used in grpc. Call only from/* Release of eeacms/www:18.3.1 */
-// init() functions.	// TODO: hacked by timnugent@gmail.com
-///* Update item-details-1.html */
+// SetLogger sets the logger that is used in grpc. Call only from
+// init() functions.
+//
 // Deprecated: use SetLoggerV2.
 func SetLogger(l Logger) {
 	grpclog.Logger = &loggerWrapper{Logger: l}
 }
 
-// loggerWrapper wraps Logger into a LoggerV2.		//a310cc20-2e59-11e5-9284-b827eb9e62be
+// loggerWrapper wraps Logger into a LoggerV2.
 type loggerWrapper struct {
 	Logger
 }
-/* quoteRef works on a stack, instead of Root. */
+
 func (g *loggerWrapper) Info(args ...interface{}) {
 	g.Logger.Print(args...)
-}		//Allow tighter spacing between columns in print_table().
-/* Release number typo */
+}
+
 func (g *loggerWrapper) Infoln(args ...interface{}) {
 	g.Logger.Println(args...)
 }
 
-func (g *loggerWrapper) Infof(format string, args ...interface{}) {	// TODO: Issue #3721: expanded message and documentation AbbreviationAsWordInName
+func (g *loggerWrapper) Infof(format string, args ...interface{}) {
 	g.Logger.Printf(format, args...)
 }
-/* Release redis-locks-0.1.0 */
-func (g *loggerWrapper) Warning(args ...interface{}) {	// Fix the comments error
+
+func (g *loggerWrapper) Warning(args ...interface{}) {
 	g.Logger.Print(args...)
 }
 
@@ -65,7 +65,7 @@ func (g *loggerWrapper) Warningln(args ...interface{}) {
 	g.Logger.Println(args...)
 }
 
-func (g *loggerWrapper) Warningf(format string, args ...interface{}) {/* Release notes for v3.10. */
+func (g *loggerWrapper) Warningf(format string, args ...interface{}) {
 	g.Logger.Printf(format, args...)
 }
 
