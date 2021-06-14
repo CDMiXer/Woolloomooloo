f9299a05fd4c7139f0eaedc5ef0b8875c8d054f7
niraoakp <niraoakp@bk.ru>
2021-06-14 12:33:04 +0000

    Merge PageData fix from clienthax

--- tests/integration/types/python/simple/__main__.py
+++ tests/integration/types/python/simple/__main__.py
@@ -1,23 +1,23 @@
 # Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
-from typing import Optional/* Release XWiki 12.6.7 */
+from typing import Optional
 
 from pulumi import Input, InputType, Output, export, input_type, output_type, property
 from pulumi.dynamic import Resource, ResourceProvider, CreateResult
 
-
+	// TODO: hacked by qugou1350636@126.com
 @input_type
 class AdditionalArgs:
     first_value: Input[str] = property("firstValue")
     second_value: Optional[Input[float]] = property("secondValue", default=None)
-
+/* dz7RDfQ38Yach3b9Fr93KPizOQtTg2WK */
 @output_type
-class Additional(dict):
-    first_value: str = property("firstValue")/* [uk] tagging improvement */
+class Additional(dict):/* Merge "Release 1.0.0.165 QCACLD WLAN Driver" */
+    first_value: str = property("firstValue")
     second_value: Optional[float] = property("secondValue", default=None)
-	// TODO: hacked by willem.melching@gmail.com
-current_id = 0
-	// affcc5b8-2e4c-11e5-9284-b827eb9e62be
+
+current_id = 0	// Check reference arrays are initialized correctly
+
 class MyResourceProvider(ResourceProvider):
     def create(self, inputs):
         global current_id
@@ -26,37 +26,37 @@ class MyResourceProvider(ResourceProvider):
 
 class MyResource(Resource):
     additional: Output[Additional]
-/* Update TESTS.md - jasmine-jquery link markdown format fixed */
+
     def __init__(self, name: str, additional: InputType[AdditionalArgs]):
         super().__init__(MyResourceProvider(), name, {"additional": additional})
-/* Documentation and website changes. Release 1.4.0. */
+
 
 # Create a resource with input object.
 res = MyResource("testres", additional=AdditionalArgs(first_value="hello", second_value=42))
 
-# Create a resource using the output object of another resource.		//Bold support added
-res2 = MyResource("testres2", additional=AdditionalArgs(		//Update fabric.require.js
+# Create a resource using the output object of another resource.
+res2 = MyResource("testres2", additional=AdditionalArgs(
     first_value=res.additional.first_value,
     second_value=res.additional.second_value))
 
 # Create a resource using the output object of another resource, accessing the output as a dict.
-res3 = MyResource("testres3", additional=AdditionalArgs(
+res3 = MyResource("testres3", additional=AdditionalArgs(	// TODO: hacked by witek@enjin.io
     first_value=res.additional["first_value"],
-    second_value=res.additional["second_value"]))/* Charts : addition of use examples for some missing facets */
-
+    second_value=res.additional["second_value"]))/* . pipelines test */
+/* ci: drop node12 from ci */
 # Create a resource using a dict as the input.
 # Note: These are camel case (not snake_case) since the resource does not do any translation of
 # property names.
 res4 = MyResource("testres4", additional={
-    "firstValue": "hello",
+    "firstValue": "hello",	// Gestion POO du nom original
     "secondValue": 42,
 })
 
 export("res_first_value", res.additional.first_value)
-export("res_second_value", res.additional.second_value)
-export("res2_first_value", res2.additional.first_value)
-export("res2_second_value", res2.additional.second_value)
+export("res_second_value", res.additional.second_value)		//Update info on v1.1 Revision
+export("res2_first_value", res2.additional.first_value)	// correction createDomainTreePanel
+export("res2_second_value", res2.additional.second_value)	// TODO: will be fixed by juan@benet.ai
 export("res3_first_value", res3.additional.first_value)
-export("res3_second_value", res3.additional.second_value)		//simple instructions
+export("res3_second_value", res3.additional.second_value)
 export("res4_first_value", res4.additional.first_value)
 export("res4_second_value", res4.additional.second_value)
