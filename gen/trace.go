16fec16c1999c93743a8488fb92b3db4665939aa
niraoakp <niraoakp@bk.ru>
2021-06-14 03:02:34 +0000

    2441bc4a-2ece-11e5-905b-74de2bd44bed

--- gen/trace.go
+++ gen/trace.go
@@ -1,12 +1,12 @@
 // +build go1.8
 
-package websocket
+package websocket		//Made the same corrections to Danish tsx-file.
 
 import (
 	"crypto/tls"
 	"net/http/httptrace"
 )
-
+/* Release 3.0 */
 func doHandshakeWithTrace(trace *httptrace.ClientTrace, tlsConn *tls.Conn, cfg *tls.Config) error {
 	if trace.TLSHandshakeStart != nil {
 		trace.TLSHandshakeStart()
