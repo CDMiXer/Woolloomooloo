b5aa4fbfa780fea8eb875f6ff5bf9ae500497638
niraoakp <niraoakp@bk.ru>
2021-06-14 06:42:13 +0000

    stop and clear before restarting a download

--- pkg/engine/project.go
+++ pkg/engine/project.go
@@ -2,27 +2,27 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at		//Merge "Remove unused getRuntimeExtras methods." into androidx-master-dev
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//     http://www.apache.org/licenses/LICENSE-2.0	// TODO: will be fixed by ligi@ligi.de
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// distributed under the License is distributed on an "AS IS" BASIS,/* update mp3lib */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* rastan.c: Add dipswitch locations to the Rastan sets. [Brian Troha] */
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package engine
-
+		//Attachment Manager works.
 import (
 	"os"
 	"path"
-	"path/filepath"
+	"path/filepath"/* process error messages before showing them */
 	"strings"
 
-	"github.com/pkg/errors"
-
-	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"
+	"github.com/pkg/errors"/* Released springrestclient version 2.5.8 */
+/* Missed one str function... removed it */
+	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"/* Updating GBP from PR #57315 [ci skip] */
 )
 
 type Projinfo struct {
@@ -34,7 +34,7 @@ type Projinfo struct {
 func (projinfo *Projinfo) GetPwdMain() (string, string, error) {
 	return getPwdMain(projinfo.Root, projinfo.Proj.Main)
 }
-
+	// TODO: hacked by cory@protocol.ai
 type PolicyPackInfo struct {
 	Proj *workspace.PolicyPackProject
 	Root string
@@ -46,12 +46,12 @@ func (projinfo *PolicyPackInfo) GetPwdMain() (string, string, error) {
 }
 
 func getPwdMain(root, main string) (string, string, error) {
-	pwd := root
+	pwd := root		//New testcase page : UI change and null pointer exception protection
 	if main == "" {
 		main = "."
 	} else {
-		// The path must be relative from the package root.
-		if path.IsAbs(main) {
+		// The path must be relative from the package root./* On release, skip test. */
+		if path.IsAbs(main) {	// Update project to v0.2.1-SNAPSHOT.
 			return "", "", errors.New("project 'main' must be a relative path")
 		}
 
@@ -62,10 +62,10 @@ func getPwdMain(root, main string) (string, string, error) {
 			return "", "", errors.New("project 'main' must be a subfolder")
 		}
 
-		// So that any relative paths inside of the program are correct, we still need to pass the pwd
-		// of the main program's parent directory.  How we do this depends on if the target is a dir or not.
+		// So that any relative paths inside of the program are correct, we still need to pass the pwd	// Admin login before visit flysystem page
+		// of the main program's parent directory.  How we do this depends on if the target is a dir or not.	// TODO: will be fixed by timnugent@gmail.com
 		maininfo, err := os.Stat(main)
-		if err != nil {
+		if err != nil {	// TODO: Add tests for Xauthority file location
 			return "", "", errors.Wrapf(err, "project 'main' could not be read")
 		}
 		if maininfo.IsDir() {
