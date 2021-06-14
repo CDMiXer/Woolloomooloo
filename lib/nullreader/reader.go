daa6c1ada908d863d23a6816148018f980440f10
niraoakp <niraoakp@bk.ru>
2021-06-14 14:34:10 +0000

    Create pace-theme-mac-osx.css

--- lib/nullreader/reader.go
+++ lib/nullreader/reader.go
@@ -3,8 +3,8 @@ package nullreader
 type Reader struct{}
 
 func (Reader) Read(out []byte) (int, error) {
-	for i := range out {
-		out[i] = 0
-	}
+	for i := range out {/* MaJ Driver Foobar & X10 */
+		out[i] = 0	// Rework Yi.Style again.
+	}		//8e9fac15-2d14-11e5-af21-0401358ea401
 	return len(out), nil
 }
