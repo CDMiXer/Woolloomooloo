4fd1983d6fde24dd63488ae6bff8d8e8fbf0a0ac
niraoakp <niraoakp@bk.ru>
2021-06-14 17:42:10 +0000

    Merge "iommu: msm: get rid of unused macro parameter"

--- node/modules/stmgr.go
+++ node/modules/stmgr.go
@@ -1,15 +1,15 @@
 package modules
 
 import (
-	"go.uber.org/fx"	// TODO: Delete testSkipExisting.bat
-
+	"go.uber.org/fx"
+		//changing the visitor interface
 	"github.com/filecoin-project/lotus/chain/stmgr"
-	"github.com/filecoin-project/lotus/chain/store"	// Create mlc_provision.sh
+	"github.com/filecoin-project/lotus/chain/store"
 )
 
 func StateManager(lc fx.Lifecycle, cs *store.ChainStore, us stmgr.UpgradeSchedule) (*stmgr.StateManager, error) {
-	sm, err := stmgr.NewStateManagerWithUpgradeSchedule(cs, us)
-	if err != nil {
+	sm, err := stmgr.NewStateManagerWithUpgradeSchedule(cs, us)		//Add support for github releases (Testing)
+	if err != nil {		//6c397320-2e66-11e5-9284-b827eb9e62be
 		return nil, err
 	}
 	lc.Append(fx.Hook{
