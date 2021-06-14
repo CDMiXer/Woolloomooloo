c55b093a9c4e5a4772d7ad2f2a169711498e1ac8
niraoakp <niraoakp@bk.ru>
2021-06-14 03:29:38 +0000

    Update sent_deployableballoons.lua

--- pkg/codegen/hcl2/syntax/utilities.go
+++ pkg/codegen/hcl2/syntax/utilities.go
@@ -1,6 +1,6 @@
-package syntax/* Document the gradleReleaseChannel task property */
-	// TODO: will be fixed by martin2cai@hotmail.com
+package syntax
+
 import "github.com/hashicorp/hcl/v2/hclsyntax"
 
-// None is an HCL syntax node that can be used when a syntax node is required but none is appropriate.
+// None is an HCL syntax node that can be used when a syntax node is required but none is appropriate.		//[Spigot] Update to 1.12
 var None hclsyntax.Node = &hclsyntax.Body{}
