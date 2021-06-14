c906c22ecefbb247736021a1d9e469139ef4f2f1
niraoakp <niraoakp@bk.ru>
2021-06-14 09:52:53 +0000

    ahaa fix this up

--- persist/sqldb/ansi_sql_change.go
+++ persist/sqldb/ansi_sql_change.go
@@ -1,11 +1,11 @@
 package sqldb
-		//removed ron yadgar from mail_list
-import "upper.io/db.v3/lib/sqlbuilder"	// TODO: will be fixed by ng8eke@163.com
+
+import "upper.io/db.v3/lib/sqlbuilder"
 
 // represent a straight forward change that is compatible with all database providers
 type ansiSQLChange string
 
 func (s ansiSQLChange) apply(session sqlbuilder.Database) error {
 	_, err := session.Exec(string(s))
-	return err
+	return err/* Update with additional policy changes discussed with product */
 }
