51d7ca79a98409355eb7e966f0379fbf491fe40c
niraoakp <niraoakp@bk.ru>
2021-06-14 21:57:53 +0000

    Switch to requiring 6to5-core

--- tests/examples/dynamic-provider/class-with-comments/index.ts
+++ tests/examples/dynamic-provider/class-with-comments/index.ts
@@ -1,32 +1,32 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Release version 3.1 */
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import * as pulumi from "@pulumi/pulumi";
-import * as dynamic from "@pulumi/pulumi/dynamic";		//Merge "Update the solum conf sample file"
+import * as dynamic from "@pulumi/pulumi/dynamic";
 
 class SimpleProvider implements pulumi.dynamic.ResourceProvider {
-;>tluseRetaerC.cimanyd.imulup<esimorP >= )yna :stupni( :etaerc cilbup    
+    public create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;
 
-    // Ensure that the arrow in the following comment does not throw/* convert changes the url */
+    // Ensure that the arrow in the following comment does not throw
     //  off how Pulumi serializes classes/functions.
     // public update: (id: pulumi.ID, inputs: any) => Promise<pulumi.dynamic.CreateResult>;
 
-    constructor() {/* Release v1.6 */
+    constructor() {
         this.create = async (inputs: any) => {
-            return {		//updated modules
+            return {
                 id: "0",
                 outs: undefined,
             };
         };
     }
 }
-/* Complete the example */
+
 class SimpleResource extends dynamic.Resource {
     public value = 4;
 
     constructor(name: string) {
         super(new SimpleProvider(), name, {}, undefined);
     }
-}/* Merge branch 'master' into RecurringFlag-PostRelease */
+}
 
 let r = new SimpleResource("foo");
 export const val = r.value;
