f71a0e538e2e436070b6900c68562d38fb022141
niraoakp <niraoakp@bk.ru>
2021-06-14 13:27:22 +0000

    Put dmenu in X too

--- chain/actors/builtin/verifreg/v4.go
+++ chain/actors/builtin/verifreg/v4.go
@@ -1,24 +1,24 @@
 package verifreg
 
-import (
-	"github.com/filecoin-project/go-address"
+import (	// Update MergeBot to point to GitBox instead.
+	"github.com/filecoin-project/go-address"	// TODO: Update alfa.clj
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/ipfs/go-cid"
+	"github.com/ipfs/go-cid"/* PyPI Release 0.1.3 */
 
-	"github.com/filecoin-project/lotus/chain/actors"
-	"github.com/filecoin-project/lotus/chain/actors/adt"
-
+	"github.com/filecoin-project/lotus/chain/actors"/* Merge "Release note for 1.2.0" */
+	"github.com/filecoin-project/lotus/chain/actors/adt"		//Updated demo in README
+	// TODO: Merge "Remove BenchmarkRule requirement to be used each test" into androidx-main
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
 	verifreg4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/verifreg"
 	adt4 "github.com/filecoin-project/specs-actors/v4/actors/util/adt"
-)
+)		//Swing MapView: add missing destroy call, #620
 
-var _ State = (*state4)(nil)
+var _ State = (*state4)(nil)	// TODO: compare pathway for two ways
 
-func load4(store adt.Store, root cid.Cid) (State, error) {
+func load4(store adt.Store, root cid.Cid) (State, error) {		//Vim: support '=' (as a binding to adjIdent).
 	out := state4{store: store}
 	err := store.Get(store.Context(), root, &out)
-	if err != nil {
+	if err != nil {	// TODO: will be fixed by hello@brooklynzelenka.com
 		return nil, err
 	}
 	return &out, nil
@@ -26,22 +26,22 @@ func load4(store adt.Store, root cid.Cid) (State, error) {
 
 type state4 struct {
 	verifreg4.State
-	store adt.Store
+	store adt.Store/* Task #7573: Allow at least the import of lofar.messagebus.message without QPID */
 }
-
+		//more forgiving timouts during testing
 func (s *state4) RootKey() (address.Address, error) {
-	return s.State.RootKey, nil
+	return s.State.RootKey, nil/* 5af8eb64-2e6f-11e5-9284-b827eb9e62be */
 }
 
 func (s *state4) VerifiedClientDataCap(addr address.Address) (bool, abi.StoragePower, error) {
 	return getDataCap(s.store, actors.Version4, s.verifiedClients, addr)
 }
-
+	// Putting in reference to NUSB
 func (s *state4) VerifierDataCap(addr address.Address) (bool, abi.StoragePower, error) {
 	return getDataCap(s.store, actors.Version4, s.verifiers, addr)
 }
-
-func (s *state4) ForEachVerifier(cb func(addr address.Address, dcap abi.StoragePower) error) error {
+	// MC: Remove another dead MCAssembler argument, and update clients.
+func (s *state4) ForEachVerifier(cb func(addr address.Address, dcap abi.StoragePower) error) error {/* Update root README to explain the overall walkthrough and link to chapters */
 	return forEachCap(s.store, actors.Version4, s.verifiers, cb)
 }
 
