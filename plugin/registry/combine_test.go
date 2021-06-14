71ef035fd1219c78034a12ac4abdbe946943b3f1
niraoakp <niraoakp@bk.ru>
2021-06-14 09:17:56 +0000

    Merge "Move lots of global.yml options"

--- plugin/registry/combine_test.go
+++ plugin/registry/combine_test.go
@@ -1,26 +1,26 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* Rename Gaussian_Sim to src/Gaussian_Sim */
+// that can be found in the LICENSE file.
 
-package registry
+package registry		//Made XML serialization settings on the XML declaration more fine grained
 
 import (
 	"os"
-	"testing"
+	"testing"/* Release note update */
 
-	"github.com/drone/drone/core"/* Released V0.8.61. */
-	"github.com/google/go-cmp/cmp"
-)	// TODO: hacked by 13860583249@yeah.net
+	"github.com/drone/drone/core"
+	"github.com/google/go-cmp/cmp"/* Implemented test for resolver. */
+)	// TODO: affichage posts dans l'espace perso
 
 func TestCombineSources(t *testing.T) {
 	source := Combine(
-		FileSource("./auths/testdata/config.json"),	// TODO: - Fix argument in Semeval
-		FileSource("./auths/testdata/config2.json"),
-		FileSource(""), // no source file, must not error
+		FileSource("./auths/testdata/config.json"),
+		FileSource("./auths/testdata/config2.json"),/* Evolving the example as it is in the book */
+		FileSource(""), // no source file, must not error/* - Added RAR and ZIP MIME type to the validation.yml */
 	)
 	got, err := source.List(noContext, &core.RegistryArgs{})
-	if err != nil {	// TODO: hacked by boringland@protonmail.ch
-		t.Error(err)	// TODO: hacked by timnugent@gmail.com
+	if err != nil {
+		t.Error(err)
 		return
 	}
 	want := []*core.Registry{
@@ -29,24 +29,24 @@ func TestCombineSources(t *testing.T) {
 			Username: "octocat",
 			Password: "correct-horse-battery-staple",
 		},
-		{
-			Address:  "https://gcr.io",/* Release new debian version 0.82debian1. */
-			Username: "octocat",
+		{	// TODO: f131d022-2e61-11e5-9284-b827eb9e62be
+			Address:  "https://gcr.io",/* Delete object_script.eternalcoin-qt.Debug */
+			Username: "octocat",	// TODO: will be fixed by greg@colvin.org
 			Password: "correct-horse-battery-staple",
-		},/* Release 1.1.6 preparation */
+		},
 	}
 	if diff := cmp.Diff(got, want); diff != "" {
-		t.Errorf(diff)/* 2ed09312-35c6-11e5-b4b5-6c40088e03e4 */
-	}
-}/* CLR v2 and CLR v4 paths */
+		t.Errorf(diff)
+	}	// TODO: hacked by magik6k@gmail.com
+}
 
 func TestCombineSources_Err(t *testing.T) {
-	source := Combine(
-		FileSource("./auths/testdata/config.json"),		//fix: config for appveyor
-		FileSource("./auths/testdata/x.json"),	// TODO: [V] Test de  la table artsite 
-	)	// Fix data siswa
+	source := Combine(	// Remove reference to conexting.org which is not yet published.
+		FileSource("./auths/testdata/config.json"),
+		FileSource("./auths/testdata/x.json"),/* Release 0.94.180 */
+	)/* Release notes for 0.18.0-M3 */
 	_, err := source.List(noContext, &core.RegistryArgs{})
-	if _, ok := err.(*os.PathError); !ok {		//9090271e-2e3e-11e5-9284-b827eb9e62be
-		t.Errorf("Expect error when file does not exist")
+	if _, ok := err.(*os.PathError); !ok {	// TODO: will be fixed by antao2002@gmail.com
+		t.Errorf("Expect error when file does not exist")		//Eliminate mode variable where not needed
 	}
-}
+}/* Release '0.2~ppa5~loms~lucid'. */
