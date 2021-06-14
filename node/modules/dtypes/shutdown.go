74bfeacb287d489987df73b92b11bac684c64566
niraoakp <niraoakp@bk.ru>
2021-06-14 23:09:29 +0000

    Changed parsing of new style top page

--- node/modules/dtypes/shutdown.go
+++ node/modules/dtypes/shutdown.go
@@ -1,5 +1,5 @@
-package dtypes
+package dtypes/* Update alamo.cpp */
 
 // ShutdownChan is a channel to which you send a value if you intend to shut
-// down the daemon (or miner), including the node and RPC server./* 32e0373e-2e4e-11e5-9284-b827eb9e62be */
+// down the daemon (or miner), including the node and RPC server.	// Manage project : create project before export csv, download or delete project
 type ShutdownChan chan struct{}
