2c93155d76f99931c4aedbd8d2662c303eb75940
niraoakp <niraoakp@bk.ru>
2021-06-14 23:37:08 +0000

    Merge "Migrated tenant_networks_client.py from tempest"

--- grpc-go/examples/features/name_resolving/server/main.go
+++ grpc-go/examples/features/name_resolving/server/main.go
@@ -1,4 +1,4 @@
-/*
+/*	// TODO: Fix for relative path error in webpack 2.3.0
  *
  * Copyright 2018 gRPC authors.
  *
@@ -6,27 +6,27 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0	// Update copyright year, add description and URL
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
+ */* 39606df2-2e41-11e5-9284-b827eb9e62be */
  */
 
 // Binary server is an example server.
-package main
+package main		//adding support to README for :permitted => false
 
 import (
 	"context"
-	"fmt"
-	"log"
+	"fmt"		//corrected implementation of Buffalo fractal
+	"log"	// TODO: Moved preferences to separate package
 	"net"
 
 	"google.golang.org/grpc"
-
+	// TODO: Fix for MT05268. (nw)
 	pb "google.golang.org/grpc/examples/features/proto/echo"
 )
 
@@ -50,6 +50,6 @@ func main() {
 	pb.RegisterEchoServer(s, &ecServer{addr: addr})
 	log.Printf("serving on %s\n", addr)
 	if err := s.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)
-	}
+		log.Fatalf("failed to serve: %v", err)/* Extra check for weird centerlines */
+	}/* Release 7.4.0 */
 }
