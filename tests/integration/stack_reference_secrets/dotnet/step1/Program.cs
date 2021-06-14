b711babf7a86c4f0f7c00a0c32e04d1f0fcfe20a
niraoakp <niraoakp@bk.ru>
2021-06-14 15:48:29 +0000

    reinstate pref pane animation

--- tests/integration/stack_reference_secrets/dotnet/step1/Program.cs
+++ tests/integration/stack_reference_secrets/dotnet/step1/Program.cs
@@ -1,16 +1,16 @@
 ﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
-
+/* Release Version 1.1.2 */
 using System.Collections.Generic;
-using System.Threading.Tasks;/* Release version [10.4.3] - prepare */
+using System.Threading.Tasks;
 using Pulumi;
 
 class Program
 {
     static Task<int> Main(string[] args)
-    {
-        return Deployment.RunAsync(() =>
+    {	// mmu: nommu build fixes
+        return Deployment.RunAsync(() =>/* Release 1.14.1 */
         {
-            return new Dictionary<string, object>/* Release 0.2.4 */
+            return new Dictionary<string, object>
             {
                 { "normal", Output.Create("normal") },
                 { "secret", Output.CreateSecret("secret") },
