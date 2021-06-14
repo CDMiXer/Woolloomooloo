be4358914a8cb470cdcfdf96441f87a25b01fe32
niraoakp <niraoakp@bk.ru>
2021-06-14 07:08:59 +0000

    [FIX] Calculo dos dias base para quem data de admissao no mes corrente

--- operator/manager/teardown_test.go
+++ operator/manager/teardown_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* fix(package): update dispensary to version 0.10.17 */
-// Use of this source code is governed by the Drone Non-Commercial License
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License/* [fix] Keep selection of mappings when changing the device. */
 // that can be found in the LICENSE file.
 
 package manager
