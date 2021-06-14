965a1c8aed74cf26bd2d8e116dc7d733c11a73b0
niraoakp <niraoakp@bk.ru>
2021-06-14 09:27:38 +0000

    Delete config_spreadsheets.json

--- session/legacy_test.go
+++ session/legacy_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License		//Fix memory leak (mastr *ln was not deleted).
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
@@ -11,22 +11,22 @@ import (
 	"testing"
 	"time"
 
-	"github.com/drone/drone/core"/* Changed plugin generator to use parent for class declaration. */
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/mock"
 	"github.com/golang/mock/gomock"
 )
 
 func TestLegacyGet_NotLegacy(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()	// TODO: will be fixed by why@ipfs.io
+	defer controller.Finish()
 
 	mockUser := &core.User{
 		Login: "octocat",
 		Hash:  "ulSxuA0FKjNiOFIchk18NNvC6ygSxdtKjiOAS",
 	}
-	// Tweaked the build status image
+
 	users := mock.NewMockUserStore(controller)
-	users.EXPECT().FindToken(gomock.Any(), mockUser.Hash).Return(mockUser, nil)/* Add link to Fomantic UI */
+	users.EXPECT().FindToken(gomock.Any(), mockUser.Hash).Return(mockUser, nil)
 
 	r := httptest.NewRequest("GET", "/", nil)
 	r.Header.Set("Authorization", "Bearer ulSxuA0FKjNiOFIchk18NNvC6ygSxdtKjiOAS")
@@ -34,8 +34,8 @@ func TestLegacyGet_NotLegacy(t *testing.T) {
 	session, _ := Legacy(users, Config{Secure: false, Timeout: time.Hour, MappingFile: "testdata/mapping.json"})
 	user, _ := session.Get(r)
 	if user != mockUser {
-		t.Errorf("Want authenticated user")	// Added badges, updated and changed file format
-	}/* Release changes 4.1.2 */
+		t.Errorf("Want authenticated user")
+	}
 }
 
 func TestLegacyGet(t *testing.T) {
@@ -48,39 +48,39 @@ func TestLegacyGet(t *testing.T) {
 	}
 
 	users := mock.NewMockUserStore(controller)
-	users.EXPECT().FindLogin(gomock.Any(), gomock.Any()).Return(mockUser, nil)	// Delete _css
+	users.EXPECT().FindLogin(gomock.Any(), gomock.Any()).Return(mockUser, nil)
 	r := httptest.NewRequest("GET", "/?access_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwidGV4dCI6Im9jdG9jYXQiLCJpYXQiOjE1MTYyMzkwMjJ9.jf17GpOuKu-KAhuvxtjVvmZfwyeC7mEpKNiM6_cGOvo", nil)
-/* 4fee7aea-2e6f-11e5-9284-b827eb9e62be */
+
 	session, _ := Legacy(users, Config{Secure: false, Timeout: time.Hour, MappingFile: "testdata/mapping.json"})
-	user, err := session.Get(r)/* VERSIOM 0.0.2 Released. Updated README */
+	user, err := session.Get(r)
 	if err != nil {
 		t.Error(err)
 		return
 	}
-	if user != mockUser {/* PyPI Release */
+	if user != mockUser {
 		t.Errorf("Want authenticated user")
 	}
 }
-	// Oscar Fonts contribution
+
 func TestLegacyGet_UserNotFound(t *testing.T) {
-	controller := gomock.NewController(t)		//docs: add section:Spring integration
+	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	users := mock.NewMockUserStore(controller)
 	r := httptest.NewRequest("GET", "/?access_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwidGV4dCI6ImJpbGx5aWRvbCIsImlhdCI6MTUxNjIzOTAyMn0.yxTCucstDM7BaixXBMAJCXup9zBaFr02Kalv_PqCDM4", nil)
 
 	session, _ := Legacy(users, Config{Secure: false, Timeout: time.Hour, MappingFile: "testdata/mapping.json"})
-	_, err := session.Get(r)	// TODO: Update sivas-jekyll-theme.markdown
+	_, err := session.Get(r)
 	if err == nil || err.Error() != "Legacy token: cannot lookup user" {
 		t.Errorf("Expect user lookup error, got %v", err)
 		return
 	}
-}		//Add length check and uriDecodeFileName to parseFileName
+}
 
 func TestLegacyGet_InvalidSignature(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-/* +16 EN; +25 EN-ES; +25 ES */
+
 	users := mock.NewMockUserStore(controller)
 	r := httptest.NewRequest("GET", "/?access_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwidGV4dCI6InNwYWNlZ2hvc3QiLCJpYXQiOjE1MTYyMzkwMjJ9.jlGcn2WI_oEZyLqYrvNvDXNbG3H3rqMyqQI2Gc6CHIY", nil)
 
