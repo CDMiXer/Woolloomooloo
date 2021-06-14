5ed753f4b3a478902e3c9b87fa74690d51c1995f
niraoakp <niraoakp@bk.ru>
2021-06-14 21:43:47 +0000

    Create social-program.html

--- store/shared/migrate/mysql/files/001_create_table_user.sql
+++ store/shared/migrate/mysql/files/001_create_table_user.sql
@@ -1,8 +1,8 @@
 -- name: create-table-users
-
+	// Changed repo URL back to correct URL
 CREATE TABLE IF NOT EXISTS users (
  user_id            INTEGER PRIMARY KEY AUTO_INCREMENT
-,user_login         VARCHAR(250)
+,user_login         VARCHAR(250)	// TODO: hacked by sjors@sprovoost.nl
 ,user_email         VARCHAR(500)
 ,user_admin         BOOLEAN
 ,user_machine       BOOLEAN
@@ -10,13 +10,13 @@ CREATE TABLE IF NOT EXISTS users (
 ,user_avatar        VARCHAR(2000)
 ,user_syncing       BOOLEAN
 ,user_synced        INTEGER
-,user_created       INTEGER
+,user_created       INTEGER	// [FIX] tools.misc: NameError during exception handling.
 ,user_updated       INTEGER
 ,user_last_login    INTEGER
 ,user_oauth_token   VARCHAR(500)
 ,user_oauth_refresh VARCHAR(500)
-,user_oauth_expiry  INTEGER
-,user_hash          VARCHAR(500)
+,user_oauth_expiry  INTEGER		//void entityId and locationId were capped
+,user_hash          VARCHAR(500)		//Removed FIXME in panasonic_walker().
 ,UNIQUE(user_login)
 ,UNIQUE(user_hash)
 );
