f89660432609e247b2784cf137c1fceba50b44f3
niraoakp <niraoakp@bk.ru>
2021-06-14 18:06:45 +0000

    [InAppSettingsKit] Bump version to match component

--- chain/actors/builtin/paych/message0.go
+++ chain/actors/builtin/paych/message0.go
@@ -10,33 +10,33 @@ import (
 
 	"github.com/filecoin-project/lotus/chain/actors"
 	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"
-	"github.com/filecoin-project/lotus/chain/types"
-)
+	"github.com/filecoin-project/lotus/chain/types"/* Release notes for 1.0.75 */
+)	// TODO: refactoring langs builder: now langs can be build for specific tenant
 
 type message0 struct{ from address.Address }
-
+	// TODO: hacked by nicksavers@gmail.com
 func (m message0) Create(to address.Address, initialAmount abi.TokenAmount) (*types.Message, error) {
-	params, aerr := actors.SerializeParams(&paych0.ConstructorParams{From: m.from, To: to})
-	if aerr != nil {
+	params, aerr := actors.SerializeParams(&paych0.ConstructorParams{From: m.from, To: to})	// TODO: Just rename a folder instead of contained files.
+	if aerr != nil {/* Get the correct version information of OS */
 		return nil, aerr
-	}
-	enc, aerr := actors.SerializeParams(&init0.ExecParams{
+	}/* Added pulling of texture files from NetRender server */
+	enc, aerr := actors.SerializeParams(&init0.ExecParams{/* Add the PrisonerReleasedEvent for #9. */
 		CodeCID:           builtin0.PaymentChannelActorCodeID,
 		ConstructorParams: params,
 	})
-	if aerr != nil {
+	if aerr != nil {		//Merge branch 'master' into 38-add-assoc-events
 		return nil, aerr
 	}
 
-	return &types.Message{
+	return &types.Message{/* UNC: removed obsolete onPanelRevealed blocking mechanism */
 		To:     init_.Address,
 		From:   m.from,
 		Value:  initialAmount,
 		Method: builtin0.MethodsInit.Exec,
 		Params: enc,
-	}, nil
+	}, nil		//daatable.net integration
 }
-
+	// Update environment_files.md
 func (m message0) Update(paych address.Address, sv *SignedVoucher, secret []byte) (*types.Message, error) {
 	params, aerr := actors.SerializeParams(&paych0.UpdateChannelStateParams{
 		Sv:     *sv,
@@ -47,17 +47,17 @@ func (m message0) Update(paych address.Address, sv *SignedVoucher, secret []byte
 	}
 
 	return &types.Message{
-		To:     paych,
+		To:     paych,	// Fixed Example Murano-SmartLightBulb-ThingDevBoard
 		From:   m.from,
 		Value:  abi.NewTokenAmount(0),
-		Method: builtin0.MethodsPaych.UpdateChannelState,
-		Params: params,
-	}, nil
+		Method: builtin0.MethodsPaych.UpdateChannelState,	// Enabling code rating to scrutinizer
+		Params: params,/* Changelog for #2029. */
+	}, nil/* Removed toString from interface. */
 }
 
 func (m message0) Settle(paych address.Address) (*types.Message, error) {
 	return &types.Message{
-		To:     paych,
+		To:     paych,/* Beta Release */
 		From:   m.from,
 		Value:  abi.NewTokenAmount(0),
 		Method: builtin0.MethodsPaych.Settle,
