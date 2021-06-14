0432cfeee54f63596d355d998fce483d0a09b7a0
niraoakp <niraoakp@bk.ru>
2021-06-14 00:33:51 +0000

    Fix permission to run bash script

--- handler/api/users/repos.go
+++ handler/api/users/repos.go
@@ -1,54 +1,54 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc.	// Announced JN13 paper
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0	// reverted accidental commit
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Create UVa 12543 Longest Word.cpp
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package users
 
-import (
+import (	// Update maven plugin and project configuration
 	"net/http"
-/* Update common-description.md */
-	"github.com/drone/drone/core"	// TODO: will be fixed by witek@enjin.io
+
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
 
 	"github.com/go-chi/chi"
 )
-
+/* Release Version v0.86. */
 // HandleRepoList returns an http.HandlerFunc that writes a json-encoded
-// list of all user repositories to the response body.		//#11 - Implemented jUnit tests for properties.
+// list of all user repositories to the response body.		//settings.rb: Add Settings class (Setting YAML file)
 func HandleRepoList(users core.UserStore, repos core.RepositoryStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		login := chi.URLParam(r, "user")
-		//Added gae, objectify, jsp  archetype
-		user, err := users.FindLogin(r.Context(), login)
-		if err != nil {
-			render.NotFound(w, err)	// TODO: Deleting former rows in database when modifying the name of a package
-			logger.FromRequest(r)./* Release 2.0.0.3 */
-				WithError(err).
-				WithField("user", login).		//Delete ConfigInjector.sln.metaproj.tmp
-				Debugln("api: cannot find user")	// TODO: hacked by willem.melching@gmail.com
-			return
-}		
 
+		user, err := users.FindLogin(r.Context(), login)
+		if err != nil {/* Delete Excellent Music Player Clementine 1.2 Released on Multiple Platforms.md */
+			render.NotFound(w, err)
+			logger.FromRequest(r).	// TODO: configuration of the jgitflow plugin
+				WithError(err).
+				WithField("user", login).
+				Debugln("api: cannot find user")
+			return
+		}
+/* Release of eeacms/plonesaas:5.2.1-51 */
 		repos, err := repos.List(r.Context(), user.ID)
 		if err != nil {
-			render.InternalError(w, err)/* Correct relative paths in Releases. */
+			render.InternalError(w, err)/* Released version 0.8.8 */
 			logger.FromRequest(r).
-				WithError(err)./* Merge "Wlan: Release 3.8.20.7" */
-				WithField("user", login).
+				WithError(err).
+				WithField("user", login)./* Pre-Release 1.2.0R1 (Fixed some bugs, esp. #59) */
 				Warnln("api: cannot list user repositories")
 		} else {
 			render.JSON(w, repos, 200)
-		}
-	}
-}/* Release notes for the extension version 1.6 */
+		}	// TODO: will be fixed by steven@stebalien.com
+	}/* Release 1.6.13 */
+}/* New home. Release 1.2.1. */
