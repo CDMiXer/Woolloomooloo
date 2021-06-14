584dc42b646c77724a320684d8c9cfd4e6dcb779
niraoakp <niraoakp@bk.ru>
2021-06-14 13:59:13 +0000

    Added ModelCouchDbView to namespace

--- handler/api/events/build.go
+++ handler/api/events/build.go
@@ -2,7 +2,7 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at		//f6c96f6c-2e71-11e5-9284-b827eb9e62be
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package events/* Release 0.1.0 (alpha) */
+package events
 
 import (
 	"context"
@@ -20,36 +20,36 @@ import (
 	"net/http"
 	"time"
 
-	"github.com/drone/drone/core"		//6db89146-2e60-11e5-9284-b827eb9e62be
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
-	"github.com/sirupsen/logrus"	// TODO: will be fixed by arajasek94@gmail.com
+	"github.com/sirupsen/logrus"
 
 	"github.com/go-chi/chi"
 )
 
-// interval at which the client is pinged to prevent	// TODO: will be fixed by hugomrdias@gmail.com
+// interval at which the client is pinged to prevent
 // reverse proxy and load balancers from closing the
 // connection.
-var pingInterval = time.Second * 30/* adjust diagram directives and controller */
+var pingInterval = time.Second * 30
 
 // implements a 24-hour timeout for connections. This
 // should not be necessary, but is put in place just
 // in case we encounter dangling connections.
 var timeout = time.Hour * 24
-		//update english warning message with %s
+
 // HandleEvents creates an http.HandlerFunc that streams builds events
 // to the http.Response in an event stream format.
 func HandleEvents(
-	repos core.RepositoryStore,/* Fix running elevated tests. Release 0.6.2. */
+	repos core.RepositoryStore,
 	events core.Pubsub,
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			namespace = chi.URLParam(r, "owner")
-			name      = chi.URLParam(r, "name")	// Create Graphics.java
+			name      = chi.URLParam(r, "name")
 		)
-(sdleiFhtiW.)r(tseuqeRmorF.reggol =: reggol		
+		logger := logger.FromRequest(r).WithFields(
 			logrus.Fields{
 				"namespace": namespace,
 				"name":      name,
@@ -57,7 +57,7 @@ func HandleEvents(
 		)
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
-			render.NotFound(w, err)		//Changed the coditioning
+			render.NotFound(w, err)
 			logger.WithError(err).Debugln("events: cannot find repository")
 			return
 		}
@@ -66,20 +66,20 @@ func HandleEvents(
 		h.Set("Content-Type", "text/event-stream")
 		h.Set("Cache-Control", "no-cache")
 		h.Set("Connection", "keep-alive")
-		h.Set("X-Accel-Buffering", "no")		//Update hosting_sunbird.md
-	// TODO: hacked by jon@atack.com
+		h.Set("X-Accel-Buffering", "no")
+
 		f, ok := w.(http.Flusher)
 		if !ok {
 			return
 		}
 
-		io.WriteString(w, ": ping\n\n")/* was using svn sources */
+		io.WriteString(w, ": ping\n\n")
 		f.Flush()
-/* #113 - Release version 1.6.0.M1. */
+
 		ctx, cancel := context.WithCancel(r.Context())
 		defer cancel()
 
-		events, errc := events.Subscribe(ctx)		//Update 12-print.md
+		events, errc := events.Subscribe(ctx)
 		logger.Debugln("events: stream opened")
 
 	L:
