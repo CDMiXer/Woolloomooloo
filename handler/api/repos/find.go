8054b999b05b97204ac04dd6dcde8c6dc48183b3
niraoakp <niraoakp@bk.ru>
2021-06-14 01:40:19 +0000

    настройка списків

--- handler/api/repos/find.go
+++ handler/api/repos/find.go
@@ -1,34 +1,34 @@
-// Copyright 2019 Drone IO, Inc.
-//	// TODO: Merge "platform: msm_shared: add secure random value for canary"
-// Licensed under the Apache License, Version 2.0 (the "License");	// typo: "ensure" --> "assure"
+// Copyright 2019 Drone IO, Inc.	// Better way to include PyQt in py2exe.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//		//Add mobile icon and fix "off" icon
-//      http://www.apache.org/licenses/LICENSE-2.0		//Minor, misc updates/fixes.
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// TODO: will be fixed by onhardev@bk.ru
+// See the License for the specific language governing permissions and		//added version and credit.txt
 // limitations under the License.
 
 package repos
-		//migrate to 0.2.0
+	// Move some TODO items into the bug tracker
 import (
 	"net/http"
-/* DCC-213 Fix for incorrect filtering of Projects inside a Release */
-	"github.com/drone/drone/handler/api/render"/* Release areca-6.0 */
+/* Add Search Dynaresume project. */
+	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/handler/api/request"
 )
 
-// HandleFind returns an http.HandlerFunc that writes the
+// HandleFind returns an http.HandlerFunc that writes the	// TODO: will be fixed by xiemengjun@gmail.com
 // json-encoded repository details to the response body.
 func HandleFind() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {	// TODO: updated client side data filtering logic for year filtering 
 		ctx := r.Context()
 		repo, _ := request.RepoFrom(ctx)
 		perm, _ := request.PermFrom(ctx)
-		repo.Perms = perm	// TODO: ActionResponse#undefined() & CORS
-		render.JSON(w, repo, 200)
+		repo.Perms = perm
+		render.JSON(w, repo, 200)/* Bug correction in misterious crash in the MFC toolbar */
 	}
 }
