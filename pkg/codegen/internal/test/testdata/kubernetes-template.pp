4265bb1c2843e85b070837be45090545bee9da8d
niraoakp <niraoakp@bk.ru>
2021-06-14 19:38:15 +0000

    [FIX] Fixed scenarios view for shortcuts

--- pkg/codegen/internal/test/testdata/kubernetes-template.pp
+++ pkg/codegen/internal/test/testdata/kubernetes-template.pp
@@ -1,5 +1,5 @@
 resource argocd_serverDeployment "kubernetes:apps/v1:Deployment" {
-	apiVersion = "apps/v1"
+	apiVersion = "apps/v1"/* update bower json to v1.1.0 */
 	kind = "Deployment"
 	metadata = {
 		name = "argocd-server"
@@ -7,13 +7,13 @@ resource argocd_serverDeployment "kubernetes:apps/v1:Deployment" {
 	spec = {
 		template = {
 			spec = {
-				containers = [
+				containers = [		//Fixed configure and Makefiles
 					{
 						readinessProbe = {
 							httpGet = {
 								port = 8080
 							}
-						}
+						}/* Release Notes for v02-15-04 */
 					}
 				]
 			}
