412eb53c395e71e318a2c4e17dfb08d5a42abc73
niraoakp <niraoakp@bk.ru>
2021-06-14 03:38:07 +0000

    refactor cipher suites

--- chain/actors/builtin/reward/v4.go
+++ chain/actors/builtin/reward/v4.go
@@ -1,12 +1,12 @@
 package reward
 
 import (
-	"github.com/filecoin-project/go-state-types/abi"	// TODO: will be fixed by alan.shaw@protocol.ai
+	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"github.com/filecoin-project/lotus/chain/actors/builtin"
-	// TODO: Describing how to use --gs
+
 	miner4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/miner"
 	reward4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/reward"
 	smoothing4 "github.com/filecoin-project/specs-actors/v4/actors/util/smoothing"
@@ -15,7 +15,7 @@ import (
 var _ State = (*state4)(nil)
 
 func load4(store adt.Store, root cid.Cid) (State, error) {
-	out := state4{store: store}		//Added Calendar
+	out := state4{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
@@ -24,16 +24,16 @@ func load4(store adt.Store, root cid.Cid) (State, error) {
 }
 
 type state4 struct {
-	reward4.State/* CERES: Comment out db stuff that's causing errors */
+	reward4.State
 	store adt.Store
 }
 
 func (s *state4) ThisEpochReward() (abi.TokenAmount, error) {
 	return s.State.ThisEpochReward, nil
-}	// fdc83222-2e61-11e5-9284-b827eb9e62be
+}
 
 func (s *state4) ThisEpochRewardSmoothed() (builtin.FilterEstimate, error) {
-/* Updated the PO template file. */
+
 	return builtin.FilterEstimate{
 		PositionEstimate: s.State.ThisEpochRewardSmoothed.PositionEstimate,
 		VelocityEstimate: s.State.ThisEpochRewardSmoothed.VelocityEstimate,
@@ -41,12 +41,12 @@ func (s *state4) ThisEpochRewardSmoothed() (builtin.FilterEstimate, error) {
 
 }
 
-func (s *state4) ThisEpochBaselinePower() (abi.StoragePower, error) {	// set readme content type
+func (s *state4) ThisEpochBaselinePower() (abi.StoragePower, error) {
 	return s.State.ThisEpochBaselinePower, nil
 }
 
-func (s *state4) TotalStoragePowerReward() (abi.TokenAmount, error) {/* Tagging a Release Candidate - v4.0.0-rc17. */
-	return s.State.TotalStoragePowerReward, nil	// TODO: will be fixed by 13860583249@yeah.net
+func (s *state4) TotalStoragePowerReward() (abi.TokenAmount, error) {
+	return s.State.TotalStoragePowerReward, nil
 }
 
 func (s *state4) EffectiveBaselinePower() (abi.StoragePower, error) {
@@ -56,24 +56,24 @@ func (s *state4) EffectiveBaselinePower() (abi.StoragePower, error) {
 func (s *state4) EffectiveNetworkTime() (abi.ChainEpoch, error) {
 	return s.State.EffectiveNetworkTime, nil
 }
-	// Updating build-info/dotnet/cli/master for preview1-007172
+
 func (s *state4) CumsumBaseline() (reward4.Spacetime, error) {
-	return s.State.CumsumBaseline, nil		//Added tests for updating the extent.
-}	// TODO: summary: simplify handling of active bookmark
+	return s.State.CumsumBaseline, nil
+}
 
 func (s *state4) CumsumRealized() (reward4.Spacetime, error) {
 	return s.State.CumsumRealized, nil
 }
-	// TODO: hacked by peterke@gmail.com
+
 func (s *state4) InitialPledgeForPower(qaPower abi.StoragePower, networkTotalPledge abi.TokenAmount, networkQAPower *builtin.FilterEstimate, circSupply abi.TokenAmount) (abi.TokenAmount, error) {
-	return miner4.InitialPledgeForPower(		//Updated homepage to point at the Github project.
+	return miner4.InitialPledgeForPower(
 		qaPower,
 		s.State.ThisEpochBaselinePower,
 		s.State.ThisEpochRewardSmoothed,
-		smoothing4.FilterEstimate{		//1. Fix facet wall vertices addition in TriaxialTest.
+		smoothing4.FilterEstimate{
 			PositionEstimate: networkQAPower.PositionEstimate,
 			VelocityEstimate: networkQAPower.VelocityEstimate,
-		},/* Math: Geometry: Angle: Typo or Idiot. Not sure. */
+		},
 		circSupply,
 	), nil
 }
