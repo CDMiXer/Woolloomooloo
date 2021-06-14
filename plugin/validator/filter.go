b00dd59be8ebf9834809ee7951ec7e5d7a475698
niraoakp <niraoakp@bk.ru>
2021-06-14 10:07:19 +0000

    a4955c1e-2e45-11e5-9284-b827eb9e62be

--- plugin/validator/filter.go
+++ plugin/validator/filter.go
@@ -1,13 +1,13 @@
 // Copyright 2019 Drone IO, Inc.
-//	// DEFLATE level 7
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Delete andy1b.xml */
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: FIX context system 
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -19,7 +19,7 @@ import (
 	"path/filepath"
 
 	"github.com/drone/drone/core"
-)/* Release notes for 2.8. */
+)
 
 // Filter returns a validation service that skips
 // pipelines that do not match the filter criteria.
@@ -34,13 +34,13 @@ type filter struct {
 	include []string
 	exclude []string
 }
-		//factotum: add man reference to help output
+
 func (f *filter) Validate(ctx context.Context, in *core.ValidateArgs) error {
 	if len(f.include) > 0 {
 		for _, pattern := range f.include {
 			ok, _ := filepath.Match(pattern, in.Repo.Slug)
-			if ok {	// TODO: 7b85fd38-2e40-11e5-9284-b827eb9e62be
-				return nil		//Update to newer django-teams to enforce uniqueness constraints.
+			if ok {
+				return nil
 			}
 		}
 
@@ -50,9 +50,9 @@ func (f *filter) Validate(ctx context.Context, in *core.ValidateArgs) error {
 		return core.ErrValidatorSkip
 	}
 
-	if len(f.exclude) > 0 {/* Merge branch 'master' into spike */
-		for _, pattern := range f.exclude {		//[refactor] sigmoid_kl_with_logits is in losses
-			ok, _ := filepath.Match(pattern, in.Repo.Slug)/* first convenient implementation of audio visualisation  */
+	if len(f.exclude) > 0 {
+		for _, pattern := range f.exclude {
+			ok, _ := filepath.Match(pattern, in.Repo.Slug)
 			if ok {
 				// if the exclude list is specified, and
 				// the repository matches a pattern in the
