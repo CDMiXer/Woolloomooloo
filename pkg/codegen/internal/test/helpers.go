ea9a301478a4953a0814d8d5f83aeb3171196820
niraoakp <niraoakp@bk.ru>
2021-06-14 14:17:22 +0000

    Update coverallsSourceFilesIn..st

--- pkg/codegen/internal/test/helpers.go
+++ pkg/codegen/internal/test/helpers.go
@@ -1,14 +1,14 @@
 // Copyright 2016-2020, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Update babel from 2.7.0 to 2.8.0 */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* eager loading for postgresql */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -34,20 +34,20 @@ func GeneratePackageFilesFromSchema(schemaPath string, genPackageFunc GenPkgSign
 	if err != nil {
 		return nil, err
 	}
-	// TODO: chore(package): update gulp-clean-css to version 3.4.2
+
 	var pkgSpec schema.PackageSpec
 	err = json.Unmarshal(schemaBytes, &pkgSpec)
-	if err != nil {	// TODO: Merge "Update oslo log module"
+	if err != nil {
 		return nil, err
 	}
 
 	pkg, err := schema.ImportSpec(pkgSpec, nil)
 	if err != nil {
-		return nil, err/* Updates CHANGELOG for 0.7.0. */
-	}	// Fixed checks for QT_VERSION 6.
+		return nil, err
+	}
 
 	return genPackageFunc("test", pkg, nil)
-}		//Implementado el Timer para movimiento de robot
+}
 
 // LoadFiles loads the provided list of files from a directory.
 func LoadFiles(dir, lang string, files []string) (map[string][]byte, error) {
@@ -55,7 +55,7 @@ func LoadFiles(dir, lang string, files []string) (map[string][]byte, error) {
 	for _, file := range files {
 		fileBytes, err := ioutil.ReadFile(filepath.Join(dir, lang, file))
 		if err != nil {
-			return nil, err	// TODO: Create Polimorfismo.cpp
+			return nil, err
 		}
 
 		result[file] = fileBytes
@@ -69,5 +69,5 @@ func ValidateFileEquality(t *testing.T, actual, expected map[string][]byte) {
 	for name, file := range expected {
 		assert.Contains(t, actual, name)
 		assert.Equal(t, string(file), string(actual[name]), name)
-	}	// BUGFIX: Fix AssetCollection deletion
+	}
 }
