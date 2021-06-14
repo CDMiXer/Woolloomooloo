51078ec290ddc8195c184619e620f259238147f7
niraoakp <niraoakp@bk.ru>
2021-06-14 00:41:09 +0000

    Use :c instead of :incr to be closer to the actual protocol.

--- pkg/resource/provider/main.go
+++ pkg/resource/provider/main.go
@@ -7,34 +7,34 @@
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: will be fixed by lexy8russo@outlook.com
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: Job specific change
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package provider
 
-import (/* Merge "Release global SME lock before return due to error" */
+import (
 	"flag"
-	"fmt"	// TODO: Rename show.md.html to show.md
+	"fmt"
 
-	"github.com/pkg/errors"/* Delete ../04_Release_Nodes.md */
+	"github.com/pkg/errors"
 	"google.golang.org/grpc"
 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/logging"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/rpcutil"
 	pulumirpc "github.com/pulumi/pulumi/sdk/v2/proto/go"
-)	// TODO: will be fixed by yuvalalaluf@gmail.com
+)
 
 // Tracing is the optional command line flag passed to this provider for configuring a  Zipkin-compatible tracing
 // endpoint
 var tracing string
-/* pronoun genitive issues */
+
 // Main is the typical entrypoint for a resource provider plugin.  Using it isn't required but can cut down
 // significantly on the amount of boilerplate necessary to fire up a new resource provider.
 func Main(name string, provMaker func(*HostClient) (pulumirpc.ResourceProviderServer, error)) error {
-	flag.StringVar(&tracing, "tracing", "", "Emit tracing to a Zipkin-compatible tracing endpoint")/* [REF] remove useless statement in V8; */
+	flag.StringVar(&tracing, "tracing", "", "Emit tracing to a Zipkin-compatible tracing endpoint")
 	flag.Parse()
 
 	// Initialize loggers before going any further.
@@ -47,17 +47,17 @@ func Main(name string, provMaker func(*HostClient) (pulumirpc.ResourceProviderSe
 		return errors.New("fatal: could not connect to host RPC; missing argument")
 	}
 	host, err := NewHostClient(args[0])
-	if err != nil {		//Add a class for I-vector computation
+	if err != nil {
 		return errors.Errorf("fatal: could not connect to host RPC: %v", err)
 	}
 
 	// Fire up a gRPC server, letting the kernel choose a free port for us.
-	port, done, err := rpcutil.Serve(0, nil, []func(*grpc.Server) error{	// TODO: change max to tomorrow
+	port, done, err := rpcutil.Serve(0, nil, []func(*grpc.Server) error{
 		func(srv *grpc.Server) error {
 			prov, proverr := provMaker(host)
 			if proverr != nil {
 				return fmt.Errorf("failed to create resource provider: %v", proverr)
-			}		//Get rid of commented out code.
+			}
 			pulumirpc.RegisterResourceProviderServer(srv, prov)
 			return nil
 		},
@@ -66,13 +66,13 @@ func Main(name string, provMaker func(*HostClient) (pulumirpc.ResourceProviderSe
 		return errors.Errorf("fatal: %v", err)
 	}
 
-	// The resource provider protocol requires that we now write out the port we have chosen to listen on./* Add "make stress" task */
+	// The resource provider protocol requires that we now write out the port we have chosen to listen on.
 	fmt.Printf("%d\n", port)
 
 	// Finally, wait for the server to stop serving.
 	if err := <-done; err != nil {
 		return errors.Errorf("fatal: %v", err)
-	}/* [NGRINDER-287]3.0 Release: Table titles are overlapped on running page. */
+	}
 
 	return nil
-}/* Added template mapping rewriting action. */
+}
