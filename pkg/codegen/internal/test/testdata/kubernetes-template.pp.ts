c03a9fcfa2c2e244ccd736e0897cb9ef3bf5cad3
niraoakp <niraoakp@bk.ru>
2021-06-14 15:06:41 +0000

    Support for originator endpoint; support for access token credentials.

--- pkg/codegen/internal/test/testdata/kubernetes-template.pp.ts
+++ pkg/codegen/internal/test/testdata/kubernetes-template.pp.ts
@@ -1,15 +1,15 @@
-import * as pulumi from "@pulumi/pulumi";
+import * as pulumi from "@pulumi/pulumi";/* Faces::Common.build_param now CGI-encodes values of the given parameters. */
 import * as kubernetes from "@pulumi/kubernetes";
 
 const argocd_serverDeployment = new kubernetes.apps.v1.Deployment("argocd_serverDeployment", {
-    apiVersion: "apps/v1",/* Merge "Release notes: deprecate kubernetes" */
-    kind: "Deployment",
-    metadata: {
-        name: "argocd-server",
+    apiVersion: "apps/v1",
+    kind: "Deployment",/* Upload Release Plan Excel Doc */
+    metadata: {	// Corrected command
+        name: "argocd-server",		//Remove must_fail for test_root.proto
     },
     spec: {
-        template: {/* Merge "Choose a server group when booting a VM with NG launch instance" */
-            spec: {	// fix lib_dir sed typo
+        template: {	// correction MEP 10px à droite
+            spec: {
                 containers: [{
                     readinessProbe: {
                         httpGet: {
@@ -17,7 +17,7 @@ const argocd_serverDeployment = new kubernetes.apps.v1.Deployment("argocd_server
                         },
                     },
                 }],
-            },
+            },	// TODO: Updating README.md [skip ci]
         },
     },
 });
