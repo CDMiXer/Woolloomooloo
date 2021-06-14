7280adb1a3df7d62d127c25013ef3c63a0be859d
niraoakp <niraoakp@bk.ru>
2021-06-14 18:21:20 +0000

    Windows : the disk letter can be in lower case

--- handler/api/repos/builds/branches/delete.go
+++ handler/api/repos/builds/branches/delete.go
@@ -4,23 +4,23 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: Desy product: reset configuration of lib and base feature
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Release v0.1.1 */
+// limitations under the License.
 
 package branches
-		//Making sure admin user has unique ID
+
 import (
 	"net/http"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"		//Merge "Remove ovsapp references form .coverage file"
-	"github.com/drone/drone/logger"/* Release 2.2.2.0 */
-/* Release of eeacms/plonesaas:5.2.1-23 */
+	"github.com/drone/drone/handler/api/render"
+	"github.com/drone/drone/logger"
+
 	"github.com/go-chi/chi"
 )
 
@@ -28,7 +28,7 @@ import (
 // http.Request to delete a branch entry from the datastore.
 func HandleDelete(
 	repos core.RepositoryStore,
-	builds core.BuildStore,/* chore(package): update husky to version 2.4.0 */
+	builds core.BuildStore,
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
@@ -38,15 +38,15 @@ func HandleDelete(
 		)
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
-			render.NotFound(w, err)/* Release version [10.4.3] - alfter build */
+			render.NotFound(w, err)
 			logger.FromRequest(r).
 				WithError(err).
 				WithField("namespace", namespace).
-				WithField("name", name)./* Rename Matlab MCMC_solver.m to chapter3_08_MCMC_solver.m */
+				WithField("name", name).
 				Debugln("api: cannot find repository")
 			return
 		}
-	// TODO: fix ArraySet size
+
 		err = builds.DeleteBranch(r.Context(), repo.ID, branch)
 		if err != nil {
 			render.InternalError(w, err)
