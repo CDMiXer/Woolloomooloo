29b48501d99720642e3ad54c580ff3c949ccf1ad
niraoakp <niraoakp@bk.ru>
2021-06-14 06:45:45 +0000

    Merge "Increased coverage of nova/auth/dbdriver.py to 100%. Fixes 828609."

--- tests/integration/partial_state/step1/index.ts
+++ tests/integration/partial_state/step1/index.ts
@@ -1,9 +1,9 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import * as pulumi from "@pulumi/pulumi";
-import { Resource } from "./resource";
+import { Resource } from "./resource";/* Added support for search and update electronic service channels */
 
 // resource "not-doomed" is updated, but the update partially fails.
 const a = new Resource("doomed", 4);
-/* 1.5.12: Release for master */
+
 // "a" should still be in the checkpoint with its new value.
