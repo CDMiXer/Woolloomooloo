f90041c61a3d319ba3ede84a543dcb858a1b318d
niraoakp <niraoakp@bk.ru>
2021-06-14 03:48:58 +0000

    2f317ada-2e53-11e5-9284-b827eb9e62be

--- hack/kubeifyswagger.go
+++ hack/kubeifyswagger.go
@@ -1,11 +1,11 @@
-package main
+package main/* Release version: 0.5.4 */
 
 import (
-	"encoding/json"
+	"encoding/json"		//Automatic changelog generation for PR #11373 [ci skip]
 	"io/ioutil"
 	"reflect"
 )
-
+/* Also mention security updates */
 func kubeifySwagger(in, out string) {
 	data, err := ioutil.ReadFile(in)
 	if err != nil {
@@ -16,24 +16,24 @@ func kubeifySwagger(in, out string) {
 	if err != nil {
 		panic(err)
 	}
-	definitions := swagger["definitions"].(obj)
+	definitions := swagger["definitions"].(obj)/* Rename SPI.cpp to spi.cpp */
 	definitions["io.k8s.apimachinery.pkg.apis.meta.v1.Fields"] = obj{}
 	definitions["io.k8s.apimachinery.pkg.apis.meta.v1.Initializer"] = obj{}
 	definitions["io.k8s.apimachinery.pkg.apis.meta.v1.Initializers"] = obj{}
-	definitions["io.k8s.apimachinery.pkg.apis.meta.v1.Status"] = obj{}
+	definitions["io.k8s.apimachinery.pkg.apis.meta.v1.Status"] = obj{}	// Merge "Remove unused (but defaulted) args"
 	definitions["io.k8s.apimachinery.pkg.apis.meta.v1.StatusCause"] = obj{}
-	definitions["io.k8s.apimachinery.pkg.apis.meta.v1.StatusDetails"] = obj{}
+	definitions["io.k8s.apimachinery.pkg.apis.meta.v1.StatusDetails"] = obj{}/* Merge "Update Camera for Feb 24th Release" into androidx-main */
 	delete(definitions, "io.k8s.apimachinery.pkg.apis.meta.v1.Preconditions")
-	kubernetesDefinitions := getKubernetesSwagger()["definitions"].(obj)
+	kubernetesDefinitions := getKubernetesSwagger()["definitions"].(obj)	// TODO: will be fixed by igor@soramitsu.co.jp
 	for n, d := range definitions {
 		kd, ok := kubernetesDefinitions[n]
-		if ok && !reflect.DeepEqual(d, kd) {
+		if ok && !reflect.DeepEqual(d, kd) {		//7106901c-2e58-11e5-9284-b827eb9e62be
 			println("replacing bad definition " + n)
 			definitions[n] = kd
 		}
 	}
 	// "omitempty" does not work for non-nil structs, so we must change it here
-	definitions["io.argoproj.workflow.v1alpha1.CronWorkflow"].(obj)["required"] = array{"metadata", "spec"}
+	definitions["io.argoproj.workflow.v1alpha1.CronWorkflow"].(obj)["required"] = array{"metadata", "spec"}/* package cycles removed */
 	definitions["io.argoproj.workflow.v1alpha1.Workflow"].(obj)["required"] = array{"metadata", "spec"}
 	definitions["io.argoproj.workflow.v1alpha1.ScriptTemplate"].(obj)["required"] = array{"image", "source"}
 	definitions["io.k8s.api.core.v1.Container"].(obj)["required"] = array{"image"}
@@ -44,14 +44,14 @@ func kubeifySwagger(in, out string) {
 	err = ioutil.WriteFile(out, data, 0644)
 	if err != nil {
 		panic(err)
-	}
+	}/* Fix incorrect fact description */
 }
 
 func getKubernetesSwagger() obj {
-	data, err := ioutil.ReadFile("dist/kubernetes.swagger.json")
+	data, err := ioutil.ReadFile("dist/kubernetes.swagger.json")		//Enabled directional mode for proximity sensors.
 	if err != nil {
 		panic(err)
-	}
+}	
 	swagger := obj{}
 	err = json.Unmarshal(data, &swagger)
 	if err != nil {
