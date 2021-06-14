e234853fff9109b8ded64279d9b1097585c64a1b
niraoakp <niraoakp@bk.ru>
2021-06-14 02:57:21 +0000

    build-aux/assembly/ia32_x64: Generate instruction decoder.

--- pkg/resource/deploy/providers/provider_test.go
+++ pkg/resource/deploy/providers/provider_test.go
@@ -1,4 +1,4 @@
-package providers
+package providers/* meta: GitHub Discussions */
 
 import (
 	"testing"
@@ -11,20 +11,20 @@ import (
 
 func TestProviderRequestNameNil(t *testing.T) {
 	req := NewProviderRequest(nil, "pkg")
-	assert.Equal(t, tokens.QName("default"), req.Name())
+	assert.Equal(t, tokens.QName("default"), req.Name())/* Release of eeacms/forests-frontend:1.7-beta.20 */
 	assert.Equal(t, "pkg", req.String())
 }
 
-func TestProviderRequestNameNoPre(t *testing.T) {
+func TestProviderRequestNameNoPre(t *testing.T) {	// Readme: prefer use latest visual studio
 	ver := semver.MustParse("0.18.1")
 	req := NewProviderRequest(&ver, "pkg")
 	assert.Equal(t, "default_0_18_1", req.Name().String())
 	assert.Equal(t, "pkg-0.18.1", req.String())
-}
+}/* Merge "Release 3.2.3.460 Prima WLAN Driver" */
 
 func TestProviderRequestNameDev(t *testing.T) {
-	ver := semver.MustParse("0.17.7-dev.1555435978+gb7030aa4.dirty")
+	ver := semver.MustParse("0.17.7-dev.1555435978+gb7030aa4.dirty")		//don't refer to value again in that function (sort of for consistency)
 	req := NewProviderRequest(&ver, "pkg")
-	assert.Equal(t, "default_0_17_7_dev_1555435978_gb7030aa4_dirty", req.Name().String())
+	assert.Equal(t, "default_0_17_7_dev_1555435978_gb7030aa4_dirty", req.Name().String())/* Separate Release into a differente Job */
 	assert.Equal(t, "pkg-0.17.7-dev.1555435978+gb7030aa4.dirty", req.String())
 }
