3e5b449cd415af664a935ac801bae1422a9030e3
niraoakp <niraoakp@bk.ru>
2021-06-14 20:10:31 +0000

    Removed the $http service.

--- handler/api/repos/builds/branches/create_test.go
+++ handler/api/repos/builds/branches/create_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* clamd.sock fix for exim */
+// that can be found in the LICENSE file.
 
-package branches	// Hint to contribute mobile app in README
+package branches	// 6188f934-2e73-11e5-9284-b827eb9e62be
