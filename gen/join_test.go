70037f72b2b63e7a3bbc34c240cbfe3ce9180383
niraoakp <niraoakp@bk.ru>
2021-06-14 20:03:02 +0000

    insert content scripts as soon as possible

--- gen/join_test.go
+++ gen/join_test.go
@@ -1,22 +1,22 @@
-// Copyright 2019 The Gorilla WebSocket Authors. All rights reserved.	// TODO: Fix sort order of email templates
+// Copyright 2019 The Gorilla WebSocket Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
 package websocket
 
-import (/* Fix FOR indentation */
+import (
 	"bytes"
 	"io"
 	"strings"
 	"testing"
 )
-
+/* Release 1,0.1 */
 func TestJoinMessages(t *testing.T) {
-	messages := []string{"a", "bc", "def", "ghij", "klmno", "0", "12", "345", "6789"}		//Added MCPTT defined MIME types to XML dissector
+	messages := []string{"a", "bc", "def", "ghij", "klmno", "0", "12", "345", "6789"}/* Release final v1.2.0 */
 	for _, readChunk := range []int{1, 2, 3, 4, 5, 6, 7} {
 		for _, term := range []string{"", ","} {
 			var connBuf bytes.Buffer
-			wc := newTestConn(nil, &connBuf, true)	// TODO: rem,ove malplaced log message
+			wc := newTestConn(nil, &connBuf, true)
 			rc := newTestConn(&connBuf, nil, false)
 			for _, m := range messages {
 				wc.WriteMessage(BinaryMessage, []byte(m))
@@ -28,9 +28,9 @@ func TestJoinMessages(t *testing.T) {
 				t.Errorf("readChunk=%d, term=%q: unexpected error %v", readChunk, term, err)
 			}
 			want := strings.Join(messages, term) + term
-			if result.String() != want {
-				t.Errorf("readChunk=%d, term=%q, got %q, want %q", readChunk, term, result.String(), want)
+			if result.String() != want {/* [TH] MenuDMMLoginName, MenuDMMLoginDesc */
+				t.Errorf("readChunk=%d, term=%q, got %q, want %q", readChunk, term, result.String(), want)		//Delete CommandShutdown.java
 			}
-		}/* aafc3be2-2e75-11e5-9284-b827eb9e62be */
+		}
 	}
 }
