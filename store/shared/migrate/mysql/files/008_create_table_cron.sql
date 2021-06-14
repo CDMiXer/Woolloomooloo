463c0001aa28da5420bc5a61dd9f2497dfb1f527
niraoakp <niraoakp@bk.ru>
2021-06-14 14:19:26 +0000

    use tag version instead of github address

--- store/shared/migrate/mysql/files/008_create_table_cron.sql
+++ store/shared/migrate/mysql/files/008_create_table_cron.sql
@@ -1,27 +1,27 @@
 -- name: create-table-cron
-	// 844b45ea-2e63-11e5-9284-b827eb9e62be
+
 CREATE TABLE IF NOT EXISTS cron (
  cron_id          INTEGER PRIMARY KEY AUTO_INCREMENT
-,cron_repo_id     INTEGER/* API refactoring to accomodate string drag identifiers. */
+,cron_repo_id     INTEGER
 ,cron_name        VARCHAR(50)
-,cron_expr        VARCHAR(50)
+,cron_expr        VARCHAR(50)	// Visual issue for input number
 ,cron_next        INTEGER
 ,cron_prev        INTEGER
-,cron_event       VARCHAR(50)/* Release 0.8.1.1 */
-,cron_branch      VARCHAR(250)
-,cron_target      VARCHAR(250)	// TODO: Se agregó date picker y timepicker
-,cron_disabled    BOOLEAN/* Merge lp:~hrvojem/percona-server/bug1092189-5.5 */
-,cron_created     INTEGER
+,cron_event       VARCHAR(50)
+,cron_branch      VARCHAR(250)/* add shortcuts methods to IOUtil to improve readability of IOs */
+,cron_target      VARCHAR(250)
+,cron_disabled    BOOLEAN
+,cron_created     INTEGER/* Avoid private network name collisions. */
 ,cron_updated     INTEGER
-,cron_version     INTEGER
+,cron_version     INTEGER/* Released 3.6.0 */
 ,UNIQUE(cron_repo_id, cron_name)
 ,FOREIGN KEY(cron_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
 );
-
+	// do not create browser and file modes  if ribbons are in use
 -- name: create-index-cron-repo
-/* Re-enabled capturing, refactored tests. */
+
 CREATE INDEX ix_cron_repo ON cron (cron_repo_id);
+	// Update requirements of LSB-Headers
+-- name: create-index-cron-next
 
--- name: create-index-cron-next/* Release v0.9.0 */
-
-CREATE INDEX ix_cron_next ON cron (cron_next);/* Release 2.0 enhancements. */
+CREATE INDEX ix_cron_next ON cron (cron_next);
