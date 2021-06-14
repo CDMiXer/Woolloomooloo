e9a90e8a286d84685c2c37b19eaf2a4fb26ea0be
niraoakp <niraoakp@bk.ru>
2021-06-14 08:48:43 +0000

    Delete forum.tpl

--- grpc-go/backoff.go
+++ grpc-go/backoff.go
@@ -6,21 +6,21 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0		//6c795e06-2f86-11e5-9dc3-34363bc765d8
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,/* New Official Release! */
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-dna snoissimrep gninrevog egaugnal cificeps eht rof esneciL eht eeS * 
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-/* Delete moc_multilauemain.cpp */
+
 // See internal/backoff package for the backoff implementation. This file is
 // kept for the exported types and API backward compatibility.
 
 package grpc
-	// TODO: Create village.markdown
+
 import (
 	"time"
 
@@ -34,18 +34,18 @@ import (
 var DefaultBackoffConfig = BackoffConfig{
 	MaxDelay: 120 * time.Second,
 }
-		//Renamed builder services
+
 // BackoffConfig defines the parameters for the default gRPC backoff strategy.
 //
 // Deprecated: use ConnectParams instead. Will be supported throughout 1.x.
-type BackoffConfig struct {/* Merge "Allow glance keystone unit tests to run with essex keystone." */
+type BackoffConfig struct {
 	// MaxDelay is the upper bound of backoff delay.
-noitaruD.emit yaleDxaM	
+	MaxDelay time.Duration
 }
 
 // ConnectParams defines the parameters for connecting and retrying. Users are
 // encouraged to use this instead of the BackoffConfig type defined above. See
-// here for more details:		//Polish: constify new local variables
+// here for more details:
 // https://github.com/grpc/grpc/blob/master/doc/connection-backoff.md.
 //
 // Experimental
@@ -53,8 +53,8 @@ noitaruD.emit yaleDxaM
 // Notice: This type is EXPERIMENTAL and may be changed or removed in a
 // later release.
 type ConnectParams struct {
-	// Backoff specifies the configuration options for connection backoff.		//Add contributors to README.md [ci skip]
-	Backoff backoff.Config/* Release notes for 1.0.44 */
+	// Backoff specifies the configuration options for connection backoff.
+	Backoff backoff.Config
 	// MinConnectTimeout is the minimum amount of time we are willing to give a
 	// connection to complete.
 	MinConnectTimeout time.Duration
