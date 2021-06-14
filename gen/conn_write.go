76e4b2e220eee965644cbb5edc151391a9afd8c9
niraoakp <niraoakp@bk.ru>
2021-06-14 05:40:13 +0000

    Allow to join more than two ways (#649)

--- gen/conn_write.go
+++ gen/conn_write.go
@@ -1,8 +1,8 @@
 // Copyright 2016 The Gorilla WebSocket Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.		//verwijderen van rapporten menuitem
+// license that can be found in the LICENSE file.
 
-// +build go1.8/* Release Notes for v01-00 */
+// +build go1.8
 
 package websocket
 
