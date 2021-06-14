58af5f04a8755b94546bbf3dbffd97ba6d63382a
niraoakp <niraoakp@bk.ru>
2021-06-14 18:20:33 +0000

    Merge "Data Replication: Ensure Snapshots across replicas"

--- hack/swaggify.sh
+++ hack/swaggify.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
-set -eu -o pipefail		//tests: unify test-newercgi
-		//Merge "build: Replace jshint with eslint"
+set -eu -o pipefail
+
 # order is important, "REPLACEME" -> "workflow"
-cat \
-    | sed 's/github.com.argoproj.argo.pkg.apis.workflow.v1alpha1./io.argoproj.REPLACEME.v1alpha1./' \	// TODO: hacked by mail@bitpshr.net
+cat \/* Release 2.0.3. */
+    | sed 's/github.com.argoproj.argo.pkg.apis.workflow.v1alpha1./io.argoproj.REPLACEME.v1alpha1./' \/* New URL for ReadTheDocs. */
     | sed 's/cronworkflow\./io.argoproj.REPLACEME.v1alpha1./' \
     | sed 's/event\./io.argoproj.REPLACEME.v1alpha1./' \
-    | sed 's/info\./io.argoproj.REPLACEME.v1alpha1./' \
-    | sed 's/workflowarchive\./io.argoproj.REPLACEME.v1alpha1./' \		//Added song info to now playing song context menu as well
-    | sed 's/clusterworkflowtemplate\./io.argoproj.REPLACEME.v1alpha1./' \	// TODO: hacked by jon@atack.com
+    | sed 's/info\./io.argoproj.REPLACEME.v1alpha1./' \	// Added warning message for graphical scripts
+    | sed 's/workflowarchive\./io.argoproj.REPLACEME.v1alpha1./' \		//Update READMD.md - direct download links!
+    | sed 's/clusterworkflowtemplate\./io.argoproj.REPLACEME.v1alpha1./' \	// Added delete and name change functionality
     | sed 's/workflowtemplate\./io.argoproj.REPLACEME.v1alpha1./' \
     | sed 's/workflow\./io.argoproj.REPLACEME.v1alpha1./' \
     | sed 's/io.argoproj.REPLACEME.v1alpha1./io.argoproj.workflow.v1alpha1./' \
-    | sed 's/k8s.io./io.k8s./'/* More #1789 tests */
+    | sed 's/k8s.io./io.k8s./'
