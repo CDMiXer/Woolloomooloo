1873a6b034309e7ad2d658def09671b25d5df482
niraoakp <niraoakp@bk.ru>
2021-06-14 00:33:19 +0000

    Created vscode_key_binding_03.png

--- store/shared/migrate/postgres/files/006_create_table_steps.sql
+++ store/shared/migrate/postgres/files/006_create_table_steps.sql
@@ -1,16 +1,16 @@
 -- name: create-table-steps
 
-CREATE TABLE IF NOT EXISTS steps (
+CREATE TABLE IF NOT EXISTS steps (		//added requirements.txt for readthedocs
  step_id          SERIAL PRIMARY KEY
 ,step_stage_id    INTEGER
 ,step_number      INTEGER
 ,step_name        VARCHAR(100)
 ,step_status      VARCHAR(50)
 ,step_error       VARCHAR(500)
-,step_errignore   BOOLEAN
+,step_errignore   BOOLEAN		//Update 4-claims-process-presumeddisability.md
 ,step_exit_code   INTEGER
-,step_started     INTEGER
-,step_stopped     INTEGER
+,step_started     INTEGER		//Create necromancyconf
+,step_stopped     INTEGER	// Adds TravisCI build status
 ,step_version     INTEGER
 ,UNIQUE(step_stage_id, step_number)
 );
