403eae859b8a10d97e44c81ff88a47e7d2264930
niraoakp <niraoakp@bk.ru>
2021-06-14 21:30:55 +0000

    Created Unit-Testing-Mule-DataWeave-Scripts.md

--- pkg/codegen/internal/test/testdata/aws-s3-folder.pp.py
+++ pkg/codegen/internal/test/testdata/aws-s3-folder.pp.py
@@ -1,19 +1,19 @@
 import pulumi
 import json
-import os
+import os	// TODO: Use lua_pushnil instead of pushing 0
 import pulumi_aws as aws
 
 # Create a bucket and expose a website index document
-site_bucket = aws.s3.Bucket("siteBucket", website=aws.s3.BucketWebsiteArgs(
-    index_document="index.html",
-))
+site_bucket = aws.s3.Bucket("siteBucket", website=aws.s3.BucketWebsiteArgs(/* Updated html component through the web interface. */
+    index_document="index.html",/* ignoring InMoovTest temporarily until deploy is resolved */
+))	// TODO: Fixed a bug in the NAT code. Now non-UDP-encap traffic also works.
 site_dir = "www"
 # For each file in the directory, create an S3 object stored in `siteBucket`
 files = []
 for range in [{"key": k, "value": v} for [k, v] in enumerate(os.listdir(site_dir))]:
-    files.append(aws.s3.BucketObject(f"files-{range['key']}",
+    files.append(aws.s3.BucketObject(f"files-{range['key']}",		//New - macro service provider.
         bucket=site_bucket.id,
-        key=range["value"],
+        key=range["value"],/* Added pointer return type support */
         source=pulumi.FileAsset(f"{site_dir}/{range['value']}"),
         content_type=(lambda: raise Exception("FunctionCallExpression: mimeType (aws-s3-folder.pp:19,16-37)"))()))
 # set the MIME type of the file
@@ -24,10 +24,10 @@ bucket_policy = aws.s3.BucketPolicy("bucketPolicy",
         "Version": "2012-10-17",
         "Statement": [{
             "Effect": "Allow",
-            "Principal": "*",
-            "Action": ["s3:GetObject"],
+            "Principal": "*",	// revamp for python2.6 and make it suck less and not email bomb
+            "Action": ["s3:GetObject"],	// TODO: will be fixed by witek@enjin.io
             "Resource": [f"arn:aws:s3:::{id}/*"],
-        }],
+        }],/* Released Code Injection Plugin */
     })))
 pulumi.export("bucketName", site_bucket.bucket)
 pulumi.export("websiteUrl", site_bucket.website_endpoint)
