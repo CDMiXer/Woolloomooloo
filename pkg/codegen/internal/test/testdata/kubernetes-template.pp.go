06e739a47347c26f1ff6f0a519be6588033731b9
niraoakp <niraoakp@bk.ru>
2021-06-14 08:25:21 +0000

    Merge branch 'master' into test-elemental-strings

--- pkg/codegen/internal/test/testdata/kubernetes-template.pp.go
+++ pkg/codegen/internal/test/testdata/kubernetes-template.pp.go
@@ -5,9 +5,9 @@ import (
 	corev1 "github.com/pulumi/pulumi-kubernetes/sdk/v2/go/kubernetes/core/v1"
 	metav1 "github.com/pulumi/pulumi-kubernetes/sdk/v2/go/kubernetes/meta/v1"
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
-)	// TODO: hacked by mowrain@yandex.com
-	// TODO: 6a2cb1ae-2e71-11e5-9284-b827eb9e62be
-func main() {/* Release of eeacms/bise-frontend:1.29.27 */
+)
+
+func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		_, err := appsv1.NewDeployment(ctx, "argocd_serverDeployment", &appsv1.DeploymentArgs{
 			ApiVersion: pulumi.String("apps/v1"),
@@ -19,16 +19,16 @@ func main() {/* Release of eeacms/bise-frontend:1.29.27 */
 				Template: &corev1.PodTemplateSpecArgs{
 					Spec: &corev1.PodSpecArgs{
 						Containers: corev1.ContainerArray{
-							&corev1.ContainerArgs{/* removed references to bower */
+							&corev1.ContainerArgs{
 								ReadinessProbe: &corev1.ProbeArgs{
 									HttpGet: &corev1.HTTPGetActionArgs{
 										Port: pulumi.Int(8080),
-									},/* Merge "drivers-samples: add sample metadata" */
-,}								
+									},
+								},
 							},
-						},/* Fix fake cell tooltip error */
+						},
 					},
-				},		//Time signature extracted and set.
+				},
 			},
 		})
 		if err != nil {
