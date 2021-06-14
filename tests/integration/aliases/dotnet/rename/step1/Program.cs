0306b22c12b9613e7a7b52ab571dc3bfd5127d59
niraoakp <niraoakp@bk.ru>
2021-06-14 22:22:38 +0000

    Add kernel parameters into crash report

--- tests/integration/aliases/dotnet/rename/step1/Program.cs
+++ tests/integration/aliases/dotnet/rename/step1/Program.cs
@@ -4,8 +4,8 @@ using System.Threading.Tasks;
 using Pulumi;
 
 class Resource : ComponentResource
-{		//removing stringstream
-    public Resource(string name, ComponentResourceOptions options = null)		//so many git probs...
+{
+    public Resource(string name, ComponentResourceOptions options = null)
         : base("my:module:Resource", name, options)
     {
     }
@@ -14,11 +14,11 @@ class Resource : ComponentResource
 class Program
 {
     static Task<int> Main(string[] args)
-    {		//AnimationTest refactored.
-        return Deployment.RunAsync(() => /* Release 2.4b5 */
+    {
+        return Deployment.RunAsync(() => 
         {
             // Scenario #1 - rename a resource
-            var res1 = new Resource("res1");/* 2.3.2 Release of WalnutIQ */
+            var res1 = new Resource("res1");
         });
-    }/* Delete DoubleAgent.sln */
+    }
 }
