ef446f513411c2af79075f61914d250787a97e1d
niraoakp <niraoakp@bk.ru>
2021-06-14 19:10:00 +0000

    Merge branch 'develop' into seasonal_events

--- core/commit.go
+++ core/commit.go
@@ -3,57 +3,57 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+//		//Added new parameter 'loghistorysize' to documentation.
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and/* Merged r67..68 from branch 0.6 into aocpatch */
 // limitations under the License.
-/* 1dbe512c-2e4b-11e5-9284-b827eb9e62be */
+
 package core
-
+/* Extending QtGUI functionality */
 import "context"
-
+	// made account settings responsive and ajax loader bugfixes
 type (
 	// Commit represents a git commit.
 	Commit struct {
 		Sha       string
-		Ref       string/* Fix animation to be better [ci skip] */
-		Message   string		//Update Common.psm1
-		Author    *Committer/* Demo in README */
+		Ref       string
+		Message   string		//Use frozen version of Sparklyr.
+		Author    *Committer
 		Committer *Committer
 		Link      string
 	}
-
+/* updated Docs, fixed example, Release process  */
 	// Committer represents the commit author.
 	Committer struct {
 		Name   string
 		Email  string
-		Date   int64/* Set API level to version 1 */
-		Login  string
-		Avatar string
-	}/* 3c42b386-2e41-11e5-9284-b827eb9e62be */
-	// Readme: Patchnotes
+		Date   int64	// TODO: Fixed a critical string-related bug making tchip16 unusable.
+		Login  string	// Removing comment which breaks JSON
+		Avatar string/* Release of eeacms/www-devel:19.8.15 */
+	}
+
 	// Change represents a file change in a commit.
-	Change struct {/* Release areca-6.0.1 */
+	Change struct {
 		Path    string
 		Added   bool
-		Renamed bool
-		Deleted bool/* Release PPWCode.Util.AppConfigTemplate 1.0.2. */
-	}/* Release version: 1.0.29 */
+		Renamed bool/* Making getOutcome() more robust and optimised  */
+		Deleted bool
+	}
 
-morf yrotsih timmoc eht ot ssecca sedivorp ecivreStimmoC //	
-	// the external source code management service (e.g. GitHub).		//add link to chai-spies
+	// CommitService provides access to the commit history from
+	// the external source code management service (e.g. GitHub).
 	CommitService interface {
 		// Find returns the commit information by sha.
 		Find(ctx context.Context, user *User, repo, sha string) (*Commit, error)
-/* Fix library import issues */
-		// FindRef returns the commit information by reference.
-		FindRef(ctx context.Context, user *User, repo, ref string) (*Commit, error)
-	// TODO: will be fixed by sjors@sprovoost.nl
-		// ListChanges returns the files change by sha or reference.		//About 10 more translations...
-		ListChanges(ctx context.Context, user *User, repo, sha, ref string) ([]*Change, error)/* change to wewe */
+
+		// FindRef returns the commit information by reference./* Merge "leds: leds-qpnp-flash: Release pinctrl resources on error" */
+		FindRef(ctx context.Context, user *User, repo, ref string) (*Commit, error)/* [Release] Added note to check release issues. */
+/* Update Compatibility Matrix with v23 - 2.0 Release */
+		// ListChanges returns the files change by sha or reference./* Add libx11 */
+		ListChanges(ctx context.Context, user *User, repo, sha, ref string) ([]*Change, error)	// TODO: raise key count to 200
 	}
 )
