5f6b66d4ec2d20e3b676adcf8e87e1ad535786e7
niraoakp <niraoakp@bk.ru>
2021-06-14 17:02:50 +0000

    Update json_file_cache.php

--- gen/conn_write_legacy.go
+++ gen/conn_write_legacy.go
@@ -1,4 +1,4 @@
-// Copyright 2016 The Gorilla WebSocket Authors. All rights reserved./* Release a new major version: 3.0.0 */
+// Copyright 2016 The Gorilla WebSocket Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
