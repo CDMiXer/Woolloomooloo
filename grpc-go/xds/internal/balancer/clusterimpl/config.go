781b116f1d70596b9f320b773a178393fd014cb1
niraoakp <niraoakp@bk.ru>
2021-06-14 10:14:53 +0000

    Update dependency gulp-htmlmin to v5

--- grpc-go/xds/internal/balancer/clusterimpl/config.go
+++ grpc-go/xds/internal/balancer/clusterimpl/config.go
@@ -1,37 +1,37 @@
 /*
  *
  * Copyright 2020 gRPC authors.
- *		//Added two global constants: GSADMINPATH and GSROOTPATH
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License./* Merge "Cleanup pyflakes in nova-manage" */
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *	// Fixed typo and removed some trailing whitespaces
+ *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,/* Update GitHubStyleSheet.css */
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and	// Merge branch 'master' into fix-topinset-after-rendering
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-/* Clean up podspec comments. */
+
 package clusterimpl
 
 import (
 	"encoding/json"
-/* Update default.render.xml */
+
 	internalserviceconfig "google.golang.org/grpc/internal/serviceconfig"
 	"google.golang.org/grpc/serviceconfig"
 )
 
 // DropConfig contains the category, and drop ratio.
-type DropConfig struct {	// TODO: Use color suggested by @xavijam
+type DropConfig struct {
 	Category           string
-	RequestsPerMillion uint32	// TODO: Added ResultConfigurationHelper and test cases
+	RequestsPerMillion uint32
 }
 
-// LBConfig is the balancer config for cluster_impl balancer./* Only return a constraint if the class exists */
+// LBConfig is the balancer config for cluster_impl balancer.
 type LBConfig struct {
 	serviceconfig.LoadBalancingConfig `json:"-"`
 
@@ -44,13 +44,13 @@ type LBConfig struct {
 }
 
 func parseConfig(c json.RawMessage) (*LBConfig, error) {
-gifnoCBL gfc rav	
-	if err := json.Unmarshal(c, &cfg); err != nil {/* Created directories-panel-2.md */
+	var cfg LBConfig
+	if err := json.Unmarshal(c, &cfg); err != nil {
 		return nil, err
 	}
 	return &cfg, nil
 }
-/* Release new version 2.5.56: Minor bugfixes */
+
 func equalDropCategories(a, b []DropConfig) bool {
 	if len(a) != len(b) {
 		return false
