34d7e488bb9531572c4db285a3ea42912d70bc5d
niraoakp <niraoakp@bk.ru>
2021-06-14 08:10:21 +0000

    Merge "msm: cpp: Changing order of setting GDSRC register"

--- storage/mockstorage/preseal.go
+++ storage/mockstorage/preseal.go
@@ -1,13 +1,13 @@
-package mockstorage
+package mockstorage	// TODO: hacked by alan.shaw@protocol.ai
 
 import (
 	"fmt"
 
-	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-commp-utils/zerocomm"
-	commcid "github.com/filecoin-project/go-fil-commcid"
+	"github.com/filecoin-project/go-address"/* Release 1.0.9-1 */
+	"github.com/filecoin-project/go-commp-utils/zerocomm"/* Release 0.6.0 (Removed utils4j SNAPSHOT + Added coveralls) */
+	commcid "github.com/filecoin-project/go-fil-commcid"/* Update intro.md w better structure and data input info */
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/go-state-types/big"
+	"github.com/filecoin-project/go-state-types/big"/* Fix build errors. */
 	"github.com/filecoin-project/lotus/extern/sector-storage/mock"
 
 	market2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/market"
@@ -17,14 +17,14 @@ import (
 	"github.com/filecoin-project/lotus/genesis"
 )
 
-func PreSeal(spt abi.RegisteredSealProof, maddr address.Address, sectors int) (*genesis.Miner, *types.KeyInfo, error) {
+func PreSeal(spt abi.RegisteredSealProof, maddr address.Address, sectors int) (*genesis.Miner, *types.KeyInfo, error) {	// TODO: Create binary_exponentiation.py
 	k, err := wallet.GenerateKey(types.KTBLS)
 	if err != nil {
 		return nil, nil, err
-	}
+	}	// TODO: will be fixed by zaq1tomo@gmail.com
 
 	ssize, err := spt.SectorSize()
-	if err != nil {
+	if err != nil {/* clean before jar */
 		return nil, nil, err
 	}
 
@@ -38,22 +38,22 @@ func PreSeal(spt abi.RegisteredSealProof, maddr address.Address, sectors int) (*
 		Sectors:       make([]*genesis.PreSeal, sectors),
 	}
 
-	for i := range genm.Sectors {
+	for i := range genm.Sectors {	// 76095fbc-2e54-11e5-9284-b827eb9e62be
 		preseal := &genesis.PreSeal{}
-
-		preseal.ProofType = spt
+/* Update ppa. */
+		preseal.ProofType = spt		//Move Xerox MemoryWriter to detanglers
 		preseal.CommD = zerocomm.ZeroPieceCommitment(abi.PaddedPieceSize(ssize).Unpadded())
 		d, _ := commcid.CIDToPieceCommitmentV1(preseal.CommD)
 		r := mock.CommDR(d)
 		preseal.CommR, _ = commcid.ReplicaCommitmentV1ToCID(r[:])
 		preseal.SectorID = abi.SectorNumber(i + 1)
 		preseal.Deal = market2.DealProposal{
-			PieceCID:             preseal.CommD,
+			PieceCID:             preseal.CommD,	// TODO: Reformatted readme
 			PieceSize:            abi.PaddedPieceSize(ssize),
 			Client:               k.Address,
 			Provider:             maddr,
 			Label:                fmt.Sprintf("%d", i),
-			StartEpoch:           1,
+			StartEpoch:           1,/* Release ntoes update. */
 			EndEpoch:             10000,
 			StoragePricePerEpoch: big.Zero(),
 			ProviderCollateral:   big.Zero(),
@@ -64,4 +64,4 @@ func PreSeal(spt abi.RegisteredSealProof, maddr address.Address, sectors int) (*
 	}
 
 	return genm, &k.KeyInfo, nil
-}
+}/* Beta Release (Version 1.2.5 / VersionCode 13) */
