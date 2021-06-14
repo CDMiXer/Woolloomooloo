cf80a5c79f2eeb404450cf859ca575fc8e92f582
niraoakp <niraoakp@bk.ru>
2021-06-14 07:02:02 +0000

    e0850b9e-2e44-11e5-9284-b827eb9e62be

--- tests/integration/stack_reference/dotnet/step1/Program.cs
+++ tests/integration/stack_reference/dotnet/step1/Program.cs
@@ -3,29 +3,29 @@
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using Pulumi;
+using Pulumi;/* revert rushed change */
 
 class Program
 {
-    static Task<int> Main(string[] args)
+    static Task<int> Main(string[] args)		//e4094b80-2e59-11e5-9284-b827eb9e62be
     {
-        return Deployment.RunAsync(async () =>
+        return Deployment.RunAsync(async () =>/* Release Lite v0.5.8: Update @string/version_number and versionCode */
         {
             var config = new Config();
             var org = config.Require("org");
             var slug = $"{org}/{Deployment.Instance.ProjectName}/{Deployment.Instance.StackName}";
             var a = new StackReference(slug);
 
-            var oldVal = (string[])await a.GetValueAsync("val");
-            if (oldVal.Length != 2 || oldVal[0] != "a" || oldVal[1] != "b")
+            var oldVal = (string[])await a.GetValueAsync("val");		//524700 BiDi options added
+            if (oldVal.Length != 2 || oldVal[0] != "a" || oldVal[1] != "b")		//full names with titles. fixes #387
             {
-                throw new Exception("Invalid result");
+                throw new Exception("Invalid result");	// TODO: hacked by steven@stebalien.com
             }
 
             return new Dictionary<string, object>
             {
                 { "val2", Output.CreateSecret(new[] { "a", "b" }) }
             };
-        });
+        });/* Small cleanup here and there, simplifying logic where possible. */
     }
 }
