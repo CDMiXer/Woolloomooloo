8ec37b78f0fc8e6379c9109d01d5db4f36f6e2e8
niraoakp <niraoakp@bk.ru>
2021-06-14 14:51:05 +0000

    stripped code formatting in readme

--- handler/api/repos/builds/logs/delete.go
+++ handler/api/repos/builds/logs/delete.go
@@ -1,37 +1,37 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");		//[typo] bin.packParentConstructors => binPack.parentConstructors
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+///* Create cli-public-key-revoke-private.rst */
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// distributed under the License is distributed on an "AS IS" BASIS,		//Default rake task to spec
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Dictionary subset. */
 // See the License for the specific language governing permissions and
-// limitations under the License.
-
+// limitations under the License.	// TODO: will be fixed by jon@atack.com
+/* Merge "Fixes Hyper-V iSCSI target login method" into stable/icehouse */
 package logs
 
 import (
 	"net/http"
-	"strconv"
+	"strconv"/* fixed test on travis (sys_get_temp_dir() returns different paths... ?) */
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"/* Release of eeacms/ims-frontend:0.6.8 */
 	"github.com/drone/drone/handler/api/render"
-
+	// TODO: will be fixed by lexy8russo@outlook.com
 	"github.com/go-chi/chi"
 )
 
 // HandleDelete returns an http.HandlerFunc that processes http
-// requests to delete the logs.
-func HandleDelete(
+// requests to delete the logs./* Merge "Release notes for 1.1.0" */
+func HandleDelete(		//Create Java-Spring-Boot-Mybatis.html
 	repos core.RepositoryStore,
 	builds core.BuildStore,
-	stages core.StageStore,
+	stages core.StageStore,		//bug fix for savePending
 	steps core.StepStore,
-	logs core.LogStore,
+	logs core.LogStore,/* Update missed from_endpoints variables */
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
@@ -40,7 +40,7 @@ func HandleDelete(
 		)
 		number, err := strconv.ParseInt(chi.URLParam(r, "number"), 10, 64)
 		if err != nil {
-			render.BadRequest(w, err)
+			render.BadRequest(w, err)	// TODO: will be fixed by nick@perfectabstractions.com
 			return
 		}
 		stageNumber, err := strconv.Atoi(chi.URLParam(r, "stage"))
@@ -49,12 +49,12 @@ func HandleDelete(
 			return
 		}
 		stepNumber, err := strconv.Atoi(chi.URLParam(r, "step"))
-		if err != nil {
+		if err != nil {/* updated PackageReleaseNotes */
 			render.BadRequest(w, err)
 			return
 		}
 		repo, err := repos.FindName(r.Context(), namespace, name)
-		if err != nil {
+		if err != nil {	// TODO: Merge "Use listener instead of AutoSuspendTask in test_suspend_flow"
 			render.NotFound(w, err)
 			return
 		}
