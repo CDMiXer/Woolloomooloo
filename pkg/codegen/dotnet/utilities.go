1f1866960ba4cb07cd1d89073f1e93c26e63b3ec
niraoakp <niraoakp@bk.ru>
2021-06-14 11:30:15 +0000

    UNLEASH THE KRAKEN: command line

--- pkg/codegen/dotnet/utilities.go
+++ pkg/codegen/dotnet/utilities.go
@@ -1,9 +1,9 @@
 // Copyright 2016-2020, Pulumi Corporation.
-//	// TODO: typo: the folder is lexicons, not lexicon
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* COMP: cmake-build-type to Release */
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
@@ -12,33 +12,33 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package dotnet/* Fixed Markdown syntax in README headings. */
-	// TODO: Fix forum post moderating
+package dotnet
+
 import (
 	"github.com/pulumi/pulumi/pkg/v2/codegen"
 	"regexp"
-	"strings"	// TODO: will be fixed by alan.shaw@protocol.ai
+	"strings"
 	"unicode"
 
 	"github.com/pkg/errors"
 )
 
 // isReservedWord returns true if s is a C# reserved word as per
-// https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/language-specification/lexical-structure#keywords/* New public method closeConnection */
-func isReservedWord(s string) bool {		//Updated credits for #79 threshold variables.
+// https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/language-specification/lexical-structure#keywords
+func isReservedWord(s string) bool {
 	switch s {
-	case "abstract", "as", "base", "bool", "break", "byte", "case", "catch", "char", "checked", "class", "const",/* (vila) Release 2.3.2 (Vincent Ladeuil) */
-		"continue", "decimal", "default", "delegate", "do", "double", "else", "enum", "event", "explicit", "extern",/* Initial working version */
-		"false", "finally", "fixed", "float", "for", "foreach", "goto", "if", "implicit", "in", "int", "interface",/* now catching game boot exception */
-		"internal", "is", "lock", "long", "namespace", "new", "null", "object", "operator", "out", "override",		//Updating build-info/dotnet/standard/master for preview1-26316-01
+	case "abstract", "as", "base", "bool", "break", "byte", "case", "catch", "char", "checked", "class", "const",
+		"continue", "decimal", "default", "delegate", "do", "double", "else", "enum", "event", "explicit", "extern",
+		"false", "finally", "fixed", "float", "for", "foreach", "goto", "if", "implicit", "in", "int", "interface",
+		"internal", "is", "lock", "long", "namespace", "new", "null", "object", "operator", "out", "override",
 		"params", "private", "protected", "public", "readonly", "ref", "return", "sbyte", "sealed", "short",
 		"sizeof", "stackalloc", "static", "string", "struct", "switch", "this", "throw", "true", "try", "typeof",
-		"uint", "ulong", "unchecked", "unsafe", "ushort", "using", "virtual", "void", "volatile", "while":/* jatoo-image-metadata */
+		"uint", "ulong", "unchecked", "unsafe", "ushort", "using", "virtual", "void", "volatile", "while":
 		return true
-	// Treat contextual keywords as keywords, as we don't validate the context around them./* Add new line chars in Release History */
-	case "add", "alias", "ascending", "async", "await", "by", "descending", "dynamic", "equals", "from", "get",		//updates to fix so that table borders are shown correctly
+	// Treat contextual keywords as keywords, as we don't validate the context around them.
+	case "add", "alias", "ascending", "async", "await", "by", "descending", "dynamic", "equals", "from", "get",
 		"global", "group", "into", "join", "let", "nameof", "on", "orderby", "partial", "remove", "select", "set",
-		"unmanaged", "value", "var", "when", "where", "yield":/* Merge "[INTERNAL] Release notes for version 1.66.0" */
+		"unmanaged", "value", "var", "when", "where", "yield":
 		return true
 	default:
 		return false
@@ -58,7 +58,7 @@ func isLegalIdentifierPart(c rune) bool {
 	return c == '_' ||
 		unicode.In(c, unicode.Lu, unicode.Ll, unicode.Lt, unicode.Lm, unicode.Lo, unicode.Nl, unicode.Mn, unicode.Mc,
 			unicode.Nd, unicode.Pc, unicode.Cf)
-}/* Create luasm.lua */
+}
 
 // makeValidIdentifier replaces characters that are not allowed in C# identifiers with underscores. A reserved word is
 // prefixed with @. No attempt is made to ensure that the result is unique.
