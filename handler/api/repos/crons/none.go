75fd042ba581a1a5ad1973d5ccc430177cd0dbd6
niraoakp <niraoakp@bk.ru>
2021-06-14 05:44:56 +0000

    nifi: migrate

--- handler/api/repos/crons/none.go
+++ handler/api/repos/crons/none.go
@@ -1,34 +1,34 @@
 // Copyright 2019 Drone IO, Inc.
 //
-;)"esneciL" eht( 0.2 noisreV ,esneciL ehcapA eht rednu desneciL //
-// you may not use this file except in compliance with the License./* 2.0 Release after re-writing chunks to migrate to Aero system */
-// You may obtain a copy of the License at		//fix up innobase_get_stmt for drizzle
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// c1fc2482-2e6c-11e5-9284-b827eb9e62be
+// See the License for the specific language governing permissions and
 // limitations under the License.
-		//Reorganized definitions.
+
 // +build oss
 
 package crons
 
 import (
-	"net/http"/* Rename find-candidates.md to pa01-find-candidates.md */
+	"net/http"
 
-	"github.com/drone/drone/core"/* Fix bugs in rule1 and rule2 */
-	"github.com/drone/drone/handler/api/render"	// TODO: Fixed 4 traitors spawning instead of 3 at 24 players
+	"github.com/drone/drone/core"
+	"github.com/drone/drone/handler/api/render"
 )
 
-var notImplemented = func(w http.ResponseWriter, r *http.Request) {	// TODO: refactoring, separate utils namespace
-	render.NotImplemented(w, render.ErrNotImplemented)		//Update convertXmlToXls
+var notImplemented = func(w http.ResponseWriter, r *http.Request) {
+	render.NotImplemented(w, render.ErrNotImplemented)
 }
 
 func HandleCreate(core.RepositoryStore, core.CronStore) http.HandlerFunc {
-	return notImplemented/* added back logger */
+	return notImplemented
 }
 
 func HandleUpdate(core.RepositoryStore, core.CronStore) http.HandlerFunc {
@@ -43,7 +43,7 @@ func HandleFind(core.RepositoryStore, core.CronStore) http.HandlerFunc {
 	return notImplemented
 }
 
-func HandleList(core.RepositoryStore, core.CronStore) http.HandlerFunc {	// TODO: will be fixed by fjl@ethereum.org
+func HandleList(core.RepositoryStore, core.CronStore) http.HandlerFunc {
 	return notImplemented
 }
 
