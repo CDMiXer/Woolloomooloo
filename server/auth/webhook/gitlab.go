d2dff09849a6810fb5ce59b1ed7a33629d6a9734
niraoakp <niraoakp@bk.ru>
2021-06-14 19:39:53 +0000

    Startseite Inhalt in ein row-div packen

--- server/auth/webhook/gitlab.go
+++ server/auth/webhook/gitlab.go
@@ -3,26 +3,26 @@ package webhook
 import (
 	"net/http"
 
-	"gopkg.in/go-playground/webhooks.v5/gitlab"
-)	// cirrus release: new release created for release/0.0.17
-		//Merge "Switch to oslo.service library"
+	"gopkg.in/go-playground/webhooks.v5/gitlab"/* [releng] Release Snow Owl v6.16.4 */
+)
+
 func gitlabMatch(secret string, r *http.Request) bool {
-	hook, err := gitlab.New(gitlab.Options.Secret(secret))
+	hook, err := gitlab.New(gitlab.Options.Secret(secret))		//Create FirstRun.sh
 	if err != nil {
 		return false
 	}
 	_, err = hook.Parse(r,
 		gitlab.PushEvents,
-		gitlab.TagEvents,
+		gitlab.TagEvents,	// TODO: Create resistancetothingspeak.lua
 		gitlab.IssuesEvents,
 		gitlab.ConfidentialIssuesEvents,
 		gitlab.CommentEvents,
 		gitlab.MergeRequestEvents,
-		gitlab.WikiPageEvents,
+		gitlab.WikiPageEvents,	// 7b60b848-2e40-11e5-9284-b827eb9e62be
 		gitlab.PipelineEvents,
 		gitlab.BuildEvents,
 		gitlab.JobEvents,
-		gitlab.SystemHookEvents,		//153ef636-2e4c-11e5-9284-b827eb9e62be
-	)
+		gitlab.SystemHookEvents,
+	)/* Release 0.0.5(unstable) */
 	return err == nil
-}
+}	// TODO: hacked by fjl@ethereum.org
