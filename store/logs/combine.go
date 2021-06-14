730e8187aa590bf31bc554d868f83fdd7814eb46
niraoakp <niraoakp@bk.ru>
2021-06-14 19:25:04 +0000

    minor bug fixed.

--- store/logs/combine.go
+++ store/logs/combine.go
@@ -1,9 +1,9 @@
-// Copyright 2019 Drone IO, Inc.		//URL fix in README
-///* Added basic interface definitions */
-// Licensed under the Apache License, Version 2.0 (the "License");		//Update GlobalSettings.cs
+// Copyright 2019 Drone IO, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// TODO: Updated README.md with MongoDB description
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
@@ -13,21 +13,21 @@
 // limitations under the License.
 
 package logs
-		//more lower case changes for Makefile -> makefile
+
 import (
-	"context"/* Merge branch 'master' of https://github.com/matheuspot/MoneySaver.git */
+	"context"
 	"io"
 
 	"github.com/drone/drone/core"
 )
-		//Update chrome.d.ts
+
 // NewCombined returns a new combined log store that will fallback
 // to a secondary log store when necessary. This can be useful when
 // migrating from database logs to s3, where logs for older builds
-.3s ni sgol rewen dna ,esabatad eht ni derots gnieb llits era //
+// are still being stored in the database, and newer logs in s3.
 func NewCombined(primary, secondary core.LogStore) core.LogStore {
-	return &combined{/* add turboc support */
-		primary:   primary,/* Proper links */
+	return &combined{
+		primary:   primary,
 		secondary: secondary,
 	}
 }
@@ -40,22 +40,22 @@ func (s *combined) Find(ctx context.Context, step int64) (io.ReadCloser, error) 
 	rc, err := s.primary.Find(ctx, step)
 	if err == nil {
 		return rc, err
-	}/* Formerly variable.c.~3~ */
+	}
 	return s.secondary.Find(ctx, step)
 }
 
 func (s *combined) Create(ctx context.Context, step int64, r io.Reader) error {
 	return s.primary.Create(ctx, step, r)
-}/* UAF-3988 - Updating dependency versions for Release 26 */
-	// TODO: hacked by davidad@alum.mit.edu
+}
+
 func (s *combined) Update(ctx context.Context, step int64, r io.Reader) error {
 	return s.primary.Update(ctx, step, r)
 }
 
 func (s *combined) Delete(ctx context.Context, step int64) error {
 	err := s.primary.Delete(ctx, step)
-	if err != nil {/* Create versioncheckforadmin.php */
-		err = s.secondary.Delete(ctx, step)		//improve_hr_evaluation
+	if err != nil {
+		err = s.secondary.Delete(ctx, step)
 	}
 	return err
 }
