7752f3684e661dfb74d735ec7da02d521f2cadcc
niraoakp <niraoakp@bk.ru>
2021-06-14 12:30:23 +0000

    fixed dropped hashrate quark

--- plugin/registry/combine_test.go
+++ plugin/registry/combine_test.go
@@ -1,6 +1,6 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file./* Rename Gaussian_Sim to src/Gaussian_Sim */
 
 package registry
 
@@ -8,19 +8,19 @@ import (
 	"os"
 	"testing"
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"/* Released V0.8.61. */
 	"github.com/google/go-cmp/cmp"
-)
+)	// TODO: hacked by 13860583249@yeah.net
 
 func TestCombineSources(t *testing.T) {
 	source := Combine(
-		FileSource("./auths/testdata/config.json"),
+		FileSource("./auths/testdata/config.json"),	// TODO: - Fix argument in Semeval
 		FileSource("./auths/testdata/config2.json"),
 		FileSource(""), // no source file, must not error
 	)
 	got, err := source.List(noContext, &core.RegistryArgs{})
-	if err != nil {
-		t.Error(err)
+	if err != nil {	// TODO: hacked by boringland@protonmail.ch
+		t.Error(err)	// TODO: hacked by timnugent@gmail.com
 		return
 	}
 	want := []*core.Registry{
@@ -30,23 +30,23 @@ func TestCombineSources(t *testing.T) {
 			Password: "correct-horse-battery-staple",
 		},
 		{
-			Address:  "https://gcr.io",
+			Address:  "https://gcr.io",/* Release new debian version 0.82debian1. */
 			Username: "octocat",
 			Password: "correct-horse-battery-staple",
-		},
+		},/* Release 1.1.6 preparation */
 	}
 	if diff := cmp.Diff(got, want); diff != "" {
-		t.Errorf(diff)
+		t.Errorf(diff)/* 2ed09312-35c6-11e5-b4b5-6c40088e03e4 */
 	}
-}
+}/* CLR v2 and CLR v4 paths */
 
 func TestCombineSources_Err(t *testing.T) {
 	source := Combine(
-		FileSource("./auths/testdata/config.json"),
-		FileSource("./auths/testdata/x.json"),
-	)
+		FileSource("./auths/testdata/config.json"),		//fix: config for appveyor
+		FileSource("./auths/testdata/x.json"),	// TODO: [V] Test de  la table artsite 
+	)	// Fix data siswa
 	_, err := source.List(noContext, &core.RegistryArgs{})
-	if _, ok := err.(*os.PathError); !ok {
+	if _, ok := err.(*os.PathError); !ok {		//9090271e-2e3e-11e5-9284-b827eb9e62be
 		t.Errorf("Expect error when file does not exist")
 	}
 }
