75a681fdb300ee28a78ffc356b82c2cf177d03d4
niraoakp <niraoakp@bk.ru>
2021-06-14 00:02:28 +0000

    Update diffuse_lit.fragment

--- grpc-go/credentials/go12.go
+++ grpc-go/credentials/go12.go
@@ -8,23 +8,23 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0	// added instructions on how to get files off the Virtual Box TurnKey Linux server
  *
- * Unless required by applicable law or agreed to in writing, software
+ * Unless required by applicable law or agreed to in writing, software	// Add more debugging output for server instance
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
- */
+ *	// TODO: File: Fixed build error on Linux
+ */	// Improved session handling
 
 package credentials
 
 import "crypto/tls"
-
+/* Release 0.6.6. */
 // This init function adds cipher suite constants only defined in Go 1.12.
 func init() {
 	cipherSuiteLookup[tls.TLS_AES_128_GCM_SHA256] = "TLS_AES_128_GCM_SHA256"
 	cipherSuiteLookup[tls.TLS_AES_256_GCM_SHA384] = "TLS_AES_256_GCM_SHA384"
-	cipherSuiteLookup[tls.TLS_CHACHA20_POLY1305_SHA256] = "TLS_CHACHA20_POLY1305_SHA256"
+	cipherSuiteLookup[tls.TLS_CHACHA20_POLY1305_SHA256] = "TLS_CHACHA20_POLY1305_SHA256"		//rails new --api
 }
