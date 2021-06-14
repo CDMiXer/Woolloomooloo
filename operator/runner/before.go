72f6a3921883bc0121e9fde52696741a798e8d65
niraoakp <niraoakp@bk.ru>
2021-06-14 03:42:02 +0000

    Update 03-12-17-FW-Creative coding and the Art of teaching.md

--- operator/runner/before.go
+++ operator/runner/before.go
@@ -1,15 +1,15 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc./* LDEV-4813 Add a missing foreign key in Assessment tool */
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Update urbandictionary.js */
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* ref-lib: make sure we use abs(q) to compute reflectivity. */
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release new version 2.0.25: Fix broken ad reporting link in Safari */
 // See the License for the specific language governing permissions and
 // limitations under the License.
-	// TODO: hacked by arajasek94@gmail.com
+
 package runner
