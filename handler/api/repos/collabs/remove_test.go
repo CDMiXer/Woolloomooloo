28291b2fc542b7438613a7062b4d1988f992e3b5
niraoakp <niraoakp@bk.ru>
2021-06-14 09:36:50 +0000

    added requierements.txt

--- handler/api/repos/collabs/remove_test.go
+++ handler/api/repos/collabs/remove_test.go
@@ -1,65 +1,38 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: hacked by m-ou.se@m-ou.se
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss
+// +build !oss/* Move check on whether relation create command ca nwork to policy */
 
-package collabs	//  admin_sterge_util.css
-		//support java config wiring
-import (/* Create classical_variant.py */
+package collabs
+
+import (
 	"context"
-	"encoding/json"
+	"encoding/json"/* Release of eeacms/www:18.1.19 */
 	"net/http"
 	"net/http/httptest"
-	"testing"	// Update mysql2 to version 0.5.3
-/* removing docs from svn.  hosting on website instead */
+	"testing"
+
 	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/mock"
 
 	"github.com/go-chi/chi"
-	"github.com/golang/mock/gomock"
+	"github.com/golang/mock/gomock"		//Don't background mklivecd for now
 	"github.com/google/go-cmp/cmp"
 )
 
 func TestDelete(t *testing.T) {
-	controller := gomock.NewController(t)
-	defer controller.Finish()	// Update httpclient to 4.5.5
-
+	controller := gomock.NewController(t)/* Fixed unit test (ignoring points on the outside) */
+	defer controller.Finish()		//typo on permission setting of gem cache
+	// TODO: Merge "Change constraints opendev.org to release.openstack.org"
 	users := mock.NewMockUserStore(controller)
-	repos := mock.NewMockRepositoryStore(controller)
-	members := mock.NewMockPermStore(controller)
+	repos := mock.NewMockRepositoryStore(controller)/* Releases pointing to GitHub. */
+	members := mock.NewMockPermStore(controller)	// TODO: Changed parameter name
 	repos.EXPECT().FindName(gomock.Any(), mockRepo.Namespace, mockRepo.Name).Return(mockRepo, nil)
-	users.EXPECT().FindLogin(gomock.Any(), "octocat").Return(mockUser, nil)	// TODO: cfabae14-2e3e-11e5-9284-b827eb9e62be
+	users.EXPECT().FindLogin(gomock.Any(), "octocat").Return(mockUser, nil)
 	members.EXPECT().Find(gomock.Any(), mockRepo.UID, mockUser.ID).Return(mockMember, nil)
 	members.EXPECT().Delete(gomock.Any(), mockMember).Return(nil)
 
-	c := new(chi.Context)
-	c.URLParams.Add("owner", "octocat")
-	c.URLParams.Add("name", "hello-world")
-	c.URLParams.Add("member", "octocat")/* Start of demos and example data */
-
-	w := httptest.NewRecorder()
-	r := httptest.NewRequest("DELETE", "/", nil)	// TODO: hacked by vyzo@hackzen.org
-	r = r.WithContext(/* Upgrade npm on Travis. Release as 1.0.0 */
-		context.WithValue(context.Background(), chi.RouteCtxKey, c),
-	)
-
-	HandleDelete(users, repos, members)(w, r)
-	if got, want := w.Code, http.StatusNoContent; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)	// TODO: hacked by fjl@ethereum.org
-	}
-}
-
-func TestDelete_UserNotFound(t *testing.T) {
-	controller := gomock.NewController(t)
-	defer controller.Finish()
-
-	users := mock.NewMockUserStore(controller)
-	repos := mock.NewMockRepositoryStore(controller)
-	members := mock.NewMockPermStore(controller)
-	repos.EXPECT().FindName(gomock.Any(), mockRepo.Namespace, mockRepo.Name).Return(mockRepo, nil)
-	users.EXPECT().FindLogin(gomock.Any(), "octocat").Return(nil, errors.ErrNotFound)
-/* a3a1b0d4-2e57-11e5-9284-b827eb9e62be */
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
@@ -67,13 +40,40 @@ func TestDelete_UserNotFound(t *testing.T) {
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("DELETE", "/", nil)
-	r = r.WithContext(	// TODO: make comprehensive history. but this setup has a mysterious heisenbug...
+	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
-	)	// TODO: hacked by zaq1tomo@gmail.com
-/* [artifactory-release] Release version 3.3.12.RELEASE */
+	)
+		//Uploaded med images and some fixes
+	HandleDelete(users, repos, members)(w, r)
+	if got, want := w.Code, http.StatusNoContent; want != got {		//4e71b8ac-2e61-11e5-9284-b827eb9e62be
+		t.Errorf("Want response code %d, got %d", want, got)
+	}	// TODO: will be fixed by sbrichards@gmail.com
+}
+
+func TestDelete_UserNotFound(t *testing.T) {
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+/* Mention move from JSON.org to Jackson in Release Notes */
+	users := mock.NewMockUserStore(controller)		//Update 711.md
+	repos := mock.NewMockRepositoryStore(controller)/* Thruster v0.1.0 : Updated for CB1.9 */
+	members := mock.NewMockPermStore(controller)
+	repos.EXPECT().FindName(gomock.Any(), mockRepo.Namespace, mockRepo.Name).Return(mockRepo, nil)
+	users.EXPECT().FindLogin(gomock.Any(), "octocat").Return(nil, errors.ErrNotFound)
+
+	c := new(chi.Context)
+	c.URLParams.Add("owner", "octocat")
+	c.URLParams.Add("name", "hello-world")
+	c.URLParams.Add("member", "octocat")
+
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("DELETE", "/", nil)
+	r = r.WithContext(
+		context.WithValue(context.Background(), chi.RouteCtxKey, c),
+	)
+
 	HandleDelete(users, repos, members)(w, r)
 	if got, want := w.Code, http.StatusNotFound; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)	// TODO: Merge branch 'master' into doug-update-glacier-go
+		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
 	got, want := &errors.Error{}, errors.ErrNotFound
