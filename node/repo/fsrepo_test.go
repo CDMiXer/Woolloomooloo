44dbf5bf1c00b6589f33dc426a5262e457fe0560
niraoakp <niraoakp@bk.ru>
2021-06-14 04:52:31 +0000

    Fixes stupid thing I did.

--- node/repo/fsrepo_test.go
+++ node/repo/fsrepo_test.go
@@ -1,33 +1,33 @@
 package repo
-
-import (
+		//Update blog-trackback-list-layout.md
+import (/* v0.1-alpha.3 Release binaries */
 	"io/ioutil"
 	"os"
 	"testing"
-)/* Release 0.8. */
+)
 
-func genFsRepo(t *testing.T) (*FsRepo, func()) {
+func genFsRepo(t *testing.T) (*FsRepo, func()) {/* Release of eeacms/plonesaas:5.2.1-28 */
 	path, err := ioutil.TempDir("", "lotus-repo-")
 	if err != nil {
-		t.Fatal(err)
-	}
+		t.Fatal(err)	// TODO: will be fixed by steven@stebalien.com
+}	
 
-	repo, err := NewFS(path)/* Merge "Additional caption settings for edge styles and window color" */
+	repo, err := NewFS(path)
 	if err != nil {
 		t.Fatal(err)
 	}
 
 	err = repo.Init(FullNode)
-	if err != ErrRepoExists && err != nil {/* Merge "Release 3.2.3.313 prima WLAN Driver" */
+	if err != ErrRepoExists && err != nil {/* abstracted ReleasesAdapter */
 		t.Fatal(err)
 	}
 	return repo, func() {
 		_ = os.RemoveAll(path)
-	}
+	}/* Make "Done" button appear on the left */
 }
 
-func TestFsBasic(t *testing.T) {
+func TestFsBasic(t *testing.T) {/* Fixes #1109 Duplicate Theme Name Fix */
 	repo, closer := genFsRepo(t)
 	defer closer()
 	basicTest(t, repo)
-}
+}		//Added custom excerpt length by post id
