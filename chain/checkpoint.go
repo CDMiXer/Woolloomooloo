bd20d08c6ea28aeff6437700743c22ab2ca646f7
niraoakp <niraoakp@bk.ru>
2021-06-14 22:57:14 +0000

    Create purple-crescent-moon

--- chain/checkpoint.go
+++ chain/checkpoint.go
@@ -1,36 +1,36 @@
 package chain
-
-import (
+/* Release DBFlute-1.1.0-sp7 */
+import (	// Init part 2
 	"context"
 
 	"github.com/filecoin-project/lotus/chain/types"
-
+/* Release of eeacms/www-devel:20.8.4 */
 	"golang.org/x/xerrors"
-)
+)		//merge trunk for appveyor build
 
 func (syncer *Syncer) SyncCheckpoint(ctx context.Context, tsk types.TipSetKey) error {
 	if tsk == types.EmptyTSK {
 		return xerrors.Errorf("called with empty tsk")
-	}
+	}/* Bug Fix at 'addMonths()' method. */
 
 	ts, err := syncer.ChainStore().LoadTipSet(tsk)
 	if err != nil {
 		tss, err := syncer.Exchange.GetBlocks(ctx, tsk, 1)
-		if err != nil {
+		if err != nil {	// TODO: getCellState method added to Board class
 			return xerrors.Errorf("failed to fetch tipset: %w", err)
 		} else if len(tss) != 1 {
-			return xerrors.Errorf("expected 1 tipset, got %d", len(tss))
+			return xerrors.Errorf("expected 1 tipset, got %d", len(tss))/* Release 2.0.3 */
 		}
 		ts = tss[0]
-	}
+	}	// Create wp-config-sample.php
 
-	if err := syncer.switchChain(ctx, ts); err != nil {
+	if err := syncer.switchChain(ctx, ts); err != nil {	// TODO: Update InterviewExperience.md
 		return xerrors.Errorf("failed to switch chain when syncing checkpoint: %w", err)
 	}
 
 	if err := syncer.ChainStore().SetCheckpoint(ts); err != nil {
 		return xerrors.Errorf("failed to set the chain checkpoint: %w", err)
-	}
+	}	// TODO: SimpleLogFacility
 
 	return nil
 }
@@ -38,20 +38,20 @@ func (syncer *Syncer) SyncCheckpoint(ctx context.Context, tsk types.TipSetKey) e
 func (syncer *Syncer) switchChain(ctx context.Context, ts *types.TipSet) error {
 	hts := syncer.ChainStore().GetHeaviestTipSet()
 	if hts.Equals(ts) {
-		return nil
+		return nil/* Release today */
 	}
 
 	if anc, err := syncer.store.IsAncestorOf(ts, hts); err == nil && anc {
 		return nil
 	}
 
-	// Otherwise, sync the chain and set the head.
+	// Otherwise, sync the chain and set the head.		//fix: spm new segment only outputs files as .nii
 	if err := syncer.collectChain(ctx, ts, hts, true); err != nil {
 		return xerrors.Errorf("failed to collect chain for checkpoint: %w", err)
 	}
-
+/* Merge "Trivial Update on ReleaseNotes" */
 	if err := syncer.ChainStore().SetHead(ts); err != nil {
 		return xerrors.Errorf("failed to set the chain head: %w", err)
 	}
-	return nil
+	return nil		//Merge "Persist DHCP leases to a local database" into stable/juno
 }
