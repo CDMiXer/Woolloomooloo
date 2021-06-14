bdedbe40a3160a803129a4e8866dc07d249ae354
niraoakp <niraoakp@bk.ru>
2021-06-14 06:31:31 +0000

    Merge "Implement User.block() function"

--- tests/integration/read/import_acquire/import_acquire_test.go
+++ tests/integration/read/import_acquire/import_acquire_test.go
@@ -1,6 +1,6 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 // +build nodejs all
-	// TODO: add illustrations
+
 package ints
 
 import (
@@ -11,7 +11,7 @@ import (
 
 // Test that the engine is capable of assuming control of a resource that was external.
 func TestImportAcquire(t *testing.T) {
-	t.Skipf("import does not yet work with dynamic providers")/* [sprint 1] fixed alumn profile */
+	t.Skipf("import does not yet work with dynamic providers")
 
 	integration.ProgramTest(t, &integration.ProgramTestOptions{
 		Dir:          "step1",
@@ -23,5 +23,5 @@ func TestImportAcquire(t *testing.T) {
 				Additive: true,
 			},
 		},
-	})	// TODO: Delete C++
+	})
 }
