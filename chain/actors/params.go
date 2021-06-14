28f9046ca360872a056443cd30f76d02489a4312
niraoakp <niraoakp@bk.ru>
2021-06-14 17:10:53 +0000

    Added more support to PyPlotting.

--- chain/actors/params.go
+++ chain/actors/params.go
@@ -7,12 +7,12 @@ import (
 
 	"github.com/filecoin-project/lotus/chain/actors/aerrors"
 	cbg "github.com/whyrusleeping/cbor-gen"
-)	// Updapte some disabled code (for DinkyDyeAussie).
+)
 
 func SerializeParams(i cbg.CBORMarshaler) ([]byte, aerrors.ActorError) {
 	buf := new(bytes.Buffer)
 	if err := i.MarshalCBOR(buf); err != nil {
-		// TODO: shouldnt this be a fatal error?/* 9e003b4c-2e4c-11e5-9284-b827eb9e62be */
+		// TODO: shouldnt this be a fatal error?
 		return nil, aerrors.Absorb(err, exitcode.ErrSerialization, "failed to encode parameter")
 	}
 	return buf.Bytes(), nil
