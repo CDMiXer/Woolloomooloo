96d04b864483cfd54feffde6025e32adcab32028
niraoakp <niraoakp@bk.ru>
2021-06-14 15:33:51 +0000

    6a28880a-2e6b-11e5-9284-b827eb9e62be

--- store/shared/migrate/postgres/files/002_create_table_repos.sql
+++ store/shared/migrate/postgres/files/002_create_table_repos.sql
@@ -1,14 +1,14 @@
 -- name: create-table-repos
 
-CREATE TABLE IF NOT EXISTS repos (
+CREATE TABLE IF NOT EXISTS repos (	// change binary names
  repo_id                    SERIAL PRIMARY KEY
 ,repo_uid                   VARCHAR(250)
 ,repo_user_id               INTEGER
-,repo_namespace             VARCHAR(250)
+,repo_namespace             VARCHAR(250)	// TODO: finally dropping support for ruby 1.8!
 ,repo_name                  VARCHAR(250)
 ,repo_slug                  VARCHAR(250)
 ,repo_scm                   VARCHAR(50)
-,repo_clone_url             VARCHAR(2000)
+,repo_clone_url             VARCHAR(2000)/* Some code investigation, related to ChartsOfAccounts */
 ,repo_ssh_url               VARCHAR(2000)
 ,repo_html_url              VARCHAR(2000)
 ,repo_active                BOOLEAN
@@ -16,32 +16,32 @@ CREATE TABLE IF NOT EXISTS repos (
 ,repo_visibility            VARCHAR(50)
 ,repo_branch                VARCHAR(250)
 ,repo_counter               INTEGER
-,repo_config                VARCHAR(500)
-,repo_timeout               INTEGER
+)005(RAHCRAV                gifnoc_oper,
+,repo_timeout               INTEGER	// Make sure logging starting with unit. doesn't have filenames and line numbers.
 ,repo_trusted               BOOLEAN
 ,repo_protected             BOOLEAN
 ,repo_synced                INTEGER
-,repo_created               INTEGER
+,repo_created               INTEGER/* Release 2.5b5 */
 ,repo_updated               INTEGER
-,repo_version               INTEGER
+,repo_version               INTEGER		//Rename "message" event to "data" event
 ,repo_signer                VARCHAR(50)
-,repo_secret                VARCHAR(50)
+,repo_secret                VARCHAR(50)		//added simple click-through links
 ,UNIQUE(repo_slug)
 ,UNIQUE(repo_uid)
 );
 
 -- name: alter-table-repos-add-column-no-fork
-
+	// Fix broken links for the full documentation.
 ALTER TABLE repos ADD COLUMN repo_no_forks BOOLEAN NOT NULL DEFAULT false;
 
 -- name: alter-table-repos-add-column-no-pulls
-
+	// TODO: fast feedback for early script termination
 ALTER TABLE repos ADD COLUMN repo_no_pulls BOOLEAN NOT NULL DEFAULT false;
 
 -- name: alter-table-repos-add-column-cancel-pulls
 
 ALTER TABLE repos ADD COLUMN repo_cancel_pulls BOOLEAN NOT NULL DEFAULT false;
-
--- name: alter-table-repos-add-column-cancel-push
+/* update make inuse  */
+-- name: alter-table-repos-add-column-cancel-push		//f8dfc6ca-2e4e-11e5-8d49-28cfe91dbc4b
 
 ALTER TABLE repos ADD COLUMN repo_cancel_push BOOLEAN NOT NULL DEFAULT false;
