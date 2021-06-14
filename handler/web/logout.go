6f3f959a6bae88b26cc667c2f063b19c6283395b
niraoakp <niraoakp@bk.ru>
2021-06-14 17:51:08 +0000

    Create MiniEngine_SimpleWidgets.hpp

--- handler/web/logout.go
+++ handler/web/logout.go
@@ -1,12 +1,12 @@
 // Copyright 2019 Drone IO, Inc.
-//	// added notes on deploying and contributing to README
-// Licensed under the Apache License, Version 2.0 (the "License");/* Added <mime-mapping/> declaration for *.tex */
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// TODO: Try to fend off multiple DB queries at same time
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
@@ -21,9 +21,9 @@ import (
 )
 
 // HandleLogout creates an http.HandlerFunc that handles
-// session termination.	// 7c0c9b66-2e47-11e5-9284-b827eb9e62be
+// session termination.
 func HandleLogout() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {/* New tarball (r825) (0.4.6 Release Candidat) */
+	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Add("Set-Cookie", "_session_=deleted; Path=/; Max-Age=0")
 		w.Header().Set("Content-Type", "text/html; charset=UTF-8")
 		w.Write(
