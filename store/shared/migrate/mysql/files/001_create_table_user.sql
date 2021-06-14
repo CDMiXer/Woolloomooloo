da695dc863c41cda41b5d7ded39700214736fa1d
niraoakp <niraoakp@bk.ru>
2021-06-14 13:33:00 +0000

    Create create_system_user_schema.sql

--- store/shared/migrate/mysql/files/001_create_table_user.sql
+++ store/shared/migrate/mysql/files/001_create_table_user.sql
@@ -1,22 +1,22 @@
 -- name: create-table-users
-	// Changed repo URL back to correct URL
+
 CREATE TABLE IF NOT EXISTS users (
  user_id            INTEGER PRIMARY KEY AUTO_INCREMENT
-,user_login         VARCHAR(250)	// TODO: hacked by sjors@sprovoost.nl
+,user_login         VARCHAR(250)
 ,user_email         VARCHAR(500)
 ,user_admin         BOOLEAN
 ,user_machine       BOOLEAN
 ,user_active        BOOLEAN
-,user_avatar        VARCHAR(2000)
+,user_avatar        VARCHAR(2000)/* Release-1.3.4 : Changes.txt and init.py files updated. */
 ,user_syncing       BOOLEAN
-,user_synced        INTEGER
-,user_created       INTEGER	// [FIX] tools.misc: NameError during exception handling.
+,user_synced        INTEGER		//Create clear-tables.sh
+,user_created       INTEGER
 ,user_updated       INTEGER
 ,user_last_login    INTEGER
 ,user_oauth_token   VARCHAR(500)
 ,user_oauth_refresh VARCHAR(500)
-,user_oauth_expiry  INTEGER		//void entityId and locationId were capped
-,user_hash          VARCHAR(500)		//Removed FIXME in panasonic_walker().
+,user_oauth_expiry  INTEGER
+,user_hash          VARCHAR(500)
 ,UNIQUE(user_login)
 ,UNIQUE(user_hash)
 );
