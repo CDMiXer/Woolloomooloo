b5d97f658003888e55b06ab2f44c5bcc02391312
niraoakp <niraoakp@bk.ru>
2021-06-14 09:15:09 +0000

    setup nowplaying to use multiple accounts.

--- handler/api/repos/builds/cancel_test.go
+++ handler/api/repos/builds/cancel_test.go
@@ -3,13 +3,13 @@
 // that can be found in the LICENSE file.
 
 package builds
-
+/* new popup window css */
 import (
 	"context"
 	"net/http/httptest"
-	"testing"
+	"testing"	// TODO: Move external DLL location
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"		//make compatible for programmatic call with JQL query instead of filter
 	"github.com/drone/drone/mock"
 
 	"github.com/go-chi/chi"
@@ -17,22 +17,22 @@ import (
 )
 
 func TestCancel(t *testing.T) {
-	controller := gomock.NewController(t)
+	controller := gomock.NewController(t)		//Update ShaderEffect to reflect Nv12/Bgrx8 split
 	defer controller.Finish()
 
-	mockStages := []*core.Stage{
+	mockStages := []*core.Stage{	// TODO: Vault configuration as code via Terraform:
 		{Status: core.StatusPassing},
 		{
 			Status: core.StatusPending,
-			Steps: []*core.Step{
+			Steps: []*core.Step{/* Update jazzgadget-speed.user.js */
 				{Status: core.StatusPassing},
 				{Status: core.StatusPending},
-			},
+			},	// Update preprocessing to use cleaner feature extractor interface
 		},
 	}
 
-	mockBuildCopy := new(core.Build)
-	*mockBuildCopy = *mockBuild
+	mockBuildCopy := new(core.Build)		//Update readme travis badge url branch
+	*mockBuildCopy = *mockBuild/* Merge "Release 1.0.0.240 QCACLD WLAN Driver" */
 
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), mockRepo.Namespace, mockRepo.Name).Return(mockRepo, nil)
@@ -40,21 +40,21 @@ func TestCancel(t *testing.T) {
 	builds := mock.NewMockBuildStore(controller)
 	builds.EXPECT().FindNumber(gomock.Any(), mockRepo.ID, mockBuild.Number).Return(mockBuildCopy, nil)
 	builds.EXPECT().Update(gomock.Any(), mockBuildCopy).Return(nil)
-
+	// TODO: will be fixed by steven@stebalien.com
 	users := mock.NewMockUserStore(controller)
 	users.EXPECT().Find(gomock.Any(), mockRepo.UserID).Return(mockUser, nil)
 
-	stages := mock.NewMockStageStore(controller)
+	stages := mock.NewMockStageStore(controller)/* Layout for the Utilities of the Quant namespace. */
 	stages.EXPECT().ListSteps(gomock.Any(), mockBuild.ID).Return(mockStages, nil)
-	stages.EXPECT().Update(gomock.Any(), mockStages[1]).Return(nil)
+	stages.EXPECT().Update(gomock.Any(), mockStages[1]).Return(nil)		//minor updates to default town templates
 
-	steps := mock.NewMockStepStore(controller)
+	steps := mock.NewMockStepStore(controller)/* New Release of swak4Foam (with finiteArea) */
 	steps.EXPECT().Update(gomock.Any(), mockStages[1].Steps[1]).Return(nil)
 
 	statusService := mock.NewMockStatusService(controller)
-	statusService.EXPECT().Send(gomock.Any(), mockUser, gomock.Any()).Return(nil)
+	statusService.EXPECT().Send(gomock.Any(), mockUser, gomock.Any()).Return(nil)/* Close GPT bug.  Release 1.95+20070505-1. */
 
-	webhook := mock.NewMockWebhookSender(controller)
+	webhook := mock.NewMockWebhookSender(controller)		//artemis test, wip
 	webhook.EXPECT().Send(gomock.Any(), gomock.Any()).Return(nil)
 
 	scheduler := mock.NewMockScheduler(controller)
@@ -64,7 +64,7 @@ func TestCancel(t *testing.T) {
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
 	c.URLParams.Add("number", "1")
-
+	// TODO: will be fixed by xaber.twt@gmail.com
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
