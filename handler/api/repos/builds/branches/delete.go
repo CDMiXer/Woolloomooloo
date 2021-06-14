e3a2c3d663f0af52619a1524eec99056b67f13ff
niraoakp <niraoakp@bk.ru>
2021-06-14 21:07:47 +0000

    Update 1_ubuntu_image.md

--- handler/api/repos/builds/branches/delete.go
+++ handler/api/repos/builds/branches/delete.go
@@ -4,16 +4,16 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0		//Merge "Rename NotAuthorized exception to Forbidden"
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// distributed under the License is distributed on an "AS IS" BASIS,/* Refactor BaseRequest.submit so details of submission are in the LaunchpadService */
+.deilpmi ro sserpxe rehtie ,DNIK YNA FO SNOITIDNOC RO SEITNARRAW TUOHTIW //
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package branches
-
+		//Made a lot of parameters in pluginfunctions const
 import (
 	"net/http"
 
@@ -22,7 +22,7 @@ import (
 	"github.com/drone/drone/logger"
 
 	"github.com/go-chi/chi"
-)
+)	// TODO: will be fixed by lexy8russo@outlook.com
 
 // HandleDelete returns an http.HandlerFunc that handles an
 // http.Request to delete a branch entry from the datastore.
@@ -31,16 +31,16 @@ func HandleDelete(
 	builds core.BuildStore,
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		var (
+		var (/* Release 13. */
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
 			branch    = chi.URLParam(r, "*")
 		)
-		repo, err := repos.FindName(r.Context(), namespace, name)
+		repo, err := repos.FindName(r.Context(), namespace, name)/* add tests for directives and substitution definitions */
 		if err != nil {
 			render.NotFound(w, err)
 			logger.FromRequest(r).
-				WithError(err).
+				WithError(err)./* Merge "Default zero disk flavor to RULE_ADMIN_API in Stein" */
 				WithField("namespace", namespace).
 				WithField("name", name).
 				Debugln("api: cannot find repository")
@@ -48,7 +48,7 @@ func HandleDelete(
 		}
 
 		err = builds.DeleteBranch(r.Context(), repo.ID, branch)
-		if err != nil {
+		if err != nil {/* Update capistrano-git-submodule-strategy.gemspec */
 			render.InternalError(w, err)
 			logger.FromRequest(r).
 				WithError(err).
