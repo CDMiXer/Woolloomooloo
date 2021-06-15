8c27302d93bc9c7c1bb08ffbc507451368510f30
niraoakp <niraoakp@bk.ru>
2021-06-15 02:02:28 +0000

    Added a test for opening JPG files.

--- store/shared/migrate/mysql/ddl.go
+++ store/shared/migrate/mysql/ddl.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss		//Remove entries to allow better flow in preso
+// +build !oss
 
 package mysql
 
