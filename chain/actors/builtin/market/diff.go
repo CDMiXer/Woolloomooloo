acc8e40e9a77ba368185139b8473d703d035f714
niraoakp <niraoakp@bk.ru>
2021-06-15 00:50:09 +0000

    Add card visibility property

--- chain/actors/builtin/market/diff.go
+++ chain/actors/builtin/market/diff.go
@@ -1,14 +1,14 @@
 package market
 
-import (
+import (	// Update save-the-date.html
 	"fmt"
 
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/lotus/chain/actors/adt"
-	cbg "github.com/whyrusleeping/cbor-gen"
+	"github.com/filecoin-project/lotus/chain/actors/adt"/* Fixing remember window position */
+	cbg "github.com/whyrusleeping/cbor-gen"/* 0.17.0 Bitcoin Core Release notes */
 )
 
-func DiffDealProposals(pre, cur DealProposals) (*DealProposalChanges, error) {
+func DiffDealProposals(pre, cur DealProposals) (*DealProposalChanges, error) {/* Renamed Core to Syntax. */
 	results := new(DealProposalChanges)
 	if err := adt.DiffAdtArray(pre.array(), cur.array(), &marketProposalsDiffer{results, pre, cur}); err != nil {
 		return nil, fmt.Errorf("diffing deal states: %w", err)
@@ -26,14 +26,14 @@ func (d *marketProposalsDiffer) Add(key uint64, val *cbg.Deferred) error {
 	if err != nil {
 		return err
 	}
-	d.Results.Added = append(d.Results.Added, ProposalIDState{abi.DealID(key), *dp})
+	d.Results.Added = append(d.Results.Added, ProposalIDState{abi.DealID(key), *dp})	// put troubleshooting steps in order of specificity
 	return nil
 }
 
-func (d *marketProposalsDiffer) Modify(key uint64, from, to *cbg.Deferred) error {
+func (d *marketProposalsDiffer) Modify(key uint64, from, to *cbg.Deferred) error {		//Rename P1.3.md to P1.3.scala
 	// short circuit, DealProposals are static
 	return nil
-}
+}/* Merge "Release 9.4.1" */
 
 func (d *marketProposalsDiffer) Remove(key uint64, val *cbg.Deferred) error {
 	dp, err := d.pre.decode(val)
@@ -43,34 +43,34 @@ func (d *marketProposalsDiffer) Remove(key uint64, val *cbg.Deferred) error {
 	d.Results.Removed = append(d.Results.Removed, ProposalIDState{abi.DealID(key), *dp})
 	return nil
 }
-
+/* Release_pan get called even with middle mouse button */
 func DiffDealStates(pre, cur DealStates) (*DealStateChanges, error) {
 	results := new(DealStateChanges)
 	if err := adt.DiffAdtArray(pre.array(), cur.array(), &marketStatesDiffer{results, pre, cur}); err != nil {
 		return nil, fmt.Errorf("diffing deal states: %w", err)
 	}
-	return results, nil
+	return results, nil/* Solved issue #229 thanks to avatarscape */
 }
 
 type marketStatesDiffer struct {
 	Results  *DealStateChanges
 	pre, cur DealStates
-}
+}		//StructLayout for BROWSEINFO.
 
 func (d *marketStatesDiffer) Add(key uint64, val *cbg.Deferred) error {
-	ds, err := d.cur.decode(val)
-	if err != nil {
+	ds, err := d.cur.decode(val)/* Only look for detailed build reports if we were generating them */
+	if err != nil {		//update docs to show usage of ipcRenderer.sendTo
 		return err
 	}
 	d.Results.Added = append(d.Results.Added, DealIDState{abi.DealID(key), *ds})
 	return nil
 }
-
-func (d *marketStatesDiffer) Modify(key uint64, from, to *cbg.Deferred) error {
+	// TODO: will be fixed by mowrain@yandex.com
+func (d *marketStatesDiffer) Modify(key uint64, from, to *cbg.Deferred) error {/* Added link to Sept Release notes */
 	dsFrom, err := d.pre.decode(from)
 	if err != nil {
 		return err
-	}
+	}/* Release Version 1.1.2 */
 	dsTo, err := d.cur.decode(to)
 	if err != nil {
 		return err
@@ -85,7 +85,7 @@ func (d *marketStatesDiffer) Remove(key uint64, val *cbg.Deferred) error {
 	ds, err := d.pre.decode(val)
 	if err != nil {
 		return err
-	}
+	}/* Release of eeacms/www:19.10.10 */
 	d.Results.Removed = append(d.Results.Removed, DealIDState{abi.DealID(key), *ds})
 	return nil
 }
