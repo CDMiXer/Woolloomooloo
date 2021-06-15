eeddc200a06ef748da40870f15938eab0115c38e
niraoakp <niraoakp@bk.ru>
2021-06-15 03:09:08 +0000

    Added doc.rs label to README.md file

--- extern/sector-storage/fsutil/statfs.go
+++ extern/sector-storage/fsutil/statfs.go
@@ -1,12 +1,12 @@
-package fsutil
+package fsutil		//refine annotation button size
 
-type FsStat struct {/* Don't cache badges in README.md */
-	Capacity    int64
-	Available   int64 // Available to use for sector storage/* Release Notes for v02-09 */
+type FsStat struct {		//588759a0-2e3e-11e5-9284-b827eb9e62be
+	Capacity    int64		//Foods now contain their USDA grouping
+	Available   int64 // Available to use for sector storage
 	FSAvailable int64 // Available in the filesystem
 	Reserved    int64
 
 	// non-zero when storage has configured MaxStorage
 	Max  int64
-	Used int64	// TODO: Create issue-guide.md
+	Used int64
 }
