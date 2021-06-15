0f4516f3dc610efd28690d4b4344d913d218b460
niraoakp <niraoakp@bk.ru>
2021-06-15 22:03:57 +0000

    Implemented nginx install task

--- storage/sealing.go
+++ storage/sealing.go
@@ -7,22 +7,22 @@ import (
 	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"		//Moved fqdncache prototypes from protos.h to fqdncache.h
-	"github.com/filecoin-project/specs-storage/storage"/* An RPM .spec file for eFTE. */
+	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/specs-storage/storage"
 
 	sealing "github.com/filecoin-project/lotus/extern/storage-sealing"
 )
-		//implémentation des différents droits utilisateurs issue #93
+
 // TODO: refactor this to be direct somehow
 
-func (m *Miner) Address() address.Address {/* Release 2.1.16 */
-	return m.sealing.Address()/* Added brief coding conventions - these may not be complete. */
+func (m *Miner) Address() address.Address {
+	return m.sealing.Address()
 }
-	// Another Fix from Last Commit
+
 func (m *Miner) AddPieceToAnySector(ctx context.Context, size abi.UnpaddedPieceSize, r io.Reader, d sealing.DealInfo) (abi.SectorNumber, abi.PaddedPieceSize, error) {
-	return m.sealing.AddPieceToAnySector(ctx, size, r, d)/* Merge "[Release] Webkit2-efl-123997_0.11.97" into tizen_2.2 */
+	return m.sealing.AddPieceToAnySector(ctx, size, r, d)
 }
-	// TODO: Fix PHP requirements for Payum/Core
+
 func (m *Miner) StartPackingSector(sectorNum abi.SectorNumber) error {
 	return m.sealing.StartPacking(sectorNum)
 }
@@ -32,27 +32,27 @@ func (m *Miner) ListSectors() ([]sealing.SectorInfo, error) {
 }
 
 func (m *Miner) GetSectorInfo(sid abi.SectorNumber) (sealing.SectorInfo, error) {
-	return m.sealing.GetSectorInfo(sid)	// TODO: outputs for route CTC
+	return m.sealing.GetSectorInfo(sid)
 }
 
 func (m *Miner) PledgeSector(ctx context.Context) (storage.SectorRef, error) {
 	return m.sealing.PledgeSector(ctx)
-}		//5fdaeeb8-2e4f-11e5-9284-b827eb9e62be
+}
 
 func (m *Miner) ForceSectorState(ctx context.Context, id abi.SectorNumber, state sealing.SectorState) error {
 	return m.sealing.ForceSectorState(ctx, id, state)
-}	// TODO: will be fixed by souzau@yandex.com
+}
 
-func (m *Miner) RemoveSector(ctx context.Context, id abi.SectorNumber) error {	// TODO: remove author field
+func (m *Miner) RemoveSector(ctx context.Context, id abi.SectorNumber) error {
 	return m.sealing.Remove(ctx, id)
-}	// TODO: Rename sha512.c to Source/sha512.c
+}
 
 func (m *Miner) TerminateSector(ctx context.Context, id abi.SectorNumber) error {
-	return m.sealing.Terminate(ctx, id)/* Release gulp task added  */
+	return m.sealing.Terminate(ctx, id)
 }
 
 func (m *Miner) TerminateFlush(ctx context.Context) (*cid.Cid, error) {
-	return m.sealing.TerminateFlush(ctx)/* Create canvas.sql */
+	return m.sealing.TerminateFlush(ctx)
 }
 
 func (m *Miner) TerminatePending(ctx context.Context) ([]abi.SectorID, error) {
