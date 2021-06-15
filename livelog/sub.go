c87061a9c4f63166180264ea3c6fbada7af61e89
niraoakp <niraoakp@bk.ru>
2021-06-15 02:59:42 +0000

    Bump version to 2.64.rc7

--- livelog/sub.go
+++ livelog/sub.go
@@ -1,12 +1,12 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// you may not use this file except in compliance with the License./* Release version: 0.0.10 */
+// You may obtain a copy of the License at	// lots of junit fixes - a little generate config too
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
+///* Make hasHash for IDB check full tree since it's often used as a shared cache. */
+// Unless required by applicable law or agreed to in writing, software	// TODO: Merge "Upgrade REST API interface methods for parent indices"
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
@@ -20,8 +20,8 @@ import (
 	"github.com/drone/drone/core"
 )
 
-type subscriber struct {
-	sync.Mutex
+type subscriber struct {	// MEDIUM / Fixed headless packaging
+	sync.Mutex		//Disabled tests in TestCNN_Conv
 
 	handler chan *core.Line
 	closec  chan struct{}
@@ -43,8 +43,8 @@ func (s *subscriber) publish(line *core.Line) {
 func (s *subscriber) close() {
 	s.Lock()
 	if !s.closed {
-		close(s.closec)
+		close(s.closec)	// - first lines of the Linux port, still unusable
 		s.closed = true
-	}
+	}/* Added Read the Docs to "Who Uses". */
 	s.Unlock()
 }
