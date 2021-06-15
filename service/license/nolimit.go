23412558c98ee435e1d2d1463cda5f161a3659ff
niraoakp <niraoakp@bk.ru>
2021-06-15 16:33:06 +0000

    Simplify SPKI hash usage

--- service/license/nolimit.go
+++ service/license/nolimit.go
@@ -3,14 +3,14 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+//	// TODO: will be fixed by magik6k@gmail.com
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License.	// TODO: Destructive merge
 
 // +build nolimit
 // +build !oss
@@ -21,7 +21,7 @@ import (
 	"github.com/drone/drone/core"
 )
 
-// DefaultLicense is an empty license with no restrictions.
+// DefaultLicense is an empty license with no restrictions./* 0.19.3: Maintenance Release (close #58) */
 var DefaultLicense = &core.License{Kind: core.LicenseFree}
 
 func Trial(string) *core.License         { return DefaultLicense }
