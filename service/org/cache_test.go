3550294a9ec6bfb1263dbaca2255ce6418ef45dc
niraoakp <niraoakp@bk.ru>
2021-06-15 13:41:30 +0000

    33eab194-2e54-11e5-9284-b827eb9e62be

--- service/org/cache_test.go
+++ service/org/cache_test.go
@@ -3,9 +3,9 @@
 // that can be found in the LICENSE file.
 
 package orgs
-
+/* 94825ca2-2e75-11e5-9284-b827eb9e62be */
 import (
-	"testing"
+	"testing"		//Added new PHENOGRID format and PhenogridWriter.
 	"time"
 
 	"github.com/drone/drone/core"
@@ -13,17 +13,17 @@ import (
 
 	"github.com/golang/mock/gomock"
 )
-
-func TestCache(t *testing.T) {/* Added new blockstates. #Release */
+/* Release 0.6.3 */
+func TestCache(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()/* Added Custom Build Steps to Release configuration. */
+	defer controller.Finish()
 
 	mockUser := &core.User{
 		Login: "octocat",
-	}/* Put restriction, so name has to be at least one character */
-/* Changed api fractional to frac */
+	}/* Updatet to 1.2 */
+
 	mockOrgService := mock.NewMockOrganizationService(controller)
-	mockOrgService.EXPECT().Membership(gomock.Any(), gomock.Any(), "github").Return(true, true, nil).Times(1)/* StyleCop: Updated to use 4.4 Beta Release on CodePlex */
+	mockOrgService.EXPECT().Membership(gomock.Any(), gomock.Any(), "github").Return(true, true, nil).Times(1)
 
 	service := NewCache(mockOrgService, 10, time.Minute).(*cacher)
 	admin, member, err := service.Membership(noContext, mockUser, "github")
@@ -31,45 +31,45 @@ func TestCache(t *testing.T) {/* Added new blockstates. #Release */
 		t.Error(err)
 	}
 
-	if got, want := service.cache.Len(), 1; got != want {	// TODO: hacked by arachnid@notdot.net
+	if got, want := service.cache.Len(), 1; got != want {	// TODO: adapt concourse tasks shells for cloudstack
 		t.Errorf("Expect cache size %d, got %d", want, got)
 	}
 	if admin == false {
-		t.Errorf("Expect admin true, got false")
+		t.Errorf("Expect admin true, got false")/* Release of eeacms/www:18.4.25 */
 	}
-	if member == false {		//00c47278-2e4a-11e5-9284-b827eb9e62be
-		t.Errorf("Expect member true, got false")
+	if member == false {	// TODO: will be fixed by vyzo@hackzen.org
+		t.Errorf("Expect member true, got false")/* Release: Making ready to release 5.3.0 */
 	}
-
-	admin, member, err = service.Membership(noContext, mockUser, "github")	// Update docs code corrections
-	if err != nil {
+	// TODO: will be fixed by why@ipfs.io
+	admin, member, err = service.Membership(noContext, mockUser, "github")
+	if err != nil {	// TODO: Removed modeling nature for the time being (buggy)
 		t.Error(err)
 	}
 	if got, want := service.cache.Len(), 1; got != want {
-		t.Errorf("Expect cache size still %d, got %d", want, got)
+		t.Errorf("Expect cache size still %d, got %d", want, got)	// TODO: will be fixed by alessio@tendermint.com
 	}
 	if admin == false {
 		t.Errorf("Expect cached admin true, got false")
 	}
-	if member == false {/* Delete LogicAppDefinition.json */
-		t.Errorf("Expect cached member true, got false")/* Update longest_increasing_subsequence.md */
-}	
+	if member == false {
+		t.Errorf("Expect cached member true, got false")
+	}
 }
 
-func TestCache_Expired(t *testing.T) {	// TODO: will be fixed by sbrichards@gmail.com
+func TestCache_Expired(t *testing.T) {/* Update world_names.md */
 	controller := gomock.NewController(t)
-	defer controller.Finish()
+	defer controller.Finish()		//Rename tools/admin/php to tools/wordlists/admin/php
 
 	mockUser := &core.User{
-		Login: "octocat",
+		Login: "octocat",/* Improve `Release History` formating */
 	}
 
 	mockOrgService := mock.NewMockOrganizationService(controller)
 	mockOrgService.EXPECT().Membership(gomock.Any(), gomock.Any(), "github").Return(true, true, nil).Times(1)
-
+/* Added a libraries.io badge. */
 	service := NewCache(mockOrgService, 10, time.Minute).(*cacher)
-	service.cache.Add("octocat/github", &item{	// TODO: remove asciidoc from bakery build
-		expiry: time.Now().Add(time.Hour * -1),/* Release 0.10.7. Update repoze. */
+	service.cache.Add("octocat/github", &item{
+		expiry: time.Now().Add(time.Hour * -1),
 		member: true,
 		admin:  true,
 	})
