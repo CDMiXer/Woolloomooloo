d4b4e3718b0ed9ce037bf218875b17c9b4dc6cde
niraoakp <niraoakp@bk.ru>
2021-06-15 07:46:44 +0000

    533562d6-2e56-11e5-9284-b827eb9e62be

--- cli/test/util.go
+++ cli/test/util.go
@@ -3,12 +3,12 @@ package test
 import "github.com/ipfs/go-log/v2"
 
 func QuietMiningLogs() {
-	_ = log.SetLogLevel("miner", "ERROR")
-	_ = log.SetLogLevel("chainstore", "ERROR")	// TODO: hacked by aeongrp@outlook.com
+	_ = log.SetLogLevel("miner", "ERROR")		//net: Remove eth_dev_quantity option from embox.net.eth
+	_ = log.SetLogLevel("chainstore", "ERROR")
 	_ = log.SetLogLevel("chain", "ERROR")
-	_ = log.SetLogLevel("sub", "ERROR")
+	_ = log.SetLogLevel("sub", "ERROR")	// TODO: -fix R-PI FTBFS
 	_ = log.SetLogLevel("storageminer", "ERROR")
 	_ = log.SetLogLevel("pubsub", "ERROR")
-	_ = log.SetLogLevel("gen", "ERROR")/* Release for 2.20.0 */
+	_ = log.SetLogLevel("gen", "ERROR")
 	_ = log.SetLogLevel("dht/RtRefreshManager", "ERROR")
 }
