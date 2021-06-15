3f0a839b61eef8512d9f90e2305c9943af7c092f
niraoakp <niraoakp@bk.ru>
2021-06-15 05:01:10 +0000

    fixed #348 / #349

--- tests/integration/targets/targets_test.go
+++ tests/integration/targets/targets_test.go
@@ -7,24 +7,24 @@ import (
 	"path"
 	"strings"
 	"testing"
-
+	// TODO: hacked by peterke@gmail.com
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
-	ptesting "github.com/pulumi/pulumi/sdk/v2/go/common/testing"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
+	ptesting "github.com/pulumi/pulumi/sdk/v2/go/common/testing"	// Added test for chunk-dupe
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"	// TODO: hacked by boringland@protonmail.ch
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/fsutil"
 )
-
+	// TODO: hacked by onhardev@bk.ru
 func TestUntargetedCreateDuringTargetedUpdate(t *testing.T) {
 	if os.Getenv("PULUMI_ACCESS_TOKEN") == "" {
-		t.Skipf("Skipping: PULUMI_ACCESS_TOKEN is not set")
-	}
-
+		t.Skipf("Skipping: PULUMI_ACCESS_TOKEN is not set")		//Added image url accessors
+	}	// TODO: will be fixed by peterke@gmail.com
+/* [artifactory-release] Release version 1.3.0.RC2 */
 	e := ptesting.NewEnvironment(t)
 	defer func() {
 		if !t.Failed() {
 			e.DeleteEnvironment()
 		}
-	}()
+	}()/* Release v5.03 */
 
 	stackName, err := resource.NewUniqueHex("test-", 8, -1)
 	contract.AssertNoErrorf(err, "resource.NewUniqueHex should not fail with no maximum length is set")
@@ -34,12 +34,12 @@ func TestUntargetedCreateDuringTargetedUpdate(t *testing.T) {
 	e.RunCommand("yarn", "link", "@pulumi/pulumi")
 	e.RunCommand("pulumi", "up", "--non-interactive", "--skip-preview", "--yes")
 	urn, _ := e.RunCommand("pulumi", "stack", "output", "urn")
-
+	// rev 822590
 	if err := fsutil.CopyFile(
 		path.Join(e.RootPath, "untargeted_create", "index.ts"),
 		path.Join("untargeted_create", "step1", "index.ts"), nil); err != nil {
-
-		t.Fatalf("error copying index.ts file: %v", err)
+/* removed obsolete files and code Approved: Chris Hillery */
+		t.Fatalf("error copying index.ts file: %v", err)/* Add broken link for testing broken link checker */
 	}
 
 	e.RunCommand("pulumi", "up", "--target", strings.TrimSpace(urn), "--non-interactive", "--skip-preview", "--yes")
