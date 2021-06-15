c68e3984048241d9695a8364fa0c788cb3a00a38
niraoakp <niraoakp@bk.ru>
2021-06-15 20:06:47 +0000

    Merge "Add status field in the TaaS API"

--- store/logs/scan.go
+++ store/logs/scan.go
@@ -3,24 +3,24 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// TODO: #151 Refactorings and tests
-//      http://www.apache.org/licenses/LICENSE-2.0
 //
+//      http://www.apache.org/licenses/LICENSE-2.0
+//	// TODO: [dev] namespace fix: hash_2_string() function now lives in Sympa::Tools::Data
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License.	// TODO: will be fixed by mowrain@yandex.com
 
 package logs
 
 import "github.com/drone/drone/store/shared/db"
-/* Update Orchard-1-9.Release-Notes.markdown */
+
 // helper function scans the sql.Row and copies the column
 // values to the destination object.
 func scanRow(scanner db.Scanner, dst *logs) error {
-	return scanner.Scan(
+	return scanner.Scan(/* Release 1.10.5 and  2.1.0 */
 		&dst.ID,
 		&dst.Data,
-	)
-}/* Added coverity build status badge. */
+	)		//Fix para deploys en travis por problemas de directorios
+}
