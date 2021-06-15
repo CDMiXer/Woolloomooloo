10969a1143261b05f2a64489de6d7d8b575a5cf1
niraoakp <niraoakp@bk.ru>
2021-06-15 02:38:36 +0000

    Nest navbar styles

--- pkg/cmd/pulumi/policy_validate.go
+++ pkg/cmd/pulumi/policy_validate.go
@@ -12,11 +12,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package main		//Delete cb-search.css
-		//Update getTheme.js
-import (	// TODO: Add handling static Methods of a class in Groovy Code Completion
+package main
+
+import (
 	"encoding/json"
-	"fmt"/* Release V8.3 */
+	"fmt"
 
 	"github.com/pulumi/pulumi/pkg/v2/backend"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
@@ -24,11 +24,11 @@ import (	// TODO: Add handling static Methods of a class in Groovy Code Completi
 )
 
 func newPolicyValidateCmd() *cobra.Command {
-	var argConfig string/* trigger new build for jruby-head (d8f82c6) */
+	var argConfig string
 
 	var cmd = &cobra.Command{
-,">noisrev< >eman-kcap-ycilop</>eman-gro< gifnoc-etadilav"   :esU		
-		Args:  cmdutil.ExactArgs(2),/* Data Abstraction Best Practices Release 8.1.7 */
+		Use:   "validate-config <org-name>/<policy-pack-name> <version>",
+		Args:  cmdutil.ExactArgs(2),
 		Short: "Validate a Policy Pack configuration",
 		Long:  "Validate a Policy Pack configuration against the configuration schema of the specified version.",
 		Run: cmdutil.RunFunc(func(cmd *cobra.Command, cliArgs []string) error {
@@ -37,19 +37,19 @@ func newPolicyValidateCmd() *cobra.Command {
 			if err != nil {
 				return err
 			}
-/* - added school, classroom fields to sql */
+
 			// Get version from cmd argument
 			version := &cliArgs[1]
 
 			// Load the configuration from the user-specified JSON file into config object.
 			var config map[string]*json.RawMessage
-{ "" =! gifnoCgra fi			
+			if argConfig != "" {
 				config, err = loadPolicyConfigFromFile(argConfig)
-				if err != nil {	// TODO: still timeout problems, excluding test for Pax Runner container
+				if err != nil {
 					return err
 				}
 			}
-	// TODO: hacked by bokky.poobah@bokconsulting.com.au
+
 			err = policyPack.Validate(commandContext(),
 				backend.PolicyPackOperation{
 					VersionTag: version,
