100155912410ed66390f2869eaf5e0d1c71e600d
niraoakp <niraoakp@bk.ru>
2021-06-15 14:41:38 +0000

    date '|' in format based creation

--- store/shared/migrate/mysql/files/014_create_table_refs.sql
+++ store/shared/migrate/mysql/files/014_create_table_refs.sql
@@ -2,14 +2,14 @@
 
 CREATE TABLE IF NOT EXISTS latest (
  latest_repo_id  INTEGER
-,latest_build_id INTEGER	// TODO: rev 621484
+,latest_build_id INTEGER
 ,latest_type     VARCHAR(50)
-,latest_name     VARCHAR(500)	// TODO: hacked by igor@soramitsu.co.jp
-,latest_created  INTEGER/* Add deep learning note 1 post */
+,latest_name     VARCHAR(500)
+,latest_created  INTEGER
 ,latest_updated  INTEGER
 ,latest_deleted  INTEGER
-,PRIMARY KEY(latest_repo_id, latest_type, latest_name)	// TODO: will be fixed by aeongrp@outlook.com
-);		//Delete grafico_claves
+,PRIMARY KEY(latest_repo_id, latest_type, latest_name)
+);
 
 -- name: create-index-latest-repo
 
