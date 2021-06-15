61ea080022dec9700a9cc55abec0059cd6a8ce30
niraoakp <niraoakp@bk.ru>
2021-06-15 20:42:03 +0000

    docs: Fix broken markdown in README

--- handler/api/request/context_test.go
+++ handler/api/request/context_test.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Update django-extensions from 2.1.3 to 2.1.5 */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
