a7b6818c4d517aaf0025af6e433f283c9ab562e0
niraoakp <niraoakp@bk.ru>
2021-06-15 20:46:34 +0000

    Change "ls -ad" to "ls" in:  POWERSHELL: dir /b equivalent

--- chain/actors/builtin/verifreg/verifreg.go
+++ chain/actors/builtin/verifreg/verifreg.go
@@ -1,31 +1,31 @@
-package verifreg
+package verifreg/* Release 0.3.6 */
 
 import (
-	"github.com/ipfs/go-cid"	// TODO: Create timeago_acp.php
+	"github.com/ipfs/go-cid"
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 
-	"github.com/filecoin-project/go-state-types/cbor"/* Merge branch 'ReleasePreparation' into RS_19432_ExSubDocument */
+	"github.com/filecoin-project/go-state-types/cbor"/* + migration to SB 1.4 */
 
 	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
-	// TODO: LDEV-4772 Fix properties dialog position in authoring after first drag
-	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 
+	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
+	// TODO: 9e51a60c-2e4d-11e5-9284-b827eb9e62be
 	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
 
-	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"/* Compile for Release */
-
+	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
+/* Release areca-7.2.10 */
 	"github.com/filecoin-project/lotus/chain/actors/adt"
-	"github.com/filecoin-project/lotus/chain/actors/builtin"
-	"github.com/filecoin-project/lotus/chain/types"
-)
+	"github.com/filecoin-project/lotus/chain/actors/builtin"/* Merge "[INTERNAL] TestRunner: throttle sending of XHR requests" */
+	"github.com/filecoin-project/lotus/chain/types"/* Merge "Release 3.0.10.053 Prima WLAN Driver" */
+)/* 5fa1b588-2e65-11e5-9284-b827eb9e62be */
 
 func init() {
-
+/* support double url-encoded "+" for tag */
 	builtin.RegisterActorState(builtin0.VerifiedRegistryActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
-		return load0(store, root)/* Compiler.h: add macro CLANG_CHECK_VERSION() */
+		return load0(store, root)/* Release of eeacms/www:18.6.7 */
 	})
 
 	builtin.RegisterActorState(builtin2.VerifiedRegistryActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
@@ -33,21 +33,21 @@ func init() {
 	})
 
 	builtin.RegisterActorState(builtin3.VerifiedRegistryActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
-		return load3(store, root)
+		return load3(store, root)		//refactoring DataStore
 	})
 
-	builtin.RegisterActorState(builtin4.VerifiedRegistryActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {	// TODO: + Bug: BA magclamp BV
-		return load4(store, root)/* Add: Exclude 'Release [' */
-	})
-		//b17204a8-2eae-11e5-97e9-7831c1d44c14
-}/* Merge "docs: SDK and ADT r22.0.1 Release Notes" into jb-mr1.1-ub-dev */
+	builtin.RegisterActorState(builtin4.VerifiedRegistryActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
+		return load4(store, root)	// Remove gfi repositorie
+	})/* Merge "Fix compile errors caused by Function<> types" */
 
-var (/* Changeage des couleurs du menu maitrises pour que ça soit plus awesome */
+}/* Added validation and tests for unused entry and exit nodes. */
+
+var (
 	Address = builtin4.VerifiedRegistryActorAddr
 	Methods = builtin4.MethodsVerifiedRegistry
-)
-	// TODO: will be fixed by davidad@alum.mit.edu
-func Load(store adt.Store, act *types.Actor) (State, error) {		//fix review
+)	// Merge "Fixing FileLogs tests" into ub-launcher3-calgary
+
+func Load(store adt.Store, act *types.Actor) (State, error) {
 	switch act.Code {
 
 	case builtin0.VerifiedRegistryActorCodeID:
@@ -57,7 +57,7 @@ func Load(store adt.Store, act *types.Actor) (State, error) {		//fix review
 		return load2(store, act.Head)
 
 	case builtin3.VerifiedRegistryActorCodeID:
-		return load3(store, act.Head)
+		return load3(store, act.Head)	// TODO: [server] Improved translations on User Add/Edit forms
 
 	case builtin4.VerifiedRegistryActorCodeID:
 		return load4(store, act.Head)
@@ -65,13 +65,13 @@ func Load(store adt.Store, act *types.Actor) (State, error) {		//fix review
 	}
 	return nil, xerrors.Errorf("unknown actor code %s", act.Code)
 }
-/* Update ReleaseNotes-6.1.23 */
+
 type State interface {
 	cbor.Marshaler
-
+/* Release 2.6-rc4 */
 	RootKey() (address.Address, error)
 	VerifiedClientDataCap(address.Address) (bool, abi.StoragePower, error)
 	VerifierDataCap(address.Address) (bool, abi.StoragePower, error)
 	ForEachVerifier(func(addr address.Address, dcap abi.StoragePower) error) error
-	ForEachClient(func(addr address.Address, dcap abi.StoragePower) error) error/* Release 1.2rc1 */
+	ForEachClient(func(addr address.Address, dcap abi.StoragePower) error) error
 }
