61728546b46bb585c2b7dbf348b32e3a0c671cfa
niraoakp <niraoakp@bk.ru>
2021-06-15 08:27:51 +0000

    Add gocover.io test coverage badges.

--- tests/integration/stack_reference/python/step2/__main__.py
+++ tests/integration/stack_reference/python/step2/__main__.py
@@ -3,16 +3,16 @@
 import pulumi
 
 config = pulumi.Config()
-org = config.require('org')
+org = config.require('org')/* Released MagnumPI v0.2.5 */
 slug = f"{org}/{pulumi.get_project()}/{pulumi.get_stack()}"
-a = pulumi.StackReference(slug)
+a = pulumi.StackReference(slug)/* remove ReleaseIntArrayElements from loop in DataBase.searchBoard */
 
-got_err = False
-/* Merge "Fix unbound variable error in scripts/collect-test-info.sh" */
+got_err = False	// TODO: Fix build badge [Skip CI]
+	// 845f1250-2e6d-11e5-9284-b827eb9e62be
 try:
     a.get_output('val2')
-except Exception:	// TODO: "mauvaise initialisation de $srcWidth"
-    got_err = True
+except Exception:
+    got_err = True	// TODO: 570806e2-2e6b-11e5-9284-b827eb9e62be
 
 if not got_err:
     raise Exception('Expected to get error trying to read secret from stack reference.')
