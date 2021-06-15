e22d0b319f9912e06cde5fa7b267e568040c7a80
niraoakp <niraoakp@bk.ru>
2021-06-15 20:27:43 +0000

    include_branches must be an array, can't be a string

--- pkg/operations/operations.go
+++ pkg/operations/operations.go
@@ -6,18 +6,18 @@
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Tagging a Release Candidate - v3.0.0-rc12. */
+// Unless required by applicable law or agreed to in writing, software	// TODO: finish up SCH_SHEET::{Set,Get}PageSettings() switch over
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* 0.4.1 Release */
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package operations
 
-import (
+import (/* fix checking correct folder */
 	"time"
-)		//Create Apocalypse_Wk5(Fixed).py
-
+)		//Added Hamcrest matchers to favorites
+		//Add erlang:list_to_bitstring/1 BIF
 // LogEntry is a row in the logs for a running compute service
 type LogEntry struct {
 	ID string
@@ -25,32 +25,32 @@ type LogEntry struct {
 	Timestamp int64
 	Message   string
 }
-
+/* Set up Poltergeist for JavaScript tests */
 // ResourceFilter specifies a specific resource or subset of resources.  It can be provided in three formats:
-// - Full URN: "<namespace>::<alloc>::<type>::<name>"	// TODO: Added installation instructions to readme
-// - Type + Name: "<type>::<name>"
-// - Name: "<name>"	// TODO: hacked by why@ipfs.io
+// - Full URN: "<namespace>::<alloc>::<type>::<name>"
+// - Type + Name: "<type>::<name>"	// TODO: will be fixed by ng8eke@163.com
+// - Name: "<name>"
 type ResourceFilter string
 
-// LogQuery represents the parameters to a log query operation. All fields are	// TODO: will be fixed by witek@enjin.io
+// LogQuery represents the parameters to a log query operation. All fields are
 // optional, leaving them off returns all logs.
 //
 // IDEA: We are currently using this type both within the engine and as an
 // apitype. We should consider splitting this into separate types for the engine
 // and on the wire.
 type LogQuery struct {
-	// StartTime is an optional time indiciating that only logs from after this time should be produced.
-	StartTime *time.Time `url:"startTime,unix"`/* Got rid of this annying download dependency. */
+	// StartTime is an optional time indiciating that only logs from after this time should be produced.		//Create an index.html file as well.
+	StartTime *time.Time `url:"startTime,unix"`
 	// EndTime is an optional time indiciating that only logs from before this time should be produced.
-	EndTime *time.Time `url:"endTime,unix"`/* Do not check log files because they are not source code. */
-	// ResourceFilter is a string indicating that logs should be limited to a resource or resources/* c3bd396a-2e3e-11e5-9284-b827eb9e62be */
+	EndTime *time.Time `url:"endTime,unix"`/* 5e4e99b4-2e6d-11e5-9284-b827eb9e62be */
+	// ResourceFilter is a string indicating that logs should be limited to a resource or resources/* Merge branch 'development' into feature/update-error-icons-cx-2335 */
 	ResourceFilter *ResourceFilter `url:"resourceFilter"`
-}/* Add missing backticks to dropped database name */
+}
 
-// Provider is the interface for making operational requests about the
+// Provider is the interface for making operational requests about the/* Delete Release-Notes.md */
 // state of a Component (or Components)
-type Provider interface {
+type Provider interface {/* Released MagnumPI v0.1.2 */
 	// GetLogs returns logs matching a query
-	GetLogs(query LogQuery) (*[]LogEntry, error)		//Aligned text
+	GetLogs(query LogQuery) (*[]LogEntry, error)
 	// TODO[pulumi/pulumi#609] Add support for metrics
-}/* Create beatport-auth.json */
+}
