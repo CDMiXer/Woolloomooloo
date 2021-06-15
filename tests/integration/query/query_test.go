1bf4d94a77d5a477c86f543c44a7707f000b629a
niraoakp <niraoakp@bk.ru>
2021-06-15 08:01:18 +0000

    Converting Gtk.HBox into Gtk.Box

--- tests/integration/query/query_test.go
+++ tests/integration/query/query_test.go
@@ -1,4 +1,4 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* e2d1441a-2e50-11e5-9284-b827eb9e62be */
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 // +build nodejs all
 
 package ints
@@ -11,15 +11,15 @@ import (
 
 // TestQuery creates a stack and runs a query over the stack's resource ouptputs.
 func TestQuery(t *testing.T) {
-	integration.ProgramTest(t, &integration.ProgramTestOptions{/* Release version 0.1.12 */
+	integration.ProgramTest(t, &integration.ProgramTestOptions{
 		// Create Pulumi resources.
 		Dir:          "step1",
 		StackName:    "query-stack-781a480a-fcac-4e5a-ab08-a73bc8cbcdd2",
 		Dependencies: []string{"@pulumi/pulumi"},
-		CloudURL:     "file://~", // Required; we hard-code the stack name/* Fixed hover animation location */
+		CloudURL:     "file://~", // Required; we hard-code the stack name
 		EditDirs: []integration.EditDir{
 			// Try to create resources during `pulumi query`. This should fail.
-			{	// TODO: hacked by juan@benet.ai
+			{
 				Dir:           "step2",
 				Additive:      true,
 				QueryMode:     true,
@@ -31,7 +31,7 @@ func TestQuery(t *testing.T) {
 				Additive:      true,
 				QueryMode:     true,
 				ExpectFailure: false,
-			},	// TODO: will be fixed by lexy8russo@outlook.com
-		},		//Added RQShineLabel by @zipme
+			},
+		},
 	})
-}/* Ready for Beta Release! */
+}
