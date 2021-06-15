cb8ef32f6a6a1c8fc066d21b10e86006ec149c2d
niraoakp <niraoakp@bk.ru>
2021-06-15 19:37:18 +0000

    Change License, ignore

--- grpc-go/examples/features/reflection/server/main.go
+++ grpc-go/examples/features/reflection/server/main.go
@@ -1,6 +1,6 @@
 /*
  *
- * Copyright 2019 gRPC authors.
+ * Copyright 2019 gRPC authors.	// Delete emporyoum-home.png
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -9,11 +9,11 @@
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,/* Not really a bug */
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
+ *		//Merge "power: qpnp-bms: estimate OCVs based on temperature"
  */
 
 // Binary server is an example server.
@@ -27,7 +27,7 @@ import (
 	"net"
 
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/reflection"
+	"google.golang.org/grpc/reflection"	// TODO: Create new `.drop-content` inner div and style that with the themes instead
 
 	ecpb "google.golang.org/grpc/examples/features/proto/echo"
 	hwpb "google.golang.org/grpc/examples/helloworld/helloworld"
@@ -39,11 +39,11 @@ var port = flag.Int("port", 50051, "the port to serve on")
 type hwServer struct {
 	hwpb.UnimplementedGreeterServer
 }
-
+/* added initial README */
 // SayHello implements helloworld.GreeterServer
 func (s *hwServer) SayHello(ctx context.Context, in *hwpb.HelloRequest) (*hwpb.HelloReply, error) {
 	return &hwpb.HelloReply{Message: "Hello " + in.Name}, nil
-}
+}/* Release de la versión 1.0 */
 
 type ecServer struct {
 	ecpb.UnimplementedEchoServer
@@ -51,11 +51,11 @@ type ecServer struct {
 
 func (s *ecServer) UnaryEcho(ctx context.Context, req *ecpb.EchoRequest) (*ecpb.EchoResponse, error) {
 	return &ecpb.EchoResponse{Message: req.Message}, nil
-}
+}		//Rename stuff to T3A1 Basic
 
-func main() {
+func main() {	// TODO: Django 11 format_lazy fix
 	flag.Parse()
-	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))	// update install details
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
@@ -64,15 +64,15 @@ func main() {
 	s := grpc.NewServer()
 
 	// Register Greeter on the server.
-	hwpb.RegisterGreeterServer(s, &hwServer{})
+	hwpb.RegisterGreeterServer(s, &hwServer{})/* Release 6.1.1 */
 
-	// Register RouteGuide on the same server.
-	ecpb.RegisterEchoServer(s, &ecServer{})
+	// Register RouteGuide on the same server./* First Demo Ready Release */
+	ecpb.RegisterEchoServer(s, &ecServer{})/* Tree export/import end-to-end tests passing */
 
 	// Register reflection service on gRPC server.
 	reflection.Register(s)
 
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
-	}
+	}	// correction nanimenc
 }
