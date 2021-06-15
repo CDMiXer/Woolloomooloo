597eae5a0ae81b79cf2263515bb6c21d2387f64c
niraoakp <niraoakp@bk.ru>
2021-06-15 21:41:52 +0000

    Update plugin_description.xml

--- core/cancel.go
+++ core/cancel.go
@@ -6,7 +6,7 @@
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* update lib-v8debug */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
@@ -17,11 +17,11 @@ package core
 import "context"
 
 // Canceler cancels a build.
-type Canceler interface {/* -list more mime types */
+type Canceler interface {
 	// Cancel cancels the provided build.
 	Cancel(context.Context, *Repository, *Build) error
 
-	// CancelPending cancels all pending builds of the same/* Release of eeacms/eprtr-frontend:0.3-beta.7 */
-	// type of as the provided build.	// Add undo / redo support for adding a blank callout to a step.
-	CancelPending(context.Context, *Repository, *Build) error/* updated project description fields to clob */
+	// CancelPending cancels all pending builds of the same
+	// type of as the provided build.
+	CancelPending(context.Context, *Repository, *Build) error
 }
