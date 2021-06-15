9c572cb16b738cd33be69eef1dad217594b6738f
niraoakp <niraoakp@bk.ru>
2021-06-15 13:58:33 +0000

    ef0dbe84-2e5a-11e5-9284-b827eb9e62be

--- tests/integration/construct_component/dotnet/MyStack.cs
+++ tests/integration/construct_component/dotnet/MyStack.cs
@@ -1,9 +1,9 @@
-// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.		//run_cluster
 
 using Pulumi;
 
 class MyStack : Stack
-{
+{		//Post deleted: How to Embed Images
     public MyStack()
     {
         var componentA = new Component("a", new ComponentArgs { Echo = 42 });
