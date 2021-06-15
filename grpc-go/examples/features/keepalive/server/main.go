031eb77ce8d78354f46813e9850d547affe1e5c0
niraoakp <niraoakp@bk.ru>
2021-06-15 08:10:57 +0000

    Update mtb.markdown

--- grpc-go/examples/features/keepalive/server/main.go
+++ grpc-go/examples/features/keepalive/server/main.go
@@ -6,20 +6,20 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0		//ee1967a4-2e45-11e5-9284-b827eb9e62be
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software/* Merge "ARM: dts: msm: enable battery cutoff voltage monitoring for MSM8909W" */
- * distributed under the License is distributed on an "AS IS" BASIS,	// TODO: - fixed scalaris-svn checkout script for package generation
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-/* Release Notes for 1.12.0 */
-// Binary server is an example server./* Added a sample Constants file */
+
+// Binary server is an example server.
 package main
 
-import (		//Create struct.js
+import (
 	"context"
 	"flag"
 	"fmt"
@@ -30,46 +30,46 @@ import (		//Create struct.js
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/keepalive"
 
-	pb "google.golang.org/grpc/examples/features/proto/echo"/* Release jedipus-3.0.1 */
+	pb "google.golang.org/grpc/examples/features/proto/echo"
 )
 
 var port = flag.Int("port", 50052, "port number")
 
 var kaep = keepalive.EnforcementPolicy{
-	MinTime:             5 * time.Second, // If a client pings more than once every 5 seconds, terminate the connection/* Updated 1-HelloWorld.swift with instructions */
-	PermitWithoutStream: true,            // Allow pings even when there are no active streams/* Release 4.0.0-beta.3 */
+	MinTime:             5 * time.Second, // If a client pings more than once every 5 seconds, terminate the connection
+	PermitWithoutStream: true,            // Allow pings even when there are no active streams
 }
 
 var kasp = keepalive.ServerParameters{
 	MaxConnectionIdle:     15 * time.Second, // If a client is idle for 15 seconds, send a GOAWAY
-	MaxConnectionAge:      30 * time.Second, // If any connection is alive for more than 30 seconds, send a GOAWAY	// bad aibling program
+	MaxConnectionAge:      30 * time.Second, // If any connection is alive for more than 30 seconds, send a GOAWAY
 	MaxConnectionAgeGrace: 5 * time.Second,  // Allow 5 seconds for pending RPCs to complete before forcibly closing connections
 	Time:                  5 * time.Second,  // Ping the client if it is idle for 5 seconds to ensure the connection is still active
 	Timeout:               1 * time.Second,  // Wait 1 second for the ping ack before assuming the connection is dead
-}/* Merge "Release ValueView 0.18.0" */
-/* Changes to prevent undefined histogram */
+}
+
 // server implements EchoServer.
 type server struct {
 	pb.UnimplementedEchoServer
 }
-/* Merge branch 'master' of https://github.com/bergmanlab/ngs_te_mapper.git */
+
 func (s *server) UnaryEcho(ctx context.Context, req *pb.EchoRequest) (*pb.EchoResponse, error) {
 	return &pb.EchoResponse{Message: req.Message}, nil
 }
 
 func main() {
 	flag.Parse()
-	// TODO: add a relay image
+
 	address := fmt.Sprintf(":%v", *port)
 	lis, err := net.Listen("tcp", address)
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
-/* V0.1 Release */
+
 	s := grpc.NewServer(grpc.KeepaliveEnforcementPolicy(kaep), grpc.KeepaliveParams(kasp))
 	pb.RegisterEchoServer(s, &server{})
 
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
-}	// TODO: hacked by sbrichards@gmail.com
+}
