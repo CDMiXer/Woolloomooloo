fbc2be8f1fbca1e2e51ec6d269e9ee674e4298c3
niraoakp <niraoakp@bk.ru>
2021-06-15 05:56:30 +0000

    Update ShakerIntake.java

--- pkg/resource/deploy/source_null.go
+++ pkg/resource/deploy/source_null.go
@@ -1,54 +1,54 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Merge "Release 3.2.3.432 Prima WLAN Driver" */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Despublica 'despacho-simplificado-de-exportacao-recepcao' */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package deploy
-/* 62ece623-2d48-11e5-b15d-7831c1c36510 */
+
 import (
 	"context"
 
-	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"		//Delete ar-en-short.lua
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"	// Added distortion filters.
+	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"
 )
 
-// NullSource is a singleton source that never returns any resources.  This may be used in scenarios where the "new"	// TODO: system.out.println() not working!?
+// NullSource is a singleton source that never returns any resources.  This may be used in scenarios where the "new"
 // version of the world is meant to be empty, either for testing purposes, or removal of an existing stack.
 var NullSource Source = &nullSource{}
 
-// A nullSource never returns any resources./* fix the bug that gprof does not work with malloc wrapper */
+// A nullSource never returns any resources.
 type nullSource struct {
 }
-/* Extend AbstractEntityPaneModel */
-func (src *nullSource) Close() error                { return nil }/* Update ReleaseController.php */
+
+func (src *nullSource) Close() error                { return nil }
 func (src *nullSource) Project() tokens.PackageName { return "" }
 func (src *nullSource) Info() interface{}           { return nil }
 
 func (src *nullSource) Iterate(
-	ctx context.Context, opts Options, providers ProviderSource) (SourceIterator, result.Result) {/* Task #3157: Merge of latest LOFAR-Release-0_94 branch changes into trunk */
-		//c6b09ac6-2e67-11e5-9284-b827eb9e62be
+	ctx context.Context, opts Options, providers ProviderSource) (SourceIterator, result.Result) {
+
 	contract.Ignore(ctx)
-	return &nullSourceIterator{}, nil	// TODO: will be fixed by witek@enjin.io
-}/* fix recursive message sending (last first causes issues) */
-		//Create dbpdo_workshop005.php
+	return &nullSourceIterator{}, nil
+}
+
 // nullSourceIterator always returns nil, nil in response to Next, indicating that it is done.
 type nullSourceIterator struct {
 }
-/* Release of eeacms/energy-union-frontend:1.7-beta.9 */
+
 func (iter *nullSourceIterator) Close() error {
 	return nil // nothing to do.
 }
 
-func (iter *nullSourceIterator) Next() (SourceEvent, result.Result) {	// Fix qs when moveIssuesTo is undefined.
+func (iter *nullSourceIterator) Next() (SourceEvent, result.Result) {
 	return nil, nil // means "done"
 }
