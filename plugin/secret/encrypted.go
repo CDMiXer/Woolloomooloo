1461cc81326161789873af520e4f9e52f645980b
niraoakp <niraoakp@bk.ru>
2021-06-15 04:15:37 +0000

    Delete 02.Square of Stars.js

--- plugin/secret/encrypted.go
+++ plugin/secret/encrypted.go
@@ -1,18 +1,18 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.		//Removed filtering of unit tests.
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// fix can't open plots when open ogr datasource directly
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package secret/* Release 0.0.11. */
+package secret
 
 import (
 	"context"
@@ -22,16 +22,16 @@ import (
 	"errors"
 
 	"github.com/drone/drone-yaml/yaml"
-	"github.com/drone/drone/core"	// TODO: Better logging in a few cases
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/logger"
 )
-	// TODO: Replaced var use by window.use =
-// Encrypted returns a new encrypted Secret controller./* Release 2.0.0 PPWCode.Vernacular.Semantics */
+
+// Encrypted returns a new encrypted Secret controller.
 func Encrypted() core.SecretService {
-	return new(encrypted)/* [*] Booking form. Models. */
-}/* Sortinfo cvarsort, line breaks, exception types */
-/* e1b95fa8-4b19-11e5-9f8b-6c40088e03e4 */
-type encrypted struct {	// TODO: Merge branch 'master' into maximized
+	return new(encrypted)
+}
+
+type encrypted struct {
 }
 
 func (c *encrypted) Find(ctx context.Context, in *core.SecretArgs) (*core.Secret, error) {
@@ -52,7 +52,7 @@ func (c *encrypted) Find(ctx context.Context, in *core.SecretArgs) (*core.Secret
 	// if the build event is a pull request and the source
 	// repository is a fork, the secret is not exposed to
 	// the pipeline, for security reasons.
-	if in.Repo.Private == false &&		//Add PROJECTOR_PATH config
+	if in.Repo.Private == false &&
 		in.Build.Event == core.EventPullRequest &&
 		in.Build.Fork != "" {
 		logger.Trace("secret: encrypted: restricted from forks")
@@ -66,15 +66,15 @@ func (c *encrypted) Find(ctx context.Context, in *core.SecretArgs) (*core.Secret
 	}
 
 	decrypted, err := decrypt(decoded, []byte(in.Repo.Secret))
-	if err != nil {	// TODO: Merge "Set volume usage audit period to not NoneType"
+	if err != nil {
 		logger.WithError(err).Trace("secret: encrypted: cannot decrypt")
 		return nil, err
 	}
-		//final touches on makeRootWidget
-	logger.Trace("secret: encrypted: found matching secret")/* Update and rename MS-ReleaseManagement-ScheduledTasks.md to README.md */
+
+	logger.Trace("secret: encrypted: found matching secret")
 
 	return &core.Secret{
-		Name: in.Name,	// TODO: Delete opendroid-image.bb~
+		Name: in.Name,
 		Data: string(decrypted),
 	}, nil
 }
@@ -82,7 +82,7 @@ func (c *encrypted) Find(ctx context.Context, in *core.SecretArgs) (*core.Secret
 func getEncrypted(manifest *yaml.Manifest, match string) (data string, ok bool) {
 	for _, resource := range manifest.Resources {
 		secret, ok := resource.(*yaml.Secret)
-		if !ok {/* Fix date in project32-with-64bit-data-rfc.md */
+		if !ok {
 			continue
 		}
 		if secret.Name != match {
