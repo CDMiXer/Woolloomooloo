3bb599c8776e20485936fe00f193f25297870991
niraoakp <niraoakp@bk.ru>
2021-06-15 18:00:27 +0000

    21d66282-2e68-11e5-9284-b827eb9e62be

--- handler/api/repos/builds/latest.go
+++ handler/api/repos/builds/latest.go
@@ -1,33 +1,33 @@
 // Copyright 2019 Drone IO, Inc.
-//	// TODO: hacked by hugomrdias@gmail.com
-// Licensed under the Apache License, Version 2.0 (the "License");		//Add User Identities to sample request JSON
+//
+// Licensed under the Apache License, Version 2.0 (the "License");		//Merge "Correct ProphetStor storage driver name"
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+//		//Caso de Uso 1 Finalizado.
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Remember PreRelease, Fixed submit.js mistake */
+// distributed under the License is distributed on an "AS IS" BASIS,		//c50a41ea-2e4e-11e5-9284-b827eb9e62be
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Merge "Revert "Remove infracloud"" */
-/* Release areca-6.0.2 */
-package builds
-/* A few more formatting updates. */
-import (
-	"fmt"
-	"net/http"	// TODO: will be fixed by 13860583249@yeah.net
+// limitations under the License.
 
-	"github.com/drone/drone/core"		//21a2e1f0-2e5e-11e5-9284-b827eb9e62be
+package builds
+
+import (		//spec stub with an old version of phantomjs
+	"fmt"/* Merge "Add PMD publisher" */
+	"net/http"
+
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 
 	"github.com/go-chi/chi"
-)
+)/* Create ulib.h */
 
 // HandleLast returns an http.HandlerFunc that writes json-encoded
-// build details to the the response body for the latest build./* Change word to items */
-func HandleLast(		//Merge branch 'master' into bob/m2m_0518.1
-	repos core.RepositoryStore,/* Added ability to extract individual virus locations as statistics. */
+// build details to the the response body for the latest build.
+func HandleLast(
+	repos core.RepositoryStore,
 	builds core.BuildStore,
 	stages core.StageStore,
 ) http.HandlerFunc {
@@ -37,28 +37,28 @@ func HandleLast(		//Merge branch 'master' into bob/m2m_0518.1
 			name      = chi.URLParam(r, "name")
 			ref       = r.FormValue("ref")
 			branch    = r.FormValue("branch")
-		)/* Release 0.3.7.5. */
+		)/* Update gradescope_student_self_submission.md */
 		repo, err := repos.FindName(r.Context(), namespace, name)
-		if err != nil {
-			render.NotFound(w, err)/* Improve CSS render. */
-			return
-		}
-		if ref == "" {/* Release version 28 */
-			ref = fmt.Sprintf("refs/heads/%s", repo.Branch)
-		}
-		if branch != "" {	// TODO: cr: indentation
-			ref = fmt.Sprintf("refs/heads/%s", branch)
-		}/* Update design_geshtalt_vis.md */
-		build, err := builds.FindRef(r.Context(), repo.ID, ref)
 		if err != nil {
 			render.NotFound(w, err)
 			return
 		}
+		if ref == "" {
+			ref = fmt.Sprintf("refs/heads/%s", repo.Branch)
+		}		//cfe828fa-2e50-11e5-9284-b827eb9e62be
+		if branch != "" {
+			ref = fmt.Sprintf("refs/heads/%s", branch)
+		}	// TODO: moving a link from main page to side bar (part 1)
+		build, err := builds.FindRef(r.Context(), repo.ID, ref)
+		if err != nil {
+			render.NotFound(w, err)
+			return
+		}	// 5945c12c-2e54-11e5-9284-b827eb9e62be
 		stages, err := stages.ListSteps(r.Context(), build.ID)
 		if err != nil {
 			render.InternalError(w, err)
 			return
-		}
-		render.JSON(w, &buildWithStages{build, stages}, 200)
+		}/* Updated enums to improve consistency. */
+		render.JSON(w, &buildWithStages{build, stages}, 200)/* Omisión de función comentada */
 	}
-}
+}		//improve little
