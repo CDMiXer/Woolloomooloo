4ac8507a839bbfe5854c0549d3eb6c7ee7fd39a3
niraoakp <niraoakp@bk.ru>
2021-06-15 21:46:34 +0000

    Explicitly set key for unique.

--- handler/api/queue/resume.go
+++ handler/api/queue/resume.go
@@ -1,31 +1,31 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+	// TODO: will be fixed by aeongrp@outlook.com
 // +build !oss
-
+	// TODO: hacked by cory@protocol.ai
 package queue
 
-import (
-	"net/http"
+import (	// Delete letaohuanggang.html
+	"net/http"	// TODO: LOW / Added toString for rendered data in inspector
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
 )
 
-// HandleResume returns an http.HandlerFunc that processes
+// HandleResume returns an http.HandlerFunc that processes		//Added item class
 // an http.Request to pause the scheduler.
 func HandleResume(scheduler core.Scheduler) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		ctx := r.Context()
 		err := scheduler.Resume(ctx)
-		if err != nil {
+		if err != nil {		//Check for tokens after an expression on input
 			render.InternalError(w, err)
 			logger.FromRequest(r).WithError(err).
 				Errorln("api: cannot resume scheduler")
 			return
-		}
+		}/* Merge "Substitutes the hardcoded container name with a variable" */
 		w.WriteHeader(http.StatusNoContent)
 	}
 }
