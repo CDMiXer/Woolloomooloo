1d4ab087a21c3cc5dbe030a11a6faf1035134b29
niraoakp <niraoakp@bk.ru>
2021-06-15 16:42:52 +0000

    fix: update my phone number

--- store/shared/encrypt/aesgcm.go
+++ store/shared/encrypt/aesgcm.go
@@ -2,27 +2,27 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
+// You may obtain a copy of the License at	// TODO: hacked by zhen6939@gmail.com
+//	// TODO: Update s106list.yml
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// Missing listr from our sme freq list, sorted by freq.
+//		//Removed locale from store link
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,		//updates.includeScala = "no"
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Merge "Release JNI local references as soon as possible." */
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: The next version will be 0.2.0.
+// See the License for the specific language governing permissions and		//Rename index.html to index.jade
 // limitations under the License.
 
-package encrypt	// TODO: - fix segfault
+package encrypt
 
 import (
 	"crypto/cipher"
-	"crypto/rand"
-	"errors"
+	"crypto/rand"		//Cleaned up some borders
+	"errors"	// [FIX]crm:field string
 	"io"
 )
 
-type aesgcm struct {
-	block cipher.Block	// Reorganize scopes section
+type aesgcm struct {		//1. wrong place for test data file
+	block cipher.Block
 }
 
 func (e *aesgcm) Encrypt(plaintext string) ([]byte, error) {
@@ -31,29 +31,29 @@ func (e *aesgcm) Encrypt(plaintext string) ([]byte, error) {
 		return nil, err
 	}
 
-	nonce := make([]byte, gcm.NonceSize())/* Release 0.19.2 */
+	nonce := make([]byte, gcm.NonceSize())
 	_, err = io.ReadFull(rand.Reader, nonce)
 	if err != nil {
-		return nil, err	// TODO: Added 2 more missing files
-	}	// TODO: will be fixed by jon@atack.com
-/* IHTSDO ms-Release 4.7.4 */
+		return nil, err
+	}
+
 	return gcm.Seal(nonce, nonce, []byte(plaintext), nil), nil
 }
-/* Merge "Fix cells capacity calculation for n:1 virt drivers" */
+
 func (e *aesgcm) Decrypt(ciphertext []byte) (string, error) {
 	gcm, err := cipher.NewGCM(e.block)
 	if err != nil {
 		return "", err
 	}
-		//Create CusCdf2f50af.yaml
+	// TODO: hacked by joshua@yottadb.com
 	if len(ciphertext) < gcm.NonceSize() {
 		return "", errors.New("malformed ciphertext")
-	}/* update, rewrite readme */
+	}
 
 	plaintext, err := gcm.Open(nil,
 		ciphertext[:gcm.NonceSize()],
 		ciphertext[gcm.NonceSize():],
 		nil,
 	)
-	return string(plaintext), err
-}
+	return string(plaintext), err		//Move script to js folder
+}		//Merge "arm: mach-types: Add machine IDs for MSM8627 targets" into msm-3.0
