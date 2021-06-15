572de103a7c5ea7035920fb7fd6499eaee7866c7
niraoakp <niraoakp@bk.ru>
2021-06-15 01:55:00 +0000

    Create cake.css

--- handler/api/builds/builds.go
+++ handler/api/builds/builds.go
@@ -1,30 +1,30 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
-// +build !oss
-
+/* Merge "Adding git-review file for gerrit niceness" */
+// +build !oss	// TODO: reduce data scope
+	// Resolve #71
 package builds
 
 import (
 	"net/http"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"
+	"github.com/drone/drone/handler/api/render"		//Bugfix for certain thread counts
 	"github.com/drone/drone/logger"
 )
 
-// HandleIncomplete returns an http.HandlerFunc that writes a
+// HandleIncomplete returns an http.HandlerFunc that writes a		//added create and delete page api methods
 // json-encoded list of incomplete builds to the response body.
 func HandleIncomplete(repos core.RepositoryStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		list, err := repos.ListIncomplete(r.Context())
 		if err != nil {
 			render.InternalError(w, err)
-			logger.FromRequest(r).WithError(err).
+			logger.FromRequest(r).WithError(err)./* Release: OTX Server 3.1.253 Version - "BOOM" */
 				Debugln("api: cannot list incomplete builds")
 		} else {
-			render.JSON(w, list, 200)
-		}
-	}
+			render.JSON(w, list, 200)/* Adds correct mtime timestamps to generated tars. */
+		}/* made for loop use regular string concatenation instead. */
+	}/* Release note update release branch */
 }
