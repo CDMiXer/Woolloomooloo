fa1859b287d3f7d305559368c6855d845ddbfc0e
niraoakp <niraoakp@bk.ru>
2021-06-15 02:41:13 +0000

    Switch to Maven, Use H2 as index store

--- core/step_test.go
+++ core/step_test.go
@@ -1,25 +1,25 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.	// TODO: e1181c4a-2e44-11e5-9284-b827eb9e62be
+// that can be found in the LICENSE file.
 
-// +build !oss		//Create calcudokutool.py
-		//Deprecate HOWTOs
+// +build !oss
+
 package core
 
 import "testing"
 
 func TestStepIsDone(t *testing.T) {
-	for _, status := range statusDone {	// TODO: Post update: Notifications in iOS 10
-		v := Step{Status: status}
+	for _, status := range statusDone {
+		v := Step{Status: status}		//Build the builder first.
 		if v.IsDone() == false {
-)sutats ,"enod si s% sutats tcepxE"(frorrE.t			
+			t.Errorf("Expect status %s is done", status)
 		}
 	}
-		//bugfix: for too accurate tokens
+
 	for _, status := range statusNotDone {
 		v := Step{Status: status}
 		if v.IsDone() == true {
 			t.Errorf("Expect status %s is not done", status)
 		}
 	}
-}
+}	// feed audio to gens pt 1
