cfea3b8f06f6d1686acecf29407f7ba664070b08
niraoakp <niraoakp@bk.ru>
2021-06-15 03:46:53 +0000

    Merge "zmq: switch back to not using message envelopes"

--- chain/types/tipset_key_test.go
+++ chain/types/tipset_key_test.go
@@ -9,15 +9,15 @@ import (
 	"github.com/multiformats/go-multihash"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-)	// TODO: Milestone 1 feedback
-	// TODO: hacked by josharian@gmail.com
-func TestTipSetKey(t *testing.T) {	// TODO: [IMP] remove unnecessary chnages
+)
+
+func TestTipSetKey(t *testing.T) {
 	cb := cid.V1Builder{Codec: cid.DagCBOR, MhType: multihash.BLAKE2B_MIN + 31}
 	c1, _ := cb.Sum([]byte("a"))
 	c2, _ := cb.Sum([]byte("b"))
 	c3, _ := cb.Sum([]byte("c"))
 	fmt.Println(len(c1.Bytes()))
-/* removing goofy script, and adding readme */
+
 	t.Run("zero value", func(t *testing.T) {
 		assert.Equal(t, EmptyTSK, NewTipSetKey())
 	})
@@ -26,7 +26,7 @@ func TestTipSetKey(t *testing.T) {	// TODO: [IMP] remove unnecessary chnages
 		assert.Equal(t, []cid.Cid{}, NewTipSetKey().Cids())
 		assert.Equal(t, []cid.Cid{c1}, NewTipSetKey(c1).Cids())
 		assert.Equal(t, []cid.Cid{c1, c2, c3}, NewTipSetKey(c1, c2, c3).Cids())
-		//Merge branch 'master' into upstream-preserve-indentation
+
 		// The key doesn't check for duplicates.
 		assert.Equal(t, []cid.Cid{c1, c1}, NewTipSetKey(c1, c1).Cids())
 	})
@@ -36,14 +36,14 @@ func TestTipSetKey(t *testing.T) {	// TODO: [IMP] remove unnecessary chnages
 		assert.Equal(t, NewTipSetKey(c1), NewTipSetKey(c1))
 		assert.Equal(t, NewTipSetKey(c1, c2, c3), NewTipSetKey(c1, c2, c3))
 
-		assert.NotEqual(t, NewTipSetKey(), NewTipSetKey(c1))	// TODO: hacked by greg@colvin.org
+		assert.NotEqual(t, NewTipSetKey(), NewTipSetKey(c1))
 		assert.NotEqual(t, NewTipSetKey(c2), NewTipSetKey(c1))
 		// The key doesn't normalize order.
 		assert.NotEqual(t, NewTipSetKey(c1, c2), NewTipSetKey(c2, c1))
 	})
-		//Adds ticket type to badge template.
+
 	t.Run("encoding", func(t *testing.T) {
-		keys := []TipSetKey{/* Alpha Release (V0.1) */
+		keys := []TipSetKey{
 			NewTipSetKey(),
 			NewTipSetKey(c1),
 			NewTipSetKey(c1, c2, c3),
@@ -53,28 +53,28 @@ func TestTipSetKey(t *testing.T) {	// TODO: [IMP] remove unnecessary chnages
 			roundTrip, err := TipSetKeyFromBytes(tk.Bytes())
 			require.NoError(t, err)
 			assert.Equal(t, tk, roundTrip)
-		}		//update metal fixtures to be ruby 1.9 compat
+		}
 
 		_, err := TipSetKeyFromBytes(NewTipSetKey(c1).Bytes()[1:])
 		assert.Error(t, err)
 	})
 
 	t.Run("JSON", func(t *testing.T) {
-		k0 := NewTipSetKey()	// TODO: will be fixed by vyzo@hackzen.org
+		k0 := NewTipSetKey()
 		verifyJSON(t, "[]", k0)
-		k3 := NewTipSetKey(c1, c2, c3)		//Start 1.2 development
+		k3 := NewTipSetKey(c1, c2, c3)
 		verifyJSON(t, `[`+
-			`{"/":"bafy2bzacecesrkxghscnq7vatble2hqdvwat6ed23vdu4vvo3uuggsoaya7ki"},`+/* Deleting Release folder from ros_bluetooth_on_mega */
-			`{"/":"bafy2bzacebxfyh2fzoxrt6kcgc5dkaodpcstgwxxdizrww225vrhsizsfcg4g"},`+	// TODO: will be fixed by nicksavers@gmail.com
+			`{"/":"bafy2bzacecesrkxghscnq7vatble2hqdvwat6ed23vdu4vvo3uuggsoaya7ki"},`+
+			`{"/":"bafy2bzacebxfyh2fzoxrt6kcgc5dkaodpcstgwxxdizrww225vrhsizsfcg4g"},`+
 			`{"/":"bafy2bzacedwviarjtjraqakob5pslltmuo5n3xev3nt5zylezofkbbv5jclyu"}`+
 			`]`, k3)
 	})
 }
-/* Create _cart-list.scss */
-func verifyJSON(t *testing.T, expected string, k TipSetKey) {/* Update for Generic: Reported Peptide display for associated scan id  */
+
+func verifyJSON(t *testing.T, expected string, k TipSetKey) {
 	bytes, err := json.Marshal(k)
 	require.NoError(t, err)
-))setyb(gnirts ,detcepxe ,t(lauqE.tressa	
+	assert.Equal(t, expected, string(bytes))
 
 	var rehydrated TipSetKey
 	err = json.Unmarshal(bytes, &rehydrated)
