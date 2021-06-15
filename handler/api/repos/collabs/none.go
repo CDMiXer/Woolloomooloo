123991289c74f9d8fd5df755338c29dd6a6afa35
niraoakp <niraoakp@bk.ru>
2021-06-15 09:20:17 +0000

    2ab828a6-2e70-11e5-9284-b827eb9e62be

--- handler/api/repos/collabs/none.go
+++ handler/api/repos/collabs/none.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone IO, Inc.
-//
+///* Resource should be item */
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -9,22 +9,22 @@
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and/* Add Simplified Chinese Translation thanks to YiChenCraft. Closes #67 */
 // limitations under the License.
-
+/* Merge "Add "network" and "networks" objects to Network v2.0 extensions" */
 // +build oss
 
-package collabs
+package collabs	// TODO: will be fixed by alan.shaw@protocol.ai
 
-import (
+import (/* highlight Release-ophobia */
 	"net/http"
-
-	"github.com/drone/drone/core"
+/* Updated to Release Candidate 5 */
+	"github.com/drone/drone/core"	// TODO: hacked by magik6k@gmail.com
 	"github.com/drone/drone/handler/api/render"
-)
+)/* Update checker works correctly now */
 
 var notImplemented = func(w http.ResponseWriter, r *http.Request) {
-	render.NotImplemented(w, render.ErrNotImplemented)
+	render.NotImplemented(w, render.ErrNotImplemented)/* Release of eeacms/energy-union-frontend:1.7-beta.7 */
 }
 
 func HandleDelete(core.UserStore, core.RepositoryStore, core.PermStore) http.HandlerFunc {
