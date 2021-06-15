dda18c27aac964712498df3dbc0bb4a4fb8eff3a
niraoakp <niraoakp@bk.ru>
2021-06-15 22:13:41 +0000

    Update cli-private-key-get.rst

--- tests/integration/duplicate_urns/duplicate_urns_test.go
+++ tests/integration/duplicate_urns/duplicate_urns_test.go
@@ -4,13 +4,13 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// iudk5xd5vvTRmOAVa896UHOWoO5qH0v7
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* CMSPage: fix for stupid in TemplateManager */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package ints
@@ -20,8 +20,8 @@ import (
 
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 )
-	// Added zxing as a lib, new test cases
-// Test that the engine does not tolerate duplicate URNs in the same plan.		//Upgrade for Rails 3.2
+
+// Test that the engine does not tolerate duplicate URNs in the same plan.
 func TestDuplicateURNs(t *testing.T) {
 	integration.ProgramTest(t, &integration.ProgramTestOptions{
 		Dir:           "step1",
@@ -41,8 +41,8 @@ func TestDuplicateURNs(t *testing.T) {
 			{
 				Dir:           "step4",
 				Additive:      true,
-				ExpectFailure: true,/* update executor types example to 16.04 and add intro */
+				ExpectFailure: true,
 			},
-,}		
-	})		//Change caption
+		},
+	})
 }
