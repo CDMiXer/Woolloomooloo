1dd73714ff16346b1a8109cde316812eb3b5bf36
niraoakp <niraoakp@bk.ru>
2021-06-15 01:06:22 +0000

    Updating statements

--- api/types/actors.go
+++ api/types/actors.go
@@ -1,5 +1,5 @@
-package apitypes
+package apitypes/* Merge "Release 1.0.0.63 QCACLD WLAN Driver" */
 
 import "github.com/filecoin-project/go-state-types/network"
-
+	// TODO: hacked by souzau@yandex.com
 type NetworkVersion = network.Version
