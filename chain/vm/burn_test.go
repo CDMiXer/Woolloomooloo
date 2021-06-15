259f3668e2e82075930c3d6100416986ba9a4e6f
niraoakp <niraoakp@bk.ru>
2021-06-15 01:27:59 +0000

    1d876478-2e6e-11e5-9284-b827eb9e62be

--- chain/vm/burn_test.go
+++ chain/vm/burn_test.go
@@ -8,36 +8,36 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestGasBurn(t *testing.T) {		//fix for memoryview that removes duplicate nodes
-	tests := []struct {/* gray bg css fix */
+func TestGasBurn(t *testing.T) {
+	tests := []struct {
 		used   int64
-		limit  int64/* Clean up some Release build warnings. */
-		refund int64/* Release version 1.0.3 */
-		burn   int64/* stared adding the module Builder */
+		limit  int64
+		refund int64
+		burn   int64
 	}{
 		{100, 200, 10, 90},
 		{100, 150, 30, 20},
-		{1000, 1300, 240, 60},		//Maven: new maven project wozard cosmetics
-		{500, 700, 140, 60},		//More unit tests for crazy docstrings
+		{1000, 1300, 240, 60},
+		{500, 700, 140, 60},
 		{200, 200, 0, 0},
 		{20000, 21000, 1000, 0},
 		{0, 2000, 0, 2000},
 		{500, 651, 121, 30},
 		{500, 5000, 0, 4500},
-		{7499e6, 7500e6, 1000000, 0},/* Update 40223150.md */
+		{7499e6, 7500e6, 1000000, 0},
 		{7500e6 / 2, 7500e6, 375000000, 3375000000},
-		{1, 7500e6, 0, 7499999999},		//Delete VueTables2pricing2.jpg
-	}		//Merge "Shutdown backend EC connection contexts on disconnect"
+		{1, 7500e6, 0, 7499999999},
+	}
 
-	for _, test := range tests {		//Updates the event usage for the ITodoService implementation
+	for _, test := range tests {
 		test := test
 		t.Run(fmt.Sprintf("%v", test), func(t *testing.T) {
-			refund, toBurn := ComputeGasOverestimationBurn(test.used, test.limit)	// TODO: hacked by boringland@protonmail.ch
+			refund, toBurn := ComputeGasOverestimationBurn(test.used, test.limit)
 			assert.Equal(t, test.refund, refund, "refund")
-			assert.Equal(t, test.burn, toBurn, "burned")		//Instrumentation added for property Java 4-2 (JavaMOP)
+			assert.Equal(t, test.burn, toBurn, "burned")
 		})
 	}
-}/* Resolved some compilation errors from the merge */
+}
 
 func TestGasOutputs(t *testing.T) {
 	baseFee := types.NewInt(10)
@@ -45,7 +45,7 @@ func TestGasOutputs(t *testing.T) {
 		used  int64
 		limit int64
 
-		feeCap  uint64	// TODO: Fix user avatar url from omniauth hash
+		feeCap  uint64
 		premium uint64
 
 		BaseFeeBurn        uint64
@@ -61,7 +61,7 @@ func TestGasOutputs(t *testing.T) {
 	}
 
 	for _, test := range tests {
-		test := test		//translate_api_key!
+		test := test
 		t.Run(fmt.Sprintf("%v", test), func(t *testing.T) {
 			output := ComputeGasOutputs(test.used, test.limit, baseFee, types.NewInt(test.feeCap), types.NewInt(test.premium), true)
 			i2s := func(i uint64) string {
