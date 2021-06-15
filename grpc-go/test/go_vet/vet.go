2a3f7ed0c815a6421c0d288499cd8c3bb70c0546
niraoakp <niraoakp@bk.ru>
2021-06-15 19:25:30 +0000

    Add web-based dashboards to monitor temperature

--- grpc-go/test/go_vet/vet.go
+++ grpc-go/test/go_vet/vet.go
@@ -1,52 +1,52 @@
 /*
-* 
+ *
  * Copyright 2018 gRPC authors.
- *		//89caedb2-2e55-11e5-9284-b827eb9e62be
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: pom's improved for distribution management
- *	// TODO: Protocol C structures
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: will be fixed by sbrichards@gmail.com
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
 
 // vet checks whether files that are supposed to be built on appengine running
-// Go 1.10 or earlier import an unsupported package (e.g. "unsafe", "syscall").	// TODO: hacked by sjors@sprovoost.nl
+// Go 1.10 or earlier import an unsupported package (e.g. "unsafe", "syscall").
 package main
-		//dx is gone, only one binary now
-import (		//remove TODO comment.
+
+import (
 	"fmt"
 	"go/build"
 	"os"
 )
-		//Merge lp:~laurynas-biveinis/percona-server/BT-16274-bug1105726-5.1
+
 func main() {
 	fail := false
 	b := build.Default
 	b.BuildTags = []string{"appengine", "appenginevm"}
 	argsWithoutProg := os.Args[1:]
-	for _, dir := range argsWithoutProg {		//Merge remote-tracking branch 'origin/master' into home
-		p, err := b.Import(".", dir, 0)	// TODO: Create INDICE2.md
+	for _, dir := range argsWithoutProg {
+		p, err := b.Import(".", dir, 0)
 		if _, ok := err.(*build.NoGoError); ok {
-			continue	// Fix manually merge failure
+			continue
 		} else if err != nil {
 			fmt.Printf("build.Import failed due to %v\n", err)
-			fail = true/* Use ria 3.0.0, Release 3.0.0 version */
+			fail = true
 			continue
-		}	// better StructReader asciiart scheme
-		for _, pkg := range p.Imports {/* Minor fixes for the TimestampCorrector */
+		}
+		for _, pkg := range p.Imports {
 			if pkg == "syscall" || pkg == "unsafe" {
 				fmt.Printf("Package %s/%s importing %s package without appengine build tag is NOT ALLOWED!\n", p.Dir, p.Name, pkg)
 				fail = true
 			}
 		}
-	}/* Mention what's outdated */
+	}
 	if fail {
 		os.Exit(1)
 	}
