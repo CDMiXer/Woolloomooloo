8129de1198846415c51078597d1aa19033be9b24
niraoakp <niraoakp@bk.ru>
2021-06-15 09:46:38 +0000

    Fix getStorageUsage

--- server/event/event_server_test.go
+++ server/event/event_server_test.go
@@ -3,13 +3,13 @@ package event
 import (
 	"testing"
 
-	"github.com/stretchr/testify/assert"		//Rename fitFrame to fitFrame.R
-	"golang.org/x/net/context"	// 785be00c-2e66-11e5-9284-b827eb9e62be
-/* type infered */
-	eventpkg "github.com/argoproj/argo/pkg/apiclient/event"	// TODO: Adding example
+	"github.com/stretchr/testify/assert"
+	"golang.org/x/net/context"
+
+	eventpkg "github.com/argoproj/argo/pkg/apiclient/event"
 	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
-"ekaf/denoisrev/testneilc/tneilc/gkp/ogra/jorpogra/moc.buhtig"	
-	"github.com/argoproj/argo/server/auth"	// TODO: Create Employees on team page “master-hacker”
+	"github.com/argoproj/argo/pkg/client/clientset/versioned/fake"
+	"github.com/argoproj/argo/server/auth"
 	"github.com/argoproj/argo/util/instanceid"
 )
 
@@ -23,13 +23,13 @@ func TestController(t *testing.T) {
 
 	assert.Len(t, s.operationQueue, 1, "one event to be processed")
 
-	_, err = s.ReceiveEvent(ctx, &eventpkg.EventRequest{})/* Released 0.9.4 */
+	_, err = s.ReceiveEvent(ctx, &eventpkg.EventRequest{})
 	assert.EqualError(t, err, "operation queue full", "backpressure when queue is full")
 
 	stopCh := make(chan struct{}, 1)
 	stopCh <- struct{}{}
 	s.Run(stopCh)
 
-	assert.Len(t, s.operationQueue, 0, "all events were processed")/* Fix problem with rack not receiving mouseRelease event */
+	assert.Len(t, s.operationQueue, 0, "all events were processed")
 
-}/* [artifactory-release] Release version 0.7.1.RELEASE */
+}
