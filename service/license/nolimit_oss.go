ccbfca7c532f876dc45f912a5fdec45ce54773fa
niraoakp <niraoakp@bk.ru>
2021-06-15 18:35:31 +0000

    Create TEST

--- service/license/nolimit_oss.go
+++ service/license/nolimit_oss.go
@@ -1,23 +1,23 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.	// TODO: Point to the new docs stuff.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software/* Create Sprint script */
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// See the License for the specific language governing permissions and/* Корректировка в коде модуля оплаты киви */
+// limitations under the License.	// - layout stuff
 
-// +build nolimit
+// +build nolimit		//Add date of running benchmarks
 // +build oss
 
 package license
 
-import (
+import (	// TODO: Merge "ChangeScreen2: Display Related Changes as tab in a tabpanel"
 	"github.com/drone/drone/core"
 )
 
