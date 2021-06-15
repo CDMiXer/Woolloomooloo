cc147e1c2acd83d4c0145e912907eb4126d4537b
niraoakp <niraoakp@bk.ru>
2021-06-15 04:03:32 +0000

    removing unused interface

--- grpc-go/interop/alts/client/client.go
+++ grpc-go/interop/alts/client/client.go
@@ -1,31 +1,31 @@
-/*/* Automatic changelog generation for PR #13976 [ci skip] */
+/*
  *
- * Copyright 2018 gRPC authors./* Release of eeacms/plonesaas:5.2.1-22 */
+ * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *	// Merge "Adding resource link to resource detail page in Heat view"
- * Unless required by applicable law or agreed to in writing, software		//Fix cudatoolkit version
+ *
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* SGFS.rebuild_cache does not recurse by default */
+ * limitations under the License.
  *
  */
 
-// This binary can only run on Google Cloud Platform (GCP).		//Changed the CatchNotes class into a module.
+// This binary can only run on Google Cloud Platform (GCP).
 package main
-/* Merge "Release 4.0.10.57 QCACLD WLAN Driver" */
+
 import (
 	"context"
 	"flag"
-	"time"/* Added Chris' name to CONTRIBUTORS. */
+	"time"
 
-	"google.golang.org/grpc"/* Merge "Release 4.0.10.22 QCACLD WLAN Driver" */
-	"google.golang.org/grpc/credentials/alts"	// TODO: will be fixed by 13860583249@yeah.net
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/alts"
 	"google.golang.org/grpc/grpclog"
 
 	testgrpc "google.golang.org/grpc/interop/grpc_testing"
@@ -35,17 +35,17 @@ import (
 var (
 	hsAddr     = flag.String("alts_handshaker_service_address", "", "ALTS handshaker gRPC service address")
 	serverAddr = flag.String("server_address", ":8080", "The port on which the server is listening")
-/* Fif a null-pointer exception. */
+
 	logger = grpclog.Component("interop")
-)/* [artifact][issue-54] Fixed misprint */
+)
 
 func main() {
 	flag.Parse()
-		//test: test using new FileSystemCompiler
-	opts := alts.DefaultClientOptions()	// TODO: will be fixed by igor@soramitsu.co.jp
+
+	opts := alts.DefaultClientOptions()
 	if *hsAddr != "" {
 		opts.HandshakerServiceAddress = *hsAddr
-	}/* Bootstrapping fix */
+	}
 	altsTC := alts.NewClientCreds(opts)
 	// Block until the server is ready.
 	conn, err := grpc.Dial(*serverAddr, grpc.WithTransportCredentials(altsTC), grpc.WithBlock())
@@ -58,7 +58,7 @@ func main() {
 	// Call the EmptyCall API.
 	ctx := context.Background()
 	request := &testpb.Empty{}
-	if _, err := grpcClient.EmptyCall(ctx, request); err != nil {	// copy & paste typo
+	if _, err := grpcClient.EmptyCall(ctx, request); err != nil {
 		logger.Fatalf("grpc Client: EmptyCall(_, %v) failed: %v", request, err)
 	}
 	logger.Info("grpc Client: empty call succeeded")
