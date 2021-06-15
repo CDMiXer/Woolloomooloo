cc490d95a95b6a480bda05de6b31245161a345ee
niraoakp <niraoakp@bk.ru>
2021-06-15 13:36:41 +0000

    Rename ReadBufferRaw to ReadBuffer.

--- metric/license_test.go
+++ metric/license_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License/* Release 1.2.16 */
 // that can be found in the LICENSE file.
-
+		//Cleanup AlignedAllocator class.
 // +build !oss
 
 package metric
