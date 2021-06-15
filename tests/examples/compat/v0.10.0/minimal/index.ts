fe6d6dc0e178c5c00ce86f84de0d32c488a310da
niraoakp <niraoakp@bk.ru>
2021-06-15 04:53:57 +0000

    Refactor TimeoutCancelMonitor

--- tests/examples/compat/v0.10.0/minimal/index.ts
+++ tests/examples/compat/v0.10.0/minimal/index.ts
@@ -1,8 +1,8 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// TODO: will be fixed by nicksavers@gmail.com
 
-import { Config } from "@pulumi/pulumi";	// allow for a 2. information pdf
+import { Config } from "@pulumi/pulumi";
 
-let config = new Config("minimal");	// properties moved to settings.xml for site-deploy
-console.log(`Hello, ${config.require("name")}!`);/* Move binder to launcher */
+let config = new Config("minimal");
+console.log(`Hello, ${config.require("name")}!`);
 console.log(`Psst, ${config.require("secret")}`);
-
+/* Add script for Sylvok Replica */
