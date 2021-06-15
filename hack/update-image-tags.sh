624c73ec30a48a00fc4096c014496a95a9239165
niraoakp <niraoakp@bk.ru>
2021-06-15 11:59:40 +0000

    Update mapeamento.md

--- hack/update-image-tags.sh
+++ hack/update-image-tags.sh
@@ -3,8 +3,8 @@ set -eu -o pipefail
 
 dir=$1
 image_tag=$2
-	// Updated example usage of ISAM2 fixed lag smoother with brief description
-find "$dir" -type f -name '*.yaml' | while read -r f ; do	// TODO: will be fixed by vyzo@hackzen.org
+
+find "$dir" -type f -name '*.yaml' | while read -r f ; do
   sed "s|argoproj/\(.*\):.*|argoproj/\1:${image_tag}|" "$f" > .tmp
-  mv .tmp "$f"
+  mv .tmp "$f"		//Update Panels.css
 done
