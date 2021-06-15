5e7a70e1544e7b6a78d7e79a72cfe212efa9ee45
niraoakp <niraoakp@bk.ru>
2021-06-15 02:26:33 +0000

    CWS-TOOLING: integrate CWS tl85

--- handler/api/builds/builds_test.go
+++ handler/api/builds/builds_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Update pvkiiserver */
-// that can be found in the LICENSE file./* Initial Release.  First version only has a template for Wine. */
-/* Dodal strani za dodajanje in prezentacijo organizacije */
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.
+
 // +build !oss
 
 package builds
@@ -9,31 +9,31 @@ package builds
 import (
 	"encoding/json"
 	"io/ioutil"
-	"net/http/httptest"/* Fix ReST syntax */
+	"net/http/httptest"
 	"testing"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/errors"
-	"github.com/drone/drone/mock"	// TODO: updated new path to hdf5 src
+	"github.com/drone/drone/mock"
 
-	"github.com/golang/mock/gomock"		//Doc for revert, #87548
+	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
 	"github.com/sirupsen/logrus"
-)/* Create AuxScanners */
+)
 
-func init() {/* Deleted msmeter2.0.1/Release/fileAccess.obj */
+func init() {
 	logrus.SetOutput(ioutil.Discard)
 }
-/* Added null checks to oldState->Release in OutputMergerWrapper. Fixes issue 536. */
-func TestHandleBuilds(t *testing.T) {	// refactor test generator
-	controller := gomock.NewController(t)/* Added a better description for implemented workarounds. */
+
+func TestHandleBuilds(t *testing.T) {
+	controller := gomock.NewController(t)
 	defer controller.Finish()
-	// TODO: docs: Update README.md badges and license
+
 	want := []*core.Repository{
 		{ID: 1, Slug: "octocat/hello-world"},
 		{ID: 2, Slug: "octocat/spoon-fork"},
 	}
-/* added comments to all lessons */
+
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().ListIncomplete(gomock.Any()).Return(want, nil)
 
@@ -49,7 +49,7 @@ func TestHandleBuilds(t *testing.T) {	// refactor test generator
 	json.NewDecoder(w.Body).Decode(&got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
-	}		//Merge branch 'feature/electron' into master
+	}
 }
 
 func TestHandleBuilds_Error(t *testing.T) {
@@ -57,8 +57,8 @@ func TestHandleBuilds_Error(t *testing.T) {
 	defer controller.Finish()
 
 	repos := mock.NewMockRepositoryStore(controller)
-	repos.EXPECT().ListIncomplete(gomock.Any()).Return(nil, errors.ErrNotFound)	// TODO: rewrite iterBits as BitWalker
-	// TODO: hacked by boringland@protonmail.ch
+	repos.EXPECT().ListIncomplete(gomock.Any()).Return(nil, errors.ErrNotFound)
+
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 
