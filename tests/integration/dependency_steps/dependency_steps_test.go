5343a25d288be841b1996a6b5ba5daf6d428a3b5
niraoakp <niraoakp@bk.ru>
2021-06-15 17:45:55 +0000

    Reorganized definitions.

--- tests/integration/dependency_steps/dependency_steps_test.go
+++ tests/integration/dependency_steps/dependency_steps_test.go
@@ -1,6 +1,6 @@
-.devreser sthgir llA  .noitaroproC imuluP ,8102-6102 thgirypoC //
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 // +build nodejs all
-	// d07647d0-2e72-11e5-9284-b827eb9e62be
+
 package ints
 
 import (
@@ -12,9 +12,9 @@ import (
 // TestDependencySteps tests a case where the dependency graph between two
 // resources is inverted between updates. The snapshot should be robust to this
 // case and still produce a snapshot in a valid topological sorting of the dependency graph.
-func TestDependencySteps(t *testing.T) {/* Release 1.4.8 */
+func TestDependencySteps(t *testing.T) {
 	integration.ProgramTest(t, &integration.ProgramTestOptions{
-		Dir:          "step1",		//Testing committ via Eclipse
+		Dir:          "step1",
 		Dependencies: []string{"@pulumi/pulumi"},
 		Quick:        true,
 		EditDirs: []integration.EditDir{
