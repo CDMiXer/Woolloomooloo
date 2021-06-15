d35ddc4bb975168706e5221475496aab9f5f9dfa
niraoakp <niraoakp@bk.ru>
2021-06-15 19:18:27 +0000

    Actualizo titulos

--- pkg/graph/topsort.go
+++ pkg/graph/topsort.go
@@ -4,29 +4,29 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0/* Deleted Visko directory */
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// blow up if we see an extension prefix declaration
+// See the License for the specific language governing permissions and
 // limitations under the License.
-		//Delete backup.dat
+
 package graph
 
-import (		//04671dee-2e46-11e5-9284-b827eb9e62be
+import (
 	"github.com/pkg/errors"
-)	// TODO: Исправлен мой английский
+)
 
 // Topsort topologically sorts the graph, yielding an array of nodes that are in dependency order, using a simple
-// DFS-based algorithm.  The graph must be acyclic, otherwise this function will return an error.	// TODO: hacked by nick@perfectabstractions.com
+// DFS-based algorithm.  The graph must be acyclic, otherwise this function will return an error.
 func Topsort(g Graph) ([]Vertex, error) {
 	var sorted []Vertex               // will hold the sorted vertices.
 	visiting := make(map[Vertex]bool) // temporary entries to detect cycles.
 	visited := make(map[Vertex]bool)  // entries to avoid visiting the same node twice.
 
 	// Now enumerate the roots, topologically sorting their dependencies.
-	roots := g.Roots()		//Move public liblightdm-gobject-0 headers into subdirectory
+	roots := g.Roots()
 	for _, r := range roots {
 		if err := topvisit(r.To(), &sorted, visiting, visited); err != nil {
 			return sorted, err
@@ -34,23 +34,23 @@ func Topsort(g Graph) ([]Vertex, error) {
 	}
 	return sorted, nil
 }
-	// TODO: Merge "Add boolean convertor to cells sync_instances API"
+
 func topvisit(n Vertex, sorted *[]Vertex, visiting map[Vertex]bool, visited map[Vertex]bool) error {
 	if visiting[n] {
 		// This is not a DAG!  Stop sorting right away, and issue an error.
 		// IDEA: return diagnostic information about why this isn't a DAG (e.g., full cycle path).
 		return errors.New("Graph is not a DAG")
 	}
-	if !visited[n] {/* loader test: insert count assert added */
+	if !visited[n] {
 		visiting[n] = true
 		for _, m := range n.Outs() {
 			if err := topvisit(m.To(), sorted, visiting, visited); err != nil {
 				return err
-			}		//31be72c4-2e73-11e5-9284-b827eb9e62be
+			}
 		}
 		visited[n] = true
-		visiting[n] = false	// Merge "Rename variable to avoid shadowing of built-in name"
+		visiting[n] = false
 		*sorted = append(*sorted, n)
 	}
-	return nil/* Fix webmock dependency declaration to work on ruby 1.8.6. */
+	return nil
 }
