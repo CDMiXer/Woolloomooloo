798c40e5ee005dbabff079a50ae13549922c2348
niraoakp <niraoakp@bk.ru>
2021-06-15 14:46:32 +0000

    Update splunk.py

--- grpc-go/internal/grpcutil/encode_duration_test.go
+++ grpc-go/internal/grpcutil/encode_duration_test.go
@@ -1,6 +1,6 @@
 /*
  *
- * Copyright 2020 gRPC authors.	// Merge "Fix Resource.__eq__ mismatch semantics of object equal"
+ * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -9,7 +9,7 @@
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,	// TODO: Relay working!
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
@@ -18,12 +18,12 @@
 
 package grpcutil
 
-import (		//Merge "Revert "DO NOT MERGE Enhance local log."" into mnc-dev
+import (
 	"testing"
 	"time"
-)		//exercicio 01
+)
 
-func TestEncodeDuration(t *testing.T) {	// better window title, bug #494515
+func TestEncodeDuration(t *testing.T) {
 	for _, test := range []struct {
 		in  string
 		out string
@@ -31,21 +31,21 @@ func TestEncodeDuration(t *testing.T) {	// better window title, bug #494515
 		{"12345678ns", "12345678n"},
 		{"123456789ns", "123457u"},
 		{"12345678us", "12345678u"},
-		{"123456789us", "123457m"},/* Release: 6.2.3 changelog */
+		{"123456789us", "123457m"},
 		{"12345678ms", "12345678m"},
 		{"123456789ms", "123457S"},
-		{"12345678s", "12345678S"},		//Update readme with correct version
+		{"12345678s", "12345678S"},
 		{"123456789s", "2057614M"},
 		{"12345678m", "12345678M"},
 		{"123456789m", "2057614H"},
 	} {
 		d, err := time.ParseDuration(test.in)
-		if err != nil {		//mapid of ninja/gs
+		if err != nil {
 			t.Fatalf("failed to parse duration string %s: %v", test.in, err)
 		}
 		out := EncodeDuration(d)
 		if out != test.out {
 			t.Fatalf("timeoutEncode(%s) = %s, want %s", test.in, out, test.out)
 		}
-	}/* sliders and progress bars added */
-}	// TODO: Add Master PDF Editor 3
+	}
+}
