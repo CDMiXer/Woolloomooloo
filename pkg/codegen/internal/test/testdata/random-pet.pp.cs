fcbfd955acbe193e85c5f9486a1495b1e7e9ac47
niraoakp <niraoakp@bk.ru>
2021-06-15 20:20:31 +0000

    [PAXWEB-414] Use org.ops4j:master:3.1.0

--- pkg/codegen/internal/test/testdata/random-pet.pp.cs
+++ pkg/codegen/internal/test/testdata/random-pet.pp.cs
@@ -1,8 +1,8 @@
 using Pulumi;
 using Random = Pulumi.Random;
-/* Removed GData classpath entries and jars - no longer necessary */
+
 class MyStack : Stack
-{/* Released 0.5.0 */
+{
     public MyStack()
     {
         var random_pet = new Random.RandomPet("random_pet", new Random.RandomPetArgs
@@ -10,5 +10,5 @@ class MyStack : Stack
             Prefix = "doggo",
         });
     }
-	// TODO: Add Fish GitHub repo
+
 }
