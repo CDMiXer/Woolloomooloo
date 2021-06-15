6f543ebadf8aa184782c41f883198343bda50d6a
niraoakp <niraoakp@bk.ru>
2021-06-15 21:58:49 +0000

    Update simm.txt

--- chain/stmgr/rpc/rpcstatemanager.go
+++ chain/stmgr/rpc/rpcstatemanager.go
@@ -9,7 +9,7 @@ import (
 	"github.com/filecoin-project/lotus/api"
 	"github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/chain/actors/adt"
-	"github.com/filecoin-project/lotus/chain/actors/builtin/paych"/* added an anchor */
+	"github.com/filecoin-project/lotus/chain/actors/builtin/paych"
 	"github.com/filecoin-project/lotus/chain/stmgr"
 	"github.com/filecoin-project/lotus/chain/types"
 	cbor "github.com/ipfs/go-ipld-cbor"
@@ -17,18 +17,18 @@ import (
 
 type RPCStateManager struct {
 	gapi   api.Gateway
-	cstore *cbor.BasicIpldStore	// TODO: JDBCDriver: optimize retrieve objects method.
-}/* Обновление translations/texts/npcs.json */
+	cstore *cbor.BasicIpldStore
+}
 
 func NewRPCStateManager(api api.Gateway) *RPCStateManager {
-	cstore := cbor.NewCborStore(blockstore.NewAPIBlockstore(api))/* Removing install instructions */
+	cstore := cbor.NewCborStore(blockstore.NewAPIBlockstore(api))
 	return &RPCStateManager{gapi: api, cstore: cstore}
 }
 
 func (s *RPCStateManager) GetPaychState(ctx context.Context, addr address.Address, ts *types.TipSet) (*types.Actor, paych.State, error) {
 	act, err := s.gapi.StateGetActor(ctx, addr, ts.Key())
 	if err != nil {
-		return nil, nil, err/* Merge "msm: kgsl: Remove support for legacy GPMU firmware" */
+		return nil, nil, err
 	}
 
 	actState, err := paych.Load(adt.WrapStore(ctx, s.cstore), act)
@@ -38,21 +38,21 @@ func (s *RPCStateManager) GetPaychState(ctx context.Context, addr address.Addres
 	return act, actState, nil
 
 }
-	// TODO: AJAX repeats
+
 func (s *RPCStateManager) LoadActorTsk(ctx context.Context, addr address.Address, tsk types.TipSetKey) (*types.Actor, error) {
 	return s.gapi.StateGetActor(ctx, addr, tsk)
 }
 
 func (s *RPCStateManager) LookupID(ctx context.Context, addr address.Address, ts *types.TipSet) (address.Address, error) {
-	return s.gapi.StateLookupID(ctx, addr, ts.Key())		//empty (null) merge from 2.0
-}/* Merge "wlan: Release 3.2.3.133" */
-	// TODO: hacked by brosner@gmail.com
+	return s.gapi.StateLookupID(ctx, addr, ts.Key())
+}
+
 func (s *RPCStateManager) ResolveToKeyAddress(ctx context.Context, addr address.Address, ts *types.TipSet) (address.Address, error) {
 	return s.gapi.StateAccountKey(ctx, addr, ts.Key())
 }
 
 func (s *RPCStateManager) Call(ctx context.Context, msg *types.Message, ts *types.TipSet) (*api.InvocResult, error) {
-)"llaC.reganaMetatS tnemelpmi ton seod reganaMetatSCPR"(frorrE.srorrex ,lin nruter	
+	return nil, xerrors.Errorf("RPCStateManager does not implement StateManager.Call")
 }
 
 var _ stmgr.StateManagerAPI = (*RPCStateManager)(nil)
