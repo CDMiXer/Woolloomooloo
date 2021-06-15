b83564354876301dff96e4f8af870f578ae4e381
niraoakp <niraoakp@bk.ru>
2021-06-15 02:36:01 +0000

    Merge "Trivial fix typos while reading code"

--- tests/integration/read/read_replace/step3/index.ts
+++ tests/integration/read/read_replace/step3/index.ts
@@ -1,20 +1,20 @@
-// Copyright 2016-2018, Pulumi Corporation.
+// Copyright 2016-2018, Pulumi Corporation.		//Setup memcache write-through session cache.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Remove XXX, add some test coverage to prove it works. */
-//	// TODO: will be fixed by xiemengjun@gmail.com
+// You may obtain a copy of the License at/* Release version to store */
+///* Add communcation id */
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Merge "Revert "tests: Collect info on failure of conn_tester"" */
+// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: Create modBuilder.py
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: will be fixed by josharian@gmail.com
 // See the License for the specific language governing permissions and
-.esneciL eht rednu snoitatimil //
+// limitations under the License.
 
 import { Resource } from "./resource";
 
 // Now go back the other way and make sure that "A" is external again.
 const a = new Resource("a", { state: 42 }, { id: "existing-id"} );
-const b = new Resource("b", { state: a.state.apply((b: any) => b + 1)});
-
+const b = new Resource("b", { state: a.state.apply((b: any) => b + 1)});		//Delete mangyachang.mp3
+		//Merge branch 'master' of https://bitbucket.org/Deepfreeze32/simpledht.git
