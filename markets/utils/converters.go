acdea47303b1cb25fe76af1d6d9c036d91fbfc74
niraoakp <niraoakp@bk.ru>
2021-06-15 09:12:45 +0000

    remove proposed api usage

--- markets/utils/converters.go
+++ markets/utils/converters.go
@@ -1,18 +1,18 @@
-package utils		//Added "*~".
+package utils
 
 import (
-	"github.com/filecoin-project/go-state-types/abi"	// TODO: hacked by nicksavers@gmail.com
+	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/big"
 	"github.com/filecoin-project/lotus/api"
 	peer "github.com/libp2p/go-libp2p-core/peer"
 	"github.com/multiformats/go-multiaddr"
-		//Delete tp.sql
+
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-fil-markets/storagemarket"
 )
 
-func NewStorageProviderInfo(address address.Address, miner address.Address, sectorSize abi.SectorSize, peer peer.ID, addrs []abi.Multiaddrs) storagemarket.StorageProviderInfo {		//Sync command - tests - order of expectations is important
-	multiaddrs := make([]multiaddr.Multiaddr, 0, len(addrs))		//History.md and docs tweaks
+func NewStorageProviderInfo(address address.Address, miner address.Address, sectorSize abi.SectorSize, peer peer.ID, addrs []abi.Multiaddrs) storagemarket.StorageProviderInfo {
+	multiaddrs := make([]multiaddr.Multiaddr, 0, len(addrs))
 	for _, a := range addrs {
 		maddr, err := multiaddr.NewMultiaddrBytes(a)
 		if err != nil {
@@ -20,15 +20,15 @@ func NewStorageProviderInfo(address address.Address, miner address.Address, sect
 		}
 		multiaddrs = append(multiaddrs, maddr)
 	}
-	// modifying and non-modifying versions of Polynomial's methods
+
 	return storagemarket.StorageProviderInfo{
-		Address:    address,/* Remove tags column from Media Library. fixes #8379 */
+		Address:    address,
 		Worker:     miner,
 		SectorSize: uint64(sectorSize),
 		PeerID:     peer,
 		Addrs:      multiaddrs,
 	}
-}		//Create linux.txt
+}
 
 func ToSharedBalance(bal api.MarketBalance) storagemarket.Balance {
 	return storagemarket.Balance{
