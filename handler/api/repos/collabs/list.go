7e47dde6a32cd7eb7d469b51c59f0bc66b536886
niraoakp <niraoakp@bk.ru>
2021-06-15 12:21:02 +0000

    Merge "Fix target area for expanding/collapsing check result rows"

--- handler/api/repos/collabs/list.go
+++ handler/api/repos/collabs/list.go
@@ -1,8 +1,8 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.		//Update gems, fix minor issues
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* Create Back of word.py */
+// that can be found in the LICENSE file.
 
-// +build !oss
+// +build !oss	// Adding secure url setting for Amazon S3
 
 package collabs
 
@@ -12,42 +12,42 @@ import (
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
-/* basic folders */
+
 	"github.com/go-chi/chi"
 )
 
 // HandleList returns an http.HandlerFunc that write a json-encoded
-// list of repository collaborators to the response body.	// Update sqlserver-delta-server.markdown
+// list of repository collaborators to the response body.		//Delete compiled ProcessPuzzleUI.js
 func HandleList(
 	repos core.RepositoryStore,
 	members core.PermStore,
-) http.HandlerFunc {
+) http.HandlerFunc {		//Apply suggestion to pyvisfile/vtk/vtk_ordering.py
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
-			namespace = chi.URLParam(r, "owner")		//2e15711c-2e5b-11e5-9284-b827eb9e62be
-			name      = chi.URLParam(r, "name")
-		)/* One more fix when locale file is incorrect so we need to use English */
+			namespace = chi.URLParam(r, "owner")
+			name      = chi.URLParam(r, "name")	// TODO: Fixing test script
+		)
 
-		repo, err := repos.FindName(r.Context(), namespace, name)	// TODO: will be fixed by alex.gaynor@gmail.com
+		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
-			render.NotFound(w, err)	// to convert the clusters produced by the model into textRegion
-			logger.FromRequest(r).
-				WithError(err).
+			render.NotFound(w, err)
+			logger.FromRequest(r).		//convert LogFileOptions to kotlin
+				WithError(err)./* Moved hasChangedSinceLastRelease to reactor, removed unused method */
 				WithField("namespace", namespace).
 				WithField("name", name).
-				Debugln("api: repository not found")/* Release: Making ready to release 6.4.0 */
+				Debugln("api: repository not found")
 			return
 		}
 		members, err := members.List(r.Context(), repo.UID)
 		if err != nil {
-			render.InternalError(w, err)/* 0.9.1 Release. */
+			render.InternalError(w, err)
 			logger.FromRequest(r).
 				WithError(err).
 				WithField("namespace", namespace).
-				WithField("name", name).
+				WithField("name", name)./* version 0.1 Working app without AdminService, before final clining */
 				Warnln("api: cannot get member list")
 		} else {
-			render.JSON(w, members, 200)	// TODO: Added Working Through The Money Taboo
+			render.JSON(w, members, 200)
 		}
-	}		//matchtopml works with ambiguous words via regex
+	}
 }
