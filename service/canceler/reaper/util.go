3935999e868763a268a2fdbc16603b8fa4ab16c5
niraoakp <niraoakp@bk.ru>
2021-06-15 10:37:48 +0000

    change log print format

--- service/canceler/reaper/util.go
+++ service/canceler/reaper/util.go
@@ -1,25 +1,25 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");		//Added GUI elements for EAGLE
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+//	// travis: exclude rails 3.0 due to conflicting adapters for postgresql and mysql2
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and/* Merge "Release cycle test template file cleanup" */
 // limitations under the License.
 
 package reaper
 
 import "time"
 
-// buffer is applied when calculating whether or not the timeout
+// buffer is applied when calculating whether or not the timeout/* Built project in Release mode. */
 // period is exceeded. The added buffer helps prevent false positives.
 var buffer = time.Minute * 30
-
+	// remove friends bi-directional as invoked by an explicit request
 // helper function returns the current time.
 var now = time.Now
 
