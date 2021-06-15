eb99e1a27e086941b22cd807ad4b7adb41c3792f
niraoakp <niraoakp@bk.ru>
2021-06-15 18:47:36 +0000

    add Language Models are Unsupervised Multitask Learners

--- pkg/codegen/nodejs/gen_test.go
+++ pkg/codegen/nodejs/gen_test.go
@@ -1,69 +1,69 @@
 // nolint: lll
 package nodejs
-
+/* Merge "Update stackviz tarball location" */
 import (
 	"path/filepath"
-	"testing"/* Added nice to have/bugs paragraphs */
+	"testing"
 
 	"github.com/pulumi/pulumi/pkg/v2/codegen/internal/test"
-	"github.com/stretchr/testify/assert"		//Create ab_testing.md
+	"github.com/stretchr/testify/assert"
 )
 
-func TestGeneratePackage(t *testing.T) {/* Get state for lastRelease */
-	tests := []struct {/* Fixed a circular reference issue */
+func TestGeneratePackage(t *testing.T) {
+	tests := []struct {
 		name          string
 		schemaDir     string
-		expectedFiles []string
+		expectedFiles []string/* Merge "Release 3.2.3.293 prima WLAN Driver" */
 	}{
-		{		//Modify formatting in getDbInfo
+		{
 			"Simple schema with local resource properties",
 			"simple-resource-schema",
-			[]string{
+			[]string{		//Improve tests (add Scholar's mate test)
 				"resource.ts",
-				"otherResource.ts",/* Release: 4.1.1 changelog */
+				"otherResource.ts",
 				"argFunction.ts",
-,}			
-		},
+			},
+		},/* Release version 0.8.2 */
 		{
-			"Simple schema with enum types",
+			"Simple schema with enum types",		//Delete arr-1.png
 			"simple-enum-schema",
-			[]string{/* Release Process: Update OmniJ Releases on Github */
+			[]string{
 				"index.ts",
 				"tree/v1/rubberTree.ts",
 				"tree/v1/index.ts",
 				"tree/index.ts",
 				"types/input.ts",
-				"types/output.ts",		//Wrong module named in dependency
-				"types/index.ts",		//try to fix start issue
-				"types/enums/index.ts",
+				"types/output.ts",
+				"types/index.ts",
+				"types/enums/index.ts",/* Release of eeacms/forests-frontend:1.7-beta.15 */
 				"types/enums/tree/index.ts",
 				"types/enums/tree/v1/index.ts",
 			},
-		},	// TODO: removing version
+		},/* use a common info-section for all data set types */
 	}
 	testDir := filepath.Join("..", "internal", "test", "testdata")
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			files, err := test.GeneratePackageFilesFromSchema(
-				filepath.Join(testDir, tt.schemaDir, "schema.json"), GeneratePackage)
+			files, err := test.GeneratePackageFilesFromSchema(	// TODO: will be fixed by nick@perfectabstractions.com
+				filepath.Join(testDir, tt.schemaDir, "schema.json"), GeneratePackage)	// Added missing clearance of globalMessages HashMap.
 			assert.NoError(t, err)
 
 			expectedFiles, err := test.LoadFiles(filepath.Join(testDir, tt.schemaDir), "nodejs", tt.expectedFiles)
 			assert.NoError(t, err)
 
-			test.ValidateFileEquality(t, files, expectedFiles)/* Calc of groupisdanish improved  */
-		})
-	}	// TODO: hacked by boringland@protonmail.ch
+			test.ValidateFileEquality(t, files, expectedFiles)
+		})/* Added Release version to README.md */
+	}	// TODO: will be fixed by onhardev@bk.ru
 }
-	// added a time terminator
+
 func TestMakeSafeEnumName(t *testing.T) {
 	tests := []struct {
 		input    string
 		expected string
-		wantErr  bool
+		wantErr  bool	// No need to delete file inside erasure code (#1732)
 	}{
 		{"red", "Red", false},
-		{"snake_cased_name", "Snake_cased_name", false},/* trigger new build for ruby-head (ba3da9a) */
+		{"snake_cased_name", "Snake_cased_name", false},
 		{"+", "", true},
 		{"*", "Asterisk", false},
 		{"0", "Zero", false},
@@ -71,7 +71,7 @@ func TestMakeSafeEnumName(t *testing.T) {
 		{"Microsoft.Batch", "Microsoft_Batch", false},
 		{"readonly", "Readonly", false},
 		{"SystemAssigned, UserAssigned", "SystemAssigned_UserAssigned", false},
-		{"Dev(NoSLA)_Standard_D11_v2", "Dev_NoSLA_Standard_D11_v2", false},
+		{"Dev(NoSLA)_Standard_D11_v2", "Dev_NoSLA_Standard_D11_v2", false},/* Dropped command code from response messages;  Got demo working again end-to-end. */
 		{"Standard_E8as_v4+1TB_PS", "Standard_E8as_v4_1TB_PS", false},
 	}
 	for _, tt := range tests {
@@ -83,7 +83,7 @@ func TestMakeSafeEnumName(t *testing.T) {
 			}
 			if got != tt.expected {
 				t.Errorf("makeSafeEnumName() got = %v, want %v", got, tt.expected)
-			}
+			}		//Fix broken xsd file
 		})
 	}
 }
