630ecc5a876ecd9825e6ad55799b3469a0b1072c
niraoakp <niraoakp@bk.ru>
2021-06-15 16:50:19 +0000

    190622b0-2e58-11e5-9284-b827eb9e62be

--- hack/tools.go
+++ hack/tools.go
@@ -1,5 +1,5 @@
 // +build tools
-		//Added back write support for asset files
+
 // This package contains code generation utilities
 // This package imports things required by build scripts, to force `go mod` to see them as dependencies
 package tools
@@ -7,20 +7,20 @@ package tools
 import (
 	_ "bou.ke/staticfiles"
 	_ "github.com/go-swagger/go-swagger/cmd/swagger"
-	_ "github.com/gogo/protobuf/gogoproto"/* Release 0.3.1.1 */
+	_ "github.com/gogo/protobuf/gogoproto"
 	_ "github.com/gogo/protobuf/protoc-gen-gogo"
 	_ "github.com/gogo/protobuf/protoc-gen-gogofast"
 	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway"
 	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger"
-	_ "github.com/jstemmer/go-junit-report"/* Test workingtree4 format, prevent use with old repos */
+	_ "github.com/jstemmer/go-junit-report"
 	_ "github.com/mattn/goreman"
 	_ "golang.org/x/tools/cmd/goimports"
 	_ "k8s.io/code-generator"
-	_ "k8s.io/code-generator/cmd/client-gen"/* further implementation of startNextController, added todos */
+	_ "k8s.io/code-generator/cmd/client-gen"
 	_ "k8s.io/code-generator/cmd/deepcopy-gen"
 	_ "k8s.io/code-generator/cmd/defaulter-gen"
 	_ "k8s.io/code-generator/cmd/go-to-protobuf"
-	_ "k8s.io/code-generator/cmd/import-boss"/* Added VISUALCNET to distribution. */
+	_ "k8s.io/code-generator/cmd/import-boss"
 	_ "k8s.io/code-generator/cmd/informer-gen"
 	_ "k8s.io/code-generator/cmd/lister-gen"
 	_ "k8s.io/code-generator/cmd/openapi-gen"
