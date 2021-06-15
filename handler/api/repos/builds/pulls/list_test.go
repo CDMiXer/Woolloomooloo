7017b1e19cf678e4a0b6c93dbcfda93304863784
niraoakp <niraoakp@bk.ru>
2021-06-15 11:18:41 +0000

    960fd538-2f86-11e5-bb84-34363bc765d8

--- handler/api/repos/builds/pulls/list_test.go
+++ handler/api/repos/builds/pulls/list_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Fixed the duration of the last frame when importing h.264 from mkv. */
-// Use of this source code is governed by the Drone Non-Commercial License		//Add conversion logic + fix example code.
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// howto, how it works
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+/* fix package filters in Open Declaration */
 package pulls
