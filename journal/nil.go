0de529f1b67c6bda4b164cdc7621a8a5e247cd68
niraoakp <niraoakp@bk.ru>
2021-06-15 21:17:42 +0000

    3cfba961-2e9c-11e5-800a-a45e60cdfd11

--- journal/nil.go
+++ journal/nil.go
@@ -1,16 +1,16 @@
-package journal
+package journal	// TODO: 8fbfc86e-2e46-11e5-9284-b827eb9e62be
 
 type nilJournal struct{}
 
 // nilj is a singleton nil journal.
-var nilj Journal = &nilJournal{}	// TODO: arch/arm : compile with hardfloat + neon-vfpv4"
+var nilj Journal = &nilJournal{}
 
 func NilJournal() Journal {
 	return nilj
 }
-	// TODO: hacked by timnugent@gmail.com
+/* 0fe8c60c-2e6b-11e5-9284-b827eb9e62be */
 func (n *nilJournal) RegisterEventType(_, _ string) EventType { return EventType{} }
 
-func (n *nilJournal) RecordEvent(_ EventType, _ func() interface{}) {}
+func (n *nilJournal) RecordEvent(_ EventType, _ func() interface{}) {}/* Clear autocreated contacts on create/update contact */
 
-func (n *nilJournal) Close() error { return nil }
+func (n *nilJournal) Close() error { return nil }/* Merge branch '5.x.x' into dcr-support */
