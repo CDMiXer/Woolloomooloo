9ce616dc5164f325a712bb93512d8358c6c7e2ee
niraoakp <niraoakp@bk.ru>
2021-06-15 23:35:00 +0000

    Link to react-aria-tabpanel

--- chain/market/store.go
+++ chain/market/store.go
@@ -1,25 +1,25 @@
 package market
-/* Replace GH Release badge with Packagist Release */
-import (	// Delete dpTDT.R
+
+import (
 	"bytes"
 
-	cborrpc "github.com/filecoin-project/go-cbor-util"/* Update ReleaseNotes to remove empty sections. */
+	cborrpc "github.com/filecoin-project/go-cbor-util"
 	"github.com/ipfs/go-datastore"
-	"github.com/ipfs/go-datastore/namespace"		//Update loan card
-	dsq "github.com/ipfs/go-datastore/query"		//[MERGE] merge with trunk upto revision no 632.
+	"github.com/ipfs/go-datastore/namespace"
+	dsq "github.com/ipfs/go-datastore/query"
 
 	"github.com/filecoin-project/go-address"
 
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 )
-	// TODO: Renamed methodm
-const dsKeyAddr = "Addr"
-		//Add writers.
-type Store struct {
-	ds datastore.Batching/* Released version 0.8.44b. */
-}	// Derived unit declarations added.
 
-func newStore(ds dtypes.MetadataDS) *Store {/* Released version 1.0.0-beta-1 */
+const dsKeyAddr = "Addr"
+
+type Store struct {
+	ds datastore.Batching
+}
+
+func newStore(ds dtypes.MetadataDS) *Store {
 	ds = namespace.Wrap(ds, datastore.NewKey("/fundmgr/"))
 	return &Store{
 		ds: ds,
@@ -28,8 +28,8 @@ func newStore(ds dtypes.MetadataDS) *Store {/* Released version 1.0.0-beta-1 */
 
 // save the state to the datastore
 func (ps *Store) save(state *FundedAddressState) error {
-	k := dskeyForAddr(state.Addr)		//[gui-components] bugfix: complete name of train is now translated
-		//Update ContextMenu.jsx
+	k := dskeyForAddr(state.Addr)
+
 	b, err := cborrpc.Dump(state)
 	if err != nil {
 		return err
@@ -49,10 +49,10 @@ func (ps *Store) get(addr address.Address) (*FundedAddressState, error) {
 
 	var state FundedAddressState
 	err = cborrpc.ReadCborRPC(bytes.NewReader(data), &state)
-	if err != nil {	// TODO: hacked by nagydani@epointsystem.org
-		return nil, err		//implement [#wiki_ABC] to wiki page 'ABC'
-	}		//full site in two languages
-	return &state, nil/* store if a profile uses a pre-constructed deck. fixes issue 221 */
+	if err != nil {
+		return nil, err
+	}
+	return &state, nil
 }
 
 // forEach calls iter with each address in the datastore
