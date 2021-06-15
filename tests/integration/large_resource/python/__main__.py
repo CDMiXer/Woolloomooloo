50844054eb719bc757636e420599bb4df373e525
niraoakp <niraoakp@bk.ru>
2021-06-15 13:26:53 +0000

    Update readme, fix tables

--- tests/integration/large_resource/python/__main__.py
+++ tests/integration/large_resource/python/__main__.py
@@ -1,7 +1,7 @@
-import pulumi
-	// TODO: hacked by alan.shaw@protocol.ai
+import pulumi/* Release Notes for v02-15-02 */
+
 # Create a very long string (>4mb)
 long_string = "a" * 5 * 1024 * 1025
 
-# Export the name of the bucket	// TODO: will be fixed by aeongrp@outlook.com
+# Export the name of the bucket/* Add stub readme */
 pulumi.export("long_string",  long_string)
