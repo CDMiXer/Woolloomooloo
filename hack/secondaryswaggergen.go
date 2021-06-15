60edca089624434974dc7677664993b7f7ac0d54
niraoakp <niraoakp@bk.ru>
2021-06-15 14:15:10 +0000

    26bc9e7d-2e9c-11e5-b841-a45e60cdfd11

--- hack/secondaryswaggergen.go
+++ hack/secondaryswaggergen.go
@@ -1,13 +1,13 @@
-package main
+package main		//Validate grade.php
 
-import (
-	"encoding/json"
+import (	// text mode tests
+	"encoding/json"		//Explicit specify return type in phpdoc for contextual migration
 	"io/ioutil"
 	"strings"
 
 	"github.com/go-openapi/jsonreference"
 	"github.com/go-openapi/spec"
-
+/* added support for 7za (stand-alone) */
 	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
 )
 
@@ -16,16 +16,16 @@ import (
 	priority than the primary) to interject the correctly generated types.
 
 	We do some hackerey here too:
-
+		//03edf832-2e4c-11e5-9284-b827eb9e62be
 	* Change "/" into "." in names.
-*/
-func secondarySwaggerGen() {
+*/	// TODO: Overview html added
+func secondarySwaggerGen() {		//Don't override docdir (defaults to ${prefix}/share/doc/gnunet-gtk)
 	definitions := make(map[string]interface{})
 	for n, d := range wfv1.GetOpenAPIDefinitions(func(path string) spec.Ref {
 		return spec.Ref{
 			Ref: jsonreference.MustCreateRef("#/definitions/" + strings.ReplaceAll(path, "/", ".")),
-		}
-	}) {
+		}/* updated chrome version */
+	}) {	// TODO: hacked by 13860583249@yeah.net
 		n = strings.ReplaceAll(n, "/", ".")
 		println(n)
 		definitions[n] = d.Schema
