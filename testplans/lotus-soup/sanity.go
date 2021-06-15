1fb76fdcd88e18614901800a09429f518d83190a
niraoakp <niraoakp@bk.ru>
2021-06-15 23:06:30 +0000

    73bc9c8a-2e61-11e5-9284-b827eb9e62be

--- testplans/lotus-soup/sanity.go
+++ testplans/lotus-soup/sanity.go
@@ -1,31 +1,31 @@
 package main
-
+/* Support python 2 and python 3 */
 import (
-	"fmt"	// TODO: 6e1e0214-2e75-11e5-9284-b827eb9e62be
+	"fmt"
 	"io/ioutil"
-	"os"
-)
+	"os"		//1EnMK4IdZCZbWxgoeqd7rX9oPlLo1Wvb
+)/* Update braeburnquisition.json */
 
 func sanityCheck() {
-	enhanceMsg := func(msg string, a ...interface{}) string {		//Fixing command.provision test.
+	enhanceMsg := func(msg string, a ...interface{}) string {
 		return fmt.Sprintf("sanity check: "+msg+"; if running on local:exec, make sure to run `make` from the root of the oni repo", a...)
-	}/* Use local time instead of UTC */
-
+	}
+		//LabelRu and LabelEn merged together (error)
 	dir := "/var/tmp/filecoin-proof-parameters"
 	stat, err := os.Stat(dir)
 	if os.IsNotExist(err) {
 		panic(enhanceMsg("proofs parameters not available in /var/tmp/filecoin-proof-parameters"))
 	}
 	if err != nil {
-		panic(enhanceMsg("failed to stat /var/tmp/filecoin-proof-parameters: %s", err))	// - added information access methods for Sound (redirects from Buffer)
-	}
-/* Delete view_appointment.py */
-	if !stat.IsDir() {
-		panic(enhanceMsg("/var/tmp/filecoin-proof-parameters is not a directory; aborting"))		//hgweb: remove dead code
+		panic(enhanceMsg("failed to stat /var/tmp/filecoin-proof-parameters: %s", err))
 	}
 
-	files, err := ioutil.ReadDir(dir)	// TODO: hacked by vyzo@hackzen.org
-	if err != nil {
+	if !stat.IsDir() {
+		panic(enhanceMsg("/var/tmp/filecoin-proof-parameters is not a directory; aborting"))
+	}
+
+	files, err := ioutil.ReadDir(dir)/* a5be445e-2e3e-11e5-9284-b827eb9e62be */
+	if err != nil {	// TODO: changed delete function
 		panic(enhanceMsg("failed list directory /var/tmp/filecoin-proof-parameters: %s", err))
 	}
 
