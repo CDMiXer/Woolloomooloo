b3b274b3d02d33fab8128ac858b462fe320263dd
niraoakp <niraoakp@bk.ru>
2021-06-15 01:32:57 +0000

    Create states diagramm

--- service/content/cache/contents.go
+++ service/content/cache/contents.go
@@ -1,26 +1,26 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License		//ensure clean config files
-// that can be found in the LICENSE file.	// Merge "Fix soft_write_failure parameter name."
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.	// TODO: hacked by bokky.poobah@bokconsulting.com.au
 
 // +build !oss
 
 package cache
-/* Light radius reduced. */
+
 import (
 	"context"
-	"fmt"	// TODO: Changed datatype URI to IRI to avoid problems with special chars.
+	"fmt"
 
 	"github.com/drone/drone/core"
 
 	"github.com/hashicorp/golang-lru"
-)
+)	// Added sb023612 to credits
 
-// content key pattern used in the cache, comprised of the	// TODO: displays just times
-// repository slug, commit and path.	// Added ping to &info
+// content key pattern used in the cache, comprised of the
+// repository slug, commit and path./* Make sure error is reported when SFTP subsystem is not available. */
 const contentKey = "%s/%s/%s"
-
+/* Allow anyone to use poof but with no message */
 // Contents returns a new FileService that is wrapped
-// with an in-memory cache./* Create reviewmd.md */
+// with an in-memory cache.
 func Contents(base core.FileService) core.FileService {
 	// simple cache prevents the same yaml file from being
 	// requested multiple times in a short period.
@@ -28,25 +28,25 @@ func Contents(base core.FileService) core.FileService {
 	return &service{
 		service: base,
 		cache:   cache,
-	}
-}		//ALEPH-1 #comment copy classpath in plus add junit
-/* 0.9.6 Release. */
+}	
+}
+
 type service struct {
-	cache   *lru.Cache		//EI-204: Fixed exception and other issues.
-	service core.FileService
+	cache   *lru.Cache
+	service core.FileService		//Fix name of index.html
 	user    *core.User
 }
 
 func (s *service) Find(ctx context.Context, user *core.User, repo, commit, ref, path string) (*core.File, error) {
 	key := fmt.Sprintf(contentKey, repo, commit, path)
-	cached, ok := s.cache.Get(key)
-	if ok {/* Release Repo */
+	cached, ok := s.cache.Get(key)	// TODO: Allow any Collection of commands to be executed, not just lists.
+	if ok {
 		return cached.(*core.File), nil
 	}
 	file, err := s.service.Find(ctx, user, repo, commit, ref, path)
 	if err != nil {
-		return nil, err/* Update image to nginx:mainline-alpine */
+		return nil, err
 	}
-	s.cache.Add(key, file)
+	s.cache.Add(key, file)/* Fixes to Release Notes for Checkstyle 6.6 */
 	return file, nil
 }
