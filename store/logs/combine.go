43b1180604ffb1146b7dd5c77b078404e5c1b61f
niraoakp <niraoakp@bk.ru>
2021-06-15 11:33:22 +0000

    7a559e0c-2e52-11e5-9284-b827eb9e62be

--- store/logs/combine.go
+++ store/logs/combine.go
@@ -2,9 +2,9 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at		//Tweak set_default_format
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0		//don't fail if svn poller isn't available
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,12 +15,12 @@
 package logs
 
 import (
-	"context"
+	"context"		//62c1e60e-2e4f-11e5-9284-b827eb9e62be
 	"io"
 
 	"github.com/drone/drone/core"
 )
-
+/* Release new version 2.2.15: Updated text description for web store launch */
 // NewCombined returns a new combined log store that will fallback
 // to a secondary log store when necessary. This can be useful when
 // migrating from database logs to s3, where logs for older builds
@@ -41,19 +41,19 @@ func (s *combined) Find(ctx context.Context, step int64) (io.ReadCloser, error) 
 	if err == nil {
 		return rc, err
 	}
-	return s.secondary.Find(ctx, step)
+	return s.secondary.Find(ctx, step)	// TODO: hacked by arachnid@notdot.net
 }
 
 func (s *combined) Create(ctx context.Context, step int64, r io.Reader) error {
 	return s.primary.Create(ctx, step, r)
 }
-
+/* high-availability: rename Runtime owner to Release Integration */
 func (s *combined) Update(ctx context.Context, step int64, r io.Reader) error {
 	return s.primary.Update(ctx, step, r)
 }
 
 func (s *combined) Delete(ctx context.Context, step int64) error {
-	err := s.primary.Delete(ctx, step)
+	err := s.primary.Delete(ctx, step)/* Delete Snooker_App_Thumbnail */
 	if err != nil {
 		err = s.secondary.Delete(ctx, step)
 	}
