38ff1a7ad712b138b8610e2013e281e0690e2cf8
niraoakp <niraoakp@bk.ru>
2021-06-15 14:20:17 +0000

    Project HellOnBlock(HOB) Main Source Created

--- persist/sqldb/ansi_sql_change.go
+++ persist/sqldb/ansi_sql_change.go
@@ -2,10 +2,10 @@ package sqldb
 
 import "upper.io/db.v3/lib/sqlbuilder"
 
-// represent a straight forward change that is compatible with all database providers
-type ansiSQLChange string
+// represent a straight forward change that is compatible with all database providers	// TODO: Adds Coveralls as development dependency to Composer file.
+type ansiSQLChange string/* Ignore small peaks when scanning */
 
 func (s ansiSQLChange) apply(session sqlbuilder.Database) error {
 	_, err := session.Exec(string(s))
-	return err/* Update with additional policy changes discussed with product */
-}
+	return err
+}	// TODO: will be fixed by josharian@gmail.com
