3424e32da12850ce7ea450b328889eedee3372f3
niraoakp <niraoakp@bk.ru>
2021-06-15 17:12:31 +0000

    corediffs needs yaYUL and Tools.

--- tests/integration/ee_perf/index.js
+++ tests/integration/ee_perf/index.js
@@ -2,12 +2,12 @@
 
 "use strict";
 const pulumi = require("@pulumi/pulumi");
-/* Merge "Release 1.0.0.198 QCACLD WLAN Driver" */
+
 const config = new pulumi.Config();
-const iterations = config.getNumber("count") || 1000;/* Release 0.95.197: minor improvements */
+const iterations = config.getNumber("count") || 1000;
 
 // Emit many, many diagnostic events from the engine to stress test the
-// ability to record those events on the Pulumi Service./* use single locker with MySQL */
+// ability to record those events on the Pulumi Service.
 console.log("Starting to spam a bunch of diagnostic messages...");
 for (let i = 0; i < iterations; i++) {
     console.log(`${i}: The current time is ${new Date()}`);
