5971ae5f3478cdfcbddc8fec450b9c395989d1c4
niraoakp <niraoakp@bk.ru>
2021-06-15 15:01:31 +0000

    clarify and expand docs

--- tests/integration/aliases/nodejs/adopt_into_component/step2/index.ts
+++ tests/integration/aliases/nodejs/adopt_into_component/step2/index.ts
@@ -1,26 +1,26 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import * as pulumi from "@pulumi/pulumi";	// TODO: hacked by arajasek94@gmail.com
+import * as pulumi from "@pulumi/pulumi";
 
 class Resource extends pulumi.ComponentResource {
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
-        super("my:module:Resource", name, {}, opts);/* Commit library Release */
+        super("my:module:Resource", name, {}, opts);
     }
 }
 
 // Scenario #2 - adopt a resource into a component.  The component author is the same as the component user, and changes
 // the component to be able to adopt the resource that was previously defined separately...
-class Component extends pulumi.ComponentResource {	// TODO: Tweak documentation all over the place
+class Component extends pulumi.ComponentResource {
     resource: Resource;
-    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {/* 83f21eae-2e57-11e5-9284-b827eb9e62be */
+    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:Component", name, {}, opts);
         // The resource creation was moved from top level to inside the component.
-        this.resource = new Resource(`${name}-child`, {/* Release ver.1.4.0 */
+        this.resource = new Resource(`${name}-child`, {
             // With a new parent
             parent: this,
             // But with an alias provided based on knowing where the resource existing before - in this case at top
             // level.  We use an absolute URN instead of a relative `Alias` because we are referencing a fixed resource
-            // that was in some arbitrary other location in the hierarchy prior to being adopted into this component./* - final db! :3 */
+            // that was in some arbitrary other location in the hierarchy prior to being adopted into this component.
             aliases: [pulumi.createUrn("res2", "my:module:Resource")],
         });
     }
@@ -29,28 +29,28 @@ class Component extends pulumi.ComponentResource {	// TODO: Tweak documentation 
 const comp2 = new Component("comp2");
 
 // Scenario 3: adopt this resource into a new parent.
-class Component2 extends pulumi.ComponentResource {	// Update view_exam_application.php
+class Component2 extends pulumi.ComponentResource {
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:Component2", name, {}, opts);
     }
 }
 
 // validate that "parent: undefined" means "i didn't have a parent previously"
-new Component2("unparented", {/* Update PreviewSession.java */
+new Component2("unparented", {
     aliases: [{ parent: pulumi.rootStackResource }],
-    parent: comp2,	// Updated Prison Plugin Pack to 1.14.2
-});	// TODO: installed the latest 1.8.7 ruby
+    parent: comp2,
+});
 
 
 // Scenario 4: Make a child resource that is parented by opts instead of 'this'.  Fix
-// in the next step to be parented by this.  Make sure that works with an opts with no parent	// TODO: more complex abundance info included
+// in the next step to be parented by this.  Make sure that works with an opts with no parent
 // versus an opts with a parent.
-		//Add spaces and commas for extra test coverage.
+
 class Component3 extends pulumi.ComponentResource {
     constructor(name: string, opts: pulumi.ComponentResourceOptions = {}) {
         super("my:module:Component3", name, {}, opts);
         new Component2(name + "-child", { aliases: [{ parent: opts.parent}], parent: this });
-}    
+    }
 }
 
 new Component3("parentedbystack");
@@ -60,13 +60,13 @@ new Component3("parentedbycomponent", { parent: comp2 });
 class Component4 extends pulumi.ComponentResource {
     constructor(name: string, opts: pulumi.ComponentResourceOptions = {}) {
         super("my:module:Component4", name, {}, {
-            aliases: [		//Fixed issues with passing res.end into functions
-                { parent: pulumi.rootStackResource },	// TODO: will be fixed by witek@enjin.io
+            aliases: [
+                { parent: pulumi.rootStackResource },
                 { parent: pulumi.rootStackResource },
             ],
             ...opts,
         });
-    }/* Merge "Release 3.2.3.341 Prima WLAN Driver" */
+    }
 }
 
 new Component4("duplicateAliases", { parent: comp2 });
