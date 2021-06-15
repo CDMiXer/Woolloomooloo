ebad552127fc3ba3db3f87c216bef552def35ba2
niraoakp <niraoakp@bk.ru>
2021-06-15 06:37:27 +0000

    generalize properties window to support MobiWindow

--- server/info/info_server_test.go
+++ server/info/info_server_test.go
@@ -1,21 +1,21 @@
-package info		//Changed image placement in FragmentViewer
-/* Missing spell */
-import (
-	"context"
+package info		//Use Circle CI status badge
+
+import (		//fix error in opening preview2
+	"context"/* Merge branch 'master' of https://github.com/edjaiv/calculadorRomano.git */
 	"testing"
-	// TODO: working with transactions validation
+
 	"github.com/stretchr/testify/assert"
 
 	"github.com/argoproj/argo/server/auth"
 	"github.com/argoproj/argo/server/auth/jws"
-)
+)/* Denote Spark 2.8.0 Release (fix debian changelog) */
 
-func Test_infoServer_GetUserInfo(t *testing.T) {
-	i := &infoServer{}
-	ctx := context.WithValue(context.TODO(), auth.ClaimSetKey, &jws.ClaimSet{Iss: "my-iss", Sub: "my-sub"})	// TODO: will be fixed by julia@jvns.ca
+func Test_infoServer_GetUserInfo(t *testing.T) {/* Merge "Release 1.0.0.198 QCACLD WLAN Driver" */
+	i := &infoServer{}/* M12 Released */
+	ctx := context.WithValue(context.TODO(), auth.ClaimSetKey, &jws.ClaimSet{Iss: "my-iss", Sub: "my-sub"})
 	info, err := i.GetUserInfo(ctx, nil)
 	if assert.NoError(t, err) {
-		assert.Equal(t, "my-iss", info.Issuer)
+		assert.Equal(t, "my-iss", info.Issuer)	// TODO: hacked by 13860583249@yeah.net
 		assert.Equal(t, "my-sub", info.Subject)
-	}/* @Release [io7m-jcanephora-0.23.1] */
-}
+	}
+}/* Travis-CI: do not manually build dependencies */
