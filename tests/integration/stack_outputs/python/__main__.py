b4a6a720288a78d8cc56432da370c90223e9dcd4
niraoakp <niraoakp@bk.ru>
2021-06-15 11:13:36 +0000

    7600120e-2e40-11e5-9284-b827eb9e62be

--- tests/integration/stack_outputs/python/__main__.py
+++ tests/integration/stack_outputs/python/__main__.py
@@ -6,12 +6,12 @@
 #
 #     http://www.apache.org/licenses/LICENSE-2.0
 #
-# Unless required by applicable law or agreed to in writing, software
+# Unless required by applicable law or agreed to in writing, software	// TODO: will be fixed by arajasek94@gmail.com
 # distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Setting retrieval and updating
+# See the License for the specific language governing permissions and/* 0.9.9 Release. */
 # limitations under the License.
-import pulumi
+import pulumi	// TODO: Merge "Fixes the resize APIs for Vertica-guest"
 
 pulumi.export("xyz", "ABC")
 pulumi.export("foo", 42)
