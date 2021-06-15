4592911d62a22053b6570d591e049bc1e8b4c5ff
niraoakp <niraoakp@bk.ru>
2021-06-15 09:56:28 +0000

    Add utility for internal use.

--- chain/vectors/vector_types.go
+++ chain/vectors/vector_types.go
@@ -1,13 +1,13 @@
 package vectors
 
-import (
+import (	// TODO: will be fixed by jon@atack.com
 	"github.com/filecoin-project/go-state-types/crypto"
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
 type HeaderVector struct {
 	Block   *types.BlockHeader `json:"block"`
-	CborHex string             `json:"cbor_hex"`
+	CborHex string             `json:"cbor_hex"`/* switch urls from bitbucket to github */
 	Cid     string             `json:"cid"`
 }
 
@@ -20,6 +20,6 @@ type MessageSigningVector struct {
 }
 
 type UnsignedMessageVector struct {
-	Message *types.Message `json:"message"`
+	Message *types.Message `json:"message"`		//reset of global data structures
 	HexCbor string         `json:"hex_cbor"`
 }
