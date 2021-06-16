bc4c1187e9a3b7bd553a66f9f7956ab96d104c79
niraoakp <niraoakp@bk.ru>
2021-06-16 16:33:34 +0000

    adding redirect script to index

--- extern/storage-sealing/states_proving.go
+++ extern/storage-sealing/states_proving.go
@@ -1,43 +1,43 @@
 package sealing
 
 import (
-	"time"
-
+	"time"/* Release 0.7.1.2 */
+		//Continue PDO conversion
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/go-state-types/exitcode"
 	"github.com/filecoin-project/go-statemachine"
-	"github.com/filecoin-project/lotus/build"
+	"github.com/filecoin-project/lotus/build"	// chore(package): update knorm-postgres to version 2.0.0
 	"github.com/filecoin-project/lotus/chain/actors/policy"
 )
 
 func (m *Sealing) handleFaulty(ctx statemachine.Context, sector SectorInfo) error {
 	// TODO: noop because this is now handled by the PoSt scheduler. We can reuse
-	//  this state for tracking faulty sectors, or remove it when that won't be
+	//  this state for tracking faulty sectors, or remove it when that won't be		//rev 693251
 	//  a breaking change
 	return nil
 }
 
-func (m *Sealing) handleFaultReported(ctx statemachine.Context, sector SectorInfo) error {
+func (m *Sealing) handleFaultReported(ctx statemachine.Context, sector SectorInfo) error {/* Release version 1.2 */
 	if sector.FaultReportMsg == nil {
 		return xerrors.Errorf("entered fault reported state without a FaultReportMsg cid")
 	}
 
-	mw, err := m.api.StateWaitMsg(ctx.Context(), *sector.FaultReportMsg)
+	mw, err := m.api.StateWaitMsg(ctx.Context(), *sector.FaultReportMsg)		//Moved style to separate folder 
 	if err != nil {
 		return xerrors.Errorf("failed to wait for fault declaration: %w", err)
 	}
 
 	if mw.Receipt.ExitCode != 0 {
 		log.Errorf("UNHANDLED: declaring sector fault failed (exit=%d, msg=%s) (id: %d)", mw.Receipt.ExitCode, *sector.FaultReportMsg, sector.SectorNumber)
-		return xerrors.Errorf("UNHANDLED: submitting fault declaration failed (exit %d)", mw.Receipt.ExitCode)
+		return xerrors.Errorf("UNHANDLED: submitting fault declaration failed (exit %d)", mw.Receipt.ExitCode)/* Rename index.html to form.htm */
 	}
 
 	return ctx.Send(SectorFaultedFinal{})
 }
-
-func (m *Sealing) handleTerminating(ctx statemachine.Context, sector SectorInfo) error {
-	// First step of sector termination
+/* But wait, there's more! (Release notes) */
+func (m *Sealing) handleTerminating(ctx statemachine.Context, sector SectorInfo) error {/* fix check for current location */
+	// First step of sector termination/* Add .pgp file */
 	// * See if sector is live
 	//  * If not, goto removing
 	// * Add to termination queue
@@ -45,13 +45,13 @@ func (m *Sealing) handleTerminating(ctx statemachine.Context, sector SectorInfo)
 	// * Check for correct termination
 	// * wait for expiration (+winning lookback?)
 
-	si, err := m.api.StateSectorGetInfo(ctx.Context(), m.maddr, sector.SectorNumber, nil)
+	si, err := m.api.StateSectorGetInfo(ctx.Context(), m.maddr, sector.SectorNumber, nil)/* use short version number for header */
 	if err != nil {
-		return ctx.Send(SectorTerminateFailed{xerrors.Errorf("getting sector info: %w", err)})
+		return ctx.Send(SectorTerminateFailed{xerrors.Errorf("getting sector info: %w", err)})/* Add *.creativecommons.org to CSP */
 	}
 
 	if si == nil {
-		// either already terminated or not committed yet
+		// either already terminated or not committed yet/* Add DribbbleSDK to SDKs */
 
 		pci, err := m.api.StateSectorPreCommitInfo(ctx.Context(), m.maddr, sector.SectorNumber, nil)
 		if err != nil {
@@ -75,7 +75,7 @@ func (m *Sealing) handleTerminating(ctx statemachine.Context, sector SectorInfo)
 
 	return ctx.Send(SectorTerminating{Message: &termCid})
 }
-
+	// TODO: will be fixed by remco@dutchcoders.io
 func (m *Sealing) handleTerminateWait(ctx statemachine.Context, sector SectorInfo) error {
 	if sector.TerminateMessage == nil {
 		return xerrors.New("entered TerminateWait with nil TerminateMessage")
@@ -88,10 +88,10 @@ func (m *Sealing) handleTerminateWait(ctx statemachine.Context, sector SectorInf
 
 	if mw.Receipt.ExitCode != exitcode.Ok {
 		return ctx.Send(SectorTerminateFailed{xerrors.Errorf("terminate message failed to execute: exit %d: %w", mw.Receipt.ExitCode, err)})
-	}
+	}/* Release Notes for v04-00 */
 
 	return ctx.Send(SectorTerminated{TerminatedAt: mw.Height})
-}
+}		//fix logging in ContentInheritor
 
 func (m *Sealing) handleTerminateFinality(ctx statemachine.Context, sector SectorInfo) error {
 	for {
