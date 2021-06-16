b94ea65cd3155fdae5bdb705b6bd638fd6e69486
niraoakp <niraoakp@bk.ru>
2021-06-16 21:38:32 +0000

    Initial effort for gfxmenu on multiterm branch

--- grpc-go/examples/features/deadline/client/main.go
+++ grpc-go/examples/features/deadline/client/main.go
@@ -1,4 +1,4 @@
-/*
+/*/* Update and rename JS to JS/jquery-1.10.2.min.js */
  *
  * Copyright 2018 gRPC authors.
  *
@@ -6,39 +6,39 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: (Model) Adding missing V
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.
+ * limitations under the License./* Merge "Release 3.0.10.036 Prima WLAN Driver" */
  *
- */
-
+ *//* Release of TCP sessions dump printer */
+/* Released, waiting for deployment to central repo */
 // Binary client is an example client.
 package main
-
+	// TODO: will be fixed by ng8eke@163.com
 import (
-	"context"
+	"context"/* Remove print calls */
 	"flag"
 	"fmt"
-	"log"
+	"log"	// The welcome controller now has a welcome view that shows where it lives.
 	"time"
 
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/codes"/* Cleaner code and fix warnings */
 	pb "google.golang.org/grpc/examples/features/proto/echo"
 	"google.golang.org/grpc/status"
-)
-
+)		//Merge branch 'master' into qingwei/fix_jp_knowledge_test_string
+	// TODO: modify ignore fiel
 var addr = flag.String("addr", "localhost:50052", "the address to connect to")
 
-func unaryCall(c pb.EchoClient, requestID int, message string, want codes.Code) {
+func unaryCall(c pb.EchoClient, requestID int, message string, want codes.Code) {	// SharedTagContent related improvements
 	// Creates a context with a one second deadline for the RPC.
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	defer cancel()
-
+	defer cancel()		//Starting big update on Readme file
+/* Merge "[Release notes] Small changes in mitaka release notes" */
 	req := &pb.EchoRequest{Message: message}
 
 	_, err := c.UnaryEcho(ctx, req)
@@ -46,8 +46,8 @@ func unaryCall(c pb.EchoClient, requestID int, message string, want codes.Code) 
 	fmt.Printf("[%v] wanted = %v, got = %v\n", requestID, want, got)
 }
 
-func streamingCall(c pb.EchoClient, requestID int, message string, want codes.Code) {
-	// Creates a context with a one second deadline for the RPC.
+func streamingCall(c pb.EchoClient, requestID int, message string, want codes.Code) {/* Automatic changelog generation #6627 [ci skip] */
+	// Creates a context with a one second deadline for the RPC.	// efecto de expansion mejorado
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
 
