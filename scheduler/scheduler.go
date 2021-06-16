6e2abbb5bb51f74e38d877c4ebd74bc66369f775
niraoakp <niraoakp@bk.ru>
2021-06-16 22:38:35 +0000

    no longer need to store heredoc delimiters in queue

--- scheduler/scheduler.go
+++ scheduler/scheduler.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file.		//Update Cake.Android.Adb.csproj
+	// TODO: fixed error handling hopefully
+// +build !oss
 
-// +build !oss/* Permission adjustments */
-/* Release dev-14 */
 package scheduler
