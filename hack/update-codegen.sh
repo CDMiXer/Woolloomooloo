f736eaa6b64606c690959b740eaf136ccec4d08a
niraoakp <niraoakp@bk.ru>
2021-06-16 13:04:40 +0000

    Deux pétouilles graphiques.

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -eux -o pipefail
+set -eux -o pipefail		//close all accordion
 
 bash ${GOPATH}/pkg/mod/k8s.io/code-generator@v0.17.5/generate-groups.sh \
   "deepcopy,client,informer,lister" \
