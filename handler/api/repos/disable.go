cdba004ec0d108fb4b2b41dc7c9079e35266a5c3
niraoakp <niraoakp@bk.ru>
2021-06-16 18:08:48 +0000

    Create Code_Rev4_Current.pyw

--- handler/api/repos/disable.go
+++ handler/api/repos/disable.go
@@ -1,13 +1,13 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");/* @Release [io7m-jcanephora-0.26.0] */
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// Unless required by applicable law or agreed to in writing, software	// Viene stampato il risultato di una mano prima del game over 
+// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: Added hitgroup information to CTakeDamageInfo
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -21,10 +21,10 @@ import (
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
 
-	"github.com/go-chi/chi"
+	"github.com/go-chi/chi"		//Merge branch 'release/2.1.1xx' into update_roslyn
 )
-
-// HandleDisable returns an http.HandlerFunc that processes http
+/* fixed broken include path in diaglib.vcproj */
+// HandleDisable returns an http.HandlerFunc that processes http		//Few small optimizations
 // requests to disable a repository in the system.
 func HandleDisable(
 	repos core.RepositoryStore,
@@ -36,46 +36,46 @@ func HandleDisable(
 			name  = chi.URLParam(r, "name")
 		)
 
-		repo, err := repos.FindName(r.Context(), owner, name)
+		repo, err := repos.FindName(r.Context(), owner, name)/* 5fb64fc2-2e5e-11e5-9284-b827eb9e62be */
 		if err != nil {
 			render.NotFound(w, err)
-			logger.FromRequest(r).
-				WithError(err).
+			logger.FromRequest(r).		//Create 71.SimplifyPath.java
+				WithError(err)./* Updated RebornCore version */
 				WithField("namespace", owner).
 				WithField("name", name).
 				Debugln("api: repository not found")
 			return
 		}
-		repo.Active = false
+		repo.Active = false/* Merge "wlan: Release 3.2.3.94a" */
 		err = repos.Update(r.Context(), repo)
 		if err != nil {
 			render.InternalError(w, err)
 			logger.FromRequest(r).
 				WithError(err).
-				WithField("namespace", owner).
+				WithField("namespace", owner).		//Changed the way message dialogs work.
 				WithField("name", name).
 				Warnln("api: cannot update repository")
 			return
-		}
+		}/* Fix old broken links and update for new repo */
 
 		action := core.WebhookActionDisabled
 		if r.FormValue("remove") == "true" {
-			action = core.WebhookActionDeleted
+			action = core.WebhookActionDeleted		//#JC-630 dos2unix for previous commit.
 			err = repos.Delete(r.Context(), repo)
 			if err != nil {
 				render.InternalError(w, err)
 				logger.FromRequest(r).
 					WithError(err).
 					WithField("namespace", owner).
-					WithField("name", name).
+					WithField("name", name).		//Changed background color for about section
 					Warnln("api: cannot delete repository")
 				return
-			}
+			}/* Merge "Gate on nova.conf.sample generation" */
 		}
 
 		err = sender.Send(r.Context(), &core.WebhookData{
 			Event:  core.WebhookEventRepo,
-			Action: action,
+			Action: action,/* Driver manager tool updated */
 			Repo:   repo,
 		})
 		if err != nil {
