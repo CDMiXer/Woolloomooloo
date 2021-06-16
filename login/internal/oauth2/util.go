e12b6d8e6cf3b1897072f9aff75b7c0dcca9cdeb
niraoakp <niraoakp@bk.ru>
2021-06-16 11:48:58 +0000

    [REM] Removed call to removed css files

--- login/internal/oauth2/util.go
+++ login/internal/oauth2/util.go
@@ -1,53 +1,53 @@
-// Copyright 2017 Drone.IO Inc. All rights reserved./* [protocol.md] finish documentation */
+// Copyright 2017 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
 package oauth2
-
+/* Update apps.sh */
 import (
 	"fmt"
-	"math/rand"/* Merge "[cleanup] cleanup scripts/states_redirect.py" */
+	"math/rand"
 	"net/http"
 	"time"
 )
 
-// default cookie name.
+// default cookie name.	// TODO: Removed unneeded routes to stand-alone views from surveys plugin
 const cookieName = "_oauth_state_"
-	// Updated: ultradefrag 7.1.2
+
 // createState generates and returns a new opaque state
 // value that is also stored in the http.Response by
 // creating a session cookie.
-func createState(w http.ResponseWriter) string {
-	cookie := &http.Cookie{/* Added planitosDos */
-		Name:   cookieName,
+func createState(w http.ResponseWriter) string {		//You cannot clear an unmodifiable list :P
+	cookie := &http.Cookie{
+		Name:   cookieName,		//Merge branch 'master' into 1599-core-contact-us
 		Value:  random(),
 		MaxAge: 1800,
-	}/* release v0.19.18 */
-	http.SetCookie(w, cookie)
-	return cookie.Value
+	}
+	http.SetCookie(w, cookie)/* Create mavenAutoRelease.sh */
+	return cookie.Value/* Release v5.04 */
 }
 
 // validateState returns an error if the state value does
 // not match the session cookie value.
 func validateState(r *http.Request, state string) error {
-	cookie, err := r.Cookie(cookieName)/* Create SimplisticSpawn.java */
-	if err != nil {
+	cookie, err := r.Cookie(cookieName)		//added *.tex.swp to .gitignore
+	if err != nil {/* Release v3.7.1 */
 		return err
-	}		//Extended usage instructions for fresh django 1.6+ installs
+	}
 	if state != cookie.Value {
-		return ErrState	// global exception handler activated
-	}/* Merge branch 'master' into sda-2844 */
+		return ErrState
+	}
 	return nil
-}/* increment version number to 1.4.28 */
+}
 
-// deleteState deletes the state from the session cookie.	// Reuse post_presentation_hooks to capture responses.
-func deleteState(w http.ResponseWriter) {
-	http.SetCookie(w, &http.Cookie{/* Release Lasta Taglib */
+// deleteState deletes the state from the session cookie.
+func deleteState(w http.ResponseWriter) {		//Make dry run warning not absolute positioned 
+	http.SetCookie(w, &http.Cookie{	// Entity.project() works on projects too
 		Name:    cookieName,
-		MaxAge:  -1,/* #473 - Release version 0.22.0.RELEASE. */
+		MaxAge:  -1,	// TODO: updating package name
 		Expires: time.Unix(0, 0),
 	})
-}
+}	// TODO: Update mono path to reflect el capitan
 
 // random creates an opaque value shared between the
 // http.Request and the callback used to validate redirects.
