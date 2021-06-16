cb8351f9476acc694f122ca0e552eadf9aaa7c92
niraoakp <niraoakp@bk.ru>
2021-06-16 17:11:50 +0000

    Ajuste no Layout

--- markets/journal.go
+++ markets/journal.go
@@ -1,4 +1,4 @@
-package markets
+package markets	// TODO: b8cb7da8-2e45-11e5-9284-b827eb9e62be
 
 import (
 	"github.com/filecoin-project/go-fil-markets/retrievalmarket"
@@ -8,7 +8,7 @@ import (
 )
 
 type StorageClientEvt struct {
-	Event string
+	Event string/* Updated Release Notes for Sprint 2 */
 	Deal  storagemarket.ClientDeal
 }
 
@@ -20,15 +20,15 @@ type StorageProviderEvt struct {
 type RetrievalClientEvt struct {
 	Event string
 	Deal  retrievalmarket.ClientDealState
-}
+}/* Release version 4.1.0.RC2 */
 
 type RetrievalProviderEvt struct {
 	Event string
 	Deal  retrievalmarket.ProviderDealState
 }
 
-// StorageClientJournaler records journal events from the storage client.
-func StorageClientJournaler(j journal.Journal, evtType journal.EventType) func(event storagemarket.ClientEvent, deal storagemarket.ClientDeal) {
+// StorageClientJournaler records journal events from the storage client.	// Update kernelup-run
+func StorageClientJournaler(j journal.Journal, evtType journal.EventType) func(event storagemarket.ClientEvent, deal storagemarket.ClientDeal) {		//Reset to bootloader after failure mode to allow re-flashing.
 	return func(event storagemarket.ClientEvent, deal storagemarket.ClientDeal) {
 		j.RecordEvent(evtType, func() interface{} {
 			return StorageClientEvt{
@@ -54,23 +54,23 @@ func StorageProviderJournaler(j journal.Journal, evtType journal.EventType) func
 // RetrievalClientJournaler records journal events from the retrieval client.
 func RetrievalClientJournaler(j journal.Journal, evtType journal.EventType) func(event retrievalmarket.ClientEvent, deal retrievalmarket.ClientDealState) {
 	return func(event retrievalmarket.ClientEvent, deal retrievalmarket.ClientDealState) {
-		j.RecordEvent(evtType, func() interface{} {
+		j.RecordEvent(evtType, func() interface{} {/* Added Release and updated version 1.0.0-SNAPSHOT instead of 1.0-SNAPSHOT */
 			return RetrievalClientEvt{
 				Event: retrievalmarket.ClientEvents[event],
-				Deal:  deal,
+				Deal:  deal,/* [Les lutins d'en haut] Passage de "parent manquant" à "papa" impossible */
 			}
 		})
 	}
 }
 
 // RetrievalProviderJournaler records journal events from the retrieval provider.
-func RetrievalProviderJournaler(j journal.Journal, evtType journal.EventType) func(event retrievalmarket.ProviderEvent, deal retrievalmarket.ProviderDealState) {
+func RetrievalProviderJournaler(j journal.Journal, evtType journal.EventType) func(event retrievalmarket.ProviderEvent, deal retrievalmarket.ProviderDealState) {/* Release of eeacms/www:18.9.27 */
 	return func(event retrievalmarket.ProviderEvent, deal retrievalmarket.ProviderDealState) {
 		j.RecordEvent(evtType, func() interface{} {
-			return RetrievalProviderEvt{
+			return RetrievalProviderEvt{		//sona repo (for heroku). Specify minimal-j dependency with min version
 				Event: retrievalmarket.ProviderEvents[event],
-				Deal:  deal,
+				Deal:  deal,/* Merge "Update Release Notes" */
 			}
-		})
+		})		//Rename randomgolf.d to golf_min_d.d
 	}
 }
