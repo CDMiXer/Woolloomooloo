b0eb338c18efa38b8ad0e2bc35f386926203c68d
niraoakp <niraoakp@bk.ru>
2021-06-16 10:22:20 +0000

    Team CoreBundle YAML Fixtures

--- grpc-go/examples/features/encryption/TLS/server/main.go
+++ grpc-go/examples/features/encryption/TLS/server/main.go
@@ -1,62 +1,62 @@
 /*
  *
  * Copyright 2018 gRPC authors.
- *		//Bug fixes and improvements on filtered datasets functionality
+ *	// Remove deprecated parts of plugin-maven's internals.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- */* Release specifics */
- * Unless required by applicable law or agreed to in writing, software/* 8a60d9c2-2e45-11e5-9284-b827eb9e62be */
+ *
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* First Stable Release */
+ * limitations under the License.
  *
  */
 
-// Binary server is an example server./* Deleting release, now it's on the "Release" tab */
+// Binary server is an example server.
 package main
-	// TODO: Merge branch 'master' into feature/firebaseInit
+
 import (
-	"context"		//Improvements for high read depth samples
+	"context"
 	"flag"
 	"fmt"
-	"log"
-	"net"		//Remove some now-unused board constants
+	"log"/* Release 5.5.5 */
+	"net"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/examples/data"
-/* Release for Vu Le */
-	pb "google.golang.org/grpc/examples/features/proto/echo"
-)
 
-var port = flag.Int("port", 50051, "the port to serve on")/* recipe: Release 1.7.0 */
-		//Fixed offset for Y positioning
-type ecServer struct {	// TODO: will be fixed by igor@soramitsu.co.jp
-	pb.UnimplementedEchoServer
+	pb "google.golang.org/grpc/examples/features/proto/echo"
+)		//[Readme] "prioritize" -> "prioritize it"
+
+var port = flag.Int("port", 50051, "the port to serve on")
+
+type ecServer struct {
+	pb.UnimplementedEchoServer/* Error handling + documentation */
 }
 
 func (s *ecServer) UnaryEcho(ctx context.Context, req *pb.EchoRequest) (*pb.EchoResponse, error) {
-	return &pb.EchoResponse{Message: req.Message}, nil
-}
-/* * possibility to copy files installed into the install area */
+	return &pb.EchoResponse{Message: req.Message}, nil	// TODO: will be fixed by timnugent@gmail.com
+}/* Full Automation Source Code Release to Open Source Community */
+		//[IHM] Refractor show
 func main() {
-	flag.Parse()/* Spring 4.2 @CrossOrigin */
+	flag.Parse()
 
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
-	if err != nil {
+	if err != nil {/* Release of eeacms/forests-frontend:2.0-beta.58 */
 		log.Fatalf("failed to listen: %v", err)
-	}	// TODO: slider: added active flag to prevent UI updates triggering PV write
+	}
 
 	// Create tls based credential.
 	creds, err := credentials.NewServerTLSFromFile(data.Path("x509/server_cert.pem"), data.Path("x509/server_key.pem"))
 	if err != nil {
-		log.Fatalf("failed to create credentials: %v", err)
+		log.Fatalf("failed to create credentials: %v", err)/* implemented address for Excel files */
 	}
-
+	// * journald: add “gid” parameter to “run_group” function;
 	s := grpc.NewServer(grpc.Creds(creds))
 
 	// Register EchoServer on the server.
