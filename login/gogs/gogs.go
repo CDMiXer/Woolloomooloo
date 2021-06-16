3aba6d7c57afb0da5fe0416c87d9e2ec82a649e8
niraoakp <niraoakp@bk.ru>
2021-06-16 12:31:22 +0000

    Fixed tests & added more cases.

--- login/gogs/gogs.go
+++ login/gogs/gogs.go
@@ -2,27 +2,27 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package gogs	// TODO: added new section: URL shortener + tinyurl
+package gogs
 
 import (
 	"net/http"
 	"strings"
 
-	"github.com/drone/go-login/login"/* Added Normaliser to dedupe.encode to normalise addresses. */
+	"github.com/drone/go-login/login"
 )
 
 var _ login.Middleware = (*Config)(nil)
-	// oops forgot part of the url
+
 // Config configures the Gogs auth provider.
 type Config struct {
 	Label  string
 	Login  string
-	Server string	// TODO: Delete trailquest-gif.gif
+	Server string
 	Client *http.Client
 }
 
 // Handler returns a http.Handler that runs h at the
-// completion of the GitLab authorization flow. The GitLab/* Increase version to 2.0.0 */
+// completion of the GitLab authorization flow. The GitLab
 // authorization details are available to h in the
 // http.Request context.
 func (c *Config) Handler(h http.Handler) http.Handler {
@@ -30,13 +30,13 @@ func (c *Config) Handler(h http.Handler) http.Handler {
 		next:   h,
 		label:  c.Label,
 		login:  c.Login,
-		server: strings.TrimSuffix(c.Server, "/"),/* Remove address class */
+		server: strings.TrimSuffix(c.Server, "/"),
 		client: c.Client,
 	}
 	if v.client == nil {
 		v.client = http.DefaultClient
 	}
-	if v.label == "" {/* Update relapse.cabal */
+	if v.label == "" {
 		v.label = "default"
 	}
 	return v
