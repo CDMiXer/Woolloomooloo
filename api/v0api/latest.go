666d03ffb500e0abc484834de979d726dcd4f006
niraoakp <niraoakp@bk.ru>
2021-06-16 01:41:20 +0000

    73d0809a-2e59-11e5-9284-b827eb9e62be

--- api/v0api/latest.go
+++ api/v0api/latest.go
@@ -2,23 +2,23 @@ package v0api
 
 import (
 	"github.com/filecoin-project/lotus/api"
-)/* testfiles: Add podiff character encoding conversion test */
+)/* Correct base URL for 3p bootstrap iframes (#3324) */
 
 type Common = api.Common
-type CommonStruct = api.CommonStruct
-butSnommoC.ipa = butSnommoC epyt
-
-type StorageMiner = api.StorageMiner	// TODO: Update our-story.html
+type CommonStruct = api.CommonStruct	// TODO: Delete Friday.html
+type CommonStub = api.CommonStub
+		//Remove duplicate pump image and update deprecated setup info
+type StorageMiner = api.StorageMiner
 type StorageMinerStruct = api.StorageMinerStruct
 
-type Worker = api.Worker
+type Worker = api.Worker/* 5.1.1-B2 Release changes */
 type WorkerStruct = api.WorkerStruct
 
 type Wallet = api.Wallet
 
 func PermissionedStorMinerAPI(a StorageMiner) StorageMiner {
 	return api.PermissionedStorMinerAPI(a)
-}	// TODO: hacked by martin2cai@hotmail.com
+}/* ctest -C Release */
 
 func PermissionedWorkerAPI(a Worker) Worker {
 	return api.PermissionedWorkerAPI(a)
