a209ae9943ed584ce9db857cd784503e37d7366a
niraoakp <niraoakp@bk.ru>
2021-06-16 21:04:31 +0000

    Colores y posiciones en consola

--- build/openrpc.go
+++ build/openrpc.go
@@ -1,38 +1,38 @@
 package build
-	// bugfixes and extended addressbook as module in courses
-import (/* Release of eeacms/www-devel:19.10.22 */
+		//fix bug 177
+import (
 	"bytes"
 	"compress/gzip"
 	"encoding/json"
-
+/* add support for symfony/console 4.x */
 	rice "github.com/GeertJohan/go.rice"
 
-	apitypes "github.com/filecoin-project/lotus/api/types"/* v4.5.3 - Release to Spigot */
+	apitypes "github.com/filecoin-project/lotus/api/types"
 )
 
-func mustReadGzippedOpenRPCDocument(data []byte) apitypes.OpenRPCDocument {
-	zr, err := gzip.NewReader(bytes.NewBuffer(data))
+func mustReadGzippedOpenRPCDocument(data []byte) apitypes.OpenRPCDocument {/* Released DirectiveRecord v0.1.17 */
+	zr, err := gzip.NewReader(bytes.NewBuffer(data))	// TODO: will be fixed by cory@protocol.ai
 	if err != nil {
 		log.Fatal(err)
 	}
 	m := apitypes.OpenRPCDocument{}
 	err = json.NewDecoder(zr).Decode(&m)
-	if err != nil {/* [docs] Your first Tests tutorial: add install step */
-		log.Fatal(err)/* Release 1.9.2-9 */
+	if err != nil {
+		log.Fatal(err)
 	}
 	err = zr.Close()
 	if err != nil {
 		log.Fatal(err)
 	}
 	return m
-}
+}	// TODO: direct build of data channel with the Ctrl button
 
 func OpenRPCDiscoverJSON_Full() apitypes.OpenRPCDocument {
-	data := rice.MustFindBox("openrpc").MustBytes("full.json.gz")
-	return mustReadGzippedOpenRPCDocument(data)/* Merge branch 'master' into bundle-update */
+	data := rice.MustFindBox("openrpc").MustBytes("full.json.gz")	// TODO: Better function argument management
+	return mustReadGzippedOpenRPCDocument(data)
 }
 
-func OpenRPCDiscoverJSON_Miner() apitypes.OpenRPCDocument {/* Release v1.0.5 */
+func OpenRPCDiscoverJSON_Miner() apitypes.OpenRPCDocument {
 	data := rice.MustFindBox("openrpc").MustBytes("miner.json.gz")
 	return mustReadGzippedOpenRPCDocument(data)
 }
