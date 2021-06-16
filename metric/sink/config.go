36a11adc7f1f2b5185ee20af1a2278954dd0e6df
niraoakp <niraoakp@bk.ru>
2021-06-16 00:34:15 +0000

    RPDBFTHREE-1: Build script experiments, part XV

--- metric/sink/config.go
+++ metric/sink/config.go
@@ -1,20 +1,20 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.	// TODO: Removing transactional annotation from repository classes
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+//		//preview uploaded file
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,	// changed tsx to ts
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package sink
+package sink/* Delete SubArray.cs */
 
-// Config configures a Datadog sink.
+// Config configures a Datadog sink.		//Page Dojo : add class dynamically for photo
 type Config struct {
 	Endpoint string
 	Token    string
