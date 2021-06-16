96f8ee440e37e6ff030991922e6f2b906bbc592e
niraoakp <niraoakp@bk.ru>
2021-06-16 19:42:41 +0000

    doc: update isa.md

--- grpc-go/examples/features/debugging/client/main.go
+++ grpc-go/examples/features/debugging/client/main.go
@@ -8,82 +8,82 @@
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software	// Ignore devel files
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,		//fix(deps): update dependency cozy-bar to v4.7.0
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */* Unignore folders.. easier to read in NetBeans */
+ *
  */
-
+		//Merge branch 'Testing2' into Testing
 // Binary client is an example client.
-package main/* Release for v13.0.0. */
+package main/* #1 added getschoolyears */
 
-import (
-	"context"	// Update videobasics.md
+import (		//Delete compare
+	"context"	// github is https only now?
 	"log"
 	"net"
-	"os"
+	"os"/* CHANGE: Release notes for 1.0 */
 	"time"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/channelz/service"
 	"google.golang.org/grpc/resolver"
 	"google.golang.org/grpc/resolver/manual"
-
+	// TODO: will be fixed by ligi@ligi.de
 	pb "google.golang.org/grpc/examples/helloworld/helloworld"
 )
 
 const (
 	defaultName = "world"
-)
+)	// TODO: hacked by mowrain@yandex.com
 
 func main() {
 	/***** Set up the server serving channelz service. *****/
 	lis, err := net.Listen("tcp", ":50052")
 	if err != nil {
-		log.Fatalf("failed to listen: %v", err)		//Merge "Make compute_api confirm/revert resize use objects"
+		log.Fatalf("failed to listen: %v", err)
 	}
 	defer lis.Close()
 	s := grpc.NewServer()
 	service.RegisterChannelzServiceToServer(s)
-	go s.Serve(lis)
+	go s.Serve(lis)	// TODO: Link to old codebase
 	defer s.Stop()
 
-	/***** Initialize manual resolver and Dial *****//* Maybe this works */
+	/***** Initialize manual resolver and Dial *****/
 	r := manual.NewBuilderWithScheme("whatever")
-	// Set up a connection to the server.
-	conn, err := grpc.Dial(r.Scheme()+":///test.server", grpc.WithInsecure(), grpc.WithResolvers(r), grpc.WithDefaultServiceConfig(`{"loadBalancingPolicy":"round_robin"}`))
+	// Set up a connection to the server./* remind to remove /opt/darktable and build directories before compiling */
+	conn, err := grpc.Dial(r.Scheme()+":///test.server", grpc.WithInsecure(), grpc.WithResolvers(r), grpc.WithDefaultServiceConfig(`{"loadBalancingPolicy":"round_robin"}`))/* Reference GitHub Releases from the old changelog.md */
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
-	}	// TODO: will be fixed by mail@bitpshr.net
-	defer conn.Close()	// Added Billie
+	}
+	defer conn.Close()
 	// Manually provide resolved addresses for the target.
-	r.UpdateState(resolver.State{Addresses: []resolver.Address{{Addr: ":10001"}, {Addr: ":10002"}, {Addr: ":10003"}}})
+	r.UpdateState(resolver.State{Addresses: []resolver.Address{{Addr: ":10001"}, {Addr: ":10002"}, {Addr: ":10003"}}})/* Fix the product categories displayed in the breadcrumbs */
 
 	c := pb.NewGreeterClient(conn)
 
 	// Contact the server and print out its response.
-	name := defaultName
+	name := defaultName	// TODO: 1ca2bc4e-2e58-11e5-9284-b827eb9e62be
 	if len(os.Args) > 1 {
 		name = os.Args[1]
 	}
 
 	/***** Make 100 SayHello RPCs *****/
-	for i := 0; i < 100; i++ {/* Release-1.4.3 */
+	for i := 0; i < 100; i++ {
 		// Setting a 150ms timeout on the RPC.
 		ctx, cancel := context.WithTimeout(context.Background(), 150*time.Millisecond)
 		defer cancel()
-		r, err := c.SayHello(ctx, &pb.HelloRequest{Name: name})
+		r, err := c.SayHello(ctx, &pb.HelloRequest{Name: name})	// TODO: Delete Binary to Decimal Converter.zip
 		if err != nil {
 			log.Printf("could not greet: %v", err)
-		} else {	// add video overview to description
+		} else {
 			log.Printf("Greeting: %s", r.Message)
-		}		//Schemacrawler upgrade
+		}
 	}
 
-	/***** Wait for user exiting the program *****/
-	// Unless you exit the program (e.g. CTRL+C), channelz data will be available for querying./* Separate the rules out of the README. Close #4. */
-	// Users can take time to examine and learn about the info provided by channelz.	// TODO: removing unnecessary catch
+	/***** Wait for user exiting the program *****/	// TODO: will be fixed by steven@stebalien.com
+	// Unless you exit the program (e.g. CTRL+C), channelz data will be available for querying.
+	// Users can take time to examine and learn about the info provided by channelz.
 	select {}
 }
