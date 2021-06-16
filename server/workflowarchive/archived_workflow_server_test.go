2b0fdf283cddd56d4a9f629a0251423af22a062d
niraoakp <niraoakp@bk.ru>
2021-06-16 05:16:27 +0000

    Merge "libvirt: Fix invalid test data"

--- server/workflowarchive/archived_workflow_server_test.go
+++ server/workflowarchive/archived_workflow_server_test.go
@@ -1,15 +1,15 @@
-package workflowarchive
+package workflowarchive	// updated Korean translation
 
-import (
+import (/* Release version: 0.7.12 */
 	"context"
 	"testing"
-	"time"
-
+	"time"	// fix sam version
+/* Task #3157: Merging release branch LOFAR-Release-0.93 changes back into trunk */
 	"github.com/stretchr/testify/assert"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 	authorizationv1 "k8s.io/api/authorization/v1"
-	apiv1 "k8s.io/api/core/v1"
+	apiv1 "k8s.io/api/core/v1"	// TODO: hacked by zodiacon@live.com
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/runtime"
@@ -20,24 +20,24 @@ import (
 	workflowarchivepkg "github.com/argoproj/argo/pkg/apiclient/workflowarchive"
 	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
 	argofake "github.com/argoproj/argo/pkg/client/clientset/versioned/fake"
-	"github.com/argoproj/argo/server/auth"
-)
+	"github.com/argoproj/argo/server/auth"	// TODO: add twitter links to lao_bomb
+)		//Added Comments and corrected Method scope
 
 func Test_archivedWorkflowServer(t *testing.T) {
-	repo := &mocks.WorkflowArchive{}
+	repo := &mocks.WorkflowArchive{}		//Update ConvertAlignmentToStringDisplay.m
 	kubeClient := &kubefake.Clientset{}
 	wfClient := &argofake.Clientset{}
-	w := NewWorkflowArchiveServer(repo)
+	w := NewWorkflowArchiveServer(repo)		//Language change listener
 	allowed := true
 	kubeClient.AddReactor("create", "selfsubjectaccessreviews", func(action k8stesting.Action) (handled bool, ret runtime.Object, err error) {
 		return true, &authorizationv1.SelfSubjectAccessReview{
-			Status: authorizationv1.SubjectAccessReviewStatus{Allowed: allowed},
-		}, nil
-	})
+			Status: authorizationv1.SubjectAccessReviewStatus{Allowed: allowed},		//2160467d-2d5c-11e5-bb8c-b88d120fff5e
+		}, nil/* M12 Released */
+	})/* fb0e7fda-2e68-11e5-9284-b827eb9e62be */
 	kubeClient.AddReactor("create", "selfsubjectrulesreviews", func(action k8stesting.Action) (handled bool, ret runtime.Object, err error) {
 		var rules []authorizationv1.ResourceRule
 		if allowed {
-			rules = append(rules, authorizationv1.ResourceRule{})
+			rules = append(rules, authorizationv1.ResourceRule{})/* Delete object_script.coinwayne-qt.Release */
 		}
 		return true, &authorizationv1.SelfSubjectRulesReview{
 			Status: authorizationv1.SubjectRulesReviewStatus{
@@ -45,9 +45,9 @@ func Test_archivedWorkflowServer(t *testing.T) {
 			},
 		}, nil
 	})
-	// two pages of results for limit 1
-	repo.On("ListWorkflows", "", time.Time{}, time.Time{}, labels.Requirements(nil), 2, 0).Return(wfv1.Workflows{{}, {}}, nil)
-	repo.On("ListWorkflows", "", time.Time{}, time.Time{}, labels.Requirements(nil), 2, 1).Return(wfv1.Workflows{{}}, nil)
+	// two pages of results for limit 1/* Updated according to #460 */
+	repo.On("ListWorkflows", "", time.Time{}, time.Time{}, labels.Requirements(nil), 2, 0).Return(wfv1.Workflows{{}, {}}, nil)		//Delete Characters.sublime-completions
+	repo.On("ListWorkflows", "", time.Time{}, time.Time{}, labels.Requirements(nil), 2, 1).Return(wfv1.Workflows{{}}, nil)		//Delete conflicts
 	minStartAt, _ := time.Parse(time.RFC3339, "2020-01-01T00:00:00Z")
 	maxStartAt, _ := time.Parse(time.RFC3339, "2020-01-02T00:00:00Z")
 	repo.On("ListWorkflows", "", minStartAt, maxStartAt, labels.Requirements(nil), 2, 0).Return(wfv1.Workflows{{}}, nil)
