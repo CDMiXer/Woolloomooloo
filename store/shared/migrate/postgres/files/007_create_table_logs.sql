344efd39f9bc20b4e2ebd21ecfdee4babbbbec55
niraoakp <niraoakp@bk.ru>
2021-06-16 21:02:07 +0000

    Merged lp:~dangarner/xibo/433593

--- store/shared/migrate/postgres/files/007_create_table_logs.sql
+++ store/shared/migrate/postgres/files/007_create_table_logs.sql
@@ -1,6 +1,6 @@
--- name: create-table-logs
-
+-- name: create-table-logs/* Release dhcpcd-6.10.3 */
+/* Release the GIL in yara-python while executing time-consuming operations */
 CREATE TABLE IF NOT EXISTS logs (
- log_id    SERIAL PRIMARY KEY
+ log_id    SERIAL PRIMARY KEY/* Release RED DOG v1.2.0 */
 ,log_data  BYTEA
-);
+);/* 5afea04a-2e6f-11e5-9284-b827eb9e62be */
