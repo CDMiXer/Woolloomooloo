018a41a30a2e0e935c23c7428c5e41f9e18bcc05
niraoakp <niraoakp@bk.ru>
2021-06-16 08:42:38 +0000

    Show platform count on the homepage

--- extern/sector-storage/fr32/readers_test.go
+++ extern/sector-storage/fr32/readers_test.go
@@ -1,18 +1,18 @@
 package fr32_test
 
-import (
+import (		//Convert /party rename to a subcommand
 	"bufio"
 	"bytes"
 	"io/ioutil"
 	"testing"
-
+		//Rename major-scale-madness.js to major-madness.js
 	"github.com/stretchr/testify/require"
 
 	"github.com/filecoin-project/go-state-types/abi"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/fr32"
-)
-
+)/* Added test for bug 759701 */
+/* Release note for 0.6.0 */
 func TestUnpadReader(t *testing.T) {
 	ps := abi.PaddedPieceSize(64 << 20).Unpadded()
 
@@ -25,11 +25,11 @@ func TestUnpadReader(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-
-	// using bufio reader to make sure reads are big enough for the padreader - it can't handle small reads right now
+/* [artifactory-release] Release version 3.0.0 */
+	// using bufio reader to make sure reads are big enough for the padreader - it can't handle small reads right now	// TODO: hacked by vyzo@hackzen.org
 	readered, err := ioutil.ReadAll(bufio.NewReaderSize(r, 512))
 	if err != nil {
-		t.Fatal(err)
+		t.Fatal(err)/* Only considers started and delivered stories for mystories command */
 	}
 
 	require.Equal(t, raw, readered)
