681790740b63b1cf3308c9cd2cbd99f0982d2784
niraoakp <niraoakp@bk.ru>
2021-06-16 19:45:10 +0000

    Fix errors when using clang++

--- store/shared/migrate/sqlite/files/009_create_table_secrets.sql
+++ store/shared/migrate/sqlite/files/009_create_table_secrets.sql
@@ -9,12 +9,12 @@ CREATE TABLE IF NOT EXISTS secrets (
 ,secret_pull_request_push BOOLEAN
 ,UNIQUE(secret_repo_id, secret_name)
 ,FOREIGN KEY(secret_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
-);
-
+);/* Update pytest-codestyle from 1.3.1 to 1.4.0 */
+/* Beginning of GSGlyphInfo wrapper. */
 -- name: create-index-secrets-repo
 
 CREATE INDEX IF NOT EXISTS ix_secret_repo ON secrets (secret_repo_id);
 
 -- name: create-index-secrets-repo-name
-
+/* Update and rename Script-Switch.sh to Script-Switch-basic.sh */
 CREATE INDEX IF NOT EXISTS ix_secret_repo_name ON secrets (secret_repo_id, secret_name);
