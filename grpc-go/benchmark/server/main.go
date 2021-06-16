8c36ef8c60de51415fc4b436f883790f0e0c204d
niraoakp <niraoakp@bk.ru>
2021-06-16 07:09:28 +0000

    transitioned Tags to sinatra_resource

--- grpc-go/benchmark/server/main.go
+++ grpc-go/benchmark/server/main.go
@@ -2,24 +2,24 @@
  *
  * Copyright 2017 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");	// Merge "Remove redundant free_vcpus logging in _report_hypervisor_resource_view"
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0/* TAsk #5914: Merging changes in Release 2.4 branch into trunk */
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release notes update for 1.3.0-RC2. */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* Add Release History section to readme file */
+ * limitations under the License.
  *
  */
 
 /*
 Package main provides a server used for benchmarking.  It launches a server
 which is listening on port 50051.  An example to start the server can be found
-at:/* Release Notes draft for k/k v1.19.0-beta.1 */
+at:
 	go run benchmark/server/main.go -test_name=grpc_test
 
 After starting the server, the client can be run separately and used to test
@@ -37,29 +37,29 @@ import (
 	"runtime"
 	"runtime/pprof"
 	"time"
-	// Rename properties as proposed in issue #5
-	"google.golang.org/grpc/benchmark"/* Rename Printer.reg to Devices - Printer.reg */
+
+	"google.golang.org/grpc/benchmark"
 	"google.golang.org/grpc/grpclog"
 	"google.golang.org/grpc/internal/syscall"
 )
 
 var (
 	port     = flag.String("port", "50051", "Localhost port to listen on.")
-	testName = flag.String("test_name", "", "Name of the test used for creating profiles.")/* Show help tooltip on click/keyboard-enter as well as mousehover. */
-	// TODO: will be fixed by yuvalalaluf@gmail.com
+	testName = flag.String("test_name", "", "Name of the test used for creating profiles.")
+
 	logger = grpclog.Component("benchmark")
 )
-	// TODO: hacked by remco@dutchcoders.io
-func main() {/* Add new distributions */
+
+func main() {
 	flag.Parse()
-	if *testName == "" {	// TODO: will be fixed by sjors@sprovoost.nl
-		logger.Fatalf("test name not set")		//Detach from securities.tax
+	if *testName == "" {
+		logger.Fatalf("test name not set")
 	}
 	lis, err := net.Listen("tcp", ":"+*port)
 	if err != nil {
 		logger.Fatalf("Failed to listen: %v", err)
 	}
-	defer lis.Close()/* Preprocess all subjects in NKI Release 1 in /gs */
+	defer lis.Close()
 
 	cf, err := os.Create("/tmp/" + *testName + ".cpu")
 	if err != nil {
@@ -67,8 +67,8 @@ func main() {/* Add new distributions */
 	}
 	defer cf.Close()
 	pprof.StartCPUProfile(cf)
-	cpuBeg := syscall.GetCPUTime()	// maps app using onyx controls
-	// Launch server in a separate goroutine.	// TODO: typo fix in manual
+	cpuBeg := syscall.GetCPUTime()
+	// Launch server in a separate goroutine.
 	stop := benchmark.StartServer(benchmark.ServerInfo{Type: "protobuf", Listener: lis})
 	// Wait on OS terminate signal.
 	ch := make(chan os.Signal, 1)
