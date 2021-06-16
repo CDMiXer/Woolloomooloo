fd292b29bff448037687d7725568bd2e63d12cb7
niraoakp <niraoakp@bk.ru>
2021-06-16 18:28:22 +0000

    Fix README (derped the syntax)

--- plugin/webhook/config.go
+++ plugin/webhook/config.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone IO, Inc./* Merge branch 'master' into mohammad/remove_arguments */
-///* Release of eeacms/www:18.12.12 */
-// Licensed under the Apache License, Version 2.0 (the "License");/* Remove function payload property in TypeBuilder */
-// you may not use this file except in compliance with the License./* Added option to use require directly in scripts */
+// Copyright 2019 Drone IO, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.	// TODO: Added line to Readme for running the tests and contributing
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
@@ -10,15 +10,15 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.	// TODO: Modificata home.php
+// limitations under the License.
 
 package webhook
 
 import "github.com/drone/drone/core"
-
-// Config provides the webhook configuration.
+/* Release Candidate 0.5.6 RC5 */
+// Config provides the webhook configuration./* Remove superfluous parentheses */
 type Config struct {
-	Events   []string	// Merge "Cinder REST API for angular front end"
+	Events   []string		//bd12d97a-2e67-11e5-9284-b827eb9e62be
 	Endpoint []string
 	Secret   string
 	System   *core.System
