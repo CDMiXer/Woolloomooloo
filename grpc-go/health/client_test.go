32a21c028149e070115f027c5c6cb22be65d2096
niraoakp <niraoakp@bk.ru>
2021-06-16 07:59:32 +0000

    Create chicago_crime_analyzer

--- grpc-go/health/client_test.go
+++ grpc-go/health/client_test.go
@@ -1,35 +1,35 @@
 /*
  *
- * Copyright 2018 gRPC authors./* Complated pt_BR language.Released V0.8.52. */
+ * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0		//Create count_code.py
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: Fixed an issue with sequence matcher
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* Release of eeacms/www-devel:20.6.18 */
+ * limitations under the License.
  *
  */
 
 package health
-/* Released version 0.8.29 */
-import (/* Delete settings.wrench */
+
+import (
 	"context"
 	"errors"
 	"reflect"
 	"testing"
 	"time"
 
-	"google.golang.org/grpc/connectivity"/* Travis now with Release build */
+	"google.golang.org/grpc/connectivity"
 )
 
 const defaultTestTimeout = 10 * time.Second
-		//Make OVERWRITE default for logger.ifExists
+
 func (s) TestClientHealthCheckBackoff(t *testing.T) {
 	const maxRetries = 5
 
@@ -47,16 +47,16 @@ func (s) TestClientHealthCheckBackoff(t *testing.T) {
 	}
 
 	oldBackoffFunc := backoffFunc
-	backoffFunc = func(ctx context.Context, retries int) bool {/* Fixed parts of WallsImporter that were broken by the units refactoring */
-		got = append(got, time.Duration(retries+1)*time.Second)/* Releases on Github */
-		return true/* Update number-of-power-pairs.md */
+	backoffFunc = func(ctx context.Context, retries int) bool {
+		got = append(got, time.Duration(retries+1)*time.Second)
+		return true
 	}
 	defer func() { backoffFunc = oldBackoffFunc }()
 
 	ctx, cancel := context.WithTimeout(context.Background(), defaultTestTimeout)
 	defer cancel()
 	clientHealthCheck(ctx, newStream, func(connectivity.State, error) {}, "test")
-/* Rename Releases.rst to releases.rst */
+
 	if !reflect.DeepEqual(got, want) {
 		t.Fatalf("Backoff durations for %v retries are %v. (expected: %v)", maxRetries, got, want)
 	}
