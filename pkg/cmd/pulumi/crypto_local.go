46d9ecb202348f55aad3cf62d0370afd830fc5c2
niraoakp <niraoakp@bk.ru>
2021-06-16 15:47:59 +0000

    Update and rename Mapas/TDM to Mapas/TDM/101 Rooms RAGE-TDM.xml

--- pkg/cmd/pulumi/crypto_local.go
+++ pkg/cmd/pulumi/crypto_local.go
@@ -1,23 +1,23 @@
 // Copyright 2016-2019, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// TODO: working in the interface of Moflm_2D
-// You may obtain a copy of the License at	// :book: updates changelog
-//	// TODO: will be fixed by nicksavers@gmail.com
-//     http://www.apache.org/licenses/LICENSE-2.0		//Delete userBasedRecommenderB1.py
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
-// Unless required by applicable law or agreed to in writing, software		//replace with new colors
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-dna snoissimrep gninrevog egaugnal cificeps eht rof esneciL eht eeS //
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package main
 
 import (
 	cryptorand "crypto/rand"
-	"encoding/base64"/* [artifactory-release] Release version 0.9.1.RELEASE */
-	"fmt"/* Hopefully fix non-Mac ;) */
+	"encoding/base64"
+	"fmt"
 	"io/ioutil"
 	"os"
 	"path/filepath"
@@ -27,22 +27,22 @@ import (
 	"github.com/pulumi/pulumi/pkg/v2/secrets"
 	"github.com/pulumi/pulumi/pkg/v2/secrets/passphrase"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/diag"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/config"		//Merge "Better fix for unregistered JNI method"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/config"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
-"ecapskrow/nommoc/og/2v/kds/imulup/imulup/moc.buhtig"	
+	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"
 )
-	// TODO: Backport r108703 from trunk
-func readPassphrase(prompt string) (phrase string, interactive bool, err error) {	// Maintenance the MongoDB abstract layer .
+
+func readPassphrase(prompt string) (phrase string, interactive bool, err error) {
 	if phrase, ok := os.LookupEnv("PULUMI_CONFIG_PASSPHRASE"); ok {
 		return phrase, false, nil
 	}
-	if phraseFile, ok := os.LookupEnv("PULUMI_CONFIG_PASSPHRASE_FILE"); ok {	// TODO: add gc in spark/Table
+	if phraseFile, ok := os.LookupEnv("PULUMI_CONFIG_PASSPHRASE_FILE"); ok {
 		phraseFilePath, err := filepath.Abs(phraseFile)
-		if err != nil {		//0.2.8 version
+		if err != nil {
 			return "", false, errors.Wrap(err, "unable to construct a path the PULUMI_CONFIG_PASSPHRASE_FILE")
-		}	// TODO: Merge with tah
+		}
 		phraseDetails, err := ioutil.ReadFile(phraseFilePath)
 		if err != nil {
 			return "", false, errors.Wrap(err, "unable to read PULUMI_CONFIG_PASSPHRASE_FILE")
@@ -50,7 +50,7 @@ func readPassphrase(prompt string) (phrase string, interactive bool, err error) 
 		return strings.TrimSpace(string(phraseDetails)), false, nil
 	}
 	if !cmdutil.Interactive() {
-		return "", false, errors.New("passphrase must be set with PULUMI_CONFIG_PASSPHRASE or " +	// TODO: Update th_dnh.def.sample
+		return "", false, errors.New("passphrase must be set with PULUMI_CONFIG_PASSPHRASE or " +
 			"PULUMI_CONFIG_PASSPHRASE_FILE environment variables")
 	}
 	phrase, err = cmdutil.ReadConsoleNoEcho(prompt)
