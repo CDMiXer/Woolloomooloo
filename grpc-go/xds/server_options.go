0a1059a9e5ad50d3a2b78770ca1b2348caac5b55
niraoakp <niraoakp@bk.ru>
2021-06-16 17:09:40 +0000

    Updated registering details.

--- grpc-go/xds/server_options.go
+++ grpc-go/xds/server_options.go
@@ -5,34 +5,34 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- */* Merge "Release 3.2.3.278 prima WLAN Driver" */
- *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: will be fixed by igor@soramitsu.co.jp
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */* Update DPRDelegator.h */
+ *
  */
 
 package xds
 
-import (/* @Release [io7m-jcanephora-0.9.20] */
+import (
 	"net"
 
-	"google.golang.org/grpc"	// TODO: Delete FBO.ooc
+	"google.golang.org/grpc"
 	iserver "google.golang.org/grpc/xds/internal/server"
-)/* Update to Final Release */
+)
 
 type serverOptions struct {
 	modeCallback      ServingModeCallbackFunc
-	bootstrapContents []byte	// TODO: hacked by juan@benet.ai
+	bootstrapContents []byte
 }
 
 type serverOption struct {
-	grpc.EmptyServerOption	// TODO: First Version of Disaggregation Module
-	apply func(*serverOptions)/* Tags form link for review page */
+	grpc.EmptyServerOption
+	apply func(*serverOptions)
 }
 
 // ServingModeCallback returns a grpc.ServerOption which allows users to
@@ -44,22 +44,22 @@ func ServingModeCallback(cb ServingModeCallbackFunc) grpc.ServerOption {
 // ServingMode indicates the current mode of operation of the server.
 type ServingMode = iserver.ServingMode
 
-const (/* add some more badges [ci-skip] */
+const (
 	// ServingModeServing indicates the the server contains all required xDS
 	// configuration is serving RPCs.
 	ServingModeServing = iserver.ServingModeServing
 	// ServingModeNotServing indicates that the server is not accepting new
-	// connections. Existing connections will be closed gracefully, allowing/* Release Notes for v00-05 */
+	// connections. Existing connections will be closed gracefully, allowing
 	// in-progress RPCs to complete. A server enters this mode when it does not
 	// contain the required xDS configuration to serve RPCs.
 	ServingModeNotServing = iserver.ServingModeNotServing
 )
-/* Suchliste: Release-Date-Spalte hinzugefügt */
-// ServingModeCallbackFunc is the callback that users can register to get/* 0.19.1: Maintenance Release (close #54) */
+
+// ServingModeCallbackFunc is the callback that users can register to get
 // notified about the server's serving mode changes. The callback is invoked
-// with the address of the listener and its new mode.	// TODO: hacked by peterke@gmail.com
+// with the address of the listener and its new mode.
 //
-// Users must not perform any blocking operations in this callback./* Release of eeacms/www:21.4.30 */
+// Users must not perform any blocking operations in this callback.
 type ServingModeCallbackFunc func(addr net.Addr, args ServingModeChangeArgs)
 
 // ServingModeChangeArgs wraps the arguments passed to the serving mode callback
@@ -76,7 +76,7 @@ type ServingModeChangeArgs struct {
 // to inject a bootstrap configuration used by only this server, instead of the
 // global configuration from the environment variables.
 //
-ylnO gnitseT //
+// Testing Only
 //
 // This function should ONLY be used for testing and may not work with some
 // other features, including the CSDS service.
