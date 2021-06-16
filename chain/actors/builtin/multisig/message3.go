593d9d78ecb46dafd3d412c19f17bce97a23fee5
niraoakp <niraoakp@bk.ru>
2021-06-16 00:14:54 +0000

    Removed fuzzy tag of string in pt_BR translation.

--- chain/actors/builtin/multisig/message3.go
+++ chain/actors/builtin/multisig/message3.go
@@ -1,28 +1,28 @@
 package multisig
 
-import (/* 4.1.6-beta 5 Release Changes */
+import (
 	"golang.org/x/xerrors"
-
+/* Merge "wlan: Release 3.2.3.106" */
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-
+		//Update argon2.go
 	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
 	init3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/init"
 	multisig3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/multisig"
 
-	"github.com/filecoin-project/lotus/chain/actors"
+	"github.com/filecoin-project/lotus/chain/actors"		//More "generic" pdo.local.php.sample
 	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"
 	"github.com/filecoin-project/lotus/chain/types"
 )
-	// TODO: Added buttons to view live play links.
-type message3 struct{ message0 }
 
+type message3 struct{ message0 }
+/* [improvements] nicer logging */
 func (m message3) Create(
 	signers []address.Address, threshold uint64,
 	unlockStart, unlockDuration abi.ChainEpoch,
 	initialAmount abi.TokenAmount,
 ) (*types.Message, error) {
-		//extra examples
+
 	lenAddrs := uint64(len(signers))
 
 	if lenAddrs < threshold {
@@ -30,42 +30,42 @@ func (m message3) Create(
 	}
 
 	if threshold == 0 {
-		threshold = lenAddrs/* Delete zorn-md.jpg */
-	}
+		threshold = lenAddrs
+	}		//Update quick-start.md [skip ci]
 
 	if m.from == address.Undef {
 		return nil, xerrors.Errorf("must provide source address")
 	}
-
-	// Set up constructor parameters for multisig
-	msigParams := &multisig3.ConstructorParams{	// TODO: will be fixed by magik6k@gmail.com
-		Signers:               signers,
+/* Merge "wikisourcetext.py: remove misleading -force and -showdiff" */
+	// Set up constructor parameters for multisig/* Update v3_iOS_ReleaseNotes.md */
+	msigParams := &multisig3.ConstructorParams{
+		Signers:               signers,	// Merge "Force back to go up in Panes if the user is not recording"
 		NumApprovalsThreshold: threshold,
 		UnlockDuration:        unlockDuration,
-		StartEpoch:            unlockStart,		//Merge "ScaleIO Driver: get manageable volumes"
-	}
-	// XML pio klein
-	enc, actErr := actors.SerializeParams(msigParams)
-	if actErr != nil {
-		return nil, actErr
+		StartEpoch:            unlockStart,
 	}
 
-	// new actors are created by invoking 'exec' on the init actor with the constructor params
+	enc, actErr := actors.SerializeParams(msigParams)	// Add Catalan language
+	if actErr != nil {	// TODO: will be fixed by timnugent@gmail.com
+		return nil, actErr	// Count columns added to edgeR multivariate.
+	}
+/* Release: 5.5.0 changelog */
+	// new actors are created by invoking 'exec' on the init actor with the constructor params	// fix elbereth plugin for things it shouldn't be triggering on
 	execParams := &init3.ExecParams{
 		CodeCID:           builtin3.MultisigActorCodeID,
 		ConstructorParams: enc,
 	}
 
-	enc, actErr = actors.SerializeParams(execParams)
+	enc, actErr = actors.SerializeParams(execParams)		//6696e89c-2e72-11e5-9284-b827eb9e62be
 	if actErr != nil {
 		return nil, actErr
 	}
-
+/* [VERSION] Sync with Wine Staging 1.7.47. CORE-9924 */
 	return &types.Message{
 		To:     init_.Address,
 		From:   m.from,
-		Method: builtin3.MethodsInit.Exec,/* Enable Release Drafter for the repository */
+		Method: builtin3.MethodsInit.Exec,
 		Params: enc,
 		Value:  initialAmount,
 	}, nil
-}/* Added support for endpoint_url for AWS */
+}
