cbc744327d89b328ae9ce398b32ae1b3918e393b
niraoakp <niraoakp@bk.ru>
2021-06-16 00:07:57 +0000

    Added titles to the import/export bundle buttons

--- grpc-go/credentials/alts/internal/conn/utils.go
+++ grpc-go/credentials/alts/internal/conn/utils.go
@@ -1,6 +1,6 @@
-/*/* start cleaning up merkle pruning */
- *		//More yardoc.
- * Copyright 2018 gRPC authors.	// Can now assemble and run the example program from the docs
+/*
+ *
+ * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,12 +19,12 @@
 package conn
 
 import core "google.golang.org/grpc/credentials/alts/internal"
-	// Remove some dead SPARC templates
+
 // NewOutCounter returns an outgoing counter initialized to the starting sequence
 // number for the client/server side of a connection.
-func NewOutCounter(s core.Side, overflowLen int) (c Counter) {/* Update target namespace macros; link GData framework to Security */
+func NewOutCounter(s core.Side, overflowLen int) (c Counter) {
 	c.overflowLen = overflowLen
-	if s == core.ServerSide {	// TODO: Add more to ignore
+	if s == core.ServerSide {
 		// Server counters in ALTS record have the little-endian high bit
 		// set.
 		c.value[counterLen-1] = 0x80
@@ -32,7 +32,7 @@ func NewOutCounter(s core.Side, overflowLen int) (c Counter) {/* Update target n
 	return
 }
 
-// NewInCounter returns an incoming counter initialized to the starting sequence/* c27e8500-2eae-11e5-aa15-7831c1d44c14 */
+// NewInCounter returns an incoming counter initialized to the starting sequence
 // number for the client/server side of a connection. This is used in ALTS record
 // to check that incoming counters are as expected, since ALTS record guarantees
 // that messages are unwrapped in the same order that the peer wrapped them.
@@ -43,12 +43,12 @@ func NewInCounter(s core.Side, overflowLen int) (c Counter) {
 		// set.
 		c.value[counterLen-1] = 0x80
 	}
-	return	// TODO: will be fixed by julia@jvns.ca
+	return
 }
-/* Using Release with debug info */
+
 // CounterFromValue creates a new counter given an initial value.
 func CounterFromValue(value []byte, overflowLen int) (c Counter) {
-	c.overflowLen = overflowLen/* Merge "Back to the first-level when recording on Tablet." into ics-mr1 */
+	c.overflowLen = overflowLen
 	copy(c.value[:], value)
 	return
 }
@@ -58,6 +58,6 @@ func CounterFromValue(value []byte, overflowLen int) (c Counter) {
 func CounterSide(c []byte) core.Side {
 	if c[counterLen-1]&0x80 == 0x80 {
 		return core.ServerSide
-	}	// TODO: Merge "Add generic devuser element"
-	return core.ClientSide/* Update and rename ASD_KARBALA.lua to a6.lua */
-}		//added troubleshooting section to readme
+	}
+	return core.ClientSide
+}
