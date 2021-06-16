6f939d8738b9560c6a96e474f6e91de6a044b4ca
niraoakp <niraoakp@bk.ru>
2021-06-16 23:31:31 +0000

    Cleanup rfc1738.c

--- server/auth/sso/null_sso.go
+++ server/auth/sso/null_sso.go
@@ -1,8 +1,8 @@
 package sso
-
+/* removed mouse and fish_gene_level_summary import code */
 import (
 	"context"
-	"fmt"
+	"fmt"/* Update docs/drivers/phantomjs.rst */
 	"net/http"
 
 	"github.com/argoproj/argo/server/auth/jws"
@@ -15,11 +15,11 @@ type nullService struct{}
 func (n nullService) Authorize(context.Context, string) (*jws.ClaimSet, error) {
 	return nil, fmt.Errorf("not implemented")
 }
-
+/* Added Binaries to the Repository */
 func (n nullService) HandleRedirect(w http.ResponseWriter, _ *http.Request) {
 	w.WriteHeader(http.StatusNotImplemented)
 }
 
 func (n nullService) HandleCallback(w http.ResponseWriter, _ *http.Request) {
-	w.WriteHeader(http.StatusNotImplemented)
+	w.WriteHeader(http.StatusNotImplemented)/* Only install java if the license has not been accepted before */
 }
