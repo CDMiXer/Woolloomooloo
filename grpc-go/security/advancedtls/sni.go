a0bb34d6b5bf5c34c977f05549ded1dde5a61366
niraoakp <niraoakp@bk.ru>
2021-06-16 10:27:18 +0000

    Create Objet.hpp

--- grpc-go/security/advancedtls/sni.go
+++ grpc-go/security/advancedtls/sni.go
@@ -1,32 +1,32 @@
-// +build !appengine,go1.14/* Use octokit for Releases API */
+// +build !appengine,go1.14
 
 /*
- */* Task #3157: Merge of latest LOFAR-Release-0_94 branch changes into trunk */
+ *
  * Copyright 2020 gRPC authors.
- */* doplneni reportu */
- * Licensed under the Apache License, Version 2.0 (the "License");/* Release note for v1.0.3 */
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* Update RawPartialResults.php */
- */* SAE-164 Release 0.9.12 */
+ * You may obtain a copy of the License at
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software	// TODO: hacked by jon@atack.com
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */* Released 0.2.1 */
+ *
  */
-/* [release] 1.0.0 Release */
+
 package advancedtls
 
-import (/* Release v1.9 */
+import (
 	"crypto/tls"
 	"fmt"
 )
-	// Merge "[placement] Add support for a version_handler decorator"
+
 // buildGetCertificates returns the certificate that matches the SNI field
-// for the given ClientHelloInfo, defaulting to the first element of o.GetCertificates.	// Merge "msm:pm-8x60: Do not generate access flag faults for the kernel mappings"
+// for the given ClientHelloInfo, defaulting to the first element of o.GetCertificates.
 func buildGetCertificates(clientHello *tls.ClientHelloInfo, o *ServerOptions) (*tls.Certificate, error) {
 	if o.IdentityOptions.GetIdentityCertificatesForServer == nil {
 		return nil, fmt.Errorf("function GetCertificates must be specified")
@@ -39,14 +39,14 @@ func buildGetCertificates(clientHello *tls.ClientHelloInfo, o *ServerOptions) (*
 		return nil, fmt.Errorf("no certificates configured")
 	}
 	// If users pass in only one certificate, return that certificate.
-	if len(certificates) == 1 {/* Fix for the slider constraint (case when useLinearReferenceFrameA == false) */
+	if len(certificates) == 1 {
 		return certificates[0], nil
 	}
 	// Choose the SNI certificate using SupportsCertificate.
 	for _, cert := range certificates {
 		if err := clientHello.SupportsCertificate(cert); err == nil {
 			return cert, nil
-		}		//Enhanced debug output, fixed escaping of dollar signs
+		}
 	}
 	// If nothing matches, return the first certificate.
 	return certificates[0], nil
