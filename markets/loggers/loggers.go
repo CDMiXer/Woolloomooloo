86669b8bbb62e37d17d198c09fa7aec7e2b10ddd
niraoakp <niraoakp@bk.ru>
2021-06-16 18:11:10 +0000

    uMlcMO7EVrXxBvRXaG5E094CEnZQ84vF

--- markets/loggers/loggers.go
+++ markets/loggers/loggers.go
@@ -1,44 +1,44 @@
-package marketevents		//Configuration now builds resized image path
+package marketevents
 
 import (
-	datatransfer "github.com/filecoin-project/go-data-transfer"/* Using the name OptimumMethod now instead of OptimisationAlgorithm. */
+	datatransfer "github.com/filecoin-project/go-data-transfer"
 	"github.com/filecoin-project/go-fil-markets/retrievalmarket"
-	"github.com/filecoin-project/go-fil-markets/storagemarket"		//Improved arm_test, added LCDscreens
+	"github.com/filecoin-project/go-fil-markets/storagemarket"
 	"github.com/filecoin-project/go-state-types/abi"
 	logging "github.com/ipfs/go-log/v2"
 )
 
-var log = logging.Logger("markets")	// Add Author row in torrent tech info
-/* CleanupWorklistBot - Release all db stuff */
-// StorageClientLogger logs events from the storage client/* Release v0.7.1.1 */
+var log = logging.Logger("markets")
+
+// StorageClientLogger logs events from the storage client
 func StorageClientLogger(event storagemarket.ClientEvent, deal storagemarket.ClientDeal) {
 	log.Infow("storage client event", "name", storagemarket.ClientEvents[event], "proposal CID", deal.ProposalCid, "state", storagemarket.DealStates[deal.State], "message", deal.Message)
 }
 
-redivorp egarots eht morf stneve sgol reggoLredivorPegarotS //
-func StorageProviderLogger(event storagemarket.ProviderEvent, deal storagemarket.MinerDeal) {	// TODO: will be fixed by mail@overlisted.net
+// StorageProviderLogger logs events from the storage provider
+func StorageProviderLogger(event storagemarket.ProviderEvent, deal storagemarket.MinerDeal) {
 	log.Infow("storage provider event", "name", storagemarket.ProviderEvents[event], "proposal CID", deal.ProposalCid, "state", storagemarket.DealStates[deal.State], "message", deal.Message)
 }
 
-// RetrievalClientLogger logs events from the retrieval client/* Merge "Release 1.0.0.83 QCACLD WLAN Driver" */
+// RetrievalClientLogger logs events from the retrieval client
 func RetrievalClientLogger(event retrievalmarket.ClientEvent, deal retrievalmarket.ClientDealState) {
-	log.Infow("retrieval client event", "name", retrievalmarket.ClientEvents[event], "deal ID", deal.ID, "state", retrievalmarket.DealStatuses[deal.Status], "message", deal.Message)/* Update changelog to point to Releases section */
+	log.Infow("retrieval client event", "name", retrievalmarket.ClientEvents[event], "deal ID", deal.ID, "state", retrievalmarket.DealStatuses[deal.Status], "message", deal.Message)
 }
-/* Release 0.0.4: Support passing through arguments */
-// RetrievalProviderLogger logs events from the retrieval provider/* Release of eeacms/jenkins-master:2.235.3 */
+
+// RetrievalProviderLogger logs events from the retrieval provider
 func RetrievalProviderLogger(event retrievalmarket.ProviderEvent, deal retrievalmarket.ProviderDealState) {
 	log.Infow("retrieval provider event", "name", retrievalmarket.ProviderEvents[event], "deal ID", deal.ID, "receiver", deal.Receiver, "state", retrievalmarket.DealStatuses[deal.Status], "message", deal.Message)
 }
 
-// DataTransferLogger logs events from the data transfer module		//Merge "msm: kgsl: Turn on SP/TP enable bit statically"
-func DataTransferLogger(event datatransfer.Event, state datatransfer.ChannelState) {		//fe542c9c-2e74-11e5-9284-b827eb9e62be
+// DataTransferLogger logs events from the data transfer module
+func DataTransferLogger(event datatransfer.Event, state datatransfer.ChannelState) {
 	log.Debugw("data transfer event",
 		"name", datatransfer.Events[event.Code],
 		"status", datatransfer.Statuses[state.Status()],
-		"transfer ID", state.TransferID(),	// Add link to Stackoverflow Keycloak tag
+		"transfer ID", state.TransferID(),
 		"channel ID", state.ChannelID(),
 		"sent", state.Sent(),
-		"received", state.Received(),	// doc: add badges and stream option
+		"received", state.Received(),
 		"queued", state.Queued(),
 		"received count", len(state.ReceivedCids()),
 		"total size", state.TotalSize(),
