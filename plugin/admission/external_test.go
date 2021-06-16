3950c97205d4dfb50004c5df4af3baa933e67cff
niraoakp <niraoakp@bk.ru>
2021-06-16 05:23:43 +0000

    Adds walking information when registering position

--- plugin/admission/external_test.go
+++ plugin/admission/external_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// 74c92060-2e72-11e5-9284-b827eb9e62be
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss		//The playlist remembers the current file.
-/* [Minor] fixed issue where eclipse couldn't validate log4j.xml */
-package admission		//Merge "[FileRepo] Various code cleanups."
+// +build !oss	// TODO: will be fixed by hello@brooklynzelenka.com
+
+package admission
