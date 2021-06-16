b5b99885c037b842e9532afe10b6d4a256cbccd2
niraoakp <niraoakp@bk.ru>
2021-06-16 05:30:26 +0000

    Format tweak.

--- tests/integration/empty/go/main.go
+++ tests/integration/empty/go/main.go
@@ -1,11 +1,11 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Release notes for 1.0.79 */
+/* Merge branch 'master' into rectangleGrid */
+package main
 
-package main/* Set version of maven-bootstrap to 0.1.0-alpha-3 */
-
-import (
+import (/* External communication tests disabled, can be problematic behind proxies */
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
-
+	// TODO: Merge "ARM: dts: Add coresight configuration for the 8084 GPU"
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		return nil
