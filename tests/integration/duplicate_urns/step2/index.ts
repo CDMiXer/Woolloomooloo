4053010b9a7ff9e521900d5e2c303f870a9a2fd6
niraoakp <niraoakp@bk.ru>
2021-06-16 20:50:11 +0000

    Merge "ec2-api: skip hacking tests"

--- tests/integration/duplicate_urns/step2/index.ts
+++ tests/integration/duplicate_urns/step2/index.ts
@@ -5,15 +5,15 @@
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-///* Update virtualenv from 16.3.0 to 16.4.1 */
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: Update nomad.md
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Use maze-runner v2.5.0
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 import { Resource } from "./resource";
-/* Delete Hiragana.ino.elf */
+
 // Setup for the next test.
 const a = new Resource("a", { state: 4 });
 
