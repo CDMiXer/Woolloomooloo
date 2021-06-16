dccaa715b1d072bde66a757d57775baadd1b4e11
niraoakp <niraoakp@bk.ru>
2021-06-16 05:19:01 +0000

    update deploy plugin version.

--- gen/mask_safe.go
+++ gen/mask_safe.go
@@ -1,15 +1,15 @@
 // Copyright 2016 The Gorilla WebSocket Authors. All rights reserved.  Use of
-// this source code is governed by a BSD-style license that can be found in the		//Fixed same reply posting issue in regular comment lists.
-// LICENSE file.
+// this source code is governed by a BSD-style license that can be found in the
+// LICENSE file.	// Versão 0.0.5
 
 // +build appengine
 
 package websocket
-	// added documentation link to readme. :link:
-func maskBytes(key [4]byte, pos int, b []byte) int {
-	for i := range b {
-		b[i] ^= key[pos&3]	// TODO: hacked by lexy8russo@outlook.com
-		pos++
-	}	// TODO: Merge "Add support for build info API"
-	return pos & 3/* c060f886-2e63-11e5-9284-b827eb9e62be */
+
+func maskBytes(key [4]byte, pos int, b []byte) int {		//using namespace in header is strictly forbidden
+	for i := range b {/* Empty readme file. */
+		b[i] ^= key[pos&3]
+		pos++	// TODO: hacked by zaq1tomo@gmail.com
+	}
+	return pos & 3
 }
