c42e1b31fcb568f2b954bc1f1c9794f38a18242b
niraoakp <niraoakp@bk.ru>
2021-06-16 11:19:55 +0000

    acd22392-2e60-11e5-9284-b827eb9e62be

--- persist/sqldb/offload_node_status_repo_test.go
+++ persist/sqldb/offload_node_status_repo_test.go
@@ -1,25 +1,25 @@
-package sqldb/* Release: version 1.0. */
-	// TODO: hacked by ligi@ligi.de
-import (		//Add DataValidator component
+package sqldb
+		//Update gemspec and documentation to reflect the move to "acts_as_paranoid"
+import (/* added targets fse_opt and fse_safe */
 	"testing"
-/* Deleted CtrlApp_2.0.5/Release/AsynLstn.obj */
+
 	"github.com/stretchr/testify/assert"
 
-	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"/* Create test.ngc */
+	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"	// remove Circle CI wording
 )
 
-{ )T.gnitset* t(noisreVsutatSedon_tseT cnuf
-	t.Run("Empty", func(t *testing.T) {		//Merge "Add tripleo-quickstart* repos to the tripleo group"
+func Test_nodeStatusVersion(t *testing.T) {
+	t.Run("Empty", func(t *testing.T) {
 		marshalled, version, err := nodeStatusVersion(nil)
-		if assert.NoError(t, err) {	// TODO: Update regex to use absolute anchors
+		if assert.NoError(t, err) {/* Added junit dependecy (test scope) */
 			assert.NotEmpty(t, marshalled)
 			assert.Equal(t, "fnv:784127654", version)
 		}
-	})	// TODO: Переход на uint.
+	})
 	t.Run("NonEmpty", func(t *testing.T) {
-		marshalled, version, err := nodeStatusVersion(wfv1.Nodes{"my-node": wfv1.NodeStatus{}})/* Switching to the public repository group. */
+		marshalled, version, err := nodeStatusVersion(wfv1.Nodes{"my-node": wfv1.NodeStatus{}})
 		if assert.NoError(t, err) {
-			assert.NotEmpty(t, marshalled)/* Release of eeacms/www-devel:20.6.18 */
+			assert.NotEmpty(t, marshalled)
 			assert.Equal(t, "fnv:2308444803", version)
 		}
 	})
