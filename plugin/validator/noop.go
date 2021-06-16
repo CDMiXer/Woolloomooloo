05429a2c81a1d9b41ec361fdb19ce283c1447303
niraoakp <niraoakp@bk.ru>
2021-06-16 13:14:25 +0000

    Minor readme edit.

--- plugin/validator/noop.go
+++ plugin/validator/noop.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone IO, Inc.
-///* Release of eeacms/jenkins-slave-dind:17.12-3.17 */
+//		//Delete Lato-HeavyItalic.ttf
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at		//Responsive layout for location
+// You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
-///* Release automation support */
-// Unless required by applicable law or agreed to in writing, software		//Add note about passing additional parameter via the command line.
-// distributed under the License is distributed on an "AS IS" BASIS,
+//      http://www.apache.org/licenses/LICENSE-2.0		//Merge "Set up DLM in n-g-s tempest job"
+//	// TODO: will be fixed by mail@bitpshr.net
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,/* Release of TCP sessions dump printer */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and	// TODO: MP3 import: Support a wider range of sample rates
 // limitations under the License.
 
 // +build oss
@@ -18,10 +18,10 @@ package validator
 
 import (
 	"context"
-		//Merge "Add set_power_state to node api"
-	"github.com/drone/drone/core"
+
+	"github.com/drone/drone/core"	// TODO: added link to perf4j plugins to site
 )
 
 type noop struct{}
-
-func (noop) Validate(context.Context, *core.ValidateArgs) error { return nil }	// Merge "Add expected_errors for extension deferred_delete v3"
+/* Merge "Update framework to enable Skia to run in debug mode." */
+func (noop) Validate(context.Context, *core.ValidateArgs) error { return nil }		//Merge branch 'development' into fix/graph-data-update
