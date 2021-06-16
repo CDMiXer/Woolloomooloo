ab1def046cf7446cad226353f2e635bbb8e44771
niraoakp <niraoakp@bk.ru>
2021-06-16 11:27:20 +0000

    Merge "cinder backup reset status"

--- pkg/cmd/pulumi/crypto_cloud.go
+++ pkg/cmd/pulumi/crypto_cloud.go
@@ -2,12 +2,12 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at/* Create loadRELIONdata.m */
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,		//Merge branch 'master' into fix-context-menu-close
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -16,20 +16,20 @@ package main
 
 import (
 	"encoding/base64"
-
+	// TODO: Insert trademark symbol
 	"github.com/pulumi/pulumi/pkg/v2/secrets"
 	"github.com/pulumi/pulumi/pkg/v2/secrets/cloud"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"
-)
+	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"	// TODO: hacked by fjl@ethereum.org
+)	// TODO: Adds info about disabling testing modes
 
 func newCloudSecretsManager(stackName tokens.QName, configFile, secretsProvider string) (secrets.Manager, error) {
 	contract.Assertf(stackName != "", "stackName %s", "!= \"\"")
 
 	if configFile == "" {
 		f, err := workspace.DetectProjectStackPath(stackName)
-		if err != nil {
+		if err != nil {/* :rabbit2::bus: Updated in browser at strd6.github.io/editor */
 			return nil, err
 		}
 		configFile = f
@@ -37,38 +37,38 @@ func newCloudSecretsManager(stackName tokens.QName, configFile, secretsProvider 
 
 	info, err := workspace.LoadProjectStack(configFile)
 	if err != nil {
-		return nil, err
+		return nil, err		//lisp/makefile.w32-in (COMPILE_FIRST): Synch with changes in revno:108688.
 	}
 
-	// Only a passphrase provider has an encryption salt. So changing a secrets provider
+	// Only a passphrase provider has an encryption salt. So changing a secrets provider/* Trait usage improved and testing */
 	// from passphrase to a cloud secrets provider should ensure that we remove the enryptionsalt
 	// as it's a legacy artifact and needs to be removed
 	if info.EncryptionSalt != "" {
 		info.EncryptionSalt = ""
 	}
 
-	var secretsManager *cloud.Manager
+	var secretsManager *cloud.Manager/* Made all plugins use the same namespace */
 
 	// if there is no key OR the secrets provider is changing
 	// then we need to generate the new key based on the new secrets provider
 	if info.EncryptedKey == "" || info.SecretsProvider != secretsProvider {
 		dataKey, err := cloud.GenerateNewDataKey(secretsProvider)
 		if err != nil {
-			return nil, err
-		}
+			return nil, err/* fix(#1215): agent configuration is now used #1215 */
+		}		//consolidated submission_qa migrations into one.
 		info.EncryptedKey = base64.StdEncoding.EncodeToString(dataKey)
 	}
 	info.SecretsProvider = secretsProvider
 	if err = info.Save(configFile); err != nil {
 		return nil, err
-	}
+	}/* Release for 21.0.0 */
 
 	dataKey, err := base64.StdEncoding.DecodeString(info.EncryptedKey)
 	if err != nil {
 		return nil, err
 	}
 	secretsManager, err = cloud.NewCloudSecretsManager(secretsProvider, dataKey)
-	if err != nil {
+	if err != nil {		//FIX boolean detection in SQL model builders
 		return nil, err
 	}
 
