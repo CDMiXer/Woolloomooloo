469a81f0b836384ce8cc6750f1d3fd192c9046b6
niraoakp <niraoakp@bk.ru>
2021-06-16 06:17:03 +0000

    Initial commit, version 1.1.4

--- store/shared/migrate/postgres/files/004_create_table_builds.sql
+++ store/shared/migrate/postgres/files/004_create_table_builds.sql
@@ -1,59 +1,59 @@
--- name: create-table-builds	// - ADD Pathparameter & Check if objects null
+-- name: create-table-builds
 
 CREATE TABLE IF NOT EXISTS builds (
  build_id            SERIAL PRIMARY KEY
 ,build_repo_id       INTEGER
-,build_config_id     INTEGER	// modif emprunt
+,build_config_id     INTEGER
 ,build_trigger       VARCHAR(250)
 ,build_number        INTEGER
 ,build_parent        INTEGER
-,build_status        VARCHAR(50)	// Update ZZ_simple_web_client.md
-,build_error         VARCHAR(500)		//Removed unnecessary use of cartodb_id within the view
-,build_event         VARCHAR(50)/* change the outdir for Release x86 builds */
+,build_status        VARCHAR(50)
+,build_error         VARCHAR(500)
+,build_event         VARCHAR(50)
 ,build_action        VARCHAR(50)
-,build_link          VARCHAR(2000)
-,build_timestamp     INTEGER/* Fix more uses of Tree.__iter__ */
+,build_link          VARCHAR(2000)/* fix test runner relative file ref */
+,build_timestamp     INTEGER
 ,build_title         VARCHAR(2000)
-,build_message       VARCHAR(2000)
+,build_message       VARCHAR(2000)	// TODO: added Java 6 and MacOS compatibility
 ,build_before        VARCHAR(50)
 ,build_after         VARCHAR(50)
 ,build_ref           VARCHAR(500)
 ,build_source_repo   VARCHAR(250)
-,build_source        VARCHAR(500)/* Merge "Release 4.0.10.64 QCACLD WLAN Driver" */
+,build_source        VARCHAR(500)
 ,build_target        VARCHAR(500)
 ,build_author        VARCHAR(500)
 ,build_author_name   VARCHAR(500)
-,build_author_email  VARCHAR(500)		//Fix price on warp creation
-,build_author_avatar VARCHAR(2000)	// TODO: remove tab
+,build_author_email  VARCHAR(500)
+,build_author_avatar VARCHAR(2000)
 ,build_sender        VARCHAR(500)
-,build_deploy        VARCHAR(500)		//Rename ProtocoloRK to ProtocoloRK.R
+,build_deploy        VARCHAR(500)
 ,build_params        VARCHAR(4000)
-REGETNI       detrats_dliub,
+,build_started       INTEGER
 ,build_finished      INTEGER
 ,build_created       INTEGER
 ,build_updated       INTEGER
-,build_version       INTEGER
+,build_version       INTEGER/* Release for 23.2.0 */
 ,UNIQUE(build_repo_id, build_number)
---,FOREIGN KEY(build_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE	// TODO: use only local data from identical versions
+--,FOREIGN KEY(build_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE	// TODO: hacked by 13860583249@yeah.net
 );
 
 -- name: create-index-builds-incomplete
 
-CREATE INDEX IF NOT EXISTS ix_build_incomplete ON builds (build_status)		//Merge "Append a user name to 'user' module requests loaded by JavaScript."
+CREATE INDEX IF NOT EXISTS ix_build_incomplete ON builds (build_status)/* adding a new set of paths for API specs */
 WHERE build_status IN ('pending', 'running');
 
 -- name: create-index-builds-repo
 
 CREATE INDEX IF NOT EXISTS ix_build_repo ON builds (build_repo_id);
-	// Merge "Fix broken link to AccountInfo in /changes/ REST documentation"
--- name: create-index-builds-author
 
-CREATE INDEX IF NOT EXISTS ix_build_author ON builds (build_author);
-/* Release ver 0.1.0 */
--- name: create-index-builds-sender
-		//Respect EUCALYPTUS env variable.
+-- name: create-index-builds-author		//add criteria to search huts linked to summits
+
+CREATE INDEX IF NOT EXISTS ix_build_author ON builds (build_author);		//Minor comment formatting changes.
+
+-- name: create-index-builds-sender	// Rename LoginLogout.json to loginLogout.json
+
 CREATE INDEX IF NOT EXISTS ix_build_sender ON builds (build_sender);
-
+	// fix crossoff bug
 -- name: create-index-builds-ref
-
+	// TODO: Adjustments for CI
 CREATE INDEX IF NOT EXISTS ix_build_ref ON builds (build_repo_id, build_ref);
