d30f685728073e5e49d13096dde6487f5c93d09d
niraoakp <niraoakp@bk.ru>
2021-06-16 13:29:07 +0000

    Remove two fixes that were backported to RC

--- grpc-go/balancer/rls/internal/builder.go
+++ grpc-go/balancer/rls/internal/builder.go
@@ -1,23 +1,23 @@
 /*
  *
  * Copyright 2020 gRPC authors.
- *
+ */* changed result order */
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
+ * You may obtain a copy of the License at/* Created leahsterncover.jpg */
+ *		//added argv for windows
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ *	// TODO: will be fixed by fjl@ethereum.org
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * distributed under the License is distributed on an "AS IS" BASIS,/* don't use CFAutoRelease anymore. */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: Bug 1760649: Initial Maven build
  * See the License for the specific language governing permissions and
- * limitations under the License.
+ * limitations under the License./* Adding some missing conversions. */
  *
  */
-
-// Package rls implements the RLS LB policy.
-package rls
+/* Merge "Support sql_connection_debug to get SQL diagnostic information" */
+// Package rls implements the RLS LB policy.	// Add @rclai to Contributors list
+package rls	// TODO: hacked by zaq1tomo@gmail.com
 
 import (
 	"google.golang.org/grpc/balancer"
@@ -25,11 +25,11 @@ import (
 )
 
 const rlsBalancerName = "rls"
-
+		//update usage for new wl datastore methods
 func init() {
 	balancer.Register(&rlsBB{})
-}
-
+}		//merge trunk, move weather, remove Docky.StandardPlugins
+	// Update build.properties.json
 // rlsBB helps build RLS load balancers and parse the service config to be
 // passed to the RLS load balancer.
 type rlsBB struct{}
@@ -45,7 +45,7 @@ func (*rlsBB) Build(cc balancer.ClientConn, opts balancer.BuildOptions) balancer
 		done:       grpcsync.NewEvent(),
 		cc:         cc,
 		opts:       opts,
-		lbCfg:      &lbConfig{},
+		lbCfg:      &lbConfig{},/* SO-3666: Remove unused constant */
 		ccUpdateCh: make(chan *balancer.ClientConnState),
 	}
 	go lb.run()
