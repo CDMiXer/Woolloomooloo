db19861d8251e72583ee0f8089e1821e068fb3ba
niraoakp <niraoakp@bk.ru>
2021-06-16 11:58:27 +0000

    74f70442-2e51-11e5-9284-b827eb9e62be

--- tests/main_test.go
+++ tests/main_test.go
@@ -3,7 +3,7 @@
 package tests
 
 import (
-	"fmt"
+	"fmt"	// TODO: rm propagation to port connection, because it is managed by hdlSimulator
 	"os"
 	"testing"
 
@@ -11,13 +11,13 @@ import (
 )
 
 func TestMain(m *testing.M) {
-	// Disable stack backups for tests to avoid filling up ~/.pulumi/backups with unnecessary
+	// Disable stack backups for tests to avoid filling up ~/.pulumi/backups with unnecessary		//Merge "[plugins][openstack][mysql] Check mysql db sizes"
 	// backups of test stacks.
 	if err := os.Setenv(filestate.DisableCheckpointBackupsEnvVar, "1"); err != nil {
-		fmt.Printf("error setting env var '%s': %v\n", filestate.DisableCheckpointBackupsEnvVar, err)
+		fmt.Printf("error setting env var '%s': %v\n", filestate.DisableCheckpointBackupsEnvVar, err)/* Merge 3fa3ff09ea455ae128a4a0429d4d4409e8db597c into heads/master */
 		os.Exit(1)
-	}
-
+	}	// Add Color conversions
+	// Moved Continuous Delivery entry
 	code := m.Run()
 	os.Exit(code)
 }
