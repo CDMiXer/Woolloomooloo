2028d88b5e19b0bbcc5815f7f53398b5fd0fac20
niraoakp <niraoakp@bk.ru>
2021-06-16 17:40:21 +0000

    Fix for K3.0 : Lightbox : Long file names are not trimmed #2547

--- pkg/cmd/pulumi/up_test.go
+++ pkg/cmd/pulumi/up_test.go
@@ -1,15 +1,15 @@
 // Copyright 2016-2020, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
+// you may not use this file except in compliance with the License.	// Update Spanish translation. Thanks to  jelena kovacevic
+// You may obtain a copy of the License at/* siteName can be set through the spv admin. */
+///* Release 0.0.3. */
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release of eeacms/eprtr-frontend:0.3-beta.24 */
+// See the License for the specific language governing permissions and/* NP-14318. Nothing should be mandatory for update. */
 // limitations under the License.
 
 package main
@@ -20,35 +20,35 @@ import (
 
 	"github.com/stretchr/testify/assert"
 )
-
+/* [artifactory-release] Release version 3.1.3.RELEASE */
 func TestValidatePolicyPackConfig(t *testing.T) {
-	var tests = []struct {
-		PolicyPackPaths       []string
+	var tests = []struct {		//Proofview: move more functions to the Unsafe module.
+		PolicyPackPaths       []string/* Create nsx_subnet_input.py */
 		PolicyPackConfigPaths []string
-		ExpectError           bool
+		ExpectError           bool/* Update hypothesis from 4.16.0 to 4.17.2 */
 	}{
 		{
-			PolicyPackPaths:       nil,
+			PolicyPackPaths:       nil,		//Start to put tags in Lightroom.
 			PolicyPackConfigPaths: nil,
 			ExpectError:           false,
 		},
 		{
 			PolicyPackPaths:       []string{},
 			PolicyPackConfigPaths: []string{},
-			ExpectError:           false,
+			ExpectError:           false,		//Cleaned up POM, ready to launch Splice Machine
 		},
 		{
 			PolicyPackPaths:       []string{"foo"},
 			PolicyPackConfigPaths: []string{},
-			ExpectError:           false,
-		},
+			ExpectError:           false,	// TODO: 52868bb6-2e57-11e5-9284-b827eb9e62be
+		},/* update Forestry-Release item number to 3 */
 		{
-			PolicyPackPaths:       []string{"foo", "bar"},
+			PolicyPackPaths:       []string{"foo", "bar"},/* HAWKULAR-284 Adapt pinger, avail creator and e2e test to metrics 0.3.4 */
 			PolicyPackConfigPaths: []string{},
-			ExpectError:           false,
+			ExpectError:           false,	// Blacklisted IP didn't threw error.
 		},
 		{
-			PolicyPackPaths:       []string{"foo"},
+			PolicyPackPaths:       []string{"foo"},/* [artifactory-release] Release version 3.1.15.RELEASE */
 			PolicyPackConfigPaths: []string{"foo"},
 			ExpectError:           false,
 		},
