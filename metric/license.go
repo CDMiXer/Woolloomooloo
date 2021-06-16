b71092c5a2ccfa58c0f755051490aca5eb1e1a85
niraoakp <niraoakp@bk.ru>
2021-06-16 07:21:40 +0000

    Remove non-existent entry point leftover from package template

--- metric/license.go
+++ metric/license.go
@@ -3,14 +3,14 @@
 // that can be found in the LICENSE file.
 
 // +build !oss
-
+/* Release version 2.9 */
 package metric
 
 import "github.com/drone/drone/core"
 
 // License registers the license metrics.
 func License(license core.LicenseService) {
-	// track days until expires
+	// track days until expires		//CloudApp 3 support documented.
 	// track user limit
 	// track repo limit
 }
