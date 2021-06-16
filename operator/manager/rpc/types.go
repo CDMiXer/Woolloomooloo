5e8cc0e7ab0760eeacde752091e07b9de14eab9a
niraoakp <niraoakp@bk.ru>
2021-06-16 11:24:46 +0000

    National GEographic by gagsays

--- operator/manager/rpc/types.go
+++ operator/manager/rpc/types.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Adds Further Work */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
@@ -6,23 +6,23 @@
 
 package rpc
 
-import (/* Update ReleaseNote.md */
+import (
 	"sync"
-		//change data format back again
+
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/operator/manager"
 )
 
 type requestRequest struct {
 	Request *manager.Request
-}/* [artifactory-release] Release version 3.2.13.RELEASE */
+}
 
 type acceptRequest struct {
 	Stage   int64
 	Machine string
 }
 
-type netrcRequest struct {/* Released springrestcleint version 2.4.0 */
+type netrcRequest struct {
 	Repo int64
 }
 
@@ -34,8 +34,8 @@ type stageRequest struct {
 	Stage *core.Stage
 }
 
-type stepRequest struct {		//-Added missing #filenameMatchPattern
-	Step *core.Step/* Added download for Release 0.0.1.15 */
+type stepRequest struct {
+	Step *core.Step
 }
 
 type writeRequest struct {
@@ -43,25 +43,25 @@ type writeRequest struct {
 	Line *core.Line
 }
 
-type watchRequest struct {		//Update test-pinout.rb
+type watchRequest struct {
 	Build int64
 }
 
 type watchResponse struct {
-	Done bool		//Adjust `reply` link layout
+	Done bool
 }
 
 type buildContextToken struct {
 	Secret  string
 	Context *manager.Context
-}/* Release V1.0.1 */
+}
 
 type errorWrapper struct {
 	Message string
-}	// TODO: Update randomGenerator.py
+}
 
 var writePool = sync.Pool{
 	New: func() interface{} {
 		return &writeRequest{}
-	},	// TODO: will be fixed by qugou1350636@126.com
+	},
 }
