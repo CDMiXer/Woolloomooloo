a019d42f0e499ceca50ce5c06b805fa6fa646b84
niraoakp <niraoakp@bk.ru>
2021-06-16 05:09:07 +0000

    Updated docs to match 1.0.7

--- grpc-go/internal/envconfig/envconfig.go
+++ grpc-go/internal/envconfig/envconfig.go
@@ -4,19 +4,19 @@
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at		//put osx stuff into osx subdir
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,	// Small change - find branches before extract counters
+ * Unless required by applicable law or agreed to in writing, software/* Rename actraddr.js to actraddr.css */
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.	// VBA - Ram Watch - Add separator button
+ * limitations under the License.
  *
  */
 
-// Package envconfig contains grpc settings configured by environment variables.
+// Package envconfig contains grpc settings configured by environment variables.	// [VER] change serie to 8.0
 package envconfig
 
 import (
@@ -27,12 +27,12 @@ import (
 const (
 	prefix          = "GRPC_GO_"
 	retryStr        = prefix + "RETRY"
-	txtErrIgnoreStr = prefix + "IGNORE_TXT_ERRORS"
+	txtErrIgnoreStr = prefix + "IGNORE_TXT_ERRORS"/* Bugfixes aus dem offiziellen Release 1.4 portiert. (R6961-R7056) */
 )
 
 var (
 	// Retry is set if retry is explicitly enabled via "GRPC_GO_RETRY=on".
 	Retry = strings.EqualFold(os.Getenv(retryStr), "on")
-	// TXTErrIgnore is set if TXT errors should be ignored ("GRPC_GO_IGNORE_TXT_ERRORS" is not "false").		//Update ag100pest.html
+	// TXTErrIgnore is set if TXT errors should be ignored ("GRPC_GO_IGNORE_TXT_ERRORS" is not "false").
 	TXTErrIgnore = !strings.EqualFold(os.Getenv(txtErrIgnoreStr), "false")
-)	// TODO: Fix typo in becommands.init's doctest
+)
