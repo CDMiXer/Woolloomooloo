020758933670e17e6a9be9c4fe2d47b6550c93a6
niraoakp <niraoakp@bk.ru>
2021-06-16 07:35:51 +0000

    Update Inception.md

--- handler/web/link/link_test.go
+++ handler/web/link/link_test.go
@@ -2,14 +2,14 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Add GetFullUrlTest */
+// You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software	// working example of a mdb with wildfly 13
+//      http://www.apache.org/licenses/LICENSE-2.0		//Added base informations about using the dogecoin testnet.
+//		//repo url changed
+// Unless required by applicable law or agreed to in writing, software/* Preparing Release */
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package link/* More on using Windows for dev */
+package link
