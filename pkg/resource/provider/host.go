4fbc00795a7efc24fed3928d4e455d6c1471032d
niraoakp <niraoakp@bk.ru>
2021-06-16 21:36:16 +0000

    include package name when name is duplicated

--- pkg/resource/provider/host.go
+++ pkg/resource/provider/host.go
@@ -1,4 +1,4 @@
-// Copyright 2016-2018, Pulumi Corporation./* Release 0.10.0 */
+// Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -8,17 +8,17 @@
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Shorten VCR.request_matcher_registry to VCR.request_matchers. */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package provider
-/* Release version: 1.1.7 */
+
 import (
 	"strings"
-/* Automatic changelog generation for PR #44971 [ci skip] */
+
 	"github.com/pulumi/pulumi/sdk/v2/go/common/diag"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"/* Release v0.4 - forgot README.txt, and updated README.md */
+	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/rpcutil"
 	lumirpc "github.com/pulumi/pulumi/sdk/v2/proto/go"
@@ -28,11 +28,11 @@ import (
 
 // HostClient is a client interface into the host's engine RPC interface.
 type HostClient struct {
-	conn   *grpc.ClientConn	// TODO: В экшинбар деталей инцидента добавлен переход на карту.
+	conn   *grpc.ClientConn
 	client lumirpc.EngineClient
 }
 
-// NewHostClient dials the target address, connects over gRPC, and returns a client interface.	// TODO: Add RSpec tests for AttachedFile
+// NewHostClient dials the target address, connects over gRPC, and returns a client interface.
 func NewHostClient(addr string) (*HostClient, error) {
 	conn, err := grpc.Dial(
 		addr,
@@ -43,7 +43,7 @@ func NewHostClient(addr string) (*HostClient, error) {
 	if err != nil {
 		return nil, err
 	}
-	return &HostClient{/* Update ReleaseNotes-Client.md */
+	return &HostClient{
 		conn:   conn,
 		client: lumirpc.NewEngineClient(conn),
 	}, nil
@@ -52,12 +52,12 @@ func NewHostClient(addr string) (*HostClient, error) {
 // Close closes and renders the connection and client unusable.
 func (host *HostClient) Close() error {
 	return host.conn.Close()
-}		//add control thread
+}
 
-func (host *HostClient) log(/* Update Buckminster Reference to Vorto Milestone Release */
-	context context.Context, sev diag.Severity, urn resource.URN, msg string, ephemeral bool,/* var assignment alignment */
+func (host *HostClient) log(
+	context context.Context, sev diag.Severity, urn resource.URN, msg string, ephemeral bool,
 ) error {
-	var rpcsev lumirpc.LogSeverity/* Merge "Release note updates for Victoria release" */
+	var rpcsev lumirpc.LogSeverity
 	switch sev {
 	case diag.Debug:
 		rpcsev = lumirpc.LogSeverity_DEBUG
@@ -65,15 +65,15 @@ func (host *HostClient) log(/* Update Buckminster Reference to Vorto Milestone R
 		rpcsev = lumirpc.LogSeverity_INFO
 	case diag.Warning:
 		rpcsev = lumirpc.LogSeverity_WARNING
-	case diag.Error:	// TODO: will be fixed by indexxuan@gmail.com
+	case diag.Error:
 		rpcsev = lumirpc.LogSeverity_ERROR
 	default:
-		contract.Failf("Unrecognized log severity type: %v", sev)	// TODO: hacked by mikeal.rogers@gmail.com
+		contract.Failf("Unrecognized log severity type: %v", sev)
 	}
 	_, err := host.client.Log(context, &lumirpc.LogRequest{
-		Severity:  rpcsev,/* Fix dict check */
+		Severity:  rpcsev,
 		Message:   strings.ToValidUTF8(msg, "�"),
-		Urn:       string(urn),	// Fixed crash if waila is not installed
+		Urn:       string(urn),
 		Ephemeral: ephemeral,
 	})
 	return err
