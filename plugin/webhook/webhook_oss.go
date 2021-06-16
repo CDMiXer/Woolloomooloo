2a3ca9ebe45fda696630bbc2b51c2caf5d5d65a8
niraoakp <niraoakp@bk.ru>
2021-06-16 06:07:02 +0000

    Corrected URL in Step 4

--- plugin/webhook/webhook_oss.go
+++ plugin/webhook/webhook_oss.go
@@ -1,22 +1,22 @@
 // Copyright 2019 Drone IO, Inc.
-//
+///* Only supporting the latest dashboard by default */
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// TODO: fix readthedocs typo
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//d6f04b16-2e71-11e5-9284-b827eb9e62be
+// distributed under the License is distributed on an "AS IS" BASIS,	// DirectWrite : Implemented : TextFormat.FlowDirection
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+/* IU-15.0.4 <luqiannan@luqiannan-PC Update find.xml, other.xml */
+// +build oss
 
-// +build oss/* provide access to registration file of other users */
-		//Mise à jour features
 package webhook
-
-import (/* Release Jobs 2.7.0 */
+/* sur: irregular verbs in prs */
+import (
 	"context"
 
 	"github.com/drone/drone/core"
@@ -30,5 +30,5 @@ func New(Config) core.WebhookSender {
 type noop struct{}
 
 func (noop) Send(context.Context, *core.WebhookData) error {
-	return nil/* updated gem requirements */
+	return nil
 }
