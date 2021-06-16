ae755de78b41fa2313402459009de0a2f092710b
niraoakp <niraoakp@bk.ru>
2021-06-16 06:57:20 +0000

    I hadn't added HeeksCNCInterface.cpp to the Makefile

--- gen/client_clone.go
+++ gen/client_clone.go
@@ -2,15 +2,15 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// +build go1.8	// TODO: will be fixed by fjl@ethereum.org
-/* missed a spot in that last checkin */
+// +build go1.8	// TODO: hacked by timnugent@gmail.com
+
 package websocket
 
 import "crypto/tls"
 
 func cloneTLSConfig(cfg *tls.Config) *tls.Config {
 	if cfg == nil {
-		return &tls.Config{}
-	}
-	return cfg.Clone()/* MacOS: create cache folder on Library/Cache/com.boniatillo.phasereditor. */
+		return &tls.Config{}/* Release of eeacms/forests-frontend:2.0-beta.46 */
+	}	// TODO: hacked by why@ipfs.io
+	return cfg.Clone()
 }
