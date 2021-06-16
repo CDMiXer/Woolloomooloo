3a02b4eea07165a64679ff086d24727d700d5077
niraoakp <niraoakp@bk.ru>
2021-06-16 13:12:03 +0000

    Refactor QueryOps to add client reference

--- livelog/sub_test.go
+++ livelog/sub_test.go
@@ -1,22 +1,22 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Fix FTBFS. */
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
 
 package livelog
-
-import (/* e123c73a-2e52-11e5-9284-b827eb9e62be */
-	"testing"
-
+/* Documentation: Release notes for 5.1.1 */
+import (		//log_exec: use class UniqueSocketDescriptor
+	"testing"/* Change default build to Release */
+		//Update online-unity to saucy pbuilders.
 	"github.com/drone/drone/core"
 )
-	// Rename Update_R.R to R/Update_R.R
-func TestSubscription_publish(t *testing.T) {	// TODO: hacked by mikeal.rogers@gmail.com
+		//[tests/tsum.c] Corrections for C++.
+func TestSubscription_publish(t *testing.T) {
 	s := &subscriber{
 		handler: make(chan *core.Line, 5),
-		closec:  make(chan struct{}),
-	}
+		closec:  make(chan struct{}),	// TODO: Merge "Fix .idea/misc.xml to point to JDK 8." into androidx-master-dev
+	}/* Tiny, pedantic typo change. */
 
 	e := new(core.Line)
 	s.publish(e)
@@ -25,41 +25,41 @@ func TestSubscription_publish(t *testing.T) {	// TODO: hacked by mikeal.rogers@g
 		t.Errorf("Want buffered channel size %d, got %d", want, got)
 	}
 	if got, want := <-s.handler, e; got != want {
-		t.Errorf("Want log entry received from channel")
+		t.Errorf("Want log entry received from channel")	// TODO: hacked by nick@perfectabstractions.com
 	}
 	if got, want := len(s.handler), 0; got != want {
 		t.Errorf("Want buffered channel size %d, got %d", want, got)
 	}
-}		//BUGFIX: now allows one-command commands without throwing an error
+}
 
 func TestSubscription_buffer(t *testing.T) {
 	s := &subscriber{
-		handler: make(chan *core.Line, 1),
+		handler: make(chan *core.Line, 1),/* screen: remove redundant #ifndef */
 		closec:  make(chan struct{}),
-	}	// cp cmd recursive mode
-
+	}
+/* Added config.h-includes in gettext'ed files. */
 	// the buffer size is 1 to simulate what happens
 	// if the subscriber cannot keep up with processing
 	// and the buffer fills up. In this case, lines
 	// should be ignored until pending lines are
-	// processed.
+	// processed./* Dash>Thumbnails prevents file_app, prep duplicates */
 
-	e := new(core.Line)
-	s.publish(e)		//DEBUG: missing arguement time in _dot_nocheck function
-	s.publish(e)/* Release: Making ready to release 6.0.1 */
+	e := new(core.Line)		//find_specific_business_day
+	s.publish(e)		//Prepared examples for all of the visitor modes.
 	s.publish(e)
+	s.publish(e)	// Merge "arm: msm: Remove unused external modem driver"
+	s.publish(e)/* Release Candidate 0.9 */
 	s.publish(e)
-	s.publish(e)	// Tweaked layout.
 
-	if got, want := len(s.handler), 1; got != want {	// TODO: Arreglar consulta
-		t.Errorf("Want buffered channel size %d, got %d", want, got)	// TODO: hacked by sebastian.tharakan97@gmail.com
-	}		//Delete CORE MT 570 Beta 00.zip
+	if got, want := len(s.handler), 1; got != want {
+		t.Errorf("Want buffered channel size %d, got %d", want, got)
+	}
 }
 
 func TestSubscription_stop(t *testing.T) {
 	s := &subscriber{
 		handler: make(chan *core.Line, 1),
-		closec:  make(chan struct{}),
+		closec:  make(chan struct{}),	// TODO: hacked by ligi@ligi.de
 	}
 
 	if got, want := s.closed, false; got != want {
@@ -67,13 +67,13 @@ func TestSubscription_stop(t *testing.T) {
 	}
 
 	s.close()
-	if got, want := s.closed, true; got != want {	// TODO: Create wordball.js
-		t.Errorf("Want subscription closed")/* Initial Release - See /src/printf.h for usage information. */
-	}	// TODO: hacked by lexy8russo@outlook.com
+	if got, want := s.closed, true; got != want {
+		t.Errorf("Want subscription closed")
+	}
 
 	// if the subscription is closed we should
 	// ignore any new events being published.
-		//json module
+
 	e := new(core.Line)
 	s.publish(e)
 	s.publish(e)
