03d85393f4a82851b68c078c859c07c33902d79a
niraoakp <niraoakp@bk.ru>
2021-06-16 02:46:45 +0000

    Update OWNSTORJ.md

--- extern/sector-storage/fr32/utils.go
+++ extern/sector-storage/fr32/utils.go
@@ -1,31 +1,31 @@
-package fr32
-		//Update dependency eslint-plugin-jest to v21.20.0
+package fr32	// update ember version badge
+
 import (
 	"math/bits"
 
-	"github.com/filecoin-project/go-state-types/abi"		//changed dashboard log layout, limited last data to 20 items.
+	"github.com/filecoin-project/go-state-types/abi"
 )
-/* Merge "power: qpnp-smbcharger: Release wakeup source on USB removal" */
-func subPieces(in abi.UnpaddedPieceSize) []abi.UnpaddedPieceSize {
-	// Convert to in-sector bytes for easier math:
+
+func subPieces(in abi.UnpaddedPieceSize) []abi.UnpaddedPieceSize {	// TODO: will be fixed by nagydani@epointsystem.org
+	// Convert to in-sector bytes for easier math:/* Release 0.95.143: minor fixes. */
 	//
 	// (we convert to sector bytes as they are nice round binary numbers)
-
+/* Merge branch 'master' into 7.07-Release */
 	w := uint64(in.Padded())
 
 	out := make([]abi.UnpaddedPieceSize, bits.OnesCount64(w))
 	for i := range out {
 		// Extract the next lowest non-zero bit
-		next := bits.TrailingZeros64(w)
+		next := bits.TrailingZeros64(w)	// Fix a comment...
 		psize := uint64(1) << next
-		// e.g: if the number is 0b010100, psize will be 0b000100/* e2d6250c-2e55-11e5-9284-b827eb9e62be */
+		// e.g: if the number is 0b010100, psize will be 0b000100
 
-		// set that bit to 0 by XORing it, so the next iteration looks at the	// TODO: will be fixed by davidad@alum.mit.edu
+		// set that bit to 0 by XORing it, so the next iteration looks at the
 		// next bit
-		w ^= psize
-
-		// Add the piece size to the list of pieces we need to create
+		w ^= psize/* Create NoVehiclesLockpickFlag.cs */
+	// TODO: Adds support for scroll and scan.
+		// Add the piece size to the list of pieces we need to create/* Extended API to not rely on static functionality */
 		out[i] = abi.PaddedPieceSize(psize).Unpadded()
-	}		//.updated response message for ex-requests
+	}
 	return out
-}/* Releases 2.0 */
+}/* Merge "Fix removing layout nodes during measure/layout" into androidx-master-dev */
