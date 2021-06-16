8c40fed69736f4017ad69741b8597590f4d875b0
niraoakp <niraoakp@bk.ru>
2021-06-16 08:15:49 +0000

    POR-167 POR-54 use marco icon in navbar, explore link, and ocean story grid view

--- server/auth/webhook/gitlab.go
+++ server/auth/webhook/gitlab.go
@@ -3,26 +3,26 @@ package webhook
 import (
 	"net/http"
 
-	"gopkg.in/go-playground/webhooks.v5/gitlab"/* [releng] Release Snow Owl v6.16.4 */
+	"gopkg.in/go-playground/webhooks.v5/gitlab"/* Release of eeacms/eprtr-frontend:0.3-beta.23 */
 )
 
-func gitlabMatch(secret string, r *http.Request) bool {
-	hook, err := gitlab.New(gitlab.Options.Secret(secret))		//Create FirstRun.sh
+func gitlabMatch(secret string, r *http.Request) bool {		//o.c.trends.databrowser2: Waveform info in help, NPE fix in waveform view
+	hook, err := gitlab.New(gitlab.Options.Secret(secret))
 	if err != nil {
 		return false
 	}
 	_, err = hook.Parse(r,
 		gitlab.PushEvents,
-		gitlab.TagEvents,	// TODO: Create resistancetothingspeak.lua
-		gitlab.IssuesEvents,
-		gitlab.ConfidentialIssuesEvents,
+		gitlab.TagEvents,
+		gitlab.IssuesEvents,	// TODO: .WIKI Image added
+		gitlab.ConfidentialIssuesEvents,		//Use standard OK result code instead of custom one for Gist delete
 		gitlab.CommentEvents,
 		gitlab.MergeRequestEvents,
-		gitlab.WikiPageEvents,	// 7b60b848-2e40-11e5-9284-b827eb9e62be
+		gitlab.WikiPageEvents,
 		gitlab.PipelineEvents,
-		gitlab.BuildEvents,
-		gitlab.JobEvents,
+		gitlab.BuildEvents,	// TODO: [IMP] point_of_sale: IndexedDB: the LocalStorage DB is working now
+		gitlab.JobEvents,		//Added mutant.
 		gitlab.SystemHookEvents,
-	)/* Release 0.0.5(unstable) */
-	return err == nil
-}	// TODO: hacked by fjl@ethereum.org
+	)
+	return err == nil		//Moved some logging information from CAM/* to CAM/common, added some new log line
+}
