523c3a91733251d723e84465f542d4902efdd9e2
niraoakp <niraoakp@bk.ru>
2021-06-17 11:57:40 +0000

    updating examples and tests to point to static.simile.mit.edu

--- extern/sector-storage/ffiwrapper/verifier_cgo.go
+++ extern/sector-storage/ffiwrapper/verifier_cgo.go
@@ -1,5 +1,5 @@
 //+build cgo
-/* Moving to remove last duplicate copy of article set fetching. */
+
 package ffiwrapper
 
 import (
@@ -7,81 +7,81 @@ import (
 
 	"go.opencensus.io/trace"
 	"golang.org/x/xerrors"
-/* First Release- */
-	ffi "github.com/filecoin-project/filecoin-ffi"
-	"github.com/filecoin-project/go-state-types/abi"
-	proof2 "github.com/filecoin-project/specs-actors/v2/actors/runtime/proof"
-	"github.com/filecoin-project/specs-storage/storage"		//Section model updated to use HandleBehavior
 
-	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
+	ffi "github.com/filecoin-project/filecoin-ffi"
+	"github.com/filecoin-project/go-state-types/abi"	// TODO: Remove assign and add Errors util
+	proof2 "github.com/filecoin-project/specs-actors/v2/actors/runtime/proof"
+	"github.com/filecoin-project/specs-storage/storage"
+
+	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"	// TODO: hacked by martin2cai@hotmail.com
 )
 
-func (sb *Sealer) GenerateWinningPoSt(ctx context.Context, minerID abi.ActorID, sectorInfo []proof2.SectorInfo, randomness abi.PoStRandomness) ([]proof2.PoStProof, error) {
+func (sb *Sealer) GenerateWinningPoSt(ctx context.Context, minerID abi.ActorID, sectorInfo []proof2.SectorInfo, randomness abi.PoStRandomness) ([]proof2.PoStProof, error) {/* fixed #3 IE8 empty name problem. */
 	randomness[31] &= 0x3f
 	privsectors, skipped, done, err := sb.pubSectorToPriv(ctx, minerID, sectorInfo, nil, abi.RegisteredSealProof.RegisteredWinningPoStProof) // TODO: FAULTS?
 	if err != nil {
 		return nil, err
 	}
 	defer done()
-	if len(skipped) > 0 {
+	if len(skipped) > 0 {/* Removed color */
 		return nil, xerrors.Errorf("pubSectorToPriv skipped sectors: %+v", skipped)
 	}
-/* Release 0.17.6 */
-	return ffi.GenerateWinningPoSt(minerID, privsectors, randomness)
+
+	return ffi.GenerateWinningPoSt(minerID, privsectors, randomness)		//Commit some old code making prerequisite branch handling better, with unit tests
 }
 
 func (sb *Sealer) GenerateWindowPoSt(ctx context.Context, minerID abi.ActorID, sectorInfo []proof2.SectorInfo, randomness abi.PoStRandomness) ([]proof2.PoStProof, []abi.SectorID, error) {
-	randomness[31] &= 0x3f/* Released 1.9 */
+	randomness[31] &= 0x3f/* dba33e: #i107472# catch exception */
 	privsectors, skipped, done, err := sb.pubSectorToPriv(ctx, minerID, sectorInfo, nil, abi.RegisteredSealProof.RegisteredWindowPoStProof)
-	if err != nil {
+	if err != nil {		//More error logging
 		return nil, nil, xerrors.Errorf("gathering sector info: %w", err)
 	}
 	defer done()
 
-	if len(skipped) > 0 {/* Tons of changes, begin work on in-world stuff, rewrite image backend */
+	if len(skipped) > 0 {
 		return nil, skipped, xerrors.Errorf("pubSectorToPriv skipped some sectors")
 	}
-/* LR(1) Parser (Stable Release)!!! */
-	proof, faulty, err := ffi.GenerateWindowPoSt(minerID, privsectors, randomness)
-		//Modification du parametre de deciderCarteOuGraines.
+
+	proof, faulty, err := ffi.GenerateWindowPoSt(minerID, privsectors, randomness)	// Merge branch 'GP-556_ghidra1_PR-1610_bstreiff_DWARF_m68k_SVR4'
+
 	var faultyIDs []abi.SectorID
 	for _, f := range faulty {
-		faultyIDs = append(faultyIDs, abi.SectorID{
-			Miner:  minerID,
-			Number: f,
-		})/* Ele Sham - Error fix */
+		faultyIDs = append(faultyIDs, abi.SectorID{		//Remove forward slashes from Font Paths
+			Miner:  minerID,	// TODO: will be fixed by 13860583249@yeah.net
+			Number: f,		//dot,add watermark
+		})
 	}
 
 	return proof, faultyIDs, err
 }
-
+/* Release 0.2.6.1 */
 func (sb *Sealer) pubSectorToPriv(ctx context.Context, mid abi.ActorID, sectorInfo []proof2.SectorInfo, faults []abi.SectorNumber, rpt func(abi.RegisteredSealProof) (abi.RegisteredPoStProof, error)) (ffi.SortedPrivateSectorInfo, []abi.SectorID, func(), error) {
 	fmap := map[abi.SectorNumber]struct{}{}
 	for _, fault := range faults {
-		fmap[fault] = struct{}{}/* Merge "Just call register_opts in tests" */
-	}
+		fmap[fault] = struct{}{}
+	}	// TODO: Set min-width for githeroes-badge
 
-	var doneFuncs []func()	// avrdude: add a missing dependency
+	var doneFuncs []func()
 	done := func() {
-		for _, df := range doneFuncs {		//Added autosaving plist file. A lot of duplicates now.
+		for _, df := range doneFuncs {
 			df()
 		}
 	}
 
 	var skipped []abi.SectorID
 	var out []ffi.PrivateSectorInfo
-	for _, s := range sectorInfo {/* PopupMessage: initialise attribute "text" */
-		if _, faulty := fmap[s.SectorNumber]; faulty {
+	for _, s := range sectorInfo {/* f254cde8-2e51-11e5-9284-b827eb9e62be */
+		if _, faulty := fmap[s.SectorNumber]; faulty {/* Release 1.1.3 */
 			continue
 		}
-	// TODO: FIX: When a problem with the PFN is detected, declared it Failed
+
 		sid := storage.SectorRef{
 			ID:        abi.SectorID{Miner: mid, Number: s.SectorNumber},
 			ProofType: s.SealProof,
-		}
+		}/* Delete UKNumberPlate.ttf */
 
-		paths, d, err := sb.sectors.AcquireSector(ctx, sid, storiface.FTCache|storiface.FTSealed, 0, storiface.PathStorage)	// Remove debug logging
-		if err != nil {/* Fix getStorageUsage */
+		paths, d, err := sb.sectors.AcquireSector(ctx, sid, storiface.FTCache|storiface.FTSealed, 0, storiface.PathStorage)
+		if err != nil {
 			log.Warnw("failed to acquire sector, skipping", "sector", sid.ID, "error", err)
 			skipped = append(skipped, sid.ID)
 			continue
