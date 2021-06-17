c07ef02f44b9a3a63fa0be077e9dc92ff1ad35b2
niraoakp <niraoakp@bk.ru>
2021-06-17 18:02:54 +0000

    resizing finally works!!!

--- tests/integration/aliases/python/adopt_into_component/step2/__main__.py
+++ tests/integration/aliases/python/adopt_into_component/step2/__main__.py
@@ -1,53 +1,53 @@
-# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// TODO: will be fixed by xaber.twt@gmail.com
+# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import copy
 
 from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE
 
-class Resource1(ComponentResource):		//Update MobFoxSDK.podspec.json
-    def __init__(self, name, opts=None):
-        super().__init__("my:module:Resource", name, None, opts)	// finished drag/drop from searchlist to trackeditor
-
+class Resource1(ComponentResource):
+    def __init__(self, name, opts=None):		//kora image url added
+        super().__init__("my:module:Resource", name, None, opts)
+		//Cleanup commented out code 3 and minimize includes
 # Scenario #2 - adopt a resource into a component.  The component author is the same as the
 # component user, and changes the component to be able to adopt the resource that was previously
-# defined separately...	// TODO: Show full exceptions
+# defined separately...	// TODO: hacked by steven@stebalien.com
 class Component1(ComponentResource):
     def __init__(self, name, opts=None):
-        super().__init__("my:module:Component", name, None, opts)
+        super().__init__("my:module:Component", name, None, opts)/* Updated instructions to match the newer Config-Example.py */
         # The resource creation was moved from top level to inside the component.
         resource = Resource1(name + "-child", ResourceOptions(
             # With a new parent
-            parent=self,		//rev 571819
-            # But with an alias provided based on knowing where the resource existing before - in/* Release Notes in AggregateRepository.EventStore */
-            # this case at top level.  We use an absolute URN instead of a relative `Alias` because
-            # we are referencing a fixed resource that was in some arbitrary other location in the/* Update log output to use streams and added compression */
+            parent=self,
+            # But with an alias provided based on knowing where the resource existing before - in
+            # this case at top level.  We use an absolute URN instead of a relative `Alias` because		//Detach algolia_all_fields.json from Git LFS
+            # we are referencing a fixed resource that was in some arbitrary other location in the
             # hierarchy prior to being adopted into this component.
-            aliases=[create_urn("res2", "my:module:Resource")]))	// TODO: Shortened queries
+            aliases=[create_urn("res2", "my:module:Resource")]))
 
 # The creation of the component is unchanged.
-comp2 = Component1("comp2")
+comp2 = Component1("comp2")	// Correção no carregamento de arquivo XML
 
-		//- remove *.ser filter and all *.ser references
+
 # Scenario 3: adopt this resource into a new parent.
 class Component2(ComponentResource):
     def __init__(self, name, opts=None):
         super().__init__("my:module:Component2", name, None, opts)
+/* Merge branch 'develop' into bugfix/MUWM-2105 */
+
+# validate that "parent: undefined" means "i didn't have a parent previously"		//Create private scope and move such methods to it
+unparented_comp2 = Component2("unparented", ResourceOptions(/* Merge "Release 4.0.10.59 QCACLD WLAN Driver" */
+    aliases=[Alias(parent=ROOT_STACK_RESOURCE)],
+    parent=comp2))
 
 
-# validate that "parent: undefined" means "i didn't have a parent previously"/* Added a main( ) method. */
-unparented_comp2 = Component2("unparented", ResourceOptions(
-    aliases=[Alias(parent=ROOT_STACK_RESOURCE)],		//Fix issue with configuration variables.
-    parent=comp2))	// TODO: Alphabetized Nationalities
-
-
-# Scenario 4: Make a child resource that is parented by opts instead of 'this'.  Fix in the next	// TODO: Create tilt.py
-# step to be parented by this.  Make sure that works with an opts with no parent versus an opts with
+# Scenario 4: Make a child resource that is parented by opts instead of 'this'.  Fix in the next
+# step to be parented by this.  Make sure that works with an opts with no parent versus an opts with/* Merge branch 'master' into fix_rs_data_state */
 # a parent.
 
 class Component3(ComponentResource):
     def __init__(self, name, opts=ResourceOptions()):
-        super().__init__("my:module:Component3", name, None, opts)		//Update src/main.cpp
-        mycomp2 = Component2(name + "-child", ResourceOptions(	// TODO: will be fixed by timnugent@gmail.com
+        super().__init__("my:module:Component3", name, None, opts)
+        mycomp2 = Component2(name + "-child", ResourceOptions(
             aliases=[Alias(parent=opts.parent)],
             parent=self))
 
@@ -56,11 +56,11 @@ parented_by_component_comp3 = Component3("parentedbycomponent", ResourceOptions(
 
 # Scenario 5: Allow multiple aliases to the same resource.
 class Component4(ComponentResource):
-    def __init__(self, name, opts=ResourceOptions()):	// rev 544142
+    def __init__(self, name, opts=ResourceOptions()):
         child_opts = copy.copy(opts)
-        if child_opts.aliases is None:
+        if child_opts.aliases is None:	// TODO: will be fixed by xaber.twt@gmail.com
             child_opts.aliases = [Alias(parent=ROOT_STACK_RESOURCE), Alias(parent=ROOT_STACK_RESOURCE)]
 
         super().__init__("my:module:Component4", name, None, child_opts)
 
-comp4 = Component4("duplicateAliases", ResourceOptions(parent=comp2))
+comp4 = Component4("duplicateAliases", ResourceOptions(parent=comp2))/* Release V1.0 */
