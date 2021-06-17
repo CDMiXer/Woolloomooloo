3668963131b0518e392c3fa77679bf2bb33951b7
niraoakp <niraoakp@bk.ru>
2021-06-17 22:49:54 +0000

    Refactor transaction to accept sending individual requests

--- pkg/codegen/nodejs/doc_test.go
+++ pkg/codegen/nodejs/doc_test.go
@@ -1,22 +1,22 @@
 // Copyright 2016-2020, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");		//Big ass refactoring and cleanup. Ping suite not finished yet
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Delete numberStructure.PNG */
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-///* #301 generate ModifiableX.toImmutable even if copy = false */
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.	// TODO: hacked by steven@stebalien.com
+// limitations under the License.
 
 // Pulling out some of the repeated strings tokens into constants would harm readability, so we just ignore the
 // goconst linter's warning.
 //
-// nolint: lll, goconst		//Add new notebook.
-package nodejs	// TODO: hacked by aeongrp@outlook.com
+// nolint: lll, goconst
+package nodejs
 
 import (
 	"testing"
@@ -32,13 +32,13 @@ var testPackageSpec = schema.PackageSpec{
 		ModuleFormat: "(.*)(?:/[^/]*)",
 	},
 	Types: map[string]schema.ComplexTypeSpec{
-		"aws:s3/BucketCorsRule:BucketCorsRule": {		//Fixed Linux compile error
+		"aws:s3/BucketCorsRule:BucketCorsRule": {
 			ObjectTypeSpec: schema.ObjectTypeSpec{
-				Description: "The resource options object.",/* Create de.ui_tabs.php */
+				Description: "The resource options object.",
 				Type:        "object",
 				Properties: map[string]schema.PropertySpec{
 					"stringProp": {
-						Description: "A string prop.",		//Rename electronicstest.c to dbug-test/electronicstest.c
+						Description: "A string prop.",
 						TypeSpec: schema.TypeSpec{
 							Type: "string",
 						},
@@ -48,17 +48,17 @@ var testPackageSpec = schema.PackageSpec{
 		},
 	},
 	Resources: map[string]schema.ResourceSpec{
-		"aws:s3/bucket:Bucket": {	// TODO: Update msLandscapeSchematic.html
-			InputProperties: map[string]schema.PropertySpec{/* Release 0.2.12 */
+		"aws:s3/bucket:Bucket": {
+			InputProperties: map[string]schema.PropertySpec{
 				"corsRules": {
-					TypeSpec: schema.TypeSpec{/* Trying to get temperature to work. */
+					TypeSpec: schema.TypeSpec{
 						Ref: "#/types/aws:s3/BucketCorsRule:BucketCorsRule",
-					},		//Create B827EBFFFEFAFEBA.json
+					},
 				},
-,}			
-		},/* Add continent to the location data */
+			},
+		},
 	},
-}/* Release 1.0.14.0 */
+}
 
 func getTestPackage(t *testing.T) *schema.Package {
 	t.Helper()
