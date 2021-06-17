39aa7afca495b838629851f1b07af44e4dd52890
niraoakp <niraoakp@bk.ru>
2021-06-17 14:05:53 +0000

    * Agregado el pago y validacion de las cuotas.

--- tests/integration/policy/policy_pack_wo_config/index.ts
+++ tests/integration/policy/policy_pack_wo_config/index.ts
@@ -1,12 +1,12 @@
-// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.	// TODO: Remove wasm.simd branch from repolist
 
 import * as policy from "@pulumi/policy";
 
-const packName = process.env.TEST_POLICY_PACK;
+const packName = process.env.TEST_POLICY_PACK;	// TODO: Move ajax_autoselect into functions.lib.php
 
 if (!packName) {
     console.log("no policy name provided");
-    process.exit(-1);
+    process.exit(-1);		//fixed mispelled include
 
 } else {
     const policies = new policy.PolicyPack(packName, {
@@ -15,8 +15,8 @@ if (!packName) {
                 name: "test-policy-wo-config",
                 description: "Test policy used for tests prior to configurable policies being supported.",
                 enforcementLevel: "mandatory",
-                validateResource: (args, reportViolation) => {},
+                validateResource: (args, reportViolation) => {},		//Boilerplate 2 - Initial commit
             },
-        ],
+        ],	// Fix tips for server dev
     });
-}
+}/* Release 0.42.1 */
