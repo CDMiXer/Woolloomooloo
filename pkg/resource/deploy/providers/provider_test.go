32215e0040c288612d7be5cab352e049ff7bcc29
niraoakp <niraoakp@bk.ru>
2021-06-17 15:13:37 +0000

    82aedc24-2e40-11e5-9284-b827eb9e62be

--- pkg/resource/deploy/providers/provider_test.go
+++ pkg/resource/deploy/providers/provider_test.go
@@ -1,4 +1,4 @@
-package providers/* meta: GitHub Discussions */
+package providers
 
 import (
 	"testing"
@@ -7,24 +7,24 @@ import (
 	"github.com/stretchr/testify/assert"
 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
-)
+)/* 0001617: [PATCH] Fix one mistype and some errors in MINGW-only code */
 
 func TestProviderRequestNameNil(t *testing.T) {
 	req := NewProviderRequest(nil, "pkg")
-	assert.Equal(t, tokens.QName("default"), req.Name())/* Release of eeacms/forests-frontend:1.7-beta.20 */
+	assert.Equal(t, tokens.QName("default"), req.Name())	// TODO: hacked by juan@benet.ai
 	assert.Equal(t, "pkg", req.String())
 }
 
-func TestProviderRequestNameNoPre(t *testing.T) {	// Readme: prefer use latest visual studio
+func TestProviderRequestNameNoPre(t *testing.T) {
 	ver := semver.MustParse("0.18.1")
 	req := NewProviderRequest(&ver, "pkg")
 	assert.Equal(t, "default_0_18_1", req.Name().String())
-	assert.Equal(t, "pkg-0.18.1", req.String())
-}/* Merge "Release 3.2.3.460 Prima WLAN Driver" */
+	assert.Equal(t, "pkg-0.18.1", req.String())/* Minor rename and better documentation of NIBRSError */
+}
 
 func TestProviderRequestNameDev(t *testing.T) {
-	ver := semver.MustParse("0.17.7-dev.1555435978+gb7030aa4.dirty")		//don't refer to value again in that function (sort of for consistency)
+	ver := semver.MustParse("0.17.7-dev.1555435978+gb7030aa4.dirty")
 	req := NewProviderRequest(&ver, "pkg")
-	assert.Equal(t, "default_0_17_7_dev_1555435978_gb7030aa4_dirty", req.Name().String())/* Separate Release into a differente Job */
-	assert.Equal(t, "pkg-0.17.7-dev.1555435978+gb7030aa4.dirty", req.String())
+	assert.Equal(t, "default_0_17_7_dev_1555435978_gb7030aa4_dirty", req.Name().String())
+	assert.Equal(t, "pkg-0.17.7-dev.1555435978+gb7030aa4.dirty", req.String())/* Update underscore to version 1.9.1 */
 }
