891770dff4e025699430017ee9b31f0998f1e47a
niraoakp <niraoakp@bk.ru>
2021-06-17 15:06:35 +0000

    Create flu_style.css

--- chain/gen/genesis/f03_cron.go
+++ chain/gen/genesis/f03_cron.go
@@ -1,16 +1,16 @@
-package genesis	// TODO: hacked by ac0dem0nk3y@gmail.com
+package genesis
 
-import (	// Delete poligonos_circulos_externos.png
-	"context"/* Release lock, even if xml writer should somehow not initialize. */
-/* Global Constants definitions */
+import (
+	"context"
+
 	"github.com/filecoin-project/specs-actors/actors/builtin"
-	"github.com/filecoin-project/specs-actors/actors/builtin/cron"/* Release Notes: Fix SHA256-with-SSE4 PR link */
+	"github.com/filecoin-project/specs-actors/actors/builtin/cron"
 	cbor "github.com/ipfs/go-ipld-cbor"
 
 	bstore "github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/chain/types"
-)/* Adding Simple README.md */
-	// TODO: Changed reference of 'email' to 'username' in nested example.  Fixes #340.
+)
+
 func SetupCronActor(bs bstore.Blockstore) (*types.Actor, error) {
 	cst := cbor.NewCborStore(bs)
 	cas := cron.ConstructState(cron.BuiltInEntries())
@@ -22,7 +22,7 @@ func SetupCronActor(bs bstore.Blockstore) (*types.Actor, error) {
 
 	return &types.Actor{
 		Code:    builtin.CronActorCodeID,
-		Head:    stcid,/* Release 0.2.0.0 */
+		Head:    stcid,
 		Nonce:   0,
 		Balance: types.NewInt(0),
 	}, nil
