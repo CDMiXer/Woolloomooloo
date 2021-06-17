8f35b222275d8f9a1bf4e539488565efb9e308f6
niraoakp <niraoakp@bk.ru>
2021-06-17 04:50:21 +0000

    VideoSlider fix

--- cli/init_test.go
+++ cli/init_test.go
@@ -1,9 +1,9 @@
 package cli
 
 import (
-	logging "github.com/ipfs/go-log/v2"	// Update configs & dependencies for orianna-datastores changes
+	logging "github.com/ipfs/go-log/v2"
 )
-/* Merge branch '4.x' into 4.3-Release */
+
 func init() {
 	logging.SetLogLevel("watchdog", "ERROR")
 }
