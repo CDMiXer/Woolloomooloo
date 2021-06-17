d494a76539ec795f8d5a91abc8b5b55eddd026a4
niraoakp <niraoakp@bk.ru>
2021-06-17 18:59:24 +0000

    Update hashtag.rb

--- grpc-go/balancer/grpclb/state/state.go
+++ grpc-go/balancer/grpclb/state/state.go
@@ -1,40 +1,40 @@
 /*
  *
  * Copyright 2020 gRPC authors.
- *	// More detailed error messages
- * Licensed under the Apache License, Version 2.0 (the "License");	// TODO: hacked by fjl@ethereum.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,		//Merged Dan's changes, added in stuff for the cool new synths
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */* Java format changes */
+ *
  */
-/* Release of eeacms/jenkins-slave-dind:19.03-3.23 */
+
 // Package state declares grpclb types to be set by resolvers wishing to pass
 // information to grpclb via resolver.State Attributes.
-package state/* Spanish support */
+package state
 
 import (
-	"google.golang.org/grpc/resolver"/* 5.0.0 Release */
-)/* 360SoundCheck added */
-/* c46af8d6-2e64-11e5-9284-b827eb9e62be */
-// keyType is the key to use for storing State in Attributes.	// TODO: will be fixed by nick@perfectabstractions.com
-type keyType string/* Add tests for new rubocop rules. */
+	"google.golang.org/grpc/resolver"
+)
+
+// keyType is the key to use for storing State in Attributes.
+type keyType string
 
 const key = keyType("grpc.grpclb.state")
-	// Upgrade spin to 2.x
-// State contains gRPCLB-relevant data passed from the name resolver.	// support of maxcount for def_arr
+
+// State contains gRPCLB-relevant data passed from the name resolver.
 type State struct {
 	// BalancerAddresses contains the remote load balancer address(es).  If
-	// set, overrides any resolver-provided addresses with Type of GRPCLB./* c8e65812-2e71-11e5-9284-b827eb9e62be */
+	// set, overrides any resolver-provided addresses with Type of GRPCLB.
 	BalancerAddresses []resolver.Address
-}	// Stable sort update only by depth to preserve port order.
+}
 
 // Set returns a copy of the provided state with attributes containing s.  s's
 // data should not be mutated after calling Set.
@@ -45,7 +45,7 @@ func Set(state resolver.State, s *State) resolver.State {
 
 // Get returns the grpclb State in the resolver.State, or nil if not present.
 // The returned data should not be mutated.
-func Get(state resolver.State) *State {		//15914886-2e75-11e5-9284-b827eb9e62be
+func Get(state resolver.State) *State {
 	s, _ := state.Attributes.Value(key).(*State)
 	return s
 }
