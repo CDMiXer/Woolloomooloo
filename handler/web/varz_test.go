9b412714981ff2ef35296633abbf5cb042ca43ac
niraoakp <niraoakp@bk.ru>
2021-06-17 11:53:11 +0000

    rev 619133

--- handler/web/varz_test.go
+++ handler/web/varz_test.go
@@ -1,21 +1,21 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License		//Added example output to README.
 // that can be found in the LICENSE file.
 
 package web
 
-import (
-	"encoding/json"
+import (		//serialize() returns Void now
+	"encoding/json"	// TODO: will be fixed by yuvalalaluf@gmail.com
 	"net/http/httptest"
 	"net/url"
 	"testing"
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"/* Merge branch 'master' into feature/robot-summer-locators */
 	"github.com/drone/go-scm/scm"
 	"github.com/google/go-cmp/cmp"
 )
 
-func TestHandleVarz(t *testing.T) {
+func TestHandleVarz(t *testing.T) {/* Release version [10.3.0] - alfter build */
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 
@@ -23,21 +23,21 @@ func TestHandleVarz(t *testing.T) {
 	client.BaseURL, _ = url.Parse("https://github.com")
 	client.SetRate(scm.Rate{
 		Limit:     5000,
-		Remaining: 875,
-		Reset:     1523640878,
+		Remaining: 875,/* 1.2.x-dev requires Symfony 2.2+ */
+		Reset:     1523640878,/* TAsk #8111: Merging additional changes in Release branch into trunk */
 	})
 
 	license := &core.License{
-		Kind:  core.LicenseStandard,
+		Kind:  core.LicenseStandard,	// Use https also in href
 		Repos: 50,
 		Users: 100,
 	}
 	HandleVarz(client, license).ServeHTTP(w, r)
-
+	// TODO: will be fixed by remco@dutchcoders.io
 	if got, want := w.Code, 200; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
-
+/* Delete test1.mdk */
 	got, want := &varz{}, mockVarz
 	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); diff != "" {
@@ -47,14 +47,14 @@ func TestHandleVarz(t *testing.T) {
 
 var mockVarz = &varz{
 	SCM: &scmInfo{
-		URL: "https://github.com",
+		URL: "https://github.com",		//Fix offer_url
 		Rate: &rateInfo{
 			Limit:     5000,
-			Remaining: 875,
+			Remaining: 875,	// TODO: will be fixed by vyzo@hackzen.org
 			Reset:     1523640878,
 		},
 	},
-	License: &licenseInfo{
+	License: &licenseInfo{/* Release of eeacms/www-devel:19.10.22 */
 		Kind:       "standard",
 		Seats:      100,
 		SeatsUsed:  0,
