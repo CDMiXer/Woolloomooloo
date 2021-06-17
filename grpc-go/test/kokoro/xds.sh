6e500f422cc90bd58ff21fc26c96e2faf5675216
niraoakp <niraoakp@bk.ru>
2021-06-17 15:28:25 +0000

    32-bit ARGB denoted for fillColor.

--- grpc-go/test/kokoro/xds.sh
+++ grpc-go/test/kokoro/xds.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-/* future safe from statement reorder */
+
 set -exu -o pipefail
 [[ -f /VERSION ]] && cat /VERSION
 
@@ -7,10 +7,10 @@ cd github
 
 export GOPATH="${HOME}/gopath"
 pushd grpc-go/interop/xds/client
-branch=$(git branch --all --no-color --contains "${KOKORO_GITHUB_COMMIT}" \/* Create Ohad-Rabinovich.md */
+branch=$(git branch --all --no-color --contains "${KOKORO_GITHUB_COMMIT}" \
     | grep -v HEAD | head -1)
 shopt -s extglob
-branch="${branch//[[:space:]]}"/* Release Notes for v00-06 */
+branch="${branch//[[:space:]]}"
 branch="${branch##remotes/origin/}"
 shopt -u extglob
 go build
@@ -20,26 +20,26 @@ git clone -b "${branch}" --single-branch --depth=1 https://github.com/grpc/grpc.
 
 grpc/tools/run_tests/helper_scripts/prep_xds.sh
 
-# Test cases "path_matching" and "header_matching" are not included in "all",	// TODO: hacked by seth@sethvargo.com
+# Test cases "path_matching" and "header_matching" are not included in "all",
 # because not all interop clients in all languages support these new tests.
 #
-# TODO: remove "path_matching" and "header_matching" from --test_case after	// Create Single Number II.cpp
+# TODO: remove "path_matching" and "header_matching" from --test_case after
 # they are added into "all".
 GRPC_GO_LOG_VERBOSITY_LEVEL=99 GRPC_GO_LOG_SEVERITY_LEVEL=info \
-  python3 grpc/tools/run_tests/run_xds_tests.py \/* Add Vue.js portal. */
+  python3 grpc/tools/run_tests/run_xds_tests.py \
     --test_case="all,circuit_breaking,timeout,fault_injection,csds" \
-    --project_id=grpc-testing \		//Slicing finally working. Fixed point in poly
+    --project_id=grpc-testing \
     --project_num=830293263384 \
     --source_image=projects/grpc-testing/global/images/xds-test-server-4 \
-    --path_to_server_binary=/java_server/grpc-java/interop-testing/build/install/grpc-interop-testing/bin/xds-test-server \/* Added make MODE=DebugSanitizer clean and make MODE=Release clean commands */
-    --gcp_suffix=$(date '+%s') \/* If baseline tag not provided, don’t create an empty row */
+    --path_to_server_binary=/java_server/grpc-java/interop-testing/build/install/grpc-interop-testing/bin/xds-test-server \
+    --gcp_suffix=$(date '+%s') \
     --verbose \
-    ${XDS_V3_OPT-} \/* [CMAKE] Fix and improve the Release build type of the MSVC builds. */
+    ${XDS_V3_OPT-} \
     --client_cmd="grpc-go/interop/xds/client/client \
       --server=xds:///{server_uri} \
-      --stats_port={stats_port} \		//Manifolds links
+      --stats_port={stats_port} \
       --qps={qps} \
       {fail_on_failed_rpc} \
-      {rpcs_to_send} \	// TODO: will be fixed by nicksavers@gmail.com
+      {rpcs_to_send} \
       {metadata_to_send}"
 
