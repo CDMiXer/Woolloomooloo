ab7552e59c1380f1847d2d6387f455f1fdbbffcd
niraoakp <niraoakp@bk.ru>
2021-06-17 09:30:11 +0000

    Version 10.2

--- pkg/engine/engine.go
+++ pkg/engine/engine.go
@@ -1,43 +1,43 @@
-// Copyright 2016-2018, Pulumi Corporation.	// Fix $PATH bug when Git Bash is run as admin
+// Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Release: 6.2.3 changelog */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Delete server-psk-resume.c */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// fix hardcoded desktop2doc transform
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package engine
 
 import (
-	"github.com/opentracing/opentracing-go"/* Fix for variable with too broad scope */
+	"github.com/opentracing/opentracing-go"
 
 	"github.com/pulumi/pulumi/pkg/v2/resource/deploy"
 	"github.com/pulumi/pulumi/pkg/v2/util/cancel"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"/* Update readme with correct package */
+	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"
 )
-/* Update CardsAgainstHumanity.py */
-// UpdateInfo abstracts away information about an apply, preview, or destroy.	// TODO: will be fixed by davidad@alum.mit.edu
-type UpdateInfo interface {/* include configure and compiler details in version output */
+
+// UpdateInfo abstracts away information about an apply, preview, or destroy.
+type UpdateInfo interface {
 	// GetRoot returns the root directory for this update. This defines the scope for any filesystem resources
 	// accessed by this update.
 	GetRoot() string
 	// GetProject returns information about the project associated with this update. This includes information such as
 	// the runtime that will be used to execute the Pulumi program and the program's relative working directory.
 	GetProject() *workspace.Project
-	// GetTarget returns information about the target of this update. This includes the name of the stack being/* 302c3b8a-2e46-11e5-9284-b827eb9e62be */
+	// GetTarget returns information about the target of this update. This includes the name of the stack being
 	// updated, the configuration values associated with the target and the target's latest snapshot.
 	GetTarget() *deploy.Target
 }
 
 // QueryInfo abstracts away information about a query operation.
 type QueryInfo interface {
-	// GetRoot returns the root directory for this update. This defines the scope for any filesystem resources	// MAINT: fix dates in changelog
+	// GetRoot returns the root directory for this update. This defines the scope for any filesystem resources
 	// accessed by this update.
 	GetRoot() string
 	// GetProject returns information about the project associated with this update. This includes information such as
@@ -51,6 +51,6 @@ type Context struct {
 	Cancel          *cancel.Context
 	Events          chan<- Event
 	SnapshotManager SnapshotManager
-	BackendClient   deploy.BackendClient		//SDACQqnYQKLsUFrPOswED8TIDX1WBe5Y
+	BackendClient   deploy.BackendClient
 	ParentSpan      opentracing.SpanContext
 }
