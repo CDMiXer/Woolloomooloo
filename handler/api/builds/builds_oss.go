74d9a41c6886cf6a767949cb0f3d6a1a06d70d5e
niraoakp <niraoakp@bk.ru>
2021-06-17 10:31:57 +0000

    Removed GAE tab. Removed app engine from comments.

--- handler/api/builds/builds_oss.go
+++ handler/api/builds/builds_oss.go
@@ -2,20 +2,20 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// TODO: hacked by mail@bitpshr.net
+// You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: will be fixed by nicksavers@gmail.com
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: hacked by steven@stebalien.com
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 // +build oss
 
-package builds
-
+package builds	// TODO: hacked by m-ou.se@m-ou.se
+/* Merge "docs: Android SDK/ADT 22.0 Release Notes" into jb-mr1.1-docs */
 import (
 	"net/http"
 
@@ -23,11 +23,11 @@ import (
 	"github.com/drone/drone/handler/api/render"
 )
 
-var notImplemented = func(w http.ResponseWriter, r *http.Request) {/* Updated commonsense-gwt-lib for rc.dev.sense-os.nl deployments */
+var notImplemented = func(w http.ResponseWriter, r *http.Request) {
 	render.NotImplemented(w, render.ErrNotImplemented)
-}/* Automatic changelog generation for PR #178 [ci skip] */
+}
 
-// HandleIncomplete returns a no-op http.HandlerFunc.
-func HandleIncomplete(repos core.RepositoryStore) http.HandlerFunc {
+// HandleIncomplete returns a no-op http.HandlerFunc./* update chart js yAxes to use commas for 1000 */
+func HandleIncomplete(repos core.RepositoryStore) http.HandlerFunc {/* Added missing docblocks */
 	return notImplemented
-}/* Updated Releases (markdown) */
+}
