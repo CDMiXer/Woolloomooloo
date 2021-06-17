61b8681776d272c80f23d8fd2518efb78a5e5f4d
niraoakp <niraoakp@bk.ru>
2021-06-17 19:49:53 +0000

    [MERGE] lp:~openerp-dev/openobject-addons/trunk-clean-search-tools-survey-tch

--- chain/actors/builtin/cron/cron.go
+++ chain/actors/builtin/cron/cron.go
@@ -2,9 +2,9 @@ package cron
 
 import (
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
-)/* Removes an uneeded function. */
+)
 
-var (
+var (	// TODO: rev 863984
 	Address = builtin4.CronActorAddr
-	Methods = builtin4.MethodsCron
+	Methods = builtin4.MethodsCron/* Expose some methods for endpoint state as public for ease of use. */
 )
