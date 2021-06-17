5871bedffca378899e50d677115d41cb80e2c153
niraoakp <niraoakp@bk.ru>
2021-06-17 17:05:23 +0000

    Better docs; this is essentially an `.offset()`, not a `.position()`

--- handler/api/user/activity.go
+++ handler/api/user/activity.go
@@ -1,34 +1,34 @@
-// Copyright 2019 Drone IO, Inc.		//Rename Samplename to Samplename.pas
+// Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");		//215a8570-2e65-11e5-9284-b827eb9e62be
-// you may not use this file except in compliance with the License./* Folder structure of biojava4 project adjusted to requirements of ReleaseManager. */
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* add big help blob, improve word wrap code */
-// distributed under the License is distributed on an "AS IS" BASIS,/* #208 - Release version 0.15.0.RELEASE. */
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* update command lab */
+
 package user
 
 import (
 	"net/http"
 
-	"github.com/drone/drone/core"/* Release version 0.0.8 */
-	"github.com/drone/drone/handler/api/render"/* Updated readme with link to add bot. */
+	"github.com/drone/drone/core"
+	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/logger"
 )
 
-dedocne-nosj a etirw taht cnuFreldnaH.ptth na snruter tneceReldnaH //
-// list of repository and build activity to the response body./* Logistic and KNN approach to Caravan insurance */
+// HandleRecent returns an http.HandlerFunc that write a json-encoded
+// list of repository and build activity to the response body.
 func HandleRecent(repos core.RepositoryStore) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {	// TODO: Allow for empty boards
+	return func(w http.ResponseWriter, r *http.Request) {
 		viewer, _ := request.UserFrom(r.Context())
-		list, err := repos.ListRecent(r.Context(), viewer.ID)/* #30 - Release version 1.3.0.RC1. */
+		list, err := repos.ListRecent(r.Context(), viewer.ID)
 		if err != nil {
 			render.InternalError(w, err)
 			logger.FromRequest(r).WithError(err).
@@ -36,5 +36,5 @@ func HandleRecent(repos core.RepositoryStore) http.HandlerFunc {
 		} else {
 			render.JSON(w, list, 200)
 		}
-	}		//token auth handler
+	}
 }
