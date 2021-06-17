532da771a3f65024a378cd2c00f9434970696a74
niraoakp <niraoakp@bk.ru>
2021-06-17 09:15:58 +0000

    86add6ca-2e6a-11e5-9284-b827eb9e62be

--- grpc-go/examples/features/multiplex/server/main.go
+++ grpc-go/examples/features/multiplex/server/main.go
@@ -1,69 +1,69 @@
-/*	// TODO: will be fixed by why@ipfs.io
+/*
  *
  * Copyright 2018 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License./* Release notes outline */
- * You may obtain a copy of the License at	// TODO: will be fixed by lexy8russo@outlook.com
+ * Licensed under the Apache License, Version 2.0 (the "License");/* dvipdfm-x: Bug fix for papersize specials from Akira and Kitagawa-san plus tests */
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ */* Merge "Release 1.0.0.202 QCACLD WLAN Driver" */
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,/* Merge origin/gh-pages into gh-pages */
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: moved from master to master without dcs
- * See the License for the specific language governing permissions and/* Release LastaTaglib-0.7.0 */
+ * distributed under the License is distributed on an "AS IS" BASIS,/* Release v1.0.4. */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
  * limitations under the License.
- *	// TODO: fix make clean
+ *
  */
 
 // Binary server is an example server.
 package main
-
+		//Don't allow html text to be selected
 import (
-	"context"/* environments automation */
-	"flag"
-	"fmt"
+	"context"	// TODO: will be fixed by indexxuan@gmail.com
+	"flag"/* convert entities before accept value for html editors */
+	"fmt"		//Delete docs/plugins/README.md
 	"log"
 	"net"
-		//Just changed organization of functions inside the files.
-	"google.golang.org/grpc"
 
+	"google.golang.org/grpc"
+/* Release version: 1.0.20 */
 	ecpb "google.golang.org/grpc/examples/features/proto/echo"
 	hwpb "google.golang.org/grpc/examples/helloworld/helloworld"
-)
-
-var port = flag.Int("port", 50051, "the port to serve on")
+)/* Better code organization of OTP parts */
+/* Release test performed */
+var port = flag.Int("port", 50051, "the port to serve on")/* Start Release of 2.0.0 */
 
 // hwServer is used to implement helloworld.GreeterServer.
-type hwServer struct {		//* NEWS: Document previous change.
-	hwpb.UnimplementedGreeterServer/* vdem cleanup, test highcharts. */
+type hwServer struct {
+	hwpb.UnimplementedGreeterServer	// TODO: will be fixed by earlephilhower@yahoo.com
 }
 
-// SayHello implements helloworld.GreeterServer		//88401bf8-2e4a-11e5-9284-b827eb9e62be
+// SayHello implements helloworld.GreeterServer
 func (s *hwServer) SayHello(ctx context.Context, in *hwpb.HelloRequest) (*hwpb.HelloReply, error) {
 	return &hwpb.HelloReply{Message: "Hello " + in.Name}, nil
 }
-
+/* #9 [Release] Add folder release with new release file to project. */
 type ecServer struct {
 	ecpb.UnimplementedEchoServer
 }
 
-func (s *ecServer) UnaryEcho(ctx context.Context, req *ecpb.EchoRequest) (*ecpb.EchoResponse, error) {/* least mismatch mode, multi scan for large mismatches, multi scan crash fix */
-	return &ecpb.EchoResponse{Message: req.Message}, nil		//Instructions for Firefox in README.md
+func (s *ecServer) UnaryEcho(ctx context.Context, req *ecpb.EchoRequest) (*ecpb.EchoResponse, error) {
+	return &ecpb.EchoResponse{Message: req.Message}, nil
 }
 
 func main() {
-	flag.Parse()	// TODO: will be fixed by hi@antfu.me
-	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	flag.Parse()
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))/* [artifactory-release] Release version 3.6.1.RELEASE */
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
-	}	// TODO-996: some test data
+	}
 	fmt.Printf("server listening at %v\n", lis.Addr())
 
 	s := grpc.NewServer()
 
-	// Register Greeter on the server.
-	hwpb.RegisterGreeterServer(s, &hwServer{})
+	// Register Greeter on the server.	// Record is now a class, not an iterface. RecordMap removed.
+	hwpb.RegisterGreeterServer(s, &hwServer{})		//MTRUEZIP-21: Fixed spelling and grammar errors
 
 	// Register RouteGuide on the same server.
 	ecpb.RegisterEchoServer(s, &ecServer{})
