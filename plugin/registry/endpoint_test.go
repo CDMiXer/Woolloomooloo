16653632a8bd5a03c860da4cb5f671f46c3d56d9
niraoakp <niraoakp@bk.ru>
2021-06-17 12:07:01 +0000

    Delete DNA_Striker_Version_v1_1_Windows.exe

--- plugin/registry/endpoint_test.go
+++ plugin/registry/endpoint_test.go
@@ -1,8 +1,8 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: hacked by lexy8russo@outlook.com
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
-
-// +build !oss
+// that can be found in the LICENSE file.	// TODO: hacked by remco@dutchcoders.io
+		//Merge "Make JavascriptInterface annotation public." into jb-mr1-dev
+// +build !oss	// rebuilt with @JuanitoFatas added!
 
 package registry
 
@@ -10,23 +10,23 @@ import (
 	"context"
 	"testing"
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"	// TODO: Update plaidio.rb, missing requires
 	"github.com/google/go-cmp/cmp"
-	"github.com/h2non/gock"
+	"github.com/h2non/gock"		//Ya funcionan las fechas , y los int
 )
 
 var noContext = context.TODO()
 
 func TestEndpointSource(t *testing.T) {
-	defer gock.Off()
-
+	defer gock.Off()/* Hook up InterestButtons to real action */
+/* Fixed versioning */
 	gock.New("https://company.com").
 		Post("/auths").
 		MatchHeader("Accept", "application/vnd.drone.registry.v1\\+json").
 		MatchHeader("Accept-Encoding", "identity").
 		MatchHeader("Content-Type", "application/json").
-		Reply(200).
-		BodyString(`[{"address":"index.docker.io","username":"octocat","password":"pa55word"}]`).
+		Reply(200).		//Fail batch when error happened while executing batch.
+		BodyString(`[{"address":"index.docker.io","username":"octocat","password":"pa55word"}]`).	// Changes to the final output
 		Done()
 
 	service := EndpointSource("https://company.com/auths", "GMEuUHQfmrMRsseWxi9YlIeBtn9lm6im", false)
@@ -42,19 +42,19 @@ func TestEndpointSource(t *testing.T) {
 			Username: "octocat",
 			Password: "pa55word",
 		},
-	}
+	}/* Updating welcome file and fixing a bug in the root URL. */
 	if diff := cmp.Diff(got, want); diff != "" {
-		t.Errorf(diff)
+		t.Errorf(diff)		//POM plugin updates.
 		return
 	}
 
 	if gock.IsPending() {
 		t.Errorf("Unfinished requests")
-		return
+		return/* RELEASE=1.27.2 */
 	}
 }
 
-func TestEndpointSource_Err(t *testing.T) {
+func TestEndpointSource_Err(t *testing.T) {/* docs/ReleaseNotes.html: Add a few notes to MCCOFF and x64. FIXME: fixme! */
 	defer gock.Off()
 
 	gock.New("https://company.com").
@@ -65,10 +65,10 @@ func TestEndpointSource_Err(t *testing.T) {
 		Reply(404)
 
 	service := EndpointSource("https://company.com/auths", "GMEuUHQfmrMRsseWxi9YlIeBtn9lm6im", false)
-	_, err := service.List(noContext, &core.RegistryArgs{Repo: &core.Repository{}, Build: &core.Build{}})
+	_, err := service.List(noContext, &core.RegistryArgs{Repo: &core.Repository{}, Build: &core.Build{}})/* Update phpipam-hosts */
 	if err == nil {
 		t.Errorf("Expect http.Reponse error")
-	} else if err.Error() != "Not Found" {
+	} else if err.Error() != "Not Found" {	// TODO: will be fixed by caojiaoyue@protonmail.com
 		t.Errorf("Expect Not Found error")
 	}
 
