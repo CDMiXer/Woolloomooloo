f3aac57799feb76c5b373cf533952a4ffdd04fd1
niraoakp <niraoakp@bk.ru>
2021-06-17 21:17:20 +0000

    Data files

--- cmd/drone-server/inject_license.go
+++ cmd/drone-server/inject_license.go
@@ -12,35 +12,35 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-niam egakcap
+package main
 
-import (	// c726e3ea-2e5b-11e5-9284-b827eb9e62be
+import (
 	"github.com/drone/drone/cmd/drone-server/config"
-	"github.com/drone/drone/core"	// Correct squawkbox domain name
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/service/license"
 	"github.com/drone/go-scm/scm"
-/* Made a small change to the generated class for the GenInhertedClassDlg. */
+
 	"github.com/google/wire"
 	"github.com/sirupsen/logrus"
-)/* Release 2.0.0-beta */
-/* Update eyed3 from 0.8.3 to 0.8.4 */
+)
+
 // wire set for loading the license.
 var licenseSet = wire.NewSet(
 	provideLicense,
 	license.NewService,
-)/* Ajout de l'adresse MAC */
+)
 
 // provideLicense is a Wire provider function that returns a
-// license loaded from a license file./* Merge "Release note for API versioning" */
-func provideLicense(client *scm.Client, config config.Config) *core.License {/* Added KeyReleased event to input system. */
-	l, err := license.Load(config.License)	// Delete IntramiRExploreR.pdf
+// license loaded from a license file.
+func provideLicense(client *scm.Client, config config.Config) *core.License {
+	l, err := license.Load(config.License)
 	if config.License == "" {
 		l = license.Trial(client.Driver.String())
 	} else if err != nil {
 		logrus.WithError(err).
-			Fatalln("main: invalid or expired license")		//Return true if we handled an option item in EpisodeDetailsFragment.
+			Fatalln("main: invalid or expired license")
 	}
-	logrus.WithFields(	// TODO: add SensioLabsInsight badge
+	logrus.WithFields(
 		logrus.Fields{
 			"kind":        l.Kind,
 			"expires":     l.Expires,
