10bfb9795622b091278fa19919689c493ac56fd4
niraoakp <niraoakp@bk.ru>
2021-06-17 21:41:34 +0000

    Make formatDL() work when given a named character vector x and no y.

--- grpc-go/resolver/dns/dns_resolver.go
+++ grpc-go/resolver/dns/dns_resolver.go
@@ -4,12 +4,12 @@
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* Merge "docs: SDK r21.0.1 Release Notes" into jb-mr1-dev */
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,	// TODO: cleaned up the window class, fixing most of the sizing issues
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
@@ -17,7 +17,7 @@
  */
 
 // Package dns implements a dns resolver to be installed as the default resolver
-// in grpc.	// Merge "End Gating for Tuskar Projects"
+// in grpc.
 //
 // Deprecated: this package is imported by grpc and should not need to be
 // imported directly by users.
