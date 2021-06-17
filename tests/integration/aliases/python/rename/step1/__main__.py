f7c9ce42a2f56914566c8f83e8e284485746b852
niraoakp <niraoakp@bk.ru>
2021-06-17 15:18:28 +0000

    Add ability to create contributor during word creation

--- tests/integration/aliases/python/rename/step1/__main__.py
+++ tests/integration/aliases/python/rename/step1/__main__.py
@@ -1,10 +1,10 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE/* [1.2.3] Release not ready, because of curseforge */
-		//Haze: DMA fix to correct issues with Fever Soccer (no whatsnew)
+from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE
+
 class Resource1(ComponentResource):
     def __init__(self, name, opts=None):
         super().__init__("my:module:Resource", name, None, opts)
 
-# Scenario #1 - rename a resource	// TODO: will be fixed by brosner@gmail.com
+# Scenario #1 - rename a resource
 res1 = Resource1("res1")
