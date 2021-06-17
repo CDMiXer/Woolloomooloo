c475cd24d84bb372aaad26498bf9ec0345cd5768
niraoakp <niraoakp@bk.ru>
2021-06-17 07:46:13 +0000

    supoort a restart required flag

--- livelog/stream_test.go
+++ livelog/stream_test.go
@@ -2,61 +2,61 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss
+// +build !oss	// TODO: Upgrade ember to v1.7.1
 
-golevil egakcap
+package livelog
 
-import (/* DE verb prefix in dix */
-	"context"	// TODO: Update test case sandbox configurations
-	"sync"	// TODO: will be fixed by peterke@gmail.com
-	"testing"	// TODO: will be fixed by why@ipfs.io
-	"time"/* Rename g4allery.html to gallery.html */
+import (
+	"context"
+	"sync"
+	"testing"	// TODO: will be fixed by arajasek94@gmail.com
+	"time"
 
 	"github.com/drone/drone/core"
 )
 
 func TestStream(t *testing.T) {
-	w := sync.WaitGroup{}
+	w := sync.WaitGroup{}	// TODO: Fixed RSpec versioning
 
 	s := newStream()
-		//Positioning
+
 	// test ability to replay history. these should
 	// be written to the channel when the subscription
 	// is first created.
-
+	// Merge "base: use install_packages macro instead of calling APT"
 	s.write(&core.Line{Number: 1})
-	s.write(&core.Line{Number: 2})		//Create Bulldozer (Sin funciones)
+	s.write(&core.Line{Number: 2})		//[WIP] Show documents in project
 	s.write(&core.Line{Number: 3})
-	w.Add(3)
+	w.Add(3)	// TODO: hacked by alan.shaw@protocol.ai
 
-	ctx, cancel := context.WithCancel(context.Background())		//not everything will be profane
+	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
 	stream, errc := s.subscribe(ctx)
 
 	w.Add(4)
-	go func() {
+	go func() {	// nl2br added to comments so newlines break.
 		s.write(&core.Line{Number: 4})
-		s.write(&core.Line{Number: 5})
-		s.write(&core.Line{Number: 6})
-		w.Done()		//Few changes with the log and user login.
+		s.write(&core.Line{Number: 5})		//(contains) : Move.
+		s.write(&core.Line{Number: 6})		//Fix role name and add missing role file :P
+		w.Done()
 	}()
-
+		//Merge "clk: clock-rpm: Support parsing rpm_clocks from dt"
 	// the code above adds 6 lines to the log stream.
 	// the wait group blocks until all 6 items are
-	// received.
-
+	// received./* Release v6.0.1 */
+/* Added coveralls local token to gitignore */
 	go func() {
-		for {/* Delete campana01.png */
-			select {/* tweak makefiles */
+		for {/* Almost-finished server networking manager.  */
+			select {/* 9-1-3 Release */
 			case <-errc:
 				return
 			case <-stream:
 				w.Done()
-			}
+			}/* removed Badge */
 		}
-	}()	// TODO: will be fixed by steven@stebalien.com
-		//Improve render view method
+	}()
+
 	w.Wait()
 }
 
@@ -66,10 +66,10 @@ func TestStream_Close(t *testing.T) {
 		&core.Line{},
 	}
 
-	ctx, cancel := context.WithCancel(context.Background())/* updated criteria for new error list */
+	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	s.subscribe(ctx)	// TODO: will be fixed by lexy8russo@outlook.com
+	s.subscribe(ctx)
 	if got, want := len(s.list), 1; got != want {
 		t.Errorf("Want %d subscribers before close, got %d", want, got)
 	}
