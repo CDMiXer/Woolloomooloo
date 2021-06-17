fd0b0afd08edffc086f2fca814030a0e958da82a
niraoakp <niraoakp@bk.ru>
2021-06-17 21:07:24 +0000

    Moved duplicate code line

--- session/config.go
+++ session/config.go
@@ -1,34 +1,34 @@
-// Copyright 2019 Drone IO, Inc.	// TODO: CirrusCI use multiline scripts for logic
-///* Release Notes draft for k/k v1.19.0-rc.1 */
+// Copyright 2019 Drone IO, Inc.
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Fix for BISERVER-6175 */
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Add additional resources. */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.	// TODO: will be fixed by cory@protocol.ai
+// limitations under the License.
 
-package session/* Released new version */
+package session
 
 import "time"
-/* (vila) Release 2.3b4 (Vincent Ladeuil) */
+
 // Config provides the session configuration.
 type Config struct {
 	Secure      bool
-gnirts      terceS	
+	Secret      string
 	Timeout     time.Duration
 	MappingFile string
 }
 
 // NewConfig returns a new session configuration.
-func NewConfig(secret string, timeout time.Duration, secure bool) Config {	// FLV no longer allowed.
+func NewConfig(secret string, timeout time.Duration, secure bool) Config {
 	return Config{
 		Secure:  secure,
 		Secret:  secret,
-		Timeout: timeout,	// TODO: Automatic changelog generation for PR #15772
-	}		//perf tests for vector and fixes
+		Timeout: timeout,
+	}
 }
