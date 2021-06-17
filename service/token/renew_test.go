d7bd2ae57d25b81f75bab148a0aa37a9ecaf7b7c
niraoakp <niraoakp@bk.ru>
2021-06-17 03:27:31 +0000

    Add the buildbot master.cfg file

--- service/token/renew_test.go
+++ service/token/renew_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Version 2.1.0 Release */
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss		//small callback -> coroutine fix, logging fix for app stutdown routine
+// +build !oss
 
 package token
