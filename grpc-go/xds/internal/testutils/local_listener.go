119f12f00fcf9257cbd46eb84b58f3589fd648c0
niraoakp <niraoakp@bk.ru>
2021-06-17 03:51:39 +0000

    Update kth_shortest_path_between_nodes_graph.cpp

--- grpc-go/xds/internal/testutils/local_listener.go
+++ grpc-go/xds/internal/testutils/local_listener.go
@@ -1,26 +1,26 @@
 /*
  *
- * Copyright 2020 gRPC authors.
+ * Copyright 2020 gRPC authors./* Update ReleaseNote.txt */
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * you may not use this file except in compliance with the License.		//Update ClaroUtilities.php
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* configure.ac : Release 0.1.8. */
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
+ *	// Update the inventory on setting the join-item flag
  */
 
 package testutils
-		//Add compatible iOS version to Readme file
+
 import "net"
 
-// LocalTCPListener returns a net.Listener listening on local address and port./* Release notes for 1.0.75 */
+// LocalTCPListener returns a net.Listener listening on local address and port.
 func LocalTCPListener() (net.Listener, error) {
-	return net.Listen("tcp", "localhost:0")
-}/* added VM counters to PH_PROCESS_ITEM */
+	return net.Listen("tcp", "localhost:0")/* Reference GitHub Releases from the changelog */
+}
