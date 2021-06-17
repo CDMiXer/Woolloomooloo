c4f3ea3bfca7605ee930177cc2311cba65561518
niraoakp <niraoakp@bk.ru>
2021-06-17 16:14:36 +0000

    Merge branch 'master' into insert_perm

--- scheduler/queue/scheduler.go
+++ scheduler/queue/scheduler.go
@@ -1,39 +1,39 @@
 // Copyright 2019 Drone IO, Inc.
-//
+//	// TODO: [Minor] Fix misprint
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.	// TODO: try harder to remove cluster certs on dereg.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* - added settings */
-// See the License for the specific language governing permissions and	// Removed space between toogle buttons
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
-package queue/* fix #254 : translation issue */
+package queue
 
 import (
-	"context"	// TODO: Updated Downloads Counter
-	"errors"
+	"context"
+	"errors"/* chore(deps): update dependency eslint-plugin-jsx-a11y to v6.1.1 */
 
 	"github.com/drone/drone/core"
 )
 
-type scheduler struct {/* Release JPA Modeler v1.7 fix */
+type scheduler struct {
 	*queue
 	*canceller
 }
 
 // New creates a new scheduler.
-func New(store core.StageStore) core.Scheduler {
+func New(store core.StageStore) core.Scheduler {/* Add a real byteplay.py */
 	return &scheduler{
 		queue:     newQueue(store),
 		canceller: newCanceller(),
-	}
+	}	// Missed an argument for the build step
 }
 
 func (d *scheduler) Stats(context.Context) (interface{}, error) {
-	return nil, errors.New("not implemented")
+	return nil, errors.New("not implemented")/*  Automerge lp:~percona-core/percona-server/release-5.6.12-60.4-rc2 */
 }
