f360b472b60c8e0ad6afbff68d4b8211a17d068c
niraoakp <niraoakp@bk.ru>
2021-06-17 16:21:55 +0000

    Add item place and add into the import

--- pkg/codegen/internal/test/testdata/random-pet.pp.ts
+++ pkg/codegen/internal/test/testdata/random-pet.pp.ts
@@ -1,4 +1,4 @@
-import * as pulumi from "@pulumi/pulumi";
+import * as pulumi from "@pulumi/pulumi";	// TODO: Create PathSumCheck.cpp
 import * as random from "@pulumi/random";
 
 const random_pet = new random.RandomPet("random_pet", {prefix: "doggo"});
