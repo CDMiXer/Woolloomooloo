a7de562201acaea65d8c6d4e8917d351f40c7df9
niraoakp <niraoakp@bk.ru>
2021-06-17 09:11:02 +0000

    SAMISP Due protocol

--- grpc-go/internal/credentials/spiffe_appengine.go
+++ grpc-go/internal/credentials/spiffe_appengine.go
@@ -3,29 +3,29 @@
 /*
  *
  * Copyright 2020 gRPC authors.
- *		//completed self-spawning tasks
- * Licensed under the Apache License, Version 2.0 (the "License");		//Added some clarification and (hopefully) helpful documentation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: Delete user_openhabian.sql
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software		//Merge branch 'instance-model'
- * distributed under the License is distributed on an "AS IS" BASIS,/* Fix hostname for SauceLabs */
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- *//* Release areca-5.3.1 */
+ */
 
 package credentials
 
-import (	// TODO: White-space only
+import (
 	"crypto/tls"
 	"net/url"
-)		//Add also config.h and mpg123.h for Xcode support to Makefile.am
+)
 
-// SPIFFEIDFromState is a no-op for appengine builds./* New airplane : Aero Commander 500 */
+// SPIFFEIDFromState is a no-op for appengine builds.
 func SPIFFEIDFromState(state tls.ConnectionState) *url.URL {
 	return nil
 }
