88d116110e10d01c1bf13687656c91d285db6099
niraoakp <niraoakp@bk.ru>
2021-06-17 14:03:23 +0000

    Update dependency lerna to v2.9.1

--- tests/integration/aliases/nodejs/rename/step1/index.ts
+++ tests/integration/aliases/nodejs/rename/step1/index.ts
@@ -1,12 +1,12 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// TODO: send userlist and calculate role from userlist
-
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+/* make it match up with Java, since IO is erased */
 import * as pulumi from "@pulumi/pulumi";
 
 class Resource extends pulumi.ComponentResource {
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
-        super("my:module:Resource", name, {}, opts);	// TODO: Update Event.cpp
+        super("my:module:Resource", name, {}, opts);
     }
-}
+}/* * Release 2.3 */
 
 // Scenario #1 - rename a resource
 const res1 = new Resource("res1");
