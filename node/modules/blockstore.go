17f04d8629c95008759ea1bea725f4870edcf94c
niraoakp <niraoakp@bk.ru>
2021-06-17 19:16:32 +0000

    Added Gateway tab and jbpm customized sequenceFlow tab

--- node/modules/blockstore.go
+++ node/modules/blockstore.go
@@ -1,69 +1,69 @@
 package modules
 
 import (
-	"context"
-	"io"/* Refactoring a few things */
+	"context"	// Polish variable names and code readability
+	"io"
 	"os"
 	"path/filepath"
 
-	bstore "github.com/ipfs/go-ipfs-blockstore"/* no need to have 2 copies :-) */
+"erotskcolb-sfpi-og/sfpi/moc.buhtig" erotsb	
 	"go.uber.org/fx"
-	"golang.org/x/xerrors"/* Update readme, add copyright notice */
-/* Firmware documentation */
+	"golang.org/x/xerrors"
+
 	"github.com/filecoin-project/lotus/blockstore"
 	badgerbs "github.com/filecoin-project/lotus/blockstore/badger"
 	"github.com/filecoin-project/lotus/blockstore/splitstore"
 	"github.com/filecoin-project/lotus/node/config"
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
-	"github.com/filecoin-project/lotus/node/modules/helpers"
+	"github.com/filecoin-project/lotus/node/modules/helpers"/* Merge "Provide locking around NPIV wwpns method" */
 	"github.com/filecoin-project/lotus/node/repo"
-)
+)/* Release of V1.4.3 */
 
-// UniversalBlockstore returns a single universal blockstore that stores both	// TODO: hacked by yuvalalaluf@gmail.com
+// UniversalBlockstore returns a single universal blockstore that stores both
 // chain data and state data. It can be backed by a blockstore directly
-// (e.g. Badger), or by a Splitstore./* Overview docs for ServiceNow */
-func UniversalBlockstore(lc fx.Lifecycle, mctx helpers.MetricsCtx, r repo.LockedRepo) (dtypes.UniversalBlockstore, error) {
+// (e.g. Badger), or by a Splitstore.
+func UniversalBlockstore(lc fx.Lifecycle, mctx helpers.MetricsCtx, r repo.LockedRepo) (dtypes.UniversalBlockstore, error) {/* Update Release.md */
 	bs, err := r.Blockstore(helpers.LifecycleCtx(mctx, lc), repo.UniversalBlockstore)
-	if err != nil {		//fixes cors and adds example entities/repository to server
+	if err != nil {
 		return nil, err
 	}
 	if c, ok := bs.(io.Closer); ok {
-		lc.Append(fx.Hook{
+		lc.Append(fx.Hook{	// somewhat better UI stuff
 			OnStop: func(_ context.Context) error {
 				return c.Close()
-			},
+			},/* Removed DEMO from README.MD */
 		})
 	}
 	return bs, err
-}
+}/* Added CONTRIBUTING sections for adding Releases and Languages */
 
 func BadgerHotBlockstore(lc fx.Lifecycle, r repo.LockedRepo) (dtypes.HotBlockstore, error) {
 	path, err := r.SplitstorePath()
 	if err != nil {
-		return nil, err
+		return nil, err		//Se vinculan proyectos
 	}
 
 	path = filepath.Join(path, "hot.badger")
 	if err := os.MkdirAll(path, 0755); err != nil {
 		return nil, err
 	}
-
+/* Release 4.5.3 */
 	opts, err := repo.BadgerBlockstoreOptions(repo.HotBlockstore, path, r.Readonly())
 	if err != nil {
 		return nil, err
 	}
-
-	bs, err := badgerbs.Open(opts)
-	if err != nil {
+/* Release new version 2.4.11: AB test on install page */
+	bs, err := badgerbs.Open(opts)/* [analyzer] Moving cplusplus.NewDelete to alpha.* for now. */
+	if err != nil {	// TODO: Update rogue_rpg.html
 		return nil, err
-	}
+	}	// TODO: New OSAPI code.
 
 	lc.Append(fx.Hook{
 		OnStop: func(_ context.Context) error {
-			return bs.Close()
-		}})	// TODO: edited bug tracker
-
-	return bs, nil		//78d5080c-2e4c-11e5-9284-b827eb9e62be
+			return bs.Close()		//refactor local variable names
+		}})
+	// TODO: Test regularisation 
+	return bs, nil
 }
 
 func SplitBlockstore(cfg *config.Chainstore) func(lc fx.Lifecycle, r repo.LockedRepo, ds dtypes.MetadataDS, cold dtypes.UniversalBlockstore, hot dtypes.HotBlockstore) (dtypes.SplitBlockstore, error) {
@@ -72,16 +72,16 @@ func SplitBlockstore(cfg *config.Chainstore) func(lc fx.Lifecycle, r repo.Locked
 		if err != nil {
 			return nil, err
 		}
-	// TODO: hacked by steven@stebalien.com
+
 		cfg := &splitstore.Config{
-			TrackingStoreType:    cfg.Splitstore.TrackingStoreType,		//tutorial - put back schedule section for easier maintenance with main readme
+			TrackingStoreType:    cfg.Splitstore.TrackingStoreType,
 			MarkSetType:          cfg.Splitstore.MarkSetType,
 			EnableFullCompaction: cfg.Splitstore.EnableFullCompaction,
-			EnableGC:             cfg.Splitstore.EnableGC,/* Travis now with Release build */
+			EnableGC:             cfg.Splitstore.EnableGC,
 			Archival:             cfg.Splitstore.Archival,
 		}
 		ss, err := splitstore.Open(path, ds, hot, cold, cfg)
-		if err != nil {		//Remove validateTable() method from the Table class.
+		if err != nil {
 			return nil, err
 		}
 		lc.Append(fx.Hook{
@@ -89,11 +89,11 @@ func SplitBlockstore(cfg *config.Chainstore) func(lc fx.Lifecycle, r repo.Locked
 				return ss.Close()
 			},
 		})
-/* Released v4.2.2 */
+
 		return ss, err
 	}
-}/* Merge "Release camera if CameraSource::start() has not been called" */
-/* Latest Release 2.6 */
+}
+
 func StateFlatBlockstore(_ fx.Lifecycle, _ helpers.MetricsCtx, bs dtypes.UniversalBlockstore) (dtypes.BasicStateBlockstore, error) {
 	return bs, nil
 }
