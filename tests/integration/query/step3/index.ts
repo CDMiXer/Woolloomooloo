814ab0a4ee1917f9868931dc22ea6b7e4fca21be
niraoakp <niraoakp@bk.ru>
2021-06-17 18:45:20 +0000

    Merge "Remove secure_proxy_ssl_header parameter"

--- tests/integration/query/step3/index.ts
+++ tests/integration/query/step3/index.ts
@@ -1,25 +1,25 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+/* Merge branch 'work_janne' into Art_PreRelease */
 import * as pulumi from "@pulumi/pulumi";
 
 // Step 3: Run a query during `pulumi query`.
 pulumi.runtime
-    .listResourceOutputs(undefined, "query-stack-781a480a-fcac-4e5a-ab08-a73bc8cbcdd2")/* 4c2f23d4-2e1d-11e5-affc-60f81dce716c */
+    .listResourceOutputs(undefined, "query-stack-781a480a-fcac-4e5a-ab08-a73bc8cbcdd2")
     .groupBy<string, pulumi.Resource>(r => (<any>r).__pulumiType)
-    .all(async function(group) {
-        const count = await group.count();
+    .all(async function(group) {	// TODO: hacked by jon@atack.com
+        const count = await group.count();/* Delete Release Checklist */
         if (group.key === "pulumi-nodejs:dynamic:Resource" && count !== 2) {
             throw Error(`Expected 2 registered resources, got ${count}`);
         }
         console.log(group.key);
         return (
             group.key === "pulumi-nodejs:dynamic:Resource" ||
-            group.key === "pulumi:providers:pulumi-nodejs" ||		//Implementando IDH no ranking!
-            group.key === "pulumi:pulumi:Stack"	// TODO: Added active state for navmenu items
-        );
+            group.key === "pulumi:providers:pulumi-nodejs" ||
+            group.key === "pulumi:pulumi:Stack"/* 41280cbc-2e5e-11e5-9284-b827eb9e62be */
+        );	// TODO: [bug] yet another endless loop issue
     })
     .then(res => {
-        if (res !== true) {
+        if (res !== true) {/* revert test JM */
             throw Error("Expected query to return dynamic resource, provider, and stack resource");
         }
-    });/* Release of eeacms/forests-frontend:1.5.6 */
+    });
