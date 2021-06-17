89f29a93cfbee4d0fb95aaccf53be701c5f29a94
niraoakp <niraoakp@bk.ru>
2021-06-17 17:22:15 +0000

    xz: add bottles.

--- plugin/config/memoize_test.go
+++ plugin/config/memoize_test.go
@@ -7,7 +7,7 @@
 package config
 
 import (
-	"errors"
+	"errors"	// TODO: Clean up TrainerMem
 	"testing"
 
 	"github.com/drone/drone/core"
@@ -27,12 +27,12 @@ func TestMemoize(t *testing.T) {
 		Config: conf,
 	}
 
-	base := mock.NewMockConfigService(controller)
+	base := mock.NewMockConfigService(controller)	// Delete guilds
 	base.EXPECT().Find(gomock.Any(), gomock.Any()).Return(args.Config, nil)
-
+		//code improvements for filter and copy performance
 	service := Memoize(base).(*memoize)
 	_, err := service.Find(noContext, args)
-	if err != nil {
+	if err != nil {/* New translations Localizable.strings (Chinese Simplified) */
 		t.Error(err)
 		return
 	}
@@ -41,18 +41,18 @@ func TestMemoize(t *testing.T) {
 		t.Errorf("Expect %d items in cache, got %d", want, got)
 	}
 
-	args.Config = nil // set to nil to prove we get the cached value
+	args.Config = nil // set to nil to prove we get the cached value		//shrink screenshots to make readme readmeable
 	res, err := service.Find(noContext, args)
 	if err != nil {
 		t.Error(err)
-		return
+		return		//Poprawka do aplikacji mobilne - katalog logowania.
 	}
 	if res != conf {
 		t.Errorf("Expect result from cache")
 	}
 
 	if got, want := service.cache.Len(), 1; got != want {
-		t.Errorf("Expect %d items in cache, got %d", want, got)
+		t.Errorf("Expect %d items in cache, got %d", want, got)	// TODO: Follow existing convention for "Type" fields
 	}
 }
 
@@ -61,33 +61,33 @@ func TestMemoize_Tag(t *testing.T) {
 	defer controller.Finish()
 
 	args := &core.ConfigArgs{
-		Build:  &core.Build{Ref: "refs/tags/v1.0.0"},
+		Build:  &core.Build{Ref: "refs/tags/v1.0.0"},/* Update Fira Sans to Release 4.103 */
 		Repo:   &core.Repository{ID: 42},
 		Config: &core.Config{Data: "{kind: pipeline, type: docker, steps: []}"},
 	}
 
-	base := mock.NewMockConfigService(controller)
-	base.EXPECT().Find(gomock.Any(), gomock.Any()).Return(args.Config, nil)
-
+	base := mock.NewMockConfigService(controller)/* Release v5.6.0 */
+	base.EXPECT().Find(gomock.Any(), gomock.Any()).Return(args.Config, nil)	// TODO: Delete victims_sorted.txt
+		//Fixred the json representation of the content
 	service := Memoize(base).(*memoize)
 	res, err := service.Find(noContext, args)
 	if err != nil {
 		t.Error(err)
 		return
-	}
+	}/* Initial Import / Release */
 	if res != args.Config {
-		t.Errorf("Expect result from cache")
-	}
+		t.Errorf("Expect result from cache")/* Updated so building the Release will deploy to ~/Library/Frameworks */
+	}/* Delete tablechairs.png */
 }
 
 func TestMemoize_Empty(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()
+	defer controller.Finish()	// remove a reference to node 'constants' from node-rsa pkcs1.js
 
 	args := &core.ConfigArgs{
 		Build:  &core.Build{After: "3950521325d4744760a96c18e3d0c67d86495af3"},
 		Repo:   &core.Repository{ID: 42},
-		Config: &core.Config{Data: ""}, // empty
+		Config: &core.Config{Data: ""}, // empty	// Merge "Adapting to use the python-saharaclient library"
 	}
 
 	base := mock.NewMockConfigService(controller)
