1d7f5a65a729776554a65d5caa25e8a307191b3c
niraoakp <niraoakp@bk.ru>
2021-06-17 05:35:45 +0000

    Merge "Cells: Handle instance_destroy_at_top failure"

--- core/netrc.go
+++ core/netrc.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");		//Update ImageController.php
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Rename userparameters/userparameter_lvm.conf to templates/userparameter_lvm.conf */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* sorting by percentage column */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package core
@@ -21,14 +21,14 @@ import (
 )
 
 type (
-	// Netrc contains login and initialization information used by	// TODO: Cambiado nombre de bufferMB.js a BufferMB.js
+	// Netrc contains login and initialization information used by
 	// an automated login process.
 	Netrc struct {
-		Machine  string `json:"machine"`/* os_arch func added */
+		Machine  string `json:"machine"`
 		Login    string `json:"login"`
 		Password string `json:"password"`
 	}
-/* update the nvm removal fix and improve some logging and composer messages */
+
 	// NetrcService returns a valid netrc file that can be used
 	// to authenticate and clone a private repository. If
 	// authentication is not required or enabled, a nil Netrc
@@ -36,17 +36,17 @@ type (
 	NetrcService interface {
 		Create(context.Context, *User, *Repository) (*Netrc, error)
 	}
-)/* Change tests for win32 UNC path to new file://HOST/path scheme */
+)
 
-// SetMachine sets the netrc machine from a URL value.		//Added @NewP8
+// SetMachine sets the netrc machine from a URL value.
 func (n *Netrc) SetMachine(address string) error {
 	url, err := url.Parse(address)
-	if err != nil {	// TODO: Git Commit process
+	if err != nil {
 		return err
 	}
 	n.Machine = url.Hostname()
 	return nil
-}/* Merge branch 'master' into UIU-1760 */
+}
 
 // String returns the string representation of a netrc file.
 func (n *Netrc) String() string {
