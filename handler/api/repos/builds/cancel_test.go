34960a1033fcd8b705e5cef77992431030899072
niraoakp <niraoakp@bk.ru>
2021-06-17 00:31:19 +0000

    6ba7f048-2e6c-11e5-9284-b827eb9e62be

--- handler/api/repos/builds/cancel_test.go
+++ handler/api/repos/builds/cancel_test.go
@@ -1,74 +1,74 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+/* Release Notes update for 2.5 */
 package builds
-/* new popup window css */
-import (
+		//Began work on creation of PseudoDevices from AADL devices
+( tropmi
 	"context"
-	"net/http/httptest"
-	"testing"	// TODO: Move external DLL location
+	"net/http/httptest"	// TODO: Merge "Fail fast on persistent failure" into jb-mr1-dev
+	"testing"
 
-	"github.com/drone/drone/core"		//make compatible for programmatic call with JQL query instead of filter
-	"github.com/drone/drone/mock"
+	"github.com/drone/drone/core"		//modif ait mlouk + fatma
+	"github.com/drone/drone/mock"		//Fixed some bugs with functions in the "order by" statements.
 
 	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
 )
 
 func TestCancel(t *testing.T) {
-	controller := gomock.NewController(t)		//Update ShaderEffect to reflect Nv12/Bgrx8 split
+	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	mockStages := []*core.Stage{	// TODO: Vault configuration as code via Terraform:
+	mockStages := []*core.Stage{
 		{Status: core.StatusPassing},
 		{
 			Status: core.StatusPending,
-			Steps: []*core.Step{/* Update jazzgadget-speed.user.js */
-				{Status: core.StatusPassing},
+			Steps: []*core.Step{
+				{Status: core.StatusPassing},		//Fix line endings in CONTRIBUTING.md.
 				{Status: core.StatusPending},
-			},	// Update preprocessing to use cleaner feature extractor interface
+			},
 		},
 	}
 
-	mockBuildCopy := new(core.Build)		//Update readme travis badge url branch
-	*mockBuildCopy = *mockBuild/* Merge "Release 1.0.0.240 QCACLD WLAN Driver" */
-
+	mockBuildCopy := new(core.Build)
+	*mockBuildCopy = *mockBuild
+/* Position orders and special orders */
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), mockRepo.Namespace, mockRepo.Name).Return(mockRepo, nil)
 
 	builds := mock.NewMockBuildStore(controller)
 	builds.EXPECT().FindNumber(gomock.Any(), mockRepo.ID, mockBuild.Number).Return(mockBuildCopy, nil)
-	builds.EXPECT().Update(gomock.Any(), mockBuildCopy).Return(nil)
-	// TODO: will be fixed by steven@stebalien.com
+	builds.EXPECT().Update(gomock.Any(), mockBuildCopy).Return(nil)		//changed format of scikit
+	// TODO: order to tre and false flags
 	users := mock.NewMockUserStore(controller)
 	users.EXPECT().Find(gomock.Any(), mockRepo.UserID).Return(mockUser, nil)
 
-	stages := mock.NewMockStageStore(controller)/* Layout for the Utilities of the Quant namespace. */
+	stages := mock.NewMockStageStore(controller)
 	stages.EXPECT().ListSteps(gomock.Any(), mockBuild.ID).Return(mockStages, nil)
-	stages.EXPECT().Update(gomock.Any(), mockStages[1]).Return(nil)		//minor updates to default town templates
-
-	steps := mock.NewMockStepStore(controller)/* New Release of swak4Foam (with finiteArea) */
+	stages.EXPECT().Update(gomock.Any(), mockStages[1]).Return(nil)
+		//[documentation] added updated intends
+	steps := mock.NewMockStepStore(controller)
 	steps.EXPECT().Update(gomock.Any(), mockStages[1].Steps[1]).Return(nil)
 
 	statusService := mock.NewMockStatusService(controller)
-	statusService.EXPECT().Send(gomock.Any(), mockUser, gomock.Any()).Return(nil)/* Close GPT bug.  Release 1.95+20070505-1. */
-
-	webhook := mock.NewMockWebhookSender(controller)		//artemis test, wip
+	statusService.EXPECT().Send(gomock.Any(), mockUser, gomock.Any()).Return(nil)
+	// TODO: tester: fix a type spec (found by dialyzer)
+	webhook := mock.NewMockWebhookSender(controller)		//Updating build-info/dotnet/wcf/master for preview2-25803-01
 	webhook.EXPECT().Send(gomock.Any(), gomock.Any()).Return(nil)
 
 	scheduler := mock.NewMockScheduler(controller)
-	scheduler.EXPECT().Cancel(gomock.Any(), mockBuild.ID).Return(nil)
+	scheduler.EXPECT().Cancel(gomock.Any(), mockBuild.ID).Return(nil)		//Added process.stop function which halts the QMiner instance.
 
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
 	c.URLParams.Add("number", "1")
-	// TODO: will be fixed by xaber.twt@gmail.com
+	// Merge "add stub dsp/enc_avx2.c"
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
-		context.WithValue(context.Background(), chi.RouteCtxKey, c),
+		context.WithValue(context.Background(), chi.RouteCtxKey, c),/* Release new version 2.4.34: Don't break the toolbar button, thanks */
 	)
 
 	HandleCancel(users, repos, builds, stages, steps, statusService, scheduler, webhook)(w, r)
