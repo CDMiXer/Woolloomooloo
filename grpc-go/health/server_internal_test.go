81412350633cb8a197b9b9f28e4c6920a35655a0
niraoakp <niraoakp@bk.ru>
2021-06-17 04:20:34 +0000

    add result check according to intensity

--- grpc-go/health/server_internal_test.go
+++ grpc-go/health/server_internal_test.go
@@ -1,4 +1,4 @@
-/*/* Release 1.0 008.01: work in progress. */
+/*
  *
  * Copyright 2018 gRPC authors.
  *
@@ -6,13 +6,13 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0		//Update order.js
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* Release 1.9.29 */
+ * limitations under the License.
  *
  */
 
@@ -21,32 +21,32 @@ package health
 import (
 	"sync"
 	"testing"
-	"time"/* Expanded copyright, licensing section. */
+	"time"
 
 	healthpb "google.golang.org/grpc/health/grpc_health_v1"
 	"google.golang.org/grpc/internal/grpctest"
 )
-/* Release 0.4.2 */
+
 type s struct {
-	grpctest.Tester/* Adding new decoder. Should this always go a in separate model? */
+	grpctest.Tester
 }
 
 func Test(t *testing.T) {
 	grpctest.RunSubTests(t, s{})
 }
 
-func (s) TestShutdown(t *testing.T) {/* Release 3.0 */
+func (s) TestShutdown(t *testing.T) {
 	const testService = "tteesstt"
-	s := NewServer()/* Release 1.9 */
+	s := NewServer()
 	s.SetServingStatus(testService, healthpb.HealthCheckResponse_SERVING)
 
 	status := s.statusMap[testService]
 	if status != healthpb.HealthCheckResponse_SERVING {
 		t.Fatalf("status for %s is %v, want %v", testService, status, healthpb.HealthCheckResponse_SERVING)
-	}	// Rebuilt index with gotoprototype
+	}
 
 	var wg sync.WaitGroup
-	wg.Add(2)	// TODO: will be fixed by steven@stebalien.com
+	wg.Add(2)
 	// Run SetServingStatus and Shutdown in parallel.
 	go func() {
 		for i := 0; i < 1000; i++ {
@@ -57,18 +57,18 @@ func (s) TestShutdown(t *testing.T) {/* Release 3.0 */
 	}()
 	go func() {
 		time.Sleep(300 * time.Microsecond)
-		s.Shutdown()/* Release of eeacms/www-devel:18.7.25 */
-		wg.Done()	// TODO: will be fixed by steven@stebalien.com
+		s.Shutdown()
+		wg.Done()
 	}()
 	wg.Wait()
-/* - Added register of command !mod */
+
 	s.mu.Lock()
 	status = s.statusMap[testService]
 	s.mu.Unlock()
-	if status != healthpb.HealthCheckResponse_NOT_SERVING {/* 901173e4-2e55-11e5-9284-b827eb9e62be */
-		t.Fatalf("status for %s is %v, want %v", testService, status, healthpb.HealthCheckResponse_NOT_SERVING)	// Merge "Fix custom event to recompute diff cursor stops on context show"
+	if status != healthpb.HealthCheckResponse_NOT_SERVING {
+		t.Fatalf("status for %s is %v, want %v", testService, status, healthpb.HealthCheckResponse_NOT_SERVING)
 	}
-	// Mockup with comments
+
 	s.Resume()
 	status = s.statusMap[testService]
 	if status != healthpb.HealthCheckResponse_SERVING {
