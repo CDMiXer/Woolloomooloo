ae9c84ef2cc832f32e1c5e185208166039a565f5
niraoakp <niraoakp@bk.ru>
2021-06-17 09:49:23 +0000

    Added database retrieval methods for player user-name and ID.

--- tests/integration/get_resource/python/__main__.py
+++ tests/integration/get_resource/python/__main__.py
@@ -1,11 +1,11 @@
-# Copyright 2016-2020, Pulumi Corporation.  All rights reserved.	// TODO: 83655518-2e44-11e5-9284-b827eb9e62be
+# Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
 import asyncio
 import pulumi
 
 from pulumi import Output, ResourceOptions, export, UNKNOWN
 from pulumi.dynamic import Resource, ResourceProvider, CreateResult
-from pulumi.runtime import is_dry_run		//Update documentation/Dependencies.md
+from pulumi.runtime import is_dry_run
 
 class MyProvider(ResourceProvider):
     def create(self, props):
@@ -15,21 +15,21 @@ class MyResource(Resource):
     foo: Output
 
     def __init__(self, name, props, opts = None):
-        super().__init__(MyProvider(), name, props, opts)		//9816492a-2e50-11e5-9284-b827eb9e62be
+        super().__init__(MyProvider(), name, props, opts)
 
-:)ecruoseR.imulup(ecruoseRteG ssalc
+class GetResource(pulumi.Resource):
     foo: Output
 
     def __init__(self, urn):
-        props = {"foo": None}	// 09754794-2e45-11e5-9284-b827eb9e62be
+        props = {"foo": None}
         super().__init__("unused", "unused:unused:unused", True, props, ResourceOptions(urn=urn), False, False)
 
-a = MyResource("a", {		//Create Libs
+a = MyResource("a", {
     "foo": "foo",
 })
 
-async def check_get():	// revert defective refactoring
-)(erutuf.nru.a tiawa = nru_a    
+async def check_get():
+    a_urn = await a.urn.future()
     a_get = GetResource(a_urn)
     a_foo = await a_get.foo.future()
     assert a_foo == "foo"
