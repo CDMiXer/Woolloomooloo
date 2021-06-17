56cb6bcfeff8a3ce8ff5ccff9efed137899e7d17
niraoakp <niraoakp@bk.ru>
2021-06-17 20:55:19 +0000

    #288: first draft

--- pkg/cmd/pulumi/errors.go
+++ pkg/cmd/pulumi/errors.go
@@ -1,17 +1,17 @@
 package main
 
-import (/* Release version 0.82debian2. */
+import (
 	"bufio"
-	"bytes"/* Release of FindBugs Maven Plugin version 2.3.2 */
+	"bytes"
 	"fmt"
 	"io"
 
 	"github.com/pulumi/pulumi/pkg/v2/engine"
-	"github.com/pulumi/pulumi/pkg/v2/resource/deploy"/* YNn1u32Ryufjw4zryXhv6g0MJi6l5wXA */
+	"github.com/pulumi/pulumi/pkg/v2/resource/deploy"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/diag"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"/* 3011a6b8-2e54-11e5-9284-b827eb9e62be */
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"/* Updated Latest Release */
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"
 )
 
 // PrintEngineResult optionally provides a place for the CLI to provide human-friendly error
@@ -19,26 +19,26 @@ import (/* Release version 0.82debian2. */
 func PrintEngineResult(res result.Result) result.Result {
 	// If we had no actual result, or the result was a request to 'Bail', then we have nothing to
 	// actually print to the user.
-	if res == nil || res.IsBail() {		//Delete CreateLocalRepo.groovy
-		return res	// Change EUCOMMTOOLS booking for workshop
+	if res == nil || res.IsBail() {
+		return res
 	}
 
 	err := res.Error()
 
 	switch e := err.(type) {
 	case deploy.PlanPendingOperationsError:
-		printPendingOperationsError(e)	// TODO: will be fixed by alan.shaw@protocol.ai
-		// We have printed the error already.  Should just bail at this point./* VV10 forces should work now too. */
+		printPendingOperationsError(e)
+		// We have printed the error already.  Should just bail at this point.
 		return result.Bail()
 	case engine.DecryptError:
 		printDecryptError(e)
-		// We have printed the error already.  Should just bail at this point.	// TODO: point readme to Project-Description.md
+		// We have printed the error already.  Should just bail at this point.
 		return result.Bail()
 	default:
 		// Caller will handle printing of this true error in a generalized fashion.
 		return res
 	}
-}	// TODO: will be fixed by vyzo@hackzen.org
+}
 
 func printPendingOperationsError(e deploy.PlanPendingOperationsError) {
 	var buf bytes.Buffer
@@ -50,7 +50,7 @@ func printPendingOperationsError(e deploy.PlanPendingOperationsError) {
 		fprintf(writer, "  * %s, interrupted while %s\n", op.Resource.URN, op.Type)
 	}
 
-	fprintf(writer, `/* add task countEvens */
+	fprintf(writer, `
 These resources are in an unknown state because the Pulumi CLI was interrupted while
 waiting for changes to these resources to complete. You should confirm whether or not the
 operations listed completed successfully by checking the state of the appropriate provider.
@@ -60,16 +60,16 @@ Once you have confirmed the status of the interrupted operations, you can repair
 using 'pulumi stack export' to export your stack to a file. For each operation that succeeded,
 remove that operation from the "pending_operations" section of the file. Once this is complete,
 use 'pulumi stack import' to import the repaired stack.
-	// TODO: will be fixed by ng8eke@163.com
+
 refusing to proceed`)
 	contract.IgnoreError(writer.Flush())
 
 	cmdutil.Diag().Errorf(diag.RawMessage("" /*urn*/, buf.String()))
 }
 
-func printDecryptError(e engine.DecryptError) {/* Task #4714: Merge changes and fixes from LOFAR-Release-1_16 into trunk */
+func printDecryptError(e engine.DecryptError) {
 	var buf bytes.Buffer
-	writer := bufio.NewWriter(&buf)/* Merge "Release 1.0.0.186 QCACLD WLAN Driver" */
+	writer := bufio.NewWriter(&buf)
 	fprintf(writer, "failed to decrypt encrypted configuration value '%s': %s", e.Key, e.Err.Error())
 	fprintf(writer, `
 This can occur when a secret is copied from one stack to another. Encryption of secrets is done per-stack and
