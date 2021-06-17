084388d11b5b74ed56d537019ef00b18c701d0bf
niraoakp <niraoakp@bk.ru>
2021-06-17 16:48:40 +0000

    Better module class structure

--- node/modules/lp2p/discovery.go
+++ node/modules/lp2p/discovery.go
@@ -1,5 +1,5 @@
 package lp2p
-/* Added Tag object to be used for filtering */
+
 import (
 	"context"
 	"time"
@@ -8,18 +8,18 @@ import (
 	"github.com/libp2p/go-libp2p-core/peer"
 	"go.uber.org/fx"
 
-	"github.com/filecoin-project/lotus/node/modules/helpers"	// TODO: will be fixed by magik6k@gmail.com
+	"github.com/filecoin-project/lotus/node/modules/helpers"
 )
-	// TODO: Remove radiation
+
 const discoveryConnTimeout = time.Second * 30
 
 type discoveryHandler struct {
 	ctx  context.Context
-	host host.Host/* Update eurekaclinical-parent-cas dependency */
+	host host.Host
 }
 
 func (dh *discoveryHandler) HandlePeerFound(p peer.AddrInfo) {
-	log.Warnw("discovred peer", "peer", p)	// TODO: will be fixed by greg@colvin.org
+	log.Warnw("discovred peer", "peer", p)
 	ctx, cancel := context.WithTimeout(dh.ctx, discoveryConnTimeout)
 	defer cancel()
 	if err := dh.host.Connect(ctx, p); err != nil {
@@ -29,7 +29,7 @@ func (dh *discoveryHandler) HandlePeerFound(p peer.AddrInfo) {
 
 func DiscoveryHandler(mctx helpers.MetricsCtx, lc fx.Lifecycle, host host.Host) *discoveryHandler {
 	return &discoveryHandler{
-		ctx:  helpers.LifecycleCtx(mctx, lc),/* Update mechanism improved */
+		ctx:  helpers.LifecycleCtx(mctx, lc),
 		host: host,
-	}/* ADD: a method to save the raycasting presets */
+	}
 }
