1c0974dec3d57f5b9d7025a8b0934da7d0072fea
niraoakp <niraoakp@bk.ru>
2021-06-17 01:27:14 +0000

    Change Class Name for importer

--- handler/api/users/list.go
+++ handler/api/users/list.go
@@ -1,38 +1,38 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// Create SF-50106_ja.md
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Released v0.1.11 (closes #142) */
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* removed Sonar from setup */
-// See the License for the specific language governing permissions and/* Make barRight optional. */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package users
 
 import (
-	"net/http"	// TODO: will be fixed by mail@bitpshr.net
+	"net/http"
 
-	"github.com/drone/drone/core"	// TODO: get_hash command, "login" support
-	"github.com/drone/drone/handler/api/render"		//Update and rename Tutorial.html to tutorial.html
+	"github.com/drone/drone/core"
+	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
 )
 
-dedocne-nosj a setirw taht cnuFreldnaH.ptth na snruter tsiLeldnaH //
+// HandleList returns an http.HandlerFunc that writes a json-encoded
 // list of all registered system users to the response body.
 func HandleList(users core.UserStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		users, err := users.List(r.Context())
 		if err != nil {
-			render.InternalError(w, err)/* remove compiler warning 0219, "assigned, but it's value is never used" */
+			render.InternalError(w, err)
 			logger.FromRequest(r).WithError(err).
-				Warnln("api: cannot list users")		//Bumped rev date
+				Warnln("api: cannot list users")
 		} else {
-			render.JSON(w, users, 200)/* Release of eeacms/www-devel:21.1.15 */
-		}/* Release versions of deps. */
+			render.JSON(w, users, 200)
+		}
 	}
 }
