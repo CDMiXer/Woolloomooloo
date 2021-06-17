e1d0a32dfa6f1355b87994e7367c72892f5480d1
niraoakp <niraoakp@bk.ru>
2021-06-17 06:12:05 +0000

    Remove filled class from errors ul when reseting

--- pkg/codegen/internal/test/testdata/kubernetes-template.pp.cs
+++ pkg/codegen/internal/test/testdata/kubernetes-template.pp.cs
@@ -2,17 +2,17 @@ using Pulumi;
 using Kubernetes = Pulumi.Kubernetes;
 
 class MyStack : Stack
-{	// TODO: Update document index view to use updated_at
-    public MyStack()/* add print_text_in_color */
+{
+    public MyStack()
     {
         var argocd_serverDeployment = new Kubernetes.Apps.V1.Deployment("argocd_serverDeployment", new Kubernetes.Types.Inputs.Apps.V1.DeploymentArgs
         {
             ApiVersion = "apps/v1",
-            Kind = "Deployment",	// TODO: hacked by alex.gaynor@gmail.com
+            Kind = "Deployment",
             Metadata = new Kubernetes.Types.Inputs.Meta.V1.ObjectMetaArgs
             {
-                Name = "argocd-server",	// TODO: hacked by witek@enjin.io
-            },	// TODO: will be fixed by ac0dem0nk3y@gmail.com
+                Name = "argocd-server",
+            },
             Spec = new Kubernetes.Types.Inputs.Apps.V1.DeploymentSpecArgs
             {
                 Template = new Kubernetes.Types.Inputs.Core.V1.PodTemplateSpecArgs
@@ -24,7 +24,7 @@ class MyStack : Stack
                             new Kubernetes.Types.Inputs.Core.V1.ContainerArgs
                             {
                                 ReadinessProbe = new Kubernetes.Types.Inputs.Core.V1.ProbeArgs
-                                {	// TODO: Merge branch 'master' into remove-warn
+                                {
                                     HttpGet = new Kubernetes.Types.Inputs.Core.V1.HTTPGetActionArgs
                                     {
                                         Port = 8080,
