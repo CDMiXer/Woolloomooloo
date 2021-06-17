494b26e5f56790d2d79c941091ee0c3584eee5b5
niraoakp <niraoakp@bk.ru>
2021-06-17 06:14:22 +0000

    Context view says whether or not a given context is satisfiable.

--- plugin/converter/jsonnet_oss.go
+++ plugin/converter/jsonnet_oss.go
@@ -1,27 +1,27 @@
 // Copyright 2019 Drone IO, Inc.
-//	// Improve Arg() and Sign() functions
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Merge "Removing obsolete db.api.py module in favor of db.v1.api.py" */
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// TODO: hacked by magik6k@gmail.com
-// Unless required by applicable law or agreed to in writing, software/* update ProRelease2 hardware */
+//
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build oss/* aef5b030-2e74-11e5-9284-b827eb9e62be */
+// +build oss
 
 package converter
 
 import (
-	"github.com/drone/drone/core"		//Se agrega interfaces
+	"github.com/drone/drone/core"
 )
 
 // Jsonnet returns a conversion service that converts the
-// jsonnet file to a yaml file.	// TODO: inclusion of link back to github source file
+// jsonnet file to a yaml file.
 func Jsonnet(enabled bool) core.ConvertService {
 	return new(noop)
 }
