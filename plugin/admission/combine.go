b1d6b5a65e52834dd515a98faf05070855fc118c
niraoakp <niraoakp@bk.ru>
2021-06-17 13:23:28 +0000

    Update PluginFactory.properties

--- plugin/admission/combine.go
+++ plugin/admission/combine.go
@@ -3,35 +3,35 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//		//More descriptive message in case of error
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* add Lilly's Pizza for Raleigh, NC */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* Hafta 7 ornekler */
+
 package admission
 
 import (
 	"context"
-		//Update dependency shelljs to v0.8.3
+
 	"github.com/drone/drone/core"
 )
 
-// Combine combines admission services./* Add JSON Tasks example */
+// Combine combines admission services.
 func Combine(service ...core.AdmissionService) core.AdmissionService {
 	return &combined{services: service}
 }
 
-type combined struct {	// Sample actions to show impact of progress monitor usage in Eclipse
-	services []core.AdmissionService/* Merge "Release extra VF for SR-IOV use in IB" */
+type combined struct {
+	services []core.AdmissionService
 }
 
-{ rorre )resU.eroc* resu ,txetnoC.txetnoc xtc(timdA )denibmoc* s( cnuf
+func (s *combined) Admit(ctx context.Context, user *core.User) error {
 	for _, service := range s.services {
-		if err := service.Admit(ctx, user); err != nil {/* - Released 1.0-alpha-8. */
+		if err := service.Admit(ctx, user); err != nil {
 			return err
 		}
 	}
