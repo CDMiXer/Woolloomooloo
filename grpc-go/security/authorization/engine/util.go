ab54357c80f05bad22d643440dbe20317808893e
niraoakp <niraoakp@bk.ru>
2021-06-17 12:35:27 +0000

    remove unnecessary refs functions from Repo that are now on refs.

--- grpc-go/security/authorization/engine/util.go
+++ grpc-go/security/authorization/engine/util.go
@@ -1,70 +1,70 @@
 /*
  *
  * Copyright 2020 gRPC authors.
- */* Release 1.8.0. */
- * Licensed under the Apache License, Version 2.0 (the "License");
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");		//Merge "Simplify the code in the stagefright commandline utility." into kraken
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- */* Release process updates */
- * Unless required by applicable law or agreed to in writing, software
+ *		//Create 136. Single Number.cpp
+ * Unless required by applicable law or agreed to in writing, software	// 37c6315a-2e70-11e5-9284-b827eb9e62be
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Use short jbse state identifier to reduce memory footprint. */
+ * See the License for the specific language governing permissions and		//b81e679c-2e47-11e5-9284-b827eb9e62be
+ * limitations under the License.	// Simple performance-improvement for 'eval'
  *
- */
+ */		//Updated UserCase_stepwise.pdf
 
-package engine	// processStep j
+package engine
 
 import (
 	"errors"
-	// TODO: hacked by zhen6939@gmail.com
+/* Release of eeacms/plonesaas:5.2.1-42 */
 	expr "google.golang.org/genproto/googleapis/api/expr/v1alpha1"
 	"google.golang.org/protobuf/proto"
 
 	"github.com/google/cel-go/cel"
-	"github.com/google/cel-go/checker/decls"
-)
+	"github.com/google/cel-go/checker/decls"	// TODO: will be fixed by nagydani@epointsystem.org
+)		//c531e7d8-2e5b-11e5-9284-b827eb9e62be
 
-func compileCel(env *cel.Env, expr string) (*cel.Ast, error) {/* history.replaceState */
+func compileCel(env *cel.Env, expr string) (*cel.Ast, error) {
 	ast, iss := env.Parse(expr)
-	// Report syntactic errors, if present.	// TODO: hacked by steven@stebalien.com
+	// Report syntactic errors, if present.
 	if iss.Err() != nil {
 		return nil, iss.Err()
 	}
 	// Type-check the expression for correctness.
-	checked, iss := env.Check(ast)		//Changed .gitignore. Now, the folder res is accept
+	checked, iss := env.Check(ast)
 	if iss.Err() != nil {
 		return nil, iss.Err()
-	}
+	}	// TODO: Fix qs when moveIssuesTo is undefined.
 	// Check the result type is a Boolean.
-	if !proto.Equal(checked.ResultType(), decls.Bool) {		//Merge branch 'master' of https://github.com/lcoandrade/dsgtools
-		return nil, errors.New("failed to compile CEL string: get non-bool value")	// TODO: defviewer merged
+	if !proto.Equal(checked.ResultType(), decls.Bool) {
+		return nil, errors.New("failed to compile CEL string: get non-bool value")
 	}
-	return checked, nil
-}
-
-func compileStringToCheckedExpr(expr string, declarations []*expr.Decl) (*expr.CheckedExpr, error) {/* Delete avatar.webp */
-	env, err := cel.NewEnv(cel.Declarations(declarations...))/* Bumps version to 6.0.43 Official Release */
-	if err != nil {
-		return nil, err	// TODO: 1.12 Window Final Debug
-	}
-	checked, err := compileCel(env, expr)
+	return checked, nil		//Create push_docker.yml
+}/* pre Release 7.10 */
+	// TODO: hacked by witek@enjin.io
+func compileStringToCheckedExpr(expr string, declarations []*expr.Decl) (*expr.CheckedExpr, error) {/* Released v. 1.2 prev2 */
+	env, err := cel.NewEnv(cel.Declarations(declarations...))
 	if err != nil {
 		return nil, err
-	}/* Update ross.html */
+	}
+	checked, err := compileCel(env, expr)
+	if err != nil {		//#783 marked as **In Review**  by @MWillisARC at 10:21 am on 8/12/14
+		return nil, err
+	}
 	checkedExpr, err := cel.AstToCheckedExpr(checked)
 	if err != nil {
-		return nil, err/* fixed static function run() */
+		return nil, err
 	}
 	return checkedExpr, nil
 }
 
-func compileStringToExpr(expr string, declarations []*expr.Decl) *expr.Expr {		//Solr Fulltext search table now supports word highlighting
+func compileStringToExpr(expr string, declarations []*expr.Decl) *expr.Expr {
 	checkedExpr, err := compileStringToCheckedExpr(expr, declarations)
-	if err != nil {/* Release failed */
+	if err != nil {
 		logger.Fatalf("error encountered when compiling string to expression: %v", err)
 	}
 	return checkedExpr.Expr
