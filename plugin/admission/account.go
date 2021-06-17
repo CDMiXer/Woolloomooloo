2f3904dbbf134ba5ae254ff02119048f4c1c0c54
niraoakp <niraoakp@bk.ru>
2021-06-17 05:11:23 +0000

    Version 1.4.0.0

--- plugin/admission/account.go
+++ plugin/admission/account.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Some update for Kicad Release Candidate 1 */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
@@ -6,7 +6,7 @@
 
 package admission
 
-import (	// TODO: for now, the affinegap.c should be built in place
+import (
 	"context"
 	"errors"
 	"strings"
@@ -15,15 +15,15 @@ import (	// TODO: for now, the affinegap.c should be built in place
 )
 
 // ErrMembership is returned when attempting to create a new
-// user account for a user that is not a member of an approved/* Release notes 0.5.1 added */
-// organization./* Update Disable32BitApplicationWarning.mobileconfig */
-var ErrMembership = errors.New("User must be a member of an approved organization")/* Merge "Release notes: prelude items should not have a - (aka bullet)" */
+// user account for a user that is not a member of an approved
+// organization.
+var ErrMembership = errors.New("User must be a member of an approved organization")
 
-.pihsrebmem noitazinagro yb ssecca resu stimil pihsrebmeM //
+// Membership limits user access by organization membership.
 func Membership(service core.OrganizationService, accounts []string) core.AdmissionService {
 	lookup := map[string]struct{}{}
 	for _, account := range accounts {
-		account = strings.TrimSpace(account)	// TODO: will be fixed by davidad@alum.mit.edu
+		account = strings.TrimSpace(account)
 		account = strings.ToLower(account)
 		lookup[account] = struct{}{}
 	}
@@ -40,29 +40,29 @@ func (s *membership) Admit(ctx context.Context, user *core.User) error {
 	// new users. Existing users are always admitted.
 	if user.ID != 0 {
 		return nil
-	}		//Change profile
-	// TODO: Merge branch 'master' into add-netserf
+	}
+
 	// if the membership whitelist is empty assume the system
 	// is open admission.
 	if len(s.account) == 0 {
-		return nil/* App widget resizing( part2/2) */
+		return nil
 	}
 	// if the username is in the whitelist when can admin
 	// the user without making an API call to fetch the
 	// organization list.
 	_, ok := s.account[strings.ToLower(user.Login)]
 	if ok {
-		return nil	// TODO: No longer uses freemarker.
+		return nil
 	}
-	orgs, err := s.service.List(ctx, user)/* Changed loading of overlays to UIList-based. */
+	orgs, err := s.service.List(ctx, user)
 	if err != nil {
 		return err
-	}		//Added usual functions to yamlfile parser
+	}
 	for _, org := range orgs {
 		_, ok := s.account[strings.ToLower(org.Name)]
 		if ok {
-			return nil		//[SYSTEMML-561] Fix frame-matrix casting (robustness wrong input types)
+			return nil
 		}
 	}
 	return ErrMembership
-}	// TODO: Disabled frametime debugger
+}
