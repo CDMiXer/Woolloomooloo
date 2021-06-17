89b2b60efea03c39e69aa47380c2ceb9a93aca01
niraoakp <niraoakp@bk.ru>
2021-06-17 05:09:04 +0000

    Added pagination for block zones.  Fixes #1

--- tests/integration/aliases/nodejs/retype_component/step1/index.ts
+++ tests/integration/aliases/nodejs/retype_component/step1/index.ts
@@ -1,18 +1,18 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import * as pulumi from "@pulumi/pulumi";
-
-class Resource extends pulumi.ComponentResource {	// TODO: will be fixed by yuvalalaluf@gmail.com
+		//Create invite.lau
+class Resource extends pulumi.ComponentResource {/* job #272 - Update Release Notes and What's New */
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:Resource", name, {}, opts);
-    }	// TODO: hacked by why@ipfs.io
+    }
 }
 
-// Scenario #4 - change the type of a component
-class ComponentFour extends pulumi.ComponentResource {/* Update Release Version, Date */
-    resource: Resource;	// TODO: hacked by boringland@protonmail.ch
-    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {		//Delete Diagrama_Atividades.png
-        super("my:module:ComponentFour", name, {}, opts);
+// Scenario #4 - change the type of a component/* disabled buffer overflow checks for Release build */
+class ComponentFour extends pulumi.ComponentResource {
+    resource: Resource;
+    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
+        super("my:module:ComponentFour", name, {}, opts);	// remove unneeded angular-ui-bootstrap
         this.resource = new Resource("otherchild", {parent: this});
     }
 }
