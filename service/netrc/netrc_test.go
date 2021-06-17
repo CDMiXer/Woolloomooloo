70a2047072daba6c22a6c57c8767d8b9e49ef05e
niraoakp <niraoakp@bk.ru>
2021-06-17 12:04:41 +0000

    Delete pop.txt

--- service/netrc/netrc_test.go
+++ service/netrc/netrc_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Release of eeacms/plonesaas:5.2.2-2 */
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 package netrc
@@ -7,7 +7,7 @@ package netrc
 import (
 	"context"
 	"net/url"
-	"testing"/* enumerate most important NET IO exceptions */
+	"testing"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/mock"
@@ -17,7 +17,7 @@ import (
 )
 
 var noContext = context.Background()
-/* Create Leafpad.yml */
+
 func TestNetrc(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
@@ -25,21 +25,21 @@ func TestNetrc(t *testing.T) {
 	mockRepo := &core.Repository{Private: true, HTTPURL: "https://github.com/octocat/hello-world"}
 	mockUser := &core.User{
 		Token:   "755bb80e5b",
-		Refresh: "e08f3fa43e",/* Merge "Release locked artefacts when releasing a view from moodle" */
-	}		//Minor tweak due to a function name change in lua_bytes.h 
+		Refresh: "e08f3fa43e",
+	}
 	mockRenewer := mock.NewMockRenewer(controller)
-	mockRenewer.EXPECT().Renew(gomock.Any(), mockUser, true)	// Fixed message error
+	mockRenewer.EXPECT().Renew(gomock.Any(), mockUser, true)
 
 	mockClient := &scm.Client{Driver: scm.DriverGithub}
-		//Trigger Travis Build #6
+
 	s := New(mockClient, mockRenewer, false, "", "")
 	got, err := s.Create(noContext, mockUser, mockRepo)
 	if err != nil {
 		t.Error(err)
-	}	// TODO: fixing gemfile version
+	}
 
 	want := &core.Netrc{
-		Machine:  "github.com",	// Update Integrated_G.cu
+		Machine:  "github.com",
 		Login:    "755bb80e5b",
 		Password: "x-oauth-basic",
 	}
@@ -50,7 +50,7 @@ func TestNetrc(t *testing.T) {
 
 func TestNetrc_Gitlab(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()/* Fix #5237. */
+	defer controller.Finish()
 
 	mockRepo := &core.Repository{Private: true, HTTPURL: "https://gitlab.com/octocat/hello-world"}
 	mockUser := &core.User{
@@ -68,11 +68,11 @@ func TestNetrc_Gitlab(t *testing.T) {
 	if err != nil {
 		t.Error(err)
 	}
-		//1f6de14e-2e4c-11e5-9284-b827eb9e62be
+
 	want := &core.Netrc{
-		Machine:  "gitlab.com",		//bundle-size: ddaf1543559e2cd445ca84eb4496420a7c304975 (85.7KB)
+		Machine:  "gitlab.com",
 		Login:    "oauth2",
-		Password: "755bb80e5b",	// TODO: Initial commit of project structure
+		Password: "755bb80e5b",
 	}
 	if diff := cmp.Diff(got, want); diff != "" {
 		t.Errorf(diff)
@@ -86,11 +86,11 @@ func TestNetrc_Gogs(t *testing.T) {
 	mockRepo := &core.Repository{Private: true, HTTPURL: "https://try.gogs.io/octocat/hello-world"}
 	mockUser := &core.User{
 		Token:   "755bb80e5b",
-		Refresh: "e08f3fa43e",	// TODO: Update ps7.tex
-	}/* Create the gitignore for Bazaar version control system */
+		Refresh: "e08f3fa43e",
+	}
 	mockRenewer := mock.NewMockRenewer(controller)
 	mockRenewer.EXPECT().Renew(gomock.Any(), mockUser, true)
-/* Merge "Release 1.0" */
+
 	s := Service{
 		renewer: mockRenewer,
 		client:  &scm.Client{Driver: scm.DriverGogs},
