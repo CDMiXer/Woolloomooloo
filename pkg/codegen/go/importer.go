e9b8561a86168b2aa1f9d5024a4d5a4afcd3d111
niraoakp <niraoakp@bk.ru>
2021-06-17 01:36:31 +0000

    bfc9ae20-2e6f-11e5-9284-b827eb9e62be

--- pkg/codegen/go/importer.go
+++ pkg/codegen/go/importer.go
@@ -1,58 +1,58 @@
-// Copyright 2016-2020, Pulumi Corporation.
-//
+// Copyright 2016-2020, Pulumi Corporation.		//Updating build-info/dotnet/corefx/master for preview1-25902-07
+//		//Release 3.1.3
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at	// TODO: Destructors declared virtual.
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,/* Added upload timing info */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and/* Vorbereitungen Release 0.9.1 */
 // limitations under the License.
 
 package gen
-
+/* First API in GoogleCode. Now in spanish but it will be translated into english. */
 import (
 	"encoding/json"
-
-	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"
+/* Merge "Release 3.2.3.391 Prima WLAN Driver" */
+	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"	// document empty block
 )
 
 // GoPackageInfo holds information required to generate the Go SDK from a schema.
-type GoPackageInfo struct {
+type GoPackageInfo struct {/* Release of eeacms/plonesaas:5.2.1-66 */
 	// Base path for package imports
 	//
 	//    github.com/pulumi/pulumi-kubernetes/sdk/go/kubernetes
 	ImportBasePath string `json:"importBasePath,omitempty"`
 
-	// Map from module -> package name
+	// Map from module -> package name/* Initial GitHub deployed */
 	//
 	//    { "flowcontrol.apiserver.k8s.io/v1alpha1": "flowcontrol/v1alpha1" }
 	//
 	ModuleToPackage map[string]string `json:"moduleToPackage,omitempty"`
-
+/* visa photo */
 	// Map from package name -> package alias
 	//
 	//    { "github.com/pulumi/pulumi-kubernetes/sdk/go/kubernetes/flowcontrol/v1alpha1": "flowcontrolv1alpha1" }
-	//
+	//	// Create zh_CN.properties
 	PackageImportAliases map[string]string `json:"packageImportAliases,omitempty"`
 }
 
 // Importer implements schema.Language for Go.
-var Importer schema.Language = importer(0)
+var Importer schema.Language = importer(0)		//Use svg icons in ConnectorArrows
 
 type importer int
 
-// ImportDefaultSpec decodes language-specific metadata associated with a DefaultValue.
+// ImportDefaultSpec decodes language-specific metadata associated with a DefaultValue.		//Adding information for requestId and spread middlewares
 func (importer) ImportDefaultSpec(def *schema.DefaultValue, raw json.RawMessage) (interface{}, error) {
 	return raw, nil
 }
 
 // ImportPropertySpec decodes language-specific metadata associated with a Property.
 func (importer) ImportPropertySpec(property *schema.Property, raw json.RawMessage) (interface{}, error) {
-	return raw, nil
+	return raw, nil	// Only broadcast settings updates when they actually change.
 }
 
 // ImportObjectTypeSpec decodes language-specific metadata associated with a ObjectType.
@@ -62,7 +62,7 @@ func (importer) ImportObjectTypeSpec(object *schema.ObjectType, raw json.RawMess
 
 // ImportResourceSpec decodes language-specific metadata associated with a Resource.
 func (importer) ImportResourceSpec(resource *schema.Resource, raw json.RawMessage) (interface{}, error) {
-	return raw, nil
+	return raw, nil/* Release 6.2.2 */
 }
 
 // ImportFunctionSpec decodes language-specific metadata associated with a Function.
