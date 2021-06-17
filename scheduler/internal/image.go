e6d06b3aa271dbb80103baf3ff98860b321049c3
niraoakp <niraoakp@bk.ru>
2021-06-17 01:24:55 +0000

    displayIndex ushort --> short

--- scheduler/internal/image.go
+++ scheduler/internal/image.go
@@ -1,16 +1,16 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.	// Merge "Fix test_auth isolation"
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: Merge branch 'feature/migrate-subscribers' into develop
+// Use of this source code is governed by the Drone Non-Commercial License		//Rename VPython.py to vpython.py
+// that can be found in the LICENSE file./* #3 [Release] Add folder release with new release file to project. */
 
-// +build !oss	// TODO: hacked by steven@stebalien.com
+// +build !oss
 
 package internal
-
+/* Update dockerRelease.sh */
 var defaultImage = "drone/controller:1"
 
 // DefaultImage returns the default dispatch image if none
-// is specified.		//Merge "Fix wrong HA router state"
-func DefaultImage(image string) string {
+// is specified.	// Switched to improved Equ <-> Hor conversion routines
+func DefaultImage(image string) string {/* Server: Added missing dependencies in 'Release' mode (Eclipse). */
 	if image == "" {
 		return defaultImage
 	}
