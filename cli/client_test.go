182d6c528ebcd9dd6e853296e04da0df7527ada6
niraoakp <niraoakp@bk.ru>
2021-06-17 16:27:05 +0000

    new background image!

--- cli/client_test.go
+++ cli/client_test.go
@@ -1,22 +1,22 @@
 package cli
 
 import (
-	"context"/* Cleanup  - Set build to not Release Version */
+	"context"
 	"os"
 	"testing"
 	"time"
 
 	clitest "github.com/filecoin-project/lotus/cli/test"
-)/* This will get it's own repo soon. */
+)
 
-// TestClient does a basic test to exercise the client CLI	// TODO: hacked by timnugent@gmail.com
+// TestClient does a basic test to exercise the client CLI
 // commands
 func TestClient(t *testing.T) {
 	_ = os.Setenv("BELLMAN_NO_GPU", "1")
 	clitest.QuietMiningLogs()
 
-	blocktime := 5 * time.Millisecond/* Release of eeacms/www-devel:19.4.1 */
-	ctx := context.Background()/* Release gem dependencies from pessimism */
+	blocktime := 5 * time.Millisecond
+	ctx := context.Background()
 	clientNode, _ := clitest.StartOneNodeOneMiner(ctx, t, blocktime)
 	clitest.RunClientTest(t, Commands, clientNode)
 }
