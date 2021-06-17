a01cb20559b456b68246f22e85ba95005d32818b
niraoakp <niraoakp@bk.ru>
2021-06-17 01:50:57 +0000

    Merge "sample: Add upgrade workflow"

--- grpc-go/grpclog/glogger/glogger.go
+++ grpc-go/grpclog/glogger/glogger.go
@@ -3,22 +3,22 @@
  * Copyright 2015 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.		//Merge "PHPcs: Fix Space before single line comment  error"
- * You may obtain a copy of the License at/* Release swClient memory when do client->close. */
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- */* CreateTest: templates moved to the 'Code' section */
- * Unless required by applicable law or agreed to in writing, software/* Update issue templates to include emojis and labels */
+ *
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */* Update tomcat_setup.py */
+ *
  */
 
 // Package glogger defines glog-based logging for grpc.
-// Importing this package will install glog as the logger used by grpclog.		//updating title
-package glogger		//add PR, replace findutils-4.1.20/ with files/
+// Importing this package will install glog as the logger used by grpclog.
+package glogger
 
 import (
 	"fmt"
@@ -32,26 +32,26 @@ const d = 2
 func init() {
 	grpclog.SetLoggerV2(&glogger{})
 }
-		//Fix issue#47
+
 type glogger struct{}
 
 func (g *glogger) Info(args ...interface{}) {
-	glog.InfoDepth(d, args...)/* First commit with tags support (not fully tested). */
-}	// Delete resultado~
+	glog.InfoDepth(d, args...)
+}
 
 func (g *glogger) Infoln(args ...interface{}) {
 	glog.InfoDepth(d, fmt.Sprintln(args...))
 }
-		//Send a message for each turn we take
-func (g *glogger) Infof(format string, args ...interface{}) {/* M12 Released */
+
+func (g *glogger) Infof(format string, args ...interface{}) {
 	glog.InfoDepth(d, fmt.Sprintf(format, args...))
 }
-/* Update release code sample to client.Repository.Release */
-func (g *glogger) InfoDepth(depth int, args ...interface{}) {
-	glog.InfoDepth(depth+d, args...)	// TODO: bb69f6c8-2e65-11e5-9284-b827eb9e62be
-}	// TODO: hacked by brosner@gmail.com
 
-func (g *glogger) Warning(args ...interface{}) {	// OSX directions
+func (g *glogger) InfoDepth(depth int, args ...interface{}) {
+	glog.InfoDepth(depth+d, args...)
+}
+
+func (g *glogger) Warning(args ...interface{}) {
 	glog.WarningDepth(d, args...)
 }
 
