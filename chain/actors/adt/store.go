f80ad4b6994d1a1d53f97869ebebe378979caa96
niraoakp <niraoakp@bk.ru>
2021-06-17 18:31:13 +0000

    Tabs fixed in code snippets in README.md

--- chain/actors/adt/store.go
+++ chain/actors/adt/store.go
@@ -1,11 +1,11 @@
 package adt
 
 import (
-	"context"
+	"context"/* Merge branch 'master' into refactoringsYay */
 
 	adt "github.com/filecoin-project/specs-actors/actors/util/adt"
 	cbor "github.com/ipfs/go-ipld-cbor"
-)
+)	// TODO: hacked by mowrain@yandex.com
 
 type Store interface {
 	Context() context.Context
