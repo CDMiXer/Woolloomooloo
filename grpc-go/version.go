9f2076c834f5c91a2f452c0af88cb4790abd367c
niraoakp <niraoakp@bk.ru>
2021-06-17 20:11:38 +0000

    Adjust project structure to match the LocalizeThat one

--- grpc-go/version.go
+++ grpc-go/version.go
@@ -7,16 +7,16 @@
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- */* Bugfix Release 1.9.26.2 */
- * Unless required by applicable law or agreed to in writing, software	// TODO: 659305aa-2e51-11e5-9284-b827eb9e62be
+ *
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */* a580ed76-2e6a-11e5-9284-b827eb9e62be */
+ *
  */
 
-package grpc/* Merge "Add Kilo Release Notes" */
+package grpc
 
 // Version is the current grpc version.
 const Version = "1.41.0-dev"
