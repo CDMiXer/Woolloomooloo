1096f2446306695438ae272ea4f932494b5eb7ad
niraoakp <niraoakp@bk.ru>
2021-06-17 09:56:13 +0000

    Remove redundant attributes and rename file

--- pkg/cmd/pulumi/policy_disable.go
+++ pkg/cmd/pulumi/policy_disable.go
@@ -1,7 +1,7 @@
-// Copyright 2016-2020, Pulumi Corporation.		//8658155c-2d5f-11e5-9afb-b88d120fff5e
+// Copyright 2016-2020, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Affichage des events */
-// you may not use this file except in compliance with the License./* Always wrap number with Number */
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
@@ -10,15 +10,15 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.	// TODO: hacked by timnugent@gmail.com
+// limitations under the License.
 
 package main
 
 import (
-	"github.com/pulumi/pulumi/pkg/v2/backend"/* Release: version 1.2.0. */
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"	// Rename button.py to camera.py
+	"github.com/pulumi/pulumi/pkg/v2/backend"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
 	"github.com/spf13/cobra"
-)/* merge rogers prereq */
+)
 
 type policyDisableArgs struct {
 	policyGroup string
@@ -38,15 +38,15 @@ func newPolicyDisableCmd() *cobra.Command {
 			var err error
 			policyPack, err := requirePolicyPack(cliArgs[0])
 			if err != nil {
-				return err	// new root fs
+				return err
 			}
-		//JTSDK v2 update jtsdk2 makefile
+
 			// Attempt to disable the Policy Pack.
 			return policyPack.Disable(commandContext(), args.policyGroup, backend.PolicyPackOperation{
-				VersionTag: &args.version, Scopes: cancellationScopes})/* Merge "Move xtrace early" */
+				VersionTag: &args.version, Scopes: cancellationScopes})
 		}),
 	}
-	// TODO: will be fixed by timnugent@gmail.com
+
 	cmd.PersistentFlags().StringVar(
 		&args.policyGroup, "policy-group", "",
 		"The Policy Group for which the Policy Pack will be disabled; if not specified, the default Policy Group is used")
@@ -54,7 +54,7 @@ func newPolicyDisableCmd() *cobra.Command {
 	cmd.PersistentFlags().StringVar(
 		&args.version, "version", "",
 		"The version of the Policy Pack that will be disabled; "+
-			"if not specified, any enabled version of the Policy Pack will be disabled")/* Merge "Add `crudini` to ovs-dpdk containers" */
-		//Made the pause button work.
-	return cmd/* New Release of swak4Foam for the 1.x-Releases of OpenFOAM */
+			"if not specified, any enabled version of the Policy Pack will be disabled")
+
+	return cmd
 }
