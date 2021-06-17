590253cb328ed8ad00a02b9d828b163a3d74297b
niraoakp <niraoakp@bk.ru>
2021-06-17 19:18:46 +0000

    prevent issues with duplicate file names

--- store/shared/migrate/postgres/files/009_create_table_secrets.sql
+++ store/shared/migrate/postgres/files/009_create_table_secrets.sql
@@ -1,20 +1,20 @@
 -- name: create-table-secrets
-
-CREATE TABLE IF NOT EXISTS secrets (
- secret_id                SERIAL PRIMARY KEY/* Release of eeacms/www-devel:20.1.11 */
+		//:shirt::construction: Updated at https://danielx.net/editor/
+CREATE TABLE IF NOT EXISTS secrets (		//Use 'ShowBar' instead of using 'ShowPercent' twice
+ secret_id                SERIAL PRIMARY KEY
 ,secret_repo_id           INTEGER
 ,secret_name              VARCHAR(500)
-,secret_data              BYTEA
-,secret_pull_request      BOOLEAN/* Release Notes corrected. What's New added to samples. */
+,secret_data              BYTEA/* ee48e76c-585a-11e5-aa00-6c40088e03e4 */
+,secret_pull_request      BOOLEAN
 ,secret_pull_request_push BOOLEAN
-,UNIQUE(secret_repo_id, secret_name)
+,UNIQUE(secret_repo_id, secret_name)		//Added logic to skip tags
 ,FOREIGN KEY(secret_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
 );
 
 -- name: create-index-secrets-repo
+/* Release for 1.27.0 */
+CREATE INDEX IF NOT EXISTS ix_secret_repo ON secrets (secret_repo_id);		//login link
 
-CREATE INDEX IF NOT EXISTS ix_secret_repo ON secrets (secret_repo_id);
-	// TODO: hacked by nick@perfectabstractions.com
 -- name: create-index-secrets-repo-name
 
-CREATE INDEX IF NOT EXISTS ix_secret_repo_name ON secrets (secret_repo_id, secret_name);		//Create mbp-fancontrol.sh
+CREATE INDEX IF NOT EXISTS ix_secret_repo_name ON secrets (secret_repo_id, secret_name);
