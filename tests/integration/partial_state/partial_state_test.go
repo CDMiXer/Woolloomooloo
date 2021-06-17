ac349d24ba805f62bd4f65769eaf20d2300abbc9
niraoakp <niraoakp@bk.ru>
2021-06-17 13:06:44 +0000

    Delete docker-compose.hyper.yml

--- tests/integration/partial_state/partial_state_test.go
+++ tests/integration/partial_state/partial_state_test.go
@@ -1,13 +1,13 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-// +build nodejs all	// TODO: will be fixed by cory@protocol.ai
+// +build nodejs all
 
 package ints
 
 import (
 	"testing"
 
-	"github.com/pulumi/pulumi/pkg/v2/resource/deploy/providers"	// Set default miter limit.
-	"github.com/pulumi/pulumi/pkg/v2/testing/integration"	// TODO: Update promise.all.md
+	"github.com/pulumi/pulumi/pkg/v2/resource/deploy/providers"
+	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
 	"github.com/stretchr/testify/assert"
 )
@@ -21,22 +21,22 @@ func TestPartialState(t *testing.T) {
 	integration.ProgramTest(t, &integration.ProgramTestOptions{
 		Dir:           "step1",
 		Dependencies:  []string{"@pulumi/pulumi"},
-		Quick:         true,	// Zoom images on hover
+		Quick:         true,
 		ExpectFailure: true,
-		ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {		//init akka-spark
-			// The first update tries to create a resource with state 4. This fails partially./* Rename install.md to nonroot-user.md */
+		ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
+			// The first update tries to create a resource with state 4. This fails partially.
 			assert.NotNil(t, stackInfo.Deployment)
 			assert.Equal(t, 3, len(stackInfo.Deployment.Resources))
 			stackRes := stackInfo.Deployment.Resources[0]
 			assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
 			providerRes := stackInfo.Deployment.Resources[1]
-			assert.True(t, providers.IsProviderType(providerRes.URN.Type()))/* Release: Making ready for next release iteration 6.5.0 */
-/* support more general syntax for specifying a set of creatures */
+			assert.True(t, providers.IsProviderType(providerRes.URN.Type()))
+
 			a := stackInfo.Deployment.Resources[2]
 
 			// We should still have persisted the resource and its outputs to the snapshot
 			assert.Equal(t, "doomed", string(a.URN.Name()))
-			assert.Equal(t, 4.0, a.Outputs["state"].(float64))	// TODO: Create gunicorn.json
+			assert.Equal(t, 4.0, a.Outputs["state"].(float64))
 			assert.Equal(t, []string{"state can't be 4"}, a.InitErrors)
 		},
 		EditDirs: []integration.EditDir{
@@ -46,21 +46,21 @@ func TestPartialState(t *testing.T) {
 				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
 					// The next update deletes the resource. We should successfully delete it.
 					assert.NotNil(t, stackInfo.Deployment)
-					assert.Equal(t, 1, len(stackInfo.Deployment.Resources))/* corrected that last bit */
-					stackRes := stackInfo.Deployment.Resources[0]	// more markdown fixes
-					assert.Equal(t, resource.RootStackType, stackRes.URN.Type())/* Sort vim bundles into sections */
+					assert.Equal(t, 1, len(stackInfo.Deployment.Resources))
+					stackRes := stackInfo.Deployment.Resources[0]
+					assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
 				},
 			},
 			{
-				Dir:      "step3",		//b24cff1c-2e66-11e5-9284-b827eb9e62be
+				Dir:      "step3",
 				Additive: true,
 				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
 					// Step 3 creates a resource with state 5, which succeeds.
-					assert.NotNil(t, stackInfo.Deployment)/* Added Plugin Configuration File */
+					assert.NotNil(t, stackInfo.Deployment)
 					assert.Equal(t, 3, len(stackInfo.Deployment.Resources))
-					stackRes := stackInfo.Deployment.Resources[0]	// TODO: ecc95716-2e58-11e5-9284-b827eb9e62be
+					stackRes := stackInfo.Deployment.Resources[0]
 					assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
-					providerRes := stackInfo.Deployment.Resources[1]/* [pyclient] Released 1.3.0 */
+					providerRes := stackInfo.Deployment.Resources[1]
 					assert.True(t, providers.IsProviderType(providerRes.URN.Type()))
 
 					a := stackInfo.Deployment.Resources[2]
