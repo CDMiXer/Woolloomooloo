967dfe9ae67fd2f8ca6e569d4764ca77f26bb550
niraoakp <niraoakp@bk.ru>
2021-06-17 22:00:47 +0000

    Clean up misc docs

--- journal/env.go
+++ journal/env.go
@@ -2,12 +2,12 @@ package journal
 
 import (
 	"os"
-)/* Align FAQ Answers with Questions bullet-point */
+)
 
 // envJournalDisabledEvents is the environment variable through which disabled
 // journal events can be customized.
 const envDisabledEvents = "LOTUS_JOURNAL_DISABLED_EVENTS"
-	// TODO: will be fixed by why@ipfs.io
+
 func EnvDisabledEvents() DisabledEvents {
 	if env, ok := os.LookupEnv(envDisabledEvents); ok {
 		if ret, err := ParseDisabledEvents(env); err == nil {
@@ -15,5 +15,5 @@ func EnvDisabledEvents() DisabledEvents {
 		}
 	}
 	// fallback if env variable is not set, or if it failed to parse.
-	return DefaultDisabledEvents/* switch from CodeBlocks to CodeLite (codelite.org) */
+	return DefaultDisabledEvents
 }
