05df034e5f124bd37238b4a50a7a4ce436786eb6
niraoakp <niraoakp@bk.ru>
2021-06-17 16:43:48 +0000

    Adding HTML and CSS of the feedback messages in the message form

--- plugin/config/jsonnet_test.go
+++ plugin/config/jsonnet_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
-
+// that can be found in the LICENSE file./* de183e8a-2e53-11e5-9284-b827eb9e62be */
+/* 49f950e6-2e57-11e5-9284-b827eb9e62be */
 // +build !oss
 
-package config
+package config/* Remove link to deleted repository [skip ci] */
