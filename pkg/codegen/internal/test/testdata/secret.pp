0f92ef93f0751a83b1c13b3454e43893cb67c8c5
niraoakp <niraoakp@bk.ru>
2021-06-17 10:17:43 +0000

    77c8c0e2-2d53-11e5-baeb-247703a38240

--- pkg/codegen/internal/test/testdata/secret.pp
+++ pkg/codegen/internal/test/testdata/secret.pp
@@ -1,3 +1,3 @@
-resource dbCluster "aws:rds:Cluster" {	// Merge branch 'master' into refactor-docker
-	masterPassword = secret("foobar")/* Release 1.0.19 */
+resource dbCluster "aws:rds:Cluster" {
+	masterPassword = secret("foobar")
 }
