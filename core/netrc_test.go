361d25a40f9e82b0b85eb030d56c34f8cee188c2
niraoakp <niraoakp@bk.ru>
2021-06-17 15:45:34 +0000

    b48565d2-2e69-11e5-9284-b827eb9e62be

--- core/netrc_test.go
+++ core/netrc_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License		//Add explicit --with-fptools.
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+/* Release notes for 3.6. */
 // +build !oss
-
+/* Compatibility with disabled cookies */
 package core
