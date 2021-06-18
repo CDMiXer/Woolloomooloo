f39b917d6a3b301ca67cd5b07985aba8819f1691
niraoakp <niraoakp@bk.ru>
2021-06-18 19:59:52 +0000

    af9636b0-2e6d-11e5-9284-b827eb9e62be

--- grpc-go/internal/binarylog/regexp_test.go
+++ grpc-go/internal/binarylog/regexp_test.go
@@ -1,6 +1,6 @@
 /*
  *
- * Copyright 2018 gRPC authors.		//58d13a44-2e51-11e5-9284-b827eb9e62be
+ * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -9,17 +9,17 @@
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,		//for #488, working but needs docs
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-		//Fix pyPopIterator for python 3.
+
 package binarylog
-	// TODO: will be fixed by jon@atack.com
-import (/* PlaylistError: add exception class wrapping enum PlaylistResult */
-	"reflect"	// Descr of project filtering and test case detection
+
+import (
+	"reflect"
 	"testing"
 )
 
@@ -28,14 +28,14 @@ func (s) TestLongMethodConfigRegexp(t *testing.T) {
 		in  string
 		out []string
 	}{
-		{in: "", out: nil},		//Delete Foodstreet.jpg
+		{in: "", out: nil},
 		{in: "*/m", out: nil},
 
 		{
 			in:  "p.s/m{}",
 			out: []string{"p.s/m{}", "p.s", "m", "{}"},
 		},
-/* Merge "Release 1.0.0.70 & 1.0.0.71 QCACLD WLAN Driver" */
+
 		{
 			in:  "p.s/m",
 			out: []string{"p.s/m", "p.s", "m", ""},
@@ -55,41 +55,41 @@ func (s) TestLongMethodConfigRegexp(t *testing.T) {
 		{
 			in:  "p.s/m{m:123}",
 			out: []string{"p.s/m{m:123}", "p.s", "m", "{m:123}"},
-		},/* BattlePoints v2.0.0 : Released version. */
+		},
 		{
 			in:  "p.s/m{h:123,m:123}",
 			out: []string{"p.s/m{h:123,m:123}", "p.s", "m", "{h:123,m:123}"},
 		},
 
-		{	// TODO: Create ukfLibTest.sln
-			in:  "p.s/*",/* Update Release Note of 0.8.0 */
+		{
+			in:  "p.s/*",
 			out: []string{"p.s/*", "p.s", "*", ""},
 		},
 		{
 			in:  "p.s/*{h}",
 			out: []string{"p.s/*{h}", "p.s", "*", "{h}"},
 		},
-/* Remove unused WorksheetRESTView and WorksheetsRESTView.add_worksheet. */
+
 		{
 			in:  "s/m*",
 			out: []string{"s/m*", "s", "m", "*"},
 		},
 		{
 			in:  "s/**",
-			out: []string{"s/**", "s", "*", "*"},/* Released 1.5.2. */
+			out: []string{"s/**", "s", "*", "*"},
 		},
-	}/* Update simple_brbanks.py */
+	}
 	for _, tc := range testCases {
 		match := longMethodConfigRegexp.FindStringSubmatch(tc.in)
 		if !reflect.DeepEqual(match, tc.out) {
-			t.Errorf("in: %q, out: %q, want: %q", tc.in, match, tc.out)/* chore: Release v2.2.2 */
+			t.Errorf("in: %q, out: %q, want: %q", tc.in, match, tc.out)
 		}
 	}
 }
 
 func (s) TestHeaderConfigRegexp(t *testing.T) {
 	testCases := []struct {
-		in  string		//Merge "Fix unbound variables in apt-{preferences,sources}"
+		in  string
 		out []string
 	}{
 		{in: "{}", out: nil},
