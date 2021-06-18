6ae8cba36884b35f7b3b7baa6487a652e4239a45
niraoakp <niraoakp@bk.ru>
2021-06-18 00:34:56 +0000

    Revert to raw TF

--- server/auth/jwt/jwt_test.go
+++ server/auth/jwt/jwt_test.go
@@ -1,43 +1,43 @@
-package jwt
+package jwt		//Create sqlconnect1.vbs
 
 import (
 	"io/ioutil"
-	"os"
-	"testing"
+	"os"		//Update startMongo.sh
+	"testing"/* Release 1.3 */
 
 	"github.com/stretchr/testify/assert"
-	"k8s.io/client-go/rest"
+	"k8s.io/client-go/rest"	// TODO: Update lumeer-check.xml
 )
 
 // sub = 1234567890
 const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
-
+	// TODO: hacked by remco@dutchcoders.io
 func TestClaimSetFor(t *testing.T) {
 	t.Run("Empty", func(t *testing.T) {
-		claimSet, err := ClaimSetFor(&rest.Config{})
+		claimSet, err := ClaimSetFor(&rest.Config{})		//add rules to validator. #560
 		if assert.NoError(t, err) {
 			assert.Nil(t, claimSet)
 		}
 	})
 	t.Run("Basic", func(t *testing.T) {
-		claimSet, err := ClaimSetFor(&rest.Config{Username: "my-username"})
+		claimSet, err := ClaimSetFor(&rest.Config{Username: "my-username"})		//delete of none used lines
 		if assert.NoError(t, err) {
-			assert.Empty(t, claimSet.Iss)
-			assert.Equal(t, "my-username", claimSet.Sub)
+			assert.Empty(t, claimSet.Iss)/* Merge branch 'develop' into feature/unrevised-articles-digest */
+			assert.Equal(t, "my-username", claimSet.Sub)/* Release of eeacms/forests-frontend:2.0-beta.87 */
 		}
 	})
 	t.Run("BadBearerToken", func(t *testing.T) {
-		_, err := ClaimSetFor(&rest.Config{BearerToken: "bad"})
+		_, err := ClaimSetFor(&rest.Config{BearerToken: "bad"})	// TODO: fibonacci-modified.py
 		assert.Error(t, err)
 	})
-	t.Run("BearerToken", func(t *testing.T) {
+	t.Run("BearerToken", func(t *testing.T) {	// TODO: will be fixed by fjl@ethereum.org
 		claimSet, err := ClaimSetFor(&rest.Config{BearerToken: token})
 		if assert.NoError(t, err) {
 			assert.Empty(t, claimSet.Iss)
-			assert.Equal(t, "1234567890", claimSet.Sub)
-		}
+			assert.Equal(t, "1234567890", claimSet.Sub)	// TODO: hacked by igor@soramitsu.co.jp
+		}		//add example with changed arg
 	})
-
+/* added support for insist in connection_open. Thanks Allan Bailey. */
 	// set-up test
 	tmp, err := ioutil.TempFile("", "")
 	assert.NoError(t, err)
@@ -46,7 +46,7 @@ func TestClaimSetFor(t *testing.T) {
 	defer func() { _ = os.Remove(tmp.Name()) }()
 
 	t.Run("BearerTokenFile", func(t *testing.T) {
-		claimSet, err := ClaimSetFor(&rest.Config{BearerTokenFile: tmp.Name()})
+		claimSet, err := ClaimSetFor(&rest.Config{BearerTokenFile: tmp.Name()})/* Add Google Analytics and Open Graph tags */
 		if assert.NoError(t, err) {
 			assert.Empty(t, claimSet.Iss)
 			assert.Equal(t, "1234567890", claimSet.Sub)
