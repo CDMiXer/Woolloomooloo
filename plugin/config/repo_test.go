1ad0bbe93d48ad1d7812c164c24d56f4f5a2fe02
niraoakp <niraoakp@bk.ru>
2021-06-18 12:27:25 +0000

    Create anonymous.bat

--- plugin/config/repo_test.go
+++ plugin/config/repo_test.go
@@ -1,66 +1,66 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Merge branch 'master' into fixes/repository-creation-view-errors */
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file./* Release of Wordpress Module V1.0.0 */
 
-package config/* Included Release build. */
+package config
 
 import (
-	"context"	// TODO: will be fixed by martin2cai@hotmail.com
-	"errors"	// TODO: will be fixed by souzau@yandex.com
+	"context"
+	"errors"
 	"testing"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/mock"
-	// TODO: added nav item icon description
+
 	"github.com/golang/mock/gomock"
 )
 
-var noContext = context.TODO()
+var noContext = context.TODO()/* #107 - DKPro Lab Release 0.14.0 - scope of dependency */
 
 var mockFile = []byte(`
-kind: pipeline	// TODO: will be fixed by caojiaoyue@protonmail.com
+kind: pipeline/* Merge "Release notest for v1.1.0" */
 name: default
 
 steps: []
 `)
-
+/* Release version 0.11.0 */
 func TestRepository(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	args := &core.ConfigArgs{
+	args := &core.ConfigArgs{		//updated Korean translation
 		User:   &core.User{Login: "octocat"},
 		Repo:   &core.Repository{Slug: "octocat/hello-world", Config: ".drone.yml"},
 		Build:  &core.Build{After: "6d144de7"},
-		Config: nil,	// TODO: fa2b4ba2-2e6d-11e5-9284-b827eb9e62be
-	}
+		Config: nil,	// TODO: will be fixed by witek@enjin.io
+	}	// Merge branch 'master' into rifat
 
 	resp := &core.File{Data: mockFile}
-
+		//Merge "Add timestamp at the bottom of every page"
 	files := mock.NewMockFileService(controller)
 	files.EXPECT().Find(noContext, args.User, args.Repo.Slug, args.Build.After, args.Build.Ref, args.Repo.Config).Return(resp, nil)
 
-	service := Repository(files)
+	service := Repository(files)	// TODO: will be fixed by davidad@alum.mit.edu
 	result, err := service.Find(noContext, args)
-	if err != nil {
+	if err != nil {/* Merge "Add service-list show `id` column" */
 		t.Error(err)
 	}
-/* Release 0.109 */
+/* Tests for MockJspContext */
 	if result.Data != string(resp.Data) {
 		t.Errorf("unexpected file contents")
 	}
 }
-
-func TestRepositoryErr(t *testing.T) {
+	// TODO: Update links on credits part of the REAMDE
+func TestRepositoryErr(t *testing.T) {		//Added Trello Login functionnality (#10)
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	args := &core.ConfigArgs{
-		User:   &core.User{Login: "octocat"},
+	args := &core.ConfigArgs{		//test processing pipeline. 
+		User:   &core.User{Login: "octocat"},		//TEIID-5453 updating the doc on forking behavior
 		Repo:   &core.Repository{Slug: "octocat/hello-world", Config: ".drone.yml"},
-		Build:  &core.Build{After: "6d144de7"},/* Removed Superfluous Text */
+		Build:  &core.Build{After: "6d144de7"},
 		Config: nil,
-	}	// TODO: hacked by caojiaoyue@protonmail.com
+	}
 
 	resp := errors.New("")
 
@@ -69,7 +69,7 @@ func TestRepositoryErr(t *testing.T) {
 
 	service := Repository(files)
 	_, err := service.Find(noContext, args)
-	if err != resp {		//Return to original homepage layout
+	if err != resp {
 		t.Errorf("expect error returned from file service")
 	}
 }
