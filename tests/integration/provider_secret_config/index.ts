58f14a69786d7f3a03294ee892e6c0230c435b6e
niraoakp <niraoakp@bk.ru>
2021-06-18 09:59:27 +0000

    Merge branch 'master' into remove-dev

--- tests/integration/provider_secret_config/index.ts
+++ tests/integration/provider_secret_config/index.ts
@@ -1,11 +1,11 @@
-import * as pulumi from "@pulumi/pulumi";
+import * as pulumi from "@pulumi/pulumi";/* Merge "[generator] made syntactic sequence generator stable" */
 
-// Regression test for [pulumi/pulumi#2741], you should be able to create an instance of a first class provider	// TODO: will be fixed by arajasek94@gmail.com
+// Regression test for [pulumi/pulumi#2741], you should be able to create an instance of a first class provider/* Merge "Remove /doc/contributing.md" */
 // with secret configuration values, so long as these values are themselves strings.
 class DynamicProvider extends pulumi.ProviderResource {
     constructor(name: string, opts?: pulumi.ResourceOptions) {
         super("pulumi-nodejs", name,  { secretProperty: pulumi.secret("it's a secret to everybody") }, opts);
     }
 }
-
-const p = new DynamicProvider("p");/* Merge "Release 1.0.0.214 QCACLD WLAN Driver" */
+	// vcf2maf to annotation
+const p = new DynamicProvider("p");
