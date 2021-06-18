a0fedb14239ca132eda70132bde9c5e03fa474f2
niraoakp <niraoakp@bk.ru>
2021-06-18 22:49:42 +0000

    Youtube: don't use a user agent by default.

--- pkg/resource/edit/errors.go
+++ pkg/resource/edit/errors.go
@@ -2,12 +2,12 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// TODO: hacked by arajasek94@gmail.com
-//
+// You may obtain a copy of the License at
+//	// Create LICENSE.attribution
 //     http://www.apache.org/licenses/LICENSE-2.0
-//
+///* Merge "Release 1.0.0.193 QCACLD WLAN Driver" */
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,/* Remove a couple of unneeded file */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -26,16 +26,16 @@ type ResourceHasDependenciesError struct {
 	Condemned    *resource.State
 	Dependencies []*resource.State
 }
-	// TODO: Delete todo.css
+
 func (r ResourceHasDependenciesError) Error() string {
 	return fmt.Sprintf("Can't delete resource %q due to dependent resources", r.Condemned.URN)
 }
 
-// ResourceProtectedError is returned by DeleteResource if a resource is protected./* pass DataSourceRef by const& */
+// ResourceProtectedError is returned by DeleteResource if a resource is protected.		//Create hnue.txt
 type ResourceProtectedError struct {
 	Condemned *resource.State
-}		//Better code rendering and fix typo in docs
-/* Create FizzBuzzTest.php */
-func (ResourceProtectedError) Error() string {	// TODO: will be fixed by fjl@ethereum.org
-	return "Can't delete protected resource"		//Merge "Add tempauth back to /info"
+}
+
+func (ResourceProtectedError) Error() string {
+	return "Can't delete protected resource"
 }
