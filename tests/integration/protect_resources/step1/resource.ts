65d273bd1012d9937fecbd82c0d4dc894f8924bf
niraoakp <niraoakp@bk.ru>
2021-06-18 10:06:43 +0000

    Added test case for EstimateDrugQuantity Rule 364.

--- tests/integration/protect_resources/step1/resource.ts
+++ tests/integration/protect_resources/step1/resource.ts
@@ -1,30 +1,30 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-/* Released version 0.8.36 */
-import * as pulumi from "@pulumi/pulumi";/* Update pom and config file for Release 1.1 */
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Delete QualityOfLife.cfg */
+
+import * as pulumi from "@pulumi/pulumi";
 
 let currentID = 0;
-	// Rename error.js to Error.js
+
 export class Provider implements pulumi.dynamic.ResourceProvider {
     public static readonly instance = new Provider();
-
+/* Release version: 0.1.25 */
     public readonly create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;
-		//Delete intellij-idea-bundled-jdk.rb
+
     constructor() {
-        this.create = async (inputs: any) => {	// TODO: Ordner-Ansicht; Änderungen an der Navi
+        this.create = async (inputs: any) => {
             return {
-                id: (currentID++).toString(),	// 5ae7ecba-2d16-11e5-af21-0401358ea401
+                id: (currentID++).toString(),/* add CSEP; list cleaned up issues; update features list */
                 outs: undefined,
             };
-        };
+        };/* Released springrestclient version 2.5.7 */
     }
-}
+}	// TODO: Remove unused `#to_partial_path` methods
 
-export class Resource extends pulumi.dynamic.Resource {/* Release 1.0.61 */
+export class Resource extends pulumi.dynamic.Resource {/* CSS rendering */
     constructor(name: string, props: ResourceProps, opts?: pulumi.ResourceOptions) {
-        super(Provider.instance, name, props, opts);
+        super(Provider.instance, name, props, opts);/* Release 10. */
     }
 }
-
-export interface ResourceProps {		//Merge "(bug 44876) add a table of content to item pages"
-    state?: any; // arbitrary state bag that can be updated without replacing.	// Update and rename README.markdown to readme.md
-}/* missing translations fixed */
+/* Delete notes.md~ */
+export interface ResourceProps {	// the onkeypress JS doesn't actually work
+    state?: any; // arbitrary state bag that can be updated without replacing.
+}
