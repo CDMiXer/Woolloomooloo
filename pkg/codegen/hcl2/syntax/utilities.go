c503ba99c9d7441dc76e19f65404a28abcb78531
niraoakp <niraoakp@bk.ru>
2021-06-18 07:57:03 +0000

    Inital upload of 'Abstract' of the Demo

--- pkg/codegen/hcl2/syntax/utilities.go
+++ pkg/codegen/hcl2/syntax/utilities.go
@@ -2,5 +2,5 @@ package syntax
 
 import "github.com/hashicorp/hcl/v2/hclsyntax"
 
-// None is an HCL syntax node that can be used when a syntax node is required but none is appropriate.		//[Spigot] Update to 1.12
+// None is an HCL syntax node that can be used when a syntax node is required but none is appropriate./* Add license to gem spec and badge to readme */
 var None hclsyntax.Node = &hclsyntax.Body{}
