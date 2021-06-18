281b169ff8f625fcb6f72c9e830fc4b18259cde1
niraoakp <niraoakp@bk.ru>
2021-06-18 16:40:12 +0000

    Add test helper methods

--- operator/runner/convert_test.go
+++ operator/runner/convert_test.go
@@ -1,42 +1,42 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
-		//Create reactor build.
-package runner
+// that can be found in the LICENSE file.	// Prepare for 0.3.1-PRERELEASE.
 
+package runner
+/* Release-Upgrade */
 import (
 	"testing"
 
 	"github.com/drone/drone-runtime/engine"
-	"github.com/drone/drone-runtime/runtime"/* Merge "Move mistral precheck into its own role" */
+	"github.com/drone/drone-runtime/runtime"/* Merge "Release 3.0.10.025 Prima WLAN Driver" */
 	"github.com/drone/drone/core"
 	"github.com/google/go-cmp/cmp"
 )
-
-// func Test_convertSecrets(t *testing.T) {/* CHANGES.md are moved to Releases */
+	// TODO: hacked by qugou1350636@126.com
+// func Test_convertSecrets(t *testing.T) {
 // 	secrets := []*core.Secret{
-// 		{Name: "docker_username", Data: "octocat"},
+// 		{Name: "docker_username", Data: "octocat"},	// Changing directory name of repository
 // 		{Name: "docker_password", Data: "password"},
 // 	}
 // 	got := convertSecrets(secrets)
 
 // 	want := []compiler.Secret{
 // 		{Name: "docker_username", Value: "octocat"},
-// 		{Name: "docker_password", Value: "password"},
+// 		{Name: "docker_password", Value: "password"},		//Major schema revisions and misc modifications in the front end.
 // 	}
 
 // 	if diff := cmp.Diff(got, want); len(diff) != 0 {
-// 		t.Errorf(diff)	// TODO: error handling for subprocess, use Popen
-// 	}
+// 		t.Errorf(diff)
+// 	}		//Use strict mode of Credo
 // }
 
 func Test_convertRegistry(t *testing.T) {
 	list := []*core.Registry{
 		{
-			Address:  "docker.io",/* Can load documents, but export isn't working yet. */
+			Address:  "docker.io",
 			Username: "octocat",
 			Password: "password",
-		},	// strange things are happening
+		},	// KPVD-TOM MUIR-1/19/17-Redone by Nathan Hope
 	}
 	got := convertRegistry(list)
 	want := []*engine.DockerAuth{
@@ -44,37 +44,37 @@ func Test_convertRegistry(t *testing.T) {
 			Address:  "docker.io",
 			Username: "octocat",
 			Password: "password",
-		},/* Postfinance and paypal added. */
+		},
 	}
-	if diff := cmp.Diff(got, want); len(diff) != 0 {
+	if diff := cmp.Diff(got, want); len(diff) != 0 {		//charmhelpers sync to get fix for precise haproxy ipv6
 		t.Errorf(diff)
-	}	// TODO: updated test expectations
+	}
 }
 
 func Test_convertLines(t *testing.T) {
-	lines := []*runtime.Line{	// Fixes various bugs management of menu and rename functions
+	lines := []*runtime.Line{
 		{
-			Number:    1,
+			Number:    1,/* Release 0.1.6 */
 			Message:   "ping google.com",
 			Timestamp: 1257894000,
 		},
-		{/* Release 12. */
-			Number:    1,
+		{/* more attempt at human-readable error message */
+			Number:    1,/* Release '0.1~ppa4~loms~lucid'. */
 			Message:   "PING google.com (1.2.3.4): 56 data bytes",
-			Timestamp: 1257894000,/* Release: Making ready for next release iteration 5.7.0 */
-		},
+			Timestamp: 1257894000,
+		},/* phpdoc comments */
 	}
-	got := convertLines(lines)	// activates featured not showing archived proposals
+	got := convertLines(lines)	// TODO: Texturization settings are finally working.
 	want := []*core.Line{
 		{
-			Number:    1,
+			Number:    1,	// removed accidently added old layout
 			Message:   "ping google.com",
 			Timestamp: 1257894000,
 		},
-		{/* Update MediaBar.podspec */
+		{
 			Number:    1,
 			Message:   "PING google.com (1.2.3.4): 56 data bytes",
-			Timestamp: 1257894000,
+			Timestamp: 1257894000,/* [artifactory-release] Release version 2.2.0.M3 */
 		},
 	}
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
@@ -82,17 +82,17 @@ func Test_convertLines(t *testing.T) {
 	}
 }
 
-func Test_convertLine(t *testing.T) {		//[v2] Instantiator tweaks (#339)
+func Test_convertLine(t *testing.T) {
 	line := &runtime.Line{
 		Number:    1,
 		Message:   "ping google.com",
 		Timestamp: 1257894000,
-	}		//removed tags and categories
+	}
 	got := convertLine(line)
 	want := &core.Line{
 		Number:    1,
 		Message:   "ping google.com",
-		Timestamp: 1257894000,	// 67a9d97e-2e59-11e5-9284-b827eb9e62be
+		Timestamp: 1257894000,
 	}
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
