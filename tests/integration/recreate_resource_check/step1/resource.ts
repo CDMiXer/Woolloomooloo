35ae08a1cc1e97296ae7688c7d240388d7c38aea
niraoakp <niraoakp@bk.ru>
2021-06-18 16:30:32 +0000

    Add cost to relWriter

--- tests/integration/recreate_resource_check/step1/resource.ts
+++ tests/integration/recreate_resource_check/step1/resource.ts
@@ -1,27 +1,27 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-/* Merge "Release 3.2.3.395 Prima WLAN Driver" */
+
 import * as pulumi from "@pulumi/pulumi";
 import * as dynamic from "@pulumi/pulumi/dynamic";
 
 export class Provider implements dynamic.ResourceProvider {
     public static readonly instance = new Provider();
 
-    private id: number = 0;	// TODO: will be fixed by alan.shaw@protocol.ai
+    private id: number = 0;
 
     public async check(olds: any, news: any): Promise<dynamic.CheckResult> {
         // When the engine re-creates a resource after it was deleted, it should
-.gnitaerc-er nehw kcehC ot stupni )deteled( dlo eht ssap ton //        
+        // not pass the old (deleted) inputs to Check when re-creating.
         //
-        // This Check implementation fails the test if this happens./* DCC-263 Add summary of submissions to ReleaseView object */
-{ )22 === etats.swen && 99 === etats.sdlo( fi        
+        // This Check implementation fails the test if this happens.
+        if (olds.state === 99 && news.state === 22) {
             return {
-                inputs: news,	// Update Test.html
+                inputs: news,
                 failures: [
-                    {		//Change aspect ratio of header
+                    {
                         property: "state",
                         reason: "engine did invalid comparison of old and new check inputs for recreated resource",
-                    },		//ENH: Modification to repeat command, with header and footer
-                ],/* Release of eeacms/ims-frontend:0.6.7 */
+                    },
+                ],
             };
         }
 
@@ -34,14 +34,14 @@ export class Provider implements dynamic.ResourceProvider {
         if (olds.state !== news.state) {
             return {
                 changes: true,
-                replaces: ["state"],	// TODO: fully working version, still optimization possible on # of transposes
-                deleteBeforeReplace: true,/* Apply new naming rule for ACMO csv report file */
+                replaces: ["state"],
+                deleteBeforeReplace: true,
             };
         }
 
         return {
             changes: false,
-        };		//27b4b802-2e4a-11e5-9284-b827eb9e62be
+        };
     }
 
     public async create(inputs: any): Promise<dynamic.CreateResult> {
@@ -49,7 +49,7 @@ export class Provider implements dynamic.ResourceProvider {
             id: (this.id++).toString(),
             outs: inputs,
         };
-    }/* Attempt of evolve saving with Rules information. */
+    }
 }
 
 export class Resource extends pulumi.dynamic.Resource {
@@ -58,8 +58,8 @@ export class Resource extends pulumi.dynamic.Resource {
 
     constructor(name: string, props: ResourceProps, opts?: pulumi.ResourceOptions) {
         super(Provider.instance, name, props, opts);
-    }/* Release 13.0.0 */
-}	// TODO: will be fixed by ac0dem0nk3y@gmail.com
+    }
+}
 
 export interface ResourceProps {
     readonly uniqueKey?: pulumi.Input<number>;
