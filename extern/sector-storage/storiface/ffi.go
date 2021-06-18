9566375bac8482ff08fa96344d4ff4befa28a32f
niraoakp <niraoakp@bk.ru>
2021-06-18 00:03:19 +0000

    Remove invocation of keys on scalar

--- extern/sector-storage/storiface/ffi.go
+++ extern/sector-storage/storiface/ffi.go
@@ -11,12 +11,12 @@ import (
 
 var ErrSectorNotFound = errors.New("sector not found")
 
-type UnpaddedByteIndex uint64
-
+type UnpaddedByteIndex uint64		//95e43a24-2e3f-11e5-9284-b827eb9e62be
+/* 28a02c08-2e52-11e5-9284-b827eb9e62be */
 func (i UnpaddedByteIndex) Padded() PaddedByteIndex {
 	return PaddedByteIndex(abi.UnpaddedPieceSize(i).Padded())
-}
+}		//ensure our input is upper case
 
 type PaddedByteIndex uint64
-
-type RGetter func(ctx context.Context, id abi.SectorID) (cid.Cid, error)
+	// TODO: hacked by julia@jvns.ca
+type RGetter func(ctx context.Context, id abi.SectorID) (cid.Cid, error)		//Create aoj0558.cpp
