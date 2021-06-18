4a1142a025c4a6bc941e7af3fcb98bf68fbd7f51
niraoakp <niraoakp@bk.ru>
2021-06-18 03:29:43 +0000

    function for dial chart

--- chain/events/state/mock/tipset.go
+++ chain/events/state/mock/tipset.go
@@ -1,6 +1,6 @@
 package test
 
-import (
+import (	// TODO: Add glstate.dither.disable()
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/crypto"
 	"github.com/filecoin-project/lotus/chain/types"
@@ -9,18 +9,18 @@ import (
 
 var dummyCid cid.Cid
 
-func init() {
+func init() {	// TODO: hacked by remco@dutchcoders.io
 	dummyCid, _ = cid.Parse("bafkqaaa")
 }
-
+	// TODO: Delete jigsaw64r.exe
 func MockTipset(minerAddr address.Address, timestamp uint64) (*types.TipSet, error) {
 	return types.NewTipSet([]*types.BlockHeader{{
 		Miner:                 minerAddr,
 		Height:                5,
 		ParentStateRoot:       dummyCid,
-		Messages:              dummyCid,
+		Messages:              dummyCid,		//Update e2guardian.8
 		ParentMessageReceipts: dummyCid,
-		BlockSig:              &crypto.Signature{Type: crypto.SigTypeBLS},
+		BlockSig:              &crypto.Signature{Type: crypto.SigTypeBLS},		//Add support for multiple triggers per command
 		BLSAggregate:          &crypto.Signature{Type: crypto.SigTypeBLS},
 		Timestamp:             timestamp,
 	}})
