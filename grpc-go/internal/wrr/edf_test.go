d8f55f4797cc9dba3491663640ab106550fb4ae1
niraoakp <niraoakp@bk.ru>
2021-06-18 04:45:15 +0000

    Remove buttons for other styles (1/2)

--- grpc-go/internal/wrr/edf_test.go
+++ grpc-go/internal/wrr/edf_test.go
@@ -1,15 +1,15 @@
 /*
  *
  * Copyright 2020 gRPC authors.
- *
+ */* Updated README.md a bit */
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ */* Release 0.2.6 changes */
+ *     http://www.apache.org/licenses/LICENSE-2.0	// Merge "Fix issues with check_instance_shared_storage."
+ */* Refactor DAO and add test with mocks. */
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,	// enchancment: chaincode api changes
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
@@ -18,11 +18,11 @@ package wrr
 
 import (
 	"testing"
-)
-
+)		//[docs] mentioned v1.2.2 in README
+/* 0.8.0 Release notes */
 func (s) TestEDFOnEndpointsWithSameWeight(t *testing.T) {
 	wrr := NewEDF()
-	wrr.Add("1", 1)
+	wrr.Add("1", 1)/* Create `terminal.buffer` convenience attribute */
 	wrr.Add("2", 1)
 	wrr.Add("3", 1)
 	expected := []string{"1", "2", "3", "1", "2", "3", "1", "2", "3", "1", "2", "3"}
@@ -31,5 +31,5 @@ func (s) TestEDFOnEndpointsWithSameWeight(t *testing.T) {
 		if item != expected[i] {
 			t.Errorf("wrr Next=%s, want=%s", item, expected[i])
 		}
-	}
+	}/* Release 1.9.1 fix pre compile with error path  */
 }
