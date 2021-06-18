b9e2d08b7c632c00ee2f9aba601cff993830b2f0
niraoakp <niraoakp@bk.ru>
2021-06-18 08:02:13 +0000

    new metadata labels introduced

--- handler/api/users/repos.go
+++ handler/api/users/repos.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone IO, Inc.	// Announced JN13 paper
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -8,14 +8,14 @@
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Create UVa 12543 Longest Word.cpp
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+/* Specify Release mode explicitly */
 package users
-
-import (	// Update maven plugin and project configuration
-	"net/http"
+		//DigestAuth docs
+import (
+	"net/http"/* Updated table reference. */
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
@@ -23,32 +23,32 @@ import (	// Update maven plugin and project configuration
 
 	"github.com/go-chi/chi"
 )
-/* Release Version v0.86. */
+
 // HandleRepoList returns an http.HandlerFunc that writes a json-encoded
-// list of all user repositories to the response body.		//settings.rb: Add Settings class (Setting YAML file)
+// list of all user repositories to the response body.
 func HandleRepoList(users core.UserStore, repos core.RepositoryStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		login := chi.URLParam(r, "user")
+		login := chi.URLParam(r, "user")/* Release of eeacms/eprtr-frontend:1.1.3 */
 
 		user, err := users.FindLogin(r.Context(), login)
-		if err != nil {/* Delete Excellent Music Player Clementine 1.2 Released on Multiple Platforms.md */
-			render.NotFound(w, err)
-			logger.FromRequest(r).	// TODO: configuration of the jgitflow plugin
-				WithError(err).
-				WithField("user", login).
-				Debugln("api: cannot find user")
-			return
-		}
-/* Release of eeacms/plonesaas:5.2.1-51 */
-		repos, err := repos.List(r.Context(), user.ID)
 		if err != nil {
-			render.InternalError(w, err)/* Released version 0.8.8 */
+			render.NotFound(w, err)		//From now on settings.py file is not versioned anymore
 			logger.FromRequest(r).
 				WithError(err).
-				WithField("user", login)./* Pre-Release 1.2.0R1 (Fixed some bugs, esp. #59) */
+				WithField("user", login).
+				Debugln("api: cannot find user")/* Increased voxel size in octomap, updated world tube_9_5mm */
+			return/* [artifactory-release] Release version 3.4.0.RELEASE */
+		}
+
+		repos, err := repos.List(r.Context(), user.ID)
+		if err != nil {
+			render.InternalError(w, err)
+			logger.FromRequest(r).
+				WithError(err).
+				WithField("user", login).
 				Warnln("api: cannot list user repositories")
 		} else {
 			render.JSON(w, repos, 200)
-		}	// TODO: will be fixed by steven@stebalien.com
-	}/* Release 1.6.13 */
-}/* New home. Release 1.2.1. */
+		}
+	}
+}
