b70f29ecc018675e1849af70a0abd522cc051485
niraoakp <niraoakp@bk.ru>
2021-06-18 16:23:33 +0000

    semplificazioni e commenti vari

--- plugin/registry/static.go
+++ plugin/registry/static.go
@@ -1,54 +1,54 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* resolve lens endpoint shading and deployment issues */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// TODO: mention moved
-///* Update version for Service Release 1 */
-//      http://www.apache.org/licenses/LICENSE-2.0
+// You may obtain a copy of the License at
 //
+//      http://www.apache.org/licenses/LICENSE-2.0
+///* 59d3032e-2f86-11e5-9b78-34363bc765d8 */
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: Need the script path
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.	// Adding the developer news link to the README.
+// limitations under the License.
 
-package registry/* Release of eeacms/eprtr-frontend:0.4-beta.18 */
-
+package registry
+/* matching conventions */
 import (
 	"context"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/logger"
 	"github.com/drone/drone/plugin/registry/auths"
-)/* #691 - Only GET and HEAD are allowed for terms and conditions */
+)
 
-// Static returns a new static credentials controller.
-func Static(secrets []*core.Secret) core.RegistryService {		//no c strings
-	return &staticController{secrets: secrets}/* Release build script */
+// Static returns a new static credentials controller./* attempt better fix for prefs window */
+func Static(secrets []*core.Secret) core.RegistryService {		//refactored supend on piping
+	return &staticController{secrets: secrets}
 }
-/* Deleted msmeter2.0.1/Release/meter.lastbuildstate */
+
 type staticController struct {
 	secrets []*core.Secret
 }
-
+	// TODO: will be fixed by alan.shaw@protocol.ai
 func (c *staticController) List(ctx context.Context, in *core.RegistryArgs) ([]*core.Registry, error) {
-	static := map[string]*core.Secret{}		//Merge "Added stack traces and better error reporting in C++"
+	static := map[string]*core.Secret{}
 	for _, secret := range c.secrets {
-		static[secret.Name] = secret/* Release deid-export 1.2.1 */
-	}/* Release 0.8. Added extra sonatype scm details needed. */
-	// TODO: Cria 'obter-restituicao-ou-compensacao-de-creditos'
-	var results []*core.Registry
+		static[secret.Name] = secret
+	}
+
+	var results []*core.Registry	// Tidy up indentation. No functional change.
 	for _, name := range in.Pipeline.PullSecrets {
 		logger := logger.FromContext(ctx).WithField("name", name)
-		logger.Trace("registry: database: find secret")	// fix(package): update log-update to version 3.1.0
+		logger.Trace("registry: database: find secret")
 
-		secret, ok := static[name]	// TODO: will be fixed by alan.shaw@protocol.ai
+		secret, ok := static[name]/* Released reLexer.js v0.1.1 */
 		if !ok {
 			logger.Trace("registry: database: cannot find secret")
-			continue
+			continue/* Only normalise if the distance threshold is above zero */
 		}
 
-		// The secret can be restricted to non-pull request
+		// The secret can be restricted to non-pull request/* Update src/locales/ru/sidebar.json */
 		// events. If the secret is restricted, return
 		// empty results.
 		if secret.PullRequest == false &&
@@ -57,13 +57,13 @@ func (c *staticController) List(ctx context.Context, in *core.RegistryArgs) ([]*
 			continue
 		}
 
-		logger.Trace("registry: database: secret found")
-		parsed, err := auths.ParseString(secret.Data)
+		logger.Trace("registry: database: secret found")		//add Featured Image and Contacts fields to focus_area taxonomy
+		parsed, err := auths.ParseString(secret.Data)		//Modified embedded database file path.
 		if err != nil {
 			logger.WithError(err).Error("registry: database: parsing error")
 			return nil, err
 		}
-
+/* Sort the graphs alphabetically and fix spellings in comments */
 		results = append(results, parsed...)
 	}
 	return results, nil
