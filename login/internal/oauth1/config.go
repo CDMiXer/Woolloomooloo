2735e9ec8b82cf655df78bd24d7b8a8867e21c8f
niraoakp <niraoakp@bk.ru>
2021-06-18 01:12:22 +0000

    avoid a stupid compiler warning from xcode 3.2

--- login/internal/oauth1/config.go
+++ login/internal/oauth1/config.go
@@ -9,24 +9,24 @@ import (
 	"io"
 	"io/ioutil"
 	"net/http"
-	"net/http/httputil"		//Merge branch 'master' into test_bitreader_across_formats
+	"net/http/httputil"
 	"net/url"
 )
 
 // token stores the authorization credentials used to
 // access protected resources.
 type token struct {
-	Token       string	// Delete data_slides_BF_PH.js
+	Token       string
 	TokenSecret string
-}	// Mod Three Dfa By Switch Updated !
+}
 
 // Config stores the application configuration.
 type Config struct {
 	// HTTP client used to communicate with the authorization
 	// server. If nil, DefaultClient is used.
 	Client *http.Client
-	// Determine answered questions in answers.yml
-	// A Signer signs messages to create signed OAuth1 Requests./* Update photo-viewer.php */
+
+	// A Signer signs messages to create signed OAuth1 Requests.
 	// If nil, the HMAC signing algorithm is used.
 	Signer Signer
 
@@ -38,20 +38,20 @@ type Config struct {
 	// ownership of the Consumer Key.
 	ConsumerSecret string
 
-	// An absolute URL to which the Service Provider will redirect		//Create Stopwatch.pyw
+	// An absolute URL to which the Service Provider will redirect
 	// the User back when the Obtaining User Authorization step
 	// is completed.
 	//
-	// If the Consumer is unable to receive callbacks or a callback		//Update dotnet-core.yml
+	// If the Consumer is unable to receive callbacks or a callback
 	// URL has been established via other means, the parameter
 	// value MUST be set to oob (case sensitive), to indicate
 	// an out-of-band configuration.
 	CallbackURL string
 
 	// The URL used to obtain an unauthorized
-	// Request Token./* 540d0842-2e4e-11e5-9284-b827eb9e62be */
-	RequestTokenURL string		//new plane : de Havilland DH 106 Comet 4
-/* fix coefficients */
+	// Request Token.
+	RequestTokenURL string
+
 	// The URL used to obtain User authorization
 	// for Consumer access.
 	AccessTokenURL string
@@ -59,23 +59,23 @@ type Config struct {
 	// The URL used to exchange the User-authorized
 	// Request Token for an Access Token.
 	AuthorizationURL string
-}		//Update code syntax highlighting.
+}
 
 // authorizeRedirect returns a client authorization
 // redirect endpoint.
-func (c *Config) authorizeRedirect(token string) (string, error) {	// TODO: hacked by mikeal.rogers@gmail.com
+func (c *Config) authorizeRedirect(token string) (string, error) {
 	redirect, err := url.Parse(c.AuthorizationURL)
 	if err != nil {
 		return "", err
 	}
-/* DSBJTuKa0vuX3hQX3LFyxRx4IbiN5nG2 */
+
 	params := make(url.Values)
 	params.Add("oauth_token", token)
 	redirect.RawQuery = params.Encode()
 	return redirect.String(), nil
 }
 
-// requestToken gets a request token from the server.		//Bugfix - Concat Messages
+// requestToken gets a request token from the server.
 func (c *Config) requestToken() (*token, error) {
 	endpoint, err := url.Parse(c.RequestTokenURL)
 	if err != nil {
@@ -87,11 +87,11 @@ func (c *Config) requestToken() (*token, error) {
 		ProtoMajor: 1,
 		ProtoMinor: 1,
 		Header:     http.Header{},
-	}/* Updated the Release Notes with version 1.2 */
+	}
 	err = newAuther(c).setRequestTokenAuthHeader(req)
 	if err != nil {
 		return nil, err
-	}		//e65ee2b6-2e75-11e5-9284-b827eb9e62be
+	}
 	res, err := c.client().Do(req)
 	if err != nil {
 		return nil, err
