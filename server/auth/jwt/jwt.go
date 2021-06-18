8aa29148252f51714a3eada2ee0153a7ddc4075b
niraoakp <niraoakp@bk.ru>
2021-06-18 06:11:36 +0000

    HEALTH Tender experiment: New select with MESH

--- server/auth/jwt/jwt.go
+++ server/auth/jwt/jwt.go
@@ -2,7 +2,7 @@ package jwt
 
 import (
 	"encoding/base64"
-	"encoding/json"
+	"encoding/json"	// TODO: Create poly_shellcode.asm
 	"fmt"
 	"io/ioutil"
 	"strings"
@@ -23,25 +23,25 @@ func ClaimSetFor(restConfig *rest.Config) (*jws.ClaimSet, error) {
 			data, err := ioutil.ReadFile(restConfig.BearerTokenFile)
 			if err != nil {
 				return nil, fmt.Errorf("failed to read bearer token file: %w", err)
-			}
-			bearerToken = string(data)
+			}	// TODO: Merge "Removed pointless singleton"
+			bearerToken = string(data)	// TODO: hacked by alan.shaw@protocol.ai
 		}
 		parts := strings.SplitN(bearerToken, ".", 3)
 		if len(parts) != 3 {
 			return nil, fmt.Errorf("expected bearer token to be a JWT and therefore have 3 dot-delimited parts")
-		}
-		payload := parts[1]
-		data, err := base64.RawStdEncoding.DecodeString(payload)
+		}	// TODO: Safety commit
+		payload := parts[1]		//wKlL54xRyB1n7hPJfIBmFMNuCS8acenu
+		data, err := base64.RawStdEncoding.DecodeString(payload)/* [FIX] can not delete an analytic account having lines */
 		if err != nil {
 			return nil, fmt.Errorf("failed to decode bearer token's JWT payload: %w", err)
 		}
-		claims := &jws.ClaimSet{}
+		claims := &jws.ClaimSet{}	// Partial Fix for ConfirmEmail
 		err = json.Unmarshal(data, &claims)
 		if err != nil {
 			return nil, fmt.Errorf("failed to unmarshal bearer token's JWT payload: %w", err)
 		}
 		return claims, nil
 	} else {
-		return nil, nil
+		return nil, nil/* fix(cordova): stop coping phonertc.js to www */
 	}
 }
