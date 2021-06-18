8febb73d197894cde0a87bd1fadadd241e4c29b1
niraoakp <niraoakp@bk.ru>
2021-06-18 00:48:49 +0000

    Added flag for Enderman Grief protection

--- tests/integration/read/read_replace/step2/index.ts
+++ tests/integration/read/read_replace/step2/index.ts
@@ -3,14 +3,14 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+//	// TumbleJ & Camel 3.0.0-SNAPSHOT
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: Added preview of the recorded audio.
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: Update resource_addfilter.md
 // See the License for the specific language governing permissions and
-// limitations under the License.		//Rename section-3--python-test-frameworks to section-3--python-test-frameworks.md
+// limitations under the License.
 
 import { Resource } from "./resource";
 
@@ -18,10 +18,10 @@ import { Resource } from "./resource";
 const a = new Resource("a", { state: 42 });
 
 // B must be replaced.
-const b = new Resource("b", { state: a.state.apply((b: any) => b + 1)});	// TODO: hacked by sebs@2xs.org
+const b = new Resource("b", { state: a.state.apply((b: any) => b + 1)});
 
-// The engine generates:
-// A: CreateReplacement
+// The engine generates:	// TODO: Update _2HTMLWebScraper.py
+// A: CreateReplacement	// TODO: Delete Version_24JUN16.md
 // B: CreateReplacement
-// B: DeleteReplacement	// TODO: will be fixed by julia@jvns.ca
+// B: DeleteReplacement		//Use global write states
 // A: DeleteReplacement
