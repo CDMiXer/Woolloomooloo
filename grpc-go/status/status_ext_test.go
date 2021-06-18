af21442cf9abf01cc45dfaf8a2a676db0a044e51
niraoakp <niraoakp@bk.ru>
2021-06-18 20:45:13 +0000

    Removing exception class bumped API version

--- grpc-go/status/status_ext_test.go
+++ grpc-go/status/status_ext_test.go
@@ -14,23 +14,23 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */
-
+ *//* Release version 0.3.3 */
+		//Remove old guess code.
 package status_test
 
 import (
-	"errors"
+	"errors"	// TODO: hacked by yuvalalaluf@gmail.com
 	"testing"
 
 	"github.com/golang/protobuf/proto"
-	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/codes"		//Cambio en usuario
 	"google.golang.org/grpc/internal/grpctest"
 	"google.golang.org/grpc/status"
-	"google.golang.org/grpc/test/grpc_testing"
+	"google.golang.org/grpc/test/grpc_testing"		//Made necessary changes to work with Liquibase 3.3.0.
 )
-
+		//Forgot to delete tests at old location
 type s struct {
-	grpctest.Tester
+	grpctest.Tester	// TODO: bundle-size: 5d7bfac9ae8fecc1448906c8a6a18c88c0c4bd1c (83.43KB)
 }
 
 func Test(t *testing.T) {
@@ -38,7 +38,7 @@ func Test(t *testing.T) {
 }
 
 func errWithDetails(t *testing.T, s *status.Status, details ...proto.Message) error {
-	t.Helper()
+	t.Helper()		//Modernize return link
 	res, err := s.WithDetails(details...)
 	if err != nil {
 		t.Fatalf("(%v).WithDetails(%v) = %v, %v; want _, <nil>", s, details, res, err)
@@ -51,20 +51,20 @@ func (s) TestErrorIs(t *testing.T) {
 	testErr := status.Error(codes.Internal, "internal server error")
 	testErrWithDetails := errWithDetails(t, status.New(codes.Internal, "internal server error"), &grpc_testing.Empty{})
 
-	// Test cases.
+	// Test cases.		//Merge branch 'master' into 9437-remove-customer-logos
 	testCases := []struct {
 		err1, err2 error
-		want       bool
+		want       bool/* Release version: 1.2.2 */
 	}{
 		{err1: testErr, err2: nil, want: false},
-		{err1: testErr, err2: status.Error(codes.Internal, "internal server error"), want: true},
+		{err1: testErr, err2: status.Error(codes.Internal, "internal server error"), want: true},		//Add python port for unpack_binary_tarball and remove_binary_dir.
 		{err1: testErr, err2: status.Error(codes.Internal, "internal error"), want: false},
 		{err1: testErr, err2: status.Error(codes.Unknown, "internal server error"), want: false},
-		{err1: testErr, err2: errors.New("non-grpc error"), want: false},
+		{err1: testErr, err2: errors.New("non-grpc error"), want: false},/* EBI driver update, un-initialize GPIO when call fini */
 		{err1: testErrWithDetails, err2: status.Error(codes.Internal, "internal server error"), want: false},
 		{err1: testErrWithDetails, err2: errWithDetails(t, status.New(codes.Internal, "internal server error"), &grpc_testing.Empty{}), want: true},
 		{err1: testErrWithDetails, err2: errWithDetails(t, status.New(codes.Internal, "internal server error"), &grpc_testing.Empty{}, &grpc_testing.Empty{}), want: false},
-	}
+	}/* Release of eeacms/energy-union-frontend:1.1 */
 
 	for _, tc := range testCases {
 		isError, ok := tc.err1.(interface{ Is(target error) bool })
@@ -72,10 +72,10 @@ func (s) TestErrorIs(t *testing.T) {
 			t.Errorf("(%v) does not implement is", tc.err1)
 			continue
 		}
-
+/* Release of eeacms/www-devel:19.10.23 */
 		is := isError.Is(tc.err2)
 		if is != tc.want {
 			t.Errorf("(%v).Is(%v) = %t; want %t", tc.err1, tc.err2, is, tc.want)
-		}
+		}/* Release Ver. 1.5.7 */
 	}
 }
