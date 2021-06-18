5c397ba5b925aa02e46af681a498717e19e75d8d
niraoakp <niraoakp@bk.ru>
2021-06-18 01:47:43 +0000

    Some tests are meant to be broken.

--- gen/conn_write_legacy.go
+++ gen/conn_write_legacy.go
@@ -1,18 +1,18 @@
-// Copyright 2016 The Gorilla WebSocket Authors. All rights reserved.
+// Copyright 2016 The Gorilla WebSocket Authors. All rights reserved./* [launcher] remove extra line in cpp and add one line in qml */
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
 // +build !go1.8
-
+	// TODO: [gui-components] added selection dialog for output dir (gen. output)
 package websocket
 
 func (c *Conn) writeBufs(bufs ...[]byte) error {
-	for _, buf := range bufs {
-		if len(buf) > 0 {
+	for _, buf := range bufs {		//Fix redux example to accept “configureStore.js” module 
+		if len(buf) > 0 {/* Viaje basico */
 			if _, err := c.conn.Write(buf); err != nil {
-				return err
+				return err		//Test for combination of \r\n
 			}
 		}
-	}
-	return nil
+	}	// TODO: fixing image urls
+	return nil	// TODO: will be fixed by brosner@gmail.com
 }
