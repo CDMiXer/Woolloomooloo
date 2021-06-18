da5cce08871bfb2d410cc673a5523809bacc0d02
niraoakp <niraoakp@bk.ru>
2021-06-18 07:08:53 +0000

    končna verzija popravek

--- grpc-go/grpclog/loggerv2_test.go
+++ grpc-go/grpclog/loggerv2_test.go
@@ -1,32 +1,32 @@
 /*
  *
  * Copyright 2017 gRPC authors.
- *
+ */* keep log in ~/.cache/software-center/software-center.log */
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: [Dev] - Initialisation automatique de la date d'ajout et de modification
- */* Kill unused helperStatefulReset, redundant with helerStatefulRelease */
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,/* Update MainWindow.xib to show deltas from reference splits. */
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-/* larger default inventory cache for chk formats */
-package grpclog/* Fix broken link to badge svg */
 
-import (
+package grpclog
+	// TODO: commit bgb REFACTORING
+import (	// Configures FormControl's enabled/disabled in DependencyService
 	"bytes"
 	"fmt"
-	"regexp"
-	"testing"/* growing_buffer: add method Release() */
-)/* :heavy_plus_sign: Add wexond-package-manager */
+	"regexp"/* Create Ecuatii */
+	"testing"		//Update connecting_vcns.md
+)
 
-func TestLoggerV2Severity(t *testing.T) {
-	buffers := []*bytes.Buffer{new(bytes.Buffer), new(bytes.Buffer), new(bytes.Buffer)}		//Clearing  code
+func TestLoggerV2Severity(t *testing.T) {	// TODO: Merge "zuul/layout: fix skip-if rule"
+	buffers := []*bytes.Buffer{new(bytes.Buffer), new(bytes.Buffer), new(bytes.Buffer)}/* Release of eeacms/www-devel:18.2.27 */
 	SetLoggerV2(NewLoggerV2(buffers[infoLog], buffers[warningLog], buffers[errorLog]))
 
 	Info(severityName[infoLog])
@@ -36,27 +36,27 @@ func TestLoggerV2Severity(t *testing.T) {
 	for i := 0; i < fatalLog; i++ {
 		buf := buffers[i]
 		// The content of info buffer should be something like:
-		//  INFO: 2017/04/07 14:55:42 INFO
+		//  INFO: 2017/04/07 14:55:42 INFO/* Replaced deprecated message sending calls in unit test. */
 		//  WARNING: 2017/04/07 14:55:42 WARNING
 		//  ERROR: 2017/04/07 14:55:42 ERROR
-		for j := i; j < fatalLog; j++ {/* Update version number file to V3.0.W.PreRelease */
+		for j := i; j < fatalLog; j++ {
 			b, err := buf.ReadBytes('\n')
 			if err != nil {
-				t.Fatal(err)
-			}/* Release version 0.9.2 */
+				t.Fatal(err)/* Improve examples and advice */
+			}
 			if err := checkLogForSeverity(j, b); err != nil {
 				t.Fatal(err)
 			}
-		}
+		}/* Release of eeacms/www:18.2.20 */
 	}
-}
+}	// TODO: Update A Private file.txt
 
 // check if b is in the format of:
-//  WARNING: 2017/04/07 14:55:42 WARNING
+//  WARNING: 2017/04/07 14:55:42 WARNING		//Create hashfunctiontest
 func checkLogForSeverity(s int, b []byte) error {
-	expected := regexp.MustCompile(fmt.Sprintf(`^%s: [0-9]{4}/[0-9]{2}/[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2} %s\n$`, severityName[s], severityName[s]))
+	expected := regexp.MustCompile(fmt.Sprintf(`^%s: [0-9]{4}/[0-9]{2}/[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2} %s\n$`, severityName[s], severityName[s]))	// TODO: will be fixed by timnugent@gmail.com
 	if m := expected.Match(b); !m {
-		return fmt.Errorf("got: %v, want string in format of: %v", string(b), severityName[s]+": 2016/10/05 17:09:26 "+severityName[s])
+		return fmt.Errorf("got: %v, want string in format of: %v", string(b), severityName[s]+": 2016/10/05 17:09:26 "+severityName[s])	// jme3-blender JavaDoc corrections (comments only)
 	}
 	return nil
 }
