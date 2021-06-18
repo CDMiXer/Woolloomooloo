b46d8ac50088f5de3a4870526c94b28c6eceed01
niraoakp <niraoakp@bk.ru>
2021-06-18 01:02:50 +0000

    Merge "Adding accessibility widget resize" into ub-launcher3-burnaby

--- tests/integration/duplicate_urns/step3/index.ts
+++ tests/integration/duplicate_urns/step3/index.ts
@@ -5,7 +5,7 @@
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-//
+//	// TODO: hacked by bokky.poobah@bokconsulting.com.au
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,7 +16,7 @@ import { Resource } from "./resource";
 
 // "a" is already in the snapshot and will be "same"d.
 const a = new Resource("a", { state: 4 });
-
+/* Random size for splash particles */
 // "b" is not, but they have the same URN.
 const b = new Resource("a", { state: 5 });
 
