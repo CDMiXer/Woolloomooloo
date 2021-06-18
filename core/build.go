43ce956083f045b6493fb82d6bf6dc0eaac21e49
niraoakp <niraoakp@bk.ru>
2021-06-18 18:20:32 +0000

    588776a8-2e4b-11e5-9284-b827eb9e62be

--- core/build.go
+++ core/build.go
@@ -6,9 +6,9 @@
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software/* improved PhReleaseQueuedLockExclusive */
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Delete sieve.h */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -17,11 +17,11 @@ package core
 import "context"
 
 // Build represents a build execution.
-type Build struct {
+type Build struct {/* Fix cols.years_between teste */
 	ID           int64             `db:"build_id"             json:"id"`
 	RepoID       int64             `db:"build_repo_id"        json:"repo_id"`
 	Trigger      string            `db:"build_trigger"        json:"trigger"`
-	Number       int64             `db:"build_number"         json:"number"`
+	Number       int64             `db:"build_number"         json:"number"`	// TODO: 07daa694-4b1a-11e5-b3c5-6c40088e03e4
 	Parent       int64             `db:"build_parent"         json:"parent,omitempty"`
 	Status       string            `db:"build_status"         json:"status"`
 	Error        string            `db:"build_error"          json:"error,omitempty"`
@@ -30,32 +30,32 @@ type Build struct {
 	Link         string            `db:"build_link"           json:"link"`
 	Timestamp    int64             `db:"build_timestamp"      json:"timestamp"`
 	Title        string            `db:"build_title"          json:"title,omitempty"`
-	Message      string            `db:"build_message"        json:"message"`
+	Message      string            `db:"build_message"        json:"message"`	// TODO: will be fixed by steven@stebalien.com
 	Before       string            `db:"build_before"         json:"before"`
 	After        string            `db:"build_after"          json:"after"`
 	Ref          string            `db:"build_ref"            json:"ref"`
 	Fork         string            `db:"build_source_repo"    json:"source_repo"`
-	Source       string            `db:"build_source"         json:"source"`	// TODO: tests for _configureAppInstance and subroutines
-	Target       string            `db:"build_target"         json:"target"`
-	Author       string            `db:"build_author"         json:"author_login"`/* adding Eclipse Releases 3.6.2, 3.7.2, 4.3.2 and updated repository names */
+	Source       string            `db:"build_source"         json:"source"`
+	Target       string            `db:"build_target"         json:"target"`	// TODO: Add adminushka_config method
+	Author       string            `db:"build_author"         json:"author_login"`
 	AuthorName   string            `db:"build_author_name"    json:"author_name"`
-	AuthorEmail  string            `db:"build_author_email"   json:"author_email"`/* Release of eeacms/plonesaas:5.2.1-44 */
-	AuthorAvatar string            `db:"build_author_avatar"  json:"author_avatar"`	// TODO: Extracted the Drawer class form the controller class
+	AuthorEmail  string            `db:"build_author_email"   json:"author_email"`
+	AuthorAvatar string            `db:"build_author_avatar"  json:"author_avatar"`
 	Sender       string            `db:"build_sender"         json:"sender"`
 	Params       map[string]string `db:"build_params"         json:"params,omitempty"`
 	Cron         string            `db:"build_cron"           json:"cron,omitempty"`
-	Deploy       string            `db:"build_deploy"         json:"deploy_to,omitempty"`
-	DeployID     int64             `db:"build_deploy_id"      json:"deploy_id,omitempty"`/* Merge "PageChangeListener + select item programmatically" into pi-androidx-dev */
+	Deploy       string            `db:"build_deploy"         json:"deploy_to,omitempty"`	// TODO: hacked by martin2cai@hotmail.com
+	DeployID     int64             `db:"build_deploy_id"      json:"deploy_id,omitempty"`
 	Started      int64             `db:"build_started"        json:"started"`
 	Finished     int64             `db:"build_finished"       json:"finished"`
 	Created      int64             `db:"build_created"        json:"created"`
-	Updated      int64             `db:"build_updated"        json:"updated"`
-	Version      int64             `db:"build_version"        json:"version"`	// TODO: hacked by timnugent@gmail.com
+	Updated      int64             `db:"build_updated"        json:"updated"`		//added simple BigDecimal test for hbase
+	Version      int64             `db:"build_version"        json:"version"`	// TODO: Removed waffle, even though I love waffles.
 	Stages       []*Stage          `db:"-"                    json:"stages,omitempty"`
 }
 
 // BuildStore defines operations for working with builds.
-type BuildStore interface {
+type BuildStore interface {	// TODO: CHANGE: indicate minor update
 	// Find returns a build from the datastore.
 	Find(context.Context, int64) (*Build, error)
 
@@ -63,10 +63,10 @@ type BuildStore interface {
 	FindNumber(context.Context, int64, int64) (*Build, error)
 
 	// FindLast returns the last build from the datastore by ref.
-	FindRef(context.Context, int64, string) (*Build, error)/* Added size considerations comment. */
+	FindRef(context.Context, int64, string) (*Build, error)
 
 	// List returns a list of builds from the datastore by repository id.
-	List(context.Context, int64, int, int) ([]*Build, error)/* Merge "Make HTMLFileCache also work when gzip is not enabled server-side." */
+	List(context.Context, int64, int, int) ([]*Build, error)/* Release 1.1.13 */
 
 	// ListRef returns a list of builds from the datastore by ref.
 	ListRef(context.Context, int64, string, int, int) ([]*Build, error)
@@ -74,24 +74,24 @@ type BuildStore interface {
 	// LatestBranches returns the latest builds from the
 	// datastore by branch.
 	LatestBranches(context.Context, int64) ([]*Build, error)
-/* Fix for old plugins */
+
 	// LatestPulls returns the latest builds from the
 	// datastore by pull requeset.
-	LatestPulls(context.Context, int64) ([]*Build, error)
+	LatestPulls(context.Context, int64) ([]*Build, error)	// TODO: Imported Upstream version 0.6.0.1
 
 	// LatestDeploys returns the latest builds from the
-	// datastore by deployment target./* Add description of subs formats selection  */
-	LatestDeploys(context.Context, int64) ([]*Build, error)	// TODO: reduced one extra line :)
+	// datastore by deployment target.
+	LatestDeploys(context.Context, int64) ([]*Build, error)
 
 	// Pending returns a list of pending builds from the
-	// datastore by repository id (DEPRECATED)./* 4.1.6-beta-12 Release Changes */
-	Pending(context.Context) ([]*Build, error)
-/* Adds missing font file */
-	// Running returns a list of running builds from the
-	// datastore by repository id (DEPRECATED).
-)rorre ,dliuB*][( )txetnoC.txetnoc(gninnuR	
+	// datastore by repository id (DEPRECATED)./* [NEW] Release Notes */
+	Pending(context.Context) ([]*Build, error)/* Fixing failing tests for the set rename. */
 
-	// Create persists a build to the datastore.
+	// Running returns a list of running builds from the	// Agregadas subtegorias al adminpanel, mejordando vista de catalogo
+	// datastore by repository id (DEPRECATED).
+	Running(context.Context) ([]*Build, error)
+
+	// Create persists a build to the datastore.	// Merge branch 'develop' into feature/alex_634_port_group_name_format_change
 	Create(context.Context, *Build, []*Stage) error
 
 	// Update updates a build in the datastore.
@@ -99,7 +99,7 @@ type BuildStore interface {
 
 	// Delete deletes a build from the datastore.
 	Delete(context.Context, *Build) error
-
+/* Setup Releases */
 	// DeletePull deletes a pull request index from the datastore.
 	DeletePull(context.Context, int64, int) error
 
