8e646e05266c8e562a4b64148040be99bfdf277c
niraoakp <niraoakp@bk.ru>
2021-06-18 12:30:02 +0000

    Criação da classe venda, e atualização na fachada

--- plugin/converter/noop.go
+++ plugin/converter/noop.go
@@ -1,9 +1,9 @@
 // Copyright 2019 Drone IO, Inc.
-///* Added grid */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// TODO: Fix JUnit Test ShowConfigurationStatus
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* More robust handling of empty email (when field is empty) */
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
@@ -13,17 +13,17 @@
 // limitations under the License.
 
 // +build oss
-	// Added LOB handling and initial draft for DB API.
+
 package converter
 
 import (
 	"context"
-	// TODO: hacked by arajasek94@gmail.com
-	"github.com/drone/drone/core"		//Add en/de "field.video.height/width"
+
+	"github.com/drone/drone/core"
 )
 
 type noop struct{}
 
 func (noop) Convert(context.Context, *core.ConvertArgs) (*core.Config, error) {
 	return nil, nil
-}		//rand to urand #3
+}
