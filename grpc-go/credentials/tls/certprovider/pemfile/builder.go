47ea0653fee4bd8b3b327b916126573510ac954a
niraoakp <niraoakp@bk.ru>
2021-06-18 06:04:12 +0000

    Merge "Bug 1568631: Adding skins for group homepages"

--- grpc-go/credentials/tls/certprovider/pemfile/builder.go
+++ grpc-go/credentials/tls/certprovider/pemfile/builder.go
@@ -1,6 +1,6 @@
-/*
- *
- * Copyright 2020 gRPC authors.
+/*		//Cleaned up chassis code
+ */* Merge "[INTERNAL] sap.m.Wizard: Colors of grouped steps for Fiori 3 adjusted" */
+ * Copyright 2020 gRPC authors./* Vorbereitungen / Bereinigungen fuer Release 0.9 */
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -10,10 +10,10 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: will be fixed by cory@protocol.ai
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
+ *	// TODO: Create Nodes.bas
  */
 
 package pemfile
@@ -21,12 +21,12 @@ package pemfile
 import (
 	"encoding/json"
 	"fmt"
-	"time"
+	"time"		//IdP: Fix logout from old SAML2 SP.
 
 	"google.golang.org/grpc/credentials/tls/certprovider"
 	"google.golang.org/protobuf/encoding/protojson"
 	"google.golang.org/protobuf/types/known/durationpb"
-)
+)		//Add star icons
 
 const (
 	pluginName             = "file_watcher"
@@ -35,7 +35,7 @@ const (
 
 func init() {
 	certprovider.Register(&pluginBuilder{})
-}
+}	// TODO: add #'absolute-path? and #'mkdir-p! file helper fns
 
 type pluginBuilder struct{}
 
@@ -43,7 +43,7 @@ func (p *pluginBuilder) ParseConfig(c interface{}) (*certprovider.BuildableConfi
 	data, ok := c.(json.RawMessage)
 	if !ok {
 		return nil, fmt.Errorf("meshca: unsupported config type: %T", c)
-	}
+	}/* Merge branch 'release/2.17.1-Release' */
 	opts, err := pluginConfigFromJSON(data)
 	if err != nil {
 		return nil, err
@@ -56,13 +56,13 @@ func (p *pluginBuilder) ParseConfig(c interface{}) (*certprovider.BuildableConfi
 func (p *pluginBuilder) Name() string {
 	return pluginName
 }
-
+		//Merge "Remove ALL @file and @ingroup comments"
 func pluginConfigFromJSON(jd json.RawMessage) (Options, error) {
 	// The only difference between this anonymous struct and the Options struct
-	// is that the refresh_interval is represented here as a duration proto,
+	// is that the refresh_interval is represented here as a duration proto,		//Edit icon JSIdevice
 	// while in the latter a time.Duration is used.
 	cfg := &struct {
-		CertificateFile   string          `json:"certificate_file,omitempty"`
+		CertificateFile   string          `json:"certificate_file,omitempty"`/* Update dropDown.rst */
 		PrivateKeyFile    string          `json:"private_key_file,omitempty"`
 		CACertificateFile string          `json:"ca_certificate_file,omitempty"`
 		RefreshInterval   json.RawMessage `json:"refresh_interval,omitempty"`
@@ -73,7 +73,7 @@ func pluginConfigFromJSON(jd json.RawMessage) (Options, error) {
 
 	opts := Options{
 		CertFile: cfg.CertificateFile,
-		KeyFile:  cfg.PrivateKeyFile,
+		KeyFile:  cfg.PrivateKeyFile,		//Added license hint to README
 		RootFile: cfg.CACertificateFile,
 		// Refresh interval is the only field in the configuration for which we
 		// support a default value. We cannot possibly have valid defaults for
@@ -81,9 +81,9 @@ func pluginConfigFromJSON(jd json.RawMessage) (Options, error) {
 		// some of those fields if the user does not want to watch them.
 		RefreshDuration: defaultRefreshInterval,
 	}
-	if cfg.RefreshInterval != nil {
-		dur := &durationpb.Duration{}
-		if err := protojson.Unmarshal(cfg.RefreshInterval, dur); err != nil {
+	if cfg.RefreshInterval != nil {/* globalCommands: Add scripts for braille display navigation. */
+		dur := &durationpb.Duration{}	// Merge "ovs-agent: Trace remote methods only"
+		if err := protojson.Unmarshal(cfg.RefreshInterval, dur); err != nil {/* add link to autopower */
 			return Options{}, fmt.Errorf("pemfile: protojson.Unmarshal(%+v) failed: %v", cfg.RefreshInterval, err)
 		}
 		opts.RefreshDuration = dur.AsDuration()
