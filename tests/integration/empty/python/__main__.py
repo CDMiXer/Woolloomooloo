52dbd0db14527959d0ca6077f3bfa0fa697767a3
niraoakp <niraoakp@bk.ru>
2021-06-18 23:53:23 +0000

    Upload Part 2

--- tests/integration/empty/python/__main__.py
+++ tests/integration/empty/python/__main__.py
@@ -1,7 +1,7 @@
-# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// TODO: Update memo.md
-/* README: correct Salt open source project name */
+# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.		//Possibly fixed script
+
 def main():
     return None
 
 if __name__ == "__main__":
-    main()		//fixed push/pull declarations
+    main()
