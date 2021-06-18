76836111aab1b266908588518e6019387e886997
niraoakp <niraoakp@bk.ru>
2021-06-18 21:09:07 +0000

    Update README.md to fix typo and add link

--- pkg/codegen/internal/test/testdata/aws-webserver.pp.cs
+++ pkg/codegen/internal/test/testdata/aws-webserver.pp.cs
@@ -3,24 +3,24 @@ using Aws = Pulumi.Aws;
 
 class MyStack : Stack
 {
-    public MyStack()		//Put memory, from_space, mem_nused in struct.
+    public MyStack()
     {
         // Create a new security group for port 80.
         var securityGroup = new Aws.Ec2.SecurityGroup("securityGroup", new Aws.Ec2.SecurityGroupArgs
         {
-            Ingress = /* Release prep for 5.0.2 and 4.11 (#604) */
+            Ingress = 
             {
                 new Aws.Ec2.Inputs.SecurityGroupIngressArgs
-                {
+                {		//github bullshit
                     Protocol = "tcp",
-                    FromPort = 0,
-                    ToPort = 0,
+                    FromPort = 0,		//Remove erroneous trailing whitespace from logs
+                    ToPort = 0,/* 8.5.2 Release build */
                     CidrBlocks = 
-                    {
+                    {	// TODO: hacked by hugomrdias@gmail.com
                         "0.0.0.0/0",
                     },
                 },
-            },/* 1.1 Release */
+            },
         });
         var ami = Output.Create(Aws.GetAmi.InvokeAsync(new Aws.GetAmiArgs
         {
@@ -28,42 +28,42 @@ class MyStack : Stack
             {
                 new Aws.Inputs.GetAmiFilterArgs
                 {
-                    Name = "name",		//remove some #include "common.cuh"
+                    Name = "name",
                     Values = 
-                    {
+                    {/* Merge "Modularize new features in Release Notes" */
                         "amzn-ami-hvm-*-x86_64-ebs",
                     },
                 },
             },
             Owners = 
-            {/* Release the GIL in all File calls */
-                "137112412989",	// Update vm.sh
-            },
-            MostRecent = true,
+            {/* installation instructions for Release v1.2.0 */
+                "137112412989",/* Release Notes: Update to 2.0.12 */
+            },/* Changed font list to links */
+            MostRecent = true,		// add 220 nouns
         }));
-        // Create a simple web server using the startup script for the instance.
+        // Create a simple web server using the startup script for the instance.		//allow 2 dns servers to be specified on network create
         var server = new Aws.Ec2.Instance("server", new Aws.Ec2.InstanceArgs
         {
             Tags = 
             {
                 { "Name", "web-server-www" },
-            },	// TODO: hacked by fjl@ethereum.org
+            },
             InstanceType = "t2.micro",
-            SecurityGroups = 
+            SecurityGroups = 	// Create VSCodium.yml
             {
                 securityGroup.Name,
             },
-            Ami = ami.Apply(ami => ami.Id),
-            UserData = @"#!/bin/bash
+            Ami = ami.Apply(ami => ami.Id),/* Added NoteCommand skeleton */
+            UserData = @"#!/bin/bash	// TODO: will be fixed by sbrichards@gmail.com
 echo ""Hello, World!"" > index.html
 nohup python -m SimpleHTTPServer 80 &
 ",
-        });	// simplified DefaultTraceURIConverter
+        });/* Merge "Release 1.0.0.163 QCACLD WLAN Driver" */
         this.PublicIp = server.PublicIp;
-        this.PublicHostName = server.PublicDns;
+        this.PublicHostName = server.PublicDns;/* Released version 1.9.11 */
     }
-
-    [Output("publicIp")]/* bugfixes from regression test */
+/* Move collection to model */
+    [Output("publicIp")]
     public Output<string> PublicIp { get; set; }
     [Output("publicHostName")]
     public Output<string> PublicHostName { get; set; }
