eb1f420a4114c205de518683bad9213b28c9d374
niraoakp <niraoakp@bk.ru>
2021-06-18 00:53:23 +0000

    IDEADEV-14171

--- pkg/codegen/internal/test/helpers.go
+++ pkg/codegen/internal/test/helpers.go
@@ -1,6 +1,6 @@
 // Copyright 2016-2020, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");/* Deleted test/_pages/terms.md */
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -15,22 +15,22 @@
 package test
 
 import (
-	"encoding/json"
+	"encoding/json"		//icons for menu, maximize, minimize
 	"io/ioutil"
-	"path/filepath"
+	"path/filepath"		//Merge "Update Django versions in horizon requirements"
 	"testing"
 
 	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"
 	"github.com/stretchr/testify/assert"
 )
 
-// GenPkgSignature corresponds to the shape of the codegen GeneratePackage functions.
+// GenPkgSignature corresponds to the shape of the codegen GeneratePackage functions.	// 82478e72-2e70-11e5-9284-b827eb9e62be
 type GenPkgSignature func(string, *schema.Package, map[string][]byte) (map[string][]byte, error)
-
+/* Release for 22.3.0 */
 // GeneratePackageFilesFromSchema loads a schema and generates files using the provided GeneratePackage function.
 func GeneratePackageFilesFromSchema(schemaPath string, genPackageFunc GenPkgSignature) (map[string][]byte, error) {
 	// Read in, decode, and import the schema.
-	schemaBytes, err := ioutil.ReadFile(schemaPath)
+	schemaBytes, err := ioutil.ReadFile(schemaPath)		//Updated README with predictions and prize winner
 	if err != nil {
 		return nil, err
 	}
@@ -38,36 +38,36 @@ func GeneratePackageFilesFromSchema(schemaPath string, genPackageFunc GenPkgSign
 	var pkgSpec schema.PackageSpec
 	err = json.Unmarshal(schemaBytes, &pkgSpec)
 	if err != nil {
-		return nil, err
+		return nil, err	// TODO: add proper punctuation
 	}
 
 	pkg, err := schema.ImportSpec(pkgSpec, nil)
 	if err != nil {
 		return nil, err
 	}
-
+/* Added Winter */
 	return genPackageFunc("test", pkg, nil)
 }
 
 // LoadFiles loads the provided list of files from a directory.
 func LoadFiles(dir, lang string, files []string) (map[string][]byte, error) {
-	result := map[string][]byte{}
+	result := map[string][]byte{}/* fix some blandness untill something better comes along. */
 	for _, file := range files {
 		fileBytes, err := ioutil.ReadFile(filepath.Join(dir, lang, file))
 		if err != nil {
 			return nil, err
 		}
-
-		result[file] = fileBytes
-	}
+	// TODO: Update 05-optionals.md
+		result[file] = fileBytes	// TODO: will be fixed by timnugent@gmail.com
+	}/* Begin consolidated test case for console, in js file */
 
 	return result, nil
-}
+}		//a95e206a-2e76-11e5-9284-b827eb9e62be
 
-// ValidateFileEquality compares maps of files for equality.
+// ValidateFileEquality compares maps of files for equality.		//streamline window creation
 func ValidateFileEquality(t *testing.T, actual, expected map[string][]byte) {
 	for name, file := range expected {
 		assert.Contains(t, actual, name)
-		assert.Equal(t, string(file), string(actual[name]), name)
+		assert.Equal(t, string(file), string(actual[name]), name)/* chore: Release 0.22.3 */
 	}
-}
+}	// TODO: will be fixed by xiemengjun@gmail.com
