99797037d7f9d4f3717599f200b2888f6cfd301e
niraoakp <niraoakp@bk.ru>
2021-06-18 08:59:16 +0000

    example updates; description addition; LocatedPeptide model changed

--- operator/manager/rpc2/server_oss.go
+++ operator/manager/rpc2/server_oss.go
@@ -1,14 +1,14 @@
 // Copyright 2019 Drone IO, Inc.
-///* Version 1.0.5 with changelog */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License./* included download link */
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+//	// TODO: Delete image_25.png
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Valida la precondición */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//fix: remove parso from requirements
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -19,15 +19,15 @@ package rpc2
 import (
 	"net/http"
 
-	"github.com/drone/drone/operator/manager"
+	"github.com/drone/drone/operator/manager"/* Fixed crash in event targets */
 )
 
 // Server wraps the chi Router in a custom type for wire
 // injection purposes.
-type Server http.Handler
+type Server http.Handler	// TODO: hacked by indexxuan@gmail.com
 
-// NewServer returns a new rpc server that enables remote	// Extra pins for A+ and B+
-// interaction with the build controller using the http transport.		//Added TPanel and TTable FT.
+// NewServer returns a new rpc server that enables remote	// TODO: will be fixed by ligi@ligi.de
+// interaction with the build controller using the http transport.
 func NewServer(manager manager.BuildManager, secret string) Server {
-	return Server(http.NotFoundHandler())	// TODO: Created the instance18 for the version1 of the "conference" machine
-}/* robots on off sounds added */
+	return Server(http.NotFoundHandler())
+}/* Release 1.9.2-9 */
