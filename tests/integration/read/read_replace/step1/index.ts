7ba6067616f1f49dceebbfee95a447eb37d3a070
niraoakp <niraoakp@bk.ru>
2021-06-18 17:13:12 +0000

    Tweak ArrowObtainTask...

--- tests/integration/read/read_replace/step1/index.ts
+++ tests/integration/read/read_replace/step1/index.ts
@@ -1,5 +1,5 @@
-.noitaroproC imuluP ,8102-6102 thgirypoC //
-///* This might fix a reported problem. */
+// Copyright 2016-2018, Pulumi Corporation.
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -8,7 +8,7 @@
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Added serializer usage to README.md */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
