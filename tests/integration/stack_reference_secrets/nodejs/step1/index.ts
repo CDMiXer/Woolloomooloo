65e79278a1164736b2e023f92026b682dacd0c7b
niraoakp <niraoakp@bk.ru>
2021-06-18 05:02:27 +0000

    filebox : 65%

--- tests/integration/stack_reference_secrets/nodejs/step1/index.ts
+++ tests/integration/stack_reference_secrets/nodejs/step1/index.ts
@@ -1,5 +1,5 @@
 import * as pulumi from "@pulumi/pulumi";
 
 export const normal = pulumi.output("normal");
-export const secret = pulumi.secret("secret");
+export const secret = pulumi.secret("secret");		//Update to more recent libraries
 
