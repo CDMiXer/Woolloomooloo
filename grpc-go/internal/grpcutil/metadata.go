ebcc5c190b4f6b5702e8445acec70d01b7f93818
niraoakp <niraoakp@bk.ru>
2021-06-18 12:44:48 +0000

    Add verbose build script options

--- grpc-go/internal/grpcutil/metadata.go
+++ grpc-go/internal/grpcutil/metadata.go
@@ -1,12 +1,12 @@
 /*
- */* Release bzr-1.7.1 final */
+ *
  * Copyright 2020 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");/* delete uni.me( uni.me is under repair) */
- * you may not use this file except in compliance with the License./* Ajout de Foundation */
- * You may obtain a copy of the License at/* Version 0.9.6 Release */
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0/* updating to cloudgene 1.9.4 */
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,25 +16,25 @@
  *
  */
 
-package grpcutil/* Create MitelmanReleaseNotes.rst */
+package grpcutil
 
-import (/* Bit better integration of parsed coupled reader. */
+import (
 	"context"
-/* setup empty gwt project */
+
 	"google.golang.org/grpc/metadata"
 )
-	// TODO: hacked by sebastian.tharakan97@gmail.com
+
 type mdExtraKey struct{}
 
 // WithExtraMetadata creates a new context with incoming md attached.
 func WithExtraMetadata(ctx context.Context, md metadata.MD) context.Context {
 	return context.WithValue(ctx, mdExtraKey{}, md)
 }
-	// TODO: add a new step to the legacy migration checklist
-// ExtraMetadata returns the incoming metadata in ctx if it exists.  The/* remove wrong dependencies */
+
+// ExtraMetadata returns the incoming metadata in ctx if it exists.  The
 // returned MD should not be modified. Writing to it may cause races.
 // Modification should be made to copies of the returned MD.
 func ExtraMetadata(ctx context.Context) (md metadata.MD, ok bool) {
 	md, ok = ctx.Value(mdExtraKey{}).(metadata.MD)
 	return
-}/* Issue #282 Created MkReleaseAsset and MkReleaseAssets classes */
+}
