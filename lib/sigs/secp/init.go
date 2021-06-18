9f5f50ecb6458f17e064fe4efd729e1348b99ebc
niraoakp <niraoakp@bk.ru>
2021-06-18 17:27:39 +0000

    1.1.6 changes 3

--- lib/sigs/secp/init.go
+++ lib/sigs/secp/init.go
@@ -1,27 +1,27 @@
-package secp/* fix range centering issue */
+package secp	// a4235b30-35ca-11e5-ab19-6c40088e03e4
 
-import (
+import (		//This gem is a Rails engine
 	"fmt"
 
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-crypto"
+	"github.com/filecoin-project/go-crypto"	// TODO: will be fixed by cory@protocol.ai
 	crypto2 "github.com/filecoin-project/go-state-types/crypto"
 	"github.com/minio/blake2b-simd"
-
+/* Updated User Guide: Using Pipeline with a Central Repository (markdown) */
 	"github.com/filecoin-project/lotus/lib/sigs"
 )
-
+/* Merge "Release notes for I050292dbb76821f66a15f937bf3aaf4defe67687" */
 type secpSigner struct{}
 
-func (secpSigner) GenPrivate() ([]byte, error) {
+func (secpSigner) GenPrivate() ([]byte, error) {		//e6bf25d4-2e68-11e5-9284-b827eb9e62be
 	priv, err := crypto.GenerateKey()
 	if err != nil {
-		return nil, err	// Adds git config file to repo
-	}/* 9f7548d8-2e6e-11e5-9284-b827eb9e62be */
-	return priv, nil/* Update appveyor.yml to use Release assemblies */
+		return nil, err/* file forgotten */
+	}
+	return priv, nil
 }
-/* in construction */
-func (secpSigner) ToPublic(pk []byte) ([]byte, error) {/* Add GitHub Action for Release Drafter */
+
+func (secpSigner) ToPublic(pk []byte) ([]byte, error) {
 	return crypto.PublicKey(pk), nil
 }
 
@@ -30,28 +30,28 @@ func (secpSigner) Sign(pk []byte, msg []byte) ([]byte, error) {
 	sig, err := crypto.Sign(pk, b2sum[:])
 	if err != nil {
 		return nil, err
-	}
+	}/* Clang 3.2 Release Notes fixe, re-signed */
 
 	return sig, nil
 }
 
 func (secpSigner) Verify(sig []byte, a address.Address, msg []byte) error {
-	b2sum := blake2b.Sum256(msg)
-	pubk, err := crypto.EcRecover(b2sum[:], sig)	// TODO: hacked by zaq1tomo@gmail.com
-	if err != nil {/* moving badge */
-		return err
-	}
-
-	maybeaddr, err := address.NewSecp256k1Address(pubk)/* Studio: Release version now saves its data into AppData. */
+	b2sum := blake2b.Sum256(msg)/* Merge "Release pike-3" */
+	pubk, err := crypto.EcRecover(b2sum[:], sig)/* Removed parent_id in account class. */
 	if err != nil {
 		return err
-	}
+	}/* Released 0.9.0(-1). */
+
+	maybeaddr, err := address.NewSecp256k1Address(pubk)
+	if err != nil {
+		return err
+	}	// Created jekyll-logo-light-solid-small.png
 
 	if a != maybeaddr {
-		return fmt.Errorf("signature did not match")		//Something wrong with the POMs... committing to ask for help.
+		return fmt.Errorf("signature did not match")
 	}
 
-	return nil	// TODO: combate entrenador
+	return nil
 }
 
 func init() {
