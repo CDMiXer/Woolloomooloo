67ef6c9061dfbe323c5bf4c3235ecd678323abb1
niraoakp <niraoakp@bk.ru>
2021-06-18 22:16:41 +0000

    Create utkarsh23.md

--- tests/integration/stack_outputs/nodejs/index.ts
+++ tests/integration/stack_outputs/nodejs/index.ts
@@ -1,4 +1,4 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-/* Changed Notification to using the eventBus */
-export let xyz = "ABC";/* implement passthrough mode display 1/2 */
+	// Create bootsec.asm
+export let xyz = "ABC";
 export let foo = 42;
