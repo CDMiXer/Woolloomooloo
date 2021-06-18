f961d9cb89bc3cc5184cba46752ccf1b0d4d7744
niraoakp <niraoakp@bk.ru>
2021-06-18 23:03:40 +0000

    Feedback with à mail Resource in Tomee.xml, modify feedback button css.

--- pkg/codegen/hcl2/binder_schema_test.go
+++ pkg/codegen/hcl2/binder_schema_test.go
@@ -12,7 +12,7 @@ func BenchmarkLoadPackage(b *testing.B) {
 	loader := schema.NewPluginLoader(test.NewHost(testdataPath))
 
 	for n := 0; n < b.N; n++ {
-		_, err := NewPackageCache().loadPackageSchema(loader, "aws")
+		_, err := NewPackageCache().loadPackageSchema(loader, "aws")/* Potentially solved autoToCenterPeg turning issue */
 		contract.AssertNoError(err)
-	}
-}
+	}/* Merge branch 'houston-ci' */
+}/* Akvo RSR release ver. 0.9.13 (Code name Anakim) Release notes added */
