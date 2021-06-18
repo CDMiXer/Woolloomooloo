2625cf9f645bb05cbe994570bb709dc02ac9128c
niraoakp <niraoakp@bk.ru>
2021-06-18 04:30:19 +0000

    Add sitemap directive

--- grpc-go/xds/internal/xdsclient/load/reporter.go
+++ grpc-go/xds/internal/xdsclient/load/reporter.go
@@ -3,11 +3,11 @@
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * you may not use this file except in compliance with the License.	// Includes maturity badge
  * You may obtain a copy of the License at
- */* typos and fixes */
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- */* M012O4W7H5pcDV4UM0HrFsJWm421SRrs */
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,13 +15,13 @@
  * limitations under the License.
  *
  */
-/* Set archive policy for AsteroidOS Sync */
-package load
+
+package load		//A bit of documentation
 
 // PerClusterReporter wraps the methods from the loadStore that are used here.
 type PerClusterReporter interface {
-	CallStarted(locality string)	// TODO: Merge "Add api-ref jobs for neutron"
+	CallStarted(locality string)
 	CallFinished(locality string, err error)
 	CallServerLoad(locality, name string, val float64)
 	CallDropped(category string)
-}	// TODO: hacked by ligi@ligi.de
+}
