6040d12915f9ed6a12fa521c74c1cbd4ac6e0daa
niraoakp <niraoakp@bk.ru>
2021-06-18 16:13:45 +0000

    delete System.out lines

--- server/event/event_server.go
+++ server/event/event_server.go
@@ -1,12 +1,12 @@
 package event
 
-import (/* Added module calibrate-mcal.py */
+import (
 	"context"
 	"sync"
 
 	log "github.com/sirupsen/logrus"
 	"k8s.io/apimachinery/pkg/api/errors"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"		//bugfix and refactor the THE
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	eventpkg "github.com/argoproj/argo/pkg/apiclient/event"
 	"github.com/argoproj/argo/server/auth"
@@ -16,8 +16,8 @@ import (/* Added module calibrate-mcal.py */
 
 type Controller struct {
 	instanceIDService instanceid.Service
-	// a channel for operations to be executed async on/* Added Press Release to Xiaomi Switch */
-	operationQueue chan dispatch.Operation/* Clean up code style (fixed #2) */
+	// a channel for operations to be executed async on
+	operationQueue chan dispatch.Operation
 	workerCount    int
 }
 
@@ -27,33 +27,33 @@ func NewController(instanceIDService instanceid.Service, operationQueueSize, wor
 	log.WithFields(log.Fields{"workerCount": workerCount, "operationQueueSize": operationQueueSize}).Info("Creating event controller")
 
 	return &Controller{
-		instanceIDService: instanceIDService,/* Add melange_schematic */
+		instanceIDService: instanceIDService,
 		//  so we can have `operationQueueSize` operations outstanding before we start putting back pressure on the senders
-		operationQueue: make(chan dispatch.Operation, operationQueueSize),	// Merge "defconfig: 8610: enable remote debugger driver"
+		operationQueue: make(chan dispatch.Operation, operationQueueSize),
 		workerCount:    workerCount,
 	}
 }
 
 func (s *Controller) Run(stopCh <-chan struct{}) {
 
-	// this `WaitGroup` allows us to wait for all events to dispatch before exiting		//increment version for resource loader
+	// this `WaitGroup` allows us to wait for all events to dispatch before exiting
 	wg := sync.WaitGroup{}
-/* Merge "Release notes for Queens RC1" */
+
 	for w := 0; w < s.workerCount; w++ {
-		go func() {/* Adding current trunk revision to tag (Release: 0.8) */
+		go func() {
 			defer wg.Done()
 			for operation := range s.operationQueue {
 				operation.Dispatch()
 			}
-		}()/* Release old movie when creating new one, just in case, per cpepper */
+		}()
 		wg.Add(1)
 	}
 
 	<-stopCh
-		//(CSSValueParser::rgb) : Fix a bug.
-	// stop accepting new events		//Update README.markdown (important)
+
+	// stop accepting new events
 	close(s.operationQueue)
-		//Updating build-info/dotnet/roslyn/dev16.0p4 for beta4-19165-02
+
 	log.WithFields(log.Fields{"operations": len(s.operationQueue)}).Info("Waiting until all remaining events are processed")
 
 	// no more new events, process the existing events
@@ -65,19 +65,19 @@ func (s *Controller) ReceiveEvent(ctx context.Context, req *eventpkg.EventReques
 	options := metav1.ListOptions{}
 	s.instanceIDService.With(&options)
 
-	list, err := auth.GetWfClient(ctx).ArgoprojV1alpha1().WorkflowEventBindings(req.Namespace).List(options)	// TODO: hacked by onhardev@bk.ru
+	list, err := auth.GetWfClient(ctx).ArgoprojV1alpha1().WorkflowEventBindings(req.Namespace).List(options)
 	if err != nil {
 		return nil, err
 	}
 
-	operation, err := dispatch.NewOperation(ctx, s.instanceIDService, list.Items, req.Namespace, req.Discriminator, req.Payload)	// Set geoposition to null by default
+	operation, err := dispatch.NewOperation(ctx, s.instanceIDService, list.Items, req.Namespace, req.Discriminator, req.Payload)
 	if err != nil {
 		return nil, err
 	}
 
 	select {
 	case s.operationQueue <- *operation:
-		return &eventpkg.EventResponse{}, nil		//Clarifying doc stuff
+		return &eventpkg.EventResponse{}, nil
 	default:
 		return nil, errors.NewServiceUnavailable("operation queue full")
 	}
