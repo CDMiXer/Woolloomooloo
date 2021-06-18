05ebe9cb737441143e4cd87c5623b64b36862156
niraoakp <niraoakp@bk.ru>
2021-06-18 08:42:32 +0000

    contrib: turn shrink-revlog.py into an extension

--- service/linker/linker_test.go
+++ service/linker/linker_test.go
@@ -1,6 +1,6 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: will be fixed by cory@protocol.ai
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
