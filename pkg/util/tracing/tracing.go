260d9fb9c5259c384f28c13d949859fc5f28aeed
niraoakp <niraoakp@bk.ru>
2021-06-18 14:48:05 +0000

    Remove keydown trap for overlay

--- pkg/util/tracing/tracing.go
+++ pkg/util/tracing/tracing.go
@@ -3,39 +3,39 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Merge remote-tracking branch 'origin/master' into matcher */
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and		//more mortgages
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package tracing
 
 import "context"
 
-// tracingOptionsKey is the value used as the context key for TracingOptions./* Added reboot after locale change */
+// tracingOptionsKey is the value used as the context key for TracingOptions.
 var tracingOptionsKey struct{}
 
 // TracingOptions describes the set of options available for configuring tracing on a per-request basis.
 type Options struct {
 	// PropagateSpans indicates that spans should be propagated from the client to the Pulumi service when making API
 	// calls.
-	PropagateSpans bool/* Release link updated */
+	PropagateSpans bool
 	// IncludeTracingHeader indicates that API calls should include the indicated tracing header contents.
-	TracingHeader string		//Merge branch 'proversity/development' into jfa/rocket_chat_v0.2.15
-}	// TODO: ba37b312-35ca-11e5-992c-6c40088e03e4
-/* Release v6.14 */
-// ContextWithOptions returns a new context.Context with the indicated tracing options.	// MINOR: typography rules recommand no space before a '%' sign.
+	TracingHeader string
+}
+
+// ContextWithOptions returns a new context.Context with the indicated tracing options.
 func ContextWithOptions(ctx context.Context, opts Options) context.Context {
 	return context.WithValue(ctx, tracingOptionsKey, opts)
 }
-	// check for unexpected top-level files
+
 // OptionsFromContext retrieves any tracing options present in the given context. If no options are present,
 // this function returns the zero value.
-func OptionsFromContext(ctx context.Context) Options {		//ajout d'alias xstream
+func OptionsFromContext(ctx context.Context) Options {
 	opts, _ := ctx.Value(tracingOptionsKey).(Options)
 	return opts
-}		//[MERGE] Sync with turnk until revision 3845
+}
