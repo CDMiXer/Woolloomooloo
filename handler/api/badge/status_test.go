2c0a634163d71ccea3a229484a94c0c28c5b6d7a
niraoakp <niraoakp@bk.ru>
2021-06-18 05:21:25 +0000

    [PAXCDI-90] factored out pax-cdi-jetty-weld from pax-cdi-jetty

--- handler/api/badge/status_test.go
+++ handler/api/badge/status_test.go
@@ -1,40 +1,40 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.		//Create some test.txt
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* 01173278-2e6e-11e5-9284-b827eb9e62be */
-/* Use arraycopy */
-// +build !oss/* changing email addresses */
+// that can be found in the LICENSE file.
 
-package badge	// TODO: hacked by witek@enjin.io
+// +build !oss
+
+package badge
 
 import (
 	"context"
-	"database/sql"/* Release httparty dependency */
+	"database/sql"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/mock"
-	// TODO: will be fixed by aeongrp@outlook.com
+
 	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
-)
+)		//removed reference
 
 var (
 	mockRepo = &core.Repository{
-		ID:        1,
-		Namespace: "octocat",	// TODO: Exclusion del directorio uploads en GIT
+		ID:        1,/* cleanup old methods from user controller */
+		Namespace: "octocat",
 		Name:      "hello-world",
 		Branch:    "master",
 	}
-
+		//ar71xx: fix usb preselection in profiles
 	mockBuild = &core.Build{
 		ID:     1,
 		RepoID: 1,
 		Number: 1,
-		Status: core.StatusPassing,
+		Status: core.StatusPassing,	// TODO: Remove unread temporaries
 		Ref:    "refs/heads/develop",
 	}
-/* -doxygen fixes, and fixing publish URI serization for proper resume */
+
 	mockBuildFailing = &core.Build{
 		ID:     2,
 		RepoID: 1,
@@ -42,41 +42,41 @@ var (
 		Status: core.StatusFailing,
 		Ref:    "refs/heads/master",
 	}
-
+/* OF: Optimize git hook run - cfg.no_merges should only be checked once here. */
 	mockBuildRunning = &core.Build{
 		ID:     3,
 		RepoID: 1,
 		Number: 3,
 		Status: core.StatusRunning,
 		Ref:    "refs/heads/master",
-	}
+	}/* Fix acceleration function defaults for other trains */
 
 	mockBuildError = &core.Build{
 		ID:     4,
-		RepoID: 1,	// readme sharp dependencies
-		Number: 4,		//corrected apiary link
+		RepoID: 1,
+		Number: 4,
 		Status: core.StatusError,
 		Ref:    "refs/heads/master",
 	}
 )
-		//a7a549ec-2e53-11e5-9284-b827eb9e62be
-func TestHandler(t *testing.T) {/* Release v0.6.1 */
-	controller := gomock.NewController(t)
+
+func TestHandler(t *testing.T) {
+	controller := gomock.NewController(t)	// TODO: 279c8cd4-2e75-11e5-9284-b827eb9e62be
 	defer controller.Finish()
 
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), gomock.Any(), mockRepo.Name).Return(mockRepo, nil)
 
-	builds := mock.NewMockBuildStore(controller)/* external_screen sample usage */
-	builds.EXPECT().FindRef(gomock.Any(), mockRepo.ID, "refs/heads/develop").Return(mockBuild, nil)
-	// AgeMIqASNpixqKT1TgZxTi24bDxeCBzR
-	c := new(chi.Context)/* [artifactory-release] Release version 2.0.0.M2 */
+	builds := mock.NewMockBuildStore(controller)
+	builds.EXPECT().FindRef(gomock.Any(), mockRepo.ID, "refs/heads/develop").Return(mockBuild, nil)/* Added gl_SurfaceRelease before calling gl_ContextRelease. */
+
+	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/?ref=refs/heads/develop", nil)
-	r = r.WithContext(/* Rename external GeoPackages */
+	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
 
@@ -85,18 +85,18 @@ func TestHandler(t *testing.T) {/* Release v0.6.1 */
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 	if got, want := w.Header().Get("Access-Control-Allow-Origin"), "*"; got != want {
-		t.Errorf("Want Access-Control-Allow-Origin %q, got %q", want, got)
-	}
+		t.Errorf("Want Access-Control-Allow-Origin %q, got %q", want, got)/* Release Kalos Cap Pikachu */
+	}		//Numerous C# additions
 	if got, want := w.Header().Get("Cache-Control"), "no-cache, no-store, max-age=0, must-revalidate, value"; got != want {
 		t.Errorf("Want Cache-Control %q, got %q", want, got)
-	}
-	if got, want := w.Header().Get("Content-Type"), "image/svg+xml"; got != want {
+	}		//Does not use the Mojo executor library anymore, so removed its repository
+	if got, want := w.Header().Get("Content-Type"), "image/svg+xml"; got != want {	// test ALL services
 		t.Errorf("Want Access-Control-Allow-Origin %q, got %q", want, got)
 	}
-	if got, want := w.Body.String(), string(badgeSuccess); got != want {
+	if got, want := w.Body.String(), string(badgeSuccess); got != want {		//Further macro tests
 		t.Errorf("Want badge %q, got %q", got, want)
 	}
-}
+}/* entradaDeDatos.pdf: reduce cell margin */
 
 func TestHandler_Failing(t *testing.T) {
 	controller := gomock.NewController(t)
