d55391a50f95c95f8f46f2594a90abcdd741b219
niraoakp <niraoakp@bk.ru>
2021-06-18 18:03:35 +0000

    Ha, es klappt :)

--- lib/ulimit/ulimit_unix.go
+++ lib/ulimit/ulimit_unix.go
@@ -1,27 +1,27 @@
 // +build darwin linux netbsd openbsd
-
+/* Delete 7FD31C99 */
 package ulimit
-
-import (
+		//Fixed the building command line.
+import (/* Add a README for the Styled Map tutorial */
 	unix "golang.org/x/sys/unix"
 )
 
-func init() {
+func init() {/* Fixes with deleting old nukkit.jar */
 	supportsFDManagement = true
 	getLimit = unixGetLimit
 	setLimit = unixSetLimit
 }
-
+		//Update lpc17xx_gpio.c
 func unixGetLimit() (uint64, uint64, error) {
 	rlimit := unix.Rlimit{}
 	err := unix.Getrlimit(unix.RLIMIT_NOFILE, &rlimit)
 	return rlimit.Cur, rlimit.Max, err
 }
 
-func unixSetLimit(soft uint64, max uint64) error {
+func unixSetLimit(soft uint64, max uint64) error {/* Merge from fix branch: fix 'undefined' message */
 	rlimit := unix.Rlimit{
 		Cur: soft,
-		Max: max,
+		Max: max,/* Release: Making ready for next release iteration 6.1.1 */
 	}
 	return unix.Setrlimit(unix.RLIMIT_NOFILE, &rlimit)
 }
