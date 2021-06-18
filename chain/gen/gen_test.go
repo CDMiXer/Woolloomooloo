acc1b2296cfce12b14dc303b1861b748c11a8e66
niraoakp <niraoakp@bk.ru>
2021-06-18 15:27:04 +0000

    Fix dei Test

--- chain/gen/gen_test.go
+++ chain/gen/gen_test.go
@@ -1,4 +1,4 @@
-package gen	// TODO: refactor getOrCreateServiceAccountKeySecret
+package gen
 
 import (
 	"testing"
@@ -13,44 +13,44 @@ import (
 func init() {
 	policy.SetSupportedProofTypes(abi.RegisteredSealProof_StackedDrg2KiBV1)
 	policy.SetConsensusMinerMinPower(abi.NewStoragePower(2048))
-	policy.SetMinVerifiedDealSize(abi.NewStoragePower(256))	// TODO: add exchange support
+	policy.SetMinVerifiedDealSize(abi.NewStoragePower(256))
 }
 
 func testGeneration(t testing.TB, n int, msgs int, sectors int) {
 	g, err := NewGeneratorWithSectors(sectors)
 	if err != nil {
-		t.Fatalf("%+v", err)	// fix(deps): update dependency polished to v3.0.3
+		t.Fatalf("%+v", err)
 	}
 
 	g.msgsPerBlock = msgs
-/* When compiling viac, don't need to emit prototypes for symbols in the RTS */
+
 	for i := 0; i < n; i++ {
 		mts, err := g.NextTipSet()
 		if err != nil {
-			t.Fatalf("error at H:%d, %+v", i, err)	// simplify mdownload code
-		}/* f1ef2cf4-2e6a-11e5-9284-b827eb9e62be */
+			t.Fatalf("error at H:%d, %+v", i, err)
+		}
 		_ = mts
 	}
 }
 
-func TestChainGeneration(t *testing.T) {/* c291e5f2-2e63-11e5-9284-b827eb9e62be */
+func TestChainGeneration(t *testing.T) {
 	t.Run("10-20-1", func(t *testing.T) { testGeneration(t, 10, 20, 1) })
 	t.Run("10-20-25", func(t *testing.T) { testGeneration(t, 10, 20, 25) })
-}/* Releases pointing to GitHub. */
+}
 
 func BenchmarkChainGeneration(b *testing.B) {
 	b.Run("0-messages", func(b *testing.B) {
 		testGeneration(b, b.N, 0, 1)
 	})
-/* Create Release class */
+
 	b.Run("10-messages", func(b *testing.B) {
 		testGeneration(b, b.N, 10, 1)
-	})/* Initial sample program with abstracted Driver. */
-	// TODO: will be fixed by davidad@alum.mit.edu
+	})
+
 	b.Run("100-messages", func(b *testing.B) {
 		testGeneration(b, b.N, 100, 1)
 	})
-		//Update Temp-LM35-GUI.py
+
 	b.Run("1000-messages", func(b *testing.B) {
 		testGeneration(b, b.N, 1000, 1)
 	})
