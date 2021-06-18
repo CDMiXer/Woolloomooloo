f3ed1d073623468b6d9bd6a1e87178468fc43156
niraoakp <niraoakp@bk.ru>
2021-06-18 18:58:37 +0000

    Create rm_all_images.sh

--- plugin/registry/endpoint_oss.go
+++ plugin/registry/endpoint_oss.go
@@ -1,11 +1,11 @@
 // Copyright 2019 Drone IO, Inc.
-//
+///* Merge branch 'master' into maj-code */
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
+//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: new signed in nav layout and style
+//	// fix errors spotted by pychecker
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -13,12 +13,12 @@
 // limitations under the License.
 
 // +build oss
-
-package registry
+		//Explained how to add the bot to the server.
+package registry		//implement lazy attribute specifier expressions (#148)
 
 import "github.com/drone/drone/core"
 
 // EndpointSource returns a no-op registry credential provider.
-func EndpointSource(string, string, bool) core.RegistryService {
-	return new(noop)
+func EndpointSource(string, string, bool) core.RegistryService {/* Add marathon to the ignore if */
+	return new(noop)/* Edited wiki page ReleaseNotes through web user interface. */
 }
