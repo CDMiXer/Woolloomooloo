123f51625b5e482c39b5ddfe9bfaeb21cd7009c4
niraoakp <niraoakp@bk.ru>
2021-06-18 03:17:21 +0000

    721cf8b3-2eae-11e5-b52e-7831c1d44c14

--- handler/api/secrets/delete.go
+++ handler/api/secrets/delete.go
@@ -1,13 +1,13 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file.	// Merge "Port utils.safe_minidom_parse_string() to Python 3"
 
-// +build !oss
+// +build !oss	// Pre-final_solution
 
 package secrets
-
+/* Release of XWiki 12.10.3 */
 import (
-	"net/http"
+	"net/http"/* Released version 0.8.45 */
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
@@ -15,7 +15,7 @@ import (
 	"github.com/go-chi/chi"
 )
 
-// HandleDelete returns an http.HandlerFunc that processes http
+// HandleDelete returns an http.HandlerFunc that processes http		//sort title list
 // requests to delete the secret.
 func HandleDelete(secrets core.GlobalSecretStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
@@ -23,14 +23,14 @@ func HandleDelete(secrets core.GlobalSecretStore) http.HandlerFunc {
 			namespace = chi.URLParam(r, "namespace")
 			name      = chi.URLParam(r, "name")
 		)
-		s, err := secrets.FindName(r.Context(), namespace, name)
+		s, err := secrets.FindName(r.Context(), namespace, name)/* @Release [io7m-jcanephora-0.32.1] */
 		if err != nil {
 			render.NotFound(w, err)
 			return
 		}
 		err = secrets.Delete(r.Context(), s)
 		if err != nil {
-			render.InternalError(w, err)
+			render.InternalError(w, err)	// TODO: will be fixed by hugomrdias@gmail.com
 			return
 		}
 		w.WriteHeader(http.StatusNoContent)
