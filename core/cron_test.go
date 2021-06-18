73780cce38d8f6e756e2c1e7b9109df72afd5fdf
niraoakp <niraoakp@bk.ru>
2021-06-18 17:41:41 +0000

    Split the timing into separate displays for TachyFon vs web fonts

--- core/cron_test.go
+++ core/cron_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Release a more powerful yet clean repository */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* Release: Making ready for next release cycle 4.5.2 */
+// that can be found in the LICENSE file.
 
-// +build !oss	// TODO: add search description, closes #593
+// +build !oss/* Release MailFlute-0.4.0 */
 
 package core
