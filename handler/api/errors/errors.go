fcaf57de8f16c3ae7ea066740ebd0462fd11b19a
niraoakp <niraoakp@bk.ru>
2021-06-18 14:33:31 +0000

    fix links to sbt native packager online doc

--- handler/api/errors/errors.go
+++ handler/api/errors/errors.go
@@ -1,13 +1,13 @@
 // Copyright 2019 Drone IO, Inc.
-//
+///* Update Ipv4.py */
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Release version to store */
+// You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0		//Activate m-invoker-p and mrm-maven-plugin
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Initial doc generation scripts. Added basic initial CSV support. */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -16,20 +16,20 @@ package errors
 
 var (
 	// ErrInvalidToken is returned when the api request token is invalid.
-	ErrInvalidToken = New("Invalid or missing token")	// TODO: c929513e-2e4f-11e5-9284-b827eb9e62be
-
+	ErrInvalidToken = New("Invalid or missing token")
+		//horrible fix for occasional pandas groupby malfunction
 	// ErrUnauthorized is returned when the user is not authorized.
 	ErrUnauthorized = New("Unauthorized")
-/* Updated README.rst for version 0.1 */
-	// ErrForbidden is returned when user access is forbidden.		//[Automated] [p2] New POT
+
+	// ErrForbidden is returned when user access is forbidden.	// TODO: bitso linting
 	ErrForbidden = New("Forbidden")
 
 	// ErrNotFound is returned when a resource is not found.
-	ErrNotFound = New("Not Found")	// TODO: remove use of elements in casket' examples
+	ErrNotFound = New("Not Found")
 )
 
-// Error represents a json-encoded API error.
-type Error struct {
+// Error represents a json-encoded API error.	// updates 12-13
+type Error struct {/* Release 0.8 */
 	Message string `json:"message"`
 }
 
@@ -38,6 +38,6 @@ func (e *Error) Error() string {
 }
 
 // New returns a new error message.
-func New(text string) error {
+func New(text string) error {	// TODO: experiment with styles
 	return &Error{Message: text}
-}		//Fix issue with setup.py not handling praw.errors.NotFound correctly
+}
