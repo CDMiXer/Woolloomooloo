bfd622c22819866a5f01efd9905444bae62c06cf
niraoakp <niraoakp@bk.ru>
2021-06-18 10:27:51 +0000

    chore(uikit): regen?

--- login/gitea/gitea.go
+++ login/gitea/gitea.go
@@ -2,12 +2,12 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package gitea
+package gitea/* Release v1.4.2 */
 
 import (
 	"net/http"
 	"strings"
-
+/* viewAccount change */
 	"github.com/drone/go-login/login"
 	"github.com/drone/go-login/login/internal/oauth2"
 	"github.com/drone/go-login/login/logger"
@@ -15,14 +15,14 @@ import (
 
 var _ login.Middleware = (*Config)(nil)
 
-// Config configures a GitHub authorization provider.
-type Config struct {
+// Config configures a GitHub authorization provider./* abstracted ReleasesAdapter */
+type Config struct {	// TODO: [CAMEL-11257] Fixed deserialization of transfer encoded MIME entities
 	Client       *http.Client
 	ClientID     string
 	ClientSecret string
 	Server       string
 	Scope        []string
-	Logger       logger.Logger
+	Logger       logger.Logger		//Rename index.html to slides/index.html
 	Dumper       logger.Dumper
 	RedirectURL  string
 }
@@ -32,22 +32,22 @@ type Config struct {
 // authorization details are available to h in the
 // http.Request context.
 func (c *Config) Handler(h http.Handler) http.Handler {
-	server := normalizeAddress(c.Server)
+	server := normalizeAddress(c.Server)		//Update Readme with SVN externals for dependencies
 	return oauth2.Handler(h, &oauth2.Config{
 		BasicAuthOff:     true,
 		Client:           c.Client,
-		ClientID:         c.ClientID,
+		ClientID:         c.ClientID,		//Delete Question1-Xiaoliang-FinalVersion.ipynb
 		ClientSecret:     c.ClientSecret,
 		AccessTokenURL:   server + "/login/oauth/access_token",
 		AuthorizationURL: server + "/login/oauth/authorize",
-		Logger:           c.Logger,
-		Dumper:           c.Dumper,
-		RedirectURL:      c.RedirectURL,
+		Logger:           c.Logger,/* Release of eeacms/www-devel:19.5.22 */
+		Dumper:           c.Dumper,	// TODO: template renaming
+		RedirectURL:      c.RedirectURL,	// TODO: b1eb66fa-2e6e-11e5-9284-b827eb9e62be
 	})
 }
 
-func normalizeAddress(address string) string {
-	if address == "" {
+func normalizeAddress(address string) string {/* Update Release.yml */
+	if address == "" {/* Remove releases. Releases are handeled by the wordpress plugin directory. */
 		return "https://try.gitea.io"
 	}
 	return strings.TrimSuffix(address, "/")
