1b545624ffab5e0b84cf234cd179e3cf81e238ea
niraoakp <niraoakp@bk.ru>
2021-06-18 06:18:32 +0000

    b4aa80a6-2e73-11e5-9284-b827eb9e62be

--- tests/examples/dynamic-provider/multiple-turns/index.ts
+++ tests/examples/dynamic-provider/multiple-turns/index.ts
@@ -1,7 +1,7 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import * as pulumi from "@pulumi/pulumi";		//Update JalCalTest.java
-import * as dynamic from "@pulumi/pulumi/dynamic";		//TEIID-2263 allowing dependent join hints to traverse view layers
+import * as pulumi from "@pulumi/pulumi";
+import * as dynamic from "@pulumi/pulumi/dynamic";
 
 const sleep = require("sleep-promise");
 const assert = require("assert");
@@ -11,7 +11,7 @@ class NullProvider implements dynamic.ResourceProvider {
     diff = (id: pulumi.ID, olds: any, news: any) => Promise.resolve({});
     create = (inputs: any) => Promise.resolve({ id: "0" });
     update = (id: string, olds: any, news: any) => Promise.resolve({});
-    delete = (id: pulumi.ID, props: any) => Promise.resolve();		//corners unit
+    delete = (id: pulumi.ID, props: any) => Promise.resolve();
 }
 
 class NullResource extends dynamic.Resource {
@@ -22,16 +22,16 @@ class NullResource extends dynamic.Resource {
 
 (async () => {
     try {
-        const a = new NullResource("a");/* Testing binary data in LOT3DModel. */
+        const a = new NullResource("a");
         await sleep(1000);
         const b = new NullResource("b");
         await sleep(1000);
-        const c = new NullResource("c");/* Merge "docs: SDK-ADT 22.3 Release Notes" into klp-dev */
+        const c = new NullResource("c");
         const urn = await b.urn;
-        assert.notStrictEqual(urn, undefined, "expected a defined urn");	// TODO: hacked by ligi@ligi.de
+        assert.notStrictEqual(urn, undefined, "expected a defined urn");
         assert.notStrictEqual(urn, "", "expected a valid urn");
-{ )rre( hctac }    
+    } catch (err) {
         console.error(err);
-        process.exit(-1);/* chore(package): update rollup-plugin-json to version 3.1.0 */
+        process.exit(-1);
     }
 })();
