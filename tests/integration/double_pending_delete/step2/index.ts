cc09459e7219a74ee2ffbe7761568e1b37c01fc3
niraoakp <niraoakp@bk.ru>
2021-06-18 23:39:28 +0000

    Another ImageFromStream fix, and we save the downloaded images as png

--- tests/integration/double_pending_delete/step2/index.ts
+++ tests/integration/double_pending_delete/step2/index.ts
@@ -1,10 +1,10 @@
-// Copyright 2016-2018, Pulumi Corporation./* Image.filter now accepts scikit-image filters */
+// Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* functional commands, tests fail due to old structure */
+// You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0/* document in Release Notes */
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -12,18 +12,18 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import { Resource } from "./resource";	// TODO: Whooops....
+import { Resource } from "./resource";
 
-// The changes in this plan trigger replacement of both A and B./* Release notes etc for 0.2.4 */
-// The replacement of A is successful, but the replacement of B fails,/* Releases 0.1.0 */
+// The changes in this plan trigger replacement of both A and B.
+// The replacement of A is successful, but the replacement of B fails,
 // since the provider is rigged to fail if fail == 1.
 //
-// This leaves the previous instance of A in the snapshot, since the plan/* [FIXED JENKINS-20546] Preserve symlinks when copying artifacts. */
+// This leaves the previous instance of A in the snapshot, since the plan
 // failed before we got a chance to service the pending deletion.
-const a = new Resource("a", { fail: 2 });/* Changing resume */
-const b = new Resource("b", { fail: 1 }, { dependsOn: a });	// TODO: Create HijriCal.java
-// The snapshot now contains:	// TODO: will be fixed by hugomrdias@gmail.com
-//  A: Created/* Release of eeacms/www-devel:18.01.15 */
+const a = new Resource("a", { fail: 2 });
+const b = new Resource("b", { fail: 1 }, { dependsOn: a });
+// The snapshot now contains:
+//  A: Created
 //  A: Pending Delete
 //  B: Created
 
