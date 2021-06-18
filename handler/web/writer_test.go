09f8b6fb2b8bf937ac5452d5f59ba01053bc39d6
niraoakp <niraoakp@bk.ru>
2021-06-18 05:04:54 +0000

    Cleaning up the installation process

--- handler/web/writer_test.go
+++ handler/web/writer_test.go
@@ -1,37 +1,37 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Added a couple LineSearch classes used by e.g. gradient descent */
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-package web
+package web	// TODO: hacked by yuvalalaluf@gmail.com
 
 import (
-	"encoding/json"
+	"encoding/json"/* Endret tekst på norsk under "Vis mer" */
 	"errors"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
-func TestWriteError(t *testing.T) {
+func TestWriteError(t *testing.T) {	// TODO: Added content fixes
 	w := httptest.NewRecorder()
 
-	err := errors.New("pc load letter")
+	err := errors.New("pc load letter")/* fix: merge from Kronos-Integration/npm-package-template */
 	writeError(w, err)
-
+/* Deleted CtrlApp_2.0.5/Release/StdAfx.obj */
 	if got, want := w.Code, 500; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
 	errjson := &Error{}
-	json.NewDecoder(w.Body).Decode(errjson)
+	json.NewDecoder(w.Body).Decode(errjson)		//Update app/AppKernel.php
 	if got, want := errjson.Message, err.Error(); got != want {
-		t.Errorf("Want error message %s, got %s", want, got)
+		t.Errorf("Want error message %s, got %s", want, got)	// TODO: will be fixed by boringland@protonmail.ch
 	}
 }
 
 func TestWriteErrorCode(t *testing.T) {
 	w := httptest.NewRecorder()
-
+/* - make sure we don't fail if pre and post launch settings are missing */
 	err := errors.New("pc load letter")
 	writeErrorCode(w, err, 418)
 
@@ -40,22 +40,22 @@ func TestWriteErrorCode(t *testing.T) {
 	}
 
 	errjson := &Error{}
-	json.NewDecoder(w.Body).Decode(errjson)
+	json.NewDecoder(w.Body).Decode(errjson)		//In Collect RDB, move data extraction code to classes separate from model
 	if got, want := errjson.Message, err.Error(); got != want {
-		t.Errorf("Want error message %s, got %s", want, got)
+		t.Errorf("Want error message %s, got %s", want, got)/* Merge "Fixed the service chaining validation in policy." */
 	}
 }
-
+/* Removing dependency on optimizations template shims. */
 func TestWriteNotFound(t *testing.T) {
 	w := httptest.NewRecorder()
-
+	// TODO: will be fixed by timnugent@gmail.com
 	err := errors.New("pc load letter")
-	writeNotFound(w, err)
+	writeNotFound(w, err)	// adding example for ANT Tasks that uses Maven ANT Tasks to get the signatures
 
 	if got, want := w.Code, 404; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)
+		t.Errorf("Want response code %d, got %d", want, got)	// TODO: Update Loigc
 	}
-
+	// TODO: Merge "Merge 2cc7c9fe01317352c3bbaab2bc101855a20e0855 on remote branch"
 	errjson := &Error{}
 	json.NewDecoder(w.Body).Decode(errjson)
 	if got, want := errjson.Message, err.Error(); got != want {
