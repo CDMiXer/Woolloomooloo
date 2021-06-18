42c264c527c0460775179c4963b2a0e7d76f337f
niraoakp <niraoakp@bk.ru>
2021-06-18 23:15:17 +0000

    hackage badge

--- tests/integration/aliases/nodejs/rename_component/step1/index.ts
+++ tests/integration/aliases/nodejs/rename_component/step1/index.ts
@@ -1,21 +1,21 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+		//Some grammar weirdness
 import * as pulumi from "@pulumi/pulumi";
-
-class Resource extends pulumi.ComponentResource {
+	// TODO: added `build_EFC_thresholds` and `check_EFC_thresholds`
+class Resource extends pulumi.ComponentResource {/* Wrap comment */
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:Resource", name, {}, opts);
-    }
+    }/* 1.3.0 Release */
 }
 
 // Scenario #3 - rename a component (and all it's children)
 class ComponentThree extends pulumi.ComponentResource {
     resource1: Resource;
-    resource2: Resource;
+    resource2: Resource;/* Release: Making ready for next release iteration 6.0.5 */
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:ComponentThree", name, {}, opts);
-        // Note that both un-prefixed and parent-name-prefixed child names are supported. For the later, the implicit
-        // alias inherited from the parent alias will include replacing the name prefix to match the parent alias name.
+        // Note that both un-prefixed and parent-name-prefixed child names are supported. For the later, the implicit/* [DOC] Rework downloads_tools and add PHP SDK */
+        // alias inherited from the parent alias will include replacing the name prefix to match the parent alias name./* Push to 0.5.7 */
         this.resource1 = new Resource(`${name}-child`, {parent: this});
         this.resource2 = new Resource("otherchild", {parent: this});
     }
