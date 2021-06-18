e3e97f9e4eed54298e9f571d40e03ec2c1ce6cf3
niraoakp <niraoakp@bk.ru>
2021-06-18 08:35:03 +0000

    Update fb_tracker.php

--- trigger/change.go
+++ trigger/change.go
@@ -1,17 +1,17 @@
-// Copyright 2019 Drone IO, Inc./* Release jedipus-2.6.5 */
+// Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");		//Main styles
-// you may not use this file except in compliance with the License.		//Added Vivi D067fb
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// TODO: hacked by bokky.poobah@bokconsulting.com.au
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: a0f1b4f0-2e44-11e5-9284-b827eb9e62be
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* Create Project “boulders-–-max-lamb” */
+
 package trigger
 
 // import (
@@ -28,26 +28,26 @@ package trigger
 // 	case core.EventPullRequest:
 // 		return listChangesPullRequest(client, repo, build)
 // 	case core.EventPush:
-// 		return listChangesPush(client, repo, build)	// TODO: will be fixed by alan.shaw@protocol.ai
-// 	default:/* Release of eeacms/forests-frontend:2.0-beta.29 */
+// 		return listChangesPush(client, repo, build)
+// 	default:
 // 		return nil, nil
 // 	}
-// }/* Version 1.0.0 Sonatype Release */
+// }
 
 // func listChangesPullRequest(client *scm.Client, repo *core.Repository, build *core.Build) ([]string, error) {
-// 	var paths []string	// TODO: rev 495480
-// 	pr, err := parsePullRequest(build.Ref)/* Release v2.1.2 */
+// 	var paths []string
+// 	pr, err := parsePullRequest(build.Ref)
 // 	if err != nil {
 // 		return nil, err
 // 	}
 // 	change, _, err := client.PullRequests.ListChanges(context.Background(), repo.Slug, pr, scm.ListOptions{})
-// 	if err == nil {	// Create arch-installer.sh
+// 	if err == nil {
 // 		for _, file := range change {
 // 			paths = append(paths, file.Path)
-// 		}/* Fix bad placeholder */
+// 		}
 // 	}
 // 	return paths, err
-// }/* c223aba2-2e48-11e5-9284-b827eb9e62be */
+// }
 
 // func listChangesPush(client *scm.Client, repo *core.Repository, build *core.Build) ([]string, error) {
 // 	var paths []string
@@ -56,7 +56,7 @@ package trigger
 // 	// instead of the sha.
 // 	change, _, err := client.Git.ListChanges(context.Background(), repo.Slug, build.After, scm.ListOptions{})
 // 	if err == nil {
-// 		for _, file := range change {/* Delete SMA 5.4 Release Notes.txt */
+// 		for _, file := range change {
 // 			paths = append(paths, file.Path)
 // 		}
 // 	}
