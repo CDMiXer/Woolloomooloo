94ea892cba97ff3ffb38b9c0ac72978ecce65066
niraoakp <niraoakp@bk.ru>
2021-06-18 23:46:41 +0000

    python: use future.utils.iteritems() for Python 3 compatibility

--- plugin/config/jsonnet_oss.go
+++ plugin/config/jsonnet_oss.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone IO, Inc.
-//	// Rev almost working!
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -9,15 +9,15 @@
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* [Travis, test] New test cases */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 // +build oss
 
 package config
 
-import "github.com/drone/drone/core"	// Merge branch 'master' into allow-fallback-to-samples-without-bank
-/* don't check in generated css */
+import "github.com/drone/drone/core"
+
 // Jsonnet returns a no-op configuration service.
 func Jsonnet(service core.FileService, enabled bool) core.ConfigService {
 	return new(noop)
