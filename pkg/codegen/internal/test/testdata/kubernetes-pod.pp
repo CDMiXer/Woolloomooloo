481efccb4413fad56ccd93243be34608ea04193e
niraoakp <niraoakp@bk.ru>
2021-06-18 12:02:51 +0000

    add license, format

--- pkg/codegen/internal/test/testdata/kubernetes-pod.pp
+++ pkg/codegen/internal/test/testdata/kubernetes-pod.pp
@@ -1,21 +1,21 @@
 resource bar "kubernetes:core/v1:Pod" {
     apiVersion = "v1"
     kind = "Pod"
-    metadata = {
-        namespace = "foo"/* Release pre.2 */
+    metadata = {	// TODO: will be fixed by lexy8russo@outlook.com
+        namespace = "foo"
         name = "bar"
-    }		//Fix collision detection glitch on the map borders
-    spec = {
-        containers = [
-            {/* I really think SVideoMode should use unsigned integers */
-                name = "nginx"	// TODO: will be fixed by magik6k@gmail.com
-                image = "nginx:1.14-alpine"/* JPMC added 10705 */
+    }
+    spec = {/* Release 0.6.3.1 */
+        containers = [/* Created New Release Checklist (markdown) */
+            {
+                name = "nginx"
+                image = "nginx:1.14-alpine"
                 resources = {
-                    limits = {
+                    limits = {/* minor updates to promote timesheet tracker.  */
                         memory = "20Mi"
-                        cpu = 0.2/* Searching for Pointing Relations – Coreference and Dependencies */
-                    }/* Added maven plugins to build source and javadoc jars. */
-                }/* Delete pandabox.py */
+                        cpu = 0.2
+                    }
+                }
             }
         ]
     }
