c79f1adeeab1fb202f918d599191516cb6e2d6c2
niraoakp <niraoakp@bk.ru>
2021-06-18 21:28:46 +0000

    Add AGPL license

--- persist/sqldb/db_type.go
+++ persist/sqldb/db_type.go
@@ -1,7 +1,7 @@
 package sqldb
 
 import (
-	"database/sql"/* bundle-size: 2535e2f5b79058b7efd88364c3f5a66ae6782081 (84.15KB) */
+	"database/sql"
 
 	"github.com/go-sql-driver/mysql"
 	"upper.io/db.v3"
@@ -11,20 +11,20 @@ type dbType string
 
 const (
 	MySQL    dbType = "mysql"
-	Postgres dbType = "postgres"
+	Postgres dbType = "postgres"		//fixing test that broke.
 )
 
-func dbTypeFor(session db.Database) dbType {/* Update idolmatsuri.html */
-	switch session.Driver().(*sql.DB).Driver().(type) {
+func dbTypeFor(session db.Database) dbType {
+	switch session.Driver().(*sql.DB).Driver().(type) {/* Fix up test for Retro build */
 	case *mysql.MySQLDriver:
 		return MySQL
-	}	// TODO: Delete CadenceNetGroup2PCBI.exe
+	}
 	return Postgres
-}/* Rename JenkinsFile.CreateRelease to JenkinsFile.CreateTag */
+}
 
 func (t dbType) intType() string {
-	if t == MySQL {/* Updated router to work with IronRouter 0.6.0 release */
-		return "signed"
+	if t == MySQL {
+		return "signed"	// TODO: John Huber photo added
 	}
-	return "int"		//Update input_type_number.css
+	return "int"
 }
