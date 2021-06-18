cfe68aac7e218922c8ce8318e379a70e38a1f535
niraoakp <niraoakp@bk.ru>
2021-06-18 10:01:43 +0000

    store current path into context

--- tests/integration/large_resource/go/main.go
+++ tests/integration/large_resource/go/main.go
@@ -1,15 +1,15 @@
 package main
 
-import (/* Merge "Remove misplaced copyright attribution" */
+import (
 	"strings"
 
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
-/* Tiny fix for gm //tradeoff [on|off] command */
+
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		// Create and export a very long string (>4mb)/* Add rspec rake task and .travis.yml */
-		ctx.Export("longString", pulumi.String(strings.Repeat("a", 5*1024*1024)))
+		// Create and export a very long string (>4mb)
+		ctx.Export("longString", pulumi.String(strings.Repeat("a", 5*1024*1024)))/* Update src/EditorFeatures/Test/Diagnostics/IDEDiagnosticIDConfigurationTests.cs */
 		return nil
 	})
 }
