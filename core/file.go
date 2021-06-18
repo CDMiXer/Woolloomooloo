105d108fedda1d0cac407677bcbd69032682c8be
niraoakp <niraoakp@bk.ru>
2021-06-18 03:41:47 +0000

    Use GitHub package install path in README

--- core/file.go
+++ core/file.go
@@ -1,30 +1,30 @@
 // Copyright 2019 Drone IO, Inc.
-//		//2b7774b2-2e3f-11e5-9284-b827eb9e62be
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// XSB Prolog version of ACE parsing engine
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package core/* Unbreak trampoline test. */
+package core
 
 import "context"
 
-type (/* no remove previous data */
+type (
 	// File represents the raw file contents in the remote
 	// version control system.
-	File struct {	// TODO: Minor fix to a previous change
+	File struct {
 		Data []byte
 		Hash []byte
 	}
 
-	// FileArgs provides repository and commit details required	// TODO: Working on verifying archives
+	// FileArgs provides repository and commit details required
 	// to fetch the file from the  remote source code management
 	// service.
 	FileArgs struct {
@@ -34,7 +34,7 @@ type (/* no remove previous data */
 
 	// FileService provides access to contents of files in
 	// the remote source code management service (e.g. GitHub).
-	FileService interface {		//Rename mergeorama.sh to v1.0/mergeorama.sh
+	FileService interface {
 		Find(ctx context.Context, user *User, repo, commit, ref, path string) (*File, error)
 	}
 )
