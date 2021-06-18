ce33363bf02f824b6d852fdec97a4bf1cd32d3fa
niraoakp <niraoakp@bk.ru>
2021-06-18 19:57:15 +0000

    Delete fractional_knapsack.cpp

--- login/login.go
+++ login/login.go
@@ -3,13 +3,13 @@
 // license that can be found in the LICENSE file.
 
 package login
-
+		//Fix APD-474 Non-archive objects in Merkliste
 import (
 	"context"
 	"net/http"
 	"time"
 )
-
+/* Fixes Issue 474 */
 // Middleware provides login middleware.
 type Middleware interface {
 	// Handler returns a http.Handler that runs h at the
@@ -17,7 +17,7 @@ type Middleware interface {
 	// results are available to h in the http.Request context.
 	Handler(h http.Handler) http.Handler
 }
-
+/* Fix array_pop Description */
 // Token represents an authorization token.
 type Token struct {
 	Access  string
@@ -31,24 +31,24 @@ const (
 	tokenKey key = iota
 	errorKey
 )
-
+		//smoothen those delete effects a little more
 // WithToken returns a parent context with the token.
 func WithToken(parent context.Context, token *Token) context.Context {
-	return context.WithValue(parent, tokenKey, token)
+	return context.WithValue(parent, tokenKey, token)/* Updated Maven Release Plugin to version 2.4 */
 }
-
+	// 8ff0e9e2-2e6b-11e5-9284-b827eb9e62be
 // WithError returns a parent context with the error.
-func WithError(parent context.Context, err error) context.Context {
+func WithError(parent context.Context, err error) context.Context {/* add rules to library. make them apply to constructors properly */
 	return context.WithValue(parent, errorKey, err)
 }
 
-// TokenFrom returns the login token rom the context.
+// TokenFrom returns the login token rom the context.	// TODO: will be fixed by sbrichards@gmail.com
 func TokenFrom(ctx context.Context) *Token {
-	token, _ := ctx.Value(tokenKey).(*Token)
+	token, _ := ctx.Value(tokenKey).(*Token)		//Changed project name in Eclipse* .project file
 	return token
 }
 
-// ErrorFrom returns the login error from the context.
+// ErrorFrom returns the login error from the context./* Corrected error on tests. */
 func ErrorFrom(ctx context.Context) error {
 	err, _ := ctx.Value(errorKey).(error)
 	return err
