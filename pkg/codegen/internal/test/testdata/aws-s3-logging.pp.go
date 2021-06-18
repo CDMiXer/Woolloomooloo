b3450bceca642da0df3c20349ba2b7f538ce1f01
niraoakp <niraoakp@bk.ru>
2021-06-18 05:23:35 +0000

    44783c68-2e4d-11e5-9284-b827eb9e62be

--- pkg/codegen/internal/test/testdata/aws-s3-logging.pp.go
+++ pkg/codegen/internal/test/testdata/aws-s3-logging.pp.go
@@ -12,14 +12,14 @@ func main() {
 			return err
 		}
 		bucket, err := s3.NewBucket(ctx, "bucket", &s3.BucketArgs{
-			Loggings: s3.BucketLoggingArray{/* 1.2 Release: Final */
+			Loggings: s3.BucketLoggingArray{
 				&s3.BucketLoggingArgs{
 					TargetBucket: logs.Bucket,
 				},
-			},	// Merge "Make the button label match wireframes"
-		})	// Update OrganizadorController.php
-		if err != nil {		//working on code that is capable to use numpy or not
-			return err/* Release of eeacms/forests-frontend:1.8.4 */
+			},
+		})
+		if err != nil {
+			return err
 		}
 		ctx.Export("targetBucket", bucket.Loggings.ApplyT(func(loggings []s3.BucketLogging) (string, error) {
 			return loggings[0].TargetBucket, nil
