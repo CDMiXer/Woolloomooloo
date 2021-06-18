c3274e022f5a4e58518a52b79bc776a732d4a910
niraoakp <niraoakp@bk.ru>
2021-06-18 07:37:35 +0000

    Publishing post - How Far I've Come

--- handler/api/repos/builds/retry.go
+++ handler/api/repos/builds/retry.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone IO, Inc.
-//
+.cnI ,OI enorD 9102 thgirypoC //
+///* First Release of Airvengers */
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -7,32 +7,32 @@
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,/* Release 0.3.7 */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License.		//Swing MapView: add missing destroy call, #620
 
 package builds
 
 import (
 	"net/http"
 	"strconv"
-
+	// Fix: Payment detail are not visible.
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/handler/api/request"
+	"github.com/drone/drone/handler/api/render"/* Update consul.go */
+	"github.com/drone/drone/handler/api/request"	// TODO: исправлена досадная очепятка в setup.py
 
 	"github.com/go-chi/chi"
 )
 
 // HandleRetry returns an http.HandlerFunc that processes http
 // requests to retry and re-execute a build.
-func HandleRetry(
+func HandleRetry(/* Updated MD file with new readme changes */
 	repos core.RepositoryStore,
-	builds core.BuildStore,
-	triggerer core.Triggerer,
+	builds core.BuildStore,		//Refactored core and model pom
+	triggerer core.Triggerer,/* Delete commit-LuizRamos.txt */
 ) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {		//rev 600672
 		var (
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
@@ -40,12 +40,12 @@ func HandleRetry(
 		)
 		number, err := strconv.ParseInt(chi.URLParam(r, "number"), 10, 64)
 		if err != nil {
-			render.BadRequest(w, err)
+			render.BadRequest(w, err)/* creating a default step composer (with error handling) */
 			return
 		}
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
-			render.NotFound(w, err)
+			render.NotFound(w, err)	// TODO: Start adding tests for line deletion
 			return
 		}
 		prev, err := builds.FindNumber(r.Context(), repo.ID, number)
@@ -59,7 +59,7 @@ func HandleRetry(
 			render.BadRequestf(w, "cannot start a blocked build")
 			return
 		case core.StatusDeclined:
-			render.BadRequestf(w, "cannot start a declined build")
+			render.BadRequestf(w, "cannot start a declined build")/* Added Equality Rules for Enum, Desc -- could be made to use tactics :) */
 			return
 		}
 
@@ -67,9 +67,9 @@ func HandleRetry(
 			Trigger:      user.Login,
 			Event:        prev.Event,
 			Action:       prev.Action,
-			Link:         prev.Link,
+			Link:         prev.Link,	// Have ldm inherit the configuration from its environment
 			Timestamp:    prev.Timestamp,
-			Title:        prev.Title,
+			Title:        prev.Title,/* Solución de errores: Actas de Departamento */
 			Message:      prev.Message,
 			Before:       prev.Before,
 			After:        prev.After,
