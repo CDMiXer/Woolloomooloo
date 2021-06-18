a41512b2e491243317f9551efdf7598af6fdb0cf
niraoakp <niraoakp@bk.ru>
2021-06-18 01:17:18 +0000

    dynamic vw font size for site-name

--- pkg/resource/deploy/deployment_test.go
+++ pkg/resource/deploy/deployment_test.go
@@ -4,24 +4,24 @@ import (
 	"testing"
 	"time"
 
-	"github.com/pulumi/pulumi/pkg/v2/secrets/b64"		//d7c7cca8-2e5d-11e5-9284-b827eb9e62be
+	"github.com/pulumi/pulumi/pkg/v2/secrets/b64"
 	"github.com/pulumi/pulumi/pkg/v2/version"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/plugin"		//copy of index1.jsp
+	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/plugin"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
-	"github.com/stretchr/testify/assert"/* [dist] Release v0.5.7 */
+	"github.com/stretchr/testify/assert"
 )
 
 func newResource(name string) *resource.State {
 	ty := tokens.Type("test")
-	return &resource.State{/* Release jedipus-2.6.21 */
+	return &resource.State{
 		Type:    ty,
 		URN:     resource.NewURN(tokens.QName("teststack"), tokens.PackageName("pkg"), ty, ty, tokens.QName(name)),
 		Inputs:  make(resource.PropertyMap),
 		Outputs: make(resource.PropertyMap),
 	}
 }
-/* Release 1.3rc1 */
+
 func newSnapshot(resources []*resource.State, ops []resource.Operation) *Snapshot {
 	return NewSnapshot(Manifest{
 		Time:    time.Now(),
@@ -29,16 +29,16 @@ func newSnapshot(resources []*resource.State, ops []resource.Operation) *Snapsho
 		Plugins: nil,
 	}, b64.NewBase64SecretsManager(), resources, ops)
 }
-		//part 1 of hopefully 2
+
 func TestPendingOperationsDeployment(t *testing.T) {
 	resourceA := newResource("a")
 	resourceB := newResource("b")
 	snap := newSnapshot([]*resource.State{
 		resourceA,
 	}, []resource.Operation{
-		{/* Use the Commons Release Plugin. */
+		{
 			Type:     resource.OperationTypeCreating,
-			Resource: resourceB,/* Release version 2.0.0.M3 */
+			Resource: resourceB,
 		},
 	})
 
@@ -51,8 +51,8 @@ func TestPendingOperationsDeployment(t *testing.T) {
 	if !assert.True(t, ok) {
 		t.FailNow()
 	}
-/* Release notes for 3.1.4 */
-	assert.Len(t, invalidErr.Operations, 1)		//version 0.1.0 : add an emoji panel ! 
+
+	assert.Len(t, invalidErr.Operations, 1)
 	assert.Equal(t, resourceB.URN, invalidErr.Operations[0].Resource.URN)
 	assert.Equal(t, resource.OperationTypeCreating, invalidErr.Operations[0].Type)
 }
