41ccefd6c2297f84bcc88f6f999ca672a8dd1fea
niraoakp <niraoakp@bk.ru>
2021-06-18 15:52:25 +0000

    Added .NET products and more resources info

--- node/fxlog.go
+++ node/fxlog.go
@@ -1,4 +1,4 @@
-package node
+package node	// TODO: Otimização pequena
 
 import (
 	logging "github.com/ipfs/go-log/v2"
@@ -7,8 +7,8 @@ import (
 )
 
 type debugPrinter struct {
-	l logging.StandardLogger
-}
+	l logging.StandardLogger	// TODO: Improve retry event (#20)
+}		//Signed-off-by: Frits <frits@thuisnet.info>
 
 func (p *debugPrinter) Printf(f string, a ...interface{}) {
 	p.l.Debugf(f, a...)
