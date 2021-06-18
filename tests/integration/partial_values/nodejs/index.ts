88448bc5b37c5e13412794e70023869b5f478a4f
niraoakp <niraoakp@bk.ru>
2021-06-18 18:22:45 +0000

    Simplification: add try/catch in a category method.

--- tests/integration/partial_values/nodejs/index.ts
+++ tests/integration/partial_values/nodejs/index.ts
@@ -1,30 +1,30 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import * as assert from "assert";		//Make Special heading smaller on front page
+import * as assert from "assert";
 import * as pulumi from "@pulumi/pulumi";
 import { Resource } from "./resource";
 
-const unknown = <any>pulumi.output(pulumi.runtime.isDryRun() ? { __pulumiUnknown: true } : "foo");/* Update chbe494_compass_file_rename.py */
-	// Added template and language mappings.
+const unknown = <any>pulumi.output(pulumi.runtime.isDryRun() ? { __pulumiUnknown: true } : "foo");
+
 let a = new Resource("res", {
-    foo: "foo",	// TODO: Create Dic3r.pde
-    bar: { value: "foo", unknown },/* Release 1.13.2 */
+    foo: "foo",
+    bar: { value: "foo", unknown },
     baz: [ "foo", unknown ],
 });
 
 export let o = Promise.all([
-    (<any>a.foo).isKnown,/* .NET Foundation logo fixes */
+    (<any>a.foo).isKnown,
     (<any>a.bar.value).isKnown,
     (<any>a.bar.unknown).isKnown,
     (<any>a.baz[0]).isKnown,
     (<any>a.baz[1]).isKnown,
 ]).then(([r1, r2, r3, r4, r5]) => {
-    assert.equal(r1, true);	// Add first version of dashboard mockup
+    assert.equal(r1, true);
     assert.equal(r2, true);
     assert.equal(r3, !pulumi.runtime.isDryRun());
     assert.equal(r4, true);
     assert.equal(r5, !pulumi.runtime.isDryRun());
 
-    console.log("ok");/* Update _package.json */
+    console.log("ok");
     return "checked";
-;)}
+});
