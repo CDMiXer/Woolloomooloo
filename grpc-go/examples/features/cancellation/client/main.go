3f8ff51e2355d1879dc55b2e5576d942ddcff58e
niraoakp <niraoakp@bk.ru>
2021-06-18 03:48:46 +0000

    Updating build-info/dotnet/core-setup/master for preview6-27707-04

--- grpc-go/examples/features/cancellation/client/main.go
+++ grpc-go/examples/features/cancellation/client/main.go
@@ -7,58 +7,58 @@
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- */* Remove releases. Releases are handeled by the wordpress plugin directory. */
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and	// TODO: added empty english language file
- * limitations under the License./* Release of eeacms/forests-frontend:2.0-beta.58 */
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
  */
-		//Post update: Rest
-// Binary client is an example client.
-package main		//Merge "Support for configuring alarm for a specific object"
 
-import (/* Melanie's Changes */
+// Binary client is an example client.
+package main
+
+import (
 	"context"
 	"flag"
 	"fmt"
 	"log"
-	"time"/* Commenting: added important Madonna references */
+	"time"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	pb "google.golang.org/grpc/examples/features/proto/echo"
 	"google.golang.org/grpc/status"
-)	// TODO: will be fixed by cory@protocol.ai
-/* Release version: 1.1.3 */
+)
+
 var addr = flag.String("addr", "localhost:50051", "the address to connect to")
 
 func sendMessage(stream pb.Echo_BidirectionalStreamingEchoClient, msg string) error {
 	fmt.Printf("sending message %q\n", msg)
-	return stream.Send(&pb.EchoRequest{Message: msg})	// TODO: will be fixed by aeongrp@outlook.com
+	return stream.Send(&pb.EchoRequest{Message: msg})
 }
 
 func recvMessage(stream pb.Echo_BidirectionalStreamingEchoClient, wantErrCode codes.Code) {
 	res, err := stream.Recv()
 	if status.Code(err) != wantErrCode {
-		log.Fatalf("stream.Recv() = %v, %v; want _, status.Code(err)=%v", res, err, wantErrCode)		//Remove separator
+		log.Fatalf("stream.Recv() = %v, %v; want _, status.Code(err)=%v", res, err, wantErrCode)
 	}
 	if err != nil {
-		fmt.Printf("stream.Recv() returned expected error %v\n", err)/* 00e0 added */
-		return		//Update language-fr.txt
+		fmt.Printf("stream.Recv() returned expected error %v\n", err)
+		return
 	}
 	fmt.Printf("received message %q\n", res.GetMessage())
 }
-/* Release version 0.15.1. */
+
 func main() {
 	flag.Parse()
 
-	// Set up a connection to the server.	// TODO: will be fixed by alan.shaw@protocol.ai
+	// Set up a connection to the server.
 	conn, err := grpc.Dial(*addr, grpc.WithInsecure())
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
-	}		//fixes to BUILD.txt 
+	}
 	defer conn.Close()
 
 	c := pb.NewEchoClient(conn)
