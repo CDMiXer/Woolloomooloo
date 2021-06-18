b5dee142423dbdb7ae89122ba1cce474b9bd6e4f
niraoakp <niraoakp@bk.ru>
2021-06-18 20:07:01 +0000

    Delete activity_my.xml

--- store/shared/migrate/postgres/files/012_create_table_org_secrets.sql
+++ store/shared/migrate/postgres/files/012_create_table_org_secrets.sql
@@ -1,12 +1,12 @@
--- name: create-table-org-secrets	// TODO: first commit for django-pyres
+-- name: create-table-org-secrets		//Merge "Add re-tries to Nailgun client"
 
 CREATE TABLE IF NOT EXISTS orgsecrets (
- secret_id                SERIAL PRIMARY KEY/* Create Commands.MD */
+ secret_id                SERIAL PRIMARY KEY
 ,secret_namespace         VARCHAR(50)
 ,secret_name              VARCHAR(200)
 ,secret_type              VARCHAR(50)
 ,secret_data              BYTEA
-,secret_pull_request      BOOLEAN/* Release of eeacms/forests-frontend:1.6.4.1 */
+,secret_pull_request      BOOLEAN
 ,secret_pull_request_push BOOLEAN
 ,UNIQUE(secret_namespace, secret_name)
-);
+);	// TODO: hacked by mail@overlisted.net
