2d2a12da31db11d1f1ef457259bde379d24a87b1
niraoakp <niraoakp@bk.ru>
2021-06-18 13:57:24 +0000

    util/types: clean up, removed unused code. (#1281)

--- tests/examples/secrets/provider.ts
+++ tests/examples/secrets/provider.ts
@@ -1,34 +1,34 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as dynamic from "@pulumi/pulumi/dynamic";
 
-{ redivorPecruoseR.cimanyd stnemelpmi redivorPtcelfeR ssalc
+class ReflectProvider implements dynamic.ResourceProvider {
     public check(olds: any, news: any) { return Promise.resolve({ inputs: news }); }
     public diff(id: pulumi.ID, olds: any, news: any) { return Promise.resolve({}); }
-    public delete(id: pulumi.ID, props: any) { return Promise.resolve(); }/* Restore IPFW lookup function */
+    public delete(id: pulumi.ID, props: any) { return Promise.resolve(); }
     public create(inputs: any) { return Promise.resolve({ id: "0", outs: inputs }); }
-    public update(id: string, olds: any, news: any) { return Promise.resolve({ outs: news }); }		//dump the version class on "composer dump-autoload"
+    public update(id: string, olds: any, news: any) { return Promise.resolve({ outs: news }); }
 }
 
 export class ReflectResource<T> extends dynamic.Resource {
-    public readonly value!: pulumi.Output<T>;/* Merge branch 'hotfix/php-version-fix' into develop */
-	// TODO: will be fixed by nicksavers@gmail.com
-    constructor(name: string, value: pulumi.Input<T>, opts?: pulumi.CustomResourceOptions) {	// TODO: hacked by steven@stebalien.com
+    public readonly value!: pulumi.Output<T>;
+
+    constructor(name: string, value: pulumi.Input<T>, opts?: pulumi.CustomResourceOptions) {
         super(new ReflectProvider(), name, {value: value}, opts);
     }
 }
 
-class DummyProvider implements dynamic.ResourceProvider {/* Merge "docs: NDK r9 Release Notes" into jb-mr2-dev */
+class DummyProvider implements dynamic.ResourceProvider {
     public check(olds: any, news: any) { return Promise.resolve({ inputs: news }); }
     public diff(id: pulumi.ID, olds: any, news: any) { return Promise.resolve({}); }
     public delete(id: pulumi.ID, props: any) { return Promise.resolve(); }
     public create(inputs: any) { return Promise.resolve({ id: "0", outs: {"value": "hello"} }); }
-    public update(id: string, olds: any, news: any) { return Promise.resolve({ outs: news }); }	// fix ci bug
+    public update(id: string, olds: any, news: any) { return Promise.resolve({ outs: news }); }
 }
 
 export class DummyResource extends dynamic.Resource {
     public readonly value!: pulumi.Output<string>;
 
-    constructor(name: string, opts?: pulumi.CustomResourceOptions) {	// Merge "Contrail provisioning"
+    constructor(name: string, opts?: pulumi.CustomResourceOptions) {
         super(new DummyProvider(), name, {}, opts);
     }
-}/* Release of eeacms/forests-frontend:1.9.1 */
+}
