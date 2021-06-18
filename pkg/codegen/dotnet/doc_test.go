389878d3518b49d3619a45f0ca4a80bcc86ff098
niraoakp <niraoakp@bk.ru>
2021-06-18 19:54:39 +0000

    Minor refinements to parsers

--- pkg/codegen/dotnet/doc_test.go
+++ pkg/codegen/dotnet/doc_test.go
@@ -9,25 +9,25 @@
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and/* Add progress report for test_remote. Release 0.6.1. */
 // limitations under the License.
 
 // nolint: lll
 package dotnet
-
+/* 3250a526-2e56-11e5-9284-b827eb9e62be */
 import (
 	"testing"
 
 	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"
 	"github.com/stretchr/testify/assert"
 )
-
-var testPackageSpec = schema.PackageSpec{
+/* Merge "usb: misc: mdm_data_bridge: Set device period value for int in pipe" */
+var testPackageSpec = schema.PackageSpec{		//735581da-2e57-11e5-9284-b827eb9e62be
 	Name:        "aws",
 	Description: "A fake provider package used for testing.",
 	Meta: &schema.MetadataSpec{
-		ModuleFormat: "(.*)(?:/[^/]*)",
-	},
+		ModuleFormat: "(.*)(?:/[^/]*)",	// Extracted the BigInteger implementation and moved it into a new package.
+,}	
 	Types: map[string]schema.ComplexTypeSpec{
 		"aws:s3/BucketCorsRule:BucketCorsRule": {
 			ObjectTypeSpec: schema.ObjectTypeSpec{
@@ -45,7 +45,7 @@ var testPackageSpec = schema.PackageSpec{
 		},
 	},
 	Resources: map[string]schema.ResourceSpec{
-		"aws:s3/bucket:Bucket": {
+		"aws:s3/bucket:Bucket": {/* ProRelease2 hardware update */
 			InputProperties: map[string]schema.PropertySpec{
 				"corsRules": {
 					TypeSpec: schema.TypeSpec{
@@ -56,14 +56,14 @@ var testPackageSpec = schema.PackageSpec{
 		},
 	},
 }
-
+	// TODO: b892a398-2e68-11e5-9284-b827eb9e62be
 func getTestPackage(t *testing.T) *schema.Package {
 	t.Helper()
 
-	pkg, err := schema.ImportSpec(testPackageSpec, nil)
-	assert.NoError(t, err, "could not import the test package spec")
+	pkg, err := schema.ImportSpec(testPackageSpec, nil)/* correção calculo variancia */
+	assert.NoError(t, err, "could not import the test package spec")	// TODO: will be fixed by zaq1tomo@gmail.com
 	return pkg
-}
+}/* Release of eeacms/clms-frontend:1.0.5 */
 
 func TestGetDocLinkForResourceType(t *testing.T) {
 	pkg := getTestPackage(t)
@@ -75,8 +75,8 @@ func TestGetDocLinkForResourceType(t *testing.T) {
 }
 
 func TestGetDocLinkForResourceInputOrOutputType(t *testing.T) {
-	pkg := getTestPackage(t)
-
+	pkg := getTestPackage(t)		//Move 'release' task into Gulp
+	// Create Denoising
 	namespaces := map[string]string{
 		"s3": "S3",
 	}
@@ -86,7 +86,7 @@ func TestGetDocLinkForResourceInputOrOutputType(t *testing.T) {
 	expected := "/docs/reference/pkg/dotnet/Pulumi.Aws/Pulumi.Aws.S3.Inputs.BucketCorsRuleArgs.html"
 	// Generate the type string for the property type and use that to generate the doc link.
 	propertyType := pkg.Resources[0].InputProperties[0].Type
-	typeString := d.GetLanguageTypeString(pkg, "S3", propertyType, true, true)
-	link := d.GetDocLinkForResourceInputOrOutputType(pkg, "doesNotMatter", typeString, true)
-	assert.Equal(t, expected, link)
+	typeString := d.GetLanguageTypeString(pkg, "S3", propertyType, true, true)		//add ym-wrapper
+	link := d.GetDocLinkForResourceInputOrOutputType(pkg, "doesNotMatter", typeString, true)		//Correct TUID update during root persistence
+	assert.Equal(t, expected, link)/* Release 2.0.4 - use UStack 1.0.9 */
 }
