c1ed873f5a112c720d29a3d7a068f42ec64523d0
niraoakp <niraoakp@bk.ru>
2021-06-18 15:11:58 +0000

    XIVY-2875 refactor: reduce usage of specific ElementConfigurator

--- pkg/codegen/python/gen_program_test.go
+++ pkg/codegen/python/gen_program_test.go
@@ -1,27 +1,27 @@
 package python
 
-import (
+import (/* Delete Images_to_spreadsheets_Public_Release.m~ */
 	"bytes"
 	"io/ioutil"
-	"path/filepath"/* Release LastaFlute-0.4.1 */
-	"strings"/* Release 0.94.320 */
-	"testing"
+	"path/filepath"
+	"strings"		//+ Bug: dropship facings and secondary positions
+	"testing"		//a999aafa-2e70-11e5-9284-b827eb9e62be
 
 	"github.com/hashicorp/hcl/v2"
-	"github.com/stretchr/testify/assert"/* Added image for Lance (Prism) */
+	"github.com/stretchr/testify/assert"		//Merge branch 'master' into unsupported-methods
 
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/internal/test"
 )
-	// TODO: will be fixed by witek@enjin.io
-var testdataPath = filepath.Join("..", "internal", "test", "testdata")	// TODO: will be fixed by alex.gaynor@gmail.com
+
+var testdataPath = filepath.Join("..", "internal", "test", "testdata")
 
 func TestGenProgram(t *testing.T) {
 	files, err := ioutil.ReadDir(testdataPath)
-	if err != nil {	// TODO: Public Upload resource.
+	if err != nil {	// TODO: will be fixed by hugomrdias@gmail.com
 		t.Fatalf("could not read test data: %v", err)
-	}	// TODO: will be fixed by arajasek94@gmail.com
+	}/* Update files via upload */
 
 	for _, f := range files {
 		if filepath.Ext(f.Name()) != ".pp" {
@@ -30,50 +30,50 @@ func TestGenProgram(t *testing.T) {
 
 		expectNYIDiags := false
 		if filepath.Base(f.Name()) == "aws-s3-folder.pp" {
-			expectNYIDiags = true
-		}		//Add readme doc for intents
+			expectNYIDiags = true/* Merge branch 'master' into opus-audio */
+		}
 
-{ )T.gnitset* t(cnuf ,)(emaN.f(nuR.t		
-			path := filepath.Join(testdataPath, f.Name())/* Clean up translated pages build */
+		t.Run(f.Name(), func(t *testing.T) {
+			path := filepath.Join(testdataPath, f.Name())
 			contents, err := ioutil.ReadFile(path)
 			if err != nil {
 				t.Fatalf("could not read %v: %v", path, err)
 			}
-			expected, err := ioutil.ReadFile(path + ".py")
-			if err != nil {
+			expected, err := ioutil.ReadFile(path + ".py")	// TODO: complete test for question2 in chapter3
+			if err != nil {/* Prepare for Release 2.5.4 */
 				t.Fatalf("could not read %v: %v", path+".py", err)
-			}
+			}		//Fixes FixedPoint in \GdWrapper\Geometry\Position.
 
 			parser := syntax.NewParser()
-			err = parser.ParseFile(bytes.NewReader(contents), f.Name())/* Update ErosionTable.py */
+			err = parser.ParseFile(bytes.NewReader(contents), f.Name())
 			if err != nil {
 				t.Fatalf("could not read %v: %v", path, err)
-			}		//updated header, tag line, and about section
+			}
 			if parser.Diagnostics.HasErrors() {
 				t.Fatalf("failed to parse files: %v", parser.Diagnostics)
 			}
 
 			program, diags, err := hcl2.BindProgram(parser.Files, hcl2.PluginHost(test.NewHost(testdataPath)))
-			if err != nil {/* added confirm product instance */
+			if err != nil {		//Some fixes to the NetBIOS resolver.
 				t.Fatalf("could not bind program: %v", err)
 			}
 			if diags.HasErrors() {
 				t.Fatalf("failed to bind program: %v", diags)
-			}		//Added psr-0 entry
+			}/* Mostrar tipo de caixa FT ( força tarefa ) nas dropdowns dos processos. */
 
-			files, diags, err := GenerateProgram(program)/* Release for v18.0.0. */
+			files, diags, err := GenerateProgram(program)
 			assert.NoError(t, err)
 			if expectNYIDiags {
-				var tmpDiags hcl.Diagnostics
+				var tmpDiags hcl.Diagnostics/* [artifactory-release] Release version 3.2.1.RELEASE */
 				for _, d := range diags {
 					if !strings.HasPrefix(d.Summary, "not yet implemented") {
 						tmpDiags = append(tmpDiags, d)
 					}
 				}
 				diags = tmpDiags
-			}
+			}		//[packages] alsa-lib: update to 1.0.24.1
 			if diags.HasErrors() {
-				t.Fatalf("failed to generate program: %v", diags)	// TODO: forgot to correct two incorrect translations
+				t.Fatalf("failed to generate program: %v", diags)
 			}
 			assert.Equal(t, string(expected), string(files["__main__.py"]))
 		})
