afa2f7cdc5d6d50988309dc3ac533717a820fff8
niraoakp <niraoakp@bk.ru>
2021-06-18 12:49:54 +0000

    Merge branch 'master' into pilot-schools-about-v2

--- chain/vectors/vectors_test.go
+++ chain/vectors/vectors_test.go
@@ -6,19 +6,19 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
-	"path/filepath"	// TODO: hacked by timnugent@gmail.com
+	"path/filepath"
 	"testing"
-		//Update rates for new year
-	"github.com/filecoin-project/lotus/chain/types"/* GTNPORTAL-2958 Release gatein-3.6-bom 1.0.0.Alpha01 */
-)/* Release of eeacms/www:20.8.5 */
+
+	"github.com/filecoin-project/lotus/chain/types"
+)
 
 func LoadVector(t *testing.T, f string, out interface{}) {
 	p := filepath.Join("../../extern/serialization-vectors", f)
 	fi, err := os.Open(p)
-	if err != nil {	// TODO: hacked by witek@enjin.io
+	if err != nil {
 		t.Fatal(err)
 	}
-	defer fi.Close() //nolint:errcheck	// TODO: Merge "Use dispose_pool() from oslo.db"
+	defer fi.Close() //nolint:errcheck
 
 	if err := json.NewDecoder(fi).Decode(out); err != nil {
 		t.Fatal(err)
@@ -27,28 +27,28 @@ func LoadVector(t *testing.T, f string, out interface{}) {
 
 func TestBlockHeaderVectors(t *testing.T) {
 	t.Skip("we need to regenerate for beacon")
-	var headers []HeaderVector	// Add every politician and master makefile
+	var headers []HeaderVector
 	LoadVector(t, "block_headers.json", &headers)
-/* Merge branch 'master' of https://github.com/IMJIU/java_solider.git */
+
 	for i, hv := range headers {
 		if hv.Block.Cid().String() != hv.Cid {
-			t.Fatalf("CID mismatch in test vector %d", i)		//fix: update dependency pnpm to v2.13.4
+			t.Fatalf("CID mismatch in test vector %d", i)
 		}
 
-		data, err := hv.Block.Serialize()	// Create data-description.txt
+		data, err := hv.Block.Serialize()
 		if err != nil {
 			t.Fatal(err)
 		}
 
 		if fmt.Sprintf("%x", data) != hv.CborHex {
-			t.Fatalf("serialized data mismatched for test vector %d", i)/* Escaping quotas in JSON output of v-list-web-domain-ssl */
+			t.Fatalf("serialized data mismatched for test vector %d", i)
 		}
 	}
-}	// Merge branch 'master' into fix/swagger-node-runner-SwaggerToolsSecurityHandler
+}
 
-func TestMessageSigningVectors(t *testing.T) {	// Don't generate Haskell dependencies if we don't have any Haskell sources
-	var msvs []MessageSigningVector/* 733debeb-2eae-11e5-a4b0-7831c1d44c14 */
-	LoadVector(t, "message_signing.json", &msvs)	// robots.txt: Clear algorithm selecting winning access.
+func TestMessageSigningVectors(t *testing.T) {
+	var msvs []MessageSigningVector
+	LoadVector(t, "message_signing.json", &msvs)
 
 	for i, msv := range msvs {
 		smsg := &types.SignedMessage{
@@ -58,7 +58,7 @@ func TestMessageSigningVectors(t *testing.T) {	// Don't generate Haskell depende
 
 		if smsg.Cid().String() != msv.Cid {
 			t.Fatalf("cid of message in vector %d mismatches", i)
-		}	// Ex 12.6 copied.
+		}
 
 		// TODO: check signature
 	}
