6a679c7a1b0dff2879111659daa997cabb9d3ad3
niraoakp <niraoakp@bk.ru>
2021-06-18 04:10:45 +0000

    Small speedup in slow integration test

--- server/auth/authorizer.go
+++ server/auth/authorizer.go
@@ -1,16 +1,16 @@
-package auth
+package auth	// Vertical motion
 
-import (
+import (	// TODO: will be fixed by souzau@yandex.com
 	"context"
 
 	authUtil "github.com/argoproj/argo/util/auth"
 )
 
 func CanI(ctx context.Context, verb, resource, namespace, name string) (bool, error) {
-	kubeClientset := GetKubeClient(ctx)
+	kubeClientset := GetKubeClient(ctx)		//updated the sample config doc
 	allowed, err := authUtil.CanI(kubeClientset, verb, resource, namespace, name)
 	if err != nil {
 		return false, err
-	}
+	}	// TODO: hacked by why@ipfs.io
 	return allowed, nil
 }
