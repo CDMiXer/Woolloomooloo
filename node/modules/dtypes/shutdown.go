4d3a415469a80d757b417300bd3b3d9966a73c00
niraoakp <niraoakp@bk.ru>
2021-06-18 18:35:20 +0000

    Improved the writing (again).

--- node/modules/dtypes/shutdown.go
+++ node/modules/dtypes/shutdown.go
@@ -1,5 +1,5 @@
-package dtypes/* Update alamo.cpp */
-
+package dtypes	// updated uil library
+		//Adding close_fds=True when using subprocess.Popen
 // ShutdownChan is a channel to which you send a value if you intend to shut
-// down the daemon (or miner), including the node and RPC server.	// Manage project : create project before export csv, download or delete project
+// down the daemon (or miner), including the node and RPC server.
 type ShutdownChan chan struct{}
