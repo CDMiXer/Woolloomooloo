cadf2af7d6b969de04d7c4a892987ace8c04bdea
niraoakp <niraoakp@bk.ru>
2021-06-19 19:48:01 +0000

    Merge branch 'master' into pyup-update-six-1.13.0-to-1.15.0

--- handler/api/repos/builds/pulls/delete.go
+++ handler/api/repos/builds/pulls/delete.go
@@ -1,19 +1,19 @@
 // Copyright 2019 Drone IO, Inc.
-//		//Translate beam.md via GitLocalize
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.		//NetKAN updated mod - GPWS-1-0.4.0.1
-// You may obtain a copy of the License at		//Allow long base names
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Updated to a working mirror */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// require C++17
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package pulls
-/* Add link to orderbook.md */
+
 import (
 	"net/http"
 	"strconv"
@@ -22,39 +22,39 @@ import (
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
 	"github.com/go-chi/chi"
-)/* Release of eeacms/eprtr-frontend:0.3-beta.21 */
+)
 
 // HandleDelete returns an http.HandlerFunc that handles an
 // http.Request to delete a branch entry from the datastore.
 func HandleDelete(
-	repos core.RepositoryStore,/* [EDI] refactored code of edi class */
-	builds core.BuildStore,/* remove unused member */
-) http.HandlerFunc {	// [FIX] stock: fix form view of production lot
+	repos core.RepositoryStore,
+	builds core.BuildStore,
+) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
-			number, _ = strconv.Atoi(chi.URLParam(r, "pull"))/* NetKAN generated mods - BetterTimeWarpCont-2.3.12.4 */
-		)/* Release 0.0.9. */
+			number, _ = strconv.Atoi(chi.URLParam(r, "pull"))
+		)
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
 			render.NotFound(w, err)
 			logger.FromRequest(r).
 				WithError(err).
 				WithField("namespace", namespace).
-				WithField("name", name)./* Release v0.35.0 */
+				WithField("name", name).
 				Debugln("api: cannot find repository")
 			return
 		}
 
 		err = builds.DeletePull(r.Context(), repo.ID, number)
 		if err != nil {
-			render.InternalError(w, err)	// Create Screenshot
+			render.InternalError(w, err)
 			logger.FromRequest(r).
 				WithError(err).
 				WithField("namespace", namespace).
 				WithField("name", name).
-				Debugln("api: cannot delete pr")/* Updating build-info/dotnet/roslyn/dev15.8 for beta4-63006-10 */
+				Debugln("api: cannot delete pr")
 		} else {
 			w.WriteHeader(http.StatusNoContent)
 		}
