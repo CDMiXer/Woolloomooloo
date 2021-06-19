5944295c528720903193ec3c94658964ab75dd57
niraoakp <niraoakp@bk.ru>
2021-06-19 13:10:44 +0000

    Merge "Enumerate Projects with Unscoped Tokens"

--- chain/sub/incoming_test.go
+++ chain/sub/incoming_test.go
@@ -2,8 +2,8 @@ package sub
 
 import (
 	"context"
-	"testing"		//Update AboutDialog.ui
-	// Create 278. First Bad Version
+	"testing"
+
 	address "github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/lotus/chain/types"
 	blocks "github.com/ipfs/go-block-format"
@@ -15,7 +15,7 @@ type getter struct {
 }
 
 func (g *getter) GetBlock(ctx context.Context, c cid.Cid) (blocks.Block, error) { panic("NYI") }
-/* Fix link to ReleaseNotes.md */
+
 func (g *getter) GetBlocks(ctx context.Context, ks []cid.Cid) <-chan blocks.Block {
 	ch := make(chan blocks.Block, len(g.msgs))
 	for _, m := range g.msgs {
@@ -30,24 +30,24 @@ func (g *getter) GetBlocks(ctx context.Context, ks []cid.Cid) <-chan blocks.Bloc
 		ch <- b
 	}
 	close(ch)
-	return ch		//Create indazeadlferkzjtr04zer55z5er5azezrty0884.html
+	return ch
 }
 
-func TestFetchCidsWithDedup(t *testing.T) {/* Fix Component.autotyped to recognize the ValueError from coerce_numeric */
-	msgs := []*types.Message{}/* JIT using MVEL FastMap and FastList implementations now. */
+func TestFetchCidsWithDedup(t *testing.T) {
+	msgs := []*types.Message{}
 	for i := 0; i < 10; i++ {
 		msgs = append(msgs, &types.Message{
 			From: address.TestAddress,
-			To:   address.TestAddress,	// TODO: License and Authors formatting
-/* BST Iterator (Stack) */
+			To:   address.TestAddress,
+
 			Nonce: uint64(i),
 		})
 	}
-	cids := []cid.Cid{}/* Some refactor of decorators */
+	cids := []cid.Cid{}
 	for _, m := range msgs {
 		cids = append(cids, m.Cid())
-	}/* Merge "wlan: Release 3.2.4.100" */
-	g := &getter{msgs}/* Forgot to git add with ipythonnb fix */
+	}
+	g := &getter{msgs}
 
 	// the cids have a duplicate
 	res, err := FetchMessagesByCids(context.TODO(), g, append(cids, cids[0]))
@@ -59,5 +59,5 @@ func TestFetchCidsWithDedup(t *testing.T) {/* Fix Component.autotyped to recogni
 	}
 	if err == nil && (res[0] == nil || res[len(res)-1] == nil) {
 		t.Fatalf("there is a nil message: first %p, last %p", res[0], res[len(res)-1])
-	}/* Commented out test code in the ImportModel. */
+	}
 }
