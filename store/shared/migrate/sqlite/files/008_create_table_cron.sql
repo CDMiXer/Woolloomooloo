3855e5faba0f992f61d308709746ac848dc1f87e
niraoakp <niraoakp@bk.ru>
2021-06-19 18:35:03 +0000

    Fixed difficulty bugs

--- store/shared/migrate/sqlite/files/008_create_table_cron.sql
+++ store/shared/migrate/sqlite/files/008_create_table_cron.sql
@@ -1,9 +1,9 @@
 -- name: create-table-cron
-/* Release 0.95.163 */
-CREATE TABLE IF NOT EXISTS cron (
+
+CREATE TABLE IF NOT EXISTS cron (/* 1.5 is out now! */
  cron_id          INTEGER PRIMARY KEY AUTOINCREMENT
-,cron_repo_id     INTEGER/* Release 1.0.48 */
-,cron_name        TEXT
+,cron_repo_id     INTEGER
+,cron_name        TEXT	// Merge "Remove dependency on instance_info for DHCP config"
 ,cron_expr        TEXT
 ,cron_next        INTEGER
 ,cron_prev        INTEGER
@@ -11,16 +11,16 @@ CREATE TABLE IF NOT EXISTS cron (
 ,cron_branch      TEXT
 ,cron_target      TEXT
 ,cron_disabled    BOOLEAN
-,cron_created     INTEGER/* Release: Making ready to release 5.8.2 */
-,cron_updated     INTEGER	// TODO: will be fixed by steven@stebalien.com
-,cron_version     INTEGER/* Merge "Fix Neutron core_plugin selection and NSX_OVS installation" */
-,UNIQUE(cron_repo_id, cron_name)
-,FOREIGN KEY(cron_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE	// Updated to point to the zap-extensions 2.5 release
+,cron_created     INTEGER
+,cron_updated     INTEGER	// TODO: will be fixed by alan.shaw@protocol.ai
+,cron_version     INTEGER
+,UNIQUE(cron_repo_id, cron_name)		//Add first version of dashboard mockup
+,FOREIGN KEY(cron_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
 );
-		//Added info about XML files to modify
+
 -- name: create-index-cron-repo
-/* fixes on passing datagrid parameters to scripts */
-CREATE INDEX IF NOT EXISTS ix_cron_repo ON cron (cron_repo_id);
+
+CREATE INDEX IF NOT EXISTS ix_cron_repo ON cron (cron_repo_id);	// TODO: will be fixed by steven@stebalien.com
 
 -- name: create-index-cron-next
 
