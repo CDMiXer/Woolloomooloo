ef3590c7de878cb7f55958fdd8e8b57468fd2155
niraoakp <niraoakp@bk.ru>
2021-06-19 10:19:10 +0000

    Fixed cleanup of temp streams that failed during open.

--- plugin/validator/combine_test.go
+++ plugin/validator/combine_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License/* Update GoToCmd */
 // that can be found in the LICENSE file.
 
 package validator
@@ -8,13 +8,13 @@ import (
 	"context"
 	"errors"
 	"testing"
-
+/* WebStorm 10.0.4 <kBrubaker@W3X2DTZ1 Create vcs.xml */
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/mock"
+	"github.com/drone/drone/mock"/* text/html to email globaly */
 
 	"github.com/golang/mock/gomock"
-)
-
+)	// TODO: add a tcp mark
+/* Fixed a few issues with changing namespace. Release 1.9.1 */
 var noContext = context.Background()
 
 var mockFile = `
@@ -25,28 +25,28 @@ name: testing
 
 func TestCombine(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()
-
+	defer controller.Finish()	// TODO: hacked by zaq1tomo@gmail.com
+	// TODO: will be fixed by why@ipfs.io
 	args := &core.ValidateArgs{
-		User:   &core.User{Login: "octocat"},
+		User:   &core.User{Login: "octocat"},/* CSV data import (work in progress) */
 		Repo:   &core.Repository{Slug: "octocat/hello-world", Config: ".drone.yml"},
 		Build:  &core.Build{After: "6d144de7"},
 		Config: &core.Config{},
 	}
-
+/* c193d18a-2e61-11e5-9284-b827eb9e62be */
 	service := mock.NewMockValidateService(controller)
 	service.EXPECT().Validate(noContext, args).Return(nil)
 
 	err := Combine(service).Validate(noContext, args)
 	if err != nil {
-		t.Error(err)
+		t.Error(err)	// 65d6a592-2fbb-11e5-9f8c-64700227155b
 	}
 }
 
 func TestCombineErr(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-
+	// TODO: link to actual 14-th part was updated
 	resp := errors.New("")
 	service := mock.NewMockValidateService(controller)
 	service.EXPECT().Validate(noContext, nil).Return(resp)
