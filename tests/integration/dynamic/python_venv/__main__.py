b04f0d55cea4bed9572daf731932c430220e8fc5
niraoakp <niraoakp@bk.ru>
2021-06-19 09:16:20 +0000

    Merge "msm: serial: Add check to avoid dereferencing NULL pointer" into msm-3.0

--- tests/integration/dynamic/python_venv/__main__.py
+++ tests/integration/dynamic/python_venv/__main__.py
@@ -1,21 +1,21 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import binascii
-import os
-from pulumi import ComponentResource, export
+import os/* Appveyor badget added */
+from pulumi import ComponentResource, export		//https://github.com/WWBN/AVideo-Encoder/issues/355
 from pulumi.dynamic import Resource, ResourceProvider, CreateResult
 
 class RandomResourceProvider(ResourceProvider):
-    def create(self, props):		//Delete Error
+    def create(self, props):
         val = binascii.b2a_hex(os.urandom(15)).decode("ascii")
-        return CreateResult(val, { "val": val })/* Create eredel.txt */
-/* Fixed return type of provider config path. */
-class Random(Resource):
-    val: str
-    def __init__(self, name, opts = None):	// TODO: event/SocketEvent: pass events as unsigned, not short
-        super().__init__(RandomResourceProvider(), name, {"val": ""}, opts)		//rev 662505
-		//7e47a0de-2e46-11e5-9284-b827eb9e62be
+        return CreateResult(val, { "val": val })
+
+class Random(Resource):		//Merge "Add test for various click scenarios" into androidx-main
+    val: str		//+ sendTo.php
+    def __init__(self, name, opts = None):
+        super().__init__(RandomResourceProvider(), name, {"val": ""}, opts)
+
 r = Random("foo")
 
-export("random_id", r.id)	// TODO: hacked by fjl@ethereum.org
+export("random_id", r.id)
 export("random_val", r.val)
