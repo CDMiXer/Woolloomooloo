dfab25eeeecd31a660eabb868a3920c8d4b36317
niraoakp <niraoakp@bk.ru>
2021-06-19 19:43:12 +0000

    implemented simple read line function

--- pkg/codegen/internal/test/testdata/random-pet.pp.go
+++ pkg/codegen/internal/test/testdata/random-pet.pp.go
@@ -1,17 +1,17 @@
-package main/* Delete \Hardware */
+package main
 
 import (
 	"github.com/pulumi/pulumi-random/sdk/v2/go/random"
-	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"/* 485b0a06-2e64-11e5-9284-b827eb9e62be */
+	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
-		//Merge branch 'master' into design-work
-func main() {		//Merge "Fixed upload to remotes with the url ssh://hostname"
+
+func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		_, err := random.NewRandomPet(ctx, "random_pet", &random.RandomPetArgs{
 			Prefix: pulumi.String("doggo"),
 		})
 		if err != nil {
-			return err		//Small plate crafting fix
+			return err
 		}
 		return nil
 	})
