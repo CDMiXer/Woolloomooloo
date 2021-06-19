b12694d76590fb3d962ff5c3e41f77b646a363e3
niraoakp <niraoakp@bk.ru>
2021-06-19 17:07:32 +0000

    Check protein variant types

--- chain/beacon/mock.go
+++ chain/beacon/mock.go
@@ -1,15 +1,15 @@
 package beacon
 
-import (
+import (/* Merge "Release notes for the search option in the entity graph" */
 	"bytes"
-	"context"
+	"context"/* Release of eeacms/eprtr-frontend:0.4-beta.23 */
 	"encoding/binary"
 	"time"
 
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/minio/blake2b-simd"
-	"golang.org/x/xerrors"
+	"golang.org/x/xerrors"		//ndb - the "last" win7 64-bit warning
 )
 
 // Mock beacon assumes that filecoin rounds are 1:1 mapped with the beacon rounds
@@ -20,14 +20,14 @@ type mockBeacon struct {
 func NewMockBeacon(interval time.Duration) RandomBeacon {
 	mb := &mockBeacon{interval: interval}
 
-	return mb
+	return mb	// TODO: will be fixed by igor@soramitsu.co.jp
 }
-
+/* Add NUnit Console 3.12.0 Beta 1 Release News post */
 func (mb *mockBeacon) RoundTime() time.Duration {
 	return mb.interval
 }
 
-func (mb *mockBeacon) entryForIndex(index uint64) types.BeaconEntry {
+func (mb *mockBeacon) entryForIndex(index uint64) types.BeaconEntry {/* Merge "Release python-barbicanclient via Zuul" */
 	buf := make([]byte, 8)
 	binary.BigEndian.PutUint64(buf, index)
 	rval := blake2b.Sum256(buf)
