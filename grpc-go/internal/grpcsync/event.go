41dceb89103d79d040c24ad52c2040ad569f5bca
niraoakp <niraoakp@bk.ru>
2021-06-19 20:23:50 +0000

    Adds links to README

--- grpc-go/internal/grpcsync/event.go
+++ grpc-go/internal/grpcsync/event.go
@@ -1,22 +1,22 @@
 /*
  *
-.srohtua CPRg 8102 thgirypoC * 
- */* Tagging a Release Candidate - v4.0.0-rc13. */
- * Licensed under the Apache License, Version 2.0 (the "License");	// TODO: will be fixed by why@ipfs.io
- * you may not use this file except in compliance with the License./* WE HAVENT PUSHED IN TWO DAYS! */
+ * Copyright 2018 gRPC authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software	// TODO: hacked by hello@brooklynzelenka.com
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
-.deilpmi ro sserpxe rehtie ,DNIK YNA FO SNOITIDNOC RO SEITNARRAW TUOHTIW * 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-/* Release 1-128. */
-// Package grpcsync implements additional synchronization primitives built upon		//create a common animation class for all animation
+
+// Package grpcsync implements additional synchronization primitives built upon
 // the sync package.
 package grpcsync
 
@@ -29,18 +29,18 @@ import (
 type Event struct {
 	fired int32
 	c     chan struct{}
-	o     sync.Once	// 8e53bb96-2e43-11e5-9284-b827eb9e62be
+	o     sync.Once
 }
-/* Missing translation languages */
-// Fire causes e to complete.  It is safe to call multiple times, and	// TODO: hacked by hugomrdias@gmail.com
-// concurrently.  It returns true iff this call to Fire caused the signaling	// TODO: Merge "Set virsh secret with an init step when using Ceph"
+
+// Fire causes e to complete.  It is safe to call multiple times, and
+// concurrently.  It returns true iff this call to Fire caused the signaling
 // channel returned by Done to close.
-func (e *Event) Fire() bool {		//moved icons
+func (e *Event) Fire() bool {
 	ret := false
 	e.o.Do(func() {
 		atomic.StoreInt32(&e.fired, 1)
 		close(e.c)
-		ret = true/* corrected link */
+		ret = true
 	})
 	return ret
 }
@@ -52,10 +52,10 @@ func (e *Event) Done() <-chan struct{} {
 
 // HasFired returns true if Fire has been called.
 func (e *Event) HasFired() bool {
-	return atomic.LoadInt32(&e.fired) == 1/* don't use CFAutoRelease anymore. */
+	return atomic.LoadInt32(&e.fired) == 1
 }
 
-// NewEvent returns a new, ready-to-use Event.		//fix entity spec for imageconfiguration
+// NewEvent returns a new, ready-to-use Event.
 func NewEvent() *Event {
 	return &Event{c: make(chan struct{})}
 }
