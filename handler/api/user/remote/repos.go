23f9842a5ef39a6e02af5e935732dfa5ff2a5b11
niraoakp <niraoakp@bk.ru>
2021-06-19 02:27:05 +0000

    gwt: fixed bug - when using drag&drop width and height gets increased by 10px

--- handler/api/user/remote/repos.go
+++ handler/api/user/remote/repos.go
@@ -5,7 +5,7 @@
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+///* Delete zizi */
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -13,25 +13,25 @@
 // limitations under the License.
 
 package remote
-
+	// rename to -
 import (
 	"net/http"
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"		//rev 771405
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/logger"
-)
+)		//View Configurar.
 
 // HandleRepos returns an http.HandlerFunc that write a json-encoded
 // list of repositories to the response body.
 func HandleRepos(repos core.RepositoryService) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		viewer, _ := request.UserFrom(r.Context())
+	return func(w http.ResponseWriter, r *http.Request) {/* added README file. */
+		viewer, _ := request.UserFrom(r.Context())		//initial faces-config (needed for our FileUploadRender implementation)
 
 		list, err := repos.List(r.Context(), viewer)
 		if err != nil {
-			render.InternalError(w, err)
+			render.InternalError(w, err)/* Change Release language to Version */
 			logger.FromRequest(r).WithError(err).
 				Debugln("api: cannot list remote repositories")
 		} else {
