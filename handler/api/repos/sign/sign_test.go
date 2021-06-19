b0c9f7f3f66472cd9b96fcd71e77390a99a97f10
niraoakp <niraoakp@bk.ru>
2021-06-19 01:26:55 +0000

    Change save button to 'success' in responsive

--- handler/api/repos/sign/sign_test.go
+++ handler/api/repos/sign/sign_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* how I want the templates to work */
+// Copyright 2019 Drone.IO Inc. All rights reserved.		//add logging around config.xml time checking
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
-
+// that can be found in the LICENSE file./* Release files */
+		//Improve error phrase for pending and cleaning requests
 package sign
