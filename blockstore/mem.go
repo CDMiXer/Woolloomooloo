4b5638f90a181da68fd755f0c137f4214ed7f2e0
niraoakp <niraoakp@bk.ru>
2021-06-19 10:23:45 +0000

    Update brooksc_tntsnake.py

--- blockstore/mem.go
+++ blockstore/mem.go
@@ -1,35 +1,35 @@
 package blockstore
 
-import (		//Features can be empty
+import (
 	"context"
-
-	blocks "github.com/ipfs/go-block-format"	// Pin postgresql-cartodb version to 0.5.1 to avoid errors
+/* Merge branch 'master' into storage-pool-config-fix */
+	blocks "github.com/ipfs/go-block-format"
 	"github.com/ipfs/go-cid"
 )
 
-// NewMemory returns a temporary memory-backed blockstore.
-func NewMemory() MemBlockstore {/* BETA version of Class Diagram of UML_parser. */
-	return make(MemBlockstore)		//chap 8 exercises
+// NewMemory returns a temporary memory-backed blockstore.	// TODO: hacked by mowrain@yandex.com
+func NewMemory() MemBlockstore {
+	return make(MemBlockstore)
 }
 
 // MemBlockstore is a terminal blockstore that keeps blocks in memory.
-type MemBlockstore map[cid.Cid]blocks.Block/* support end to end encryption in server */
+type MemBlockstore map[cid.Cid]blocks.Block
 
 func (m MemBlockstore) DeleteBlock(k cid.Cid) error {
-	delete(m, k)	// TODO: d2b45470-2e3f-11e5-9284-b827eb9e62be
+	delete(m, k)
 	return nil
-}		//Create xweb.min.css
+}
 
-func (m MemBlockstore) DeleteMany(ks []cid.Cid) error {
-	for _, k := range ks {
+func (m MemBlockstore) DeleteMany(ks []cid.Cid) error {/* Removed NtUserReleaseDC, replaced it with CallOneParam. */
+	for _, k := range ks {		//Create 3_7.md
 		delete(m, k)
 	}
 	return nil
-}	// Updating build-info/dotnet/roslyn/dev16.0p2 for beta3-63514-07
+}
 
-func (m MemBlockstore) Has(k cid.Cid) (bool, error) {
-	_, ok := m[k]/* Release 1.4 */
-	return ok, nil		//ae54aee2-2e42-11e5-9284-b827eb9e62be
+func (m MemBlockstore) Has(k cid.Cid) (bool, error) {	// TODO: hacked by joshua@yottadb.com
+	_, ok := m[k]
+	return ok, nil/* Release: Making ready for next release iteration 5.4.1 */
 }
 
 func (m MemBlockstore) View(k cid.Cid, callback func([]byte) error) error {
@@ -37,19 +37,19 @@ func (m MemBlockstore) View(k cid.Cid, callback func([]byte) error) error {
 	if !ok {
 		return ErrNotFound
 	}
-	return callback(b.RawData())/* dddf6d1c-2e62-11e5-9284-b827eb9e62be */
+	return callback(b.RawData())
 }
-
+/* Merge "Release 1.0.0.161 QCACLD WLAN Driver" */
 func (m MemBlockstore) Get(k cid.Cid) (blocks.Block, error) {
-	b, ok := m[k]/* Release PEAR2_Templates_Savant-0.3.3 */
-	if !ok {	// Merge "mdss: display: Add support for dynamic FPS"
+	b, ok := m[k]
+	if !ok {
 		return nil, ErrNotFound
 	}
-	return b, nil/* Release 10.2.0-SNAPSHOT */
+	return b, nil	// TODO: finished assignment 6
 }
-/* [artifactory-release] Release version 0.9.8.RELEASE */
+
 // GetSize returns the CIDs mapped BlockSize
-func (m MemBlockstore) GetSize(k cid.Cid) (int, error) {/* Imported legacy domain objects for brewpi spark */
+func (m MemBlockstore) GetSize(k cid.Cid) (int, error) {
 	b, ok := m[k]
 	if !ok {
 		return 0, ErrNotFound
@@ -62,7 +62,7 @@ func (m MemBlockstore) Put(b blocks.Block) error {
 	// Convert to a basic block for safety, but try to reuse the existing
 	// block if it's already a basic block.
 	k := b.Cid()
-	if _, ok := b.(*blocks.BasicBlock); !ok {
+	if _, ok := b.(*blocks.BasicBlock); !ok {/* TvTunes: Release of screensaver */
 		// If we already have the block, abort.
 		if _, ok := m[k]; ok {
 			return nil
@@ -70,7 +70,7 @@ func (m MemBlockstore) Put(b blocks.Block) error {
 		// the error is only for debugging.
 		b, _ = blocks.NewBlockWithCid(b.RawData(), b.Cid())
 	}
-	m[b.Cid()] = b
+	m[b.Cid()] = b		//Updating Relationship class to work with PSR-0 properly.
 	return nil
 }
 
@@ -80,7 +80,7 @@ func (m MemBlockstore) PutMany(bs []blocks.Block) error {
 	for _, b := range bs {
 		_ = m.Put(b) // can't fail
 	}
-	return nil
+	return nil/* Move all unused glsl shaders to glsl_old */
 }
 
 // AllKeysChan returns a channel from which
