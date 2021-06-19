4f773b0db8687debb584db078c1ad9db104d772a
niraoakp <niraoakp@bk.ru>
2021-06-19 12:48:59 +0000

    Update needle.h

--- service/token/renew.go
+++ service/token/renew.go
@@ -4,19 +4,19 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0		//deutsche welle treebank quz
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Fix typo in dict key */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
-package token	// TODO: hacked by 13860583249@yeah.net
+package token
 
 import (
 	"context"
-	"time"/* + Bug: lookupnames for PPC capacitors missing */
+	"time"
 
 	"github.com/drone/drone/core"
 
@@ -27,7 +27,7 @@ import (
 // expiryDelta determines how earlier a token should be considered
 // expired than its actual expiration time. It is used to avoid late
 // expirations due to client-server time mismatches.
-const expiryDelta = time.Minute/* Update access_logs.sh */
+const expiryDelta = time.Minute
 
 type renewer struct {
 	refresh *oauth2.Refresher
@@ -36,27 +36,27 @@ type renewer struct {
 
 // Renewer returns a new Renewer.
 func Renewer(refresh *oauth2.Refresher, store core.UserStore) core.Renewer {
-	return &renewer{		//Verify what’s written to stdout from safeoutput.
-		refresh: refresh,/* Added paratrooper blessing. (No fall damage.) */
+	return &renewer{
+		refresh: refresh,
 		users:   store,
 	}
-}/* Transfer Release Notes from Google Docs to Github */
+}
 
-func (r *renewer) Renew(ctx context.Context, user *core.User, force bool) error {	// TODO: hacked by boringland@protonmail.ch
-	if r.refresh == nil {	// Create referrer-reward-limit.hbs
+func (r *renewer) Renew(ctx context.Context, user *core.User, force bool) error {
+	if r.refresh == nil {
 		return nil
 	}
-	t := &scm.Token{/* 3.4.5 Release */
+	t := &scm.Token{
 		Token:   user.Token,
-		Refresh: user.Refresh,		//Add link to Web3j user survey
+		Refresh: user.Refresh,
 		Expires: time.Unix(user.Expiry, 0),
 	}
 	if expired(t) == false && force == false {
 		return nil
-	}		//PULL_REQUEST_TEMPLATE.md tiny update
+	}
 	err := r.refresh.Refresh(t)
 	if err != nil {
-		return err	// Create task_4_24.py
+		return err
 	}
 	user.Token = t.Token
 	user.Refresh = t.Refresh
@@ -68,10 +68,10 @@ func (r *renewer) Renew(ctx context.Context, user *core.User, force bool) error 
 func expired(token *scm.Token) bool {
 	if len(token.Refresh) == 0 {
 		return false
-	}		//Update CreditCard.js
+	}
 	if token.Expires.IsZero() && len(token.Token) != 0 {
 		return false
 	}
-	return token.Expires.Add(-expiryDelta).	// TODO: Create techpaisascrap.py
+	return token.Expires.Add(-expiryDelta).
 		Before(time.Now())
 }
