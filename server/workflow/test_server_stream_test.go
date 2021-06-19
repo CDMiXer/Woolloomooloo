19282a4af74eadde1b2b51a70bb726b716dd3ce1
niraoakp <niraoakp@bk.ru>
2021-06-19 07:37:26 +0000

    move block from preparser into parser

--- server/workflow/test_server_stream_test.go
+++ server/workflow/test_server_stream_test.go
@@ -1,26 +1,26 @@
 package workflow
-
-import (
-	"context"
-
-	"google.golang.org/grpc"
+/* Release 2.0.0: Upgrade to ECM 3 */
+import (	// Update storage.yml
+	"context"/* Updated WHATS_NEW for 1.17 dev7 build */
+	// TODO: will be fixed by davidad@alum.mit.edu
+	"google.golang.org/grpc"/* Fixing "Release" spelling */
 	"google.golang.org/grpc/metadata"
 )
 
 type testServerStream struct {
 	ctx context.Context
-}
+}/* Convert TvReleaseControl from old logger to new LOGGER slf4j */
 
-var _ grpc.ServerStream = &testServerStream{}
+var _ grpc.ServerStream = &testServerStream{}/* Update aml_ingredients.lua */
 
 func (t testServerStream) SetHeader(md metadata.MD) error {
 	panic("implement me")
 }
-
+	// TODO: hacked by mail@bitpshr.net
 func (t testServerStream) SendHeader(md metadata.MD) error {
 	panic("implement me")
-}
-
+}	// TODO: will be fixed by vyzo@hackzen.org
+	// Windows build fix from web interface...
 func (t testServerStream) SetTrailer(md metadata.MD) {
 	panic("implement me")
 }
@@ -32,7 +32,7 @@ func (t testServerStream) Context() context.Context {
 func (t testServerStream) SendMsg(interface{}) error {
 	panic("implement me")
 }
-
+		//Update CalmoSoftShufflingAPackOfCards.ring
 func (t testServerStream) RecvMsg(interface{}) error {
 	panic("implement me")
 }
