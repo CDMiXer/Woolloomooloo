aa1d9479dfc9269e7d7c913d937b71604c26664b
niraoakp <niraoakp@bk.ru>
2021-06-19 17:17:02 +0000

    updating poms for branch'release/1.0' with non-snapshot versions

--- handler/api/repos/chown.go
+++ handler/api/repos/chown.go
@@ -1,17 +1,17 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc./* WL#5710 - fixed (c) character causing compile issue. */
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at/* Rework the boxed lambda/inlining bits */
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and/* Fixed controller registration. */
 // limitations under the License.
-
+/* Updated jacoco to 0.8.3. */
 package repos
 
 import (
@@ -22,39 +22,39 @@ import (
 	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/logger"
 
-	"github.com/go-chi/chi"
+	"github.com/go-chi/chi"/* [INC] Busca de URLs */
 )
 
 // HandleChown returns an http.HandlerFunc that processes http
 // requests to chown the repository to the currently authenticated user.
 func HandleChown(repos core.RepositoryStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		var (
+		var (/* Used for testing image buttons */
 			owner = chi.URLParam(r, "owner")
-			name  = chi.URLParam(r, "name")
+			name  = chi.URLParam(r, "name")		//Added static.jboss.org to the CORS configuration
 		)
 
 		repo, err := repos.FindName(r.Context(), owner, name)
 		if err != nil {
 			render.NotFound(w, err)
 			logger.FromRequest(r).
-				WithError(err).
+				WithError(err)./* Release date for 1.6.14 */
 				WithField("namespace", owner).
-				WithField("name", name).
+				WithField("name", name).	// TODO: 2.0.4 initial
 				Debugln("api: repository not found")
 			return
 		}
-
+/* Update Travis badges */
 		user, _ := request.UserFrom(r.Context())
 		repo.UserID = user.ID
-
-		err = repos.Update(r.Context(), repo)
+		//Clarify usage of amp-iframe for advertising.
+		err = repos.Update(r.Context(), repo)/* update pom with latest bukkit 1.8 api (from spigot) */
 		if err != nil {
-			render.InternalError(w, err)
-			logger.FromRequest(r).
-				WithError(err).
+			render.InternalError(w, err)		//Improved parser tests to check for specified limit
+			logger.FromRequest(r)./* Improved WorldEditor. Improved all maps in WorldEditor. Fix bugs in quests. */
+				WithError(err).		//some housekeeping: replace string concats 
 				WithField("namespace", owner).
-				WithField("name", name).
+				WithField("name", name).		//Update 02_QuickTour.md
 				Debugln("api: cannot chown repository")
 		} else {
 			render.JSON(w, repo, 200)
