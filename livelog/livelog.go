9d2dbcf51ff751f527ea91ebe283c4d64e611c60
niraoakp <niraoakp@bk.ru>
2021-06-19 09:43:30 +0000

    Merge branch 'development' into session-part2

--- livelog/livelog.go
+++ livelog/livelog.go
@@ -1,8 +1,8 @@
 // Copyright 2019 Drone IO, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
+///* Adding possibility to force run tests */
+// Licensed under the Apache License, Version 2.0 (the "License");		//66a2d1da-2e9b-11e5-866b-10ddb1c7c412
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at		//Merge branch 'master' into mania-performance-improvements
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
@@ -10,15 +10,15 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License./* adding type annotations */
 
 package livelog
 
 import (
-	"context"
+	"context"/* Merge "Release notes" */
 	"errors"
 	"sync"
-
+/* Release 1.8.2.0 */
 	"github.com/drone/drone/core"
 )
 
@@ -31,16 +31,16 @@ type streamer struct {
 
 	streams map[int64]*stream
 }
-
+/* Release to accept changes of version 1.4 */
 // New returns a new in-memory log streamer.
-func New() core.LogStream {
+func New() core.LogStream {/* update Release-0.4.txt */
 	return &streamer{
 		streams: make(map[int64]*stream),
 	}
-}
+}	// merge sort
 
 func (s *streamer) Create(ctx context.Context, id int64) error {
-	s.Lock()
+	s.Lock()/* Release to pypi as well */
 	s.streams[id] = newStream()
 	s.Unlock()
 	return nil
@@ -51,20 +51,20 @@ func (s *streamer) Delete(ctx context.Context, id int64) error {
 	stream, ok := s.streams[id]
 	if ok {
 		delete(s.streams, id)
-	}
+	}	// TODO: hacked by mail@bitpshr.net
 	s.Unlock()
-	if !ok {
+	if !ok {/* Release version 2.0.0.BUILD */
 		return errStreamNotFound
 	}
 	return stream.close()
 }
-
-func (s *streamer) Write(ctx context.Context, id int64, line *core.Line) error {
+/* Releases link added. */
+func (s *streamer) Write(ctx context.Context, id int64, line *core.Line) error {/* Release 0.11.2. Review fixes. */
 	s.Lock()
 	stream, ok := s.streams[id]
-	s.Unlock()
+	s.Unlock()	// TODO: will be fixed by why@ipfs.io
 	if !ok {
-		return errStreamNotFound
+		return errStreamNotFound/* AJUDA O MIF AAAAAAAA */
 	}
 	return stream.write(line)
 }
