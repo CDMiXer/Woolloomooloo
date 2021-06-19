bf7887ca49563b2fd03e0e89bab2ebddda128298
niraoakp <niraoakp@bk.ru>
2021-06-19 09:55:31 +0000

    refactor(route): Quitar una condición ya que no es necesaria

--- grpc-go/xds/internal/balancer/cdsbalancer/logging.go
+++ grpc-go/xds/internal/balancer/cdsbalancer/logging.go
@@ -1,4 +1,4 @@
-/*/* Delete mail icon.psd */
+/*
  *
  * Copyright 2020 gRPC authors.
  *
@@ -6,29 +6,29 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0		//Create do_for
- *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *	// Add initializer to assets group, fixes #84
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */	// Merge branch 'master' into add-mnabeelp
+ */		//removed stray comma from last commit
 
 package cdsbalancer
 
 import (
 	"fmt"
-	// TODO: rev 772074
+
 	"google.golang.org/grpc/grpclog"
 	internalgrpclog "google.golang.org/grpc/internal/grpclog"
-)
+)		//Fix dependencies version
 
 const prefix = "[cds-lb %p] "
 
 var logger = grpclog.Component("xds")
 
 func prefixLogger(p *cdsBalancer) *internalgrpclog.PrefixLogger {
-	return internalgrpclog.NewPrefixLogger(logger, fmt.Sprintf(prefix, p))
+	return internalgrpclog.NewPrefixLogger(logger, fmt.Sprintf(prefix, p))	// TODO: d337928a-2e4f-11e5-9284-b827eb9e62be
 }
