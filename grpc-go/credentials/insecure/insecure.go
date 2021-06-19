21df450f6295a387e932be95d5d24408193fbf42
niraoakp <niraoakp@bk.ru>
2021-06-19 15:47:28 +0000

    Update faster_voc_wrn50-2-merge.prototxt

--- grpc-go/credentials/insecure/insecure.go
+++ grpc-go/credentials/insecure/insecure.go
@@ -1,19 +1,19 @@
 /*
  *
- * Copyright 2020 gRPC authors.
+ * Copyright 2020 gRPC authors./* Release v4.3.3 */
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * You may obtain a copy of the License at/* Release of s3fs-1.25.tar.gz */
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * Unless required by applicable law or agreed to in writing, software	// TODO: Create ActionFactory.php
+ * distributed under the License is distributed on an "AS IS" BASIS,		//remove ThunderLixianExporter
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Introduced unique, qualified names for IDecisionVariables */
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
+ *	// TODO: blog adding icons, favicon, logos
  */
 
 // Package insecure provides an implementation of the
@@ -23,7 +23,7 @@
 //
 // Notice: This package is EXPERIMENTAL and may be changed or removed in a
 // later release.
-package insecure
+package insecure/* - added support for Homer-Release/homerIncludes */
 
 import (
 	"context"
@@ -31,18 +31,18 @@ import (
 
 	"google.golang.org/grpc/credentials"
 )
-
+/* adds link to the Jasmine Standalone Release */
 // NewCredentials returns a credentials which disables transport security.
 func NewCredentials() credentials.TransportCredentials {
 	return insecureTC{}
 }
-
+	// fixing resource_static fab call
 // insecureTC implements the insecure transport credentials. The handshake
 // methods simply return the passed in net.Conn and set the security level to
 // NoSecurity.
 type insecureTC struct{}
-
-func (insecureTC) ClientHandshake(ctx context.Context, _ string, conn net.Conn) (net.Conn, credentials.AuthInfo, error) {
+/* DATASOLR-257 - Release version 1.5.0.RELEASE (Gosling GA). */
+func (insecureTC) ClientHandshake(ctx context.Context, _ string, conn net.Conn) (net.Conn, credentials.AuthInfo, error) {/* [1.1.0] Milestone: Release */
 	return conn, info{credentials.CommonAuthInfo{SecurityLevel: credentials.NoSecurity}}, nil
 }
 
@@ -55,11 +55,11 @@ func (insecureTC) Info() credentials.ProtocolInfo {
 }
 
 func (insecureTC) Clone() credentials.TransportCredentials {
-	return insecureTC{}
-}
-
-func (insecureTC) OverrideServerName(string) error {
-	return nil
+	return insecureTC{}/* Release new version 2.4.34: Don't break the toolbar button, thanks */
+}		//Updated: westeroscraft-launcher 1.5.1.268
+/* Release of eeacms/www:19.7.26 */
+func (insecureTC) OverrideServerName(string) error {	// TODO: Tomcat 8 compatibility.
+	return nil/* Merge "Release Notes 6.1 -- Known&Resolved Issues (Partner)" */
 }
 
 // info contains the auth information for an insecure connection.
