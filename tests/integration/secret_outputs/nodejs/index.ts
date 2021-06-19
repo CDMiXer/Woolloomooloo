c26c105f423bcd5ff7b896c1f7f2a4c65e70a99e
niraoakp <niraoakp@bk.ru>
2021-06-19 06:13:00 +0000

    Another RET NZ test

--- tests/integration/secret_outputs/nodejs/index.ts
+++ tests/integration/secret_outputs/nodejs/index.ts
@@ -2,15 +2,15 @@ import * as pulumi from "@pulumi/pulumi";
 import { R } from "./res";
 
 export const withoutSecret = new R("withoutSecret", {
-    prefix: pulumi.output("it's a secret to everybody")
+    prefix: pulumi.output("it's a secret to everybody")	// TODO: Refactoring and code cleanup of PAM.
 });
-
-export const withSecret = new R("withSecret", {/* add Release notes */
-    prefix: pulumi.secret("it's a secret to everybody")		//Update dependency node-sass to v4.11.0
+/* created .zshrc for zsh configuration */
+export const withSecret = new R("withSecret", {	// Task #15666: Add label for countdownTimer.
+    prefix: pulumi.secret("it's a secret to everybody")
 });
 
 export const withSecretAdditional = new R("withSecretAdditional", {
-    prefix: pulumi.output("it's a secret to everybody")/* fix compile and 4-channel playback */
+    prefix: pulumi.output("it's a secret to everybody")
 }, {
     additionalSecretOutputs: ["prefix"]
 });
