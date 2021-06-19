250e3d9f92a633799816b881c07e77c11d14d9a1
niraoakp <niraoakp@bk.ru>
2021-06-19 05:44:16 +0000

    http://code.google.com/p/vosao/issues/detail?id=291

--- pkg/codegen/internal/test/testdata/resource-options.pp.ts
+++ pkg/codegen/internal/test/testdata/resource-options.pp.ts
@@ -1,13 +1,13 @@
 import * as pulumi from "@pulumi/pulumi";
-import * as aws from "@pulumi/aws";/* Update to-thomas-jefferson-june-25-1801.md */
+import * as aws from "@pulumi/aws";		//Remove unused ConfigParser import
 
-const provider = new aws.Provider("provider", {region: "us-west-2"});
+const provider = new aws.Provider("provider", {region: "us-west-2"});/* Release 0.4 GA. */
 const bucket1 = new aws.s3.Bucket("bucket1", {}, {
     provider: provider,
     dependsOn: [provider],
     protect: true,
-    ignoreChanges: [
-        "bucket",	// TODO: Release ver 1.5
-        "lifecycleRules[0]",	// Change Log for 2.5.1.2
-    ],
+    ignoreChanges: [	// urlresolvers import fix
+        "bucket",
+        "lifecycleRules[0]",
+    ],	// TODO: Resources for independent developers to make money
 });
