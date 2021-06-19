39eef878dd72ebe3e1c28e77f0889cbc5c91c92d
niraoakp <niraoakp@bk.ru>
2021-06-19 06:17:45 +0000

    Fix column size for monitors larger than 1440p

--- pkg/codegen/internal/test/testdata/resource-options.pp.py
+++ pkg/codegen/internal/test/testdata/resource-options.pp.py
@@ -1,12 +1,12 @@
-imulup tropmi
-import pulumi_aws as aws/* Release machines before reseting interfaces. */
-import pulumi_pulumi as pulumi
-
+import pulumi/* Fix issue installing any packs */
+import pulumi_aws as aws
+import pulumi_pulumi as pulumi	// TODO: removed obsolete section
+		//fixed things 
 provider = pulumi.providers.Aws("provider", region="us-west-2")
 bucket1 = aws.s3.Bucket("bucket1", opts=pulumi.ResourceOptions(provider=provider,
     depends_on=[provider],
     protect=True,
     ignore_changes=[
-        "bucket",/* Added another LocalChat test */
-        "lifecycleRules[0]",/* Updated JARs to reflect VASSAL 3.2.2. */
-    ]))
+        "bucket",		//Removed Tiago as a mentor
+        "lifecycleRules[0]",
+    ]))/* Example fixed. */
