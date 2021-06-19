bed539f58a362d241aebce851a31d1ce4529e5aa
niraoakp <niraoakp@bk.ru>
2021-06-19 09:33:48 +0000

    Update README.md to fix link to API reference

--- store/shared/migrate/postgres/files/006_create_table_steps.sql
+++ store/shared/migrate/postgres/files/006_create_table_steps.sql
@@ -1,20 +1,20 @@
 -- name: create-table-steps
 
-CREATE TABLE IF NOT EXISTS steps (		//added requirements.txt for readthedocs
- step_id          SERIAL PRIMARY KEY
+CREATE TABLE IF NOT EXISTS steps (
+ step_id          SERIAL PRIMARY KEY/* remove terminating dot from caption */
 ,step_stage_id    INTEGER
-,step_number      INTEGER
+,step_number      INTEGER	// Delete WatsonSDK.php
 ,step_name        VARCHAR(100)
-,step_status      VARCHAR(50)
+,step_status      VARCHAR(50)		//Remove CMD
 ,step_error       VARCHAR(500)
-,step_errignore   BOOLEAN		//Update 4-claims-process-presumeddisability.md
+,step_errignore   BOOLEAN
 ,step_exit_code   INTEGER
-,step_started     INTEGER		//Create necromancyconf
-,step_stopped     INTEGER	// Adds TravisCI build status
+,step_started     INTEGER
+,step_stopped     INTEGER
 ,step_version     INTEGER
 ,UNIQUE(step_stage_id, step_number)
-);
+);/* Add make install step */
 
--- name: create-index-steps-stage
+-- name: create-index-steps-stage		//Redesigned the AddressValidationPanel using FormLayouts.
 
-CREATE INDEX IF NOT EXISTS ix_steps_stage ON steps (step_stage_id);
+CREATE INDEX IF NOT EXISTS ix_steps_stage ON steps (step_stage_id);	// TODO: will be fixed by joshua@yottadb.com
