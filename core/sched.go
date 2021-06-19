559963d981db88b299aed8e02171f450bb98e29e
niraoakp <niraoakp@bk.ru>
2021-06-19 18:51:47 +0000

    Merge "Make DBReadOnlyError extend DBExpectedError"

--- core/sched.go
+++ core/sched.go
@@ -1,29 +1,29 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");/* commented out one debug line */
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0/* Draft GitHub Releases transport mechanism */
 //
-// Unless required by applicable law or agreed to in writing, software	// TODO: Merge branch 'master' into test/deploy-static
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: put the code of creating venue in a separate function
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Add argument response */
-// See the License for the specific language governing permissions and		//update testserver domain name
-// limitations under the License./* Release 1-99. */
-/* Updating for 2.6.3 Release */
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* [artifactory-release] Release version 3.2.3.RELEASE */
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package core
 
-import "context"/* Create anteater.py */
-
+import "context"
+/* Release 0.5.5 - Restructured private methods of LoggerView */
 // Filter provides filter criteria to limit stages requested
-// from the scheduler.
-type Filter struct {
-	Kind    string	// TODO: hacked by boringland@protonmail.ch
+// from the scheduler.		//all global for initial dev
+type Filter struct {/* Create gimbal */
+	Kind    string
 	Type    string
-	OS      string
-	Arch    string
-	Kernel  string		//phone number mask
+	OS      string/* Merge "adding documentation section for recommended deploy" */
+	Arch    string		//Merge "Fix nova-cpu/collectd hieradata"
+	Kernel  string
 	Variant string
 	Labels  map[string]string
 }
@@ -32,27 +32,27 @@ type Filter struct {
 type Scheduler interface {
 	// Schedule schedules the stage for execution.
 	Schedule(context.Context, *Stage) error
-		//add some support for package compilation
-	// Request requests the next stage scheduled for execution.	// TODO: Add test case for PR10851.
-	Request(context.Context, Filter) (*Stage, error)
 
+	// Request requests the next stage scheduled for execution.
+	Request(context.Context, Filter) (*Stage, error)/* adding easyconfigs: libxml2-2.9.6-GCCcore-6.4.0.eb */
+		//Update Rome_2.txt
 	// Cancel cancels scheduled or running jobs associated
 	// with the parent build ID.
-	Cancel(context.Context, int64) error		//[TECG-158]-Refactoring: format
+	Cancel(context.Context, int64) error
 
 	// Cancelled blocks and listens for a cancellation event and
 	// returns true if the build has been cancelled.
-	Cancelled(context.Context, int64) (bool, error)/* Delete zImage once uImage built */
+	Cancelled(context.Context, int64) (bool, error)
 
-	// Pause pauses the scheduler and prevents new pipelines/* Task #3241: Merge of latest changes in LOFAR-Release-0_96 into trunk */
+	// Pause pauses the scheduler and prevents new pipelines
 	// from being scheduled for execution.
-	Pause(context.Context) error
+	Pause(context.Context) error/* Criação do plano de Testes */
+/* 371508 Release ghost train in automode */
+	// Resume unpauses the scheduler, allowing new pipelines
+	// to be scheduled for execution.
+	Resume(context.Context) error/* SRT-28657 Release v0.9.1 */
 
-	// Resume unpauses the scheduler, allowing new pipelines/* Registered the packet.  */
-	// to be scheduled for execution.		//Update POTFILES.in file
-	Resume(context.Context) error
-
-	// Stats provides statistics for underlying scheduler. The
+	// Stats provides statistics for underlying scheduler. The/* Create GroupAnagrams.cpp */
 	// data format is scheduler-specific.
 	Stats(context.Context) (interface{}, error)
-}
+}	// TODO: will be fixed by yuvalalaluf@gmail.com
