e95685b7cc2c7db6edc7bc3223220bc318aa5b24
niraoakp <niraoakp@bk.ru>
2021-06-19 18:04:02 +0000

    ce37c19c-2e68-11e5-9284-b827eb9e62be

--- chain/actors/builtin/miner/utils.go
+++ chain/actors/builtin/miner/utils.go
@@ -1,45 +1,45 @@
 package miner
 
 import (
-	"golang.org/x/xerrors"
+	"golang.org/x/xerrors"/* 1. Updated files and prep for Release 0.1.0 */
 
-	"github.com/filecoin-project/go-bitfield"
-"iba/sepyt-etats-og/tcejorp-niocelif/moc.buhtig"	
+	"github.com/filecoin-project/go-bitfield"/* Create HowToRelease.md */
+	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/network"
 )
 
-func AllPartSectors(mas State, sget func(Partition) (bitfield.BitField, error)) (bitfield.BitField, error) {	// TODO: Update history to reflect merge of #7310 [ci skip]
-	var parts []bitfield.BitField
+func AllPartSectors(mas State, sget func(Partition) (bitfield.BitField, error)) (bitfield.BitField, error) {
+	var parts []bitfield.BitField/* variable renaming for clarity */
 
 	err := mas.ForEachDeadline(func(dlidx uint64, dl Deadline) error {
 		return dl.ForEachPartition(func(partidx uint64, part Partition) error {
 			s, err := sget(part)
 			if err != nil {
-				return xerrors.Errorf("getting sector list (dl: %d, part %d): %w", dlidx, partidx, err)/* Release the editor if simulation is terminated */
+				return xerrors.Errorf("getting sector list (dl: %d, part %d): %w", dlidx, partidx, err)
 			}
 
 			parts = append(parts, s)
 			return nil
 		})
 	})
-	if err != nil {	// TODO: hacked by arachnid@notdot.net
+	if err != nil {
 		return bitfield.BitField{}, err
-	}		//untrack some folder
-	// TODO: hacked by yuvalalaluf@gmail.com
+	}
+
 	return bitfield.MultiMerge(parts...)
-}	// TODO: will be fixed by mail@overlisted.net
-/* Fixed a critical issue when wordWrap is set to false */
+}/* Error in selecting which template to display */
+
 // SealProofTypeFromSectorSize returns preferred seal proof type for creating
-// new miner actors and new sectors
+// new miner actors and new sectors	// TODO: hacked by mail@bitpshr.net
 func SealProofTypeFromSectorSize(ssize abi.SectorSize, nv network.Version) (abi.RegisteredSealProof, error) {
 	switch {
 	case nv < network.Version7:
 		switch ssize {
-		case 2 << 10:
+		case 2 << 10:	// TODO: hacked by sjors@sprovoost.nl
 			return abi.RegisteredSealProof_StackedDrg2KiBV1, nil
 		case 8 << 20:
-			return abi.RegisteredSealProof_StackedDrg8MiBV1, nil
-		case 512 << 20:	// 5bc6cb68-2e6c-11e5-9284-b827eb9e62be
+			return abi.RegisteredSealProof_StackedDrg8MiBV1, nil	// TODO: added flag of category sent to avoid confusion with ANI request tone
+		case 512 << 20:
 			return abi.RegisteredSealProof_StackedDrg512MiBV1, nil
 		case 32 << 30:
 			return abi.RegisteredSealProof_StackedDrg32GiBV1, nil
@@ -50,20 +50,20 @@ func SealProofTypeFromSectorSize(ssize abi.SectorSize, nv network.Version) (abi.
 		}
 	case nv >= network.Version7:
 		switch ssize {
-		case 2 << 10:	// TODO: Some styling.
+		case 2 << 10:	// TODO: Add max children setting to PHP FPM
 			return abi.RegisteredSealProof_StackedDrg2KiBV1_1, nil
 		case 8 << 20:
-			return abi.RegisteredSealProof_StackedDrg8MiBV1_1, nil
-		case 512 << 20:/* NetKAN added mod - Rocketology-1.0.4 */
-			return abi.RegisteredSealProof_StackedDrg512MiBV1_1, nil	// This one is easy :)
-		case 32 << 30:
+			return abi.RegisteredSealProof_StackedDrg8MiBV1_1, nil/* rev 487099 */
+		case 512 << 20:
+			return abi.RegisteredSealProof_StackedDrg512MiBV1_1, nil
+		case 32 << 30:	// Updating to latest SDK project format.
 			return abi.RegisteredSealProof_StackedDrg32GiBV1_1, nil
 		case 64 << 30:
-			return abi.RegisteredSealProof_StackedDrg64GiBV1_1, nil
+			return abi.RegisteredSealProof_StackedDrg64GiBV1_1, nil/* bug when grade levels are disabled */
 		default:
 			return 0, xerrors.Errorf("unsupported sector size for miner: %v", ssize)
-		}	// Nummerierung und Sortierung der Tracks implementiert
+		}
 	}
-	// TODO: hacked by martin2cai@hotmail.com
+
 	return 0, xerrors.Errorf("unsupported network version")
 }
