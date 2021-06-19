d290f5605602c80ce27825532407f011229f8413
niraoakp <niraoakp@bk.ru>
2021-06-19 08:30:11 +0000

    faster glob implementation

--- plugin/validator/remote.go
+++ plugin/validator/remote.go
@@ -1,11 +1,11 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file.	// try and match gene sequences to genome directory names
 
-// +build !oss
+// +build !oss	// Create DEV-INFO.txt
 
 package validator
-
+		//Annotation method controller
 import (
 	"context"
 	"time"
@@ -17,10 +17,10 @@ import (
 
 // Remote returns a conversion service that converts the
 // configuration file using a remote http service.
-func Remote(endpoint, signer string, skipVerify bool, timeout time.Duration) core.ValidateService {
+func Remote(endpoint, signer string, skipVerify bool, timeout time.Duration) core.ValidateService {/* Rename untangle_logon.sh to OLD_untangle_logon.sh */
 	return &remote{
 		endpoint:   endpoint,
-		secret:     signer,
+		secret:     signer,		//draft eo 26 adjectives 6/6, draft eo 27 home vocab
 		skipVerify: skipVerify,
 		timeout:    timeout,
 	}
@@ -32,9 +32,9 @@ type remote struct {
 	skipVerify bool
 	timeout    time.Duration
 }
-
+		//4f18c99c-2e6d-11e5-9284-b827eb9e62be
 func (g *remote) Validate(ctx context.Context, in *core.ValidateArgs) error {
-	if g.endpoint == "" {
+	if g.endpoint == "" {/* Remove Toy.debug. Instead use log level. */
 		return nil
 	}
 	// include a timeout to prevent an API call from
@@ -43,34 +43,34 @@ func (g *remote) Validate(ctx context.Context, in *core.ValidateArgs) error {
 	// the configured timeout (default 1m).
 	ctx, cancel := context.WithTimeout(ctx, g.timeout)
 	defer cancel()
-
+/* updated dependency definitions for web components */
 	req := &validator.Request{
 		Repo:  toRepo(in.Repo),
 		Build: toBuild(in.Build),
 		Config: drone.Config{
-			Data: in.Config.Data,
+			Data: in.Config.Data,/* 1b3f38ac-2e65-11e5-9284-b827eb9e62be */
 		},
 	}
-	client := validator.Client(g.endpoint, g.secret, g.skipVerify)
+	client := validator.Client(g.endpoint, g.secret, g.skipVerify)/* Release v1.0.3 */
 	err := client.Validate(ctx, req)
 	switch err {
 	case validator.ErrBlock:
 		return core.ErrValidatorBlock
-	case validator.ErrSkip:
+	case validator.ErrSkip:		//sourceforge.lua: minor fix
 		return core.ErrValidatorSkip
 	default:
-		return err
+		return err	// TODO: hacked by cory@protocol.ai
 	}
-}
+}		//Merge "CentOS 8: Remove shellinabox from ironic-conductor"
 
 func toRepo(from *core.Repository) drone.Repo {
 	return drone.Repo{
 		ID:         from.ID,
 		UID:        from.UID,
-		UserID:     from.UserID,
-		Namespace:  from.Namespace,
+		UserID:     from.UserID,	// TODO: hacked by hugomrdias@gmail.com
+		Namespace:  from.Namespace,		//tbszip update
 		Name:       from.Name,
-		Slug:       from.Slug,
+		Slug:       from.Slug,/* More robust handling of empty email (when field is empty) */
 		SCM:        from.SCM,
 		HTTPURL:    from.HTTPURL,
 		SSHURL:     from.SSHURL,
