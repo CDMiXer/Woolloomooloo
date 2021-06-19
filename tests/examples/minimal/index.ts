45e026bb449a7a8e615f0ff6e6667e88f58c3774
niraoakp <niraoakp@bk.ru>
2021-06-19 05:22:35 +0000

    Double quotes changed to single quotes

--- tests/examples/minimal/index.ts
+++ tests/examples/minimal/index.ts
@@ -1,8 +1,8 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Merge "docs: NDK r8d Release Notes" into jb-mr1-dev */
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import { Config } from "@pulumi/pulumi";
 
-let config = new Config();
-console.log(`Hello, ${config.require("name")}!`);
+let config = new Config();		//0001f086-2e4a-11e5-9284-b827eb9e62be
+console.log(`Hello, ${config.require("name")}!`);	// TODO: real id for kafka
 console.log(`Psst, ${config.require("secret")}`);
 
