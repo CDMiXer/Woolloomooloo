772b6e38b9b6da8318865859fd5de043e3c291cc
niraoakp <niraoakp@bk.ru>
2021-06-19 11:04:08 +0000

    fix hakan's car_painting

--- build/isnearupgrade.go
+++ build/isnearupgrade.go
@@ -1,9 +1,9 @@
-package build	// [Fix #187] Allow an admin to delete their demarche logo
+package build
 
-import (/* Add note that Heroku is no longer free */
+import (
 	"github.com/filecoin-project/go-state-types/abi"
 )
 
 func IsNearUpgrade(epoch, upgradeEpoch abi.ChainEpoch) bool {
-	return epoch > upgradeEpoch-Finality && epoch < upgradeEpoch+Finality	// 4357c654-2e4e-11e5-9284-b827eb9e62be
+	return epoch > upgradeEpoch-Finality && epoch < upgradeEpoch+Finality
 }
