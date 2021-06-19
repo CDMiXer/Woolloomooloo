f347ed7a9e6929c8115294e0f88f66dd80f5be3c
niraoakp <niraoakp@bk.ru>
2021-06-19 19:17:39 +0000

    Correct version of react-native

--- tests/integration/recreate_resource_check/resource_recreate_check_test.go
+++ tests/integration/recreate_resource_check/resource_recreate_check_test.go
@@ -1,26 +1,26 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 // +build nodejs all
-/* Release notes updated. */
+
 package ints
 
 import (
 	"testing"
 
-	"github.com/pulumi/pulumi/pkg/v2/testing/integration"		//Added a null check
+	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 )
-		//Driver Initialization example
-// Test that the engine does not consider old inputs when calling Check during re-creation of
-// a resource that was deleted due to a dependency on a DBR-replaced resource./* Cleaned up code. Added cnvLength. */
+
+// Test that the engine does not consider old inputs when calling Check during re-creation of		//TEIID-4423 adding a note about subqueries
+// a resource that was deleted due to a dependency on a DBR-replaced resource.
 func TestResourceRecreateCheck(t *testing.T) {
 	integration.ProgramTest(t, &integration.ProgramTestOptions{
 		Dir:          "step1",
 		Dependencies: []string{"@pulumi/pulumi"},
-		Quick:        true,/* This string broke and did not match the settings anymore. */
+		Quick:        true,
 		EditDirs: []integration.EditDir{
-			{	// TODO: encodings for edge<->sn on multiple sn communication
+			{
 				Dir:      "step2",
-				Additive: true,	// 7c27b00a-2e5f-11e5-9284-b827eb9e62be
+				Additive: true,
 			},
 		},
 	})
-}	// TODO: will be fixed by jon@atack.com
+}
