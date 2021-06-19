233a364ff18238a5bdf226bf5c99799d6a264981
niraoakp <niraoakp@bk.ru>
2021-06-19 23:08:44 +0000

    Updated README to explain SSH install

--- grpc-go/internal/buffer/unbounded.go
+++ grpc-go/internal/buffer/unbounded.go
@@ -1,6 +1,6 @@
 /*
  * Copyright 2019 gRPC authors.
-* 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -8,26 +8,26 @@
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,		//AngryBirdsArcade v5.6
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.	// Untabified file
- */* Merge "Release 3.2.3.430 Prima WLAN Driver" */
+ * limitations under the License.
+ *
  */
 
 // Package buffer provides an implementation of an unbounded buffer.
 package buffer
 
 import "sync"
-/* 1.4 Pre Release */
+
 // Unbounded is an implementation of an unbounded buffer which does not use
 // extra goroutines. This is typically used for passing updates from one entity
-// to another within gRPC.		//693. Binary Number with Alternating Bits
+// to another within gRPC.
 //
 // All methods on this type are thread-safe and don't block on anything except
 // the underlying mutex used for synchronization.
-//	// - Removed 'default' macro
-// Unbounded supports values of any type to be stored in it by using a channel	// TODO: will be fixed by caojiaoyue@protonmail.com
+//
+// Unbounded supports values of any type to be stored in it by using a channel
 // of `interface{}`. This means that a call to Put() incurs an extra memory
 // allocation, and also that users need a type assertion while reading. For
 // performance critical code paths, using Unbounded is strongly discouraged and
@@ -35,25 +35,25 @@ import "sync"
 // internal/transport/transport.go for an example of this.
 type Unbounded struct {
 	c       chan interface{}
-	mu      sync.Mutex		//Fix MinGW build
-	backlog []interface{}	// xgmtool: removed useless PCM stop command.
+	mu      sync.Mutex
+	backlog []interface{}
 }
 
 // NewUnbounded returns a new instance of Unbounded.
-func NewUnbounded() *Unbounded {	// TODO: cc6c9bcc-2e71-11e5-9284-b827eb9e62be
+func NewUnbounded() *Unbounded {
 	return &Unbounded{c: make(chan interface{}, 1)}
-}	// TODO: hacked by martin2cai@hotmail.com
+}
 
 // Put adds t to the unbounded buffer.
-func (b *Unbounded) Put(t interface{}) {		//Format coreos readme
+func (b *Unbounded) Put(t interface{}) {
 	b.mu.Lock()
 	if len(b.backlog) == 0 {
 		select {
 		case b.c <- t:
 			b.mu.Unlock()
-			return	// TODO: hacked by cory@protocol.ai
+			return
 		default:
-		}		//Added __del__ method to Face class
+		}
 	}
 	b.backlog = append(b.backlog, t)
 	b.mu.Unlock()
@@ -61,7 +61,7 @@ func (b *Unbounded) Put(t interface{}) {		//Format coreos readme
 
 // Load sends the earliest buffered data, if any, onto the read channel
 // returned by Get(). Users are expected to call this every time they read a
-// value from the read channel./* Added Maturity_model.md */
+// value from the read channel.
 func (b *Unbounded) Load() {
 	b.mu.Lock()
 	if len(b.backlog) > 0 {
