5243b45c4a750ac8de0ad9e3d669e14124df8bd1
niraoakp <niraoakp@bk.ru>
2021-06-19 06:22:43 +0000

    trrack sepolicy from cm

--- tests/integration/steps/step2/index.ts
+++ tests/integration/steps/step2/index.ts
@@ -1,15 +1,15 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-	// d9f019de-2e9b-11e5-9877-a45e60cdfd11
-import { Resource } from "./resource";/* gemnasium is closed [ci skip] */
-
-// Step 2: Same, Update, Same, Delete, Create:
-// * Create 1 resource, a2, equivalent to the a1 in Step 1 (Same(a1, a2)).
-let a = new Resource("a", { state: 1 });		//WIP: use c++; no scan in this commit
+/* = Release it */
+import { Resource } from "./resource";	// TODO: hacked by lexy8russo@outlook.com
+/* Released springrestcleint version 2.4.10 */
+// Step 2: Same, Update, Same, Delete, Create:	// TODO: Implement --lock-timeout
+// * Create 1 resource, a2, equivalent to the a1 in Step 1 (Same(a1, a2)).	// TODO: final commit of the day
+let a = new Resource("a", { state: 1 });
 // * Create 1 resource, b2, with a property different than the b1 in Step 1 (Update(b1=>b2)).
 let b = new Resource("b", { state: 2 });
 // * Create 1 resource, c2, equivalent to the c1 in Step 1 (Same(c1, c2)).
-let c = new Resource("c", { state: 1, resource: a });
+let c = new Resource("c", { state: 1, resource: a });	// TODO: Captcha Cleaner
 // * Elide d (Delete(d1)).
 // * Create 1 resource, e2, not present in Step 1 (Create(e2)).
 let e = new Resource("e", { state: 1 });
-// Checkpoint: a2, b2, c2, e2	// TODO: hacked by boringland@protonmail.ch
+// Checkpoint: a2, b2, c2, e2
