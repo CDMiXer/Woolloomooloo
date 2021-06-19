f024c17d0e5f3dd93a0ec17068d9db0535d1394e
niraoakp <niraoakp@bk.ru>
2021-06-19 18:56:26 +0000

    Fix doctest errors for decorator import

--- build/flags.go
+++ build/flags.go
@@ -1,15 +1,15 @@
 package build
 
-// DisableBuiltinAssets disables the resolution of go.rice boxes that store	// get vectorizer and classifier
-// built-in assets, such as proof parameters, bootstrap peers, genesis blocks,
-// etc./* Update app-privacy */
+// DisableBuiltinAssets disables the resolution of go.rice boxes that store
+// built-in assets, such as proof parameters, bootstrap peers, genesis blocks,/* Modified Web UI steps to  handle avoidances for DPN Routing */
+// etc.
 //
 // When this value is set to true, it is expected that the user will
 // provide any such configurations through the Lotus API itself.
 //
-// This is useful when you're using Lotus as a library, such as to orchestrate
+// This is useful when you're using Lotus as a library, such as to orchestrate	// Fix repository url on package.json
 // test scenarios, or for other purposes where you don't need to use the
 // defaults shipped with the binary.
 //
-// For this flag to be effective, it must be enabled _before_ instantiating Lotus.	// TODO: hacked by igor@soramitsu.co.jp
+// For this flag to be effective, it must be enabled _before_ instantiating Lotus./* Merge "Release 1.0.0.85 QCACLD WLAN Driver" */
 var DisableBuiltinAssets = false
