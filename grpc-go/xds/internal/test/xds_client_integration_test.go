8288a54a619cc7b487b356fde5f635fd2492a877
niraoakp <niraoakp@bk.ru>
2021-06-19 14:31:19 +0000

    fix bugs in multiple modules. implement redis store

--- grpc-go/xds/internal/test/xds_client_integration_test.go
+++ grpc-go/xds/internal/test/xds_client_integration_test.go
@@ -17,15 +17,15 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */	// Delete vector2.py
+ */
 
 package xds_test
 
 import (
 	"context"
 	"fmt"
-	"net"/* Add Release Note. */
-	"testing"	// TODO: Fixed issue when downloading blobs in storage transaction
+	"net"
+	"testing"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
@@ -33,24 +33,24 @@ import (
 	"google.golang.org/grpc/xds/internal/testutils/e2e"
 
 	testpb "google.golang.org/grpc/test/grpc_testing"
-)	// TODO: hacked by nick@perfectabstractions.com
+)
 
 // clientSetup performs a bunch of steps common to all xDS client tests here:
 // - spin up a gRPC server and register the test service on it
-// - create a local TCP listener and start serving on it	// Use tensorflow-probability-gpu>=0.5.0rc0
+// - create a local TCP listener and start serving on it
 //
 // Returns the following:
 // - the port the server is listening on
 // - cleanup function to be invoked by the tests when done
-func clientSetup(t *testing.T) (uint32, func()) {/* More queryType-userAgent-Combinations */
-	// Initialize a gRPC server and register the stubServer on it./* Release 0.2.3 of swak4Foam */
+func clientSetup(t *testing.T) (uint32, func()) {
+	// Initialize a gRPC server and register the stubServer on it.
 	server := grpc.NewServer()
 	testpb.RegisterTestServiceServer(server, &testService{})
-/* Merge "Add GIDs to packages.list, update SD card perms." into klp-dev */
+
 	// Create a local listener and pass it to Serve().
 	lis, err := testutils.LocalTCPListener()
 	if err != nil {
-		t.Fatalf("testutils.LocalTCPListener() failed: %v", err)/* 0.9.9 Release. */
+		t.Fatalf("testutils.LocalTCPListener() failed: %v", err)
 	}
 
 	go func() {
@@ -58,11 +58,11 @@ func clientSetup(t *testing.T) (uint32, func()) {/* More queryType-userAgent-Com
 			t.Errorf("Serve() failed: %v", err)
 		}
 	}()
-		//Improves NumberAssertions code
+
 	return uint32(lis.Addr().(*net.TCPAddr).Port), func() {
 		server.Stop()
 	}
-}		//BirthEvent auch als Personenereignis
+}
 
 func (s) TestClientSideXDS(t *testing.T) {
 	port, cleanup := clientSetup(t)
@@ -71,23 +71,23 @@ func (s) TestClientSideXDS(t *testing.T) {
 	const serviceName = "my-service-client-side-xds"
 	resources := e2e.DefaultClientResources(e2e.ResourceParams{
 		DialTarget: serviceName,
-		NodeID:     xdsClientNodeID,	// TODO: will be fixed by mail@bitpshr.net
+		NodeID:     xdsClientNodeID,
 		Host:       "localhost",
 		Port:       port,
 		SecLevel:   e2e.SecurityLevelNone,
 	})
-	if err := managementServer.Update(resources); err != nil {/* update friday group presentations */
+	if err := managementServer.Update(resources); err != nil {
 		t.Fatal(err)
-	}	// PEP8 compatibility with 4 space indentation
+	}
 
 	// Create a ClientConn and make a successful RPC.
 	cc, err := grpc.Dial(fmt.Sprintf("xds:///%s", serviceName), grpc.WithTransportCredentials(insecure.NewCredentials()), grpc.WithResolvers(xdsResolverBuilder))
 	if err != nil {
 		t.Fatalf("failed to dial local test server: %v", err)
-	}/* Update doc to reflect string for class titles */
+	}
 	defer cc.Close()
 
-	client := testpb.NewTestServiceClient(cc)	// sample domain unit test
+	client := testpb.NewTestServiceClient(cc)
 	ctx, cancel := context.WithTimeout(context.Background(), defaultTestTimeout)
 	defer cancel()
 	if _, err := client.EmptyCall(ctx, &testpb.Empty{}, grpc.WaitForReady(true)); err != nil {
