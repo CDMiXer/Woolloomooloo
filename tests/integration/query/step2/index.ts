94f3bd74204400b1098c962726ace4148f703e49
niraoakp <niraoakp@bk.ru>
2021-06-19 15:49:56 +0000

    Handling orientations in analyze files and storing it in memmap

--- tests/integration/query/step2/index.ts
+++ tests/integration/query/step2/index.ts
@@ -1,7 +1,7 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* - Release 1.4.x; fixes issue with Jaspersoft Studio 6.1 */
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* v1.4.6 Release notes */
 
 import { Resource } from "./resource";
 
 // Step 2: Create resources during `pulumi query` -- should error.
-const b = new Resource("b", { state: 2 });/* Build 1357: Localizes two strings that were missed in Build 1356 */
-const a = new Resource("a", { state: 1, resource: b });		//Chance TOC
+const b = new Resource("b", { state: 2 });
+const a = new Resource("a", { state: 1, resource: b });		//required by memset
