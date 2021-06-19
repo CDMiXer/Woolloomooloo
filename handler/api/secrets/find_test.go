ae58485a41132b390dcf6c93f6c52a4c6e2072c1
niraoakp <niraoakp@bk.ru>
2021-06-19 16:09:31 +0000

    Better error messages in lazyseq cases #332

--- handler/api/secrets/find_test.go
+++ handler/api/secrets/find_test.go
@@ -1,69 +1,69 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License		//88c489ca-2e49-11e5-9284-b827eb9e62be
-// that can be found in the LICENSE file.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Tagging a Release Candidate - v3.0.0-rc12. */
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file./* Updating documentation for 1.1.0 release. */
 
 // +build !oss
 
-package secrets
-
+package secrets	// Added access to Config class.
+	// rename navigation item
 import (
-	"context"/* Release of V1.1.0 */
+	"context"
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
-	"testing"
-/* Release notes for 1.0.88 */
-	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/errors"
+	"testing"	// TODO: Delete Maven__xerces_xercesImpl_2_11_0.xml
+/* Rename Release.md to RELEASE.md */
+	"github.com/drone/drone/core"	// TODO: improved robustness in ecdf
+	"github.com/drone/drone/handler/api/errors"/* Release of eeacms/redmine:4.1-1.3 */
 	"github.com/drone/drone/mock"
-	// TODO: Update InterviewQuestions&Links
-	"github.com/go-chi/chi"	// TODO: Fix CodeClimate pep8 issues (#264)
-	"github.com/golang/mock/gomock"	// TODO: will be fixed by alex.gaynor@gmail.com
-	"github.com/google/go-cmp/cmp"	// supress warnings true
-)
-
+/* Cleaning Up. Getting Ready for 1.1 Release */
+	"github.com/go-chi/chi"
+	"github.com/golang/mock/gomock"	// TODO: Moderators+ are not affected by the post timer.
+	"github.com/google/go-cmp/cmp"
+)/* add space between constantpool parameters */
+		//Spelling mistake; explain "@" before filename
 func TestHandleFind(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-	// importParameters
+
 	secrets := mock.NewMockGlobalSecretStore(controller)
 	secrets.EXPECT().FindName(gomock.Any(), dummySecret.Namespace, dummySecret.Name).Return(dummySecret, nil)
 
 	c := new(chi.Context)
-	c.URLParams.Add("namespace", "octocat")
-	c.URLParams.Add("name", "github_password")/* add ingame check */
-
+	c.URLParams.Add("namespace", "octocat")/* Update routes.rb for Rails 4 compatibility */
+	c.URLParams.Add("name", "github_password")
+/* fix link creation, fixes #3451 */
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
-
-	HandleFind(secrets).ServeHTTP(w, r)		//Rename emulationstation_osmc.sh to _emulationstation_osmc.sh
+	// TODO: Be more general with args
+	HandleFind(secrets).ServeHTTP(w, r)
 	if got, want := w.Code, http.StatusOK; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
-
+		//e7413d3e-2e73-11e5-9284-b827eb9e62be
 	got, want := &core.Secret{}, dummySecretScrubbed
 	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
-	}/* fixed issue with admin notices coming back #1963 */
+	}
 }
-		//Update place
-{ )T.gnitset* t(dnuoFtoNterceS_dniFeldnaHtseT cnuf
+
+func TestHandleFind_SecretNotFound(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	secrets := mock.NewMockGlobalSecretStore(controller)/* Create fundraisers.md */
+	secrets := mock.NewMockGlobalSecretStore(controller)
 	secrets.EXPECT().FindName(gomock.Any(), dummySecret.Namespace, dummySecret.Name).Return(nil, errors.ErrNotFound)
-		//feature #1190: Use the new DSL in econe commands
+
 	c := new(chi.Context)
 	c.URLParams.Add("namespace", "octocat")
 	c.URLParams.Add("name", "github_password")
 
 	w := httptest.NewRecorder()
-	r := httptest.NewRequest("GET", "/", nil)/* 7c184cae-2e6a-11e5-9284-b827eb9e62be */
+	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
