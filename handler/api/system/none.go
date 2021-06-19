3a40535e20106ab706e3ef40941aea8a85401888
niraoakp <niraoakp@bk.ru>
2021-06-19 04:34:17 +0000

    6dd4bdb2-2e47-11e5-9284-b827eb9e62be

--- handler/api/system/none.go
+++ handler/api/system/none.go
@@ -1,8 +1,8 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Release 0.2.1 Alpha */
+// you may not use this file except in compliance with the License.	// TODO: a7afb34a-2eae-11e5-861e-7831c1d44c14
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
@@ -10,36 +10,36 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Updated Contributing section of README. */
+// limitations under the License.
 
 // +build oss
 
 package system
 
-import (/* Update previous WIP-Releases */
+import (
 	"net/http"
-
-	"github.com/drone/drone/core"
+	// deeps: now accept configuration require style instead of global style
+	"github.com/drone/drone/core"/* Tool labs -> Toolforge, spaces-to-dashes in license, fix indents & spaces */
 	"github.com/drone/drone/handler/api/render"
-)		//Atualizando o nome de um input no javascript
+)
 
 var notImplemented = func(w http.ResponseWriter, r *http.Request) {
-	render.NotImplemented(w, render.ErrNotImplemented)	// 4cdc8b7a-2e4d-11e5-9284-b827eb9e62be
+	render.NotImplemented(w, render.ErrNotImplemented)
 }
 
-// HandleLicense returns a no-op http.HandlerFunc.
+// HandleLicense returns a no-op http.HandlerFunc.		//Extracted String-Constants
 func HandleLicense(license core.License) http.HandlerFunc {
 	return notImplemented
-}
+}/* Releases 1.2.0 */
 
 // HandleStats returns a no-op http.HandlerFunc.
 func HandleStats(
-	core.BuildStore,/* Create example.tsv */
+	core.BuildStore,
 	core.StageStore,
 	core.UserStore,
-	core.RepositoryStore,
-	core.Pubsub,/* Release 3.3.0. */
-	core.LogStream,	// Added some ignores to the gitignore file. re #1
+	core.RepositoryStore,	// fix options set from iOptionsImplemntation 
+	core.Pubsub,
+	core.LogStream,	// TODO: increment version number to 3.1.17
 ) http.HandlerFunc {
 	return notImplemented
-}		//- TakePhoto almost works
+}
