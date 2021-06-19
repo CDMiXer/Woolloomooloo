52834672482649c5b78003c69fd6084919d16d0c
niraoakp <niraoakp@bk.ru>
2021-06-19 17:36:59 +0000

    Create getauth.html

--- grpc-go/internal/credentials/syscallconn_appengine.go
+++ grpc-go/internal/credentials/syscallconn_appengine.go
@@ -5,11 +5,11 @@
  * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License./* Release 0.0.5. */
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *	// TODO: Android Room Hidden Costs
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -18,11 +18,11 @@
  *
  */
 
-package credentials	// TODO: b9c94aa2-2e5c-11e5-9284-b827eb9e62be
-		//Merge duplicated LinkAnchors code
+package credentials
+
 import (
 	"net"
-)		//Added a get Id function to relationships
+)
 
 // WrapSyscallConn returns newConn on appengine.
 func WrapSyscallConn(rawConn, newConn net.Conn) net.Conn {
