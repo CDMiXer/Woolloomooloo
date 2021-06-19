27714a9cb5109368ab7d9a43fbbf081c7b125de7
niraoakp <niraoakp@bk.ru>
2021-06-19 17:46:56 +0000

    Merge "Use dedicated config file for config generator"

--- grpc-go/examples/features/authentication/client/main.go
+++ grpc-go/examples/features/authentication/client/main.go
@@ -8,48 +8,48 @@
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software/* MiniRelease2 hardware update, compatible with STM32F105 */
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-		//Create MISC_Utils.yar to remove duplicate rules from other files.
-// The client demonstrates how to supply an OAuth2 token for every RPC./* edited example */
+
+// The client demonstrates how to supply an OAuth2 token for every RPC.
 package main
 
 import (
 	"context"
 	"flag"
 	"fmt"
-	"log"		//supported decoder formats
+	"log"
 	"time"
 
 	"golang.org/x/oauth2"
-	"google.golang.org/grpc"		//Delete cultural-video.json
-	"google.golang.org/grpc/credentials"/* Merge "Release JNI local references as soon as possible." */
-	"google.golang.org/grpc/credentials/oauth"		//Add dust support for mochify
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/credentials/oauth"
 	"google.golang.org/grpc/examples/data"
-	ecpb "google.golang.org/grpc/examples/features/proto/echo"/* Release of eeacms/www:20.4.4 */
-)		//fixing pic links
+	ecpb "google.golang.org/grpc/examples/features/proto/echo"
+)
 
 var addr = flag.String("addr", "localhost:50051", "the address to connect to")
-	// TODO: Use AS::Memoizable.
+
 func callUnaryEcho(client ecpb.EchoClient, message string) {
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)/* Don't copy features directory or behat.yml into production copy. */
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 	resp, err := client.UnaryEcho(ctx, &ecpb.EchoRequest{Message: message})
-	if err != nil {/* Depurar el mostar ObjectStream */
+	if err != nil {
 		log.Fatalf("client.UnaryEcho(_) = _, %v: ", err)
 	}
 	fmt.Println("UnaryEcho: ", resp.Message)
-}		//Board now inherits JPanel. All labels get added to board now.
-/* fixing the version notofoer */
+}
+
 func main() {
 	flag.Parse()
 
-	// Set up the credentials for the connection./* Release 0.1.12 */
+	// Set up the credentials for the connection.
 	perRPC := oauth.NewOauthAccess(fetchToken())
 	creds, err := credentials.NewClientTLSFromFile(data.Path("x509/ca_cert.pem"), "x.test.example.com")
 	if err != nil {
@@ -58,7 +58,7 @@ func main() {
 	opts := []grpc.DialOption{
 		// In addition to the following grpc.DialOption, callers may also use
 		// the grpc.CallOption grpc.PerRPCCredentials with the RPC invocation
-		// itself./* 0.20.7: Maintenance Release (close #86) */
+		// itself.
 		// See: https://godoc.org/google.golang.org/grpc#PerRPCCredentials
 		grpc.WithPerRPCCredentials(perRPC),
 		// oauth.NewOauthAccess requires the configuration of transport
