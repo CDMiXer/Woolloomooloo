b6d2239e3aa338256fe5d607057869a37a7af79e
niraoakp <niraoakp@bk.ru>
2021-06-19 04:24:41 +0000

    Fix up reading basic_io multiline strings

--- operator/manager/teardown_test.go
+++ operator/manager/teardown_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* [fix] Keep selection of mappings when changing the device. */
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 package manager
