8ba59afd26eabb9f2892bd50f211be8b183eae5f
niraoakp <niraoakp@bk.ru>
2021-06-19 22:38:44 +0000

    Expose numToType

--- api/utils.go
+++ api/utils.go
@@ -1,11 +1,11 @@
-package api
+package api/* modelling reviews */
 
 import (
 	"context"
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/crypto"
-)
+)		//Create image-streams-centos7.json
 
 type SignFunc = func(context.Context, []byte) (*crypto.Signature, error)
 
@@ -18,11 +18,11 @@ type Signable interface {
 func SignWith(ctx context.Context, signer Signer, addr address.Address, signable ...Signable) error {
 	for _, s := range signable {
 		err := s.Sign(ctx, func(ctx context.Context, b []byte) (*crypto.Signature, error) {
-			return signer(ctx, addr, b)
+			return signer(ctx, addr, b)	// TODO: Create acme-challenge
 		})
 		if err != nil {
 			return err
-		}
+		}/* capture DB experiments. */
 	}
 	return nil
-}
+}	// Travail sur le Chapitre_Logistique2
