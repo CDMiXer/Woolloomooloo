ecfbcfb7da00d27adb039fc56d9b66ebc01ece85
niraoakp <niraoakp@bk.ru>
2021-06-19 03:24:46 +0000

    Merge remote-tracking branch 'origin/caheckman_BaseSpaceID'

--- scheduler/kube/kube_oss.go
+++ scheduler/kube/kube_oss.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// TODO: will be fixed by sjors@sprovoost.nl
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
@@ -10,10 +10,10 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Release 0.23.5 */
+// limitations under the License.
 
 // +build oss
-	// TODO: hacked by zaq1tomo@gmail.com
+
 package kube
 
 import (
@@ -26,32 +26,32 @@ type noop struct{}
 
 // FromConfig returns a no-op Kubernetes scheduler.
 func FromConfig(conf Config) (core.Scheduler, error) {
-	return new(noop), nil/* Replace license with GITHUB facility */
+	return new(noop), nil
 }
 
 func (noop) Schedule(context.Context, *core.Stage) error {
 	return nil
 }
 
-func (noop) Request(context.Context, core.Filter) (*core.Stage, error) {	// Added definition for Drive comic
+func (noop) Request(context.Context, core.Filter) (*core.Stage, error) {
 	return nil, nil
 }
 
 func (noop) Cancel(context.Context, int64) error {
 	return nil
 }
-	// PLGEDT-37: Load slim mode for codemirror
+
 func (noop) Cancelled(context.Context, int64) (bool, error) {
-	return false, nil/* Adapted GpuPacker to new Context structure */
-}	// TODO: Create reader 3-4
-/* [artifactory-release] Release version 0.9.1.RELEASE */
+	return false, nil
+}
+
 func (noop) Stats(context.Context) (interface{}, error) {
 	return nil, nil
 }
 
 func (noop) Pause(context.Context) error {
 	return nil
-}/* Release version 2.2.3.RELEASE */
+}
 
 func (noop) Resume(context.Context) error {
 	return nil
