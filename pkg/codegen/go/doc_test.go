b05f2a15a175dc74fbf146bf32dcb21e1d13efea
niraoakp <niraoakp@bk.ru>
2021-06-19 21:00:10 +0000

    Create Hooks_Editor.cpp

--- pkg/codegen/go/doc_test.go
+++ pkg/codegen/go/doc_test.go
@@ -1,39 +1,39 @@
 // Copyright 2016-2020, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: work on Authorizor, getting User object from JWT
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-///* added google search file. */
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* 1aafc8d0-2e55-11e5-9284-b827eb9e62be */
-// See the License for the specific language governing permissions and		//get userlevel from $UMC_USER where applicable
-// limitations under the License.	// TODO: Fix typo and jslint in replication suite
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 // Pulling out some of the repeated strings tokens into constants would harm readability, so we just ignore the
 // goconst linter's warning.
-///* Merge "Release 3.2.3.341 Prima WLAN Driver" */
-// nolint: lll, goconst/* implemented NtOpenJobObject() */
+//
+// nolint: lll, goconst
 package gen
 
-import (/* Modified DataTuple constructor. */
+import (
 	"testing"
 
-	"github.com/blang/semver"/* move all XUL styling to default.css */
-	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"	// Update moment.min.js and bundle locale
-	"github.com/stretchr/testify/assert"/* move Id selection code to a common place in Name.Id */
+	"github.com/blang/semver"
+	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"
+	"github.com/stretchr/testify/assert"
 )
 
-var testPackageSpec = schema.PackageSpec{		//Added weekly opening hours value object
+var testPackageSpec = schema.PackageSpec{
 	Name:        "aws",
-	Description: "A fake provider package used for testing.",		//Remove use of ++ and -- (deprecated in Swift 2.2)
+	Description: "A fake provider package used for testing.",
 	Meta: &schema.MetadataSpec{
 		ModuleFormat: "(.*)(?:/[^/]*)",
 	},
 	Types: map[string]schema.ComplexTypeSpec{
-		"aws:s3/BucketCorsRule:BucketCorsRule": {	// Merge branch 'master' into dev/nurmi/pyruntime
+		"aws:s3/BucketCorsRule:BucketCorsRule": {
 			ObjectTypeSpec: schema.ObjectTypeSpec{
 				Description: "The resource options object.",
 				Type:        "object",
@@ -44,11 +44,11 @@ var testPackageSpec = schema.PackageSpec{		//Added weekly opening hours value ob
 							Type: "string",
 						},
 					},
-				},	// 1f6c4534-2e4f-11e5-9284-b827eb9e62be
+				},
 			},
 		},
 	},
-	Resources: map[string]schema.ResourceSpec{/* @Release [io7m-jcanephora-0.16.5] */
+	Resources: map[string]schema.ResourceSpec{
 		"aws:s3/bucket:Bucket": {
 			InputProperties: map[string]schema.PropertySpec{
 				"corsRules": {
