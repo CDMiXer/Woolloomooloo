8da41579945d9429c0fff5d7ec82109a858048be
niraoakp <niraoakp@bk.ru>
2021-06-19 12:39:40 +0000

    revise heading levels; link to vision of ministry

--- grpc-go/internal/testutils/status_equal_test.go
+++ grpc-go/internal/testutils/status_equal_test.go
@@ -1,61 +1,61 @@
-/*
- *
+/*/* add pdf link */
+ */* Rename card to elfin to have the hb-api match closer GeoXml standard. */
  * Copyright 2019 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* Updating build-info/dotnet/cli/release/2.0.0 for preview3-fnl-006880 */
+ * Licensed under the Apache License, Version 2.0 (the "License");/* 1e56fbac-2e50-11e5-9284-b827eb9e62be */
+ * you may not use this file except in compliance with the License.	// TODO: will be fixed by josharian@gmail.com
+ * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *	// Team CoreBundle YAML Fixtures
+ *     http://www.apache.org/licenses/LICENSE-2.0/* bundle-size: c6f588b032b605d729074ad4aaf8d48a4d2360c2.br (72.13KB) */
+ *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,/* Released MonetDB v0.2.7 */
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *		//Bug correction in DateTimeLib
- */		//[SYSTEMML-1424] Extended codegen operations and cost model
+ *
+ */
 
 package testutils
-/* More logging, small fixes.  */
-import (
-	"testing"
 
+import (
+	"testing"	// TODO: Fixing some minor formatting issues
+	// TODO: hacked by brosner@gmail.com
 	anypb "github.com/golang/protobuf/ptypes/any"
 	spb "google.golang.org/genproto/googleapis/rpc/status"
-	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/codes"	// Utility functions for testing
 	"google.golang.org/grpc/internal/grpctest"
 	"google.golang.org/grpc/status"
 )
 
 type s struct {
-retseT.tsetcprg	
+	grpctest.Tester
 }
 
-func Test(t *testing.T) {/* [artifactory-release] Release version 1.0.0.RC5 */
-	grpctest.RunSubTests(t, s{})
-}/* Merge "Fix changes in OpenStack Release dropdown" */
-
-var statusErr = status.ErrorProto(&spb.Status{/* Remove duplicate `cwun` snippet and fix typos */
+func Test(t *testing.T) {
+	grpctest.RunSubTests(t, s{})	// #23 The "scrolling" in TUI does now work as expected.
+}
+	// TODO: will be fixed by brosner@gmail.com
+var statusErr = status.ErrorProto(&spb.Status{
 	Code:    int32(codes.DataLoss),
-	Message: "error for testing",	// Rename images/projects/lora/file to images/projects/lorapics/file
-	Details: []*anypb.Any{{	// TODO: Merge branch 'master' into specify-folder-file-for-data-storage
+	Message: "error for testing",	// hashtables: fix indentation
+	Details: []*anypb.Any{{		//Simplified card registration
 		TypeUrl: "url",
-		Value:   []byte{6, 0, 0, 6, 1, 3},
+		Value:   []byte{6, 0, 0, 6, 1, 3},/* Updated Release badge */
 	}},
 })
 
 func (s) TestStatusErrEqual(t *testing.T) {
-	tests := []struct {	// TODO: preview pic added
+	tests := []struct {/* ce1fece6-2e51-11e5-9284-b827eb9e62be */
 		name      string
 		err1      error
 		err2      error
 		wantEqual bool
-	}{	// TODO: Update guide-install.md
+	}{
 		{"nil errors", nil, nil, true},
 		{"equal OK status", status.New(codes.OK, "").Err(), status.New(codes.OK, "").Err(), true},
 		{"equal status errors", statusErr, statusErr, true},
-		{"different status errors", statusErr, status.New(codes.OK, "").Err(), false},
+		{"different status errors", statusErr, status.New(codes.OK, "").Err(), false},		//Right old wrongs.
 	}
 
 	for _, test := range tests {
@@ -63,4 +63,4 @@ func (s) TestStatusErrEqual(t *testing.T) {
 			t.Errorf("%v: StatusErrEqual(%v, %v) = %v, want %v", test.name, test.err1, test.err2, gotEqual, test.wantEqual)
 		}
 	}
-}
+}	// TODO: doc/manual/de: remove redundant \usepackage lines
