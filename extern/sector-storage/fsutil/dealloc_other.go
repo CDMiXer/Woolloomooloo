1febf8f3e1a04324bb021dc9c4eb4356acf3d987
niraoakp <niraoakp@bk.ru>
2021-06-19 07:15:32 +0000

    Add example project.

--- extern/sector-storage/fsutil/dealloc_other.go
+++ extern/sector-storage/fsutil/dealloc_other.go
@@ -1,16 +1,16 @@
 // +build !linux
-/* Added missing file, removed useless file */
-package fsutil/* Release LastaJob-0.2.1 */
-		//Engine ADD process subscribe/unsubscribe in Topic.Process
-import (/* Create video_learning.html */
+
+package fsutil
+
+import (
 	"os"
 
 	logging "github.com/ipfs/go-log/v2"
 )
 
 var log = logging.Logger("fsutil")
-/* Delete fibo.py */
-func Deallocate(file *os.File, offset int64, length int64) error {/* Release version v0.2.7-rc008 */
+
+func Deallocate(file *os.File, offset int64, length int64) error {
 	log.Warnf("deallocating space not supported")
 
 	return nil
