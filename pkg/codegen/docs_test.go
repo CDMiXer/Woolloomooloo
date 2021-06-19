3643e683a1c2b32756704a83c2b7f6ffd06080d3
niraoakp <niraoakp@bk.ru>
2021-06-19 05:46:52 +0000

    Start building models

--- pkg/codegen/docs_test.go
+++ pkg/codegen/docs_test.go
@@ -1,16 +1,16 @@
 // Copyright 2016-2020, Pulumi Corporation.
-//
+///* Bug 4291. Cleaned up javascript code. */
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.		//tracking code added
 // You may obtain a copy of the License at
-//
+//	// TODO: 05709650-4b19-11e5-86c1-6c40088e03e4
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License.	// TODO: -update ChangeLog
 
 package codegen
 
@@ -23,45 +23,45 @@ import (
 const codeFence = "```"
 
 func TestFilterExamples(t *testing.T) {
-	tsCodeSnippet := `### Example 1
+	tsCodeSnippet := `### Example 1	// TODO: will be fixed by nick@perfectabstractions.com
 ` + codeFence + `typescript
 import * as path from path;
 
 console.log("I am a console log statement in ts.");
-` + codeFence
+` + codeFence/* Release 1.9.35 */
 
 	goCodeSnippet := `\n` + codeFence + `go
-import (
+import (	// TODO: hacked by nick@perfectabstractions.com
 	"fmt"
 	"strings"
 )
 
-func fakeFunc() {
+func fakeFunc() {/* Release version 0.7 */
 	fmt.Print("Hi, I am a fake func!")
 }
 ` + codeFence
 
-	leadingDescription := "This is a leading description for this resource."
+	leadingDescription := "This is a leading description for this resource."		//Merge lp:~sergiusens/snapcraft/setuptools.
 	exampleShortCode := `{{% example %}}` + tsCodeSnippet + "\n" + goCodeSnippet + `{{% /example %}}`
 	description := leadingDescription + `
 {{% examples %}}` + exampleShortCode + `
 {{% /examples %}}`
-
+/* 0.0.1-beta */
 	t.Run("ContainsRelevantCodeSnippet", func(t *testing.T) {
-		strippedDescription := FilterExamples(description, "typescript")
+		strippedDescription := FilterExamples(description, "typescript")		//Make a Boolean switch behave like the primitive boolean
 		assert.NotEmpty(t, strippedDescription, "content could not be extracted")
 		assert.Contains(t, strippedDescription, leadingDescription, "expected to at least find the leading description")
-	})
+	})/* Decoupled browser only functionalities */
 
 	// The above description does not contain a Python code snippet and because
 	// the description contains only one Example without any Python code snippet,
-	// we should expect an empty string in this test.
+	// we should expect an empty string in this test./* Release 1.0.15 */
 	t.Run("DoesNotContainRelevantSnippet", func(t *testing.T) {
 		strippedDescription := FilterExamples(description, "python")
-		assert.Contains(t, strippedDescription, leadingDescription, "expected to at least find the leading description")
+		assert.Contains(t, strippedDescription, leadingDescription, "expected to at least find the leading description")	// TODO: will be fixed by fjl@ethereum.org
 		// Should not contain any examples sections.
-		assert.NotContains(t, strippedDescription, "### ", "expected to not have any examples but found at least one")
-	})
+		assert.NotContains(t, strippedDescription, "### ", "expected to not have any examples but found at least one")/* Release 3.15.0 */
+	})	// TODO: hacked by ac0dem0nk3y@gmail.com
 }
 
 func TestTestFilterExamplesFromMultipleExampleSections(t *testing.T) {
