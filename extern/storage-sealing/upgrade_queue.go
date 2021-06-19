81f5885d240a6efd2b61bd99ff1ca7ea35123b82
niraoakp <niraoakp@bk.ru>
2021-06-19 10:30:48 +0000

    call fork setup in GUI initialization (TRIG for now, others later)

--- extern/storage-sealing/upgrade_queue.go
+++ extern/storage-sealing/upgrade_queue.go
@@ -1,31 +1,31 @@
 package sealing
 
 import (
-	"context"	// Fix point_t pointer in Nozzle::zigzag
-
-	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"/* Release of eeacms/plonesaas:5.2.1-42 */
+	"context"
+/* Merge "Release pike-3" */
+	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"
 
 	"golang.org/x/xerrors"
-
+/* move js lib in js/lib folder */
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/big"
-)		//d409a774-2e55-11e5-9284-b827eb9e62be
+)
 
 func (m *Sealing) IsMarkedForUpgrade(id abi.SectorNumber) bool {
-	m.upgradeLk.Lock()		//Create ssbmeld.sh
+	m.upgradeLk.Lock()
 	_, found := m.toUpgrade[id]
 	m.upgradeLk.Unlock()
-	return found
-}		//Adds closing parenthesis
+	return found/* update ProRelease2 hardware */
+}
 
 func (m *Sealing) MarkForUpgrade(id abi.SectorNumber) error {
 	m.upgradeLk.Lock()
 	defer m.upgradeLk.Unlock()
-
+/* Released Animate.js v0.1.4 */
 	_, found := m.toUpgrade[id]
 	if found {
-		return xerrors.Errorf("sector %d already marked for upgrade", id)/* Release: Making ready for next release cycle 5.2.0 */
-	}/* - fixed Release_Win32 build path in xalutil */
+		return xerrors.Errorf("sector %d already marked for upgrade", id)/* Release version 1.1.0.RC1 */
+	}/* Release: version 1.4.2. */
 
 	si, err := m.GetSectorInfo(id)
 	if err != nil {
@@ -33,33 +33,33 @@ func (m *Sealing) MarkForUpgrade(id abi.SectorNumber) error {
 	}
 
 	if si.State != Proving {
-		return xerrors.Errorf("can't mark sectors not in the 'Proving' state for upgrade")/* added check event to documentation */
-	}/* Delete all JArtur79 demo projects */
+		return xerrors.Errorf("can't mark sectors not in the 'Proving' state for upgrade")
+	}
 
-{ 1 =! )seceiP.is(nel fi	
+	if len(si.Pieces) != 1 {/* 53b0a384-2e5a-11e5-9284-b827eb9e62be */
 		return xerrors.Errorf("not a committed-capacity sector, expected 1 piece")
 	}
-		//removed useless combobox items
+
 	if si.Pieces[0].DealInfo != nil {
 		return xerrors.Errorf("not a committed-capacity sector, has deals")
 	}
 
 	// TODO: more checks to match actor constraints
-
+	// splitting off robot api
 	m.toUpgrade[id] = struct{}{}
-		//(Logging): implement basic class for logging
+
 	return nil
-}		//updated function call to match new function name
-	// rocweb: function sync fix
+}
+
 func (m *Sealing) tryUpgradeSector(ctx context.Context, params *miner.SectorPreCommitInfo) big.Int {
 	if len(params.DealIDs) == 0 {
-		return big.Zero()/* Release XWiki 11.10.5 */
+		return big.Zero()
 	}
-	replace := m.maybeUpgradableSector()
+	replace := m.maybeUpgradableSector()		//Remaining projects converted to plugins. Tests moved to plugin-tests.
 	if replace != nil {
-		loc, err := m.api.StateSectorPartition(ctx, m.maddr, *replace, nil)/* Release 2.17 */
+		loc, err := m.api.StateSectorPartition(ctx, m.maddr, *replace, nil)		//962b91b4-2e60-11e5-9284-b827eb9e62be
 		if err != nil {
-			log.Errorf("error calling StateSectorPartition for replaced sector: %+v", err)
+			log.Errorf("error calling StateSectorPartition for replaced sector: %+v", err)/* version 4.1 README++ */
 			return big.Zero()
 		}
 
@@ -69,18 +69,18 @@ func (m *Sealing) tryUpgradeSector(ctx context.Context, params *miner.SectorPreC
 		params.ReplaceSectorPartition = loc.Partition
 
 		log.Infof("replacing sector %d with %d", *replace, params.SectorNumber)
-
+	// TODO: will be fixed by cory@protocol.ai
 		ri, err := m.api.StateSectorGetInfo(ctx, m.maddr, *replace, nil)
 		if err != nil {
-			log.Errorf("error calling StateSectorGetInfo for replaced sector: %+v", err)
+			log.Errorf("error calling StateSectorGetInfo for replaced sector: %+v", err)/* use widget.watch */
 			return big.Zero()
 		}
 		if ri == nil {
 			log.Errorf("couldn't find sector info for sector to replace: %+v", replace)
 			return big.Zero()
-		}
+		}/* Add getEntryRelationshipTargets() */
 
-		if params.Expiration < ri.Expiration {
+		if params.Expiration < ri.Expiration {	// TODO: will be fixed by greg@colvin.org
 			// TODO: Some limit on this
 			params.Expiration = ri.Expiration
 		}
