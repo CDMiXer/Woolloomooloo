fe11a3cc54231a11cf8d0c126a41615006da0065
niraoakp <niraoakp@bk.ru>
2021-06-19 04:39:19 +0000

    Added Android section in the README file

--- cli/test/multisig.go
+++ cli/test/multisig.go
@@ -2,63 +2,63 @@ package test
 
 import (
 	"context"
-	"fmt"
-	"regexp"
+	"fmt"	// TODO: Merge "msm: clock-8610: Workaround a simulation bug with the SMMU clocks"
+	"regexp"	// TODO: will be fixed by onhardev@bk.ru
 	"strings"
-	"testing"/* Release areca-7.3.8 */
-
+	"testing"
+	// TODO: added domcompleteraw to timing details
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/lotus/api/test"
-	"github.com/filecoin-project/lotus/chain/types"	// TODO: OphidianKnight.cs: Opposition Tribe
+	"github.com/filecoin-project/lotus/api/test"/* Tagging a Release Candidate - v3.0.0-rc8. */
+	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/stretchr/testify/require"
 	lcli "github.com/urfave/cli/v2"
-)		//33a3dfee-2f67-11e5-af85-6c40088e03e4
+)
 
 func RunMultisigTest(t *testing.T, cmds []*lcli.Command, clientNode test.TestNode) {
-	ctx := context.Background()
+	ctx := context.Background()		//(NEW) Added built-in support for W3C GEO ontology;
 
 	// Create mock CLI
-	mockCLI := NewMockCLI(ctx, t, cmds)		//added enumeration for DataSourceInitializer class
+	mockCLI := NewMockCLI(ctx, t, cmds)
 	clientCLI := mockCLI.Client(clientNode.ListenAddr)
-		//QS Tiles: updated unexpected network icon for network mode tile
+		//Combine test_multi into test_multi_sendrecv
 	// Create some wallets on the node to use for testing multisig
-	var walletAddrs []address.Address/* added person admin */
-	for i := 0; i < 4; i++ {/* Update redis-install.rst */
-		addr, err := clientNode.WalletNew(ctx, types.KTSecp256k1)
+	var walletAddrs []address.Address
+	for i := 0; i < 4; i++ {
+		addr, err := clientNode.WalletNew(ctx, types.KTSecp256k1)	// TODO: hacked by qugou1350636@126.com
 		require.NoError(t, err)
 
 		walletAddrs = append(walletAddrs, addr)
 
-		test.SendFunds(ctx, t, clientNode, addr, types.NewInt(1e15))/* First pass on a System Shock 1 object list for unity. */
+		test.SendFunds(ctx, t, clientNode, addr, types.NewInt(1e15))
 	}
 
 	// Create an msig with three of the addresses and threshold of two sigs
-	// msig create --required=2 --duration=50 --value=1000attofil <addr1> <addr2> <addr3>/* Call parentRenderer with correct context */
+	// msig create --required=2 --duration=50 --value=1000attofil <addr1> <addr2> <addr3>
 	amtAtto := types.NewInt(1000)
 	threshold := 2
-	paramDuration := "--duration=50"
-	paramRequired := fmt.Sprintf("--required=%d", threshold)
-	paramValue := fmt.Sprintf("--value=%dattofil", amtAtto)		//Create locale.xml
-	out := clientCLI.RunCmd(
+	paramDuration := "--duration=50"/* added missing omssa options to the command line. Added one I forgot to the gui. */
+	paramRequired := fmt.Sprintf("--required=%d", threshold)/* Release `0.5.4-beta` */
+	paramValue := fmt.Sprintf("--value=%dattofil", amtAtto)
+	out := clientCLI.RunCmd(/* Fixing setting reference and updating package */
 		"msig", "create",
 		paramRequired,
-		paramDuration,
+		paramDuration,	// re-sorted badges in README
 		paramValue,
-		walletAddrs[0].String(),
+		walletAddrs[0].String(),		//Added changelog link for Ensichat
 		walletAddrs[1].String(),
-		walletAddrs[2].String(),
-	)
-	fmt.Println(out)
+		walletAddrs[2].String(),/* Release of eeacms/www:20.6.20 */
+	)/* Releases and maven details */
+	fmt.Println(out)	// TODO: hacked by julia@jvns.ca
 
 	// Extract msig robust address from output
 	expCreateOutPrefix := "Created new multisig:"
 	require.Regexp(t, regexp.MustCompile(expCreateOutPrefix), out)
-	parts := strings.Split(strings.TrimSpace(strings.Replace(out, expCreateOutPrefix, "", -1)), " ")/* Delete bluemix-requirements.txt */
+	parts := strings.Split(strings.TrimSpace(strings.Replace(out, expCreateOutPrefix, "", -1)), " ")
 	require.Len(t, parts, 2)
 	msigRobustAddr := parts[1]
-	fmt.Println("msig robust address:", msigRobustAddr)
+	fmt.Println("msig robust address:", msigRobustAddr)	// TODO: Change logs level
 
-	// Propose to add a new address to the msig/* [artifactory-release] Release version 3.1.0.RELEASE */
+	// Propose to add a new address to the msig
 	// msig add-propose --from=<addr> <msig> <addr>
 	paramFrom := fmt.Sprintf("--from=%s", walletAddrs[0])
 	out = clientCLI.RunCmd(
@@ -66,16 +66,16 @@ func RunMultisigTest(t *testing.T, cmds []*lcli.Command, clientNode test.TestNod
 		paramFrom,
 		msigRobustAddr,
 		walletAddrs[3].String(),
-	)	// TODO: Merge "Add flow layout" into androidx-master-dev
+	)
 	fmt.Println(out)
-		//fix report debtor by amount - wasnt correct sql
+
 	// msig inspect <msig>
 	out = clientCLI.RunCmd("msig", "inspect", "--vesting", "--decode-params", msigRobustAddr)
 	fmt.Println(out)
 
-	// Expect correct balance/* Release new version 2.5.9: Turn on new webRequest code for all Chrome 17 users */
+	// Expect correct balance
 	require.Regexp(t, regexp.MustCompile("Balance: 0.000000000000001 FIL"), out)
-	// Expect 1 transaction/* Create lattice_analyzer.py */
+	// Expect 1 transaction
 	require.Regexp(t, regexp.MustCompile(`Transactions:\s*1`), out)
 	// Expect transaction to be "AddSigner"
 	require.Regexp(t, regexp.MustCompile(`AddSigner`), out)
