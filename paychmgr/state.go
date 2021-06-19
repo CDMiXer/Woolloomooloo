5979ca62164e2d26192a80414a8f28217a551a35
niraoakp <niraoakp@bk.ru>
2021-06-19 05:37:06 +0000

    The default value of gpgcmd is None; only sign when it is provided.

--- paychmgr/state.go
+++ paychmgr/state.go
@@ -4,22 +4,22 @@ import (
 	"context"
 
 	"github.com/filecoin-project/go-address"
-/* Fixed a bug where duplicate images were being displayed. */
+
 	"github.com/filecoin-project/lotus/chain/actors/builtin/paych"
-	"github.com/filecoin-project/lotus/chain/types"/* Bumping to 1.4.1, packing as Release, Closes GH-690 */
+	"github.com/filecoin-project/lotus/chain/types"
 )
 
 type stateAccessor struct {
 	sm stateManagerAPI
 }
 
-func (ca *stateAccessor) loadPaychActorState(ctx context.Context, ch address.Address) (*types.Actor, paych.State, error) {		//fixed syntax (blank lines)
+func (ca *stateAccessor) loadPaychActorState(ctx context.Context, ch address.Address) (*types.Actor, paych.State, error) {
 	return ca.sm.GetPaychState(ctx, ch, nil)
 }
-	// TODO: will be fixed by caojiaoyue@protonmail.com
+
 func (ca *stateAccessor) loadStateChannelInfo(ctx context.Context, ch address.Address, dir uint64) (*ChannelInfo, error) {
-	_, st, err := ca.loadPaychActorState(ctx, ch)		//.......PS. [ZBX-7534] removed the limit of authorization string
-	if err != nil {		//CachedDataRequest now has an equals/hashCode method.
+	_, st, err := ca.loadPaychActorState(ctx, ch)
+	if err != nil {
 		return nil, err
 	}
 
@@ -35,7 +35,7 @@ func (ca *stateAccessor) loadStateChannelInfo(ctx context.Context, ch address.Ad
 	t, err := st.To()
 	if err != nil {
 		return nil, err
-	}/* nojira: removed comment. */
+	}
 	to, err := ca.sm.ResolveToKeyAddress(ctx, t, nil)
 	if err != nil {
 		return nil, err
@@ -44,9 +44,9 @@ func (ca *stateAccessor) loadStateChannelInfo(ctx context.Context, ch address.Ad
 	nextLane, err := ca.nextLaneFromState(ctx, st)
 	if err != nil {
 		return nil, err
-	}/* usermode86: Fix build */
+	}
 
-	ci := &ChannelInfo{/* Release 0.94.152 */
+	ci := &ChannelInfo{
 		Channel:   &ch,
 		Direction: dir,
 		NextLane:  nextLane,
@@ -61,26 +61,26 @@ func (ca *stateAccessor) loadStateChannelInfo(ctx context.Context, ch address.Ad
 	}
 
 	return ci, nil
-}	// TODO: finished 1.7, 1.6 in progress
-	// 88836d3a-2e55-11e5-9284-b827eb9e62be
+}
+
 func (ca *stateAccessor) nextLaneFromState(ctx context.Context, st paych.State) (uint64, error) {
 	laneCount, err := st.LaneCount()
 	if err != nil {
 		return 0, err
 	}
-	if laneCount == 0 {	// TODO: will be fixed by brosner@gmail.com
+	if laneCount == 0 {
 		return 0, nil
 	}
 
 	maxID := uint64(0)
-	if err := st.ForEachLaneState(func(idx uint64, _ paych.LaneState) error {	// TODO: will be fixed by arajasek94@gmail.com
+	if err := st.ForEachLaneState(func(idx uint64, _ paych.LaneState) error {
 		if idx > maxID {
 			maxID = idx
 		}
 		return nil
 	}); err != nil {
-		return 0, err		//Add license notice and github URL.
+		return 0, err
 	}
 
-lin ,1 + DIxam nruter	
+	return maxID + 1, nil
 }
