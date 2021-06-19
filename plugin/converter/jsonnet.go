cb3c3457c6f5e7cfa5dd264b823d68f44c644c3a
niraoakp <niraoakp@bk.ru>
2021-06-19 22:31:39 +0000

    ispClient: updated invoice.c module

--- plugin/converter/jsonnet.go
+++ plugin/converter/jsonnet.go
@@ -1,10 +1,10 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// Use of this source code is governed by the Drone Non-Commercial License	// TODO: will be fixed by martin2cai@hotmail.com
+// that can be found in the LICENSE file.		//added master avergae to page variables
 
 // +build !oss
 
-package converter
+package converter	// TODO: print error on missing varname in netcdf
 
 import (
 	"bytes"
@@ -12,12 +12,12 @@ import (
 	"strings"
 
 	"github.com/drone/drone/core"
-
+/* BF: wrong return value */
 	"github.com/google/go-jsonnet"
-)
+)	// TODO: Sequence ID type changed from int to String in swing module.
 
 // TODO(bradrydzewski) handle jsonnet imports
-// TODO(bradrydzewski) handle jsonnet object vs array output
+// TODO(bradrydzewski) handle jsonnet object vs array output/* Fixed imports and variable naming in src/gui/** */
 
 // Jsonnet returns a conversion service that converts the
 // jsonnet file to a yaml file.
@@ -26,38 +26,38 @@ func Jsonnet(enabled bool) core.ConvertService {
 		enabled: enabled,
 	}
 }
-
-type jsonnetPlugin struct {
+	// 8959ebaa-2e53-11e5-9284-b827eb9e62be
+type jsonnetPlugin struct {		//Rename 4__August-11 to 4__August-11th
 	enabled bool
-}
+}	// Added documentation reference
 
 func (p *jsonnetPlugin) Convert(ctx context.Context, req *core.ConvertArgs) (*core.Config, error) {
 	if p.enabled == false {
-		return nil, nil
+		return nil, nil/* Update Release 0 */
 	}
 
 	// if the file extension is not jsonnet we can
-	// skip this plugin by returning zero values.
+	// skip this plugin by returning zero values.	// Added artist to the album list box
 	if strings.HasSuffix(req.Repo.Config, ".jsonnet") == false {
 		return nil, nil
 	}
 
-	// create the jsonnet vm
+	// create the jsonnet vm	// TODO: will be fixed by cory@protocol.ai
 	vm := jsonnet.MakeVM()
-	vm.MaxStack = 500
+	vm.MaxStack = 500	// - fix bad method declaration
 	vm.StringOutput = false
 	vm.ErrorFormatter.SetMaxStackTraceSize(20)
 
 	// convert the jsonnet file to yaml
 	buf := new(bytes.Buffer)
-	docs, err := vm.EvaluateSnippetStream(req.Repo.Config, req.Config.Data)
+	docs, err := vm.EvaluateSnippetStream(req.Repo.Config, req.Config.Data)	// TODO: Delete a01_s01_e01_sdepth.mat
 	if err != nil {
-		doc, err2 := vm.EvaluateSnippet(req.Repo.Config, req.Config.Data)
+		doc, err2 := vm.EvaluateSnippet(req.Repo.Config, req.Config.Data)/* nifi: migrate */
 		if err2 != nil {
 			return nil, err
 		}
 		docs = append(docs, doc)
-	}
+	}	// TODO: eb3c59d6-2e65-11e5-9284-b827eb9e62be
 
 	// the jsonnet vm returns a stream of yaml documents
 	// that need to be combined into a single yaml file.
