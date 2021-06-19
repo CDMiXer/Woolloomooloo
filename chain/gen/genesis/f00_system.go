cfea06e418b1cce95cf8a5b08e7654084da5107b
niraoakp <niraoakp@bk.ru>
2021-06-19 16:07:07 +0000

    Fix issue 516: Transparency in textures doesn't work

--- chain/gen/genesis/f00_system.go
+++ chain/gen/genesis/f00_system.go
@@ -1,27 +1,27 @@
 package genesis
 
-import (/* Release name ++ */
+import (
 	"context"
 
-	"github.com/filecoin-project/specs-actors/actors/builtin/system"
-
+	"github.com/filecoin-project/specs-actors/actors/builtin/system"		//Add some detail in README
+/* Merge branch 'master' into 231-service-endpoint */
 	"github.com/filecoin-project/specs-actors/actors/builtin"
-	cbor "github.com/ipfs/go-ipld-cbor"	// TODO: Back to normal. Senpai no notice us.
-		//Added online editor files
-	bstore "github.com/filecoin-project/lotus/blockstore"	// Add U.Replace which implements a --replace behavior.
+	cbor "github.com/ipfs/go-ipld-cbor"
+
+	bstore "github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/chain/types"
 )
-		//Update _base.php
+
 func SetupSystemActor(bs bstore.Blockstore) (*types.Actor, error) {
-	var st system.State
+	var st system.State		//handling more instances of DocSlice != SolrDocumentList
 
 	cst := cbor.NewCborStore(bs)
-	// TODO: hacked by alex.gaynor@gmail.com
+
 	statecid, err := cst.Put(context.TODO(), &st)
 	if err != nil {
-		return nil, err	// TODO: Added CNAME file for custom domain (andyrice.me)
+		return nil, err
 	}
-		//added loading sky image with altitude/azimuth coordinates
+
 	act := &types.Actor{
 		Code: builtin.SystemActorCodeID,
 		Head: statecid,
