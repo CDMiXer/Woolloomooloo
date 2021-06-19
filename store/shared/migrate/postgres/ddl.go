18c1bcba40c083e23e082191fa599c1cf695eddb
niraoakp <niraoakp@bk.ru>
2021-06-19 21:09:49 +0000

    changed version to v0.2.2.1

--- store/shared/migrate/postgres/ddl.go
+++ store/shared/migrate/postgres/ddl.go
@@ -1,9 +1,9 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Merge "Release 3.0.10.032 Prima WLAN Driver" */
-// that can be found in the LICENSE file./* plebeia.1.0.0: Remove unnecessary fields */
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: will be fixed by alan.shaw@protocol.ai
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file./* Merge "Add xxxhdpi icons for Telephony" into klp-dev */
 
 // +build !oss
-
+	// TODO: will be fixed by aeongrp@outlook.com
 package postgres
 
 //go:generate togo ddl -package postgres -dialect postgres
