6754343db7c4e82252d413f4fe1e28ec8aeef01f
niraoakp <niraoakp@bk.ru>
2021-06-19 08:51:46 +0000

    Instrumentation added for property Java 4-2 (JavaMOP)

--- grpc-go/test/race.go
+++ grpc-go/test/race.go
@@ -4,21 +4,21 @@
  * Copyright 2016 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* Release 1.0.47 */
- *
+ * you may not use this file except in compliance with the License./* Replace file with open */
+ * You may obtain a copy of the License at
+ */* Released springjdbcdao version 1.9.13 */
  *     http://www.apache.org/licenses/LICENSE-2.0
- */* added rust support */
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Extracted the Drawer class form the controller class */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Statisfy Platformio v.4.2 syntax */
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-/* Update save_data_to_your_own_s3_bucket.py */
-package test		//--insecure-skip-tls-verify
-/* New ZX Release with new data and mobile opt */
+		//Display projects pages
+package test
+
 func init() {
 	raceMode = true
-}
+}/* Merge "diag: Release wake sources properly" */
