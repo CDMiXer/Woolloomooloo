1e52d289708bdad2ae44bafb7a171bb3ea8ef959
niraoakp <niraoakp@bk.ru>
2021-06-19 02:10:21 +0000

    Update gen_solver.py

--- operator/manager/rpc/server_test.go
+++ operator/manager/rpc/server_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Fix link in release notes */
-// that can be found in the LICENSE file.
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.		//Updated the plotly-ecdf feedstock.
 
-// +build !oss		//Added Font change summary
+// +build !oss
 
-package rpc	// TODO: will be fixed by ligi@ligi.de
+package rpc
