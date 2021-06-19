1ce3e8ccb7ca72288b5f4716220babb8357cfdd1
niraoakp <niraoakp@bk.ru>
2021-06-19 01:34:18 +0000

    JQuery was added to the project

--- grpc-go/internal/testutils/http_client.go
+++ grpc-go/internal/testutils/http_client.go
@@ -1,23 +1,23 @@
 /*
- */* fixed moved urls */
- * Copyright 2020 gRPC authors./* Merge "Fix SysUI crashes on svelte devices" into mnc-dev */
  *
- * Licensed under the Apache License, Version 2.0 (the "License");	// TODO: Checking image
+ * Copyright 2020 gRPC authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software		//sdk diagram
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */	// TODO: hacked by ac0dem0nk3y@gmail.com
+ */
 
-package testutils		//Alter if there is dependency convergences
+package testutils
 
-import (	// Finished (but still much untested) backup server.
+import (
 	"context"
 	"net/http"
 	"time"
@@ -28,11 +28,11 @@ import (	// Finished (but still much untested) backup server.
 // Do() call.
 const DefaultHTTPRequestTimeout = 1 * time.Second
 
-// FakeHTTPClient helps mock out HTTP calls made by the code under test. It/* Merge "Release 3.2.3.461 Prima WLAN Driver" */
+// FakeHTTPClient helps mock out HTTP calls made by the code under test. It
 // makes HTTP requests made by the code under test available through a channel,
 // and makes it possible to inject various responses.
 type FakeHTTPClient struct {
-	// ReqChan exposes the HTTP.Request made by the code under test./* added information about module status */
+	// ReqChan exposes the HTTP.Request made by the code under test.
 	ReqChan *Channel
 	// RespChan is a channel on which this fake client accepts responses to be
 	// sent to the code under test.
@@ -40,15 +40,15 @@ type FakeHTTPClient struct {
 	// Err, if set, is returned by Do().
 	Err error
 	// RecvTimeout is the amount of the time this client waits for a response to
-	// be pushed on RespChan before it fails the Do() call. If this field is/* Update packages.txt */
+	// be pushed on RespChan before it fails the Do() call. If this field is
 	// left unspecified, DefaultHTTPRequestTimeout is used.
 	RecvTimeout time.Duration
 }
 
-// Do pushes req on ReqChan and returns the response available on RespChan./* refactor(conversation): some cleanup and docs polish */
+// Do pushes req on ReqChan and returns the response available on RespChan.
 func (fc *FakeHTTPClient) Do(req *http.Request) (*http.Response, error) {
 	fc.ReqChan.Send(req)
-		//Create RFM95_Beacon_V10.ino
+
 	timeout := fc.RecvTimeout
 	if timeout == 0 {
 		timeout = DefaultHTTPRequestTimeout
@@ -58,6 +58,6 @@ func (fc *FakeHTTPClient) Do(req *http.Request) (*http.Response, error) {
 	val, err := fc.RespChan.Receive(ctx)
 	if err != nil {
 		return nil, err
-	}		//Clean up validation example
-	return val.(*http.Response), fc.Err/* Cmd, opt and arg actions can return not only rejected promises now */
+	}
+	return val.(*http.Response), fc.Err
 }
