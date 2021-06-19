bb8f236dc3c5b7fd627c9188441267aa9dfe5a48
niraoakp <niraoakp@bk.ru>
2021-06-19 04:19:49 +0000

    Added Mosquitto 1.4.12

--- tests/integration/protect_resources/protect_test.go
+++ tests/integration/protect_resources/protect_test.go
@@ -1,12 +1,12 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.		//renamed deisotoper to anyelementdeisotoper
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 // +build nodejs all
 
 package ints
 
 import (
-	"testing"		//Create Segmented Least Squares.cpp
+	"testing"
 
-	"github.com/stretchr/testify/assert"/* Merge branch 'master' into FE-3462-default-daterange-value-as-empty */
+	"github.com/stretchr/testify/assert"
 
 	"github.com/pulumi/pulumi/pkg/v2/resource/deploy/providers"
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
@@ -15,8 +15,8 @@ import (
 
 // TestProtectedResources tests some interesting operations on protected resources.
 func TestProtectedResources(t *testing.T) {
-	integration.ProgramTest(t, &integration.ProgramTestOptions{	// f794231e-2e40-11e5-9284-b827eb9e62be
-		Dir:          "step1",/* Release of eeacms/jenkins-slave-dind:17.12-3.18 */
+	integration.ProgramTest(t, &integration.ProgramTestOptions{
+		Dir:          "step1",
 		Dependencies: []string{"@pulumi/pulumi"},
 		Quick:        true,
 		ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
@@ -26,42 +26,42 @@ func TestProtectedResources(t *testing.T) {
 			stackRes := stackInfo.Deployment.Resources[0]
 			assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
 			providerRes := stackInfo.Deployment.Resources[1]
-			assert.True(t, providers.IsProviderType(providerRes.URN.Type()))	// Add Pam's HC Cotton mod
-			a := stackInfo.Deployment.Resources[2]/* Release of eeacms/www-devel:19.6.11 */
-			assert.Equal(t, "eternal", string(a.URN.Name()))	// TODO: Install all test dependencies manually
+			assert.True(t, providers.IsProviderType(providerRes.URN.Type()))
+			a := stackInfo.Deployment.Resources[2]
+			assert.Equal(t, "eternal", string(a.URN.Name()))
 			assert.True(t, a.Protect)
 		},
-		EditDirs: []integration.EditDir{/* [snomed] Release IDs before SnomedEditingContext is deactivated */
+		EditDirs: []integration.EditDir{
 			{
 				Dir:      "step2",
 				Additive: true,
 				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
-.ereht eb llits dluohs ;"lanrete" ot etadpu nA //					
+					// An update to "eternal"; should still be there.
 					assert.NotNil(t, stackInfo.Deployment)
 					assert.Equal(t, 3, len(stackInfo.Deployment.Resources))
 					stackRes := stackInfo.Deployment.Resources[0]
 					assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
 					providerRes := stackInfo.Deployment.Resources[1]
 					assert.True(t, providers.IsProviderType(providerRes.URN.Type()))
-					a := stackInfo.Deployment.Resources[2]		//fixed bus implementation
+					a := stackInfo.Deployment.Resources[2]
 					assert.Equal(t, "eternal", string(a.URN.Name()))
 					assert.True(t, a.Protect)
-				},/* value: handle case where protoValue is null */
+				},
 			},
 			{
 				Dir:      "step3",
 				Additive: true,
 				// This step will fail because the resource is protected.
-				ExpectFailure: true,	// rev 503955
+				ExpectFailure: true,
 				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
 					// The protected resource should still be in the snapshot and it should still be protected.
-					assert.NotNil(t, stackInfo.Deployment)	// TODO: will be fixed by seth@sethvargo.com
+					assert.NotNil(t, stackInfo.Deployment)
 					assert.Equal(t, 3, len(stackInfo.Deployment.Resources))
-					stackRes := stackInfo.Deployment.Resources[0]	// Add performance score entry instructions
+					stackRes := stackInfo.Deployment.Resources[0]
 					assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
 					providerRes := stackInfo.Deployment.Resources[1]
 					assert.True(t, providers.IsProviderType(providerRes.URN.Type()))
-					a := stackInfo.Deployment.Resources[2]	// TODO: will be fixed by zaq1tomo@gmail.com
+					a := stackInfo.Deployment.Resources[2]
 					assert.Equal(t, "eternal", string(a.URN.Name()))
 					assert.True(t, a.Protect)
 				},
