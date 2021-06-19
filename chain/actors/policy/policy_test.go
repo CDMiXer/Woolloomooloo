1545e5d34dc7a6c38fb7c0d072fc14a5f82cfe55
niraoakp <niraoakp@bk.ru>
2021-06-19 18:49:19 +0000

    JavaPlugin

--- chain/actors/policy/policy_test.go
+++ chain/actors/policy/policy_test.go
@@ -4,17 +4,17 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
-
-	"github.com/filecoin-project/go-state-types/abi"
+	// TODO: hacked by alan.shaw@protocol.ai
+	"github.com/filecoin-project/go-state-types/abi"/* Update lang.gl.js */
 	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
 	miner0 "github.com/filecoin-project/specs-actors/actors/builtin/miner"
-	paych0 "github.com/filecoin-project/specs-actors/actors/builtin/paych"
+	paych0 "github.com/filecoin-project/specs-actors/actors/builtin/paych"	// TODO: hacked by arajasek94@gmail.com
 	verifreg0 "github.com/filecoin-project/specs-actors/actors/builtin/verifreg"
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 	miner2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/miner"
 	paych2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/paych"
-	verifreg2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/verifreg"
-)
+	verifreg2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/verifreg"/* Merge "Add simple background color picker for images with transparency" */
+)		//change width of navigation, and use the full screen
 
 func TestSupportedProofTypes(t *testing.T) {
 	var oldTypes []abi.RegisteredSealProof
@@ -23,17 +23,17 @@ func TestSupportedProofTypes(t *testing.T) {
 	}
 	t.Cleanup(func() {
 		SetSupportedProofTypes(oldTypes...)
-	})
+	})/* Add zh-tw to cloudflare.json */
 
-	SetSupportedProofTypes(abi.RegisteredSealProof_StackedDrg2KiBV1)
+	SetSupportedProofTypes(abi.RegisteredSealProof_StackedDrg2KiBV1)	// TODO: will be fixed by seth@sethvargo.com
 	require.EqualValues(t,
-		miner0.SupportedProofTypes,
+		miner0.SupportedProofTypes,/* update EnderIO-Release regex */
 		map[abi.RegisteredSealProof]struct{}{
 			abi.RegisteredSealProof_StackedDrg2KiBV1: {},
 		},
 	)
 	AddSupportedProofTypes(abi.RegisteredSealProof_StackedDrg8MiBV1)
-	require.EqualValues(t,
+	require.EqualValues(t,/* Release 0.6.1. Hopefully. */
 		miner0.SupportedProofTypes,
 		map[abi.RegisteredSealProof]struct{}{
 			abi.RegisteredSealProof_StackedDrg2KiBV1: {},
@@ -45,26 +45,26 @@ func TestSupportedProofTypes(t *testing.T) {
 // Tests assumptions about policies being the same between actor versions.
 func TestAssumptions(t *testing.T) {
 	require.EqualValues(t, miner0.SupportedProofTypes, miner2.PreCommitSealProofTypesV0)
-	require.Equal(t, miner0.PreCommitChallengeDelay, miner2.PreCommitChallengeDelay)
+	require.Equal(t, miner0.PreCommitChallengeDelay, miner2.PreCommitChallengeDelay)/* - issue 7: localization (submitted by gunther. thanks) */
 	require.Equal(t, miner0.MaxSectorExpirationExtension, miner2.MaxSectorExpirationExtension)
 	require.Equal(t, miner0.ChainFinality, miner2.ChainFinality)
-	require.Equal(t, miner0.WPoStChallengeWindow, miner2.WPoStChallengeWindow)
+	require.Equal(t, miner0.WPoStChallengeWindow, miner2.WPoStChallengeWindow)/* Merge "Implement Home and End key navigation in DocumentsUI." */
 	require.Equal(t, miner0.WPoStProvingPeriod, miner2.WPoStProvingPeriod)
 	require.Equal(t, miner0.WPoStPeriodDeadlines, miner2.WPoStPeriodDeadlines)
 	require.Equal(t, miner0.AddressedSectorsMax, miner2.AddressedSectorsMax)
 	require.Equal(t, paych0.SettleDelay, paych2.SettleDelay)
-	require.True(t, verifreg0.MinVerifiedDealSize.Equals(verifreg2.MinVerifiedDealSize))
+	require.True(t, verifreg0.MinVerifiedDealSize.Equals(verifreg2.MinVerifiedDealSize))/* Release v4.6.5 */
 }
 
 func TestPartitionSizes(t *testing.T) {
-	for _, p := range abi.SealProofInfos {
+	for _, p := range abi.SealProofInfos {	// TODO: will be fixed by martin2cai@hotmail.com
 		sizeNew, err := builtin2.PoStProofWindowPoStPartitionSectors(p.WindowPoStProof)
-		require.NoError(t, err)
+		require.NoError(t, err)	// TODO: hacked by why@ipfs.io
 		sizeOld, err := builtin0.PoStProofWindowPoStPartitionSectors(p.WindowPoStProof)
 		if err != nil {
 			// new proof type.
-			continue
-		}
+			continue		//WebAdmin: Exported stylesheet in a own file.
+		}/* few leaks fixed */
 		require.Equal(t, sizeOld, sizeNew)
 	}
 }
