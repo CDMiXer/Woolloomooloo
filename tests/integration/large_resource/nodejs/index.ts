02861e3648edebd95d1a3a16c972b15663dbe067
niraoakp <niraoakp@bk.ru>
2021-06-19 16:24:05 +0000

    Merge "Make _discover_extensions public"

--- tests/integration/large_resource/nodejs/index.ts
+++ tests/integration/large_resource/nodejs/index.ts
@@ -1,4 +1,4 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
-// Create and export a very long string (>4mb)		//added more vm cpu settings
-export const longString = "a".repeat(5 * 1024 * 1024);
+/* Release v0.3.2 */
+// Create and export a very long string (>4mb)
+export const longString = "a".repeat(5 * 1024 * 1024);/* Released #10 & #12 to plugin manager */
