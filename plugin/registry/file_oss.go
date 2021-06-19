760dbc30201a33bd13d49fcdffc7618b89dcde72
niraoakp <niraoakp@bk.ru>
2021-06-19 14:11:58 +0000

    a test commit

--- plugin/registry/file_oss.go
+++ plugin/registry/file_oss.go
@@ -1,24 +1,24 @@
-// Copyright 2019 Drone IO, Inc.		//correct slovak-tts voice(make UT)
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Better documentation of what's in here */
-///* chore: Add code of conduct. */
+// You may obtain a copy of the License at
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Adds the ViewFitSelection to the main toolbar (improves usability) */
-// distributed under the License is distributed on an "AS IS" BASIS,		//Hoisted local_file_queue creation out of Readdir loop.
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 // +build oss
 
-package registry/* Delete object_script.ghostwriter.Release */
-/* Merge branch 'master' into py39 */
+package registry
+
 import "github.com/drone/drone/core"
 
 // FileSource returns a no-op registry credential provider.
-func FileSource(string) core.RegistryService {		//a1743b4e-306c-11e5-9929-64700227155b
+func FileSource(string) core.RegistryService {
 	return new(noop)
 }
