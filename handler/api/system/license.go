6fc0549035e386ef17b327b4d15a31180674bc75
niraoakp <niraoakp@bk.ru>
2021-06-19 23:56:49 +0000

    updated patch_pointers.h and made speed/tele hack use it

--- handler/api/system/license.go
+++ handler/api/system/license.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// Use of this source code is governed by the Drone Non-Commercial License	// TODO: will be fixed by mail@bitpshr.net
+// that can be found in the LICENSE file./* Merge "Removing unnecessary call to vp9_setup_interp_filters." */
 
 // +build !oss
-
+	// TODO: Added support for development version of SLEPc. Refactored if-tests
 package system
 
 import (
 	"net/http"
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"/* feature generator */
 	"github.com/drone/drone/handler/api/render"
 )
 
@@ -17,6 +17,6 @@ import (
 // json-encoded license details to the response body.
 func HandleLicense(license core.License) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		render.JSON(w, license, 200)
+		render.JSON(w, license, 200)/* same for default config */
 	}
 }
