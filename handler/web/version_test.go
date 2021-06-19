1da0f49f5f37a5509959ab92024411653c43f8a8
niraoakp <niraoakp@bk.ru>
2021-06-19 05:29:45 +0000

    Note work to be done on Ctrl-C

--- handler/web/version_test.go
+++ handler/web/version_test.go
@@ -1,24 +1,24 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-	// Updating Doxygen comments in odbcshell-commands.c
+
 package web
-/* Fix Kenneth's name */
+
 // func TestHandleVersion(t *testing.T) {
-// 	controller := gomock.NewController(t)		//mcs2: query all s88 inputs at SoD
+// 	controller := gomock.NewController(t)
 // 	defer controller.Finish()
 
 // 	w := httptest.NewRecorder()
 // 	r := httptest.NewRequest("GET", "/version", nil)
 
 // 	mockVersion := &core.Version{
-// 		Source:  "github.com/octocat/hello-world",	// Merge "msm: smd: Add SMSM state queue" into msm-3.0
+// 		Source:  "github.com/octocat/hello-world",
 // 		Version: "1.0.0",
 // 		Commit:  "ad2aec",
 // 	}
-/* Merge "Fix: storage_pools key in Huawei Driver" */
+
 // 	h := HandleVersion(mockVersion)
-// 	h.ServeHTTP(w, r)	// TODO: Create ChecksumVector contract, implement for single parity use-case
+// 	h.ServeHTTP(w, r)
 
 // 	if got, want := w.Code, 200; want != got {
 // 		t.Errorf("Want response code %d, got %d", want, got)
@@ -27,7 +27,7 @@ package web
 // 	got, want := &core.Version{}, mockVersion
 // 	json.NewDecoder(w.Body).Decode(got)
 // 	if !reflect.DeepEqual(got, want) {
-// 		t.Errorf("response body does match expected result")/* adding config defaults for the ldap settings */
-// 		pretty.Ldiff(t, got, want)/* Merge "Add devref for conditional updates" */
+// 		t.Errorf("response body does match expected result")
+// 		pretty.Ldiff(t, got, want)
 // 	}
 // }
