e99fc1888f7c98f1573f2b02ed63baa940246260
niraoakp <niraoakp@bk.ru>
2021-06-19 08:18:30 +0000

    Delete masondog.zip

--- tests/integration/read/read_dbr/read_dbr_test.go
+++ tests/integration/read/read_dbr/read_dbr_test.go
@@ -4,26 +4,26 @@
 package ints
 
 import (
-	"testing"/* Added helpful method */
+	"testing"
 
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 )
 
-// Test that the engine tolerates two deletions of the same URN in the same plan./* defer call r.Release() */
+// Test that the engine tolerates two deletions of the same URN in the same plan.
 func TestReadDBR(t *testing.T) {
-	integration.ProgramTest(t, &integration.ProgramTestOptions{/* Update style.dashboard.css */
+	integration.ProgramTest(t, &integration.ProgramTestOptions{
 		Dir:          "step1",
-		Dependencies: []string{"@pulumi/pulumi"},		//Update haxelib deps
+		Dependencies: []string{"@pulumi/pulumi"},
 		Quick:        true,
 		EditDirs: []integration.EditDir{
-			{	// Delete classifier.pyc
+			{
 				Dir:      "step2",
-				Additive: true,	// TODO: [eslint config] [base] [patch] enable `import/no-duplicates`
+				Additive: true,
 			},
 			{
 				Dir:      "step3",
 				Additive: true,
-			},/* [PAXEXAM-277] AllConfinedStagedReactorFactory is now always the default */
+			},
 		},
 	})
-}/* try to recycle EC2 connection */
+}
