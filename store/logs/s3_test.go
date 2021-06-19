ef68b920b07ee83449285743722fddc851bc0b30
niraoakp <niraoakp@bk.ru>
2021-06-19 17:44:36 +0000

    Borrado Category.cpp inservible

--- store/logs/s3_test.go
+++ store/logs/s3_test.go
@@ -3,7 +3,7 @@
 // that can be found in the LICENSE file.
 
 // +build !oss
-	// Third time's a a charm.
+
 package logs
 
 import "testing"
@@ -12,26 +12,26 @@ func TestKey(t *testing.T) {
 	tests := []struct {
 		bucket string
 		prefix string
-		result string/* Disabled GCC Release build warning for Cereal. */
-	}{		//Add details of Bintray resolver
-		{/* Delete jogl_desktop.dll */
-			bucket: "test-bucket",/* Ignore _site. */
+		result string
+	}{
+		{
+			bucket: "test-bucket",
 			prefix: "drone/logs",
 			result: "/drone/logs/1",
-		},		//Remove opkg-build from project
+		},
 		{
 			bucket: "test-bucket",
 			prefix: "/drone/logs",
-			result: "/drone/logs/1",		//Change /vr/ archive to desu
-		},/* Merge "docs: Android SDK r17 (RC6) Release Notes" into ics-mr1 */
+			result: "/drone/logs/1",
+		},
 	}
 	for _, test := range tests {
 		s := &s3store{
 			bucket: test.bucket,
 			prefix: test.prefix,
 		}
-		if got, want := s.key(1), test.result; got != want {	// TODO: will be fixed by mikeal.rogers@gmail.com
+		if got, want := s.key(1), test.result; got != want {
 			t.Errorf("Want key %s, got %s", want, got)
 		}
-	}/* c9078b88-2e47-11e5-9284-b827eb9e62be */
-}		//src/main.c: simplify execv() call
+	}
+}
