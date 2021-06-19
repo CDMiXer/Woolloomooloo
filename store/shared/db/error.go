46345a28cf66131f823b30451a0080ed0bcfc036
niraoakp <niraoakp@bk.ru>
2021-06-19 07:44:55 +0000

    revert to 0.9.3.5, fixed another bug

--- store/shared/db/error.go
+++ store/shared/db/error.go
@@ -1,22 +1,22 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* modified sm sql */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0/* Release 1.0.3b */
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//hmm, forgot what I changed :)
-// See the License for the specific language governing permissions and/* Fix View Releases link */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and/* Create Mod Task List */
 // limitations under the License.
 
-package db
+package db	// Añadido Moúlo para manejo puerto serie
 
-import "errors"		//85627914-2d15-11e5-af21-0401358ea401
+import "errors"
 
 // ErrOptimisticLock is returned by if the struct being
 // modified has a Version field and the value is not equal
-// to the current value in the database	// TODO: hacked by alan.shaw@protocol.ai
+// to the current value in the database
 var ErrOptimisticLock = errors.New("Optimistic Lock Error")
