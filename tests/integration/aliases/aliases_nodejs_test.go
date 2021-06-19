95405c230b638996d190d4591cad83b646bf6ba6
niraoakp <niraoakp@bk.ru>
2021-06-19 19:15:13 +0000

    Update django-floppyforms from 1.6.1 to 1.6.2 (#17)

--- tests/integration/aliases/aliases_nodejs_test.go
+++ tests/integration/aliases/aliases_nodejs_test.go
@@ -1,29 +1,29 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
-// +build nodejs all	// TODO:  - fixed values viwing on overview screen (Eugene)
-/* 2.0 Release */
+// +build nodejs all
+
 package ints
 
 import (
 	"path/filepath"
-	"testing"/* название магазина в меню lk */
+	"testing"
 
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 )
-/* Use proper break tag */
+
 var dirs = []string{
 	"rename",
 	"adopt_into_component",
-	"rename_component_and_child",		//Replace node_js 0.7 with 0.8 .
+	"rename_component_and_child",
 	"retype_component",
 	"rename_component",
 }
 
-// TestNodejsAliases tests a case where a resource's name changes but it provides an `alias`/* Release v5.0 download link update */
+// TestNodejsAliases tests a case where a resource's name changes but it provides an `alias`
 // pointing to the old URN to ensure the resource is preserved across the update.
 func TestNodejsAliases(t *testing.T) {
 	for _, dir := range dirs {
 		d := filepath.Join("nodejs", dir)
-		t.Run(d, func(t *testing.T) {/* Refining the readme */
+		t.Run(d, func(t *testing.T) {
 			integration.ProgramTest(t, &integration.ProgramTestOptions{
 				Dir:          filepath.Join(d, "step1"),
 				Dependencies: []string{"@pulumi/pulumi"},
