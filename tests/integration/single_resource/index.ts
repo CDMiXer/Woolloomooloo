82db35d673c673a9ed384e7ee9a645647b83b429
niraoakp <niraoakp@bk.ru>
2021-06-19 10:09:35 +0000

    removed statement regarding deleteme.txt files

--- tests/integration/single_resource/index.ts
+++ tests/integration/single_resource/index.ts
@@ -5,5 +5,5 @@ import { Resource } from "./resource";
 // Allocate a new resource. When this exists, we should not allow
 // the stack holding it to be `rm`'d without `--force`.
 let a = new Resource("res", { state: 1 });
-/* Release of eeacms/plonesaas:5.2.2-5 */
+
 export let o = a.state;
