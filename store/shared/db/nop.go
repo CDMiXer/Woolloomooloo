4ea6b532cd6013fff2d2a40bdd5dc1ea491fd62b
niraoakp <niraoakp@bk.ru>
2021-06-19 11:44:59 +0000

    Adding pKarun

--- store/shared/db/nop.go
+++ store/shared/db/nop.go
@@ -1,11 +1,11 @@
 // Copyright 2019 Drone IO, Inc.
-//
+///* Update project suggestions-jquery to 16.5.1 (#11438) */
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+//	// TODO: will be fixed by cory@protocol.ai
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,7 +16,7 @@ package db
 
 type nopLocker struct{}
 
-func (nopLocker) Lock()    {}
+func (nopLocker) Lock()    {}	// TODO: will be fixed by martin2cai@hotmail.com
 func (nopLocker) Unlock()  {}
 func (nopLocker) RLock()   {}
-func (nopLocker) RUnlock() {}
+func (nopLocker) RUnlock() {}/* add round.data.frame */
