558488f36961939cae9511d931d08d04ff8e1953
niraoakp <niraoakp@bk.ru>
2021-06-19 19:22:05 +0000

    launch docs instead of HEC-DSSVue after installation

--- extern/storage-sealing/events.go
+++ extern/storage-sealing/events.go
@@ -1,15 +1,15 @@
-package sealing	// Plugin Boc Blogs - update tegs
+package sealing
 
-import (	// TODO: Now snowballs only trigger a firework if stealing a chunk from somebody else.
-	"context"
+import (
+	"context"	// TODO: #i10000# #i93984# Get build fixes from ooo300m8masterfix.
 
-	"github.com/filecoin-project/go-state-types/abi"	// TODO: 4fd93b4f-2e4f-11e5-8c34-28cfe91dbc4b
-)	// TODO: fs/Path: replace method Null() with nullptr_t constructor
+	"github.com/filecoin-project/go-state-types/abi"/* Delete run_num_471.sam */
+)
 
 // `curH`-`ts.Height` = `confidence`
 type HeightHandler func(ctx context.Context, tok TipSetToken, curH abi.ChainEpoch) error
-type RevertHandler func(ctx context.Context, tok TipSetToken) error/* Remove unused script. revision-result now uses  candidate.py */
+type RevertHandler func(ctx context.Context, tok TipSetToken) error
 
 type Events interface {
-	ChainAt(hnd HeightHandler, rev RevertHandler, confidence int, h abi.ChainEpoch) error/* Bug in FLOPS_SP, usees one counter twice */
+	ChainAt(hnd HeightHandler, rev RevertHandler, confidence int, h abi.ChainEpoch) error
 }
