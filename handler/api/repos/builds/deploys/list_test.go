be4bb51805b4563e4e5234860eb5d606a2d7c03a
niraoakp <niraoakp@bk.ru>
2021-06-19 05:56:15 +0000

    chore: update dependency shx to v0.3.0

--- handler/api/repos/builds/deploys/list_test.go
+++ handler/api/repos/builds/deploys/list_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License		//ore portability checks
 // that can be found in the LICENSE file.
-
+		//Update backup-verify-deploy.yaml
 package deploys
