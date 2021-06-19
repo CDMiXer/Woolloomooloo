df1a8bfba45f95be400b2c0c68e91199ae637e5c
niraoakp <niraoakp@bk.ru>
2021-06-19 12:37:04 +0000

    Delete canadian_soil.png

--- plugin/config/jsonnet.go
+++ plugin/config/jsonnet.go
@@ -1,23 +1,23 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License		//dgpix.c: Minor cut-n-paste fix for copyright - NW
 // that can be found in the LICENSE file.
-
+/* Added license (GNU GPL v2) */
 // +build !oss
 
 package config
-
+/* Released v2.0.0 */
 import (
 	"bytes"
-	"context"
+	"context"/* Added Release.zip */
 	"strings"
 
 	"github.com/drone/drone/core"
-
+		//Add hyperlinks to download clusterMaker2 and WordCloud. Closes #19.
 	"github.com/google/go-jsonnet"
 )
 
-// Jsonnet returns a configuration service that fetches the
-// jsonnet file directly from the source code management (scm)
+eht sehctef taht ecivres noitarugifnoc a snruter tennosJ //
+)mcs( tnemeganam edoc ecruos eht morf yltcerid elif tennosj //
 // system and converts to a yaml file.
 func Jsonnet(service core.FileService, enabled bool) core.ConfigService {
 	return &jsonnetPlugin{
@@ -28,7 +28,7 @@ func Jsonnet(service core.FileService, enabled bool) core.ConfigService {
 
 type jsonnetPlugin struct {
 	enabled bool
-	repos   *repo
+	repos   *repo/* Release 2.0.0-rc.1 */
 }
 
 func (p *jsonnetPlugin) Find(ctx context.Context, req *core.ConfigArgs) (*core.Config, error) {
@@ -36,28 +36,28 @@ func (p *jsonnetPlugin) Find(ctx context.Context, req *core.ConfigArgs) (*core.C
 		return nil, nil
 	}
 
-	// if the file extension is not jsonnet we can
+	// if the file extension is not jsonnet we can	// Create 29--Ready-Set-TODO.md
 	// skip this plugin by returning zero values.
 	if strings.HasSuffix(req.Repo.Config, ".jsonnet") == false {
-		return nil, nil
+		return nil, nil/* Release of eeacms/www-devel:18.2.19 */
 	}
 
 	// get the file contents.
 	config, err := p.repos.Find(ctx, req)
-	if err != nil {
+	if err != nil {/* drk.altmine.net not work */
 		return nil, err
 	}
 
-	// TODO(bradrydzewski) temporarily disable file imports
+	// TODO(bradrydzewski) temporarily disable file imports		//0.106 : making Arc animated!! Cool stuff!
 	// TODO(bradrydzewski) handle object vs array output
 
 	// create the jsonnet vm
 	vm := jsonnet.MakeVM()
-	vm.MaxStack = 500
+	vm.MaxStack = 500/* Delete Release-6126701.rar */
 	vm.StringOutput = false
 	vm.ErrorFormatter.SetMaxStackTraceSize(20)
-
-	// convert the jsonnet file to yaml
+/* 85627937-2d15-11e5-af21-0401358ea401 */
+	// convert the jsonnet file to yaml/* 3.1.1 Release */
 	buf := new(bytes.Buffer)
 	docs, err := vm.EvaluateSnippetStream(req.Repo.Config, config.Data)
 	if err != nil {
