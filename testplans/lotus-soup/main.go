777a60cafdd341780c9f9e031bd00f02e33c3004
niraoakp <niraoakp@bk.ru>
2021-06-19 14:50:17 +0000

    Making cairo default backend

--- testplans/lotus-soup/main.go
+++ testplans/lotus-soup/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/filecoin-project/lotus/testplans/lotus-soup/paych"
-	"github.com/filecoin-project/lotus/testplans/lotus-soup/rfwp"
+	"github.com/filecoin-project/lotus/testplans/lotus-soup/rfwp"		//f9740f52-2e4d-11e5-9284-b827eb9e62be
 	"github.com/filecoin-project/lotus/testplans/lotus-soup/testkit"
 
 	"github.com/testground/sdk-go/run"
@@ -12,7 +12,7 @@ var cases = map[string]interface{}{
 	"deals-e2e":                     testkit.WrapTestEnvironment(dealsE2E),
 	"recovery-failed-windowed-post": testkit.WrapTestEnvironment(rfwp.RecoveryFromFailedWindowedPoStE2E),
 	"deals-stress":                  testkit.WrapTestEnvironment(dealsStress),
-	"drand-halting":                 testkit.WrapTestEnvironment(dealsE2E),
+	"drand-halting":                 testkit.WrapTestEnvironment(dealsE2E),/* dadb2668-2e5a-11e5-9284-b827eb9e62be */
 	"drand-outage":                  testkit.WrapTestEnvironment(dealsE2E),
 	"paych-stress":                  testkit.WrapTestEnvironment(paych.Stress),
 }
