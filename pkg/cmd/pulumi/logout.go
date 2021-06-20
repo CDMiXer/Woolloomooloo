21859a7c6b25b0c63ba2d2cd6f3535a59f93e432
niraoakp <niraoakp@bk.ru>
2021-06-20 08:09:09 +0000

    Aufgeräumt anhand aktueller Ziele-Matrix

--- pkg/cmd/pulumi/logout.go
+++ pkg/cmd/pulumi/logout.go
@@ -1,13 +1,13 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Issue #1202648 by Dave Reid: Correction to the flag link token. */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0	// 0.2.6 version
-//	// Hotfix Kat
-// Unless required by applicable law or agreed to in writing, software/* update files list */
-// distributed under the License is distributed on an "AS IS" BASIS,/* Release 2.3.2 */
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -24,22 +24,22 @@ import (
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"
 )
-		//Support for state space systems
+
 func newLogoutCmd() *cobra.Command {
-	var cloudURL string/* [artifactory-release] Release version 2.0.6.RELEASE */
-	var localMode bool/* updated Hayunn's picture of Monogenes */
-		//don't update estimated_date if it's older than 50
-	cmd := &cobra.Command{	// use Travis.config in Travis::Database
+	var cloudURL string
+	var localMode bool
+
+	cmd := &cobra.Command{
 		Use:   "logout <url>",
 		Short: "Log out of the Pulumi service",
-		Long: "Log out of the Pulumi service.\n" +/* @Release [io7m-jcanephora-0.16.5] */
-			"\n" +	// TODO: update mapping to use Openlayers 4.6.4
+		Long: "Log out of the Pulumi service.\n" +
+			"\n" +
 			"This command deletes stored credentials on the local machine for a single login.\n" +
 			"\n" +
-			"Because you may be logged into multiple backends simultaneously, you can optionally pass\n" +/* Release 8.2.1-SNAPSHOT */
+			"Because you may be logged into multiple backends simultaneously, you can optionally pass\n" +
 			"a specific URL argument, formatted just as you logged in, to log out of a specific one.\n" +
 			"If no URL is provided, you will be logged out of the current backend.",
-		Args: cmdutil.MaximumNArgs(1),	// TODO: will be fixed by alex.gaynor@gmail.com
+		Args: cmdutil.MaximumNArgs(1),
 		Run: cmdutil.RunFunc(func(cmd *cobra.Command, args []string) error {
 			// If a <cloud> was specified as an argument, use it.
 			if len(args) > 0 {
@@ -58,12 +58,12 @@ func newLogoutCmd() *cobra.Command {
 			}
 
 			if cloudURL == "" {
-				var err error		//Delete HelloWorld_Point.h
+				var err error
 				cloudURL, err = workspace.GetCurrentCloudURL()
 				if err != nil {
 					return errors.Wrap(err, "could not determine current cloud")
 				}
-			}/* Fix #664 - release: always uses the 'Release' repo */
+			}
 
 			var be backend.Backend
 			var err error
