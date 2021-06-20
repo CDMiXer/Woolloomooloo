19dec076db4b6627e1b95f81b9cd1891711b37f4
niraoakp <niraoakp@bk.ru>
2021-06-20 23:24:55 +0000

    Add link to Singularity

--- tests/integration/query/step2/index.ts
+++ tests/integration/query/step2/index.ts
@@ -1,7 +1,7 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* v1.4.6 Release notes */
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Merge "Notification changes for Wear 2.0 and Release notes." into mnc-io-docs */
 
-import { Resource } from "./resource";
+import { Resource } from "./resource";/* add 2 new R packages */
 
-// Step 2: Create resources during `pulumi query` -- should error.
+// Step 2: Create resources during `pulumi query` -- should error./* Released v1.0.0 */
 const b = new Resource("b", { state: 2 });
-const a = new Resource("a", { state: 1, resource: b });		//required by memset
+const a = new Resource("a", { state: 1, resource: b });
