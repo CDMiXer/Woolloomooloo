bfab00d33703521c4e01f9b32ddb14a822e9f7b9
niraoakp <niraoakp@bk.ru>
2021-06-20 02:25:25 +0000

    add automatic handling of $$ and $# entries.

--- pkg/codegen/internal/test/testdata/aws-s3-logging.pp.ts
+++ pkg/codegen/internal/test/testdata/aws-s3-logging.pp.ts
@@ -1,7 +1,7 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 
-const logs = new aws.s3.Bucket("logs", {});	// TODO: will be fixed by 13860583249@yeah.net
+const logs = new aws.s3.Bucket("logs", {});
 const bucket = new aws.s3.Bucket("bucket", {loggings: [{
     targetBucket: logs.bucket,
 }]});
