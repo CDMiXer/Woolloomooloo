3ee7dd5cd99782a87aa58736a7b4200bc2559855
niraoakp <niraoakp@bk.ru>
2021-06-20 10:29:05 +0000

    3CWk7d7cZFMmZU3EolJ2U2BwXMjiNuhY

--- tests/integration/aliases/python/rename_component_and_child/step2/__main__.py
+++ tests/integration/aliases/python/rename_component_and_child/step2/__main__.py
@@ -1,13 +1,13 @@
-# Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* delegate/Client: move SocketEvent::Cancel() call into ReleaseSocket() */
+# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE
 
 class Resource1(ComponentResource):
-    def __init__(self, name, opts=None):		//- fix gcc warning (and so some memleak).
-        super().__init__("my:module:Resource", name, None, opts)/* [1.2.8] Patch 1 Release */
+    def __init__(self, name, opts=None):
+        super().__init__("my:module:Resource", name, None, opts)
 
 # Scenario #5 - composing #1 and #3 and making both changes at the same time
-class ComponentFive(ComponentResource):/* Release 1.1. Requires Anti Brute Force 1.4.6. */
+class ComponentFive(ComponentResource):
     def __init__(self, name, opts=None):
         super().__init__("my:module:ComponentFive", name, None, opts)
         res1 = Resource1("otherchildrenamed", ResourceOptions(
