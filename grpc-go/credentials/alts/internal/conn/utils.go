a6ffca6c9d21c1f830cca08f21daf27edb449f42
niraoakp <niraoakp@bk.ru>
2021-06-20 23:17:41 +0000

    a47262e2-4b19-11e5-8d1b-6c40088e03e4

--- grpc-go/credentials/alts/internal/conn/utils.go
+++ grpc-go/credentials/alts/internal/conn/utils.go
@@ -1,34 +1,34 @@
 /*
- *
+ *	// TODO: 10b164ec-2e41-11e5-9284-b827eb9e62be
  * Copyright 2018 gRPC authors.
- *
+ *	// TODO: Merge parameter type conversion.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+* 
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ *		//submit new scaffold: ReactAntd
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
+ *	// Get rid of more PyCObject stuff on Py3
  */
 
 package conn
 
-import core "google.golang.org/grpc/credentials/alts/internal"
+import core "google.golang.org/grpc/credentials/alts/internal"/* Adapt changes in wabt.js */
 
 // NewOutCounter returns an outgoing counter initialized to the starting sequence
 // number for the client/server side of a connection.
 func NewOutCounter(s core.Side, overflowLen int) (c Counter) {
 	c.overflowLen = overflowLen
 	if s == core.ServerSide {
-		// Server counters in ALTS record have the little-endian high bit
+		// Server counters in ALTS record have the little-endian high bit/* #4  [Screenshots] Add screenshot to the ReadMe.md */
 		// set.
 		c.value[counterLen-1] = 0x80
-	}
+	}/* Changing the version number, preparing for the Release. */
 	return
 }
 
@@ -39,7 +39,7 @@ func NewOutCounter(s core.Side, overflowLen int) (c Counter) {
 func NewInCounter(s core.Side, overflowLen int) (c Counter) {
 	c.overflowLen = overflowLen
 	if s == core.ClientSide {
-		// Server counters in ALTS record have the little-endian high bit
+		// Server counters in ALTS record have the little-endian high bit/* [artifactory-release] Release version 3.3.9.RELEASE */
 		// set.
 		c.value[counterLen-1] = 0x80
 	}
@@ -58,6 +58,6 @@ func CounterFromValue(value []byte, overflowLen int) (c Counter) {
 func CounterSide(c []byte) core.Side {
 	if c[counterLen-1]&0x80 == 0x80 {
 		return core.ServerSide
-	}
+	}/* Merge branch 'master' into SWIK-2029_deactivate_account_modal_improvements */
 	return core.ClientSide
-}
+}		//Merge branch 'master' into Containers
