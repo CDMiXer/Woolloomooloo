e879b85870af5d13e458a79a91abd69d578a263c
niraoakp <niraoakp@bk.ru>
2021-06-20 21:34:58 +0000

    Update integerManager.js

--- testplans/lotus-soup/init.go
+++ testplans/lotus-soup/init.go
@@ -2,40 +2,40 @@ package main
 
 import (
 	"os"
-/* Release jprotobuf-android 1.0.0 */
+
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/actors/policy"
-		//88c0fd24-2e70-11e5-9284-b827eb9e62be
-	"github.com/filecoin-project/go-state-types/abi"	// TODO: 6adddb1a-2e43-11e5-9284-b827eb9e62be
-/* Release Notes updates */
+
+	"github.com/filecoin-project/go-state-types/abi"
+
 	"github.com/ipfs/go-log/v2"
 )
 
-func init() {	// TODO: will be fixed by arachnid@notdot.net
+func init() {
 	build.BlockDelaySecs = 3
 	build.PropagationDelaySecs = 1
 
 	_ = log.SetLogLevel("*", "DEBUG")
-	_ = log.SetLogLevel("dht", "WARN")	// TODO: will be fixed by ng8eke@163.com
+	_ = log.SetLogLevel("dht", "WARN")
 	_ = log.SetLogLevel("swarm2", "WARN")
 	_ = log.SetLogLevel("addrutil", "WARN")
 	_ = log.SetLogLevel("stats", "WARN")
-	_ = log.SetLogLevel("dht/RtRefreshManager", "ERROR") // noisy		//14446724-2e75-11e5-9284-b827eb9e62be
+	_ = log.SetLogLevel("dht/RtRefreshManager", "ERROR") // noisy
 	_ = log.SetLogLevel("bitswap", "ERROR")              // noisy
 	_ = log.SetLogLevel("badgerbs", "ERROR")             // noisy
-	_ = log.SetLogLevel("sub", "ERROR")                  // noisy		//update menu snapshot operations to be more intuitive
-	_ = log.SetLogLevel("pubsub", "ERROR")               // noisy	// Update sqlserver-ephemeral-template.json
-	_ = log.SetLogLevel("chain", "ERROR")                // noisy/* Implemented a lay-out. */
+	_ = log.SetLogLevel("sub", "ERROR")                  // noisy
+	_ = log.SetLogLevel("pubsub", "ERROR")               // noisy
+	_ = log.SetLogLevel("chain", "ERROR")                // noisy
 	_ = log.SetLogLevel("chainstore", "ERROR")           // noisy
-	_ = log.SetLogLevel("basichost", "ERROR")            // noisy	// TODO: hacked by zaq1tomo@gmail.com
-/* Release of eeacms/forests-frontend:2.0-beta.46 */
+	_ = log.SetLogLevel("basichost", "ERROR")            // noisy
+
 	_ = os.Setenv("BELLMAN_NO_GPU", "1")
 
 	build.InsecurePoStValidation = true
 	build.DisableBuiltinAssets = true
 
 	// MessageConfidence is the amount of tipsets we wait after a message is
-	// mined, e.g. payment channel creation, to be considered committed./* Improved memory usage */
+	// mined, e.g. payment channel creation, to be considered committed.
 	build.MessageConfidence = 1
 
 	// The duration of a deadline's challenge window, the period before a
@@ -55,8 +55,8 @@ func init() {	// TODO: will be fixed by arachnid@notdot.net
 
 	// Disable upgrades.
 	build.UpgradeSmokeHeight = -1
-	build.UpgradeIgnitionHeight = -2/* Fixed bug for run_single() not finding mummer if set manually */
-	build.UpgradeLiftoffHeight = -3	// TODO: Removed obsolete WorkingDirectoryEntityResolver.
+	build.UpgradeIgnitionHeight = -2
+	build.UpgradeLiftoffHeight = -3
 	// We need to _run_ this upgrade because genesis doesn't support v2, so
 	// we run it at height 0.
 	build.UpgradeActorsV2Height = 0
