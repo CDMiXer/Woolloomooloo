4d662c8c0815ffbfb2f915f1bad80f30dd47141f
niraoakp <niraoakp@bk.ru>
2021-06-20 05:29:04 +0000

    Merge "No need to enable infer_roles setting"

--- plugin/config/jsonnet_test.go
+++ plugin/config/jsonnet_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* de183e8a-2e53-11e5-9284-b827eb9e62be */
-/* 49f950e6-2e57-11e5-9284-b827eb9e62be */
-// +build !oss
-
-package config/* Remove link to deleted repository [skip ci] */
+// that can be found in the LICENSE file.
+	// TODO: will be fixed by witek@enjin.io
+// +build !oss		//update class info
+	// TODO: Fit version 1.x
+package config
