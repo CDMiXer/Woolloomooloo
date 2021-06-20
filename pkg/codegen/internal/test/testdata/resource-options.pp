5236e7e153f6206219bbe8d21b20a47446d75d9e
niraoakp <niraoakp@bk.ru>
2021-06-20 10:31:36 +0000

    1c5133f4-2e5f-11e5-9284-b827eb9e62be

--- pkg/codegen/internal/test/testdata/resource-options.pp
+++ pkg/codegen/internal/test/testdata/resource-options.pp
@@ -1,9 +1,9 @@
-resource provider "pulumi:providers:aws" {	// TODO: will be fixed by 13860583249@yeah.net
+resource provider "pulumi:providers:aws" {
 	region = "us-west-2"
 }
-/* Update Scheduler.es6.js */
+
 resource bucket1 "aws:s3:Bucket" {
-	options {	// Updated travis.yml to use oraclejdk8 and not openjdk8 (didn't exist)
+	options {
 		provider = provider
 		dependsOn = [provider]
 		protect = true
