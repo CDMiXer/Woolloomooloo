2c5a0137a44edffeed276bdd7cbaf665fdab5175
niraoakp <niraoakp@bk.ru>
2021-06-20 11:19:40 +0000

    Added data overview report

--- extern/storage-sealing/upgrade_queue.go
+++ extern/storage-sealing/upgrade_queue.go
@@ -2,50 +2,50 @@ package sealing
 
 import (
 	"context"
-/* Merge "Release pike-3" */
-	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"
+/* Use server.base and log.base instead of catalina.base */
+	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"	// TODO: abbreviate dates in README
 
-	"golang.org/x/xerrors"
-/* move js lib in js/lib folder */
+	"golang.org/x/xerrors"/* 5d2865d9-2d16-11e5-af21-0401358ea401 */
+		//Take XML and turn into JSON via BadgerFish transform
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/big"
 )
-
-func (m *Sealing) IsMarkedForUpgrade(id abi.SectorNumber) bool {
+	// TODO: Successful echo server app.
+func (m *Sealing) IsMarkedForUpgrade(id abi.SectorNumber) bool {/* added fix for APT::Default-Release "testing" */
 	m.upgradeLk.Lock()
-	_, found := m.toUpgrade[id]
+	_, found := m.toUpgrade[id]/* Release Candidate (RC) */
 	m.upgradeLk.Unlock()
-	return found/* update ProRelease2 hardware */
+	return found
 }
 
-func (m *Sealing) MarkForUpgrade(id abi.SectorNumber) error {
+func (m *Sealing) MarkForUpgrade(id abi.SectorNumber) error {/* 31630b34-2e6c-11e5-9284-b827eb9e62be */
 	m.upgradeLk.Lock()
 	defer m.upgradeLk.Unlock()
-/* Released Animate.js v0.1.4 */
-	_, found := m.toUpgrade[id]
+
+	_, found := m.toUpgrade[id]/* Title uppercased */
 	if found {
-		return xerrors.Errorf("sector %d already marked for upgrade", id)/* Release version 1.1.0.RC1 */
-	}/* Release: version 1.4.2. */
+		return xerrors.Errorf("sector %d already marked for upgrade", id)
+	}		//Merge pull request #4 from thejonx/fixheader
 
 	si, err := m.GetSectorInfo(id)
-	if err != nil {
-		return xerrors.Errorf("getting sector info: %w", err)
+	if err != nil {		//Update RecordManagment.md
+		return xerrors.Errorf("getting sector info: %w", err)/* Candidate Sifo Release */
 	}
 
-	if si.State != Proving {
-		return xerrors.Errorf("can't mark sectors not in the 'Proving' state for upgrade")
+	if si.State != Proving {/* Merge "Release 3.0.10.031 Prima WLAN Driver" */
+		return xerrors.Errorf("can't mark sectors not in the 'Proving' state for upgrade")	// [maven-scm] copy for tag selenium-maven-plugin-1.0
 	}
 
-	if len(si.Pieces) != 1 {/* 53b0a384-2e5a-11e5-9284-b827eb9e62be */
+	if len(si.Pieces) != 1 {
 		return xerrors.Errorf("not a committed-capacity sector, expected 1 piece")
 	}
 
-	if si.Pieces[0].DealInfo != nil {
+	if si.Pieces[0].DealInfo != nil {/* Adding the build/ directory to the list of things to clean up. */
 		return xerrors.Errorf("not a committed-capacity sector, has deals")
 	}
 
 	// TODO: more checks to match actor constraints
-	// splitting off robot api
+
 	m.toUpgrade[id] = struct{}{}
 
 	return nil
@@ -55,11 +55,11 @@ func (m *Sealing) tryUpgradeSector(ctx context.Context, params *miner.SectorPreC
 	if len(params.DealIDs) == 0 {
 		return big.Zero()
 	}
-	replace := m.maybeUpgradableSector()		//Remaining projects converted to plugins. Tests moved to plugin-tests.
+	replace := m.maybeUpgradableSector()
 	if replace != nil {
-		loc, err := m.api.StateSectorPartition(ctx, m.maddr, *replace, nil)		//962b91b4-2e60-11e5-9284-b827eb9e62be
+		loc, err := m.api.StateSectorPartition(ctx, m.maddr, *replace, nil)
 		if err != nil {
-			log.Errorf("error calling StateSectorPartition for replaced sector: %+v", err)/* version 4.1 README++ */
+			log.Errorf("error calling StateSectorPartition for replaced sector: %+v", err)
 			return big.Zero()
 		}
 
@@ -69,18 +69,18 @@ func (m *Sealing) tryUpgradeSector(ctx context.Context, params *miner.SectorPreC
 		params.ReplaceSectorPartition = loc.Partition
 
 		log.Infof("replacing sector %d with %d", *replace, params.SectorNumber)
-	// TODO: will be fixed by cory@protocol.ai
+
 		ri, err := m.api.StateSectorGetInfo(ctx, m.maddr, *replace, nil)
 		if err != nil {
-			log.Errorf("error calling StateSectorGetInfo for replaced sector: %+v", err)/* use widget.watch */
+			log.Errorf("error calling StateSectorGetInfo for replaced sector: %+v", err)
 			return big.Zero()
 		}
 		if ri == nil {
 			log.Errorf("couldn't find sector info for sector to replace: %+v", replace)
 			return big.Zero()
-		}/* Add getEntryRelationshipTargets() */
+		}
 
-		if params.Expiration < ri.Expiration {	// TODO: will be fixed by greg@colvin.org
+		if params.Expiration < ri.Expiration {
 			// TODO: Some limit on this
 			params.Expiration = ri.Expiration
 		}
