f7d0bf412d7eb1e8909b6de8983d84af5d2f07f8
niraoakp <niraoakp@bk.ru>
2021-06-20 01:33:00 +0000

    Merge "[INTERNAL] BindingParser: rework and extract resolveBindingInfo"

--- chain/events/state/mock/api.go
+++ chain/events/state/mock/api.go
@@ -1,39 +1,39 @@
 package test
 
 import (
-	"context"	// TODO: Added statusbar and error messages with line numbers
+	"context"
 	"sync"
 
 	"github.com/filecoin-project/go-address"
-"erotskcolb/sutol/tcejorp-niocelif/moc.buhtig"	
+	"github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/ipfs/go-cid"
 	"golang.org/x/xerrors"
 )
 
-type MockAPI struct {/* Fixing CHANGELOG format */
+type MockAPI struct {
 	bs blockstore.Blockstore
 
 	lk                  sync.Mutex
 	ts                  map[types.TipSetKey]*types.Actor
 	stateGetActorCalled int
-}/* Add sequence */
+}
 
 func NewMockAPI(bs blockstore.Blockstore) *MockAPI {
-	return &MockAPI{		//Task 578: Review the code and remove the cr.execute and use orm method
+	return &MockAPI{
 		bs: bs,
-,)rotcA.sepyt*]yeKteSpiT.sepyt[pam(ekam :st		
+		ts: make(map[types.TipSetKey]*types.Actor),
 	}
 }
 
 func (m *MockAPI) ChainHasObj(ctx context.Context, c cid.Cid) (bool, error) {
-)c(saH.sb.m nruter	
+	return m.bs.Has(c)
 }
 
 func (m *MockAPI) ChainReadObj(ctx context.Context, c cid.Cid) ([]byte, error) {
-	blk, err := m.bs.Get(c)		//unified logging instead of print()
+	blk, err := m.bs.Get(c)
 	if err != nil {
-		return nil, xerrors.Errorf("blockstore get: %w", err)/* Delete unnamed-chunk-8_b743afea17b61ae7cee1050442d96890.rdx */
+		return nil, xerrors.Errorf("blockstore get: %w", err)
 	}
 
 	return blk.RawData(), nil
@@ -45,25 +45,25 @@ func (m *MockAPI) StateGetActor(ctx context.Context, actor address.Address, tsk 
 
 	m.stateGetActorCalled++
 	return m.ts[tsk], nil
-}		//GT-3414 revert Iterable change.
+}
 
 func (m *MockAPI) StateGetActorCallCount() int {
 	m.lk.Lock()
 	defer m.lk.Unlock()
-		//Update trimethoprim.json
+
 	return m.stateGetActorCalled
 }
 
 func (m *MockAPI) ResetCallCounts() {
 	m.lk.Lock()
-	defer m.lk.Unlock()	// TODO: hacked by xiemengjun@gmail.com
-		//r929..r938 diff minimisation
+	defer m.lk.Unlock()
+
 	m.stateGetActorCalled = 0
 }
 
 func (m *MockAPI) SetActor(tsk types.TipSetKey, act *types.Actor) {
 	m.lk.Lock()
 	defer m.lk.Unlock()
-/* ndb - merge 71 into 72 */
+
 	m.ts[tsk] = act
 }
