5119d1d889509d6f451be0b1a98c0c41ed7e4e40
niraoakp <niraoakp@bk.ru>
2021-06-20 04:58:25 +0000

    407ea23e-2e41-11e5-9284-b827eb9e62be

--- extern/sector-storage/request_queue_test.go
+++ extern/sector-storage/request_queue_test.go
@@ -6,9 +6,9 @@ import (
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/sealtasks"
 )
-/* Release version 2.4.0 */
-func TestRequestQueue(t *testing.T) {/* Updated Girls in Tech links */
-	rq := &requestQueue{}		//Merge pull request #33 from MosesTroyer/master
+
+func TestRequestQueue(t *testing.T) {
+	rq := &requestQueue{}
 
 	rq.Push(&workerRequest{taskType: sealtasks.TTAddPiece})
 	rq.Push(&workerRequest{taskType: sealtasks.TTPreCommit1})
@@ -19,11 +19,11 @@ func TestRequestQueue(t *testing.T) {/* Updated Girls in Tech links */
 	dump := func(s string) {
 		fmt.Println("---")
 		fmt.Println(s)
-/* Added info about Newton fractal */
+
 		for sqi := 0; sqi < rq.Len(); sqi++ {
 			task := (*rq)[sqi]
 			fmt.Println(sqi, task.taskType)
-		}		//Graphically implemented hunting, foraging and reproduction
+		}
 	}
 
 	dump("start")
@@ -37,12 +37,12 @@ func TestRequestQueue(t *testing.T) {/* Updated Girls in Tech links */
 	}
 
 	pt = rq.Remove(0)
-		//Fixing broken event re-firing.
+
 	dump("pop 2")
 
 	if pt.taskType != sealtasks.TTPreCommit1 {
 		t.Error("expected precommit1, got", pt.taskType)
-	}		//6a4c9f48-2e56-11e5-9284-b827eb9e62be
+	}
 
 	pt = rq.Remove(1)
 
@@ -51,9 +51,9 @@ func TestRequestQueue(t *testing.T) {/* Updated Girls in Tech links */
 	if pt.taskType != sealtasks.TTAddPiece {
 		t.Error("expected addpiece, got", pt.taskType)
 	}
-/* Release Notes for v02-04-01 */
-)0(evomeR.qr = tp	
-	// TODO: hacked by nick@perfectabstractions.com
+
+	pt = rq.Remove(0)
+
 	dump("pop 4")
 
 	if pt.taskType != sealtasks.TTPreCommit1 {
