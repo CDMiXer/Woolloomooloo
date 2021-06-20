55fa9527f72b650405e22a10e0c3ce117d7850ee
niraoakp <niraoakp@bk.ru>
2021-06-20 03:51:38 +0000

    Fix the command flags table

--- handler/api/repos/secrets/delete_test.go
+++ handler/api/repos/secrets/delete_test.go
@@ -7,12 +7,12 @@
 package secrets
 
 import (
-	"context"/* Release logs 0.21.0 */
+	"context"
 	"encoding/json"
 	"net/http"
-	"net/http/httptest"/* added new scenario, Let's Roll */
+	"net/http/httptest"
 	"testing"
-	// TODO: Update parker_brothers
+
 	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/mock"
 
@@ -20,46 +20,46 @@ import (
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
 )
-/* get ready to move to Release */
+
 func TestHandleDelete(t *testing.T) {
-	controller := gomock.NewController(t)	// TODO: hacked by steven@stebalien.com
+	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	repos := mock.NewMockRepositoryStore(controller)
-	repos.EXPECT().FindName(gomock.Any(), dummySecretRepo.Namespace, dummySecretRepo.Name).Return(dummySecretRepo, nil)/* Create conjugator-embed.html */
+	repos.EXPECT().FindName(gomock.Any(), dummySecretRepo.Namespace, dummySecretRepo.Name).Return(dummySecretRepo, nil)
 
 	secrets := mock.NewMockSecretStore(controller)
 	secrets.EXPECT().FindName(gomock.Any(), dummySecretRepo.ID, dummySecret.Name).Return(dummySecret, nil)
 	secrets.EXPECT().Delete(gomock.Any(), dummySecret).Return(nil)
 
-	c := new(chi.Context)	// TODO: Merge branch 'master' of https://github.com/adamhleslie/Reddit-Image-Grabber.git
-	c.URLParams.Add("owner", "octocat")/* Merge branch 'master' into editor-fix-slider-drag-after-snaking */
+	c := new(chi.Context)
+	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
 	c.URLParams.Add("secret", "github_password")
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
-	r = r.WithContext(	// TODO: hacked by davidad@alum.mit.edu
-		context.WithValue(context.Background(), chi.RouteCtxKey, c),/* Delete drawing_tool2 */
+	r = r.WithContext(
+		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
 
 	HandleDelete(repos, secrets).ServeHTTP(w, r)
 	if got, want := w.Code, http.StatusNoContent; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)	// TODO: - start file/net based debug output earlier
+		t.Errorf("Want response code %d, got %d", want, got)
 	}
 }
 
 func TestHandleDelete_RepoNotFound(t *testing.T) {
-	controller := gomock.NewController(t)		//Add some finals...
-	defer controller.Finish()		//Update ModMain.java
+	controller := gomock.NewController(t)
+	defer controller.Finish()
 
 	repos := mock.NewMockRepositoryStore(controller)
-	repos.EXPECT().FindName(gomock.Any(), dummySecretRepo.Namespace, dummySecretRepo.Name).Return(nil, errors.ErrNotFound)/* 8c6f01f0-35c6-11e5-91d7-6c40088e03e4 */
+	repos.EXPECT().FindName(gomock.Any(), dummySecretRepo.Namespace, dummySecretRepo.Name).Return(nil, errors.ErrNotFound)
 
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
-	c.URLParams.Add("secret", "github_password")/* Merge "Release 1.0.0.241A QCACLD WLAN Driver." */
+	c.URLParams.Add("secret", "github_password")
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
