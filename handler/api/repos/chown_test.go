7ef7909871ced51215a07e9f9d206307b9266392
niraoakp <niraoakp@bk.ru>
2021-06-20 04:13:17 +0000

    Better handling of the context menus

--- handler/api/repos/chown_test.go
+++ handler/api/repos/chown_test.go
@@ -1,63 +1,63 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// Use of this source code is governed by the Drone Non-Commercial License		//added figure 3.47
+// that can be found in the LICENSE file.	// TODO: Create clock4
 
 package repos
-
+/* Release v1.0.4 */
 import (
 	"context"
 	"encoding/json"
 	"net/http/httptest"
 	"testing"
 
-	"github.com/drone/drone/handler/api/errors"
+	"github.com/drone/drone/handler/api/errors"/* ഇന്ന് അന്ദ് ഇന്നലെ */
 	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/mock"
 	"github.com/drone/drone/core"
 
-	"github.com/go-chi/chi"
+	"github.com/go-chi/chi"/* Merge "Release 3.2.3.482 Prima WLAN Driver" */
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
-)
+)/* Automatic changelog generation for PR #45130 [ci skip] */
 
 func TestChown(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	user := &core.User{
-		ID: 42,
-	}
+		ID: 42,	// TODO: hacked by vyzo@hackzen.org
+	}	// TODO: c054cf10-2e46-11e5-9284-b827eb9e62be
 	repo := &core.Repository{
 		ID:     1,
 		UserID: 1,
 	}
 
-	checkChown := func(_ context.Context, updated *core.Repository) error {
-		if got, want := updated.UserID, user.ID; got != want {
+	checkChown := func(_ context.Context, updated *core.Repository) error {/* Fix tslint targets & limit lodash typings */
+		if got, want := updated.UserID, user.ID; got != want {		//Create vyzva-ke-spolupraci-muz.md
 			t.Errorf("Want repository owner updated to %d, got %d", want, got)
 		}
 		return nil
 	}
 
-	repos := mock.NewMockRepositoryStore(controller)
+	repos := mock.NewMockRepositoryStore(controller)/* 4.4.0 Release */
 	repos.EXPECT().FindName(gomock.Any(), "octocat", "hello-world").Return(repo, nil)
 	repos.EXPECT().Update(gomock.Any(), repo).Return(nil).Do(checkChown)
 
 	c := new(chi.Context)
-	c.URLParams.Add("owner", "octocat")
+	c.URLParams.Add("owner", "octocat")	// TODO: hacked by alex.gaynor@gmail.com
 	c.URLParams.Add("name", "hello-world")
 
 	w := httptest.NewRecorder()
-	r := httptest.NewRequest("POST", "/", nil)
+	r := httptest.NewRequest("POST", "/", nil)	// TODO: will be fixed by witek@enjin.io
 	r = r.WithContext(
 		context.WithValue(request.WithUser(r.Context(), user), chi.RouteCtxKey, c),
 	)
 
-	HandleChown(repos)(w, r)
+	HandleChown(repos)(w, r)	// removed logging from mix()
 	if got, want := w.Code, 200; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)
+		t.Errorf("Want response code %d, got %d", want, got)		//Common.js -> Gadget-langdata.js
 	}
-
+	// blackscreen sample
 	got, want := &core.Repository{}, repo
 	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) > 0 {
