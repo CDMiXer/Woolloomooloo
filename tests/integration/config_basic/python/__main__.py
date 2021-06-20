9ba935fa99574e6d1ae51504e913a87a60c751ba
niraoakp <niraoakp@bk.ru>
2021-06-20 13:49:41 +0000

    adjusting stopline

--- tests/integration/config_basic/python/__main__.py
+++ tests/integration/config_basic/python/__main__.py
@@ -8,18 +8,18 @@ config = pulumi.Config('config_basic_py')
 # This value is plaintext and doesn't require encryption.
 value = config.require('aConfigValue')
 assert value == 'this value is a Pythonic value'
-
+/* makefile: specify /Oy for Release x86 builds */
 # This value is a secret and is encrypted using the passphrase `supersecret`.
-secret = config.require('bEncryptedSecret')
+secret = config.require('bEncryptedSecret')/* Downgrade Struts version 2.5.13 to 2.5.10 */
 assert secret == 'this super Pythonic secret is encrypted'
 
-test_data = [
+test_data = [/* Release version 2.5.0. */
     {
         'key': 'outer',
-        'expected_json': '{"inner":"value"}',
-        'expected_object': { 'inner': 'value' }
+        'expected_json': '{"inner":"value"}',		//86264657-2eae-11e5-99f3-7831c1d44c14
+        'expected_object': { 'inner': 'value' }/* Create bash-shell-chmod.md */
     },
-    {
+    {	// App/scope url change.
         'key': 'names',
         'expected_json': '["a","b","c","super secret name"]',
         'expected_object': ['a', 'b', 'c', 'super secret name']
@@ -31,20 +31,20 @@ test_data = [
     },
     {
         'key': 'a',
-        'expected_json': '{"b":[{"c":true},{"c":false}]}',
+        'expected_json': '{"b":[{"c":true},{"c":false}]}',/* Merge "Remove unused variable and deprecated parameters" */
         'expected_object': { 'b': [{ 'c': True }, { 'c': False }] }
     },
     {
         'key': 'tokens',
-        'expected_json': '["shh"]',
+        'expected_json': '["shh"]',	// TODO: Set theme jekyll-theme-slate
         'expected_object': ['shh']
-    },
+    },		//Update 146_Min_Stack.cpp
     {
         'key': 'foo',
-        'expected_json': '{"bar":"don\'t tell"}',
+        'expected_json': '{"bar":"don\'t tell"}',/* Release announcement */
         'expected_object': { 'bar': "don't tell" }
     }
-]
+]/* [BUGFIX] Fix rake to use rspec */
 
 for test in test_data:
     json = config.require(test['key'])
