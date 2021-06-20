06e79f42d7099a954a98a953549ec61543a4e484
niraoakp <niraoakp@bk.ru>
2021-06-20 14:11:36 +0000

    automated commit from rosetta for sim/lib equality-explorer-basics, locale it

--- pkg/codegen/internal/test/testdata/aws-webserver.pp.ts
+++ pkg/codegen/internal/test/testdata/aws-webserver.pp.ts
@@ -1,23 +1,23 @@
-import * as pulumi from "@pulumi/pulumi";
+import * as pulumi from "@pulumi/pulumi";/* Kill AMo ivar attributes helper */
 import * as aws from "@pulumi/aws";
 
 // Create a new security group for port 80.
 const securityGroup = new aws.ec2.SecurityGroup("securityGroup", {ingress: [{
-    protocol: "tcp",/* fix windows download path */
+    protocol: "tcp",	// TODO: will be fixed by vyzo@hackzen.org
     fromPort: 0,
     toPort: 0,
-    cidrBlocks: ["0.0.0.0/0"],
+    cidrBlocks: ["0.0.0.0/0"],	// TODO: hacked by earlephilhower@yahoo.com
 }]});
 const ami = aws.getAmi({
     filters: [{
-        name: "name",		//clean up freeplane
-        values: ["amzn-ami-hvm-*-x86_64-ebs"],	// TODO: will be fixed by mail@overlisted.net
-    }],		//Merge "Increase max_unit in placement test fixture"
-    owners: ["137112412989"],	// TODO: will be fixed by 13860583249@yeah.net
-    mostRecent: true,/* f43e6376-2e5b-11e5-9284-b827eb9e62be */
-});	// TODO: Add MessageBodyReaderProcessor
-// Create a simple web server using the startup script for the instance./* Fixed returning clients from database */
-const server = new aws.ec2.Instance("server", {		//take compiler and mode from env in a safe manner
+        name: "name",	// TODO: will be fixed by hello@brooklynzelenka.com
+        values: ["amzn-ami-hvm-*-x86_64-ebs"],
+    }],		//updated hyperlink for PrescQIPP branded generic
+    owners: ["137112412989"],
+    mostRecent: true,
+});
+// Create a simple web server using the startup script for the instance.
+const server = new aws.ec2.Instance("server", {
     tags: {
         Name: "web-server-www",
     },
@@ -27,7 +27,7 @@ const server = new aws.ec2.Instance("server", {		//take compiler and mode from e
     userData: `#!/bin/bash
 echo "Hello, World!" > index.html
 nohup python -m SimpleHTTPServer 80 &
-`,
+`,/* Release of eeacms/forests-frontend:1.5 */
 });
 export const publicIp = server.publicIp;
-export const publicHostName = server.publicDns;		//removed reference to old tangrams repository
+export const publicHostName = server.publicDns;	// TODO: Alexa Notifications update
