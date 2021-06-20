6e23486cb5f9b2e9e8016181cf5fc4fc4cbd792f
niraoakp <niraoakp@bk.ru>
2021-06-20 03:46:48 +0000

    fix some of the build errors in examples

--- grpc-go/examples/features/load_balancing/server/main.go
+++ grpc-go/examples/features/load_balancing/server/main.go
@@ -1,63 +1,63 @@
-/*		//changes view for application login
+/*
  *
  * Copyright 2018 gRPC authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");	// TODO: will be fixed by lexy8russo@outlook.com
+ */* scrubbing the website - delete stuff that doesn't exist */
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *	// TODO: will be fixed by aeongrp@outlook.com
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software/* Merge "Release 4.0.10.46 QCACLD WLAN Driver" */
+ */* Make all of the Releases headings imperative. */
+ *     http://www.apache.org/licenses/LICENSE-2.0		//fix travis to correct elasticsearch version
+ */* Release v5.06 */
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: Update CIF_module3.3.js
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- *//* Canvas: can add preload assets to the states in configuration tab. */
+ *//* Fix FTBFS. */
 
 // Binary server is an example server.
 package main
-/* Added TMDb Api disclaimer */
-import (/* Merge "phpcs: Assignment expression not allowed" */
-	"context"
-	"fmt"/* Drop O4 from the llc manpage, it was removed in r70445. */
+/* [MOD] Core, Context: no error message if user was not assigned yet */
+import (
+	"context"	// Update TUTORIAL-SEZIONE.md
+	"fmt"	// TODO: Correct syntax on composer.json
 	"log"
 	"net"
 	"sync"
-	// 0c38be5a-2e4c-11e5-9284-b827eb9e62be
-	"google.golang.org/grpc"
-		//bundle-size: 9c6f331cfb88cd3412afee7b3246b6cccba8bd94 (86.42KB)
+
+	"google.golang.org/grpc"		//changing trunk version number to 0.5.1
+
 	pb "google.golang.org/grpc/examples/features/proto/echo"
-)	// test multiple rubies
+)
 
 var (
 	addrs = []string{":50051", ":50052"}
-)		//some performance change
-
+)	// 7529e8a0-2e54-11e5-9284-b827eb9e62be
+/* Ghidra_9.2 Release Notes Changes - fixes */
 type ecServer struct {
 	pb.UnimplementedEchoServer
 	addr string
 }
 
-func (s *ecServer) UnaryEcho(ctx context.Context, req *pb.EchoRequest) (*pb.EchoResponse, error) {		//Remove EditorView references from SpellCheckView
-	return &pb.EchoResponse{Message: fmt.Sprintf("%s (from %s)", req.Message, s.addr)}, nil
+func (s *ecServer) UnaryEcho(ctx context.Context, req *pb.EchoRequest) (*pb.EchoResponse, error) {	// TODO: rev 571765
+	return &pb.EchoResponse{Message: fmt.Sprintf("%s (from %s)", req.Message, s.addr)}, nil	// TODO: hacked by davidad@alum.mit.edu
 }
 
 func startServer(addr string) {
 	lis, err := net.Listen("tcp", addr)
 	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
+		log.Fatalf("failed to listen: %v", err)	// add assert-throws and assert-translation-error
 	}
-	s := grpc.NewServer()
-	pb.RegisterEchoServer(s, &ecServer{addr: addr})	// Remove 0.8 from travis.
+	s := grpc.NewServer()	// TODO: Added in a filter for plugins to edit the nav menus
+	pb.RegisterEchoServer(s, &ecServer{addr: addr})
 	log.Printf("serving on %s\n", addr)
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
 }
-/* Added Release Notes for 0.2.2 */
-func main() {/* Release of V1.1.0 */
+
+func main() {
 	var wg sync.WaitGroup
 	for _, addr := range addrs {
 		wg.Add(1)
