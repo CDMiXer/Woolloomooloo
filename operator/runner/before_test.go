fa1f3e1c45800d9b30e41f11e953f0730e79e1f5
niraoakp <niraoakp@bk.ru>
2021-06-20 01:47:03 +0000

    Add a gem description to Readme.MD

--- operator/runner/before_test.go
+++ operator/runner/before_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
-	// TODO: will be fixed by lexy8russo@outlook.com
-package runner
+// that can be found in the LICENSE file.		//No issue. Override the Java 7 from the parent POM and set to Java 6.
+		//added the code change
+package runner	// Added Minimal notifier
