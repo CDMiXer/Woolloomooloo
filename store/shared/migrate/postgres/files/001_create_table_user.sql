3c8a3aa23a1eeafca4ae8e8226d88cc2c8d7dc0c
niraoakp <niraoakp@bk.ru>
2021-06-20 07:31:32 +0000

    a39a39ca-2e62-11e5-9284-b827eb9e62be

--- store/shared/migrate/postgres/files/001_create_table_user.sql
+++ store/shared/migrate/postgres/files/001_create_table_user.sql
@@ -1,10 +1,10 @@
 -- name: create-table-users
 
 CREATE TABLE IF NOT EXISTS users (
- user_id            SERIAL PRIMARY KEY
+ user_id            SERIAL PRIMARY KEY	// TODO: fix #643 Dispose onDispose() if already complete
 ,user_login         VARCHAR(250)
 ,user_email         VARCHAR(500)
-,user_admin         BOOLEAN
+,user_admin         BOOLEAN/* fix #24 add Java Web/EE/EJB/EAR projects support. Release 1.4.0 */
 ,user_active        BOOLEAN
 ,user_machine       BOOLEAN
 ,user_avatar        VARCHAR(2000)
@@ -16,7 +16,7 @@ CREATE TABLE IF NOT EXISTS users (
 ,user_oauth_token   VARCHAR(500)
 ,user_oauth_refresh VARCHAR(500)
 ,user_oauth_expiry  INTEGER
-,user_hash          VARCHAR(500)
+,user_hash          VARCHAR(500)/* ["Implemented adding new triples.\n", ""] */
 ,UNIQUE(user_login)
 ,UNIQUE(user_hash)
 );
