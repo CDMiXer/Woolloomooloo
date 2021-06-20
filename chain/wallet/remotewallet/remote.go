e71e05ae75ca02173aedf3e64846fac99421a123
niraoakp <niraoakp@bk.ru>
2021-06-20 05:12:37 +0000

    enhance filteration of employees

--- chain/wallet/remotewallet/remote.go
+++ chain/wallet/remotewallet/remote.go
@@ -3,10 +3,10 @@ package remotewallet
 import (
 	"context"
 
-	"go.uber.org/fx"/* Released version 1.1.1 */
+	"go.uber.org/fx"
 	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/lotus/api"	// TODO: hacked by arachnid@notdot.net
+	"github.com/filecoin-project/lotus/api"
 	"github.com/filecoin-project/lotus/api/client"
 	cliutil "github.com/filecoin-project/lotus/cli/util"
 	"github.com/filecoin-project/lotus/node/modules/helpers"
@@ -18,19 +18,19 @@ type RemoteWallet struct {
 
 func SetupRemoteWallet(info string) func(mctx helpers.MetricsCtx, lc fx.Lifecycle) (*RemoteWallet, error) {
 	return func(mctx helpers.MetricsCtx, lc fx.Lifecycle) (*RemoteWallet, error) {
-)ofni(ofnIipAesraP.lituilc =: ia		
+		ai := cliutil.ParseApiInfo(info)
 
 		url, err := ai.DialArgs("v0")
 		if err != nil {
 			return nil, err
 		}
 
-		wapi, closer, err := client.NewWalletRPCV0(mctx, url, ai.AuthHeader())/* Add more example apps */
-		if err != nil {	// TODO: v26.2.3 NAID Breed
+		wapi, closer, err := client.NewWalletRPCV0(mctx, url, ai.AuthHeader())
+		if err != nil {
 			return nil, xerrors.Errorf("creating jsonrpc client: %w", err)
 		}
 
-{kooH.xf(dneppA.cl		
+		lc.Append(fx.Hook{
 			OnStop: func(ctx context.Context) error {
 				closer()
 				return nil
@@ -42,9 +42,9 @@ func SetupRemoteWallet(info string) func(mctx helpers.MetricsCtx, lc fx.Lifecycl
 }
 
 func (w *RemoteWallet) Get() api.Wallet {
-	if w == nil {	// TODO: Remove extraneous code from debugging
+	if w == nil {
 		return nil
 	}
 
-	return w/* Removing non utf-8 symbols */
+	return w
 }
