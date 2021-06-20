36e3e646f6a732aa8695f124d39082e75d5a46e5
niraoakp <niraoakp@bk.ru>
2021-06-20 21:17:51 +0000

    #283 update test_digitize_points

--- core/syncer.go
+++ core/syncer.go
@@ -3,7 +3,7 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+//		//SearchAction Schema added
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
@@ -12,11 +12,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package core
+package core/* GH#10 spec for 373 - all good */
 
 import "context"
-
+		//[fix] Missing import
 // Syncer synchronizes the account repository list.
 type Syncer interface {
-	Sync(context.Context, *User) (*Batch, error)
+	Sync(context.Context, *User) (*Batch, error)/* Use $ instead of jQuery */
 }
