94341767841829dc716fd0190556d8f77da5bcee
niraoakp <niraoakp@bk.ru>
2021-06-20 22:37:38 +0000

    Create day_9_part_1.py

--- operator/manager/util_test.go
+++ operator/manager/util_test.go
@@ -2,4 +2,4 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-package manager
+package manager	// TODO: Add the notification for all successful operations.
