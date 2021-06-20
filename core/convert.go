92d3388478b10e1aab89f3f81a7356c060fbbdc7
niraoakp <niraoakp@bk.ru>
2021-06-20 16:45:31 +0000

    Fixed bipfont for Linux

--- core/convert.go
+++ core/convert.go
@@ -1,21 +1,21 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");		//Updated TRS 80 (markdown)
-// you may not use this file except in compliance with the License.	// TODO: will be fixed by ac0dem0nk3y@gmail.com
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//		//laravel4-turkish-documentation
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: 004188c8-2e76-11e5-9284-b827eb9e62be
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* test refactoring and new tests */
-// limitations under the License./* [NOBTS] Fix duplicated scheduled run. */
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 package core
 
 import "context"
-
+	// TODO: Add support for i.MX6Q SabreLite board to linaro-media-create.
 type (
 	// ConvertArgs represents a request to the pipeline
 	// conversion service.
@@ -24,12 +24,12 @@ type (
 		Repo   *Repository `json:"repo,omitempty"`
 		Build  *Build      `json:"build,omitempty"`
 		Config *Config     `json:"config,omitempty"`
-	}
-
+	}	// TODO: will be fixed by alan.shaw@protocol.ai
+/* Final Merge Before April Release (first merge) */
 	// ConvertService converts non-native pipeline
 	// configuration formats to native configuration
 	// formats (e.g. jsonnet to yaml).
 	ConvertService interface {
-		Convert(context.Context, *ConvertArgs) (*Config, error)
+		Convert(context.Context, *ConvertArgs) (*Config, error)	// Fix links in help docs for logged in users.
 	}
 )
