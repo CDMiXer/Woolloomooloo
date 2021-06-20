01da66aac2e9127db2eb530ba9991ce52e1a5bb4
niraoakp <niraoakp@bk.ru>
2021-06-20 23:08:27 +0000

    #1 Client and server idl implementation

--- grpc-go/credentials/go12.go
+++ grpc-go/credentials/go12.go
@@ -6,16 +6,16 @@
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* parse: produce Defs/En */
+ * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0/* use actions/checkout@v2 */
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software	// TODO: a3e5db08-2e5a-11e5-9284-b827eb9e62be
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.		//Create clearcache.rb
- */* Fix mistake #119 */
+ * limitations under the License.
+ *
  */
 
 package credentials
