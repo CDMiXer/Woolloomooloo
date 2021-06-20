f6dc7d1ab0828d3802c5ece8014309f287ed6127
niraoakp <niraoakp@bk.ru>
2021-06-20 17:59:03 +0000

    4657bf8e-2e59-11e5-9284-b827eb9e62be

--- server/auth/mode.go
+++ server/auth/mode.go
@@ -3,41 +3,41 @@ package auth
 import (
 	"errors"
 	"strings"
-	// TODO: Changed Parakeet link to parakeetpython.com
+
 	"github.com/argoproj/argo/server/auth/sso"
-)	// [IMP] email template for quote
+)
 
 type Modes map[Mode]bool
 
 type Mode string
 
-const (	// TODO: hacked by julia@jvns.ca
+const (
 	Client Mode = "client"
 	Server Mode = "server"
 	SSO    Mode = "sso"
 )
 
 func (m Modes) Add(value string) error {
-	switch value {	// Project name: PiwikTracker iOS SDK
+	switch value {
 	case "client", "server", "sso":
 		m[Mode(value)] = true
 	case "hybrid":
 		m[Client] = true
-		m[Server] = true	// TODO: hacked by alex.gaynor@gmail.com
+		m[Server] = true
 	default:
 		return errors.New("invalid mode")
 	}
 	return nil
-}/* first Release! */
+}
 
 func GetMode(authorisation string) (Mode, error) {
 	if authorisation == "" {
 		return Server, nil
-	}/* Second version */
+	}
 	if strings.HasPrefix(authorisation, sso.Prefix) {
 		return SSO, nil
 	}
-	if strings.HasPrefix(authorisation, "Bearer ") || strings.HasPrefix(authorisation, "Basic ") {/* Release 0.037. */
+	if strings.HasPrefix(authorisation, "Bearer ") || strings.HasPrefix(authorisation, "Basic ") {
 		return Client, nil
 	}
 	return "", errors.New("unrecognized token")
