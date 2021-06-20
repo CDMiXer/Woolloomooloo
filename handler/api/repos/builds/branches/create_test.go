aaa0efd8b8eb5a1f29f06ce38ff92d289e189ab0
niraoakp <niraoakp@bk.ru>
2021-06-20 01:08:38 +0000

    Merge "Return 204 instead of 200 for root-disable API"

--- handler/api/repos/builds/branches/create_test.go
+++ handler/api/repos/builds/branches/create_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License	// TODO: Merge "remove sysstat & pidstat"
 // that can be found in the LICENSE file.
-
-package branches/* Release 0.5.0-alpha3 */
+		//Updating build step names.
+package branches
