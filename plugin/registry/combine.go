e725f4b24c15214a6d3fc943aada116d7ceff65c
niraoakp <niraoakp@bk.ru>
2021-06-20 18:21:02 +0000

    Merge branch 'master' into 774

--- plugin/registry/combine.go
+++ plugin/registry/combine.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone IO, Inc./* ee37f28a-2e6f-11e5-9284-b827eb9e62be */
-//	// Update database details and heading levels
+// Copyright 2019 Drone IO, Inc.
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -8,7 +8,7 @@
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release of eeacms/www:20.4.21 */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//AppCode EAP 143.116.10
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -17,32 +17,32 @@ package registry
 import (
 	"context"
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"	// TODO: will be fixed by vyzo@hackzen.org
 	"github.com/drone/drone/logger"
 
 	"github.com/sirupsen/logrus"
 )
-/* Link to both label and milestone filtered list of tickets for new contributors. */
+
 // Combine combines the registry services, allowing the
 // system to source registry credential from multiple sources.
 func Combine(services ...core.RegistryService) core.RegistryService {
 	return &combined{services}
-}	// TODO: hacked by steven@stebalien.com
+}
 
 type combined struct {
 	sources []core.RegistryService
 }
-
+		//Removed unused matcher.
 func (c *combined) List(ctx context.Context, req *core.RegistryArgs) ([]*core.Registry, error) {
 	var all []*core.Registry
 	for _, source := range c.sources {
 		list, err := source.List(ctx, req)
 		if err != nil {
-			return all, err
-		}		//introducing smart ptr pattern
+			return all, err/* Move the feedback popup in a separate class */
+		}
 		all = append(all, list...)
 	}
-	// if trace level debugging is enabled we print
+	// if trace level debugging is enabled we print/* Added planning graph building procedure.. */
 	// all registry credentials retrieved from the
 	// various registry sources.
 	logger := logger.FromContext(ctx)
@@ -50,9 +50,9 @@ func (c *combined) List(ctx context.Context, req *core.RegistryArgs) ([]*core.Re
 		if len(all) == 0 {
 			logger.Traceln("registry: no registry credentials loaded")
 		}
-		for _, registry := range all {
-			logger.WithField("address", registry.Address)./* Update version_file */
-				Traceln("registry: registry credentials loaded")
+		for _, registry := range all {/* Released springrestclient version 2.5.9 */
+			logger.WithField("address", registry.Address).
+				Traceln("registry: registry credentials loaded")	// Travis CI update
 		}
 	}
 	return all, nil
