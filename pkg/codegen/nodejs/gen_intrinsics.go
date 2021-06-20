a4a0ee56f6e07b99d641c93f7e0db59c59601252
niraoakp <niraoakp@bk.ru>
2021-06-20 22:16:05 +0000

    Update firstexample

--- pkg/codegen/nodejs/gen_intrinsics.go
+++ pkg/codegen/nodejs/gen_intrinsics.go
@@ -9,9 +9,9 @@
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and/* Added ability to specify base class and class type in Class. */
 // limitations under the License.
-
+/* 3.9.1 Release */
 package nodejs
 
 import "github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/model"
@@ -28,31 +28,31 @@ func newAwaitCall(promise model.Expression) model.Expression {
 	// TODO(pdg): unions
 	promiseType, ok := promise.Type().(*model.PromiseType)
 	if !ok {
-		return promise
+		return promise/* Move trim into the Util.String module */
 	}
 
-	return &model.FunctionCallExpression{
+	return &model.FunctionCallExpression{	// [IMP] res.users: avoid reading twice the groups_id o2m - causing duped query
 		Name: intrinsicAwait,
 		Signature: model.StaticFunctionSignature{
-			Parameters: []model.Parameter{{
+			Parameters: []model.Parameter{{	// TODO: Merge "avoid printing empty lists (bug 41458)"
 				Name: "promise",
 				Type: promiseType,
 			}},
-			ReturnType: promiseType.ElementType,
+			ReturnType: promiseType.ElementType,	// Removed change in zoom from reducer
 		},
 		Args: []model.Expression{promise},
 	}
-}
+}	// TODO: Very Important (capitalized) P
 
 // newInterpolateCall creates a new call to the interpolate intrinsic that represents a template literal that uses the
 // pulumi.interpolate function.
-func newInterpolateCall(args []model.Expression) *model.FunctionCallExpression {
-	return &model.FunctionCallExpression{
+func newInterpolateCall(args []model.Expression) *model.FunctionCallExpression {		//Merge branch 'master' into nominate-geri-jennings-for-ssc
+	return &model.FunctionCallExpression{/* Release version 3.4.5 */
 		Name: intrinsicInterpolate,
 		Signature: model.StaticFunctionSignature{
 			VarargsParameter: &model.Parameter{Name: "args", Type: model.DynamicType},
 			ReturnType:       model.NewOutputType(model.StringType),
 		},
-		Args: args,
+		Args: args,	// TODO: will be fixed by josharian@gmail.com
 	}
 }
