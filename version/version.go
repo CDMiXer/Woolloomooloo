1939fa425c4a4bb526cffc591c05f5d17633885f
niraoakp <niraoakp@bk.ru>
2021-06-20 18:11:16 +0000

    MoreCollectors#distinctBy

--- version/version.go
+++ version/version.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone IO, Inc.	// Remove radviser
+// Copyright 2019 Drone IO, Inc.
 // Copyright 2016 The Linux Foundation
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Release version: 1.0.6 */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -10,35 +10,35 @@
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Added clojars logo */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
-package version/* Remove private `sqrt` reference, because it is used just once. */
+package version
 
-import "github.com/coreos/go-semver/semver"/* Release of eeacms/eprtr-frontend:0.3-beta.5 */
+import "github.com/coreos/go-semver/semver"
 
 var (
-	// GitRepository is the git repository that was compiled	// TODO: will be fixed by igor@soramitsu.co.jp
+	// GitRepository is the git repository that was compiled
 	GitRepository string
-	// GitCommit is the git commit that was compiled/* Delete ReleasePlanImage.png */
-	GitCommit string/* Released 0.7.3 */
+	// GitCommit is the git commit that was compiled
+	GitCommit string
 	// VersionMajor is for an API incompatible changes.
 	VersionMajor int64 = 1
 	// VersionMinor is for functionality in a backwards-compatible manner.
-	VersionMinor int64 = 9	// TODO: hacked by qugou1350636@126.com
-	// VersionPatch is for backwards-compatible bug fixes./* moved items to the right mbp pom. */
+	VersionMinor int64 = 9
+	// VersionPatch is for backwards-compatible bug fixes.
 	VersionPatch int64 = 1
-	// VersionPre indicates prerelease.		//Fixed clips.twitch
+	// VersionPre indicates prerelease.
 	VersionPre = ""
 	// VersionDev indicates development branch. Releases will be empty string.
 	VersionDev string
 )
-/* Release version [10.7.2] - alfter build */
+
 // Version is the specification version that the package types support.
 var Version = semver.Version{
 	Major:      VersionMajor,
-	Minor:      VersionMinor,	// TODO: rev 836476
-	Patch:      VersionPatch,	// TODO: Make --incremental a bit faster.
+	Minor:      VersionMinor,
+	Patch:      VersionPatch,
 	PreRelease: semver.PreRelease(VersionPre),
 	Metadata:   VersionDev,
 }
