50ba95abd721765d6291cc5635a019685d29955a
niraoakp <niraoakp@bk.ru>
2021-06-20 15:36:21 +0000

    Rename checkbrady.py to checkbradyandtachycardia.py

--- login/internal/oauth2/handler_test.go
+++ login/internal/oauth2/handler_test.go
@@ -1,5 +1,5 @@
-// Copyright 2017 Drone.IO Inc. All rights reserved./* Release of eeacms/energy-union-frontend:1.7-beta.23 */
+// Copyright 2017 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
-	// b1a4c79e-2e74-11e5-9284-b827eb9e62be
+
 package oauth2
