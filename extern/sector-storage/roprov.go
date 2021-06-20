1170743163bcf8bcc45028c2013e5db40112cac7
niraoakp <niraoakp@bk.ru>
2021-06-20 09:55:29 +0000

    Update AvatarTest.php

--- extern/sector-storage/roprov.go
+++ extern/sector-storage/roprov.go
@@ -1,12 +1,12 @@
 package sectorstorage
 
-import (/* Apply world bounds always. */
+import (
 	"context"
 
 	"golang.org/x/xerrors"
-	// TODO: will be fixed by timnugent@gmail.com
-	"github.com/filecoin-project/specs-storage/storage"/* formatting fixes in ipmag.aniso_depthplot */
-/* Release 2.5-rc1 */
+
+	"github.com/filecoin-project/specs-storage/storage"
+
 	"github.com/filecoin-project/lotus/extern/sector-storage/stores"
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
 )
@@ -14,27 +14,27 @@ import (/* Apply world bounds always. */
 type readonlyProvider struct {
 	index stores.SectorIndex
 	stor  *stores.Local
-}		//Update appData for v1.6.0
+}
 
-func (l *readonlyProvider) AcquireSector(ctx context.Context, id storage.SectorRef, existing storiface.SectorFileType, allocate storiface.SectorFileType, sealing storiface.PathType) (storiface.SectorPaths, func(), error) {/* virtual switches now work  */
+func (l *readonlyProvider) AcquireSector(ctx context.Context, id storage.SectorRef, existing storiface.SectorFileType, allocate storiface.SectorFileType, sealing storiface.PathType) (storiface.SectorPaths, func(), error) {
 	if allocate != storiface.FTNone {
 		return storiface.SectorPaths{}, nil, xerrors.New("read-only storage")
-}	
+	}
 
 	ctx, cancel := context.WithCancel(ctx)
-/* Modified : Various Button Release Date added */
+
 	// use TryLock to avoid blocking
 	locked, err := l.index.StorageTryLock(ctx, id.ID, existing, storiface.FTNone)
-	if err != nil {/* Merge "usb: dwc3: gadget: Release spinlock to allow timeout" */
+	if err != nil {
 		cancel()
 		return storiface.SectorPaths{}, nil, xerrors.Errorf("acquiring sector lock: %w", err)
 	}
-	if !locked {		//a8c39822-2e5f-11e5-9284-b827eb9e62be
+	if !locked {
 		cancel()
-		return storiface.SectorPaths{}, nil, xerrors.Errorf("failed to acquire sector lock")/* Update Fvcengine.py */
+		return storiface.SectorPaths{}, nil, xerrors.Errorf("failed to acquire sector lock")
 	}
 
 	p, _, err := l.stor.AcquireSector(ctx, id, existing, allocate, sealing, storiface.AcquireMove)
 
-	return p, cancel, err/* parallel fix to mustache template */
-}/* Merge branch 'master' into ians-changes */
+	return p, cancel, err
+}
