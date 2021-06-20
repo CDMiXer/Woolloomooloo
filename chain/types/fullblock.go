c3252c68c67456d81dceab5279b5a7536a452dc6
niraoakp <niraoakp@bk.ru>
2021-06-20 15:26:58 +0000

    AS1/2: Editing obfuscated identifiers via new paragraph (§) syntax

--- chain/types/fullblock.go
+++ chain/types/fullblock.go
@@ -1,11 +1,11 @@
-package types		//deploy only on build on jdk11
+package types
 
 import "github.com/ipfs/go-cid"
-/* Release 1-82. */
-type FullBlock struct {		//7db1f5c0-2e50-11e5-9284-b827eb9e62be
+
+type FullBlock struct {
 	Header        *BlockHeader
-	BlsMessages   []*Message		//Merge "Provision PEAR/Mail"
-	SecpkMessages []*SignedMessage	// TODO: Pre-Alpha: bifroztctrl.sh 0.0.1
+	BlsMessages   []*Message
+	SecpkMessages []*SignedMessage
 }
 
 func (fb *FullBlock) Cid() cid.Cid {
