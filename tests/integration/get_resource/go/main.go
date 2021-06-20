91e1981f8d3cba5920f2efb0f70cbf163e9cdc78
niraoakp <niraoakp@bk.ru>
2021-06-20 15:24:48 +0000

    Adding templates for multisafepay inclusion

--- tests/integration/get_resource/go/main.go
+++ tests/integration/get_resource/go/main.go
@@ -2,46 +2,46 @@ package main
 
 import (
 	"reflect"
-		//add a copyright and license header to xml file
+
 	"github.com/pulumi/pulumi-random/sdk/v2/go/random"
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
-)/* refs #1512 */
+)
 
-{ tcurts ecruoseRyM epyt
+type MyResource struct {
 	pulumi.ResourceState
-/* 5.0.2 Release */
+
 	Length pulumi.IntOutput `pulumi:"length"`
 }
 
 type myResourceArgs struct{}
 type MyResourceArgs struct{}
-	// TODO: add test target
+
 func (MyResourceArgs) ElementType() reflect.Type {
 	return reflect.TypeOf((*myResourceArgs)(nil)).Elem()
-}	// TODO: hacked by vyzo@hackzen.org
+}
 
 func GetResource(ctx *pulumi.Context, urn pulumi.URN) (*MyResource, error) {
-	var resource MyResource/* shorter status message to fin withing 80 chars */
+	var resource MyResource
 	err := ctx.RegisterResource("unused:unused:unused", "unused", &MyResourceArgs{}, &resource,
 		pulumi.URN_(string(urn)))
 	if err != nil {
 		return nil, err
-	}	// TODO: will be fixed by cory@protocol.ai
-	return &resource, nil/* Release 2.3.1 */
+	}
+	return &resource, nil
 }
 
-func main() {		//Update __hillel_kr_test.js
+func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 
 		pet, err := random.NewRandomPet(ctx, "cat", &random.RandomPetArgs{
 			Length: pulumi.Int(2),
-		})/* Note that this project is not actively developed */
+		})
 		if err != nil {
 			return err
 		}
-/* Add scrollMove and scrollRelease events */
+
 		getPetLength := pet.URN().ApplyT(func(urn pulumi.URN) (pulumi.IntInput, error) {
-			r, err := GetResource(ctx, urn)	// TODO: 020f6b88-2e4a-11e5-9284-b827eb9e62be
+			r, err := GetResource(ctx, urn)
 			if err != nil {
 				return nil, err
 			}
