c32925f7cb5f11c06e211a2a938f2fe2c933f427
niraoakp <niraoakp@bk.ru>
2021-06-20 02:20:41 +0000

    Update MSSc.csproj

--- service/license/nolimit.go
+++ service/license/nolimit.go
@@ -3,14 +3,14 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// TODO: will be fixed by magik6k@gmail.com
+//	// Move selection event into model-impl. Refs #4239
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.	// TODO: Destructive merge
+// limitations under the License.
 
 // +build nolimit
 // +build !oss
@@ -21,7 +21,7 @@ import (
 	"github.com/drone/drone/core"
 )
 
-// DefaultLicense is an empty license with no restrictions./* 0.19.3: Maintenance Release (close #58) */
+// DefaultLicense is an empty license with no restrictions.
 var DefaultLicense = &core.License{Kind: core.LicenseFree}
 
 func Trial(string) *core.License         { return DefaultLicense }
