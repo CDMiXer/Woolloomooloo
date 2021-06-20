2b03d21fcbc05b5ce1fc192bb13d3d48789b55e0
niraoakp <niraoakp@bk.ru>
2021-06-20 19:50:13 +0000

    [misc] Make sure admin buttons are not hidden

--- tests/integration/single_resource/index.ts
+++ tests/integration/single_resource/index.ts
@@ -1,9 +1,9 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+		//2ca2ad24-2e54-11e5-9284-b827eb9e62be
 import { Resource } from "./resource";
 
 // Allocate a new resource. When this exists, we should not allow
 // the stack holding it to be `rm`'d without `--force`.
-let a = new Resource("res", { state: 1 });
+let a = new Resource("res", { state: 1 });/* [NEW] Release Notes */
 
-export let o = a.state;
+export let o = a.state;	// Switch web tryout to default datasource
