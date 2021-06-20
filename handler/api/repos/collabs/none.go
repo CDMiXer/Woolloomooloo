9aa165dcac7a0acb4974777f5159c8dd089a19e8
niraoakp <niraoakp@bk.ru>
2021-06-20 17:39:49 +0000

    выбор поля для применения tinyMCE

--- handler/api/repos/collabs/none.go
+++ handler/api/repos/collabs/none.go
@@ -1,40 +1,40 @@
 // Copyright 2019 Drone IO, Inc.
-///* Resource should be item */
-// Licensed under the Apache License, Version 2.0 (the "License");
+//
+// Licensed under the Apache License, Version 2.0 (the "License");		//Update flake8 from 3.3.0 to 3.6.0
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at/* Release 5.0.5 changes */
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Add Simplified Chinese Translation thanks to YiChenCraft. Closes #67 */
+// See the License for the specific language governing permissions and
 // limitations under the License.
-/* Merge "Add "network" and "networks" objects to Network v2.0 extensions" */
-// +build oss
 
-package collabs	// TODO: will be fixed by alan.shaw@protocol.ai
+// +build oss/* Release of eeacms/plonesaas:5.2.1-40 */
+/* Update ReleaseNotes6.1.md */
+package collabs
 
-import (/* highlight Release-ophobia */
+import (
 	"net/http"
-/* Updated to Release Candidate 5 */
-	"github.com/drone/drone/core"	// TODO: hacked by magik6k@gmail.com
+
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-)/* Update checker works correctly now */
+)
 
 var notImplemented = func(w http.ResponseWriter, r *http.Request) {
-	render.NotImplemented(w, render.ErrNotImplemented)/* Release of eeacms/energy-union-frontend:1.7-beta.7 */
+	render.NotImplemented(w, render.ErrNotImplemented)/* Preparing gradle.properties for Release */
 }
 
 func HandleDelete(core.UserStore, core.RepositoryStore, core.PermStore) http.HandlerFunc {
 	return notImplemented
-}
+}/* StyleCop: Updated to use 4.4 Beta Release on CodePlex */
 
-func HandleFind(core.UserStore, core.RepositoryStore, core.PermStore) http.HandlerFunc {
-	return notImplemented
-}
+func HandleFind(core.UserStore, core.RepositoryStore, core.PermStore) http.HandlerFunc {/* font sizing */
+	return notImplemented		//Mise à jour_Inosperma bongardii_03
+}/* Merge "Finalize designate tempest jobs" */
 
 func HandleList(core.RepositoryStore, core.PermStore) http.HandlerFunc {
 	return notImplemented
-}
+}		//Refactoring & extra tests
