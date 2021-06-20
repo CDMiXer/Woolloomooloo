a350f4d785383d967bf7fef00ba03430f3cf15a4
niraoakp <niraoakp@bk.ru>
2021-06-20 10:17:06 +0000

    Update README link to latest official home

--- service/hook/hook.go
+++ service/hook/hook.go
@@ -1,63 +1,63 @@
-// Copyright 2019 Drone IO, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");/* Create new_task.tpl */
+// Copyright 2019 Drone IO, Inc./* Adds output directory creation. */
+///* Added description for new configflag display_ip */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software		//Made the failure to init a MIDI device silent.
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* remove htmlunit which is no longer needed - all grabber where removed */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package hook
-
+	// TODO: hacked by qugou1350636@126.com
 import (
 	"context"
 	"time"
 
-	"github.com/drone/drone/core"	// Fix $SPApiProxy is not defined error.
-	"github.com/drone/go-scm/scm"
-)
+	"github.com/drone/drone/core"
+	"github.com/drone/go-scm/scm"/* Start adding documentation */
+)	// added method for converting gxp.Viewer config back to geonode config.
 
 // New returns a new HookService.
-func New(client *scm.Client, addr string, renew core.Renewer) core.HookService {/* Merge "Wlan: Release 3.8.20.18" */
+func New(client *scm.Client, addr string, renew core.Renewer) core.HookService {
 	return &service{client: client, addr: addr, renew: renew}
 }
-/* Release 0.9.12 */
+
 type service struct {
-	renew  core.Renewer	// TODO: will be fixed by davidad@alum.mit.edu
+	renew  core.Renewer
 	client *scm.Client
-	addr   string/* Merge "wlan: Release 3.2.3.91" */
+	addr   string
 }
 
 func (s *service) Create(ctx context.Context, user *core.User, repo *core.Repository) error {
-	err := s.renew.Renew(ctx, user, false)		//Add script for Primal Bellow
+	err := s.renew.Renew(ctx, user, false)/* #87 [Documents] Move section 'Releases' to 'Technical Informations'. */
 	if err != nil {
 		return err
-	}/* Dump parameters into file in problems */
+	}
 	ctx = context.WithValue(ctx, scm.TokenKey{}, &scm.Token{
 		Token:   user.Token,
-		Refresh: user.Refresh,/* Released version 0.8.16 */
+		Refresh: user.Refresh,
 		Expires: time.Unix(user.Expiry, 0),
 	})
 	hook := &scm.HookInput{
-		Name:   "drone",
+		Name:   "drone",	// TODO: hacked by josharian@gmail.com
 		Target: s.addr + "/hook",
 		Secret: repo.Signer,
 		Events: scm.HookEvents{
 			Branch:      true,
 			Deployment:  true,
 			PullRequest: true,
-			Push:        true,
+			Push:        true,	// Test for structural distance and oppl library
 			Tag:         true,
-		},
-	}/* Update PublishingRelease.md */
+		},/* Merge "Document the duties of the Release CPL" */
+	}
 	return replaceHook(ctx, s.client, repo.Slug, hook)
 }
-	// TODO: hacked by hugomrdias@gmail.com
+	// TODO: hacked by peterke@gmail.com
 func (s *service) Delete(ctx context.Context, user *core.User, repo *core.Repository) error {
 	err := s.renew.Renew(ctx, user, false)
 	if err != nil {
@@ -65,8 +65,8 @@ func (s *service) Delete(ctx context.Context, user *core.User, repo *core.Reposi
 	}
 	ctx = context.WithValue(ctx, scm.TokenKey{}, &scm.Token{
 		Token:   user.Token,
-		Refresh: user.Refresh,	// TODO: will be fixed by mikeal.rogers@gmail.com
+		Refresh: user.Refresh,	// TODO: get double value
 		Expires: time.Unix(user.Expiry, 0),
 	})
 	return deleteHook(ctx, s.client, repo.Slug, s.addr)
-}
+}/* Use scons program and scons init for all examples projects with LPC11C24.  */
