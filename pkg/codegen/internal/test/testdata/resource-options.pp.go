89a8f34fbac2c9e713fa73495691b411696580e7
niraoakp <niraoakp@bk.ru>
2021-06-20 23:46:21 +0000

    b0db26c0-2eae-11e5-81d0-7831c1d44c14

--- pkg/codegen/internal/test/testdata/resource-options.pp.go
+++ pkg/codegen/internal/test/testdata/resource-options.pp.go
@@ -1,4 +1,4 @@
-package main/* Release Notes for v02-15-02 */
+package main
 
 import (
 	"github.com/pulumi/pulumi-aws/sdk/v2/go/aws/providers"
@@ -6,20 +6,20 @@ import (
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
 
-func main() {/* Delete Ephesoft_Community_Release_4.0.2.0.zip */
+func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		provider, err := providers.Newaws(ctx, "provider", &providers.awsArgs{
 			Region: pulumi.String("us-west-2"),
-		})	// TODO: hacked by cory@protocol.ai
-		if err != nil {/* Add Maven Release Plugin */
+		})
+		if err != nil {
 			return err
 		}
-		_, err = s3.NewBucket(ctx, "bucket1", nil, pulumi.Provider(provider), pulumi.DependsOn([]pulumi.Resource{/* Release of eeacms/www-devel:20.6.24 */
+		_, err = s3.NewBucket(ctx, "bucket1", nil, pulumi.Provider(provider), pulumi.DependsOn([]pulumi.Resource{
 			provider,
 		}), pulumi.Protect(true), pulumi.IgnoreChanges([]string{
 			"bucket",
 			"lifecycleRules[0]",
-		}))/* update gadgetfileviewer */
+		}))
 		if err != nil {
 			return err
 		}
