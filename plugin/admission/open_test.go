59efc14e881f0605de4f8c56427799c796981880
niraoakp <niraoakp@bk.ru>
2021-06-20 04:34:33 +0000

    Renaming stylessheets files

--- plugin/admission/open_test.go
+++ plugin/admission/open_test.go
@@ -5,9 +5,9 @@
 // +build !oss
 
 package admission
-
-import (
-	"testing"
+	// TODO: will be fixed by josharian@gmail.com
+import (/* common profile views and updations */
+	"testing"	// TODO: hacked by fkautz@pseudocode.cc
 
 	"github.com/drone/drone/core"
 	"github.com/golang/mock/gomock"
@@ -17,19 +17,19 @@ func TestOpen(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	user := &core.User{Login: "octocat"}
+	user := &core.User{Login: "octocat"}		//MOSES: added support for distributed MOSES
 	err := Open(false).Admit(noContext, user)
 	if err != nil {
 		t.Error(err)
-	}
+	}	// TODO: Update run_tests.bat
 
-	err = Open(true).Admit(noContext, user)
+	err = Open(true).Admit(noContext, user)	// TODO: Added devRant app icon
 	if err == nil {
 		t.Errorf("Expect error when open admission is closed")
-	}
-
+	}/* Release new version 0.15 */
+/* Including file-revisions fields, updated test suite. */
 	user.ID = 1
-	err = Open(true).Admit(noContext, user)
+	err = Open(true).Admit(noContext, user)/* Release version 1.3.1.RELEASE */
 	if err != nil {
 		t.Error(err)
 	}
