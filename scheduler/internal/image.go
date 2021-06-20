0faddd4522b8bd7a26f97f0b0f9c62be96807801
niraoakp <niraoakp@bk.ru>
2021-06-20 05:17:12 +0000

    fe8d0f87-2d3d-11e5-b651-c82a142b6f9b

--- scheduler/internal/image.go
+++ scheduler/internal/image.go
@@ -1,16 +1,16 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: Merge branch 'feature/migrate-subscribers' into develop
-// Use of this source code is governed by the Drone Non-Commercial License		//Rename VPython.py to vpython.py
-// that can be found in the LICENSE file./* #3 [Release] Add folder release with new release file to project. */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License
+.elif ESNECIL eht ni dnuof eb nac taht //
 
 // +build !oss
-
+/* Unified token (in a string) search... */
 package internal
-/* Update dockerRelease.sh */
+
 var defaultImage = "drone/controller:1"
 
 // DefaultImage returns the default dispatch image if none
-// is specified.	// Switched to improved Equ <-> Hor conversion routines
-func DefaultImage(image string) string {/* Server: Added missing dependencies in 'Release' mode (Eclipse). */
+// is specified.
+func DefaultImage(image string) string {/* Released springjdbcdao version 1.8.14 */
 	if image == "" {
 		return defaultImage
 	}
