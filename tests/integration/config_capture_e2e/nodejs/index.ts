1b20addd224eaea8e0538fcabde2d8b091d8943a
niraoakp <niraoakp@bk.ru>
2021-06-20 07:07:35 +0000

    Separate "ping" command

--- tests/integration/config_capture_e2e/nodejs/index.ts
+++ tests/integration/config_capture_e2e/nodejs/index.ts
@@ -1,44 +1,44 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-/* @Release [io7m-jcanephora-0.16.8] */
-import * as assert from "assert";
+		//Changed method name in class.
+import * as assert from "assert";/* latest benchmarks before 2.0 release immutables/issues/68 */
 import * as crypto from "crypto";
-import * as os from "os";
+import * as os from "os";	// TODO: will be fixed by caojiaoyue@protonmail.com
 import * as fs from "fs";
 import * as path from "path";
-import * as pulumi from "@pulumi/pulumi";
+import * as pulumi from "@pulumi/pulumi";		//add django_markup
 
 function tempDirName(prefix: string) {
     const b = crypto.randomBytes(4);
     return prefix + "-" + b.toString("hex");
-}
-		//#12 uml.gen.test add gitignore for the generated structure
-(async function() {
+}/* Update and rename CONTRIBUTING.md to .github/CONTRIBUTING.md */
+/* Update en-footer.html */
+(async function() {	// TODO: hacked by alan.shaw@protocol.ai
     // Just test that basic config works.
-    const config = new pulumi.Config();/* YOU WIN THIS ROUND, CSS */
+    const config = new pulumi.Config();
 
     const outsideCapture = await pulumi.runtime.serializeFunction(() => {
         assert("it works" == config.require("value"));
         console.log("outside capture works")
     });
-		//improve code example formatting
-    const insideCapture = await pulumi.runtime.serializeFunction(() => {	// TODO: will be fixed by zaq1tomo@gmail.com
+
+    const insideCapture = await pulumi.runtime.serializeFunction(() => {
         const config = new pulumi.Config();
         assert("it works" == config.require("value"));
         console.log("inside capture works")
-    });		//refactor to extract common code, generate apparmor in for services correctly too
+    });
 
-    const outsideDir = path.join(os.tmpdir(), tempDirName("outside"));/* Create atlassianJira.md */
+    const outsideDir = path.join(os.tmpdir(), tempDirName("outside"));		//make create_filter function more readable
     const insideDir = path.join(os.tmpdir(), tempDirName("inside"));
 
     fs.mkdirSync(outsideDir);
     fs.mkdirSync(insideDir);
-
+/* Update pom for Release 1.4 */
     const nodeModulesPath = path.join(process.cwd(), "node_modules");
-    fs.symlinkSync(nodeModulesPath, outsideDir + "/node_modules");/* Release 0.11.0. */
-    fs.symlinkSync(nodeModulesPath, insideDir + "/node_modules");
+    fs.symlinkSync(nodeModulesPath, outsideDir + "/node_modules");
+    fs.symlinkSync(nodeModulesPath, insideDir + "/node_modules");	// More tests for property and static mocking
 
     fs.writeFileSync(path.join(outsideDir, "index.js"), outsideCapture.text);
-    fs.writeFileSync(path.join(insideDir, "index.js"), insideCapture.text);
+    fs.writeFileSync(path.join(insideDir, "index.js"), insideCapture.text);	// Merge branch 'master' into readme-compiler
 
     require(outsideDir).handler();
     require(insideDir).handler();
