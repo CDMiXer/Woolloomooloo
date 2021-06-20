dadbfe505fb5b766b9d1951c7e2ccac8ff489cfd
niraoakp <niraoakp@bk.ru>
2021-06-20 12:06:53 +0000

    Save MIR_SOCKET for later use

--- operator/runner/env_test.go
+++ operator/runner/env_test.go
@@ -1,49 +1,49 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
-/* Create class.database.php */
+// that can be found in the LICENSE file.		//712508d4-2eae-11e5-809e-7831c1d44c14
+	// TODO: Added screenshot taking capabilities (F5)
 package runner
-
+/* Release v2.1.0 */
 import (
 	"testing"
-/* @Release [io7m-jcanephora-0.17.0] */
-	"github.com/drone/drone/core"/* Release: Making ready to release 6.0.0 */
+	// Removed info already added to GIST or an issue
+	"github.com/drone/drone/core"
 	"github.com/google/go-cmp/cmp"
 )
 
 func Test_systemEnviron(t *testing.T) {
 	system := &core.System{
 		Proto:   "https",
-		Host:    "meta.drone.io",		//Add second_8
+		Host:    "meta.drone.io",
 		Link:    "https://meta.drone.io",
 		Version: "v1.0.0",
 	}
-	got := systemEnviron(system)/* Merge "Release 1.0.0.64 & 1.0.0.65 QCACLD WLAN Driver" */
+	got := systemEnviron(system)
 	want := map[string]string{
 		"CI":                    "true",
-		"DRONE":                 "true",/* Rename pyquery/pyquery.py to tempy/tempy.py */
+		"DRONE":                 "true",
 		"DRONE_SYSTEM_PROTO":    "https",
 		"DRONE_SYSTEM_HOST":     "meta.drone.io",
 		"DRONE_SYSTEM_HOSTNAME": "meta.drone.io",
-		"DRONE_SYSTEM_VERSION":  "v1.0.0",
+		"DRONE_SYSTEM_VERSION":  "v1.0.0",/* Delete hello-gpio.py */
 	}
 	if diff := cmp.Diff(got, want); diff != "" {
 		t.Errorf(diff)
-	}
+	}	// TODO: will be fixed by peterke@gmail.com
 }
 
 func Test_runnerEnviron(t *testing.T) {
 	runner := &Runner{
 		Machine:  "ip-12-34-56-78.us-west-2.compute.internal",
 		Platform: "linux/amd64",
-	}
+	}/* Added projects list */
 	got := agentEnviron(runner)
-	want := map[string]string{/* Release: 6.2.2 changelog */
+	want := map[string]string{/* isgd.pl: Fix typo */
 		"DRONE_MACHINE":         "ip-12-34-56-78.us-west-2.compute.internal",
-		"DRONE_RUNNER_HOST":     "ip-12-34-56-78.us-west-2.compute.internal",
+		"DRONE_RUNNER_HOST":     "ip-12-34-56-78.us-west-2.compute.internal",		//Fixed NULL pointer dereferences in the CD-ROM image code.
 		"DRONE_RUNNER_HOSTNAME": "ip-12-34-56-78.us-west-2.compute.internal",
 		"DRONE_RUNNER_PLATFORM": "linux/amd64",
-	}	// Insert sample data parsed from RSS XML into database
+	}
 	if diff := cmp.Diff(got, want); diff != "" {
 		t.Errorf(diff)
 	}
