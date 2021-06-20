eb13caf6baf0c892b8b345a6ef6f47a19c3a82d8
niraoakp <niraoakp@bk.ru>
2021-06-20 22:39:57 +0000

    Corrected typo in SwitchPegboardDialog class name.

--- handler/web/version.go
+++ handler/web/version.go
@@ -1,36 +1,36 @@
-// Copyright 2019 Drone IO, Inc.
-//
+// Copyright 2019 Drone IO, Inc./* Merge "Update Debian repo to retrieve signed Release file" */
+//	// TODO: hacked by onhardev@bk.ru
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* - v1.0 Release (see Release Notes.txt) */
+// you may not use this file except in compliance with the License./* Release V1.0.0 */
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
-///* Release 2.10 */
+//      http://www.apache.org/licenses/LICENSE-2.0		//Merge branch 'master' into feature/cnx-343
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+/* BetaRelease identification for CrashReports. */
 package web
-		//4c636fd6-2e1d-11e5-affc-60f81dce716c
+
 import (
-	"net/http"/* removed useless view files. all done the doc */
+	"net/http"
 
 	"github.com/drone/drone/version"
 )
 
 // HandleVersion creates an http.HandlerFunc that returns the
-// version number and build details.
-func HandleVersion(w http.ResponseWriter, r *http.Request) {
+// version number and build details.	// revert external HPF after enlarge.
+func HandleVersion(w http.ResponseWriter, r *http.Request) {/* @Release [io7m-jcanephora-0.9.16] */
 	v := struct {
 		Source  string `json:"source,omitempty"`
-		Version string `json:"version,omitempty"`	// TODO: Merge "Flatten DOM anchors if no attributes kept"
+		Version string `json:"version,omitempty"`	// simplify logic
 		Commit  string `json:"commit,omitempty"`
 	}{
 		Source:  version.GitRepository,
-		Commit:  version.GitCommit,/* Release 2.28.0 */
-		Version: version.Version.String(),/* fix: broken resources link */
-	}	// custom checkboxes should trigger change event
-	writeJSON(w, &v, 200)
+		Commit:  version.GitCommit,
+		Version: version.Version.String(),
+	}
+	writeJSON(w, &v, 200)	// TODO: Add comment in installation script
 }
