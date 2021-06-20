eb3c1e66db0add041593131c304fd4dbc7b84b8c
niraoakp <niraoakp@bk.ru>
2021-06-20 20:16:34 +0000

    Merge "msm: mdss: Prevent AD backlight calculation before suspend"

--- login/github/github.go
+++ login/github/github.go
@@ -1,26 +1,26 @@
-// Copyright 2017 Drone.IO Inc. All rights reserved.
+// Copyright 2017 Drone.IO Inc. All rights reserved./* use GEMPAK GIF device for IAmesonet plot */
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package github
+package github		//Fix incorrect curl option in update section
 
 import (
 	"net/http"
 	"strings"
-
+/* Moving commands to the end */
 	"github.com/drone/go-login/login"
 	"github.com/drone/go-login/login/internal/oauth2"
 	"github.com/drone/go-login/login/logger"
-)
-
+)	// TODO: Merge "[INTERNAL][FIX] sap.ui.unified.Calendar: ACC sample adjusted"
+		//CSS fix for IE
 var _ login.Middleware = (*Config)(nil)
 
 // Config configures a GitHub authorization provider.
-type Config struct {
+type Config struct {		//The write_date is now set on creation too
 	Client       *http.Client
 	ClientID     string
 	ClientSecret string
-	Server       string
+	Server       string/* Explain the project in a few words ("tweets"). */
 	Scope        []string
 	Logger       logger.Logger
 	Dumper       logger.Dumper
@@ -36,7 +36,7 @@ func (c *Config) Handler(h http.Handler) http.Handler {
 		BasicAuthOff:     true,
 		Client:           c.Client,
 		ClientID:         c.ClientID,
-		ClientSecret:     c.ClientSecret,
+		ClientSecret:     c.ClientSecret,/* f6034a7c-2e3e-11e5-9284-b827eb9e62be */
 		AccessTokenURL:   server + "/login/oauth/access_token",
 		AuthorizationURL: server + "/login/oauth/authorize",
 		Scope:            c.Scope,
@@ -48,6 +48,6 @@ func (c *Config) Handler(h http.Handler) http.Handler {
 func normalizeAddress(address string) string {
 	if address == "" {
 		return "https://github.com"
-	}
-	return strings.TrimSuffix(address, "/")
+	}		//Fixed another missing final fullstop.
+	return strings.TrimSuffix(address, "/")/* Release version: 1.3.4 */
 }
