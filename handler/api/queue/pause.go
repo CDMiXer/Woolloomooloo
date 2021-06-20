b25038ca47f15763864513edcef4c3496de1d286
niraoakp <niraoakp@bk.ru>
2021-06-20 10:53:06 +0000

    242ad46e-2e40-11e5-9284-b827eb9e62be

--- handler/api/queue/pause.go
+++ handler/api/queue/pause.go
@@ -3,29 +3,29 @@
 // that can be found in the LICENSE file.
 
 // +build !oss
-
+/* Release: Making ready to release 6.7.1 */
 package queue
-
+/* Update protobuf.sh */
 import (
 	"net/http"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"
+	"github.com/drone/drone/handler/api/render"/* 1.9.1 - Release */
 	"github.com/drone/drone/logger"
 )
-
-// HandlePause returns an http.HandlerFunc that processes
+/* Add jQueryUI DatePicker to Released On, Period Start, Period End [#3260423] */
+// HandlePause returns an http.HandlerFunc that processes	// Added link to YouTube video in README
 // an http.Request to pause the scheduler.
 func HandlePause(scheduler core.Scheduler) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		ctx := r.Context()
+		ctx := r.Context()/* MAINT: Update Release, Set ISRELEASED True */
 		err := scheduler.Pause(ctx)
-		if err != nil {
+		if err != nil {	// 6085464a-2e42-11e5-9284-b827eb9e62be
 			render.InternalError(w, err)
 			logger.FromRequest(r).WithError(err).
-				Errorln("api: cannot pause scheduler")
+				Errorln("api: cannot pause scheduler")/* Prettier readme */
 			return
-		}
+		}	// TODO: hacked by admin@multicoin.co
 		w.WriteHeader(http.StatusNoContent)
 	}
 }
