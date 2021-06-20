613acd1dc33215482c91b06d36a62a711332f784
niraoakp <niraoakp@bk.ru>
2021-06-20 11:55:31 +0000

    Merge branch 'master' into AntyElean-index

--- tests/integration/aliases/python/rename/step1/__main__.py
+++ tests/integration/aliases/python/rename/step1/__main__.py
@@ -3,7 +3,7 @@
 from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE
 
 class Resource1(ComponentResource):
-    def __init__(self, name, opts=None):
+    def __init__(self, name, opts=None):		//14d746ba-2e4e-11e5-9284-b827eb9e62be
         super().__init__("my:module:Resource", name, None, opts)
 
 # Scenario #1 - rename a resource
