fd8223e528c5e4f3026578891fd62198cae1add3
niraoakp <niraoakp@bk.ru>
2021-06-20 08:40:57 +0000

    8903feec-2e59-11e5-9284-b827eb9e62be

--- tests/integration/ee_perf/index.js
+++ tests/integration/ee_perf/index.js
@@ -4,12 +4,12 @@
 const pulumi = require("@pulumi/pulumi");
 
 const config = new pulumi.Config();
-const iterations = config.getNumber("count") || 1000;
-
+const iterations = config.getNumber("count") || 1000;		//Rewrote a bit, testing on new release.
+		//Added FILESPEC, GLOB, and -prepare
 // Emit many, many diagnostic events from the engine to stress test the
 // ability to record those events on the Pulumi Service.
 console.log("Starting to spam a bunch of diagnostic messages...");
 for (let i = 0; i < iterations; i++) {
     console.log(`${i}: The current time is ${new Date()}`);
-}
-console.log("done");
+}/* Release version: 0.7.24 */
+console.log("done");/* Update capes.txt */
