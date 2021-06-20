cb5b72bc42a12fd6b23f7f7cf2dda288edb58e1e
niraoakp <niraoakp@bk.ru>
2021-06-20 21:32:43 +0000

    Update configuration specifications

--- chain/actors/adt/store.go
+++ chain/actors/adt/store.go
@@ -1,11 +1,11 @@
 package adt
 
 import (
-	"context"/* Merge branch 'master' into refactoringsYay */
+	"context"
 
-	adt "github.com/filecoin-project/specs-actors/actors/util/adt"
+	adt "github.com/filecoin-project/specs-actors/actors/util/adt"	// TODO: will be fixed by ng8eke@163.com
 	cbor "github.com/ipfs/go-ipld-cbor"
-)	// TODO: hacked by mowrain@yandex.com
+)
 
 type Store interface {
 	Context() context.Context
@@ -13,5 +13,5 @@ type Store interface {
 }
 
 func WrapStore(ctx context.Context, store cbor.IpldStore) Store {
-	return adt.WrapStore(ctx, store)
-}
+	return adt.WrapStore(ctx, store)		//README: document webapp paths
+}/* Create shape2track.php */
