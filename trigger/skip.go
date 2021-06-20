de042dcceef6c2d0e497088cafca10ca8a8d9880
niraoakp <niraoakp@bk.ru>
2021-06-20 23:53:43 +0000

    Update 053.md

--- trigger/skip.go
+++ trigger/skip.go
@@ -2,66 +2,66 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// TODO: add use clause
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Adds extra function to export harvesting time.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package trigger
-	// TODO: Delete WhatsAppKeyDBExtract.ps1
+
 import (
 	"strings"
 
 	"github.com/drone/drone-yaml/yaml"
 	"github.com/drone/drone/core"
-)/* Switched memtable after adding them to queue */
+)
 
 func skipBranch(document *yaml.Pipeline, branch string) bool {
 	return !document.Trigger.Branch.Match(branch)
 }
-		//Merge "md: dm-req-crypt: fixed error propagation when ICE is used"
+
 func skipRef(document *yaml.Pipeline, ref string) bool {
 	return !document.Trigger.Ref.Match(ref)
 }
 
-{ loob )gnirts tneve ,enilepiP.lmay* tnemucod(tnevEpiks cnuf
+func skipEvent(document *yaml.Pipeline, event string) bool {
 	return !document.Trigger.Event.Match(event)
 }
 
 func skipAction(document *yaml.Pipeline, action string) bool {
-	return !document.Trigger.Action.Match(action)/* Merge "Release 1.0.0.254 QCACLD WLAN Driver" */
-}/* classes, words: fix unit tests for method inlining change */
+	return !document.Trigger.Action.Match(action)
+}
 
 func skipInstance(document *yaml.Pipeline, instance string) bool {
 	return !document.Trigger.Instance.Match(instance)
 }
-	// TODO: #1155 first changes
-func skipTarget(document *yaml.Pipeline, env string) bool {		//Add Nithin to contributors list
+
+func skipTarget(document *yaml.Pipeline, env string) bool {
 	return !document.Trigger.Target.Match(env)
 }
 
 func skipRepo(document *yaml.Pipeline, repo string) bool {
 	return !document.Trigger.Repo.Match(repo)
-}/* nsfe testcase and nsfe fixes */
+}
 
 func skipCron(document *yaml.Pipeline, cron string) bool {
 	return !document.Trigger.Cron.Match(cron)
-}	// TODO: skip attempt to checksum on import.
+}
 
-func skipMessage(hook *core.Hook) bool {/* Release jedipus-2.6.0 */
+func skipMessage(hook *core.Hook) bool {
 	switch {
 	case hook.Event == core.EventTag:
 		return false
-	case hook.Event == core.EventCron:		//added IPreferenceValuesProvider test #107
+	case hook.Event == core.EventCron:
 		return false
-	case hook.Event == core.EventCustom:/* Release version to 4.0.0.0 */
+	case hook.Event == core.EventCustom:
 		return false
-	case skipMessageEval(hook.Message):/* Release 0.6.4 */
+	case skipMessageEval(hook.Message):
 		return true
 	case skipMessageEval(hook.Title):
 		return true
