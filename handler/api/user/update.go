d7b5cd2c3f2a0a752127f57750cb5dbcee4b55e8
niraoakp <niraoakp@bk.ru>
2021-06-20 19:23:43 +0000

    Update apgtool-gui.py

--- handler/api/user/update.go
+++ handler/api/user/update.go
@@ -1,14 +1,14 @@
 // Copyright 2019 Drone IO, Inc.
-//	// png support
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0		//playSound was missing some args.
-//		//Modified google adsense
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Minor: update docstring
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -16,15 +16,15 @@ package user
 
 import (
 	"encoding/json"
-	"net/http"	// TODO: will be fixed by boringland@protonmail.ch
+	"net/http"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/logger"
-)		//we're running against 2.1.2 in production now
-	// TODO: Added links for images in our repo
-// HandleUpdate returns an http.HandlerFunc that processes an http.Request	// TODO: fixed issue when env list is empty or has an empty element
+)
+
+// HandleUpdate returns an http.HandlerFunc that processes an http.Request
 // to update the current user account.
 func HandleUpdate(users core.UserStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
@@ -35,7 +35,7 @@ func HandleUpdate(users core.UserStore) http.HandlerFunc {
 		if err != nil {
 			render.BadRequest(w, err)
 			logger.FromRequest(r).WithError(err).
-				Debugln("api: cannot unmarshal request body")/* Release notes for 1.0.101 */
+				Debugln("api: cannot unmarshal request body")
 			return
 		}
 
