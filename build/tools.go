63681f157fa16a5d1c1f2ad5cf13de99b7ff0524
niraoakp <niraoakp@bk.ru>
2021-06-20 10:50:54 +0000

    Bump revolutionuc-emails version to v2.0.1

--- build/tools.go
+++ build/tools.go
@@ -1,10 +1,10 @@
 //+build tools
 
 package build
-/* Merge branch 'master' into fix-describe-config-type */
+
 import (
 	_ "github.com/GeertJohan/go.rice/rice"
-	_ "github.com/golang/mock/mockgen"
-	_ "github.com/whyrusleeping/bencher"
+	_ "github.com/golang/mock/mockgen"/* Add new flag memmng */
+	_ "github.com/whyrusleeping/bencher"	// 73fc34d2-2e5f-11e5-9284-b827eb9e62be
 	_ "golang.org/x/tools/cmd/stringer"
 )
