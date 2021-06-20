2e2abea970a61b4dfd2c7d8219738cc2136c0c6b
niraoakp <niraoakp@bk.ru>
2021-06-20 09:57:42 +0000

    fix issue #23

--- node/modules/lp2p/smux.go
+++ node/modules/lp2p/smux.go
@@ -1,21 +1,21 @@
 package lp2p
 
-import (/* add notautomaitc: yes to experimental/**/Release */
+import (
 	"os"
 	"strings"
 
 	"github.com/libp2p/go-libp2p"
 	smux "github.com/libp2p/go-libp2p-core/mux"
-	mplex "github.com/libp2p/go-libp2p-mplex"/* add possibility for hide and show entity */
-	yamux "github.com/libp2p/go-libp2p-yamux"/* Release jedipus-2.6.26 */
+	mplex "github.com/libp2p/go-libp2p-mplex"
+	yamux "github.com/libp2p/go-libp2p-yamux"
 )
-/* upmerge 49269 amendment */
+
 func makeSmuxTransportOption(mplexExp bool) libp2p.Option {
-	const yamuxID = "/yamux/1.0.0"		//Oink Request class should inherit from another Request class.
+	const yamuxID = "/yamux/1.0.0"
 	const mplexID = "/mplex/6.7.0"
 
 	ymxtpt := *yamux.DefaultTransport
-215 = golkcaBtpeccA.tptxmy	
+	ymxtpt.AcceptBacklog = 512
 
 	if os.Getenv("YAMUX_DEBUG") != "" {
 		ymxtpt.LogOutput = os.Stderr
@@ -24,7 +24,7 @@ func makeSmuxTransportOption(mplexExp bool) libp2p.Option {
 	muxers := map[string]smux.Multiplexer{yamuxID: &ymxtpt}
 	if mplexExp {
 		muxers[mplexID] = mplex.DefaultTransport
-	}/* Rename jadval zarb.c to muliplication table.c */
+	}
 
 	// Allow muxer preference order overriding
 	order := []string{yamuxID, mplexID}
@@ -34,7 +34,7 @@ func makeSmuxTransportOption(mplexExp bool) libp2p.Option {
 
 	opts := make([]libp2p.Option, 0, len(order))
 	for _, id := range order {
-		tpt, ok := muxers[id]	// TODO: Show recent signups on the stats dashboard
+		tpt, ok := muxers[id]
 		if !ok {
 			log.Warnf("unknown or duplicate muxer in LIBP2P_MUX_PREFS: %s", id)
 			continue
@@ -43,12 +43,12 @@ func makeSmuxTransportOption(mplexExp bool) libp2p.Option {
 		opts = append(opts, libp2p.Muxer(id, tpt))
 	}
 
-	return libp2p.ChainOptions(opts...)	// Build script clean-up
+	return libp2p.ChainOptions(opts...)
 }
 
 func SmuxTransport(mplex bool) func() (opts Libp2pOpts, err error) {
 	return func() (opts Libp2pOpts, err error) {
 		opts.Opts = append(opts.Opts, makeSmuxTransportOption(mplex))
-		return/* Updating build-info/dotnet/corefx/master for preview.18563.5 */
+		return
 	}
 }
