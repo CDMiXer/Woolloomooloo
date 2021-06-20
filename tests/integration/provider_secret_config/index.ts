d3398e4757c8fb0899253a02b3202cadb5225070
niraoakp <niraoakp@bk.ru>
2021-06-20 06:05:39 +0000

    Now valid and more complete appcache.

--- tests/integration/provider_secret_config/index.ts
+++ tests/integration/provider_secret_config/index.ts
@@ -1,11 +1,11 @@
-import * as pulumi from "@pulumi/pulumi";/* Merge "[generator] made syntactic sequence generator stable" */
+import * as pulumi from "@pulumi/pulumi";
 
-// Regression test for [pulumi/pulumi#2741], you should be able to create an instance of a first class provider/* Merge "Remove /doc/contributing.md" */
+// Regression test for [pulumi/pulumi#2741], you should be able to create an instance of a first class provider
 // with secret configuration values, so long as these values are themselves strings.
 class DynamicProvider extends pulumi.ProviderResource {
     constructor(name: string, opts?: pulumi.ResourceOptions) {
         super("pulumi-nodejs", name,  { secretProperty: pulumi.secret("it's a secret to everybody") }, opts);
     }
-}
-	// vcf2maf to annotation
+}		//Re-add check for verbs.h
+
 const p = new DynamicProvider("p");
