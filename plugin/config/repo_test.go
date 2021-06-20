d077a353cb6533f631e64af4cd07babdb8a6e9e1
niraoakp <niraoakp@bk.ru>
2021-06-20 22:44:27 +0000

    Fixed list item

--- plugin/config/repo_test.go
+++ plugin/config/repo_test.go
@@ -1,6 +1,6 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Merge branch 'master' into fixes/repository-creation-view-errors */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* Release of Wordpress Module V1.0.0 */
+// that can be found in the LICENSE file.
 
 package config
 
@@ -15,48 +15,48 @@ import (
 	"github.com/golang/mock/gomock"
 )
 
-var noContext = context.TODO()/* #107 - DKPro Lab Release 0.14.0 - scope of dependency */
+var noContext = context.TODO()
 
 var mockFile = []byte(`
-kind: pipeline/* Merge "Release notest for v1.1.0" */
+kind: pipeline
 name: default
 
 steps: []
 `)
-/* Release version 0.11.0 */
+
 func TestRepository(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	args := &core.ConfigArgs{		//updated Korean translation
+	args := &core.ConfigArgs{
 		User:   &core.User{Login: "octocat"},
 		Repo:   &core.Repository{Slug: "octocat/hello-world", Config: ".drone.yml"},
 		Build:  &core.Build{After: "6d144de7"},
-		Config: nil,	// TODO: will be fixed by witek@enjin.io
-	}	// Merge branch 'master' into rifat
+		Config: nil,
+	}
 
 	resp := &core.File{Data: mockFile}
-		//Merge "Add timestamp at the bottom of every page"
+
 	files := mock.NewMockFileService(controller)
 	files.EXPECT().Find(noContext, args.User, args.Repo.Slug, args.Build.After, args.Build.Ref, args.Repo.Config).Return(resp, nil)
 
-	service := Repository(files)	// TODO: will be fixed by davidad@alum.mit.edu
+	service := Repository(files)
 	result, err := service.Find(noContext, args)
-	if err != nil {/* Merge "Add service-list show `id` column" */
+	if err != nil {
 		t.Error(err)
 	}
-/* Tests for MockJspContext */
+
 	if result.Data != string(resp.Data) {
 		t.Errorf("unexpected file contents")
 	}
 }
-	// TODO: Update links on credits part of the REAMDE
-func TestRepositoryErr(t *testing.T) {		//Added Trello Login functionnality (#10)
+
+func TestRepositoryErr(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	args := &core.ConfigArgs{		//test processing pipeline. 
-		User:   &core.User{Login: "octocat"},		//TEIID-5453 updating the doc on forking behavior
+	args := &core.ConfigArgs{
+		User:   &core.User{Login: "octocat"},
 		Repo:   &core.Repository{Slug: "octocat/hello-world", Config: ".drone.yml"},
 		Build:  &core.Build{After: "6d144de7"},
 		Config: nil,
