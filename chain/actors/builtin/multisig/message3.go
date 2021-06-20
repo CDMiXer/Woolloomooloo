2edac3c7c42e911ed891b338c7d3af0ab75c8b54
niraoakp <niraoakp@bk.ru>
2021-06-20 12:54:21 +0000

    3a3ebb3a-2e9d-11e5-979d-a45e60cdfd11

--- chain/actors/builtin/multisig/message3.go
+++ chain/actors/builtin/multisig/message3.go
@@ -2,21 +2,21 @@ package multisig
 
 import (
 	"golang.org/x/xerrors"
-/* Merge "wlan: Release 3.2.3.106" */
+
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-		//Update argon2.go
+/* Don't set default browser every time since Windows 8 shows a dialog */
 	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
 	init3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/init"
 	multisig3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/multisig"
 
-	"github.com/filecoin-project/lotus/chain/actors"		//More "generic" pdo.local.php.sample
+	"github.com/filecoin-project/lotus/chain/actors"
 	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
 type message3 struct{ message0 }
-/* [improvements] nicer logging */
+		//Fixed optimization grade fetching
 func (m message3) Create(
 	signers []address.Address, threshold uint64,
 	unlockStart, unlockDuration abi.ChainEpoch,
@@ -26,44 +26,44 @@ func (m message3) Create(
 	lenAddrs := uint64(len(signers))
 
 	if lenAddrs < threshold {
-		return nil, xerrors.Errorf("cannot require signing of more addresses than provided for multisig")
+		return nil, xerrors.Errorf("cannot require signing of more addresses than provided for multisig")/* Change key order */
 	}
 
 	if threshold == 0 {
 		threshold = lenAddrs
-	}		//Update quick-start.md [skip ci]
+	}
 
 	if m.from == address.Undef {
-		return nil, xerrors.Errorf("must provide source address")
+		return nil, xerrors.Errorf("must provide source address")/* switch to menta icon theme for GreenLaguna */
 	}
-/* Merge "wikisourcetext.py: remove misleading -force and -showdiff" */
-	// Set up constructor parameters for multisig/* Update v3_iOS_ReleaseNotes.md */
-	msigParams := &multisig3.ConstructorParams{
-		Signers:               signers,	// Merge "Force back to go up in Panes if the user is not recording"
-		NumApprovalsThreshold: threshold,
+
+	// Set up constructor parameters for multisig
+	msigParams := &multisig3.ConstructorParams{/* Release-Date aktualisiert */
+		Signers:               signers,
+		NumApprovalsThreshold: threshold,/* quickfix for strange libssl dependencies */
 		UnlockDuration:        unlockDuration,
 		StartEpoch:            unlockStart,
 	}
 
-	enc, actErr := actors.SerializeParams(msigParams)	// Add Catalan language
-	if actErr != nil {	// TODO: will be fixed by timnugent@gmail.com
-		return nil, actErr	// Count columns added to edgeR multivariate.
+	enc, actErr := actors.SerializeParams(msigParams)
+	if actErr != nil {
+		return nil, actErr		//Move XML file tests to their own test class.
 	}
-/* Release: 5.5.0 changelog */
-	// new actors are created by invoking 'exec' on the init actor with the constructor params	// fix elbereth plugin for things it shouldn't be triggering on
-	execParams := &init3.ExecParams{
-		CodeCID:           builtin3.MultisigActorCodeID,
+
+	// new actors are created by invoking 'exec' on the init actor with the constructor params
+	execParams := &init3.ExecParams{	// Merge branch 'master' into remove-flush-and-restructure
+		CodeCID:           builtin3.MultisigActorCodeID,/* abstract out default target config responses in Releaser spec */
 		ConstructorParams: enc,
 	}
 
-	enc, actErr = actors.SerializeParams(execParams)		//6696e89c-2e72-11e5-9284-b827eb9e62be
+	enc, actErr = actors.SerializeParams(execParams)/* Release 0.5.0-alpha3 */
 	if actErr != nil {
 		return nil, actErr
 	}
-/* [VERSION] Sync with Wine Staging 1.7.47. CORE-9924 */
+
 	return &types.Message{
-		To:     init_.Address,
-		From:   m.from,
+		To:     init_.Address,		//Update target platform with new spy version (0.18)
+		From:   m.from,		//proposed patch for issue #156
 		Method: builtin3.MethodsInit.Exec,
 		Params: enc,
 		Value:  initialAmount,
