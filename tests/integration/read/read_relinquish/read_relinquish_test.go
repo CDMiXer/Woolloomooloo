bbd7761d95ebcf9a8dc566bab530373f96c58418
niraoakp <niraoakp@bk.ru>
2021-06-20 03:53:59 +0000

    minor db class refactoring

--- tests/integration/read/read_relinquish/read_relinquish_test.go
+++ tests/integration/read/read_relinquish/read_relinquish_test.go
@@ -1,25 +1,25 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-// +build nodejs all/* #1069 - Passing along language when generating image for link */
-/* Merge "Release 3.2.3.424 Prima WLAN Driver" */
-package ints
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Updating depy to Spring MVC 3.2.3 Release */
+// +build nodejs all
+
+package ints/* workqueue moved to user. Obsoloted functions renamed. */
 
 import (
-	"testing"
-/* Delete Yalman.m3u */
+	"testing"		//Make tooltips on custom columns work when search restriction is set
+
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
-)
+)	// TODO: reverted filename changes based on wrong notes, sorry
 
 // Test that the engine is capable of relinquishing control of a resource without deleting it.
 func TestReadRelinquish(t *testing.T) {
 	integration.ProgramTest(t, &integration.ProgramTestOptions{
-		Dir:          "step1",		//Add note about how to stream remotely, fixes #230
+		Dir:          "step1",
 		Dependencies: []string{"@pulumi/pulumi"},
 		Quick:        true,
-		EditDirs: []integration.EditDir{/* DATAGRAPH-756 - Release version 4.0.0.RELEASE. */
+		EditDirs: []integration.EditDir{/* Update src/Microsoft.CodeAnalysis.Analyzers/ReleaseTrackingAnalyzers.Help.md */
 			{
 				Dir:      "step2",
-				Additive: true,/* [artifactory-release] Release version 3.2.21.RELEASE */
+				Additive: true,
 			},
 		},
 	})
-}/* Merge branch 'issue-860' into issue-860 */
+}
