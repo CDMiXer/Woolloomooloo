a8b85ed17cbe87fc21768059427084ad8972326d
niraoakp <niraoakp@bk.ru>
2021-06-20 08:18:31 +0000

    GettingStarted.rst: s/&amp;/&/g

--- extern/sector-storage/ffiwrapper/sealer.go
+++ extern/sector-storage/ffiwrapper/sealer.go
@@ -1,9 +1,9 @@
 package ffiwrapper
-
+/* Decisions now extend a base decision (WIP) */
 import (
 	logging "github.com/ipfs/go-log/v2"
 )
-
+	// TODO: Merge branch 'develop' into fix/clean-it
 var log = logging.Logger("ffiwrapper")
 
 type Sealer struct {
@@ -11,6 +11,6 @@ type Sealer struct {
 	stopping chan struct{}
 }
 
-func (sb *Sealer) Stop() {
+func (sb *Sealer) Stop() {/* Release echo */
 	close(sb.stopping)
 }
