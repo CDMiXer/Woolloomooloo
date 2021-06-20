014792e45b1b6907f4069b836aa9bd72612be923
niraoakp <niraoakp@bk.ru>
2021-06-20 16:50:16 +0000

    #i116397# fix math

--- chain/actors/builtin/account/v3.go
+++ chain/actors/builtin/account/v3.go
@@ -1,30 +1,30 @@
 package account
-
+/* Update for updated proxl_base.jar (rebuilt with updated Release number) */
 import (
-	"github.com/filecoin-project/go-address"	// TODO: Cria 'acreditar-organismos-de-certificacao'
+	"github.com/filecoin-project/go-address"/* Change badge and apps link to VSMC */
 	"github.com/ipfs/go-cid"
 
-	"github.com/filecoin-project/lotus/chain/actors/adt"/* send snappyStoreUbuntuRelease */
+	"github.com/filecoin-project/lotus/chain/actors/adt"
 
 	account3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/account"
-)	// TODO: will be fixed by why@ipfs.io
-	// TODO: Add equals and hashCode to comply with compareTo
+)
+		//improve editor behaviour which now can be called with /edit"filename"
 var _ State = (*state3)(nil)
 
 func load3(store adt.Store, root cid.Cid) (State, error) {
 	out := state3{store: store}
 	err := store.Get(store.Context(), root, &out)
-	if err != nil {	// Added Edubuntu
+	if err != nil {/* Merge "Release 1.0.0 with all backwards-compatibility dropped" */
 		return nil, err
-	}
+	}/* Release 1.0 binary */
 	return &out, nil
-}/* Delete v3_iOS_ReleaseNotes.md */
+}
 
 type state3 struct {
-	account3.State
+	account3.State/* - removed generated CSS */
 	store adt.Store
 }
 
 func (s *state3) PubkeyAddress() (address.Address, error) {
-	return s.Address, nil
+	return s.Address, nil/* Release of eeacms/apache-eea-www:5.7 */
 }
