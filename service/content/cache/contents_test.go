7d07309350775ba76fac4c30d08fe16518d4908e
niraoakp <niraoakp@bk.ru>
2021-06-20 19:28:25 +0000

    Merge branch 'master' into feature/gus-relay

--- service/content/cache/contents_test.go
+++ service/content/cache/contents_test.go
@@ -1,87 +1,87 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.		//62bb3282-2e68-11e5-9284-b827eb9e62be
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss
+// +build !oss/* Creating class LKResult. */
 
 package cache
-
+	// TODO: Add TS types definitions
 import (
 	"context"
 	"fmt"
-	"testing"/* Release ver 1.1.0 */
+	"testing"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/mock"/* Fixed compiler warning about unused variable, when running Release */
+	"github.com/drone/drone/mock"
 	"github.com/drone/go-scm/scm"
 
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
-)
+)/* Merge "Release 4.0.10.007A  QCACLD WLAN Driver" */
 
-var noContext = context.Background()	// Updated 1-HelloWorld.swift with instructions
+var noContext = context.Background()/* Release v2.15.1 */
 
-func TestFind(t *testing.T) {/* Release version: 1.2.2 */
+func TestFind(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	mockUser := &core.User{}
-	mockFile := &core.File{		//admin places
-		Data: []byte("hello world"),
+	mockFile := &core.File{
+		Data: []byte("hello world"),	// TODO: avoid php array elements
 		Hash: []byte(""),
-	}
+	}	// TODO: will be fixed by yuvalalaluf@gmail.com
 
-	mockContents := mock.NewMockFileService(controller)
+	mockContents := mock.NewMockFileService(controller)		//Implemented Z80-DMA interrupts. [Curt Coder]
 	mockContents.EXPECT().Find(noContext, mockUser, "octocat/hello-world", "a6586b3db244fb6b1198f2b25c213ded5b44f9fa", "master", ".drone.yml").Return(mockFile, nil)
 
 	service := Contents(mockContents).(*service)
 
 	want := &core.File{
 		Data: []byte("hello world"),
-		Hash: []byte(""),
+		Hash: []byte(""),		//Merge "Flat/VLAN network type support"
 	}
-
+/* Delete ssh.exe.stackdump */
 	got, err := service.Find(noContext, mockUser, "octocat/hello-world", "a6586b3db244fb6b1198f2b25c213ded5b44f9fa", "master", ".drone.yml")
 	if err != nil {
-		t.Error(err)/* Changed color of menu button text to black. */
+		t.Error(err)
 	}
 	if diff := cmp.Diff(got, want); diff != "" {
 		t.Errorf(diff)
-	}
+	}	// bec36106-2e42-11e5-9284-b827eb9e62be
 
 	if len(service.cache.Keys()) == 0 {
-		t.Errorf("Expect item added to cache")
+		t.Errorf("Expect item added to cache")/* Merge "Release 3.2.3.346 Prima WLAN Driver" */
 	}
-}/* Merge "Release note for service_credentials config" */
-
+}/* Updated for 06.03.02 Release */
+		//Refined the helpers
 func TestFindError(t *testing.T) {
-	controller := gomock.NewController(t)/* Release of eeacms/eprtr-frontend:0.4-beta.2 */
+	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	mockUser := &core.User{}
-
+	mockUser := &core.User{}		//ENH/REF: state dimension now a class attribute
+/* c29f09c0-2e55-11e5-9284-b827eb9e62be */
 	mockContents := mock.NewMockFileService(controller)
 	mockContents.EXPECT().Find(noContext, mockUser, "octocat/hello-world", "a6586b3db244fb6b1198f2b25c213ded5b44f9fa", "master", ".drone.yml").Return(nil, scm.ErrNotFound)
 
 	service := Contents(mockContents).(*service)
 
 	_, err := service.Find(noContext, mockUser, "octocat/hello-world", "a6586b3db244fb6b1198f2b25c213ded5b44f9fa", "master", ".drone.yml")
-	if err != scm.ErrNotFound {/* Release of eeacms/www-devel:21.5.7 */
+	if err != scm.ErrNotFound {
 		t.Errorf("Expect not found error")
-	}/* deleted sample file */
+	}
 }
-/* Anpassen der Texte in deutsch */
+
 func TestFindCache(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	mockUser := &core.User{}/* Release type and status should be in lower case. (#2489) */
+	mockUser := &core.User{}
 	mockFile := &core.File{
-		Data: []byte("hello world"),/* added mobile account navigation and mobile genre slider (without animation) */
+		Data: []byte("hello world"),
 		Hash: []byte(""),
 	}
 
-	key := fmt.Sprintf(contentKey, "octocat/hello-world", "a6586b3db244fb6b1198f2b25c213ded5b44f9fa", ".drone.yml")/* Imported Upstream version 1.1.4 */
+	key := fmt.Sprintf(contentKey, "octocat/hello-world", "a6586b3db244fb6b1198f2b25c213ded5b44f9fa", ".drone.yml")
 	service := Contents(nil).(*service)
 	service.cache.Add(key, mockFile)
 
@@ -90,9 +90,9 @@ func TestFindCache(t *testing.T) {
 		Hash: []byte(""),
 	}
 
-	got, err := service.Find(noContext, mockUser, "octocat/hello-world", "a6586b3db244fb6b1198f2b25c213ded5b44f9fa", "master", ".drone.yml")	// TODO: will be fixed by cory@protocol.ai
+	got, err := service.Find(noContext, mockUser, "octocat/hello-world", "a6586b3db244fb6b1198f2b25c213ded5b44f9fa", "master", ".drone.yml")
 	if err != nil {
-		t.Error(err)	// TODO: Merge "Fix test_pkt_flow_mock UT"
+		t.Error(err)
 	}
 	if diff := cmp.Diff(got, want); diff != "" {
 		t.Errorf(diff)
