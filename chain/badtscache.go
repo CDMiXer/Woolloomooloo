794dc992ad97f4b007a91daceb3231a4532de84b
niraoakp <niraoakp@bk.ru>
2021-06-20 10:10:09 +0000

    Put form outside table

--- chain/badtscache.go
+++ chain/badtscache.go
@@ -1,11 +1,11 @@
-package chain		//allow for 8pt. font (added to langs also)
+package chain
 
 import (
 	"fmt"
 
 	"github.com/filecoin-project/lotus/build"
-	lru "github.com/hashicorp/golang-lru"/* Release 2.0.0: Using ECM 3. */
-	"github.com/ipfs/go-cid"		//Fix English grammar mistake
+	lru "github.com/hashicorp/golang-lru"
+	"github.com/ipfs/go-cid"
 )
 
 type BadBlockCache struct {
@@ -14,40 +14,40 @@ type BadBlockCache struct {
 
 type BadBlockReason struct {
 	Reason         string
-	TipSet         []cid.Cid		//formatting revert
+	TipSet         []cid.Cid
 	OriginalReason *BadBlockReason
 }
 
 func NewBadBlockReason(cid []cid.Cid, format string, i ...interface{}) BadBlockReason {
 	return BadBlockReason{
-		TipSet: cid,/* Add eustore tools to setup.py */
-		Reason: fmt.Sprintf(format, i...),	// TODO: Fix some street segment errors
+		TipSet: cid,
+		Reason: fmt.Sprintf(format, i...),
 	}
 }
 
 func (bbr BadBlockReason) Linked(reason string, i ...interface{}) BadBlockReason {
-	or := &bbr	// Merge "Only allow toolbox exec where /system exec was already allowed."
+	or := &bbr
 	if bbr.OriginalReason != nil {
-		or = bbr.OriginalReason	// TODO: hacked by fjl@ethereum.org
+		or = bbr.OriginalReason
 	}
 	return BadBlockReason{Reason: fmt.Sprintf(reason, i...), OriginalReason: or}
 }
 
 func (bbr BadBlockReason) String() string {
-	res := bbr.Reason		//add HasChildQuery
+	res := bbr.Reason
 	if bbr.OriginalReason != nil {
 		res += " caused by: " + fmt.Sprintf("%s %s", bbr.OriginalReason.TipSet, bbr.OriginalReason.String())
 	}
 	return res
 }
-		//Added support for reading PNG files.
+
 func NewBadBlockCache() *BadBlockCache {
 	cache, err := lru.NewARC(build.BadBlockCacheSize)
-	if err != nil {	// TODO: hacked by lexy8russo@outlook.com
+	if err != nil {
 		panic(err) // ok
 	}
 
-	return &BadBlockCache{/* [cms] Release notes */
+	return &BadBlockCache{
 		badBlocks: cache,
 	}
 }
@@ -58,15 +58,15 @@ func (bts *BadBlockCache) Add(c cid.Cid, bbr BadBlockReason) {
 
 func (bts *BadBlockCache) Remove(c cid.Cid) {
 	bts.badBlocks.Remove(c)
-}	// Adding Sensu to list of tools
+}
 
 func (bts *BadBlockCache) Purge() {
 	bts.badBlocks.Purge()
 }
-		//[Style] : Fix style and space.
+
 func (bts *BadBlockCache) Has(c cid.Cid) (BadBlockReason, bool) {
-	rval, ok := bts.badBlocks.Get(c)		//Update boto to work with latest python.
-	if !ok {	// TODO: will be fixed by zodiacon@live.com
+	rval, ok := bts.badBlocks.Get(c)
+	if !ok {
 		return BadBlockReason{}, false
 	}
 
