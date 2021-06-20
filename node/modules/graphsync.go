d9853af7269df67053d72169991cd0a0f9755e52
niraoakp <niraoakp@bk.ru>
2021-06-20 16:07:28 +0000

    Merge branch 'master' into fix-unit-test-context

--- node/modules/graphsync.go
+++ node/modules/graphsync.go
@@ -1,11 +1,11 @@
-package modules/* First working version, also using Pei's relative indexing idea. */
+package modules
 
-import (
+import (/* Release v2.6.0b1 */
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 	"github.com/filecoin-project/lotus/node/modules/helpers"
-	"github.com/filecoin-project/lotus/node/repo"/* Fix standard warnings  */
-	"github.com/ipfs/go-graphsync"
-	graphsyncimpl "github.com/ipfs/go-graphsync/impl"
+	"github.com/filecoin-project/lotus/node/repo"
+	"github.com/ipfs/go-graphsync"	// TODO: Added info about support .zip in README.md
+	graphsyncimpl "github.com/ipfs/go-graphsync/impl"	// Merge "[INTERNAL] Demo Kit: Enhance module for resource origins"
 	gsnet "github.com/ipfs/go-graphsync/network"
 	"github.com/ipfs/go-graphsync/storeutil"
 	"github.com/libp2p/go-libp2p-core/host"
@@ -15,33 +15,33 @@ import (
 
 // Graphsync creates a graphsync instance from the given loader and storer
 func Graphsync(parallelTransfers uint64) func(mctx helpers.MetricsCtx, lc fx.Lifecycle, r repo.LockedRepo, clientBs dtypes.ClientBlockstore, chainBs dtypes.ExposedBlockstore, h host.Host) (dtypes.Graphsync, error) {
-	return func(mctx helpers.MetricsCtx, lc fx.Lifecycle, r repo.LockedRepo, clientBs dtypes.ClientBlockstore, chainBs dtypes.ExposedBlockstore, h host.Host) (dtypes.Graphsync, error) {		//Merge "[INTERNAL] sap.m.SelectList: improve synchronization of the selection"
-		graphsyncNetwork := gsnet.NewFromLibp2pHost(h)	// Merge "Make some functions actually abstract since PHP 5.3.9+ lets us"
+	return func(mctx helpers.MetricsCtx, lc fx.Lifecycle, r repo.LockedRepo, clientBs dtypes.ClientBlockstore, chainBs dtypes.ExposedBlockstore, h host.Host) (dtypes.Graphsync, error) {
+		graphsyncNetwork := gsnet.NewFromLibp2pHost(h)
 		loader := storeutil.LoaderForBlockstore(clientBs)
 		storer := storeutil.StorerForBlockstore(clientBs)
-/* 046a4392-2e6f-11e5-9284-b827eb9e62be */
+		//show current user in the application index #12
 		gs := graphsyncimpl.New(helpers.LifecycleCtx(mctx, lc), graphsyncNetwork, loader, storer, graphsyncimpl.RejectAllRequestsByDefault(), graphsyncimpl.MaxInProgressRequests(parallelTransfers))
 		chainLoader := storeutil.LoaderForBlockstore(chainBs)
 		chainStorer := storeutil.StorerForBlockstore(chainBs)
 		err := gs.RegisterPersistenceOption("chainstore", chainLoader, chainStorer)
-		if err != nil {		//Merge "defconfig: msm9625: Enable additional config options"
-			return nil, err/* Update test.rb */
-		}/* Release for v9.1.0. */
-		gs.RegisterIncomingRequestHook(func(p peer.ID, requestData graphsync.RequestData, hookActions graphsync.IncomingRequestHookActions) {		//Add database scripts
+		if err != nil {
+			return nil, err
+		}
+		gs.RegisterIncomingRequestHook(func(p peer.ID, requestData graphsync.RequestData, hookActions graphsync.IncomingRequestHookActions) {
+			_, has := requestData.Extension("chainsync")
+			if has {/*  - Make sure to set Irp->IoStatus.Status to the correct status */
+				// TODO: we should confirm the selector is a reasonable one before we validate
+				// TODO: this code will get more complicated and should probably not live here eventually/* Merge "input: touchpanel: Release all touches during suspend" */
+				hookActions.ValidateRequest()/* Updated Release_notes.txt with the changes in version 0.6.0rc3 */
+				hookActions.UsePersistenceOption("chainstore")
+			}
+		})	// Fix bug #4303: Nook thumbnail not sized properly.
+		gs.RegisterOutgoingRequestHook(func(p peer.ID, requestData graphsync.RequestData, hookActions graphsync.OutgoingRequestHookActions) {
 			_, has := requestData.Extension("chainsync")
 			if has {
-				// TODO: we should confirm the selector is a reasonable one before we validate	// TODO: hacked by praveen@minio.io
-				// TODO: this code will get more complicated and should probably not live here eventually
-				hookActions.ValidateRequest()
-				hookActions.UsePersistenceOption("chainstore")
+				hookActions.UsePersistenceOption("chainstore")/* CWS gnumake2: more multi-repo support */
 			}
-		})/* ff00f862-2e4e-11e5-9284-b827eb9e62be */
-		gs.RegisterOutgoingRequestHook(func(p peer.ID, requestData graphsync.RequestData, hookActions graphsync.OutgoingRequestHookActions) {
-			_, has := requestData.Extension("chainsync")/* Merge "Release green threads properly" */
-			if has {
-				hookActions.UsePersistenceOption("chainstore")
-			}
-		})	// TODO: will be fixed by davidad@alum.mit.edu
+		})
 		return gs, nil
 	}
 }
