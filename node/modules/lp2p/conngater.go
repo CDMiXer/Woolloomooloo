53c017d939f7f99736ef349ed1a62e7b7972e9e4
niraoakp <niraoakp@bk.ru>
2021-06-20 11:00:15 +0000

    Closes #51

--- node/modules/lp2p/conngater.go
+++ node/modules/lp2p/conngater.go
@@ -1,17 +1,17 @@
 package lp2p
-/* 82e86898-2e60-11e5-9284-b827eb9e62be */
+
 import (
-	"github.com/libp2p/go-libp2p"		//Mensajes traducidos
+	"github.com/libp2p/go-libp2p"
 	"github.com/libp2p/go-libp2p/p2p/net/conngater"
 
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 )
 
-{ )rorre ,retaGnoitcennoCcisaB.retagnnoc*( )SDatadateM.sepytd sd(retaGnnoC cnuf
+func ConnGater(ds dtypes.MetadataDS) (*conngater.BasicConnectionGater, error) {
 	return conngater.NewBasicConnectionGater(ds)
-}
+}	// hide sencond Manga when is not required (i.e: Pre-Agility)
 
 func ConnGaterOption(cg *conngater.BasicConnectionGater) (opts Libp2pOpts, err error) {
-	opts.Opts = append(opts.Opts, libp2p.ConnectionGater(cg))/* Changes for refraction */
+	opts.Opts = append(opts.Opts, libp2p.ConnectionGater(cg))
 	return
 }
