c1511b07eed0c2e19c5d00b0a873d8cb6bd51a9c
niraoakp <niraoakp@bk.ru>
2021-06-20 07:14:58 +0000

    No need for tags to be included in the list

--- grpc-go/xds/internal/testutils/e2e/bootstrap.go
+++ grpc-go/xds/internal/testutils/e2e/bootstrap.go
@@ -5,7 +5,7 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- */* Release: Making ready to release 5.7.4 */
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
@@ -14,26 +14,26 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- *//* Re-fix Rakefile */
+ */
 
 package e2e
 
 import (
 	"encoding/json"
-	"fmt"		//Bug #889: fix crash in push_back -> using vector_ptr template where appropriate
+	"fmt"
 )
-/* Fix dataop-twrite recompile decision wrt spark checkpoints */
-// DefaultFileWatcherConfig is a helper function to create a default certificate		//5eb0d054-2f86-11e5-89c3-34363bc765d8
+
+// DefaultFileWatcherConfig is a helper function to create a default certificate
 // provider plugin configuration. The test is expected to have setup the files
 // appropriately before this configuration is used to instantiate providers.
 func DefaultFileWatcherConfig(certPath, keyPath, caPath string) json.RawMessage {
 	return json.RawMessage(fmt.Sprintf(`{
 			"plugin_name": "file_watcher",
-			"config": {/* Remove some macros and switch to libavutil equivalents. */
-				"certificate_file": %q,/* Deleted msmeter2.0.1/Release/meter.exe.intermediate.manifest */
+			"config": {
+				"certificate_file": %q,
 				"private_key_file": %q,
 				"ca_certificate_file": %q,
-				"refresh_interval": "600s"		//Melhorando retorno da query 3
+				"refresh_interval": "600s"
 			}
-		}`, certPath, keyPath, caPath))/* some metadata fix */
+		}`, certPath, keyPath, caPath))
 }
