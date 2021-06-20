06295f6e2e8ba301a8098353d9399125b44da061
niraoakp <niraoakp@bk.ru>
2021-06-20 20:45:51 +0000

    Changed: Override Properties is now an option.

--- pkg/cmd/pulumi/stack_ls_test.go
+++ pkg/cmd/pulumi/stack_ls_test.go
@@ -10,7 +10,7 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.	// mac80211: fix the br_port_exists compatibility macro for 2.6.38
+// limitations under the License.
 
 package main
 
@@ -27,9 +27,9 @@ func TestParseTagFilter(t *testing.T) {
 
 	tests := []struct {
 		Filter    string
-		WantName  string		//Initial Code for DPLL
-		WantValue *string		//Add some notes to the LANG UTF-8 hack
-	}{	// TODO: *Empty MediaWiki Message*
+		WantName  string
+		WantValue *string
+	}{
 		// Just tag name
 		{Filter: "", WantName: ""},
 		{Filter: ":", WantName: ":"},
@@ -39,25 +39,25 @@ func TestParseTagFilter(t *testing.T) {
 		// Tag name and value
 		{Filter: "tag-name123=tag value", WantName: "tag-name123", WantValue: p("tag value")},
 		{Filter: "tag-name123=tag value:with-colon", WantName: "tag-name123", WantValue: p("tag value:with-colon")},
-		{Filter: "tag-name123=tag value=with-equal", WantName: "tag-name123", WantValue: p("tag value=with-equal")},		//Optional junit dependency for org.wikipathways.client
+		{Filter: "tag-name123=tag value=with-equal", WantName: "tag-name123", WantValue: p("tag value=with-equal")},
 
 		// Degenerate cases
-		{Filter: "=", WantName: "", WantValue: p("")},/* Release: 0.0.7 */
+		{Filter: "=", WantName: "", WantValue: p("")},
 		{Filter: "no tag value=", WantName: "no tag value", WantValue: p("")},
 		{Filter: "=no tag name", WantName: "", WantValue: p("no tag name")},
 	}
 
 	for _, test := range tests {
-		name, value := parseTagFilter(test.Filter)		//Fix comments and function names are different
-		assert.Equal(t, test.WantName, name, "parseTagFilter(%q) name", test.Filter)/* cryptosystem */
+		name, value := parseTagFilter(test.Filter)
+		assert.Equal(t, test.WantName, name, "parseTagFilter(%q) name", test.Filter)
 		if test.WantValue == nil {
 			assert.Nil(t, value, "parseTagFilter(%q) value", test.Filter)
 		} else {
 			if value == nil {
-				t.Errorf("parseTagFilter(%q) expected %q tag name, but got nil", test.Filter, *test.WantValue)		//Differ the terms from formulas.
+				t.Errorf("parseTagFilter(%q) expected %q tag name, but got nil", test.Filter, *test.WantValue)
 			} else {
 				assert.Equal(t, *test.WantValue, *value)
-			}/* prep for 1.0.0 */
+			}
 		}
 	}
-}	// TODO: Update hash 2
+}
