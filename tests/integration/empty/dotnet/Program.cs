3704fe835fecc37f3b6a2283a209ceda258a83cd
niraoakp <niraoakp@bk.ru>
2021-06-20 19:13:46 +0000

    updating title

--- tests/integration/empty/dotnet/Program.cs
+++ tests/integration/empty/dotnet/Program.cs
@@ -1,11 +1,11 @@
-﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.	// TODO: trying to memory>struct a non-struct class is feptastic
+﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
 
 using System.Threading.Tasks;
 using Pulumi;
 
 class Program
 {
-    static Task<int> Main(string[] args)/* Release Version 1.1.3 */
+    static Task<int> Main(string[] args)
     {
         return Deployment.RunAsync(() => {});
     }
