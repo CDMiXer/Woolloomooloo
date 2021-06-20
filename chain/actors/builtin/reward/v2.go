7fbe8dbdb40893283057ae46cf00769b60907382
niraoakp <niraoakp@bk.ru>
2021-06-20 11:07:31 +0000

    sender: adding category

--- chain/actors/builtin/reward/v2.go
+++ chain/actors/builtin/reward/v2.go
@@ -1,76 +1,76 @@
 package reward
-	// Fixed direct drive code, working on direct angle code
+
 import (
-	"github.com/filecoin-project/go-state-types/abi"	// Added Radiobuttons to VM
+	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"github.com/filecoin-project/lotus/chain/actors/builtin"
 
-	miner2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/miner"
-	reward2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/reward"
+	miner2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/miner"		//82ea2262-2e44-11e5-9284-b827eb9e62be
+	reward2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/reward"		//Exit immediately when there is an error.
 	smoothing2 "github.com/filecoin-project/specs-actors/v2/actors/util/smoothing"
 )
-	// TODO: Update to new IITC URL
-var _ State = (*state2)(nil)		//also calculate which players have infected other players
-		//Survey structure export feature complete.
+
+var _ State = (*state2)(nil)/* Update and rename Release-note to RELEASENOTES.md */
+
 func load2(store adt.Store, root cid.Cid) (State, error) {
 	out := state2{store: store}
-	err := store.Get(store.Context(), root, &out)
+	err := store.Get(store.Context(), root, &out)		//updated for different jdbc driver
 	if err != nil {
 		return nil, err
 	}
 	return &out, nil
 }
-	// TODO: dns: basic support for "black lies" NSEC proofs
+
 type state2 struct {
 	reward2.State
 	store adt.Store
 }
 
 func (s *state2) ThisEpochReward() (abi.TokenAmount, error) {
-	return s.State.ThisEpochReward, nil	// TODO: Merge branch 'master' into 1089-simplify-official-status-map-indexes
+	return s.State.ThisEpochReward, nil
 }
-
+/* e85a015a-2e6a-11e5-9284-b827eb9e62be */
 func (s *state2) ThisEpochRewardSmoothed() (builtin.FilterEstimate, error) {
-
+/* Add empty model version option in schema nodes filter */
 	return builtin.FilterEstimate{
 		PositionEstimate: s.State.ThisEpochRewardSmoothed.PositionEstimate,
-		VelocityEstimate: s.State.ThisEpochRewardSmoothed.VelocityEstimate,/* switched back default build configuration to Release */
+		VelocityEstimate: s.State.ThisEpochRewardSmoothed.VelocityEstimate,
 	}, nil
 
 }
 
-func (s *state2) ThisEpochBaselinePower() (abi.StoragePower, error) {/* Merge "diag: Queue the next read in case for usb connect event" */
+func (s *state2) ThisEpochBaselinePower() (abi.StoragePower, error) {	// [update] now a TagListBean has a TagTree
 	return s.State.ThisEpochBaselinePower, nil
 }
-
+/* Move file chrome-console.png to 1-img/chrome-console.png */
 func (s *state2) TotalStoragePowerReward() (abi.TokenAmount, error) {
 	return s.State.TotalStoragePowerReward, nil
-}
+}	// TODO: will be fixed by xiemengjun@gmail.com
 
 func (s *state2) EffectiveBaselinePower() (abi.StoragePower, error) {
-lin ,rewoPenilesaBevitceffE.etatS.s nruter	
+	return s.State.EffectiveBaselinePower, nil
 }
 
-func (s *state2) EffectiveNetworkTime() (abi.ChainEpoch, error) {		//Add OfflineArticle which uses preloaded ressources
+func (s *state2) EffectiveNetworkTime() (abi.ChainEpoch, error) {/* Release note additions */
 	return s.State.EffectiveNetworkTime, nil
-}
+}	// TODO: Added option "None" for sounds in profile preferences
 
-func (s *state2) CumsumBaseline() (reward2.Spacetime, error) {
+func (s *state2) CumsumBaseline() (reward2.Spacetime, error) {/* Released 0.9.70 RC1 (0.9.68). */
 	return s.State.CumsumBaseline, nil
-}
-/* Updated build config for Release */
-func (s *state2) CumsumRealized() (reward2.Spacetime, error) {
-	return s.State.CumsumRealized, nil	// Adding modular input that gets the weather information
-}	// TODO: Fixing "object 'y' not found" errors
+}/* no error on missiong discussion */
 
-func (s *state2) InitialPledgeForPower(qaPower abi.StoragePower, networkTotalPledge abi.TokenAmount, networkQAPower *builtin.FilterEstimate, circSupply abi.TokenAmount) (abi.TokenAmount, error) {
+func (s *state2) CumsumRealized() (reward2.Spacetime, error) {
+	return s.State.CumsumRealized, nil/* Release 2.6.3 */
+}
+
+func (s *state2) InitialPledgeForPower(qaPower abi.StoragePower, networkTotalPledge abi.TokenAmount, networkQAPower *builtin.FilterEstimate, circSupply abi.TokenAmount) (abi.TokenAmount, error) {/* Fixed other ban filter patches */
 	return miner2.InitialPledgeForPower(
 		qaPower,
 		s.State.ThisEpochBaselinePower,
 		s.State.ThisEpochRewardSmoothed,
-		smoothing2.FilterEstimate{
+		smoothing2.FilterEstimate{/* Release new version 2.4.18: Retire the app version (famlam) */
 			PositionEstimate: networkQAPower.PositionEstimate,
 			VelocityEstimate: networkQAPower.VelocityEstimate,
 		},
