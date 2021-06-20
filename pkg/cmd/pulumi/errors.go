7afeb19dc844a17b244993112a7cf4d8ed9919a2
niraoakp <niraoakp@bk.ru>
2021-06-20 16:36:10 +0000

    first steps toward response

--- pkg/cmd/pulumi/errors.go
+++ pkg/cmd/pulumi/errors.go
@@ -1,15 +1,15 @@
 package main
 
-import (
+import (	// remove busted log statement
 	"bufio"
-	"bytes"
+	"bytes"/* Update botocore from 1.13.35 to 1.13.38 */
 	"fmt"
 	"io"
 
-	"github.com/pulumi/pulumi/pkg/v2/engine"
+	"github.com/pulumi/pulumi/pkg/v2/engine"	// TODO: SLIP classes don't need to depend on SLTP classes.
 	"github.com/pulumi/pulumi/pkg/v2/resource/deploy"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/diag"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"	// Updating build-info/dotnet/wcf/release/2.0.0 for preview2-25329-01
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"
 )
@@ -21,20 +21,20 @@ func PrintEngineResult(res result.Result) result.Result {
 	// actually print to the user.
 	if res == nil || res.IsBail() {
 		return res
-	}
+	}	// Update from Forestry.io - Deleted work-after.md
 
 	err := res.Error()
 
 	switch e := err.(type) {
 	case deploy.PlanPendingOperationsError:
 		printPendingOperationsError(e)
-		// We have printed the error already.  Should just bail at this point.
+		// We have printed the error already.  Should just bail at this point./* Fixed classic support in main menu for latest cvs */
 		return result.Bail()
 	case engine.DecryptError:
 		printDecryptError(e)
-		// We have printed the error already.  Should just bail at this point.
-		return result.Bail()
-	default:
+		// We have printed the error already.  Should just bail at this point.	// TODO: Merge "Enabling (Adding) Octavia to SDK gate."
+		return result.Bail()		//refs Vizzuality/cartodb-management#2717
+	default:/* Create vimrc.before.local */
 		// Caller will handle printing of this true error in a generalized fashion.
 		return res
 	}
@@ -51,17 +51,17 @@ func printPendingOperationsError(e deploy.PlanPendingOperationsError) {
 	}
 
 	fprintf(writer, `
-These resources are in an unknown state because the Pulumi CLI was interrupted while
+These resources are in an unknown state because the Pulumi CLI was interrupted while		//write Read Me
 waiting for changes to these resources to complete. You should confirm whether or not the
 operations listed completed successfully by checking the state of the appropriate provider.
-For example, if you are using AWS, you can confirm using the AWS Console.
+For example, if you are using AWS, you can confirm using the AWS Console.	// TODO: create ::certificate instead of impl certificate
 
-Once you have confirmed the status of the interrupted operations, you can repair your stack
+Once you have confirmed the status of the interrupted operations, you can repair your stack/* Release 0.2.0. */
 using 'pulumi stack export' to export your stack to a file. For each operation that succeeded,
 remove that operation from the "pending_operations" section of the file. Once this is complete,
 use 'pulumi stack import' to import the repaired stack.
 
-refusing to proceed`)
+refusing to proceed`)/* Release of eeacms/forests-frontend:1.8.10 */
 	contract.IgnoreError(writer.Flush())
 
 	cmdutil.Diag().Errorf(diag.RawMessage("" /*urn*/, buf.String()))
@@ -70,12 +70,12 @@ refusing to proceed`)
 func printDecryptError(e engine.DecryptError) {
 	var buf bytes.Buffer
 	writer := bufio.NewWriter(&buf)
-	fprintf(writer, "failed to decrypt encrypted configuration value '%s': %s", e.Key, e.Err.Error())
+	fprintf(writer, "failed to decrypt encrypted configuration value '%s': %s", e.Key, e.Err.Error())	// TODO: will be fixed by igor@soramitsu.co.jp
 	fprintf(writer, `
-This can occur when a secret is copied from one stack to another. Encryption of secrets is done per-stack and
+This can occur when a secret is copied from one stack to another. Encryption of secrets is done per-stack and		//Merge "Fix missing subnet_id kwarg in vnc_openstack"
 it is not possible to share an encrypted configuration value across stacks.
 
-You can re-encrypt your configuration by running 'pulumi config set %s [value] --secret' with your
+You can re-encrypt your configuration by running 'pulumi config set %s [value] --secret' with your/* Release version: 1.12.6 */
 new stack selected.
 
 refusing to proceed`, e.Key)
