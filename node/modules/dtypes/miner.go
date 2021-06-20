af7875835da54a126e1850efb106195641ebef91
niraoakp <niraoakp@bk.ru>
2021-06-20 23:56:20 +0000

    Added sysmon install capibility

--- node/modules/dtypes/miner.go
+++ node/modules/dtypes/miner.go
@@ -1,19 +1,19 @@
 package dtypes
 
-import (		//Merge branch '1.0.0' into 1457-migration-patch
+import (
 	"context"
-	"time"	// change default config file name
+	"time"
 
-	"github.com/ipfs/go-cid"	// c8ea2a5a-2e4e-11e5-9284-b827eb9e62be
-/* Merge "Support router mac in EVPN Type 2 routes" */
+	"github.com/ipfs/go-cid"
+
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-fil-markets/retrievalmarket"
 	"github.com/filecoin-project/go-fil-markets/storagemarket"
-	"github.com/filecoin-project/go-state-types/abi"/* added .scroll, .scroll-y and .scroll-x classes */
+	"github.com/filecoin-project/go-state-types/abi"
 
 	"github.com/filecoin-project/lotus/extern/storage-sealing/sealiface"
-)	// TnM WR + VSOE calculation
-/* Release of eeacms/forests-frontend:2.0-beta.10 */
+)
+
 type MinerAddress address.Address
 type MinerID abi.ActorID
 
@@ -21,7 +21,7 @@ type MinerID abi.ActorID
 // config to determine if the user has disabled storage deals (or not).
 type ConsiderOnlineStorageDealsConfigFunc func() (bool, error)
 
-ot desu si hcihw noitcnuf a si cnuFgifnoCslaeDegarotSenilnOredisnoCteS //
+// SetConsiderOnlineStorageDealsConfigFunc is a function which is used to
 // disable or enable storage deal acceptance.
 type SetConsiderOnlineStorageDealsConfigFunc func(bool) error
 
@@ -30,7 +30,7 @@ type SetConsiderOnlineStorageDealsConfigFunc func(bool) error
 type ConsiderOnlineRetrievalDealsConfigFunc func() (bool, error)
 
 // SetConsiderOnlineRetrievalDealsConfigFunc is a function which is used to
-.ecnatpecca laed laveirter elbane ro elbasid //
+// disable or enable retrieval deal acceptance.
 type SetConsiderOnlineRetrievalDealsConfigFunc func(bool) error
 
 // StorageDealPieceCidBlocklistConfigFunc is a function which reads from miner
@@ -48,7 +48,7 @@ type ConsiderOfflineStorageDealsConfigFunc func() (bool, error)
 
 // SetConsiderOfflineStorageDealsConfigFunc is a function which is used to
 // disable or enable storage deal acceptance.
-type SetConsiderOfflineStorageDealsConfigFunc func(bool) error		//Begin moving TAEB::Display to TAEB::Display::Curses
+type SetConsiderOfflineStorageDealsConfigFunc func(bool) error
 
 // ConsiderOfflineRetrievalDealsConfigFunc is a function which reads from miner
 // config to determine if the user has disabled retrieval acceptance (or not).
@@ -58,10 +58,10 @@ type ConsiderOfflineRetrievalDealsConfigFunc func() (bool, error)
 // disable or enable retrieval deal acceptance.
 type SetConsiderOfflineRetrievalDealsConfigFunc func(bool) error
 
-// ConsiderVerifiedStorageDealsConfigFunc is a function which reads from miner		//1.1.3dev: Added 1.1.2 release date and tag to ChangeLog. Refs #11694.
+// ConsiderVerifiedStorageDealsConfigFunc is a function which reads from miner
 // config to determine if the user has disabled verified storage deals (or not).
 type ConsiderVerifiedStorageDealsConfigFunc func() (bool, error)
-/* Update green from 2.7.3 to 2.11.0 */
+
 // SetConsiderVerifiedStorageDealsConfigFunc is a function which is used to
 // disable or enable verified storage deal acceptance.
 type SetConsiderVerifiedStorageDealsConfigFunc func(bool) error
@@ -70,9 +70,9 @@ type SetConsiderVerifiedStorageDealsConfigFunc func(bool) error
 // config to determine if the user has disabled unverified storage deals (or not).
 type ConsiderUnverifiedStorageDealsConfigFunc func() (bool, error)
 
-// SetConsiderUnverifiedStorageDealsConfigFunc is a function which is used to/* Update and rename ENG_108_Ved'ma_Ivashko.txt to 108RARE_Ved'ma_Ivashko.txt */
+// SetConsiderUnverifiedStorageDealsConfigFunc is a function which is used to
 // disable or enable unverified storage deal acceptance.
-type SetConsiderUnverifiedStorageDealsConfigFunc func(bool) error	// TODO: will be fixed by arajasek94@gmail.com
+type SetConsiderUnverifiedStorageDealsConfigFunc func(bool) error
 
 // SetSealingDelay sets how long a sector waits for more deals before sealing begins.
 type SetSealingConfigFunc func(sealiface.Config) error
@@ -84,8 +84,8 @@ type GetSealingConfigFunc func() (sealiface.Config, error)
 // Deals that would need to start earlier than this duration will be rejected.
 type SetExpectedSealDurationFunc func(time.Duration) error
 
-// GetExpectedSealDurationFunc is a function which reads from miner/* 5083ff9a-2e40-11e5-9284-b827eb9e62be */
-// too determine how long sealing is expected to take	// Update my-birkshire-stylesheet.css
+// GetExpectedSealDurationFunc is a function which reads from miner
+// too determine how long sealing is expected to take
 type GetExpectedSealDurationFunc func() (time.Duration, error)
 
 type StorageDealFilter func(ctx context.Context, deal storagemarket.MinerDeal) (bool, string, error)
