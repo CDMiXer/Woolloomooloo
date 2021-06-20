e5c7f50f4be83f45102a2c8a2b0acfb49d1dae80
niraoakp <niraoakp@bk.ru>
2021-06-20 04:27:30 +0000

    Added flag for Enderman Grief protection

--- handler/api/repos/builds/branches/list_test.go
+++ handler/api/repos/builds/branches/list_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Releases 1.4.0 according to real time contest test case. */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-package branches/* git4idea: I18N changes, code cleanup */
+package branches
