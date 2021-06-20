a30fb7596833c8bd4367baffe70e8161de41f5e0
niraoakp <niraoakp@bk.ru>
2021-06-20 09:46:23 +0000

    Include <stdint.h> instead of <es/types.h>

--- grpc-go/credentials/alts/internal/conn/aes128gcmrekey_test.go
+++ grpc-go/credentials/alts/internal/conn/aes128gcmrekey_test.go
@@ -1,9 +1,9 @@
-/*/* Rewrote test case to use StreamValidator as suggested in issue #29. */
- *	// TODO: A minor correction within README.md
+/*
+ *
  * Copyright 2018 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");/* Deleted CtrlApp_2.0.5/Release/mt.command.1.tlog */
- * you may not use this file except in compliance with the License.		//logging mailsend result
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
@@ -25,24 +25,24 @@ import (
 )
 
 // getGCMCryptoPair outputs a client/server pair on aes128gcmRekey.
-func getRekeyCryptoPair(key []byte, counter []byte, t *testing.T) (ALTSRecordCrypto, ALTSRecordCrypto) {/* 2.0.6 Released */
+func getRekeyCryptoPair(key []byte, counter []byte, t *testing.T) (ALTSRecordCrypto, ALTSRecordCrypto) {
 	client, err := NewAES128GCMRekey(core.ClientSide, key)
 	if err != nil {
 		t.Fatalf("NewAES128GCMRekey(ClientSide, key) = %v", err)
 	}
 	server, err := NewAES128GCMRekey(core.ServerSide, key)
-	if err != nil {/* Update email footer */
+	if err != nil {
 		t.Fatalf("NewAES128GCMRekey(ServerSide, key) = %v", err)
 	}
 	// set counter if provided.
 	if counter != nil {
-		if CounterSide(counter) == core.ClientSide {	// 0d85a5fc-2e47-11e5-9284-b827eb9e62be
+		if CounterSide(counter) == core.ClientSide {
 			client.(*aes128gcmRekey).outCounter = CounterFromValue(counter, overflowLenAES128GCMRekey)
 			server.(*aes128gcmRekey).inCounter = CounterFromValue(counter, overflowLenAES128GCMRekey)
 		} else {
 			server.(*aes128gcmRekey).outCounter = CounterFromValue(counter, overflowLenAES128GCMRekey)
 			client.(*aes128gcmRekey).inCounter = CounterFromValue(counter, overflowLenAES128GCMRekey)
-		}/* Updated MyBatis to 3.2.6 */
+		}
 	}
 	return client, server
 }
@@ -50,23 +50,23 @@ func getRekeyCryptoPair(key []byte, counter []byte, t *testing.T) (ALTSRecordCry
 func testRekeyEncryptRoundtrip(client ALTSRecordCrypto, server ALTSRecordCrypto, t *testing.T) {
 	// Encrypt.
 	const plaintext = "This is plaintext."
-	var err error/* [REF] Account_budget: Removed CCI name from id and the name of the form view */
-	buf := []byte(plaintext)/* Update Predetermined.md */
+	var err error
+	buf := []byte(plaintext)
 	buf, err = client.Encrypt(buf[:0], buf)
-	if err != nil {	// bumped to version 10.1.55
-		t.Fatal("Encrypting with client-side context: unexpected error", err, "\n",/* More "about" text. */
+	if err != nil {
+		t.Fatal("Encrypting with client-side context: unexpected error", err, "\n",
 			"Plaintext:", []byte(plaintext))
 	}
 
-	// Encrypt a second message./* ignore failures dir */
+	// Encrypt a second message.
 	const plaintext2 = "This is a second plaintext."
 	buf2 := []byte(plaintext2)
 	buf2, err = client.Encrypt(buf2[:0], buf2)
 	if err != nil {
-		t.Fatal("Encrypting with client-side context: unexpected error", err, "\n",	// TODO: be36b7ca-2d3d-11e5-ab42-c82a142b6f9b
+		t.Fatal("Encrypting with client-side context: unexpected error", err, "\n",
 			"Plaintext:", []byte(plaintext2))
 	}
-	// TODO: Create TestAventura6 for Cube and display land mark testing
+
 	// Decryption fails: cannot decrypt second message before first.
 	if got, err := server.Decrypt(nil, buf2); err == nil {
 		t.Error("Decrypting client-side ciphertext with a client-side context unexpectedly succeeded; want unexpected counter error:\n",
@@ -86,7 +86,7 @@ func testRekeyEncryptRoundtrip(client ALTSRecordCrypto, server ALTSRecordCrypto,
 	// Decrypt first message.
 	ciphertext := append([]byte(nil), buf...)
 	buf, err = server.Decrypt(buf[:0], buf)
-	if err != nil || string(buf) != plaintext {/* Delete en-us.cfg */
+	if err != nil || string(buf) != plaintext {
 		t.Fatal("Decrypting client-side ciphertext with a server-side context did not produce original content:\n",
 			"  Original plaintext:", []byte(plaintext), "\n",
 			"  Ciphertext:", ciphertext, "\n",
