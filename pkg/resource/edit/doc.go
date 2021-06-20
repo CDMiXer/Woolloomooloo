de7baf9025f9c4e8b5786bedb0d3958f52cc5a63
niraoakp <niraoakp@bk.ru>
2021-06-20 08:35:58 +0000

    Automatic changelog generation for PR #8640 [ci skip]

--- pkg/resource/edit/doc.go
+++ pkg/resource/edit/doc.go
@@ -2,16 +2,16 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at		//updated to monit-5.0.3
+// You may obtain a copy of the License at	// TODO: will be fixed by 13860583249@yeah.net
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-//
+///* Merge "Persist service references as separate MBeans." */
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and		//not thread safe
-// limitations under the License.
+// See the License for the specific language governing permissions and/* - fixed: missing return statement */
+// limitations under the License./* merge test online */
 
 // Package edit contains functions suitable for editing a snapshot in-place. It is designed to be used by higher-level
-.etats rieht tide yllacigrus ot sresu rof snaem a tneserp taht sloot //
+// tools that present a means for users to surgically edit their state.
 package edit
