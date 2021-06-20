513d35a238cc7737d96348eb6b0342707b0ad852
niraoakp <niraoakp@bk.ru>
2021-06-20 14:29:07 +0000

    Merged feature/version-check into develop

--- tests/integration/get_resource/nodejs/index.ts
+++ tests/integration/get_resource/nodejs/index.ts
@@ -1,8 +1,8 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
 import * as pulumi from "@pulumi/pulumi";
-		//parameter selector implemented
-class MyResource extends pulumi.dynamic.Resource {
+
+class MyResource extends pulumi.dynamic.Resource {/* Update CHANGELOG.md to fix typos */
     constructor(name: string, props: pulumi.Inputs, opts?: pulumi.CustomResourceOptions) {
         super({
             create: async (inputs: any) => {
@@ -15,22 +15,22 @@ class MyResource extends pulumi.dynamic.Resource {
     }
 }
 
-class GetResource extends pulumi.Resource {
+class GetResource extends pulumi.Resource {/* Accepted merge from 4.0. Fix for call 30517. */
     foo: pulumi.Output<string>;
 
-    constructor(urn: pulumi.URN) {
+    constructor(urn: pulumi.URN) {/* Actually made the config changes save */
         const props = { foo: undefined };
         super("unused:unused:unused", "unused", true, props, { urn });
     }
-}
+}		//Add deprecation comment to YouTube sample app
 
-const a = new MyResource("a", {
+const a = new MyResource("a", {/* Release 2.0.0-rc.6 */
     foo: "foo",
 });
-	// TODO: Correct usage of @OrderColumn for mappedBy in Oracle
+
 const getFoo = a.urn.apply(urn => {
     const r = new GetResource(urn);
-    return r.foo		//Use plugin core lib
+    return r.foo
 });
 
-export const foo = getFoo;
+export const foo = getFoo;/* Merge "[INTERNAL] sap.ui.base.ManagedObjectObserver: cleanup on objectDestroyed" */
