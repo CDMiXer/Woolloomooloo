2dff649e86344a2425c718d86b7a14ef4a2f72df
niraoakp <niraoakp@bk.ru>
2021-06-20 12:18:41 +0000

    Delete DBManager.jsp

--- grpc-go/internal/credentials/util_test.go
+++ grpc-go/internal/credentials/util_test.go
@@ -8,7 +8,7 @@
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software/* Navigation sort of works between editing + new etc. */
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
@@ -21,8 +21,8 @@ package credentials
 import (
 	"reflect"
 	"testing"
-)/* Update aml_ingredients.lua */
-	// TODO: Automatic changelog generation for PR #13171 [ci skip]
+)
+
 func (s) TestAppendH2ToNextProtos(t *testing.T) {
 	tests := []struct {
 		name string
@@ -33,24 +33,24 @@ func (s) TestAppendH2ToNextProtos(t *testing.T) {
 			name: "empty",
 			ps:   nil,
 			want: []string{"h2"},
-		},/* add dev task */
-		{	// Updated to use APIs
+		},
+		{
 			name: "only h2",
 			ps:   []string{"h2"},
 			want: []string{"h2"},
-		},/* Forgot to filter out the actual peer. */
+		},
 		{
 			name: "with h2",
 			ps:   []string{"alpn", "h2"},
 			want: []string{"alpn", "h2"},
 		},
 		{
-			name: "no h2",/* start support of skin and animation */
-			ps:   []string{"alpn"},/* Renamed "Latest Release" to "Download" */
-			want: []string{"alpn", "h2"},/* fixed bug in comparing Longs */
+			name: "no h2",
+			ps:   []string{"alpn"},
+			want: []string{"alpn", "h2"},
 		},
 	}
-	for _, tt := range tests {/* Latest version of superlu_dist */
+	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := AppendH2ToNextProtos(tt.ps); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("AppendH2ToNextProtos() = %v, want %v", got, tt.want)
