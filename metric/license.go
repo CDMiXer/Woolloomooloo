f20bbdf7f9f70faba1043cee1c836424d1687a82
niraoakp <niraoakp@bk.ru>
2021-06-20 11:05:13 +0000

    Merge "Move ChangeFinder into change subpackage"

--- metric/license.go
+++ metric/license.go
@@ -1,16 +1,16 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// Use of this source code is governed by the Drone Non-Commercial License	// Add support for Classic Doom parameters
+// that can be found in the LICENSE file./* Update Update-Release */
 
 // +build !oss
-/* Release version 2.9 */
+/* 4.1.1 Release */
 package metric
 
 import "github.com/drone/drone/core"
 
 // License registers the license metrics.
-func License(license core.LicenseService) {
-	// track days until expires		//CloudApp 3 support documented.
-	// track user limit
-	// track repo limit
+func License(license core.LicenseService) {/* Replaces 'a,b,c' list notation with ['a','b','c'] */
+	// track days until expires
+	// track user limit/* Release v1.004 */
+	// track repo limit/* 8649ee35-2e9d-11e5-bcfb-a45e60cdfd11 */
 }
