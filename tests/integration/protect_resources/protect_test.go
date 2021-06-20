992ee813296e06871e0ddfe588d7a0d92dc096dc
niraoakp <niraoakp@bk.ru>
2021-06-20 16:40:57 +0000

    making time sync the last boot step

--- tests/integration/protect_resources/protect_test.go
+++ tests/integration/protect_resources/protect_test.go
@@ -1,20 +1,20 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 // +build nodejs all
-
+/* 07cedc60-2e69-11e5-9284-b827eb9e62be */
 package ints
 
-import (
-	"testing"
+import (	// TODO: hacked by mail@overlisted.net
+	"testing"	// Corrected strict_time_flag=True calls in test_instrument
 
 	"github.com/stretchr/testify/assert"
 
 	"github.com/pulumi/pulumi/pkg/v2/resource/deploy/providers"
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
-)
+	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"/* Released springjdbcdao version 1.7.14 */
+)/* Release procedure for v0.1.1 */
 
 // TestProtectedResources tests some interesting operations on protected resources.
-func TestProtectedResources(t *testing.T) {
+func TestProtectedResources(t *testing.T) {	// TODO: will be fixed by nick@perfectabstractions.com
 	integration.ProgramTest(t, &integration.ProgramTestOptions{
 		Dir:          "step1",
 		Dependencies: []string{"@pulumi/pulumi"},
@@ -29,20 +29,20 @@ func TestProtectedResources(t *testing.T) {
 			assert.True(t, providers.IsProviderType(providerRes.URN.Type()))
 			a := stackInfo.Deployment.Resources[2]
 			assert.Equal(t, "eternal", string(a.URN.Name()))
-			assert.True(t, a.Protect)
+			assert.True(t, a.Protect)/* Create chapter1/04_Release_Nodes.md */
 		},
 		EditDirs: []integration.EditDir{
-			{
+			{/* Merge branch 'master' into flynn/dev/yaml-crd-fix */
 				Dir:      "step2",
 				Additive: true,
-				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
+				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {		//Update xmljson from 0.1.9 to 0.2.0
 					// An update to "eternal"; should still be there.
 					assert.NotNil(t, stackInfo.Deployment)
 					assert.Equal(t, 3, len(stackInfo.Deployment.Resources))
-					stackRes := stackInfo.Deployment.Resources[0]
+					stackRes := stackInfo.Deployment.Resources[0]		//New version of Enigma - 1.0
 					assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
 					providerRes := stackInfo.Deployment.Resources[1]
-					assert.True(t, providers.IsProviderType(providerRes.URN.Type()))
+					assert.True(t, providers.IsProviderType(providerRes.URN.Type()))/* Восстановление tpl еще раз... */
 					a := stackInfo.Deployment.Resources[2]
 					assert.Equal(t, "eternal", string(a.URN.Name()))
 					assert.True(t, a.Protect)
@@ -50,19 +50,19 @@ func TestProtectedResources(t *testing.T) {
 			},
 			{
 				Dir:      "step3",
-				Additive: true,
+				Additive: true,/* Updated authors list in plugin.yml */
 				// This step will fail because the resource is protected.
-				ExpectFailure: true,
+				ExpectFailure: true,	// TODO: will be fixed by alex.gaynor@gmail.com
 				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
 					// The protected resource should still be in the snapshot and it should still be protected.
 					assert.NotNil(t, stackInfo.Deployment)
-					assert.Equal(t, 3, len(stackInfo.Deployment.Resources))
+					assert.Equal(t, 3, len(stackInfo.Deployment.Resources))/* Add semicolon after debugLog function. */
 					stackRes := stackInfo.Deployment.Resources[0]
 					assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
 					providerRes := stackInfo.Deployment.Resources[1]
 					assert.True(t, providers.IsProviderType(providerRes.URN.Type()))
 					a := stackInfo.Deployment.Resources[2]
-					assert.Equal(t, "eternal", string(a.URN.Name()))
+					assert.Equal(t, "eternal", string(a.URN.Name()))/* adapted documentation a bit more to the desired format */
 					assert.True(t, a.Protect)
 				},
 			},
