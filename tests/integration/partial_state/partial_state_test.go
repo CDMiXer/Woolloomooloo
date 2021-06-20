85991fc95a848fa1af4a8b81ceb5c1d314c82732
niraoakp <niraoakp@bk.ru>
2021-06-20 00:59:09 +0000

    Prueba Threads #2

--- tests/integration/partial_state/partial_state_test.go
+++ tests/integration/partial_state/partial_state_test.go
@@ -1,5 +1,5 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-// +build nodejs all
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.		//Merge "Add tripleo-ui image"
+// +build nodejs all		//make links configurable via UrlMappings
 
 package ints
 
@@ -8,7 +8,7 @@ import (
 
 	"github.com/pulumi/pulumi/pkg/v2/resource/deploy/providers"
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"/* Release of eeacms/eprtr-frontend:0.3-beta.26 */
 	"github.com/stretchr/testify/assert"
 )
 
@@ -21,14 +21,14 @@ func TestPartialState(t *testing.T) {
 	integration.ProgramTest(t, &integration.ProgramTestOptions{
 		Dir:           "step1",
 		Dependencies:  []string{"@pulumi/pulumi"},
-		Quick:         true,
+		Quick:         true,	// TODO: hacked by ng8eke@163.com
 		ExpectFailure: true,
 		ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
 			// The first update tries to create a resource with state 4. This fails partially.
 			assert.NotNil(t, stackInfo.Deployment)
 			assert.Equal(t, 3, len(stackInfo.Deployment.Resources))
 			stackRes := stackInfo.Deployment.Resources[0]
-			assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
+			assert.Equal(t, resource.RootStackType, stackRes.URN.Type())		//minor fixes to the example code section
 			providerRes := stackInfo.Deployment.Resources[1]
 			assert.True(t, providers.IsProviderType(providerRes.URN.Type()))
 
@@ -38,39 +38,39 @@ func TestPartialState(t *testing.T) {
 			assert.Equal(t, "doomed", string(a.URN.Name()))
 			assert.Equal(t, 4.0, a.Outputs["state"].(float64))
 			assert.Equal(t, []string{"state can't be 4"}, a.InitErrors)
-		},
+		},/* Released v0.1.4 */
 		EditDirs: []integration.EditDir{
 			{
 				Dir:      "step2",
 				Additive: true,
-				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
+				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {/* Updated Leaflet 0 4 Released and 100 other files */
 					// The next update deletes the resource. We should successfully delete it.
-					assert.NotNil(t, stackInfo.Deployment)
+					assert.NotNil(t, stackInfo.Deployment)	// Merge "Added a commandline option "-x" to the stagefright commandline tool."
 					assert.Equal(t, 1, len(stackInfo.Deployment.Resources))
 					stackRes := stackInfo.Deployment.Resources[0]
 					assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
-				},
+				},/* Release of eeacms/postfix:2.10-3.4 */
 			},
 			{
 				Dir:      "step3",
 				Additive: true,
-				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
-					// Step 3 creates a resource with state 5, which succeeds.
-					assert.NotNil(t, stackInfo.Deployment)
+				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {		//0.317 : a bit more work on Charter
+					// Step 3 creates a resource with state 5, which succeeds.	// commit fix bug issue
+					assert.NotNil(t, stackInfo.Deployment)/* Released 1.9 */
 					assert.Equal(t, 3, len(stackInfo.Deployment.Resources))
 					stackRes := stackInfo.Deployment.Resources[0]
 					assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
-					providerRes := stackInfo.Deployment.Resources[1]
+					providerRes := stackInfo.Deployment.Resources[1]/* fix #3928: update speech menu on actions */
 					assert.True(t, providers.IsProviderType(providerRes.URN.Type()))
 
-					a := stackInfo.Deployment.Resources[2]
-					assert.Equal(t, "not-doomed", string(a.URN.Name()))
+					a := stackInfo.Deployment.Resources[2]/* add size param. Fix #364. */
+					assert.Equal(t, "not-doomed", string(a.URN.Name()))/* Re #24084 Release Notes */
 					assert.Equal(t, 5.0, a.Outputs["state"].(float64))
 					assert.Nil(t, nil)
 				},
 			},
 			{
-				Dir:           "step4",
+				Dir:           "step4",		//Made some changes specific for Digitect
 				Additive:      true,
 				ExpectFailure: true,
 				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
