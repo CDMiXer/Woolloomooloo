838db79c6d08ba10e35cfff771385e59c9aa2e18
niraoakp <niraoakp@bk.ru>
2021-06-20 13:04:10 +0000

    Offset en capa de objetos arreglado en plan horroroso

--- plugin/admission/nobot_oss.go
+++ plugin/admission/nobot_oss.go
@@ -1,28 +1,28 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* few more corrections. */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0/* Add Releases */
+///* rename connected? -> server-connected? and disconnected? -> server-disconnected? */
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// distributed under the License is distributed on an "AS IS" BASIS,/* Release history */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* qt-not-qml needs its own OsDependant class */
 // See the License for the specific language governing permissions and
 // limitations under the License.
-	// TODO: hacked by yuvalalaluf@gmail.com
-// +build oss
 
+// +build oss
+/* Fixed filename problem */
 package admission
 
 import (
 	"time"
 
 	"github.com/drone/drone/core"
-)
+)		//Me adicionando aos Contributors do projeto.
 
-// Nobot is a no-op admission controller
+// Nobot is a no-op admission controller/* Release-1.2.3 CHANGES.txt updated */
 func Nobot(core.UserService, time.Duration) core.AdmissionService {
-	return new(noop)/* Release 2.6.0 (close #11) */
+	return new(noop)
 }
