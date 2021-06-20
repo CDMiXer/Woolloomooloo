18efb0a0c1ba462d61fea139f3ad2554553a4869
niraoakp <niraoakp@bk.ru>
2021-06-20 02:13:22 +0000

    Translated using Qt Linguist (Ukrainian)

--- handler/api/repos/crons/exec_test.go
+++ handler/api/repos/crons/exec_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License	// TODO: chore(package): update @types/node to version 12.0.4
 // that can be found in the LICENSE file.
-
+/* DOC: update readme badge links */
 // +build !oss
 
-package crons
+package crons/* badge ref URL */
