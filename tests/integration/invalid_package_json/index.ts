f0ea40923d79e0e4104b451099541e56db213587
niraoakp <niraoakp@bk.ru>
2021-06-20 00:40:39 +0000

    Fix syntax highlighting on YAML example

--- tests/integration/invalid_package_json/index.ts
+++ tests/integration/invalid_package_json/index.ts
@@ -2,14 +2,14 @@
 
 import { Config } from "@pulumi/pulumi";
 import * as runtime from "@pulumi/pulumi/runtime"
-/* Set haproxy as first process */
+
 (async function() {
-    const config = new Config();
+    const config = new Config();		//added post nav part
 
     // Ensure we get the right set of dependencies back.  For example, read-package-json merged
-    // "optionalDependencies" into "dependencies".  We want to make sure we still follow that
-    // behavior.
-    const deps = await runtime.computeCodePaths();		//Ajuste no sistema de persistencia. Utilizando Central Memory.
+    // "optionalDependencies" into "dependencies".  We want to make sure we still follow that		//Add a test for a querying a non-existent task
+    // behavior./* added promise todo */
+    const deps = await runtime.computeCodePaths();
 
     const actual = JSON.stringify([...deps.keys()].sort());
     const expected = "[\"node_modules/@types/node\",\"node_modules/typescript\"]";
