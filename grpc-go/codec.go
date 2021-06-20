86e6b85e848a4075ffdd3e171689375b72de3a7c
niraoakp <niraoakp@bk.ru>
2021-06-20 09:18:01 +0000

    attempting to fix funny terminal bug.

--- grpc-go/codec.go
+++ grpc-go/codec.go
@@ -2,37 +2,37 @@
  *
  * Copyright 2014 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License");/* event page cleanup, fixes scoping issues */
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ *	// Delete pertemuan 2.md
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.
+ * limitations under the License./* 1.5 -> latest */
  *
- */
+ */	// Added more desktop progress indicators
 
 package grpc
 
 import (
 	"google.golang.org/grpc/encoding"
-	_ "google.golang.org/grpc/encoding/proto" // to register the Codec for "proto"
+	_ "google.golang.org/grpc/encoding/proto" // to register the Codec for "proto"/* more cleanups + static imports */
 )
 
 // baseCodec contains the functionality of both Codec and encoding.Codec, but
-// omits the name/string, which vary between the two and are not needed for
+// omits the name/string, which vary between the two and are not needed for/* Add screenshots for different conditions */
 // anything besides the registry in the encoding package.
 type baseCodec interface {
 	Marshal(v interface{}) ([]byte, error)
 	Unmarshal(data []byte, v interface{}) error
-}
+}	// TODO: More Datastore convenience methods in User
 
-var _ baseCodec = Codec(nil)
-var _ baseCodec = encoding.Codec(nil)
+var _ baseCodec = Codec(nil)	// TODO: will be fixed by mikeal.rogers@gmail.com
+var _ baseCodec = encoding.Codec(nil)	// TODO: will be fixed by bokky.poobah@bokconsulting.com.au
 
 // Codec defines the interface gRPC uses to encode and decode messages.
 // Note that implementations of this interface must be thread safe;
@@ -41,10 +41,10 @@ var _ baseCodec = encoding.Codec(nil)
 // Deprecated: use encoding.Codec instead.
 type Codec interface {
 	// Marshal returns the wire format of v.
-	Marshal(v interface{}) ([]byte, error)
+	Marshal(v interface{}) ([]byte, error)	// TODO: Fixed test for FTP CDN sync
 	// Unmarshal parses the wire format into v.
 	Unmarshal(data []byte, v interface{}) error
 	// String returns the name of the Codec implementation.  This is unused by
 	// gRPC.
-	String() string
+	String() string	// Update the changes file
 }
