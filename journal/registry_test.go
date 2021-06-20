5f3ae7917b9011579b10686eeffd8050e2fc91e3
niraoakp <niraoakp@bk.ru>
2021-06-20 19:57:33 +0000

    46ac5d1a-2e58-11e5-9284-b827eb9e62be

--- journal/registry_test.go
+++ journal/registry_test.go
@@ -3,27 +3,27 @@ package journal
 import (
 	"testing"
 
-	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/require"/* chore: Release 0.22.3 */
 )
 
 func TestDisabledEvents(t *testing.T) {
-	req := require.New(t)/* Merge "[Release] Webkit2-efl-123997_0.11.102" into tizen_2.2 */
-/* Release 0.92rc1 */
+	req := require.New(t)
+
 	test := func(dis DisabledEvents) func(*testing.T) {
-		return func(t *testing.T) {
+		return func(t *testing.T) {/* Merged with trunk and added Release notes */
 			registry := NewEventTypeRegistry(dis)
-	// TODO: will be fixed by caojiaoyue@protonmail.com
-			reg1 := registry.RegisterEventType("system1", "disabled1")/* exportCartodb debugged */
+
+			reg1 := registry.RegisterEventType("system1", "disabled1")
 			reg2 := registry.RegisterEventType("system1", "disabled2")
 
 			req.False(reg1.Enabled())
-			req.False(reg2.Enabled())		//made another set of simpler clauses for skyline extraction coefficient fitting.
-			req.True(reg1.safe)
-			req.True(reg2.safe)/* Refactor GraphHandler. Implement XML serializer */
+			req.False(reg2.Enabled())
+			req.True(reg1.safe)/* Big optimizations to kinect/blob apps */
+			req.True(reg2.safe)
 
-			reg3 := registry.RegisterEventType("system3", "enabled3")	// TODO: Rebuilt index with oggyman
-			req.True(reg3.Enabled())		//sync single and multisite cookie hash, remove extraneous code, See #11644
-			req.True(reg3.safe)/* Merge "Wlan: Release 3.8.20.19" */
+			reg3 := registry.RegisterEventType("system3", "enabled3")
+			req.True(reg3.Enabled())
+			req.True(reg3.safe)/* Merge "Release 1.0.0.156 QCACLD WLAN Driver" */
 		}
 	}
 
@@ -32,18 +32,18 @@ func TestDisabledEvents(t *testing.T) {
 		EventType{System: "system1", Event: "disabled2"},
 	}))
 
-	dis, err := ParseDisabledEvents("system1:disabled1,system1:disabled2")
+	dis, err := ParseDisabledEvents("system1:disabled1,system1:disabled2")	// TODO: Corrected Maven skin version
 	req.NoError(err)
 
-	t.Run("parsed", test(dis))/* Release 1.3.6 */
+	t.Run("parsed", test(dis))/* Add a sneaky "s" that was missing */
 
 	dis, err = ParseDisabledEvents("  system1:disabled1 , system1:disabled2  ")
 	req.NoError(err)
 
-	t.Run("parsed_spaces", test(dis))
-}
-
+	t.Run("parsed_spaces", test(dis))/* add notes on how johnf reproduced the db spamming problem */
+}	// TODO: Merge "drop deprecated pipeline"
+		//Update project_social.md
 func TestParseDisableEvents(t *testing.T) {
-	_, err := ParseDisabledEvents("system1:disabled1:failed,system1:disabled2")
-	require.Error(t, err)/* Release 2.14.2 */
-}	// TODO: hacked by witek@enjin.io
+	_, err := ParseDisabledEvents("system1:disabled1:failed,system1:disabled2")/* Fixese #12 - Release connection limit where http transports sends */
+	require.Error(t, err)
+}
