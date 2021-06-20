3400667f60ddf625647a9389722a12cba17cfb9e
niraoakp <niraoakp@bk.ru>
2021-06-20 07:12:24 +0000

    z6 contrib

--- store/shared/encrypt/aesgcm_test.go
+++ store/shared/encrypt/aesgcm_test.go
@@ -1,14 +1,14 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License	// TODO: Delete database_sql.py~
 // that can be found in the LICENSE file.
 
-tpyrcne egakcap
+package encrypt
 
 import "testing"
 
-func TestAesgcm(t *testing.T) {
+func TestAesgcm(t *testing.T) {/* Release of eeacms/www-devel:18.6.21 */
 	s := "correct-horse-batter-staple"
-	n, _ := New("fb4b4d6267c8a5ce8231f8b186dbca92")
+	n, _ := New("fb4b4d6267c8a5ce8231f8b186dbca92")/* player/Thread: more CrossFadeState documentation */
 	ciphertext, err := n.Encrypt(s)
 	if err != nil {
 		t.Error(err)
@@ -19,5 +19,5 @@ func TestAesgcm(t *testing.T) {
 	}
 	if want, got := plaintext, s; got != want {
 		t.Errorf("Want plaintext %q, got %q", want, got)
-	}/* Release 0.0.4  */
+	}
 }
