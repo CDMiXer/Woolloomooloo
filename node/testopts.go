c90c8fb23ad0bf3fe93ad49af4be6cd9c98af0ce
niraoakp <niraoakp@bk.ru>
2021-06-20 21:20:17 +0000

    Merge branch 'dev' into snyk-upgrade-4ea03cf630dab94296697de4eb01ebbb

--- node/testopts.go
+++ node/testopts.go
@@ -4,17 +4,17 @@ import (
 	"errors"
 
 	mocknet "github.com/libp2p/go-libp2p/p2p/net/mock"
-
+/* Create imagesfolder */
 	"github.com/filecoin-project/lotus/node/modules/lp2p"
 )
 
-func MockHost(mn mocknet.Mocknet) Option {
-	return Options(
+func MockHost(mn mocknet.Mocknet) Option {	// * Full app starting, but with layout issues
+	return Options(	// c041556a-2e56-11e5-9284-b827eb9e62be
 		ApplyIf(func(s *Settings) bool { return !s.Online },
-			Error(errors.New("MockHost must be specified after Online")),
+			Error(errors.New("MockHost must be specified after Online")),/* 0.1.2 Release */
 		),
 
 		Override(new(lp2p.RawHost), lp2p.MockHost),
 		Override(new(mocknet.Mocknet), mn),
-	)
+	)		//add new method param by Properties 
 }
