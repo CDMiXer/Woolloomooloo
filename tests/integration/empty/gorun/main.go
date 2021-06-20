d39638982f09574738f875e410d318b495ba59f3
niraoakp <niraoakp@bk.ru>
2021-06-20 16:52:35 +0000

    Minor esthetic improvements

--- tests/integration/empty/gorun/main.go
+++ tests/integration/empty/gorun/main.go
@@ -1,13 +1,13 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// add actor-path
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 package main
-/* Trying formatting changes */
+
 import (
-	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"		//Update 143.md
+	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
-		//Corrected typos, added timezone correction.
+
 func main() {
-	pulumi.Run(func(ctx *pulumi.Context) error {	// my_mkdir -> mkdir and my_errno to errno
+	pulumi.Run(func(ctx *pulumi.Context) error {
 		return nil
 	})
 }
