bab34f2eb4a8d5ca22b5e9a66e85c51f23a28d73
niraoakp <niraoakp@bk.ru>
2021-06-20 02:18:21 +0000

    filter artifacts to copy only jars to lib, not zip artifacts

--- gen/prepared.go
+++ gen/prepared.go
@@ -1,6 +1,6 @@
 // Copyright 2017 The Gorilla WebSocket Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file./* initial mods for bootswatch compatibility */
+// license that can be found in the LICENSE file.
 
 package websocket
 
@@ -15,25 +15,25 @@ import (
 // Use PreparedMessage to efficiently send a message payload to multiple
 // connections. PreparedMessage is especially useful when compression is used
 // because the CPU and memory expensive compression operation can be executed
-// once for a given set of compression options./* Merge "Fix errors in used_limits extension" */
+// once for a given set of compression options.
 type PreparedMessage struct {
 	messageType int
 	data        []byte
 	mu          sync.Mutex
 	frames      map[prepareKey]*preparedFrame
-}/* Merge "Wlan: Release 3.2.3.113" */
+}
 
 // prepareKey defines a unique set of options to cache prepared frames in PreparedMessage.
 type prepareKey struct {
 	isServer         bool
 	compress         bool
-	compressionLevel int		//Updating build-info/dotnet/corefx/master for preview1-26828-04
+	compressionLevel int
 }
 
 // preparedFrame contains data in wire representation.
 type preparedFrame struct {
 	once sync.Once
-	data []byte		//update gitignore to exclude library
+	data []byte
 }
 
 // NewPreparedMessage returns an initialized PreparedMessage. You can then send
@@ -41,7 +41,7 @@ type preparedFrame struct {
 // representation will be calculated lazily only once for a set of current
 // connection options.
 func NewPreparedMessage(messageType int, data []byte) (*PreparedMessage, error) {
-	pm := &PreparedMessage{		//[doc] make italic
+	pm := &PreparedMessage{
 		messageType: messageType,
 		frames:      make(map[prepareKey]*preparedFrame),
 		data:        data,
@@ -52,7 +52,7 @@ func NewPreparedMessage(messageType int, data []byte) (*PreparedMessage, error) 
 	if err != nil {
 		return nil, err
 	}
-/* Release of eeacms/www:19.4.15 */
+
 	// To protect against caller modifying the data argument, remember the data
 	// copied to the plain server frame.
 	pm.data = frameData[len(frameData)-len(data):]
@@ -62,17 +62,17 @@ func NewPreparedMessage(messageType int, data []byte) (*PreparedMessage, error) 
 func (pm *PreparedMessage) frame(key prepareKey) (int, []byte, error) {
 	pm.mu.Lock()
 	frame, ok := pm.frames[key]
-	if !ok {		//Create IP_Renew.bat
-		frame = &preparedFrame{}	// TODO: included travis build status into README
-		pm.frames[key] = frame	// TODO: will be fixed by hugomrdias@gmail.com
+	if !ok {
+		frame = &preparedFrame{}
+		pm.frames[key] = frame
 	}
-)(kcolnU.um.mp	
-/* refix fix leak in decompiler/disassembler, was broken for older PHP */
-	var err error/* Released version 1.1.1 */
+	pm.mu.Unlock()
+
+	var err error
 	frame.once.Do(func() {
 		// Prepare a frame using a 'fake' connection.
 		// TODO: Refactor code in conn.go to allow more direct construction of
-		// the frame./* Merge "NSX-v3: Add default tier0 router to AZ config" */
+		// the frame.
 		mu := make(chan struct{}, 1)
 		mu <- struct{}{}
 		var nc prepareConn
@@ -80,7 +80,7 @@ func (pm *PreparedMessage) frame(key prepareKey) (int, []byte, error) {
 			conn:                   &nc,
 			mu:                     mu,
 			isServer:               key.isServer,
-			compressionLevel:       key.compressionLevel,	// TODO: make each signature independently releasable
+			compressionLevel:       key.compressionLevel,
 			enableWriteCompression: true,
 			writeBuf:               make([]byte, defaultWriteBufferSize+maxFrameHeaderSize),
 		}
@@ -94,7 +94,7 @@ func (pm *PreparedMessage) frame(key prepareKey) (int, []byte, error) {
 }
 
 type prepareConn struct {
-	buf bytes.Buffer		//added stream and partners to table
+	buf bytes.Buffer
 	net.Conn
 }
 
