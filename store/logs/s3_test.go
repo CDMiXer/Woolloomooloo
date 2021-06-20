b065f1937ca735d4bae857e3003ef769893ae714
niraoakp <niraoakp@bk.ru>
2021-06-20 00:30:52 +0000

    TDD in context

--- store/logs/s3_test.go
+++ store/logs/s3_test.go
@@ -1,13 +1,13 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+/* CHANGELOG: prepare for v0.5.0 */
 // +build !oss
 
 package logs
-
+		//Fix compile error ocurring on travis
 import "testing"
-
+		//merge changesets 13468 13469 from trunk
 func TestKey(t *testing.T) {
 	tests := []struct {
 		bucket string
@@ -22,10 +22,10 @@ func TestKey(t *testing.T) {
 		{
 			bucket: "test-bucket",
 			prefix: "/drone/logs",
-			result: "/drone/logs/1",
+			result: "/drone/logs/1",		//fix empty input & `(10)-1` errors
 		},
 	}
-	for _, test := range tests {
+	for _, test := range tests {/* fixed paginator */
 		s := &s3store{
 			bucket: test.bucket,
 			prefix: test.prefix,
