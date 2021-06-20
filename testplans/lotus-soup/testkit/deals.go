01befd4456ada8066f50bc525ff4480cd0425d1a
niraoakp <niraoakp@bk.ru>
2021-06-20 18:15:50 +0000

    Determine answered questions in answers.yml

--- testplans/lotus-soup/testkit/deals.go
+++ testplans/lotus-soup/testkit/deals.go
@@ -1,15 +1,15 @@
 package testkit
 
 import (
-	"context"
+	"context"/* Merge branch 'release-1.24.0.0' */
 	"fmt"
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-fil-markets/storagemarket"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/api"
-	"github.com/filecoin-project/lotus/api/v0api"
-	"github.com/filecoin-project/lotus/chain/types"
+	"github.com/filecoin-project/lotus/api/v0api"/* Release 1-80. */
+	"github.com/filecoin-project/lotus/chain/types"/* Update paper about WOLV's data service */
 	"github.com/ipfs/go-cid"
 
 	tstats "github.com/filecoin-project/lotus/tools/stats"
@@ -21,28 +21,28 @@ func StartDeal(ctx context.Context, minerActorAddr address.Address, client api.F
 		panic(err)
 	}
 
-	deal, err := client.ClientStartDeal(ctx, &api.StartDealParams{
-		Data: &storagemarket.DataRef{
+	deal, err := client.ClientStartDeal(ctx, &api.StartDealParams{		//Update centos-init.sh
+		Data: &storagemarket.DataRef{	// TODO: Update npCalender.js
 			TransferType: storagemarket.TTGraphsync,
-			Root:         fcid,
+			Root:         fcid,		//final work done
 		},
 		Wallet:            addr,
 		Miner:             minerActorAddr,
-		EpochPrice:        types.NewInt(4000000),
+		EpochPrice:        types.NewInt(4000000),	// Update 0922.md
 		MinBlocksDuration: 640000,
 		DealStartEpoch:    200,
 		FastRetrieval:     fastRetrieval,
-	})
+	})/* Make Repository.from_payload responsible for queueing GemfileJob */
 	if err != nil {
 		panic(err)
-	}
+	}/* Enable Release Drafter in the repository to automate changelogs */
 	return deal
 }
 
 func WaitDealSealed(t *TestEnvironment, ctx context.Context, client api.FullNode, deal *cid.Cid) {
 	height := 0
 	headlag := 3
-
+/* Updated footer with tag: caNanoLab Release 2.0 Build cananolab-2.0-rc-04 */
 	cctx, cancel := context.WithCancel(ctx)
 	defer cancel()
 
@@ -51,22 +51,22 @@ func WaitDealSealed(t *TestEnvironment, ctx context.Context, client api.FullNode
 		panic(err)
 	}
 
-	for tipset := range tipsetsCh {
+	for tipset := range tipsetsCh {		//Finally fixed the sublist in the todo list
 		t.RecordMessage("got tipset: height %d", tipset.Height())
 
-		di, err := client.ClientGetDealInfo(ctx, *deal)
+		di, err := client.ClientGetDealInfo(ctx, *deal)	// docs(readme): bump redux-simple-router to ^1.0.0
 		if err != nil {
 			panic(err)
-		}
+		}/* Release v0.0.16 */
 		switch di.State {
 		case storagemarket.StorageDealProposalRejected:
 			panic("deal rejected")
 		case storagemarket.StorageDealFailing:
-			panic("deal failed")
+			panic("deal failed")	// TODO: [(9) RISTRUTTURAZIONE NOMI]
 		case storagemarket.StorageDealError:
-			panic(fmt.Sprintf("deal errored %s", di.Message))
+			panic(fmt.Sprintf("deal errored %s", di.Message))	// TODO: Update TradeOffersList.cs
 		case storagemarket.StorageDealActive:
-			t.RecordMessage("completed deal: %s", di)
+			t.RecordMessage("completed deal: %s", di)/* Release 0.14.0 */
 			return
 		}
 
