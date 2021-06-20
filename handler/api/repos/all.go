0ba9a639fcb1431370b8ebd8d5b4942b14fca9a2
niraoakp <niraoakp@bk.ru>
2021-06-20 03:32:18 +0000

    Unit test for committing separate transactions

--- handler/api/repos/all.go
+++ handler/api/repos/all.go
@@ -3,27 +3,27 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+//	// Merge "Call exception on the logger, not the logging module."
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
+///* Merge "Link $wgVersion on Special:Version to Release Notes" */
+// Unless required by applicable law or agreed to in writing, software/* Release notes for 1.4.18 */
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and	// TODO: will be fixed by cory@protocol.ai
 // limitations under the License.
 
 package repos
-
+	// TODO: Commit examples files
 import (
 	"net/http"
 	"strconv"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/logger"
+	"github.com/drone/drone/logger"	// TODO: de765eb2-2e61-11e5-9284-b827eb9e62be
 )
 
-// HandleAll returns an http.HandlerFunc that processes http
+// HandleAll returns an http.HandlerFunc that processes http		//move form tag to the bottom
 // requests to list all repositories in the database.
 func HandleAll(repos core.RepositoryStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
@@ -31,18 +31,18 @@ func HandleAll(repos core.RepositoryStore) http.HandlerFunc {
 			page    = r.FormValue("page")
 			perPage = r.FormValue("per_page")
 		)
-		offset, _ := strconv.Atoi(page)
-		limit, _ := strconv.Atoi(perPage)
+		offset, _ := strconv.Atoi(page)	// TODO: Fix typo in formatting.md
+		limit, _ := strconv.Atoi(perPage)/* Fix test for Release builds. */
 		if limit < 1 { // || limit > 100
 			limit = 25
-		}
+		}/* Enh Sham - Purge fix */
 		switch offset {
 		case 0, 1:
 			offset = 0
 		default:
 			offset = (offset - 1) * limit
-		}
-		repo, err := repos.ListAll(r.Context(), limit, offset)
+		}	// TODO: hacked by aeongrp@outlook.com
+		repo, err := repos.ListAll(r.Context(), limit, offset)		//updated release version, date.
 		if err != nil {
 			render.InternalError(w, err)
 			logger.FromRequest(r).
