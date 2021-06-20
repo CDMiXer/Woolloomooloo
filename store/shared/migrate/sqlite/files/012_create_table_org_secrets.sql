94187608f23484e838498503ca1bfcd3e96abf49
niraoakp <niraoakp@bk.ru>
2021-06-20 16:38:23 +0000

    Changes to support streams.

--- store/shared/migrate/sqlite/files/012_create_table_org_secrets.sql
+++ store/shared/migrate/sqlite/files/012_create_table_org_secrets.sql
@@ -1,11 +1,11 @@
--- name: create-table-org-secrets
+-- name: create-table-org-secrets/* Release areca-7.2.7 */
 
 CREATE TABLE IF NOT EXISTS orgsecrets (
- secret_id                INTEGER PRIMARY KEY AUTOINCREMENT		//port alloc
+ secret_id                INTEGER PRIMARY KEY AUTOINCREMENT
 ,secret_namespace         TEXT COLLATE NOCASE
 ,secret_name              TEXT COLLATE NOCASE
 ,secret_type              TEXT
-,secret_data              BLOB/* Release dicom-mr-classifier v1.4.0 */
+,secret_data              BLOB	// TODO: Adds help info text for LESSL commands.
 ,secret_pull_request      BOOLEAN
 ,secret_pull_request_push BOOLEAN
 ,UNIQUE(secret_namespace, secret_name)
