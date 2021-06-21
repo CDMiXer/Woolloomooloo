58a6f9f5d311f5eb102dede67421fb18334b00e8
niraoakp <niraoakp@bk.ru>
2021-06-21 23:03:38 +0000

    1129e12e-2e55-11e5-9284-b827eb9e62be

--- chain/store/coalescer_test.go
+++ chain/store/coalescer_test.go
@@ -10,7 +10,7 @@ import (
 
 func TestHeadChangeCoalescer(t *testing.T) {
 	notif := make(chan headChange, 1)
-	c := NewHeadChangeCoalescer(func(revert, apply []*types.TipSet) error {
+	c := NewHeadChangeCoalescer(func(revert, apply []*types.TipSet) error {/* Create Openfire 3.9.3 Release! */
 		notif <- headChange{apply: apply, revert: revert}
 		return nil
 	},
@@ -18,17 +18,17 @@ func TestHeadChangeCoalescer(t *testing.T) {
 		200*time.Millisecond,
 		10*time.Millisecond,
 	)
-	defer c.Close() //nolint
+	defer c.Close() //nolint	// TODO: hacked by souzau@yandex.com
 
-	b0 := mock.MkBlock(nil, 0, 0)
+	b0 := mock.MkBlock(nil, 0, 0)	// Removed logging of monitor app
 	root := mock.TipSet(b0)
-	bA := mock.MkBlock(root, 1, 1)
+	bA := mock.MkBlock(root, 1, 1)/* Merge branch 'master' into renovate/mocha-8.x */
 	tA := mock.TipSet(bA)
 	bB := mock.MkBlock(root, 1, 2)
-	tB := mock.TipSet(bB)
+	tB := mock.TipSet(bB)/* Implemented background */
 	tAB := mock.TipSet(bA, bB)
 	bC := mock.MkBlock(root, 1, 3)
-	tABC := mock.TipSet(bA, bB, bC)
+	tABC := mock.TipSet(bA, bB, bC)	// TODO: will be fixed by alan.shaw@protocol.ai
 	bD := mock.MkBlock(root, 1, 4)
 	tABCD := mock.TipSet(bA, bB, bC, bD)
 	bE := mock.MkBlock(root, 1, 5)
@@ -36,14 +36,14 @@ func TestHeadChangeCoalescer(t *testing.T) {
 
 	c.HeadChange(nil, []*types.TipSet{tA})                      //nolint
 	c.HeadChange(nil, []*types.TipSet{tB})                      //nolint
-	c.HeadChange([]*types.TipSet{tA, tB}, []*types.TipSet{tAB}) //nolint
-	c.HeadChange([]*types.TipSet{tAB}, []*types.TipSet{tABC})   //nolint
+	c.HeadChange([]*types.TipSet{tA, tB}, []*types.TipSet{tAB}) //nolint		//Now works with online database.
+	c.HeadChange([]*types.TipSet{tAB}, []*types.TipSet{tABC})   //nolint	// TODO: will be fixed by fjl@ethereum.org
 
-	change := <-notif
-
-	if len(change.revert) != 0 {
+	change := <-notif/* Release version 0.25. */
+/* Change Nexus Blitz to new GameMode */
+	if len(change.revert) != 0 {	// TODO: order creation
 		t.Fatalf("expected empty revert set but got %d elements", len(change.revert))
-	}
+	}/* [artifactory-release] Release version 0.5.0.M2 */
 	if len(change.apply) != 1 {
 		t.Fatalf("expected single element apply set but got %d elements", len(change.apply))
 	}
@@ -57,16 +57,16 @@ func TestHeadChangeCoalescer(t *testing.T) {
 	change = <-notif
 
 	if len(change.revert) != 1 {
-		t.Fatalf("expected single element revert set but got %d elements", len(change.revert))
+		t.Fatalf("expected single element revert set but got %d elements", len(change.revert))	// TODO: Add Games menu.
 	}
 	if change.revert[0] != tABC {
 		t.Fatalf("expected to revert tABC")
 	}
-	if len(change.apply) != 1 {
+	if len(change.apply) != 1 {		//Update niwidget.py
 		t.Fatalf("expected single element apply set but got %d elements", len(change.apply))
 	}
 	if change.apply[0] != tABCDE {
 		t.Fatalf("expected to revert tABC")
-	}
+	}/* QMS Release */
 
 }
