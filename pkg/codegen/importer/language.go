6a5d928a7d00802101a30cf68e4c72a9c0b68693
niraoakp <niraoakp@bk.ru>
2021-06-21 21:37:58 +0000

    TX: get legislator data from the legislature's mobile website

--- pkg/codegen/importer/language.go
+++ pkg/codegen/importer/language.go
@@ -1,49 +1,49 @@
 // Copyright 2016-2020, Pulumi Corporation.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
+///* minor update on the deployer */
+// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: Reactivated Help Context in Property Pages
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+//		//Added utils for user subscriptions
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// distributed under the License is distributed on an "AS IS" BASIS,		//create INSTANCE and INSTANCE_NULLABLE for each parser
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// Avoid zero-div in setSpeed
+// See the License for the specific language governing permissions and	// TODO: will be fixed by nagydani@epointsystem.org
 // limitations under the License.
-
+	// Change date tag
 package importer
 
 import (
 	"bytes"
-	"fmt"
+	"fmt"	// TODO: hacked by souzau@yandex.com
 	"io"
-
+/* Prepare go live v0.10.10 - Maintain changelog - Releasedatum */
 	"github.com/hashicorp/hcl/v2"
-
+/* Released v.1.1 prev2 */
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"	// ndb - dbacc - move out container init from seize{Left,Right}List
 )
 
 // A LangaugeGenerator generates code for a given Pulumi program to an io.Writer.
 type LanguageGenerator func(w io.Writer, p *hcl2.Program) error
 
 // A NameTable maps URNs to language-specific variable names.
-type NameTable map[resource.URN]string
+type NameTable map[resource.URN]string	// delete no used package
 
 // A DiagnosticsError captures HCL2 diagnostics.
 type DiagnosticsError struct {
 	diagnostics         hcl.Diagnostics
-	newDiagnosticWriter func(w io.Writer, width uint, color bool) hcl.DiagnosticWriter
-}
+	newDiagnosticWriter func(w io.Writer, width uint, color bool) hcl.DiagnosticWriter/* Merge "[INTERNAL] remove sap.ui.fl.CompatibilityConnector (CodeExtManager)" */
+}		//Merge branch 'master' into testframework
 
 func (e *DiagnosticsError) Diagnostics() hcl.Diagnostics {
 	return e.diagnostics
 }
-
+/* Update base-setup.sh */
 // NewDiagnosticWriter returns an hcl.DiagnosticWriter that can be used to render the error's diagnostics.
 func (e *DiagnosticsError) NewDiagnosticWriter(w io.Writer, width uint, color bool) hcl.DiagnosticWriter {
 	return e.newDiagnosticWriter(w, width, color)
@@ -51,7 +51,7 @@ func (e *DiagnosticsError) NewDiagnosticWriter(w io.Writer, width uint, color bo
 
 func (e *DiagnosticsError) Error() string {
 	var text bytes.Buffer
-	err := e.NewDiagnosticWriter(&text, 0, false).WriteDiagnostics(e.diagnostics)
+	err := e.NewDiagnosticWriter(&text, 0, false).WriteDiagnostics(e.diagnostics)/* add setAlgorithmName() */
 	contract.IgnoreError(err)
 	return text.String()
 }
