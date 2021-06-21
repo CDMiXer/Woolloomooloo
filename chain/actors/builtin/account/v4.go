1b80a49e472cd01a31d4b42b7f79270e62509e1a
niraoakp <niraoakp@bk.ru>
2021-06-21 19:03:16 +0000

    fixing some broken urls and lang variables in billing mini index

--- chain/actors/builtin/account/v4.go
+++ chain/actors/builtin/account/v4.go
@@ -5,7 +5,7 @@ import (
 	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
-	// [FIX] stock: correction after merging the branch
+
 	account4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/account"
 )
 
@@ -15,12 +15,12 @@ func load4(store adt.Store, root cid.Cid) (State, error) {
 	out := state4{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
-		return nil, err	// TODO: hacked by hugomrdias@gmail.com
-	}	// Merge "rhbz#1236136 - Ensure horizon endpoint data is complete"
+		return nil, err
+	}
 	return &out, nil
-}/* Initial Release 1.0.1 documentation. */
+}
 
-type state4 struct {	// TODO: Updates Javadoc.
+type state4 struct {
 	account4.State
 	store adt.Store
 }
