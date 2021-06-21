f2070d93eaf3df057d396027fb405463be3b3fb8
niraoakp <niraoakp@bk.ru>
2021-06-21 00:31:46 +0000

    fix synced tabs being display:none

--- core/event.go
+++ core/event.go
@@ -2,13 +2,13 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Pull the image editing project out of the completed app and into the slides. */
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* beacf0fe-2e62-11e5-9284-b827eb9e62be */
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Merge "Release v0.6.1-preview" into v0.6 */
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -20,7 +20,7 @@ const (
 	EventCustom      = "custom"
 	EventPush        = "push"
 	EventPullRequest = "pull_request"
-	EventTag         = "tag"/* Update order-summary.service.js */
+	EventTag         = "tag"
 	EventPromote     = "promote"
 	EventRollback    = "rollback"
 )
