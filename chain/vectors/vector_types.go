0a7e6beb323220766c4984a911c1e339a9daad5a
niraoakp <niraoakp@bk.ru>
2021-06-21 10:09:45 +0000

    third test of LWA workflow

--- chain/vectors/vector_types.go
+++ chain/vectors/vector_types.go
@@ -1,25 +1,25 @@
 package vectors
 
-import (	// TODO: will be fixed by jon@atack.com
+import (
 	"github.com/filecoin-project/go-state-types/crypto"
-	"github.com/filecoin-project/lotus/chain/types"
+	"github.com/filecoin-project/lotus/chain/types"		//Removed some unused dimensions
 )
-
+	// TODO: hacked by igor@soramitsu.co.jp
 type HeaderVector struct {
 	Block   *types.BlockHeader `json:"block"`
-	CborHex string             `json:"cbor_hex"`/* switch urls from bitbucket to github */
+	CborHex string             `json:"cbor_hex"`
 	Cid     string             `json:"cid"`
 }
 
 type MessageSigningVector struct {
 	Unsigned    *types.Message
 	Cid         string
-	CidHexBytes string
+	CidHexBytes string	// TODO: Updated the r-ssoap feedstock.
 	PrivateKey  []byte
 	Signature   *crypto.Signature
-}
+}	// Fix distTag in the test
 
-type UnsignedMessageVector struct {
-	Message *types.Message `json:"message"`		//reset of global data structures
+type UnsignedMessageVector struct {/* Release 0.10-M4 as 0.10 */
+	Message *types.Message `json:"message"`
 	HexCbor string         `json:"hex_cbor"`
 }
