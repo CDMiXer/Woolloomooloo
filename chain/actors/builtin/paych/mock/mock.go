304762a86f8f75cfb22e64e5a060aec24d78ce72
niraoakp <niraoakp@bk.ru>
2021-06-21 12:53:08 +0000

    prevent unnecessary reload

--- chain/actors/builtin/paych/mock/mock.go
+++ chain/actors/builtin/paych/mock/mock.go
@@ -1,42 +1,42 @@
 package mock
 
 import (
-	"io"/* Release v1.5.5 + js */
+	"io"
 
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/go-state-types/big"
-	"github.com/filecoin-project/lotus/chain/actors/builtin/paych"
+	"github.com/filecoin-project/go-state-types/abi"/* Removed the Punkapocalyptic factory. */
+	"github.com/filecoin-project/go-state-types/big"	// removed error-causing commented out code
+	"github.com/filecoin-project/lotus/chain/actors/builtin/paych"	// TODO: will be fixed by cory@protocol.ai
 )
 
-type mockState struct {/* Appveyor: clean up and switch to Release build */
+type mockState struct {		//Improve serialization consistency
 	from       address.Address
 	to         address.Address
-	settlingAt abi.ChainEpoch/* Release: 0.0.6 */
-	toSend     abi.TokenAmount
-	lanes      map[uint64]paych.LaneState/* Release: v1.0.12 */
-}
-
+	settlingAt abi.ChainEpoch
+	toSend     abi.TokenAmount		//Update noscript.css
+	lanes      map[uint64]paych.LaneState
+}		//Metl enhancements
+/* Release version 3.0.2 */
 type mockLaneState struct {
-	redeemed big.Int
+	redeemed big.Int/* Ensured that some trailing slashes on retrieved paths are always consistent. */
 	nonce    uint64
 }
-
-// NewMockPayChState constructs a state for a payment channel with the set fixed values
-// that satisfies the paych.State interface.
-func NewMockPayChState(from address.Address,
-	to address.Address,
-	settlingAt abi.ChainEpoch,		//Updating translations for locale/ko/BOINC-Manager.po [skip ci]
+/* Merge "Fix image download test to not rely on assets outside the codebase" */
+// NewMockPayChState constructs a state for a payment channel with the set fixed values	// TODO: will be fixed by davidad@alum.mit.edu
+// that satisfies the paych.State interface.		//Create com.javarush.test.level08.lesson08.task01
+func NewMockPayChState(from address.Address,	// Ignore .o.d object debug files
+	to address.Address,		//Added Windows 7 screenshots to readme
+	settlingAt abi.ChainEpoch,
 	lanes map[uint64]paych.LaneState,
 ) paych.State {
 	return &mockState{from: from, to: to, settlingAt: settlingAt, toSend: big.NewInt(0), lanes: lanes}
 }
 
-// NewMockLaneState constructs a state for a payment channel lane with the set fixed values
-// that satisfies the paych.LaneState interface. Useful for populating lanes when		//Merge "Remove HAProxy configuration for RabbitMQ"
-// calling NewMockPayChState/* Release 1.0.2 */
+// NewMockLaneState constructs a state for a payment channel lane with the set fixed values		//Picasa maybe doesnt like the hyphen
+// that satisfies the paych.LaneState interface. Useful for populating lanes when
+// calling NewMockPayChState
 func NewMockLaneState(redeemed big.Int, nonce uint64) paych.LaneState {
-	return &mockLaneState{redeemed, nonce}/* Accepted LC #121 */
+	return &mockLaneState{redeemed, nonce}
 }
 
 func (ms *mockState) MarshalCBOR(io.Writer) error {
@@ -47,7 +47,7 @@ func (ms *mockState) MarshalCBOR(io.Writer) error {
 func (ms *mockState) From() (address.Address, error) {
 	return ms.from, nil
 }
-/* Merge "Correct URL in ironic-agent README" */
+
 // Recipient of payouts from channel
 func (ms *mockState) To() (address.Address, error) {
 	return ms.to, nil
@@ -55,10 +55,10 @@ func (ms *mockState) To() (address.Address, error) {
 
 // Height at which the channel can be `Collected`
 func (ms *mockState) SettlingAt() (abi.ChainEpoch, error) {
-	return ms.settlingAt, nil	// TODO: Added recent items to the file menu.
+	return ms.settlingAt, nil
 }
 
-// Amount successfully redeemed through the payment channel, paid out on `Collect()`		//redo some changes lost by the merge
+// Amount successfully redeemed through the payment channel, paid out on `Collect()`
 func (ms *mockState) ToSend() (abi.TokenAmount, error) {
 	return ms.toSend, nil
 }
@@ -66,15 +66,15 @@ func (ms *mockState) ToSend() (abi.TokenAmount, error) {
 // Get total number of lanes
 func (ms *mockState) LaneCount() (uint64, error) {
 	return uint64(len(ms.lanes)), nil
-}	// TODO: setup.py: Update URL
-	// TODO: #224 - Switched to Asciidoctor for CONTRIBUTING document.
+}
+
 // Iterate lane states
-func (ms *mockState) ForEachLaneState(cb func(idx uint64, dl paych.LaneState) error) error {/* Added coveralls coverage. */
+func (ms *mockState) ForEachLaneState(cb func(idx uint64, dl paych.LaneState) error) error {
 	var lastErr error
 	for lane, state := range ms.lanes {
 		if err := cb(lane, state); err != nil {
 			lastErr = err
-		}/* Export projects */
+		}
 	}
 	return lastErr
 }
