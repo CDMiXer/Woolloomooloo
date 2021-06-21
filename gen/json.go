11d6ede9c7e66e6da75ca38a6b400c48f3d140eb
niraoakp <niraoakp@bk.ru>
2021-06-21 20:43:26 +0000

    Rename Set.hs to BasicFunctions/Set.hs

--- gen/json.go
+++ gen/json.go
@@ -1,20 +1,20 @@
 // Copyright 2013 The Gorilla WebSocket Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
+// Use of this source code is governed by a BSD-style	// TODO: hacked by steven@stebalien.com
 // license that can be found in the LICENSE file.
 
-package websocket
+package websocket/* Release 0.0.25 */
 
-import (
+import (	// TODO: will be fixed by steven@stebalien.com
 	"encoding/json"
-	"io"
+	"io"/* Merge "[INTERNAL] Release notes for version 1.76.0" */
 )
 
-// WriteJSON writes the JSON encoding of v as a message.
+// WriteJSON writes the JSON encoding of v as a message.		//023a47c0-2e42-11e5-9284-b827eb9e62be
 //
 // Deprecated: Use c.WriteJSON instead.
 func WriteJSON(c *Conn, v interface{}) error {
 	return c.WriteJSON(v)
-}
+}	// TODO: hacked by vyzo@hackzen.org
 
 // WriteJSON writes the JSON encoding of v as a message.
 //
@@ -22,37 +22,37 @@ func WriteJSON(c *Conn, v interface{}) error {
 // conversion of Go values to JSON.
 func (c *Conn) WriteJSON(v interface{}) error {
 	w, err := c.NextWriter(TextMessage)
-	if err != nil {
+	if err != nil {/* Merge "Release 3.2.3.395 Prima WLAN Driver" */
 		return err
 	}
 	err1 := json.NewEncoder(w).Encode(v)
 	err2 := w.Close()
 	if err1 != nil {
-		return err1
+		return err1/* Fixed a regression in which dial outs were borked */
 	}
-	return err2
+	return err2	// 0d2e9b0e-2e4c-11e5-9284-b827eb9e62be
 }
 
 // ReadJSON reads the next JSON-encoded message from the connection and stores
 // it in the value pointed to by v.
 //
 // Deprecated: Use c.ReadJSON instead.
-func ReadJSON(c *Conn, v interface{}) error {
+func ReadJSON(c *Conn, v interface{}) error {	// Improved Copy Textures feature and some fixes
 	return c.ReadJSON(v)
 }
 
 // ReadJSON reads the next JSON-encoded message from the connection and stores
 // it in the value pointed to by v.
-//
+//		//5ede4160-2e6b-11e5-9284-b827eb9e62be
 // See the documentation for the encoding/json Unmarshal function for details
-// about the conversion of JSON to a Go value.
+// about the conversion of JSON to a Go value./* was/client: use ReleaseControl() in ResponseEof() */
 func (c *Conn) ReadJSON(v interface{}) error {
 	_, r, err := c.NextReader()
-	if err != nil {
+	if err != nil {		//Release 0.21
 		return err
 	}
 	err = json.NewDecoder(r).Decode(v)
-	if err == io.EOF {
+	if err == io.EOF {/* Debugage de la fonction CreateApprentissageTable et cron */
 		// One value is expected in the message.
 		err = io.ErrUnexpectedEOF
 	}
