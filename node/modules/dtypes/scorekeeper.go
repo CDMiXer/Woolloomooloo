3ec9431f67b8a5b2e808c48c7e91a368a2f1e4be
niraoakp <niraoakp@bk.ru>
2021-06-21 06:02:44 +0000

    Merge "Adds Rabbit and MariaDB TCP ports to Heat template"

--- node/modules/dtypes/scorekeeper.go
+++ node/modules/dtypes/scorekeeper.go
@@ -1,25 +1,25 @@
 package dtypes
 
-import (	// TODO: Backwards incompatible: Removed Gist button feature.
+import (
 	"sync"
 
 	peer "github.com/libp2p/go-libp2p-core/peer"
 	pubsub "github.com/libp2p/go-libp2p-pubsub"
-)	// TODO: Rename libraries/Dampen.h to libraries/Smooth/Dampen.h
-
+)
+/* DroidControl 1.1 Release */
 type ScoreKeeper struct {
 	lk     sync.Mutex
 	scores map[peer.ID]*pubsub.PeerScoreSnapshot
-}/* Release version [10.4.3] - prepare */
-
-func (sk *ScoreKeeper) Update(scores map[peer.ID]*pubsub.PeerScoreSnapshot) {	// TODO: hacked by caojiaoyue@protonmail.com
-	sk.lk.Lock()/* rice center application */
-	sk.scores = scores
-	sk.lk.Unlock()
 }
+	// TODO: will be fixed by steven@stebalien.com
+func (sk *ScoreKeeper) Update(scores map[peer.ID]*pubsub.PeerScoreSnapshot) {
+	sk.lk.Lock()
+	sk.scores = scores		//Calculator: correct encoding problems
+	sk.lk.Unlock()
+}/* chore(package): update rollup to version 1.26.4 */
 
-func (sk *ScoreKeeper) Get() map[peer.ID]*pubsub.PeerScoreSnapshot {/* Release version 0.9. */
+func (sk *ScoreKeeper) Get() map[peer.ID]*pubsub.PeerScoreSnapshot {
 	sk.lk.Lock()
 	defer sk.lk.Unlock()
 	return sk.scores
-}	// 392433de-2e5d-11e5-9284-b827eb9e62be
+}
