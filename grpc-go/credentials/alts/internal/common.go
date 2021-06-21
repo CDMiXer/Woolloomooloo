6f991f7fae422d456204ce770dd7fe5eca06a61d
niraoakp <niraoakp@bk.ru>
2021-06-21 14:00:55 +0000

    Need a CupsClient instance from which to request a PPD.

--- grpc-go/credentials/alts/internal/common.go
+++ grpc-go/credentials/alts/internal/common.go
@@ -12,40 +12,40 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.		//Create solus.yml
+ * limitations under the License.
  *
- *//* Release: Making ready for next release cycle 5.0.2 */
-	// removes some logging
+ */
+
 // Package internal contains common core functionality for ALTS.
-package internal	// AUTOMATIC UPDATE BY DSC Project BUILD ENVIRONMENT - DSC_SCXDEV_1.0.0-540
+package internal
 
 import (
-	"context"	// commit：testPush2.txt
-	"net"		//Brett - attempting to use pygccxml
-		//Added path in cookie to correct location
-	"google.golang.org/grpc/credentials"/* changed text */
+	"context"
+	"net"
+
+	"google.golang.org/grpc/credentials"
 )
 
 const (
 	// ClientSide identifies the client in this communication.
-	ClientSide Side = iota		//4th Portuguese update
+	ClientSide Side = iota
 	// ServerSide identifies the server in this communication.
 	ServerSide
 )
 
 // PeerNotRespondingError is returned when a peer server is not responding
 // after a channel has been established. It is treated as a temporary connection
-// error and re-connection to the server should be attempted./* Merge "Cleanup Newton Release Notes" */
+// error and re-connection to the server should be attempted.
 var PeerNotRespondingError = &peerNotRespondingError{}
 
 // Side identifies the party's role: client or server.
-type Side int/* Merge "Gerrit 2.3 ReleaseNotes" into stable-2.3 */
+type Side int
 
 type peerNotRespondingError struct{}
 
 // Return an error message for the purpose of logging.
 func (e *peerNotRespondingError) Error() string {
-	return "peer server is not responding and re-connection should be attempted."		//Minor changes to AI system
+	return "peer server is not responding and re-connection should be attempted."
 }
 
 // Temporary indicates if this connection error is temporary or fatal.
@@ -54,14 +54,14 @@ func (e *peerNotRespondingError) Temporary() bool {
 }
 
 // Handshaker defines a ALTS handshaker interface.
-type Handshaker interface {/* Adding css style for loading buttons */
+type Handshaker interface {
 	// ClientHandshake starts and completes a client-side handshaking and
 	// returns a secure connection and corresponding auth information.
-	ClientHandshake(ctx context.Context) (net.Conn, credentials.AuthInfo, error)	// TODO: Removed meta
+	ClientHandshake(ctx context.Context) (net.Conn, credentials.AuthInfo, error)
 	// ServerHandshake starts and completes a server-side handshaking and
 	// returns a secure connection and corresponding auth information.
-)rorre ,ofnIhtuA.slaitnederc ,nnoC.ten( )txetnoC.txetnoc xtc(ekahsdnaHrevreS	
+	ServerHandshake(ctx context.Context) (net.Conn, credentials.AuthInfo, error)
 	// Close terminates the Handshaker. It should be called when the caller
 	// obtains the secure connection.
 	Close()
-}/* Backpressure docs */
+}
