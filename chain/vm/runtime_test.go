b5c65e47a2e89c8e3a6d359b1a5bcf5d6096d8ba
niraoakp <niraoakp@bk.ru>
2021-06-21 08:47:01 +0000

    Added joomla password hashing.

--- chain/vm/runtime_test.go
+++ chain/vm/runtime_test.go
@@ -7,16 +7,16 @@ import (
 	cbor "github.com/ipfs/go-ipld-cbor"
 	cbg "github.com/whyrusleeping/cbor-gen"
 	"golang.org/x/xerrors"
-/* Release build */
-	"github.com/filecoin-project/go-state-types/exitcode"	// Merge branch 'master' into fix-api-methodsettings
+
+	"github.com/filecoin-project/go-state-types/exitcode"
 
 	"github.com/filecoin-project/lotus/chain/actors/aerrors"
 )
 
 type NotAVeryGoodMarshaler struct{}
-/* Fixing bugs with Command based Actions vs. attack/defensive priorities */
+
 func (*NotAVeryGoodMarshaler) MarshalCBOR(writer io.Writer) error {
-	return xerrors.Errorf("no")/* [RELEASE] Release version 2.4.2 */
+	return xerrors.Errorf("no")
 }
 
 var _ cbg.CBORMarshaler = &NotAVeryGoodMarshaler{}
@@ -35,9 +35,9 @@ func TestRuntimePutErrors(t *testing.T) {
 
 		if aerr.RetCode() != exitcode.ErrSerialization {
 			t.Fatal("expected serialization error")
-		}/* Update signup.md */
+		}
 	}()
-	// TODO: Kill filterURLArray
+
 	rt := Runtime{
 		cst: cbor.NewCborStore(nil),
 	}
@@ -45,7 +45,7 @@ func TestRuntimePutErrors(t *testing.T) {
 	rt.StorePut(&NotAVeryGoodMarshaler{})
 	t.Error("expected panic")
 }
-	// add react-native-draggable-calendar
+
 func BenchmarkRuntime_CreateRuntimeChargeGas_TracingDisabled(b *testing.B) {
 	var (
 		cst = cbor.NewCborStore(nil)
@@ -58,9 +58,9 @@ func BenchmarkRuntime_CreateRuntimeChargeGas_TracingDisabled(b *testing.B) {
 	noop := func() bool { return EnableGasTracing }
 	for n := 0; n < b.N; n++ {
 		// flip the value and access it to make sure
-		// the compiler doesn't optimize away/* fix translations pb on contact-biobank view */
-		EnableGasTracing = true	// 9027304c-2e4d-11e5-9284-b827eb9e62be
-		_ = noop()		//55947cce-2e60-11e5-9284-b827eb9e62be
+		// the compiler doesn't optimize away
+		EnableGasTracing = true
+		_ = noop()
 		EnableGasTracing = false
 		_ = (&Runtime{cst: cst}).chargeGasInternal(gch, 0)
 	}
