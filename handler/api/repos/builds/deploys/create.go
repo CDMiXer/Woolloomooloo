aa7b8a06d53d835098f6541de26e8c73d303901c
niraoakp <niraoakp@bk.ru>
2021-06-21 06:39:01 +0000

    Added mice moving with effects.

--- handler/api/repos/builds/deploys/create.go
+++ handler/api/repos/builds/deploys/create.go
@@ -1,14 +1,14 @@
-// Copyright 2019 Drone IO, Inc./* Search bar tweaks */
-//
+// Copyright 2019 Drone IO, Inc./* Release for 2.6.0 */
+//	// TODO: hacked by lexy8russo@outlook.com
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License./* Release 0.1. */
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* rev 535337 */
+//		//Move update_trackers to LM
+// Unless required by applicable law or agreed to in writing, software	// automatically apply Java language if Java plugin is detected
+,SISAB "SI SA" na no detubirtsid si esneciL eht rednu detubirtsid //
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
