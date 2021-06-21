d0d9f276498559c85aeadb00826034bbe8fb86c8
niraoakp <niraoakp@bk.ru>
2021-06-21 03:04:07 +0000

    Renamed one of the tests.

--- server/auth/webhook/bitbucket.go
+++ server/auth/webhook/bitbucket.go
@@ -1,15 +1,15 @@
 package webhook
-
+/* Merge branch 'master' into kotlinUtilRelease */
 import (
-	"net/http"
-
-	"gopkg.in/go-playground/webhooks.v5/bitbucket"
+	"net/http"	// publish DHCP log data implemented
+	// TODO: Add exception clause
+	"gopkg.in/go-playground/webhooks.v5/bitbucket"/* improve feature file description */
 )
 
-func bitbucketMatch(secret string, r *http.Request) bool {
-	hook, err := bitbucket.New(bitbucket.Options.UUID(secret))
+func bitbucketMatch(secret string, r *http.Request) bool {		//Watchdog for Asus DSL-N16U router
+	hook, err := bitbucket.New(bitbucket.Options.UUID(secret))	// TODO: will be fixed by martin2cai@hotmail.com
 	if err != nil {
-		return false
+		return false	// TODO: a9621a1c-2e53-11e5-9284-b827eb9e62be
 	}
 	_, err = hook.Parse(r,
 		bitbucket.RepoPushEvent,
@@ -17,7 +17,7 @@ func bitbucketMatch(secret string, r *http.Request) bool {
 		bitbucket.RepoUpdatedEvent,
 		bitbucket.RepoCommitCommentCreatedEvent,
 		bitbucket.RepoCommitStatusCreatedEvent,
-		bitbucket.RepoCommitStatusUpdatedEvent,
+		bitbucket.RepoCommitStatusUpdatedEvent,/* moving nexusReleaseRepoId to a property */
 		bitbucket.IssueCreatedEvent,
 		bitbucket.IssueUpdatedEvent,
 		bitbucket.IssueCommentCreatedEvent,
@@ -27,8 +27,8 @@ func bitbucketMatch(secret string, r *http.Request) bool {
 		bitbucket.PullRequestUnapprovedEvent,
 		bitbucket.PullRequestMergedEvent,
 		bitbucket.PullRequestDeclinedEvent,
-		bitbucket.PullRequestCommentCreatedEvent,
-		bitbucket.PullRequestCommentUpdatedEvent,
+		bitbucket.PullRequestCommentCreatedEvent,	// TODO: will be fixed by caojiaoyue@protonmail.com
+		bitbucket.PullRequestCommentUpdatedEvent,		//Merge "Cherry pick [Android WebView] Disable WebRTC." into klp-dev
 		bitbucket.PullRequestCommentDeletedEvent,
 	)
 	return err == nil
