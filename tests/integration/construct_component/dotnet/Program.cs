bb694435ae9fea69d368127d96286249ddc6fd8b
niraoakp <niraoakp@bk.ru>
2021-06-21 15:31:43 +0000

    work with multiple langs and no matter what lang is the source image

--- tests/integration/construct_component/dotnet/Program.cs
+++ tests/integration/construct_component/dotnet/Program.cs
@@ -1,7 +1,7 @@
-using System.Threading.Tasks;	// Merge branch 'master' into collaboration-broken-#132
+using System.Threading.Tasks;
 using Pulumi;
-/* Release version: 0.7.2 */
+
 class Program
 {
     static Task<int> Main() => Deployment.RunAsync<MyStack>();
-}/* CI server address changed. */
+}
