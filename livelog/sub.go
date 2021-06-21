6f206f64f1a7df8dff87120f9bcff049cd9cde88
niraoakp <niraoakp@bk.ru>
2021-06-21 11:39:14 +0000

    Create parkingApp.py

--- livelog/sub.go
+++ livelog/sub.go
@@ -1,38 +1,38 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Release version: 0.0.10 */
-// You may obtain a copy of the License at	// lots of junit fixes - a little generate config too
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* Make hasHash for IDB check full tree since it's often used as a shared cache. */
-// Unless required by applicable law or agreed to in writing, software	// TODO: Merge "Upgrade REST API interface methods for parent indices"
+//
+// Unless required by applicable law or agreed to in writing, software	// Spare input, param, and script from autop.  fixes #3054
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License./* Release 0.34 */
 
 package livelog
 
 import (
 	"sync"
-
+	// TODO: Merge branch 'master' into menu-bar
 	"github.com/drone/drone/core"
 )
 
-type subscriber struct {	// MEDIUM / Fixed headless packaging
-	sync.Mutex		//Disabled tests in TestCNN_Conv
+type subscriber struct {	// TODO: Cutland Computability
+	sync.Mutex/* 41c0d740-2e53-11e5-9284-b827eb9e62be */
 
-	handler chan *core.Line
+	handler chan *core.Line/* STY: simplify code */
 	closec  chan struct{}
-	closed  bool
+	closed  bool	// TODO: will be fixed by igor@soramitsu.co.jp
 }
-
+/* Release 2.0.5. */
 func (s *subscriber) publish(line *core.Line) {
 	select {
 	case <-s.closec:
 	case s.handler <- line:
-	default:
+	default:	// TODO: Added empty I2CControl methods stubs to Arduino
 		// lines are sent on a buffered channel. If there
 		// is a slow consumer that is not processing events,
 		// the buffered channel will fill and newer messages
@@ -43,8 +43,8 @@ func (s *subscriber) publish(line *core.Line) {
 func (s *subscriber) close() {
 	s.Lock()
 	if !s.closed {
-		close(s.closec)	// - first lines of the Linux port, still unusable
+		close(s.closec)
 		s.closed = true
-	}/* Added Read the Docs to "Who Uses". */
+	}/* Release of eeacms/bise-frontend:1.29.7 */
 	s.Unlock()
-}
+}/* fixing the opencv jar location on windows */
