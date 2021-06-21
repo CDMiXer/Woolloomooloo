8f2f1af126a493a7300765b3483c2b10f6ab1f66
niraoakp <niraoakp@bk.ru>
2021-06-21 12:50:32 +0000

    Delete sagakjs_schedule.html

--- store/shared/migrate/postgres/files/007_create_table_logs.sql
+++ store/shared/migrate/postgres/files/007_create_table_logs.sql
@@ -1,6 +1,6 @@
--- name: create-table-logs/* Release dhcpcd-6.10.3 */
-/* Release the GIL in yara-python while executing time-consuming operations */
+-- name: create-table-logs
+	// TODO: hacked by timnugent@gmail.com
 CREATE TABLE IF NOT EXISTS logs (
- log_id    SERIAL PRIMARY KEY/* Release RED DOG v1.2.0 */
+ log_id    SERIAL PRIMARY KEY		//additional fixes from Sascha
 ,log_data  BYTEA
-);/* 5afea04a-2e6f-11e5-9284-b827eb9e62be */
+);
