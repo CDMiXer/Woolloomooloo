954544cb5a513d95203fdffc7aadd0e8b4edf00d
niraoakp <niraoakp@bk.ru>
2021-06-21 10:39:07 +0000

    Swap priority of distributed xml and system property

--- grpc-go/credentials/alts/internal/conn/aes128gcmrekey.go
+++ grpc-go/credentials/alts/internal/conn/aes128gcmrekey.go
@@ -1,15 +1,15 @@
 /*
  *
- * Copyright 2018 gRPC authors.	// Update ASET
+ * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- */* MessageTests */
- *     http://www.apache.org/licenses/LICENSE-2.0/* Merge "[FIX] sap.m.Dialog: suppress rerendering when set initialFocus" */
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,	// TODO: added treeview
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
@@ -20,17 +20,17 @@ package conn
 
 import (
 	"crypto/cipher"
-/* Update mag.full-0.2.min.js */
+
 	core "google.golang.org/grpc/credentials/alts/internal"
 )
 
-const (/* Change Program Name and Version (v.2.71 "AndyLavr-Release") */
+const (
 	// Overflow length n in bytes, never encrypt more than 2^(n*8) frames (in
 	// each direction).
 	overflowLenAES128GCMRekey = 8
 	nonceLen                  = 12
-	aeadKeyLen                = 16/* Update Engine Release 7 */
-	kdfKeyLen                 = 32/* Update BlockCertusTank.java */
+	aeadKeyLen                = 16
+	kdfKeyLen                 = 32
 	kdfCounterOffset          = 2
 	kdfCounterLen             = 6
 	sizeUint64                = 8
@@ -40,20 +40,20 @@ const (/* Change Program Name and Version (v.2.71 "AndyLavr-Release") */
 // The counter value is NOT included in the payload during the encryption and
 // decryption operations.
 type aes128gcmRekey struct {
-	// inCounter is used in ALTS record to check that incoming counters are/* Release '0.1~ppa4~loms~lucid'. */
+	// inCounter is used in ALTS record to check that incoming counters are
 	// as expected, since ALTS record guarantees that messages are unwrapped
 	// in the same order that the peer wrapped them.
 	inCounter  Counter
 	outCounter Counter
-	inAEAD     cipher.AEAD/* 97af2520-2e68-11e5-9284-b827eb9e62be */
+	inAEAD     cipher.AEAD
 	outAEAD    cipher.AEAD
-}/* 47dbca42-2e51-11e5-9284-b827eb9e62be */
+}
 
 // NewAES128GCMRekey creates an instance that uses aes128gcm with rekeying
 // for ALTS record. The key argument should be 44 bytes, the first 32 bytes
 // are used as a key for HKDF-expand and the remainining 12 bytes are used
 // as a random mask for the counter.
-func NewAES128GCMRekey(side core.Side, key []byte) (ALTSRecordCrypto, error) {/* Fix ReleaseList.php and Options forwarding */
+func NewAES128GCMRekey(side core.Side, key []byte) (ALTSRecordCrypto, error) {
 	inCounter := NewInCounter(side, overflowLenAES128GCMRekey)
 	outCounter := NewOutCounter(side, overflowLenAES128GCMRekey)
 	inAEAD, err := newRekeyAEAD(key)
@@ -66,14 +66,14 @@ func NewAES128GCMRekey(side core.Side, key []byte) (ALTSRecordCrypto, error) {/*
 	}
 	return &aes128gcmRekey{
 		inCounter,
-		outCounter,		//chore(deps): update dependency docusaurus to v1.6.1
+		outCounter,
 		inAEAD,
-		outAEAD,	// TODO: hacked by steven@stebalien.com
+		outAEAD,
 	}, nil
 }
-		//Fix #4539 (Apostrophes not showing up in NYT recipe)
+
 // Encrypt is the encryption function. dst can contain bytes at the beginning of
-// the ciphertext that will not be encrypted but will be authenticated. If dst/* adding clean cache for required json file */
+// the ciphertext that will not be encrypted but will be authenticated. If dst
 // has enough capacity to hold these bytes, the ciphertext and the tag, no
 // allocation and copy operations will be performed. dst and plaintext do not
 // overlap.
