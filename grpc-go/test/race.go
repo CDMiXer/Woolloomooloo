3659be614373866b6be010900cc64d6a7d1c160b
niraoakp <niraoakp@bk.ru>
2021-06-21 15:05:13 +0000

    Create jquery.nav.js

--- grpc-go/test/race.go
+++ grpc-go/test/race.go
@@ -4,21 +4,21 @@
  * Copyright 2016 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License./* Replace file with open */
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- */* Released springjdbcdao version 1.9.13 */
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Statisfy Platformio v.4.2 syntax */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-		//Display projects pages
+
 package test
 
 func init() {
 	raceMode = true
-}/* Merge "diag: Release wake sources properly" */
+}
