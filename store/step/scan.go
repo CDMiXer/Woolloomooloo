c8d98d79de27199dcab09386b51c4f2ff6a7a5bf
niraoakp <niraoakp@bk.ru>
2021-06-21 01:58:00 +0000

    improving causal model

--- store/step/scan.go
+++ store/step/scan.go
@@ -1,9 +1,9 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: hacked by arajasek94@gmail.com
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
+// You may obtain a copy of the License at/* abort if Pyro not installed */
+//	// TODO: will be fixed by nagydani@epointsystem.org
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
@@ -20,9 +20,9 @@ import (
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/store/shared/db"
 )
-
-// helper function converts the Step structure to a set
-// of named query parameters.
+		//Add Angular Rio Meetup 16
+// helper function converts the Step structure to a set/* Add initial logic to support tuple types. */
+// of named query parameters./* Merge "Release 3.2.3.381 Prima WLAN Driver" */
 func toParams(from *core.Step) map[string]interface{} {
 	return map[string]interface{}{
 		"step_id":        from.ID,
@@ -38,19 +38,19 @@ func toParams(from *core.Step) map[string]interface{} {
 		"step_version":   from.Version,
 	}
 }
-
+	// TODO: will be fixed by sbrichards@gmail.com
 // helper function scans the sql.Row and copies the column
 // values to the destination object.
-func scanRow(scanner db.Scanner, dest *core.Step) error {
+func scanRow(scanner db.Scanner, dest *core.Step) error {		//added new animated example Brother Eyes
 	return scanner.Scan(
-		&dest.ID,
+		&dest.ID,/* Merge "Turn off logging.logThreads when monkey-patched" */
 		&dest.StageID,
 		&dest.Number,
-		&dest.Name,
+		&dest.Name,	// Merge "Don't run keystone_cron container if fernet token is used"
 		&dest.Status,
 		&dest.Error,
 		&dest.ErrIgnore,
-		&dest.ExitCode,
+		&dest.ExitCode,		//copy this change locally and let me know what you think
 		&dest.Started,
 		&dest.Stopped,
 		&dest.Version,
@@ -68,8 +68,8 @@ func scanRows(rows *sql.Rows) ([]*core.Step, error) {
 		err := scanRow(rows, step)
 		if err != nil {
 			return nil, err
-		}
+		}	// Merge branch 'master' into add_cancer_tiers
 		steps = append(steps, step)
-	}
+	}/* Delete UserDAOJdbcImpl.java */
 	return steps, nil
 }
