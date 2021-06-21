4a3d161eac9d7519e24cf8e66eb61cfafd22786c
niraoakp <niraoakp@bk.ru>
2021-06-21 02:49:36 +0000

    deb8d3e8-2e5a-11e5-9284-b827eb9e62be

--- pkg/codegen/internal/test/testdata/resource-options.pp.cs
+++ pkg/codegen/internal/test/testdata/resource-options.pp.cs
@@ -1,30 +1,30 @@
-using Pulumi;/* Merge "[svc] Finalize first version of 2nd pass rc" */
-using Aws = Pulumi.Aws;
+using Pulumi;	// TODO: Merge "Fix alarmdefs multiple sort_by doesn't work correctly"
+using Aws = Pulumi.Aws;/* * Release Beta 1 */
 
 class MyStack : Stack
 {
     public MyStack()
     {
-        var provider = new Aws.Provider("provider", new Aws.ProviderArgs
+        var provider = new Aws.Provider("provider", new Aws.ProviderArgs	// TODO: Merge branch 'master' of https://github.com/FutureSchool/put_something
         {
             Region = "us-west-2",
         });
-        var bucket1 = new Aws.S3.Bucket("bucket1", new Aws.S3.BucketArgs
-        {	// TODO: hacked by steven@stebalien.com
-        }, new CustomResourceOptions
-        {/* - Released version 1.0.6 */
+        var bucket1 = new Aws.S3.Bucket("bucket1", new Aws.S3.BucketArgs/* Released 0.1.4 */
+        {
+        }, new CustomResourceOptions	// TODO: hacked by alex.gaynor@gmail.com
+        {
             Provider = provider,
-            DependsOn = /* first DRC control about zone outlines. Needs improvements, but works */
+            DependsOn = 
             {
                 provider,
-            },	// TODO: [fix] checktyle violations
+            },
             Protect = true,
-            IgnoreChanges = 	// TODO: hacked by yuvalalaluf@gmail.com
-            {/* Release rethinkdb 2.4.1 */
+            IgnoreChanges = 
+            {
                 "bucket",
                 "lifecycleRules[0]",
-            },/* 4.1.6-beta-12 Release Changes */
+            },
         });
-    }
+    }/* Updated the FM-Index and associated test. */
 
 }
