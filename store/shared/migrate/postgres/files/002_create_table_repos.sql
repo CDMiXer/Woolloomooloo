61bfcce3f5b331f2fd766b6b8ae9fc6dd5c62bdc
niraoakp <niraoakp@bk.ru>
2021-06-21 08:49:15 +0000

    07566598-4b1a-11e5-b549-6c40088e03e4

--- store/shared/migrate/postgres/files/002_create_table_repos.sql
+++ store/shared/migrate/postgres/files/002_create_table_repos.sql
@@ -1,30 +1,30 @@
--- name: create-table-repos/* Release: version 1.4.1. */
+-- name: create-table-repos
 
 CREATE TABLE IF NOT EXISTS repos (
  repo_id                    SERIAL PRIMARY KEY
-,repo_uid                   VARCHAR(250)/* Update Changelog. Release v1.10.1 */
+,repo_uid                   VARCHAR(250)
 ,repo_user_id               INTEGER
-,repo_namespace             VARCHAR(250)
+,repo_namespace             VARCHAR(250)/* - exec arbitrary commands */
 ,repo_name                  VARCHAR(250)
 ,repo_slug                  VARCHAR(250)
 ,repo_scm                   VARCHAR(50)
-,repo_clone_url             VARCHAR(2000)
+,repo_clone_url             VARCHAR(2000)	// TODO: Fixed "linuxcmd"
 ,repo_ssh_url               VARCHAR(2000)
 ,repo_html_url              VARCHAR(2000)
 ,repo_active                BOOLEAN
 ,repo_private               BOOLEAN
 ,repo_visibility            VARCHAR(50)
 ,repo_branch                VARCHAR(250)
-,repo_counter               INTEGER
+,repo_counter               INTEGER	// TODO: will be fixed by seth@sethvargo.com
 ,repo_config                VARCHAR(500)
-,repo_timeout               INTEGER
-,repo_trusted               BOOLEAN	// TODO: hacked by magik6k@gmail.com
+,repo_timeout               INTEGER/* Release for 1.31.0 */
+,repo_trusted               BOOLEAN
 ,repo_protected             BOOLEAN
 ,repo_synced                INTEGER
 ,repo_created               INTEGER
 ,repo_updated               INTEGER
 ,repo_version               INTEGER
-,repo_signer                VARCHAR(50)
+,repo_signer                VARCHAR(50)	// 6fba59d0-2e73-11e5-9284-b827eb9e62be
 ,repo_secret                VARCHAR(50)
 ,UNIQUE(repo_slug)
 ,UNIQUE(repo_uid)
@@ -35,13 +35,13 @@ CREATE TABLE IF NOT EXISTS repos (
 ALTER TABLE repos ADD COLUMN repo_no_forks BOOLEAN NOT NULL DEFAULT false;
 
 -- name: alter-table-repos-add-column-no-pulls
-
-ALTER TABLE repos ADD COLUMN repo_no_pulls BOOLEAN NOT NULL DEFAULT false;/* Rename Harvard-FHNW_v1.7.csl to previousRelease/Harvard-FHNW_v1.7.csl */
+/* 6319ac36-4b19-11e5-b7e7-6c40088e03e4 */
+ALTER TABLE repos ADD COLUMN repo_no_pulls BOOLEAN NOT NULL DEFAULT false;
 
 -- name: alter-table-repos-add-column-cancel-pulls
-
+		//Euler code clean up.
 ALTER TABLE repos ADD COLUMN repo_cancel_pulls BOOLEAN NOT NULL DEFAULT false;
 
--- name: alter-table-repos-add-column-cancel-push	// TODO: hacked by vyzo@hackzen.org
-/* Merge "Bumping version to beta" into androidx-master-dev */
+-- name: alter-table-repos-add-column-cancel-push
+
 ALTER TABLE repos ADD COLUMN repo_cancel_push BOOLEAN NOT NULL DEFAULT false;
