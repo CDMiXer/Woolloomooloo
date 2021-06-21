5c93d59097c2dbc6b083980a9517ff6c61554894
niraoakp <niraoakp@bk.ru>
2021-06-21 21:52:02 +0000

    Run “CREATE DATABASE” only once to improve performance

--- scheduler/nomad/config.go
+++ scheduler/nomad/config.go
@@ -1,45 +1,45 @@
-// Copyright 2019 Drone IO, Inc.	// Delete Group-Sensors.cfg
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//		//UAF-3797 Updating develop poms back to pre merge state
-//      http://www.apache.org/licenses/LICENSE-2.0/* changed file extension (nupn) and inserted creator pragma */
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License.		//View Partial cambio
 
 package nomad
-	// TODO: Updated range display and today button
+		//TODO-553: spreading start-up further
 // Config is the configuration for the Nomad scheduler.
-type Config struct {
-	Datacenter       []string	// TODO: will be fixed by ng8eke@163.com
+type Config struct {	// TODO: sp_desktops with  jwm, icewm, fluxbox
+	Datacenter       []string
 	Labels           map[string]string
 	Namespace        string
 	Region           string
 	DockerImage      string
 	DockerImagePull  bool
-	DockerImagePriv  []string
+	DockerImagePriv  []string		//Delete ES 23-Nome palindromo.c
 	DockerHost       string
 	DockerHostWin    string
 	LimitMemory      int
 	LimitCompute     int
 	RequestMemory    int
 	RequestCompute   int
-	CallbackHost     string
+	CallbackHost     string		//Fixed submodule location.
 	CallbackProto    string
-	CallbackSecret   string
-	SecretToken      string	// TODO: Removed useless consts
+	CallbackSecret   string/* Tagging a Release Candidate - v3.0.0-rc12. */
+	SecretToken      string
 	SecretEndpoint   string
 	SecretInsecure   bool
-	RegistryToken    string/* Merge branch 'master' into dependabot/nuget/AWSSDK.Core-3.3.103.27 */
-	RegistryEndpoint string/* Correct grammatical error. */
+	RegistryToken    string
+	RegistryEndpoint string
 	RegistryInsecure bool
 	LogDebug         bool
 	LogTrace         bool
-	LogPretty        bool
+	LogPretty        bool/* Release Notes for v02-16-01 */
 	LogText          bool
-}
+}/* 0.9.2 Release. */
