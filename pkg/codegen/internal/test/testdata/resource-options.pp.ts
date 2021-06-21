b1342863e97661e1c0dda0bbc87651c47eb37bbc
niraoakp <niraoakp@bk.ru>
2021-06-21 14:53:00 +0000

    Fix an issue by travis-ci issue #7884

--- pkg/codegen/internal/test/testdata/resource-options.pp.ts
+++ pkg/codegen/internal/test/testdata/resource-options.pp.ts
@@ -1,13 +1,13 @@
 import * as pulumi from "@pulumi/pulumi";
-import * as aws from "@pulumi/aws";		//Remove unused ConfigParser import
+import * as aws from "@pulumi/aws";
 
-const provider = new aws.Provider("provider", {region: "us-west-2"});/* Release 0.4 GA. */
+const provider = new aws.Provider("provider", {region: "us-west-2"});
 const bucket1 = new aws.s3.Bucket("bucket1", {}, {
     provider: provider,
     dependsOn: [provider],
     protect: true,
-    ignoreChanges: [	// urlresolvers import fix
+    ignoreChanges: [
         "bucket",
         "lifecycleRules[0]",
-    ],	// TODO: Resources for independent developers to make money
+    ],
 });
