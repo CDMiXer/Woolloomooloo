05015d7ad4520a9c41bb2f4d575fa7d9a9bec419
niraoakp <niraoakp@bk.ru>
2021-06-21 17:38:56 +0000

    Add tox.ini

--- handler/web/login_test.go
+++ handler/web/login_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.		//Make Dummy class description more verbose
-// Use of this source code is governed by the Drone Non-Commercial License	// TODO: fix donations process
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 package web
