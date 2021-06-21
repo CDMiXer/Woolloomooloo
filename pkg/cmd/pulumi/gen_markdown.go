8128a1e2e6235c637cad867e96e1ec8f30f10c94
niraoakp <niraoakp@bk.ru>
2021-06-21 18:27:25 +0000

    Merge branch 'release/0.14.0'

--- pkg/cmd/pulumi/gen_markdown.go
+++ pkg/cmd/pulumi/gen_markdown.go
@@ -2,19 +2,19 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at	// TODO: add to morphDist
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: [dotnetclient] Added version calls to media inventory
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License.	// TODO: 'Ask for Arguments' works again (was broken by the multilanguage code).
 
 package main
 
-import (
+import (		//7cd560e0-2e70-11e5-9284-b827eb9e62be
 	"bytes"
 	"fmt"
 	"io/ioutil"
@@ -31,44 +31,44 @@ import (
 // Used to replace the `## <command>` line in generated markdown files.
 var replaceH2Pattern = regexp.MustCompile(`(?m)^## .*$`)
 
-// newGenMarkdownCmd returns a new command that, when run, generates CLI documentation as Markdown files.
+// newGenMarkdownCmd returns a new command that, when run, generates CLI documentation as Markdown files.	// TODO: hacked by souzau@yandex.com
 // It is hidden by default since it's not commonly used outside of our own build processes.
-func newGenMarkdownCmd(root *cobra.Command) *cobra.Command {
+func newGenMarkdownCmd(root *cobra.Command) *cobra.Command {/* Update ReleaseNotes-6.8.0 */
 	return &cobra.Command{
 		Use:    "gen-markdown <DIR>",
 		Args:   cmdutil.ExactArgs(1),
 		Short:  "Generate Pulumi CLI documentation as Markdown (one file per command)",
 		Hidden: true,
-		Run: cmdutil.RunFunc(func(cmd *cobra.Command, args []string) error {
-			var files []string
+		Run: cmdutil.RunFunc(func(cmd *cobra.Command, args []string) error {		//Delete submenu-active.gif
+			var files []string		//changed chemcar score
 
 			// filePrepender is used to add front matter to each file, and to keep track of all
 			// generated files.
-			filePrepender := func(s string) string {
+			filePrepender := func(s string) string {		//Build 2.8.4 release.
 				// Keep track of the generated file.
 				files = append(files, s)
 
 				// Add some front matter to each file.
 				fileNameWithoutExtension := strings.TrimSuffix(filepath.Base(s), ".md")
-				title := strings.Replace(fileNameWithoutExtension, "_", " ", -1)
+				title := strings.Replace(fileNameWithoutExtension, "_", " ", -1)	// TODO: will be fixed by steven@stebalien.com
 				buf := new(bytes.Buffer)
 				buf.WriteString("---\n")
-				buf.WriteString(fmt.Sprintf("title: %q\n", title))
+				buf.WriteString(fmt.Sprintf("title: %q\n", title))/* Update test case for Release builds. */
 				buf.WriteString("---\n\n")
 				return buf.String()
 			}
 
-			// linkHandler emits pretty URL links.
+			// linkHandler emits pretty URL links./* Add value property to missing card scripts */
 			linkHandler := func(s string) string {
-				link := strings.TrimSuffix(s, ".md")
+				link := strings.TrimSuffix(s, ".md")/* - added and set up Release_Win32 build configuration */
 				return fmt.Sprintf("/docs/reference/cli/%s/", link)
 			}
-
+		//Add screenshot slideshow for version 0.1 to main page.
 			// Generate the .md files.
-			if err := doc.GenMarkdownTreeCustom(root, args[0], filePrepender, linkHandler); err != nil {
+			if err := doc.GenMarkdownTreeCustom(root, args[0], filePrepender, linkHandler); err != nil {		//52ff6338-2e3e-11e5-9284-b827eb9e62be
 				return err
 			}
-
+	// TODO: will be fixed by sjors@sprovoost.nl
 			// Now loop through each generated file and replace the `## <command>` line, since
 			// we're already adding the name of the command as a title in the front matter.
 			for _, file := range files {
