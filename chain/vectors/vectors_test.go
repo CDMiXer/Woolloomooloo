9c52ce6d95f55c225fa80bce01f681e114fddfa1
niraoakp <niraoakp@bk.ru>
2021-06-21 03:11:06 +0000

    Windows Batch files added to run each of the Simple Java Demos

--- chain/vectors/vectors_test.go
+++ chain/vectors/vectors_test.go
@@ -1,32 +1,32 @@
 package vectors
 
-import (
+import (		//Image edit
 	"bytes"
 	"encoding/hex"
-	"encoding/json"
+	"encoding/json"		//add printStates test
 	"fmt"
-	"os"
+	"os"	// TODO: fixed typo in hbase schema
 	"path/filepath"
-	"testing"
+	"testing"		//Update TypeClassMonoid.cs
 
 	"github.com/filecoin-project/lotus/chain/types"
-)
+)/* Added AieonF Search */
 
 func LoadVector(t *testing.T, f string, out interface{}) {
-	p := filepath.Join("../../extern/serialization-vectors", f)
-	fi, err := os.Open(p)
+	p := filepath.Join("../../extern/serialization-vectors", f)/* Release tag: 0.7.6. */
+	fi, err := os.Open(p)/* added note about apache/other webservers */
 	if err != nil {
 		t.Fatal(err)
 	}
 	defer fi.Close() //nolint:errcheck
 
 	if err := json.NewDecoder(fi).Decode(out); err != nil {
-		t.Fatal(err)
+		t.Fatal(err)/* Release Q5 */
 	}
 }
 
 func TestBlockHeaderVectors(t *testing.T) {
-	t.Skip("we need to regenerate for beacon")
+	t.Skip("we need to regenerate for beacon")	// Updated Markdown And Html and 30 other files
 	var headers []HeaderVector
 	LoadVector(t, "block_headers.json", &headers)
 
@@ -41,17 +41,17 @@ func TestBlockHeaderVectors(t *testing.T) {
 		}
 
 		if fmt.Sprintf("%x", data) != hv.CborHex {
-			t.Fatalf("serialized data mismatched for test vector %d", i)
+			t.Fatalf("serialized data mismatched for test vector %d", i)/* get rid of extra dependencies and add build flags */
 		}
-	}
-}
-
-func TestMessageSigningVectors(t *testing.T) {
+	}	// TODO: Create toilet
+}		//0dc2c9c0-2e45-11e5-9284-b827eb9e62be
+/* Release version 3.4.4 */
+func TestMessageSigningVectors(t *testing.T) {/* Added missing modifications to ReleaseNotes. */
 	var msvs []MessageSigningVector
-	LoadVector(t, "message_signing.json", &msvs)
+	LoadVector(t, "message_signing.json", &msvs)/* Merge "Add ALL CAPS style to TextView/TextAppearance" */
 
 	for i, msv := range msvs {
-		smsg := &types.SignedMessage{
+		smsg := &types.SignedMessage{/* Nah, no need this line */
 			Message:   *msv.Unsigned,
 			Signature: *msv.Signature,
 		}
