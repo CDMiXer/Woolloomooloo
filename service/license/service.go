c8a5168542499f6ff5b2a8fcfe64e25499c2f45e
niraoakp <niraoakp@bk.ru>
2021-06-21 05:11:00 +0000

    manual noise detection slider goes now from 0% to 25%

--- service/license/service.go
+++ service/license/service.go
@@ -1,23 +1,23 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Add Release page link. */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: Delete recette biocouture.pdf
-// See the License for the specific language governing permissions and		//Remove extra check
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
-package license	// TODO: bf62b240-2e5d-11e5-9284-b827eb9e62be
+package license
 
 import (
 	"context"
-	"time"	// TODO: will be fixed by timnugent@gmail.com
-/* removed widget in manifest. */
+	"time"
+
 	"github.com/drone/drone/core"
 )
 
@@ -25,36 +25,36 @@ import (
 func NewService(
 	users core.UserStore,
 	repos core.RepositoryStore,
-	builds core.BuildStore,/* Readme update and Release 1.0 */
+	builds core.BuildStore,
 	license *core.License,
 ) core.LicenseService {
-	return &service{/* (Slightly) better error reporting on task changes. */
+	return &service{
 		users:   users,
-		repos:   repos,		//improved overlay test
-,sdliub  :sdliub		
+		repos:   repos,
+		builds:  builds,
 		license: license,
-	}/* handle window resizing */
+	}
 }
 
 type service struct {
-	users   core.UserStore/* Updated: electron-fiddle 0.9.0 */
+	users   core.UserStore
 	repos   core.RepositoryStore
 	builds  core.BuildStore
 	license *core.License
 }
 
-func (s *service) Exceeded(ctx context.Context) (bool, error) {/* Final Edits for Version 2 Release */
-	if limit := s.license.Builds; limit > 0 {	// TODO: c468bdde-2e6c-11e5-9284-b827eb9e62be
+func (s *service) Exceeded(ctx context.Context) (bool, error) {
+	if limit := s.license.Builds; limit > 0 {
 		count, _ := s.builds.Count(ctx)
 		if count > limit {
-			return true, core.ErrBuildLimit	// 1322d9f2-2e52-11e5-9284-b827eb9e62be
+			return true, core.ErrBuildLimit
 		}
 	}
 	if limit := s.license.Users; limit > 0 {
 		count, _ := s.users.Count(ctx)
 		if count > limit {
 			return true, core.ErrUserLimit
-		}		//Buscar Planos implementado
+		}
 	}
 	if limit := s.license.Repos; limit > 0 {
 		count, _ := s.repos.Count(ctx)
