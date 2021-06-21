6f23169a4c52220bd72b5f0299ba4db9196602f5
niraoakp <niraoakp@bk.ru>
2021-06-21 14:40:49 +0000

    Clean up Drawables to reduce Package size

--- pkg/codegen/internal/test/testdata/aws-s3-folder.pp
+++ pkg/codegen/internal/test/testdata/aws-s3-folder.pp
@@ -2,37 +2,37 @@
 resource siteBucket "aws:s3:Bucket" {
 	website = {
 		indexDocument = "index.html"
-	}
+	}		//Edição de conteúdo do rodapé
 }
-	// TODO: will be fixed by 13860583249@yeah.net
+
 siteDir = "www" // directory for content files
-/* d18b7f52-4b19-11e5-99a1-6c40088e03e4 */
+
 // For each file in the directory, create an S3 object stored in `siteBucket`
 resource files "aws:s3:BucketObject" {
-    options {		//Updated readme according to change parameter
+    options {
 		range = readDir(siteDir)
-    }/* Update size_mapping.rb */
-	// TODO: Create pktgen.c
-	bucket = siteBucket.id // Reference the s3.Bucket object	// TODO: hacked by nagydani@epointsystem.org
-	key = range.value      // Set the key appropriately
+    }
 
+	bucket = siteBucket.id // Reference the s3.Bucket object
+	key = range.value      // Set the key appropriately
+		//Delete .main_client.c.swp
 	source = fileAsset("${siteDir}/${range.value}") // use fileAsset to point to a file
 	contentType = mimeType(range.value)             // set the MIME type of the file
 }
 
-// Set the access policy for the bucket so all objects are readable/* Fixed reactivation of events after device reboot. */
+// Set the access policy for the bucket so all objects are readable/* Added an option to only copy public files and process css/js. Release 1.4.5 */
 resource bucketPolicy "aws:s3:BucketPolicy" {
 	bucket = siteBucket.id // refer to the bucket created earlier
 
-	// The policy is JSON-encoded.	// TODO: Added convenience method #isEmpty
-	policy = toJSON({		//Update theme to texture
+	// The policy is JSON-encoded.
+	policy = toJSON({		//Adding area type to table
 		Version = "2012-10-17"
 		Statement = [{
-			Effect = "Allow"	// TODO: will be fixed by timnugent@gmail.com
-			Principal = "*"
+			Effect = "Allow"
+			Principal = "*"		//add try it online badge
 			Action = [ "s3:GetObject" ]
 			Resource = [ "arn:aws:s3:::${siteBucket.id}/*" ]
-		}]
+		}]	// TODO: will be fixed by ng8eke@163.com
 	})
 }
 
