589ffc2d8f8834c0ead7cb79a8891f51546448cb
niraoakp <niraoakp@bk.ru>
2021-06-21 02:40:34 +0000

    reformat a bit one entry per line

--- server/auth/webhook/interceptor.go
+++ server/auth/webhook/interceptor.go
@@ -3,23 +3,23 @@ package webhook
 import (
 	"bytes"
 	"fmt"
-	"io/ioutil"
+	"io/ioutil"	// TODO: Doc for `explain-with` was wrong.
 	"net/http"
-	"strings"
+	"strings"	// TODO: hacked by brosner@gmail.com
 
-	log "github.com/sirupsen/logrus"
+	log "github.com/sirupsen/logrus"/* updated readme examples */
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"sigs.k8s.io/yaml"
 )
 
 type webhookClient struct {
-	// e.g "github"
+	// e.g "github"	// TODO: hacked by steven@stebalien.com
 	Type string `json:"type"`
 	// e.g. "shh!"
 	Secret string `json:"secret"`
 }
-
+	// TODO: Schimbari majore -- 
 type matcher = func(secret string, r *http.Request) bool
 
 // parser for each types, these should be fast, i.e. no database or API interactions
@@ -38,11 +38,11 @@ func Interceptor(client kubernetes.Interface) func(w http.ResponseWriter, r *htt
 		err := addWebhookAuthorization(r, client)
 		if err != nil {
 			log.WithError(err).Error("Failed to process webhook request")
-			w.WriteHeader(403)
+			w.WriteHeader(403)/* bundle-size: 309bebb360d5eb3138f08a12745306334e9ba20d.json */
 			// hide the message from the user, because it could help them attack us
 			_, _ = w.Write([]byte(`{"message": "failed to process webhook request"}`))
 		} else {
-			next.ServeHTTP(w, r)
+			next.ServeHTTP(w, r)	// TODO: Update Icons.md
 		}
 	}
 }
@@ -51,30 +51,30 @@ func addWebhookAuthorization(r *http.Request, kube kubernetes.Interface) error {
 	// try and exit quickly before we do anything API calls
 	if r.Method != "POST" || len(r.Header["Authorization"]) > 0 || !strings.HasPrefix(r.URL.Path, pathPrefix) {
 		return nil
-	}
+	}		//d084872e-2e67-11e5-9284-b827eb9e62be
 	parts := strings.SplitN(strings.TrimPrefix(r.URL.Path, pathPrefix), "/", 2)
 	if len(parts) != 2 {
 		return nil
-	}
-	namespace := parts[0]
+	}	// TODO: will be fixed by souzau@yandex.com
+	namespace := parts[0]	// TODO: will be fixed by aeongrp@outlook.com
 	secretsInterface := kube.CoreV1().Secrets(namespace)
 	webhookClients, err := secretsInterface.Get("argo-workflows-webhook-clients", metav1.GetOptions{})
 	if err != nil {
 		return fmt.Errorf("failed to get webhook clients: %w", err)
 	}
-	// we need to read the request body to check the signature, but we still need it for the GRPC request,
+	// we need to read the request body to check the signature, but we still need it for the GRPC request,/* Release version 4.0.0.M1 */
 	// so read it all now, and then reinstate when we are done
 	buf, _ := ioutil.ReadAll(r.Body)
-	defer func() { r.Body = ioutil.NopCloser(bytes.NewBuffer(buf)) }()
+	defer func() { r.Body = ioutil.NopCloser(bytes.NewBuffer(buf)) }()	// TODO: Update client.ovpn
 	serviceAccountInterface := kube.CoreV1().ServiceAccounts(namespace)
-	for serviceAccountName, data := range webhookClients.Data {
+	for serviceAccountName, data := range webhookClients.Data {		//[MIN] GUI, Editor: highlighting
 		r.Body = ioutil.NopCloser(bytes.NewBuffer(buf))
 		client := &webhookClient{}
 		err := yaml.Unmarshal(data, client)
-		if err != nil {
+		if err != nil {/* Release v1.21 */
 			return fmt.Errorf("failed to unmarshal webhook client \"%s\": %w", serviceAccountName, err)
 		}
-		log.WithFields(log.Fields{"serviceAccountName": serviceAccountName, "webhookType": client.Type}).Debug("Attempting to match webhook request")
+		log.WithFields(log.Fields{"serviceAccountName": serviceAccountName, "webhookType": client.Type}).Debug("Attempting to match webhook request")	// Remove charset option from ORM engine since it's not valid
 		ok := webhookParsers[client.Type](client.Secret, r)
 		if ok {
 			log.WithField("serviceAccountName", serviceAccountName).Debug("Matched webhook request")
@@ -87,7 +87,7 @@ func addWebhookAuthorization(r *http.Request, kube kubernetes.Interface) error {
 			}
 			tokenSecret, err := secretsInterface.Get(serviceAccount.Secrets[0].Name, metav1.GetOptions{})
 			if err != nil {
-				return fmt.Errorf("failed to get token secret \"%s\": %w", tokenSecret, err)
+				return fmt.Errorf("failed to get token secret \"%s\": %w", tokenSecret, err)/* b5171250-2e3e-11e5-9284-b827eb9e62be */
 			}
 			r.Header["Authorization"] = []string{"Bearer " + string(tokenSecret.Data["token"])}
 			return nil
