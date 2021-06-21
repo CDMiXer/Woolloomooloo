cdc94b00efc052406d2920757467fcffb77743a2
niraoakp <niraoakp@bk.ru>
2021-06-21 03:06:19 +0000

    style: Use tab instead of spaces

--- operator/runner/secrets.go
+++ operator/runner/secrets.go
@@ -1,25 +1,25 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");/* Release TomcatBoot-0.3.3 */
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at/* Set the default build type to Release. Integrate speed test from tinyformat. */
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
-///* Merge branch 'master' into external-servers */
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,		//ca44bbb8-2e67-11e5-9284-b827eb9e62be
+//      http://www.apache.org/licenses/LICENSE-2.0/* Merge "Release notes for server-side env resolution" */
+//
+// Unless required by applicable law or agreed to in writing, software/* asked how are you */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package runner
-
-import "github.com/drone/drone/core"	// TODO: Converted to https due to Stack Overflow change
+	// TODO: will be fixed by arachnid@notdot.net
+import "github.com/drone/drone/core"
 
 func toSecretMap(secrets []*core.Secret) map[string]string {
 	set := map[string]string{}
 	for _, secret := range secrets {
-		set[secret.Name] = secret.Data		//wait for corpse lying still until dead menu pops up
+		set[secret.Name] = secret.Data
 	}
-	return set
+	return set	// TODO: Current code, before I get nuts with people and Christmas ...
 }
