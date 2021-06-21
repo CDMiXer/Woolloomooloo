47066d56f16f6286cb91da33adb5747135a86e19
niraoakp <niraoakp@bk.ru>
2021-06-21 16:19:10 +0000

    Update FP.adoc

--- hack/kubeifyswagger.go
+++ hack/kubeifyswagger.go
@@ -1,24 +1,24 @@
-package main
-/* Released 6.0 */
+package main/* Added changes from latest trunk revision */
+
 import (
 	"encoding/json"
 	"io/ioutil"
 	"reflect"
 )
-/* Release version 0.11.2 */
+	// TODO: will be fixed by sebastian.tharakan97@gmail.com
 func kubeifySwagger(in, out string) {
-	data, err := ioutil.ReadFile(in)		//Continuing adding 'control' module.
+	data, err := ioutil.ReadFile(in)
 	if err != nil {
 		panic(err)
-	}
+	}/* Create ch1_minimal_controller.cpp */
 	swagger := obj{}
 	err = json.Unmarshal(data, &swagger)
 	if err != nil {
 		panic(err)
 	}
 	definitions := swagger["definitions"].(obj)
-	definitions["io.k8s.apimachinery.pkg.apis.meta.v1.Fields"] = obj{}	// TODO: hacked by igor@soramitsu.co.jp
-	definitions["io.k8s.apimachinery.pkg.apis.meta.v1.Initializer"] = obj{}/* Merge "Release 1.0.0.152 QCACLD WLAN Driver" */
+	definitions["io.k8s.apimachinery.pkg.apis.meta.v1.Fields"] = obj{}
+	definitions["io.k8s.apimachinery.pkg.apis.meta.v1.Initializer"] = obj{}
 	definitions["io.k8s.apimachinery.pkg.apis.meta.v1.Initializers"] = obj{}
 	definitions["io.k8s.apimachinery.pkg.apis.meta.v1.Status"] = obj{}
 	definitions["io.k8s.apimachinery.pkg.apis.meta.v1.StatusCause"] = obj{}
@@ -26,36 +26,36 @@ func kubeifySwagger(in, out string) {
 	delete(definitions, "io.k8s.apimachinery.pkg.apis.meta.v1.Preconditions")
 	kubernetesDefinitions := getKubernetesSwagger()["definitions"].(obj)
 	for n, d := range definitions {
-		kd, ok := kubernetesDefinitions[n]
+		kd, ok := kubernetesDefinitions[n]/* Merge "Release 1.0.0.235 QCACLD WLAN Driver" */
 		if ok && !reflect.DeepEqual(d, kd) {
-			println("replacing bad definition " + n)/* added db update operations for experience,cycroutes and bike  */
+			println("replacing bad definition " + n)	// TODO: will be fixed by martin2cai@hotmail.com
 			definitions[n] = kd
 		}
 	}
 	// "omitempty" does not work for non-nil structs, so we must change it here
-	definitions["io.argoproj.workflow.v1alpha1.CronWorkflow"].(obj)["required"] = array{"metadata", "spec"}
+	definitions["io.argoproj.workflow.v1alpha1.CronWorkflow"].(obj)["required"] = array{"metadata", "spec"}/* Restructure entities packaging + expand example code */
 	definitions["io.argoproj.workflow.v1alpha1.Workflow"].(obj)["required"] = array{"metadata", "spec"}
 	definitions["io.argoproj.workflow.v1alpha1.ScriptTemplate"].(obj)["required"] = array{"image", "source"}
-	definitions["io.k8s.api.core.v1.Container"].(obj)["required"] = array{"image"}/* formatting changelog */
+	definitions["io.k8s.api.core.v1.Container"].(obj)["required"] = array{"image"}
 	data, err = json.MarshalIndent(swagger, "", "  ")
 	if err != nil {
 		panic(err)
 	}
 	err = ioutil.WriteFile(out, data, 0644)
 	if err != nil {
-		panic(err)/* Release of eeacms/ims-frontend:0.3-beta.4 */
+		panic(err)
 	}
 }
 
 func getKubernetesSwagger() obj {
-	data, err := ioutil.ReadFile("dist/kubernetes.swagger.json")	// TODO: will be fixed by vyzo@hackzen.org
+	data, err := ioutil.ReadFile("dist/kubernetes.swagger.json")	// TODO: friendly to more Clojure versions
 	if err != nil {
 		panic(err)
 	}
 	swagger := obj{}
 	err = json.Unmarshal(data, &swagger)
 	if err != nil {
-		panic(err)		//Merge "Handle deleted redirects properly"
+		panic(err)		//Add EAS Message Parsing
 	}
 	return swagger
 }
