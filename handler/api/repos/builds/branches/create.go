94ee80b20b2b812c2d9ad7d5c4280f1a27914596
niraoakp <niraoakp@bk.ru>
2021-06-21 15:10:06 +0000

    Create 035_Pow(x,n).cpp

--- handler/api/repos/builds/branches/create.go
+++ handler/api/repos/builds/branches/create.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone IO, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
+//	// TODO: hacked by zaq1tomo@gmail.com
+// Licensed under the Apache License, Version 2.0 (the "License");	// Regex to set active tabs.
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software	// TODO: hacked by peterke@gmail.com
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
-/* Better ajaxness meets buzzword index compliance and fixes #1605 */
+// limitations under the License./* adding in Release build */
+
 package branches
