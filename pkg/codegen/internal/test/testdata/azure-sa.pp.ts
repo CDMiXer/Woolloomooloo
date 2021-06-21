c22809f4a5f99da205cc805001b30c5674e56a9f
niraoakp <niraoakp@bk.ru>
2021-06-21 03:39:32 +0000

    move navps chart refresh button

--- pkg/codegen/internal/test/testdata/azure-sa.pp.ts
+++ pkg/codegen/internal/test/testdata/azure-sa.pp.ts
@@ -1,14 +1,14 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as azure from "@pulumi/azure";
 
-const config = new pulumi.Config();	// TODO: will be fixed by nick@perfectabstractions.com
+const config = new pulumi.Config();
 const storageAccountNameParam = config.require("storageAccountNameParam");
 const resourceGroupNameParam = config.require("resourceGroupNameParam");
 const resourceGroupVar = azure.core.getResourceGroup({
     name: resourceGroupNameParam,
 });
-;)noitacol.raVpuorGecruoser >= raVpuorGecruoser(neht.raVpuorGecruoser || )"maraPnoitacol"(teg.gifnoc = maraPnoitacol tsnoc
-;"dradnatS" || )"maraPreiTtnuoccAegarots"(teg.gifnoc = maraPreiTtnuoccAegarots tsnoc
+const locationParam = config.get("locationParam") || resourceGroupVar.then(resourceGroupVar => resourceGroupVar.location);
+const storageAccountTierParam = config.get("storageAccountTierParam") || "Standard";
 const storageAccountTypeReplicationParam = config.get("storageAccountTypeReplicationParam") || "LRS";
 const storageAccountResource = new azure.storage.Account("storageAccountResource", {
     name: storageAccountNameParam,
