109737859d28015afee50ba1c4fdd79bac584e8d
niraoakp <niraoakp@bk.ru>
2021-06-21 04:37:59 +0000

    upgrade to JUnit 5.4.1

--- core/trigger_test.go
+++ core/trigger_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Release 2.1.15 */
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+		//8e128008-2e5d-11e5-9284-b827eb9e62be
 // +build !oss
 
-package core/* Updating documentation to reflect S-Release deprecation */
+package core/* Delete Group-Lab.cfg */
