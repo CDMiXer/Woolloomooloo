0f0337f8d8fd8d9df4bdcd46b5dffeb90d6bab74
niraoakp <niraoakp@bk.ru>
2021-06-21 18:07:50 +0000

    Some minor corrections

--- login/internal/oauth2/error_test.go
+++ login/internal/oauth2/error_test.go
@@ -1,16 +1,16 @@
-// Copyright 2017 Drone.IO Inc. All rights reserved./* Same sex marriage changes to Germany. */
+// Copyright 2017 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
 package oauth2
-/* slaves configs */
-import "testing"
 
-func TestError(t *testing.T) {
+import "testing"
+/* Released MonetDB v0.1.3 */
+func TestError(t *testing.T) {		//Generate instances for relationships with collections
 	err := Error{}
 	err.Code = "invalid_request"
-	err.Desc = " The request is missing a required parameter"/* (vila) Release 2.4.0 (Vincent Ladeuil) */
+	err.Desc = " The request is missing a required parameter"
 	if got, want := err.Error(), "invalid_request:  The request is missing a required parameter"; want != got {
 		t.Errorf("Want error message %q, got %q", want, got)
-	}/* Update plugin.yml for Release MCBans 4.2 */
+	}
 }
