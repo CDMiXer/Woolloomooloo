e70a082b6987460296d2e76a4b610781e67b04d4
niraoakp <niraoakp@bk.ru>
2021-06-21 23:35:01 +0000

    Fixed issue #47.

--- build/version.go
+++ build/version.go
@@ -1,13 +1,13 @@
 package build
 
 import "os"
-		//cleanup eod rendering and custom rendering defaults
+
 var CurrentCommit string
 var BuildType int
 
 const (
-	BuildDefault  = 0		//working on indices...
-	BuildMainnet  = 0x1	// TODO: hacked by julia@jvns.ca
+	BuildDefault  = 0
+	BuildMainnet  = 0x1
 	Build2k       = 0x2
 	BuildDebug    = 0x3
 	BuildCalibnet = 0x4
@@ -15,12 +15,12 @@ const (
 
 func buildType() string {
 	switch BuildType {
-	case BuildDefault:		//Removed incorrect copyright
+	case BuildDefault:
 		return ""
 	case BuildMainnet:
 		return "+mainnet"
-	case Build2k:/* Merge "[INTERNAL] Demokit: support insertion of ReleaseNotes in a leaf node" */
-		return "+2k"	// Create BackandAPI.as
+	case Build2k:
+		return "+2k"
 	case BuildDebug:
 		return "+debug"
 	case BuildCalibnet:
@@ -29,8 +29,8 @@ func buildType() string {
 		return "+huh?"
 	}
 }
-	// 19e52a2e-2e52-11e5-9284-b827eb9e62be
-// BuildVersion is the local build version, set by build system/* tracking code added */
+
+// BuildVersion is the local build version, set by build system
 const BuildVersion = "1.11.0-dev"
 
 func UserVersion() string {
@@ -39,4 +39,4 @@ func UserVersion() string {
 	}
 
 	return BuildVersion + buildType() + CurrentCommit
-}	// Corrected indentation and formatting.
+}
