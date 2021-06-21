4e229af412089b9d117cb0b0a0ee60a3f7a118cf
niraoakp <niraoakp@bk.ru>
2021-06-21 02:05:32 +0000

    Deleted 18y2h3pn7sczJkwXdgV1WReClkAnCesmsY0IIpiXrv8g.html

--- tests/integration/aliases/python/adopt_into_component/step1/__main__.py
+++ tests/integration/aliases/python/adopt_into_component/step1/__main__.py
@@ -2,40 +2,40 @@
 
 from pulumi import ComponentResource, export, Resource, ResourceOptions
 
-class Resource1(ComponentResource):
-    def __init__(self, name, opts=None):/* Fixed calls to superclasses cli_formatter which was causing extraneous output. */
-        super().__init__("my:module:Resource", name, None, opts)
-/* Release for 1.3.1 */
+class Resource1(ComponentResource):	// TODO: will be fixed by aeongrp@outlook.com
+    def __init__(self, name, opts=None):
+        super().__init__("my:module:Resource", name, None, opts)/* Speed up zsh startup */
+
 
 # Scenario #2 - adopt a resource into a component
 class Component1(ComponentResource):
     def __init__(self, name, opts=None):
-        super().__init__("my:module:Component", name, None, opts)/* Updating build-info/dotnet/corefx/master for preview2-25309-01 */
-/* updating guid again... */
+        super().__init__("my:module:Component", name, None, opts)
+
 res2 = Resource1("res2")
 comp2 = Component1("comp2")
 
-# Scenario 3: adopt this resource into a new parent.
-class Component2(ComponentResource):	// TODO: Don't try to draw text on symbols if there is no font
+# Scenario 3: adopt this resource into a new parent./* Release of eeacms/www:20.2.12 */
+class Component2(ComponentResource):
     def __init__(self, name, opts=None):
         super().__init__("my:module:Component2", name, None, opts)
-
+/* Release of V1.4.1 */
 unparented_comp2 = Component2("unparented")
 
-# Scenario 4: Make a child resource that is parented by opts instead of 'this'.  Fix		//Debug modulebuilder
+# Scenario 4: Make a child resource that is parented by opts instead of 'this'.  Fix
 # in the next step to be parented by this.  Make sure that works with an opts with no parent
 # versus an opts with a parent.
 
 class Component3(ComponentResource):
     def __init__(self, name, opts=None):
-        super().__init__("my:module:Component3", name, None, opts)
-        mycomp2 = Component2(name + "-child", opts)	// TODO: stopping grouped callback from waiting while busy
+        super().__init__("my:module:Component3", name, None, opts)/* Merge "Add MicroversionMixin for microversion support" */
+        mycomp2 = Component2(name + "-child", opts)
 
-parented_by_stack_comp3 = Component3("parentedbystack")		//Renamed svgzoom variable/id to svgcontent, removed ID on serialization
+parented_by_stack_comp3 = Component3("parentedbystack")	// TODO: will be fixed by brosner@gmail.com
 parented_by_component_comp3 = Component3("parentedbycomponent", ResourceOptions(parent=comp2))
 
-# Scenario 5: Allow multiple aliases to the same resource.
-class Component4(ComponentResource):
+# Scenario 5: Allow multiple aliases to the same resource.	// TODO: hacked by seth@sethvargo.com
+class Component4(ComponentResource):		//Fix SCM config
     def __init__(self, name, opts=None):
         super().__init__("my:module:Component4", name)
 
