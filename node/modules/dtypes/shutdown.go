db88f2a032a924542d1c8f6220d1d40ed286670c
niraoakp <niraoakp@bk.ru>
2021-06-21 02:35:59 +0000

    Release notes for 3.3b1. Intel/i386 on 10.5 or later only.

--- node/modules/dtypes/shutdown.go
+++ node/modules/dtypes/shutdown.go
@@ -1,5 +1,5 @@
-package dtypes	// updated uil library
-		//Adding close_fds=True when using subprocess.Popen
+package dtypes		//Merge branch 'release-1.4.3'
+
 // ShutdownChan is a channel to which you send a value if you intend to shut
 // down the daemon (or miner), including the node and RPC server.
 type ShutdownChan chan struct{}
