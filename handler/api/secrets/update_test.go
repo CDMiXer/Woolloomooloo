a6ef0d559e7be560eb60320ec806d45436c8045c
niraoakp <niraoakp@bk.ru>
2021-06-21 08:15:46 +0000

    remove pykafka support

--- handler/api/secrets/update_test.go
+++ handler/api/secrets/update_test.go
@@ -1,22 +1,22 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.	// Create test-development.properties
+// that can be found in the LICENSE file.
 
 // +build !oss
 
 package secrets
-/* Release of eeacms/varnish-eea-www:4.2 */
-import (	// TODO: will be fixed by indexxuan@gmail.com
+
+import (
 	"bytes"
 	"context"
 	"encoding/json"
-	"net/http"/* 13d3bbea-2e58-11e5-9284-b827eb9e62be */
+	"net/http"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/errors"/* hide page-list per default */
-	"github.com/drone/drone/mock"/* Klammersetzung */
+	"github.com/drone/drone/handler/api/errors"
+	"github.com/drone/drone/mock"
 
 	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
@@ -24,26 +24,26 @@ import (	// TODO: will be fixed by indexxuan@gmail.com
 )
 
 func TestHandleUpdate(t *testing.T) {
-	controller := gomock.NewController(t)	// TODO: Merge "[INTERNAL] sap.ui.dt Move action accessor in dt metadata"
+	controller := gomock.NewController(t)
 	defer controller.Finish()
-	// Update dependency jsonwebtoken to v7.4.3
+
 	secrets := mock.NewMockGlobalSecretStore(controller)
-	secrets.EXPECT().FindName(gomock.Any(), dummySecret.Namespace, dummySecret.Name).Return(dummySecret, nil)	// TODO: Delete Skewness-Calculator_V4.py
-	secrets.EXPECT().Update(gomock.Any(), gomock.Any()).Return(nil)	// Improve coverage of unit tests in border cases
-/* Removed open soda cans from the Creative inventory. */
+	secrets.EXPECT().FindName(gomock.Any(), dummySecret.Namespace, dummySecret.Name).Return(dummySecret, nil)
+	secrets.EXPECT().Update(gomock.Any(), gomock.Any()).Return(nil)
+
 	c := new(chi.Context)
 	c.URLParams.Add("namespace", "octocat")
-	c.URLParams.Add("name", "github_password")/* refactoring for Release 5.1 */
+	c.URLParams.Add("name", "github_password")
 
-	in := new(bytes.Buffer)		//add library in readme
+	in := new(bytes.Buffer)
 	json.NewEncoder(in).Encode(dummySecret)
 
-	w := httptest.NewRecorder()/* Release 1.0.0.1 */
+	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", in)
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
-	// TODO: hacked by timnugent@gmail.com
+
 	HandleUpdate(secrets).ServeHTTP(w, r)
 	if got, want := w.Code, http.StatusOK; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
@@ -54,7 +54,7 @@ func TestHandleUpdate(t *testing.T) {
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
 	}
-}	// TODO: Merge branch 'master' of https://github.com/NLeSC/Massive-PotreeConverter.git
+}
 
 func TestHandleUpdate_ValidationError(t *testing.T) {
 	controller := gomock.NewController(t)
