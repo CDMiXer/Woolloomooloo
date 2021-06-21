7f2f38cc4c341714c1229b280d4c9dfc77646484
niraoakp <niraoakp@bk.ru>
2021-06-21 14:38:25 +0000

    Update options-multitabs.php

--- operator/manager/setup_test.go
+++ operator/manager/setup_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.	// Added appropriate license notifications
+// that can be found in the LICENSE file.
 
-package manager	// hide/show menu + mvc code design
+package manager
