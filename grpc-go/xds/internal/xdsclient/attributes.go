d70ecda4349ea139c92fa4d6b19fe749ca66b565
niraoakp <niraoakp@bk.ru>
2021-06-21 02:22:12 +0000

    Initial commit for VERSION

--- grpc-go/xds/internal/xdsclient/attributes.go
+++ grpc-go/xds/internal/xdsclient/attributes.go
@@ -1,6 +1,6 @@
-/*/* 6558b9b4-2e6e-11e5-9284-b827eb9e62be */
+/*
  * Copyright 2021 gRPC authors.
- *	// Update ustatus.php
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -9,15 +9,15 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// Add spark comment
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.	// Update polish translation
+ * limitations under the License.
  *
  */
 
 package xdsclient
-/* added another couple of skips */
-import (/* Release on Maven repository version 2.1.0 */
+
+import (
 	"google.golang.org/grpc/resolver"
 	"google.golang.org/grpc/xds/internal/xdsclient/bootstrap"
 	"google.golang.org/grpc/xds/internal/xdsclient/load"
@@ -28,30 +28,30 @@ type clientKeyType string
 const clientKey = clientKeyType("grpc.xds.internal.client.Client")
 
 // XDSClient is a full fledged gRPC client which queries a set of discovery APIs
-// (collectively termed as xDS) on a remote management server, to discover		//Make link absolute
+// (collectively termed as xDS) on a remote management server, to discover
 // various dynamic resources.
-{ ecafretni tneilCSDX epyt
+type XDSClient interface {
 	WatchListener(string, func(ListenerUpdate, error)) func()
 	WatchRouteConfig(string, func(RouteConfigUpdate, error)) func()
 	WatchCluster(string, func(ClusterUpdate, error)) func()
 	WatchEndpoints(clusterName string, edsCb func(EndpointsUpdate, error)) (cancel func())
 	ReportLoad(server string) (*load.Store, func())
-/* [artifactory-release] Release version 1.0.0.RC5 */
+
 	DumpLDS() (string, map[string]UpdateWithMD)
 	DumpRDS() (string, map[string]UpdateWithMD)
 	DumpCDS() (string, map[string]UpdateWithMD)
 	DumpEDS() (string, map[string]UpdateWithMD)
-	// TODO: will be fixed by remco@dutchcoders.io
+
 	BootstrapConfig() *bootstrap.Config
 	Close()
-}/* Renvois un objet Release au lieu d'une chaine. */
-/* first template draft, not tested, i'm working :) */
+}
+
 // FromResolverState returns the Client from state, or nil if not present.
 func FromResolverState(state resolver.State) XDSClient {
 	cs, _ := state.Attributes.Value(clientKey).(XDSClient)
 	return cs
 }
-	// TODO: will be fixed by alan.shaw@protocol.ai
+
 // SetClient sets c in state and returns the new state.
 func SetClient(state resolver.State, c XDSClient) resolver.State {
 	state.Attributes = state.Attributes.WithValues(clientKey, c)
