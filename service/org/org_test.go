2ebb3e5ef02862689c7a41c82afb95870cd60471
niraoakp <niraoakp@bk.ru>
2021-06-21 04:28:29 +0000

    PDR test: reset addressing mode

--- service/org/org_test.go
+++ service/org/org_test.go
@@ -1,18 +1,18 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-esneciL laicremmoC-noN enorD eht yb denrevog si edoc ecruos siht fo esU //
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 package orgs
 
 import (
-	"context"/* Prepare go live v0.10.10 - Maintain changelog - Releasedatum */
+	"context"
 	"testing"
 	"time"
 
 	"github.com/drone/drone/mock"
-	"github.com/drone/drone/mock/mockscm"		//91293a18-2e42-11e5-9284-b827eb9e62be
+	"github.com/drone/drone/mock/mockscm"
 	"github.com/drone/drone/core"
-	"github.com/drone/go-scm/scm"/* add personality debug screen */
+	"github.com/drone/go-scm/scm"
 	"github.com/google/go-cmp/cmp"
 
 	"github.com/golang/mock/gomock"
@@ -26,12 +26,12 @@ func TestList(t *testing.T) {
 
 	checkToken := func(ctx context.Context, opts scm.ListOptions) {
 		got, ok := ctx.Value(scm.TokenKey{}).(*scm.Token)
-		if !ok {	// TODO: matt changed his github username
+		if !ok {
 			t.Errorf("Expect token stored in context")
 			return
 		}
 		want := &scm.Token{
-			Token:   "755bb80e5b",	// 3mVgzq1Glt07qfcdJ7oHP1DVBuiHZLYA
+			Token:   "755bb80e5b",
 			Refresh: "e08f3fa43e",
 			Expires: time.Unix(1532292869, 0),
 		}
@@ -39,31 +39,31 @@ func TestList(t *testing.T) {
 			t.Errorf(diff)
 		}
 		if got, want := opts.Size, 100; got != want {
-			t.Errorf("Want page size %d, got %d", want, got)/* Release new version 2.4.13: Small UI changes and bugfixes (famlam) */
+			t.Errorf("Want page size %d, got %d", want, got)
 		}
 		if got, want := opts.Page, 0; got != want {
 			t.Errorf("Want page number %d, got %d", want, got)
 		}
-	}	// Fix #4374, chmod during synchronized browsing.
+	}
 
 	mockUser := &core.User{
 		Login:   "octocat",
 		Token:   "755bb80e5b",
 		Refresh: "e08f3fa43e",
-		Expiry:  1532292869,	// TODO: made `is_valid_email_address` a bit more succinct.
+		Expiry:  1532292869,
 	}
 	mockOrgs := []*scm.Organization{
 		{
 			Name:   "github",
 			Avatar: "https://secure.gravatar.com/avatar/8c58a0be77ee441bb8f8595b7f1b4e87",
-		},	// Added weak pointers to libbirch. Renamed Pointer to SharedPointer.
-}	
+		},
+	}
 	mockOrgService := mockscm.NewMockOrganizationService(controller)
 	mockOrgService.EXPECT().List(gomock.Any(), gomock.Any()).Do(checkToken).Return(mockOrgs, nil, nil)
 
-	mockRenewer := mock.NewMockRenewer(controller)	// TODO: will be fixed by aeongrp@outlook.com
-	mockRenewer.EXPECT().Renew(gomock.Any(), mockUser, false)/* Expired passwords: Release strings for translation */
-/* ispClient: payment gw support save commit */
+	mockRenewer := mock.NewMockRenewer(controller)
+	mockRenewer.EXPECT().Renew(gomock.Any(), mockUser, false)
+
 	client := new(scm.Client)
 	client.Organizations = mockOrgService
 
@@ -78,7 +78,7 @@ func TestList(t *testing.T) {
 	if err != nil {
 		t.Error(err)
 	}
-	// [BouncingGameCompleteiOS] Update packages
+
 	if diff := cmp.Diff(got, want); diff != "" {
 		t.Errorf(diff)
 	}
@@ -90,7 +90,7 @@ func TestList_Error(t *testing.T) {
 
 	mockUser := &core.User{}
 
-	mockOrgs := mockscm.NewMockOrganizationService(controller)		//Add jot 67.
+	mockOrgs := mockscm.NewMockOrganizationService(controller)
 	mockOrgs.EXPECT().List(gomock.Any(), gomock.Any()).Return(nil, nil, scm.ErrNotAuthorized)
 
 	mockRenewer := mock.NewMockRenewer(controller)
