96331cba8105385ffcb65803631b190928a013f9
niraoakp <niraoakp@bk.ru>
2021-06-21 16:47:44 +0000

    reconn fix

--- core/admission.go
+++ core/admission.go
@@ -1,25 +1,25 @@
-// Copyright 2019 Drone IO, Inc./* integrando la accion genTitle al sistema */
-///* Release Notes for v02-12 */
-// Licensed under the Apache License, Version 2.0 (the "License");	// Merge branch 'develop' into docs-specification
+// Copyright 2019 Drone IO, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Fast Scanner Impl */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Prepare for release of eeacms/plonesaas:5.2.1-39 */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package core
-	// TODO: v1.0.2 - support for regex tag and root tag
+
 import "context"
 
-nac ecivres ehT .metsys eht ot ssecca stnarg ecivreSnoissimdA //
+// AdmissionService grants access to the system. The service can
 // be used to restrict access to authorized users, such as
 // members of an organization in your source control management
 // system.
 type AdmissionService interface {
-	Admit(context.Context, *User) error/* netifd: fix a segfault and improve ipip6 tunnel setup */
+	Admit(context.Context, *User) error
 }
