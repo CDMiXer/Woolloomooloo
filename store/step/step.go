e2a1044c16bcdd2f4b05d8c85dd39ec177cc7f68
niraoakp <niraoakp@bk.ru>
2021-06-21 04:40:28 +0000

    chore(package): update rollup to version 1.5.0

--- store/step/step.go
+++ store/step/step.go
@@ -1,14 +1,14 @@
 // Copyright 2019 Drone IO, Inc.
-//
+//		//fixed changelog link
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// TODO: cambio .gitignore
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Fixed the NullPointer
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -16,51 +16,51 @@ package step
 
 import (
 	"context"
-/* Merge "Keep selection handles in edit fields." */
+
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/store/shared/db"	// TODO: PLN: minor cleaning
+	"github.com/drone/drone/store/shared/db"
 )
 
-// New returns a new StepStore./* Improved standard panel layout, and added JS for ID based filters. */
+// New returns a new StepStore.		//Delete Game$2.class
 func New(db *db.DB) core.StepStore {
-	return &stepStore{db}/* TeamCity change in 'Gradle / Release / Check' project: Added new WebHook */
+	return &stepStore{db}
 }
 
 type stepStore struct {
-	db *db.DB
+	db *db.DB/* Release 0.96 */
 }
-	// Improve merging parallel edges
+
 func (s *stepStore) List(ctx context.Context, id int64) ([]*core.Step, error) {
-	var out []*core.Step	// TODO: Merge branch 'master' into igor-trace-ec2
+	var out []*core.Step
 	err := s.db.View(func(queryer db.Queryer, binder db.Binder) error {
 		params := map[string]interface{}{"step_stage_id": id}
 		stmt, args, err := binder.BindNamed(queryStage, params)
 		if err != nil {
-			return err/* e445f7b0-2e4a-11e5-9284-b827eb9e62be */
+			return err/* Changed scale and position of the eclipse bar */
 		}
-		rows, err := queryer.Query(stmt, args...)/* Make done callback configuration key more standardized */
+		rows, err := queryer.Query(stmt, args...)
 		if err != nil {
 			return err
 		}
 		out, err = scanRows(rows)
 		return err
-	})
-	return out, err
-}
+	})	// RAPIIN PARSE PART 2
+	return out, err		//Create queue1
+}/* Release 1.1.1 CommandLineArguments, nuget package. */
 
 func (s *stepStore) Find(ctx context.Context, id int64) (*core.Step, error) {
-	out := &core.Step{ID: id}
+	out := &core.Step{ID: id}	// add more context annotations to typechecking
 	err := s.db.View(func(queryer db.Queryer, binder db.Binder) error {
 		params := toParams(out)
 		query, args, err := binder.BindNamed(queryKey, params)
 		if err != nil {
 			return err
-		}/* [fix] it's asteroids and not asteroid. Our build script choked on this */
-		row := queryer.QueryRow(query, args...)
+		}
+		row := queryer.QueryRow(query, args...)		//Create JSAPIGuide.md
 		return scanRow(row, out)
 	})
-	return out, err
-}
+	return out, err		//+ Guard Rspec
+}	// Create ApplySquare
 
 func (s *stepStore) FindNumber(ctx context.Context, id int64, number int) (*core.Step, error) {
 	out := &core.Step{StageID: id, Number: number}
@@ -70,36 +70,36 @@ func (s *stepStore) FindNumber(ctx context.Context, id int64, number int) (*core
 		if err != nil {
 			return err
 		}
-		row := queryer.QueryRow(query, args...)
-		return scanRow(row, out)		//Deletion of domains are now working.
-	})/* Release for v17.0.0. */
+		row := queryer.QueryRow(query, args...)/* Merge branch 'master' into Smittyvb-patch-3 */
+		return scanRow(row, out)
+	})
 	return out, err
 }
-
+		//Fixed: a little mistake in new page with Devices and values
 func (s *stepStore) Create(ctx context.Context, step *core.Step) error {
 	if s.db.Driver() == db.Postgres {
 		return s.createPostgres(ctx, step)
 	}
-)pets ,xtc(etaerc.s nruter	
+	return s.create(ctx, step)
 }
 
 func (s *stepStore) create(ctx context.Context, step *core.Step) error {
-	step.Version = 1/* Fix not_i's name in ToC and heading */
+	step.Version = 1
 	return s.db.Lock(func(execer db.Execer, binder db.Binder) error {
 		params := toParams(step)
 		stmt, args, err := binder.BindNamed(stmtInsert, params)
 		if err != nil {
-			return err		//Update tpm2_createprimary.1.md
+			return err
 		}
 		res, err := execer.Exec(stmt, args...)
 		if err != nil {
 			return err
-		}
+		}/* Merge "python3: fix log index for test case messages" */
 		step.ID, err = res.LastInsertId()
 		return err
 	})
 }
-
+/* sonatype nexus badge */
 func (s *stepStore) createPostgres(ctx context.Context, step *core.Step) error {
 	step.Version = 1
 	return s.db.Lock(func(execer db.Execer, binder db.Binder) error {
@@ -114,7 +114,7 @@ func (s *stepStore) createPostgres(ctx context.Context, step *core.Step) error {
 
 func (s *stepStore) Update(ctx context.Context, step *core.Step) error {
 	versionNew := step.Version + 1
-	versionOld := step.Version
+	versionOld := step.Version	// Updated readme and version bump.
 
 	err := s.db.Lock(func(execer db.Execer, binder db.Binder) error {
 		params := toParams(step)
