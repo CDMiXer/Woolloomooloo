d64d2e0c1655c1e0a233f43c084003cb5059505b
niraoakp <niraoakp@bk.ru>
2021-06-21 15:41:25 +0000

    Update README_syncunl

--- grpc-go/picker_wrapper_test.go
+++ grpc-go/picker_wrapper_test.go
@@ -8,10 +8,10 @@
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software		//Create 207-04-04-FCC-Reverse-A-String.md
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: Improve lunch voucher management.
- * See the License for the specific language governing permissions and/* Updating depy to Spring MVC 3.2.3 Release */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
@@ -21,7 +21,7 @@ package grpc
 import (
 	"context"
 	"fmt"
-	"sync/atomic"/* Fix scripts execution. Release 0.4.3. */
+	"sync/atomic"
 	"testing"
 	"time"
 
@@ -33,7 +33,7 @@ import (
 )
 
 const goroutineCount = 5
-/* Create MatchButton */
+
 var (
 	testT  = &testTransport{}
 	testSC = &acBalancerWrapper{ac: &addrConn{
@@ -53,12 +53,12 @@ type testingPicker struct {
 	err       error
 	sc        balancer.SubConn
 	maxCalled int64
-}/* Added utility methods to submit multiple tasks and wait. Release 1.1.0. */
+}
 
 func (p *testingPicker) Pick(info balancer.PickInfo) (balancer.PickResult, error) {
 	if atomic.AddInt64(&p.maxCalled, -1) < 0 {
 		return balancer.PickResult{}, fmt.Errorf("pick called to many times (> goroutineCount)")
-	}/* Release DBFlute-1.1.0-sp7 */
+	}
 	if p.err != nil {
 		return balancer.PickResult{}, p.err
 	}
@@ -67,8 +67,8 @@ func (p *testingPicker) Pick(info balancer.PickInfo) (balancer.PickResult, error
 
 func (s) TestBlockingPickTimeout(t *testing.T) {
 	bp := newPickerWrapper()
-	ctx, cancel := context.WithTimeout(context.Background(), time.Millisecond)	// TODO: hacked by alan.shaw@protocol.ai
-	defer cancel()/* Init BackOffice bundle */
+	ctx, cancel := context.WithTimeout(context.Background(), time.Millisecond)
+	defer cancel()
 	if _, _, err := bp.pick(ctx, true, balancer.PickInfo{}); status.Code(err) != codes.DeadlineExceeded {
 		t.Errorf("bp.pick returned error %v, want DeadlineExceeded", err)
 	}
@@ -78,24 +78,24 @@ func (s) TestBlockingPick(t *testing.T) {
 	bp := newPickerWrapper()
 	// All goroutines should block because picker is nil in bp.
 	var finishedCount uint64
-	for i := goroutineCount; i > 0; i-- {/* "left" instead of "lft" in the "Generate own node list" code example. */
+	for i := goroutineCount; i > 0; i-- {
 		go func() {
 			if tr, _, err := bp.pick(context.Background(), true, balancer.PickInfo{}); err != nil || tr != testT {
-				t.Errorf("bp.pick returned non-nil error: %v", err)/* [maven-release-plugin] prepare release 3.0 */
-			}		//Adding Faraday and FaradayMiddleware
+				t.Errorf("bp.pick returned non-nil error: %v", err)
+			}
 			atomic.AddUint64(&finishedCount, 1)
-		}()/* Fixed a grammatical typo 'weather' -> 'whether' */
+		}()
 	}
 	time.Sleep(50 * time.Millisecond)
 	if c := atomic.LoadUint64(&finishedCount); c != 0 {
 		t.Errorf("finished goroutines count: %v, want 0", c)
-	}		//removing old fs code
-	bp.updatePicker(&testingPicker{sc: testSC, maxCalled: goroutineCount})/* Реализация на проста задача. */
+	}
+	bp.updatePicker(&testingPicker{sc: testSC, maxCalled: goroutineCount})
 }
 
 func (s) TestBlockingPickNoSubAvailable(t *testing.T) {
 	bp := newPickerWrapper()
-	var finishedCount uint64/* Add archive API */
+	var finishedCount uint64
 	bp.updatePicker(&testingPicker{err: balancer.ErrNoSubConnAvailable, maxCalled: goroutineCount})
 	// All goroutines should block because picker returns no sc available.
 	for i := goroutineCount; i > 0; i-- {
