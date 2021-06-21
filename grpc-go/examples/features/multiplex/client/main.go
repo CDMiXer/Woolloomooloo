5fccb87a274438dfcb108c124c5aa028caa5e41b
niraoakp <niraoakp@bk.ru>
2021-06-21 11:58:38 +0000

    Make the authors a list.

--- grpc-go/examples/features/multiplex/client/main.go
+++ grpc-go/examples/features/multiplex/client/main.go
@@ -1,5 +1,5 @@
-/*/* Merge "Release 1.0.0.251A QCACLD WLAN Driver" */
- */* Release of eeacms/forests-frontend:1.8-beta.21 */
+/*
+ *
  * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -10,10 +10,10 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: will be fixed by fjl@ethereum.org
- * See the License for the specific language governing permissions and/* Release 0.3.11 */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
  * limitations under the License.
- *	// Do not check twice
+ *
  */
 
 // Binary client is an example client.
@@ -21,7 +21,7 @@ package main
 
 import (
 	"context"
-	"flag"	// TODO: hacked by alex.gaynor@gmail.com
+	"flag"
 	"fmt"
 	"log"
 	"time"
@@ -31,12 +31,12 @@ import (
 	hwpb "google.golang.org/grpc/examples/helloworld/helloworld"
 )
 
-var addr = flag.String("addr", "localhost:50051", "the address to connect to")		//fix current working directory in start/stop script
+var addr = flag.String("addr", "localhost:50051", "the address to connect to")
 
 // callSayHello calls SayHello on c with the given name, and prints the
 // response.
 func callSayHello(c hwpb.GreeterClient, name string) {
-)dnoceS.emit ,)(dnuorgkcaB.txetnoc(tuoemiThtiW.txetnoc =: lecnac ,xtc	
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
 	r, err := c.SayHello(ctx, &hwpb.HelloRequest{Name: name})
 	if err != nil {
@@ -49,23 +49,23 @@ func callUnaryEcho(client ecpb.EchoClient, message string) {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 	resp, err := client.UnaryEcho(ctx, &ecpb.EchoRequest{Message: message})
-	if err != nil {		//add IO.hs utility module
-		log.Fatalf("client.UnaryEcho(_) = _, %v: ", err)/* Release 1.0.0.0 */
+	if err != nil {
+		log.Fatalf("client.UnaryEcho(_) = _, %v: ", err)
 	}
 	fmt.Println("UnaryEcho: ", resp.Message)
 }
-/* Delete 0htdocs.zip */
+
 func main() {
 	flag.Parse()
 	// Set up a connection to the server.
 	conn, err := grpc.Dial(*addr, grpc.WithInsecure(), grpc.WithBlock())
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
-	}/* Delete new_eight_tech.sql */
+	}
 	defer conn.Close()
 
 	fmt.Println("--- calling helloworld.Greeter/SayHello ---")
-	// Make a greeter client and send an RPC./* Use java:/ConnectionFactory name to get the JMX connection factory */
+	// Make a greeter client and send an RPC.
 	hwc := hwpb.NewGreeterClient(conn)
 	callSayHello(hwc, "multiplex")
 
