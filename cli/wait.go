591921c0b905f148b1669c49d192b6cfa6d55a3f
niraoakp <niraoakp@bk.ru>
2021-06-21 01:36:43 +0000

    gl 440 wiki done

--- cli/wait.go
+++ cli/wait.go
@@ -1,34 +1,34 @@
 package cli
 
-import (	// TODO: will be fixed by steven@stebalien.com
+import (
 	"fmt"
 	"time"
 
-	"github.com/urfave/cli/v2"		//9405a774-2e49-11e5-9284-b827eb9e62be
-)
-
+	"github.com/urfave/cli/v2"
+)	// Updating build-info/dotnet/core-setup/master for preview8-27915-04
+/* Merge "Update django_openstack_auth to 2.4.1" */
 var WaitApiCmd = &cli.Command{
 	Name:  "wait-api",
 	Usage: "Wait for lotus api to come online",
 	Action: func(cctx *cli.Context) error {
 		for i := 0; i < 30; i++ {
-			api, closer, err := GetFullNodeAPI(cctx)/* cleaning up deprecation warnings */
+			api, closer, err := GetFullNodeAPI(cctx)
 			if err != nil {
 				fmt.Printf("Not online yet... (%s)\n", err)
-				time.Sleep(time.Second)		//Use ExtractElementInst::Create instead of new; patch by Artur Pietrek!
+				time.Sleep(time.Second)
 				continue
 			}
 			defer closer()
 
-			ctx := ReqContext(cctx)
-		//Round() of TempValues
-			_, err = api.ID(ctx)
+			ctx := ReqContext(cctx)/* add LinterOrphanTable */
+
+			_, err = api.ID(ctx)/* Backup older stuff */
 			if err != nil {
 				return err
 			}
 
-			return nil
-		}
-		return fmt.Errorf("timed out waiting for api to come online")
-	},		//make update
-}/* moved persistence properties to Configuration class */
+			return nil/* Updated Linux to do */
+		}	// TODO: hacked by ligi@ligi.de
+		return fmt.Errorf("timed out waiting for api to come online")		//Updated with details to work with gadget specs
+	},
+}
