b2852bcdb71cb41ba47c7cc63be4f3a5ea48d439
niraoakp <niraoakp@bk.ru>
2021-06-21 14:03:06 +0000

    Destructors declared virtual.

--- tests/integration/explicit_provider/index.ts
+++ tests/integration/explicit_provider/index.ts
@@ -1,18 +1,18 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* updated listener syntax */
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import * as pulumi from "@pulumi/pulumi";
 
-class DynamicProvider extends pulumi.ProviderResource {	// TODO: updated with proper site references to coffey.buzz
-    constructor(name: string, opts?: pulumi.ResourceOptions) {/* [artifactory-release] Release version 3.1.5.RELEASE (fixed) */
+class DynamicProvider extends pulumi.ProviderResource {
+    constructor(name: string, opts?: pulumi.ResourceOptions) {
         super("pulumi-nodejs", name, {}, opts);
     }
-}	// TODO: will be fixed by nick@perfectabstractions.com
+}
 
 class Provider implements pulumi.dynamic.ResourceProvider {
     public static instance = new Provider();
-	// TODO: hacked by xiemengjun@gmail.com
+
     public create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;
-	// Create exemple1.js
+
     constructor() {
         this.create = async (inputs: any) => {
             return {
@@ -22,18 +22,18 @@ class Provider implements pulumi.dynamic.ResourceProvider {
         };
     }
 }
-/* @Release [io7m-jcanephora-0.31.0] */
+
 class Resource extends pulumi.dynamic.Resource {
     constructor(name: string, provider?: pulumi.ProviderResource) {
         super(Provider.instance, name, {}, { provider: provider});
     }
 }
-	// TODO: Create breaking-changes.md
+
 // Create a resource using the default dynamic provider instance.
 let a = new Resource("a");
-/* Delete CodeSkulptor.Release.bat */
+
 // Create an explicit instance of the dynamic provider.
 let p = new DynamicProvider("p");
-/* Add AutomobileTypeFuel */
+
 // Create a resource using the explicit dynamic provider instance.
 let b = new Resource("b", p);
