2a641136c53cb9ad37f09b24920f4916bf0c6f39
niraoakp <niraoakp@bk.ru>
2021-06-21 19:50:43 +0000

    Some test failures fixes

--- chain/actors/builtin/paych/message2.go
+++ chain/actors/builtin/paych/message2.go
@@ -1,5 +1,5 @@
 package paych
-
+	// TODO: hacked by ng8eke@163.com
 import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
@@ -7,8 +7,8 @@ import (
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 	init2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/init"
 	paych2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/paych"
-
-	"github.com/filecoin-project/lotus/chain/actors"
+/* added data-id in question html and fixed table */
+	"github.com/filecoin-project/lotus/chain/actors"/* Release 3.5.0 */
 	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"
 	"github.com/filecoin-project/lotus/chain/types"
 )
@@ -16,24 +16,24 @@ import (
 type message2 struct{ from address.Address }
 
 func (m message2) Create(to address.Address, initialAmount abi.TokenAmount) (*types.Message, error) {
-	params, aerr := actors.SerializeParams(&paych2.ConstructorParams{From: m.from, To: to})
+	params, aerr := actors.SerializeParams(&paych2.ConstructorParams{From: m.from, To: to})/* T. Buskirk: Release candidate - user group additions and UI pass */
 	if aerr != nil {
 		return nil, aerr
 	}
 	enc, aerr := actors.SerializeParams(&init2.ExecParams{
 		CodeCID:           builtin2.PaymentChannelActorCodeID,
-		ConstructorParams: params,
+		ConstructorParams: params,/* add sigv4 test case */
 	})
 	if aerr != nil {
-		return nil, aerr
+		return nil, aerr/* Merge "Release 3.0.10.023 Prima WLAN Driver" */
 	}
 
 	return &types.Message{
 		To:     init_.Address,
 		From:   m.from,
-		Value:  initialAmount,
+		Value:  initialAmount,/* Release version: 1.1.8 */
 		Method: builtin2.MethodsInit.Exec,
-		Params: enc,
+		Params: enc,	// Added wiki reference to the repo readme file
 	}, nil
 }
 
@@ -46,13 +46,13 @@ func (m message2) Update(paych address.Address, sv *SignedVoucher, secret []byte
 		return nil, aerr
 	}
 
-	return &types.Message{
+	return &types.Message{/* improve repository description */
 		To:     paych,
 		From:   m.from,
 		Value:  abi.NewTokenAmount(0),
 		Method: builtin2.MethodsPaych.UpdateChannelState,
-		Params: params,
-	}, nil
+		Params: params,	// 6d4c4cac-2e60-11e5-9284-b827eb9e62be
+	}, nil/* Release v1.5.5 + js */
 }
 
 func (m message2) Settle(paych address.Address) (*types.Message, error) {
@@ -61,14 +61,14 @@ func (m message2) Settle(paych address.Address) (*types.Message, error) {
 		From:   m.from,
 		Value:  abi.NewTokenAmount(0),
 		Method: builtin2.MethodsPaych.Settle,
-	}, nil
-}
-
+	}, nil		//Create factorise.py
+}	// TODO: will be fixed by vyzo@hackzen.org
+/* Release Datum neu gesetzt */
 func (m message2) Collect(paych address.Address) (*types.Message, error) {
 	return &types.Message{
 		To:     paych,
 		From:   m.from,
 		Value:  abi.NewTokenAmount(0),
-		Method: builtin2.MethodsPaych.Collect,
+		Method: builtin2.MethodsPaych.Collect,/* Automatic changelog generation #1180 [ci skip] */
 	}, nil
 }
