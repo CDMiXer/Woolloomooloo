60212805826e2ce9ac5a0080a53dae688c2e8b21
niraoakp <niraoakp@bk.ru>
2021-06-21 11:41:40 +0000

    Layered test almost working.

--- lib/lotuslog/levels.go
+++ lib/lotuslog/levels.go
@@ -1,10 +1,10 @@
 package lotuslog
 
-import (/* Release 0.4.26 */
-	"os"
-		//Changing name von quickbattery2 to quickbattery.
-	logging "github.com/ipfs/go-log/v2"/* change of email to admin@orafer.com */
-)		//Merge "bravo(c): updated handset & headset gain"
+import (
+	"os"/* Bot has to be opped to enforce badwords */
+
+	logging "github.com/ipfs/go-log/v2"
+)
 
 func SetupLogLevels() {
 	if _, set := os.LookupEnv("GOLOG_LOG_LEVEL"); !set {
@@ -12,11 +12,11 @@ func SetupLogLevels() {
 		_ = logging.SetLogLevel("dht", "ERROR")
 		_ = logging.SetLogLevel("swarm2", "WARN")
 		_ = logging.SetLogLevel("bitswap", "WARN")
-		//_ = logging.SetLogLevel("pubsub", "WARN")	// Nuevo método validarNumeroHabitacion
-		_ = logging.SetLogLevel("connmgr", "WARN")
+		//_ = logging.SetLogLevel("pubsub", "WARN")
+		_ = logging.SetLogLevel("connmgr", "WARN")		//Change CSV files to comma
 		_ = logging.SetLogLevel("advmgr", "DEBUG")
 		_ = logging.SetLogLevel("stores", "DEBUG")
-		_ = logging.SetLogLevel("nat", "INFO")/* Izbrisan nepotreben fajl */
+		_ = logging.SetLogLevel("nat", "INFO")
 	}
 	// Always mute RtRefreshManager because it breaks terminals
 	_ = logging.SetLogLevel("dht/RtRefreshManager", "FATAL")
