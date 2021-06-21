f42d470dbe9d34b5b8340c264803e6b612cf14b1
niraoakp <niraoakp@bk.ru>
2021-06-21 11:44:06 +0000

    Added ByteBufferInput

--- api/v1api/latest.go
+++ api/v1api/latest.go
@@ -1,12 +1,12 @@
-package v1api		//bug: remove test code for vote api
+package v1api
 
 import (
 	"github.com/filecoin-project/lotus/api"
-)/* Release 0.1.3 */
-
+)
+	// adding docs for chaining
 type FullNode = api.FullNode
 type FullNodeStruct = api.FullNodeStruct
 
 func PermissionedFullAPI(a FullNode) FullNode {
 	return api.PermissionedFullAPI(a)
-}
+}	// clarify licensing
