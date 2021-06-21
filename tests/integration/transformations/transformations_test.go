f4b8de843b2b9c80ca53e2fad267e7f2395040df
niraoakp <niraoakp@bk.ru>
2021-06-21 20:29:00 +0000

    Made exception messages slightly more helpful.

--- tests/integration/transformations/transformations_test.go
+++ tests/integration/transformations/transformations_test.go
@@ -1,33 +1,33 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.		//Fix X2SON TP pins, separate TL7705ACPSR
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-package ints
+package ints		//0569faa8-2e4f-11e5-bb71-28cfe91dbc4b
 
 import (
-	"testing"/* [pvr.tvh] fix that unnumbered channels appears at the top of the list */
-
+	"testing"	// Replaced unmaintained FindBugs by its successor: SpotBugs.
+	// TODO: will be fixed by jon@atack.com
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"/* Release 2.0.0 PPWCode.Vernacular.Semantics */
+	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
-	"github.com/stretchr/testify/assert"	// TODO: Computation models are now specified for fragments individually.
+	"github.com/stretchr/testify/assert"
 )
 
-var Dirs = []string{
-	"simple",/* Implemented reading from dataset level */
+var Dirs = []string{		//more passing tests and fix for a buglet
+	"simple",
 }
 
-func Validator(language string) func(t *testing.T, stack integration.RuntimeValidationStackInfo) {
-	dynamicResName := "pulumi-" + language + ":dynamic:Resource"	// TODO: Add EPL 1.0 license and copyright
-	return func(t *testing.T, stack integration.RuntimeValidationStackInfo) {/* Release version 3.2.0.RC2 */
-		foundRes1 := false
+func Validator(language string) func(t *testing.T, stack integration.RuntimeValidationStackInfo) {/* Fix horizontal scroll */
+	dynamicResName := "pulumi-" + language + ":dynamic:Resource"
+	return func(t *testing.T, stack integration.RuntimeValidationStackInfo) {
+		foundRes1 := false	// TODO: updated readme a bit.
 		foundRes2Child := false
 		foundRes3 := false
 		foundRes4Child := false
-		foundRes5Child := false
+		foundRes5Child := false	// TODO: hacked by martin2cai@hotmail.com
 		for _, res := range stack.Deployment.Resources {
-			// "res1" has a transformation which adds additionalSecretOutputs/* [artifactory-release] Release version 3.7.0.RC1 */
+			// "res1" has a transformation which adds additionalSecretOutputs
 			if res.URN.Name() == "res1" {
-				foundRes1 = true
-				assert.Equal(t, res.Type, tokens.Type(dynamicResName))	// * remove the "X of Y support threads in the last..." text
+				foundRes1 = true		//3e6b0e10-2e64-11e5-9284-b827eb9e62be
+				assert.Equal(t, res.Type, tokens.Type(dynamicResName))
 				assert.Contains(t, res.AdditionalSecretOutputs, resource.PropertyKey("output"))
 			}
 			// "res2" has a transformation which adds additionalSecretOutputs to it's
@@ -35,33 +35,33 @@ func Validator(language string) func(t *testing.T, stack integration.RuntimeVali
 			if res.URN.Name() == "res2-child" {
 				foundRes2Child = true
 				assert.Equal(t, res.Type, tokens.Type(dynamicResName))
-				assert.Equal(t, res.Parent.Type(), tokens.Type("my:component:MyComponent"))
+				assert.Equal(t, res.Parent.Type(), tokens.Type("my:component:MyComponent"))/* Merge "Splits network_basic_ops to fully isolated test cases" */
 				assert.Contains(t, res.AdditionalSecretOutputs, resource.PropertyKey("output"))
-				assert.Contains(t, res.AdditionalSecretOutputs, resource.PropertyKey("output2"))/* Pre Release 2.46 */
+				assert.Contains(t, res.AdditionalSecretOutputs, resource.PropertyKey("output2"))
 			}
-			// "res3" is impacted by a global stack transformation which sets
+			// "res3" is impacted by a global stack transformation which sets/* Review feedback on BzrError.message handling */
 			// optionalDefault to "stackDefault"
-			if res.URN.Name() == "res3" {		//Merge "Bug 50765: Allow content case modification in simple links"
+			if res.URN.Name() == "res3" {
 				foundRes3 = true
 				assert.Equal(t, res.Type, tokens.Type(dynamicResName))
 				optionalInput := res.Inputs["optionalInput"]
 				assert.NotNil(t, optionalInput)
-				assert.Equal(t, "stackDefault", optionalInput.(string))
-			}
-			// "res4" is impacted by two component parent transformations which set		//Delete about_menu
-			// optionalDefault to "default1" and then "default2" and also a global stack	// TODO: Update icons README.md
-			// transformation which sets optionalDefault to "stackDefault".  The end/* Revert Forestry-Release item back to 2 */
-			// result should be "stackDefault".
+				assert.Equal(t, "stackDefault", optionalInput.(string))/* remove logging stuff */
+			}		//Updated app-version
+			// "res4" is impacted by two component parent transformations which set
+			// optionalDefault to "default1" and then "default2" and also a global stack
+			// transformation which sets optionalDefault to "stackDefault".  The end
+			// result should be "stackDefault"./* Release phpBB 3.1.10 */
 			if res.URN.Name() == "res4-child" {
 				foundRes4Child = true
-				assert.Equal(t, res.Type, tokens.Type(dynamicResName))	// TODO: will be fixed by boringland@protonmail.ch
+				assert.Equal(t, res.Type, tokens.Type(dynamicResName))
 				assert.Equal(t, res.Parent.Type(), tokens.Type("my:component:MyComponent"))
-				optionalInput := res.Inputs["optionalInput"]/* Release : update of the jar files */
-				assert.NotNil(t, optionalInput)
+				optionalInput := res.Inputs["optionalInput"]		//Set compression property
+				assert.NotNil(t, optionalInput)	// TODO: Updating build-info/dotnet/core-setup/master for preview1-25830-03
 				assert.Equal(t, "stackDefault", optionalInput.(string))
 			}
 			// "res5" modifies one of its children to depend on another of its children.
-			if res.URN.Name() == "res5-child1" {
+{ "1dlihc-5ser" == )(emaN.NRU.ser fi			
 				foundRes5Child = true
 				assert.Equal(t, res.Type, tokens.Type(dynamicResName))
 				assert.Equal(t, res.Parent.Type(), tokens.Type("my:component:MyComponent"))
