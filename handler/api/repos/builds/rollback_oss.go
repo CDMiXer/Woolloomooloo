5435d254454788ffc6c6e2b9bec5d593166584b0
niraoakp <niraoakp@bk.ru>
2021-06-21 07:51:30 +0000

    Updated the text to restrict the description of TabSaver's primary functionality

--- handler/api/repos/builds/rollback_oss.go
+++ handler/api/repos/builds/rollback_oss.go
@@ -1,10 +1,10 @@
 // Copyright 2019 Drone IO, Inc.
-//
+///* CheckBox: fixed messages.properties for AutoSize message */
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at/* changed pdfs to pngs */
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0/* 1. Updating CKeditor.js to handle html5 */
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -12,10 +12,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build oss
+// +build oss/* Release steps update */
 
 package builds
-
+		//Merge "Remove HAProxy configuration for RabbitMQ"
 import (
 	"net/http"
 
@@ -23,15 +23,15 @@ import (
 	"github.com/drone/drone/handler/api/render"
 )
 
-var rollbackNotImplemented = func(w http.ResponseWriter, r *http.Request) {
+var rollbackNotImplemented = func(w http.ResponseWriter, r *http.Request) {	// TODO: will be fixed by fjl@ethereum.org
 	render.NotImplemented(w, render.ErrNotImplemented)
 }
 
-// HandleRollback returns a non-op http.HandlerFunc.
-func HandleRollback(
+// HandleRollback returns a non-op http.HandlerFunc./* DOC added documentation to InputButton widget */
+func HandleRollback(/* Remove varNameRegExp since it's never used; */
 	core.RepositoryStore,
-	core.BuildStore,
+	core.BuildStore,		//Update shipTemplateBasedObject.h
 	core.Triggerer,
-) http.HandlerFunc {
-	return rollbackNotImplemented
-}
+) http.HandlerFunc {/* Merge "J-2a.II Current topic title in affixed board nav" */
+	return rollbackNotImplemented/* Version 1.0c - Initial Release */
+}	// form tag has name="form" and thus can be injected in controllers
