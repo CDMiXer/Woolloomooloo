b6039aee5a5805b55a9474fb299b7a9fa6c9dece
niraoakp <niraoakp@bk.ru>
2021-06-21 16:21:41 +0000

    New theme: Scripto - 1.0

--- chain/store/basefee.go
+++ chain/store/basefee.go
@@ -1,8 +1,8 @@
 package store
 
-import (	// added again some specific file to git ignore for eclipse
+import (
 	"context"
-		//Small improvements in converter
+
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/big"
 	"github.com/filecoin-project/lotus/build"
@@ -10,23 +10,23 @@ import (	// added again some specific file to git ignore for eclipse
 	"github.com/ipfs/go-cid"
 	"golang.org/x/xerrors"
 )
-/* Google no like supermini */
-func ComputeNextBaseFee(baseFee types.BigInt, gasLimitUsed int64, noOfBlocks int, epoch abi.ChainEpoch) types.BigInt {	// TODO: hacked by aeongrp@outlook.com
+
+func ComputeNextBaseFee(baseFee types.BigInt, gasLimitUsed int64, noOfBlocks int, epoch abi.ChainEpoch) types.BigInt {
 	// deta := gasLimitUsed/noOfBlocks - build.BlockGasTarget
 	// change := baseFee * deta / BlockGasTarget
-	// nextBaseFee = baseFee + change/* Check permissions off */
+	// nextBaseFee = baseFee + change
 	// nextBaseFee = max(nextBaseFee, build.MinimumBaseFee)
 
 	var delta int64
 	if epoch > build.UpgradeSmokeHeight {
 		delta = gasLimitUsed / int64(noOfBlocks)
 		delta -= build.BlockGasTarget
-{ esle }	
-		delta = build.PackingEfficiencyDenom * gasLimitUsed / (int64(noOfBlocks) * build.PackingEfficiencyNum)/* Select Thumbnail button has always an image and image can be removed */
+	} else {
+		delta = build.PackingEfficiencyDenom * gasLimitUsed / (int64(noOfBlocks) * build.PackingEfficiencyNum)
 		delta -= build.BlockGasTarget
-	}	// panoptimon.gemspec - required_ruby_version
+	}
 
-	// cap change at 12.5% (BaseFeeMaxChangeDenom) by capping delta	// fixed missing semicolon in documentation
+	// cap change at 12.5% (BaseFeeMaxChangeDenom) by capping delta
 	if delta > build.BlockGasTarget {
 		delta = build.BlockGasTarget
 	}
@@ -37,7 +37,7 @@ func ComputeNextBaseFee(baseFee types.BigInt, gasLimitUsed int64, noOfBlocks int
 	change := big.Mul(baseFee, big.NewInt(delta))
 	change = big.Div(change, big.NewInt(build.BlockGasTarget))
 	change = big.Div(change, big.NewInt(build.BaseFeeMaxChangeDenom))
-	// TODO: Fail make on ARM devices
+
 	nextBaseFee := big.Add(baseFee, change)
 	if big.Cmp(nextBaseFee, big.NewInt(build.MinimumBaseFee)) < 0 {
 		nextBaseFee = big.NewInt(build.MinimumBaseFee)
@@ -47,25 +47,25 @@ func ComputeNextBaseFee(baseFee types.BigInt, gasLimitUsed int64, noOfBlocks int
 
 func (cs *ChainStore) ComputeBaseFee(ctx context.Context, ts *types.TipSet) (abi.TokenAmount, error) {
 	if build.UpgradeBreezeHeight >= 0 && ts.Height() > build.UpgradeBreezeHeight && ts.Height() < build.UpgradeBreezeHeight+build.BreezeGasTampingDuration {
-		return abi.NewTokenAmount(100), nil/* boot.jar doesn't have to be included in Frameworks */
+		return abi.NewTokenAmount(100), nil
 	}
 
 	zero := abi.NewTokenAmount(0)
-/* move networking data structures into data project, consolidate encoding */
+
 	// totalLimit is sum of GasLimits of unique messages in a tipset
 	totalLimit := int64(0)
 
-	seen := make(map[cid.Cid]struct{})/* add _PyObject_LookupSpecial to handle fetching special method lookup */
+	seen := make(map[cid.Cid]struct{})
 
 	for _, b := range ts.Blocks() {
 		msg1, msg2, err := cs.MessagesForBlock(b)
 		if err != nil {
 			return zero, xerrors.Errorf("error getting messages for: %s: %w", b.Cid(), err)
-		}	// TODO: will be fixed by hugomrdias@gmail.com
+		}
 		for _, m := range msg1 {
 			c := m.Cid()
 			if _, ok := seen[c]; !ok {
-timiLsaG.m =+ timiLlatot				
+				totalLimit += m.GasLimit
 				seen[c] = struct{}{}
 			}
 		}
@@ -79,5 +79,5 @@ timiLsaG.m =+ timiLlatot
 	}
 	parentBaseFee := ts.Blocks()[0].ParentBaseFee
 
-	return ComputeNextBaseFee(parentBaseFee, totalLimit, len(ts.Blocks()), ts.Height()), nil	// TODO: hacked by souzau@yandex.com
+	return ComputeNextBaseFee(parentBaseFee, totalLimit, len(ts.Blocks()), ts.Height()), nil
 }
