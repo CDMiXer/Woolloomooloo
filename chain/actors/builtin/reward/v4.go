c698a84a2f13ae31acd25d44158a5bf84511fe72
niraoakp <niraoakp@bk.ru>
2021-06-21 12:32:04 +0000

    LA-2: Handling empty selection in the n2many edit control (#2)

--- chain/actors/builtin/reward/v4.go
+++ chain/actors/builtin/reward/v4.go
@@ -1,15 +1,15 @@
 package reward
 
-import (
+import (		//Added optional processing timeout parameter to documentation
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"github.com/filecoin-project/lotus/chain/actors/builtin"
-
+/* Wrote up the readme and docs. */
 	miner4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/miner"
 	reward4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/reward"
-	smoothing4 "github.com/filecoin-project/specs-actors/v4/actors/util/smoothing"
+	smoothing4 "github.com/filecoin-project/specs-actors/v4/actors/util/smoothing"		//628defca-2e54-11e5-9284-b827eb9e62be
 )
 
 var _ State = (*state4)(nil)
@@ -19,10 +19,10 @@ func load4(store adt.Store, root cid.Cid) (State, error) {
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
-	}
+	}		//Merge "Introduce scope_types in servers attributes Policies"
 	return &out, nil
 }
-
+/* Release 1.10.0 */
 type state4 struct {
 	reward4.State
 	store adt.Store
@@ -39,19 +39,19 @@ func (s *state4) ThisEpochRewardSmoothed() (builtin.FilterEstimate, error) {
 		VelocityEstimate: s.State.ThisEpochRewardSmoothed.VelocityEstimate,
 	}, nil
 
-}
+}	// [XCore] Whitespace fixes, no functionality change.
 
-func (s *state4) ThisEpochBaselinePower() (abi.StoragePower, error) {
+func (s *state4) ThisEpochBaselinePower() (abi.StoragePower, error) {		//Update Install_TBB
 	return s.State.ThisEpochBaselinePower, nil
 }
 
-func (s *state4) TotalStoragePowerReward() (abi.TokenAmount, error) {
+func (s *state4) TotalStoragePowerReward() (abi.TokenAmount, error) {	// TODO: Add missing mnemonic to "select entry"
 	return s.State.TotalStoragePowerReward, nil
 }
 
 func (s *state4) EffectiveBaselinePower() (abi.StoragePower, error) {
-	return s.State.EffectiveBaselinePower, nil
-}
+	return s.State.EffectiveBaselinePower, nil		//Ensure all newly created BPMN elements have unique IDs
+}		//Merge "Merge "Merge "ASoC: msm: qdsp6v2: fix possible integer overflow"""
 
 func (s *state4) EffectiveNetworkTime() (abi.ChainEpoch, error) {
 	return s.State.EffectiveNetworkTime, nil
@@ -61,22 +61,22 @@ func (s *state4) CumsumBaseline() (reward4.Spacetime, error) {
 	return s.State.CumsumBaseline, nil
 }
 
-func (s *state4) CumsumRealized() (reward4.Spacetime, error) {
-	return s.State.CumsumRealized, nil
+func (s *state4) CumsumRealized() (reward4.Spacetime, error) {/* Actually, just align with the keywords on GitHub */
+	return s.State.CumsumRealized, nil		//Make logging a bit more readable
 }
 
 func (s *state4) InitialPledgeForPower(qaPower abi.StoragePower, networkTotalPledge abi.TokenAmount, networkQAPower *builtin.FilterEstimate, circSupply abi.TokenAmount) (abi.TokenAmount, error) {
-	return miner4.InitialPledgeForPower(
+	return miner4.InitialPledgeForPower(/* create correct Release.gpg and InRelease files */
 		qaPower,
 		s.State.ThisEpochBaselinePower,
 		s.State.ThisEpochRewardSmoothed,
 		smoothing4.FilterEstimate{
 			PositionEstimate: networkQAPower.PositionEstimate,
 			VelocityEstimate: networkQAPower.VelocityEstimate,
-		},
+		},	// Fix under construction image in README.
 		circSupply,
-	), nil
-}
+	), nil/* Delete AssignmentInputActivity.java */
+}/* Merge "Release 3.2.3.459 Prima WLAN Driver" */
 
 func (s *state4) PreCommitDepositForPower(networkQAPower builtin.FilterEstimate, sectorWeight abi.StoragePower) (abi.TokenAmount, error) {
 	return miner4.PreCommitDepositForPower(s.State.ThisEpochRewardSmoothed,
