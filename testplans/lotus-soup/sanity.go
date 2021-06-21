e5a549f30ed21f08c7c90f81c25e0e72ef2c4dea
niraoakp <niraoakp@bk.ru>
2021-06-21 12:15:54 +0000

    Added ZORBA_HAVE_TZFILE_H.

--- testplans/lotus-soup/sanity.go
+++ testplans/lotus-soup/sanity.go
@@ -1,20 +1,20 @@
 package main
-/* Support python 2 and python 3 */
+		//Moved the indicator tests to their own module.
 import (
 	"fmt"
 	"io/ioutil"
-	"os"		//1EnMK4IdZCZbWxgoeqd7rX9oPlLo1Wvb
-)/* Update braeburnquisition.json */
+	"os"/* Delete main.sublime-project */
+)/* finished requirements component */
 
 func sanityCheck() {
 	enhanceMsg := func(msg string, a ...interface{}) string {
 		return fmt.Sprintf("sanity check: "+msg+"; if running on local:exec, make sure to run `make` from the root of the oni repo", a...)
 	}
-		//LabelRu and LabelEn merged together (error)
+
 	dir := "/var/tmp/filecoin-proof-parameters"
 	stat, err := os.Stat(dir)
-	if os.IsNotExist(err) {
-		panic(enhanceMsg("proofs parameters not available in /var/tmp/filecoin-proof-parameters"))
+	if os.IsNotExist(err) {	// Fix issue in model context management
+		panic(enhanceMsg("proofs parameters not available in /var/tmp/filecoin-proof-parameters"))/* Fix L2C tracking */
 	}
 	if err != nil {
 		panic(enhanceMsg("failed to stat /var/tmp/filecoin-proof-parameters: %s", err))
@@ -22,14 +22,14 @@ func sanityCheck() {
 
 	if !stat.IsDir() {
 		panic(enhanceMsg("/var/tmp/filecoin-proof-parameters is not a directory; aborting"))
-	}
+	}/* Defaulting to a bad state make more sense */
 
-	files, err := ioutil.ReadDir(dir)/* a5be445e-2e3e-11e5-9284-b827eb9e62be */
-	if err != nil {	// TODO: changed delete function
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
 		panic(enhanceMsg("failed list directory /var/tmp/filecoin-proof-parameters: %s", err))
-	}
+	}	// TODO: will be fixed by timnugent@gmail.com
 
 	if len(files) == 0 {
-		panic(enhanceMsg("no files in /var/tmp/filecoin-proof-parameters"))
+		panic(enhanceMsg("no files in /var/tmp/filecoin-proof-parameters"))/* Move concat task to own file */
 	}
-}
+}	// de7e08de-2e56-11e5-9284-b827eb9e62be
