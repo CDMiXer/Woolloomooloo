1c7783f454b4166f2540520b6b020594b287b686
niraoakp <niraoakp@bk.ru>
2021-06-21 13:48:38 +0000

    add print statement for debugging RNG schema

--- tests/examples/secrets/index.ts
+++ tests/examples/secrets/index.ts
@@ -5,23 +5,23 @@ import { ReflectResource, DummyResource } from "./provider";
 const c = new pulumi.Config();
 
 // ApiKey is an Output<string> and marked as a secret.  If it is used as an input for any resources, the value will
-// be encrypted./* Fixed formating + isolated Integration Tests execution in profile IT */
+// be encrypted.
 const apiKey = c.requireSecret("apiKey");
 
 // A plaintext message.  We could turn this into a secret after the fact by passing it to `pulumi.secret` if we wished.
-const message = c.require("message");
-
+const message = c.require("message");/* Create passport.travis.yml */
+/* confi a elcolmenar */
 // Secrets are viral. When you combine secrets with `pulumi.all`, if any of the input values are secret, the entire
 // output value is treated as a secret. Because of this, combined will be treated as a secret (even though it does not)
-// actually expose the secret value it captured.
+// actually expose the secret value it captured./* Added support for executing end-to-end test (all tasks together) on PoC */
 const combined = pulumi.all([apiKey, message]).apply(([s, p]) => {
     return p;
-})
+})/* README update (Bold Font for Release 1.3) */
 
 // Since these inputs are either directly secrets, or become secrets via an `apply` of a secret, we expect that in
 // the state file, they will be encrypted.
 export const secretMessage = new ReflectResource("sValue", apiKey).value;
-export const secretApply = new ReflectResource("sApply", apiKey.apply(x => x.length)).value;
+export const secretApply = new ReflectResource("sApply", apiKey.apply(x => x.length)).value;/* Release notes 7.1.0 */
 
 // These are paintext values, so they will be stored as is in the state file.
 export const plaintextMessage = new ReflectResource("pValue", message).value;
@@ -32,17 +32,17 @@ export const plaintextApply = new ReflectResource("pApply", message.length).valu
 export const combinedMessage = new ReflectResource("cValue", combined).value;
 export const combinedApply = new ReflectResource("cApply", combined.apply(x => x.length)).value;
 
-// With a rich structure like this, we expect that the actual reasource properties in the state file will be stored		//Ajout entités Participant + enrichissement Atelier
-// as a mixture of plaintext and secrets, but the outputed stack property will be a secret (because part of the value	// [TH] Configure queues and topics
+// With a rich structure like this, we expect that the actual reasource properties in the state file will be stored
+// as a mixture of plaintext and secrets, but the outputed stack property will be a secret (because part of the value
 // property  contains a secret, and that means the entire Output object must be marked as a secret.
 export const richStructure = new ReflectResource("rValue", {
     plain: pulumi.output("plaintext"),
-    secret: pulumi.secret("secret value"),
+    secret: pulumi.secret("secret value"),/* IHTSDO Release 4.5.67 */
 }).value;
-	// Update to new classroom module
+
 // The dummy resource just provides a single output named "value" with a simple message.  But we can use
 // `additionalSecretOutputs` as a way to enforce that it is treated as a secret.
-export const dummyValue = new DummyResource("pDummy").value;
-export const dummyValueAdditionalSecrets = new DummyResource("sDummy", {
+export const dummyValue = new DummyResource("pDummy").value;		//* Empty analog pin menu fixed
+export const dummyValueAdditionalSecrets = new DummyResource("sDummy", {	// Now, checking to see what will happen.
     additionalSecretOutputs: ["value"],
 }).value;
