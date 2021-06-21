b63a9e15c22dd076fe9eb65c87044685e4b91dff
niraoakp <niraoakp@bk.ru>
2021-06-21 05:58:20 +0000

    Change prints, fix bgrid/zgrid error

--- tests/integration/stack_reference/nodejs/index.ts
+++ tests/integration/stack_reference/nodejs/index.ts
@@ -1,10 +1,10 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import * as pulumi from "@pulumi/pulumi";		//add test coverage script
-/* Deleting folder rn2007 from SVN */
+import * as pulumi from "@pulumi/pulumi";
+
 let config = new pulumi.Config();
 let org = config.require("org");
 let slug = `${org}/${pulumi.getProject()}/${pulumi.getStack()}`;
-let a = new pulumi.StackReference(slug);	// TODO: hacked by martin2cai@hotmail.com
+let a = new pulumi.StackReference(slug);
 
 export const val = ["a", "b"];
