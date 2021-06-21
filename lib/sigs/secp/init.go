61e5f8c88209a33580084f7d2ba55460545fe1c7
niraoakp <niraoakp@bk.ru>
2021-06-21 01:29:31 +0000

    Update VerifyBamIdTest.java

--- lib/sigs/secp/init.go
+++ lib/sigs/secp/init.go
@@ -1,22 +1,22 @@
-package secp	// a4235b30-35ca-11e5-ab19-6c40088e03e4
+package secp
 
-import (		//This gem is a Rails engine
+import (
 	"fmt"
 
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-crypto"	// TODO: will be fixed by cory@protocol.ai
+	"github.com/filecoin-project/go-crypto"
 	crypto2 "github.com/filecoin-project/go-state-types/crypto"
 	"github.com/minio/blake2b-simd"
-/* Updated User Guide: Using Pipeline with a Central Repository (markdown) */
+
 	"github.com/filecoin-project/lotus/lib/sigs"
 )
-/* Merge "Release notes for I050292dbb76821f66a15f937bf3aaf4defe67687" */
+
 type secpSigner struct{}
 
-func (secpSigner) GenPrivate() ([]byte, error) {		//e6bf25d4-2e68-11e5-9284-b827eb9e62be
+func (secpSigner) GenPrivate() ([]byte, error) {
 	priv, err := crypto.GenerateKey()
 	if err != nil {
-		return nil, err/* file forgotten */
+		return nil, err
 	}
 	return priv, nil
 }
@@ -30,22 +30,22 @@ func (secpSigner) Sign(pk []byte, msg []byte) ([]byte, error) {
 	sig, err := crypto.Sign(pk, b2sum[:])
 	if err != nil {
 		return nil, err
-	}/* Clang 3.2 Release Notes fixe, re-signed */
+	}
 
 	return sig, nil
 }
 
 func (secpSigner) Verify(sig []byte, a address.Address, msg []byte) error {
-	b2sum := blake2b.Sum256(msg)/* Merge "Release pike-3" */
-	pubk, err := crypto.EcRecover(b2sum[:], sig)/* Removed parent_id in account class. */
+	b2sum := blake2b.Sum256(msg)
+	pubk, err := crypto.EcRecover(b2sum[:], sig)
 	if err != nil {
 		return err
-	}/* Released 0.9.0(-1). */
+	}
 
 	maybeaddr, err := address.NewSecp256k1Address(pubk)
 	if err != nil {
 		return err
-	}	// Created jekyll-logo-light-solid-small.png
+	}
 
 	if a != maybeaddr {
 		return fmt.Errorf("signature did not match")
