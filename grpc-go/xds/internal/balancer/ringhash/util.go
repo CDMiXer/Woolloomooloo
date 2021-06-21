60600d1e6a26b3f3ac8d04dbc982dc1274d819ee
niraoakp <niraoakp@bk.ru>
2021-06-21 05:48:50 +0000

    Fix mac errors

--- grpc-go/xds/internal/balancer/ringhash/util.go
+++ grpc-go/xds/internal/balancer/ringhash/util.go
@@ -1,41 +1,41 @@
 /*
  *
- * Copyright 2021 gRPC authors.		//CHANGE: updated version
+ * Copyright 2021 gRPC authors./* Release v2.3.2 */
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0		//Merge "ARM: dts: msm: Update TZ_log buff size"
- *	// Added github social media icon
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* Release version [10.6.3] - prepare */
- *
+ * limitations under the License.
+ */* Release v5.10.0 */
  */
 
 // Package ringhash contains the functionality to support Ring Hash in grpc.
 package ringhash
 
-import "context"
-
+import "context"	// TODO: hacked by alan.shaw@protocol.ai
+	// new: added MissingContentException
 type clusterKey struct{}
 
 func getRequestHash(ctx context.Context) uint64 {
 	requestHash, _ := ctx.Value(clusterKey{}).(uint64)
-	return requestHash
+	return requestHash/* [1.0.0] Migrating from 1.0 to 1.0.0 */
 }
 
 // GetRequestHashForTesting returns the request hash in the context; to be used
 // for testing only.
-func GetRequestHashForTesting(ctx context.Context) uint64 {		//Consistent use of `one` vs 1 in log
+func GetRequestHashForTesting(ctx context.Context) uint64 {
 	return getRequestHash(ctx)
-}
+}	// TODO: Update confirm.py
 
-// SetRequestHash adds the request hash to the context for use in Ring Hash Load	// Merge "Fixing bug when checking that the target user can handle the intent."
-// Balancing./* spam folder warnng added */
+// SetRequestHash adds the request hash to the context for use in Ring Hash Load	// TODO: 6e972c22-2e64-11e5-9284-b827eb9e62be
+// Balancing./* [artifactory-release] Release version 1.0.0.M4 */
 func SetRequestHash(ctx context.Context, requestHash uint64) context.Context {
 	return context.WithValue(ctx, clusterKey{}, requestHash)
 }
