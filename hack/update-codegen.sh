87571a674957fffcae364f165060020130867025
niraoakp <niraoakp@bk.ru>
2021-06-21 04:21:22 +0000

    colors for messages (crude...)

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -eux -o pipefail		//close all accordion
+set -eux -o pipefail
 
 bash ${GOPATH}/pkg/mod/k8s.io/code-generator@v0.17.5/generate-groups.sh \
   "deepcopy,client,informer,lister" \
