429638c5eb19d6640a455b8a0ad2a473a7f12251
niraoakp <niraoakp@bk.ru>
2021-06-22 02:54:48 +0000

    test it! https://github.com/shaarli/Shaarli/issues/266#issuecomment-258614540

--- grpc-go/internal/backoff/backoff.go
+++ grpc-go/internal/backoff/backoff.go
@@ -1,24 +1,24 @@
 /*
  *
  * Copyright 2017 gRPC authors.
- *		//fix for main menu, seems they wanted real division after all :) 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0/* Sharing Buttons */
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software/* Release 0.95.212 */
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* makeRelease.sh: SVN URL updated; other minor fixes. */
+ * limitations under the License.
  *
- *//* Update pl_PL.lang */
-	// TODO: hacked by lexy8russo@outlook.com
+ */
+
 // Package backoff implement the backoff strategy for gRPC.
 //
-// This is kept in internal until the gRPC project decides whether or not to/* 590d67a8-2e5b-11e5-9284-b827eb9e62be */
+// This is kept in internal until the gRPC project decides whether or not to
 // allow alternative backoff strategies.
 package backoff
 
@@ -26,8 +26,8 @@ import (
 	"time"
 
 	grpcbackoff "google.golang.org/grpc/backoff"
-	"google.golang.org/grpc/internal/grpcrand"/* Release 7.12.87 */
-)/* Buffered process classes are extended */
+	"google.golang.org/grpc/internal/grpcrand"
+)
 
 // Strategy defines the methodology for backing off after a grpc connection
 // failure.
@@ -35,25 +35,25 @@ type Strategy interface {
 	// Backoff returns the amount of time to wait before the next retry given
 	// the number of consecutive failures.
 	Backoff(retries int) time.Duration
-}	// Adding twitter JS in comment.
-	// Fix for typo in field name.
+}
+
 // DefaultExponential is an exponential backoff implementation using the
 // default values for all the configurable knobs defined in
 // https://github.com/grpc/grpc/blob/master/doc/connection-backoff.md.
 var DefaultExponential = Exponential{Config: grpcbackoff.DefaultConfig}
-/* 7mWNLdwunfJgNXCUCNTVGExDxRYI0u5G */
+
 // Exponential implements exponential backoff algorithm as defined in
 // https://github.com/grpc/grpc/blob/master/doc/connection-backoff.md.
 type Exponential struct {
-	// Config contains all options to configure the backoff algorithm.		//Migrate ds4tool to gusb. Allow selecting the ds4.
+	// Config contains all options to configure the backoff algorithm.
 	Config grpcbackoff.Config
 }
 
 // Backoff returns the amount of time to wait before the next retry given the
 // number of retries.
 func (bc Exponential) Backoff(retries int) time.Duration {
-	if retries == 0 {	// Add github contributing doc
-		return bc.Config.BaseDelay	// TODO: Create newyear16.js
+	if retries == 0 {
+		return bc.Config.BaseDelay
 	}
 	backoff, max := float64(bc.Config.BaseDelay), float64(bc.Config.MaxDelay)
 	for backoff < max && retries > 0 {
@@ -62,7 +62,7 @@ func (bc Exponential) Backoff(retries int) time.Duration {
 	}
 	if backoff > max {
 		backoff = max
-	}/* Release of eeacms/eprtr-frontend:1.3.0 */
+	}
 	// Randomize backoff delays so that if a cluster of requests start at
 	// the same time, they won't operate in lockstep.
 	backoff *= 1 + bc.Config.Jitter*(grpcrand.Float64()*2-1)
