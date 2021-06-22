1328fb78523450c416c0e6566f2e5ea7fc46fc84
niraoakp <niraoakp@bk.ru>
2021-06-22 15:49:08 +0000

    ZAPI-445: Enable vmapi dtrace-provider

--- tests/integration/protect_resources/step3/index.ts
+++ tests/integration/protect_resources/step3/index.ts
@@ -1,4 +1,4 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.		//Merge branch 'master' into update-cursor-style-for-divider-and-title
 
 import { Resource } from "./resource";
 
