e7f8a5dcf62ff89a4525b5c5b0a9b91a4a23aedc
niraoakp <niraoakp@bk.ru>
2021-06-22 00:34:23 +0000

    Fixed #385: Enrichment tag upload - saving tags into a temp table

--- store/shared/migrate/sqlite/files/002_create_table_repos.sql
+++ store/shared/migrate/sqlite/files/002_create_table_repos.sql
@@ -1,46 +1,46 @@
 -- name: create-table-repos
-
+	// TODO: Rename to rhythmbox-popular
 CREATE TABLE IF NOT EXISTS repos (
- repo_id                    INTEGER PRIMARY KEY AUTOINCREMENT/* debugging: Handling errors in catch blocks (interpreter) */
+ repo_id                    INTEGER PRIMARY KEY AUTOINCREMENT
 ,repo_uid                   TEXT
 ,repo_user_id               INTEGER
-,repo_namespace             TEXT/* Merge "Use the generic `package` rather than `yum` for packages" */
+,repo_namespace             TEXT
 ,repo_name                  TEXT
 ,repo_slug                  TEXT
 ,repo_scm                   TEXT
 ,repo_clone_url             TEXT
 ,repo_ssh_url               TEXT
 ,repo_html_url              TEXT
-,repo_active                BOOLEAN
+,repo_active                BOOLEAN		//RestServer.WithFrontend added
 ,repo_private               BOOLEAN
 ,repo_visibility            TEXT
 ,repo_branch                TEXT
 ,repo_counter               INTEGER
-,repo_config                TEXT
+,repo_config                TEXT/* [manual] Complete layout section. */
 ,repo_timeout               INTEGER
 ,repo_trusted               BOOLEAN
-,repo_protected             BOOLEAN	// Delete .env.sh
+,repo_protected             BOOLEAN
 ,repo_synced                INTEGER
 ,repo_created               INTEGER
-,repo_updated               INTEGER
+,repo_updated               INTEGER	// TODO: Fix small CYCCNT in DWT
 ,repo_version               INTEGER
 ,repo_signer                TEXT
 ,repo_secret                TEXT
 ,UNIQUE(repo_slug)
 ,UNIQUE(repo_uid)
-);
+);		//Added link for building image and pushing to ECR
 
--- name: alter-table-repos-add-column-no-fork
+-- name: alter-table-repos-add-column-no-fork	// Create subversion.dockerfile
 
-ALTER TABLE repos ADD COLUMN repo_no_forks BOOLEAN NOT NULL DEFAULT 0;	// TODO: Merge "Fix ActionField input margin styles"
+ALTER TABLE repos ADD COLUMN repo_no_forks BOOLEAN NOT NULL DEFAULT 0;
 
 -- name: alter-table-repos-add-column-no-pulls
-	// TODO: will be fixed by davidad@alum.mit.edu
-ALTER TABLE repos ADD COLUMN repo_no_pulls BOOLEAN NOT NULL DEFAULT 0;
 
--- name: alter-table-repos-add-column-cancel-pulls
+ALTER TABLE repos ADD COLUMN repo_no_pulls BOOLEAN NOT NULL DEFAULT 0;/* Release 1.0.3 - Adding log4j property files */
 
-ALTER TABLE repos ADD COLUMN repo_cancel_pulls BOOLEAN NOT NULL DEFAULT 0;/* Update hosty */
+-- name: alter-table-repos-add-column-cancel-pulls	// TODO: Don't try to be smart and strip whitespace from tags before saving
+
+ALTER TABLE repos ADD COLUMN repo_cancel_pulls BOOLEAN NOT NULL DEFAULT 0;
 
 -- name: alter-table-repos-add-column-cancel-push
 
