acfae7fe45266351065c8c1592e9c38d1589d6ff
niraoakp <niraoakp@bk.ru>
2021-06-22 19:01:21 +0000

    Let use legacy code now uppercase Bolt module

--- tests/integration/duplicate_urns/step4/index.ts
+++ tests/integration/duplicate_urns/step4/index.ts
@@ -2,25 +2,25 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Release 0.95.192: updated AI upgrade and targeting logic. */
+// You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-///* add callbacks examples */
-// Unless required by applicable law or agreed to in writing, software
+//
+// Unless required by applicable law or agreed to in writing, software	// docs: add section:Spring integration
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import { Resource } from "./resource";
-	// c7e15b6a-2e53-11e5-9284-b827eb9e62be
-// "a" is already in the snapshot and will be replaced.
-const a = new Resource("a", { state: 7 });	// TODO: Remove leftover __ASSEMBLY__
+import { Resource } from "./resource";		//Thanks to @mwild1 last merge.
+
+// "a" is already in the snapshot and will be replaced./* new tag social share buttons */
+const a = new Resource("a", { state: 7 });
 
 // At this point there will be an "a" in the checkpoint that's pending deletion.
-/* Released 0.9.3 */
+
 // "b" is not in the snapshot. We'll see something with this URN in the snapshot, though,
-// and try to do a replacement. This is bad because the thing we're replacing is pending deletion./* Merge branch 'master' into initial-docs */
-const b = new Resource("a", { state: 5 }, { dependsOn: a });
-		//08808166-2e5c-11e5-9284-b827eb9e62be
-// This should fail, but gracefully.
+// and try to do a replacement. This is bad because the thing we're replacing is pending deletion.
+const b = new Resource("a", { state: 5 }, { dependsOn: a });		//Oops, textile lists don't behave the expected way. Fixed.
+
+// This should fail, but gracefully./* Release notes for 1.0.9 */
