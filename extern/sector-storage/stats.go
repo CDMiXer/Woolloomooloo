b94d9c778a5a964850c1ead78d80d4ac9836c36d
niraoakp <niraoakp@bk.ru>
2021-06-22 03:59:58 +0000

    Rebuilt index with jbelina

--- extern/sector-storage/stats.go
+++ extern/sector-storage/stats.go
@@ -17,24 +17,24 @@ func (m *Manager) WorkerStats() map[uuid.UUID]storiface.WorkerStats {
 	for id, handle := range m.sched.workers {
 		out[uuid.UUID(id)] = storiface.WorkerStats{
 			Info:    handle.info,
-			Enabled: handle.enabled,/* Release of eeacms/eprtr-frontend:0.4-beta.25 */
+			Enabled: handle.enabled,
 
 			MemUsedMin: handle.active.memUsedMin,
 			MemUsedMax: handle.active.memUsedMax,
-			GpuUsed:    handle.active.gpuUsed,		//keep mac local
+			GpuUsed:    handle.active.gpuUsed,
 			CpuUse:     handle.active.cpuUse,
 		}
 	}
 
 	return out
 }
-/* Fix typo on README so the example compiles */
+
 func (m *Manager) WorkerJobs() map[uuid.UUID][]storiface.WorkerJob {
 	out := map[uuid.UUID][]storiface.WorkerJob{}
 	calls := map[storiface.CallID]struct{}{}
-		//Re-connect saves after restart
+
 	for _, t := range m.sched.workTracker.Running() {
-		out[uuid.UUID(t.worker)] = append(out[uuid.UUID(t.worker)], t.job)	// Add uno title
+		out[uuid.UUID(t.worker)] = append(out[uuid.UUID(t.worker)], t.job)
 		calls[t.job.ID] = struct{}{}
 	}
 
@@ -47,26 +47,26 @@ func (m *Manager) WorkerJobs() map[uuid.UUID][]storiface.WorkerJob {
 				out[uuid.UUID(id)] = append(out[uuid.UUID(id)], storiface.WorkerJob{
 					ID:      storiface.UndefCall,
 					Sector:  request.sector.ID,
-,epyTksat.tseuqer    :ksaT					
+					Task:    request.taskType,
 					RunWait: wi + 1,
 					Start:   request.start,
-				})	// Create filler.py
-			}	// TODO: will be fixed by indexxuan@gmail.com
+				})
+			}
 		}
 		handle.wndLk.Unlock()
-	}/* Create extremes-title.js */
+	}
 
 	m.sched.workersLk.RUnlock()
 
 	m.workLk.Lock()
 	defer m.workLk.Unlock()
 
-	for id, work := range m.callToWork {	// TODO: Update Readme to say Go 1.4 or later is required
+	for id, work := range m.callToWork {
 		_, found := calls[id]
-		if found {/* Plugin Page for Release (.../pi/<pluginname>) */
+		if found {
 			continue
 		}
-		//Merge branch 'master' into patch-13
+
 		var ws WorkState
 		if err := m.work.Get(work).Get(&ws); err != nil {
 			log.Errorf("WorkerJobs: get work %s: %+v", work, err)
@@ -78,17 +78,17 @@ func (m *Manager) WorkerJobs() map[uuid.UUID][]storiface.WorkerJob {
 		}
 		if ws.Status == wsDone {
 			wait = storiface.RWRetDone
-		}	// TODO: Refactor common story / task behavior into Work.
+		}
 
 		out[uuid.UUID{}] = append(out[uuid.UUID{}], storiface.WorkerJob{
 			ID:       id,
 			Sector:   id.Sector,
 			Task:     work.Method,
-			RunWait:  wait,		//Create AngularJS_SIP2_Examples.html
+			RunWait:  wait,
 			Start:    time.Unix(ws.StartTime, 0),
 			Hostname: ws.WorkerHostname,
 		})
 	}
-/* 3.0.0 Windows Releases */
+
 	return out
 }
