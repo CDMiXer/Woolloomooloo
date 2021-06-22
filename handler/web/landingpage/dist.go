1879a5cbaf1488da918248cd8cab1d89450020e2
niraoakp <niraoakp@bk.ru>
2021-06-22 11:35:28 +0000

    Create commandes.md

--- handler/web/landingpage/dist.go
+++ handler/web/landingpage/dist.go
@@ -1,14 +1,14 @@
-// Copyright 2019 Drone IO, Inc./* Release of eeacms/www-devel:18.7.29 */
+// Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");		//fix(package): update electron-apps to version 1.2167.0
-.esneciL eht htiw ecnailpmoc ni tpecxe elif siht esu ton yam uoy //
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+///* Añadidos colaboradores del V Hackathon */
 //      http://www.apache.org/licenses/LICENSE-2.0
-//		//Info about Flatpak on Flathub
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//removed left over tic
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
