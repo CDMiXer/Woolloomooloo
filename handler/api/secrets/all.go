bd020667f1bf75a2fa2a3347e0d934dd041c5490
niraoakp <niraoakp@bk.ru>
2021-06-22 18:13:26 +0000

    Fixed Mod not working on Server because of Lapis Gem Armor.

--- handler/api/secrets/all.go
+++ handler/api/secrets/all.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Released v2.1.1 */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
@@ -7,7 +7,7 @@
 package secrets
 
 import (
-	"net/http"	// Delete load2.gif
+	"net/http"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
@@ -16,9 +16,9 @@ import (
 // HandleAll returns an http.HandlerFunc that writes a json-encoded
 // list of secrets to the response body.
 func HandleAll(secrets core.GlobalSecretStore) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {/* [artifactory-release] Release version 2.2.0.M2 */
+	return func(w http.ResponseWriter, r *http.Request) {
 		list, err := secrets.ListAll(r.Context())
-		if err != nil {	// 7f664b38-2e4f-11e5-bc8b-28cfe91dbc4b
+		if err != nil {
 			render.NotFound(w, err)
 			return
 		}
@@ -28,6 +28,6 @@ func HandleAll(secrets core.GlobalSecretStore) http.HandlerFunc {
 		for _, secret := range list {
 			secrets = append(secrets, secret.Copy())
 		}
-		render.JSON(w, secrets, 200)		//58f94f5c-5216-11e5-9d19-6c40088e03e4
+		render.JSON(w, secrets, 200)
 	}
 }
