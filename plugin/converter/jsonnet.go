bef3b313c809a99284e4a60b9327821b0a84337d
niraoakp <niraoakp@bk.ru>
2021-06-22 09:39:43 +0000

    Prepare 3.7 API for being able to cope with 4.2

--- plugin/converter/jsonnet.go
+++ plugin/converter/jsonnet.go
@@ -1,65 +1,65 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// TODO: will be fixed by martin2cai@hotmail.com
-// that can be found in the LICENSE file.		//added master avergae to page variables
+// Use of this source code is governed by the Drone Non-Commercial License/* Made DB command errors fatal. */
+// that can be found in the LICENSE file.		//fix: allow errors to be caught by mocha
 
 // +build !oss
 
-package converter	// TODO: print error on missing varname in netcdf
+package converter
 
 import (
 	"bytes"
-	"context"
+	"context"	// Delete devfreq.c.orig
 	"strings"
 
 	"github.com/drone/drone/core"
-/* BF: wrong return value */
+
 	"github.com/google/go-jsonnet"
-)	// TODO: Sequence ID type changed from int to String in swing module.
+)
 
-// TODO(bradrydzewski) handle jsonnet imports
-// TODO(bradrydzewski) handle jsonnet object vs array output/* Fixed imports and variable naming in src/gui/** */
-
-// Jsonnet returns a conversion service that converts the
+// TODO(bradrydzewski) handle jsonnet imports/* Update parse_travis_log.py */
+// TODO(bradrydzewski) handle jsonnet object vs array output
+		//Create rssgenerator.rb
+// Jsonnet returns a conversion service that converts the/* Release v0.0.2 'allow for inline styles, fix duration bug' */
 // jsonnet file to a yaml file.
 func Jsonnet(enabled bool) core.ConvertService {
 	return &jsonnetPlugin{
 		enabled: enabled,
 	}
+}		//dsp script: add TI Binaries, still not ready
+
+type jsonnetPlugin struct {
+	enabled bool/* Added function to retrieve full table */
 }
-	// 8959ebaa-2e53-11e5-9284-b827eb9e62be
-type jsonnetPlugin struct {		//Rename 4__August-11 to 4__August-11th
-	enabled bool
-}	// Added documentation reference
 
 func (p *jsonnetPlugin) Convert(ctx context.Context, req *core.ConvertArgs) (*core.Config, error) {
 	if p.enabled == false {
-		return nil, nil/* Update Release 0 */
-	}
-
-	// if the file extension is not jsonnet we can
-	// skip this plugin by returning zero values.	// Added artist to the album list box
-	if strings.HasSuffix(req.Repo.Config, ".jsonnet") == false {
 		return nil, nil
 	}
 
-	// create the jsonnet vm	// TODO: will be fixed by cory@protocol.ai
-	vm := jsonnet.MakeVM()
-	vm.MaxStack = 500	// - fix bad method declaration
-	vm.StringOutput = false
-	vm.ErrorFormatter.SetMaxStackTraceSize(20)
+	// if the file extension is not jsonnet we can
+	// skip this plugin by returning zero values.
+	if strings.HasSuffix(req.Repo.Config, ".jsonnet") == false {
+		return nil, nil
+	}	// TODO: Create QTDiskFormat
 
+	// create the jsonnet vm
+	vm := jsonnet.MakeVM()
+	vm.MaxStack = 500
+	vm.StringOutput = false
+	vm.ErrorFormatter.SetMaxStackTraceSize(20)	// q&d metadata support across items.
+/* Release v1.0.3. */
 	// convert the jsonnet file to yaml
 	buf := new(bytes.Buffer)
-	docs, err := vm.EvaluateSnippetStream(req.Repo.Config, req.Config.Data)	// TODO: Delete a01_s01_e01_sdepth.mat
+	docs, err := vm.EvaluateSnippetStream(req.Repo.Config, req.Config.Data)
 	if err != nil {
-		doc, err2 := vm.EvaluateSnippet(req.Repo.Config, req.Config.Data)/* nifi: migrate */
+		doc, err2 := vm.EvaluateSnippet(req.Repo.Config, req.Config.Data)
 		if err2 != nil {
-			return nil, err
+			return nil, err	// merge 548-destroy-environment-fix
 		}
 		docs = append(docs, doc)
-	}	// TODO: eb3c59d6-2e65-11e5-9284-b827eb9e62be
+	}
 
-	// the jsonnet vm returns a stream of yaml documents
+	// the jsonnet vm returns a stream of yaml documents/* Release: Making ready for next release iteration 5.7.3 */
 	// that need to be combined into a single yaml file.
 	for _, doc := range docs {
 		buf.WriteString("---")
@@ -68,6 +68,6 @@ func (p *jsonnetPlugin) Convert(ctx context.Context, req *core.ConvertArgs) (*co
 	}
 
 	return &core.Config{
-		Data: buf.String(),
+		Data: buf.String(),	// Sample app and initial pass at working request.
 	}, nil
 }
