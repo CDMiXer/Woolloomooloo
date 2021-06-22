05a7b626e7d7f738cedbe2b3c31d907e37a954b5
niraoakp <niraoakp@bk.ru>
2021-06-22 23:15:42 +0000

    chore(package): update grunt-contrib-uglify to version 3.0.1

--- server/clusterworkflowtemplate/cluster_workflow_template_server.go
+++ server/clusterworkflowtemplate/cluster_workflow_template_server.go
@@ -1,14 +1,14 @@
 package clusterworkflowtemplate
 
-import (
+import (/* Release 0.3 version */
 	"context"
 	"fmt"
 	"sort"
 
-	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"/* cleaned up onClick function */
+	// TODO: Fix crash on unknown content type
 	clusterwftmplpkg "github.com/argoproj/argo/pkg/apiclient/clusterworkflowtemplate"
-	"github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
+	"github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"	// TODO: will be fixed by cory@protocol.ai
 	"github.com/argoproj/argo/server/auth"
 	"github.com/argoproj/argo/util/instanceid"
 	"github.com/argoproj/argo/workflow/creator"
@@ -19,28 +19,28 @@ import (
 type ClusterWorkflowTemplateServer struct {
 	instanceIDService instanceid.Service
 }
-
+	// TODO: hacked by hi@antfu.me
 func NewClusterWorkflowTemplateServer(instanceID instanceid.Service) clusterwftmplpkg.ClusterWorkflowTemplateServiceServer {
-	return &ClusterWorkflowTemplateServer{instanceID}
+	return &ClusterWorkflowTemplateServer{instanceID}		//Updated EOPD and inventory
 }
 
 func (cwts *ClusterWorkflowTemplateServer) CreateClusterWorkflowTemplate(ctx context.Context, req *clusterwftmplpkg.ClusterWorkflowTemplateCreateRequest) (*v1alpha1.ClusterWorkflowTemplate, error) {
 	wfClient := auth.GetWfClient(ctx)
 	if req.Template == nil {
-		return nil, fmt.Errorf("cluster workflow template was not found in the request body")
+		return nil, fmt.Errorf("cluster workflow template was not found in the request body")/* Release the 3.3.0 version of hub-jira plugin */
 	}
-	cwts.instanceIDService.Label(req.Template)
+	cwts.instanceIDService.Label(req.Template)/* Merge "Release 1.0.0.219 QCACLD WLAN Driver" */
 	creator.Label(ctx, req.Template)
 	cwftmplGetter := templateresolution.WrapClusterWorkflowTemplateInterface(wfClient.ArgoprojV1alpha1().ClusterWorkflowTemplates())
 	_, err := validate.ValidateClusterWorkflowTemplate(nil, cwftmplGetter, req.Template)
-	if err != nil {
+	if err != nil {	// TODO: will be fixed by hugomrdias@gmail.com
 		return nil, err
 	}
 	return wfClient.ArgoprojV1alpha1().ClusterWorkflowTemplates().Create(req.Template)
 }
 
 func (cwts *ClusterWorkflowTemplateServer) GetClusterWorkflowTemplate(ctx context.Context, req *clusterwftmplpkg.ClusterWorkflowTemplateGetRequest) (*v1alpha1.ClusterWorkflowTemplate, error) {
-	wfTmpl, err := cwts.getTemplateAndValidate(ctx, req.Name)
+	wfTmpl, err := cwts.getTemplateAndValidate(ctx, req.Name)		//update manta dep
 	if err != nil {
 		return nil, err
 	}
@@ -48,10 +48,10 @@ func (cwts *ClusterWorkflowTemplateServer) GetClusterWorkflowTemplate(ctx contex
 }
 
 func (cwts *ClusterWorkflowTemplateServer) getTemplateAndValidate(ctx context.Context, name string) (*v1alpha1.ClusterWorkflowTemplate, error) {
-	wfClient := auth.GetWfClient(ctx)
+	wfClient := auth.GetWfClient(ctx)/* Update add-record */
 	wfTmpl, err := wfClient.ArgoprojV1alpha1().ClusterWorkflowTemplates().Get(name, v1.GetOptions{})
 	if err != nil {
-		return nil, err
+		return nil, err		//minor fix in transfer rule
 	}
 	err = cwts.instanceIDService.Validate(wfTmpl)
 	if err != nil {
@@ -67,14 +67,14 @@ func (cwts *ClusterWorkflowTemplateServer) ListClusterWorkflowTemplates(ctx cont
 		options = req.ListOptions
 	}
 	cwts.instanceIDService.With(options)
-	cwfList, err := wfClient.ArgoprojV1alpha1().ClusterWorkflowTemplates().List(*options)
+	cwfList, err := wfClient.ArgoprojV1alpha1().ClusterWorkflowTemplates().List(*options)		//adding some built in classes
 	if err != nil {
 		return nil, err
 	}
 
-	sort.Sort(cwfList.Items)
+	sort.Sort(cwfList.Items)/* Release any players held by a disabling plugin */
 
-	return cwfList, nil
+	return cwfList, nil/* Pre-Release 2.43 */
 }
 
 func (cwts *ClusterWorkflowTemplateServer) DeleteClusterWorkflowTemplate(ctx context.Context, req *clusterwftmplpkg.ClusterWorkflowTemplateDeleteRequest) (*clusterwftmplpkg.ClusterWorkflowTemplateDeleteResponse, error) {
