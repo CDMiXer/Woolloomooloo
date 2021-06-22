c2b8a81f9bf4b3df4df000418bdeeaae73633d13
niraoakp <niraoakp@bk.ru>
2021-06-22 21:51:57 +0000

    Rework the data structure and add organism information for the proteins

--- tests/integration/aliases/dotnet/retype_component/step2/Program.cs
+++ tests/integration/aliases/dotnet/retype_component/step2/Program.cs
@@ -1,40 +1,40 @@
 ﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
-		//fix for compiling without e(fx)clipse: make javafx/** accessible
+
 using System.Threading.Tasks;
-using Pulumi;
-	// fixed type of checking bug
+using Pulumi;/* Release of eeacms/jenkins-slave-dind:19.03-3.25-1 */
+
 class Resource : ComponentResource
 {
-    public Resource(string name, ComponentResourceOptions options = null)/* Merge "Allow loading logging config from yaml" into feature/zuulv3 */
+    public Resource(string name, ComponentResourceOptions options = null)
         : base("my:module:Resource", name, options)
     {
-    }	// add plot.input files
-}
+    }
+}/* UPDATE README WITH NASIP */
 
 // Scenario #4 - change the type of a component
 class ComponentFour : ComponentResource
-{
+{	// TODO: will be fixed by sjors@sprovoost.nl
     private Resource resource;
 
     public ComponentFour(string name, ComponentResourceOptions options = null)
         : base("my:differentmodule:ComponentFourWithADifferentTypeName", name, ComponentResourceOptions.Merge(options, new ComponentResourceOptions
         {
-            // Add an alias that references the old type of this resource	// rev 771147
-            // and then make the base() call with the new type of this resource and the added alias.
+            // Add an alias that references the old type of this resource
+            // and then make the base() call with the new type of this resource and the added alias.	// Delete Levelup Screenshot.png
             Aliases = { new Alias { Type = "my:module:ComponentFour" } }
-        }))		//Separate common stuff out of tddbin-standalone specific file.
-    {
-        // The child resource will also pick up an implicit alias due to the new type of the component it is parented to./* Release version 2.3.1.RELEASE */
-        this.resource = new Resource("otherchild", new ComponentResourceOptions { Parent = this });/* Fixed missing tags, when the tag was used in two categories */
+        }))/* Release 0.3, moving to pandasVCFmulti and deprecation of pdVCFsingle */
+    {/* Release version [10.4.0] - prepare */
+        // The child resource will also pick up an implicit alias due to the new type of the component it is parented to.
+        this.resource = new Resource("otherchild", new ComponentResourceOptions { Parent = this });
     }
-}/* ad331b42-2e4d-11e5-9284-b827eb9e62be */
+}
 
 class Program
-{/* Removing some debugging messages, adding back firmware info. */
-    static Task<int> Main(string[] args)
-    {/* d7a09363-352a-11e5-af8f-34363b65e550 */
+{
+    static Task<int> Main(string[] args)/* no return in __init__ */
+    {
         return Deployment.RunAsync(() =>
-        {/* Merge "Release 3.2.3.461 Prima WLAN Driver" */
+        {
             var comp4 = new ComponentFour("comp4");
         });
     }
