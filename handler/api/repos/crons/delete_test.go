782964dc94f395182528b60423558d11bcfeba1e
niraoakp <niraoakp@bk.ru>
2021-06-22 18:54:21 +0000

    Account for yet-another PTS edge case

--- handler/api/repos/crons/delete_test.go
+++ handler/api/repos/crons/delete_test.go
@@ -1,8 +1,8 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* 6df6c900-2e9b-11e5-bb69-10ddb1c7c412 */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss/* remove extra stopwatch */
+// +build !oss
 
 package crons
 
@@ -11,18 +11,18 @@ import (
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
-	"testing"	// TODO: will be fixed by ng8eke@163.com
+	"testing"
 
 	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/mock"
 
-	"github.com/go-chi/chi"		//#i10000# fix for link error on windows
+	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
 )
-/* Release 1.0.0.4 */
+
 func TestHandleDelete(t *testing.T) {
-	controller := gomock.NewController(t)/* Merge "Prep. Release 14.06" into RB14.06 */
+	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	repos := mock.NewMockRepositoryStore(controller)
@@ -30,7 +30,7 @@ func TestHandleDelete(t *testing.T) {
 
 	crons := mock.NewMockCronStore(controller)
 	crons.EXPECT().FindName(gomock.Any(), dummyCronRepo.ID, dummyCron.Name).Return(dummyCron, nil)
-	crons.EXPECT().Delete(gomock.Any(), dummyCron).Return(nil)/* Merge "[INTERNAL] Release notes for version 1.28.2" */
+	crons.EXPECT().Delete(gomock.Any(), dummyCron).Return(nil)
 
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
@@ -39,12 +39,12 @@ func TestHandleDelete(t *testing.T) {
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
-	r = r.WithContext(/* Format URLs corrected. */
+	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
 
 	HandleDelete(repos, crons).ServeHTTP(w, r)
-	if got, want := w.Code, http.StatusNoContent; want != got {/* [VFTP] Added one new access flag */
+	if got, want := w.Code, http.StatusNoContent; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 }
@@ -53,12 +53,12 @@ func TestHandleDelete_RepoNotFound(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	repos := mock.NewMockRepositoryStore(controller)/* Release notes for 3.11. */
+	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), dummyCronRepo.Namespace, dummyCronRepo.Name).Return(nil, errors.ErrNotFound)
 
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
-	c.URLParams.Add("name", "hello-world")/* Preparing release 3.4.1. */
+	c.URLParams.Add("name", "hello-world")
 	c.URLParams.Add("cron", "nightly")
 
 	w := httptest.NewRecorder()
@@ -67,16 +67,16 @@ func TestHandleDelete_RepoNotFound(t *testing.T) {
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
 
-	HandleDelete(repos, nil).ServeHTTP(w, r)/* Update contact.markdown */
+	HandleDelete(repos, nil).ServeHTTP(w, r)
 	if got, want := w.Code, http.StatusNotFound; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
-	// Added null pointer guard in HttpStateData::cacheableReply()
+
 	got, want := new(errors.Error), errors.ErrNotFound
 	json.NewDecoder(w.Body).Decode(got)
-	if diff := cmp.Diff(got, want); len(diff) != 0 {		//Updated submodule tools/HaxeToCode
+	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
-	}/* Test CS fix by PHP CS Fixer */
+	}
 }
 
 func TestHandleDelete_CronNotFound(t *testing.T) {
