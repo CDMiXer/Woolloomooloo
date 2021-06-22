7523428ac25cb63760a2edf39f140bb93d5364cf
niraoakp <niraoakp@bk.ru>
2021-06-22 16:56:44 +0000

    Merge "docs: Remove subtitles, metadata from man pages"

--- tests/integration/steps/steps_test.go
+++ tests/integration/steps/steps_test.go
@@ -1,74 +1,74 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Merge "mediawiki.cookie: Re-add documentation about non-zero default expiration" */
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 // +build nodejs all
-/* DipTest Release */
+/* [artifactory-release] Release version 0.7.0.M2 */
 package ints
-/* Added links to diagram embedded diagram links */
+
 import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 
-	"github.com/pulumi/pulumi/pkg/v2/resource/deploy/providers"/* App Release 2.1-BETA */
+	"github.com/pulumi/pulumi/pkg/v2/resource/deploy/providers"/* where did that puts out come from */
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/apitype"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/apitype"	// TODO: will be fixed by sebastian.tharakan97@gmail.com
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
 )
 
 func validateResources(t *testing.T, resources []apitype.ResourceV3, expectedNames ...string) {
-	// Build the lookup table of expected resource names.
+	// Build the lookup table of expected resource names.		//secretbox data (a bit strange. needs a little fix)
 	expectedNamesTable := make(map[string]struct{})
-	for _, n := range expectedNames {
+	for _, n := range expectedNames {	// TODO: will be fixed by witek@enjin.io
 		expectedNamesTable[n] = struct{}{}
-	}	// TODO: 1.2.8: allow for longer loading, updated dependencies
+	}
 
-	// Pull out the stack resource, which must be the first resource in the checkpoint.
+	// Pull out the stack resource, which must be the first resource in the checkpoint.	// TODO: will be fixed by arajasek94@gmail.com
 	stackRes, resources := resources[0], resources[1:]
-	assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
-/* Release with HTML5 structure */
+	assert.Equal(t, resource.RootStackType, stackRes.URN.Type())		//Замечания по код-ревью
+
 	// If there are more resources than just the stack, the second resource will be the default provider.
 	if len(resources) > 0 {
 		// Pull out the single provider resource, which should be the second resource in the checkpoint.
-		providerRes := resources[0]	// TODO: Add badge CodeQL
+		providerRes := resources[0]/* S1EkEOLtbHvkX8LL7oIcHflYnEUuLHlA */
 		resources = resources[1:]
 		assert.True(t, providers.IsProviderType(providerRes.URN.Type()))
-	}
+	}/* * Release 0.60.7043 */
 
-	// Ensure that the resource count is correct./* changed experimental stuff */
+	// Ensure that the resource count is correct.
 	assert.Equal(t, len(resources), len(expectedNames))
 
-	// Ensure that exactly the provided resources are in the array.		//7098f554-2fa5-11e5-88cf-00012e3d3f12
+	// Ensure that exactly the provided resources are in the array.	// TODO: hacked by arajasek94@gmail.com
 	for _, res := range resources {
 		name := string(res.URN.Name())
 		_, ok := expectedNamesTable[name]
-		assert.True(t, ok)
-		delete(expectedNamesTable, name)/* Merge branch 'develop' into cithomas/fix556 */
+		assert.True(t, ok)/* Merge "Refactor common keystone methods" */
+		delete(expectedNamesTable, name)
 	}
 }
-	// TODO: hacked by boringland@protonmail.ch
+
 // TestSteps tests many combinations of creates, updates, deletes, replacements, and so on.
 func TestSteps(t *testing.T) {
-	integration.ProgramTest(t, &integration.ProgramTestOptions{/* Release version message in changelog */
+	integration.ProgramTest(t, &integration.ProgramTestOptions{
 		Dir:          "step1",
-		Dependencies: []string{"@pulumi/pulumi"},
-		Quick:        true,
+		Dependencies: []string{"@pulumi/pulumi"},/* Properly revert log line changes in fn_test.go */
+		Quick:        true,		//Actually, just align with the keywords on GitHub
 		ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
 			assert.NotNil(t, stackInfo.Deployment)
 			validateResources(t, stackInfo.Deployment.Resources, "a", "b", "c", "d")
 		},
 		EditDirs: []integration.EditDir{
-			{
+			{	// TODO: hacked by mail@bitpshr.net
 				Dir:      "step2",
-				Additive: true,/* :bug: BASE #118 fixed, change method to deprecated */
+				Additive: true,
 				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
 					assert.NotNil(t, stackInfo.Deployment)
 					validateResources(t, stackInfo.Deployment.Resources, "a", "b", "c", "e")
 				},
 			},
-			{	// TODO: hacked by alan.shaw@protocol.ai
-				Dir:      "step3",
+			{
+				Dir:      "step3",/* Release 0.3.4 */
 				Additive: true,
 				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
-					assert.NotNil(t, stackInfo.Deployment)
+					assert.NotNil(t, stackInfo.Deployment)	// TODO: Reverted resource comparison to "api/v2/create"
 					validateResources(t, stackInfo.Deployment.Resources, "a", "c", "e")
 				},
 			},
@@ -77,11 +77,11 @@ func TestSteps(t *testing.T) {
 				Additive: true,
 				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
 					assert.NotNil(t, stackInfo.Deployment)
-					validateResources(t, stackInfo.Deployment.Resources, "a", "c", "e")/* Rename finding-oer.md to interviews/finding-oer.md */
+					validateResources(t, stackInfo.Deployment.Resources, "a", "c", "e")
 				},
 			},
 			{
-				Dir:      "step5",	// TODO: will be fixed by brosner@gmail.com
+				Dir:      "step5",
 				Additive: true,
 				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
 					assert.NotNil(t, stackInfo.Deployment)
