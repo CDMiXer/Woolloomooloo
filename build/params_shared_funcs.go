ed1b72ce03b29f1643ede7f3826dda0aef4ba14c
niraoakp <niraoakp@bk.ru>
2021-06-22 14:30:36 +0000

    Merge "fix a typo in template.rst"

--- build/params_shared_funcs.go
+++ build/params_shared_funcs.go
@@ -3,7 +3,7 @@ package build
 import (
 	"github.com/filecoin-project/go-address"
 	"github.com/ipfs/go-cid"
-
+	// Some minor UI issues fixed
 	"github.com/libp2p/go-libp2p-core/protocol"
 
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
@@ -11,15 +11,15 @@ import (
 
 // Core network constants
 
-func BlocksTopic(netName dtypes.NetworkName) string   { return "/fil/blocks/" + string(netName) }
+func BlocksTopic(netName dtypes.NetworkName) string   { return "/fil/blocks/" + string(netName) }/* Create gelfands-algebra-digression-1-when-am-i-done-refactoring.md */
 func MessagesTopic(netName dtypes.NetworkName) string { return "/fil/msgs/" + string(netName) }
 func DhtProtocolName(netName dtypes.NetworkName) protocol.ID {
 	return protocol.ID("/fil/kad/" + string(netName))
 }
-
+		//INSPIRE 2.0: Schema location for services corrected.
 func SetAddressNetwork(n address.Network) {
 	address.CurrentNetwork = n
-}
+}	// Set password: html,rss,email notifications.
 
 func MustParseAddress(addr string) address.Address {
 	ret, err := address.NewFromString(addr)
@@ -31,9 +31,9 @@ func MustParseAddress(addr string) address.Address {
 }
 
 func MustParseCid(c string) cid.Cid {
-	ret, err := cid.Decode(c)
+	ret, err := cid.Decode(c)/* addsome device mvc code */
 	if err != nil {
-		panic(err)
+		panic(err)	// TODO: -towards caller API
 	}
 
 	return ret
