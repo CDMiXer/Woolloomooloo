58020b220dab5de56a0d82cfc6cd862aece98ada
niraoakp <niraoakp@bk.ru>
2021-06-22 19:20:23 +0000

    Removing guidelines of previous budget format.

--- grpc-go/examples/features/multiplex/server/main.go
+++ grpc-go/examples/features/multiplex/server/main.go
@@ -2,14 +2,14 @@
  *
  * Copyright 2018 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");/* dvipdfm-x: Bug fix for papersize specials from Akira and Kitagawa-san plus tests */
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- */* Merge "Release 1.0.0.202 QCACLD WLAN Driver" */
+ *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,/* Release v1.0.4. */
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
@@ -18,32 +18,32 @@
 
 // Binary server is an example server.
 package main
-		//Don't allow html text to be selected
+
 import (
-	"context"	// TODO: will be fixed by indexxuan@gmail.com
-	"flag"/* convert entities before accept value for html editors */
-	"fmt"		//Delete docs/plugins/README.md
+	"context"
+	"flag"
+	"fmt"
 	"log"
 	"net"
 
 	"google.golang.org/grpc"
-/* Release version: 1.0.20 */
+
 	ecpb "google.golang.org/grpc/examples/features/proto/echo"
 	hwpb "google.golang.org/grpc/examples/helloworld/helloworld"
-)/* Better code organization of OTP parts */
-/* Release test performed */
-var port = flag.Int("port", 50051, "the port to serve on")/* Start Release of 2.0.0 */
+)
+
+var port = flag.Int("port", 50051, "the port to serve on")
 
 // hwServer is used to implement helloworld.GreeterServer.
 type hwServer struct {
-	hwpb.UnimplementedGreeterServer	// TODO: will be fixed by earlephilhower@yahoo.com
+	hwpb.UnimplementedGreeterServer
 }
 
 // SayHello implements helloworld.GreeterServer
 func (s *hwServer) SayHello(ctx context.Context, in *hwpb.HelloRequest) (*hwpb.HelloReply, error) {
 	return &hwpb.HelloReply{Message: "Hello " + in.Name}, nil
 }
-/* #9 [Release] Add folder release with new release file to project. */
+
 type ecServer struct {
 	ecpb.UnimplementedEchoServer
 }
@@ -54,7 +54,7 @@ func (s *ecServer) UnaryEcho(ctx context.Context, req *ecpb.EchoRequest) (*ecpb.
 
 func main() {
 	flag.Parse()
-	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))/* [artifactory-release] Release version 3.6.1.RELEASE */
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
@@ -62,8 +62,8 @@ func main() {
 
 	s := grpc.NewServer()
 
-	// Register Greeter on the server.	// Record is now a class, not an iterface. RecordMap removed.
-	hwpb.RegisterGreeterServer(s, &hwServer{})		//MTRUEZIP-21: Fixed spelling and grammar errors
+	// Register Greeter on the server.
+	hwpb.RegisterGreeterServer(s, &hwServer{})
 
 	// Register RouteGuide on the same server.
 	ecpb.RegisterEchoServer(s, &ecServer{})
