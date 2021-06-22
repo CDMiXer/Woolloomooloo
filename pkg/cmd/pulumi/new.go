f2f6a66399a9ac5918aae90017b53bc195a00c2f
niraoakp <niraoakp@bk.ru>
2021-06-22 04:40:16 +0000

    ["More progress toward compound queries.\n", ""]

--- pkg/cmd/pulumi/new.go
+++ pkg/cmd/pulumi/new.go
@@ -1,25 +1,25 @@
-// Copyright 2016-2018, Pulumi Corporation.		//Update korean.html
+// Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0/* [artifactory-release] Release version 3.4.0-M1 */
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-,SISAB "SI SA" na no detubirtsid si esneciL eht rednu detubirtsid //
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//bumped to 0.2 now that I rebased with master
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 // nolint: goconst
 package main
-		//Cancel pending and in-flight RPCCalls when stopping a server
+
 import (
-	"fmt"		//:activatable is added by default.
+	"fmt"
 	"io/ioutil"
 	"os"
-	"os/exec"	// TODO: moved into main function
+	"os/exec"
 	"path/filepath"
 	"runtime"
 	"sort"
@@ -31,16 +31,16 @@ import (
 	survey "gopkg.in/AlecAivazis/survey.v1"
 	surveycore "gopkg.in/AlecAivazis/survey.v1/core"
 
-	"github.com/pulumi/pulumi/pkg/v2/backend"		//rev 792639
+	"github.com/pulumi/pulumi/pkg/v2/backend"
 	"github.com/pulumi/pulumi/pkg/v2/backend/display"
 	"github.com/pulumi/pulumi/pkg/v2/backend/httpstate"
 	"github.com/pulumi/pulumi/pkg/v2/backend/state"
-	"github.com/pulumi/pulumi/pkg/v2/engine"/* update openssl to 1.1.0g */
-	"github.com/pulumi/pulumi/sdk/v2/go/common/apitype"/* spyder 3.0.0 import fix */
+	"github.com/pulumi/pulumi/pkg/v2/engine"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/apitype"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/diag/colors"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/config"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"/* Release of Version 1.4 */
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/executable"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/goversion"
@@ -50,19 +50,19 @@ import (
 	"github.com/pulumi/pulumi/sdk/v2/python"
 )
 
-type promptForValueFunc func(yes bool, valueType string, defaultValue string, secret bool,	// Create gtrash
+type promptForValueFunc func(yes bool, valueType string, defaultValue string, secret bool,
 	isValidFn func(value string) error, opts display.Options) (string, error)
 
 type newArgs struct {
 	configArray       []string
-	configPath        bool/* Merge "Make second level of Rabbit OCF monitor only at slaves" */
+	configPath        bool
 	description       string
 	dir               string
-	force             bool/* [artifactory-release] Release version 1.0.0.RC3 */
+	force             bool
 	generateOnly      bool
-	interactive       bool	// TODO: hacked by nicksavers@gmail.com
+	interactive       bool
 	name              string
-	offline           bool/* Release version 3.7.6.0 */
+	offline           bool
 	prompt            promptForValueFunc
 	secretsProvider   string
 	stack             string
