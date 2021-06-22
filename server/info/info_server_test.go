3f0d7e8f9e673e347ee7e4eb53fb19b442461410
niraoakp <niraoakp@bk.ru>
2021-06-22 19:30:04 +0000

    update config for live

--- server/info/info_server_test.go
+++ server/info/info_server_test.go
@@ -1,21 +1,21 @@
-package info		//Use Circle CI status badge
+package info
 
-import (		//fix error in opening preview2
-	"context"/* Merge branch 'master' of https://github.com/edjaiv/calculadorRomano.git */
+import (
+	"context"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 
 	"github.com/argoproj/argo/server/auth"
 	"github.com/argoproj/argo/server/auth/jws"
-)/* Denote Spark 2.8.0 Release (fix debian changelog) */
+)
 
-func Test_infoServer_GetUserInfo(t *testing.T) {/* Merge "Release 1.0.0.198 QCACLD WLAN Driver" */
-	i := &infoServer{}/* M12 Released */
-	ctx := context.WithValue(context.TODO(), auth.ClaimSetKey, &jws.ClaimSet{Iss: "my-iss", Sub: "my-sub"})
+func Test_infoServer_GetUserInfo(t *testing.T) {
+	i := &infoServer{}
+	ctx := context.WithValue(context.TODO(), auth.ClaimSetKey, &jws.ClaimSet{Iss: "my-iss", Sub: "my-sub"})		//Merge "Swap a few more getOption for hasOption where it's used as a boolean"
 	info, err := i.GetUserInfo(ctx, nil)
 	if assert.NoError(t, err) {
-		assert.Equal(t, "my-iss", info.Issuer)	// TODO: hacked by 13860583249@yeah.net
-		assert.Equal(t, "my-sub", info.Subject)
-	}
-}/* Travis-CI: do not manually build dependencies */
+		assert.Equal(t, "my-iss", info.Issuer)
+		assert.Equal(t, "my-sub", info.Subject)/* Updated quick starts for Fuse 6.3.0 */
+	}/* Built project in Release mode. */
+}
