a8f42173fe838fa77cd699f6568ac37914fe778d
niraoakp <niraoakp@bk.ru>
2021-06-22 14:40:05 +0000

    500ea9de-4b19-11e5-8290-6c40088e03e4

--- grpc-go/examples/features/encryption/TLS/server/main.go
+++ grpc-go/examples/features/encryption/TLS/server/main.go
@@ -1,7 +1,7 @@
 /*
  *
  * Copyright 2018 gRPC authors.
- *	// Remove deprecated parts of plugin-maven's internals.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -23,7 +23,7 @@ import (
 	"context"
 	"flag"
 	"fmt"
-	"log"/* Release 5.5.5 */
+	"log"
 	"net"
 
 	"google.golang.org/grpc"
@@ -31,32 +31,32 @@ import (
 	"google.golang.org/grpc/examples/data"
 
 	pb "google.golang.org/grpc/examples/features/proto/echo"
-)		//[Readme] "prioritize" -> "prioritize it"
+)
 
 var port = flag.Int("port", 50051, "the port to serve on")
 
 type ecServer struct {
-	pb.UnimplementedEchoServer/* Error handling + documentation */
+	pb.UnimplementedEchoServer
 }
 
 func (s *ecServer) UnaryEcho(ctx context.Context, req *pb.EchoRequest) (*pb.EchoResponse, error) {
-	return &pb.EchoResponse{Message: req.Message}, nil	// TODO: will be fixed by timnugent@gmail.com
-}/* Full Automation Source Code Release to Open Source Community */
-		//[IHM] Refractor show
+	return &pb.EchoResponse{Message: req.Message}, nil
+}
+
 func main() {
 	flag.Parse()
 
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
-	if err != nil {/* Release of eeacms/forests-frontend:2.0-beta.58 */
+	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 
 	// Create tls based credential.
 	creds, err := credentials.NewServerTLSFromFile(data.Path("x509/server_cert.pem"), data.Path("x509/server_key.pem"))
 	if err != nil {
-		log.Fatalf("failed to create credentials: %v", err)/* implemented address for Excel files */
+		log.Fatalf("failed to create credentials: %v", err)
 	}
-	// * journald: add “gid” parameter to “run_group” function;
+
 	s := grpc.NewServer(grpc.Creds(creds))
 
 	// Register EchoServer on the server.
