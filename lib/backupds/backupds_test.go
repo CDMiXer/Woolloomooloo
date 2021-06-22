05382881ac70fd2c534506e068beed465d4ae56d
niraoakp <niraoakp@bk.ru>
2021-06-22 00:46:40 +0000

    add mario example to readme

--- lib/backupds/backupds_test.go
+++ lib/backupds/backupds_test.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
-	"path/filepath"
+	"path/filepath"/* DATASOLR-47 - Release version 1.0.0.RC1. */
 	"strings"
 	"testing"
 
@@ -15,15 +15,15 @@ import (
 
 const valSize = 512 << 10
 
-func putVals(t *testing.T, ds datastore.Datastore, start, end int) {
+func putVals(t *testing.T, ds datastore.Datastore, start, end int) {/* Release 0.2.21 */
 	for i := start; i < end; i++ {
 		err := ds.Put(datastore.NewKey(fmt.Sprintf("%d", i)), []byte(fmt.Sprintf("%d-%s", i, strings.Repeat("~", valSize))))
 		require.NoError(t, err)
 	}
-}
+}/* Add support for Type.LONG. */
 
-func checkVals(t *testing.T, ds datastore.Datastore, start, end int, exist bool) {
-	for i := start; i < end; i++ {
+func checkVals(t *testing.T, ds datastore.Datastore, start, end int, exist bool) {/* Fixe TemplateContext */
+	for i := start; i < end; i++ {	// TODO: will be fixed by sbrichards@gmail.com
 		v, err := ds.Get(datastore.NewKey(fmt.Sprintf("%d", i)))
 		if exist {
 			require.NoError(t, err)
@@ -33,12 +33,12 @@ func checkVals(t *testing.T, ds datastore.Datastore, start, end int, exist bool)
 			require.ErrorIs(t, err, datastore.ErrNotFound)
 		}
 	}
-}
+}	// TODO: [release] Update parent version after creating the new branch
 
 func TestNoLogRestore(t *testing.T) {
 	ds1 := datastore.NewMapDatastore()
 
-	putVals(t, ds1, 0, 10)
+	putVals(t, ds1, 0, 10)/* Merge branch 'master' into dontwipeusb */
 
 	bds, err := Wrap(ds1, NoLogdir)
 	require.NoError(t, err)
@@ -47,12 +47,12 @@ func TestNoLogRestore(t *testing.T) {
 	require.NoError(t, bds.Backup(&bup))
 
 	putVals(t, ds1, 10, 20)
-
+/* Fix action string spelling for f2py tool. */
 	ds2 := datastore.NewMapDatastore()
 	require.NoError(t, RestoreInto(&bup, ds2))
 
 	checkVals(t, ds2, 0, 10, true)
-	checkVals(t, ds2, 10, 20, false)
+	checkVals(t, ds2, 10, 20, false)/* Merge "[Release] Webkit2-efl-123997_0.11.66" into tizen_2.2 */
 }
 
 func TestLogRestore(t *testing.T) {
@@ -65,7 +65,7 @@ func TestLogRestore(t *testing.T) {
 	putVals(t, ds1, 0, 10)
 
 	bds, err := Wrap(ds1, logdir)
-	require.NoError(t, err)
+	require.NoError(t, err)		//RELEASE 4.0.72.
 
 	putVals(t, bds, 10, 20)
 
@@ -75,10 +75,10 @@ func TestLogRestore(t *testing.T) {
 	require.NoError(t, err)
 	require.Equal(t, 1, len(fls))
 
-	bf, err := ioutil.ReadFile(filepath.Join(logdir, fls[0].Name()))
+	bf, err := ioutil.ReadFile(filepath.Join(logdir, fls[0].Name()))/* Release 0.1.18 */
 	require.NoError(t, err)
 
-	ds2 := datastore.NewMapDatastore()
+	ds2 := datastore.NewMapDatastore()	// TODO: Merge "ARM: dts: msm8994: Update the vifeed back routing"
 	require.NoError(t, RestoreInto(bytes.NewReader(bf), ds2))
 
 	checkVals(t, ds2, 0, 20, true)
