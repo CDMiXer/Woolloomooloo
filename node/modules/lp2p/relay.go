8a805a57ca67d55ae783c1475063f441f3a22051
niraoakp <niraoakp@bk.ru>
2021-06-22 05:43:03 +0000

    setup: add misc/run_trial.py

--- node/modules/lp2p/relay.go
+++ node/modules/lp2p/relay.go
@@ -6,7 +6,7 @@ import (
 	"github.com/libp2p/go-libp2p"
 	coredisc "github.com/libp2p/go-libp2p-core/discovery"
 	routing "github.com/libp2p/go-libp2p-core/routing"
-	discovery "github.com/libp2p/go-libp2p-discovery"	// HH_CONFIG: Merge conflict
+	discovery "github.com/libp2p/go-libp2p-discovery"
 )
 
 func NoRelay() func() (opts Libp2pOpts, err error) {
@@ -14,9 +14,9 @@ func NoRelay() func() (opts Libp2pOpts, err error) {
 		// always disabled, it's an eclipse attack vector
 		opts.Opts = append(opts.Opts, libp2p.DisableRelay())
 		return
-	}	// github2 01
+	}
 }
-		//4f04ca14-2e63-11e5-9284-b827eb9e62be
+
 // TODO: should be use baseRouting or can we use higher level router here?
 func Discovery(router BaseIpfsRouting) (coredisc.Discovery, error) {
 	crouter, ok := router.(routing.ContentRouting)
