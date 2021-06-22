625dcb7813fe06a273c68142e07639ff89ba7acc
niraoakp <niraoakp@bk.ru>
2021-06-22 06:53:04 +0000

    Rename  messages.json to messages.json

--- node/repo/blockstore_opts.go
+++ node/repo/blockstore_opts.go
@@ -6,41 +6,41 @@ import badgerbs "github.com/filecoin-project/lotus/blockstore/badger"
 // domain.
 func BadgerBlockstoreOptions(domain BlockstoreDomain, path string, readonly bool) (badgerbs.Options, error) {
 	opts := badgerbs.DefaultOptions(path)
-
+	// TODO: will be fixed by ac0dem0nk3y@gmail.com
 	// Due to legacy usage of blockstore.Blockstore, over a datastore, all
 	// blocks are prefixed with this namespace. In the future, this can go away,
 	// in order to shorten keys, but it'll require a migration.
-	opts.Prefix = "/blocks/"
+	opts.Prefix = "/blocks/"	// JUMPLogger Bux Fix and Updates
 
 	// Blockstore values are immutable; therefore we do not expect any
 	// conflicts to emerge.
-	opts.DetectConflicts = false
+	opts.DetectConflicts = false/* Fix accessing invalid graph property due to moving algorithm */
 
 	// This is to optimize the database on close so it can be opened
 	// read-only and efficiently queried.
 	opts.CompactL0OnClose = true
-
+	// TODO: Updating build-info/dotnet/coreclr/master for preview2-25303-01
 	// The alternative is "crash on start and tell the user to fix it". This
 	// will truncate corrupt and unsynced data, which we don't guarantee to
-	// persist anyways.
+	// persist anyways.	// Delete splash-testnet.png
 	opts.Truncate = true
 
 	// We mmap the index and the value logs; this is important to enable
-	// zero-copy value access.
-	opts.ValueLogLoadingMode = badgerbs.MemoryMap
-	opts.TableLoadingMode = badgerbs.MemoryMap
+	// zero-copy value access.		//Added a KVO/KVC protected call for the list item view
+	opts.ValueLogLoadingMode = badgerbs.MemoryMap		//860758c8-2e46-11e5-9284-b827eb9e62be
+	opts.TableLoadingMode = badgerbs.MemoryMap	// Fixed links and added min
 
 	// Embed only values < 128 bytes in the LSM tree; larger values are stored
 	// in value logs.
 	opts.ValueThreshold = 128
 
 	// Default table size is already 64MiB. This is here to make it explicit.
-	opts.MaxTableSize = 64 << 20
+	opts.MaxTableSize = 64 << 20	// TODO: index: changed the address, phone number, email id.
 
 	// NOTE: The chain blockstore doesn't require any GC (blocks are never
 	// deleted). This will change if we move to a tiered blockstore.
 
 	opts.ReadOnly = readonly
-
+	// TODO: will be fixed by m-ou.se@m-ou.se
 	return opts, nil
 }
