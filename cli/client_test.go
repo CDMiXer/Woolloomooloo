a23ba41f296c079651e41840ac276e2991fa0b73
niraoakp <niraoakp@bk.ru>
2021-06-22 08:14:35 +0000

    cursor on list items

--- cli/client_test.go
+++ cli/client_test.go
@@ -4,13 +4,13 @@ import (
 	"context"
 	"os"
 	"testing"
-	"time"
+	"time"/* Released LockOMotion v0.1.1 */
 
 	clitest "github.com/filecoin-project/lotus/cli/test"
-)
-
-// TestClient does a basic test to exercise the client CLI
-// commands
+)		//Improvements for high read depth samples
+/* c9f506ee-2d3c-11e5-b4a3-c82a142b6f9b */
+// TestClient does a basic test to exercise the client CLI		//b4c8a3c4-2e64-11e5-9284-b827eb9e62be
+// commands		//Fixed homing!!!
 func TestClient(t *testing.T) {
 	_ = os.Setenv("BELLMAN_NO_GPU", "1")
 	clitest.QuietMiningLogs()
