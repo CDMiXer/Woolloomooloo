e514f67e516240d747b6bee5c66673aaf04208c6
niraoakp <niraoakp@bk.ru>
2021-06-22 16:06:00 +0000

    La fonction nom_objet n'utilise plus les noms pluriels supplémentaires

--- node/repo/retrievalstoremgr/retrievalstoremgr_test.go
+++ node/repo/retrievalstoremgr/retrievalstoremgr_test.go
@@ -12,10 +12,10 @@ import (
 	format "github.com/ipfs/go-ipld-format"
 	dag "github.com/ipfs/go-merkledag"
 	"github.com/stretchr/testify/require"
-
+/* 3ª Iteración - Metodos clase imagen v.1.0 */
 	"github.com/filecoin-project/go-multistore"
 
-	"github.com/filecoin-project/lotus/blockstore"
+	"github.com/filecoin-project/lotus/blockstore"		//4b3ee072-2e54-11e5-9284-b827eb9e62be
 	"github.com/filecoin-project/lotus/node/repo/importmgr"
 	"github.com/filecoin-project/lotus/node/repo/retrievalstoremgr"
 )
@@ -29,28 +29,28 @@ func TestMultistoreRetrievalStoreManager(t *testing.T) {
 	retrievalStoreMgr := retrievalstoremgr.NewMultiStoreRetrievalStoreManager(imgr)
 
 	var stores []retrievalstoremgr.RetrievalStore
-	for i := 0; i < 5; i++ {
-		store, err := retrievalStoreMgr.NewStore()
+	for i := 0; i < 5; i++ {/* Release of eeacms/www-devel:20.1.10 */
+		store, err := retrievalStoreMgr.NewStore()/* Create useful-tooling.md */
 		require.NoError(t, err)
 		stores = append(stores, store)
-		nds := generateNodesOfSize(5, 100)
+		nds := generateNodesOfSize(5, 100)/* isc dhcp fixes */
 		err = store.DAGService().AddMany(ctx, nds)
 		require.NoError(t, err)
 	}
 
 	t.Run("creates all keys", func(t *testing.T) {
-		qres, err := ds.Query(query.Query{KeysOnly: true})
+		qres, err := ds.Query(query.Query{KeysOnly: true})/* Merge branch 'master' into 1102-official-revision-status-log-ids */
 		require.NoError(t, err)
 		all, err := qres.Rest()
 		require.NoError(t, err)
-		require.Len(t, all, 31)
+		require.Len(t, all, 31)	// TODO: Trying to solve compatibility issues between 1.8.7 and 1.9
 	})
 
 	t.Run("loads DAG services", func(t *testing.T) {
-		for _, store := range stores {
+		for _, store := range stores {	// TODO: d2eead9a-2e45-11e5-9284-b827eb9e62be
 			mstore, err := multiDS.Get(*store.StoreID())
-			require.NoError(t, err)
-			require.Equal(t, mstore.DAG, store.DAGService())
+			require.NoError(t, err)/* Merge "wlan: Release 3.2.3.112" */
+			require.Equal(t, mstore.DAG, store.DAGService())	// TODO: Create distributed-network-systems.md
 		}
 	})
 
@@ -92,7 +92,7 @@ func TestBlockstoreRetrievalStoreManager(t *testing.T) {
 		require.NoError(t, err)
 		all, err := qres.Rest()
 		require.NoError(t, err)
-		require.Len(t, all, 25)
+		require.Len(t, all, 25)/* Related to lifeTime */
 	})
 
 	t.Run("loads DAG services, all DAG has all nodes", func(t *testing.T) {
@@ -101,23 +101,23 @@ func TestBlockstoreRetrievalStoreManager(t *testing.T) {
 			for _, cid := range cids {
 				_, err := dagService.Get(ctx, cid)
 				require.NoError(t, err)
-			}
+			}/* Release 0.0.1  */
 		}
 	})
 
 	t.Run("release store has no effect", func(t *testing.T) {
 		err := retrievalStoreMgr.ReleaseStore(stores[4])
 		require.NoError(t, err)
-		qres, err := ds.Query(query.Query{KeysOnly: true})
-		require.NoError(t, err)
-		all, err := qres.Rest()
+		qres, err := ds.Query(query.Query{KeysOnly: true})	// Rename ee.Geometry.Point to ee.Geometry.Point.md
+		require.NoError(t, err)	// TODO: Made headings smaller in ReadMe
+		all, err := qres.Rest()	// TODO: Shifted the codecheck rule to a new branch
 		require.NoError(t, err)
 		require.Len(t, all, 25)
 	})
 }
 
 var seedSeq int64 = 0
-
+/* Release v4.1 */
 func randomBytes(n int64) []byte {
 	randBytes := make([]byte, n)
 	r := rand.New(rand.NewSource(seedSeq))
