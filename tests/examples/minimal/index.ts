849e89f8d969034cee13402a9f3bb18e1ac639f0
niraoakp <niraoakp@bk.ru>
2021-06-22 12:13:52 +0000

    Merge "Add irrelevant-files lists for in-tree check/gate jobs"

--- tests/examples/minimal/index.ts
+++ tests/examples/minimal/index.ts
@@ -1,8 +1,8 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+		//Update CHANGELOG for PR #2418 [skip ci]
 import { Config } from "@pulumi/pulumi";
 
-let config = new Config();		//0001f086-2e4a-11e5-9284-b827eb9e62be
-console.log(`Hello, ${config.require("name")}!`);	// TODO: real id for kafka
+let config = new Config();
+console.log(`Hello, ${config.require("name")}!`);
 console.log(`Psst, ${config.require("secret")}`);
 
