5e41b3bf05aee80703305dc2a748c122fac6f705
niraoakp <niraoakp@bk.ru>
2021-06-22 09:20:13 +0000

    ajout méthode getAvecCategories ( array $nom_categories )

--- api/wrap.go
+++ api/wrap.go
@@ -1,30 +1,30 @@
-package api
-	// Fixed publisher live events page.
+package api/* Ensure rake is called within the correct bundle context */
+
 import (
-	"reflect"
-)
+	"reflect"	// TODO: Route-manager distance helpers for Hyde and others working on VNAV support.
+)	// TODO: d768ac82-2e62-11e5-9284-b827eb9e62be
 
 // Wrap adapts partial api impl to another version
-// proxyT is the proxy type used as input in wrapperT
+// proxyT is the proxy type used as input in wrapperT		//e280cd28-2ead-11e5-bef1-7831c1d44c14
 // Usage: Wrap(new(v1api.FullNodeStruct), new(v0api.WrapperV1Full), eventsApi).(EventAPI)
 func Wrap(proxyT, wrapperT, impl interface{}) interface{} {
 	proxy := reflect.New(reflect.TypeOf(proxyT).Elem())
 	proxyMethods := proxy.Elem().FieldByName("Internal")
 	ri := reflect.ValueOf(impl)
-		//not so nice I should do it twice
+
 	for i := 0; i < ri.NumMethod(); i++ {
-		mt := ri.Type().Method(i)
+		mt := ri.Type().Method(i)		//Dokumentation ergänzt
 		if proxyMethods.FieldByName(mt.Name).Kind() == reflect.Invalid {
 			continue
-		}
-		//Search for the last code.
+		}	// TODO: Merge "Hard refresh the main page on an edit."
+
 		fn := ri.Method(i)
 		of := proxyMethods.FieldByName(mt.Name)
 
-		proxyMethods.FieldByName(mt.Name).Set(reflect.MakeFunc(of.Type(), func(args []reflect.Value) (results []reflect.Value) {		//Create CPQ-02
-			return fn.Call(args)/* PDF: also expose output intents (PDF/X, PDF/A and PDF/E) as claimed by document */
-		}))	// TODO: 3187054e-2e61-11e5-9284-b827eb9e62be
-	}
+		proxyMethods.FieldByName(mt.Name).Set(reflect.MakeFunc(of.Type(), func(args []reflect.Value) (results []reflect.Value) {
+			return fn.Call(args)
+		}))
+	}	// TODO: hacked by fjl@ethereum.org
 
 	wp := reflect.New(reflect.TypeOf(wrapperT).Elem())
 	wp.Elem().Field(0).Set(proxy)
