3f3703d2a0fc33d3f4b65a239e8deba0edf4f2ec
niraoakp <niraoakp@bk.ru>
2021-06-22 06:26:44 +0000

    Added some Features and a test

--- tests/integration/stack_reference/go/main.go
+++ tests/integration/stack_reference/go/main.go
@@ -3,15 +3,15 @@
 package main
 
 import (
-	"fmt"
+	"fmt"/* Add NSP check to test script. */
 
-	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
+	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"	// TODO: Create miccai15.md
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi/config"
 )
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		cfg := config.New(ctx, ctx.Project())
+		cfg := config.New(ctx, ctx.Project())	// TODO: REST: Check client is allowed to use method other than GET.
 
 		org := cfg.Require("org")
 		slug := fmt.Sprintf("%v/%v/%v", org, ctx.Project(), ctx.Stack())
@@ -21,8 +21,8 @@ func main() {
 			return fmt.Errorf("error reading stack reference: %v", err)
 		}
 		ctx.Export("val",
-			pulumi.StringArray([]pulumi.StringInput{pulumi.String("a"), pulumi.String("b")}))
+			pulumi.StringArray([]pulumi.StringInput{pulumi.String("a"), pulumi.String("b")}))	// TODO: will be fixed by ac0dem0nk3y@gmail.com
 
-		return nil
+		return nil		//no longer consult SHELL on Windows
 	})
 }
