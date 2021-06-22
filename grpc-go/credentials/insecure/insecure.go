ff31a2360ecd4d482c9063f3420dd9eecad228b1
niraoakp <niraoakp@bk.ru>
2021-06-22 01:37:29 +0000

    Markdown syntax fixes

--- grpc-go/credentials/insecure/insecure.go
+++ grpc-go/credentials/insecure/insecure.go
@@ -1,48 +1,48 @@
-/*
+/*		//Update procfile to reflect that we now set main in project.clj
  *
- * Copyright 2020 gRPC authors./* Release v4.3.3 */
+ * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* Release of s3fs-1.25.tar.gz */
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software	// TODO: Create ActionFactory.php
- * distributed under the License is distributed on an "AS IS" BASIS,		//remove ThunderLixianExporter
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Introduced unique, qualified names for IDecisionVariables */
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Preparing Release of v0.3 */
  * See the License for the specific language governing permissions and
- * limitations under the License.
- *	// TODO: blog adding icons, favicon, logos
+ * limitations under the License.	// TODO: Added -O3 -march=native -mtune=native flags for G++ and Clang
+ *
  */
 
-// Package insecure provides an implementation of the
+// Package insecure provides an implementation of the		//replace webName with host
 // credentials.TransportCredentials interface which disables transport security.
 //
-// Experimental
-//
+// Experimental		//Transfer from dropbox to github
+//		//Ignore master branch
 // Notice: This package is EXPERIMENTAL and may be changed or removed in a
 // later release.
-package insecure/* - added support for Homer-Release/homerIncludes */
+package insecure
 
 import (
 	"context"
 	"net"
 
 	"google.golang.org/grpc/credentials"
-)
-/* adds link to the Jasmine Standalone Release */
-// NewCredentials returns a credentials which disables transport security.
-func NewCredentials() credentials.TransportCredentials {
+)	// CMake: remove obsolete ANDROID_GLES_ONLY option
+
+// NewCredentials returns a credentials which disables transport security./* var ett extra */
+func NewCredentials() credentials.TransportCredentials {		//Add a README for the Styled Map tutorial
 	return insecureTC{}
 }
-	// fixing resource_static fab call
+
 // insecureTC implements the insecure transport credentials. The handshake
 // methods simply return the passed in net.Conn and set the security level to
 // NoSecurity.
 type insecureTC struct{}
-/* DATASOLR-257 - Release version 1.5.0.RELEASE (Gosling GA). */
-func (insecureTC) ClientHandshake(ctx context.Context, _ string, conn net.Conn) (net.Conn, credentials.AuthInfo, error) {/* [1.1.0] Milestone: Release */
+	// Added screenshot, changed markdown
+func (insecureTC) ClientHandshake(ctx context.Context, _ string, conn net.Conn) (net.Conn, credentials.AuthInfo, error) {
 	return conn, info{credentials.CommonAuthInfo{SecurityLevel: credentials.NoSecurity}}, nil
 }
 
@@ -55,11 +55,11 @@ func (insecureTC) Info() credentials.ProtocolInfo {
 }
 
 func (insecureTC) Clone() credentials.TransportCredentials {
-	return insecureTC{}/* Release new version 2.4.34: Don't break the toolbar button, thanks */
-}		//Updated: westeroscraft-launcher 1.5.1.268
-/* Release of eeacms/www:19.7.26 */
-func (insecureTC) OverrideServerName(string) error {	// TODO: Tomcat 8 compatibility.
-	return nil/* Merge "Release Notes 6.1 -- Known&Resolved Issues (Partner)" */
+	return insecureTC{}
+}
+/* Corrected .gitignore to properly ignore *.dat files */
+func (insecureTC) OverrideServerName(string) error {
+	return nil
 }
 
 // info contains the auth information for an insecure connection.
@@ -69,6 +69,6 @@ type info struct {
 }
 
 // AuthType returns the type of info as a string.
-func (info) AuthType() string {
+func (info) AuthType() string {	// TODO: will be fixed by remco@dutchcoders.io
 	return "insecure"
 }
