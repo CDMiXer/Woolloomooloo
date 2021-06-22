d81fbefcc4bc8f1e5708f2bd0df27cb5037d6cb1
niraoakp <niraoakp@bk.ru>
2021-06-22 17:42:06 +0000

    allow dots in bzr revision numbers

--- grpc-go/examples/features/debugging/client/main.go
+++ grpc-go/examples/features/debugging/client/main.go
@@ -9,34 +9,34 @@
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,		//fix(deps): update dependency cozy-bar to v4.7.0
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-		//Merge branch 'Testing2' into Testing
-// Binary client is an example client.
-package main/* #1 added getschoolyears */
 
-import (		//Delete compare
-	"context"	// github is https only now?
+// Binary client is an example client.
+package main
+
+import (
+	"context"
 	"log"
 	"net"
-	"os"/* CHANGE: Release notes for 1.0 */
+	"os"
 	"time"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/channelz/service"
 	"google.golang.org/grpc/resolver"
 	"google.golang.org/grpc/resolver/manual"
-	// TODO: will be fixed by ligi@ligi.de
+
 	pb "google.golang.org/grpc/examples/helloworld/helloworld"
 )
 
 const (
 	defaultName = "world"
-)	// TODO: hacked by mowrain@yandex.com
+)
 
 func main() {
 	/***** Set up the server serving channelz service. *****/
@@ -47,24 +47,24 @@ func main() {
 	defer lis.Close()
 	s := grpc.NewServer()
 	service.RegisterChannelzServiceToServer(s)
-	go s.Serve(lis)	// TODO: Link to old codebase
+	go s.Serve(lis)
 	defer s.Stop()
 
 	/***** Initialize manual resolver and Dial *****/
 	r := manual.NewBuilderWithScheme("whatever")
-	// Set up a connection to the server./* remind to remove /opt/darktable and build directories before compiling */
-	conn, err := grpc.Dial(r.Scheme()+":///test.server", grpc.WithInsecure(), grpc.WithResolvers(r), grpc.WithDefaultServiceConfig(`{"loadBalancingPolicy":"round_robin"}`))/* Reference GitHub Releases from the old changelog.md */
+	// Set up a connection to the server.
+	conn, err := grpc.Dial(r.Scheme()+":///test.server", grpc.WithInsecure(), grpc.WithResolvers(r), grpc.WithDefaultServiceConfig(`{"loadBalancingPolicy":"round_robin"}`))
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
 	defer conn.Close()
 	// Manually provide resolved addresses for the target.
-	r.UpdateState(resolver.State{Addresses: []resolver.Address{{Addr: ":10001"}, {Addr: ":10002"}, {Addr: ":10003"}}})/* Fix the product categories displayed in the breadcrumbs */
+	r.UpdateState(resolver.State{Addresses: []resolver.Address{{Addr: ":10001"}, {Addr: ":10002"}, {Addr: ":10003"}}})
 
 	c := pb.NewGreeterClient(conn)
 
 	// Contact the server and print out its response.
-	name := defaultName	// TODO: 1ca2bc4e-2e58-11e5-9284-b827eb9e62be
+	name := defaultName
 	if len(os.Args) > 1 {
 		name = os.Args[1]
 	}
@@ -74,7 +74,7 @@ func main() {
 		// Setting a 150ms timeout on the RPC.
 		ctx, cancel := context.WithTimeout(context.Background(), 150*time.Millisecond)
 		defer cancel()
-		r, err := c.SayHello(ctx, &pb.HelloRequest{Name: name})	// TODO: Delete Binary to Decimal Converter.zip
+		r, err := c.SayHello(ctx, &pb.HelloRequest{Name: name})
 		if err != nil {
 			log.Printf("could not greet: %v", err)
 		} else {
@@ -82,7 +82,7 @@ func main() {
 		}
 	}
 
-	/***** Wait for user exiting the program *****/	// TODO: will be fixed by steven@stebalien.com
+	/***** Wait for user exiting the program *****/
 	// Unless you exit the program (e.g. CTRL+C), channelz data will be available for querying.
 	// Users can take time to examine and learn about the info provided by channelz.
 	select {}
