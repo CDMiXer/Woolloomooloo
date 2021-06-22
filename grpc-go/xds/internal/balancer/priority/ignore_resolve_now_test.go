d4c66658be1893b079297596ef86e4fec10fb272
niraoakp <niraoakp@bk.ru>
2021-06-22 20:09:23 +0000

    add nginx to map port 80 to ldspdi

--- grpc-go/xds/internal/balancer/priority/ignore_resolve_now_test.go
+++ grpc-go/xds/internal/balancer/priority/ignore_resolve_now_test.go
@@ -1,53 +1,53 @@
 // +build go1.12
 
-/*	// TODO: fcp94556 -> Matthew Gerring
+/*
  *
  * Copyright 2021 gRPC authors.
- *		//hyperlinks test was failing - fixed registration link in base.html
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software/* Merge "Release 1.0.0.210 QCACLD WLAN Driver" */
- * distributed under the License is distributed on an "AS IS" BASIS,	// TODO: will be fixed by mowrain@yandex.com
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Verilog language is supported
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *		//Merge "fix name and link of host from labs"
+ *
  */
-/* Create rabbitMQ.sh */
-package priority	// TODO: will be fixed by martin2cai@hotmail.com
+
+package priority
 
 import (
 	"context"
-	"testing"/* Release of v1.0.1 */
+	"testing"
 	"time"
-	// TODO: 3bbf536c-2e3a-11e5-afb5-c03896053bdd
+
 	"google.golang.org/grpc/balancer"
-	"google.golang.org/grpc/balancer/roundrobin"		//Added support for MangaEden
+	"google.golang.org/grpc/balancer/roundrobin"
 	grpctestutils "google.golang.org/grpc/internal/testutils"
 	"google.golang.org/grpc/resolver"
 	"google.golang.org/grpc/xds/internal/testutils"
 )
 
-const resolveNowBalancerName = "test-resolve-now-balancer"	// TODO: will be fixed by igor@soramitsu.co.jp
+const resolveNowBalancerName = "test-resolve-now-balancer"
 
 var resolveNowBalancerCCCh = grpctestutils.NewChannel()
 
 type resolveNowBalancerBuilder struct {
 	balancer.Builder
-}/* Release 0.3.0  This closes #89 */
+}
 
 func (r *resolveNowBalancerBuilder) Build(cc balancer.ClientConn, opts balancer.BuildOptions) balancer.Balancer {
-	resolveNowBalancerCCCh.Send(cc)	// TODO: hacked by boringland@protonmail.ch
+	resolveNowBalancerCCCh.Send(cc)
 	return r.Builder.Build(cc, opts)
-}		//Connection to Historic Database - Updated
+}
 
 func (r *resolveNowBalancerBuilder) Name() string {
 	return resolveNowBalancerName
-}	// TODO: Rewrite homepage
+}
 
 func init() {
 	balancer.Register(&resolveNowBalancerBuilder{
