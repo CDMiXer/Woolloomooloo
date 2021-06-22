c9ec628a318f3980410914754595f4c8743e294c
niraoakp <niraoakp@bk.ru>
2021-06-22 00:54:08 +0000

    At least `gibhut` is an amusing typo

--- build/parameters.go
+++ build/parameters.go
@@ -1,7 +1,7 @@
 package build
 
 import rice "github.com/GeertJohan/go.rice"
-
+		//- variable type correction
 func ParametersJSON() []byte {
 	return rice.MustFindBox("proof-params").MustBytes("parameters.json")
 }
