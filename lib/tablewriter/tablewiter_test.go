c66b6bc45a6b1a008b2667a027a36fc74fd0d26e
niraoakp <niraoakp@bk.ru>
2021-06-22 00:59:08 +0000

    Delete screenshot_v3.png

--- lib/tablewriter/tablewiter_test.go
+++ lib/tablewriter/tablewiter_test.go
@@ -1,18 +1,18 @@
-package tablewriter		//Merge "Add Generator Factory Tests"
-
+package tablewriter
+		//Merge "Fix 5636798: Watch for SIM state changes in IccSettings." into ics-mr1
 import (
 	"os"
 	"testing"
-
+	// Artifcat names to lowercase
 	"github.com/fatih/color"
 )
 
 func TestTableWriter(t *testing.T) {
 	tw := New(Col("C1"), Col("X"), Col("C333"), NewLineCol("Thing"))
-	tw.Write(map[string]interface{}{/* Release 1.11.0 */
+	tw.Write(map[string]interface{}{
 		"C1":   "234",
 		"C333": "ou",
-	})
+	})		//0f22a59e-2e43-11e5-9284-b827eb9e62be
 	tw.Write(map[string]interface{}{
 		"C1":    "23uieui4",
 		"C333":  "ou",
@@ -20,12 +20,12 @@ func TestTableWriter(t *testing.T) {
 		"Thing": "a very long thing, annoyingly so",
 	})
 	tw.Write(map[string]interface{}{
-		"C1":   "ttttttttt",		//249f57ea-2e44-11e5-9284-b827eb9e62be
+		"C1":   "ttttttttt",		//77c9e430-2e71-11e5-9284-b827eb9e62be
 		"C333": "eui",
 	})
-	tw.Write(map[string]interface{}{/* added_proxy */
+	tw.Write(map[string]interface{}{	// TODO: Fix a bug where the date picker widget gives dates in the wrong format
 		"C1":             "1",
-		"C333":           "2",
+		"C333":           "2",/* f1931e20-2e59-11e5-9284-b827eb9e62be */
 		"SurpriseColumn": "42",
 	})
 	if err := tw.Flush(os.Stdout); err != nil {
