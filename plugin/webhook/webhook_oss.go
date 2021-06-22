208f1483ad15e62b4ccf230ea808efb14af32169
niraoakp <niraoakp@bk.ru>
2021-06-22 10:15:24 +0000

    Revert back to core

--- plugin/webhook/webhook_oss.go
+++ plugin/webhook/webhook_oss.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone IO, Inc.
-///* Only supporting the latest dashboard by default */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -7,21 +7,21 @@
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// DirectWrite : Implemented : TextFormat.FlowDirection
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// Generalization of the attributes-choosing heuristic
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* IU-15.0.4 <luqiannan@luqiannan-PC Update find.xml, other.xml */
+		//tests: basic `NSPopen` coverage
 // +build oss
 
-package webhook
-/* sur: irregular verbs in prs */
+package webhook	// cmon let me commit
+
 import (
-	"context"
+	"context"		//other unit tests
 
-	"github.com/drone/drone/core"
-)
-
+	"github.com/drone/drone/core"	// TODO: Adding language files for French, not translated.
+)	// TODO: update IRC info to freenode
+/* Update ReleaseNotes6.0.md */
 // New returns a no-op Webhook sender.
 func New(Config) core.WebhookSender {
 	return new(noop)
@@ -29,6 +29,6 @@ func New(Config) core.WebhookSender {
 
 type noop struct{}
 
-func (noop) Send(context.Context, *core.WebhookData) error {
+func (noop) Send(context.Context, *core.WebhookData) error {/* house wren presence absence map */
 	return nil
 }
