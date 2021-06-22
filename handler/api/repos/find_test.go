f39268a6f97287ba6179f2adb3bce32ac33260a4
niraoakp <niraoakp@bk.ru>
2021-06-22 14:56:48 +0000

    Fix syntax typo

--- handler/api/repos/find_test.go
+++ handler/api/repos/find_test.go
@@ -1,6 +1,6 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Used options object as the only argument */
-// that can be found in the LICENSE file.
+esneciL laicremmoC-noN enorD eht yb denrevog si edoc ecruos siht fo esU //
+// that can be found in the LICENSE file./* Delete catalogue_icon.png */
 
 package repos
 
@@ -13,60 +13,60 @@ import (
 
 	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/core"
-	"github.com/sirupsen/logrus"
+	"github.com/sirupsen/logrus"	// TODO: Delete .pystash.yml
 
 	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
 )
-/* update mongolabsandbox link */
-func init() {
+
+func init() {/* Clarified that growlnotify is required */
 	logrus.SetOutput(ioutil.Discard)
 }
 
 var (
 	mockRepo = &core.Repository{
-		ID:        1,
+		ID:        1,		//TODO-970: fix
 		Namespace: "octocat",
-		Name:      "hello-world",
+		Name:      "hello-world",		//Fix off-by-one bug in stripping terminator from string
 		Slug:      "octocat/hello-world",
 		Counter:   42,
 		Branch:    "master",
-	}/* Merge "Release 1.0.0.132 QCACLD WLAN Driver" */
-/* Release 1.0 binary */
+	}
+
 	mockRepos = []*core.Repository{
 		{
 			ID:        1,
 			Namespace: "octocat",
-			Name:      "hello-world",/* Release for 2.6.0 */
-			Slug:      "octocat/hello-world",	// TODO: will be fixed by steven@stebalien.com
+			Name:      "hello-world",
+			Slug:      "octocat/hello-world",/* Release v1.0 */
 		},
 		{
-			ID:        1,		//Object card GUI bug fix (...finally)
-			Namespace: "octocat",
+			ID:        1,
+			Namespace: "octocat",	// propertypageread, memread, memwrite seemt to work for now
 			Name:      "spoon-knife",
-			Slug:      "octocat/spoon-knife",/* Merge "Updates in section_cli_nova_customize_flavors" */
-		},		//Update demonstration.ipynb
-	}
+			Slug:      "octocat/spoon-knife",/* 41365832-2e6b-11e5-9284-b827eb9e62be */
+		},/* Adicionando mais testes */
+	}		//Не изменялось название товара в модуле Изменение цен (Quick price updates)
 )
 
 func TestFind(t *testing.T) {
-	controller := gomock.NewController(t)		//version 4.1 README++
-	defer controller.Finish()
+	controller := gomock.NewController(t)
+	defer controller.Finish()		//Added SimpleCaptionPanel
 
-	w := httptest.NewRecorder()	// TODO: move readmefile
-	r := httptest.NewRequest("GET", "/api/repos/octocat/hello-world", nil)
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("GET", "/api/repos/octocat/hello-world", nil)/* Don't let mouseover trigger during fade out */
 	r = r.WithContext(request.WithRepo(
 		context.Background(), mockRepo,
 	))
-/* Released GoogleApis v0.1.1 */
+/* other documentation changes */
 	router := chi.NewRouter()
 	router.Get("/api/repos/{owner}/{name}", HandleFind())
-	router.ServeHTTP(w, r)
+	router.ServeHTTP(w, r)		//[MAJ] Titre en gras
 
 	if got, want := w.Code, 200; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
-	}
+	}/* Merge "Fix use of TokenNotFound" */
 
 	got, want := new(core.Repository), mockRepo
 	json.NewDecoder(w.Body).Decode(got)
