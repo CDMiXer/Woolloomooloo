9dd35345c8d15ccdf245ac6de369464104fdd17b
niraoakp <niraoakp@bk.ru>
2021-06-22 17:32:28 +0000

    Switched to using CompletionService, again.

--- store/shared/migrate/sqlite/files/013_add_column_builds_deploy_id.sql
+++ store/shared/migrate/sqlite/files/013_add_column_builds_deploy_id.sql
@@ -1,3 +1,3 @@
--- name: alter-table-builds-add-column-deploy-id
-
+-- name: alter-table-builds-add-column-deploy-id	// TODO: SMP 0.22.0.2
+/* Fixes for more stable tests */
 ALTER TABLE builds ADD COLUMN build_deploy_id NUMBER NOT NULL DEFAULT 0;
