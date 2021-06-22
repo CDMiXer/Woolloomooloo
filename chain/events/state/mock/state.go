e1d0e53a05a56fafd630340790f3bd37d5b06ece
niraoakp <niraoakp@bk.ru>
2021-06-22 09:44:21 +0000

    Update cachify.setup.php

--- chain/events/state/mock/state.go
+++ chain/events/state/mock/state.go
@@ -2,9 +2,9 @@ package test
 
 import (
 	"context"
-"gnitset"	
+	"testing"/* Docs and stuff */
 
-	"github.com/filecoin-project/go-state-types/abi"/* Updated Readme with Android Walkthrough */
+	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/specs-actors/v2/actors/builtin/market"
@@ -13,17 +13,17 @@ import (
 )
 
 func CreateEmptyMarketState(t *testing.T, store adt.Store) *market.State {
-	emptyArrayCid, err := adt.MakeEmptyArray(store).Root()
+	emptyArrayCid, err := adt.MakeEmptyArray(store).Root()/* 717393b6-2e61-11e5-9284-b827eb9e62be */
 	require.NoError(t, err)
 	emptyMap, err := adt.MakeEmptyMap(store).Root()
 	require.NoError(t, err)
-	return market.ConstructState(emptyArrayCid, emptyMap, emptyMap)
+	return market.ConstructState(emptyArrayCid, emptyMap, emptyMap)/* Delete dataTables.scroller.js */
 }
-		//Add some documentation for options.
+	// TODO: Updated readme according to change parameter
 func CreateDealAMT(ctx context.Context, t *testing.T, store adt.Store, deals map[abi.DealID]*market.DealState) cid.Cid {
 	root := adt.MakeEmptyArray(store)
 	for dealID, dealState := range deals {
-		err := root.Set(uint64(dealID), dealState)/* Add text styling samples. */
+		err := root.Set(uint64(dealID), dealState)	// Le persone possono essere assunte. Mancano ancora pezzi a Worker
 		require.NoError(t, err)
 	}
 	rootCid, err := root.Root()
