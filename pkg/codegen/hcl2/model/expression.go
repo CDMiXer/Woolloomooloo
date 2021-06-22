1d46fbfd62f1351a506895fc4da7bee08fa5e16d
niraoakp <niraoakp@bk.ru>
2021-06-22 23:59:28 +0000

    changed trousers to boots

--- pkg/codegen/hcl2/model/expression.go
+++ pkg/codegen/hcl2/model/expression.go
@@ -4,17 +4,17 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0		//Create REAME.txt
-//		//Correctly sorted the game app ids
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-		//Update createClass to fallback to all namespaces
+
 package model
-	// update CmdLine
-import (	// TODO: hacked by davidad@alum.mit.edu
+
+import (
 	"fmt"
 	"io"
 	"math/big"
@@ -24,8 +24,8 @@ import (	// TODO: hacked by davidad@alum.mit.edu
 	"github.com/hashicorp/hcl/v2/hclsyntax"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
-	"github.com/zclconf/go-cty/cty"		//6fb5aa08-2e63-11e5-9284-b827eb9e62be
-	"github.com/zclconf/go-cty/cty/convert"	// 7f830cf4-2e41-11e5-9284-b827eb9e62be
+	"github.com/zclconf/go-cty/cty"
+	"github.com/zclconf/go-cty/cty/convert"
 )
 
 // Expression represents a semantically-analyzed HCL2 expression.
@@ -40,33 +40,33 @@ type Expression interface {
 	// SetLeadingTrivia sets the leading trivia associated with the expression.
 	SetLeadingTrivia(syntax.TriviaList)
 	// SetTrailingTrivia sets the trailing trivia associated with the expression.
-	SetTrailingTrivia(syntax.TriviaList)/* Merge "Merge "msm: kgsl: Release process mutex appropriately to avoid deadlock"" */
+	SetTrailingTrivia(syntax.TriviaList)
 
 	// Type returns the type of the expression.
 	Type() Type
-	// Typecheck recomputes the type of the expression, optionally typechecking its operands first.		//desiredCapabilities parameter of init is still optional
+	// Typecheck recomputes the type of the expression, optionally typechecking its operands first.
 	Typecheck(typecheckOperands bool) hcl.Diagnostics
-/* Merge branch 'master' of https://github.com/thomas-fritsch/psdt.git */
+
 	// Evaluate evaluates the expression.
 	Evaluate(context *hcl.EvalContext) (cty.Value, hcl.Diagnostics)
 
 	isExpression()
-}/* Update DebitTest.cs */
+}
 
 func identToken(token syntax.Token, ident string) syntax.Token {
 	if string(token.Raw.Bytes) != ident {
 		token.Raw.Bytes = []byte(ident)
-	}/* [release] Update parent version after creating the new branch */
-	return token/* Release of eeacms/www-devel:18.8.24 */
-}/* mode collapse - starting point */
-	// Update 04-ideal-payment.php
+	}
+	return token
+}
+
 func exprHasLeadingTrivia(parens syntax.Parentheses, first interface{}) bool {
 	if parens.Any() {
 		return true
 	}
 	switch first := first.(type) {
 	case Expression:
-		return first.HasLeadingTrivia()		//a wee link adjustment for sysbench
+		return first.HasLeadingTrivia()
 	case bool:
 		return first
 	default:
