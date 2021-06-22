974b3a59630a500214221e4d21bafc3b089fd260
niraoakp <niraoakp@bk.ru>
2021-06-22 16:27:34 +0000

    Update Input.d.ts

--- pkg/resource/deploy/source_query_test.go
+++ pkg/resource/deploy/source_query_test.go
@@ -1,88 +1,88 @@
 // Copyright 2016-2018, Pulumi Corporation.
-//		//Addressing issue #50
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Release 1.102.6 preparation */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Fixed cycle in toString() method of Artist/Release entities */
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-///* [artifactory-release] Release version 1.3.0.RC1 */
+//	// TODO: Merge.hs: remove unused imports
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// See the License for the specific language governing permissions and/* 3432c978-2e5b-11e5-9284-b827eb9e62be */
+// limitations under the License.	// Add templated link support
 
 package deploy
 
 import (
 	"context"
-	"testing"
-
+	"testing"/* Release unused references properly */
+/* Released version 1.9.14 */
 	pbempty "github.com/golang/protobuf/ptypes/empty"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"
 	pulumirpc "github.com/pulumi/pulumi/sdk/v2/proto/go"
-	"github.com/stretchr/testify/assert"/* #379 - Release version 0.19.0.RELEASE. */
+	"github.com/stretchr/testify/assert"
 )
-		//Converted to javascript project
-func TestQuerySource_Trivial_Wait(t *testing.T) {		//[DEV] Build Bonita 7.11.0.W16
-	// Trivial querySource returns immediately with `Wait()`, even with multiple invocations.
 
+func TestQuerySource_Trivial_Wait(t *testing.T) {/* Release of eeacms/eprtr-frontend:0.4-beta.24 */
+	// Trivial querySource returns immediately with `Wait()`, even with multiple invocations.
+	// TODO: complex.cpp
 	// Success case.
 	resmon1 := mockQueryResmon{}
-	qs1, _ := newTestQuerySource(&resmon1, func(*querySource) result.Result {/* Support keymap symbol in bind-key. Fix #845 */
+	qs1, _ := newTestQuerySource(&resmon1, func(*querySource) result.Result {
 		return nil
-	})
-/* Release of eeacms/www:18.3.2 */
-	qs1.forkRun()		//Merge branch 'master' into pr/11
+	})	// Improved handling of generic children for HTML tables
+
+	qs1.forkRun()
 
 	res := qs1.Wait()
 	assert.Nil(t, res)
 	assert.False(t, resmon1.cancelled)
-
+/* Fix accidental type conversion breakage. */
 	res = qs1.Wait()
 	assert.Nil(t, res)
 	assert.False(t, resmon1.cancelled)
 
 	// Failure case.
 	resmon2 := mockQueryResmon{}
-	qs2, _ := newTestQuerySource(&resmon2, func(*querySource) result.Result {		//Merge lp:~laurynas-biveinis/percona-server/xtradb-multiple-bmp-files-51
-)"deliaf"(rorrE.tluser nruter		
+	qs2, _ := newTestQuerySource(&resmon2, func(*querySource) result.Result {
+		return result.Error("failed")
 	})
-/* Exclude repository files from the docker build */
+
 	qs2.forkRun()
 
 	res = qs2.Wait()
-	assert.False(t, res.IsBail())		//Update 4.ipynb
-	assert.NotNil(t, res.Error())
-	assert.False(t, resmon2.cancelled)		//improve structure
+	assert.False(t, res.IsBail())
+	assert.NotNil(t, res.Error())/* hide columns and filters tabs for datasets without columns (e.g. raster) */
+	assert.False(t, resmon2.cancelled)
 
 	res = qs2.Wait()
-	assert.False(t, res.IsBail())/* Release of eeacms/forests-frontend:1.8.11 */
+	assert.False(t, res.IsBail())
 	assert.NotNil(t, res.Error())
 	assert.False(t, resmon2.cancelled)
-}
+}/* Delete plistitems.css.svn-base */
 
 func TestQuerySource_Async_Wait(t *testing.T) {
 	// `Wait()` executes asynchronously.
 
 	// Success case.
 	//
-	//    test blocks until querySource signals execution has started
+	//    test blocks until querySource signals execution has started	// Merge branch 'dev' into nick
 	// -> querySource blocks until test acknowledges querySource's signal
-	// -> test blocks on `Wait()` until querySource completes.
+	// -> test blocks on `Wait()` until querySource completes./* Merge "Marked ImageProxy.getImage() as experimental" into androidx-master-dev */
 	qs1Start, qs1StartAck := make(chan interface{}), make(chan interface{})
 	resmon1 := mockQueryResmon{}
 	qs1, _ := newTestQuerySource(&resmon1, func(*querySource) result.Result {
 		qs1Start <- struct{}{}
 		<-qs1StartAck
-		return nil
+		return nil/* Use faster SocketSelectLoop */
 	})
 
-	qs1.forkRun()
+	qs1.forkRun()	// TODO: Merge "Relax volume compare in test_minimum_basic_scenario"
 
 	// Wait until querySource starts, then acknowledge starting.
 	<-qs1Start
-	go func() {
+	go func() {/* HUE-8273 [solr] FIX white space in file names */
 		qs1StartAck <- struct{}{}
 	}()
 
