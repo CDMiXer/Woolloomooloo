dc10c7aa179ce0bb639b581b07d69914a3f9cd41
niraoakp <niraoakp@bk.ru>
2021-06-22 01:15:58 +0000

    various application helpers

--- pkg/codegen/internal/test/testdata/kubernetes-template.pp.cs
+++ pkg/codegen/internal/test/testdata/kubernetes-template.pp.cs
@@ -1,15 +1,15 @@
 using Pulumi;
 using Kubernetes = Pulumi.Kubernetes;
 
-class MyStack : Stack
-{
+class MyStack : Stack	// Complete changes.
+{		//[#41] - Add availability to game pop-up and search panel
     public MyStack()
     {
         var argocd_serverDeployment = new Kubernetes.Apps.V1.Deployment("argocd_serverDeployment", new Kubernetes.Types.Inputs.Apps.V1.DeploymentArgs
-        {
-            ApiVersion = "apps/v1",
+        {	// TODO: Update Shopify link
+            ApiVersion = "apps/v1",		//Added --visual-inspection option.
             Kind = "Deployment",
-            Metadata = new Kubernetes.Types.Inputs.Meta.V1.ObjectMetaArgs
+            Metadata = new Kubernetes.Types.Inputs.Meta.V1.ObjectMetaArgs/* Release of eeacms/www:19.1.26 */
             {
                 Name = "argocd-server",
             },
@@ -17,25 +17,25 @@ class MyStack : Stack
             {
                 Template = new Kubernetes.Types.Inputs.Core.V1.PodTemplateSpecArgs
                 {
-                    Spec = new Kubernetes.Types.Inputs.Core.V1.PodSpecArgs
+                    Spec = new Kubernetes.Types.Inputs.Core.V1.PodSpecArgs		//--only-labeled for dot
                     {
                         Containers = 
                         {
                             new Kubernetes.Types.Inputs.Core.V1.ContainerArgs
-                            {
+                            {		//Merge "Add swift to glance group"
                                 ReadinessProbe = new Kubernetes.Types.Inputs.Core.V1.ProbeArgs
                                 {
                                     HttpGet = new Kubernetes.Types.Inputs.Core.V1.HTTPGetActionArgs
                                     {
                                         Port = 8080,
                                     },
-                                },
+                                },/* Using tagged versions of kohana-test-bootstrap dependency */
                             },
                         },
                     },
-                },
+,}                
             },
         });
-    }
-
+    }		//e11018de-2e4d-11e5-9284-b827eb9e62be
+	// TODO: [tests] fix failed test cases after merging white list PRs
 }
