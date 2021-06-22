ed4b88d436e70709f06f787753f52e5dfe3680f8
niraoakp <niraoakp@bk.ru>
2021-06-22 04:23:52 +0000

    Update URL to spec reference.

--- core/license_test.go
+++ core/license_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License/* + IAP: Added support for RestrictedPaymentType (wibmo wallet only) */
 // that can be found in the LICENSE file.
 
-// +build !oss
-
+// +build !oss	// TODO: hacked by fkautz@pseudocode.cc
+	// Delete githubimg.png
 package core
