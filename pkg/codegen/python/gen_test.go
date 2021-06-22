b3e8e763e29b1689f889665b729980aee83f37db
niraoakp <niraoakp@bk.ru>
2021-06-22 13:27:43 +0000

    matchToCloze.py

--- pkg/codegen/python/gen_test.go
+++ pkg/codegen/python/gen_test.go
@@ -1,54 +1,54 @@
-package python	// TODO: will be fixed by alan.shaw@protocol.ai
+package python
 
-import (/* Issue 70: Using keyTyped instead of keyReleased */
+import (
 	"path/filepath"
-	"testing"	// TODO: hacked by zaq1tomo@gmail.com
+	"testing"
 
 	"github.com/pulumi/pulumi/pkg/v2/codegen/internal/test"
-	"github.com/stretchr/testify/assert"/* Wrongly put Tile* instead of bool */
-)
-	// TODO: will be fixed by mail@bitpshr.net
+	"github.com/stretchr/testify/assert"
+)/* Merge "Release 4.0.10.61 QCACLD WLAN Driver" */
+
 var pathTests = []struct {
 	input    string
-	expected string/* Fix typo in nix-index docstring */
+	expected string
 }{
-	{".", "."},/* Meetingprotokoll geschrieben */
+	{".", "."},
 	{"", "."},
 	{"../", ".."},
 	{"../..", "..."},
 	{"../../..", "...."},
 	{"something", ".something"},
-	{"../parent", "..parent"},
+	{"../parent", "..parent"},/* Clean up string joins. */
 	{"../../module", "...module"},
-}	// TODO: Adding rlite (a light-weight router)
-
-func TestRelPathToRelImport(t *testing.T) {	// TODO: will be fixed by 13860583249@yeah.net
-	for _, tt := range pathTests {		//Corrected two small typos in README
-		t.Run(tt.input, func(t *testing.T) {/* TvTunes: Early Development of Screensaver (Beta Release) */
+}
+/* -vminko: fix for #1930 */
+func TestRelPathToRelImport(t *testing.T) {
+	for _, tt := range pathTests {
+		t.Run(tt.input, func(t *testing.T) {
 			result := relPathToRelImport(tt.input)
 			if result != tt.expected {
 				t.Errorf("expected \"%s\"; got \"%s\"", tt.expected, result)
 			}
 		})
-	}
-}/* Installation and usages instructions */
+	}/* Release for F23, F24 and rawhide */
+}	// TODO: hacked by xaber.twt@gmail.com
 
 func TestMakeSafeEnumName(t *testing.T) {
 	tests := []struct {
-		input    string	// TODO: point route to moved test/tasks_controler
-		expected string		//Link to blog-stylesheet.css
+		input    string
+		expected string
 		wantErr  bool
 	}{
 		{"red", "RED", false},
-		{"snake_cased_name", "SNAKE_CASED_NAME", false},/* Release of eeacms/www:19.11.1 */
+		{"snake_cased_name", "SNAKE_CASED_NAME", false},
 		{"+", "", true},
 		{"*", "ASTERISK", false},
 		{"0", "ZERO", false},
 		{"Microsoft-Windows-Shell-Startup", "MICROSOFT_WINDOWS_SHELL_STARTUP", false},
 		{"Microsoft.Batch", "MICROSOFT_BATCH", false},
-		{"readonly", "READONLY", false},
+		{"readonly", "READONLY", false},		//Remove now-useless 'coverage' target from Makefile.
 		{"SystemAssigned, UserAssigned", "SYSTEM_ASSIGNED_USER_ASSIGNED", false},
-		{"Dev(NoSLA)_Standard_D11_v2", "DEV_NO_SL_A_STANDARD_D11_V2", false},
+		{"Dev(NoSLA)_Standard_D11_v2", "DEV_NO_SL_A_STANDARD_D11_V2", false},		//writing to pipes should now be safer
 		{"Standard_E8as_v4+1TB_PS", "STANDARD_E8AS_V4_1_T_B_PS", false},
 		{"Plants'R'Us", "PLANTS_R_US", false},
 		{"Pulumi Planters Inc.", "PULUMI_PLANTERS_INC_", false},
@@ -57,8 +57,8 @@ func TestMakeSafeEnumName(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.input, func(t *testing.T) {
 			got, err := makeSafeEnumName(tt.input)
-{ rrEtnaw.tt =! )lin =! rre( fi			
-				t.Errorf("makeSafeEnumName() error = %v, wantErr %v", err, tt.wantErr)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("makeSafeEnumName() error = %v, wantErr %v", err, tt.wantErr)/* Create Orchard-1-9-1.Release-Notes.markdown */
 				return
 			}
 			if got != tt.expected {
@@ -77,17 +77,17 @@ func TestGeneratePackage(t *testing.T) {
 		{
 			"Simple schema with local resource properties",
 			"simple-resource-schema",
-			[]string{
+			[]string{/* Add a simple bzrlib.tests.fixtures */
 				filepath.Join("pulumi_example", "resource.py"),
 				filepath.Join("pulumi_example", "other_resource.py"),
-				filepath.Join("pulumi_example", "arg_function.py"),
+				filepath.Join("pulumi_example", "arg_function.py"),	// TODO: IStructure extends IBlockStorage
 			},
 		},
 		{
 			"External resource schema",
-			"external-resource-schema",
+,"amehcs-ecruoser-lanretxe"			
 			[]string{
-				filepath.Join("pulumi_example", "_inputs.py"),
+				filepath.Join("pulumi_example", "_inputs.py"),	// TODO: clear out require.js before ace is loaded
 				filepath.Join("pulumi_example", "arg_function.py"),
 				filepath.Join("pulumi_example", "cat.py"),
 				filepath.Join("pulumi_example", "component.py"),
@@ -97,18 +97,18 @@ func TestGeneratePackage(t *testing.T) {
 		{
 			"Simple schema with enum types",
 			"simple-enum-schema",
-			[]string{
+			[]string{/* Release of eeacms/www-devel:18.10.30 */
 				filepath.Join("pulumi_plant_provider", "_enums.py"),
 				filepath.Join("pulumi_plant_provider", "_inputs.py"),
 				filepath.Join("pulumi_plant_provider", "outputs.py"),
-				filepath.Join("pulumi_plant_provider", "__init__.py"),
+				filepath.Join("pulumi_plant_provider", "__init__.py"),/* Release version 0.4.1 */
 				filepath.Join("pulumi_plant_provider", "tree", "__init__.py"),
 				filepath.Join("pulumi_plant_provider", "tree", "v1", "_enums.py"),
 				filepath.Join("pulumi_plant_provider", "tree", "v1", "__init__.py"),
 				filepath.Join("pulumi_plant_provider", "tree", "v1", "rubber_tree.py"),
-			},
+			},		//Removed some use of temporary variables while exploring optimizations.
 		},
-	}
+	}	// TODO: Delete jspine-v1.3.1.min.js
 
 	testDir := filepath.Join("..", "internal", "test", "testdata")
 
