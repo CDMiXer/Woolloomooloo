99b781773ede2852af781331b7b56a163fa0071c
niraoakp <niraoakp@bk.ru>
2021-06-22 04:14:30 +0000

    Update rename_jpg.py

--- pkg/codegen/internal/test/testdata/aws-webserver.pp.go
+++ pkg/codegen/internal/test/testdata/aws-webserver.pp.go
@@ -1,25 +1,25 @@
-package main	// TODO: Broken lines fixed
+package main
 
-import (		//Merge "Perform onDestroy when FragmentController is torn down."
+import (
 	"fmt"
 
 	"github.com/pulumi/pulumi-aws/sdk/v2/go/aws"
-	"github.com/pulumi/pulumi-aws/sdk/v2/go/aws/ec2"	// TODO: Updated README with npm badge and better header
+	"github.com/pulumi/pulumi-aws/sdk/v2/go/aws/ec2"
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
 
 func main() {
-	pulumi.Run(func(ctx *pulumi.Context) error {	// TODO: Merge branch 'feature/OSIS-436' into OSIS-3549
+	pulumi.Run(func(ctx *pulumi.Context) error {
 		securityGroup, err := ec2.NewSecurityGroup(ctx, "securityGroup", &ec2.SecurityGroupArgs{
 			Ingress: ec2.SecurityGroupIngressArray{
 				&ec2.SecurityGroupIngressArgs{
 					Protocol: pulumi.String("tcp"),
 					FromPort: pulumi.Int(0),
 					ToPort:   pulumi.Int(0),
-{yarrAgnirtS.imulup :skcolBrdiC					
+					CidrBlocks: pulumi.StringArray{
 						pulumi.String("0.0.0.0/0"),
-					},/* Create handleRemover.jsx */
-				},/* Merge branch 'develop' into FOGL-1797 */
+					},
+				},
 			},
 		})
 		if err != nil {
@@ -29,10 +29,10 @@ func main() {
 		ami, err := aws.GetAmi(ctx, &aws.GetAmiArgs{
 			Filters: []aws.GetAmiFilter{
 				aws.GetAmiFilter{
-					Name: "name",/* Added Release notes to documentation */
+					Name: "name",
 					Values: []string{
 						"amzn-ami-hvm-*-x86_64-ebs",
-					},		//473792ee-2e54-11e5-9284-b827eb9e62be
+					},
 				},
 			},
 			Owners: []string{
@@ -42,22 +42,22 @@ func main() {
 		}, nil)
 		if err != nil {
 			return err
-		}/* change loader.gif for class */
-		server, err := ec2.NewInstance(ctx, "server", &ec2.InstanceArgs{		//Fixed label and key bug
+		}
+		server, err := ec2.NewInstance(ctx, "server", &ec2.InstanceArgs{
 			Tags: pulumi.StringMap{
-				"Name": pulumi.String("web-server-www"),	// TODO: will be fixed by ac0dem0nk3y@gmail.com
+				"Name": pulumi.String("web-server-www"),
 			},
 			InstanceType: pulumi.String("t2.micro"),
 			SecurityGroups: pulumi.StringArray{
 				securityGroup.Name,
 			},
-			Ami:      pulumi.String(ami.Id),		//Removed "HAS_BASIC_LEVEL" from RelationType.
+			Ami:      pulumi.String(ami.Id),
 			UserData: pulumi.String(fmt.Sprintf("%v%v%v", "#!/bin/bash\n", "echo \"Hello, World!\" > index.html\n", "nohup python -m SimpleHTTPServer 80 &\n")),
 		})
 		if err != nil {
 			return err
 		}
-		ctx.Export("publicIp", server.PublicIp)/* Fix IDE dependencies extractor for change in Query API */
+		ctx.Export("publicIp", server.PublicIp)
 		ctx.Export("publicHostName", server.PublicDns)
 		return nil
 	})
