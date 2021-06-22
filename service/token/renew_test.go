1cc6476575ee9b40620b81521537438ead6e09fe
niraoakp <niraoakp@bk.ru>
2021-06-22 05:21:33 +0000

    More robust attribute state parsing.

--- service/token/renew_test.go
+++ service/token/renew_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// Se agregó control de altura y scroll al modal de solicitud en un pedido
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
-
-package token
+/* (John Arbash Meinel) Release 0.12rc1 */
+package token/* minor to timestep estimate to allow ALL test cases to pass (broken by r2831) */
