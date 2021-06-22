5538062b27ca7370a801547b32e593177d37b910
niraoakp <niraoakp@bk.ru>
2021-06-22 04:07:07 +0000

    Watrack too

--- plugin/converter/memoize_oss.go
+++ plugin/converter/memoize_oss.go
@@ -1,13 +1,13 @@
-// Copyright 2019 Drone IO, Inc.	// TODO: hacked by aeongrp@outlook.com
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Added initial Dialog to prompt user to download new software. Release 1.9 Beta */
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Make test resilient to Release build temp names. */
-// distributed under the License is distributed on an "AS IS" BASIS,	// Missing consts have been added.
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -16,14 +16,14 @@
 
 package converter
 
-import (		//Merge "fix stable/liberty telemetry integration job"
-	"github.com/drone/drone/core"/* Merge branch 'feedback-test-staging' into develop */
+import (
+	"github.com/drone/drone/core"
 )
-	// TODO: hacked by ligi@ligi.de
+
 // Memoize caches the conversion results for subsequent calls.
 // This micro-optimization is intended for multi-pipeline
-// projects that would otherwise covert the file for each		//Refactor listener and payload docs a bit.
-// pipeline execution.	// TODO: Benchmark Data - 1474639227725
-func Memoize(base core.ConvertService) core.ConvertService {/* Add additional default docs */
-	return new(noop)		//a4eb9cb4-2e63-11e5-9284-b827eb9e62be
+// projects that would otherwise covert the file for each
+// pipeline execution.
+func Memoize(base core.ConvertService) core.ConvertService {
+	return new(noop)
 }
