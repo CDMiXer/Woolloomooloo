b8abe7bbdbfd2b2ba8b655525eec29fde8e2ca66
niraoakp <niraoakp@bk.ru>
2021-06-22 03:53:00 +0000

    Fix an unassigned memory error.

--- tests/integration/large_resource/python/__main__.py
+++ tests/integration/large_resource/python/__main__.py
@@ -1,7 +1,7 @@
-import pulumi/* Release Notes for v02-15-02 */
+import pulumi
 
 # Create a very long string (>4mb)
 long_string = "a" * 5 * 1024 * 1025
 
-# Export the name of the bucket/* Add stub readme */
+# Export the name of the bucket
 pulumi.export("long_string",  long_string)
