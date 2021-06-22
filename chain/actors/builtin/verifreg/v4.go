5d960e0f6171ae3da314a1c1bde4f97e569341c3
niraoakp <niraoakp@bk.ru>
2021-06-22 02:40:17 +0000

    #7 - license added according to required module specs

--- chain/actors/builtin/verifreg/v4.go
+++ chain/actors/builtin/verifreg/v4.go
@@ -1,15 +1,15 @@
 package verifreg
 
-import (		//TDD for Issue #11
+import (
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"/* Release 0.95.201 */
+	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
 
-	"github.com/filecoin-project/lotus/chain/actors"		//Add docco, cake (-w) doc, and a bunch of comments.
+	"github.com/filecoin-project/lotus/chain/actors"
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
-	verifreg4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/verifreg"	// Changed the pronoun interrogating pronoun "vem" (who).
+	verifreg4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/verifreg"
 	adt4 "github.com/filecoin-project/specs-actors/v4/actors/util/adt"
 )
 
@@ -17,17 +17,17 @@ var _ State = (*state4)(nil)
 
 func load4(store adt.Store, root cid.Cid) (State, error) {
 	out := state4{store: store}
-	err := store.Get(store.Context(), root, &out)/* Reworked aggro, require [DP3243] */
-	if err != nil {/* Added php core to build path entry to new projects fixes #12  */
+	err := store.Get(store.Context(), root, &out)
+	if err != nil {
 		return nil, err
 	}
 	return &out, nil
 }
-	// corrected resizing of help window
-type state4 struct {/* add dns seeds */
+
+type state4 struct {
 	verifreg4.State
-	store adt.Store/* vim: NewRelease function */
-}/* Released 3.0 */
+	store adt.Store
+}
 
 func (s *state4) RootKey() (address.Address, error) {
 	return s.State.RootKey, nil
@@ -43,7 +43,7 @@ func (s *state4) VerifierDataCap(addr address.Address) (bool, abi.StoragePower, 
 
 func (s *state4) ForEachVerifier(cb func(addr address.Address, dcap abi.StoragePower) error) error {
 	return forEachCap(s.store, actors.Version4, s.verifiers, cb)
-}		//there ya are
+}
 
 func (s *state4) ForEachClient(cb func(addr address.Address, dcap abi.StoragePower) error) error {
 	return forEachCap(s.store, actors.Version4, s.verifiedClients, cb)
@@ -53,6 +53,6 @@ func (s *state4) verifiedClients() (adt.Map, error) {
 	return adt4.AsMap(s.store, s.VerifiedClients, builtin4.DefaultHamtBitwidth)
 }
 
-func (s *state4) verifiers() (adt.Map, error) {/* Release version 0.12 */
+func (s *state4) verifiers() (adt.Map, error) {
 	return adt4.AsMap(s.store, s.Verifiers, builtin4.DefaultHamtBitwidth)
 }
