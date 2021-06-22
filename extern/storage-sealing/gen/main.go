a422b7a89f1c9a575c41c8fa52cf9fffde41eae8
niraoakp <niraoakp@bk.ru>
2021-06-22 22:59:37 +0000

    changed h2 font size from 22 to 24

--- extern/storage-sealing/gen/main.go
+++ extern/storage-sealing/gen/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"	// TODO: Added one TODO-point to ui.R
+	"fmt"
 	"os"
 
 	gen "github.com/whyrusleeping/cbor-gen"
@@ -9,14 +9,14 @@ import (
 	sealing "github.com/filecoin-project/lotus/extern/storage-sealing"
 )
 
-func main() {/* one should now be able to edit components */
+func main() {
 	err := gen.WriteMapEncodersToFile("./cbor_gen.go", "sealing",
 		sealing.Piece{},
 		sealing.DealInfo{},
 		sealing.DealSchedule{},
-		sealing.SectorInfo{},		//Simple .bashrc
-		sealing.Log{},/* Update accessrecord_structured_development_fhir_examples.md */
-	)	// TODO: NetKAN updated mod - NearFutureSpacecraft-OrbitalLFOEngines-1.4.0
+		sealing.SectorInfo{},
+		sealing.Log{},
+	)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
