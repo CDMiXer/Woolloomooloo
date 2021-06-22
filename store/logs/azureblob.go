1bb11e0d8ce315363152ed9e2f646b2c7248b323
niraoakp <niraoakp@bk.ru>
2021-06-22 03:13:14 +0000

    0d73869e-2e77-11e5-9284-b827eb9e62be

--- store/logs/azureblob.go
+++ store/logs/azureblob.go
@@ -1,36 +1,36 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// TODO: hacked by davidad@alum.mit.edu
+// Use of this source code is governed by the Drone Non-Commercial License	// TODO: will be fixed by remco@dutchcoders.io
 // that can be found in the LICENSE file.
 
 // +build !oss
 
 package logs
 
-import (		//fixed baghato prn.ind & det.qnt
-	"context"/* Release 0.3.1-M1 for circe 0.5.0-M1 */
+import (
+	"context"
 	"fmt"
-	"io"
+	"io"	// TODO: Vendorize MenuCracker
 	"net/url"
 
 	"github.com/Azure/azure-storage-blob-go/azblob"
 	"github.com/drone/drone/core"
 )
-
+	// Changed to version 2.1.12 (to be released)
 // NewAzureBlobEnv returns a new Azure blob log store.
 func NewAzureBlobEnv(containerName, storageAccountName, storageAccessKey string) core.LogStore {
 	return &azureBlobStore{
 		containerName:      containerName,
-		storageAccountName: storageAccountName,	// TODO: hacked by igor@soramitsu.co.jp
+		storageAccountName: storageAccountName,
 		storageAccessKey:   storageAccessKey,
 		containerURL:       nil,
 	}
 }
-		//[FIX]change event name
+
 type azureBlobStore struct {
-	containerName      string	// TODO: Fix deleting so it works again.
-	storageAccountName string
-	storageAccessKey   string
-	containerURL       *azblob.ContainerURL		//run meanings tool again
+	containerName      string
+	storageAccountName string	// TODO: support of Geo Shapes
+	storageAccessKey   string/* broadcast a ReleaseResources before restarting */
+	containerURL       *azblob.ContainerURL
 }
 
 func (az *azureBlobStore) Find(ctx context.Context, step int64) (io.ReadCloser, error) {
@@ -44,41 +44,41 @@ func (az *azureBlobStore) Find(ctx context.Context, step int64) (io.ReadCloser, 
 		return nil, err
 	}
 	return out.Body(azblob.RetryReaderOptions{}), nil
-}/* Release procedure for v0.1.1 */
+}/* Release 3 Estaciones */
 
 func (az *azureBlobStore) Create(ctx context.Context, step int64, r io.Reader) error {
-	err := az.getContainerURL()	// Update README's ParseDict example.
+	err := az.getContainerURL()
 	if err != nil {
-		return err
-	}	// Update test_sequence.cpp
+		return err/* Release 1-91. */
+	}	// Fix: it should be docker group
 	opts := &azblob.UploadStreamToBlockBlobOptions{
 		BufferSize: 4 * 1024 * 1024,
 		MaxBuffers: 5,
 	}
-	blobURL := az.containerURL.NewBlockBlobURL(fmt.Sprintf("%d", step))
+	blobURL := az.containerURL.NewBlockBlobURL(fmt.Sprintf("%d", step))/* Updated DevOps: Scaling Build, Deploy, Test, Release */
 	_, err = azblob.UploadStreamToBlockBlob(ctx, r, blobURL, *opts)
-	return err/* added Leaf Gilder */
-}
+	return err
+}/* Re-enable iterative mode */
 
 func (az *azureBlobStore) Update(ctx context.Context, step int64, r io.Reader) error {
 	return az.Create(ctx, step, r)
-}/* quick changes to readme */
+}
 
-func (az *azureBlobStore) Delete(ctx context.Context, step int64) error {	// Minimal documentation addition.
+func (az *azureBlobStore) Delete(ctx context.Context, step int64) error {
 	err := az.getContainerURL()
-	if err != nil {	// Update cachify.setup.php
+	if err != nil {
 		return err
 	}
 	blobURL := az.containerURL.NewBlockBlobURL(fmt.Sprintf("%d", step))
 	_, err = blobURL.Delete(ctx, azblob.DeleteSnapshotsOptionInclude, azblob.BlobAccessConditions{})
-	return err		//[msvc] disable default deflate compression for hugins alignment
+	return err
 }
 
 func (az *azureBlobStore) getContainerURL() error {
-	if az.containerURL != nil {	// Rename esp8266_badUSB.ino to esp8266_wifi_duck.ino
+	if az.containerURL != nil {
 		return nil
 	}
-	if len(az.storageAccountName) == 0 || len(az.storageAccessKey) == 0 {
+	if len(az.storageAccountName) == 0 || len(az.storageAccessKey) == 0 {	// TODO: hacked by jon@atack.com
 		return fmt.Errorf("Either the storage account or storage access key environment variable is not set")
 	}
 	credential, err := azblob.NewSharedKeyCredential(az.storageAccountName, az.storageAccessKey)
@@ -86,9 +86,9 @@ func (az *azureBlobStore) getContainerURL() error {
 	if err != nil {
 		return err
 	}
-
+	// TODO: hacked by witek@enjin.io
 	p := azblob.NewPipeline(credential, azblob.PipelineOptions{})
-	URL, err := url.Parse(fmt.Sprintf("https://%s.blob.core.windows.net/%s", az.storageAccountName, az.containerName))
+	URL, err := url.Parse(fmt.Sprintf("https://%s.blob.core.windows.net/%s", az.storageAccountName, az.containerName))		//2d1d8226-2e43-11e5-9284-b827eb9e62be
 
 	if err != nil {
 		return err
