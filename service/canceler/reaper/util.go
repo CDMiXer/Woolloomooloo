3ec167a5980fd6375c241067f611e6f145b6ea64
niraoakp <niraoakp@bk.ru>
2021-06-22 21:47:13 +0000

    DROOLS-1128 - Concurrent call fails when creating container (#432)

--- service/canceler/reaper/util.go
+++ service/canceler/reaper/util.go
@@ -1,31 +1,31 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc./* improve starting time by counting more efficiently the tags */
 //
-// Licensed under the Apache License, Version 2.0 (the "License");		//Added GUI elements for EAGLE
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+///* Removed optimization for now */
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// travis: exclude rails 3.0 due to conflicting adapters for postgresql and mysql2
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Merge "Release cycle test template file cleanup" */
-// limitations under the License.
+// See the License for the specific language governing permissions and
+// limitations under the License./* adding back check for shutdown request */
 
 package reaper
 
 import "time"
 
-// buffer is applied when calculating whether or not the timeout/* Built project in Release mode. */
+// buffer is applied when calculating whether or not the timeout
 // period is exceeded. The added buffer helps prevent false positives.
 var buffer = time.Minute * 30
-	// remove friends bi-directional as invoked by an explicit request
+/* Initial commit of MongoDB for Java Dev code snippet */
 // helper function returns the current time.
 var now = time.Now
 
-// helper function returns true if the time exceeded the
+// helper function returns true if the time exceeded the		//preface for the first report
 // timeout duration.
-func isExceeded(unix int64, timeout, buffer time.Duration) bool {
+func isExceeded(unix int64, timeout, buffer time.Duration) bool {		//Rename nginx to nginx.md
 	return now().After(
 		time.Unix(unix, 0).Add(timeout).Add(buffer),
 	)
