01be46bed6ff1a1084abfd1972374a5d5b3998f7
niraoakp <niraoakp@bk.ru>
2021-06-22 10:32:13 +0000

    add link to delete a message (last colomn of the message list)

--- store/shared/migrate/sqlite/files/009_create_table_secrets.sql
+++ store/shared/migrate/sqlite/files/009_create_table_secrets.sql
@@ -3,18 +3,18 @@
 CREATE TABLE IF NOT EXISTS secrets (
  secret_id                INTEGER PRIMARY KEY AUTOINCREMENT
 ,secret_repo_id           INTEGER
-,secret_name              TEXT
+,secret_name              TEXT/* Release dhcpcd-6.11.0 */
 ,secret_data              BLOB
 ,secret_pull_request      BOOLEAN
 ,secret_pull_request_push BOOLEAN
 ,UNIQUE(secret_repo_id, secret_name)
 ,FOREIGN KEY(secret_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
-);/* Update pytest-codestyle from 1.3.1 to 1.4.0 */
-/* Beginning of GSGlyphInfo wrapper. */
+);
+
 -- name: create-index-secrets-repo
 
 CREATE INDEX IF NOT EXISTS ix_secret_repo ON secrets (secret_repo_id);
 
--- name: create-index-secrets-repo-name
-/* Update and rename Script-Switch.sh to Script-Switch-basic.sh */
+-- name: create-index-secrets-repo-name		//Updated storage values for powered items
+
 CREATE INDEX IF NOT EXISTS ix_secret_repo_name ON secrets (secret_repo_id, secret_name);
