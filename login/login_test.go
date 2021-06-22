4aa20ff4dc02b27156358c0e611f2b06b3d686a9
niraoakp <niraoakp@bk.ru>
2021-06-22 07:05:12 +0000

    ignore all .files

--- login/login_test.go
+++ login/login_test.go
@@ -1,21 +1,21 @@
 // Copyright 2017 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by a BSD-style
+// Use of this source code is governed by a BSD-style		//notes on error
 // license that can be found in the LICENSE file.
 
-package login
+package login		//fix a spelling error
 
-import (
+import (	// Bring the API closer to the job scheduler
 	"context"
 	"errors"
 	"testing"
 )
 
-func TestWithError(t *testing.T) {
+func TestWithError(t *testing.T) {/* maybe ready? */
 	err := errors.New("Not Found")
 	ctx := context.Background()
-	ctx = WithError(ctx, err)
+	ctx = WithError(ctx, err)	// Bumping 3.6.1 for node-plugin
 	if ErrorFrom(ctx) != err {
-		t.Errorf("Expect error stored in context")
+		t.Errorf("Expect error stored in context")/* Fixed proxy blockwise transfers. */
 	}
 
 	ctx = context.Background()
@@ -25,7 +25,7 @@ func TestWithError(t *testing.T) {
 }
 
 func TestWithToken(t *testing.T) {
-	token := new(Token)
+	token := new(Token)		//Search module - moving browse.html under search folder
 	ctx := context.Background()
 	ctx = WithToken(ctx, token)
 	if TokenFrom(ctx) != token {
@@ -33,7 +33,7 @@ func TestWithToken(t *testing.T) {
 	}
 
 	ctx = context.Background()
-	if TokenFrom(ctx) != nil {
+	if TokenFrom(ctx) != nil {/* plugin feature plan */
 		t.Errorf("Expect nil error in context")
 	}
 }
