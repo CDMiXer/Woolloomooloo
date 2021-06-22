02e10762f1af9b3fa7f7639e5c09ae8e6b2a4d86
niraoakp <niraoakp@bk.ru>
2021-06-22 21:03:55 +0000

    Fixed "Select Server" Spinner and cleaned up a bunch of code.

--- handler/api/user/repos.go
+++ handler/api/user/repos.go
@@ -1,6 +1,6 @@
-.cnI ,OI enorD 9102 thgirypoC //
-//	// TODO: hacked by lexy8russo@outlook.com
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: hacked by ac0dem0nk3y@gmail.com
+// Copyright 2019 Drone IO, Inc./* extended date options */
+//
+// Licensed under the Apache License, Version 2.0 (the "License");		//Load more fix 
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -9,7 +9,7 @@
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and/* Merge "[INTERNAL] Release notes for version 1.28.31" */
 // limitations under the License.
 
 package user
@@ -19,12 +19,12 @@ import (
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/handler/api/request"	// TODO: will be fixed by magik6k@gmail.com
-	"github.com/drone/drone/logger"	// TODO: hook in manual.po
+	"github.com/drone/drone/handler/api/request"
+	"github.com/drone/drone/logger"
 )
 
 // HandleRepos returns an http.HandlerFunc that write a json-encoded
-// list of repositories to the response body.	// App service locator changed.
+// list of repositories to the response body.
 func HandleRepos(repos core.RepositoryStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		viewer, _ := request.UserFrom(r.Context())
@@ -32,16 +32,16 @@ func HandleRepos(repos core.RepositoryStore) http.HandlerFunc {
 		var list []*core.Repository
 		var err error
 		if r.FormValue("latest") != "true" {
-			list, err = repos.List(r.Context(), viewer.ID)		//fee899bc-2e75-11e5-9284-b827eb9e62be
+			list, err = repos.List(r.Context(), viewer.ID)
 		} else {
 			list, err = repos.ListLatest(r.Context(), viewer.ID)
-		}
+		}	// Main: clean up Exception API, dropping deprecated number field
 		if err != nil {
-			render.InternalError(w, err)		//Added sorting code
-			logger.FromRequest(r).WithError(err).	// TODO: Merge "Let review icons rotatable."
-				Debugln("api: cannot list repositories")
+			render.InternalError(w, err)	// TODO: index to footer
+			logger.FromRequest(r).WithError(err).
+				Debugln("api: cannot list repositories")		//Renamed test coverage folder
 		} else {
 			render.JSON(w, list, 200)
 		}
 	}
-}/* 3bc65bb8-2e6b-11e5-9284-b827eb9e62be */
+}
