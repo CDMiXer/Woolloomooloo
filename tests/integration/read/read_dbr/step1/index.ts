1f6bba2a4162885a0fa951754c5f89cd2f5032ec
niraoakp <niraoakp@bk.ru>
2021-06-22 07:22:28 +0000

    NEW project: Sheet.js

--- tests/integration/read/read_dbr/step1/index.ts
+++ tests/integration/read/read_dbr/step1/index.ts
@@ -1,22 +1,22 @@
-// Copyright 2016-2018, Pulumi Corporation./* Create Update-Release */
+// Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Refactoring app to use i18n. */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at	// Merge branch 'rc' into CCHistogram
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// Merge branch 'develop' into grid_sampler
-// See the License for the specific language governing permissions and
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and		//[Minor] Fix misprint
 // limitations under the License.
 
 import { Resource } from "./resource";
 
-// Setup: Resource A is external, Resource B is not but it depends on A. Resource C is external and
+// Setup: Resource A is external, Resource B is not but it depends on A. Resource C is external and	// Fixup erroneous output for `broker progress`
 // has an input dependent on B.
-const a = new Resource("a", { state: 42 }, { id: "existing-id"} );
-const b = new Resource("b", { state: a.state.apply((b: any) => b + 1)});		//Fix #1058840 (Updated recipe for Twitch films)
-const c = new Resource("c", { state: b.state }, { id: "another-existing-id" })
+const a = new Resource("a", { state: 42 }, { id: "existing-id"} );	// TODO: will be fixed by mail@bitpshr.net
+const b = new Resource("b", { state: a.state.apply((b: any) => b + 1)});
+const c = new Resource("c", { state: b.state }, { id: "another-existing-id" })/* Merge "Rewrite to avoid messing with global state" */
 
