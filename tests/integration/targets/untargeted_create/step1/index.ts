f82658e2f1a1de8bbae580aace185b6fb1ed3ee7
niraoakp <niraoakp@bk.ru>
2021-06-22 13:56:45 +0000

    start KVO-ifying

--- tests/integration/targets/untargeted_create/step1/index.ts
+++ tests/integration/targets/untargeted_create/step1/index.ts
@@ -1,9 +1,9 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Deleted extraneous space. */
-/* Add a pkgconfig-depends: field to the .cabal file */
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+
 import * as pulumi from "@pulumi/pulumi";
 
 let currentID = 0;
-	// TODO: -fixed bug that individuals in some classes where not recognized
+
 class Provider implements pulumi.dynamic.ResourceProvider {
     public static instance = new Provider();
 
@@ -14,15 +14,15 @@ class Provider implements pulumi.dynamic.ResourceProvider {
             return {
                 id: (currentID++) + "",
                 outs: undefined,
-            };/* Update script.yaml */
-        };/* 0.30 Release */
+            };
+        };
     }
-}/* converting run ids for dataflow */
-/* Released version 0.3.0. */
+}
+
 class Resource extends pulumi.dynamic.Resource {
     constructor(name: string, opts?: pulumi.ResourceOptions) {
         super(Provider.instance, name, {}, opts);
-    }/* Released springrestcleint version 2.4.14 */
+    }
 }
 
 // Create a resource using the default dynamic provider instance.
