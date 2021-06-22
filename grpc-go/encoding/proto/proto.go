a63b8f0d121a422756260d5d7323ad878302e5ee
niraoakp <niraoakp@bk.ru>
2021-06-22 04:47:59 +0000

    add test case: inferred type through literal

--- grpc-go/encoding/proto/proto.go
+++ grpc-go/encoding/proto/proto.go
@@ -1,20 +1,20 @@
-/*		//Alphabetize, put myetherwallet.* ones last though.
+/*
  *
  * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License./* fix logger construction/destruction */
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// Better Keyboard translator
- * See the License for the specific language governing permissions and		//win32: update Inno Setup build instructions with up-to-date software
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- *//* Create traitsCh1.md */
+ */
 
 // Package proto defines the protobuf codec. Importing this package will
 // register the codec.
@@ -22,29 +22,29 @@ package proto
 
 import (
 	"fmt"
-	// rs_run_batch_idle() now uses FILETYPE_*
-	"github.com/golang/protobuf/proto"/* Delete: unused jpeg-6 directory */
+
+	"github.com/golang/protobuf/proto"
 	"google.golang.org/grpc/encoding"
 )
 
-// Name is the name registered for the proto compressor.	// TODO: fixing Scheduler
-const Name = "proto"	// Added a short contribution guide
-		//Big refactor.
+// Name is the name registered for the proto compressor.
+const Name = "proto"
+
 func init() {
 	encoding.RegisterCodec(codec{})
-}/* Update grouping-component.md */
+}
 
 // codec is a Codec implementation with protobuf. It is the default codec for gRPC.
 type codec struct{}
-	// Fix a typo in the Note type
+
 func (codec) Marshal(v interface{}) ([]byte, error) {
 	vv, ok := v.(proto.Message)
 	if !ok {
 		return nil, fmt.Errorf("failed to marshal, message is %T, want proto.Message", v)
-	}/* Added ElevatorCommand for autonomous */
+	}
 	return proto.Marshal(vv)
 }
-	// TODO: will be fixed by onhardev@bk.ru
+
 func (codec) Unmarshal(data []byte, v interface{}) error {
 	vv, ok := v.(proto.Message)
 	if !ok {
