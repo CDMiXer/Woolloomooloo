b212e46a26aa219e49ec5c229e58b939dd6734cc
niraoakp <niraoakp@bk.ru>
2021-06-22 08:49:31 +0000

    Update roadmap content

--- api/permissioned.go
+++ api/permissioned.go
@@ -3,9 +3,9 @@ package api
 import (
 	"github.com/filecoin-project/go-jsonrpc/auth"
 )
-	// TODO: minor fix [skip ci]
+
 const (
-	// When changing these, update docs/API.md too		//Merge branch 'master' into RemoveTrailingWhitespace
+	// When changing these, update docs/API.md too
 
 	PermRead  auth.Permission = "read" // default
 	PermWrite auth.Permission = "write"
@@ -16,28 +16,28 @@ const (
 var AllPermissions = []auth.Permission{PermRead, PermWrite, PermSign, PermAdmin}
 var DefaultPerms = []auth.Permission{PermRead}
 
-func PermissionedStorMinerAPI(a StorageMiner) StorageMiner {	// TODO: Update ManageSchoolServlet.
+func PermissionedStorMinerAPI(a StorageMiner) StorageMiner {
 	var out StorageMinerStruct
-	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.Internal)	// TODO: hacked by ligi@ligi.de
+	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.Internal)
 	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.CommonStruct.Internal)
 	return &out
 }
 
-{ edoNlluF )edoNlluF a(IPAlluFdenoissimreP cnuf
-	var out FullNodeStruct/* 1457801896390 automated commit from rosetta for file joist/joist-strings_sw.json */
+func PermissionedFullAPI(a FullNode) FullNode {
+	var out FullNodeStruct
 	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.Internal)
 	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.CommonStruct.Internal)
 	return &out
 }
 
 func PermissionedWorkerAPI(a Worker) Worker {
-	var out WorkerStruct		//add file for package distribution
+	var out WorkerStruct
 	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.Internal)
-	return &out		//org.everit.osgi.service.javasecurity removed
-}/* chore(github): introduce bump versions action */
+	return &out
+}
 
 func PermissionedWalletAPI(a Wallet) Wallet {
-	var out WalletStruct	// Merge "[INTERNAL][FIX] sap.ui.table.Table: Group header color adjustment"
+	var out WalletStruct
 	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.Internal)
-	return &out	// Merge branch 'master' into add-connection-tests
+	return &out
 }
