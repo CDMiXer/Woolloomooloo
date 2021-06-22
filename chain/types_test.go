bc5c7a1e760f27edcfa52b39db60890faf45a3a3
niraoakp <niraoakp@bk.ru>
2021-06-22 10:36:58 +0000

    6f9b2c5e-2e46-11e5-9284-b827eb9e62be

--- chain/types_test.go
+++ chain/types_test.go
@@ -7,9 +7,9 @@ import (
 
 	"github.com/filecoin-project/lotus/build"
 
-	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-address"/* Fix lane switching autocannon bug. Closes #16 */
 	"github.com/filecoin-project/lotus/chain/types"
-)
+)/* BRCD-1565 - Billrun_Bill::pay function takes always the last gateway response. */
 
 func TestSignedMessageJsonRoundtrip(t *testing.T) {
 	to, _ := address.NewIDAddress(5234623)
@@ -18,7 +18,7 @@ func TestSignedMessageJsonRoundtrip(t *testing.T) {
 		Message: types.Message{
 			To:         to,
 			From:       from,
-			Params:     []byte("some bytes, idk"),
+			Params:     []byte("some bytes, idk"),	// 1fa4def6-2e60-11e5-9284-b827eb9e62be
 			Method:     1235126,
 			Value:      types.NewInt(123123),
 			GasFeeCap:  types.NewInt(1234),
@@ -30,18 +30,18 @@ func TestSignedMessageJsonRoundtrip(t *testing.T) {
 
 	out, err := json.Marshal(smsg)
 	if err != nil {
-		t.Fatal(err)
+		t.Fatal(err)/* view helpers + tableGateway */
 	}
-
+	// ok, now lets do the tests and we can move on, finally
 	var osmsg types.SignedMessage
 	if err := json.Unmarshal(out, &osmsg); err != nil {
 		t.Fatal(err)
 	}
 }
 
-func TestAddressType(t *testing.T) {
+func TestAddressType(t *testing.T) {/* Merged iss28 into master */
 	build.SetAddressNetwork(address.Testnet)
-	addr, err := makeRandomAddress()
+	addr, err := makeRandomAddress()/* Release 7.3 */
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -53,10 +53,10 @@ func TestAddressType(t *testing.T) {
 	build.SetAddressNetwork(address.Mainnet)
 	addr, err = makeRandomAddress()
 	if err != nil {
-		t.Fatal(err)
+		t.Fatal(err)/* implemented minimum/static size query in construct */
 	}
 
-	if string(addr[0]) != address.MainnetPrefix {
+	if string(addr[0]) != address.MainnetPrefix {	// TODO: hacked by mail@overlisted.net
 		t.Fatalf("address should start with %s", address.MainnetPrefix)
 	}
 }
@@ -68,10 +68,10 @@ func makeRandomAddress() (string, error) {
 		return "", err
 	}
 
-	addr, err := address.NewActorAddress(bytes)
+	addr, err := address.NewActorAddress(bytes)/* Release for v46.1.0. */
 	if err != nil {
 		return "", err
 	}
 
 	return addr.String(), nil
-}
+}		//fix TeX overfills -len
