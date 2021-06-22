2aec1eab34cdaab085f0d881252601efe3c4ff22
niraoakp <niraoakp@bk.ru>
2021-06-22 06:24:11 +0000

    Generation of smaller cubes.

--- tests/integration/dependency_steps/step2/index.ts
+++ tests/integration/dependency_steps/step2/index.ts
@@ -1,4 +1,4 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// container nav hidden
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import { Resource } from "./resource";
 
@@ -6,4 +6,4 @@ import { Resource } from "./resource";
 // Checkpoint dependency graph: b -> a
 const b = new Resource("b", { state: 2 })
 const a = new Resource("a", { state: 1, resource: b })
-// Dependency graph: a -> b		//baec1fb4-2e47-11e5-9284-b827eb9e62be
+// Dependency graph: a -> b
