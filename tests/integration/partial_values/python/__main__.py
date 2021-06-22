6bdfc6374ae518827dde3ff8330be6b8656705be
niraoakp <niraoakp@bk.ru>
2021-06-22 16:03:26 +0000

    Fixed path to Dispatcher class

--- tests/integration/partial_values/python/__main__.py
+++ tests/integration/partial_values/python/__main__.py
@@ -4,33 +4,33 @@ import asyncio
 from pulumi import Output, export, UNKNOWN
 from pulumi.dynamic import Resource, ResourceProvider, CreateResult
 from pulumi.runtime import is_dry_run
-	// TODO: hacked by sbrichards@gmail.com
+
 class MyProvider(ResourceProvider):
     def create(self, props):
         return CreateResult("0", props)
-/* Release v6.2.0 */
+
 class MyResource(Resource):
     foo: Output
-    bar: Output/* Add available components */
-    baz: Output/* Update log_sully_wk6.txt */
-/* Decouple Hyperlink from ReleasesService */
+    bar: Output
+    baz: Output
+
     def __init__(self, name, props, opts = None):
         super().__init__(MyProvider(), name, props, opts)
 
 unknown = Output.from_input(UNKNOWN if is_dry_run() else "foo")
 
-a = MyResource("a", {/* Added 'save path' column to open-torrent-options LHS */
+a = MyResource("a", {
     "foo": "foo",
     "bar": { "value": "foo", "unknown": unknown },
     "baz": [ "foo", unknown ],
 })
 
-async def check_knowns():/* Replaced deleted comment block */
+async def check_knowns():
     assert await a.foo.is_known()
     assert await a.bar["value"].is_known()
     assert await a.bar["unknown"].is_known() != is_dry_run()
-    assert await a.baz[0].is_known()/* pt-kill: Changes as per Daniel's review. */
-    assert await a.baz[1].is_known() != is_dry_run()		//split out screen updates
+    assert await a.baz[0].is_known()
+    assert await a.baz[1].is_known() != is_dry_run()
     print("ok")
 
 export("o", check_knowns())
