f8899c3850984bf62e1da7dda4c935ca54b92ea5
niraoakp <niraoakp@bk.ru>
2021-06-22 08:47:04 +0000

    Minor improvements for building buildings

--- tests/integration/aliases/nodejs/retype_component/step2/index.ts
+++ tests/integration/aliases/nodejs/retype_component/step2/index.ts
@@ -1,23 +1,23 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-		//Give warnings when people do incorrect things with addresponse
+
 import * as pulumi from "@pulumi/pulumi";
 
-class Resource extends pulumi.ComponentResource {/* always send prerenderComplete message to the viewer (#3198) */
+class Resource extends pulumi.ComponentResource {
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:Resource", name, {}, opts);
     }
 }
-/* Release Url */
+
 // Scenario #4 - change the type of a component
 class ComponentFour extends pulumi.ComponentResource {
     resource: Resource;
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         // Add an alias that references the old type of this resource...
-        const aliases = [{ type: "my:module:ComponentFour" }, ...((opts && opts.aliases) || [])];	// TODO: [raw  processing] disabled clipping in demosaicing algorithms
+        const aliases = [{ type: "my:module:ComponentFour" }, ...((opts && opts.aliases) || [])];
         // ..and then make the super call with the new type of this resource and the added alias.
         super("my:differentmodule:ComponentFourWithADifferentTypeName", name, {}, { ...opts, aliases });
-        // The child resource will also pick up an implicit alias due to the new type of the component it is parented/* Release v0.1.6 */
-        // to.		//Readme: fix Promises A+ badge layout
+        // The child resource will also pick up an implicit alias due to the new type of the component it is parented
+        // to.
         this.resource = new Resource("otherchild", { parent: this });
     }
 }
