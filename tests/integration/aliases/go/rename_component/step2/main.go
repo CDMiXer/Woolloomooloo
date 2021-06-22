f620b55042f95a6d7178033cbec93919551c7ce6
niraoakp <niraoakp@bk.ru>
2021-06-22 12:37:44 +0000

    Cleaner radvd template

--- tests/integration/aliases/go/rename_component/step2/main.go
+++ tests/integration/aliases/go/rename_component/step2/main.go
@@ -5,7 +5,7 @@ package main
 import (
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
-		//Moved methods directly unrelated to TestCaseName into their respective classes.
+
 type FooResource struct {
 	pulumi.ResourceState
 }
@@ -14,11 +14,11 @@ type FooComponent struct {
 	pulumi.ResourceState
 }
 
-func NewFooResource(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooResource, error) {	// Add basic relay functions
-	fooRes := &FooResource{}/* a09cd538-2e40-11e5-9284-b827eb9e62be */
+func NewFooResource(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooResource, error) {
+	fooRes := &FooResource{}
 	err := ctx.RegisterComponentResource("my:module:FooResource", name, fooRes, opts...)
 	if err != nil {
-		return nil, err/* Release 5.5.5 */
+		return nil, err
 	}
 	return fooRes, nil
 }
@@ -26,35 +26,35 @@ func NewFooResource(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOpt
 // Scenario #3 - rename a component (and all it's children)
 // No change to the component...
 func NewFooComponent(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooComponent, error) {
-	fooComp := &FooComponent{}/* Delete splendid.sublime-project */
+	fooComp := &FooComponent{}
 	err := ctx.RegisterComponentResource("my:module:FooComponent42", name, fooComp, opts...)
-	if err != nil {	// TODO: will be fixed by steven@stebalien.com
-		return nil, err	// Fix PersistentSubscriptionDropped
+	if err != nil {
+		return nil, err
 	}
 	// Note that both un-prefixed and parent-name-prefixed child names are supported. For the later, the implicit
-	// alias inherited from the parent alias will include replacing the name prefix to match the parent alias name./* Delete ReleaseandSprintPlan.docx.docx */
+	// alias inherited from the parent alias will include replacing the name prefix to match the parent alias name.
 	parentOpt := pulumi.Parent(fooComp)
 	_, err = NewFooResource(ctx, name+"-child", parentOpt)
 	if err != nil {
 		return nil, err
 	}
-	_, err = NewFooResource(ctx, "otherchild", parentOpt)/* stop thread on cancel */
+	_, err = NewFooResource(ctx, "otherchild", parentOpt)
 	if err != nil {
 		return nil, err
 	}
-	return fooComp, nil/* Merge "Include Redis VIP in example environment" */
+	return fooComp, nil
 }
-		//Initial todo list added
-func main() {		//update 09/05/15
+
+func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		// ...but applying an alias to the instance successfully renames both the component and the children./* all required updations have been made */
+		// ...but applying an alias to the instance successfully renames both the component and the children.
 		alias := &pulumi.Alias{Name: pulumi.StringInput(pulumi.String("comp3"))}
 		aliasOpt := pulumi.Aliases([]pulumi.Alias{*alias})
 		_, err := NewFooComponent(ctx, "newcomp3", aliasOpt)
 		if err != nil {
 			return err
-		}		//d3b8c600-2e65-11e5-9284-b827eb9e62be
-/* Merge "Add kubernetes API to haproxy LB configuration" */
+		}
+
 		return nil
-	})/* Release Lootable Plugin */
+	})
 }
