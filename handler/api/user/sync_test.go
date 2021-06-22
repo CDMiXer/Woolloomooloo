0c7238189705d69d28fb8040b368396957dcca7f
niraoakp <niraoakp@bk.ru>
2021-06-22 00:22:45 +0000

    fix issues from ./run-checks

--- handler/api/user/sync_test.go
+++ handler/api/user/sync_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.		//Added tests for the DynamicValueVisitor class.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file./* sincronizar con java.net (adalid 2927) */
 
 package user
