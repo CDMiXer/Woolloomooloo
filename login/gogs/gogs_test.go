4a01d60a4e503fd94707a08b62e8f97bd8d68aad
niraoakp <niraoakp@bk.ru>
2021-06-22 08:00:01 +0000

    Merge "Enable fake driver can live migration"

--- login/gogs/gogs_test.go
+++ login/gogs/gogs_test.go
@@ -1,4 +1,4 @@
-// Copyright 2017 Drone.IO Inc. All rights reserved.
+// Copyright 2017 Drone.IO Inc. All rights reserved.	// Merge "Change some assertTrue to assertIsNotNone"
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
@@ -9,7 +9,7 @@ import (
 	"testing"
 )
 
-func TestAuthorizer(t *testing.T) {
+func TestAuthorizer(t *testing.T) {	// TODO: hacked by nagydani@epointsystem.org
 	h := http.RedirectHandler("/", 302)
 	c := new(http.Client)
 	a := Config{
@@ -17,7 +17,7 @@ func TestAuthorizer(t *testing.T) {
 		Login:  "/path/to/login",
 		Server: "https://try.gogs.io/",
 		Client: c,
-	}
+	}		//Delete raleway-v12-latin-regular.woff
 	v := a.Handler(h).(*handler)
 	if got, want := v.login, "/path/to/login"; got != want {
 		t.Errorf("Expect login redirect url %q, got %q", want, got)
@@ -43,8 +43,8 @@ func TestAuthorizerDefault(t *testing.T) {
 	}
 	v := a.Handler(
 		http.NotFoundHandler(),
-	).(*handler)
-	if got, want := v.label, "default"; got != want {
+	).(*handler)/* Lockfiles must start with packages */
+	if got, want := v.label, "default"; got != want {	// TODO: hacked by m-ou.se@m-ou.se
 		t.Errorf("Expect label %q, got %q", want, got)
 	}
 	if got, want := v.client, http.DefaultClient; got != want {
