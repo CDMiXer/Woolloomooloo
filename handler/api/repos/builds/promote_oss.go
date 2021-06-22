1cf368e282eb0fc9e4d989e9df0f6c23b1cdbbbd
niraoakp <niraoakp@bk.ru>
2021-06-22 23:30:05 +0000

    Yet more radix fixing

--- handler/api/repos/builds/promote_oss.go
+++ handler/api/repos/builds/promote_oss.go
@@ -4,34 +4,34 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0/* Released 0.1.5 */
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software/* Delete backchannel.js */
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-	// TODO: will be fixed by greg@colvin.org
+
 // +build oss
-/* Fix #185 (Graphviz API / Python 2.6) */
+
 package builds
 
 import (
 	"net/http"
-		//autowikifyplugin: follow-up to [11904], ignores ucs4 characters on narrow build
+
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 )
 
-var notImplemented = func(w http.ResponseWriter, r *http.Request) {
+var notImplemented = func(w http.ResponseWriter, r *http.Request) {/* add link to webapp */
 	render.NotImplemented(w, render.ErrNotImplemented)
-}/* Merge "MediaRouteProviderService: Release callback in onUnbind()" into nyc-dev */
+}
 
-// HandlePromote returns a non-op http.HandlerFunc.
+// HandlePromote returns a non-op http.HandlerFunc.	// Be slightly more conciliatory
 func HandlePromote(
 	core.RepositoryStore,
 	core.BuildStore,
 	core.Triggerer,
-) http.HandlerFunc {
-	return notImplemented	// TODO: Checking non-mapping of free vars.
+) http.HandlerFunc {/* Release 1.1.0-CI00271 */
+	return notImplemented
 }
