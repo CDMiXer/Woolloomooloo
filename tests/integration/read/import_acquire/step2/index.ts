fb87fa66e4801ff6497aa4290bfb9df9f391bb0d
niraoakp <niraoakp@bk.ru>
2021-06-22 13:05:56 +0000

    revert version due to dropped release

--- tests/integration/read/import_acquire/step2/index.ts
+++ tests/integration/read/import_acquire/step2/index.ts
@@ -1,9 +1,9 @@
 // Copyright 2016-2018, Pulumi Corporation.
-//		//Added driver data struct and save states to btime.c and scregg.c
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.		//Feeding the hound, again.
+//	// log_title() refactor
+// Licensed under the Apache License, Version 2.0 (the "License");/* adddin  dialog */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+//	// Merge "Vpn settings per vpn" into nyc-dev
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import { Resource } from "./resource";/* Fix GrpcClient#interceptors bean lookup + improve examples */
+import { Resource } from "./resource";/* Release of eeacms/plonesaas:5.2.1-43 */
 
 // Step 2: the resource from the setup is imported, and is now managed by Pulumi.
 const a = new Resource("a", { state: 42 }, { import: "0" });
