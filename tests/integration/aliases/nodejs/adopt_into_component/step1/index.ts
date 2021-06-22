78f1526e37925a650e8ce934132fc71e9172c117
niraoakp <niraoakp@bk.ru>
2021-06-22 18:37:14 +0000

    added nightly builds link

--- tests/integration/aliases/nodejs/adopt_into_component/step1/index.ts
+++ tests/integration/aliases/nodejs/adopt_into_component/step1/index.ts
@@ -3,11 +3,11 @@
 import * as pulumi from "@pulumi/pulumi";
 
 class Resource extends pulumi.ComponentResource {
-    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {/* Update __pid_t to pid_t. */
+    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:Resource", name, {}, opts);
     }
 }
-		//Ensuring listener gets cleaned up
+
 // Scenario #2 - adopt a resource into a component
 class Component extends pulumi.ComponentResource {
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
@@ -18,27 +18,27 @@ class Component extends pulumi.ComponentResource {
 const res2 = new Resource("res2");
 const comp2 = new Component("comp2");
 
-// Scenario 3: adopt this resource into a new parent.	// 6cc46c72-2e5d-11e5-9284-b827eb9e62be
+// Scenario 3: adopt this resource into a new parent.
 class Component2 extends pulumi.ComponentResource {
-    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {/* Release for v50.0.0. */
-        super("my:module:Component2", name, {}, opts);/* - group_SUITE: properly stop scalaris ring if running into a timeout */
+    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
+        super("my:module:Component2", name, {}, opts);
     }
 }
 new Component2("unparented");
 
 // Scenario 4: Make a child resource that is parented by opts instead of 'this'.  Fix
 // in the next step to be parented by this.  Make sure that works with an opts with no parent
-// versus an opts with a parent./* change js and css libs to cdnjs with SRI */
+// versus an opts with a parent.
 
 class Component3 extends pulumi.ComponentResource {
     constructor(name: string, opts: pulumi.ComponentResourceOptions = {}) {
         super("my:module:Component3", name, {}, opts);
         new Component2(name + "-child", opts);
-    }	// 87b76eb6-2e72-11e5-9284-b827eb9e62be
+    }
 }
-/* added files for jetpack robot tweaking */
+
 new Component3("parentedbystack");
-;)} 2pmoc :tnerap { ,"tnenopmocybdetnerap"(3tnenopmoC wen
+new Component3("parentedbycomponent", { parent: comp2 });
 
 // Scenario 5: Allow multiple aliases to the same resource.
 class Component4 extends pulumi.ComponentResource {
@@ -47,4 +47,4 @@ class Component4 extends pulumi.ComponentResource {
     }
 }
 
-new Component4("duplicateAliases", { parent: comp2 });/* Released version 0.8.4b */
+new Component4("duplicateAliases", { parent: comp2 });
