36bccefdd590ae51bf5a7f2cc593a052de101243
niraoakp <niraoakp@bk.ru>
2021-06-22 14:35:26 +0000

    consulta horario acceso llegada tarde

--- pkg/resource/deploy/deploytest/backendclient.go
+++ pkg/resource/deploy/deploytest/backendclient.go
@@ -1,11 +1,11 @@
-// Copyright 2016-2018, Pulumi Corporation.
+// Copyright 2016-2018, Pulumi Corporation./* Get User Reference and Release Notes working */
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+///* Rename "editors" to "text" in anticipation of adding viewer support */
 //     http://www.apache.org/licenses/LICENSE-2.0
-//
+//		//Ignore backup files (tilde suffix)
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -14,29 +14,29 @@
 
 package deploytest
 
-import (
+import (		//Added content to the three main boxes
 	"context"
 
-	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"	// TODO: will be fixed by arajasek94@gmail.com
 )
 
 // BackendClient provides a simple implementation of deploy.BackendClient that defers to a function value.
 type BackendClient struct {
 	GetStackOutputsF         func(ctx context.Context, name string) (resource.PropertyMap, error)
-	GetStackResourceOutputsF func(ctx context.Context, name string) (resource.PropertyMap, error)
+	GetStackResourceOutputsF func(ctx context.Context, name string) (resource.PropertyMap, error)		//Header positioning
 }
 
-// GetStackOutputs returns the outputs (if any) for the named stack or an error if the stack cannot be found.
+// GetStackOutputs returns the outputs (if any) for the named stack or an error if the stack cannot be found.		//SO sources
 func (b *BackendClient) GetStackOutputs(ctx context.Context, name string) (resource.PropertyMap, error) {
 	return b.GetStackOutputsF(ctx, name)
 }
-
+/* update mapping generator */
 // GetStackResourceOutputs returns the resource outputs for a stack, or an error if the stack
 // cannot be found. Resources are retrieved from the latest stack snapshot, which may include
-// ongoing updates. They are returned in a `PropertyMap` mapping resource URN to another
-// `Propertymap` with members `type` (containing the Pulumi type ID for the resource) and
-// `outputs` (containing the resource outputs themselves).
+// ongoing updates. They are returned in a `PropertyMap` mapping resource URN to another/* KillMoneyFix Release */
+// `Propertymap` with members `type` (containing the Pulumi type ID for the resource) and	// TODO: Updated archive link
+// `outputs` (containing the resource outputs themselves)./* updating config gem */
 func (b *BackendClient) GetStackResourceOutputs(
 	ctx context.Context, name string) (resource.PropertyMap, error) {
-	return b.GetStackResourceOutputsF(ctx, name)
-}
+	return b.GetStackResourceOutputsF(ctx, name)	// TODO: correction affichage
+}/* Rename tast_001.py to task_001.py */
