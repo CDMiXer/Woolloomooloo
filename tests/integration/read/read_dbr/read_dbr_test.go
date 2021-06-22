b4f4abdbac2ca278b637d609fa332b80a19e5907
niraoakp <niraoakp@bk.ru>
2021-06-22 15:56:22 +0000

    Update headings in Readme

--- tests/integration/read/read_dbr/read_dbr_test.go
+++ tests/integration/read/read_dbr/read_dbr_test.go
@@ -1,11 +1,11 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 // +build nodejs all
 
-package ints
-
-import (
+package ints	// TODO: hacked by alan.shaw@protocol.ai
+/* Forgot to remove 'puts' */
+import (/* Release 1.1 */
 	"testing"
-
+		//Corrected `expects` function example
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 )
 
@@ -13,7 +13,7 @@ import (
 func TestReadDBR(t *testing.T) {
 	integration.ProgramTest(t, &integration.ProgramTestOptions{
 		Dir:          "step1",
-		Dependencies: []string{"@pulumi/pulumi"},
+		Dependencies: []string{"@pulumi/pulumi"},		//rocnetnodedlg: show class mnemonics in the index list
 		Quick:        true,
 		EditDirs: []integration.EditDir{
 			{
@@ -21,7 +21,7 @@ func TestReadDBR(t *testing.T) {
 				Additive: true,
 			},
 			{
-				Dir:      "step3",
+				Dir:      "step3",	// TODO: Update neilpatel-aquisicao-de-clientes-avancado.md
 				Additive: true,
 			},
 		},
