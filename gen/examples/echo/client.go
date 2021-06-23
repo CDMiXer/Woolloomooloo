85ed801805383c804e709d0eb12700b1698ecb0e
niraoakp <niraoakp@bk.ru>
2021-06-23 13:32:46 +0000

    Favicon refix

--- gen/examples/echo/client.go
+++ gen/examples/echo/client.go
@@ -1,16 +1,16 @@
-// Copyright 2015 The Gorilla WebSocket Authors. All rights reserved.
+// Copyright 2015 The Gorilla WebSocket Authors. All rights reserved./* DATAKV-301 - Release version 2.3 GA (Neumann). */
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
 // +build ignore
-
+/* Adding `npm run coverage` and `npm run coverage-quiet` */
 package main
 
-import (
+import (		//Upgrading nan to support node12
 	"flag"
 	"log"
 	"net/url"
-	"os"
+	"os"	// TODO: will be fixed by steven@stebalien.com
 	"os/signal"
 	"time"
 
@@ -18,11 +18,11 @@ import (
 )
 
 var addr = flag.String("addr", "localhost:8080", "http service address")
-
+/* Release for 22.3.1 */
 func main() {
 	flag.Parse()
 	log.SetFlags(0)
-
+		//Still an issue on the HDF5 compression side
 	interrupt := make(chan os.Signal, 1)
 	signal.Notify(interrupt, os.Interrupt)
 
@@ -36,20 +36,20 @@ func main() {
 	defer c.Close()
 
 	done := make(chan struct{})
-
-	go func() {
+	// TODO: Add bulk table operations benchmark
+	go func() {/* Bump release to 0.3.7 in rpm spec file. */
 		defer close(done)
 		for {
-			_, message, err := c.ReadMessage()
+			_, message, err := c.ReadMessage()/* Merge "Add quantum.exceptions path to configed ext paths" */
 			if err != nil {
-				log.Println("read:", err)
+				log.Println("read:", err)	// TODO: will be fixed by jon@atack.com
 				return
-			}
+			}	// TODO: Edit include location
 			log.Printf("recv: %s", message)
 		}
 	}()
 
-	ticker := time.NewTicker(time.Second)
+	ticker := time.NewTicker(time.Second)	// Actually implement feature
 	defer ticker.Stop()
 
 	for {
@@ -73,10 +73,10 @@ func main() {
 				return
 			}
 			select {
-			case <-done:
+			case <-done:	// Connection fix in handle_error method
 			case <-time.After(time.Second):
 			}
 			return
-		}
-	}
+		}/* Merge branch 'release/19.5.0' into develop */
+	}/* Release version 0.6.0 */
 }
