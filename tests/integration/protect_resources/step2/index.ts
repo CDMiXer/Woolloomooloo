c44892ffe9f0df475b7acd10f921f530a64b3037
niraoakp <niraoakp@bk.ru>
2021-06-23 02:25:44 +0000

    cambio siete

--- tests/integration/protect_resources/step2/index.ts
+++ tests/integration/protect_resources/step2/index.ts
@@ -1,6 +1,6 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import { Resource } from "./resource";
-	// Merge branch 'master' into button_label
+
 // Now, simply update the resource; this should work fine:
 let a = new Resource("eternal", { state: 2 }, { protect: true });
