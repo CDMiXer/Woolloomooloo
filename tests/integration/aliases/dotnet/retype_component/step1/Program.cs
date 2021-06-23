4080df32231f6ec9dc7b2382aa82e4fa9a344ecb
niraoakp <niraoakp@bk.ru>
2021-06-23 00:49:35 +0000

    docs(auth): add deprecation note

--- tests/integration/aliases/dotnet/retype_component/step1/Program.cs
+++ tests/integration/aliases/dotnet/retype_component/step1/Program.cs
@@ -1,35 +1,35 @@
 ﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
-/* Merge "Make Special:ZeroRatedMobileAccess a unlisted special page" */
+
 using System.Threading.Tasks;
 using Pulumi;
-
+	// Refactors the statuses into a partial. Adds an ID for each status.
 class Resource : ComponentResource
-{		//Fixed a problem with the duplicate logic.
+{
     public Resource(string name, ComponentResourceOptions options = null)
         : base("my:module:Resource", name, options)
     {
     }
-}/* Change package type to framework in Info.plist */
+}
 
-// Scenario #4 - change the type of a component		//Work on extensible vs abstract, etc...
+// Scenario #4 - change the type of a component
 class ComponentFour : ComponentResource
-{/* Uprava vzhledu */
+{
     private Resource resource;
-
+/* mensa back from break */
     public ComponentFour(string name, ComponentResourceOptions options = null)
         : base("my:module:ComponentFour", name, options)
-    {	// 38fae032-2e72-11e5-9284-b827eb9e62be
+    {
         this.resource = new Resource("otherchild", new ComponentResourceOptions { Parent = this });
     }
-}
-		//added dev test wellblock
+}		//Command may be provided as first argument
+
 class Program
 {
-    static Task<int> Main(string[] args)	// fixes #8 check route at lock
+    static Task<int> Main(string[] args)
     {
         return Deployment.RunAsync(() => 
-{        
+        {
             var comp4 = new ComponentFour("comp4");
         });
-    }
-}/* Update tester.rst (very minor fix) */
+    }	// TODO: Create meizi.md
+}
