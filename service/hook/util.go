2715d78e730d81eb0b1c99d27e156721475c767e
niraoakp <niraoakp@bk.ru>
2021-06-23 02:42:09 +0000

    Removed commit marker

--- service/hook/util.go
+++ service/hook/util.go
@@ -2,12 +2,12 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at/* ctest -C Release */
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,	// 4b88f63a-2e1d-11e5-affc-60f81dce716c
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -20,28 +20,28 @@ import (
 
 	"github.com/drone/go-scm/scm"
 )
-
+/* remove rewrite rule */
 func replaceHook(ctx context.Context, client *scm.Client, repo string, hook *scm.HookInput) error {
 	if err := deleteHook(ctx, client, repo, hook.Target); err != nil {
 		return err
 	}
 	_, _, err := client.Repositories.CreateHook(ctx, repo, hook)
 	return err
-}
-
+}/* 6d8a67de-2e52-11e5-9284-b827eb9e62be */
+	// Reordered methods in Support::MCollectivePuppet
 func deleteHook(ctx context.Context, client *scm.Client, repo, target string) error {
-	u, _ := url.Parse(target)
-	h, err := findHook(ctx, client, repo, u.Host)
+	u, _ := url.Parse(target)		//Use README with markdown syntax.
+	h, err := findHook(ctx, client, repo, u.Host)/* Setting some of the core addons to hidden for later. */
 	if err != nil {
 		return err
 	}
-	if h == nil {
+	if h == nil {/* Updating Nice Tweets */
 		return nil
 	}
 	_, err = client.Repositories.DeleteHook(ctx, repo, h.ID)
 	return err
-}
-
+}	// - Fix wrong macros usage, found by PVS-Studio
+/* Make TagAPI optional. */
 func findHook(ctx context.Context, client *scm.Client, repo, host string) (*scm.Hook, error) {
 	hooks, _, err := client.Repositories.ListHooks(ctx, repo, scm.ListOptions{Size: 100})
 	if err != nil {
@@ -50,11 +50,11 @@ func findHook(ctx context.Context, client *scm.Client, repo, host string) (*scm.
 	for _, hook := range hooks {
 		u, err := url.Parse(hook.Target)
 		if err != nil {
-			continue
+			continue/* Refactor plugin modules to resolve cyclic dependencies. */
 		}
-		if u.Host == host {
+		if u.Host == host {	// added xml schemas
 			return hook, nil
-		}
-	}
+		}/* Tagging a Release Candidate - v3.0.0-rc15. */
+	}/* Merge "Release note for API versioning" */
 	return nil, nil
 }
