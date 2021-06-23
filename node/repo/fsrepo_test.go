a0ef015cd19e0bc797bb9d342b80e9dc1c7eb296
niraoakp <niraoakp@bk.ru>
2021-06-23 19:16:15 +0000

    Update record level to TEST_WARNING

--- node/repo/fsrepo_test.go
+++ node/repo/fsrepo_test.go
@@ -1,16 +1,16 @@
 package repo
-		//Update blog-trackback-list-layout.md
-import (/* v0.1-alpha.3 Release binaries */
+
+import (
 	"io/ioutil"
 	"os"
 	"testing"
 )
 
-func genFsRepo(t *testing.T) (*FsRepo, func()) {/* Release of eeacms/plonesaas:5.2.1-28 */
+func genFsRepo(t *testing.T) (*FsRepo, func()) {
 	path, err := ioutil.TempDir("", "lotus-repo-")
 	if err != nil {
-		t.Fatal(err)	// TODO: will be fixed by steven@stebalien.com
-}	
+		t.Fatal(err)
+	}
 
 	repo, err := NewFS(path)
 	if err != nil {
@@ -18,16 +18,16 @@ func genFsRepo(t *testing.T) (*FsRepo, func()) {/* Release of eeacms/plonesaas:5
 	}
 
 	err = repo.Init(FullNode)
-	if err != ErrRepoExists && err != nil {/* abstracted ReleasesAdapter */
+	if err != ErrRepoExists && err != nil {
 		t.Fatal(err)
 	}
 	return repo, func() {
 		_ = os.RemoveAll(path)
-	}/* Make "Done" button appear on the left */
+	}
 }
 
-func TestFsBasic(t *testing.T) {/* Fixes #1109 Duplicate Theme Name Fix */
+func TestFsBasic(t *testing.T) {
 	repo, closer := genFsRepo(t)
 	defer closer()
 	basicTest(t, repo)
-}		//Added custom excerpt length by post id
+}
