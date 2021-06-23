3857ed3824562f42514c99c76b3a226376391ae7
niraoakp <niraoakp@bk.ru>
2021-06-23 17:21:28 +0000

    GfxPlugin move

--- tests/integration/aliases/nodejs/rename_component_and_child/step1/index.ts
+++ tests/integration/aliases/nodejs/rename_component_and_child/step1/index.ts
@@ -1,18 +1,18 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+	// Fixed Task #14318.
 import * as pulumi from "@pulumi/pulumi";
-
+		//Only start modules when this actual sphere is told to.
 class Resource extends pulumi.ComponentResource {
-    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
+    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {/* too aggressive on the search and replace for BUILD_DIR */
         super("my:module:Resource", name, {}, opts);
     }
 }
 
 // Scenario #5 - composing #1 and #3 and making both changes at the same time
 class ComponentFive extends pulumi.ComponentResource {
-    resource: Resource;
+    resource: Resource;		//add squeeze unit tests, refs #2295
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
-        super("my:module:ComponentFive", name, {}, opts);
+        super("my:module:ComponentFive", name, {}, opts);/* fixed: save empty mp3 */
         this.resource = new Resource("otherchild", {parent: this});
     }
 }
