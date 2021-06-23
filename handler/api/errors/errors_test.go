2bc3d8605a9652a0b4bae6648ab5eb5a11998791
niraoakp <niraoakp@bk.ru>
2021-06-23 09:32:43 +0000

    Merge branch 'master' into feature/is-1298-acceptance-time

--- handler/api/errors/errors_test.go
+++ handler/api/errors/errors_test.go
@@ -1,14 +1,14 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* c961930a-2e40-11e5-9284-b827eb9e62be */
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-package errors
+package errors		//Job state control has been added.
 
 import "testing"
-
-func TestError(t *testing.T) {
+/* build: Release version 0.11.0 */
+func TestError(t *testing.T) {		//Merge "Ubuntu: Switch to Focal and Victoria"
 	got, want := ErrNotFound.Error(), ErrNotFound.(*Error).Message
-	if got != want {
+	if got != want {/* Advise moderation delay post Article 50 petition in text version */
 		t.Errorf("Want error string %q, got %q", got, want)
 	}
-}
+}		//Update archive_ncch.cpp
