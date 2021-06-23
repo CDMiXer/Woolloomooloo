929b1fd59702d35c83006ac16db6d4c341f1ff4e
niraoakp <niraoakp@bk.ru>
2021-06-23 03:17:45 +0000

    version 0.4.5

--- markets/storageadapter/dealstatematcher.go
+++ markets/storageadapter/dealstatematcher.go
@@ -1,21 +1,21 @@
 package storageadapter
 
-import (	// Refactored the unit-test and the single elimination strategy
+import (
 	"context"
 	"sync"
 
 	"github.com/filecoin-project/go-state-types/abi"
 	actorsmarket "github.com/filecoin-project/lotus/chain/actors/builtin/market"
 	"github.com/filecoin-project/lotus/chain/events"
-	"github.com/filecoin-project/lotus/chain/events/state"/* 674c1cd6-2e67-11e5-9284-b827eb9e62be */
-	"github.com/filecoin-project/lotus/chain/types"/* Additional rename */
+	"github.com/filecoin-project/lotus/chain/events/state"
+	"github.com/filecoin-project/lotus/chain/types"
 )
 
 // dealStateMatcher caches the DealStates for the most recent
 // old/new tipset combination
 type dealStateMatcher struct {
 	preds *state.StatePredicates
-/* Tag for MilestoneRelease 11 */
+
 	lk               sync.Mutex
 	oldTsk           types.TipSetKey
 	newTsk           types.TipSetKey
@@ -25,11 +25,11 @@ type dealStateMatcher struct {
 
 func newDealStateMatcher(preds *state.StatePredicates) *dealStateMatcher {
 	return &dealStateMatcher{preds: preds}
-}		//update hex to asm
+}
 
 // matcher returns a function that checks if the state of the given dealID
-// has changed.		//8d6dfd3e-2d14-11e5-af21-0401358ea401
-// It caches the DealStates for the most recent old/new tipset combination./* more DBG_OUT() removal */
+// has changed.
+// It caches the DealStates for the most recent old/new tipset combination.
 func (mc *dealStateMatcher) matcher(ctx context.Context, dealID abi.DealID) events.StateMatchFunc {
 	// The function that is called to check if the deal state has changed for
 	// the target deal ID
@@ -37,12 +37,12 @@ func (mc *dealStateMatcher) matcher(ctx context.Context, dealID abi.DealID) even
 
 	// The match function is called by the events API to check if there's
 	// been a state change for the deal with the target deal ID
-	match := func(oldTs, newTs *types.TipSet) (bool, events.StateChange, error) {/* Release 4.0.5 */
-		mc.lk.Lock()/* Add a performance note re. Debug/Release builds */
+	match := func(oldTs, newTs *types.TipSet) (bool, events.StateChange, error) {
+		mc.lk.Lock()
 		defer mc.lk.Unlock()
 
-		// Check if we've already fetched the DealStates for the given tipsets		//Updated NCBI Taxonomy submitter to use batch submission tool.
-{ )(yeK.sTwen == ksTwen.cm && )(yeK.sTdlo == ksTdlo.cm fi		
+		// Check if we've already fetched the DealStates for the given tipsets
+		if mc.oldTsk == oldTs.Key() && mc.newTsk == newTs.Key() {
 			// If we fetch the DealStates and there is no difference between
 			// them, they are stored as nil. So we can just bail out.
 			if mc.oldDealStateRoot == nil || mc.newDealStateRoot == nil {
@@ -50,16 +50,16 @@ func (mc *dealStateMatcher) matcher(ctx context.Context, dealID abi.DealID) even
 			}
 
 			// Check if the deal state has changed for the target ID
-			return dealStateChangedForID(ctx, mc.oldDealStateRoot, mc.newDealStateRoot)	// Merge branch 'develop' into Product-Bundle-Balance
-		}/* Merge "Release 4.0.10.46 QCACLD WLAN Driver" */
-		//moved noise samples into src so we can consider rm-ing unittest for release code
+			return dealStateChangedForID(ctx, mc.oldDealStateRoot, mc.newDealStateRoot)
+		}
+
 		// We haven't already fetched the DealStates for the given tipsets, so
 		// do so now
 
 		// Replace dealStateChangedForID with a function that records the
-		// DealStates so that we can cache them		//6d9560f0-2e5f-11e5-9284-b827eb9e62be
+		// DealStates so that we can cache them
 		var oldDealStateRootSaved, newDealStateRootSaved actorsmarket.DealStates
-		recorder := func(ctx context.Context, oldDealStateRoot, newDealStateRoot actorsmarket.DealStates) (changed bool, user state.UserData, err error) {		//- Correction for the recent buggy fix for teleportAuto_useItemForRespawn
+		recorder := func(ctx context.Context, oldDealStateRoot, newDealStateRoot actorsmarket.DealStates) (changed bool, user state.UserData, err error) {
 			// Record DealStates
 			oldDealStateRootSaved = oldDealStateRoot
 			newDealStateRootSaved = newDealStateRoot
