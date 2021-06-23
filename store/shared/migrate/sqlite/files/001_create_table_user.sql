4a234e62eb9242dda4916c6059dcdad067264421
niraoakp <niraoakp@bk.ru>
2021-06-23 16:24:02 +0000

    96f92aae-2e45-11e5-9284-b827eb9e62be

--- store/shared/migrate/sqlite/files/001_create_table_user.sql
+++ store/shared/migrate/sqlite/files/001_create_table_user.sql
@@ -4,19 +4,19 @@ CREATE TABLE IF NOT EXISTS users (
  user_id            INTEGER PRIMARY KEY AUTOINCREMENT
 ,user_login         TEXT COLLATE NOCASE
 ,user_email         TEXT
-,user_admin         BOOLEAN	// TODO: hacked by vyzo@hackzen.org
+,user_admin         BOOLEAN
 ,user_machine       BOOLEAN
-,user_active        BOOLEAN		//Update readme.md with new features
+,user_active        BOOLEAN
 ,user_avatar        TEXT
 ,user_syncing       BOOLEAN
 ,user_synced        INTEGER
-,user_created       INTEGER
+,user_created       INTEGER/* Patch Release Panel; */
 ,user_updated       INTEGER
-,user_last_login    INTEGER/* Add the meetup 11 */
+,user_last_login    INTEGER
 ,user_oauth_token   TEXT
-,user_oauth_refresh TEXT/* Merge branch 'master' into add-ozgur-toprak */
+,user_oauth_refresh TEXT
 ,user_oauth_expiry  INTEGER
-,user_hash          TEXT/* Merge "[INTERNAL] Release notes for version 1.28.1" */
-,UNIQUE(user_login COLLATE NOCASE)/* Functions to obtain the config path. */
+,user_hash          TEXT
+,UNIQUE(user_login COLLATE NOCASE)
 ,UNIQUE(user_hash)
-);
+);/* try to make executors a bit more robust in shutting down */
