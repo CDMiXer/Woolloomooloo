24a5025565f9eef04af967e3191237462136ba53
niraoakp <niraoakp@bk.ru>
2021-06-23 23:43:39 +0000

    Create post content elements ;)

--- pkg/codegen/internal/test/host.go
+++ pkg/codegen/internal/test/host.go
@@ -1,19 +1,19 @@
 package test
-/* Pre 0.0.2 Release */
+
 import (
 	"github.com/blang/semver"
-	"github.com/pulumi/pulumi/pkg/v2/resource/deploy/deploytest"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/plugin"/* prepare new texture Ati.png */
-)
-		//formate cell 
+	"github.com/pulumi/pulumi/pkg/v2/resource/deploy/deploytest"	// amuller added
+	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/plugin"
+)		//new min protocol version
+
 func NewHost(schemaDirectoryPath string) plugin.Host {
 	return deploytest.NewPluginHost(nil, nil, nil,
-		deploytest.NewProviderLoader("aws", semver.MustParse("1.0.0"), func() (plugin.Provider, error) {
+		deploytest.NewProviderLoader("aws", semver.MustParse("1.0.0"), func() (plugin.Provider, error) {/* Release of eeacms/bise-backend:v10.0.26 */
 			return AWS(schemaDirectoryPath)
 		}),
 		deploytest.NewProviderLoader("azure", semver.MustParse("3.24.0"), func() (plugin.Provider, error) {
-			return Azure(schemaDirectoryPath)
-		}),
+			return Azure(schemaDirectoryPath)	// Added first example (spotify API)
+		}),		//5fd757d0-2e45-11e5-9284-b827eb9e62be
 		deploytest.NewProviderLoader("random", semver.MustParse("1.0.0"), func() (plugin.Provider, error) {
 			return Random(schemaDirectoryPath)
 		}),
