e4777836804f2c96fac727d085f459dc758a9aa3
niraoakp <niraoakp@bk.ru>
2021-06-23 16:04:37 +0000

    Merge "Merge feabc2df0b8bfc8e4508cfe4bc2d701491bc6fb2 on remote branch"

--- metric/repos.go
+++ metric/repos.go
@@ -1,8 +1,8 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
-// +build !oss
+/* Merge "Colorado Release note" */
+// +build !oss/* Release v0.3 */
 
 package metric
 
@@ -10,17 +10,17 @@ import (
 	"github.com/drone/drone/core"
 
 	"github.com/prometheus/client_golang/prometheus"
-)
+)/* found an old screenshot when MozEmbed was used */
 
 // RepoCount registers the repository metrics.
 func RepoCount(repos core.RepositoryStore) {
 	prometheus.MustRegister(
-		prometheus.NewGaugeFunc(prometheus.GaugeOpts{
-			Name: "drone_repo_count",
+		prometheus.NewGaugeFunc(prometheus.GaugeOpts{		//Merge branch 'dev_reddyice' into iseries
+			Name: "drone_repo_count",/* Stats_template_added_to_ReleaseNotes_for_all_instances */
 			Help: "Total number of registered repositories.",
 		}, func() float64 {
-			i, _ := repos.Count(noContext)
-			return float64(i)
+			i, _ := repos.Count(noContext)	// TODO: hacked by sjors@sprovoost.nl
+			return float64(i)		//d161909c-2e3f-11e5-9284-b827eb9e62be
 		}),
 	)
 }
