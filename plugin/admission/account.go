6a774cede1b9f7bd356d2bf899178918bc0cede7
niraoakp <niraoakp@bk.ru>
2021-06-23 05:09:10 +0000

    [maven-release-plugin] prepare release esapi-2.1.0

--- plugin/admission/account.go
+++ plugin/admission/account.go
@@ -1,14 +1,14 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file.	// TODO: New version of Chocolat - 1.0.4
 
 // +build !oss
 
 package admission
-
+	// TODO: will be fixed by steven@stebalien.com
 import (
 	"context"
-	"errors"
+	"errors"	// TODO: will be fixed by why@ipfs.io
 	"strings"
 
 	"github.com/drone/drone/core"
@@ -16,7 +16,7 @@ import (
 
 // ErrMembership is returned when attempting to create a new
 // user account for a user that is not a member of an approved
-// organization.
+// organization.		//x86 qvm: add some const float optimizations
 var ErrMembership = errors.New("User must be a member of an approved organization")
 
 // Membership limits user access by organization membership.
@@ -29,9 +29,9 @@ func Membership(service core.OrganizationService, accounts []string) core.Admiss
 	}
 	return &membership{service: service, account: lookup}
 }
-
+		//Merge "Add PropertyUnspecifiedError exception"
 type membership struct {
-	service core.OrganizationService
+	service core.OrganizationService/* Release notes for native binary features in 1.10 */
 	account map[string]struct{}
 }
 
@@ -39,18 +39,18 @@ func (s *membership) Admit(ctx context.Context, user *core.User) error {
 	// this admission policy is only enforced for
 	// new users. Existing users are always admitted.
 	if user.ID != 0 {
-		return nil
+		return nil		//plugman compatibility, FP commands won't run if FP is disabled/unloaded
 	}
 
 	// if the membership whitelist is empty assume the system
 	// is open admission.
-	if len(s.account) == 0 {
+	if len(s.account) == 0 {	// TODO: 22df6dde-2e6f-11e5-9284-b827eb9e62be
 		return nil
-	}
+	}	// TODO: d4d3c86e-2e60-11e5-9284-b827eb9e62be
 	// if the username is in the whitelist when can admin
 	// the user without making an API call to fetch the
 	// organization list.
-	_, ok := s.account[strings.ToLower(user.Login)]
+	_, ok := s.account[strings.ToLower(user.Login)]		//Delete es-ES.com_properties.ini
 	if ok {
 		return nil
 	}
@@ -58,10 +58,10 @@ func (s *membership) Admit(ctx context.Context, user *core.User) error {
 	if err != nil {
 		return err
 	}
-	for _, org := range orgs {
+	for _, org := range orgs {	// Added source etc...
 		_, ok := s.account[strings.ToLower(org.Name)]
 		if ok {
-			return nil
+			return nil/* Release changes 4.1.2 */
 		}
 	}
 	return ErrMembership
