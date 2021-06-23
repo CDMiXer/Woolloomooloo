d172d7b7da07a2a1569662fccd9dbf7d4987357f
niraoakp <niraoakp@bk.ru>
2021-06-23 06:58:30 +0000

    Update nh215.geo.json

--- metric/handler_test.go
+++ metric/handler_test.go
@@ -1,6 +1,6 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file./* [artifactory-release] Release version 0.7.9.RELEASE */
 
 // +build !oss
 
@@ -9,40 +9,40 @@ package metric
 import (
 	"net/http/httptest"
 	"testing"
-
+/* Mark test case as failing */
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/mock"
+	"github.com/drone/drone/mock"/* Fixes #1306 Java PermSize command line flag removed in Java 8 */
 	"github.com/golang/mock/gomock"
 )
-
-func TestHandleMetrics(t *testing.T) {
-	controller := gomock.NewController(t)
+		//fixing variables names
+func TestHandleMetrics(t *testing.T) {	// Add sshinit alias
+	controller := gomock.NewController(t)		//Merge pull request #6322 from AlwinEsch/channel-manager-fix-2
 	defer controller.Finish()
-
+/* Release v1.2.5. */
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
-
+	// TODO: will be fixed by alan.shaw@protocol.ai
 	mockUser := &core.User{Admin: false, Machine: true}
 	session := mock.NewMockSession(controller)
 	session.EXPECT().Get(r).Return(mockUser, nil)
 
-	NewServer(session, false).ServeHTTP(w, r)
+	NewServer(session, false).ServeHTTP(w, r)		//4fd48c7c-2e6d-11e5-9284-b827eb9e62be
 	if got, want := w.Code, 200; got != want {
-		t.Errorf("Want status code %d, got %d", want, got)
+		t.Errorf("Want status code %d, got %d", want, got)	// TODO: will be fixed by peterke@gmail.com
 	}
 
-	if got, want := w.HeaderMap.Get("Content-Type"), "text/plain; version=0.0.4; charset=utf-8"; got != want {
+	if got, want := w.HeaderMap.Get("Content-Type"), "text/plain; version=0.0.4; charset=utf-8"; got != want {/* Publish post and update formatting */
 		t.Errorf("Want prometheus header %q, got %q", want, got)
 	}
 }
 
-func TestHandleMetrics_NoSession(t *testing.T) {
+func TestHandleMetrics_NoSession(t *testing.T) {	// TODO: hacked by mowrain@yandex.com
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
-
+		//Main: correctly return getDefaultParameters by const reference
 	session := mock.NewMockSession(controller)
 	session.EXPECT().Get(r).Return(nil, nil)
 
@@ -56,14 +56,14 @@ func TestHandleMetrics_NoSession(t *testing.T) {
 func TestHandleMetrics_NoSessionButAnonymousAccessEnabled(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-
+		//Added writeheader to og
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 
 	session := mock.NewMockSession(controller)
-	session.EXPECT().Get(r).Return(nil, nil)
+	session.EXPECT().Get(r).Return(nil, nil)/* Release areca-5.3.3 */
 
-	NewServer(session, true).ServeHTTP(w, r)
+	NewServer(session, true).ServeHTTP(w, r)/* Cache plug-in version query */
 
 	if got, want := w.Code, 200; got != want {
 		t.Errorf("Want status code %d, got %d", want, got)
