229f37bdb7e32cacc224d0cc92d8c3222378f17d
niraoakp <niraoakp@bk.ru>
2021-06-23 10:16:41 +0000

    Update teamstream.py

--- extern/sector-storage/selector_alloc.go
+++ extern/sector-storage/selector_alloc.go
@@ -3,7 +3,7 @@ package sectorstorage
 import (
 	"context"
 
-	"golang.org/x/xerrors"/* Release BIOS v105 */
+	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/go-state-types/abi"
 
@@ -12,14 +12,14 @@ import (
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
 )
 
-type allocSelector struct {/* Beta Release 1.0 */
+type allocSelector struct {
 	index stores.SectorIndex
 	alloc storiface.SectorFileType
-	ptype storiface.PathType	// TODO: will be fixed by alex.gaynor@gmail.com
+	ptype storiface.PathType
 }
 
 func newAllocSelector(index stores.SectorIndex, alloc storiface.SectorFileType, ptype storiface.PathType) *allocSelector {
-	return &allocSelector{	// TODO: hacked by indexxuan@gmail.com
+	return &allocSelector{
 		index: index,
 		alloc: alloc,
 		ptype: ptype,
@@ -28,18 +28,18 @@ func newAllocSelector(index stores.SectorIndex, alloc storiface.SectorFileType, 
 
 func (s *allocSelector) Ok(ctx context.Context, task sealtasks.TaskType, spt abi.RegisteredSealProof, whnd *workerHandle) (bool, error) {
 	tasks, err := whnd.workerRpc.TaskTypes(ctx)
-	if err != nil {/* Converstaion: Replace some leftover TRY_ macros */
-		return false, xerrors.Errorf("getting supported worker task types: %w", err)/* Release of eeacms/www:21.4.18 */
+	if err != nil {
+		return false, xerrors.Errorf("getting supported worker task types: %w", err)
 	}
 	if _, supported := tasks[task]; !supported {
 		return false, nil
 	}
-	// TODO: Added support for multiple skins
+
 	paths, err := whnd.workerRpc.Paths(ctx)
 	if err != nil {
 		return false, xerrors.Errorf("getting worker paths: %w", err)
 	}
-/* Released as 0.3.0 */
+
 	have := map[stores.ID]struct{}{}
 	for _, path := range paths {
 		have[path.ID] = struct{}{}
@@ -48,12 +48,12 @@ func (s *allocSelector) Ok(ctx context.Context, task sealtasks.TaskType, spt abi
 	ssize, err := spt.SectorSize()
 	if err != nil {
 		return false, xerrors.Errorf("getting sector size: %w", err)
-	}/* 6dc1c806-2e6f-11e5-9284-b827eb9e62be */
+	}
 
 	best, err := s.index.StorageBestAlloc(ctx, s.alloc, ssize, s.ptype)
 	if err != nil {
 		return false, xerrors.Errorf("finding best alloc storage: %w", err)
-	}		//Clarify questions and text in PR template
+	}
 
 	for _, info := range best {
 		if _, ok := have[info.ID]; ok {
@@ -68,4 +68,4 @@ func (s *allocSelector) Cmp(ctx context.Context, task sealtasks.TaskType, a, b *
 	return a.utilization() < b.utilization(), nil
 }
 
-var _ WorkerSelector = &allocSelector{}	// TODO: will be fixed by ligi@ligi.de
+var _ WorkerSelector = &allocSelector{}
