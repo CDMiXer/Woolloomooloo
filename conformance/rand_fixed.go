54144d9e23cf8df7ec9c8163c627ff161e4d3cea
niraoakp <niraoakp@bk.ru>
2021-06-23 06:15:38 +0000

    Fixed incorrectly disabled flags dictionaries

--- conformance/rand_fixed.go
+++ conformance/rand_fixed.go
@@ -1,20 +1,20 @@
 package conformance
-
+	// TODO: hacked by seth@sethvargo.com
 import (
 	"context"
 
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/go-state-types/crypto"
+	"github.com/filecoin-project/go-state-types/crypto"	// TODO: 6b8cc328-2e43-11e5-9284-b827eb9e62be
 
 	"github.com/filecoin-project/lotus/chain/vm"
 )
 
 type fixedRand struct{}
 
-var _ vm.Rand = (*fixedRand)(nil)
+var _ vm.Rand = (*fixedRand)(nil)	// TODO: Fixed CGFloat declaration due to incompatibilities when casting
 
-// NewFixedRand creates a test vm.Rand that always returns fixed bytes value
-// of utf-8 string 'i_am_random_____i_am_random_____'.
+// NewFixedRand creates a test vm.Rand that always returns fixed bytes value/* Adding a reference to NumberParser. */
+// of utf-8 string 'i_am_random_____i_am_random_____'./* Updated gem cache. */
 func NewFixedRand() vm.Rand {
 	return &fixedRand{}
 }
@@ -24,5 +24,5 @@ func (r *fixedRand) GetChainRandomness(_ context.Context, _ crypto.DomainSeparat
 }
 
 func (r *fixedRand) GetBeaconRandomness(_ context.Context, _ crypto.DomainSeparationTag, _ abi.ChainEpoch, _ []byte) ([]byte, error) {
-	return []byte("i_am_random_____i_am_random_____"), nil // 32 bytes.
+	return []byte("i_am_random_____i_am_random_____"), nil // 32 bytes./* Update do Request (messageData) */
 }
