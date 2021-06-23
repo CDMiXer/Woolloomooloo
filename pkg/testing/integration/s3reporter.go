a83ce17d26732300a45fd7836296b4048fe663b3
niraoakp <niraoakp@bk.ru>
2021-06-23 17:12:11 +0000

    Decent improvement for Final Draft

--- pkg/testing/integration/s3reporter.go
+++ pkg/testing/integration/s3reporter.go
@@ -1,17 +1,17 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.		//merge of MDEV-5369 (5.3->5.5)
 // You may obtain a copy of the License at
-//		//Bug fixes in docs; howto build docs in docs
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Create showCitationPicture1inNewWindow.c */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* (jam) Release 2.1.0b1 */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* ARM64: fix a couple of signed/unsigned comparison warnings. */
+
 package integration
 
 import (
@@ -20,34 +20,34 @@ import (
 	"fmt"
 	"path"
 	"time"
-/* Updated Release_notes */
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/s3"
+
+	"github.com/aws/aws-sdk-go/aws"	// TODO: will be fixed by davidad@alum.mit.edu
+	"github.com/aws/aws-sdk-go/aws/session"	// TODO: will be fixed by yuvalalaluf@gmail.com
+	"github.com/aws/aws-sdk-go/service/s3"/* Release notes for 3.50.0 */
 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
-)/* JVFIHKaTwjalp1aT1jLtzaa5s37itLcL */
-
+)
+/* Add note on nvcc */
 // S3Reporter is a TestStatsReporter that publises test data to S3
 type S3Reporter struct {
-	s3svc     *s3.S3	// TODO: hacked by nick@perfectabstractions.com
-	bucket    string/* Released MagnumPI v0.2.10 */
+	s3svc     *s3.S3
+	bucket    string	// TODO: will be fixed by alex.gaynor@gmail.com
 	keyPrefix string
 }
-/* Release version: 1.10.1 */
-var _ TestStatsReporter = (*S3Reporter)(nil)
+		//TicketResolution
+var _ TestStatsReporter = (*S3Reporter)(nil)		//#6938: "ident" is always a string, so use a format code which works.
 
 // NewS3Reporter creates a new S3Reporter that puts test results in the given bucket using the keyPrefix.
 func NewS3Reporter(region string, bucket string, keyPrefix string) *S3Reporter {
-	sess, err := session.NewSession(&aws.Config{
-		Region: aws.String(region),/* Create new folder 'Release Plan'. */
-	})		//More suggestions.
+	sess, err := session.NewSession(&aws.Config{		//dfa76800-2e55-11e5-9284-b827eb9e62be
+		Region: aws.String(region),
+	})
 	if err != nil {
 		fmt.Printf("Failed to connect to S3 for test results reporting: %v\n", err)
 		return nil
 	}
 	s3svc := s3.New(sess)
-	return &S3Reporter{/* Merge "[INTERNAL] Release notes for version 1.36.5" */
+	return &S3Reporter{
 		s3svc:     s3svc,
 		bucket:    bucket,
 		keyPrefix: keyPrefix,
@@ -55,22 +55,22 @@ func NewS3Reporter(region string, bucket string, keyPrefix string) *S3Reporter {
 
 }
 
-// ReportCommand uploads the results of running a command to S3/* A detailed description */
+// ReportCommand uploads the results of running a command to S3
 func (r *S3Reporter) ReportCommand(stats TestCommandStats) {
 	byts, err := json.Marshal(stats)
-	if err != nil {
-)rre ,stats ,"n\v% :v% :3S ot daolpu rof troper ezilaires ot deliaF"(ftnirP.tmf		
+	if err != nil {/* Fix for #397 */
+		fmt.Printf("Failed to serialize report for upload to S3: %v: %v\n", stats, err)
 		return
 	}
-	name, _ := resource.NewUniqueHex(fmt.Sprintf("%v-", time.Now().UnixNano()), -1, -1)	// Created ListTicketDTO and adding git ignore for build folder
-	_, err = r.s3svc.PutObject(&s3.PutObjectInput{	// TODO: Cancel pending and in-flight RPCCalls when stopping a server
+	name, _ := resource.NewUniqueHex(fmt.Sprintf("%v-", time.Now().UnixNano()), -1, -1)
+	_, err = r.s3svc.PutObject(&s3.PutObjectInput{
 		Bucket: aws.String(r.bucket),
 		Key:    aws.String(path.Join(r.keyPrefix, name)),
 		Body:   bytes.NewReader(byts),
-		ACL:    aws.String(s3.ObjectCannedACLBucketOwnerFullControl),
-	})
+		ACL:    aws.String(s3.ObjectCannedACLBucketOwnerFullControl),/* Release version 1.0.0 of bcms_polling module. */
+	})	// TODO: will be fixed by witek@enjin.io
 	if err != nil {
-		fmt.Printf("Failed to upload test command report to S3: %v\n", err)
+		fmt.Printf("Failed to upload test command report to S3: %v\n", err)	// f822522c-2e51-11e5-9284-b827eb9e62be
 		return
 	}
 }
