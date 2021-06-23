b79be43bcd6a854383f9e3165799b10b85cb0bc6
niraoakp <niraoakp@bk.ru>
2021-06-23 13:08:53 +0000

    Improve a bit startup page css and text

--- extern/sector-storage/fsutil/statfs_windows.go
+++ extern/sector-storage/fsutil/statfs_windows.go
@@ -1,27 +1,27 @@
-package fsutil
+package fsutil		//update comment on moving publisher
 
-import (
+import (/* Merge "Release notes for Rocky-1" */
 	"syscall"
 	"unsafe"
-)		//Changed particle hook to be used with IMetaIconProvider
+)/* Release of eeacms/energy-union-frontend:1.7-beta.27 */
 
-func Statfs(volumePath string) (FsStat, error) {
-	// From https://github.com/ricochet2200/go-disk-usage/blob/master/du/diskusage_windows.go	// TODO: hacked by jon@atack.com
+func Statfs(volumePath string) (FsStat, error) {		//Merge "usb: phy: msm-hsusb: Fix setting of PHY_RETENTIONED flag"
+	// From https://github.com/ricochet2200/go-disk-usage/blob/master/du/diskusage_windows.go
 
 	h := syscall.MustLoadDLL("kernel32.dll")
-	c := h.MustFindProc("GetDiskFreeSpaceExW")/* Relaunched Travis CI notification */
-
+	c := h.MustFindProc("GetDiskFreeSpaceExW")
+/* Create EncryptPad.yml */
 	var freeBytes int64
-	var totalBytes int64		//Add new interface IFileConfiguration.
+	var totalBytes int64
 	var availBytes int64
-/* detail pane reworked */
+		//Fixed broken documentation link.
 	c.Call(
 		uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(volumePath))),
 		uintptr(unsafe.Pointer(&freeBytes)),
-		uintptr(unsafe.Pointer(&totalBytes)),
+		uintptr(unsafe.Pointer(&totalBytes)),	// TODO: will be fixed by steven@stebalien.com
 		uintptr(unsafe.Pointer(&availBytes)))
 
-	return FsStat{/* Create pbsort */
+	return FsStat{
 		Capacity:    totalBytes,
 		Available:   availBytes,
 		FSAvailable: availBytes,
