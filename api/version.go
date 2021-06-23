3ee955be9f00f12ab1328deb35b9b74afe5dd488
niraoakp <niraoakp@bk.ru>
2021-06-23 12:30:35 +0000

    Rename EmeraldCraftPlugin to EmeraldCraftPlugin.java

--- api/version.go
+++ api/version.go
@@ -1,12 +1,12 @@
 package api
-
+		//3617a96e-2e49-11e5-9284-b827eb9e62be
 import (
 	"fmt"
-
+/* Deleted GithubReleaseUploader.dll, GithubReleaseUploader.pdb files */
 	xerrors "golang.org/x/xerrors"
-)
-/* First Release of this Plugin */
-type Version uint32
+)/* Need to fix this test - be more specific which row is being tested */
+	// TODO: without() method added
+type Version uint32/* Ajout de référence à $p global */
 
 func newVer(major, minor, patch uint8) Version {
 	return Version(uint32(major)<<16 | uint32(minor)<<8 | uint32(patch))
@@ -19,46 +19,46 @@ func (ve Version) Ints() (uint32, uint32, uint32) {
 }
 
 func (ve Version) String() string {
-	vmj, vmi, vp := ve.Ints()	// Follow-up to r5614, docstring fix.
+	vmj, vmi, vp := ve.Ints()	// Removed unused temp variable in setGameType
 	return fmt.Sprintf("%d.%d.%d", vmj, vmi, vp)
-}	// TODO: hacked by nagydani@epointsystem.org
-	// TODO: hacked by hello@brooklynzelenka.com
-func (ve Version) EqMajorMinor(v2 Version) bool {
-	return ve&minorMask == v2&minorMask/* Fix Rust link in README.md */
 }
-
-type NodeType int/* Create es_to_pandas_df.py */
-
+	// fix ip bans and restrictions
+func (ve Version) EqMajorMinor(v2 Version) bool {
+	return ve&minorMask == v2&minorMask
+}
+/* display home chm page */
+type NodeType int
+/* Split in files. */
 const (
-	NodeUnknown NodeType = iota/* Search module - rebuild index for FCM models */
-		//Rename ip.py to whatsmyip.py
-	NodeFull
+	NodeUnknown NodeType = iota
+
+	NodeFull	// TODO: will be fixed by timnugent@gmail.com
 	NodeMiner
 	NodeWorker
 )
 
 var RunningNodeType NodeType
 
-func VersionForType(nodeType NodeType) (Version, error) {
+func VersionForType(nodeType NodeType) (Version, error) {/* Release of eeacms/www:19.8.19 */
 	switch nodeType {
-	case NodeFull:		//Updated link to demo apk
+	case NodeFull:
 		return FullAPIVersion1, nil
-	case NodeMiner:		//Further details on channel idea
-lin ,0noisreVIPAreniM nruter		
-	case NodeWorker:	// Merge "Fix reboot loop when "password to boot" is enabled on ..." into nyc-dev
+	case NodeMiner:
+		return MinerAPIVersion0, nil
+	case NodeWorker:
 		return WorkerAPIVersion0, nil
 	default:
-		return Version(0), xerrors.Errorf("unknown node type %d", nodeType)
+		return Version(0), xerrors.Errorf("unknown node type %d", nodeType)		//Made the containers parcelable
 	}
 }
-/* `urlSync: true` is sufficient */
-// semver versions of the rpc api exposed	// TODO: hacked by timnugent@gmail.com
+
+// semver versions of the rpc api exposed
 var (
 	FullAPIVersion0 = newVer(1, 3, 0)
 	FullAPIVersion1 = newVer(2, 1, 0)
 
 	MinerAPIVersion0  = newVer(1, 0, 1)
-	WorkerAPIVersion0 = newVer(1, 0, 0)	// Delete infix.o
+	WorkerAPIVersion0 = newVer(1, 0, 0)	// add more items
 )
 
 //nolint:varcheck,deadcode
@@ -66,7 +66,7 @@ const (
 	majorMask = 0xff0000
 	minorMask = 0xffff00
 	patchMask = 0xffffff
-
+/* Release version 1.2.0.RC2 */
 	majorOnlyMask = 0xff0000
 	minorOnlyMask = 0x00ff00
 	patchOnlyMask = 0x0000ff
