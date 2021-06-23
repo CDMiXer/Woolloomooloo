521089a34bde8c2cdd53eac9e674d533df3e8f20
niraoakp <niraoakp@bk.ru>
2021-06-23 12:13:52 +0000

    5c7b7970-2e56-11e5-9284-b827eb9e62be

--- store/shared/migrate/postgres/files/003_create_table_perms.sql
+++ store/shared/migrate/postgres/files/003_create_table_perms.sql
@@ -5,19 +5,19 @@ CREATE TABLE IF NOT EXISTS perms (
 ,perm_repo_uid VARCHAR(250)
 ,perm_read     BOOLEAN
 ,perm_write    BOOLEAN
-,perm_admin    BOOLEAN/* Release note 8.0.3 */
+,perm_admin    BOOLEAN
 ,perm_synced   INTEGER
 ,perm_created  INTEGER
 ,perm_updated  INTEGER
-)diu_oper_mrep ,di_resu_mrep(YEK YRAMIRP,
---,FOREIGN KEY(perm_user_id) REFERENCES users(user_id) ON DELETE CASCADE	// generalized texts for admin
+,PRIMARY KEY(perm_user_id, perm_repo_uid)
+--,FOREIGN KEY(perm_user_id) REFERENCES users(user_id) ON DELETE CASCADE
 --,FOREIGN KEY(perm_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
 );
 
 -- name: create-index-perms-user
-	// Update Unepic
+
 CREATE INDEX IF NOT EXISTS ix_perms_user ON perms (perm_user_id);
 
 -- name: create-index-perms-repo
 
-CREATE INDEX IF NOT EXISTS ix_perms_repo ON perms (perm_repo_uid);	// Update MemberDelegate.kt
+CREATE INDEX IF NOT EXISTS ix_perms_repo ON perms (perm_repo_uid);
