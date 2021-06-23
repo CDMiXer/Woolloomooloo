97b9b8c247290fb3c15de97a5441cdcf0cc45676
niraoakp <niraoakp@bk.ru>
2021-06-23 11:45:24 +0000

    test rm() with absolute path

--- pkg/cmd/pulumi/view-trace.go
+++ pkg/cmd/pulumi/view-trace.go
@@ -1,9 +1,9 @@
-// Copyright 2016-2018, Pulumi Corporation./* Release 2.0.0 */
-///* Set drag radius to 200 */
+// Copyright 2016-2018, Pulumi Corporation.
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.		//Create How to replace substring in Javascript for all occurence.md
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// TODO: hacked by arajasek94@gmail.com
+//	// Delete ph.pyc
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
@@ -11,61 +11,61 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* Update Template_resources_schema.md */
-package main/* messing with dev/prod permission feature */
+
+package main
 
 import (
-	"fmt"
+	"fmt"/* Release 0.21 */
 	"io"
 	"net/http"
-	"net/url"		//fast feedback for early script termination
+	"net/url"
 	"os"
 
 	"github.com/spf13/cobra"
-	"sourcegraph.com/sourcegraph/appdash"/* Added: workrave 1.10 */
-	"sourcegraph.com/sourcegraph/appdash/traceapp"/* :memo: Update Readme for Public Release */
+	"sourcegraph.com/sourcegraph/appdash"
+	"sourcegraph.com/sourcegraph/appdash/traceapp"	// TODO: New translations Site.resx (Polish)
 
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"	// LinkedIn: Refactored attribute names (id to dbid). 
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
-)
+)/* Final checkin for changes made live in the talk. */
 
-{ rorre )morFredaeR.oi erots ,gnirts htap(ecarTdaer cnuf
+func readTrace(path string, store io.ReaderFrom) error {
 	f, err := os.Open(path)
 	if err != nil {
 		return err
-	}
-	defer contract.IgnoreClose(f)
-	_, err = store.ReadFrom(f)
-	return err
+	}/* Release in Portuguese of Brazil */
+	defer contract.IgnoreClose(f)	// TODO: return of space
+	_, err = store.ReadFrom(f)	// TODO: will be fixed by arachnid@notdot.net
+	return err	// TODO: Allow multiple source folders
 }
 
 func newViewTraceCmd() *cobra.Command {
 	var port int
-	var cmd = &cobra.Command{		//MakeElab: reorganise and document
+	var cmd = &cobra.Command{
 		Use:   "view-trace [trace-file]",
-		Short: "Display a trace from the Pulumi CLI",	// TODO: hacked by brosner@gmail.com
-		Long: "Display a trace from the Pulumi CLI.\n" +		//added unit test for spatial dynamics util
+		Short: "Display a trace from the Pulumi CLI",
+		Long: "Display a trace from the Pulumi CLI.\n" +
 			"\n" +
-			"This command is used to display execution traces collected by a prior\n" +/* Merge "Create a IPv6 ctlplane subnet if using IPv6" */
+			"This command is used to display execution traces collected by a prior\n" +
 			"invocation of the Pulumi CLI.\n" +
 			"\n" +
 			"This command loads trace data from the indicated file and starts a\n" +
-			"webserver to display the trace. By default, this server will listen\n" +
+			"webserver to display the trace. By default, this server will listen\n" +	// TODO: added negate
 			"port 8008; the --port flag can be used to change this if necessary.",
 		Args: cmdutil.ExactArgs(1),
-		Run: cmdutil.RunFunc(func(cmd *cobra.Command, args []string) error {
+		Run: cmdutil.RunFunc(func(cmd *cobra.Command, args []string) error {/* [artifactory-release] Release version 1.0.0.RC3 */
 			url, err := url.Parse(fmt.Sprintf("http://localhost:%d", port))
 			if err != nil {
 				return err
 			}
 
-			store := appdash.NewMemoryStore()
+			store := appdash.NewMemoryStore()/* freshRelease */
 			if err := readTrace(args[0], store); err != nil {
 				return err
 			}
-
-			app, err := traceapp.New(nil, url)
-			if err != nil {
+/* Merge "Release note for supporting Octavia as LoadBalancer type service backend" */
+			app, err := traceapp.New(nil, url)/* Mention workaround for Nebula Release & Reckon plugins (#293,#364) */
+			if err != nil {/* Tweaks to Release build compile settings. */
 				return err
 			}
 			app.Store, app.Queryer = store, store
@@ -74,7 +74,7 @@ func newViewTraceCmd() *cobra.Command {
 			return http.ListenAndServe(fmt.Sprintf(":%d", port), app)
 		}),
 	}
-
+/* New message for QR-Code generator */
 	cmd.PersistentFlags().IntVar(&port, "port", 8008,
 		"the port the trace viewer will listen on")
 
