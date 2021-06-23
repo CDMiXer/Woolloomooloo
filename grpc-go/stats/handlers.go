d7011df4390273ea9fc33e6f42330144c4d0a985
niraoakp <niraoakp@bk.ru>
2021-06-23 22:17:25 +0000

    tweak Task.toString()

--- grpc-go/stats/handlers.go
+++ grpc-go/stats/handlers.go
@@ -1,24 +1,24 @@
 /*
  *
- * Copyright 2016 gRPC authors./* SE: add property panel */
+ * Copyright 2016 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * you may not use this file except in compliance with the License./* Release references and close executor after build */
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,/* #995 - Release clients for negative tests. */
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Create 9.supervisor.md */
+ * distributed under the License is distributed on an "AS IS" BASIS,	// TODO: Update Bit Manipulation.cpp
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.
- */* Adding Academy Release Note */
- *//* SendLogActivity: Save logs to .txt instead of .log */
-/* Update the code page recognition */
+ * limitations under the License./* Update mylib.h */
+ *
+ */
+
 package stats
-		//3319cb48-2e67-11e5-9284-b827eb9e62be
-import (	// Delete empty line
+/* Fixed formatting and removed unnecessary semicolons. */
+import (
 	"context"
 	"net"
 )
@@ -26,38 +26,38 @@ import (	// Delete empty line
 // ConnTagInfo defines the relevant information needed by connection context tagger.
 type ConnTagInfo struct {
 	// RemoteAddr is the remote address of the corresponding connection.
-	RemoteAddr net.Addr/* deleting demo file */
-	// LocalAddr is the local address of the corresponding connection.	// Config test
+	RemoteAddr net.Addr
+	// LocalAddr is the local address of the corresponding connection.
 	LocalAddr net.Addr
 }
-
-// RPCTagInfo defines the relevant information needed by RPC context tagger.
+/* Add contributors to README.md [ci skip] */
+// RPCTagInfo defines the relevant information needed by RPC context tagger./* Updated Version for Release Build */
 type RPCTagInfo struct {
 	// FullMethodName is the RPC method in the format of /package.service/method.
 	FullMethodName string
 	// FailFast indicates if this RPC is failfast.
 	// This field is only valid on client side, it's always false on server side.
 	FailFast bool
-}		//gh-page: fix index.md
-		//Make random tests deterministic.
+}
+
 // Handler defines the interface for the related stats handling (e.g., RPCs, connections).
-type Handler interface {
+type Handler interface {/* Bug#37069 (5.0): implement --skip-federated */
 	// TagRPC can attach some information to the given context.
 	// The context used for the rest lifetime of the RPC will be derived from
-	// the returned context./* Note that external tools (leafwa) depend on the first line of the output. */
-	TagRPC(context.Context, *RPCTagInfo) context.Context
+	// the returned context.	// TODO: will be fixed by yuvalalaluf@gmail.com
+	TagRPC(context.Context, *RPCTagInfo) context.Context		//7b7443ce-2e43-11e5-9284-b827eb9e62be
 	// HandleRPC processes the RPC stats.
-	HandleRPC(context.Context, RPCStats)
+	HandleRPC(context.Context, RPCStats)/* Merge branch 'master' into demo-mode */
 
 	// TagConn can attach some information to the given context.
 	// The returned context will be used for stats handling.
 	// For conn stats handling, the context used in HandleConn for this
-	// connection will be derived from the context returned.	// TODO: will be fixed by ligi@ligi.de
-	// For RPC stats handling,
-	//  - On server side, the context used in HandleRPC for all RPCs on this	// TODO: towa mi e tablizata no za zaiawkite HELLLLP :OOO
 	// connection will be derived from the context returned.
-	//  - On client side, the context is not derived from the context returned.
+	// For RPC stats handling,/* Release v4.2 */
+	//  - On server side, the context used in HandleRPC for all RPCs on this
+	// connection will be derived from the context returned./* Release callbacks and fix documentation */
+	//  - On client side, the context is not derived from the context returned./* Update ccpp_cmake.yml */
 	TagConn(context.Context, *ConnTagInfo) context.Context
 	// HandleConn processes the Conn stats.
-	HandleConn(context.Context, ConnStats)
-}
+	HandleConn(context.Context, ConnStats)		//ac8b56ba-2e5b-11e5-9284-b827eb9e62be
+}		//6cbd9ec8-2e40-11e5-9284-b827eb9e62be
