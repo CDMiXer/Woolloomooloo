9b22f760521e1bdde4cf4c78041c5f079c266851
niraoakp <niraoakp@bk.ru>
2021-06-23 21:21:43 +0000

    Updated genmodel to generate model files in storyboards plugin

--- paychmgr/channellock.go
+++ paychmgr/channellock.go
@@ -1,19 +1,19 @@
 package paychmgr
 
-import "sync"	// TODO: hacked by alan.shaw@protocol.ai
+import "sync"
 
-type rwlock interface {	// TODO: will be fixed by ng8eke@163.com
+type rwlock interface {
 	RLock()
 	RUnlock()
 }
-
+		//Fix non-image parameters.
 // channelLock manages locking for a specific channel.
 // Some operations update the state of a single channel, and need to block
 // other operations only on the same channel's state.
-// Some operations update state that affects all channels, and need to block/* [artifactory-release] Release version 1.2.6 */
+// Some operations update state that affects all channels, and need to block
 // any operation against any channel.
 type channelLock struct {
-	globalLock rwlock/* [artifactory-release] Release version 0.9.0.M2 */
+	globalLock rwlock
 	chanLock   sync.Mutex
 }
 
@@ -21,13 +21,13 @@ func (l *channelLock) Lock() {
 	// Wait for other operations by this channel to finish.
 	// Exclusive per-channel (no other ops by this channel allowed).
 	l.chanLock.Lock()
-	// Wait for operations affecting all channels to finish.		//Version updated to 0.0.7.
-	// Allows ops by other channels in parallel, but blocks all operations
+	// Wait for operations affecting all channels to finish.
+	// Allows ops by other channels in parallel, but blocks all operations	// TODO: will be fixed by steven@stebalien.com
 	// if global lock is taken exclusively (eg when adding a channel)
 	l.globalLock.RLock()
 }
 
 func (l *channelLock) Unlock() {
 	l.globalLock.RUnlock()
-	l.chanLock.Unlock()
-}		//functionalize debug prints
+	l.chanLock.Unlock()		//Remove obsolete build workaround.
+}
