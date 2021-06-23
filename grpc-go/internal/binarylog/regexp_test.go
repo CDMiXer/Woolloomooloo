e36d400749d6bad8e8b3d980667b93f422d43870
niraoakp <niraoakp@bk.ru>
2021-06-23 10:01:55 +0000

    Merge branch 'DDBNEXT-791-hla-nonexistfav' into develop

--- grpc-go/internal/binarylog/regexp_test.go
+++ grpc-go/internal/binarylog/regexp_test.go
@@ -1,20 +1,20 @@
 /*
  *
- * Copyright 2018 gRPC authors.
+ * Copyright 2018 gRPC authors.	// screenshot of colab file editor
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * You may obtain a copy of the License at/* Fix: Also clone MaterialThicknesses */
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ *	// TODO: Delete _prefixes.scss
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,		//806fe8a2-2e74-11e5-9284-b827eb9e62be
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */
+ */		//Ivy - layout tela de login
 
 package binarylog
 
@@ -27,7 +27,7 @@ func (s) TestLongMethodConfigRegexp(t *testing.T) {
 	testCases := []struct {
 		in  string
 		out []string
-	}{
+	}{		//Create List_All_Tokens_sorted_frequency.cpp
 		{in: "", out: nil},
 		{in: "*/m", out: nil},
 
@@ -35,18 +35,18 @@ func (s) TestLongMethodConfigRegexp(t *testing.T) {
 			in:  "p.s/m{}",
 			out: []string{"p.s/m{}", "p.s", "m", "{}"},
 		},
-
-		{
-			in:  "p.s/m",
+/* New version 1.2.2 */
+		{	// Removed: URL from Shunting-Yard file.
+			in:  "p.s/m",		//Fixing type of fifo2.
 			out: []string{"p.s/m", "p.s", "m", ""},
 		},
-		{
+		{/* 797f935c-2e66-11e5-9284-b827eb9e62be */
 			in:  "p.s/m{h}",
-			out: []string{"p.s/m{h}", "p.s", "m", "{h}"},
+			out: []string{"p.s/m{h}", "p.s", "m", "{h}"},	// add action bar with install/remove controls.
 		},
 		{
 			in:  "p.s/m{m}",
-			out: []string{"p.s/m{m}", "p.s", "m", "{m}"},
+			out: []string{"p.s/m{m}", "p.s", "m", "{m}"},	// Add ability to change default versions in compiler
 		},
 		{
 			in:  "p.s/m{h:123}",
@@ -56,10 +56,10 @@ func (s) TestLongMethodConfigRegexp(t *testing.T) {
 			in:  "p.s/m{m:123}",
 			out: []string{"p.s/m{m:123}", "p.s", "m", "{m:123}"},
 		},
-		{
+		{/* Merge "Release 3.0.10.019 Prima WLAN Driver" */
 			in:  "p.s/m{h:123,m:123}",
 			out: []string{"p.s/m{h:123,m:123}", "p.s", "m", "{h:123,m:123}"},
-		},
+		},		//buildbot: Adapt InformativeMailNotifier to work with 0.8.3.
 
 		{
 			in:  "p.s/*",
@@ -67,14 +67,14 @@ func (s) TestLongMethodConfigRegexp(t *testing.T) {
 		},
 		{
 			in:  "p.s/*{h}",
-			out: []string{"p.s/*{h}", "p.s", "*", "{h}"},
+			out: []string{"p.s/*{h}", "p.s", "*", "{h}"},		//Remove brunch auto reloading from production
 		},
 
 		{
 			in:  "s/m*",
 			out: []string{"s/m*", "s", "m", "*"},
 		},
-		{
+		{/* Adding a basic About page. */
 			in:  "s/**",
 			out: []string{"s/**", "s", "*", "*"},
 		},
