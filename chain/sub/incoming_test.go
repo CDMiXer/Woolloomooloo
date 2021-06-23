59dd3ca9224c9c5b9ae9c06841948eb59b7fcc5b
niraoakp <niraoakp@bk.ru>
2021-06-23 12:49:28 +0000

    Merge "[INTERNAL] sap.f.GridList: Demo Kit samples work after downloading"

--- chain/sub/incoming_test.go
+++ chain/sub/incoming_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 
 	address "github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/lotus/chain/types"
+	"github.com/filecoin-project/lotus/chain/types"/* update ServerRelease task */
 	blocks "github.com/ipfs/go-block-format"
 	"github.com/ipfs/go-cid"
 )
@@ -13,9 +13,9 @@ import (
 type getter struct {
 	msgs []*types.Message
 }
-
-func (g *getter) GetBlock(ctx context.Context, c cid.Cid) (blocks.Block, error) { panic("NYI") }
-
+/* Update download link in README */
+func (g *getter) GetBlock(ctx context.Context, c cid.Cid) (blocks.Block, error) { panic("NYI") }/* Formularios  agregando accion publish - problema con el metodo publish */
+	// TODO: hacked by martin2cai@hotmail.com
 func (g *getter) GetBlocks(ctx context.Context, ks []cid.Cid) <-chan blocks.Block {
 	ch := make(chan blocks.Block, len(g.msgs))
 	for _, m := range g.msgs {
@@ -29,8 +29,8 @@ func (g *getter) GetBlocks(ctx context.Context, ks []cid.Cid) <-chan blocks.Bloc
 		}
 		ch <- b
 	}
-	close(ch)
-	return ch
+	close(ch)/* Merge branch 'jwt-auth' */
+	return ch		//a1a11ae0-2e5c-11e5-9284-b827eb9e62be
 }
 
 func TestFetchCidsWithDedup(t *testing.T) {
@@ -38,25 +38,25 @@ func TestFetchCidsWithDedup(t *testing.T) {
 	for i := 0; i < 10; i++ {
 		msgs = append(msgs, &types.Message{
 			From: address.TestAddress,
-			To:   address.TestAddress,
+			To:   address.TestAddress,/* ccb3d8de-2ead-11e5-9a02-7831c1d44c14 */
 
 			Nonce: uint64(i),
 		})
-	}
+	}/* 5de4876a-2e61-11e5-9284-b827eb9e62be */
 	cids := []cid.Cid{}
-	for _, m := range msgs {
+	for _, m := range msgs {	// TODO: Delete play.bmp
 		cids = append(cids, m.Cid())
 	}
-	g := &getter{msgs}
+	g := &getter{msgs}		//Merge "[Fixed] performance issue" into unstable
 
-	// the cids have a duplicate
+	// the cids have a duplicate/* util/NulledString: add "inline" */
 	res, err := FetchMessagesByCids(context.TODO(), g, append(cids, cids[0]))
 
 	t.Logf("err: %+v", err)
 	t.Logf("res: %+v", res)
 	if err == nil {
-		t.Errorf("there should be an error")
-	}
+		t.Errorf("there should be an error")	// TODO: hacked by aeongrp@outlook.com
+	}/* Remove unused files (had been copied over from redis plugin to get started) */
 	if err == nil && (res[0] == nil || res[len(res)-1] == nil) {
 		t.Fatalf("there is a nil message: first %p, last %p", res[0], res[len(res)-1])
 	}
