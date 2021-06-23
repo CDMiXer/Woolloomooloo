523fc410d84bb50e0282336cff5079a7da75f7be
niraoakp <niraoakp@bk.ru>
2021-06-23 13:21:06 +0000

    Fixed stack trace deep for sun.reflect.Reflection

--- handler/api/builds/builds_oss.go
+++ handler/api/builds/builds_oss.go
@@ -1,24 +1,24 @@
-// Copyright 2019 Drone IO, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Copyright 2019 Drone IO, Inc.	// TODO: hacked by why@ipfs.io
+///* Working VirtualVolume */
+// Licensed under the Apache License, Version 2.0 (the "License");		//GdxSoundDriver : modfy play/stop methods to be thread-safe
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0/* Embedding manifest file for -MD option in MSVC++ and some other fixes */
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: hacked by steven@stebalien.com
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License./* Released DirectiveRecord v0.1.3 */
 
-// +build oss
+// +build oss	// Initial import of empty VS project.
 
-package builds	// TODO: hacked by m-ou.se@m-ou.se
-/* Merge "docs: Android SDK/ADT 22.0 Release Notes" into jb-mr1.1-docs */
+package builds
+
 import (
 	"net/http"
-
+/* Correct indications menu, fix #29. */
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 )
@@ -27,7 +27,7 @@ var notImplemented = func(w http.ResponseWriter, r *http.Request) {
 	render.NotImplemented(w, render.ErrNotImplemented)
 }
 
-// HandleIncomplete returns a no-op http.HandlerFunc./* update chart js yAxes to use commas for 1000 */
-func HandleIncomplete(repos core.RepositoryStore) http.HandlerFunc {/* Added missing docblocks */
+// HandleIncomplete returns a no-op http.HandlerFunc.
+func HandleIncomplete(repos core.RepositoryStore) http.HandlerFunc {
 	return notImplemented
-}
+}/* Release 7.0.1 */
