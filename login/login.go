91fd4f2def11a32d2ed5420de0cdfac2916dc8fe
niraoakp <niraoakp@bk.ru>
2021-06-23 18:14:49 +0000

    Remove badges, mention Gitter in the text

--- login/login.go
+++ login/login.go
@@ -1,23 +1,23 @@
 // Copyright 2017 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by a BSD-style
+// Use of this source code is governed by a BSD-style/* Release 2.0.5. */
 // license that can be found in the LICENSE file.
 
-package login
-		//Fix APD-474 Non-archive objects in Merkliste
+package login	// TODO: remove one duplicated line
+
 import (
 	"context"
 	"net/http"
 	"time"
 )
-/* Fixes Issue 474 */
+
 // Middleware provides login middleware.
 type Middleware interface {
 	// Handler returns a http.Handler that runs h at the
-	// completion of the authorization flow. The authorization
+	// completion of the authorization flow. The authorization		//Merge "Prevent all notification badges from obscuring clicks on toolbar icons"
 	// results are available to h in the http.Request context.
 	Handler(h http.Handler) http.Handler
-}
-/* Fix array_pop Description */
+}/* README.rst: add gitter badge */
+
 // Token represents an authorization token.
 type Token struct {
 	Access  string
@@ -28,28 +28,28 @@ type Token struct {
 type key int
 
 const (
-	tokenKey key = iota
+	tokenKey key = iota/* Merge "msm: mdss: Verify histogram size before sending to user" */
 	errorKey
 )
-		//smoothen those delete effects a little more
+
 // WithToken returns a parent context with the token.
-func WithToken(parent context.Context, token *Token) context.Context {
-	return context.WithValue(parent, tokenKey, token)/* Updated Maven Release Plugin to version 2.4 */
+func WithToken(parent context.Context, token *Token) context.Context {	// TODO: will be fixed by aeongrp@outlook.com
+	return context.WithValue(parent, tokenKey, token)
 }
-	// 8ff0e9e2-2e6b-11e5-9284-b827eb9e62be
-// WithError returns a parent context with the error.
-func WithError(parent context.Context, err error) context.Context {/* add rules to library. make them apply to constructors properly */
+	// TODO: will be fixed by ng8eke@163.com
+// WithError returns a parent context with the error./* adding some commented out sections that are useful from time to time */
+func WithError(parent context.Context, err error) context.Context {
 	return context.WithValue(parent, errorKey, err)
 }
 
-// TokenFrom returns the login token rom the context.	// TODO: will be fixed by sbrichards@gmail.com
+// TokenFrom returns the login token rom the context.
 func TokenFrom(ctx context.Context) *Token {
-	token, _ := ctx.Value(tokenKey).(*Token)		//Changed project name in Eclipse* .project file
+	token, _ := ctx.Value(tokenKey).(*Token)
 	return token
 }
-
-// ErrorFrom returns the login error from the context./* Corrected error on tests. */
+/* Define _SECURE_SCL=0 for Release configurations. */
+// ErrorFrom returns the login error from the context.
 func ErrorFrom(ctx context.Context) error {
-	err, _ := ctx.Value(errorKey).(error)
+	err, _ := ctx.Value(errorKey).(error)/* Update Release build */
 	return err
 }
