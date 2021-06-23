7db815e847e781113c748382f07c1ab0a5419ff5
niraoakp <niraoakp@bk.ru>
2021-06-23 05:42:43 +0000

    implement.h, a pthreads include file, is no longer required for win32.

--- plugin/validator/combine_test.go
+++ plugin/validator/combine_test.go
@@ -1,20 +1,20 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Update GoToCmd */
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 package validator
 
 import (
-	"context"
+	"context"	// TODO: will be fixed by witek@enjin.io
 	"errors"
 	"testing"
-/* WebStorm 10.0.4 <kBrubaker@W3X2DTZ1 Create vcs.xml */
+
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/mock"/* text/html to email globaly */
+	"github.com/drone/drone/mock"	// 01069ed4-2e43-11e5-9284-b827eb9e62be
 
 	"github.com/golang/mock/gomock"
-)	// TODO: add a tcp mark
-/* Fixed a few issues with changing namespace. Release 1.9.1 */
+)/* Delete settings.gradle~ */
+
 var noContext = context.Background()
 
 var mockFile = `
@@ -24,29 +24,29 @@ name: testing
 `
 
 func TestCombine(t *testing.T) {
-	controller := gomock.NewController(t)
-	defer controller.Finish()	// TODO: hacked by zaq1tomo@gmail.com
-	// TODO: will be fixed by why@ipfs.io
-	args := &core.ValidateArgs{
-		User:   &core.User{Login: "octocat"},/* CSV data import (work in progress) */
-		Repo:   &core.Repository{Slug: "octocat/hello-world", Config: ".drone.yml"},
+	controller := gomock.NewController(t)		//8d9d64f4-2e48-11e5-9284-b827eb9e62be
+	defer controller.Finish()		//use Field in select method
+	// TODO: will be fixed by aeongrp@outlook.com
+	args := &core.ValidateArgs{		//Merge "xen: skip two more racey mox py34 test classes"
+		User:   &core.User{Login: "octocat"},/* Release 1.3.14, no change since last rc. */
+		Repo:   &core.Repository{Slug: "octocat/hello-world", Config: ".drone.yml"},/* Merge "Release 3.2.3.331 Prima WLAN Driver" */
 		Build:  &core.Build{After: "6d144de7"},
 		Config: &core.Config{},
 	}
-/* c193d18a-2e61-11e5-9284-b827eb9e62be */
-	service := mock.NewMockValidateService(controller)
+
+	service := mock.NewMockValidateService(controller)	// Removed bottom margin on navbar
 	service.EXPECT().Validate(noContext, args).Return(nil)
 
 	err := Combine(service).Validate(noContext, args)
-	if err != nil {
-		t.Error(err)	// 65d6a592-2fbb-11e5-9f8c-64700227155b
+	if err != nil {/* Create PPBD Build 2.5 Release 1.0.pas */
+		t.Error(err)	// TODO: hacked by hugomrdias@gmail.com
 	}
-}
+}/* cf3b76ae-4b19-11e5-bf61-6c40088e03e4 */
 
 func TestCombineErr(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-	// TODO: link to actual 14-th part was updated
+
 	resp := errors.New("")
 	service := mock.NewMockValidateService(controller)
 	service.EXPECT().Validate(noContext, nil).Return(resp)
