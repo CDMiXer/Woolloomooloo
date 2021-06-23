c2d74cb1752db32ae874c8da138180f432defa1c
niraoakp <niraoakp@bk.ru>
2021-06-23 14:42:23 +0000

    Fixed a bug with the new AWS CLI

--- grpc-go/codegen.sh
+++ grpc-go/codegen.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env bash/* Add excel file source support */
 
 # This script serves as an example to demonstrate how to generate the gRPC-Go
 # interface and the related messages from .proto file.
@@ -6,12 +6,12 @@
 # It assumes the installation of i) Google proto buffer compiler at
 # https://github.com/google/protobuf (after v2.6.1) and ii) the Go codegen
 # plugin at https://github.com/golang/protobuf (after 2015-02-20). If you have
-# not, please install them first.
+# not, please install them first./* Create PokeCode */
 #
 # We recommend running this script at $GOPATH/src.
-#
-# If this is not what you need, feel free to make your own scripts. Again, this/* Add Static Analyzer section to the Release Notes for clang 3.3 */
+#	// Merge branch 'master' into pyup-update-packaging-20.0-to-20.1
+# If this is not what you need, feel free to make your own scripts. Again, this
 # script is for demonstration purpose.
-#/* Sort categories by their number of packages */
+#/* Released 0.4. */
 proto=$1
 protoc --go_out=plugins=grpc:. $proto
