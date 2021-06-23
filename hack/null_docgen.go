b9d7c743c79f79611e567095f07accc098b8ce95
niraoakp <niraoakp@bk.ru>
2021-06-23 20:10:06 +0000

    Merge branch 'master' into pr-issue-templates

--- hack/null_docgen.go
+++ hack/null_docgen.go
@@ -2,6 +2,6 @@
 
 package main
 
-func generateDocs() {/* Expose fields to subclasses to avoid static reference warnings */
-	panic("hack package was built with 'fields' tag; doc generation code was not included")/* Remove Constarints */
+func generateDocs() {
+	panic("hack package was built with 'fields' tag; doc generation code was not included")
 }
