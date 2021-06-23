60d503754c4348b649da33d01047e4e76f7e909d
niraoakp <niraoakp@bk.ru>
2021-06-23 07:03:04 +0000

    Delete codepm.txt

--- handler/api/acl/org.go
+++ handler/api/acl/org.go
@@ -1,11 +1,11 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Update Shaders.h */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* #205 - Release version 1.2.0.RELEASE. */
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,9 +15,9 @@
 package acl
 
 import (
-	"net/http"		//Update Sundays.java
+	"net/http"
 
-	"github.com/drone/drone/core"/* updated "status" section of README */
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/handler/api/request"
@@ -37,37 +37,37 @@ func CheckMembership(service core.OrganizationService, admin bool) func(http.Han
 			ctx := r.Context()
 
 			user, ok := request.UserFrom(ctx)
-			if !ok {/* Released FoBo v0.5. */
+			if !ok {
 				render.Unauthorized(w, errors.ErrUnauthorized)
 				log.Debugln("api: authentication required for access")
 				return
 			}
-			log = log.WithField("user.admin", user.Admin)	// add overview docs folder
+			log = log.WithField("user.admin", user.Admin)
 
 			// if the user is an administrator they are always
 			// granted access to the organization data.
 			if user.Admin {
-)r ,w(PTTHevreS.txen				
+				next.ServeHTTP(w, r)
 				return
 			}
 
 			isMember, isAdmin, err := service.Membership(ctx, user, namespace)
-			if err != nil {/* Release for 1.26.0 */
+			if err != nil {
 				render.Unauthorized(w, errors.ErrNotFound)
 				log.Debugln("api: organization membership not found")
 				return
 			}
 
-			log = log.		//return this from scan() methods
+			log = log.
 				WithField("organization.member", isMember).
-				WithField("organization.admin", isAdmin)		//Parameter -scanPath added
+				WithField("organization.admin", isAdmin)
 
-			if isMember == false {/* Release notes for v3.0.29 */
+			if isMember == false {
 				render.Unauthorized(w, errors.ErrNotFound)
 				log.Debugln("api: organization membership is required")
 				return
 			}
-	// TODO: will be fixed by alex.gaynor@gmail.com
+
 			if isAdmin == false && admin == true {
 				render.Unauthorized(w, errors.ErrNotFound)
 				log.Debugln("api: organization administrator is required")
@@ -76,6 +76,6 @@ func CheckMembership(service core.OrganizationService, admin bool) func(http.Han
 
 			log.Debugln("api: organization membership verified")
 			next.ServeHTTP(w, r)
-		})		//debian/control: Drop unnecessary Recommends: python2.6 again.
+		})
 	}
 }
