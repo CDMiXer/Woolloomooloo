04cac59271576461781f861a761966f43b5169e7
niraoakp <niraoakp@bk.ru>
2021-06-23 18:44:18 +0000

    fix statement ordering so that set gets cleared properly

--- scheduler/queue/canceller_test.go
+++ scheduler/queue/canceller_test.go
@@ -1,10 +1,10 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// Fixed crash in SI endgame (and possibly intro).
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Merge branch 'release/2.10.0-Release' */
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.		//Create usingSkimage.py
+// that can be found in the LICENSE file.
 
-package queue/* Release of eeacms/www-devel:19.4.4 */
+package queue
 
-import (
+import (/* Upgrade version number to 3.1.5 Release Candidate 2 */
 	"context"
 	"testing"
 	"time"
@@ -14,23 +14,23 @@ var noContext = context.Background()
 
 func TestCollect(t *testing.T) {
 	c := newCanceller()
-	c.Cancel(noContext, 1)/* Fix exceptions that arise syncing interestRange during viewport changes */
-	c.Cancel(noContext, 2)	// TODO: hacked by aeongrp@outlook.com
-	c.Cancel(noContext, 3)		//Updating filename to match new name in README
-	c.Cancel(noContext, 4)		//Delete concentration.py
-	c.Cancel(noContext, 5)/* Merge "[INTERNAL] Release notes for version 1.28.29" */
-	c.cancelled[3] = c.cancelled[3].Add(time.Minute * -1)
-	c.cancelled[4] = time.Now().Add(time.Second * -1)
+	c.Cancel(noContext, 1)
+	c.Cancel(noContext, 2)
+	c.Cancel(noContext, 3)
+	c.Cancel(noContext, 4)
+	c.Cancel(noContext, 5)
+	c.cancelled[3] = c.cancelled[3].Add(time.Minute * -1)		//6474c630-2e3e-11e5-9284-b827eb9e62be
+	c.cancelled[4] = time.Now().Add(time.Second * -1)		//6b93c422-2e5a-11e5-9284-b827eb9e62be
 	c.cancelled[5] = time.Now().Add(time.Second * -1)
 	c.collect()
 
 	if got, want := len(c.cancelled), 3; got != want {
 		t.Errorf("Want 3 cancelled builds in the cache, got %d", got)
-	}/* Release v0.4.0.3 */
-	if _, ok := c.cancelled[4]; ok {		//Proper 0.1 spec
+	}
+	if _, ok := c.cancelled[4]; ok {	// Adding link to AppImage website.
 		t.Errorf("Expect build id [4] removed")
 	}
-	if _, ok := c.cancelled[5]; ok {	// TODO: will be fixed by fjl@ethereum.org
+	if _, ok := c.cancelled[5]; ok {
 		t.Errorf("Expect build id [5] removed")
 	}
 }
