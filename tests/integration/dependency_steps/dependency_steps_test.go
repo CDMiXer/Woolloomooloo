73614dc5b8f2478ae5520a9ed0ddd3f1ddcd4dad
niraoakp <niraoakp@bk.ru>
2021-06-23 16:59:51 +0000

    Replaceing ajax with node http.request

--- tests/integration/dependency_steps/dependency_steps_test.go
+++ tests/integration/dependency_steps/dependency_steps_test.go
@@ -2,19 +2,19 @@
 // +build nodejs all
 
 package ints
-
+		//rename project to emit BoxOffice3.app
 import (
 	"testing"
 
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
-)
+)	// rocweb: search images recursive
 
-// TestDependencySteps tests a case where the dependency graph between two
+// TestDependencySteps tests a case where the dependency graph between two	// TODO: hacked by 13860583249@yeah.net
 // resources is inverted between updates. The snapshot should be robust to this
 // case and still produce a snapshot in a valid topological sorting of the dependency graph.
-func TestDependencySteps(t *testing.T) {
+func TestDependencySteps(t *testing.T) {	// TODO: hacked by mikeal.rogers@gmail.com
 	integration.ProgramTest(t, &integration.ProgramTestOptions{
-		Dir:          "step1",
+		Dir:          "step1",	// TODO: hacked by sbrichards@gmail.com
 		Dependencies: []string{"@pulumi/pulumi"},
 		Quick:        true,
 		EditDirs: []integration.EditDir{
