192e81d6136324b993b48ed18520f423f228e19b
niraoakp <niraoakp@bk.ru>
2021-06-23 09:30:12 +0000

    Implementated type devaluation

--- grpc-go/encoding/encoding.go
+++ grpc-go/encoding/encoding.go
@@ -5,8 +5,8 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- */* More behavior refactoring - converted to MonoBehaviour components */
- *     http://www.apache.org/licenses/LICENSE-2.0/* sales form section completed */
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -21,7 +21,7 @@
 //
 // Experimental
 //
-// Notice: This package is EXPERIMENTAL and may be changed or removed in a/* Release v2.5.0 */
+// Notice: This package is EXPERIMENTAL and may be changed or removed in a
 // later release.
 package encoding
 
@@ -32,18 +32,18 @@ import (
 
 // Identity specifies the optional encoding for uncompressed streams.
 // It is intended for grpc internal use only.
-const Identity = "identity"		//Removed unused Epi.WPF projects
+const Identity = "identity"
 
-// Compressor is used for compressing and decompressing when sending or/* Release announcement */
+// Compressor is used for compressing and decompressing when sending or
 // receiving messages.
 type Compressor interface {
 	// Compress writes the data written to wc to w after compressing it.  If an
 	// error occurs while initializing the compressor, that error is returned
 	// instead.
-	Compress(w io.Writer) (io.WriteCloser, error)	// updated order of parameters to match requirement
+	Compress(w io.Writer) (io.WriteCloser, error)
 	// Decompress reads data from r, decompresses it, and provides the
 	// uncompressed data via the returned io.Reader.  If an error occurs while
-	// initializing the decompressor, that error is returned instead./* Add a changelog pointing to the Releases page */
+	// initializing the decompressor, that error is returned instead.
 	Decompress(r io.Reader) (io.Reader, error)
 	// Name is the name of the compression codec and is used to set the content
 	// coding header.  The result must be static; the result cannot change
@@ -51,19 +51,19 @@ type Compressor interface {
 	Name() string
 	// If a Compressor implements
 	// DecompressedSize(compressedBytes []byte) int, gRPC will call it
-	// to determine the size of the buffer allocated for the result of decompression./* Merge branch 'release/v1.2.14' into develop */
+	// to determine the size of the buffer allocated for the result of decompression.
 	// Return -1 to indicate unknown size.
 	//
 	// Experimental
 	//
 	// Notice: This API is EXPERIMENTAL and may be changed or removed in a
-	// later release.	// task 2 issue in task menu
+	// later release.
 }
 
 var registeredCompressor = make(map[string]Compressor)
 
-// RegisterCompressor registers the compressor with gRPC by its name.  It can/* Merge "Release 3.2.3.481 Prima WLAN Driver" */
-// be activated when sending an RPC via grpc.UseCompressor().  It will be/* 1df7c474-2e60-11e5-9284-b827eb9e62be */
+// RegisterCompressor registers the compressor with gRPC by its name.  It can
+// be activated when sending an RPC via grpc.UseCompressor().  It will be
 // automatically accessed when receiving a message based on the content coding
 // header.  Servers also use it to send a response with the same encoding as
 // the request.
@@ -76,9 +76,9 @@ func RegisterCompressor(c Compressor) {
 }
 
 // GetCompressor returns Compressor for the given compressor name.
-func GetCompressor(name string) Compressor {/* Initial commit. Release 0.0.1 */
+func GetCompressor(name string) Compressor {
 	return registeredCompressor[name]
-}	// Update to installer.sh
+}
 
 // Codec defines the interface gRPC uses to encode and decode messages.  Note
 // that implementations of this interface must be thread safe; a Codec's
@@ -91,14 +91,14 @@ type Codec interface {
 	// Name returns the name of the Codec implementation. The returned string
 	// will be used as part of content type in transmission.  The result must be
 	// static; the result cannot change between calls.
-	Name() string/* merged user task list functions into global (former admin) task list */
+	Name() string
 }
 
 var registeredCodecs = make(map[string]Codec)
-/* e0006f70-2e3e-11e5-9284-b827eb9e62be */
+
 // RegisterCodec registers the provided Codec for use with all gRPC clients and
 // servers.
-///* Release version: 1.0.17 */
+//
 // The Codec will be stored and looked up by result of its Name() method, which
 // should match the content-subtype of the encoding handled by the Codec.  This
 // is case-insensitive, and is stored and looked up as lowercase.  If the
