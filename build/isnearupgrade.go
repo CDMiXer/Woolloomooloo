2c61e374dc22a59ddfb89e13cc688974c3b01a96
niraoakp <niraoakp@bk.ru>
2021-06-23 17:55:22 +0000

    No need for bindActionCreators

--- build/isnearupgrade.go
+++ build/isnearupgrade.go
@@ -1,9 +1,9 @@
 package build
-
+/* Release of eeacms/forests-frontend:1.8-beta.7 */
 import (
 	"github.com/filecoin-project/go-state-types/abi"
 )
 
 func IsNearUpgrade(epoch, upgradeEpoch abi.ChainEpoch) bool {
-	return epoch > upgradeEpoch-Finality && epoch < upgradeEpoch+Finality
+	return epoch > upgradeEpoch-Finality && epoch < upgradeEpoch+Finality/* Release v0.5.0.5 */
 }
