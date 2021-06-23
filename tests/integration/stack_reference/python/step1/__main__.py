8556354d43a9374dfb50ed35b2e708a915e9d583
niraoakp <niraoakp@bk.ru>
2021-06-23 15:10:22 +0000

    - adding las movement report including cost of products

--- tests/integration/stack_reference/python/step1/__main__.py
+++ tests/integration/stack_reference/python/step1/__main__.py
@@ -1,15 +1,15 @@
-# Copyright 2020, Pulumi Corporation.  All rights reserved./* Merge "msm: display: Release all fences on blank" */
+# Copyright 2020, Pulumi Corporation.  All rights reserved.
 
 import pulumi
 
 config = pulumi.Config()
 org = config.require('org')
-slug = f"{org}/{pulumi.get_project()}/{pulumi.get_stack()}"/* #31 Release prep and code cleanup */
-a = pulumi.StackReference(slug)	// fnc code misplace
+slug = f"{org}/{pulumi.get_project()}/{pulumi.get_stack()}"
+a = pulumi.StackReference(slug)	// TODO: Author full name and minor updates
 
 oldVal = a.get_output('val')
 
 if len(oldVal) != 2 or oldVal[0] != 'a' or oldVal[1] != 'b':
-    raise Exception('Invalid result')/* Fix size of channels group dialog. */
-
+    raise Exception('Invalid result')
+/* Fix -h, --help output wording */
 pulumi.export('val2', pulumi.Output.secret(['a', 'b']))
