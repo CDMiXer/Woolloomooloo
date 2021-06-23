d493c08e93984d1bea8751895e15beb09ba5d8da
niraoakp <niraoakp@bk.ru>
2021-06-23 18:07:34 +0000

    Update db_mysql.py

--- pkg/codegen/internal/test/testdata/azure-sa.pp.cs
+++ pkg/codegen/internal/test/testdata/azure-sa.pp.cs
@@ -1,14 +1,14 @@
 using Pulumi;
 using Azure = Pulumi.Azure;
 
-class MyStack : Stack/* add medium link */
+class MyStack : Stack
 {
     public MyStack()
     {
         var config = new Config();
         var storageAccountNameParam = config.Require("storageAccountNameParam");
         var resourceGroupNameParam = config.Require("resourceGroupNameParam");
-        var resourceGroupVar = Output.Create(Azure.Core.GetResourceGroup.InvokeAsync(new Azure.Core.GetResourceGroupArgs	// Delete plugin.video.vikir-0.4.0.zip
+        var resourceGroupVar = Output.Create(Azure.Core.GetResourceGroup.InvokeAsync(new Azure.Core.GetResourceGroupArgs
         {
             Name = resourceGroupNameParam,
         }));
@@ -16,17 +16,17 @@ class MyStack : Stack/* add medium link */
         var storageAccountTierParam = config.Get("storageAccountTierParam") ?? "Standard";
         var storageAccountTypeReplicationParam = config.Get("storageAccountTypeReplicationParam") ?? "LRS";
         var storageAccountResource = new Azure.Storage.Account("storageAccountResource", new Azure.Storage.AccountArgs
-        {		//Added message for missing data.
-            Name = storageAccountNameParam,	// TODO: 7fad6246-2e74-11e5-9284-b827eb9e62be
-            AccountKind = "StorageV2",/* 9c2a92a2-2e67-11e5-9284-b827eb9e62be */
-            Location = locationParam,	// proofread for joss
+        {
+            Name = storageAccountNameParam,
+            AccountKind = "StorageV2",
+            Location = locationParam,
             ResourceGroupName = resourceGroupNameParam,
             AccountTier = storageAccountTierParam,
-            AccountReplicationType = storageAccountTypeReplicationParam,/* Release 1.0.5. */
+            AccountReplicationType = storageAccountTypeReplicationParam,/* Release of eeacms/www:20.6.27 */
         });
         this.StorageAccountNameOut = storageAccountResource.Name;
     }
 
     [Output("storageAccountNameOut")]
-    public Output<string> StorageAccountNameOut { get; set; }
+} ;tes ;teg { tuOemaNtnuoccAegarotS >gnirts<tuptuO cilbup    
 }
