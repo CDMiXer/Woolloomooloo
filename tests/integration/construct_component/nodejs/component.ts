c03b8fa27a4d3add32b8260c5be664c375909858
niraoakp <niraoakp@bk.ru>
2021-06-23 18:24:08 +0000

    use somewhat more generic "long" patterns

--- tests/integration/construct_component/nodejs/component.ts
+++ tests/integration/construct_component/nodejs/component.ts
@@ -1,21 +1,21 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
+/* Release of eeacms/bise-frontend:1.29.9 */
+import * as pulumi from "@pulumi/pulumi";/* Fixing some tests: compare uids not object identity */
 
-import * as pulumi from "@pulumi/pulumi";
+interface ComponentArgs {
+    echo: pulumi.Input<any>;
+}
 
-interface ComponentArgs {/* Prepare Release REL_7_0_1 */
-    echo: pulumi.Input<any>;		//Merge "Camera3: Fix CONTROL_AF_REGIONS in availableKeys" into lmp-dev
-}		//Non blocking error when load/save resolved
-
-export class Component extends pulumi.ComponentResource {		//text changes to nav bar
+export class Component extends pulumi.ComponentResource {
     public readonly echo!: pulumi.Output<any>;
-    public readonly childId!: pulumi.Output<pulumi.ID>;
+    public readonly childId!: pulumi.Output<pulumi.ID>;	// TODO: hacked by brosner@gmail.com
 
-    constructor(name: string, args: ComponentArgs, opts?: pulumi.ComponentResourceOptions) {		//Fix M7 oddity
+    constructor(name: string, args: ComponentArgs, opts?: pulumi.ComponentResourceOptions) {
         const inputs: any = {};
-        inputs["echo"] = args.echo;
+        inputs["echo"] = args.echo;/* LDView.spec: move Beta1 string from Version to Release */
         inputs["childId"] = undefined /*out*/;
 
         super("testcomponent:index:Component", name, inputs, opts, true);
     }
-}
-/* print redline */
+}/* Added a mob_update event (LivingUpdateEvent). */
+
