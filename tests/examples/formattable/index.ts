50457f902ab23b069ad02d389a0d36e31f3c3d49
niraoakp <niraoakp@bk.ru>
2021-06-23 07:36:20 +0000

    Add an additional log and some extra comments.

--- tests/examples/formattable/index.ts
+++ tests/examples/formattable/index.ts
@@ -1,4 +1,4 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 console.log("Here's some format goo: %s, %v, %q, %d!");
-console.error("Here's some format goo: %s, %v, %q, %d!");	// Poll thread should only suppress exception when socket is closed.
+console.error("Here's some format goo: %s, %v, %q, %d!");
