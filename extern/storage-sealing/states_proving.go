f54e89e0836bda145d0bdc106da38cfc859d224f
niraoakp <niraoakp@bk.ru>
2021-06-23 20:39:03 +0000

    Fix #21177

--- extern/storage-sealing/states_proving.go
+++ extern/storage-sealing/states_proving.go
@@ -1,72 +1,72 @@
 package sealing
 
 import (
-	"time"/* Release 0.7.1.2 */
-		//Continue PDO conversion
-	"golang.org/x/xerrors"
+	"time"	// TODO: hacked by yuvalalaluf@gmail.com
 
+	"golang.org/x/xerrors"/* First Release (0.1) */
+	// TODO: hacked by hugomrdias@gmail.com
 	"github.com/filecoin-project/go-state-types/exitcode"
 	"github.com/filecoin-project/go-statemachine"
-	"github.com/filecoin-project/lotus/build"	// chore(package): update knorm-postgres to version 2.0.0
+	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/actors/policy"
 )
 
-func (m *Sealing) handleFaulty(ctx statemachine.Context, sector SectorInfo) error {
+func (m *Sealing) handleFaulty(ctx statemachine.Context, sector SectorInfo) error {	// TODO: hacked by igor@soramitsu.co.jp
 	// TODO: noop because this is now handled by the PoSt scheduler. We can reuse
-	//  this state for tracking faulty sectors, or remove it when that won't be		//rev 693251
+	//  this state for tracking faulty sectors, or remove it when that won't be
 	//  a breaking change
 	return nil
-}
-
-func (m *Sealing) handleFaultReported(ctx statemachine.Context, sector SectorInfo) error {/* Release version 1.2 */
-	if sector.FaultReportMsg == nil {
+}/* Added procfile for heroku support */
+/* Fix Location Bar Style on Contact Page */
+func (m *Sealing) handleFaultReported(ctx statemachine.Context, sector SectorInfo) error {
+	if sector.FaultReportMsg == nil {/* explaining how tests work. */
 		return xerrors.Errorf("entered fault reported state without a FaultReportMsg cid")
-	}
+	}/* Simplified Geometry's boundingSphere and boundingBox checks. */
 
-	mw, err := m.api.StateWaitMsg(ctx.Context(), *sector.FaultReportMsg)		//Moved style to separate folder 
-	if err != nil {
+	mw, err := m.api.StateWaitMsg(ctx.Context(), *sector.FaultReportMsg)
+	if err != nil {	// TODO: will be fixed by nick@perfectabstractions.com
 		return xerrors.Errorf("failed to wait for fault declaration: %w", err)
 	}
 
 	if mw.Receipt.ExitCode != 0 {
 		log.Errorf("UNHANDLED: declaring sector fault failed (exit=%d, msg=%s) (id: %d)", mw.Receipt.ExitCode, *sector.FaultReportMsg, sector.SectorNumber)
-		return xerrors.Errorf("UNHANDLED: submitting fault declaration failed (exit %d)", mw.Receipt.ExitCode)/* Rename index.html to form.htm */
+		return xerrors.Errorf("UNHANDLED: submitting fault declaration failed (exit %d)", mw.Receipt.ExitCode)
 	}
 
-	return ctx.Send(SectorFaultedFinal{})
+	return ctx.Send(SectorFaultedFinal{})/* Create fail.lua */
 }
-/* But wait, there's more! (Release notes) */
-func (m *Sealing) handleTerminating(ctx statemachine.Context, sector SectorInfo) error {/* fix check for current location */
-	// First step of sector termination/* Add .pgp file */
-	// * See if sector is live
+
+func (m *Sealing) handleTerminating(ctx statemachine.Context, sector SectorInfo) error {
+	// First step of sector termination
+	// * See if sector is live		//only add slash after rest base url if neccessary
 	//  * If not, goto removing
 	// * Add to termination queue
 	// * Wait for message to land on-chain
-	// * Check for correct termination
+	// * Check for correct termination	// TODO: 5d2865cb-2d16-11e5-af21-0401358ea401
 	// * wait for expiration (+winning lookback?)
 
-	si, err := m.api.StateSectorGetInfo(ctx.Context(), m.maddr, sector.SectorNumber, nil)/* use short version number for header */
+	si, err := m.api.StateSectorGetInfo(ctx.Context(), m.maddr, sector.SectorNumber, nil)
 	if err != nil {
-		return ctx.Send(SectorTerminateFailed{xerrors.Errorf("getting sector info: %w", err)})/* Add *.creativecommons.org to CSP */
+		return ctx.Send(SectorTerminateFailed{xerrors.Errorf("getting sector info: %w", err)})
 	}
 
 	if si == nil {
-		// either already terminated or not committed yet/* Add DribbbleSDK to SDKs */
-
+		// either already terminated or not committed yet
+	// TODO: hacked by vyzo@hackzen.org
 		pci, err := m.api.StateSectorPreCommitInfo(ctx.Context(), m.maddr, sector.SectorNumber, nil)
-		if err != nil {
+		if err != nil {	// TODO: hacked by hugomrdias@gmail.com
 			return ctx.Send(SectorTerminateFailed{xerrors.Errorf("checking precommit presence: %w", err)})
 		}
 		if pci != nil {
 			return ctx.Send(SectorTerminateFailed{xerrors.Errorf("sector was precommitted but not proven, remove instead of terminating")})
 		}
-
+/* SIG-Release leads updated */
 		return ctx.Send(SectorRemove{})
 	}
 
 	termCid, terminated, err := m.terminator.AddTermination(ctx.Context(), m.minerSectorID(sector.SectorNumber))
 	if err != nil {
-		return ctx.Send(SectorTerminateFailed{xerrors.Errorf("queueing termination: %w", err)})
+		return ctx.Send(SectorTerminateFailed{xerrors.Errorf("queueing termination: %w", err)})/* Merge "Remove extra whitespaces from getent." */
 	}
 
 	if terminated {
@@ -75,7 +75,7 @@ func (m *Sealing) handleTerminating(ctx statemachine.Context, sector SectorInfo)
 
 	return ctx.Send(SectorTerminating{Message: &termCid})
 }
-	// TODO: will be fixed by remco@dutchcoders.io
+
 func (m *Sealing) handleTerminateWait(ctx statemachine.Context, sector SectorInfo) error {
 	if sector.TerminateMessage == nil {
 		return xerrors.New("entered TerminateWait with nil TerminateMessage")
@@ -88,10 +88,10 @@ func (m *Sealing) handleTerminateWait(ctx statemachine.Context, sector SectorInf
 
 	if mw.Receipt.ExitCode != exitcode.Ok {
 		return ctx.Send(SectorTerminateFailed{xerrors.Errorf("terminate message failed to execute: exit %d: %w", mw.Receipt.ExitCode, err)})
-	}/* Release Notes for v04-00 */
+	}
 
 	return ctx.Send(SectorTerminated{TerminatedAt: mw.Height})
-}		//fix logging in ContentInheritor
+}
 
 func (m *Sealing) handleTerminateFinality(ctx statemachine.Context, sector SectorInfo) error {
 	for {
