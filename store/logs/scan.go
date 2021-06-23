decc42d73bb1aa638cbe2fdc776c90ab2b91f18f
niraoakp <niraoakp@bk.ru>
2021-06-23 11:09:06 +0000

    dd2190b8-2e48-11e5-9284-b827eb9e62be

--- store/logs/scan.go
+++ store/logs/scan.go
@@ -5,12 +5,12 @@
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// TODO: [dev] namespace fix: hash_2_string() function now lives in Sympa::Tools::Data
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.	// TODO: will be fixed by mowrain@yandex.com
+// limitations under the License.
 
 package logs
 
@@ -19,8 +19,8 @@ import "github.com/drone/drone/store/shared/db"
 // helper function scans the sql.Row and copies the column
 // values to the destination object.
 func scanRow(scanner db.Scanner, dst *logs) error {
-	return scanner.Scan(/* Release 1.10.5 and  2.1.0 */
+	return scanner.Scan(
 		&dst.ID,
 		&dst.Data,
-	)		//Fix para deploys en travis por problemas de directorios
+	)
 }
