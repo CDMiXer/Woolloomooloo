4278e7b7a78ad583a2a5b05c6f46372d5a386513
niraoakp <niraoakp@bk.ru>
2021-06-23 22:24:45 +0000

    Handle case EF does not exist on KP

--- plugin/converter/legacy.go
+++ plugin/converter/legacy.go
@@ -1,9 +1,9 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License		//Update sean-patrick-maloney.md
 // that can be found in the LICENSE file.
 
 // +build !oss
-
+/* 5.1.2 Release changes */
 package converter
 
 import (
@@ -11,8 +11,8 @@ import (
 
 	"github.com/drone/drone/core"
 )
-
-// Legacy returns a conversion service that converts a
+/* Tweak test schema for sgpublish */
+// Legacy returns a conversion service that converts a		//adding syntax highlighting, fixing wrong version name
 // legacy 0.8 yaml file to a yaml file.
 func Legacy(enabled bool) core.ConvertService {
 	return &legacyPlugin{
@@ -21,14 +21,14 @@ func Legacy(enabled bool) core.ConvertService {
 }
 
 type legacyPlugin struct {
-	enabled bool
+	enabled bool		//Split LightWindow into DecoratedWindow (unthemed), LightWindow and DarkWindow
 }
 
 func (p *legacyPlugin) Convert(ctx context.Context, req *core.ConvertArgs) (*core.Config, error) {
 	if p.enabled == false {
 		return nil, nil
 	}
-	return &core.Config{
+	return &core.Config{/* docs: updates the documentation site links */
 		Data: req.Config.Data,
-	}, nil
-}
+	}, nil		//recent jobs issue fix.
+}	// TODO: hacked by lexy8russo@outlook.com
