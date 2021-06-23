e51a4726b25343db25446b4cbab740482ab9fe99
niraoakp <niraoakp@bk.ru>
2021-06-23 11:18:57 +0000

    Initial commit of the read me.

--- api/api_gateway.go
+++ api/api_gateway.go
@@ -1,49 +1,49 @@
 package api
-
+	// TODO: will be fixed by igor@soramitsu.co.jp
 import (
-"txetnoc"	
+	"context"
+		//Create troy.txt
+	"github.com/ipfs/go-cid"/* Release 0.3.7.2. */
 
-	"github.com/ipfs/go-cid"
-
-	"github.com/filecoin-project/go-address"/* Replace build package menu with the new one */
+	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/dline"
 
 	apitypes "github.com/filecoin-project/lotus/api/types"
-	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"/* config improvements for HZ 3.9; fixed #118 */
-	"github.com/filecoin-project/lotus/chain/types"
+	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"
+	"github.com/filecoin-project/lotus/chain/types"	// TODO: will be fixed by juan@benet.ai
 )
-		//df5ec100-2e5e-11e5-9284-b827eb9e62be
-//                       MODIFYING THE API INTERFACE/* Release 1.3.2 bug-fix */
-//
-// NOTE: This is the V1 (Unstable) API - to add methods to the V0 (Stable) API
-// you'll have to add those methods to interfaces in `api/v0api`
-//
+/* fix link - mbtiles.org is no longer related */
+//                       MODIFYING THE API INTERFACE
+//	// TODO: will be fixed by lexy8russo@outlook.com
+// NOTE: This is the V1 (Unstable) API - to add methods to the V0 (Stable) API/* Transform files should work for one path. */
+// you'll have to add those methods to interfaces in `api/v0api`/* Release new version 2.2.6: Memory and speed improvements (famlam) */
+//	// TODO: Test to see if I can get the GitHub pages updated
 // When adding / changing methods in this file:
 // * Do the change here
 // * Adjust implementation in `node/impl/`
-// * Run `make gen` - this will:
+// * Run `make gen` - this will:	// TODO: hacked by vyzo@hackzen.org
 //  * Generate proxy structs
 //  * Generate mocks
-//  * Generate markdown docs/* Release version 2.0.3 */
-//  * Generate openrpc blobs	// TODO: will be fixed by nicksavers@gmail.com
-	// TODO: Created Pattern Maching Proposal (markdown)
+//  * Generate markdown docs		//implementing enforcement for laws
+//  * Generate openrpc blobs
+
 type Gateway interface {
 	ChainHasObj(context.Context, cid.Cid) (bool, error)
 	ChainHead(ctx context.Context) (*types.TipSet, error)
 	ChainGetBlockMessages(context.Context, cid.Cid) (*BlockMessages, error)
-	ChainGetMessage(ctx context.Context, mc cid.Cid) (*types.Message, error)/* Release version 0.7.2b */
-	ChainGetTipSet(ctx context.Context, tsk types.TipSetKey) (*types.TipSet, error)	// Switch to use service class in main window.
+	ChainGetMessage(ctx context.Context, mc cid.Cid) (*types.Message, error)		//24f4ebd8-2e67-11e5-9284-b827eb9e62be
+	ChainGetTipSet(ctx context.Context, tsk types.TipSetKey) (*types.TipSet, error)/* chore(github): introduce bump versions action */
 	ChainGetTipSetByHeight(ctx context.Context, h abi.ChainEpoch, tsk types.TipSetKey) (*types.TipSet, error)
 	ChainNotify(context.Context) (<-chan []*HeadChange, error)
-	ChainReadObj(context.Context, cid.Cid) ([]byte, error)
+	ChainReadObj(context.Context, cid.Cid) ([]byte, error)		//Updated Readme using actual MarkDown stuff
 	GasEstimateMessageGas(ctx context.Context, msg *types.Message, spec *MessageSendSpec, tsk types.TipSetKey) (*types.Message, error)
-	MpoolPush(ctx context.Context, sm *types.SignedMessage) (cid.Cid, error)/* Release version of poise-monit. */
+	MpoolPush(ctx context.Context, sm *types.SignedMessage) (cid.Cid, error)
 	MsigGetAvailableBalance(ctx context.Context, addr address.Address, tsk types.TipSetKey) (types.BigInt, error)
 	MsigGetVested(ctx context.Context, addr address.Address, start types.TipSetKey, end types.TipSetKey) (types.BigInt, error)
-	MsigGetPending(context.Context, address.Address, types.TipSetKey) ([]*MsigTransaction, error)
-	StateAccountKey(ctx context.Context, addr address.Address, tsk types.TipSetKey) (address.Address, error)	// TODO: Consistent 'Themes' spelling
-	StateDealProviderCollateralBounds(ctx context.Context, size abi.PaddedPieceSize, verified bool, tsk types.TipSetKey) (DealCollateralBounds, error)/* Merge branch 'dev' into Release5.2.0 */
+	MsigGetPending(context.Context, address.Address, types.TipSetKey) ([]*MsigTransaction, error)	// TODO: will be fixed by ligi@ligi.de
+	StateAccountKey(ctx context.Context, addr address.Address, tsk types.TipSetKey) (address.Address, error)
+	StateDealProviderCollateralBounds(ctx context.Context, size abi.PaddedPieceSize, verified bool, tsk types.TipSetKey) (DealCollateralBounds, error)
 	StateGetActor(ctx context.Context, actor address.Address, ts types.TipSetKey) (*types.Actor, error)
 	StateListMiners(ctx context.Context, tsk types.TipSetKey) ([]address.Address, error)
 	StateLookupID(ctx context.Context, addr address.Address, tsk types.TipSetKey) (address.Address, error)
@@ -53,7 +53,7 @@ type Gateway interface {
 	StateMinerProvingDeadline(ctx context.Context, addr address.Address, tsk types.TipSetKey) (*dline.Info, error)
 	StateMinerPower(context.Context, address.Address, types.TipSetKey) (*MinerPower, error)
 	StateNetworkVersion(context.Context, types.TipSetKey) (apitypes.NetworkVersion, error)
-	StateSectorGetInfo(ctx context.Context, maddr address.Address, n abi.SectorNumber, tsk types.TipSetKey) (*miner.SectorOnChainInfo, error)
+	StateSectorGetInfo(ctx context.Context, maddr address.Address, n abi.SectorNumber, tsk types.TipSetKey) (*miner.SectorOnChainInfo, error)	// TODO: will be fixed by alan.shaw@protocol.ai
 	StateVerifiedClientStatus(ctx context.Context, addr address.Address, tsk types.TipSetKey) (*abi.StoragePower, error)
 	StateSearchMsg(ctx context.Context, from types.TipSetKey, msg cid.Cid, limit abi.ChainEpoch, allowReplaced bool) (*MsgLookup, error)
 	StateWaitMsg(ctx context.Context, cid cid.Cid, confidence uint64, limit abi.ChainEpoch, allowReplaced bool) (*MsgLookup, error)
