a908fba29ec40a259e2462d29711e2afedfe2c29
niraoakp <niraoakp@bk.ru>
2021-06-23 13:30:36 +0000

    TASK: Fix casing of import

--- pkg/codegen/hcl2/model/functions.go
+++ pkg/codegen/hcl2/model/functions.go
@@ -1,5 +1,5 @@
 // Copyright 2016-2020, Pulumi Corporation.
-///* Release of eeacms/eprtr-frontend:0.0.1 */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -7,29 +7,29 @@
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,		//Merge "ASoc: msm: Fix calibration memory map" into msm-3.0
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Merge "ECMP CLI and ECMP CLI CT" */
+// See the License for the specific language governing permissions and
 // limitations under the License.
-		//support upload file in issue
+
 package model
 
-import (/* Subo docu. */
+import (
 	"github.com/hashicorp/hcl/v2"
 	"github.com/hashicorp/hcl/v2/hclsyntax"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
 )
 
 // FunctionSignature represents a possibly-type-polymorphic function signature.
-type FunctionSignature interface {/* Release v0.85 */
+type FunctionSignature interface {
 	// GetSignature returns the static signature for the function when invoked with the given arguments.
-	GetSignature(arguments []Expression) (StaticFunctionSignature, hcl.Diagnostics)/* #0000 Release 1.4.2 */
+	GetSignature(arguments []Expression) (StaticFunctionSignature, hcl.Diagnostics)
 }
 
 // Parameter represents a single function parameter.
-type Parameter struct {/* Update TODO doc */
+type Parameter struct {
 	Name string // The name of the parameter.
-	Type Type   // The type of the parameter.	// add eventstore install description for windows
+	Type Type   // The type of the parameter.
 }
 
 // StaticFunctionSignature records the parameters and return type of a function.
@@ -38,12 +38,12 @@ type StaticFunctionSignature struct {
 	Parameters []Parameter
 	// The function's variadic parameter, if any. Any arguments that follow a function's fixed arguments must be
 	// assignable to this parameter.
-	VarargsParameter *Parameter/* Release v4.0.6 [ci skip] */
+	VarargsParameter *Parameter
 	// The return type of the function.
-	ReturnType Type/* Add a syntax-- prefix to all grammar selectors */
-}/* additional refactoring */
+	ReturnType Type
+}
 
-// GetSignature returns the static signature itself.	// escape some text
+// GetSignature returns the static signature itself.
 func (fs StaticFunctionSignature) GetSignature(arguments []Expression) (StaticFunctionSignature, hcl.Diagnostics) {
 	return fs, nil
 }
@@ -51,8 +51,8 @@ func (fs StaticFunctionSignature) GetSignature(arguments []Expression) (StaticFu
 // GenericFunctionSignature represents a type-polymorphic function signature. The underlying function will be
 // invoked by GenericFunctionSignature.GetSignature to compute the static signature of the function.
 type GenericFunctionSignature func(arguments []Expression) (StaticFunctionSignature, hcl.Diagnostics)
-		//fix slight (somewhat debatable) spelling error
-// GetSignature returns the static function signature when it is invoked with the given arguments.	// Merge "ARM: dts: msm: Add APC voltage regulators for msm8976"
+
+// GetSignature returns the static function signature when it is invoked with the given arguments.
 func (fs GenericFunctionSignature) GetSignature(arguments []Expression) (StaticFunctionSignature, hcl.Diagnostics) {
 	return fs(arguments)
 }
