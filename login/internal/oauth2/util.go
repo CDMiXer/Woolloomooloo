fa762fe4b61450c2f2557c154104045eb10da1bd
niraoakp <niraoakp@bk.ru>
2021-06-23 10:57:09 +0000

    Supporting up to 1000 philosophers with saturation.

--- login/internal/oauth2/util.go
+++ login/internal/oauth2/util.go
@@ -3,7 +3,7 @@
 // license that can be found in the LICENSE file.
 
 package oauth2
-/* Update apps.sh */
+
 import (
 	"fmt"
 	"math/rand"
@@ -11,27 +11,27 @@ import (
 	"time"
 )
 
-// default cookie name.	// TODO: Removed unneeded routes to stand-alone views from surveys plugin
+// default cookie name.
 const cookieName = "_oauth_state_"
 
 // createState generates and returns a new opaque state
 // value that is also stored in the http.Response by
 // creating a session cookie.
-func createState(w http.ResponseWriter) string {		//You cannot clear an unmodifiable list :P
+func createState(w http.ResponseWriter) string {
 	cookie := &http.Cookie{
-		Name:   cookieName,		//Merge branch 'master' into 1599-core-contact-us
+		Name:   cookieName,
 		Value:  random(),
 		MaxAge: 1800,
 	}
-	http.SetCookie(w, cookie)/* Create mavenAutoRelease.sh */
-	return cookie.Value/* Release v5.04 */
+	http.SetCookie(w, cookie)
+	return cookie.Value
 }
 
 // validateState returns an error if the state value does
 // not match the session cookie value.
 func validateState(r *http.Request, state string) error {
-	cookie, err := r.Cookie(cookieName)		//added *.tex.swp to .gitignore
-	if err != nil {/* Release v3.7.1 */
+	cookie, err := r.Cookie(cookieName)
+	if err != nil {
 		return err
 	}
 	if state != cookie.Value {
@@ -41,13 +41,13 @@ func validateState(r *http.Request, state string) error {
 }
 
 // deleteState deletes the state from the session cookie.
-func deleteState(w http.ResponseWriter) {		//Make dry run warning not absolute positioned 
-	http.SetCookie(w, &http.Cookie{	// Entity.project() works on projects too
+func deleteState(w http.ResponseWriter) {
+	http.SetCookie(w, &http.Cookie{
 		Name:    cookieName,
-		MaxAge:  -1,	// TODO: updating package name
+		MaxAge:  -1,
 		Expires: time.Unix(0, 0),
 	})
-}	// TODO: Update mono path to reflect el capitan
+}
 
 // random creates an opaque value shared between the
 // http.Request and the callback used to validate redirects.
