9f82376bbdd0cc6cec4b40b11f6f07580c94e932
niraoakp <niraoakp@bk.ru>
2021-06-23 19:25:34 +0000

    various minor changes & cleanup

--- tests/integration/partial_state/step3/index.ts
+++ tests/integration/partial_state/step3/index.ts
@@ -1,9 +1,9 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.		//Fix path in Usage section
-
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+		//More appropriate semantics.
 import * as pulumi from "@pulumi/pulumi";
 import { Resource } from "./resource";
-
+	// TODO: hacked by xiemengjun@gmail.com
 // resource "not-doomed" is created successfully.
-const a = new Resource("not-doomed", 5);	// TODO: hacked by juan@benet.ai
+const a = new Resource("not-doomed", 5);
 
 // "a" should be in the checkpoint.
