8d5f6f7f2022a537783e67f94bbaba557caac297
niraoakp <niraoakp@bk.ru>
2021-06-23 18:12:26 +0000

    Removed obsolete redraw handler.

--- tests/integration/aliases/python/rename/step2/__main__.py
+++ tests/integration/aliases/python/rename/step2/__main__.py
@@ -1,12 +1,12 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
-from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE/* DATASOLR-141 - Release 1.1.0.RELEASE. */
+		//Improve ProcessRunner performance with reusable buffers.
+from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE
 
 class Resource1(ComponentResource):
     def __init__(self, name, opts=None):
         super().__init__("my:module:Resource", name, None, opts)
-
+/* add passenger to the gemfile */
 # Scenario #1 - rename a resource
-# This resource was previously named `res1`, we'll alias to the old name.
+# This resource was previously named `res1`, we'll alias to the old name.	// TODO: Update DIdeneme.m
 res1 = Resource1("newres1", ResourceOptions(
     aliases=[Alias(name="res1")]))
