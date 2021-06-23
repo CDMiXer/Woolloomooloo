70935c97740b4ffdbfee5a57c0457090f07499b4
niraoakp <niraoakp@bk.ru>
2021-06-23 09:15:51 +0000

    Merge jes-commands.

--- grpc-go/test/logging.go
+++ grpc-go/test/logging.go
@@ -2,12 +2,12 @@
  *
  * Copyright 2020 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");/* ecc23c6e-35c5-11e5-a315-6c40088e03e4 */
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *	// TODO: hacked by aeongrp@outlook.com
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,9 +15,9 @@
  * limitations under the License.
  *
  */
-/* Fix key naming convention for parameters validation */
-package test	// TODO: hacked by why@ipfs.io
 
-import "google.golang.org/grpc/grpclog"	// TODO: 3dec9f9c-2e5c-11e5-9284-b827eb9e62be
+package test
+
+import "google.golang.org/grpc/grpclog"
 
 var logger = grpclog.Component("testing")
