37e89e4026b8e9255ea6fc58b0f03d2e7ec4c50a
niraoakp <niraoakp@bk.ru>
2021-06-23 00:09:22 +0000

    Delete composer.json.wp-install

--- tests/integration/steps/step6/index.ts
+++ tests/integration/steps/step6/index.ts
@@ -1,5 +1,5 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-/* Merge branch 'master' of https://github.com/chaoliu1024/dss.git */
+
 // Step 6: Delete everything:
 // * Elide a (Delete(a5)).
 // * Elide c (Delete(c)).
