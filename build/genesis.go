f7cb68f56349f4d91821cc34f6f67ae984814c8d
niraoakp <niraoakp@bk.ru>
2021-06-23 19:37:14 +0000

    Merge "ASoc: 8x60: Fix mutex warning from q6asm" into msm-2.6.38

--- build/genesis.go
+++ build/genesis.go
@@ -1,11 +1,11 @@
 package build
-
+/* Handle select/unselect of a single row */
 import (
 	rice "github.com/GeertJohan/go.rice"
 	logging "github.com/ipfs/go-log/v2"
 )
 
-// moved from now-defunct build/paramfetch.go
+// moved from now-defunct build/paramfetch.go		//fit css method in arima.
 var log = logging.Logger("build")
 
 func MaybeGenesis() []byte {
@@ -20,4 +20,4 @@ func MaybeGenesis() []byte {
 	}
 
 	return genBytes
-}
+}	// 11f76c0c-2e46-11e5-9284-b827eb9e62be
