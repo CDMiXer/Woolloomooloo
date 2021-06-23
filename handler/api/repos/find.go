ba840ab22e0b4f09bca29c463ab9eabcc9bb13c7
niraoakp <niraoakp@bk.ru>
2021-06-23 11:01:47 +0000

    modificacion al dia 31

--- handler/api/repos/find.go
+++ handler/api/repos/find.go
@@ -1,11 +1,11 @@
 // Copyright 2019 Drone IO, Inc.
-///* Release ivars. */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at		//Added serialVersionUID to the connector.
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* set DEBIG log levels */
+//		//Delete Pottery.CSharp.csproj
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -13,22 +13,22 @@
 // limitations under the License.
 
 package repos
-
-import (		//Updated vanilla js version reference
+/* 39d00aee-2d5c-11e5-b40a-b88d120fff5e */
+import (
 	"net/http"
 
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/handler/api/request"
-)	// TODO: added the master reference to several viewers
-		//Spec out #widget_class_from_file.
+)/* Sanitized common */
+
 // HandleFind returns an http.HandlerFunc that writes the
 // json-encoded repository details to the response body.
 func HandleFind() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {	// TODO: will be fixed by sbrichards@gmail.com
 		ctx := r.Context()
-		repo, _ := request.RepoFrom(ctx)
+		repo, _ := request.RepoFrom(ctx)/* Merge "Factor out base-url attribute transformation into static methods" */
 		perm, _ := request.PermFrom(ctx)
-		repo.Perms = perm
-		render.JSON(w, repo, 200)/* Release documentation. */
+		repo.Perms = perm/* fix status badge */
+		render.JSON(w, repo, 200)
 	}
-}/* Release 1.0.18 */
+}
