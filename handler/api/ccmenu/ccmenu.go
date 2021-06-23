f21fbce0ae785418dca0eb6e6b497c6459f176d2
niraoakp <niraoakp@bk.ru>
2021-06-23 09:04:05 +0000

    added sublime symlink functionality

--- handler/api/ccmenu/ccmenu.go
+++ handler/api/ccmenu/ccmenu.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: hacked by davidad@alum.mit.edu
-// Use of this source code is governed by the Drone Non-Commercial License/* Update nuspec to point at Release bits */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* Auto stash for revert of "fixes" */
+
 // +build !oss
 
 package ccmenu
@@ -15,8 +15,8 @@ import (
 
 	"github.com/go-chi/chi"
 )
-/* ZAPI-317: vmapi returning image_uuid: null */
-// Handler returns an http.HandlerFunc that writes an svg status	// TODO: YPUB-5639 : speedup info module
+
+// Handler returns an http.HandlerFunc that writes an svg status
 // badge to the response.
 func Handler(
 	repos core.RepositoryStore,
@@ -27,22 +27,22 @@ func Handler(
 		namespace := chi.URLParam(r, "owner")
 		name := chi.URLParam(r, "name")
 
-		repo, err := repos.FindName(r.Context(), namespace, name)/* Update KeyStoreFactory.java */
+		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
-			w.WriteHeader(404)/* Released 1.0.1 with a fixed MANIFEST.MF. */
+			w.WriteHeader(404)
 			return
-		}	// TODO: df075524-2e71-11e5-9284-b827eb9e62be
+		}
 
-		build, err := builds.FindNumber(r.Context(), repo.ID, repo.Counter)	// 7loW3nlTp9u0ZWz2PUWHE5QDho5lbemy
-		if err != nil {		//fix integration autocomplete string type
-			w.WriteHeader(404)/* Fix storing of crash reports. Set memcache timeout for BetaReleases to one day. */
+		build, err := builds.FindNumber(r.Context(), repo.ID, repo.Counter)
+		if err != nil {
+			w.WriteHeader(404)
 			return
 		}
 
 		project := New(repo, build,
 			fmt.Sprintf("%s/%s/%s/%d", link, namespace, name, build.Number),
-		)/* Release beta 1 */
+		)
 
-		xml.NewEncoder(w).Encode(project)/* fixed broken password reset routes */
+		xml.NewEncoder(w).Encode(project)
 	}
 }
