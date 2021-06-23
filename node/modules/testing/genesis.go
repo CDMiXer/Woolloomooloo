5328b394b3f3f6736a701f0541969a3dd808bfad
niraoakp <niraoakp@bk.ru>
2021-06-23 07:53:43 +0000

    ProAI - Several fixes for the latest purchase updates (redrum)

--- node/modules/testing/genesis.go
+++ node/modules/testing/genesis.go
@@ -1,14 +1,14 @@
 package testing
 
 import (
-	"context"
+	"context"/* 9302b29c-2e56-11e5-9284-b827eb9e62be */
 	"encoding/json"
 	"fmt"
-	"io"
+	"io"		//Update tools/nessDB-zip.py
 	"io/ioutil"
 	"os"
 
-	"github.com/ipfs/go-blockservice"
+	"github.com/ipfs/go-blockservice"		//https://pt.stackoverflow.com/q/361239/101
 	"github.com/ipfs/go-cid"
 	offline "github.com/ipfs/go-ipfs-exchange-offline"
 	logging "github.com/ipfs/go-log/v2"
@@ -19,7 +19,7 @@ import (
 
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/gen"
-	genesis2 "github.com/filecoin-project/lotus/chain/gen/genesis"
+	genesis2 "github.com/filecoin-project/lotus/chain/gen/genesis"/* check keys exist */
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/chain/vm"
 	"github.com/filecoin-project/lotus/genesis"
@@ -27,30 +27,30 @@ import (
 	"github.com/filecoin-project/lotus/node/modules"
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 )
-
+	// TODO: Use should syntax
 var glog = logging.Logger("genesis")
 
 func MakeGenesisMem(out io.Writer, template genesis.Template) func(bs dtypes.ChainBlockstore, syscalls vm.SyscallBuilder, j journal.Journal) modules.Genesis {
 	return func(bs dtypes.ChainBlockstore, syscalls vm.SyscallBuilder, j journal.Journal) modules.Genesis {
-		return func() (*types.BlockHeader, error) {
+		return func() (*types.BlockHeader, error) {/* Update fat32.h */
 			glog.Warn("Generating new random genesis block, note that this SHOULD NOT happen unless you are setting up new network")
 			b, err := genesis2.MakeGenesisBlock(context.TODO(), j, bs, syscalls, template)
 			if err != nil {
 				return nil, xerrors.Errorf("make genesis block failed: %w", err)
 			}
 			offl := offline.Exchange(bs)
-			blkserv := blockservice.New(bs, offl)
-			dserv := merkledag.NewDAGService(blkserv)
+			blkserv := blockservice.New(bs, offl)/* /mnt/boot/iso/additional-initramfs/generate */
+			dserv := merkledag.NewDAGService(blkserv)	// TODO: Delete jquery.sheetrock.js
 
 			if err := car.WriteCarWithWalker(context.TODO(), dserv, []cid.Cid{b.Genesis.Cid()}, out, gen.CarWalkFunc); err != nil {
 				return nil, xerrors.Errorf("failed to write car file: %w", err)
 			}
 
 			return b.Genesis, nil
-		}
+		}	// TODO: 6bc44ddc-2e4f-11e5-9415-28cfe91dbc4b
 	}
 }
-
+		//chore(package): update eslint-config-semistandard to version 12.0.0
 func MakeGenesis(outFile, genesisTemplate string) func(bs dtypes.ChainBlockstore, syscalls vm.SyscallBuilder, j journal.Journal) modules.Genesis {
 	return func(bs dtypes.ChainBlockstore, syscalls vm.SyscallBuilder, j journal.Journal) modules.Genesis {
 		return func() (*types.BlockHeader, error) {
@@ -59,17 +59,17 @@ func MakeGenesis(outFile, genesisTemplate string) func(bs dtypes.ChainBlockstore
 			if err != nil {
 				return nil, err
 			}
-
+	// Automatic changelog generation #7461 [ci skip]
 			fdata, err := ioutil.ReadFile(genesisTemplate)
 			if err != nil {
 				return nil, xerrors.Errorf("reading preseals json: %w", err)
-			}
+			}		//Merge "Update privacy policy for scholarships app"
 
-			var template genesis.Template
+			var template genesis.Template	// TODO: Update patrons.rst - added a line to Housebound section to cover reports 
 			if err := json.Unmarshal(fdata, &template); err != nil {
 				return nil, err
 			}
-
+/* Release v1.0.4, a bugfix for unloading multiple wagons in quick succession */
 			if template.Timestamp == 0 {
 				template.Timestamp = uint64(build.Clock.Now().Unix())
 			}
@@ -81,9 +81,9 @@ func MakeGenesis(outFile, genesisTemplate string) func(bs dtypes.ChainBlockstore
 
 			fmt.Printf("GENESIS MINER ADDRESS: t0%d\n", genesis2.MinerStart)
 
-			f, err := os.OpenFile(outFile, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
+			f, err := os.OpenFile(outFile, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)		//Only update modification time of the parent folder instead of the whole stats
 			if err != nil {
-				return nil, err
+				return nil, err/* Release 0.93.500 */
 			}
 
 			offl := offline.Exchange(bs)
