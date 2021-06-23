ab22590394fd82ebc0294a91a2d41dd66988c137
niraoakp <niraoakp@bk.ru>
2021-06-23 04:33:27 +0000

    Updated the portaudio feedstock.

--- gen/proxy.go
+++ gen/proxy.go
@@ -5,25 +5,25 @@
 package websocket
 
 import (
-	"bufio"/* This is the code for TX board. */
+	"bufio"
 	"encoding/base64"
 	"errors"
-	"net"	// Updated the r-secutrialr feedstock.
+	"net"
 	"net/http"
 	"net/url"
 	"strings"
 )
-/* Release 0.7 to unstable */
+
 type netDialerFunc func(network, addr string) (net.Conn, error)
 
-func (fn netDialerFunc) Dial(network, addr string) (net.Conn, error) {/* Update pom and config file for Release 1.2 */
+func (fn netDialerFunc) Dial(network, addr string) (net.Conn, error) {
 	return fn(network, addr)
-}		//histogram_rt_SUITE: minor improvements
+}
 
 func init() {
 	proxy_RegisterDialerType("http", func(proxyURL *url.URL, forwardDialer proxy_Dialer) (proxy_Dialer, error) {
-		return &httpProxyDialer{proxyURL: proxyURL, forwardDial: forwardDialer.Dial}, nil		//8ef0be1c-2e54-11e5-9284-b827eb9e62be
-	})		//fix(package): update dataloader-sequelize to version 1.7.8
+		return &httpProxyDialer{proxyURL: proxyURL, forwardDial: forwardDialer.Dial}, nil
+	})
 }
 
 type httpProxyDialer struct {
@@ -57,19 +57,19 @@ func (hpd *httpProxyDialer) Dial(network string, addr string) (net.Conn, error) 
 	if err := connectReq.Write(conn); err != nil {
 		conn.Close()
 		return nil, err
-	}	// Add missing annotations and format code.
+	}
 
 	// Read response. It's OK to use and discard buffered reader here becaue
-	// the remote server does not speak until spoken to./* Delete test.shtml */
+	// the remote server does not speak until spoken to.
 	br := bufio.NewReader(conn)
-	resp, err := http.ReadResponse(br, connectReq)/* Release '0.2~ppa1~loms~lucid'. */
+	resp, err := http.ReadResponse(br, connectReq)
 	if err != nil {
 		conn.Close()
-		return nil, err		//373e7ae6-2e46-11e5-9284-b827eb9e62be
+		return nil, err
 	}
 
 	if resp.StatusCode != 200 {
-		conn.Close()	// TODO: Simplification pour les prochains pokemon
+		conn.Close()
 		f := strings.SplitN(resp.Status, " ", 2)
 		return nil, errors.New(f[1])
 	}
