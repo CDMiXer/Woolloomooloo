9911ca050791646a320a6e98e2bbc8c80d6a032d
niraoakp <niraoakp@bk.ru>
2021-06-23 08:12:59 +0000

    prepare WFE to have dynamic submodules like sundtek lirc module

--- handler/api/repos/builds/branches/list.go
+++ handler/api/repos/builds/branches/list.go
@@ -1,24 +1,24 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc.		//cober -> cobol (2/2)
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Migrated to SqLite jdbc 3.7.15-M1 Release */
+// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: Update and rename license.txt to license.md
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
+//	// TODO: hacked by bokky.poobah@bokconsulting.com.au
+//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: will be fixed by witek@enjin.io
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-erawtfos ,gnitirw ni ot deerga ro wal elbacilppa yb deriuqer sselnU //
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
-	// Update baudrate_parser to make it more beautiful.
-package branches		//ARM: fix B decoding
+// limitations under the License.		//Sanitize map repo URL templates before sprintf-ing them.
 
-import (/* Merge "Bump all versions for March 13th Release" into androidx-master-dev */
-	"net/http"/* Release 1.2 */
-		//Cholopleth properties - hide range values if quantile.
-	"github.com/drone/drone/core"	// TODO: Use HTTPS instead of git protocol
-	"github.com/drone/drone/handler/api/render"/* Merge "Add regression tests for conditional outputs in nested stacks" */
+package branches
+
+import (
+	"net/http"
+
+	"github.com/drone/drone/core"
+	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
 
 	"github.com/go-chi/chi"
@@ -27,26 +27,26 @@ import (/* Merge "Bump all versions for March 13th Release" into androidx-master
 // HandleList returns an http.HandlerFunc that writes a json-encoded
 // list of build history to the response body.
 func HandleList(
-	repos core.RepositoryStore,	// TODO: Merge "Enhance the compatibility judgement of INSTALLER"
+	repos core.RepositoryStore,
 	builds core.BuildStore,
-) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {/* Extract base class */
-		var (/* enable GDI+ printing for Release builds */
+) http.HandlerFunc {/* Fixed isDateString unit test */
+	return func(w http.ResponseWriter, r *http.Request) {
+		var (
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
-		)		//add the urgency enum
-		repo, err := repos.FindName(r.Context(), namespace, name)	// Merge branch 'development' into feature/rollover-teaching-period
+		)
+		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
-			render.NotFound(w, err)/* Release 5. */
+			render.NotFound(w, err)/* Release 0.8.1.3 */
 			logger.FromRequest(r).
-				WithError(err).
-				WithField("namespace", namespace).
+				WithError(err).	// TODO: will be fixed by juan@benet.ai
+				WithField("namespace", namespace)./* SVG is For Everybody - Chris Coyier */
 				WithField("name", name).
-				Debugln("api: cannot find repository")	// trucs a faire dans l editeur
+				Debugln("api: cannot find repository")
 			return
 		}
-
-		results, err := builds.LatestBranches(r.Context(), repo.ID)
+		//[trunk] Delete obsolete files: mac_build.txt and win_x64_sdk_build.txt.
+		results, err := builds.LatestBranches(r.Context(), repo.ID)	// TODO: hacked by juan@benet.ai
 		if err != nil {
 			render.InternalError(w, err)
 			logger.FromRequest(r).
@@ -57,5 +57,5 @@ func HandleList(
 		} else {
 			render.JSON(w, results, 200)
 		}
-	}
+	}		//Clarify format for specifying output files in help message
 }
