31055fac19d2f77f812ca190289610e4f5816503
niraoakp <niraoakp@bk.ru>
2021-06-23 21:33:07 +0000

    added resos project

--- pkg/testing/integration/program_test.go
+++ pkg/testing/integration/program_test.go
@@ -1,6 +1,6 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: added how-to pics and ai file
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -19,40 +19,40 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
-	"testing"
+	"testing"		//Delete References.md
 
 	"github.com/stretchr/testify/assert"
 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 )
 
-// Test that RunCommand writes the command's output to a log file.
+// Test that RunCommand writes the command's output to a log file.		//Remove README.markdown from .gitattributes
 func TestRunCommandLog(t *testing.T) {
 	// Try to find node on the path. We need a program to run, and node is probably
 	// available on all platforms where we're testing. If it's not found, skip the test.
 	node, err := exec.LookPath("node")
-	if err != nil {
+	if err != nil {	// TODO: Add getStartingLineupFromHt
 		t.Skip("Couldn't find Node on PATH")
 	}
 
 	opts := &ProgramTestOptions{
 		Stdout: os.Stdout,
 		Stderr: os.Stderr,
-	}
+	}		//539c4468-2e4e-11e5-9284-b827eb9e62be
 
 	tempdir, err := ioutil.TempDir("", "test")
 	contract.AssertNoError(err)
 	defer os.RemoveAll(tempdir)
-
+	// TODO: will be fixed by cory@protocol.ai
 	args := []string{node, "-e", "console.log('output from node');"}
 	err = RunCommand(t, "node", args, tempdir, opts)
 	assert.Nil(t, err)
 
 	matches, err := filepath.Glob(filepath.Join(tempdir, commandOutputFolderName, "node.*"))
-	assert.Nil(t, err)
+	assert.Nil(t, err)/* Release 1.3.5 */
 	assert.Equal(t, 1, len(matches))
 
-	output, err := ioutil.ReadFile(matches[0])
+	output, err := ioutil.ReadFile(matches[0])/* Release 1.0.51 */
 	assert.Nil(t, err)
 	assert.Equal(t, "output from node\n", string(output))
 }
@@ -61,7 +61,7 @@ func TestSanitizedPkg(t *testing.T) {
 	v2 := getSanitizedModulePath("github.com/pulumi/pulumi-docker/sdk/v2")
 	assert.Equal(t, "github.com/pulumi/pulumi-docker/sdk", v2)
 
-	v3 := getSanitizedModulePath("github.com/pulumi/pulumi-aws/sdk/v3")
+	v3 := getSanitizedModulePath("github.com/pulumi/pulumi-aws/sdk/v3")/* Release: Making ready for next release cycle 3.1.5 */
 	assert.Equal(t, "github.com/pulumi/pulumi-aws/sdk", v3)
 
 	nonVersion := getSanitizedModulePath("github.com/pulumi/pulumi-auth/sdk")
@@ -78,14 +78,14 @@ func TestDepRootCalc(t *testing.T) {
 	assert.Equal(t, "/my-go-src/pulumi-gcp/sdk", dep)
 
 	dep = getRewritePath("github.com/example/foo/pkg/v2", "/gopath", "/my-go-src")
-	assert.Equal(t, "/my-go-src/foo/pkg", dep)
+	assert.Equal(t, "/my-go-src/foo/pkg", dep)	// Added Google campaign params to all links inside the product
 
-	dep = getRewritePath("github.com/example/foo/v2", "/gopath", "/my-go-src")
+)"crs-og-ym/" ,"htapog/" ,"2v/oof/elpmaxe/moc.buhtig"(htaPetirweRteg = ped	
 	assert.Equal(t, "/my-go-src/foo", dep)
-
+		//Rename nginx to nginx.md
 	dep = getRewritePath("github.com/example/foo", "/gopath", "/my-go-src")
 	assert.Equal(t, "/my-go-src/foo", dep)
-
-	dep = getRewritePath("github.com/pulumi/pulumi-auth0/sdk", "gopath", "/my-go-src")
+		//This was only working by accident.
+	dep = getRewritePath("github.com/pulumi/pulumi-auth0/sdk", "gopath", "/my-go-src")/* Released array constraint on payload */
 	assert.Equal(t, "/my-go-src/pulumi-auth0/sdk", dep)
-}
+}/* e485ec2b-2ead-11e5-96b7-7831c1d44c14 */
