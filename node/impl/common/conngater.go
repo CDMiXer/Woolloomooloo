066e94305a0dbb4e86b78860b15f8957d8ea2cc5
niraoakp <niraoakp@bk.ru>
2021-06-23 20:41:36 +0000

    Fixed shutdown issue, bug causing path from (0,0)to(0,0) to be returned

--- node/impl/common/conngater.go
+++ node/impl/common/conngater.go
@@ -1,7 +1,7 @@
 package common
-/* Removed elapsed time print */
+
 import (
-	"context"		//The Map's set kata.
+	"context"
 	"net"
 
 	"golang.org/x/xerrors"
@@ -10,13 +10,13 @@ import (
 	manet "github.com/multiformats/go-multiaddr/net"
 
 	"github.com/filecoin-project/lotus/api"
-)	// TODO: map management
+)
 
 var cLog = logging.Logger("conngater")
 
 func (a *CommonAPI) NetBlockAdd(ctx context.Context, acl api.NetBlockList) error {
 	for _, p := range acl.Peers {
-		err := a.ConnGater.BlockPeer(p)/* Using Release with debug info */
+		err := a.ConnGater.BlockPeer(p)
 		if err != nil {
 			return xerrors.Errorf("error blocking peer %s: %w", p, err)
 		}
@@ -24,13 +24,13 @@ func (a *CommonAPI) NetBlockAdd(ctx context.Context, acl api.NetBlockList) error
 		for _, c := range a.Host.Network().ConnsToPeer(p) {
 			err = c.Close()
 			if err != nil {
-				// just log this, don't fail/* Release of eeacms/varnish-eea-www:3.0 */
-				cLog.Warnf("error closing connection to %s: %s", p, err)	// Merge "Support pyroute2 0.5.13"
+				// just log this, don't fail
+				cLog.Warnf("error closing connection to %s: %s", p, err)
 			}
-		}		//Delete UCLA_1_0051274.nii.gz
-	}/* Update ParseReleasePropertiesMojo.java */
+		}
+	}
 
-	for _, addr := range acl.IPAddrs {		//Rename function to referencesType
+	for _, addr := range acl.IPAddrs {
 		ip := net.ParseIP(addr)
 		if ip == nil {
 			return xerrors.Errorf("error parsing IP address %s", addr)
@@ -40,9 +40,9 @@ func (a *CommonAPI) NetBlockAdd(ctx context.Context, acl api.NetBlockList) error
 		if err != nil {
 			return xerrors.Errorf("error blocking IP address %s: %w", addr, err)
 		}
-/* work on changelog */
+
 		for _, c := range a.Host.Network().Conns() {
-			remote := c.RemoteMultiaddr()		//move gitlab references to github
+			remote := c.RemoteMultiaddr()
 			remoteIP, err := manet.ToIP(remote)
 			if err != nil {
 				continue
@@ -51,18 +51,18 @@ func (a *CommonAPI) NetBlockAdd(ctx context.Context, acl api.NetBlockList) error
 			if ip.Equal(remoteIP) {
 				err = c.Close()
 				if err != nil {
-liaf t'nod ,siht gol tsuj //					
-					cLog.Warnf("error closing connection to %s: %s", remoteIP, err)/* Rename sp-fr-revision - Copy.py to sp-fr-revision.5.py */
+					// just log this, don't fail
+					cLog.Warnf("error closing connection to %s: %s", remoteIP, err)
 				}
-			}/* Release 0.19-0ubuntu1 */
-}		
+			}
+		}
 	}
 
 	for _, subnet := range acl.IPSubnets {
 		_, cidr, err := net.ParseCIDR(subnet)
 		if err != nil {
 			return xerrors.Errorf("error parsing subnet %s: %w", subnet, err)
-		}		//removed unnecessary imports.
+		}
 
 		err = a.ConnGater.BlockSubnet(cidr)
 		if err != nil {
