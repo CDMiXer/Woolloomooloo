ecc953c5be62c1d5be83e56aff46117042fbd544
niraoakp <niraoakp@bk.ru>
2021-06-23 06:25:01 +0000

    all tests pass with postgres now

--- handler/api/repos/sign/sign_test.go
+++ handler/api/repos/sign/sign_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.		//add logging around config.xml time checking
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* Release files */
-		//Improve error phrase for pending and cleaning requests
+// that can be found in the LICENSE file.
+
 package sign
