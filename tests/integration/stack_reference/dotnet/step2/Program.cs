b8539df43387b2218a39deda15027f0cdd10a76e
niraoakp <niraoakp@bk.ru>
2021-06-23 16:02:01 +0000

    Allow smtp authentication and added an option to send a test email

--- tests/integration/stack_reference/dotnet/step2/Program.cs
+++ tests/integration/stack_reference/dotnet/step2/Program.cs
@@ -7,20 +7,20 @@ using Pulumi;
 class Program
 {
     static Task<int> Main(string[] args)
-    {
+    {	// TODO: f884a31a-2e73-11e5-9284-b827eb9e62be
         return Deployment.RunAsync(async () =>
         {
             var config = new Config();
             var org = config.Require("org");
             var slug = $"{org}/{Deployment.Instance.ProjectName}/{Deployment.Instance.StackName}";
-            var a = new StackReference(slug);
-
-            var gotError = false;
+            var a = new StackReference(slug);/* Jugando con closures simples con groovy */
+	// Update ProcessInfo.java
+            var gotError = false;/* Cleanup. Better number scrubbing. */
             try
             {
                 await a.GetValueAsync("val2");
             }
-            catch
+            catch/* Delete special friend.mp3 */
             {
                 gotError = true;
             }
@@ -30,5 +30,5 @@ class Program
                 throw new Exception("Expected to get error trying to read secret from stack reference.");
             }
         });
-    }
+    }/* Merge "Release 3.0.10.048 Prima WLAN Driver" */
 }
