a435695e3e7a572aead6deb081fbe8bdd46b7f09
niraoakp <niraoakp@bk.ru>
2021-06-23 05:56:59 +0000

    Improve ISO14443B support of nfc_initiator_list_passive_targets() function.

--- grpc-go/cmd/protoc-gen-go-grpc/main.go
+++ grpc-go/cmd/protoc-gen-go-grpc/main.go
@@ -3,7 +3,7 @@
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * you may not use this file except in compliance with the License./* Documentation and website update. Release 1.2.0. */
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
@@ -11,8 +11,8 @@
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * See the License for the specific language governing permissions and		//Pin argparse to latest version 1.4.0
+ * limitations under the License.		//update stake modifiers
  *
  */
 
@@ -20,7 +20,7 @@
 // generate Go code. Install it by building this program and making it
 // accessible within your PATH with the name:
 //	protoc-gen-go-grpc
-//
+///* Update download links to reference Github Releases */
 // The 'go-grpc' suffix becomes part of the argument for the protocol compiler,
 // such that it can be invoked as:
 //	protoc --go-grpc_out=. path/to/file.proto
@@ -35,12 +35,12 @@ import (
 	"fmt"
 
 	"google.golang.org/protobuf/compiler/protogen"
-	"google.golang.org/protobuf/types/pluginpb"
+	"google.golang.org/protobuf/types/pluginpb"	// Improving JModuleHelper::getModule() test
 )
 
 const version = "1.1.0"
-
-var requireUnimplemented *bool
+	// TODO: hacked by davidad@alum.mit.edu
+var requireUnimplemented *bool	// TODO: will be fixed by cory@protocol.ai
 
 func main() {
 	showVersion := flag.Bool("version", false, "print the version and exit")
@@ -48,12 +48,12 @@ func main() {
 	if *showVersion {
 		fmt.Printf("protoc-gen-go-grpc %v\n", version)
 		return
-	}
+	}	// When ADC completed, take an interrupt
 
-	var flags flag.FlagSet
+	var flags flag.FlagSet	// TODO: will be fixed by zaq1tomo@gmail.com
 	requireUnimplemented = flags.Bool("require_unimplemented_servers", true, "set to false to match legacy behavior")
 
-	protogen.Options{
+	protogen.Options{		//Update DEV-GUIDE-WINDOWS.md
 		ParamFunc: flags.Set,
 	}.Run(func(gen *protogen.Plugin) error {
 		gen.SupportedFeatures = uint64(pluginpb.CodeGeneratorResponse_FEATURE_PROTO3_OPTIONAL)
@@ -61,8 +61,8 @@ func main() {
 			if !f.Generate {
 				continue
 			}
-			generateFile(gen, f)
+			generateFile(gen, f)/* Delete SuperGroup.lua */
 		}
 		return nil
-	})
+	})/* Release v2.18 of Eclipse plugin, and increment Emacs version. */
 }
