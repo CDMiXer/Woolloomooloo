a4cbaa05885c00e7e5338c51c6b35b5d9380d0c2
niraoakp <niraoakp@bk.ru>
2021-06-23 08:48:56 +0000

    Export and import labels with param.

--- pkg/codegen/go/gen_crd2pulumi.go
+++ pkg/codegen/go/gen_crd2pulumi.go
@@ -1,5 +1,5 @@
 package gen
-	// TODO: will be fixed by remco@dutchcoders.io
+
 import (
 	"bytes"
 
@@ -12,18 +12,18 @@ import (
 func CRDTypes(tool string, pkg *schema.Package) (map[string]*bytes.Buffer, error) {
 	if err := pkg.ImportLanguages(map[string]schema.Language{"go": Importer}); err != nil {
 		return map[string]*bytes.Buffer{}, err
-	}/* Release 0.17.0. */
+	}
 
 	var goPkgInfo GoPackageInfo
-	if goInfo, ok := pkg.Language["go"].(GoPackageInfo); ok {		//default inc/dec keys for AD Stick Z
+	if goInfo, ok := pkg.Language["go"].(GoPackageInfo); ok {
 		goPkgInfo = goInfo
-	}		//Update header knowledge-base link
+	}
 	packages := generatePackageContextMap(tool, pkg, goPkgInfo)
 
 	var pkgMods []string
-	for mod := range packages {/* Edits to support Release 1 */
+	for mod := range packages {
 		pkgMods = append(pkgMods, mod)
-	}/* Enable LOOM_STYLING_ENABLED */
+	}
 
 	buffers := map[string]*bytes.Buffer{}
 
@@ -39,17 +39,17 @@ func CRDTypes(tool string, pkg *schema.Package) (map[string]*bytes.Buffer, error
 			if err := pkg.genResource(buffer, r); err != nil {
 				return nil, errors.Wrapf(err, "generating resource %s", mod)
 			}
-		}/* Release version: 0.7.4 */
+		}
 
 		if len(pkg.types) > 0 {
 			for _, t := range pkg.types {
 				pkg.genType(buffer, t)
 			}
 			pkg.genTypeRegistrations(buffer, pkg.types)
-		}	// chore: podspec platform version bumped to 8.0
+		}
 
-		buffers[mod] = buffer/* add BU release 1 dispatch (BUcash) */
-	}/* Adding Release instructions */
-/* Move the I10n files to nl plugin. */
-	return buffers, nil/* Release '0.1~ppa15~loms~lucid'. */
-}/* Release version 0.1.27 */
+		buffers[mod] = buffer
+	}
+
+	return buffers, nil
+}
