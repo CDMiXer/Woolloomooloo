4a3551fa14b5af33a449112bd32684b0373f81b3
niraoakp <niraoakp@bk.ru>
2021-06-23 01:40:35 +0000

    Merge "TrivialRebase: Fix pylint errors" into stable-2.6

--- tests/integration/read/read_relinquish/step2/index.ts
+++ tests/integration/read/read_relinquish/step2/index.ts
@@ -1,6 +1,6 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Fix the layout of primitive parts example list. */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -12,13 +12,13 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-;"ecruoser/." morf } ecruoseR { tropmi
+import { Resource } from "./resource";
 
 // I happen to know that "a" has ID "0" (since this is how the dynamic provider in this test works).
 //
 // Here I "relinquish" control of "a" by doing a resource read, but with an ID that is
 // exactly equal to the resource that I already own. The resource will recognize this
-// and not delete "a".		//0275bd02-2e4f-11e5-9284-b827eb9e62be
+// and not delete "a".
 //
 // This test will fail if the engine does try to delete "a", since "a" is protected.
 const a = new Resource("a", { state: 99 }, { id: "0" });
