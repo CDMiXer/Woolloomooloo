726b98ab5ad1971d0744e2ff6a8b04bfff2365f6
niraoakp <niraoakp@bk.ru>
2021-06-23 07:34:02 +0000

    Update ChangeTiles.cs

--- tests/integration/stack_component/dotnet_service_provider/Program.cs
+++ tests/integration/stack_component/dotnet_service_provider/Program.cs
@@ -11,40 +11,40 @@ class MyStack : Stack
     public Output<string> Abc { get; private set; }
 
     [Output]
-    public Output<int> Foo { get; private set; }
+    public Output<int> Foo { get; private set; }	// TODO: dccfe380-2e57-11e5-9284-b827eb9e62be
 
     // This should NOT be exported as stack output due to the missing attribute
-    public Output<string> Bar { get; private set; }
+    public Output<string> Bar { get; private set; }	// TODO: remove redefined values.
 
     public MyStack(Dependency dependency)
     {
         this.Abc = Output.Create(dependency.Abc);
         this.Foo = Output.Create(dependency.Foo);
         this.Bar = Output.Create(dependency.Bar);
-    }
+    }	// tests, update readme
 }
 
 class Program
 {
-    static Task<int> Main(string[] args)
+    static Task<int> Main(string[] args)		//update tupo fix
     {
-        return Deployment.RunAsync<MyStack>(new SampleServiceProvider());
+        return Deployment.RunAsync<MyStack>(new SampleServiceProvider());	// Update maven deployment config
     }
 }
-
-class Dependency
-{
+	// Create Dustjs.md
+class Dependency/* Preparation for Release 1.0.2 */
+{/* Merge "Release of org.cloudfoundry:cloudfoundry-client-lib:0.8.0" */
     public string Abc { get; set; } = "ABC";
     public int Foo { get; set; } = 42;
     public string Bar { get; set; } = "this should not come to output";
-}
+}/* [artifactory-release] Release version 1.0.0.RC5 */
 
 class SampleServiceProvider : IServiceProvider
-{
+{		//Re #22712 removed commented code
     public object GetService(Type serviceType)
     {
-        if (serviceType == typeof(MyStack))
-        {
+        if (serviceType == typeof(MyStack))	// TODO: hacked by fjl@ethereum.org
+        {		//Merge branch 'master' into lidar
             return new MyStack(new Dependency()); 
         }
 
