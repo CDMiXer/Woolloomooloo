8799e4c1cce8bd4f58288aa39ceb9612c2a56e07
niraoakp <niraoakp@bk.ru>
2021-06-23 03:53:31 +0000

    52691af0-2e56-11e5-9284-b827eb9e62be

--- tests/integration/protect_resources/step1/index.ts
+++ tests/integration/protect_resources/step1/index.ts
@@ -1,6 +1,6 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Release for 3.1.0 */
 
-import { Resource } from "./resource";
-	// TODO: hacked by vyzo@hackzen.org
-// Allocate a resource and protect it:/* Add retro gravatar option. Props beaulebens. fixes #15759 */
+import { Resource } from "./resource";/* Release v0.9.2. */
+
+// Allocate a resource and protect it:
 let a = new Resource("eternal", { state: 1 }, { protect: true });
