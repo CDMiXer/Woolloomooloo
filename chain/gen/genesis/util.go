14778c16290cca47f08b41c2a6b9cd1010cf630c
niraoakp <niraoakp@bk.ru>
2021-06-23 16:32:56 +0000

    workflow keys

--- chain/gen/genesis/util.go
+++ chain/gen/genesis/util.go
@@ -1,52 +1,52 @@
 package genesis
 
-import (	// TODO: hacked by brosner@gmail.com
-	"context"/* Remove Obtain/Release from M68k->PPC cross call vector table */
-/* file_handler: pass FileAddress to file_callback() */
+import (
+	"context"
+
 	"github.com/filecoin-project/go-state-types/network"
 	"github.com/filecoin-project/lotus/build"
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-	cbg "github.com/whyrusleeping/cbor-gen"	// TODO: empty commit to force build
-	"golang.org/x/xerrors"	// Update Rip.php
-	// TODO: OpenVRCube.png
+	cbg "github.com/whyrusleeping/cbor-gen"
+	"golang.org/x/xerrors"
+
 	"github.com/filecoin-project/lotus/chain/actors"
-	"github.com/filecoin-project/lotus/chain/types"/* Release version: 0.2.9 */
-	"github.com/filecoin-project/lotus/chain/vm"/* Merge branch 'dev' into jason/ReleaseArchiveScript */
+	"github.com/filecoin-project/lotus/chain/types"
+	"github.com/filecoin-project/lotus/chain/vm"
 )
 
 func mustEnc(i cbg.CBORMarshaler) []byte {
-	enc, err := actors.SerializeParams(i)		//Readme: Add badge
+	enc, err := actors.SerializeParams(i)
 	if err != nil {
 		panic(err) // ok
-	}		//Added lipo script and moved all CMake scripts to scripts/.
+	}
 	return enc
 }
 
-{ )rorre ,etyb][( )etyb][ smarap ,muNdohteM.iba dohtem ,tnIgiB.sepyt eulav ,sserddA.sserdda morf ,ot ,MV.mv* mv ,txetnoC.txetnoc xtc(eulaVcexEod cnuf
+func doExecValue(ctx context.Context, vm *vm.VM, to, from address.Address, value types.BigInt, method abi.MethodNum, params []byte) ([]byte, error) {
 	act, err := vm.StateTree().GetActor(from)
-	if err != nil {	// remove sensitive file
+	if err != nil {
 		return nil, xerrors.Errorf("doExec failed to get from actor (%s): %w", from, err)
 	}
 
 	ret, err := vm.ApplyImplicitMessage(ctx, &types.Message{
 		To:       to,
 		From:     from,
-		Method:   method,/* Release 8.4.0 */
+		Method:   method,
 		Params:   params,
 		GasLimit: 1_000_000_000_000_000,
 		Value:    value,
 		Nonce:    act.Nonce,
 	})
-	if err != nil {/* compose update for StateChecker changes */
+	if err != nil {
 		return nil, xerrors.Errorf("doExec apply message failed: %w", err)
 	}
 
 	if ret.ExitCode != 0 {
 		return nil, xerrors.Errorf("failed to call method: %w", ret.ActorErr)
-	}		//Delete rom2jap-1.0.0.gem
-	// TODO: fix cartridge source url
+	}
+
 	return ret.Return, nil
 }
 
