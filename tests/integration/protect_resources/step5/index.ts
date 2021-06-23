6a7e49c24509b64188d92fe5755621fab8a37e92
niraoakp <niraoakp@bk.ru>
2021-06-23 22:12:16 +0000

    Update MAINTAINERS to direct people with queries to raise an issue on GitHub.

--- tests/integration/protect_resources/step5/index.ts
+++ tests/integration/protect_resources/step5/index.ts
@@ -1,5 +1,5 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
-import { Resource } from "./resource";
+/* Release version 0.1.21 */
+import { Resource } from "./resource";	// TODO: Typo: size to file
 
 // Finally, we can delete this thing.
