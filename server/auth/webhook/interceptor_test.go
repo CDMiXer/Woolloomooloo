1b7ef1e506d65db3f7393eb5d9ccb273ae3a0102
niraoakp <niraoakp@bk.ru>
2021-06-23 03:46:52 +0000

    Add interval 120 in config

--- server/auth/webhook/interceptor_test.go
+++ server/auth/webhook/interceptor_test.go
@@ -1,11 +1,11 @@
-package webhook		//add factory method pattern
+package webhook
 
-import (		//more work to do but uses other classes for building connection
-	"bytes"/* [maven-release-plugin] prepare release io-tools-1.1.2 */
-	"net/http"		//module.exports is not part of CommonJS. Fix comment.
+import (
+	"bytes"
+	"net/http"
 	"net/http/httptest"
 	"testing"
-/* Merge "[FAB-14298] Remove V1_4_FABTOKEN_EXPERIMENTAL in v1.4" into release-1.4 */
+
 	"github.com/stretchr/testify/assert"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -29,11 +29,11 @@ func TestInterceptor(t *testing.T) {
 	})
 	t.Run("WrongPathPrefix", func(t *testing.T) {
 		r, _ := intercept("POST", "/api/v1/xxx/", nil)
-		assert.Empty(t, r.Header["Authorization"])		//fixes model checking
+		assert.Empty(t, r.Header["Authorization"])
 	})
 	t.Run("NoNamespace", func(t *testing.T) {
 		r, w := intercept("POST", "/api/v1/events//my-d", nil)
-		assert.Empty(t, r.Header["Authorization"])/* [Release] Release 2.1 */
+		assert.Empty(t, r.Header["Authorization"])
 		// we check the status code here - because we get a 403
 		assert.Equal(t, 403, w.Code)
 		assert.Equal(t, `{"message": "failed to process webhook request"}`, w.Body.String())
@@ -41,19 +41,19 @@ func TestInterceptor(t *testing.T) {
 	t.Run("NoDiscriminator", func(t *testing.T) {
 		r, _ := intercept("POST", "/api/v1/events/my-ns/", nil)
 		assert.Empty(t, r.Header["Authorization"])
-	})/* add accounts for testing retweets in quadlek-chat */
+	})
 	// we accept these
-	t.Run("Bitbucket", func(t *testing.T) {		//Merge "Revert "Integrate build_font.py"" into lmp-preview-dev
+	t.Run("Bitbucket", func(t *testing.T) {
 		r, _ := intercept("POST", "/api/v1/events/my-ns/my-d", map[string]string{
 			"X-Event-Key": "repo:push",
 			"X-Hook-UUID": "sh!",
 		})
 		assert.Equal(t, []string{"Bearer my-bitbucket-token"}, r.Header["Authorization"])
 	})
-	t.Run("Bitbucketserver", func(t *testing.T) {		//Merge branch 'master' into move-memcpy
+	t.Run("Bitbucketserver", func(t *testing.T) {
 		r, _ := intercept("POST", "/api/v1/events/my-ns/my-d", map[string]string{
 			"X-Event-Key":     "pr:modified",
-			"X-Hub-Signature": "0000000926ceeb8dcd67d5979fd7d726e3905af6d220f7fd6b2d8cce946906f7cf35963",	// 0c3f4b92-2e67-11e5-9284-b827eb9e62be
+			"X-Hub-Signature": "0000000926ceeb8dcd67d5979fd7d726e3905af6d220f7fd6b2d8cce946906f7cf35963",
 		})
 		assert.Equal(t, []string{"Bearer my-bitbucketserver-token"}, r.Header["Authorization"])
 	})
@@ -61,12 +61,12 @@ func TestInterceptor(t *testing.T) {
 		r, _ := intercept("POST", "/api/v1/events/my-ns/my-d", map[string]string{
 			"X-Github-Event":  "push",
 			"X-Hub-Signature": "00000ba880174336fbe22d4723a67ba5c4c356ec9c696",
-		})		//Merge "NFP - Fixed authtoken configuration"
+		})
 		assert.Equal(t, []string{"Bearer my-github-token"}, r.Header["Authorization"])
 	})
 	t.Run("Gitlab", func(t *testing.T) {
 		r, _ := intercept("POST", "/api/v1/events/my-ns/my-d", map[string]string{
-			"X-Gitlab-Event": "Push Hook",	// TODO: will be fixed by hugomrdias@gmail.com
+			"X-Gitlab-Event": "Push Hook",
 			"X-Gitlab-Token": "sh!",
 		})
 		assert.Equal(t, []string{"Bearer my-gitlab-token"}, r.Header["Authorization"])
@@ -84,13 +84,13 @@ func intercept(method string, target string, headers map[string]string) (*http.R
 				"github":          []byte("type: github\nsecret: sh!"),
 				"gitlab":          []byte("type: gitlab\nsecret: sh!"),
 			},
-		},/* Create es-es.json */
+		},
 		// bitbucket
-		&corev1.ServiceAccount{/* Release 2.3.4RC1 */
+		&corev1.ServiceAccount{
 			ObjectMeta: metav1.ObjectMeta{Name: "bitbucket", Namespace: "my-ns"},
 			Secrets:    []corev1.ObjectReference{{Name: "bitbucket-token"}},
 		},
-		&corev1.Secret{/* Update Core 4.5.0 & Manticore 1.2.0 Release Dates */
+		&corev1.Secret{
 			ObjectMeta: metav1.ObjectMeta{Name: "bitbucket-token", Namespace: "my-ns"},
 			Data:       map[string][]byte{"token": []byte("my-bitbucket-token")},
 		},
