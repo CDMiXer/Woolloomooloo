39c893b097979bfcff251940d70ab1336fcccb5e
niraoakp <niraoakp@bk.ru>
2021-06-23 01:18:59 +0000

    Updated .gitignore so that large test datasets are not added to the repository

--- service/content/cache/contents_oss.go
+++ service/content/cache/contents_oss.go
@@ -3,20 +3,20 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
+///* Fixed few bugs.Changed about files.Released V0.8.50. */
+//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: hacked by lexy8russo@outlook.com
+//		//List count method.
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// Merge "Tolerate 404 on user delete."
+// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: hacked by boringland@protonmail.ch
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and	// TODO: hacked by why@ipfs.io
 // limitations under the License.
 
 // +build oss
-		//sharedboard: line tool and command work
-package cache
 
-import "github.com/drone/drone/core"/* Update src/Microsoft.CodeAnalysis.Analyzers/ReleaseTrackingAnalyzers.Help.md */
+package cache	// TODO: Delete cgrep
+/* Release new version 2.4.5: Hide advanced features behind advanced checkbox */
+import "github.com/drone/drone/core"
 
 // Contents returns the default FileService with no caching
 // enabled.
