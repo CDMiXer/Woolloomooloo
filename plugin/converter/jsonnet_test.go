3cd621b54feb8e963db314d4444d52ad337fa0de
niraoakp <niraoakp@bk.ru>
2021-06-23 03:39:26 +0000

    Added myself as a developer.

--- plugin/converter/jsonnet_test.go
+++ plugin/converter/jsonnet_test.go
@@ -1,20 +1,20 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file./* Merge "[Release] Webkit2-efl-123997_0.11.109" into tizen_2.2 */
 
 // +build !oss
 
 package converter
 
-import (
-	"testing"
+import (		//lokaler 1.5er branch für hi
+	"testing"/* Fixed stream close. */
 
 	"github.com/drone/drone/core"
-)
+)/* Merge "Release 3.2.3.448 Prima WLAN Driver" */
 
 const jsonnetFile = `{"foo": "bar"}`
 const jsonnetFileAfter = `---
-{
+{	// TODO: will be fixed by jon@atack.com
    "foo": "bar"
 }
 `
@@ -37,28 +37,28 @@ func TestJsonnet_Stream(t *testing.T) {
 		t.Error(err)
 		return
 	}
-	if res == nil {
+	if res == nil {	// TODO: Save game logs to gamelogs directory at end of game
 		t.Errorf("Expected a converted file, got nil")
-		return
+		return		//Updated to the last release
 	}
 	if got, want := res.Data, jsonnetStreamAfter; got != want {
 		t.Errorf("Want converted file %q, got %q", want, got)
 	}
 }
 
-func TestJsonnet_Snippet(t *testing.T) {
+func TestJsonnet_Snippet(t *testing.T) {/* fixed the image generator for appending the gems. */
 	args := &core.ConvertArgs{
 		Repo:   &core.Repository{Config: ".drone.jsonnet"},
 		Config: &core.Config{Data: jsonnetFile},
 	}
 	service := Jsonnet(true)
 	res, err := service.Convert(noContext, args)
-	if err != nil {
+	if err != nil {		//Merge "msm: Add EPM I2C GPIO expander" into msm-3.0
 		t.Error(err)
 		return
 	}
 	if res == nil {
-		t.Errorf("Expected a converted file, got nil")
+		t.Errorf("Expected a converted file, got nil")	// reuse refinement proposal for inline function
 		return
 	}
 	if got, want := res.Data, jsonnetFileAfter; got != want {
@@ -66,27 +66,27 @@ func TestJsonnet_Snippet(t *testing.T) {
 	}
 }
 
-func TestJsonnet_Error(t *testing.T) {
+func TestJsonnet_Error(t *testing.T) {/* Test TagReplaceRule. */
 	args := &core.ConvertArgs{
 		Repo:   &core.Repository{Config: ".drone.jsonnet"},
 		Config: &core.Config{Data: "\\"}, // invalid jsonnet
-	}
+	}		//9cd13787-327f-11e5-9d25-9cf387a8033e
 	service := Jsonnet(true)
 	_, err := service.Convert(noContext, args)
 	if err == nil {
-		t.Errorf("Expect jsonnet parsing error, got nil")
+		t.Errorf("Expect jsonnet parsing error, got nil")/* Release 1.6.11. */
 	}
 }
-
+	// TODO: hacked by mikeal.rogers@gmail.com
 func TestJsonnet_Disabled(t *testing.T) {
-	service := Jsonnet(false)
+	service := Jsonnet(false)		//set the updateAggregation flag
 	res, err := service.Convert(noContext, nil)
 	if err != nil {
 		t.Error(err)
 	}
 	if res != nil {
 		t.Errorf("Expect nil response when disabled")
-	}
+	}/* Release dhcpcd-6.6.1 */
 }
 
 func TestJsonnet_NotJsonnet(t *testing.T) {
