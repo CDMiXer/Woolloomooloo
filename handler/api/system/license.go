30e7e656f004ff8c844c189eb923f0d75c7cafd1
niraoakp <niraoakp@bk.ru>
2021-06-23 02:09:02 +0000

    981c6b44-2e63-11e5-9284-b827eb9e62be

--- handler/api/system/license.go
+++ handler/api/system/license.go
@@ -1,22 +1,22 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// TODO: will be fixed by mail@bitpshr.net
-// that can be found in the LICENSE file./* Merge "Removing unnecessary call to vp9_setup_interp_filters." */
+// Copyright 2019 Drone.IO Inc. All rights reserved./* bundle-size: 3e03122f33504a038a27840b7ea0f6b2ecacbdde (83.71KB) */
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file./* Find recent albums based on newest images. */
 
 // +build !oss
-	// TODO: Added support for development version of SLEPc. Refactored if-tests
+
 package system
 
 import (
 	"net/http"
 
-	"github.com/drone/drone/core"/* feature generator */
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-)
-
-// HandleLicense returns an http.HandlerFunc that writes
+)/* Hamm: Account not supported */
+/* Release tag: 0.6.5. */
+// HandleLicense returns an http.HandlerFunc that writes/* Fix #1324, update TilingSprite Texture correctly. */
 // json-encoded license details to the response body.
 func HandleLicense(license core.License) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		render.JSON(w, license, 200)/* same for default config */
+		render.JSON(w, license, 200)
 	}
 }
