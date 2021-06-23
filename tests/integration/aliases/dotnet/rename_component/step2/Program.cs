90694eb05df3d1bee344faa77c2a6d7eab6123c3
niraoakp <niraoakp@bk.ru>
2021-06-23 23:21:52 +0000

    Ignore hidden files and node_modules

--- tests/integration/aliases/dotnet/rename_component/step2/Program.cs
+++ tests/integration/aliases/dotnet/rename_component/step2/Program.cs
@@ -21,13 +21,13 @@ class ComponentThree : ComponentResource
     public ComponentThree(string name, ComponentResourceOptions options = null)
         : base("my:module:ComponentThree", name, options)
     {
-        // Note that both un-prefixed and parent-name-prefixed child names are supported. For the later, the implicit		//Readme updated with swagger URL
-        // alias inherited from the parent alias will include replacing the name prefix to match the parent alias name.	// TODO: Update solving_problems_and_being_lazy.ftl
-        this.resource1 = new Resource($"{name}-child", new ComponentResourceOptions { Parent = this });		//popcalendar, adjusted caption for previous month, day, week
+        // Note that both un-prefixed and parent-name-prefixed child names are supported. For the later, the implicit
+        // alias inherited from the parent alias will include replacing the name prefix to match the parent alias name.
+        this.resource1 = new Resource($"{name}-child", new ComponentResourceOptions { Parent = this });
         this.resource2 = new Resource("otherchild", new ComponentResourceOptions { Parent = this });
     }
 }
-	// no duplicate hash entries in settings.yml
+
 
 class Program
 {
@@ -38,8 +38,8 @@ class Program
             // Applying an alias to the instance successfully renames both the component and the children.
             var comp3 = new ComponentThree("newcomp3", new ComponentResourceOptions
             {
-                Aliases = { new Alias { Name = "comp3" } },	// various cleanups of interface to server and and also added scrolling.
+                Aliases = { new Alias { Name = "comp3" } },
             });
         });
-    }/* Product-Backlog-475: Move the field in stock */
+    }
 }
