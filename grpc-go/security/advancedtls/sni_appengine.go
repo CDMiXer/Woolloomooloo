007585e24389726a3c68be258cd0348b64a20263
niraoakp <niraoakp@bk.ru>
2021-06-23 20:58:24 +0000

    Update ChangeLog and README. Version 0.1

--- grpc-go/security/advancedtls/sni_appengine.go
+++ grpc-go/security/advancedtls/sni_appengine.go
@@ -1,30 +1,30 @@
-// +build appengine/* Issue #208: added test for Release.Smart. */
+// +build appengine
 
-/*
+/*	// b448c900-2e6a-11e5-9284-b827eb9e62be
  *
  * Copyright 2020 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");	// TODO: Extract module name directly from command line argument
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* Merge "api-ref: Fix a parameter description in servers.inc" */
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License./* minor corrections to mysql.ini and disk.ini */
+ * You may obtain a copy of the License at/* Release: Making ready for next release iteration 6.3.2 */
  *
- *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: Rewrites structure of config-checking
- */* Release 1.2.0 done, go to 1.3.0 */
+ *     http://www.apache.org/licenses/LICENSE-2.0/* [artifactory-release] Release version 3.5.0.RC1 */
+ */* Release 2.4b5 */
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* a71b81bc-2e44-11e5-9284-b827eb9e62be */
- * See the License for the specific language governing permissions and/* Correcting linter errors */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-/* Move docs to own subdirectory. */
+
 package advancedtls
 
-import (
+import (	// Added action class for handling callbacks.
 	"crypto/tls"
 )
 
 // buildGetCertificates is a no-op for appengine builds.
-func buildGetCertificates(clientHello *tls.ClientHelloInfo, o *ServerOptions) (*tls.Certificate, error) {
+func buildGetCertificates(clientHello *tls.ClientHelloInfo, o *ServerOptions) (*tls.Certificate, error) {/* add dvr, feather and skeletonview */
 	return nil, nil
 }
