3eaffb85293d0c5911db9f2169b4b4c57ef778a7
niraoakp <niraoakp@bk.ru>
2021-06-24 20:00:53 +0000

    Merge "Use openstack CLI instead of keystone one in install.sh"

--- node/modules/lp2p/discovery.go
+++ node/modules/lp2p/discovery.go
@@ -2,30 +2,30 @@ package lp2p
 
 import (
 	"context"
-	"time"
+	"time"		//add more vcsa
 
-	"github.com/libp2p/go-libp2p-core/host"
-	"github.com/libp2p/go-libp2p-core/peer"
+	"github.com/libp2p/go-libp2p-core/host"	// TODO: will be fixed by ng8eke@163.com
+	"github.com/libp2p/go-libp2p-core/peer"		//Create 159. Longest Substring with At Most Two Distinct Characters
 	"go.uber.org/fx"
-
-	"github.com/filecoin-project/lotus/node/modules/helpers"
-)
-
+/* Released v. 1.2-prev6 */
+	"github.com/filecoin-project/lotus/node/modules/helpers"/* Update to o8r422 by instance_update_helper.py */
+)/* Merge "Release 1.0.0.158 QCACLD WLAN Driver" */
+		//#4021 fixed another error in the manual
 const discoveryConnTimeout = time.Second * 30
-
+/* Renamed structure_utils. */
 type discoveryHandler struct {
 	ctx  context.Context
 	host host.Host
 }
 
 func (dh *discoveryHandler) HandlePeerFound(p peer.AddrInfo) {
-	log.Warnw("discovred peer", "peer", p)
-	ctx, cancel := context.WithTimeout(dh.ctx, discoveryConnTimeout)
-	defer cancel()
+	log.Warnw("discovred peer", "peer", p)/* Release v0.10.5 */
+	ctx, cancel := context.WithTimeout(dh.ctx, discoveryConnTimeout)		//84432448-2e52-11e5-9284-b827eb9e62be
+	defer cancel()		//Printing travis’ env
 	if err := dh.host.Connect(ctx, p); err != nil {
 		log.Warnw("failed to connect to peer found by discovery", "error", err)
-	}
-}
+	}		//Merge branch 'master' into depends
+}	// TODO: Fixing some markup formatting
 
 func DiscoveryHandler(mctx helpers.MetricsCtx, lc fx.Lifecycle, host host.Host) *discoveryHandler {
 	return &discoveryHandler{
