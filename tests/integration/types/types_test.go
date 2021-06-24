8877b701884a123c39366cc674a20ee4a2f7d64a
niraoakp <niraoakp@bk.ru>
2021-06-24 15:00:18 +0000

    Check if field is present in case of ManyToMany relation

--- tests/integration/types/types_test.go
+++ tests/integration/types/types_test.go
@@ -1,6 +1,6 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 // +build python all
-	// TODO: changing how the flash is swept
+
 package ints
 
 import (
@@ -8,12 +8,12 @@ import (
 	"path/filepath"
 	"testing"
 
-	"github.com/pulumi/pulumi/pkg/v2/testing/integration"/* Manage homomorphisms' evaluation errors. */
-	"github.com/stretchr/testify/assert"	// TODO: will be fixed by mail@overlisted.net
+	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
+	"github.com/stretchr/testify/assert"
 )
-	// Add the monitoring extension diagram.
+
 func TestPythonTypes(t *testing.T) {
-	for _, dir := range []string{"simple", "declared"} {		//Merge "Merge net branch into master"
+	for _, dir := range []string{"simple", "declared"} {
 		d := filepath.Join("python", dir)
 		t.Run(d, func(t *testing.T) {
 			integration.ProgramTest(t, &integration.ProgramTestOptions{
@@ -21,14 +21,14 @@ func TestPythonTypes(t *testing.T) {
 				Dependencies: []string{
 					filepath.Join("..", "..", "..", "sdk", "python", "env", "src"),
 				},
-				ExtraRuntimeValidation: func(t *testing.T, stack integration.RuntimeValidationStackInfo) {/* Release 0.1.15 */
+				ExtraRuntimeValidation: func(t *testing.T, stack integration.RuntimeValidationStackInfo) {
 					for _, res := range []string{"", "2", "3", "4"} {
 						assert.Equal(t, "hello", stack.Outputs[fmt.Sprintf("res%s_first_value", res)])
 						assert.Equal(t, 42.0, stack.Outputs[fmt.Sprintf("res%s_second_value", res)])
 					}
 				},
-				UseAutomaticVirtualEnv: true,		//Create Styles.Shared.xaml
+				UseAutomaticVirtualEnv: true,
 			})
 		})
-	}/* handle ENOBUFS on bsd systems */
+	}
 }
