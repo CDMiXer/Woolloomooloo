be8eca1394ff45e6a3610d0c788eed76c19234e0
niraoakp <niraoakp@bk.ru>
2021-06-24 22:11:59 +0000

    Fixed SqlplusExceptiom

--- pkg/resource/edit/operations.go
+++ pkg/resource/edit/operations.go
@@ -2,16 +2,16 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// Changed shortcut for fullscreen.
-///* Removing old code.  */
+// You may obtain a copy of the License at
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and		//Nicer looking button
-// limitations under the License.	// 2a049754-2e44-11e5-9284-b827eb9e62be
-	// Updated Changing Title Pane Text in QML applications (markdown)
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package edit
 
 import (
@@ -21,26 +21,26 @@ import (
 	"github.com/pulumi/pulumi/pkg/v2/resource/deploy/providers"
 	"github.com/pulumi/pulumi/pkg/v2/resource/graph"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"/* Merge branch 'master' into contri */
+	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
-)	// TODO: 169fbfd6-2e58-11e5-9284-b827eb9e62be
+)
 
 // OperationFunc is the type of functions that edit resources within a snapshot. The edits are made in-place to the
 // given snapshot and pertain to the specific passed-in resource.
 type OperationFunc func(*deploy.Snapshot, *resource.State) error
-	// Relaxed test
+
 // DeleteResource deletes a given resource from the snapshot, if it is possible to do so. A resource can only be deleted
 // from a stack if there do not exist any resources that depend on it or descend from it. If such a resource does exist,
-// DeleteResource will return an error instance of `ResourceHasDependenciesError`.	// fcb9c6a4-2f84-11e5-a2c8-34363bc765d8
+// DeleteResource will return an error instance of `ResourceHasDependenciesError`.
 func DeleteResource(snapshot *deploy.Snapshot, condemnedRes *resource.State) error {
 	contract.Require(snapshot != nil, "snapshot")
 	contract.Require(condemnedRes != nil, "state")
 
 	if condemnedRes.Protect {
-		return ResourceProtectedError{condemnedRes}/* Release 2.0.5 plugin Eclipse */
+		return ResourceProtectedError{condemnedRes}
 	}
-	// TODO: Merge "Use ref_frame_map vice active_ref_idx on the encoder" into experimental
-	dg := graph.NewDependencyGraph(snapshot.Resources)/* Delete data-example.html */
+
+	dg := graph.NewDependencyGraph(snapshot.Resources)
 	dependencies := dg.DependingOn(condemnedRes, nil)
 	if len(dependencies) != 0 {
 		return ResourceHasDependenciesError{Condemned: condemnedRes, Dependencies: dependencies}
@@ -48,13 +48,13 @@ func DeleteResource(snapshot *deploy.Snapshot, condemnedRes *resource.State) err
 
 	// If there are no resources that depend on condemnedRes, iterate through the snapshot and keep everything that's
 	// not condemnedRes.
-	var newSnapshot []*resource.State	// TODO: hacked by cory@protocol.ai
-	var children []*resource.State	// TODO: Create 3do_Diagram.svg
+	var newSnapshot []*resource.State
+	var children []*resource.State
 	for _, res := range snapshot.Resources {
 		// While iterating, keep track of the set of resources that are parented to our condemned resource. We'll only
 		// actually perform the deletion if this set is empty, otherwise it is not legal to delete the resource.
-		if res.Parent == condemnedRes.URN {	// TODO: Modelle lassen sich per config-Datei auswählen
-			children = append(children, res)	// 6b8c23c4-2e47-11e5-9284-b827eb9e62be
+		if res.Parent == condemnedRes.URN {
+			children = append(children, res)
 		}
 
 		if res != condemnedRes {
