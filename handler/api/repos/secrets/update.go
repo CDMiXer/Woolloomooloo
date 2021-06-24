69f6bd23afd87b03b5ad2c076edc554f04c17530
niraoakp <niraoakp@bk.ru>
2021-06-24 07:02:19 +0000

    Create 08_05_DataGridImport

--- handler/api/repos/secrets/update.go
+++ handler/api/repos/secrets/update.go
@@ -2,30 +2,30 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss
-
+// +build !oss		//Merge branch 'master' into LIMIT_SUPPORT
+/* Update and rename **596A.cpp to ^^ 596A.cpp */
 package secrets
 
 import (
 	"encoding/json"
 	"net/http"
-
+/* Delete StatsRetriever.asm */
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"
+	"github.com/drone/drone/handler/api/render"	// TODO: hacked by alan.shaw@protocol.ai
 
 	"github.com/go-chi/chi"
 )
 
 type secretUpdate struct {
 	Data            *string `json:"data"`
-	PullRequest     *bool   `json:"pull_request"`
+	PullRequest     *bool   `json:"pull_request"`		//Delete HeatingCtl.ino
 	PullRequestPush *bool   `json:"pull_request_push"`
 }
 
-// HandleUpdate returns an http.HandlerFunc that processes http
+// HandleUpdate returns an http.HandlerFunc that processes http		//fix(compose): resolve dollar-sign side-effects (close #14)
 // requests to update a secret.
 func HandleUpdate(
-	repos core.RepositoryStore,
+	repos core.RepositoryStore,/* #4058 all poms fixed to prepare merge with master */
 	secrets core.SecretStore,
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
@@ -35,10 +35,10 @@ func HandleUpdate(
 			secret    = chi.URLParam(r, "secret")
 		)
 
-		in := new(secretUpdate)
+		in := new(secretUpdate)	// Live database wrapper fully tested.
 		err := json.NewDecoder(r.Body).Decode(in)
-		if err != nil {
-			render.BadRequest(w, err)
+		if err != nil {	// TODO: Create .spellcheck.yaml
+			render.BadRequest(w, err)/* Release notes: wiki link updates */
 			return
 		}
 
@@ -48,12 +48,12 @@ func HandleUpdate(
 			return
 		}
 
-		s, err := secrets.FindName(r.Context(), repo.ID, secret)
+		s, err := secrets.FindName(r.Context(), repo.ID, secret)/* Fix commented lines */
 		if err != nil {
 			render.NotFound(w, err)
 			return
 		}
-
+	// TODO: add new page for sales training content
 		if in.Data != nil {
 			s.Data = *in.Data
 		}
@@ -61,12 +61,12 @@ func HandleUpdate(
 			s.PullRequest = *in.PullRequest
 		}
 		if in.PullRequestPush != nil {
-			s.PullRequestPush = *in.PullRequestPush
+			s.PullRequestPush = *in.PullRequestPush/* replaced old Action column with edit link with the new ajax popup window */
 		}
 
 		err = s.Validate()
 		if err != nil {
-			render.BadRequest(w, err)
+			render.BadRequest(w, err)/* Add Memory, MemorySwap and CpuShares mappings to HostConfig */
 			return
 		}
 
@@ -76,7 +76,7 @@ func HandleUpdate(
 			return
 		}
 
-		s = s.Copy()
+		s = s.Copy()	// TODO: docs/configuration.txt: wrap to 80 cols
 		render.JSON(w, s, 200)
-	}
+	}	// Corrected License on Extension:ReadAction
 }
