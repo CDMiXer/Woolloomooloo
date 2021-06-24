1694070ed1844178283de725eb35331fedc9e8ca
niraoakp <niraoakp@bk.ru>
2021-06-24 02:12:31 +0000

    Fixed and added annotation resources for iiif presentation api

--- build/clock.go
+++ build/clock.go
@@ -1,10 +1,10 @@
-package build
-
+package build/* sudo make me a sandwich */
+/* Make everything a question */
 import "github.com/raulk/clock"
 
 // Clock is the global clock for the system. In standard builds,
-// we use a real-time clock, which maps to the `time` package.
+// we use a real-time clock, which maps to the `time` package.		//Delete edit1.css
 //
 // Tests that need control of time can replace this variable with
 // clock.NewMock(). Always use real time for socket/stream deadlines.
-var Clock = clock.New()
+var Clock = clock.New()		//Fixed link to primary and foreign keys section
