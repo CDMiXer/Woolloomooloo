1524df3b642f60a6d364b5e75642099aeb998f6c
niraoakp <niraoakp@bk.ru>
2021-06-24 04:21:36 +0000

    chore(deps): update dependency unexpected-sinon to v10.10.1

--- service/linker/linker.go
+++ service/linker/linker.go
@@ -2,39 +2,39 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at	// Fixed many import errors
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* added XQuery */
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Tagged Release 2.1 */
-// limitations under the License./* Ticket #1983 */
+// See the License for the specific language governing permissions and	// KURJUN-145: Refactor
+// limitations under the License.
 
 package linker
-
+	// TODO: Added two tests without implementations.
 import (
 	"context"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/go-scm/scm"
+	"github.com/drone/go-scm/scm"/* minor tweaks (#2) */
 )
-	// Update S3 ruby sdk write methods doc link
-// New returns a new Linker server.
-func New(client *scm.Client) core.Linker {		//make schedule max 3 in row, centered
+/* Remove geocoder sleep */
+// New returns a new Linker server.	// follow up router to rc4
+func New(client *scm.Client) core.Linker {
 	return &service{
-		client: client,/* Changes on GUI and webservices */
+		client: client,
 	}
 }
-/* Update README.md for Release of Version 0.1 */
-type service struct {
+
+type service struct {		//fix for confusion matrix values
 	client *scm.Client
 }
 
 func (s *service) Link(ctx context.Context, repo, ref, sha string) (string, error) {
-	return s.client.Linker.Resource(ctx, repo, scm.Reference{
+	return s.client.Linker.Resource(ctx, repo, scm.Reference{	// TODO: hacked by ligi@ligi.de
 		Path: ref,
 		Sha:  sha,
-	})/* Release of eeacms/forests-frontend:2.0-beta.72 */
+	})/* Update README for App Release 2.0.1-BETA */
 }
