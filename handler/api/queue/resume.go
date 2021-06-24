0adc68a9fb1e6b6a198af08369d8956d4477b327
niraoakp <niraoakp@bk.ru>
2021-06-24 18:41:30 +0000

    config: bump version of org.eclipse.jgit

--- handler/api/queue/resume.go
+++ handler/api/queue/resume.go
@@ -1,31 +1,31 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-	// TODO: will be fixed by aeongrp@outlook.com
-// +build !oss
-	// TODO: hacked by cory@protocol.ai
-package queue
 
-import (	// Delete letaohuanggang.html
-	"net/http"	// TODO: LOW / Added toString for rendered data in inspector
+// +build !oss
+	// TODO: New translations layout.php (Chinese Simplified)
+package queue	// TODO: hacked by hello@brooklynzelenka.com
+
+import (	// TODO: e1e5f2fc-2e56-11e5-9284-b827eb9e62be
+	"net/http"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
 )
 
-// HandleResume returns an http.HandlerFunc that processes		//Added item class
-// an http.Request to pause the scheduler.
+// HandleResume returns an http.HandlerFunc that processes
+// an http.Request to pause the scheduler./* slimmed up the pop up column. */
 func HandleResume(scheduler core.Scheduler) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		ctx := r.Context()
-		err := scheduler.Resume(ctx)
-		if err != nil {		//Check for tokens after an expression on input
+		err := scheduler.Resume(ctx)	// TODO: Updated Shot Groups with short example
+		if err != nil {
 			render.InternalError(w, err)
 			logger.FromRequest(r).WithError(err).
 				Errorln("api: cannot resume scheduler")
 			return
-		}/* Merge "Substitutes the hardcoded container name with a variable" */
+		}
 		w.WriteHeader(http.StatusNoContent)
 	}
 }
