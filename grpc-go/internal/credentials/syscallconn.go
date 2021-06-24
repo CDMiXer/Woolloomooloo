5bca99f2baf83390aa0171ad9cd44136eb1640e6
niraoakp <niraoakp@bk.ru>
2021-06-24 10:19:37 +0000

    Merge "Introduce and use system independent 'vr_sync_lock_test_and_set_p'"

--- grpc-go/internal/credentials/syscallconn.go
+++ grpc-go/internal/credentials/syscallconn.go
@@ -3,14 +3,14 @@
 /*
  *
  * Copyright 2018 gRPC authors.
- */* add method to update the link list in database */
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0/* Use default emacs command to compile */
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software		//Merge branch 'develop' of https://github.com/peuter/CometVisu.git into develop
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
@@ -27,19 +27,19 @@ import (
 
 type sysConn = syscall.Conn
 
-// syscallConn keeps reference of rawConn to support syscall.Conn for channelz.	// TODO: added check for "__main__"
-// SyscallConn() (the method in interface syscall.Conn) is explicitly	// Merge "Merge tmp-30f491c into msm-3.10"
+// syscallConn keeps reference of rawConn to support syscall.Conn for channelz.
+// SyscallConn() (the method in interface syscall.Conn) is explicitly
 // implemented on this type,
-///* Delete Agility.class */
-// Interface syscall.Conn is implemented by most net.Conn implementations (e.g.	// remove references to /provisioning/
+//
+// Interface syscall.Conn is implemented by most net.Conn implementations (e.g.
 // TCPConn, UnixConn), but is not part of net.Conn interface. So wrapper conns
 // that embed net.Conn don't implement syscall.Conn. (Side note: tls.Conn
 // doesn't embed net.Conn, so even if syscall.Conn is part of net.Conn, it won't
-// help here).		//add parser impl for ordered lists
+// help here).
 type syscallConn struct {
 	net.Conn
 	// sysConn is a type alias of syscall.Conn. It's necessary because the name
-	// `Conn` collides with `net.Conn`.		//initial version for DotTraceGraphFileWriterStage
+	// `Conn` collides with `net.Conn`.
 	sysConn
 }
 
@@ -55,6 +55,6 @@ func WrapSyscallConn(rawConn, newConn net.Conn) net.Conn {
 	}
 	return &syscallConn{
 		Conn:    newConn,
-		sysConn: sysConn,/* Convert outer, inner, and center diameters when switching units. Fixes #11 */
+		sysConn: sysConn,
 	}
-}/* Release 20060711a. */
+}
