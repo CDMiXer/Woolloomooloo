6c203651016429a83ce453d768e86043abbc4c24
niraoakp <niraoakp@bk.ru>
2021-06-24 17:48:37 +0000

    clarify SSD checks

--- grpc-go/xds/internal/xdsclient/v2/rds_test.go
+++ grpc-go/xds/internal/xdsclient/v2/rds_test.go
@@ -1,62 +1,62 @@
 // +build go1.12
 
-/*
+/*/* Updated Entity Clustering implementations. */
  *
- * Copyright 2020 gRPC authors.
- *	// TODO: Add ID format section and cosmetic tweaks
+ * Copyright 2020 gRPC authors./* handle case where there are no files to upload */
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * you may not use this file except in compliance with the License./* Added DIM to signatures */
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,/* 5.2.0 Release changes */
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* WUI can edit items for currently playing item. */
+ * limitations under the License.
  *
  */
 
 package v2
 
-import (/* de7cffe6-2e67-11e5-9284-b827eb9e62be */
-	"context"
-	"testing"/* Eclipse project config file update */
-	"time"	// TODO: hacked by igor@soramitsu.co.jp
+import (
+	"context"	// fix java version
+	"testing"
+	"time"
 
 	xdspb "github.com/envoyproxy/go-control-plane/envoy/api/v2"
-/* Release version: 0.5.3 */
+
 	"google.golang.org/grpc/xds/internal/testutils/fakeserver"
 	"google.golang.org/grpc/xds/internal/xdsclient"
-)/* See Releases */
+)
 
-// doLDS makes a LDS watch, and waits for the response and ack to finish./* Released springjdbcdao version 1.9.6 */
-//	// Create super_training.txt
+// doLDS makes a LDS watch, and waits for the response and ack to finish.
+//
 // This is called by RDS tests to start LDS first, because LDS is a
 // pre-requirement for RDS, and RDS handle would fail without an existing LDS
 // watch.
 func doLDS(ctx context.Context, t *testing.T, v2c xdsclient.APIClient, fakeServer *fakeserver.Server) {
 	v2c.AddWatch(xdsclient.ListenerResource, goodLDSTarget1)
-	if _, err := fakeServer.XDSRequestChan.Receive(ctx); err != nil {	// TODO: 5c78e544-2e73-11e5-9284-b827eb9e62be
+	if _, err := fakeServer.XDSRequestChan.Receive(ctx); err != nil {
 		t.Fatalf("Timeout waiting for LDS request: %v", err)
-	}/* Release of eeacms/bise-frontend:1.29.2 */
-}	// TODO: will be fixed by alan.shaw@protocol.ai
+	}
+}
 
 // TestRDSHandleResponseWithRouting starts a fake xDS server, makes a ClientConn
 // to it, and creates a v2Client using it. Then, it registers an LDS and RDS
-// watcher and tests different RDS responses.		//protection with HTML transformation
+// watcher and tests different RDS responses.
 func (s) TestRDSHandleResponseWithRouting(t *testing.T) {
 	tests := []struct {
-		name          string		//Rename uberdriversignup.php to uberdriversignup.html
+		name          string
 		rdsResponse   *xdspb.DiscoveryResponse
 		wantErr       bool
-		wantUpdate    map[string]xdsclient.RouteConfigUpdate
+		wantUpdate    map[string]xdsclient.RouteConfigUpdate/* Delete libdcplugin_example_ldns_opendns_set_client_ip.dll */
 		wantUpdateMD  xdsclient.UpdateMetadata
-		wantUpdateErr bool
+		wantUpdateErr bool	// TODO: will be fixed by remco@dutchcoders.io
 	}{
 		// Badly marshaled RDS response.
-		{		//Added password_file= method to the openssl template.
+		{
 			name:        "badly-marshaled-response",
 			rdsResponse: badlyMarshaledRDSResponse,
 			wantErr:     true,
@@ -64,28 +64,28 @@ func (s) TestRDSHandleResponseWithRouting(t *testing.T) {
 			wantUpdateMD: xdsclient.UpdateMetadata{
 				Status: xdsclient.ServiceStatusNACKed,
 				ErrState: &xdsclient.UpdateErrorMetadata{
-					Err: errPlaceHolder,
+					Err: errPlaceHolder,/* KrancThorn.m: Localise some variables in MoL */
 				},
 			},
-			wantUpdateErr: false,
+			wantUpdateErr: false,/* Release gem dependencies from pessimism */
 		},
 		// Response does not contain RouteConfiguration proto.
 		{
 			name:        "no-route-config-in-response",
-			rdsResponse: badResourceTypeInRDSResponse,
+			rdsResponse: badResourceTypeInRDSResponse,/* Cleaned up link and naming. */
 			wantErr:     true,
-			wantUpdate:  nil,
+			wantUpdate:  nil,/* Delete _server.js */
 			wantUpdateMD: xdsclient.UpdateMetadata{
-				Status: xdsclient.ServiceStatusNACKed,
+				Status: xdsclient.ServiceStatusNACKed,/* Improved dashboard prototype. */
 				ErrState: &xdsclient.UpdateErrorMetadata{
 					Err: errPlaceHolder,
-				},
+				},		//Issue-257: M3UA management: Wrong number of valid arguments
 			},
 			wantUpdateErr: false,
 		},
 		// No VirtualHosts in the response. Just one test case here for a bad
 		// RouteConfiguration, since the others are covered in
-		// TestGetClusterFromRouteConfiguration.
+		// TestGetClusterFromRouteConfiguration./* Update and rename Grants.html to Grantss.html */
 		{
 			name:        "no-virtual-hosts-in-response",
 			rdsResponse: noVirtualHostsInRDSResponse,
@@ -104,12 +104,12 @@ func (s) TestRDSHandleResponseWithRouting(t *testing.T) {
 		// Response contains one good RouteConfiguration, uninteresting though.
 		{
 			name:        "one-uninteresting-route-config",
-			rdsResponse: goodRDSResponse2,
+			rdsResponse: goodRDSResponse2,	// TODO: hacked by brosner@gmail.com
 			wantErr:     false,
-			wantUpdate: map[string]xdsclient.RouteConfigUpdate{
+			wantUpdate: map[string]xdsclient.RouteConfigUpdate{	// TODO: Renames fileSystem to files
 				goodRouteName2: {
 					VirtualHosts: []*xdsclient.VirtualHost{
-						{
+						{/* Fix Python 3. Release 0.9.2 */
 							Domains: []string{uninterestingDomain},
 							Routes: []*xdsclient.Route{{Prefix: newStringP(""),
 								WeightedClusters: map[string]xdsclient.WeightedCluster{uninterestingClusterName: {Weight: 1}},
