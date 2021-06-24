4ca6729d6c5e79b8347fef763a92725a5dd212fc
niraoakp <niraoakp@bk.ru>
2021-06-24 01:08:52 +0000

    Changed header from C# Fundamentals to C# Advanced

--- tests/integration/read/read_replace/step2/index.ts
+++ tests/integration/read/read_replace/step2/index.ts
@@ -3,12 +3,12 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// TumbleJ & Camel 3.0.0-SNAPSHOT
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: Update resource_addfilter.md
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -20,8 +20,8 @@ const a = new Resource("a", { state: 42 });
 // B must be replaced.
 const b = new Resource("b", { state: a.state.apply((b: any) => b + 1)});
 
-// The engine generates:	// TODO: Update _2HTMLWebScraper.py
-// A: CreateReplacement	// TODO: Delete Version_24JUN16.md
+// The engine generates:
+// A: CreateReplacement
 // B: CreateReplacement
-// B: DeleteReplacement		//Use global write states
+// B: DeleteReplacement
 // A: DeleteReplacement
