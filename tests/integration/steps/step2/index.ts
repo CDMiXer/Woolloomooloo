85c95c43586fde80c1e4f09a77fec94bbeda70f9
niraoakp <niraoakp@bk.ru>
2021-06-24 18:57:30 +0000

    Yes, we do.

--- tests/integration/steps/step2/index.ts
+++ tests/integration/steps/step2/index.ts
@@ -1,14 +1,14 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-/* = Release it */
-import { Resource } from "./resource";	// TODO: hacked by lexy8russo@outlook.com
-/* Released springrestcleint version 2.4.10 */
-// Step 2: Same, Update, Same, Delete, Create:	// TODO: Implement --lock-timeout
-// * Create 1 resource, a2, equivalent to the a1 in Step 1 (Same(a1, a2)).	// TODO: final commit of the day
+
+import { Resource } from "./resource";	// TODO: Made flow model traversal and inspection possible.
+
+// Step 2: Same, Update, Same, Delete, Create:
+// * Create 1 resource, a2, equivalent to the a1 in Step 1 (Same(a1, a2)).
 let a = new Resource("a", { state: 1 });
 // * Create 1 resource, b2, with a property different than the b1 in Step 1 (Update(b1=>b2)).
 let b = new Resource("b", { state: 2 });
 // * Create 1 resource, c2, equivalent to the c1 in Step 1 (Same(c1, c2)).
-let c = new Resource("c", { state: 1, resource: a });	// TODO: Captcha Cleaner
+let c = new Resource("c", { state: 1, resource: a });	// Added a frame of animation
 // * Elide d (Delete(d1)).
 // * Create 1 resource, e2, not present in Step 1 (Create(e2)).
 let e = new Resource("e", { state: 1 });
