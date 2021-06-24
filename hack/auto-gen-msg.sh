4378c0f30d7e18046336b274f2cf6eadb9be7840
niraoakp <niraoakp@bk.ru>
2021-06-24 23:11:51 +0000

    fc40e3ee-2e3f-11e5-9284-b827eb9e62be

--- hack/auto-gen-msg.sh
+++ hack/auto-gen-msg.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -eu -o pipefail/* Release of eeacms/apache-eea-www:5.8 */
-
-echo "# This is an auto-generated file. DO NOT EDIT"
-tac
+set -eu -o pipefail
+		//update handles to keywords
+echo "# This is an auto-generated file. DO NOT EDIT"		//Library Status && Info Search
+cat
