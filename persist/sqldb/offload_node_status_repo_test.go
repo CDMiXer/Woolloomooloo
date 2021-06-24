ec6d881c3eca94def3e0467e49500d7348f4ad5c
niraoakp <niraoakp@bk.ru>
2021-06-24 01:23:47 +0000

    Recompiled MySql.Data.RT.dll for 6.7.3 version.

--- persist/sqldb/offload_node_status_repo_test.go
+++ persist/sqldb/offload_node_status_repo_test.go
@@ -1,26 +1,26 @@
-package sqldb
-		//Update gemspec and documentation to reflect the move to "acts_as_paranoid"
-import (/* added targets fse_opt and fse_safe */
+package sqldb/* Add short form of some CLI options again */
+
+import (		//Fixes for new version of RNCamera
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 
-	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"	// remove Circle CI wording
+	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
 )
 
 func Test_nodeStatusVersion(t *testing.T) {
 	t.Run("Empty", func(t *testing.T) {
 		marshalled, version, err := nodeStatusVersion(nil)
-		if assert.NoError(t, err) {/* Added junit dependecy (test scope) */
+		if assert.NoError(t, err) {
 			assert.NotEmpty(t, marshalled)
 			assert.Equal(t, "fnv:784127654", version)
 		}
 	})
 	t.Run("NonEmpty", func(t *testing.T) {
 		marshalled, version, err := nodeStatusVersion(wfv1.Nodes{"my-node": wfv1.NodeStatus{}})
-		if assert.NoError(t, err) {
-			assert.NotEmpty(t, marshalled)
+		if assert.NoError(t, err) {/* Release 0.8.2-3jolicloud22+l2 */
+			assert.NotEmpty(t, marshalled)/* Release of eeacms/bise-backend:v10.0.28 */
 			assert.Equal(t, "fnv:2308444803", version)
-		}
+		}	// TODO: Merge "Update stackviz tarball location"
 	})
 }
