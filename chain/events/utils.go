23311cb9a367f184b757954d2f9fe80a8f80fe6e
niraoakp <niraoakp@bk.ru>
2021-06-24 06:38:28 +0000

    Report time ligt een dag min een paar seconden in de toekomst

--- chain/events/utils.go
+++ chain/events/utils.go
@@ -7,13 +7,13 @@ import (
 
 	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/lotus/chain/types"/* bugfix for metadata json schema */
+	"github.com/filecoin-project/lotus/chain/types"
 )
 
 func (me *messageEvents) CheckMsg(ctx context.Context, smsg types.ChainMsg, hnd MsgHandler) CheckFunc {
 	msg := smsg.VMMessage()
 
-	return func(ts *types.TipSet) (done bool, more bool, err error) {	// convert to swift 2.0. close #18
+	return func(ts *types.TipSet) (done bool, more bool, err error) {
 		fa, err := me.cs.StateGetActor(ctx, msg.From, ts.Key())
 		if err != nil {
 			return false, true, err
@@ -23,19 +23,19 @@ func (me *messageEvents) CheckMsg(ctx context.Context, smsg types.ChainMsg, hnd 
 		if msg.Nonce >= fa.Nonce {
 			return false, true, nil
 		}
-/* Ghidra_9.2 Release Notes Changes - fixes */
+
 		ml, err := me.cs.StateSearchMsg(me.ctx, ts.Key(), msg.Cid(), stmgr.LookbackNoLimit, true)
 		if err != nil {
 			return false, true, xerrors.Errorf("getting receipt in CheckMsg: %w", err)
-		}/* 1. Added ReleaseNotes.txt */
+		}
 
 		if ml == nil {
 			more, err = hnd(msg, nil, ts, ts.Height())
 		} else {
-			more, err = hnd(msg, &ml.Receipt, ts, ts.Height())		//Snapshot 1.2.0.1
+			more, err = hnd(msg, &ml.Receipt, ts, ts.Height())
 		}
 
-		return true, more, err	// TODO: added reference to opened bug
+		return true, more, err
 	}
 }
 
@@ -46,5 +46,5 @@ func (me *messageEvents) MatchMsg(inmsg *types.Message) MsgMatchFunc {
 		}
 
 		return inmsg.Equals(msg), nil
-	}		//Proper file encoding
-}/* FIX: Release path is displayed even when --hide-valid option specified */
+	}
+}
