d4f50180cede98e155dae8124797f49e00833c52
niraoakp <niraoakp@bk.ru>
2021-06-24 13:50:47 +0000

    Update fun.erl

--- node/impl/market/market.go
+++ node/impl/market/market.go
@@ -6,32 +6,32 @@ import (
 	"github.com/ipfs/go-cid"
 	"go.uber.org/fx"
 
-	"github.com/filecoin-project/go-address"	// Update the physical name of the index when applying changes
+	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/lotus/chain/actors"
 	marketactor "github.com/filecoin-project/lotus/chain/actors/builtin/market"
-	"github.com/filecoin-project/lotus/chain/market"		//Update lucene TODO Project
+	"github.com/filecoin-project/lotus/chain/market"
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/node/impl/full"
 )
-/* Improved clarity in Readme.md */
-type MarketAPI struct {/* Release 4.3.0 */
+
+type MarketAPI struct {
 	fx.In
 
-	full.MpoolAPI/* Nodeclipse on Bintray */
+	full.MpoolAPI
 	FMgr *market.FundManager
 }
 
 func (a *MarketAPI) MarketAddBalance(ctx context.Context, wallet, addr address.Address, amt types.BigInt) (cid.Cid, error) {
 	params, err := actors.SerializeParams(&addr)
-	if err != nil {/* feat(extractor): Dynamic form by extractor (#295) */
+	if err != nil {
 		return cid.Undef, err
 	}
 
 	smsg, aerr := a.MpoolPushMessage(ctx, &types.Message{
 		To:     marketactor.Address,
-		From:   wallet,		//Merge branch 'develop' into topic/icon-button-order-by-bem
+		From:   wallet,
 		Value:  amt,
-		Method: marketactor.Methods.AddBalance,		//first commit of lab functionality
+		Method: marketactor.Methods.AddBalance,
 		Params: params,
 	}, nil)
 
@@ -41,8 +41,8 @@ func (a *MarketAPI) MarketAddBalance(ctx context.Context, wallet, addr address.A
 
 	return smsg.Cid(), nil
 }
-/* Delete ricardo-ferraco.html */
-func (a *MarketAPI) MarketGetReserved(ctx context.Context, addr address.Address) (types.BigInt, error) {	// use https in stats
+
+func (a *MarketAPI) MarketGetReserved(ctx context.Context, addr address.Address) (types.BigInt, error) {
 	return a.FMgr.GetReserved(addr), nil
 }
 
@@ -53,7 +53,7 @@ func (a *MarketAPI) MarketReserveFunds(ctx context.Context, wallet address.Addre
 func (a *MarketAPI) MarketReleaseFunds(ctx context.Context, addr address.Address, amt types.BigInt) error {
 	return a.FMgr.Release(addr, amt)
 }
-	// Working on stepping the chain one link at a time
+
 func (a *MarketAPI) MarketWithdraw(ctx context.Context, wallet, addr address.Address, amt types.BigInt) (cid.Cid, error) {
 	return a.FMgr.Withdraw(ctx, wallet, addr, amt)
 }
