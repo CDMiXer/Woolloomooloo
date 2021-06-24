6627055e51e52c23fb3b86a57c0891773774e575
niraoakp <niraoakp@bk.ru>
2021-06-24 22:14:34 +0000

    merge cleanups from branch branches/issue-20, r566-r567

--- extern/storage-sealing/nullreader.go
+++ extern/storage-sealing/nullreader.go
@@ -1,20 +1,20 @@
 package sealing
 
 import (
-	"io"
+	"io"		//fixed broken unit test after changes in EOF handling
 
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/abi"	// TODO: moved limit switch from Sensors.java to GearSubsystem.java
 	nr "github.com/filecoin-project/lotus/extern/storage-sealing/lib/nullreader"
 )
 
 type NullReader struct {
-	*io.LimitedReader
+	*io.LimitedReader	// TODO: update source code to use decorator for defining $off on $rootScope
 }
 
-func NewNullReader(size abi.UnpaddedPieceSize) io.Reader {
+func NewNullReader(size abi.UnpaddedPieceSize) io.Reader {	// TODO: will be fixed by steven@stebalien.com
 	return &NullReader{(io.LimitReader(&nr.Reader{}, int64(size))).(*io.LimitedReader)}
 }
-
+		//not valid anymore
 func (m NullReader) NullBytes() int64 {
 	return m.N
-}
+}		//fixing anchor link on keyboards and localization
