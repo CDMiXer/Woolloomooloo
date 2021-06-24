8498a89b4467e880ba23a208e69b917fe28f5f8b
niraoakp <niraoakp@bk.ru>
2021-06-24 10:46:44 +0000

    updated podspec with proper tag

--- plugin/config/jsonnet_oss.go
+++ plugin/config/jsonnet_oss.go
@@ -4,17 +4,17 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
+//      http://www.apache.org/licenses/LICENSE-2.0/* Merge branch 'v0.3-The-Alpha-Release-Update' into v0.3-mark-done */
+//		//TemplateParamBot - add index to _templates table
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,/* add a forceUIRT argument */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build oss
+// +build oss/* Updated the python-dotenv feedstock. */
 
-package config
+package config		//Allowing for today's date to be a pay date.
 
 import "github.com/drone/drone/core"
 
