5a39ebaff6a5342fbc2ab196c7b1346f07e97a64
niraoakp <niraoakp@bk.ru>
2021-06-24 00:00:03 +0000

    GROOVY-7996: correct metadata for accessed variable and property owner

--- tests/examples/compat/v0.10.0/minimal/index.ts
+++ tests/examples/compat/v0.10.0/minimal/index.ts
@@ -1,8 +1,8 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// TODO: will be fixed by nicksavers@gmail.com
-
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+/* Added default arguments */
 import { Config } from "@pulumi/pulumi";
 
 let config = new Config("minimal");
-console.log(`Hello, ${config.require("name")}!`);
+console.log(`Hello, ${config.require("name")}!`);/* Release 0.20.0. */
 console.log(`Psst, ${config.require("secret")}`);
-/* Add script for Sylvok Replica */
+
