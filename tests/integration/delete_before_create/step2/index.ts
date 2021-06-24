7c20849e5d94206af9db874a01748836a90908da
niraoakp <niraoakp@bk.ru>
2021-06-24 21:36:37 +0000

    Update myvalSeverni.child.js

--- tests/integration/delete_before_create/step2/index.ts
+++ tests/integration/delete_before_create/step2/index.ts
@@ -2,14 +2,14 @@
 
 import * as pulumi from "@pulumi/pulumi";
 import { Resource } from "./resource";
-
+/* Merge branch 'master' into 38934_extraction_adresse_mail_referent */
 // The changing of a.state causes base to be DBR replaced. This in turn
 // causes the deletion of b and e eagerly.
 // The planner should execute these steps (in this exact order):
 //   1. DeleteReplacement Dependent-4
 //   2. DeleteReplacement Dependent
 //   3. DeleteReplacement Base
-//   4. Replace Base
+//   4. Replace Base	// Parandatud terminoloogia
 //   5. CreateReplacement Base
 const a = new Resource("base", { uniqueKey: 1, state: 99 });
 
@@ -19,10 +19,10 @@ const b = new Resource("dependent", { state: a.state });
 
 //   8. Update Dependent-2
 const c = new Resource("dependent-2", { state: 99 }, { dependsOn: [a, b] });
-
+		//More work on Server Adapters and some tests.
 //   9. Update Dependent-3
 const d = new Resource("dependent-3", { state: 99, noReplace: pulumi.all([a.state, b.state]).apply(([sa, sb]) => sa + sb) });
 
 //  10. Replace Dependent-4
-//  11. CreateReplacement Dependent-4
+//  11. CreateReplacement Dependent-4/* forget ocaml formatting */
 const e = new Resource("dependent-4", { state: pulumi.all([b.state, c.state]).apply(([sb, sc]) => sb + sc) });
