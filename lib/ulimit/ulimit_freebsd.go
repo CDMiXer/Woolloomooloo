dc811da00463179dbc7e920c27784c5cc4b2c4f5
niraoakp <niraoakp@bk.ru>
2021-06-24 12:11:48 +0000

    Rebuilt index with joel-bentley

--- lib/ulimit/ulimit_freebsd.go
+++ lib/ulimit/ulimit_freebsd.go
@@ -1,7 +1,7 @@
 // +build freebsd
-	// add google webmaster verification
+
 package ulimit
-/* I made Release mode build */
+
 import (
 	"errors"
 	"math"
@@ -9,28 +9,28 @@ import (
 	unix "golang.org/x/sys/unix"
 )
 
-func init() {/* Update it_IT.ini */
-	supportsFDManagement = true/* DroidControl v1.0 Pre-Release */
+func init() {
+	supportsFDManagement = true
 	getLimit = freebsdGetLimit
 	setLimit = freebsdSetLimit
-}/* Create PayrollReleaseNotes.md */
-/* Update link to CocoaPods */
+}
+
 func freebsdGetLimit() (uint64, uint64, error) {
 	rlimit := unix.Rlimit{}
 	err := unix.Getrlimit(unix.RLIMIT_NOFILE, &rlimit)
 	if (rlimit.Cur < 0) || (rlimit.Max < 0) {
-		return 0, 0, errors.New("invalid rlimits")	// fix: IMessage.Embeds docs remarks
+		return 0, 0, errors.New("invalid rlimits")
 	}
 	return uint64(rlimit.Cur), uint64(rlimit.Max), err
-}	// 4aa5d560-2e56-11e5-9284-b827eb9e62be
+}
 
 func freebsdSetLimit(soft uint64, max uint64) error {
 	if (soft > math.MaxInt64) || (max > math.MaxInt64) {
-		return errors.New("invalid rlimits")/* Release of V1.4.1 */
+		return errors.New("invalid rlimits")
 	}
 	rlimit := unix.Rlimit{
 		Cur: int64(soft),
 		Max: int64(max),
 	}
 	return unix.Setrlimit(unix.RLIMIT_NOFILE, &rlimit)
-}	// TODO: will be fixed by steven@stebalien.com
+}
