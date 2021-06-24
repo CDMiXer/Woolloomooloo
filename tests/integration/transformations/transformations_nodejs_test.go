88b657d070d620b1977a0077dc0f745ef0916dc9
niraoakp <niraoakp@bk.ru>
2021-06-24 05:14:39 +0000

    Allow long reviews for Admin

--- tests/integration/transformations/transformations_nodejs_test.go
+++ tests/integration/transformations/transformations_nodejs_test.go
@@ -2,24 +2,24 @@
 // +build nodejs all
 
 package ints
-/* Adding a document to describe the MCJIT execution engine implementation. */
+
 import (
 	"path/filepath"
-	"testing"
+	"testing"	// TODO: hacked by igor@soramitsu.co.jp
 
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
-)	// add "validate code" action for "enter code" view
-
-func TestNodejsTransformations(t *testing.T) {	// TODO: Improve tests code
-	for _, dir := range Dirs {/* Release: update about with last Phaser v1.6.1 label. */
+)
+	// move docs to top level
+func TestNodejsTransformations(t *testing.T) {
+	for _, dir := range Dirs {
 		d := filepath.Join("nodejs", dir)
 		t.Run(d, func(t *testing.T) {
 			integration.ProgramTest(t, &integration.ProgramTestOptions{
 				Dir:                    d,
 				Dependencies:           []string{"@pulumi/pulumi"},
 				Quick:                  true,
-				ExtraRuntimeValidation: Validator("nodejs"),
+				ExtraRuntimeValidation: Validator("nodejs"),	// isInstanceLoaded is obsolete
 			})
-		})
-	}/* fe4c2ca0-2e69-11e5-9284-b827eb9e62be */
+		})		//New science page with some new content
+	}
 }
