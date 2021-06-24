1c81669fd141cb7a6b33efab5c8e41921c72282a
niraoakp <niraoakp@bk.ru>
2021-06-24 16:50:35 +0000

    just teasing me now

--- operator/manager/rpc/server_test.go
+++ operator/manager/rpc/server_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.		//new behavior language
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.		//Updated the plotly-ecdf feedstock.
-
+// that can be found in the LICENSE file.
+		//Updated icons and fixed some lint warnings
 // +build !oss
 
 package rpc
