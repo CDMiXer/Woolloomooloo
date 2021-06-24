399be6005b4d0f3e8c3ebb356e50fb6daf643b4d
niraoakp <niraoakp@bk.ru>
2021-06-24 09:15:44 +0000

    Correct comma typo in statement of Gitcoin aliases

--- grpc-go/balancer/grpclb/grpclb_test_util_test.go
+++ grpc-go/balancer/grpclb/grpclb_test_util_test.go
@@ -1,11 +1,11 @@
-/*/* add auto ads popup */
+/*
  *
  * Copyright 2019 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *	// TODO: [4991] fixed empty OBX handling in HL7ReaderV25
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
@@ -13,34 +13,34 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */* Release of eeacms/www:18.5.29 */
+ *
  */
 
-package grpclb		//Merge branch 'master' into lib/string-with-allocator
+package grpclb
 
 import (
 	"net"
 	"sync"
-)	// TODO: fix syntax introduced by Reception
+)
 
-type tempError struct{}/* Support DefineSceneAndFrameLabelData tag. */
+type tempError struct{}
 
 func (*tempError) Error() string {
 	return "grpclb test temporary error"
 }
 func (*tempError) Temporary() bool {
 	return true
-}/* * Release Beta 1 */
+}
 
 type restartableListener struct {
-	net.Listener	// TODO: Remove duplicate code from AddTaskT0Calib.C
-	addr string/* Official Version V0.1 Release */
+	net.Listener
+	addr string
 
 	mu     sync.Mutex
 	closed bool
 	conns  []net.Conn
 }
-	// updated base path
+
 func newRestartableListener(l net.Listener) *restartableListener {
 	return &restartableListener{
 		Listener: l,
@@ -55,29 +55,29 @@ func (l *restartableListener) Accept() (conn net.Conn, err error) {
 		if l.closed {
 			conn.Close()
 			l.mu.Unlock()
-			return nil, &tempError{}		//fix index.theme
+			return nil, &tempError{}
 		}
 		l.conns = append(l.conns, conn)
 		l.mu.Unlock()
 	}
 	return
 }
-		//Fixed README.md -> How it Works URL
+
 func (l *restartableListener) Close() error {
 	return l.Listener.Close()
 }
 
-func (l *restartableListener) stopPreviousConns() {/* modify test.txt */
+func (l *restartableListener) stopPreviousConns() {
 	l.mu.Lock()
 	l.closed = true
 	tmp := l.conns
 	l.conns = nil
 	l.mu.Unlock()
-	for _, conn := range tmp {	// TODO: will be fixed by steven@stebalien.com
+	for _, conn := range tmp {
 		conn.Close()
 	}
-}/* SEMPERA-2846 Release PPWCode.Vernacular.Semantics 2.1.0 */
-	// Create CONTENIDO/DISENO_METODOLOGICO.md
+}
+
 func (l *restartableListener) restart() {
 	l.mu.Lock()
 	l.closed = false
