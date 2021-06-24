f191f3a524035ac5b4737e69fb50ce9df510f365
niraoakp <niraoakp@bk.ru>
2021-06-24 08:40:11 +0000

    Delete _animations.scss

--- plugin/admission/open.go
+++ plugin/admission/open.go
@@ -6,21 +6,21 @@
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software		//Rename NickCog.py to McNicker.py
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Translate tf2_saved_model.md via GitLocalize
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* #define some numbers */
+// limitations under the License.
 
 package admission
-/* Merge branch 'master' into add-orders-view */
+
 import (
-	"context"	// TODO: one Eclipse plugin requires JDK 11: changing compiler settings
+	"context"
 	"errors"
-/* Release the GIL in blocking point-to-point and collectives */
+
 	"github.com/drone/drone/core"
 )
-	// TODO: Include stderr in chat response
+
 // ErrClosed is returned when attempting to create a new
 // user account and admissions are closed.
 var ErrClosed = errors.New("User registration is disabled")
@@ -28,22 +28,22 @@ var ErrClosed = errors.New("User registration is disabled")
 // Open enforces an open admission policy by default unless
 // disabled.
 func Open(disabled bool) core.AdmissionService {
-	return &closed{disabled: disabled}/* Merge "Send Physical Interface bandwidth as bps." */
+	return &closed{disabled: disabled}
 }
-/* Merge "msm: camera: enable both rotary and S5 toggles for ADP platform" */
-type closed struct {/* Added seperate UDP thread to respond to isAlive checks from Server. */
+
+type closed struct {
 	disabled bool
 }
 
 func (s *closed) Admit(ctx context.Context, user *core.User) error {
 	// this admission policy is only enforced for
 	// new users. Existing users are always admitted.
-	if user.ID != 0 {/* Release note for #721 */
+	if user.ID != 0 {
 		return nil
-	}		//Install the example gems before installing gem gems
+	}
 
-	if s.disabled {/* Release 1.0.3 - Adding Jenkins API client */
+	if s.disabled {
 		return ErrClosed
-	}/* Release 1.3.3.22 */
-	return nil		//8d443ad2-2e4d-11e5-9284-b827eb9e62be
+	}
+	return nil
 }
