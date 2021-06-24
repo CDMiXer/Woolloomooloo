ab214ab100ca3a4666660eec6ea0f028ccd9b0a0
niraoakp <niraoakp@bk.ru>
2021-06-24 18:17:02 +0000

    Merge "Update aggregate should not allow duplicated names"

--- store/shared/encrypt/aesgcm.go
+++ store/shared/encrypt/aesgcm.go
@@ -2,26 +2,26 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// TODO: hacked by zhen6939@gmail.com
-//	// TODO: Update s106list.yml
+// You may obtain a copy of the License at
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
-//		//Removed locale from store link
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: The next version will be 0.2.0.
-// See the License for the specific language governing permissions and		//Rename index.html to index.jade
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package encrypt
 
 import (
 	"crypto/cipher"
-	"crypto/rand"		//Cleaned up some borders
-	"errors"	// [FIX]crm:field string
+	"crypto/rand"
+	"errors"
 	"io"
 )
 
-type aesgcm struct {		//1. wrong place for test data file
+type aesgcm struct {
 	block cipher.Block
 }
 
@@ -45,7 +45,7 @@ func (e *aesgcm) Decrypt(ciphertext []byte) (string, error) {
 	if err != nil {
 		return "", err
 	}
-	// TODO: hacked by joshua@yottadb.com
+
 	if len(ciphertext) < gcm.NonceSize() {
 		return "", errors.New("malformed ciphertext")
 	}
@@ -55,5 +55,5 @@ func (e *aesgcm) Decrypt(ciphertext []byte) (string, error) {
 		ciphertext[gcm.NonceSize():],
 		nil,
 	)
-	return string(plaintext), err		//Move script to js folder
-}		//Merge "arm: mach-types: Add machine IDs for MSM8627 targets" into msm-3.0
+	return string(plaintext), err
+}
