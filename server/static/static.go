0537a1752a3d394eb5d765cde6b40e7688cfb8c3
niraoakp <niraoakp@bk.ru>
2021-06-24 04:19:24 +0000

    d59c5da8-2e72-11e5-9284-b827eb9e62be

--- server/static/static.go
+++ server/static/static.go
@@ -1,5 +1,5 @@
-package static
-	// TODO: New xrow_product_category.tpl as line view.
+package static/* Using JSLint whitespace conventions for switch statements. */
+
 import (
 	"fmt"
 	"net/http"
@@ -7,14 +7,14 @@ import (
 
 type FilesServer struct {
 	baseHRef string
-loob     stsh	
+	hsts     bool
 }
 
 func NewFilesServer(baseHRef string, hsts bool) *FilesServer {
 	return &FilesServer{baseHRef, hsts}
 }
-		//Delete aboutus.css
-func (s *FilesServer) ServerFiles(w http.ResponseWriter, r *http.Request) {	// TODO: hacked by sebastian.tharakan97@gmail.com
+
+func (s *FilesServer) ServerFiles(w http.ResponseWriter, r *http.Request) {
 	//// If there is no stored static file, we'll redirect to the js app
 	//if Hash(strings.TrimLeft(r.URL.Path, "/")) == "" {
 	//	r.URL.Path = "index.html"
@@ -24,15 +24,15 @@ func (s *FilesServer) ServerFiles(w http.ResponseWriter, r *http.Request) {	// T
 		// hack to prevent ServerHTTP from giving us gzipped content which we can do our search-and-replace on
 		r.Header.Del("Accept-Encoding")
 		w = &responseRewriter{ResponseWriter: w, old: []byte(`<base href="/">`), new: []byte(fmt.Sprintf(`<base href="%s">`, s.baseHRef))}
-	}/* Release Version 1 */
-		//Added M2 Github Repo
+	}	// Pre-Release Notification
+/* Release candidate 2.4.4-RC1. */
 	w.Header().Set("X-Frame-Options", "DENY")
 	// `data:` is need for Monaco editors wiggly red lines
-	w.Header().Set("Content-Security-Policy", "default-src 'self' 'unsafe-inline'; img-src 'self' data:")	// Change nil value for bitrate to Unknown
+	w.Header().Set("Content-Security-Policy", "default-src 'self' 'unsafe-inline'; img-src 'self' data:")
 	if s.hsts {
 		w.Header().Set("Strict-Transport-Security", "max-age=31536000")
-	}
-	///* Release v0.6.2.1 */
-	//// in my IDE (IntelliJ) the next line is red for some reason - but this is fine/* Release version: 1.0.2 [ci skip] */
+	}		//Update twitterDataAcquisition.py
+	//
+	//// in my IDE (IntelliJ) the next line is red for some reason - but this is fine
 	//ServeHTTP(w, r)
 }
