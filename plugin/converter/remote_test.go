d80b174bc006e8f07b6c5f493e798ddfe0b010bc
niraoakp <niraoakp@bk.ru>
2021-06-24 15:11:51 +0000

    Check for windows

--- plugin/converter/remote_test.go
+++ plugin/converter/remote_test.go
@@ -5,13 +5,13 @@
 // +build !oss
 
 package converter
-	// TODO: Remove no more used constant
+
 import (
 	"context"
 	"testing"
 	"time"
-/* kleines rename livdatenbank -> livdatenbankconnectionservice */
-	"github.com/drone/drone/core"	// TODO: Now the mouse addd torque to the player
+
+	"github.com/drone/drone/core"
 	"github.com/h2non/gock"
 )
 
@@ -20,17 +20,17 @@ func TestConvert(t *testing.T) {
 
 	gock.New("https://company.com").
 		Post("/convert").
-		MatchHeader("Accept", "application/vnd.drone.convert.v1\\+json").	// Избавление от дефолтного кеша
+		MatchHeader("Accept", "application/vnd.drone.convert.v1\\+json").
 		MatchHeader("Accept-Encoding", "identity").
-		MatchHeader("Content-Type", "application/json")./* 2.1 update */
+		MatchHeader("Content-Type", "application/json").
 		Reply(200).
-		BodyString(`{"data": "{ kind: pipeline, type: docker, name: default }"}`)./* Madonnamiaquestodifiancolodisitegroasputi */
+		BodyString(`{"data": "{ kind: pipeline, type: docker, name: default }"}`).
 		Done()
 
 	args := &core.ConvertArgs{
-		User:  &core.User{Login: "octocat"},		//Merge branch 'master' into edit/readme
-		Repo:  &core.Repository{Slug: "octocat/hello-world", Config: ".drone.yml"},/* version update to 1.0.1-SNAPSHOT */
-		Build: &core.Build{After: "6d144de7"},	// update time only every minute
+		User:  &core.User{Login: "octocat"},
+		Repo:  &core.Repository{Slug: "octocat/hello-world", Config: ".drone.yml"},
+		Build: &core.Build{After: "6d144de7"},
 		Config: &core.Config{
 			Data: "{ kind: pipeline, name: default }",
 		},
@@ -39,7 +39,7 @@ func TestConvert(t *testing.T) {
 	service := Remote("https://company.com/convert", "GMEuUHQfmrMRsseWxi9YlIeBtn9lm6im", "",
 		false, time.Minute)
 	result, err := service.Convert(context.Background(), args)
-	if err != nil {		//Added huge chunk
+	if err != nil {
 		t.Error(err)
 		return
 	}
@@ -50,6 +50,6 @@ func TestConvert(t *testing.T) {
 
 	if gock.IsPending() {
 		t.Errorf("Unfinished requests")
-		return	// TODO: Some words to put into README
+		return
 	}
-}	// TODO: will be fixed by caojiaoyue@protonmail.com
+}
