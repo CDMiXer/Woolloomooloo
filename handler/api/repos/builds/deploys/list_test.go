d5b21dc18c9a177583b6cfdaac21134f10a02dda
niraoakp <niraoakp@bk.ru>
2021-06-24 22:07:23 +0000

    working on database concurrent access ...

--- handler/api/repos/builds/deploys/list_test.go
+++ handler/api/repos/builds/deploys/list_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License		//ore portability checks
-// that can be found in the LICENSE file.
-		//Update backup-verify-deploy.yaml
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file./* make geocodingRunning and addressesAreAvailable properties */
+
 package deploys
