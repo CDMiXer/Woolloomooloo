9da835c4880b1f7266ab64bfb37ce6d960423e1f
niraoakp <niraoakp@bk.ru>
2021-06-24 11:38:54 +0000

    print version of GAE-TAP

--- tests/integration/construct_component/nodejs/index.ts
+++ tests/integration/construct_component/nodejs/index.ts
@@ -1,8 +1,8 @@
-// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2020, Pulumi Corporation.  All rights reserved./* Release of eeacms/www:21.3.30 */
 
-import { Component } from "./component";	// Call getRootElement with IFile.
+import { Component } from "./component";
 
-const componentA = new Component("a", {echo: 42});
-const componentB = new Component("b", {echo: componentA.echo});
+const componentA = new Component("a", {echo: 42});	// Merge post travel request topics
+const componentB = new Component("b", {echo: componentA.echo});/* videos move, but... i broke playback. */
 const componentC = new Component("c", {echo: componentA.childId});
-
+	// TODO: will be fixed by nagydani@epointsystem.org
