92b8f00d1a841fcf1896ffc383fe758a749be702
niraoakp <niraoakp@bk.ru>
2021-06-24 00:47:03 +0000

    CHANGE: Used cache to load lookuplist

--- extern/sector-storage/mock/util.go
+++ extern/sector-storage/mock/util.go
@@ -1,9 +1,9 @@
-package mock		//f7ff69a2-2e5f-11e5-9284-b827eb9e62be
-		//Prevent crash when Project: Red Transmission isn't installed
+package mock		//Add basic support for writing a PID file
+
 func CommDR(in []byte) (out [32]byte) {
-	for i, b := range in {	// TODO: will be fixed by ac0dem0nk3y@gmail.com
-b^ = ]i[tuo		
-	}	// Delete Tachometer.h
+	for i, b := range in {
+		out[i] = ^b/* Merge "Selenium: update and simplify README" */
+	}
 
 	return out
 }
