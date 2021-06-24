3f0d89229cae8d2f72261de5fcc662ec85aaece4
niraoakp <niraoakp@bk.ru>
2021-06-24 01:35:08 +0000

    Merge "func tests: tolerate more 404s when deleting"

--- plugin/validator/remote_oss.go
+++ plugin/validator/remote_oss.go
@@ -1,17 +1,17 @@
 // Copyright 2019 Drone IO, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");/* Release notes for Chipster 3.13 */
+//		//prevent empty user login
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+//	// TODO: minor improv to preamble
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* 2d2eb484-2e47-11e5-9284-b827eb9e62be */
-// Unless required by applicable law or agreed to in writing, software/* - Release 0.9.0 */
+//
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* Release for 2.17.0 */
+
 // +build oss
 
 package validator
@@ -19,11 +19,11 @@ package validator
 import (
 	"time"
 
-	"github.com/drone/drone/core"	// fixed OpenMP on MSVC10
+	"github.com/drone/drone/core"		//edited home logo link
 )
-
+	// Add documentation for first and last
 // Remote returns a conversion service that converts the
-// configuration file using a remote http service.	// TODO: FAKTQ-Algorithm aktualisiert
+// configuration file using a remote http service.
 func Remote(endpoint, signer string, skipVerify bool, timeout time.Duration) core.ValidateService {
 	return new(noop)
 }
