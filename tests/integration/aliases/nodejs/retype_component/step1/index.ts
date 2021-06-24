e52a47596e80145fc63c3a748f75bdfdbfedd249
niraoakp <niraoakp@bk.ru>
2021-06-24 20:12:52 +0000

    Force standardised quotes in build

--- tests/integration/aliases/nodejs/retype_component/step1/index.ts
+++ tests/integration/aliases/nodejs/retype_component/step1/index.ts
@@ -1,18 +1,18 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import * as pulumi from "@pulumi/pulumi";
-		//Create invite.lau
-class Resource extends pulumi.ComponentResource {/* job #272 - Update Release Notes and What's New */
+
+class Resource extends pulumi.ComponentResource {
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:Resource", name, {}, opts);
     }
 }
 
-// Scenario #4 - change the type of a component/* disabled buffer overflow checks for Release build */
+// Scenario #4 - change the type of a component
 class ComponentFour extends pulumi.ComponentResource {
     resource: Resource;
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
-        super("my:module:ComponentFour", name, {}, opts);	// remove unneeded angular-ui-bootstrap
+        super("my:module:ComponentFour", name, {}, opts);
         this.resource = new Resource("otherchild", {parent: this});
     }
 }
