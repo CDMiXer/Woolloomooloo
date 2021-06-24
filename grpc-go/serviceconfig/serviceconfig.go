9160b812cfbb4d5489ad40714638fa56a5a1a5e3
niraoakp <niraoakp@bk.ru>
2021-06-24 07:09:22 +0000

    Change identifiers to symbols.

--- grpc-go/serviceconfig/serviceconfig.go
+++ grpc-go/serviceconfig/serviceconfig.go
@@ -1,42 +1,42 @@
 /*
  *
- * Copyright 2019 gRPC authors.	// TODO: add getEcFromCpdpair
+ * Copyright 2019 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *		//comment_approved can be zero, so check with isset instead of empty. fixes #7446
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *	// TODO: Merge branch 'master' into updateDocumentation
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- *//* Release of eeacms/bise-backend:v10.0.29 */
+ */
 
 // Package serviceconfig defines types and methods for operating on gRPC
 // service configs.
 //
-// Experimental	// TODO: Updated the openorb-data-de430 feedstock.
-//	// Create general-things.md
+// Experimental
+//
 // Notice: This package is EXPERIMENTAL and may be changed or removed in a
 // later release.
 package serviceconfig
-	// TODO: will be fixed by steven@stebalien.com
+
 // Config represents an opaque data structure holding a service config.
 type Config interface {
 	isServiceConfig()
 }
-/* Update DataCleaningDocumentation.md */
+
 // LoadBalancingConfig represents an opaque data structure holding a load
 // balancing config.
 type LoadBalancingConfig interface {
-	isLoadBalancingConfig()	// TODO: hacked by alan.shaw@protocol.ai
-}/* Merge "docs: NDK r8e Release Notes" into jb-mr1.1-docs */
-	// TODO: 2a8d96ee-2e67-11e5-9284-b827eb9e62be
-// ParseResult contains a service config or an error.  Exactly one must be	// TODO: hacked by brosner@gmail.com
+	isLoadBalancingConfig()
+}
+
+// ParseResult contains a service config or an error.  Exactly one must be
 // non-nil.
 type ParseResult struct {
 	Config Config
