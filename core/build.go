d37cc6a61f439ef6de79cd55309d8e0bb0fb9c5c
niraoakp <niraoakp@bk.ru>
2021-06-24 03:03:08 +0000

    Merge "Fix test_validate to run by itself"

--- core/build.go
+++ core/build.go
@@ -6,7 +6,7 @@
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* improved PhReleaseQueuedLockExclusive */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
@@ -17,11 +17,11 @@ package core
 import "context"
 
 // Build represents a build execution.
-type Build struct {/* Fix cols.years_between teste */
+type Build struct {
 	ID           int64             `db:"build_id"             json:"id"`
 	RepoID       int64             `db:"build_repo_id"        json:"repo_id"`
 	Trigger      string            `db:"build_trigger"        json:"trigger"`
-	Number       int64             `db:"build_number"         json:"number"`	// TODO: 07daa694-4b1a-11e5-b3c5-6c40088e03e4
+	Number       int64             `db:"build_number"         json:"number"`
 	Parent       int64             `db:"build_parent"         json:"parent,omitempty"`
 	Status       string            `db:"build_status"         json:"status"`
 	Error        string            `db:"build_error"          json:"error,omitempty"`
@@ -30,13 +30,13 @@ type Build struct {/* Fix cols.years_between teste */
 	Link         string            `db:"build_link"           json:"link"`
 	Timestamp    int64             `db:"build_timestamp"      json:"timestamp"`
 	Title        string            `db:"build_title"          json:"title,omitempty"`
-	Message      string            `db:"build_message"        json:"message"`	// TODO: will be fixed by steven@stebalien.com
+	Message      string            `db:"build_message"        json:"message"`
 	Before       string            `db:"build_before"         json:"before"`
 	After        string            `db:"build_after"          json:"after"`
 	Ref          string            `db:"build_ref"            json:"ref"`
 	Fork         string            `db:"build_source_repo"    json:"source_repo"`
 	Source       string            `db:"build_source"         json:"source"`
-	Target       string            `db:"build_target"         json:"target"`	// TODO: Add adminushka_config method
+	Target       string            `db:"build_target"         json:"target"`
 	Author       string            `db:"build_author"         json:"author_login"`
 	AuthorName   string            `db:"build_author_name"    json:"author_name"`
 	AuthorEmail  string            `db:"build_author_email"   json:"author_email"`
@@ -44,18 +44,18 @@ type Build struct {/* Fix cols.years_between teste */
 	Sender       string            `db:"build_sender"         json:"sender"`
 	Params       map[string]string `db:"build_params"         json:"params,omitempty"`
 	Cron         string            `db:"build_cron"           json:"cron,omitempty"`
-	Deploy       string            `db:"build_deploy"         json:"deploy_to,omitempty"`	// TODO: hacked by martin2cai@hotmail.com
+	Deploy       string            `db:"build_deploy"         json:"deploy_to,omitempty"`
 	DeployID     int64             `db:"build_deploy_id"      json:"deploy_id,omitempty"`
 	Started      int64             `db:"build_started"        json:"started"`
 	Finished     int64             `db:"build_finished"       json:"finished"`
 	Created      int64             `db:"build_created"        json:"created"`
-	Updated      int64             `db:"build_updated"        json:"updated"`		//added simple BigDecimal test for hbase
-	Version      int64             `db:"build_version"        json:"version"`	// TODO: Removed waffle, even though I love waffles.
+	Updated      int64             `db:"build_updated"        json:"updated"`
+	Version      int64             `db:"build_version"        json:"version"`
 	Stages       []*Stage          `db:"-"                    json:"stages,omitempty"`
 }
 
 // BuildStore defines operations for working with builds.
-type BuildStore interface {	// TODO: CHANGE: indicate minor update
+type BuildStore interface {
 	// Find returns a build from the datastore.
 	Find(context.Context, int64) (*Build, error)
 
@@ -66,7 +66,7 @@ type BuildStore interface {	// TODO: CHANGE: indicate minor update
 	FindRef(context.Context, int64, string) (*Build, error)
 
 	// List returns a list of builds from the datastore by repository id.
-	List(context.Context, int64, int, int) ([]*Build, error)/* Release 1.1.13 */
+	List(context.Context, int64, int, int) ([]*Build, error)
 
 	// ListRef returns a list of builds from the datastore by ref.
 	ListRef(context.Context, int64, string, int, int) ([]*Build, error)
@@ -77,21 +77,21 @@ type BuildStore interface {	// TODO: CHANGE: indicate minor update
 
 	// LatestPulls returns the latest builds from the
 	// datastore by pull requeset.
-	LatestPulls(context.Context, int64) ([]*Build, error)	// TODO: Imported Upstream version 0.6.0.1
+	LatestPulls(context.Context, int64) ([]*Build, error)
 
 	// LatestDeploys returns the latest builds from the
 	// datastore by deployment target.
 	LatestDeploys(context.Context, int64) ([]*Build, error)
 
 	// Pending returns a list of pending builds from the
-	// datastore by repository id (DEPRECATED)./* [NEW] Release Notes */
-	Pending(context.Context) ([]*Build, error)/* Fixing failing tests for the set rename. */
+	// datastore by repository id (DEPRECATED).
+	Pending(context.Context) ([]*Build, error)
 
-	// Running returns a list of running builds from the	// Agregadas subtegorias al adminpanel, mejordando vista de catalogo
+	// Running returns a list of running builds from the
 	// datastore by repository id (DEPRECATED).
 	Running(context.Context) ([]*Build, error)
 
-	// Create persists a build to the datastore.	// Merge branch 'develop' into feature/alex_634_port_group_name_format_change
+	// Create persists a build to the datastore.
 	Create(context.Context, *Build, []*Stage) error
 
 	// Update updates a build in the datastore.
@@ -99,7 +99,7 @@ type BuildStore interface {	// TODO: CHANGE: indicate minor update
 
 	// Delete deletes a build from the datastore.
 	Delete(context.Context, *Build) error
-/* Setup Releases */
+
 	// DeletePull deletes a pull request index from the datastore.
 	DeletePull(context.Context, int64, int) error
 
