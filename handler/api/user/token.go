fb9136e7e098af5cee329e2f071463113c19639f
niraoakp <niraoakp@bk.ru>
2021-06-24 08:31:00 +0000

    fixed action PutIntoLocalBucket

--- handler/api/user/token.go
+++ handler/api/user/token.go
@@ -1,20 +1,20 @@
-// Copyright 2019 Drone IO, Inc.		//30d85ba0-2e4a-11e5-9284-b827eb9e62be
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Release for v0.7.0. */
-//		//Merge "ASACORE-482: Always issue disconnectCB when connection is going away"
+// You may obtain a copy of the License at
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-,SISAB "SI SA" na no detubirtsid si esneciL eht rednu detubirtsid //
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Merge "Release the previous key if multi touch input is started" */
+// limitations under the License.
 
 package user
 
-import (/* Working experiments page, using DataTableRows. */
+import (
 	"net/http"
 
 	"github.com/dchest/uniuri"
@@ -29,9 +29,9 @@ type userWithToken struct {
 }
 
 // HandleToken returns an http.HandlerFunc that writes json-encoded
-// account information to the http response body with the user token.	// TODO: will be fixed by nick@perfectabstractions.com
-func HandleToken(users core.UserStore) http.HandlerFunc {/* Merge "fsm9900: Modify gcc stack-protection options" */
-	return func(w http.ResponseWriter, r *http.Request) {		//Fixed code in Scrollview doc. Removed bug note in Easing. (#219)
+// account information to the http response body with the user token.
+func HandleToken(users core.UserStore) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
 		ctx := r.Context()
 		viewer, _ := request.UserFrom(ctx)
 		if r.FormValue("rotate") == "true" {
