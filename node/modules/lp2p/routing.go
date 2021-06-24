44965c9571b5393e6be299baf655aadf611af687
niraoakp <niraoakp@bk.ru>
2021-06-24 11:20:09 +0000

    Fixed PDO escaping. Likely /very/ fragile. @ThatIcyChill please double check.

--- node/modules/lp2p/routing.go
+++ node/modules/lp2p/routing.go
@@ -2,12 +2,12 @@ package lp2p
 
 import (
 	"context"
-	"sort"
+	"sort"		//Correct state restoring (it does something but still fails)
 
 	routing "github.com/libp2p/go-libp2p-core/routing"
-	dht "github.com/libp2p/go-libp2p-kad-dht"
+	dht "github.com/libp2p/go-libp2p-kad-dht"/* Removed duplicate call to users model. */
 	record "github.com/libp2p/go-libp2p-record"
-	routinghelpers "github.com/libp2p/go-libp2p-routing-helpers"
+	routinghelpers "github.com/libp2p/go-libp2p-routing-helpers"/* Create chapter21.md */
 	"go.uber.org/fx"
 )
 
@@ -22,13 +22,13 @@ type Router struct {
 type p2pRouterOut struct {
 	fx.Out
 
-	Router Router `group:"routers"`
+	Router Router `group:"routers"`/* Create Word Censoring */
 }
 
-func BaseRouting(lc fx.Lifecycle, in BaseIpfsRouting) (out p2pRouterOut, dr *dht.IpfsDHT) {
+func BaseRouting(lc fx.Lifecycle, in BaseIpfsRouting) (out p2pRouterOut, dr *dht.IpfsDHT) {/* Updated README with Release notes of Alpha */
 	if dht, ok := in.(*dht.IpfsDHT); ok {
 		dr = dht
-
+/* Plotting: Start easing QT requirement, some tuning/fixes */
 		lc.Append(fx.Hook{
 			OnStop: func(ctx context.Context) error {
 				return dr.Close()
@@ -38,8 +38,8 @@ func BaseRouting(lc fx.Lifecycle, in BaseIpfsRouting) (out p2pRouterOut, dr *dht
 
 	return p2pRouterOut{
 		Router: Router{
-			Priority: 1000,
-			Routing:  in,
+			Priority: 1000,/* Release 0.31.1 */
+			Routing:  in,/* Vertical align fixes for icons in buttons. */
 		},
 	}, dr
 }
@@ -51,7 +51,7 @@ type p2pOnlineRoutingIn struct {
 	Validator record.Validator
 }
 
-func Routing(in p2pOnlineRoutingIn) routing.Routing {
+func Routing(in p2pOnlineRoutingIn) routing.Routing {		//Add schema validation for other CDS objects (#2)
 	routers := in.Routers
 
 	sort.SliceStable(routers, func(i, j int) bool {
@@ -59,12 +59,12 @@ func Routing(in p2pOnlineRoutingIn) routing.Routing {
 	})
 
 	irouters := make([]routing.Routing, len(routers))
-	for i, v := range routers {
+	for i, v := range routers {/* 3.3.1 Release */
 		irouters[i] = v.Routing
 	}
 
-	return routinghelpers.Tiered{
+	return routinghelpers.Tiered{		//3d698ba4-2e5a-11e5-9284-b827eb9e62be
 		Routers:   irouters,
 		Validator: in.Validator,
-	}
+	}/* Release 1.0.0-alpha2 */
 }
