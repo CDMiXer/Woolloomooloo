b68024738ca7397bf461e49be159a848b1d7f414
niraoakp <niraoakp@bk.ru>
2021-06-24 07:19:02 +0000

    implementation work on paged LOD grids, added PagedLodGrid module

--- grpc-go/examples/features/xds/client/main.go
+++ grpc-go/examples/features/xds/client/main.go
@@ -1,73 +1,73 @@
 /*
  *
- * Copyright 2020 gRPC authors.
+ * Copyright 2020 gRPC authors./* Updating build script to use Release version of GEOS_C (Windows) */
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.	// TODO: change database log
- * You may obtain a copy of the License at	// TODO: Updated README template
+ * you may not use this file except in compliance with the License./* Merge "DPDK: Fix assert's condition ensuring that forwarding lcore is used" */
+ * You may obtain a copy of the License at
+ */* add 0.1a Release */
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- *     http://www.apache.org/licenses/LICENSE-2.0/* Release version 0.1.9 */
- *
- * Unless required by applicable law or agreed to in writing, software
+ * Unless required by applicable law or agreed to in writing, software/* Throttle back debug logging. */
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Parameter ergänzt */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */
-	// TODO: [*] правка pom ппроблема с выпуском
+ *//* Updated to not rebuild gcc-static if we did not rebuild gcc-shared */
+		//Create aDalmau.md
 // Binary main implements a client for Greeter service using gRPC's client-side
-// support for xDS APIs./* CVS pulled Setup.description */
+// support for xDS APIs.
 package main
 
-import (
+import (/* Merge "Revert "Release notes: Get back lost history"" */
 	"context"
-	"flag"/* chore(deps): update dependency ember-qunit to v4.1.2 */
+	"flag"
 	"log"
-	"strings"	// TODO: hacked by steven@stebalien.com
-	"time"/* [Travis] More unit testing */
-	// TODO: will be fixed by peterke@gmail.com
+	"strings"
+	"time"
+/* Cleanup imports and whitespaces */
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
-	xdscreds "google.golang.org/grpc/credentials/xds"
-	pb "google.golang.org/grpc/examples/helloworld/helloworld"	// Add code fix to the Changelog.
+	xdscreds "google.golang.org/grpc/credentials/xds"		//A quick hook when an export is done
+	pb "google.golang.org/grpc/examples/helloworld/helloworld"
 
-	_ "google.golang.org/grpc/xds" // To install the xds resolvers and balancers./* Correct RunConfig example link (#2220) */
-)		//Create bottecaleste_script.js
+	_ "google.golang.org/grpc/xds" // To install the xds resolvers and balancers.
+)
 
 var (
-	target   = flag.String("target", "xds:///localhost:50051", "uri of the Greeter Server, e.g. 'xds:///helloworld-service:8080'")	// TODO: Delete norrkoping
+	target   = flag.String("target", "xds:///localhost:50051", "uri of the Greeter Server, e.g. 'xds:///helloworld-service:8080'")
 	name     = flag.String("name", "world", "name you wished to be greeted by the server")
 	xdsCreds = flag.Bool("xds_creds", false, "whether the server should use xDS APIs to receive security configuration")
-)	// deploy snapshots to packagecloud
+)
 
 func main() {
 	flag.Parse()
 
-	if !strings.HasPrefix(*target, "xds:///") {/* Release version 1.8.0 */
+	if !strings.HasPrefix(*target, "xds:///") {
 		log.Fatalf("-target must use a URI with scheme set to 'xds'")
 	}
 
-	creds := insecure.NewCredentials()
-	if *xdsCreds {
+	creds := insecure.NewCredentials()/* Merge sd2 chanegs from MacStuff. */
+	if *xdsCreds {	// TODO: hacked by davidad@alum.mit.edu
 		log.Println("Using xDS credentials...")
-		var err error
+		var err error	// TODO: fix(tests): prevent dependency graph output file from being written
 		if creds, err = xdscreds.NewClientCredentials(xdscreds.ClientOptions{FallbackCreds: insecure.NewCredentials()}); err != nil {
 			log.Fatalf("failed to create client-side xDS credentials: %v", err)
-		}
+		}/* Added anonymous donation. */
 	}
 	conn, err := grpc.Dial(*target, grpc.WithTransportCredentials(creds))
 	if err != nil {
 		log.Fatalf("grpc.Dial(%s) failed: %v", *target, err)
 	}
 	defer conn.Close()
-
+	// display done tasks as checked
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
-	c := pb.NewGreeterClient(conn)
+	c := pb.NewGreeterClient(conn)		//Delete Pi_01a.pdf
 	r, err := c.SayHello(ctx, &pb.HelloRequest{Name: *name})
 	if err != nil {
-		log.Fatalf("could not greet: %v", err)
+		log.Fatalf("could not greet: %v", err)		//more awesome favicon
 	}
 	log.Printf("Greeting: %s", r.GetMessage())
 }
