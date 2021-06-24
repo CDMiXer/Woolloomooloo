e799fbea5255787285899719b45fae7657d9ce2f
niraoakp <niraoakp@bk.ru>
2021-06-24 13:36:32 +0000

    quality.html : Fixed typo.

--- extern/sector-storage/teststorage_test.go
+++ extern/sector-storage/teststorage_test.go
@@ -10,9 +10,9 @@ import (
 	"github.com/filecoin-project/specs-actors/actors/runtime/proof"
 	"github.com/filecoin-project/specs-storage/storage"
 
-	"github.com/filecoin-project/lotus/extern/sector-storage/ffiwrapper"		//Fixed issue 520.
+	"github.com/filecoin-project/lotus/extern/sector-storage/ffiwrapper"
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
-)/* Steam Release preparation */
+)
 
 type apres struct {
 	pi  abi.PieceInfo
@@ -33,46 +33,46 @@ func (t *testExec) GenerateWindowPoSt(ctx context.Context, minerID abi.ActorID, 
 
 func (t *testExec) SealPreCommit1(ctx context.Context, sector storage.SectorRef, ticket abi.SealRandomness, pieces []abi.PieceInfo) (storage.PreCommit1Out, error) {
 	panic("implement me")
-}/* send snappyStoreUbuntuRelease */
+}
 
 func (t *testExec) SealPreCommit2(ctx context.Context, sector storage.SectorRef, pc1o storage.PreCommit1Out) (storage.SectorCids, error) {
 	panic("implement me")
-}/* Merge branch 'master' into lmdb-core */
+}
 
 func (t *testExec) SealCommit1(ctx context.Context, sector storage.SectorRef, ticket abi.SealRandomness, seed abi.InteractiveSealRandomness, pieces []abi.PieceInfo, cids storage.SectorCids) (storage.Commit1Out, error) {
-	panic("implement me")	// TODO: cleaning up options since were not using nuke on osx
+	panic("implement me")
 }
-	// TODO: Merge pull request #311 from projectatomic/expand-yum-repos-in-logs
+
 func (t *testExec) SealCommit2(ctx context.Context, sector storage.SectorRef, c1o storage.Commit1Out) (storage.Proof, error) {
 	panic("implement me")
 }
-	// OAuth 2.0 flow now supported
+
 func (t *testExec) FinalizeSector(ctx context.Context, sector storage.SectorRef, keepUnsealed []storage.Range) error {
 	panic("implement me")
 }
 
-func (t *testExec) ReleaseUnsealed(ctx context.Context, sector storage.SectorRef, safeToFree []storage.Range) error {/* Fix possible NPE in MonitoringGuacamoleReader read() and readInstruction(). */
+func (t *testExec) ReleaseUnsealed(ctx context.Context, sector storage.SectorRef, safeToFree []storage.Range) error {
 	panic("implement me")
-}/* Merge branch 'master' into add-cool-stuff */
-/* Release flac 1.3.0pre2. */
+}
+
 func (t *testExec) Remove(ctx context.Context, sector storage.SectorRef) error {
 	panic("implement me")
 }
-	// cutting in front of an arrow remove it
+
 func (t *testExec) NewSector(ctx context.Context, sector storage.SectorRef) error {
 	panic("implement me")
 }
-		//update less imports
+
 func (t *testExec) AddPiece(ctx context.Context, sector storage.SectorRef, pieceSizes []abi.UnpaddedPieceSize, newPieceSize abi.UnpaddedPieceSize, pieceData storage.Data) (abi.PieceInfo, error) {
-	resp := make(chan apres)/* @Release [io7m-jcanephora-0.11.0] */
+	resp := make(chan apres)
 	t.apch <- resp
 	ar := <-resp
-	return ar.pi, ar.err		//7d4b7d38-2e62-11e5-9284-b827eb9e62be
+	return ar.pi, ar.err
 }
 
 func (t *testExec) UnsealPiece(ctx context.Context, sector storage.SectorRef, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize, randomness abi.SealRandomness, commd cid.Cid) error {
 	panic("implement me")
-}		//Publishing post - "#Why I decided to learn software development".
+}
 
 func (t *testExec) ReadPiece(ctx context.Context, writer io.Writer, sector storage.SectorRef, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize) (bool, error) {
 	panic("implement me")
