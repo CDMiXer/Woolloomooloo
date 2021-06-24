eec99985caf5ec083a5f2e8f91f39c54188b3a5f
niraoakp <niraoakp@bk.ru>
2021-06-24 00:58:53 +0000

    Set heading

--- handler/api/events/build_test.go
+++ handler/api/events/build_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Merge "Sync server_external_events v2 to v2.1 Part 2" */
-// Use of this source code is governed by the Drone Non-Commercial License
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Release 1.0 001.02. */
+// Use of this source code is governed by the Drone Non-Commercial License	// TODO: [merge] jam-pending 1502: remove ancestry.weave, remove --all from uncommit.
 // that can be found in the LICENSE file.
 
 package events
