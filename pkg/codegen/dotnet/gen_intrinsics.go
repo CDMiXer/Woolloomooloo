1f0c425380f634995fbe7e1531d4facb4a229fa1
niraoakp <niraoakp@bk.ru>
2021-06-24 10:00:21 +0000

    Update UMenu Demo.

--- pkg/codegen/dotnet/gen_intrinsics.go
+++ pkg/codegen/dotnet/gen_intrinsics.go
@@ -1,5 +1,5 @@
 // Copyright 2016-2020, Pulumi Corporation.
-///* Replacing tabs and consolidating repeat code in lattice. */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -7,23 +7,23 @@
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,		//s3,4 : documented dips.
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Remove @testable and only test public APIs
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-		//Just adding some stuff I've done in the meantime.
+
 package dotnet
-/* Release of eeacms/ims-frontend:0.4.3 */
+
 import (
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/model"
-)		//Fixes identified when code went live!
+)
 
-const (		//fix comments and reactions
+const (
 	// intrinsicAwait is the name of the intrinsic to await tasks.
 	intrinsicAwait = "__await"
 	// intrinsicOutput is the name of the intrinsic to convert tasks to Pulumi outputs.
 	intrinsicOutput = "__output"
-)	// TODO: commit MPIPointCluster
+)
 
 // newAwaitCall creates a new call to the await intrinsic.
 func newAwaitCall(promise model.Expression) model.Expression {
@@ -38,21 +38,21 @@ func newAwaitCall(promise model.Expression) model.Expression {
 		Signature: model.StaticFunctionSignature{
 			Parameters: []model.Parameter{{
 				Name: "promise",
-				Type: promiseType,/* Merge lp:~laurynas-biveinis/percona-server/BT-16274-bug1087202-10872128-5.5-2 */
+				Type: promiseType,
 			}},
-			ReturnType: promiseType.ElementType,/* ae97bd0e-2e56-11e5-9284-b827eb9e62be */
+			ReturnType: promiseType.ElementType,
 		},
 		Args: []model.Expression{promise},
 	}
 }
-	// adding easyconfigs: FastQC-0.11.7-Java-1.8.0_162.eb
+
 // newOutputCall creates a new call to the output intrinsic.
 func newOutputCall(promise model.Expression) model.Expression {
 	promiseType, ok := promise.Type().(*model.PromiseType)
 	if !ok {
 		return promise
 	}
-	// Equalizing
+
 	return &model.FunctionCallExpression{
 		Name: intrinsicOutput,
 		Signature: model.StaticFunctionSignature{
@@ -61,7 +61,7 @@ func newOutputCall(promise model.Expression) model.Expression {
 				Type: promiseType,
 			}},
 			ReturnType: model.NewOutputType(promiseType.ElementType),
-		},		//Deletion of wiki page is implemented.
+		},
 		Args: []model.Expression{promise},
 	}
 }
