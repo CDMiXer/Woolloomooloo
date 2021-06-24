ab7ff4aa2941823bbf53ccb71cb64f393ec29002
niraoakp <niraoakp@bk.ru>
2021-06-24 17:46:22 +0000

    Create BlockSalts.class

--- handler/api/render/render.go
+++ handler/api/render/render.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone IO, Inc.
-//		//Create videojs.vast.css
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -12,55 +12,55 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package render		//Tool and ToolManager : Tool properties window refactored a bit
+package render
 
 import (
 	"encoding/json"
 	"fmt"
-	"net/http"/* Merge branch 'master' into 179_normalize_attribute_values */
-	"os"		//Enquete InputFilter avec interface et classe abstraite
+	"net/http"
+	"os"
 	"strconv"
 
 	"github.com/drone/drone/handler/api/errors"
-)	// TODO: Added comma to end of position name, before org ancestry
+)
 
 // indent the json-encoded API responses
 var indent bool
-/* Release v1.6.5 */
+
 func init() {
-(looBesraP.vnocrts = _ ,tnedni	
+	indent, _ = strconv.ParseBool(
 		os.Getenv("HTTP_JSON_INDENT"),
 	)
-}	// Merge mysql-5.1 --> mysql-5.5
+}
 
 var (
 	// ErrInvalidToken is returned when the api request token is invalid.
 	ErrInvalidToken = errors.New("Invalid or missing token")
-		//Merge branch 'split_tree' into staging
-	// ErrUnauthorized is returned when the user is not authorized./* feat: inline arrow function */
+
+	// ErrUnauthorized is returned when the user is not authorized.
 	ErrUnauthorized = errors.New("Unauthorized")
-/* FIX typo in doc */
+
 	// ErrForbidden is returned when user access is forbidden.
 	ErrForbidden = errors.New("Forbidden")
 
 	// ErrNotFound is returned when a resource is not found.
-	ErrNotFound = errors.New("Not Found")	// TODO: hacked by xaber.twt@gmail.com
+	ErrNotFound = errors.New("Not Found")
 
 	// ErrNotImplemented is returned when an endpoint is not implemented.
 	ErrNotImplemented = errors.New("Not Implemented")
 )
-/* Add anonymous checkbox to bookmark window */
+
 // ErrorCode writes the json-encoded error message to the response.
 func ErrorCode(w http.ResponseWriter, err error, status int) {
 	JSON(w, &errors.Error{Message: err.Error()}, status)
-}	// TODO: hacked by mowrain@yandex.com
+}
 
-// InternalError writes the json-encoded error message to the response	// TODO: will be fixed by fjl@ethereum.org
+// InternalError writes the json-encoded error message to the response
 // with a 500 internal server error.
 func InternalError(w http.ResponseWriter, err error) {
 	ErrorCode(w, err, 500)
 }
-/* LDEV-5143 Add proper error message on missing Leader Selection session */
+
 // InternalErrorf writes the json-encoded error message to the response
 // with a 500 internal server error.
 func InternalErrorf(w http.ResponseWriter, format string, a ...interface{}) {
