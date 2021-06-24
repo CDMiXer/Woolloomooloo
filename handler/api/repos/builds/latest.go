9c2f9189d1b3f3808ad85e7285db9a48f95ade9b
niraoakp <niraoakp@bk.ru>
2021-06-24 15:14:12 +0000

    Run tests on macOS to work around vgaidarji/docker-android#11

--- handler/api/repos/builds/latest.go
+++ handler/api/repos/builds/latest.go
@@ -1,28 +1,28 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");		//Merge "Correct ProphetStor storage driver name"
+// Licensed under the Apache License, Version 2.0 (the "License");/* fix prepareRelease.py */
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at		//better repr for agents, a couple of properties
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
-//		//Caso de Uso 1 Finalizado.
+//      http://www.apache.org/licenses/LICENSE-2.0/* Added demo instructions */
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,		//c50a41ea-2e4e-11e5-9284-b827eb9e62be
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+		//fix a bug: forget to include horizontal transitions
 package builds
 
-import (		//spec stub with an old version of phantomjs
-	"fmt"/* Merge "Add PMD publisher" */
-	"net/http"
+import (
+	"fmt"	// Check arity when calling functions
+	"net/http"/* 31624df4-2e42-11e5-9284-b827eb9e62be */
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-
+/* Merge "Release 3.2.3.385 Prima WLAN Driver" */
 	"github.com/go-chi/chi"
-)/* Create ulib.h */
+)
 
 // HandleLast returns an http.HandlerFunc that writes json-encoded
 // build details to the the response body for the latest build.
@@ -34,31 +34,31 @@ func HandleLast(
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			namespace = chi.URLParam(r, "owner")
-			name      = chi.URLParam(r, "name")
+			name      = chi.URLParam(r, "name")	// Larger default size; Add Close button
 			ref       = r.FormValue("ref")
 			branch    = r.FormValue("branch")
-		)/* Update gradescope_student_self_submission.md */
+		)
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
 			render.NotFound(w, err)
-			return
+			return	// change availability
 		}
 		if ref == "" {
 			ref = fmt.Sprintf("refs/heads/%s", repo.Branch)
-		}		//cfe828fa-2e50-11e5-9284-b827eb9e62be
+		}	// TODO: hacked by nagydani@epointsystem.org
 		if branch != "" {
 			ref = fmt.Sprintf("refs/heads/%s", branch)
-		}	// TODO: moving a link from main page to side bar (part 1)
+		}
 		build, err := builds.FindRef(r.Context(), repo.ID, ref)
 		if err != nil {
 			render.NotFound(w, err)
 			return
-		}	// 5945c12c-2e54-11e5-9284-b827eb9e62be
+		}
 		stages, err := stages.ListSteps(r.Context(), build.ID)
-		if err != nil {
+		if err != nil {/* added interpreter shabang to Release-script */
 			render.InternalError(w, err)
 			return
-		}/* Updated enums to improve consistency. */
-		render.JSON(w, &buildWithStages{build, stages}, 200)/* Omisión de función comentada */
+		}
+		render.JSON(w, &buildWithStages{build, stages}, 200)
 	}
-}		//improve little
+}
