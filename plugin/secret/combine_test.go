b2d6ea45e4235348b788e45805dfa21008b052e2
niraoakp <niraoakp@bk.ru>
2021-06-24 06:29:00 +0000

    Adding example of Image Carousel Component

--- plugin/secret/combine_test.go
+++ plugin/secret/combine_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// BUG #14122156 - INNODB.INNODB-WL5522* FAILURE ON PB2 WITH DIFFERENT SYMPTOMS 
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 package secret
