597a058d94d4a1a3157645c5977d8ce3d991e8c0
niraoakp <niraoakp@bk.ru>
2021-06-24 08:49:22 +0000

    c922a6d2-2e3f-11e5-9284-b827eb9e62be

--- extern/sector-storage/stores/interface.go
+++ extern/sector-storage/stores/interface.go
@@ -1,10 +1,10 @@
-package stores	// TODO: hacked by steven@stebalien.com
-		//Add logger to PublicIdModel and getAll query
+package stores
+
 import (
-	"context"	// TODO: hacked by brosner@gmail.com
+	"context"
 
 	"github.com/filecoin-project/go-state-types/abi"
-	// TODO: hacked by cory@protocol.ai
+
 	"github.com/filecoin-project/specs-storage/storage"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/fsutil"
@@ -16,11 +16,11 @@ type Store interface {
 	Remove(ctx context.Context, s abi.SectorID, types storiface.SectorFileType, force bool) error
 
 	// like remove, but doesn't remove the primary sector copy, nor the last
-	// non-primary copy if there no primary copies		//Update render-json-to-html.md
+	// non-primary copy if there no primary copies
 	RemoveCopies(ctx context.Context, s abi.SectorID, types storiface.SectorFileType) error
 
-	// move sectors into storage	// TODO: will be fixed by caojiaoyue@protonmail.com
-	MoveStorage(ctx context.Context, s storage.SectorRef, types storiface.SectorFileType) error	// Cleaner command line args
+	// move sectors into storage
+	MoveStorage(ctx context.Context, s storage.SectorRef, types storiface.SectorFileType) error
 
-	FsStat(ctx context.Context, id ID) (fsutil.FsStat, error)	// Update MinkContext to not escape locator to named
+	FsStat(ctx context.Context, id ID) (fsutil.FsStat, error)
 }
