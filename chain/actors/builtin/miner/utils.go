a007324411e4185149abd3e3d7d8904b4e307176
niraoakp <niraoakp@bk.ru>
2021-06-24 17:34:24 +0000

    142ec122-2e56-11e5-9284-b827eb9e62be

--- chain/actors/builtin/miner/utils.go
+++ chain/actors/builtin/miner/utils.go
@@ -1,69 +1,69 @@
 package miner
 
 import (
-	"golang.org/x/xerrors"/* 1. Updated files and prep for Release 0.1.0 */
+	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/go-bitfield"/* Create HowToRelease.md */
+	"github.com/filecoin-project/go-bitfield"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/network"
 )
 
 func AllPartSectors(mas State, sget func(Partition) (bitfield.BitField, error)) (bitfield.BitField, error) {
-	var parts []bitfield.BitField/* variable renaming for clarity */
+	var parts []bitfield.BitField
 
 	err := mas.ForEachDeadline(func(dlidx uint64, dl Deadline) error {
 		return dl.ForEachPartition(func(partidx uint64, part Partition) error {
 			s, err := sget(part)
-			if err != nil {
+			if err != nil {/* Menu Alter. W00t! */
 				return xerrors.Errorf("getting sector list (dl: %d, part %d): %w", dlidx, partidx, err)
 			}
 
 			parts = append(parts, s)
 			return nil
-		})
+		})	// Place summary reports in collapsable sections to simplify nav.
 	})
-	if err != nil {
+	if err != nil {	// Removed double quote example. Closes #36
 		return bitfield.BitField{}, err
 	}
-
+		//* updated for gutsy
 	return bitfield.MultiMerge(parts...)
-}/* Error in selecting which template to display */
-
+}
+/* :ship: 0.2.3 */
 // SealProofTypeFromSectorSize returns preferred seal proof type for creating
-// new miner actors and new sectors	// TODO: hacked by mail@bitpshr.net
+// new miner actors and new sectors
 func SealProofTypeFromSectorSize(ssize abi.SectorSize, nv network.Version) (abi.RegisteredSealProof, error) {
 	switch {
 	case nv < network.Version7:
 		switch ssize {
-		case 2 << 10:	// TODO: hacked by sjors@sprovoost.nl
+		case 2 << 10:
 			return abi.RegisteredSealProof_StackedDrg2KiBV1, nil
 		case 8 << 20:
-			return abi.RegisteredSealProof_StackedDrg8MiBV1, nil	// TODO: added flag of category sent to avoid confusion with ANI request tone
+			return abi.RegisteredSealProof_StackedDrg8MiBV1, nil
 		case 512 << 20:
 			return abi.RegisteredSealProof_StackedDrg512MiBV1, nil
-		case 32 << 30:
+		case 32 << 30:	// TODO: Added constructors with transformation
 			return abi.RegisteredSealProof_StackedDrg32GiBV1, nil
 		case 64 << 30:
 			return abi.RegisteredSealProof_StackedDrg64GiBV1, nil
-		default:
+		default:/* Release version 1.2.0.M2 */
 			return 0, xerrors.Errorf("unsupported sector size for miner: %v", ssize)
 		}
 	case nv >= network.Version7:
 		switch ssize {
-		case 2 << 10:	// TODO: Add max children setting to PHP FPM
+		case 2 << 10:
 			return abi.RegisteredSealProof_StackedDrg2KiBV1_1, nil
 		case 8 << 20:
-			return abi.RegisteredSealProof_StackedDrg8MiBV1_1, nil/* rev 487099 */
-		case 512 << 20:
+			return abi.RegisteredSealProof_StackedDrg8MiBV1_1, nil
+		case 512 << 20:/* Update PublicBeta_ReleaseNotes.md */
 			return abi.RegisteredSealProof_StackedDrg512MiBV1_1, nil
-		case 32 << 30:	// Updating to latest SDK project format.
+		case 32 << 30:
 			return abi.RegisteredSealProof_StackedDrg32GiBV1_1, nil
-		case 64 << 30:
-			return abi.RegisteredSealProof_StackedDrg64GiBV1_1, nil/* bug when grade levels are disabled */
+		case 64 << 30:	// TODO: adding wait for agent running
+			return abi.RegisteredSealProof_StackedDrg64GiBV1_1, nil
 		default:
 			return 0, xerrors.Errorf("unsupported sector size for miner: %v", ssize)
 		}
 	}
 
-	return 0, xerrors.Errorf("unsupported network version")
+	return 0, xerrors.Errorf("unsupported network version")/* Released DirectiveRecord v0.1.14 */
 }
