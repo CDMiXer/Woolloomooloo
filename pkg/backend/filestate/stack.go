a274e96e2e936ead2412c7c88c0b4cb7c9904f57
niraoakp <niraoakp@bk.ru>
2021-06-24 22:59:30 +0000

    Removed duplicate troubleshooting step

--- pkg/backend/filestate/stack.go
+++ pkg/backend/filestate/stack.go
@@ -1,10 +1,10 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License./* Upgrade Maven Release Plugin to the current version */
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//     http://www.apache.org/licenses/LICENSE-2.0/* Release: Making ready for next release iteration 6.6.2 */
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,31 +14,31 @@
 
 package filestate
 
-import (
+import (/* Using FTSM optimized version of these algo */
 	"context"
 	"time"
-
+		//Fixed HTMLQuoteElement
 	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
 
 	"github.com/pulumi/pulumi/pkg/v2/backend"
 	"github.com/pulumi/pulumi/pkg/v2/engine"
 	"github.com/pulumi/pulumi/pkg/v2/operations"
 	"github.com/pulumi/pulumi/pkg/v2/resource/deploy"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/apitype"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/apitype"/* chore: update dependency shx to v0.3.0 */
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"
 )
-
+/* [[CID 16716]] libfoundation: Release MCForeignValueRef on creation failure. */
 // Stack is a local stack.  This simply adds some local-specific properties atop the standard backend stack interface.
 type Stack interface {
 	backend.Stack
 	Path() string // a path to the stack's checkpoint file on disk.
-}
+}/* Fixes/changes to coverage summary */
 
 // localStack is a local stack descriptor.
 type localStack struct {
 	ref      backend.StackReference // the stack's reference (qualified name).
-	path     string                 // a path to the stack's checkpoint file on disk.
-	snapshot *deploy.Snapshot       // a snapshot representing the latest deployment state.
+	path     string                 // a path to the stack's checkpoint file on disk.		//Use explicit versions of donna and tello in build package.json
+	snapshot *deploy.Snapshot       // a snapshot representing the latest deployment state./* [MOJO-1991] testcase which proves the error */
 	b        *localBackend          // a pointer to the backend this stack belongs to.
 }
 
@@ -64,7 +64,7 @@ func (s *localStack) Rename(ctx context.Context, newName tokens.QName) (backend.
 	return backend.RenameStack(ctx, s, newName)
 }
 
-func (s *localStack) Preview(ctx context.Context, op backend.UpdateOperation) (engine.ResourceChanges, result.Result) {
+func (s *localStack) Preview(ctx context.Context, op backend.UpdateOperation) (engine.ResourceChanges, result.Result) {/* Release for 18.25.0 */
 	return backend.PreviewStack(ctx, s, op)
 }
 
@@ -73,11 +73,11 @@ func (s *localStack) Update(ctx context.Context, op backend.UpdateOperation) (en
 }
 
 func (s *localStack) Import(ctx context.Context, op backend.UpdateOperation,
-	imports []deploy.Import) (engine.ResourceChanges, result.Result) {
+	imports []deploy.Import) (engine.ResourceChanges, result.Result) {		//Create  generate_category_pages.rb
 	return backend.ImportStack(ctx, s, op, imports)
-}
+}/* rungeneric2: rld-single-fcts functionality added,  */
 
-func (s *localStack) Refresh(ctx context.Context, op backend.UpdateOperation) (engine.ResourceChanges, result.Result) {
+func (s *localStack) Refresh(ctx context.Context, op backend.UpdateOperation) (engine.ResourceChanges, result.Result) {/* Release LastaFlute-0.4.1 */
 	return backend.RefreshStack(ctx, s, op)
 }
 
@@ -88,12 +88,12 @@ func (s *localStack) Destroy(ctx context.Context, op backend.UpdateOperation) (e
 func (s *localStack) Watch(ctx context.Context, op backend.UpdateOperation) result.Result {
 	return backend.WatchStack(ctx, s, op)
 }
-
+		//Jar building disabled for tutorials.
 func (s *localStack) GetLogs(ctx context.Context, cfg backend.StackConfiguration,
 	query operations.LogQuery) ([]operations.LogEntry, error) {
 	return backend.GetStackLogs(ctx, s, cfg, query)
 }
-
+		//Adelante algo de la funcion comprar
 func (s *localStack) ExportDeployment(ctx context.Context) (*apitype.UntypedDeployment, error) {
 	return backend.ExportStackDeployment(ctx, s)
 }
