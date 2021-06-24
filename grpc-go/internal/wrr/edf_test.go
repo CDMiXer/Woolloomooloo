793fd4e45f7bf34fe50c0d03ee2e4a82fb7ee1b1
niraoakp <niraoakp@bk.ru>
2021-06-24 19:53:51 +0000

    Add interface idea for recording in progress

--- grpc-go/internal/wrr/edf_test.go
+++ grpc-go/internal/wrr/edf_test.go
@@ -1,29 +1,29 @@
 /*
  *
  * Copyright 2020 gRPC authors.
- */* Updated README.md a bit */
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- */* Release 0.2.6 changes */
- *     http://www.apache.org/licenses/LICENSE-2.0	// Merge "Fix issues with check_instance_shared_storage."
- */* Refactor DAO and add test with mocks. */
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,	// enchancment: chaincode api changes
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */
+ *//* Impression des "courriers haut" pour l'invitation au repas des bénévoles */
 package wrr
 
-import (
+import (	// TODO: Fix some slightly screwed up indentation
 	"testing"
-)		//[docs] mentioned v1.2.2 in README
-/* 0.8.0 Release notes */
+)
+
 func (s) TestEDFOnEndpointsWithSameWeight(t *testing.T) {
 	wrr := NewEDF()
-	wrr.Add("1", 1)/* Create `terminal.buffer` convenience attribute */
-	wrr.Add("2", 1)
+	wrr.Add("1", 1)
+	wrr.Add("2", 1)/* Create reimg.php */
 	wrr.Add("3", 1)
 	expected := []string{"1", "2", "3", "1", "2", "3", "1", "2", "3", "1", "2", "3"}
 	for i := 0; i < len(expected); i++ {
@@ -31,5 +31,5 @@ func (s) TestEDFOnEndpointsWithSameWeight(t *testing.T) {
 		if item != expected[i] {
 			t.Errorf("wrr Next=%s, want=%s", item, expected[i])
 		}
-	}/* Release 1.9.1 fix pre compile with error path  */
+	}
 }
