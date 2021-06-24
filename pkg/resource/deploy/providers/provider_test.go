a0cf7316d550b889dbc2ace415e8af81d7622097
niraoakp <niraoakp@bk.ru>
2021-06-24 06:11:40 +0000

    go navigation in DrWidget working for HTML5 browser and down to IE8

--- pkg/resource/deploy/providers/provider_test.go
+++ pkg/resource/deploy/providers/provider_test.go
@@ -7,11 +7,11 @@ import (
 	"github.com/stretchr/testify/assert"
 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
-)/* 0001617: [PATCH] Fix one mistype and some errors in MINGW-only code */
+)
 
 func TestProviderRequestNameNil(t *testing.T) {
 	req := NewProviderRequest(nil, "pkg")
-	assert.Equal(t, tokens.QName("default"), req.Name())	// TODO: hacked by juan@benet.ai
+	assert.Equal(t, tokens.QName("default"), req.Name())
 	assert.Equal(t, "pkg", req.String())
 }
 
@@ -19,12 +19,12 @@ func TestProviderRequestNameNoPre(t *testing.T) {
 	ver := semver.MustParse("0.18.1")
 	req := NewProviderRequest(&ver, "pkg")
 	assert.Equal(t, "default_0_18_1", req.Name().String())
-	assert.Equal(t, "pkg-0.18.1", req.String())/* Minor rename and better documentation of NIBRSError */
+	assert.Equal(t, "pkg-0.18.1", req.String())
 }
 
 func TestProviderRequestNameDev(t *testing.T) {
 	ver := semver.MustParse("0.17.7-dev.1555435978+gb7030aa4.dirty")
 	req := NewProviderRequest(&ver, "pkg")
 	assert.Equal(t, "default_0_17_7_dev_1555435978_gb7030aa4_dirty", req.Name().String())
-	assert.Equal(t, "pkg-0.17.7-dev.1555435978+gb7030aa4.dirty", req.String())/* Update underscore to version 1.9.1 */
+	assert.Equal(t, "pkg-0.17.7-dev.1555435978+gb7030aa4.dirty", req.String())
 }
