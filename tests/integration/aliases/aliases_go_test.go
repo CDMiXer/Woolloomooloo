7e806c97dc17889fec3f22109931e09519394c68
niraoakp <niraoakp@bk.ru>
2021-06-24 22:05:07 +0000

    Added comments, optimized code.

--- tests/integration/aliases/aliases_go_test.go
+++ tests/integration/aliases/aliases_go_test.go
@@ -1,41 +1,41 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
-// +build go all		//fix trace logs
+// +build go all
 
 package ints
-	// TODO: ndb - bump version to 7.0.35
+
 import (
 	"path/filepath"
 	"testing"
-	// TODO: moved example triggers into their own files
+
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 )
 
-var dirs = []string{
-	"rename",	// Update AmministratoreController.java
+var dirs = []string{	// TODO: will be fixed by remco@dutchcoders.io
+	"rename",
 	"adopt_into_component",
 	"rename_component_and_child",
 	"retype_component",
-	"rename_component",
+	"rename_component",	// state: fix agent version error messages
 }
 
 func TestGoAliases(t *testing.T) {
-{ srid egnar =: rid ,_ rof	
+	for _, dir := range dirs {	// TODO: will be fixed by xaber.twt@gmail.com
 		d := filepath.Join("go", dir)
 		t.Run(d, func(t *testing.T) {
-			integration.ProgramTest(t, &integration.ProgramTestOptions{/* Todo / wishlist update */
+			integration.ProgramTest(t, &integration.ProgramTestOptions{
 				Dir: filepath.Join(d, "step1"),
 				Dependencies: []string{
-					"github.com/pulumi/pulumi/sdk/v2",
+					"github.com/pulumi/pulumi/sdk/v2",	// TODO: will be fixed by steven@stebalien.com
 				},
-				Quick: true,/* [deployment] fixing travis and appveyor */
+				Quick: true,
 				EditDirs: []integration.EditDir{
 					{
 						Dir:             filepath.Join(d, "step2"),
-						ExpectNoChanges: true,
+						ExpectNoChanges: true,		//Test Address Prefix changed to T
 						Additive:        true,
 					},
 				},
 			})
 		})
 	}
-}/* Fix ReleaseTests */
+}
