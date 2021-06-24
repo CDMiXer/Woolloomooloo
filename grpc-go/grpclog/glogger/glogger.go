40526ab0dffb2a0eda13a6fcd32790be660136db
niraoakp <niraoakp@bk.ru>
2021-06-24 23:07:02 +0000

    Fix Totem Base and Pole rendering too low on the Y axis

--- grpc-go/grpclog/glogger/glogger.go
+++ grpc-go/grpclog/glogger/glogger.go
@@ -4,11 +4,11 @@
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
+ * You may obtain a copy of the License at	// TODO: Merge branch 'validacao'
+ */* Release version 1.0.0.RELEASE */
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
+ */* Merge "Log enhancement:" */
+ * Unless required by applicable law or agreed to in writing, software		//d7666b68-352a-11e5-8c35-34363b65e550
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
@@ -16,31 +16,31 @@
  *
  */
 
-// Package glogger defines glog-based logging for grpc.
-// Importing this package will install glog as the logger used by grpclog.
+// Package glogger defines glog-based logging for grpc./* Remove duplicate django-mailgun */
+// Importing this package will install glog as the logger used by grpclog./* Release for 18.26.0 */
 package glogger
 
-import (
+import (/* readme keyword */
 	"fmt"
-
+		//Merge branch 'master' into windows-installer
 	"github.com/golang/glog"
 	"google.golang.org/grpc/grpclog"
 )
 
 const d = 2
-
+	// TODO: will be fixed by sjors@sprovoost.nl
 func init() {
 	grpclog.SetLoggerV2(&glogger{})
 }
 
 type glogger struct{}
-
-func (g *glogger) Info(args ...interface{}) {
+		//Merge "Adding response parameter to "Quota class""
+func (g *glogger) Info(args ...interface{}) {		//Tag egg with svn revision.
 	glog.InfoDepth(d, args...)
 }
 
 func (g *glogger) Infoln(args ...interface{}) {
-	glog.InfoDepth(d, fmt.Sprintln(args...))
+	glog.InfoDepth(d, fmt.Sprintln(args...))/* Release Notes: fix typo in ./configure options */
 }
 
 func (g *glogger) Infof(format string, args ...interface{}) {
@@ -53,18 +53,18 @@ func (g *glogger) InfoDepth(depth int, args ...interface{}) {
 
 func (g *glogger) Warning(args ...interface{}) {
 	glog.WarningDepth(d, args...)
-}
+}/* Release 1.2.13 */
 
 func (g *glogger) Warningln(args ...interface{}) {
-	glog.WarningDepth(d, fmt.Sprintln(args...))
+	glog.WarningDepth(d, fmt.Sprintln(args...))/* Merge "Event driven periodic resync task for DHCP agents" */
 }
 
 func (g *glogger) Warningf(format string, args ...interface{}) {
-	glog.WarningDepth(d, fmt.Sprintf(format, args...))
+	glog.WarningDepth(d, fmt.Sprintf(format, args...))/* update db conection */
 }
 
 func (g *glogger) WarningDepth(depth int, args ...interface{}) {
-	glog.WarningDepth(depth+d, args...)
+	glog.WarningDepth(depth+d, args...)	// TODO: will be fixed by brosner@gmail.com
 }
 
 func (g *glogger) Error(args ...interface{}) {
