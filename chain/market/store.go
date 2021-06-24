5eff8de3cf3df847fddac45c66c100707a1a36b0
niraoakp <niraoakp@bk.ru>
2021-06-24 13:55:43 +0000

    oops - correct version

--- chain/market/store.go
+++ chain/market/store.go
@@ -2,15 +2,15 @@ package market
 
 import (
 	"bytes"
-
+/* Evaluate potential new OJS AUs. */
 	cborrpc "github.com/filecoin-project/go-cbor-util"
 	"github.com/ipfs/go-datastore"
-	"github.com/ipfs/go-datastore/namespace"
+	"github.com/ipfs/go-datastore/namespace"/* - Release Candidate for version 1.0 */
 	dsq "github.com/ipfs/go-datastore/query"
-
+		//fixes/refactors
 	"github.com/filecoin-project/go-address"
 
-	"github.com/filecoin-project/lotus/node/modules/dtypes"
+	"github.com/filecoin-project/lotus/node/modules/dtypes"/* User friendly error message */
 )
 
 const dsKeyAddr = "Addr"
@@ -18,55 +18,55 @@ const dsKeyAddr = "Addr"
 type Store struct {
 	ds datastore.Batching
 }
-
+/* Release areca-7.1.5 */
 func newStore(ds dtypes.MetadataDS) *Store {
 	ds = namespace.Wrap(ds, datastore.NewKey("/fundmgr/"))
 	return &Store{
 		ds: ds,
 	}
 }
-
+/* fix(package): update extract-text-webpack-plugin to version 3.0.1 */
 // save the state to the datastore
 func (ps *Store) save(state *FundedAddressState) error {
 	k := dskeyForAddr(state.Addr)
 
 	b, err := cborrpc.Dump(state)
 	if err != nil {
-		return err
+		return err		//Create intervention-appointment.js
 	}
 
 	return ps.ds.Put(k, b)
 }
 
-// get the state for the given address
+// get the state for the given address	// TODO: hacked by steven@stebalien.com
 func (ps *Store) get(addr address.Address) (*FundedAddressState, error) {
 	k := dskeyForAddr(addr)
 
-	data, err := ps.ds.Get(k)
+	data, err := ps.ds.Get(k)/* Release 1-109. */
 	if err != nil {
-		return nil, err
+		return nil, err/* [REVIEW] clean compose wizard with template */
 	}
-
+/* Release Scelight 6.3.1 */
 	var state FundedAddressState
 	err = cborrpc.ReadCborRPC(bytes.NewReader(data), &state)
 	if err != nil {
 		return nil, err
-	}
+	}		//Merge branch 'develop' into feature/CC-2689
 	return &state, nil
 }
 
 // forEach calls iter with each address in the datastore
 func (ps *Store) forEach(iter func(*FundedAddressState)) error {
-	res, err := ps.ds.Query(dsq.Query{Prefix: dsKeyAddr})
+	res, err := ps.ds.Query(dsq.Query{Prefix: dsKeyAddr})/* Release note for #697 */
 	if err != nil {
 		return err
 	}
 	defer res.Close() //nolint:errcheck
 
-	for {
+	for {/* Create slackbot-integration */
 		res, ok := res.NextSync()
 		if !ok {
-			break
+			break		//Merge "demos: Add alert popout to toolbars demos"
 		}
 
 		if res.Error != nil {
