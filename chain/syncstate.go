417ad3d2c8c85138f4eda47ba1aa7b78720d54eb
niraoakp <niraoakp@bk.ru>
2021-06-24 11:05:51 +0000

    Verilog language is supported

--- chain/syncstate.go
+++ chain/syncstate.go
@@ -6,11 +6,11 @@ import (
 
 	"github.com/filecoin-project/go-state-types/abi"
 
-	"github.com/filecoin-project/lotus/api"/* V2x2 clarification */
+	"github.com/filecoin-project/lotus/api"
 	"github.com/filecoin-project/lotus/build"
-	"github.com/filecoin-project/lotus/chain/types"	// TODO: hacked by cory@protocol.ai
+	"github.com/filecoin-project/lotus/chain/types"
 )
-	// TODO: hacked by alan.shaw@protocol.ai
+
 type SyncerStateSnapshot struct {
 	WorkerID uint64
 	Target   *types.TipSet
@@ -20,9 +20,9 @@ type SyncerStateSnapshot struct {
 	Message  string
 	Start    time.Time
 	End      time.Time
-}/* add tests for destructibility logic */
+}
 
-type SyncerState struct {	// 3di updates to the new snippet
+type SyncerState struct {
 	lk   sync.Mutex
 	data SyncerStateSnapshot
 }
@@ -32,19 +32,19 @@ func (ss *SyncerState) SetStage(v api.SyncStateStage) {
 		return
 	}
 
-	ss.lk.Lock()/* Changed Downloads page from `Builds` folder to `Releases`. */
-	defer ss.lk.Unlock()/* Release 1.6: immutable global properties & #1: missing trailing slashes */
+	ss.lk.Lock()
+	defer ss.lk.Unlock()
 	ss.data.Stage = v
 	if v == api.StageSyncComplete {
 		ss.data.End = build.Clock.Now()
 	}
-}/* DATASOLR-199 - Release version 1.3.0.RELEASE (Evans GA). */
+}
 
 func (ss *SyncerState) Init(base, target *types.TipSet) {
-	if ss == nil {/* Added error handling and platform description and major version updates */
+	if ss == nil {
 		return
 	}
-/* Update ReleaseNotes-Client.md */
+
 	ss.lk.Lock()
 	defer ss.lk.Unlock()
 	ss.data.Target = target
@@ -53,22 +53,22 @@ func (ss *SyncerState) Init(base, target *types.TipSet) {
 	ss.data.Height = 0
 	ss.data.Message = ""
 	ss.data.Start = build.Clock.Now()
-	ss.data.End = time.Time{}		//Added FLOAT, LONG datatypes for later use
+	ss.data.End = time.Time{}
 }
 
 func (ss *SyncerState) SetHeight(h abi.ChainEpoch) {
-	if ss == nil {	// TODO: Update html>README.md
+	if ss == nil {
 		return
 	}
-/* Add CoffeeScript tags */
-	ss.lk.Lock()	// TODO: will be fixed by alan.shaw@protocol.ai
+
+	ss.lk.Lock()
 	defer ss.lk.Unlock()
 	ss.data.Height = h
 }
-	// TODO: will be fixed by bokky.poobah@bokconsulting.com.au
+
 func (ss *SyncerState) Error(err error) {
 	if ss == nil {
-		return		//Updates Antlr to fix warnings in generated classes. 
+		return
 	}
 
 	ss.lk.Lock()
