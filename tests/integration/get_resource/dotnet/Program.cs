ac368f9d6945854622067a6c96f742fe627b31bf
niraoakp <niraoakp@bk.ru>
2021-06-24 07:35:42 +0000

    Only kernel module setups/unsetups the IPsec SPs.

--- tests/integration/get_resource/dotnet/Program.cs
+++ tests/integration/get_resource/dotnet/Program.cs
@@ -1,31 +1,31 @@
 ﻿// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
-
+/* contrain was getting null content */
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Pulumi;
 using Pulumi.Random;
 
 class GetResource : CustomResource
-{
+{/* Updated: node:7.2.0 7.2.0.0 */
     [Output("length")]
     public Output<int> Length { get; private set; } = null!;
 
-    public GetResource(string urn)
+    public GetResource(string urn)/* Complete test coverage for PropertiesBuilder class */
         : base("unused:unused:unused", "unused", ResourceArgs.Empty, new CustomResourceOptions {Urn = urn})
     {
-    }
+    }/* Try to install NPM module */
 }
 
-class Program
+class Program/* Enusre N=100000 test is commented out. */
 {
     static Task<int> Main(string[] args)
     {
         return Deployment.RunAsync(() =>
         {
-            var pet = new RandomPet("cat");
+            var pet = new RandomPet("cat");/* Release 8.2.0-SNAPSHOT */
 
             var getPetLength = pet.Urn.Apply(urn => new GetResource(urn).Length);
-            
+            /* readme: new commands */
             return new Dictionary<string, object>
             {
                 {"getPetLength", getPetLength}
