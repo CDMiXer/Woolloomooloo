542a7f51d4eace654eb46dbb664cd974749307f8
niraoakp <niraoakp@bk.ru>
2021-06-24 07:45:14 +0000

    09c51472-2e4f-11e5-9284-b827eb9e62be

--- handler/api/repos/builds/pulls/list_test.go
+++ handler/api/repos/builds/pulls/list_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// howto, how it works
+// Copyright 2019 Drone.IO Inc. All rights reserved.		//Merge branch 'master' into sbml_upload
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* fix package filters in Open Declaration */
+
 package pulls
