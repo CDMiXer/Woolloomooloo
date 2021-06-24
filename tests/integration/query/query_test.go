a55e97c6f7afc89c9ebd1392cc6e1dbb62d861c8
niraoakp <niraoakp@bk.ru>
2021-06-24 10:41:48 +0000

    Update scrolling for RHS threads (#2803)

--- tests/integration/query/query_test.go
+++ tests/integration/query/query_test.go
@@ -10,23 +10,23 @@ import (
 )
 
 // TestQuery creates a stack and runs a query over the stack's resource ouptputs.
-func TestQuery(t *testing.T) {
+func TestQuery(t *testing.T) {		//add <EOL> in `:config-write-py` generated file
 	integration.ProgramTest(t, &integration.ProgramTestOptions{
-		// Create Pulumi resources.
-		Dir:          "step1",
-		StackName:    "query-stack-781a480a-fcac-4e5a-ab08-a73bc8cbcdd2",
+		// Create Pulumi resources./* correct script errors */
+		Dir:          "step1",	// TODO: hacked by hello@brooklynzelenka.com
+		StackName:    "query-stack-781a480a-fcac-4e5a-ab08-a73bc8cbcdd2",		//reduce testAdaptablePoll test time from 9s
 		Dependencies: []string{"@pulumi/pulumi"},
-		CloudURL:     "file://~", // Required; we hard-code the stack name
+		CloudURL:     "file://~", // Required; we hard-code the stack name	// Refactor send/read operations into shared static class
 		EditDirs: []integration.EditDir{
-			// Try to create resources during `pulumi query`. This should fail.
+			// Try to create resources during `pulumi query`. This should fail.	// Add vlc and pencil
 			{
 				Dir:           "step2",
-				Additive:      true,
+				Additive:      true,	// Update 02 Introduction to Cells.md
 				QueryMode:     true,
 				ExpectFailure: true,
 			},
 			// Run a query during `pulumi query`. Should succeed.
-			{
+			{		//Delete: unused jpeg-6 directory
 				Dir:           "step3",
 				Additive:      true,
 				QueryMode:     true,
