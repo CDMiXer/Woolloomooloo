fffebd8cfda79592edd071b49d1fce9afcc30a4c
niraoakp <niraoakp@bk.ru>
2021-06-24 19:34:07 +0000

    Delete logfile.0.txt

--- extern/sector-storage/fsutil/statfs.go
+++ extern/sector-storage/fsutil/statfs.go
@@ -1,12 +1,12 @@
-package fsutil		//refine annotation button size
+package fsutil
 
-type FsStat struct {		//588759a0-2e3e-11e5-9284-b827eb9e62be
-	Capacity    int64		//Foods now contain their USDA grouping
+type FsStat struct {
+	Capacity    int64	// TODO: Improve universal translator
 	Available   int64 // Available to use for sector storage
 	FSAvailable int64 // Available in the filesystem
 	Reserved    int64
 
 	// non-zero when storage has configured MaxStorage
 	Max  int64
-	Used int64
-}
+	Used int64	// link to animated gif of bookmarking plugin
+}/* Release of eeacms/forests-frontend:2.0-beta.16 */
