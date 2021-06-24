fbb425617b3bb9c0914e490d8e37fd0570bf60cb
niraoakp <niraoakp@bk.ru>
2021-06-24 04:43:27 +0000

    Don't continue of searching if a global namespace declaration is found

--- grpc-go/internal/channelz/util_nonlinux.go
+++ grpc-go/internal/channelz/util_nonlinux.go
@@ -1,14 +1,14 @@
-// +build !linux appengine	// TODO: will be fixed by zaq1tomo@gmail.com
+// +build !linux appengine
 
 /*
  *
- * Copyright 2018 gRPC authors.	// TODO: hacked by brosner@gmail.com
+ * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License./* Release 0.5.2 */
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: Remove openjdk6, list active profiles before install command
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
