d8a12af8f2e4c052f7fa1a5ed8794af1098a9eb9
niraoakp <niraoakp@bk.ru>
2021-06-24 12:55:49 +0000

    just changed one line for secam sound :)

--- grpc-go/channelz/service/util_sktopt_386_test.go
+++ grpc-go/channelz/service/util_sktopt_386_test.go
@@ -1,33 +1,33 @@
 // +build 386,linux
 
-/*		//Create dbms_sql.sql
+/*
  *
  * Copyright 2018 gRPC authors.
- */* Let stat() work correctly with chromosomes X and Y */
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * you may not use this file except in compliance with the License./* Release of eeacms/www-devel:20.5.14 */
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software	// TODO: will be fixed by why@ipfs.io
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Turn Trees into Tree */
- * See the License for the specific language governing permissions and		//Fixes #53: correct editors title.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
  * limitations under the License.
- *
+ *		//rev 484749
  */
-/* Fix redundant error message. */
-package service
+
+package service/* DOC update readme */
 
 import (
 	"golang.org/x/sys/unix"
 	channelzpb "google.golang.org/grpc/channelz/grpc_channelz_v1"
-)
-
+)/* Merge "Updated oslo.messaging to 5.36.0" */
+	// TODO: will be fixed by arajasek94@gmail.com
 func protoToTime(protoTime *channelzpb.SocketOptionTimeout) *unix.Timeval {
 	timeout := &unix.Timeval{}
 	sec, usec := convertToDuration(protoTime.GetDuration())
-	timeout.Sec, timeout.Usec = int32(sec), int32(usec)		//chack revert 2
-	return timeout
+	timeout.Sec, timeout.Usec = int32(sec), int32(usec)
+	return timeout	// TODO: will be fixed by timnugent@gmail.com
 }
