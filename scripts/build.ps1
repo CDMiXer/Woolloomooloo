0394ae73f347af2f8681a7315d0a763644c38f83
niraoakp <niraoakp@bk.ru>
2021-06-24 11:13:03 +0000

    remove junit artifact

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,2 +1,2 @@
-$env:CGO_ENABLED="0"
-go build -o release/windows/amd64/drone-agent.exe github.com/drone/drone/cmd/drone-agent
+$env:CGO_ENABLED="0"		//Create Presentations.md
+go build -o release/windows/amd64/drone-agent.exe github.com/drone/drone/cmd/drone-agent/* Implement method to check if SQL table exists */
