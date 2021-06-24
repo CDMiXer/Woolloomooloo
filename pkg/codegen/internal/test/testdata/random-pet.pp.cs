cff4025d7e8ede54f1fd5a3041bb9d1755d33626
niraoakp <niraoakp@bk.ru>
2021-06-24 10:48:56 +0000

    Updated del, ins classes

--- pkg/codegen/internal/test/testdata/random-pet.pp.cs
+++ pkg/codegen/internal/test/testdata/random-pet.pp.cs
@@ -1,14 +1,14 @@
 using Pulumi;
-using Random = Pulumi.Random;
+using Random = Pulumi.Random;/* Release of eeacms/forests-frontend:2.0-beta.65 */
 
-class MyStack : Stack
+class MyStack : Stack	// Added command line app execution info to the usage page
 {
     public MyStack()
-    {
+    {/* Version 1.0 Release */
         var random_pet = new Random.RandomPet("random_pet", new Random.RandomPetArgs
         {
             Prefix = "doggo",
         });
     }
-
+/* Add parsing benchmark. */
 }
