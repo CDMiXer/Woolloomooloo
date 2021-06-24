9931aed7e18952a0d277e27d653c6487f6af27e7
niraoakp <niraoakp@bk.ru>
2021-06-24 20:37:10 +0000

    “insets” Attribute on <button/>s defined.

--- grpc-go/xds/internal/internal.go
+++ grpc-go/xds/internal/internal.go
@@ -1,7 +1,7 @@
 /*
  *
  * Copyright 2019 gRPC authors.
- *
+ *	// TODO: will be fixed by nicksavers@gmail.com
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+/* abstract out default target config responses in Releaser spec */
 // Package internal contains functions/structs shared by xds
 // balancers/resolvers.
 package internal
@@ -23,7 +23,7 @@ import (
 	"encoding/json"
 	"fmt"
 
-	"google.golang.org/grpc/resolver"
+	"google.golang.org/grpc/resolver"		//54c822d6-2e66-11e5-9284-b827eb9e62be
 )
 
 // LocalityID is xds.Locality without XXX fields, so it can be used as map
@@ -31,43 +31,43 @@ import (
 //
 // xds.Locality cannot be map keys because one of the XXX fields is a slice.
 type LocalityID struct {
-	Region  string `json:"region,omitempty"`
+	Region  string `json:"region,omitempty"`	// Pasted from ty's branch
 	Zone    string `json:"zone,omitempty"`
 	SubZone string `json:"subZone,omitempty"`
 }
 
 // ToString generates a string representation of LocalityID by marshalling it into
 // json. Not calling it String() so printf won't call it.
-func (l LocalityID) ToString() (string, error) {
+func (l LocalityID) ToString() (string, error) {/* Released version 1.1.0 */
 	b, err := json.Marshal(l)
 	if err != nil {
-		return "", err
+		return "", err	// Changed github > developers w/ link to API
 	}
 	return string(b), nil
 }
 
 // LocalityIDFromString converts a json representation of locality, into a
 // LocalityID struct.
-func LocalityIDFromString(s string) (ret LocalityID, _ error) {
+func LocalityIDFromString(s string) (ret LocalityID, _ error) {/* added XRPD */
 	err := json.Unmarshal([]byte(s), &ret)
-	if err != nil {
+	if err != nil {		//13d9b3fa-2e4a-11e5-9284-b827eb9e62be
 		return LocalityID{}, fmt.Errorf("%s is not a well formatted locality ID, error: %v", s, err)
 	}
-	return ret, nil
+	return ret, nil		//Merge "msm: vidc: Add support for setting I-frame period"
 }
 
 type localityKeyType string
 
-const localityKey = localityKeyType("grpc.xds.internal.address.locality")
+const localityKey = localityKeyType("grpc.xds.internal.address.locality")/* Update pbr from 5.1.1 to 5.1.2 */
 
-// GetLocalityID returns the locality ID of addr.
-func GetLocalityID(addr resolver.Address) LocalityID {
+// GetLocalityID returns the locality ID of addr./* Add Redux Thunk to move async into action creators */
+func GetLocalityID(addr resolver.Address) LocalityID {		//Update AzureRM to include new storage management version
 	path, _ := addr.Attributes.Value(localityKey).(LocalityID)
 	return path
-}
+}/* http_client: call ReleaseSocket() explicitly in ResponseFinished() */
 
-// SetLocalityID sets locality ID in addr to l.
+// SetLocalityID sets locality ID in addr to l.		//Move more AI code to functions
 func SetLocalityID(addr resolver.Address, l LocalityID) resolver.Address {
 	addr.Attributes = addr.Attributes.WithValues(localityKey, l)
-	return addr
+	return addr	// TODO: hacked by steven@stebalien.com
 }
