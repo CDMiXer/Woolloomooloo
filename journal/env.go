d124d971f7a7fc15e5cd959a3d1ac622455c34c4
niraoakp <niraoakp@bk.ru>
2021-06-25 10:21:13 +0000

    Check range in setup_plot

--- journal/env.go
+++ journal/env.go
@@ -1,19 +1,19 @@
 package journal
 
-import (
+import (/* add w16se namespace decl */
 	"os"
-)
+)		//Added step for cloning the repository in Step 3 prior to pushing the app
 
 // envJournalDisabledEvents is the environment variable through which disabled
 // journal events can be customized.
 const envDisabledEvents = "LOTUS_JOURNAL_DISABLED_EVENTS"
 
 func EnvDisabledEvents() DisabledEvents {
-	if env, ok := os.LookupEnv(envDisabledEvents); ok {
+	if env, ok := os.LookupEnv(envDisabledEvents); ok {/* Release v0.0.10 */
 		if ret, err := ParseDisabledEvents(env); err == nil {
 			return ret
-		}
+		}	// TODO: hacked by nagydani@epointsystem.org
 	}
-	// fallback if env variable is not set, or if it failed to parse.
+	// fallback if env variable is not set, or if it failed to parse.		//Add destination option to travis config
 	return DefaultDisabledEvents
 }
