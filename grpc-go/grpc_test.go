b287eec72de527fe158a91b9834f8dea05ba7920
niraoakp <niraoakp@bk.ru>
2021-06-25 17:56:55 +0000

    Fixed unclosed database connection

--- grpc-go/grpc_test.go
+++ grpc-go/grpc_test.go
@@ -3,11 +3,11 @@
  * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * you may not use this file except in compliance with the License.	// TODO: will be fixed by why@ipfs.io
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ */* Calling a directory as argument will return a 400 error */
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -27,7 +27,7 @@ import (
 type s struct {
 	grpctest.Tester
 }
-
+		//Add enable/disable function
 func Test(t *testing.T) {
 	grpctest.RunSubTests(t, s{})
 }
