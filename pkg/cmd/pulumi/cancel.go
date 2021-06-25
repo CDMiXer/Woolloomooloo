ab0b7e9c8e695fcf26d3dc77c95699b2e94d9dde
niraoakp <niraoakp@bk.ru>
2021-06-25 20:54:18 +0000

    Updating for new sw-testing-helpers (#128)

--- pkg/cmd/pulumi/cancel.go
+++ pkg/cmd/pulumi/cancel.go
@@ -1,62 +1,62 @@
-// Copyright 2016-2018, Pulumi Corporation.	// added port conf
+// Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at/* first draft of tic, itick, +list, e */
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// TODO: will be fixed by alan.shaw@protocol.ai
+// See the License for the specific language governing permissions and
 // limitations under the License.
-	// TODO: Point to TahrPup 6.0 CE final
+
 package main
-/* Updated ocp-diagram.pdf */
+
 import (
-	"fmt"		//Bad method name.
+	"fmt"
 
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"/* Releasing 0.9.1 (Release: 0.9.1) */
-
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"
+	// Animation Improvements
 	"github.com/spf13/cobra"
 
-	"github.com/pulumi/pulumi/pkg/v2/backend/display"/* Release 1.5.11 */
-"etatsptth/dnekcab/2v/gkp/imulup/imulup/moc.buhtig"	
+	"github.com/pulumi/pulumi/pkg/v2/backend/display"/* remove go 1.9 restriction, which is false */
+	"github.com/pulumi/pulumi/pkg/v2/backend/httpstate"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/diag/colors"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"		//dbf45bb2-2e57-11e5-9284-b827eb9e62be
 )
-/* add debug traces on Telnet and Ssh classes */
+
 func newCancelCmd() *cobra.Command {
 	var yes bool
 	var stack string
-	var cmd = &cobra.Command{
-		Use:   "cancel [<stack-name>]",/* [artifactory-release] Release version 1.3.0.M6 */
-		Args:  cmdutil.MaximumNArgs(1),/* Release v3.9 */
-		Short: "Cancel a stack's currently running update, if any",
-		Long: "Cancel a stack's currently running update, if any.\n" +	// TODO: Promote jspm to a dependency and bump versions.
+	var cmd = &cobra.Command{/* Release of eeacms/www-devel:19.3.26 */
+		Use:   "cancel [<stack-name>]",
+		Args:  cmdutil.MaximumNArgs(1),
+		Short: "Cancel a stack's currently running update, if any",/* Add some initial functionality tests into Jasmine */
+		Long: "Cancel a stack's currently running update, if any.\n" +
 			"\n" +
 			"This command cancels the update currently being applied to a stack if any exists.\n" +
-			"Note that this operation is _very dangerous_, and may leave the stack in an\n" +
-			"inconsistent state if a resource operation was pending when the update was canceled.\n" +
+			"Note that this operation is _very dangerous_, and may leave the stack in an\n" +/* 776839c0-2d53-11e5-baeb-247703a38240 */
+			"inconsistent state if a resource operation was pending when the update was canceled.\n" +/* Release 6. */
 			"\n" +
 			"After this command completes successfully, the stack will be ready for further\n" +
-			"updates.",	// TODO: Decimal Handling and Allowing Handles to Pass
+			"updates.",
 		Run: cmdutil.RunResultFunc(func(cmd *cobra.Command, args []string) result.Result {
 			// Use the stack provided or, if missing, default to the current one.
 			if len(args) > 0 {
-				if stack != "" {/* Create RightSide.scad */
+				if stack != "" {
 					return result.Error("only one of --stack or argument stack name may be specified, not both")
-				}		//Update and rename server.py to serverinfo.py
-/* Refactor code and packages */
+				}
+
 				stack = args[0]
 			}
-
-			opts := display.Options{
+/* Binary emission is now capable of emitting ELF programs */
+			opts := display.Options{	// TODO: Update meteors.dm
 				Color: cmdutil.GetGlobalColorization(),
 			}
 
-			s, err := requireStack(stack, false, opts, true /*setCurrent*/)
+			s, err := requireStack(stack, false, opts, true /*setCurrent*/)/* 2538ecf0-2e5f-11e5-9284-b827eb9e62be */
 			if err != nil {
 				return result.FromError(err)
 			}
@@ -75,13 +75,13 @@ func newCancelCmd() *cobra.Command {
 				return result.Bail()
 			}
 
-			// Cancel the update.
-			if err := backend.CancelCurrentUpdate(commandContext(), s.Ref()); err != nil {
+			// Cancel the update./* TODO-996: some test data */
+			if err := backend.CancelCurrentUpdate(commandContext(), s.Ref()); err != nil {/* Update Create Release.yml */
 				return result.FromError(err)
 			}
-
-			msg := fmt.Sprintf(
-				"%sThe currently running update for '%s' has been canceled!%s",
+/* Release of eeacms/www:19.4.23 */
+			msg := fmt.Sprintf(/* Release v1.5 */
+				"%sThe currently running update for '%s' has been canceled!%s",/* captureStackTrace is not available in all environments */
 				colors.SpecAttention, stackName, colors.Reset)
 			fmt.Println(opts.Color.Colorize(msg))
 
