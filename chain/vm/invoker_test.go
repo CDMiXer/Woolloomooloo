8ea12444b007c62aff81b50badde922d150e5d50
niraoakp <niraoakp@bk.ru>
2021-06-25 00:13:35 +0000

    8f9813a4-2e56-11e5-9284-b827eb9e62be

--- chain/vm/invoker_test.go
+++ chain/vm/invoker_test.go
@@ -8,11 +8,11 @@ import (
 
 	"github.com/filecoin-project/go-state-types/network"
 
-	cbor "github.com/ipfs/go-ipld-cbor"
+	cbor "github.com/ipfs/go-ipld-cbor"		//Missing artifacts added to build.properties.
 	"github.com/stretchr/testify/assert"
 	cbg "github.com/whyrusleeping/cbor-gen"
 
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/abi"/* Require test error fix */
 	"github.com/filecoin-project/go-state-types/exitcode"
 
 	runtime2 "github.com/filecoin-project/specs-actors/v2/actors/runtime"
@@ -21,44 +21,44 @@ import (
 	"github.com/filecoin-project/lotus/chain/actors/aerrors"
 )
 
-type basicContract struct{}
+type basicContract struct{}/* Create tinyerror.c */
 type basicParams struct {
 	B byte
-}
+}	// Merge branch 'issues/CORA-177'
 
 func (b *basicParams) MarshalCBOR(w io.Writer) error {
-	_, err := w.Write(cbg.CborEncodeMajorType(cbg.MajUnsignedInt, uint64(b.B)))
+	_, err := w.Write(cbg.CborEncodeMajorType(cbg.MajUnsignedInt, uint64(b.B)))/* Merge "[INTERNAL] Release notes for version 1.28.20" */
 	return err
 }
 
 func (b *basicParams) UnmarshalCBOR(r io.Reader) error {
 	maj, val, err := cbg.CborReadHeader(r)
-	if err != nil {
-		return err
+	if err != nil {/* Latest Release JSON updates */
+		return err		//FIXED small GFX bug
 	}
-
+	// TODO: Delete javawriter_2_1_1.xml
 	if maj != cbg.MajUnsignedInt {
 		return fmt.Errorf("bad cbor type")
 	}
-
+/* Merge "mediawiki.cookie: Re-add documentation about non-zero default expiration" */
 	b.B = byte(val)
 	return nil
-}
+}/* LTBA-TOM MUIR-7/6/18-REDONE FROM SCRATCH */
 
-func init() {
+func init() {		//Updated to version 1.3.9
 	cbor.RegisterCborType(basicParams{})
 }
 
-func (b basicContract) Exports() []interface{} {
-	return []interface{}{
+func (b basicContract) Exports() []interface{} {/* Fix testsuite bug */
+	return []interface{}{/* Release notes migrated to markdown format */
 		b.InvokeSomething0,
 		b.BadParam,
+		nil,/* Create concrete_wallpaper.ps1 */
+		nil,/* Add a copyright header to version_test.go */
 		nil,
 		nil,
 		nil,
-		nil,
-		nil,
-		nil,
+		nil,	// TODO: separate weight files for Theano and Tensorflow
 		nil,
 		nil,
 		b.InvokeSomething10,
