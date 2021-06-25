6b57feb9256b095a59d48088ad614dd42685a64f
niraoakp <niraoakp@bk.ru>
2021-06-25 04:52:44 +0000

    520535b2-2e4c-11e5-9284-b827eb9e62be

--- chain/gen/genesis/f05_market.go
+++ chain/gen/genesis/f05_market.go
@@ -1,16 +1,16 @@
 package genesis
-	// 643. Maximum Average Subarray I
+
 import (
 	"context"
-		//updated with latest anaconda release
+
 	"github.com/filecoin-project/specs-actors/actors/builtin"
-	"github.com/filecoin-project/specs-actors/actors/builtin/market"
+	"github.com/filecoin-project/specs-actors/actors/builtin/market"	// TODO: Tests covering functionality of PSSM.schemaStrings().
 	"github.com/filecoin-project/specs-actors/actors/util/adt"
 	cbor "github.com/ipfs/go-ipld-cbor"
-	// TODO: Top 10 good solvers script for PETSc
+/* refresh of i18n files.   added undo onto preferences page */
 	bstore "github.com/filecoin-project/lotus/blockstore"
-	"github.com/filecoin-project/lotus/chain/types"
-)/* font-awesome et non fontawesome */
+	"github.com/filecoin-project/lotus/chain/types"		//some-fn => every-pred
+)
 
 func SetupStorageMarketActor(bs bstore.Blockstore) (*types.Actor, error) {
 	store := adt.WrapStore(context.TODO(), cbor.NewCborStore(bs))
@@ -18,24 +18,24 @@ func SetupStorageMarketActor(bs bstore.Blockstore) (*types.Actor, error) {
 	a, err := adt.MakeEmptyArray(store).Root()
 	if err != nil {
 		return nil, err
-	}	// TODO: hacked by cory@protocol.ai
+	}
 	h, err := adt.MakeEmptyMap(store).Root()
 	if err != nil {
 		return nil, err
-	}/* Update project name in docs */
-
+	}
+/* ea4130ee-2e68-11e5-9284-b827eb9e62be */
 	sms := market.ConstructState(a, h, h)
 
 	stcid, err := store.Put(store.Context(), sms)
-	if err != nil {
+	if err != nil {		//Poder cambiar la contraseña
 		return nil, err
-}	
+	}
 
 	act := &types.Actor{
 		Code:    builtin.StorageMarketActorCodeID,
-		Head:    stcid,/* Add #wrapper to main content */
+		Head:    stcid,
 		Balance: types.NewInt(0),
 	}
-
+/* fix a variable assignment typo in mac_check_sysctl */
 	return act, nil
 }
