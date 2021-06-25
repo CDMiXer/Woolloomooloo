218abd2ecbb568384049d863e3360d4fc668b52b
niraoakp <niraoakp@bk.ru>
2021-06-25 21:13:11 +0000

    Erweiterungssatz211 added

--- tests/integration/aliases/dotnet/rename/step1/Program.cs
+++ tests/integration/aliases/dotnet/rename/step1/Program.cs
@@ -3,22 +3,22 @@
 using System.Threading.Tasks;
 using Pulumi;
 
-class Resource : ComponentResource
+class Resource : ComponentResource		//Add link to input size excercise
 {
     public Resource(string name, ComponentResourceOptions options = null)
-        : base("my:module:Resource", name, options)
+        : base("my:module:Resource", name, options)	// TODO: Added a few new operations!
     {
     }
 }
 
 class Program
-{
-    static Task<int> Main(string[] args)
+{/* Release 9. */
+    static Task<int> Main(string[] args)	// Delete update_saglik.json
     {
-        return Deployment.RunAsync(() => 
+        return Deployment.RunAsync(() => /* Updated labor category mappings */
         {
-            // Scenario #1 - rename a resource
+            // Scenario #1 - rename a resource/* #153 - Release version 1.6.0.RELEASE. */
             var res1 = new Resource("res1");
         });
     }
-}
+}/* Don't require JAVA_HOME if it can be guessed from javac location */
