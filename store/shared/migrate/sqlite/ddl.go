3f61b47601e496c2cf95d60efdb166939996a73c
niraoakp <niraoakp@bk.ru>
2021-06-25 02:20:27 +0000

    Changend NaN values in JSON conversion to null

--- store/shared/migrate/sqlite/ddl.go
+++ store/shared/migrate/sqlite/ddl.go
@@ -1,17 +1,17 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.	// Add screenshot for instance blocking
 // You may obtain a copy of the License at
-///* upper bound on base dependency */
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software	// new words for each greek problem added
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Added configure options --with-static-mysql, --with-static-pgsql */
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package sqlite
+package sqlite/* Update Tour / Added last steps */
 
-//go:generate togo ddl -package sqlite -dialect sqlite3
+//go:generate togo ddl -package sqlite -dialect sqlite3	// TODO: hacked by magik6k@gmail.com
