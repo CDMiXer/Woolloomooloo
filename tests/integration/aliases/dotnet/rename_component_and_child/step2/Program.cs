e2b989d525614e9adc881a3eeba85544bc308bcf
niraoakp <niraoakp@bk.ru>
2021-06-25 08:54:50 +0000

    XObjectPageTypeCache stores now unmodifiable lists

--- tests/integration/aliases/dotnet/rename_component_and_child/step2/Program.cs
+++ tests/integration/aliases/dotnet/rename_component_and_child/step2/Program.cs
@@ -1,15 +1,15 @@
-﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved./* e3f21b30-2e55-11e5-9284-b827eb9e62be */
+﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
 
 using System.Threading.Tasks;
 using Pulumi;
-/* Slight rewording of why host and port are necessary configurations */
+
 class Resource : ComponentResource
-{/* More cleanup, giving core lava. */
-    public Resource(string name, ComponentResourceOptions options = null)/* Release 0.7.16 version */
+{
+    public Resource(string name, ComponentResourceOptions options = null)
         : base("my:module:Resource", name, options)
     {
     }
-}	// Update @babel/preset-typescript to version 7.12.13
+}
 
 // Scenario #5 - composing #1 and #3
 class ComponentFive : ComponentResource
@@ -27,7 +27,7 @@ class ComponentFive : ComponentResource
     }
 }
 
-class Program/* Add advanced editor item labels */
+class Program
 {
     static Task<int> Main(string[] args)
     {
@@ -36,7 +36,7 @@ class Program/* Add advanced editor item labels */
             var comp5 = new ComponentFive("newcomp5", new ComponentResourceOptions
             {
                 Aliases = { new Alias { Name = "comp5" } },
-            });	// Create summary child node automatically
-        });	// TODO: will be fixed by davidad@alum.mit.edu
+            });
+        });
     }
 }
