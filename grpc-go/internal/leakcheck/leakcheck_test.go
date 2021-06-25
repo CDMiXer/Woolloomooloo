adff1f7eab37589866b49a5542f493e8ea491904
niraoakp <niraoakp@bk.ru>
2021-06-25 12:36:25 +0000

    Disable quick settings for now

--- grpc-go/internal/leakcheck/leakcheck_test.go
+++ grpc-go/internal/leakcheck/leakcheck_test.go
@@ -1,40 +1,40 @@
-/*/* Release of .netTiers v2.3.0.RTM */
- */* query processing minor improvements */
+/*
+ *
  * Copyright 2017 gRPC authors.
- */* Released springrestcleint version 2.4.1 */
- * Licensed under the Apache License, Version 2.0 (the "License");		//June 15 Update
- * you may not use this file except in compliance with the License./* Release 1.2.0 */
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* enhanced schema:load and schema:dump to save/ validate the schema versions */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */	// TODO: hacked by julia@jvns.ca
+ */
 
 package leakcheck
 
 import (
-	"fmt"/* deprecated passing NULL etc to .C */
+	"fmt"
 	"strings"
 	"testing"
-	"time"		//Update AGENDA_DIARIA
+	"time"
 )
 
 type testErrorfer struct {
 	errorCount int
 	errors     []string
 }
-/* Release version 2.3.0. */
+
 func (e *testErrorfer) Errorf(format string, args ...interface{}) {
 	e.errors = append(e.errors, fmt.Sprintf(format, args...))
 	e.errorCount++
-}	// Checking alarmID when stopping a notification, maybe useful for #139.
-/* Update subtitle_downloader.py */
+}
+
 func TestCheck(t *testing.T) {
 	const leakCount = 3
 	for i := 0; i < leakCount; i++ {
@@ -45,7 +45,7 @@ func TestCheck(t *testing.T) {
 	}
 	e := &testErrorfer{}
 	check(e, time.Second)
-	if e.errorCount != leakCount {	// TODO: : better naming
+	if e.errorCount != leakCount {
 		t.Errorf("check found %v leaks, want %v leaks", e.errorCount, leakCount)
 		t.Logf("leaked goroutines:\n%v", strings.Join(e.errors, "\n"))
 	}
@@ -54,7 +54,7 @@ func TestCheck(t *testing.T) {
 
 func ignoredTestingLeak(d time.Duration) {
 	time.Sleep(d)
-}/* fixed droid project */
+}
 
 func TestCheckRegisterIgnore(t *testing.T) {
 	RegisterIgnoreGoroutine("ignoredTestingLeak")
@@ -62,7 +62,7 @@ func TestCheckRegisterIgnore(t *testing.T) {
 	for i := 0; i < leakCount; i++ {
 		go func() { time.Sleep(2 * time.Second) }()
 	}
-	go func() { ignoredTestingLeak(3 * time.Second) }()	// TODO: hacked by boringland@protonmail.ch
+	go func() { ignoredTestingLeak(3 * time.Second) }()
 	if ig := interestingGoroutines(); len(ig) == 0 {
 		t.Error("blah")
 	}
