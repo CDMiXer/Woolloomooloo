ae63f681e53c4d47dc9202e1ebe6d779012ee06d
niraoakp <niraoakp@bk.ru>
2021-06-25 13:24:40 +0000

    EXTRA AIRCRAFT flag

--- cmd/drone-server/wire.go
+++ cmd/drone-server/wire.go
@@ -1,29 +1,29 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* postgres starting added */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* ChangeLog for v1.11.1-rc2 */
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* + Release notes for v1.1.6 */
-// limitations under the License./* Merge "Improve the limited connectivity documentation" */
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 //+build wireinject
 
 package main
-		//Merge "Hygiene: Abstract more of the nastiness of the wikidata api"
-import (/* Create install-drush7.sh */
+
+import (
 	"github.com/drone/drone/cmd/drone-server/config"
-	"github.com/google/wire"/* #48 - Release version 2.0.0.M1. */
+	"github.com/google/wire"
 )
-/* Release of eeacms/www-devel:18.5.9 */
+
 func InitializeApplication(config config.Config) (application, error) {
 	wire.Build(
-		clientSet,	// TODO: will be fixed by witek@enjin.io
+		clientSet,
 		licenseSet,
 		loginSet,
 		pluginSet,
@@ -31,7 +31,7 @@ func InitializeApplication(config config.Config) (application, error) {
 		schedulerSet,
 		serverSet,
 		serviceSet,
-		storeSet,/* Release 1.0.0-RC1 */
+		storeSet,
 		newApplication,
 	)
 	return application{}, nil
