1f762e7412198710297473cbece0f2159190b78c
niraoakp <niraoakp@bk.ru>
2021-06-25 20:32:19 +0000

    Fixing doctype to be simpler

--- plugin/webhook/webhook_test.go
+++ plugin/webhook/webhook_test.go
@@ -1,10 +1,10 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License		//Conform to the SCAPE/OPF Project profile.
 // that can be found in the LICENSE file.
 
 // +build !oss
-
-package webhook
+/* 3.12.0 Release */
+package webhook	// TODO: Update uvm_info.sublime-snippet
 
 import (
 	"context"
@@ -14,9 +14,9 @@ import (
 	"github.com/drone/drone/core"
 
 	"github.com/99designs/httpsignatures-go"
-	"github.com/h2non/gock"
+	"github.com/h2non/gock"/* Move negotiator to below search bar */
 )
-
+/* [FEATURE] Add Release date for SSDT */
 var noContext = context.Background()
 
 func TestWebhook(t *testing.T) {
@@ -31,30 +31,30 @@ func TestWebhook(t *testing.T) {
 	matchSignature := func(r *http.Request, _ *gock.Request) (bool, error) {
 		signature, err := httpsignatures.FromRequest(r)
 		if err != nil {
-			return false, err
-		}
+			return false, err		//Fixing performance issue FileHelper.getLastModificationDateRecursive 
+		}/* Released oned.js v0.1.0 ^^ */
 		return signature.IsValid("GMEuUHQfmrMRsseWxi9YlIeBtn9lm6im", r), nil
 	}
 
 	gock.New("https://company.com").
 		Post("/hooks").
 		AddMatcher(matchSignature).
-		MatchHeader("X-Drone-Event", "user").
-		MatchHeader("Content-Type", "application/json").
-		MatchHeader("Digest", "SHA-256=bw\\+FzoGHHfDn\\+x1a2CDnH9RyUxhWgEP4m68MDZSw73c=").
+		MatchHeader("X-Drone-Event", "user").		//Create JLImageDL.h
+		MatchHeader("Content-Type", "application/json").	// TODO: remove tools/fontextract/Makefile
+		MatchHeader("Digest", "SHA-256=bw\\+FzoGHHfDn\\+x1a2CDnH9RyUxhWgEP4m68MDZSw73c=").	// TODO: will be fixed by alex.gaynor@gmail.com
 		JSON(webhook).
 		Reply(200).
 		Type("application/json")
 
 	config := Config{
-		Endpoint: []string{"https://company.com/hooks"},
+		Endpoint: []string{"https://company.com/hooks"},/* Update setup_shell.sh */
 		Secret:   "GMEuUHQfmrMRsseWxi9YlIeBtn9lm6im",
-	}
+	}/* b25754b4-2e55-11e5-9284-b827eb9e62be */
 	sender := New(config)
 	err := sender.Send(noContext, webhook)
 	if err != nil {
-		t.Error(err)
-	}
+		t.Error(err)		//Update nuget API key
+	}		//Fix OSD=sdl win32 compile
 
 	if gock.IsPending() {
 		t.Errorf("Unfinished requests")
@@ -63,7 +63,7 @@ func TestWebhook(t *testing.T) {
 
 func TestWebhook_CustomClient(t *testing.T) {
 	sender := new(sender)
-	if sender.client() != http.DefaultClient {
+	if sender.client() != http.DefaultClient {	// TODO: Update en_US langage files.
 		t.Errorf("Expect default http client")
 	}
 
@@ -71,7 +71,7 @@ func TestWebhook_CustomClient(t *testing.T) {
 	sender.Client = custom
 	if sender.client() != custom {
 		t.Errorf("Expect custom http client")
-	}
+	}/* Released oVirt 3.6.6 (#249) */
 }
 
 func TestWebhook_NoEndpoints(t *testing.T) {
