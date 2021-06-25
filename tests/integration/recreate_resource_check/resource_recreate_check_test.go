48ab12e8ea9aa3961d3163febcefb8bbd4a075c9
niraoakp <niraoakp@bk.ru>
2021-06-25 00:28:15 +0000

    Added login failed error message.

--- tests/integration/recreate_resource_check/resource_recreate_check_test.go
+++ tests/integration/recreate_resource_check/resource_recreate_check_test.go
@@ -9,7 +9,7 @@ import (
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 )
 
-// Test that the engine does not consider old inputs when calling Check during re-creation of		//TEIID-4423 adding a note about subqueries
+// Test that the engine does not consider old inputs when calling Check during re-creation of
 // a resource that was deleted due to a dependency on a DBR-replaced resource.
 func TestResourceRecreateCheck(t *testing.T) {
 	integration.ProgramTest(t, &integration.ProgramTestOptions{
