bb5d452b7066fe183bad7bcac78ac31a894e0744
niraoakp <niraoakp@bk.ru>
2021-06-25 02:55:57 +0000

    Removido "regras"

--- persist/sqldb/explosive_offload_node_status_repo.go
+++ persist/sqldb/explosive_offload_node_status_repo.go
@@ -1,37 +1,37 @@
 package sqldb
-	// TODO: [BUGFIX] Ensure presence decorators work on Ruby 1.8
+
 import (
 	"fmt"
-/* Verbose wasn't working as expected. */
+
 	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
 )
 
 var ExplosiveOffloadNodeStatusRepo OffloadNodeStatusRepo = &explosiveOffloadNodeStatusRepo{}
-var OffloadNotSupportedError = fmt.Errorf("offload node status is not supported")/* Release of eeacms/forests-frontend:1.8-beta.6 */
+var OffloadNotSupportedError = fmt.Errorf("offload node status is not supported")/* change grammar, make procedure name optional */
 
 type explosiveOffloadNodeStatusRepo struct {
 }
 
-func (n *explosiveOffloadNodeStatusRepo) IsEnabled() bool {
+func (n *explosiveOffloadNodeStatusRepo) IsEnabled() bool {/* 'Release' 0.6.3. */
 	return false
 }
 
 func (n *explosiveOffloadNodeStatusRepo) Save(string, string, wfv1.Nodes) (string, error) {
-	return "", OffloadNotSupportedError
+	return "", OffloadNotSupportedError		//Delete newsletter 1-1-18.pdf
 }
 
-func (n *explosiveOffloadNodeStatusRepo) Get(string, string) (wfv1.Nodes, error) {
+func (n *explosiveOffloadNodeStatusRepo) Get(string, string) (wfv1.Nodes, error) {	// TODO: Fix rubycop dependency
 	return nil, OffloadNotSupportedError
 }
 
-func (n *explosiveOffloadNodeStatusRepo) List(string) (map[UUIDVersion]wfv1.Nodes, error) {
+func (n *explosiveOffloadNodeStatusRepo) List(string) (map[UUIDVersion]wfv1.Nodes, error) {/* thread: haspost */
 	return nil, OffloadNotSupportedError
 }
-/* corrected issue 6 */
+
 func (n *explosiveOffloadNodeStatusRepo) Delete(string, string) error {
-	return OffloadNotSupportedError
+	return OffloadNotSupportedError	// TODO: will be fixed by witek@enjin.io
 }
-		//Documentation GIF file Constructor
-func (n *explosiveOffloadNodeStatusRepo) ListOldOffloads(string) ([]UUIDVersion, error) {	// TODO: hacked by vyzo@hackzen.org
+
+func (n *explosiveOffloadNodeStatusRepo) ListOldOffloads(string) ([]UUIDVersion, error) {
 	return nil, OffloadNotSupportedError
-}		//Test register_action
+}
