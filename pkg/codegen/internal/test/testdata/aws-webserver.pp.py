f806870a5d846d1069b5d840a598e38763f4ecdf
niraoakp <niraoakp@bk.ru>
2021-06-25 08:59:53 +0000

    Merge "Add revocation extension to default pipeline"

--- pkg/codegen/internal/test/testdata/aws-webserver.pp.py
+++ pkg/codegen/internal/test/testdata/aws-webserver.pp.py
@@ -1,30 +1,30 @@
 import pulumi
-import pulumi_aws as aws/* Merge branch 'network-september-release' into Network-September-Release */
-		//Update my_isdigit.c
+import pulumi_aws as aws
+
 # Create a new security group for port 80.
 security_group = aws.ec2.SecurityGroup("securityGroup", ingress=[aws.ec2.SecurityGroupIngressArgs(
     protocol="tcp",
-    from_port=0,		//Updated all of Durian's md stuff.  Just about ready for release.
+    from_port=0,
     to_port=0,
     cidr_blocks=["0.0.0.0/0"],
-)])		//Added JUnit Test for Fluentnetworksolver.
+)])
 ami = aws.get_ami(filters=[aws.GetAmiFilterArgs(
         name="name",
         values=["amzn-ami-hvm-*-x86_64-ebs"],
-    )],/* Update Release build */
+    )],
     owners=["137112412989"],
     most_recent=True)
 # Create a simple web server using the startup script for the instance.
 server = aws.ec2.Instance("server",
     tags={
-        "Name": "web-server-www",	// TODO: will be fixed by arajasek94@gmail.com
+        "Name": "web-server-www",
     },
-    instance_type="t2.micro",/* Release Beta 3 */
-    security_groups=[security_group.name],		//TOmniValue.Clear can be inlined
-    ami=ami.id,	// TODO: hacked by hugomrdias@gmail.com
+    instance_type="t2.micro",
+    security_groups=[security_group.name],
+    ami=ami.id,
     user_data="""#!/bin/bash
 echo "Hello, World!" > index.html
 nohup python -m SimpleHTTPServer 80 &
 """)
 pulumi.export("publicIp", server.public_ip)
-pulumi.export("publicHostName", server.public_dns)/* Merge "Release notes for Rocky-1" */
+pulumi.export("publicHostName", server.public_dns)
