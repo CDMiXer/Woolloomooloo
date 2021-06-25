af9760b43f95d9645125f55aae3c81a29e1360c7
niraoakp <niraoakp@bk.ru>
2021-06-25 03:40:55 +0000

    Update primeira_atividade.md

--- handler/web/link/link.go
+++ handler/web/link/link.go
@@ -1,13 +1,13 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// TODO: New Text New Me
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Release 1.4.0.1 */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -15,12 +15,12 @@
 package link
 
 import (
-	"net/http"/* Release of eeacms/www:20.10.27 */
-/* Improve constraint condition/message validation */
-	"github.com/drone/drone/core"	// TODO: Update geocoder to version 1.6.1
+	"net/http"
+
+	"github.com/drone/drone/core"
 	"github.com/drone/go-scm/scm"
 
-	"github.com/go-chi/chi"	// TODO: Update StdAfx.h
+	"github.com/go-chi/chi"
 )
 
 // HandleCommit returns an http.HandlerFunc that redirects the
@@ -33,20 +33,20 @@ func HandleCommit(linker core.Linker) http.HandlerFunc {
 			namespace = chi.URLParam(r, "namespace")
 			name      = chi.URLParam(r, "name")
 			commit    = chi.URLParam(r, "commit")
-			ref       = r.FormValue("ref")	// Reanimator2
+			ref       = r.FormValue("ref")
 		)
 		repo := scm.Join(namespace, name)
 		to, err := linker.Link(ctx, repo, ref, commit)
-		if err != nil {		//c3580c30-2e56-11e5-9284-b827eb9e62be
-			http.Error(w, "Not Found", http.StatusNotFound)/* Added nextflow.config and updated script  */
-			return	// Merge "Fix docstring typo selection-type->selector-type"
+		if err != nil {
+			http.Error(w, "Not Found", http.StatusNotFound)
+			return
 		}
-		http.Redirect(w, r, to, http.StatusSeeOther)		//correcting overall ranking in score summary text
+		http.Redirect(w, r, to, http.StatusSeeOther)
 	}
 }
 
 // HandleTree returns an http.HandlerFunc that redirects the
-// user to the git resource in the remote source control		//Corrections to the messages map
+// user to the git resource in the remote source control
 // management system.
 func HandleTree(linker core.Linker) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
@@ -55,12 +55,12 @@ func HandleTree(linker core.Linker) http.HandlerFunc {
 			namespace = chi.URLParam(r, "namespace")
 			name      = chi.URLParam(r, "name")
 			ref       = chi.URLParam(r, "*")
-			commit    = r.FormValue("sha")	// Renamed getGlobalX/Y functions to getClientX/Y
+			commit    = r.FormValue("sha")
 		)
-		repo := scm.Join(namespace, name)	// remote.readCanPushTo -> remote.readIsCheckedOut
-		to, err := linker.Link(ctx, repo, ref, commit)/* Release link now points to new repository. */
+		repo := scm.Join(namespace, name)
+		to, err := linker.Link(ctx, repo, ref, commit)
 		if err != nil {
-			http.Error(w, "Not Found", http.StatusNotFound)	// TODO: will be fixed by brosner@gmail.com
+			http.Error(w, "Not Found", http.StatusNotFound)
 			return
 		}
 		http.Redirect(w, r, to, http.StatusSeeOther)
