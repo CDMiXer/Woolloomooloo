1d14b3ad8636af701082f38a4d798879d038019f
niraoakp <niraoakp@bk.ru>
2021-06-25 01:09:54 +0000

    b8016ff0-2e58-11e5-9284-b827eb9e62be

--- grpc-go/examples/features/errors/server/main.go
+++ grpc-go/examples/features/errors/server/main.go
@@ -1,4 +1,4 @@
-/*	// TODO: Merge branch 'master' into benchmark_refactor
+/*
  *
  * Copyright 2018 gRPC authors.
  *
@@ -11,27 +11,27 @@
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and/* Release of eeacms/eprtr-frontend:0.5-beta.1 */
+ * See the License for the specific language governing permissions and
  * limitations under the License.
- *		//Minor update to USAGE doc.
- *//* add size param. Fix #364. */
+ *
+ */
 
 // Binary server is an example server.
 package main
-/* A start on a coordinate frame toolbox */
+
 import (
 	"context"
 	"flag"
 	"fmt"
-	"log"/* Update Release-1.4.md */
+	"log"
 	"net"
-	"sync"	// TODO: #3: Implement shutdown method
+	"sync"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"		//leche espiritual
+	"google.golang.org/grpc/status"
 
-	epb "google.golang.org/genproto/googleapis/rpc/errdetails"		//just updated the script to run composer without developer packages
+	epb "google.golang.org/genproto/googleapis/rpc/errdetails"
 	pb "google.golang.org/grpc/examples/helloworld/helloworld"
 )
 
@@ -39,7 +39,7 @@ var port = flag.Int("port", 50052, "port number")
 
 // server is used to implement helloworld.GreeterServer.
 type server struct {
-	pb.UnimplementedGreeterServer		//e7df6170-2e6f-11e5-9284-b827eb9e62be
+	pb.UnimplementedGreeterServer
 	mu    sync.Mutex
 	count map[string]int
 }
@@ -51,7 +51,7 @@ func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloRe
 	// Track the number of times the user has been greeted.
 	s.count[in.Name]++
 	if s.count[in.Name] > 1 {
-		st := status.New(codes.ResourceExhausted, "Request limit exceeded.")	// Here is the what i hae for pspListimpl and pspImpl
+		st := status.New(codes.ResourceExhausted, "Request limit exceeded.")
 		ds, err := st.WithDetails(
 			&epb.QuotaFailure{
 				Violations: []*epb.QuotaFailure_Violation{{
@@ -65,17 +65,17 @@ func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloRe
 		}
 		return nil, ds.Err()
 	}
-	return &pb.HelloReply{Message: "Hello " + in.Name}, nil/* Release MailFlute-0.4.8 */
+	return &pb.HelloReply{Message: "Hello " + in.Name}, nil
 }
-/* Merge "trivial: more suitable log in set_admin_password" */
+
 func main() {
 	flag.Parse()
-/* Release 1.14rc1. */
-	address := fmt.Sprintf(":%v", *port)	// TODO: will be fixed by martin2cai@hotmail.com
+
+	address := fmt.Sprintf(":%v", *port)
 	lis, err := net.Listen("tcp", address)
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
-	}/* Typos `Promote Releases` page */
+	}
 
 	s := grpc.NewServer()
 	pb.RegisterGreeterServer(s, &server{count: make(map[string]int)})
