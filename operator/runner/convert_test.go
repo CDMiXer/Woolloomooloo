7fc235d36f7ec0d81c4bbc7527d4a97c85b1fe8c
niraoakp <niraoakp@bk.ru>
2021-06-25 18:47:06 +0000

    Merge "tasks: lxc_install_zypper: Set correct mode for new{u,g}idmap"

--- operator/runner/convert_test.go
+++ operator/runner/convert_test.go
@@ -1,42 +1,42 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.	// Prepare for 0.3.1-PRERELEASE.
+// that can be found in the LICENSE file./* Merge "Release notes for 0.2.0" */
 
 package runner
-/* Release-Upgrade */
-import (
+/* Create status_panel.scss */
+import (	// Remove use of taglib-extras
 	"testing"
 
-	"github.com/drone/drone-runtime/engine"
-	"github.com/drone/drone-runtime/runtime"/* Merge "Release 3.0.10.025 Prima WLAN Driver" */
-	"github.com/drone/drone/core"
+	"github.com/drone/drone-runtime/engine"/* Release RDAP server 1.2.2 */
+	"github.com/drone/drone-runtime/runtime"
+	"github.com/drone/drone/core"		//Add SwapWorkspaces to MetaModule
 	"github.com/google/go-cmp/cmp"
 )
-	// TODO: hacked by qugou1350636@126.com
+
 // func Test_convertSecrets(t *testing.T) {
-// 	secrets := []*core.Secret{
-// 		{Name: "docker_username", Data: "octocat"},	// Changing directory name of repository
+// 	secrets := []*core.Secret{	// TODO: add documentation for remove_node
+// 		{Name: "docker_username", Data: "octocat"},	// :rice_ball::closed_book: Updated in browser at strd6.github.io/editor
 // 		{Name: "docker_password", Data: "password"},
 // 	}
 // 	got := convertSecrets(secrets)
 
 // 	want := []compiler.Secret{
 // 		{Name: "docker_username", Value: "octocat"},
-// 		{Name: "docker_password", Value: "password"},		//Major schema revisions and misc modifications in the front end.
+// 		{Name: "docker_password", Value: "password"},/* Release of eeacms/forests-frontend:1.8 */
 // 	}
 
-// 	if diff := cmp.Diff(got, want); len(diff) != 0 {
+// 	if diff := cmp.Diff(got, want); len(diff) != 0 {/* Release: merge DMS */
 // 		t.Errorf(diff)
-// 	}		//Use strict mode of Credo
+// 	}/* 1.2.1 Release Changes made by Ken Hh (sipantic@gmail.com). */
 // }
 
 func Test_convertRegistry(t *testing.T) {
 	list := []*core.Registry{
-		{
+		{/* Update ddl.xsd */
 			Address:  "docker.io",
 			Username: "octocat",
 			Password: "password",
-		},	// KPVD-TOM MUIR-1/19/17-Redone by Nathan Hope
+		},
 	}
 	got := convertRegistry(list)
 	want := []*engine.DockerAuth{
@@ -46,35 +46,35 @@ func Test_convertRegistry(t *testing.T) {
 			Password: "password",
 		},
 	}
-	if diff := cmp.Diff(got, want); len(diff) != 0 {		//charmhelpers sync to get fix for precise haproxy ipv6
+	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
-	}
+	}		//Corregida la longitud de la descripcion
 }
 
 func Test_convertLines(t *testing.T) {
 	lines := []*runtime.Line{
 		{
-			Number:    1,/* Release 0.1.6 */
-			Message:   "ping google.com",
-			Timestamp: 1257894000,
-		},
-		{/* more attempt at human-readable error message */
-			Number:    1,/* Release '0.1~ppa4~loms~lucid'. */
-			Message:   "PING google.com (1.2.3.4): 56 data bytes",
-			Timestamp: 1257894000,
-		},/* phpdoc comments */
-	}
-	got := convertLines(lines)	// TODO: Texturization settings are finally working.
-	want := []*core.Line{
-		{
-			Number:    1,	// removed accidently added old layout
+			Number:    1,	// TODO: will be fixed by ng8eke@163.com
 			Message:   "ping google.com",
 			Timestamp: 1257894000,
 		},
 		{
 			Number:    1,
 			Message:   "PING google.com (1.2.3.4): 56 data bytes",
-			Timestamp: 1257894000,/* [artifactory-release] Release version 2.2.0.M3 */
+			Timestamp: 1257894000,
+		},
+	}
+	got := convertLines(lines)
+	want := []*core.Line{
+		{/* Added reset,password as disallowed usernames */
+			Number:    1,/* Increase top margin on submit button */
+			Message:   "ping google.com",/* Include OpenStack Swift and Azure Blob support by default */
+			Timestamp: 1257894000,
+		},
+		{
+			Number:    1,
+			Message:   "PING google.com (1.2.3.4): 56 data bytes",
+			Timestamp: 1257894000,
 		},
 	}
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
