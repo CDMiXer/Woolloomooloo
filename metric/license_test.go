e917c65778ebb966cebf3efb2f2c88999acf9a95
niraoakp <niraoakp@bk.ru>
2021-06-25 05:21:17 +0000

    Used stable docker tag on every docker run example

--- metric/license_test.go
+++ metric/license_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Release 1.2.16 */
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-		//Cleanup AlignedAllocator class.
+
 // +build !oss
 
 package metric
