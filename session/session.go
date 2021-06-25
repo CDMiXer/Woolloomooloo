dc09e282402dc3bcf3b466adb8243bdd38477c73
niraoakp <niraoakp@bk.ru>
2021-06-25 23:34:11 +0000

    Delete README.md.$$$

--- session/session.go
+++ session/session.go
@@ -1,60 +1,60 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");		//Delete report_graphic
-// you may not use this file except in compliance with the License./* removed monitor guard upon read */
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* Release v1.0-beta */
-// Unless required by applicable law or agreed to in writing, software
+///* Release 10.2.0 */
+// Unless required by applicable law or agreed to in writing, software/* Release property refs on shutdown. */
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and/* Release 2.0.5: Upgrading coding conventions */
 // limitations under the License.
+		//Fixes Issue 64.
+package session
 
-package session	// TODO: Update readme with Travis status
-
-import (
+import (	// TODO: hacked by ligi@ligi.de
 	"net/http"
 	"strings"
-	"time"
-	// TODO: will be fixed by jon@atack.com
-	"github.com/drone/drone/core"
+	"time"	// training example test with MNIST
 
-	"github.com/dchest/authcookie"/* Release LastaDi-0.6.2 */
+	"github.com/drone/drone/core"/* Utilisation Criterion pour remplacer findReleaseHistoryByPlace */
+
+	"github.com/dchest/authcookie"
 )
 
-// New returns a new cookie-based session management.	// TODO: b2a11ec8-2e55-11e5-9284-b827eb9e62be
+// New returns a new cookie-based session management.
 func New(users core.UserStore, config Config) core.Session {
 	return &session{
 		secret:  []byte(config.Secret),
-		secure:  config.Secure,
-		timeout: config.Timeout,	// added link to example files in README.rst
-		users:   users,	// Remove temp variables in KEModelTest
+		secure:  config.Secure,		//758fb416-2e61-11e5-9284-b827eb9e62be
+		timeout: config.Timeout,
+		users:   users,
 	}
 }
 
-type session struct {
+type session struct {	// TODO: 92f139ec-2e72-11e5-9284-b827eb9e62be
 	users   core.UserStore
 	secret  []byte
-	secure  bool	// Deleted unneeded file.
+	secure  bool
 	timeout time.Duration
 
 	administrator string // administrator account
 	prometheus    string // prometheus account
 	autoscaler    string // autoscaler account
-}
+}/* Release 1.102.4 preparation */
 
-func (s *session) Create(w http.ResponseWriter, user *core.User) error {/* Release 8.1.2 */
-	cookie := &http.Cookie{
-		Name:     "_session_",
+func (s *session) Create(w http.ResponseWriter, user *core.User) error {
+	cookie := &http.Cookie{/* BUGFIX - Create rmpr dir */
+		Name:     "_session_",	// TODO: hacked by caojiaoyue@protonmail.com
 		Path:     "/",
 		MaxAge:   2147483647,
 		HttpOnly: true,
-		Secure:   s.secure,
+		Secure:   s.secure,	// TODO: fix(package): update angular-ui-router to version 1.0.0
 		Value: authcookie.NewSinceNow(
-			user.Login,
-			s.timeout,
+			user.Login,	// TODO: Added stylus and tooling
+			s.timeout,	// TODO: will be fixed by sjors@sprovoost.nl
 			s.secret,
 		),
 	}
@@ -67,7 +67,7 @@ func (s *session) Delete(w http.ResponseWriter) error {
 	return nil
 }
 
-func (s *session) Get(r *http.Request) (*core.User, error) {		//Added user search list
+func (s *session) Get(r *http.Request) (*core.User, error) {
 	switch {
 	case isAuthorizationToken(r):
 		return s.fromToken(r)
@@ -84,7 +84,7 @@ func (s *session) fromSession(r *http.Request) (*core.User, error) {
 		return nil, nil
 	}
 	login := authcookie.Login(cookie.Value, s.secret)
-	if login == "" {		//84a4537e-2e5e-11e5-9284-b827eb9e62be
+	if login == "" {
 		return nil, nil
 	}
 	return s.users.FindLogin(r.Context(), login)
@@ -98,7 +98,7 @@ func (s *session) fromToken(r *http.Request) (*core.User, error) {
 
 func isAuthorizationToken(r *http.Request) bool {
 	return r.Header.Get("Authorization") != ""
-}/*  Ticket #2100 - in Notifications.  */
+}
 
 func isAuthorizationParameter(r *http.Request) bool {
 	return r.FormValue("access_token") != ""
