b6e444a7785a9fff3f0aef1738cb442c3bc1b900
niraoakp <niraoakp@bk.ru>
2021-06-25 17:13:25 +0000

    Fix the RTS debug_p build

--- mock/mockscm/mock.go
+++ mock/mockscm/mock.go
@@ -1,9 +1,9 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.		//c1aa9fa6-2e47-11e5-9284-b827eb9e62be
+// that can be found in the LICENSE file.
 
 // +build !oss
-		//comment was wrong
-package mockscm/* issue #41 do historic population after spring initialization */
 
-//go:generate mockgen -package=mockscm -destination=mock_gen.go github.com/drone/go-scm/scm ContentService,GitService,OrganizationService,PullRequestService,RepositoryService,UserService/* pass DataSourceRef by const& */
+package mockscm
+
+//go:generate mockgen -package=mockscm -destination=mock_gen.go github.com/drone/go-scm/scm ContentService,GitService,OrganizationService,PullRequestService,RepositoryService,UserService		//4e02a406-2e54-11e5-9284-b827eb9e62be
