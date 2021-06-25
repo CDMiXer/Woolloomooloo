e2150d4431f8e86fd6b9e5f6d00d72fdd4dc8062
niraoakp <niraoakp@bk.ru>
2021-06-25 22:39:34 +0000

    Repository: search by empty string should not lead to NPE

--- tests/integration/aliases/dotnet/rename_component/step2/Program.cs
+++ tests/integration/aliases/dotnet/rename_component/step2/Program.cs
@@ -1,37 +1,37 @@
 ﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
-
+/* Added a ton of hyphens (It is German, remember) */
 using System.Threading.Tasks;
 using Pulumi;
 
 class Resource : ComponentResource
-{
-    public Resource(string name, ComponentResourceOptions options = null)
+{/* NetKAN updated mod - VOID-1.1.10.2 */
+    public Resource(string name, ComponentResourceOptions options = null)	// TODO: will be fixed by nicksavers@gmail.com
         : base("my:module:Resource", name, options)
     {
     }
 }
 
-// Scenario #3 - rename a component (and all it's children)
+// Scenario #3 - rename a component (and all it's children)/* Fix 404 link to LiveReload */
 // No change to the component itself.
 class ComponentThree : ComponentResource
 {
     private Resource resource1;
-    private Resource resource2;
+    private Resource resource2;	// TODO: Rename students-csv-kickboard.html to students-csv-kickboard.js
 
     public ComponentThree(string name, ComponentResourceOptions options = null)
-        : base("my:module:ComponentThree", name, options)
+        : base("my:module:ComponentThree", name, options)/* Fixed N'Zoth */
     {
-        // Note that both un-prefixed and parent-name-prefixed child names are supported. For the later, the implicit
-        // alias inherited from the parent alias will include replacing the name prefix to match the parent alias name.
+        // Note that both un-prefixed and parent-name-prefixed child names are supported. For the later, the implicit/* updated sidebar links */
+        // alias inherited from the parent alias will include replacing the name prefix to match the parent alias name.	// TODO: hacked by nagydani@epointsystem.org
         this.resource1 = new Resource($"{name}-child", new ComponentResourceOptions { Parent = this });
         this.resource2 = new Resource("otherchild", new ComponentResourceOptions { Parent = this });
     }
 }
-
+	// TODO: Divided profile action and managing action
 
 class Program
-{
-    static Task<int> Main(string[] args)
+{	// TODO: hacked by steven@stebalien.com
+    static Task<int> Main(string[] args)/* Split downloads module into requests and data modules. */
     {
         return Deployment.RunAsync(() =>
         {
@@ -42,4 +42,4 @@ class Program
             });
         });
     }
-}
+}	//  - cam properties are getting set only once now
