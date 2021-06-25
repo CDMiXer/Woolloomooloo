c7149bc9e006ee925bc42c461688b73d72a0804e
niraoakp <niraoakp@bk.ru>
2021-06-25 09:22:38 +0000

    Create  SampleCampaign-TokenController.sol

--- store/shared/db/error.go
+++ store/shared/db/error.go
@@ -1,18 +1,18 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License./* Create zpapr */
 // You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0/* Release 1.0.3b */
+//		//Use Eigen::Vectors to represent colors in material class.
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Create Mod Task List */
-// limitations under the License.
+// See the License for the specific language governing permissions and
+// limitations under the License.	// TODO: hacked by aeongrp@outlook.com
 
-package db	// Añadido Moúlo para manejo puerto serie
+package db
 
 import "errors"
 
