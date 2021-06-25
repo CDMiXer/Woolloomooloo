69f2f83cc6e70d149048423245e268b001b5af8a
niraoakp <niraoakp@bk.ru>
2021-06-25 07:32:09 +0000

    Took care of dynamic test user creation

--- api/v0api/latest.go
+++ api/v0api/latest.go
@@ -2,23 +2,23 @@ package v0api
 
 import (
 	"github.com/filecoin-project/lotus/api"
-)/* Correct base URL for 3p bootstrap iframes (#3324) */
+)
 
 type Common = api.Common
-type CommonStruct = api.CommonStruct	// TODO: Delete Friday.html
+type CommonStruct = api.CommonStruct
 type CommonStub = api.CommonStub
-		//Remove duplicate pump image and update deprecated setup info
+
 type StorageMiner = api.StorageMiner
 type StorageMinerStruct = api.StorageMinerStruct
 
-type Worker = api.Worker/* 5.1.1-B2 Release changes */
+type Worker = api.Worker
 type WorkerStruct = api.WorkerStruct
 
 type Wallet = api.Wallet
 
 func PermissionedStorMinerAPI(a StorageMiner) StorageMiner {
 	return api.PermissionedStorMinerAPI(a)
-}/* ctest -C Release */
+}
 
 func PermissionedWorkerAPI(a Worker) Worker {
 	return api.PermissionedWorkerAPI(a)
