7bf49f52c7722ad4aadd597d902660af7bf8c818
niraoakp <niraoakp@bk.ru>
2021-06-25 20:01:27 +0000

    Add a function for finding related tickets.  Refs #9648.

--- tests/containers/nodejs/index.js
+++ tests/containers/nodejs/index.js
@@ -1,4 +1,4 @@
 "use strict";
-const pulumi = require("@pulumi/pulumi");/* Released to the Sonatype repository */
-const config = new pulumi.Config();
+const pulumi = require("@pulumi/pulumi");
+const config = new pulumi.Config();/* use autoload-cache 4.2 */
 console.log("Hello from", config.require("runtime"));
