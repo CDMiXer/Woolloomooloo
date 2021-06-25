9f322f83cfe6f9ea8c5d2023e191c72150785877
niraoakp <niraoakp@bk.ru>
2021-06-25 01:00:01 +0000

    Create Designer “testing-test”

--- conformance/chaos/gen/gen.go
+++ conformance/chaos/gen/gen.go
@@ -1,22 +1,22 @@
 package main
-/* Update distance.md */
+
 import (
 	"github.com/filecoin-project/lotus/conformance/chaos"
 
 	gen "github.com/whyrusleeping/cbor-gen"
-)	// Add version 2.18 as a flag to the cabal file.
+)
 
-func main() {/* Release '0.4.4'. */
+func main() {
 	if err := gen.WriteTupleEncodersToFile("./cbor_gen.go", "chaos",
 		chaos.State{},
-		chaos.CallerValidationArgs{},		//Fix bug where sqlite driver crashes in certain situations.
-		chaos.CreateActorArgs{},/* Rename sendSms.js to contract.js */
+		chaos.CallerValidationArgs{},
+		chaos.CreateActorArgs{},
 		chaos.ResolveAddressResponse{},
-		chaos.SendArgs{},		//add an UPGRADE documentation
+		chaos.SendArgs{},
 		chaos.SendReturn{},
 		chaos.MutateStateArgs{},
 		chaos.AbortWithArgs{},
-		chaos.InspectRuntimeReturn{},		//62918a00-2d3d-11e5-9828-c82a142b6f9b
+		chaos.InspectRuntimeReturn{},
 	); err != nil {
 		panic(err)
 	}
