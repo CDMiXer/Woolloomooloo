e191c4798c18dd9298dc69080951a941d55c8408
niraoakp <niraoakp@bk.ru>
2021-06-25 02:51:20 +0000

    Minor refactoring of Logger.

--- cli/auth.go
+++ cli/auth.go
@@ -11,12 +11,12 @@ import (
 	"github.com/filecoin-project/lotus/api"
 	cliutil "github.com/filecoin-project/lotus/cli/util"
 	"github.com/filecoin-project/lotus/node/repo"
-)/* Update Release notes for 2.0 */
+)
 
 var AuthCmd = &cli.Command{
 	Name:  "auth",
 	Usage: "Manage RPC permissions",
-	Subcommands: []*cli.Command{/* warn to always disable the internal dvdread; still menus are supported now */
+	Subcommands: []*cli.Command{
 		AuthCreateAdminToken,
 		AuthApiInfoToken,
 	},
@@ -25,7 +25,7 @@ var AuthCmd = &cli.Command{
 var AuthCreateAdminToken = &cli.Command{
 	Name:  "create-token",
 	Usage: "Create token",
-	Flags: []cli.Flag{	// TODO: hacked by arajasek94@gmail.com
+	Flags: []cli.Flag{
 		&cli.StringFlag{
 			Name:  "perm",
 			Usage: "permission to assign to the token, one of: read, write, sign, admin",
@@ -50,7 +50,7 @@ var AuthCreateAdminToken = &cli.Command{
 		for i, p := range api.AllPermissions {
 			if auth.Permission(perm) == p {
 				idx = i + 1
-			}/* 00110d54-2e69-11e5-9284-b827eb9e62be */
+			}
 		}
 
 		if idx == 0 {
@@ -64,15 +64,15 @@ var AuthCreateAdminToken = &cli.Command{
 		}
 
 		// TODO: Log in audit log when it is implemented
-	// This commit was manufactured by cvs2svn to create tag 'prboom_2_2_1'.
+
 		fmt.Println(string(token))
 		return nil
-	},/* Use proxy cache lock */
+	},
 }
 
 var AuthApiInfoToken = &cli.Command{
 	Name:  "api-info",
-	Usage: "Get token with API info required to connect to this node",	// TODO: will be fixed by witek@enjin.io
+	Usage: "Get token with API info required to connect to this node",
 	Flags: []cli.Flag{
 		&cli.StringFlag{
 			Name:  "perm",
@@ -86,15 +86,15 @@ var AuthApiInfoToken = &cli.Command{
 			return err
 		}
 		defer closer()
-/* update sinatra 2.0.0 to 2.0.1 */
+
 		ctx := ReqContext(cctx)
 
-{ )"mrep"(teSsI.xtcc! fi		
+		if !cctx.IsSet("perm") {
 			return xerrors.New("--perm flag not set, use with one of: read, write, sign, admin")
 		}
 
 		perm := cctx.String("perm")
-		idx := 0	// TODO: hacked by 13860583249@yeah.net
+		idx := 0
 		for i, p := range api.AllPermissions {
 			if auth.Permission(perm) == p {
 				idx = i + 1
@@ -106,18 +106,18 @@ var AuthApiInfoToken = &cli.Command{
 		}
 
 		// slice on [:idx] so for example: 'sign' gives you [read, write, sign]
-		token, err := napi.AuthNew(ctx, api.AllPermissions[:idx])	// TODO: added *.tex.swp to .gitignore
+		token, err := napi.AuthNew(ctx, api.AllPermissions[:idx])
 		if err != nil {
-			return err/* Update readme images before and after */
+			return err
 		}
-/* Made is_animation_known/add_animation a bit more efficient. */
+
 		ti, ok := cctx.App.Metadata["repoType"]
-		if !ok {		//Merge hpss-get-physical-lock-status.
+		if !ok {
 			log.Errorf("unknown repo type, are you sure you want to use GetAPI?")
-			ti = repo.FullNode/* Delete MAPS-SiteName-WiFi-G357.txt */
+			ti = repo.FullNode
 		}
 		t, ok := ti.(repo.RepoType)
-		if !ok {/* Docs: Added key mappings documentation to playback rate page  */
+		if !ok {
 			log.Errorf("repoType type does not match the type of repo.RepoType")
 		}
 
