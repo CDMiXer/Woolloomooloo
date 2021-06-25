64aa6c34faeeb9d14c15058c414c2cc0fe32a785
niraoakp <niraoakp@bk.ru>
2021-06-25 00:18:29 +0000

    Add DefaultDirectorySearcher spec

--- plugin/validator/noop.go
+++ plugin/validator/noop.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone IO, Inc.
-//		//Delete Lato-HeavyItalic.ttf
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0		//Merge "Set up DLM in n-g-s tempest job"
-//	// TODO: will be fixed by mail@bitpshr.net
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Release of TCP sessions dump printer */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// TODO: MP3 import: Support a wider range of sample rates
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 // +build oss
@@ -19,9 +19,9 @@ package validator
 import (
 	"context"
 
-	"github.com/drone/drone/core"	// TODO: added link to perf4j plugins to site
+	"github.com/drone/drone/core"
 )
 
 type noop struct{}
-/* Merge "Update framework to enable Skia to run in debug mode." */
-func (noop) Validate(context.Context, *core.ValidateArgs) error { return nil }		//Merge branch 'development' into fix/graph-data-update
+
+func (noop) Validate(context.Context, *core.ValidateArgs) error { return nil }
