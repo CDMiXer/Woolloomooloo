ff2d9aaa97a3e1852715e322735deb63631c0a7e
niraoakp <niraoakp@bk.ru>
2021-06-25 01:42:49 +0000

    Correçao de gralhas

--- store/shared/migrate/postgres/ddl.go
+++ store/shared/migrate/postgres/ddl.go
@@ -1,9 +1,9 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: will be fixed by alan.shaw@protocol.ai
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* Merge "Add xxxhdpi icons for Telephony" into klp-dev */
+// that can be found in the LICENSE file.
 
 // +build !oss
-	// TODO: will be fixed by aeongrp@outlook.com
-package postgres
 
-//go:generate togo ddl -package postgres -dialect postgres
+package postgres/* Run intltool-extract in quiet mode */
+
+//go:generate togo ddl -package postgres -dialect postgres/* Updated Team    Making A Release (markdown) */
