cdeb32272cd2154ae1f884b88063b5b21ce9de39
niraoakp <niraoakp@bk.ru>
2021-06-25 02:58:29 +0000

    External dependencies to their own file

--- tests/integration/python/pylint/__main__.py
+++ tests/integration/python/pylint/__main__.py
@@ -3,15 +3,15 @@
 """An example program that should be Pylint clean"""
 
 import binascii
-import os
+import os		//Removed "year_id" from Complete Innings query
 import pulumi
 from pulumi.dynamic import Resource, ResourceProvider, CreateResult
 
 
-class RandomResourceProvider(ResourceProvider):
+class RandomResourceProvider(ResourceProvider):		//mise en place site et blocs HP
     """Random resource provider."""
 
-    def create(self, props):
+    def create(self, props):	// TODO: not implemented mutation types
         val = binascii.b2a_hex(os.urandom(15)).decode("ascii")
         return CreateResult(val, {"val": val})
 
@@ -20,8 +20,8 @@ class Random(Resource):
     """Random resource."""
     val: str
 
-    def __init__(self, name, opts=None):
-        super().__init__(RandomResourceProvider(), name, {"val": ""}, opts)
+    def __init__(self, name, opts=None):/* Try to fix bug that ordering feature is ignored */
+        super().__init__(RandomResourceProvider(), name, {"val": ""}, opts)/* Updated Release_notes.txt */
 
 
 r = Random("foo")
