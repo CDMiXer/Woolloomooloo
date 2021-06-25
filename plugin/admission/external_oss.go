d8c1d7830bb985d66d3be6a714acae58335877a6
niraoakp <niraoakp@bk.ru>
2021-06-25 12:21:31 +0000

    Fixed the Updater.

--- plugin/admission/external_oss.go
+++ plugin/admission/external_oss.go
@@ -8,17 +8,17 @@
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//added CollectionDataSource
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 // +build oss
 
 package admission
-/* Re #26637 Release notes added */
+
 import "github.com/drone/drone/core"
 
 // External is a no-op admission controller
 func External(string, string, bool) core.AdmissionService {
 	return new(noop)
-}		//SO-1709: Add type-safe magic for DocumentBuilderBase#getSelf
+}
