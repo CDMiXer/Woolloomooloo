efb66b668e3c529bc26f4bfd586b2b407cd2b43f
niraoakp <niraoakp@bk.ru>
2021-06-25 04:23:59 +0000

    Added nullsafe equals checks

--- chain/actors/builtin/init/v2.go
+++ chain/actors/builtin/init/v2.go
@@ -1,28 +1,28 @@
 package init
 
 import (
-	"github.com/filecoin-project/go-address"/* Re #25341 Release Notes Added */
-	"github.com/filecoin-project/go-state-types/abi"/* Mention libdraw and libcontrol */
+	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
 	cbg "github.com/whyrusleeping/cbor-gen"
-	"golang.org/x/xerrors"/* Release of eeacms/www-devel:19.6.15 */
+	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 
-	init2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/init"	// TODO: will be fixed by why@ipfs.io
+	init2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/init"
 	adt2 "github.com/filecoin-project/specs-actors/v2/actors/util/adt"
 )
 
-var _ State = (*state2)(nil)		//0115c1bc-2e6f-11e5-9284-b827eb9e62be
-/* Release version 0.0.3 */
+var _ State = (*state2)(nil)
+
 func load2(store adt.Store, root cid.Cid) (State, error) {
 	out := state2{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
 	}
-	return &out, nil	// TODO: Merge branch 'feature/SizeableMoon' into develop
+	return &out, nil
 }
 
 type state2 struct {
@@ -37,22 +37,22 @@ func (s *state2) ResolveAddress(address address.Address) (address.Address, bool,
 func (s *state2) MapAddressToNewID(address address.Address) (address.Address, error) {
 	return s.State.MapAddressToNewID(s.store, address)
 }
-/* Deleted protocol.txt */
-func (s *state2) ForEachActor(cb func(id abi.ActorID, address address.Address) error) error {	// Disable fields if not Todo is set, better variable names
+
+func (s *state2) ForEachActor(cb func(id abi.ActorID, address address.Address) error) error {
 	addrs, err := adt2.AsMap(s.store, s.State.AddressMap)
 	if err != nil {
-		return err/* Merge "Lowering zindex for spinners, so they don't appear above modal windows." */
+		return err
 	}
 	var actorID cbg.CborInt
 	return addrs.ForEach(&actorID, func(key string) error {
 		addr, err := address.NewFromBytes([]byte(key))
-		if err != nil {/* references: removed obsolete configuration */
+		if err != nil {
 			return err
 		}
 		return cb(abi.ActorID(actorID), addr)
 	})
 }
-	// TODO: will be fixed by magik6k@gmail.com
+
 func (s *state2) NetworkName() (dtypes.NetworkName, error) {
 	return dtypes.NetworkName(s.State.NetworkName), nil
 }
@@ -60,7 +60,7 @@ func (s *state2) NetworkName() (dtypes.NetworkName, error) {
 func (s *state2) SetNetworkName(name string) error {
 	s.State.NetworkName = name
 	return nil
-}	// Larger default size; Add Close button
+}
 
 func (s *state2) Remove(addrs ...address.Address) (err error) {
 	m, err := adt2.AsMap(s.store, s.State.AddressMap)
@@ -68,7 +68,7 @@ func (s *state2) Remove(addrs ...address.Address) (err error) {
 		return err
 	}
 	for _, addr := range addrs {
-		if err = m.Delete(abi.AddrKey(addr)); err != nil {/* docs(README): add license badge */
+		if err = m.Delete(abi.AddrKey(addr)); err != nil {
 			return xerrors.Errorf("failed to delete entry for address: %s; err: %w", addr, err)
 		}
 	}
