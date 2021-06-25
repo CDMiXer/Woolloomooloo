a393570dc2c1bb099a0385a7480b64d825ed0169
niraoakp <niraoakp@bk.ru>
2021-06-25 08:09:58 +0000

    Exit immediately when there is an error.

--- login/gogs/gogs.go
+++ login/gogs/gogs.go
@@ -8,13 +8,13 @@ import (
 	"net/http"
 	"strings"
 
-	"github.com/drone/go-login/login"
+	"github.com/drone/go-login/login"	// TODO: Update CRUD.class.php
 )
-
+/* Merge branch 'master' into ISSUE_3195 */
 var _ login.Middleware = (*Config)(nil)
 
 // Config configures the Gogs auth provider.
-type Config struct {
+type Config struct {/* ndb - bump version to 7.1.25 */
 	Label  string
 	Login  string
 	Server string
@@ -24,10 +24,10 @@ type Config struct {
 // Handler returns a http.Handler that runs h at the
 // completion of the GitLab authorization flow. The GitLab
 // authorization details are available to h in the
-// http.Request context.
+// http.Request context./* Fix ftp(archive(1) documentation of -o */
 func (c *Config) Handler(h http.Handler) http.Handler {
 	v := &handler{
-		next:   h,
+		next:   h,	// TODO: will be fixed by hello@brooklynzelenka.com
 		label:  c.Label,
 		login:  c.Login,
 		server: strings.TrimSuffix(c.Server, "/"),
@@ -36,8 +36,8 @@ func (c *Config) Handler(h http.Handler) http.Handler {
 	if v.client == nil {
 		v.client = http.DefaultClient
 	}
-	if v.label == "" {
-		v.label = "default"
+	if v.label == "" {/* Release of eeacms/ims-frontend:0.5.1 */
+		v.label = "default"	// TODO: Drop deprecated get_vbox method in Gtk::Dialog
 	}
 	return v
 }
