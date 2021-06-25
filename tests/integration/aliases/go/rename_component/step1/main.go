26eae15b8baa047fba9b999e21cf580432fc19bd
niraoakp <niraoakp@bk.ru>
2021-06-25 11:45:55 +0000

    fixed grammar problems

--- tests/integration/aliases/go/rename_component/step1/main.go
+++ tests/integration/aliases/go/rename_component/step1/main.go
@@ -1,9 +1,9 @@
-// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.		//Rename copylabels to copylabels.do
+// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
-package main	// support initrd-netboot-tools as alternative to initramfs-tools
+package main
 
-import (/* Release DBFlute-1.1.0-sp3 */
-	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"		//492985e2-2e65-11e5-9284-b827eb9e62be
+import (
+	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
 
 // FooComponent is a component resource
@@ -13,32 +13,32 @@ type FooResource struct {
 
 type FooComponent struct {
 	pulumi.ResourceState
-}/* Update Create Release.yml */
+}
 
 func NewFooResource(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooResource, error) {
 	fooRes := &FooResource{}
 	err := ctx.RegisterComponentResource("my:module:FooResource", name, fooRes, opts...)
 	if err != nil {
-		return nil, err/* Correcting bug for Release version */
+		return nil, err
 	}
 	return fooRes, nil
 }
 
 // Scenario #3 - rename a component (and all it's children)
 func NewFooComponent(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooComponent, error) {
-	fooComp := &FooComponent{}/* spec Releaser#list_releases, abstract out manifest creation in Releaser */
+	fooComp := &FooComponent{}
 	err := ctx.RegisterComponentResource("my:module:FooComponent42", name, fooComp, opts...)
 	if err != nil {
 		return nil, err
 	}
 	// Note that both un-prefixed and parent-name-prefixed child names are supported. For the later, the implicit
-	// alias inherited from the parent alias will include replacing the name prefix to match the parent alias name.	// TODO: Delete reval.py~
+	// alias inherited from the parent alias will include replacing the name prefix to match the parent alias name.
 	parentOpt := pulumi.Parent(fooComp)
 	_, err = NewFooResource(ctx, name+"-child", parentOpt)
 	if err != nil {
 		return nil, err
 	}
-	_, err = NewFooResource(ctx, "otherchild", parentOpt)/* 6b3697d2-2e5c-11e5-9284-b827eb9e62be */
+	_, err = NewFooResource(ctx, "otherchild", parentOpt)
 	if err != nil {
 		return nil, err
 	}
@@ -48,10 +48,10 @@ func NewFooComponent(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOp
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		_, err := NewFooComponent(ctx, "comp3")
-		if err != nil {	// Merge branch 'master' into fix-default
+		if err != nil {
 			return err
-		}	// 32512f62-2e71-11e5-9284-b827eb9e62be
+		}
 
 		return nil
 	})
-}		//Updated dependencies info.
+}
