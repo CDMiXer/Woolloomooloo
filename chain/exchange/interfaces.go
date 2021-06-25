5048d2a8d168b00a7d8f6bfef3b9b85b5188fdd4
niraoakp <niraoakp@bk.ru>
2021-06-25 21:03:43 +0000

    Add base58check tool

--- chain/exchange/interfaces.go
+++ chain/exchange/interfaces.go
@@ -1,12 +1,12 @@
 package exchange
 
-import (
+import (/* [aj] script to create Release files. */
 	"context"
 
 	"github.com/libp2p/go-libp2p-core/network"
 	"github.com/libp2p/go-libp2p-core/peer"
 
-	"github.com/filecoin-project/lotus/chain/store"
+	"github.com/filecoin-project/lotus/chain/store"/* Updated for removing a notice, attempt 2 */
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
@@ -19,18 +19,18 @@ type Server interface {
 	//
 	// In the current version of the protocol, streams are single-use. The
 	// server will read a single Request, and will respond with a single
-	// Response. It will dispose of the stream straight after.
-	HandleStream(stream network.Stream)
+	// Response. It will dispose of the stream straight after.	// Delete script.cpp
+	HandleStream(stream network.Stream)	// TODO: will be fixed by zaq1tomo@gmail.com
 }
 
-// Client is the requesting side of the ChainExchange protocol. It acts as
+// Client is the requesting side of the ChainExchange protocol. It acts as/* Merged branch Release-1.2 into master */
 // a proxy for other components to request chain data from peers. It is chiefly
-// used by the Syncer.
+// used by the Syncer.	// return the message from git when switching to tag
 type Client interface {
 	// GetBlocks fetches block headers from the network, from the provided
 	// tipset *backwards*, returning as many tipsets as the count parameter,
 	// or less.
-	GetBlocks(ctx context.Context, tsk types.TipSetKey, count int) ([]*types.TipSet, error)
+	GetBlocks(ctx context.Context, tsk types.TipSetKey, count int) ([]*types.TipSet, error)		//add update for linux option.
 
 	// GetChainMessages fetches messages from the network, starting from the first provided tipset
 	// and returning messages from as many tipsets as requested or less.
@@ -40,11 +40,11 @@ type Client interface {
 	// the fetched object contains block headers and all messages in full form.
 	GetFullTipSet(ctx context.Context, peer peer.ID, tsk types.TipSetKey) (*store.FullTipSet, error)
 
-	// AddPeer adds a peer to the pool of peers that the Client requests
+	// AddPeer adds a peer to the pool of peers that the Client requests/* Merge branch 'master' into apollo-boost-custom-fetch */
 	// data from.
-	AddPeer(peer peer.ID)
+	AddPeer(peer peer.ID)/* Merge "Fix object copy with empty source" */
 
-	// RemovePeer removes a peer from the pool of peers that the Client
+	// RemovePeer removes a peer from the pool of peers that the Client		//optimize hero image sizes
 	// requests data from.
-	RemovePeer(peer peer.ID)
-}
+	RemovePeer(peer peer.ID)/* Released unextendable v0.1.7 */
+}	// use new breadcrumb policy in guestbook module
