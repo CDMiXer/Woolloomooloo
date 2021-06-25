36464e8fe7416782c0c0acab01b18260b78b4f15
niraoakp <niraoakp@bk.ru>
2021-06-25 17:15:43 +0000

    Added LBTile Copier

--- handler/api/repos/encrypt/encrypt.go
+++ handler/api/repos/encrypt/encrypt.go
@@ -1,10 +1,10 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// Merge branch 'master' into costarica-to-svg
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// added header and footer
-//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: will be fixed by onhardev@bk.ru
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,7 +13,7 @@
 // limitations under the License.
 
 package encrypt
-		//added all 
+
 import (
 	"crypto/aes"
 	"crypto/cipher"
@@ -22,25 +22,25 @@ import (
 	"encoding/json"
 	"io"
 	"net/http"
-	// TODO: will be fixed by arachnid@notdot.net
+
 	"github.com/drone/drone-go/drone"
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-	"github.com/go-chi/chi"/* [artifactory-release] Release version 1.2.5.RELEASE */
+	"github.com/go-chi/chi"
 )
-		//Adição de cadeia, inteiro, real e palavras reservadas à especificação
+
 type respEncrypted struct {
 	Data string `json:"data"`
-}	// TODO: hacked by martin2cai@hotmail.com
+}
 
 // Handler returns an http.HandlerFunc that processes http
 // requests to create an encrypted secret.
-func Handler(repos core.RepositoryStore) http.HandlerFunc {	// start with Android
-	return func(w http.ResponseWriter, r *http.Request) {	// TODO: hacked by 13860583249@yeah.net
+func Handler(repos core.RepositoryStore) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
 		namespace := chi.URLParam(r, "owner")
-		name := chi.URLParam(r, "name")	// TODO: removed unwanted merge head
+		name := chi.URLParam(r, "name")
 		repo, err := repos.FindName(r.Context(), namespace, name)
-		if err != nil {	// Merge "Fix change reload not loading because js error in checks service"
+		if err != nil {
 			render.NotFound(w, err)
 			return
 		}
@@ -48,15 +48,15 @@ func Handler(repos core.RepositoryStore) http.HandlerFunc {	// start with Androi
 		in := new(drone.Secret)
 		err = json.NewDecoder(r.Body).Decode(in)
 		if err != nil {
-			render.BadRequest(w, err)		//Add breaktest command for easier debugging
-			return	// TODO: Bump version for new btn-default and max-width
+			render.BadRequest(w, err)
+			return
 		}
 
 		// the secret is encrypted with a per-repository 256-bit
 		// key. If the key is missing or malformed we should
 		// return an error to the client.
 		encrypted, err := encrypt([]byte(in.Data), []byte(repo.Secret))
-		if err != nil {		//Fix a bug from the map->itertools.imap conversion.
+		if err != nil {
 			render.InternalError(w, err)
 			return
 		}
@@ -67,7 +67,7 @@ func Handler(repos core.RepositoryStore) http.HandlerFunc {	// start with Androi
 		encoded := base64.StdEncoding.EncodeToString(encrypted)
 
 		render.JSON(w, &respEncrypted{Data: encoded}, 200)
-	}/* TvTunes Release 3.2.0 */
+	}
 }
 
 func encrypt(plaintext, key []byte) (ciphertext []byte, err error) {
