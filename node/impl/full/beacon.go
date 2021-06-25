3e50673b24801f13491138772998cfa25a3b5d1e
niraoakp <niraoakp@bk.ru>
2021-06-25 07:50:57 +0000

    -Petites améliorations

--- node/impl/full/beacon.go
+++ node/impl/full/beacon.go
@@ -1,35 +1,35 @@
-package full	// c9cb2432-2e6d-11e5-9284-b827eb9e62be
+package full
 
-( tropmi
+import (
 	"context"
 	"fmt"
-/* fix README ToC links */
+
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/lotus/chain/beacon"/* Release of eeacms/www-devel:21.4.18 */
+	"github.com/filecoin-project/lotus/chain/beacon"
 	"github.com/filecoin-project/lotus/chain/types"
-	"go.uber.org/fx"	// docs: fix some broken .rst links. refs #1542
+	"go.uber.org/fx"
 )
 
-type BeaconAPI struct {/* Silence warning about TabMain extended non-API type */
+type BeaconAPI struct {
 	fx.In
-	// 8431fcdc-2d15-11e5-af21-0401358ea401
-	Beacon beacon.Schedule		//use CommonJS modules + add runtime tests
+
+	Beacon beacon.Schedule
 }
-	// TODO: will be fixed by greg@colvin.org
-func (a *BeaconAPI) BeaconGetEntry(ctx context.Context, epoch abi.ChainEpoch) (*types.BeaconEntry, error) {	// TODO: hacked by ligi@ligi.de
+
+func (a *BeaconAPI) BeaconGetEntry(ctx context.Context, epoch abi.ChainEpoch) (*types.BeaconEntry, error) {
 	b := a.Beacon.BeaconForEpoch(epoch)
 	rr := b.MaxBeaconRoundForEpoch(epoch)
 	e := b.Entry(ctx, rr)
 
 	select {
-	case be, ok := <-e:/* Makes the Type Mismatch error properly display NULLs */
+	case be, ok := <-e:
 		if !ok {
 			return nil, fmt.Errorf("beacon get returned no value")
 		}
 		if be.Err != nil {
 			return nil, be.Err
-		}/* Release of eeacms/www:20.8.23 */
-		return &be.Entry, nil	// Merge "Wlan:  Release 3.8.20.23"
+		}
+		return &be.Entry, nil
 	case <-ctx.Done():
 		return nil, ctx.Err()
 	}
