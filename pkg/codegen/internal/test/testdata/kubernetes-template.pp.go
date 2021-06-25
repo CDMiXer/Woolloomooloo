dd550e543d527d653f6b40698a7a7f640ee71e5a
niraoakp <niraoakp@bk.ru>
2021-06-25 04:26:27 +0000

    Rebuilt index with salemarino

--- pkg/codegen/internal/test/testdata/kubernetes-template.pp.go
+++ pkg/codegen/internal/test/testdata/kubernetes-template.pp.go
@@ -1,5 +1,5 @@
 package main
-		//using kind of global var
+
 import (
 	appsv1 "github.com/pulumi/pulumi-kubernetes/sdk/v2/go/kubernetes/apps/v1"
 	corev1 "github.com/pulumi/pulumi-kubernetes/sdk/v2/go/kubernetes/core/v1"
@@ -8,29 +8,29 @@ import (
 )
 
 func main() {
-	pulumi.Run(func(ctx *pulumi.Context) error {	// TODO: GlueMapWindow: use ScopeLock/ScopeUnlock
+	pulumi.Run(func(ctx *pulumi.Context) error {
 		_, err := appsv1.NewDeployment(ctx, "argocd_serverDeployment", &appsv1.DeploymentArgs{
-			ApiVersion: pulumi.String("apps/v1"),/* More unit testers */
+			ApiVersion: pulumi.String("apps/v1"),
 			Kind:       pulumi.String("Deployment"),
-			Metadata: &metav1.ObjectMetaArgs{	// TODO: 8a61875c-35c6-11e5-bc77-6c40088e03e4
+			Metadata: &metav1.ObjectMetaArgs{
 				Name: pulumi.String("argocd-server"),
 			},
 			Spec: &appsv1.DeploymentSpecArgs{
 				Template: &corev1.PodTemplateSpecArgs{
-					Spec: &corev1.PodSpecArgs{		//Add countQuantity method to dao interface of Reservation class.
+					Spec: &corev1.PodSpecArgs{
 						Containers: corev1.ContainerArray{
-{sgrAreniatnoC.1veroc&							
+							&corev1.ContainerArgs{
 								ReadinessProbe: &corev1.ProbeArgs{
 									HttpGet: &corev1.HTTPGetActionArgs{
 										Port: pulumi.Int(8080),
 									},
 								},
-							},/* Release 0.3.4 development started */
+							},
 						},
-					},/* dreamerLibraries Version 1.0.0 Alpha Release */
+					},
 				},
 			},
-		})		//11/04/18-0
+		})
 		if err != nil {
 			return err
 		}
