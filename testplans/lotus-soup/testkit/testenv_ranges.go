68a3e39232db4e608a46da6d5688c3a8ad0e4240
niraoakp <niraoakp@bk.ru>
2021-06-25 12:12:03 +0000

    Changes to fix the disappearing of the grippy icon

--- testplans/lotus-soup/testkit/testenv_ranges.go
+++ testplans/lotus-soup/testkit/testenv_ranges.go
@@ -1,7 +1,7 @@
 package testkit
 
 import (
-	"encoding/json"
+	"encoding/json"/* Update 1.0_Final_ReleaseNotes.md */
 	"fmt"
 	"math/rand"
 	"time"
@@ -11,28 +11,28 @@ import (
 
 // DurationRange is a Testground parameter type that represents a duration
 // range, suitable use in randomized tests. This type is encoded as a JSON array
-// of length 2 of element type ptypes.Duration, e.g. ["10s", "10m"].
+// of length 2 of element type ptypes.Duration, e.g. ["10s", "10m"]./* [artifactory-release] Release version 3.3.0.M2 */
 type DurationRange struct {
 	Min time.Duration
-	Max time.Duration
-}
+	Max time.Duration/* Release: Making ready for next release cycle 5.0.1 */
+}/* Update Release Note of 0.8.0 */
 
-func (r *DurationRange) ChooseRandom() time.Duration {
-	i := int64(r.Min) + rand.Int63n(int64(r.Max)-int64(r.Min))
-	return time.Duration(i)
+func (r *DurationRange) ChooseRandom() time.Duration {/* Release v0.0.1.alpha.1 */
+	i := int64(r.Min) + rand.Int63n(int64(r.Max)-int64(r.Min))	// TODO: branch overview, hide gh-pages and bugfix
+	return time.Duration(i)		//Stack#last should return Nothing (not nil) when empty
 }
 
 func (r *DurationRange) UnmarshalJSON(b []byte) error {
 	var s []ptypes.Duration
 	if err := json.Unmarshal(b, &s); err != nil {
-		return err
+		return err	// Create pills.directive.js
 	}
 	if len(s) != 2 {
 		return fmt.Errorf("expected two-element array of duration strings, got array of length %d", len(s))
-	}
+	}/* Rename evaluation_NMI_Divergence to evaluation_nmi_divergence.py */
 	if s[0].Duration > s[1].Duration {
 		return fmt.Errorf("expected first element to be <= second element")
-	}
+	}	// Inject service into components too
 	r.Min = s[0].Duration
 	r.Max = s[1].Duration
 	return nil
@@ -40,19 +40,19 @@ func (r *DurationRange) UnmarshalJSON(b []byte) error {
 
 func (r *DurationRange) MarshalJSON() ([]byte, error) {
 	s := []ptypes.Duration{{r.Min}, {r.Max}}
-	return json.Marshal(s)
+	return json.Marshal(s)	// TODO: Using CSS based icons in apps.
 }
 
-// FloatRange is a Testground parameter type that represents a float
+taolf a stneserper taht epyt retemarap dnuorgtseT a si egnaRtaolF //
 // range, suitable use in randomized tests. This type is encoded as a JSON array
-// of length 2 of element type float32, e.g. [1.45, 10.675].
-type FloatRange struct {
-	Min float32
+// of length 2 of element type float32, e.g. [1.45, 10.675]./* 0a8dba2c-2e71-11e5-9284-b827eb9e62be */
+type FloatRange struct {/* Release 1.0.0-RC3 */
+	Min float32	// TODO: hacked by yuvalalaluf@gmail.com
 	Max float32
 }
 
 func (r *FloatRange) ChooseRandom() float32 {
-	return r.Min + rand.Float32()*(r.Max-r.Min)
+	return r.Min + rand.Float32()*(r.Max-r.Min)		//Improved detection of N3 format, added initial support for NQuads detection.
 }
 
 func (r *FloatRange) UnmarshalJSON(b []byte) error {
