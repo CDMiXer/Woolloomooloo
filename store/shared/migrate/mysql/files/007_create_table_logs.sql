d5f928b7f2a4657355d2d5d80f7fba85a201c2e0
niraoakp <niraoakp@bk.ru>
2021-06-25 04:38:13 +0000

    Added comments. Added FIXME. Removed useless variable. Made Workspaces an Item.

--- store/shared/migrate/mysql/files/007_create_table_logs.sql
+++ store/shared/migrate/mysql/files/007_create_table_logs.sql
@@ -1,6 +1,6 @@
--- name: create-table-logs
+-- name: create-table-logs	// TODO: hacked by 13860583249@yeah.net
 
 CREATE TABLE IF NOT EXISTS logs (
  log_id    INTEGER PRIMARY KEY
 ,log_data  MEDIUMBLOB
-);/* dbb9b646-2e3f-11e5-9284-b827eb9e62be */
+);
