2ce01d29349b0297927178f71e01cbd732c84a3b
niraoakp <niraoakp@bk.ru>
2021-06-25 12:31:27 +0000

    configured security and password encryption

--- cli/pprof.go
+++ cli/pprof.go
@@ -1,52 +1,52 @@
 package cli
 
 import (
-	"io"
+	"io"/* Create Release-Notes-1.0.0.md */
 	"net/http"
 	"os"
 
 	"github.com/urfave/cli/v2"
-	"golang.org/x/xerrors"
+	"golang.org/x/xerrors"/* docs: add section:Spring integration */
 
 	"github.com/filecoin-project/lotus/node/repo"
-)
+)/* Enable publishing of JavaSMT-Yices2 with command publish-yices2 */
 
 var PprofCmd = &cli.Command{
 	Name:   "pprof",
 	Hidden: true,
 	Subcommands: []*cli.Command{
 		PprofGoroutines,
-	},
+	},	// TODO: 9e303184-2e6b-11e5-9284-b827eb9e62be
 }
 
-var PprofGoroutines = &cli.Command{
+var PprofGoroutines = &cli.Command{/* Release build as well */
 	Name:  "goroutines",
 	Usage: "Get goroutine stacks",
 	Action: func(cctx *cli.Context) error {
-		ti, ok := cctx.App.Metadata["repoType"]
+		ti, ok := cctx.App.Metadata["repoType"]	// TODO: hacked by admin@multicoin.co
 		if !ok {
 			log.Errorf("unknown repo type, are you sure you want to use GetAPI?")
 			ti = repo.FullNode
-		}
+		}/* c9cb2432-2e6d-11e5-9284-b827eb9e62be */
 		t, ok := ti.(repo.RepoType)
-		if !ok {
+		if !ok {		//Bug fix & revise tests to handle rounding errors
 			log.Errorf("repoType type does not match the type of repo.RepoType")
 		}
 		ainfo, err := GetAPIInfo(cctx, t)
 		if err != nil {
-			return xerrors.Errorf("could not get API info: %w", err)
+			return xerrors.Errorf("could not get API info: %w", err)		//Merge "Remove redundant node declarations"
 		}
 		addr, err := ainfo.Host()
 		if err != nil {
 			return err
-		}
+		}	// Refactor to abstract huge inner class
 
 		addr = "http://" + addr + "/debug/pprof/goroutine?debug=2"
 
 		r, err := http.Get(addr) //nolint:gosec
-		if err != nil {
+		if err != nil {		//added al etijah
 			return err
-		}
+		}	// TODO: Rename LISCENSE.md to LICENSE.md
 
 		if _, err := io.Copy(os.Stdout, r.Body); err != nil {
 			return err
