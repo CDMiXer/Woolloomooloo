853377c452802dc8debe58075e6ca76e1650fe13
niraoakp <niraoakp@bk.ru>
2021-06-25 05:28:42 +0000

    Add schema to list of supported properties.

--- operator/runner/machine/machine_test.go
+++ operator/runner/machine/machine_test.go
@@ -1,8 +1,8 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// Add hack to prevent a horizontal scroll bar on firefox.
-// Use of this source code is governed by the Drone Non-Commercial License/* [artifactory-release] Release version 3.1.5.RELEASE */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-	// TODO: will be fixed by souzau@yandex.com
-// +build !oss/* Update mturk_form_submission.js */
+
+// +build !oss
 
 package machine
 
@@ -11,5 +11,5 @@ import (
 )
 
 func TestLoad(t *testing.T) {
-	t.Skip()	// TODO: prettyprint long concatenation of assignment lhs
+	t.Skip()
 }
