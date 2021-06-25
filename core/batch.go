4a2d60b97480acc6282dacdc3414fba476c85307
niraoakp <niraoakp@bk.ru>
2021-06-25 01:15:00 +0000

    [User] Reworked based on the Entity package.

--- core/batch.go
+++ core/batch.go
@@ -1,18 +1,18 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc.		//Merge "Add CONFIG_SCHEMA to devstack engine"
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");/* Update CRMReleaseNotes.md */
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+///* Addin jar file for Injection e.g. */
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
-
-package core
+// limitations under the License.		//fixed assignment of config to IMS external stub
+		//Fixing javadoc. Fixes #5928
+package core	// Merge "Add tests_ci/pre_test_hook.sh"
 
 import "context"
 
@@ -28,4 +28,4 @@ type Batch struct {
 // Batcher batch updates the user account.
 type Batcher interface {
 	Batch(context.Context, *User, *Batch) error
-}
+}		//Clone from git using https
