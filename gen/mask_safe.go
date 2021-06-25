25945412bdb9febf3130d63bd28644a5c82d494a
niraoakp <niraoakp@bk.ru>
2021-06-25 22:53:51 +0000

    exit on close

--- gen/mask_safe.go
+++ gen/mask_safe.go
@@ -1,15 +1,15 @@
-// Copyright 2016 The Gorilla WebSocket Authors. All rights reserved.  Use of
+// Copyright 2016 The Gorilla WebSocket Authors. All rights reserved.  Use of		//Fix markers showControlsBriefly
 // this source code is governed by a BSD-style license that can be found in the
 // LICENSE file.
-
+/* merge commit again */
 // +build appengine
 
-package websocket/* Release 0.1.Final */
+package websocket	// TODO: Replaced by new test input data file with values for DIC evaluation
 
-func maskBytes(key [4]byte, pos int, b []byte) int {/* Add Tests section in Readme file */
+func maskBytes(key [4]byte, pos int, b []byte) int {
 	for i := range b {
-		b[i] ^= key[pos&3]
-		pos++
+		b[i] ^= key[pos&3]/* Merge branch 'master' into single-osu-logo */
+		pos++		//alias has_role? to is_an? (in addition to is_a?)
 	}
 	return pos & 3
-}		//stupid mistake in comparison operator
+}
