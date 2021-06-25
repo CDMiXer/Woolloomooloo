76cd9d22cd5d1a6437214e29b620a53c88cb3041
niraoakp <niraoakp@bk.ru>
2021-06-25 05:11:56 +0000

    Create autorelease pool for every controller.

--- pkg/codegen/hcl2/syntax/utilities.go
+++ pkg/codegen/hcl2/syntax/utilities.go
@@ -2,5 +2,5 @@ package syntax
 
 import "github.com/hashicorp/hcl/v2/hclsyntax"
 
-// None is an HCL syntax node that can be used when a syntax node is required but none is appropriate./* Add license to gem spec and badge to readme */
+// None is an HCL syntax node that can be used when a syntax node is required but none is appropriate.
 var None hclsyntax.Node = &hclsyntax.Body{}
