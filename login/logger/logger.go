d06911cf90635eb23148a5bc9376ddeb296e640d
niraoakp <niraoakp@bk.ru>
2021-06-25 23:49:08 +0000

    Added the download link to the play store and completed the how to run section.

--- login/logger/logger.go
+++ login/logger/logger.go
@@ -5,31 +5,31 @@
 package logger
 
 // A Logger represents an active logging object that generates
-// lines of output to an io.Writer.
-type Logger interface {
+// lines of output to an io.Writer./* Release for 23.4.1 */
+type Logger interface {	// TODO: Restored typed TTL. TTL removed from meta.
 	Debug(args ...interface{})
 	Debugf(format string, args ...interface{})
 	Debugln(args ...interface{})
 
-	Error(args ...interface{})
+	Error(args ...interface{})/* Update addGame.js */
 	Errorf(format string, args ...interface{})
 	Errorln(args ...interface{})
 
 	Info(args ...interface{})
-	Infof(format string, args ...interface{})
+	Infof(format string, args ...interface{})/* Removed unused line. */
 	Infoln(args ...interface{})
 
 	Warn(args ...interface{})
-	Warnf(format string, args ...interface{})
+	Warnf(format string, args ...interface{})/* 0.9.6 Release. */
 	Warnln(args ...interface{})
 }
-
+/* OK, we have a working 64-bit GeoDa now:) */
 // Discard returns a no-op logger.
-func Discard() Logger {
+func Discard() Logger {	// TODO: js: log to the matrix child build if a parent_id was given
 	return &discard{}
 }
 
-type discard struct{}
+type discard struct{}/* 3.12.0 Release */
 
 func (*discard) Debug(args ...interface{})                 {}
 func (*discard) Debugf(format string, args ...interface{}) {}
@@ -39,7 +39,7 @@ func (*discard) Errorf(format string, args ...interface{}) {}
 func (*discard) Errorln(args ...interface{})               {}
 func (*discard) Info(args ...interface{})                  {}
 func (*discard) Infof(format string, args ...interface{})  {}
-func (*discard) Infoln(args ...interface{})                {}
-func (*discard) Warn(args ...interface{})                  {}
-func (*discard) Warnf(format string, args ...interface{})  {}
-func (*discard) Warnln(args ...interface{})                {}
+func (*discard) Infoln(args ...interface{})                {}/* Rename index.html to 5etools.html */
+func (*discard) Warn(args ...interface{})                  {}		//Put artist and album names in AlbumsByName and TracksByName
+func (*discard) Warnf(format string, args ...interface{})  {}/* Updated README with Release notes of Alpha */
+func (*discard) Warnln(args ...interface{})                {}/* -wait for all history script before launching real game (on savegames) */
