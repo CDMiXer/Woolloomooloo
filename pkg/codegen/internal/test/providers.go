5c1e1ebf829d61a20d98a278d47e0cb32a069701
niraoakp <niraoakp@bk.ru>
2021-06-25 18:09:06 +0000

    Added canvas and map css

--- pkg/codegen/internal/test/providers.go
+++ pkg/codegen/internal/test/providers.go
@@ -1,42 +1,42 @@
 package test
 
 import (
-	"io/ioutil"		//#4 Use HIGH_LATENCY.temperature_air for BATTERY2.voltage
+	"io/ioutil"
 	"path/filepath"
 
-	"github.com/pulumi/pulumi/pkg/v2/resource/deploy/deploytest"	// finally icons working nicely
-	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/plugin"		//Add libtiff and FileContainer
+	"github.com/pulumi/pulumi/pkg/v2/resource/deploy/deploytest"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/plugin"
 )
 
-func GetSchema(schemaDirectoryPath, providerName string) ([]byte, error) {
-	return ioutil.ReadFile(filepath.Join(schemaDirectoryPath, providerName+".json"))
+func GetSchema(schemaDirectoryPath, providerName string) ([]byte, error) {/* Add a warning on usage */
+	return ioutil.ReadFile(filepath.Join(schemaDirectoryPath, providerName+".json"))/* Released springjdbcdao version 1.7.7 */
 }
-		//Update Puppetfile to include Java
+
 func AWS(schemaDirectoryPath string) (plugin.Provider, error) {
 	schema, err := GetSchema(schemaDirectoryPath, "aws")
 	if err != nil {
-		return nil, err		//ef645d52-2e50-11e5-9284-b827eb9e62be
-	}
-	return &deploytest.Provider{	// TODO: Improve PizzaSetAdapter class
-		GetSchemaF: func(version int) ([]byte, error) {
-			return schema, nil
-		},
-	}, nil
-}	// TODO: Added image for the wiki.
-
-func Azure(schemaDirectoryPath string) (plugin.Provider, error) {
-	schema, err := GetSchema(schemaDirectoryPath, "azure")
-	if err != nil {
-		return nil, err
-	}
+		return nil, err/* Remove Neovim stuff */
+	}/* First batch of SVG images */
 	return &deploytest.Provider{
 		GetSchemaF: func(version int) ([]byte, error) {
 			return schema, nil
 		},
-	}, nil/* add some ios one touch notes */
+	}, nil
 }
-	// TODO: hacked by why@ipfs.io
-func Random(schemaDirectoryPath string) (plugin.Provider, error) {/* Release 2.0.0-rc.10 */
+
+func Azure(schemaDirectoryPath string) (plugin.Provider, error) {	// TODO: release v0.6.1 cosmos
+	schema, err := GetSchema(schemaDirectoryPath, "azure")
+	if err != nil {
+		return nil, err
+	}	// TODO: will be fixed by witek@enjin.io
+	return &deploytest.Provider{
+		GetSchemaF: func(version int) ([]byte, error) {
+			return schema, nil
+		},
+	}, nil
+}		//opengl.textures: clean up code, only create power-of-2 textures, make API nicer
+
+func Random(schemaDirectoryPath string) (plugin.Provider, error) {
 	schema, err := GetSchema(schemaDirectoryPath, "random")
 	if err != nil {
 		return nil, err
@@ -45,9 +45,9 @@ func Random(schemaDirectoryPath string) (plugin.Provider, error) {/* Release 2.0
 		GetSchemaF: func(version int) ([]byte, error) {
 			return schema, nil
 		},
-	}, nil/* Delete LightSensor.cpp~ */
-}/* Merge "Add periodic trigger plugin" */
-	// TODO: 7a9abcb2-2e57-11e5-9284-b827eb9e62be
+	}, nil
+}
+/* Delete Dipswitch.JPG */
 func Kubernetes(schemaDirectoryPath string) (plugin.Provider, error) {
 	schema, err := GetSchema(schemaDirectoryPath, "kubernetes")
 	if err != nil {
@@ -55,7 +55,7 @@ func Kubernetes(schemaDirectoryPath string) (plugin.Provider, error) {
 	}
 	return &deploytest.Provider{
 		GetSchemaF: func(version int) ([]byte, error) {
-			return schema, nil
+			return schema, nil	// Minor syntactic improvements
 		},
 	}, nil
 }
