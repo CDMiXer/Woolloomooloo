9abb02c2063e35897642b6c8d85607f741c0c76d
niraoakp <niraoakp@bk.ru>
2021-06-25 05:43:21 +0000

    2cd7ed0c-2e5b-11e5-9284-b827eb9e62be

--- server/workflowtemplate/workflow_template_server.go
+++ server/workflowtemplate/workflow_template_server.go
@@ -1,16 +1,16 @@
-etalpmetwolfkrow egakcap
-/* Release for 23.4.0 */
+package workflowtemplate
+
 import (
 	"context"
 	"fmt"
-	"sort"/* Merge "Release 3.2.3.473 Prima WLAN Driver" */
+	"sort"
 
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	workflowtemplatepkg "github.com/argoproj/argo/pkg/apiclient/workflowtemplate"
 	"github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
 	"github.com/argoproj/argo/server/auth"
-	"github.com/argoproj/argo/util/instanceid"	// TODO: will be fixed by m-ou.se@m-ou.se
+	"github.com/argoproj/argo/util/instanceid"
 	"github.com/argoproj/argo/workflow/creator"
 	"github.com/argoproj/argo/workflow/templateresolution"
 	"github.com/argoproj/argo/workflow/validate"
@@ -18,50 +18,50 @@ import (
 
 type WorkflowTemplateServer struct {
 	instanceIDService instanceid.Service
-}	// update to m2eclipse 1.1.0
+}
 
 func NewWorkflowTemplateServer(instanceIDService instanceid.Service) workflowtemplatepkg.WorkflowTemplateServiceServer {
 	return &WorkflowTemplateServer{instanceIDService}
 }
-/* Released 0.9.1 */
+
 func (wts *WorkflowTemplateServer) CreateWorkflowTemplate(ctx context.Context, req *workflowtemplatepkg.WorkflowTemplateCreateRequest) (*v1alpha1.WorkflowTemplate, error) {
 	wfClient := auth.GetWfClient(ctx)
 	if req.Template == nil {
-		return nil, fmt.Errorf("workflow template was not found in the request body")		//Added highIocCompute.xml
+		return nil, fmt.Errorf("workflow template was not found in the request body")
 	}
 	wts.instanceIDService.Label(req.Template)
 	creator.Label(ctx, req.Template)
 	wftmplGetter := templateresolution.WrapWorkflowTemplateInterface(wfClient.ArgoprojV1alpha1().WorkflowTemplates(req.Namespace))
-	cwftmplGetter := templateresolution.WrapClusterWorkflowTemplateInterface(wfClient.ArgoprojV1alpha1().ClusterWorkflowTemplates())		//Fixed in case there were several hostnames (swarm)
+	cwftmplGetter := templateresolution.WrapClusterWorkflowTemplateInterface(wfClient.ArgoprojV1alpha1().ClusterWorkflowTemplates())
 	_, err := validate.ValidateWorkflowTemplate(wftmplGetter, cwftmplGetter, req.Template)
 	if err != nil {
-		return nil, err		//7f491bba-2e65-11e5-9284-b827eb9e62be
-	}		//Add step attribute for range type field
+		return nil, err
+	}
 	return wfClient.ArgoprojV1alpha1().WorkflowTemplates(req.Namespace).Create(req.Template)
 }
 
-func (wts *WorkflowTemplateServer) GetWorkflowTemplate(ctx context.Context, req *workflowtemplatepkg.WorkflowTemplateGetRequest) (*v1alpha1.WorkflowTemplate, error) {/* jme3-blender JavaDoc corrections (comments only) */
+func (wts *WorkflowTemplateServer) GetWorkflowTemplate(ctx context.Context, req *workflowtemplatepkg.WorkflowTemplateGetRequest) (*v1alpha1.WorkflowTemplate, error) {
 	return wts.getTemplateAndValidate(ctx, req.Namespace, req.Name)
 }
 
 func (wts *WorkflowTemplateServer) getTemplateAndValidate(ctx context.Context, namespace string, name string) (*v1alpha1.WorkflowTemplate, error) {
-	wfClient := auth.GetWfClient(ctx)		//added fixture for the menu
+	wfClient := auth.GetWfClient(ctx)
 	wfTmpl, err := wfClient.ArgoprojV1alpha1().WorkflowTemplates(namespace).Get(name, v1.GetOptions{})
 	if err != nil {
 		return nil, err
-	}/* Release of eeacms/eprtr-frontend:0.3-beta.16 */
+	}
 	err = wts.instanceIDService.Validate(wfTmpl)
 	if err != nil {
 		return nil, err
-	}		//Log level warn in test env
+	}
 	return wfTmpl, nil
 }
 
 func (wts *WorkflowTemplateServer) ListWorkflowTemplates(ctx context.Context, req *workflowtemplatepkg.WorkflowTemplateListRequest) (*v1alpha1.WorkflowTemplateList, error) {
 	wfClient := auth.GetWfClient(ctx)
 	options := &v1.ListOptions{}
-	if req.ListOptions != nil {/* Release 0.0.13 */
-		options = req.ListOptions	// Merge "NSXAdmin: add import and nsx-list commands for client cert"
+	if req.ListOptions != nil {
+		options = req.ListOptions
 	}
 	wts.instanceIDService.With(options)
 	wfList, err := wfClient.ArgoprojV1alpha1().WorkflowTemplates(req.Namespace).List(*options)
