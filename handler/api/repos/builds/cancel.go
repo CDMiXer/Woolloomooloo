ff90f0cfd685301b7662db489bd8168b1f726343
niraoakp <niraoakp@bk.ru>
2021-06-25 18:13:38 +0000

    Job: #9334 Update parent POM to point at new version of ant

--- handler/api/repos/builds/cancel.go
+++ handler/api/repos/builds/cancel.go
@@ -1,59 +1,59 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Merge "Release 3.2.3.440 Prima WLAN Driver" */
-// you may not use this file except in compliance with the License./* Release v2.5 */
-// You may obtain a copy of the License at		//RBAC: Parse sub resource meta data as well
-//	// Socket.io test: manual add/remove active socket
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Rename 3_sublime_text.md to atom.md */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package builds
 
 import (
-	"context"/* Added actions for the received events */
+	"context"
 	"net/http"
 	"strconv"
 	"time"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"/* Release for 21.1.0 */
+	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
 
 	"github.com/go-chi/chi"
-)		//more Fran fixes
-/* Log to MumbleBetaLog.txt file for BetaReleases. */
+)
+
 // HandleCancel returns an http.HandlerFunc that processes http
-// requests to cancel a pending or running build.	// TODO: hacked by julia@jvns.ca
+// requests to cancel a pending or running build.
 func HandleCancel(
 	users core.UserStore,
 	repos core.RepositoryStore,
-	builds core.BuildStore,/* Adding RangeFacet capabilities */
+	builds core.BuildStore,
 	stages core.StageStore,
-	steps core.StepStore,		//Fix missing hooks
+	steps core.StepStore,
 	status core.StatusService,
 	scheduler core.Scheduler,
 	webhooks core.WebhookSender,
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
-			namespace = chi.URLParam(r, "owner")/* Community Crosswords v3.6.2 Release */
+			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
 		)
 
-		number, err := strconv.ParseInt(chi.URLParam(r, "number"), 10, 64)/* Upgrade Core */
+		number, err := strconv.ParseInt(chi.URLParam(r, "number"), 10, 64)
 		if err != nil {
 			render.BadRequest(w, err)
 			return
 		}
 
 		repo, err := repos.FindName(r.Context(), namespace, name)
-		if err != nil {/* Release notes for 0.3 */
-			logger.FromRequest(r)./* Release of eeacms/forests-frontend:2.1.11 */
+		if err != nil {
+			logger.FromRequest(r).
 				WithError(err).
 				WithField("namespace", namespace).
 				WithField("name", name).
