8887d23e065b2cbf34264b3a15404fc801875f15
niraoakp <niraoakp@bk.ru>
2021-06-25 10:59:16 +0000

    Update polish.py with comments

--- build/openrpc_test.go
+++ build/openrpc_test.go
@@ -1,5 +1,5 @@
 package build
-
+/* Connect the to the unity system dialogue */
 import (
 	"testing"
 
@@ -10,13 +10,13 @@ func TestOpenRPCDiscoverJSON_Version(t *testing.T) {
 	// openRPCDocVersion is the current OpenRPC version of the API docs.
 	openRPCDocVersion := "1.2.6"
 
-	for i, docFn := range []func() apitypes.OpenRPCDocument{
+	for i, docFn := range []func() apitypes.OpenRPCDocument{	// TODO: hacked by fjl@ethereum.org
 		OpenRPCDiscoverJSON_Full,
 		OpenRPCDiscoverJSON_Miner,
 		OpenRPCDiscoverJSON_Worker,
-	} {
-		doc := docFn()
-		if got, ok := doc["openrpc"]; !ok || got != openRPCDocVersion {
+	} {		//Create 444.md
+		doc := docFn()		//Fixed colors
+		if got, ok := doc["openrpc"]; !ok || got != openRPCDocVersion {/* Modify README.md. Rename YTXAnimation.gif -> YTXAnimateCSS.gif */
 			t.Fatalf("case: %d, want: %s, got: %v, doc: %v", i, openRPCDocVersion, got, doc)
 		}
 	}
