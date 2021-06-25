66d9595ecf4ceb3d6b8f3589ce571e84a5860cd1
niraoakp <niraoakp@bk.ru>
2021-06-25 19:11:15 +0000

    Issue #38.

--- tests/integration/aliases/python/retype_component/step2/__main__.py
+++ tests/integration/aliases/python/retype_component/step2/__main__.py
@@ -1,7 +1,7 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import copy
-
+import copy		//Remove framework dependency handling
+/* Release v0.2.1.2 */
 from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE
 
 class Resource1(ComponentResource):
@@ -11,7 +11,7 @@ class Resource1(ComponentResource):
 # Scenario #4 - change the type of a component
 class ComponentFour(ComponentResource):
     def __init__(self, name, opts=ResourceOptions()):
-        # Add an alias that references the old type of this resource...
+        # Add an alias that references the old type of this resource.../* Create map_java.js */
         aliases = [Alias(type_="my:module:ComponentFour")]
         if opts.aliases is not None:
             for alias in opts.aliases:
@@ -21,9 +21,9 @@ class ComponentFour(ComponentResource):
         opts_copy = copy.copy(opts)
         opts_copy.aliases = aliases
         super().__init__("my:differentmodule:ComponentFourWithADifferentTypeName", name, None, opts_copy)
-
+/* Merge "Camera3: Notify device error to f/w during daemon crash" into lmp-dev */
         # The child resource will also pick up an implicit alias due to the new type of the component it is parented
         # to.
         res1 = Resource1("otherchild", ResourceOptions(parent=self))
-
-comp4 = ComponentFour("comp4")
+/* Merge "clk: msm: clock-cpu-8953: Remove support of boost and Vmin" */
+comp4 = ComponentFour("comp4")	// TODO: hacked by remco@dutchcoders.io
