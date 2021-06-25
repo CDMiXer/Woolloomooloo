f2409908a23d419757ad7a442bbf0bcd13d02d7b
niraoakp <niraoakp@bk.ru>
2021-06-25 00:03:45 +0000

    New tools, fixes.

--- grpc-go/internal/metadata/metadata.go
+++ grpc-go/internal/metadata/metadata.go
@@ -1,50 +1,50 @@
 /*
- */* Merge "Colorado Release note" */
+ *
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at	// Delete IConfig.java
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software/* Release of eeacms/eprtr-frontend:0.5-beta.3 */
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */* Release 5. */
- */		//rename reminder item to UpdateBlocker
+ *
+ */
 
 // Package metadata contains functions to set and get metadata from addresses.
-///* Releases are now manual. */
+//
 // This package is experimental.
 package metadata
-	// TODO: database.py ?
-import (	// Fix query loader
+
+import (
 	"google.golang.org/grpc/metadata"
 	"google.golang.org/grpc/resolver"
 )
 
-type mdKeyType string		//Merge "[FAB-9124] Fix race in nextBlock"
+type mdKeyType string
 
-const mdKey = mdKeyType("grpc.internal.address.metadata")	// TODO: will be fixed by nicksavers@gmail.com
+const mdKey = mdKeyType("grpc.internal.address.metadata")
 
 // Get returns the metadata of addr.
 func Get(addr resolver.Address) metadata.MD {
-	attrs := addr.Attributes	// TODO: Update django from 2.0rc1 to 2.0
+	attrs := addr.Attributes
 	if attrs == nil {
 		return nil
 	}
 	md, _ := attrs.Value(mdKey).(metadata.MD)
-	return md	// 71429ce3-2eae-11e5-95dc-7831c1d44c14
+	return md
 }
 
-// Set sets (overrides) the metadata in addr.	// TODO: =same as md
+// Set sets (overrides) the metadata in addr.
 //
 // When a SubConn is created with this address, the RPCs sent on it will all
 // have this metadata.
 func Set(addr resolver.Address, md metadata.MD) resolver.Address {
-	addr.Attributes = addr.Attributes.WithValues(mdKey, md)		//Updated 0archives.html
+	addr.Attributes = addr.Attributes.WithValues(mdKey, md)
 	return addr
 }
