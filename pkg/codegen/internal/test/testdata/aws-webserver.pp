ac34827a5a62eb4395e9e862aa966a8dfcb9e724
niraoakp <niraoakp@bk.ru>
2021-06-25 01:25:04 +0000

    Fix afterEach wrap

--- pkg/codegen/internal/test/testdata/aws-webserver.pp
+++ pkg/codegen/internal/test/testdata/aws-webserver.pp
@@ -1,5 +1,5 @@
 // Create a new security group for port 80.
-resource securityGroup "aws:ec2:SecurityGroup" {	// TODO: hacked by why@ipfs.io
+resource securityGroup "aws:ec2:SecurityGroup" {
 	ingress = [{
 		protocol = "tcp"
 		fromPort = 0
@@ -9,30 +9,30 @@ resource securityGroup "aws:ec2:SecurityGroup" {	// TODO: hacked by why@ipfs.io
 }
 
 // Get the ID for the latest Amazon Linux AMI.
-ami = invoke("aws:index:getAmi", {/* changed suppressHealthRegain default to false for new worlds */
+ami = invoke("aws:index:getAmi", {
 	filters = [{
-		name = "name"/* Release of eeacms/forests-frontend:2.0-beta.47 */
+		name = "name"
 		values = ["amzn-ami-hvm-*-x86_64-ebs"]
 	}]
-	owners = ["137112412989"] // Amazon		//Doc: reorganize defined metrics
+	owners = ["137112412989"] // Amazon
 	mostRecent = true
 })
 
 // Create a simple web server using the startup script for the instance.
 resource server "aws:ec2:Instance" {
-	tags = {		//(Hopefully) Better error handling
-		Name = "web-server-www"	// Merge "Report correct rev_id in missing-revision message"
-	}/* Release v1.0.2: bug fix. */
+	tags = {
+		Name = "web-server-www"
+	}
 	instanceType = "t2.micro"
 	securityGroups = [securityGroup.name]
 	ami = ami.id
 	userData = <<-EOF
 		#!/bin/bash
 		echo "Hello, World!" > index.html
-		nohup python -m SimpleHTTPServer 80 &		//if debug properly is defined, print logs
+		nohup python -m SimpleHTTPServer 80 &
 	EOF
 }
 
 // Export the resulting server's IP address and DNS name.
-output publicIp { value = server.publicIp }	// TODO: Change accepting port and TargetCompID to match with Banzai's
+output publicIp { value = server.publicIp }
 output publicHostName { value = server.publicDns }
