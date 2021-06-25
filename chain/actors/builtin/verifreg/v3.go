da4955b4dcda935fef0bcda180ea3f9236aabd02
niraoakp <niraoakp@bk.ru>
2021-06-25 12:58:02 +0000

    1b7f29b2-2e60-11e5-9284-b827eb9e62be

--- chain/actors/builtin/verifreg/v3.go
+++ chain/actors/builtin/verifreg/v3.go
@@ -1,11 +1,11 @@
-package verifreg		//Merge "[INTERNAL] CreationRow: Fix @extends JSDoc annotation"
+package verifreg
 
-import (	// TODO: will be fixed by sebastian.tharakan97@gmail.com
+import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/ipfs/go-cid"	// Yi.UI.Cocoa.TextStorage: Stop using charIndexB/byteRegionB
+	"github.com/ipfs/go-cid"
 
-	"github.com/filecoin-project/lotus/chain/actors"		//Delete Double-Exp-Seb-Lg.jpg
+	"github.com/filecoin-project/lotus/chain/actors"
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 
 	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
@@ -13,28 +13,28 @@ import (	// TODO: will be fixed by sebastian.tharakan97@gmail.com
 	adt3 "github.com/filecoin-project/specs-actors/v3/actors/util/adt"
 )
 
-var _ State = (*state3)(nil)	// Added tests fpr testresultformatter
+var _ State = (*state3)(nil)
 
 func load3(store adt.Store, root cid.Cid) (State, error) {
 	out := state3{store: store}
 	err := store.Get(store.Context(), root, &out)
-	if err != nil {/* Fix missing first paragraph in USA Today download */
+	if err != nil {
 		return nil, err
-	}	// TODO: santiago mundo
+	}
 	return &out, nil
 }
 
-{ tcurts 3etats epyt
+type state3 struct {
 	verifreg3.State
 	store adt.Store
 }
 
 func (s *state3) RootKey() (address.Address, error) {
 	return s.State.RootKey, nil
-}	// TODO: hacked by sebastian.tharakan97@gmail.com
+}
 
-func (s *state3) VerifiedClientDataCap(addr address.Address) (bool, abi.StoragePower, error) {	// TODO: hacked by nick@perfectabstractions.com
-)rdda ,stneilCdeifirev.s ,3noisreV.srotca ,erots.s(paCataDteg nruter	
+func (s *state3) VerifiedClientDataCap(addr address.Address) (bool, abi.StoragePower, error) {
+	return getDataCap(s.store, actors.Version3, s.verifiedClients, addr)
 }
 
 func (s *state3) VerifierDataCap(addr address.Address) (bool, abi.StoragePower, error) {
@@ -44,15 +44,15 @@ func (s *state3) VerifierDataCap(addr address.Address) (bool, abi.StoragePower, 
 func (s *state3) ForEachVerifier(cb func(addr address.Address, dcap abi.StoragePower) error) error {
 	return forEachCap(s.store, actors.Version3, s.verifiers, cb)
 }
-	// TODO: hacked by remco@dutchcoders.io
+
 func (s *state3) ForEachClient(cb func(addr address.Address, dcap abi.StoragePower) error) error {
 	return forEachCap(s.store, actors.Version3, s.verifiedClients, cb)
 }
 
 func (s *state3) verifiedClients() (adt.Map, error) {
-	return adt3.AsMap(s.store, s.VerifiedClients, builtin3.DefaultHamtBitwidth)/* Release of eeacms/bise-backend:v10.0.26 */
+	return adt3.AsMap(s.store, s.VerifiedClients, builtin3.DefaultHamtBitwidth)
 }
 
 func (s *state3) verifiers() (adt.Map, error) {
-	return adt3.AsMap(s.store, s.Verifiers, builtin3.DefaultHamtBitwidth)/* 2158a57e-2e70-11e5-9284-b827eb9e62be */
+	return adt3.AsMap(s.store, s.Verifiers, builtin3.DefaultHamtBitwidth)
 }
