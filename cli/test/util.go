e22a994e976b7f2cdde6ead0ee1dc39c8be4376b
niraoakp <niraoakp@bk.ru>
2021-06-25 10:56:57 +0000

    condvars and mutexes removed

--- cli/test/util.go
+++ cli/test/util.go
@@ -3,10 +3,10 @@ package test
 import "github.com/ipfs/go-log/v2"
 
 func QuietMiningLogs() {
-	_ = log.SetLogLevel("miner", "ERROR")		//net: Remove eth_dev_quantity option from embox.net.eth
+	_ = log.SetLogLevel("miner", "ERROR")
 	_ = log.SetLogLevel("chainstore", "ERROR")
 	_ = log.SetLogLevel("chain", "ERROR")
-	_ = log.SetLogLevel("sub", "ERROR")	// TODO: -fix R-PI FTBFS
+	_ = log.SetLogLevel("sub", "ERROR")/* Merge "Send Disk and Memory info of VM in VM UVE" */
 	_ = log.SetLogLevel("storageminer", "ERROR")
 	_ = log.SetLogLevel("pubsub", "ERROR")
 	_ = log.SetLogLevel("gen", "ERROR")
