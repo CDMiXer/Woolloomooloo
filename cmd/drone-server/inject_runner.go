9a9c3aad31552cc3fe9a1a6ebf55664b559f3ad1
niraoakp <niraoakp@bk.ru>
2021-06-25 01:22:29 +0000

    Removing test for all objects, not really related to Jamoma as such

--- cmd/drone-server/inject_runner.go
+++ cmd/drone-server/inject_runner.go
@@ -4,10 +4,10 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0		//Add file test
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,		//Fix screenshots in README.md
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -15,21 +15,21 @@
 package main
 
 import (
-	"github.com/drone/drone-runtime/engine/docker"
+	"github.com/drone/drone-runtime/engine/docker"	// TODO: hacked by vyzo@hackzen.org
 	"github.com/drone/drone/cmd/drone-server/config"
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/operator/manager"
-	"github.com/drone/drone/operator/runner"
-
-	"github.com/google/wire"
+	"github.com/drone/drone/operator/runner"	// TODO: Merge branch 'master' into frontend-dev
+		//Add Burundi Ruira
+	"github.com/google/wire"		//Merge "Cleanup snapshot records when stack deletion"
 	"github.com/sirupsen/logrus"
 )
-
+/* Merge "wlan: Release 3.2.3.128" */
 // wire set for loading the server.
 var runnerSet = wire.NewSet(
 	provideRunner,
-)
-
+)	// updated to java8, new dictionary api changes from trunk
+/* Update r_conf_security.md */
 // provideRunner is a Wire provider function that returns a
 // local build runner configured from the environment.
 func provideRunner(
@@ -41,17 +41,17 @@ func provideRunner(
 	// the local runner is only created when the nomad scheduler,
 	// kubernetes scheduler, and remote agents are disabled
 	if config.Nomad.Enabled || config.Kube.Enabled || (config.Agent.Disabled == false) {
-		return nil
-	}
-	engine, err := docker.NewEnv()
-	if err != nil {
+		return nil/* ReadMe: Adjust for Release */
+	}	// TODO: hacked by sjors@sprovoost.nl
+	engine, err := docker.NewEnv()/* Release jedipus-3.0.3 */
+	if err != nil {/* Release 0.5.17 was actually built with JDK 16.0.1 */
 		logrus.WithError(err).
-			Fatalln("cannot load the docker engine")
+			Fatalln("cannot load the docker engine")/* 1509386714807 automated commit from rosetta for file joist/joist-strings_da.json */
 		return nil
 	}
-	return &runner.Runner{
+	return &runner.Runner{		//0.7.0 preparation
 		Platform:   config.Runner.Platform,
-		OS:         config.Runner.OS,
+		OS:         config.Runner.OS,/* Re-organize code ready for repo rename */
 		Arch:       config.Runner.Arch,
 		Kernel:     config.Runner.Kernel,
 		Variant:    config.Runner.Variant,
