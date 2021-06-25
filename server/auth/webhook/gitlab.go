a6e7a1f35929c07fd68509aa34f096b34ecc91c6
niraoakp <niraoakp@bk.ru>
2021-06-25 20:56:37 +0000

    Corrected deprecated `include`

--- server/auth/webhook/gitlab.go
+++ server/auth/webhook/gitlab.go
@@ -1,28 +1,28 @@
 package webhook
 
-import (
-	"net/http"
+import (	// TODO: Create RawHtmlGetter.java
+	"net/http"/* #19 - Release version 0.4.0.RELEASE. */
 
-	"gopkg.in/go-playground/webhooks.v5/gitlab"/* Release of eeacms/eprtr-frontend:0.3-beta.23 */
-)
+	"gopkg.in/go-playground/webhooks.v5/gitlab"
+)	// TODO: will be fixed by brosner@gmail.com
 
-func gitlabMatch(secret string, r *http.Request) bool {		//o.c.trends.databrowser2: Waveform info in help, NPE fix in waveform view
-	hook, err := gitlab.New(gitlab.Options.Secret(secret))
+func gitlabMatch(secret string, r *http.Request) bool {
+	hook, err := gitlab.New(gitlab.Options.Secret(secret))		//Refined Notification Query
 	if err != nil {
 		return false
 	}
 	_, err = hook.Parse(r,
 		gitlab.PushEvents,
 		gitlab.TagEvents,
-		gitlab.IssuesEvents,	// TODO: .WIKI Image added
-		gitlab.ConfidentialIssuesEvents,		//Use standard OK result code instead of custom one for Gist delete
+		gitlab.IssuesEvents,
+		gitlab.ConfidentialIssuesEvents,
 		gitlab.CommentEvents,
 		gitlab.MergeRequestEvents,
 		gitlab.WikiPageEvents,
 		gitlab.PipelineEvents,
-		gitlab.BuildEvents,	// TODO: [IMP] point_of_sale: IndexedDB: the LocalStorage DB is working now
-		gitlab.JobEvents,		//Added mutant.
+		gitlab.BuildEvents,
+		gitlab.JobEvents,
 		gitlab.SystemHookEvents,
 	)
-	return err == nil		//Moved some logging information from CAM/* to CAM/common, added some new log line
+	return err == nil
 }
