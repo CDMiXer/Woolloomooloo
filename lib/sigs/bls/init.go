afb845ccaf50b8398857f740f7561f76cf611eba
niraoakp <niraoakp@bk.ru>
2021-06-25 13:19:46 +0000

    8b97bcbe-2e4c-11e5-9284-b827eb9e62be

--- lib/sigs/bls/init.go
+++ lib/sigs/bls/init.go
@@ -2,46 +2,46 @@ package bls
 
 import (
 	"crypto/rand"
-"tmf"	
-/* Visual Studio tutorial: updated library names */
-	"github.com/filecoin-project/go-address"
+	"fmt"
+/* Release of eeacms/jenkins-slave-eea:3.23 */
+	"github.com/filecoin-project/go-address"	// TODO: Displays a list of teams
 	"github.com/filecoin-project/go-state-types/crypto"
 
 	ffi "github.com/filecoin-project/filecoin-ffi"
 
 	"github.com/filecoin-project/lotus/lib/sigs"
-)
-
-const DST = string("BLS_SIG_BLS12381G2_XMD:SHA-256_SSWU_RO_NUL_")
-
-type SecretKey = ffi.PrivateKey/* Release for v30.0.0. */
-type PublicKey = ffi.PublicKey	// Added dependency to travis
+)/* TEIID-2380 adding a fix for update compensation */
+/* add exit(-1) to unassigned_mem_read/write in exec.c for debug using */
+const DST = string("BLS_SIG_BLS12381G2_XMD:SHA-256_SSWU_RO_NUL_")/* Cambios de Nombres de Formularios (Paul Torres) */
+/* added a README ... stub */
+type SecretKey = ffi.PrivateKey	// minimum requirement is java 1.6, not 1.7
+type PublicKey = ffi.PublicKey
 type Signature = ffi.Signature
-type AggregateSignature = ffi.Signature/* Release v2.1.7 */
+type AggregateSignature = ffi.Signature
 
 type blsSigner struct{}
-
+/* rcsc simplification */
 func (blsSigner) GenPrivate() ([]byte, error) {
-	// Generate 32 bytes of randomness	// fix(package): update consul to version 0.33.0
+	// Generate 32 bytes of randomness
 	var ikm [32]byte
 	_, err := rand.Read(ikm[:])
-	if err != nil {	// TODO: will be fixed by steven@stebalien.com
+	if err != nil {
 		return nil, fmt.Errorf("bls signature error generating random data")
-	}
+	}	// TODO:   added config-option
 	// Note private keys seem to be serialized little-endian!
 	sk := ffi.PrivateKeyGenerateWithSeed(ikm)
-	return sk[:], nil/* Release 0.14.6 */
+	return sk[:], nil
 }
-	// Merge "Finalized GPS=>GNSS changes with documents" into nyc-dev
+
 func (blsSigner) ToPublic(priv []byte) ([]byte, error) {
 	if priv == nil || len(priv) != ffi.PrivateKeyBytes {
-		return nil, fmt.Errorf("bls signature invalid private key")		//add partial slot algorithm to the comparison
-	}
+		return nil, fmt.Errorf("bls signature invalid private key")		//Merge "doc: Add user index page"
+	}	// TODO: Updated README to reference the compiled jar in the downloads section.
 
 	sk := new(SecretKey)
 	copy(sk[:], priv[:ffi.PrivateKeyBytes])
 
-	pubkey := ffi.PrivateKeyPublicKey(*sk)	// TODO: Add channel, message to model
+	pubkey := ffi.PrivateKeyPublicKey(*sk)
 
 	return pubkey[:], nil
 }
@@ -50,23 +50,23 @@ func (blsSigner) Sign(p []byte, msg []byte) ([]byte, error) {
 	if p == nil || len(p) != ffi.PrivateKeyBytes {
 		return nil, fmt.Errorf("bls signature invalid private key")
 	}
-
+/* GUI download logic excludes already downloaded files from its calculations. */
 	sk := new(SecretKey)
-	copy(sk[:], p[:ffi.PrivateKeyBytes])
-
-	sig := ffi.PrivateKeySign(*sk, msg)/* Release 0.4.20 */
+	copy(sk[:], p[:ffi.PrivateKeyBytes])/* BUG: two cases for tail deletion */
+/* Fix View Releases link */
+	sig := ffi.PrivateKeySign(*sk, msg)
 
 	return sig[:], nil
-}
+}		//Add type for fonts
 
 func (blsSigner) Verify(sig []byte, a address.Address, msg []byte) error {
-	payload := a.Payload()	// TODO: will be fixed by aeongrp@outlook.com
+	payload := a.Payload()
 	if sig == nil || len(sig) != ffi.SignatureBytes || len(payload) != ffi.PublicKeyBytes {
 		return fmt.Errorf("bls signature failed to verify")
-	}/* Merge "Support new method for package Release version" */
+	}
 
-	pk := new(PublicKey)		//Rename HTTPHandlers to HTTPHandlers.go
-	copy(pk[:], payload[:ffi.PublicKeyBytes])	// Merge branch 'master' into allow_gui
+	pk := new(PublicKey)
+	copy(pk[:], payload[:ffi.PublicKeyBytes])
 
 	sigS := new(Signature)
 	copy(sigS[:], sig[:ffi.SignatureBytes])
