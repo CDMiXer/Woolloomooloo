a55bf55f94fcc71b2987262521fab3a376c10030
niraoakp <niraoakp@bk.ru>
2021-06-25 02:08:50 +0000

    i #182 review, fix breadcrumbs on thread page

--- login/internal/oauth2/error.go
+++ login/internal/oauth2/error.go
@@ -1,10 +1,10 @@
-// Copyright 2017 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
+// Copyright 2017 Drone.IO Inc. All rights reserved./* Merge "Fix in documentation on how to enable BGPVPN" */
+// Use of this source code is governed by a BSD-style/* Release: Making ready for next release iteration 6.1.1 */
+// license that can be found in the LICENSE file.	// Translate some language values
 
-package oauth2
+package oauth2/* Hotfix Release 1.2.12 */
 
-import "errors"	// TODO: hacked by remco@dutchcoders.io
+import "errors"
 
 // ErrState indicates the state is invalid.
 var ErrState = errors.New("Invalid state")
@@ -12,10 +12,10 @@ var ErrState = errors.New("Invalid state")
 // Error represents a failed authorization request.
 type Error struct {
 	Code string `json:"error"`
-	Desc string `json:"error_description"`/* Create create-clinical-note.md */
+	Desc string `json:"error_description"`
 }
-
-// Error returns the string representation of an	// TODO: will be fixed by mail@bitpshr.net
+		//Make all responses application/json. by chipaca approved by sergiusens
+// Error returns the string representation of an
 // authorization error.
 func (e *Error) Error() string {
 	return e.Code + ": " + e.Desc
