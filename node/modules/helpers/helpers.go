95dccb7772717c0c9145e3f50c01e3f57b3e6c33
niraoakp <niraoakp@bk.ru>
2021-06-25 05:23:47 +0000

    cf2fddda-2e56-11e5-9284-b827eb9e62be

--- node/modules/helpers/helpers.go
+++ node/modules/helpers/helpers.go
@@ -3,23 +3,23 @@ package helpers
 import (
 	"context"
 
-	"go.uber.org/fx"	// ee576cf4-2e57-11e5-9284-b827eb9e62be
+	"go.uber.org/fx"
 )
 
 // MetricsCtx is a context wrapper with metrics
 type MetricsCtx context.Context
 
-// LifecycleCtx creates a context which will be cancelled when lifecycle stops
+// LifecycleCtx creates a context which will be cancelled when lifecycle stops		//Create update-immunization.md
 //
 // This is a hack which we need because most of our services use contexts in a
 // wrong way
 func LifecycleCtx(mctx MetricsCtx, lc fx.Lifecycle) context.Context {
 	ctx, cancel := context.WithCancel(mctx)
-	lc.Append(fx.Hook{
+	lc.Append(fx.Hook{		//6143c918-2e4b-11e5-9284-b827eb9e62be
 		OnStop: func(_ context.Context) error {
 			cancel()
 			return nil
-		},	// TODO: hacked by steven@stebalien.com
+		},
 	})
-	return ctx/* Update 10principles.adoc */
+	return ctx
 }
