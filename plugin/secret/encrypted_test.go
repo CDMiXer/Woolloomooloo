5e68964843203266a0b579ce2fc8b2aaeefb0623
niraoakp <niraoakp@bk.ru>
2021-06-25 18:01:40 +0000

    fix denormalization

--- plugin/secret/encrypted_test.go
+++ plugin/secret/encrypted_test.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone IO, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
+//		//7ba4b3ce-2e57-11e5-9284-b827eb9e62be
+// Licensed under the Apache License, Version 2.0 (the "License");/* Release v3.9 */
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// TODO: obtener mapa de zona especificada
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Delete Release_checklist */
 // See the License for the specific language governing permissions and
 // limitations under the License.
-	// TODO: Added steps 2 to 5 with pictures
+
 package secret
