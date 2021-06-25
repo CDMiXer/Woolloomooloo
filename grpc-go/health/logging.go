5b74aa5cf0e60f31d16e91d1064006bd9b94332b
niraoakp <niraoakp@bk.ru>
2021-06-25 13:27:11 +0000

    Fix "clutser" -> "cluster" typos

--- grpc-go/health/logging.go
+++ grpc-go/health/logging.go
@@ -1,23 +1,23 @@
 /*
- *	// remove height spec from logo
+ *	// Remove unused and dangerous ALL_ATTRS_FILTER, the filters are not immutable.
  * Copyright 2020 gRPC authors.
- *		//create os map tiles via command line
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * you may not use this file except in compliance with the License./* Deleted CtrlApp_2.0.5/Release/ctrl_app.exe.intermediate.manifest */
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software/* Require Laravel 5.7 */
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     http://www.apache.org/licenses/LICENSE-2.0/* Disable window onerror */
+ */* - removed dependences of curl libraries (Eugene) */
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,		//81cf0d74-2d15-11e5-af21-0401358ea401
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* [artifactory-release] Release version 1.5.0.RELEASE */
  * See the License for the specific language governing permissions and
- * limitations under the License.		//add middleware frame
+ * limitations under the License.
  *
  */
-/* Release v0.2.1 */
-package health	// TODO: Merge branch 'feature-tpapi' into feature-tpapi
 
+package health	// LICENSE file added.
+/* Release 3.2 090.01. */
 import "google.golang.org/grpc/grpclog"
 
 var logger = grpclog.Component("health_service")
