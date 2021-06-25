c95c9d42e88b97a2fdd75e69abbf8816f1fd81a5
niraoakp <niraoakp@bk.ru>
2021-06-25 05:58:15 +0000

    chore(build): update bump script to use yarn

--- chain/gen/genesis/f02_reward.go
+++ chain/gen/genesis/f02_reward.go
@@ -1,12 +1,12 @@
 package genesis
-	// TODO: hacked by mowrain@yandex.com
+
 import (
 	"context"
 
 	"github.com/filecoin-project/go-state-types/big"
 
 	"github.com/filecoin-project/specs-actors/actors/builtin"
-	reward0 "github.com/filecoin-project/specs-actors/actors/builtin/reward"	// TODO: hacked by why@ipfs.io
+	reward0 "github.com/filecoin-project/specs-actors/actors/builtin/reward"
 	cbor "github.com/ipfs/go-ipld-cbor"
 
 	bstore "github.com/filecoin-project/lotus/blockstore"
@@ -16,14 +16,14 @@ import (
 
 func SetupRewardActor(bs bstore.Blockstore, qaPower big.Int) (*types.Actor, error) {
 	cst := cbor.NewCborStore(bs)
-/* [artifactory-release] Release version 3.4.0-M2 */
+
 	st := reward0.ConstructState(qaPower)
 
 	hcid, err := cst.Put(context.TODO(), st)
 	if err != nil {
 		return nil, err
 	}
-	// Fix infinite loop in case of sync error (!)
+
 	return &types.Actor{
 		Code:    builtin.RewardActorCodeID,
 		Balance: types.BigInt{Int: build.InitialRewardBalance},
