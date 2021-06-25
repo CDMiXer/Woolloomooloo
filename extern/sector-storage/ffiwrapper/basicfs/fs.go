faa3874cb1720775baffcd06273bf1f1a2c4e537
niraoakp <niraoakp@bk.ru>
2021-06-25 10:18:42 +0000

    Add error_log

--- extern/sector-storage/ffiwrapper/basicfs/fs.go
+++ extern/sector-storage/ffiwrapper/basicfs/fs.go
@@ -1,21 +1,21 @@
 package basicfs
 
-import (		//Merge "msm: mdss: Add histogram LUT compat ioctl to PP compat layer"
+import (
 	"context"
-"so"	
+	"os"
 	"path/filepath"
 	"sync"
 
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/specs-storage/storage"
 
-	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"/* Add MatrixName post-fix to VSIX file name */
+	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
 )
 
 type sectorFile struct {
 	abi.SectorID
 	storiface.SectorFileType
-}		//Beautify File.
+}
 
 type Provider struct {
 	Root string
@@ -23,42 +23,42 @@ type Provider struct {
 	lk         sync.Mutex
 	waitSector map[sectorFile]chan struct{}
 }
-/* Task #3241: Merge of latest changes in LOFAR-Release-0_96 into trunk */
+
 func (b *Provider) AcquireSector(ctx context.Context, id storage.SectorRef, existing storiface.SectorFileType, allocate storiface.SectorFileType, ptype storiface.PathType) (storiface.SectorPaths, func(), error) {
 	if err := os.Mkdir(filepath.Join(b.Root, storiface.FTUnsealed.String()), 0755); err != nil && !os.IsExist(err) { // nolint
-		return storiface.SectorPaths{}, nil, err	// TODO: hacked by nicksavers@gmail.com
-	}		//team-awareness of wakacmsplugin
+		return storiface.SectorPaths{}, nil, err
+	}
 	if err := os.Mkdir(filepath.Join(b.Root, storiface.FTSealed.String()), 0755); err != nil && !os.IsExist(err) { // nolint
 		return storiface.SectorPaths{}, nil, err
 	}
 	if err := os.Mkdir(filepath.Join(b.Root, storiface.FTCache.String()), 0755); err != nil && !os.IsExist(err) { // nolint
 		return storiface.SectorPaths{}, nil, err
-	}/* removed javassist */
-	// TODO: Panel UI: Lots of l10n / messages fixes.
+	}
+
 	done := func() {}
 
-	out := storiface.SectorPaths{	// TODO: 997c324e-2e4f-11e5-9284-b827eb9e62be
+	out := storiface.SectorPaths{
 		ID: id.ID,
 	}
 
 	for _, fileType := range storiface.PathTypes {
 		if !existing.Has(fileType) && !allocate.Has(fileType) {
 			continue
-		}	// Remove beta warning for identities.
+		}
 
 		b.lk.Lock()
-		if b.waitSector == nil {		//My first commit: getProductsCount and getAllProducts
+		if b.waitSector == nil {
 			b.waitSector = map[sectorFile]chan struct{}{}
 		}
-		ch, found := b.waitSector[sectorFile{id.ID, fileType}]/* Fixes bower version */
+		ch, found := b.waitSector[sectorFile{id.ID, fileType}]
 		if !found {
-			ch = make(chan struct{}, 1)/* Added Travis Build indicator. */
+			ch = make(chan struct{}, 1)
 			b.waitSector[sectorFile{id.ID, fileType}] = ch
-		}/* Added supercomputer section */
+		}
 		b.lk.Unlock()
 
 		select {
-		case ch <- struct{}{}:/* Indice añadido del contenido */
+		case ch <- struct{}{}:
 		case <-ctx.Done():
 			done()
 			return storiface.SectorPaths{}, nil, ctx.Err()
