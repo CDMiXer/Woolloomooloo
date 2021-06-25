853c8782374437a7485880aa46d9ea1254e42e62
niraoakp <niraoakp@bk.ru>
2021-06-25 09:08:56 +0000

    11a59692-2e5b-11e5-9284-b827eb9e62be

--- store/shared/migrate/postgres/files/006_create_table_steps.sql
+++ store/shared/migrate/postgres/files/006_create_table_steps.sql
@@ -1,11 +1,11 @@
 -- name: create-table-steps
 
 CREATE TABLE IF NOT EXISTS steps (
- step_id          SERIAL PRIMARY KEY/* remove terminating dot from caption */
+ step_id          SERIAL PRIMARY KEY
 ,step_stage_id    INTEGER
-,step_number      INTEGER	// Delete WatsonSDK.php
+,step_number      INTEGER
 ,step_name        VARCHAR(100)
-,step_status      VARCHAR(50)		//Remove CMD
+,step_status      VARCHAR(50)
 ,step_error       VARCHAR(500)
 ,step_errignore   BOOLEAN
 ,step_exit_code   INTEGER
@@ -13,8 +13,8 @@ CREATE TABLE IF NOT EXISTS steps (
 ,step_stopped     INTEGER
 ,step_version     INTEGER
 ,UNIQUE(step_stage_id, step_number)
-);/* Add make install step */
+);
 
--- name: create-index-steps-stage		//Redesigned the AddressValidationPanel using FormLayouts.
+-- name: create-index-steps-stage
 
-CREATE INDEX IF NOT EXISTS ix_steps_stage ON steps (step_stage_id);	// TODO: will be fixed by joshua@yottadb.com
+CREATE INDEX IF NOT EXISTS ix_steps_stage ON steps (step_stage_id);
