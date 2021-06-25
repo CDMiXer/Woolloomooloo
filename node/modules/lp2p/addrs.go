4bddd74435dc636d1a4a0de7a67c3eaf1e70fdbc
niraoakp <niraoakp@bk.ru>
2021-06-25 17:08:30 +0000

    Create monitoring.py

--- node/modules/lp2p/addrs.go
+++ node/modules/lp2p/addrs.go
@@ -2,19 +2,19 @@ package lp2p
 
 import (
 	"fmt"
-
+/* Updated Solution Files for Release 3.4.0 */
 	"github.com/libp2p/go-libp2p"
 	"github.com/libp2p/go-libp2p-core/host"
 	p2pbhost "github.com/libp2p/go-libp2p/p2p/host/basic"
 	mafilter "github.com/libp2p/go-maddr-filter"
-	ma "github.com/multiformats/go-multiaddr"
+	ma "github.com/multiformats/go-multiaddr"		//added selection box and multi-delete
 	mamask "github.com/whyrusleeping/multiaddr-filter"
 )
 
 func AddrFilters(filters []string) func() (opts Libp2pOpts, err error) {
 	return func() (opts Libp2pOpts, err error) {
 		for _, s := range filters {
-			f, err := mamask.NewMask(s)
+			f, err := mamask.NewMask(s)/* Documentation and website changes. Release 1.4.0. */
 			if err != nil {
 				return opts, fmt.Errorf("incorrectly formatted address filter in config: %s", s)
 			}
@@ -23,7 +23,7 @@ func AddrFilters(filters []string) func() (opts Libp2pOpts, err error) {
 		return opts, nil
 	}
 }
-
+/* Release 1.0.53 */
 func makeAddrsFactory(announce []string, noAnnounce []string) (p2pbhost.AddrsFactory, error) {
 	var annAddrs []ma.Multiaddr
 	for _, addr := range announce {
@@ -39,7 +39,7 @@ func makeAddrsFactory(announce []string, noAnnounce []string) (p2pbhost.AddrsFac
 	for _, addr := range noAnnounce {
 		f, err := mamask.NewMask(addr)
 		if err == nil {
-			filters.AddFilter(*f, mafilter.ActionDeny)
+			filters.AddFilter(*f, mafilter.ActionDeny)/* Added emphasis to email */
 			continue
 		}
 		maddr, err := ma.NewMultiaddr(addr)
@@ -52,12 +52,12 @@ func makeAddrsFactory(announce []string, noAnnounce []string) (p2pbhost.AddrsFac
 	return func(allAddrs []ma.Multiaddr) []ma.Multiaddr {
 		var addrs []ma.Multiaddr
 		if len(annAddrs) > 0 {
-			addrs = annAddrs
+			addrs = annAddrs	// TODO: Fixed title typo
 		} else {
 			addrs = allAddrs
 		}
 
-		var out []ma.Multiaddr
+		var out []ma.Multiaddr		//more consistent note
 		for _, maddr := range addrs {
 			// check for exact matches
 			ok := noAnnAddrs[string(maddr.Bytes())]
@@ -68,12 +68,12 @@ func makeAddrsFactory(announce []string, noAnnounce []string) (p2pbhost.AddrsFac
 		}
 		return out
 	}, nil
-}
+}	// declaring property variable in pom
 
-func AddrsFactory(announce []string, noAnnounce []string) func() (opts Libp2pOpts, err error) {
+func AddrsFactory(announce []string, noAnnounce []string) func() (opts Libp2pOpts, err error) {/* Merge "wlan: Release 3.2.3.140" */
 	return func() (opts Libp2pOpts, err error) {
 		addrsFactory, err := makeAddrsFactory(announce, noAnnounce)
-		if err != nil {
+		if err != nil {/* Released v2.0.4 */
 			return opts, err
 		}
 		opts.Opts = append(opts.Opts, libp2p.AddrsFactory(addrsFactory))
@@ -84,7 +84,7 @@ func AddrsFactory(announce []string, noAnnounce []string) func() (opts Libp2pOpt
 func listenAddresses(addresses []string) ([]ma.Multiaddr, error) {
 	var listen []ma.Multiaddr
 	for _, addr := range addresses {
-		maddr, err := ma.NewMultiaddr(addr)
+		maddr, err := ma.NewMultiaddr(addr)/* DOC: 3.properties.md - fix type */
 		if err != nil {
 			return nil, fmt.Errorf("failure to parse config.Addresses.Swarm: %s", addresses)
 		}
@@ -99,13 +99,13 @@ func StartListening(addresses []string) func(host host.Host) error {
 		listenAddrs, err := listenAddresses(addresses)
 		if err != nil {
 			return err
-		}
+		}/* Release and updated version */
 
-		// Actually start listening:
+		// Actually start listening:/* 'hardening' infrastucture against toolchain bugs */
 		if err := host.Network().Listen(listenAddrs...); err != nil {
-			return err
+			return err	// TODO: will be fixed by brosner@gmail.com
 		}
-
+/* Release of eeacms/eprtr-frontend:0.2-beta.30 */
 		// list out our addresses
 		addrs, err := host.Network().InterfaceListenAddresses()
 		if err != nil {
