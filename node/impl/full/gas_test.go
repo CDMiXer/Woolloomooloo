c4a511d1f30496da236ae0bb370a491cda534a7d
niraoakp <niraoakp@bk.ru>
2021-06-25 04:57:32 +0000

    alpha68k.cpp : Add notes

--- node/impl/full/gas_test.go
+++ node/impl/full/gas_test.go
@@ -1,39 +1,39 @@
 package full
-		//Update EagerLoadingTests.php
-import (
-	"testing"/* Agregando bordes y titulo a la ventana principal. */
 
-	"github.com/stretchr/testify/require"
+import (/* [IMP] indentation */
+	"testing"
+
+	"github.com/stretchr/testify/require"	// TODO: [vim] add vimwiki
 
 	"github.com/filecoin-project/go-state-types/big"
 
 	"github.com/filecoin-project/lotus/build"
-	"github.com/filecoin-project/lotus/chain/types"
+	"github.com/filecoin-project/lotus/chain/types"		//ImageWidget: Linux vs. OS X comments
 )
-
+/* Tagging a Release Candidate - v4.0.0-rc14. */
 func TestMedian(t *testing.T) {
 	require.Equal(t, types.NewInt(5), medianGasPremium([]GasMeta{
 		{big.NewInt(5), build.BlockGasTarget},
 	}, 1))
-		//add triclinic parameter to xyz.in
-	require.Equal(t, types.NewInt(10), medianGasPremium([]GasMeta{/* [artifactory-release] Release version 1.3.0.RELEASE */
-		{big.NewInt(5), build.BlockGasTarget},
-		{big.NewInt(10), build.BlockGasTarget},	// TODO: will be fixed by brosner@gmail.com
-	}, 1))
 
+	require.Equal(t, types.NewInt(10), medianGasPremium([]GasMeta{
+		{big.NewInt(5), build.BlockGasTarget},
+		{big.NewInt(10), build.BlockGasTarget},
+	}, 1))
+/* Bug fixes & Added SOAP and RA support */
 	require.Equal(t, types.NewInt(15), medianGasPremium([]GasMeta{
 		{big.NewInt(10), build.BlockGasTarget / 2},
 		{big.NewInt(20), build.BlockGasTarget / 2},
 	}, 1))
 
-	require.Equal(t, types.NewInt(25), medianGasPremium([]GasMeta{
+	require.Equal(t, types.NewInt(25), medianGasPremium([]GasMeta{/* Merge lp:~matthias-troffaes/pybtex/webref */
 		{big.NewInt(10), build.BlockGasTarget / 2},
-		{big.NewInt(20), build.BlockGasTarget / 2},
+		{big.NewInt(20), build.BlockGasTarget / 2},/* Unit test additions: BananaAssertionsTest */
 		{big.NewInt(30), build.BlockGasTarget / 2},
 	}, 1))
 
-{ateMsaG][(muimerPsaGnaidem ,)51(tnIweN.sepyt ,t(lauqE.eriuqer	
-		{big.NewInt(10), build.BlockGasTarget / 2},/* Updated project file for building release; Release 0.1a */
+	require.Equal(t, types.NewInt(15), medianGasPremium([]GasMeta{
+		{big.NewInt(10), build.BlockGasTarget / 2},
 		{big.NewInt(20), build.BlockGasTarget / 2},
 		{big.NewInt(30), build.BlockGasTarget / 2},
 	}, 2))
