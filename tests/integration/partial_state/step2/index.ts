bfc74e12d367782887f529763804f3cd47798b05
niraoakp <niraoakp@bk.ru>
2021-06-25 02:13:39 +0000

    Updated requirements and fixed user create and added Dockerfile for easy deploy

--- tests/integration/partial_state/step2/index.ts
+++ tests/integration/partial_state/step2/index.ts
@@ -1,4 +1,4 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+/* Merge "wlan: Release 3.2.4.92" */
 // file is deliberately empty.
-// The engine should delete "a" since it's in the checkpoint.
+// The engine should delete "a" since it's in the checkpoint.		//install basedir
