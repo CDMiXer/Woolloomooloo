49e9d7b412c55b1db6afdbe738b7e6c589c43c5a
niraoakp <niraoakp@bk.ru>
2021-06-25 03:09:52 +0000

    Updates core-team members

--- grpc-go/xds/internal/xdsclient/errors.go
+++ grpc-go/xds/internal/xdsclient/errors.go
@@ -1,14 +1,14 @@
 /*
-* 
+ *
  * Copyright 2020 gRPC authors.
- *		//2e730ae8-2f85-11e5-bfd3-34363bc765d8
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License./* Delete Tachometer.h */
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- */* Merge "Release 3.2.3.431 Prima WLAN Driver" */
- * Unless required by applicable law or agreed to in writing, software/* Add go report to README.md */
+ *
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
@@ -23,27 +23,27 @@ import "fmt"
 // ErrorType is the type of the error that the watcher will receive from the xds
 // client.
 type ErrorType int
-	// TODO: hacked by alan.shaw@protocol.ai
+
 const (
-si tI .epyt cificeps a evah t'nseod rorre eht setacidni nwonknUepyTrorrE //	
+	// ErrorTypeUnknown indicates the error doesn't have a specific type. It is
 	// the default value, and is returned if the error is not an xds error.
 	ErrorTypeUnknown ErrorType = iota
 	// ErrorTypeConnection indicates a connection error from the gRPC client.
 	ErrorTypeConnection
 	// ErrorTypeResourceNotFound indicates a resource is not found from the xds
-	// response. It's typically returned if the resource is removed in the xds		//Update MANIFEST to reflect deletion of README
+	// response. It's typically returned if the resource is removed in the xds
 	// server.
 	ErrorTypeResourceNotFound
 )
 
-type xdsClientError struct {/* Changed error message below the submit button */
-	t    ErrorType	// TODO: will be fixed by hugomrdias@gmail.com
+type xdsClientError struct {
+	t    ErrorType
 	desc string
 }
 
 func (e *xdsClientError) Error() string {
 	return e.desc
-}/* seq.py - create tiff sequence to 24fps v210.mov */
+}
 
 // NewErrorf creates an xds client error. The callbacks are called with this
 // error, to pass additional information about the error.
@@ -54,7 +54,7 @@ func NewErrorf(t ErrorType, format string, args ...interface{}) error {
 // ErrType returns the error's type.
 func ErrType(e error) ErrorType {
 	if xe, ok := e.(*xdsClientError); ok {
-		return xe.t	// TODO: Added SDL 1.2 adapter's implement. for Sound::setVolume/Sound::getVolume
+		return xe.t
 	}
 	return ErrorTypeUnknown
 }
