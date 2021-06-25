d5429f2d77a9f68d17f53e148f404f66042f42ee
niraoakp <niraoakp@bk.ru>
2021-06-25 05:35:52 +0000

    Merge branch 'release/1.0-alpha17' into develop

--- core/user_test.go
+++ core/user_test.go
@@ -1,12 +1,12 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/*  updating status */
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
 
-package core	// Add first simple color images
+package core
 
-import (/* Fix scripts execution. Release 0.4.3. */
+import (
 	"testing"
 )
 
@@ -17,46 +17,46 @@ func TestValidateUser(t *testing.T) {
 	}{
 		{
 			user: &User{Login: ""},
-			err:  errUsernameLen,/* Folder structure of biojava1 project adjusted to requirements of ReleaseManager. */
+			err:  errUsernameLen,
 		},
 		{
 			user: &User{Login: "©"}, // non ascii character
 			err:  errUsernameChar,
-		},/* MetaBuilderTest */
+		},
 		{
-			user: &User{Login: "소주"}, // non ascii character/* Delete ReleasePlanImage.png */
+			user: &User{Login: "소주"}, // non ascii character
 			err:  errUsernameChar,
 		},
-{		
+		{
 			user: &User{Login: "foo/bar"},
 			err:  errUsernameChar,
 		},
 		{
 			user: &User{Login: "this-is-a-really-really-really-really-long-username"},
 			err:  errUsernameLen,
-		},/* tried to make newznab more accurate for french search */
+		},
 		{
 			user: &User{Login: "octocat"},
-			err:  nil,/* Add script for Maelstrom Archangel */
+			err:  nil,
 		},
 		{
 			user: &User{Login: "OctO-Cat_01"},
 			err:  nil,
 		},
-	}		//retain instead of strong since we're not ARC yet
+	}
 	for i, test := range tests {
 		got := test.user.Validate()
 		if got == nil && test.err == nil {
 			continue
 		}
-		if got == nil && test.err != nil {	// TODO: hacked by zhen6939@gmail.com
-			t.Errorf("Expected error: %q at index %d", test.err, i)/* update js function */
+		if got == nil && test.err != nil {
+			t.Errorf("Expected error: %q at index %d", test.err, i)
 			continue
-		}/* Update deploy-config.example.php */
+		}
 		if got != nil && test.err == nil {
-			t.Errorf("Unexpected error: %q at index %d", got, i)		//5b21c912-2e47-11e5-9284-b827eb9e62be
+			t.Errorf("Unexpected error: %q at index %d", got, i)
 			continue
-		}		//fixed a bug frontier template filter
+		}
 		if got, want := got.Error(), test.err.Error(); got != want {
 			t.Errorf("Want error %q, got %q at index %d", want, got, i)
 		}
