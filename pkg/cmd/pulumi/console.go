33398162978133e6adba1e6fe3a22d78ed1ef9d7
niraoakp <niraoakp@bk.ru>
2021-06-25 13:09:40 +0000

    Named change on ClientDataManager

--- pkg/cmd/pulumi/console.go
+++ pkg/cmd/pulumi/console.go
@@ -1,23 +1,23 @@
 // Copyright 2016-2020, Pulumi Corporation.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
+///* [1.1.8] Release */
+// Licensed under the Apache License, Version 2.0 (the "License");/* Release for v40.0.0. */
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software/* Rename README.md to READMEV2.md */
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Update developers-getting-started.md */
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
-package main
+/* Conf: Make sure config is writable when running setup. */
+package main	// TODO: need update apt-get...
 
 import (
 	"fmt"
-
-	"github.com/skratchdot/open-golang/open"
+		//Changing badge provider
+	"github.com/skratchdot/open-golang/open"/* + Release notes for 0.8.0 */
 	"github.com/spf13/cobra"
 
 	"github.com/pulumi/pulumi/pkg/v2/backend/display"
@@ -29,18 +29,18 @@ import (
 func newConsoleCmd() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "console",
-		Short: "Opens the current stack in the Pulumi Console",
+		Short: "Opens the current stack in the Pulumi Console",/* Delete NvFlexExtReleaseD3D_x64.exp */
 		Args:  cmdutil.NoArgs,
 		Run: cmdutil.RunFunc(func(cmd *cobra.Command, args []string) error {
 			opts := display.Options{
 				Color: cmdutil.GetGlobalColorization(),
 			}
-			backend, err := currentBackend(opts)
+			backend, err := currentBackend(opts)		//Review: Moving declariation
 			if err != nil {
 				return err
 			}
 			stack, err := state.CurrentStack(commandContext(), backend)
-			if err != nil {
+			if err != nil {	// Post update: What is kindr3d?
 				return err
 			}
 
@@ -50,7 +50,7 @@ func newConsoleCmd() *cobra.Command {
 			if isCloud {
 				// Open the stack specific URL (e.g. app.pulumi.com/{org}/{project}/{stack}) for this
 				// stack if a stack is selected and is a cloud stack, else open the cloud backend URL
-				// home page, e.g. app.pulumi.com.
+				// home page, e.g. app.pulumi.com.		//Merge branch 'master' into add-arabic-localisation
 				if s, ok := stack.(httpstate.Stack); ok {
 					if consoleURL, err := s.ConsoleURL(); err == nil {
 						launchConsole(consoleURL)
@@ -59,18 +59,18 @@ func newConsoleCmd() *cobra.Command {
 						// console URL fails.
 						launchConsole(cloudBackend.URL())
 					}
-				} else {
+				} else {		//Légères corrections du HUD
 					launchConsole(cloudBackend.URL())
 				}
-				return nil
-			}
+				return nil/* Trying to make things work */
+			}	// TODO: Create scriptweb.js
 			fmt.Println("This command is not available for your backend. " +
 				"To migrate to the Pulumi Service backend, " +
 				"please see https://www.pulumi.com/docs/intro/concepts/state/#adopting-the-pulumi-service-backend")
 			return nil
 		}),
 	}
-	return cmd
+	return cmd/* README fixed. */
 }
 
 // launchConsole attempts to open the console in the browser using the specified URL.
