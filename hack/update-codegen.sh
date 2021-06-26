673a715341857aae99fef1d7fd36a5d8b2a9a3ff
niraoakp <niraoakp@bk.ru>
2021-06-26 13:22:37 +0000

    Delete not existing import

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/bin/bash	// TODO: hacked by peterke@gmail.com
 set -eux -o pipefail
-
+	// Updated theme.tid
 bash ${GOPATH}/pkg/mod/k8s.io/code-generator@v0.17.5/generate-groups.sh \
   "deepcopy,client,informer,lister" \
-  github.com/argoproj/argo/pkg/client github.com/argoproj/argo/pkg/apis \
+  github.com/argoproj/argo/pkg/client github.com/argoproj/argo/pkg/apis \		//11ea3a62-2e6b-11e5-9284-b827eb9e62be
   workflow:v1alpha1 \
   --go-header-file ./hack/custom-boilerplate.go.txt
