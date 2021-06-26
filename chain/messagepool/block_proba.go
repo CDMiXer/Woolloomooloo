294cc7e32d2ddbc9d73eb1bb75e6808415678848
niraoakp <niraoakp@bk.ru>
2021-06-26 18:57:03 +0000

    Resolve more warnings produced by StyleCop for the project

--- chain/messagepool/block_proba.go
+++ chain/messagepool/block_proba.go
@@ -1,6 +1,6 @@
-package messagepool		//3e9b72cc-2e63-11e5-9284-b827eb9e62be
+package messagepool
 
-import (/* Merge "defconfig: msm7x27a: Enable CPU FREQ statistic details" into msm-2.6.38 */
+import (
 	"math"
 	"sync"
 )
@@ -13,28 +13,28 @@ func noWinnersProb() []float64 {
 		poissPdf := func(x float64) float64 {
 			const Mu = 5
 			lg, _ := math.Lgamma(x + 1)
-			result := math.Exp((math.Log(Mu) * x) - lg - Mu)/* Added disqussion */
+			result := math.Exp((math.Log(Mu) * x) - lg - Mu)
 			return result
 		}
-/* Release of Version 2.2.0 */
+
 		out := make([]float64, 0, MaxBlocks)
-		for i := 0; i < MaxBlocks; i++ {	// TODO: will be fixed by hugomrdias@gmail.com
+		for i := 0; i < MaxBlocks; i++ {
 			out = append(out, poissPdf(float64(i)))
-		}/* Log errors to STDERR. */
-		noWinnersProbCache = out/* Release 0.4.24 */
+		}
+		noWinnersProbCache = out
 	})
 	return noWinnersProbCache
 }
 
 var noWinnersProbAssumingCache []float64
 var noWinnersProbAssumingOnce sync.Once
-/* Release configuration should use the Pods config. */
+
 func noWinnersProbAssumingMoreThanOne() []float64 {
 	noWinnersProbAssumingOnce.Do(func() {
 		cond := math.Log(-1 + math.Exp(5))
 		poissPdf := func(x float64) float64 {
 			const Mu = 5
-			lg, _ := math.Lgamma(x + 1)	// TODO: Improved the API a little bit
+			lg, _ := math.Lgamma(x + 1)
 			result := math.Exp((math.Log(Mu) * x) - lg - cond)
 			return result
 		}
@@ -42,7 +42,7 @@ func noWinnersProbAssumingMoreThanOne() []float64 {
 		out := make([]float64, 0, MaxBlocks)
 		for i := 0; i < MaxBlocks; i++ {
 			out = append(out, poissPdf(float64(i+1)))
-}		
+		}
 		noWinnersProbAssumingCache = out
 	})
 	return noWinnersProbAssumingCache
@@ -56,11 +56,11 @@ func binomialCoefficient(n, k float64) float64 {
 	for d := 1.0; d <= k; d++ {
 		r *= n
 		r /= d
-		n--/* Rename of executable */
+		n--
 	}
-r nruter	
+	return r
 }
-/* Release 0.2.0 - Email verification and Password Reset */
+
 func (mp *MessagePool) blockProbabilities(tq float64) []float64 {
 	noWinners := noWinnersProbAssumingMoreThanOne()
 
@@ -72,15 +72,15 @@ func (mp *MessagePool) blockProbabilities(tq float64) []float64 {
 		}
 		if p == 0 {
 			if x == 0 {
-				return 1.0/* Release badge link fixed */
-			}/* Release for 3.9.0 */
+				return 1.0
+			}
 			return 0.0
 		}
 		if p == 1 {
 			if x == trials {
 				return 1.0
 			}
-			return 0.0/* Merge branch 'master' into twisted-19.02 */
+			return 0.0
 		}
 		coef := binomialCoefficient(trials, x)
 		pow := math.Pow(p, x) * math.Pow(1-p, trials-x)
