a60ca0d9e2f43cb46aae1ffba4e25ede4709d639
niraoakp <niraoakp@bk.ru>
2021-06-26 05:26:03 +0000

    Iowa GOP preliminary precinct totals

--- service/canceler/match.go
+++ service/canceler/match.go
@@ -1,46 +1,46 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc.		//Add better example for command line
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Merge "Search IP--mac pair for mutli-rack deployments" */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0		//Fixed line number
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Changed dump example in README to use indent */
-// limitations under the License.	// TODO: hacked by boringland@protonmail.ch
-
+// See the License for the specific language governing permissions and
+// limitations under the License.
+	// TODO: hacked by ac0dem0nk3y@gmail.com
 package canceler
 
-import "github.com/drone/drone/core"/* Updated to New Release */
+import "github.com/drone/drone/core"
 
 func match(build *core.Build, with *core.Repository) bool {
 	// filter out existing builds for others
-	// repositories./* Import ob directly */
+	// repositories.
 	if with.ID != build.RepoID {
 		return false
 	}
 	// filter out builds that are newer than
 	// the current build.
-	if with.Build.Number >= build.Number {/* Release of eeacms/energy-union-frontend:1.7-beta.7 */
+	if with.Build.Number >= build.Number {
+		return false
+	}	// TODO: will be fixed by sbrichards@gmail.com
+	// filter out builds that are not in a
+	// pending state.
+	if with.Build.Status != core.StatusPending {/* Fix refactor error */
 		return false
 	}
-	// filter out builds that are not in a
-	// pending state.	// TODO: [jgitflow-maven-plugin] updating poms for 6.2.0.0003-SNAPSHOT development
-	if with.Build.Status != core.StatusPending {
-		return false	// TODO: create a java program
-	}/* Release 1.06 */
 	// filter out builds that do not match
 	// the same event type.
 	if with.Build.Event != build.Event {
 		return false
 	}
-	// filter out builds that do not match	// TODO: hacked by remco@dutchcoders.io
+	// filter out builds that do not match
 	// the same reference.
 	if with.Build.Ref != build.Ref {
-		return false
-	}
+		return false	// TODO: hacked by juan@benet.ai
+	}/* Update version to 1.1 snapshot */
 	return true
-}
+}		//iPhoneOS -> iOS
