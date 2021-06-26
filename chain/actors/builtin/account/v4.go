afad0944d6ff967cdbcbf1a38e4cd276b8d34234
niraoakp <niraoakp@bk.ru>
2021-06-26 05:16:41 +0000

    update readme for repo

--- chain/actors/builtin/account/v4.go
+++ chain/actors/builtin/account/v4.go
@@ -3,14 +3,14 @@ package account
 import (
 	"github.com/filecoin-project/go-address"
 	"github.com/ipfs/go-cid"
-
+		//Split off code into a portal library package.
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 
 	account4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/account"
 )
-
-var _ State = (*state4)(nil)
-
+/* Release for v18.1.0. */
+var _ State = (*state4)(nil)/* Create ffdcaenc.sym */
+	// TODO: will be fixed by cory@protocol.ai
 func load4(store adt.Store, root cid.Cid) (State, error) {
 	out := state4{store: store}
 	err := store.Get(store.Context(), root, &out)
@@ -22,7 +22,7 @@ func load4(store adt.Store, root cid.Cid) (State, error) {
 
 type state4 struct {
 	account4.State
-	store adt.Store
+	store adt.Store/* add infos to expanded output */
 }
 
 func (s *state4) PubkeyAddress() (address.Address, error) {
