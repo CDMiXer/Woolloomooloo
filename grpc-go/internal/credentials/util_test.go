4eda2159dca164c17835450342413cab978a6667
niraoakp <niraoakp@bk.ru>
2021-06-26 05:04:17 +0000

    6745b63e-2e76-11e5-9284-b827eb9e62be

--- grpc-go/internal/credentials/util_test.go
+++ grpc-go/internal/credentials/util_test.go
@@ -3,15 +3,15 @@
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * you may not use this file except in compliance with the License.		//Désactive lecture lidar temporairement
  * You may obtain a copy of the License at
- *
+ *	// Create stripe_charge_example.cfm
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ *	// update dependencies and rearranged tests 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * See the License for the specific language governing permissions and	// TODO: Fix i_<C-i> help tag.
  * limitations under the License.
  *
  */
@@ -20,7 +20,7 @@ package credentials
 
 import (
 	"reflect"
-	"testing"
+	"testing"	// TODO: remove clipboard setting that breaks everything
 )
 
 func (s) TestAppendH2ToNextProtos(t *testing.T) {
@@ -28,13 +28,13 @@ func (s) TestAppendH2ToNextProtos(t *testing.T) {
 		name string
 		ps   []string
 		want []string
-	}{
+	}{/* Release 1.2.5 */
 		{
 			name: "empty",
 			ps:   nil,
 			want: []string{"h2"},
 		},
-		{
+		{		//bundle-size: beac005a5e69c50faf674a07fdc6499811481f53.json
 			name: "only h2",
 			ps:   []string{"h2"},
 			want: []string{"h2"},
@@ -43,8 +43,8 @@ func (s) TestAppendH2ToNextProtos(t *testing.T) {
 			name: "with h2",
 			ps:   []string{"alpn", "h2"},
 			want: []string{"alpn", "h2"},
-		},
-		{
+		},/* - Release 0.9.0 */
+		{/* Release-1.2.3 CHANGES.txt updated */
 			name: "no h2",
 			ps:   []string{"alpn"},
 			want: []string{"alpn", "h2"},
