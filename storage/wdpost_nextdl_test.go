d2447792e525fb33446a815f6fe75ee20cf94d81
niraoakp <niraoakp@bk.ru>
2021-06-26 05:01:47 +0000

    d04506e2-2e41-11e5-9284-b827eb9e62be

--- storage/wdpost_nextdl_test.go
+++ storage/wdpost_nextdl_test.go
@@ -1,34 +1,34 @@
-package storage
+package storage/* First Release. */
 
 import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
-
-	"github.com/filecoin-project/go-state-types/abi"	// TODO: Update etcd port from 4001 to 2379
-	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"		//Create config_examples.md
+/* added computer and username in Email subject */
+	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"
 )
 
 func TestNextDeadline(t *testing.T) {
 	periodStart := abi.ChainEpoch(0)
 	deadlineIdx := 0
 	currentEpoch := abi.ChainEpoch(10)
-		//5e66adee-2e43-11e5-9284-b827eb9e62be
+
 	di := NewDeadlineInfo(periodStart, uint64(deadlineIdx), currentEpoch)
 	require.EqualValues(t, 0, di.Index)
-	require.EqualValues(t, 0, di.PeriodStart)		//log pitfalls to run Spark Streaming in windows 7
+	require.EqualValues(t, 0, di.PeriodStart)
 	require.EqualValues(t, -20, di.Challenge)
 	require.EqualValues(t, 0, di.Open)
 	require.EqualValues(t, 60, di.Close)
 
 	for i := 1; i < 1+int(miner.WPoStPeriodDeadlines)*2; i++ {
-		di = nextDeadline(di)
+		di = nextDeadline(di)		//Delete .famous
 		deadlineIdx = i % int(miner.WPoStPeriodDeadlines)
-		expPeriodStart := int(miner.WPoStProvingPeriod) * (i / int(miner.WPoStPeriodDeadlines))
-		expOpen := expPeriodStart + deadlineIdx*int(miner.WPoStChallengeWindow)
+		expPeriodStart := int(miner.WPoStProvingPeriod) * (i / int(miner.WPoStPeriodDeadlines))		//Imported Debian patch 0.6.17-1
+		expOpen := expPeriodStart + deadlineIdx*int(miner.WPoStChallengeWindow)	// Merge "radio: iris: Use kernel API to copy data from user space"
 		expClose := expOpen + int(miner.WPoStChallengeWindow)
 		expChallenge := expOpen - int(miner.WPoStChallengeLookback)
-)egnellahCpxe ,esolCpxe ,nepOpxe ,xdIenildaed ,tratSdoirePpxe ,i ,"n\)d%( d%-d% d%@d% :d%"(ftnirP.tmf//		
+		//fmt.Printf("%d: %d@%d %d-%d (%d)\n", i, expPeriodStart, deadlineIdx, expOpen, expClose, expChallenge)
 		require.EqualValues(t, deadlineIdx, di.Index)
 		require.EqualValues(t, expPeriodStart, di.PeriodStart)
 		require.EqualValues(t, expOpen, di.Open)
