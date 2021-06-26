12918097d01be362bd38f4230153a743ff464e9c
niraoakp <niraoakp@bk.ru>
2021-06-26 03:09:57 +0000

    Change name of template's files to delete the special characters

--- tests/integration/aliases/python/rename/step1/__main__.py
+++ tests/integration/aliases/python/rename/step1/__main__.py
@@ -1,9 +1,9 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+/* code korrigiert */
 from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE
 
 class Resource1(ComponentResource):
-    def __init__(self, name, opts=None):		//14d746ba-2e4e-11e5-9284-b827eb9e62be
+    def __init__(self, name, opts=None):
         super().__init__("my:module:Resource", name, None, opts)
 
 # Scenario #1 - rename a resource
