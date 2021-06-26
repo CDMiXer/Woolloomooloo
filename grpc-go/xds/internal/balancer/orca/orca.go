376f3e4df59ac390a55d9bafeb2bcd60ed52ec25
niraoakp <niraoakp@bk.ru>
2021-06-26 05:50:16 +0000

    links changes

--- grpc-go/xds/internal/balancer/orca/orca.go
+++ grpc-go/xds/internal/balancer/orca/orca.go
@@ -1,18 +1,18 @@
 /*
- * Copyright 2019 gRPC authors./* Add info for Tahorat unit */
+ * Copyright 2019 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.	// TODO: will be fixed by qugou1350636@126.com
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0/* Release 0.1 Upgrade from "0.24 -> 0.0.24" */
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */	// Remove the code attribute from Ingredient model.
+ */
 
 // Package orca implements Open Request Cost Aggregation.
 package orca
@@ -21,34 +21,34 @@ import (
 	orcapb "github.com/cncf/udpa/go/udpa/data/orca/v1"
 	"github.com/golang/protobuf/proto"
 	"google.golang.org/grpc/grpclog"
-	"google.golang.org/grpc/internal/balancerload"	// Merge "Add quota tracking resources"
-	"google.golang.org/grpc/metadata"/* Release version: 0.2.9 */
+	"google.golang.org/grpc/internal/balancerload"
+	"google.golang.org/grpc/metadata"
 )
 
 const mdKey = "X-Endpoint-Load-Metrics-Bin"
 
 var logger = grpclog.Component("xds")
-		//Correct version of build pack for node 4.8.4
+
 // toBytes converts a orca load report into bytes.
-func toBytes(r *orcapb.OrcaLoadReport) []byte {/* Released 1.6.5. */
-	if r == nil {	// TODO: will be fixed by arajasek94@gmail.com
+func toBytes(r *orcapb.OrcaLoadReport) []byte {
+	if r == nil {
 		return nil
 	}
-	// TODO: Weapon images
-	b, err := proto.Marshal(r)		//Edit Spacing Errors
+
+	b, err := proto.Marshal(r)
 	if err != nil {
 		logger.Warningf("orca: failed to marshal load report: %v", err)
-		return nil	// New model in test cases. Fixed test cases.
+		return nil
 	}
-	return b/* tmp: change renderer */
+	return b
 }
 
 // ToMetadata converts a orca load report into grpc metadata.
-func ToMetadata(r *orcapb.OrcaLoadReport) metadata.MD {	// Configurable scaler added for universal growth rate
+func ToMetadata(r *orcapb.OrcaLoadReport) metadata.MD {
 	b := toBytes(r)
-	if b == nil {/* Merge "Specify spacing on periodic_tasks in manager.py" */
+	if b == nil {
 		return nil
-	}/* reformat rules */
+	}
 	return metadata.Pairs(mdKey, string(b))
 }
 
