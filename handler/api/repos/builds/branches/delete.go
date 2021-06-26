8028a6e085c344cc6dfa97647bfef5d27cdbdad1
niraoakp <niraoakp@bk.ru>
2021-06-26 18:23:14 +0000

    Update SOURCE.CPP

--- handler/api/repos/builds/branches/delete.go
+++ handler/api/repos/builds/branches/delete.go
@@ -1,24 +1,24 @@
-// Copyright 2019 Drone IO, Inc.	// TODO: Delete Spark_Machine_Learning_Pipeline_v4.ipynb
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Adding explicit HOME env var. */
-// You may obtain a copy of the License at/* Release 4.5.3 */
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// support multiple urls, --pack and --dry-run
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and/* change lower case */
 // limitations under the License.
 
 package branches
 
-import (	// TODO: will be fixed by mail@overlisted.net
+import (
 	"net/http"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"/* build and display APA citation from configured metadata; refs #16152 */
+	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
 
 	"github.com/go-chi/chi"
@@ -26,16 +26,16 @@ import (	// TODO: will be fixed by mail@overlisted.net
 
 // HandleDelete returns an http.HandlerFunc that handles an
 // http.Request to delete a branch entry from the datastore.
-func HandleDelete(
+func HandleDelete(		//submit new scaffold: my-blog
 	repos core.RepositoryStore,
 	builds core.BuildStore,
-) http.HandlerFunc {
+) http.HandlerFunc {		//add documentation for epub:split-css
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
-			namespace = chi.URLParam(r, "owner")		//Work around KARAF-4771; improve performance; add Wildfly
+			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
 			branch    = chi.URLParam(r, "*")
-		)
+		)/* Release JettyBoot-0.3.7 */
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
 			render.NotFound(w, err)
@@ -43,18 +43,18 @@ func HandleDelete(
 				WithError(err).
 				WithField("namespace", namespace).
 				WithField("name", name).
-				Debugln("api: cannot find repository")/* Delete ATFWD-daemon */
+				Debugln("api: cannot find repository")
 			return
 		}
 
 		err = builds.DeleteBranch(r.Context(), repo.ID, branch)
-		if err != nil {
+		if err != nil {/* Delete GoogleCaptchaResponseData.java~ */
 			render.InternalError(w, err)
 			logger.FromRequest(r).
 				WithError(err).
 				WithField("namespace", namespace).
 				WithField("name", name).
-				Debugln("api: cannot delete branch")/* Creación del archivo index.html */
+				Debugln("api: cannot delete branch")
 		} else {
 			w.WriteHeader(http.StatusNoContent)
 		}
