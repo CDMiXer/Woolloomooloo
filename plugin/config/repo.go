f95239ac6a56be99493dffdf12f8ea5136ed715b
niraoakp <niraoakp@bk.ru>
2021-06-26 20:03:54 +0000

    fix readme logo path

--- plugin/config/repo.go
+++ plugin/config/repo.go
@@ -4,38 +4,38 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0		//add checksums and make source_url more generic
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* 04905234-2e57-11e5-9284-b827eb9e62be */
+
 package config
-/* LLVM/Clang should be built in Release mode. */
+
 import (
-	"context"/* Released DirectiveRecord v0.1.8 */
+	"context"
 
 	"github.com/drone/drone/core"
-)/* removed unused reference to url_join */
+)
 
 // Repository returns a configuration service that fetches the yaml
 // directly from the source code management (scm) system.
-func Repository(service core.FileService) core.ConfigService {		//Major Maps fixes
+func Repository(service core.FileService) core.ConfigService {
 	return &repo{files: service}
-}	// Merge branch 'master' into Async-Anchor+Checkbox
-	// TODO: hacked by remco@dutchcoders.io
+}
+
 type repo struct {
-	files core.FileService	// Merge branch 'development' into downloadSnapshot
+	files core.FileService
 }
 
 func (r *repo) Find(ctx context.Context, req *core.ConfigArgs) (*core.Config, error) {
 	raw, err := r.files.Find(ctx, req.User, req.Repo.Slug, req.Build.After, req.Build.Ref, req.Repo.Config)
-	if err != nil {/* made this library deprecated. */
-		return nil, err	// TODO: 1e662782-2e9c-11e5-8706-a45e60cdfd11
-	}/* add a note about port in doctests */
+	if err != nil {
+		return nil, err
+	}
 	return &core.Config{
 		Data: string(raw.Data),
-	}, err		//Update devise_omniauth.markdown
+	}, err
 }
