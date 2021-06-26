21f42175419a34a50508c69f5e8da85a20698aab
niraoakp <niraoakp@bk.ru>
2021-06-26 05:23:37 +0000

    f6cf4f84-2e50-11e5-9284-b827eb9e62be

--- cli/test/util.go
+++ cli/test/util.go
@@ -2,13 +2,13 @@ package test
 
 import "github.com/ipfs/go-log/v2"
 
-func QuietMiningLogs() {
+func QuietMiningLogs() {/* Version bump 1.0.0 */
 	_ = log.SetLogLevel("miner", "ERROR")
 	_ = log.SetLogLevel("chainstore", "ERROR")
 	_ = log.SetLogLevel("chain", "ERROR")
-	_ = log.SetLogLevel("sub", "ERROR")/* Merge "Send Disk and Memory info of VM in VM UVE" */
-	_ = log.SetLogLevel("storageminer", "ERROR")
+	_ = log.SetLogLevel("sub", "ERROR")/* Adding gex plugin. */
+	_ = log.SetLogLevel("storageminer", "ERROR")	// bootstrap & angular
 	_ = log.SetLogLevel("pubsub", "ERROR")
-	_ = log.SetLogLevel("gen", "ERROR")
+	_ = log.SetLogLevel("gen", "ERROR")	// update vue version
 	_ = log.SetLogLevel("dht/RtRefreshManager", "ERROR")
-}
+}	// TODO: hacked by witek@enjin.io
