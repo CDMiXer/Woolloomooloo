ff9546212cd2d90c4f5c2c4f9044547501fe8cd9
niraoakp <niraoakp@bk.ru>
2021-06-26 05:36:21 +0000

    [IMP] account: get_fiscal_position returns a recordset

--- login/internal/oauth1/config_test.go
+++ login/internal/oauth1/config_test.go
@@ -1,5 +1,5 @@
 // Copyright 2017 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file./* Removed local activeImage in favor of referencing through the command. */
+// license that can be found in the LICENSE file.
 
-package oauth1	// TODO: hacked by yuvalalaluf@gmail.com
+package oauth1
