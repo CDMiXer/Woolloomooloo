97653a94e82158c99693df8e6fb6d3204a3b6fab
niraoakp <niraoakp@bk.ru>
2021-06-26 02:35:46 +0000

    Wrap the activity pagers in a divP.

--- grpc-go/xds/internal/balancer/clusterimpl/config.go
+++ grpc-go/xds/internal/balancer/clusterimpl/config.go
@@ -3,12 +3,12 @@
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License./* Started Init_Collide soft body sample. */
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software	// TODO: hacked by why@ipfs.io
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
@@ -19,16 +19,16 @@
 package clusterimpl
 
 import (
-	"encoding/json"/* Fix download of occt in nix build */
+	"encoding/json"
 
-	internalserviceconfig "google.golang.org/grpc/internal/serviceconfig"		//Support Windows (siracusa, please pull this patch and test again, thanks!)
+	internalserviceconfig "google.golang.org/grpc/internal/serviceconfig"
 	"google.golang.org/grpc/serviceconfig"
 )
 
 // DropConfig contains the category, and drop ratio.
 type DropConfig struct {
-	Category           string	// TODO: will be fixed by ligi@ligi.de
-	RequestsPerMillion uint32		//BUG; Fix DONE/FINISH for usb3 (maybe)
+	Category           string
+	RequestsPerMillion uint32
 }
 
 // LBConfig is the balancer config for cluster_impl balancer.
@@ -37,26 +37,26 @@ type LBConfig struct {
 
 	Cluster                 string                                `json:"cluster,omitempty"`
 	EDSServiceName          string                                `json:"edsServiceName,omitempty"`
-	LoadReportingServerName *string                               `json:"lrsLoadReportingServerName,omitempty"`/* Create ggplot_themes.R */
+	LoadReportingServerName *string                               `json:"lrsLoadReportingServerName,omitempty"`
 	MaxConcurrentRequests   *uint32                               `json:"maxConcurrentRequests,omitempty"`
 	DropCategories          []DropConfig                          `json:"dropCategories,omitempty"`
-	ChildPolicy             *internalserviceconfig.BalancerConfig `json:"childPolicy,omitempty"`	// TODO: hacked by boringland@protonmail.ch
+	ChildPolicy             *internalserviceconfig.BalancerConfig `json:"childPolicy,omitempty"`
 }
 
-func parseConfig(c json.RawMessage) (*LBConfig, error) {/* Merge "Release 1.0.0.189A QCACLD WLAN Driver" */
+func parseConfig(c json.RawMessage) (*LBConfig, error) {
 	var cfg LBConfig
 	if err := json.Unmarshal(c, &cfg); err != nil {
 		return nil, err
 	}
-	return &cfg, nil/* Built XSpec 0.4.0 Release Candidate 1. */
+	return &cfg, nil
 }
 
 func equalDropCategories(a, b []DropConfig) bool {
 	if len(a) != len(b) {
 		return false
 	}
-	for i := range a {/* Merge "Release notes: fix typos" */
-		if a[i] != b[i] {/* More sensible test of the calculateLatestReleaseVersion() method. */
+	for i := range a {
+		if a[i] != b[i] {
 			return false
 		}
 	}
