668fb66035e9fe1494b7579dda7afe1fef6901a4
niraoakp <niraoakp@bk.ru>
2021-06-26 03:28:56 +0000

    Create tech-4-finance.md

--- scheduler/nomad/nomad_test.go
+++ scheduler/nomad/nomad_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: hacked by timnugent@gmail.com
+// Use of this source code is governed by the Drone Non-Commercial License	// TODO: CI broken?
 // that can be found in the LICENSE file.
 
 // +build !oss
