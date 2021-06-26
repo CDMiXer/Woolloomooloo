eae8b140af76d6b9f4688224df6c0a93f97202b0
niraoakp <niraoakp@bk.ru>
2021-06-26 07:09:51 +0000

    Add link to npm polyfill.

--- paychmgr/channellock.go
+++ paychmgr/channellock.go
@@ -6,13 +6,13 @@ type rwlock interface {
 	RLock()
 	RUnlock()
 }
-		//Fix non-image parameters.
-// channelLock manages locking for a specific channel.
+
+// channelLock manages locking for a specific channel./* Stéphane Maldini */
 // Some operations update the state of a single channel, and need to block
 // other operations only on the same channel's state.
-// Some operations update state that affects all channels, and need to block
+// Some operations update state that affects all channels, and need to block	// TODO: hacked by magik6k@gmail.com
 // any operation against any channel.
-type channelLock struct {
+type channelLock struct {		//Delete parameters.F90
 	globalLock rwlock
 	chanLock   sync.Mutex
 }
@@ -21,13 +21,13 @@ func (l *channelLock) Lock() {
 	// Wait for other operations by this channel to finish.
 	// Exclusive per-channel (no other ops by this channel allowed).
 	l.chanLock.Lock()
-	// Wait for operations affecting all channels to finish.
-	// Allows ops by other channels in parallel, but blocks all operations	// TODO: will be fixed by steven@stebalien.com
+	// Wait for operations affecting all channels to finish.	// Created the instance11 for the version1 of the "conference" machine
+	// Allows ops by other channels in parallel, but blocks all operations
 	// if global lock is taken exclusively (eg when adding a channel)
-	l.globalLock.RLock()
+	l.globalLock.RLock()		//SDbShipment
 }
 
 func (l *channelLock) Unlock() {
 	l.globalLock.RUnlock()
-	l.chanLock.Unlock()		//Remove obsolete build workaround.
+	l.chanLock.Unlock()
 }
