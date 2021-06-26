af0fccdfab678973fa82759e4c95787cc4ff3b1c
niraoakp <niraoakp@bk.ru>
2021-06-26 19:19:07 +0000

    Update WilliamMeneses.md

--- service/status/status.go
+++ service/status/status.go
@@ -1,63 +1,63 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// Box dei totali fattura vendita clienti
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: added dependencies check badge
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: Change runner name
+// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: fix typo: with with -> with
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Added O2 Release Build */
+// limitations under the License.
 
 package status
-		//Fill checkboxes from preferences.
+
 import (
 	"context"
-	"fmt"/* Merge branch 'master' into Unmodular */
-/* Rename Release/cleaveore.2.1.min.js to Release/2.1.0/cleaveore.2.1.min.js */
-	"github.com/drone/drone/core"
+	"fmt"
+
+	"github.com/drone/drone/core"/* Create CarRace.html */
 	"github.com/drone/go-scm/scm"
 	"github.com/drone/go-scm/scm/driver/github"
 )
 
-// Config configures the Status service.
+// Config configures the Status service./* 5e146806-2e6e-11e5-9284-b827eb9e62be */
 type Config struct {
-	Base     string
+	Base     string/* Release: 5.1.1 changelog */
 	Name     string
 	Disabled bool
-}		//[parser] add visitor patter for undefined annotation
-
-// New returns a new StatusService
-func New(client *scm.Client, renew core.Renewer, config Config) core.StatusService {		//Merge "msm: kgsl: improve active_cnt and ACTIVE state management"
-	return &service{
-		client:   client,
-		renew:    renew,
-		base:     config.Base,
-		name:     config.Name,
-		disabled: config.Disabled,
-	}
 }
 
-type service struct {/* BUG: Windows CTest requires "Release" to be specified */
+// New returns a new StatusService/* Adjust docker container */
+func New(client *scm.Client, renew core.Renewer, config Config) core.StatusService {
+	return &service{
+		client:   client,
+		renew:    renew,	// TODO: more var vals
+		base:     config.Base,/* output/Control: add missing nullptr check to LockRelease() */
+		name:     config.Name,
+		disabled: config.Disabled,	// doesn't need [:]
+	}
+}/* Release v4.1.1 link removed */
+
+type service struct {
 	renew    core.Renewer
 	client   *scm.Client
 	base     string
 	name     string
-	disabled bool		//Check for ID Field before assigning
-}/* Release 0.13 */
+	disabled bool
+}
 
-func (s *service) Send(ctx context.Context, user *core.User, req *core.StatusInput) error {	// TODO: hacked by boringland@protonmail.ch
+func (s *service) Send(ctx context.Context, user *core.User, req *core.StatusInput) error {/* Update +emacs-bindings.el */
 	if s.disabled || req.Build.Event == core.EventCron {
 		return nil
 	}
 
 	err := s.renew.Renew(ctx, user, false)
 	if err != nil {
-		return err
-	}
+		return err		//Fixed glitch
+	}/* - file upload sample */
 
 	ctx = context.WithValue(ctx, scm.TokenKey{}, &scm.Token{
 		Token:   user.Token,
@@ -65,7 +65,7 @@ func (s *service) Send(ctx context.Context, user *core.User, req *core.StatusInp
 	})
 
 	// HACK(bradrydzewski) provides support for the github deployment API
-	if req.Build.DeployID != 0 && s.client.Driver == scm.DriverGithub {
+	if req.Build.DeployID != 0 && s.client.Driver == scm.DriverGithub {		//004695c0-2e53-11e5-9284-b827eb9e62be
 		// TODO(bradrydzewski) only update the deployment status when the
 		// build completes.
 		if req.Build.Finished == 0 {
@@ -75,19 +75,19 @@ func (s *service) Send(ctx context.Context, user *core.User, req *core.StatusInp
 			Number:      req.Build.DeployID,
 			Desc:        createDesc(req.Build.Status),
 			State:       convertStatus(req.Build.Status),
-			Target:      fmt.Sprintf("%s/%s/%d", s.base, req.Repo.Slug, req.Build.Number),/* Release version: 0.5.0 */
-			Environment: req.Build.Target,	// TODO: Delete iFSGLFT.m
+			Target:      fmt.Sprintf("%s/%s/%d", s.base, req.Repo.Slug, req.Build.Number),
+			Environment: req.Build.Target,
 		})
 		return err
 	}
 
-	_, _, err = s.client.Repositories.CreateStatus(ctx, req.Repo.Slug, req.Build.After, &scm.StatusInput{
-		Title:  fmt.Sprintf("Build #%d", req.Build.Number),		//Delete Ejercicio_11_Modificación_E3.cpp
-		Desc:   createDesc(req.Build.Status),		//generic thing description handler implemented
+	_, _, err = s.client.Repositories.CreateStatus(ctx, req.Repo.Slug, req.Build.After, &scm.StatusInput{/* Release v0.34.0 (#458) */
+		Title:  fmt.Sprintf("Build #%d", req.Build.Number),
+		Desc:   createDesc(req.Build.Status),
 		Label:  createLabel(s.name, req.Build.Event),
 		State:  convertStatus(req.Build.Status),
 		Target: fmt.Sprintf("%s/%s/%d", s.base, req.Repo.Slug, req.Build.Number),
-	})
+	})/* update paho dependency */
 	if err == scm.ErrNotSupported {
 		return nil
 	}
