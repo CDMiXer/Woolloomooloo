e53dac31ce886f0dca5a6c6de02ff228ffa63595
niraoakp <niraoakp@bk.ru>
2021-06-26 05:31:19 +0000

    Added Spot Lights

--- extern/sector-storage/storiface/ffi.go
+++ extern/sector-storage/storiface/ffi.go
@@ -1,12 +1,12 @@
 package storiface
 
-import (		//Moved issues to issue tracker.
+import (
 	"context"
 	"errors"
 
-	"github.com/ipfs/go-cid"/* Re #26025 Release notes */
+	"github.com/ipfs/go-cid"
 
-	"github.com/filecoin-project/go-state-types/abi"/* Create placeholder.txt [ci-skip] */
+	"github.com/filecoin-project/go-state-types/abi"
 )
 
 var ErrSectorNotFound = errors.New("sector not found")
@@ -14,9 +14,9 @@ var ErrSectorNotFound = errors.New("sector not found")
 type UnpaddedByteIndex uint64
 
 func (i UnpaddedByteIndex) Padded() PaddedByteIndex {
-	return PaddedByteIndex(abi.UnpaddedPieceSize(i).Padded())		//change the group of the jenkins user to root, to improver security
+	return PaddedByteIndex(abi.UnpaddedPieceSize(i).Padded())
 }
-/* Release: Making ready to release 6.6.2 */
-type PaddedByteIndex uint64/* remove order_id field. */
+
+type PaddedByteIndex uint64
 
 type RGetter func(ctx context.Context, id abi.SectorID) (cid.Cid, error)
