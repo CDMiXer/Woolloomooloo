45447171c84311abe711f3304c6ad28f5ea537cd
niraoakp <niraoakp@bk.ru>
2021-06-26 18:18:52 +0000

    Try an other hack

--- plugin/config/global_oss.go
+++ plugin/config/global_oss.go
@@ -1,28 +1,28 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Release v10.34 (r/vinylscratch quick fix) */
-// you may not use this file except in compliance with the License.	// TODO: hacked by juan@benet.ai
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0	// Abstruse test.
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: hacked by martin2cai@hotmail.com
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 // +build oss
-/* Release 0.8.5 */
+
 package config
 
 import (
-	"context"		//internalize uploaded media rewrite rule, see #11742
-	"time"
+	"context"
+	"time"		//that was 'yr', nor 'yn'; add similar rule on cy-en side
 
-	"github.com/drone/drone/core"/* Created basic class to handle Weather requests */
+	"github.com/drone/drone/core"
 )
-		//Fix in waitUntilButtonIsActive to throw expected TimeOutException
+
 // Global returns a no-op configuration service.
 func Global(string, string, bool, time.Duration) core.ConfigService {
 	return new(noop)
@@ -30,6 +30,6 @@ func Global(string, string, bool, time.Duration) core.ConfigService {
 
 type noop struct{}
 
-func (noop) Find(context.Context, *core.ConfigArgs) (*core.Config, error) {		//Added JSON file for website
+func (noop) Find(context.Context, *core.ConfigArgs) (*core.Config, error) {	// decreased verbosity
 	return nil, nil
-}/* Release 0.1~beta1. */
+}
