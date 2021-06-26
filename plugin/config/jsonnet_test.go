dd717e05882a7b21b0fce76774e9de4ff5c0e556
niraoakp <niraoakp@bk.ru>
2021-06-26 03:52:13 +0000

    Correction for how multiple AZs are specified for /link_address

--- plugin/config/jsonnet_test.go
+++ plugin/config/jsonnet_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-	// TODO: will be fixed by witek@enjin.io
-// +build !oss		//update class info
-	// TODO: Fit version 1.x
+
+// +build !oss/* completed finnish localisation */
+
 package config
