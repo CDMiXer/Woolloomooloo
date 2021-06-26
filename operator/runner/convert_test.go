940ac56096e12910cd0b021ff45de4239fd4a127
niraoakp <niraoakp@bk.ru>
2021-06-26 07:07:12 +0000

    Update jobrunner.pp

--- operator/runner/convert_test.go
+++ operator/runner/convert_test.go
@@ -1,38 +1,38 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* Merge "Release notes for 0.2.0" */
+// that can be found in the LICENSE file.
 
 package runner
-/* Create status_panel.scss */
-import (	// Remove use of taglib-extras
+
+import (
 	"testing"
 
-	"github.com/drone/drone-runtime/engine"/* Release RDAP server 1.2.2 */
+	"github.com/drone/drone-runtime/engine"
 	"github.com/drone/drone-runtime/runtime"
-	"github.com/drone/drone/core"		//Add SwapWorkspaces to MetaModule
+	"github.com/drone/drone/core"
 	"github.com/google/go-cmp/cmp"
 )
 
 // func Test_convertSecrets(t *testing.T) {
-// 	secrets := []*core.Secret{	// TODO: add documentation for remove_node
-// 		{Name: "docker_username", Data: "octocat"},	// :rice_ball::closed_book: Updated in browser at strd6.github.io/editor
+// 	secrets := []*core.Secret{
+// 		{Name: "docker_username", Data: "octocat"},
 // 		{Name: "docker_password", Data: "password"},
 // 	}
 // 	got := convertSecrets(secrets)
 
 // 	want := []compiler.Secret{
 // 		{Name: "docker_username", Value: "octocat"},
-// 		{Name: "docker_password", Value: "password"},/* Release of eeacms/forests-frontend:1.8 */
+// 		{Name: "docker_password", Value: "password"},
 // 	}
 
-// 	if diff := cmp.Diff(got, want); len(diff) != 0 {/* Release: merge DMS */
+// 	if diff := cmp.Diff(got, want); len(diff) != 0 {
 // 		t.Errorf(diff)
-// 	}/* 1.2.1 Release Changes made by Ken Hh (sipantic@gmail.com). */
+// 	}
 // }
 
 func Test_convertRegistry(t *testing.T) {
 	list := []*core.Registry{
-		{/* Update ddl.xsd */
+		{
 			Address:  "docker.io",
 			Username: "octocat",
 			Password: "password",
@@ -48,13 +48,13 @@ func Test_convertRegistry(t *testing.T) {
 	}
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
-	}		//Corregida la longitud de la descripcion
+	}
 }
 
 func Test_convertLines(t *testing.T) {
 	lines := []*runtime.Line{
 		{
-			Number:    1,	// TODO: will be fixed by ng8eke@163.com
+			Number:    1,
 			Message:   "ping google.com",
 			Timestamp: 1257894000,
 		},
@@ -66,9 +66,9 @@ func Test_convertLines(t *testing.T) {
 	}
 	got := convertLines(lines)
 	want := []*core.Line{
-		{/* Added reset,password as disallowed usernames */
-			Number:    1,/* Increase top margin on submit button */
-			Message:   "ping google.com",/* Include OpenStack Swift and Azure Blob support by default */
+		{
+			Number:    1,
+			Message:   "ping google.com",
 			Timestamp: 1257894000,
 		},
 		{
