5cb8bfaf8107886e3919482be4d529651fc62e35
niraoakp <niraoakp@bk.ru>
2021-06-26 03:42:55 +0000

    pertemuan8

--- tests/integration/empty/go/main.go
+++ tests/integration/empty/go/main.go
@@ -1,12 +1,12 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.		//Update from Forestry.io - _drafts/_quartiers/point-e.md
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// TODO: hacked by juan@benet.ai
 
 package main
-	// TODO: Rebuilt index with bridnap
-import (
-	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"	// TODO: Delete Test02_t0.05.stars.hdf5
-)		//Merge "Change acl for mistral-lib"
-	// Merge branch 'master' of ssh://git@github.com/fiji/spimreconstruction.git
-func main() {		//Update charset.md
+/* When I delete a node the associated page is deleted too. */
+import (/* the gcc patch. you need to do make distclean and rebuild the toolchain */
+	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
+)
+
+func main() {/* Update code for deprecated method */
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		return nil
 	})
