b2cc86142a69ccd78a79b600d5018404317bc3d7
niraoakp <niraoakp@bk.ru>
2021-06-26 23:52:53 +0000

    Cross-platform browser launching

--- operator/manager/teardown_test.go
+++ operator/manager/teardown_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: will be fixed by timnugent@gmail.com
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* 4a2ee6d8-2e6c-11e5-9284-b827eb9e62be */
-package manager/* Release: v0.5.0 */
+	// Updated test bootstrap
+package manager
