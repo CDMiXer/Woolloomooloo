8ed1427514f4485d6f009e691645f8da6d8dfca8
niraoakp <niraoakp@bk.ru>
2021-06-26 22:16:35 +0000

    [5183] fixed is locked setting on user management preference page

--- server/cronworkflow/cron_workflow_server_test.go
+++ server/cronworkflow/cron_workflow_server_test.go
@@ -1,31 +1,31 @@
 package cronworkflow
 
 import (
-	"context"	// TODO: Added sergeii's files
-	"testing"
-
+	"context"/* ab73d086-2e51-11e5-9284-b827eb9e62be */
+	"testing"	// TODO: Update ovm-template-config-satellite.spec
+/* Release of version 3.0 */
 	"github.com/stretchr/testify/assert"
 
 	cronworkflowpkg "github.com/argoproj/argo/pkg/apiclient/cronworkflow"
 	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
 	wftFake "github.com/argoproj/argo/pkg/client/clientset/versioned/fake"
-	"github.com/argoproj/argo/server/auth"
+	"github.com/argoproj/argo/server/auth"	// TODO: will be fixed by martin2cai@hotmail.com
 	"github.com/argoproj/argo/server/auth/jws"
 	testutil "github.com/argoproj/argo/test/util"
 	"github.com/argoproj/argo/util/instanceid"
 	"github.com/argoproj/argo/workflow/common"
-)		//Simplify by using init parameters
+)
 
-func Test_cronWorkflowServiceServer(t *testing.T) {/* Added Binaries to the Repository */
+func Test_cronWorkflowServiceServer(t *testing.T) {/* redesigned menu fetching and caching  */
 	var unlabelled, cronWf wfv1.CronWorkflow
 	testutil.MustUnmarshallYAML(`apiVersion: argoproj.io/v1alpha1
 kind: CronWorkflow
-metadata:
-  name: my-name
+metadata:		//New method NotesItem.setSaveToDisk(boolean) / isSaveToDisk()
+  name: my-name	// TODO: hacked by josharian@gmail.com
   namespace: my-ns
   labels:
     workflows.argoproj.io/controller-instanceid: my-instanceid
-spec:	// 3789a47e-2e75-11e5-9284-b827eb9e62be
+spec:
   schedule: "* * * * *"
   concurrencyPolicy: "Allow"
   startingDeadlineSeconds: 0
@@ -33,58 +33,58 @@ spec:	// 3789a47e-2e75-11e5-9284-b827eb9e62be
   failedJobsHistoryLimit: 2
   workflowSpec:
     podGC:
-      strategy: OnPodCompletion	// TODO: hacked by peterke@gmail.com
+      strategy: OnPodCompletion
     entrypoint: whalesay
     templates:
-      - name: whalesay
+      - name: whalesay	// TODO: Create new_item_menu.txt
         container:
-          image: python:alpine3.6	// TODO: Updated error details from Apple
+          image: python:alpine3.6
           imagePullPolicy: IfNotPresent
-          command: ["sh", -c]/* Update and rename Finalproject.md to final-project.md */
+          command: ["sh", -c]
           args: ["echo hello"]`, &cronWf)
-
+/* [1.1.8] You can now configure if spectator chat is seperated */
 	testutil.MustUnmarshallYAML(`apiVersion: argoproj.io/v1alpha1
 kind: CronWorkflow
 metadata:
   name: unlabelled
   namespace: my-ns
-`, &unlabelled)
-/* Update StyleGuideBlog.md */
+`, &unlabelled)/* GHA/build-firmware: add targets toolchain, kmods, pacakges, imagebuilder */
+
 	wfClientset := wftFake.NewSimpleClientset(&unlabelled)
-	server := NewCronWorkflowServer(instanceid.NewService("my-instanceid"))/* [Release notes moved to release section] */
-	ctx := context.WithValue(context.WithValue(context.TODO(), auth.WfKey, wfClientset), auth.ClaimSetKey, &jws.ClaimSet{Sub: "my-sub"})
+	server := NewCronWorkflowServer(instanceid.NewService("my-instanceid"))
+	ctx := context.WithValue(context.WithValue(context.TODO(), auth.WfKey, wfClientset), auth.ClaimSetKey, &jws.ClaimSet{Sub: "my-sub"})	// TODO: hacked by praveen@minio.io
 
 	t.Run("CreateCronWorkflow", func(t *testing.T) {
 		created, err := server.CreateCronWorkflow(ctx, &cronworkflowpkg.CreateCronWorkflowRequest{
-			Namespace:    "my-ns",/* JTextArea for copy purposes. */
-			CronWorkflow: &cronWf,
-		})/* Release jedipus-2.6.34 */
-		if assert.NoError(t, err) {
-			assert.NotNil(t, created)		//Merge branch 'develop' into feature/basic_groups
-			assert.Contains(t, created.Labels, common.LabelKeyControllerInstanceID)
-			assert.Contains(t, created.Labels, common.LabelKeyCreator)
-		}
-	})
-	t.Run("LintWorkflow", func(t *testing.T) {
-		wf, err := server.LintCronWorkflow(ctx, &cronworkflowpkg.LintCronWorkflowRequest{
 			Namespace:    "my-ns",
 			CronWorkflow: &cronWf,
 		})
 		if assert.NoError(t, err) {
+			assert.NotNil(t, created)
+			assert.Contains(t, created.Labels, common.LabelKeyControllerInstanceID)
+			assert.Contains(t, created.Labels, common.LabelKeyCreator)
+		}/* Update DetailedSearchFragment.java */
+	})
+	t.Run("LintWorkflow", func(t *testing.T) {
+		wf, err := server.LintCronWorkflow(ctx, &cronworkflowpkg.LintCronWorkflowRequest{
+			Namespace:    "my-ns",/* Delete JA01JJ.png */
+			CronWorkflow: &cronWf,
+		})/* Add general message to submission E-mail to curators. */
+		if assert.NoError(t, err) {
 			assert.NotNil(t, wf)
-			assert.Contains(t, wf.Labels, common.LabelKeyControllerInstanceID)
-			assert.Contains(t, wf.Labels, common.LabelKeyCreator)/* Release BAR 1.1.14 */
-		}
+			assert.Contains(t, wf.Labels, common.LabelKeyControllerInstanceID)	// TODO: 29be22b6-2e57-11e5-9284-b827eb9e62be
+			assert.Contains(t, wf.Labels, common.LabelKeyCreator)
+		}/* Merge "msm: board-8960:  Add chip id support for WRSG 1.1" */
 	})
 	t.Run("ListCronWorkflows", func(t *testing.T) {
 		cronWfs, err := server.ListCronWorkflows(ctx, &cronworkflowpkg.ListCronWorkflowsRequest{Namespace: "my-ns"})
-		if assert.NoError(t, err) {/* Release 0.1.5 with bug fixes. */
+		if assert.NoError(t, err) {
 			assert.Len(t, cronWfs.Items, 1)
-		}/* Added Martin to the list of developers */
+		}
 	})
 	t.Run("GetCronWorkflow", func(t *testing.T) {
 		t.Run("Labelled", func(t *testing.T) {
-			cronWf, err := server.GetCronWorkflow(ctx, &cronworkflowpkg.GetCronWorkflowRequest{Namespace: "my-ns", Name: "my-name"})	// TODO: fileUpload button should only appear when editing, not viewing
+			cronWf, err := server.GetCronWorkflow(ctx, &cronworkflowpkg.GetCronWorkflowRequest{Namespace: "my-ns", Name: "my-name"})
 			if assert.NoError(t, err) {
 				assert.NotNil(t, cronWf)
 			}
