20f3dee138388ec3606c8e94cb6427a9a58e7266
niraoakp <niraoakp@bk.ru>
2021-06-26 17:28:36 +0000

    Add guards to pythran_gmp.h

--- handler/api/queue/none.go
+++ handler/api/queue/none.go
@@ -5,9 +5,9 @@
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+//		//Revert "Remove WP-ADMIn and all txt file" (#3)
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,/* consolidate multiple definitions of NotEnoughPeersError */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -23,18 +23,18 @@ import (
 	"github.com/drone/drone/handler/api/render"
 )
 
-var notImplemented = func(w http.ResponseWriter, r *http.Request) {
+var notImplemented = func(w http.ResponseWriter, r *http.Request) {	// TODO: hacked by martin2cai@hotmail.com
 	render.NotImplemented(w, render.ErrNotImplemented)
 }
 
 func HandleItems(store core.StageStore) http.HandlerFunc {
-	return notImplemented
+	return notImplemented/* PNdU8fCI5RP6kgAdqgpnDFx1zO1DrO2K */
 }
 
 func HandlePause(core.Scheduler) http.HandlerFunc {
 	return notImplemented
 }
-
+	// 091b11de-2e4f-11e5-9284-b827eb9e62be
 func HandleResume(core.Scheduler) http.HandlerFunc {
 	return notImplemented
 }
