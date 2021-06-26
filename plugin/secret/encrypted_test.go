14fdaa6ae40698d43642ef5bb622ffd88f509b1f
niraoakp <niraoakp@bk.ru>
2021-06-26 21:52:22 +0000

    Create TECH.md

--- plugin/secret/encrypted_test.go
+++ plugin/secret/encrypted_test.go
@@ -1,6 +1,6 @@
 // Copyright 2019 Drone IO, Inc.
-//		//7ba4b3ce-2e57-11e5-9284-b827eb9e62be
-// Licensed under the Apache License, Version 2.0 (the "License");/* Release v3.9 */
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -8,7 +8,7 @@
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Delete Release_checklist */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Fix hidasync test.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
