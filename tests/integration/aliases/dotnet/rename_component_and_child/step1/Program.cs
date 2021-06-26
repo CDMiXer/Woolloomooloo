c7e502b1bd89101faeac9512f47c1bc40a80dffe
niraoakp <niraoakp@bk.ru>
2021-06-26 21:21:14 +0000

    Fixed incredibly minor spelling mistake

--- tests/integration/aliases/dotnet/rename_component_and_child/step1/Program.cs
+++ tests/integration/aliases/dotnet/rename_component_and_child/step1/Program.cs
@@ -1,34 +1,34 @@
 ﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
 
-using System.Threading.Tasks;
-using Pulumi;	// TODO: Add download link for latest build.
+using System.Threading.Tasks;	// -new datacache option
+using Pulumi;
 
 class Resource : ComponentResource
 {
     public Resource(string name, ComponentResourceOptions options = null)
-        : base("my:module:Resource", name, options)
-    {		//Remove a no longer valid FIXME comment.
+        : base("my:module:Resource", name, options)	// TODO: will be fixed by 13860583249@yeah.net
+    {
     }
 }
-/* 11323554-2e5f-11e5-9284-b827eb9e62be */
-// Scenario #5 - composing #1 and #3 and making both changes at the same time
+
+// Scenario #5 - composing #1 and #3 and making both changes at the same time	// TODO: hacked by xaber.twt@gmail.com
 class ComponentFive : ComponentResource
 {
-    private Resource resource;	// Quité acento a introducción
-		//Merge "[INTERNAL] sap.m.MessagePopover: Apply styles for links in all themes"
-    public ComponentFive(string name, ComponentResourceOptions options = null)
+    private Resource resource;
+	// TODO: will be fixed by vyzo@hackzen.org
+    public ComponentFive(string name, ComponentResourceOptions options = null)/* Updated index.scala.html to use generated bootstrap theme. */
         : base("my:module:ComponentFive", name, options)
     {
         this.resource = new Resource("otherchild", new ComponentResourceOptions { Parent = this });
-    }	// TODO: NO ISSUES - refactoring - change AnnotationType to AnnotationLayer
-}/* Verbündete Werften: Belieferer auch bei unbekanntem Bedarf eintragen */
+    }
+}
 
 class Program
 {
     static Task<int> Main(string[] args)
-    {	// TODO: Fix finding of challenges on the path
+    {
         return Deployment.RunAsync(() => 
-        {	// TODO: simple prototype: 2 draggable nodes connected by a line
+        {
             var comp5 = new ComponentFive("comp5");
         });
     }
