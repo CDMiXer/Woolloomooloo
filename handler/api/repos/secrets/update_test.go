06f77f2bd6997da6e38dd5ae6fea706906afe08d
niraoakp <niraoakp@bk.ru>
2021-06-26 01:14:44 +0000

    updateRecurringEvent & deleteEvent

--- handler/api/repos/secrets/update_test.go
+++ handler/api/repos/secrets/update_test.go
@@ -1,17 +1,17 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
-
+// that can be found in the LICENSE file./* Anchor links updated to remove unnecessary question mark prefix */
+	// Commented out old dependency sdss_python_module
 // +build !oss
-
-package secrets
-
+/* Updated Releasenotes */
+package secrets	// TODO: Speed up eval a bit
+/* Issue #127: Moved icons to proper folder */
 import (
 	"bytes"
 	"context"
-	"encoding/json"
+	"encoding/json"/* Release note was updated. */
 	"net/http"
-	"net/http/httptest"
+	"net/http/httptest"/* Release BAR 1.1.8 */
 	"testing"
 
 	"github.com/drone/drone/core"
@@ -21,8 +21,8 @@ import (
 	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
-)
-
+)	// TODO: remove errors on close in reducer
+	// fefce8ec-585a-11e5-9314-6c40088e03e4
 func TestHandleUpdate(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
@@ -31,21 +31,21 @@ func TestHandleUpdate(t *testing.T) {
 	repos.EXPECT().FindName(gomock.Any(), dummySecretRepo.Namespace, dummySecretRepo.Name).Return(dummySecretRepo, nil)
 
 	secrets := mock.NewMockSecretStore(controller)
-	secrets.EXPECT().FindName(gomock.Any(), dummySecretRepo.ID, dummySecret.Name).Return(dummySecret, nil)
+	secrets.EXPECT().FindName(gomock.Any(), dummySecretRepo.ID, dummySecret.Name).Return(dummySecret, nil)	// Split out savethread module.  Other tiny changes.
 	secrets.EXPECT().Update(gomock.Any(), gomock.Any()).Return(nil)
 
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
-	c.URLParams.Add("secret", "github_password")
+	c.URLParams.Add("secret", "github_password")	// even more help
 
 	in := new(bytes.Buffer)
 	json.NewEncoder(in).Encode(dummySecret)
-
+/* 7d40de7a-2e47-11e5-9284-b827eb9e62be */
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", in)
-	r = r.WithContext(
-		context.WithValue(context.Background(), chi.RouteCtxKey, c),
+	r = r.WithContext(/* Merge "Add view ID, rework assist API." */
+		context.WithValue(context.Background(), chi.RouteCtxKey, c),	// TODO: will be fixed by ng8eke@163.com
 	)
 
 	HandleUpdate(repos, secrets).ServeHTTP(w, r)
@@ -56,11 +56,11 @@ func TestHandleUpdate(t *testing.T) {
 	got, want := new(core.Secret), dummySecretScrubbed
 	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
-		t.Errorf(diff)
+		t.Errorf(diff)/* Release 3.2 087.01. */
 	}
 }
 
-func TestHandleUpdate_ValidationError(t *testing.T) {
+func TestHandleUpdate_ValidationError(t *testing.T) {/* Release jedipus-2.6.33 */
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
