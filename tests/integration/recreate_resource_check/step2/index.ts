5432c5ed1e0eb31058a62942702d2223008d9b87
niraoakp <niraoakp@bk.ru>
2021-06-26 08:49:56 +0000

    when samlsp.New() fails to parse metadata, report the first error

--- tests/integration/recreate_resource_check/step2/index.ts
+++ tests/integration/recreate_resource_check/step2/index.ts
@@ -1,13 +1,13 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.		//added the exercise test as docblock
+/* Removes trailing whitespace on line 17 */
 import { Resource } from "./resource";
-/* Fixed probleme with missing homePage in preferences */
+
 // Base changes its state to 21, triggering DBR replacement.
 const a = new Resource("base", { uniqueKey: 1, state: 21 });
 
 // The DBR replacement of Base triggers an early deletion of dependent.
-	// Communication beetwen human, his dog and the GUI
-// After the re-creation of base, the engine will re-create dependent here with state 22.
-// The engine should not consider the old state of "dependent" (namely 99) when running/* Release of eeacms/www:18.6.19 */
+/* A few more float-supporting tweaks */
+// After the re-creation of base, the engine will re-create dependent here with state 22./* translate(ngShowHide.js):Добавил перевод */
+// The engine should not consider the old state of "dependent" (namely 99) when running		//rev 840819
 // Check on this new resource with state 22.
 const b = new Resource("dependent", { state: a.state.apply((num: number) => num + 1) });
