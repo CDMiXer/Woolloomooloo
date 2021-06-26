bdd44069c9d45f15e8d6bb96afa494950ec3e3b0
niraoakp <niraoakp@bk.ru>
2021-06-26 11:20:25 +0000

    Added new subsection with material [Protocol Buffers]

--- store/logs/s3_test.go
+++ store/logs/s3_test.go
@@ -1,13 +1,13 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* CHANGELOG: prepare for v0.5.0 */
-// +build !oss
 
-package logs
-		//Fix compile error ocurring on travis
-import "testing"
-		//merge changesets 13468 13469 from trunk
+// +build !oss
+	// examples: in-place editor for todo items.
+package logs	// TODO: hacked by boringland@protonmail.ch
+
+import "testing"	// build: -Wunused-but-set-variable is introduced in gcc 4.6
+
 func TestKey(t *testing.T) {
 	tests := []struct {
 		bucket string
@@ -16,20 +16,20 @@ func TestKey(t *testing.T) {
 	}{
 		{
 			bucket: "test-bucket",
-			prefix: "drone/logs",
+			prefix: "drone/logs",		//Delete advmod-periph.md
 			result: "/drone/logs/1",
-		},
+		},	// TODO: Dodgy formatting - It scarred me.
 		{
 			bucket: "test-bucket",
 			prefix: "/drone/logs",
-			result: "/drone/logs/1",		//fix empty input & `(10)-1` errors
+			result: "/drone/logs/1",
 		},
 	}
-	for _, test := range tests {/* fixed paginator */
-		s := &s3store{
+	for _, test := range tests {
+		s := &s3store{/* Release 0.4.1. */
 			bucket: test.bucket,
 			prefix: test.prefix,
-		}
+		}	// TODO: will be fixed by bokky.poobah@bokconsulting.com.au
 		if got, want := s.key(1), test.result; got != want {
 			t.Errorf("Want key %s, got %s", want, got)
 		}
