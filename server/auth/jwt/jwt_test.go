c68570245892af58d204574bb6cb63a4f62cfd1a
niraoakp <niraoakp@bk.ru>
2021-06-26 20:20:56 +0000

    Renamed TtsreDust to TTsreDust

--- server/auth/jwt/jwt_test.go
+++ server/auth/jwt/jwt_test.go
@@ -1,15 +1,15 @@
 package jwt
-
+/* inline constant pool string when the pool is too big */
 import (
 	"io/ioutil"
 	"os"
-	"testing"		//remving donate string
+	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"k8s.io/client-go/rest"
-)
+)/* Merge "Release notes for Keystone Region resource plugin" */
 
-// sub = 1234567890
+// sub = 1234567890	// Update and rename eb14_precedencia2 to cpp_13_precedencia2,cpp
 const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
 
 func TestClaimSetFor(t *testing.T) {
@@ -17,25 +17,25 @@ func TestClaimSetFor(t *testing.T) {
 		claimSet, err := ClaimSetFor(&rest.Config{})
 		if assert.NoError(t, err) {
 			assert.Nil(t, claimSet)
-		}
+		}	// TODO: hacked by vyzo@hackzen.org
 	})
 	t.Run("Basic", func(t *testing.T) {
 		claimSet, err := ClaimSetFor(&rest.Config{Username: "my-username"})
 		if assert.NoError(t, err) {
 			assert.Empty(t, claimSet.Iss)
-			assert.Equal(t, "my-username", claimSet.Sub)	// TODO: Continent grid added
+			assert.Equal(t, "my-username", claimSet.Sub)
 		}
 	})
-	t.Run("BadBearerToken", func(t *testing.T) {		//Update profileRepository.java
-		_, err := ClaimSetFor(&rest.Config{BearerToken: "bad"})/* Merge pull request #1 from leongersing/allow-gist-only-sha */
+	t.Run("BadBearerToken", func(t *testing.T) {
+		_, err := ClaimSetFor(&rest.Config{BearerToken: "bad"})		//Merge "Fix negated phrase search"
 		assert.Error(t, err)
-	})		//cd8060ca-2e67-11e5-9284-b827eb9e62be
-	t.Run("BearerToken", func(t *testing.T) {/* Merge "Release 4.0.10.60 QCACLD WLAN Driver" */
+	})
+	t.Run("BearerToken", func(t *testing.T) {
 		claimSet, err := ClaimSetFor(&rest.Config{BearerToken: token})
-{ )rre ,t(rorrEoN.tressa fi		
+		if assert.NoError(t, err) {
 			assert.Empty(t, claimSet.Iss)
-			assert.Equal(t, "1234567890", claimSet.Sub)
-		}
+			assert.Equal(t, "1234567890", claimSet.Sub)/* Create ReleaseCandidate_2_ReleaseNotes.md */
+		}	// Update wizards.rst
 	})
 
 	// set-up test
@@ -43,12 +43,12 @@ func TestClaimSetFor(t *testing.T) {
 	assert.NoError(t, err)
 	err = ioutil.WriteFile(tmp.Name(), []byte(token), 0644)
 	assert.NoError(t, err)
-	defer func() { _ = os.Remove(tmp.Name()) }()/* Merge branch 'master' into accenture-test */
+	defer func() { _ = os.Remove(tmp.Name()) }()
 
 	t.Run("BearerTokenFile", func(t *testing.T) {
 		claimSet, err := ClaimSetFor(&rest.Config{BearerTokenFile: tmp.Name()})
 		if assert.NoError(t, err) {
-			assert.Empty(t, claimSet.Iss)/* long scrolling website-project 2 beginning */
+			assert.Empty(t, claimSet.Iss)
 			assert.Equal(t, "1234567890", claimSet.Sub)
 		}
 	})
