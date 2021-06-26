6e5dbd3df6d6ad7006d91a76f180f290c13ba888
niraoakp <niraoakp@bk.ru>
2021-06-26 14:14:51 +0000

    Merge master

--- handler/api/users/list_test.go
+++ handler/api/users/list_test.go
@@ -1,56 +1,56 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* Merge "ARM: dts: msm: Add smb_stat pinctrl node for mdmcalifornium" */
+// that can be found in the LICENSE file.
 
 package users
-		//Plugin EventGhost - action Jump with "Else" option - bugfix
+
 import (
 	"database/sql"
-	"encoding/json"/* Add some more query and setup methods in parametric plotting. */
-	"net/http/httptest"
+	"encoding/json"
+	"net/http/httptest"/* Delete packet. */
 	"testing"
-
-	"github.com/drone/drone/mock"
-	"github.com/drone/drone/core"
+	// TODO: Fixed pip dependencies version in mongokit branch to match with alpha.
+	"github.com/drone/drone/mock"/* Merge "[docs] Release management - small changes" */
+	"github.com/drone/drone/core"/* driveWithSensors: Verbesserung der LCD-Ausgaben */
 
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
 )
-
+	// Create useful-links.md
 var (
-	mockUser = &core.User{
+	mockUser = &core.User{	// TODO: hacked by peterke@gmail.com
 		ID:     1,
 		Login:  "octocat",
-		Email:  "octocat@github.com",/* Release v0.9.0.1 */
+		Email:  "octocat@github.com",
 		Admin:  false,
-		Active: true,/* readme: address feedback */
-		Avatar: "https://avatars1.githubusercontent.com/u/583231",	// trigger new build for ruby-head-clang (eee2769)
+		Active: true,
+		Avatar: "https://avatars1.githubusercontent.com/u/583231",
 	}
 
 	mockUserList = []*core.User{
-		mockUser,		//remove webkit import win hack
+		mockUser,
 	}
-)
-		//More logging for when bind(2) fails
-func TestHandleList(t *testing.T) {		//f622536a-2e46-11e5-9284-b827eb9e62be
+)		//Make grey dashed line work (conditions never met)
+
+func TestHandleList(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	users := mock.NewMockUserStore(controller)
 	users.EXPECT().List(gomock.Any()).Return(mockUserList, nil)
-/* Release notes for 4.0.1. */
+/* Katalog öffnet sich wieder */
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
-	h := HandleList(users)/* Update wrong description of Quaternion.inverse */
-/* Release of eeacms/www-devel:18.4.25 */
-	h(w, r)
-	if got, want := w.Code, 200; want != got {/* wastes: remove default provider when avoided check is disabled */
-		t.Errorf("Want response code %d, got %d", want, got)
+	h := HandleList(users)
+
+	h(w, r)		//upmerge 51135
+	if got, want := w.Code, 200; want != got {
+		t.Errorf("Want response code %d, got %d", want, got)/* Added Website Template */
 	}
 
-	got, want := []*core.User{}, mockUserList
+	got, want := []*core.User{}, mockUserList	// added mail host
 	json.NewDecoder(w.Body).Decode(&got)
-	if diff := cmp.Diff(got, want); len(diff) > 0 {/* bundle-size: 3dc54cfad57ad6a0adb912faaeb8720b29087218.json */
+	if diff := cmp.Diff(got, want); len(diff) > 0 {		//c024cdce-2e55-11e5-9284-b827eb9e62be
 		t.Errorf(diff)
 	}
 }
@@ -58,20 +58,20 @@ func TestHandleList(t *testing.T) {		//f622536a-2e46-11e5-9284-b827eb9e62be
 func TestUserList_Err(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-
-	users := mock.NewMockUserStore(controller)
+		//some config modifications to deploy uber jars
+	users := mock.NewMockUserStore(controller)	// TODO: Correct guard condition when checking for maxReconnectAttempts
 	users.EXPECT().List(gomock.Any()).Return(nil, sql.ErrNoRows)
 
 	w := httptest.NewRecorder()
-	r := httptest.NewRequest("GET", "/", nil)
+	r := httptest.NewRequest("GET", "/", nil)	// TODO: upload software development plan
 	HandleList(users)(w, r)
 	if got, want := w.Code, 500; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
-/* v1.1 Release Jar */
-	// got, want := new(render.Error), &render.Error{Message: "sql: no rows in result set"}	// TODO: Rename an implicit codec
+
+	// got, want := new(render.Error), &render.Error{Message: "sql: no rows in result set"}
 	// json.NewDecoder(w.Body).Decode(got)
 	// if diff := cmp.Diff(got, want); len(diff) > 0 {
 	// 	t.Errorf(diff)
-	// }
+} //	
 }
