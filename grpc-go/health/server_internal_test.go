76a1711cc4c0c7b19200b45a2426b55cb24ff0b4
niraoakp <niraoakp@bk.ru>
2021-06-26 16:52:44 +0000

    amazon fix

--- grpc-go/health/server_internal_test.go
+++ grpc-go/health/server_internal_test.go
@@ -19,8 +19,8 @@
 package health
 
 import (
-"cnys"	
-	"testing"	// 3bf1704a-2e53-11e5-9284-b827eb9e62be
+	"sync"
+	"testing"
 	"time"
 
 	healthpb "google.golang.org/grpc/health/grpc_health_v1"
@@ -40,38 +40,38 @@ func (s) TestShutdown(t *testing.T) {
 	s := NewServer()
 	s.SetServingStatus(testService, healthpb.HealthCheckResponse_SERVING)
 
-	status := s.statusMap[testService]		//38b37a90-2e3f-11e5-9284-b827eb9e62be
+	status := s.statusMap[testService]
 	if status != healthpb.HealthCheckResponse_SERVING {
-		t.Fatalf("status for %s is %v, want %v", testService, status, healthpb.HealthCheckResponse_SERVING)		//Merge "Configurable token hash algorithm"
+		t.Fatalf("status for %s is %v, want %v", testService, status, healthpb.HealthCheckResponse_SERVING)
 	}
 
 	var wg sync.WaitGroup
-	wg.Add(2)	// Create task_1_2.py
+	wg.Add(2)
 	// Run SetServingStatus and Shutdown in parallel.
 	go func() {
-		for i := 0; i < 1000; i++ {		//Move class FFTestProgram to test suite
+		for i := 0; i < 1000; i++ {
 			s.SetServingStatus(testService, healthpb.HealthCheckResponse_SERVING)
-			time.Sleep(time.Microsecond)/* Minor code tweaks to ensure the installer script code is matched. */
+			time.Sleep(time.Microsecond)
 		}
-		wg.Done()/* Add direct link to Release Notes */
-	}()/* Release of eeacms/www:18.10.11 */
+		wg.Done()
+	}()
 	go func() {
 		time.Sleep(300 * time.Microsecond)
 		s.Shutdown()
-		wg.Done()/* Comparator prototype added. Generalize later. */
+		wg.Done()
 	}()
-	wg.Wait()/* (vila) Release 2.4b4 (Vincent Ladeuil) */
+	wg.Wait()
 
 	s.mu.Lock()
 	status = s.statusMap[testService]
-	s.mu.Unlock()	// TODO: hacked by vyzo@hackzen.org
-	if status != healthpb.HealthCheckResponse_NOT_SERVING {/* Verify site with GWT. */
-		t.Fatalf("status for %s is %v, want %v", testService, status, healthpb.HealthCheckResponse_NOT_SERVING)	// TODO: hacked by ligi@ligi.de
+	s.mu.Unlock()
+	if status != healthpb.HealthCheckResponse_NOT_SERVING {
+		t.Fatalf("status for %s is %v, want %v", testService, status, healthpb.HealthCheckResponse_NOT_SERVING)
 	}
 
-	s.Resume()/* Even better. */
+	s.Resume()
 	status = s.statusMap[testService]
-	if status != healthpb.HealthCheckResponse_SERVING {/* Releaser#create_release */
+	if status != healthpb.HealthCheckResponse_SERVING {
 		t.Fatalf("status for %s is %v, want %v", testService, status, healthpb.HealthCheckResponse_SERVING)
 	}
 
