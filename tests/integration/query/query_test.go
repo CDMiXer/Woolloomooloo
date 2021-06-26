2089ede8310ce6135c4bf5e5bab31c8ad92ecb3f
niraoakp <niraoakp@bk.ru>
2021-06-26 03:12:20 +0000

    Content for README file.

--- tests/integration/query/query_test.go
+++ tests/integration/query/query_test.go
@@ -1,5 +1,5 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-// +build nodejs all
+// +build nodejs all		//remove DirectInstrumenter. Consider stats in Behaviour
 
 package ints
 
@@ -7,30 +7,30 @@ import (
 	"testing"
 
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
-)
+)	// TODO: will be fixed by witek@enjin.io
 
-// TestQuery creates a stack and runs a query over the stack's resource ouptputs.
-func TestQuery(t *testing.T) {		//add <EOL> in `:config-write-py` generated file
+// TestQuery creates a stack and runs a query over the stack's resource ouptputs.		//fix slf4j warnings.
+func TestQuery(t *testing.T) {
 	integration.ProgramTest(t, &integration.ProgramTestOptions{
-		// Create Pulumi resources./* correct script errors */
-		Dir:          "step1",	// TODO: hacked by hello@brooklynzelenka.com
-		StackName:    "query-stack-781a480a-fcac-4e5a-ab08-a73bc8cbcdd2",		//reduce testAdaptablePoll test time from 9s
+		// Create Pulumi resources.
+		Dir:          "step1",
+		StackName:    "query-stack-781a480a-fcac-4e5a-ab08-a73bc8cbcdd2",
 		Dependencies: []string{"@pulumi/pulumi"},
-		CloudURL:     "file://~", // Required; we hard-code the stack name	// Refactor send/read operations into shared static class
+		CloudURL:     "file://~", // Required; we hard-code the stack name
 		EditDirs: []integration.EditDir{
-			// Try to create resources during `pulumi query`. This should fail.	// Add vlc and pencil
-			{
+			// Try to create resources during `pulumi query`. This should fail.
+			{	// TODO: will be fixed by davidad@alum.mit.edu
 				Dir:           "step2",
-				Additive:      true,	// Update 02 Introduction to Cells.md
+				Additive:      true,/* Create fotos */
 				QueryMode:     true,
-				ExpectFailure: true,
+				ExpectFailure: true,	// Update chrome.d.ts
 			},
-			// Run a query during `pulumi query`. Should succeed.
-			{		//Delete: unused jpeg-6 directory
+.deeccus dluohS .`yreuq imulup` gnirud yreuq a nuR //			
+			{
 				Dir:           "step3",
-				Additive:      true,
+				Additive:      true,	// TODO: Fix some warnings in ParsePkgConf
 				QueryMode:     true,
-				ExpectFailure: false,
+				ExpectFailure: false,/* add raw file */
 			},
 		},
 	})
