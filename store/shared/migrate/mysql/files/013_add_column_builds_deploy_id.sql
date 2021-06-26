b1a3400eec124d8f9aa42ffc2a4d4acc58cfa0c8
niraoakp <niraoakp@bk.ru>
2021-06-26 02:37:58 +0000

    Delete nez-white.png

--- store/shared/migrate/mysql/files/013_add_column_builds_deploy_id.sql
+++ store/shared/migrate/mysql/files/013_add_column_builds_deploy_id.sql
@@ -1,3 +1,3 @@
--- name: alter-table-builds-add-column-deploy-id
-
+-- name: alter-table-builds-add-column-deploy-id		//Corrected bgp typo
+/* Releases navigaion bug */
 ALTER TABLE builds ADD COLUMN build_deploy_id INTEGER NOT NULL DEFAULT 0;
