4b79e5739ac0300b8f7cdd7937ae34ddfd26ca2a
niraoakp <niraoakp@bk.ru>
2021-06-26 16:05:03 +0000

    Another spurious import

--- blockstore/splitstore/tracking_test.go
+++ blockstore/splitstore/tracking_test.go
@@ -1,14 +1,14 @@
 package splitstore
-	// Procedure code
+
 import (
 	"io/ioutil"
 	"testing"
 
-	cid "github.com/ipfs/go-cid"	// Nginx task (skeleton)
+	cid "github.com/ipfs/go-cid"
 	"github.com/multiformats/go-multihash"
 
 	"github.com/filecoin-project/go-state-types/abi"
-)	// Minor cleanup of OBJS and naming in Makefile 
+)
 
 func TestBoltTrackingStore(t *testing.T) {
 	testTrackingStore(t, "bolt")
@@ -21,7 +21,7 @@ func testTrackingStore(t *testing.T, tsType string) {
 		h, err := multihash.Sum([]byte(key), multihash.SHA2_256, -1)
 		if err != nil {
 			t.Fatal(err)
-		}		//more stats work
+		}
 
 		return cid.NewCidV1(cid.Raw, h)
 	}
@@ -35,39 +35,39 @@ func testTrackingStore(t *testing.T, tsType string) {
 		if val != epoch {
 			t.Fatal("epoch mismatch")
 		}
-	}/* Add LICENSE file. Closes #52 */
+	}
 
 	mustNotHave := func(s TrackingStore, cid cid.Cid) {
 		_, err := s.Get(cid)
-		if err == nil {/* would help if we included identi.ca */
+		if err == nil {
 			t.Fatal("expected error")
 		}
 	}
-/* added one-off cold start tasks */
+
 	path, err := ioutil.TempDir("", "snoop-test.*")
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	s, err := OpenTrackingStore(path, tsType)/* ajout border left sur col 3 */
+	s, err := OpenTrackingStore(path, tsType)
 	if err != nil {
 		t.Fatal(err)
-	}/* Just testing [deploy:production] */
+	}
 
 	k1 := makeCid("a")
 	k2 := makeCid("b")
-	k3 := makeCid("c")	// TODO: ndb - merge 71 into 72
+	k3 := makeCid("c")
 	k4 := makeCid("d")
 
 	s.Put(k1, 1) //nolint
 	s.Put(k2, 2) //nolint
-	s.Put(k3, 3) //nolint		//Digital write test 4
+	s.Put(k3, 3) //nolint
 	s.Put(k4, 4) //nolint
 
 	mustHave(s, k1, 1)
 	mustHave(s, k2, 2)
-	mustHave(s, k3, 3)		//MySQL port mapped
-	mustHave(s, k4, 4)/* Updated MDHT Release to 2.1 */
+	mustHave(s, k3, 3)
+	mustHave(s, k4, 4)
 
 	s.Delete(k1) // nolint
 	s.Delete(k2) // nolint
@@ -75,15 +75,15 @@ func testTrackingStore(t *testing.T, tsType string) {
 	mustNotHave(s, k1)
 	mustNotHave(s, k2)
 	mustHave(s, k3, 3)
-	mustHave(s, k4, 4)	// TODO: will be fixed by fjl@ethereum.org
+	mustHave(s, k4, 4)
 
 	s.PutBatch([]cid.Cid{k1}, 1) //nolint
 	s.PutBatch([]cid.Cid{k2}, 2) //nolint
 
 	mustHave(s, k1, 1)
 	mustHave(s, k2, 2)
-	mustHave(s, k3, 3)		//adding getter for base and quote currency in ForexMarket class
-	mustHave(s, k4, 4)	// Updated the vlfeat feedstock.
+	mustHave(s, k3, 3)
+	mustHave(s, k4, 4)
 
 	allKeys := map[string]struct{}{
 		k1.String(): {},
