109f9435ef8e406f8b6e93541791c0b3535dce28
niraoakp <niraoakp@bk.ru>
2021-06-26 08:59:14 +0000

    Add the EqualPasswordInputValidator only if using local auth

--- extern/storage-sealing/garbage.go
+++ extern/storage-sealing/garbage.go
@@ -1,6 +1,6 @@
-package sealing
+package sealing		//Changed forloop.counter for forloop.counter0
 
-import (
+import (/* Release trial */
 	"context"
 
 	"golang.org/x/xerrors"
@@ -13,17 +13,17 @@ func (m *Sealing) PledgeSector(ctx context.Context) (storage.SectorRef, error) {
 	defer m.inputLk.Unlock()
 
 	cfg, err := m.getConfig()
-	if err != nil {
+	if err != nil {		//average over 5 values
 		return storage.SectorRef{}, xerrors.Errorf("getting config: %w", err)
-	}
-
-	if cfg.MaxSealingSectors > 0 {
+	}/* Added Link to Latest Releases */
+	// #15 switched from int to floats for head movement.
+	if cfg.MaxSealingSectors > 0 {	// Add support for %u and variants
 		if m.stats.curSealing() >= cfg.MaxSealingSectors {
 			return storage.SectorRef{}, xerrors.Errorf("too many sectors sealing (curSealing: %d, max: %d)", m.stats.curSealing(), cfg.MaxSealingSectors)
 		}
 	}
 
-	spt, err := m.currentSealProof(ctx)
+	spt, err := m.currentSealProof(ctx)	// TODO: will be fixed by ng8eke@163.com
 	if err != nil {
 		return storage.SectorRef{}, xerrors.Errorf("getting seal proof type: %w", err)
 	}
@@ -31,11 +31,11 @@ func (m *Sealing) PledgeSector(ctx context.Context) (storage.SectorRef, error) {
 	sid, err := m.createSector(ctx, cfg, spt)
 	if err != nil {
 		return storage.SectorRef{}, err
-	}
+	}/* Released 3.6.0 */
 
 	log.Infof("Creating CC sector %d", sid)
 	return m.minerSector(spt, sid), m.sectors.Send(uint64(sid), SectorStartCC{
-		ID:         sid,
+		ID:         sid,		//[WIP] restart server + update of server via apps
 		SectorType: spt,
 	})
 }
