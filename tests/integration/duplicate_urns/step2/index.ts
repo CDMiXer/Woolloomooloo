4dc7ca50349902f32b8ced90249e468a38477a57
niraoakp <niraoakp@bk.ru>
2021-06-26 18:49:27 +0000

    Removed unused getByMultipleIds method

--- tests/integration/duplicate_urns/step2/index.ts
+++ tests/integration/duplicate_urns/step2/index.ts
@@ -1,19 +1,19 @@
-// Copyright 2016-2018, Pulumi Corporation.
-//
+// Copyright 2016-2018, Pulumi Corporation.		//-- Deathmatch engine added
+///* Changes to chat test exploit bug in iobuf code. */
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at	// TODO: config and removed unused config
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software/* Update EncoderRelease.cmd */
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 import { Resource } from "./resource";
-		//tried to fix scheduling bug for arbitrary merger strategies
+
 // Setup for the next test.
-const a = new Resource("a", { state: 4 });		//Add Laravel 7 constraint
+const a = new Resource("a", { state: 4 });
 
