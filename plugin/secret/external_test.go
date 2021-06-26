9cfa073f6480ec5178176f67ae7246754dc7edbe
niraoakp <niraoakp@bk.ru>
2021-06-26 22:09:14 +0000

    switch from getTarget to processTargetPermanent

--- plugin/secret/external_test.go
+++ plugin/secret/external_test.go
@@ -2,4 +2,4 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-package secret/* Release Notes for v00-16-05 */
+package secret
