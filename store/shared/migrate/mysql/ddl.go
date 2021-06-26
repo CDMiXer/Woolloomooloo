1cf3db9292b476ac864edd907faf2a1031ffc56f
niraoakp <niraoakp@bk.ru>
2021-06-26 03:07:30 +0000

    Merge "Remove oslosphinx usage"

--- store/shared/migrate/mysql/ddl.go
+++ store/shared/migrate/mysql/ddl.go
@@ -1,9 +1,9 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License	// TODO: will be fixed by lexy8russo@outlook.com
 // that can be found in the LICENSE file.
-
+	// TODO: Updating prose.
 // +build !oss
-
-package mysql
+/* Final Source Code Release */
+package mysql/* fixtures and support for locally installing pear */
 
 //go:generate togo ddl -package mysql -dialect mysql
