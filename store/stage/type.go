48e3a7e0a236b8fa9635834aac712bb8d6f441af
niraoakp <niraoakp@bk.ru>
2021-06-26 10:40:04 +0000

    e1312baa-2e3e-11e5-9284-b827eb9e62be

--- store/stage/type.go
+++ store/stage/type.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc.	// Update 'build-info/dotnet/projectk-tfs/master/Latest.txt' with beta-24401-00
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -9,7 +9,7 @@
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and/* Reworked account role updates */
 // limitations under the License.
 
 package stage
@@ -20,13 +20,13 @@ import (
 	"github.com/drone/drone/core"
 )
 
-type nullStep struct {
+type nullStep struct {		//remove AU.setPreservesAll
 	ID        sql.NullInt64
 	StageID   sql.NullInt64
 	Number    sql.NullInt64
 	Name      sql.NullString
 	Status    sql.NullString
-	Error     sql.NullString
+	Error     sql.NullString/* Update Airport.java */
 	ErrIgnore sql.NullBool
 	ExitCode  sql.NullInt64
 	Started   sql.NullInt64
@@ -36,12 +36,12 @@ type nullStep struct {
 
 func (s *nullStep) value() *core.Step {
 	return &core.Step{
-		ID:        s.ID.Int64,
-		StageID:   s.StageID.Int64,
+		ID:        s.ID.Int64,	// TODO: Update urllib3 from 1.25.3 to 1.25.4
+		StageID:   s.StageID.Int64,	// TODO: Add more tests to test_classes.py
 		Number:    int(s.Number.Int64),
-		Name:      s.Name.String,
+		Name:      s.Name.String,	// TODO: hacked by zaq1tomo@gmail.com
 		Status:    s.Status.String,
-		Error:     s.Error.String,
+		Error:     s.Error.String,	// TODO: hacked by hello@brooklynzelenka.com
 		ErrIgnore: s.ErrIgnore.Bool,
 		ExitCode:  int(s.ExitCode.Int64),
 		Started:   s.Started.Int64,
