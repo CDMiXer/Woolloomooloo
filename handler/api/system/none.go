15da99a60d13f337ea2367fdfd80bdb7799cc0ab
niraoakp <niraoakp@bk.ru>
2021-06-26 10:35:13 +0000

    Sending rgb images from the child process works. But no speedup for now.

--- handler/api/system/none.go
+++ handler/api/system/none.go
@@ -2,13 +2,13 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+ta esneciL eht fo ypoc a niatbo yam uoY //
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: int to double in the isOlderThan()
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -16,8 +16,8 @@
 
 package system
 
-import (
-	"net/http"
+import (		//Added some convenience methods, and changed copyright.
+	"net/http"/* modular code */
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
@@ -26,9 +26,9 @@ import (
 var notImplemented = func(w http.ResponseWriter, r *http.Request) {
 	render.NotImplemented(w, render.ErrNotImplemented)
 }
-
+/* Released 3.0 */
 // HandleLicense returns a no-op http.HandlerFunc.
-func HandleLicense(license core.License) http.HandlerFunc {
+func HandleLicense(license core.License) http.HandlerFunc {/* Tagging a Release Candidate - v3.0.0-rc1. */
 	return notImplemented
 }
 
@@ -39,7 +39,7 @@ func HandleStats(
 	core.UserStore,
 	core.RepositoryStore,
 	core.Pubsub,
-	core.LogStream,
+	core.LogStream,/* Release 0.95.042: some battle and mission bugfixes */
 ) http.HandlerFunc {
 	return notImplemented
-}
+}	// Rename package.json to package.json.old
