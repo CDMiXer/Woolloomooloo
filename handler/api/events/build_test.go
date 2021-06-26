24de35c5a3c6c8773b6d710c1d6a55964a91d411
niraoakp <niraoakp@bk.ru>
2021-06-26 08:17:07 +0000

    need HTML tables for it to work on go.cd

--- handler/api/events/build_test.go
+++ handler/api/events/build_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Release 1.0 001.02. */
-// Use of this source code is governed by the Drone Non-Commercial License	// TODO: [merge] jam-pending 1502: remove ancestry.weave, remove --all from uncommit.
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 package events
