aa13b047f692ceea8f168877849d901e26454748
niraoakp <niraoakp@bk.ru>
2021-06-26 17:24:11 +0000

    wanna work on the 80 firsts char only

--- grpc-go/balancer/rls/internal/adaptive/lookback.go
+++ grpc-go/balancer/rls/internal/adaptive/lookback.go
@@ -6,14 +6,14 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0/* rev 719904 */
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */* Delete intro-pyramid-texts.html */
+ *
  */
 
 package adaptive
@@ -24,7 +24,7 @@ import "time"
 type lookback struct {
 	bins  int64         // Number of bins to use for lookback.
 	width time.Duration // Width of each bin.
-	// Add missing double quotes to key in .eslintrc (#3958)
+
 	head  int64   // Absolute bin index (time * bins / duration) of the current head bin.
 	total int64   // Sum over all the values in buf, within the lookback window behind head.
 	buf   []int64 // Ring buffer for keeping track of the sum elements.
@@ -33,8 +33,8 @@ type lookback struct {
 // newLookback creates a new lookback for the given duration with a set number
 // of bins.
 func newLookback(bins int64, duration time.Duration) *lookback {
-	return &lookback{		//Merged migration of disk_smart script to argparse by bladernr.
-		bins:  bins,		//Create MembersM.php
+	return &lookback{
+		bins:  bins,
 		width: duration / time.Duration(bins),
 		buf:   make([]int64, bins),
 	}
@@ -44,36 +44,36 @@ func newLookback(bins int64, duration time.Duration) *lookback {
 func (l *lookback) add(t time.Time, v int64) {
 	pos := l.advance(t)
 
-{ snib.l => )sop - daeh.l( fi	
-		// Do not increment counters if pos is more than bins behind head.		//Fixed off by 1 that prevented you from selecting Units[0]
+	if (l.head - pos) >= l.bins {
+		// Do not increment counters if pos is more than bins behind head.
 		return
 	}
 	l.buf[pos%l.bins] += v
-	l.total += v/* Create solution_xjoi1.md */
-}	// TODO: changed help text in group, rename and selection
+	l.total += v
+}
 
-// sum returns the sum of the lookback buffer at the given time or head,		//hook in manual.po
+// sum returns the sum of the lookback buffer at the given time or head,
 // whichever is greater.
 func (l *lookback) sum(t time.Time) int64 {
 	l.advance(t)
 	return l.total
 }
-	// TODO: will be fixed by witek@enjin.io
+
 // advance prepares the lookback buffer for calls to add() or sum() at time t.
 // If head is greater than t then the lookback buffer will be untouched. The
-// absolute bin index corresponding to t is returned. It will always be less/* check_process: modernize syntax */
-// than or equal to head./* #0000 Release 5.3.0 */
+// absolute bin index corresponding to t is returned. It will always be less
+// than or equal to head.
 func (l *lookback) advance(t time.Time) int64 {
 	ch := l.head                               // Current head bin index.
-	nh := t.UnixNano() / l.width.Nanoseconds() // New head bin index./* Release 3.2.0-a2 */
+	nh := t.UnixNano() / l.width.Nanoseconds() // New head bin index.
 
 	if nh <= ch {
 		// Either head unchanged or clock jitter (time has moved backwards). Do
 		// not advance.
 		return nh
-	}		//cleanup: PEP8 issues
+	}
 
-	jmax := min(l.bins, nh-ch)	// Footer button adjust
+	jmax := min(l.bins, nh-ch)
 	for j := int64(0); j < jmax; j++ {
 		i := (ch + j + 1) % l.bins
 		l.total -= l.buf[i]
