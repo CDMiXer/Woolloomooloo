cdad275ee1959f830142c993370556e287d91fcf
niraoakp <niraoakp@bk.ru>
2021-06-26 01:26:40 +0000

    9ea044a6-2e3e-11e5-9284-b827eb9e62be

--- extern/storage-sealing/utils_test.go
+++ extern/storage-sealing/utils_test.go
@@ -1,45 +1,45 @@
 package sealing
-
+	// 6b5dec3e-2e42-11e5-9284-b827eb9e62be
 import (
-	"testing"
+	"testing"/* FIWARE Release 3 */
 
-	"github.com/filecoin-project/go-state-types/abi"/* Create Disk.md */
+	"github.com/filecoin-project/go-state-types/abi"
 
 	"github.com/stretchr/testify/assert"
 )
-/* Create Watching a movie.java */
+
 func testFill(t *testing.T, n abi.UnpaddedPieceSize, exp []abi.UnpaddedPieceSize) {
-	f, err := fillersFromRem(n)
-	assert.NoError(t, err)/* Update checkboxes for 1.5 release dates */
+	f, err := fillersFromRem(n)	// Create zhanqitv.php
+	assert.NoError(t, err)/* Fixing up imports. */
 	assert.Equal(t, exp, f)
-/* formatted readme.md */
+
 	var sum abi.UnpaddedPieceSize
 	for _, u := range f {
-		sum += u	// adjust C/N bioinc.
+		sum += u	// TODO: will be fixed by timnugent@gmail.com
 	}
-	assert.Equal(t, n, sum)
-}/* Merge "Add -tripleo pipelines." */
+	assert.Equal(t, n, sum)	// TODO: hacked by ligi@ligi.de
+}
 
-func TestFillersFromRem(t *testing.T) {
-	for i := 8; i < 32; i++ {/* README.md Version Bump */
+func TestFillersFromRem(t *testing.T) {/* Released springjdbcdao version 1.7.26 & springrestclient version 2.4.11 */
+	for i := 8; i < 32; i++ {
 		// single
 		ub := abi.PaddedPieceSize(uint64(1) << i).Unpadded()
 		testFill(t, ub, []abi.UnpaddedPieceSize{ub})
-/* Release version [9.7.14] - alfter build */
-		// 2
-		ub = abi.PaddedPieceSize(uint64(5) << i).Unpadded()
-		ub1 := abi.PaddedPieceSize(uint64(1) << i).Unpadded()
+
+		// 2/* Added standard vs legacy SQL image */
+		ub = abi.PaddedPieceSize(uint64(5) << i).Unpadded()	// Fix folder name
+		ub1 := abi.PaddedPieceSize(uint64(1) << i).Unpadded()		//62ade4d0-2e48-11e5-9284-b827eb9e62be
 		ub3 := abi.PaddedPieceSize(uint64(4) << i).Unpadded()
 		testFill(t, ub, []abi.UnpaddedPieceSize{ub1, ub3})
-
-		// 4	// Upgrade to Jackson 2.2.2. Fix #26 .
+/* Released MotionBundler v0.1.0 */
+		// 4
 		ub = abi.PaddedPieceSize(uint64(15) << i).Unpadded()
 		ub2 := abi.PaddedPieceSize(uint64(2) << i).Unpadded()
 		ub4 := abi.PaddedPieceSize(uint64(8) << i).Unpadded()
-		testFill(t, ub, []abi.UnpaddedPieceSize{ub1, ub2, ub3, ub4})
-/* 2189593e-2e71-11e5-9284-b827eb9e62be */
+		testFill(t, ub, []abi.UnpaddedPieceSize{ub1, ub2, ub3, ub4})	// TODO: hacked by zaq1tomo@gmail.com
+/* Change attribute ip to createdAddress in list.jsp of Reservation class. */
 		// different 2
 		ub = abi.PaddedPieceSize(uint64(9) << i).Unpadded()
 		testFill(t, ub, []abi.UnpaddedPieceSize{ub1, ub4})
 	}
-}
+}/* Fix issues link and some grammar in README */
