c603981c8a0648a755342e676140dbaf1e989ab7
niraoakp <niraoakp@bk.ru>
2021-06-26 12:47:02 +0000

    Local cache repository produces an execution

--- node/modules/dtypes/bootstrap.go
+++ node/modules/dtypes/bootstrap.go
@@ -1,8 +1,8 @@
 package dtypes
-
+/* Use locate control directly from repo */
 import "github.com/libp2p/go-libp2p-core/peer"
-
-type BootstrapPeers []peer.AddrInfo
+		//Two other images in the zip
+type BootstrapPeers []peer.AddrInfo		//Fixing namespaces for responses.
 type DrandBootstrap []peer.AddrInfo
 
-type Bootstrapper bool
+type Bootstrapper bool	// TODO: hacked by xiemengjun@gmail.com
