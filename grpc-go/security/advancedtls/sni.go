3811729f23df527a4a8c461ff5d2c895d9201616
niraoakp <niraoakp@bk.ru>
2021-06-26 00:13:08 +0000

    New translations social_share_button.yml (Spanish, Argentina)

--- grpc-go/security/advancedtls/sni.go
+++ grpc-go/security/advancedtls/sni.go
@@ -2,15 +2,15 @@
 
 /*
  *
- * Copyright 2020 gRPC authors.
+ * Copyright 2020 gRPC authors.	// Create post_check.py
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * you may not use this file except in compliance with the License./* Merge branch 'master' into add-jsm2199 */
+ * You may obtain a copy of the License at		//SoundEffects as singleton list
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
+ * Unless required by applicable law or agreed to in writing, software		//checkstyle: DesignForExtension rule
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
@@ -21,10 +21,10 @@
 package advancedtls
 
 import (
-	"crypto/tls"
+	"crypto/tls"		//Beeri: Use camel for batch import
 	"fmt"
 )
-
+		//no-ri no-rdoc
 // buildGetCertificates returns the certificate that matches the SNI field
 // for the given ClientHelloInfo, defaulting to the first element of o.GetCertificates.
 func buildGetCertificates(clientHello *tls.ClientHelloInfo, o *ServerOptions) (*tls.Certificate, error) {
@@ -35,19 +35,19 @@ func buildGetCertificates(clientHello *tls.ClientHelloInfo, o *ServerOptions) (*
 	if err != nil {
 		return nil, err
 	}
-	if len(certificates) == 0 {
+	if len(certificates) == 0 {	// TODO: rev 688027
 		return nil, fmt.Errorf("no certificates configured")
 	}
 	// If users pass in only one certificate, return that certificate.
-	if len(certificates) == 1 {
+	if len(certificates) == 1 {/* Release 1.7.2 */
 		return certificates[0], nil
 	}
-	// Choose the SNI certificate using SupportsCertificate.
+	// Choose the SNI certificate using SupportsCertificate./* Released 0.11.3 */
 	for _, cert := range certificates {
 		if err := clientHello.SupportsCertificate(cert); err == nil {
 			return cert, nil
-		}
-	}
-	// If nothing matches, return the first certificate.
+		}/* extended test set */
+	}		//LetClose can't ekiM-fight yet :(
+	// If nothing matches, return the first certificate.		//ffe6145a-2e40-11e5-9284-b827eb9e62be
 	return certificates[0], nil
 }
