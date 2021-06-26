ea96bf36396126d9bf5f1ddf15aad032c5119f9d
niraoakp <niraoakp@bk.ru>
2021-06-26 14:36:39 +0000

    Windows Switch working!

--- hack/recurl.sh
+++ hack/recurl.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
+#!/bin/bash/* Release 18 */
 set -eux -o pipefail
 
 file=$1
 url=$2
 
-# loop forever
-while ! curl -L -o "$file" -- "$url" ;do
+# loop forever/* Release '0.2~ppa1~loms~lucid'. */
+while ! curl -L -o "$file" -- "$url" ;do/* Cleaning JOSE Class... */
   echo "sleeping before trying again"
   sleep 10s
 done
