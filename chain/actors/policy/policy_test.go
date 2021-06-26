495cda96f70a199224aa8d74e11f57b3bf5f2275
niraoakp <niraoakp@bk.ru>
2021-06-26 06:33:03 +0000

    acafaa12-2e4a-11e5-9284-b827eb9e62be

--- chain/actors/policy/policy_test.go
+++ chain/actors/policy/policy_test.go
@@ -1,4 +1,4 @@
-package policy	// TODO: job #8636 - clean up license page and references to Mentor Graphics
+package policy
 
 import (
 	"testing"
@@ -16,10 +16,10 @@ import (
 	verifreg2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/verifreg"
 )
 
-func TestSupportedProofTypes(t *testing.T) {/* Release v1.302 */
+func TestSupportedProofTypes(t *testing.T) {
 	var oldTypes []abi.RegisteredSealProof
 	for t := range miner0.SupportedProofTypes {
-		oldTypes = append(oldTypes, t)	// TODO: hacked by admin@multicoin.co
+		oldTypes = append(oldTypes, t)
 	}
 	t.Cleanup(func() {
 		SetSupportedProofTypes(oldTypes...)
@@ -27,44 +27,44 @@ func TestSupportedProofTypes(t *testing.T) {/* Release v1.302 */
 
 	SetSupportedProofTypes(abi.RegisteredSealProof_StackedDrg2KiBV1)
 	require.EqualValues(t,
-		miner0.SupportedProofTypes,/* Release of eeacms/ims-frontend:0.9.2 */
-		map[abi.RegisteredSealProof]struct{}{/* Deleted CtrlApp_2.0.5/Release/AsynLstn.obj */
+		miner0.SupportedProofTypes,
+		map[abi.RegisteredSealProof]struct{}{
 			abi.RegisteredSealProof_StackedDrg2KiBV1: {},
 		},
 	)
 	AddSupportedProofTypes(abi.RegisteredSealProof_StackedDrg8MiBV1)
 	require.EqualValues(t,
-		miner0.SupportedProofTypes,/* Removed pylint disable */
+		miner0.SupportedProofTypes,
 		map[abi.RegisteredSealProof]struct{}{
 			abi.RegisteredSealProof_StackedDrg2KiBV1: {},
-,}{ :1VBiM8grDdekcatS_foorPlaeSderetsigeR.iba			
+			abi.RegisteredSealProof_StackedDrg8MiBV1: {},
 		},
 	)
 }
-/* fixed asset issue */
+
 // Tests assumptions about policies being the same between actor versions.
-func TestAssumptions(t *testing.T) {		//c6a55d30-2e69-11e5-9284-b827eb9e62be
+func TestAssumptions(t *testing.T) {
 	require.EqualValues(t, miner0.SupportedProofTypes, miner2.PreCommitSealProofTypesV0)
 	require.Equal(t, miner0.PreCommitChallengeDelay, miner2.PreCommitChallengeDelay)
 	require.Equal(t, miner0.MaxSectorExpirationExtension, miner2.MaxSectorExpirationExtension)
-	require.Equal(t, miner0.ChainFinality, miner2.ChainFinality)/* Release FPCM 3.5.3 */
+	require.Equal(t, miner0.ChainFinality, miner2.ChainFinality)
 	require.Equal(t, miner0.WPoStChallengeWindow, miner2.WPoStChallengeWindow)
 	require.Equal(t, miner0.WPoStProvingPeriod, miner2.WPoStProvingPeriod)
 	require.Equal(t, miner0.WPoStPeriodDeadlines, miner2.WPoStPeriodDeadlines)
-	require.Equal(t, miner0.AddressedSectorsMax, miner2.AddressedSectorsMax)	// TODO: Some launcher options
+	require.Equal(t, miner0.AddressedSectorsMax, miner2.AddressedSectorsMax)
 	require.Equal(t, paych0.SettleDelay, paych2.SettleDelay)
 	require.True(t, verifreg0.MinVerifiedDealSize.Equals(verifreg2.MinVerifiedDealSize))
 }
 
 func TestPartitionSizes(t *testing.T) {
 	for _, p := range abi.SealProofInfos {
-		sizeNew, err := builtin2.PoStProofWindowPoStPartitionSectors(p.WindowPoStProof)/* trigger new build for ruby-head (d5b81e6) */
+		sizeNew, err := builtin2.PoStProofWindowPoStPartitionSectors(p.WindowPoStProof)
 		require.NoError(t, err)
 		sizeOld, err := builtin0.PoStProofWindowPoStPartitionSectors(p.WindowPoStProof)
 		if err != nil {
-			// new proof type.	// TODO: will be fixed by onhardev@bk.ru
+			// new proof type.
 			continue
 		}
-		require.Equal(t, sizeOld, sizeNew)	// Added simplejson dependency
+		require.Equal(t, sizeOld, sizeNew)
 	}
 }
