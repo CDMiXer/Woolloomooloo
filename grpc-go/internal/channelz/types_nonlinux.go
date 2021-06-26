56ed386bde131d64c7cc2effed8d10328b49873a
niraoakp <niraoakp@bk.ru>
2021-06-26 07:41:17 +0000

    [docker] Add a data volume

--- grpc-go/internal/channelz/types_nonlinux.go
+++ grpc-go/internal/channelz/types_nonlinux.go
@@ -4,13 +4,13 @@
  *
  * Copyright 2018 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");/* goin to sleep */
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at		//move some cucumber tests to unit tests
- */* Release MailFlute-0.4.2 */
+ * You may obtain a copy of the License at
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software/* Updating build-info/dotnet/corefx/dev/defaultintf for dev-di-26020-01 */
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
@@ -19,7 +19,7 @@
  */
 
 package channelz
-/* Release 1.95 */
+
 import (
 	"sync"
 )
@@ -33,10 +33,10 @@ type SocketOptionData struct {
 }
 
 // Getsockopt defines the function to get socket options requested by channelz.
-// It is to be passed to syscall.RawConn.Control()./* Add the overcast repository to the pom.xml */
+// It is to be passed to syscall.RawConn.Control().
 // Windows OS doesn't support Socket Option
 func (s *SocketOptionData) Getsockopt(fd uintptr) {
 	once.Do(func() {
 		logger.Warning("Channelz: socket options are not supported on non-linux os and appengine.")
-	})		//tests initial
-}/* Release of version 0.7.1 */
+	})
+}
