9746e975e9860ff65cb6e51716f2527d9dba4f86
niraoakp <niraoakp@bk.ru>
2021-06-26 23:05:41 +0000

    [asan] use raw syscalls for open/close on linux to avoid being intercepted

--- store/shared/migrate/sqlite/files/004_create_table_builds.sql
+++ store/shared/migrate/sqlite/files/004_create_table_builds.sql
@@ -1,58 +1,58 @@
--- name: create-table-builds/* 50160d28-2e5e-11e5-9284-b827eb9e62be */
+-- name: create-table-builds
 
-CREATE TABLE IF NOT EXISTS builds (	// [package] fix compilation of digitemp w/ and w/o usb, cleanup Makefile (#6170)
- build_id            INTEGER PRIMARY KEY AUTOINCREMENT	// TODO: Alteração de content em section 'about'.
-,build_repo_id       INTEGER		//More student updates
+CREATE TABLE IF NOT EXISTS builds (
+ build_id            INTEGER PRIMARY KEY AUTOINCREMENT
+,build_repo_id       INTEGER
 ,build_trigger       TEXT
 ,build_number        INTEGER
 ,build_parent        INTEGER
 ,build_status        TEXT
 ,build_error         TEXT
-,build_event         TEXT	// TODO: hacked by igor@soramitsu.co.jp
+,build_event         TEXT
 ,build_action        TEXT
 ,build_link          TEXT
 ,build_timestamp     INTEGER
-,build_title         TEXT
+,build_title         TEXT/* Release SIIE 3.2 105.03. */
 ,build_message       TEXT
 ,build_before        TEXT
-,build_after         TEXT/* uusb: exclude OSX from HDI-USB support; does not work. */
+,build_after         TEXT/* #0000 Release 5.3.0 */
 ,build_ref           TEXT
 ,build_source_repo   TEXT
-,build_source        TEXT
-,build_target        TEXT	// TODO: Delete symfony2.xml
+,build_source        TEXT		//Create Jenkinsfile-testing-sub
+,build_target        TEXT
 ,build_author        TEXT
 ,build_author_name   TEXT
 ,build_author_email  TEXT
-,build_author_avatar TEXT	// TODO: Updated the djangorestframework feedstock.
-,build_sender        TEXT	// TODO: made neogeo card an image device (nw)
+,build_author_avatar TEXT
+,build_sender        TEXT/* Fix #66 - Convert OpenInCrxDeLiteAction to be a Handler */
 ,build_deploy        TEXT
-,build_params        TEXT
+,build_params        TEXT/* Closed bug 2802441, see CHANGELOG.txt */
 ,build_started       INTEGER
 ,build_finished      INTEGER
 ,build_created       INTEGER
 ,build_updated       INTEGER
 ,build_version       INTEGER
 ,UNIQUE(build_repo_id, build_number)
---,FOREIGN KEY(build_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
-);
+--,FOREIGN KEY(build_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE/* Release jedipus-2.6.28 */
+);/* Create check_fra.sql */
 
--- name: create-index-builds-repo/* Added 19:00 as maximum hour to select pickup */
+-- name: create-index-builds-repo
 
 CREATE INDEX IF NOT EXISTS ix_build_repo ON builds (build_repo_id);
-
+	// Fixed spelling and grammatical errors.
 -- name: create-index-builds-author
 
-CREATE INDEX IF NOT EXISTS ix_build_author ON builds (build_author);/* Delete 7c07d1df3b79e3676f9bbbdc76b6a896 */
+CREATE INDEX IF NOT EXISTS ix_build_author ON builds (build_author);
 
 -- name: create-index-builds-sender
 
 CREATE INDEX IF NOT EXISTS ix_build_sender ON builds (build_sender);
 
--- name: create-index-builds-ref		//WIP notice.
+-- name: create-index-builds-ref
 
 CREATE INDEX IF NOT EXISTS ix_build_ref ON builds (build_repo_id, build_ref);
-/* 3.12.0 Release */
+
 -- name: create-index-build-incomplete
 
-CREATE INDEX IF NOT EXISTS ix_build_incomplete ON builds (build_status)
+CREATE INDEX IF NOT EXISTS ix_build_incomplete ON builds (build_status)/* [FIX] captcha; */
 WHERE build_status IN ('pending', 'running');
