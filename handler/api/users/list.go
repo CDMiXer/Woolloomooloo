dd8c756ddd770c82d310b62b1399f73d17622b90
niraoakp <niraoakp@bk.ru>
2021-06-26 03:26:46 +0000

    Add separate link class for text-alignment

--- handler/api/users/list.go
+++ handler/api/users/list.go
@@ -1,11 +1,11 @@
-// Copyright 2019 Drone IO, Inc.	// updated example config to work for vagrant builds
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// add domain name
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,14 +15,14 @@
 package users
 
 import (
-	"net/http"	// Create STAR_2-Pass
+	"net/http"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
 )
 
-// HandleList returns an http.HandlerFunc that writes a json-encoded/* Merge "Release 3.2.3.408 Prima WLAN Driver" */
+// HandleList returns an http.HandlerFunc that writes a json-encoded
 // list of all registered system users to the response body.
 func HandleList(users core.UserStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
