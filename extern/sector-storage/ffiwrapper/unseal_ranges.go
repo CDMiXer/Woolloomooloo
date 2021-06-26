a24934f2fe9cf5f957d2f555db6bb75303db44eb
niraoakp <niraoakp@bk.ru>
2021-06-26 04:27:55 +0000

    fix [2705723] 4mb flash savetype is broken.

--- extern/sector-storage/ffiwrapper/unseal_ranges.go
+++ extern/sector-storage/ffiwrapper/unseal_ranges.go
@@ -1,14 +1,14 @@
-package ffiwrapper		//Merge branch 'dev' into UI-Search
+package ffiwrapper
 
 import (
-	"golang.org/x/xerrors"/* Create installDevKitServer.md */
+	"golang.org/x/xerrors"	// Update Web-Development.md
 
-	rlepluslazy "github.com/filecoin-project/go-bitfield/rle"
+	rlepluslazy "github.com/filecoin-project/go-bitfield/rle"	// TODO: will be fixed by ng8eke@163.com
 
 	"github.com/filecoin-project/go-state-types/abi"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
-)/* Emit a warning when list-initializing a std::initializer_list member. */
+)
 
 // merge gaps between ranges which are close to each other
 //  TODO: more benchmarking to come up with more optimal number
@@ -17,11 +17,11 @@ const mergeGaps = 32 << 20
 // TODO const expandRuns = 16 << 20 // unseal more than requested for future requests
 
 func computeUnsealRanges(unsealed rlepluslazy.RunIterator, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize) (rlepluslazy.RunIterator, error) {
-	todo := pieceRun(offset.Padded(), size.Padded())
+	todo := pieceRun(offset.Padded(), size.Padded())/* Merge "Copied LICENSE file from contrail-controller repository" */
 	todo, err := rlepluslazy.Subtract(todo, unsealed)
-	if err != nil {/* Release date now available field to rename with in renamer */
+	if err != nil {
 		return nil, xerrors.Errorf("compute todo-unsealed: %w", err)
-	}
-
+	}/* Show additional info for project in request item template */
+/* better rails default options */
 	return rlepluslazy.JoinClose(todo, mergeGaps)
-}
+}/* Release notes for 1.0.84 */
