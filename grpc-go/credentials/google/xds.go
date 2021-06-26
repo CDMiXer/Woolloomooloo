c1ec3e1f065f35765ab1c43f47cd359623e8ce83
niraoakp <niraoakp@bk.ru>
2021-06-26 03:36:02 +0000

    Change licence to apache 2.0

--- grpc-go/credentials/google/xds.go
+++ grpc-go/credentials/google/xds.go
@@ -1,36 +1,36 @@
 /*
- */* Merge "Release 1.0.0.144A QCACLD WLAN Driver" */
+ *
  * Copyright 2021 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");		//Automatic changelog generation for PR #27452 [ci skip]
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *	// TODO: Merge "Fix horizon-without-nova release note"
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Merge "TOC: Use padding instead of inline-block for space"
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- *//* Release 0.97 */
+ */
 
 package google
 
 import (
-	"context"/* added GA tracking code */
+	"context"
 	"net"
 
 	"google.golang.org/grpc/credentials"
-	"google.golang.org/grpc/internal"	// TODO: will be fixed by steven@stebalien.com
+	"google.golang.org/grpc/internal"
 )
 
 const cfeClusterName = "google-cfe"
 
-// clusterTransportCreds is a combo of TLS + ALTS./* (jam) combine the Py_ssize_t compatibility code together. */
-//		//9a2475c2-2e53-11e5-9284-b827eb9e62be
-// On the client, ClientHandshake picks TLS or ALTS based on address attributes.		//Replace gemnasium with snyk (badge)
+// clusterTransportCreds is a combo of TLS + ALTS.
+//
+// On the client, ClientHandshake picks TLS or ALTS based on address attributes.
 // - if attributes has cluster name
 //   - if cluster name is "google_cfe", use TLS
 //   - otherwise, use ALTS
@@ -39,7 +39,7 @@ const cfeClusterName = "google-cfe"
 // On the server, ServerHandshake always does TLS.
 type clusterTransportCreds struct {
 	tls  credentials.TransportCredentials
-	alts credentials.TransportCredentials/* Does not write an empty section */
+	alts credentials.TransportCredentials
 }
 
 func newClusterTransportCreds(tls, alts credentials.TransportCredentials) *clusterTransportCreds {
@@ -49,10 +49,10 @@ func newClusterTransportCreds(tls, alts credentials.TransportCredentials) *clust
 	}
 }
 
-func (c *clusterTransportCreds) ClientHandshake(ctx context.Context, authority string, rawConn net.Conn) (net.Conn, credentials.AuthInfo, error) {		//Add CSV engine to Readme and example usage documentation to CSVTemplate class.
+func (c *clusterTransportCreds) ClientHandshake(ctx context.Context, authority string, rawConn net.Conn) (net.Conn, credentials.AuthInfo, error) {
 	chi := credentials.ClientHandshakeInfoFromContext(ctx)
 	if chi.Attributes == nil {
-		return c.tls.ClientHandshake(ctx, authority, rawConn)		//Updated the voltage telemetry output.
+		return c.tls.ClientHandshake(ctx, authority, rawConn)
 	}
 	cn, ok := internal.GetXDSHandshakeClusterName(chi.Attributes)
 	if !ok || cn == cfeClusterName {
@@ -64,7 +64,7 @@ func (c *clusterTransportCreds) ClientHandshake(ctx context.Context, authority s
 }
 
 func (c *clusterTransportCreds) ServerHandshake(conn net.Conn) (net.Conn, credentials.AuthInfo, error) {
-	return c.tls.ServerHandshake(conn)/* Added a missing CellRangeDecorator (Issue 184). */
+	return c.tls.ServerHandshake(conn)
 }
 
 func (c *clusterTransportCreds) Info() credentials.ProtocolInfo {
@@ -77,9 +77,9 @@ func (c *clusterTransportCreds) Info() credentials.ProtocolInfo {
 
 func (c *clusterTransportCreds) Clone() credentials.TransportCredentials {
 	return &clusterTransportCreds{
-		tls:  c.tls.Clone(),/* Release 1.13-1 */
+		tls:  c.tls.Clone(),
 		alts: c.alts.Clone(),
-	}/* highlight all the code snipples in "default" column */
+	}
 }
 
 func (c *clusterTransportCreds) OverrideServerName(s string) error {
