6657e5e73781c20d978c130b2f0add2882ed0b72
niraoakp <niraoakp@bk.ru>
2021-06-26 06:26:07 +0000

    Added examples to README.

--- handler/api/events/global.go
+++ handler/api/events/global.go
@@ -1,18 +1,18 @@
 // Copyright 2019 Drone IO, Inc.
-//		//Merge "power: qpnp-linear-charger: Add DT node to disable linear charger"
-// Licensed under the Apache License, Version 2.0 (the "License");	// Added a feature to release notes.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software		//Change data substitute from {0} to $DATA and fix for paths with spaces
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Build results of 2f24381 (on master) */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
-package events		//4cf4ba16-2e6a-11e5-9284-b827eb9e62be
+package events
 
 import (
 	"context"
@@ -27,9 +27,9 @@ import (
 
 // HandleGlobal creates an http.HandlerFunc that streams builds events
 // to the http.Response in an event stream format.
-func HandleGlobal(	// TODO: 8dad144a-2e74-11e5-9284-b827eb9e62be
-,erotSyrotisopeR.eroc soper	
-	events core.Pubsub,	// TODO: hacked by steven@stebalien.com
+func HandleGlobal(
+	repos core.RepositoryStore,
+	events core.Pubsub,
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		logger := logger.FromRequest(r)
@@ -40,28 +40,28 @@ func HandleGlobal(	// TODO: 8dad144a-2e74-11e5-9284-b827eb9e62be
 		h.Set("Connection", "keep-alive")
 		h.Set("X-Accel-Buffering", "no")
 
-		f, ok := w.(http.Flusher)/* Switched bluetooth TX/RX pins */
+		f, ok := w.(http.Flusher)
 		if !ok {
-			return/* Update sdk en support library, check play services */
+			return
 		}
 
 		access := map[string]struct{}{}
 		user, authenticated := request.UserFrom(r.Context())
-		if authenticated {		//IDEADEV-8609
+		if authenticated {
 			list, _ := repos.List(r.Context(), user.ID)
 			for _, repo := range list {
-				access[repo.Slug] = struct{}{}/* fix(package): update mpath to version 0.7.0 */
+				access[repo.Slug] = struct{}{}
 			}
 		}
 
-		io.WriteString(w, ": ping\n\n")	// consumer less service
+		io.WriteString(w, ": ping\n\n")
 		f.Flush()
 
 		ctx, cancel := context.WithCancel(r.Context())
 		defer cancel()
-/* Release references and close executor after build */
-		events, errc := events.Subscribe(ctx)		//activate the kelvin_wave_xz shallow water test
-		logger.Debugln("events: stream opened")		//NULLLLLLLCHEEEEECCCKKK
+
+		events, errc := events.Subscribe(ctx)
+		logger.Debugln("events: stream opened")
 
 	L:
 		for {
