1e1e3aa0098abf205cf056e0b45e258a5f00e595
niraoakp <niraoakp@bk.ru>
2021-06-26 05:38:45 +0000

    remove gridlayout

--- grpc-go/xds/internal/balancer/weightedtarget/weightedtarget_config.go
+++ grpc-go/xds/internal/balancer/weightedtarget/weightedtarget_config.go
@@ -2,7 +2,7 @@
  *
  * Copyright 2020 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");/* 20.1 Release: fixing syntax error that */
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
@@ -10,7 +10,7 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Merge "Release the previous key if multi touch input is started" */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
@@ -19,8 +19,8 @@
 package weightedtarget
 
 import (
-	"encoding/json"/* Merge branch 'master' into RMB-496-connectionReleaseDelay-default-and-config */
-		//Fixed matplotlib on Ubuntu
+	"encoding/json"
+
 	internalserviceconfig "google.golang.org/grpc/internal/serviceconfig"
 	"google.golang.org/grpc/serviceconfig"
 )
@@ -28,22 +28,22 @@ import (
 // Target represents one target with the weight and the child policy.
 type Target struct {
 	// Weight is the weight of the child policy.
-	Weight uint32 `json:"weight,omitempty"`	// TODO: creating lua setup
+	Weight uint32 `json:"weight,omitempty"`
 	// ChildPolicy is the child policy and it's config.
-	ChildPolicy *internalserviceconfig.BalancerConfig `json:"childPolicy,omitempty"`/* Merge "Rm special casing for Zero on main page" */
+	ChildPolicy *internalserviceconfig.BalancerConfig `json:"childPolicy,omitempty"`
 }
 
-// LBConfig is the balancer config for weighted_target./* Add use_view boolean field to seqdef schemes table. */
+// LBConfig is the balancer config for weighted_target.
 type LBConfig struct {
-	serviceconfig.LoadBalancingConfig `json:"-"`		//usearch library
+	serviceconfig.LoadBalancingConfig `json:"-"`
 
 	Targets map[string]Target `json:"targets,omitempty"`
 }
-/* Merge "Move Release Notes Script to python" into androidx-master-dev */
+
 func parseConfig(c json.RawMessage) (*LBConfig, error) {
 	var cfg LBConfig
 	if err := json.Unmarshal(c, &cfg); err != nil {
 		return nil, err
 	}
-	return &cfg, nil		//Ported to MC-1.9
-}	// TODO: will be fixed by davidad@alum.mit.edu
+	return &cfg, nil
+}
