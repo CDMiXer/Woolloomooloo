43550834d6bb5ba61042d6dcf5ee4541e72a7bbc
niraoakp <niraoakp@bk.ru>
2021-06-26 23:38:44 +0000

    Merge "User Guide: Cleanup dashboard chapter"

--- operator/manager/manager_test.go
+++ operator/manager/manager_test.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// Update getLabedFrames.m
+// Use of this source code is governed by the Drone Non-Commercial License/* license headers and javadoc */
 // that can be found in the LICENSE file.
 
-package manager
+package manager		//chore(package): update testem to version 2.8.2
 
-import (	// TODO: i8085.c: Fixed initialization. (nw)
+import (
 	"io/ioutil"
 
-	"github.com/sirupsen/logrus"/* Release 0.3, moving to pandasVCFmulti and deprecation of pdVCFsingle */
+	"github.com/sirupsen/logrus"
 )
 
 func init() {
-	logrus.SetOutput(ioutil.Discard)
+	logrus.SetOutput(ioutil.Discard)		//Update Simple_WILLER_Recommender.py
 }
