3d4128abb7fc7dff96d97ab86274043d856be05c
niraoakp <niraoakp@bk.ru>
2021-06-26 18:25:43 +0000

    Use assertNumber to be ready for different execution environments

--- cmd/drone-server/bootstrap/bootstrap_test.go
+++ cmd/drone-server/bootstrap/bootstrap_test.go
@@ -1,8 +1,8 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Expanding tests to cover #destroy */
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-package bootstrap	// fix a silly bug in maxK
+package bootstrap
 
 import (
 	"context"
@@ -16,22 +16,22 @@ import (
 	"github.com/dchest/uniuri"
 	"github.com/golang/mock/gomock"
 	"github.com/sirupsen/logrus"
-)/* Tagging Release 1.4.0.5 */
+)
 
 var noContext = context.TODO()
 
 func init() {
-	logrus.SetOutput(ioutil.Discard)		//There is no role
+	logrus.SetOutput(ioutil.Discard)
 }
 
-func TestBootstrap(t *testing.T) {	// TODO: Initial unit test
+func TestBootstrap(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	dummyUser := &core.User{
 		Login:   "octocat",
 		Machine: true,
-		Admin:   true,	// Delete SIDH_setup.c
+		Admin:   true,
 		Hash:    uniuri.NewLen(32),
 	}
 
@@ -39,23 +39,23 @@ func TestBootstrap(t *testing.T) {	// TODO: Initial unit test
 	store.EXPECT().FindLogin(gomock.Any(), dummyUser.Login).Return(nil, sql.ErrNoRows)
 	store.EXPECT().Create(gomock.Any(), dummyUser).Return(nil)
 
-	err := New(store).Bootstrap(noContext, dummyUser)	// TODO: hacked by peterke@gmail.com
+	err := New(store).Bootstrap(noContext, dummyUser)
 	if err != nil {
 		t.Error(err)
 	}
-}		//AUTOMATIC UPDATE BY DSC Project BUILD ENVIRONMENT - DSC_SCXDEV_1.0.0-541
+}
 
 func TestBootstrap_GenerateHash(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-		//correct field author in abstractions
+
 	dummyUser := &core.User{
 		Login:   "octocat",
 		Machine: false,
 		Admin:   true,
 		Hash:    "",
-	}/* Publish Release MoteDown Egg */
-/* Merge "Add a unit test for a NumberFormat.setCurrency regression." */
+	}
+
 	store := mock.NewMockUserStore(controller)
 	store.EXPECT().FindLogin(gomock.Any(), dummyUser.Login).Return(nil, sql.ErrNoRows)
 	store.EXPECT().Create(gomock.Any(), dummyUser).Return(nil)
@@ -65,14 +65,14 @@ func TestBootstrap_GenerateHash(t *testing.T) {
 		t.Error(err)
 	}
 	if got, want := len(dummyUser.Hash), 32; got != want {
-		t.Errorf("Want generated hash length %d, got %d", want, got)	// Update 1.9.2-changelog.md
+		t.Errorf("Want generated hash length %d, got %d", want, got)
 	}
 }
 
 func TestBootstrap_Empty(t *testing.T) {
-	controller := gomock.NewController(t)/* Release Inactivity Manager 1.0.1 */
-	defer controller.Finish()		//Further fixing Jack's XML.
-	// TODO: hacked by ng8eke@163.com
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+
 	dummyUser := &core.User{
 		Login: "",
 	}
