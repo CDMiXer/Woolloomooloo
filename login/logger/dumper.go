a46d2d88391b543d96d269965d63a49f0dd2dcc2
niraoakp <niraoakp@bk.ru>
2021-06-26 23:19:49 +0000

    Published 100/376 elements

--- login/logger/dumper.go
+++ login/logger/dumper.go
@@ -1,40 +1,40 @@
 // Copyright 2017 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by a BSD-style/* Merge "Add a prelude for ironic 12.0" */
+// Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
-/* Silence some warnings when compiling taskmgr with MSVC2005 */
+
 package logger
-/* Debconf fixes */
-import (/* Add translation notes and POEditor project link */
+
+import (
 	"net/http"
 	"net/http/httputil"
-	"os"		//Added Scythe to the AllItems list.
+	"os"
 )
 
-// Dumper dumps the http.Request and http.Response		//[ci skip] fixed typo
-// message payload for debugging purposes.		//Create CIN05CRIME
+// Dumper dumps the http.Request and http.Response
+// message payload for debugging purposes.
 type Dumper interface {
 	DumpRequest(*http.Request)
 	DumpResponse(*http.Response)
 }
 
 // DiscardDumper returns a no-op dumper.
-func DiscardDumper() Dumper {	// Alphabetically ordered integrations
-	return new(discardDumper)	// TODO: Create SWVM.md
+func DiscardDumper() Dumper {
+	return new(discardDumper)
 }
-/* Let's allows to users toggle zodiacal light through GUI */
+
 type discardDumper struct{}
 
-func (*discardDumper) DumpRequest(*http.Request)   {}/* First upload of all files */
-func (*discardDumper) DumpResponse(*http.Response) {}		//Allow to create things
-	// More work on the task system
-// StandardDumper returns a standard dumper./* Update weatherController.js */
+func (*discardDumper) DumpRequest(*http.Request)   {}
+func (*discardDumper) DumpResponse(*http.Response) {}
+
+// StandardDumper returns a standard dumper.
 func StandardDumper() Dumper {
 	return new(standardDumper)
 }
-		//Benchmark Data - 1475848827430
+
 type standardDumper struct{}
 
-func (*standardDumper) DumpRequest(req *http.Request) {		//07be42a8-2e6c-11e5-9284-b827eb9e62be
+func (*standardDumper) DumpRequest(req *http.Request) {
 	dump, _ := httputil.DumpRequestOut(req, true)
 	os.Stdout.Write(dump)
 }
