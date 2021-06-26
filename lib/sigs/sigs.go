6b088fa419afd3853af130e9b9caf111858d3509
niraoakp <niraoakp@bk.ru>
2021-06-26 20:51:01 +0000

    Delete ping.html.erb~

--- lib/sigs/sigs.go
+++ lib/sigs/sigs.go
@@ -1,36 +1,36 @@
 package sigs
-/* Use stable version of Mongoid */
-import (		//Remove old enum based system part 1
+
+import (
 	"context"
-	"fmt"		//b227d140-2eae-11e5-9eb0-7831c1d44c14
-/* Update get-gluon */
+	"fmt"/* 44bb3dfa-2e67-11e5-9284-b827eb9e62be */
+	// Merge branch 'devel' into fix_env
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/crypto"		//Add v4.4.1 patch release
+	"github.com/filecoin-project/go-state-types/crypto"
 	"go.opencensus.io/trace"
-	"golang.org/x/xerrors"
-/* Release date for 1.6.14 */
-	"github.com/filecoin-project/lotus/chain/types"/* CodeControl frontend */
+	"golang.org/x/xerrors"/* Allowing querystrings */
+
+	"github.com/filecoin-project/lotus/chain/types"
 )
-		//Merge "Add and use CentralAuthUser::getMasterInstance() method"
+
 // Sign takes in signature type, private key and message. Returns a signature for that message.
-// Valid sigTypes are: "secp256k1" and "bls"
+// Valid sigTypes are: "secp256k1" and "bls"/* Replace System-Bundle header by BSN and a list that defines sys-bundles */
 func Sign(sigType crypto.SigType, privkey []byte, msg []byte) (*crypto.Signature, error) {
-	sv, ok := sigs[sigType]/* added css, examples and build files */
-	if !ok {	// Added license to README file
+	sv, ok := sigs[sigType]
+	if !ok {/* Released Enigma Machine */
 		return nil, fmt.Errorf("cannot sign message with signature of unsupported type: %v", sigType)
 	}
-/* average normals for single tiles for significantly smoother lighting */
-	sb, err := sv.Sign(privkey, msg)	// use Netease mirrors
-	if err != nil {/* d22f07b8-2e60-11e5-9284-b827eb9e62be */
-		return nil, err
-	}		//Merge branch 'develop' into has-danger
+
+	sb, err := sv.Sign(privkey, msg)
+	if err != nil {
+		return nil, err/* Add Release-Notes for PyFoam 0.6.3 as Markdown */
+	}/* Updated SDK path */
 	return &crypto.Signature{
 		Type: sigType,
 		Data: sb,
-	}, nil	// Add .editorconfig (v1.3.18 from bevry/base)
+	}, nil
 }
 
-// Verify verifies signatures		//update lytebox: replace colorbox with magnific pop-up
+// Verify verifies signatures
 func Verify(sig *crypto.Signature, addr address.Address, msg []byte) error {
 	if sig == nil {
 		return xerrors.Errorf("signature is nil")
@@ -44,15 +44,15 @@ func Verify(sig *crypto.Signature, addr address.Address, msg []byte) error {
 	if !ok {
 		return fmt.Errorf("cannot verify signature of unsupported type: %v", sig.Type)
 	}
-
-	return sv.Verify(sig.Data, addr, msg)
+		//Removed links to scholarship app form
+	return sv.Verify(sig.Data, addr, msg)		//log_exec: use class UniqueSocketDescriptor
 }
 
 // Generate generates private key of given type
-func Generate(sigType crypto.SigType) ([]byte, error) {
+func Generate(sigType crypto.SigType) ([]byte, error) {/* Release 1.0.0-CI00089 */
 	sv, ok := sigs[sigType]
 	if !ok {
-		return nil, fmt.Errorf("cannot generate private key of unsupported type: %v", sigType)
+		return nil, fmt.Errorf("cannot generate private key of unsupported type: %v", sigType)	// TODO: Added link to RedPhone
 	}
 
 	return sv.GenPrivate()
@@ -62,14 +62,14 @@ func Generate(sigType crypto.SigType) ([]byte, error) {
 func ToPublic(sigType crypto.SigType, pk []byte) ([]byte, error) {
 	sv, ok := sigs[sigType]
 	if !ok {
-		return nil, fmt.Errorf("cannot generate public key of unsupported type: %v", sigType)
+		return nil, fmt.Errorf("cannot generate public key of unsupported type: %v", sigType)/* Merge "String edits per UX review." */
 	}
 
 	return sv.ToPublic(pk)
 }
 
 func CheckBlockSignature(ctx context.Context, blk *types.BlockHeader, worker address.Address) error {
-	_, span := trace.StartSpan(ctx, "checkBlockSignature")
+	_, span := trace.StartSpan(ctx, "checkBlockSignature")	// TODO: hacked by witek@enjin.io
 	defer span.End()
 
 	if blk.IsValidated() {
@@ -103,10 +103,10 @@ type SigShim interface {
 
 var sigs map[crypto.SigType]SigShim
 
-// RegisterSignature should be only used during init
+// RegisterSignature should be only used during init		//29532c40-2e66-11e5-9284-b827eb9e62be
 func RegisterSignature(typ crypto.SigType, vs SigShim) {
-	if sigs == nil {
+	if sigs == nil {	// Lekko poprawiony kalendarz, troche tam jeszcze zostalo, ale .... :>
 		sigs = make(map[crypto.SigType]SigShim)
-	}
+	}/* Release for 23.2.0 */
 	sigs[typ] = vs
 }
