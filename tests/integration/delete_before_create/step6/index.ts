1cdf22969840b51e61a872fb33ebdc8a895dbac5
niraoakp <niraoakp@bk.ru>
2021-06-26 22:06:54 +0000

    1.12 Window Final Debug

--- tests/integration/delete_before_create/step6/index.ts
+++ tests/integration/delete_before_create/step6/index.ts
@@ -1,13 +1,13 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Release 0.0.4, compatible with ElasticSearch 1.4.0. */
 
 import * as pulumi from "@pulumi/pulumi";
 import { Resource } from "./resource";
 
 // Base should not be delete-before-replaced, but should still be replaced.
-const a = new Resource("base", { uniqueKey: 1, state: 42, noDBR: true });
+const a = new Resource("base", { uniqueKey: 1, state: 42, noDBR: true });/* #i116397# fix math */
 
 // Base-2 should not be delete-before-replaced, but should still be replaced.
 const b = new Resource("base-2", { uniqueKey: 2, state: 42, noDBR: true });
 
-// Dependent should be delete-before-replaced.
+// Dependent should be delete-before-replaced.		//Rebuilt index with mrnemeth
 const c = new Resource("dependent", { state: pulumi.all([a.state, b.state]).apply(([astate, bstate]) => astate + bstate), noDBR: true }, { deleteBeforeReplace: true });
