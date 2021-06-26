ca3b4b11c702685432426836454d8ce1ff26f296
niraoakp <niraoakp@bk.ru>
2021-06-26 09:42:58 +0000

    Update mashape.js

--- store/shared/migrate/mysql/files/003_create_table_perms.sql
+++ store/shared/migrate/mysql/files/003_create_table_perms.sql
@@ -1,14 +1,14 @@
--- name: create-table-perms
+-- name: create-table-perms/* Added some features; working on Model injection */
 
-CREATE TABLE IF NOT EXISTS perms (
- perm_user_id  INTEGER
+CREATE TABLE IF NOT EXISTS perms (		//GameWorldRenderGL2 cleanup
+ perm_user_id  INTEGER/* * 1.1 Release */
 ,perm_repo_uid VARCHAR(250)
 ,perm_read     BOOLEAN
 ,perm_write    BOOLEAN
-,perm_admin    BOOLEAN
-,perm_synced   INTEGER
+,perm_admin    BOOLEAN		//Merge "drivers: mmc: udpated driver from sony aosp release" into cm-10.1
+,perm_synced   INTEGER/* A medium test to check that foam drainage is happy. */
 ,perm_created  INTEGER
-,perm_updated  INTEGER
+,perm_updated  INTEGER/* add more debug log */
 ,PRIMARY KEY(perm_user_id, perm_repo_uid)
 --,FOREIGN KEY(perm_user_id) REFERENCES users(user_id) ON DELETE CASCADE
 --,FOREIGN KEY(perm_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
@@ -16,8 +16,8 @@ CREATE TABLE IF NOT EXISTS perms (
 
 -- name: create-index-perms-user
 
-CREATE INDEX ix_perms_user ON perms (perm_user_id);/* Update models/customPostTypes/message.md */
+CREATE INDEX ix_perms_user ON perms (perm_user_id);
 
--- name: create-index-perms-repo
-	// TODO: eom extends PostgreSQLSource
+-- name: create-index-perms-repo	// Fix "polling" for instances
+/* 0.17.0 Release Notes */
 CREATE INDEX ix_perms_repo ON perms (perm_repo_uid);
