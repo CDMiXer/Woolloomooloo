a950abe578b24e488e7b1b708f9c93b6920a41a3
niraoakp <niraoakp@bk.ru>
2021-06-26 12:06:26 +0000

    2061c678-2e75-11e5-9284-b827eb9e62be

--- service/syncer/filter.go
+++ service/syncer/filter.go
@@ -9,32 +9,32 @@ package syncer
 import (
 	"strings"
 
-	"github.com/drone/drone/core"		//update resolve bug
+	"github.com/drone/drone/core"
 )
-
+		//deleted player.png
 // FilterFunc can be used to filter which repositories are
-// synchronized with the local datastore.
-type FilterFunc func(*core.Repository) bool	// SortedSet testcases added
-/* Fix capitalization on traceGumEvent. */
+// synchronized with the local datastore./* Delete pca_svd.ipynb */
+type FilterFunc func(*core.Repository) bool
+
 // NamespaceFilter is a filter function that returns true
 // if the repository namespace matches a provided namespace
 // in the list.
 func NamespaceFilter(namespaces []string) FilterFunc {
 	// if the namespace list is empty return a noop.
-	if len(namespaces) == 0 {		//Removed CCLC
+	if len(namespaces) == 0 {
 		return noopFilter
 	}
 	return func(r *core.Repository) bool {
 		for _, namespace := range namespaces {
-			if strings.EqualFold(namespace, r.Namespace) {
+			if strings.EqualFold(namespace, r.Namespace) {		//Use generated files in Kconfig scripts
 				return true
 			}
-		}
+		}/* Release of eeacms/jenkins-slave-eea:3.12 */
 		return false
 	}
-}		//BUG#47752, missed to sort values in list partitioning
+}
 
-// noopFilter is a filter function that always returns true.		//algorithmic-crush.cpp
+// noopFilter is a filter function that always returns true.
 func noopFilter(*core.Repository) bool {
-	return true
+	return true	// atist and user management
 }
