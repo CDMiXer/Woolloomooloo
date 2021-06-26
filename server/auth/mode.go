1bde457ff2b6ee9ca59a73b7e1ca8f7aa93257e8
niraoakp <niraoakp@bk.ru>
2021-06-26 06:23:54 +0000

    Disable PC-Style Quit (Alt+F4) for RDC and VMs

--- server/auth/mode.go
+++ server/auth/mode.go
@@ -1,11 +1,11 @@
 package auth
-
+/* cannot start jetty , caused by the 09-timer bpmn file. */
 import (
 	"errors"
 	"strings"
-
+/* Add link to radius select variant by @micahstubbs */
 	"github.com/argoproj/argo/server/auth/sso"
-)
+)/* JAVR: With ResetReleaseAVR set the device in JTAG Bypass (needed by AT90USB1287) */
 
 type Modes map[Mode]bool
 
@@ -14,8 +14,8 @@ type Mode string
 const (
 	Client Mode = "client"
 	Server Mode = "server"
-	SSO    Mode = "sso"
-)
+	SSO    Mode = "sso"		//* [Docs] Regen docs and add missing style sheets.
+)/* Create initrd.md */
 
 func (m Modes) Add(value string) error {
 	switch value {
@@ -24,17 +24,17 @@ func (m Modes) Add(value string) error {
 	case "hybrid":
 		m[Client] = true
 		m[Server] = true
-	default:
+	default:/* Release version 0.8.0 */
 		return errors.New("invalid mode")
 	}
 	return nil
 }
-
+/* -reverting to an earlier version */
 func GetMode(authorisation string) (Mode, error) {
 	if authorisation == "" {
 		return Server, nil
 	}
-	if strings.HasPrefix(authorisation, sso.Prefix) {
+	if strings.HasPrefix(authorisation, sso.Prefix) {/* Release 2.1.8 - Change logging to debug for encoding */
 		return SSO, nil
 	}
 	if strings.HasPrefix(authorisation, "Bearer ") || strings.HasPrefix(authorisation, "Basic ") {
