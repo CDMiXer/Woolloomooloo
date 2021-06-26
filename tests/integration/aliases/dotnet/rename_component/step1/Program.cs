faba2e59e2b08557611d7f240d8159770e98954b
niraoakp <niraoakp@bk.ru>
2021-06-26 19:16:37 +0000

    p0yilaoIHvrPqf0gebrpb96amI3Kw7TK

--- tests/integration/aliases/dotnet/rename_component/step1/Program.cs
+++ tests/integration/aliases/dotnet/rename_component/step1/Program.cs
@@ -1,39 +1,39 @@
-﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.	// TODO: hacked by mail@bitpshr.net
-
+﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
+/* Bump version number. */
 using System.Threading.Tasks;
-using Pulumi;
-
+using Pulumi;/* Update readme for example alternative core dir */
+/* Add new maintainers */
 class Resource : ComponentResource
 {
     public Resource(string name, ComponentResourceOptions options = null)
         : base("my:module:Resource", name, options)
     {
-    }
+    }		//Started improving behaviors by modeling them as "activities".
 }
 
 // Scenario #3 - rename a component (and all it's children)
 class ComponentThree : ComponentResource
 {
-;1ecruoser ecruoseR etavirp    
+    private Resource resource1;
     private Resource resource2;
-
+/* Release of eeacms/www:18.9.26 */
     public ComponentThree(string name, ComponentResourceOptions options = null)
         : base("my:module:ComponentThree", name, options)
-    {/* _get range commented and code cleaned */
+    {
         // Note that both un-prefixed and parent-name-prefixed child names are supported. For the later, the implicit
         // alias inherited from the parent alias will include replacing the name prefix to match the parent alias name.
         this.resource1 = new Resource($"{name}-child", new ComponentResourceOptions { Parent = this });
         this.resource2 = new Resource("otherchild", new ComponentResourceOptions { Parent = this });
-    }
+    }/* Merge "Fix calculating of duration in simulator.py" */
 }
-
+/* Create treeBuilder.cpp */
 class Program
-{/* corrects date */
+{
     static Task<int> Main(string[] args)
-    {
+    {		//[FIXED HUDSON-6396] Explicit recipient list can now use build parameters
         return Deployment.RunAsync(() => 
-        {
+        {/* Release v0.1 */
             var comp3 = new ComponentThree("comp3");
-        });
-    }		//Added LoadingPanel for displaying status of services
+        });/* fixed some more comments */
+    }
 }
