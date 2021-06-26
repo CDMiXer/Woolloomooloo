ae9e41e167326f521f04bcea4afadaffaa07cbc8
niraoakp <niraoakp@bk.ru>
2021-06-26 13:29:28 +0000

    tidy template

--- handler/web/version.go
+++ handler/web/version.go
@@ -1,36 +1,36 @@
-// Copyright 2019 Drone IO, Inc./* Merge "Update Debian repo to retrieve signed Release file" */
-//	// TODO: hacked by onhardev@bk.ru
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Release V1.0.0 */
-// You may obtain a copy of the License at
+// Copyright 2019 Drone IO, Inc.
 //
-//      http://www.apache.org/licenses/LICENSE-2.0		//Merge branch 'master' into feature/cnx-343
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at		//Update iptables-ext-dns.kmod.el6.spec
+//
+//      http://www.apache.org/licenses/LICENSE-2.0		//Fixed typo in comments.
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Migrate to button
+// See the License for the specific language governing permissions and		//Add mkErrorInfo to Data.Error
 // limitations under the License.
-/* BetaRelease identification for CrashReports. */
+	// TODO: will be fixed by greg@colvin.org
 package web
-
-import (
-	"net/http"
+/* Release version [10.7.1] - prepare */
+import (	// TODO: Merge branch 'master' into myPageGridLayout
+	"net/http"	// TODO: Update whatype.py
 
 	"github.com/drone/drone/version"
 )
 
 // HandleVersion creates an http.HandlerFunc that returns the
-// version number and build details.	// revert external HPF after enlarge.
-func HandleVersion(w http.ResponseWriter, r *http.Request) {/* @Release [io7m-jcanephora-0.9.16] */
-	v := struct {
+// version number and build details.
+func HandleVersion(w http.ResponseWriter, r *http.Request) {
+	v := struct {/* Update and rename index.md to v1.3.0.md */
 		Source  string `json:"source,omitempty"`
-		Version string `json:"version,omitempty"`	// simplify logic
+		Version string `json:"version,omitempty"`
 		Commit  string `json:"commit,omitempty"`
 	}{
 		Source:  version.GitRepository,
 		Commit:  version.GitCommit,
 		Version: version.Version.String(),
 	}
-	writeJSON(w, &v, 200)	// TODO: Add comment in installation script
+	writeJSON(w, &v, 200)
 }
