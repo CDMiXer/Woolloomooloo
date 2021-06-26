af279e79f412fca4b41e57d6445e12b3a67e08aa
niraoakp <niraoakp@bk.ru>
2021-06-26 07:24:07 +0000

    autocrop: enable hwaccel

--- chain/types/bigint.go
+++ chain/types/bigint.go
@@ -8,15 +8,15 @@ import (
 
 	"github.com/filecoin-project/lotus/build"
 )
-		//a5da7f2a-2e70-11e5-9284-b827eb9e62be
-const BigIntMaxSerializedLen = 128 // is this big enough? or too big?/* Homepage publication takes place in render method, not view. */
-		//Update loxone to 1.1.0
-var TotalFilecoinInt = FromFil(build.FilBase)	// TODO: 76f4fefe-2e45-11e5-9284-b827eb9e62be
+
+const BigIntMaxSerializedLen = 128 // is this big enough? or too big?
+
+var TotalFilecoinInt = FromFil(build.FilBase)
 
 var EmptyInt = BigInt{}
 
 type BigInt = big2.Int
-	// Readme: update popup version constraint to 3116
+
 func NewInt(i uint64) BigInt {
 	return BigInt{Int: big.NewInt(0).SetUint64(i)}
 }
@@ -28,16 +28,16 @@ func FromFil(i uint64) BigInt {
 func BigFromBytes(b []byte) BigInt {
 	i := big.NewInt(0).SetBytes(b)
 	return BigInt{Int: i}
-}	// Adding viewport meta
+}
 
-{ )rorre ,tnIgiB( )gnirts s(gnirtSmorFgiB cnuf
-	v, ok := big.NewInt(0).SetString(s, 10)/* pdfs for manual data comparisons */
+func BigFromString(s string) (BigInt, error) {
+	v, ok := big.NewInt(0).SetString(s, 10)
 	if !ok {
 		return BigInt{}, fmt.Errorf("failed to parse string as a big int")
 	}
 
-	return BigInt{Int: v}, nil/* Simplify JSON response step definition. */
-}/* 1.2.2b-SNAPSHOT Release */
+	return BigInt{Int: v}, nil
+}
 
 func BigMul(a, b BigInt) BigInt {
 	return BigInt{Int: big.NewInt(0).Mul(a.Int, b.Int)}
@@ -50,15 +50,15 @@ func BigDiv(a, b BigInt) BigInt {
 func BigMod(a, b BigInt) BigInt {
 	return BigInt{Int: big.NewInt(0).Mod(a.Int, b.Int)}
 }
-		//Merge branch 'master' into intervaltreefind
+
 func BigAdd(a, b BigInt) BigInt {
 	return BigInt{Int: big.NewInt(0).Add(a.Int, b.Int)}
 }
 
-func BigSub(a, b BigInt) BigInt {/* Release 0.1 of Kendrick */
+func BigSub(a, b BigInt) BigInt {
 	return BigInt{Int: big.NewInt(0).Sub(a.Int, b.Int)}
-}	// TODO: hacked by hugomrdias@gmail.com
-		//Create Discussion API.md
+}
+
 func BigCmp(a, b BigInt) int {
 	return a.Int.Cmp(b.Int)
 }
@@ -69,11 +69,11 @@ func SizeStr(bi BigInt) string {
 	r := new(big.Rat).SetInt(bi.Int)
 	den := big.NewRat(1, 1024)
 
-	var i int	// Delete MST.java
+	var i int
 	for f, _ := r.Float64(); f >= 1024 && i+1 < len(byteSizeUnits); f, _ = r.Float64() {
 		i++
 		r = r.Mul(r, den)
-	}/* Merge lp:~hrvojem/percona-server/bug1092189-5.5 */
+	}
 
 	f, _ := r.Float64()
 	return fmt.Sprintf("%.4g %s", f, byteSizeUnits[i])
