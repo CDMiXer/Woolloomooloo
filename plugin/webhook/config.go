c364eaa96d17e1fcbb07f41f312da273a0613843
niraoakp <niraoakp@bk.ru>
2021-06-27 08:30:54 +0000

    Merge branch 'master' into fix_win_handlers

--- plugin/webhook/config.go
+++ plugin/webhook/config.go
@@ -1,18 +1,18 @@
-// Copyright 2019 Drone IO, Inc.
-//	// TODO: will be fixed by steven@stebalien.com
-// Licensed under the Apache License, Version 2.0 (the "License");		//Same sex marriage changes to Germany.
-// you may not use this file except in compliance with the License.	// Merge "Replace loop by __builtin_ctz"
-// You may obtain a copy of the License at
-///* http_cache: pass references instead of pointers */
+// Copyright 2019 Drone IO, Inc.	// TODO: hacked by martin2cai@hotmail.com
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.	// TODO: I made a stupid mistake.
+// You may obtain a copy of the License at/* Release new version 2.5.9: Turn on new webRequest code for all Chrome 17 users */
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Display reviews for staff on Release page */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Change S. Lee St from Local to Major Collector */
-// See the License for the specific language governing permissions and
-// limitations under the License.	// jenkins pipeline & shared libraries tips
-		//38b1e978-2e53-11e5-9284-b827eb9e62be
-package webhook
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and	// definition of pp routine: EquidistanceProvider
+// limitations under the License.
+
+package webhook	// TODO: adapted desc to current implementation
 
 import "github.com/drone/drone/core"
 
@@ -20,6 +20,6 @@ import "github.com/drone/drone/core"
 type Config struct {
 	Events   []string
 	Endpoint []string
-	Secret   string		//Update arch_timer.h
-	System   *core.System/* [artifactory-release] Release version 0.8.1.RELEASE */
+	Secret   string/* create main_css */
+	System   *core.System
 }
