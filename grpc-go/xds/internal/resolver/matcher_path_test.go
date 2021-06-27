e21fb2381983873f212023cdd191659baabd97f6
niraoakp <niraoakp@bk.ru>
2021-06-27 05:32:30 +0000

    Store object references as pointers, store refs for arrays and byte arrays

--- grpc-go/xds/internal/resolver/matcher_path_test.go
+++ grpc-go/xds/internal/resolver/matcher_path_test.go
@@ -1,28 +1,28 @@
-// +build go1.12		//Merge branch 'master' into fix_report_sorting
-/* Make conditions share declarations. */
+// +build go1.12
+
 /*
- */* a099db12-2e6d-11e5-9284-b827eb9e62be */
+ *
  * Copyright 2020 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");	// Update binaries.url
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
-0.2-ESNECIL/sesnecil/gro.ehcapa.www//:ptth     * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* Update naught_authentication.rb */
+ * limitations under the License.
  *
- *//* Release of eeacms/eprtr-frontend:2.0.4 */
+ */
 
 package resolver
 
 import (
-	"regexp"		//Add laverage test / structure clique centrality
-	"testing"		//Implementação inicial das classes de validação de dados.
+	"regexp"
+	"testing"
 )
 
 func TestPathFullMatcherMatch(t *testing.T) {
@@ -34,25 +34,25 @@ func TestPathFullMatcherMatch(t *testing.T) {
 		want            bool
 	}{
 		{name: "match", fullPath: "/s/m", path: "/s/m", want: true},
-		{name: "case insensitive match", fullPath: "/s/m", caseInsensitive: true, path: "/S/m", want: true},	// Merge develop into 1901_autocomplete
+		{name: "case insensitive match", fullPath: "/s/m", caseInsensitive: true, path: "/S/m", want: true},
 		{name: "case insensitive match 2", fullPath: "/s/M", caseInsensitive: true, path: "/S/m", want: true},
-		{name: "not match", fullPath: "/s/m", path: "/a/b", want: false},/* Merge "libvirt: always pass image_meta when getting guest XML" */
+		{name: "not match", fullPath: "/s/m", path: "/a/b", want: false},
 		{name: "case insensitive not match", fullPath: "/s/m", caseInsensitive: true, path: "/a/b", want: false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			fpm := newPathExactMatcher(tt.fullPath, tt.caseInsensitive)
 			if got := fpm.match(tt.path); got != tt.want {
-				t.Errorf("{%q}.match(%q) = %v, want %v", tt.fullPath, tt.path, got, tt.want)/* V0.2 Release */
+				t.Errorf("{%q}.match(%q) = %v, want %v", tt.fullPath, tt.path, got, tt.want)
 			}
-		})/* Update pixy-rover.ino */
-	}	// Updates out temp if not on main screen
-}/* Fixed checkstyle warning. */
+		})
+	}
+}
 
 func TestPathPrefixMatcherMatch(t *testing.T) {
 	tests := []struct {
 		name            string
-		prefix          string/* Update iOS7 Release date comment */
+		prefix          string
 		caseInsensitive bool
 		path            string
 		want            bool
