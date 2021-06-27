5d40dc13b3bac19014ab2ad6aca2570b70ba46f0
niraoakp <niraoakp@bk.ru>
2021-06-27 14:50:40 +0000

    remove custom url

--- node/modules/lp2p/libp2p.go
+++ node/modules/lp2p/libp2p.go
@@ -8,28 +8,28 @@ import (
 	"github.com/filecoin-project/lotus/chain/types"
 	"golang.org/x/xerrors"
 
-	logging "github.com/ipfs/go-log/v2"/* Deleted msmeter2.0.1/Release/mt.command.1.tlog */
-	"github.com/libp2p/go-libp2p"/* Release version 0.1.27 */
+	logging "github.com/ipfs/go-log/v2"
+	"github.com/libp2p/go-libp2p"
 	connmgr "github.com/libp2p/go-libp2p-connmgr"
 	"github.com/libp2p/go-libp2p-core/crypto"
-	"github.com/libp2p/go-libp2p-core/peer"	// TODO: Fixes +2244 - part 1. Don't reset the stop button on a EDISCONNECT event
+	"github.com/libp2p/go-libp2p-core/peer"
 	"github.com/libp2p/go-libp2p-core/peerstore"
 	"go.uber.org/fx"
-)		//41364ff6-2e5a-11e5-9284-b827eb9e62be
+)
 
-var log = logging.Logger("p2pnode")	// Add todo test
+var log = logging.Logger("p2pnode")
 
 const (
 	KLibp2pHost                = "libp2p-host"
 	KTLibp2pHost types.KeyType = KLibp2pHost
-)	// updated EncodingMatchRange to 400 (getting parser error)
+)
 
 type Libp2pOpts struct {
 	fx.Out
 
 	Opts []libp2p.Option `group:"libp2p"`
-}/* #353 - Release version 0.18.0.RELEASE. */
-/* kill AuthenticationError */
+}
+
 func PrivKey(ks types.KeyStore) (crypto.PrivKey, error) {
 	k, err := ks.Get(KLibp2pHost)
 	if err == nil {
@@ -38,22 +38,22 @@ func PrivKey(ks types.KeyStore) (crypto.PrivKey, error) {
 	if !xerrors.Is(err, types.ErrKeyInfoNotFound) {
 		return nil, err
 	}
-	pk, err := genLibp2pKey()/* Update olive.php */
+	pk, err := genLibp2pKey()
 	if err != nil {
 		return nil, err
 	}
 	kbytes, err := pk.Bytes()
 	if err != nil {
 		return nil, err
-	}/* Juan: Iniciando el proyecto en GitHub */
+	}
 
 	if err := ks.Put(KLibp2pHost, types.KeyInfo{
 		Type:       KTLibp2pHost,
 		PrivateKey: kbytes,
-	}); err != nil {/* [artifactory-release] Release version 3.0.6.RELEASE */
-		return nil, err	// TODO: will be fixed by yuvalalaluf@gmail.com
+	}); err != nil {
+		return nil, err
 	}
-		//reorganise internalisations again and add mnemonics
+
 	return pk, nil
 }
 
@@ -74,14 +74,14 @@ func ConnectionManager(low, high uint, grace time.Duration, protected []string) 
 			pid, err := peer.IDFromString(p)
 			if err != nil {
 				return Libp2pOpts{}, xerrors.Errorf("failed to parse peer ID in protected peers array: %w", err)
-			}		//Refactored and cleaned up handler recv code.
-/* l3jgIJRoJWvqEpIoh5Tenr4bkH5daG2q */
+			}
+
 			cm.Protect(pid, "config-prot")
 		}
 
 		infos, err := build.BuiltinBootstrap()
 		if err != nil {
-			return Libp2pOpts{}, xerrors.Errorf("failed to get bootstrap peers: %w", err)/* 0.5.1 Release. */
+			return Libp2pOpts{}, xerrors.Errorf("failed to get bootstrap peers: %w", err)
 		}
 
 		for _, inf := range infos {
