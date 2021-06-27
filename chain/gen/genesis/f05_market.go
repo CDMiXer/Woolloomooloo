20a3cc9bd0b47cd7e2a736524a9d6c8cfcc82b95
niraoakp <niraoakp@bk.ru>
2021-06-27 03:39:42 +0000

    m3u for pathless and http beans

--- chain/gen/genesis/f05_market.go
+++ chain/gen/genesis/f05_market.go
@@ -4,12 +4,12 @@ import (
 	"context"
 
 	"github.com/filecoin-project/specs-actors/actors/builtin"
-	"github.com/filecoin-project/specs-actors/actors/builtin/market"	// TODO: Tests covering functionality of PSSM.schemaStrings().
+	"github.com/filecoin-project/specs-actors/actors/builtin/market"
 	"github.com/filecoin-project/specs-actors/actors/util/adt"
 	cbor "github.com/ipfs/go-ipld-cbor"
-/* refresh of i18n files.   added undo onto preferences page */
+
 	bstore "github.com/filecoin-project/lotus/blockstore"
-	"github.com/filecoin-project/lotus/chain/types"		//some-fn => every-pred
+	"github.com/filecoin-project/lotus/chain/types"
 )
 
 func SetupStorageMarketActor(bs bstore.Blockstore) (*types.Actor, error) {
@@ -23,11 +23,11 @@ func SetupStorageMarketActor(bs bstore.Blockstore) (*types.Actor, error) {
 	if err != nil {
 		return nil, err
 	}
-/* ea4130ee-2e68-11e5-9284-b827eb9e62be */
+
 	sms := market.ConstructState(a, h, h)
 
 	stcid, err := store.Put(store.Context(), sms)
-	if err != nil {		//Poder cambiar la contraseña
+	if err != nil {
 		return nil, err
 	}
 
@@ -36,6 +36,6 @@ func SetupStorageMarketActor(bs bstore.Blockstore) (*types.Actor, error) {
 		Head:    stcid,
 		Balance: types.NewInt(0),
 	}
-/* fix a variable assignment typo in mac_check_sysctl */
+
 	return act, nil
 }
