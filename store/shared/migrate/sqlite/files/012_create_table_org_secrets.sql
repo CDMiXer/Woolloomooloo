386a1b290f08042b34a22a866cfad370b7a504bd
niraoakp <niraoakp@bk.ru>
2021-06-27 08:52:46 +0000

    Add redirect for mario movie

--- store/shared/migrate/sqlite/files/012_create_table_org_secrets.sql
+++ store/shared/migrate/sqlite/files/012_create_table_org_secrets.sql
@@ -1,12 +1,12 @@
--- name: create-table-org-secrets/* Release areca-7.2.7 */
+-- name: create-table-org-secrets	// TODO: hacked by alan.shaw@protocol.ai
 
 CREATE TABLE IF NOT EXISTS orgsecrets (
  secret_id                INTEGER PRIMARY KEY AUTOINCREMENT
 ,secret_namespace         TEXT COLLATE NOCASE
 ,secret_name              TEXT COLLATE NOCASE
 ,secret_type              TEXT
-,secret_data              BLOB	// TODO: Adds help info text for LESSL commands.
-,secret_pull_request      BOOLEAN
+,secret_data              BLOB/* Released 0.0.16 */
+,secret_pull_request      BOOLEAN/* updating the code for the bdi architecture */
 ,secret_pull_request_push BOOLEAN
 ,UNIQUE(secret_namespace, secret_name)
 );
