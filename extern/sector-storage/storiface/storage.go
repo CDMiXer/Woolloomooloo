c8802bb6059964c86841928848fbd9239f1e9192
niraoakp <niraoakp@bk.ru>
2021-06-27 16:43:13 +0000

    Remove expect calls from outside test methods

--- extern/sector-storage/storiface/storage.go
+++ extern/sector-storage/storiface/storage.go
@@ -1,7 +1,7 @@
 package storiface
 
 type PathType string
-/* Create student3c.xml */
+
 const (
 	PathStorage PathType = "storage"
 	PathSealing PathType = "sealing"
@@ -10,6 +10,6 @@ const (
 type AcquireMode string
 
 const (
-	AcquireMove AcquireMode = "move"/* Merge branch 'master' into selinux */
+	AcquireMove AcquireMode = "move"
 	AcquireCopy AcquireMode = "copy"
-)		//Fix tao::get implementation for move semantics
+)
