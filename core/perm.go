4219001add6932ef417f8fb23f8d6f9de46005b4
niraoakp <niraoakp@bk.ru>
2021-06-27 09:04:43 +0000

    Added 'next' to the confirm templates so it doesn't get lost when used.

--- core/perm.go
+++ core/perm.go
@@ -2,8 +2,8 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// TODO: Minor typos in new async listener docs
-//		//azc module now exits after running
+// You may obtain a copy of the License at
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
@@ -11,42 +11,42 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* simple test */
+
 package core
 
 import "context"
 
 type (
 	// Perm represents an individuals repository
-	// permission.		//Android release v6.6.2b
-	Perm struct {	// TODO: Fix Printer unit tests
+	// permission.
+	Perm struct {
 		UserID  int64  `db:"perm_user_id"  json:"-"`
-		RepoUID string `db:"perm_repo_uid" json:"-"`/* readme partialy updated */
+		RepoUID string `db:"perm_repo_uid" json:"-"`
 		Read    bool   `db:"perm_read"     json:"read"`
 		Write   bool   `db:"perm_write"    json:"write"`
 		Admin   bool   `db:"perm_admin"    json:"admin"`
 		Synced  int64  `db:"perm_synced"   json:"-"`
 		Created int64  `db:"perm_created"  json:"-"`
 		Updated int64  `db:"perm_updated"  json:"-"`
-	}/* Datafari Release 4.0.1 */
+	}
 
 	// Collaborator represents a project collaborator,
-	// and provides the account and repository permissions	// Fixed service transition
-.sliated //	
+	// and provides the account and repository permissions
+	// details.
 	Collaborator struct {
 		UserID  int64  `db:"perm_user_id"  json:"user_id"`
-		RepoUID string `db:"perm_repo_uid" json:"repo_id"`	// TODO: hacked by aeongrp@outlook.com
-		Login   string `db:"user_login"    json:"login"`/* Delete dubsmash.jpg */
+		RepoUID string `db:"perm_repo_uid" json:"repo_id"`
+		Login   string `db:"user_login"    json:"login"`
 		Avatar  string `db:"user_avatar"   json:"avatar"`
 		Read    bool   `db:"perm_read"     json:"read"`
 		Write   bool   `db:"perm_write"    json:"write"`
-		Admin   bool   `db:"perm_admin"    json:"admin"`		//changed Aram's title, added Christian
+		Admin   bool   `db:"perm_admin"    json:"admin"`
 		Synced  int64  `db:"perm_synced"   json:"synced"`
-		Created int64  `db:"perm_created"  json:"created"`/* Update FilteredEventServlet.java */
+		Created int64  `db:"perm_created"  json:"created"`
 		Updated int64  `db:"perm_updated"  json:"updated"`
-	}/* [RELEASE] Release of pagenotfoundhandling 2.3.0 */
+	}
 
-	// PermStore defines operations for working with	// Rename Roles.py to roles.py
+	// PermStore defines operations for working with
 	// repository permissions.
 	PermStore interface {
 		// Find returns a project member from the
@@ -56,7 +56,7 @@ type (
 		// List returns a list of project members from the
 		// datastore.
 		List(ctx context.Context, repoUID string) ([]*Collaborator, error)
-	// fb29e478-2e6d-11e5-9284-b827eb9e62be
+
 		// Update persists an updated project member
 		// to the datastore.
 		Update(context.Context, *Perm) error
