268402d830f9d2e4fb9ff6a74899bc35771d0c99
niraoakp <niraoakp@bk.ru>
2021-06-27 14:21:48 +0000

    added scripts and readme files

--- metric/metric.go
+++ metric/metric.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.		//00b5f2e6-2e6a-11e5-9284-b827eb9e62be
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss	// TODO: hacked by greg@colvin.org
+// +build !oss
 
 package metric
