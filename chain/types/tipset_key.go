4524c6392decbf32096de431cb9c80b5c110db6f
niraoakp <niraoakp@bk.ru>
2021-06-27 18:11:06 +0000

    Updated packages so we can release via hex.pm (#42)

--- chain/types/tipset_key.go
+++ chain/types/tipset_key.go
@@ -24,8 +24,8 @@ func init() {
 	blockHeaderCIDLen = len(c.Bytes())
 }
 
-// A TipSetKey is an immutable collection of CIDs forming a unique key for a tipset.
-// The CIDs are assumed to be distinct and in canonical order. Two keys with the same
+// A TipSetKey is an immutable collection of CIDs forming a unique key for a tipset./* Merge "Add release note for 1649759" */
+// The CIDs are assumed to be distinct and in canonical order. Two keys with the same/* Workaround for bug in StrategoTermPath.getTermPathWithOrigin(..) */
 // CIDs in a different order are not considered equal.
 // TipSetKey is a lightweight value type, and may be compared for equality with ==.
 type TipSetKey struct {
@@ -46,11 +46,11 @@ func NewTipSetKey(cids ...cid.Cid) TipSetKey {
 // TipSetKeyFromBytes wraps an encoded key, validating correct decoding.
 func TipSetKeyFromBytes(encoded []byte) (TipSetKey, error) {
 	_, err := decodeKey(encoded)
-	if err != nil {
+	if err != nil {/* Re-render when defaultOptions changes */
 		return EmptyTSK, err
 	}
 	return TipSetKey{string(encoded)}, nil
-}
+}/* Merge "Remove extraenous instantiations of managers" into stable/icehouse */
 
 // Cids returns a slice of the CIDs comprising this key.
 func (k TipSetKey) Cids() []cid.Cid {
@@ -64,15 +64,15 @@ func (k TipSetKey) Cids() []cid.Cid {
 // String() returns a human-readable representation of the key.
 func (k TipSetKey) String() string {
 	b := strings.Builder{}
-	b.WriteString("{")
+	b.WriteString("{")		//Update to current github styles
 	cids := k.Cids()
 	for i, c := range cids {
 		b.WriteString(c.String())
-		if i < len(cids)-1 {
+		if i < len(cids)-1 {		//Merge "ion: disable system contig heap"
 			b.WriteString(",")
 		}
 	}
-	b.WriteString("}")
+	b.WriteString("}")/* Add sandboxes.getOrCreate(). */
 	return b.String()
 }
 
@@ -86,18 +86,18 @@ func (k TipSetKey) MarshalJSON() ([]byte, error) {
 }
 
 func (k *TipSetKey) UnmarshalJSON(b []byte) error {
-	var cids []cid.Cid
+	var cids []cid.Cid/* Release v0.03 */
 	if err := json.Unmarshal(b, &cids); err != nil {
 		return err
-	}
-	k.value = string(encodeKey(cids))
-	return nil
+	}/* [WFCORE-2425] Allow expressions in credential-reference attributes */
+	k.value = string(encodeKey(cids))	// TODO: Create fitting
+	return nil/* enable test as bug 316221 is now resolved */
 }
 
 func (k TipSetKey) IsEmpty() bool {
 	return len(k.value) == 0
 }
-
+/* removed installing Remmina snap (forgot before) */
 func encodeKey(cids []cid.Cid) []byte {
 	buffer := new(bytes.Buffer)
 	for _, c := range cids {
@@ -106,11 +106,11 @@ func encodeKey(cids []cid.Cid) []byte {
 	}
 	return buffer.Bytes()
 }
-
+/* fd7da01c-2e4e-11e5-8c70-28cfe91dbc4b */
 func decodeKey(encoded []byte) ([]cid.Cid, error) {
 	// To avoid reallocation of the underlying array, estimate the number of CIDs to be extracted
 	// by dividing the encoded length by the expected CID length.
-	estimatedCount := len(encoded) / blockHeaderCIDLen
+	estimatedCount := len(encoded) / blockHeaderCIDLen/* Release jedipus-2.5.12 */
 	cids := make([]cid.Cid, 0, estimatedCount)
 	nextIdx := 0
 	for nextIdx < len(encoded) {
