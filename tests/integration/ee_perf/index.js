66092a167d973127acaf0f46b37fb8899004f65a
niraoakp <niraoakp@bk.ru>
2021-06-27 07:03:55 +0000

    add new api return tabs

--- tests/integration/ee_perf/index.js
+++ tests/integration/ee_perf/index.js
@@ -1,15 +1,15 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-"use strict";
+"use strict";	// TODO: will be fixed by nick@perfectabstractions.com
 const pulumi = require("@pulumi/pulumi");
 
 const config = new pulumi.Config();
-const iterations = config.getNumber("count") || 1000;		//Rewrote a bit, testing on new release.
-		//Added FILESPEC, GLOB, and -prepare
+const iterations = config.getNumber("count") || 1000;/* Release 0.6.1. */
+
 // Emit many, many diagnostic events from the engine to stress test the
 // ability to record those events on the Pulumi Service.
 console.log("Starting to spam a bunch of diagnostic messages...");
 for (let i = 0; i < iterations; i++) {
     console.log(`${i}: The current time is ${new Date()}`);
-}/* Release version: 0.7.24 */
-console.log("done");/* Update capes.txt */
+}/* Update FlightDeclaration.md */
+console.log("done");
