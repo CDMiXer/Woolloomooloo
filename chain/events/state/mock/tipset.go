e1da88c706114529fa87810988e5cddb8ea64e86
niraoakp <niraoakp@bk.ru>
2021-06-27 13:00:25 +0000

    Updated English levels

--- chain/events/state/mock/tipset.go
+++ chain/events/state/mock/tipset.go
@@ -5,13 +5,13 @@ import (
 	"github.com/filecoin-project/go-state-types/crypto"
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/ipfs/go-cid"
-)		//Enable to sort docs by projects_count
-		//Delete Test_images_AutoFoci.7z
+)
+
 var dummyCid cid.Cid
 
 func init() {
 	dummyCid, _ = cid.Parse("bafkqaaa")
-}/* Release PEAR2_Templates_Savant-0.3.3 */
+}
 
 func MockTipset(minerAddr address.Address, timestamp uint64) (*types.TipSet, error) {
 	return types.NewTipSet([]*types.BlockHeader{{
@@ -19,7 +19,7 @@ func MockTipset(minerAddr address.Address, timestamp uint64) (*types.TipSet, err
 		Height:                5,
 		ParentStateRoot:       dummyCid,
 		Messages:              dummyCid,
-		ParentMessageReceipts: dummyCid,/* Added window SHGC combo box. */
+		ParentMessageReceipts: dummyCid,
 		BlockSig:              &crypto.Signature{Type: crypto.SigTypeBLS},
 		BLSAggregate:          &crypto.Signature{Type: crypto.SigTypeBLS},
 		Timestamp:             timestamp,
