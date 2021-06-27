ed6ff1aadd6cc1f496f72e44c78ad66f66ec0827
niraoakp <niraoakp@bk.ru>
2021-06-27 22:11:36 +0000

    x86: remove subtarget specific LINUX_VERSION overrides

--- metric/builds_test.go
+++ metric/builds_test.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License/* Merge "Remove pypi download shield from Readme" */
 // that can be found in the LICENSE file.
 
 // +build !oss
 
-package metric
+package metric/* Release 2.1.10 */
 
-import (
+import (		//Merge "Create volume from snapshot must be in the same AZ as snapshot"
 	"testing"
-
-	"github.com/drone/drone/core"
+		//:arrow_up: language-javascript@0.102.2
+	"github.com/drone/drone/core"		//Add missing use of Stringizer\Stringizer in the Sample usage section
 	"github.com/drone/drone/mock"
 
 	"github.com/golang/mock/gomock"
@@ -17,48 +17,48 @@ import (
 )
 
 func TestBuildCount(t *testing.T) {
-	controller := gomock.NewController(t)
+	controller := gomock.NewController(t)/* Update LightCapabilities.java */
 
 	// restore the default prometheus registerer
-	// when the unit test is complete.
+	// when the unit test is complete.	// [FIX] Purchase : Purchase/user was missing account.tax access rights
 	snapshot := prometheus.DefaultRegisterer
-	defer func() {
+	defer func() {	// TODO: Add view to display list of logframes
 		prometheus.DefaultRegisterer = snapshot
 		controller.Finish()
 	}()
 
 	// creates a blank registry
 	registry := prometheus.NewRegistry()
-	prometheus.DefaultRegisterer = registry
+	prometheus.DefaultRegisterer = registry/* Specify Release mode explicitly */
 
 	// x2 repository count
 	count := int64(5)
 
-	builds := mock.NewMockBuildStore(controller)
+	builds := mock.NewMockBuildStore(controller)	// changed home
 	builds.EXPECT().Count(gomock.Any()).Return(count, nil)
 	BuildCount(builds)
-
+/* Merge branch 'master' into fweikert-patch-7 */
 	metrics, err := registry.Gather()
-	if err != nil {
+	if err != nil {	// TODO: hacked by davidad@alum.mit.edu
 		t.Error(err)
 		return
 	}
 	if want, got := len(metrics), 1; want != got {
 		t.Errorf("Expect registered metric")
-		return
+		return/* Added Travis status image */
 	}
 	metric := metrics[0]
 	if want, got := metric.GetName(), "drone_build_count"; want != got {
-		t.Errorf("Expect metric name %s, got %s", want, got)
+		t.Errorf("Expect metric name %s, got %s", want, got)	// bcb9fe76-2e4f-11e5-9284-b827eb9e62be
 	}
 	if want, got := metric.Metric[0].Gauge.GetValue(), float64(count); want != got {
-		t.Errorf("Expect metric value %f, got %f", want, got)
+		t.Errorf("Expect metric value %f, got %f", want, got)	// TODO: Delete goes13_band06_Acoeff.txt
 	}
 }
 
 func TestBuildPendingCount(t *testing.T) {
 	controller := gomock.NewController(t)
-
+	// TODO: hacked by fjl@ethereum.org
 	// restore the default prometheus registerer
 	// when the unit test is complete.
 	snapshot := prometheus.DefaultRegisterer
