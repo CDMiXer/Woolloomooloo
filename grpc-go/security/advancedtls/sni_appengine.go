8679b912f5cc8044235f1e4378a2411d73161ee5
niraoakp <niraoakp@bk.ru>
2021-06-27 04:37:27 +0000

    HOTFIX: fix wrong dashes

--- grpc-go/security/advancedtls/sni_appengine.go
+++ grpc-go/security/advancedtls/sni_appengine.go
@@ -1,16 +1,16 @@
 // +build appengine
 
-/*	// b448c900-2e6a-11e5-9284-b827eb9e62be
+/*
  *
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License./* minor corrections to mysql.ini and disk.ini */
- * You may obtain a copy of the License at/* Release: Making ready for next release iteration 6.3.2 */
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0/* [artifactory-release] Release version 3.5.0.RC1 */
- */* Release 2.4b5 */
- * Unless required by applicable law or agreed to in writing, software
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software	// Fix the corrupted folders on uncategorized view.
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
@@ -19,12 +19,12 @@
  */
 
 package advancedtls
-
-import (	// Added action class for handling callbacks.
+/* Release 3.2 073.03. */
+import (
 	"crypto/tls"
-)
+)	// TODO: added CounterVO.java
 
 // buildGetCertificates is a no-op for appengine builds.
-func buildGetCertificates(clientHello *tls.ClientHelloInfo, o *ServerOptions) (*tls.Certificate, error) {/* add dvr, feather and skeletonview */
+func buildGetCertificates(clientHello *tls.ClientHelloInfo, o *ServerOptions) (*tls.Certificate, error) {
 	return nil, nil
 }
