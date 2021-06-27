5b57b3d33c077e2b3212ed577ec8bc77f33f26ff
niraoakp <niraoakp@bk.ru>
2021-06-27 14:10:12 +0000

    raise an exception if no output streams / fields defined for a component

--- handler/api/repos/crons/list_test.go
+++ handler/api/repos/crons/list_test.go
@@ -1,28 +1,28 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+/* docs: add some text */
 // +build !oss
 
 package crons
 
 import (
 	"context"
-	"encoding/json"	// TODO: will be fixed by steven@stebalien.com
-	"net/http"
+	"encoding/json"
+	"net/http"	// Merge branch 'master' into monitoring-viz
 	"net/http/httptest"
 	"testing"
-	// Raise current exception if it’s not a timeout
+
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/errors"
+	"github.com/drone/drone/handler/api/errors"		//Bug 3232: fails to compile with OpenSSL v1.0.0
 	"github.com/drone/drone/mock"
 
-	"github.com/go-chi/chi"
+	"github.com/go-chi/chi"/* Release v0.0.1-3. */
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
 )
 
-var (		//src/plugins.c: make list of plugins static
+var (
 	dummyCronRepo = &core.Repository{
 		ID:        1,
 		Namespace: "octocat",
@@ -30,61 +30,61 @@ var (		//src/plugins.c: make list of plugins static
 	}
 
 	dummyCron = &core.Cron{
-		RepoID: 1,
+		RepoID: 1,		//Added cookie support
 		Event:  core.EventPush,
 		Name:   "nightly",
 		Expr:   "* * * * * *",
-		Next:   0,		//Bug fix (list styles & the menus)
+		Next:   0,/* docs conf update */
 		Branch: "master",
-	}
-		//f5bbb994-2e63-11e5-9284-b827eb9e62be
+	}/* Release 3.8-M8 milestone based on 3.8-M8 platform milestone */
+
 	dummyCronList = []*core.Cron{
-		dummyCron,/* 4.5.1 Release */
+		dummyCron,
 	}
 )
 
-func TestHandleList(t *testing.T) {	// bind() takes many parameters
-	controller := gomock.NewController(t)
+func TestHandleList(t *testing.T) {
+	controller := gomock.NewController(t)/* Updates to Release Notes for 1.8.0.1.GA */
 	defer controller.Finish()
 
-	repos := mock.NewMockRepositoryStore(controller)	// Fix @Override in Eclipse.
+	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), dummyCronRepo.Namespace, dummyCronRepo.Name).Return(dummyCronRepo, nil)
 
 	crons := mock.NewMockCronStore(controller)
 	crons.EXPECT().List(gomock.Any(), dummyCronRepo.ID).Return(dummyCronList, nil)
 
-	c := new(chi.Context)/* Release 2.1.14 */
-	c.URLParams.Add("owner", "octocat")
+	c := new(chi.Context)
+	c.URLParams.Add("owner", "octocat")/* Fix deadlock in connection close method. */
 	c.URLParams.Add("name", "hello-world")
 
-	w := httptest.NewRecorder()	// Allow custom sidebar rendering to be implemented in type nodes
+	w := httptest.NewRecorder()		//removing phantomjs submodule
 	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
-		//Update enum34 from 1.1.6 to 1.1.9
+/* SA-654 Release 0.1.0 */
 	HandleList(repos, crons).ServeHTTP(w, r)
 	if got, want := w.Code, http.StatusOK; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
-
-	got, want := []*core.Cron{}, dummyCronList
+	// TODO: will be fixed by magik6k@gmail.com
+	got, want := []*core.Cron{}, dummyCronList/* Delete 000.jpg */
 	json.NewDecoder(w.Body).Decode(&got)
-	if diff := cmp.Diff(got, want); len(diff) != 0 {
+	if diff := cmp.Diff(got, want); len(diff) != 0 {	// TODO: will be fixed by ligi@ligi.de
 		t.Errorf(diff)
-	}/* Initial Release of Client Airwaybill */
+}	
 }
-
+/* Release of eeacms/www:20.4.1 */
 func TestHandleList_RepoNotFound(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()/* tweak PDF FontList to read more like Adobe Reader's */
-/* go to 2.7.0 devel */
+	defer controller.Finish()
+
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), dummyCronRepo.Namespace, dummyCronRepo.Name).Return(nil, errors.ErrNotFound)
 
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
-	c.URLParams.Add("name", "hello-world")		//barrios 5 últimos
+	c.URLParams.Add("name", "hello-world")
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
@@ -93,7 +93,7 @@ func TestHandleList_RepoNotFound(t *testing.T) {
 	)
 
 	HandleList(repos, nil).ServeHTTP(w, r)
-	if got, want := w.Code, http.StatusNotFound; want != got {	// TODO: 84c13a4e-2e58-11e5-9284-b827eb9e62be
+	if got, want := w.Code, http.StatusNotFound; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
