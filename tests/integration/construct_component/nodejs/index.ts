0eaf80e69b3f590c754857d8d87c1d076d8fc2d7
niraoakp <niraoakp@bk.ru>
2021-06-27 17:59:24 +0000

    Merge "Build layoutlib_create tests." into lmp-dev

--- tests/integration/construct_component/nodejs/index.ts
+++ tests/integration/construct_component/nodejs/index.ts
@@ -1,8 +1,8 @@
-// Copyright 2016-2020, Pulumi Corporation.  All rights reserved./* Release of eeacms/www:21.3.30 */
+// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
 import { Component } from "./component";
 
-const componentA = new Component("a", {echo: 42});	// Merge post travel request topics
-const componentB = new Component("b", {echo: componentA.echo});/* videos move, but... i broke playback. */
+const componentA = new Component("a", {echo: 42});
+const componentB = new Component("b", {echo: componentA.echo});
 const componentC = new Component("c", {echo: componentA.childId});
-	// TODO: will be fixed by nagydani@epointsystem.org
+
