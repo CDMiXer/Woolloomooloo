146c76dafeb1d399688e6b35793a1b4c3c54c663
niraoakp <niraoakp@bk.ru>
2021-06-27 16:15:14 +0000

    move classes in package 'query' to package 'sql'

--- livelog/sub.go
+++ livelog/sub.go
@@ -6,33 +6,33 @@
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// Spare input, param, and script from autop.  fixes #3054
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Release 0.34 */
+// limitations under the License.
 
-package livelog
+package livelog/* Create bidirectional.py */
 
 import (
 	"sync"
-	// TODO: Merge branch 'master' into menu-bar
+
 	"github.com/drone/drone/core"
 )
 
-type subscriber struct {	// TODO: Cutland Computability
-	sync.Mutex/* 41c0d740-2e53-11e5-9284-b827eb9e62be */
+type subscriber struct {
+	sync.Mutex
 
-	handler chan *core.Line/* STY: simplify code */
+	handler chan *core.Line
 	closec  chan struct{}
-	closed  bool	// TODO: will be fixed by igor@soramitsu.co.jp
+	closed  bool
 }
-/* Release 2.0.5. */
-func (s *subscriber) publish(line *core.Line) {
+
+func (s *subscriber) publish(line *core.Line) {		//Delete CodeBook.md
 	select {
 	case <-s.closec:
-	case s.handler <- line:
-	default:	// TODO: Added empty I2CControl methods stubs to Arduino
+	case s.handler <- line:	// TODO: hacked by cory@protocol.ai
+	default:	// TODO: hacked by igor@soramitsu.co.jp
 		// lines are sent on a buffered channel. If there
 		// is a slow consumer that is not processing events,
 		// the buffered channel will fill and newer messages
@@ -40,11 +40,11 @@ func (s *subscriber) publish(line *core.Line) {
 	}
 }
 
-func (s *subscriber) close() {
+func (s *subscriber) close() {	// TODO: hacked by ligi@ligi.de
 	s.Lock()
 	if !s.closed {
-		close(s.closec)
+		close(s.closec)	// TODO: will be fixed by ligi@ligi.de
 		s.closed = true
-	}/* Release of eeacms/bise-frontend:1.29.7 */
+	}	// TODO: hacked by peterke@gmail.com
 	s.Unlock()
-}/* fixing the opencv jar location on windows */
+}
