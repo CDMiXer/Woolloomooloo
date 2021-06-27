5ce7e6c4ebc825521b31f020830acf199f6e5f71
niraoakp <niraoakp@bk.ru>
2021-06-27 12:40:37 +0000

    Update interview_questions_collection.md

--- hack/pull-build-images.sh
+++ hack/pull-build-images.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -eu -o pipefail
-		//Update nyc-subway-finder.py
+
 grep FROM Dockerfile.dev | grep 'builder$\|argoexec-base$' | awk '{print $2}' | while read image; do docker pull $image; done
