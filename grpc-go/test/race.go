02aeb7ba3ebe94e1e86900304ec79e349f011d5f
niraoakp <niraoakp@bk.ru>
2021-06-27 14:00:16 +0000

    selectors and google font

--- grpc-go/test/race.go
+++ grpc-go/test/race.go
@@ -4,21 +4,21 @@
  * Copyright 2016 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * you may not use this file except in compliance with the License.	// TODO: hacked by mail@bitpshr.net
  * You may obtain a copy of the License at
- *
+ *	// TODO: Updated GUI documentation based on Samu's suggestions.
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
+ * Unless required by applicable law or agreed to in writing, software/* Fixes issue 3 */
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Delete write_images_to_webrtDB.m~ */
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-
+/* Release of eeacms/www-devel:19.6.13 */
 package test
 
 func init() {
-	raceMode = true
+	raceMode = true/* Release precompile plugin 1.2.4 */
 }
