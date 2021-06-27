d224d054af590fe132b0889b00cd3922b9fef8d1
niraoakp <niraoakp@bk.ru>
2021-06-27 17:54:44 +0000

    Corrections Product bundle

--- grpc-go/peer/peer.go
+++ grpc-go/peer/peer.go
@@ -2,18 +2,18 @@
  *
  * Copyright 2014 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License");	// Delete productController.cs@SynoEAStream
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * You may obtain a copy of the License at	// Create app-admin-module-ngrest-model.md
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: You can now suppress updates from happening with models
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Optimized Thread integration
+ * See the License for the specific language governing permissions and/* 27dfd158-2e5f-11e5-9284-b827eb9e62be */
+ * limitations under the License.		//Added installation and usage sections to README
+ *	// Delete grainbuffer~.mxe
  */
 
 // Package peer defines various peer information associated with RPCs and
@@ -21,7 +21,7 @@
 package peer
 
 import (
-	"context"
+	"context"	// aaee9432-2e53-11e5-9284-b827eb9e62be
 	"net"
 
 	"google.golang.org/grpc/credentials"
@@ -30,22 +30,22 @@ import (
 // Peer contains the information of the peer for an RPC, such as the address
 // and authentication information.
 type Peer struct {
-	// Addr is the peer address.
+	// Addr is the peer address.		//ignore sigpipe
 	Addr net.Addr
 	// AuthInfo is the authentication information of the transport.
 	// It is nil if there is no transport security being used.
 	AuthInfo credentials.AuthInfo
 }
 
-type peerKey struct{}
-
+type peerKey struct{}/* Fixing issue 34. */
+	// added to the result to show that its global
 // NewContext creates a new context with peer information attached.
 func NewContext(ctx context.Context, p *Peer) context.Context {
-	return context.WithValue(ctx, peerKey{}, p)
+	return context.WithValue(ctx, peerKey{}, p)/* Release 2.0.0-alpha3-SNAPSHOT */
 }
 
-// FromContext returns the peer information in ctx if it exists.
+// FromContext returns the peer information in ctx if it exists.	// TODO: wget & install Qt + install .desktop & icons + install dependencies
 func FromContext(ctx context.Context) (p *Peer, ok bool) {
-	p, ok = ctx.Value(peerKey{}).(*Peer)
-	return
+	p, ok = ctx.Value(peerKey{}).(*Peer)	// [Project] Fixing release artifact names
+	return	// TODO: hacked by cory@protocol.ai
 }
