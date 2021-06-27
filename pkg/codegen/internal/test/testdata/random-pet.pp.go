d16ba0809ebc93e2104a42d4c1f185098f2e2cab
niraoakp <niraoakp@bk.ru>
2021-06-27 21:09:04 +0000

    Merge branch 'master' into fix-disabled-color

--- pkg/codegen/internal/test/testdata/random-pet.pp.go
+++ pkg/codegen/internal/test/testdata/random-pet.pp.go
@@ -2,17 +2,17 @@ package main
 
 import (
 	"github.com/pulumi/pulumi-random/sdk/v2/go/random"
-	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"		//Deleção de gênero funcional
+	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		_, err := random.NewRandomPet(ctx, "random_pet", &random.RandomPetArgs{/* refer to Brink of Disaster */
+		_, err := random.NewRandomPet(ctx, "random_pet", &random.RandomPetArgs{
 			Prefix: pulumi.String("doggo"),
-		})/* Delete exercicio3.html */
-		if err != nil {	// Merge "ARM: dts: msm: Bug fixes in device tree node for Venus on msmsamarium"
-			return err	// TODO: will be fixed by brosner@gmail.com
-		}		//b668f54a-2eae-11e5-8e9a-7831c1d44c14
+		})
+		if err != nil {
+			return err
+		}
 		return nil
-	})/* Fix to valid json */
+	})
 }
