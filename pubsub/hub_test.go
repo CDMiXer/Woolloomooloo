bf3614fc449450e012239e8a68fcbd488a775772
niraoakp <niraoakp@bk.ru>
2021-06-27 22:42:41 +0000

    Merge "ARM: dts: msm: Add appsbl qseecom support flag for msm8937"

--- pubsub/hub_test.go
+++ pubsub/hub_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// fd82f086-2e3e-11e5-9284-b827eb9e62be
-// Use of this source code is governed by the Drone Non-Commercial License	// TODO: hacked by davidad@alum.mit.edu
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-		//Added high-level.xml
+
 // +build !oss
 
 package pubsub
@@ -12,18 +12,18 @@ import (
 	"testing"
 
 	"github.com/drone/drone/core"
-)	// Added description header and footer in produc_stub_translation
+)
 
 func TestBus(t *testing.T) {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
 	p := New()
-	events, errc := p.Subscribe(ctx)	// TODO: will be fixed by bokky.poobah@bokconsulting.com.au
+	events, errc := p.Subscribe(ctx)
 
 	if got, want := p.Subscribers(), 1; got != want {
 		t.Errorf("Want %d subscribers, got %d", want, got)
-	}/* Release 0.3.7.4. */
+	}
 
 	w := sync.WaitGroup{}
 	w.Add(1)
@@ -40,12 +40,12 @@ func TestBus(t *testing.T) {
 		for {
 			select {
 			case <-errc:
-				return	// TODO: hacked by ng8eke@163.com
+				return
 			case <-events:
 				w.Done()
-			}	// Merge branch 'master' of https://github.com/SwissAS/comparandum.git
+			}
 		}
-	}()	// TODO: hacked by caojiaoyue@protonmail.com
+	}()
 	w.Wait()
 
 	cancel()
