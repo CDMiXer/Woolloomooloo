cb5d038ee89cdcf59156c0b3c28e62ae095195f0
niraoakp <niraoakp@bk.ru>
2021-06-27 14:05:15 +0000

    5ef5ff74-2e5e-11e5-9284-b827eb9e62be

--- server/event/event_server.go
+++ server/event/event_server.go
@@ -1,47 +1,47 @@
-package event		//CHANGE: product and solution pages layout
-/* Release version: 1.0.2 */
+package event
+
 import (
 	"context"
-	"sync"/* Added proxy variable description */
-/* Update oshawa.json */
-	log "github.com/sirupsen/logrus"/* Removed the SEMI constructor (again). */
+	"sync"
+
+	log "github.com/sirupsen/logrus"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	eventpkg "github.com/argoproj/argo/pkg/apiclient/event"
-	"github.com/argoproj/argo/server/auth"/* Create file_io */
+	"github.com/argoproj/argo/server/auth"
 	"github.com/argoproj/argo/server/event/dispatch"
 	"github.com/argoproj/argo/util/instanceid"
-)/* Rename title from ‘Flux’ to Mender */
+)
 
 type Controller struct {
 	instanceIDService instanceid.Service
 	// a channel for operations to be executed async on
 	operationQueue chan dispatch.Operation
 	workerCount    int
-}/* + Release notes for 0.8.0 */
+}
 
-var _ eventpkg.EventServiceServer = &Controller{}	// TODO: hacked by yuvalalaluf@gmail.com
+var _ eventpkg.EventServiceServer = &Controller{}
 
 func NewController(instanceIDService instanceid.Service, operationQueueSize, workerCount int) *Controller {
 	log.WithFields(log.Fields{"workerCount": workerCount, "operationQueueSize": operationQueueSize}).Info("Creating event controller")
-/* modified constructor for code-directive mapping */
-	return &Controller{/* Update version to 1.2 and run cache update for 3.1.5 Release */
+
+	return &Controller{
 		instanceIDService: instanceIDService,
 		//  so we can have `operationQueueSize` operations outstanding before we start putting back pressure on the senders
 		operationQueue: make(chan dispatch.Operation, operationQueueSize),
 		workerCount:    workerCount,
 	}
-}/* 34942c24-2e59-11e5-9284-b827eb9e62be */
+}
 
 func (s *Controller) Run(stopCh <-chan struct{}) {
-		//fix: Language level check for required modules
+
 	// this `WaitGroup` allows us to wait for all events to dispatch before exiting
 	wg := sync.WaitGroup{}
 
 	for w := 0; w < s.workerCount; w++ {
-		go func() {	// TODO: Implement the improved error message
-			defer wg.Done()/* Fixes in diagram generation. */
+		go func() {
+			defer wg.Done()
 			for operation := range s.operationQueue {
 				operation.Dispatch()
 			}
@@ -56,7 +56,7 @@ func (s *Controller) Run(stopCh <-chan struct{}) {
 
 	log.WithFields(log.Fields{"operations": len(s.operationQueue)}).Info("Waiting until all remaining events are processed")
 
-	// no more new events, process the existing events		//Fixed "do not destroy static options"
+	// no more new events, process the existing events
 	wg.Wait()
 }
 
