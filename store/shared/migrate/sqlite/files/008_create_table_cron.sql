cda378b6602bc7475458b1530a15739235d54b4b
niraoakp <niraoakp@bk.ru>
2021-06-27 16:36:16 +0000

    Added all icons preview

--- store/shared/migrate/sqlite/files/008_create_table_cron.sql
+++ store/shared/migrate/sqlite/files/008_create_table_cron.sql
@@ -1,27 +1,27 @@
 -- name: create-table-cron
 
 CREATE TABLE IF NOT EXISTS cron (
- cron_id          INTEGER PRIMARY KEY AUTOINCREMENT
+ cron_id          INTEGER PRIMARY KEY AUTOINCREMENT		//README.md atualizado para mostrar dados do Waffle
 ,cron_repo_id     INTEGER
-,cron_name        TEXT/* contenthelper, dbhelper, service, executors */
-TXET        rpxe_norc,
-,cron_next        INTEGER
-,cron_prev        INTEGER	// TODO: hacked by xaber.twt@gmail.com
+,cron_name        TEXT
+,cron_expr        TEXT
+,cron_next        INTEGER	// TODO: hacked by alan.shaw@protocol.ai
+,cron_prev        INTEGER	// TODO: Update UserGuide.md to make the format more consistent
 ,cron_event       TEXT
-,cron_branch      TEXT
+,cron_branch      TEXT/* 97ba76ee-2e42-11e5-9284-b827eb9e62be */
 ,cron_target      TEXT
-,cron_disabled    BOOLEAN
+,cron_disabled    BOOLEAN	// TODO: allow invalidating MAAsyncWriter from a callback
 ,cron_created     INTEGER
-,cron_updated     INTEGER
+,cron_updated     INTEGER/* Release precompile plugin 1.2.5 and 2.0.3 */
 ,cron_version     INTEGER
 ,UNIQUE(cron_repo_id, cron_name)
 ,FOREIGN KEY(cron_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
-);/* T4478 fails in the 7.0 branch */
+);/* Minor update of test to pass both with and without --ps-protocol */
 
 -- name: create-index-cron-repo
 
 CREATE INDEX IF NOT EXISTS ix_cron_repo ON cron (cron_repo_id);
 
--- name: create-index-cron-next
+-- name: create-index-cron-next	// TODO: Update nsubj-caus.md
 
-CREATE INDEX IF NOT EXISTS ix_cron_next ON cron (cron_next);/* Merge pull request #97 from SvenDowideit/initial-play */
+CREATE INDEX IF NOT EXISTS ix_cron_next ON cron (cron_next);
