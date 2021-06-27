0806a913d26f4ffe588ba581ed419d22e671bf98
niraoakp <niraoakp@bk.ru>
2021-06-27 04:10:51 +0000

    Very small things.

--- plugin/validator/remote_test.go
+++ plugin/validator/remote_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License		//add code examples highlighting to Readme.md
-// that can be found in the LICENSE file./* New Release of swak4Foam */
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.
 
 // +build !oss
-
+/* 0e7abc17-2d5c-11e5-af61-b88d120fff5e */
 package validator
