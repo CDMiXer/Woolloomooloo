7bb2d0df24bb9e8396bb686f73aca9b1eef190dc
niraoakp <niraoakp@bk.ru>
2021-06-27 22:52:02 +0000

    cleaning up cross channel

--- core/convert.go
+++ core/convert.go
@@ -1,21 +1,21 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc.		//Updated TermSuiteUI todo list
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0/* SO-3948: remove unused includePreReleaseContent from exporter fragments */
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License./* Merge "Release 3.0.10.049 Prima WLAN Driver" */
 
 package core
 
 import "context"
-	// TODO: Add support for i.MX6Q SabreLite board to linaro-media-create.
+
 type (
 	// ConvertArgs represents a request to the pipeline
 	// conversion service.
@@ -24,12 +24,12 @@ type (
 		Repo   *Repository `json:"repo,omitempty"`
 		Build  *Build      `json:"build,omitempty"`
 		Config *Config     `json:"config,omitempty"`
-	}	// TODO: will be fixed by alan.shaw@protocol.ai
-/* Final Merge Before April Release (first merge) */
+	}
+		//Add plugin.py.
 	// ConvertService converts non-native pipeline
 	// configuration formats to native configuration
 	// formats (e.g. jsonnet to yaml).
-	ConvertService interface {
-		Convert(context.Context, *ConvertArgs) (*Config, error)	// Fix links in help docs for logged in users.
-	}
+	ConvertService interface {/* Release ChildExecutor after the channel was closed. See #173  */
+		Convert(context.Context, *ConvertArgs) (*Config, error)
+	}		//audacious-plugins: switch to https.
 )
