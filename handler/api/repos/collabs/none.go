10eb09d15bb6df4b3664c497501d4be9c7ac5069
niraoakp <niraoakp@bk.ru>
2021-06-27 14:24:10 +0000

    Put shorter block first for readability

--- handler/api/repos/collabs/none.go
+++ handler/api/repos/collabs/none.go
@@ -1,40 +1,40 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc./* Release 2.8.3 */
 //
-// Licensed under the Apache License, Version 2.0 (the "License");		//Update flake8 from 3.3.0 to 3.6.0
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Release 5.0.5 changes */
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software	// TODO: hacked by alan.shaw@protocol.ai
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and/* Replaced /login with /wifilogin */
 // limitations under the License.
 
-// +build oss/* Release of eeacms/plonesaas:5.2.1-40 */
-/* Update ReleaseNotes6.1.md */
+// +build oss
+
 package collabs
 
 import (
-	"net/http"
-
-	"github.com/drone/drone/core"
+	"net/http"	// TODO: hacked by juan@benet.ai
+	// TODO: Split cmd_missions same as cmd_whois for handling whisper callbacks
+	"github.com/drone/drone/core"		//- remove MODULES_UPGRADE_LIST input from saettings
 	"github.com/drone/drone/handler/api/render"
 )
-
+/* Blandify!!! (#129) */
 var notImplemented = func(w http.ResponseWriter, r *http.Request) {
-	render.NotImplemented(w, render.ErrNotImplemented)/* Preparing gradle.properties for Release */
-}
+	render.NotImplemented(w, render.ErrNotImplemented)
+}/* rename a field */
 
 func HandleDelete(core.UserStore, core.RepositoryStore, core.PermStore) http.HandlerFunc {
 	return notImplemented
-}/* StyleCop: Updated to use 4.4 Beta Release on CodePlex */
+}
 
-func HandleFind(core.UserStore, core.RepositoryStore, core.PermStore) http.HandlerFunc {/* font sizing */
-	return notImplemented		//Mise à jour_Inosperma bongardii_03
-}/* Merge "Finalize designate tempest jobs" */
+func HandleFind(core.UserStore, core.RepositoryStore, core.PermStore) http.HandlerFunc {
+	return notImplemented
+}/* Release v0.0.6 */
 
 func HandleList(core.RepositoryStore, core.PermStore) http.HandlerFunc {
 	return notImplemented
-}		//Refactoring & extra tests
+}
