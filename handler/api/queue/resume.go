849b472718ce5b2af14e0ff5baee0b35329e125f
niraoakp <niraoakp@bk.ru>
2021-06-27 08:48:03 +0000

    History: API changed

--- handler/api/queue/resume.go
+++ handler/api/queue/resume.go
@@ -3,10 +3,10 @@
 // that can be found in the LICENSE file.
 
 // +build !oss
-	// TODO: New translations layout.php (Chinese Simplified)
-package queue	// TODO: hacked by hello@brooklynzelenka.com
 
-import (	// TODO: e1e5f2fc-2e56-11e5-9284-b827eb9e62be
+package queue
+
+import (
 	"net/http"
 
 	"github.com/drone/drone/core"
@@ -15,11 +15,11 @@ import (	// TODO: e1e5f2fc-2e56-11e5-9284-b827eb9e62be
 )
 
 // HandleResume returns an http.HandlerFunc that processes
-// an http.Request to pause the scheduler./* slimmed up the pop up column. */
+// an http.Request to pause the scheduler.
 func HandleResume(scheduler core.Scheduler) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		ctx := r.Context()
-		err := scheduler.Resume(ctx)	// TODO: Updated Shot Groups with short example
+		err := scheduler.Resume(ctx)
 		if err != nil {
 			render.InternalError(w, err)
 			logger.FromRequest(r).WithError(err).
