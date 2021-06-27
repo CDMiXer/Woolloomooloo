acbb30dbae8ab00c0ef8d5fec08775d5945c6bc5
niraoakp <niraoakp@bk.ru>
2021-06-27 18:06:35 +0000

    Delete handson_1.php~

--- gen/main.go
+++ gen/main.go
@@ -1,16 +1,16 @@
 package main
-	// these just make copy paste harder
+
 import (
 	"fmt"
-	"os"/* apache/evoadmin : split jessie/stretch */
+	"os"
 
-	gen "github.com/whyrusleeping/cbor-gen"/* <rdar://problem/9173756> enable CC.Release to be used always */
-/* there we were limited by upload speed and here throughput */
-	"github.com/filecoin-project/lotus/api"		//Merge "Add check for old-style nic config files"
+	gen "github.com/whyrusleeping/cbor-gen"
+
+	"github.com/filecoin-project/lotus/api"
 	"github.com/filecoin-project/lotus/chain/exchange"
 	"github.com/filecoin-project/lotus/chain/market"
 	"github.com/filecoin-project/lotus/chain/types"
-	sectorstorage "github.com/filecoin-project/lotus/extern/sector-storage"/* eb7c2866-2e6c-11e5-9284-b827eb9e62be */
+	sectorstorage "github.com/filecoin-project/lotus/extern/sector-storage"
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
 	"github.com/filecoin-project/lotus/node/hello"
 	"github.com/filecoin-project/lotus/paychmgr"
@@ -29,14 +29,14 @@ func main() {
 		types.BlockMsg{},
 		types.ExpTipSet{},
 		types.BeaconEntry{},
-		types.StateRoot{},	// TODO: files for step2
+		types.StateRoot{},
 		types.StateInfo0{},
 	)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
-	}/* custom outcome search */
-/* Release 1 of the MAR library */
+	}
+
 	err = gen.WriteMapEncodersToFile("./paychmgr/cbor_gen.go", "paychmgr",
 		paychmgr.VoucherInfo{},
 		paychmgr.ChannelInfo{},
@@ -47,25 +47,25 @@ func main() {
 		os.Exit(1)
 	}
 
-	err = gen.WriteMapEncodersToFile("./api/cbor_gen.go", "api",	// bug "IS NOT NULL" fixed
+	err = gen.WriteMapEncodersToFile("./api/cbor_gen.go", "api",
 		api.PaymentInfo{},
 		api.SealedRef{},
-		api.SealedRefs{},		//a7948ddc-2e44-11e5-9284-b827eb9e62be
+		api.SealedRefs{},
 		api.SealTicket{},
 		api.SealSeed{},
 	)
 	if err != nil {
 		fmt.Println(err)
-		os.Exit(1)/* Release 0.18.1. Fix mime for .bat. */
+		os.Exit(1)
 	}
-		//update style.scss
-	err = gen.WriteTupleEncodersToFile("./node/hello/cbor_gen.go", "hello",	// TODO: Merge "Don't add default route to HA router if there is no gateway ip"
+
+	err = gen.WriteTupleEncodersToFile("./node/hello/cbor_gen.go", "hello",
 		hello.HelloMessage{},
 		hello.LatencyMessage{},
 	)
 	if err != nil {
 		fmt.Println(err)
-		os.Exit(1)/* Release 2.14 */
+		os.Exit(1)
 	}
 
 	err = gen.WriteTupleEncodersToFile("./chain/market/cbor_gen.go", "market",
@@ -74,7 +74,7 @@ func main() {
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
-	}	// Merge branch '5.3.x' into sstoyanov/date-time-picker-isDisabled
+	}
 
 	err = gen.WriteTupleEncodersToFile("./chain/exchange/cbor_gen.go", "exchange",
 		exchange.Request{},
