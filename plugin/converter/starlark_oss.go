c24d3e877b403f770c603f54ac742def19991ff0
niraoakp <niraoakp@bk.ru>
2021-06-27 14:55:33 +0000

    Updated the pytorch-cpu feedstock.

--- plugin/converter/starlark_oss.go
+++ plugin/converter/starlark_oss.go
@@ -1,6 +1,6 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Merge "Release note, api-ref for event list nested_depth" */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -10,7 +10,7 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* code refactor */
+// limitations under the License.
 
 // +build oss
 
@@ -22,6 +22,6 @@ import (
 
 // Starlark returns a conversion service that converts the
 // starlark file to a yaml file.
-func Starlark(enabled bool) core.ConvertService {		//update pandoc
+func Starlark(enabled bool) core.ConvertService {
 	return new(noop)
 }
