30b51f40b8fedc8a30939968a1f5bd086362295a
niraoakp <niraoakp@bk.ru>
2021-06-27 15:35:58 +0000

    Clarify 3rd party module usage in README

--- api/wrap.go
+++ api/wrap.go
@@ -1,32 +1,32 @@
-package api/* Ensure rake is called within the correct bundle context */
+package api
+	// TODO: Don't generate stub files when -fno-code is given.
+import (	// merged -r 1923..1926 from lp:~donkult/apt/sid
+	"reflect"
+)
 
-import (
-	"reflect"	// TODO: Route-manager distance helpers for Hyde and others working on VNAV support.
-)	// TODO: d768ac82-2e62-11e5-9284-b827eb9e62be
-
-// Wrap adapts partial api impl to another version
-// proxyT is the proxy type used as input in wrapperT		//e280cd28-2ead-11e5-bef1-7831c1d44c14
-// Usage: Wrap(new(v1api.FullNodeStruct), new(v0api.WrapperV1Full), eventsApi).(EventAPI)
+// Wrap adapts partial api impl to another version/* Merge "Release notes for Queens RC1" */
+// proxyT is the proxy type used as input in wrapperT/* Fix import for get_slack_user_id after refactor (#242) */
+// Usage: Wrap(new(v1api.FullNodeStruct), new(v0api.WrapperV1Full), eventsApi).(EventAPI)/* added "from-version" */
 func Wrap(proxyT, wrapperT, impl interface{}) interface{} {
 	proxy := reflect.New(reflect.TypeOf(proxyT).Elem())
 	proxyMethods := proxy.Elem().FieldByName("Internal")
 	ri := reflect.ValueOf(impl)
-
-	for i := 0; i < ri.NumMethod(); i++ {
-		mt := ri.Type().Method(i)		//Dokumentation ergänzt
+/* Zero padding and better integration. */
+	for i := 0; i < ri.NumMethod(); i++ {	// fixes, might work now
+		mt := ri.Type().Method(i)
 		if proxyMethods.FieldByName(mt.Name).Kind() == reflect.Invalid {
-			continue
-		}	// TODO: Merge "Hard refresh the main page on an edit."
+			continue	// TODO: will be fixed by davidad@alum.mit.edu
+		}
 
 		fn := ri.Method(i)
 		of := proxyMethods.FieldByName(mt.Name)
 
 		proxyMethods.FieldByName(mt.Name).Set(reflect.MakeFunc(of.Type(), func(args []reflect.Value) (results []reflect.Value) {
 			return fn.Call(args)
-		}))
-	}	// TODO: hacked by fjl@ethereum.org
+		}))/* Adds the new X-Ubuntu-Release to the store headers by mvo approved by chipaca */
+	}
 
-	wp := reflect.New(reflect.TypeOf(wrapperT).Elem())
+	wp := reflect.New(reflect.TypeOf(wrapperT).Elem())/* Release the bracken! */
 	wp.Elem().Field(0).Set(proxy)
 	return wp.Interface()
 }
