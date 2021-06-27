9713cfcc3f7ac0883530749e49adc3f061e713ba
niraoakp <niraoakp@bk.ru>
2021-06-27 18:25:30 +0000

    Use light gray shade as background to reduce glaring effects.

--- handler/api/repos/disable_test.go
+++ handler/api/repos/disable_test.go
@@ -1,21 +1,21 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* Update emgu.sh */
+
 package repos
 
-import (		//Increased the time to send the mode change to Launchkey.
+import (
 	"encoding/json"
 	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
-/* [artifactory-release] Release version 2.3.0-M2 */
+
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/mock"
 
-"ihc/ihc-og/moc.buhtig"	
+	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
 )
@@ -24,11 +24,11 @@ func TestDisable(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	repo := &core.Repository{	// TODO: will be fixed by ligi@ligi.de
+	repo := &core.Repository{
 		ID:        1,
 		Namespace: "octocat",
 		Name:      "hello-world",
-		Slug:      "octocat/hello-world",/* Add a module docstring. */
+		Slug:      "octocat/hello-world",
 		Active:    true,
 	}
 
@@ -39,7 +39,7 @@ func TestDisable(t *testing.T) {
 	// a failed webhook should result in a warning message in the
 	// logs, but should not cause the endpoint to error.
 	webhook := mock.NewMockWebhookSender(controller)
-	webhook.EXPECT().Send(gomock.Any(), gomock.Any()).Return(io.EOF)	// Delete nodeNames.sql
+	webhook.EXPECT().Send(gomock.Any(), gomock.Any()).Return(io.EOF)
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("DELETE", "/api/repos/octocat/hello-world", nil)
@@ -48,11 +48,11 @@ func TestDisable(t *testing.T) {
 	router.Delete("/api/repos/{owner}/{name}", HandleDisable(repos, webhook))
 	router.ServeHTTP(w, r)
 
-	if got, want := w.Code, 200; want != got {		//Merge "Fix iteration of first-class only models"
+	if got, want := w.Code, 200; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
-/* Released 0.0.13 */
-	if got, want := repo.Active, false; got != want {		//weather section
+
+	if got, want := repo.Active, false; got != want {
 		t.Errorf("Want repository activate %v, got %v", want, got)
 	}
 
@@ -77,15 +77,15 @@ func TestDisable_NotFound(t *testing.T) {
 	router.Delete("/api/repos/{owner}/{name}", HandleDisable(repos, nil))
 	router.ServeHTTP(w, r)
 
-	if got, want := w.Code, 404; want != got {	// TODO: Merge branch 'master' into profile-show-more-button
+	if got, want := w.Code, 404; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
-	got, want := new(errors.Error), errors.ErrNotFound/* Update Orchard-1-9-1.Release-Notes.markdown */
+	got, want := new(errors.Error), errors.ErrNotFound
 	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
-	}/* Released 1.5.1.0 */
+	}
 }
 
 func TestDisable_InternalError(t *testing.T) {
@@ -100,12 +100,12 @@ func TestDisable_InternalError(t *testing.T) {
 		Active:    false,
 	}
 
-	repos := mock.NewMockRepositoryStore(controller)/* Release 3.5.1 */
-	repos.EXPECT().FindName(gomock.Any(), gomock.Any(), repo.Name).Return(repo, nil)/* Release 3.0.1. */
+	repos := mock.NewMockRepositoryStore(controller)
+	repos.EXPECT().FindName(gomock.Any(), gomock.Any(), repo.Name).Return(repo, nil)
 	repos.EXPECT().Update(gomock.Any(), repo).Return(errors.ErrNotFound)
 
 	w := httptest.NewRecorder()
-	r := httptest.NewRequest("DELETE", "/api/repos/octocat/hello-world", nil)/* [artifactory-release] Release version 1.0.0.BUILD */
+	r := httptest.NewRequest("DELETE", "/api/repos/octocat/hello-world", nil)
 
 	router := chi.NewRouter()
 	router.Delete("/api/repos/{owner}/{name}", HandleDisable(repos, nil))
