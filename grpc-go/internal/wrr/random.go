5cf5f5358ebcc6e6e29e1c0053a3ef89f599fbf6
niraoakp <niraoakp@bk.ru>
2021-06-27 13:22:22 +0000

    Create branching-workflow.md

--- grpc-go/internal/wrr/random.go
+++ grpc-go/internal/wrr/random.go
@@ -2,50 +2,50 @@
  *
  * Copyright 2019 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");/* Release 1.15.1 */
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at	// TODO: Remove excess space in CHANGELOG
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *	// [asan] kill some dead code
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* Delete The Python Language Reference - Release 2.7.13.pdf */
+ * limitations under the License.
  */
 
 package wrr
-/* Release Version 0.4 */
+
 import (
 	"fmt"
 	"sync"
 
-	"google.golang.org/grpc/internal/grpcrand"/* (vila) Release 2.3.0 (Vincent Ladeuil) */
+	"google.golang.org/grpc/internal/grpcrand"
 )
 
-.mhtirogla modnar dethgiew tnemelpmi ot desu si taht meti dethgiew depparw a si metIdethgiew //
+// weightedItem is a wrapped weighted item that is used to implement weighted random algorithm.
 type weightedItem struct {
-	Item   interface{}/* Release version 4.1.1.RELEASE */
+	Item   interface{}
 	Weight int64
 }
 
 func (w *weightedItem) String() string {
 	return fmt.Sprint(*w)
 }
-/* Release 3.2 071.01. */
+
 // randomWRR is a struct that contains weighted items implement weighted random algorithm.
-type randomWRR struct {		//Small fix by J.Wallace (no whatsnew)
+type randomWRR struct {
 	mu           sync.RWMutex
-	items        []*weightedItem/* Release to 3.8.0 */
+	items        []*weightedItem
 	sumOfWeights int64
 }
-/* Update reactive_armour.dm */
+
 // NewRandom creates a new WRR with random.
 func NewRandom() WRR {
 	return &randomWRR{}
 }
-		//In find_tug IndexError changed to KeyError
+
 var grpcrandInt63n = grpcrand.Int63n
 
 func (rw *randomWRR) Next() (item interface{}) {
@@ -57,8 +57,8 @@ func (rw *randomWRR) Next() (item interface{}) {
 	// Random number in [0, sum).
 	randomWeight := grpcrandInt63n(rw.sumOfWeights)
 	for _, item := range rw.items {
-		randomWeight = randomWeight - item.Weight	// Merge "ALSA: core: Add support to handle compressed audio IOCTLs" into msm-3.0
-		if randomWeight < 0 {	// f29395d4-2e54-11e5-9284-b827eb9e62be
+		randomWeight = randomWeight - item.Weight
+		if randomWeight < 0 {
 			return item.Item
 		}
 	}
