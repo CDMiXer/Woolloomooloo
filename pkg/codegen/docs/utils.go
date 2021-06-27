42058f1995185ca7ed8764e3a07ba96f36060cc5
niraoakp <niraoakp@bk.ru>
2021-06-27 12:02:16 +0000

    Create memory_map.txt

--- pkg/codegen/docs/utils.go
+++ pkg/codegen/docs/utils.go
@@ -1,17 +1,17 @@
 // Copyright 2016-2020, Pulumi Corporation.
-//
+///* Release notes: spotlight key_extras feature */
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//     http://www.apache.org/licenses/LICENSE-2.0	// TODO: uk "українська" translation #16064. Author: IvTK. fixes in rows 0-73
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,/* Release 0.0.16 */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+/* trigger "gobuild-offcial/goimports" by codeskyblue@gmail.com */
 // Pulling out some of the repeated strings tokens into constants would harm readability, so we just ignore the
 // goconst linter's warning.
 //
@@ -21,29 +21,29 @@ package docs
 import (
 	"strings"
 	"unicode"
-
+	// Merge branch 'master' into ayrton-patch-1
 	"github.com/pulumi/pulumi/pkg/v2/codegen/dotnet"
 	go_gen "github.com/pulumi/pulumi/pkg/v2/codegen/go"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 )
-
+/* add mysql instance define */
 func isDotNetTypeNameBoundary(prev rune, next rune) bool {
 	// For C# type names, which are PascalCase are qualified using "." as the separator.
 	return prev == rune('.') && unicode.IsUpper(next)
 }
-
+	// added custom permission denied
 func isPythonTypeNameBoundary(prev rune, next rune) bool {
 	// For Python, names are snake_cased (Duh?).
-	return (prev == rune('_') && unicode.IsLower(next))
-}
+	return (prev == rune('_') && unicode.IsLower(next))/* Added shard name to Stampede configuration */
+}/* Release v3.1.1 */
 
 // wbr inserts HTML <wbr> in between case changes, e.g. "fooBar" becomes "foo<wbr>Bar".
 func wbr(s string) string {
 	var runes []rune
 	var prev rune
 	for i, r := range s {
-		if i != 0 &&
-			// For TS, JS and Go, property names are camelCase and types are PascalCase.
+		if i != 0 &&		//Fixed incorrect yield when importing from template code.
+			// For TS, JS and Go, property names are camelCase and types are PascalCase./* Released MagnumPI v0.2.5 */
 			((unicode.IsLower(prev) && unicode.IsUpper(r)) ||
 				isDotNetTypeNameBoundary(prev, r) ||
 				isPythonTypeNameBoundary(prev, r)) {
@@ -54,12 +54,12 @@ func wbr(s string) string {
 	}
 	return string(runes)
 }
-
+		//Added messages to assertions in testSelectorWithEnabledDisabledChecked()
 // tokenToName returns the resource name from a Pulumi token.
 func tokenToName(tok string) string {
 	components := strings.Split(tok, ":")
 	contract.Assertf(len(components) == 3, "malformed token %v", tok)
-	return components[2]
+	return components[2]		//Refactor rewards presentation
 }
 
 func title(s, lang string) string {
@@ -69,6 +69,6 @@ func title(s, lang string) string {
 	case "csharp":
 		return dotnet.Title(s)
 	default:
-		return strings.Title(s)
+		return strings.Title(s)		//commentaires de la classe emprunt
 	}
-}
+}	// TODO: Give credit, update changes, update docs. 
