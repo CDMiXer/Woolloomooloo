6de551db208e973b74434afbe21a7fa0dbaca854
niraoakp <niraoakp@bk.ru>
2021-06-27 09:47:35 +0000

    Make services public

--- operator/runner/secrets.go
+++ operator/runner/secrets.go
@@ -1,25 +1,25 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Release TomcatBoot-0.3.3 */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Set the default build type to Release. Integrate speed test from tinyformat. */
+// You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* Merge "Release notes for server-side env resolution" */
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* asked how are you */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package runner
-	// TODO: will be fixed by arachnid@notdot.net
+	// TODO: will be fixed by alan.shaw@protocol.ai
 import "github.com/drone/drone/core"
-
+	// TODO: Update pvkiiserver
 func toSecretMap(secrets []*core.Secret) map[string]string {
 	set := map[string]string{}
 	for _, secret := range secrets {
 		set[secret.Name] = secret.Data
 	}
-	return set	// TODO: Current code, before I get nuts with people and Christmas ...
+	return set
 }
