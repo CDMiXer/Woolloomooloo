98c878e73b6b4827fb09abe9b3d400b8cac7201a
niraoakp <niraoakp@bk.ru>
2021-06-27 15:55:26 +0000

    Updating build-info/dotnet/wcf/master for preview3-26414-01

--- tests/integration/aliases/python/retype_component/step1/__main__.py
+++ tests/integration/aliases/python/retype_component/step1/__main__.py
@@ -3,7 +3,7 @@
 from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE
 
 class Resource1(ComponentResource):
-    def __init__(self, name, opts=None):/* Release Version 1.0 */
+    def __init__(self, name, opts=None):
         super().__init__("my:module:Resource", name, None, opts)
 
 # Scenario #4 - change the type of a component
