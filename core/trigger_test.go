04bb3f60f2859a40c9a2e1113c2d533648892fd8
niraoakp <niraoakp@bk.ru>
2021-06-27 20:26:07 +0000

    chore(docs): create project-structure docs

--- core/trigger_test.go
+++ core/trigger_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Release 2.1.15 */
-// Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
-		//8e128008-2e5d-11e5-9284-b827eb9e62be
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License/* Release to Github as Release instead of draft */
+// that can be found in the LICENSE file./* 552cf0a8-2e70-11e5-9284-b827eb9e62be */
+
 // +build !oss
 
-package core/* Delete Group-Lab.cfg */
+package core
