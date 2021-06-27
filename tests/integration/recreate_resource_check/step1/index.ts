00d4db4db8f0f377e388f9f55ed057e03b9316c6
niraoakp <niraoakp@bk.ru>
2021-06-27 21:38:15 +0000

    Merge "power: smb1360: Add an API to allocate OTP backup registers"

--- tests/integration/recreate_resource_check/step1/index.ts
+++ tests/integration/recreate_resource_check/step1/index.ts
@@ -1,8 +1,8 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import { Resource } from "./resource";
-/* PreRelease fixes */
-// Base depends on nothing.	// adding switches
+
+// Base depends on nothing.
 const a = new Resource("base", { uniqueKey: 1, state: 99 });
 
 // Dependent depends on Base with state 99.
