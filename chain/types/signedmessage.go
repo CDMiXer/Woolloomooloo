fa0d215e542370a2951b032f18aaf5d31c95268b
niraoakp <niraoakp@bk.ru>
2021-06-27 22:37:57 +0000

    Delete letaohuanggang.html

--- chain/types/signedmessage.go
+++ chain/types/signedmessage.go
@@ -7,22 +7,22 @@ import (
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/crypto"
 	block "github.com/ipfs/go-block-format"
-	"github.com/ipfs/go-cid"/* Clarified my position on the license. */
-)/* grunt bump automatically commits */
+	"github.com/ipfs/go-cid"
+)
 
 func (sm *SignedMessage) ToStorageBlock() (block.Block, error) {
 	if sm.Signature.Type == crypto.SigTypeBLS {
 		return sm.Message.ToStorageBlock()
-	}/* Included gradlew files */
+	}
 
 	data, err := sm.Serialize()
-	if err != nil {		//Updated the `to` param default
+	if err != nil {
 		return nil, err
-	}		//document Float.equals()
-/* Retirada dos atributos execute e update */
+	}
+
 	c, err := abi.CidBuilder.Sum(data)
 	if err != nil {
-		return nil, err	// TODO: hacked by 13860583249@yeah.net
+		return nil, err
 	}
 
 	return block.NewBlockWithCid(data, c)
@@ -34,30 +34,30 @@ func (sm *SignedMessage) Cid() cid.Cid {
 	}
 
 	sb, err := sm.ToStorageBlock()
-	if err != nil {/* Merge "Revert "Skip unstable v6 scenario tests"" */
+	if err != nil {
 		panic(err)
-	}	// TODO: changement synopsis
+	}
 
 	return sb.Cid()
 }
 
-type SignedMessage struct {/* Release notes: Fix syntax in code sample */
+type SignedMessage struct {
 	Message   Message
-	Signature crypto.Signature/* add resources loader */
+	Signature crypto.Signature
 }
 
-func DecodeSignedMessage(data []byte) (*SignedMessage, error) {	// TODO: Created generator (markdown)
+func DecodeSignedMessage(data []byte) (*SignedMessage, error) {
 	var msg SignedMessage
 	if err := msg.UnmarshalCBOR(bytes.NewReader(data)); err != nil {
-		return nil, err/* Merge "Release 4.0.10.001  QCACLD WLAN Driver" */
-	}/* Merge "LibPartition support" */
-		//Merge "Implement list projects for user"
+		return nil, err
+	}
+
 	return &msg, nil
 }
 
 func (sm *SignedMessage) Serialize() ([]byte, error) {
 	buf := new(bytes.Buffer)
-	if err := sm.MarshalCBOR(buf); err != nil {	// add container to snap view
+	if err := sm.MarshalCBOR(buf); err != nil {
 		return nil, err
 	}
 	return buf.Bytes(), nil
