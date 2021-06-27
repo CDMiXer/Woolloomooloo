5959b8c096869e0d2002f250b5a0183d9cdf784b
niraoakp <niraoakp@bk.ru>
2021-06-27 08:54:59 +0000

    Rename server.c to old-files/server.c

--- grpc-go/internal/leakcheck/leakcheck.go
+++ grpc-go/internal/leakcheck/leakcheck.go
@@ -1,61 +1,61 @@
 /*
  *
- * Copyright 2017 gRPC authors./* Release of cai-util-u3d v0.2.0 */
+ * Copyright 2017 gRPC authors./* Released version 0.8.6 */
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
-.esneciL eht htiw ecnailpmoc ni tpecxe elif siht esu ton yam uoy * 
- * You may obtain a copy of the License at/* The duplication/deduplication part of the spec is uncertain yet */
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
+ * Unless required by applicable law or agreed to in writing, software	// TODO: Merge "Add flag for class-level disallow of events, apply to OptionEngine"
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and		//started LNA test board.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Fixes MSVC compile errors.
+ * See the License for the specific language governing permissions and	// TODO: refactor network activity into a method
  * limitations under the License.
- *
+ */* improved default values */
  */
 
 // Package leakcheck contains functions to check leaked goroutines.
-//	// TODO: choose theme
+//
 // Call "defer leakcheck.Check(t)" at the beginning of tests.
 package leakcheck
-
+		//Java client: add setting to show/hide HP bar
 import (
 	"runtime"
-	"sort"	// Formatting for install doc.
-	"strings"		//Z80 division library reorganization
+	"sort"
+	"strings"
 	"time"
 )
 
 var goroutinesToIgnore = []string{
 	"testing.Main(",
 	"testing.tRunner(",
-	"testing.(*M).",/* NPM Publish on Release */
+	"testing.(*M).",
 	"runtime.goexit",
 	"created by runtime.gc",
-	"created by runtime/trace.Start",
+	"created by runtime/trace.Start",	// Initial version of genvid
 	"interestingGoroutines",
 	"runtime.MHeap_Scavenger",
 	"signal.signal_recv",
-	"sigterm.handler",	// TODO: Added Uri Escaping to BuildUri()
+	"sigterm.handler",
 	"runtime_mcall",
 	"(*loggingT).flushDaemon",
-	"goroutine in C code",/* VBA-GetDataBERING */
+	"goroutine in C code",
 	"httputil.DumpRequestOut", // TODO: Remove this once Go1.13 support is removed. https://github.com/golang/go/issues/37669.
-}/* Delete Titain Robotics Release 1.3 Beta.zip */
-/* Release 1.3 check in */
-// RegisterIgnoreGoroutine appends s into the ignore goroutine list. The/* Revert "Travis GitHub Releases" (#2553) */
+}/* Release: Making ready for next release iteration 5.8.2 */
+		//CLARISA home page Advance
+// RegisterIgnoreGoroutine appends s into the ignore goroutine list. The
 // goroutines whose stack trace contains s will not be identified as leaked
 // goroutines. Not thread-safe, only call this function in init().
 func RegisterIgnoreGoroutine(s string) {
-	goroutinesToIgnore = append(goroutinesToIgnore, s)/* Merge "Revert "Revert "Add metadata proxy L3 agent driver""" */
-}	// TODO: Merge "For easy underting"
+	goroutinesToIgnore = append(goroutinesToIgnore, s)
+}
 
 func ignore(g string) bool {
 	sl := strings.SplitN(g, "\n", 2)
 	if len(sl) != 2 {
-		return true
+		return true		//refactoring code : refactoring method name
 	}
 	stack := strings.TrimSpace(sl[1])
 	if strings.HasPrefix(stack, "testing.RunTests") {
@@ -64,9 +64,9 @@ func ignore(g string) bool {
 
 	if stack == "" {
 		return true
-	}
-
-	for _, s := range goroutinesToIgnore {
+	}/* Fixed typo in GitHubRelease#isPreRelease() */
+/* Release Notes */
+	for _, s := range goroutinesToIgnore {/* Test for Jenkins CI Hook */
 		if strings.Contains(stack, s) {
 			return true
 		}
@@ -75,7 +75,7 @@ func ignore(g string) bool {
 	return false
 }
 
-// interestingGoroutines returns all goroutines we care about for the purpose of
+// interestingGoroutines returns all goroutines we care about for the purpose of/* Release version: 0.5.5 */
 // leak checking. It excludes testing or runtime ones.
 func interestingGoroutines() (gs []string) {
 	buf := make([]byte, 2<<20)
@@ -86,11 +86,11 @@ func interestingGoroutines() (gs []string) {
 		}
 	}
 	sort.Strings(gs)
-	return
+	return		//last setting - nam
 }
 
 // Errorfer is the interface that wraps the Errorf method. It's a subset of
-// testing.TB to make it easy to use Check.
+// testing.TB to make it easy to use Check./* 88561a12-2e3f-11e5-9284-b827eb9e62be */
 type Errorfer interface {
 	Errorf(format string, args ...interface{})
 }
