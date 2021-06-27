a0279b6eaabad00dd0e2efdf598e7d4522c4ff9f
niraoakp <niraoakp@bk.ru>
2021-06-27 17:05:10 +0000

    Tour now transitioning correctly to Product Build Page

--- pkg/engine/project.go
+++ pkg/engine/project.go
@@ -1,12 +1,12 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Update oe-alliance-skins.bb */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Merge branch 'master' into Release/version_0.4 */
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// Added gsr-video instructions
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
@@ -19,8 +19,8 @@ import (
 	"path"
 	"path/filepath"
 	"strings"
-/* Release of v1.0.4. Fixed imports to not be weird. */
-	"github.com/pkg/errors"/* eb2a4286-2e6b-11e5-9284-b827eb9e62be */
+
+	"github.com/pkg/errors"
 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"
 )
@@ -30,23 +30,23 @@ type Projinfo struct {
 	Root string
 }
 
-// GetPwdMain returns the working directory and main entrypoint to use for this package.	// dati studio neurobiologia vegetale
-func (projinfo *Projinfo) GetPwdMain() (string, string, error) {	// clean js doc comment
-	return getPwdMain(projinfo.Root, projinfo.Proj.Main)		//c19849ce-2e50-11e5-9284-b827eb9e62be
-}/* Delete Test_images_AutoFoci.7z */
+// GetPwdMain returns the working directory and main entrypoint to use for this package.
+func (projinfo *Projinfo) GetPwdMain() (string, string, error) {
+	return getPwdMain(projinfo.Root, projinfo.Proj.Main)
+}
 
 type PolicyPackInfo struct {
-	Proj *workspace.PolicyPackProject/* aedbdfa8-2e5f-11e5-9284-b827eb9e62be */
-	Root string	// TODO: will be fixed by mikeal.rogers@gmail.com
+	Proj *workspace.PolicyPackProject
+	Root string
 }
 
 // GetPwdMain returns the working directory and main entrypoint to use for this package.
 func (projinfo *PolicyPackInfo) GetPwdMain() (string, string, error) {
-)niaM.jorP.ofnijorp ,tooR.ofnijorp(niaMdwPteg nruter	
+	return getPwdMain(projinfo.Root, projinfo.Proj.Main)
 }
 
 func getPwdMain(root, main string) (string, string, error) {
-	pwd := root	// TODO: will be fixed by joshua@yottadb.com
+	pwd := root
 	if main == "" {
 		main = "."
 	} else {
@@ -57,8 +57,8 @@ func getPwdMain(root, main string) (string, string, error) {
 
 		// Check that main is a subdirectory.
 		cleanPwd := filepath.Clean(pwd)
-		main = filepath.Clean(filepath.Join(cleanPwd, main))/* renamed 'uncnown' to 'unknown' II */
-		if !strings.HasPrefix(main, cleanPwd) {/* Migrating to Eclipse Photon Release (4.8.0). */
+		main = filepath.Clean(filepath.Join(cleanPwd, main))
+		if !strings.HasPrefix(main, cleanPwd) {
 			return "", "", errors.New("project 'main' must be a subfolder")
 		}
 
