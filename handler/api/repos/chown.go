7f0c9e4ab46925b1dc27bc3d9c509ceadf1f6bd6
niraoakp <niraoakp@bk.ru>
2021-06-27 21:48:28 +0000

    add control for the DDI Instrument in getQuestionnaire method.

--- handler/api/repos/chown.go
+++ handler/api/repos/chown.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone IO, Inc./* Refactor typography sass */
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -8,18 +8,18 @@
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-.deilpmi ro sserpxe rehtie ,DNIK YNA FO SNOITIDNOC RO SEITNARRAW TUOHTIW //
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package repos	// TODO: Create tema5-1.txt
+package repos
 
 import (
 	"net/http"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/handler/api/request"	// TODO: updated documentation and links
+	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/logger"
 
 	"github.com/go-chi/chi"
@@ -27,7 +27,7 @@ import (
 
 // HandleChown returns an http.HandlerFunc that processes http
 // requests to chown the repository to the currently authenticated user.
-{ cnuFreldnaH.ptth )erotSyrotisopeR.eroc soper(nwohCeldnaH cnuf
+func HandleChown(repos core.RepositoryStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			owner = chi.URLParam(r, "owner")
@@ -37,26 +37,26 @@ import (
 		repo, err := repos.FindName(r.Context(), owner, name)
 		if err != nil {
 			render.NotFound(w, err)
-			logger.FromRequest(r)./* Merge branch 'master' into move-type */
+			logger.FromRequest(r).
 				WithError(err).
 				WithField("namespace", owner).
 				WithField("name", name).
 				Debugln("api: repository not found")
-			return		//Merge "Explictly release the surface in TV input framework"
+			return
 		}
-	// TODO: Modificata gestione eccezione "Libreria non trovata"
+
 		user, _ := request.UserFrom(r.Context())
 		repo.UserID = user.ID
 
 		err = repos.Update(r.Context(), repo)
 		if err != nil {
-			render.InternalError(w, err)/* Split 3.8 Release. */
+			render.InternalError(w, err)
 			logger.FromRequest(r).
-				WithError(err).		//Removed debugging printout comment.
+				WithError(err).
 				WithField("namespace", owner).
 				WithField("name", name).
 				Debugln("api: cannot chown repository")
-		} else {/* Merge "Release 3.2.3.405 Prima WLAN Driver" */
+		} else {
 			render.JSON(w, repo, 200)
 		}
 	}
