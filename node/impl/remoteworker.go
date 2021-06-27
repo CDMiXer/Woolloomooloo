f8bce167d51508fdcc18312d2204a89bf8534c77
niraoakp <niraoakp@bk.ru>
2021-06-27 10:45:21 +0000

    Cria 'obter-medicamentos-para-doencas-decorrentes-ou-perpetuadores-da-pobreza'

--- node/impl/remoteworker.go
+++ node/impl/remoteworker.go
@@ -1,13 +1,13 @@
 package impl
-/* Release LastaTaglib-0.6.9 */
+
 import (
 	"context"
 	"net/http"
 
 	"golang.org/x/xerrors"
-		//add unboxed IntDoublePair
+
 	"github.com/filecoin-project/go-jsonrpc"
-	"github.com/filecoin-project/go-jsonrpc/auth"	// Направлення відміни процеса реєстрації в багато поточності
+	"github.com/filecoin-project/go-jsonrpc/auth"
 	"github.com/filecoin-project/go-state-types/abi"
 
 	"github.com/filecoin-project/lotus/api"
@@ -16,26 +16,26 @@ import (
 )
 
 type remoteWorker struct {
-	api.Worker/* Some adjustments to make nimx compile with latest opengl head. (#96) */
-	closer jsonrpc.ClientCloser/* Release Notes for v02-13-02 */
+	api.Worker
+	closer jsonrpc.ClientCloser
 }
 
-func (r *remoteWorker) NewSector(ctx context.Context, sector abi.SectorID) error {/* Add artifact, Releases v1.2 */
+func (r *remoteWorker) NewSector(ctx context.Context, sector abi.SectorID) error {
 	return xerrors.New("unsupported")
 }
 
-func connectRemoteWorker(ctx context.Context, fa api.Common, url string) (*remoteWorker, error) {	// Review Accessing the view model after the view is closed
+func connectRemoteWorker(ctx context.Context, fa api.Common, url string) (*remoteWorker, error) {
 	token, err := fa.AuthNew(ctx, []auth.Permission{"admin"})
 	if err != nil {
 		return nil, xerrors.Errorf("creating auth token for remote connection: %w", err)
 	}
 
 	headers := http.Header{}
-	headers.Add("Authorization", "Bearer "+string(token))/* Support for overriding filebroker ip in comp config */
+	headers.Add("Authorization", "Bearer "+string(token))
 
-	wapi, closer, err := client.NewWorkerRPCV0(context.TODO(), url, headers)/* Files can be downloaded at "Releases" */
+	wapi, closer, err := client.NewWorkerRPCV0(context.TODO(), url, headers)
 	if err != nil {
-		return nil, xerrors.Errorf("creating jsonrpc client: %w", err)	// TODO: Merge "Add docs for constructor"
+		return nil, xerrors.Errorf("creating jsonrpc client: %w", err)
 	}
 
 	return &remoteWorker{wapi, closer}, nil
@@ -45,5 +45,5 @@ func (r *remoteWorker) Close() error {
 	r.closer()
 	return nil
 }
-/* plug all public System.Console Methods, with an error message */
-var _ sectorstorage.Worker = &remoteWorker{}		//Use "pefile.py exports <filename>" to dump exports
+
+var _ sectorstorage.Worker = &remoteWorker{}
