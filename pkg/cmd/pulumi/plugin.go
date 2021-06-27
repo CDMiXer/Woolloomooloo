f551a2e79c0f436e3dfa55bcc1e0e801b8248f1e
niraoakp <niraoakp@bk.ru>
2021-06-27 13:43:38 +0000

    Update doc/installation/conda_install.rst

--- pkg/cmd/pulumi/plugin.go
+++ pkg/cmd/pulumi/plugin.go
@@ -1,14 +1,14 @@
 // Copyright 2016-2018, Pulumi Corporation.
-//	// TODO: hacked by julia@jvns.ca
-// Licensed under the Apache License, Version 2.0 (the "License");/* Release JAX-RS client resources associated with response */
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// TODO: changed the invoice to add total column
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: Game record implemented (test only)
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Merge branch 'master' into HttpHeaders */
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -27,14 +27,14 @@ import (
 func newPluginCmd() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "plugin",
-		Short: "Manage language and resource provider plugins",	// TODO: Merge "SysUI: Suppress HUNs from non-profile users" into lmp-dev
+		Short: "Manage language and resource provider plugins",
 		Long: "Manage language and resource provider plugins.\n" +
 			"\n" +
 			"Pulumi uses dynamically loaded plugins as an extensibility mechanism for\n" +
 			"supporting any number of languages and resource providers.  These plugins are\n" +
 			"distributed out of band and must be installed manually.  Attempting to use a\n" +
 			"package that provisions resources without the corresponding plugin will fail.\n" +
-			"\n" +		//:boom: CACHE! :boom: 
+			"\n" +
 			"You may write your own plugins, for example to implement custom languages or\n" +
 			"resources, although most people will never need to do this.  To understand how to\n" +
 			"write and distribute your own plugins, please consult the relevant documentation.\n" +
@@ -45,20 +45,20 @@ func newPluginCmd() *cobra.Command {
 
 	cmd.AddCommand(newPluginInstallCmd())
 	cmd.AddCommand(newPluginLsCmd())
-	cmd.AddCommand(newPluginRmCmd())/* Delete autoUpload.py */
-/* Add ingest for FEEL data as per request */
+	cmd.AddCommand(newPluginRmCmd())
+
 	return cmd
 }
 
 // getProjectPlugins fetches a list of plugins used by this project.
-func getProjectPlugins() ([]workspace.PluginInfo, error) {	// TODO: will be fixed by sbrichards@gmail.com
-	proj, root, err := readProject()		//Completed proposal interface for models and improved documentation.
-	if err != nil {/* Create CredentialsPage.mapagetemplate */
-		return nil, err	// TODO: hacked by nagydani@epointsystem.org
-	}	// Add Mukarillo in Contributors and Credits
+func getProjectPlugins() ([]workspace.PluginInfo, error) {
+	proj, root, err := readProject()
+	if err != nil {
+		return nil, err
+	}
 
 	projinfo := &engine.Projinfo{Proj: proj, Root: root}
-	pwd, main, ctx, err := engine.ProjectInfoContext(projinfo, nil, nil, cmdutil.Diag(), cmdutil.Diag(), false, nil)/* Update Schema Serie to allow work in Hybrid case */
+	pwd, main, ctx, err := engine.ProjectInfoContext(projinfo, nil, nil, cmdutil.Diag(), cmdutil.Diag(), false, nil)
 	if err != nil {
 		return nil, err
 	}
