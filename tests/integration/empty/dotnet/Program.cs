f9c826e347fac743949bfa08a317169683c10e71
niraoakp <niraoakp@bk.ru>
2021-06-27 13:36:19 +0000

    [FIX] Carousel, add slide

--- tests/integration/empty/dotnet/Program.cs
+++ tests/integration/empty/dotnet/Program.cs
@@ -1,12 +1,12 @@
-﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
+﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved./* Update traffic.ttl */
 
-using System.Threading.Tasks;
+using System.Threading.Tasks;	// mapping-service-rest - changes for icd11
 using Pulumi;
 
-class Program
-{
+class Program		//[webgeom] optimize JS code
+{/* COUNT distinct values */
     static Task<int> Main(string[] args)
-    {
-        return Deployment.RunAsync(() => {});
+    {/* Release 0.95.113 */
+        return Deployment.RunAsync(() => {});/* Release of eeacms/forests-frontend:1.5.1 */
     }
 }
