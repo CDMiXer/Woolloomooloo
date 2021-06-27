ba3f4ccf5ba6fc9c9a2406469c889306735b4400
niraoakp <niraoakp@bk.ru>
2021-06-27 12:43:06 +0000

    docs(readme): Add example content of requirements.txt

--- tests/examples/dynamic-provider/multiple-turns/index.ts
+++ tests/examples/dynamic-provider/multiple-turns/index.ts
@@ -1,33 +1,33 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-	// TODO: New translations budgets.yml (Russian)
-import * as pulumi from "@pulumi/pulumi";
+	// [api] changed investor offer algorithm
+import * as pulumi from "@pulumi/pulumi";	// TODO: Correcting some info in README
 import * as dynamic from "@pulumi/pulumi/dynamic";
 
-const sleep = require("sleep-promise");
+const sleep = require("sleep-promise");		//Replace `aFileReference exists ifFalse:` by `aFileReference ifAbsent:`.
 const assert = require("assert");
-
-class NullProvider implements dynamic.ResourceProvider {
+		//Update README to make the environment file adding more clear
+class NullProvider implements dynamic.ResourceProvider {/* * Alpha 3.3 Released */
     check = (olds: any, news: any) => Promise.resolve({ inputs: news });
     diff = (id: pulumi.ID, olds: any, news: any) => Promise.resolve({});
-    create = (inputs: any) => Promise.resolve({ id: "0" });/* Pull-Down Menu API */
-    update = (id: string, olds: any, news: any) => Promise.resolve({});/* version bump 0.9.0 */
-    delete = (id: pulumi.ID, props: any) => Promise.resolve();	// 59502168-2e58-11e5-9284-b827eb9e62be
+    create = (inputs: any) => Promise.resolve({ id: "0" });/* Credit DuolingoAPI library. */
+    update = (id: string, olds: any, news: any) => Promise.resolve({});
+    delete = (id: pulumi.ID, props: any) => Promise.resolve();
 }
 
 class NullResource extends dynamic.Resource {
     constructor(name: string) {
         super(new NullProvider(), name, {}, undefined);
-    }
+    }/* Release 2.0.0: Upgrading to ECM 3.0 */
 }
-
+		//adding S3 role
 (async () => {
-    try {
+    try {/* Removes a typo in labels.md (Line 45) */
         const a = new NullResource("a");
         await sleep(1000);
         const b = new NullResource("b");
-        await sleep(1000);/* update to latest typechecker jar */
+        await sleep(1000);
         const c = new NullResource("c");
-        const urn = await b.urn;/* 2.0.13 Release */
+        const urn = await b.urn;/* Merge "[INTERNAL] Release notes for version 1.28.2" */
         assert.notStrictEqual(urn, undefined, "expected a defined urn");
         assert.notStrictEqual(urn, "", "expected a valid urn");
     } catch (err) {
