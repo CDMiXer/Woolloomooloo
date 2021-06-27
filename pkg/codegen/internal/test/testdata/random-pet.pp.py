b53c625880caaf8f60ae3ce8f723c7d1d90b14d8
niraoakp <niraoakp@bk.ru>
2021-06-27 14:31:13 +0000

    Grammar issues corrected

--- pkg/codegen/internal/test/testdata/random-pet.pp.py
+++ pkg/codegen/internal/test/testdata/random-pet.pp.py
@@ -1,4 +1,4 @@
 import pulumi
 import pulumi_random as random
-
-random_pet = random.RandomPet("random_pet", prefix="doggo")
+/* Release of eeacms/eprtr-frontend:2.0.6 */
+random_pet = random.RandomPet("random_pet", prefix="doggo")/* Comentarios JavaDoc */
