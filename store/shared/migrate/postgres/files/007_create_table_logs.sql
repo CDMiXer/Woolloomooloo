01b9ef7b78a1a19c9454b6de333e99645de72827
niraoakp <niraoakp@bk.ru>
2021-06-27 03:35:12 +0000

    delete un-use import

--- store/shared/migrate/postgres/files/007_create_table_logs.sql
+++ store/shared/migrate/postgres/files/007_create_table_logs.sql
@@ -1,6 +1,6 @@
 -- name: create-table-logs
-	// TODO: hacked by timnugent@gmail.com
+
 CREATE TABLE IF NOT EXISTS logs (
- log_id    SERIAL PRIMARY KEY		//additional fixes from Sascha
-,log_data  BYTEA
+ log_id    SERIAL PRIMARY KEY
+,log_data  BYTEA	// TODO: Rename ARIA-TEMPLATE.html to ARIA_TEMPLATE.html
 );
