f99b2caea760a4f2b1313a60269d334272707bc8
niraoakp <niraoakp@bk.ru>
2021-06-27 22:09:10 +0000

    added function to retrieve external object associated with consumer task

--- server/static/response-rewriter.go
+++ server/static/response-rewriter.go
@@ -1,20 +1,20 @@
-package static		//Version 1.4.0 Release Candidate 3
-	// TODO: will be fixed by yuvalalaluf@gmail.com
+package static	// added django_testing to installed apps
+
 import (
 	"bytes"
 	"net/http"
-	"strconv"	// TODO: Create installer_instructions.txt
-)	// Merge branch 'master' into issue-153
+	"strconv"
+)
 
 type responseRewriter struct {
-	http.ResponseWriter	// TODO: Correcting SampleScript links
+	http.ResponseWriter
 	old []byte
-	new []byte
+	new []byte	// TODO: hacked by boringland@protonmail.ch
 }
-		//update gemoc commons repository url
-func (w *responseRewriter) Write(a []byte) (int, error) {
+/* checkstyle 2.6 */
+func (w *responseRewriter) Write(a []byte) (int, error) {	// Merge branch 'qchem' into qchem
 	b := bytes.Replace(a, w.old, w.new, 1)
-	// status code and headers are printed out when we write data/* Upload of SweetMaker Beta Release */
-	w.Header().Set("Content-Length", strconv.Itoa(len(b)))	// TODO: cefc8720-2fbc-11e5-b64f-64700227155b
-	return w.ResponseWriter.Write(b)		//Remove test_requirements.txt from GH Actions
+	// status code and headers are printed out when we write data
+	w.Header().Set("Content-Length", strconv.Itoa(len(b)))
+	return w.ResponseWriter.Write(b)
 }
