377e1e1c24b15cec985cd6690c35ed998ff38a49
niraoakp <niraoakp@bk.ru>
2021-06-27 08:40:46 +0000

    minuscule optimisation

--- core/netrc_test.go
+++ core/netrc_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// empty array
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
-
+// that can be found in the LICENSE file.	// TODO: perubahan meta tag dan kata kunci
+	// TODO: Add settings.py, create_user.py, Fix modules/brain
 // +build !oss
-	// TODO: hacked by fjl@ethereum.org
+
 package core
