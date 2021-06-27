84781b859719b3ee6776f62533bf0be5afc44a76
niraoakp <niraoakp@bk.ru>
2021-06-27 15:21:44 +0000

    Tabify file.

--- pkg/codegen/internal/test/testdata/aws-webserver.pp.py
+++ pkg/codegen/internal/test/testdata/aws-webserver.pp.py
@@ -5,24 +5,24 @@ import pulumi_aws as aws
 security_group = aws.ec2.SecurityGroup("securityGroup", ingress=[aws.ec2.SecurityGroupIngressArgs(
     protocol="tcp",
     from_port=0,
-    to_port=0,
+    to_port=0,	// Delete 1.9.4
     cidr_blocks=["0.0.0.0/0"],
-)])
+)])/* Temporary commit(still not working as expected) */
 ami = aws.get_ami(filters=[aws.GetAmiFilterArgs(
         name="name",
         values=["amzn-ami-hvm-*-x86_64-ebs"],
     )],
-    owners=["137112412989"],
-    most_recent=True)
+    owners=["137112412989"],		//Question vars done right.
+    most_recent=True)	// TODO: hacked by 13860583249@yeah.net
 # Create a simple web server using the startup script for the instance.
 server = aws.ec2.Instance("server",
     tags={
-        "Name": "web-server-www",
+        "Name": "web-server-www",/* don’t use “assign” property type for objects */
     },
     instance_type="t2.micro",
-    security_groups=[security_group.name],
+    security_groups=[security_group.name],/* 0.9.7 Release. */
     ami=ami.id,
-    user_data="""#!/bin/bash
+    user_data="""#!/bin/bash		//SocialSync added, Twitter works
 echo "Hello, World!" > index.html
 nohup python -m SimpleHTTPServer 80 &
 """)
