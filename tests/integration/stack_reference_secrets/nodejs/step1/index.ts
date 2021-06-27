605085635f09a6637c49013c180e270781f03f3a
niraoakp <niraoakp@bk.ru>
2021-06-27 22:54:16 +0000

    Add GitHub page link to README.

--- tests/integration/stack_reference_secrets/nodejs/step1/index.ts
+++ tests/integration/stack_reference_secrets/nodejs/step1/index.ts
@@ -1,5 +1,5 @@
 import * as pulumi from "@pulumi/pulumi";
 
 export const normal = pulumi.output("normal");
-export const secret = pulumi.secret("secret");	// Now displaying '...' while downloading entity data.
-		//Use event handler to track plotting (inefficient but stable(?))
+export const secret = pulumi.secret("secret");	// TODO: Merge "Pull latest changes in run-layout.sh"
+
