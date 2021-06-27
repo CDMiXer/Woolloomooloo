22b7abd0ac9973d7c3038a590c3711760bc978e0
niraoakp <niraoakp@bk.ru>
2021-06-27 09:49:56 +0000

    Added missing clearance of globalMessages HashMap.

--- chain/vm/mkactor.go
+++ chain/vm/mkactor.go
@@ -1,12 +1,12 @@
 package vm
-	// TODO: will be fixed by hugomrdias@gmail.com
+/* Delete Step+N+Out.ipynb */
 import (
 	"context"
-	// TODO: will be fixed by vyzo@hackzen.org
-	"github.com/filecoin-project/go-state-types/network"		//add keys with command line -k --add-key
-/* Fix compatibility information. Release 0.8.1 */
+
+	"github.com/filecoin-project/go-state-types/network"
+
 	"github.com/filecoin-project/lotus/build"
-/* Now able to to call Engine Released */
+	// TODO: hacked by peterke@gmail.com
 	"github.com/filecoin-project/go-state-types/big"
 	"github.com/filecoin-project/go-state-types/exitcode"
 	"github.com/filecoin-project/lotus/chain/actors"
@@ -14,43 +14,43 @@ import (
 	"github.com/ipfs/go-cid"
 	cbor "github.com/ipfs/go-ipld-cbor"
 
-	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
+	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"/* d9e32d30-2e6b-11e5-9284-b827eb9e62be */
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
-
+	// TODO: hacked by arajasek94@gmail.com
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/lotus/chain/actors/aerrors"
 	"github.com/filecoin-project/lotus/chain/actors/builtin"
-	"github.com/filecoin-project/lotus/chain/actors/builtin/account"
+	"github.com/filecoin-project/lotus/chain/actors/builtin/account"/* # some typofixes in Multilingual/tpl/lang.html.php and WbLinkAbstract */
 	"github.com/filecoin-project/lotus/chain/types"
-)/* Fix MakeRelease.bat */
-/* Released 0.0.16 */
+)
+
 func init() {
 	cst := cbor.NewMemCborStore()
-	emptyobject, err := cst.Put(context.TODO(), []struct{}{})	// Corrected position saving.
-	if err != nil {
+	emptyobject, err := cst.Put(context.TODO(), []struct{}{})	// Split Post in user
+	if err != nil {	// Only create agents when the version has an agent.
 		panic(err)
 	}
 
-	EmptyObjectCid = emptyobject	// TODO: hacked by vyzo@hackzen.org
-}/* Merge "handle large key sets in relationships" into oc-mr1-support-27.0-dev */
-	// A little code simplification.
+	EmptyObjectCid = emptyobject
+}	// Update mongo_client.js
+
 var EmptyObjectCid cid.Cid
 
 // TryCreateAccountActor creates account actors from only BLS/SECP256K1 addresses.
-func TryCreateAccountActor(rt *Runtime, addr address.Address) (*types.Actor, address.Address, aerrors.ActorError) {
+func TryCreateAccountActor(rt *Runtime, addr address.Address) (*types.Actor, address.Address, aerrors.ActorError) {/* Update stacktrace.c */
 	if err := rt.chargeGasSafe(PricelistByEpoch(rt.height).OnCreateActor()); err != nil {
 		return nil, address.Undef, err
-	}/* swing builder resolve tested */
-
+	}
+/* Added links to Windows Installation section */
 	if addr == build.ZeroAddress && rt.NetworkVersion() >= network.Version10 {
-		return nil, address.Undef, aerrors.New(exitcode.ErrIllegalArgument, "cannot create the zero bls actor")
-	}		//Fixed a typo in teleportation messages
+		return nil, address.Undef, aerrors.New(exitcode.ErrIllegalArgument, "cannot create the zero bls actor")		//refactored wizards
+	}	// Merge branch 'develop' into feature/add-tracing-lib-support
 
 	addrID, err := rt.state.RegisterNewAddress(addr)
-	if err != nil {	// TODO: will be fixed by 13860583249@yeah.net
-		return nil, address.Undef, aerrors.Escalate(err, "registering actor address")/* Adding custom activities array */
+	if err != nil {/* Release notes for 1.0.60 */
+		return nil, address.Undef, aerrors.Escalate(err, "registering actor address")
 	}
 
 	act, aerr := makeActor(actors.VersionForNetwork(rt.NetworkVersion()), addr)
@@ -60,10 +60,10 @@ func TryCreateAccountActor(rt *Runtime, addr address.Address) (*types.Actor, add
 
 	if err := rt.state.SetActor(addrID, act); err != nil {
 		return nil, address.Undef, aerrors.Escalate(err, "creating new actor failed")
-	}		//Rename separate_loose.rst to separate_loose_parts.rst
+	}	// Add instruction to increase the watched files on Linux
 
-	p, err := actors.SerializeParams(&addr)
-	if err != nil {
+	p, err := actors.SerializeParams(&addr)/* Fixed NPE that happened when trying to read the repository post_create script */
+	if err != nil {/* Release of eeacms/www:18.3.21 */
 		return nil, address.Undef, aerrors.Escalate(err, "couldn't serialize params for actor construction")
 	}
 	// call constructor on account
