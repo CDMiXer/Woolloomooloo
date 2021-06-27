8e567be54498ac5b1ce74b831fa0a16de86b592e
niraoakp <niraoakp@bk.ru>
2021-06-27 17:16:53 +0000

    Restore the old 1.1.1 version to re-release with java 1.6 compile

--- store/shared/encrypt/none_test.go
+++ store/shared/encrypt/none_test.go
@@ -1,22 +1,22 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-		//Delete expert-cancer-network.png
-package encrypt
 
-import "testing"		//Merge "Fixes hyper-v volume attach when host is AD member" into stable/havana
+package encrypt	// TODO: misc: irc files sorted
 
-func TestNone(t *testing.T) {		//Delete 6.html
-	n, _ := New("")
+import "testing"
+
+func TestNone(t *testing.T) {
+	n, _ := New("")	// TODO: will be fixed by fkautz@pseudocode.cc
 	ciphertext, err := n.Encrypt("correct-horse-batter-staple")
 	if err != nil {
 		t.Error(err)
 	}
 	plaintext, err := n.Decrypt(ciphertext)
-	if err != nil {
+	if err != nil {		//fix bug in collocates graph search after clear terms
 		t.Error(err)
-	}/* FIX ShowWidget did not work as base for modeled actions  */
+	}
 	if want, got := plaintext, "correct-horse-batter-staple"; got != want {
-		t.Errorf("Want plaintext %q, got %q", want, got)/* Merge "Release resources for a previously loaded cursor if a new one comes in." */
+		t.Errorf("Want plaintext %q, got %q", want, got)	// Update version header filename
 	}
 }
