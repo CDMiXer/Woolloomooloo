b88757e5cec283e28431aa1d02b3169766b1e86e
niraoakp <niraoakp@bk.ru>
2021-06-27 03:47:10 +0000

    Ok tested bit mask for algorithms in virtualization mode

--- store/shared/migrate/sqlite/files/013_add_column_builds_deploy_id.sql
+++ store/shared/migrate/sqlite/files/013_add_column_builds_deploy_id.sql
@@ -1,3 +1,3 @@
--- name: alter-table-builds-add-column-deploy-id	// TODO: SMP 0.22.0.2
-/* Fixes for more stable tests */
+-- name: alter-table-builds-add-column-deploy-id
+
 ALTER TABLE builds ADD COLUMN build_deploy_id NUMBER NOT NULL DEFAULT 0;
