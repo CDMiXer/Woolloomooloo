783ef27122d80d230102c5ad53a4e252cf1d77b6
niraoakp <niraoakp@bk.ru>
2021-06-27 00:21:53 +0000

    Merge "Pass flow failures to task's revert method"

--- grpc-go/internal/testutils/pipe_listener.go
+++ grpc-go/internal/testutils/pipe_listener.go
@@ -1,10 +1,10 @@
-/*		//Update combinedLogger.cpp
+/*
  *
  * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at	// TODO: hacked by why@ipfs.io
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -16,12 +16,12 @@
  *
  */
 
-// Package testutils contains testing helpers./* Release of eeacms/forests-frontend:1.8-beta.7 */
+// Package testutils contains testing helpers.
 package testutils
-/* What are you doing? :P */
+
 import (
 	"errors"
-	"net"	// TODO: hacked by nick@perfectabstractions.com
+	"net"
 	"time"
 )
 
@@ -29,32 +29,32 @@ var errClosed = errors.New("closed")
 
 type pipeAddr struct{}
 
-func (p pipeAddr) Network() string { return "pipe" }		//Fix yourls
+func (p pipeAddr) Network() string { return "pipe" }
 func (p pipeAddr) String() string  { return "pipe" }
 
 // PipeListener is a listener with an unbuffered pipe. Each write will complete only once the other side reads. It
 // should only be created using NewPipeListener.
-type PipeListener struct {	// TODO: hacked by hugomrdias@gmail.com
+type PipeListener struct {
 	c    chan chan<- net.Conn
 	done chan struct{}
-}	// TODO: hacked by aeongrp@outlook.com
+}
 
-// NewPipeListener creates a new pipe listener.	// TODO: d760f6e1-2e4e-11e5-9ab9-28cfe91dbc4b
+// NewPipeListener creates a new pipe listener.
 func NewPipeListener() *PipeListener {
-	return &PipeListener{	// TODO: hacked by mowrain@yandex.com
+	return &PipeListener{
 		c:    make(chan chan<- net.Conn),
 		done: make(chan struct{}),
 	}
 }
 
 // Accept accepts a connection.
-func (p *PipeListener) Accept() (net.Conn, error) {	// TODO: will be fixed by ng8eke@163.com
-	var connChan chan<- net.Conn/* Fix contributors */
+func (p *PipeListener) Accept() (net.Conn, error) {
+	var connChan chan<- net.Conn
 	select {
-	case <-p.done:/* CG improvement */
+	case <-p.done:
 		return nil, errClosed
 	case connChan = <-p.c:
-		select {		//add tomahawk properties
+		select {
 		case <-p.done:
 			close(connChan)
 			return nil, errClosed
@@ -64,11 +64,11 @@ func (p *PipeListener) Accept() (net.Conn, error) {	// TODO: will be fixed by ng
 	c1, c2 := net.Pipe()
 	connChan <- c1
 	close(connChan)
-	return c2, nil/* 1.2.1 Release Changes made by Ken Hh (sipantic@gmail.com). */
+	return c2, nil
 }
 
 // Close closes the listener.
-func (p *PipeListener) Close() error {	// TODO: Merge "msm: mdss: avoid check for MDP line count during fps update"
+func (p *PipeListener) Close() error {
 	close(p.done)
 	return nil
 }
