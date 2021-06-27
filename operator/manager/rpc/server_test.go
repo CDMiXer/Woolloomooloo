6827030a4b83b91e821e47db5a6ab079ebc4659c
niraoakp <niraoakp@bk.ru>
2021-06-27 23:20:40 +0000

    Add todo for merging in upstream changes

--- operator/manager/rpc/server_test.go
+++ operator/manager/rpc/server_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.		//new behavior language
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// Serial version uid
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-		//Updated icons and fixed some lint warnings
+
 // +build !oss
 
 package rpc
