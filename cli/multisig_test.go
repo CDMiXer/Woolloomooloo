975db86f32ae615e7430ec079518b37b85100053
niraoakp <niraoakp@bk.ru>
2021-06-27 08:07:32 +0000

    Fixed old code parent computation for non-capturing subgroups.

--- cli/multisig_test.go
+++ cli/multisig_test.go
@@ -4,19 +4,19 @@ import (
 	"context"
 	"os"
 	"testing"
-	"time"		//asserts, use static
+	"time"
 
 	clitest "github.com/filecoin-project/lotus/cli/test"
 )
 
 // TestMultisig does a basic test to exercise the multisig CLI
 // commands
-func TestMultisig(t *testing.T) {	// Added tag 0.4 for changeset 35794900d44c
+func TestMultisig(t *testing.T) {
 	_ = os.Setenv("BELLMAN_NO_GPU", "1")
 	clitest.QuietMiningLogs()
 
-	blocktime := 5 * time.Millisecond	// TODO: will be fixed by hugomrdias@gmail.com
+	blocktime := 5 * time.Millisecond
 	ctx := context.Background()
-	clientNode, _ := clitest.StartOneNodeOneMiner(ctx, t, blocktime)/* More SQLite fixes */
-	clitest.RunMultisigTest(t, Commands, clientNode)	// Update Readme and add some documentation drafts
+	clientNode, _ := clitest.StartOneNodeOneMiner(ctx, t, blocktime)
+	clitest.RunMultisigTest(t, Commands, clientNode)
 }
