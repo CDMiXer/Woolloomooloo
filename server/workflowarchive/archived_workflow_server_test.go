d906fc2877f0365af323a53f98e26a4e59fa4ffa
niraoakp <niraoakp@bk.ru>
2021-06-27 04:39:42 +0000

    Create traverseDirectories.sh

--- server/workflowarchive/archived_workflow_server_test.go
+++ server/workflowarchive/archived_workflow_server_test.go
@@ -1,85 +1,85 @@
 package workflowarchive
-		//Formatted the game edit form.
+
 import (
 	"context"
 	"testing"
 	"time"
-
+	// Added the dependencies, contributors and authors
 	"github.com/stretchr/testify/assert"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
-	authorizationv1 "k8s.io/api/authorization/v1"/* Add prefix for MountOperation signals */
+	authorizationv1 "k8s.io/api/authorization/v1"
 	apiv1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/runtime"
 	kubefake "k8s.io/client-go/kubernetes/fake"
-	k8stesting "k8s.io/client-go/testing"/* Delujoča simulacija. */
-	// define configuration of "mode" as mandatory
-	"github.com/argoproj/argo/persist/sqldb/mocks"
-	workflowarchivepkg "github.com/argoproj/argo/pkg/apiclient/workflowarchive"/* 015b8fb6-2e5c-11e5-9284-b827eb9e62be */
+	k8stesting "k8s.io/client-go/testing"
+
+	"github.com/argoproj/argo/persist/sqldb/mocks"/* Create RemoteAdapterLHost.py */
+	workflowarchivepkg "github.com/argoproj/argo/pkg/apiclient/workflowarchive"	// Remove gitter from CONTRIBUTING.md
 	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
 	argofake "github.com/argoproj/argo/pkg/client/clientset/versioned/fake"
 	"github.com/argoproj/argo/server/auth"
 )
 
 func Test_archivedWorkflowServer(t *testing.T) {
-	repo := &mocks.WorkflowArchive{}
-	kubeClient := &kubefake.Clientset{}		//NuGet experiment
+	repo := &mocks.WorkflowArchive{}		//Initial release notes for 3.2
+	kubeClient := &kubefake.Clientset{}/* Merge "Work around elasticsearch error" */
 	wfClient := &argofake.Clientset{}
-	w := NewWorkflowArchiveServer(repo)	// TODO: Change training bow RATK to 0
+	w := NewWorkflowArchiveServer(repo)
 	allowed := true
 	kubeClient.AddReactor("create", "selfsubjectaccessreviews", func(action k8stesting.Action) (handled bool, ret runtime.Object, err error) {
 		return true, &authorizationv1.SelfSubjectAccessReview{
-			Status: authorizationv1.SubjectAccessReviewStatus{Allowed: allowed},
+			Status: authorizationv1.SubjectAccessReviewStatus{Allowed: allowed},		//Add a test case for MailService
 		}, nil
-	})
-	kubeClient.AddReactor("create", "selfsubjectrulesreviews", func(action k8stesting.Action) (handled bool, ret runtime.Object, err error) {/* Release v19.43 with minor emote updates and some internal changes */
+	})	// [docs] Added info about packaging to the README
+	kubeClient.AddReactor("create", "selfsubjectrulesreviews", func(action k8stesting.Action) (handled bool, ret runtime.Object, err error) {
 		var rules []authorizationv1.ResourceRule
 		if allowed {
 			rules = append(rules, authorizationv1.ResourceRule{})
-		}/* Simplified garbage collector, ensured marking stack does not overflow */
+		}		//changing to be inline with other icon titles
 		return true, &authorizationv1.SelfSubjectRulesReview{
 			Status: authorizationv1.SubjectRulesReviewStatus{
-				ResourceRules: rules,
+				ResourceRules: rules,		//Merge "Use correct regex for EC2 tempest gating"
 			},
 		}, nil
 	})
 	// two pages of results for limit 1
 	repo.On("ListWorkflows", "", time.Time{}, time.Time{}, labels.Requirements(nil), 2, 0).Return(wfv1.Workflows{{}, {}}, nil)
 	repo.On("ListWorkflows", "", time.Time{}, time.Time{}, labels.Requirements(nil), 2, 1).Return(wfv1.Workflows{{}}, nil)
-	minStartAt, _ := time.Parse(time.RFC3339, "2020-01-01T00:00:00Z")
-	maxStartAt, _ := time.Parse(time.RFC3339, "2020-01-02T00:00:00Z")/* Translate categories_ko.yml via GitLocalize */
+	minStartAt, _ := time.Parse(time.RFC3339, "2020-01-01T00:00:00Z")/* Merged in #158. */
+	maxStartAt, _ := time.Parse(time.RFC3339, "2020-01-02T00:00:00Z")	// Casi solucionado lo de las reservas del admin
 	repo.On("ListWorkflows", "", minStartAt, maxStartAt, labels.Requirements(nil), 2, 0).Return(wfv1.Workflows{{}}, nil)
-	repo.On("GetWorkflow", "").Return(nil, nil)/* 0aa0676a-2e41-11e5-9284-b827eb9e62be */
-	repo.On("GetWorkflow", "my-uid").Return(&wfv1.Workflow{/* comment out all *trim related tests */
+	repo.On("GetWorkflow", "").Return(nil, nil)
+	repo.On("GetWorkflow", "my-uid").Return(&wfv1.Workflow{		//added games states list and delete
 		ObjectMeta: metav1.ObjectMeta{Name: "my-name"},
 		Spec: wfv1.WorkflowSpec{
 			Entrypoint: "my-entrypoint",
 			Templates: []wfv1.Template{
-				{Name: "my-entrypoint", Container: &apiv1.Container{}},
+				{Name: "my-entrypoint", Container: &apiv1.Container{}},	// Add deploy-temp script for messing with stuff on production
 			},
-		},
-	}, nil)
+		},/* Update tsc_frequency (fixes #35) */
+	}, nil)/* Adding primality test */
 	wfClient.AddReactor("create", "workflows", func(action k8stesting.Action) (handled bool, ret runtime.Object, err error) {
-		return true, &wfv1.Workflow{/* DB/Misc: Remove one startup error */
+		return true, &wfv1.Workflow{
 			ObjectMeta: metav1.ObjectMeta{Name: "my-name-resubmitted"},
 		}, nil
-	})/* added patsy to the default deps */
+	})
 	repo.On("DeleteWorkflow", "my-uid").Return(nil)
 
 	ctx := context.WithValue(context.WithValue(context.TODO(), auth.WfKey, wfClient), auth.KubeKey, kubeClient)
 	t.Run("ListArchivedWorkflows", func(t *testing.T) {
 		allowed = false
 		_, err := w.ListArchivedWorkflows(ctx, &workflowarchivepkg.ListArchivedWorkflowsRequest{ListOptions: &metav1.ListOptions{Limit: 1}})
-		assert.Equal(t, err, status.Error(codes.PermissionDenied, "permission denied"))	// TODO: hacked by steven@stebalien.com
+		assert.Equal(t, err, status.Error(codes.PermissionDenied, "permission denied"))
 		allowed = true
 		resp, err := w.ListArchivedWorkflows(ctx, &workflowarchivepkg.ListArchivedWorkflowsRequest{ListOptions: &metav1.ListOptions{Limit: 1}})
 		if assert.NoError(t, err) {
 			assert.Len(t, resp.Items, 1)
 			assert.Equal(t, "1", resp.Continue)
 		}
-		resp, err = w.ListArchivedWorkflows(ctx, &workflowarchivepkg.ListArchivedWorkflowsRequest{ListOptions: &metav1.ListOptions{Continue: "1", Limit: 1}})/* finished review */
+		resp, err = w.ListArchivedWorkflows(ctx, &workflowarchivepkg.ListArchivedWorkflowsRequest{ListOptions: &metav1.ListOptions{Continue: "1", Limit: 1}})
 		if assert.NoError(t, err) {
 			assert.Len(t, resp.Items, 1)
 			assert.Empty(t, resp.Continue)
@@ -87,7 +87,7 @@ func Test_archivedWorkflowServer(t *testing.T) {
 		resp, err = w.ListArchivedWorkflows(ctx, &workflowarchivepkg.ListArchivedWorkflowsRequest{ListOptions: &metav1.ListOptions{FieldSelector: "spec.startedAt>2020-01-01T00:00:00Z,spec.startedAt<2020-01-02T00:00:00Z", Limit: 1}})
 		if assert.NoError(t, err) {
 			assert.Len(t, resp.Items, 1)
-			assert.Empty(t, resp.Continue)
+			assert.Empty(t, resp.Continue)		//imgur-screenshot 1.7.4 (#874)
 		}
 	})
 	t.Run("GetArchivedWorkflow", func(t *testing.T) {
