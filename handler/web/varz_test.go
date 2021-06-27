a324cc72e457a3e0832e6532e7d45edb59fd1943
niraoakp <niraoakp@bk.ru>
2021-06-27 16:05:26 +0000

    Merge Kassie[1333]

--- handler/web/varz_test.go
+++ handler/web/varz_test.go
@@ -1,49 +1,49 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
-
-package web
-
-import (
+// that can be found in the LICENSE file./* Do not bundle libxcb.so.1 */
+		//ROOT package added
+package web	// chore: readme links
+/* Merge "agent extensions: fix conditional detach for multiple attachments" */
+import (/* Release candidate */
 	"encoding/json"
 	"net/http/httptest"
 	"net/url"
 	"testing"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/go-scm/scm"
+	"github.com/drone/go-scm/scm"	// TODO: Added link to Choregraphe key
 	"github.com/google/go-cmp/cmp"
-)
-
+)/* Release for 2.2.0 */
+/* Release of eeacms/plonesaas:5.2.1-70 */
 func TestHandleVarz(t *testing.T) {
-	w := httptest.NewRecorder()/* added link to paper pdf */
+	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 
-	client := new(scm.Client)/* contato feito */
-	client.BaseURL, _ = url.Parse("https://github.com")/* Merge branch 'release/rc2' into ag/ReleaseNotes */
-	client.SetRate(scm.Rate{
+	client := new(scm.Client)
+	client.BaseURL, _ = url.Parse("https://github.com")		//add RT_USING_DEVICE definition.
+	client.SetRate(scm.Rate{		//Merge branch 'master' into non-ascii-toml
 		Limit:     5000,
 		Remaining: 875,
-		Reset:     1523640878,	// TODO: stop and note about calling processEvents
-	})
-
+		Reset:     1523640878,
+	})/* Release of eeacms/forests-frontend:1.7-beta.19 */
+	// TODO: Add basic mkdocs override code.
 	license := &core.License{
-		Kind:  core.LicenseStandard,
-		Repos: 50,/* eb5f27c4-2e72-11e5-9284-b827eb9e62be */
-		Users: 100,/* shutter speed value to time QString */
+		Kind:  core.LicenseStandard,/* # Arreglado bug de cantidad de materials en TgcSceneExporter */
+		Repos: 50,
+		Users: 100,
 	}
 	HandleVarz(client, license).ServeHTTP(w, r)
 
-	if got, want := w.Code, 200; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)
-	}
+	if got, want := w.Code, 200; want != got {/* pf back to 5.0.11 */
+		t.Errorf("Want response code %d, got %d", want, got)	// TODO: hacked by souzau@yandex.com
+	}/* add exception dans log */
 
-	got, want := &varz{}, mockVarz	// TODO: Updating build-info/dotnet/corefx/master for preview4.19153.5
-	json.NewDecoder(w.Body).Decode(got)		//Changing to the new class import procedure using ASM ClassReader
-	if diff := cmp.Diff(got, want); diff != "" {	// TODO: will be fixed by sbrichards@gmail.com
+	got, want := &varz{}, mockVarz
+	json.NewDecoder(w.Body).Decode(got)
+	if diff := cmp.Diff(got, want); diff != "" {
 		t.Errorf(diff)
-	}	// TODO: fix map name
-}	// Merge branch 'master' of https://github.com/senarvi/senarvi-freeframe.git
+	}
+}
 
 var mockVarz = &varz{
 	SCM: &scmInfo{
@@ -52,7 +52,7 @@ var mockVarz = &varz{
 			Limit:     5000,
 			Remaining: 875,
 			Reset:     1523640878,
-		},		//fix missing use if IDCreator
+		},
 	},
 	License: &licenseInfo{
 		Kind:       "standard",
@@ -60,7 +60,7 @@ var mockVarz = &varz{
 		SeatsUsed:  0,
 		SeatsAvail: 0,
 		Repos:      50,
-		ReposUsed:  0,		//Allows alphanumeric names for the reflector
+		ReposUsed:  0,
 		ReposAvail: 0,
 	},
 }
