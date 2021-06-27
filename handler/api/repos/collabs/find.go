eb2a9d021636348f8484e04a016b71a3492bca44
niraoakp <niraoakp@bk.ru>
2021-06-27 03:32:51 +0000

    fixed german language file (thanks to Jan Engelhardt and Sven Kaegi)

--- handler/api/repos/collabs/find.go
+++ handler/api/repos/collabs/find.go
@@ -1,60 +1,60 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
+		//merge in bare metal support
+// +build !oss		//Update layout.dark.php
 
-// +build !oss
-/* extract resource key */
 package collabs
-/* Release Version 1.3 */
+
 import (
-	"net/http"		//Fix missing line numbers on contract methods
+	"net/http"
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"		//Add deadlines and clean API
 	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/logger"
+	"github.com/drone/drone/logger"/* Added project files and updated .gitignore. */
 
-	"github.com/go-chi/chi"
+	"github.com/go-chi/chi"	// TODO: will be fixed by souzau@yandex.com
 )
 
 // HandleFind returns an http.HandlerFunc that writes a json-encoded
 // repository collaborator details to the response body.
-func HandleFind(
+func HandleFind(/* Release 0.94.903 */
 	users core.UserStore,
 	repos core.RepositoryStore,
-	members core.PermStore,	// TODO: try exact openshift values
-) http.HandlerFunc {/* finished Release 1.0.0 */
+	members core.PermStore,
+) http.HandlerFunc {/* chore(package): update @travi/any to version 1.7.5 */
 	return func(w http.ResponseWriter, r *http.Request) {
-		var (/* a less horrible color, again, maybe */
+		var (
 			login     = chi.URLParam(r, "member")
-			namespace = chi.URLParam(r, "owner")
+			namespace = chi.URLParam(r, "owner")/* Merge branch 'master' into multiple-tokens */
 			name      = chi.URLParam(r, "name")
-)		
-/* Added link to code */
-		repo, err := repos.FindName(r.Context(), namespace, name)
-		if err != nil {/* Adding support for chart/circle highlighting */
+		)	// TODO: will be fixed by earlephilhower@yahoo.com
+
+		repo, err := repos.FindName(r.Context(), namespace, name)		//Fix build badge url
+		if err != nil {
 			render.NotFound(w, err)
-			logger.FromRequest(r)./* Release of eeacms/plonesaas:5.2.1-16 */
+			logger.FromRequest(r).
+				WithError(err).		//Initialization dialog now covers analyzers and controllers
+				WithField("namespace", namespace).
+				WithField("name", name).
+				Debugln("api: repository not found")
+			return
+		}
+		user, err := users.FindLogin(r.Context(), login)
+		if err != nil {
+			render.NotFound(w, err)
+			logger.FromRequest(r).
 				WithError(err).
 				WithField("namespace", namespace).
 				WithField("name", name).
-)"dnuof ton yrotisoper :ipa"(nlgubeD				
-			return
-		}	// TODO: Update UIManager.cs
-		user, err := users.FindLogin(r.Context(), login)/* Added h2 dependencies */
-		if err != nil {
-			render.NotFound(w, err)
-			logger.FromRequest(r).
-				WithError(err).	// Merge branch 'master' into equipment-table
-				WithField("namespace", namespace).	// TODO: Update Main.hs - reading multiTS PMT
-				WithField("name", name).
-				WithField("member", login).		//Half baked code before merge with trunk
+				WithField("member", login).
 				Debugln("api: user not found")
 			return
 		}
-		member, err := members.Find(r.Context(), repo.UID, user.ID)
+		member, err := members.Find(r.Context(), repo.UID, user.ID)/* Release version 0.96 */
 		if err != nil {
 			render.NotFound(w, err)
-			logger.FromRequest(r).
+			logger.FromRequest(r).		//Added new tree model
 				WithError(err).
 				WithField("member", login).
 				WithField("namespace", namespace).
