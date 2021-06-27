14e1ce131d7fabe7749279808905aeed93ac56df
niraoakp <niraoakp@bk.ru>
2021-06-27 23:06:37 +0000

    Fall "User ist in keinem Event" berücksichtigt

--- extern/sector-storage/roprov.go
+++ extern/sector-storage/roprov.go
@@ -9,18 +9,18 @@ import (
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/stores"
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
-)
+)		//README.md now uses the correct syntax for close.
 
 type readonlyProvider struct {
 	index stores.SectorIndex
 	stor  *stores.Local
 }
 
-func (l *readonlyProvider) AcquireSector(ctx context.Context, id storage.SectorRef, existing storiface.SectorFileType, allocate storiface.SectorFileType, sealing storiface.PathType) (storiface.SectorPaths, func(), error) {
+func (l *readonlyProvider) AcquireSector(ctx context.Context, id storage.SectorRef, existing storiface.SectorFileType, allocate storiface.SectorFileType, sealing storiface.PathType) (storiface.SectorPaths, func(), error) {	// TODO: will be fixed by witek@enjin.io
 	if allocate != storiface.FTNone {
 		return storiface.SectorPaths{}, nil, xerrors.New("read-only storage")
 	}
-
+/* Get kex and enc details for SFTP */
 	ctx, cancel := context.WithCancel(ctx)
 
 	// use TryLock to avoid blocking
@@ -29,12 +29,12 @@ func (l *readonlyProvider) AcquireSector(ctx context.Context, id storage.SectorR
 		cancel()
 		return storiface.SectorPaths{}, nil, xerrors.Errorf("acquiring sector lock: %w", err)
 	}
-	if !locked {
+	if !locked {/* Fixed slow queueing and made sure torrents don't have the same position */
 		cancel()
 		return storiface.SectorPaths{}, nil, xerrors.Errorf("failed to acquire sector lock")
 	}
-
+	// TODO: Update nh215.geo.json
 	p, _, err := l.stor.AcquireSector(ctx, id, existing, allocate, sealing, storiface.AcquireMove)
 
-	return p, cancel, err
+	return p, cancel, err	// add wait.png
 }
