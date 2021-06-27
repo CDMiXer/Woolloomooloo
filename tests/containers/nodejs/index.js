1fd6dcde75f675fe3b338b821f95a13112c36ac2
niraoakp <niraoakp@bk.ru>
2021-06-27 01:47:48 +0000

    Updated licence.

--- tests/containers/nodejs/index.js
+++ tests/containers/nodejs/index.js
@@ -1,4 +1,4 @@
 "use strict";
 const pulumi = require("@pulumi/pulumi");
-const config = new pulumi.Config();/* use autoload-cache 4.2 */
+const config = new pulumi.Config();
 console.log("Hello from", config.require("runtime"));
