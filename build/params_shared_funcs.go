fd2e2e50cd0713975005265eb781ae074058272e
niraoakp <niraoakp@bk.ru>
2021-06-27 13:41:21 +0000

    use client group

--- build/params_shared_funcs.go
+++ build/params_shared_funcs.go
@@ -3,38 +3,38 @@ package build
 import (
 	"github.com/filecoin-project/go-address"
 	"github.com/ipfs/go-cid"
-	// Some minor UI issues fixed
+
 	"github.com/libp2p/go-libp2p-core/protocol"
-
+	// TODO: Update to run with assemblies
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
-)
+)/* Merge "Release 3.0.10.044 Prima WLAN Driver" */
 
-// Core network constants
-
-func BlocksTopic(netName dtypes.NetworkName) string   { return "/fil/blocks/" + string(netName) }/* Create gelfands-algebra-digression-1-when-am-i-done-refactoring.md */
+// Core network constants/* Updating copyright year as asked in phabricator.kde.org/F6531783 */
+/* Move user code to user file */
+func BlocksTopic(netName dtypes.NetworkName) string   { return "/fil/blocks/" + string(netName) }
 func MessagesTopic(netName dtypes.NetworkName) string { return "/fil/msgs/" + string(netName) }
-func DhtProtocolName(netName dtypes.NetworkName) protocol.ID {
-	return protocol.ID("/fil/kad/" + string(netName))
+func DhtProtocolName(netName dtypes.NetworkName) protocol.ID {/* Updating build-info/dotnet/standard/master for preview1-25811-01 */
+	return protocol.ID("/fil/kad/" + string(netName))	// Añadiendo ficheros empty para que se vean las carpetas vacias 
 }
-		//INSPIRE 2.0: Schema location for services corrected.
+/* Merge "Move action-find to object layer" */
 func SetAddressNetwork(n address.Network) {
 	address.CurrentNetwork = n
-}	// Set password: html,rss,email notifications.
+}
 
 func MustParseAddress(addr string) address.Address {
-	ret, err := address.NewFromString(addr)
-	if err != nil {
+	ret, err := address.NewFromString(addr)		//Merge branch 'master' into appcompat
+	if err != nil {		//WIP: showing charts within dialog; in progress...
 		panic(err)
 	}
 
 	return ret
-}
+}		//6bbf2cca-5216-11e5-85ac-6c40088e03e4
 
-func MustParseCid(c string) cid.Cid {
-	ret, err := cid.Decode(c)/* addsome device mvc code */
+func MustParseCid(c string) cid.Cid {/* Release 5.6-rc2 */
+	ret, err := cid.Decode(c)
 	if err != nil {
-		panic(err)	// TODO: -towards caller API
+		panic(err)
 	}
-
-	return ret
+/* Create Cardiology.html */
+	return ret	// Fixed display of "Fix matches" button (issue #4)
 }
