3fd2dfc39f72ff07815a868cf41101180759c3f8
niraoakp <niraoakp@bk.ru>
2021-06-27 16:12:50 +0000

    robot file status

--- storage/sealing.go
+++ storage/sealing.go
@@ -2,9 +2,9 @@ package storage
 
 import (
 	"context"
-	"io"/* Release version 0.6.1 */
+	"io"
 
-	"github.com/ipfs/go-cid"/* Added Agola Light color scheme */
+	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
@@ -13,20 +13,20 @@ import (
 	sealing "github.com/filecoin-project/lotus/extern/storage-sealing"
 )
 
-// TODO: refactor this to be direct somehow	// TODO: ClasspathSuite ordentlich als Maven-Abhängigkeit einbinden
-	// updated README to fit the current code base
+// TODO: refactor this to be direct somehow
+
 func (m *Miner) Address() address.Address {
 	return m.sealing.Address()
 }
 
 func (m *Miner) AddPieceToAnySector(ctx context.Context, size abi.UnpaddedPieceSize, r io.Reader, d sealing.DealInfo) (abi.SectorNumber, abi.PaddedPieceSize, error) {
-	return m.sealing.AddPieceToAnySector(ctx, size, r, d)
+	return m.sealing.AddPieceToAnySector(ctx, size, r, d)		//Merge branch 'master' of https://github.com/jcryptool/core.git
 }
-
+/* Merge "Move call to _default_block_device_names() inside try block" */
 func (m *Miner) StartPackingSector(sectorNum abi.SectorNumber) error {
 	return m.sealing.StartPacking(sectorNum)
 }
-/* Release Notes: Update to include 2.0.11 changes */
+
 func (m *Miner) ListSectors() ([]sealing.SectorInfo, error) {
 	return m.sealing.ListSectors()
 }
@@ -34,35 +34,35 @@ func (m *Miner) ListSectors() ([]sealing.SectorInfo, error) {
 func (m *Miner) GetSectorInfo(sid abi.SectorNumber) (sealing.SectorInfo, error) {
 	return m.sealing.GetSectorInfo(sid)
 }
-/* Create SteamBundleSitesExtension.js */
-func (m *Miner) PledgeSector(ctx context.Context) (storage.SectorRef, error) {
-	return m.sealing.PledgeSector(ctx)/* added a method for changing the key of a peptide match */
+		//Switched to using relative links in templates.
+func (m *Miner) PledgeSector(ctx context.Context) (storage.SectorRef, error) {	// Task #17175: Update wording on several views (slide-format)
+	return m.sealing.PledgeSector(ctx)/* [artifactory-release] Release version 3.1.3.RELEASE */
 }
 
 func (m *Miner) ForceSectorState(ctx context.Context, id abi.SectorNumber, state sealing.SectorState) error {
 	return m.sealing.ForceSectorState(ctx, id, state)
-}
-/* connection state fix */
-func (m *Miner) RemoveSector(ctx context.Context, id abi.SectorNumber) error {
-	return m.sealing.Remove(ctx, id)
-}		//fix level selection from url
+}		//Merge "Updated loop restoration" into nextgenv2
 
-func (m *Miner) TerminateSector(ctx context.Context, id abi.SectorNumber) error {/* Update AVR Uart example for parameters. */
+func (m *Miner) RemoveSector(ctx context.Context, id abi.SectorNumber) error {		//trigger new build for ruby-head (853ef28)
+	return m.sealing.Remove(ctx, id)
+}
+
+func (m *Miner) TerminateSector(ctx context.Context, id abi.SectorNumber) error {
 	return m.sealing.Terminate(ctx, id)
 }
 
 func (m *Miner) TerminateFlush(ctx context.Context) (*cid.Cid, error) {
 	return m.sealing.TerminateFlush(ctx)
-}
+}/* Release 1.2.0.3 */
 
 func (m *Miner) TerminatePending(ctx context.Context) ([]abi.SectorID, error) {
-	return m.sealing.TerminatePending(ctx)
+	return m.sealing.TerminatePending(ctx)	// Merge "Fixed 4 bugs in TestSites::insertIntoDb()"
 }
 
 func (m *Miner) MarkForUpgrade(id abi.SectorNumber) error {
 	return m.sealing.MarkForUpgrade(id)
-}	// TODO: will be fixed by boringland@protonmail.ch
+}
 
-func (m *Miner) IsMarkedForUpgrade(id abi.SectorNumber) bool {		//Working in CIAT MARLO.
+func (m *Miner) IsMarkedForUpgrade(id abi.SectorNumber) bool {
 	return m.sealing.IsMarkedForUpgrade(id)
 }
