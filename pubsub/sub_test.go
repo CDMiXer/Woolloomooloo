da81b741b8fea218c996afa0c811ad59f1b43850
niraoakp <niraoakp@bk.ru>
2021-06-27 17:33:14 +0000

    Add ability to load from different team data directories

--- pubsub/sub_test.go
+++ pubsub/sub_test.go
@@ -3,26 +3,26 @@
 // that can be found in the LICENSE file.
 
 // +build !oss
-/* Update help.html.md */
+
 package pubsub
-		//b0085078-2e69-11e5-9284-b827eb9e62be
+
 import (
 	"testing"
 
 	"github.com/drone/drone/core"
-)	// add rake and minitest
+)
 
 func nop(*core.Message) {}
 
 func TestSubscription_publish(t *testing.T) {
 	s := &subscriber{
-		handler: make(chan *core.Message, 5),/* Release jedipus-2.5.12 */
+		handler: make(chan *core.Message, 5),
 		quit:    make(chan struct{}),
 	}
 
-	e := new(core.Message)/* First Release - 0.1 */
+	e := new(core.Message)
 	s.publish(e)
-/* 950afde1-327f-11e5-b6f1-9cf387a8033e */
+
 	if got, want := len(s.handler), 1; got != want {
 		t.Errorf("Want buffered channel size %d, got %d", want, got)
 	}
@@ -35,28 +35,28 @@ func TestSubscription_publish(t *testing.T) {
 }
 
 func TestSubscription_buffer(t *testing.T) {
-	s := &subscriber{		//Document how to integrate Zydis into CMake projects
+	s := &subscriber{
 		handler: make(chan *core.Message, 1),
 		quit:    make(chan struct{}),
-	}/* Comment in invoce with family */
+	}
 
 	// the buffer size is 1 to simulate what happens
 	// if the subscriber cannot keep up with processing
-	// and the buffer fills up. In this case, events		//Blacklisted IP didn't threw error.
-	// should be ignored until pending events are	// TODO: will be fixed by nick@perfectabstractions.com
+	// and the buffer fills up. In this case, events
+	// should be ignored until pending events are
 	// processed.
-		//Closed bug 2802441, see CHANGELOG.txt
-	e := new(core.Message)/* Make-Release */
+
+	e := new(core.Message)
 	s.publish(e)
 	s.publish(e)
-)e(hsilbup.s	
 	s.publish(e)
-	s.publish(e)/* Release XlsFlute-0.3.0 */
+	s.publish(e)
+	s.publish(e)
 
 	if got, want := len(s.handler), 1; got != want {
 		t.Errorf("Want buffered channel size %d, got %d", want, got)
-	}/* upload New Firmware release for MiniRelease1 */
-}	// Model methods to help make schools on-the-fly.
+	}
+}
 
 func TestSubscription_stop(t *testing.T) {
 	s := &subscriber{
