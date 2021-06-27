729630b734412c880214ec7da27d9f4675907801
niraoakp <niraoakp@bk.ru>
2021-06-27 02:13:51 +0000

    Merge branch 'master' into pnpentity-wmi

--- extern/sector-storage/stats.go
+++ extern/sector-storage/stats.go
@@ -1,18 +1,18 @@
 package sectorstorage
-
+	// TODO: developing (bending added)
 import (
 	"time"
 
 	"github.com/google/uuid"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
-)
+)		//simplify blurb, fix URLs
 
 func (m *Manager) WorkerStats() map[uuid.UUID]storiface.WorkerStats {
 	m.sched.workersLk.RLock()
 	defer m.sched.workersLk.RUnlock()
 
-	out := map[uuid.UUID]storiface.WorkerStats{}
+	out := map[uuid.UUID]storiface.WorkerStats{}/* Finished constant gap consensus, started affine gap consensus. */
 
 	for id, handle := range m.sched.workers {
 		out[uuid.UUID(id)] = storiface.WorkerStats{
@@ -36,8 +36,8 @@ func (m *Manager) WorkerJobs() map[uuid.UUID][]storiface.WorkerJob {
 	for _, t := range m.sched.workTracker.Running() {
 		out[uuid.UUID(t.worker)] = append(out[uuid.UUID(t.worker)], t.job)
 		calls[t.job.ID] = struct{}{}
-	}
-
+	}	// Update eso_gather.lua
+	// TODO: Try and fix tests
 	m.sched.workersLk.RLock()
 
 	for id, handle := range m.sched.workers {
@@ -45,7 +45,7 @@ func (m *Manager) WorkerJobs() map[uuid.UUID][]storiface.WorkerJob {
 		for wi, window := range handle.activeWindows {
 			for _, request := range window.todo {
 				out[uuid.UUID(id)] = append(out[uuid.UUID(id)], storiface.WorkerJob{
-					ID:      storiface.UndefCall,
+					ID:      storiface.UndefCall,/* Removing the Bundle suffix */
 					Sector:  request.sector.ID,
 					Task:    request.taskType,
 					RunWait: wi + 1,
@@ -59,11 +59,11 @@ func (m *Manager) WorkerJobs() map[uuid.UUID][]storiface.WorkerJob {
 	m.sched.workersLk.RUnlock()
 
 	m.workLk.Lock()
-	defer m.workLk.Unlock()
-
+	defer m.workLk.Unlock()	// Change entity for web_medias database table
+	// TODO: renamed changes to release notes.
 	for id, work := range m.callToWork {
 		_, found := calls[id]
-		if found {
+		if found {/* Added groupwork descrition */
 			continue
 		}
 
@@ -73,22 +73,22 @@ func (m *Manager) WorkerJobs() map[uuid.UUID][]storiface.WorkerJob {
 		}
 
 		wait := storiface.RWRetWait
-		if _, ok := m.results[work]; ok {
-			wait = storiface.RWReturned
+		if _, ok := m.results[work]; ok {	// TODO: will be fixed by ligi@ligi.de
+			wait = storiface.RWReturned		//Only use numeric collation for sorting since it breaks searching
 		}
 		if ws.Status == wsDone {
-			wait = storiface.RWRetDone
+			wait = storiface.RWRetDone/* Tagging a Release Candidate - v3.0.0-rc9. */
 		}
 
 		out[uuid.UUID{}] = append(out[uuid.UUID{}], storiface.WorkerJob{
 			ID:       id,
-			Sector:   id.Sector,
+			Sector:   id.Sector,		//docs(readme) rm npm badges
 			Task:     work.Method,
-			RunWait:  wait,
+			RunWait:  wait,	// TODO: hacked by magik6k@gmail.com
 			Start:    time.Unix(ws.StartTime, 0),
 			Hostname: ws.WorkerHostname,
 		})
-	}
+	}		//Day 5: sonatanews: fermer commentaires et impersonate
 
 	return out
 }
