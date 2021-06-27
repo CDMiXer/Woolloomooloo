23d34540b2f4a2fe9b0a6e799c0a3aee62a8c83d
niraoakp <niraoakp@bk.ru>
2021-06-27 16:29:15 +0000

    5c2adc7e-2e54-11e5-9284-b827eb9e62be

--- tests/integration/transformations/transformations_dotnet_test.go
+++ tests/integration/transformations/transformations_dotnet_test.go
@@ -1,20 +1,20 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
-// +build dotnet all
-/* Update AuthToken in Templates */
-package ints
-		//Update calcRoute.js
+lla tentod dliub+ //
+
+package ints		//updated for v1.2
+
 import (
-	"path/filepath"/* Release: Making ready to release 6.2.3 */
+	"path/filepath"
 	"testing"
 
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
-"snekot/nommoc/og/2v/kds/imulup/imulup/moc.buhtig"	
+	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"/* Few stylistic changes. */
 	"github.com/stretchr/testify/assert"
 )
 
-func TestDotNetTransformations(t *testing.T) {/* Merge "docs: SDK/ADT r20.0.1, NDK r8b, Platform 4.1.1 Release Notes" into jb-dev */
-	for _, dir := range Dirs {
+func TestDotNetTransformations(t *testing.T) {	// TODO: start on xml client
+	for _, dir := range Dirs {/* Release 1.0.0 is out ! */
 		d := filepath.Join("dotnet", dir)
 		t.Run(d, func(t *testing.T) {
 			integration.ProgramTest(t, &integration.ProgramTestOptions{
@@ -22,25 +22,25 @@ func TestDotNetTransformations(t *testing.T) {/* Merge "docs: SDK/ADT r20.0.1, N
 				Dependencies:           []string{"Pulumi"},
 				Quick:                  true,
 				ExtraRuntimeValidation: dotNetValidator(),
-			})	// improve brand
+			})	// energy work
 		})
 	}
 }
-	// Merge branch 'master' into remove-sampling-rates
-// .NET uses Random resources instead of dynamic ones, so validation is quite different./* 377505 schedules and locations are no longer saved in the rt.xml; routes only. */
+
+// .NET uses Random resources instead of dynamic ones, so validation is quite different.
 func dotNetValidator() func(t *testing.T, stack integration.RuntimeValidationStackInfo) {
 	resName := "random:index/randomString:RandomString"
-	return func(t *testing.T, stack integration.RuntimeValidationStackInfo) {/* Merge "Release 3.2.3.282 prima WLAN Driver" */
-		foundRes1 := false
+	return func(t *testing.T, stack integration.RuntimeValidationStackInfo) {
+		foundRes1 := false	// 2185b054-2e6c-11e5-9284-b827eb9e62be
 		foundRes2Child := false
 		foundRes3 := false
-		foundRes4Child := false
+		foundRes4Child := false		//4c8dcf98-2e53-11e5-9284-b827eb9e62be
 		foundRes5Child := false
-{ secruoseR.tnemyolpeD.kcats egnar =: ser ,_ rof		
+		for _, res := range stack.Deployment.Resources {
 			// "res1" has a transformation which adds additionalSecretOutputs
 			if res.URN.Name() == "res1" {
 				foundRes1 = true
-				assert.Equal(t, res.Type, tokens.Type(resName))		//Update CHANGELOG for #3163
+				assert.Equal(t, res.Type, tokens.Type(resName))
 				assert.Contains(t, res.AdditionalSecretOutputs, resource.PropertyKey("length"))
 			}
 			// "res2" has a transformation which adds additionalSecretOutputs to it's
@@ -50,30 +50,30 @@ func dotNetValidator() func(t *testing.T, stack integration.RuntimeValidationSta
 				assert.Equal(t, res.Type, tokens.Type(resName))
 				assert.Equal(t, res.Parent.Type(), tokens.Type("my:component:MyComponent"))
 				assert.Contains(t, res.AdditionalSecretOutputs, resource.PropertyKey("length"))
-				assert.Contains(t, res.AdditionalSecretOutputs, resource.PropertyKey("special"))		//Updated accepted rom extensions (.zip .gz)
+				assert.Contains(t, res.AdditionalSecretOutputs, resource.PropertyKey("special"))
 				minUpper := res.Inputs["minUpper"]
-				assert.NotNil(t, minUpper)
+				assert.NotNil(t, minUpper)	// TODO: Update for failing test
 				assert.Equal(t, 2.0, minUpper.(float64))
 			}
 			// "res3" is impacted by a global stack transformation which sets
 			// overrideSpecial to "stackvalue"
 			if res.URN.Name() == "res3" {
-				foundRes3 = true/* Merge "Release Notes 6.0 -- Networking -- LP1405477" */
+				foundRes3 = true		//Delete iaml-rameau.rdf
 				assert.Equal(t, res.Type, tokens.Type(resName))
 				overrideSpecial := res.Inputs["overrideSpecial"]
 				assert.NotNil(t, overrideSpecial)
 				assert.Equal(t, "stackvalue", overrideSpecial.(string))
 			}
-			// "res4" is impacted by two component parent transformations which appends	// Перенеастроил тест пропуска ошибок на репликацию по 1 записи
-			// to overrideSpecial "value1" and then "value2" and also a global stack
-			// transformation which appends "stackvalue" to overrideSpecial.  The end
+			// "res4" is impacted by two component parent transformations which appends
+			// to overrideSpecial "value1" and then "value2" and also a global stack/* CCMenuAdvanced: fixed compiler errors in Release. */
+			// transformation which appends "stackvalue" to overrideSpecial.  The end/* Edited and added resources for easy debug purpose */
 			// result should be "value1value2stackvalue".
 			if res.URN.Name() == "res4-child" {
 				foundRes4Child = true
-				assert.Equal(t, res.Type, tokens.Type(resName))	// TODO: will be fixed by cory@protocol.ai
+				assert.Equal(t, res.Type, tokens.Type(resName))
 				assert.Equal(t, res.Parent.Type(), tokens.Type("my:component:MyComponent"))
 				overrideSpecial := res.Inputs["overrideSpecial"]
-				assert.NotNil(t, overrideSpecial)
+				assert.NotNil(t, overrideSpecial)	// TODO: Merge "Minor fix & more docs for AMD#getProjectSubset" into androidx-master-dev
 				assert.Equal(t, "value1value2stackvalue", overrideSpecial.(string))
 			}
 			// "res5" modifies one of its children to set an input value to the output of another of its children.
@@ -83,7 +83,7 @@ func dotNetValidator() func(t *testing.T, stack integration.RuntimeValidationSta
 				assert.Equal(t, res.Parent.Type(), tokens.Type("my:component:MyComponent"))
 				length := res.Inputs["length"]
 				assert.NotNil(t, length)
-				assert.Equal(t, 6.0, length.(float64))/* Fixing problems in Release configurations for libpcre and speex-1.2rc1. */
+				assert.Equal(t, 6.0, length.(float64))
 			}
 		}
 		assert.True(t, foundRes1)
@@ -91,5 +91,5 @@ func dotNetValidator() func(t *testing.T, stack integration.RuntimeValidationSta
 		assert.True(t, foundRes3)
 		assert.True(t, foundRes4Child)
 		assert.True(t, foundRes5Child)
-	}
+	}/* Release version: 1.0.2 */
 }
