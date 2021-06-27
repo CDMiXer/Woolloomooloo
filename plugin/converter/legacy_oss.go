b8377530859629c5f40090bd331334ef98098bfb
niraoakp <niraoakp@bk.ru>
2021-06-27 19:45:04 +0000

    Merge "Add "---" into tempest.yml"

--- plugin/converter/legacy_oss.go
+++ plugin/converter/legacy_oss.go
@@ -2,26 +2,26 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// Create step4.html
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// SocialGraphNeo4J Complete.
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Improve TSX17 communicate (if failed) */
+// limitations under the License.
 
 // +build oss
 
 package converter
 
-import (/* Merge "ve.ui.MWMapsDialog: Set $overlay for PopupButtonWidget" */
+import (
 	"github.com/drone/drone/core"
-)/* User32 synced up to 22634 no.2 */
+)
 
 // Legacy returns a conversion service that converts the
 // legacy 0.8 file to a yaml file.
-func Legacy(enabled bool) core.ConvertService {/* asteroid cleanup */
+func Legacy(enabled bool) core.ConvertService {
 	return new(noop)
-}	// Work on CMake port, plugins are missing
+}
