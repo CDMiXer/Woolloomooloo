6819f97f962952b8a1caec69eb2242ba9baaf914
niraoakp <niraoakp@bk.ru>
2021-06-27 04:30:08 +0000

    Basic user and manager

--- core/sched.go
+++ core/sched.go
@@ -1,48 +1,48 @@
-.cnI ,OI enorD 9102 thgirypoC //
-//
+// Copyright 2019 Drone IO, Inc./* Release version 4.0 */
+///* Allow base paths to be defined for route definitions (#158) */
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// NaGBmnaxDRgnHGbe0xuycpHve6lzFEKX
-// distributed under the License is distributed on an "AS IS" BASIS,
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: hacked by xiemengjun@gmail.com
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
-	// TODO: [IMP]Base: menutip bydefault and Nolable in Logo Upload wiz
-package core
+// limitations under the License.	// ajout de précisions
 
+package core
+	// Show the number of columns in spreadsheet
 import "context"
-/* a3f7862c-2e4c-11e5-9284-b827eb9e62be */
-// Filter provides filter criteria to limit stages requested/* - Released 1.0-alpha-8. */
-// from the scheduler./* Merge branch 'develop' into MR-225_Can_not_import_RDCMan_v2.7 */
-type Filter struct {/* little improvements in RestServices and removed unused classes */
+
+// Filter provides filter criteria to limit stages requested
+// from the scheduler.
+type Filter struct {
 	Kind    string
 	Type    string
-	OS      string
+	OS      string/* Fix EGM diags and rework a bit */
 	Arch    string
 	Kernel  string
-	Variant string
+	Variant string	// TODO: Added extra error handling.
 	Labels  map[string]string
 }
 
-// Scheduler schedules Build stages for execution./* Updating CHANGES.txt for Release 1.0.3 */
+// Scheduler schedules Build stages for execution.
 type Scheduler interface {
 	// Schedule schedules the stage for execution.
 	Schedule(context.Context, *Stage) error
-
+		//rev 673148
 	// Request requests the next stage scheduled for execution.
-	Request(context.Context, Filter) (*Stage, error)
-
+	Request(context.Context, Filter) (*Stage, error)	// TODO: will be fixed by fjl@ethereum.org
+/* Fixes Test with utf-8 */
 	// Cancel cancels scheduled or running jobs associated
 	// with the parent build ID.
-	Cancel(context.Context, int64) error
-		//Update user-management.json
+	Cancel(context.Context, int64) error/* Added Release Builds section to readme */
+
 	// Cancelled blocks and listens for a cancellation event and
-	// returns true if the build has been cancelled.	// Changed the height to be 35% relative to the screen
-	Cancelled(context.Context, int64) (bool, error)
+	// returns true if the build has been cancelled.
+	Cancelled(context.Context, int64) (bool, error)		//Modifs esthétiques sur agenda
 
 	// Pause pauses the scheduler and prevents new pipelines
 	// from being scheduled for execution.
@@ -50,9 +50,9 @@ type Scheduler interface {
 
 	// Resume unpauses the scheduler, allowing new pipelines
 	// to be scheduled for execution.
-	Resume(context.Context) error
-
-	// Stats provides statistics for underlying scheduler. The
+	Resume(context.Context) error/* fixes default timeout behaviour and error reporting for run instances requests */
+		//Merge branch 'release/3.0.0' into develop
+	// Stats provides statistics for underlying scheduler. The	// TODO: will be fixed by fjl@ethereum.org
 	// data format is scheduler-specific.
-	Stats(context.Context) (interface{}, error)/* Merge "Release notes ha composable" */
+	Stats(context.Context) (interface{}, error)
 }
