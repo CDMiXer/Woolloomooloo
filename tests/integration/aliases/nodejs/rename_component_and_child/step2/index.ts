fddf9f467004c636377aa3bcfd9ba3cf1354aa5d
niraoakp <niraoakp@bk.ru>
2021-06-27 07:57:55 +0000

    f97b56c4-2e4b-11e5-9284-b827eb9e62be

--- tests/integration/aliases/nodejs/rename_component_and_child/step2/index.ts
+++ tests/integration/aliases/nodejs/rename_component_and_child/step2/index.ts
@@ -1,24 +1,24 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Merge "msm: camera: kernel driver for sensor imx135" */
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import * as pulumi from "@pulumi/pulumi";
-
-class Resource extends pulumi.ComponentResource {	// TODO: extra bits
-    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
+		//d34f3222-2e50-11e5-9284-b827eb9e62be
+class Resource extends pulumi.ComponentResource {
+    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {/* Merge "Correcting description of image_update API method." */
         super("my:module:Resource", name, {}, opts);
     }
-}/* Release version [10.8.2] - alfter build */
+}	// TODO: Better formatting, gameplay changes, controls
 
 // Scenario #5 - composing #1 and #3
-{ ecruoseRtnenopmoC.imulup sdnetxe eviFtnenopmoC ssalc
+class ComponentFive extends pulumi.ComponentResource {
     resource: Resource;
-{ )snoitpOecruoseRtnenopmoC.imulup :?stpo ,gnirts :eman(rotcurtsnoc    
+    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {	// TODO: Fixed wrong composer name in installation instructions
         super("my:module:ComponentFive", name, {}, opts);
         this.resource = new Resource("otherchildrenamed", {
             parent: this,
             aliases: [{ name: "otherchild", parent: this }],
-        });
-    }
+        });		//Delete van.jpg
+    }/* 8e1776fa-2e5b-11e5-9284-b827eb9e62be */
 }
-const comp5 = new ComponentFive("newcomp5", {
-    aliases: [{ name: "comp5" }],/* Added missing modifications to ReleaseNotes. */
+const comp5 = new ComponentFive("newcomp5", {		//this was bothering me
+    aliases: [{ name: "comp5" }],
 });
