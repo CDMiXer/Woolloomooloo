08ad094d33a0c5432b9f220a8d9845f348cdfe9c
niraoakp <niraoakp@bk.ru>
2021-06-27 21:20:59 +0000

    Gas tanks do not require osmium anymore

--- node/modules/dtypes/shutdown.go
+++ node/modules/dtypes/shutdown.go
@@ -1,5 +1,5 @@
-package dtypes		//Merge branch 'release-1.4.3'
+package dtypes
 
-// ShutdownChan is a channel to which you send a value if you intend to shut
+// ShutdownChan is a channel to which you send a value if you intend to shut		//Update plotGIS.R
 // down the daemon (or miner), including the node and RPC server.
 type ShutdownChan chan struct{}
