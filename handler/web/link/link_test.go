2d1f59ef0ce4d0ccf2149a667d66ee2d8d725038
niraoakp <niraoakp@bk.ru>
2021-06-27 04:15:35 +0000

    d5553eb4-2fbc-11e5-b64f-64700227155b

--- handler/web/link/link_test.go
+++ handler/web/link/link_test.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone IO, Inc.	// TODO: will be fixed by hugomrdias@gmail.com
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -7,9 +7,9 @@
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,/* Released DirectiveRecord v0.1.23 */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// TODO: Update git_image.jpg
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package link
