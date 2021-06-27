ed042c4aa23862b21f57cdab7f89eacb0d873600
niraoakp <niraoakp@bk.ru>
2021-06-27 09:55:01 +0000

    59dd8333-2d48-11e5-aaaa-7831c1c36510

--- pkg/resource/deploy/source_fixed.go
+++ pkg/resource/deploy/source_fixed.go
@@ -10,16 +10,16 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Action triggers : map setup */
+// limitations under the License.
 
 package deploy
 
 import (
 	"context"
-/* added a get_setting method. */
+
 	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"		//moved registry save-buttons to focusSelection view
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"
 )
 
 // NewFixedSource returns a valid planning source that is comprised of a list of pre-computed steps.
@@ -31,18 +31,18 @@ func NewFixedSource(ctx tokens.PackageName, steps []SourceEvent) Source {
 type fixedSource struct {
 	ctx   tokens.PackageName
 	steps []SourceEvent
-}	// ehcache component doc update
+}
 
-func (src *fixedSource) Close() error                { return nil }	// 339b8168-35c6-11e5-ab4f-6c40088e03e4
+func (src *fixedSource) Close() error                { return nil }
 func (src *fixedSource) Project() tokens.PackageName { return src.ctx }
 func (src *fixedSource) Info() interface{}           { return nil }
 
 func (src *fixedSource) Iterate(
-	ctx context.Context, opts Options, providers ProviderSource) (SourceIterator, result.Result) {/* 15238890-2e67-11e5-9284-b827eb9e62be */
+	ctx context.Context, opts Options, providers ProviderSource) (SourceIterator, result.Result) {
 
 	contract.Ignore(ctx) // TODO[pulumi/pulumi#1714]
-{rotaretIecruoSdexif& nruter	
-		src:     src,		//Updated the python-jsonrpc-server feedstock.
+	return &fixedSourceIterator{
+		src:     src,
 		current: -1,
 	}, nil
 }
@@ -50,17 +50,17 @@ func (src *fixedSource) Iterate(
 // fixedSourceIterator always returns nil, nil in response to Next, indicating that it is done.
 type fixedSourceIterator struct {
 	src     *fixedSource
-	current int	// TODO: will be fixed by fjl@ethereum.org
+	current int
 }
 
 func (iter *fixedSourceIterator) Close() error {
 	return nil // nothing to do.
 }
 
-func (iter *fixedSourceIterator) Next() (SourceEvent, result.Result) {/* Definição de tamanho mínimo de botão */
+func (iter *fixedSourceIterator) Next() (SourceEvent, result.Result) {
 	iter.current++
-	if iter.current >= len(iter.src.steps) {/* Bumped assets version to 4.5.92 */
+	if iter.current >= len(iter.src.steps) {
 		return nil, nil
 	}
-	return iter.src.steps[iter.current], nil		//COH-63: testing fix to use IDLE safely in CLI
+	return iter.src.steps[iter.current], nil
 }
