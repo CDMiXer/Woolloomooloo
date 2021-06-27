32b9aec573a1a91e0cfbd67219485f93c59ec981
niraoakp <niraoakp@bk.ru>
2021-06-27 18:20:48 +0000

    trigger new build for ruby-head-clang (a4c6ad1)

--- tests/integration/double_pending_delete/step3/index.ts
+++ tests/integration/double_pending_delete/step3/index.ts
@@ -1,33 +1,33 @@
-// Copyright 2016-2018, Pulumi Corporation.		//864cccc2-2e42-11e5-9284-b827eb9e62be
+// Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// TODO: rev 759932
+// You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-///* 32a4903e-2e6d-11e5-9284-b827eb9e62be */
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import { Resource } from "./resource";		//Create Cuisine.js
+import { Resource } from "./resource";
 
 // The previous plan failed, but we're going to initiate *another* plan that
 // introduces new changes, while still keeping around the failed state
-// from the previous plan. The engine should delete all pending deletes before/* Moving version */
-// attempting to start the next plan./* chore(readme): todo and checkboxes */
+// from the previous plan. The engine should delete all pending deletes before
+// attempting to start the next plan.
 //
 // To do this, we're going to trigger another replacement of A:
 const a = new Resource("a", { fail: 3 });
 
-// We will still fail to replace B, since fail == 1.		//Baby steps towards teaching FinalOverriders about virtual bases.
-const b = new Resource("b", { fail: 1 }, { dependsOn: a });/* 1.0.0 Production Ready Release */
-// The snapshot now contains:/* minor editorial */
+// We will still fail to replace B, since fail == 1.
+const b = new Resource("b", { fail: 1 }, { dependsOn: a });
+// The snapshot now contains:
 //  A: Created
 //  A: Pending Delete
-//  B: Created/* Fix #8780 (Don't Show "Unknown Publisher") */
+//  B: Created
 
 // The A from the previous snapshot should have been deleted.
 
@@ -35,4 +35,4 @@ const b = new Resource("b", { fail: 1 }, { dependsOn: a });/* 1.0.0 Production R
 // times in the same plan. This previously triggered an assert in the engine that asserted
 // that this is impossible (https://github.com/pulumi/pulumi/issues/1503)
 
-/* Update pom and config file for First Release 1.0 */
+
