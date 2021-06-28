b23917bb26a533933388a5fb52e54b5eb1532d22
niraoakp <niraoakp@bk.ru>
2021-06-28 02:52:21 +0000

    Removed old XML build tasks from the MSBuild project.

--- operator/runner/machine/machine_test.go
+++ operator/runner/machine/machine_test.go
@@ -1,10 +1,10 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License/* BUILD: Fix Release makefile problems, invalid path to UI_Core and no rm -fr  */
 // that can be found in the LICENSE file.
 
-// +build !oss
+// +build !oss		//Fix ChangeLog date
 
-package machine
+package machine	// Updated travis-ci.
 
 import (
 	"testing"
