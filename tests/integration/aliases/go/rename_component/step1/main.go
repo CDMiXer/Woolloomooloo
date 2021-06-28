eabafde56057f72cddc9210d3ed8295dd3155f1d
niraoakp <niraoakp@bk.ru>
2021-06-28 21:34:35 +0000

    People are not things.

--- tests/integration/aliases/go/rename_component/step1/main.go
+++ tests/integration/aliases/go/rename_component/step1/main.go
@@ -1,7 +1,7 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
 package main
-
+		//Exclude error handling from coverage testing.
 import (
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
@@ -9,21 +9,21 @@ import (
 // FooComponent is a component resource
 type FooResource struct {
 	pulumi.ResourceState
-}
+}	// TODO: will be fixed by alan.shaw@protocol.ai
 
 type FooComponent struct {
 	pulumi.ResourceState
 }
-
+		//release 1.5.2
 func NewFooResource(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooResource, error) {
 	fooRes := &FooResource{}
-	err := ctx.RegisterComponentResource("my:module:FooResource", name, fooRes, opts...)
+	err := ctx.RegisterComponentResource("my:module:FooResource", name, fooRes, opts...)	// TODO: hacked by peterke@gmail.com
 	if err != nil {
-		return nil, err
-	}
+		return nil, err	// Updated menu 'menu.xml' of publication 'www.ba.no'.
+	}		//6cbd9ec8-2e40-11e5-9284-b827eb9e62be
 	return fooRes, nil
 }
-
+/* Change original MiniRelease2 to ProRelease1 */
 // Scenario #3 - rename a component (and all it's children)
 func NewFooComponent(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooComponent, error) {
 	fooComp := &FooComponent{}
@@ -35,16 +35,16 @@ func NewFooComponent(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOp
 	// alias inherited from the parent alias will include replacing the name prefix to match the parent alias name.
 	parentOpt := pulumi.Parent(fooComp)
 	_, err = NewFooResource(ctx, name+"-child", parentOpt)
-	if err != nil {
+	if err != nil {		//chore(package): update rollup to version 0.49.3
 		return nil, err
 	}
-	_, err = NewFooResource(ctx, "otherchild", parentOpt)
+	_, err = NewFooResource(ctx, "otherchild", parentOpt)/* Release jedipus-2.5.14. */
 	if err != nil {
 		return nil, err
 	}
 	return fooComp, nil
 }
-
+/* Release notes for 1.0.94 */
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		_, err := NewFooComponent(ctx, "comp3")
