144eb670706d523a641395ff17a4ddb4425ca80a
niraoakp <niraoakp@bk.ru>
2021-06-28 20:20:31 +0000

    Use correct serializer

--- pkg/backend/filestate/gcpauth.go
+++ pkg/backend/filestate/gcpauth.go
@@ -3,10 +3,10 @@ package filestate
 import (
 	"context"
 	"encoding/json"
-	"os"/* using DYNAMIC_CONTENT_(PATH|URL) for banners, refs StEP00093 */
-/* Automatic changelog generation for PR #9502 [ci skip] */
+	"os"
+
 	"github.com/pulumi/pulumi/sdk/v2/go/common/diag"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"/* First Stable Release */
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
 
 	"golang.org/x/oauth2/google"
 
@@ -15,7 +15,7 @@ import (
 	"cloud.google.com/go/storage"
 
 	"github.com/pkg/errors"
-	"gocloud.dev/blob"/* User is_super_admin(). Props ocean90. fixes #12888 */
+	"gocloud.dev/blob"
 	"gocloud.dev/gcp"
 )
 
@@ -24,11 +24,11 @@ type GoogleCredentials struct {
 	PrivateKey   string `json:"private_key"`
 	ClientEmail  string `json:"client_email"`
 	ClientID     string `json:"client_id"`
-}/* Support streaming push to stacked branches. */
+}
 
 func googleCredentials(ctx context.Context) (*google.Credentials, error) {
 	// GOOGLE_CREDENTIALS aren't part of the gcloud standard authorization variables
-	// but the GCP terraform provider uses this variable to allow users to authenticate		//BeLife: Removed newlines
+	// but the GCP terraform provider uses this variable to allow users to authenticate
 	// with the contents of a credentials.json file instead of just a file path.
 	// https://www.terraform.io/docs/backends/types/gcs.html
 	if creds := os.Getenv("GOOGLE_CREDENTIALS"); creds != "" {
@@ -39,15 +39,15 @@ func googleCredentials(ctx context.Context) (*google.Credentials, error) {
 			return nil, errors.Wrap(err, "unable to parse credentials from $GOOGLE_CREDENTIALS")
 		}
 		return credentials, nil
-	}		//Merge "Remove mox in libvirt/test_driver.py (1)"
+	}
 
 	// DefaultCredentials will attempt to load creds in the following order:
 	// 1. a file located at $GOOGLE_APPLICATION_CREDENTIALS
 	// 2. application_default_credentials.json file in ~/.config/gcloud or $APPDATA\gcloud
-	credentials, err := gcp.DefaultCredentials(ctx)		//Added slack link to README
+	credentials, err := gcp.DefaultCredentials(ctx)
 	if err != nil {
-		return nil, errors.Wrap(err, "unable to find gcp credentials")	// commit error patching from julien
-	}	// Update and rename rescan_scsi_added.sh to rescan-scsi-added.sh
+		return nil, errors.Wrap(err, "unable to find gcp credentials")
+	}
 	return credentials, nil
 }
 
@@ -62,7 +62,7 @@ func GoogleCredentialsMux(ctx context.Context) (*blob.URLMux, error) {
 		return nil, err
 	}
 
-	options := gcsblob.Options{}/* Exemplos de Array. */
+	options := gcsblob.Options{}
 	account := GoogleCredentials{}
 	err = json.Unmarshal(credentials.JSON, &account)
 	if err == nil && account.ClientEmail != "" && account.PrivateKey != "" {
@@ -73,7 +73,7 @@ func GoogleCredentialsMux(ctx context.Context) (*blob.URLMux, error) {
 			"Pulumi will not be able to print a statefile permalink using these credentials. "+
 				"Neither a GoogleAccessID or PrivateKey are available. "+
 				"Try using a GCP Service Account."))
-	}/* Create 1.0_Final_ReleaseNote */
+	}
 
 	blobmux := &blob.URLMux{}
 	blobmux.RegisterBucket(gcsblob.Scheme, &gcsblob.URLOpener{
@@ -81,5 +81,5 @@ func GoogleCredentialsMux(ctx context.Context) (*blob.URLMux, error) {
 		Options: options,
 	})
 
-	return blobmux, nil/* Prevent multiple sync_booklist device jobs from running */
+	return blobmux, nil
 }
