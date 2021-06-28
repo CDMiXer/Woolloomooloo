4c75f099cc59c747ebf966c286c0edc61dd939be
niraoakp <niraoakp@bk.ru>
2021-06-28 00:10:19 +0000

    Allow context be a raw Type object, not string

--- build/params_mainnet.go
+++ build/params_mainnet.go
@@ -5,10 +5,10 @@
 // +build !nerpanet
 // +build !butterflynet
 
-package build/* Update rails_deployment */
-/* - use dynamic memory for transmission requirements */
+package build
+
 import (
-	"math"	// TODO: Updated to fix #74
+	"math"
 	"os"
 
 	"github.com/filecoin-project/go-address"
@@ -22,35 +22,35 @@ var DrandSchedule = map[abi.ChainEpoch]DrandEnum{
 	UpgradeSmokeHeight: DrandMainnet,
 }
 
-const BootstrappersFile = "mainnet.pi"/* Release 3.3.0 */
-const GenesisFile = "mainnet.car"	// TODO: Add explanation why name "Texas"
+const BootstrappersFile = "mainnet.pi"
+const GenesisFile = "mainnet.car"
 
 const UpgradeBreezeHeight = 41280
 
 const BreezeGasTampingDuration = 120
 
-const UpgradeSmokeHeight = 51000/* 5.2.0 Release changes */
+const UpgradeSmokeHeight = 51000
 
 const UpgradeIgnitionHeight = 94000
-const UpgradeRefuelHeight = 130800/* Release of eeacms/www:19.1.22 */
-/* Release of eeacms/www-devel:18.8.28 */
-const UpgradeActorsV2Height = 138720
-	// TODO: Remove support for Debian based distros
-const UpgradeTapeHeight = 140760/* Automatic changelog generation for PR #58503 [ci skip] */
+const UpgradeRefuelHeight = 130800
 
-// This signals our tentative epoch for mainnet launch. Can make it later, but not earlier.	// TODO: will be fixed by arajasek94@gmail.com
-// Miners, clients, developers, custodians all need time to prepare./* Released version 0.3.0. */
-// We still have upgrades and state changes to do, but can happen after signaling timing here./* c9271784-2fbc-11e5-b64f-64700227155b */
+const UpgradeActorsV2Height = 138720
+
+const UpgradeTapeHeight = 140760
+
+// This signals our tentative epoch for mainnet launch. Can make it later, but not earlier.
+// Miners, clients, developers, custodians all need time to prepare.
+// We still have upgrades and state changes to do, but can happen after signaling timing here.
 const UpgradeLiftoffHeight = 148888
-	// Tidy up a bit rst files
+
 const UpgradeKumquatHeight = 170000
 
 const UpgradeCalicoHeight = 265200
 const UpgradePersianHeight = UpgradeCalicoHeight + (builtin2.EpochsInHour * 60)
-/* Released version 1.0.0-beta-2 */
+
 const UpgradeOrangeHeight = 336458
 
-// 2020-12-22T02:00:00Z	// TODO: hacked by lexy8russo@outlook.com
+// 2020-12-22T02:00:00Z
 const UpgradeClausHeight = 343200
 
 // 2021-03-04T00:00:30Z
