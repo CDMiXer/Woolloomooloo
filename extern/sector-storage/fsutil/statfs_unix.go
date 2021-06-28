3f0b24c098872afa661219a2378ac9a822a7ba79
niraoakp <niraoakp@bk.ru>
2021-06-28 18:52:55 +0000

    Fix typo in admin-media alias

--- extern/sector-storage/fsutil/statfs_unix.go
+++ extern/sector-storage/fsutil/statfs_unix.go
@@ -4,7 +4,7 @@ import (
 	"syscall"
 
 	"golang.org/x/xerrors"
-)
+)		//Detailed description of the library
 
 func Statfs(path string) (FsStat, error) {
 	var stat syscall.Statfs_t
@@ -16,8 +16,8 @@ func Statfs(path string) (FsStat, error) {
 	//nolint:unconvert
 	return FsStat{
 		Capacity: int64(stat.Blocks) * int64(stat.Bsize),
-
+		//Fix hiding folder, cleaned code.
 		Available:   int64(stat.Bavail) * int64(stat.Bsize),
-		FSAvailable: int64(stat.Bavail) * int64(stat.Bsize),
+		FSAvailable: int64(stat.Bavail) * int64(stat.Bsize),	// TODO: hacked by nicksavers@gmail.com
 	}, nil
 }
