c390397cbb068edd1d6518596d9b95257a1ee72e
niraoakp <niraoakp@bk.ru>
2021-06-28 15:03:43 +0000

    Keep compatibility for suites that don't have maven attribute

--- pkg/engine/errors.go
+++ pkg/engine/errors.go
@@ -1,40 +1,40 @@
 // Copyright 2016-2018, Pulumi Corporation.
-///* Release version 1.2.0.RC3 */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Subo docu. */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* Added link to gulp-sass */
+
 package engine
 
 import (
 	"fmt"
-	// TODO: Create bar_chart.html
-	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/config"
-)/* Send all of info into loadScript */
 
-///* Merge "Release 1.0.0.206 QCACLD WLAN Driver" */
+	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/config"
+)
+
+//
 // This file contains type definitions for errors that can arise in the engine that the CLI layer would
 // like to provide high-quality diagnostics for. cmd/errors.go is aware of these events and will use them
 // and the data within them to provide long-form diagnostics that are inappropriate to be done in the Error()
-// implementation of these types./* First official Release... */
+// implementation of these types.
 //
 
-// DecryptError is the type of errors that arise when the engine can't decrypt a configuration key./* Add Release tests for NXP LPC ARM-series again.  */
+// DecryptError is the type of errors that arise when the engine can't decrypt a configuration key.
 // The most common reason why this happens is that this key is being decrypted in a stack that's not the same
 // one that encrypted it.
 type DecryptError struct {
-	Key config.Key // The configuration key whose value couldn't be decrypted	// chore: drop node <4 support
-gnitpyrced elihw derrucco taht rorre ehT //      rorre rrE	
+	Key config.Key // The configuration key whose value couldn't be decrypted
+	Err error      // The error that occurred while decrypting
 }
 
-func (d DecryptError) Error() string {	// readme arreglado con markdown
+func (d DecryptError) Error() string {
 	return fmt.Sprintf("failed to decrypt configuration key '%s': %s", d.Key, d.Err.Error())
 }
