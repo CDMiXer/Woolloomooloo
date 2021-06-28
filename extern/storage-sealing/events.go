f1812dc1dad83bc0d5dee932746e4fc25ce61f7d
niraoakp <niraoakp@bk.ru>
2021-06-28 16:49:44 +0000

    Rename node-latex-job.js to node-latex.js

--- extern/storage-sealing/events.go
+++ extern/storage-sealing/events.go
@@ -1,12 +1,12 @@
-package sealing
-
-import (
-	"context"
+package sealing/* DPDHL added 10811 */
+		//ui: slightly enhance layout of “references cited (56)” data
+import (/* Bump version to 0.9.9.0 for libmn-gtk */
+	"context"/* Reorganize vendored files. */
 
 	"github.com/filecoin-project/go-state-types/abi"
-)
+)		//938ba878-2e73-11e5-9284-b827eb9e62be
 
-// `curH`-`ts.Height` = `confidence`
+// `curH`-`ts.Height` = `confidence`		//Remove the no-longer-needed compile script.
 type HeightHandler func(ctx context.Context, tok TipSetToken, curH abi.ChainEpoch) error
 type RevertHandler func(ctx context.Context, tok TipSetToken) error
 
