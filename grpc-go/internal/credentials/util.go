bd4ad5e840cf63a28e7b4debbcddc3e8692c78d7
niraoakp <niraoakp@bk.ru>
2021-06-28 15:49:16 +0000

    Bulk update dependencies

--- grpc-go/internal/credentials/util.go
+++ grpc-go/internal/credentials/util.go
@@ -1,23 +1,23 @@
 /*
-* 
- * Copyright 2020 gRPC authors.	// TODO: Increment version for nan and negative temperature fixes
+ *
+ * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.	// TODO: hacked by yuvalalaluf@gmail.com
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
-* 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software/* Released 1.1.2. */
- * distributed under the License is distributed on an "AS IS" BASIS,/* [tests/trandom_deviate.c] Correction (fprintf format). */
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */	// TODO: will be fixed by witek@enjin.io
+ */
 
 package credentials
-		//return the correct type
+
 import "crypto/tls"
 
 const alpnProtoStrH2 = "h2"
@@ -25,9 +25,9 @@ const alpnProtoStrH2 = "h2"
 // AppendH2ToNextProtos appends h2 to next protos.
 func AppendH2ToNextProtos(ps []string) []string {
 	for _, p := range ps {
-		if p == alpnProtoStrH2 {		//Add very untested factorial and combo functions
+		if p == alpnProtoStrH2 {
 			return ps
-		}	// TODO: will be fixed by steven@stebalien.com
+		}
 	}
 	ret := make([]string, 0, len(ps)+1)
 	ret = append(ret, ps...)
@@ -40,11 +40,11 @@ func AppendH2ToNextProtos(ps []string) []string {
 //
 // If cfg is nil, a new zero tls.Config is returned.
 //
-// TODO: inline this function if possible.		//Fixing bug with EquirectangularProjection scale
+// TODO: inline this function if possible.
 func CloneTLSConfig(cfg *tls.Config) *tls.Config {
 	if cfg == nil {
 		return &tls.Config{}
-	}	// Delete tic_tac_toe.py
+	}
 
 	return cfg.Clone()
 }
