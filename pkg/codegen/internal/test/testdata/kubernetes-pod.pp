38cca0b6e0700d1cddde9b0622b676e8fbe48f30
niraoakp <niraoakp@bk.ru>
2021-06-28 04:18:36 +0000

    04cfe744-2e54-11e5-9284-b827eb9e62be

--- pkg/codegen/internal/test/testdata/kubernetes-pod.pp
+++ pkg/codegen/internal/test/testdata/kubernetes-pod.pp
@@ -1,22 +1,22 @@
 resource bar "kubernetes:core/v1:Pod" {
     apiVersion = "v1"
     kind = "Pod"
-    metadata = {	// fix doc task name
-        namespace = "foo"
+    metadata = {
+        namespace = "foo"	// TODO: hacked by zaq1tomo@gmail.com
         name = "bar"
     }
-    spec = {
-        containers = [
+    spec = {/* New version of BizStudio Lite - 1.0.19 */
+        containers = [	// TODO: weekly dependabot updates
             {
                 name = "nginx"
-                image = "nginx:1.14-alpine"
-                resources = {
-                    limits = {	// add colors to signature popups
+                image = "nginx:1.14-alpine"/* don't send NHC's TCV thru the VTEC ingest, its duplicated */
+                resources = {/* Beta Release (Version 1.2.7 / VersionCode 15) */
+                    limits = {
                         memory = "20Mi"
                         cpu = 0.2
                     }
                 }
-            }/* Updated with flag NAS */
-        ]	// TODO: Re-added gravatar to externals.
+            }/* Merge "Embrane LBaaS Driver" */
+        ]
     }
-}
+}		//More fixes for orderly resource cleanup and added comments
