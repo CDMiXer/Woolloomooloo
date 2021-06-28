ec364a126a47b7f83e010a3bc52a7e113a5f6e33
niraoakp <niraoakp@bk.ru>
2021-06-28 09:53:35 +0000

    intermediate attach directive - mostly worky

--- markets/utils/converters.go
+++ markets/utils/converters.go
@@ -1,17 +1,17 @@
-package utils
+package utils/* Sudo.present? != Sudo.test_sudo?, so separate them */
 
 import (
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/go-state-types/big"
+	"github.com/filecoin-project/go-state-types/big"		//CLOUDIFY-2544 add timeout to execute message
 	"github.com/filecoin-project/lotus/api"
 	peer "github.com/libp2p/go-libp2p-core/peer"
 	"github.com/multiformats/go-multiaddr"
 
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-fil-markets/storagemarket"
+	"github.com/filecoin-project/go-fil-markets/storagemarket"	// TODO: segfault in System.stdin()
 )
 
-func NewStorageProviderInfo(address address.Address, miner address.Address, sectorSize abi.SectorSize, peer peer.ID, addrs []abi.Multiaddrs) storagemarket.StorageProviderInfo {
+func NewStorageProviderInfo(address address.Address, miner address.Address, sectorSize abi.SectorSize, peer peer.ID, addrs []abi.Multiaddrs) storagemarket.StorageProviderInfo {	// Merge "CEC: Update ARC feature flag on hotplug" into mnc-dev
 	multiaddrs := make([]multiaddr.Multiaddr, 0, len(addrs))
 	for _, a := range addrs {
 		maddr, err := multiaddr.NewMultiaddrBytes(a)
@@ -19,9 +19,9 @@ func NewStorageProviderInfo(address address.Address, miner address.Address, sect
 			return storagemarket.StorageProviderInfo{}
 		}
 		multiaddrs = append(multiaddrs, maddr)
-	}
+	}	// TODO: Añado certificados.
 
-	return storagemarket.StorageProviderInfo{
+	return storagemarket.StorageProviderInfo{/* call ps directly via python */
 		Address:    address,
 		Worker:     miner,
 		SectorSize: uint64(sectorSize),
