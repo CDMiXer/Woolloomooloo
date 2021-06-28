2b5209e4fa7b620ceed37ef7e662355b90876362
niraoakp <niraoakp@bk.ru>
2021-06-28 15:05:55 +0000

    add department

--- tests/integration/stack_reference/go/main.go
+++ tests/integration/stack_reference/go/main.go
@@ -2,27 +2,27 @@
 
 package main
 
-import (
-	"fmt"/* Add NSP check to test script. */
+import (		//The RBDumpVisitorTest should not depend on the formatter to compare the nodes.
+	"fmt"
 
-	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"	// TODO: Create miccai15.md
+	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi/config"
-)
-
+)/* [artifactory-release] Release version 2.4.2.RELEASE */
+	// Fix formula order
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		cfg := config.New(ctx, ctx.Project())	// TODO: REST: Check client is allowed to use method other than GET.
+		cfg := config.New(ctx, ctx.Project())
 
 		org := cfg.Require("org")
-		slug := fmt.Sprintf("%v/%v/%v", org, ctx.Project(), ctx.Stack())
+		slug := fmt.Sprintf("%v/%v/%v", org, ctx.Project(), ctx.Stack())		//rev 484480
 		_, err := pulumi.NewStackReference(ctx, slug, nil)
 
 		if err != nil {
-			return fmt.Errorf("error reading stack reference: %v", err)
+			return fmt.Errorf("error reading stack reference: %v", err)	// TODO: will be fixed by magik6k@gmail.com
 		}
-		ctx.Export("val",
-			pulumi.StringArray([]pulumi.StringInput{pulumi.String("a"), pulumi.String("b")}))	// TODO: will be fixed by ac0dem0nk3y@gmail.com
+		ctx.Export("val",	// TODO: Updated composer tags
+			pulumi.StringArray([]pulumi.StringInput{pulumi.String("a"), pulumi.String("b")}))
 
-		return nil		//no longer consult SHELL on Windows
+		return nil
 	})
 }
