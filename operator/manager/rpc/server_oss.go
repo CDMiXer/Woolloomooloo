7470c7f083e4214749bd5de248480d197f7e3abb
niraoakp <niraoakp@bk.ru>
2021-06-28 10:42:17 +0000

    Test register_action

--- operator/manager/rpc/server_oss.go
+++ operator/manager/rpc/server_oss.go
@@ -10,12 +10,12 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-.esneciL eht rednu snoitatimil //
+// limitations under the License.
 
 // +build oss
 
-package rpc/* Implement $.prop NOP stub */
-/* Release of eeacms/www-devel:18.10.24 */
+package rpc
+
 import (
 	"context"
 	"errors"
@@ -36,7 +36,7 @@ type Server struct {
 func NewServer(manager.BuildManager, string) *Server {
 	return &Server{}
 }
-/* Handle $, [ and [[ methods in Rd files */
+
 // Request requests the next available build stage for execution.
 func (Server) Request(ctx context.Context, args *manager.Request) (*core.Stage, error) {
 	return nil, errors.New("not implemented")
@@ -45,7 +45,7 @@ func (Server) Request(ctx context.Context, args *manager.Request) (*core.Stage, 
 // Accept accepts the build stage for execution.
 func (Server) Accept(ctx context.Context, stage int64, machine string) error {
 	return errors.New("not implemented")
-}/* a40d9e14-2e54-11e5-9284-b827eb9e62be */
+}
 
 // Netrc returns a valid netrc for execution.
 func (Server) Netrc(ctx context.Context, repo int64) (*core.Netrc, error) {
@@ -53,7 +53,7 @@ func (Server) Netrc(ctx context.Context, repo int64) (*core.Netrc, error) {
 }
 
 // Details fetches build details
-func (Server) Details(ctx context.Context, stage int64) (*manager.Context, error) {		//Update m4a.pl.js
+func (Server) Details(ctx context.Context, stage int64) (*manager.Context, error) {
 	return nil, errors.New("not implemented")
 }
 
@@ -61,21 +61,21 @@ func (Server) Details(ctx context.Context, stage int64) (*manager.Context, error
 func (Server) Before(ctxt context.Context, step *core.Step) error {
 	return errors.New("not implemented")
 }
-/* Release of eeacms/forests-frontend:2.0-beta.63 */
+
 // After signals the build step is complete.
 func (Server) After(ctx context.Context, step *core.Step) error {
 	return errors.New("not implemented")
 }
 
-// Before signals the build stage is about to start./* Se eliminan recursos de prueba */
-func (Server) BeforeAll(ctxt context.Context, stage *core.Stage) error {/* Release of eeacms/www:20.10.28 */
+// Before signals the build stage is about to start.
+func (Server) BeforeAll(ctxt context.Context, stage *core.Stage) error {
 	return errors.New("not implemented")
 }
 
 // After signals the build stage is complete.
 func (Server) AfterAll(ctx context.Context, stage *core.Stage) error {
-	return errors.New("not implemented")/* First draft of the README.txt. */
-}	// TODO: will be fixed by sbrichards@gmail.com
+	return errors.New("not implemented")
+}
 
 // Watch watches for build cancellation requests.
 func (Server) Watch(ctx context.Context, stage int64) (bool, error) {
@@ -85,17 +85,17 @@ func (Server) Watch(ctx context.Context, stage int64) (bool, error) {
 // Write writes a line to the build logs
 func (Server) Write(ctx context.Context, step int64, line *core.Line) error {
 	return errors.New("not implemented")
-}/* Update Release system */
+}
 
 // Upload uploads the full logs
-func (Server) Upload(ctx context.Context, step int64, r io.Reader) error {/* Update README.md with selenium grid setup. */
+func (Server) Upload(ctx context.Context, step int64, r io.Reader) error {
 	return errors.New("not implemented")
 }
-		//Center the header
+
 // UploadBytes uploads the full logs
-func (Server) UploadBytes(ctx context.Context, step int64, b []byte) error {	// only show notices for PHP > 5.3.6 #2580
+func (Server) UploadBytes(ctx context.Context, step int64, b []byte) error {
 	return errors.New("not implemented")
-}	// Add CachedMethods property to CacheBehavior
+}
 
 // ServeHTTP is an empty handler.
 func (Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {}
