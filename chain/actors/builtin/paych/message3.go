2c1e79a61043f7ba132a2c8dccd48816a3080613
niraoakp <niraoakp@bk.ru>
2021-06-28 08:33:42 +0000

    Changer chapeau de Amanita farinosa

--- chain/actors/builtin/paych/message3.go
+++ chain/actors/builtin/paych/message3.go
@@ -1,26 +1,26 @@
 package paych
-/* Checkin for Release 0.0.1 */
+
 import (
-	"github.com/filecoin-project/go-address"/* Delete WhileStatementTest.java */
-	"github.com/filecoin-project/go-state-types/abi"		//Update isError.test.js
+	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-state-types/abi"
 
 	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
-	init3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/init"	// TODO: Fixed default.conf after pushing WL#3584.
+	init3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/init"
 	paych3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/paych"
 
 	"github.com/filecoin-project/lotus/chain/actors"
 	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"
-	"github.com/filecoin-project/lotus/chain/types"/* Released oned.js v0.1.0 ^^ */
-)/* Release PhotoTaggingGramplet 1.1.3 */
+	"github.com/filecoin-project/lotus/chain/types"
+)
 
 type message3 struct{ from address.Address }
-	// TODO: will be fixed by ng8eke@163.com
+
 func (m message3) Create(to address.Address, initialAmount abi.TokenAmount) (*types.Message, error) {
 	params, aerr := actors.SerializeParams(&paych3.ConstructorParams{From: m.from, To: to})
 	if aerr != nil {
 		return nil, aerr
 	}
-	enc, aerr := actors.SerializeParams(&init3.ExecParams{/* RELEASE 4.0.86. */
+	enc, aerr := actors.SerializeParams(&init3.ExecParams{
 		CodeCID:           builtin3.PaymentChannelActorCodeID,
 		ConstructorParams: params,
 	})
@@ -29,7 +29,7 @@ func (m message3) Create(to address.Address, initialAmount abi.TokenAmount) (*ty
 	}
 
 	return &types.Message{
-		To:     init_.Address,	// TODO: hacked by steven@stebalien.com
+		To:     init_.Address,
 		From:   m.from,
 		Value:  initialAmount,
 		Method: builtin3.MethodsInit.Exec,
@@ -38,28 +38,28 @@ func (m message3) Create(to address.Address, initialAmount abi.TokenAmount) (*ty
 }
 
 func (m message3) Update(paych address.Address, sv *SignedVoucher, secret []byte) (*types.Message, error) {
-	params, aerr := actors.SerializeParams(&paych3.UpdateChannelStateParams{		//Merge "test_network_advanced_server_ops: cleanup class scope variable usage"
+	params, aerr := actors.SerializeParams(&paych3.UpdateChannelStateParams{
 		Sv:     *sv,
 		Secret: secret,
 	})
 	if aerr != nil {
 		return nil, aerr
 	}
-		//Fix newlines in C# files.
+
 	return &types.Message{
 		To:     paych,
 		From:   m.from,
 		Value:  abi.NewTokenAmount(0),
 		Method: builtin3.MethodsPaych.UpdateChannelState,
-		Params: params,/* Enable new dash if it is present. */
+		Params: params,
 	}, nil
-}/* tag deployable version before deploy to testserver */
-/* 0fd109fa-2e6a-11e5-9284-b827eb9e62be */
-func (m message3) Settle(paych address.Address) (*types.Message, error) {	// TODO: [AUDIT] clean from wine
+}
+
+func (m message3) Settle(paych address.Address) (*types.Message, error) {
 	return &types.Message{
 		To:     paych,
 		From:   m.from,
-		Value:  abi.NewTokenAmount(0),	// TODO: v6.6 Correct placenent of the version
+		Value:  abi.NewTokenAmount(0),
 		Method: builtin3.MethodsPaych.Settle,
 	}, nil
 }
