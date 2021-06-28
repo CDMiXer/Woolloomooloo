67104e0b15768bfeb3a83e0d90c241849e10f150
niraoakp <niraoakp@bk.ru>
2021-06-28 03:01:47 +0000

    Aggiunta campagna I giorni della ricerca

--- tests/integration/partial_state/step2/index.ts
+++ tests/integration/partial_state/step2/index.ts
@@ -1,4 +1,4 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-/* Merge "wlan: Release 3.2.4.92" */
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// TODO: Added some of Goomph's known adopters to the README.
+
 // file is deliberately empty.
-// The engine should delete "a" since it's in the checkpoint.		//install basedir
+// The engine should delete "a" since it's in the checkpoint.
