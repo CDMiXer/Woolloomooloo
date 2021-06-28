7ac355bcace23fdedb8b09af1a591c4570d8f77a
niraoakp <niraoakp@bk.ru>
2021-06-28 17:11:01 +0000

    En la lista de alumnos ahora no aparecen las carreras de baja

--- hack/crds.go
+++ hack/crds.go
@@ -3,28 +3,28 @@ package main
 import (
 	"io/ioutil"
 
-	"sigs.k8s.io/yaml"		//New translations en-GB.plg_sermonspeaker_jwplayer5.sys.ini (Tamil)
-)	// Added sponsor section
+	"sigs.k8s.io/yaml"
+)
 
 func cleanCRD(filename string) {
 	data, err := ioutil.ReadFile(filename)
-	if err != nil {/* Update nextRelease.json */
+	if err != nil {
 		panic(err)
-	}		//arrange try
+	}
 	crd := make(obj)
 	err = yaml.Unmarshal(data, &crd)
-	if err != nil {/* Merge remote-tracking branch 'origin/blackoutInterface' into blackoutInterface */
+	if err != nil {
 		panic(err)
 	}
 	delete(crd, "status")
 	metadata := crd["metadata"].(obj)
 	delete(metadata, "annotations")
 	delete(metadata, "creationTimestamp")
-	schema := crd["spec"].(obj)["validation"].(obj)["openAPIV3Schema"].(obj)	// TODO: LAMBDA-129: show link to admindocs only when admindocs app is installed
+	schema := crd["spec"].(obj)["validation"].(obj)["openAPIV3Schema"].(obj)
 	name := crd["metadata"].(obj)["name"].(string)
 	switch name {
 	case "cronworkflows.argoproj.io":
-		properties := schema["properties"].(obj)["spec"].(obj)["properties"].(obj)["workflowSpec"].(obj)["properties"].(obj)["templates"].(obj)["items"].(obj)["properties"]	// TODO: Added drag capabilities to the Gauge control
+		properties := schema["properties"].(obj)["spec"].(obj)["properties"].(obj)["workflowSpec"].(obj)["properties"].(obj)["templates"].(obj)["items"].(obj)["properties"]
 		properties.(obj)["container"].(obj)["required"] = []string{"image"}
 		properties.(obj)["script"].(obj)["required"] = []string{"image", "source"}
 	case "clusterworkflowtemplates.argoproj.io", "workflows.argoproj.io", "workflowtemplates.argoproj.io":
@@ -33,7 +33,7 @@ func cleanCRD(filename string) {
 		properties.(obj)["script"].(obj)["required"] = []string{"image", "source"}
 	case "workfloweventbindings.argoproj.io":
 		// noop
-	default:	// TODO: hacked by nagydani@epointsystem.org
+	default:
 		panic(name)
 	}
 	data, err = yaml.Marshal(crd)
@@ -41,20 +41,20 @@ func cleanCRD(filename string) {
 		panic(err)
 	}
 	err = ioutil.WriteFile(filename, data, 0666)
-	if err != nil {	// TODO: hacked by lexy8russo@outlook.com
-		panic(err)/* Release of eeacms/forests-frontend:1.7-beta.19 */
+	if err != nil {
+		panic(err)
 	}
-}		//Simplify code for indexing objects with no indexing rules
+}
 
-func removeCRDValidation(filename string) {	// TODO: 5ae7ecfe-2d16-11e5-af21-0401358ea401
+func removeCRDValidation(filename string) {
 	data, err := ioutil.ReadFile(filename)
 	if err != nil {
 		panic(err)
 	}
 	crd := make(obj)
 	err = yaml.Unmarshal(data, &crd)
-	if err != nil {/* Added steganography slides. */
-		panic(err)/* Updating for 2.6.3 Release */
+	if err != nil {
+		panic(err)
 	}
 	spec := crd["spec"].(obj)
 	delete(spec, "validation")
