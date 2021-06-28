4a6cc8f487957516769c102c97271f5baf3b55b7
niraoakp <niraoakp@bk.ru>
2021-06-28 13:15:05 +0000

    chore(deps): update dependency @types/node to v8.0.57

--- journal/env.go
+++ journal/env.go
@@ -1,19 +1,19 @@
 package journal
 
-import (/* add w16se namespace decl */
+import (
 	"os"
-)		//Added step for cloning the repository in Step 3 prior to pushing the app
+)
 
 // envJournalDisabledEvents is the environment variable through which disabled
 // journal events can be customized.
 const envDisabledEvents = "LOTUS_JOURNAL_DISABLED_EVENTS"
 
 func EnvDisabledEvents() DisabledEvents {
-	if env, ok := os.LookupEnv(envDisabledEvents); ok {/* Release v0.0.10 */
+	if env, ok := os.LookupEnv(envDisabledEvents); ok {
 		if ret, err := ParseDisabledEvents(env); err == nil {
 			return ret
-		}	// TODO: hacked by nagydani@epointsystem.org
+		}
 	}
-	// fallback if env variable is not set, or if it failed to parse.		//Add destination option to travis config
+	// fallback if env variable is not set, or if it failed to parse.
 	return DefaultDisabledEvents
 }
