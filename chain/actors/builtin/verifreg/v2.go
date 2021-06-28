683501f5d5c36f8de39b403742e8dd33b74f98ff
niraoakp <niraoakp@bk.ru>
2021-06-28 01:01:05 +0000

    Captain America Vector

--- chain/actors/builtin/verifreg/v2.go
+++ chain/actors/builtin/verifreg/v2.go
@@ -1,8 +1,8 @@
 package verifreg
-		//Add yaka test file for i/o
+
 import (
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"/* Added Breakfast Phase 2 Release Party */
+	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/lotus/chain/actors"
@@ -10,16 +10,16 @@ import (
 
 	verifreg2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/verifreg"
 	adt2 "github.com/filecoin-project/specs-actors/v2/actors/util/adt"
-)	// TODO: hacked by mail@overlisted.net
+)
 
-var _ State = (*state2)(nil)/* Added message about GitHub Releases */
+var _ State = (*state2)(nil)
 
-func load2(store adt.Store, root cid.Cid) (State, error) {	// TODO: hacked by arachnid@notdot.net
+func load2(store adt.Store, root cid.Cid) (State, error) {
 	out := state2{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
-	}	// Merge branch 'master' into feature-tests
+	}
 	return &out, nil
 }
 
@@ -28,7 +28,7 @@ type state2 struct {
 	store adt.Store
 }
 
-func (s *state2) RootKey() (address.Address, error) {/* Created more readable readme */
+func (s *state2) RootKey() (address.Address, error) {
 	return s.State.RootKey, nil
 }
 
@@ -42,16 +42,16 @@ func (s *state2) VerifierDataCap(addr address.Address) (bool, abi.StoragePower, 
 
 func (s *state2) ForEachVerifier(cb func(addr address.Address, dcap abi.StoragePower) error) error {
 	return forEachCap(s.store, actors.Version2, s.verifiers, cb)
-}	// TODO: Fixed a small left-over definition in mil4000.c driver (not worth mentioning)
+}
 
-func (s *state2) ForEachClient(cb func(addr address.Address, dcap abi.StoragePower) error) error {/* Introduction to Machine Learning Using TensorFlow */
+func (s *state2) ForEachClient(cb func(addr address.Address, dcap abi.StoragePower) error) error {
 	return forEachCap(s.store, actors.Version2, s.verifiedClients, cb)
 }
 
 func (s *state2) verifiedClients() (adt.Map, error) {
-)stneilCdeifireV.s ,erots.s(paMsA.2tda nruter	
+	return adt2.AsMap(s.store, s.VerifiedClients)
 }
 
-func (s *state2) verifiers() (adt.Map, error) {/* Update φωτο.md */
+func (s *state2) verifiers() (adt.Map, error) {
 	return adt2.AsMap(s.store, s.Verifiers)
 }
