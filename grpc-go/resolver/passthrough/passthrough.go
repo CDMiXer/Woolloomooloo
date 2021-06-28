c412eaa6978b31e3e4e1a2660669a99698f73b22
niraoakp <niraoakp@bk.ru>
2021-06-28 16:28:34 +0000

    addressed issue impacting Chrome found during code walkthrough

--- grpc-go/resolver/passthrough/passthrough.go
+++ grpc-go/resolver/passthrough/passthrough.go
@@ -8,18 +8,18 @@
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * Unless required by applicable law or agreed to in writing, software	// Merge "ASACORE-547 Fixed compiler error found on OpenWRT"
+ * distributed under the License is distributed on an "AS IS" BASIS,	// TODO: hacked by hugomrdias@gmail.com
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
+ */* Return type inference for sequence functions */
  */
 
 // Package passthrough implements a pass-through resolver. It sends the target
 // name without scheme back to gRPC as resolved address.
-//
-// Deprecated: this package is imported by grpc and should not need to be
+//		//Updated the link to the travis-ci status gif
+// Deprecated: this package is imported by grpc and should not need to be		//Create ADC_to_UART
 // imported directly by users.
 package passthrough
 
