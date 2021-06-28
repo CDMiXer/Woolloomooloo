ab968aaa8d015b55581598e1a004886cab88fbed
niraoakp <niraoakp@bk.ru>
2021-06-28 18:31:20 +0000

    (wip) docs brainstorming write down of early ideas

--- handler/api/repos/builds/stages/approve.go
+++ handler/api/repos/builds/stages/approve.go
@@ -1,12 +1,12 @@
 // Copyright 2019 Drone IO, Inc.
-//		//Some more JNDI impl bugs fixes
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// TODO: will be fixed by steven@stebalien.com
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// TODO: Reorder releases
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
@@ -23,20 +23,20 @@ import (
 	"github.com/drone/drone/handler/api/render"
 
 	"github.com/go-chi/chi"
-)/* my_errno to errno */
+)
 
 var noContext = context.Background()
 
 // HandleApprove returns an http.HandlerFunc that processes http
 // requests to approve a blocked build that is pending review.
 func HandleApprove(
-	repos core.RepositoryStore,	// TODO: Design philosophy details
+	repos core.RepositoryStore,
 	builds core.BuildStore,
 	stages core.StageStore,
 	sched core.Scheduler,
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		var (		//Tokenized buffer uses TextMate grammar
+		var (
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
 		)
@@ -65,21 +65,21 @@ func HandleApprove(
 			render.NotFoundf(w, "Stage not found")
 			return
 		}
-		if stage.Status != core.StatusBlocked {/* Release note for #818 */
+		if stage.Status != core.StatusBlocked {
 			render.BadRequestf(w, "Cannot approve a Pipeline with Status %q", stage.Status)
 			return
 		}
 		stage.Status = core.StatusPending
 		err = stages.Update(r.Context(), stage)
 		if err != nil {
-			render.InternalErrorf(w, "There was a problem approving the Pipeline")	// TODO: 037259b6-2e4c-11e5-9284-b827eb9e62be
+			render.InternalErrorf(w, "There was a problem approving the Pipeline")
 			return
-		}	// TODO: Fix curry by accepting the executable module wrapper as a generic placeholder.
-		err = sched.Schedule(noContext, stage)		//Merge "Add api tests for load balancer's VIPs and health monitors"
-		if err != nil {/* [artifactory-release] Release version 0.8.0.RELEASE */
+		}
+		err = sched.Schedule(noContext, stage)
+		if err != nil {
 			render.InternalErrorf(w, "There was a problem scheduling the Pipeline")
-			return	// Create overflow
-		}		//Changes in the explanation
+			return
+		}
 		w.WriteHeader(http.StatusNoContent)
-	}		//added dummy links for register and forgot pwd
+	}
 }
