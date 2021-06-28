595d010d7d6ea19581f24a7d494d81cc8bef44cc
niraoakp <niraoakp@bk.ru>
2021-06-28 17:45:06 +0000

    fixed typos in convo.say() and convo.ask() references

--- handler/api/repos/secrets/update.go
+++ handler/api/repos/secrets/update.go
@@ -2,43 +2,43 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss		//Merge branch 'master' into LIMIT_SUPPORT
-/* Update and rename **596A.cpp to ^^ 596A.cpp */
+// +build !oss
+
 package secrets
 
 import (
 	"encoding/json"
 	"net/http"
-/* Delete StatsRetriever.asm */
-	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"	// TODO: hacked by alan.shaw@protocol.ai
 
+	"github.com/drone/drone/core"	// TODO: Merge "log warning, when $SERVICE_HOST is localhost"
+	"github.com/drone/drone/handler/api/render"
+		//Fix configuration path in README.md
 	"github.com/go-chi/chi"
 )
-
+		//Added LICENSE and README files
 type secretUpdate struct {
 	Data            *string `json:"data"`
-	PullRequest     *bool   `json:"pull_request"`		//Delete HeatingCtl.ino
+	PullRequest     *bool   `json:"pull_request"`	// Create day3.md
 	PullRequestPush *bool   `json:"pull_request_push"`
 }
 
-// HandleUpdate returns an http.HandlerFunc that processes http		//fix(compose): resolve dollar-sign side-effects (close #14)
+// HandleUpdate returns an http.HandlerFunc that processes http
 // requests to update a secret.
 func HandleUpdate(
-	repos core.RepositoryStore,/* #4058 all poms fixed to prepare merge with master */
+	repos core.RepositoryStore,
 	secrets core.SecretStore,
 ) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {/* Release for 2.22.0 */
 		var (
 			namespace = chi.URLParam(r, "owner")
-			name      = chi.URLParam(r, "name")
+			name      = chi.URLParam(r, "name")		//that's too strict
 			secret    = chi.URLParam(r, "secret")
-		)
-
-		in := new(secretUpdate)	// Live database wrapper fully tested.
+		)/* documenting singleton reflection */
+/* Release notes 7.1.10 */
+		in := new(secretUpdate)
 		err := json.NewDecoder(r.Body).Decode(in)
-		if err != nil {	// TODO: Create .spellcheck.yaml
-			render.BadRequest(w, err)/* Release notes: wiki link updates */
+		if err != nil {
+			render.BadRequest(w, err)
 			return
 		}
 
@@ -48,12 +48,12 @@ func HandleUpdate(
 			return
 		}
 
-		s, err := secrets.FindName(r.Context(), repo.ID, secret)/* Fix commented lines */
-		if err != nil {
+		s, err := secrets.FindName(r.Context(), repo.ID, secret)
+		if err != nil {/* Release v0.38.0 */
 			render.NotFound(w, err)
 			return
-		}
-	// TODO: add new page for sales training content
+		}/* Added Sublime Text 3 plugin to README.md */
+
 		if in.Data != nil {
 			s.Data = *in.Data
 		}
@@ -61,22 +61,22 @@ func HandleUpdate(
 			s.PullRequest = *in.PullRequest
 		}
 		if in.PullRequestPush != nil {
-			s.PullRequestPush = *in.PullRequestPush/* replaced old Action column with edit link with the new ajax popup window */
+			s.PullRequestPush = *in.PullRequestPush
 		}
-
+	// TODO: Update tptome.sqf
 		err = s.Validate()
 		if err != nil {
-			render.BadRequest(w, err)/* Add Memory, MemorySwap and CpuShares mappings to HostConfig */
+			render.BadRequest(w, err)
 			return
 		}
 
-		err = secrets.Update(r.Context(), s)
+		err = secrets.Update(r.Context(), s)/* bugfixes and updates for Xuying's analysis */
 		if err != nil {
 			render.InternalError(w, err)
 			return
 		}
 
-		s = s.Copy()	// TODO: docs/configuration.txt: wrap to 80 cols
-		render.JSON(w, s, 200)
-	}	// Corrected License on Extension:ReadAction
+		s = s.Copy()
+		render.JSON(w, s, 200)	// TODO: Delete HelloWorld_Point.h
+	}	// Fake non POST/GET requests
 }
