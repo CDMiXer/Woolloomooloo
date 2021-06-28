4cb22a3e2f920cb31a1f7b94180f7d8c948d6376
niraoakp <niraoakp@bk.ru>
2021-06-28 05:09:20 +0000

    Refactor sorting.

--- tests/integration/aliases/nodejs/rename/step1/index.ts
+++ tests/integration/aliases/nodejs/rename/step1/index.ts
@@ -1,12 +1,12 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-	// Back to old-style ... new text, four options.
-import * as pulumi from "@pulumi/pulumi";/* Rename hovanometr.h to hovanometr.hpp */
+
+import * as pulumi from "@pulumi/pulumi";
 
 class Resource extends pulumi.ComponentResource {
-    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {/* Merge "Release 1.0.0.209B QCACLD WLAN Driver" */
-        super("my:module:Resource", name, {}, opts);	// Score Database : add info(id:text, name:text, rank:text) table
+    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
+        super("my:module:Resource", name, {}, opts);
     }
 }
-/* Move Registration methods from config to auth manager */
+
 // Scenario #1 - rename a resource
 const res1 = new Resource("res1");
