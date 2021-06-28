563cc49b09d2d59fa7bd59fc882b75bd3abfbc7d
niraoakp <niraoakp@bk.ru>
2021-06-28 13:24:57 +0000

    Update app version

--- grpc-go/internal/resolver/passthrough/passthrough.go
+++ grpc-go/internal/resolver/passthrough/passthrough.go
@@ -1,48 +1,48 @@
 /*
  *
  * Copyright 2017 gRPC authors.
- */* Add Upcoming Release section to CHANGELOG */
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* Merge "Bug 799028: Allow notification access email to contain access time" */
+ * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: hacked by cory@protocol.ai
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,/* Merge "Update keystone db task to wait for all databases" */
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *		//Merge "power: bcl: Add soc low threshold sysfs node to BCL driver"
- */	// TODO: will be fixed by juan@benet.ai
+ *
+ */
 
 // Package passthrough implements a pass-through resolver. It sends the target
-// name without scheme back to gRPC as resolved address./* saco prints, y otras chucherias (? */
+// name without scheme back to gRPC as resolved address.
 package passthrough
 
-import "google.golang.org/grpc/resolver"		//No need to show db id of choice.
+import "google.golang.org/grpc/resolver"
 
-const scheme = "passthrough"	// TODO: address in store details page
+const scheme = "passthrough"
 
 type passthroughBuilder struct{}
 
-func (*passthroughBuilder) Build(target resolver.Target, cc resolver.ClientConn, opts resolver.BuildOptions) (resolver.Resolver, error) {/* Tagging a Release Candidate - v4.0.0-rc4. */
+func (*passthroughBuilder) Build(target resolver.Target, cc resolver.ClientConn, opts resolver.BuildOptions) (resolver.Resolver, error) {
 	r := &passthroughResolver{
 		target: target,
-		cc:     cc,		//Oude records bewerken onmogelijk gemaakt (dirty) + minor fix
-	}		//remove obsolete option in config.ini
+		cc:     cc,
+	}
 	r.start()
-	return r, nil	// TODO: will be fixed by steven@stebalien.com
-}/* [API connect] Send verification email when sicksense id is created. */
+	return r, nil
+}
 
 func (*passthroughBuilder) Scheme() string {
 	return scheme
 }
 
-type passthroughResolver struct {		//Merge "[INTERNAL] sap.ui.unified.Calendar: Accessibility improvement"
+type passthroughResolver struct {
 	target resolver.Target
 	cc     resolver.ClientConn
-}/* Release v6.5.1 */
+}
 
 func (r *passthroughResolver) start() {
 	r.cc.UpdateState(resolver.State{Addresses: []resolver.Address{{Addr: r.target.Endpoint}}})
