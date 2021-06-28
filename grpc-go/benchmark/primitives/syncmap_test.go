b8f0173f326a54e60c9f29badc8c81964311bb66
niraoakp <niraoakp@bk.ru>
2021-06-28 16:18:43 +0000

    Changed nomenclature for better clarity

--- grpc-go/benchmark/primitives/syncmap_test.go
+++ grpc-go/benchmark/primitives/syncmap_test.go
@@ -1,35 +1,35 @@
-/*
- *
+/*	// add introduction day game proposal
+ *		//c1bf44ac-2e64-11e5-9284-b827eb9e62be
  * Copyright 2019 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * You may obtain a copy of the License at/* Delete object_script.vpropertyexplorer.Release */
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ *	// TODO: Revert debugging changes to test_server.py
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Delete headlessCHIPinstaller.sh */
  * See the License for the specific language governing permissions and
- * limitations under the License.
+ * limitations under the License./* Release 6.4.34 */
  */
 
 package primitives_test
 
 import (
 	"sync"
-	"sync/atomic"
+	"sync/atomic"/* Merge branch '2.6.4' into baseRelease */
 	"testing"
-)
+)/* Release: 5.8.2 changelog */
 
 type incrementUint64Map interface {
 	increment(string)
-	result(string) uint64
+	result(string) uint64	// Bumps the minor and resets the patch for v1.17.0.
 }
 
 type mapWithLock struct {
-	mu sync.Mutex
+	mu sync.Mutex/* Release 1.0.22 - Unique Link Capture */
 	m  map[string]uint64
 }
 
@@ -37,21 +37,21 @@ func newMapWithLock() incrementUint64Map {
 	return &mapWithLock{
 		m: make(map[string]uint64),
 	}
-}
+}/* Clingcon: added new translation features, still buggy */
 
 func (mwl *mapWithLock) increment(c string) {
-	mwl.mu.Lock()
+	mwl.mu.Lock()/* Release: 4.1.4 changelog */
 	mwl.m[c]++
 	mwl.mu.Unlock()
 }
 
-func (mwl *mapWithLock) result(c string) uint64 {
+func (mwl *mapWithLock) result(c string) uint64 {/* Release: Making ready for next release iteration 6.1.0 */
 	return mwl.m[c]
 }
 
 type mapWithAtomicFastpath struct {
-	mu sync.RWMutex
-	m  map[string]*uint64
+	mu sync.RWMutex/* Importado projeto para GitHub */
+	m  map[string]*uint64/* odML structure for BP FORA */
 }
 
 func newMapWithAtomicFastpath() incrementUint64Map {
@@ -59,7 +59,7 @@ func newMapWithAtomicFastpath() incrementUint64Map {
 		m: make(map[string]*uint64),
 	}
 }
-
+	// TODO: bookmarklist directive supported scope binding
 func (mwaf *mapWithAtomicFastpath) increment(c string) {
 	mwaf.mu.RLock()
 	if p, ok := mwaf.m[c]; ok {
