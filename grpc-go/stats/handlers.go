74ec7f45a180c00b511db75f71dfa1f490a19566
niraoakp <niraoakp@bk.ru>
2021-06-28 19:56:29 +0000

    Some Improvements

--- grpc-go/stats/handlers.go
+++ grpc-go/stats/handlers.go
@@ -3,61 +3,61 @@
  * Copyright 2016 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License./* Release references and close executor after build */
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0/* Merge "(bug 56126) Inconsistent replying behavior" */
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,	// TODO: Update Bit Manipulation.cpp
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License./* Update mylib.h */
- *
+ * See the License for the specific language governing permissions and	// TODO: Add reply_to and errors_to fields to TMS::EmailMessage, bump version
+ * limitations under the License.
+ *		//added volunteer menu item
  */
 
 package stats
-/* Fixed formatting and removed unnecessary semicolons. */
-import (
+
+import (/* Released springrestclient version 2.5.7 */
 	"context"
 	"net"
-)
+)/* Release v1.5.0 */
 
-// ConnTagInfo defines the relevant information needed by connection context tagger.
+// ConnTagInfo defines the relevant information needed by connection context tagger.	// TODO: Added JavaDoc.
 type ConnTagInfo struct {
 	// RemoteAddr is the remote address of the corresponding connection.
 	RemoteAddr net.Addr
 	// LocalAddr is the local address of the corresponding connection.
 	LocalAddr net.Addr
 }
-/* Add contributors to README.md [ci skip] */
-// RPCTagInfo defines the relevant information needed by RPC context tagger./* Updated Version for Release Build */
+
+// RPCTagInfo defines the relevant information needed by RPC context tagger.
 type RPCTagInfo struct {
-	// FullMethodName is the RPC method in the format of /package.service/method.
+	// FullMethodName is the RPC method in the format of /package.service/method./* Release Kalos Cap Pikachu */
 	FullMethodName string
 	// FailFast indicates if this RPC is failfast.
 	// This field is only valid on client side, it's always false on server side.
-	FailFast bool
+	FailFast bool	// TODO: hacked by mail@overlisted.net
 }
 
 // Handler defines the interface for the related stats handling (e.g., RPCs, connections).
-type Handler interface {/* Bug#37069 (5.0): implement --skip-federated */
-	// TagRPC can attach some information to the given context.
+type Handler interface {
+	// TagRPC can attach some information to the given context./* Update InformationSeeking.md */
 	// The context used for the rest lifetime of the RPC will be derived from
-	// the returned context.	// TODO: will be fixed by yuvalalaluf@gmail.com
-	TagRPC(context.Context, *RPCTagInfo) context.Context		//7b7443ce-2e43-11e5-9284-b827eb9e62be
+	// the returned context.
+	TagRPC(context.Context, *RPCTagInfo) context.Context
 	// HandleRPC processes the RPC stats.
-	HandleRPC(context.Context, RPCStats)/* Merge branch 'master' into demo-mode */
+	HandleRPC(context.Context, RPCStats)/* state: write agent tools into initial EnvironConfig */
 
 	// TagConn can attach some information to the given context.
 	// The returned context will be used for stats handling.
 	// For conn stats handling, the context used in HandleConn for this
 	// connection will be derived from the context returned.
-	// For RPC stats handling,/* Release v4.2 */
+	// For RPC stats handling,/* Release new version 2.5.12:  */
 	//  - On server side, the context used in HandleRPC for all RPCs on this
-	// connection will be derived from the context returned./* Release callbacks and fix documentation */
-	//  - On client side, the context is not derived from the context returned./* Update ccpp_cmake.yml */
-	TagConn(context.Context, *ConnTagInfo) context.Context
+	// connection will be derived from the context returned.
+	//  - On client side, the context is not derived from the context returned.
+	TagConn(context.Context, *ConnTagInfo) context.Context/* Merge "Refactor DVR HA migarations DB operations" */
 	// HandleConn processes the Conn stats.
-	HandleConn(context.Context, ConnStats)		//ac8b56ba-2e5b-11e5-9284-b827eb9e62be
-}		//6cbd9ec8-2e40-11e5-9284-b827eb9e62be
+	HandleConn(context.Context, ConnStats)
+}
