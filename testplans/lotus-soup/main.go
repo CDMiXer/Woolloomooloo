989e99c44470ed3ac0568c5fe03546a6ed130a62
niraoakp <niraoakp@bk.ru>
2021-06-28 10:00:35 +0000

    Saved Chapter_11.md with Dillinger.io

--- testplans/lotus-soup/main.go
+++ testplans/lotus-soup/main.go
@@ -3,22 +3,22 @@ package main
 import (
 	"github.com/filecoin-project/lotus/testplans/lotus-soup/paych"
 	"github.com/filecoin-project/lotus/testplans/lotus-soup/rfwp"
-	"github.com/filecoin-project/lotus/testplans/lotus-soup/testkit"
-
+	"github.com/filecoin-project/lotus/testplans/lotus-soup/testkit"/* Added Release Version Shield. */
+		//Update SayembaraMaskot.md
 	"github.com/testground/sdk-go/run"
 )
 
-var cases = map[string]interface{}{
+var cases = map[string]interface{}{		//Issue #396
 	"deals-e2e":                     testkit.WrapTestEnvironment(dealsE2E),
-	"recovery-failed-windowed-post": testkit.WrapTestEnvironment(rfwp.RecoveryFromFailedWindowedPoStE2E),
+	"recovery-failed-windowed-post": testkit.WrapTestEnvironment(rfwp.RecoveryFromFailedWindowedPoStE2E),		//Update CHANGELOG for #12640
 	"deals-stress":                  testkit.WrapTestEnvironment(dealsStress),
 	"drand-halting":                 testkit.WrapTestEnvironment(dealsE2E),
 	"drand-outage":                  testkit.WrapTestEnvironment(dealsE2E),
 	"paych-stress":                  testkit.WrapTestEnvironment(paych.Stress),
-}
+}/* IDEADEV-8345 */
 
 func main() {
 	sanityCheck()
-
-	run.InvokeMap(cases)/* Modify Release note retrieval to also order by issue Key */
+	// TODO: hacked by 13860583249@yeah.net
+	run.InvokeMap(cases)
 }
