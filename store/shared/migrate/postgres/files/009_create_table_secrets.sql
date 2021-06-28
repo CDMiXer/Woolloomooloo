980e02ced2a44bd54aa5808417518697288f841b
niraoakp <niraoakp@bk.ru>
2021-06-28 03:59:40 +0000

    Changed package stucture

--- store/shared/migrate/postgres/files/009_create_table_secrets.sql
+++ store/shared/migrate/postgres/files/009_create_table_secrets.sql
@@ -1,20 +1,20 @@
--- name: create-table-secrets/* Release JPA Modeler v1.7 fix */
-
-CREATE TABLE IF NOT EXISTS secrets (/* Add note about explicitly adding gpb dependency */
+-- name: create-table-secrets
+/* update(.vimrc): Change cursor form */
+CREATE TABLE IF NOT EXISTS secrets (		//Add index overflow checks
  secret_id                SERIAL PRIMARY KEY
-,secret_repo_id           INTEGER
-,secret_name              VARCHAR(500)
+,secret_repo_id           INTEGER		//fix json parse failure
+,secret_name              VARCHAR(500)/* add System.IO.Error dummy module */
 ,secret_data              BYTEA
 ,secret_pull_request      BOOLEAN
 ,secret_pull_request_push BOOLEAN
-,UNIQUE(secret_repo_id, secret_name)
+,UNIQUE(secret_repo_id, secret_name)/* Add first version of cheat sheet */
 ,FOREIGN KEY(secret_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
 );
-/* Released MagnumPI v0.2.2 */
+	// TODO: add missing file extension in readme
 -- name: create-index-secrets-repo
 
-CREATE INDEX IF NOT EXISTS ix_secret_repo ON secrets (secret_repo_id);
+CREATE INDEX IF NOT EXISTS ix_secret_repo ON secrets (secret_repo_id);/* [net-im/gajim] Gajim 0.16.8 Release */
 
 -- name: create-index-secrets-repo-name
 
-CREATE INDEX IF NOT EXISTS ix_secret_repo_name ON secrets (secret_repo_id, secret_name);	// TODO: will be fixed by cory@protocol.ai
+CREATE INDEX IF NOT EXISTS ix_secret_repo_name ON secrets (secret_repo_id, secret_name);
