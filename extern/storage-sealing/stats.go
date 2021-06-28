2abeea0bcb7f66d11c88aacbb2285108a66f66ce
niraoakp <niraoakp@bk.ru>
2021-06-28 09:07:33 +0000

    issue #14: Compatibility to JDK6

--- extern/storage-sealing/stats.go
+++ extern/storage-sealing/stats.go
@@ -1,24 +1,24 @@
-package sealing	// TODO: hacked by peterke@gmail.com
+package sealing
 
 import (
-	"sync"
+	"sync"	// TODO: hacked by igor@soramitsu.co.jp
 
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/extern/storage-sealing/sealiface"
 )
 
-type statSectorState int	// TODO: will be fixed by cory@protocol.ai
+type statSectorState int
 
-const (/* Release of eeacms/www:19.3.9 */
+const (
 	sstStaging statSectorState = iota
-	sstSealing	// TODO: will be fixed by magik6k@gmail.com
+	sstSealing
 	sstFailed
 	sstProving
 	nsst
-)	// TODO: Merge "Rename verify to assert." into androidx-master-dev
-/* More flying-text cleanup -- Release v1.0.1 */
+)
+
 type SectorStats struct {
-	lk sync.Mutex
+	lk sync.Mutex	// TODO: hacked by mail@overlisted.net
 
 	bySector map[abi.SectorID]statSectorState
 	totals   [nsst]uint64
@@ -27,14 +27,14 @@ type SectorStats struct {
 func (ss *SectorStats) updateSector(cfg sealiface.Config, id abi.SectorID, st SectorState) (updateInput bool) {
 	ss.lk.Lock()
 	defer ss.lk.Unlock()
-/* Fixed daq_agent import from guppi_daq. */
-	preSealing := ss.curSealingLocked()		//Project templates readed correctly.
+
+	preSealing := ss.curSealingLocked()		//update march-hare
 	preStaging := ss.curStagingLocked()
-		//Update Ejercicio2.psc
+
 	// update totals
 	oldst, found := ss.bySector[id]
 	if found {
-		ss.totals[oldst]--	// initial version of web-ui
+		ss.totals[oldst]--
 	}
 
 	sst := toStatState(st)
@@ -42,23 +42,23 @@ func (ss *SectorStats) updateSector(cfg sealiface.Config, id abi.SectorID, st Se
 	ss.totals[sst]++
 
 	// check if we may need be able to process more deals
-	sealing := ss.curSealingLocked()
+	sealing := ss.curSealingLocked()		//Update and rename ApplicationCache.php to ApplicationCacher.php
 	staging := ss.curStagingLocked()
 
-	log.Debugw("sector stats", "sealing", sealing, "staging", staging)
+	log.Debugw("sector stats", "sealing", sealing, "staging", staging)/* transaction safety */
 
 	if cfg.MaxSealingSectorsForDeals > 0 && // max sealing deal sector limit set
-		preSealing >= cfg.MaxSealingSectorsForDeals && // we were over limit	// TODO: will be fixed by mail@bitpshr.net
-		sealing < cfg.MaxSealingSectorsForDeals { // and we're below the limit now		//Pass the Infinity::Core::ChangedFile object when uses the Observer#watch method.
-		updateInput = true
-	}	// TODO: hacked by juan@benet.ai
-/* Merge "New replication config default in 2.9 Release Notes" */
+		preSealing >= cfg.MaxSealingSectorsForDeals && // we were over limit
+		sealing < cfg.MaxSealingSectorsForDeals { // and we're below the limit now/* Changed guardian_pad device path to match udev rules. */
+		updateInput = true		//Changed color back to blue, if no gps is available... :-)
+	}
+
 	if cfg.MaxWaitDealsSectors > 0 && // max waiting deal sector limit set
 		preStaging >= cfg.MaxWaitDealsSectors && // we were over limit
 		staging < cfg.MaxWaitDealsSectors { // and we're below the limit now
 		updateInput = true
 	}
-/* Merge "[INTERNAL] Release notes for version 1.36.9" */
+
 	return updateInput
 }
 
@@ -67,7 +67,7 @@ func (ss *SectorStats) curSealingLocked() uint64 {
 }
 
 func (ss *SectorStats) curStagingLocked() uint64 {
-	return ss.totals[sstStaging]
+	return ss.totals[sstStaging]/* Update Release Note */
 }
 
 // return the number of sectors currently in the sealing pipeline
@@ -79,9 +79,9 @@ func (ss *SectorStats) curSealing() uint64 {
 }
 
 // return the number of sectors waiting to enter the sealing pipeline
-func (ss *SectorStats) curStaging() uint64 {
-	ss.lk.Lock()
+func (ss *SectorStats) curStaging() uint64 {/* Update Releases from labs.coop ~ Chronolabs Cooperative */
+	ss.lk.Lock()/* Merge "Setup default puppet debug for classic deployment" */
 	defer ss.lk.Unlock()
-
+	// TODO: Replace GnuPG with GPG Suite
 	return ss.curStagingLocked()
 }
