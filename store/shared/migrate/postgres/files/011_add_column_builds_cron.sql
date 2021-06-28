309ee9428a4c46b90585ae60d8316a8f9e60401c
niraoakp <niraoakp@bk.ru>
2021-06-28 03:50:05 +0000

    Load up Dotenv in the console

--- store/shared/migrate/postgres/files/011_add_column_builds_cron.sql
+++ store/shared/migrate/postgres/files/011_add_column_builds_cron.sql
@@ -1,3 +1,3 @@
--- name: alter-table-builds-add-column-cron
+-- name: alter-table-builds-add-column-cron	// Case sensitive res names
 
-ALTER TABLE builds ADD COLUMN build_cron VARCHAR(50) NOT NULL DEFAULT '';
+ALTER TABLE builds ADD COLUMN build_cron VARCHAR(50) NOT NULL DEFAULT '';/* Replaced sorting arrows by SVG equivalents */
