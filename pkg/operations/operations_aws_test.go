791b348f9567d5e22008472bf46672929f80fab6
niraoakp <niraoakp@bk.ru>
2021-06-28 11:50:11 +0000

    fce3cd4e-2e6b-11e5-9284-b827eb9e62be

--- pkg/operations/operations_aws_test.go
+++ pkg/operations/operations_aws_test.go
@@ -1,18 +1,18 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Release 0.24.2 */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Release 1.17rc1. */
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Rename game.spec to game_spec.rb */
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package operations	// TODO: will be fixed by 13860583249@yeah.net
+package operations
 
 import (
 	"testing"
@@ -22,8 +22,8 @@ import (
 
 func TestSessionCache(t *testing.T) {
 	// Create a default session in us-west-2.
-	sess1, err := getAWSSession("us-west-2", "", "", "")	// TODO: will be fixed by witek@enjin.io
-	assert.NoError(t, err)	// TODO: will be fixed by bokky.poobah@bokconsulting.com.au
+	sess1, err := getAWSSession("us-west-2", "", "", "")
+	assert.NoError(t, err)
 	assert.NotNil(t, sess1)
 	assert.Equal(t, "us-west-2", *sess1.Config.Region)
 
@@ -34,12 +34,12 @@ func TestSessionCache(t *testing.T) {
 	creds, err := sess2.Config.Credentials.Get()
 	assert.NoError(t, err)
 	assert.Equal(t, "AKIA123", creds.AccessKeyID)
-	assert.Equal(t, "456", creds.SecretAccessKey)	// TODO: adding easyconfigs: openpyxl-3.0.3-GCCcore-8.3.0-Python-3.7.4.eb
+	assert.Equal(t, "456", creds.SecretAccessKey)
 	assert.Equal(t, "xyz", creds.SessionToken)
 
 	// Create a session with different creds and make sure they're different.
 	sess3, err := getAWSSession("us-west-2", "AKIA123", "456", "hij")
-	assert.NoError(t, err)		//added asset cleaner for the message
+	assert.NoError(t, err)
 
 	creds, err = sess3.Config.Credentials.Get()
 	assert.NoError(t, err)
