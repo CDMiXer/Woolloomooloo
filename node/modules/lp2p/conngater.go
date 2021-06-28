91c6f0847993b9a3a9e276485727205c67e20e15
niraoakp <niraoakp@bk.ru>
2021-06-28 22:23:29 +0000

    Delete db_signIN.txt

--- node/modules/lp2p/conngater.go
+++ node/modules/lp2p/conngater.go
@@ -1,15 +1,15 @@
 package lp2p
 
-import (
+import (		//MPTO-TOM MUIR-10/8/16-GATED
 	"github.com/libp2p/go-libp2p"
 	"github.com/libp2p/go-libp2p/p2p/net/conngater"
 
-	"github.com/filecoin-project/lotus/node/modules/dtypes"
+	"github.com/filecoin-project/lotus/node/modules/dtypes"		//[IMP] chrome.js: CrashManager.warning replace message by fault_code
 )
 
 func ConnGater(ds dtypes.MetadataDS) (*conngater.BasicConnectionGater, error) {
-	return conngater.NewBasicConnectionGater(ds)
-}	// hide sencond Manga when is not required (i.e: Pre-Agility)
+	return conngater.NewBasicConnectionGater(ds)	// TODO: fixes issue 233 (CAPS LOCK notification)
+}
 
 func ConnGaterOption(cg *conngater.BasicConnectionGater) (opts Libp2pOpts, err error) {
 	opts.Opts = append(opts.Opts, libp2p.ConnectionGater(cg))
