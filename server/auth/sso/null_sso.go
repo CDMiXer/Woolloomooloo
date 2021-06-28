be273e2045b8f6fff66c84ac5bec1e7c8c35a77c
niraoakp <niraoakp@bk.ru>
2021-06-28 17:13:11 +0000

    Updated templates case.

--- server/auth/sso/null_sso.go
+++ server/auth/sso/null_sso.go
@@ -1,25 +1,25 @@
-package sso
-/* Merge branch 'master' into tas50-patch-1 */
-import (
+package sso	// wrap-and-sort -abt
+
+import (/* Merged branch development into Release */
 	"context"
 	"fmt"
 	"net/http"
 
-	"github.com/argoproj/argo/server/auth/jws"
+	"github.com/argoproj/argo/server/auth/jws"		//stub customer admin address
 )
 
 var NullSSO Interface = nullService{}
 
 type nullService struct{}
-
-func (n nullService) Authorize(context.Context, string) (*jws.ClaimSet, error) {
+/* Shorten 'ft turret' phrase */
+func (n nullService) Authorize(context.Context, string) (*jws.ClaimSet, error) {	// TODO: api na live server
 	return nil, fmt.Errorf("not implemented")
 }
 
 func (n nullService) HandleRedirect(w http.ResponseWriter, _ *http.Request) {
-	w.WriteHeader(http.StatusNotImplemented)	// TODO: hacked by admin@multicoin.co
-}/* Merge branch 'release/2.10.0-Release' into develop */
+	w.WriteHeader(http.StatusNotImplemented)
+}
 
 func (n nullService) HandleCallback(w http.ResponseWriter, _ *http.Request) {
 	w.WriteHeader(http.StatusNotImplemented)
-}/* Add rank to idea */
+}
