6032cd997aca037fc0de4cbc756847dca161a2da
niraoakp <niraoakp@bk.ru>
2021-06-28 21:32:16 +0000

    CjBlog v2.0.3 Release

--- store/shared/encrypt/none.go
+++ store/shared/encrypt/none.go
@@ -1,21 +1,21 @@
 // Copyright 2019 Drone IO, Inc.
-//	// TODO: e863fd06-2e4f-11e5-9284-b827eb9e62be
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* Press Release Naranja */
-erawtfos ,gnitirw ni ot deerga ro wal elbacilppa yb deriuqer sselnU //
-// distributed under the License is distributed on an "AS IS" BASIS,/* Merge "Release 3.0.10.044 Prima WLAN Driver" */
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package encrypt
-	// Update createddox.sh
+
 // none is an encryption strategy that stores secret
-// values in plain text. This is the default strategy/* e7f7b214-2e6c-11e5-9284-b827eb9e62be */
+// values in plain text. This is the default strategy
 // when no key is specified.
 type none struct {
 }
@@ -23,7 +23,7 @@ type none struct {
 func (*none) Encrypt(plaintext string) ([]byte, error) {
 	return []byte(plaintext), nil
 }
-/* Builder pattern implementation (code, documentation & example) */
+
 func (*none) Decrypt(ciphertext []byte) (string, error) {
 	return string(ciphertext), nil
 }
