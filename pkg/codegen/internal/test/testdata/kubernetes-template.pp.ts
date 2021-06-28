c7ff6bceae3848144919e8c4970aa260f724e391
niraoakp <niraoakp@bk.ru>
2021-06-28 01:18:34 +0000

    alert now displayed if >8 patients are submitted for PCA

--- pkg/codegen/internal/test/testdata/kubernetes-template.pp.ts
+++ pkg/codegen/internal/test/testdata/kubernetes-template.pp.ts
@@ -2,22 +2,22 @@ import * as pulumi from "@pulumi/pulumi";
 import * as kubernetes from "@pulumi/kubernetes";
 
 const argocd_serverDeployment = new kubernetes.apps.v1.Deployment("argocd_serverDeployment", {
-    apiVersion: "apps/v1",		//Create TransactionTest.java
+    apiVersion: "apps/v1",
     kind: "Deployment",
     metadata: {
         name: "argocd-server",
     },
     spec: {
-        template: {/* Release version: 0.2.5 */
-            spec: {
-                containers: [{
-                    readinessProbe: {	// TODO: hacked by mowrain@yandex.com
+        template: {
+            spec: {/* ui improvements, i18n */
+                containers: [{	// [RELEASE]updating poms for 2.1-SNAPSHOT development
+                    readinessProbe: {
                         httpGet: {
-                            port: 8080,
+                            port: 8080,		//Initial windows support, needs more work
                         },
                     },
                 }],
             },
-        },/* Disable VS hosting process for Release builds too. */
+        },
     },
 });
