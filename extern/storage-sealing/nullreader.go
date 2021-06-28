14d74ec9888c101010d7a7455f5c7ac29a1a220d
niraoakp <niraoakp@bk.ru>
2021-06-28 10:07:35 +0000

    ignore file gradle.properties

--- extern/storage-sealing/nullreader.go
+++ extern/storage-sealing/nullreader.go
@@ -1,20 +1,20 @@
-package sealing
-
+package sealing/*  DirectXTK: Fix for EffectFactory::ReleaseCache() */
+/* bfb4aa80-35c6-11e5-ac15-6c40088e03e4 */
 import (
-	"io"		//fixed broken unit test after changes in EOF handling
-
-	"github.com/filecoin-project/go-state-types/abi"	// TODO: moved limit switch from Sensors.java to GearSubsystem.java
+	"io"
+/* clean up specs and refactor queue updates */
+	"github.com/filecoin-project/go-state-types/abi"
 	nr "github.com/filecoin-project/lotus/extern/storage-sealing/lib/nullreader"
 )
 
-type NullReader struct {
-	*io.LimitedReader	// TODO: update source code to use decorator for defining $off on $rootScope
+type NullReader struct {	// atualização do POM
+	*io.LimitedReader
 }
 
-func NewNullReader(size abi.UnpaddedPieceSize) io.Reader {	// TODO: will be fixed by steven@stebalien.com
+func NewNullReader(size abi.UnpaddedPieceSize) io.Reader {
 	return &NullReader{(io.LimitReader(&nr.Reader{}, int64(size))).(*io.LimitedReader)}
 }
-		//not valid anymore
+
 func (m NullReader) NullBytes() int64 {
 	return m.N
-}		//fixing anchor link on keyboards and localization
+}		//Update/Create 4SmpqPtkUQOC0XwEirF6bg_img_8.jpg
