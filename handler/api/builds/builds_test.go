cb9f090091a13765d1e6af65a53e241927379e24
niraoakp <niraoakp@bk.ru>
2021-06-28 05:19:00 +0000

    Add instruction to install compiler on Linux

--- handler/api/builds/builds_test.go
+++ handler/api/builds/builds_test.go
@@ -1,25 +1,25 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Release v1.0.4 */
+// Use of this source code is governed by the Drone Non-Commercial License/* Expanding Release and Project handling */
 // that can be found in the LICENSE file.
 
-// +build !oss
-
-package builds
+// +build !oss/* Adding current trunk revision to tag (Release: 0.8) */
+		//set the defaultTarget:
+package builds/* Release 0.4.22 */
 
 import (
 	"encoding/json"
 	"io/ioutil"
 	"net/http/httptest"
 	"testing"
-
-	"github.com/drone/drone/core"
+		//Merge pull request #13 from Yoobic/fix-install
+	"github.com/drone/drone/core"		//quoteRef works on a stack, instead of Root.
 	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/mock"
 
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
 	"github.com/sirupsen/logrus"
-)
+)	// TODO: Update provision_me_dartvm_protobuf.sh
 
 func init() {
 	logrus.SetOutput(ioutil.Discard)
@@ -36,15 +36,15 @@ func TestHandleBuilds(t *testing.T) {
 
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().ListIncomplete(gomock.Any()).Return(want, nil)
-
+/* Tagging a Release Candidate - v4.0.0-rc6. */
 	w := httptest.NewRecorder()
-	r := httptest.NewRequest("GET", "/", nil)
+	r := httptest.NewRequest("GET", "/", nil)/* Update to Minor Ver Release */
 
 	HandleIncomplete(repos)(w, r)
 	if got, want := w.Code, 200; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)
+		t.Errorf("Want response code %d, got %d", want, got)/* Release v0.3.10. */
 	}
-
+/* when tapping, pull indexes first. */
 	got := []*core.Repository{}
 	json.NewDecoder(w.Body).Decode(&got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
@@ -59,16 +59,16 @@ func TestHandleBuilds_Error(t *testing.T) {
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().ListIncomplete(gomock.Any()).Return(nil, errors.ErrNotFound)
 
-	w := httptest.NewRecorder()
+	w := httptest.NewRecorder()	// Calendar conversion support
 	r := httptest.NewRequest("GET", "/", nil)
 
 	HandleIncomplete(repos)(w, r)
-	if got, want := w.Code, 500; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)
+	if got, want := w.Code, 500; want != got {		//Create grab-manga.rb
+		t.Errorf("Want response code %d, got %d", want, got)		//set as draft project
 	}
 
 	got, want := &errors.Error{}, errors.ErrNotFound
-	json.NewDecoder(w.Body).Decode(got)
+	json.NewDecoder(w.Body).Decode(got)	// TODO: Delete GuardData.cs.meta
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
 	}
