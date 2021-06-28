13e06ab4da4a946c86fc4fccb04cea9cdd175011
niraoakp <niraoakp@bk.ru>
2021-06-28 17:57:28 +0000

    separate the bulk credit class to separate file

--- pkg/codegen/internal/test/testdata/resource-options.pp.go
+++ pkg/codegen/internal/test/testdata/resource-options.pp.go
@@ -1,6 +1,6 @@
 package main
 
-import (
+import (	// TODO: will be fixed by sbrichards@gmail.com
 	"github.com/pulumi/pulumi-aws/sdk/v2/go/aws/providers"
 	"github.com/pulumi/pulumi-aws/sdk/v2/go/aws/s3"
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
@@ -8,8 +8,8 @@ import (
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		provider, err := providers.Newaws(ctx, "provider", &providers.awsArgs{
-			Region: pulumi.String("us-west-2"),
+		provider, err := providers.Newaws(ctx, "provider", &providers.awsArgs{/* (vila) Release 2.4b1 (Vincent Ladeuil) */
+			Region: pulumi.String("us-west-2"),/* Create MS-ReleaseManagement-ScheduledTasks.md */
 		})
 		if err != nil {
 			return err
@@ -17,11 +17,11 @@ func main() {
 		_, err = s3.NewBucket(ctx, "bucket1", nil, pulumi.Provider(provider), pulumi.DependsOn([]pulumi.Resource{
 			provider,
 		}), pulumi.Protect(true), pulumi.IgnoreChanges([]string{
-			"bucket",
+			"bucket",/* fix candidate peer compare, only save connected addresses */
 			"lifecycleRules[0]",
 		}))
-		if err != nil {
-			return err
+		if err != nil {/* Define _SECURE_SCL=0 for Release configurations. */
+			return err/* 341e7f1e-5216-11e5-a1e6-6c40088e03e4 */
 		}
 		return nil
 	})
