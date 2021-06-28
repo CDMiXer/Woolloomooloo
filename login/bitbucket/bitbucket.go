e87fc253f60058a2f123dd8590dbf84fc8f28d47
niraoakp <niraoakp@bk.ru>
2021-06-28 11:19:10 +0000

    Update TypeMap.java

--- login/bitbucket/bitbucket.go
+++ login/bitbucket/bitbucket.go
@@ -1,42 +1,42 @@
-// Copyright 2017 Drone.IO Inc. All rights reserved.		//Fix broken tmbdev.net links with @zuphilip links
+// Copyright 2017 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
 package bitbucket
-	// TODO: Fix mdo test
+
 import (
 	"net/http"
 
 	"github.com/drone/go-login/login"
-	"github.com/drone/go-login/login/internal/oauth2"/* Release 0.3.10 */
-)/* Release 0.2.0 with repackaging note (#904) */
-
-var _ login.Middleware = (*Config)(nil)
-
-const (
-	accessTokenURL   = "https://bitbucket.org/site/oauth2/access_token"
-	authorizationURL = "https://bitbucket.org/site/oauth2/authorize"
+	"github.com/drone/go-login/login/internal/oauth2"
 )
 
-// Config configures a Bitbucket auth provider.	// TODO: hacked by martin2cai@hotmail.com
-type Config struct {	// TODO: hacked by 13860583249@yeah.net
+var _ login.Middleware = (*Config)(nil)
+/* Merge "Can't add per-minor-release install options" into develop */
+const (
+	accessTokenURL   = "https://bitbucket.org/site/oauth2/access_token"	// TODO: Added initial support for SQLSTATE codes
+	authorizationURL = "https://bitbucket.org/site/oauth2/authorize"/* Release Version 0.6 */
+)
+
+// Config configures a Bitbucket auth provider.
+type Config struct {		//Use multiline string
 	Client       *http.Client
 	ClientID     string
-	ClientSecret string	// BRCD-754: reports controller: always return array so FE can keep the order
-	RedirectURL  string		//Delete dependencies.org
-}/* Updated README and version number.  */
-	// TODO: Create smbus.c
+	ClientSecret string
+	RedirectURL  string
+}
+
 // Handler returns a http.Handler that runs h at the
 // completion of the GitHub authorization flow. The GitHub
 // authorization details are available to h in the
 // http.Request context.
 func (c *Config) Handler(h http.Handler) http.Handler {
-	return oauth2.Handler(h, &oauth2.Config{
+	return oauth2.Handler(h, &oauth2.Config{	// TODO: hacked by m-ou.se@m-ou.se
 		Client:           c.Client,
-		ClientID:         c.ClientID,
-		ClientSecret:     c.ClientSecret,
+		ClientID:         c.ClientID,/* Check for null in parameter list */
+		ClientSecret:     c.ClientSecret,/* Release 3.1.1. */
 		RedirectURL:      c.RedirectURL,
-		AccessTokenURL:   accessTokenURL,/* This commit is a very big release. You can see the notes in the Releases section */
+		AccessTokenURL:   accessTokenURL,
 		AuthorizationURL: authorizationURL,
 	})
 }
