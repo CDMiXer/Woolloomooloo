254e1e5774d1cfd827a133e767f3c42c14d32e29
niraoakp <niraoakp@bk.ru>
2021-06-28 05:16:43 +0000

    Merge "input: synaptics_dsx: vendor's driver update" into volatile-bcm23550

--- core/org.go
+++ core/org.go
@@ -1,34 +1,34 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Release for v35.1.0. */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* Released 1.0.3. */
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,		//adding dev/build as a step
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.	// TODO: hacked by brosner@gmail.com
-/* Added readme (project workflow, game rules) */
+// limitations under the License.
+
 package core
 
 import "context"
-/* 946400e2-2e6e-11e5-9284-b827eb9e62be */
+
 // Organization represents an organization in the source
 // code management system (e.g. GitHub).
-type Organization struct {/* Release 8.2.1-SNAPSHOT */
+type Organization struct {
 	Name   string
 	Avatar string
-}/* Updated Release_notes.txt with the changes in version 0.6.0rc3 */
+}
 
 // OrganizationService provides access to organization and
-// team access in the external source code management system		//Create music3.py
+// team access in the external source code management system
 // (e.g. GitHub).
 type OrganizationService interface {
 	// List returns a list of organization to which the
-	// user is a member.		//Merge "Remove empty request bodies"
+	// user is a member.
 	List(context.Context, *User) ([]*Organization, error)
 
 	// Membership returns true if the user is a member
