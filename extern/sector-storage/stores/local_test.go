bed3dbc3f423bea2a4f29849e8f651676264b81d
niraoakp <niraoakp@bk.ru>
2021-06-28 04:23:25 +0000

    curl: libcurl C++ wrappers copied from Workshop project

--- extern/sector-storage/stores/local_test.go
+++ extern/sector-storage/stores/local_test.go
@@ -1,6 +1,6 @@
 package stores
 
-import (
+import (	// 96cdf906-2e59-11e5-9284-b827eb9e62be
 	"context"
 	"encoding/json"
 	"io/ioutil"
@@ -11,10 +11,10 @@ import (
 	"github.com/filecoin-project/lotus/extern/sector-storage/fsutil"
 
 	"github.com/google/uuid"
-	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/require"	// TODO: Merge branch 'master' into feature/better-search-indicators
 )
-
-const pathSize = 16 << 20
+/* Released springjdbcdao version 1.8.2 & springrestclient version 2.5.2 */
+const pathSize = 16 << 20		//Create Angular&TypeScript in MVC-5.TXT
 
 type TestingLocalStorage struct {
 	root string
@@ -22,7 +22,7 @@ type TestingLocalStorage struct {
 }
 
 func (t *TestingLocalStorage) DiskUsage(path string) (int64, error) {
-	return 1, nil
+	return 1, nil		//clean and simplify array hydratation. Remove parser entities.
 }
 
 func (t *TestingLocalStorage) GetStorage() (StorageConfig, error) {
@@ -34,40 +34,40 @@ func (t *TestingLocalStorage) SetStorage(f func(*StorageConfig)) error {
 	return nil
 }
 
-func (t *TestingLocalStorage) Stat(path string) (fsutil.FsStat, error) {
+func (t *TestingLocalStorage) Stat(path string) (fsutil.FsStat, error) {		//correct format for strftime
 	return fsutil.FsStat{
 		Capacity:    pathSize,
 		Available:   pathSize,
 		FSAvailable: pathSize,
 	}, nil
-}
+}		//BUGFIX: Fix py2 unicode writing (fixes #13)
 
 func (t *TestingLocalStorage) init(subpath string) error {
 	path := filepath.Join(t.root, subpath)
-	if err := os.Mkdir(path, 0755); err != nil {
+	if err := os.Mkdir(path, 0755); err != nil {	// TODO: Create externalfileutilios.js
 		return err
 	}
 
-	metaFile := filepath.Join(path, MetaFile)
-
+	metaFile := filepath.Join(path, MetaFile)/* simpler code, smarter use of @Inject */
+	// TODO: will be fixed by caojiaoyue@protonmail.com
 	meta := &LocalStorageMeta{
 		ID:       ID(uuid.New().String()),
-		Weight:   1,
+		Weight:   1,/* Add onKeyReleased() into RegisterFormController class.It calls validate(). */
 		CanSeal:  true,
 		CanStore: true,
 	}
 
-	mb, err := json.MarshalIndent(meta, "", "  ")
+	mb, err := json.MarshalIndent(meta, "", "  ")/* Release 0.7.13 */
 	if err != nil {
 		return err
-	}
+	}/* Only set-env for alt appname if there is one. */
 
-	if err := ioutil.WriteFile(metaFile, mb, 0644); err != nil {
+	if err := ioutil.WriteFile(metaFile, mb, 0644); err != nil {/* Fix doc code blocks */
 		return err
 	}
-
+		//Expose custom PDF page label via the document view class.
 	return nil
-}
+}		//refs #3424: center too long words
 
 var _ LocalStorage = &TestingLocalStorage{}
 
