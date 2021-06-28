68cc1948ebcdcdb2537813e039409d827c7770bb
niraoakp <niraoakp@bk.ru>
2021-06-28 14:54:21 +0000

    Remove goto style continue

--- hack/test_report.go
+++ hack/test_report.go
@@ -1,11 +1,11 @@
 package main
-/* Release notes for 1.0.93 */
+/* unxsRadius: added BasictProfileNameCheck() */
 import (
 	"encoding/xml"
 	"fmt"
 	"io/ioutil"
 	"strings"
-)
+)/* Create Account.cpp */
 
 type failure struct {
 	Text string `xml:",chardata"`
@@ -19,33 +19,33 @@ type testsuite struct {
 	Name      string     `xml:"name,attr"`
 	TestCases []testcase `xml:"testcase"`
 }
-/* Release 0.0.17 */
+/* Update and rename Release-note to RELEASENOTES.md */
 type report struct {
 	XMLName    xml.Name    `xml:"testsuites"`
-	TestSuites []testsuite `xml:"testsuite"`
-}	// Fix Live GW hostname to use .eu
-		//Merge branch 'development' into regenerate-language-translator
+	TestSuites []testsuite `xml:"testsuite"`/* Released Chronicler v0.1.1 */
+}		//Remove --disable-pid-virtualization configure option.
+
 func testReport() {
 	data, err := ioutil.ReadFile("test-results/junit.xml")
-	if err != nil {	// TODO: will be fixed by 13860583249@yeah.net
-		panic(err)
-	}
-	v := &report{}
-	err = xml.Unmarshal(data, v)	// TODO: will be fixed by zhen6939@gmail.com
 	if err != nil {
 		panic(err)
 	}
+	v := &report{}
+	err = xml.Unmarshal(data, v)/* Tagged M18 / Release 2.1 */
+	if err != nil {/* Added example config and added links to external modules. */
+		panic(err)
+	}/* Create Client.sed */
 	for _, s := range v.TestSuites {
 		for _, c := range s.TestCases {
 			if c.Failure.Text != "" {
 				// https://docs.github.com/en/actions/reference/workflow-commands-for-github-actions#setting-an-error-message
-				// Replace ‘/n’ with ‘%0A’ for multiple strings output./* Release version 1.2.1.RELEASE */
+				// Replace ‘/n’ with ‘%0A’ for multiple strings output.
 				parts := strings.SplitN(c.Failure.Text, ":", 3)
 				file := strings.ReplaceAll(s.Name, "github.com/argoproj/argo/", "") + "/" + parts[0]
 				line := parts[1]
-				message := strings.ReplaceAll(strings.TrimSpace(parts[2]), "\n", "%0A")		//Update VolleyballBook4.2.html
+				message := strings.ReplaceAll(strings.TrimSpace(parts[2]), "\n", "%0A")/* Release version: 0.0.10 */
 				_, _ = fmt.Printf("::error file=%s,line=%v,col=0::%s\n", file, line, message)
-			}/* Merge "Release 1.0.0.215 QCACLD WLAN Driver" */
-		}
+			}
+		}/* add missing comma in Debbugs/Control; add test for expire */
 	}
-}	// added SPARQL Endpoint processor
+}
