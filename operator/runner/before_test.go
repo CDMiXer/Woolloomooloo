f5b23ba87b171f345bd05c4fda642639452d9923
niraoakp <niraoakp@bk.ru>
2021-06-28 09:23:45 +0000

    Fix link appearance

--- operator/runner/before_test.go
+++ operator/runner/before_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.		//No issue. Override the Java 7 from the parent POM and set to Java 6.
-		//added the code change
-package runner	// Added Minimal notifier
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: will be fixed by hugomrdias@gmail.com
+// Use of this source code is governed by the Drone Non-Commercial License	// TODO: Properly working Rails plugin.
+// that can be found in the LICENSE file.		//Merge "gtest needs -lpthread."
+
+package runner
