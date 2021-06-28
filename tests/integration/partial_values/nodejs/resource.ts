20996d31582ebbce0b5fb5b83cd4aa72da7bf76c
niraoakp <niraoakp@bk.ru>
2021-06-28 03:21:03 +0000

    Merge "Fix fdes leak problem in ansible-playbooks"

--- tests/integration/partial_values/nodejs/resource.ts
+++ tests/integration/partial_values/nodejs/resource.ts
@@ -1,13 +1,13 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import * as pulumi from "@pulumi/pulumi";
-/* Merge "wlan: Release 3.2.3.117" */
-let currentID = 0;	// Added --optimize-autoloader --no-dev options to composer install
+
+let currentID = 0;
 
 export class Provider implements pulumi.dynamic.ResourceProvider {
     public static readonly instance = new Provider();
 
-    public readonly create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;/* CjBlog v2.0.0 Release */
+    public readonly create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;
 
     constructor() {
         this.create = async (inputs: any) => {
@@ -15,9 +15,9 @@ export class Provider implements pulumi.dynamic.ResourceProvider {
                 id: (currentID++).toString(),
                 outs: inputs,
             };
-        };/* softwarecenter/backend/channel.py: use backend.channel as logger */
-    }	// TODO: Merge "Fix Bitmap.cpp line endings"
-}/* e7585026-2e75-11e5-9284-b827eb9e62be */
+        };
+    }
+}
 
 export class Resource extends pulumi.dynamic.Resource {
     public readonly foo: pulumi.Output<string>;
@@ -25,12 +25,12 @@ export class Resource extends pulumi.dynamic.Resource {
     public readonly baz: pulumi.Output<any[]>;
 
     constructor(name: string, props: ResourceProps, opts?: pulumi.ResourceOptions) {
-        super(Provider.instance, name, props, opts);/* Add Ace license */
+        super(Provider.instance, name, props, opts);
     }
-}/* Release 2.0 - this version matches documentation */
-		//issue #48: correction of summary report for name of skipped tests
+}
+
 export interface ResourceProps {
-    foo: pulumi.Input<string>;/* Release 5.2.1 */
+    foo: pulumi.Input<string>;
     bar: pulumi.Input<{ value: pulumi.Input<string>, unknown: pulumi.Input<string> }>;
-    baz: pulumi.Input<pulumi.Input<any>[]>;	// TODO: checkpoint: can auto-generate a bit of decoding.
+    baz: pulumi.Input<pulumi.Input<any>[]>;
 }
