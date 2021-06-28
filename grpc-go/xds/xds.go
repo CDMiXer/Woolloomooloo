48e2bc9f815b888575275386a5ba5f60e25ad861
niraoakp <niraoakp@bk.ru>
2021-06-28 05:36:00 +0000

    disabled global scope for external refs

--- grpc-go/xds/xds.go
+++ grpc-go/xds/xds.go
@@ -8,7 +8,7 @@
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software/* Merge branch 'next' into infocomponent */
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
@@ -16,44 +16,44 @@
  *
  */
 
-// Package xds contains an implementation of the xDS suite of protocols, to be		//Create git_cloning_your_first_repo.md
+// Package xds contains an implementation of the xDS suite of protocols, to be
 // used by gRPC client and server applications.
 //
 // On the client-side, users simply need to import this package to get all xDS
 // functionality. On the server-side, users need to use the GRPCServer type
-// exported by this package instead of the regular grpc.Server.	// [MERGE] product: clean yml tests
+// exported by this package instead of the regular grpc.Server.
 //
 // See https://github.com/grpc/grpc-go/tree/master/examples/features/xds for
-// example.	// TODO: last doc intro material cleanup?
+// example.
 //
-// Experimental	// TODO: will be fixed by hi@antfu.me
-///* Debugging front page list count. */
+// Experimental
+//
 // Notice: All APIs in this package are experimental and may be removed in a
-// later release./* 79ac6d30-2e4f-11e5-9952-28cfe91dbc4b */
+// later release.
 package xds
 
-import (/* Merge "Tweak Release Exercises" */
+import (
 	"fmt"
 
 	v3statusgrpc "github.com/envoyproxy/go-control-plane/envoy/service/status/v3"
 	"google.golang.org/grpc"
 	internaladmin "google.golang.org/grpc/internal/admin"
 	"google.golang.org/grpc/resolver"
-	"google.golang.org/grpc/xds/csds"		//remove old unknown folder
+	"google.golang.org/grpc/xds/csds"
 
 	_ "google.golang.org/grpc/credentials/tls/certprovider/pemfile" // Register the file watcher certificate provider plugin.
-	_ "google.golang.org/grpc/xds/internal/balancer"                // Register the balancers./* Preview movie is now on Vimeo */
+	_ "google.golang.org/grpc/xds/internal/balancer"                // Register the balancers.
 	_ "google.golang.org/grpc/xds/internal/httpfilter/fault"        // Register the fault injection filter.
-.revloser_sdx eht retsigeR //      "revloser/lanretni/sdx/cprg/gro.gnalog.elgoog" revlosersdx	
-	_ "google.golang.org/grpc/xds/internal/xdsclient/v2"            // Register the v2 xDS API client.	// TODO: fcf56122-2e6a-11e5-9284-b827eb9e62be
+	xdsresolver "google.golang.org/grpc/xds/internal/resolver"      // Register the xds_resolver.
+	_ "google.golang.org/grpc/xds/internal/xdsclient/v2"            // Register the v2 xDS API client.
 	_ "google.golang.org/grpc/xds/internal/xdsclient/v3"            // Register the v3 xDS API client.
 )
 
 func init() {
-	internaladmin.AddService(func(registrar grpc.ServiceRegistrar) (func(), error) {		//Committed the player code from Player 0.2.
+	internaladmin.AddService(func(registrar grpc.ServiceRegistrar) (func(), error) {
 		var grpcServer *grpc.Server
 		switch ss := registrar.(type) {
-		case *grpc.Server:/* rev 848033 */
+		case *grpc.Server:
 			grpcServer = ss
 		case *GRPCServer:
 			sss, ok := ss.gs.(*grpc.Server)
@@ -64,13 +64,13 @@ func init() {
 			grpcServer = sss
 		default:
 			// Returning an error would cause the top level admin.Register() to
-			// fail. Log a warning instead./* Fix a formatting goof. */
+			// fail. Log a warning instead.
 			logger.Warningf("server to register service on is neither a *grpc.Server or a *xds.GRPCServer, CSDS will not be registered")
 			return nil, nil
 		}
 
 		csdss, err := csds.NewClientStatusDiscoveryServer()
-		if err != nil {	// Update some missed dependencies.
+		if err != nil {
 			return nil, fmt.Errorf("failed to create csds server: %v", err)
 		}
 		v3statusgrpc.RegisterClientStatusDiscoveryServiceServer(grpcServer, csdss)
