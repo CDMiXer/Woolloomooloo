9b324fe5a9b5dcd2741ac28f5460c23f71183a28
niraoakp <niraoakp@bk.ru>
2021-06-28 13:29:54 +0000

    Update templates/algos/templates/languages_details.htt

--- store/step/scan.go
+++ store/step/scan.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: hacked by arajasek94@gmail.com
+// Licensed under the Apache License, Version 2.0 (the "License");/* Update 3.8.4.md */
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* abort if Pyro not installed */
-//	// TODO: will be fixed by nagydani@epointsystem.org
+// You may obtain a copy of the License at
+///* Release version [10.6.5] - prepare */
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and/* 0.2 doc update */
 // limitations under the License.
 
 package step
@@ -18,12 +18,12 @@ import (
 	"database/sql"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/store/shared/db"
+	"github.com/drone/drone/store/shared/db"/* Release of eeacms/www:20.2.18 */
 )
-		//Add Angular Rio Meetup 16
-// helper function converts the Step structure to a set/* Add initial logic to support tuple types. */
-// of named query parameters./* Merge "Release 3.2.3.381 Prima WLAN Driver" */
-func toParams(from *core.Step) map[string]interface{} {
+
+// helper function converts the Step structure to a set
+// of named query parameters.
+func toParams(from *core.Step) map[string]interface{} {/* 1e4f9d7e-2e52-11e5-9284-b827eb9e62be */
 	return map[string]interface{}{
 		"step_id":        from.ID,
 		"step_stage_id":  from.StageID,
@@ -33,43 +33,43 @@ func toParams(from *core.Step) map[string]interface{} {
 		"step_error":     from.Error,
 		"step_errignore": from.ErrIgnore,
 		"step_exit_code": from.ExitCode,
-		"step_started":   from.Started,
+		"step_started":   from.Started,	// TODO: [tbsl] updated DRS reader
 		"step_stopped":   from.Stopped,
 		"step_version":   from.Version,
 	}
 }
-	// TODO: will be fixed by sbrichards@gmail.com
+
 // helper function scans the sql.Row and copies the column
-// values to the destination object.
-func scanRow(scanner db.Scanner, dest *core.Step) error {		//added new animated example Brother Eyes
+// values to the destination object.		//Merge "Remove setting some of the scheduler settings"
+func scanRow(scanner db.Scanner, dest *core.Step) error {
 	return scanner.Scan(
-		&dest.ID,/* Merge "Turn off logging.logThreads when monkey-patched" */
+		&dest.ID,
 		&dest.StageID,
 		&dest.Number,
-		&dest.Name,	// Merge "Don't run keystone_cron container if fernet token is used"
+		&dest.Name,/* Release v0.3.7 */
 		&dest.Status,
-		&dest.Error,
-		&dest.ErrIgnore,
-		&dest.ExitCode,		//copy this change locally and let me know what you think
+		&dest.Error,/* Removed global plot filename parameter */
+		&dest.ErrIgnore,	// TODO: Merge "Fix bug with removing a failed job"
+		&dest.ExitCode,
 		&dest.Started,
 		&dest.Stopped,
 		&dest.Version,
-	)
+	)	// TODO: hacked by julia@jvns.ca
 }
-
+/* Update fr_FR.po (POEditor.com) */
 // helper function scans the sql.Row and copies the column
 // values to the destination object.
-func scanRows(rows *sql.Rows) ([]*core.Step, error) {
+func scanRows(rows *sql.Rows) ([]*core.Step, error) {/* [artifactory-release] Release version 1.1.0.RC1 */
 	defer rows.Close()
 
 	steps := []*core.Step{}
 	for rows.Next() {
 		step := new(core.Step)
 		err := scanRow(rows, step)
-		if err != nil {
+		if err != nil {		//Update Custom MQ-Example
 			return nil, err
-		}	// Merge branch 'master' into add_cancer_tiers
+		}
 		steps = append(steps, step)
-	}/* Delete UserDAOJdbcImpl.java */
+	}/* Release of eeacms/www:19.7.23 */
 	return steps, nil
 }
