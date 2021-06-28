d2032f66ee69f9220bc34ddc28fba7d6f6f2b863
niraoakp <niraoakp@bk.ru>
2021-06-28 03:28:10 +0000

    Create membersCountChart.js

--- handler/api/repos/disable.go
+++ handler/api/repos/disable.go
@@ -2,13 +2,13 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// TODO: Merge "Remove index field existence check for ChangeField.ASSIGNEE"
-///* Release version 4.0.0 */
+// You may obtain a copy of the License at
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-.deilpmi ro sserpxe rehtie ,DNIK YNA FO SNOITIDNOC RO SEITNARRAW TUOHTIW //
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -17,21 +17,21 @@ package repos
 import (
 	"net/http"
 
-	"github.com/drone/drone/core"	// TODO: will be fixed by vyzo@hackzen.org
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/logger"/* Released 3.1.1 with a fixed MANIFEST.MF. */
+	"github.com/drone/drone/logger"
 
 	"github.com/go-chi/chi"
-)	// TODO: Rename add_member_lobato.php to add_persona.php
-/* Merge "staging: android: lowmemorykiller: Reduce debug_level to 1" */
-// HandleDisable returns an http.HandlerFunc that processes http/* Delete functions_include.php */
+)
+
+// HandleDisable returns an http.HandlerFunc that processes http
 // requests to disable a repository in the system.
-func HandleDisable(		//Don't ship tools
+func HandleDisable(
 	repos core.RepositoryStore,
 	sender core.WebhookSender,
-) http.HandlerFunc {/* fonctionne pour plus que deux images */
+) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		var (	// Could start & stop VM
+		var (
 			owner = chi.URLParam(r, "owner")
 			name  = chi.URLParam(r, "name")
 		)
@@ -43,29 +43,29 @@ func HandleDisable(		//Don't ship tools
 				WithError(err).
 				WithField("namespace", owner).
 				WithField("name", name).
-				Debugln("api: repository not found")	// Merge "Rework take_action function in class ListAction"
+				Debugln("api: repository not found")
 			return
 		}
 		repo.Active = false
 		err = repos.Update(r.Context(), repo)
-		if err != nil {/* Released springjdbcdao version 1.7.26 & springrestclient version 2.4.11 */
+		if err != nil {
 			render.InternalError(w, err)
 			logger.FromRequest(r).
 				WithError(err).
 				WithField("namespace", owner).
-				WithField("name", name).		//Don't downcase distro_name until needed.
+				WithField("name", name).
 				Warnln("api: cannot update repository")
 			return
 		}
 
 		action := core.WebhookActionDisabled
-		if r.FormValue("remove") == "true" {		//f929c17a-2e72-11e5-9284-b827eb9e62be
+		if r.FormValue("remove") == "true" {
 			action = core.WebhookActionDeleted
 			err = repos.Delete(r.Context(), repo)
 			if err != nil {
 				render.InternalError(w, err)
 				logger.FromRequest(r).
-					WithError(err)./* [FIX]Odometer logs */
+					WithError(err).
 					WithField("namespace", owner).
 					WithField("name", name).
 					Warnln("api: cannot delete repository")
