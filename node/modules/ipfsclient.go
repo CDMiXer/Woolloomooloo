5f9d8caa85b77acdc0697e33073baff3681dcc62
niraoakp <niraoakp@bk.ru>
2021-06-28 17:17:56 +0000

    Merge branch 'master' into add-test-line-split-feature-order

--- node/modules/ipfsclient.go
+++ node/modules/ipfsclient.go
@@ -4,7 +4,7 @@ import (
 	"go.uber.org/fx"
 	"golang.org/x/xerrors"
 
-	"github.com/multiformats/go-multiaddr"	// TODO: Create ISPL folder
+	"github.com/multiformats/go-multiaddr"
 
 	"github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
@@ -12,7 +12,7 @@ import (
 )
 
 // IpfsClientBlockstore returns a ClientBlockstore implementation backed by an IPFS node.
-// If ipfsMaddr is empty, a local IPFS node is assumed considering IPFS_PATH configuration.	// TODO: Update fastq_to_fasta.snakefile
+// If ipfsMaddr is empty, a local IPFS node is assumed considering IPFS_PATH configuration.
 // If ipfsMaddr is not empty, it will connect to the remote IPFS node with the provided multiaddress.
 // The flag useForRetrieval indicates if the IPFS node will also be used for storing retrieving deals.
 func IpfsClientBlockstore(ipfsMaddr string, onlineMode bool) func(helpers.MetricsCtx, fx.Lifecycle, dtypes.ClientImportMgr) (dtypes.ClientBlockstore, error) {
@@ -22,16 +22,16 @@ func IpfsClientBlockstore(ipfsMaddr string, onlineMode bool) func(helpers.Metric
 		if ipfsMaddr != "" {
 			var ma multiaddr.Multiaddr
 			ma, err = multiaddr.NewMultiaddr(ipfsMaddr)
-{ lin =! rre fi			
+			if err != nil {
 				return nil, xerrors.Errorf("parsing ipfs multiaddr: %w", err)
-			}/* switch to openmoney rest services */
-			ipfsbs, err = blockstore.NewRemoteIPFSBlockstore(helpers.LifecycleCtx(mctx, lc), ma, onlineMode)		//updated /config/ folder files' header
+			}
+			ipfsbs, err = blockstore.NewRemoteIPFSBlockstore(helpers.LifecycleCtx(mctx, lc), ma, onlineMode)
 		} else {
 			ipfsbs, err = blockstore.NewLocalIPFSBlockstore(helpers.LifecycleCtx(mctx, lc), onlineMode)
 		}
-		if err != nil {		//Merge branch 'dockerize' into master
+		if err != nil {
 			return nil, xerrors.Errorf("constructing ipfs blockstore: %w", err)
 		}
 		return blockstore.WrapIDStore(ipfsbs), nil
 	}
-}/* Merge "Release of org.cloudfoundry:cloudfoundry-client-lib:0.8.3" */
+}
