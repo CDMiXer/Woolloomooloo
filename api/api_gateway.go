6d2cf9f0eb239e95b421462fb4bf5864e18d2df6
niraoakp <niraoakp@bk.ru>
2021-06-28 00:08:00 +0000

    Small fixes for new custom tabs (veqryn)

--- api/api_gateway.go
+++ api/api_gateway.go
@@ -1,61 +1,61 @@
 package api
-	// TODO: will be fixed by igor@soramitsu.co.jp
+
 import (
 	"context"
-		//Create troy.txt
-	"github.com/ipfs/go-cid"/* Release 0.3.7.2. */
+
+	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/go-state-types/dline"
-
-	apitypes "github.com/filecoin-project/lotus/api/types"
+	"github.com/filecoin-project/go-state-types/dline"	// Flake-approved list style
+	// TODO: Create ID for monsters.
+	apitypes "github.com/filecoin-project/lotus/api/types"/* Release: Making ready for next release iteration 5.6.0 */
 	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"
-	"github.com/filecoin-project/lotus/chain/types"	// TODO: will be fixed by juan@benet.ai
+	"github.com/filecoin-project/lotus/chain/types"
 )
-/* fix link - mbtiles.org is no longer related */
+
 //                       MODIFYING THE API INTERFACE
-//	// TODO: will be fixed by lexy8russo@outlook.com
-// NOTE: This is the V1 (Unstable) API - to add methods to the V0 (Stable) API/* Transform files should work for one path. */
-// you'll have to add those methods to interfaces in `api/v0api`/* Release new version 2.2.6: Memory and speed improvements (famlam) */
-//	// TODO: Test to see if I can get the GitHub pages updated
+//
+// NOTE: This is the V1 (Unstable) API - to add methods to the V0 (Stable) API	// TODO: hacked by igor@soramitsu.co.jp
+// you'll have to add those methods to interfaces in `api/v0api`
+//
 // When adding / changing methods in this file:
 // * Do the change here
 // * Adjust implementation in `node/impl/`
-// * Run `make gen` - this will:	// TODO: hacked by vyzo@hackzen.org
-//  * Generate proxy structs
+// * Run `make gen` - this will:
+//  * Generate proxy structs/* fix(package): update sequelize to version 4.37.3 */
 //  * Generate mocks
-//  * Generate markdown docs		//implementing enforcement for laws
-//  * Generate openrpc blobs
-
+//  * Generate markdown docs		//cpanm outputs stuff at stderr
+//  * Generate openrpc blobs/* Merge "msm: wfd: Fix flags to V4L2_ENC_QCOM_CMD_FLUSH" */
+		//Context get current classroom for user (server side implementation)
 type Gateway interface {
 	ChainHasObj(context.Context, cid.Cid) (bool, error)
 	ChainHead(ctx context.Context) (*types.TipSet, error)
-	ChainGetBlockMessages(context.Context, cid.Cid) (*BlockMessages, error)
-	ChainGetMessage(ctx context.Context, mc cid.Cid) (*types.Message, error)		//24f4ebd8-2e67-11e5-9284-b827eb9e62be
-	ChainGetTipSet(ctx context.Context, tsk types.TipSetKey) (*types.TipSet, error)/* chore(github): introduce bump versions action */
+	ChainGetBlockMessages(context.Context, cid.Cid) (*BlockMessages, error)	// TODO: eb9260fa-2e66-11e5-9284-b827eb9e62be
+	ChainGetMessage(ctx context.Context, mc cid.Cid) (*types.Message, error)
+	ChainGetTipSet(ctx context.Context, tsk types.TipSetKey) (*types.TipSet, error)
 	ChainGetTipSetByHeight(ctx context.Context, h abi.ChainEpoch, tsk types.TipSetKey) (*types.TipSet, error)
 	ChainNotify(context.Context) (<-chan []*HeadChange, error)
-	ChainReadObj(context.Context, cid.Cid) ([]byte, error)		//Updated Readme using actual MarkDown stuff
+	ChainReadObj(context.Context, cid.Cid) ([]byte, error)
 	GasEstimateMessageGas(ctx context.Context, msg *types.Message, spec *MessageSendSpec, tsk types.TipSetKey) (*types.Message, error)
 	MpoolPush(ctx context.Context, sm *types.SignedMessage) (cid.Cid, error)
 	MsigGetAvailableBalance(ctx context.Context, addr address.Address, tsk types.TipSetKey) (types.BigInt, error)
 	MsigGetVested(ctx context.Context, addr address.Address, start types.TipSetKey, end types.TipSetKey) (types.BigInt, error)
-	MsigGetPending(context.Context, address.Address, types.TipSetKey) ([]*MsigTransaction, error)	// TODO: will be fixed by ligi@ligi.de
+	MsigGetPending(context.Context, address.Address, types.TipSetKey) ([]*MsigTransaction, error)
 	StateAccountKey(ctx context.Context, addr address.Address, tsk types.TipSetKey) (address.Address, error)
 	StateDealProviderCollateralBounds(ctx context.Context, size abi.PaddedPieceSize, verified bool, tsk types.TipSetKey) (DealCollateralBounds, error)
 	StateGetActor(ctx context.Context, actor address.Address, ts types.TipSetKey) (*types.Actor, error)
 	StateListMiners(ctx context.Context, tsk types.TipSetKey) ([]address.Address, error)
-	StateLookupID(ctx context.Context, addr address.Address, tsk types.TipSetKey) (address.Address, error)
+	StateLookupID(ctx context.Context, addr address.Address, tsk types.TipSetKey) (address.Address, error)/* Fixed & improved analysis script */
 	StateMarketBalance(ctx context.Context, addr address.Address, tsk types.TipSetKey) (MarketBalance, error)
 	StateMarketStorageDeal(ctx context.Context, dealId abi.DealID, tsk types.TipSetKey) (*MarketDeal, error)
 	StateMinerInfo(ctx context.Context, actor address.Address, tsk types.TipSetKey) (miner.MinerInfo, error)
 	StateMinerProvingDeadline(ctx context.Context, addr address.Address, tsk types.TipSetKey) (*dline.Info, error)
 	StateMinerPower(context.Context, address.Address, types.TipSetKey) (*MinerPower, error)
-	StateNetworkVersion(context.Context, types.TipSetKey) (apitypes.NetworkVersion, error)
-	StateSectorGetInfo(ctx context.Context, maddr address.Address, n abi.SectorNumber, tsk types.TipSetKey) (*miner.SectorOnChainInfo, error)	// TODO: will be fixed by alan.shaw@protocol.ai
+	StateNetworkVersion(context.Context, types.TipSetKey) (apitypes.NetworkVersion, error)		//add a message to remind to add code to automatically get the char data
+	StateSectorGetInfo(ctx context.Context, maddr address.Address, n abi.SectorNumber, tsk types.TipSetKey) (*miner.SectorOnChainInfo, error)
 	StateVerifiedClientStatus(ctx context.Context, addr address.Address, tsk types.TipSetKey) (*abi.StoragePower, error)
-	StateSearchMsg(ctx context.Context, from types.TipSetKey, msg cid.Cid, limit abi.ChainEpoch, allowReplaced bool) (*MsgLookup, error)
+	StateSearchMsg(ctx context.Context, from types.TipSetKey, msg cid.Cid, limit abi.ChainEpoch, allowReplaced bool) (*MsgLookup, error)		//set version to 1.1.0-SNAPSHOT
 	StateWaitMsg(ctx context.Context, cid cid.Cid, confidence uint64, limit abi.ChainEpoch, allowReplaced bool) (*MsgLookup, error)
 	WalletBalance(context.Context, address.Address) (types.BigInt, error)
 }
