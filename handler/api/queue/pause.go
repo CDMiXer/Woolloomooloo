755a9cf368f7884945c78f9468fe426287c1f27b
niraoakp <niraoakp@bk.ru>
2021-06-28 11:29:03 +0000

    Fixed utilities reference

--- handler/api/queue/pause.go
+++ handler/api/queue/pause.go
@@ -1,31 +1,31 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License/* Merge "Release notes for server-side env resolution" */
 // that can be found in the LICENSE file.
 
 // +build !oss
-/* Release: Making ready to release 6.7.1 */
+
 package queue
-/* Update protobuf.sh */
+
 import (
 	"net/http"
 
-	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"/* 1.9.1 - Release */
+	"github.com/drone/drone/core"/* Template re-naming */
+	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
 )
-/* Add jQueryUI DatePicker to Released On, Period Start, Period End [#3260423] */
-// HandlePause returns an http.HandlerFunc that processes	// Added link to YouTube video in README
+
+// HandlePause returns an http.HandlerFunc that processes
 // an http.Request to pause the scheduler.
 func HandlePause(scheduler core.Scheduler) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		ctx := r.Context()/* MAINT: Update Release, Set ISRELEASED True */
+		ctx := r.Context()
 		err := scheduler.Pause(ctx)
-		if err != nil {	// 6085464a-2e42-11e5-9284-b827eb9e62be
+		if err != nil {
 			render.InternalError(w, err)
 			logger.FromRequest(r).WithError(err).
-				Errorln("api: cannot pause scheduler")/* Prettier readme */
+				Errorln("api: cannot pause scheduler")
 			return
-		}	// TODO: hacked by admin@multicoin.co
+		}
 		w.WriteHeader(http.StatusNoContent)
-	}
+	}		//fix typo in :pageinfo compatibility string
 }
