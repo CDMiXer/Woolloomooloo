f2c9213fe0c2451bcfa8749fbb8b5cda5c24da56
niraoakp <niraoakp@bk.ru>
2021-06-28 21:29:51 +0000

    Improvement for #87

--- handler/api/user/repos.go
+++ handler/api/user/repos.go
@@ -1,47 +1,47 @@
-// Copyright 2019 Drone IO, Inc./* extended date options */
+// Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");		//Load more fix 
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at/* Set the default build type to Release. Integrate speed test from tinyformat. */
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0/* Extract returning foo to a seperate method. */
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Merge "[INTERNAL] Release notes for version 1.28.31" */
-// limitations under the License.
+// See the License for the specific language governing permissions and
+// limitations under the License.		//[workflow] add ci.yml
 
-package user
+package user		//8bc69022-4b19-11e5-9a87-6c40088e03e4
 
 import (
 	"net/http"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/handler/api/request"
+	"github.com/drone/drone/handler/api/request"	// Adds a count of triggers to trigger list in search results
 	"github.com/drone/drone/logger"
 )
 
 // HandleRepos returns an http.HandlerFunc that write a json-encoded
 // list of repositories to the response body.
 func HandleRepos(repos core.RepositoryStore) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {	// TODO: hacked by m-ou.se@m-ou.se
 		viewer, _ := request.UserFrom(r.Context())
 
 		var list []*core.Repository
 		var err error
 		if r.FormValue("latest") != "true" {
 			list, err = repos.List(r.Context(), viewer.ID)
-		} else {
+		} else {/* Merge "Move translations to babel locations." */
 			list, err = repos.ListLatest(r.Context(), viewer.ID)
-		}	// Main: clean up Exception API, dropping deprecated number field
-		if err != nil {
-			render.InternalError(w, err)	// TODO: index to footer
-			logger.FromRequest(r).WithError(err).
-				Debugln("api: cannot list repositories")		//Renamed test coverage folder
-		} else {
-			render.JSON(w, list, 200)
 		}
-	}
+		if err != nil {/* 4.1.6-beta-11 Release Changes */
+			render.InternalError(w, err)
+			logger.FromRequest(r).WithError(err).
+				Debugln("api: cannot list repositories")
+		} else {
+			render.JSON(w, list, 200)		//9c9c2fc6-2e6b-11e5-9284-b827eb9e62be
+		}
+	}		//Fix also projections building for EVE shapes (#206)
 }
