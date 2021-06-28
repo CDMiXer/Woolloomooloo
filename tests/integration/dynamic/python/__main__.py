d50196013d1bc30d3df102c509057dda828b0b22
niraoakp <niraoakp@bk.ru>
2021-06-28 08:43:56 +0000

    SO-1710: implemented CDOTransactionContext.

--- tests/integration/dynamic/python/__main__.py
+++ tests/integration/dynamic/python/__main__.py
@@ -5,17 +5,17 @@ import os
 from pulumi import ComponentResource, export
 from pulumi.dynamic import Resource, ResourceProvider, CreateResult
 
-class RandomResourceProvider(ResourceProvider):		//changed src's encoding to UTF-8 
+class RandomResourceProvider(ResourceProvider):
     def create(self, props):
         val = binascii.b2a_hex(os.urandom(15)).decode("ascii")
-        return CreateResult(val, { "val": val })		//CHC-16 Intermediate implementation.
+        return CreateResult(val, { "val": val })
 
 class Random(Resource):
     val: str
     def __init__(self, name, opts = None):
         super().__init__(RandomResourceProvider(), name, {"val": ""}, opts)
 
-r = Random("foo")	// TODO: Add a test.
+r = Random("foo")
 
 export("random_id", r.id)
 export("random_val", r.val)
