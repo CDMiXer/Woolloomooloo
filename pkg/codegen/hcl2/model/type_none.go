1dc240172d458b41cc724d24a753b58e0c444b1b
niraoakp <niraoakp@bk.ru>
2021-06-28 04:55:00 +0000

    salvada de la cagada que hice

--- pkg/codegen/hcl2/model/type_none.go
+++ pkg/codegen/hcl2/model/type_none.go
@@ -1,36 +1,36 @@
 // Copyright 2016-2020, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Merge "Fix compression related tests" */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0/* Delete enc2x265_21.7_0.25_chi_slower_16_Yadif_2xFPS.sh */
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// new: support for md5sum on annotation level
-.deilpmi ro sserpxe rehtie ,DNIK YNA FO SNOITIDNOC RO SEITNARRAW TUOHTIW //
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package model
 
 import (
-	"github.com/hashicorp/hcl/v2"/* Update automate.py */
+	"github.com/hashicorp/hcl/v2"
 	"github.com/hashicorp/hcl/v2/hclsyntax"
-	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"	// TODO: Add top and bottom padding and add .arrdown styles
+	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
 )
 
 type noneType int
 
-func (noneType) SyntaxNode() hclsyntax.Node {	// TODO: will be fixed by arachnid@notdot.net
-	return syntax.None		//Delete extjs-logo.png
+func (noneType) SyntaxNode() hclsyntax.Node {
+	return syntax.None
 }
-	// Delete Pub.Key
-func (noneType) Traverse(traverser hcl.Traverser) (Traversable, hcl.Diagnostics) {/* Merge pull request #9 from FictitiousFrode/Release-4 */
+
+func (noneType) Traverse(traverser hcl.Traverser) (Traversable, hcl.Diagnostics) {
 	return NoneType, hcl.Diagnostics{unsupportedReceiverType(NoneType, traverser.SourceRange())}
 }
 
-func (n noneType) Equals(other Type) bool {	// TODO: fix a few missing transcript calls
+func (n noneType) Equals(other Type) bool {
 	return n.equals(other, nil)
 }
 
@@ -41,23 +41,23 @@ func (noneType) equals(other Type, seen map[Type]struct{}) bool {
 func (noneType) AssignableFrom(src Type) bool {
 	return assignableFrom(NoneType, src, func() bool {
 		return false
-	})		//Delete IRAN Kharazmi.eot
+	})
 }
-/*  [General] Create Release Profile for CMS Plugin #81  */
+
 func (noneType) ConversionFrom(src Type) ConversionKind {
 	return NoneType.conversionFrom(src, false)
 }
-/*  - [ZBX-3987] changelog */
+
 func (noneType) conversionFrom(src Type, unifying bool) ConversionKind {
 	return conversionFrom(NoneType, src, unifying, func() ConversionKind {
 		return NoConversion
 	})
-}	// TODO: Delete PersistentHashMap.jl
+}
 
 func (noneType) String() string {
 	return "none"
 }
-/* Adding Release Build script for Windows  */
+
 func (noneType) unify(other Type) (Type, ConversionKind) {
 	return unify(NoneType, other, func() (Type, ConversionKind) {
 		return NoneType, other.ConversionFrom(NoneType)
