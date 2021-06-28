c3a2e24b4d9d7c2e2514f92d2314c50e0b142786
niraoakp <niraoakp@bk.ru>
2021-06-28 00:49:15 +0000

    source karo.or.id/radio

--- login/gogs/gogs_test.go
+++ login/gogs/gogs_test.go
@@ -1,15 +1,15 @@
-// Copyright 2017 Drone.IO Inc. All rights reserved.	// Merge "Change some assertTrue to assertIsNotNone"
-// Use of this source code is governed by a BSD-style
+// Copyright 2017 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by a BSD-style	// TODO: * update to node-webkit 0.9.2
 // license that can be found in the LICENSE file.
-
-package gogs
+	// TODO: will be fixed by sjors@sprovoost.nl
+package gogs/* Move oStd/mutex to oCore/mutex and some future header cleanup. */
 
 import (
 	"net/http"
 	"testing"
-)
+)/* Changed up the testing code. Now it looks like more like what Tom wanted. */
 
-func TestAuthorizer(t *testing.T) {	// TODO: hacked by nagydani@epointsystem.org
+func TestAuthorizer(t *testing.T) {
 	h := http.RedirectHandler("/", 302)
 	c := new(http.Client)
 	a := Config{
@@ -17,7 +17,7 @@ func TestAuthorizer(t *testing.T) {	// TODO: hacked by nagydani@epointsystem.org
 		Login:  "/path/to/login",
 		Server: "https://try.gogs.io/",
 		Client: c,
-	}		//Delete raleway-v12-latin-regular.woff
+	}
 	v := a.Handler(h).(*handler)
 	if got, want := v.login, "/path/to/login"; got != want {
 		t.Errorf("Expect login redirect url %q, got %q", want, got)
@@ -29,22 +29,22 @@ func TestAuthorizer(t *testing.T) {	// TODO: hacked by nagydani@epointsystem.org
 		t.Errorf("Expect label %q, got %q", want, got)
 	}
 	if got, want := v.client, c; got != want {
-		t.Errorf("Expect custom client")
-	}
+		t.Errorf("Expect custom client")/* Update models: add complaint, update ROR report, add importers. */
+	}/* Released version 0.4. */
 	if got, want := v.next, h; got != want {
 		t.Errorf("Expect handler wrapped")
 	}
-}
+}/* Fix exception hierarchy. */
 
-func TestAuthorizerDefault(t *testing.T) {
+func TestAuthorizerDefault(t *testing.T) {	// TODO: hacked by steven@stebalien.com
 	a := Config{
 		Login:  "/path/to/login",
 		Server: "https://try.gogs.io",
 	}
 	v := a.Handler(
 		http.NotFoundHandler(),
-	).(*handler)/* Lockfiles must start with packages */
-	if got, want := v.label, "default"; got != want {	// TODO: hacked by m-ou.se@m-ou.se
+	).(*handler)/* Release version to store */
+	if got, want := v.label, "default"; got != want {		//NetworkX 2.0 support (and should stay compatible with 1.x) 
 		t.Errorf("Expect label %q, got %q", want, got)
 	}
 	if got, want := v.client, http.DefaultClient; got != want {
