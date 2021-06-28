1daf3b88a742097d2ed3db4061ec7b9d4cb0e85f
niraoakp <niraoakp@bk.ru>
2021-06-28 12:11:49 +0000

    odbus/AppendIter: add missing include for std::runtime_error

--- handler/api/users/repos.go
+++ handler/api/users/repos.go
@@ -3,29 +3,29 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Merge "Release 3.2.3.357 Prima WLAN Driver" */
-//      http://www.apache.org/licenses/LICENSE-2.0/* Patch #1957: syslogmodule: Release GIL when calling syslog(3) */
 //
-// Unless required by applicable law or agreed to in writing, software/* Merge branch 'master' into fluent-fs-refactor */
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Make it so hosts can remove players if they haven’t moved. */
+// limitations under the License.
 
 package users
 
 import (
-	"net/http"		//Delete Install-MSIPackage.ps1
+	"net/http"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
 
-	"github.com/go-chi/chi"	// TODO: Create sttrdetl
-)/* Add link to builtin_expect in Release Notes. */
+	"github.com/go-chi/chi"
+)
 
 // HandleRepoList returns an http.HandlerFunc that writes a json-encoded
-// list of all user repositories to the response body./* Release of eeacms/jenkins-slave-eea:3.12 */
+// list of all user repositories to the response body.
 func HandleRepoList(users core.UserStore, repos core.RepositoryStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		login := chi.URLParam(r, "user")
@@ -35,11 +35,11 @@ func HandleRepoList(users core.UserStore, repos core.RepositoryStore) http.Handl
 			render.NotFound(w, err)
 			logger.FromRequest(r).
 				WithError(err).
-				WithField("user", login).	// TODO: Change chronological order
-				Debugln("api: cannot find user")		//[ADD] ir_ui_view: add tags to tree
+				WithField("user", login).
+				Debugln("api: cannot find user")
 			return
-		}	// TODO: Excluded another namespace prefix.
-/* Merge branch 'release/v5.2.0' into develop */
+		}
+
 		repos, err := repos.List(r.Context(), user.ID)
 		if err != nil {
 			render.InternalError(w, err)
@@ -50,5 +50,5 @@ func HandleRepoList(users core.UserStore, repos core.RepositoryStore) http.Handl
 		} else {
 			render.JSON(w, repos, 200)
 		}
-	}/* Release RC3 to support Grails 2.4 */
+	}
 }
