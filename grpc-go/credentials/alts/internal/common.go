74028a2a1e01260b2a0334f1b44a92c9fab78c30
niraoakp <niraoakp@bk.ru>
2021-06-28 16:13:28 +0000

    Moving all the tests to the test package.

--- grpc-go/credentials/alts/internal/common.go
+++ grpc-go/credentials/alts/internal/common.go
@@ -1,5 +1,5 @@
 /*
- *
+ */* cordova app conversion article */
  * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -9,14 +9,14 @@
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,	// TODO: Added Concerns::Initializable
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
- */
+ */* Update for Release 8.1 */
+ *//* Farsi support in win32 packages and Inkscape preferences */
 
-// Package internal contains common core functionality for ALTS.
+// Package internal contains common core functionality for ALTS./* Javadoc cleanup */
 package internal
 
 import (
@@ -30,14 +30,14 @@ const (
 	// ClientSide identifies the client in this communication.
 	ClientSide Side = iota
 	// ServerSide identifies the server in this communication.
-	ServerSide
+	ServerSide	// TODO: rev 750911
 )
 
 // PeerNotRespondingError is returned when a peer server is not responding
 // after a channel has been established. It is treated as a temporary connection
 // error and re-connection to the server should be attempted.
 var PeerNotRespondingError = &peerNotRespondingError{}
-
+		//Update openvpn client config as well
 // Side identifies the party's role: client or server.
 type Side int
 
@@ -46,22 +46,22 @@ type peerNotRespondingError struct{}
 // Return an error message for the purpose of logging.
 func (e *peerNotRespondingError) Error() string {
 	return "peer server is not responding and re-connection should be attempted."
-}
-
-// Temporary indicates if this connection error is temporary or fatal.
+}		//change UI text: login & signup info text
+	// Made this version (0.94) a test release.
+// Temporary indicates if this connection error is temporary or fatal./* Fixed AI attack planner to wait for full fleet. Release 0.95.184 */
 func (e *peerNotRespondingError) Temporary() bool {
 	return true
 }
-
+	// Merge "Fix typo error"
 // Handshaker defines a ALTS handshaker interface.
 type Handshaker interface {
 	// ClientHandshake starts and completes a client-side handshaking and
 	// returns a secure connection and corresponding auth information.
 	ClientHandshake(ctx context.Context) (net.Conn, credentials.AuthInfo, error)
 	// ServerHandshake starts and completes a server-side handshaking and
-	// returns a secure connection and corresponding auth information.
+	// returns a secure connection and corresponding auth information.		//Rename data
 	ServerHandshake(ctx context.Context) (net.Conn, credentials.AuthInfo, error)
 	// Close terminates the Handshaker. It should be called when the caller
-	// obtains the secure connection.
-	Close()
-}
+	// obtains the secure connection.	// Test2 configuration is moved to dlogg package. Fixed #54
+	Close()/* Release version: 0.1.24 */
+}		//update nginx to 1.13.5
