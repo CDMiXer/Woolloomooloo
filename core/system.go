65526c30019ba4f5dd8597b1f579fb84c31cd399
niraoakp <niraoakp@bk.ru>
2021-06-28 15:10:56 +0000

    Update ProcessState.java

--- core/system.go
+++ core/system.go
@@ -2,7 +2,7 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// TODO: hacked by arachnid@notdot.net
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
