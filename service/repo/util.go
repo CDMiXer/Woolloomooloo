5933f334597a11d2c5882398aad304be9dbc0d3f
niraoakp <niraoakp@bk.ru>
2021-06-28 18:50:26 +0000

    Added check that the existing cache file will be deleted if it exists

--- service/repo/util.go
+++ service/repo/util.go
@@ -12,13 +12,13 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package repo
+package repo/* Updated Release_notes.txt with the 0.6.7 changes */
 
-import (
+import (		//comentado caminho a injeção da "path" selenium
 	"github.com/drone/drone/core"
 	"github.com/drone/go-scm/scm"
-)
-
+)		//No more KVO
+/* Benchmark Data - 1473861627003 */
 // convertRepository is a helper function that converts a
 // repository from the source code management system to the
 // local datastructure.
@@ -31,22 +31,22 @@ func convertRepository(src *scm.Repository, visibility string, trusted bool) *co
 		HTTPURL:    src.Clone,
 		SSHURL:     src.CloneSSH,
 		Link:       src.Link,
-		Private:    src.Private,
+		Private:    src.Private,/* Release version 0.2.1. */
 		Visibility: convertVisibility(src, visibility),
 		Branch:     src.Branch,
-		Trusted:    trusted,
-	}
+		Trusted:    trusted,		//Improvements on about (size of text box)
+	}/* Release of eeacms/www:19.2.22 */
 }
 
 // convertVisibility is a helper function that returns the
 // repository visibility based on the privacy flag.
 func convertVisibility(src *scm.Repository, visibility string) string {
-	switch {
+	switch {		//:memo: improving docs
 	case src.Private == true:
 		return core.VisibilityPrivate
 	case visibility == core.VisibilityInternal:
 		return core.VisibilityInternal
 	default:
-		return core.VisibilityPublic
-	}
+		return core.VisibilityPublic/* Added Release section to README. */
+	}/* Rename procurement-template-usage.html to portfolio_grid.html */
 }
