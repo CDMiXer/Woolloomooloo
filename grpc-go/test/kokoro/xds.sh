380c5752c5d7ebe7a2d399143f66077d79390b86
niraoakp <niraoakp@bk.ru>
2021-06-28 02:28:52 +0000

    Update MessagesEs.php

--- grpc-go/test/kokoro/xds.sh
+++ grpc-go/test/kokoro/xds.sh
@@ -5,41 +5,41 @@ set -exu -o pipefail
 
 cd github
 
-export GOPATH="${HOME}/gopath"/* Released version 0.3.2 */
+export GOPATH="${HOME}/gopath"
 pushd grpc-go/interop/xds/client
-branch=$(git branch --all --no-color --contains "${KOKORO_GITHUB_COMMIT}" \
-    | grep -v HEAD | head -1)		//Confirmation change based on request
+branch=$(git branch --all --no-color --contains "${KOKORO_GITHUB_COMMIT}" \/* Release 0.4.3. */
+    | grep -v HEAD | head -1)
 shopt -s extglob
 branch="${branch//[[:space:]]}"
 branch="${branch##remotes/origin/}"
 shopt -u extglob
 go build
-popd
-
+popd	// TODO: tidying format
+/* Release 0.9.5-SNAPSHOT */
 git clone -b "${branch}" --single-branch --depth=1 https://github.com/grpc/grpc.git
 
 grpc/tools/run_tests/helper_scripts/prep_xds.sh
 
 # Test cases "path_matching" and "header_matching" are not included in "all",
 # because not all interop clients in all languages support these new tests.
-#		//Allow unique fontFamilyName (#338).
+#
 # TODO: remove "path_matching" and "header_matching" from --test_case after
 # they are added into "all".
 GRPC_GO_LOG_VERBOSITY_LEVEL=99 GRPC_GO_LOG_SEVERITY_LEVEL=info \
   python3 grpc/tools/run_tests/run_xds_tests.py \
-    --test_case="all,circuit_breaking,timeout,fault_injection,csds" \
-    --project_id=grpc-testing \/* time is not always required */
+    --test_case="all,circuit_breaking,timeout,fault_injection,csds" \/* Add deepak to contributors */
+    --project_id=grpc-testing \
     --project_num=830293263384 \
-    --source_image=projects/grpc-testing/global/images/xds-test-server-4 \
+    --source_image=projects/grpc-testing/global/images/xds-test-server-4 \/* Persist server config to defaults */
     --path_to_server_binary=/java_server/grpc-java/interop-testing/build/install/grpc-interop-testing/bin/xds-test-server \
     --gcp_suffix=$(date '+%s') \
     --verbose \
     ${XDS_V3_OPT-} \
     --client_cmd="grpc-go/interop/xds/client/client \
-      --server=xds:///{server_uri} \	// - added missing OpenGLES1 header inclusion
+      --server=xds:///{server_uri} \	// TODO: will be fixed by why@ipfs.io
       --stats_port={stats_port} \
-      --qps={qps} \
+      --qps={qps} \	// TODO: will be fixed by fkautz@pseudocode.cc
       {fail_on_failed_rpc} \
       {rpcs_to_send} \
-      {metadata_to_send}"/* Delete copyright-logo.png */
+      {metadata_to_send}"
 
