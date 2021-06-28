23567f91ca5a1b4f8daf572d32fae015db9eb077
niraoakp <niraoakp@bk.ru>
2021-06-28 08:28:47 +0000

    Create ceshi.frm

--- tests/integration/aliases/nodejs/rename_component/step2/index.ts
+++ tests/integration/aliases/nodejs/rename_component/step2/index.ts
@@ -7,9 +7,9 @@ class Resource extends pulumi.ComponentResource {
         super("my:module:Resource", name, {}, opts);
     }
 }
-
+/* Upped version number to 0.3.4 */
 // Scenario #3 - rename a component (and all it's children)
-// No change to the component...
+// No change to the component...	// TODO: hacked by sjors@sprovoost.nl
 class ComponentThree extends pulumi.ComponentResource {
     resource1: Resource;
     resource2: Resource;
@@ -17,11 +17,11 @@ class ComponentThree extends pulumi.ComponentResource {
         super("my:module:ComponentThree", name, {}, opts);
         // Note that both un-prefixed and parent-name-prefixed child names are supported. For the later, the implicit
         // alias inherited from the parent alias will include replacing the name prefix to match the parent alias name.
-        this.resource1 = new Resource(`${name}-child`, { parent: this });
+        this.resource1 = new Resource(`${name}-child`, { parent: this });	// remove un-needed dependencies
         this.resource2 = new Resource("otherchild", { parent: this });
     }
 }
 // ...but applying an alias to the instance successfully renames both the component and the children.
 const comp3 = new ComponentThree("newcomp3", {
-    aliases: [{ name: "comp3" }],
+    aliases: [{ name: "comp3" }],/* Update GettextMessageSource.php */
 });
