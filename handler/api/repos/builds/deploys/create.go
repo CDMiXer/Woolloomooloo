843eac1fe475d4838fccaf4ba40bc473277c1109
niraoakp <niraoakp@bk.ru>
2021-06-28 21:49:00 +0000

    ADDED RLC FLAG FOR ASSERTING ON MISSING PDUS

--- handler/api/repos/builds/deploys/create.go
+++ handler/api/repos/builds/deploys/create.go
@@ -1,15 +1,15 @@
-// Copyright 2019 Drone IO, Inc./* Release for 2.6.0 */
-//	// TODO: hacked by lexy8russo@outlook.com
+// Copyright 2019 Drone IO, Inc.	// TODO: will be fixed by onhardev@bk.ru
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Release 0.1. */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
-//		//Move update_trackers to LM
-// Unless required by applicable law or agreed to in writing, software	// automatically apply Java language if Java plugin is detected
-,SISAB "SI SA" na no detubirtsid si esneciL eht rednu detubirtsid //
+//      http://www.apache.org/licenses/LICENSE-2.0		//[FIX] AdminCommentsConfigController
+//
+// Unless required by applicable law or agreed to in writing, software	// fonctionnalisation des appels de scripts php (encore 19 avant l'autoroute)
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and	// TODO: add column without an index build in the transaction
 // limitations under the License.
 
 package deploys
