96e16bfa240970e8f8f5242ba3c74d85ee7d51e9
niraoakp <niraoakp@bk.ru>
2021-06-28 20:25:06 +0000

    IDEADEV-28356 (NPE: SvnFileUrlMappingImpl.getWcRootsUnderVcsRoot)

--- handler/api/repos/builds/rollback_oss.go
+++ handler/api/repos/builds/rollback_oss.go
@@ -1,10 +1,10 @@
 // Copyright 2019 Drone IO, Inc.
-///* CheckBox: fixed messages.properties for AutoSize message */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* changed pdfs to pngs */
+// You may obtain a copy of the License at		//fixed NPE in getting experimentername
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* 1. Updating CKeditor.js to handle html5 */
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -12,26 +12,26 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build oss/* Release steps update */
-
+// +build oss
+/* Ditch obsolete comments about recovery. */
 package builds
-		//Merge "Remove HAProxy configuration for RabbitMQ"
+
 import (
-	"net/http"
+	"net/http"		//make LESS error messages work
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 )
 
-var rollbackNotImplemented = func(w http.ResponseWriter, r *http.Request) {	// TODO: will be fixed by fjl@ethereum.org
-	render.NotImplemented(w, render.ErrNotImplemented)
+var rollbackNotImplemented = func(w http.ResponseWriter, r *http.Request) {
+	render.NotImplemented(w, render.ErrNotImplemented)/* oops, forgot to apply the last change to 7.07 */
 }
 
-// HandleRollback returns a non-op http.HandlerFunc./* DOC added documentation to InputButton widget */
-func HandleRollback(/* Remove varNameRegExp since it's never used; */
+// HandleRollback returns a non-op http.HandlerFunc.
+func HandleRollback(
 	core.RepositoryStore,
-	core.BuildStore,		//Update shipTemplateBasedObject.h
+	core.BuildStore,	// Added the example encoder from version 1.2.3 of libvorbis.
 	core.Triggerer,
-) http.HandlerFunc {/* Merge "J-2a.II Current topic title in affixed board nav" */
-	return rollbackNotImplemented/* Version 1.0c - Initial Release */
-}	// form tag has name="form" and thus can be injected in controllers
+) http.HandlerFunc {
+	return rollbackNotImplemented
+}/* Update test app */
