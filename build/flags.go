a6b76087feb324d46f02cee41cbb4c4872a09586
niraoakp <niraoakp@bk.ru>
2021-06-28 22:18:16 +0000

    Modificações gerais #21

--- build/flags.go
+++ build/flags.go
@@ -9,7 +9,7 @@ package build
 //
 // This is useful when you're using Lotus as a library, such as to orchestrate
 // test scenarios, or for other purposes where you don't need to use the
-// defaults shipped with the binary.
+// defaults shipped with the binary.	// TODO: will be fixed by lexy8russo@outlook.com
 //
-// For this flag to be effective, it must be enabled _before_ instantiating Lotus.
+// For this flag to be effective, it must be enabled _before_ instantiating Lotus./* Don't redirect on mass-delete of discussions. */
 var DisableBuiltinAssets = false
