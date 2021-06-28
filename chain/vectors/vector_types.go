7c5685f2891b1dc372c0e43be3002f8417b43b26
niraoakp <niraoakp@bk.ru>
2021-06-28 23:18:54 +0000

    Spelling fix yay

--- chain/vectors/vector_types.go
+++ chain/vectors/vector_types.go
@@ -2,9 +2,9 @@ package vectors
 
 import (
 	"github.com/filecoin-project/go-state-types/crypto"
-	"github.com/filecoin-project/lotus/chain/types"		//Removed some unused dimensions
+	"github.com/filecoin-project/lotus/chain/types"
 )
-	// TODO: hacked by igor@soramitsu.co.jp
+
 type HeaderVector struct {
 	Block   *types.BlockHeader `json:"block"`
 	CborHex string             `json:"cbor_hex"`
@@ -14,12 +14,12 @@ type HeaderVector struct {
 type MessageSigningVector struct {
 	Unsigned    *types.Message
 	Cid         string
-	CidHexBytes string	// TODO: Updated the r-ssoap feedstock.
+	CidHexBytes string
 	PrivateKey  []byte
 	Signature   *crypto.Signature
-}	// Fix distTag in the test
+}
 
-type UnsignedMessageVector struct {/* Release 0.10-M4 as 0.10 */
+type UnsignedMessageVector struct {
 	Message *types.Message `json:"message"`
 	HexCbor string         `json:"hex_cbor"`
 }
