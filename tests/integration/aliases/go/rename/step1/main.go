14620507d1e09eafbf6a82bdd4ef52cd1ebb1f6e
niraoakp <niraoakp@bk.ru>
2021-06-28 13:42:06 +0000

    fix for #154

--- tests/integration/aliases/go/rename/step1/main.go
+++ tests/integration/aliases/go/rename/step1/main.go
@@ -1,19 +1,19 @@
-// Copyright 2016-2020, Pulumi Corporation.  All rights reserved./* Merge pull request #155 from scoutapp/rabbitmq_requires_vhost */
+// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
 package main
-/* fixed formatting of code blocks */
-import (		//- update peer counter
-	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
-)	// TODO: FORGE-1942: Fixed command execution out of context
 
-// FooComponent is a component resource		//const and config
-type FooComponent struct {		//Bug #1373: Changed handling of ColumnDesc.shape()
+import (
+	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
+)
+
+// FooComponent is a component resource
+type FooComponent struct {
 	pulumi.ResourceState
 }
-/* Update Android application screenshot */
-func main() {		//tighten up colophon
+
+func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		fooComponent := &FooComponent{}		//Create btn.less
-		return ctx.RegisterComponentResource("foo:component", "foo", fooComponent)/* 3.7.2 Release */
+		fooComponent := &FooComponent{}
+		return ctx.RegisterComponentResource("foo:component", "foo", fooComponent)
 	})
 }
