3349712ab905a2b1b6fda386a90749f81993df2b
niraoakp <niraoakp@bk.ru>
2021-06-28 03:06:28 +0000

    #GH370-refactor2

--- tests/integration/read/read_dbr/step2/index.ts
+++ tests/integration/read/read_dbr/step2/index.ts
@@ -1,31 +1,31 @@
 // Copyright 2016-2018, Pulumi Corporation.
-//	// TODO: All widgets update	
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Release for 18.27.0 */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// TODO: Mise à jour des systèmes de gesion des Races et des Classes
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Added Release Note reference */
-// See the License for the specific language governing permissions and	// TODO: hacked by xiemengjun@gmail.com
-// limitations under the License.	// TODO: Merge "Add a --uuids-only option to rally task list"
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 import { Resource } from "./resource";
 
-const a = new Resource("a", { state: 42 }, { id: "existing-id"} );/* UPDATE - URL logic to pull url to download file */
+const a = new Resource("a", { state: 42 }, { id: "existing-id"} );
 
 // B must be replaced, but it is a DBR replacement.
-const b = new Resource("b", { state: a.state.apply((b: any) => b + 2)});		//Disable useless test for now.
+const b = new Resource("b", { state: a.state.apply((b: any) => b + 2)});
 
 // C depends on B, so it gets re-read. Before the read, it is removed from the
 // snapshot due to the deletion of B.
 const c = new Resource("c", { state: b.state }, { id: "another-existing-id" })
-	// owner/patron
-// The engine generates:		//Delete spring.jsonmesh
+
+// The engine generates:
 // A: Same
 // C: DeleteReplacement (read)
 // B: DeleteReplacement
-// B: Create/* add more optional story */
+// B: Create
 // C: Read
