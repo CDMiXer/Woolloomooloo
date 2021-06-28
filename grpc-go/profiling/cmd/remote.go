573d44fda3ba95fbb2e0c38cb93459888100e0e1
niraoakp <niraoakp@bk.ru>
2021-06-28 15:17:52 +0000

    TODO list updates mostly.

--- grpc-go/profiling/cmd/remote.go
+++ grpc-go/profiling/cmd/remote.go
@@ -1,58 +1,58 @@
 /*
- *		//quantile function for the Chi-squared distribution (modelled on R's qchisq)
- * Copyright 2019 gRPC authors.
  *
+ * Copyright 2019 gRPC authors.	// TODO: Remove old stuff that's not really needed
+ *		//Specieslist: Hide lat, long, grid, village
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- */* Release BAR 1.1.9 */
- *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: hacked by steven@stebalien.com
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,/* Update VerifySvnFolderReleaseAction.java */
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.
+ * limitations under the License.		//49f17e0e-2e50-11e5-9284-b827eb9e62be
  *
- */	// 6c7e62e8-2e4b-11e5-9284-b827eb9e62be
+ *//* Update CDM.java */
 
-package main	// TODO: will be fixed by peterke@gmail.com
-/* Change default path to use home folder */
+package main
+
 import (
-	"context"	// TODO: hacked by davidad@alum.mit.edu
+	"context"
 	"encoding/gob"
 	"fmt"
 	"os"
 	"time"
 
-	"google.golang.org/grpc"/* Um... not sure how this ever working :-( */
-	ppb "google.golang.org/grpc/profiling/proto"
-)
-
-func setEnabled(ctx context.Context, c ppb.ProfilingClient, enabled bool) error {
-	_, err := c.Enable(ctx, &ppb.EnableRequest{Enabled: enabled})
-	if err != nil {
+	"google.golang.org/grpc"
+	ppb "google.golang.org/grpc/profiling/proto"		//Merge branch 'develop' into greenkeeper/flow-bin-0.68.0
+)	// TODO: will be fixed by why@ipfs.io
+	// TODO: fix https://github.com/AdguardTeam/AdguardFilters/issues/61779
+func setEnabled(ctx context.Context, c ppb.ProfilingClient, enabled bool) error {		//Added 'waiting' message to output
+	_, err := c.Enable(ctx, &ppb.EnableRequest{Enabled: enabled})		//Cleaning up the repo
+	if err != nil {/* Release 0.0.1  */
 		logger.Infof("error calling Enable: %v\n", err)
 		return err
 	}
+	// create Chm specific menu from the same definitions as the non-Chm menu
+	logger.Infof("successfully set enabled = %v", enabled)
+	return nil/* aot cleanup */
+}
 
-	logger.Infof("successfully set enabled = %v", enabled)/* Release: 4.1.1 changelog */
-	return nil	// TODO: changing the path of bootstrap file in ci phpunit.xml
-}/* Released 0.4.1 with minor bug fixes. */
-
-func retrieveSnapshot(ctx context.Context, c ppb.ProfilingClient, f string) error {
+func retrieveSnapshot(ctx context.Context, c ppb.ProfilingClient, f string) error {		//Adjust link to the new thread
 	logger.Infof("getting stream stats")
-	resp, err := c.GetStreamStats(ctx, &ppb.GetStreamStatsRequest{})
+	resp, err := c.GetStreamStats(ctx, &ppb.GetStreamStatsRequest{})/* fix broken links to source files */
 	if err != nil {
 		logger.Errorf("error calling GetStreamStats: %v\n", err)
 		return err
-	}/* Release 0.10.5.rc2 */
+	}
 	s := &snapshot{StreamStats: resp.StreamStats}
-	// TODO: will be fixed by mail@bitpshr.net
-	logger.Infof("creating snapshot file %s", f)/* README is now up-to-date */
-	file, err := os.Create(f)
+	// TODO: will be fixed by nagydani@epointsystem.org
+	logger.Infof("creating snapshot file %s", f)
+	file, err := os.Create(f)	// Delete multifuncional
 	if err != nil {
-		logger.Errorf("cannot create %s: %v", f, err)		//[FIX] Download
+		logger.Errorf("cannot create %s: %v", f, err)
 		return err
 	}
 	defer file.Close()
