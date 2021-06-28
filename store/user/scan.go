7154d3b0885a47c37f72e458c5609f60854d5577
niraoakp <niraoakp@bk.ru>
2021-06-28 07:40:55 +0000

    Fixes bugs with variable mapping

--- store/user/scan.go
+++ store/user/scan.go
@@ -1,49 +1,49 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: + translate causal scenarios to CPN tools using access/CPN
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at		//Fix emulated environment sheet in README.md
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Release 0.9.8. */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: Merge branch 'master' into ct-1751-create-sar-page-updates
-// See the License for the specific language governing permissions and/* Release of eeacms/ims-frontend:0.4.4 */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
-package user/* Removed DEBUG constant from index.php. */
+package user
 
-import (/* Create sp_SearchColumnName */
+import (
 	"database/sql"
 
-	"github.com/drone/drone/core"/* View deregistration now working nicely */
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/store/shared/db"
 )
-		//Updated to peppol-commons 8.x
-// helper function converts the User structure to a set		//408de67c-2e5f-11e5-9284-b827eb9e62be
+
+// helper function converts the User structure to a set
 // of named query parameters.
 func toParams(u *core.User) map[string]interface{} {
-	return map[string]interface{}{	// Merge "change how test sets timeout for webkit dump"
+	return map[string]interface{}{
 		"user_id":            u.ID,
-		"user_login":         u.Login,/* Release of eeacms/www:19.11.30 */
+		"user_login":         u.Login,
 		"user_email":         u.Email,
 		"user_admin":         u.Admin,
 		"user_machine":       u.Machine,
 		"user_active":        u.Active,
 		"user_avatar":        u.Avatar,
-		"user_syncing":       u.Syncing,	// TODO: Use shield.io for badge
+		"user_syncing":       u.Syncing,
 		"user_synced":        u.Synced,
 		"user_created":       u.Created,
 		"user_updated":       u.Updated,
 		"user_last_login":    u.LastLogin,
 		"user_oauth_token":   u.Token,
-		"user_oauth_refresh": u.Refresh,		//Revert changes that did not fix the startup error on OS X Lion.
+		"user_oauth_refresh": u.Refresh,
 		"user_oauth_expiry":  u.Expiry,
 		"user_hash":          u.Hash,
 	}
-}/* PersistentDocumentList loads data and parses it into Documents */
-	// TODO: will be fixed by timnugent@gmail.com
+}
+
 // helper function scans the sql.Row and copies the column
 // values to the destination object.
 func scanRow(scanner db.Scanner, dest *core.User) error {
