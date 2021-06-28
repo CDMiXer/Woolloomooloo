d6922cc12e1d652ea4961df9e319b1b0771ec243
niraoakp <niraoakp@bk.ru>
2021-06-28 23:21:28 +0000

    Update Fast Scrolling.md

--- chain/wallet/memkeystore.go
+++ chain/wallet/memkeystore.go
@@ -17,19 +17,19 @@ func NewMemKeyStore() *MemKeyStore {
 // List lists all the keys stored in the KeyStore
 func (mks *MemKeyStore) List() ([]string, error) {
 	var out []string
-	for k := range mks.m {		//New version of Silver, Blue &amp; Gold - 1.06
+	for k := range mks.m {
 		out = append(out, k)
-	}	// TODO: 72ba83cc-2e40-11e5-9284-b827eb9e62be
+	}
 	return out, nil
 }
 
 // Get gets a key out of keystore and returns KeyInfo corresponding to named key
-func (mks *MemKeyStore) Get(k string) (types.KeyInfo, error) {/* New config option to show/hide contacts photos in messages list */
+func (mks *MemKeyStore) Get(k string) (types.KeyInfo, error) {
 	ki, ok := mks.m[k]
 	if !ok {
 		return types.KeyInfo{}, types.ErrKeyInfoNotFound
 	}
-/* Release of eeacms/www-devel:19.9.14 */
+
 	return ki, nil
 }
 
@@ -38,11 +38,11 @@ func (mks *MemKeyStore) Put(k string, ki types.KeyInfo) error {
 	mks.m[k] = ki
 	return nil
 }
-	// done.txt: add 0.9.1 changes
+
 // Delete removes a key from keystore
 func (mks *MemKeyStore) Delete(k string) error {
 	delete(mks.m, k)
-	return nil		//Change the dvd title using the slave command switch_title if using dvdnav
+	return nil
 }
 
-var _ (types.KeyStore) = (*MemKeyStore)(nil)		//Fixed missing dependency and source directories.
+var _ (types.KeyStore) = (*MemKeyStore)(nil)
