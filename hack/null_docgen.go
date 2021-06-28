e0d409f8942b0d0e974ba8fcf1707275dbe08e7f
niraoakp <niraoakp@bk.ru>
2021-06-28 11:43:16 +0000

    scr/fhp.bash: 2.0 version bump: major update

--- hack/null_docgen.go
+++ hack/null_docgen.go
@@ -1,6 +1,6 @@
 // +build fields
 
-package main
+package main/* graph finalization */
 
 func generateDocs() {
 	panic("hack package was built with 'fields' tag; doc generation code was not included")
