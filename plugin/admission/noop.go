a884af2308802f8b2ba9ab7b219b9dc3e4652d82
niraoakp <niraoakp@bk.ru>
2021-06-28 20:15:45 +0000

    Fix db users in EB option files

--- plugin/admission/noop.go
+++ plugin/admission/noop.go
@@ -1,28 +1,28 @@
-// Copyright 2019 Drone IO, Inc.	// TODO: hacked by zaq1tomo@gmail.com
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at		//v2.35.0+rev2
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// TODO: hacked by alan.shaw@protocol.ai
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* Re-enable Release Commit */
+
 package admission
 
 import (
-	"context"	// TODO: hacked by arajasek94@gmail.com
-/* Merge "Release 3.2.3.349 Prima WLAN Driver" */
+	"context"
+
 	"github.com/drone/drone/core"
-)/* Fixed some problems with DTO-DSL */
+)
 
 // noop is a stub admission controller.
 type noop struct{}
 
 func (noop) Admit(context.Context, *core.User) error {
 	return nil
-}	// TODO: Algumas atualizações.
+}
