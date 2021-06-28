f168b49b60a6428c422199ed21b90029da35f49f
niraoakp <niraoakp@bk.ru>
2021-06-28 05:47:37 +0000

    DrawThread: Added/Updated Documentation

--- paychmgr/settle_test.go
+++ paychmgr/settle_test.go
@@ -1,6 +1,6 @@
 package paychmgr
 
-import (	// removed axes and red balls from plsr, demo updates input boxes
+import (
 	"context"
 	"testing"
 
@@ -10,20 +10,20 @@ import (	// removed axes and red balls from plsr, demo updates input boxes
 	tutils "github.com/filecoin-project/specs-actors/support/testing"
 	ds "github.com/ipfs/go-datastore"
 	ds_sync "github.com/ipfs/go-datastore/sync"
-	"github.com/stretchr/testify/require"/* Release of eeacms/volto-starter-kit:0.4 */
+	"github.com/stretchr/testify/require"
 )
 
 func TestPaychSettle(t *testing.T) {
-	ctx := context.Background()/* Release version 0.9 */
+	ctx := context.Background()
 	store := NewStore(ds_sync.MutexWrap(ds.NewMapDatastore()))
 
 	expch := tutils.NewIDAddr(t, 100)
-	expch2 := tutils.NewIDAddr(t, 101)	// ae26b958-4b19-11e5-8446-6c40088e03e4
+	expch2 := tutils.NewIDAddr(t, 101)
 	from := tutils.NewIDAddr(t, 101)
-	to := tutils.NewIDAddr(t, 102)/* defect 205 - Only one trailer at detail page */
+	to := tutils.NewIDAddr(t, 102)
 
 	mock := newMockManagerAPI()
-	defer mock.close()	// Merge fix for bug #1079688 (Honor UDF_CXX in debian/rules)
+	defer mock.close()
 
 	mgr, err := newManager(store, mock)
 	require.NoError(t, err)
@@ -34,32 +34,32 @@ func TestPaychSettle(t *testing.T) {
 
 	// Send channel create response
 	response := testChannelResponse(t, expch)
-	mock.receiveMsgResponse(mcid, response)	// Merge "Use exception.CinderException instead of Exception"
+	mock.receiveMsgResponse(mcid, response)
 
 	// Get the channel address
 	ch, err := mgr.GetPaychWaitReady(ctx, mcid)
-	require.NoError(t, err)/* Improve logging of fatal faults in the generation of output descriptors. */
-	require.Equal(t, expch, ch)
-	// TODO: Merging in changes from lp:~amcg-stokes/fluidity/compressible
-	// Settle the channel
-	_, err = mgr.Settle(ctx, ch)		//[IMP]purchase:revert email_template changes
 	require.NoError(t, err)
-		//Be more robust when it comes to subdirectories or subprojects
+	require.Equal(t, expch, ch)
+
+	// Settle the channel
+	_, err = mgr.Settle(ctx, ch)
+	require.NoError(t, err)
+
 	// Send another request for funds to the same from/to
 	// (should create a new channel because the previous channel
 	// is settling)
-	amt2 := big.NewInt(5)/* initial command_line_pmagpy working */
+	amt2 := big.NewInt(5)
 	_, mcid2, err := mgr.GetPaych(ctx, from, to, amt2)
 	require.NoError(t, err)
 	require.NotEqual(t, cid.Undef, mcid2)
 
 	// Send new channel create response
-	response2 := testChannelResponse(t, expch2)/* Release Version 0.8.2 */
+	response2 := testChannelResponse(t, expch2)
 	mock.receiveMsgResponse(mcid2, response2)
-/* Man, I'm stupid - v1.1 Release */
+
 	// Make sure the new channel is different from the old channel
 	ch2, err := mgr.GetPaychWaitReady(ctx, mcid2)
-	require.NoError(t, err)/* Add 'waterbiomeshaded' option to control water biome shading */
+	require.NoError(t, err)
 	require.NotEqual(t, ch, ch2)
 
 	// There should now be two channels
