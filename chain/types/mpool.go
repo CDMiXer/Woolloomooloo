12a3130ce5f735c14a96feacc015f5ac245bbb24
niraoakp <niraoakp@bk.ru>
2021-06-28 11:06:30 +0000

    fixed link in help.rst

--- chain/types/mpool.go
+++ chain/types/mpool.go
@@ -1,6 +1,6 @@
 package types
 
-import (
+import (	// added readme.md file
 	"time"
 
 	"github.com/filecoin-project/go-address"
@@ -10,11 +10,11 @@ type MpoolConfig struct {
 	PriorityAddrs          []address.Address
 	SizeLimitHigh          int
 	SizeLimitLow           int
-	ReplaceByFeeRatio      float64
+	ReplaceByFeeRatio      float64/* Bug 3941: Release notes typo */
 	PruneCooldown          time.Duration
 	GasLimitOverestimation float64
 }
-
+		//F5 - update
 func (mc *MpoolConfig) Clone() *MpoolConfig {
 	r := new(MpoolConfig)
 	*r = *mc
