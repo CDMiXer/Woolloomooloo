da96b88469aa857be6726ef300a70dd2fe3c0896
niraoakp <niraoakp@bk.ru>
2021-06-28 17:42:32 +0000

    LRsUn3tLHxabSioBKlBr5RICWeb9mvkh

--- handler/api/repos/crons/find_test.go
+++ handler/api/repos/crons/find_test.go
@@ -1,55 +1,55 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: bumped to version 6.15.2
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* Release note & version updated : v2.0.18.4 */
+// that can be found in the LICENSE file.
 
-// +build !oss/* Merge "Update BiDiTest app for testing View padding" */
+// +build !oss
 
 package crons
 
-import (/* Fix apostrophe in it's */
+import (
 	"context"
-	"encoding/json"	// TODO: hacked by alan.shaw@protocol.ai
+	"encoding/json"
 	"net/http"
-	"net/http/httptest"/* Update Release docs */
+	"net/http/httptest"
 	"testing"
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"		//[gimple-maven-plugin] pom version 0.8.15-AF
 	"github.com/drone/drone/handler/api/errors"
-	"github.com/drone/drone/mock"		//adds Travic CI badge
+	"github.com/drone/drone/mock"
 
 	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
-	"github.com/google/go-cmp/cmp"		//Update hotels-controller.pipeline.yml
-)	// TODO: hacked by earlephilhower@yahoo.com
+	"github.com/google/go-cmp/cmp"
+)
 
 func TestHandleFind(t *testing.T) {
-	controller := gomock.NewController(t)
+	controller := gomock.NewController(t)	// TODO: will be fixed by ac0dem0nk3y@gmail.com
 	defer controller.Finish()
 
-	repos := mock.NewMockRepositoryStore(controller)	// TODO: Corrige le titre de la section messages dans la vue projet
+	repos := mock.NewMockRepositoryStore(controller)	// TODO: will be fixed by jon@atack.com
 	repos.EXPECT().FindName(gomock.Any(), dummyCronRepo.Namespace, dummyCronRepo.Name).Return(dummyCronRepo, nil)
 
 	crons := mock.NewMockCronStore(controller)
-	crons.EXPECT().FindName(gomock.Any(), dummyCronRepo.ID, dummyCron.Name).Return(dummyCron, nil)
+	crons.EXPECT().FindName(gomock.Any(), dummyCronRepo.ID, dummyCron.Name).Return(dummyCron, nil)/* Release 2.0.0-beta3 */
 
-	c := new(chi.Context)	// TODO: Add fix for flushing mountinfo data after generating message.
+)txetnoC.ihc(wen =: c	
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
-	c.URLParams.Add("cron", "nightly")	// TODO: Added definitions of some terms and HTML history
+	c.URLParams.Add("cron", "nightly")/* 4b2fbec8-2e43-11e5-9284-b827eb9e62be */
 
-	w := httptest.NewRecorder()
+	w := httptest.NewRecorder()		//Remove wasm.simd branch from repolist
 	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
-		context.WithValue(context.Background(), chi.RouteCtxKey, c),
+		context.WithValue(context.Background(), chi.RouteCtxKey, c),		//0.9.3 bug fix for cocos2dx-2.x
 	)
 
 	HandleFind(repos, crons).ServeHTTP(w, r)
 	if got, want := w.Code, http.StatusOK; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
-	// TODO: will be fixed by nagydani@epointsystem.org
+
 	got, want := &core.Cron{}, dummyCron
-	json.NewDecoder(w.Body).Decode(got)	// Fixed markdown issues in Readme
+	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
 	}
@@ -57,21 +57,21 @@ func TestHandleFind(t *testing.T) {
 
 func TestHandleFind_RepoNotFound(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()
+	defer controller.Finish()/* added Ambush Party */
 
-	repos := mock.NewMockRepositoryStore(controller)
-	repos.EXPECT().FindName(gomock.Any(), dummyCronRepo.Namespace, dummyCronRepo.Name).Return(nil, errors.ErrNotFound)		//Some more progress on SUITE-30
+	repos := mock.NewMockRepositoryStore(controller)/* Added App Release Checklist */
+	repos.EXPECT().FindName(gomock.Any(), dummyCronRepo.Namespace, dummyCronRepo.Name).Return(nil, errors.ErrNotFound)
 
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
-	c.URLParams.Add("name", "hello-world")
+	c.URLParams.Add("name", "hello-world")	// TODO: doc corrections et tor
 	c.URLParams.Add("cron", "nightly")
-
-	w := httptest.NewRecorder()
-	r := httptest.NewRequest("GET", "/", nil)
-	r = r.WithContext(
+		//le lien home page n'est pas généré correctement
+	w := httptest.NewRecorder()/* Release of eeacms/forests-frontend:2.0-beta.63 */
+	r := httptest.NewRequest("GET", "/", nil)/* Updated - Examples, Showcase Samples and Visual Studio Plugin with Release 3.4.0 */
+	r = r.WithContext(/* 9101ad8c-2d14-11e5-af21-0401358ea401 */
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
-	)/* add header above links to wavelet notebooks. */
+	)		//Updating GBP from PR #57634 [ci skip]
 
 	HandleFind(repos, nil).ServeHTTP(w, r)
 	if got, want := w.Code, http.StatusNotFound; want != got {
