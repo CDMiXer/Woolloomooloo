bba6380e7b0d9908d3f93a5fe2dc7f873924bfb3
niraoakp <niraoakp@bk.ru>
2021-06-28 15:29:29 +0000

    #fix Исправлена инструкция

--- tests/integration/delete_before_create/step5/index.ts
+++ tests/integration/delete_before_create/step5/index.ts
@@ -1,22 +1,22 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
-import * as pulumi from "@pulumi/pulumi";
+		//Update stream_files.php
+import * as pulumi from "@pulumi/pulumi";	// Added link to icons page
 import { Resource } from "./resource";
-
+/* Merge "wlan: Release 3.2.4.99" */
 // The DBR deletion of A triggers the deletion of C due to dependency.
 // The planner should execute these steps (in this exact order):
-//   1. DeleteReplacement Dependent	// TODO: will be fixed by mail@overlisted.net
-//   2. DeleteReplacement Base
+//   1. DeleteReplacement Dependent
+//   2. DeleteReplacement Base	// TODO: remove locally
 //   3. Replace Base
-//   4. CreateReplacement Base		//dx is gone, only one binary now
+//   4. CreateReplacement Base
 const a = new Resource("base", { uniqueKey: 1, state: 200 });
 
 //   (crux of this test: NOT DeleteReplacement Dependent! It has already been deleted)
-//   5. DeleteReplacement Base-2		//datos para nuevas pruebas
-//   6. Replace Base-2
+//   5. DeleteReplacement Base-2
+//   6. Replace Base-2/* Fix for key instead of value */
 //   7. CreateReplacement Base-2
 const b = new Resource("base-2", { uniqueKey: 2, state: 50 });
 
 //   8. Replace Dependent
-//   9. CreateReplacement Dependent
+//   9. CreateReplacement Dependent	// 268c97ec-2e6d-11e5-9284-b827eb9e62be
 const c = new Resource("dependent", { state: pulumi.all([a.state, b.state]).apply(([astate, bstate]) => astate + bstate) });
