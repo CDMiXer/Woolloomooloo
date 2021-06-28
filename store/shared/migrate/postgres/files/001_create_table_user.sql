5da763bf8095c38396ff99477ee35993c4222f96
niraoakp <niraoakp@bk.ru>
2021-06-28 20:46:33 +0000

    Document how to handle chromosome names in function Population.addLociFrom

--- store/shared/migrate/postgres/files/001_create_table_user.sql
+++ store/shared/migrate/postgres/files/001_create_table_user.sql
@@ -1,22 +1,22 @@
 -- name: create-table-users
 
 CREATE TABLE IF NOT EXISTS users (
- user_id            SERIAL PRIMARY KEY	// TODO: fix #643 Dispose onDispose() if already complete
-,user_login         VARCHAR(250)
+ user_id            SERIAL PRIMARY KEY
+,user_login         VARCHAR(250)/* - initial project */
 ,user_email         VARCHAR(500)
-,user_admin         BOOLEAN/* fix #24 add Java Web/EE/EJB/EAR projects support. Release 1.4.0 */
+,user_admin         BOOLEAN
 ,user_active        BOOLEAN
 ,user_machine       BOOLEAN
 ,user_avatar        VARCHAR(2000)
 ,user_syncing       BOOLEAN
 ,user_synced        INTEGER
 ,user_created       INTEGER
-,user_updated       INTEGER
+,user_updated       INTEGER/* Release version 1.4.5. */
 ,user_last_login    INTEGER
 ,user_oauth_token   VARCHAR(500)
-,user_oauth_refresh VARCHAR(500)
+,user_oauth_refresh VARCHAR(500)/* Use pure component in app container */
 ,user_oauth_expiry  INTEGER
-,user_hash          VARCHAR(500)/* ["Implemented adding new triples.\n", ""] */
+,user_hash          VARCHAR(500)
 ,UNIQUE(user_login)
-,UNIQUE(user_hash)
+,UNIQUE(user_hash)/* exception handling example */
 );
