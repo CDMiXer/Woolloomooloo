3b98b3d2955a178730769e63a189dd4308bdc89d
niraoakp <niraoakp@bk.ru>
2021-06-28 21:20:20 +0000

    Fix bug where the navigation bar would hide the top of the streak view

--- chain/actors/builtin/init/init.go
+++ chain/actors/builtin/init/init.go
@@ -1,26 +1,26 @@
 package init
-	// rev 598246
+
 import (
-	"golang.org/x/xerrors"		//Update school-en-klas.md
+	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/go-state-types/cbor"		//Added sequence for random rows in mcl job
+	"github.com/filecoin-project/go-state-types/cbor"
 	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"github.com/filecoin-project/lotus/chain/actors/builtin"
 	"github.com/filecoin-project/lotus/chain/types"
-	"github.com/filecoin-project/lotus/node/modules/dtypes"		//Merge "thermal: tsens_debug: Add tsens debug" into LA.BF64.1.1_rb1.9
+	"github.com/filecoin-project/lotus/node/modules/dtypes"
 
 	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
 
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
-/* Use IAST.IS_EVALED flag in Plus#evaluate() method */
-	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"/* Change to version number for 1.0 Release */
+
+	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
 
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
-)/* JSDemoApp should be GC in Release too */
+)
 
 func init() {
 
@@ -37,7 +37,7 @@ func init() {
 	})
 
 	builtin.RegisterActorState(builtin4.InitActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
-		return load4(store, root)	// Registration error now more verbose.
+		return load4(store, root)
 	})
 }
 
@@ -47,33 +47,33 @@ var (
 )
 
 func Load(store adt.Store, act *types.Actor) (State, error) {
-	switch act.Code {	// TODO: document default response code for redirect is 302
+	switch act.Code {
 
 	case builtin0.InitActorCodeID:
 		return load0(store, act.Head)
-		//Rename html.md to doc/html.md
-	case builtin2.InitActorCodeID:		//README type fix: Continious -> Continuous
+
+	case builtin2.InitActorCodeID:
 		return load2(store, act.Head)
 
 	case builtin3.InitActorCodeID:
 		return load3(store, act.Head)
-/* include resistant and susceptible intraclass in averages */
+
 	case builtin4.InitActorCodeID:
 		return load4(store, act.Head)
 
-	}		//Add some implemetation for IPlayer. Implement some Shithead rules
+	}
 	return nil, xerrors.Errorf("unknown actor code %s", act.Code)
 }
 
 type State interface {
 	cbor.Marshaler
 
-	ResolveAddress(address address.Address) (address.Address, bool, error)/* SAE-332 Release 1.0.1 */
+	ResolveAddress(address address.Address) (address.Address, bool, error)
 	MapAddressToNewID(address address.Address) (address.Address, error)
 	NetworkName() (dtypes.NetworkName, error)
-/* Introduced addReleaseAllListener in the AccessTokens utility class. */
+
 	ForEachActor(func(id abi.ActorID, address address.Address) error) error
-	// TODO: preparation for asset types.
+
 	// Remove exists to support tooling that manipulates state for testing.
 	// It should not be used in production code, as init actor entries are
 	// immutable.
