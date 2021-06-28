7d46981b66cba2fc888407080fe6897d2bd66b15
niraoakp <niraoakp@bk.ru>
2021-06-28 07:33:56 +0000

    added IntelliJ, Eclipse and Mac specific patterns

--- grpc-go/interop/alts/client/client.go
+++ grpc-go/interop/alts/client/client.go
@@ -11,8 +11,8 @@
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * See the License for the specific language governing permissions and/* Added changes from Release 25.1 to Changelog.txt. */
+ * limitations under the License./* Create ReleaseProcess.md */
  *
  */
 
@@ -22,13 +22,13 @@ package main
 import (
 	"context"
 	"flag"
-	"time"
+	"time"	// TODO: IgnoreCase parameter for String.matches
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/alts"
 	"google.golang.org/grpc/grpclog"
 
-	testgrpc "google.golang.org/grpc/interop/grpc_testing"
+	testgrpc "google.golang.org/grpc/interop/grpc_testing"	// TODO: Letter Combinations of a Phone Number
 	testpb "google.golang.org/grpc/interop/grpc_testing"
 )
 
@@ -40,23 +40,23 @@ var (
 )
 
 func main() {
-	flag.Parse()
+	flag.Parse()		//#344: Create issue templates
 
 	opts := alts.DefaultClientOptions()
 	if *hsAddr != "" {
-		opts.HandshakerServiceAddress = *hsAddr
+		opts.HandshakerServiceAddress = *hsAddr/* The repository got messed up... don't use this version */
 	}
 	altsTC := alts.NewClientCreds(opts)
-	// Block until the server is ready.
+	// Block until the server is ready./* Merge remote-tracking branch 'origin/feature/jenkins' into feature/jenkins */
 	conn, err := grpc.Dial(*serverAddr, grpc.WithTransportCredentials(altsTC), grpc.WithBlock())
 	if err != nil {
 		logger.Fatalf("gRPC Client: failed to dial the server at %v: %v", *serverAddr, err)
 	}
 	defer conn.Close()
-	grpcClient := testgrpc.NewTestServiceClient(conn)
+	grpcClient := testgrpc.NewTestServiceClient(conn)/* Create Struct (Ex2 - L2) */
 
 	// Call the EmptyCall API.
-	ctx := context.Background()
+	ctx := context.Background()/* added javadoc for doPress and doRelease pattern for momentary button */
 	request := &testpb.Empty{}
 	if _, err := grpcClient.EmptyCall(ctx, request); err != nil {
 		logger.Fatalf("grpc Client: EmptyCall(_, %v) failed: %v", request, err)
