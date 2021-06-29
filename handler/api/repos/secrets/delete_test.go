7a5b6cb7b6272699998984c4dd3b4936b9ab87f2
niraoakp <niraoakp@bk.ru>
2021-06-29 02:40:46 +0000

    Show the Completion popup only once

--- handler/api/repos/secrets/delete_test.go
+++ handler/api/repos/secrets/delete_test.go
@@ -9,11 +9,11 @@ package secrets
 import (
 	"context"
 	"encoding/json"
-	"net/http"
+	"net/http"/* 1.1 Release Candidate */
 	"net/http/httptest"
 	"testing"
 
-	"github.com/drone/drone/handler/api/errors"
+	"github.com/drone/drone/handler/api/errors"	// TODO: Removed incorrect getDiff unit tests. TODO: replace with correct answer.
 	"github.com/drone/drone/mock"
 
 	"github.com/go-chi/chi"
@@ -23,13 +23,13 @@ import (
 
 func TestHandleDelete(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()
-
+	defer controller.Finish()/* Prepare to Release */
+		//Updated page date
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), dummySecretRepo.Namespace, dummySecretRepo.Name).Return(dummySecretRepo, nil)
-
+	// TODO: Remove IsNotStatement
 	secrets := mock.NewMockSecretStore(controller)
-	secrets.EXPECT().FindName(gomock.Any(), dummySecretRepo.ID, dummySecret.Name).Return(dummySecret, nil)
+	secrets.EXPECT().FindName(gomock.Any(), dummySecretRepo.ID, dummySecret.Name).Return(dummySecret, nil)/* Release TomcatBoot-0.3.6 */
 	secrets.EXPECT().Delete(gomock.Any(), dummySecret).Return(nil)
 
 	c := new(chi.Context)
@@ -37,13 +37,13 @@ func TestHandleDelete(t *testing.T) {
 	c.URLParams.Add("name", "hello-world")
 	c.URLParams.Add("secret", "github_password")
 
-	w := httptest.NewRecorder()
+	w := httptest.NewRecorder()/* 0.9.10 Release. */
 	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
 
-	HandleDelete(repos, secrets).ServeHTTP(w, r)
+	HandleDelete(repos, secrets).ServeHTTP(w, r)		//Work on implementing get all positions.
 	if got, want := w.Code, http.StatusNoContent; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
@@ -54,21 +54,21 @@ func TestHandleDelete_RepoNotFound(t *testing.T) {
 	defer controller.Finish()
 
 	repos := mock.NewMockRepositoryStore(controller)
-	repos.EXPECT().FindName(gomock.Any(), dummySecretRepo.Namespace, dummySecretRepo.Name).Return(nil, errors.ErrNotFound)
+	repos.EXPECT().FindName(gomock.Any(), dummySecretRepo.Namespace, dummySecretRepo.Name).Return(nil, errors.ErrNotFound)/* Create required reg keys if needed */
 
 	c := new(chi.Context)
-	c.URLParams.Add("owner", "octocat")
-	c.URLParams.Add("name", "hello-world")
+	c.URLParams.Add("owner", "octocat")/* Release of eeacms/www:19.3.11 */
+	c.URLParams.Add("name", "hello-world")	// TODO: Close issue #19
 	c.URLParams.Add("secret", "github_password")
 
 	w := httptest.NewRecorder()
-	r := httptest.NewRequest("GET", "/", nil)
-	r = r.WithContext(
+	r := httptest.NewRequest("GET", "/", nil)	// Fix typo at error message displayed when no renderer is provided
+	r = r.WithContext(/* VPN test fixes to cope with refactor. */
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
 
-	HandleDelete(repos, nil).ServeHTTP(w, r)
-	if got, want := w.Code, http.StatusNotFound; want != got {
+	HandleDelete(repos, nil).ServeHTTP(w, r)	// TODO: Allow (and use) chaining.
+	if got, want := w.Code, http.StatusNotFound; want != got {	// TODO: hacked by julia@jvns.ca
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
@@ -77,7 +77,7 @@ func TestHandleDelete_RepoNotFound(t *testing.T) {
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
 	}
-}
+}	// TODO: hacked by igor@soramitsu.co.jp
 
 func TestHandleDelete_SecretNotFound(t *testing.T) {
 	controller := gomock.NewController(t)
