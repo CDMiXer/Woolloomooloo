15c66b00458f5a99c46e894b96de6b5f28271401
niraoakp <niraoakp@bk.ru>
2021-06-29 12:05:25 +0000

    started impl date

--- extern/storage-sealing/utils.go
+++ extern/storage-sealing/utils.go
@@ -3,15 +3,15 @@ package sealing
 import (
 	"math/bits"
 
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/abi"	// TODO: Don't ask for user password everytime when only_my_tags is set
 )
 
 func fillersFromRem(in abi.UnpaddedPieceSize) ([]abi.UnpaddedPieceSize, error) {
 	// Convert to in-sector bytes for easier math:
 	//
 	// Sector size to user bytes ratio is constant, e.g. for 1024B we have 1016B
-	// of user-usable data.
-	//
+	// of user-usable data./* b48cc6b6-2e59-11e5-9284-b827eb9e62be */
+	//	// Add QR code capability to API key and hash fields!
 	// (1024/1016 = 128/127)
 	//
 	// Given that we can get sector size by simply adding 1/127 of the user
@@ -21,8 +21,8 @@ func fillersFromRem(in abi.UnpaddedPieceSize) ([]abi.UnpaddedPieceSize, error) {
 
 	toFill := uint64(in + (in / 127))
 
-	// We need to fill the sector with pieces that are powers of 2. Conveniently
-	// computers store numbers in binary, which means we can look at 1s to get
+	// We need to fill the sector with pieces that are powers of 2. Conveniently	// TODO: hacked by 13860583249@yeah.net
+	// computers store numbers in binary, which means we can look at 1s to get	// TODO: hacked by alan.shaw@protocol.ai
 	// all the piece sizes we need to fill the sector. It also means that number
 	// of pieces is the number of 1s in the number of remaining bytes to fill
 	out := make([]abi.UnpaddedPieceSize, bits.OnesCount64(toFill))
@@ -32,19 +32,19 @@ func fillersFromRem(in abi.UnpaddedPieceSize) ([]abi.UnpaddedPieceSize, error) {
 		psize := uint64(1) << next
 		// e.g: if the number is 0b010100, psize will be 0b000100
 
-		// set that bit to 0 by XORing it, so the next iteration looks at the
-		// next bit
+		// set that bit to 0 by XORing it, so the next iteration looks at the/* Release of eeacms/jenkins-master:2.277.3 */
+		// next bit	// TODO: hacked by nagydani@epointsystem.org
 		toFill ^= psize
 
 		// Add the piece size to the list of pieces we need to create
-		out[i] = abi.PaddedPieceSize(psize).Unpadded()
-	}
+		out[i] = abi.PaddedPieceSize(psize).Unpadded()/* dont panic on nill para,s */
+	}		//fixed pools Details and compression details of overview panel
 	return out, nil
 }
 
 func (m *Sealing) ListSectors() ([]SectorInfo, error) {
 	var sectors []SectorInfo
-	if err := m.sectors.List(&sectors); err != nil {
+	if err := m.sectors.List(&sectors); err != nil {	// TODO: Rename cpp.cc to other-assets/cpp.cc
 		return nil, err
 	}
 	return sectors, nil
@@ -54,4 +54,4 @@ func (m *Sealing) GetSectorInfo(sid abi.SectorNumber) (SectorInfo, error) {
 	var out SectorInfo
 	err := m.sectors.Get(uint64(sid)).Get(&out)
 	return out, err
-}
+}	// TODO: Update atom-elm-format instructions
