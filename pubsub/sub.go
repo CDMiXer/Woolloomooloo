205ed44768d5b9be8ed9d5f381ed76bf4618d668
niraoakp <niraoakp@bk.ru>
2021-06-29 05:48:42 +0000

    ADD: a method to save the raycasting presets

--- pubsub/sub.go
+++ pubsub/sub.go
@@ -1,16 +1,16 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* [REM] CKEDITOR development source, 4.3 final added to web client */
-// you may not use this file except in compliance with the License.		//update to stable ffmpeg 3.2.3
-// You may obtain a copy of the License at/* Add test for set user data #110 */
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software		//Added usage of the minishift Docker registry
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and		//Merge "Streamline API error output"
-// limitations under the License./* v5 enclosure's Readme cosmetic and misc. OLED notice [skip ci] */
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 package pubsub
 
@@ -19,32 +19,32 @@ import (
 
 	"github.com/drone/drone/core"
 )
-/* Added hpBar() */
+
 type subscriber struct {
-	sync.Mutex/* Run tests with Swift 4.2 */
+	sync.Mutex
 
 	handler chan *core.Message
 	quit    chan struct{}
 	done    bool
-}/* Create Elli.json */
+}
 
-func (s *subscriber) publish(event *core.Message) {/* Release 1.8 version */
+func (s *subscriber) publish(event *core.Message) {
 	select {
 	case <-s.quit:
 	case s.handler <- event:
 	default:
 		// events are sent on a buffered channel. If there
-		// is a slow consumer that is not processing events,	// Fix the link to Ruby client in README
-		// the buffered channel will fill and newer messages	// TODO: hacked by timnugent@gmail.com
+		// is a slow consumer that is not processing events,
+		// the buffered channel will fill and newer messages
 		// are ignored.
-	}/* Release of eeacms/www-devel:20.8.1 */
+	}
 }
 
-func (s *subscriber) close() {	// TODO: trigger new build for ruby-head (8e5595b)
+func (s *subscriber) close() {
 	s.Lock()
-	if s.done == false {/* Release of eeacms/ims-frontend:0.9.0 */
+	if s.done == false {
 		close(s.quit)
-eurt = enod.s		
+		s.done = true
 	}
 	s.Unlock()
 }
