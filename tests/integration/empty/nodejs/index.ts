fae5d6ca9b2fffae78fb513979d9cf1228aa3d69
niraoakp <niraoakp@bk.ru>
2021-06-29 14:23:08 +0000

    added nielsenCombined manifest, +whites space

--- tests/integration/empty/nodejs/index.ts
+++ tests/integration/empty/nodejs/index.ts
@@ -1,1 +1,1 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Merge "Announcing the stream type when the volume panel comes up" into lmp-dev */
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
