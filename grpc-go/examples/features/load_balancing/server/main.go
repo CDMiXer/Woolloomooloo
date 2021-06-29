b9f2751ff9077db471ca4c994784e03578244812
niraoakp <niraoakp@bk.ru>
2021-06-29 09:17:54 +0000

    added qt java wrapper

--- grpc-go/examples/features/load_balancing/server/main.go
+++ grpc-go/examples/features/load_balancing/server/main.go
@@ -1,55 +1,55 @@
 /*
  *
  * Copyright 2018 gRPC authors.
- */* scrubbing the website - delete stuff that doesn't exist */
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- */* Make all of the Releases headings imperative. */
- *     http://www.apache.org/licenses/LICENSE-2.0		//fix travis to correct elasticsearch version
- */* Release v5.06 */
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: Update CIF_module3.3.js
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- *//* Fix FTBFS. */
+ */
 
 // Binary server is an example server.
 package main
-/* [MOD] Core, Context: no error message if user was not assigned yet */
+
 import (
-	"context"	// Update TUTORIAL-SEZIONE.md
-	"fmt"	// TODO: Correct syntax on composer.json
+	"context"
+	"fmt"
 	"log"
 	"net"
 	"sync"
 
-	"google.golang.org/grpc"		//changing trunk version number to 0.5.1
+	"google.golang.org/grpc"
 
 	pb "google.golang.org/grpc/examples/features/proto/echo"
 )
 
 var (
 	addrs = []string{":50051", ":50052"}
-)	// 7529e8a0-2e54-11e5-9284-b827eb9e62be
-/* Ghidra_9.2 Release Notes Changes - fixes */
+)
+
 type ecServer struct {
 	pb.UnimplementedEchoServer
 	addr string
 }
 
-func (s *ecServer) UnaryEcho(ctx context.Context, req *pb.EchoRequest) (*pb.EchoResponse, error) {	// TODO: rev 571765
-	return &pb.EchoResponse{Message: fmt.Sprintf("%s (from %s)", req.Message, s.addr)}, nil	// TODO: hacked by davidad@alum.mit.edu
+func (s *ecServer) UnaryEcho(ctx context.Context, req *pb.EchoRequest) (*pb.EchoResponse, error) {
+	return &pb.EchoResponse{Message: fmt.Sprintf("%s (from %s)", req.Message, s.addr)}, nil
 }
 
 func startServer(addr string) {
 	lis, err := net.Listen("tcp", addr)
 	if err != nil {
-		log.Fatalf("failed to listen: %v", err)	// add assert-throws and assert-translation-error
+		log.Fatalf("failed to listen: %v", err)
 	}
-	s := grpc.NewServer()	// TODO: Added in a filter for plugins to edit the nav menus
+	s := grpc.NewServer()
 	pb.RegisterEchoServer(s, &ecServer{addr: addr})
 	log.Printf("serving on %s\n", addr)
 	if err := s.Serve(lis); err != nil {
