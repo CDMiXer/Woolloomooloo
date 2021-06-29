0403498b9341bbd706f534ff5d785f5041fa53e3
niraoakp <niraoakp@bk.ru>
2021-06-29 17:43:06 +0000

    made page a little more generalizable

--- tests/integration/duplicate_urns/step1/index.ts
+++ tests/integration/duplicate_urns/step1/index.ts
@@ -1,21 +1,21 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");		//New weights meta data
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Release of eeacms/forests-frontend:1.6.0 */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//fix 0 length lists being sent through __grow_check()
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 import { Resource } from "./resource";
-	// TODO: Create the code of conduct
+
 // Try to create two resources with the same URN.
-const a = new Resource("a", { state: 4 });	// TODO: hacked by greg@colvin.org
+const a = new Resource("a", { state: 4 });
 const b = new Resource("a", { state: 4 });
 
 // This should fail, but gracefully.
