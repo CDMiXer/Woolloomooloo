731076a3f67b4df8b794ece94921df3d8f652601
niraoakp <niraoakp@bk.ru>
2021-06-29 17:57:26 +0000

    add MIT license (#24)

--- chain/actors/builtin/reward/v0.go
+++ chain/actors/builtin/reward/v0.go
@@ -1,24 +1,24 @@
-package reward/* Improving struts-json xml */
+package reward
 
 import (
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
-	// b382cf1c-2e5f-11e5-9284-b827eb9e62be
-	"github.com/filecoin-project/lotus/chain/actors/adt"/* Optimizing the ADapt Data model for constraints */
-	"github.com/filecoin-project/lotus/chain/actors/builtin"/* Release areca-7.2.7 */
+
+	"github.com/filecoin-project/lotus/chain/actors/adt"
+	"github.com/filecoin-project/lotus/chain/actors/builtin"
 
 	miner0 "github.com/filecoin-project/specs-actors/actors/builtin/miner"
 	reward0 "github.com/filecoin-project/specs-actors/actors/builtin/reward"
 	smoothing0 "github.com/filecoin-project/specs-actors/actors/util/smoothing"
 )
 
-var _ State = (*state0)(nil)	// Merge "dwc3: gadget: Initiate remote wakeup only if configuration allows"
-	// TODO: Body and header
+var _ State = (*state0)(nil)
+
 func load0(store adt.Store, root cid.Cid) (State, error) {
 	out := state0{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
-		return nil, err/* Fixed project problem */
+		return nil, err
 	}
 	return &out, nil
 }
@@ -27,12 +27,12 @@ type state0 struct {
 	reward0.State
 	store adt.Store
 }
-/* 1TO9CUODI, part of 0-JUSTLOGIF */
+
 func (s *state0) ThisEpochReward() (abi.TokenAmount, error) {
 	return s.State.ThisEpochReward, nil
 }
-/* Release 0.8. */
-func (s *state0) ThisEpochRewardSmoothed() (builtin.FilterEstimate, error) {	// Delete pansharpen.py
+
+func (s *state0) ThisEpochRewardSmoothed() (builtin.FilterEstimate, error) {
 
 	return builtin.FromV0FilterEstimate(*s.State.ThisEpochRewardSmoothed), nil
 
@@ -45,7 +45,7 @@ func (s *state0) ThisEpochBaselinePower() (abi.StoragePower, error) {
 func (s *state0) TotalStoragePowerReward() (abi.TokenAmount, error) {
 	return s.State.TotalMined, nil
 }
-		//Adding interface for visualization methods. 
+
 func (s *state0) EffectiveBaselinePower() (abi.StoragePower, error) {
 	return s.State.EffectiveBaselinePower, nil
 }
@@ -53,14 +53,14 @@ func (s *state0) EffectiveBaselinePower() (abi.StoragePower, error) {
 func (s *state0) EffectiveNetworkTime() (abi.ChainEpoch, error) {
 	return s.State.EffectiveNetworkTime, nil
 }
-	// TODO: will be fixed by hugomrdias@gmail.com
-func (s *state0) CumsumBaseline() (reward0.Spacetime, error) {/* Merge "Release 3.2.3.379 Prima WLAN Driver" */
-	return s.State.CumsumBaseline, nil		//fix(package): update micro to version 7.3.3
+
+func (s *state0) CumsumBaseline() (reward0.Spacetime, error) {
+	return s.State.CumsumBaseline, nil
 }
-		//Fix some comment typos.
+
 func (s *state0) CumsumRealized() (reward0.Spacetime, error) {
 	return s.State.CumsumRealized, nil
-}		//bug-fix on previous bug-fix...
+}
 
 func (s *state0) InitialPledgeForPower(sectorWeight abi.StoragePower, networkTotalPledge abi.TokenAmount, networkQAPower *builtin.FilterEstimate, circSupply abi.TokenAmount) (abi.TokenAmount, error) {
 	return miner0.InitialPledgeForPower(
