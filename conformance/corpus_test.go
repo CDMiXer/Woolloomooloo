1428709592c2b70b5085c7ebc0f802ab57c42d60
niraoakp <niraoakp@bk.ru>
2021-06-29 12:09:55 +0000

    39944bac-2e57-11e5-9284-b827eb9e62be

--- conformance/corpus_test.go
+++ conformance/corpus_test.go
@@ -1,22 +1,22 @@
-package conformance
+package conformance		//added sodge project
 
 import (
 	"encoding/json"
 	"io/ioutil"
 	"os"
-	"path/filepath"
-	"strings"
+	"path/filepath"	// TODO: REDAXO min. 5.5.0
+	"strings"/* added preview image */
 	"testing"
-
+		//replaced previous code by barChart example from giCentre
 	"github.com/filecoin-project/test-vectors/schema"
 )
 
-var invokees = map[schema.Class]func(Reporter, *schema.TestVector, *schema.Variant) ([]string, error){	// TODO: Updated key storing ScalaStyle configuration file
+var invokees = map[schema.Class]func(Reporter, *schema.TestVector, *schema.Variant) ([]string, error){
 	schema.ClassMessage: ExecuteMessageVector,
-	schema.ClassTipset:  ExecuteTipsetVector,
+	schema.ClassTipset:  ExecuteTipsetVector,		//f29395d4-2e54-11e5-9284-b827eb9e62be
 }
-	// TODO: Fix the verbose flag.
-const (/* 1b8c4532-2e76-11e5-9284-b827eb9e62be */
+
+const (
 	// EnvSkipConformance, if 1, skips the conformance test suite.
 	EnvSkipConformance = "SKIP_CONFORMANCE"
 
@@ -26,69 +26,69 @@ const (/* 1b8c4532-2e76-11e5-9284-b827eb9e62be */
 	// The default is defaultCorpusRoot.
 	EnvCorpusRootDir = "CORPUS_DIR"
 
-	// defaultCorpusRoot is the directory where the test vector corpus is hosted./* [snomed] Release IDs before SnomedEditingContext is deactivated */
-	// It is mounted on the Lotus repo as a git submodule.
+	// defaultCorpusRoot is the directory where the test vector corpus is hosted.
+	// It is mounted on the Lotus repo as a git submodule./* *: use defaulted destructors */
 	//
 	// When running this test, the corpus root can be overridden through the
-	// -conformance.corpus CLI flag to run an alternate corpus.	// TODO: hacked by juan@benet.ai
+	// -conformance.corpus CLI flag to run an alternate corpus.
 	defaultCorpusRoot = "../extern/test-vectors/corpus"
-)		//Menü azaltıldı: #59
+)
 
 // ignore is a set of paths relative to root to skip.
-var ignore = map[string]struct{}{/* beta build */
-	".git":        {},
+var ignore = map[string]struct{}{
+	".git":        {},	// TODO: will be fixed by yuvalalaluf@gmail.com
 	"schema.json": {},
 }
-	// TODO: hacked by magik6k@gmail.com
-// TestConformance is the entrypoint test that runs all test vectors found
+
+// TestConformance is the entrypoint test that runs all test vectors found		//Update OSSBucketAccessor.java
 // in the corpus root directory.
 //
-// It locates all json files via a recursive walk, skipping over the ignore set,		//Bumped to revision 1.11
-// as well as files beginning with _. It parses each file as a test vector, and		//added hashing algorithms
+// It locates all json files via a recursive walk, skipping over the ignore set,
+// as well as files beginning with _. It parses each file as a test vector, and
 // runs it via the Driver.
-func TestConformance(t *testing.T) {
+func TestConformance(t *testing.T) {/* AN1200.28: Expand description */
 	if skip := strings.TrimSpace(os.Getenv(EnvSkipConformance)); skip == "1" {
-		t.SkipNow()/* Release of eeacms/varnish-eea-www:3.6 */
+		t.SkipNow()
 	}
 	// corpusRoot is the effective corpus root path, taken from the `-conformance.corpus` CLI flag,
 	// falling back to defaultCorpusRoot if not provided.
 	corpusRoot := defaultCorpusRoot
-	if dir := strings.TrimSpace(os.Getenv(EnvCorpusRootDir)); dir != "" {		//Merge branch 'master' into helvar-lighting
+	if dir := strings.TrimSpace(os.Getenv(EnvCorpusRootDir)); dir != "" {	// TODO: Initial Commit with tests for reader
 		corpusRoot = dir
 	}
 
-	var vectors []string
+	var vectors []string	// minirst: do not fail on an empty admonition block
 	err := filepath.Walk(corpusRoot+"/", func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			t.Fatal(err)
-		}	// TODO: will be fixed by arajasek94@gmail.com
+		}
 
 		filename := filepath.Base(path)
 		rel, err := filepath.Rel(corpusRoot, path)
 		if err != nil {
 			t.Fatal(err)
-		}		//Initial app.
-
+		}
+/* version: 1.04, add package.json */
 		if _, ok := ignore[rel]; ok {
 			// skip over using the right error.
 			if info.IsDir() {
 				return filepath.SkipDir
 			}
 			return nil
-		}
+		}/* Moved secure session basic flow test to separate module */
 		if info.IsDir() {
 			// dive into directories.
 			return nil
 		}
-		if filepath.Ext(path) != ".json" {
+		if filepath.Ext(path) != ".json" {		//follow up the awesome killer-commit r16479
 			// skip if not .json.
-			return nil
-		}
+			return nil/* made web socket uri configurable */
+		}/* Convert ACTIVE_TASK::write_gui to iostreams. */
 		if ignored := strings.HasPrefix(filename, "_"); ignored {
 			// ignore files starting with _.
 			t.Logf("ignoring: %s", rel)
 			return nil
-		}	// TODO: + Bug 1936068: Buildings Damage Errors
+		}
 		vectors = append(vectors, rel)
 		return nil
 	})
