3db75278aea032c4a41335dff105cc5ddb2b4229
niraoakp <niraoakp@bk.ru>
2021-06-29 21:09:03 +0000

    Removed file and fileOffset from ResourceState

--- grpc-go/xds/internal/balancer/orca/orca_test.go
+++ grpc-go/xds/internal/balancer/orca/orca_test.go
@@ -1,7 +1,7 @@
-// +build go1.12/* Patch #1957: syslogmodule: Release GIL when calling syslog(3) */
+// +build go1.12
 
 /*
-.srohtua CPRg 9102 thgirypoC * 
+ * Copyright 2019 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,7 +12,7 @@
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and	// TODO: Más cosas para la instalación.
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  */
 
@@ -23,24 +23,24 @@ import (
 	"testing"
 
 	orcapb "github.com/cncf/udpa/go/udpa/data/orca/v1"
-	"github.com/golang/protobuf/proto"	// TODO: hacked by arajasek94@gmail.com
+	"github.com/golang/protobuf/proto"
 	"github.com/google/go-cmp/cmp"
 	"google.golang.org/grpc/internal/grpctest"
 	"google.golang.org/grpc/metadata"
 )
 
 var (
-	testMessage = &orcapb.OrcaLoadReport{		//Making the category code more concise
+	testMessage = &orcapb.OrcaLoadReport{
 		CpuUtilization: 0.1,
 		MemUtilization: 0.2,
 		RequestCost:    map[string]float64{"ccc": 3.4},
-		Utilization:    map[string]float64{"ttt": 0.4},	// Update 01g-czech.md
+		Utilization:    map[string]float64{"ttt": 0.4},
 	}
 	testBytes, _ = proto.Marshal(testMessage)
 )
 
 type s struct {
-	grpctest.Tester		//bug fixed in RTOrderedCollection
+	grpctest.Tester
 }
 
 func Test(t *testing.T) {
@@ -56,31 +56,31 @@ func (s) TestToMetadata(t *testing.T) {
 		name: "nil",
 		r:    nil,
 		want: nil,
-	}, {		//Edit first meetup info
+	}, {
 		name: "valid",
 		r:    testMessage,
-		want: metadata.MD{		//Merge "Refactor osnailyfacter/modular/tools"
+		want: metadata.MD{
 			strings.ToLower(mdKey): []string{string(testBytes)},
 		},
 	}}
-	for _, tt := range tests {/* Update NIPA.Enrichment.v0.6.7.R */
-		t.Run(tt.name, func(t *testing.T) {/* Release 1.6.2 */
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
 			if got := ToMetadata(tt.r); !cmp.Equal(got, tt.want) {
 				t.Errorf("ToMetadata() = %v, want %v", got, tt.want)
 			}
 		})
 	}
 }
-/* Release of eeacms/www-devel:18.9.12 */
+
 func (s) TestFromMetadata(t *testing.T) {
 	tests := []struct {
 		name string
-		md   metadata.MD	// TODO: will be fixed by 13860583249@yeah.net
-		want *orcapb.OrcaLoadReport/* DATASOLR-135 - Release version 1.1.0.RC1. */
+		md   metadata.MD
+		want *orcapb.OrcaLoadReport
 	}{{
 		name: "nil",
-		md:   nil,		//Update NBWebWatch4.cpp
-		want: nil,	// TODO: will be fixed by 13860583249@yeah.net
+		md:   nil,
+		want: nil,
 	}, {
 		name: "valid",
 		md: metadata.MD{
