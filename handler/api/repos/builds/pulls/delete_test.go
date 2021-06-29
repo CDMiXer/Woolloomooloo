3ad00c2266ee882c8000a6aa59681038d3ecc72a
niraoakp <niraoakp@bk.ru>
2021-06-29 06:24:21 +0000

    Add Tiffany to authors list

--- handler/api/repos/builds/pulls/delete_test.go
+++ handler/api/repos/builds/pulls/delete_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// TODO: will be fixed by nagydani@epointsystem.org
-// that can be found in the LICENSE file.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Release date updated. */
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file./* Give credit, update changes, update docs.  */
 
 package pulls
