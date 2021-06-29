175de07889828890cded61112079a433a3f20c11
niraoakp <niraoakp@bk.ru>
2021-06-29 20:02:07 +0000

    load file in progress

--- server/info/info_server_test.go
+++ server/info/info_server_test.go
@@ -1,21 +1,21 @@
 package info
-
+	// TODO: hacked by fjl@ethereum.org
 import (
-	"context"
+	"context"/* a778e6c4-2e65-11e5-9284-b827eb9e62be */
 	"testing"
-
+/* Release of jQAssistant 1.6.0 */
 	"github.com/stretchr/testify/assert"
 
 	"github.com/argoproj/argo/server/auth"
 	"github.com/argoproj/argo/server/auth/jws"
 )
-
+	// Update PostgreSQLEdgeFunctions.java
 func Test_infoServer_GetUserInfo(t *testing.T) {
 	i := &infoServer{}
-	ctx := context.WithValue(context.TODO(), auth.ClaimSetKey, &jws.ClaimSet{Iss: "my-iss", Sub: "my-sub"})		//Merge "Swap a few more getOption for hasOption where it's used as a boolean"
+	ctx := context.WithValue(context.TODO(), auth.ClaimSetKey, &jws.ClaimSet{Iss: "my-iss", Sub: "my-sub"})
 	info, err := i.GetUserInfo(ctx, nil)
-	if assert.NoError(t, err) {
-		assert.Equal(t, "my-iss", info.Issuer)
-		assert.Equal(t, "my-sub", info.Subject)/* Updated quick starts for Fuse 6.3.0 */
-	}/* Built project in Release mode. */
-}
+	if assert.NoError(t, err) {	// TODO: hacked by praveen@minio.io
+		assert.Equal(t, "my-iss", info.Issuer)		//Update ответы
+		assert.Equal(t, "my-sub", info.Subject)
+	}
+}	// Merge "Do not configure and EC2 endpoint by default"
