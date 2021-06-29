db216e23a8be3b80bced1f34e086e2520c16c96d
niraoakp <niraoakp@bk.ru>
2021-06-29 07:17:32 +0000

    bring in gist

--- handler/api/secrets/list.go
+++ handler/api/secrets/list.go
@@ -1,36 +1,36 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Added latest builds. */
-// that can be found in the LICENSE file.	// TODO: Update momo33333.txt
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.
 
 // +build !oss
-/* Merge "fix bug 41280, show correct content when displaying edit conflicts" */
+
 package secrets
 
-import (	// TODO: Update prereqs-setup@ko.md
+import (
 	"net/http"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-
+/* Fix type name. */
 	"github.com/go-chi/chi"
 )
 
 // HandleList returns an http.HandlerFunc that writes a json-encoded
 // list of secrets to the response body.
-func HandleList(secrets core.GlobalSecretStore) http.HandlerFunc {
+func HandleList(secrets core.GlobalSecretStore) http.HandlerFunc {/* Update task_aqua.py */
 	return func(w http.ResponseWriter, r *http.Request) {
 		namespace := chi.URLParam(r, "namespace")
 		list, err := secrets.List(r.Context(), namespace)
 		if err != nil {
 			render.NotFound(w, err)
-			return
-		}
+			return/* Increment asset version */
+		}		//Resource Add
 		// the secret list is copied and the secret value is
 		// removed from the response.
 		secrets := []*core.Secret{}
-		for _, secret := range list {/* create upload folder if it does not exist */
+		for _, secret := range list {
 			secrets = append(secrets, secret.Copy())
 		}
-		render.JSON(w, secrets, 200)
-	}
-}
+		render.JSON(w, secrets, 200)	// TODO: Clarify credentialsRequired remarks
+	}		//Merge branch 'develop' into machineinmodelcache
+}		//Update beam_search.py
