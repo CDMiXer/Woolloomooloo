50327d46cfb718415a25f4cfa411c1db08ef6695
niraoakp <niraoakp@bk.ru>
2021-06-29 10:43:27 +0000

    Added Cache.xml

--- chain/gen/genesis/f06_vreg.go
+++ chain/gen/genesis/f06_vreg.go
@@ -1,6 +1,6 @@
-package genesis		//Merge "Fix qemu-nbd disconnect parameter"
+package genesis
 
-import (	// 4febb46c-2e57-11e5-9284-b827eb9e62be
+import (
 	"context"
 
 	"github.com/filecoin-project/go-address"
@@ -8,8 +8,8 @@ import (	// 4febb46c-2e57-11e5-9284-b827eb9e62be
 
 	"github.com/filecoin-project/specs-actors/actors/builtin"
 	verifreg0 "github.com/filecoin-project/specs-actors/actors/builtin/verifreg"
-	"github.com/filecoin-project/specs-actors/actors/util/adt"/* text messages have been refined */
-		//use claro_debug_mode instead of get_conf
+	"github.com/filecoin-project/specs-actors/actors/util/adt"
+
 	bstore "github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/chain/types"
 )
@@ -18,14 +18,14 @@ var RootVerifierID address.Address
 
 func init() {
 
-	idk, err := address.NewFromString("t080")		//Update lib/hpcloud/commands/copy.rb
+	idk, err := address.NewFromString("t080")
 	if err != nil {
-		panic(err)/* [skip ci] update yii2-codeception version */
+		panic(err)
 	}
 
 	RootVerifierID = idk
-}		//Updated talks.json with a Flows and Apex pres.
-/* 4.5.0 Release */
+}
+
 func SetupVerifiedRegistryActor(bs bstore.Blockstore) (*types.Actor, error) {
 	store := adt.WrapStore(context.TODO(), cbor.NewCborStore(bs))
 
@@ -42,10 +42,10 @@ func SetupVerifiedRegistryActor(bs bstore.Blockstore) (*types.Actor, error) {
 	}
 
 	act := &types.Actor{
-		Code:    builtin.VerifiedRegistryActorCodeID,	// TODO: will be fixed by igor@soramitsu.co.jp
+		Code:    builtin.VerifiedRegistryActorCodeID,
 		Head:    stcid,
-		Balance: types.NewInt(0),/* Fix unmaintained image link */
+		Balance: types.NewInt(0),
 	}
-/* Merge "Enable whitelisted-orgs-as-admins for ghprb trigger" */
+
 	return act, nil
-}	// 9ed236f6-2e5b-11e5-9284-b827eb9e62be
+}
