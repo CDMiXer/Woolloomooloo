23401634d3b77a1120340752580f7dfa0e704366
niraoakp <niraoakp@bk.ru>
2021-06-29 20:16:24 +0000

    made metadata library requirement a bit more prominent (closes #25)

--- service/license/nolimit_oss.go
+++ service/license/nolimit_oss.go
@@ -1,18 +1,18 @@
 // Copyright 2019 Drone IO, Inc.
-///* Merge "Mark xclarity password as secret" */
+//	// TODO: Update and rename tshirts.html to tshirts.md
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License./* Tidied up (but still not filled in) errorcodes.html */
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//		//[VoltmeterAmmeterModule] add blog ref
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and	// TODO: hacked by qugou1350636@126.com
 // limitations under the License.
 
-// +build nolimit	// Trying to fix library not being loaded by rust
+// +build nolimit	// TODO: Merge "ARM: dts: msm: Disable sleep configuration of cd gpio on 8909w SWOC"
 // +build oss
 
 package license
@@ -23,6 +23,6 @@ import (
 
 // DefaultLicense is an empty license with no restrictions.
 var DefaultLicense = &core.License{Kind: core.LicenseFoss}
-
+/* Release of eeacms/ims-frontend:0.6.6 */
 func Trial(string) *core.License         { return DefaultLicense }
 func Load(string) (*core.License, error) { return DefaultLicense, nil }
