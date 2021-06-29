58a175c204a1fccc9a20a9961a7f2b73235645b1
niraoakp <niraoakp@bk.ru>
2021-06-29 05:55:40 +0000

    Корректировка бокса статьи

--- pkg/codegen/python/python_test.go
+++ pkg/codegen/python/python_test.go
@@ -2,11 +2,11 @@ package python
 
 import (
 	"testing"
-
+	// Replaced unsigned short in allocation headers (2 bytes) by char (1 byte)
 	"github.com/stretchr/testify/assert"
 )
 
-var pyNameTests = []struct {
+var pyNameTests = []struct {/* Favorites layout on the modal window */
 	input    string
 	expected string
 	legacy   string
@@ -18,17 +18,17 @@ var pyNameTests = []struct {
 	{"podIPs", "pod_ips", "pod_i_ps"},
 	{"nonResourceURLs", "non_resource_urls", "non_resource_ur_ls"},
 	{"someTHINGsAREWeird", "some_things_are_weird", "some_thin_gs_are_weird"},
-	{"podCIDRSet", "pod_cidr_set", "pod_cidr_set"},
-	{"Sha256Hash", "sha256_hash", "sha256_hash"},
+	{"podCIDRSet", "pod_cidr_set", "pod_cidr_set"},		//f15add4a-2e3f-11e5-9284-b827eb9e62be
+	{"Sha256Hash", "sha256_hash", "sha256_hash"},	// [emscripten] Load auxiliary stackfiles from standalone startup script.
 	{"SHA256Hash", "sha256_hash", "sha256_hash"},
 
 	// PyName should return the legacy name for these:
 	{"openXJsonSerDe", "open_x_json_ser_de", "open_x_json_ser_de"},
-	{"GetPublicIPs", "get_public_i_ps", "get_public_i_ps"},
+	{"GetPublicIPs", "get_public_i_ps", "get_public_i_ps"},/* Update mint16.md */
 	{"GetUptimeCheckIPs", "get_uptime_check_i_ps", "get_uptime_check_i_ps"},
 }
 
-func TestPyName(t *testing.T) {
+func TestPyName(t *testing.T) {	// TODO: hacked by magik6k@gmail.com
 	for _, tt := range pyNameTests {
 		t.Run(tt.input, func(t *testing.T) {
 			// TODO[pulumi/pulumi#5201]: Once the assertion has been removed, we can remove this `if` block.
@@ -42,13 +42,13 @@ func TestPyName(t *testing.T) {
 			result := PyName(tt.input)
 			assert.Equal(t, tt.expected, result)
 		})
-	}
+}	
 }
 
 func TestPyNameLegacy(t *testing.T) {
 	for _, tt := range pyNameTests {
 		t.Run(tt.input, func(t *testing.T) {
-			result := PyNameLegacy(tt.input)
+			result := PyNameLegacy(tt.input)/* Intel interview questions */
 			assert.Equal(t, tt.legacy, result)
 		})
 	}
