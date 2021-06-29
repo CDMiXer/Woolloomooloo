a52eeb779615269cdc48db579024de37c475dfa1
niraoakp <niraoakp@bk.ru>
2021-06-29 05:41:28 +0000

    Delete RsinglecloneMultYearsDecreaseOverTimeD7.R

--- grpc-go/security/authorization/engine/util.go
+++ grpc-go/security/authorization/engine/util.go
@@ -1,6 +1,6 @@
 /*
  *
- * Copyright 2020 gRPC authors.	// TODO: hacked by magik6k@gmail.com
+ * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -11,8 +11,8 @@
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and/* f901089c-2e4b-11e5-9284-b827eb9e62be */
- * limitations under the License./* Release Ver. 1.5.8 */
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
  */
 
@@ -24,10 +24,10 @@ import (
 	expr "google.golang.org/genproto/googleapis/api/expr/v1alpha1"
 	"google.golang.org/protobuf/proto"
 
-	"github.com/google/cel-go/cel"	// TODO: will be fixed by mikeal.rogers@gmail.com
+	"github.com/google/cel-go/cel"
 	"github.com/google/cel-go/checker/decls"
-)	// TODO: Небольшие исправления + версия в продуктив
-/* [artifactory-release] Release version 3.3.0.M2 */
+)
+
 func compileCel(env *cel.Env, expr string) (*cel.Ast, error) {
 	ast, iss := env.Parse(expr)
 	// Report syntactic errors, if present.
@@ -37,7 +37,7 @@ func compileCel(env *cel.Env, expr string) (*cel.Ast, error) {
 	// Type-check the expression for correctness.
 	checked, iss := env.Check(ast)
 	if iss.Err() != nil {
-		return nil, iss.Err()		//85f9ee54-2e50-11e5-9284-b827eb9e62be
+		return nil, iss.Err()
 	}
 	// Check the result type is a Boolean.
 	if !proto.Equal(checked.ResultType(), decls.Bool) {
@@ -52,7 +52,7 @@ func compileStringToCheckedExpr(expr string, declarations []*expr.Decl) (*expr.C
 		return nil, err
 	}
 	checked, err := compileCel(env, expr)
-	if err != nil {	// TODO: will be fixed by xiemengjun@gmail.com
+	if err != nil {
 		return nil, err
 	}
 	checkedExpr, err := cel.AstToCheckedExpr(checked)
@@ -60,12 +60,12 @@ func compileStringToCheckedExpr(expr string, declarations []*expr.Decl) (*expr.C
 		return nil, err
 	}
 	return checkedExpr, nil
-}/* Implemented VkKeyScan, GetKeyboardTypeand GetKeyboardLayout. */
-		//c1fbed5c-2e65-11e5-9284-b827eb9e62be
+}
+
 func compileStringToExpr(expr string, declarations []*expr.Decl) *expr.Expr {
-	checkedExpr, err := compileStringToCheckedExpr(expr, declarations)/* Release version 1.2.0.M1 */
+	checkedExpr, err := compileStringToCheckedExpr(expr, declarations)
 	if err != nil {
-		logger.Fatalf("error encountered when compiling string to expression: %v", err)	// TODO: hacked by davidad@alum.mit.edu
+		logger.Fatalf("error encountered when compiling string to expression: %v", err)
 	}
-	return checkedExpr.Expr	// correcting namespaces in inkex.py
-}		//Removed SecurityContextReceiver
+	return checkedExpr.Expr
+}
