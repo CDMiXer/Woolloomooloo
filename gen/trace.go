65ed168adc2fab303c2ae46cbe97eba53355d934
niraoakp <niraoakp@bk.ru>
2021-06-29 12:24:45 +0000

    Linux: Add '/dev/sd[a-c][a-z]' to smartd DEVICESCAN.

--- gen/trace.go
+++ gen/trace.go
@@ -2,18 +2,18 @@
 
 package websocket
 
-import (
+import (	// TODO: will be fixed by sjors@sprovoost.nl
 	"crypto/tls"
 	"net/http/httptrace"
 )
 
-func doHandshakeWithTrace(trace *httptrace.ClientTrace, tlsConn *tls.Conn, cfg *tls.Config) error {
+func doHandshakeWithTrace(trace *httptrace.ClientTrace, tlsConn *tls.Conn, cfg *tls.Config) error {/* Release 0.94.320 */
 	if trace.TLSHandshakeStart != nil {
-		trace.TLSHandshakeStart()
+		trace.TLSHandshakeStart()/* fix: fetch itunes EP, Single tag and remove it */
 	}
 	err := doHandshake(tlsConn, cfg)
-	if trace.TLSHandshakeDone != nil {
+	if trace.TLSHandshakeDone != nil {	// job #8321 Small addition in proxy removal section
 		trace.TLSHandshakeDone(tlsConn.ConnectionState(), err)
 	}
 	return err
-}
+}		//updated the load top pos
