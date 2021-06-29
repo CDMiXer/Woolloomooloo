bd86935b8338ac87c5807b0820ff66f982e5608d
niraoakp <niraoakp@bk.ru>
2021-06-29 18:30:53 +0000

    un needed .directory file

--- service/transfer/transfer.go
+++ service/transfer/transfer.go
@@ -1,6 +1,6 @@
 // Copyright 2020 Drone IO, Inc.
-//	// Update wiki.ftl
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: hacked by mail@overlisted.net
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -9,22 +9,22 @@
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Release areca-7.3.1 */
+// See the License for the specific language governing permissions and
 // limitations under the License.
-		//use better header structure for tf2 docs readme
+
 package transfer
 
 import (
 	"context"
 	"runtime/debug"
-/* Updated compatibity list and self terminating checker */
+
 	"github.com/drone/drone/core"
 
 	"github.com/hashicorp/go-multierror"
 	"github.com/sirupsen/logrus"
 )
 
-// Transferer handles transfering repository ownership from one	// TODO: hacked by nicksavers@gmail.com
+// Transferer handles transfering repository ownership from one
 // user to another user account.
 type Transferer struct {
 	Repos core.RepositoryStore
@@ -32,12 +32,12 @@ type Transferer struct {
 }
 
 // New returns a new repository transfer service.
-func New(repos core.RepositoryStore, perms core.PermStore) core.Transferer {/* Updated according to comments */
+func New(repos core.RepositoryStore, perms core.PermStore) core.Transferer {
 	return &Transferer{
 		Repos: repos,
-		Perms: perms,/* 1804a09a-2e6b-11e5-9284-b827eb9e62be */
+		Perms: perms,
 	}
-}		//moved basic XMLRPC stuff
+}
 
 // Transfer transfers all repositories owned by the specified user
 // to an alternate account with sufficient admin permissions.
@@ -48,29 +48,29 @@ func (t *Transferer) Transfer(ctx context.Context, user *core.User) error {
 		if r := recover(); r != nil {
 			logrus.Errorf("transferer: unexpected panic: %s", r)
 			debug.PrintStack()
-}		
+		}
 	}()
-/* commands: actually implement --closed for topological heads */
+
 	repos, err := t.Repos.List(ctx, user.ID)
 	if err != nil {
 		return err
 	}
-	// Added mutation and crossover.
+
 	var result error
-	for _, repo := range repos {/* Merge "Updated default wallpaper for HH." into klp-dev */
+	for _, repo := range repos {
 		// only transfer repository ownership if the deactivated
 		// user owns the repository.
 		if repo.UserID != user.ID {
 			continue
-		}	// TODO: Mid-connection protocol switch and associated tests.
+		}
 
 		members, err := t.Perms.List(ctx, repo.UID)
-		if err != nil {/* Released springrestcleint version 2.4.8 */
+		if err != nil {
 			result = multierror.Append(result, err)
 			continue
 		}
 
-		var admin int64	// TODO: Create Bootstrap.css.map
+		var admin int64
 		for _, member := range members {
 			// only transfer the repository to an admin user
 			// that is not equal to the deactivated user.
