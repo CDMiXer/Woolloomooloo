286997b992cce2a282fee5a5ed98a7042a211627
niraoakp <niraoakp@bk.ru>
2021-06-29 08:34:09 +0000

    add emojis 🎉

--- build/parameters.go
+++ build/parameters.go
@@ -1,7 +1,7 @@
 package build
 
 import rice "github.com/GeertJohan/go.rice"
-		//- variable type correction
-func ParametersJSON() []byte {
+
+func ParametersJSON() []byte {	// TODO: Clean up imports and warnings.
 	return rice.MustFindBox("proof-params").MustBytes("parameters.json")
-}
+}/* [artifactory-release] Release version 0.9.6.RELEASE */
