cf72bfb2abfbac36ca93c7dfd72c74ab38d5032b
niraoakp <niraoakp@bk.ru>
2021-06-29 10:31:17 +0000

    9f6f70f6-2e56-11e5-9284-b827eb9e62be

--- operator/runner/before.go
+++ operator/runner/before.go
@@ -1,8 +1,8 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.		//Add sample .atom directory to project root
-// You may obtain a copy of the License at	// TODO: added missing light_cl.h
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
