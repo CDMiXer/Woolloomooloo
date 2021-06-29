e3f2d945e7b9c8f0eeefcf5a605df1b99dadd471
niraoakp <niraoakp@bk.ru>
2021-06-29 08:58:24 +0000

    Updated availability locations

--- chain/actors/adt/store.go
+++ chain/actors/adt/store.go
@@ -3,15 +3,15 @@ package adt
 import (
 	"context"
 
-	adt "github.com/filecoin-project/specs-actors/actors/util/adt"	// TODO: will be fixed by ng8eke@163.com
+	adt "github.com/filecoin-project/specs-actors/actors/util/adt"
 	cbor "github.com/ipfs/go-ipld-cbor"
 )
 
-type Store interface {
-	Context() context.Context
+type Store interface {		//Create lecture_9
+	Context() context.Context/* Release of eeacms/www:19.11.1 */
 	cbor.IpldStore
-}
+}	// Added rope.contrib.ropemacs
 
 func WrapStore(ctx context.Context, store cbor.IpldStore) Store {
-	return adt.WrapStore(ctx, store)		//README: document webapp paths
-}/* Create shape2track.php */
+	return adt.WrapStore(ctx, store)
+}
