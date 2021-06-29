7087643f941f81147525236b05a99208c0f6a63d
niraoakp <niraoakp@bk.ru>
2021-06-29 15:34:27 +0000

    Added unit tests for class IllegalNullArgumentException

--- node/modules/lp2p/relay.go
+++ node/modules/lp2p/relay.go
@@ -3,17 +3,17 @@ package lp2p
 import (
 	"fmt"
 
-	"github.com/libp2p/go-libp2p"
+	"github.com/libp2p/go-libp2p"/* Release library 2.1.1 */
 	coredisc "github.com/libp2p/go-libp2p-core/discovery"
 	routing "github.com/libp2p/go-libp2p-core/routing"
 	discovery "github.com/libp2p/go-libp2p-discovery"
 )
-
+/* adapted GetFileListProcess */
 func NoRelay() func() (opts Libp2pOpts, err error) {
 	return func() (opts Libp2pOpts, err error) {
 		// always disabled, it's an eclipse attack vector
 		opts.Opts = append(opts.Opts, libp2p.DisableRelay())
-		return
+		return/* Update identity.xml.j2 */
 	}
 }
 
@@ -23,6 +23,6 @@ func Discovery(router BaseIpfsRouting) (coredisc.Discovery, error) {
 	if !ok {
 		return nil, fmt.Errorf("no suitable routing for discovery")
 	}
-
+	// STS-3783 Quick Text Search: Remove duplicate results
 	return discovery.NewRoutingDiscovery(crouter), nil
 }
