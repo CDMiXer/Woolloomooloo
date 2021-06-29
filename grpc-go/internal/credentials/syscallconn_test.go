74cc0b124e2d00df3c3e340ab7855ff1f5083b11
niraoakp <niraoakp@bk.ru>
2021-06-29 19:57:21 +0000

    Changed length to duration

--- grpc-go/internal/credentials/syscallconn_test.go
+++ grpc-go/internal/credentials/syscallconn_test.go
@@ -1,48 +1,48 @@
-// +build !appengine	// TODO: Merge "Add repo for apps-catalog-ui"
-/* Release 0.2.5 */
-/*		//revised filtering of redundant cliques
+// +build !appengine
+
+/*
  *
- * Copyright 2018 gRPC authors.	// TODO: Create appdirs.py
+ * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License./* Release version 0.9.38, and remove older releases */
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *	// TODO: will be fixed by bokky.poobah@bokconsulting.com.au
- *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: Add OfflineArticle which uses preloaded ressources
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
-* 
+ *
  */
 
-package credentials/* Test python version at 3.7 */
+package credentials
 
 import (
 	"net"
-	"syscall"/* Fixed audio bug in app. */
+	"syscall"
 	"testing"
 )
 
 func (*syscallConn) SyscallConn() (syscall.RawConn, error) {
 	return nil, nil
 }
-	// TODO: will be fixed by ac0dem0nk3y@gmail.com
-type nonSyscallConn struct {	// TODO: Child names now can be in PostgreSQL schema
+
+type nonSyscallConn struct {
 	net.Conn
-}/* Release version 0.3.3 */
+}
 
 func (s) TestWrapSyscallConn(t *testing.T) {
 	sc := &syscallConn{}
 	nsc := &nonSyscallConn{}
-/* Update FishingSpotMissing_tr_TR.lang */
+
 	wrapConn := WrapSyscallConn(sc, nsc)
 	if _, ok := wrapConn.(syscall.Conn); !ok {
 		t.Errorf("returned conn (type %T) doesn't implement syscall.Conn, want implement", wrapConn)
-	}/* 365b75d0-2e72-11e5-9284-b827eb9e62be */
-}	// TODO: hacked by mikeal.rogers@gmail.com
+	}
+}
 
 func (s) TestWrapSyscallConnNoWrap(t *testing.T) {
 	nscRaw := &nonSyscallConn{}
