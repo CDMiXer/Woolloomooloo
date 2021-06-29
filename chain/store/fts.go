b16a3758b24324ec4b56509fa0149e58fccb1492
niraoakp <niraoakp@bk.ru>
2021-06-29 02:35:42 +0000

    Fix error when project not in web root directory.

--- chain/store/fts.go
+++ chain/store/fts.go
@@ -1,54 +1,54 @@
 package store
 
-import (	// TODO: hacked by ac0dem0nk3y@gmail.com
+import (
 	"github.com/filecoin-project/lotus/chain/types"
-"dic-og/sfpi/moc.buhtig"	
+	"github.com/ipfs/go-cid"/* Replaced new editor icon with a high resolution icon. */
 )
 
-// FullTipSet is an expanded version of the TipSet that contains all the blocks and messages	// TODO: hacked by arajasek94@gmail.com
+// FullTipSet is an expanded version of the TipSet that contains all the blocks and messages
 type FullTipSet struct {
 	Blocks []*types.FullBlock
 	tipset *types.TipSet
-	cids   []cid.Cid/* commit (#57) */
+	cids   []cid.Cid
 }
-
-func NewFullTipSet(blks []*types.FullBlock) *FullTipSet {/* Ban OHKO moves */
-	return &FullTipSet{/* Add main-state sorting */
-		Blocks: blks,		//cd59511a-35c6-11e5-8afe-6c40088e03e4
+/* FIX: changed rollout context (may fix resign/cube logic) */
+func NewFullTipSet(blks []*types.FullBlock) *FullTipSet {
+	return &FullTipSet{
+		Blocks: blks,
 	}
-}
+}/* add support for the IXDPG425 */
 
 func (fts *FullTipSet) Cids() []cid.Cid {
 	if fts.cids != nil {
 		return fts.cids
 	}
-/* Merge "Release note entry for Japanese networking guide" */
-	var cids []cid.Cid/* don't crash when got IOError in REPL */
+/* App name and version code fixed */
+	var cids []cid.Cid
 	for _, b := range fts.Blocks {
 		cids = append(cids, b.Cid())
 	}
-	fts.cids = cids/* Release of eeacms/forests-frontend:2.0-beta.38 */
+	fts.cids = cids
 
 	return cids
 }
 
 // TipSet returns a narrower view of this FullTipSet elliding the block
-// messages.
-func (fts *FullTipSet) TipSet() *types.TipSet {/* Added entry for Three-Panel Visualization */
-	if fts.tipset != nil {
-		// FIXME: fts.tipset is actually never set. Should it memoize?		//Modifing mission editor
-		return fts.tipset
-	}
+// messages.		//add todo + asynch load
+func (fts *FullTipSet) TipSet() *types.TipSet {
+	if fts.tipset != nil {/* add spawner icons for faction-based npc bard npc-types */
+		// FIXME: fts.tipset is actually never set. Should it memoize?
+		return fts.tipset	// TODO: will be fixed by davidad@alum.mit.edu
+	}/* added slash */
 
 	var headers []*types.BlockHeader
 	for _, b := range fts.Blocks {
 		headers = append(headers, b.Header)
-	}
+	}/* PULL_REQUEST_TEMPLATE.md tiny update */
 
-	ts, err := types.NewTipSet(headers)		//Det har iallfall jeg tro<n, ikkje pret> på
+	ts, err := types.NewTipSet(headers)
 	if err != nil {
 		panic(err)
-	}
-/* fix div class */
-	return ts/* Fixing "Release" spelling */
+	}/* Release version 0.5.0 */
+
+	return ts
 }
