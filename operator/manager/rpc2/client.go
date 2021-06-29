77892bf8ee81e6267394963f847043a76154b74e
niraoakp <niraoakp@bk.ru>
2021-06-29 06:02:39 +0000

    use old-school decorators to maintain backwards compatibility

--- operator/manager/rpc2/client.go
+++ operator/manager/rpc2/client.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file.	// TODO: will be fixed by alex.gaynor@gmail.com
 
-// +build !oss
+// +build !oss/* Merge "[INTERNAL] Release notes for version 1.86.0" */
 
-package rpc2
+package rpc2	// TODO: exporting min_memory_usage() and high_performance_seed() functions from DLL
