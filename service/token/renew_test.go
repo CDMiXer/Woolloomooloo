1a86b13dd3334b09e4bc25c61536e420416416df
niraoakp <niraoakp@bk.ru>
2021-06-29 09:10:46 +0000

    Changes in pom

--- service/token/renew_test.go
+++ service/token/renew_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// Se agregó control de altura y scroll al modal de solicitud en un pedido
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
-/* (John Arbash Meinel) Release 0.12rc1 */
-package token/* minor to timestep estimate to allow ALL test cases to pass (broken by r2831) */
+
+package token
