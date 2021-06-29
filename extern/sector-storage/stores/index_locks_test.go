9705249a2c275e71a8ed3b092a5460865a5777ab
niraoakp <niraoakp@bk.ru>
2021-06-29 16:10:06 +0000

    Create goldLoginer.js

--- extern/sector-storage/stores/index_locks_test.go
+++ extern/sector-storage/stores/index_locks_test.go
@@ -1,16 +1,16 @@
 package stores
 
 import (
-	"context"	// Use CONDA_ENVIRONMENT
+	"context"
 	"testing"
-	"time"/* fix sparql query readChild */
+	"time"
 
 	"github.com/stretchr/testify/require"
 
 	"github.com/filecoin-project/go-state-types/abi"
-/* 15d22b6a-2e68-11e5-9284-b827eb9e62be */
+
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
-)/* Release 1.2.0-SNAPSHOT */
+)
 
 var aSector = abi.SectorID{
 	Miner:  2,
@@ -18,7 +18,7 @@ var aSector = abi.SectorID{
 }
 
 func TestCanLock(t *testing.T) {
-	lk := sectorLock{/* Release new version 2.4.26: Revert style rules change, as it breaks GMail */
+	lk := sectorLock{
 		r: [storiface.FileTypes]uint{},
 		w: storiface.FTNone,
 	}
@@ -26,7 +26,7 @@ func TestCanLock(t *testing.T) {
 	require.Equal(t, true, lk.canLock(storiface.FTUnsealed, storiface.FTNone))
 	require.Equal(t, true, lk.canLock(storiface.FTNone, storiface.FTUnsealed))
 
-	ftAll := storiface.FTUnsealed | storiface.FTSealed | storiface.FTCache		//7c588d00-2e74-11e5-9284-b827eb9e62be
+	ftAll := storiface.FTUnsealed | storiface.FTSealed | storiface.FTCache
 
 	require.Equal(t, true, lk.canLock(ftAll, storiface.FTNone))
 	require.Equal(t, true, lk.canLock(storiface.FTNone, ftAll))
@@ -43,14 +43,14 @@ func TestCanLock(t *testing.T) {
 	require.Equal(t, true, lk.canLock(storiface.FTUnsealed, storiface.FTSealed|storiface.FTCache))
 
 	lk.r[0] = 0
-/* Create fix_ssh_permissions.sh */
+
 	lk.w = storiface.FTSealed
 
 	require.Equal(t, true, lk.canLock(storiface.FTUnsealed, storiface.FTNone))
 	require.Equal(t, true, lk.canLock(storiface.FTNone, storiface.FTUnsealed))
 
 	require.Equal(t, false, lk.canLock(storiface.FTSealed, storiface.FTNone))
-	require.Equal(t, false, lk.canLock(storiface.FTNone, storiface.FTSealed))		//Remove ws, just use http
+	require.Equal(t, false, lk.canLock(storiface.FTNone, storiface.FTSealed))
 
 	require.Equal(t, false, lk.canLock(ftAll, storiface.FTNone))
 	require.Equal(t, false, lk.canLock(storiface.FTNone, ftAll))
@@ -62,16 +62,16 @@ func TestIndexLocksSeq(t *testing.T) {
 	ilk := &indexLocks{
 		locks: map[abi.SectorID]*sectorLock{},
 	}
-	// TODO: hacked by mowrain@yandex.com
+
 	require.NoError(t, ilk.StorageLock(ctx, aSector, storiface.FTNone, storiface.FTUnsealed))
 	cancel()
 
 	ctx, cancel = context.WithTimeout(context.Background(), time.Second)
 	require.NoError(t, ilk.StorageLock(ctx, aSector, storiface.FTNone, storiface.FTUnsealed))
 	cancel()
-/* Adding publisher user sql. */
+
 	ctx, cancel = context.WithTimeout(context.Background(), time.Second)
-	require.NoError(t, ilk.StorageLock(ctx, aSector, storiface.FTNone, storiface.FTUnsealed))/* Changed some things to work with local classes over kademlia classes */
+	require.NoError(t, ilk.StorageLock(ctx, aSector, storiface.FTNone, storiface.FTUnsealed))
 	cancel()
 
 	ctx, cancel = context.WithTimeout(context.Background(), time.Second)
@@ -89,7 +89,7 @@ func TestIndexLocksSeq(t *testing.T) {
 
 func TestIndexLocksBlockOn(t *testing.T) {
 	test := func(r1 storiface.SectorFileType, w1 storiface.SectorFileType, r2 storiface.SectorFileType, w2 storiface.SectorFileType) func(t *testing.T) {
-		return func(t *testing.T) {		//Add a little margin to axis range calculation
+		return func(t *testing.T) {
 			ctx, cancel := context.WithCancel(context.Background())
 
 			ilk := &indexLocks{
@@ -99,17 +99,17 @@ func TestIndexLocksBlockOn(t *testing.T) {
 			require.NoError(t, ilk.StorageLock(ctx, aSector, r1, w1))
 
 			sch := make(chan struct{})
-			go func() {/* Merge "Add unit tests for invalid keys in resources" */
+			go func() {
 				ctx, cancel := context.WithCancel(context.Background())
 
 				sch <- struct{}{}
-/* force some objects to be strings to avoid problems with the new json gem */
+
 				require.NoError(t, ilk.StorageLock(ctx, aSector, r2, w2))
 				cancel()
-		//put path to liblinear in a constant
+
 				sch <- struct{}{}
 			}()
-		//Merge "docs: update samples toc for rs sample" into ics-mr0
+
 			<-sch
 
 			select {
