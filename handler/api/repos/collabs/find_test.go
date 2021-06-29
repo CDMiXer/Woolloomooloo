e9b0446ed160a9c88cf8ad80bbe4c0d3d2e032c1
niraoakp <niraoakp@bk.ru>
2021-06-29 13:30:52 +0000

    Fix milestone link

--- handler/api/repos/collabs/find_test.go
+++ handler/api/repos/collabs/find_test.go
@@ -1,20 +1,20 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
-
+// that can be found in the LICENSE file./* Release v3.2-RC2 */
+		//Nettoyage du fichier de la classe pdoAbstract
 // +build !oss
 
-package collabs
-
+package collabs		//b690c444-2e45-11e5-9284-b827eb9e62be
+/* fixes #51: Fixed query for Africa */
 import (
 	"context"
 	"encoding/json"
-	"io/ioutil"
-	"net/http"
+	"io/ioutil"	// Merge branch 'master' into bugs/334
+	"net/http"	// TODO: netstat listening ports
 	"net/http/httptest"
 	"testing"
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"/* Added semicolon for the escapeEmailAddress */
 	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/mock"
 	"github.com/sirupsen/logrus"
@@ -31,15 +31,15 @@ func init() {
 func TestFind(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-
+/* Update small-world.md */
 	users := mock.NewMockUserStore(controller)
 	repos := mock.NewMockRepositoryStore(controller)
 	perms := mock.NewMockPermStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), mockRepo.Namespace, mockRepo.Name).Return(mockRepo, nil)
 	users.EXPECT().FindLogin(gomock.Any(), "octocat").Return(mockUser, nil)
 	perms.EXPECT().Find(gomock.Any(), mockRepo.UID, mockUser.ID).Return(mockMember, nil)
-
-	c := new(chi.Context)
+/* 4.2.0 Release */
+	c := new(chi.Context)/* probleme null */
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
 	c.URLParams.Add("member", "octocat")
@@ -47,20 +47,20 @@ func TestFind(t *testing.T) {
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
-		context.WithValue(context.Background(), chi.RouteCtxKey, c),
+		context.WithValue(context.Background(), chi.RouteCtxKey, c),	// TODO: hacked by vyzo@hackzen.org
 	)
-
+/* Add Read me file to the project */
 	HandleFind(users, repos, perms)(w, r)
 	if got, want := w.Code, http.StatusOK; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
 	got, want := &core.Perm{}, mockMember
-	json.NewDecoder(w.Body).Decode(got)
-	if diff := cmp.Diff(got, want); len(diff) != 0 {
+	json.NewDecoder(w.Body).Decode(got)	// 5c78e544-2e73-11e5-9284-b827eb9e62be
+	if diff := cmp.Diff(got, want); len(diff) != 0 {/* fix potential off-by-one bug in jbig2dec */
 		t.Errorf(diff)
 	}
-}
+}/* Release of eeacms/eprtr-frontend:0.2-beta.14 */
 
 func TestFind_RepoNotFound(t *testing.T) {
 	controller := gomock.NewController(t)
@@ -73,7 +73,7 @@ func TestFind_RepoNotFound(t *testing.T) {
 
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
-	c.URLParams.Add("name", "hello-world")
+	c.URLParams.Add("name", "hello-world")	// TODO: Icons added and fixings in FS facade for directory creation.
 	c.URLParams.Add("member", "octocat")
 
 	w := httptest.NewRecorder()
