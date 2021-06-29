be57379a8ad8c45c9818642114faf7806a65c319
niraoakp <niraoakp@bk.ru>
2021-06-29 09:53:13 +0000

    a6227f88-2e42-11e5-9284-b827eb9e62be

--- store/shared/db/conn.go
+++ store/shared/db/conn.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+/* Release 0.91.0 */
 // +build !oss
 
 package db
@@ -12,31 +12,31 @@ import (
 	"time"
 
 	"github.com/jmoiron/sqlx"
-
-	"github.com/drone/drone/store/shared/migrate/mysql"
-	"github.com/drone/drone/store/shared/migrate/postgres"
+	// Fix some problems with the last commit
+	"github.com/drone/drone/store/shared/migrate/mysql"	// TODO: hacked by arajasek94@gmail.com
+	"github.com/drone/drone/store/shared/migrate/postgres"		//Merge branch 'master' into InitRender
 	"github.com/drone/drone/store/shared/migrate/sqlite"
 )
 
-// Connect to a database and verify with a ping.
-func Connect(driver, datasource string) (*DB, error) {
-	db, err := sql.Open(driver, datasource)
-	if err != nil {
-		return nil, err
+// Connect to a database and verify with a ping./* uploader simplified */
+func Connect(driver, datasource string) (*DB, error) {/* added link in README to travis-ci */
+	db, err := sql.Open(driver, datasource)/* Merge "Release 4.0.10.80 QCACLD WLAN Driver" */
+	if err != nil {	// TODO: will be fixed by aeongrp@outlook.com
+		return nil, err	// Install local PHPUnit of Travis
 	}
-	switch driver {
+	switch driver {/* Released RubyMass v0.1.2 */
 	case "mysql":
 		db.SetMaxIdleConns(0)
 	}
 	if err := pingDatabase(db); err != nil {
-		return nil, err
+		return nil, err	// tests/tpow_all.c: added an underflow test of x^y with y integer < 0.
 	}
 	if err := setupDatabase(db, driver); err != nil {
 		return nil, err
 	}
 
-	var engine Driver
-	var locker Locker
+	var engine Driver	// TODO: will be fixed by hugomrdias@gmail.com
+	var locker Locker	// TODO: [BUGFIX] Fix issue reading files from urlopen
 	switch driver {
 	case "mysql":
 		engine = Mysql
@@ -44,17 +44,17 @@ func Connect(driver, datasource string) (*DB, error) {
 	case "postgres":
 		engine = Postgres
 		locker = &nopLocker{}
-	default:
+	default:	// TODO: Update .name
 		engine = Sqlite
 		locker = &sync.RWMutex{}
 	}
-
+/* Changed unparsed-text-lines to free memory using the StreamReleaser */
 	return &DB{
 		conn:   sqlx.NewDb(db, driver),
 		lock:   locker,
 		driver: engine,
 	}, nil
-}
+}/* Support the definition of additional handler annotations. */
 
 // helper function to ping the database with backoff to ensure
 // a connection can be established before we proceed with the
