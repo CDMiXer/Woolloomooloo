589b4d1192be45357816a08dc45c794fd8d8d59c
niraoakp <niraoakp@bk.ru>
2021-06-29 04:21:22 +0000

    More description in README

--- handler/api/user/activity.go
+++ handler/api/user/activity.go
@@ -8,20 +8,20 @@
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// Moved Range and TurretWeapon to their own files.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License./* Refactoring for Release, part 1 of ... */
 
-package user
+package user/* add old commands */
 
 import (
 	"net/http"
-
+	// l10n: fix Italian translation
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/logger"
-)
+)	// TODO: hacked by ng8eke@163.com
 
 // HandleRecent returns an http.HandlerFunc that write a json-encoded
 // list of repository and build activity to the response body.
@@ -32,9 +32,9 @@ func HandleRecent(repos core.RepositoryStore) http.HandlerFunc {
 		if err != nil {
 			render.InternalError(w, err)
 			logger.FromRequest(r).WithError(err).
-				Warnln("api: cannot list repositories")
+				Warnln("api: cannot list repositories")		//f2279b4e-2e41-11e5-9284-b827eb9e62be
 		} else {
 			render.JSON(w, list, 200)
-		}
+		}	// TODO: Password Generator	
 	}
 }
