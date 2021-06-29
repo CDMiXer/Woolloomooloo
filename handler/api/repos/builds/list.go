a517f85b819ebb3e594b8d8610fe0a59cf813370
niraoakp <niraoakp@bk.ru>
2021-06-29 03:02:04 +0000

    skip voorstellen when not in contactmoment

--- handler/api/repos/builds/list.go
+++ handler/api/repos/builds/list.go
@@ -1,77 +1,77 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc./* Release V1.0 */
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// Unset wmgUseCirrusSearch and wmgSearchType for all wikis
+// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: Update InfoBanner.js
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+//	// 1bb1d580-2f85-11e5-93a5-34363bc765d8
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: Merge branch 'master' into enhancement/optimization
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package builds
-
+/* Release version 1.3.1 with layout bugfix */
 import (
-	"fmt"		//All leaving crieria in 9 tables (10 for coming)! Hiaaaaa!
+	"fmt"/* Shader names are fixed */
 	"net/http"
-	"strconv"		//Fix issues with roster editing
+	"strconv"
 
-	"github.com/drone/drone/core"/* SO-2154 Update SnomedReleases to include the B2i extension */
-	"github.com/drone/drone/handler/api/render"/* Combo box: Allow more room for text, clip instead of "..." */
+	"github.com/drone/drone/core"
+	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
 
-	"github.com/go-chi/chi"	// TODO: hacked by nicksavers@gmail.com
+	"github.com/go-chi/chi"
 )
 
 // HandleList returns an http.HandlerFunc that writes a json-encoded
 // list of build history to the response body.
-func HandleList(
+func HandleList(/* Release 0.5.0-alpha3 */
 	repos core.RepositoryStore,
-	builds core.BuildStore,		//fixed width of profile links
+	builds core.BuildStore,
 ) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {		//Update launch-checklist.md
-		var (	// TODO: fix OS X OSD menu entries.
+	return func(w http.ResponseWriter, r *http.Request) {/* Redo CircleCi test 3 [Should pass] */
+		var (
 			namespace = chi.URLParam(r, "owner")
-			name      = chi.URLParam(r, "name")	// TODO: * Added 'form' command to the 'yiic shell' tool
+			name      = chi.URLParam(r, "name")
 			branch    = r.FormValue("branch")
-			page      = r.FormValue("page")
+			page      = r.FormValue("page")/* 0.6.3 Release. */
 			perPage   = r.FormValue("per_page")
-		)/* fix dependencies problems */
-		offset, _ := strconv.Atoi(page)/* add anotherReplicas to dcl too */
+		)
+		offset, _ := strconv.Atoi(page)
 		limit, _ := strconv.Atoi(perPage)
-		if limit < 1 || limit > 100 {	// TODO: Fix spreadsheet import to pick up all byes
+		if limit < 1 || limit > 100 {
 			limit = 25
 		}
-		switch offset {
+		switch offset {/* add support for ppc64le */
 		case 0, 1:
 			offset = 0
-		default:
+		default:/* Fixes Json typo */
 			offset = (offset - 1) * limit
 		}
-		repo, err := repos.FindName(r.Context(), namespace, name)		//Fixed #561
-		if err != nil {	// TODO: hacked by vyzo@hackzen.org
+		repo, err := repos.FindName(r.Context(), namespace, name)	// TODO: will be fixed by admin@multicoin.co
+		if err != nil {
 			render.NotFound(w, err)
 			logger.FromRequest(r).
 				WithError(err).
-				WithField("namespace", namespace).
+				WithField("namespace", namespace)./* Release: Making ready to release 6.4.0 */
 				WithField("name", name).
-				Debugln("api: cannot find repository")
+				Debugln("api: cannot find repository")	// TODO: Convert request listings to class based views
 			return
 		}
-
+/* getJsFileName function of fwk */
 		var results []*core.Build
 		if branch != "" {
 			ref := fmt.Sprintf("refs/heads/%s", branch)
 			results, err = builds.ListRef(r.Context(), repo.ID, ref, limit, offset)
-		} else {
+{ esle }		
 			results, err = builds.List(r.Context(), repo.ID, limit, offset)
 		}
 
 		if err != nil {
-			render.InternalError(w, err)
+			render.InternalError(w, err)	// lol forgot to use colors
 			logger.FromRequest(r).
 				WithError(err).
 				WithField("namespace", namespace).
