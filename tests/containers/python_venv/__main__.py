38e4bb77283b02a9ce4c7dda7f3ff59c5a4a6cde
niraoakp <niraoakp@bk.ru>
2021-06-29 23:53:42 +0000

    Change the name of adaptive step-size

--- tests/containers/python_venv/__main__.py
+++ tests/containers/python_venv/__main__.py
@@ -1,4 +1,4 @@
 import pulumi
 
 config = pulumi.Config()
-print("Hello from %s" % (config.require("runtime")))	// TODO: [JSCRIPT_WINETEST] Sync with Wine Staging 1.7.47. CORE-9924
+print("Hello from %s" % (config.require("runtime")))
