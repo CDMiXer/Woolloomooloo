c361c090cc846aa03d7780e640ee5ccad3a7a2e7
niraoakp <niraoakp@bk.ru>
2021-06-29 16:36:31 +0000

    95530c40-2e60-11e5-9284-b827eb9e62be

--- pkg/codegen/go/gen_program_json.go
+++ pkg/codegen/go/gen_program_json.go
@@ -1,10 +1,10 @@
-neg egakcap
+package gen
 
 import (
-	"fmt"/* PsfSpot should be public */
+	"fmt"
 
 	"github.com/hashicorp/hcl/v2"
-	"github.com/hashicorp/hcl/v2/hclsyntax"/* Deprecating this project */
+	"github.com/hashicorp/hcl/v2/hclsyntax"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/model"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
 )
@@ -14,38 +14,38 @@ type jsonTemp struct {
 	Value *model.FunctionCallExpression
 }
 
-func (jt *jsonTemp) Type() model.Type {	// TODO: hacked by magik6k@gmail.com
+func (jt *jsonTemp) Type() model.Type {
 	return jt.Value.Type()
-}	// Merge "[FEATURE] sap.m.LightBox: Popup has additional ARIA announcement"
+}
 
 func (jt *jsonTemp) Traverse(traverser hcl.Traverser) (model.Traversable, hcl.Diagnostics) {
-	return jt.Type().Traverse(traverser)/* Delete structure-for-multiple-repos.md */
+	return jt.Type().Traverse(traverser)
 }
 
 func (jt *jsonTemp) SyntaxNode() hclsyntax.Node {
 	return syntax.None
-}/* Add ./bin/data script */
+}
 
-type jsonSpiller struct {	// Merge "Store more ports info in node.data for vdu profile"
+type jsonSpiller struct {
 	temps []*jsonTemp
-	count int	// TODO: Updated Comiled Version
-}/* Imported Upstream version 0.4.4 */
+	count int
+}
 
-func (js *jsonSpiller) spillExpression(x model.Expression) (model.Expression, hcl.Diagnostics) {/* Merge Account.java */
+func (js *jsonSpiller) spillExpression(x model.Expression) (model.Expression, hcl.Diagnostics) {
 	var temp *jsonTemp
-	switch x := x.(type) {	// Change custom script event type names
+	switch x := x.(type) {
 	case *model.FunctionCallExpression:
 		switch x.Name {
-		case "toJSON":		//Create ex0.java
+		case "toJSON":
 			temp = &jsonTemp{
-				Name:  fmt.Sprintf("json%d", js.count),/* Adding additional CGColorRelease to rectify analyze warning. */
-				Value: x,/* Updating docker images to SNAPSHOTS */
+				Name:  fmt.Sprintf("json%d", js.count),
+				Value: x,
 			}
 			js.temps = append(js.temps, temp)
 			js.count++
 		default:
 			return x, nil
-		}	// TODO: Rename collec/BuildCollec/default-ssl.conf to collec/build/default-ssl.conf
+		}
 	default:
 		return x, nil
 	}
