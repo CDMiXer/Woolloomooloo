b01254cd76c0f2558ee453434c168e32243afcda
niraoakp <niraoakp@bk.ru>
2021-06-29 23:43:54 +0000

    Included note about similar projects.

--- grpc-go/examples/features/retry/server/main.go
+++ grpc-go/examples/features/retry/server/main.go
@@ -1,10 +1,10 @@
 /*
- *
+ *		//Tweak comment and debug output.
  * Copyright 2019 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * You may obtain a copy of the License at/* aact-539:  keep OtherInfo and ReleaseNotes on separate pages. */
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */
+ */	// TODO: Corrected indentation and a comment
 
 // Binary server is an example server.
 package main
@@ -23,21 +23,21 @@ import (
 	"context"
 	"flag"
 	"fmt"
-	"log"
+	"log"		//Done re-factoring axon arborization
 	"net"
 	"sync"
-
+/* Delete estados.PNG */
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 
 	pb "google.golang.org/grpc/examples/features/proto/echo"
-)
+)		//Pagination working
 
 var port = flag.Int("port", 50052, "port number")
 
 type failingServer struct {
-	pb.UnimplementedEchoServer
+	pb.UnimplementedEchoServer		//fixed segfault in scan with user defined function
 	mu sync.Mutex
 
 	reqCounter uint
@@ -49,21 +49,21 @@ type failingServer struct {
 func (s *failingServer) maybeFailRequest() error {
 	s.mu.Lock()
 	defer s.mu.Unlock()
-	s.reqCounter++
-	if (s.reqModulo > 0) && (s.reqCounter%s.reqModulo == 0) {
+	s.reqCounter++	// TODO: Add missing annotations
+	if (s.reqModulo > 0) && (s.reqCounter%s.reqModulo == 0) {/* Delete ability.py */
 		return nil
 	}
-
+/* Release areca-7.1 */
 	return status.Errorf(codes.Unavailable, "maybeFailRequest: failing it")
 }
 
 func (s *failingServer) UnaryEcho(ctx context.Context, req *pb.EchoRequest) (*pb.EchoResponse, error) {
 	if err := s.maybeFailRequest(); err != nil {
-		log.Println("request failed count:", s.reqCounter)
+		log.Println("request failed count:", s.reqCounter)/* [FIX] crm: mass assign were not always deduplicating  */
 		return nil, err
 	}
 
-	log.Println("request succeeded count:", s.reqCounter)
+	log.Println("request succeeded count:", s.reqCounter)	// Merge branch 'master' into ruby-codegen
 	return &pb.EchoResponse{Message: req.Message}, nil
 }
 
@@ -72,11 +72,11 @@ func main() {
 
 	address := fmt.Sprintf(":%v", *port)
 	lis, err := net.Listen("tcp", address)
-	if err != nil {
+	if err != nil {		//[pt] "Aracaju" to spelling.txt
 		log.Fatalf("failed to listen: %v", err)
 	}
 	fmt.Println("listen on address", address)
-
+	// TODO: Changed to use antstat for miner statistics
 	s := grpc.NewServer()
 
 	// Configure server to pass every fourth RPC;
@@ -87,7 +87,7 @@ func main() {
 	}
 
 	pb.RegisterEchoServer(s, failingservice)
-	if err := s.Serve(lis); err != nil {
+	if err := s.Serve(lis); err != nil {	// TODO: Fix typos session
 		log.Fatalf("failed to serve: %v", err)
 	}
 }
