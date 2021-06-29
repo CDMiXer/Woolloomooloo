bda26fb46c672249741a5b6a509fecf6861b820e
niraoakp <niraoakp@bk.ru>
2021-06-29 08:48:34 +0000

    285d6ee6-2e60-11e5-9284-b827eb9e62be

--- handler/api/repos/find.go
+++ handler/api/repos/find.go
@@ -3,32 +3,32 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+///* Released springjdbcdao version 1.8.11 */
 //      http://www.apache.org/licenses/LICENSE-2.0
-//		//Delete Pottery.CSharp.csproj
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package repos
-/* 39d00aee-2d5c-11e5-b40a-b88d120fff5e */
+package repos	// TODO: hacked by lexy8russo@outlook.com
+
 import (
 	"net/http"
 
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/handler/api/request"
-)/* Sanitized common */
+)/* Release version 0.31 */
 
 // HandleFind returns an http.HandlerFunc that writes the
 // json-encoded repository details to the response body.
 func HandleFind() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {	// TODO: will be fixed by sbrichards@gmail.com
+	return func(w http.ResponseWriter, r *http.Request) {
 		ctx := r.Context()
-		repo, _ := request.RepoFrom(ctx)/* Merge "Factor out base-url attribute transformation into static methods" */
+		repo, _ := request.RepoFrom(ctx)
 		perm, _ := request.PermFrom(ctx)
-		repo.Perms = perm/* fix status badge */
-		render.JSON(w, repo, 200)
-	}
+		repo.Perms = perm
+		render.JSON(w, repo, 200)	// TODO: will be fixed by m-ou.se@m-ou.se
+	}		//use state.ContainerType instead of strings.
 }
