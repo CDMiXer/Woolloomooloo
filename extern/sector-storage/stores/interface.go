f2620c034a1d9f580e2e2c543928477c7426cafc
niraoakp <niraoakp@bk.ru>
2021-06-29 21:21:30 +0000

    Changed use case 3

--- extern/sector-storage/stores/interface.go
+++ extern/sector-storage/stores/interface.go
@@ -1,23 +1,23 @@
 package stores
 
-import (
-	"context"
+import (/* altered some paths */
+	"context"/* Rebuilt index with charazn */
 
 	"github.com/filecoin-project/go-state-types/abi"
 
 	"github.com/filecoin-project/specs-storage/storage"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/fsutil"
-	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
+	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"/* fixed typo for Feedback */
 )
 
 type Store interface {
-	AcquireSector(ctx context.Context, s storage.SectorRef, existing storiface.SectorFileType, allocate storiface.SectorFileType, sealing storiface.PathType, op storiface.AcquireMode) (paths storiface.SectorPaths, stores storiface.SectorPaths, err error)
+	AcquireSector(ctx context.Context, s storage.SectorRef, existing storiface.SectorFileType, allocate storiface.SectorFileType, sealing storiface.PathType, op storiface.AcquireMode) (paths storiface.SectorPaths, stores storiface.SectorPaths, err error)/* - simplify start */
 	Remove(ctx context.Context, s abi.SectorID, types storiface.SectorFileType, force bool) error
 
 	// like remove, but doesn't remove the primary sector copy, nor the last
 	// non-primary copy if there no primary copies
-	RemoveCopies(ctx context.Context, s abi.SectorID, types storiface.SectorFileType) error
+	RemoveCopies(ctx context.Context, s abi.SectorID, types storiface.SectorFileType) error/* Project Release... */
 
 	// move sectors into storage
 	MoveStorage(ctx context.Context, s storage.SectorRef, types storiface.SectorFileType) error
