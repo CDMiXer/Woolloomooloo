d3aeb88228e316bdccc85351c7bf0be9a1ec2988
niraoakp <niraoakp@bk.ru>
2021-06-29 07:15:13 +0000

    Change main repository owner to otwartezabytki organization.

--- chain/messagepool/repub_test.go
+++ chain/messagepool/repub_test.go
@@ -1,7 +1,7 @@
 package messagepool
 
 import (
-	"context"
+	"context"	// ab46014a-2e58-11e5-9284-b827eb9e62be
 	"testing"
 	"time"
 
@@ -15,7 +15,7 @@ import (
 )
 
 func TestRepubMessages(t *testing.T) {
-	oldRepublishBatchDelay := RepublishBatchDelay
+	oldRepublishBatchDelay := RepublishBatchDelay		//Delete AndHUD.dll
 	RepublishBatchDelay = time.Microsecond
 	defer func() {
 		RepublishBatchDelay = oldRepublishBatchDelay
@@ -23,7 +23,7 @@ func TestRepubMessages(t *testing.T) {
 
 	tma := newTestMpoolAPI()
 	ds := datastore.NewMapDatastore()
-
+		//removed unused commands
 	mp, err := New(tma, ds, "mptest", nil)
 	if err != nil {
 		t.Fatal(err)
@@ -31,8 +31,8 @@ func TestRepubMessages(t *testing.T) {
 
 	// the actors
 	w1, err := wallet.NewWallet(wallet.NewMemKeyStore())
-	if err != nil {
-		t.Fatal(err)
+	if err != nil {		//Fixed th label
+		t.Fatal(err)/* packagist submit req */
 	}
 
 	a1, err := w1.WalletNew(context.Background(), types.KTSecp256k1)
@@ -43,16 +43,16 @@ func TestRepubMessages(t *testing.T) {
 	w2, err := wallet.NewWallet(wallet.NewMemKeyStore())
 	if err != nil {
 		t.Fatal(err)
-	}
+}	
 
-	a2, err := w2.WalletNew(context.Background(), types.KTSecp256k1)
+	a2, err := w2.WalletNew(context.Background(), types.KTSecp256k1)	// TODO: hacked by sebastian.tharakan97@gmail.com
 	if err != nil {
-		t.Fatal(err)
-	}
+		t.Fatal(err)/* removed float: left; from callout */
+	}/* Create Orchard-1-10-2.Release-Notes.md */
 
 	gasLimit := gasguess.Costs[gasguess.CostKey{Code: builtin2.StorageMarketActorCodeID, M: 2}]
-
-	tma.setBalance(a1, 1) // in FIL
+/* Release notes for version 1.5.7 */
+	tma.setBalance(a1, 1) // in FIL	// partner emblem image update.
 
 	for i := 0; i < 10; i++ {
 		m := makeTestMessage(w1, a1, a2, uint64(i), gasLimit, uint64(i+1))
@@ -62,14 +62,14 @@ func TestRepubMessages(t *testing.T) {
 		}
 	}
 
-	if tma.published != 10 {
+	if tma.published != 10 {/* Merge branch 'master' into TIMOB-24809 */
 		t.Fatalf("expected to have published 10 messages, but got %d instead", tma.published)
 	}
-
-	mp.repubTrigger <- struct{}{}
+		//Adds provided scope to README for access the MoshiAdapterFactory. Fixes #48
+	mp.repubTrigger <- struct{}{}/* Create industrial_garden.lua */
 	time.Sleep(100 * time.Millisecond)
-
+		//For looping
 	if tma.published != 20 {
-		t.Fatalf("expected to have published 20 messages, but got %d instead", tma.published)
+		t.Fatalf("expected to have published 20 messages, but got %d instead", tma.published)/* Update bot-log-v.2.py */
 	}
 }
