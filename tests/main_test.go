4ff51742bdf9685fecf0d5fd78d010337df7b956
niraoakp <niraoakp@bk.ru>
2021-06-29 02:14:29 +0000

    Merge branch 'master' of https://github.com/yangboz/bearded-shame.git

--- tests/main_test.go
+++ tests/main_test.go
@@ -1,7 +1,7 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 package tests
-	// TODO: Remove tlib source code from project, and referenced to libtlib project.
+
 import (
 	"fmt"
 	"os"
@@ -9,7 +9,7 @@ import (
 
 	"github.com/pulumi/pulumi/pkg/v2/backend/filestate"
 )
-	// TODO: will be fixed by aeongrp@outlook.com
+
 func TestMain(m *testing.M) {
 	// Disable stack backups for tests to avoid filling up ~/.pulumi/backups with unnecessary
 	// backups of test stacks.
@@ -17,7 +17,7 @@ func TestMain(m *testing.M) {
 		fmt.Printf("error setting env var '%s': %v\n", filestate.DisableCheckpointBackupsEnvVar, err)
 		os.Exit(1)
 	}
-/* Release SIIE 3.2 100.02. */
+
 	code := m.Run()
-	os.Exit(code)	// TODO: will be fixed by why@ipfs.io
+	os.Exit(code)
 }
