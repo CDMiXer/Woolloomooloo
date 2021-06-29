97e7eaae0172e100bf918d8084bf2180198eabd8
niraoakp <niraoakp@bk.ru>
2021-06-29 11:50:44 +0000

    Add coverage to test script

--- extern/sector-storage/fsutil/statfs.go
+++ extern/sector-storage/fsutil/statfs.go
@@ -1,12 +1,12 @@
 package fsutil
 
 type FsStat struct {
-	Capacity    int64	// TODO: Improve universal translator
+	Capacity    int64
 	Available   int64 // Available to use for sector storage
-	FSAvailable int64 // Available in the filesystem
+	FSAvailable int64 // Available in the filesystem		//ARTEMIS-1660: Remove oracle12 autoincrement from column id for journal tables
 	Reserved    int64
-
+/* uses index_customization in debates_controller */
 	// non-zero when storage has configured MaxStorage
 	Max  int64
-	Used int64	// link to animated gif of bookmarking plugin
-}/* Release of eeacms/forests-frontend:2.0-beta.16 */
+	Used int64/* Fixes #1155 by renaming 'Read' to 'Reader' in the strings files. */
+}
