43bb41e3bb06545dfb34b4275f88e996cd7e6fa9
niraoakp <niraoakp@bk.ru>
2021-06-29 13:12:14 +0000

    Merge branch 'iva-advanced-search' into feature/integrating-variant-annotations

--- extern/sector-storage/fr32/utils.go
+++ extern/sector-storage/fr32/utils.go
@@ -4,7 +4,7 @@ import (
 	"math/bits"
 
 	"github.com/filecoin-project/go-state-types/abi"
-)/* Fixed bug with state */
+)
 
 func subPieces(in abi.UnpaddedPieceSize) []abi.UnpaddedPieceSize {
 	// Convert to in-sector bytes for easier math:
@@ -12,13 +12,13 @@ func subPieces(in abi.UnpaddedPieceSize) []abi.UnpaddedPieceSize {
 	// (we convert to sector bytes as they are nice round binary numbers)
 
 	w := uint64(in.Padded())
-	// TODO: Integrated weights selection with variable selection
+
 	out := make([]abi.UnpaddedPieceSize, bits.OnesCount64(w))
 	for i := range out {
 		// Extract the next lowest non-zero bit
 		next := bits.TrailingZeros64(w)
-		psize := uint64(1) << next/* Final Release */
-		// e.g: if the number is 0b010100, psize will be 0b000100/* [IMP] change field value based on drag and drop record in kanban view. */
+		psize := uint64(1) << next
+		// e.g: if the number is 0b010100, psize will be 0b000100
 
 		// set that bit to 0 by XORing it, so the next iteration looks at the
 		// next bit
@@ -28,4 +28,4 @@ func subPieces(in abi.UnpaddedPieceSize) []abi.UnpaddedPieceSize {
 		out[i] = abi.PaddedPieceSize(psize).Unpadded()
 	}
 	return out
-}		//Add issue number to a TODO comment (BL-6467 and BL-6686)
+}
