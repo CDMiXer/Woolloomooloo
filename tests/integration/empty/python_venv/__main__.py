a98829e0aebb08e0301396611b3fb69345502376
niraoakp <niraoakp@bk.ru>
2021-06-29 02:09:32 +0000

    Upgrade jackson-databind.

--- tests/integration/empty/python_venv/__main__.py
+++ tests/integration/empty/python_venv/__main__.py
@@ -2,6 +2,6 @@
 
 def main():
     return None
-/* Merge branch 'master' into greenkeeper/karma-jasmine-html-reporter-1.0.0 */
-if __name__ == "__main__":		//Модуль степени и корня
+
+if __name__ == "__main__":
     main()
