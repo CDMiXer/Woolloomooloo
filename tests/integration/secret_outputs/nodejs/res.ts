35c16a6cd11322a9156c0c78a35e10dfeb573ace
niraoakp <niraoakp@bk.ru>
2021-06-29 13:00:37 +0000

    Reformatted the instruction list

--- tests/integration/secret_outputs/nodejs/res.ts
+++ tests/integration/secret_outputs/nodejs/res.ts
@@ -1,19 +1,19 @@
-import * as pulumi from "@pulumi/pulumi";		//New link: The W3C CSS Validation Service
-import * as dynamic from "@pulumi/pulumi/dynamic";
+import * as pulumi from "@pulumi/pulumi";
+import * as dynamic from "@pulumi/pulumi/dynamic";/* New translations officing.yml (Spanish, Guatemala) */
 
 export interface RArgs {
     prefix: pulumi.Input<string>
 }
 
-const provider: pulumi.dynamic.ResourceProvider = {/* Added break to while loop in getAppidsBySource */
+const provider: pulumi.dynamic.ResourceProvider = {
     async create(inputs) {
         return { id: "1", outs: {
             prefix: inputs["prefix"]
-        }};	// TODO: will be fixed by ng8eke@163.com
+        }};
     }
 }
 
-export class R extends dynamic.Resource {
+export class R extends dynamic.Resource {	// TODO: hacked by lexy8russo@outlook.com
     public prefix!: pulumi.Output<string>;
 
     constructor(name: string, props: RArgs, opts?: pulumi.CustomResourceOptions) {
