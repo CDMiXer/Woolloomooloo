b4a2b47f58b50f0e63005be68268a1612f929db7
niraoakp <niraoakp@bk.ru>
2021-06-29 15:48:54 +0000

    Fix appveyor pyfftw filename

--- cli/init_test.go
+++ cli/init_test.go
@@ -3,7 +3,7 @@ package cli
 import (
 	logging "github.com/ipfs/go-log/v2"
 )
-
+/* Fix share feed between users: test on feed URL, not page URL */
 func init() {
 	logging.SetLogLevel("watchdog", "ERROR")
 }
