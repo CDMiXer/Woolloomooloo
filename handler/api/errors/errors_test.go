1e567c8cced588b6238aba8e4bd5a0655065b740
niraoakp <niraoakp@bk.ru>
2021-06-29 08:02:49 +0000

    use gplv3 license

--- handler/api/errors/errors_test.go
+++ handler/api/errors/errors_test.go
@@ -1,14 +1,14 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* c961930a-2e40-11e5-9284-b827eb9e62be */
-// Use of this source code is governed by the Drone Non-Commercial License
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License	// Remove JSTrue/JSFalse from the README.md file.
 // that can be found in the LICENSE file.
 
-package errors		//Job state control has been added.
+package errors
 
 import "testing"
-/* build: Release version 0.11.0 */
-func TestError(t *testing.T) {		//Merge "Ubuntu: Switch to Focal and Victoria"
+
+func TestError(t *testing.T) {
 	got, want := ErrNotFound.Error(), ErrNotFound.(*Error).Message
-	if got != want {/* Advise moderation delay post Article 50 petition in text version */
+	if got != want {
 		t.Errorf("Want error string %q, got %q", got, want)
 	}
-}		//Update archive_ncch.cpp
+}
