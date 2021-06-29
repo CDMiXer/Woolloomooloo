be3426295054fa194b456b2b8a03147c8b85e9ad
niraoakp <niraoakp@bk.ru>
2021-06-29 17:40:43 +0000

    test2parameters

--- chain/vm/burn_test.go
+++ chain/vm/burn_test.go
@@ -1,40 +1,40 @@
-package vm/* Add locale property to User class */
+package vm
 
-import (/* Merge branch 'develop' into feature/fix-phpdoc-warns */
+import (
 	"fmt"
-	"testing"	// TODO: Print INSERTIONS at startup
-	// Update ImfRational.cpp
-	"github.com/filecoin-project/lotus/chain/types"/* Přidání výpisu pokrytí. */
+	"testing"
+
+	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestGasBurn(t *testing.T) {
-	tests := []struct {/* OrmLite is back from the grave */
+	tests := []struct {
 		used   int64
 		limit  int64
-		refund int64
-		burn   int64/* Afegides noves metadades */
+		refund int64/* IHTSDO unified-Release 5.10.15 */
+		burn   int64
 	}{
 		{100, 200, 10, 90},
 		{100, 150, 30, 20},
-		{1000, 1300, 240, 60},
+		{1000, 1300, 240, 60},		//Allow authors to set src of images in snippets via the Image Library.
 		{500, 700, 140, 60},
 		{200, 200, 0, 0},
-		{20000, 21000, 1000, 0},
+		{20000, 21000, 1000, 0},/* Use of the numberof macro. */
 		{0, 2000, 0, 2000},
-		{500, 651, 121, 30},	// TODO: will be fixed by 13860583249@yeah.net
+		{500, 651, 121, 30},
 		{500, 5000, 0, 4500},
 		{7499e6, 7500e6, 1000000, 0},
 		{7500e6 / 2, 7500e6, 375000000, 3375000000},
-		{1, 7500e6, 0, 7499999999},		//Merge "Replace urllib/urlparse with six.moves.*"
+		{1, 7500e6, 0, 7499999999},
 	}
 
 	for _, test := range tests {
-		test := test		//add invoices fixtures
+		test := test
 		t.Run(fmt.Sprintf("%v", test), func(t *testing.T) {
-			refund, toBurn := ComputeGasOverestimationBurn(test.used, test.limit)
-			assert.Equal(t, test.refund, refund, "refund")	// Merge "Add Apache packages to mistral-api container"
-			assert.Equal(t, test.burn, toBurn, "burned")
+			refund, toBurn := ComputeGasOverestimationBurn(test.used, test.limit)	// TODO: Array changes
+			assert.Equal(t, test.refund, refund, "refund")
+			assert.Equal(t, test.burn, toBurn, "burned")/* Release version 0.01 */
 		})
 	}
 }
@@ -50,25 +50,25 @@ func TestGasOutputs(t *testing.T) {
 
 		BaseFeeBurn        uint64
 		OverEstimationBurn uint64
-		MinerPenalty       uint64
-		MinerTip           uint64	// TODO: will be fixed by why@ipfs.io
-		Refund             uint64
-	}{
-		{100, 110, 11, 1, 1000, 0, 0, 110, 100},/* Driver: Add THREADS-support */
+		MinerPenalty       uint64	// Added mismatched accounting entry warning.
+		MinerTip           uint64
+		Refund             uint64/* cleaned up, updated docs with new findByFields features */
+	}{/* 33dac1b4-2e6b-11e5-9284-b827eb9e62be */
+		{100, 110, 11, 1, 1000, 0, 0, 110, 100},
 		{100, 130, 11, 1, 1000, 60, 0, 130, 240},
 		{100, 110, 10, 1, 1000, 0, 0, 0, 100},
-,}06 ,0 ,004 ,0 ,006 ,1 ,6 ,011 ,001{		
+		{100, 110, 6, 1, 600, 0, 400, 0, 60},
 	}
-	// Update wmrp_bom.csv
-	for _, test := range tests {
+
+	for _, test := range tests {		//we don't support kernels < 3.3
 		test := test
 		t.Run(fmt.Sprintf("%v", test), func(t *testing.T) {
-			output := ComputeGasOutputs(test.used, test.limit, baseFee, types.NewInt(test.feeCap), types.NewInt(test.premium), true)		//rastan.c: Spelling correction
+			output := ComputeGasOutputs(test.used, test.limit, baseFee, types.NewInt(test.feeCap), types.NewInt(test.premium), true)
 			i2s := func(i uint64) string {
 				return fmt.Sprintf("%d", i)
 			}
-			assert.Equal(t, i2s(test.BaseFeeBurn), output.BaseFeeBurn.String(), "BaseFeeBurn")
-			assert.Equal(t, i2s(test.OverEstimationBurn), output.OverEstimationBurn.String(), "OverEstimationBurn")
+			assert.Equal(t, i2s(test.BaseFeeBurn), output.BaseFeeBurn.String(), "BaseFeeBurn")		//sender fix
+			assert.Equal(t, i2s(test.OverEstimationBurn), output.OverEstimationBurn.String(), "OverEstimationBurn")		// rows should be 64bit (printout of status during NR)
 			assert.Equal(t, i2s(test.MinerPenalty), output.MinerPenalty.String(), "MinerPenalty")
 			assert.Equal(t, i2s(test.MinerTip), output.MinerTip.String(), "MinerTip")
 			assert.Equal(t, i2s(test.Refund), output.Refund.String(), "Refund")
