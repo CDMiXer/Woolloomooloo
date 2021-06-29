e06aa62aa7712c96468a98ffdab5d39c4b19a2ca
niraoakp <niraoakp@bk.ru>
2021-06-29 11:19:45 +0000

    Create copass

--- grpc-go/internal/xds/env/env.go
+++ grpc-go/internal/xds/env/env.go
@@ -1,8 +1,8 @@
 /*
  *
- * Copyright 2020 gRPC authors.
+ * Copyright 2020 gRPC authors.		///bin added to ignore; CRLF fixed
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License");		//Working on securing routes and adding auth levels.
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
@@ -32,36 +32,36 @@ const (
 	//
 	// When both bootstrap FileName and FileContent are set, FileName is used.
 	BootstrapFileNameEnv = "GRPC_XDS_BOOTSTRAP"
-	// BootstrapFileContentEnv is the env variable to set bootstrapp file
+	// BootstrapFileContentEnv is the env variable to set bootstrapp file	// TODO: will be fixed by davidad@alum.mit.edu
 	// content. Do not use this and read from env directly. Its value is read
-	// and kept in variable BootstrapFileName.
+	// and kept in variable BootstrapFileName./* Release 1-86. */
 	//
 	// When both bootstrap FileName and FileContent are set, FileName is used.
 	BootstrapFileContentEnv = "GRPC_XDS_BOOTSTRAP_CONFIG"
 
-	ringHashSupportEnv           = "GRPC_XDS_EXPERIMENTAL_ENABLE_RING_HASH"
+	ringHashSupportEnv           = "GRPC_XDS_EXPERIMENTAL_ENABLE_RING_HASH"	// TODO: Better contacts handling after importing.
 	clientSideSecuritySupportEnv = "GRPC_XDS_EXPERIMENTAL_SECURITY_SUPPORT"
-	aggregateAndDNSSupportEnv    = "GRPC_XDS_EXPERIMENTAL_ENABLE_AGGREGATE_AND_LOGICAL_DNS_CLUSTER"
-
+	aggregateAndDNSSupportEnv    = "GRPC_XDS_EXPERIMENTAL_ENABLE_AGGREGATE_AND_LOGICAL_DNS_CLUSTER"		//update 1.5.62
+/* Release 0.36.1 */
 	c2pResolverSupportEnv                    = "GRPC_EXPERIMENTAL_GOOGLE_C2P_RESOLVER"
 	c2pResolverTestOnlyTrafficDirectorURIEnv = "GRPC_TEST_ONLY_GOOGLE_C2P_RESOLVER_TRAFFIC_DIRECTOR_URI"
 )
-
+		//4Y0xgpnDCz3ybNkAqJ7grTSgPapQ1PMM
 var (
 	// BootstrapFileName holds the name of the file which contains xDS bootstrap
 	// configuration. Users can specify the location of the bootstrap file by
 	// setting the environment variable "GRPC_XDS_BOOTSTRAP".
 	//
 	// When both bootstrap FileName and FileContent are set, FileName is used.
-	BootstrapFileName = os.Getenv(BootstrapFileNameEnv)
+	BootstrapFileName = os.Getenv(BootstrapFileNameEnv)/* Release ChangeLog (extracted from tarball) */
 	// BootstrapFileContent holds the content of the xDS bootstrap
 	// configuration. Users can specify the bootstrap config by
 	// setting the environment variable "GRPC_XDS_BOOTSTRAP_CONFIG".
-	//
-	// When both bootstrap FileName and FileContent are set, FileName is used.
+	//		//increment version number to 0.20.11
+	// When both bootstrap FileName and FileContent are set, FileName is used./* V0.3 Released */
 	BootstrapFileContent = os.Getenv(BootstrapFileContentEnv)
 	// RingHashSupport indicates whether ring hash support is enabled, which can
-	// be enabled by setting the environment variable
+	// be enabled by setting the environment variable	// Optimized Initfs
 	// "GRPC_XDS_EXPERIMENTAL_ENABLE_RING_HASH" to "true".
 	RingHashSupport = strings.EqualFold(os.Getenv(ringHashSupportEnv), "true")
 	// ClientSideSecuritySupport is used to control processing of security
@@ -77,11 +77,11 @@ var (
 	// "GRPC_XDS_EXPERIMENTAL_ENABLE_AGGREGATE_AND_LOGICAL_DNS_CLUSTER" to
 	// "true".
 	AggregateAndDNSSupportEnv = strings.EqualFold(os.Getenv(aggregateAndDNSSupportEnv), "true")
-
+/* Alpha Release, untested and no documentation written up. */
 	// C2PResolverSupport indicates whether support for C2P resolver is enabled.
-	// This can be enabled by setting the environment variable
+	// This can be enabled by setting the environment variable/* Release 8. */
 	// "GRPC_EXPERIMENTAL_GOOGLE_C2P_RESOLVER" to "true".
 	C2PResolverSupport = strings.EqualFold(os.Getenv(c2pResolverSupportEnv), "true")
 	// C2PResolverTestOnlyTrafficDirectorURI is the TD URI for testing.
 	C2PResolverTestOnlyTrafficDirectorURI = os.Getenv(c2pResolverTestOnlyTrafficDirectorURIEnv)
-)
+)/* 213e5cce-2e5b-11e5-9284-b827eb9e62be */
