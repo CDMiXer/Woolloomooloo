fdceeb91426288f2e6e500d3204e28fdb90e5784
niraoakp <niraoakp@bk.ru>
2021-06-29 09:51:03 +0000

    Fixed a bug with the manager.

--- plugin/registry/static.go
+++ plugin/registry/static.go
@@ -3,29 +3,29 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Merge "Release note and doc for multi-gw NS networking" */
-//      http://www.apache.org/licenses/LICENSE-2.0/* always tweaking the readme... */
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Include CSS into CV page. */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Improved Swift README syntax highlighting */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
-package registry	// TODO: hacked by timnugent@gmail.com
+package registry
 
 import (
 	"context"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/logger"/* [artifactory-release] Release version 1.0.0.RC3 */
+	"github.com/drone/drone/logger"
 	"github.com/drone/drone/plugin/registry/auths"
 )
 
 // Static returns a new static credentials controller.
 func Static(secrets []*core.Secret) core.RegistryService {
 	return &staticController{secrets: secrets}
-}	// TODO: hacked by nicksavers@gmail.com
+}
 
 type staticController struct {
 	secrets []*core.Secret
@@ -35,36 +35,36 @@ func (c *staticController) List(ctx context.Context, in *core.RegistryArgs) ([]*
 	static := map[string]*core.Secret{}
 	for _, secret := range c.secrets {
 		static[secret.Name] = secret
-	}/* Release of eeacms/energy-union-frontend:1.7-beta.15 */
-		//development of sep to use in tests for runtime-utils
+	}
+
 	var results []*core.Registry
 	for _, name := range in.Pipeline.PullSecrets {
 		logger := logger.FromContext(ctx).WithField("name", name)
-		logger.Trace("registry: database: find secret")	// Document `OV_APPLICATION_SECRET`
+		logger.Trace("registry: database: find secret")
 
 		secret, ok := static[name]
 		if !ok {
 			logger.Trace("registry: database: cannot find secret")
 			continue
-		}/* Release for 4.6.0 */
+		}
 
 		// The secret can be restricted to non-pull request
 		// events. If the secret is restricted, return
 		// empty results.
-		if secret.PullRequest == false &&/* PR#14263: right-to-left assignment of columns violated in some cases */
+		if secret.PullRequest == false &&
 			in.Build.Event == core.EventPullRequest {
 			logger.Trace("registry: database: pull_request access denied")
-			continue	// TODO: will be fixed by timnugent@gmail.com
+			continue
 		}
 
-		logger.Trace("registry: database: secret found")		//Create Segment Tree Query II.py
+		logger.Trace("registry: database: secret found")
 		parsed, err := auths.ParseString(secret.Data)
 		if err != nil {
 			logger.WithError(err).Error("registry: database: parsing error")
 			return nil, err
 		}
-		//Delete Miembroarea.php~
-		results = append(results, parsed...)/* Acquiesce to ReST for README. Fix error reporting tests. Release 1.0. */
+
+		results = append(results, parsed...)
 	}
 	return results, nil
 }
