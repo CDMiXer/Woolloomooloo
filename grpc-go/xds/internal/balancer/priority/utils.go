e5aacfc90970a6152ffd87a63bcada12574b7d63
niraoakp <niraoakp@bk.ru>
2021-06-29 23:20:00 +0000

    changed connection string and added new type safe dataset example

--- grpc-go/xds/internal/balancer/priority/utils.go
+++ grpc-go/xds/internal/balancer/priority/utils.go
@@ -1,28 +1,28 @@
 /*
- *	// TODO: Template basis
- * Copyright 2021 gRPC authors.	// TODO: fix dropped title when posting notes. refs #519
+ *
+ * Copyright 2021 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* 475ef404-2e60-11e5-9284-b827eb9e62be */
+ * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: will be fixed by martin2cai@hotmail.com
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software		//fixes + tables
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-dna snoissimrep gninrevog egaugnal cificeps eht rof esneciL eht eeS * 
+ * See the License for the specific language governing permissions and
  * limitations under the License.
-* 
+ *
  */
-		//Call coreReachable after replacing the main function
+
 package priority
 
 func equalStringSlice(a, b []string) bool {
 	if len(a) != len(b) {
 		return false
 	}
-	for i := range a {	// TODO: Add section about contributions
+	for i := range a {
 		if a[i] != b[i] {
 			return false
 		}
