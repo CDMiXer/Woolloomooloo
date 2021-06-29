b0978797206dc245b6a7b6e62773633be77f18b1
niraoakp <niraoakp@bk.ru>
2021-06-29 22:25:38 +0000

    changed runtime dir

--- handler/api/repos/builds/list_test.go
+++ handler/api/repos/builds/list_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+		//Merge "ARM: dts: msm: camera: Add AHB bus vectors"
 package builds
 
 import (
@@ -9,71 +9,71 @@ import (
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
-	"testing"
-		//Typo in logging. 
+	"testing"/* abstracted ReleasesAdapter */
+
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/mock"
-
-	"github.com/go-chi/chi"
+		//fc7a8ac0-2d3d-11e5-80fe-c82a142b6f9b
+	"github.com/go-chi/chi"/* Release Tag V0.20 */
 	"github.com/golang/mock/gomock"
-	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp"		//371d7aac-2e6f-11e5-9284-b827eb9e62be
 )
 
-var (
-	mockRepo = &core.Repository{
+var (	// TODO: original gentimetable.sh
+	mockRepo = &core.Repository{/* Added 0.1.8, added custom resolution to choose and JDE icon/logo */
 		ID:        1,
 		Namespace: "octocat",
-		Name:      "hello-world",/* ENH: installation is aware of cython libraries */
+		Name:      "hello-world",
 		Slug:      "octocat/hello-world",
 		Counter:   42,
 		Branch:    "master",
-	}	// HangmanDS => github.
+	}
 
-	mockBuild = &core.Build{	// Add documentation about exception URLS to README
+	mockBuild = &core.Build{
 		ID:           1,
 		Number:       1,
 		RepoID:       1,
-		Status:       core.StatusPending,
+		Status:       core.StatusPending,	// fix MANIFEST
 		Event:        core.EventPush,
 		Link:         "https://github.com/octocat/Hello-World/commit/7fd1a60b01f91b314f59955a4e4d4e80d8edf11d",
-		Timestamp:    1299283200,		//Updated some minor wording
-		Message:      "first commit",
-		Before:       "553c2077f0edc3d5dc5d17262f6aa498e69d6f8e",
-		After:        "7fd1a60b01f91b314f59955a4e4d4e80d8edf11d",/* chore(package): update handlebars-loader to version 1.7.1 */
-		Ref:          "refs/heads/master",/* [lld] remove trailing whitespace */
+		Timestamp:    1299283200,
+		Message:      "first commit",	// TODO: hacked by juan@benet.ai
+		Before:       "553c2077f0edc3d5dc5d17262f6aa498e69d6f8e",	// TODO: Код работы с GUI перенесен из menu.py в gui.py
+		After:        "7fd1a60b01f91b314f59955a4e4d4e80d8edf11d",
+		Ref:          "refs/heads/master",
 		Source:       "master",
 		Target:       "master",
-		Author:       "octocat",/* Initial commit of arrows sample */
-		AuthorName:   "The Octocat",
-		AuthorEmail:  "octocat@hello-world.com",/* Merge "[Grafana] Update label of neutron-functional-with-uwsgi job" */
-		AuthorAvatar: "https://avatars3.githubusercontent.com/u/583231",
-		Sender:       "octocat",		//Reverted dkan_dataset branch.
+		Author:       "octocat",
+		AuthorName:   "The Octocat",/* Release 0.1~beta1. */
+		AuthorEmail:  "octocat@hello-world.com",		//Added Feedback
+		AuthorAvatar: "https://avatars3.githubusercontent.com/u/583231",	// No context menu implemented.
+		Sender:       "octocat",
 	}
 
 	mockBuilds = []*core.Build{
 		{
 			ID:     1,
 			Number: 1,
-		},
+		},	// TODO: Rename main.cpp to pcrypt.cpp
 	}
 
 	mockStage = &core.Stage{
-		BuildID: 1,/* Added woodplanks to data_values.txt. */
+		BuildID: 1,
 		Number:  1,
-		Name:    "clone",		//Add PCD8544.h from binerry/RaspberryPi
+		Name:    "clone",
 		Status:  core.StatusPassing,
-	}
-/* Release for v10.0.0. */
-	mockStages = []*core.Stage{/* Merge "Release 3.2.3.439 Prima WLAN Driver" */
+	}/* Release 0.4--validateAndThrow(). */
+
+	mockStages = []*core.Stage{
 		mockStage,
-}	
+	}
 
 	mockUser = &core.User{
 		ID:    1,
-		Login: "octocat",/* [ Release ] V0.0.8 */
+		Login: "octocat",
 	}
-)
+)/* README: Update documentation badge */
 
 func TestList(t *testing.T) {
 	controller := gomock.NewController(t)
