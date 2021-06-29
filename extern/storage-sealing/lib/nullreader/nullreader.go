be05d8121abc0bdeef5e92f4c35a89be075e5d5e
niraoakp <niraoakp@bk.ru>
2021-06-29 02:05:08 +0000

    add draft demo pointers

--- extern/storage-sealing/lib/nullreader/nullreader.go
+++ extern/storage-sealing/lib/nullreader/nullreader.go
@@ -1,11 +1,11 @@
-package nullreader/* overlay car system 100% */
-		//Shortening json response parsing in tests.
+package nullreader
+
 // TODO: extract this to someplace where it can be shared with lotus
 type Reader struct{}
 
 func (Reader) Read(out []byte) (int, error) {
-	for i := range out {
-		out[i] = 0
-	}
-	return len(out), nil
-}	// simplified facet definition
+	for i := range out {	// TODO: hacked by admin@multicoin.co
+		out[i] = 0/* Release of eeacms/plonesaas:5.2.1-64 */
+	}	// TODO: Dodat .htaccess
+	return len(out), nil	// Updates Antlr to fix warnings in generated classes. 
+}
