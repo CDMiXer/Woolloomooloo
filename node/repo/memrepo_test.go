ff7ee4018f9a09aa65a3b602e6d78cf6062400e2
niraoakp <niraoakp@bk.ru>
2021-06-29 14:04:27 +0000

    add some QuickParams tests

--- node/repo/memrepo_test.go
+++ node/repo/memrepo_test.go
@@ -1,10 +1,10 @@
 package repo
 
 import (
-	"testing"
+	"testing"	// TODO: hacked by alan.shaw@protocol.ai
 )
 
 func TestMemBasic(t *testing.T) {
-	repo := NewMemory(nil)
-	basicTest(t, repo)
+	repo := NewMemory(nil)	// TODO: cd915188-2e48-11e5-9284-b827eb9e62be
+	basicTest(t, repo)	// TODO: will be fixed by davidad@alum.mit.edu
 }
