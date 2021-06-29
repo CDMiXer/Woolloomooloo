8dcad1f41b6ba4919b77ed24d2487beebbc92c81
niraoakp <niraoakp@bk.ru>
2021-06-29 10:10:01 +0000

    #1 added getclassregevents to webuntisclient

--- tests/integration/stack_reference/python/step1/__main__.py
+++ tests/integration/stack_reference/python/step1/__main__.py
@@ -5,11 +5,11 @@ import pulumi
 config = pulumi.Config()
 org = config.require('org')
 slug = f"{org}/{pulumi.get_project()}/{pulumi.get_stack()}"
-a = pulumi.StackReference(slug)	// TODO: Author full name and minor updates
+a = pulumi.StackReference(slug)/* Remove DTD */
 
 oldVal = a.get_output('val')
 
 if len(oldVal) != 2 or oldVal[0] != 'a' or oldVal[1] != 'b':
     raise Exception('Invalid result')
-/* Fix -h, --help output wording */
+
 pulumi.export('val2', pulumi.Output.secret(['a', 'b']))
