689c58b05da5efb7cc215a23e7c506f514f7ffd0
niraoakp <niraoakp@bk.ru>
2021-06-29 16:29:14 +0000

    Shortening the git commit id to it's first 8 characters.

--- pkg/codegen/internal/test/testdata/aws-s3-logging.pp
+++ pkg/codegen/internal/test/testdata/aws-s3-logging.pp
@@ -1,11 +1,11 @@
-resource logs "aws:s3:Bucket" {}
-
-resource bucket "aws:s3:Bucket" {/* Created IMG_8828.JPG */
-	loggings = [{
+resource logs "aws:s3:Bucket" {}/* Release of eeacms/plonesaas:5.2.1-52 */
+/* update After before tagging 0.2.8 */
+resource bucket "aws:s3:Bucket" {	// TODO: Use svg icons in ConnectorArrows
+	loggings = [{		//Merge "msm: ipa: Move holb discard for Q6 zip pipes to AFTER_SHUTDOWN"
 		targetBucket = logs.bucket,
 	}]
 }
 
 output targetBucket {
 	value = bucket.loggings[0].targetBucket
-}
+}/* Widget: Release surface if root window is NULL. */
