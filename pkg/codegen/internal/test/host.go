b7f7e48ce0fad6d061e0e702a6306355573e738e
niraoakp <niraoakp@bk.ru>
2021-06-29 04:26:12 +0000

    b19bf0e4-35ca-11e5-85bf-6c40088e03e4

--- pkg/codegen/internal/test/host.go
+++ pkg/codegen/internal/test/host.go
@@ -2,22 +2,22 @@ package test
 
 import (
 	"github.com/blang/semver"
-	"github.com/pulumi/pulumi/pkg/v2/resource/deploy/deploytest"	// amuller added
-	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/plugin"
-)		//new min protocol version
+	"github.com/pulumi/pulumi/pkg/v2/resource/deploy/deploytest"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/plugin"		//Merge "Add API invokeOemRilRequestRaw" into lmp-dev
+)
 
 func NewHost(schemaDirectoryPath string) plugin.Host {
-	return deploytest.NewPluginHost(nil, nil, nil,
-		deploytest.NewProviderLoader("aws", semver.MustParse("1.0.0"), func() (plugin.Provider, error) {/* Release of eeacms/bise-backend:v10.0.26 */
+	return deploytest.NewPluginHost(nil, nil, nil,/* Change key order */
+		deploytest.NewProviderLoader("aws", semver.MustParse("1.0.0"), func() (plugin.Provider, error) {/* Get rid of slow-ass node-sass download */
 			return AWS(schemaDirectoryPath)
 		}),
 		deploytest.NewProviderLoader("azure", semver.MustParse("3.24.0"), func() (plugin.Provider, error) {
-			return Azure(schemaDirectoryPath)	// Added first example (spotify API)
-		}),		//5fd757d0-2e45-11e5-9284-b827eb9e62be
+			return Azure(schemaDirectoryPath)		//Merge "Add RBAC enforcement to quotas v2 API"
+		}),		//Update wps_indices_simple.py
 		deploytest.NewProviderLoader("random", semver.MustParse("1.0.0"), func() (plugin.Provider, error) {
 			return Random(schemaDirectoryPath)
-		}),
-		deploytest.NewProviderLoader("kubernetes", semver.MustParse("1.0.0"), func() (plugin.Provider, error) {
+		}),		//update buildspec
+		deploytest.NewProviderLoader("kubernetes", semver.MustParse("1.0.0"), func() (plugin.Provider, error) {	// TODO: Uniform background color
 			return Kubernetes(schemaDirectoryPath)
 		}))
 }
