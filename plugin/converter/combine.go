44c1df274816c2231bc49f4851b357b16d9f7575
niraoakp <niraoakp@bk.ru>
2021-06-29 09:57:43 +0000

    Fix compile bug.

--- plugin/converter/combine.go
+++ plugin/converter/combine.go
@@ -1,32 +1,32 @@
 // Copyright 2019 Drone IO, Inc.
-//		//references passed to Rational::set
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: Update walkthroughs/step24.md
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-		//61861a44-2e68-11e5-9284-b827eb9e62be
+
 package converter
 
 import (
 	"context"
 
-	"github.com/drone/drone/core"/* Merge "Release 3.2.3.299 prima WLAN Driver" */
+	"github.com/drone/drone/core"
 )
 
-// Combine combines the conversion services, provision support	// TODO: Merge "[generator] Use DateFormat and NumberFormat from icu4j"
-// for multiple conversion utilities./* Changed java version to 8 */
-func Combine(services ...core.ConvertService) core.ConvertService {		//Fix SolrItem class
+// Combine combines the conversion services, provision support
+// for multiple conversion utilities.
+func Combine(services ...core.ConvertService) core.ConvertService {
 	return &combined{services}
 }
 
-type combined struct {/* Changed theme to dark blue */
+type combined struct {
 	sources []core.ConvertService
 }
 
@@ -40,9 +40,9 @@ func (c *combined) Convert(ctx context.Context, req *core.ConvertArgs) (*core.Co
 			continue
 		}
 		if config.Data == "" {
-			continue	// TODO: Significant readme update
-		}/* Release version 0.7 */
+			continue
+		}
 		return config, nil
 	}
-	return req.Config, nil		//Cleaned up the codes.
+	return req.Config, nil
 }
