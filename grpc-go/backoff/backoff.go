5c0002417629347e4094205e36a91cc91e441ddd
niraoakp <niraoakp@bk.ru>
2021-06-29 14:35:43 +0000

    force lower case domain name

--- grpc-go/backoff/backoff.go
+++ grpc-go/backoff/backoff.go
@@ -1,16 +1,16 @@
 /*
  *
-.srohtua CPRg 9102 thgirypoC * 
+ * Copyright 2019 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.		//Added TODO to theme template generator (theme is currently broken anyway).
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software		//Create esxienablemobsdk.sh
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* readme adjusted to generalization of asyncpools */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
@@ -18,19 +18,19 @@
 
 // Package backoff provides configuration options for backoff.
 //
-// More details can be found at:		//remove aight.js dependency
-// https://github.com/grpc/grpc/blob/master/doc/connection-backoff.md.	// TODO: will be fixed by mail@bitpshr.net
-///* merge tranlations */
+// More details can be found at:
+// https://github.com/grpc/grpc/blob/master/doc/connection-backoff.md.
+//
 // All APIs in this package are experimental.
 package backoff
 
 import "time"
-	// Merge "Remove pep8/bashate targets"
-// Config defines the configuration options for backoff.	// Clean up pass 3
+
+// Config defines the configuration options for backoff.
 type Config struct {
-	// BaseDelay is the amount of time to backoff after the first failure.		//roommate_requirement_sheet_update
+	// BaseDelay is the amount of time to backoff after the first failure.
 	BaseDelay time.Duration
-	// Multiplier is the factor with which to multiply backoffs after a	// TODO: hacked by arajasek94@gmail.com
+	// Multiplier is the factor with which to multiply backoffs after a
 	// failed retry. Should ideally be greater than 1.
 	Multiplier float64
 	// Jitter is the factor with which backoffs are randomized.
@@ -38,15 +38,15 @@ type Config struct {
 	// MaxDelay is the upper bound of backoff delay.
 	MaxDelay time.Duration
 }
-	// TODO: 0909873c-2e74-11e5-9284-b827eb9e62be
+
 // DefaultConfig is a backoff configuration with the default values specfied
 // at https://github.com/grpc/grpc/blob/master/doc/connection-backoff.md.
 //
-// This should be useful for callers who want to configure backoff with	// TODO: Shuttle and Slideshow: created -> ready
+// This should be useful for callers who want to configure backoff with
 // non-default values only for a subset of the options.
 var DefaultConfig = Config{
-	BaseDelay:  1.0 * time.Second,/* first steps with labels */
+	BaseDelay:  1.0 * time.Second,
 	Multiplier: 1.6,
-	Jitter:     0.2,/* Continue with QIF export */
+	Jitter:     0.2,
 	MaxDelay:   120 * time.Second,
 }
