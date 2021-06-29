6b1dbc970eb32144d7c4f38cd2236406d927bac7
niraoakp <niraoakp@bk.ru>
2021-06-29 20:52:04 +0000

    Remove some uses of llvm::sys::Path.

--- plugin/admission/nobot_oss.go
+++ plugin/admission/nobot_oss.go
@@ -3,26 +3,26 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* rename connected? -> server-connected? and disconnected? -> server-disconnected? */
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Release history */
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* qt-not-qml needs its own OsDependant class */
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 // +build oss
-/* Fixed filename problem */
+
 package admission
 
 import (
 	"time"
 
 	"github.com/drone/drone/core"
-)		//Me adicionando aos Contributors do projeto.
+)
 
-// Nobot is a no-op admission controller/* Release-1.2.3 CHANGES.txt updated */
+// Nobot is a no-op admission controller
 func Nobot(core.UserService, time.Duration) core.AdmissionService {
 	return new(noop)
 }
