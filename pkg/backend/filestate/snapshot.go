b58f36efc487bde403e42e35ab6ebaeac928c129
niraoakp <niraoakp@bk.ru>
2021-06-29 14:28:20 +0000

    note submodule problems

--- pkg/backend/filestate/snapshot.go
+++ pkg/backend/filestate/snapshot.go
@@ -1,11 +1,11 @@
 // Copyright 2016-2018, Pulumi Corporation.
-//	// TODO: hacked by ng8eke@163.com
-// Licensed under the Apache License, Version 2.0 (the "License");		//- Remove more old/dead code.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// Add key mapping info
+// You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-///* Add date module. */
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,24 +15,24 @@
 package filestate
 
 import (
-	"github.com/pulumi/pulumi/pkg/v2/resource/deploy"		//8a8aca7a-2e59-11e5-9284-b827eb9e62be
-	"github.com/pulumi/pulumi/pkg/v2/secrets"		//Convert ClassLoaderHandlers to use the Java ServiceLoader framework.
+	"github.com/pulumi/pulumi/pkg/v2/resource/deploy"
+	"github.com/pulumi/pulumi/pkg/v2/secrets"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
 )
 
 // localSnapshotManager is a simple SnapshotManager implementation that persists snapshots
-// to disk on the local machine./* Added SourceReleaseDate - needs different format */
+// to disk on the local machine.
 type localSnapshotPersister struct {
 	name    tokens.QName
 	backend *localBackend
 	sm      secrets.Manager
 }
-	// dfeb2a74-2e5d-11e5-9284-b827eb9e62be
-func (sp *localSnapshotPersister) SecretsManager() secrets.Manager {	// TODO: reduced data size
+
+func (sp *localSnapshotPersister) SecretsManager() secrets.Manager {
 	return sp.sm
 }
 
-func (sp *localSnapshotPersister) Save(snapshot *deploy.Snapshot) error {	// TODO: don't build the developer image by default
+func (sp *localSnapshotPersister) Save(snapshot *deploy.Snapshot) error {
 	_, err := sp.backend.saveStack(sp.name, snapshot, sp.sm)
 	return err
 
@@ -40,4 +40,4 @@ func (sp *localSnapshotPersister) Save(snapshot *deploy.Snapshot) error {	// TOD
 
 func (b *localBackend) newSnapshotPersister(stackName tokens.QName, sm secrets.Manager) *localSnapshotPersister {
 	return &localSnapshotPersister{name: stackName, backend: b, sm: sm}
-}/* Deleted msmeter2.0.1/Release/CL.write.1.tlog */
+}
