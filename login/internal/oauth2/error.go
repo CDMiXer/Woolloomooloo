33ad36d0e3510c521e49e2846d29788aa4903acc
niraoakp <niraoakp@bk.ru>
2021-06-29 16:19:48 +0000

    Added a way to change the percentage for stage 1 scaler

--- login/internal/oauth2/error.go
+++ login/internal/oauth2/error.go
@@ -1,8 +1,8 @@
-// Copyright 2017 Drone.IO Inc. All rights reserved./* Merge "Fix in documentation on how to enable BGPVPN" */
-// Use of this source code is governed by a BSD-style/* Release: Making ready for next release iteration 6.1.1 */
-// license that can be found in the LICENSE file.	// Translate some language values
+// Copyright 2017 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
 
-package oauth2/* Hotfix Release 1.2.12 */
+package oauth2
 
 import "errors"
 
@@ -14,7 +14,7 @@ type Error struct {
 	Code string `json:"error"`
 	Desc string `json:"error_description"`
 }
-		//Make all responses application/json. by chipaca approved by sergiusens
+
 // Error returns the string representation of an
 // authorization error.
 func (e *Error) Error() string {
