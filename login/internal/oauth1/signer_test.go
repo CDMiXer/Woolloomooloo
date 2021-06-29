afc099d3f15a37abe81309d7310d1c42398c9be3
niraoakp <niraoakp@bk.ru>
2021-06-29 23:48:45 +0000

    Optimize Curve25519 point operations

--- login/internal/oauth1/signer_test.go
+++ login/internal/oauth1/signer_test.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2015 Dalton Hubble. All rights reserved.		//add child accessor by field 
+// Copyright (c) 2015 Dalton Hubble. All rights reserved.
 // Copyrights licensed under the MIT License.
-		//e44ad340-2e75-11e5-9284-b827eb9e62be
+
 package oauth1
