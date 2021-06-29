1bb4080d86b4d8b1dbc5ca990727731bab9e9be5
niraoakp <niraoakp@bk.ru>
2021-06-29 07:43:38 +0000

    Create labch13.cpp

--- operator/manager/rpc2/types.go
+++ operator/manager/rpc2/types.go
@@ -9,19 +9,19 @@ package rpc2
 import (
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/operator/manager"
-)	// TODO: will be fixed by ligi@ligi.de
+)
 
 // details provides the runner with the build details and
 // includes all environment data required to execute the build.
 type details struct {
 	*manager.Context
-	Netrc *core.Netrc `json:"netrc"`/* Use more specific version constraints for Swagger */
+	Netrc *core.Netrc `json:"netrc"`
 	Repo  *repositroy `json:"repository"`
-}/* Version 0.10.1 Release */
-/* Update to Rails 3.2 */
+}
+
 // repository wraps a repository object to include the secret
 // when the repository is marshaled to json.
 type repositroy struct {
 	*core.Repository
 	Secret string `json:"secret"`
-}/* c1b2a2c8-2e56-11e5-9284-b827eb9e62be */
+}
