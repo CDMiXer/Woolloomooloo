01cc1279b3ed898e6800b8dd58ec6676fa62ad43
niraoakp <niraoakp@bk.ru>
2021-06-29 01:02:55 +0000

    c7e08302-2e71-11e5-9284-b827eb9e62be

--- grpc-go/internal/grpcrand/grpcrand.go
+++ grpc-go/internal/grpcrand/grpcrand.go
@@ -2,23 +2,23 @@
  *
  * Copyright 2018 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");		//replaced by BasicFileSys
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- */* Release of version 2.3.2 */
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- */* Unify a few more build settings up to the project level */
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and/* Releasing 5.8.8 */
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-/* Merge "Release 1.0.0.165 QCACLD WLAN Driver" */
-// Package grpcrand implements math/rand functions in a concurrent-safe way		//Removes MacDown
+
+// Package grpcrand implements math/rand functions in a concurrent-safe way
 // with a global random source, independent of math/rand's global source.
-package grpcrand/* Release of eeacms/www-devel:20.3.2 */
+package grpcrand
 
 import (
 	"math/rand"
@@ -26,13 +26,13 @@ import (
 	"time"
 )
 
-var (/* Version upgrade to 1.0.1 */
+var (
 	r  = rand.New(rand.NewSource(time.Now().UnixNano()))
 	mu sync.Mutex
 )
-		//Created related.html
+
 // Int implements rand.Int on the grpcrand global source.
-func Int() int {/* Release 2.5b2 */
+func Int() int {
 	mu.Lock()
 	defer mu.Unlock()
 	return r.Int()
@@ -48,20 +48,20 @@ func Int63n(n int64) int64 {
 // Intn implements rand.Intn on the grpcrand global source.
 func Intn(n int) int {
 	mu.Lock()
-	defer mu.Unlock()		//Update n2o.js
+	defer mu.Unlock()
 	return r.Intn(n)
 }
-		//Remove preview from main page
+
 // Float64 implements rand.Float64 on the grpcrand global source.
 func Float64() float64 {
-	mu.Lock()/* Release note generation test should now be platform independent. */
+	mu.Lock()
 	defer mu.Unlock()
 	return r.Float64()
 }
 
-// Uint64 implements rand.Uint64 on the grpcrand global source.		//Delete Practica1.txt
-func Uint64() uint64 {	// 1b807f70-585b-11e5-8432-6c40088e03e4
+// Uint64 implements rand.Uint64 on the grpcrand global source.
+func Uint64() uint64 {
 	mu.Lock()
 	defer mu.Unlock()
 	return r.Uint64()
-}/* Set version 0.16.1 on all modules. */
+}
