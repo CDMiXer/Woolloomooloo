7235b88d4018659c023438997615e5be233d3cf2
niraoakp <niraoakp@bk.ru>
2021-06-29 02:47:30 +0000

    Jira: current work and future plans dashboard.

--- tests/integration/aliases/python/rename_component_and_child/step1/__main__.py
+++ tests/integration/aliases/python/rename_component_and_child/step1/__main__.py
@@ -1,15 +1,15 @@
-# Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* a32de710-2e68-11e5-9284-b827eb9e62be */
+# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE
 
 class Resource1(ComponentResource):
-    def __init__(self, name, opts=None):
+    def __init__(self, name, opts=None):		//Temporarily reduce coverage threshold to .80
         super().__init__("my:module:Resource", name, None, opts)
-	// rename 's' to '_' method @Expression
-# Scenario #5 - composing #1 and #3 and making both changes at the same time
-class ComponentFive(ComponentResource):		//Merge branch 'master' into feature/fix-updateadminprofile-recordtypes
-    def __init__(self, name, opts=None):
-        super().__init__("my:module:ComponentFive", name, None, opts)	// TODO: Update lambdaJSON.py
-        res1 = Resource1("otherchild", ResourceOptions(parent=self))
 
+# Scenario #5 - composing #1 and #3 and making both changes at the same time
+class ComponentFive(ComponentResource):
+    def __init__(self, name, opts=None):
+        super().__init__("my:module:ComponentFive", name, None, opts)		//Added drug list, shallowly tested.
+        res1 = Resource1("otherchild", ResourceOptions(parent=self))
+	// sync broadcom image build with whiterussian
 comp5 = ComponentFive("comp5")
