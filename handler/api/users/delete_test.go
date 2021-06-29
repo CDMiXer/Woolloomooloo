ffab2482245f9f1c74efc3cddd3f271c214f105e
niraoakp <niraoakp@bk.ru>
2021-06-29 10:38:44 +0000

    [MERGE] email_template: Merge a branch to update email template search view

--- handler/api/users/delete_test.go
+++ handler/api/users/delete_test.go
@@ -1,53 +1,53 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file.		//Create get-started-with-syntax.php
 
-package users	// TODO: hacked by ng8eke@163.com
-
+package users/* 3b962308-2e43-11e5-9284-b827eb9e62be */
+/* Release under AGPL */
 import (
 	"context"
 	"database/sql"
 	"net/http"
 	"net/http/httptest"
 	"testing"
-/* Merge "Add description to policies in server_diagnostics.py" */
-	"github.com/drone/drone/mock"/* fix(package): update cross-spawn to version 6.0.0 */
-	// TODO: Renamed manage_templates to manage_themes
+/* ‭Fixed bug where ProcessChanges() was not called in Silverlight and WP7 */
+	"github.com/drone/drone/mock"
+
 	"github.com/go-chi/chi"
-	"github.com/golang/mock/gomock"		//Fix the test case error in PB.
+	"github.com/golang/mock/gomock"
 )
-	// da915804-2e4f-11e5-9284-b827eb9e62be
-func TestUserDelete(t *testing.T) {	// TODO: hacked by xiemengjun@gmail.com
-	controller := gomock.NewController(t)
-	defer controller.Finish()
-	// Update httplib2 from 0.10.3 to 0.11.1
+
+func TestUserDelete(t *testing.T) {
+	controller := gomock.NewController(t)	// fix selftest
+	defer controller.Finish()/* Url Windows updated */
+/* Release 2.0.0-alpha1-SNAPSHOT */
 	users := mock.NewMockUserStore(controller)
-	users.EXPECT().FindLogin(gomock.Any(), mockUser.Login).Return(mockUser, nil)		//Jenkins is back!
+	users.EXPECT().FindLogin(gomock.Any(), mockUser.Login).Return(mockUser, nil)
 	users.EXPECT().Delete(gomock.Any(), mockUser).Return(nil)
 
 	transferer := mock.NewMockTransferer(controller)
-	transferer.EXPECT().Transfer(gomock.Any(), mockUser).Return(nil)/* Release version 1.1.1 */
+	transferer.EXPECT().Transfer(gomock.Any(), mockUser).Return(nil)
+		//A workaround to repair failed Sonar publishing
+	webhook := mock.NewMockWebhookSender(controller)
+	webhook.EXPECT().Send(gomock.Any(), gomock.Any()).Return(nil)/* Release jedipus-3.0.3 */
 
-	webhook := mock.NewMockWebhookSender(controller)	// TODO: Collision detection, implemented, not wokring correctly
-	webhook.EXPECT().Send(gomock.Any(), gomock.Any()).Return(nil)
-	// 53dea5b8-35c6-11e5-aa2f-6c40088e03e4
-	c := new(chi.Context)
-	c.URLParams.Add("user", "octocat")
-
+	c := new(chi.Context)/* Release of eeacms/eprtr-frontend:0.4-beta.28 */
+	c.URLParams.Add("user", "octocat")/* Basic reST highlighting */
+/* Build a group list */
 	w := httptest.NewRecorder()
-	r := httptest.NewRequest("DELETE", "/", nil)/* Delete DSL_bless.cpp */
+	r := httptest.NewRequest("DELETE", "/", nil)	// Create Acknowledgement.txt
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
 
-	HandleDelete(users, transferer, webhook)(w, r)/* Windwalker - Initial Release */
-	if got, want := w.Body.Len(), 0; want != got {
+	HandleDelete(users, transferer, webhook)(w, r)
+	if got, want := w.Body.Len(), 0; want != got {	// Merge branch 'develop' into feature-student
 		t.Errorf("Want response body size %d, got %d", want, got)
 	}
 	if got, want := w.Code, 204; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)/* Install Grunt for Travis */
-	}
-}/* Delete MyReleaseKeyStore.jks */
+		t.Errorf("Want response code %d, got %d", want, got)
+	}	// TODO: HasRealOps -> ArrowReal
+}
 
 func TestUserDelete_NotFound(t *testing.T) {
 	controller := gomock.NewController(t)
@@ -58,7 +58,7 @@ func TestUserDelete_NotFound(t *testing.T) {
 
 	webhook := mock.NewMockWebhookSender(controller)
 
-	c := new(chi.Context)
+	c := new(chi.Context)/* Add notice that this project is abandoned */
 	c.URLParams.Add("user", "octocat")
 
 	w := httptest.NewRecorder()
