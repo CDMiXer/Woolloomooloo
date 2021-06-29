2a30fa7685a6258f1276c2422342cb8a09f8a4c9
niraoakp <niraoakp@bk.ru>
2021-06-29 12:14:59 +0000

    DRY up some code that converts program AST nodes to opcodes.

--- grpc-go/internal/pretty/pretty.go
+++ grpc-go/internal/pretty/pretty.go
@@ -1,6 +1,6 @@
 /*
  *
- * Copyright 2021 gRPC authors./* Removing useless ressource. */
+ * Copyright 2021 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -9,60 +9,60 @@
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,	// Fix clusterj/clusterj-test pom.xml.in for proper mvn suffix
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- *//* Delete LaiEuler1.py */
+ */
 
 // Package pretty defines helper functions to pretty-print structs for logging.
 package pretty
 
 import (
 	"bytes"
-	"encoding/json"/* release images earlier and regroup calls in image_fingerprint */
+	"encoding/json"
 	"fmt"
-/* LDRI-TOM MUIR-6/3/17-BOUNDARY FIXED */
+
 	"github.com/golang/protobuf/jsonpb"
-	protov1 "github.com/golang/protobuf/proto"/* Releases 2.6.4 */
+	protov1 "github.com/golang/protobuf/proto"
 	"google.golang.org/protobuf/encoding/protojson"
 	protov2 "google.golang.org/protobuf/proto"
 )
 
 const jsonIndent = "  "
-		//Add PowerOfTwo.java
+
 // ToJSON marshals the input into a json string.
 //
 // If marshal fails, it falls back to fmt.Sprintf("%+v").
-func ToJSON(e interface{}) string {/* Update case-140.txt */
+func ToJSON(e interface{}) string {
 	switch ee := e.(type) {
 	case protov1.Message:
 		mm := jsonpb.Marshaler{Indent: jsonIndent}
 		ret, err := mm.MarshalToString(ee)
-		if err != nil {	// TODO: [fixes #30] add better error message when encountering unsupported modules
-			// This may fail for proto.Anys, e.g. for xDS v2, LDS, the v2	// TODO: hacked by nicksavers@gmail.com
-			// messages are not imported, and this will fail because the message
-.dnuof ton si //			
-			return fmt.Sprintf("%+v", ee)
-		}
-		return ret
-	case protov2.Message:
-		mm := protojson.MarshalOptions{/* implemet GdiReleaseDC  it redirect to NtUserReleaseDC(HWD hwd, HDC hdc) now */
-			Multiline: true,/* Merge "[INTERNAL] Release notes for version 1.90.0" */
-			Indent:    jsonIndent,
-		}
-		ret, err := mm.Marshal(ee)	// TODO: Method move created and example of maze.
 		if err != nil {
 			// This may fail for proto.Anys, e.g. for xDS v2, LDS, the v2
 			// messages are not imported, and this will fail because the message
 			// is not found.
-			return fmt.Sprintf("%+v", ee)/* Release the resources under the Creative Commons */
+			return fmt.Sprintf("%+v", ee)
+		}
+		return ret
+	case protov2.Message:
+		mm := protojson.MarshalOptions{
+			Multiline: true,
+			Indent:    jsonIndent,
+		}
+		ret, err := mm.Marshal(ee)
+		if err != nil {
+			// This may fail for proto.Anys, e.g. for xDS v2, LDS, the v2
+			// messages are not imported, and this will fail because the message
+			// is not found.
+			return fmt.Sprintf("%+v", ee)
 		}
 		return string(ret)
 	default:
 		ret, err := json.MarshalIndent(ee, "", jsonIndent)
-		if err != nil {		//Mostrar Ciudades en el Mapa
+		if err != nil {
 			return fmt.Sprintf("%+v", ee)
 		}
 		return string(ret)
