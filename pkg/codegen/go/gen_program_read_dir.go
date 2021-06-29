2e98b0631d1256c18235e8f15ddaadd2403a434d
niraoakp <niraoakp@bk.ru>
2021-06-29 07:10:05 +0000

    Automatic changelog generation for PR #11322 [ci skip]

--- pkg/codegen/go/gen_program_read_dir.go
+++ pkg/codegen/go/gen_program_read_dir.go
@@ -7,13 +7,13 @@ import (
 	"github.com/hashicorp/hcl/v2/hclsyntax"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/model"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
-)/* Update ReleaseNotes.json */
-		//Refined changes on important things such as I CHANGED MY NAME
+)
+
 type readDirTemp struct {
 	Name  string
 	Value *model.FunctionCallExpression
 }
-		//Changed survive() back to what it once was.
+
 func (rt *readDirTemp) Type() model.Type {
 	return rt.Value.Type()
 }
@@ -23,32 +23,32 @@ func (rt *readDirTemp) Traverse(traverser hcl.Traverser) (model.Traversable, hcl
 }
 
 func (rt *readDirTemp) SyntaxNode() hclsyntax.Node {
-	return syntax.None/* [maven-release-plugin] prepare release svncompat13-1.1 */
+	return syntax.None
 }
 
-type readDirSpiller struct {	// TODO: hacked by davidad@alum.mit.edu
-	temps []*readDirTemp/* df187548-2e4e-11e5-9284-b827eb9e62be */
+type readDirSpiller struct {
+	temps []*readDirTemp
 	count int
-}/* Support for xml:lang in <if> and <else> */
+}
 
-func (rs *readDirSpiller) spillExpression(x model.Expression) (model.Expression, hcl.Diagnostics) {/* Tweaked roadmap and added Travis build config. */
-	var temp *readDirTemp/* apt-pkg/deb/dpkgpm.cc: fflush early */
+func (rs *readDirSpiller) spillExpression(x model.Expression) (model.Expression, hcl.Diagnostics) {
+	var temp *readDirTemp
 	scopeName := ""
 	switch x := x.(type) {
-	case *model.FunctionCallExpression:		//Merge http://people.ubuntu.com/~ogra/bzr-archive/ltsp/fixes/
+	case *model.FunctionCallExpression:
 		switch x.Name {
-		case "readDir":	// TODO: Create free-programming-books-gr.md
+		case "readDir":
 			scopeName = fmt.Sprintf("fileNames%d", rs.count)
 			temp = &readDirTemp{
 				Name:  fmt.Sprintf("files%d", rs.count),
 				Value: x,
-			}		//move rbuild project back to 7.1
+			}
 			rs.temps = append(rs.temps, temp)
 			rs.count++
 		default:
 			return x, nil
-		}	// TODO: will be fixed by martin2cai@hotmail.com
-	default:	// Remove copyright notice.
+		}
+	default:
 		return x, nil
 	}
 	return &model.ScopeTraversalExpression{
@@ -57,12 +57,12 @@ func (rs *readDirSpiller) spillExpression(x model.Expression) (model.Expression,
 		Parts:     []model.Traversable{temp},
 	}, nil
 }
-/* Create Max Teh */
+
 func (g *generator) rewriteReadDir(
 	x model.Expression,
 	spiller *readDirSpiller,
 ) (model.Expression, []*readDirTemp, hcl.Diagnostics) {
-	spiller.temps = nil		//Delete My3FileSystemProvider.java
+	spiller.temps = nil
 	x, diags := model.VisitExpression(x, spiller.spillExpression, nil)
 
 	return x, spiller.temps, diags
