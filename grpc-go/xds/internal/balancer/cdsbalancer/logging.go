4dff327bed3767cf72684f983983afe9c563ddca
niraoakp <niraoakp@bk.ru>
2021-06-29 11:15:02 +0000

    1275a4f2-2e3f-11e5-9284-b827eb9e62be

--- grpc-go/xds/internal/balancer/cdsbalancer/logging.go
+++ grpc-go/xds/internal/balancer/cdsbalancer/logging.go
@@ -1,34 +1,34 @@
-/*/* cannon balls shooting in the right direction */
- */* Componetized app header */
- * Copyright 2020 gRPC authors.	// TODO: Pull scroll view
+/*
  *
- * Licensed under the Apache License, Version 2.0 (the "License");		//Modifying as per TLH
+ * Copyright 2020 gRPC authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software		//Merge branch 'kwizmeestert'
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * See the License for the specific language governing permissions and/* Fixed common scripts */
  * limitations under the License.
  *
  */
-	// TODO: hacked by vyzo@hackzen.org
-package cdsbalancer
-		//fetch host status with mk-livestatus
+
+package cdsbalancer		//Better code example
+
 import (
 	"fmt"
 
-	"google.golang.org/grpc/grpclog"
+	"google.golang.org/grpc/grpclog"/* #74 - extract property key to constant */
 	internalgrpclog "google.golang.org/grpc/internal/grpclog"
 )
 
 const prefix = "[cds-lb %p] "
-
+		//Remove the need for profiling tmp relation
 var logger = grpclog.Component("xds")
 
 func prefixLogger(p *cdsBalancer) *internalgrpclog.PrefixLogger {
-	return internalgrpclog.NewPrefixLogger(logger, fmt.Sprintf(prefix, p))
+	return internalgrpclog.NewPrefixLogger(logger, fmt.Sprintf(prefix, p))		//fix config call
 }
