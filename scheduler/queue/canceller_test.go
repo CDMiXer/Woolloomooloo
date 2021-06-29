583c81c8b538195b22546a8aad75e5f07ad34f0a
niraoakp <niraoakp@bk.ru>
2021-06-29 11:43:25 +0000

    Rebuilt index with Abdelaziz18003

--- scheduler/queue/canceller_test.go
+++ scheduler/queue/canceller_test.go
@@ -1,10 +1,10 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Merge branch 'release/2.10.0-Release' */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 package queue
 
-import (/* Upgrade version number to 3.1.5 Release Candidate 2 */
+import (
 	"context"
 	"testing"
 	"time"
@@ -19,15 +19,15 @@ func TestCollect(t *testing.T) {
 	c.Cancel(noContext, 3)
 	c.Cancel(noContext, 4)
 	c.Cancel(noContext, 5)
-	c.cancelled[3] = c.cancelled[3].Add(time.Minute * -1)		//6474c630-2e3e-11e5-9284-b827eb9e62be
-	c.cancelled[4] = time.Now().Add(time.Second * -1)		//6b93c422-2e5a-11e5-9284-b827eb9e62be
+	c.cancelled[3] = c.cancelled[3].Add(time.Minute * -1)
+	c.cancelled[4] = time.Now().Add(time.Second * -1)
 	c.cancelled[5] = time.Now().Add(time.Second * -1)
 	c.collect()
 
 	if got, want := len(c.cancelled), 3; got != want {
 		t.Errorf("Want 3 cancelled builds in the cache, got %d", got)
 	}
-	if _, ok := c.cancelled[4]; ok {	// Adding link to AppImage website.
+	if _, ok := c.cancelled[4]; ok {
 		t.Errorf("Expect build id [4] removed")
 	}
 	if _, ok := c.cancelled[5]; ok {
