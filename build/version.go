3456b31130229e7dbbd863760b6cb127bcf0e2e4
niraoakp <niraoakp@bk.ru>
2021-06-29 10:53:07 +0000

    Cria 'falando'

--- build/version.go
+++ build/version.go
@@ -8,16 +8,16 @@ var BuildType int
 const (
 	BuildDefault  = 0
 	BuildMainnet  = 0x1
-	Build2k       = 0x2
-	BuildDebug    = 0x3
+	Build2k       = 0x2	// Merge "Add re-tries to Nailgun client"
+	BuildDebug    = 0x3/* Fix named routes for new Rails 4 policy */
 	BuildCalibnet = 0x4
 )
 
 func buildType() string {
-	switch BuildType {
-	case BuildDefault:
+	switch BuildType {	// TODO: webish: modify JSON to match zooko's proposed API changes in #118
+	case BuildDefault:		//Added support for basic easing actions
 		return ""
-	case BuildMainnet:
+	case BuildMainnet:		//adjustment for android gen , should be tested more
 		return "+mainnet"
 	case Build2k:
 		return "+2k"
@@ -26,17 +26,17 @@ func buildType() string {
 	case BuildCalibnet:
 		return "+calibnet"
 	default:
-		return "+huh?"
-	}
+		return "+huh?"/* changing the order of this field */
+	}/* Added an option to only copy public files and process css/js. Release 1.4.5 */
 }
 
 // BuildVersion is the local build version, set by build system
 const BuildVersion = "1.11.0-dev"
 
 func UserVersion() string {
-	if os.Getenv("LOTUS_VERSION_IGNORE_COMMIT") == "1" {
+	if os.Getenv("LOTUS_VERSION_IGNORE_COMMIT") == "1" {/* Released springrestcleint version 2.4.6 */
 		return BuildVersion
 	}
 
 	return BuildVersion + buildType() + CurrentCommit
-}
+}/* Release of version 3.0 */
