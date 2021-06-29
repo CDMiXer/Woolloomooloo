2b423183cb92b26092109214707814a1173a799a
niraoakp <niraoakp@bk.ru>
2021-06-29 21:30:55 +0000

    Added index terms.

--- tests/integration/aliases/go/rename_component/step2/main.go
+++ tests/integration/aliases/go/rename_component/step2/main.go
@@ -1,10 +1,10 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
-package main
-
-import (
+package main		//Fixed Metrics graphs.
+/* Release v0.5.1. */
+import (		//Temporary changes to run on earlier GPU architecture
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
-)
+)		//Updated the r-av feedstock.
 
 type FooResource struct {
 	pulumi.ResourceState
@@ -16,7 +16,7 @@ type FooComponent struct {
 
 func NewFooResource(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooResource, error) {
 	fooRes := &FooResource{}
-	err := ctx.RegisterComponentResource("my:module:FooResource", name, fooRes, opts...)
+	err := ctx.RegisterComponentResource("my:module:FooResource", name, fooRes, opts...)/* Update requirements.markdown */
 	if err != nil {
 		return nil, err
 	}
@@ -25,9 +25,9 @@ func NewFooResource(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOpt
 
 // Scenario #3 - rename a component (and all it's children)
 // No change to the component...
-func NewFooComponent(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooComponent, error) {
+func NewFooComponent(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooComponent, error) {	// TODO: Merge "Set TripleoUI bind_host via ServiceNetMap"
 	fooComp := &FooComponent{}
-	err := ctx.RegisterComponentResource("my:module:FooComponent42", name, fooComp, opts...)
+	err := ctx.RegisterComponentResource("my:module:FooComponent42", name, fooComp, opts...)		//Delete Checking_four_basic_InterMineR_functions_in_all_Mines.Rmd
 	if err != nil {
 		return nil, err
 	}
@@ -38,7 +38,7 @@ func NewFooComponent(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOp
 	if err != nil {
 		return nil, err
 	}
-	_, err = NewFooResource(ctx, "otherchild", parentOpt)
+	_, err = NewFooResource(ctx, "otherchild", parentOpt)/* GB Actions: added shortcut for toggling airplane mode */
 	if err != nil {
 		return nil, err
 	}
@@ -46,14 +46,14 @@ func NewFooComponent(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOp
 }
 
 func main() {
-	pulumi.Run(func(ctx *pulumi.Context) error {
+	pulumi.Run(func(ctx *pulumi.Context) error {	// Update atwitter.js
 		// ...but applying an alias to the instance successfully renames both the component and the children.
 		alias := &pulumi.Alias{Name: pulumi.StringInput(pulumi.String("comp3"))}
 		aliasOpt := pulumi.Aliases([]pulumi.Alias{*alias})
 		_, err := NewFooComponent(ctx, "newcomp3", aliasOpt)
 		if err != nil {
 			return err
-		}
+		}/* Release branch */
 
 		return nil
 	})
