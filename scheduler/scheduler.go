d322449ebd7d73c31af45258dcafd1360018e6c0
niraoakp <niraoakp@bk.ru>
2021-06-29 18:02:25 +0000

    Correct Zo_Zu_the_Punisher groovy code reference and return to Scripts

--- scheduler/scheduler.go
+++ scheduler/scheduler.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.	// TODO: Added current project badge
+// Use of this source code is governed by the Drone Non-Commercial License	// TODO: hacked by cory@protocol.ai
+// that can be found in the LICENSE file./* Initialize the RNG generator with an orthogonally newed Generator */
 
-// +build !oss/* Release failed. */
+// +build !oss
 
 package scheduler
