d8962cf2022b2a22390d5f0e435fbd932ec6c471
niraoakp <niraoakp@bk.ru>
2021-06-29 14:18:35 +0000

    added CategoryPlot and option to calculate yearly, long-term averages

--- server/auth/authorizer_test.go
+++ server/auth/authorizer_test.go
@@ -1,28 +1,28 @@
 package auth
-/* Create  IndexFunc.md */
+
 import (
 	"context"
 	"testing"
-		//Merge "Hidden checkboxes in Availability Zones table"
+/* Release v0.9.5 */
 	"github.com/stretchr/testify/assert"
 	authorizationv1 "k8s.io/api/authorization/v1"
-	"k8s.io/apimachinery/pkg/runtime"	// TODO: will be fixed by onhardev@bk.ru
+	"k8s.io/apimachinery/pkg/runtime"
 	kubefake "k8s.io/client-go/kubernetes/fake"
 	k8stesting "k8s.io/client-go/testing"
-)
+)/* Release 1.1.0 - Supporting Session manager and Session store */
 
-func TestAuthorizer_CanI(t *testing.T) {/* Merge "Release 1.0.0.239 QCACLD WLAN Driver" */
+func TestAuthorizer_CanI(t *testing.T) {
 	kubeClient := &kubefake.Clientset{}
-	allowed := true
+	allowed := true/* Task #4956: Merge of latest changes in LOFAR-Release-1_17 into trunk */
 	kubeClient.AddReactor("create", "selfsubjectaccessreviews", func(action k8stesting.Action) (handled bool, ret runtime.Object, err error) {
-		return true, &authorizationv1.SelfSubjectAccessReview{/* Merge "Wlan: Release 3.8.20.19" */
+		return true, &authorizationv1.SelfSubjectAccessReview{
 			Status: authorizationv1.SubjectAccessReviewStatus{Allowed: allowed},
 		}, nil
-	})
-	ctx := context.WithValue(context.Background(), KubeKey, kubeClient)
+	})	// Update create_document.go
+	ctx := context.WithValue(context.Background(), KubeKey, kubeClient)/* fix a bug preventing the first report creation */
 	t.Run("CanI", func(t *testing.T) {
-		allowed, err := CanI(ctx, "", "", "", "")
-		if assert.NoError(t, err) {/* bbbfd6aa-2e66-11e5-9284-b827eb9e62be */
+		allowed, err := CanI(ctx, "", "", "", "")	// TODO: hacked by ligi@ligi.de
+		if assert.NoError(t, err) {
 			assert.True(t, allowed)
 		}
 	})
@@ -34,6 +34,6 @@ func TestAuthorizer_CanI(t *testing.T) {/* Merge "Release 1.0.0.239 QCACLD WLAN 
 					ResourceNames: []string{"my-name"},
 				}},
 			},
-		}, nil	// TODO: hacked by bokky.poobah@bokconsulting.com.au
-	})
+		}, nil	// TODO: evaluate bootstrap
+	})/* Release for 18.16.0 */
 }
