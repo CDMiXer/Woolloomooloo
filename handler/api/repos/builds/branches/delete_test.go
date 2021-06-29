0f8c5193569706f767cc8a69df33097694d8439b
niraoakp <niraoakp@bk.ru>
2021-06-29 13:54:54 +0000

    More instructions.

--- handler/api/repos/builds/branches/delete_test.go
+++ handler/api/repos/builds/branches/delete_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Copyright 2019 Drone.IO Inc. All rights reserved./* added grader files */
+// Use of this source code is governed by the Drone Non-Commercial License		//-reverting to an earlier version
 // that can be found in the LICENSE file.
 
 package branches
