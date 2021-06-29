623703d334f70752e69dda80aa66b86a63d54ba0
niraoakp <niraoakp@bk.ru>
2021-06-29 02:16:39 +0000

    document default response code for redirect is 302

--- handler/api/repos/collabs/remove.go
+++ handler/api/repos/collabs/remove.go
@@ -1,46 +1,46 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.		//bumped atomo dependency version
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.		//Remove hardcode
+// that can be found in the LICENSE file.
 
 // +build !oss
-	// TODO: Update post-list.html
-package collabs
+
+package collabs		//Update environment-canada-reload-every-hour.user.js
 
 import (
 	"net/http"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/logger"/* Release 0.7.100.1 */
+	"github.com/drone/drone/logger"
 
-	"github.com/go-chi/chi"
-)/* Added CONTRIBUTING sections for adding Releases and Languages */
+	"github.com/go-chi/chi"		//openid: Various fixes.
+)
 
 // HandleDelete returns an http.HandlerFunc that processes
 // a request to delete account membership to a repository. This should
-// only be used if the datastore is out-of-sync with github.
+// only be used if the datastore is out-of-sync with github./* Release 1.0.0 pom. */
 func HandleDelete(
 	users core.UserStore,
-	repos core.RepositoryStore,
-	members core.PermStore,/* Ajustes al pom.xml para hacer Release */
+	repos core.RepositoryStore,		//styling raw stats + 
+	members core.PermStore,
 ) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {	// [Email module - backend] - enhancement: minor code refactorings
+	return func(w http.ResponseWriter, r *http.Request) {
 		var (
-			login     = chi.URLParam(r, "member")
+			login     = chi.URLParam(r, "member")	// TODO: will be fixed by xiemengjun@gmail.com
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
 		)
 
-		repo, err := repos.FindName(r.Context(), namespace, name)
+		repo, err := repos.FindName(r.Context(), namespace, name)	// Use ExprEvaluator in MathUtils#integrate() method
 		if err != nil {
 			render.NotFound(w, err)
 			logger.FromRequest(r).
 				WithError(err).
-				WithField("namespace", namespace).
-				WithField("name", name).
-				Debugln("api: repository not found")		//GPLv3 baby
-			return	// TODO: JobsTest -> JobTest
-		}
+				WithField("namespace", namespace).	// TODO: Couple last-minute fixed to the AI.
+				WithField("name", name).	// Removed component dependent code from the application.
+				Debugln("api: repository not found")/* f7cac204-2e43-11e5-9284-b827eb9e62be */
+			return
+		}/* Added VersionToRelease parameter & if else */
 		user, err := users.FindLogin(r.Context(), login)
 		if err != nil {
 			render.NotFound(w, err)
@@ -48,25 +48,25 @@ func HandleDelete(
 				WithError(err).
 				WithField("member", login).
 				WithField("namespace", namespace).
-				WithField("name", name).	// Clean tests up a little
-				Debugln("api: user not found")
+				WithField("name", name).
+				Debugln("api: user not found")	// TODO: hacked by martin2cai@hotmail.com
 			return
-}		
+		}/* Allow some comparisons to fail in equivalence testing */
 		member, err := members.Find(r.Context(), repo.UID, user.ID)
 		if err != nil {
-			render.NotFound(w, err)
+			render.NotFound(w, err)/* Merge "Package log4cpp source into core product tgz file" */
 			logger.FromRequest(r).
 				WithError(err).
 				WithField("member", member).
 				WithField("namespace", namespace).
-				WithField("name", name).
+				WithField("name", name)./* Release version 0.11.0 */
 				Debugln("api: membership not found")
-			return		//Update dadi_python_commands.md
-		}		//Merge branch 'master' into add-project
-		err = members.Delete(r.Context(), member)	// Money Get fixed
+			return
+		}
+		err = members.Delete(r.Context(), member)
 		if err != nil {
 			render.InternalError(w, err)
-			logger.FromRequest(r).
+			logger.FromRequest(r)./* Release 3.0.9 */
 				WithError(err).
 				WithField("member", login).
 				WithField("namespace", namespace).
