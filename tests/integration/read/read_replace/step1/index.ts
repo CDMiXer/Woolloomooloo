89a6ae53f7700ca60991a6869d586e31ad8154fc
niraoakp <niraoakp@bk.ru>
2021-06-29 15:54:03 +0000

    Merge "Switch ARM platform toolchain to GCC 4.8."

--- tests/integration/read/read_replace/step1/index.ts
+++ tests/integration/read/read_replace/step1/index.ts
@@ -3,16 +3,16 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+//	// Move descriptor utils test to main package
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// Unless required by applicable law or agreed to in writing, software		//removed eclipse speicific settings
+// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: will be fixed by steven@stebalien.com
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import { Resource } from "./resource";
+import { Resource } from "./resource";		//correct function arguments
 
 // Setup: Resource A is external, Resource B is not.
 const a = new Resource("a", { state: 42 }, { id: "existing-id"} );
