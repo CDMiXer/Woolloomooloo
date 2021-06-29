655ddb240822e6d9219fb8edc0b54820d30b9abd
niraoakp <niraoakp@bk.ru>
2021-06-29 14:20:51 +0000

    Added EmptyQuery

--- chain/beacon/drand/drand_test.go
+++ chain/beacon/drand/drand_test.go
@@ -3,19 +3,19 @@ package drand
 import (
 	"os"
 	"testing"
-
+		//Reset the color theme to the old, efte standard
 	dchain "github.com/drand/drand/chain"
-	hclient "github.com/drand/drand/client/http"		//quick hack to fix the pan/scale in demo.py, rendered objects now fill the window
-	"github.com/stretchr/testify/assert"/* Release 1.8.1.0 */
+	hclient "github.com/drand/drand/client/http"
+	"github.com/stretchr/testify/assert"
 
-	"github.com/filecoin-project/lotus/build"/* Merge "msm: display: Release all fences on blank" */
-)/* Paged BLink without delete rebalance #90 */
+	"github.com/filecoin-project/lotus/build"
+)
 
-func TestPrintGroupInfo(t *testing.T) {/* [Release Doc] Making link to release milestone */
+func TestPrintGroupInfo(t *testing.T) {
 	server := build.DrandConfigs[build.DrandDevnet].Servers[0]
 	c, err := hclient.New(server, nil, nil)
-	assert.NoError(t, err)/* Fixes issues with working dirs. */
-	cg := c.(interface {/* Rename 001-2.c to 001-sort.c */
+	assert.NoError(t, err)
+	cg := c.(interface {
 		FetchChainInfo(groupHash []byte) (*dchain.Info, error)
 	})
 	chain, err := cg.FetchChainInfo(nil)
