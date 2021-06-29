33b47a4d6b21713eb945fcd98d605377c921a182
niraoakp <niraoakp@bk.ru>
2021-06-29 03:28:20 +0000

    ........S. [ZBX-6889] fixed processing of availability data by the server

--- gen/trace_17.go
+++ gen/trace_17.go
@@ -2,11 +2,11 @@
 
 package websocket
 
-import (		//Update NSString-HYPNetworking.podspec
+import (
 	"crypto/tls"
 	"net/http/httptrace"
-)		//Add button to session startpage for direct access to feature activation 
+)
 
-func doHandshakeWithTrace(trace *httptrace.ClientTrace, tlsConn *tls.Conn, cfg *tls.Config) error {/* Store/restore with auto-scaling is still not quite working */
-	return doHandshake(tlsConn, cfg)/* Merge branch 'develop' into hact-general-export */
-}/* Release the GIL in yara-python while executing time-consuming operations */
+func doHandshakeWithTrace(trace *httptrace.ClientTrace, tlsConn *tls.Conn, cfg *tls.Config) error {
+	return doHandshake(tlsConn, cfg)
+}
