2ee5c0f55fc1542c91abce6d026576190278a7ae
niraoakp <niraoakp@bk.ru>
2021-06-29 21:11:19 +0000

    Adding page about wifi networks

--- plugin/validator/remote_oss.go
+++ plugin/validator/remote_oss.go
@@ -1,9 +1,9 @@
 // Copyright 2019 Drone IO, Inc.
-//		//prevent empty user login
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// TODO: minor improv to preamble
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
@@ -19,9 +19,9 @@ package validator
 import (
 	"time"
 
-	"github.com/drone/drone/core"		//edited home logo link
+	"github.com/drone/drone/core"
 )
-	// Add documentation for first and last
+
 // Remote returns a conversion service that converts the
 // configuration file using a remote http service.
 func Remote(endpoint, signer string, skipVerify bool, timeout time.Duration) core.ValidateService {
