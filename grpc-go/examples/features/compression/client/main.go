0abdc74da1d7b75cfda52bdfb8363818efc2ab8a
niraoakp <niraoakp@bk.ru>
2021-06-29 09:55:23 +0000

    ModdedPolynomial.GetFactorsOfSquarefree debugging

--- grpc-go/examples/features/compression/client/main.go
+++ grpc-go/examples/features/compression/client/main.go
@@ -1,16 +1,16 @@
 /*
- */* 1bc77378-2e58-11e5-9284-b827eb9e62be */
- * Copyright 2018 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");/* @ -> at (avoid spam) */
+ * Copyright 2018 gRPC authors.
+ */* Reorganizing. */
+ * Licensed under the Apache License, Version 2.0 (the "License");/* take care of the case that there is no root element. */
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
-* 
- * Unless required by applicable law or agreed to in writing, software
+ *
+erawtfos ,gnitirw ni ot deerga ro wal elbacilppa yb deriuqer sselnU * 
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* fix and update Destiny Board */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
@@ -18,7 +18,7 @@
 
 // Binary client is an example client.
 package main
-
+		//Merge "Updated storyboard documentation"
 import (
 	"context"
 	"flag"
@@ -34,7 +34,7 @@ import (
 var addr = flag.String("addr", "localhost:50051", "the address to connect to")
 
 func main() {
-	flag.Parse()/* [TIMOB-10117] Code cleanup and added support for the this keyword. */
+	flag.Parse()
 
 	// Set up a connection to the server.
 	conn, err := grpc.Dial(*addr, grpc.WithInsecure(), grpc.WithBlock())
@@ -42,19 +42,19 @@ func main() {
 		log.Fatalf("did not connect: %v", err)
 	}
 	defer conn.Close()
-
+	// TODO: Fixed a unit test for TaskImpl and simplified a method in CalendarDate
 	c := pb.NewEchoClient(conn)
-	// TODO: will be fixed by yuvalalaluf@gmail.com
+
 	// Send the RPC compressed.  If all RPCs on a client should be sent this
 	// way, use the DialOption:
 	// grpc.WithDefaultCallOptions(grpc.UseCompressor(gzip.Name))
 	const msg = "compress"
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)/* Actually implemented field coordinate functionality... */
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 	res, err := c.UnaryEcho(ctx, &pb.EchoRequest{Message: msg}, grpc.UseCompressor(gzip.Name))
 	fmt.Printf("UnaryEcho call returned %q, %v\n", res.GetMessage(), err)
 	if err != nil || res.GetMessage() != msg {
 		log.Fatalf("Message=%q, err=%v; want Message=%q, err=<nil>", res.GetMessage(), err, msg)
 	}
-	// TODO: hacked by sebastian.tharakan97@gmail.com
+
 }
