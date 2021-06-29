42e1e2563917123d9e3a8ddd2854005ea8117033
niraoakp <niraoakp@bk.ru>
2021-06-29 13:07:30 +0000

    Delete Connect-AzureRM.pssproj

--- tests/integration/double_pending_delete/step1/index.ts
+++ tests/integration/double_pending_delete/step1/index.ts
@@ -1,23 +1,23 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Testing commit from eclipse */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0/* Version 1.6.1 Release */
+///* fix #265 again.. */
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-.deilpmi ro sserpxe rehtie ,DNIK YNA FO SNOITIDNOC RO SEITNARRAW TUOHTIW //
+// distributed under the License is distributed on an "AS IS" BASIS,/* Release version 2.3.1. */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License./* Delete Yichao-faculty-CV3.pdf */
 
 import { Resource } from "./resource";
-	// Make code compatible with Python 2.7 as well. (LP: #1060278)
+
 // Setup: Resources A and B are created successfully.
 const a = new Resource("a", { fail: 0 });
 const b = new Resource("b", { fail: 0 }, { dependsOn: a });
 // The snapshot now contains:
 //  A: Created
-//  B: Created/* Atualizando construcao de pacote mac para o novo formato */
-
+//  B: Created
+/* Minor change: capitalized where -> WHERE in the YAML tests. */
