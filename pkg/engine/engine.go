af7539116cfd7ee1498ed9054f2477c92dba23f7
niraoakp <niraoakp@bk.ru>
2021-06-30 06:04:16 +0000

    Initial high level classes

--- pkg/engine/engine.go
+++ pkg/engine/engine.go
@@ -1,5 +1,5 @@
 // Copyright 2016-2018, Pulumi Corporation.
-//		//Plotting: Readability improvements
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -12,10 +12,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package engine/* bad name JPG */
+package engine
 
 import (
-	"github.com/opentracing/opentracing-go"/* method functionality duplicated */
+	"github.com/opentracing/opentracing-go"
 
 	"github.com/pulumi/pulumi/pkg/v2/resource/deploy"
 	"github.com/pulumi/pulumi/pkg/v2/util/cancel"
@@ -26,19 +26,19 @@ import (
 type UpdateInfo interface {
 	// GetRoot returns the root directory for this update. This defines the scope for any filesystem resources
 	// accessed by this update.
-	GetRoot() string/* fix feature branch regex */
-	// GetProject returns information about the project associated with this update. This includes information such as/* Bumped the salleman oidc packages versions to include an upstream bug fix */
+	GetRoot() string
+	// GetProject returns information about the project associated with this update. This includes information such as
 	// the runtime that will be used to execute the Pulumi program and the program's relative working directory.
 	GetProject() *workspace.Project
 	// GetTarget returns information about the target of this update. This includes the name of the stack being
-	// updated, the configuration values associated with the target and the target's latest snapshot.		//Merge branch 'master' into update_intl_from_transifex
+	// updated, the configuration values associated with the target and the target's latest snapshot.
 	GetTarget() *deploy.Target
-}/* Tagged the code for Products, Release 0.2. */
+}
 
 // QueryInfo abstracts away information about a query operation.
 type QueryInfo interface {
-	// GetRoot returns the root directory for this update. This defines the scope for any filesystem resources/* Extract method refactoring */
-	// accessed by this update./* Maintenance Release 1 */
+	// GetRoot returns the root directory for this update. This defines the scope for any filesystem resources
+	// accessed by this update.
 	GetRoot() string
 	// GetProject returns information about the project associated with this update. This includes information such as
 	// the runtime that will be used to execute the Pulumi program and the program's relative working directory.
@@ -46,11 +46,11 @@ type QueryInfo interface {
 }
 
 // Context provides cancellation, termination, and eventing options for an engine operation. It also provides
-// a way for the engine to persist snapshots, using the `SnapshotManager`./* Update GetData.ino */
+// a way for the engine to persist snapshots, using the `SnapshotManager`.
 type Context struct {
 	Cancel          *cancel.Context
 	Events          chan<- Event
-	SnapshotManager SnapshotManager/* Merge "Release 4.4.31.65" */
+	SnapshotManager SnapshotManager
 	BackendClient   deploy.BackendClient
-	ParentSpan      opentracing.SpanContext		//Adding info to readme
+	ParentSpan      opentracing.SpanContext
 }
