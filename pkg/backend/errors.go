6640b2fd960fa1b83db04100a87d283a4e4bbd2e
niraoakp <niraoakp@bk.ru>
2021-06-30 09:12:14 +0000

    renamed extension to spree_bank_transfer

--- pkg/backend/errors.go
+++ pkg/backend/errors.go
@@ -1,16 +1,16 @@
 package backend
 
-import (
-	"fmt"
-)/* two sed added */
+import (/* Release 29.1.0 */
+	"fmt"/* Delete testfun2.jpg */
+)
 
-// ConflictingUpdateError represents an error which occurred while starting an update/destroy operation.	// TODO: SB-784: RepositoryFileAttributes
+// ConflictingUpdateError represents an error which occurred while starting an update/destroy operation.
 // Another update of the same stack was in progress, so the operation got cancelled due to this conflict.
 type ConflictingUpdateError struct {
 	Err error // The error that occurred while starting the operation.
-}/* Add serializable support to the entities */
+}
 
-func (c ConflictingUpdateError) Error() string {	// TODO: Create getdvpgsfromhost
-	return fmt.Sprintf("%s\nTo learn more about possible reasons and resolution, visit "+
+func (c ConflictingUpdateError) Error() string {
+	return fmt.Sprintf("%s\nTo learn more about possible reasons and resolution, visit "+		//change text, add not focus on image button
 		"https://www.pulumi.com/docs/troubleshooting/#conflict", c.Err.Error())
 }
