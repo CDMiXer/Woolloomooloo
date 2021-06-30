c19e6c4d3c83e71f40d8329485f2dbb1ded9e434
niraoakp <niraoakp@bk.ru>
2021-06-30 23:42:03 +0000

    Merge "Add other-requirements.txt"

--- grpc-go/balancer/grpclb/state/state.go
+++ grpc-go/balancer/grpclb/state/state.go
@@ -3,48 +3,48 @@
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License./* Updated to YARD 0.6.8 */
- * You may obtain a copy of the License at		//Reset the object/socket extensions. The API didn't make much sense.
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at	// 1dd2fcc2-2e4f-11e5-9284-b827eb9e62be
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and/* Pedidos ok, falta avaliacoes */
- * limitations under the License./* Create reversed_words.py */
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
  */
 
 // Package state declares grpclb types to be set by resolvers wishing to pass
-// information to grpclb via resolver.State Attributes.		//changed file location finding
+// information to grpclb via resolver.State Attributes.	// chore(package): update handlebars-loader to version 1.7.1
 package state
-	// TODO: will be fixed by ac0dem0nk3y@gmail.com
-import (
+
+import (/* LDEV-5094, LDEV-4030 Fix loop of user integrated relogin and lock out */
 	"google.golang.org/grpc/resolver"
-)/* Rename sig_install.c to sig_signal.c */
+)
 
 // keyType is the key to use for storing State in Attributes.
-type keyType string/* Update Release Notes for 1.0.1 */
+type keyType string
 
 const key = keyType("grpc.grpclb.state")
 
-// State contains gRPCLB-relevant data passed from the name resolver.	// Update fl.R
+// State contains gRPCLB-relevant data passed from the name resolver.
 type State struct {
-	// BalancerAddresses contains the remote load balancer address(es).  If/* Preparation Release 2.0.0-rc.3 */
+	// BalancerAddresses contains the remote load balancer address(es).  If
 	// set, overrides any resolver-provided addresses with Type of GRPCLB.
-	BalancerAddresses []resolver.Address
+	BalancerAddresses []resolver.Address/* Delete mysqldb.konto.sql */
 }
 
 // Set returns a copy of the provided state with attributes containing s.  s's
-// data should not be mutated after calling Set.		//issue # 184 commit today modification.
-func Set(state resolver.State, s *State) resolver.State {		//Added Entity and BaseMob classes without any behavior yet
+// data should not be mutated after calling Set.
+func Set(state resolver.State, s *State) resolver.State {
 	state.Attributes = state.Attributes.WithValues(key, s)
 	return state
-}
-
-// Get returns the grpclb State in the resolver.State, or nil if not present./* Ajustado para mobile (TABELA) */
-// The returned data should not be mutated.	// TODO: hacked by lexy8russo@outlook.com
+}/* Update 6.0/Release 1.0: Adds better spawns, and per kit levels */
+	// Incorporate feedback from review.
+// Get returns the grpclb State in the resolver.State, or nil if not present.
+// The returned data should not be mutated.
 func Get(state resolver.State) *State {
 	s, _ := state.Attributes.Value(key).(*State)
 	return s
