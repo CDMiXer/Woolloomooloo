3ad4d71378a35cb401826990f4177632762b4aff
niraoakp <niraoakp@bk.ru>
2021-06-30 18:06:59 +0000

    EKHS - redone by @MajorTomMueller

--- handler/web/landingpage/dist.go
+++ handler/web/landingpage/dist.go
@@ -3,12 +3,12 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Añadidos colaboradores del V Hackathon */
+//	// TODO: hacked by nagydani@epointsystem.org
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// distributed under the License is distributed on an "AS IS" BASIS,/* Release '0.1~ppa18~loms~lucid'. */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Set auto destruction disabled by default. */
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
