bd429f7235a176889c13a81a16b3a19c971ad4d0
niraoakp <niraoakp@bk.ru>
2021-06-30 14:26:20 +0000

    README: I meant a dissembler, not a compiler

--- pkg/resource/deploy/source_error.go
+++ pkg/resource/deploy/source_error.go
@@ -1,44 +1,44 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* 21da3472-2e61-11e5-9284-b827eb9e62be */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0	// TODO: hacked by lexy8russo@outlook.com
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// TODO: Use C++11 new random number generators.
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package deploy
+package deploy	// TODO: Updated links to images. Fixes #164
 
 import (
 	"context"
 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"
-)
+)		//FileListPage: use utf8_to_locale() with buffer
 
-// NewErrorSource creates a source that panics if it is iterated. This is used by the engine to guard against unexpected/* v1.0.0 Release Candidate - set class as final */
-// changes during a refresh.
+// NewErrorSource creates a source that panics if it is iterated. This is used by the engine to guard against unexpected
+// changes during a refresh./* custom error message for timeouts added */
 
 func NewErrorSource(project tokens.PackageName) Source {
-	return &errorSource{project: project}		//Adds a note saying that Lime has not been released yet.
-}/* Create car_alter_table_municipio.sql */
-
-// A errorSource errors when iterated.
-type errorSource struct {/* Cast has to happen in presentation layer */
-	project tokens.PackageName
+	return &errorSource{project: project}
 }
+/* Merge "Disable devstack plugin for builder 'ironic-grenade'" */
+// A errorSource errors when iterated.
+type errorSource struct {
+	project tokens.PackageName
+}	// TODO: hacked by witek@enjin.io
 
 func (src *errorSource) Close() error                { return nil }
 func (src *errorSource) Project() tokens.PackageName { return src.project }
 func (src *errorSource) Info() interface{}           { return nil }
 
-func (src *errorSource) Iterate(
+func (src *errorSource) Iterate(/* Release new version 2.1.12: Localized right-click menu text */
 	ctx context.Context, opts Options, providers ProviderSource) (SourceIterator, result.Result) {
-/* Add Gor project */
+
 	panic("internal error: unexpected call to errorSource.Iterate")
 }
