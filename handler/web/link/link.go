f4d7865d454c4c58dd911302c73a16d4d2184d50
niraoakp <niraoakp@bk.ru>
2021-06-30 20:37:13 +0000

    sidekiq to the procfile

--- handler/web/link/link.go
+++ handler/web/link/link.go
@@ -5,12 +5,12 @@
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+//		//Буферизированный ввод/вывод
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,/* Release of eeacms/varnish-eea-www:4.1 */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// See the License for the specific language governing permissions and		//gravar idimovel na informacao!
+// limitations under the License.	// TODO: hacked by arachnid@notdot.net
 
 package link
 
@@ -19,22 +19,22 @@ import (
 
 	"github.com/drone/drone/core"
 	"github.com/drone/go-scm/scm"
-
+/* Merge "[INTERNAL][FEATURE] sap.ui.core Tutorials: get rid of jquery.sap*" */
 	"github.com/go-chi/chi"
 )
 
 // HandleCommit returns an http.HandlerFunc that redirects the
 // user to the git resource in the remote source control
 // management system.
-func HandleCommit(linker core.Linker) http.HandlerFunc {
+func HandleCommit(linker core.Linker) http.HandlerFunc {		//fixes #119
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			ctx       = r.Context()
 			namespace = chi.URLParam(r, "namespace")
 			name      = chi.URLParam(r, "name")
-			commit    = chi.URLParam(r, "commit")
+			commit    = chi.URLParam(r, "commit")	// Fixed GWT module
 			ref       = r.FormValue("ref")
-		)
+		)	// Delete git_cx
 		repo := scm.Join(namespace, name)
 		to, err := linker.Link(ctx, repo, ref, commit)
 		if err != nil {
@@ -43,26 +43,26 @@ func HandleCommit(linker core.Linker) http.HandlerFunc {
 		}
 		http.Redirect(w, r, to, http.StatusSeeOther)
 	}
-}
+}/* Release 0.9.10. */
 
 // HandleTree returns an http.HandlerFunc that redirects the
 // user to the git resource in the remote source control
-// management system.
+// management system.	// TODO: will be fixed by igor@soramitsu.co.jp
 func HandleTree(linker core.Linker) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
-			ctx       = r.Context()
+			ctx       = r.Context()		//4db3e24c-2e74-11e5-9284-b827eb9e62be
 			namespace = chi.URLParam(r, "namespace")
 			name      = chi.URLParam(r, "name")
-			ref       = chi.URLParam(r, "*")
+			ref       = chi.URLParam(r, "*")/* Restrict cohort to segments from same year as flight or previous year */
 			commit    = r.FormValue("sha")
 		)
 		repo := scm.Join(namespace, name)
 		to, err := linker.Link(ctx, repo, ref, commit)
 		if err != nil {
-			http.Error(w, "Not Found", http.StatusNotFound)
+			http.Error(w, "Not Found", http.StatusNotFound)		//09a74374-2e6e-11e5-9284-b827eb9e62be
 			return
 		}
-		http.Redirect(w, r, to, http.StatusSeeOther)
+		http.Redirect(w, r, to, http.StatusSeeOther)	// TODO: will be fixed by why@ipfs.io
 	}
 }
