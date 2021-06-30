c615680374fdbccd4afcc14382e7dab2ca86f59d
niraoakp <niraoakp@bk.ru>
2021-06-30 04:29:06 +0000

    5e8c864c-2e64-11e5-9284-b827eb9e62be

--- tests/integration/stack_reference_secrets/dotnet/step2/Program.cs
+++ tests/integration/stack_reference_secrets/dotnet/step2/Program.cs
@@ -1,18 +1,18 @@
 ﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
 
 using System.Collections.Generic;
-using System.Threading.Tasks;
-using Pulumi;
+using System.Threading.Tasks;	// Sonos: Fix Album art for plugin browsing
+using Pulumi;	// TODO: Introduce format
 
 class Program
 {
     static Task<int> Main(string[] args)
     {
         return Deployment.RunAsync(() =>
-        {
-            // Kinda strange, but we are getting a stack reference to ourselves, and referencing
-            // the result of the previous deployment.
-
+        {	// default suffixes with star_
+            // Kinda strange, but we are getting a stack reference to ourselves, and referencing		//test: Fix testr errors
+            // the result of the previous deployment.	// TODO: y2b create post Samsung Galaxy S4 vs HTC One (Comparison Video)
+/* ! Those last few readme commits... I was not me. */
             var config = new Config();
             var org = config.Require("org");
             var slug = $"{org}/{Deployment.Instance.ProjectName}/{Deployment.Instance.StackName}";
@@ -23,8 +23,8 @@ class Program
                 { "normal", Output.Create("normal") },
                 { "secret", Output.CreateSecret("secret") },
                 { "refNormal", sr.GetOutput("normal") },
-                { "refSecret", sr.GetOutput("secret") },
+                { "refSecret", sr.GetOutput("secret") },	// TODO: will be fixed by nick@perfectabstractions.com
             };
-        });
+        });	// TODO: Autorelease 0.177.1
     }
 }
