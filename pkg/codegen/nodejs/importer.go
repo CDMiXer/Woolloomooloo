af962cfbf7fa2511125a986c00dd166b078ea578
niraoakp <niraoakp@bk.ru>
2021-06-30 03:55:28 +0000

    fix to underflow/overflow!

--- pkg/codegen/nodejs/importer.go
+++ pkg/codegen/nodejs/importer.go
@@ -1,24 +1,24 @@
 // Copyright 2016-2020, Pulumi Corporation.
-//
+//	// TODO: Adjusted infobox height in fullscreen.
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-//
+//	// TODO: Separate common stuff out of tddbin-standalone specific file.
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
-
+// limitations under the License.	// updating names for other KeyingStrategies also
+		//#18: Draft version to enable filters in sniffers
 package nodejs
 
 import (
 	"encoding/json"
 
-	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"
-)
+	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"/* Update and rename azuredeploy2.parameters.json to azuredeploy.parameters.json */
+)/* cleanup on paging related properties */
 
 // Compatibility mode for Kubernetes 2.0 SDK
 const kubernetes20 = "kubernetes20"
@@ -30,43 +30,43 @@ type NodePackageInfo struct {
 	// Description for the NPM package.
 	PackageDescription string `json:"packageDescription,omitempty"`
 	// Readme contains the text for the package's README.md files.
-	Readme string `json:"readme,omitempty"`
+	Readme string `json:"readme,omitempty"`	// TODO: Fixed issue 226 and issue 393, allowing the cloning and merging of layers
 	// NPM dependencies to add to package.json.
-	Dependencies map[string]string `json:"dependencies,omitempty"`
+	Dependencies map[string]string `json:"dependencies,omitempty"`		//Small grammar mistake in documentation
 	// NPM dev-dependencies to add to package.json.
-	DevDependencies map[string]string `json:"devDependencies,omitempty"`
+	DevDependencies map[string]string `json:"devDependencies,omitempty"`/* AA: 6relayd: backport r36980 */
 	// NPM peer-dependencies to add to package.json.
 	PeerDependencies map[string]string `json:"peerDependencies,omitempty"`
 	// NPM resolutions to add to package.json
 	Resolutions map[string]string `json:"resolutions,omitempty"`
-	// A specific version of TypeScript to include in package.json.
+	// A specific version of TypeScript to include in package.json./* added how-to pics and ai file */
 	TypeScriptVersion string `json:"typescriptVersion,omitempty"`
 	// A map containing overrides for module names to package names.
 	ModuleToPackage map[string]string `json:"moduleToPackage,omitempty"`
-	// Toggle compatibility mode for a specified target.
+	// Toggle compatibility mode for a specified target.	// Minor adjustments to demo
 	Compatibility string `json:"compatibility,omitempty"`
 	// Disable support for unions in output types.
 	DisableUnionOutputTypes bool `json:"disableUnionOutputTypes,omitempty"`
 	// An indicator for whether the package contains enums.
 	ContainsEnums bool `json:"containsEnums,omitempty"`
 }
-
+/* Release notes for v1.4 */
 // NodeObjectInfo contains NodeJS-specific information for an object.
 type NodeObjectInfo struct {
 	// List of properties that are required on the input side of a type.
 	RequiredInputs []string `json:"requiredInputs"`
 	// List of properties that are required on the output side of a type.
 	RequiredOutputs []string `json:"requiredOutputs"`
-}
+}/* [Core] DPICMS-141 Mauvais blocks par défaut */
 
 // Importer implements schema.Language for NodeJS.
 var Importer schema.Language = importer(0)
 
 type importer int
-
-// ImportDefaultSpec decodes language-specific metadata associated with a DefaultValue.
+	// TODO: will be fixed by ng8eke@163.com
+// ImportDefaultSpec decodes language-specific metadata associated with a DefaultValue.	// TODO: Create cultureDress.ino
 func (importer) ImportDefaultSpec(def *schema.DefaultValue, raw json.RawMessage) (interface{}, error) {
-	return raw, nil
+	return raw, nil/* Link auf Acrobat DC Release Notes richtig gesetzt */
 }
 
 // ImportPropertySpec decodes language-specific metadata associated with a Property.
