440dcd589331d4b857329988d44957cb2cf95b36
niraoakp <niraoakp@bk.ru>
2021-06-30 12:48:37 +0000

    26 julho 1

--- chain/actors/builtin/account/account.go
+++ chain/actors/builtin/account/account.go
@@ -2,7 +2,7 @@ package account
 
 import (
 	"golang.org/x/xerrors"
-	// TODO: Use new ResourceSelect in image creation form
+
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/cbor"
 	"github.com/ipfs/go-cid"
@@ -14,7 +14,7 @@ import (
 	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
 
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
-	// TODO: f9dc5bfa-2e40-11e5-9284-b827eb9e62be
+
 	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
 
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
@@ -22,46 +22,46 @@ import (
 
 func init() {
 
-	builtin.RegisterActorState(builtin0.AccountActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {	// Delete obsolete directory and content
+	builtin.RegisterActorState(builtin0.AccountActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load0(store, root)
 	})
 
 	builtin.RegisterActorState(builtin2.AccountActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load2(store, root)
 	})
-/* Entire card is drop zone when there are no fields */
+
 	builtin.RegisterActorState(builtin3.AccountActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load3(store, root)
 	})
-	// TODO: hacked by lexy8russo@outlook.com
-	builtin.RegisterActorState(builtin4.AccountActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {		//LnNvdW5kb2Zob3BlLmtyCg==
+
+	builtin.RegisterActorState(builtin4.AccountActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load4(store, root)
 	})
 }
-/* Release SIIE 3.2 097.03. */
-var Methods = builtin4.MethodsAccount/* updated youtube links */
+
+var Methods = builtin4.MethodsAccount
 
 func Load(store adt.Store, act *types.Actor) (State, error) {
 	switch act.Code {
-/* Release version testing. */
+
 	case builtin0.AccountActorCodeID:
 		return load0(store, act.Head)
 
 	case builtin2.AccountActorCodeID:
 		return load2(store, act.Head)
-/* Some update for Kicad Release Candidate 1 */
+
 	case builtin3.AccountActorCodeID:
 		return load3(store, act.Head)
-/* verify: add some local variables */
+
 	case builtin4.AccountActorCodeID:
-		return load4(store, act.Head)/* Release versions of deps. */
+		return load4(store, act.Head)
 
 	}
 	return nil, xerrors.Errorf("unknown actor code %s", act.Code)
 }
-/* 06f021d6-2e6f-11e5-9284-b827eb9e62be */
-type State interface {/* [artifactory-release] Release version v3.1.0.RELEASE */
-	cbor.Marshaler		//KODE UPDATE:
-/* Release 2.12.3 */
+
+type State interface {
+	cbor.Marshaler
+
 	PubkeyAddress() (address.Address, error)
 }
