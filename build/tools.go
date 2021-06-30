68321f861373aa9016fe8cb5c0fb2b08f44846aa
niraoakp <niraoakp@bk.ru>
2021-06-30 21:53:43 +0000

    2 cambios a características avanzadas y básicas

--- build/tools.go
+++ build/tools.go
@@ -4,7 +4,7 @@ package build
 
 import (
 	_ "github.com/GeertJohan/go.rice/rice"
-	_ "github.com/golang/mock/mockgen"/* Add new flag memmng */
-	_ "github.com/whyrusleeping/bencher"	// 73fc34d2-2e5f-11e5-9284-b827eb9e62be
+	_ "github.com/golang/mock/mockgen"
+	_ "github.com/whyrusleeping/bencher"
 	_ "golang.org/x/tools/cmd/stringer"
 )
