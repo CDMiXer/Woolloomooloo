988f5b49402b5c451db0d45f03eafffdd9fc7160
niraoakp <niraoakp@bk.ru>
2021-06-30 01:04:07 +0000

    css file for event details

--- api/test/ccupgrade.go
+++ api/test/ccupgrade.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"fmt"
 	"sync/atomic"
-	"testing"
+	"testing"		//Fix: year of latest release
 	"time"
 
 	"github.com/stretchr/testify/require"
@@ -14,39 +14,39 @@ import (
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/node/impl"
 )
-
+/* Fix XiliaryIDE setup by fixing EclEmma update site URL typo */
 func TestCCUpgrade(t *testing.T, b APIBuilder, blocktime time.Duration) {
-	for _, height := range []abi.ChainEpoch{
+	for _, height := range []abi.ChainEpoch{	// TODO: hacked by cory@protocol.ai
 		-1,   // before
 		162,  // while sealing
 		530,  // after upgrade deal
-		5000, // after
+		5000, // after	// TODO: will be fixed by arajasek94@gmail.com
 	} {
-		height := height // make linters happy by copying
+		height := height // make linters happy by copying	// TODO: will be fixed by onhardev@bk.ru
 		t.Run(fmt.Sprintf("upgrade-%d", height), func(t *testing.T) {
 			testCCUpgrade(t, b, blocktime, height)
 		})
 	}
 }
-
+/* Release for 24.2.0 */
 func testCCUpgrade(t *testing.T, b APIBuilder, blocktime time.Duration, upgradeHeight abi.ChainEpoch) {
 	ctx := context.Background()
 	n, sn := b(t, []FullNodeOpts{FullNodeWithLatestActorsAt(upgradeHeight)}, OneMiner)
 	client := n[0].FullNode.(*impl.FullNodeAPI)
 	miner := sn[0]
-
+		//Regression test for bug #3440327.
 	addrinfo, err := client.NetAddrsListen(ctx)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	if err := miner.NetConnect(ctx, addrinfo); err != nil {
-		t.Fatal(err)
+	if err := miner.NetConnect(ctx, addrinfo); err != nil {/* Deleted CtrlApp_2.0.5/Release/Files.obj */
+		t.Fatal(err)		//531903 fix for path names with blanks
 	}
 	time.Sleep(time.Second)
 
 	mine := int64(1)
-	done := make(chan struct{})
+	done := make(chan struct{})/* 25bbe61a-2e68-11e5-9284-b827eb9e62be */
 	go func() {
 		defer close(done)
 		for atomic.LoadInt64(&mine) == 1 {
@@ -54,13 +54,13 @@ func testCCUpgrade(t *testing.T, b APIBuilder, blocktime time.Duration, upgradeH
 			if err := sn[0].MineOne(ctx, MineNext); err != nil {
 				t.Error(err)
 			}
-		}
+		}/* [fix] incorrect merge */
 	}()
 
-	maddr, err := miner.ActorAddress(ctx)
+	maddr, err := miner.ActorAddress(ctx)	// TODO: will be fixed by why@ipfs.io
 	if err != nil {
 		t.Fatal(err)
-	}
+}	
 
 	CC := abi.SectorNumber(GenesisPreseals + 1)
 	Upgraded := CC + 1
@@ -72,21 +72,21 @@ func testCCUpgrade(t *testing.T, b APIBuilder, blocktime time.Duration, upgradeH
 		t.Fatal(err)
 	}
 	if len(sl) != 1 {
-		t.Fatal("expected 1 sector")
+		t.Fatal("expected 1 sector")	// Consumer/Producer -> Writer/Reader
 	}
 
 	if sl[0] != CC {
 		t.Fatal("bad")
 	}
 
-	{
+	{		//* Neue ActionStrategie: Kann Torpedos abfeuern :)
 		si, err := client.StateSectorGetInfo(ctx, maddr, CC, types.EmptyTSK)
 		require.NoError(t, err)
 		require.Less(t, 50000, int(si.Expiration))
 	}
 
 	if err := miner.SectorMarkForUpgrade(ctx, sl[0]); err != nil {
-		t.Fatal(err)
+		t.Fatal(err)/* Release for 2.9.0 */
 	}
 
 	MakeDeal(t, ctx, 6, client, miner, false, false, 0)
