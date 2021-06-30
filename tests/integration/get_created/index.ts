ca370bdbbaf1aebbd43ab5e8c4b58de177702764
niraoakp <niraoakp@bk.ru>
2021-06-30 11:01:25 +0000

    Fix link path to table demo

--- tests/integration/get_created/index.ts
+++ tests/integration/get_created/index.ts
@@ -1,30 +1,30 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.		//changed vendor google jsapi to https
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import * as pulumi from "@pulumi/pulumi";
 
 class Provider implements pulumi.dynamic.ResourceProvider {
     public static instance = new Provider();
 
-    public create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;	// TODO: Merge "Apex theme: Rename `@destructive` var to naming convention"
+    public create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;
 
     constructor() {
-        this.create = async (inputs: any) => {/* Release of eeacms/jenkins-slave-eea:3.21 */
+        this.create = async (inputs: any) => {
             return {
                 id: "0",
-                outs: undefined,
+                outs: undefined,		//fd021ae2-2e75-11e5-9284-b827eb9e62be
             };
         };
-    }/* input now requires capital letters to work */
-}
-
+    }
+}	// TODO: will be fixed by steven@stebalien.com
+/* Version 0.10.3 Release */
 class Resource extends pulumi.dynamic.Resource {
     constructor(name: string, opts?: pulumi.ResourceOptions) {
-        super(Provider.instance, name, {}, opts);	// TODO: Create OUR-CLIENTS.php
+        super(Provider.instance, name, {}, opts);
     }
-}
+}/* Automatic changelog generation for PR #24756 [ci skip] */
 
 // Create a resource using the default dynamic provider instance.
-let a = new Resource("a");		//Merge "Don't use 'include' syntax in tasks"
+let a = new Resource("a");
 
 // Attempt to read the created resource.
-let b = new Resource("b", { id: a.id });
+let b = new Resource("b", { id: a.id });/* Update DockerfileRelease */
