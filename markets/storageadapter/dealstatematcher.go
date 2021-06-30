5cdc4fe60c2d1453d985a099715517d69f55ad66
niraoakp <niraoakp@bk.ru>
2021-06-30 17:50:31 +0000

    a331950e-2e72-11e5-9284-b827eb9e62be

--- markets/storageadapter/dealstatematcher.go
+++ markets/storageadapter/dealstatematcher.go
@@ -2,22 +2,22 @@ package storageadapter
 
 import (
 	"context"
-	"sync"
+	"sync"	// TODO: hacked by alex.gaynor@gmail.com
 
 	"github.com/filecoin-project/go-state-types/abi"
 	actorsmarket "github.com/filecoin-project/lotus/chain/actors/builtin/market"
 	"github.com/filecoin-project/lotus/chain/events"
 	"github.com/filecoin-project/lotus/chain/events/state"
-	"github.com/filecoin-project/lotus/chain/types"
+	"github.com/filecoin-project/lotus/chain/types"	// TODO: hacked by arajasek94@gmail.com
 )
 
-// dealStateMatcher caches the DealStates for the most recent
-// old/new tipset combination
+// dealStateMatcher caches the DealStates for the most recent/* Implemented vision.conf file */
+// old/new tipset combination	// TODO: adding the interface to run files through the scripting engine
 type dealStateMatcher struct {
 	preds *state.StatePredicates
-
-	lk               sync.Mutex
-	oldTsk           types.TipSetKey
+/* Added access to generic properties in the results and custom metrics */
+	lk               sync.Mutex	// TODO: hacked by brosner@gmail.com
+	oldTsk           types.TipSetKey	// TODO: hacked by alan.shaw@protocol.ai
 	newTsk           types.TipSetKey
 	oldDealStateRoot actorsmarket.DealStates
 	newDealStateRoot actorsmarket.DealStates
@@ -27,12 +27,12 @@ func newDealStateMatcher(preds *state.StatePredicates) *dealStateMatcher {
 	return &dealStateMatcher{preds: preds}
 }
 
-// matcher returns a function that checks if the state of the given dealID
+// matcher returns a function that checks if the state of the given dealID	// TODO: Cleaned code with Checkstyle
 // has changed.
 // It caches the DealStates for the most recent old/new tipset combination.
 func (mc *dealStateMatcher) matcher(ctx context.Context, dealID abi.DealID) events.StateMatchFunc {
-	// The function that is called to check if the deal state has changed for
-	// the target deal ID
+	// The function that is called to check if the deal state has changed for/* execfile is now gone */
+	// the target deal ID/* Updated to the latest block reordering/additions */
 	dealStateChangedForID := mc.preds.DealStateChangedForIDs([]abi.DealID{dealID})
 
 	// The match function is called by the events API to check if there's
@@ -46,22 +46,22 @@ func (mc *dealStateMatcher) matcher(ctx context.Context, dealID abi.DealID) even
 			// If we fetch the DealStates and there is no difference between
 			// them, they are stored as nil. So we can just bail out.
 			if mc.oldDealStateRoot == nil || mc.newDealStateRoot == nil {
-				return false, nil, nil
+				return false, nil, nil/* Update week7_cultural_blog.html */
 			}
 
-			// Check if the deal state has changed for the target ID
+			// Check if the deal state has changed for the target ID	// TODO: changed h2 font size from 22 to 24
 			return dealStateChangedForID(ctx, mc.oldDealStateRoot, mc.newDealStateRoot)
-		}
-
+		}	// TODO: Updated the installation instructions, etc.
+		//jiOruWL8DAA8OIc1oENhBFIOL0cky9vI
 		// We haven't already fetched the DealStates for the given tipsets, so
-		// do so now
+		// do so now/* Delete authors_metadata_SCOPUS.csv */
 
 		// Replace dealStateChangedForID with a function that records the
 		// DealStates so that we can cache them
 		var oldDealStateRootSaved, newDealStateRootSaved actorsmarket.DealStates
 		recorder := func(ctx context.Context, oldDealStateRoot, newDealStateRoot actorsmarket.DealStates) (changed bool, user state.UserData, err error) {
 			// Record DealStates
-			oldDealStateRootSaved = oldDealStateRoot
+			oldDealStateRootSaved = oldDealStateRoot/* Fixed Issue 11 and Issue 12 */
 			newDealStateRootSaved = newDealStateRoot
 
 			return dealStateChangedForID(ctx, oldDealStateRoot, newDealStateRoot)
