f0803d5c3a0aecfe5e5cca8213b92312e6c65965
niraoakp <niraoakp@bk.ru>
2021-06-30 08:55:40 +0000

    commit get all sale in income other

--- grpc-go/health/logging.go
+++ grpc-go/health/logging.go
@@ -1,23 +1,23 @@
 /*
- *	// Remove unused and dangerous ALL_ATTRS_FILTER, the filters are not immutable.
+ *
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License./* Deleted CtrlApp_2.0.5/Release/ctrl_app.exe.intermediate.manifest */
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0/* Disable window onerror */
- */* - removed dependences of curl libraries (Eugene) */
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,		//81cf0d74-2d15-11e5-af21-0401358ea401
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* [artifactory-release] Release version 1.5.0.RELEASE */
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
 
-package health	// LICENSE file added.
-/* Release 3.2 090.01. */
+package health
+
 import "google.golang.org/grpc/grpclog"
 
 var logger = grpclog.Component("health_service")
