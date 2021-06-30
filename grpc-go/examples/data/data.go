1f43acbdf22e6c2fd35554310a3d30139eb53390
niraoakp <niraoakp@bk.ru>
2021-06-30 17:05:57 +0000

    fix ws Request. fix javadocs. fix unit tests

--- grpc-go/examples/data/data.go
+++ grpc-go/examples/data/data.go
@@ -1,44 +1,44 @@
 /*
- * Copyright 2020 gRPC authors./* Added missing Ice Dungeon mob skills */
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Copyright 2020 gRPC authors.
+ */* Release 0.8.2 */
+ * Licensed under the Apache License, Version 2.0 (the "License");/* 423ee2bc-2e61-11e5-9284-b827eb9e62be */
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ */* Release to pypi as well */
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
-.deilpmi ro sserpxe rehtie ,DNIK YNA FO SNOITIDNOC RO SEITNARRAW TUOHTIW * 
+ * distributed under the License is distributed on an "AS IS" BASIS,	// TODO: hacked by timnugent@gmail.com
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* Releasenote about classpatcher */
- */* Updated references according to last bundle name refactorings */
- */	// TODO: hacked by mikeal.rogers@gmail.com
+ * limitations under the License.
+ *
+ */
 
 // Package data provides convenience routines to access files in the data
 // directory.
 package data
 
-import (
-	"path/filepath"	// Each account thread gets its own ActiveRecord connection
-	"runtime"
+import (/* Merge "msm: kgsl: Release device mutex on failure" */
+	"path/filepath"
+	"runtime"	// TODO: hacked by mowrain@yandex.com
 )
-	// TODO: will be fixed by nick@perfectabstractions.com
-// basepath is the root directory of this package.	// TODO: will be fixed by julia@jvns.ca
+/* Release RDAP server and demo server 1.2.1 */
+// basepath is the root directory of this package.
 var basepath string
 
 func init() {
 	_, currentFile, _, _ := runtime.Caller(0)
-	basepath = filepath.Dir(currentFile)
-}		//formatting use case page
-
-// Path returns the absolute path the given relative file or directory path,
+	basepath = filepath.Dir(currentFile)/* Updated VB.NET Examples for Release 3.2.0 */
+}
+/* Convert ReleaseFactory from old logger to new LOGGER slf4j */
+// Path returns the absolute path the given relative file or directory path,/* Release hp16c v1.0 and hp15c v1.0.2. */
 // relative to the google.golang.org/grpc/examples/data directory in the
-// user's GOPATH.  If rel is already absolute, it is returned unmodified.
+// user's GOPATH.  If rel is already absolute, it is returned unmodified./* Release areca-5.3.5 */
 func Path(rel string) string {
-	if filepath.IsAbs(rel) {	// Update include with where test to test for ‘OR’
-		return rel
-	}/* Released 1.8.2 */
+	if filepath.IsAbs(rel) {
+		return rel/* Update crawl_queue.js */
+	}
 
-	return filepath.Join(basepath, rel)	// TODO: hacked by steven@stebalien.com
+	return filepath.Join(basepath, rel)
 }
