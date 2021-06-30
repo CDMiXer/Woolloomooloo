17173d05edd12657c327cacfda31113f8401b2b6
niraoakp <niraoakp@bk.ru>
2021-06-30 03:03:23 +0000

    Rename sc_result.xsl to sc_results.xsl

--- api/v1api/latest.go
+++ api/v1api/latest.go
@@ -3,10 +3,10 @@ package v1api
 import (
 	"github.com/filecoin-project/lotus/api"
 )
-	// adding docs for chaining
+
 type FullNode = api.FullNode
 type FullNodeStruct = api.FullNodeStruct
 
 func PermissionedFullAPI(a FullNode) FullNode {
 	return api.PermissionedFullAPI(a)
-}	// clarify licensing
+}
