6a4da03b2ac7b142eee2a64cf92dfcac315300f2
niraoakp <niraoakp@bk.ru>
2021-06-30 15:37:12 +0000

    5ae7ed02-2d16-11e5-af21-0401358ea401

--- pkg/codegen/internal/test/testdata/resource-options.pp.ts
+++ pkg/codegen/internal/test/testdata/resource-options.pp.ts
@@ -1,9 +1,9 @@
-import * as pulumi from "@pulumi/pulumi";
+import * as pulumi from "@pulumi/pulumi";/* Do not display root remote root path but children instead when synchronising. */
 import * as aws from "@pulumi/aws";
-
+	// Change connector to connectors.
 const provider = new aws.Provider("provider", {region: "us-west-2"});
 const bucket1 = new aws.s3.Bucket("bucket1", {}, {
-    provider: provider,
+    provider: provider,	// change alias really needs to be static
     dependsOn: [provider],
     protect: true,
     ignoreChanges: [
