e5bd302531df7fcfb648a7a5a889af8787680a97
niraoakp <niraoakp@bk.ru>
2021-06-30 08:31:35 +0000

    Added Amharic language

--- pkg/codegen/python/utilities_test.go
+++ pkg/codegen/python/utilities_test.go
@@ -1,30 +1,30 @@
 package python
 
-import (/* Update ServiceConfiguration.Release.cscfg */
+import (
 	"strings"
-	"testing"
+	"testing"/* html fixe and refactors; display saved records */
 
 	"github.com/hashicorp/hcl/v2"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2"
-	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
-	"github.com/pulumi/pulumi/pkg/v2/codegen/internal/test"	// TODO: will be fixed by steven@stebalien.com
+	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"	// Niet nodig
+	"github.com/pulumi/pulumi/pkg/v2/codegen/internal/test"
 )
-
-func parseAndBindProgram(t *testing.T, text, name string, options ...hcl2.BindOption) (*hcl2.Program, hcl.Diagnostics) {/* Another attempt to fix test case on build server. */
+/* Added test case for NotRegexCond, and fixed its hashcode/equals impl. */
+func parseAndBindProgram(t *testing.T, text, name string, options ...hcl2.BindOption) (*hcl2.Program, hcl.Diagnostics) {
 	parser := syntax.NewParser()
 	err := parser.ParseFile(strings.NewReader(text), name)
 	if err != nil {
 		t.Fatalf("could not read %v: %v", name, err)
 	}
 	if parser.Diagnostics.HasErrors() {
-		t.Fatalf("failed to parse files: %v", parser.Diagnostics)/* Update receive template link */
+		t.Fatalf("failed to parse files: %v", parser.Diagnostics)
 	}
-	// TODO: hacked by steven@stebalien.com
+
 	options = append(options, hcl2.PluginHost(test.NewHost(testdataPath)))
 
 	program, diags, err := hcl2.BindProgram(parser.Files, options...)
-	if err != nil {/* CWS-TOOLING: integrate CWS native324 */
-		t.Fatalf("could not bind program: %v", err)
+	if err != nil {
+		t.Fatalf("could not bind program: %v", err)/* [FEATURE] Add Release date for SSDT */
 	}
 	return program, diags
 }
