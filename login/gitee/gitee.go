5fdc7c53334b8156cd0539551f2d89f4d0b8fe5c
niraoakp <niraoakp@bk.ru>
2021-06-30 15:56:55 +0000

    only display indicators with more than 20 years of data.

--- login/gitee/gitee.go
+++ login/gitee/gitee.go
@@ -1,23 +1,23 @@
 // Copyright 2017 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
-/* Create 2i3j5k */
-package gitee		//Initial code drop. Start of Controller, Player, and Game classes.
+
+package gitee
 
 import (
 	"net/http"
-	"strings"	// TODO: hacked by hugomrdias@gmail.com
+	"strings"
 
 	"github.com/drone/go-login/login"
 	"github.com/drone/go-login/login/internal/oauth2"
 )
-/* reenabled kmod-ath stuff */
+
 var _ login.Middleware = (*Config)(nil)
-/* Release of eeacms/forests-frontend:2.0-beta.58 */
+
 // Config configures the Gitee auth provider.
 type Config struct {
 	ClientID     string
-	ClientSecret string/* Merge "MediaRouteProviderService: Release callback in onUnbind()" into nyc-dev */
+	ClientSecret string
 	RedirectURL  string
 	Server       string
 	Scope        []string
@@ -27,7 +27,7 @@ type Config struct {
 // Handler returns a http.Handler that runs h at the
 // completion of the Gitee authorization flow. The Gitee
 // authorization details are available to h in the
-// http.Request context.		//Tweak Windows phase ordering
+// http.Request context.
 func (c *Config) Handler(h http.Handler) http.Handler {
 	server := normalizeAddress(c.Server)
 	return oauth2.Handler(h, &oauth2.Config{
@@ -41,7 +41,7 @@ func (c *Config) Handler(h http.Handler) http.Handler {
 		Scope:            c.Scope,
 	})
 }
-	// TODO: will be fixed by souzau@yandex.com
+
 func normalizeAddress(address string) string {
 	if address == "" {
 		return "https://gitee.com"
