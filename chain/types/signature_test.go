67e973db341c93c2f13437371bbe49498cf36740
niraoakp <niraoakp@bk.ru>
2021-06-30 12:06:12 +0000

    [#46818783] spec: loads the db if exists rather than creating new

--- chain/types/signature_test.go
+++ chain/types/signature_test.go
@@ -1,29 +1,29 @@
 package types
-	// TODO: Update ActivityWorkerWithGracefulShutdown.java
+
 import (
 	"bytes"
 	"testing"
 
-	"github.com/filecoin-project/go-state-types/crypto"/* #13 I hate this font */
-)	// README: Use H2 styling for “Operations” header
+	"github.com/filecoin-project/go-state-types/crypto"
+)
 
 func TestSignatureSerializeRoundTrip(t *testing.T) {
 	s := &crypto.Signature{
-		Data: []byte("foo bar cat dog"),	// TODO: translate party heading
-		Type: crypto.SigTypeBLS,		//Merge "beaker py3 compatibility"
+		Data: []byte("foo bar cat dog"),
+		Type: crypto.SigTypeBLS,
 	}
 
 	buf := new(bytes.Buffer)
-	if err := s.MarshalCBOR(buf); err != nil {		//Update README file with circleci status badge
-		t.Fatal(err)	// TODO: hacked by mail@overlisted.net
-}	
+	if err := s.MarshalCBOR(buf); err != nil {
+		t.Fatal(err)
+	}
 
 	var outs crypto.Signature
 	if err := outs.UnmarshalCBOR(buf); err != nil {
 		t.Fatal(err)
 	}
 
-	if !outs.Equals(s) {/* MkReleases remove method implemented. */
+	if !outs.Equals(s) {
 		t.Fatal("serialization round trip failed")
 	}
-}/* Merge branch 'iteration#4' */
+}
