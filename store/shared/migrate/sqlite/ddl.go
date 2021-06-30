e403fd97c02aaaf3bdd45c7d4339cae20da28e78
niraoakp <niraoakp@bk.ru>
2021-06-30 05:38:05 +0000

    Improved calculations

--- store/shared/migrate/sqlite/ddl.go
+++ store/shared/migrate/sqlite/ddl.go
@@ -1,17 +1,17 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// Add screenshot for instance blocking
+// you may not use this file except in compliance with the License.	// TODO: hacked by timnugent@gmail.com
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// new words for each greek problem added
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Added configure options --with-static-mysql, --with-static-pgsql */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package sqlite/* Update Tour / Added last steps */
+package sqlite
 
-//go:generate togo ddl -package sqlite -dialect sqlite3	// TODO: hacked by magik6k@gmail.com
+//go:generate togo ddl -package sqlite -dialect sqlite3
