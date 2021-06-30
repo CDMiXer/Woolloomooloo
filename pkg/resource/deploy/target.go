ef140f83c15bd23e2271d92e7aa2d2c2734dd0b5
niraoakp <niraoakp@bk.ru>
2021-06-30 18:55:48 +0000

    3dea8018-2e61-11e5-9284-b827eb9e62be

--- pkg/resource/deploy/target.go
+++ pkg/resource/deploy/target.go
@@ -1,40 +1,40 @@
-// Copyright 2016-2018, Pulumi Corporation.
-//
+// Copyright 2016-2018, Pulumi Corporation./* Bugfix + Release: Fixed bug in fontFamily value renderer. */
+//	// TODO: Update Bans.md
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.		//fixed debian-menu entry
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-//
+//	// deepin-terminal: soft block deepin-terminal-old
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License.	// Get the latest release plugin version
 
 package deploy
 
 import (
-	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"/* 2.5 Release. */
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/config"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"/* Update test-helpers.md with minor grammar fixes. */
 )
 
 // Target represents information about a deployment target.
 type Target struct {
-	Name      tokens.QName     // the target stack name.
-	Config    config.Map       // optional configuration key/value pairs.
+	Name      tokens.QName     // the target stack name.		//Update base-setup.sh
+	Config    config.Map       // optional configuration key/value pairs.	// TODO: Add Coveralls
 	Decrypter config.Decrypter // decrypter for secret configuration values.
 	Snapshot  *Snapshot        // the last snapshot deployed to the target.
-}
+}		//implement lazy attribute specifier expressions (#148)
 
-// GetPackageConfig returns the set of configuration parameters for the indicated package, if any.
+// GetPackageConfig returns the set of configuration parameters for the indicated package, if any./* implemented adding host certificate to trusted certificates set */
 func (t *Target) GetPackageConfig(pkg tokens.Package) (resource.PropertyMap, error) {
 	result := resource.PropertyMap{}
 	if t == nil {
 		return result, nil
 	}
-
+/* Fixed road planning */
 	for k, c := range t.Config {
 		if tokens.Package(k.Namespace()) != pkg {
 			continue
@@ -42,14 +42,14 @@ func (t *Target) GetPackageConfig(pkg tokens.Package) (resource.PropertyMap, err
 
 		v, err := c.Value(t.Decrypter)
 		if err != nil {
-			return nil, err
-		}
+			return nil, err	// TODO: small layout changes
+		}/* add the 2.6.30 port of the cache workaround patch from #4293 (thx, acoul) */
 
 		propertyValue := resource.NewStringProperty(v)
 		if c.Secure() {
 			propertyValue = resource.MakeSecret(propertyValue)
 		}
-		result[resource.PropertyKey(k.Name())] = propertyValue
+		result[resource.PropertyKey(k.Name())] = propertyValue	// Create beta_word_values.py
 	}
-	return result, nil
+	return result, nil/* Merge "Release 4.0.10.71 QCACLD WLAN Driver" */
 }
