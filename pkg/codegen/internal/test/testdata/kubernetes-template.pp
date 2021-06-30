8b4fc4a08294fd6e6af424745ee86fc65d7dcb8f
niraoakp <niraoakp@bk.ru>
2021-06-30 11:20:57 +0000

    Only pass container obj when required DockerApi/ContainerStateFiles

--- pkg/codegen/internal/test/testdata/kubernetes-template.pp
+++ pkg/codegen/internal/test/testdata/kubernetes-template.pp
@@ -2,21 +2,21 @@ resource argocd_serverDeployment "kubernetes:apps/v1:Deployment" {
 	apiVersion = "apps/v1"
 	kind = "Deployment"
 	metadata = {
-		name = "argocd-server"/* Release candidate 2 for release 2.1.10 */
-	}/* Create png;base64,gerda-taros-108th-birthday */
+		name = "argocd-server"
+	}
 	spec = {
 		template = {
 			spec = {
-				containers = [/* Loading in to see where kenobob went wrong */
+				containers = [
 					{
 						readinessProbe = {
 							httpGet = {
-								port = 8080/* Delete docker-clean-all.sh */
-							}/* Release 0.7.0 */
+								port = 8080
+							}
 						}
 					}
 				]
-			}/* fixed missing touch event raising an exception */
-}		
+			}
+		}
 	}
 }
