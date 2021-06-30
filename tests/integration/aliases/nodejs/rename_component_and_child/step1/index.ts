139f9a3688ca3f80e535aad5623787af30cb3984
niraoakp <niraoakp@bk.ru>
2021-06-30 23:46:46 +0000

    8c974356-2e5a-11e5-9284-b827eb9e62be

--- tests/integration/aliases/nodejs/rename_component_and_child/step1/index.ts
+++ tests/integration/aliases/nodejs/rename_component_and_child/step1/index.ts
@@ -1,18 +1,18 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-	// Fixed Task #14318.
+
 import * as pulumi from "@pulumi/pulumi";
-		//Only start modules when this actual sphere is told to.
+
 class Resource extends pulumi.ComponentResource {
-    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {/* too aggressive on the search and replace for BUILD_DIR */
+    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:Resource", name, {}, opts);
     }
 }
 
 // Scenario #5 - composing #1 and #3 and making both changes at the same time
 class ComponentFive extends pulumi.ComponentResource {
-    resource: Resource;		//add squeeze unit tests, refs #2295
+    resource: Resource;
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
-        super("my:module:ComponentFive", name, {}, opts);/* fixed: save empty mp3 */
+        super("my:module:ComponentFive", name, {}, opts);
         this.resource = new Resource("otherchild", {parent: this});
     }
 }
