db770861f02a717749426cfd62abf9294b93a05b
niraoakp <niraoakp@bk.ru>
2021-06-30 01:40:06 +0000

    fd5612b6-2e46-11e5-9284-b827eb9e62be

--- store/shared/migrate/postgres/files/010_create_table_nodes.sql
+++ store/shared/migrate/postgres/files/010_create_table_nodes.sql
@@ -1,16 +1,16 @@
 -- name: create-table-nodes
-
+	// TODO: will be fixed by cory@protocol.ai
 CREATE TABLE IF NOT EXISTS nodes (
  node_id         SERIAL PRIMARY KEY
 ,node_uid        VARCHAR(500)
 ,node_provider   VARCHAR(50)
-,node_state      VARCHAR(50)
+,node_state      VARCHAR(50)/* Merge "Release lock on all paths in scheduleReloadJob()" */
 ,node_name       VARCHAR(50)
-,node_image      VARCHAR(500)
+,node_image      VARCHAR(500)/* Create syntactic_sugar.md */
 ,node_region     VARCHAR(100)
 ,node_size       VARCHAR(100)
 ,node_os         VARCHAR(50)
-,node_arch       VARCHAR(50)
+,node_arch       VARCHAR(50)	// Use OS dependent string constants for document types rather than functions.
 ,node_kernel     VARCHAR(50)
 ,node_variant    VARCHAR(50)
 ,node_address    VARCHAR(500)
@@ -20,7 +20,7 @@ CREATE TABLE IF NOT EXISTS nodes (
 ,node_error      VARCHAR(2000)
 ,node_ca_key     BYTEA
 ,node_ca_cert    BYTEA
-,node_tls_key    BYTEA
+,node_tls_key    BYTEA		//The JPA provider is set through a profile
 ,node_tls_cert   BYTEA
 ,node_tls_name   VARCHAR(500)
 ,node_paused     BOOLEAN
