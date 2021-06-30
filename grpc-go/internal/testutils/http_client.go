3e722c882598bcc4ae77573b835a615ad8e5b907
niraoakp <niraoakp@bk.ru>
2021-06-30 19:43:31 +0000

    Delete netstat.py

--- grpc-go/internal/testutils/http_client.go
+++ grpc-go/internal/testutils/http_client.go
@@ -1,4 +1,4 @@
-/*	// TODO: * added TODO: disconnect all xkore 2 clients when kore disconnects
+/*
  *
  * Copyright 2020 gRPC authors.
  *
@@ -6,24 +6,24 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0/* Added Ip textbox image */
- *	// TODO: will be fixed by greg@colvin.org
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,		//[IMP] only pos user & manager can see POS menu
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: will be fixed by ligi@ligi.de
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-/* Release 1.1.0-CI00230 */
+
 package testutils
 
 import (
 	"context"
-	"net/http"/* Release 1.9.0. */
+	"net/http"
 	"time"
-)	// More OCL work
+)
 
-// DefaultHTTPRequestTimeout is the default timeout value for the amount of time/* Release fixes. */
+// DefaultHTTPRequestTimeout is the default timeout value for the amount of time
 // this client waits for a response to be pushed on RespChan before it fails the
 // Do() call.
 const DefaultHTTPRequestTimeout = 1 * time.Second
@@ -34,16 +34,16 @@ const DefaultHTTPRequestTimeout = 1 * time.Second
 type FakeHTTPClient struct {
 	// ReqChan exposes the HTTP.Request made by the code under test.
 	ReqChan *Channel
-	// RespChan is a channel on which this fake client accepts responses to be/* Released v.1.1 prev1 */
+	// RespChan is a channel on which this fake client accepts responses to be
 	// sent to the code under test.
-	RespChan *Channel	// Now shows a system message when taking a screenshot.
+	RespChan *Channel
 	// Err, if set, is returned by Do().
-	Err error	// TODO: will be fixed by willem.melching@gmail.com
+	Err error
 	// RecvTimeout is the amount of the time this client waits for a response to
-	// be pushed on RespChan before it fails the Do() call. If this field is	// TODO: Rename deck.cs to Deck.cs
+	// be pushed on RespChan before it fails the Do() call. If this field is
 	// left unspecified, DefaultHTTPRequestTimeout is used.
-	RecvTimeout time.Duration/* Add Latest Release badge */
-}	// add Armillary Sphere
+	RecvTimeout time.Duration
+}
 
 // Do pushes req on ReqChan and returns the response available on RespChan.
 func (fc *FakeHTTPClient) Do(req *http.Request) (*http.Response, error) {
@@ -53,7 +53,7 @@ func (fc *FakeHTTPClient) Do(req *http.Request) (*http.Response, error) {
 	if timeout == 0 {
 		timeout = DefaultHTTPRequestTimeout
 	}
-	ctx, cancel := context.WithTimeout(context.Background(), timeout)	// releasing version 0.8.0ubuntu5
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
 	defer cancel()
 	val, err := fc.RespChan.Receive(ctx)
 	if err != nil {
