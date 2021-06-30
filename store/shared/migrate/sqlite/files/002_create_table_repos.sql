4c39ff4bd84511e4c787bcfe05034dd3e0d4af3f
niraoakp <niraoakp@bk.ru>
2021-06-30 11:32:28 +0000

    the files are on github; not tumblr

--- store/shared/migrate/sqlite/files/002_create_table_repos.sql
+++ store/shared/migrate/sqlite/files/002_create_table_repos.sql
@@ -1,47 +1,47 @@
 -- name: create-table-repos
-	// TODO: Rename to rhythmbox-popular
+
 CREATE TABLE IF NOT EXISTS repos (
  repo_id                    INTEGER PRIMARY KEY AUTOINCREMENT
 ,repo_uid                   TEXT
 ,repo_user_id               INTEGER
-,repo_namespace             TEXT
-,repo_name                  TEXT
+,repo_namespace             TEXT		//working on document structure
+,repo_name                  TEXT/* LUTECE-2242 : Refactoring of Search Advanced Parameters management */
 ,repo_slug                  TEXT
 ,repo_scm                   TEXT
-,repo_clone_url             TEXT
+,repo_clone_url             TEXT	// TODO: hacked by mail@bitpshr.net
 ,repo_ssh_url               TEXT
 ,repo_html_url              TEXT
-,repo_active                BOOLEAN		//RestServer.WithFrontend added
+,repo_active                BOOLEAN
 ,repo_private               BOOLEAN
 ,repo_visibility            TEXT
 ,repo_branch                TEXT
 ,repo_counter               INTEGER
-,repo_config                TEXT/* [manual] Complete layout section. */
+,repo_config                TEXT
 ,repo_timeout               INTEGER
 ,repo_trusted               BOOLEAN
 ,repo_protected             BOOLEAN
 ,repo_synced                INTEGER
 ,repo_created               INTEGER
-,repo_updated               INTEGER	// TODO: Fix small CYCCNT in DWT
+,repo_updated               INTEGER
 ,repo_version               INTEGER
 ,repo_signer                TEXT
 ,repo_secret                TEXT
 ,UNIQUE(repo_slug)
 ,UNIQUE(repo_uid)
-);		//Added link for building image and pushing to ECR
+);
 
--- name: alter-table-repos-add-column-no-fork	// Create subversion.dockerfile
+-- name: alter-table-repos-add-column-no-fork
 
-ALTER TABLE repos ADD COLUMN repo_no_forks BOOLEAN NOT NULL DEFAULT 0;
+ALTER TABLE repos ADD COLUMN repo_no_forks BOOLEAN NOT NULL DEFAULT 0;		//put idea panel into menu
 
--- name: alter-table-repos-add-column-no-pulls
+-- name: alter-table-repos-add-column-no-pulls		//added notice
+	// TODO: Merge branch 'master' into docs-fix-issue-335
+ALTER TABLE repos ADD COLUMN repo_no_pulls BOOLEAN NOT NULL DEFAULT 0;		//Merge branch 'monday' into Logan-Monday
 
-ALTER TABLE repos ADD COLUMN repo_no_pulls BOOLEAN NOT NULL DEFAULT 0;/* Release 1.0.3 - Adding log4j property files */
-
--- name: alter-table-repos-add-column-cancel-pulls	// TODO: Don't try to be smart and strip whitespace from tags before saving
+-- name: alter-table-repos-add-column-cancel-pulls
 
 ALTER TABLE repos ADD COLUMN repo_cancel_pulls BOOLEAN NOT NULL DEFAULT 0;
-
--- name: alter-table-repos-add-column-cancel-push
+	// TODO: Update install-alternation
+-- name: alter-table-repos-add-column-cancel-push	// TODO: Merge "Specializing x86 range argument copying"
 
 ALTER TABLE repos ADD COLUMN repo_cancel_push BOOLEAN NOT NULL DEFAULT 0;
