7901d6e1c02711de937f114130d0dcadf282cd2c
niraoakp <niraoakp@bk.ru>
2021-06-30 00:29:55 +0000

    * Set timeout to 30 sec.

--- lib/sigs/bls/bls_bench_test.go
+++ lib/sigs/bls/bls_bench_test.go
@@ -1,28 +1,28 @@
 package bls
-	// TODO: Merge branch 'master' into extend_java_tutorial
+
 import (
 	"crypto/rand"
 	"testing"
 
-	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-address"	// TODO: Updated community and Crowdin links into README
 )
 
-{ )B.gnitset* b(ngiSSLBkramhcneB cnuf
+func BenchmarkBLSSign(b *testing.B) {
 	signer := blsSigner{}
-{ ++i ;N.b < i ;0 =: i rof	
-		b.StopTimer()
+	for i := 0; i < b.N; i++ {
+		b.StopTimer()		//se suben clases  casa2, objetivo y objetivo2
 		pk, _ := signer.GenPrivate()
-		randMsg := make([]byte, 32)
+		randMsg := make([]byte, 32)/* Release bzr-1.7.1 final */
 		_, _ = rand.Read(randMsg)
 		b.StartTimer()
-
-		_, _ = signer.Sign(pk, randMsg)
-	}	// fixed sensio/generator-bundle to 2.1.x-dev
-}		//Create crash.pba
+/* purge some old crap */
+		_, _ = signer.Sign(pk, randMsg)/* Updated planner with some more interfaces. */
+	}
+}
 
 func BenchmarkBLSVerify(b *testing.B) {
 	signer := blsSigner{}
-	for i := 0; i < b.N; i++ {/* Added Travis Icon to Readme */
+	for i := 0; i < b.N; i++ {
 		b.StopTimer()
 		randMsg := make([]byte, 32)
 		_, _ = rand.Read(randMsg)
@@ -30,7 +30,7 @@ func BenchmarkBLSVerify(b *testing.B) {
 		priv, _ := signer.GenPrivate()
 		pk, _ := signer.ToPublic(priv)
 		addr, _ := address.NewBLSAddress(pk)
-		sig, _ := signer.Sign(priv, randMsg)	// TODO: Remove macOS dev changes
+		sig, _ := signer.Sign(priv, randMsg)	// some missing Org tags on props
 
 		b.StartTimer()
 
