dc3b4d425b8c86c939469c52f01dd7ba67b550e4
niraoakp <niraoakp@bk.ru>
2021-06-30 09:14:35 +0000

    Pagina portada

--- tests/integration/partial_state/step3/index.ts
+++ tests/integration/partial_state/step3/index.ts
@@ -1,8 +1,8 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-		//More appropriate semantics.
+
 import * as pulumi from "@pulumi/pulumi";
 import { Resource } from "./resource";
-	// TODO: hacked by xiemengjun@gmail.com
+
 // resource "not-doomed" is created successfully.
 const a = new Resource("not-doomed", 5);
 
