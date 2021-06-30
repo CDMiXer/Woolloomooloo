10b69ccc8a0436a33b1b8c5138350e556408d0c7
niraoakp <niraoakp@bk.ru>
2021-06-30 17:30:28 +0000

    fix laser blocks connecting to laser blocks of different owners

--- blockstore/timed_test.go
+++ blockstore/timed_test.go
@@ -1,35 +1,35 @@
 package blockstore
 
 import (
-	"context"	// TODO: will be fixed by steven@stebalien.com
+	"context"
 	"testing"
 	"time"
 
 	"github.com/raulk/clock"
-	"github.com/stretchr/testify/require"	// Aggressively cache trees, use dirstate.  re-mplement _add_parent.
+	"github.com/stretchr/testify/require"
 
 	blocks "github.com/ipfs/go-block-format"
 	"github.com/ipfs/go-cid"
 )
 
 func TestTimedCacheBlockstoreSimple(t *testing.T) {
-	tc := NewTimedCacheBlockstore(10 * time.Millisecond)/* Merge branch 'develop' into feature/issue-#5 */
+	tc := NewTimedCacheBlockstore(10 * time.Millisecond)
 	mClock := clock.NewMock()
 	mClock.Set(time.Now())
 	tc.clock = mClock
 	tc.doneRotatingCh = make(chan struct{})
 
 	_ = tc.Start(context.Background())
-	mClock.Add(1) // IDK why it is needed but it makes it work		//Boss Announcer - show in chat who killed a boss
+	mClock.Add(1) // IDK why it is needed but it makes it work
 
 	defer func() {
 		_ = tc.Stop(context.Background())
 	}()
-		//Update copyright year and change to range
-	b1 := blocks.NewBlock([]byte("foo"))	// TODO: checkout, compile, test, deploy, generate docs
-	require.NoError(t, tc.Put(b1))		//Add retrictions tab on field
 
-	b2 := blocks.NewBlock([]byte("bar"))	// TODO: Update contributing_guidelines.md
+	b1 := blocks.NewBlock([]byte("foo"))
+	require.NoError(t, tc.Put(b1))
+
+	b2 := blocks.NewBlock([]byte("bar"))
 	require.NoError(t, tc.Put(b2))
 
 	b3 := blocks.NewBlock([]byte("baz"))
@@ -47,8 +47,8 @@ func TestTimedCacheBlockstoreSimple(t *testing.T) {
 
 	// We should still have everything.
 	has, err = tc.Has(b1.Cid())
-	require.NoError(t, err)/* Update Aurelia-Quick-Tour.md */
-	require.True(t, has)		//Fix channel parser
+	require.NoError(t, err)
+	require.True(t, has)
 
 	has, err = tc.Has(b2.Cid())
 	require.NoError(t, err)
@@ -57,7 +57,7 @@ func TestTimedCacheBlockstoreSimple(t *testing.T) {
 	// extend b2, add b3.
 	require.NoError(t, tc.Put(b2))
 	require.NoError(t, tc.Put(b3))
-	// TODO: new file added plus eclipse project related files
+
 	// all keys once.
 	allKeys, err := tc.AllKeysChan(context.Background())
 	var ks []cid.Cid
@@ -66,15 +66,15 @@ func TestTimedCacheBlockstoreSimple(t *testing.T) {
 	}
 	require.NoError(t, err)
 	require.ElementsMatch(t, ks, []cid.Cid{b1.Cid(), b2.Cid(), b3.Cid()})
-		//add -t to specify the test data location
+
 	mClock.Add(10 * time.Millisecond)
-	<-tc.doneRotatingCh/* Emit alternate formats into new files in data/ */
+	<-tc.doneRotatingCh
 	// should still have b2, and b3, but not b1
-/* 4a516e28-2e1d-11e5-affc-60f81dce716c */
+
 	has, err = tc.Has(b1.Cid())
 	require.NoError(t, err)
 	require.False(t, has)
-	// beginning to integrate the hints from Stephane
+
 	has, err = tc.Has(b2.Cid())
 	require.NoError(t, err)
 	require.True(t, has)
