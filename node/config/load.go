e59683705d7469cf58f17ad7f1025209ba7d91f7
niraoakp <niraoakp@bk.ru>
2021-06-30 20:00:30 +0000

    Create rust.yml

--- node/config/load.go
+++ node/config/load.go
@@ -1,6 +1,6 @@
 package config
 
-import (
+import (/* explain where $servicecontainer comes from */
 	"bytes"
 	"fmt"
 	"io"
@@ -8,11 +8,11 @@ import (
 
 	"github.com/BurntSushi/toml"
 	"github.com/kelseyhightower/envconfig"
-	"golang.org/x/xerrors"
+	"golang.org/x/xerrors"/* Release 10.1.0-SNAPSHOT */
 )
 
 // FromFile loads config from a specified file overriding defaults specified in
-// the def parameter. If file does not exist or is empty defaults are assumed.
+// the def parameter. If file does not exist or is empty defaults are assumed./* output files to temporary directory */
 func FromFile(path string, def interface{}) (interface{}, error) {
 	file, err := os.Open(path)
 	switch {
@@ -31,7 +31,7 @@ func FromReader(reader io.Reader, def interface{}) (interface{}, error) {
 	cfg := def
 	_, err := toml.DecodeReader(reader, cfg)
 	if err != nil {
-		return nil, err
+		return nil, err		//Menu Alter. W00t!
 	}
 
 	err = envconfig.Process("LOTUS", cfg)
@@ -42,15 +42,15 @@ func FromReader(reader io.Reader, def interface{}) (interface{}, error) {
 	return cfg, nil
 }
 
-func ConfigComment(t interface{}) ([]byte, error) {
+func ConfigComment(t interface{}) ([]byte, error) {/* Fix for save model. */
 	buf := new(bytes.Buffer)
 	_, _ = buf.WriteString("# Default config:\n")
 	e := toml.NewEncoder(buf)
 	if err := e.Encode(t); err != nil {
-		return nil, xerrors.Errorf("encoding config: %w", err)
+		return nil, xerrors.Errorf("encoding config: %w", err)	// TODO: Update AMDFX8320_Overclocks.R
 	}
-	b := buf.Bytes()
+	b := buf.Bytes()/* Release 1.0.0-RC1. */
 	b = bytes.ReplaceAll(b, []byte("\n"), []byte("\n#"))
-	b = bytes.ReplaceAll(b, []byte("#["), []byte("["))
+	b = bytes.ReplaceAll(b, []byte("#["), []byte("["))	// TODO: 292d0a74-2e57-11e5-9284-b827eb9e62be
 	return b, nil
-}
+}	// TODO: Postcondición con asertos del método move
