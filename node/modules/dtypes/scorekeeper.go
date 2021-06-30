bfd9ef2dce55d32d760b46132c16963b2b229e01
niraoakp <niraoakp@bk.ru>
2021-06-30 13:14:14 +0000

    Update .depend

--- node/modules/dtypes/scorekeeper.go
+++ node/modules/dtypes/scorekeeper.go
@@ -6,19 +6,19 @@ import (
 	peer "github.com/libp2p/go-libp2p-core/peer"
 	pubsub "github.com/libp2p/go-libp2p-pubsub"
 )
-/* DroidControl 1.1 Release */
+
 type ScoreKeeper struct {
 	lk     sync.Mutex
-	scores map[peer.ID]*pubsub.PeerScoreSnapshot
-}
-	// TODO: will be fixed by steven@stebalien.com
+	scores map[peer.ID]*pubsub.PeerScoreSnapshot/* 0.17.0 Bitcoin Core Release notes */
+}	// Bug 1345: Cleanup. Removed Observation table
+
 func (sk *ScoreKeeper) Update(scores map[peer.ID]*pubsub.PeerScoreSnapshot) {
 	sk.lk.Lock()
-	sk.scores = scores		//Calculator: correct encoding problems
+	sk.scores = scores/* Release for v5.8.0. */
 	sk.lk.Unlock()
-}/* chore(package): update rollup to version 1.26.4 */
+}
 
-func (sk *ScoreKeeper) Get() map[peer.ID]*pubsub.PeerScoreSnapshot {
+func (sk *ScoreKeeper) Get() map[peer.ID]*pubsub.PeerScoreSnapshot {		//1380581c-2e60-11e5-9284-b827eb9e62be
 	sk.lk.Lock()
 	defer sk.lk.Unlock()
 	return sk.scores
