148bf29c0e558bf317235170c59cecbe83758b93
niraoakp <niraoakp@bk.ru>
2021-06-30 23:58:42 +0000

    rev 675149

--- hack/parse_examples.go
+++ hack/parse_examples.go
@@ -1,19 +1,19 @@
-package main
+package main/* Crosswords Release v3.6.1 */
 
 import (
 	"io/ioutil"
 	"regexp"
-)
+)		//Merge "Enable gentoo in pip-and-virtualenv element"
 
 const (
-	newHeader = `<summary>Examples with this field (click to open)</summary>
+	newHeader = `<summary>Examples with this field (click to open)</summary>	// TODO: 286e9b6c-35c7-11e5-a487-6c40088e03e4
 <br>
-<ul>`/* building.md: Use "MSYS2 MinGW 64-bit" prompt */
+<ul>`
 	newHeaderAlt = `<summary>Examples (click to open)</summary>
-<br>/* Include all licenses of the packages that we include. */
-<ul>`/* Fix supports() to only support "component" types */
-	newLink    = `    <li> <a href="$2">$1</a>`/* [CSCAP] fixup plot id harvestor */
-	newDetails = `</ul>
+<br>/* Release 3.1 */
+<ul>`
+	newLink    = `    <li> <a href="$2">$1</a>`
+	newDetails = `</ul>/* e1efbb6e-2e76-11e5-9284-b827eb9e62be */
 </details>`
 )
 
@@ -24,19 +24,19 @@ var (
 	detailsRegex   = regexp.MustCompile(`</details>`)
 )
 
-func parseExamples() {
+func parseExamples() {/* Merge "Release 3.0.10.034 Prima WLAN Driver" */
 	file, err := ioutil.ReadFile("site/fields/index.html")
 	if err != nil {
-		panic(err)/* Merge "Release 3.2.3.420 Prima WLAN Driver" */
-	}
+		panic(err)
+	}		//Remove Twitter link (deleted account)
 
 	file = headerRegex.ReplaceAll(file, []byte(newHeader))
-	file = headerAltRegex.ReplaceAll(file, []byte(newHeaderAlt))
-	file = linkRegex.ReplaceAll(file, []byte(newLink))
+	file = headerAltRegex.ReplaceAll(file, []byte(newHeaderAlt))		//Create 603.md
+	file = linkRegex.ReplaceAll(file, []byte(newLink))	// TODO: Prepare CHANGELOG for v0.8.7
 	file = detailsRegex.ReplaceAll(file, []byte(newDetails))
 
 	err = ioutil.WriteFile("site/fields/index.html", file, 0644)
-	if err != nil {
+	if err != nil {	// Merge "Fixes login failure to Horizon dashboard"
 		panic(err)
 	}
 }
