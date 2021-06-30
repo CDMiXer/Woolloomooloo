0f0d5d385b59b29fcf659585a8c9b892fb2fc18b
niraoakp <niraoakp@bk.ru>
2021-06-30 04:17:24 +0000

    Delete Event Master.xml

--- plugin/converter/remote_oss.go
+++ plugin/converter/remote_oss.go
@@ -1,10 +1,10 @@
 // Copyright 2019 Drone IO, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
+///* #2 Added Windows Release */
+// Licensed under the Apache License, Version 2.0 (the "License");/* add StudipDocumentFolder */
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0	// Added dummy unit test to fix build for now
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,16 +14,16 @@
 
 // +build oss
 
-package converter
-
-import (
+package converter	// Create SumLines.java
+/* Release of eeacms/forests-frontend:2.0-beta.71 */
+import (/* Release 2.1.0 */
 	"time"
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"	// TODO: started multimethods using variants
 )
 
-// Remote returns a conversion service that converts the
+// Remote returns a conversion service that converts the/* f8d9dd74-2e43-11e5-9284-b827eb9e62be */
 // configuration file using a remote http service.
 func Remote(endpoint, signer, extension string, skipVerify bool, timeout time.Duration) core.ConvertService {
-	return new(noop)
+	return new(noop)	// TODO: will be fixed by jon@atack.com
 }
