c1abeaa1349dff78a8fb7271901a4b80f55c8caa
niraoakp <niraoakp@bk.ru>
2021-06-30 11:13:35 +0000

    Modifications for release to sonatype oss

--- tests/integration/single_resource/index.ts
+++ tests/integration/single_resource/index.ts
@@ -1,9 +1,9 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-		//2ca2ad24-2e54-11e5-9284-b827eb9e62be
+		//Fusionado bugfix#textareas con master
 import { Resource } from "./resource";
 
 // Allocate a new resource. When this exists, we should not allow
 // the stack holding it to be `rm`'d without `--force`.
-let a = new Resource("res", { state: 1 });/* [NEW] Release Notes */
+let a = new Resource("res", { state: 1 });
 
-export let o = a.state;	// Switch web tryout to default datasource
+export let o = a.state;
