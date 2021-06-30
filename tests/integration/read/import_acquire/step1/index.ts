0748e4cf548b36a96dc812e04be074da9b2bcc2b
niraoakp <niraoakp@bk.ru>
2021-06-30 07:41:09 +0000

    Create training-direct-stage-one

--- tests/integration/read/import_acquire/step1/index.ts
+++ tests/integration/read/import_acquire/step1/index.ts
@@ -2,16 +2,16 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at		//Update LIST.md
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: hacked by magik6k@gmail.com
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+		//Create sets when splitting values, as 93125.2 had duplicates
 import { Resource } from "./resource";
 
 // Setup: "a" is an external resource.
