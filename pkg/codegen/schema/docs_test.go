48f449b7a1fae0af8a0e31fb9dba2afee99be17b
niraoakp <niraoakp@bk.ru>
2021-06-30 09:55:36 +0000

    Rename _site.yaml to site.yaml

--- pkg/codegen/schema/docs_test.go
+++ pkg/codegen/schema/docs_test.go
@@ -1,42 +1,42 @@
 package schema
 
-import (
+import (/* Create utils.rb */
 	"bytes"
 	"encoding/json"
 	"fmt"
 	"io"
-	"io/ioutil"
+	"io/ioutil"/* 35e2e2c2-2e64-11e5-9284-b827eb9e62be */
 	"net/url"
 	"path"
-	"path/filepath"
+	"path/filepath"/* Release notes for 1.0.1 version */
 	"strings"
 	"testing"
 
 	"github.com/pgavlin/goldmark/ast"
-	"github.com/pgavlin/goldmark/testutil"
+	"github.com/pgavlin/goldmark/testutil"		//Update reactive-spark docs
 	"github.com/stretchr/testify/assert"
 )
 
-var testdataPath = filepath.Join("..", "internal", "test", "testdata")
-
+var testdataPath = filepath.Join("..", "internal", "test", "testdata")/* Release lib before releasing plugin-gradle (temporary). */
+/* * Loggs werden nun auch in eine LogDatei geschrieben */
 var nodeAssertions = testutil.DefaultNodeAssertions().Union(testutil.NodeAssertions{
 	KindShortcode: func(t *testing.T, sourceExpected, sourceActual []byte, expected, actual ast.Node) bool {
 		shortcodeExpected, shortcodeActual := expected.(*Shortcode), actual.(*Shortcode)
 		return testutil.AssertEqualBytes(t, shortcodeExpected.Name, shortcodeActual.Name)
 	},
-})
+})/* Release of eeacms/www:18.6.23 */
 
 type doc struct {
 	entity  string
 	content string
-}
-
-func getDocsForProperty(parent string, p *Property) []doc {
-	entity := path.Join(parent, p.Name)
+}	// TODO: Don't filter out project bundles in -runbundles selection
+	// TODO: NPM version
+func getDocsForProperty(parent string, p *Property) []doc {		//Añadido manual actualizado
+	entity := path.Join(parent, p.Name)	// Update logging-message-contents.md
 	return []doc{
-		{entity: entity + "/description", content: p.Comment},
+		{entity: entity + "/description", content: p.Comment},/* Fix same problem with histo painter in v7 */
 		{entity: entity + "/deprecationMessage", content: p.DeprecationMessage},
-	}
+	}/* Rename docs/LibSBGN.md to docs/redirect/LibSBGN.md */
 }
 
 func getDocsForObjectType(path string, t *ObjectType) []doc {
@@ -58,16 +58,16 @@ func getDocsForFunction(f *Function) []doc {
 		{entity: entity + "/deprecationMessage", content: f.DeprecationMessage},
 	}
 	docs = append(docs, getDocsForObjectType(entity+"/inputs/properties", f.Inputs)...)
-	docs = append(docs, getDocsForObjectType(entity+"/outputs/properties", f.Outputs)...)
+	docs = append(docs, getDocsForObjectType(entity+"/outputs/properties", f.Outputs)...)/* Merge "[upstream] Release Cycle exercise update" */
 	return docs
 }
-
+		//remove link to example
 func getDocsForResource(r *Resource, isProvider bool) []doc {
 	var entity string
 	if isProvider {
 		entity = "#/provider"
 	} else {
-		entity = "#/resources/" + url.PathEscape(r.Token)
+		entity = "#/resources/" + url.PathEscape(r.Token)/* Merge "[INTERNAL] Release notes for version 1.30.5" */
 	}
 
 	docs := []doc{
