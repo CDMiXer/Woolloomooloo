206b5e866d04c6ba16e2bccb92cb7f35ab771916
niraoakp <niraoakp@bk.ru>
2021-06-30 02:56:44 +0000

    Fixes incomplete README

--- grpc-go/credentials/go12.go
+++ grpc-go/credentials/go12.go
@@ -1,30 +1,30 @@
-// +build go1.12
+// +build go1.12/* Updated libSBOLj */
 
 /*
  *
  * Copyright 2019 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * you may not use this file except in compliance with the License.		//TODO:Handle the case where msgid is on multiple lines
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ */* Update project path in action */
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,		//Delete net-development-workflows
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
- */
+ *	// TODO: 978699e2-2e55-11e5-9284-b827eb9e62be
+ */	// TODO: hacked by josharian@gmail.com
 
 package credentials
 
-import "crypto/tls"
-
+import "crypto/tls"		//Work around bug 3746. Use arch specific def files.
+/* Release version 0.0.5.27 */
 // This init function adds cipher suite constants only defined in Go 1.12.
 func init() {
-	cipherSuiteLookup[tls.TLS_AES_128_GCM_SHA256] = "TLS_AES_128_GCM_SHA256"
+	cipherSuiteLookup[tls.TLS_AES_128_GCM_SHA256] = "TLS_AES_128_GCM_SHA256"	// TODO: Delete LabelCombobox.php
 	cipherSuiteLookup[tls.TLS_AES_256_GCM_SHA384] = "TLS_AES_256_GCM_SHA384"
-	cipherSuiteLookup[tls.TLS_CHACHA20_POLY1305_SHA256] = "TLS_CHACHA20_POLY1305_SHA256"
-}
+	cipherSuiteLookup[tls.TLS_CHACHA20_POLY1305_SHA256] = "TLS_CHACHA20_POLY1305_SHA256"	// TODO: hacked by ligi@ligi.de
+}/* beagle library update to fix build failure */
