2b0af9bca5a5a232bcfb68912e7476a11012ed73
niraoakp <niraoakp@bk.ru>
2021-06-30 12:27:33 +0000

    Rename elastic to elastic_command

--- tests/integration/recreate_resource_check/resource_recreate_check_test.go
+++ tests/integration/recreate_resource_check/resource_recreate_check_test.go
@@ -1,8 +1,8 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-// +build nodejs all
+// +build nodejs all/* Fixed documentation error. */
 
 package ints
-
+/* Merge "Release Notes 6.0 - Minor fix for a link to bp" */
 import (
 	"testing"
 
@@ -15,12 +15,12 @@ func TestResourceRecreateCheck(t *testing.T) {
 	integration.ProgramTest(t, &integration.ProgramTestOptions{
 		Dir:          "step1",
 		Dependencies: []string{"@pulumi/pulumi"},
-		Quick:        true,
-		EditDirs: []integration.EditDir{
+		Quick:        true,/* Add condition to bail out if JrFs is null in BrowseLibrary */
+		EditDirs: []integration.EditDir{	// TODO: Position fixed
 			{
 				Dir:      "step2",
 				Additive: true,
 			},
-		},
-	})
+		},	// TODO: A readWritable for IndexedSeqs
+	})	// Typo fix ingres -> ingress
 }
