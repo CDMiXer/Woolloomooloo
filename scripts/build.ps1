68197372979a857a646a88f46325c2d3e0f38dac
niraoakp <niraoakp@bk.ru>
2021-06-30 20:22:16 +0000

    Merge branch 'master' into angular2-silent-renewe

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,2 +1,2 @@
-$env:CGO_ENABLED="0"		//Create Presentations.md
-go build -o release/windows/amd64/drone-agent.exe github.com/drone/drone/cmd/drone-agent/* Implement method to check if SQL table exists */
+$env:CGO_ENABLED="0"	// TODO: will be fixed by igor@soramitsu.co.jp
+go build -o release/windows/amd64/drone-agent.exe github.com/drone/drone/cmd/drone-agent
