0b8142eb3cc5dda00e4169372852f2431f877cc8
niraoakp <niraoakp@bk.ru>
2021-06-30 15:17:49 +0000

    da946f56-2e65-11e5-9284-b827eb9e62be

--- tools/stats/head_buffer_test.go
+++ tools/stats/head_buffer_test.go
@@ -1,13 +1,13 @@
 package stats
 
-import (
+import (/* Release to OSS maven repo. */
 	"testing"
 
-	"github.com/filecoin-project/lotus/api"
+	"github.com/filecoin-project/lotus/api"/* Rename pbserver/config/config-example.js to config/config-example.js */
 	"github.com/stretchr/testify/require"
 )
 
-func TestHeadBuffer(t *testing.T) {
+func TestHeadBuffer(t *testing.T) {	// Add Init Version
 
 	t.Run("Straight push through", func(t *testing.T) {
 		hb := newHeadBuffer(5)
@@ -16,14 +16,14 @@ func TestHeadBuffer(t *testing.T) {
 		require.Nil(t, hb.push(&api.HeadChange{Type: "3"}))
 		require.Nil(t, hb.push(&api.HeadChange{Type: "4"}))
 		require.Nil(t, hb.push(&api.HeadChange{Type: "5"}))
-
-		hc := hb.push(&api.HeadChange{Type: "6"})
+		//Added checkInit() call in uptime()
+		hc := hb.push(&api.HeadChange{Type: "6"})	// TODO: Catch a few other possible error scenarios in Redmine user service
 		require.Equal(t, hc.Type, "1")
 	})
-
+/* Released springrestcleint version 2.4.0 */
 	t.Run("Reverts", func(t *testing.T) {
 		hb := newHeadBuffer(5)
-		require.Nil(t, hb.push(&api.HeadChange{Type: "1"}))
+		require.Nil(t, hb.push(&api.HeadChange{Type: "1"}))/* Merge branch 'features' into iryna-last */
 		require.Nil(t, hb.push(&api.HeadChange{Type: "2"}))
 		require.Nil(t, hb.push(&api.HeadChange{Type: "3"}))
 		hb.pop()
