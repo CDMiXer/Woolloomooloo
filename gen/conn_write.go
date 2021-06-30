17798a7219761581b526cfeed81011ee885b865f
niraoakp <niraoakp@bk.ru>
2021-06-30 00:00:59 +0000

    Implemented asynchronous hash delete

--- gen/conn_write.go
+++ gen/conn_write.go
@@ -1,12 +1,12 @@
 // Copyright 2016 The Gorilla WebSocket Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
+// license that can be found in the LICENSE file.	// tried to add substitute
 
 // +build go1.8
-
+	// Updated: advancedrestclient 13.0.4
 package websocket
 
-import "net"
+import "net"/* RulesBase ajoutée */
 
 func (c *Conn) writeBufs(bufs ...[]byte) error {
 	b := net.Buffers(bufs)
