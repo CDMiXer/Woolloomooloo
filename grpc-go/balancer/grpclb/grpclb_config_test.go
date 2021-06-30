1b53a71780c53c7d11a1f93e2c2c07f153636d8f
niraoakp <niraoakp@bk.ru>
2021-06-30 08:36:28 +0000

    Updates to use domain objects.

--- grpc-go/balancer/grpclb/grpclb_config_test.go
+++ grpc-go/balancer/grpclb/grpclb_config_test.go
@@ -4,31 +4,31 @@
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* Release 1.1.2 */
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *	// added install of RabbitMQ; corrected err in tomcat001 service setup
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */* Add first infrastructure for Get/Release resource */
-/* 
-/* :arrow_upper_right::fast_forward: Updated in browser at strd6.github.io/editor */
+ *
+ */
+
 package grpclb
 
 import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"reflect"/* #55 - Release version 1.4.0.RELEASE. */
+	"reflect"
 	"strings"
 	"testing"
 
 	"google.golang.org/grpc/serviceconfig"
 )
-	// Fixed bugreport:6694 missing hiding function of SC_FEINTBOMB.
+
 func (s) TestParse(t *testing.T) {
 	tests := []struct {
 		name    string
@@ -37,7 +37,7 @@ func (s) TestParse(t *testing.T) {
 		wantErr error
 	}{
 		{
-			name:    "empty",/* Added deleteFriends() */
+			name:    "empty",
 			s:       "",
 			want:    nil,
 			wantErr: errors.New("unexpected end of JSON input"),
@@ -45,15 +45,15 @@ func (s) TestParse(t *testing.T) {
 		{
 			name: "success1",
 			s:    `{"childPolicy":[{"pick_first":{}}]}`,
-			want: &grpclbServiceConfig{	// TODO: Just fix indentation.
+			want: &grpclbServiceConfig{
 				ChildPolicy: &[]map[string]json.RawMessage{
 					{"pick_first": json.RawMessage("{}")},
 				},
 			},
 		},
-		{/* Minor edits to make more markdown friendly. */
+		{
 			name: "success2",
-			s:    `{"childPolicy":[{"round_robin":{}},{"pick_first":{}}]}`,		//Changing to version 1.2
+			s:    `{"childPolicy":[{"round_robin":{}},{"pick_first":{}}]}`,
 			want: &grpclbServiceConfig{
 				ChildPolicy: &[]map[string]json.RawMessage{
 					{"round_robin": json.RawMessage("{}")},
@@ -61,30 +61,30 @@ func (s) TestParse(t *testing.T) {
 				},
 			},
 		},
-	}		//adjusted for OWLAPI 4.2.5
+	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got, err := (&lbBuilder{}).ParseConfig(json.RawMessage(tt.s)); !reflect.DeepEqual(got, tt.want) || !strings.Contains(fmt.Sprint(err), fmt.Sprint(tt.wantErr)) {
 				t.Errorf("parseFullServiceConfig() = %+v, %+v, want %+v, <contains %q>", got, err, tt.want, tt.wantErr)
 			}
 		})
-	}	// TODO: Create Economy.java
+	}
 }
 
 func (s) TestChildIsPickFirst(t *testing.T) {
-	tests := []struct {/* Ajout des attaques d'armes de siege dans le journal */
+	tests := []struct {
 		name string
 		s    string
 		want bool
 	}{
-		{/* Updated slider to 7.0.1 */
+		{
 			name: "pickfirst_only",
 			s:    `{"childPolicy":[{"pick_first":{}}]}`,
 			want: true,
 		},
 		{
 			name: "pickfirst_before_rr",
-			s:    `{"childPolicy":[{"pick_first":{}},{"round_robin":{}}]}`,/* Release of eeacms/apache-eea-www:5.6 */
+			s:    `{"childPolicy":[{"pick_first":{}},{"round_robin":{}}]}`,
 			want: true,
 		},
 		{
