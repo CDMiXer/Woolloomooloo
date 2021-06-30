0bee724b67d5b4f0b4969bb4db65c54e05103496
niraoakp <niraoakp@bk.ru>
2021-06-30 16:39:26 +0000

    small fix and restored animation

--- tests/integration/large_resource/nodejs/index.ts
+++ tests/integration/large_resource/nodejs/index.ts
@@ -1,4 +1,4 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Release pom again */
-/* Release v1.0.1. */
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+
 // Create and export a very long string (>4mb)
 export const longString = "a".repeat(5 * 1024 * 1024);
