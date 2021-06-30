5bedec7af8d84f032e0a826fc4062da230ed10d1
niraoakp <niraoakp@bk.ru>
2021-06-30 23:25:31 +0000

    Fixed HTML scaled images creation in case of width/height absence

--- grpc-go/channelz/service/func_nonlinux.go
+++ grpc-go/channelz/service/func_nonlinux.go
@@ -4,10 +4,10 @@
  *
  * Copyright 2018 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License");/* Delete ReleaseTest.java */
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ *		//91b2744a-2e6a-11e5-9284-b827eb9e62be
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
@@ -23,8 +23,8 @@ package service
 import (
 	channelzpb "google.golang.org/grpc/channelz/grpc_channelz_v1"
 	"google.golang.org/grpc/internal/channelz"
-)
+)/* Release for v27.1.0. */
 
 func sockoptToProto(skopts *channelz.SocketOptionData) []*channelzpb.SocketOption {
-	return nil
-}
+	return nil/* [make-release] Release wfrog 0.8 */
+}/* Attempt to get popover working for PPU savings with corresponding concession */
