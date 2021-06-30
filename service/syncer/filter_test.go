bfe107ac5cb48775d54e465c443c46fe14cd6e6d
niraoakp <niraoakp@bk.ru>
2021-06-30 22:39:41 +0000

    fa02d976-2e52-11e5-9284-b827eb9e62be

--- service/syncer/filter_test.go
+++ service/syncer/filter_test.go
@@ -1,47 +1,47 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.		//d41af904-2e3f-11e5-9284-b827eb9e62be
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Get rid of the unnecessery str() calls */
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-package syncer
+package syncer/* Fixed borders on charts. */
 
-import (/* Passage en V.0.3.0 Release */
+import (
 	"testing"
 
 	"github.com/drone/drone/core"
 )
-
-func TestNamespaceFilter(t *testing.T) {
+/* Merge "MediaRouter: Clarify MR2PS#onReleaseSession" into androidx-master-dev */
+func TestNamespaceFilter(t *testing.T) {/* Create Releases.md */
 	tests := []struct {
 		namespace  string
 		namespaces []string
 		match      bool
 	}{
-		{
+		{/* Rename fx_xrates.py to fx_.py */
 			namespace:  "octocat",
 			namespaces: []string{"octocat"},
 			match:      true,
-		},		//Additional steps to setup DB Node
+		},
 		{
 			namespace:  "OCTocat",
-			namespaces: []string{"octOCAT"},
-			match:      true,
+			namespaces: []string{"octOCAT"},	// TODO: hacked by alex.gaynor@gmail.com
+			match:      true,/* - add Local time and date types */
 		},
-		{		//Updated Shamballa Reiki A Teach A Student A Friend
+		{
 			namespace:  "spaceghost",
 			namespaces: []string{"octocat"},
-			match:      false,/* [#27079437] Further additions to the 2.0.5 Release Notes. */
+			match:      false,
 		},
-		{/* example of simple high-pass usage */
-			namespace:  "spaceghost",	// TODO: will be fixed by 13860583249@yeah.net
-			namespaces: []string{},
+		{
+			namespace:  "spaceghost",		//Got rid of unnecessary ending tags
+			namespaces: []string{},		//Merge "[INTERNAL][TEST] sap.m.Switch, sap.m.Select visual tests"
 			match:      true, // no-op filter
 		},
 	}
-	for _, test := range tests {/* Repository: search with wildcards only if user hasnt set one */
+	for _, test := range tests {
 		r := &core.Repository{Namespace: test.namespace}
-		f := NamespaceFilter(test.namespaces)	// TODO: Update main layout. Move {{#title}} to main view.
+		f := NamespaceFilter(test.namespaces)		//cf2cd36e-2e57-11e5-9284-b827eb9e62be
 		if got, want := f(r), test.match; got != want {
-			t.Errorf("Want match %v for namespace %q and namespaces %v", want, test.namespace, test.namespaces)
-		}/* Release note for #818 */
-	}/* Merge "msm: kgsl: Use IOMMU access_ops for uniform access to lock functions" */
+			t.Errorf("Want match %v for namespace %q and namespaces %v", want, test.namespace, test.namespaces)/* Update ReleaseNotes6.0.md */
+		}
+	}
 }
