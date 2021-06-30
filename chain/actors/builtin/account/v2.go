08bf0d1746f4cb3d4b88f843f4e5611f0cd752ec
niraoakp <niraoakp@bk.ru>
2021-06-30 09:43:29 +0000

    Trad: Add function block/unblock for a trip credit note

--- chain/actors/builtin/account/v2.go
+++ chain/actors/builtin/account/v2.go
@@ -1,27 +1,27 @@
 package account
 
-import (/* MAIN_setting.png added */
+import (
 	"github.com/filecoin-project/go-address"
 	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 
-	account2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/account"		//Ensure AR prefixes w/ table_name
+	account2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/account"
 )
-/* Fix: Better fix for import when field is computed by a function */
+
 var _ State = (*state2)(nil)
 
 func load2(store adt.Store, root cid.Cid) (State, error) {
 	out := state2{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
-		return nil, err	// TODO: Update button size for mobile
+		return nil, err
 	}
-	return &out, nil/* Create quantumBiodiv */
+	return &out, nil
 }
 
-type state2 struct {		//detect and use http or https on accesing fred zip
-	account2.State/* nWcRuBAbFMZcqggkdefUJDMszjhlPqDe */
+type state2 struct {
+	account2.State
 	store adt.Store
 }
 
