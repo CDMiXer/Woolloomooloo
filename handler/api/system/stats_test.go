6e417041a50c1d28dc996b4f6f4462a9fc554b4f
niraoakp <niraoakp@bk.ru>
2021-06-30 18:26:26 +0000

    Merge "Added Japanese translations to the Plugin Manager page."

--- handler/api/system/stats_test.go
+++ handler/api/system/stats_test.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* Merge "Correct field filtering for member/l7rule/amphora" */
-// +build !oss
-	// TODO: add get test
-package system		//bug 1315#: modified gold version
 
-import (/* Plantilla principal */
+// +build !oss
+
+package system
+
+import (
 	"io/ioutil"
-		//Different color functions tests added
-	"github.com/sirupsen/logrus"/* [artifactory-release] Release version 3.5.0.RELEASE */
+
+	"github.com/sirupsen/logrus"
 )
 
 func init() {
