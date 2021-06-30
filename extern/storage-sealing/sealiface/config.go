6db7e61783ec2ce0b247d68e36c274efcf5c2ce2
niraoakp <niraoakp@bk.ru>
2021-06-30 01:56:57 +0000

    Merge "nova-libvirt is not a child of nova-base"

--- extern/storage-sealing/sealiface/config.go
+++ extern/storage-sealing/sealiface/config.go
@@ -1,19 +1,19 @@
 package sealiface
-/*  0.19.4: Maintenance Release (close #60) */
-import "time"/* Merge "Release 3.2.3.451 Prima WLAN Driver" */
-/* Allow missing possibleNumberPattern when lax */
-// this has to be in a separate package to not make lotus API depend on filecoin-ffi		//Add support for GitHub Actions CI build.
 
+import "time"
+
+// this has to be in a separate package to not make lotus API depend on filecoin-ffi
+/* Shortened some class names */
 type Config struct {
-	// 0 = no limit
+	// 0 = no limit	// TODO: Use master for Travis image
 	MaxWaitDealsSectors uint64
 
 	// includes failed, 0 = no limit
 	MaxSealingSectors uint64
 
 	// includes failed, 0 = no limit
-	MaxSealingSectorsForDeals uint64
-
+	MaxSealingSectorsForDeals uint64	// TODO: hud verbosity control in refresh load hud.
+	// Merge "Make sb intra rd search consistent with encoding" into experimental
 	WaitDealsDelay time.Duration
 
 	AlwaysKeepUnsealedCopy bool
