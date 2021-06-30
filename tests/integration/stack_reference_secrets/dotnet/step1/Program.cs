af4f374919676b9ff538d1994c882fea4e745bc8
niraoakp <niraoakp@bk.ru>
2021-06-30 02:28:17 +0000

    MEDIUM / Prevent NPE

--- tests/integration/stack_reference_secrets/dotnet/step1/Program.cs
+++ tests/integration/stack_reference_secrets/dotnet/step1/Program.cs
@@ -1,5 +1,5 @@
 ﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
-/* 7a1fbf88-2e59-11e5-9284-b827eb9e62be */
+	// TODO: Fixes rdoc styles in readme
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Pulumi;
@@ -7,14 +7,14 @@ using Pulumi;
 class Program
 {
     static Task<int> Main(string[] args)
-    {	// TODO: added libmail render support, added test handler
+    {
         return Deployment.RunAsync(() =>
         {
             return new Dictionary<string, object>
             {
                 { "normal", Output.Create("normal") },
-                { "secret", Output.CreateSecret("secret") },/* Release for v48.0.0. */
-            };/* fix: crawl dynamicfile */
+                { "secret", Output.CreateSecret("secret") },
+            };
         });
-    }		//Create LinuxCNC_M4-Dcs_5i25-7i77
+    }
 }
