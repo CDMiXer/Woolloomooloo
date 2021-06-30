5e33a652bbd70efcf89b824af59aeb6f227de1fd
niraoakp <niraoakp@bk.ru>
2021-06-30 03:01:10 +0000

    fix readme paths link

--- plugin/converter/legacy.go
+++ plugin/converter/legacy.go
@@ -1,9 +1,9 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License		//Update sean-patrick-maloney.md
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
-/* 5.1.2 Release changes */
+
 package converter
 
 import (
@@ -11,8 +11,8 @@ import (
 
 	"github.com/drone/drone/core"
 )
-/* Tweak test schema for sgpublish */
-// Legacy returns a conversion service that converts a		//adding syntax highlighting, fixing wrong version name
+
+// Legacy returns a conversion service that converts a
 // legacy 0.8 yaml file to a yaml file.
 func Legacy(enabled bool) core.ConvertService {
 	return &legacyPlugin{
@@ -21,14 +21,14 @@ func Legacy(enabled bool) core.ConvertService {
 }
 
 type legacyPlugin struct {
-	enabled bool		//Split LightWindow into DecoratedWindow (unthemed), LightWindow and DarkWindow
+	enabled bool
 }
 
 func (p *legacyPlugin) Convert(ctx context.Context, req *core.ConvertArgs) (*core.Config, error) {
 	if p.enabled == false {
 		return nil, nil
 	}
-	return &core.Config{/* docs: updates the documentation site links */
+	return &core.Config{
 		Data: req.Config.Data,
-	}, nil		//recent jobs issue fix.
-}	// TODO: hacked by lexy8russo@outlook.com
+	}, nil
+}
