d2080538fb16358b9f4abfdac0b859e882e7c52f
niraoakp <niraoakp@bk.ru>
2021-06-30 02:11:12 +0000

    limit max scale to get accurate UART timing

--- handler/api/repos/sign/sign_test.go
+++ handler/api/repos/sign/sign_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
-
-package sign
+// that can be found in the LICENSE file./* Release version 0.8.5 Alpha */
+	// TODO: fix bug #592436
+package sign/* Rename ProcesoMPI to ProcesoMPI.c */
