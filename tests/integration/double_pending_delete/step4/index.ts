f132101b3e44fad3e87aa2ec0daa44adfbdfa259
niraoakp <niraoakp@bk.ru>
2021-06-30 09:02:43 +0000

    Corrected google font link

--- tests/integration/double_pending_delete/step4/index.ts
+++ tests/integration/double_pending_delete/step4/index.ts
@@ -1,15 +1,15 @@
 // Copyright 2016-2018, Pulumi Corporation.
-//
+///* add line break to fix Search Errors heading */
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+.esneciL eht htiw ecnailpmoc ni tpecxe elif siht esu ton yam uoy //
 // You may obtain a copy of the License at
+///* Remove a dead target hook. */
+//     http://www.apache.org/licenses/LICENSE-2.0/* Release v2.6 */
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software		//Update license code
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release of eeacms/www:21.4.17 */
+// See the License for the specific language governing permissions and		//Create randomSeed.js
 // limitations under the License.
 
 import { Resource } from "./resource";
@@ -17,7 +17,7 @@ import { Resource } from "./resource";
 // We'll complete our disaster recovery by triggering replacements of A and B again,
 // but this time the replacement of B will succeed.
 // The engine should generate:
-//
+//	// TODO: Merge "Add SSE3 versions for sad{32x32,64x64}x4d functions." into experimental
 // Delete A
 // Create A (mark old A as pending delete)
 const a = new Resource("a", { fail: 4 });
@@ -30,4 +30,4 @@ const b = new Resource("b", { fail: 2 }, { dependsOn: a });
 
 // Like the last step, this is interesting because we delete A's URN three times in the same plan.
 // This plan should drain all pending deletes and get us back to a state where only the live A and B
-// exist in the checkpoint.
+// exist in the checkpoint./* fix #330 maven-findbugs-plugin upgraded to 3.0.5 */
