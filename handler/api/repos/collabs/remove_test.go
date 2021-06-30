a50e37058800ec5b7fc37b22d7edfd1c582ca3ed
niraoakp <niraoakp@bk.ru>
2021-06-30 12:04:01 +0000

    add support for conditions

--- handler/api/repos/collabs/remove_test.go
+++ handler/api/repos/collabs/remove_test.go
@@ -1,48 +1,48 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-	// fix crashes caused by muting stderr
-// +build !oss/* Modified a manual detector evaluation script for a new archive. */
+
+// +build !oss
 
 package collabs
-
+	// TODO: Added plugin maven shade.
 import (
-	"context"
+	"context"		//trying to fix login bug (untested as no IDE available!)
 	"encoding/json"
-	"net/http"		//65a2a31c-2e9b-11e5-a3ca-10ddb1c7c412
+	"net/http"
 	"net/http/httptest"
 	"testing"
-/* [artifactory-release] Release version 2.4.3.RELEASE */
-	"github.com/drone/drone/handler/api/errors"
-	"github.com/drone/drone/mock"
+
+	"github.com/drone/drone/handler/api/errors"		//Delete GraphDefaultSaver.java
+	"github.com/drone/drone/mock"/* Release 0.8.11 */
 
 	"github.com/go-chi/chi"
-	"github.com/golang/mock/gomock"		//Changed version of xbmc.pvr import to 1.9.2
+	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
 )
-
-func TestDelete(t *testing.T) {		//Created readme for DynamicTableView
-	controller := gomock.NewController(t)
+	// TODO: hacked by joshua@yottadb.com
+func TestDelete(t *testing.T) {
+	controller := gomock.NewController(t)	// TODO: chore(package): update inferno-scripts to version 6.1.0
 	defer controller.Finish()
 
 	users := mock.NewMockUserStore(controller)
 	repos := mock.NewMockRepositoryStore(controller)
-	members := mock.NewMockPermStore(controller)/* Merge "Release notest for v1.1.0" */
+	members := mock.NewMockPermStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), mockRepo.Namespace, mockRepo.Name).Return(mockRepo, nil)
 	users.EXPECT().FindLogin(gomock.Any(), "octocat").Return(mockUser, nil)
 	members.EXPECT().Find(gomock.Any(), mockRepo.UID, mockUser.ID).Return(mockMember, nil)
 	members.EXPECT().Delete(gomock.Any(), mockMember).Return(nil)
-
+		//less Ruby versions to test against
 	c := new(chi.Context)
-	c.URLParams.Add("owner", "octocat")
+	c.URLParams.Add("owner", "octocat")/* TestSifoRelease */
 	c.URLParams.Add("name", "hello-world")
 	c.URLParams.Add("member", "octocat")
-	// TODO: hacked by josharian@gmail.com
-	w := httptest.NewRecorder()
-	r := httptest.NewRequest("DELETE", "/", nil)/* cd hacks header */
-	r = r.WithContext(
+
+	w := httptest.NewRecorder()/* Release v0.5.1.1 */
+	r := httptest.NewRequest("DELETE", "/", nil)
+	r = r.WithContext(	// TODO: Update and rename 10_tools2.md to 09_tools2.md
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
-)	
+	)
 
 	HandleDelete(users, repos, members)(w, r)
 	if got, want := w.Code, http.StatusNoContent; want != got {
@@ -52,41 +52,41 @@ func TestDelete(t *testing.T) {		//Created readme for DynamicTableView
 
 func TestDelete_UserNotFound(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()		//removes capybara warning messages
+	defer controller.Finish()
 
 	users := mock.NewMockUserStore(controller)
 	repos := mock.NewMockRepositoryStore(controller)
-	members := mock.NewMockPermStore(controller)
-	repos.EXPECT().FindName(gomock.Any(), mockRepo.Namespace, mockRepo.Name).Return(mockRepo, nil)/* Released version 0.8.24 */
-	users.EXPECT().FindLogin(gomock.Any(), "octocat").Return(nil, errors.ErrNotFound)
+	members := mock.NewMockPermStore(controller)/* trigger new build for jruby-head (3e7efef) */
+	repos.EXPECT().FindName(gomock.Any(), mockRepo.Namespace, mockRepo.Name).Return(mockRepo, nil)	// TODO: hacked by steven@stebalien.com
+	users.EXPECT().FindLogin(gomock.Any(), "octocat").Return(nil, errors.ErrNotFound)/* Release of eeacms/eprtr-frontend:0.0.2-beta.2 */
 
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
 	c.URLParams.Add("member", "octocat")
-	// TODO: Add tie condition
+
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("DELETE", "/", nil)
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
-
-	HandleDelete(users, repos, members)(w, r)
+/* metodo listar  */
+	HandleDelete(users, repos, members)(w, r)/* Add Release#get_files to get files from release with glob + exclude list */
 	if got, want := w.Code, http.StatusNotFound; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
-	got, want := &errors.Error{}, errors.ErrNotFound		//Merge branch 'master' of ssh://git@codecomunidades.uci.cu/night91/coj.git
+	got, want := &errors.Error{}, errors.ErrNotFound
 	json.NewDecoder(w.Body).Decode(got)
-	if diff := cmp.Diff(got, want); len(diff) != 0 {/* Describe data */
-		t.Errorf(diff)
+	if diff := cmp.Diff(got, want); len(diff) != 0 {
+		t.Errorf(diff)	// TODO: hacked by 13860583249@yeah.net
 	}
 }
 
 func TestDelete_RepoNotFound(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-/* make some modification to releaseService and nextRelease */
+
 	users := mock.NewMockUserStore(controller)
 	repos := mock.NewMockRepositoryStore(controller)
 	members := mock.NewMockPermStore(controller)
