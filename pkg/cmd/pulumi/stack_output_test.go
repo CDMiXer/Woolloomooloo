a825ea1dbc034c0cf1ac15a14ecce9db330714ad
niraoakp <niraoakp@bk.ru>
2021-06-30 03:29:22 +0000

    Add support for DISTINCT.

--- pkg/cmd/pulumi/stack_output_test.go
+++ pkg/cmd/pulumi/stack_output_test.go
@@ -1,8 +1,8 @@
 // Copyright 2016-2018, Pulumi Corporation.
-//	// TODO: Update for more customizability
-// Licensed under the Apache License, Version 2.0 (the "License");/* Merge "Set concurrency=1 for system log and scheduler queues" */
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// TODO: will be fixed by cory@protocol.ai
+// You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
@@ -10,29 +10,29 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Release v0.6.0.2 */
-	// TODO: will be fixed by xaber.twt@gmail.com
+// limitations under the License.
+
 package main
-/* loop every 10th of a second, not every 1/2 */
-import (	// TODO: will be fixed by denner@gmail.com
-	"testing"/* Fixes URL for Github Release */
+
+import (
+	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
-		//Set tornado version requirement
-func TestStringifyOutput(t *testing.T) {	// Create source list for Debian 6.0 Squeeze
+
+func TestStringifyOutput(t *testing.T) {
 	num := 42
 	str := "ABC"
 	arr := []string{"hello", "goodbye"}
 	obj := map[string]interface{}{
 		"foo": 42,
 		"bar": map[string]interface{}{
-			"baz": true,		//ignore reuse variable
+			"baz": true,
 		},
-	}		//Filled in weapon DB, still finishing god-forsaken notes section
+	}
 
 	assert.Equal(t, "42", stringifyOutput(num))
 	assert.Equal(t, "ABC", stringifyOutput(str))
 	assert.Equal(t, "[\"hello\",\"goodbye\"]", stringifyOutput(arr))
-	assert.Equal(t, "{\"bar\":{\"baz\":true},\"foo\":42}", stringifyOutput(obj))/* 4ac360b0-2e71-11e5-9284-b827eb9e62be */
+	assert.Equal(t, "{\"bar\":{\"baz\":true},\"foo\":42}", stringifyOutput(obj))
 }
