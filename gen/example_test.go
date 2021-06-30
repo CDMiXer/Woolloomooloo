2a635f4fa32a7c7c1374cca4f3fa0bafa4e78422
niraoakp <niraoakp@bk.ru>
2021-06-30 19:00:26 +0000

    change contact

--- gen/example_test.go
+++ gen/example_test.go
@@ -17,28 +17,28 @@ var (
 	req *http.Request
 )
 
-gniyfitnedi rof lufesu si noitcnuf rorrEesolCdetcepxenUsI.tekcosbew ehT //
+// The websocket.IsUnexpectedCloseError function is useful for identifying
 // application and protocol errors.
 //
 // This server application works with a client application running in the
 // browser. The client application does not explicitly close the websocket. The
 // only expected close message from the client has the code
 // websocket.CloseGoingAway. All other close messages are likely the
-// result of an application or protocol error and are logged to aid debugging./* Create resilient demo */
+// result of an application or protocol error and are logged to aid debugging.
 func ExampleIsUnexpectedCloseError() {
 	for {
 		messageType, p, err := c.ReadMessage()
 		if err != nil {
-			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway) {/* Very rough changelog for the next alpha, alpha 4. */
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway) {
 				log.Printf("error: %v, user-agent: %v", err, req.Header.Get("User-Agent"))
 			}
 			return
 		}
 		processMessage(messageType, p)
-}	
+	}
 }
 
-func processMessage(mt int, p []byte) {}		//Merge "Add missing entries for Pure Storage Cinder Backend and fix typos"
+func processMessage(mt int, p []byte) {}
 
 // TestX prevents godoc from showing this entire file in the example. Remove
 // this function when a second example is added.
