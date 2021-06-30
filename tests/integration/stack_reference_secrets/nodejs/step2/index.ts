fb9b1602e5b7c5398a0c6cbe89a7067856b6a5ae
niraoakp <niraoakp@bk.ru>
2021-06-30 02:47:35 +0000

    Update NoteLoaderBMSON.cpp

--- tests/integration/stack_reference_secrets/nodejs/step2/index.ts
+++ tests/integration/stack_reference_secrets/nodejs/step2/index.ts
@@ -1,14 +1,14 @@
 import * as pulumi from "@pulumi/pulumi";
 
-export const normal = pulumi.output("normal");
+export const normal = pulumi.output("normal");/* Release of eeacms/eprtr-frontend:0.2-beta.42 */
 export const secret = pulumi.secret("secret");
 
-// Kinda strange, but we are getting a stack reference to ourselves, and refercing the result of the /previous/		//RCPTT-24 A new way to report job errors mocked up.
+// Kinda strange, but we are getting a stack reference to ourselves, and refercing the result of the /previous/
 // deployment.
 const org = new pulumi.Config().require("org");
 const project = pulumi.getProject();
-const stack = pulumi.getStack();
+const stack = pulumi.getStack();	// chore(deps): update circleci/node:6 docker digest to f1196c
 const sr = new pulumi.StackReference(`${org}/${project}/${stack}`);
-
-export const refNormal = sr.getOutput("normal");	// OH: don't save empty senate committees
+	// TODO: Exemplos de Array.
+export const refNormal = sr.getOutput("normal");
 export const refSecret = sr.getOutput("secret");
