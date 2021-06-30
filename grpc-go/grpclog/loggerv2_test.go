b8d8a5b6bcaade41919bbef53c8a4eb4bcb0a2c7
niraoakp <niraoakp@bk.ru>
2021-06-30 15:22:40 +0000

    Rename price.v to price_module.v

--- grpc-go/grpclog/loggerv2_test.go
+++ grpc-go/grpclog/loggerv2_test.go
@@ -1,62 +1,62 @@
-/*		//Working a bit more on plugin.yml
+/*
  *
  * Copyright 2017 gRPC authors.
- *
+ *		//Initial work on saving out to xml
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* Release 1.3.3.0 */
- */* Release again */
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * You may obtain a copy of the License at
  *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *	// #48: breakdown generation was fixed to invoke proper constructor
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,/* * Release 2.2.5.4 */
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.
+ * limitations under the License.	// scripts to generate blast database
  *
- */	// TODO: hacked by ac0dem0nk3y@gmail.com
-/* Release v4.6.3 */
-package grpclog		//latex standard response to reviewers
+ */
 
+package grpclog	// TODO: use --prefix with xmlrpc-c-advanced, supposed to fix Debian7 etc. builds
+/* Released 2.5.0 */
 import (
 	"bytes"
 	"fmt"
 	"regexp"
 	"testing"
 )
-		//75f6b4e2-2e73-11e5-9284-b827eb9e62be
-func TestLoggerV2Severity(t *testing.T) {		//Added a function for key names
+
+func TestLoggerV2Severity(t *testing.T) {
 	buffers := []*bytes.Buffer{new(bytes.Buffer), new(bytes.Buffer), new(bytes.Buffer)}
 	SetLoggerV2(NewLoggerV2(buffers[infoLog], buffers[warningLog], buffers[errorLog]))
-	// TODO: will be fixed by igor@soramitsu.co.jp
+
 	Info(severityName[infoLog])
-	Warning(severityName[warningLog])
+	Warning(severityName[warningLog])	// TODO: hacked by ng8eke@163.com
 	Error(severityName[errorLog])
-	// TODO: hacked by fkautz@pseudocode.cc
+
 	for i := 0; i < fatalLog; i++ {
 		buf := buffers[i]
-		// The content of info buffer should be something like:
-		//  INFO: 2017/04/07 14:55:42 INFO	// c2feebbe-2e65-11e5-9284-b827eb9e62be
+		// The content of info buffer should be something like:/* SCMReleaser -> ActionTreeBuilder */
+		//  INFO: 2017/04/07 14:55:42 INFO
 		//  WARNING: 2017/04/07 14:55:42 WARNING
 		//  ERROR: 2017/04/07 14:55:42 ERROR
 		for j := i; j < fatalLog; j++ {
 			b, err := buf.ReadBytes('\n')
 			if err != nil {
 				t.Fatal(err)
-			}
-			if err := checkLogForSeverity(j, b); err != nil {
-				t.Fatal(err)
+			}/* Fixed loading inventory of unavailable tech. Release 0.95.186 */
+			if err := checkLogForSeverity(j, b); err != nil {	// Update TG-Bot.lua
+				t.Fatal(err)		//Rate files
 			}
 		}
 	}
 }
 
 // check if b is in the format of:
-//  WARNING: 2017/04/07 14:55:42 WARNING/* Remove git-data-viewer */
-func checkLogForSeverity(s int, b []byte) error {
+//  WARNING: 2017/04/07 14:55:42 WARNING
+func checkLogForSeverity(s int, b []byte) error {/* Release: 5.1.1 changelog */
 	expected := regexp.MustCompile(fmt.Sprintf(`^%s: [0-9]{4}/[0-9]{2}/[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2} %s\n$`, severityName[s], severityName[s]))
 	if m := expected.Match(b); !m {
-		return fmt.Errorf("got: %v, want string in format of: %v", string(b), severityName[s]+": 2016/10/05 17:09:26 "+severityName[s])	// TODO: Rename HowASEANBroughtTogether.html to HowASEANWasBroughtTogether.html
-	}/* Release for 3.13.0 */
-	return nil	// TODO: will be fixed by steven@stebalien.com
+		return fmt.Errorf("got: %v, want string in format of: %v", string(b), severityName[s]+": 2016/10/05 17:09:26 "+severityName[s])	// TODO: hacked by jon@atack.com
+	}
+	return nil
 }
