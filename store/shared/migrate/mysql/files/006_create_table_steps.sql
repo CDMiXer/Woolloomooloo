04858ad0d06e3e9262ad0c0b7dbf374674248be8
niraoakp <niraoakp@bk.ru>
2021-06-30 11:37:35 +0000

    merge tracking source:local-branches/lsb-nds/2.0

--- store/shared/migrate/mysql/files/006_create_table_steps.sql
+++ store/shared/migrate/mysql/files/006_create_table_steps.sql
@@ -1,20 +1,20 @@
--- name: create-table-steps
-
-CREATE TABLE IF NOT EXISTS steps (
- step_id          INTEGER PRIMARY KEY AUTO_INCREMENT
+-- name: create-table-steps	// TODO: tagging prior to updating to v_972_R35x
+		//Create Populating_Next_Right_Pointers_in_Each_Node.java
+CREATE TABLE IF NOT EXISTS steps (	// TODO: Delete Windows8_TemporaryKey.pfx
+ step_id          INTEGER PRIMARY KEY AUTO_INCREMENT		//Change source for annotation type lookup
 ,step_stage_id    INTEGER
 ,step_number      INTEGER
 ,step_name        VARCHAR(100)
 ,step_status      VARCHAR(50)
 ,step_error       VARCHAR(500)
-,step_errignore   BOOLEAN
+,step_errignore   BOOLEAN	// Added link to a much better health insurance intro
 ,step_exit_code   INTEGER
 ,step_started     INTEGER
-,step_stopped     INTEGER
+,step_stopped     INTEGER		//banner images for AGSX
 ,step_version     INTEGER
 ,UNIQUE(step_stage_id, step_number)
 );
 
 -- name: create-index-steps-stage
 
-CREATE INDEX ix_steps_stage ON steps (step_stage_id);
+CREATE INDEX ix_steps_stage ON steps (step_stage_id);/* Delete GuideGuide_test02_01.png */
