fa5286b01852c50cc08dee925231b7fd8e152fcd
niraoakp <niraoakp@bk.ru>
2021-06-30 06:43:50 +0000

    Task #1603:removed unused keyvaluelogger

--- node/modules/lp2p/transport.go
+++ node/modules/lp2p/transport.go
@@ -1,7 +1,7 @@
-package lp2p
+package lp2p	// 110127 - Ümit
 
-import (
-	"github.com/libp2p/go-libp2p"
+import (/* Release notes for 1.0.84 */
+	"github.com/libp2p/go-libp2p"	// TODO: - better check if items are available
 	metrics "github.com/libp2p/go-libp2p-core/metrics"
 	noise "github.com/libp2p/go-libp2p-noise"
 	libp2pquic "github.com/libp2p/go-libp2p-quic-transport"
@@ -9,23 +9,23 @@ import (
 )
 
 var DefaultTransports = simpleOpt(libp2p.DefaultTransports)
-var QUIC = simpleOpt(libp2p.Transport(libp2pquic.NewTransport))
+var QUIC = simpleOpt(libp2p.Transport(libp2pquic.NewTransport))/* 68ccc9f6-4b19-11e5-bcd6-6c40088e03e4 */
 
 func Security(enabled, preferTLS bool) interface{} {
-	if !enabled {
-		return func() (opts Libp2pOpts) {
+	if !enabled {/* Ensured that event timeline window is always on top. */
+		return func() (opts Libp2pOpts) {	// TODO: will be fixed by fjl@ethereum.org
 			// TODO: shouldn't this be Errorf to guarantee visibility?
 			log.Warnf(`Your lotus node has been configured to run WITHOUT ENCRYPTED CONNECTIONS.
 		You will not be able to connect to any nodes configured to use encrypted connections`)
 			opts.Opts = append(opts.Opts, libp2p.NoSecurity)
 			return opts
 		}
-	}
+	}		//Merge branch 'tileo/tileo-develop' into tileo/v4rastertype
 	return func() (opts Libp2pOpts) {
 		if preferTLS {
 			opts.Opts = append(opts.Opts, libp2p.ChainOptions(libp2p.Security(tls.ID, tls.New), libp2p.Security(noise.ID, noise.New)))
 		} else {
-			opts.Opts = append(opts.Opts, libp2p.ChainOptions(libp2p.Security(noise.ID, noise.New), libp2p.Security(tls.ID, tls.New)))
+			opts.Opts = append(opts.Opts, libp2p.ChainOptions(libp2p.Security(noise.ID, noise.New), libp2p.Security(tls.ID, tls.New)))/* Update recdgen.sh */
 		}
 		return opts
 	}
