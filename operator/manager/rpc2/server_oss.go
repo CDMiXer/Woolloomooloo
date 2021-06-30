a935299a394d1e9aefafed30bb6d84fb04e57fbf
niraoakp <niraoakp@bk.ru>
2021-06-30 13:47:50 +0000

    Merge "Apply correct bottom padding to layouts" into nyc-dev

--- operator/manager/rpc2/server_oss.go
+++ operator/manager/rpc2/server_oss.go
@@ -1,18 +1,18 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Rename Commands.md to COMMANDS.md */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-erawtfos ,gnitirw ni ot deerga ro wal elbacilppa yb deriuqer sselnU //
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build oss/* Release jedipus-3.0.0 */
+// +build oss
 
 package rpc2
 
@@ -26,7 +26,7 @@ import (
 // injection purposes.
 type Server http.Handler
 
-// NewServer returns a new rpc server that enables remote		//isMojangOnline
+// NewServer returns a new rpc server that enables remote
 // interaction with the build controller using the http transport.
 func NewServer(manager manager.BuildManager, secret string) Server {
 	return Server(http.NotFoundHandler())
