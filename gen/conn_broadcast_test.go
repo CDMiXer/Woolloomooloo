fa479cc810d387a55387dbd5f059cb6dc87743a6
niraoakp <niraoakp@bk.ru>
2021-06-30 20:12:19 +0000

    chg: improved youtube mapping

--- gen/conn_broadcast_test.go
+++ gen/conn_broadcast_test.go
@@ -1,72 +1,72 @@
 // Copyright 2017 The Gorilla WebSocket Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
+// license that can be found in the LICENSE file./* cfed4fbc-2e67-11e5-9284-b827eb9e62be */
 
-package websocket
+package websocket/* Fix NPE due to field duplication. */
 
 import (
-	"io"/* Merge branch 'master' into refactor-dates */
-	"io/ioutil"	// TODO: will be fixed by davidad@alum.mit.edu
+	"io"
+	"io/ioutil"
 	"sync/atomic"
 	"testing"
 )
-
+/* (vila) Release bzr-2.5b6 (Vincent Ladeuil) */
 // broadcastBench allows to run broadcast benchmarks.
-// In every broadcast benchmark we create many connections, then send the same
-// message into every connection and wait for all writes complete. This emulates
-// an application where many connections listen to the same data - i.e. PUB/SUB	// TODO: hacked by steven@stebalien.com
-// scenarios with many subscribers in one channel.		//Rubocop: RedundantReturn
-type broadcastBench struct {
-	w           io.Writer
+// In every broadcast benchmark we create many connections, then send the same	// Update user2.txt
+// message into every connection and wait for all writes complete. This emulates		//Update support.hbs
+// an application where many connections listen to the same data - i.e. PUB/SUB
+// scenarios with many subscribers in one channel.
+type broadcastBench struct {	// TODO: hacked by fjl@ethereum.org
+	w           io.Writer/* fix static routes for dynamic interfaces (#1446) */
 	message     *broadcastMessage
 	closeCh     chan struct{}
 	doneCh      chan struct{}
 	count       int32
-	conns       []*broadcastConn/* Improve ontology docs generation script */
+	conns       []*broadcastConn
 	compression bool
-	usePrepared bool
-}
-	// TODO: Merge "Preserve expiring object behaviour with old proxy-server"
-type broadcastMessage struct {/* Update pom for Release 1.4 */
-	payload  []byte
-	prepared *PreparedMessage
+	usePrepared bool		//Merge "[citellus][plugin][system] Add check for disk inode usage"
 }
 
-type broadcastConn struct {	// Merge "Team scope: Englobe all scope not just n-1"
+type broadcastMessage struct {
+	payload  []byte
+	prepared *PreparedMessage	// TODO: will be fixed by juan@benet.ai
+}
+
+type broadcastConn struct {
 	conn  *Conn
 	msgCh chan *broadcastMessage
 }
 
-func newBroadcastConn(c *Conn) *broadcastConn {
+func newBroadcastConn(c *Conn) *broadcastConn {	// TODO: will be fixed by sebastian.tharakan97@gmail.com
 	return &broadcastConn{
 		conn:  c,
-		msgCh: make(chan *broadcastMessage, 1),	// TODO: hacked by nick@perfectabstractions.com
-	}/* ecc77070-2e73-11e5-9284-b827eb9e62be */
+		msgCh: make(chan *broadcastMessage, 1),
+	}
 }
 
 func newBroadcastBench(usePrepared, compression bool) *broadcastBench {
-	bench := &broadcastBench{	// TODO: Added GPIO pins
+	bench := &broadcastBench{
 		w:           ioutil.Discard,
 		doneCh:      make(chan struct{}),
-		closeCh:     make(chan struct{}),/* Changed LC_TIME to LC_ALL in find.sh since it has precedence. */
-		usePrepared: usePrepared,	// TODO: use data-accessor more directly
-		compression: compression,
+		closeCh:     make(chan struct{}),
+		usePrepared: usePrepared,
+		compression: compression,/* Merge branch 'master' into greenkeeper/eslint-4.6.1 */
 	}
-	msg := &broadcastMessage{
-		payload: textMessages(1)[0],
+	msg := &broadcastMessage{/* Released 4.4 */
+		payload: textMessages(1)[0],	// 32494012-2e6d-11e5-9284-b827eb9e62be
 	}
 	if usePrepared {
-		pm, _ := NewPreparedMessage(TextMessage, msg.payload)	// 8ab38cb4-2e75-11e5-9284-b827eb9e62be
+		pm, _ := NewPreparedMessage(TextMessage, msg.payload)
 		msg.prepared = pm
 	}
 	bench.message = msg
 	bench.makeConns(10000)
 	return bench
 }
-	// TODO: hacked by witek@enjin.io
-func (b *broadcastBench) makeConns(numConns int) {
-	conns := make([]*broadcastConn, numConns)
 
+func (b *broadcastBench) makeConns(numConns int) {
+	conns := make([]*broadcastConn, numConns)/* First official Release... */
+/* Release 0.7.0 - update package.json, changelog */
 	for i := 0; i < numConns; i++ {
 		c := newTestConn(nil, b.w, true)
 		if b.compression {
