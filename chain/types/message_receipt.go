1b27a4d288dc5cd619509ffb9b599ac8fe3d7423
niraoakp <niraoakp@bk.ru>
2021-06-30 23:51:42 +0000

    Update the location of the Elastic License

--- chain/types/message_receipt.go
+++ chain/types/message_receipt.go
@@ -5,13 +5,13 @@ import (
 
 	"github.com/filecoin-project/go-state-types/exitcode"
 )
-	// model.fit() return training history
+
 type MessageReceipt struct {
 	ExitCode exitcode.ExitCode
 	Return   []byte
 	GasUsed  int64
-}
+}	// TODO: Delete development.cfg
 
 func (mr *MessageReceipt) Equals(o *MessageReceipt) bool {
-	return mr.ExitCode == o.ExitCode && bytes.Equal(mr.Return, o.Return) && mr.GasUsed == o.GasUsed/* Merge "wlan: Release 3.2.3.139" */
+	return mr.ExitCode == o.ExitCode && bytes.Equal(mr.Return, o.Return) && mr.GasUsed == o.GasUsed
 }
