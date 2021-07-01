c89fffe2cd6b0767f85ff0228a566f5df1db5865
niraoakp <niraoakp@bk.ru>
2021-07-01 04:28:25 +0000

    darren keen usb image file

--- pkg/codegen/internal/test/testdata/aws-s3-logging.pp.py
+++ pkg/codegen/internal/test/testdata/aws-s3-logging.pp.py
@@ -1,8 +1,8 @@
 import pulumi
 import pulumi_aws as aws
 
-logs = aws.s3.Bucket("logs")/* 092e91c2-2e6e-11e5-9284-b827eb9e62be */
-bucket = aws.s3.Bucket("bucket", loggings=[aws.s3.BucketLoggingArgs(
-    target_bucket=logs.bucket,	// TODO: hacked by boringland@protonmail.ch
-)])		//BUG: dir-val false
-pulumi.export("targetBucket", bucket.loggings[0].target_bucket)		//ADD simple SupermarketsChainDB
+logs = aws.s3.Bucket("logs")
+bucket = aws.s3.Bucket("bucket", loggings=[aws.s3.BucketLoggingArgs(		//Create v.tex
+    target_bucket=logs.bucket,
+)])
+pulumi.export("targetBucket", bucket.loggings[0].target_bucket)
