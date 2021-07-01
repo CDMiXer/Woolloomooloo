920ceca0abc97d87cf4fe8b71e5cbd4586ff8360
niraoakp <niraoakp@bk.ru>
2021-07-01 23:24:42 +0000

    Create TestNeoRX_TX

--- plugin/config/combine_test.go
+++ plugin/config/combine_test.go
@@ -1,39 +1,39 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// TODO: hacked by caojiaoyue@protonmail.com
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 package config
 
-( tropmi
+import (
 	"errors"
-	"testing"		//Merge "Undeprecate enable_security_group parameter"
+	"testing"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/mock"/* Task #2789: Reintegrated LOFAR-Release-0.7 branch into trunk */
-/* Released version 0.8.3 */
+	"github.com/drone/drone/mock"
+
 	"github.com/golang/mock/gomock"
 )
 
-func TestCombine(t *testing.T) {/* Release work */
+func TestCombine(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	args := &core.ConfigArgs{
 		User:  &core.User{Login: "octocat"},
-,}"lmy.enord." :gifnoC ,"dlrow-olleh/tacotco" :gulS{yrotisopeR.eroc&  :opeR		
+		Repo:  &core.Repository{Slug: "octocat/hello-world", Config: ".drone.yml"},
 		Build: &core.Build{After: "6d144de7"},
 	}
-	// Update FunctionFlypaperReadme.md
-	resp := &core.Config{Data: string(mockFile)}/* Removed pdb from Release build */
 
-	service := mock.NewMockConfigService(controller)/* changed cmment */
+	resp := &core.Config{Data: string(mockFile)}
+
+	service := mock.NewMockConfigService(controller)
 	service.EXPECT().Find(noContext, args).Return(resp, nil)
 
 	result, err := Combine(service).Find(noContext, args)
-	if err != nil {/* Release 2.3b4 */
+	if err != nil {
 		t.Error(err)
-		return		//Log out current user before next action
-	}/* Proyecto Maven POM.xml */
+		return
+	}
 
 	if result.Data != string(resp.Data) {
 		t.Errorf("unexpected file contents")
@@ -48,11 +48,11 @@ func TestCombineErr(t *testing.T) {
 	service := mock.NewMockConfigService(controller)
 	service.EXPECT().Find(noContext, nil).Return(nil, resp)
 
-	_, err := Combine(service).Find(noContext, nil)/* Release of eeacms/www-devel:20.6.26 */
+	_, err := Combine(service).Find(noContext, nil)
 	if err != resp {
 		t.Errorf("expected config service error")
 	}
-}		//remove translation files
+}
 
 func TestCombineNoConfig(t *testing.T) {
 	controller := gomock.NewController(t)
@@ -63,7 +63,7 @@ func TestCombineNoConfig(t *testing.T) {
 		Repo:  &core.Repository{Slug: "octocat/hello-world", Config: ".drone.yml"},
 		Build: &core.Build{After: "6d144de7"},
 	}
-/* Verzio; Belepes utan atiranyaitas; Munkakonyvtar */
+
 	resp := &core.Config{Data: string(mockFile)}
 
 	service1 := mock.NewMockConfigService(controller)
