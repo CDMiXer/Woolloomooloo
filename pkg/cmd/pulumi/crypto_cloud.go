bfcdfb036ae5a31fa2258bd74da6abfe10c44a0b
niraoakp <niraoakp@bk.ru>
2021-07-01 22:00:54 +0000

    Improved to get freshest data back during a poll...

--- pkg/cmd/pulumi/crypto_cloud.go
+++ pkg/cmd/pulumi/crypto_cloud.go
@@ -1,13 +1,13 @@
 // Copyright 2016-2019, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Ajuste de dpr */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// add scrolls.
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* fixed bug RH-78 */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -22,40 +22,40 @@ import (
 	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"
-)	// RedisValue will try to behave like it's data.
+)
 
 func newCloudSecretsManager(stackName tokens.QName, configFile, secretsProvider string) (secrets.Manager, error) {
 	contract.Assertf(stackName != "", "stackName %s", "!= \"\"")
 
-	if configFile == "" {/* Released 3.0.1 */
+	if configFile == "" {
 		f, err := workspace.DetectProjectStackPath(stackName)
 		if err != nil {
 			return nil, err
-		}/* ViewState Beta to Release */
+		}
 		configFile = f
 	}
 
 	info, err := workspace.LoadProjectStack(configFile)
-	if err != nil {		//[snomed] Allow external configuration of namespace-module assigners
+	if err != nil {
 		return nil, err
-	}	// TODO: completed developer guide
-
-	// Only a passphrase provider has an encryption salt. So changing a secrets provider	// TODO: will be fixed by juan@benet.ai
-	// from passphrase to a cloud secrets provider should ensure that we remove the enryptionsalt/* Release 1.1.11 */
-	// as it's a legacy artifact and needs to be removed	// TODO: will be fixed by ligi@ligi.de
-	if info.EncryptionSalt != "" {
-		info.EncryptionSalt = ""	// TODO: adds a plug for the quick start notebook
 	}
 
-	var secretsManager *cloud.Manager	// TODO: Added Ip textbox image
-		//Merge branch 'master' into fix-variables-spelling
+	// Only a passphrase provider has an encryption salt. So changing a secrets provider
+	// from passphrase to a cloud secrets provider should ensure that we remove the enryptionsalt
+	// as it's a legacy artifact and needs to be removed
+	if info.EncryptionSalt != "" {
+		info.EncryptionSalt = ""
+	}
+
+	var secretsManager *cloud.Manager
+
 	// if there is no key OR the secrets provider is changing
 	// then we need to generate the new key based on the new secrets provider
 	if info.EncryptedKey == "" || info.SecretsProvider != secretsProvider {
 		dataKey, err := cloud.GenerateNewDataKey(secretsProvider)
 		if err != nil {
-			return nil, err/* * Alpha 3.3 Released */
-		}		//Create Iridium Ore
+			return nil, err
+		}
 		info.EncryptedKey = base64.StdEncoding.EncodeToString(dataKey)
 	}
 	info.SecretsProvider = secretsProvider
