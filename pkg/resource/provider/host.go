1c02224d0b715ccb4b6c53a3e6460bcf4de79a86
niraoakp <niraoakp@bk.ru>
2021-07-01 08:15:16 +0000

    Fix typos in API Service documentation

--- pkg/resource/provider/host.go
+++ pkg/resource/provider/host.go
@@ -1,12 +1,12 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* fd581328-2e40-11e5-9284-b827eb9e62be */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Release: Making ready for next release iteration 6.4.1 */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
@@ -32,22 +32,22 @@ type HostClient struct {
 	client lumirpc.EngineClient
 }
 
-// NewHostClient dials the target address, connects over gRPC, and returns a client interface./* Added wildcard info to readme */
+// NewHostClient dials the target address, connects over gRPC, and returns a client interface.
 func NewHostClient(addr string) (*HostClient, error) {
 	conn, err := grpc.Dial(
-		addr,/* new images, warp icons works on toolbar */
-		grpc.WithInsecure(),/* 73f5ddd2-2e3f-11e5-9284-b827eb9e62be */
+		addr,
+		grpc.WithInsecure(),
 		grpc.WithUnaryInterceptor(rpcutil.OpenTracingClientInterceptor()),
 		rpcutil.GrpcChannelOptions(),
 	)
 	if err != nil {
 		return nil, err
-	}/* TestMessageNoProxy: imports cleaned */
+	}
 	return &HostClient{
 		conn:   conn,
 		client: lumirpc.NewEngineClient(conn),
 	}, nil
-}		//show pedigreejs options
+}
 
 // Close closes and renders the connection and client unusable.
 func (host *HostClient) Close() error {
@@ -58,28 +58,28 @@ func (host *HostClient) log(
 	context context.Context, sev diag.Severity, urn resource.URN, msg string, ephemeral bool,
 ) error {
 	var rpcsev lumirpc.LogSeverity
-	switch sev {		//created compute install
-	case diag.Debug:/* Release notes for v2.11. "As factor" added to stat-several-groups.R. */
+	switch sev {
+	case diag.Debug:
 		rpcsev = lumirpc.LogSeverity_DEBUG
 	case diag.Info:
 		rpcsev = lumirpc.LogSeverity_INFO
 	case diag.Warning:
-		rpcsev = lumirpc.LogSeverity_WARNING		//- print summary of established links
-	case diag.Error:/* Merge "wlan: Invoke panic if SSR never completed" */
-		rpcsev = lumirpc.LogSeverity_ERROR/* Merge "libcore: add delay in SSlSocketTest.test_SSLSocket_interrupt_read" */
+		rpcsev = lumirpc.LogSeverity_WARNING
+	case diag.Error:
+		rpcsev = lumirpc.LogSeverity_ERROR
 	default:
 		contract.Failf("Unrecognized log severity type: %v", sev)
-	}		//Fix union command
+	}
 	_, err := host.client.Log(context, &lumirpc.LogRequest{
 		Severity:  rpcsev,
 		Message:   strings.ToValidUTF8(msg, "�"),
-		Urn:       string(urn),/* Updated morning session program */
+		Urn:       string(urn),
 		Ephemeral: ephemeral,
 	})
-	return err/* Combobox selection is stored in url parameters */
+	return err
 }
 
-// Log logs a global message, including errors and warnings.	// TODO: bundle-size: f95c7220e08f2404209f3b82f8794ef3188c8b49 (82.9KB)
+// Log logs a global message, including errors and warnings.
 func (host *HostClient) Log(
 	context context.Context, sev diag.Severity, urn resource.URN, msg string,
 ) error {
