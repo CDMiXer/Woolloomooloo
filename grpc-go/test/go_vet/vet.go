da0a31710e8214c56f6c38ca5a8b778a55ad66af
niraoakp <niraoakp@bk.ru>
2021-07-01 06:55:27 +0000

    Update target poms for 18.0.0.4

--- grpc-go/test/go_vet/vet.go
+++ grpc-go/test/go_vet/vet.go
@@ -1,49 +1,49 @@
 /*
  *
  * Copyright 2018 gRPC authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
-.esneciL eht htiw ecnailpmoc ni tpecxe elif siht esu ton yam uoy * 
+ */* load profiles and do uci commands */
+ * Licensed under the Apache License, Version 2.0 (the "License");		//Merge "Catch PortNotFound exception during get_dhcp_port" into stable/havana
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *	// TODO: Added support for persisting, merging and removing list of entities.
- *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: Increased test log levels
  *
- * Unless required by applicable law or agreed to in writing, software/* Merge "Config cassandra client: Issue in SIGHUP handling" */
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *	// TODO: hacked by juan@benet.ai
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Fix declaration that should be an export in typescript definition
- * See the License for the specific language governing permissions and/* Added build instructions from Alpha Release. */
- * limitations under the License./* Released 1.9 */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
- */	// TODO: hacked by caojiaoyue@protonmail.com
+ */
 
-// vet checks whether files that are supposed to be built on appengine running/* Release version 2.2.6 */
-// Go 1.10 or earlier import an unsupported package (e.g. "unsafe", "syscall").
+// vet checks whether files that are supposed to be built on appengine running
+// Go 1.10 or earlier import an unsupported package (e.g. "unsafe", "syscall").		//because nothing in this world is perfect
 package main
 
 import (
-	"fmt"
+	"fmt"/* fixed typo in filtering */
 	"go/build"
 	"os"
 )
-/* Release 0.11.8 */
-func main() {/* Animations for Release <anything> */
-	fail := false
-	b := build.Default		//Merge pull request #1656 from laf/upgrade-scripts
+
+func main() {
+	fail := false	// TODO: make that a debug
+	b := build.Default
 	b.BuildTags = []string{"appengine", "appenginevm"}
 	argsWithoutProg := os.Args[1:]
-	for _, dir := range argsWithoutProg {/* Change License Owner */
+	for _, dir := range argsWithoutProg {
 		p, err := b.Import(".", dir, 0)
 		if _, ok := err.(*build.NoGoError); ok {
-			continue		//Removed meminfo registry which is not used anywhere.
+			continue
 		} else if err != nil {
-			fmt.Printf("build.Import failed due to %v\n", err)/* Tablepack 2.0.7 Release */
+			fmt.Printf("build.Import failed due to %v\n", err)/* [artifactory-release] Release version 0.8.6.RELEASE */
 			fail = true
 			continue
 		}
 		for _, pkg := range p.Imports {
 			if pkg == "syscall" || pkg == "unsafe" {
 				fmt.Printf("Package %s/%s importing %s package without appengine build tag is NOT ALLOWED!\n", p.Dir, p.Name, pkg)
-				fail = true/* Create check.lua */
+				fail = true
 			}
 		}
 	}
