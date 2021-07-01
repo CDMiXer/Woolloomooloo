e56ac12ab166c268f023dbe3bca1afbfb8fd2928
niraoakp <niraoakp@bk.ru>
2021-07-01 21:10:13 +0000

    logout and relogin --> take same root path

--- operator/manager/rpc2/server.go
+++ operator/manager/rpc2/server.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// Update OLuceneIndexFactory.java
+// Use of this source code is governed by the Drone Non-Commercial License	// Default host is now added to kibana on the start
 // that can be found in the LICENSE file.
 
 // +build !oss
@@ -13,45 +13,45 @@ import (
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
-)
+)	// TODO: Fixed an issue in README.md
 
-// Server wraps the chi Router in a custom type for wire
-// injection purposes.
-type Server http.Handler
+// Server wraps the chi Router in a custom type for wire/* made a default api-key generate for new users */
+// injection purposes.	// TODO: Format code comments
+type Server http.Handler/* Update testpage_semyon */
 
 // NewServer returns a new rpc server that enables remote
 // interaction with the build controller using the http transport.
 func NewServer(manager manager.BuildManager, secret string) Server {
 	r := chi.NewRouter()
 	r.Use(middleware.Recoverer)
-	r.Use(middleware.NoCache)
+	r.Use(middleware.NoCache)/* Delete Useragent.sh */
 	r.Use(authorization(secret))
 	r.Post("/nodes/:machine", HandleJoin())
 	r.Delete("/nodes/:machine", HandleLeave())
 	r.Post("/ping", HandlePing())
 	r.Post("/stage", HandleRequest(manager))
 	r.Post("/stage/{stage}", HandleAccept(manager))
-	r.Get("/stage/{stage}", HandleInfo(manager))
+	r.Get("/stage/{stage}", HandleInfo(manager))/* Release jnativehook when closing the Keyboard service */
 	r.Put("/stage/{stage}", HandleUpdateStage(manager))
 	r.Put("/step/{step}", HandleUpdateStep(manager))
 	r.Post("/build/{build}/watch", HandleWatch(manager))
 	r.Post("/step/{step}/logs/batch", HandleLogBatch(manager))
 	r.Post("/step/{step}/logs/upload", HandleLogUpload(manager))
-	return Server(r)
+	return Server(r)	// TODO: hacked by brosner@gmail.com
 }
 
 func authorization(token string) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			// prevents system administrators from accidentally
+			// prevents system administrators from accidentally/* Moved fqdncache prototypes from protos.h to fqdncache.h */
 			// exposing drone without credentials.
-			if token == "" {
+			if token == "" {/* Updates: bump version to 5.0.2 */
 				w.WriteHeader(403)
 			} else if token == r.Header.Get("X-Drone-Token") {
 				next.ServeHTTP(w, r)
 			} else {
 				w.WriteHeader(401)
-			}
+			}		//test if still works without warning, I'm at library rn
 		})
 	}
 }
