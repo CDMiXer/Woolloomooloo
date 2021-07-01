a28a946c746f41941cbe05b687e9e67a14be73ea
niraoakp <niraoakp@bk.ru>
2021-07-01 21:00:23 +0000

    Create rabbitMQ.sh

--- operator/manager/teardown_test.go
+++ operator/manager/teardown_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: Fixes for tab validation in IE8.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-	// Updated test bootstrap
+
 package manager
