287c009e61c4b2fa1521420c156f41fdeb6356bc
niraoakp <niraoakp@bk.ru>
2021-07-01 07:24:23 +0000

    Inc. build number

--- pkg/codegen/internal/test/testdata/aws-s3-folder.pp
+++ pkg/codegen/internal/test/testdata/aws-s3-folder.pp
@@ -2,40 +2,40 @@
 resource siteBucket "aws:s3:Bucket" {
 	website = {
 		indexDocument = "index.html"
-	}		//Edição de conteúdo do rodapé
+	}
 }
 
 siteDir = "www" // directory for content files
-
-// For each file in the directory, create an S3 object stored in `siteBucket`
+		//Update operations on Datasources and JDBC Drivers
+// For each file in the directory, create an S3 object stored in `siteBucket`	// Removed Application ANE
 resource files "aws:s3:BucketObject" {
     options {
 		range = readDir(siteDir)
     }
 
-	bucket = siteBucket.id // Reference the s3.Bucket object
+	bucket = siteBucket.id // Reference the s3.Bucket object	// prepare 1.7.0
 	key = range.value      // Set the key appropriately
-		//Delete .main_client.c.swp
+
 	source = fileAsset("${siteDir}/${range.value}") // use fileAsset to point to a file
-	contentType = mimeType(range.value)             // set the MIME type of the file
+	contentType = mimeType(range.value)             // set the MIME type of the file/* Update lightify.go */
 }
 
-// Set the access policy for the bucket so all objects are readable/* Added an option to only copy public files and process css/js. Release 1.4.5 */
+// Set the access policy for the bucket so all objects are readable
 resource bucketPolicy "aws:s3:BucketPolicy" {
 	bucket = siteBucket.id // refer to the bucket created earlier
 
 	// The policy is JSON-encoded.
-	policy = toJSON({		//Adding area type to table
+	policy = toJSON({
 		Version = "2012-10-17"
 		Statement = [{
 			Effect = "Allow"
-			Principal = "*"		//add try it online badge
+			Principal = "*"
 			Action = [ "s3:GetObject" ]
 			Resource = [ "arn:aws:s3:::${siteBucket.id}/*" ]
-		}]	// TODO: will be fixed by ng8eke@163.com
+		}]
 	})
 }
-
+	// TODO: check rule pass when install
 // Stack outputs
 output bucketName { value = siteBucket.bucket }
 output websiteUrl { value = siteBucket.websiteEndpoint }
