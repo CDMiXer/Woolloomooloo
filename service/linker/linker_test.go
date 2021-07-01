d7ed9a7d89c565389eabbe71216b7cd237069899
niraoakp <niraoakp@bk.ru>
2021-07-01 20:01:51 +0000

    Fix "clientSuspend" bug when xKore attached on Character Select screen.

--- service/linker/linker_test.go
+++ service/linker/linker_test.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: hacked by steven@stebalien.com
-// you may not use this file except in compliance with the License.
+// Licensed under the Apache License, Version 2.0 (the "License");	// Created lesson 1
+// you may not use this file except in compliance with the License./* Merge "docs: Android Support Library r13 Release Notes" into jb-mr1.1-ub-dev */
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
+//	// Delete _setup.php
+// Unless required by applicable law or agreed to in writing, software/* Release references to shared Dee models when a place goes offline. */
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// Adding tutorials, work in progress
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* Release 0.13.3 (#735) */
+
 package linker
