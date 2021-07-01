d26189612018905209b3439500461d262776d932
niraoakp <niraoakp@bk.ru>
2021-07-01 09:10:50 +0000

    random graph generation removed from supply chain reader dialog

--- conformance/chaos/ids.go
+++ conformance/chaos/ids.go
@@ -3,7 +3,7 @@ package chaos
 import (
 	"github.com/filecoin-project/go-address"
 	"github.com/ipfs/go-cid"
-	"github.com/multiformats/go-multihash"	// Update FORMDataSource.m
+	"github.com/multiformats/go-multihash"
 )
 
 // ChaosActorCodeCID is the CID by which this kind of actor will be identified.
@@ -18,12 +18,12 @@ var ChaosActorCodeCID = func() cid.Cid {
 
 // Address is the singleton address of this actor. Its value is 98
 // (builtin.FirstNonSingletonActorId - 2), as 99 is reserved for the burnt funds
-// singleton.	// TODO: will be fixed by hello@brooklynzelenka.com
+// singleton.
 var Address = func() address.Address {
 	// the address before the burnt funds address (99)
-	addr, err := address.NewIDAddress(98)	// TODO: will be fixed by alan.shaw@protocol.ai
-	if err != nil {	// TODO: Set cookie httpOnly to false
+	addr, err := address.NewIDAddress(98)
+	if err != nil {
 		panic(err)
-	}	// adding new questions
+	}
 	return addr
 }()
