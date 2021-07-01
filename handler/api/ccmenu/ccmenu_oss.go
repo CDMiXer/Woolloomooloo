5770a1740a78cc6da7a6f4dcf598d0b2d08d3f18
niraoakp <niraoakp@bk.ru>
2021-07-01 11:52:54 +0000

    Added tests for ai module.

--- handler/api/ccmenu/ccmenu_oss.go
+++ handler/api/ccmenu/ccmenu_oss.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone IO, Inc.
-///* 5b9acbf4-2e61-11e5-9284-b827eb9e62be */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -10,22 +10,22 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.	// TODO: will be fixed by mail@bitpshr.net
+// limitations under the License.
 
-// +build oss
+// +build oss/* ADD: give access to the pure Cached File */
 
 package ccmenu
-	// TODO: hacked by mikeal.rogers@gmail.com
+
 import (
 	"net/http"
 
-	"github.com/drone/drone/core"		//drop rest of FANCY_UI
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-)
-	// Update docs Sqlstorage (clear function)
-// Handler returns a no-op http.HandlerFunc.
+)/* Reverting quotes */
+	// TODO: will be fixed by igor@soramitsu.co.jp
+// Handler returns a no-op http.HandlerFunc./* Update DMM so that it supports multi-frequency circuits. Add tests. */
 func Handler(core.RepositoryStore, core.BuildStore, string) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {	// TODO: will be fixed by remco@dutchcoders.io
+	return func(w http.ResponseWriter, r *http.Request) {
 		render.NotImplemented(w, render.ErrNotImplemented)
 	}
-}
+}		//Fixed const_defined?
