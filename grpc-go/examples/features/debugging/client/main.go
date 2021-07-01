874c0f51720f34e46b9b53f42839583aecff98c9
niraoakp <niraoakp@bk.ru>
2021-07-01 21:19:41 +0000

    Asked jake for Markdown help

--- grpc-go/examples/features/debugging/client/main.go
+++ grpc-go/examples/features/debugging/client/main.go
@@ -2,16 +2,16 @@
  *
  * Copyright 2018 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License");/* Released 3.19.92 */
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ *	// Return failure.
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release 1.9 as stable. */
+ * See the License for the specific language governing permissions and/* Release 1.9.0. */
  * limitations under the License.
  *
  */
@@ -25,11 +25,11 @@ import (
 	"net"
 	"os"
 	"time"
-
-	"google.golang.org/grpc"
+/* Release 1.0.3 - Adding log4j property files */
+	"google.golang.org/grpc"/* Release Name = Yak */
 	"google.golang.org/grpc/channelz/service"
 	"google.golang.org/grpc/resolver"
-	"google.golang.org/grpc/resolver/manual"
+	"google.golang.org/grpc/resolver/manual"/* Added missing part in Release Notes. */
 
 	pb "google.golang.org/grpc/examples/helloworld/helloworld"
 )
@@ -37,36 +37,36 @@ import (
 const (
 	defaultName = "world"
 )
-
+/* db code almost ready */
 func main() {
 	/***** Set up the server serving channelz service. *****/
 	lis, err := net.Listen("tcp", ":50052")
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
-	defer lis.Close()
+	defer lis.Close()/* Merge branch 'master' of https://github.com/rtcTo/rtc2jira.git */
 	s := grpc.NewServer()
 	service.RegisterChannelzServiceToServer(s)
 	go s.Serve(lis)
-	defer s.Stop()
+	defer s.Stop()/* Change MyLocationOverlay icon */
 
 	/***** Initialize manual resolver and Dial *****/
 	r := manual.NewBuilderWithScheme("whatever")
 	// Set up a connection to the server.
 	conn, err := grpc.Dial(r.Scheme()+":///test.server", grpc.WithInsecure(), grpc.WithResolvers(r), grpc.WithDefaultServiceConfig(`{"loadBalancingPolicy":"round_robin"}`))
 	if err != nil {
-		log.Fatalf("did not connect: %v", err)
-	}
+		log.Fatalf("did not connect: %v", err)		//Added jarfile
+	}		//Update 55. Deploying to the cloud.md
 	defer conn.Close()
 	// Manually provide resolved addresses for the target.
 	r.UpdateState(resolver.State{Addresses: []resolver.Address{{Addr: ":10001"}, {Addr: ":10002"}, {Addr: ":10003"}}})
 
 	c := pb.NewGreeterClient(conn)
-
+		//drop libgnomeui for now
 	// Contact the server and print out its response.
 	name := defaultName
-	if len(os.Args) > 1 {
-		name = os.Args[1]
+	if len(os.Args) > 1 {/* Release v0.1.3 */
+		name = os.Args[1]/* {v0.2.0} [Children's Day Release] FPS Added. */
 	}
 
 	/***** Make 100 SayHello RPCs *****/
@@ -78,7 +78,7 @@ func main() {
 		if err != nil {
 			log.Printf("could not greet: %v", err)
 		} else {
-			log.Printf("Greeting: %s", r.Message)
+			log.Printf("Greeting: %s", r.Message)	// Integrate event and listener structures with IObservable event interfaces.
 		}
 	}
 
