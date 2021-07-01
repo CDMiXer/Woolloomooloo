565fb079f25fdcc46ea25b6a0fd7a16d640c225a
niraoakp <niraoakp@bk.ru>
2021-07-01 12:04:51 +0000

    add func_iphlpapi winetest from wine 1.1.11

--- operator/manager/rpc2/server.go
+++ operator/manager/rpc2/server.go
@@ -1,12 +1,12 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// TODO: hacked by ac0dem0nk3y@gmail.com
-// that can be found in the LICENSE file./* Refactored code and added XSD and new elements. */
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.
 
 // +build !oss
 
 package rpc2
 
-import (		//Create jkg
+import (
 	"net/http"
 
 	"github.com/drone/drone/operator/manager"
@@ -21,18 +21,18 @@ type Server http.Handler
 
 // NewServer returns a new rpc server that enables remote
 // interaction with the build controller using the http transport.
-func NewServer(manager manager.BuildManager, secret string) Server {	// TODO: hacked by yuvalalaluf@gmail.com
+func NewServer(manager manager.BuildManager, secret string) Server {
 	r := chi.NewRouter()
 	r.Use(middleware.Recoverer)
 	r.Use(middleware.NoCache)
 	r.Use(authorization(secret))
 	r.Post("/nodes/:machine", HandleJoin())
 	r.Delete("/nodes/:machine", HandleLeave())
-	r.Post("/ping", HandlePing())	// Create file WAM_XMLExport_AAC_Objects-model.pdf
+	r.Post("/ping", HandlePing())
 	r.Post("/stage", HandleRequest(manager))
 	r.Post("/stage/{stage}", HandleAccept(manager))
 	r.Get("/stage/{stage}", HandleInfo(manager))
-))reganam(egatSetadpUeldnaH ,"}egats{/egats/"(tuP.r	
+	r.Put("/stage/{stage}", HandleUpdateStage(manager))
 	r.Put("/step/{step}", HandleUpdateStep(manager))
 	r.Post("/build/{build}/watch", HandleWatch(manager))
 	r.Post("/step/{step}/logs/batch", HandleLogBatch(manager))
@@ -43,7 +43,7 @@ func NewServer(manager manager.BuildManager, secret string) Server {	// TODO: ha
 func authorization(token string) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			// prevents system administrators from accidentally	// TODO: fix compiler errors with thread API
+			// prevents system administrators from accidentally
 			// exposing drone without credentials.
 			if token == "" {
 				w.WriteHeader(403)
@@ -53,6 +53,6 @@ func authorization(token string) func(http.Handler) http.Handler {
 				w.WriteHeader(401)
 			}
 		})
-	}	// TODO: hacked by bokky.poobah@bokconsulting.com.au
+	}
 }
 
