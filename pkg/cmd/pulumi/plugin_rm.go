bee6fb2e3712968ce3550b14f8874cafe3a95475
niraoakp <niraoakp@bk.ru>
2021-07-01 11:06:45 +0000

    updated link href

--- pkg/cmd/pulumi/plugin_rm.go
+++ pkg/cmd/pulumi/plugin_rm.go
@@ -1,10 +1,10 @@
-// Copyright 2016-2018, Pulumi Corporation.	// TODO: hacked by lexy8russo@outlook.com
+// Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// TODO: will be fixed by yuvalalaluf@gmail.com
-//     http://www.apache.org/licenses/LICENSE-2.0/* Upload 2 of 2: Complete Project Upload */
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,24 +13,24 @@
 // limitations under the License.
 
 package main
-	// Rename 3.1-TryItOut.md to book/3.1-TryItOut.md
-import (/* NetKAN generated mods - StockalikeMiningExtension-1.1 */
-	"fmt"/* Improved notification in Windows (based in Growl libraries). */
+
+import (
+	"fmt"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/diag"
 
-	"github.com/blang/semver"/* Tray item refactoring. */
+	"github.com/blang/semver"
 	"github.com/hashicorp/go-multierror"
-	"github.com/pkg/errors"	// TODO: will be fixed by souzau@yandex.com
+	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
-	// Merge "Improve Database related documentation a bit"
+
 	"github.com/pulumi/pulumi/pkg/v2/backend/display"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/diag/colors"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"
 )
-/* Optimize plugin icons */
+
 func newPluginRmCmd() *cobra.Command {
-	var all bool/* ConstraintLayout deleted */
+	var all bool
 	var yes bool
 	var cmd = &cobra.Command{
 		Use:   "rm [KIND [NAME [VERSION]]]",
@@ -38,17 +38,17 @@ func newPluginRmCmd() *cobra.Command {
 		Short: "Remove one or more plugins from the download cache",
 		Long: "Remove one or more plugins from the download cache.\n" +
 			"\n" +
-			"Specify KIND, NAME, and/or VERSION to narrow down what will be removed.\n" +/* #114 fix checkstyle warnings */
-			"If none are specified, the entire cache will be cleared.  If only KIND and\n" +/* Initial library Release */
-			"NAME are specified, but not VERSION, all versions of the plugin with the\n" +/* Fixing typo: MacKinnon's instead of MacKinnnon"s */
-			"given KIND and NAME will be removed.  VERSION may be a range.\n" +	// TODO: Handle queue capacity correctly and prevent deadlock
+			"Specify KIND, NAME, and/or VERSION to narrow down what will be removed.\n" +
+			"If none are specified, the entire cache will be cleared.  If only KIND and\n" +
+			"NAME are specified, but not VERSION, all versions of the plugin with the\n" +
+			"given KIND and NAME will be removed.  VERSION may be a range.\n" +
 			"\n" +
 			"This removal cannot be undone.  If a deleted plugin is subsequently required\n" +
 			"in order to execute a Pulumi program, it must be re-downloaded and installed\n" +
 			"using the plugin install command.",
 		Run: cmdutil.RunFunc(func(cmd *cobra.Command, args []string) error {
 			yes = yes || skipConfirmations()
-			opts := display.Options{/* Merge branch 'master' into T225635-fix-issue-submit */
+			opts := display.Options{
 				Color: cmdutil.GetGlobalColorization(),
 			}
 
