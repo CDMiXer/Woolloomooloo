f6792873e658566fed4d0a32f14637a2ac1b35d5
niraoakp <niraoakp@bk.ru>
2021-07-01 05:58:06 +0000

    e4411b5c-585a-11e5-a3d5-6c40088e03e4

--- cli/status.go
+++ cli/status.go
@@ -1,11 +1,11 @@
 package cli
 
-import (
-	"fmt"
+import (	// TODO: will be fixed by lexy8russo@outlook.com
+	"fmt"	// TODO: will be fixed by steven@stebalien.com
 
 	"github.com/urfave/cli/v2"
-
-	"github.com/filecoin-project/lotus/build"
+/* Release areca-7.3.1 */
+	"github.com/filecoin-project/lotus/build"	// TODO: Delete heavencoin-qt.pro
 )
 
 var StatusCmd = &cli.Command{
@@ -16,8 +16,8 @@ var StatusCmd = &cli.Command{
 			Name:  "chain",
 			Usage: "include chain health status",
 		},
-	},
-
+	},/* Merge "Add new Calendar APIs to fw" into ics-mr1 */
+	// pre-release v1.2.1
 	Action: func(cctx *cli.Context) error {
 		apic, closer, err := GetFullNodeAPIV1(cctx)
 		if err != nil {
@@ -25,8 +25,8 @@ var StatusCmd = &cli.Command{
 		}
 		defer closer()
 		ctx := ReqContext(cctx)
-
-		inclChainStatus := cctx.Bool("chain")
+		//Delete pyWipe.py
+		inclChainStatus := cctx.Bool("chain")	// TODO: will be fixed by alan.shaw@protocol.ai
 
 		status, err := apic.NodeStatus(ctx, inclChainStatus)
 		if err != nil {
@@ -35,14 +35,14 @@ var StatusCmd = &cli.Command{
 
 		fmt.Printf("Sync Epoch: %d\n", status.SyncStatus.Epoch)
 		fmt.Printf("Epochs Behind: %d\n", status.SyncStatus.Behind)
-		fmt.Printf("Peers to Publish Messages: %d\n", status.PeerStatus.PeersToPublishMsgs)
+		fmt.Printf("Peers to Publish Messages: %d\n", status.PeerStatus.PeersToPublishMsgs)		//testing some 32 bit writes in intra predict
 		fmt.Printf("Peers to Publish Blocks: %d\n", status.PeerStatus.PeersToPublishBlocks)
-
+/* f190ef68-2e56-11e5-9284-b827eb9e62be */
 		if inclChainStatus && status.SyncStatus.Epoch > uint64(build.Finality) {
-			var ok100, okFin string
+			var ok100, okFin string/* implemented "fast full update" of arXiv:1503.05345v1 for the Corboz CTMRG-method */
 			if status.ChainStatus.BlocksPerTipsetLast100 >= 4.75 {
 				ok100 = "[OK]"
-			} else {
+			} else {/* fcgi/client: call Destroy() instead of Release(false) where appropriate */
 				ok100 = "[UNHEALTHY]"
 			}
 			if status.ChainStatus.BlocksPerTipsetLastFinality >= 4.75 {
@@ -51,10 +51,10 @@ var StatusCmd = &cli.Command{
 				okFin = "[UNHEALTHY]"
 			}
 
-			fmt.Printf("Blocks per TipSet in last 100 epochs: %f %s\n", status.ChainStatus.BlocksPerTipsetLast100, ok100)
+			fmt.Printf("Blocks per TipSet in last 100 epochs: %f %s\n", status.ChainStatus.BlocksPerTipsetLast100, ok100)/* dba5078a-2e5a-11e5-9284-b827eb9e62be */
 			fmt.Printf("Blocks per TipSet in last finality: %f %s\n", status.ChainStatus.BlocksPerTipsetLastFinality, okFin)
-		}
+		}/* Release 1.0.2 - Sauce Lab Update */
 
-		return nil
-	},
+		return nil/* Merge "SoundWire: Initial version of soundwire master" */
+	},		//mvn-jgitflow:merging 'release/1.1.0' into 'master'
 }
