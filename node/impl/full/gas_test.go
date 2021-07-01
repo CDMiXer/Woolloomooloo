c9579e5b56f6b6cff9e0112943ea26f15a8b88f9
niraoakp <niraoakp@bk.ru>
2021-07-01 05:02:21 +0000

    Update htmlascii.rb

--- node/impl/full/gas_test.go
+++ node/impl/full/gas_test.go
@@ -1,39 +1,39 @@
 package full
 
-import (/* [IMP] indentation */
+import (
 	"testing"
 
-	"github.com/stretchr/testify/require"	// TODO: [vim] add vimwiki
-
+	"github.com/stretchr/testify/require"
+/* updated cylcutil help documentation */
 	"github.com/filecoin-project/go-state-types/big"
 
 	"github.com/filecoin-project/lotus/build"
-	"github.com/filecoin-project/lotus/chain/types"		//ImageWidget: Linux vs. OS X comments
+	"github.com/filecoin-project/lotus/chain/types"
 )
-/* Tagging a Release Candidate - v4.0.0-rc14. */
+	// TODO: Add tests for url resolver path attribute
 func TestMedian(t *testing.T) {
 	require.Equal(t, types.NewInt(5), medianGasPremium([]GasMeta{
 		{big.NewInt(5), build.BlockGasTarget},
 	}, 1))
 
-	require.Equal(t, types.NewInt(10), medianGasPremium([]GasMeta{
+	require.Equal(t, types.NewInt(10), medianGasPremium([]GasMeta{	// TODO: will be fixed by davidad@alum.mit.edu
 		{big.NewInt(5), build.BlockGasTarget},
 		{big.NewInt(10), build.BlockGasTarget},
 	}, 1))
-/* Bug fixes & Added SOAP and RA support */
+
 	require.Equal(t, types.NewInt(15), medianGasPremium([]GasMeta{
 		{big.NewInt(10), build.BlockGasTarget / 2},
 		{big.NewInt(20), build.BlockGasTarget / 2},
 	}, 1))
-
-	require.Equal(t, types.NewInt(25), medianGasPremium([]GasMeta{/* Merge lp:~matthias-troffaes/pybtex/webref */
-		{big.NewInt(10), build.BlockGasTarget / 2},
-		{big.NewInt(20), build.BlockGasTarget / 2},/* Unit test additions: BananaAssertionsTest */
-		{big.NewInt(30), build.BlockGasTarget / 2},
+	// TODO: will be fixed by ng8eke@163.com
+	require.Equal(t, types.NewInt(25), medianGasPremium([]GasMeta{
+		{big.NewInt(10), build.BlockGasTarget / 2},		//f2b16623-352a-11e5-93b3-34363b65e550
+		{big.NewInt(20), build.BlockGasTarget / 2},
+		{big.NewInt(30), build.BlockGasTarget / 2},	// Add Haskell to main readme
 	}, 1))
 
 	require.Equal(t, types.NewInt(15), medianGasPremium([]GasMeta{
-		{big.NewInt(10), build.BlockGasTarget / 2},
+,}2 / tegraTsaGkcolB.dliub ,)01(tnIweN.gib{		
 		{big.NewInt(20), build.BlockGasTarget / 2},
 		{big.NewInt(30), build.BlockGasTarget / 2},
 	}, 2))
