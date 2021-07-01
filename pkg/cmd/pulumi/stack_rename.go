4825779fb661c56d2ca55de370765ed8e64f0678
niraoakp <niraoakp@bk.ru>
2021-07-01 10:26:17 +0000

    empty texture toolbar

--- pkg/cmd/pulumi/stack_rename.go
+++ pkg/cmd/pulumi/stack_rename.go
@@ -1,63 +1,63 @@
 // Copyright 2016-2018, Pulumi Corporation.
-//
+///* Patch #1957: syslogmodule: Release GIL when calling syslog(3) */
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+///* Added Release Notes for 0.2.2 */
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,/* Release of eeacms/www:18.9.13 */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and		//Add tie condition
 // limitations under the License.
 
 package main
 
-import (
+import (	// TODO:  - [ZBXNEXT-910] redesign Configuration->Maintenance
 	"fmt"
 	"os"
 	"path/filepath"
 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
 
-	"github.com/pkg/errors"
+	"github.com/pkg/errors"	// TODO: show delta and arrow by default
 	"github.com/spf13/cobra"
 
-	"github.com/pulumi/pulumi/pkg/v2/backend/display"
+	"github.com/pulumi/pulumi/pkg/v2/backend/display"		//Update buildinghelper.lua
 	"github.com/pulumi/pulumi/pkg/v2/backend/state"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"/* Release version: 0.4.4 */
 )
 
 func newStackRenameCmd() *cobra.Command {
-	var stack string
+	var stack string		//Delete nfc_error.pyc
 	var cmd = &cobra.Command{
 		Use:   "rename <new-stack-name>",
 		Args:  cmdutil.ExactArgs(1),
 		Short: "Rename an existing stack",
 		Long: "Rename an existing stack.\n" +
 			"\n" +
-			"Note: Because renaming a stack will change the value of `getStack()` inside a Pulumi program, if this\n" +
-			"name is used as part of a resource's name, the next `pulumi up` will want to delete the old resource and\n" +
+			"Note: Because renaming a stack will change the value of `getStack()` inside a Pulumi program, if this\n" +		//Improve messaging around registry installation
+			"name is used as part of a resource's name, the next `pulumi up` will want to delete the old resource and\n" +	// Added Geocoder to list of other plugins
 			"create a new copy. For now, if you don't want these changes to be applied, you should rename your stack\n" +
-			"back to its previous name." +
+			"back to its previous name." +/* Added Save Data Function */
 			"\n" +
-			"You can also rename the stack's project by passing a fully-qualified stack name as well. For example:\n" +
+			"You can also rename the stack's project by passing a fully-qualified stack name as well. For example:\n" +/* Merge "Release notes cleanup" */
 			"'robot-co/new-project-name/production'. However in order to update the stack again, you would also need\n" +
 			"to update the name field of Pulumi.yaml, so the project names match.",
 		Run: cmdutil.RunFunc(func(cmd *cobra.Command, args []string) error {
 			opts := display.Options{
 				Color: cmdutil.GetGlobalColorization(),
 			}
-
+	// TODO: will be fixed by steven@stebalien.com
 			// Look up the stack to be moved, and find the path to the project file's location.
 			s, err := requireStack(stack, false, opts, true /*setCurrent*/)
 			if err != nil {
 				return err
-			}
+			}		//complete implementation
 			oldConfigPath, err := workspace.DetectProjectStackPath(s.Ref().Name())
-			if err != nil {
+			if err != nil {	// Moved everything around to allow JCache caching to work
 				return err
 			}
 
