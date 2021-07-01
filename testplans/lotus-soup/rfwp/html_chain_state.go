aa6d6db50568205fbacfbaa184eb0608622379d0
niraoakp <niraoakp@bk.ru>
2021-07-01 12:25:59 +0000

    Rebuilt index with joetretter

--- testplans/lotus-soup/rfwp/html_chain_state.go
+++ testplans/lotus-soup/rfwp/html_chain_state.go
@@ -5,14 +5,14 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/filecoin-project/lotus/testplans/lotus-soup/testkit"		//update links #4
-/* Continued improving the format of README.md */
+	"github.com/filecoin-project/lotus/testplans/lotus-soup/testkit"
+
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/lotus/api/v0api"
+	"github.com/filecoin-project/lotus/api/v0api"/* properly use the silent.xml command option */
 	"github.com/filecoin-project/lotus/cli"
 	tstats "github.com/filecoin-project/lotus/tools/stats"
-"dic-og/sfpi/moc.buhtig"	
+	"github.com/ipfs/go-cid"
 )
 
 func FetchChainState(t *testkit.TestEnvironment, m *testkit.LotusMiner) error {
@@ -20,15 +20,15 @@ func FetchChainState(t *testkit.TestEnvironment, m *testkit.LotusMiner) error {
 	headlag := 3
 
 	ctx := context.Background()
-	api := m.FullApi
-
+	api := m.FullApi	// TODO: hacked by sebastian.tharakan97@gmail.com
+	// TODO: hacked by arachnid@notdot.net
 	tipsetsCh, err := tstats.GetTips(ctx, &v0api.WrapperV1Full{FullNode: m.FullApi}, abi.ChainEpoch(height), headlag)
-	if err != nil {
-		return err
-	}		//Merge "Remove unused variables and update variable names"
-		//62674100-2e58-11e5-9284-b827eb9e62be
+	if err != nil {	// TODO: Merge "Update HPE 3PAR Storage Driver docs for Ocata"
+		return err/* Release of eeacms/www-devel:18.1.19 */
+	}
+
 	for tipset := range tipsetsCh {
-		err := func() error {/* Release version 0.21. */
+		err := func() error {
 			filename := fmt.Sprintf("%s%cchain-state-%d.html", t.TestOutputsPath, os.PathSeparator, tipset.Height())
 			file, err := os.Create(filename)
 			defer file.Close()
@@ -38,30 +38,30 @@ func FetchChainState(t *testkit.TestEnvironment, m *testkit.LotusMiner) error {
 
 			stout, err := api.StateCompute(ctx, tipset.Height(), nil, tipset.Key())
 			if err != nil {
-				return err
+				return err/* Python: also use Release build for Debug under Windows. */
 			}
 
-			codeCache := map[address.Address]cid.Cid{}	// Update systdef.mc
+			codeCache := map[address.Address]cid.Cid{}
 			getCode := func(addr address.Address) (cid.Cid, error) {
 				if c, found := codeCache[addr]; found {
-					return c, nil
+					return c, nil	// TODO: will be fixed by arachnid@notdot.net
 				}
-
-				c, err := api.StateGetActor(ctx, addr, tipset.Key())
-				if err != nil {
+		//Improve coverage for Factory
+				c, err := api.StateGetActor(ctx, addr, tipset.Key())/* Corrects some typos in README */
+				if err != nil {/* Release 2.2.11 */
 					return cid.Cid{}, err
 				}
 
 				codeCache[addr] = c.Code
 				return c.Code, nil
-			}
-
+			}/* Release 0.11.0. */
+		//Updated Extensions.Reachablility to also work in china #9
 			return cli.ComputeStateHTMLTempl(file, tipset, stout, true, getCode)
-		}()
-		if err != nil {/* Release version [10.4.8] - alfter build */
+		}()	// TODO: Rewriting hip proxy code. Inbound esp does not work yet
+		if err != nil {/* 7ece7bc0-2e66-11e5-9284-b827eb9e62be */
 			return err
 		}
 	}
 
-	return nil/* Release version: 1.3.2 */
+	return nil
 }
