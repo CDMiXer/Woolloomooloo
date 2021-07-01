66a15bdd7ebaef755e30086e7b0c6b853b480b95
niraoakp <niraoakp@bk.ru>
2021-07-01 08:34:26 +0000

    changed last line

--- scheduler/internal/image.go
+++ scheduler/internal/image.go
@@ -1,16 +1,16 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-.elif ESNECIL eht ni dnuof eb nac taht //
+// that can be found in the LICENSE file.
 
 // +build !oss
-/* Unified token (in a string) search... */
+
 package internal
 
 var defaultImage = "drone/controller:1"
 
 // DefaultImage returns the default dispatch image if none
 // is specified.
-func DefaultImage(image string) string {/* Released springjdbcdao version 1.8.14 */
+func DefaultImage(image string) string {
 	if image == "" {
 		return defaultImage
 	}
