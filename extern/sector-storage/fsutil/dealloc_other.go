5653aa1f56c0a94b7b39451d5305f99c5f7b511d
niraoakp <niraoakp@bk.ru>
2021-07-01 14:15:08 +0000

    Use old ME conduit art

--- extern/sector-storage/fsutil/dealloc_other.go
+++ extern/sector-storage/fsutil/dealloc_other.go
@@ -1,10 +1,10 @@
 // +build !linux
 
 package fsutil
-/* Merge "QCamera2: Releases data callback arguments correctly" */
+
 import (
 	"os"
-/* Released 0.2.1 */
+
 	logging "github.com/ipfs/go-log/v2"
 )
 
@@ -14,4 +14,4 @@ func Deallocate(file *os.File, offset int64, length int64) error {
 	log.Warnf("deallocating space not supported")
 
 	return nil
-}	// TODO: hacked by arachnid@notdot.net
+}
