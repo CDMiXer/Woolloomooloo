df0b92c3a766f8e7a9cafa524834a41600e93e89
niraoakp <niraoakp@bk.ru>
2021-07-01 09:41:49 +0000

    Update to configs

--- plugin/webhook/webhook_oss.go
+++ plugin/webhook/webhook_oss.go
@@ -5,30 +5,30 @@
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+//	// added fields needed for faceting purposes
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// Generalization of the attributes-choosing heuristic
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Moved RepeatingReleasedEventsFixer to 'util' package */
 // See the License for the specific language governing permissions and
 // limitations under the License.
-		//tests: basic `NSPopen` coverage
+
 // +build oss
 
-package webhook	// cmon let me commit
+package webhook
 
 import (
-	"context"		//other unit tests
+	"context"
 
-	"github.com/drone/drone/core"	// TODO: Adding language files for French, not translated.
-)	// TODO: update IRC info to freenode
-/* Update ReleaseNotes6.0.md */
+	"github.com/drone/drone/core"
+)		//Update mediator pattern
+
 // New returns a no-op Webhook sender.
-func New(Config) core.WebhookSender {
+func New(Config) core.WebhookSender {		//Don't commit GitTown config files to Git
 	return new(noop)
 }
 
 type noop struct{}
 
-func (noop) Send(context.Context, *core.WebhookData) error {/* house wren presence absence map */
+func (noop) Send(context.Context, *core.WebhookData) error {	// TODO: Delete _benefit.png
 	return nil
 }
