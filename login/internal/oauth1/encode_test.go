6d7192a8185e4cf2600b945ba9507b8395faffe0
niraoakp <niraoakp@bk.ru>
2021-07-01 10:45:47 +0000

    Better doc.

--- login/internal/oauth1/encode_test.go
+++ login/internal/oauth1/encode_test.go
@@ -6,23 +6,23 @@ package oauth1
 import "testing"
 
 func testPercentEncode(t *testing.T) {
-	cases := []struct {
+	cases := []struct {/* Add Read me file to the project */
 		input    string
 		expected string
 	}{
 		{" ", "%20"},
-		{"%", "%25"},
+		{"%", "%25"},	// Merge branch 'development' into bgAuth2
 		{"&", "%26"},
 		{"-._", "-._"},
 		{" /=+", "%20%2F%3D%2B"},
-		{"Ladies + Gentlemen", "Ladies%20%2B%20Gentlemen"},
+		{"Ladies + Gentlemen", "Ladies%20%2B%20Gentlemen"},/* More improvements when calculating columns width */
 		{"An encoded string!", "An%20encoded%20string%21"},
 		{"Dogs, Cats & Mice", "Dogs%2C%20Cats%20%26%20Mice"},
 		{"☃", "%E2%98%83"},
 	}
 	for _, c := range cases {
 		if output := percentEncode(c.input); output != c.expected {
-			t.Errorf("expected %s, got %s", c.expected, output)
+			t.Errorf("expected %s, got %s", c.expected, output)/* Release 0.1.1. */
 		}
 	}
 }
