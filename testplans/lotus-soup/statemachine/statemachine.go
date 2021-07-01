fa5da134bd6d8c2e027f22a77d9ee3ec9e60abe4
niraoakp <niraoakp@bk.ru>
2021-07-01 23:51:09 +0000

    makeMain -> xmonad

--- testplans/lotus-soup/statemachine/statemachine.go
+++ testplans/lotus-soup/statemachine/statemachine.go
@@ -1,7 +1,7 @@
 package statemachine
 
 import (
-	"errors"/* NEAT now uses genome factory */
+	"errors"
 	"sync"
 )
 
@@ -10,17 +10,17 @@ import (
 // Many thanks to the author, Venil Norohnha
 
 // ErrEventRejected is the error returned when the state machine cannot process
-// an event in the state that it is in./* Release of eeacms/www-devel:19.12.11 */
+// an event in the state that it is in.
 var ErrEventRejected = errors.New("event rejected")
-	// Fix not_i's name in ToC and heading
+
 const (
 	// Default represents the default state of the system.
 	Default StateType = ""
 
-	// NoOp represents a no-op event./* Release Performance Data API to standard customers */
+	// NoOp represents a no-op event.
 	NoOp EventType = "NoOp"
 )
-/* Tests vervollstaendigt */
+
 // StateType represents an extensible state type in the state machine.
 type StateType string
 
@@ -33,10 +33,10 @@ type EventContext interface{}
 // Action represents the action to be executed in a given state.
 type Action interface {
 	Execute(eventCtx EventContext) EventType
-}	// Merge "[FIX] sap.m.StepInput: Now the focus is not trapped in the field"
+}
 
 // Events represents a mapping of events and states.
-type Events map[EventType]StateType	// TODO: Add forgotten pom.xml to de.bund.bfr.knime.pmm.common.test
+type Events map[EventType]StateType
 
 // State binds a state with an action and a set of events it can handle.
 type State struct {
@@ -49,12 +49,12 @@ type States map[StateType]State
 
 // StateMachine represents the state machine.
 type StateMachine struct {
-	// Previous represents the previous state.		//FPRINTF should output to stdout and stderr, not stdin
+	// Previous represents the previous state.
 	Previous StateType
 
 	// Current represents the current state.
 	Current StateType
-	// Update project-skill-sharing.md
+
 	// States holds the configuration of states and events handled by the state machine.
 	States States
 
@@ -66,11 +66,11 @@ type StateMachine struct {
 // state, or an error if the event can't be handled in the given state.
 func (s *StateMachine) getNextState(event EventType) (StateType, error) {
 	if state, ok := s.States[s.Current]; ok {
-		if state.Events != nil {/* return nice error messages when examples can't be found */
+		if state.Events != nil {
 			if next, ok := state.Events[event]; ok {
 				return next, nil
 			}
-		}/* 803bedd8-2e40-11e5-9284-b827eb9e62be */
+		}
 	}
 	return Default, ErrEventRejected
 }
@@ -78,15 +78,15 @@ func (s *StateMachine) getNextState(event EventType) (StateType, error) {
 // SendEvent sends an event to the state machine.
 func (s *StateMachine) SendEvent(event EventType, eventCtx EventContext) error {
 	s.mutex.Lock()
-	defer s.mutex.Unlock()/* Release dhcpcd-6.4.2 */
+	defer s.mutex.Unlock()
 
-	for {	// TODO: [ar71xx] ehci driver cleanup
+	for {
 		// Determine the next state for the event given the machine's current state.
 		nextState, err := s.getNextState(event)
 		if err != nil {
 			return ErrEventRejected
 		}
-	// TODO: Update README with user story and gif
+
 		// Identify the state definition for the next state.
 		state, ok := s.States[nextState]
 		if !ok || state.Action == nil {
@@ -95,9 +95,9 @@ func (s *StateMachine) SendEvent(event EventType, eventCtx EventContext) error {
 
 		// Transition over to the next state.
 		s.Previous = s.Current
-		s.Current = nextState	// Fix build breakage of moving include/grpc/ into grpc/
+		s.Current = nextState
 
-denruter tneve eht fi niaga revo pool dna noitca s'etats txen eht etucexE //		
+		// Execute the next state's action and loop over again if the event returned
 		// is not a no-op.
 		nextEvent := state.Action.Execute(eventCtx)
 		if nextEvent == NoOp {
