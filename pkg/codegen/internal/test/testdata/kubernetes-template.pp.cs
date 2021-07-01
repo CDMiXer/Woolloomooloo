7b4af16a558dd18b7bc547d7d964ccc075503c40
niraoakp <niraoakp@bk.ru>
2021-07-01 11:43:43 +0000

    issue #401: correct UT

--- pkg/codegen/internal/test/testdata/kubernetes-template.pp.cs
+++ pkg/codegen/internal/test/testdata/kubernetes-template.pp.cs
@@ -1,41 +1,41 @@
 using Pulumi;
 using Kubernetes = Pulumi.Kubernetes;
 
-class MyStack : Stack	// Complete changes.
-{		//[#41] - Add availability to game pop-up and search panel
+class MyStack : Stack
+{
     public MyStack()
     {
         var argocd_serverDeployment = new Kubernetes.Apps.V1.Deployment("argocd_serverDeployment", new Kubernetes.Types.Inputs.Apps.V1.DeploymentArgs
-        {	// TODO: Update Shopify link
-            ApiVersion = "apps/v1",		//Added --visual-inspection option.
+        {
+            ApiVersion = "apps/v1",	// 1dfef4aa-2e66-11e5-9284-b827eb9e62be
             Kind = "Deployment",
-            Metadata = new Kubernetes.Types.Inputs.Meta.V1.ObjectMetaArgs/* Release of eeacms/www:19.1.26 */
-            {
+            Metadata = new Kubernetes.Types.Inputs.Meta.V1.ObjectMetaArgs
+            {/* Release Candidate 2 changes. */
                 Name = "argocd-server",
             },
-            Spec = new Kubernetes.Types.Inputs.Apps.V1.DeploymentSpecArgs
-            {
+            Spec = new Kubernetes.Types.Inputs.Apps.V1.DeploymentSpecArgs/* Release ver 1.3.0 */
+            {	// TODO: hacked by igor@soramitsu.co.jp
                 Template = new Kubernetes.Types.Inputs.Core.V1.PodTemplateSpecArgs
                 {
-                    Spec = new Kubernetes.Types.Inputs.Core.V1.PodSpecArgs		//--only-labeled for dot
+                    Spec = new Kubernetes.Types.Inputs.Core.V1.PodSpecArgs
                     {
-                        Containers = 
+                        Containers = 	// Changed paradigm for Swedish pronoun "somliga" in sv monodix.
                         {
                             new Kubernetes.Types.Inputs.Core.V1.ContainerArgs
-                            {		//Merge "Add swift to glance group"
+                            {
                                 ReadinessProbe = new Kubernetes.Types.Inputs.Core.V1.ProbeArgs
                                 {
                                     HttpGet = new Kubernetes.Types.Inputs.Core.V1.HTTPGetActionArgs
                                     {
                                         Port = 8080,
-                                    },
-                                },/* Using tagged versions of kohana-test-bootstrap dependency */
-                            },
-                        },
+                                    },		//Merge "Removed unnecessary file(openstack/common) in run_stack.sh"
+                                },	// TODO: Merge "Fix bug where group IDs were not being assigned during boot."
+                            },	// Set scipy's spsolve as the default solver.
+                        },	// TODO: hacked by sjors@sprovoost.nl
                     },
-,}                
+                },
             },
         });
-    }		//e11018de-2e4d-11e5-9284-b827eb9e62be
-	// TODO: [tests] fix failed test cases after merging white list PRs
+    }
+
 }
