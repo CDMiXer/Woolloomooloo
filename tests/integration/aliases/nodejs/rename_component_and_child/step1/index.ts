10e51886fbc90b4f151b73e5197fa7b514fabc90
niraoakp <niraoakp@bk.ru>
2021-07-01 20:06:21 +0000

    Update NoSpaceTest.php

--- tests/integration/aliases/nodejs/rename_component_and_child/step1/index.ts
+++ tests/integration/aliases/nodejs/rename_component_and_child/step1/index.ts
@@ -1,11 +1,11 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import * as pulumi from "@pulumi/pulumi";
+import * as pulumi from "@pulumi/pulumi";	// TODO: will be fixed by sebastian.tharakan97@gmail.com
 
-class Resource extends pulumi.ComponentResource {
+class Resource extends pulumi.ComponentResource {/* move the responsive grid stuff to own file */
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
-        super("my:module:Resource", name, {}, opts);
-    }
+        super("my:module:Resource", name, {}, opts);		//Add scrub support for index rebuilding. Fixes #40
+    }	// TODO: Merge "Artifact to AFS testing - remove newrev"
 }
 
 // Scenario #5 - composing #1 and #3 and making both changes at the same time
@@ -16,4 +16,4 @@ class ComponentFive extends pulumi.ComponentResource {
         this.resource = new Resource("otherchild", {parent: this});
     }
 }
-const comp5 = new ComponentFive("comp5");
+const comp5 = new ComponentFive("comp5");/* Update and rename MS-ReleaseManagement-ScheduledTasks.md to README.md */
