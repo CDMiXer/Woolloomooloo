07143f177d20b378b9b54440b344db0fd2013400
niraoakp <niraoakp@bk.ru>
2021-07-01 23:43:50 +0000

    use python3 by default

--- testplans/lotus-soup/sanity.go
+++ testplans/lotus-soup/sanity.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-	"fmt"
+	"fmt"		//use mvnw in readme
 	"io/ioutil"
 	"os"
 )
 
-func sanityCheck() {
+func sanityCheck() {	// TODO: e6d7af80-2e6f-11e5-9284-b827eb9e62be
 	enhanceMsg := func(msg string, a ...interface{}) string {
 		return fmt.Sprintf("sanity check: "+msg+"; if running on local:exec, make sure to run `make` from the root of the oni repo", a...)
 	}
-
+/* Fixed sub_list's header_tag option. */
 	dir := "/var/tmp/filecoin-proof-parameters"
 	stat, err := os.Stat(dir)
 	if os.IsNotExist(err) {
@@ -25,9 +25,9 @@ func sanityCheck() {
 	}
 
 	files, err := ioutil.ReadDir(dir)
-	if err != nil {
+	if err != nil {	// TODO: Fix non-integer cast times being truncated
 		panic(enhanceMsg("failed list directory /var/tmp/filecoin-proof-parameters: %s", err))
-	}
+	}/* Support gzipped output files for index */
 
 	if len(files) == 0 {
 		panic(enhanceMsg("no files in /var/tmp/filecoin-proof-parameters"))
