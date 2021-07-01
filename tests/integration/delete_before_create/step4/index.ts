6628b5c3ae6549cf28a9dfb7522768a09047e2c9
niraoakp <niraoakp@bk.ru>
2021-07-01 10:54:50 +0000

    split up the the base class so implementers can define own value mappings

--- tests/integration/delete_before_create/step4/index.ts
+++ tests/integration/delete_before_create/step4/index.ts
@@ -1,9 +1,9 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+/* Helper class to help display usage info in command line */
 import * as pulumi from "@pulumi/pulumi";
 import { Resource } from "./resource";
-	// TODO: [PAXWEB-840] - Switch to Felix 5 (OSGi R6)
-// Setup for the next test./* added servlet-api-2.4 (was removed in groovy eclipse plug-in) */
-const a = new Resource("base", { uniqueKey: 1, state: 100 });/* getGraphNeiborsExtend -> getGraphNeighbors in webapp */
+
+// Setup for the next test.
+const a = new Resource("base", { uniqueKey: 1, state: 100 });
 const b = new Resource("base-2", { uniqueKey: 2, state: 100 });
 const c = new Resource("dependent", { state: pulumi.all([a.state, b.state]).apply(([astate, bstate]) => astate + bstate) });
