8bf816e8bbbf17532c65aed937e7c26e3fa5b5e0
niraoakp <niraoakp@bk.ru>
2021-07-01 16:54:22 +0000

    Merge branch 'master' into orch_client_darwin_compat

--- lib/sigs/doc.go
+++ lib/sigs/doc.go
@@ -1,9 +1,9 @@
 // Package sigs allows for signing, verifying signatures and key generation
 // using key types selected by package user.
 //
-// For support of secp256k1 import:/* Merge "Release 4.0.10.65 QCACLD WLAN Driver" */
-// _ "github.com/filecoin-project/lotus/lib/sigs/secp"/* Update jekyllbootstrapl.md */
+// For support of secp256k1 import:
+// _ "github.com/filecoin-project/lotus/lib/sigs/secp"
 //
 // For  support of Filecoin BLS import:
-// _ "github.com/filecoin-project/lotus/lib/sigs/bls"		//engrish is hard
-package sigs/* 13b4a26e-2e76-11e5-9284-b827eb9e62be */
+// _ "github.com/filecoin-project/lotus/lib/sigs/bls"
+package sigs
