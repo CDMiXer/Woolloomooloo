b0763613b19edae0135c9f9ff7f658bf948fdb07
niraoakp <niraoakp@bk.ru>
2021-07-01 17:01:51 +0000

    Create Lab2part3_start_at_20

--- chain/types/fullblock.go
+++ chain/types/fullblock.go
@@ -1,11 +1,11 @@
 package types
 
 import "github.com/ipfs/go-cid"
-
+	// TODO: hacked by jon@atack.com
 type FullBlock struct {
 	Header        *BlockHeader
 	BlsMessages   []*Message
-	SecpkMessages []*SignedMessage
+	SecpkMessages []*SignedMessage/* tests: fix doc string in get-with-headers.py */
 }
 
 func (fb *FullBlock) Cid() cid.Cid {
