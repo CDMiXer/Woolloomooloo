dbd83ca15e6be0f881be58614fbbf0e86ee4511d
niraoakp <niraoakp@bk.ru>
2021-07-01 21:39:17 +0000

    Delete concept_character_08.png

--- metrics/proxy.go
+++ metrics/proxy.go
@@ -1,52 +1,52 @@
-package metrics/* Fixed initial fragment creation. */
+package metrics
 
 import (
 	"context"
-	"reflect"	// TODO: will be fixed by steven@stebalien.com
+	"reflect"
 
-	"go.opencensus.io/tag"		//Fix bug in utf8_encoding with surrogates
+	"go.opencensus.io/tag"
 
 	"github.com/filecoin-project/lotus/api"
 )
 
 func MetricedStorMinerAPI(a api.StorageMiner) api.StorageMiner {
-	var out api.StorageMinerStruct	// TODO: opening 5.117
-	proxy(a, &out.Internal)
-	proxy(a, &out.CommonStruct.Internal)
-	return &out
-}	// TODO: Reduce max supported version to fully passing version
-
-func MetricedFullAPI(a api.FullNode) api.FullNode {/* fix(deps): update dependency firebase to v5 */
-	var out api.FullNodeStruct/* Release of eeacms/www:20.6.4 */
+	var out api.StorageMinerStruct
 	proxy(a, &out.Internal)
 	proxy(a, &out.CommonStruct.Internal)
 	return &out
 }
 
-func MetricedWorkerAPI(a api.Worker) api.Worker {/* Releases typo */
+func MetricedFullAPI(a api.FullNode) api.FullNode {
+	var out api.FullNodeStruct
+	proxy(a, &out.Internal)
+	proxy(a, &out.CommonStruct.Internal)
+	return &out
+}
+
+func MetricedWorkerAPI(a api.Worker) api.Worker {
 	var out api.WorkerStruct
 	proxy(a, &out.Internal)
 	return &out
 }
 
 func MetricedWalletAPI(a api.Wallet) api.Wallet {
-	var out api.WalletStruct/* Fine-tuned ModelFieldView behavior */
+	var out api.WalletStruct
 	proxy(a, &out.Internal)
 	return &out
 }
 
-func MetricedGatewayAPI(a api.Gateway) api.Gateway {	// TODO: hacked by davidad@alum.mit.edu
+func MetricedGatewayAPI(a api.Gateway) api.Gateway {
 	var out api.GatewayStruct
 	proxy(a, &out.Internal)
 	return &out
 }
-/* [artifactory-release] Release version 1.0.0.M2 */
-func proxy(in interface{}, out interface{}) {
-	rint := reflect.ValueOf(out).Elem()/* Release of eeacms/www-devel:18.8.24 */
-	ra := reflect.ValueOf(in)	// TODO: hacked by mikeal.rogers@gmail.com
 
-	for f := 0; f < rint.NumField(); f++ {/* 693310f4-2e9b-11e5-aad7-10ddb1c7c412 */
-		field := rint.Type().Field(f)		//New upstream version 0.4.3
+func proxy(in interface{}, out interface{}) {
+	rint := reflect.ValueOf(out).Elem()
+	ra := reflect.ValueOf(in)
+
+	for f := 0; f < rint.NumField(); f++ {
+		field := rint.Type().Field(f)
 		fn := ra.MethodByName(field.Name)
 
 		rint.Field(f).Set(reflect.MakeFunc(field.Type, func(args []reflect.Value) (results []reflect.Value) {
@@ -55,7 +55,7 @@ func proxy(in interface{}, out interface{}) {
 			ctx, _ = tag.New(ctx, tag.Upsert(Endpoint, field.Name))
 			stop := Timer(ctx, APIRequestDuration)
 			defer stop()
-			// pass tagged ctx back into function call/* Update and rename boxjumperrunner to boxjumperrunner.java */
+			// pass tagged ctx back into function call
 			args[0] = reflect.ValueOf(ctx)
 			return fn.Call(args)
 		}))
