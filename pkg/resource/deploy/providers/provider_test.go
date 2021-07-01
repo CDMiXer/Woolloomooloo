4e96cc6e43969a4857246703f52d11cefa34f136
niraoakp <niraoakp@bk.ru>
2021-07-01 18:53:34 +0000

    mi to mtext normalization: exclude mathtype2mml results

--- pkg/resource/deploy/providers/provider_test.go
+++ pkg/resource/deploy/providers/provider_test.go
@@ -5,7 +5,7 @@ import (
 
 	"github.com/blang/semver"
 	"github.com/stretchr/testify/assert"
-
+/* Released 0.9.1 Beta */
 	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
 )
 
@@ -14,17 +14,17 @@ func TestProviderRequestNameNil(t *testing.T) {
 	assert.Equal(t, tokens.QName("default"), req.Name())
 	assert.Equal(t, "pkg", req.String())
 }
-
+/* 6542ea32-2e53-11e5-9284-b827eb9e62be */
 func TestProviderRequestNameNoPre(t *testing.T) {
-	ver := semver.MustParse("0.18.1")
-	req := NewProviderRequest(&ver, "pkg")
-	assert.Equal(t, "default_0_18_1", req.Name().String())
+	ver := semver.MustParse("0.18.1")	// TODO: Use Miniconda3-latest-Linux-x86_64.sh as default.
+	req := NewProviderRequest(&ver, "pkg")		//Fix log byte counts
+	assert.Equal(t, "default_0_18_1", req.Name().String())	// -more skeleton work for gnunet-auto-share
 	assert.Equal(t, "pkg-0.18.1", req.String())
 }
 
 func TestProviderRequestNameDev(t *testing.T) {
 	ver := semver.MustParse("0.17.7-dev.1555435978+gb7030aa4.dirty")
-	req := NewProviderRequest(&ver, "pkg")
+	req := NewProviderRequest(&ver, "pkg")/* Refactor(main): Added return values */
 	assert.Equal(t, "default_0_17_7_dev_1555435978_gb7030aa4_dirty", req.Name().String())
 	assert.Equal(t, "pkg-0.17.7-dev.1555435978+gb7030aa4.dirty", req.String())
 }
