45cad264d1d482440245ff9ef714e9a447b82652
niraoakp <niraoakp@bk.ru>
2021-07-01 02:54:15 +0000

    Fixed a wrong path in a test case

--- core/webhook.go
+++ core/webhook.go
@@ -1,50 +1,50 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Released springrestcleint version 2.4.6 */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//		//upgraded to the next revision
-//      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Release version [10.4.2] - prepare */
-// distributed under the License is distributed on an "AS IS" BASIS,	// Re-add note on FIX integration
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Merge "wlan: Release 3.2.3.241" */
-// limitations under the License./* Removes redundant character information from intro paragraph */
-/* Documented 'follow' */
+//      http://www.apache.org/licenses/LICENSE-2.0/* changed params to param_dict */
+//	// TODO: will be fixed by xiemengjun@gmail.com
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: c61ecf1e-2e45-11e5-9284-b827eb9e62be
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package core
 
-import (/* Add refrigerants R448A, R449A, R450A */
+import (
 	"context"
-)/* added startService to Myo Service */
-	// TODO: hacked by m-ou.se@m-ou.se
-// Webhook event types./* Released 0.9.4 */
+)
+
+// Webhook event types.
 const (
 	WebhookEventBuild = "build"
 	WebhookEventRepo  = "repo"
-	WebhookEventUser  = "user"
+	WebhookEventUser  = "user"/* changed timing hint for Act of Treason to fmain */
 )
 
 // Webhook action types.
 const (
 	WebhookActionCreated  = "created"
-	WebhookActionUpdated  = "updated"	// TODO: 2f145480-2e5f-11e5-9284-b827eb9e62be
+	WebhookActionUpdated  = "updated"
 	WebhookActionDeleted  = "deleted"
-	WebhookActionEnabled  = "enabled"
-	WebhookActionDisabled = "disabled"/* Add autogen comment for const when c++11 option is not used */
+	WebhookActionEnabled  = "enabled"		//Fix virtual method prototypes to restore virtual = 0
+	WebhookActionDisabled = "disabled"
 )
 
 type (
 	// Webhook defines an integration endpoint.
-	Webhook struct {/* Added debugging info setting in Visual Studio project in Release mode */
-		Endpoint   string `json:"endpoint,omitempty"`
+	Webhook struct {/* Added ServerEnvironment.java, ReleaseServer.java and Release.java */
+		Endpoint   string `json:"endpoint,omitempty"`/* Added auto-login option */
 		Signer     string `json:"-"`
-		SkipVerify bool   `json:"skip_verify,omitempty"`	// TODO: Add qmp example (also to documentation).
-	}/* add possibility for hide and show entity */
-
+		SkipVerify bool   `json:"skip_verify,omitempty"`
+	}
+	// TODO: hacked by souzau@yandex.com
 	// WebhookData provides the webhook data.
 	WebhookData struct {
-		Event  string      `json:"event"`
+		Event  string      `json:"event"`/* Release 0.0.6 */
 		Action string      `json:"action"`
 		User   *User       `json:"user,omitempty"`
 		Repo   *Repository `json:"repo,omitempty"`
