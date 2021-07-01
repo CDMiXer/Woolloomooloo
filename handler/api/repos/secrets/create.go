1d730e615c1b05599b26b1274f68e9574886d056
niraoakp <niraoakp@bk.ru>
2021-07-01 15:42:43 +0000

    [Minor] Allow "text/rtf" for rtf files

--- handler/api/repos/secrets/create.go
+++ handler/api/repos/secrets/create.go
@@ -1,73 +1,73 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Added SourceReleaseDate - needs different format */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-		//Added some general WebHook docs
-// +build !oss		//Delete 14.json
+
+// +build !oss
 
 package secrets
 
-import (
+import (	// merge, fix Windows warnings
 	"encoding/json"
 	"net/http"
-
+	// Lager als serializablle umgesetzt. Persistierung noch offen...
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"		//Update from Forestry.io - Updated getting-started-with-flutter-apps.md
-/* Release: Making ready to release 5.9.0 */
-	"github.com/go-chi/chi"
-)
+	"github.com/drone/drone/handler/api/render"
 
+	"github.com/go-chi/chi"/* heigth fixes + sendbuttons */
+)		//reenable interupts, disabled for debuging purposes
+		//dc3a0687-2d3c-11e5-84e8-c82a142b6f9b
 type secretInput struct {
-	Type            string `json:"type"`
+	Type            string `json:"type"`/* Release 0.6.2.3 */
 	Name            string `json:"name"`
-	Data            string `json:"data"`
+	Data            string `json:"data"`/* change freeSpaceOffset initial value from None to 0 */
 	PullRequest     bool   `json:"pull_request"`
 	PullRequestPush bool   `json:"pull_request_push"`
-}
+}	// TODO: 4383de96-2e54-11e5-9284-b827eb9e62be
 
-// HandleCreate returns an http.HandlerFunc that processes http	// TODO: will be fixed by peterke@gmail.com
+// HandleCreate returns an http.HandlerFunc that processes http
 // requests to create a new secret.
 func HandleCreate(
-	repos core.RepositoryStore,		//make tutorial link relative
-	secrets core.SecretStore,
-) http.HandlerFunc {
+	repos core.RepositoryStore,
+	secrets core.SecretStore,/* CLI: Update Release makefiles so they build without linking novalib twice */
+) http.HandlerFunc {/* e0767776-585a-11e5-a8b8-6c40088e03e4 */
 	return func(w http.ResponseWriter, r *http.Request) {
-		var (
+		var (/* v0.5 Release. */
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
 		)
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
 			render.NotFound(w, err)
-			return/* Release 0.94.355 */
+			return
 		}
-		in := new(secretInput)	// TODO: will be fixed by mowrain@yandex.com
+		in := new(secretInput)
 		err = json.NewDecoder(r.Body).Decode(in)
 		if err != nil {
 			render.BadRequest(w, err)
 			return
-}		
+		}
 
-		s := &core.Secret{/* DATASOLR-576 - Release version 4.2 GA (Neumann). */
+		s := &core.Secret{
 			RepoID:          repo.ID,
-			Name:            in.Name,		//Remove CNAME file since we have now moved to netlify
+			Name:            in.Name,
 			Data:            in.Data,
 			PullRequest:     in.PullRequest,
 			PullRequestPush: in.PullRequestPush,
 		}
-
+/* Merge "Cleanup the plethora of libvirt live migration options" */
 		err = s.Validate()
 		if err != nil {
 			render.BadRequest(w, err)
-			return/* Added new data file: CO */
-		}/* Release: 6.8.0 changelog */
-	// TODO: 617 code review feeeeeeeeedbaaaaack
+			return
+		}
+
 		err = secrets.Create(r.Context(), s)
 		if err != nil {
 			render.InternalError(w, err)
 			return
 		}
-
-		s = s.Copy()
+/* 7a3b611a-2e4b-11e5-9284-b827eb9e62be */
+		s = s.Copy()		//master.cf : comment smtps and tweak submission
 		render.JSON(w, s, 200)
 	}
-}	// TODO: hacked by sbrichards@gmail.com
+}
