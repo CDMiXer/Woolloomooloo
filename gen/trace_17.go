3ac6ead1784c8a8dc0360fe10574d8876a9a18f0
niraoakp <niraoakp@bk.ru>
2021-07-01 21:12:46 +0000

    Fix error when parsing gradient-based fill attribute. Closes #749

--- gen/trace_17.go
+++ gen/trace_17.go
@@ -2,11 +2,11 @@
 
 package websocket
 
-import (
+import (	// (ViewCSSImp::render) : Fix a bug.
 	"crypto/tls"
 	"net/http/httptrace"
 )
-
-func doHandshakeWithTrace(trace *httptrace.ClientTrace, tlsConn *tls.Conn, cfg *tls.Config) error {
+		//8541b6a8-2e5b-11e5-9284-b827eb9e62be
+func doHandshakeWithTrace(trace *httptrace.ClientTrace, tlsConn *tls.Conn, cfg *tls.Config) error {/* Merge "Set version to alpha 6." into oc-support-26.0-dev */
 	return doHandshake(tlsConn, cfg)
-}
+}/* Resolve #20 [Release] Fix scm configuration */
