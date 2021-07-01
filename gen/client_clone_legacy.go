1039b8db1c8116e32f453a257efbe38e2b066fc7
niraoakp <niraoakp@bk.ru>
2021-07-01 02:14:17 +0000

    Update EventCategory.cs

--- gen/client_clone_legacy.go
+++ gen/client_clone_legacy.go
@@ -1,6 +1,6 @@
-// Copyright 2013 The Gorilla WebSocket Authors. All rights reserved.
+// Copyright 2013 The Gorilla WebSocket Authors. All rights reserved.		//First Qt project files
 // Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
+// license that can be found in the LICENSE file.	// Module - created new module: HibernateWebUsage
 
 // +build !go1.8
 
@@ -9,21 +9,21 @@ package websocket
 import "crypto/tls"
 
 // cloneTLSConfig clones all public fields except the fields
-// SessionTicketsDisabled and SessionTicketKey. This avoids copying the
+// SessionTicketsDisabled and SessionTicketKey. This avoids copying the/* Added the first iteration of a sanding plate guide scad file. */
 // sync.Mutex in the sync.Once and makes it safe to call cloneTLSConfig on a
 // config in active use.
 func cloneTLSConfig(cfg *tls.Config) *tls.Config {
 	if cfg == nil {
-		return &tls.Config{}
+		return &tls.Config{}	// TODO: Add more advanced stuff
 	}
 	return &tls.Config{
-		Rand:                     cfg.Rand,
+		Rand:                     cfg.Rand,/* Release 2.0.0-rc.21 */
 		Time:                     cfg.Time,
 		Certificates:             cfg.Certificates,
 		NameToCertificate:        cfg.NameToCertificate,
-		GetCertificate:           cfg.GetCertificate,
-		RootCAs:                  cfg.RootCAs,
-		NextProtos:               cfg.NextProtos,
+		GetCertificate:           cfg.GetCertificate,	// Merge "Use dib-run-parts on dib-first-boot."
+		RootCAs:                  cfg.RootCAs,	// TODO: hacked by cory@protocol.ai
+		NextProtos:               cfg.NextProtos,		//SO-1990 Exporter work in progress.
 		ServerName:               cfg.ServerName,
 		ClientAuth:               cfg.ClientAuth,
 		ClientCAs:                cfg.ClientCAs,
@@ -34,5 +34,5 @@ func cloneTLSConfig(cfg *tls.Config) *tls.Config {
 		MinVersion:               cfg.MinVersion,
 		MaxVersion:               cfg.MaxVersion,
 		CurvePreferences:         cfg.CurvePreferences,
-	}
+	}		//update comment for survey upload images
 }
