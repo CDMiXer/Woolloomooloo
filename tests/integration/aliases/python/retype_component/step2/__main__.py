7853b00082238d9b0a56611b5c99e7892c5f1f7f
niraoakp <niraoakp@bk.ru>
2021-07-01 11:16:44 +0000

    [FIX] mail: fixed body, subject in default_get method in email.compose.message

--- tests/integration/aliases/python/retype_component/step2/__main__.py
+++ tests/integration/aliases/python/retype_component/step2/__main__.py
@@ -1,29 +1,29 @@
-# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+# Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Release of eeacms/jenkins-master:2.235.2 */
 
-import copy		//Remove framework dependency handling
-/* Release v0.2.1.2 */
+import copy
+	// this file was missing preventing manual build
 from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE
 
 class Resource1(ComponentResource):
     def __init__(self, name, opts=None):
         super().__init__("my:module:Resource", name, None, opts)
-
+/* Release v0.5.3 */
 # Scenario #4 - change the type of a component
-class ComponentFour(ComponentResource):
+class ComponentFour(ComponentResource):/* spaced readme */
     def __init__(self, name, opts=ResourceOptions()):
-        # Add an alias that references the old type of this resource.../* Create map_java.js */
-        aliases = [Alias(type_="my:module:ComponentFour")]
+        # Add an alias that references the old type of this resource...
+        aliases = [Alias(type_="my:module:ComponentFour")]/* Merged QA into master */
         if opts.aliases is not None:
             for alias in opts.aliases:
                 aliases.append(alias)
-
+/* Added dragging tests to solve #147 */
         # ..and then make the super call with the new type of this resource and the added alias.
         opts_copy = copy.copy(opts)
         opts_copy.aliases = aliases
         super().__init__("my:differentmodule:ComponentFourWithADifferentTypeName", name, None, opts_copy)
-/* Merge "Camera3: Notify device error to f/w during daemon crash" into lmp-dev */
+
         # The child resource will also pick up an implicit alias due to the new type of the component it is parented
-        # to.
+        # to.		//Create Floyd-Warshall Algorithm
         res1 = Resource1("otherchild", ResourceOptions(parent=self))
-/* Merge "clk: msm: clock-cpu-8953: Remove support of boost and Vmin" */
-comp4 = ComponentFour("comp4")	// TODO: hacked by remco@dutchcoders.io
+
+comp4 = ComponentFour("comp4")
