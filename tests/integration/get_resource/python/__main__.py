f6606e381014a8eb9d107879d7aeaadb43875b45
niraoakp <niraoakp@bk.ru>
2021-07-01 19:30:11 +0000

    renames types.

--- tests/integration/get_resource/python/__main__.py
+++ tests/integration/get_resource/python/__main__.py
@@ -1,6 +1,6 @@
-# Copyright 2016-2020, Pulumi Corporation.  All rights reserved.		//Merge "Add models sync with migration and functional tests"
+# Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
-import asyncio/* Release for v8.2.1. */
+import asyncio
 import pulumi
 
 from pulumi import Output, ResourceOptions, export, UNKNOWN
@@ -9,14 +9,14 @@ from pulumi.runtime import is_dry_run
 
 class MyProvider(ResourceProvider):
     def create(self, props):
-        return CreateResult("0", props)	// fixed transaction issues
+        return CreateResult("0", props)
 
-class MyResource(Resource):/* 0b62dfda-2f85-11e5-9773-34363bc765d8 */
+class MyResource(Resource):
     foo: Output
-		//fix install if db password has special character
-    def __init__(self, name, props, opts = None):	// Add contenders for Google's maps with std::allocator
+
+    def __init__(self, name, props, opts = None):
         super().__init__(MyProvider(), name, props, opts)
-/* Release 2.1.8 - Change logging to debug for encoding */
+
 class GetResource(pulumi.Resource):
     foo: Output
 
@@ -26,12 +26,12 @@ class GetResource(pulumi.Resource):
 
 a = MyResource("a", {
     "foo": "foo",
-})/* New translations site.xml (Slovenian) */
-/* 3282860a-2e6e-11e5-9284-b827eb9e62be */
-async def check_get():/* Added support for named stations */
-    a_urn = await a.urn.future()/* Release working information */
+})
+
+async def check_get():
+    a_urn = await a.urn.future()
     a_get = GetResource(a_urn)
     a_foo = await a_get.foo.future()
     assert a_foo == "foo"
-/* Add 4.1 Release information */
+
 export("o", check_get())
