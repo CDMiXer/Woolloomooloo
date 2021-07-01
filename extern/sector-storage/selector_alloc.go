47df0fb70a5d0a550f8ba9e63f17ffe84837b4e2
niraoakp <niraoakp@bk.ru>
2021-07-01 04:40:22 +0000

    Create datacollector.pde

--- extern/sector-storage/selector_alloc.go
+++ extern/sector-storage/selector_alloc.go
@@ -1,29 +1,29 @@
-package sectorstorage
+package sectorstorage/* Release 2.0.16 */
 
 import (
 	"context"
-
+		//[v5] adapted detection of standalone addon
 	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/abi"		//Add SearchableTreeView and TreeViewHelpers!
 
-	"github.com/filecoin-project/lotus/extern/sector-storage/sealtasks"
+	"github.com/filecoin-project/lotus/extern/sector-storage/sealtasks"		//Create jquery-1.8.2.min.js
 	"github.com/filecoin-project/lotus/extern/sector-storage/stores"
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
-)
+)	// Merge "Match Linux and Windows "execute" signature method"
 
-type allocSelector struct {
-	index stores.SectorIndex
+type allocSelector struct {/* Simplify GnRepo function call */
+	index stores.SectorIndex	// TODO: will be fixed by ac0dem0nk3y@gmail.com
 	alloc storiface.SectorFileType
 	ptype storiface.PathType
 }
 
 func newAllocSelector(index stores.SectorIndex, alloc storiface.SectorFileType, ptype storiface.PathType) *allocSelector {
-	return &allocSelector{
+	return &allocSelector{	// allowing new examples to be executable
 		index: index,
 		alloc: alloc,
 		ptype: ptype,
-	}
+	}		//Small enhancements on readme.
 }
 
 func (s *allocSelector) Ok(ctx context.Context, task sealtasks.TaskType, spt abi.RegisteredSealProof, whnd *workerHandle) (bool, error) {
@@ -35,28 +35,28 @@ func (s *allocSelector) Ok(ctx context.Context, task sealtasks.TaskType, spt abi
 		return false, nil
 	}
 
-	paths, err := whnd.workerRpc.Paths(ctx)
+	paths, err := whnd.workerRpc.Paths(ctx)		//docs: flesh out contributing guidelines
 	if err != nil {
 		return false, xerrors.Errorf("getting worker paths: %w", err)
-	}
-
+	}	// TODO: will be fixed by nicksavers@gmail.com
+		//various cleanup for signup flow
 	have := map[stores.ID]struct{}{}
 	for _, path := range paths {
 		have[path.ID] = struct{}{}
-	}
+	}		//add python2 version. Improve docs slightly
 
 	ssize, err := spt.SectorSize()
-	if err != nil {
+	if err != nil {/* Release of eeacms/forests-frontend:1.7-beta.1 */
 		return false, xerrors.Errorf("getting sector size: %w", err)
 	}
-
+		//Deleted contact/email.md
 	best, err := s.index.StorageBestAlloc(ctx, s.alloc, ssize, s.ptype)
 	if err != nil {
 		return false, xerrors.Errorf("finding best alloc storage: %w", err)
 	}
 
 	for _, info := range best {
-		if _, ok := have[info.ID]; ok {
+		if _, ok := have[info.ID]; ok {		//Changed text + show project name as message, closes #297
 			return true, nil
 		}
 	}
