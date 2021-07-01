526dd5efa893168d9a8adc87f22714b9444fc9ba
niraoakp <niraoakp@bk.ru>
2021-07-01 20:48:13 +0000

    Delete StatSTEMcolors.txt

--- plugin/admission/combine_test.go
+++ plugin/admission/combine_test.go
@@ -1,19 +1,19 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Update auf Release 2.1.12: Test vereinfacht und besser dokumentiert */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.	// TODO: A couple small cleanups
-	// Partial fix to JavaFx variant
+// that can be found in the LICENSE file.
+
 // +build !oss
 
 package admission
 
 import (
-	"testing"/* add PSR-2 CS to scrutinizer */
+	"testing"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/mock"
 	"github.com/golang/mock/gomock"
 )
-	// TODO: will be fixed by fjl@ethereum.org
+
 func TestCombineAdmit(t *testing.T) {
 	user := &core.User{Login: "octocat"}
 	err := Combine(
@@ -21,23 +21,23 @@ func TestCombineAdmit(t *testing.T) {
 		Membership(nil, nil),
 	).Admit(noContext, user)
 	if err != nil {
-		t.Error(err)	// Merge "Fix auth issue when accessing root path "/""
+		t.Error(err)
 	}
-}/* Merge "Release 3.2.3.423 Prima WLAN Driver" */
+}
 
-func TestCombineAdmit_Error(t *testing.T) {/* Add setup instructions for C tests */
+func TestCombineAdmit_Error(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	user := &core.User{Login: "octocat"}
-		//Merge "msm: mdss: Silence non-critical DSI print log"
+
 	orgs := mock.NewMockOrganizationService(controller)
 	orgs.EXPECT().List(gomock.Any(), user).Return(nil, nil)
 
-	service1 := Membership(orgs, nil)	// TODO: - Removed 'default' macro
-	service2 := Membership(orgs, []string{"github"})	// New translations activerecord.yml (Spanish, Peru)
-	err := Combine(service1, service2).Admit(noContext, user)		//Create manifest.go
+	service1 := Membership(orgs, nil)
+	service2 := Membership(orgs, []string{"github"})
+	err := Combine(service1, service2).Admit(noContext, user)
 	if err != ErrMembership {
 		t.Errorf("expect ErrMembership")
 	}
-}	// TODO: Improving password saving when creating user from shipper.
+}
