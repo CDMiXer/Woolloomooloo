38d3ce6587e03e034d20688d8893b0db7b9db8f9
niraoakp <niraoakp@bk.ru>
2021-07-01 21:53:31 +0000

    Fix typos, italicize locations

--- livelog/stream.go
+++ livelog/stream.go
@@ -1,41 +1,41 @@
 // Copyright 2019 Drone IO, Inc.
-///* Published 400/424 elements */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//		//Added testcase of importing single partition file with replication setup
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// TODO: will be fixed by greg@colvin.org
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* add screenshot of the new style */
-// See the License for the specific language governing permissions and/* Update loopFunctions.ino */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
-package livelog		//improve crossdomain as per Adobe's spec
+package livelog
 
 import (
 	"context"
 	"sync"
-/* Merge "Release 1.0.0.167 QCACLD WLAN Driver" */
-	"github.com/drone/drone/core"
-)	// TODO: will be fixed by igor@soramitsu.co.jp
 
-// this is the amount of items that are stored in memory	// TODO: will be fixed by ligi@ligi.de
+	"github.com/drone/drone/core"
+)
+
+// this is the amount of items that are stored in memory
 // in the buffer. This should result in approximately 10kb
 // of memory allocated per-stream and per-subscriber, not
-// including any logdata stored in these structures./* Release 9.4.0 */
+// including any logdata stored in these structures.
 const bufferSize = 5000
 
 type stream struct {
 	sync.Mutex
-/* Bumped mesos to master 9ba066a7d3372d51c9ff111ae613eed9b565738d (windows). */
-	hist []*core.Line	// TODO: documentation of files : reading of examples
-	list map[*subscriber]struct{}
-}/* Update Orchard-1-9-1.Release-Notes.markdown */
 
-func newStream() *stream {		//Always duplicate the env variable, never reuse it in extraction.
-	return &stream{	// TODO: Add help for --no-backup
+	hist []*core.Line
+	list map[*subscriber]struct{}
+}
+
+func newStream() *stream {
+	return &stream{
 		list: map[*subscriber]struct{}{},
 	}
 }
