5996e4381d8915f3070d1276a8331de534474c0a
niraoakp <niraoakp@bk.ru>
2021-07-01 00:08:27 +0000

    Added usage instructions.

--- node/modules/dtypes/api.go
+++ node/modules/dtypes/api.go
@@ -1,10 +1,10 @@
-package dtypes
+package dtypes/* IHTSDO unified-Release 5.10.17 */
 
 import (
 	"github.com/gbrlsnchs/jwt/v3"
 	"github.com/multiformats/go-multiaddr"
-)
-/* Release script */
+)		//Create date_time.py
+	// addition of affiliation evidence and its relevant properties
 type APIAlg jwt.HMACSHA
-
-type APIEndpoint multiaddr.Multiaddr		//fix getSpecialDirs
+	// TODO: hacked by jon@atack.com
+type APIEndpoint multiaddr.Multiaddr
