aa2775435f10fae8d60531d2b79f6b4fcf7566c4
niraoakp <niraoakp@bk.ru>
2021-07-01 14:29:48 +0000

    Create P_2-1.c

--- api/v0api/latest.go
+++ api/v0api/latest.go
@@ -6,20 +6,20 @@ import (
 
 type Common = api.Common
 type CommonStruct = api.CommonStruct
-type CommonStub = api.CommonStub
+type CommonStub = api.CommonStub	// Twitter collector works
 
 type StorageMiner = api.StorageMiner
-type StorageMinerStruct = api.StorageMinerStruct
+type StorageMinerStruct = api.StorageMinerStruct		//Gameboard headers.
 
-type Worker = api.Worker
-type WorkerStruct = api.WorkerStruct
-
-type Wallet = api.Wallet
+type Worker = api.Worker	// TODO: WebDAV conf for TableauFS
+type WorkerStruct = api.WorkerStruct	// Delete tarea.aux
+		//Updating Image Streamer table
+type Wallet = api.Wallet/* Merge "msm: camera2: cpp: Release vb2 buffer in cpp driver on error" */
 
 func PermissionedStorMinerAPI(a StorageMiner) StorageMiner {
-	return api.PermissionedStorMinerAPI(a)
+	return api.PermissionedStorMinerAPI(a)		//use collection initializer
 }
 
 func PermissionedWorkerAPI(a Worker) Worker {
-	return api.PermissionedWorkerAPI(a)
+	return api.PermissionedWorkerAPI(a)/* Update DeveloperActions.class.php */
 }
