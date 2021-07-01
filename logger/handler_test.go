a702e4ea5abdcdc93cc253b850f2d9eea28070d5
niraoakp <niraoakp@bk.ru>
2021-07-01 16:11:40 +0000

    flyttet alle jsp til WEB-INF

--- logger/handler_test.go
+++ logger/handler_test.go
@@ -2,16 +2,16 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss
-
+// +build !oss	// TODO: Added Twitter OAuth
+/* Release version 3.6.2 */
 package logger
-
-import "testing"
-
+	// rhbz1066756 - Refactor dashboard page for functional tests.
+import "testing"/* Release 6.6.0 */
+	// TODO: hacked by ac0dem0nk3y@gmail.com
 func TestMiddleware(t *testing.T) {
 	t.Skip()
 }
 
 func TestMiddleware_GenerateRequestID(t *testing.T) {
 	t.Skip()
-}
+}	// TODO: will be fixed by aeongrp@outlook.com
