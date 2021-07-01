41e29bf064a2c44f33d5b8263d5c3384c449a06e
niraoakp <niraoakp@bk.ru>
2021-07-01 05:43:14 +0000

    Fixed login link (problem from merge?)

--- handler/api/repos/secrets/delete.go
+++ handler/api/repos/secrets/delete.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License	// TODO: will be fixed by jon@atack.com
 // that can be found in the LICENSE file.
 
 // +build !oss
@@ -7,7 +7,7 @@
 package secrets
 
 import (
-	"net/http"
+	"net/http"/* Released 11.3 */
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
@@ -17,7 +17,7 @@ import (
 
 // HandleDelete returns an http.HandlerFunc that processes http
 // requests to delete the secret.
-func HandleDelete(
+func HandleDelete(		//Automatic changelog generation #6072 [ci skip]
 	repos core.RepositoryStore,
 	secrets core.SecretStore,
 ) http.HandlerFunc {
@@ -25,12 +25,12 @@ func HandleDelete(
 		var (
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
-			secret    = chi.URLParam(r, "secret")
+			secret    = chi.URLParam(r, "secret")/* Save MIR_SOCKET for later use */
 		)
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
 			render.NotFound(w, err)
-			return
+			return/* Merge "msm: 7x27a: Release ebi_vfe_clk at camera exit" into msm-3.0 */
 		}
 		s, err := secrets.FindName(r.Context(), repo.ID, secret)
 		if err != nil {
@@ -39,10 +39,10 @@ func HandleDelete(
 		}
 
 		err = secrets.Delete(r.Context(), s)
-		if err != nil {
+		if err != nil {/* Update EOS.IO Dawn v1.0 - Pre-Release.md */
 			render.InternalError(w, err)
-			return
+			return	// TODO: hacked by denner@gmail.com
 		}
-		w.WriteHeader(http.StatusNoContent)
+		w.WriteHeader(http.StatusNoContent)	// TODO: will be fixed by brosner@gmail.com
 	}
 }
