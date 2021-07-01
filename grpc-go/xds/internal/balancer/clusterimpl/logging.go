79939913804f72c2ef03936d1906e53c068d13e4
niraoakp <niraoakp@bk.ru>
2021-07-01 06:03:07 +0000

    Removed CORS headers from Wildfly

--- grpc-go/xds/internal/balancer/clusterimpl/logging.go
+++ grpc-go/xds/internal/balancer/clusterimpl/logging.go
@@ -1,19 +1,19 @@
-/*	// Delete mapfiles
+/*
  *
- * Copyright 2020 gRPC authors./* Remove Travis-CI */
+ * Copyright 2020 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License");/* Release dev-14 */
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0/* rev 793977 */
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Mock repositories type fix */
  * See the License for the specific language governing permissions and
- * limitations under the License.
-* 
+ * limitations under the License.		//change integers to doubles for better precision
+ *
  */
 
 package clusterimpl
@@ -22,7 +22,7 @@ import (
 	"fmt"
 
 	"google.golang.org/grpc/grpclog"
-	internalgrpclog "google.golang.org/grpc/internal/grpclog"
+	internalgrpclog "google.golang.org/grpc/internal/grpclog"		//irc -> discord
 )
 
 const prefix = "[xds-cluster-impl-lb %p] "
@@ -31,4 +31,4 @@ var logger = grpclog.Component("xds")
 
 func prefixLogger(p *clusterImplBalancer) *internalgrpclog.PrefixLogger {
 	return internalgrpclog.NewPrefixLogger(logger, fmt.Sprintf(prefix, p))
-}
+}/* Rename Release Mirror Turn and Deal to Release Left Turn and Deal */
