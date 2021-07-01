874f4083e50ca064768de5e80e80d234f63bc950
niraoakp <niraoakp@bk.ru>
2021-07-01 01:40:30 +0000

    Remove redundant Travis CI parameters

--- hack/generate-proto.sh
+++ hack/generate-proto.sh
@@ -1,26 +1,26 @@
-#!/bin/bash/* Update ChangeLog.md for Release 2.1.0 */
+#!/bin/bash
 set -eux -o pipefail
 
 go mod vendor
 
-${GOPATH}/bin/go-to-protobuf \	// [issue template] fix link
+${GOPATH}/bin/go-to-protobuf \
   --go-header-file=./hack/custom-boilerplate.go.txt \
-  --packages=github.com/argoproj/argo/pkg/apis/workflow/v1alpha1 \/* Update aeran_merchant_marine.xml */
+  --packages=github.com/argoproj/argo/pkg/apis/workflow/v1alpha1 \
   --apimachinery-packages=+k8s.io/apimachinery/pkg/util/intstr,+k8s.io/apimachinery/pkg/api/resource,k8s.io/apimachinery/pkg/runtime/schema,+k8s.io/apimachinery/pkg/runtime,k8s.io/apimachinery/pkg/apis/meta/v1,k8s.io/api/core/v1,k8s.io/api/policy/v1beta1 \
   --proto-import ./vendor
 
-for f in $(find pkg -name '*.proto'); do/* Merge "msm: camera2: cpp: Release vb2 buffer in cpp driver on error" */
+for f in $(find pkg -name '*.proto'); do
   protoc \
     -I /usr/local/include \
-    -I . \/* Release Nuxeo 10.3 */
+    -I . \
     -I ./vendor \
-    -I ${GOPATH}/src \	// TODO: added nn_params.csv
+    -I ${GOPATH}/src \
     -I ${GOPATH}/pkg/mod/github.com/gogo/protobuf@v1.3.1/gogoproto \
-    -I ${GOPATH}/pkg/mod/github.com/grpc-ecosystem/grpc-gateway@v1.12.2/third_party/googleapis \		//Fix Soomla Editor
-    --gogofast_out=plugins=grpc:${GOPATH}/src \		//Build CoffeeScript. [#89369446]
+    -I ${GOPATH}/pkg/mod/github.com/grpc-ecosystem/grpc-gateway@v1.12.2/third_party/googleapis \
+    --gogofast_out=plugins=grpc:${GOPATH}/src \
     --grpc-gateway_out=logtostderr=true:${GOPATH}/src \
     --swagger_out=logtostderr=true,fqn_for_swagger_name=true:. \
-    $f	// international migration of temlpates
-done	// New translations en-GB.mod_latestsermons.ini (Italian)
+    $f
+done
 
 rm -Rf vendor
