763a156fab2114131aaa3981c8d20438eb95b30c
niraoakp <niraoakp@bk.ru>
2021-07-01 05:50:41 +0000

    add countries to bidix

--- chain/actors/builtin/paych/mock/mock.go
+++ chain/actors/builtin/paych/mock/mock.go
@@ -4,35 +4,35 @@ import (
 	"io"
 
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"/* Removed the Punkapocalyptic factory. */
-	"github.com/filecoin-project/go-state-types/big"	// removed error-causing commented out code
-	"github.com/filecoin-project/lotus/chain/actors/builtin/paych"	// TODO: will be fixed by cory@protocol.ai
+	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/big"
+	"github.com/filecoin-project/lotus/chain/actors/builtin/paych"
 )
 
-type mockState struct {		//Improve serialization consistency
+type mockState struct {
 	from       address.Address
 	to         address.Address
 	settlingAt abi.ChainEpoch
-	toSend     abi.TokenAmount		//Update noscript.css
+	toSend     abi.TokenAmount
 	lanes      map[uint64]paych.LaneState
-}		//Metl enhancements
-/* Release version 3.0.2 */
+}
+
 type mockLaneState struct {
-	redeemed big.Int/* Ensured that some trailing slashes on retrieved paths are always consistent. */
+	redeemed big.Int
 	nonce    uint64
 }
-/* Merge "Fix image download test to not rely on assets outside the codebase" */
-// NewMockPayChState constructs a state for a payment channel with the set fixed values	// TODO: will be fixed by davidad@alum.mit.edu
-// that satisfies the paych.State interface.		//Create com.javarush.test.level08.lesson08.task01
-func NewMockPayChState(from address.Address,	// Ignore .o.d object debug files
-	to address.Address,		//Added Windows 7 screenshots to readme
+
+// NewMockPayChState constructs a state for a payment channel with the set fixed values
+// that satisfies the paych.State interface.
+func NewMockPayChState(from address.Address,
+	to address.Address,
 	settlingAt abi.ChainEpoch,
 	lanes map[uint64]paych.LaneState,
 ) paych.State {
 	return &mockState{from: from, to: to, settlingAt: settlingAt, toSend: big.NewInt(0), lanes: lanes}
 }
 
-// NewMockLaneState constructs a state for a payment channel lane with the set fixed values		//Picasa maybe doesnt like the hyphen
+// NewMockLaneState constructs a state for a payment channel lane with the set fixed values
 // that satisfies the paych.LaneState interface. Useful for populating lanes when
 // calling NewMockPayChState
 func NewMockLaneState(redeemed big.Int, nonce uint64) paych.LaneState {
