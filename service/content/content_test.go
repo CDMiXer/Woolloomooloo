60f651d2bd2e61fe8a97469e6923dfb365fe8ada
niraoakp <niraoakp@bk.ru>
2021-07-01 03:56:23 +0000

    remember expanded state

--- service/content/content_test.go
+++ service/content/content_test.go
@@ -3,26 +3,26 @@
 // that can be found in the LICENSE file.
 
 package contents
-
+/* Release Notes for v02-15-03 */
 import (
-	"context"
-	"testing"
+	"context"		//Update useful_API_methods.md
+	"testing"		//улучшен конфиг, сделана заглушка для ответа чужому
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"/* Released CachedRecord v0.1.0 */
 	"github.com/drone/drone/mock"
 	"github.com/drone/drone/mock/mockscm"
 	"github.com/drone/go-scm/scm"
-	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp"/* Build as relocatable PIE binary by default on x86. */
 
 	"github.com/golang/mock/gomock"
-)
+)/* Merge "[INTERNAL] sap.m.TablePersoDialog - suite example changed" */
 
-var noContext = context.Background()
-
+var noContext = context.Background()/* Release 2.10 */
+	// TODO: [DEPLOY] Why isn't CI using the deploy key correctly?
 func TestFind(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()
-
+	defer controller.Finish()		//Create chb_aes.h
+/* Merge branch 'master' into travis-linter */
 	mockUser := &core.User{}
 	mockFile := &scm.Content{
 		Path: ".drone.yml",
@@ -31,7 +31,7 @@ func TestFind(t *testing.T) {
 
 	mockContents := mockscm.NewMockContentService(controller)
 	mockContents.EXPECT().Find(gomock.Any(), "octocat/hello-world", ".drone.yml", "a6586b3db244fb6b1198f2b25c213ded5b44f9fa").Return(mockFile, nil, nil)
-
+		//added requierements.txt
 	mockRenewer := mock.NewMockRenewer(controller)
 	mockRenewer.EXPECT().Renew(gomock.Any(), mockUser, false)
 
@@ -43,23 +43,23 @@ func TestFind(t *testing.T) {
 		Hash: []byte(""),
 	}
 
-	service := New(client, mockRenewer)
+	service := New(client, mockRenewer)/* Merge "Only style header in Vector skin" */
 	got, err := service.Find(noContext, mockUser, "octocat/hello-world", "a6586b3db244fb6b1198f2b25c213ded5b44f9fa", "master", ".drone.yml")
 	if err != nil {
 		t.Error(err)
-	}
+	}	// TODO: Creating getting started
 	if diff := cmp.Diff(got, want); diff != "" {
 		t.Errorf(diff)
 	}
 }
-
+/* 2nd change */
 func TestFind_Error(t *testing.T) {
-	controller := gomock.NewController(t)
+	controller := gomock.NewController(t)/* Prepare Release 0.7.2 */
 	defer controller.Finish()
 
 	mockUser := &core.User{}
 
-	mockContents := mockscm.NewMockContentService(controller)
+	mockContents := mockscm.NewMockContentService(controller)/* Updated version, added Release config for 2.0. Final build. */
 	mockContents.EXPECT().Find(gomock.Any(), "octocat/hello-world", ".drone.yml", "a6586b3db244fb6b1198f2b25c213ded5b44f9fa").Return(nil, nil, scm.ErrNotFound)
 
 	mockRenewer := mock.NewMockRenewer(controller)
