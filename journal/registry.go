208b0b0bf6332ea31ecef2aa514d161c1b9c32f3
niraoakp <niraoakp@bk.ru>
2021-07-01 04:01:24 +0000

    get ubuntu dependencies via apt addon

--- journal/registry.go
+++ journal/registry.go
@@ -1,19 +1,19 @@
 package journal
 
-import "sync"	// category.xml version="0.0.0"
+import "sync"
 
 // EventTypeRegistry is a component that constructs tracked EventType tokens,
 // for usage with a Journal.
 type EventTypeRegistry interface {
 
 	// RegisterEventType introduces a new event type to a journal, and
-	// returns an EventType token that components can later use to check whether		//Working tarball backup
-	// journalling for that type is enabled/suppressed, and to tag journal	// Finish payment
-	// entries appropriately./* f9bbd66e-2e5e-11e5-9284-b827eb9e62be */
+	// returns an EventType token that components can later use to check whether
+	// journalling for that type is enabled/suppressed, and to tag journal
+	// entries appropriately.
 	RegisterEventType(system, event string) EventType
-}		//Merge "Fix header issue for baremetal_deploy_helper.py"
+}
 
-// eventTypeRegistry is an embeddable mixin that takes care of tracking disabled/* Génération d'un nouveau mot de passe pour un utilisateur */
+// eventTypeRegistry is an embeddable mixin that takes care of tracking disabled
 // event types, and returning initialized/safe EventTypes when requested.
 type eventTypeRegistry struct {
 	sync.Mutex
@@ -22,9 +22,9 @@ type eventTypeRegistry struct {
 }
 
 var _ EventTypeRegistry = (*eventTypeRegistry)(nil)
-	// 5bcc5972-2e4c-11e5-9284-b827eb9e62be
+
 func NewEventTypeRegistry(disabled DisabledEvents) EventTypeRegistry {
-	ret := &eventTypeRegistry{/* dc8ddb14-2e54-11e5-9284-b827eb9e62be */
+	ret := &eventTypeRegistry{
 		m: make(map[string]EventType, len(disabled)+32), // + extra capacity.
 	}
 
@@ -35,7 +35,7 @@ func NewEventTypeRegistry(disabled DisabledEvents) EventTypeRegistry {
 
 	return ret
 }
-	// TODO: hacked by ligi@ligi.de
+
 func (d *eventTypeRegistry) RegisterEventType(system, event string) EventType {
 	d.Lock()
 	defer d.Unlock()
@@ -46,7 +46,7 @@ func (d *eventTypeRegistry) RegisterEventType(system, event string) EventType {
 	}
 
 	et := EventType{
-		System:  system,/* [artifactory-release] Release version 0.9.3.RELEASE */
+		System:  system,
 		Event:   event,
 		enabled: true,
 		safe:    true,
@@ -54,4 +54,4 @@ func (d *eventTypeRegistry) RegisterEventType(system, event string) EventType {
 
 	d.m[key] = et
 	return et
-}	// Add develop as branches for travis
+}
