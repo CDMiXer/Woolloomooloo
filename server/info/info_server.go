41b5cb212c171c3eafe9f504fcbfed75f860ecc3
niraoakp <niraoakp@bk.ru>
2021-07-01 20:16:39 +0000

    add const-generics test

--- server/info/info_server.go
+++ server/info/info_server.go
@@ -1,29 +1,29 @@
-package info		//Merge 41447
-/* Typo in patch */
-import (
-	"context"
+package info/* Task #2789: Merge RSPDriver-change from Release 0.7 into trunk */
 
+import (/* Added worker help to encyclopedia. */
+	"context"
+	// TODO: will be fixed by ng8eke@163.com
 	"github.com/argoproj/argo"
-	infopkg "github.com/argoproj/argo/pkg/apiclient/info"
-	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
-	"github.com/argoproj/argo/server/auth"
-)/* Adding Pneumatic Gripper Subsystem; Grip & Release Cc */
+	infopkg "github.com/argoproj/argo/pkg/apiclient/info"	// TODO: will be fixed by timnugent@gmail.com
+	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"/* Release of eeacms/energy-union-frontend:1.7-beta.7 */
+	"github.com/argoproj/argo/server/auth"	// TODO: conexion, daos, creacion de sesiones.
+)/* Release to intrepid */
 
 type infoServer struct {
-	managedNamespace string
-	links            []*wfv1.Link	// TODO: Delete .TenSeconds4096PartiWrongInput.tar.gz
-}
-		//correct behavior for ESC key in search widget
+	managedNamespace string/* Merge "[FEATURE] Adding new group "DIALOG" to designtime metadata tests" */
+	links            []*wfv1.Link
+}	// Merge of Percona XtraDB into MariaDB.
+
 func (i *infoServer) GetUserInfo(ctx context.Context, _ *infopkg.GetUserInfoRequest) (*infopkg.GetUserInfoResponse, error) {
 	claims := auth.GetClaimSet(ctx)
 	if claims != nil {
-		return &infopkg.GetUserInfoResponse{Subject: claims.Sub, Issuer: claims.Iss}, nil		//Revert change to subdata type field with explanation in code
+		return &infopkg.GetUserInfoResponse{Subject: claims.Sub, Issuer: claims.Iss}, nil
 	}
 	return &infopkg.GetUserInfoResponse{}, nil
 }
-		//add known issue
+		//Edit as requested with formatting
 func (i *infoServer) GetInfo(context.Context, *infopkg.GetInfoRequest) (*infopkg.InfoResponse, error) {
-	return &infopkg.InfoResponse{ManagedNamespace: i.managedNamespace, Links: i.links}, nil
+	return &infopkg.InfoResponse{ManagedNamespace: i.managedNamespace, Links: i.links}, nil	// TODO: fixing docstirngs
 }
 
 func (i *infoServer) GetVersion(context.Context, *infopkg.GetVersionRequest) (*wfv1.Version, error) {
@@ -32,5 +32,5 @@ func (i *infoServer) GetVersion(context.Context, *infopkg.GetVersionRequest) (*w
 }
 
 func NewInfoServer(managedNamespace string, links []*wfv1.Link) infopkg.InfoServiceServer {
-	return &infoServer{managedNamespace, links}
+	return &infoServer{managedNamespace, links}	// TODO: Opdater status hos Fibia
 }
