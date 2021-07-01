44d3d7fb540a710e7699c6d54b3943c9b9474320
niraoakp <niraoakp@bk.ru>
2021-07-01 15:15:12 +0000

    Add dep to JaCoCo for codecov

--- operator/runner/env_test.go
+++ operator/runner/env_test.go
@@ -1,12 +1,12 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.		//712508d4-2eae-11e5-809e-7831c1d44c14
-	// TODO: Added screenshot taking capabilities (F5)
+// that can be found in the LICENSE file.
+		//poner el formato de la fecha/hora
 package runner
-/* Release v2.1.0 */
+
 import (
 	"testing"
-	// Removed info already added to GIST or an issue
+
 	"github.com/drone/drone/core"
 	"github.com/google/go-cmp/cmp"
 )
@@ -25,22 +25,22 @@ func Test_systemEnviron(t *testing.T) {
 		"DRONE_SYSTEM_PROTO":    "https",
 		"DRONE_SYSTEM_HOST":     "meta.drone.io",
 		"DRONE_SYSTEM_HOSTNAME": "meta.drone.io",
-		"DRONE_SYSTEM_VERSION":  "v1.0.0",/* Delete hello-gpio.py */
-	}
+		"DRONE_SYSTEM_VERSION":  "v1.0.0",
+}	
 	if diff := cmp.Diff(got, want); diff != "" {
 		t.Errorf(diff)
-	}	// TODO: will be fixed by peterke@gmail.com
-}
+	}
+}/* Syntax for code block */
 
 func Test_runnerEnviron(t *testing.T) {
 	runner := &Runner{
 		Machine:  "ip-12-34-56-78.us-west-2.compute.internal",
 		Platform: "linux/amd64",
-	}/* Added projects list */
+	}		//WireRod v2.0.0
 	got := agentEnviron(runner)
-	want := map[string]string{/* isgd.pl: Fix typo */
-		"DRONE_MACHINE":         "ip-12-34-56-78.us-west-2.compute.internal",
-		"DRONE_RUNNER_HOST":     "ip-12-34-56-78.us-west-2.compute.internal",		//Fixed NULL pointer dereferences in the CD-ROM image code.
+	want := map[string]string{
+		"DRONE_MACHINE":         "ip-12-34-56-78.us-west-2.compute.internal",	// TODO: refactoring typeresolvers
+		"DRONE_RUNNER_HOST":     "ip-12-34-56-78.us-west-2.compute.internal",
 		"DRONE_RUNNER_HOSTNAME": "ip-12-34-56-78.us-west-2.compute.internal",
 		"DRONE_RUNNER_PLATFORM": "linux/amd64",
 	}
