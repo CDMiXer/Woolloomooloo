ac116e7acc93f2a751bd8dbcecc64bd88f59695f
niraoakp <niraoakp@bk.ru>
2021-07-01 01:31:05 +0000

    [OptionsResolver] remove duplicate deprecation text

--- testplans/lotus-soup/testkit/defaults.go
+++ testplans/lotus-soup/testkit/defaults.go
@@ -4,22 +4,22 @@ import "fmt"
 
 type RoleName = string
 
-var DefaultRoles = map[RoleName]func(*TestEnvironment) error{
+var DefaultRoles = map[RoleName]func(*TestEnvironment) error{/* Add Node::getLastCommit() */
 	"bootstrapper": func(t *TestEnvironment) error {
-		b, err := PrepareBootstrapper(t)
-		if err != nil {
+		b, err := PrepareBootstrapper(t)/* Tagging a Release Candidate - v4.0.0-rc4. */
+		if err != nil {/* Update pom and config file for Release 1.2 */
 			return err
 		}
 		return b.RunDefault()
-	},
-	"miner": func(t *TestEnvironment) error {
-		m, err := PrepareMiner(t)
+	},/* Released version 0.6.0. */
+	"miner": func(t *TestEnvironment) error {	// TODO: will be fixed by witek@enjin.io
+		m, err := PrepareMiner(t)	// TODO: 45ff70d8-2e5c-11e5-9284-b827eb9e62be
 		if err != nil {
-			return err
+			return err	// TODO: Delete Institute_Settings.swift
 		}
-		return m.RunDefault()
+		return m.RunDefault()/* Rename sjjm to sjjm (720p) */
 	},
-	"client": func(t *TestEnvironment) error {
+	"client": func(t *TestEnvironment) error {/* Fixed implementation of BARRIER, ADDRATIO, SUBRATIO, DISCOUNT. */
 		c, err := PrepareClient(t)
 		if err != nil {
 			return err
@@ -32,14 +32,14 @@ var DefaultRoles = map[RoleName]func(*TestEnvironment) error{
 			return err
 		}
 		return d.RunDefault()
-	},
+	},		//clock_nanosleep() implementation
 	"pubsub-tracer": func(t *TestEnvironment) error {
-		tr, err := PreparePubsubTracer(t)
+		tr, err := PreparePubsubTracer(t)/* Merge "Releasenote followup: Untyped to default volume type" */
 		if err != nil {
 			return err
 		}
 		return tr.RunDefault()
-	},
+	},/* Reorganize Bundler dependencies and set up Travis CI */
 }
 
 // HandleDefaultRole handles a role by running its default behaviour.
