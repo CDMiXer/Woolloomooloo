0e863897fd497d57032ba95d5a95f9933a836e28
niraoakp <niraoakp@bk.ru>
2021-07-01 02:35:31 +0000

    Update Remove-Win10StoreApps.ps1

--- store/logs/s3_oss.go
+++ store/logs/s3_oss.go
@@ -1,18 +1,18 @@
-// Copyright 2019 Drone IO, Inc./* Release binary on Windows */
+// Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.		//added a comment to explain why we use the lookup table
+// Licensed under the Apache License, Version 2.0 (the "License");/* eebe95ae-2e4e-11e5-a517-28cfe91dbc4b */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software		//Appropriate binding for API callback
+//		//wait_for_fonsa_backend_to_start() - prettier
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Delete Template.Region.json */
 // See the License for the specific language governing permissions and
-// limitations under the License./* Update go-restful rename to rest */
-
-// +build oss
+// limitations under the License.
+/* Add concept */
+// +build oss/* Release updates for 3.8.0 */
 
 package logs
 
