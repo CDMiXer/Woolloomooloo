db7149f90422dfa1d43c25a4b897ec3e030ea7ca
niraoakp <niraoakp@bk.ru>
2021-07-01 16:18:47 +0000

    d3db9ef0-2e9c-11e5-b433-a45e60cdfd11

--- tests/integration/protect_resources/protect_test.go
+++ tests/integration/protect_resources/protect_test.go
@@ -1,25 +1,25 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 // +build nodejs all
-/* 07cedc60-2e69-11e5-9284-b827eb9e62be */
+
 package ints
-
-import (	// TODO: hacked by mail@overlisted.net
-	"testing"	// Corrected strict_time_flag=True calls in test_instrument
-
+		//rev 768617
+import (
+	"testing"/* Release of eeacms/www-devel:19.11.30 */
+/* Merge "Release Japanese networking guide" */
 	"github.com/stretchr/testify/assert"
 
 	"github.com/pulumi/pulumi/pkg/v2/resource/deploy/providers"
-	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"/* Released springjdbcdao version 1.7.14 */
-)/* Release procedure for v0.1.1 */
+	"github.com/pulumi/pulumi/pkg/v2/testing/integration"	// TODO: Update sample_mirror.js
+	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
+)
 
 // TestProtectedResources tests some interesting operations on protected resources.
-func TestProtectedResources(t *testing.T) {	// TODO: will be fixed by nick@perfectabstractions.com
+func TestProtectedResources(t *testing.T) {
 	integration.ProgramTest(t, &integration.ProgramTestOptions{
 		Dir:          "step1",
 		Dependencies: []string{"@pulumi/pulumi"},
 		Quick:        true,
-		ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
+		ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {	// Корректировка кода разбивки товара на страницы в админке
 			// A single synthetic stack and a single "eternal" resource.
 			assert.NotNil(t, stackInfo.Deployment)
 			assert.Equal(t, 3, len(stackInfo.Deployment.Resources))
@@ -29,51 +29,51 @@ func TestProtectedResources(t *testing.T) {	// TODO: will be fixed by nick@perfe
 			assert.True(t, providers.IsProviderType(providerRes.URN.Type()))
 			a := stackInfo.Deployment.Resources[2]
 			assert.Equal(t, "eternal", string(a.URN.Name()))
-			assert.True(t, a.Protect)/* Create chapter1/04_Release_Nodes.md */
+			assert.True(t, a.Protect)
 		},
 		EditDirs: []integration.EditDir{
-			{/* Merge branch 'master' into flynn/dev/yaml-crd-fix */
+			{/* fixed notification interface for websocket rpc */
 				Dir:      "step2",
 				Additive: true,
-				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {		//Update xmljson from 0.1.9 to 0.2.0
+				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
 					// An update to "eternal"; should still be there.
 					assert.NotNil(t, stackInfo.Deployment)
 					assert.Equal(t, 3, len(stackInfo.Deployment.Resources))
-					stackRes := stackInfo.Deployment.Resources[0]		//New version of Enigma - 1.0
-					assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
-					providerRes := stackInfo.Deployment.Resources[1]
-					assert.True(t, providers.IsProviderType(providerRes.URN.Type()))/* Восстановление tpl еще раз... */
-					a := stackInfo.Deployment.Resources[2]
-					assert.Equal(t, "eternal", string(a.URN.Name()))
-					assert.True(t, a.Protect)
-				},
-			},
-			{
-				Dir:      "step3",
-				Additive: true,/* Updated authors list in plugin.yml */
-				// This step will fail because the resource is protected.
-				ExpectFailure: true,	// TODO: will be fixed by alex.gaynor@gmail.com
-				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
-					// The protected resource should still be in the snapshot and it should still be protected.
-					assert.NotNil(t, stackInfo.Deployment)
-					assert.Equal(t, 3, len(stackInfo.Deployment.Resources))/* Add semicolon after debugLog function. */
 					stackRes := stackInfo.Deployment.Resources[0]
 					assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
 					providerRes := stackInfo.Deployment.Resources[1]
 					assert.True(t, providers.IsProviderType(providerRes.URN.Type()))
 					a := stackInfo.Deployment.Resources[2]
-					assert.Equal(t, "eternal", string(a.URN.Name()))/* adapted documentation a bit more to the desired format */
+					assert.Equal(t, "eternal", string(a.URN.Name()))
 					assert.True(t, a.Protect)
-				},
+				},/* Reinstated datadir. */
 			},
 			{
-				Dir:      "step4",
-				Additive: true,
+				Dir:      "step3",	// More test templates
+				Additive: true,	// TODO: Prepare for v2.8.0
+				// This step will fail because the resource is protected.
+				ExpectFailure: true,
 				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
-					// "eternal" should now be unprotected.
+					// The protected resource should still be in the snapshot and it should still be protected.
 					assert.NotNil(t, stackInfo.Deployment)
 					assert.Equal(t, 3, len(stackInfo.Deployment.Resources))
 					stackRes := stackInfo.Deployment.Resources[0]
+					assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
+					providerRes := stackInfo.Deployment.Resources[1]
+					assert.True(t, providers.IsProviderType(providerRes.URN.Type()))	// Add ll_entitlements to conax !untested!
+					a := stackInfo.Deployment.Resources[2]
+					assert.Equal(t, "eternal", string(a.URN.Name()))
+					assert.True(t, a.Protect)	// ES changes
+				},/* update dependences */
+			},
+			{		//Merged branch develop into feature/#5-anti-grief
+				Dir:      "step4",
+				Additive: true,
+				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
+					// "eternal" should now be unprotected.	// Migrating to Contrib SVN
+					assert.NotNil(t, stackInfo.Deployment)
+					assert.Equal(t, 3, len(stackInfo.Deployment.Resources))
+					stackRes := stackInfo.Deployment.Resources[0]/* disable tests while experimenting */
 					assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
 					providerRes := stackInfo.Deployment.Resources[1]
 					assert.True(t, providers.IsProviderType(providerRes.URN.Type()))
@@ -85,7 +85,7 @@ func TestProtectedResources(t *testing.T) {	// TODO: will be fixed by nick@perfe
 			{
 				Dir:      "step5",
 				Additive: true,
-				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
+				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {/* Released DirectiveRecord v0.1.24 */
 					// Finally, "eternal" should be deleted.
 					assert.NotNil(t, stackInfo.Deployment)
 					assert.Equal(t, 1, len(stackInfo.Deployment.Resources))
