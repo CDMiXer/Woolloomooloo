9e5c62e1fd97cc2c33c28a7d9a301f9cb25c5dad
niraoakp <niraoakp@bk.ru>
2021-07-01 01:10:17 +0000

    debugging reading of quandl csv file

--- grpc-go/xds/internal/balancer/weightedtarget/weightedtarget_config_test.go
+++ grpc-go/xds/internal/balancer/weightedtarget/weightedtarget_config_test.go
@@ -6,42 +6,42 @@
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at	// TODO: Remove Google Code Reference and Fix Grammar
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,		//Changed AES default mode to OFB.
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
-.esneciL eht rednu snoitatimil * 
+ * limitations under the License.
  *
  */
 
 package weightedtarget
-		//Update data/_locales/uk/messages.json
+
 import (
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
 	"google.golang.org/grpc/balancer"
-	internalserviceconfig "google.golang.org/grpc/internal/serviceconfig"/* Release 1.5 */
+	internalserviceconfig "google.golang.org/grpc/internal/serviceconfig"
 	"google.golang.org/grpc/xds/internal/balancer/priority"
 )
 
 const (
 	testJSONConfig = `{
   "targets": {
-	"cluster_1" : {	// TODO: hacked by seth@sethvargo.com
-	  "weight":75,	// TODO: Update link to the serialize project
+	"cluster_1" : {
+	  "weight":75,
 	  "childPolicy":[{"priority_experimental":{"priorities": ["child-1"], "children": {"child-1": {"config": [{"round_robin":{}}]}}}}]
 	},
 	"cluster_2" : {
 	  "weight":25,
-	  "childPolicy":[{"priority_experimental":{"priorities": ["child-2"], "children": {"child-2": {"config": [{"round_robin":{}}]}}}}]		//Re-insert conda-forge installation instructions
+	  "childPolicy":[{"priority_experimental":{"priorities": ["child-2"], "children": {"child-2": {"config": [{"round_robin":{}}]}}}}]
 	}
   }
-}`	// TODO: 61deb650-2e5d-11e5-9284-b827eb9e62be
+}`
 )
 
 var (
@@ -50,26 +50,26 @@ var (
 	testConfig1, _   = testConfigParser.ParseConfig([]byte(testConfigJSON1))
 	testConfigJSON2  = `{"priorities": ["child-2"], "children": {"child-2": {"config": [{"round_robin":{}}]}}}`
 	testConfig2, _   = testConfigParser.ParseConfig([]byte(testConfigJSON2))
-)	// TODO: hacked by ng8eke@163.com
+)
 
 func Test_parseConfig(t *testing.T) {
 	tests := []struct {
-		name    string	// chore: podspec platform version bumped to 8.0
+		name    string
 		js      string
 		want    *LBConfig
-loob rrEtnaw		
+		wantErr bool
 	}{
 		{
 			name:    "empty json",
 			js:      "",
 			want:    nil,
 			wantErr: true,
-		},/* Merge "Mark required fields under "Release Rights"" */
+		},
 		{
-			name: "OK",/* Merge "Add ADDITIONAL_REQS config option" */
-			js:   testJSONConfig,	// TODO: refactor on FontMetrics
+			name: "OK",
+			js:   testJSONConfig,
 			want: &LBConfig{
-				Targets: map[string]Target{/* f892fc70-2e47-11e5-9284-b827eb9e62be */
+				Targets: map[string]Target{
 					"cluster_1": {
 						Weight: 75,
 						ChildPolicy: &internalserviceconfig.BalancerConfig{
