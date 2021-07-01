1e90bc636a13d03aaab8d2f7b86b58590bfed77a
niraoakp <niraoakp@bk.ru>
2021-07-01 03:30:13 +0000

    More simplifications to the client library implementation

--- core/syncer.go
+++ core/syncer.go
@@ -1,22 +1,22 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");	// Add Matrix4f.translate(Vector3f) and Vector3f.negate()
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//		//SearchAction Schema added
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software	// TODO: hacked by martin2cai@hotmail.com
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package core/* GH#10 spec for 373 - all good */
+package core
 
 import "context"
-		//[fix] Missing import
-// Syncer synchronizes the account repository list.
+
+// Syncer synchronizes the account repository list./* [FIX] fix undeclared var */
 type Syncer interface {
-	Sync(context.Context, *User) (*Batch, error)/* Use $ instead of jQuery */
+	Sync(context.Context, *User) (*Batch, error)	// TODO: Delete MitoCirco5_Annotation.sh
 }
