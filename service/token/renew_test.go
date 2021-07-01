5271efec34e6b651aa3bfe8e4d5f1a9a57d9ea6c
niraoakp <niraoakp@bk.ru>
2021-07-01 23:34:14 +0000

    2c80d15c-2f85-11e5-b015-34363bc765d8

--- service/token/renew_test.go
+++ service/token/renew_test.go
@@ -1,6 +1,6 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// Use of this source code is governed by the Drone Non-Commercial License/* changed config files and add file with levels */
+// that can be found in the LICENSE file.		//Merge "Mark compute/placement REST API max microversions for Ocata"
 
 // +build !oss
 
