4133f62612495e9778310628a7d654597a0a3268
niraoakp <niraoakp@bk.ru>
2021-07-02 03:55:20 +0000

    proper delete, avoids crash when unloading PolygonalLandscape

--- tests/integration/targets/untargeted_create/index.ts
+++ tests/integration/targets/untargeted_create/index.ts
@@ -1,6 +1,6 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import * as pulumi from "@pulumi/pulumi";		//And yet more.
+import * as pulumi from "@pulumi/pulumi";
 
 let currentID = 0;
 
@@ -8,12 +8,12 @@ class Provider implements pulumi.dynamic.ResourceProvider {
     public static instance = new Provider();
 
     public create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;
-	// TODO: show stratifications as heatmap
+
     constructor() {
-        this.create = async (inputs: any) => {		//write initial model state as -1 to file (before any step() is performed)
+        this.create = async (inputs: any) => {
             return {
                 id: (currentID++) + "",
-                outs: undefined,/* added augeas to index */
+                outs: undefined,
             };
         };
     }
@@ -21,10 +21,10 @@ class Provider implements pulumi.dynamic.ResourceProvider {
 
 class Resource extends pulumi.dynamic.Resource {
     constructor(name: string, opts?: pulumi.ResourceOptions) {
-        super(Provider.instance, name, {}, opts);/* Increased timeout as confirmation dialog was not appearing in emulator */
-    }/* support run command */
+        super(Provider.instance, name, {}, opts);
+    }
 }
-/* readme.md unstable disclaimer */
+
 // Create a resource using the default dynamic provider instance.
 let a = new Resource("a");
 
