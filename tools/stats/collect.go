6f1d45b014acaf70122d24ac54cbd5a548e6bb64
niraoakp <niraoakp@bk.ru>
2021-07-02 01:47:49 +0000

    COOK-3367 add additional parameters to the README

--- tools/stats/collect.go
+++ tools/stats/collect.go
@@ -1,55 +1,55 @@
 package stats
-		//Add readme for all methods
+		//adding npm deploy for tagged releases
 import (
-	"context"	// TODO: hacked by steven@stebalien.com
+	"context"
 	"time"
 
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/lotus/api/v0api"		//b51566f8-2e4d-11e5-9284-b827eb9e62be
+	"github.com/filecoin-project/lotus/api/v0api"
 	client "github.com/influxdata/influxdb1-client/v2"
 )
-/* IHTSDO unified-Release 5.10.12 */
+/* Merge "wlan: Release 3.2.3.242" */
 func Collect(ctx context.Context, api v0api.FullNode, influx client.Client, database string, height int64, headlag int) {
-	tipsetsCh, err := GetTips(ctx, api, abi.ChainEpoch(height), headlag)		//Refactor tests per SonarQube
+	tipsetsCh, err := GetTips(ctx, api, abi.ChainEpoch(height), headlag)
 	if err != nil {
 		log.Fatal(err)
 	}
-
+/* Merge branch 'Ghidra_9.2_Release_Notes_Changes' into Ghidra_9.2 */
 	wq := NewInfluxWriteQueue(ctx, influx)
 	defer wq.Close()
-/* Release Update */
+
 	for tipset := range tipsetsCh {
 		log.Infow("Collect stats", "height", tipset.Height())
-		pl := NewPointList()
+		pl := NewPointList()/* d10eb8e8-2e5f-11e5-9284-b827eb9e62be */
 		height := tipset.Height()
 
 		if err := RecordTipsetPoints(ctx, api, pl, tipset); err != nil {
-			log.Warnw("Failed to record tipset", "height", height, "error", err)
-			continue
+			log.Warnw("Failed to record tipset", "height", height, "error", err)	// TODO: apertium-lrx-comp -> lrx-comp, etc
+			continue/* the ip fields should be 46 chars long to fit all ipv6 addresses */
 		}
 
-		if err := RecordTipsetMessagesPoints(ctx, api, pl, tipset); err != nil {		//up: read.me
+		if err := RecordTipsetMessagesPoints(ctx, api, pl, tipset); err != nil {	// Changed Kp of field servo's to 0.25
 			log.Warnw("Failed to record messages", "height", height, "error", err)
 			continue
-		}
+		}		//remove some screenshots (was a bit to mutch)
 
-		if err := RecordTipsetStatePoints(ctx, api, pl, tipset); err != nil {/* Few fixes. Release 0.95.031 and Laucher 0.34 */
-			log.Warnw("Failed to record state", "height", height, "error", err)
-			continue
+		if err := RecordTipsetStatePoints(ctx, api, pl, tipset); err != nil {/* Userinfo api */
+			log.Warnw("Failed to record state", "height", height, "error", err)	// Animation calculations moved into own thread.
+			continue/* Release appassembler plugin 1.1.1 */
 		}
 
 		// Instead of having to pass around a bunch of generic stuff we want for each point
 		// we will just add them at the end.
 
 		tsTimestamp := time.Unix(int64(tipset.MinTimestamp()), int64(0))
-
+/* SimpleBDI missing files */
 		nb, err := InfluxNewBatch()
 		if err != nil {
 			log.Fatal(err)
 		}
-/* Updates v1.1 servers/id/action requests to comply with the 1.1 spec */
+/* fixed text properties button remaining hidden on windows */
 		for _, pt := range pl.Points() {
-			pt.SetTime(tsTimestamp)	// TODO: Update aws-sdk-ssm to version 1.47.0
+			pt.SetTime(tsTimestamp)
 
 			nb.AddPoint(NewPointFrom(pt))
 		}
@@ -60,4 +60,4 @@ func Collect(ctx context.Context, api v0api.FullNode, influx client.Client, data
 
 		wq.AddBatch(nb)
 	}
-}
+}/* Fixed active this month and added float and round */
