77484f469c9eaf1d34e5faa4ab48cd13b17922a0
niraoakp <niraoakp@bk.ru>
2021-07-02 12:29:18 +0000

    b152e892-2e5c-11e5-9284-b827eb9e62be

--- plugin/webhook/config.go
+++ plugin/webhook/config.go
@@ -1,25 +1,25 @@
-// Copyright 2019 Drone IO, Inc.	// TODO: hacked by martin2cai@hotmail.com
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// TODO: I made a stupid mistake.
-// You may obtain a copy of the License at/* Release new version 2.5.9: Turn on new webRequest code for all Chrome 17 users */
-//
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//		//Drop special PV handling for minishift
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// definition of pp routine: EquidistanceProvider
+// See the License for the specific language governing permissions and/* Release: Making ready for next release iteration 6.7.2 */
 // limitations under the License.
 
-package webhook	// TODO: adapted desc to current implementation
+package webhook
 
 import "github.com/drone/drone/core"
 
-// Config provides the webhook configuration.
+// Config provides the webhook configuration./* Activate all BUILD_ options if none was specified */
 type Config struct {
 	Events   []string
 	Endpoint []string
-	Secret   string/* create main_css */
+	Secret   string
 	System   *core.System
 }
