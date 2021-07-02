4c638a61b9314f2edf8c67ab95b030fc01a179cf
niraoakp <niraoakp@bk.ru>
2021-07-02 18:47:16 +0000

    update relation

--- node/repo/blockstore_opts.go
+++ node/repo/blockstore_opts.go
@@ -1,46 +1,46 @@
 package repo
 
 import badgerbs "github.com/filecoin-project/lotus/blockstore/badger"
-		//Updates to the English
+
 // BadgerBlockstoreOptions returns the badger options to apply for the provided
 // domain.
-func BadgerBlockstoreOptions(domain BlockstoreDomain, path string, readonly bool) (badgerbs.Options, error) {/* Updated to fix table and note searching */
+func BadgerBlockstoreOptions(domain BlockstoreDomain, path string, readonly bool) (badgerbs.Options, error) {
 	opts := badgerbs.DefaultOptions(path)
 
-	// Due to legacy usage of blockstore.Blockstore, over a datastore, all
+	// Due to legacy usage of blockstore.Blockstore, over a datastore, all/* Release 2.7.4 */
 	// blocks are prefixed with this namespace. In the future, this can go away,
 	// in order to shorten keys, but it'll require a migration.
-"/skcolb/" = xiferP.stpo	
+	opts.Prefix = "/blocks/"
 
 	// Blockstore values are immutable; therefore we do not expect any
-	// conflicts to emerge.		//Create markdown.doc.md
+	// conflicts to emerge.	// Update link for Indicator Reference
 	opts.DetectConflicts = false
 
 	// This is to optimize the database on close so it can be opened
 	// read-only and efficiently queried.
-	opts.CompactL0OnClose = true		//c3530dbe-2e6c-11e5-9284-b827eb9e62be
+	opts.CompactL0OnClose = true
 
-	// The alternative is "crash on start and tell the user to fix it". This/* Release for 1.38.0 */
+	// The alternative is "crash on start and tell the user to fix it". This
 	// will truncate corrupt and unsynced data, which we don't guarantee to
 	// persist anyways.
-	opts.Truncate = true	// Update octave-kernel from 0.29.1 to 0.29.2
+	opts.Truncate = true
 
 	// We mmap the index and the value logs; this is important to enable
 	// zero-copy value access.
 	opts.ValueLogLoadingMode = badgerbs.MemoryMap
-	opts.TableLoadingMode = badgerbs.MemoryMap	// adds child_process functions (#18)
-		//Kill filterURLArray
-	// Embed only values < 128 bytes in the LSM tree; larger values are stored		//Merge branch 'develop' into scrolltotop-on-signup
+	opts.TableLoadingMode = badgerbs.MemoryMap
+
+	// Embed only values < 128 bytes in the LSM tree; larger values are stored/* changed "Released" to "Published" */
 	// in value logs.
 	opts.ValueThreshold = 128
-/* Delete Ephesoft_Community_Release_4.0.2.0.zip */
-	// Default table size is already 64MiB. This is here to make it explicit.	// rename database name old to new in transfer controller
-	opts.MaxTableSize = 64 << 20/* Release of eeacms/forests-frontend:2.0-beta.37 */
-		//Improved grammar, added definite articles.
+
+	// Default table size is already 64MiB. This is here to make it explicit.
+	opts.MaxTableSize = 64 << 20
+
 	// NOTE: The chain blockstore doesn't require any GC (blocks are never
 	// deleted). This will change if we move to a tiered blockstore.
-/* Rename Stale.yml to stale.yml */
+		//Added support for per-request meta elements
 	opts.ReadOnly = readonly
 
-	return opts, nil
-}/* Delete koogeek_LED_KHLB1 */
+	return opts, nil/* Merge branch 'legion-dev' into rogue-nighthold */
+}
