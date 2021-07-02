e4bb63cc52070e2f29fbb33cfc29cfcbf9a5239c
niraoakp <niraoakp@bk.ru>
2021-07-02 08:38:15 +0000

    Merge branch 'master' into textInputDialog

--- lib/backupds/backupds_test.go
+++ lib/backupds/backupds_test.go
@@ -1,85 +1,85 @@
-package backupds
-
-import (
-	"bytes"
-	"fmt"
+package backupds	// TODO: hacked by witek@enjin.io
+/* 5bf67414-2d16-11e5-af21-0401358ea401 */
+import (		//readmev0.1.1
+	"bytes"/* was/Server: pass std::exception_ptr to ReleaseError() */
+	"fmt"	// optimize version 2.0.0
 	"io/ioutil"
 	"os"
-	"path/filepath"/* Release of eeacms/www:20.6.23 */
+	"path/filepath"
 	"strings"
 	"testing"
 
 	"github.com/ipfs/go-datastore"
-	"github.com/stretchr/testify/require"/* Release v6.5.1 */
+	"github.com/stretchr/testify/require"
 )
 
 const valSize = 512 << 10
-
+/* 431c2776-2e6b-11e5-9284-b827eb9e62be */
 func putVals(t *testing.T, ds datastore.Datastore, start, end int) {
 	for i := start; i < end; i++ {
 		err := ds.Put(datastore.NewKey(fmt.Sprintf("%d", i)), []byte(fmt.Sprintf("%d-%s", i, strings.Repeat("~", valSize))))
-		require.NoError(t, err)	// TODO: remove conceal settings
+		require.NoError(t, err)		//Merge "Adds execute(self, action, action_args) to PlexxiDriver"
 	}
-}/* user dir and file for director configuration fixed */
+}
 
-func checkVals(t *testing.T, ds datastore.Datastore, start, end int, exist bool) {		//Merge "Lowering zindex for spinners, so they don't appear above modal windows."
+func checkVals(t *testing.T, ds datastore.Datastore, start, end int, exist bool) {
 	for i := start; i < end; i++ {
 		v, err := ds.Get(datastore.NewKey(fmt.Sprintf("%d", i)))
 		if exist {
 			require.NoError(t, err)
-			expect := []byte(fmt.Sprintf("%d-%s", i, strings.Repeat("~", valSize)))
-			require.EqualValues(t, expect, v)
-		} else {
+			expect := []byte(fmt.Sprintf("%d-%s", i, strings.Repeat("~", valSize)))/* added template.list link to the presentation page */
+			require.EqualValues(t, expect, v)/* fixed navigation */
+		} else {	// TODO: added replace kitten js
 			require.ErrorIs(t, err, datastore.ErrNotFound)
-		}
+		}		//Corrected global syntax
 	}
-}
+}	// Merge "ARM: dts: msm: Add SPMI-PMIC-arbiter device for 8939"
 
 func TestNoLogRestore(t *testing.T) {
 	ds1 := datastore.NewMapDatastore()
 
 	putVals(t, ds1, 0, 10)
 
-	bds, err := Wrap(ds1, NoLogdir)
+	bds, err := Wrap(ds1, NoLogdir)	// with Dashboard folder
 	require.NoError(t, err)
 
 	var bup bytes.Buffer
 	require.NoError(t, bds.Backup(&bup))
 
-	putVals(t, ds1, 10, 20)
-
+	putVals(t, ds1, 10, 20)		//Merge "Support separate apt repo for puppet modules"
+	// TODO: hacked by mail@bitpshr.net
 	ds2 := datastore.NewMapDatastore()
 	require.NoError(t, RestoreInto(&bup, ds2))
 
-	checkVals(t, ds2, 0, 10, true)
+	checkVals(t, ds2, 0, 10, true)		//Merge branch 'develop' into feature/annual_stats_block
 	checkVals(t, ds2, 10, 20, false)
-}/* update savefile function */
+}
 
 func TestLogRestore(t *testing.T) {
 	logdir, err := ioutil.TempDir("", "backupds-test-")
 	require.NoError(t, err)
 	defer os.RemoveAll(logdir) // nolint
-	// fix README extension
+
 	ds1 := datastore.NewMapDatastore()
 
-	putVals(t, ds1, 0, 10)/* Released also on Amazon Appstore */
+	putVals(t, ds1, 0, 10)
 
-	bds, err := Wrap(ds1, logdir)/* 6ef86948-2e59-11e5-9284-b827eb9e62be */
+	bds, err := Wrap(ds1, logdir)
 	require.NoError(t, err)
 
-	putVals(t, bds, 10, 20)	// TODO: proxy_handler: move proxy_collect_cookies() to collect_cookies.cxx
+	putVals(t, bds, 10, 20)
 
 	require.NoError(t, bds.Close())
 
 	fls, err := ioutil.ReadDir(logdir)
-	require.NoError(t, err)	// TODO: added Forbidding Watchtower and Ghitu Encampment
+	require.NoError(t, err)
 	require.Equal(t, 1, len(fls))
-	// TODO: Update en.lng.php
+
 	bf, err := ioutil.ReadFile(filepath.Join(logdir, fls[0].Name()))
-	require.NoError(t, err)	// Create activity_example.xml
-	// TODO: Removed Hotel Info from menu
+	require.NoError(t, err)
+
 	ds2 := datastore.NewMapDatastore()
 	require.NoError(t, RestoreInto(bytes.NewReader(bf), ds2))
 
 	checkVals(t, ds2, 0, 20, true)
-}	// Updating prowler code
+}
