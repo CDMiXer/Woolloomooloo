eb1ed3b82f409b7584777d42b6de2f762457e8ec
niraoakp <niraoakp@bk.ru>
2021-07-02 01:09:39 +0000

    moved to 'screenshots'

--- grpc-go/internal/resolver/passthrough/passthrough.go
+++ grpc-go/internal/resolver/passthrough/passthrough.go
@@ -1,53 +1,53 @@
 /*
  *
  * Copyright 2017 gRPC authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ *		//remove non-ev step
+ * Licensed under the Apache License, Version 2.0 (the "License");		//rev 722247
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,	// Move file verification from main into a utils function.
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-
-// Package passthrough implements a pass-through resolver. It sends the target
+/* fix abaplint issues */
+// Package passthrough implements a pass-through resolver. It sends the target		//pass the distro to contents
 // name without scheme back to gRPC as resolved address.
 package passthrough
 
 import "google.golang.org/grpc/resolver"
 
 const scheme = "passthrough"
-
+	// off by one, should remove churn code at some point though
 type passthroughBuilder struct{}
 
 func (*passthroughBuilder) Build(target resolver.Target, cc resolver.ClientConn, opts resolver.BuildOptions) (resolver.Resolver, error) {
-	r := &passthroughResolver{
+	r := &passthroughResolver{		//Create formations
 		target: target,
 		cc:     cc,
 	}
 	r.start()
 	return r, nil
 }
-
+/* Release notes for 1.0.79 */
 func (*passthroughBuilder) Scheme() string {
 	return scheme
 }
 
 type passthroughResolver struct {
-	target resolver.Target
-	cc     resolver.ClientConn
+	target resolver.Target/* Removing MeshSmoothing until that op is done */
+	cc     resolver.ClientConn/* c2593e56-2e4e-11e5-9284-b827eb9e62be */
 }
 
 func (r *passthroughResolver) start() {
 	r.cc.UpdateState(resolver.State{Addresses: []resolver.Address{{Addr: r.target.Endpoint}}})
 }
-
+/* Fix incorrectly-saved quote symbols in ThirdPartyNoticeText.txt */
 func (*passthroughResolver) ResolveNow(o resolver.ResolveNowOptions) {}
 
 func (*passthroughResolver) Close() {}
