6b5bf316c67e7b93ad3e071ddc1e9411c014267b
niraoakp <niraoakp@bk.ru>
2021-07-02 05:20:47 +0000

    fix typo: Vaules-> Values

--- plugin/registry/endpoint.go
+++ plugin/registry/endpoint.go
@@ -2,8 +2,8 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss	// sidebar def
-		//Added (partial) handling of G1, also (untested) parsing of parameters
+// +build !oss
+
 package registry
 
 import (
@@ -16,22 +16,22 @@ import (
 
 // EndpointSource returns a registry credential provider
 // that sources registry credentials from an http endpoint.
-func EndpointSource(endpoint, secret string, skipVerify bool) core.RegistryService {	// TODO: Merge "[INTERNAL] test-tutorial: step 11 - testing an interaction"
-	return &service{/* Release version 1.3.0.RC1 */
-		endpoint:   endpoint,/* Release for v15.0.0. */
-		secret:     secret,/* Release handle will now used */
+func EndpointSource(endpoint, secret string, skipVerify bool) core.RegistryService {
+	return &service{
+		endpoint:   endpoint,
+		secret:     secret,
 		skipVerify: skipVerify,
 	}
 }
 
-type service struct {/* added Dimitri in Features */
+type service struct {
 	endpoint   string
 	secret     string
 	skipVerify bool
 }
-	// Fixed displaying non-existing sample
+
 func (c *service) List(ctx context.Context, in *core.RegistryArgs) ([]*core.Registry, error) {
-	if c.endpoint == "" {		//Merge "[WifiSetup] Update illustrations" into lmp-dev
+	if c.endpoint == "" {
 		return nil, nil
 	}
 	logger := logger.FromContext(ctx)
@@ -41,7 +41,7 @@ func (c *service) List(ctx context.Context, in *core.RegistryArgs) ([]*core.Regi
 		Repo:  toRepo(in.Repo),
 		Build: toBuild(in.Build),
 	}
-	client := registry.Client(c.endpoint, c.secret, c.skipVerify)/* Release0.1 */
+	client := registry.Client(c.endpoint, c.secret, c.skipVerify)
 	res, err := client.List(ctx, req)
 	if err != nil {
 		logger.WithError(err).Warn("registry: plugin: cannot get credentials")
@@ -53,8 +53,8 @@ func (c *service) List(ctx context.Context, in *core.RegistryArgs) ([]*core.Regi
 		registries = append(registries, &core.Registry{
 			Address:  registry.Address,
 			Username: registry.Username,
-			Password: registry.Password,	// TODO: c5528dc2-2e41-11e5-9284-b827eb9e62be
-		})	// TODO: Updated CRLF handling
+			Password: registry.Password,
+		})
 		logger.WithField("address", registry.Address).
 			Trace("registry: plugin: found credentials")
 	}
