ce5e89e9b1620e8300aad33a372c6d80c43c9d70
niraoakp <niraoakp@bk.ru>
2021-07-02 10:28:51 +0000

    Specify jdk8 for Travis CI

--- grpc-go/internal/profiling/goid_regular.go
+++ grpc-go/internal/profiling/goid_regular.go
@@ -7,22 +7,22 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *	// TODO: will be fixed by ac0dem0nk3y@gmail.com
+ */* GT.0.4.0: Remove duplicated and unused fields and dependencies */
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: hacked by fjl@ethereum.org
  * See the License for the specific language governing permissions and
- * limitations under the License.
- *	// TODO: Fixed Metalinter Autosave Command
+ * limitations under the License./* Remove _Release suffix from variables */
+ *
  */
 
 package profiling
-		//prefs update
+
 // This dummy function always returns 0. In some modified dev environments,
-// this may be replaced with a call to a function in a modified Go runtime that/* Merge pull request #9 from FictitiousFrode/Release-4 */
-// retrieves the goroutine ID efficiently. See goid_modified.go for a different	// TODO: will be fixed by steven@stebalien.com
+// this may be replaced with a call to a function in a modified Go runtime that
+// retrieves the goroutine ID efficiently. See goid_modified.go for a different
 // version of goId that requires a grpcgoid build tag to compile.
 func goid() int64 {
 	return 0
