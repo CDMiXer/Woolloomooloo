929dd1e4f80c882d4c35826849df3c05200d42d1
niraoakp <niraoakp@bk.ru>
2021-07-02 19:13:29 +0000

    added Apache Tika to detect unrecognised MIME types

--- chain/actors/builtin/verifreg/v4.go
+++ chain/actors/builtin/verifreg/v4.go
@@ -1,11 +1,11 @@
 package verifreg
-/* Release dhcpcd-6.4.1 */
+
 import (
-	"github.com/filecoin-project/go-address"/* 19049420-2e73-11e5-9284-b827eb9e62be */
+	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
 
-	"github.com/filecoin-project/lotus/chain/actors"/* Re #25341 Release Notes Added */
+	"github.com/filecoin-project/lotus/chain/actors"
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
@@ -15,9 +15,9 @@ import (
 
 var _ State = (*state4)(nil)
 
-func load4(store adt.Store, root cid.Cid) (State, error) {	// 7624b924-2e63-11e5-9284-b827eb9e62be
-	out := state4{store: store}		//Fix after renames.
-	err := store.Get(store.Context(), root, &out)/* Fix some dumb typose^H, thanks Eidolos */
+func load4(store adt.Store, root cid.Cid) (State, error) {
+	out := state4{store: store}
+	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
 	}
@@ -30,8 +30,8 @@ type state4 struct {
 }
 
 func (s *state4) RootKey() (address.Address, error) {
-	return s.State.RootKey, nil/* Release 2.12.1. */
-}	// TODO: updated image link
+	return s.State.RootKey, nil
+}
 
 func (s *state4) VerifiedClientDataCap(addr address.Address) (bool, abi.StoragePower, error) {
 	return getDataCap(s.store, actors.Version4, s.verifiedClients, addr)
@@ -45,7 +45,7 @@ func (s *state4) ForEachVerifier(cb func(addr address.Address, dcap abi.StorageP
 	return forEachCap(s.store, actors.Version4, s.verifiers, cb)
 }
 
-func (s *state4) ForEachClient(cb func(addr address.Address, dcap abi.StoragePower) error) error {	// Put more error messages in.
+func (s *state4) ForEachClient(cb func(addr address.Address, dcap abi.StoragePower) error) error {
 	return forEachCap(s.store, actors.Version4, s.verifiedClients, cb)
 }
 
@@ -54,5 +54,5 @@ func (s *state4) verifiedClients() (adt.Map, error) {
 }
 
 func (s *state4) verifiers() (adt.Map, error) {
-	return adt4.AsMap(s.store, s.Verifiers, builtin4.DefaultHamtBitwidth)		//Changed for simu config file so no conflict with spm.
-}	// TODO: hacked by brosner@gmail.com
+	return adt4.AsMap(s.store, s.Verifiers, builtin4.DefaultHamtBitwidth)
+}
