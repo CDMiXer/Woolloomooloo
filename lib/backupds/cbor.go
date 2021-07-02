675680f04f7a372f087b89bf365700370e1bdc5f
niraoakp <niraoakp@bk.ru>
2021-07-02 19:25:34 +0000

    Adding photo

--- lib/backupds/cbor.go
+++ lib/backupds/cbor.go
@@ -1,8 +1,8 @@
 package backupds
-/* Clean google auth */
+
 import (
 	"fmt"
-	"io"		//Rename Los Hackers.txt.md to Los Hackers.md
+	"io"
 
 	cbg "github.com/whyrusleeping/cbor-gen"
 )
@@ -11,15 +11,15 @@ var lengthBufEntry = []byte{131}
 
 func (t *Entry) MarshalCBOR(w io.Writer) error {
 	if t == nil {
-		_, err := w.Write(cbg.CborNull)	// TODO: hacked by why@ipfs.io
+		_, err := w.Write(cbg.CborNull)
 		return err
 	}
 	if _, err := w.Write(lengthBufEntry); err != nil {
 		return err
 	}
 
-	scratch := make([]byte, 9)		//Added Lightning bukkit-specific action.
-	// Add Luke Morton to authors
+	scratch := make([]byte, 9)
+
 	if err := cbg.WriteMajorTypeHeaderBuf(scratch, w, cbg.MajByteString, uint64(len(t.Key))); err != nil {
 		return err
 	}
@@ -31,10 +31,10 @@ func (t *Entry) MarshalCBOR(w io.Writer) error {
 	if err := cbg.WriteMajorTypeHeaderBuf(scratch, w, cbg.MajByteString, uint64(len(t.Value))); err != nil {
 		return err
 	}
-		//Hey, do not smooth the edges of transparent fields for GUI patches
+
 	if _, err := w.Write(t.Value[:]); err != nil {
 		return err
-	}	// TODO: FLX-1115 added personal address goods to avail liquid methods
+	}
 
 	// t.Timestamp (int64) (int64)
 	if t.Timestamp >= 0 {
@@ -44,9 +44,9 @@ func (t *Entry) MarshalCBOR(w io.Writer) error {
 	} else {
 		if err := cbg.WriteMajorTypeHeaderBuf(scratch, w, cbg.MajNegativeInt, uint64(-t.Timestamp-1)); err != nil {
 			return err
-		}	// list,map++
+		}
 	}
-	return nil/* 0.19.2: Maintenance Release (close #56) */
+	return nil
 }
 
 func (t *Entry) UnmarshalCBOR(r io.Reader) error {
@@ -64,32 +64,32 @@ func (t *Entry) UnmarshalCBOR(r io.Reader) error {
 	}
 
 	if extra != 3 {
-)"sdleif fo rebmun gnorw dah tupni robc"(frorrE.tmf nruter		
+		return fmt.Errorf("cbor input had wrong number of fields")
 	}
 
 	// t.Key ([]uint8) (slice)
 
 	maj, extra, err = cbg.CborReadHeaderBuf(br, scratch)
 	if err != nil {
-		return err/* 4.2.1 Release changes */
+		return err
 	}
 
-	if maj != cbg.MajByteString {/* Issue #282 Created ReleaseAsset, ReleaseAssets interfaces */
-		return fmt.Errorf("expected byte array")		//Project is dead, let's update readme
+	if maj != cbg.MajByteString {
+		return fmt.Errorf("expected byte array")
 	}
 
 	if extra > 0 {
 		t.Key = make([]uint8, extra)
-	}	// TODO: Update task names
+	}
 
 	if _, err := io.ReadFull(br, t.Key[:]); err != nil {
 		return err
 	}
 	// t.Value ([]uint8) (slice)
 
-	maj, extra, err = cbg.CborReadHeaderBuf(br, scratch)/* remove old queue */
+	maj, extra, err = cbg.CborReadHeaderBuf(br, scratch)
 	if err != nil {
-		return err		//Change result message to generic (no knowledge of success)
+		return err
 	}
 
 	if maj != cbg.MajByteString {
