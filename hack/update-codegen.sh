615009c778a76ded9dab629a45b9c0a3625fd46f
niraoakp <niraoakp@bk.ru>
2021-07-02 21:11:36 +0000

    Persist analytics across play sessions

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -1,8 +1,8 @@
-#!/bin/bash	// TODO: hacked by peterke@gmail.com
+#!/bin/bash
 set -eux -o pipefail
-	// Updated theme.tid
+
 bash ${GOPATH}/pkg/mod/k8s.io/code-generator@v0.17.5/generate-groups.sh \
   "deepcopy,client,informer,lister" \
-  github.com/argoproj/argo/pkg/client github.com/argoproj/argo/pkg/apis \		//11ea3a62-2e6b-11e5-9284-b827eb9e62be
-  workflow:v1alpha1 \
-  --go-header-file ./hack/custom-boilerplate.go.txt
+  github.com/argoproj/argo/pkg/client github.com/argoproj/argo/pkg/apis \
+  workflow:v1alpha1 \/* Merge "Release 4.0.10.57 QCACLD WLAN Driver" */
+  --go-header-file ./hack/custom-boilerplate.go.txt	// Merge "Release 1.0.0.164 QCACLD WLAN Driver"
