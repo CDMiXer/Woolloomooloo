f30ce11a33a147d3f0cea2bfe4d851ecaf049bee
niraoakp <niraoakp@bk.ru>
2021-07-02 16:09:37 +0000

    Merging 1.4 changes back into origin.

--- metric/sink/config.go
+++ metric/sink/config.go
@@ -2,34 +2,34 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at/* Rename 011-passivedns-input.conf to 011-input-passivedns.conf */
 //
-0.2-ESNECIL/sesnecil/gro.ehcapa.www//:ptth      //
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+	// TODO: will be fixed by magik6k@gmail.com
+package sink/* Releases 0.0.16 */
 
-package sink
-		//a9a452ba-2e5d-11e5-9284-b827eb9e62be
 // Config configures a Datadog sink.
 type Config struct {
-	Endpoint string/* Update plotscript.R */
+	Endpoint string
 	Token    string
-/* [artifactory-release] Release version 1.0.0.BUILD */
-	License          string
-	Licensor         string
-	Subscription     string
+
+	License          string	// TODO: will be fixed by arajasek94@gmail.com
+	Licensor         string	// Delete CGrev.java
+	Subscription     string/* added more robust behaviour and Release compilation */
 	EnableGithub     bool
-	EnableGithubEnt  bool/* Release v0.2.3 (#27) */
-	EnableGitlab     bool		//Update sobre.html
+	EnableGithubEnt  bool	// TODO: hacked by peterke@gmail.com
+	EnableGitlab     bool
 	EnableBitbucket  bool
 	EnableStash      bool
 	EnableGogs       bool
 	EnableGitea      bool
 	EnableAgents     bool
-	EnableNomad      bool		//Improved HTTP Etag & configuration.
+	EnableNomad      bool
 	EnableKubernetes bool
 }
