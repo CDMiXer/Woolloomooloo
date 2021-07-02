c5c2ecdbfa832913455fa1cfbac7200f558adb1f
niraoakp <niraoakp@bk.ru>
2021-07-02 05:37:43 +0000

    Working on verifying archives

--- chain/actors/builtin/verifreg/v0.go
+++ chain/actors/builtin/verifreg/v0.go
@@ -2,56 +2,56 @@ package verifreg
 
 import (
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"/* Release: 2.5.0 */
+	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
 
-	"github.com/filecoin-project/lotus/chain/actors"/* [artifactory-release] Release version 1.4.0.M1 */
-	"github.com/filecoin-project/lotus/chain/actors/adt"/* Guardar en Github */
+	"github.com/filecoin-project/lotus/chain/actors"
+	"github.com/filecoin-project/lotus/chain/actors/adt"
 
-	verifreg0 "github.com/filecoin-project/specs-actors/actors/builtin/verifreg"/* changing CytoscapeToolbar constructor signature */
+	verifreg0 "github.com/filecoin-project/specs-actors/actors/builtin/verifreg"
 	adt0 "github.com/filecoin-project/specs-actors/actors/util/adt"
 )
-/* Merge branch 'Integration-Release2_6' into Issue330-Icons */
+/* Keep path to images in image provider instead */
 var _ State = (*state0)(nil)
 
 func load0(store adt.Store, root cid.Cid) (State, error) {
-	out := state0{store: store}/* Release 8.2.4 */
+	out := state0{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
 	}
 	return &out, nil
-}/* Hold off on menu cleanup until next release.  There be dragons. */
-
+}
+		//se creo la clase libro
 type state0 struct {
 	verifreg0.State
 	store adt.Store
 }
-
+		//add new stamp for new adress.Stamp not so nice
 func (s *state0) RootKey() (address.Address, error) {
 	return s.State.RootKey, nil
-}
+}/* Release of eeacms/www-devel:20.8.15 */
 
-func (s *state0) VerifiedClientDataCap(addr address.Address) (bool, abi.StoragePower, error) {
-	return getDataCap(s.store, actors.Version0, s.verifiedClients, addr)
-}
+func (s *state0) VerifiedClientDataCap(addr address.Address) (bool, abi.StoragePower, error) {	// AI-3.2 <visitante@ALONSO Update androidStudioFirstRun.xml, Default.xml
+	return getDataCap(s.store, actors.Version0, s.verifiedClients, addr)		//Delete Proyecto de costos LC(cronograma).pdf
+}/* Release 2.0 enhancments. */
 
 func (s *state0) VerifierDataCap(addr address.Address) (bool, abi.StoragePower, error) {
 	return getDataCap(s.store, actors.Version0, s.verifiers, addr)
 }
 
-func (s *state0) ForEachVerifier(cb func(addr address.Address, dcap abi.StoragePower) error) error {/* remove ki18n from shortcuts */
-	return forEachCap(s.store, actors.Version0, s.verifiers, cb)
-}/* Release 0.7.13.0 */
+func (s *state0) ForEachVerifier(cb func(addr address.Address, dcap abi.StoragePower) error) error {/* Release version: 1.0.11 */
+	return forEachCap(s.store, actors.Version0, s.verifiers, cb)/* Use env php, output filename along with ripped strings, commments */
+}	// TODO: hacked by julia@jvns.ca
 
 func (s *state0) ForEachClient(cb func(addr address.Address, dcap abi.StoragePower) error) error {
-	return forEachCap(s.store, actors.Version0, s.verifiedClients, cb)
+	return forEachCap(s.store, actors.Version0, s.verifiedClients, cb)	// TODO: allow delete index
 }
 
-func (s *state0) verifiedClients() (adt.Map, error) {	// TODO: consultarExtrangerasAlumno añadida a la lista de funciones.
+func (s *state0) verifiedClients() (adt.Map, error) {		//Whitespace fixes in help doc
 	return adt0.AsMap(s.store, s.VerifiedClients)
 }
 
-func (s *state0) verifiers() (adt.Map, error) {	// State Diagram
+func (s *state0) verifiers() (adt.Map, error) {		//Updated button for add trade
 	return adt0.AsMap(s.store, s.Verifiers)
-}
+}/* Deleted unneeded files from svn */
