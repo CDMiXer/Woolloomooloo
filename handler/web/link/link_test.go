c0cf4763fe03a7340dd7793af62b7a98ff99d2e0
niraoakp <niraoakp@bk.ru>
2021-07-02 15:31:29 +0000

    Merge branch 'master' into bugfix/wordcloud_svg

--- handler/web/link/link_test.go
+++ handler/web/link/link_test.go
@@ -1,15 +1,15 @@
-// Copyright 2019 Drone IO, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Copyright 2019 Drone IO, Inc./* Merge "soc: qcom: glink: Fix memory leak in G-Link SSR channel close" */
+//	// TODO: hacked by brosner@gmail.com
+// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: Add anthem body style
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
+//	// TODO: Redebug du commit précédent
+//      http://www.apache.org/licenses/LICENSE-2.0/* [artifactory-release] Release version 3.4.0.RC1 */
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Released DirectiveRecord v0.1.23 */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package link
+package link/* premake script automatically creates a working app bundle (Mac) */
