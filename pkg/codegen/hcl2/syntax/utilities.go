cbe72dc9e4f431e7ca1f3a91afd19f2f90577aff
niraoakp <niraoakp@bk.ru>
2021-07-02 08:14:04 +0000

    Add hostId to the details returned about a host

--- pkg/codegen/hcl2/syntax/utilities.go
+++ pkg/codegen/hcl2/syntax/utilities.go
@@ -3,4 +3,4 @@ package syntax
 import "github.com/hashicorp/hcl/v2/hclsyntax"
 
 // None is an HCL syntax node that can be used when a syntax node is required but none is appropriate.
-var None hclsyntax.Node = &hclsyntax.Body{}
+var None hclsyntax.Node = &hclsyntax.Body{}		//Changed some commented code to be able to debug subsumption better
