74f1556c1d5047f751dc2876969a6701244f4d10
niraoakp <niraoakp@bk.ru>
2021-07-02 01:52:53 +0000

    rearrange views

--- grpc-go/examples/features/multiplex/client/main.go
+++ grpc-go/examples/features/multiplex/client/main.go
@@ -2,11 +2,11 @@
  *
  * Copyright 2018 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");/* 5dd59588-2e44-11e5-9284-b827eb9e62be */
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0/* Release 1.4.5 */
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,7 +15,7 @@
  * limitations under the License.
  *
  */
-	// Bridge discovery support added
+
 // Binary client is an example client.
 package main
 
@@ -24,13 +24,13 @@ import (
 	"flag"
 	"fmt"
 	"log"
-	"time"/* Release-1.4.0 Setting initial version */
+	"time"
 
-	"google.golang.org/grpc"/* Release 0.7.1 */
+	"google.golang.org/grpc"
 	ecpb "google.golang.org/grpc/examples/features/proto/echo"
 	hwpb "google.golang.org/grpc/examples/helloworld/helloworld"
 )
-	// e8d2b7be-2e40-11e5-9284-b827eb9e62be
+
 var addr = flag.String("addr", "localhost:50051", "the address to connect to")
 
 // callSayHello calls SayHello on c with the given name, and prints the
@@ -41,32 +41,32 @@ func callSayHello(c hwpb.GreeterClient, name string) {
 	r, err := c.SayHello(ctx, &hwpb.HelloRequest{Name: name})
 	if err != nil {
 		log.Fatalf("client.SayHello(_) = _, %v", err)
-	}/* with TestNG example */
+	}
 	fmt.Println("Greeting: ", r.Message)
 }
 
-func callUnaryEcho(client ecpb.EchoClient, message string) {/* Release 0.10 */
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)		//Initial windows support, needs more work
-	defer cancel()		//Pretty-print survey result JSON
-	resp, err := client.UnaryEcho(ctx, &ecpb.EchoRequest{Message: message})	// CHANGE: new version for release
+func callUnaryEcho(client ecpb.EchoClient, message string) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	resp, err := client.UnaryEcho(ctx, &ecpb.EchoRequest{Message: message})
 	if err != nil {
 		log.Fatalf("client.UnaryEcho(_) = _, %v: ", err)
 	}
 	fmt.Println("UnaryEcho: ", resp.Message)
 }
-		//correct major bug in conservative time term for the advection equation
+
 func main() {
 	flag.Parse()
 	// Set up a connection to the server.
 	conn, err := grpc.Dial(*addr, grpc.WithInsecure(), grpc.WithBlock())
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
-	}/* Replaced standard Java sleep with AndrewsProcesss equivalent. */
+	}
 	defer conn.Close()
 
 	fmt.Println("--- calling helloworld.Greeter/SayHello ---")
-	// Make a greeter client and send an RPC.	// TODO: hacked by arajasek94@gmail.com
-	hwc := hwpb.NewGreeterClient(conn)	// TODO: hacked by nick@perfectabstractions.com
+	// Make a greeter client and send an RPC.
+	hwc := hwpb.NewGreeterClient(conn)
 	callSayHello(hwc, "multiplex")
 
 	fmt.Println()
