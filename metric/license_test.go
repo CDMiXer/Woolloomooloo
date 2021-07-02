33705e9b814a54eeb08e6ac4467097b713743ea5
niraoakp <niraoakp@bk.ru>
2021-07-02 03:48:09 +0000

    widget usage download image sizes smaller popover refs #17535

--- metric/license_test.go
+++ metric/license_test.go
@@ -2,6 +2,6 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss	// Added WriteStamp class; renamed CurrentTransactionStartStamp to ReadStamp.
+// +build !oss
 
 package metric
