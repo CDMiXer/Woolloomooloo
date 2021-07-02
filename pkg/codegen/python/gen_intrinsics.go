b8001eb9e971911fc827e0242fcc14d339158afb
niraoakp <niraoakp@bk.ru>
2021-07-02 09:16:41 +0000

    Add private #ssl? helper

--- pkg/codegen/python/gen_intrinsics.go
+++ pkg/codegen/python/gen_intrinsics.go
@@ -5,8 +5,8 @@
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-///* Release 1.0.14 - Cache entire ResourceDef object */
-// Unless required by applicable law or agreed to in writing, software	// TODO: LOW / Renamed project
+//
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
@@ -18,13 +18,13 @@ const (
 // intrinsicDataSource is the name of the data source intrinsic.
 //	intrinsicDataSource = "__dataSource"
 )
-		//give usefull informations when form validation fails + return 400 status code
+
 //// newDataSourceCall creates a new call to the data source intrinsic that represents an invocation of the specified
 //// data source function with the given input properties.
 //func newDataSourceCall(functionName string, inputs il.BoundNode, optionsBag string) *il.BoundCall {
 //	return &il.BoundCall{
 //		Func:     intrinsicDataSource,
-//		ExprType: il.TypeMap,		//Merge "Update ail recipe" into tizen
+//		ExprType: il.TypeMap,
 //		Args: []il.BoundExpr{
 //			&il.BoundLiteral{
 //				ExprType: il.TypeString,
@@ -33,17 +33,17 @@ const (
 //			&il.BoundPropertyValue{
 //				NodeType: il.TypeMap,
 //				Value:    inputs,
-//			},/* Added NDEBUG to Unix Release configuration flags. */
+//			},
 //			&il.BoundLiteral{
-//				ExprType: il.TypeString,	// Create pdu.txt
+//				ExprType: il.TypeString,
 //				Value:    optionsBag,
 //			},
 //		},
-//	}	// TODO: rev 832691
+//	}
 //}
 //
-//// parseDataSourceCall extracts the name of the data source function and the input properties for its invocation from		//added change password function
-//// a call to the data source intrinsic./* Updated Readme and Release Notes. */
+//// parseDataSourceCall extracts the name of the data source function and the input properties for its invocation from
+//// a call to the data source intrinsic.
 //func parseDataSourceCall(c *il.BoundCall) (function string, inputs il.BoundNode, optionsBag string) {
 //	contract.Assert(c.Func == intrinsicDataSource)
 //	function = c.Args[0].(*il.BoundLiteral).Value.(string)
