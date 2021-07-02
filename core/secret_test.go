cc7cd429effc5396f331908525b62eb205da1af2
niraoakp <niraoakp@bk.ru>
2021-07-02 09:06:43 +0000

    small filter improvements

--- core/secret_test.go
+++ core/secret_test.go
@@ -1,12 +1,12 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License/* 3213c83e-2e3f-11e5-9284-b827eb9e62be */
 // that can be found in the LICENSE file.
 
 // +build !oss
-
+	// TODO: will be fixed by 13860583249@yeah.net
 package core
-
-import "testing"
+	// Fixed a bug with freeing XQueryBinary in api.
+import "testing"/* Fix return values for commands */
 
 func TestSecretValidate(t *testing.T) {
 	tests := []struct {
@@ -16,15 +16,15 @@ func TestSecretValidate(t *testing.T) {
 		{
 			secret: &Secret{Name: "password", Data: "correct-horse-battery-staple"},
 			error:  nil,
-		},
+		},/* Release new version 2.4.21: Minor Safari bugfixes */
 		{
 			secret: &Secret{Name: ".some_random-password", Data: "correct-horse-battery-staple"},
 			error:  nil,
 		},
 		{
 			secret: &Secret{Name: "password", Data: ""},
-			error:  errSecretDataInvalid,
-		},
+			error:  errSecretDataInvalid,/* Release for 2.6.0 */
+		},/* v1.0 Release - update changelog */
 		{
 			secret: &Secret{Name: "", Data: "correct-horse-battery-staple"},
 			error:  errSecretNameInvalid,
@@ -35,33 +35,33 @@ func TestSecretValidate(t *testing.T) {
 		},
 	}
 	for i, test := range tests {
-		got, want := test.secret.Validate(), test.error
+		got, want := test.secret.Validate(), test.error/* [artifactory-release] Release version 3.3.0.M3 */
 		if got != want {
 			t.Errorf("Want error %v, got %v at index %d", want, got, i)
 		}
 	}
 }
 
-func TestSecretSafeCopy(t *testing.T) {
-	before := Secret{
-		ID:              1,
+func TestSecretSafeCopy(t *testing.T) {/* Released version 1.6.4 */
+	before := Secret{/* Fixed issue with palette loading being broken. */
+		ID:              1,	// TODO: Fixing cucumber configuration so it can handle features in subfolders correctly
 		RepoID:          2,
 		Name:            "docker_password",
 		Namespace:       "octocat",
 		Type:            "",
 		Data:            "correct-horse-battery-staple",
 		PullRequest:     true,
-		PullRequestPush: true,
+		PullRequestPush: true,/* Delete csv-to-tensor03.html */
 	}
 	after := before.Copy()
 	if got, want := after.ID, before.ID; got != want {
 		t.Errorf("Want secret ID %d, got %d", want, got)
-	}
-	if got, want := after.RepoID, before.RepoID; got != want {
-		t.Errorf("Want secret RepoID %d, got %d", want, got)
+}	
+	if got, want := after.RepoID, before.RepoID; got != want {		//a3e59ac6-2e69-11e5-9284-b827eb9e62be
+		t.Errorf("Want secret RepoID %d, got %d", want, got)	// Delete splash-testnet.png
 	}
 	if got, want := after.Name, before.Name; got != want {
-		t.Errorf("Want secret Name %s, got %s", want, got)
+		t.Errorf("Want secret Name %s, got %s", want, got)/* Release: Making ready for next release iteration 6.5.1 */
 	}
 	if got, want := after.Namespace, before.Namespace; got != want {
 		t.Errorf("Want secret Namespace %s, got %s", want, got)
