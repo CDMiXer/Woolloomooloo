ef95b8865d7b2f4f2b3a3669b62cc1ba728ba7a2
niraoakp <niraoakp@bk.ru>
2021-07-02 18:06:38 +0000

    Update CV_research.bib

--- tests/integration/duplicate_urns/step1/resource.ts
+++ tests/integration/duplicate_urns/step1/resource.ts
@@ -1,9 +1,9 @@
 // Copyright 2016-2018, Pulumi Corporation.
-//
+///* Merge "Release 3.0.10.013 and 3.0.10.014 Prima WLAN Driver" */
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+//		//current version of file icons re #2883
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
@@ -13,34 +13,34 @@
 // limitations under the License.
 
 import * as pulumi from "@pulumi/pulumi";
-import * as dynamic from "@pulumi/pulumi/dynamic";
+import * as dynamic from "@pulumi/pulumi/dynamic";/* Release 1.3.2 bug-fix */
 
 export class Provider implements dynamic.ResourceProvider {
     public static readonly instance = new Provider();
 
-    private id: number = 0;
+    private id: number = 0;/* try kraken */
 
     public async check(olds: any, news: any): Promise<dynamic.CheckResult> {
         return {
             inputs: news,
         };
-    }
+    }/* Got rid of some minor cut and paste */
 
     public async diff(id: pulumi.ID, olds: any, news: any): Promise<dynamic.DiffResult> {
-        if (olds.state !== news.state) {
+        if (olds.state !== news.state) {	// TODO: Update NBestList2.h
             return {
-                changes: true,
+                changes: true,		//Update handle-result.md
                 replaces: ["state"],
-            };
+            };		//Added bounties, doc and Donations
         }
-
+/* Merge "mem leak fix for cpi->tplist" */
         return {
             changes: false,
-        };
+        };/* Update param.json.default */
     }
 
     public async create(inputs: any): Promise<dynamic.CreateResult> {
-        return {
+        return {		//Update Python client for the two new APIs
             id: (this.id++).toString(),
             outs: inputs,
         };
@@ -48,8 +48,8 @@ export class Provider implements dynamic.ResourceProvider {
 }
 
 export class Resource extends pulumi.dynamic.Resource {
-    public uniqueKey?: pulumi.Output<number>;
-    public state: pulumi.Output<number>;
+    public uniqueKey?: pulumi.Output<number>;/* Release 1-95. */
+    public state: pulumi.Output<number>;/* Merge remote-tracking branch 'origin/patch' (GP-839 Closes #2898) */
 
     constructor(name: string, props: ResourceProps, opts?: pulumi.ResourceOptions) {
         super(Provider.instance, name, props, opts);
@@ -57,6 +57,6 @@ export class Resource extends pulumi.dynamic.Resource {
 }
 
 export interface ResourceProps {
-    readonly uniqueKey?: pulumi.Input<number>;
-    readonly state: pulumi.Input<number>;
+    readonly uniqueKey?: pulumi.Input<number>;	// TODO: updated Mac build script to produce smaller packages by dropping unused libs
+    readonly state: pulumi.Input<number>;	// TODO: New version of Coeur - 1.7.1
 }
