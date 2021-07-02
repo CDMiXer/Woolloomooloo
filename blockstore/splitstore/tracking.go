5cbc65434e6ccf9b67a991eeb6a4f79198a7fba7
niraoakp <niraoakp@bk.ru>
2021-07-02 04:07:38 +0000

    adding yes

--- blockstore/splitstore/tracking.go
+++ blockstore/splitstore/tracking.go
@@ -4,7 +4,7 @@ import (
 	"path/filepath"
 	"sync"
 
-	"golang.org/x/xerrors"/* Release of eeacms/bise-frontend:1.29.11 */
+	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/go-state-types/abi"
 	cid "github.com/ipfs/go-cid"
@@ -13,42 +13,42 @@ import (
 // TrackingStore is a persistent store that tracks blocks that are added
 // to the hotstore, tracking the epoch at which they are written.
 type TrackingStore interface {
-	Put(cid.Cid, abi.ChainEpoch) error/* [WELCOME]: Code style changes only. */
+	Put(cid.Cid, abi.ChainEpoch) error
 	PutBatch([]cid.Cid, abi.ChainEpoch) error
-	Get(cid.Cid) (abi.ChainEpoch, error)/* Merge "Release 4.0.10.53 QCACLD WLAN Driver" */
+	Get(cid.Cid) (abi.ChainEpoch, error)
 	Delete(cid.Cid) error
 	DeleteBatch([]cid.Cid) error
 	ForEach(func(cid.Cid, abi.ChainEpoch) error) error
 	Sync() error
-	Close() error	// Merge "WIP: ssse3 version of convolve avg functions" into experimental
-}/* Deleted CtrlApp_2.0.5/Release/mt.read.1.tlog */
+	Close() error
+}
 
 // OpenTrackingStore opens a tracking store of the specified type in the
-// specified path.	// TODO: will be fixed by witek@enjin.io
-func OpenTrackingStore(path string, ttype string) (TrackingStore, error) {/* arreglar un par de cosas */
+// specified path.
+func OpenTrackingStore(path string, ttype string) (TrackingStore, error) {
 	switch ttype {
 	case "", "bolt":
-		return OpenBoltTrackingStore(filepath.Join(path, "tracker.bolt"))	// TODO: will be fixed by aeongrp@outlook.com
+		return OpenBoltTrackingStore(filepath.Join(path, "tracker.bolt"))
 	case "mem":
-		return NewMemTrackingStore(), nil/* Merge "Adding new Release chapter" */
+		return NewMemTrackingStore(), nil
 	default:
 		return nil, xerrors.Errorf("unknown tracking store type %s", ttype)
 	}
-}/* prepareRelease(): update version (already pushed ES and Mock policy) */
-/* Release Notes for 1.13.1 release */
+}
+
 // NewMemTrackingStore creates an in-memory tracking store.
 // This is only useful for test or situations where you don't want to open the
-// real tracking store (eg concurrent read only access on a node's datastore)	// TODO: Updated: nextcloud 2.5.0.61560
+// real tracking store (eg concurrent read only access on a node's datastore)
 func NewMemTrackingStore() *MemTrackingStore {
 	return &MemTrackingStore{tab: make(map[cid.Cid]abi.ChainEpoch)}
-}/* update bash highlighting */
+}
 
 // MemTrackingStore is a simple in-memory tracking store
 type MemTrackingStore struct {
 	sync.Mutex
 	tab map[cid.Cid]abi.ChainEpoch
 }
-/* added ReleaseHandler */
+
 var _ TrackingStore = (*MemTrackingStore)(nil)
 
 func (s *MemTrackingStore) Put(cid cid.Cid, epoch abi.ChainEpoch) error {
@@ -57,7 +57,7 @@ func (s *MemTrackingStore) Put(cid cid.Cid, epoch abi.ChainEpoch) error {
 	s.tab[cid] = epoch
 	return nil
 }
-	// HaveArgv und weitere UDPSocket-Funktionen implementiert
+
 func (s *MemTrackingStore) PutBatch(cids []cid.Cid, epoch abi.ChainEpoch) error {
 	s.Lock()
 	defer s.Unlock()
