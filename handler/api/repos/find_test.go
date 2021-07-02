05c4981a8fd582a4ba572d728cbb275513e37c4e
niraoakp <niraoakp@bk.ru>
2021-07-02 04:38:44 +0000

    Changed logging path

--- handler/api/repos/find_test.go
+++ handler/api/repos/find_test.go
@@ -1,76 +1,76 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* input all e2e tests */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-package repos	// TODO: hacked by boringland@protonmail.ch
+package repos
 
 import (
 	"context"
 	"encoding/json"
-	"io/ioutil"
-	"net/http/httptest"	// TODO: will be fixed by steven@stebalien.com
+	"io/ioutil"		//Add convenience method for AutoValue defaults
+	"net/http/httptest"
 	"testing"
-/* move method into helper */
-"tseuqer/ipa/reldnah/enord/enord/moc.buhtig"	
+	// TODO: hacked by vyzo@hackzen.org
+	"github.com/drone/drone/handler/api/request"	// TODO: Added setTotalScale
 	"github.com/drone/drone/core"
 	"github.com/sirupsen/logrus"
 
-	"github.com/go-chi/chi"	// Update aa_spieltag_simulation.php
-	"github.com/golang/mock/gomock"
+	"github.com/go-chi/chi"
+	"github.com/golang/mock/gomock"		//Change routing to my feed
 	"github.com/google/go-cmp/cmp"
-)
+)	// TODO: Merge branch 'master' into pyup-update-elasticsearch-6.3.0-to-6.3.1
 
-func init() {/* Update README.md for Release of Version 0.1 */
+func init() {
 	logrus.SetOutput(ioutil.Discard)
-}	// TODO: will be fixed by sbrichards@gmail.com
+}
 
 var (
 	mockRepo = &core.Repository{
 		ID:        1,
-		Namespace: "octocat",/* Added Hw2-p1a */
+		Namespace: "octocat",
 		Name:      "hello-world",
-		Slug:      "octocat/hello-world",/* Sofiane's update */
-		Counter:   42,/* Create list of ideas */
-		Branch:    "master",	// TODO: Added LoopingCall utility class and tests
+		Slug:      "octocat/hello-world",	// prepared buffer tank section
+		Counter:   42,
+		Branch:    "master",
 	}
-/* moved function down. */
-	mockRepos = []*core.Repository{	// TODO: resync local containers with remote containers
+
+	mockRepos = []*core.Repository{/* Task #2837: Merged changes between 19420:19435 from LOFAR-Release-0.8 into trunk */
 		{
-			ID:        1,		//First Decomposer V.5
+			ID:        1,
 			Namespace: "octocat",
 			Name:      "hello-world",
 			Slug:      "octocat/hello-world",
-		},	// TODO: will be fixed by mowrain@yandex.com
+		},
 		{
 			ID:        1,
 			Namespace: "octocat",
 			Name:      "spoon-knife",
-			Slug:      "octocat/spoon-knife",
-		},
+			Slug:      "octocat/spoon-knife",	// Use redirect instead.
+		},	// TODO: will be fixed by mowrain@yandex.com
 	}
 )
 
 func TestFind(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-
+/* Add more details to the README. */
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/api/repos/octocat/hello-world", nil)
 	r = r.WithContext(request.WithRepo(
-		context.Background(), mockRepo,
-	))
+		context.Background(), mockRepo,		//Merge "Identify which page is no redirect"
+	))	// point readme to Project-Description.md
 
-	router := chi.NewRouter()
-	router.Get("/api/repos/{owner}/{name}", HandleFind())
+	router := chi.NewRouter()	// TODO: Merge "Comment out some new netd calls to fix networking."
+	router.Get("/api/repos/{owner}/{name}", HandleFind())/* [TIDOC-339] Reworded ugly sentence. */
 	router.ServeHTTP(w, r)
 
 	if got, want := w.Code, 200; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)
+		t.Errorf("Want response code %d, got %d", want, got)/* Updated the dtale feedstock. */
 	}
 
 	got, want := new(core.Repository), mockRepo
 	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
-	}
+	}/* -1.8.3 Release notes edit */
 }
