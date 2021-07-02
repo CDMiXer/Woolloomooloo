71a920129b85f1575a8d38b51cd528980398580c
niraoakp <niraoakp@bk.ru>
2021-07-02 09:44:42 +0000

    removed d3 library

--- handler/api/events/build_test.go
+++ handler/api/events/build_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: hacked by caojiaoyue@protonmail.com
+// Use of this source code is governed by the Drone Non-Commercial License/* support importing from logjam device directly */
+// that can be found in the LICENSE file./* Minor changes to image path */
 
 package events
