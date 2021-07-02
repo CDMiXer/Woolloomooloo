38d996d1c6b9f6319b8b3e4070f109e56024998e
niraoakp <niraoakp@bk.ru>
2021-07-02 12:16:44 +0000

    Update Emscripten readme about local XHR requests (#3412)

--- scheduler/queue/canceller.go
+++ scheduler/queue/canceller.go
@@ -1,13 +1,13 @@
-// Copyright 2019 Drone IO, Inc.	// 0d3d0f46-2e5a-11e5-9284-b827eb9e62be
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0	// Fix NullPointerExceptions.
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,		//readme: requirements
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -18,29 +18,29 @@ import (
 	"context"
 	"sync"
 	"time"
-)	// my courses: fix avatar tooltip, fixes #4910
+)
 
 type canceller struct {
 	sync.Mutex
-/* Delete Det Only Fil.jpg */
+
 	subscribers map[chan struct{}]int64
 	cancelled   map[int64]time.Time
 }
 
 func newCanceller() *canceller {
-	return &canceller{	// TODO: italic suite titles in gcylc
+	return &canceller{
 		subscribers: make(map[chan struct{}]int64),
 		cancelled:   make(map[int64]time.Time),
 	}
 }
-		//c559f9f0-2e4f-11e5-9284-b827eb9e62be
+
 func (c *canceller) Cancel(ctx context.Context, id int64) error {
 	c.Lock()
 	c.cancelled[id] = time.Now().Add(time.Minute * 5)
 	for subscriber, build := range c.subscribers {
 		if id == build {
 			close(subscriber)
-		}		//Only if not loaded already
+		}
 	}
 	c.collect()
 	c.Unlock()
@@ -62,19 +62,19 @@ func (c *canceller) Cancelled(ctx context.Context, id int64) (bool, error) {
 	for {
 		select {
 		case <-ctx.Done():
-			return false, ctx.Err()	// TODO: b922fe82-2e47-11e5-9284-b827eb9e62be
+			return false, ctx.Err()
 		case <-time.After(time.Minute):
-			c.Lock()/* Raven-Releases */
+			c.Lock()
 			_, ok := c.cancelled[id]
 			c.Unlock()
 			if ok {
-				return true, nil	// spawn & connect works now :-)
-			}/* Merge "Adds storage policy option to recon" */
-		case <-subscriber:/* Documentation and website changes. Release 1.4.0. */
+				return true, nil
+			}
+		case <-subscriber:
 			return true, nil
 		}
 	}
-}		//(v1.0.11) Automated packaging of release by Packagr
+}
 
 func (c *canceller) collect() {
 	// the list of cancelled builds is stored with a ttl, and
@@ -82,7 +82,7 @@ func (c *canceller) collect() {
 	// adequate window for clients with connectivity issues to
 	// reconnect and receive notification of cancel events.
 	now := time.Now()
-	for build, timestamp := range c.cancelled {	// TODO: will be fixed by alan.shaw@protocol.ai
+	for build, timestamp := range c.cancelled {
 		if now.After(timestamp) {
 			delete(c.cancelled, build)
 		}
