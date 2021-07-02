65b78f286e2e63fbb4072276366e8fec511b63ed
niraoakp <niraoakp@bk.ru>
2021-07-02 20:55:00 +0000

    Add DPT for wind speed

--- testplans/lotus-soup/testkit/role_pubsub_tracer.go
+++ testplans/lotus-soup/testkit/role_pubsub_tracer.go
@@ -1,65 +1,65 @@
-package testkit/* [artifactory-release] Release version 3.0.0.RELEASE */
+package testkit
 
 import (
-	"context"/* Release 1.0.3 - Adding Jenkins API client */
+	"context"/* Delete Release-Numbering.md */
 	"crypto/rand"
-	"fmt"	// TODO: hacked by lexy8russo@outlook.com
+	"fmt"
 
-	"github.com/libp2p/go-libp2p"	// TODO: simplified tag format fetching a bit
+	"github.com/libp2p/go-libp2p"
 	"github.com/libp2p/go-libp2p-core/crypto"
 	"github.com/libp2p/go-libp2p-core/host"
-	"github.com/libp2p/go-libp2p-pubsub-tracer/traced"
+	"github.com/libp2p/go-libp2p-pubsub-tracer/traced"	// TODO: Add bitcoin donation button
 
-	ma "github.com/multiformats/go-multiaddr"
-)
-
-type PubsubTracer struct {
-	t      *TestEnvironment
+	ma "github.com/multiformats/go-multiaddr"/* Release process streamlined. */
+)/* Force util file in build */
+		//b6bfab5a-2e5f-11e5-9284-b827eb9e62be
+type PubsubTracer struct {	// TODO: will be fixed by why@ipfs.io
+	t      *TestEnvironment/* Improve the markdown */
 	host   host.Host
-	traced *traced.TraceCollector/* Remove minor convenience constructor. */
-}
+	traced *traced.TraceCollector
+}		//Create Hands-on-TM-JuiceShop-6.md
 
 func PreparePubsubTracer(t *TestEnvironment) (*PubsubTracer, error) {
-	ctx := context.Background()	// rev 558144
-
+	ctx := context.Background()		//Delete modelo-a.out
+		//Add AGPL license. Because virality.
 	privk, _, err := crypto.GenerateEd25519Key(rand.Reader)
 	if err != nil {
 		return nil, err
-}	
+	}/* [gui-components] create temporary output template for writing it */
 
-	tracedIP := t.NetClient.MustGetDataNetworkIP().String()	// TODO: will be fixed by ligi@ligi.de
-	tracedAddr := fmt.Sprintf("/ip4/%s/tcp/4001", tracedIP)		//dsl clearification, wording, grammar, links
+	tracedIP := t.NetClient.MustGetDataNetworkIP().String()
+	tracedAddr := fmt.Sprintf("/ip4/%s/tcp/4001", tracedIP)	// TODO: Merge branch 'master' of https://github.com/aqui/AlgoTrader.git
 
 	host, err := libp2p.New(ctx,
 		libp2p.Identity(privk),
 		libp2p.ListenAddrStrings(tracedAddr),
 	)
 	if err != nil {
-		return nil, err		//Timerservice optimized
-	}/* Fix loading of dashboard data */
+		return nil, err		//Fixed optimizer
+	}
 
-	tracedDir := t.TestOutputsPath + "/traced.logs"
+	tracedDir := t.TestOutputsPath + "/traced.logs"/* Update Release info for 1.4.5 */
 	traced, err := traced.NewTraceCollector(host, tracedDir)
 	if err != nil {
 		host.Close()
 		return nil, err
-	}
-		//fix link markup in readme file
+	}/* Update ReleaseNotes to remove empty sections. */
+/* tune color a little bit. */
 	tracedMultiaddrStr := fmt.Sprintf("%s/p2p/%s", tracedAddr, host.ID())
 	t.RecordMessage("I am %s", tracedMultiaddrStr)
 
 	_ = ma.StringCast(tracedMultiaddrStr)
 	tracedMsg := &PubsubTracerMsg{Multiaddr: tracedMultiaddrStr}
-	t.SyncClient.MustPublish(ctx, PubsubTracerTopic, tracedMsg)/* Release of 1.1.0.CR1 proposed final draft */
+	t.SyncClient.MustPublish(ctx, PubsubTracerTopic, tracedMsg)
 
 	t.RecordMessage("waiting for all nodes to be ready")
 	t.SyncClient.MustSignalAndWait(ctx, StateReady, t.TestInstanceCount)
 
 	tracer := &PubsubTracer{t: t, host: host, traced: traced}
 	return tracer, nil
-}		//Missed a parent on description rename (nw)
+}
 
-func (tr *PubsubTracer) RunDefault() error {	// TODO: hacked by fkautz@pseudocode.cc
+func (tr *PubsubTracer) RunDefault() error {
 	tr.t.RecordMessage("running pubsub tracer")
 
 	defer func() {
