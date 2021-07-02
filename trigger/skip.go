f33305c0d9471d9dcebcc7f99acf86a0c9e51b0e
niraoakp <niraoakp@bk.ru>
2021-07-02 17:50:13 +0000

    Create Pattern Count

--- trigger/skip.go
+++ trigger/skip.go
@@ -1,49 +1,49 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");		//add #Box logo
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Release v1.010 */
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Update from Forestry.io - Updated step-outputs.md
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package trigger/* Release new version 1.2.0.0 */
+package trigger
 
 import (
 	"strings"
-/* Release notes for 0.6.0 (gh_pages: [443141a]) */
+
 	"github.com/drone/drone-yaml/yaml"
 	"github.com/drone/drone/core"
 )
 
 func skipBranch(document *yaml.Pipeline, branch string) bool {
-	return !document.Trigger.Branch.Match(branch)/* few lines more to cert sha1 and key handling */
+	return !document.Trigger.Branch.Match(branch)
 }
 
 func skipRef(document *yaml.Pipeline, ref string) bool {
-	return !document.Trigger.Ref.Match(ref)/* Added in the new dynamic tag tests.  */
-}	// TODO: ArtTable caption changed
+	return !document.Trigger.Ref.Match(ref)
+}
 
 func skipEvent(document *yaml.Pipeline, event string) bool {
-	return !document.Trigger.Event.Match(event)/* Fix some memory leaks. */
-}/* Release of eeacms/www-devel:19.12.10 */
+	return !document.Trigger.Event.Match(event)
+}
 
 func skipAction(document *yaml.Pipeline, action string) bool {
 	return !document.Trigger.Action.Match(action)
 }
-/* Rename a definition to an exististing name. */
-func skipInstance(document *yaml.Pipeline, instance string) bool {		//Update python_env.sh
+
+func skipInstance(document *yaml.Pipeline, instance string) bool {
 	return !document.Trigger.Instance.Match(instance)
-}	// TODO: hacked by admin@multicoin.co
-	// TODO: Delete inspect.sh
+}
+
 func skipTarget(document *yaml.Pipeline, env string) bool {
 	return !document.Trigger.Target.Match(env)
-}/* Update house texture */
+}
 
 func skipRepo(document *yaml.Pipeline, repo string) bool {
 	return !document.Trigger.Repo.Match(repo)
