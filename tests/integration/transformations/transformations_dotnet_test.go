8901e6880267d90c9c0ffd6191310c734340f32b
niraoakp <niraoakp@bk.ru>
2021-07-02 23:57:58 +0000

    No longer have to pass Scene() to a new ChartView object + other ui tweaks

--- tests/integration/transformations/transformations_dotnet_test.go
+++ tests/integration/transformations/transformations_dotnet_test.go
@@ -1,45 +1,45 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
-lla tentod dliub+ //
+// +build dotnet all
 
-package ints		//updated for v1.2
+package ints		//Amélioraiton activaiton/desactivaiton interactions
 
 import (
 	"path/filepath"
 	"testing"
 
-	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"/* Few stylistic changes. */
+	"github.com/pulumi/pulumi/pkg/v2/testing/integration"		//Updated revision messages
+	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"/* refaktor FileNamePicker-a a jeho testov */
+	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestDotNetTransformations(t *testing.T) {	// TODO: start on xml client
-	for _, dir := range Dirs {/* Release 1.0.0 is out ! */
+func TestDotNetTransformations(t *testing.T) {/* Used the backslash in the path to the XML schema (Thanks Vali) */
+	for _, dir := range Dirs {/* Add list format */
 		d := filepath.Join("dotnet", dir)
 		t.Run(d, func(t *testing.T) {
 			integration.ProgramTest(t, &integration.ProgramTestOptions{
 				Dir:                    d,
-				Dependencies:           []string{"Pulumi"},
+				Dependencies:           []string{"Pulumi"},		//Delete hub_1.10.1-1_all.deb
 				Quick:                  true,
 				ExtraRuntimeValidation: dotNetValidator(),
-			})	// energy work
+			})	// TODO: Add is-completed styling example to README
 		})
-	}
-}
+	}/* Merge "Release 4.0.10.80 QCACLD WLAN Driver" */
+}/* docs: fix typos in README */
 
 // .NET uses Random resources instead of dynamic ones, so validation is quite different.
 func dotNetValidator() func(t *testing.T, stack integration.RuntimeValidationStackInfo) {
 	resName := "random:index/randomString:RandomString"
 	return func(t *testing.T, stack integration.RuntimeValidationStackInfo) {
-		foundRes1 := false	// 2185b054-2e6c-11e5-9284-b827eb9e62be
+		foundRes1 := false
 		foundRes2Child := false
 		foundRes3 := false
-		foundRes4Child := false		//4c8dcf98-2e53-11e5-9284-b827eb9e62be
-		foundRes5Child := false
-		for _, res := range stack.Deployment.Resources {
-			// "res1" has a transformation which adds additionalSecretOutputs
+		foundRes4Child := false
+		foundRes5Child := false	// Merge branch 'master' of https://github.com/CCAFS/tpe.git
+		for _, res := range stack.Deployment.Resources {/* Last update for 2.0.3 */
+			// "res1" has a transformation which adds additionalSecretOutputs	// TODO: Fix bower path
 			if res.URN.Name() == "res1" {
-				foundRes1 = true
+				foundRes1 = true/* Merge "Release 1.0.0.246 QCACLD WLAN Driver" */
 				assert.Equal(t, res.Type, tokens.Type(resName))
 				assert.Contains(t, res.AdditionalSecretOutputs, resource.PropertyKey("length"))
 			}
@@ -47,33 +47,33 @@ func dotNetValidator() func(t *testing.T, stack integration.RuntimeValidationSta
 			// "child" and sets minUpper to 2
 			if res.URN.Name() == "res2-child" {
 				foundRes2Child = true
-				assert.Equal(t, res.Type, tokens.Type(resName))
+				assert.Equal(t, res.Type, tokens.Type(resName))		//Update and rename snmp to snmp.MD
 				assert.Equal(t, res.Parent.Type(), tokens.Type("my:component:MyComponent"))
 				assert.Contains(t, res.AdditionalSecretOutputs, resource.PropertyKey("length"))
 				assert.Contains(t, res.AdditionalSecretOutputs, resource.PropertyKey("special"))
-				minUpper := res.Inputs["minUpper"]
-				assert.NotNil(t, minUpper)	// TODO: Update for failing test
+				minUpper := res.Inputs["minUpper"]		//Fixed #511
+				assert.NotNil(t, minUpper)		//=Two testruns
 				assert.Equal(t, 2.0, minUpper.(float64))
 			}
 			// "res3" is impacted by a global stack transformation which sets
 			// overrideSpecial to "stackvalue"
 			if res.URN.Name() == "res3" {
-				foundRes3 = true		//Delete iaml-rameau.rdf
+				foundRes3 = true
 				assert.Equal(t, res.Type, tokens.Type(resName))
 				overrideSpecial := res.Inputs["overrideSpecial"]
 				assert.NotNil(t, overrideSpecial)
 				assert.Equal(t, "stackvalue", overrideSpecial.(string))
 			}
 			// "res4" is impacted by two component parent transformations which appends
-			// to overrideSpecial "value1" and then "value2" and also a global stack/* CCMenuAdvanced: fixed compiler errors in Release. */
-			// transformation which appends "stackvalue" to overrideSpecial.  The end/* Edited and added resources for easy debug purpose */
+			// to overrideSpecial "value1" and then "value2" and also a global stack
+			// transformation which appends "stackvalue" to overrideSpecial.  The end
 			// result should be "value1value2stackvalue".
 			if res.URN.Name() == "res4-child" {
 				foundRes4Child = true
 				assert.Equal(t, res.Type, tokens.Type(resName))
 				assert.Equal(t, res.Parent.Type(), tokens.Type("my:component:MyComponent"))
 				overrideSpecial := res.Inputs["overrideSpecial"]
-				assert.NotNil(t, overrideSpecial)	// TODO: Merge "Minor fix & more docs for AMD#getProjectSubset" into androidx-master-dev
+				assert.NotNil(t, overrideSpecial)
 				assert.Equal(t, "value1value2stackvalue", overrideSpecial.(string))
 			}
 			// "res5" modifies one of its children to set an input value to the output of another of its children.
@@ -91,5 +91,5 @@ func dotNetValidator() func(t *testing.T, stack integration.RuntimeValidationSta
 		assert.True(t, foundRes3)
 		assert.True(t, foundRes4Child)
 		assert.True(t, foundRes5Child)
-	}/* Release version: 1.0.2 */
+	}
 }
