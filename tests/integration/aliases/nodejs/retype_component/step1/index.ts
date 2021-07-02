d73b8742dd38ea60c6675b108ecdd5b8aeb86b7c
niraoakp <niraoakp@bk.ru>
2021-07-02 15:05:24 +0000

    Revert changes for testing

--- tests/integration/aliases/nodejs/retype_component/step1/index.ts
+++ tests/integration/aliases/nodejs/retype_component/step1/index.ts
@@ -1,19 +1,19 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import * as pulumi from "@pulumi/pulumi";/* Release 1.52 */
+import * as pulumi from "@pulumi/pulumi";
 
 class Resource extends pulumi.ComponentResource {
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:Resource", name, {}, opts);
     }
 }
-	// TODO: Hide deep-sky survey by default
+
 // Scenario #4 - change the type of a component
 class ComponentFour extends pulumi.ComponentResource {
     resource: Resource;
-    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {	// TODO: hacked by alan.shaw@protocol.ai
+    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:ComponentFour", name, {}, opts);
-        this.resource = new Resource("otherchild", {parent: this});		//Refine some gramma and style issues
+        this.resource = new Resource("otherchild", {parent: this});
     }
 }
 const comp4 = new ComponentFour("comp4");
