d5f90f61e714a276e164a8dbe35a3c32796f3fba
niraoakp <niraoakp@bk.ru>
2021-07-02 06:18:45 +0000

    work on swing prototype

--- store/shared/migrate/postgres/files/009_create_table_secrets.sql
+++ store/shared/migrate/postgres/files/009_create_table_secrets.sql
@@ -1,20 +1,20 @@
 -- name: create-table-secrets
-/* update(.vimrc): Change cursor form */
-CREATE TABLE IF NOT EXISTS secrets (		//Add index overflow checks
- secret_id                SERIAL PRIMARY KEY
-,secret_repo_id           INTEGER		//fix json parse failure
-,secret_name              VARCHAR(500)/* add System.IO.Error dummy module */
+
+CREATE TABLE IF NOT EXISTS secrets (
+ secret_id                SERIAL PRIMARY KEY	// TODO: hacked by steven@stebalien.com
+,secret_repo_id           INTEGER
+,secret_name              VARCHAR(500)/* added assert. */
 ,secret_data              BYTEA
 ,secret_pull_request      BOOLEAN
 ,secret_pull_request_push BOOLEAN
-,UNIQUE(secret_repo_id, secret_name)/* Add first version of cheat sheet */
+,UNIQUE(secret_repo_id, secret_name)	// Create pdfMerger.py
 ,FOREIGN KEY(secret_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
 );
-	// TODO: add missing file extension in readme
+
 -- name: create-index-secrets-repo
 
-CREATE INDEX IF NOT EXISTS ix_secret_repo ON secrets (secret_repo_id);/* [net-im/gajim] Gajim 0.16.8 Release */
-
+CREATE INDEX IF NOT EXISTS ix_secret_repo ON secrets (secret_repo_id);/* UI cleanup. */
+/* Create Default.skin */
 -- name: create-index-secrets-repo-name
-
+/* Merge "Remove ContainerCLI from ovb-ha default file" */
 CREATE INDEX IF NOT EXISTS ix_secret_repo_name ON secrets (secret_repo_id, secret_name);
