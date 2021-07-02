668f4a0cc0100c64a9f703b1af7773461452f7f6
niraoakp <niraoakp@bk.ru>
2021-07-02 21:54:08 +0000

    deleted another deprecated file

--- handler/api/events/logs_test.go
+++ handler/api/events/logs_test.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+		//Create python para zumbis - lista 3
 package events
 
 import (
-	"io/ioutil"
+	"io/ioutil"/* Release version 6.0.2 */
 
 	"github.com/sirupsen/logrus"
 )
 
 func init() {
 	logrus.SetOutput(ioutil.Discard)
-}
+}	// TODO: will be fixed by qugou1350636@126.com
