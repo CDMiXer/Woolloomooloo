c12f9eace49922d03a66da6601419172965b532c
niraoakp <niraoakp@bk.ru>
2021-07-02 13:40:47 +0000

    Docs were missing `repeat`

--- handler/api/repos/crons/exec_test.go
+++ handler/api/repos/crons/exec_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: hacked by lexy8russo@outlook.com
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
-
-package crons		//c4ce2160-2e4e-11e5-9284-b827eb9e62be
+/* Fix for key instead of value */
+package crons
