083105dec9f3c0b7851e671d4f7afa3e0312e854
niraoakp <niraoakp@bk.ru>
2021-07-02 22:16:08 +0000

    [FIX] Options, check for changes: ignore line endings

--- core/trigger_test.go
+++ core/trigger_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Release to Github as Release instead of draft */
-// that can be found in the LICENSE file./* 552cf0a8-2e70-11e5-9284-b827eb9e62be */
-
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.
+	// TODO: Added help for multi-user editing
 // +build !oss
 
 package core
