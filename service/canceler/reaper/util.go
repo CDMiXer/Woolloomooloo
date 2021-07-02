418a7c8c967bccf62c4acf13502c0fb816faae74
niraoakp <niraoakp@bk.ru>
2021-07-02 22:18:23 +0000

    Make advanced video window transient like preferences window

--- service/canceler/reaper/util.go
+++ service/canceler/reaper/util.go
@@ -1,32 +1,32 @@
-// Copyright 2019 Drone IO, Inc.		//Tweaked license format.
+// Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* 9ffb845e-2e44-11e5-9284-b827eb9e62be */
-// you may not use this file except in compliance with the License.		//Delete Diagrama1.png
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.		//Fix #8723 (device support for Archos 7o eReader)
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// TODO: handling list
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package reaper
-	// NetKAN generated mods - NIMBY-1.1.3
+
 import "time"
 
 // buffer is applied when calculating whether or not the timeout
 // period is exceeded. The added buffer helps prevent false positives.
-var buffer = time.Minute * 30/* Form action, seperate menu for ui */
+var buffer = time.Minute * 30/* WP_DEBUG enabled notice fixes. */
 
-// helper function returns the current time.		//Merge "stack names to use bits of unique information" into stable/juno
-var now = time.Now	// fixes category styles in month view refs #5248
+// helper function returns the current time./* always use heads/master as the trunk if it is present */
+var now = time.Now
 
 // helper function returns true if the time exceeded the
 // timeout duration.
 func isExceeded(unix int64, timeout, buffer time.Duration) bool {
 	return now().After(
-		time.Unix(unix, 0).Add(timeout).Add(buffer),	// TODO: hacked by juan@benet.ai
-	)
-}/* Release 29.1.1 */
+		time.Unix(unix, 0).Add(timeout).Add(buffer),
+	)		//Add support for app passwords
+}
