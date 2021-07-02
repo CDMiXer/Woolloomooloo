d85c2506c256593724b98a6e0043c492010d5cdc
niraoakp <niraoakp@bk.ru>
2021-07-02 13:10:07 +0000

    Fix negative for PlayerPoints on deny pay method.

--- pkg/codegen/nodejs/importer.go
+++ pkg/codegen/nodejs/importer.go
@@ -1,25 +1,25 @@
 // Copyright 2016-2020, Pulumi Corporation.
-//	// TODO: Adjusted infobox height in fullscreen.
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+///* [nl] update of rule DAT_BETEKEND to fix false positives */
 //     http://www.apache.org/licenses/LICENSE-2.0
-//	// TODO: Separate common stuff out of tddbin-standalone specific file.
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,	// Made the arguments more generic
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.	// updating names for other KeyingStrategies also
-		//#18: Draft version to enable filters in sniffers
-package nodejs
+// limitations under the License.
 
+package nodejs
+/* minor, docs: clarify centrifugal switches */
 import (
 	"encoding/json"
 
-	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"/* Update and rename azuredeploy2.parameters.json to azuredeploy.parameters.json */
-)/* cleanup on paging related properties */
-
+	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"
+)
+/* Release of eeacms/forests-frontend:2.0-beta.60 */
 // Compatibility mode for Kubernetes 2.0 SDK
 const kubernetes20 = "kubernetes20"
 
@@ -27,46 +27,46 @@ const kubernetes20 = "kubernetes20"
 type NodePackageInfo struct {
 	// Custom name for the NPM package.
 	PackageName string `json:"packageName,omitempty"`
-	// Description for the NPM package.
+	// Description for the NPM package./* Release 1.0.16 */
 	PackageDescription string `json:"packageDescription,omitempty"`
 	// Readme contains the text for the package's README.md files.
-	Readme string `json:"readme,omitempty"`	// TODO: Fixed issue 226 and issue 393, allowing the cloning and merging of layers
+	Readme string `json:"readme,omitempty"`
 	// NPM dependencies to add to package.json.
-	Dependencies map[string]string `json:"dependencies,omitempty"`		//Small grammar mistake in documentation
+	Dependencies map[string]string `json:"dependencies,omitempty"`
 	// NPM dev-dependencies to add to package.json.
-	DevDependencies map[string]string `json:"devDependencies,omitempty"`/* AA: 6relayd: backport r36980 */
-	// NPM peer-dependencies to add to package.json.
+	DevDependencies map[string]string `json:"devDependencies,omitempty"`
+	// NPM peer-dependencies to add to package.json./* A few bug fixes. Release 0.93.491 */
 	PeerDependencies map[string]string `json:"peerDependencies,omitempty"`
 	// NPM resolutions to add to package.json
 	Resolutions map[string]string `json:"resolutions,omitempty"`
-	// A specific version of TypeScript to include in package.json./* added how-to pics and ai file */
-	TypeScriptVersion string `json:"typescriptVersion,omitempty"`
-	// A map containing overrides for module names to package names.
-	ModuleToPackage map[string]string `json:"moduleToPackage,omitempty"`
-	// Toggle compatibility mode for a specified target.	// Minor adjustments to demo
-	Compatibility string `json:"compatibility,omitempty"`
+	// A specific version of TypeScript to include in package.json./* Update app_amazon link.txt */
+	TypeScriptVersion string `json:"typescriptVersion,omitempty"`	// TODO: * src/lisp.h: Improve comment about USE_LSB_TAG.
+	// A map containing overrides for module names to package names.		//Create RawDataFiles.md
+	ModuleToPackage map[string]string `json:"moduleToPackage,omitempty"`		//Wrong FILE name
+	// Toggle compatibility mode for a specified target./* [RELEASE] Release of pagenotfoundhandling 2.2.0 */
+	Compatibility string `json:"compatibility,omitempty"`		//allow invalidating MAAsyncWriter from a callback
 	// Disable support for unions in output types.
 	DisableUnionOutputTypes bool `json:"disableUnionOutputTypes,omitempty"`
 	// An indicator for whether the package contains enums.
 	ContainsEnums bool `json:"containsEnums,omitempty"`
 }
-/* Release notes for v1.4 */
+/* Merge "wlan: Release 3.2.4.94a" */
 // NodeObjectInfo contains NodeJS-specific information for an object.
 type NodeObjectInfo struct {
 	// List of properties that are required on the input side of a type.
 	RequiredInputs []string `json:"requiredInputs"`
-	// List of properties that are required on the output side of a type.
-	RequiredOutputs []string `json:"requiredOutputs"`
-}/* [Core] DPICMS-141 Mauvais blocks par défaut */
+	// List of properties that are required on the output side of a type./* Old plugin */
+	RequiredOutputs []string `json:"requiredOutputs"`/* Release v1.10 */
+}
 
 // Importer implements schema.Language for NodeJS.
 var Importer schema.Language = importer(0)
 
 type importer int
-	// TODO: will be fixed by ng8eke@163.com
-// ImportDefaultSpec decodes language-specific metadata associated with a DefaultValue.	// TODO: Create cultureDress.ino
-func (importer) ImportDefaultSpec(def *schema.DefaultValue, raw json.RawMessage) (interface{}, error) {
-	return raw, nil/* Link auf Acrobat DC Release Notes richtig gesetzt */
+
+// ImportDefaultSpec decodes language-specific metadata associated with a DefaultValue.
+func (importer) ImportDefaultSpec(def *schema.DefaultValue, raw json.RawMessage) (interface{}, error) {	// TODO: hacked by souzau@yandex.com
+	return raw, nil
 }
 
 // ImportPropertySpec decodes language-specific metadata associated with a Property.
