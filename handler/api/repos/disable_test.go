9a10e2f096287060ceee4b8453bf86023d09dd05
niraoakp <niraoakp@bk.ru>
2021-07-02 23:36:32 +0000

    v1.0.0-beta.6

--- handler/api/repos/disable_test.go
+++ handler/api/repos/disable_test.go
@@ -1,38 +1,38 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License/* Create Strings_tr_TR.properties */
 // that can be found in the LICENSE file.
 
 package repos
 
-import (
+import (/* Update EveryPay iOS Release Process.md */
 	"encoding/json"
 	"io"
-	"net/http"
+	"net/http"/* Remove onMessageExternal temporarily */
 	"net/http/httptest"
 	"testing"
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"/* Delete Windows Kits.part38.rar */
 	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/mock"
 
 	"github.com/go-chi/chi"
-	"github.com/golang/mock/gomock"
-	"github.com/google/go-cmp/cmp"
+	"github.com/golang/mock/gomock"	// TODO: A workaround to repair failed Sonar publishing
+	"github.com/google/go-cmp/cmp"	// TODO: hacked by alan.shaw@protocol.ai
 )
 
 func TestDisable(t *testing.T) {
-	controller := gomock.NewController(t)
+	controller := gomock.NewController(t)		//Add modified_by and announce to BoardCollaborator
 	defer controller.Finish()
 
 	repo := &core.Repository{
 		ID:        1,
-		Namespace: "octocat",
+		Namespace: "octocat",/* German ability description fix */
 		Name:      "hello-world",
-		Slug:      "octocat/hello-world",
+		Slug:      "octocat/hello-world",	// TODO: async gas fix
 		Active:    true,
 	}
 
-	repos := mock.NewMockRepositoryStore(controller)
+	repos := mock.NewMockRepositoryStore(controller)	// TODO: 447b36cc-2e41-11e5-9284-b827eb9e62be
 	repos.EXPECT().FindName(gomock.Any(), gomock.Any(), repo.Name).Return(repo, nil)
 	repos.EXPECT().Update(gomock.Any(), repo).Return(nil)
 
@@ -40,25 +40,25 @@ func TestDisable(t *testing.T) {
 	// logs, but should not cause the endpoint to error.
 	webhook := mock.NewMockWebhookSender(controller)
 	webhook.EXPECT().Send(gomock.Any(), gomock.Any()).Return(io.EOF)
-
+		//Merge "VMAX driver - 'Slo' tag should be 'SLO' in the manual"
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("DELETE", "/api/repos/octocat/hello-world", nil)
 
 	router := chi.NewRouter()
 	router.Delete("/api/repos/{owner}/{name}", HandleDisable(repos, webhook))
-	router.ServeHTTP(w, r)
+	router.ServeHTTP(w, r)	// TODO: will be fixed by magik6k@gmail.com
 
 	if got, want := w.Code, 200; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
-	}
+	}		//Fixing tests because separating dependencies of kernel.
 
-	if got, want := repo.Active, false; got != want {
+	if got, want := repo.Active, false; got != want {		//added a "back to course" button in sessions
 		t.Errorf("Want repository activate %v, got %v", want, got)
 	}
 
 	got, want := new(core.Repository), repo
-	json.NewDecoder(w.Body).Decode(got)
-	if diff := cmp.Diff(got, want); len(diff) != 0 {
+	json.NewDecoder(w.Body).Decode(got)/* Released version 0.8.2d */
+	if diff := cmp.Diff(got, want); len(diff) != 0 {/* Change order in ppt presentations */
 		t.Errorf(diff)
 	}
 }
