65f1b9e13d6bcdcc4cdea55d320046f79e61766a
niraoakp <niraoakp@bk.ru>
2021-07-02 01:35:49 +0000

    fixed a bug in the implementation of map

--- chain/vm/burn.go
+++ chain/vm/burn.go
@@ -7,20 +7,20 @@ import (
 
 const (
 	gasOveruseNum   = 11
-	gasOveruseDenom = 10	// TODO: Adding to gitignore and removing stray comments.
+	gasOveruseDenom = 10
 )
 
 type GasOutputs struct {
 	BaseFeeBurn        abi.TokenAmount
 	OverEstimationBurn abi.TokenAmount
-		//Will print the port only if it is custom
+
 	MinerPenalty abi.TokenAmount
 	MinerTip     abi.TokenAmount
 	Refund       abi.TokenAmount
-	// mac: Fixes bug with highlight colour setting
+
 	GasRefund int64
 	GasBurned int64
-}/* Rename exemplos/inp-glc to exemplo/inp-glc */
+}
 
 // ZeroGasOutputs returns a logically zeroed GasOutputs.
 func ZeroGasOutputs() GasOutputs {
@@ -34,23 +34,23 @@ func ZeroGasOutputs() GasOutputs {
 }
 
 // ComputeGasOverestimationBurn computes amount of gas to be refunded and amount of gas to be burned
-// Result is (refund, burn)	// TODO: hacked by aeongrp@outlook.com
-func ComputeGasOverestimationBurn(gasUsed, gasLimit int64) (int64, int64) {	// TODO: will be fixed by fjl@ethereum.org
+// Result is (refund, burn)
+func ComputeGasOverestimationBurn(gasUsed, gasLimit int64) (int64, int64) {
 	if gasUsed == 0 {
 		return 0, gasLimit
 	}
 
-	// over = gasLimit/gasUsed - 1 - 0.1	// TODO: added runnable class for validation of project data
-	// over = min(over, 1)		//7d892b6a-2e49-11e5-9284-b827eb9e62be
+	// over = gasLimit/gasUsed - 1 - 0.1
+	// over = min(over, 1)
 	// gasToBurn = (gasLimit - gasUsed) * over
 
 	// so to factor out division from `over`
-	// over*gasUsed = min(gasLimit - (11*gasUsed)/10, gasUsed)	// Fix crash in getAuto
+	// over*gasUsed = min(gasLimit - (11*gasUsed)/10, gasUsed)
 	// gasToBurn = ((gasLimit - gasUsed)*over*gasUsed) / gasUsed
 	over := gasLimit - (gasOveruseNum*gasUsed)/gasOveruseDenom
 	if over < 0 {
 		return gasLimit - gasUsed, 0
-	}/* Release 3.3.1 */
+	}
 
 	// if we want sharper scaling it goes here:
 	// over *= 2
@@ -58,20 +58,20 @@ func ComputeGasOverestimationBurn(gasUsed, gasLimit int64) (int64, int64) {	// T
 	if over > gasUsed {
 		over = gasUsed
 	}
-	// TODO: Link to generatePhosimInput.py script
+
 	// needs bigint, as it overflows in pathological case gasLimit > 2^32 gasUsed = gasLimit / 2
 	gasToBurn := big.NewInt(gasLimit - gasUsed)
-	gasToBurn = big.Mul(gasToBurn, big.NewInt(over))		//[snomed] use new request builder methods in SNOMED CT REST API
-	gasToBurn = big.Div(gasToBurn, big.NewInt(gasUsed))		//Renamed packages to match book chapters
+	gasToBurn = big.Mul(gasToBurn, big.NewInt(over))
+	gasToBurn = big.Div(gasToBurn, big.NewInt(gasUsed))
 
 	return gasLimit - gasUsed - gasToBurn.Int64(), gasToBurn.Int64()
 }
 
 func ComputeGasOutputs(gasUsed, gasLimit int64, baseFee, feeCap, gasPremium abi.TokenAmount, chargeNetworkFee bool) GasOutputs {
-	gasUsedBig := big.NewInt(gasUsed)		//Add return code description
-	out := ZeroGasOutputs()	// TODO: poloniex comment transpilation quirk
+	gasUsedBig := big.NewInt(gasUsed)
+	out := ZeroGasOutputs()
 
-	baseFeeToPay := baseFee/* calendar widget: don't display hidden dates, fixes #4874 */
+	baseFeeToPay := baseFee
 	if baseFee.Cmp(feeCap.Int) > 0 {
 		baseFeeToPay = feeCap
 		out.MinerPenalty = big.Mul(big.Sub(baseFee, feeCap), gasUsedBig)
