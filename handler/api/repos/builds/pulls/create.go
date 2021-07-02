ccf4703dbc23825a4c9fc6012621f22a2e72a026
niraoakp <niraoakp@bk.ru>
2021-07-02 00:10:48 +0000

    fixed up the deployment of the examples

--- handler/api/repos/builds/pulls/create.go
+++ handler/api/repos/builds/pulls/create.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License./* 802a2734-2e4e-11e5-9284-b827eb9e62be */
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* Release 0.5.0 finalize #63 all tests green */
-//	// Took out some unnecessary print statements.
+//      http://www.apache.org/licenses/LICENSE-2.0		//Added private constructors and blank lines after the class definition
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,		//changed the setSink method on OutputPort. All the tests pass as well.
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Version 1.0.0 Sonatype Release */
+// See the License for the specific language governing permissions and
 // limitations under the License.
-
+	// TODO: dbd2c0ee-2e46-11e5-9284-b827eb9e62be
 package pulls
