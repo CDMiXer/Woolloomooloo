569b7c02c9e0a82aa406e84b4b403d47fba4888d
niraoakp <niraoakp@bk.ru>
2021-07-02 06:42:55 +0000

    Merge branch 'development' into alt-shift

--- tests/integration/aliases/nodejs/rename_component_and_child/step2/index.ts
+++ tests/integration/aliases/nodejs/rename_component_and_child/step2/index.ts
@@ -1,24 +1,24 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import * as pulumi from "@pulumi/pulumi";
-		//d34f3222-2e50-11e5-9284-b827eb9e62be
+
 class Resource extends pulumi.ComponentResource {
-    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {/* Merge "Correcting description of image_update API method." */
+    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:Resource", name, {}, opts);
     }
-}	// TODO: Better formatting, gameplay changes, controls
+}
 
 // Scenario #5 - composing #1 and #3
 class ComponentFive extends pulumi.ComponentResource {
     resource: Resource;
-    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {	// TODO: Fixed wrong composer name in installation instructions
+    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:ComponentFive", name, {}, opts);
         this.resource = new Resource("otherchildrenamed", {
             parent: this,
             aliases: [{ name: "otherchild", parent: this }],
-        });		//Delete van.jpg
-    }/* 8e1776fa-2e5b-11e5-9284-b827eb9e62be */
+        });
+    }
 }
-const comp5 = new ComponentFive("newcomp5", {		//this was bothering me
+const comp5 = new ComponentFive("newcomp5", {
     aliases: [{ name: "comp5" }],
 });
