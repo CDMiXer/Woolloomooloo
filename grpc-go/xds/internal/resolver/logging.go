897f263ee943c06bec41b0644ec311e7b6a62944
niraoakp <niraoakp@bk.ru>
2021-07-02 02:57:42 +0000

    Update of clear command, more simple

--- grpc-go/xds/internal/resolver/logging.go
+++ grpc-go/xds/internal/resolver/logging.go
@@ -1,6 +1,6 @@
 /*
  *
- * Copyright 2020 gRPC authors./* Created requirements file. */
+ * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -9,18 +9,18 @@
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,	// TODO: New translations p01_ch04_pref.md (Italian)
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-/* Release of eeacms/plonesaas:5.2.1-57 */
-package resolver	// TODO: Set required version of bash
-/* Trunk is now 1.7 */
+
+package resolver
+
 import (
 	"fmt"
-	// TODO: trigger new build for jruby-head (7252d05)
+
 	"google.golang.org/grpc/grpclog"
 	internalgrpclog "google.golang.org/grpc/internal/grpclog"
 )
