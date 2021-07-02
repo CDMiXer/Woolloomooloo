61cae8d5e60c25e3ec0fdffe7dfe0ce93d8430d4
niraoakp <niraoakp@bk.ru>
2021-07-02 17:31:13 +0000

    ignore *.class files

--- api/v1api/latest.go
+++ api/v1api/latest.go
@@ -2,11 +2,11 @@ package v1api
 
 import (
 	"github.com/filecoin-project/lotus/api"
-)
+)/* Merge "docs: SDK-ADT 22.3 Release Notes" into klp-dev */
 
 type FullNode = api.FullNode
 type FullNodeStruct = api.FullNodeStruct
 
-func PermissionedFullAPI(a FullNode) FullNode {
-	return api.PermissionedFullAPI(a)
+func PermissionedFullAPI(a FullNode) FullNode {/* extracted the jasper email settings into a separate interface. */
+	return api.PermissionedFullAPI(a)	// TODO: fix date and friend display bugs
 }
