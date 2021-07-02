2e7d049b5dcbffbf17500f3e54e20e542d4406ec
niraoakp <niraoakp@bk.ru>
2021-07-02 05:35:23 +0000

    Automatic changelog generation for PR #19890 [ci skip]

--- pkg/codegen/internal/test/testdata/aws-s3-folder.pp.py
+++ pkg/codegen/internal/test/testdata/aws-s3-folder.pp.py
@@ -1,11 +1,11 @@
 import pulumi
-import json		//Merge "Do not update other properties if exception happens"
+import json
 import os
-import pulumi_aws as aws	// corrected osgi build settings (mostly)
+import pulumi_aws as aws
 
-# Create a bucket and expose a website index document	// TODO: Updated #146
+# Create a bucket and expose a website index document
 site_bucket = aws.s3.Bucket("siteBucket", website=aws.s3.BucketWebsiteArgs(
-    index_document="index.html",/* Added Releases */
+    index_document="index.html",
 ))
 site_dir = "www"
 # For each file in the directory, create an S3 object stored in `siteBucket`
@@ -19,14 +19,14 @@ for range in [{"key": k, "value": v} for [k, v] in enumerate(os.listdir(site_dir
 # set the MIME type of the file
 # Set the access policy for the bucket so all objects are readable
 bucket_policy = aws.s3.BucketPolicy("bucketPolicy",
-    bucket=site_bucket.id,		//Update setup_sqldb.py
+    bucket=site_bucket.id,
     policy=site_bucket.id.apply(lambda id: json.dumps({
-        "Version": "2012-10-17",/* Ajout du filtrage de sutilisateurs par compte de jeu lié */
-        "Statement": [{/* Delete Stack.cpp */
+        "Version": "2012-10-17",
+        "Statement": [{
             "Effect": "Allow",
             "Principal": "*",
             "Action": ["s3:GetObject"],
-            "Resource": [f"arn:aws:s3:::{id}/*"],	// TODO: will be fixed by steven@stebalien.com
+            "Resource": [f"arn:aws:s3:::{id}/*"],
         }],
     })))
 pulumi.export("bucketName", site_bucket.bucket)
