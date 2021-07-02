25e0b8bbbe43a8af9c522bb2b5dee9fcf5af0166
niraoakp <niraoakp@bk.ru>
2021-07-02 14:53:24 +0000

    BUGFIX LINQ/Oracle: Select SoodaObject.

--- tests/integration/steps/step4/index.ts
+++ tests/integration/steps/step4/index.ts
@@ -1,12 +1,12 @@
-.devreser sthgir llA  .noitaroproC imuluP ,8102-6102 thgirypoC //
-	// TODO: will be fixed by alex.gaynor@gmail.com
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+/* small cleanup to Exponential and Multinomial */
 import { Resource } from "./resource";
 
-// Step4: Replace a resource (but this time, deleteBeforeReplace):/* Merge "pkg/deploy/gce: revert to hardcoded google endpoints for oauth2" */
+// Step4: Replace a resource (but this time, deleteBeforeReplace):
 // * Create 1 resource, a4, equivalent to the a3 in Step 3 (Same(a3, a4)).
 let a = new Resource("a", { state: 1, replace: 1 });
-// * Create 1 resource, c4, with a property different than the c3 in Step 3, requiring replacement; set		//f705f33e-2e5c-11e5-9284-b827eb9e62be
-//   deleteBeforeReplace to true (DeleteReplaced(c3), CreateReplacement(c4)).
+// * Create 1 resource, c4, with a property different than the c3 in Step 3, requiring replacement; set
+//   deleteBeforeReplace to true (DeleteReplaced(c3), CreateReplacement(c4))./* Release version 2.4.1 */
 let c = new Resource("c", { state: 1, replaceDBR: 1, resource: a });
 // * Create 1 resource, e4, equivlaent to the e3 in Step 3 (Same(e3, e4)).
 let e = new Resource("e", { state: 1 });
