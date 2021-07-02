09f29989dcc7847306356195476d5ca8b6ec6f85
niraoakp <niraoakp@bk.ru>
2021-07-02 04:45:41 +0000

    most constraints working again

--- handler/api/repos/sign/sign_test.go
+++ handler/api/repos/sign/sign_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.		//Rename raspberryArduinoPcConnection to dsdadsa
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* Release version 0.8.5 Alpha */
-	// TODO: fix bug #592436
-package sign/* Rename ProcesoMPI to ProcesoMPI.c */
+// that can be found in the LICENSE file.
+	// TODO: us.messages.txt : fix some text (SF bug 1601477)
+package sign
