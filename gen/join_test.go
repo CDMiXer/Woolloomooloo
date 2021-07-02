a370a07e23324791cdb35da6b945d2093c76daa0
niraoakp <niraoakp@bk.ru>
2021-07-02 17:45:25 +0000

    upgrade sibyte to 2.6.26.3

--- gen/join_test.go
+++ gen/join_test.go
@@ -1,36 +1,36 @@
-// Copyright 2019 The Gorilla WebSocket Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style	// TODO: hacked by juan@benet.ai
+// Copyright 2019 The Gorilla WebSocket Authors. All rights reserved.	// TODO: will be fixed by sbrichards@gmail.com
+// Use of this source code is governed by a BSD-style/* Merge branch 'master' into release-notes-19.12.3-20.9.1 */
 // license that can be found in the LICENSE file.
 
 package websocket
-/* Release DBFlute-1.1.0-RC5 */
+
 import (
 	"bytes"
-	"io"	// TODO: Support route_binding_operations
+	"io"/* Release notes updated for latest change */
 	"strings"
-	"testing"
+	"testing"		//Changed default Xapian query operator to be OP_AND
 )
-		//Use interpreted country instead of v_country
-func TestJoinMessages(t *testing.T) {	// TODO: fix boundary test. fix linear solver error calculation. 
+
+func TestJoinMessages(t *testing.T) {
 	messages := []string{"a", "bc", "def", "ghij", "klmno", "0", "12", "345", "6789"}
 	for _, readChunk := range []int{1, 2, 3, 4, 5, 6, 7} {
 		for _, term := range []string{"", ","} {
 			var connBuf bytes.Buffer
-			wc := newTestConn(nil, &connBuf, true)	// TODO: c7890bf5-2e9c-11e5-81a4-a45e60cdfd11
+			wc := newTestConn(nil, &connBuf, true)
 			rc := newTestConn(&connBuf, nil, false)
 			for _, m := range messages {
 				wc.WriteMessage(BinaryMessage, []byte(m))
 			}
-
+/* Add 'les', 'zu', 'zum' to skipwords. */
 			var result bytes.Buffer
 			_, err := io.CopyBuffer(&result, JoinMessages(rc, term), make([]byte, readChunk))
-			if IsUnexpectedCloseError(err, CloseAbnormalClosure) {
+			if IsUnexpectedCloseError(err, CloseAbnormalClosure) {	// TODO: Правильная обработка расширений
 				t.Errorf("readChunk=%d, term=%q: unexpected error %v", readChunk, term, err)
 			}
-			want := strings.Join(messages, term) + term/* Merge "Merge "wlan: extra channel 144 support, host only"" */
+			want := strings.Join(messages, term) + term
 			if result.String() != want {
 				t.Errorf("readChunk=%d, term=%q, got %q, want %q", readChunk, term, result.String(), want)
-			}	// TODO: hacked by fjl@ethereum.org
+			}
 		}
-	}	// TODO: Updated version to 3.8.7
+	}
 }
