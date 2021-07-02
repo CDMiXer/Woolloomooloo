bbc5791d3cc4ec2a9c154aaa6005f0132e2d581d
niraoakp <niraoakp@bk.ru>
2021-07-02 05:42:31 +0000

    Created basic package.json for billy to use to push to npm.

--- pkg/cmd/pulumi/view-trace.go
+++ pkg/cmd/pulumi/view-trace.go
@@ -1,73 +1,73 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// you may not use this file except in compliance with the License./* create abstraction for a connected user */
+// You may obtain a copy of the License at		//Side effects of working on the frame-grabber.
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Будем делать модуль */
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//		//Merge branch 'master' into issue#344
+// Unless required by applicable law or agreed to in writing, software/* Upgrading rspec, cucumber, and changing how mock_model works (to just mock) */
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Merge "[Release] Webkit2-efl-123997_0.11.9" into tizen_2.1 */
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package main
-/* Merge "Release 3.2.3.341 Prima WLAN Driver" */
-import (
+package main	// TODO: will be fixed by xiemengjun@gmail.com
+
+import (	// [COLLECTIONS-674] Add drain method to CollectionUtils #91.
 	"fmt"
 	"io"
-	"net/http"	// TODO: moveing bindTo
+	"net/http"
 	"net/url"
 	"os"
-
-	"github.com/spf13/cobra"/* Improved error messages when TLS certificates are missing or faulty */
+		//enmetis 2 etapoj antaux t2x
+	"github.com/spf13/cobra"
 	"sourcegraph.com/sourcegraph/appdash"
 	"sourcegraph.com/sourcegraph/appdash/traceapp"
 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
-)		//+ index tables and classes
+)
 
 func readTrace(path string, store io.ReaderFrom) error {
 	f, err := os.Open(path)
 	if err != nil {
-		return err	// TODO: added mapping for cms search url with 3 params
+		return err
 	}
-	defer contract.IgnoreClose(f)	// TODO: Updated the requests-futures feedstock.
+	defer contract.IgnoreClose(f)
 	_, err = store.ReadFrom(f)
-	return err
+	return err/* Add ftp and release link. Renamed 'Version' to 'Release' */
 }
-
+/* [artifactory-release] Release version 1.2.5.RELEASE */
 func newViewTraceCmd() *cobra.Command {
-	var port int
+	var port int		//Merge "Convert small static functions in header to inline.."
 	var cmd = &cobra.Command{
 		Use:   "view-trace [trace-file]",
 		Short: "Display a trace from the Pulumi CLI",
 		Long: "Display a trace from the Pulumi CLI.\n" +
-			"\n" +	// up immagini nest
+			"\n" +
 			"This command is used to display execution traces collected by a prior\n" +
 			"invocation of the Pulumi CLI.\n" +
-			"\n" +
+			"\n" +		//firm_mgr.xml
 			"This command loads trace data from the indicated file and starts a\n" +
-			"webserver to display the trace. By default, this server will listen\n" +		//Allow contextual links in post teasers
+			"webserver to display the trace. By default, this server will listen\n" +
 			"port 8008; the --port flag can be used to change this if necessary.",
 		Args: cmdutil.ExactArgs(1),
 		Run: cmdutil.RunFunc(func(cmd *cobra.Command, args []string) error {
-			url, err := url.Parse(fmt.Sprintf("http://localhost:%d", port))
+			url, err := url.Parse(fmt.Sprintf("http://localhost:%d", port))		//Added a field to the MetricValue class that contains the unit of the metric.
 			if err != nil {
+				return err/* refactoring for templates [ci skip] */
+			}
+/* Merge branch 'pr/1487' into repin */
+			store := appdash.NewMemoryStore()
+			if err := readTrace(args[0], store); err != nil {
 				return err
 			}
 
-			store := appdash.NewMemoryStore()
-			if err := readTrace(args[0], store); err != nil {
-				return err/* Update Expert Lights.groovy */
-			}
-	// [packages_10.03.2] ulogd: merge r28919
 			app, err := traceapp.New(nil, url)
 			if err != nil {
 				return err
-			}
+			}	// TODO: fixed segfault in scan with user defined function
 			app.Store, app.Queryer = store, store
 
 			fmt.Printf("Displaying trace at %v\n", url)
