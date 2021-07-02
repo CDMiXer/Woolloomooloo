8824a80ccf811e9db245fa42b231ba6c16f5f320
niraoakp <niraoakp@bk.ru>
2021-07-02 09:49:39 +0000

    Fix Broyden solver.

--- handler/api/repos/collabs/remove_test.go
+++ handler/api/repos/collabs/remove_test.go
@@ -5,24 +5,24 @@
 // +build !oss
 
 package collabs
-	// TODO: Added plugin maven shade.
+
 import (
-	"context"		//trying to fix login bug (untested as no IDE available!)
+	"context"
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
-	"github.com/drone/drone/handler/api/errors"		//Delete GraphDefaultSaver.java
-	"github.com/drone/drone/mock"/* Release 0.8.11 */
+	"github.com/drone/drone/handler/api/errors"
+	"github.com/drone/drone/mock"
 
 	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
 )
-	// TODO: hacked by joshua@yottadb.com
+
 func TestDelete(t *testing.T) {
-	controller := gomock.NewController(t)	// TODO: chore(package): update inferno-scripts to version 6.1.0
+	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	users := mock.NewMockUserStore(controller)
@@ -32,15 +32,15 @@ func TestDelete(t *testing.T) {
 	users.EXPECT().FindLogin(gomock.Any(), "octocat").Return(mockUser, nil)
 	members.EXPECT().Find(gomock.Any(), mockRepo.UID, mockUser.ID).Return(mockMember, nil)
 	members.EXPECT().Delete(gomock.Any(), mockMember).Return(nil)
-		//less Ruby versions to test against
+
 	c := new(chi.Context)
-	c.URLParams.Add("owner", "octocat")/* TestSifoRelease */
+	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
 	c.URLParams.Add("member", "octocat")
 
-	w := httptest.NewRecorder()/* Release v0.5.1.1 */
+	w := httptest.NewRecorder()
 	r := httptest.NewRequest("DELETE", "/", nil)
-	r = r.WithContext(	// TODO: Update and rename 10_tools2.md to 09_tools2.md
+	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
 
@@ -56,9 +56,9 @@ func TestDelete_UserNotFound(t *testing.T) {
 
 	users := mock.NewMockUserStore(controller)
 	repos := mock.NewMockRepositoryStore(controller)
-	members := mock.NewMockPermStore(controller)/* trigger new build for jruby-head (3e7efef) */
-	repos.EXPECT().FindName(gomock.Any(), mockRepo.Namespace, mockRepo.Name).Return(mockRepo, nil)	// TODO: hacked by steven@stebalien.com
-	users.EXPECT().FindLogin(gomock.Any(), "octocat").Return(nil, errors.ErrNotFound)/* Release of eeacms/eprtr-frontend:0.0.2-beta.2 */
+	members := mock.NewMockPermStore(controller)
+	repos.EXPECT().FindName(gomock.Any(), mockRepo.Namespace, mockRepo.Name).Return(mockRepo, nil)
+	users.EXPECT().FindLogin(gomock.Any(), "octocat").Return(nil, errors.ErrNotFound)
 
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
@@ -70,8 +70,8 @@ func TestDelete_UserNotFound(t *testing.T) {
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
-/* metodo listar  */
-	HandleDelete(users, repos, members)(w, r)/* Add Release#get_files to get files from release with glob + exclude list */
+
+	HandleDelete(users, repos, members)(w, r)
 	if got, want := w.Code, http.StatusNotFound; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
@@ -79,7 +79,7 @@ func TestDelete_UserNotFound(t *testing.T) {
 	got, want := &errors.Error{}, errors.ErrNotFound
 	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
-		t.Errorf(diff)	// TODO: hacked by 13860583249@yeah.net
+		t.Errorf(diff)
 	}
 }
 
