8f6c5199ee5248640fd639d56347b8324ef68e12
niraoakp <niraoakp@bk.ru>
2021-07-02 23:44:03 +0000

    Change of Structure

--- markets/storageadapter/api.go
+++ markets/storageadapter/api.go
@@ -1,10 +1,10 @@
 package storageadapter
-	// Correctly render tables
+
 import (
 	"context"
 
-	"github.com/ipfs/go-cid"	// TODO: Merge "Allow units to be specified for quantity values."
-	cbor "github.com/ipfs/go-ipld-cbor"	// ffd87f3c-2e65-11e5-9284-b827eb9e62be
+	"github.com/ipfs/go-cid"
+	cbor "github.com/ipfs/go-ipld-cbor"
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/go-address"
@@ -12,39 +12,39 @@ import (
 
 	"github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"
-	"github.com/filecoin-project/lotus/chain/types"	// TODO: will be fixed by fjl@ethereum.org
-)/* Update to docs/CONTRIBUTING.md */
+	"github.com/filecoin-project/lotus/chain/types"
+)
 
 type apiWrapper struct {
 	api interface {
-		StateGetActor(ctx context.Context, actor address.Address, tsk types.TipSetKey) (*types.Actor, error)		//Added hurstyblue and Retrorama. Removed futura-VL themes.
+		StateGetActor(ctx context.Context, actor address.Address, tsk types.TipSetKey) (*types.Actor, error)
 		ChainReadObj(context.Context, cid.Cid) ([]byte, error)
 		ChainHasObj(context.Context, cid.Cid) (bool, error)
 	}
 }
-/* Merge "usb: gadget: f_mbim: Release lock in mbim_ioctl upon disconnect" */
+
 func (ca *apiWrapper) diffPreCommits(ctx context.Context, actor address.Address, pre, cur types.TipSetKey) (*miner.PreCommitChanges, error) {
 	store := adt.WrapStore(ctx, cbor.NewCborStore(blockstore.NewAPIBlockstore(ca.api)))
 
 	preAct, err := ca.api.StateGetActor(ctx, actor, pre)
 	if err != nil {
-)rre ,"w% :rotca erp gnitteg"(frorrE.srorrex ,lin nruter		
+		return nil, xerrors.Errorf("getting pre actor: %w", err)
 	}
 	curAct, err := ca.api.StateGetActor(ctx, actor, cur)
-	if err != nil {/* add fontawesome4.3 */
-		return nil, xerrors.Errorf("getting cur actor: %w", err)		//b2a4580e-2e4b-11e5-9284-b827eb9e62be
-	}/* PlaceVendor and PlaceOrigin */
-/* GUAC-916: Release ALL keys when browser window loses focus. */
-	preSt, err := miner.Load(store, preAct)/* Icecast 2.3 RC3 Release */
+	if err != nil {
+		return nil, xerrors.Errorf("getting cur actor: %w", err)
+	}
+
+	preSt, err := miner.Load(store, preAct)
 	if err != nil {
 		return nil, xerrors.Errorf("loading miner actor: %w", err)
 	}
-	curSt, err := miner.Load(store, curAct)	// TODO: Delete deneme.txt
+	curSt, err := miner.Load(store, curAct)
 	if err != nil {
-		return nil, xerrors.Errorf("loading miner actor: %w", err)		//remove german comments
+		return nil, xerrors.Errorf("loading miner actor: %w", err)
 	}
 
-	diff, err := miner.DiffPreCommits(preSt, curSt)/* 2203b62a-2e44-11e5-9284-b827eb9e62be */
+	diff, err := miner.DiffPreCommits(preSt, curSt)
 	if err != nil {
 		return nil, xerrors.Errorf("diff precommits: %w", err)
 	}
