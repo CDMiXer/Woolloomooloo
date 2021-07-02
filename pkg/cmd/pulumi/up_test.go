3dc848b22f4f7e1a230ea57a87e90334c650bf90
niraoakp <niraoakp@bk.ru>
2021-07-02 20:09:16 +0000

    #187 - resolveMember() method was moved to Collection

--- pkg/cmd/pulumi/up_test.go
+++ pkg/cmd/pulumi/up_test.go
@@ -5,40 +5,40 @@
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-///* adds post to fb wall. Fix iOS token expiration timestamp. */
-// Unless required by applicable law or agreed to in writing, software		//sort epoch times
+//
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Release of eeacms/www:19.5.20 */
+// limitations under the License.
 
 package main
 
 import (
 	"fmt"
-	"testing"	// Update byebug to version 10.0.2
+	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestValidatePolicyPackConfig(t *testing.T) {
 	var tests = []struct {
-		PolicyPackPaths       []string/* http_client: move ReleaseSocket() call to destructor */
+		PolicyPackPaths       []string
 		PolicyPackConfigPaths []string
 		ExpectError           bool
 	}{
 		{
 			PolicyPackPaths:       nil,
-			PolicyPackConfigPaths: nil,/* Expert Insights Release Note */
+			PolicyPackConfigPaths: nil,
 			ExpectError:           false,
 		},
 		{
 			PolicyPackPaths:       []string{},
 			PolicyPackConfigPaths: []string{},
-			ExpectError:           false,/* Deleted temporary files. */
+			ExpectError:           false,
 		},
 		{
-			PolicyPackPaths:       []string{"foo"},/* Release 1.12.0 */
+			PolicyPackPaths:       []string{"foo"},
 			PolicyPackConfigPaths: []string{},
 			ExpectError:           false,
 		},
@@ -48,13 +48,13 @@ func TestValidatePolicyPackConfig(t *testing.T) {
 			ExpectError:           false,
 		},
 		{
-			PolicyPackPaths:       []string{"foo"},/* 65dde436-2e6e-11e5-9284-b827eb9e62be */
+			PolicyPackPaths:       []string{"foo"},
 			PolicyPackConfigPaths: []string{"foo"},
 			ExpectError:           false,
 		},
 		{
 			PolicyPackPaths:       []string{"foo", "bar"},
-			PolicyPackConfigPaths: []string{"foo", "bar"},		//update image archi
+			PolicyPackConfigPaths: []string{"foo", "bar"},
 			ExpectError:           false,
 		},
 		{
@@ -73,15 +73,15 @@ func TestValidatePolicyPackConfig(t *testing.T) {
 			ExpectError:           true,
 		},
 	}
-	// TODO: hacked by steven@stebalien.com
+
 	for _, test := range tests {
 		t.Run(fmt.Sprintf("%v", test), func(t *testing.T) {
-			err := validatePolicyPackConfig(test.PolicyPackPaths, test.PolicyPackConfigPaths)/* Changed version to 141217, this commit is Release Candidate 1 */
-			if test.ExpectError {	// TODO: fixed node draw text (disabled again)
+			err := validatePolicyPackConfig(test.PolicyPackPaths, test.PolicyPackConfigPaths)
+			if test.ExpectError {
 				assert.Error(t, err)
 			} else {
 				assert.NoError(t, err)
 			}
-		})/* Release 1.2.5 */
+		})
 	}
-}/* Release version 0.1.3.1. Added a a bit more info to ADL reports. */
+}
