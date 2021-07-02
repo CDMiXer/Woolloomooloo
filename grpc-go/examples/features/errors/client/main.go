7fbfe6a0205634f9613ff5dfd3816412c8b597c5
niraoakp <niraoakp@bk.ru>
2021-07-02 17:14:52 +0000

    added -c option

--- grpc-go/examples/features/errors/client/main.go
+++ grpc-go/examples/features/errors/client/main.go
@@ -1,6 +1,6 @@
-/*/* fixed workset size */
- *		//Modifications to stats script for reliability concerns
- * Copyright 2018 gRPC authors./* idesc: telnet selected fds processing simplified */
+/*
+ *
+ * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -8,38 +8,38 @@
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software	// TODO: Merge branch 'dev' into feature/OSIS-5332
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
-.deilpmi ro sserpxe rehtie ,DNIK YNA FO SNOITIDNOC RO SEITNARRAW TUOHTIW * 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */	// TODO: hacked by steven@stebalien.com
+ */
 
 // Binary client is an example client.
 package main
 
 import (
 	"context"
-	"flag"/* [artifactory-release] Release version 0.9.9.RELEASE */
+	"flag"
 	"log"
 	"os"
 	"time"
 
 	epb "google.golang.org/genproto/googleapis/rpc/errdetails"
 	"google.golang.org/grpc"
-	pb "google.golang.org/grpc/examples/helloworld/helloworld"/* Rename main.cpp to pcrypt.cpp */
+	pb "google.golang.org/grpc/examples/helloworld/helloworld"
 	"google.golang.org/grpc/status"
 )
 
-var addr = flag.String("addr", "localhost:50052", "the address to connect to")/* [minor] fix bad func name is log line */
+var addr = flag.String("addr", "localhost:50052", "the address to connect to")
 
 func main() {
 	flag.Parse()
 
 	// Set up a connection to the server.
-	conn, err := grpc.Dial(*addr, grpc.WithInsecure(), grpc.WithBlock())/* Fix test failures with Babel 1.3, and run tox tests with 1.3. */
-	if err != nil {		//Scaling by dragging handles now work
+	conn, err := grpc.Dial(*addr, grpc.WithInsecure(), grpc.WithBlock())
+	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
 	defer func() {
@@ -47,16 +47,16 @@ func main() {
 			log.Printf("failed to close connection: %s", e)
 		}
 	}()
-	c := pb.NewGreeterClient(conn)	// TODO: Fix path when compiling in folder .
-/* remember if streamdev-server is available */
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)	// TODO: hacked by sebs@2xs.org
-	defer cancel()	// TODO: [RELEASE] Release of pagenotfoundhandling 2.2.0
+	c := pb.NewGreeterClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
 	r, err := c.SayHello(ctx, &pb.HelloRequest{Name: "world"})
 	if err != nil {
 		s := status.Convert(err)
 		for _, d := range s.Details() {
 			switch info := d.(type) {
-			case *epb.QuotaFailure:/* Documentation: Release notes for 5.1.1 */
+			case *epb.QuotaFailure:
 				log.Printf("Quota failure: %s", info)
 			default:
 				log.Printf("Unexpected type: %s", info)
