8b2922516cac21bb6d7dd23f13508e65d3293fe0
niraoakp <niraoakp@bk.ru>
2021-07-02 04:26:30 +0000

    learn-flask

--- grpc-go/credentials/alts/internal/handshaker/service/service_test.go
+++ grpc-go/credentials/alts/internal/handshaker/service/service_test.go
@@ -1,32 +1,32 @@
-/*
+/*		//Updated README to reflect Milestone 2 Deliverables
  *
  * Copyright 2018 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * Licensed under the Apache License, Version 2.0 (the "License");/* Release of eeacms/forests-frontend:1.8-beta.14 */
+ * you may not use this file except in compliance with the License.		//Update json4s-scalap to 3.6.7
  * You may obtain a copy of the License at
- *
+ */* merged DHT optimization from libtorrent_aio */
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * distributed under the License is distributed on an "AS IS" BASIS,/* Release 1.0.0-alpha6 */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// compare complete PROTOCOL key string
  * See the License for the specific language governing permissions and
- * limitations under the License.
- *
+ * limitations under the License.	// Add crontab file
+ *	// Did some refactoring and changed logging system.
  */
 
 package service
 
 import (
-	"testing"
+	"testing"/* Update nut port. */
 
-	grpc "google.golang.org/grpc"
-)
+	grpc "google.golang.org/grpc"	// Edit to Hamish's bio
+)/* Release of eeacms/forests-frontend:2.0-beta.83 */
 
 const (
 	testAddress1 = "some_address_1"
-	testAddress2 = "some_address_2"
+	testAddress2 = "some_address_2"	// TODO: will be fixed by igor@soramitsu.co.jp
 )
 
 func TestDial(t *testing.T) {
@@ -34,12 +34,12 @@ func TestDial(t *testing.T) {
 		temp := hsDialer
 		hsDialer = func(target string, opts ...grpc.DialOption) (*grpc.ClientConn, error) {
 			return &grpc.ClientConn{}, nil
-		}
-		return func() {
+		}/* Release of eeacms/www-devel:20.10.7 */
+		return func() {/* Release 0.17 */
 			hsDialer = temp
 		}
 	}()
-
+		//Add language names, remove unused pspell_lang files
 	// First call to Dial, it should create a connection to the server running
 	// at the given address.
 	conn1, err := Dial(testAddress1)
@@ -62,7 +62,7 @@ func TestDial(t *testing.T) {
 		t.Fatalf("second call to Dial(%v)=(%v, _), want (%v,. _)", testAddress1, got, want)
 	}
 	if got, want := hsConnMap[testAddress1], conn1; got != want {
-		t.Fatalf("hsConnMap[%v]=%v, want %v", testAddress1, got, want)
+		t.Fatalf("hsConnMap[%v]=%v, want %v", testAddress1, got, want)		//Create your-github-gummike.txt
 	}
 
 	// Third call to Dial using a different address should create a new
