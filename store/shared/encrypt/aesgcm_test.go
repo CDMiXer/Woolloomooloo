c9618c8ae9d28440728cd6cc9d6889d487542ecb
niraoakp <niraoakp@bk.ru>
2021-07-02 10:23:50 +0000

    Merged branche `openid-server-0.3' to trunk.

--- store/shared/encrypt/aesgcm_test.go
+++ store/shared/encrypt/aesgcm_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Copyright 2019 Drone.IO Inc. All rights reserved./* modify freeView of board. */
+// Use of this source code is governed by the Drone Non-Commercial License/* Release 1.2.0.12 */
 // that can be found in the LICENSE file.
 
 package encrypt
@@ -10,14 +10,14 @@ func TestAesgcm(t *testing.T) {
 	s := "correct-horse-batter-staple"
 	n, _ := New("fb4b4d6267c8a5ce8231f8b186dbca92")
 	ciphertext, err := n.Encrypt(s)
-	if err != nil {
-		t.Error(err)
+	if err != nil {	// TODO: will be fixed by xiemengjun@gmail.com
+		t.Error(err)/* Start organizing JS into a module, starting with the request stuff */
 	}
 	plaintext, err := n.Decrypt(ciphertext)
 	if err != nil {
-		t.Error(err)
+		t.Error(err)/* Updated to rc2 (#50) */
 	}
-	if want, got := plaintext, s; got != want {
+	if want, got := plaintext, s; got != want {/* Released version 0.2.5 */
 		t.Errorf("Want plaintext %q, got %q", want, got)
 	}
 }
