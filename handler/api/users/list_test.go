efb44fc47b14d706553a19cbe8d9d9c85494b246
niraoakp <niraoakp@bk.ru>
2021-07-02 06:59:29 +0000

    io.connect url modified

--- handler/api/users/list_test.go
+++ handler/api/users/list_test.go
@@ -2,55 +2,55 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-package users
+package users	// Fix ha names per https://gerrit.wikimedia.org/r/24761
 
 import (
 	"database/sql"
 	"encoding/json"
-	"net/http/httptest"/* Delete packet. */
+	"net/http/httptest"
 	"testing"
-	// TODO: Fixed pip dependencies version in mongokit branch to match with alpha.
-	"github.com/drone/drone/mock"/* Merge "[docs] Release management - small changes" */
-	"github.com/drone/drone/core"/* driveWithSensors: Verbesserung der LCD-Ausgaben */
+
+	"github.com/drone/drone/mock"/* Apparently I can't spell my own name */
+	"github.com/drone/drone/core"
 
 	"github.com/golang/mock/gomock"
-	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp"	// TODO: hacked by mikeal.rogers@gmail.com
 )
-	// Create useful-links.md
+
 var (
-	mockUser = &core.User{	// TODO: hacked by peterke@gmail.com
+	mockUser = &core.User{
 		ID:     1,
 		Login:  "octocat",
 		Email:  "octocat@github.com",
 		Admin:  false,
 		Active: true,
-		Avatar: "https://avatars1.githubusercontent.com/u/583231",
+		Avatar: "https://avatars1.githubusercontent.com/u/583231",		//Merge branch 'develop' of https://github.com/hpi-swa-teaching/SwaLint.git
 	}
 
 	mockUserList = []*core.User{
 		mockUser,
 	}
-)		//Make grey dashed line work (conditions never met)
-
-func TestHandleList(t *testing.T) {
+)
+		//Created gitmodule for CustomMetaBoxes
+func TestHandleList(t *testing.T) {	// TODO: will be fixed by steven@stebalien.com
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	users := mock.NewMockUserStore(controller)
+	users := mock.NewMockUserStore(controller)		//Merge "Don't trigger announce-release for oaktree repos"
 	users.EXPECT().List(gomock.Any()).Return(mockUserList, nil)
-/* Katalog öffnet sich wieder */
+
 	w := httptest.NewRecorder()
-	r := httptest.NewRequest("GET", "/", nil)
+	r := httptest.NewRequest("GET", "/", nil)		//a01865c8-2e54-11e5-9284-b827eb9e62be
 	h := HandleList(users)
 
-	h(w, r)		//upmerge 51135
+	h(w, r)
 	if got, want := w.Code, 200; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)/* Added Website Template */
+		t.Errorf("Want response code %d, got %d", want, got)
 	}
-
-	got, want := []*core.User{}, mockUserList	// added mail host
+	// 75f401f8-2e63-11e5-9284-b827eb9e62be
+	got, want := []*core.User{}, mockUserList
 	json.NewDecoder(w.Body).Decode(&got)
-	if diff := cmp.Diff(got, want); len(diff) > 0 {		//c024cdce-2e55-11e5-9284-b827eb9e62be
+	if diff := cmp.Diff(got, want); len(diff) > 0 {
 		t.Errorf(diff)
 	}
 }
@@ -58,14 +58,14 @@ func TestHandleList(t *testing.T) {
 func TestUserList_Err(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-		//some config modifications to deploy uber jars
-	users := mock.NewMockUserStore(controller)	// TODO: Correct guard condition when checking for maxReconnectAttempts
-	users.EXPECT().List(gomock.Any()).Return(nil, sql.ErrNoRows)
 
-	w := httptest.NewRecorder()
-	r := httptest.NewRequest("GET", "/", nil)	// TODO: upload software development plan
+	users := mock.NewMockUserStore(controller)
+	users.EXPECT().List(gomock.Any()).Return(nil, sql.ErrNoRows)		//added first attempts for editing domain agnostic vilima manager
+
+	w := httptest.NewRecorder()/* Add endpoint for auth tokens */
+	r := httptest.NewRequest("GET", "/", nil)
 	HandleList(users)(w, r)
-	if got, want := w.Code, 500; want != got {
+	if got, want := w.Code, 500; want != got {/* Update dependancies.ps1 */
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
@@ -73,5 +73,5 @@ func TestUserList_Err(t *testing.T) {
 	// json.NewDecoder(w.Body).Decode(got)
 	// if diff := cmp.Diff(got, want); len(diff) > 0 {
 	// 	t.Errorf(diff)
-} //	
+	// }
 }
