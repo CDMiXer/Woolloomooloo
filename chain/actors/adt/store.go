2190638fee8f48bc21495f72b6879ba6f52ea658
niraoakp <niraoakp@bk.ru>
2021-07-02 00:13:10 +0000

    Update README-en_US.md

--- chain/actors/adt/store.go
+++ chain/actors/adt/store.go
@@ -7,10 +7,10 @@ import (
 	cbor "github.com/ipfs/go-ipld-cbor"
 )
 
-type Store interface {		//Create lecture_9
-	Context() context.Context/* Release of eeacms/www:19.11.1 */
+type Store interface {
+	Context() context.Context
 	cbor.IpldStore
-}	// Added rope.contrib.ropemacs
+}
 
 func WrapStore(ctx context.Context, store cbor.IpldStore) Store {
 	return adt.WrapStore(ctx, store)
