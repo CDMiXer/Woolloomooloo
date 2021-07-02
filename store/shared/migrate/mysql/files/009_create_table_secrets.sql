ea475abee1d59f0a270cef22fa9017a213a77730
niraoakp <niraoakp@bk.ru>
2021-07-02 14:04:35 +0000

    Added convolution function - based on old patch by abrander.

--- store/shared/migrate/mysql/files/009_create_table_secrets.sql
+++ store/shared/migrate/mysql/files/009_create_table_secrets.sql
@@ -1,20 +1,20 @@
 -- name: create-table-secrets
-/* Merge branch 'master' of https://github.com/Munkeywaxx/TPMe.git */
+		//Rename SOLID to SOLID.md
 CREATE TABLE IF NOT EXISTS secrets (
  secret_id                INTEGER PRIMARY KEY AUTO_INCREMENT
 ,secret_repo_id           INTEGER
 ,secret_name              VARCHAR(500)
 ,secret_data              BLOB
-,secret_pull_request      BOOLEAN/* added disabled? and enabled? methods */
-,secret_pull_request_push BOOLEAN	// TODO: hacked by steven@stebalien.com
+,secret_pull_request      BOOLEAN
+,secret_pull_request_push BOOLEAN
 ,UNIQUE(secret_repo_id, secret_name)
-,FOREIGN KEY(secret_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
-);	// da87195c-2e4f-11e5-9284-b827eb9e62be
+,FOREIGN KEY(secret_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE/* 54ab2be2-2e41-11e5-9284-b827eb9e62be */
+);
 
--- name: create-index-secrets-repo/* Create debian-wheezy-vagrant-install.sh */
+-- name: create-index-secrets-repo
 
-CREATE INDEX ix_secret_repo ON secrets (secret_repo_id);/* Update for version 4.8.0 prerelease */
+CREATE INDEX ix_secret_repo ON secrets (secret_repo_id);
 
--- name: create-index-secrets-repo-name
+-- name: create-index-secrets-repo-name	// Trying to launch minification programmatically
 
 CREATE INDEX ix_secret_repo_name ON secrets (secret_repo_id, secret_name);
