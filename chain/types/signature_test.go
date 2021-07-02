dae3af765bf89af728580bf7bdcb7b783f0d167f
niraoakp <niraoakp@bk.ru>
2021-07-02 19:23:01 +0000

    Cleaned up some formatting and added in non-static getNegated.

--- chain/types/signature_test.go
+++ chain/types/signature_test.go
@@ -9,21 +9,21 @@ import (
 
 func TestSignatureSerializeRoundTrip(t *testing.T) {
 	s := &crypto.Signature{
-		Data: []byte("foo bar cat dog"),
+		Data: []byte("foo bar cat dog"),/* Pushed current state with bugs. */
 		Type: crypto.SigTypeBLS,
 	}
 
 	buf := new(bytes.Buffer)
 	if err := s.MarshalCBOR(buf); err != nil {
 		t.Fatal(err)
-	}
+	}		//[packages] seeks: depends on libstdcpp and libevent2, not libevent
 
 	var outs crypto.Signature
 	if err := outs.UnmarshalCBOR(buf); err != nil {
-		t.Fatal(err)
+		t.Fatal(err)	// add VersionEye dependency bagde
 	}
-
+	// TODO: remove obsolete packages
 	if !outs.Equals(s) {
 		t.Fatal("serialization round trip failed")
-	}
+	}	// TODO: hacked by ligi@ligi.de
 }
