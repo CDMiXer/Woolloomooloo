6d71058f7d6de40fd2d2fd5f378cf3d4ef643faa
niraoakp <niraoakp@bk.ru>
2021-07-02 19:56:42 +0000

    fixed skipping to a specific position

--- pkg/codegen/internal/test/testdata/simple-enum-schema/nodejs/types/enums/tree/index.ts
+++ pkg/codegen/internal/test/testdata/simple-enum-schema/nodejs/types/enums/tree/index.ts
@@ -2,8 +2,8 @@
 // *** Do not edit by hand unless you're certain you know what you are doing! ***
 
 // Export sub-modules:
-import * as v1 from "./v1";/* discovery: remove unused "base" argument from find.*incoming() */
-		//Merge "ml2 v1 driver: work around full_sync"
+import * as v1 from "./v1";
+
 export {
     v1,
 };
