131a79744ca430c80b620c7e019c8e8c7e942c13
niraoakp <niraoakp@bk.ru>
2021-07-03 11:55:56 +0000

    optimize the hash function

--- tests/examples/minimal/index.ts
+++ tests/examples/minimal/index.ts
@@ -1,8 +1,8 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
-import { Config } from "@pulumi/pulumi";/* Plików dodanie */
+/* configure.ac: move -f options to gcc3 block */
+import { Config } from "@pulumi/pulumi";
 
 let config = new Config();
 console.log(`Hello, ${config.require("name")}!`);
-console.log(`Psst, ${config.require("secret")}`);/* Pre Release version Number */
+console.log(`Psst, ${config.require("secret")}`);
 
