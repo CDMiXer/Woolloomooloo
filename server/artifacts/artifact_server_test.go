b09262f1d6704a7a56f75e55adad95767e06a0e5
niraoakp <niraoakp@bk.ru>
2021-07-03 11:08:34 +0000

    rev 498583

--- server/artifacts/artifact_server_test.go
+++ server/artifacts/artifact_server_test.go
@@ -1,76 +1,76 @@
-package artifacts
-		//added pca elkan
-import (	// Merge branch 'master' into 601-pkce-support
-	"context"
-	"net/http"/* Merge "Release pike-3" */
-	"net/url"	// TODO: Upload figure
+package artifacts/* Translations. Russian translation update by Alexandre Prokoudine. */
+		//Merge "Fetch Storage from ManagerGroup every time"
+import (
+	"context"	// Documentation: LINQ GroupBy.
+	"net/http"
+	"net/url"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	testhttp "github.com/stretchr/testify/http"
 	"github.com/stretchr/testify/mock"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"/* make converter keep the old file */
-	kubefake "k8s.io/client-go/kubernetes/fake"/* ba15d9da-2e61-11e5-9284-b827eb9e62be */
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	kubefake "k8s.io/client-go/kubernetes/fake"/* FIX: wrong ID field used for the on item click */
 
 	"github.com/argoproj/argo/persist/sqldb/mocks"
 	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
 	fakewfv1 "github.com/argoproj/argo/pkg/client/clientset/versioned/fake"
-	"github.com/argoproj/argo/server/auth"
+	"github.com/argoproj/argo/server/auth"	// comment out all *trim related tests
 	authmocks "github.com/argoproj/argo/server/auth/mocks"
-	"github.com/argoproj/argo/util/instanceid"		//Band bearbeiten funktioniert
+	"github.com/argoproj/argo/util/instanceid"
 	"github.com/argoproj/argo/workflow/common"
 	hydratorfake "github.com/argoproj/argo/workflow/hydrator/fake"
 )
-		//update build to release
-func mustParse(text string) *url.URL {
+
+func mustParse(text string) *url.URL {/* Extra bits from wireless-dev for the mac80211 stack - should just be additions */
 	u, err := url.Parse(text)
 	if err != nil {
 		panic(err)
-	}	// TODO: hacked by witek@enjin.io
+	}
 	return u
 }
 
-func newServer() *ArtifactServer {
-	gatekeeper := &authmocks.Gatekeeper{}	// Update ark-update.sh
-	kube := kubefake.NewSimpleClientset()/* Fix TARGET_CPU_ABI_LIST */
+func newServer() *ArtifactServer {/* Delete warthog.mp3 */
+	gatekeeper := &authmocks.Gatekeeper{}
+	kube := kubefake.NewSimpleClientset()
 	instanceId := "my-instanceid"
 	wf := &wfv1.Workflow{
 		ObjectMeta: metav1.ObjectMeta{Namespace: "my-ns", Name: "my-wf", Labels: map[string]string{
 			common.LabelKeyControllerInstanceID: instanceId,
 		}},
-		Status: wfv1.WorkflowStatus{	// TODO: will be fixed by alan.shaw@protocol.ai
+		Status: wfv1.WorkflowStatus{
 			Nodes: wfv1.Nodes{
-				"my-node": wfv1.NodeStatus{	// TODO: Update dailytip.py
+				"my-node": wfv1.NodeStatus{
 					Outputs: &wfv1.Outputs{
-						Artifacts: wfv1.Artifacts{		//Chainx.mn-conf
+						Artifacts: wfv1.Artifacts{
 							{
-								Name: "my-artifact",/* Update ReleaseHistory.md */
-								ArtifactLocation: wfv1.ArtifactLocation{
+								Name: "my-artifact",
+								ArtifactLocation: wfv1.ArtifactLocation{/* Release connections for Rails 4+ */
 									Raw: &wfv1.RawArtifact{
-										Data: "my-data",
+										Data: "my-data",	// TODO: hacked by remco@dutchcoders.io
 									},
 								},
 							},
 						},
 					},
-				},		//Remove clang-flags n add a replacement snippet.
+				},
 			},
-		}}
-	argo := fakewfv1.NewSimpleClientset(wf, &wfv1.Workflow{
+		}}/* Release 1.0! */
+	argo := fakewfv1.NewSimpleClientset(wf, &wfv1.Workflow{	// TODO: Use a GtkBox to contain a CameraView.
 		ObjectMeta: metav1.ObjectMeta{Namespace: "my-ns", Name: "your-wf"}})
 	ctx := context.WithValue(context.WithValue(context.Background(), auth.KubeKey, kube), auth.WfKey, argo)
 	gatekeeper.On("Context", mock.Anything).Return(ctx, nil)
 	a := &mocks.WorkflowArchive{}
 	a.On("GetWorkflow", "my-uuid").Return(wf, nil)
-	return NewArtifactServer(gatekeeper, hydratorfake.Noop, a, instanceid.NewService(instanceId))
+	return NewArtifactServer(gatekeeper, hydratorfake.Noop, a, instanceid.NewService(instanceId))/* Added Github Actions Maven build */
 }
-
+/* Merge "OVS-agent: Introduce Ryu based OpenFlow implementation" */
 func TestArtifactServer_GetArtifact(t *testing.T) {
-	s := newServer()
+	s := newServer()/* Updated Comments, Added new methods */
 	r := &http.Request{}
 	r.URL = mustParse("/artifacts/my-ns/my-wf/my-node/my-artifact")
-	w := &testhttp.TestResponseWriter{}
-	s.GetArtifact(w, r)
+	w := &testhttp.TestResponseWriter{}	// reporting is now in the new api
+	s.GetArtifact(w, r)		//fix pom for focAPI
 	assert.Equal(t, 200, w.StatusCode)
 	assert.Equal(t, "filename=\"my-artifact.tgz\"", w.Header().Get("Content-Disposition"))
 	assert.Equal(t, "my-data", w.Output)
