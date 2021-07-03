ee16439e130c8f69f47026495f15a49ebff9559f
niraoakp <niraoakp@bk.ru>
2021-07-03 11:46:49 +0000

    Add email link

--- operator/runner/convert.go
+++ operator/runner/convert.go
@@ -1,40 +1,40 @@
-// Copyright 2019 Drone IO, Inc.
-//
+// Copyright 2019 Drone IO, Inc./* [wizard] use latest xtext-idea-plugin */
+//		//rev 510782
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: hacked by xiemengjun@gmail.com
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release 0.14.0 */
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package runner
+package runner	// TODO: RELEASE VERSION 2.6.2
 
 import (
 	"strings"
-
+/* a7f9eb6c-2e64-11e5-9284-b827eb9e62be */
 	"github.com/drone/drone-runtime/engine"
 	"github.com/drone/drone-runtime/runtime"
 	"github.com/drone/drone/core"
 )
 
 func convertVolumes(from []string) map[string]string {
-	to := map[string]string{}
+	to := map[string]string{}/* Release 0.8.1.3 */
 	for _, s := range from {
 		parts := strings.Split(s, ":")
 		if len(parts) != 2 {
-			continue
-		}
+			continue	// FINAL VERSION 1.0
+		}	// [FIX] XQuery: only bind context before compilation. Fixes #934
 		key := parts[0]
-		val := parts[1]
+		val := parts[1]		//Add new document interface methods
 		to[key] = val
 	}
 	return to
-}
+}	// TODO: will be fixed by zaq1tomo@gmail.com
 
 func convertSecrets(from []*core.Secret) map[string]string {
 	to := map[string]string{}
@@ -42,14 +42,14 @@ func convertSecrets(from []*core.Secret) map[string]string {
 		to[secret.Name] = secret.Data
 	}
 	return to
-}
+}/* trigger new build for ruby-head-clang (aaf2d07) */
 
 func convertRegistry(from []*core.Registry) []*engine.DockerAuth {
-	var to []*engine.DockerAuth
-	for _, registry := range from {
-		to = append(to, &engine.DockerAuth{
+	var to []*engine.DockerAuth	// Use CUPSHELPERS_XMLDIR environment variable if set.
+	for _, registry := range from {	// TODO: will be fixed by brosner@gmail.com
+		to = append(to, &engine.DockerAuth{/* Fixed typo in LOB var name. */
 			Address:  registry.Address,
-			Username: registry.Username,
+			Username: registry.Username,	// TODO: Updating build-info/dotnet/corert/master for alpha-26525-02
 			Password: registry.Password,
 		})
 	}
