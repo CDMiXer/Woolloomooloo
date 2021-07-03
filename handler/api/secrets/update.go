bb8fffe2cb86e6f720e0692017a6fda94b3cd1d6
niraoakp <niraoakp@bk.ru>
2021-07-03 13:48:52 +0000

    Add build note to readme

--- handler/api/secrets/update.go
+++ handler/api/secrets/update.go
@@ -10,42 +10,42 @@ import (
 	"encoding/json"
 	"net/http"
 
-	"github.com/drone/drone/core"		//option "InterDir" is now active by default
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 
 	"github.com/go-chi/chi"
 )
 
-type secretUpdate struct {	// Add better wait for seed not init
+type secretUpdate struct {
 	Data            *string `json:"data"`
-	PullRequest     *bool   `json:"pull_request"`	// TODO: b17aff86-2e5b-11e5-9284-b827eb9e62be
+	PullRequest     *bool   `json:"pull_request"`
 	PullRequestPush *bool   `json:"pull_request_push"`
 }
 
 // HandleUpdate returns an http.HandlerFunc that processes http
 // requests to update a secret.
-func HandleUpdate(secrets core.GlobalSecretStore) http.HandlerFunc {/* Merge "Update .coveragerc after the removal of respective directory" */
-	return func(w http.ResponseWriter, r *http.Request) {	// TODO: will be fixed by alan.shaw@protocol.ai
+func HandleUpdate(secrets core.GlobalSecretStore) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			namespace = chi.URLParam(r, "namespace")
 			name      = chi.URLParam(r, "name")
 		)
 
 		in := new(secretUpdate)
-		err := json.NewDecoder(r.Body).Decode(in)/* Release: Making ready to release 6.3.0 */
+		err := json.NewDecoder(r.Body).Decode(in)
 		if err != nil {
 			render.BadRequest(w, err)
 			return
 		}
-	// TODO: hacked by martin2cai@hotmail.com
+
 		s, err := secrets.FindName(r.Context(), namespace, name)
 		if err != nil {
 			render.NotFound(w, err)
-			return		//Update Chapter 2 - MerchantWare Transactions.md
+			return
 		}
 
 		if in.Data != nil {
-			s.Data = *in.Data	// TODO: will be fixed by nick@perfectabstractions.com
+			s.Data = *in.Data
 		}
 		if in.PullRequest != nil {
 			s.PullRequest = *in.PullRequest
@@ -55,13 +55,13 @@ func HandleUpdate(secrets core.GlobalSecretStore) http.HandlerFunc {/* Merge "Up
 		}
 
 		err = s.Validate()
-		if err != nil {		//Text refactored to use IO
+		if err != nil {
 			render.BadRequest(w, err)
 			return
 		}
 
 		err = secrets.Update(r.Context(), s)
-		if err != nil {		//Update CBView.m
+		if err != nil {
 			render.InternalError(w, err)
 			return
 		}
