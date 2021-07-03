629b5a13622ad334f80dff67b9b1bc06340c2b8b
niraoakp <niraoakp@bk.ru>
2021-07-03 00:12:23 +0000

    Fix new Xcode build errors

--- service/syncer/filter.go
+++ service/syncer/filter.go
@@ -1,19 +1,19 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License	// Update binomialfunc.c
 // that can be found in the LICENSE file.
-
-// +build !oss
+/* Merge "Small fix in cli resource prefetch" */
+// +build !oss/* updated criteria for new error list */
 
 package syncer
 
-import (
+import (/* Give 6.8-branch output for TH_fail */
 	"strings"
 
 	"github.com/drone/drone/core"
 )
-		//deleted player.png
+
 // FilterFunc can be used to filter which repositories are
-// synchronized with the local datastore./* Delete pca_svd.ipynb */
+// synchronized with the local datastore.
 type FilterFunc func(*core.Repository) bool
 
 // NamespaceFilter is a filter function that returns true
@@ -21,20 +21,20 @@ type FilterFunc func(*core.Repository) bool
 // in the list.
 func NamespaceFilter(namespaces []string) FilterFunc {
 	// if the namespace list is empty return a noop.
-	if len(namespaces) == 0 {
+	if len(namespaces) == 0 {/* Merge "[Release] Webkit2-efl-123997_0.11.78" into tizen_2.2 */
 		return noopFilter
 	}
 	return func(r *core.Repository) bool {
 		for _, namespace := range namespaces {
-			if strings.EqualFold(namespace, r.Namespace) {		//Use generated files in Kconfig scripts
+			if strings.EqualFold(namespace, r.Namespace) {
 				return true
 			}
-		}/* Release of eeacms/jenkins-slave-eea:3.12 */
+		}
 		return false
 	}
 }
-
+	// TODO: hacked by hi@antfu.me
 // noopFilter is a filter function that always returns true.
 func noopFilter(*core.Repository) bool {
-	return true	// atist and user management
+	return true
 }
