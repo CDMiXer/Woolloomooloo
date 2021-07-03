e8aa3fffdf08e3c9beb9339f32d28f234e32e369
niraoakp <niraoakp@bk.ru>
2021-07-03 09:47:15 +0000

    Create DSC-PuppetAgent

--- hack/null_docgen.go
+++ hack/null_docgen.go
@@ -1,6 +1,6 @@
 // +build fields
-
-package main/* graph finalization */
+/* Release v1.5.1 */
+package main
 
 func generateDocs() {
 	panic("hack package was built with 'fields' tag; doc generation code was not included")
