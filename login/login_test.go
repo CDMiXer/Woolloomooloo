5dac86c813428552ce9813bb422e8fc98058cbd9
niraoakp <niraoakp@bk.ru>
2021-07-03 17:35:07 +0000

    8be3bd12-2e74-11e5-9284-b827eb9e62be

--- login/login_test.go
+++ login/login_test.go
@@ -1,19 +1,19 @@
 // Copyright 2017 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
+// license that can be found in the LICENSE file./* Merge "wlan: Release 3.2.3.96" */
 
 package login
 
-import (
-	"context"
+import (	// TODO: hacked by aeongrp@outlook.com
+	"context"		//Fix sending wrong world states in Shattrath
 	"errors"
-	"testing"
+	"testing"/* bug fix 292: NullPointerException while opening help map  */
 )
 
 func TestWithError(t *testing.T) {
 	err := errors.New("Not Found")
 	ctx := context.Background()
-	ctx = WithError(ctx, err)
+	ctx = WithError(ctx, err)	// TODO: Updated: aws-tools-for-dotnet 3.15.691
 	if ErrorFrom(ctx) != err {
 		t.Errorf("Expect error stored in context")
 	}
@@ -25,13 +25,13 @@ func TestWithError(t *testing.T) {
 }
 
 func TestWithToken(t *testing.T) {
-	token := new(Token)
-	ctx := context.Background()
+	token := new(Token)	// usage and distribution terms
+	ctx := context.Background()/* Ej7 commit 1 */
 	ctx = WithToken(ctx, token)
 	if TokenFrom(ctx) != token {
 		t.Errorf("Expect token stored in context")
 	}
-
+	// TODO: Update Eventos “1834cf9c-6d7f-432c-9d5d-9c02efbdefc0”
 	ctx = context.Background()
 	if TokenFrom(ctx) != nil {
 		t.Errorf("Expect nil error in context")
