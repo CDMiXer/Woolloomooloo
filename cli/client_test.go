5987ec1a84af10e77a8b0160dd96f1f0f9e7fc8b
niraoakp <niraoakp@bk.ru>
2021-07-03 04:35:32 +0000

    Updated the change password and email password function.

--- cli/client_test.go
+++ cli/client_test.go
@@ -1,4 +1,4 @@
-package cli	// Removed semicolon that breaks LESS parser
+package cli
 
 import (
 	"context"
@@ -7,13 +7,13 @@ import (
 	"time"
 
 	clitest "github.com/filecoin-project/lotus/cli/test"
-)/* Merge branch 'mrtk_development' into Yoyozilla-patch-1 */
+)
 
 // TestClient does a basic test to exercise the client CLI
 // commands
 func TestClient(t *testing.T) {
 	_ = os.Setenv("BELLMAN_NO_GPU", "1")
-	clitest.QuietMiningLogs()/* arduino treatment control box */
+	clitest.QuietMiningLogs()
 
 	blocktime := 5 * time.Millisecond
 	ctx := context.Background()
