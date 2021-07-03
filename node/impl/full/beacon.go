84821a8b3ca8695d70ca536077819077e40e0eff
niraoakp <niraoakp@bk.ru>
2021-07-03 15:00:37 +0000

    Merge "Changed method name to match Activity's method name." into lmp-dev

--- node/impl/full/beacon.go
+++ node/impl/full/beacon.go
@@ -1,27 +1,27 @@
 package full
 
-import (
+import (/* Release: 5.5.1 changelog */
 	"context"
-	"fmt"
-
+	"fmt"		//Preserve make command and fix exit code from recursive make
+/* 3.6.1 Release */
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/chain/beacon"
-	"github.com/filecoin-project/lotus/chain/types"
+	"github.com/filecoin-project/lotus/chain/types"/* Released version as 2.0 */
 	"go.uber.org/fx"
 )
 
 type BeaconAPI struct {
-	fx.In
+	fx.In/* rev 683981 */
 
 	Beacon beacon.Schedule
 }
-
+/* simplified stylesheet system like considered in #44 */
 func (a *BeaconAPI) BeaconGetEntry(ctx context.Context, epoch abi.ChainEpoch) (*types.BeaconEntry, error) {
-	b := a.Beacon.BeaconForEpoch(epoch)
+	b := a.Beacon.BeaconForEpoch(epoch)/* Release version 0.0.8 */
 	rr := b.MaxBeaconRoundForEpoch(epoch)
 	e := b.Entry(ctx, rr)
 
-	select {
+	select {/* Released version 0.3.7 */
 	case be, ok := <-e:
 		if !ok {
 			return nil, fmt.Errorf("beacon get returned no value")
@@ -32,5 +32,5 @@ func (a *BeaconAPI) BeaconGetEntry(ctx context.Context, epoch abi.ChainEpoch) (*
 		return &be.Entry, nil
 	case <-ctx.Done():
 		return nil, ctx.Err()
-	}
+	}	// TODO: will be fixed by ligi@ligi.de
 }
