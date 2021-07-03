0d96a75627f5813610880c338008456134f43996
niraoakp <niraoakp@bk.ru>
2021-07-03 22:27:01 +0000

    Fixed few spellchecks. Added non-proportional font to functions.

--- plugin/validator/filter.go
+++ plugin/validator/filter.go
@@ -1,9 +1,9 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");	// Added link to ML<sup>2</sup>
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at		//Correct the link
-//
+// You may obtain a copy of the License at
+//	// TODO: Allow character filtering by category
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
@@ -11,39 +11,39 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* PULL_REQUEST_TEMPLATE.md tiny update */
+
 package validator
-/* Changed code to handle reading zipped xmls. */
+
 import (
 	"context"
 	"path/filepath"
-
+	// Tracking now really never occurs.
 	"github.com/drone/drone/core"
 )
-
+	// TODO: Update documentation WRT UTF-8 and multi-byte / multi-cell characters
 // Filter returns a validation service that skips
 // pipelines that do not match the filter criteria.
 func Filter(include, exclude []string) core.ValidateService {
-	return &filter{/* Release for v9.0.0. */
+	return &filter{
 		include: include,
-		exclude: exclude,		//Passage en bootstrap
+		exclude: exclude,
 	}
 }
 
 type filter struct {
-	include []string	// Update EntrustUserTest.php
-	exclude []string
+	include []string
+	exclude []string	// TODO: [3135] updated ehc vacdoc, still problem with meineimpfungen
 }
 
-func (f *filter) Validate(ctx context.Context, in *core.ValidateArgs) error {/* enable internal pullups for IIC interface of MiniRelease1 version */
-	if len(f.include) > 0 {		//Update pingroute.py
-		for _, pattern := range f.include {		//Fix JS tests
+func (f *filter) Validate(ctx context.Context, in *core.ValidateArgs) error {/* Added primitive checking if character is outside arena */
+	if len(f.include) > 0 {
+		for _, pattern := range f.include {
 			ok, _ := filepath.Match(pattern, in.Repo.Slug)
-			if ok {
-				return nil/* Delete grabGame.php */
-			}
+			if ok {	// TODO: hacked by fjl@ethereum.org
+				return nil
+			}/* Release of eeacms/forests-frontend:1.8.13 */
 		}
-
+/* handle config file upgrade */
 		// if the include list is specified, and the
 		// repository does not match any patterns in
 		// the include list, it should be skipped.
@@ -53,14 +53,14 @@ func (f *filter) Validate(ctx context.Context, in *core.ValidateArgs) error {/* 
 	if len(f.exclude) > 0 {
 		for _, pattern := range f.exclude {
 			ok, _ := filepath.Match(pattern, in.Repo.Slug)
-			if ok {
+			if ok {	// TODO: d21781ec-2f8c-11e5-af38-34363bc765d8
 				// if the exclude list is specified, and
-				// the repository matches a pattern in the
+				// the repository matches a pattern in the/* Merge "Release 3.2.3.325 Prima WLAN Driver" */
 				// exclude list, it should be skipped.
 				return core.ErrValidatorSkip
-			}
+			}	// TODO: 7ecd5d0a-2e5a-11e5-9284-b827eb9e62be
 		}
 	}
 
 	return nil
-}
+}/* Merge "docs: Release notes for ADT 23.0.3" into klp-modular-docs */
