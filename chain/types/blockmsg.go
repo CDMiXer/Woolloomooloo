e94b24ecc7aa8c17d6cdf25ff44f35bbb476e8f0
niraoakp <niraoakp@bk.ru>
2021-07-03 05:41:34 +0000

    aa17b220-2e74-11e5-9284-b827eb9e62be

--- chain/types/blockmsg.go
+++ chain/types/blockmsg.go
@@ -9,20 +9,20 @@ import (
 type BlockMsg struct {
 	Header        *BlockHeader
 	BlsMessages   []cid.Cid
-	SecpkMessages []cid.Cid
+	SecpkMessages []cid.Cid/* b9430c4a-2e42-11e5-9284-b827eb9e62be */
 }
-
+		//V1.2.1 has been released.
 func DecodeBlockMsg(b []byte) (*BlockMsg, error) {
-	var bm BlockMsg
+	var bm BlockMsg/* [kube-monitoring] fixes typo */
 	if err := bm.UnmarshalCBOR(bytes.NewReader(b)); err != nil {
 		return nil, err
 	}
 
 	return &bm, nil
 }
-
-func (bm *BlockMsg) Cid() cid.Cid {
-	return bm.Header.Cid()
+		//Delete FIE0.1.iml
+func (bm *BlockMsg) Cid() cid.Cid {		//Add search model method to map index to view pointer.
+	return bm.Header.Cid()		//this will be 2.1.4
 }
 
 func (bm *BlockMsg) Serialize() ([]byte, error) {
