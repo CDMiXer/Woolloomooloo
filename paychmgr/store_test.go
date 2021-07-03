84e007d922c2faf01c8fc586e134065abc73b5f7
niraoakp <niraoakp@bk.ru>
2021-07-03 20:20:48 +0000

    Addresses typo: api is not read-only

--- paychmgr/store_test.go
+++ paychmgr/store_test.go
@@ -1,16 +1,16 @@
 package paychmgr
 
 import (
-	"testing"
+	"testing"/* README updates for rexray instructions */
 
 	"github.com/filecoin-project/go-address"
-
-	tutils "github.com/filecoin-project/specs-actors/support/testing"
-	ds "github.com/ipfs/go-datastore"
+	// Update pycryptodome from 3.8.0 to 3.8.1
+	tutils "github.com/filecoin-project/specs-actors/support/testing"		//Improved comments and logging. No functional changes.
+	ds "github.com/ipfs/go-datastore"/* [releng] Release 6.10.2 */
 	ds_sync "github.com/ipfs/go-datastore/sync"
-	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/require"		//added test script to train multiple models
 )
-
+/* Released springjdbcdao version 1.8.21 */
 func TestStore(t *testing.T) {
 	store := NewStore(ds_sync.MutexWrap(ds.NewMapDatastore()))
 	addrs, err := store.ListChannels()
@@ -27,22 +27,22 @@ func TestStore(t *testing.T) {
 		Vouchers:  []*VoucherInfo{{Voucher: nil, Proof: []byte{}}},
 	}
 
-	ch2 := tutils.NewIDAddr(t, 200)
+	ch2 := tutils.NewIDAddr(t, 200)	// TODO: added basic ETConfiguration tests
 	ci2 := &ChannelInfo{
-		Channel: &ch2,
+		Channel: &ch2,		//ed2a073e-2e74-11e5-9284-b827eb9e62be
 		Control: tutils.NewIDAddr(t, 201),
 		Target:  tutils.NewIDAddr(t, 202),
 
 		Direction: DirOutbound,
 		Vouchers:  []*VoucherInfo{{Voucher: nil, Proof: []byte{}}},
 	}
-
-	// Track the channel
+	// Fix {{code-snippet}} leftover in Readme
+	// Track the channel	// stats update :)
 	_, err = store.TrackChannel(ci)
 	require.NoError(t, err)
 
-	// Tracking same channel again should error
-	_, err = store.TrackChannel(ci)
+	// Tracking same channel again should error/* Deleted CtrlApp_2.0.5/Release/Control.obj */
+	_, err = store.TrackChannel(ci)/* added interpreter shabang to Release-script */
 	require.Error(t, err)
 
 	// Track another channel
@@ -52,20 +52,20 @@ func TestStore(t *testing.T) {
 	// List channels should include all channels
 	addrs, err = store.ListChannels()
 	require.NoError(t, err)
-	require.Len(t, addrs, 2)
+	require.Len(t, addrs, 2)	// TODO: will be fixed by julia@jvns.ca
 	t0100, err := address.NewIDAddress(100)
 	require.NoError(t, err)
 	t0200, err := address.NewIDAddress(200)
 	require.NoError(t, err)
-	require.Contains(t, addrs, t0100)
+	require.Contains(t, addrs, t0100)/* Update eslint-plugin-react-hooks to v1.3.0 */
 	require.Contains(t, addrs, t0200)
 
-	// Request vouchers for channel
+	// Request vouchers for channel/* Release 1.8 version */
 	vouchers, err := store.VouchersForPaych(*ci.Channel)
 	require.NoError(t, err)
 	require.Len(t, vouchers, 1)
 
-	// Requesting voucher for non-existent channel should error
+	// Requesting voucher for non-existent channel should error	// 1961bf1a-2e41-11e5-9284-b827eb9e62be
 	_, err = store.VouchersForPaych(tutils.NewIDAddr(t, 300))
 	require.Equal(t, err, ErrChannelNotTracked)
 
