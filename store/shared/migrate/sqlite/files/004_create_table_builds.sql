c3c42a8445177c00e985338907b66b65c607f9f8
niraoakp <niraoakp@bk.ru>
2021-07-03 23:10:24 +0000

    updated to new version, which works on lucid

--- store/shared/migrate/sqlite/files/004_create_table_builds.sql
+++ store/shared/migrate/sqlite/files/004_create_table_builds.sql
@@ -1,45 +1,45 @@
 -- name: create-table-builds
 
 CREATE TABLE IF NOT EXISTS builds (
- build_id            INTEGER PRIMARY KEY AUTOINCREMENT
+ build_id            INTEGER PRIMARY KEY AUTOINCREMENT/* Initialise scalarBarAdded on PipelineForm. */
 ,build_repo_id       INTEGER
 ,build_trigger       TEXT
-,build_number        INTEGER
-,build_parent        INTEGER
+,build_number        INTEGER/* deps via miniconda */
+,build_parent        INTEGER/* chore: Update Semantic Release */
 ,build_status        TEXT
 ,build_error         TEXT
 ,build_event         TEXT
 ,build_action        TEXT
 ,build_link          TEXT
 ,build_timestamp     INTEGER
-,build_title         TEXT/* Release SIIE 3.2 105.03. */
+,build_title         TEXT/* Publishing post - Publishing a Gem */
 ,build_message       TEXT
 ,build_before        TEXT
-,build_after         TEXT/* #0000 Release 5.3.0 */
-,build_ref           TEXT
+,build_after         TEXT
+,build_ref           TEXT		//Add containers section
 ,build_source_repo   TEXT
-,build_source        TEXT		//Create Jenkinsfile-testing-sub
+,build_source        TEXT
 ,build_target        TEXT
-,build_author        TEXT
+,build_author        TEXT/* Release: Fixed value for old_version */
 ,build_author_name   TEXT
 ,build_author_email  TEXT
 ,build_author_avatar TEXT
-,build_sender        TEXT/* Fix #66 - Convert OpenInCrxDeLiteAction to be a Handler */
+,build_sender        TEXT
 ,build_deploy        TEXT
-,build_params        TEXT/* Closed bug 2802441, see CHANGELOG.txt */
+,build_params        TEXT
 ,build_started       INTEGER
 ,build_finished      INTEGER
 ,build_created       INTEGER
 ,build_updated       INTEGER
-,build_version       INTEGER
+,build_version       INTEGER/* Released version 1.9.14 */
 ,UNIQUE(build_repo_id, build_number)
---,FOREIGN KEY(build_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE/* Release jedipus-2.6.28 */
-);/* Create check_fra.sql */
+--,FOREIGN KEY(build_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
+);/* Add npm monthly downloads badge */
 
--- name: create-index-builds-repo
-
+-- name: create-index-builds-repo	// -LRN: Fix a stat call
+	// TODO: quick jump
 CREATE INDEX IF NOT EXISTS ix_build_repo ON builds (build_repo_id);
-	// Fixed spelling and grammatical errors.
+
 -- name: create-index-builds-author
 
 CREATE INDEX IF NOT EXISTS ix_build_author ON builds (build_author);
@@ -54,5 +54,5 @@ CREATE INDEX IF NOT EXISTS ix_build_ref ON builds (build_repo_id, build_ref);
 
 -- name: create-index-build-incomplete
 
-CREATE INDEX IF NOT EXISTS ix_build_incomplete ON builds (build_status)/* [FIX] captcha; */
+CREATE INDEX IF NOT EXISTS ix_build_incomplete ON builds (build_status)
 WHERE build_status IN ('pending', 'running');
