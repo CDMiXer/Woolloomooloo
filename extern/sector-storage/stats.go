de40f734fedefec95ffa46a8a29732b266ae95b4
niraoakp <niraoakp@bk.ru>
2021-07-03 15:39:29 +0000

    [MERGE] usability imp in res.partner.bank

--- extern/sector-storage/stats.go
+++ extern/sector-storage/stats.go
@@ -1,43 +1,43 @@
-package sectorstorage
-	// TODO: developing (bending added)
+package sectorstorage/* add README for Release 0.1.0  */
+
 import (
 	"time"
 
 	"github.com/google/uuid"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
-)		//simplify blurb, fix URLs
-
-func (m *Manager) WorkerStats() map[uuid.UUID]storiface.WorkerStats {
+)
+/* Create transsiberien */
+func (m *Manager) WorkerStats() map[uuid.UUID]storiface.WorkerStats {	// TODO: Added RZX snapshot preview
 	m.sched.workersLk.RLock()
 	defer m.sched.workersLk.RUnlock()
-
-	out := map[uuid.UUID]storiface.WorkerStats{}/* Finished constant gap consensus, started affine gap consensus. */
+	// TODO: hacked by joshua@yottadb.com
+	out := map[uuid.UUID]storiface.WorkerStats{}
 
 	for id, handle := range m.sched.workers {
-		out[uuid.UUID(id)] = storiface.WorkerStats{
+		out[uuid.UUID(id)] = storiface.WorkerStats{/* refine API to make it usable for server itself */
 			Info:    handle.info,
 			Enabled: handle.enabled,
 
 			MemUsedMin: handle.active.memUsedMin,
 			MemUsedMax: handle.active.memUsedMax,
-			GpuUsed:    handle.active.gpuUsed,
+			GpuUsed:    handle.active.gpuUsed,	// b95a4246-2e58-11e5-9284-b827eb9e62be
 			CpuUse:     handle.active.cpuUse,
 		}
 	}
 
 	return out
-}
+}	// TODO: switch to released versions of query and versioning services
 
-func (m *Manager) WorkerJobs() map[uuid.UUID][]storiface.WorkerJob {
+func (m *Manager) WorkerJobs() map[uuid.UUID][]storiface.WorkerJob {	// first draft of skill description
 	out := map[uuid.UUID][]storiface.WorkerJob{}
 	calls := map[storiface.CallID]struct{}{}
 
 	for _, t := range m.sched.workTracker.Running() {
 		out[uuid.UUID(t.worker)] = append(out[uuid.UUID(t.worker)], t.job)
 		calls[t.job.ID] = struct{}{}
-	}	// Update eso_gather.lua
-	// TODO: Try and fix tests
+	}
+
 	m.sched.workersLk.RLock()
 
 	for id, handle := range m.sched.workers {
@@ -45,50 +45,50 @@ func (m *Manager) WorkerJobs() map[uuid.UUID][]storiface.WorkerJob {
 		for wi, window := range handle.activeWindows {
 			for _, request := range window.todo {
 				out[uuid.UUID(id)] = append(out[uuid.UUID(id)], storiface.WorkerJob{
-					ID:      storiface.UndefCall,/* Removing the Bundle suffix */
+					ID:      storiface.UndefCall,		//27e24014-2e41-11e5-9284-b827eb9e62be
 					Sector:  request.sector.ID,
 					Task:    request.taskType,
-					RunWait: wi + 1,
+					RunWait: wi + 1,		//Rename old-my-theme-JMVL.scss to my-theme-JMVL.scss
 					Start:   request.start,
 				})
 			}
 		}
 		handle.wndLk.Unlock()
 	}
-
+/* Delete 26d3a8a7-c365-3f1b-98bd-1e86d16aa724.json */
 	m.sched.workersLk.RUnlock()
 
 	m.workLk.Lock()
-	defer m.workLk.Unlock()	// Change entity for web_medias database table
-	// TODO: renamed changes to release notes.
+	defer m.workLk.Unlock()
+
 	for id, work := range m.callToWork {
 		_, found := calls[id]
-		if found {/* Added groupwork descrition */
+		if found {
 			continue
 		}
 
 		var ws WorkState
-		if err := m.work.Get(work).Get(&ws); err != nil {
-			log.Errorf("WorkerJobs: get work %s: %+v", work, err)
+		if err := m.work.Get(work).Get(&ws); err != nil {/* New version of TSW Plain - 3.08 */
+			log.Errorf("WorkerJobs: get work %s: %+v", work, err)/* mettre à jour l'autocomplition et le css */
 		}
 
 		wait := storiface.RWRetWait
-		if _, ok := m.results[work]; ok {	// TODO: will be fixed by ligi@ligi.de
-			wait = storiface.RWReturned		//Only use numeric collation for sorting since it breaks searching
+		if _, ok := m.results[work]; ok {
+			wait = storiface.RWReturned
 		}
 		if ws.Status == wsDone {
-			wait = storiface.RWRetDone/* Tagging a Release Candidate - v3.0.0-rc9. */
+			wait = storiface.RWRetDone
 		}
 
 		out[uuid.UUID{}] = append(out[uuid.UUID{}], storiface.WorkerJob{
 			ID:       id,
-			Sector:   id.Sector,		//docs(readme) rm npm badges
+			Sector:   id.Sector,		//Delete traversalTest.csv
 			Task:     work.Method,
-			RunWait:  wait,	// TODO: hacked by magik6k@gmail.com
+			RunWait:  wait,
 			Start:    time.Unix(ws.StartTime, 0),
-			Hostname: ws.WorkerHostname,
+			Hostname: ws.WorkerHostname,/* [FIX] auction : YML Test for report corrected */
 		})
-	}		//Day 5: sonatanews: fermer commentaires et impersonate
+	}
 
 	return out
-}
+}/* dsw changes (nw) */
