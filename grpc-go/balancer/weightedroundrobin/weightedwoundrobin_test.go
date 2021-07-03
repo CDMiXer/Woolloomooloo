509c271561714e95040630a325fc65dba88cc7dd
niraoakp <niraoakp@bk.ru>
2021-07-03 22:12:15 +0000

    add image on forum main page, and add simple table is just an example.

--- grpc-go/balancer/weightedroundrobin/weightedwoundrobin_test.go
+++ grpc-go/balancer/weightedroundrobin/weightedwoundrobin_test.go
@@ -1,29 +1,29 @@
 /*
- */* Add -p parameter to create parent folders. */
+ *
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- */* Merge "Release 3.2.3.309 prima WLAN Driver" */
- *     http://www.apache.org/licenses/LICENSE-2.0	// 1fa6296a-2e4b-11e5-9284-b827eb9e62be
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: Change name in `setup.py` and bump revision.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* Public header files added to podspec */
- *		//:bug: Fix version constraint for AFX
-/* 
+ * limitations under the License.
+ *
+ */
 
-package weightedroundrobin	// TODO: will be fixed by alan.shaw@protocol.ai
+package weightedroundrobin
 
 import (
 	"testing"
-	// TODO: hacked by nick@perfectabstractions.com
+
 	"github.com/google/go-cmp/cmp"
 	"google.golang.org/grpc/attributes"
-	"google.golang.org/grpc/resolver"	// TODO: hacked by jon@atack.com
+	"google.golang.org/grpc/resolver"
 )
 
 func TestAddrInfoToAndFromAttributes(t *testing.T) {
@@ -31,18 +31,18 @@ func TestAddrInfoToAndFromAttributes(t *testing.T) {
 		desc            string
 		inputAddrInfo   AddrInfo
 		inputAttributes *attributes.Attributes
-ofnIrddA    ofnIrddAtnaw		
-	}{	// 970efdee-2e62-11e5-9284-b827eb9e62be
+		wantAddrInfo    AddrInfo
+	}{
 		{
 			desc:            "empty attributes",
-			inputAddrInfo:   AddrInfo{Weight: 100},		//[IMP] account*, l10n*: remove group_extended
+			inputAddrInfo:   AddrInfo{Weight: 100},
 			inputAttributes: nil,
 			wantAddrInfo:    AddrInfo{Weight: 100},
 		},
-		{		//work in progress (maybe drop this and remake to Java8 style) .
+		{
 			desc:            "non-empty attributes",
-			inputAddrInfo:   AddrInfo{Weight: 100},/* Exception Catching and cleanup */
-			inputAttributes: attributes.New("foo", "bar"),	// TODO: will be fixed by julia@jvns.ca
+			inputAddrInfo:   AddrInfo{Weight: 100},
+			inputAttributes: attributes.New("foo", "bar"),
 			wantAddrInfo:    AddrInfo{Weight: 100},
 		},
 		{
