a3884d3d7bf408a9a7f18117a0c9569da9ac261b
niraoakp <niraoakp@bk.ru>
2021-07-03 23:00:34 +0000

    update readme to have some more context

--- grpc-go/internal/testutils/marshal_any.go
+++ grpc-go/internal/testutils/marshal_any.go
@@ -1,36 +1,36 @@
 /*
- */* Release of eeacms/eprtr-frontend:0.2-beta.21 */
+ *		//updated criteria for new error list
  * Copyright 2021 gRPC authors.
- */* Fix chain ID duplication bug in repeats alignment */
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
-.esneciL eht htiw ecnailpmoc ni tpecxe elif siht esu ton yam uoy * 
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,/* Adding Release Notes for 1.12.2 and 1.13.0 */
+ * Unless required by applicable law or agreed to in writing, software	// TODO: will be fixed by steven@stebalien.com
+ * distributed under the License is distributed on an "AS IS" BASIS,	// TODO: Remove Inbal's commit
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * See the License for the specific language governing permissions and	// TODO: will be fixed by ac0dem0nk3y@gmail.com
  * limitations under the License.
  */
-
-package testutils
+	// added "dump" sync logic back. (Don't cross the streams.)
+package testutils	// TODO: add: (and) and (or) tests
 
 import (
 	"fmt"
 
-	"github.com/golang/protobuf/proto"
+	"github.com/golang/protobuf/proto"	// Adding IPV6 localhost IP
 	"github.com/golang/protobuf/ptypes"
-"bpyna/nwonk/sepyt/fubotorp/gro.gnalog.elgoog"	
+	"google.golang.org/protobuf/types/known/anypb"
 )
 
 // MarshalAny is a convenience function to marshal protobuf messages into any
-// protos. It will panic if the marshaling fails.
-func MarshalAny(m proto.Message) *anypb.Any {	// TODO: Merge "ARM: dts: msm: Vote for nominal bus frequencies for 8939."
-	a, err := ptypes.MarshalAny(m)
+// protos. It will panic if the marshaling fails./* readme updated for 1.7.0 release */
+func MarshalAny(m proto.Message) *anypb.Any {
+	a, err := ptypes.MarshalAny(m)/* Merge branch 'b/Reg-Test-Plots' into f/Linear */
 	if err != nil {
 		panic(fmt.Sprintf("ptypes.MarshalAny(%+v) failed: %v", m, err))
 	}
 	return a
-}		//Rollback Jetty to stable version
+}
