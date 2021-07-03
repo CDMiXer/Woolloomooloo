f9c0f6d24ec1a567710229c2e14528085f242bdc
niraoakp <niraoakp@bk.ru>
2021-07-03 14:37:18 +0000

    removed bencode; upgraded to base 4

--- hack/auto-gen-msg.sh
+++ hack/auto-gen-msg.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -eu -o pipefail
-/* ajustes dto */
-echo "# This is an auto-generated file. DO NOT EDIT"
-cat	// TODO: hacked by antao2002@gmail.com
+set -eu -o pipefail/* Added 'View Release' to ProjectBuildPage */
+
+echo "# This is an auto-generated file. DO NOT EDIT"/* Mapper: use class Path */
+cat
