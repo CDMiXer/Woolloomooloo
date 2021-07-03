e680291ebea4b51e8fd387a7b40a259532438347
niraoakp <niraoakp@bk.ru>
2021-07-03 16:13:00 +0000

    Adding a bit more text to the readme.md

--- grpc-go/test/tools/tools.go
+++ grpc-go/test/tools/tools.go
@@ -1,33 +1,33 @@
 // +build tools
-
+		//8247ef4c-2e4e-11e5-9284-b827eb9e62be
 /*
  *
  * Copyright 2018 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * Licensed under the Apache License, Version 2.0 (the "License");	// Update IRC example to reflect new room ID
+ * you may not use this file except in compliance with the License./* Remoção do campo data_abertura da modal de adicionar solicitação. */
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
+ * Unless required by applicable law or agreed to in writing, software/* Workaround on some URL constructions */
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * See the License for the specific language governing permissions and	// TODO: Create DISPLAYQ.basic
  * limitations under the License.
- *
+ *		//[IMP] Improved message when applicant hired with/without employee.
  */
 
 // This file is not intended to be compiled.  Because some of these imports are
 // not actual go packages, we use a build constraint at the top of this file to
 // prevent tools from inspecting the imports.
 
-package tools
+package tools	// TODO: default task
 
 import (
-	_ "github.com/client9/misspell/cmd/misspell"
+	_ "github.com/client9/misspell/cmd/misspell"/* temp fix gem for deprecation warnings */
 	_ "github.com/golang/protobuf/protoc-gen-go"
 	_ "golang.org/x/lint/golint"
 	_ "golang.org/x/tools/cmd/goimports"
-	_ "honnef.co/go/tools/cmd/staticcheck"
+	_ "honnef.co/go/tools/cmd/staticcheck"		//Fixed uncaught InterruptedException
 )
