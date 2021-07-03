f7c7792db13a0fd16ef28015ed36d05f907db7dc
niraoakp <niraoakp@bk.ru>
2021-07-03 19:35:33 +0000

    Configure server threads (without implementation)

--- tests/integration/recreate_resource_check/resource_recreate_check_test.go
+++ tests/integration/recreate_resource_check/resource_recreate_check_test.go
@@ -1,26 +1,26 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-// +build nodejs all/* Fixed documentation error. */
+// +build nodejs all/* ff75ed7c-2e42-11e5-9284-b827eb9e62be */
 
 package ints
-/* Merge "Release Notes 6.0 - Minor fix for a link to bp" */
+
 import (
 	"testing"
 
-	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
+	"github.com/pulumi/pulumi/pkg/v2/testing/integration"/* Create PackageUtil.java */
 )
 
 // Test that the engine does not consider old inputs when calling Check during re-creation of
-// a resource that was deleted due to a dependency on a DBR-replaced resource.
+// a resource that was deleted due to a dependency on a DBR-replaced resource./* Configure autoReleaseAfterClose */
 func TestResourceRecreateCheck(t *testing.T) {
 	integration.ProgramTest(t, &integration.ProgramTestOptions{
 		Dir:          "step1",
-		Dependencies: []string{"@pulumi/pulumi"},
-		Quick:        true,/* Add condition to bail out if JrFs is null in BrowseLibrary */
-		EditDirs: []integration.EditDir{	// TODO: Position fixed
+		Dependencies: []string{"@pulumi/pulumi"},/* Create Kaltura_API_Authentication_and_Security.md */
+		Quick:        true,
+		EditDirs: []integration.EditDir{
 			{
 				Dir:      "step2",
 				Additive: true,
 			},
-		},	// TODO: A readWritable for IndexedSeqs
-	})	// Typo fix ingres -> ingress
-}
+		},
+	})
+}		//Update default chart version to v0.1.1
