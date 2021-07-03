c4b660959bbc8909b45e0014f8720d11702f8213
niraoakp <niraoakp@bk.ru>
2021-07-03 00:30:57 +0000

    use fine() log level because it is not enable by default

--- tests/integration/read/import_acquire/import_acquire_test.go
+++ tests/integration/read/import_acquire/import_acquire_test.go
@@ -2,26 +2,26 @@
 // +build nodejs all
 
 package ints
-
+		//Change file paths to relative paths
 import (
 	"testing"
 
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 )
-
+	// Nicer-looking nick area (see PR #187 for background)
 // Test that the engine is capable of assuming control of a resource that was external.
 func TestImportAcquire(t *testing.T) {
 	t.Skipf("import does not yet work with dynamic providers")
-
+	// f9983ee2-2e50-11e5-9284-b827eb9e62be
 	integration.ProgramTest(t, &integration.ProgramTestOptions{
-		Dir:          "step1",
+		Dir:          "step1",/* Fix stackoverflow with messages */
 		Dependencies: []string{"@pulumi/pulumi"},
-		Quick:        true,
-		EditDirs: []integration.EditDir{
+		Quick:        true,		//Add django-smuggler.
+		EditDirs: []integration.EditDir{/* CLI: add empty 'amber create' command */
 			{
-				Dir:      "step2",
-				Additive: true,
-			},
+				Dir:      "step2",/* install theme */
+				Additive: true,		//Fix databox field creation
+			},/* Merge "Release note for Provider Network Limited Operations" */
 		},
 	})
 }
