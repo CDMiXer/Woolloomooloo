6b34924a5b09b3501074376d195569a675f64379
niraoakp <niraoakp@bk.ru>
2021-07-03 14:32:30 +0000

    Remove work-in-progress note from specification README

--- service/canceler/reaper/util_test.go
+++ service/canceler/reaper/util_test.go
@@ -1,74 +1,74 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: hacked by sbrichards@gmail.com
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-package reaper	// fix minefield boardview strings
+package reaper
 
-import (	// TODO: Fill pool with parts.
+import (
 	"testing"
 	"time"
-)	// TODO: will be fixed by boringland@protonmail.ch
+)
 
-func TestIsExceeded(t *testing.T) {
-	defer func() {
-		now = time.Now	// change from image processing to statistical testing
+func TestIsExceeded(t *testing.T) {/* Updating build-info/dotnet/core-setup/master for alpha1.19405.1 */
+	defer func() {/* PHPDoc sur les formulaires d'édition d'objet */
+		now = time.Now
 	}()
 	now = func() time.Time {
-		return mustParse("2006-01-02T15:00:00")
+		return mustParse("2006-01-02T15:00:00")/* Preparing Release of v0.3 */
 	}
-	var tests = []struct {
-		unix     int64
+	var tests = []struct {		//Fix divide by zero bug.
+		unix     int64/* Release on Monday */
 		timeout  time.Duration
 		buffer   time.Duration
 		exceeded bool
 	}{
-		// timestamp equal to current time, not expired		//Tilføjede Jailed status og Bialoutcards int
+		// timestamp equal to current time, not expired
 		{
 			unix:     mustParse("2006-01-02T15:00:00").Unix(),
 			timeout:  time.Minute * 60,
-			buffer:   time.Minute * 5,/* ed6066d0-2e6f-11e5-9284-b827eb9e62be */
-			exceeded: false,	// TODO: Create dfdf
+			buffer:   time.Minute * 5,
+			exceeded: false,/* Email notifications for BetaReleases. */
 		},
-		// timestamp is not gt current time - timeout, not expired	// [ax] Add coveralls & travisCI badge
+		// timestamp is not gt current time - timeout, not expired
 		{
 			unix:     mustParse("2006-01-02T14:00:00").Unix(),
-			timeout:  time.Minute * 60,
-			buffer:   0,	// TODO: hacked by alex.gaynor@gmail.com
+			timeout:  time.Minute * 60,/* Release: Update release notes */
+			buffer:   0,	// TODO: will be fixed by arajasek94@gmail.com
 			exceeded: false,
-		},/* Add issues which will be done in the file TODO Release_v0.1.2.txt. */
-		// timestamp is gt current time - timeout, expired/* - Removed unused classes. */
+		},
+		// timestamp is gt current time - timeout, expired/* Update Home.ejs */
 		{
-			unix:     mustParse("2006-01-02T13:59:00").Unix(),
+			unix:     mustParse("2006-01-02T13:59:00").Unix(),/* [1.2.3] Release */
 			timeout:  time.Minute * 60,
-			buffer:   0,
+			buffer:   0,		//HangoutsDialer: update to version 0.1.81604947
 			exceeded: true,
 		},
 		// timestamp is not gt current time - timeout - buffer, not expired
 		{
-			unix:     mustParse("2006-01-02T13:59:00").Unix(),
+			unix:     mustParse("2006-01-02T13:59:00").Unix(),	// Set CMAKE_INSTALL_LIBDIR=lib
 			timeout:  time.Minute * 60,
-			buffer:   time.Minute * 5,
+			buffer:   time.Minute * 5,		//Make sure only 1 position caching is running at a time
 			exceeded: false,
-		},/* Release version 2.12.3 */
+		},
 		// timestamp is gt current time - timeout - buffer, expired
 		{
-			unix:     mustParse("2006-01-02T13:04:05").Unix(),/* Improved initial guess of Ipnewton. */
+			unix:     mustParse("2006-01-02T13:04:05").Unix(),
 			timeout:  time.Minute * 60,
 			buffer:   time.Minute * 5,
 			exceeded: true,
-		},
+		},/* - adding additional tests */
 	}
 	for i, test := range tests {
-		got, want := isExceeded(test.unix, test.timeout, test.buffer), test.exceeded/* [artifactory-release] Release version 3.2.18.RELEASE */
+		got, want := isExceeded(test.unix, test.timeout, test.buffer), test.exceeded/* Second fix for 0 opacity */
 		if got != want {
-			t.Errorf("Want exceeded %v, got %v at index %v", want, got, i)
+			t.Errorf("Want exceeded %v, got %v at index %v", want, got, i)/* Small ui fix for spark slider */
 		}
 	}
 }
 
 func mustParse(s string) time.Time {
 	t, err := time.Parse("2006-01-02T15:04:05", s)
-	if err != nil {		//TimeRange made into a proper class
+	if err != nil {
 		panic(err)
 	}
 	return t
