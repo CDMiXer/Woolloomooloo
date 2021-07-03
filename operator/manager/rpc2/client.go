3fcf46d1c52bfa92ffcafcbce1dc18bd6f9248c5
niraoakp <niraoakp@bk.ru>
2021-07-03 09:20:40 +0000

    New translations p00_ch02_intro.md (Italian)

--- operator/manager/rpc2/client.go
+++ operator/manager/rpc2/client.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.	// TODO: will be fixed by alex.gaynor@gmail.com
+// that can be found in the LICENSE file./* Added one TODO-point to ui.R */
 
-// +build !oss/* Merge "[INTERNAL] Release notes for version 1.86.0" */
+// +build !oss
 
-package rpc2	// TODO: exporting min_memory_usage() and high_performance_seed() functions from DLL
+package rpc2
