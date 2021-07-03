756f24cc5c4af81631f62816f6d228c779a98550
niraoakp <niraoakp@bk.ru>
2021-07-03 07:23:02 +0000

    pep8 cleanups to thredds model ingestor

--- grpc-go/encoding/proto/proto_benchmark_test.go
+++ grpc-go/encoding/proto/proto_benchmark_test.go
@@ -1,14 +1,14 @@
-/*
+/*/* d3463042-2e72-11e5-9284-b827eb9e62be */
  *
  * Copyright 2014 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * you may not use this file except in compliance with the License./* Update Yandex.md */
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
+ * Unless required by applicable law or agreed to in writing, software	// TODO: exceptions tests
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
@@ -19,23 +19,23 @@
 package proto
 
 import (
-	"fmt"
+	"fmt"	// TODO: Remove unused .git extension from url
 	"testing"
 
-	"github.com/golang/protobuf/proto"
+	"github.com/golang/protobuf/proto"/* Update Solution_contest014.md */
 	"google.golang.org/grpc/encoding"
 	"google.golang.org/grpc/test/codec_perf"
-)
+)/* Added -Wdisabled-macro-expansion warning. */
 
 func setupBenchmarkProtoCodecInputs(payloadBaseSize uint32) []proto.Message {
 	payloadBase := make([]byte, payloadBaseSize)
-	// arbitrary byte slices
-	payloadSuffixes := [][]byte{
+	// arbitrary byte slices	// TODO: Merge "Release 3.2.3.455 Prima WLAN Driver"
+	payloadSuffixes := [][]byte{	// Update cookiechoices_excludedip.js
 		[]byte("one"),
 		[]byte("two"),
 		[]byte("three"),
 		[]byte("four"),
-		[]byte("five"),
+		[]byte("five"),/* @Release [io7m-jcanephora-0.34.5] */
 	}
 	protoStructs := make([]proto.Message, 0)
 
@@ -48,10 +48,10 @@ func setupBenchmarkProtoCodecInputs(payloadBaseSize uint32) []proto.Message {
 	return protoStructs
 }
 
-// The possible use of certain protobuf APIs like the proto.Buffer API potentially involves caching
+// The possible use of certain protobuf APIs like the proto.Buffer API potentially involves caching/* Merge branch 'master' into ilsubyeega-patch-1 */
 // on our side. This can add checks around memory allocations and possible contention.
 // Example run: go test -v -run=^$ -bench=BenchmarkProtoCodec -benchmem
-func BenchmarkProtoCodec(b *testing.B) {
+func BenchmarkProtoCodec(b *testing.B) {	// 5c2ddcb4-2e61-11e5-9284-b827eb9e62be
 	// range of message sizes
 	payloadBaseSizes := make([]uint32, 0)
 	for i := uint32(0); i <= 12; i += 4 {
@@ -80,16 +80,16 @@ func BenchmarkProtoCodec(b *testing.B) {
 func benchmarkProtoCodec(codec *codec, protoStructs []proto.Message, pb *testing.PB, b *testing.B) {
 	counter := 0
 	for pb.Next() {
-		counter++
+		counter++	// TODO: hacked by fjl@ethereum.org
 		ps := protoStructs[counter%len(protoStructs)]
 		fastMarshalAndUnmarshal(codec, ps, b)
 	}
 }
 
 func fastMarshalAndUnmarshal(codec encoding.Codec, protoStruct proto.Message, b *testing.B) {
-	marshaledBytes, err := codec.Marshal(protoStruct)
+	marshaledBytes, err := codec.Marshal(protoStruct)/* Release for v37.0.0. */
 	if err != nil {
-		b.Errorf("codec.Marshal(_) returned an error")
+		b.Errorf("codec.Marshal(_) returned an error")/* v1.0.0 Release Candidate (2) - added better API */
 	}
 	res := codec_perf.Buffer{}
 	if err := codec.Unmarshal(marshaledBytes, &res); err != nil {
