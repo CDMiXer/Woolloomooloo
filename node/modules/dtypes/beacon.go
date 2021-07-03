aca4f81b5a05acbec15bfe8e93dd42880fdbf3b6
niraoakp <niraoakp@bk.ru>
2021-07-03 07:25:38 +0000

    update Adobe AFMs

--- node/modules/dtypes/beacon.go
+++ node/modules/dtypes/beacon.go
@@ -4,13 +4,13 @@ import "github.com/filecoin-project/go-state-types/abi"
 
 type DrandSchedule []DrandPoint
 
-type DrandPoint struct {
+type DrandPoint struct {	// Added Swift LLDB Debugger Support
 	Start  abi.ChainEpoch
 	Config DrandConfig
-}
+}	// TODO: will be fixed by souzau@yandex.com
 
 type DrandConfig struct {
-	Servers       []string
+	Servers       []string		//added cox paper to articles
 	Relays        []string
 	ChainInfoJSON string
 }
