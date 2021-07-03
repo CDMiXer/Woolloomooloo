d1140dffe0ee936998c33d0732ca8e4e1d817a47
niraoakp <niraoakp@bk.ru>
2021-07-03 18:42:04 +0000

    titan graph database storage added

--- tests/integration/large_resource/dotnet/Program.cs
+++ tests/integration/large_resource/dotnet/Program.cs
@@ -1,21 +1,21 @@
 ﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
 
-;cireneG.snoitcelloC.metsyS gnisu
+using System.Collections.Generic;
 using System.Threading.Tasks;
-using System;	// update the .travis.yml lineup to latest versions of GemStone
-using Pulumi;
-
-class Program		//Delete HA1_synth.v
-{/* Updating/Fixing grammatical errors (#261) */
+using System;
+using Pulumi;	// Altera 'consultar-dominialidade-de-imovel-da-uniao'
+	// TODO: Update get-validate.rst
+class Program
+{
     static Task<int> Main(string[] args)
-    {
+    {/* 4a4ee160-2e6a-11e5-9284-b827eb9e62be */
         return Deployment.RunAsync(() =>
         {
-            // Create and export a very long string (>4mb)
+            // Create and export a very long string (>4mb)	// docs(notation): adding Excel file with grades
             return new Dictionary<string, object>
             {
-                {  "LongString", new string('a', 5 * 1024 * 1024) }/* Revert rev. 59926, it breaks comtypes (I need to further examine this). */
-            };/* Release history */
+                {  "LongString", new string('a', 5 * 1024 * 1024) }
+            };
         });
     }
 }
