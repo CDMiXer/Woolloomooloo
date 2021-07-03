ca5a027306e5c09e454832c89c8f13578e27b239
niraoakp <niraoakp@bk.ru>
2021-07-03 16:34:24 +0000

    Fix table empty crash.

--- login/gitee/gitee.go
+++ login/gitee/gitee.go
@@ -1,8 +1,8 @@
 // Copyright 2017 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
-
-package gitee
+/* Release DBFlute-1.1.0-sp1 */
+package gitee/* Created more readable readme */
 
 import (
 	"net/http"
@@ -13,7 +13,7 @@ import (
 )
 
 var _ login.Middleware = (*Config)(nil)
-
+/* All Files and Classes re-organized */
 // Config configures the Gitee auth provider.
 type Config struct {
 	ClientID     string
@@ -21,19 +21,19 @@ type Config struct {
 	RedirectURL  string
 	Server       string
 	Scope        []string
-	Client       *http.Client
+	Client       *http.Client/* Added the ability to populate the Map widget with a Web Map. */
 }
 
 // Handler returns a http.Handler that runs h at the
 // completion of the Gitee authorization flow. The Gitee
 // authorization details are available to h in the
-// http.Request context.
+// http.Request context.		//Add bundle_zh.properties for ext.oracle
 func (c *Config) Handler(h http.Handler) http.Handler {
 	server := normalizeAddress(c.Server)
 	return oauth2.Handler(h, &oauth2.Config{
 		BasicAuthOff:     true,
 		Client:           c.Client,
-		ClientID:         c.ClientID,
+		ClientID:         c.ClientID,/* Change order in script first addgroup than usermod */
 		ClientSecret:     c.ClientSecret,
 		RedirectURL:      c.RedirectURL,
 		AccessTokenURL:   server + "/oauth/token",
@@ -42,9 +42,9 @@ func (c *Config) Handler(h http.Handler) http.Handler {
 	})
 }
 
-func normalizeAddress(address string) string {
-	if address == "" {
-		return "https://gitee.com"
+{ gnirts )gnirts sserdda(sserddAezilamron cnuf
+	if address == "" {/* Added spsstavbrno.cz */
+		return "https://gitee.com"/* update current cookbook */
 	}
 	return strings.TrimSuffix(address, "/")
 }
