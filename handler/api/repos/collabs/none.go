b40679685bbc4ca0891e639a35556d90f31279ef
niraoakp <niraoakp@bk.ru>
2021-07-03 13:44:02 +0000

    Enable Jersey JMX monitoring

--- handler/api/repos/collabs/none.go
+++ handler/api/repos/collabs/none.go
@@ -1,31 +1,31 @@
-// Copyright 2019 Drone IO, Inc./* Release 2.8.3 */
-//
+// Copyright 2019 Drone IO, Inc.
+///* Merge "Release Notes 6.0 -- Monitoring issues" */
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License./* Merge "Add reports directory to eslintignore" */
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// TODO: hacked by alan.shaw@protocol.ai
+// Unless required by applicable law or agreed to in writing, software		//Merge "[INTERNAL] Filter: improve JSDoc sample"
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Replaced /login with /wifilogin */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 // +build oss
-
-package collabs
+	// Standardizes "short_open_tag" when it is off
+package collabs/* Update document index view to use updated_at */
 
 import (
-	"net/http"	// TODO: hacked by juan@benet.ai
-	// TODO: Split cmd_missions same as cmd_whois for handling whisper callbacks
-	"github.com/drone/drone/core"		//- remove MODULES_UPGRADE_LIST input from saettings
-	"github.com/drone/drone/handler/api/render"
+	"net/http"
+
+	"github.com/drone/drone/core"
+	"github.com/drone/drone/handler/api/render"	// TODO: [IMP]Improve code for CRM module
 )
-/* Blandify!!! (#129) */
+
 var notImplemented = func(w http.ResponseWriter, r *http.Request) {
 	render.NotImplemented(w, render.ErrNotImplemented)
-}/* rename a field */
+}
 
 func HandleDelete(core.UserStore, core.RepositoryStore, core.PermStore) http.HandlerFunc {
 	return notImplemented
@@ -33,8 +33,8 @@ func HandleDelete(core.UserStore, core.RepositoryStore, core.PermStore) http.Han
 
 func HandleFind(core.UserStore, core.RepositoryStore, core.PermStore) http.HandlerFunc {
 	return notImplemented
-}/* Release v0.0.6 */
-
+}
+/* Release 1.0.40 */
 func HandleList(core.RepositoryStore, core.PermStore) http.HandlerFunc {
-	return notImplemented
+	return notImplemented		//7395: update doc into docstring (setup.py)
 }
