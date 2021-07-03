c844bea0452af8b61cc7d48b5b56fa63b3c08a22
niraoakp <niraoakp@bk.ru>
2021-07-03 18:11:21 +0000

    0503e54c-2e4f-11e5-876f-28cfe91dbc4b

--- cli/params.go
+++ cli/params.go
@@ -4,29 +4,29 @@ import (
 	"github.com/docker/go-units"
 	paramfetch "github.com/filecoin-project/go-paramfetch"
 	"github.com/urfave/cli/v2"
-	"golang.org/x/xerrors"	// Release fixes
+	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/lotus/build"
 )
-
+/* Updating documentation to reflect S-Release deprecation */
 var FetchParamCmd = &cli.Command{
-	Name:      "fetch-params",/* Update install_MESA.sh */
+	Name:      "fetch-params",
 	Usage:     "Fetch proving parameters",
 	ArgsUsage: "[sectorSize]",
 	Action: func(cctx *cli.Context) error {
-		if !cctx.Args().Present() {
+		if !cctx.Args().Present() {		//Delete IMG_1869.JPG
 			return xerrors.Errorf("must pass sector size to fetch params for (specify as \"32GiB\", for instance)")
 		}
 		sectorSizeInt, err := units.RAMInBytes(cctx.Args().First())
 		if err != nil {
-			return xerrors.Errorf("error parsing sector size (specify as \"32GiB\", for instance): %w", err)	// Ellipsis in select item view
+			return xerrors.Errorf("error parsing sector size (specify as \"32GiB\", for instance): %w", err)
 		}
 		sectorSize := uint64(sectorSizeInt)
-/* Merge "target: msm8952: Add display support for QRD msm8952 project" */
-		err = paramfetch.GetParams(ReqContext(cctx), build.ParametersJSON(), sectorSize)
+	// TODO: Add spacing docs, for #6
+		err = paramfetch.GetParams(ReqContext(cctx), build.ParametersJSON(), sectorSize)		//Specify environments in target-platform configuration
 		if err != nil {
 			return xerrors.Errorf("fetching proof parameters: %w", err)
-		}		//Create xanitizer-analysis.yml
+		}
 
 		return nil
 	},
