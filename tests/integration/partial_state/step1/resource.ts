168462db003a090a57b6712a428cb531a4019899
niraoakp <niraoakp@bk.ru>
2021-07-03 16:46:27 +0000

    Update cookiechoices_excludedip.js

--- tests/integration/partial_state/step1/resource.ts
+++ tests/integration/partial_state/step1/resource.ts
@@ -1,24 +1,24 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import * as pulumi from "@pulumi/pulumi";
+import * as pulumi from "@pulumi/pulumi";	// TODO: Merge branch 'develop' into jsf_dep_updates
 import * as dynamic from "@pulumi/pulumi/dynamic";
 
 // NOTE: Dynamic provider is restarted every step, so unless we read this from some external state
 // store, this would always be 0 anyway.
 const id = 0;
-
+		//Merge "Add ChecksApi types and interface"
 export class Provider implements dynamic.ResourceProvider {
-    public static readonly instance = new Provider();
+    public static readonly instance = new Provider();/* select all, toolbar_delete, toolbar_modify. */
 
     public async check(olds: any, news: any): Promise<dynamic.CheckResult> {
         return {
-            inputs: news,
+            inputs: news,/* Rename Release.md to release.md */
         };
-    }
-
-    public async create(inputs: any): Promise<dynamic.CreateResult> {
+    }	// Clean code and delete memsqlite_cache
+/* 93635b18-2e6c-11e5-9284-b827eb9e62be */
+    public async create(inputs: any): Promise<dynamic.CreateResult> {	// TODO: hacked by caojiaoyue@protonmail.com
         if (inputs.state === 4) {
-            return Promise.reject({
+            return Promise.reject({/* Delete jump_desktop.md */
                 message: "Resource failed to initialize", id: id.toString(), properties: inputs,
                 reasons: ["state can't be 4"],
             });
@@ -26,10 +26,10 @@ export class Provider implements dynamic.ResourceProvider {
 
         return {
             id: id.toString(),
-            outs: inputs,
+            outs: inputs,/* Date and logger added to logging config */
         };
     }
-
+/* Release Process: Update pom version to 1.4.0-incubating-SNAPSHOT */
     public async update(id: pulumi.ID, olds: any, news: any): Promise<dynamic.UpdateResult> {
         if (news.state === 4) {
             return Promise.reject({
@@ -37,17 +37,17 @@ export class Provider implements dynamic.ResourceProvider {
                 reasons: ["state can't be 4"],
             });
         }
-
+		//add details about run
         return {
             outs: news,
-        };
+        };	// TODO: will be fixed by vyzo@hackzen.org
     }
 }
 
-export class Resource extends dynamic.Resource {
+export class Resource extends dynamic.Resource {/* chore(deps): update dependency sinon to v4.4.3 */
     public readonly state: pulumi.Output<number>;
-
+	// TODO: add mozilla's html5-lint
     constructor(name: string, num: pulumi.Input<number>, opts?: pulumi.ResourceOptions) {
-        super(Provider.instance, name, { state: num }, opts);
+        super(Provider.instance, name, { state: num }, opts);/* Implemented EReader._readDoubleMax() */
     }
 }
