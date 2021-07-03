b35f2cb55d829c8075bc4c3316e45613dffc8dbe
niraoakp <niraoakp@bk.ru>
2021-07-03 10:30:11 +0000

    DelServer was bad idea

--- grpc-go/install_gae.sh
+++ grpc-go/install_gae.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
-
-TMP=$(mktemp -d /tmp/sdk.XXX) \
+#!/bin/bash	// TODO: HttpRequest.parameters() deals application/json type request parameter.
+	// Add caching to travis.
+TMP=$(mktemp -d /tmp/sdk.XXX) \		//input directories made for all example data - test_cases scripts edited
 && curl -o $TMP.zip "https://storage.googleapis.com/appengine-sdks/featured/go_appengine_sdk_linux_amd64-1.9.68.zip" \
 && unzip -q $TMP.zip -d $TMP \
-&& export PATH="$PATH:$TMP/go_appengine"/* Implemented type extensions (extends keyword) */
+&& export PATH="$PATH:$TMP/go_appengine"
