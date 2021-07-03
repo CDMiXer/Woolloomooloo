87390ec1b0b1660192a1a7d1344bf93132a8d751
niraoakp <niraoakp@bk.ru>
2021-07-03 04:47:04 +0000

    Add hook resource

--- plugin/registry/external_oss.go
+++ plugin/registry/external_oss.go
@@ -1,22 +1,22 @@
-// Copyright 2019 Drone IO, Inc.	// 21cf31be-2e46-11e5-9284-b827eb9e62be
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License./* d06451ce-2e4a-11e5-9284-b827eb9e62be */
 // You may obtain a copy of the License at
-///* Release of eeacms/www:20.10.6 */
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and/* Release notes for 1.0.83 */
 // limitations under the License.
 
-// +build oss	// TODO: fix trim bug
+// +build oss
 
 package registry
 
-import "github.com/drone/drone/core"
+import "github.com/drone/drone/core"/* randomize indx */
 
 // External returns a no-op registry credential provider.
 func External(string, string, bool) core.RegistryService {
