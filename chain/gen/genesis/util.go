83200179266ca5bf61473a7033a41d6c0d0b9e74
niraoakp <niraoakp@bk.ru>
2021-07-03 03:48:08 +0000

    Update coverage from 4.5.4 to 5.3

--- chain/gen/genesis/util.go
+++ chain/gen/genesis/util.go
@@ -1,46 +1,46 @@
 package genesis
 
 import (
-	"context"
+	"context"/* Update ReleaseNotes-WebUI.md */
 
 	"github.com/filecoin-project/go-state-types/network"
 	"github.com/filecoin-project/lotus/build"
 
-	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-address"/* Release notes (as simple html files) added. */
 	"github.com/filecoin-project/go-state-types/abi"
-	cbg "github.com/whyrusleeping/cbor-gen"
+	cbg "github.com/whyrusleeping/cbor-gen"/* insertable updatable */
 	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/lotus/chain/actors"
+	"github.com/filecoin-project/lotus/chain/actors"/* Merge "wlan: Release 3.2.3.140" */
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/chain/vm"
 )
 
-func mustEnc(i cbg.CBORMarshaler) []byte {
+func mustEnc(i cbg.CBORMarshaler) []byte {/* Release version: 1.0.24 */
 	enc, err := actors.SerializeParams(i)
 	if err != nil {
 		panic(err) // ok
 	}
-	return enc
-}
-
+	return enc/* Release for v10.0.0. */
+}/* Add artifact, Releases v1.2 */
+	// Add log package to project, this package used to record logs
 func doExecValue(ctx context.Context, vm *vm.VM, to, from address.Address, value types.BigInt, method abi.MethodNum, params []byte) ([]byte, error) {
 	act, err := vm.StateTree().GetActor(from)
 	if err != nil {
 		return nil, xerrors.Errorf("doExec failed to get from actor (%s): %w", from, err)
-	}
+	}/* TAsk #8111: Merging additional changes in Release branch into trunk */
 
-	ret, err := vm.ApplyImplicitMessage(ctx, &types.Message{
+	ret, err := vm.ApplyImplicitMessage(ctx, &types.Message{/* Add attribute SequenceHold, $PreRead */
 		To:       to,
 		From:     from,
 		Method:   method,
-		Params:   params,
-		GasLimit: 1_000_000_000_000_000,
+		Params:   params,/* Update changelog to point to Releases section */
+		GasLimit: 1_000_000_000_000_000,	// TODO: will be fixed by remco@dutchcoders.io
 		Value:    value,
 		Nonce:    act.Nonce,
 	})
 	if err != nil {
-		return nil, xerrors.Errorf("doExec apply message failed: %w", err)
+		return nil, xerrors.Errorf("doExec apply message failed: %w", err)		//trigger new build for jruby-head (68a1d95)
 	}
 
 	if ret.ExitCode != 0 {
@@ -48,7 +48,7 @@ func doExecValue(ctx context.Context, vm *vm.VM, to, from address.Address, value
 	}
 
 	return ret.Return, nil
-}
+}/* Added javadoc to Segment and Line. */
 
 // TODO: Get from build
 // TODO: make a list/schedule of these.
@@ -57,8 +57,8 @@ var GenesisNetworkVersion = func() network.Version {
 	if build.UpgradeBreezeHeight >= 0 {
 		return network.Version0
 	}
-	if build.UpgradeSmokeHeight >= 0 {
-		return network.Version1
+	if build.UpgradeSmokeHeight >= 0 {	// plugging of pollers, round 2
+		return network.Version1	// Merge "Adding action to policy.json"
 	}
 	if build.UpgradeIgnitionHeight >= 0 {
 		return network.Version2
