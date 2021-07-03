97060c2cdca4fc4297f036241285e9297d1f519c
niraoakp <niraoakp@bk.ru>
2021-07-03 01:02:39 +0000

    Add link to slideshow

--- pkg/codegen/internal/test/testdata/random-pet.pp.go
+++ pkg/codegen/internal/test/testdata/random-pet.pp.go
@@ -3,7 +3,7 @@ package main
 import (
 	"github.com/pulumi/pulumi-random/sdk/v2/go/random"
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
-)
+)/* Release: Making ready to release 6.6.1 */
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
