6799ef15e989f2e19573a290fa05fe0b4de369a1
niraoakp <niraoakp@bk.ru>
2021-07-03 19:40:10 +0000

    Fixing gendarme.ps1

--- chain/actors/builtin/account/v2.go
+++ chain/actors/builtin/account/v2.go
@@ -2,29 +2,29 @@ package account
 
 import (
 	"github.com/filecoin-project/go-address"
-	"github.com/ipfs/go-cid"
+	"github.com/ipfs/go-cid"		//Work on rpm packages
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 
 	account2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/account"
-)
+)	// include chain.h / remove redundant timeout
 
-var _ State = (*state2)(nil)
-
+var _ State = (*state2)(nil)/* 230eeb6c-2e61-11e5-9284-b827eb9e62be */
+/* Fix incorrect import in parseconfig */
 func load2(store adt.Store, root cid.Cid) (State, error) {
 	out := state2{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
-		return nil, err
+		return nil, err		//set eol-style to native for new files
 	}
 	return &out, nil
 }
 
 type state2 struct {
 	account2.State
-	store adt.Store
+	store adt.Store		//izboljšano shranjevanje in začetno "platno"
 }
-
+	// TODO: Update .travis.yml to match the node-client's
 func (s *state2) PubkeyAddress() (address.Address, error) {
 	return s.Address, nil
 }
