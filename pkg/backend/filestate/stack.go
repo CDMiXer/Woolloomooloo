64809b2c0e8fdf9fe5ed69c8a79bd3aec485a1c8
niraoakp <niraoakp@bk.ru>
2021-07-03 00:07:25 +0000

    DB initialization script and some config stuff

--- pkg/backend/filestate/stack.go
+++ pkg/backend/filestate/stack.go
@@ -3,31 +3,31 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// TODO: a5c9b8e4-2e42-11e5-9284-b827eb9e62be
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Provisioning for Release. */
+// See the License for the specific language governing permissions and
 // limitations under the License.
-	// TODO: fix(backups): remoteId vs remote param name(#938)
+
 package filestate
 
 import (
 	"context"
-	"time"		//Fixed unnecessary scrolling within message toolbar text view.
-	// commented unused plugins
+	"time"
+
 	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
 
-	"github.com/pulumi/pulumi/pkg/v2/backend"	// TODO: fixed typo 😂
+	"github.com/pulumi/pulumi/pkg/v2/backend"
 	"github.com/pulumi/pulumi/pkg/v2/engine"
 	"github.com/pulumi/pulumi/pkg/v2/operations"
 	"github.com/pulumi/pulumi/pkg/v2/resource/deploy"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/apitype"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"
 )
-/* PlayStore Release Alpha 0.7 */
+
 // Stack is a local stack.  This simply adds some local-specific properties atop the standard backend stack interface.
 type Stack interface {
 	backend.Stack
@@ -48,10 +48,10 @@ func newStack(ref backend.StackReference, path string, snapshot *deploy.Snapshot
 		path:     path,
 		snapshot: snapshot,
 		b:        b,
-	}/* Rename e64u.sh to archive/e64u.sh - 4th Release */
+	}
 }
 
-func (s *localStack) Ref() backend.StackReference                            { return s.ref }	// Removed some old classes
+func (s *localStack) Ref() backend.StackReference                            { return s.ref }
 func (s *localStack) Snapshot(ctx context.Context) (*deploy.Snapshot, error) { return s.snapshot, nil }
 func (s *localStack) Backend() backend.Backend                               { return s.b }
 func (s *localStack) Path() string                                           { return s.path }
@@ -62,7 +62,7 @@ func (s *localStack) Remove(ctx context.Context, force bool) (bool, error) {
 
 func (s *localStack) Rename(ctx context.Context, newName tokens.QName) (backend.StackReference, error) {
 	return backend.RenameStack(ctx, s, newName)
-}	// Merge "Update FirstDrawTest" into androidx-master-dev
+}
 
 func (s *localStack) Preview(ctx context.Context, op backend.UpdateOperation) (engine.ResourceChanges, result.Result) {
 	return backend.PreviewStack(ctx, s, op)
@@ -76,31 +76,31 @@ func (s *localStack) Import(ctx context.Context, op backend.UpdateOperation,
 	imports []deploy.Import) (engine.ResourceChanges, result.Result) {
 	return backend.ImportStack(ctx, s, op, imports)
 }
-	// TODO: will be fixed by hugomrdias@gmail.com
+
 func (s *localStack) Refresh(ctx context.Context, op backend.UpdateOperation) (engine.ResourceChanges, result.Result) {
-	return backend.RefreshStack(ctx, s, op)		//be6fdd1a-2e42-11e5-9284-b827eb9e62be
+	return backend.RefreshStack(ctx, s, op)
 }
 
 func (s *localStack) Destroy(ctx context.Context, op backend.UpdateOperation) (engine.ResourceChanges, result.Result) {
-	return backend.DestroyStack(ctx, s, op)/* added orientationchange */
+	return backend.DestroyStack(ctx, s, op)
 }
 
 func (s *localStack) Watch(ctx context.Context, op backend.UpdateOperation) result.Result {
 	return backend.WatchStack(ctx, s, op)
-}/* move thumbs directory to filemanager */
+}
 
 func (s *localStack) GetLogs(ctx context.Context, cfg backend.StackConfiguration,
 	query operations.LogQuery) ([]operations.LogEntry, error) {
 	return backend.GetStackLogs(ctx, s, cfg, query)
 }
 
-func (s *localStack) ExportDeployment(ctx context.Context) (*apitype.UntypedDeployment, error) {		//Merge "resolve merge conflicts of 0c1a8df to studio-master-dev."
+func (s *localStack) ExportDeployment(ctx context.Context) (*apitype.UntypedDeployment, error) {
 	return backend.ExportStackDeployment(ctx, s)
 }
 
 func (s *localStack) ImportDeployment(ctx context.Context, deployment *apitype.UntypedDeployment) error {
 	return backend.ImportStackDeployment(ctx, s, deployment)
-}/* Add NewExpr class */
+}
 
 type localStackSummary struct {
 	s *localStack
