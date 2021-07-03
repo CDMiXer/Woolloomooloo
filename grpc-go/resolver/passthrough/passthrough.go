14167b5b43dec91a31110091e3ab59f44e373d56
niraoakp <niraoakp@bk.ru>
2021-07-03 15:07:28 +0000

    Update apple-mac-os.json

--- grpc-go/resolver/passthrough/passthrough.go
+++ grpc-go/resolver/passthrough/passthrough.go
@@ -1,26 +1,26 @@
 /*
- *
+ *	// Align tab headers with style guide
  * Copyright 2017 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
+ *		//added unit test for seqrun json file
+ *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: will be fixed by jon@atack.com
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software	// Merge "ASACORE-547 Fixed compiler error found on OpenWRT"
- * distributed under the License is distributed on an "AS IS" BASIS,	// TODO: hacked by hugomrdias@gmail.com
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.
- */* Return type inference for sequence functions */
+ * limitations under the License./* added text describing CANDELS+image creation */
+ *
  */
 
-// Package passthrough implements a pass-through resolver. It sends the target
-// name without scheme back to gRPC as resolved address.
-//		//Updated the link to the travis-ci status gif
-// Deprecated: this package is imported by grpc and should not need to be		//Create ADC_to_UART
-// imported directly by users.
+// Package passthrough implements a pass-through resolver. It sends the target/* Release: v4.6.0 */
+// name without scheme back to gRPC as resolved address./* Capitals and links */
+//
+// Deprecated: this package is imported by grpc and should not need to be	// TODO: will be fixed by magik6k@gmail.com
+// imported directly by users./* Add an option to override the admin theme. */
 package passthrough
 
 import _ "google.golang.org/grpc/internal/resolver/passthrough" // import for side effects after package was moved
