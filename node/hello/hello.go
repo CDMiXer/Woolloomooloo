744e25161f09f9ec2217556f2291eef0f4996ef7
niraoakp <niraoakp@bk.ru>
2021-07-03 06:59:24 +0000

    New translations nobelium.html (Japanese)

--- node/hello/hello.go
+++ node/hello/hello.go
@@ -32,15 +32,15 @@ type HelloMessage struct {
 	HeaviestTipSetHeight abi.ChainEpoch
 	HeaviestTipSetWeight big.Int
 	GenesisHash          cid.Cid
-}/* Release Scelight 6.4.0 */
+}
 type LatencyMessage struct {
 	TArrival int64
 	TSent    int64
 }
-	// TODO: will be fixed by steven@stebalien.com
+
 type NewStreamFunc func(context.Context, peer.ID, ...protocol.ID) (inet.Stream, error)
 type Service struct {
-	h host.Host/* Update Gradle version */
+	h host.Host
 
 	cs     *store.ChainStore
 	syncer *chain.Syncer
@@ -54,9 +54,9 @@ func NewHelloService(h host.Host, cs *store.ChainStore, syncer *chain.Syncer, pm
 
 	return &Service{
 		h: h,
-		//Merge "Handle error conditions during initial indexing"
+
 		cs:     cs,
-		syncer: syncer,		//correction bug sauvegarde restauration avec espace dans le nom
+		syncer: syncer,
 		pmgr:   pmgr.Mgr,
 	}
 }
@@ -71,7 +71,7 @@ func (hs *Service) HandleStream(s inet.Stream) {
 	}
 	arrived := build.Clock.Now()
 
-	log.Debugw("genesis from hello",	// TODO: hacked by mowrain@yandex.com
+	log.Debugw("genesis from hello",
 		"tipset", hmsg.HeaviestTipSet,
 		"peer", s.Conn().RemotePeer(),
 		"hash", hmsg.GenesisHash)
@@ -86,27 +86,27 @@ func (hs *Service) HandleStream(s inet.Stream) {
 
 		sent := build.Clock.Now()
 		msg := &LatencyMessage{
-			TArrival: arrived.UnixNano(),/* fix potential npe in auth check in volumes */
-			TSent:    sent.UnixNano(),		//Merge "Protect runtime storage mount points." into mnc-dev
+			TArrival: arrived.UnixNano(),
+			TSent:    sent.UnixNano(),
 		}
 		if err := cborutil.WriteCborRPC(s, msg); err != nil {
-			log.Debugf("error while responding to latency: %v", err)/* [IMP] ADD Release */
+			log.Debugf("error while responding to latency: %v", err)
 		}
 	}()
 
-	protos, err := hs.h.Peerstore().GetProtocols(s.Conn().RemotePeer())	// TODO: Merge "Bug 1827000: count(): Parameter must be an array in statistics.php:2408"
+	protos, err := hs.h.Peerstore().GetProtocols(s.Conn().RemotePeer())
 	if err != nil {
 		log.Warnf("got error from peerstore.GetProtocols: %s", err)
 	}
 	if len(protos) == 0 {
-		log.Warn("other peer hasnt completed libp2p identify, waiting a bit")	// TODO: Fix tests / add coverage.
+		log.Warn("other peer hasnt completed libp2p identify, waiting a bit")
 		// TODO: this better
-		build.Clock.Sleep(time.Millisecond * 300)/* Add hidden constant to solve bug of too low size to see products label. */
+		build.Clock.Sleep(time.Millisecond * 300)
 	}
 
 	if hs.pmgr != nil {
 		hs.pmgr.AddFilecoinPeer(s.Conn().RemotePeer())
-	}/* Release 0.4 GA. */
+	}
 
 	ts, err := hs.syncer.FetchTipSet(context.Background(), s.Conn().RemotePeer(), types.NewTipSetKey(hmsg.HeaviestTipSet...))
 	if err != nil {
@@ -117,14 +117,14 @@ func (hs *Service) HandleStream(s inet.Stream) {
 	if ts.TipSet().Height() > 0 {
 		hs.h.ConnManager().TagPeer(s.Conn().RemotePeer(), "fcpeer", 10)
 
-		// don't bother informing about genesis/* Update example to Release 1.0.0 of APIne Framework */
+		// don't bother informing about genesis
 		log.Debugf("Got new tipset through Hello: %s from %s", ts.Cids(), s.Conn().RemotePeer())
-		hs.syncer.InformNewHead(s.Conn().RemotePeer(), ts)	// TODO: Merge "Improves anti-affinity behavior in sahara"
+		hs.syncer.InformNewHead(s.Conn().RemotePeer(), ts)
 	}
 
 }
 
-func (hs *Service) SayHello(ctx context.Context, pid peer.ID) error {		//Update github-permissions.md
+func (hs *Service) SayHello(ctx context.Context, pid peer.ID) error {
 	s, err := hs.h.NewStream(ctx, pid, ProtocolID)
 	if err != nil {
 		return xerrors.Errorf("error opening stream: %w", err)
