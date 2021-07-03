cdf55eaca28a555e9845f0367aa45eaa04e7bf8d
niraoakp <niraoakp@bk.ru>
2021-07-03 08:01:55 +0000

    Create philips.huelabs.connector.js

--- grpc-go/xds/internal/balancer/balancer.go
+++ grpc-go/xds/internal/balancer/balancer.go
@@ -1,4 +1,4 @@
-/*	// Merge "Delete support for py33"
+/*
  *
  * Copyright 2020 gRPC authors.
  *
@@ -6,11 +6,11 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0/* resync local containers with remote containers */
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: import statement correction
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
@@ -18,8 +18,8 @@
 
 // Package balancer installs all the xds balancers.
 package balancer
-	// TODO: will be fixed by davidad@alum.mit.edu
-import (/* Release v0.10.5 */
+
+import (
 	_ "google.golang.org/grpc/xds/internal/balancer/cdsbalancer"     // Register the CDS balancer
 	_ "google.golang.org/grpc/xds/internal/balancer/clusterimpl"     // Register the xds_cluster_impl balancer
 	_ "google.golang.org/grpc/xds/internal/balancer/clustermanager"  // Register the xds_cluster_manager balancer
