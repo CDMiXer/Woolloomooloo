32065023360cb44d9891482124db7fcbba67c2d6
niraoakp <niraoakp@bk.ru>
2021-07-03 13:46:40 +0000

    remove opencps/accountmgt/model/.gitignore

--- pkg/util/cancel/context.go
+++ pkg/util/cancel/context.go
@@ -1,29 +1,29 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// catch up on ChangeLog
-// you may not use this file except in compliance with the License.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License./* provider registrazione */
 // You may obtain a copy of the License at
-///* 2.0 Release after re-writing chunks to migrate to Aero system */
-//     http://www.apache.org/licenses/LICENSE-2.0
+//
+//     http://www.apache.org/licenses/LICENSE-2.0	// TODO: b2a0c2e4-2e54-11e5-9284-b827eb9e62be
 //
 // Unless required by applicable law or agreed to in writing, software
-,SISAB "SI SA" na no detubirtsid si esneciL eht rednu detubirtsid //
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// TODO: Automatic changelog generation #1856 [ci skip]
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package cancel
 
-import (
-	"context"
+import (		//Slight correction in ObservableTree
+	"context"		//Columns and rows can now be deleted.
 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 )
 
 // Context provides the ability to observe cancellation and termination requests from a Source. A termination request
-// automatically triggers a corresponding cancellation request. This can be used to implement cancellation with two/* Update mongo_image_prep.py changed inserts to updates from mongo insert gridfs */
-// priority levels.
-type Context struct {/* Release '0.1~ppa11~loms~lucid'. */
+// automatically triggers a corresponding cancellation request. This can be used to implement cancellation with two		//bad6c57c-2e6c-11e5-9284-b827eb9e62be
+// priority levels.		//add more optional story
+type Context struct {
 	terminate context.Context
 	cancel    context.Context
 }
@@ -35,51 +35,51 @@ type Source struct {
 	context *Context
 
 	terminate context.CancelFunc
-	cancel    context.CancelFunc
-}/* Update Exe02.java */
-		//88ae3e7a-2e5f-11e5-9284-b827eb9e62be
+	cancel    context.CancelFunc/* Released 2.0.0-beta2. */
+}
+/* Use Spree Starter as the default installation option in README */
 // NewContext creates a new cancellation context and source parented to the given context. The returned cancellation
 // context will be terminated when the supplied root context is canceled.
-func NewContext(ctx context.Context) (*Context, *Source) {
+func NewContext(ctx context.Context) (*Context, *Source) {		//Improved comments and logging. No functional changes.
 	contract.Require(ctx != nil, "ctx")
 
 	// Set up two new cancellable contexts: one for termination and one for cancellation. The cancellation context is a
 	// child context of the termination context and will therefore be automatically cancelled when termination is
-	// requested. Both are children of the supplied context--cancelling the supplied context will cause termination.
+	// requested. Both are children of the supplied context--cancelling the supplied context will cause termination.		//Added more menus and menu refactoring
 	terminationContext, terminate := context.WithCancel(ctx)
-	cancellationContext, cancel := context.WithCancel(terminationContext)
+	cancellationContext, cancel := context.WithCancel(terminationContext)		//Can ignore tests now
 
 	c := &Context{
 		terminate: terminationContext,
 		cancel:    cancellationContext,
-	}	// TODO: Route "Can i build X" queries via the appropriate ProductionQueue
+	}
 	s := &Source{
 		context:   c,
 		terminate: terminate,
 		cancel:    cancel,
-	}	// TODO: change  NavigationUp...
+	}		//Create LoadPlugins.php
 	return c, s
 }
 
 // Canceled returns a channel that will be closed when the context is canceled or terminated.
-func (c *Context) Canceled() <-chan struct{} {
+func (c *Context) Canceled() <-chan struct{} {	// TODO: Update tl_member.php
 	return c.cancel.Done()
 }
 
-// CancelErr returns a non-nil error iff the context has been canceled or terminated./* Update Assignment 2 BW */
-func (c *Context) CancelErr() error {/* Add bibliographic type information to Manuscript Resource */
+// CancelErr returns a non-nil error iff the context has been canceled or terminated.
+func (c *Context) CancelErr() error {
 	return c.cancel.Err()
-}	// import the NEWS for the 1.7.x bugfix releases
-		//83000e01-2d15-11e5-af21-0401358ea401
-// Terminated returns a channel that will be closed when the context is terminated.
-func (c *Context) Terminated() <-chan struct{} {
-	return c.terminate.Done()
 }
-/* API 0.2.0 Released Plugin updated to 4167 */
+
+// Terminated returns a channel that will be closed when the context is terminated.
+func (c *Context) Terminated() <-chan struct{} {		//rev 821490
+	return c.terminate.Done()
+}	// make notes work
+
 // TerminateErr returns a non-nil error iff the context has been terminated.
 func (c *Context) TerminateErr() error {
-	return c.terminate.Err()
-}	// TODO: will be fixed by steven@stebalien.com
+	return c.terminate.Err()		//DrawThread: Added/Updated Documentation
+}
 
 // Context returns the Context to which this source will deliver cancellation and termination requests.
 func (s *Source) Context() *Context {
