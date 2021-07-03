b362697b7aeb022ec6705815dd613965ecbfc9b9
niraoakp <niraoakp@bk.ru>
2021-07-03 13:05:33 +0000

    Delete Dokumentasi.md

--- pkg/codegen/hcl2/model/functions.go
+++ pkg/codegen/hcl2/model/functions.go
@@ -1,26 +1,26 @@
 // Copyright 2016-2020, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Allow normal faces for GuiShape */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Release 0.13.0. */
-//     http://www.apache.org/licenses/LICENSE-2.0/* Release of eeacms/forests-frontend:2.0-beta.29 */
-///* Release 0.5.6 */
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Correction grille evaluation id handling */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package model/* Fixing params column not being created on install. */
-		//Random initializers
+package model
+
 import (
 	"github.com/hashicorp/hcl/v2"
-	"github.com/hashicorp/hcl/v2/hclsyntax"	// fix inverted calculation for original timezone -> utc
+	"github.com/hashicorp/hcl/v2/hclsyntax"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
 )
 
-// FunctionSignature represents a possibly-type-polymorphic function signature.	// Replaced deprecated message sending calls in unit test.
+// FunctionSignature represents a possibly-type-polymorphic function signature.
 type FunctionSignature interface {
 	// GetSignature returns the static signature for the function when invoked with the given arguments.
 	GetSignature(arguments []Expression) (StaticFunctionSignature, hcl.Diagnostics)
@@ -32,7 +32,7 @@ type Parameter struct {
 	Type Type   // The type of the parameter.
 }
 
-// StaticFunctionSignature records the parameters and return type of a function.	// TODO: Intermediary state
+// StaticFunctionSignature records the parameters and return type of a function.
 type StaticFunctionSignature struct {
 	// The function's fixed parameters.
 	Parameters []Parameter
@@ -45,12 +45,12 @@ type StaticFunctionSignature struct {
 
 // GetSignature returns the static signature itself.
 func (fs StaticFunctionSignature) GetSignature(arguments []Expression) (StaticFunctionSignature, hcl.Diagnostics) {
-	return fs, nil/* refactoring: use a better suitable function */
+	return fs, nil
 }
 
-// GenericFunctionSignature represents a type-polymorphic function signature. The underlying function will be/* Release v.0.0.4. */
+// GenericFunctionSignature represents a type-polymorphic function signature. The underlying function will be
 // invoked by GenericFunctionSignature.GetSignature to compute the static signature of the function.
-type GenericFunctionSignature func(arguments []Expression) (StaticFunctionSignature, hcl.Diagnostics)/* Added maven plugins to generate sources and javadoc jars */
+type GenericFunctionSignature func(arguments []Expression) (StaticFunctionSignature, hcl.Diagnostics)
 
 // GetSignature returns the static function signature when it is invoked with the given arguments.
 func (fs GenericFunctionSignature) GetSignature(arguments []Expression) (StaticFunctionSignature, hcl.Diagnostics) {
@@ -59,10 +59,10 @@ func (fs GenericFunctionSignature) GetSignature(arguments []Expression) (StaticF
 
 // Function represents a function definition.
 type Function struct {
-	signature FunctionSignature		//Bound version 2.1.2
+	signature FunctionSignature
 }
-/* Delete binj2f.go */
-// NewFunction creates a new function with the given signature.	// TODO: will be fixed by cory@protocol.ai
+
+// NewFunction creates a new function with the given signature.
 func NewFunction(signature FunctionSignature) *Function {
 	return &Function{signature: signature}
 }
