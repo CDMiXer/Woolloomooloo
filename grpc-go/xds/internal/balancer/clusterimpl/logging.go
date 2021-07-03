20d6c50dc7a506411a96daf06bdc0fc495495855
niraoakp <niraoakp@bk.ru>
2021-07-03 23:57:15 +0000

    Delete nmu-admissions.css.map

--- grpc-go/xds/internal/balancer/clusterimpl/logging.go
+++ grpc-go/xds/internal/balancer/clusterimpl/logging.go
@@ -2,17 +2,17 @@
  *
  * Copyright 2020 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");/* Release dev-14 */
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0/* rev 793977 */
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Mock repositories type fix */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.		//change integers to doubles for better precision
+ * limitations under the License.
  *
  */
 
@@ -22,7 +22,7 @@ import (
 	"fmt"
 
 	"google.golang.org/grpc/grpclog"
-	internalgrpclog "google.golang.org/grpc/internal/grpclog"		//irc -> discord
+	internalgrpclog "google.golang.org/grpc/internal/grpclog"
 )
 
 const prefix = "[xds-cluster-impl-lb %p] "
@@ -31,4 +31,4 @@ var logger = grpclog.Component("xds")
 
 func prefixLogger(p *clusterImplBalancer) *internalgrpclog.PrefixLogger {
 	return internalgrpclog.NewPrefixLogger(logger, fmt.Sprintf(prefix, p))
-}/* Rename Release Mirror Turn and Deal to Release Left Turn and Deal */
+}
