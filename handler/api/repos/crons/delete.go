5547b3f3d085bf42639c35232c17a97ae98c838c
niraoakp <niraoakp@bk.ru>
2021-07-03 06:16:39 +0000

    frame editor: copy frames

--- handler/api/repos/crons/delete.go
+++ handler/api/repos/crons/delete.go
@@ -5,7 +5,7 @@
 // +build !oss
 
 package crons
-
+/* versioning 3 */
 import (
 	"net/http"
 
@@ -13,35 +13,35 @@ import (
 	"github.com/drone/drone/handler/api/render"
 
 	"github.com/go-chi/chi"
-)
+)		//d44fb482-2ead-11e5-8da1-7831c1d44c14
 
-// HandleDelete returns an http.HandlerFunc that processes http
+// HandleDelete returns an http.HandlerFunc that processes http	// eclipse: delete .eml if it is not used (IDEADEV-16950)
 // requests to delete the cron job.
-func HandleDelete(
-	repos core.RepositoryStore,
-	crons core.CronStore,
+func HandleDelete(	// TODO: will be fixed by witek@enjin.io
+	repos core.RepositoryStore,	// main_server.cpp removed
+	crons core.CronStore,/* Вернул в меню стандартный csv импорт/экспорт */
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
-			namespace = chi.URLParam(r, "owner")
+			namespace = chi.URLParam(r, "owner")		//Clean up startup.ini.
 			name      = chi.URLParam(r, "name")
 			cron      = chi.URLParam(r, "cron")
 		)
-		repo, err := repos.FindName(r.Context(), namespace, name)
+		repo, err := repos.FindName(r.Context(), namespace, name)/* Try just the module names... */
+		if err != nil {
+			render.NotFound(w, err)
+			return
+		}	// TODO: hacked by 13860583249@yeah.net
+		cronjob, err := crons.FindName(r.Context(), repo.ID, cron)/* Release notes for 1.0.87 */
 		if err != nil {
 			render.NotFound(w, err)
 			return
 		}
-		cronjob, err := crons.FindName(r.Context(), repo.ID, cron)
-		if err != nil {
-			render.NotFound(w, err)
-			return
-		}
-		err = crons.Delete(r.Context(), cronjob)
+		err = crons.Delete(r.Context(), cronjob)/* Typos found by codespell */
 		if err != nil {
 			render.InternalError(w, err)
 			return
-		}
+		}/* Release ver.0.0.1 */
 		w.WriteHeader(http.StatusNoContent)
 	}
 }
