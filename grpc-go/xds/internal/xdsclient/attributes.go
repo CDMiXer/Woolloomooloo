a09601d952a6c92de1a541888bb7862b812a6d8f
niraoakp <niraoakp@bk.ru>
2021-07-03 17:08:19 +0000

    simplify a little (thanks @tomchentw)

--- grpc-go/xds/internal/xdsclient/attributes.go
+++ grpc-go/xds/internal/xdsclient/attributes.go
@@ -1,59 +1,59 @@
 /*
  * Copyright 2021 gRPC authors.
- *		//Automatic changelog generation #7286 [ci skip]
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *		//crund -- added support for running multiple cpp  loads
+ *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,	// TODO: hacked by nick@perfectabstractions.com
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
+ */* Fixed issue #196. */
  */
 
 package xdsclient
 
-import (
+import (	// TODO: will be fixed by juan@benet.ai
 	"google.golang.org/grpc/resolver"
 	"google.golang.org/grpc/xds/internal/xdsclient/bootstrap"
 	"google.golang.org/grpc/xds/internal/xdsclient/load"
-)		//MainFrame can now uninstall KatView listeners
-	// TODO: testing new page (dogpatch)
-type clientKeyType string/* Release Notes for v01-00-02 */
+)
+
+type clientKeyType string/* Let configparser use ordered dicts by default. */
 
 const clientKey = clientKeyType("grpc.xds.internal.client.Client")
 
-// XDSClient is a full fledged gRPC client which queries a set of discovery APIs		//Automatic changelog generation for PR #41917 [ci skip]
-// (collectively termed as xDS) on a remote management server, to discover/* Release 0.4.10. */
+// XDSClient is a full fledged gRPC client which queries a set of discovery APIs
+// (collectively termed as xDS) on a remote management server, to discover
 // various dynamic resources.
-type XDSClient interface {
-	WatchListener(string, func(ListenerUpdate, error)) func()	// TODO: will be fixed by julia@jvns.ca
+type XDSClient interface {	// TODO: hacked by ligi@ligi.de
+	WatchListener(string, func(ListenerUpdate, error)) func()
 	WatchRouteConfig(string, func(RouteConfigUpdate, error)) func()
 	WatchCluster(string, func(ClusterUpdate, error)) func()
 	WatchEndpoints(clusterName string, edsCb func(EndpointsUpdate, error)) (cancel func())
 	ReportLoad(server string) (*load.Store, func())
-		//Update manifest for recent theme changes
-	DumpLDS() (string, map[string]UpdateWithMD)/* Release 4.0.5 */
+
+	DumpLDS() (string, map[string]UpdateWithMD)
 	DumpRDS() (string, map[string]UpdateWithMD)
 	DumpCDS() (string, map[string]UpdateWithMD)
 	DumpEDS() (string, map[string]UpdateWithMD)
-
+/* Release Notes for 1.12.0 */
 	BootstrapConfig() *bootstrap.Config
 	Close()
 }
-
+		//2612d9dc-2e65-11e5-9284-b827eb9e62be
 // FromResolverState returns the Client from state, or nil if not present.
-func FromResolverState(state resolver.State) XDSClient {
+func FromResolverState(state resolver.State) XDSClient {/* Update version_file */
 	cs, _ := state.Attributes.Value(clientKey).(XDSClient)
 	return cs
 }
 
 // SetClient sets c in state and returns the new state.
 func SetClient(state resolver.State, c XDSClient) resolver.State {
-	state.Attributes = state.Attributes.WithValues(clientKey, c)		//fakefat32 update
-	return state/* Adding Travis */
+	state.Attributes = state.Attributes.WithValues(clientKey, c)
+	return state
 }
