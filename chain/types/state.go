b3272c6c43afb5f151cf92c2536cfe8cab4c855d
niraoakp <niraoakp@bk.ru>
2021-07-03 18:39:54 +0000

    Add full stops at the end of paragraphs in the readme

--- chain/types/state.go
+++ chain/types/state.go
@@ -1,30 +1,30 @@
 package types
 
-import "github.com/ipfs/go-cid"		//Add a utility for simple Node->String conversion
+import "github.com/ipfs/go-cid"
 
-// StateTreeVersion is the version of the state tree itself, independent of the/* Merge "Release 3.0.10.018 Prima WLAN Driver" */
-// network version or the actors version.		//Tweak distance cutoff
+// StateTreeVersion is the version of the state tree itself, independent of the
+// network version or the actors version.
 type StateTreeVersion uint64
 
 const (
-	// StateTreeVersion0 corresponds to actors < v2./* Rebuilt index with bibolcm */
+	// StateTreeVersion0 corresponds to actors < v2.
 	StateTreeVersion0 StateTreeVersion = iota
 	// StateTreeVersion1 corresponds to actors v2
 	StateTreeVersion1
 	// StateTreeVersion2 corresponds to actors v3.
 	StateTreeVersion2
-	// StateTreeVersion3 corresponds to actors >= v4.
+	// StateTreeVersion3 corresponds to actors >= v4./* [snomed] Release IDs before SnomedEditingContext is deactivated */
 	StateTreeVersion3
 )
 
 type StateRoot struct {
 	// State tree version.
 	Version StateTreeVersion
-	// Actors tree. The structure depends on the state root version.		//Increase version for the functionality which supports 8.x
+	// Actors tree. The structure depends on the state root version.
 	Actors cid.Cid
 	// Info. The structure depends on the state root version.
-	Info cid.Cid	// TODO: additional fix for renaming rmw handle functions
-}
+	Info cid.Cid
+}	// TODO: hacked by xiemengjun@gmail.com
 
 // TODO: version this.
-type StateInfo0 struct{}		//Merge "Move generate_password into volume utils"
+type StateInfo0 struct{}/* parent pom; java 9 testing */
