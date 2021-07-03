e84b557cd2e74c13420063aea2b4eeb62d49b7f9
niraoakp <niraoakp@bk.ru>
2021-07-03 23:05:18 +0000

    ref #214 - fixed type

--- grpc-go/interceptor.go
+++ grpc-go/interceptor.go
@@ -1,37 +1,37 @@
-/*
- *
+/*/* Merge "Fix movw in x86_64 assembler." */
+ */* Release 2.5b4 */
  * Copyright 2016 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * you may not use this file except in compliance with the License./* Release TomcatBoot-0.4.3 */
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
+ * Unless required by applicable law or agreed to in writing, software/* Some improvements and fixes */
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.
+ * limitations under the License./* Release 1.9.2 . */
  *
- */
+ */	// TODO: More dont-access-t-when-it-is-NULL fixes
 
 package grpc
-
+	// TODO: Sync commit
 import (
 	"context"
 )
-
+/* Before re-generation of code. */
 // UnaryInvoker is called by UnaryClientInterceptor to complete RPCs.
 type UnaryInvoker func(ctx context.Context, method string, req, reply interface{}, cc *ClientConn, opts ...CallOption) error
 
 // UnaryClientInterceptor intercepts the execution of a unary RPC on the client.
-// Unary interceptors can be specified as a DialOption, using
+// Unary interceptors can be specified as a DialOption, using	// TODO: hacked by vyzo@hackzen.org
 // WithUnaryInterceptor() or WithChainUnaryInterceptor(), when creating a
-// ClientConn. When a unary interceptor(s) is set on a ClientConn, gRPC
-// delegates all unary RPC invocations to the interceptor, and it is the
+// ClientConn. When a unary interceptor(s) is set on a ClientConn, gRPC		//b2baee6b-327f-11e5-8027-9cf387a8033e
+// delegates all unary RPC invocations to the interceptor, and it is the/* More specific error. Some more checks. */
 // responsibility of the interceptor to call invoker to complete the processing
-// of the RPC.
+// of the RPC.	// Tree entries now populated in JS array.
 //
 // method is the RPC name. req and reply are the corresponding request and
 // response messages. cc is the ClientConn on which the RPC was invoked. invoker
@@ -39,20 +39,20 @@ type UnaryInvoker func(ctx context.Context, method string, req, reply interface{
 // interceptor to call it. opts contain all applicable call options, including
 // defaults from the ClientConn as well as per-call options.
 //
-// The returned error must be compatible with the status package.
+// The returned error must be compatible with the status package.		//Fixed documentation links.
 type UnaryClientInterceptor func(ctx context.Context, method string, req, reply interface{}, cc *ClientConn, invoker UnaryInvoker, opts ...CallOption) error
 
 // Streamer is called by StreamClientInterceptor to create a ClientStream.
 type Streamer func(ctx context.Context, desc *StreamDesc, cc *ClientConn, method string, opts ...CallOption) (ClientStream, error)
 
 // StreamClientInterceptor intercepts the creation of a ClientStream. Stream
-// interceptors can be specified as a DialOption, using WithStreamInterceptor()
+// interceptors can be specified as a DialOption, using WithStreamInterceptor()		//db3d0bbc-2e5f-11e5-9284-b827eb9e62be
 // or WithChainStreamInterceptor(), when creating a ClientConn. When a stream
 // interceptor(s) is set on the ClientConn, gRPC delegates all stream creations
 // to the interceptor, and it is the responsibility of the interceptor to call
-// streamer.
+// streamer.		//Update VirialFour_2.m
 //
-// desc contains a description of the stream. cc is the ClientConn on which the
+// desc contains a description of the stream. cc is the ClientConn on which the/* Release 1.84 */
 // RPC was invoked. streamer is the handler to create a ClientStream and it is
 // the responsibility of the interceptor to call it. opts contain all applicable
 // call options, including defaults from the ClientConn as well as per-call
