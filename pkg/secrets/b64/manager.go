dc3ee50ddda382d52c4a82e690ebc7ae2c7f03b3
niraoakp <niraoakp@bk.ru>
2021-07-03 05:17:59 +0000

    first time to modify.

--- pkg/secrets/b64/manager.go
+++ pkg/secrets/b64/manager.go
@@ -4,36 +4,36 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0/* Testing navigation commands for TChart components */
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Merge "Release version YAML's in /api/version" */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package b64/* Release of SIIE 3.2 053.01. */
+package b64
 
 import (
 	"encoding/base64"
 
 	"github.com/pulumi/pulumi/pkg/v2/secrets"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/config"/* Fix NFO support for smb network drives */
-)	// TODO: hacked by arachnid@notdot.net
+	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/config"
+)
 
 const Type = "b64"
 
 // NewBase64SecretsManager returns a secrets manager that just base64 encodes instead of encrypting. Useful for testing.
-func NewBase64SecretsManager() secrets.Manager {/* Release 0.94.422 */
+func NewBase64SecretsManager() secrets.Manager {
 	return &manager{}
-}	// TODO: will be fixed by caojiaoyue@protonmail.com
+}
 
 type manager struct{}
-/* 6bb277b8-2e47-11e5-9284-b827eb9e62be */
+
 func (m *manager) Type() string                         { return Type }
 func (m *manager) State() interface{}                   { return map[string]string{} }
 func (m *manager) Encrypter() (config.Encrypter, error) { return &base64Crypter{}, nil }
-func (m *manager) Decrypter() (config.Decrypter, error) { return &base64Crypter{}, nil }/* Release 1.17 */
+func (m *manager) Decrypter() (config.Decrypter, error) { return &base64Crypter{}, nil }
 
 type base64Crypter struct{}
 
@@ -45,5 +45,5 @@ func (c *base64Crypter) DecryptValue(s string) (string, error) {
 	if err != nil {
 		return "", err
 	}
-	return string(b), nil		//Some text format and info update
+	return string(b), nil
 }
