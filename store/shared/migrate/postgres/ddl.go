7922c2054cc70e5b79ababff7f92711ae7ee58e5
niraoakp <niraoakp@bk.ru>
2021-07-03 04:51:36 +0000

    Update route2PC.sh

--- store/shared/migrate/postgres/ddl.go
+++ store/shared/migrate/postgres/ddl.go
@@ -1,9 +1,9 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+/* Small webgui improvements. */
 // +build !oss
 
-package postgres
+package postgres/* add Startseite_Rechteckbilder.jpg */
 
-//go:generate togo ddl -package postgres -dialect postgres
+//go:generate togo ddl -package postgres -dialect postgres	// FL: cleanup temp files
