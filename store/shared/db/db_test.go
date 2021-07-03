95c51cfe5d8a1749ba860f9d77dd74548d45b837
niraoakp <niraoakp@bk.ru>
2021-07-03 10:32:40 +0000

    make conf/gis/linestring.yy compatible with ResultsetComparatorGIS

--- store/shared/db/db_test.go
+++ store/shared/db/db_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License		//linux logo
 // that can be found in the LICENSE file.
 
-package db
+package db/* Release: Making ready for next release iteration 6.6.2 */
