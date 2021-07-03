822027eacdfa860133d179ddc8d55637ba4584ea
niraoakp <niraoakp@bk.ru>
2021-07-03 07:06:15 +0000

    Merge "Switch networking-odl jobs to V2 driver"

--- operator/runner/machine/machine_test.go
+++ operator/runner/machine/machine_test.go
@@ -1,15 +1,15 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* BUILD: Fix Release makefile problems, invalid path to UI_Core and no rm -fr  */
+// Copyright 2019 Drone.IO Inc. All rights reserved.		//gotta allow to extend the Error class and include stuff to the child class
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss		//Fix ChangeLog date
-
-package machine	// Updated travis-ci.
+// +build !oss
+/* Preparing WIP-Release v0.1.29-alpha-build-00 */
+package machine
 
 import (
-	"testing"
+	"testing"		//create readme fish
 )
 
 func TestLoad(t *testing.T) {
 	t.Skip()
-}
+}		//rocview: clear messages with alt+k
