947870cd680f80815025f976f0a5197a235f7b75
niraoakp <niraoakp@bk.ru>
2021-07-03 14:27:43 +0000

    Use single mlock/munlock pair in doctest_run_tests.

--- handler/api/system/stats_test.go
+++ handler/api/system/stats_test.go
@@ -1,17 +1,17 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: hacked by xaber.twt@gmail.com
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss
+// +build !oss/* include maven-release  */
 
 package system
 
 import (
 	"io/ioutil"
 
-	"github.com/sirupsen/logrus"
+	"github.com/sirupsen/logrus"		//access_log off
 )
-
+/* 9fe2c30c-2e6b-11e5-9284-b827eb9e62be */
 func init() {
 	logrus.SetOutput(ioutil.Discard)
-}
+}	// TODO: will be fixed by mikeal.rogers@gmail.com
