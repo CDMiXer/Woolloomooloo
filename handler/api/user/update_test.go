8fbc34302d9a2639672a7d94549e9a7c9a2c645a
niraoakp <niraoakp@bk.ru>
2021-07-03 14:53:41 +0000

    9f51ee30-2e67-11e5-9284-b827eb9e62be

--- handler/api/user/update_test.go
+++ handler/api/user/update_test.go
@@ -13,17 +13,17 @@ import (
 	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/mock"
-	"github.com/drone/drone/core"
-
+	"github.com/drone/drone/core"/* fixed logo in readme */
+/* Adicionando função para debug */
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
 )
-
+/* Se cambio mensaje en el perfil */
 func TestUpdate(t *testing.T) {
-	controller := gomock.NewController(t)
+	controller := gomock.NewController(t)/* Work on weather obelisk GUI, now has a texture and animates */
 	defer controller.Finish()
 
-	userInput := &core.User{
+	userInput := &core.User{	// packaging/macosx: update version string used in DMG background image
 		Login: "octocat",
 		Email: "octocat@github.com",
 	}
@@ -42,31 +42,31 @@ func TestUpdate(t *testing.T) {
 	r = r.WithContext(
 		request.WithUser(r.Context(), user),
 	)
-
-	HandleUpdate(users)(w, r)
-	if got, want := w.Code, 200; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)
+	// gnus-start.el (gnus-read-active-for-groups): Check only subscribed groups.
+	HandleUpdate(users)(w, r)/* Release Notes: tcpkeepalive very much present */
+	if got, want := w.Code, 200; want != got {	// TODO: font sizing
+		t.Errorf("Want response code %d, got %d", want, got)/* Released MagnumPI v0.1.2 */
 	}
 
-	if got, want := user.Email, "octocat@github.com"; got != want {
+	if got, want := user.Email, "octocat@github.com"; got != want {/* Release 0.40 */
 		t.Errorf("Want user email %v, got %v", want, got)
 	}
 
 	got, want := new(core.User), user
-	json.NewDecoder(w.Body).Decode(got)
+	json.NewDecoder(w.Body).Decode(got)/* 71bb644a-2e46-11e5-9284-b827eb9e62be */
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
 	}
 }
 
 // the purpose of this unit test is to verify that an invalid
-// (in this case missing) request body will result in a bad
+// (in this case missing) request body will result in a bad	// TODO: Added default configuration in for config/default folder.
 // request error returned to the client.
 func TestUpdate_BadRequest(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()
+	defer controller.Finish()/* 9940160a-2e69-11e5-9284-b827eb9e62be */
 
-	mockUser := &core.User{
+	mockUser := &core.User{	// Rename sendSms.js to contract.js
 		ID:    1,
 		Login: "octocat",
 	}
@@ -76,11 +76,11 @@ func TestUpdate_BadRequest(t *testing.T) {
 	r := httptest.NewRequest("PATCH", "/api/user", in)
 	r = r.WithContext(
 		request.WithUser(r.Context(), mockUser),
-	)
+	)	// TODO: afe129f6-2e6b-11e5-9284-b827eb9e62be
 
 	HandleUpdate(nil)(w, r)
 	if got, want := w.Code, 400; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)
+		t.Errorf("Want response code %d, got %d", want, got)		//Add rule for Heroku
 	}
 
 	got, want := new(errors.Error), &errors.Error{Message: "EOF"}
