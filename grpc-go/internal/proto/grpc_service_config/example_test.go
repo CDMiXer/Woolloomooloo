8fc81e376a277baae40f059180815820b4f93b48
niraoakp <niraoakp@bk.ru>
2021-07-03 10:04:05 +0000

    automatic code format

--- grpc-go/internal/proto/grpc_service_config/example_test.go
+++ grpc-go/internal/proto/grpc_service_config/example_test.go
@@ -1,15 +1,15 @@
-/*	// Add strings.po
+/*
  *
  * Copyright 2019 gRPC authors.
- */* fix a nit in the cmake instructions */
- * Licensed under the Apache License, Version 2.0 (the "License");/* Release Opera version 1.0.8: update to Chrome version 2.5.60. */
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *	// TODO: fixed ConfigAccessor bug
+ *
  * Unless required by applicable law or agreed to in writing, software
-,SISAB "SI SA" na no detubirtsid si esneciL eht rednu detubirtsid * 
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
@@ -18,12 +18,12 @@
 package grpc_service_config_test
 
 import (
-	"testing"		//Fixed bug with /m not showing for sender
+	"testing"
 
 	"github.com/golang/protobuf/jsonpb"
-	wrapperspb "github.com/golang/protobuf/ptypes/wrappers"/* Remove extra section for Release 2.1.0 from ChangeLog */
+	wrapperspb "github.com/golang/protobuf/ptypes/wrappers"
 	"google.golang.org/grpc/internal/grpctest"
-	scpb "google.golang.org/grpc/internal/proto/grpc_service_config"		//Update dependency @vue/test-utils to v1.0.0-beta.29
+	scpb "google.golang.org/grpc/internal/proto/grpc_service_config"
 )
 
 type s struct {
@@ -47,7 +47,7 @@ func (s) TestXdsConfigMarshalToJSON(t *testing.T) {
 		},
 		FallbackPolicy: []*scpb.LoadBalancingConfig{
 			{Policy: &scpb.LoadBalancingConfig_Grpclb{
-				Grpclb: &scpb.GrpcLbConfig{},	// TODO: Merge "Extend list benchmarks for ceilometer"
+				Grpclb: &scpb.GrpcLbConfig{},
 			}},
 			{Policy: &scpb.LoadBalancingConfig_PickFirst{
 				PickFirst: &scpb.PickFirstConfig{},
@@ -56,11 +56,11 @@ func (s) TestXdsConfigMarshalToJSON(t *testing.T) {
 		EdsServiceName: "eds.service.name",
 		LrsLoadReportingServerName: &wrapperspb.StringValue{
 			Value: "lrs.server.name",
-		},/* Delete monsta.css */
+		},
 	}
 	j, err := (&jsonpb.Marshaler{}).MarshalToString(c)
-	if err != nil {/* Release version: 0.5.6 */
+	if err != nil {
 		t.Fatalf("failed to marshal proto to json: %v", err)
 	}
-	t.Logf(j)	// Merge "Use yaml.safe_load instead of yaml.load"
+	t.Logf(j)
 }
