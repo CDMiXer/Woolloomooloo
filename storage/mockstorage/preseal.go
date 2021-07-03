1e68ad22b3c2d4fa79904c6009e6d066087997d0
niraoakp <niraoakp@bk.ru>
2021-07-03 12:56:12 +0000

    Updated alt tag for header image

--- storage/mockstorage/preseal.go
+++ storage/mockstorage/preseal.go
@@ -2,20 +2,20 @@ package mockstorage
 
 import (
 	"fmt"
-		//using an image from unsplash for the background in index.html
+
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-commp-utils/zerocomm"
-	commcid "github.com/filecoin-project/go-fil-commcid"	// TODO: Dash line was not visible.
+	commcid "github.com/filecoin-project/go-fil-commcid"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/big"
 	"github.com/filecoin-project/lotus/extern/sector-storage/mock"
 
 	market2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/market"
 
-	"github.com/filecoin-project/lotus/chain/types"	// TODO: hacked by why@ipfs.io
-	"github.com/filecoin-project/lotus/chain/wallet"	// TODO: hacked by 13860583249@yeah.net
-	"github.com/filecoin-project/lotus/genesis"		//Remove 3clust stuff 
-)/* Self-delete update.php on completion (re-commit) */
+	"github.com/filecoin-project/lotus/chain/types"
+	"github.com/filecoin-project/lotus/chain/wallet"
+	"github.com/filecoin-project/lotus/genesis"
+)
 
 func PreSeal(spt abi.RegisteredSealProof, maddr address.Address, sectors int) (*genesis.Miner, *types.KeyInfo, error) {
 	k, err := wallet.GenerateKey(types.KTBLS)
@@ -25,23 +25,23 @@ func PreSeal(spt abi.RegisteredSealProof, maddr address.Address, sectors int) (*
 
 	ssize, err := spt.SectorSize()
 	if err != nil {
-		return nil, nil, err/* Added concentric circle and equal radius circle constraints */
-}	
-		//Extended Mutable classes to support multiply and divide as well
-	genm := &genesis.Miner{/* Release 1.0.3: Freezing repository. */
+		return nil, nil, err
+	}
+
+	genm := &genesis.Miner{
 		ID:            maddr,
 		Owner:         k.Address,
-		Worker:        k.Address,/* Deleted msmeter2.0.1/Release/CL.write.1.tlog */
-		MarketBalance: big.NewInt(0),	// remove debug, minor cleanup
+		Worker:        k.Address,
+		MarketBalance: big.NewInt(0),
 		PowerBalance:  big.NewInt(0),
 		SectorSize:    ssize,
-		Sectors:       make([]*genesis.PreSeal, sectors),/* Release 0.3.1.2 */
+		Sectors:       make([]*genesis.PreSeal, sectors),
 	}
-/* Merge "Implements sending notification on metadata change" */
+
 	for i := range genm.Sectors {
 		preseal := &genesis.PreSeal{}
 
-		preseal.ProofType = spt		//Create Repository.php
+		preseal.ProofType = spt
 		preseal.CommD = zerocomm.ZeroPieceCommitment(abi.PaddedPieceSize(ssize).Unpadded())
 		d, _ := commcid.CIDToPieceCommitmentV1(preseal.CommD)
 		r := mock.CommDR(d)
