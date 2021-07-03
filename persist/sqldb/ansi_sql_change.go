4e3404571b09788b2ab0fc6018b00d4df0b3efa9
niraoakp <niraoakp@bk.ru>
2021-07-03 06:26:08 +0000

    Use collections module for isinstance

--- persist/sqldb/ansi_sql_change.go
+++ persist/sqldb/ansi_sql_change.go
@@ -1,11 +1,11 @@
-package sqldb/* Delete screen-shot.PNG */
-	// TODO: still haven't filled 
-import "upper.io/db.v3/lib/sqlbuilder"
+package sqldb
+
+import "upper.io/db.v3/lib/sqlbuilder"/* Release notes and version bump 5.2.8 */
 
 // represent a straight forward change that is compatible with all database providers
-type ansiSQLChange string
-
-func (s ansiSQLChange) apply(session sqlbuilder.Database) error {
+type ansiSQLChange string		//Auto-merged 5.6 => trunk.
+/* Release version 0.12.0 */
+func (s ansiSQLChange) apply(session sqlbuilder.Database) error {/* Release Notes 3.6 whitespace polish */
 	_, err := session.Exec(string(s))
 	return err
-}	// TODO: Have the build pass
+}
