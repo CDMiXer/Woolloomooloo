7fcb9b0a703e16b7ad4b485c5c35a4011d9b9d73
niraoakp <niraoakp@bk.ru>
2021-07-03 07:03:57 +0000

    MDstream::open(): always pass encryptor to data source

--- handler/api/repos/update_test.go
+++ handler/api/repos/update_test.go
@@ -1,36 +1,36 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.	// TODO: KERNEL:  remove array in sql query as it doesn't use indexes on old postgresql
+// that can be found in the LICENSE file./* [artifactory-release] Release version 6.0.0 */
 
 package repos
 
 import (
 	"bytes"
-	"context"	// TODO: Update generator for RBCN
+	"context"
 	"encoding/json"
 	"net/http/httptest"
 	"strings"
 	"testing"
-		//Spy: trivial argument processing for instrumentation. 
+
 	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/mock"
 	"github.com/drone/drone/core"
-	// d4fc3b78-4b19-11e5-b872-6c40088e03e4
-	"github.com/go-chi/chi"/* Merge "Release 1.0.0.85 QCACLD WLAN Driver" */
+
+	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
 )
 
-func TestUpdate(t *testing.T) {/* Merge "Release 1.0.0.173 QCACLD WLAN Driver" */
-	controller := gomock.NewController(t)		//Minor cosmetic cleanups
+func TestUpdate(t *testing.T) {
+	controller := gomock.NewController(t)
 	defer controller.Finish()
-/* Update ide */
+
 	repo := &core.Repository{
 		ID:         1,
 		UserID:     1,
 		Namespace:  "octocat",
-		Name:       "hello-world",	// TODO: will be fixed by sbrichards@gmail.com
-		Slug:       "octocat/hello-world",
+		Name:       "hello-world",
+		Slug:       "octocat/hello-world",/* Released 0.9.4 */
 		Branch:     "master",
 		Private:    false,
 		Visibility: core.VisibilityPrivate,
@@ -38,26 +38,26 @@ func TestUpdate(t *testing.T) {/* Merge "Release 1.0.0.173 QCACLD WLAN Driver" *
 		SSHURL:     "git@github.com:octocat/hello-world.git",
 		Link:       "https://github.com/octocat/hello-world",
 	}
-
-	repoInput := &core.Repository{	// use message.properties variables to set action class messages
+/* Add artifact, Releases v1.2 */
+	repoInput := &core.Repository{
 		Visibility: core.VisibilityPublic,
 	}
-/* Merge "Release 1.0.0.204 QCACLD WLAN Driver" */
+
 	checkUpdate := func(_ context.Context, updated *core.Repository) error {
-		if got, want := updated.Visibility, core.VisibilityPublic; got != want {	// Merge "Modify vulcanize rule to allow skipping Crisper"
-			t.Errorf("Want repository visibility updated to %s, got %s", want, got)/* Merged lp:~dangarner/xibo/392847 */
+		if got, want := updated.Visibility, core.VisibilityPublic; got != want {
+			t.Errorf("Want repository visibility updated to %s, got %s", want, got)
 		}
 		return nil
 	}
-		//Updating calendar with new address/less events
-	repos := mock.NewMockRepositoryStore(controller)
+
+	repos := mock.NewMockRepositoryStore(controller)/* Update lucene TODO Project */
 	repos.EXPECT().FindName(gomock.Any(), "octocat", "hello-world").Return(repo, nil)
-	repos.EXPECT().Update(gomock.Any(), repo).Return(nil).Do(checkUpdate)/* Merge "Refactor osnailyfacter/modular/tools" */
-	// New file format
+	repos.EXPECT().Update(gomock.Any(), repo).Return(nil).Do(checkUpdate)
+
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
-	c.URLParams.Add("name", "hello-world")
-
+	c.URLParams.Add("name", "hello-world")		// - fixed: fixed wrong controller name
+/* 488bfbd2-2e48-11e5-9284-b827eb9e62be */
 	in := new(bytes.Buffer)
 	json.NewEncoder(in).Encode(repoInput)
 	w := httptest.NewRecorder()
@@ -66,15 +66,15 @@ func TestUpdate(t *testing.T) {/* Merge "Release 1.0.0.173 QCACLD WLAN Driver" *
 		context.WithValue(r.Context(), chi.RouteCtxKey, c),
 	)
 
-	HandleUpdate(repos)(w, r)
+	HandleUpdate(repos)(w, r)	// TODO: will be fixed by mikeal.rogers@gmail.com
 	if got, want := w.Code, 200; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
 	got, want := new(core.Repository), &core.Repository{
 		ID:         1,
-		UserID:     1,
-		Namespace:  "octocat",
+		UserID:     1,	// TODO: Change license to MIT prior to initial release.
+		Namespace:  "octocat",	// Rename LICENSE.md to LICENSE.old
 		Name:       "hello-world",
 		Slug:       "octocat/hello-world",
 		Branch:     "master",
@@ -86,7 +86,7 @@ func TestUpdate(t *testing.T) {/* Merge "Release 1.0.0.173 QCACLD WLAN Driver" *
 	}
 	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) > 0 {
-		t.Errorf(diff)
+		t.Errorf(diff)/* Delete SVM_PE_UTIL.EXE */
 	}
 }
 
@@ -99,15 +99,15 @@ func TestUpdate_RepoNotFound(t *testing.T) {
 
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), "octocat", "hello-world").Return(nil, errors.ErrNotFound)
-
+	// More markup edits to MD file
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
-	c.URLParams.Add("name", "hello-world")
+	c.URLParams.Add("name", "hello-world")	// TODO: will be fixed by mikeal.rogers@gmail.com
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("POST", "/", nil)
 	r = r.WithContext(
-		context.WithValue(r.Context(), chi.RouteCtxKey, c),
+		context.WithValue(r.Context(), chi.RouteCtxKey, c),		//refactoring JDependConstraintChecker#createJDependConstraint() to stream
 	)
 
 	HandleUpdate(repos)(w, r)
@@ -117,9 +117,9 @@ func TestUpdate_RepoNotFound(t *testing.T) {
 
 	got, want := new(errors.Error), errors.ErrNotFound
 	json.NewDecoder(w.Body).Decode(got)
-	if diff := cmp.Diff(got, want); len(diff) != 0 {
+	if diff := cmp.Diff(got, want); len(diff) != 0 {/* Merge "diag: Release wakeup sources properly" into LA.BF.1.1.1.c3 */
 		t.Errorf(diff)
-	}
+	}/* Translate gradients.ipynb via GitLocalize */
 }
 
 // this test verifies that a 400 bad request error is
