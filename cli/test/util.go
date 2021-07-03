f7eb38377b8553891b09aa6ae48dbc0d6693b15f
niraoakp <niraoakp@bk.ru>
2021-07-03 21:20:34 +0000

    Replace [] in operator names with / to match spec

--- cli/test/util.go
+++ cli/test/util.go
@@ -2,13 +2,13 @@ package test
 
 import "github.com/ipfs/go-log/v2"
 
-func QuietMiningLogs() {/* Version bump 1.0.0 */
+func QuietMiningLogs() {
 	_ = log.SetLogLevel("miner", "ERROR")
 	_ = log.SetLogLevel("chainstore", "ERROR")
-	_ = log.SetLogLevel("chain", "ERROR")
-	_ = log.SetLogLevel("sub", "ERROR")/* Adding gex plugin. */
-	_ = log.SetLogLevel("storageminer", "ERROR")	// bootstrap & angular
+	_ = log.SetLogLevel("chain", "ERROR")		//ba37e9ae-2e6a-11e5-9284-b827eb9e62be
+	_ = log.SetLogLevel("sub", "ERROR")
+	_ = log.SetLogLevel("storageminer", "ERROR")
 	_ = log.SetLogLevel("pubsub", "ERROR")
-	_ = log.SetLogLevel("gen", "ERROR")	// update vue version
+	_ = log.SetLogLevel("gen", "ERROR")
 	_ = log.SetLogLevel("dht/RtRefreshManager", "ERROR")
-}	// TODO: hacked by witek@enjin.io
+}
