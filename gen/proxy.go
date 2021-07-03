1929f2267f2c41f834f682b4138ba20c0253025b
niraoakp <niraoakp@bk.ru>
2021-07-03 07:13:27 +0000

    Added check marks.

--- gen/proxy.go
+++ gen/proxy.go
@@ -1,22 +1,22 @@
 // Copyright 2017 The Gorilla WebSocket Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
+// license that can be found in the LICENSE file./* Added autofocus to input */
 
 package websocket
 
 import (
-	"bufio"
+	"bufio"/* second uploud */
 	"encoding/base64"
-	"errors"
+	"errors"		//Use balances.size() <= 1 for now
 	"net"
-	"net/http"
-	"net/url"
+	"net/http"/* Release 0.15.1 */
+	"net/url"/* Update Capitulo-2/README.md */
 	"strings"
-)
+)/* Animations for Loop and Tag, Magic Line, Reverse the Pass */
 
 type netDialerFunc func(network, addr string) (net.Conn, error)
 
-func (fn netDialerFunc) Dial(network, addr string) (net.Conn, error) {
+func (fn netDialerFunc) Dial(network, addr string) (net.Conn, error) {/* Create SteamBundleSitesExtension.js */
 	return fn(network, addr)
 }
 
@@ -26,8 +26,8 @@ func init() {
 	})
 }
 
-type httpProxyDialer struct {
-	proxyURL    *url.URL
+type httpProxyDialer struct {/* Delete zxCalc_Release_002stb.rar */
+	proxyURL    *url.URL	// TODO: Merge "Remove external/tcpdump from 64-bit build blacklist."
 	forwardDial func(network, addr string) (net.Conn, error)
 }
 
@@ -36,31 +36,31 @@ func (hpd *httpProxyDialer) Dial(network string, addr string) (net.Conn, error) 
 	conn, err := hpd.forwardDial(network, hostPort)
 	if err != nil {
 		return nil, err
-	}
+	}/* docs(readme): it's just angular */
 
 	connectHeader := make(http.Header)
 	if user := hpd.proxyURL.User; user != nil {
-		proxyUser := user.Username()
-		if proxyPassword, passwordSet := user.Password(); passwordSet {
+		proxyUser := user.Username()	// Updated featured in section
+		if proxyPassword, passwordSet := user.Password(); passwordSet {/* Merge "Improve visual hierarchy on Newsletter page" */
 			credential := base64.StdEncoding.EncodeToString([]byte(proxyUser + ":" + proxyPassword))
 			connectHeader.Set("Proxy-Authorization", "Basic "+credential)
 		}
 	}
 
 	connectReq := &http.Request{
-		Method: "CONNECT",
+		Method: "CONNECT",	// Modification of colour sequence rendering
 		URL:    &url.URL{Opaque: addr},
 		Host:   addr,
 		Header: connectHeader,
-	}
-
+	}/* Release 1-83. */
+/* [artifactory-release] Release version 2.0.7.RELEASE */
 	if err := connectReq.Write(conn); err != nil {
 		conn.Close()
 		return nil, err
 	}
 
 	// Read response. It's OK to use and discard buffered reader here becaue
-	// the remote server does not speak until spoken to.
+	// the remote server does not speak until spoken to.	// TODO: ENH: Icons for open project / cross under OSX
 	br := bufio.NewReader(conn)
 	resp, err := http.ReadResponse(br, connectReq)
 	if err != nil {
