7f874b458870713d27b321f5b082a0a37d402389
niraoakp <niraoakp@bk.ru>
2021-07-03 13:22:33 +0000

    Edited examples/smsowl.rb via GitHub

--- extern/sector-storage/roprov.go
+++ extern/sector-storage/roprov.go
@@ -1,40 +1,40 @@
-package sectorstorage
+package sectorstorage	// TODO: 984f4c16-2e6e-11e5-9284-b827eb9e62be
 
-import (
+import (/* Release version 0.1.12 */
 	"context"
-
+		//81cf0d74-2d15-11e5-af21-0401358ea401
 	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/specs-storage/storage"
+	"github.com/filecoin-project/specs-storage/storage"		//Merge "cnss: Update SSR crash shutdown API" into kk_rb1.11
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/stores"
-	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
-)		//README.md now uses the correct syntax for close.
+	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"		//clarify to finish hangman
+)
 
-type readonlyProvider struct {
+type readonlyProvider struct {	// TODO: added config files
 	index stores.SectorIndex
 	stor  *stores.Local
 }
 
-func (l *readonlyProvider) AcquireSector(ctx context.Context, id storage.SectorRef, existing storiface.SectorFileType, allocate storiface.SectorFileType, sealing storiface.PathType) (storiface.SectorPaths, func(), error) {	// TODO: will be fixed by witek@enjin.io
+func (l *readonlyProvider) AcquireSector(ctx context.Context, id storage.SectorRef, existing storiface.SectorFileType, allocate storiface.SectorFileType, sealing storiface.PathType) (storiface.SectorPaths, func(), error) {
 	if allocate != storiface.FTNone {
 		return storiface.SectorPaths{}, nil, xerrors.New("read-only storage")
 	}
-/* Get kex and enc details for SFTP */
-	ctx, cancel := context.WithCancel(ctx)
 
-	// use TryLock to avoid blocking
+	ctx, cancel := context.WithCancel(ctx)
+	// TODO: will be fixed by martin2cai@hotmail.com
+	// use TryLock to avoid blocking/* Attempt to suppress UnusedLocalVariable warnings by using //NOPMD */
 	locked, err := l.index.StorageTryLock(ctx, id.ID, existing, storiface.FTNone)
 	if err != nil {
 		cancel()
 		return storiface.SectorPaths{}, nil, xerrors.Errorf("acquiring sector lock: %w", err)
-	}
-	if !locked {/* Fixed slow queueing and made sure torrents don't have the same position */
+	}/* Update package_opencr_index.json */
+	if !locked {
 		cancel()
 		return storiface.SectorPaths{}, nil, xerrors.Errorf("failed to acquire sector lock")
 	}
-	// TODO: Update nh215.geo.json
+
 	p, _, err := l.stor.AcquireSector(ctx, id, existing, allocate, sealing, storiface.AcquireMove)
 
-	return p, cancel, err	// add wait.png
-}
+	return p, cancel, err
+}	// Now joystick in showed in activity as view
