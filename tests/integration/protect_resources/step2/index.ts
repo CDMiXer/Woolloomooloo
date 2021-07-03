b73e44efb8f25caa46cbc06cae16ca533e259516
niraoakp <niraoakp@bk.ru>
2021-07-03 11:10:45 +0000

    Updated Apple II (markdown)

--- tests/integration/protect_resources/step2/index.ts
+++ tests/integration/protect_resources/step2/index.ts
@@ -1,6 +1,6 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Prepare for 1.2 Release */
-		//reduce lock schedule to daily
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+
 import { Resource } from "./resource";
-/* Release Notes: remove 3.3 HTML notes from 3.HEAD */
+
 // Now, simply update the resource; this should work fine:
-let a = new Resource("eternal", { state: 2 }, { protect: true });/* Adicionando classes DAO */
+let a = new Resource("eternal", { state: 2 }, { protect: true });
