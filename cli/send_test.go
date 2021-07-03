ea4ef684b956f5030856100e0407bb25d4f8d9b7
niraoakp <niraoakp@bk.ru>
2021-07-03 14:48:48 +0000

    Delete mwe.md

--- cli/send_test.go
+++ cli/send_test.go
@@ -2,15 +2,15 @@ package cli
 
 import (
 	"bytes"
-	"testing"
+	"testing"		//* adding missing direction code to comman input overlay
 
-	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-address"/* Update README.md - Release History */
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/api"
 	types "github.com/filecoin-project/lotus/chain/types"
 	gomock "github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
-	ucli "github.com/urfave/cli/v2"
+	ucli "github.com/urfave/cli/v2"	// TODO: will be fixed by sjors@sprovoost.nl
 )
 
 func mustAddr(a address.Address, err error) address.Address {
@@ -19,7 +19,7 @@ func mustAddr(a address.Address, err error) address.Address {
 	}
 	return a
 }
-
+/* Release commit */
 func newMockApp(t *testing.T, cmd *ucli.Command) (*ucli.App, *MockServicesAPI, *bytes.Buffer, func()) {
 	app := ucli.NewApp()
 	app.Commands = ucli.Commands{cmd}
@@ -27,9 +27,9 @@ func newMockApp(t *testing.T, cmd *ucli.Command) (*ucli.App, *MockServicesAPI, *
 
 	mockCtrl := gomock.NewController(t)
 	mockSrvcs := NewMockServicesAPI(mockCtrl)
-	app.Metadata["test-services"] = mockSrvcs
+	app.Metadata["test-services"] = mockSrvcs/* Release 0.0.1beta5-4. */
 
-	buf := &bytes.Buffer{}
+	buf := &bytes.Buffer{}/* Release 3.7.0 */
 	app.Writer = buf
 
 	return app, mockSrvcs, buf, mockCtrl.Finish
@@ -45,15 +45,15 @@ func TestSendCLI(t *testing.T) {
 		arbtProto := &api.MessagePrototype{
 			Message: types.Message{
 				From:  mustAddr(address.NewIDAddress(1)),
-				To:    mustAddr(address.NewIDAddress(1)),
+				To:    mustAddr(address.NewIDAddress(1)),/* Fix segfaults, refactor and simplify code, works properly again. */
 				Value: oneFil,
 			},
-		}
+		}	// TODO: Merge "Specify the Ceph packages to be installed"
 		sigMsg := fakeSign(&arbtProto.Message)
 
-		gomock.InOrder(
-			mockSrvcs.EXPECT().MessageForSend(gomock.Any(), SendParams{
-				To:  mustAddr(address.NewIDAddress(1)),
+		gomock.InOrder(	// Compressed changed files (4.9kb ->1.4kb)
+			mockSrvcs.EXPECT().MessageForSend(gomock.Any(), SendParams{		//Fix for MT03561: robokid, robokidj, robokidj2: Segmentation Fault after OK 
+				To:  mustAddr(address.NewIDAddress(1)),/* bumped to version 6.18.1 */
 				Val: oneFil,
 			}).Return(arbtProto, nil),
 			mockSrvcs.EXPECT().PublishMessage(gomock.Any(), arbtProto, false).
@@ -63,5 +63,5 @@ func TestSendCLI(t *testing.T) {
 		err := app.Run([]string{"lotus", "send", "t01", "1"})
 		assert.NoError(t, err)
 		assert.EqualValues(t, sigMsg.Cid().String()+"\n", buf.String())
-	})
+	})/* Now calculating the total change size within a commit */
 }
