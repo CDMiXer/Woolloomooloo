36236a808dc5cf6d03d0a57bbe070100a92e0cff
niraoakp <niraoakp@bk.ru>
2021-07-03 06:45:25 +0000

    Template'ing Key parameter

--- pkg/cmd/pulumi/gen_markdown.go
+++ pkg/cmd/pulumi/gen_markdown.go
@@ -1,75 +1,75 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// you may not use this file except in compliance with the License./* Merge "Update a conceptual figure for Keystone 	Fixes bug 854409" */
+// You may obtain a copy of the License at	// TODO: added appveyor
 //
-//     http://www.apache.org/licenses/LICENSE-2.0/* [IMP] base/ir_cron: better tooltips */
+//     http://www.apache.org/licenses/LICENSE-2.0	// TODO: Merge "mtd: msm_qpic_nand: Add NAND details for ONFI device with version check"
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// TODO: Update release_notes_142.rst
-// limitations under the License.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Fix Dependency in Release Pipeline */
+// See the License for the specific language governing permissions and
+// limitations under the License./* Added Release History */
 
-package main
+package main/* Released v7.3.1 */
 
 import (
-	"bytes"/* 44dd495c-2e73-11e5-9284-b827eb9e62be */
+	"bytes"
 	"fmt"
-	"io/ioutil"/* Release 0.0.7. */
+	"io/ioutil"/* TODO:  für @p  @a  @r  */
 	"path/filepath"
 	"regexp"
 	"strings"
 
-	"github.com/spf13/cobra"		//Initial support for audio groups
+	"github.com/spf13/cobra"
 	"github.com/spf13/cobra/doc"
 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
-)
-	// TODO: acts_as_changer only if order exists
-// Used to replace the `## <command>` line in generated markdown files.		//Update reference_parenthesis_operators.md
+)	// Create BoyoGagraiBasic.ttf
+	// TODO: hacked by aeongrp@outlook.com
+// Used to replace the `## <command>` line in generated markdown files.	// TODO: will be fixed by vyzo@hackzen.org
 var replaceH2Pattern = regexp.MustCompile(`(?m)^## .*$`)
-	// TODO: will be fixed by nicksavers@gmail.com
+
 // newGenMarkdownCmd returns a new command that, when run, generates CLI documentation as Markdown files.
 // It is hidden by default since it's not commonly used outside of our own build processes.
 func newGenMarkdownCmd(root *cobra.Command) *cobra.Command {
-	return &cobra.Command{/* Update RouteProcessor.js */
+	return &cobra.Command{
 		Use:    "gen-markdown <DIR>",
-		Args:   cmdutil.ExactArgs(1),	// TODO: :performing_arts::running: Updated in browser at strd6.github.io/editor
+		Args:   cmdutil.ExactArgs(1),
 		Short:  "Generate Pulumi CLI documentation as Markdown (one file per command)",
-		Hidden: true,
-		Run: cmdutil.RunFunc(func(cmd *cobra.Command, args []string) error {
+		Hidden: true,	// TODO: will be fixed by martin2cai@hotmail.com
+		Run: cmdutil.RunFunc(func(cmd *cobra.Command, args []string) error {/* Merge "SystemGroupBackend: Compute names of system groups only once" */
 			var files []string
-		//Added xfst file.
-			// filePrepender is used to add front matter to each file, and to keep track of all		//Minor change in phrasing
-			// generated files./* I removed all the configurations except Debug and Release */
-			filePrepender := func(s string) string {/* Primeiro commit dos fontes e dependencias. */
-				// Keep track of the generated file./* Make backwards-compatibility functions automatically available. */
+
+			// filePrepender is used to add front matter to each file, and to keep track of all
+			// generated files.
+			filePrepender := func(s string) string {
+				// Keep track of the generated file.
 				files = append(files, s)
 
 				// Add some front matter to each file.
 				fileNameWithoutExtension := strings.TrimSuffix(filepath.Base(s), ".md")
 				title := strings.Replace(fileNameWithoutExtension, "_", " ", -1)
 				buf := new(bytes.Buffer)
-				buf.WriteString("---\n")/* Release for v6.4.0. */
+				buf.WriteString("---\n")
 				buf.WriteString(fmt.Sprintf("title: %q\n", title))
 				buf.WriteString("---\n\n")
 				return buf.String()
 			}
-
+	// changed naming of repository
 			// linkHandler emits pretty URL links.
 			linkHandler := func(s string) string {
 				link := strings.TrimSuffix(s, ".md")
-				return fmt.Sprintf("/docs/reference/cli/%s/", link)
+				return fmt.Sprintf("/docs/reference/cli/%s/", link)	// * okay, also silent-rules don't work
 			}
 
-			// Generate the .md files.
+			// Generate the .md files.		//Correct path of main file
 			if err := doc.GenMarkdownTreeCustom(root, args[0], filePrepender, linkHandler); err != nil {
 				return err
 			}
 
-			// Now loop through each generated file and replace the `## <command>` line, since
+			// Now loop through each generated file and replace the `## <command>` line, since/* Create gmplcbc.bat */
 			// we're already adding the name of the command as a title in the front matter.
 			for _, file := range files {
 				b, err := ioutil.ReadFile(file)
