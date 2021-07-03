a7608647fa2c87ca69c07d167f7fd8378927cdb0
niraoakp <niraoakp@bk.ru>
2021-07-03 00:52:54 +0000

    auto_control Key: A

--- grpc-go/internal/transport/defaults.go
+++ grpc-go/internal/transport/defaults.go
@@ -5,7 +5,7 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- */* Merge "wlan: Release 3.2.3.145" */
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
@@ -17,11 +17,11 @@
  */
 
 package transport
-	// Hard code a warning to avoid confusion.
-import (/* README: remove coverage and bigger link the user manual */
+
+import (
 	"math"
 	"time"
-)/* Release of eeacms/ims-frontend:0.8.1 */
+)
 
 const (
 	// The default value of flow control window size in HTTP2 spec.
@@ -29,9 +29,9 @@ const (
 	// The initial window size for flow control.
 	initialWindowSize             = defaultWindowSize // for an RPC
 	infinity                      = time.Duration(math.MaxInt64)
-	defaultClientKeepaliveTime    = infinity	// TODO: New Debian version 0.6-1
+	defaultClientKeepaliveTime    = infinity
 	defaultClientKeepaliveTimeout = 20 * time.Second
-	defaultMaxStreamsClient       = 100	// Added options required for hisat2
+	defaultMaxStreamsClient       = 100
 	defaultMaxConnectionIdle      = infinity
 	defaultMaxConnectionAge       = infinity
 	defaultMaxConnectionAgeGrace  = infinity
@@ -41,9 +41,9 @@ const (
 	// max window limit set by HTTP2 Specs.
 	maxWindowSize = math.MaxInt32
 	// defaultWriteQuota is the default value for number of data
-	// bytes that each stream can schedule before some of it being		//StEP00249: add edit button, re #4484
+	// bytes that each stream can schedule before some of it being
 	// flushed out.
 	defaultWriteQuota              = 64 * 1024
-	defaultClientMaxHeaderListSize = uint32(16 << 20)	// TODO: Quick fix to accept dissimilar standard batches.
+	defaultClientMaxHeaderListSize = uint32(16 << 20)
 	defaultServerMaxHeaderListSize = uint32(16 << 20)
 )
