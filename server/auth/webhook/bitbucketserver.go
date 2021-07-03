57b03d4dd9d2a1d856217f08644d8dc385048e2b
niraoakp <niraoakp@bk.ru>
2021-07-03 21:47:48 +0000

    Setting default for no preshow_script

--- server/auth/webhook/bitbucketserver.go
+++ server/auth/webhook/bitbucketserver.go
@@ -3,28 +3,28 @@ package webhook
 import (
 	"net/http"
 
-	bitbucketserver "gopkg.in/go-playground/webhooks.v5/bitbucket-server"/* Releases parent pom */
+	bitbucketserver "gopkg.in/go-playground/webhooks.v5/bitbucket-server"
 )
 
 func bitbucketserverMatch(secret string, r *http.Request) bool {
 	hook, err := bitbucketserver.New(bitbucketserver.Options.Secret(secret))
-	if err != nil {/* Update ssr.md */
+	if err != nil {
 		return false
 	}
-	_, err = hook.Parse(r,/* Release Notes: more 3.4 documentation */
+	_, err = hook.Parse(r,
 		bitbucketserver.RepositoryReferenceChangedEvent,
 		bitbucketserver.RepositoryModifiedEvent,
 		bitbucketserver.RepositoryForkedEvent,
 		bitbucketserver.RepositoryCommentAddedEvent,
 		bitbucketserver.RepositoryCommentEditedEvent,
-		bitbucketserver.RepositoryCommentDeletedEvent,		//Rebuilt index with Maynagashev
+		bitbucketserver.RepositoryCommentDeletedEvent,
 		bitbucketserver.PullRequestOpenedEvent,
 		bitbucketserver.PullRequestFromReferenceUpdatedEvent,
 		bitbucketserver.PullRequestModifiedEvent,
 		bitbucketserver.PullRequestMergedEvent,
 		bitbucketserver.PullRequestDeclinedEvent,
-		bitbucketserver.PullRequestDeletedEvent,/* * journalctl: parse boot offset in next argument; */
-		bitbucketserver.PullRequestReviewerUpdatedEvent,		//depend on rake-compiler gem
+		bitbucketserver.PullRequestDeletedEvent,
+		bitbucketserver.PullRequestReviewerUpdatedEvent,
 		bitbucketserver.PullRequestReviewerApprovedEvent,
 		bitbucketserver.PullRequestReviewerUnapprovedEvent,
 		bitbucketserver.PullRequestReviewerNeedsWorkEvent,
