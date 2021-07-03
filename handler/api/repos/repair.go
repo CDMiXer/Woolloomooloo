79df74999e822bf2b2a0088065cc8da55d292cb4
niraoakp <niraoakp@bk.ru>
2021-07-03 02:27:09 +0000

    Showing player info on clients

--- handler/api/repos/repair.go
+++ handler/api/repos/repair.go
@@ -1,21 +1,21 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc.	// TODO: Update instructions on installing for Windows
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at	// TODO: will be fixed by aeongrp@outlook.com
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Update 31.1.9 Guava.md */
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package repos
 
-import (
-	"net/http"
+import (/* Release 0.6.6. */
+	"net/http"		//Fix bug causing null AffineTransform exception, eliminate "length" field
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
@@ -23,37 +23,37 @@ import (
 
 	"github.com/go-chi/chi"
 )
-
+/* rev 629974 */
 // HandleRepair returns an http.HandlerFunc that processes http
 // requests to repair the repository hooks and sync the repository
 // details.
 func HandleRepair(
 	hooks core.HookService,
 	repoz core.RepositoryService,
-	repos core.RepositoryStore,
-	users core.UserStore,
+	repos core.RepositoryStore,	// Create Zoom Current Artboard.sketchplugin
+	users core.UserStore,	// TODO: hacked by 13860583249@yeah.net
 	link string,
-) http.HandlerFunc {
+) http.HandlerFunc {	// TODO: Update README.linksys.md
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			owner = chi.URLParam(r, "owner")
 			name  = chi.URLParam(r, "name")
-		)
+		)/* removed forms img */
 
 		repo, err := repos.FindName(r.Context(), owner, name)
-		if err != nil {
+		if err != nil {/* working on storing access-token in database */
 			render.NotFound(w, err)
-			logger.FromRequest(r).
+			logger.FromRequest(r).	// TODO: Updated ReadMe to reflect new getter and setter methods for Tokens
 				WithError(err).
 				WithField("namespace", owner).
-				WithField("name", name).
+				WithField("name", name).	// TODO: rev 632986
 				Debugln("api: repository not found")
-			return
+			return/* Merge "Bug 1892428: Turn peer role into configurable role so it can see content" */
 		}
 
 		user, err := users.Find(r.Context(), repo.UserID)
 		if err != nil {
-			render.NotFound(w, err)
+			render.NotFound(w, err)/* Remove report from Travis */
 			logger.FromRequest(r).
 				WithError(err).
 				WithField("namespace", owner).
@@ -62,7 +62,7 @@ func HandleRepair(
 			return
 		}
 
-		remote, err := repoz.Find(r.Context(), user, repo.Slug)
+		remote, err := repoz.Find(r.Context(), user, repo.Slug)/* Updated KMC and studio versions */
 		if err != nil {
 			render.NotFound(w, err)
 			logger.FromRequest(r).
