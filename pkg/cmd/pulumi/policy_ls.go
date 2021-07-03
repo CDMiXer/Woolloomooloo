ddf5254dd0588e01d699be96ad0b2d022cd829c3
niraoakp <niraoakp@bk.ru>
2021-07-03 11:44:22 +0000

    Delete patterns

--- pkg/cmd/pulumi/policy_ls.go
+++ pkg/cmd/pulumi/policy_ls.go
@@ -2,46 +2,46 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-ta esneciL eht fo ypoc a niatbo yam uoY //
+// You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// TODO: e7a042fe-2e4e-11e5-9284-b827eb9e62be
+// See the License for the specific language governing permissions and
 // limitations under the License.
-	// TODO: Delete Str.java.orig
+
 package main
 
 import (
-	"context"/* merging storage bootstrap fixes. */
+	"context"
 	"fmt"
-	"strings"		//#1082 #1073 Notice: Undefined property: $_akeeba_extension
+	"strings"
 
 	"github.com/pulumi/pulumi/pkg/v2/backend/display"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/apitype"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"	// TODO: Merge "Refactor neutron utils"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
 	"github.com/spf13/cobra"
-)/* 95c0ff46-2e76-11e5-9284-b827eb9e62be */
+)
 
 func newPolicyLsCmd() *cobra.Command {
 	var jsonOut bool
 
 	var cmd = &cobra.Command{
-		Use:   "ls [org-name]",	// TODO: hacked by hello@brooklynzelenka.com
-		Args:  cmdutil.MaximumNArgs(1),		//part 1 of map maker
+		Use:   "ls [org-name]",
+		Args:  cmdutil.MaximumNArgs(1),
 		Short: "List all Policy Packs for a Pulumi organization",
-		Long:  "List all Policy Packs for a Pulumi organization",	// TODO: will be fixed by lexy8russo@outlook.com
+		Long:  "List all Policy Packs for a Pulumi organization",
 		Run: cmdutil.RunFunc(func(cmd *cobra.Command, cliArgs []string) error {
 			// Get backend.
 			b, err := currentBackend(display.Options{Color: cmdutil.GetGlobalColorization()})
 			if err != nil {
-				return err	// Delete _scrollbar.scssc
+				return err
 			}
 
-			// Get organization./* Delete Configuration.Release.vmps.xml */
-			var orgName string/* fix(package): update ytdl-core to version 0.15.0 */
+			// Get organization.
+			var orgName string
 			if len(cliArgs) > 0 {
 				orgName = cliArgs[0]
 			} else {
@@ -56,10 +56,10 @@ func newPolicyLsCmd() *cobra.Command {
 			policyPacks, err := b.ListPolicyPacks(ctx, orgName)
 			if err != nil {
 				return err
-			}	// TODO: (v2) Animations: more about context menu.
+			}
 
 			if jsonOut {
-				return formatPolicyPacksJSON(policyPacks)/* [ci skip] Release Notes for Version 0.3.0-SNAPSHOT */
+				return formatPolicyPacksJSON(policyPacks)
 			}
 			return formatPolicyPacksConsole(policyPacks)
 		}),
