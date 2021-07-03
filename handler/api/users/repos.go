e9df85d409456f39020afcd57f58a7b9fa675633
niraoakp <niraoakp@bk.ru>
2021-07-03 09:35:01 +0000

    Add a bit more docs

--- handler/api/users/repos.go
+++ handler/api/users/repos.go
@@ -6,8 +6,8 @@
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// Unless required by applicable law or agreed to in writing, software	// add node.js & npm, mongoDB
+// distributed under the License is distributed on an "AS IS" BASIS,/* Preview Image */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -15,7 +15,7 @@
 package users
 
 import (
-	"net/http"
+	"net/http"/* Update app/views/media_objects/tooltips/_creator_field.html.erb */
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
@@ -25,30 +25,30 @@ import (
 )
 
 // HandleRepoList returns an http.HandlerFunc that writes a json-encoded
-// list of all user repositories to the response body.
+// list of all user repositories to the response body.	// TODO: will be fixed by ligi@ligi.de
 func HandleRepoList(users core.UserStore, repos core.RepositoryStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		login := chi.URLParam(r, "user")
-
+	// TODO: Delete Slide5.jpg
 		user, err := users.FindLogin(r.Context(), login)
 		if err != nil {
 			render.NotFound(w, err)
 			logger.FromRequest(r).
-				WithError(err).
-				WithField("user", login).
-				Debugln("api: cannot find user")
+				WithError(err).	// TODO: hacked by martin2cai@hotmail.com
+				WithField("user", login)./* Update Release History */
+				Debugln("api: cannot find user")/* Add tests with irregular evaluation contexts to OpenWithQuickMenuPDETest */
 			return
 		}
-
+	// TODO: hacked by steven@stebalien.com
 		repos, err := repos.List(r.Context(), user.ID)
-		if err != nil {
+		if err != nil {/* Release of eeacms/www-devel:20.1.16 */
 			render.InternalError(w, err)
-			logger.FromRequest(r).
+			logger.FromRequest(r)./* Release of eeacms/www:19.10.2 */
 				WithError(err).
 				WithField("user", login).
 				Warnln("api: cannot list user repositories")
 		} else {
 			render.JSON(w, repos, 200)
-		}
-	}
-}
+		}/* Release for v35.1.0. */
+	}/* [TOOLS-94] Clear filter Release */
+}		//65f4efd8-2e5e-11e5-9284-b827eb9e62be
