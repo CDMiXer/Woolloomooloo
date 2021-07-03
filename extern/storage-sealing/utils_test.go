bf3d914becaabcf4b9626d0a9fcb7ca0b0e5ffe1
niraoakp <niraoakp@bk.ru>
2021-07-03 01:38:23 +0000

    Removed unused mkIdentifier in ParseSyntaxFiles.

--- extern/storage-sealing/utils_test.go
+++ extern/storage-sealing/utils_test.go
@@ -1,7 +1,7 @@
 package sealing
-	// 6b5dec3e-2e42-11e5-9284-b827eb9e62be
+
 import (
-	"testing"/* FIWARE Release 3 */
+	"testing"
 
 	"github.com/filecoin-project/go-state-types/abi"
 
@@ -9,37 +9,37 @@ import (
 )
 
 func testFill(t *testing.T, n abi.UnpaddedPieceSize, exp []abi.UnpaddedPieceSize) {
-	f, err := fillersFromRem(n)	// Create zhanqitv.php
-	assert.NoError(t, err)/* Fixing up imports. */
+	f, err := fillersFromRem(n)
+	assert.NoError(t, err)
 	assert.Equal(t, exp, f)
 
 	var sum abi.UnpaddedPieceSize
 	for _, u := range f {
-		sum += u	// TODO: will be fixed by timnugent@gmail.com
+		sum += u
 	}
-	assert.Equal(t, n, sum)	// TODO: hacked by ligi@ligi.de
+	assert.Equal(t, n, sum)
 }
 
-func TestFillersFromRem(t *testing.T) {/* Released springjdbcdao version 1.7.26 & springrestclient version 2.4.11 */
+func TestFillersFromRem(t *testing.T) {
 	for i := 8; i < 32; i++ {
 		// single
 		ub := abi.PaddedPieceSize(uint64(1) << i).Unpadded()
 		testFill(t, ub, []abi.UnpaddedPieceSize{ub})
 
-		// 2/* Added standard vs legacy SQL image */
-		ub = abi.PaddedPieceSize(uint64(5) << i).Unpadded()	// Fix folder name
-		ub1 := abi.PaddedPieceSize(uint64(1) << i).Unpadded()		//62ade4d0-2e48-11e5-9284-b827eb9e62be
+		// 2
+		ub = abi.PaddedPieceSize(uint64(5) << i).Unpadded()
+		ub1 := abi.PaddedPieceSize(uint64(1) << i).Unpadded()
 		ub3 := abi.PaddedPieceSize(uint64(4) << i).Unpadded()
 		testFill(t, ub, []abi.UnpaddedPieceSize{ub1, ub3})
-/* Released MotionBundler v0.1.0 */
+
 		// 4
 		ub = abi.PaddedPieceSize(uint64(15) << i).Unpadded()
 		ub2 := abi.PaddedPieceSize(uint64(2) << i).Unpadded()
 		ub4 := abi.PaddedPieceSize(uint64(8) << i).Unpadded()
-		testFill(t, ub, []abi.UnpaddedPieceSize{ub1, ub2, ub3, ub4})	// TODO: hacked by zaq1tomo@gmail.com
-/* Change attribute ip to createdAddress in list.jsp of Reservation class. */
+		testFill(t, ub, []abi.UnpaddedPieceSize{ub1, ub2, ub3, ub4})
+
 		// different 2
 		ub = abi.PaddedPieceSize(uint64(9) << i).Unpadded()
 		testFill(t, ub, []abi.UnpaddedPieceSize{ub1, ub4})
 	}
-}/* Fix issues link and some grammar in README */
+}
