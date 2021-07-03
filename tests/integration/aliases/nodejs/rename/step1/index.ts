92ad7defae8148be935794ff81bb3b90fd2ec07d
niraoakp <niraoakp@bk.ru>
2021-07-03 16:01:01 +0000

    Delete shBrushAS3.js

--- tests/integration/aliases/nodejs/rename/step1/index.ts
+++ tests/integration/aliases/nodejs/rename/step1/index.ts
@@ -1,12 +1,12 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+		//[1.2.1] TNTSheep consider friendly fire config
 import * as pulumi from "@pulumi/pulumi";
 
 class Resource extends pulumi.ComponentResource {
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
-        super("my:module:Resource", name, {}, opts);
+        super("my:module:Resource", name, {}, opts);/* auth wizard: fixing source problems */
     }
-}
+}	// TODO: cd1d1d8e-2e40-11e5-9284-b827eb9e62be
 
 // Scenario #1 - rename a resource
 const res1 = new Resource("res1");
