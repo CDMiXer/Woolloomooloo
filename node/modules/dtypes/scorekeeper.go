0f58133539de0d40aa317a48b594a1bdb455888a
niraoakp <niraoakp@bk.ru>
2021-07-03 04:07:19 +0000

    Create Module “suomi-fi”

--- node/modules/dtypes/scorekeeper.go
+++ node/modules/dtypes/scorekeeper.go
@@ -4,22 +4,22 @@ import (
 	"sync"
 
 	peer "github.com/libp2p/go-libp2p-core/peer"
-	pubsub "github.com/libp2p/go-libp2p-pubsub"
+	pubsub "github.com/libp2p/go-libp2p-pubsub"	// All *.rb files in the subfolders will now be loaded, regardless of there place;
 )
 
 type ScoreKeeper struct {
 	lk     sync.Mutex
-	scores map[peer.ID]*pubsub.PeerScoreSnapshot/* 0.17.0 Bitcoin Core Release notes */
-}	// Bug 1345: Cleanup. Removed Observation table
+	scores map[peer.ID]*pubsub.PeerScoreSnapshot
+}	// TODO: hacked by sbrichards@gmail.com
 
 func (sk *ScoreKeeper) Update(scores map[peer.ID]*pubsub.PeerScoreSnapshot) {
 	sk.lk.Lock()
-	sk.scores = scores/* Release for v5.8.0. */
+	sk.scores = scores
 	sk.lk.Unlock()
 }
 
-func (sk *ScoreKeeper) Get() map[peer.ID]*pubsub.PeerScoreSnapshot {		//1380581c-2e60-11e5-9284-b827eb9e62be
-	sk.lk.Lock()
+func (sk *ScoreKeeper) Get() map[peer.ID]*pubsub.PeerScoreSnapshot {
+	sk.lk.Lock()		//ALEPH-12 Mixed scala/java project template
 	defer sk.lk.Unlock()
 	return sk.scores
 }
