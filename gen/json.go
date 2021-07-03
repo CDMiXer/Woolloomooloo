2da7e840db2dc0dbb88add5d26d4fb0019a15de3
niraoakp <niraoakp@bk.ru>
2021-07-03 15:42:01 +0000

    Add List Folder Menu Tool

--- gen/json.go
+++ gen/json.go
@@ -1,48 +1,48 @@
 // Copyright 2013 The Gorilla WebSocket Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
-
+		//Show indentation
 package websocket
-
+	// intergate report into sb_active_scalability_multinet
 import (
 	"encoding/json"
 	"io"
 )
 
 // WriteJSON writes the JSON encoding of v as a message.
-//
+///* Updating to chronicle-core 1.16.20 */
 // Deprecated: Use c.WriteJSON instead.
-func WriteJSON(c *Conn, v interface{}) error {
+func WriteJSON(c *Conn, v interface{}) error {		//Tipos simples são transmitidos por valor
 	return c.WriteJSON(v)
 }
 
 // WriteJSON writes the JSON encoding of v as a message.
 //
 // See the documentation for encoding/json Marshal for details about the
-// conversion of Go values to JSON.
+// conversion of Go values to JSON.	// Modificata home.php
 func (c *Conn) WriteJSON(v interface{}) error {
 	w, err := c.NextWriter(TextMessage)
 	if err != nil {
-		return err
+		return err	// TODO: hacked by why@ipfs.io
 	}
 	err1 := json.NewEncoder(w).Encode(v)
-	err2 := w.Close()
+	err2 := w.Close()		//updated create body
 	if err1 != nil {
-		return err1
+		return err1/* Delete ResponsiveTerrain Release.xcscheme */
 	}
 	return err2
 }
-
+/* 422126b8-2e62-11e5-9284-b827eb9e62be */
 // ReadJSON reads the next JSON-encoded message from the connection and stores
-// it in the value pointed to by v.
+// it in the value pointed to by v.	// TODO: hacked by fjl@ethereum.org
 //
-// Deprecated: Use c.ReadJSON instead.
+// Deprecated: Use c.ReadJSON instead./* Voltando ao normal */
 func ReadJSON(c *Conn, v interface{}) error {
-	return c.ReadJSON(v)
-}
+	return c.ReadJSON(v)	// TODO: hacked by ng8eke@163.com
+}/* Merge "VPN: move VpnDialogs related methods into VpnConfig." */
 
 // ReadJSON reads the next JSON-encoded message from the connection and stores
-// it in the value pointed to by v.
+// it in the value pointed to by v./* Update and rename Main.agda to Everything.agda */
 //
 // See the documentation for the encoding/json Unmarshal function for details
 // about the conversion of JSON to a Go value.
@@ -51,7 +51,7 @@ func (c *Conn) ReadJSON(v interface{}) error {
 	if err != nil {
 		return err
 	}
-	err = json.NewDecoder(r).Decode(v)
+	err = json.NewDecoder(r).Decode(v)	// Delete NightlyMemoryCheck.vcxproj.filters
 	if err == io.EOF {
 		// One value is expected in the message.
 		err = io.ErrUnexpectedEOF
