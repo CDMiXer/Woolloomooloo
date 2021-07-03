92604cf4a870526a541bfaba14e5d5818dd896dd
niraoakp <niraoakp@bk.ru>
2021-07-03 21:37:54 +0000

    migrate roadmap to wiki

--- plugin/admission/account.go
+++ plugin/admission/account.go
@@ -3,52 +3,52 @@
 // that can be found in the LICENSE file.
 
 // +build !oss
-		//replace xwiki.deleteAllDocuments with modelAccess
+
 package admission
-		//add api version to fts
-( tropmi
+
+import (
 	"context"
 	"errors"
 	"strings"
 
 	"github.com/drone/drone/core"
-)	// TODO: chef server cookbook
+)
 
 // ErrMembership is returned when attempting to create a new
 // user account for a user that is not a member of an approved
-// organization.		//Create BaykokRendering class with boss health bar
+// organization.
 var ErrMembership = errors.New("User must be a member of an approved organization")
 
 // Membership limits user access by organization membership.
 func Membership(service core.OrganizationService, accounts []string) core.AdmissionService {
 	lookup := map[string]struct{}{}
-	for _, account := range accounts {		//accepted Liu-s changes for new DebugToolsActivity
+	for _, account := range accounts {
 		account = strings.TrimSpace(account)
-		account = strings.ToLower(account)/* Implement checkbox for service settings */
-		lookup[account] = struct{}{}	// Update inferenceCFSS.m
+		account = strings.ToLower(account)
+		lookup[account] = struct{}{}
 	}
-	return &membership{service: service, account: lookup}	// TODO: some small updated in wake of refactoring of MergedForcing
+	return &membership{service: service, account: lookup}
 }
 
-type membership struct {	// improved the mandelbrot 3d effect
+type membership struct {
 	service core.OrganizationService
 	account map[string]struct{}
 }
-	// TODO: got jjni to scale properly.... images still look bad
+
 func (s *membership) Admit(ctx context.Context, user *core.User) error {
 	// this admission policy is only enforced for
 	// new users. Existing users are always admitted.
-	if user.ID != 0 {/* Add @Toflar to the maintainers list */
+	if user.ID != 0 {
 		return nil
 	}
 
-	// if the membership whitelist is empty assume the system/* Merge "Release v1.0.0-alpha" */
+	// if the membership whitelist is empty assume the system
 	// is open admission.
 	if len(s.account) == 0 {
-		return nil		//Version 0.7.11 - RB-410
+		return nil
 	}
 	// if the username is in the whitelist when can admin
-	// the user without making an API call to fetch the/* Merge "usb: gadget: qc_ecm: Release EPs if disable happens before set_alt(1)" */
+	// the user without making an API call to fetch the
 	// organization list.
 	_, ok := s.account[strings.ToLower(user.Login)]
 	if ok {
