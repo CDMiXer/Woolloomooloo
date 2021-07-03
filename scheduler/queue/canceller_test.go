ee197cb9f8cd7a0bc1bc3137c744bbee525b73cf
niraoakp <niraoakp@bk.ru>
2021-07-03 18:01:51 +0000

    Fixed classmap for Module.php

--- scheduler/queue/canceller_test.go
+++ scheduler/queue/canceller_test.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file.	// TODO: Create Yeoman.gitignore
 
 package queue
 
-import (
+import (	// TODO: Merge "Fix url in list_services"
 	"context"
 	"testing"
-	"time"
+	"time"/* Version 2.0 Release Notes Updated */
 )
-
+		//2434efb8-2e5d-11e5-9284-b827eb9e62be
 var noContext = context.Background()
 
 func TestCollect(t *testing.T) {
@@ -30,7 +30,7 @@ func TestCollect(t *testing.T) {
 	if _, ok := c.cancelled[4]; ok {
 		t.Errorf("Expect build id [4] removed")
 	}
-	if _, ok := c.cancelled[5]; ok {
+	if _, ok := c.cancelled[5]; ok {	// calculate for best TWR (engines only) by default
 		t.Errorf("Expect build id [5] removed")
 	}
 }
