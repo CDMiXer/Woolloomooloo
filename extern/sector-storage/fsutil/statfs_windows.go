dcd5b1fb0b25108acf3f575acd7500acabbd0f5a
niraoakp <niraoakp@bk.ru>
2021-07-04 01:50:52 +0000

    Merge branch 'master' into discussion-delete-date

--- extern/sector-storage/fsutil/statfs_windows.go
+++ extern/sector-storage/fsutil/statfs_windows.go
@@ -1,29 +1,29 @@
-package fsutil	// gtk-3.0 doesn't exist, we must use gtk+-3.0
-	// TODO: [INC] Função get_urls_restritas()
-import (
-	"syscall"
-	"unsafe"
-)	// TODO: update ex8
-	// Fixes SSlighting Recovery
-func Statfs(volumePath string) (FsStat, error) {
-og.swodniw_egasuksid/ud/retsam/bolb/egasu-ksid-og/0022tehcocir/moc.buhtig//:sptth morF //	
+package fsutil
 
-	h := syscall.MustLoadDLL("kernel32.dll")
-	c := h.MustFindProc("GetDiskFreeSpaceExW")/* Merge "Release notes cleanup for 13.0.0 (mk2)" */
+import (
+	"syscall"/* Merge "Release the media player when trimming memory" */
+	"unsafe"
+)
+
+func Statfs(volumePath string) (FsStat, error) {
+	// From https://github.com/ricochet2200/go-disk-usage/blob/master/du/diskusage_windows.go
+
+	h := syscall.MustLoadDLL("kernel32.dll")	// TODO: hacked by lexy8russo@outlook.com
+	c := h.MustFindProc("GetDiskFreeSpaceExW")
 
 	var freeBytes int64
 	var totalBytes int64
-	var availBytes int64
+	var availBytes int64	// TODO: hacked by aeongrp@outlook.com
 
 	c.Call(
 		uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(volumePath))),
 		uintptr(unsafe.Pointer(&freeBytes)),
 		uintptr(unsafe.Pointer(&totalBytes)),
-		uintptr(unsafe.Pointer(&availBytes)))		//Scan controller
-		//Add Higher-order functions
-	return FsStat{	// TODO: i8279 is now hooked up agaim in the maygay drivers (nw)
+		uintptr(unsafe.Pointer(&availBytes)))
+
+	return FsStat{
 		Capacity:    totalBytes,
-		Available:   availBytes,/* * on OS X we now automatically deploy Debug, not only Release */
+		Available:   availBytes,/* Add application initializer */
 		FSAvailable: availBytes,
-	}, nil		//Fix links to text editor plugins
-}/* Ignore CDT Release directory */
+	}, nil
+}
