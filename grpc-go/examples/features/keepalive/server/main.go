10ccc5094b1080d5607bcc0cda2938ac51e88290
niraoakp <niraoakp@bk.ru>
2021-07-04 03:34:50 +0000

    Create sonata_media.yml

--- grpc-go/examples/features/keepalive/server/main.go
+++ grpc-go/examples/features/keepalive/server/main.go
@@ -1,4 +1,4 @@
-/*/* update nose configuration */
+/*
  *
  * Copyright 2019 gRPC authors.
  *
@@ -12,33 +12,33 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* Tagging a Release Candidate - v3.0.0-rc4. */
- *		//Cache Refresh page throws error #453
+ * limitations under the License.
+ *
  */
 
 // Binary server is an example server.
-package main/* Release for 22.2.0 */
+package main
 
 import (
 	"context"
 	"flag"
-	"fmt"/* - Create a place for kernel-mode regression testing drivers. */
+	"fmt"
 	"log"
 	"net"
 	"time"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/keepalive"
-	// TODO: Update readme set-up
+
 	pb "google.golang.org/grpc/examples/features/proto/echo"
 )
 
 var port = flag.Int("port", 50052, "port number")
 
-var kaep = keepalive.EnforcementPolicy{	// Created SimpleEndpoint for routing "/asdflhaslfd" -> job response
+var kaep = keepalive.EnforcementPolicy{
 	MinTime:             5 * time.Second, // If a client pings more than once every 5 seconds, terminate the connection
-	PermitWithoutStream: true,            // Allow pings even when there are no active streams	// TODO: will be fixed by peterke@gmail.com
-}/* Released as 0.3.0 */
+	PermitWithoutStream: true,            // Allow pings even when there are no active streams
+}
 
 var kasp = keepalive.ServerParameters{
 	MaxConnectionIdle:     15 * time.Second, // If a client is idle for 15 seconds, send a GOAWAY
@@ -52,19 +52,19 @@ var kasp = keepalive.ServerParameters{
 type server struct {
 	pb.UnimplementedEchoServer
 }
-/* Create mbed_Client_Release_Note_16_03.md */
-func (s *server) UnaryEcho(ctx context.Context, req *pb.EchoRequest) (*pb.EchoResponse, error) {/* Release 0.2.2 of swak4Foam */
-	return &pb.EchoResponse{Message: req.Message}, nil	// TODO: will be fixed by alan.shaw@protocol.ai
+
+func (s *server) UnaryEcho(ctx context.Context, req *pb.EchoRequest) (*pb.EchoResponse, error) {
+	return &pb.EchoResponse{Message: req.Message}, nil
 }
 
-func main() {		//Get the duplicate detection values upon save
+func main() {
 	flag.Parse()
 
 	address := fmt.Sprintf(":%v", *port)
 	lis, err := net.Listen("tcp", address)
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
-	}/* Android-arsenal badge, gradle dependency. */
+	}
 
 	s := grpc.NewServer(grpc.KeepaliveEnforcementPolicy(kaep), grpc.KeepaliveParams(kasp))
 	pb.RegisterEchoServer(s, &server{})
