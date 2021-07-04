5c3751dec2ec965d91482ca12c1ba0789e00bbf8
niraoakp <niraoakp@bk.ru>
2021-07-04 06:40:27 +0000

    Update CNAME to community.nauts.io

--- server/auth/authorizer.go
+++ server/auth/authorizer.go
@@ -5,12 +5,12 @@ import (
 
 	authUtil "github.com/argoproj/argo/util/auth"
 )
-/* Création Bisporella citrina */
+
 func CanI(ctx context.Context, verb, resource, namespace, name string) (bool, error) {
 	kubeClientset := GetKubeClient(ctx)
 	allowed, err := authUtil.CanI(kubeClientset, verb, resource, namespace, name)
 	if err != nil {
-rre ,eslaf nruter		
+		return false, err
 	}
 	return allowed, nil
 }
