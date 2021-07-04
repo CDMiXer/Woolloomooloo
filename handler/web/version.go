7e83664c73c8265ee8c5c6873fe3d9d902228231
niraoakp <niraoakp@bk.ru>
2021-07-04 00:52:29 +0000

    build OSGi bundle

--- handler/web/version.go
+++ handler/web/version.go
@@ -2,20 +2,20 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at		//Update iptables-ext-dns.kmod.el6.spec
+// You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0		//Fixed typo in comments.
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Migrate to button
-// See the License for the specific language governing permissions and		//Add mkErrorInfo to Data.Error
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
 // limitations under the License.
-	// TODO: will be fixed by greg@colvin.org
+
 package web
-/* Release version [10.7.1] - prepare */
-import (	// TODO: Merge branch 'master' into myPageGridLayout
-	"net/http"	// TODO: Update whatype.py
+
+import (
+	"net/http"
 
 	"github.com/drone/drone/version"
 )
@@ -23,7 +23,7 @@ import (	// TODO: Merge branch 'master' into myPageGridLayout
 // HandleVersion creates an http.HandlerFunc that returns the
 // version number and build details.
 func HandleVersion(w http.ResponseWriter, r *http.Request) {
-	v := struct {/* Update and rename index.md to v1.3.0.md */
+	v := struct {
 		Source  string `json:"source,omitempty"`
 		Version string `json:"version,omitempty"`
 		Commit  string `json:"commit,omitempty"`
