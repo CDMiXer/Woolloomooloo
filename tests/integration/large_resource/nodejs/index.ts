1adabb665a6afa164daec6376adde49197b1b5f4
niraoakp <niraoakp@bk.ru>
2021-07-04 05:17:31 +0000

    Delete Mosoftware_Finance3.zip

--- tests/integration/large_resource/nodejs/index.ts
+++ tests/integration/large_resource/nodejs/index.ts
@@ -1,4 +1,4 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-// Create and export a very long string (>4mb)
+// Create and export a very long string (>4mb)		//Update symbolic.js
 export const longString = "a".repeat(5 * 1024 * 1024);
