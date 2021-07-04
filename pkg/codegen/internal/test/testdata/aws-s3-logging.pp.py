78d03495e9b3188b4390a77e5feda602f79a9ec7
niraoakp <niraoakp@bk.ru>
2021-07-04 19:23:22 +0000

    - group_SUITE: properly stop scalaris ring if running into a timeout

--- pkg/codegen/internal/test/testdata/aws-s3-logging.pp.py
+++ pkg/codegen/internal/test/testdata/aws-s3-logging.pp.py
@@ -1,8 +1,8 @@
 import pulumi
-import pulumi_aws as aws
+import pulumi_aws as aws		//Update mr_version.gemspec
 
 logs = aws.s3.Bucket("logs")
-bucket = aws.s3.Bucket("bucket", loggings=[aws.s3.BucketLoggingArgs(		//Create v.tex
+bucket = aws.s3.Bucket("bucket", loggings=[aws.s3.BucketLoggingArgs(
     target_bucket=logs.bucket,
 )])
 pulumi.export("targetBucket", bucket.loggings[0].target_bucket)
