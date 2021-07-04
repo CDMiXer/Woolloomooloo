4df441f8547cf5df2e85847369dcea528b9b84e4
niraoakp <niraoakp@bk.ru>
2021-07-04 01:26:06 +0000

    Fixed CSV export/import of class instances.

--- tests/integration/partial_state/step2/index.ts
+++ tests/integration/partial_state/step2/index.ts
@@ -1,4 +1,4 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// TODO: Added some of Goomph's known adopters to the README.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Released springjdbcdao version 1.6.9 */
 
 // file is deliberately empty.
 // The engine should delete "a" since it's in the checkpoint.
