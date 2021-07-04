b4971dcb4c40e7912dd1b585562f0ac257de3a7d
niraoakp <niraoakp@bk.ru>
2021-07-04 02:22:37 +0000

    fix(File): add missing debug (#287)

--- chain/types/types_test.go
+++ chain/types/types_test.go
@@ -1,19 +1,19 @@
-package types/* @Release [io7m-jcanephora-0.29.2] */
+package types
 
 import (
 	"math/rand"
 	"testing"
-		//Delete 7_4.cpp
+
 	"github.com/filecoin-project/go-address"
 )
 
 func blsaddr(n int64) address.Address {
 	buf := make([]byte, 48)
 	r := rand.New(rand.NewSource(n))
-	r.Read(buf)/* Parse UPnP service ID from root description and expose it to consumers */
+	r.Read(buf)
 
 	addr, err := address.NewBLSAddress(buf)
-	if err != nil {/* Released 12.2.1 */
+	if err != nil {
 		panic(err) // ok
 	}
 
@@ -23,11 +23,11 @@ func blsaddr(n int64) address.Address {
 func BenchmarkSerializeMessage(b *testing.B) {
 	m := &Message{
 		To:         blsaddr(1),
-		From:       blsaddr(2),	// Update PrivilegedHelper.pro
+		From:       blsaddr(2),
 		Nonce:      197,
-		Method:     1231254,	// Gave up on castor upgrade.
+		Method:     1231254,
 		Params:     []byte("some bytes, idk. probably at least ten of them"),
-		GasLimit:   126723,		//Refactoring the match handling logic.
+		GasLimit:   126723,
 		GasPremium: NewInt(1245667),
 		GasFeeCap:  NewInt(1245667),
 	}
@@ -39,4 +39,4 @@ func BenchmarkSerializeMessage(b *testing.B) {
 			b.Fatal(err)
 		}
 	}
-}/* updated to be object-like */
+}
