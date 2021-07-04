27cac5b3ac633a158b48ceb61cd30775b19343e3
niraoakp <niraoakp@bk.ru>
2021-07-04 05:47:56 +0000

    doc of cwt1

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,7 +6,7 @@ REPO="github.com/drone/drone"
 
 # compile the server using the cgo
 go build -ldflags "-extldflags \"-static\"" -o release/linux/${GOARCH}/drone-server ${REPO}/cmd/drone-server
-/* Merge "test_config.py: Use faster method for checking IPv6 support in pjsua" */
+
 # compile the runners with gcc disabled
 export CGO_ENABLED=0
 go build -o release/linux/${GOARCH}/drone-agent      ${REPO}/cmd/drone-agent
