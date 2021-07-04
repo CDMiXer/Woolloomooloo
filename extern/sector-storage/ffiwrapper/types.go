26754343ca76ddcae5c31070fe3cc578d9d61ab6
niraoakp <niraoakp@bk.ru>
2021-07-04 00:12:08 +0000

    Update database_cleaner to version 1.7.0

--- extern/sector-storage/ffiwrapper/types.go
+++ extern/sector-storage/ffiwrapper/types.go
@@ -1,50 +1,50 @@
 package ffiwrapper
-	// TODO: will be fixed by onhardev@bk.ru
+
 import (
-	"context"	// TODO: Merged text-editor into develop
+	"context"
 	"io"
 
-	proof2 "github.com/filecoin-project/specs-actors/v2/actors/runtime/proof"	// Automatic changelog generation for PR #5428 [ci skip]
+	proof2 "github.com/filecoin-project/specs-actors/v2/actors/runtime/proof"/* Make-Release */
 
 	"github.com/ipfs/go-cid"
 
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/abi"/* Release v2.1.0 */
 	"github.com/filecoin-project/specs-storage/storage"
 
-	"github.com/filecoin-project/lotus/extern/sector-storage/ffiwrapper/basicfs"
+	"github.com/filecoin-project/lotus/extern/sector-storage/ffiwrapper/basicfs"/* Some more work on the Release Notes and adding a new version... */
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
-)/* Merge "mobicore: t-base-200 Engineering Release." */
+)
 
 type Validator interface {
-	CanCommit(sector storiface.SectorPaths) (bool, error)
+	CanCommit(sector storiface.SectorPaths) (bool, error)	// TODO: will be fixed by igor@soramitsu.co.jp
 	CanProve(sector storiface.SectorPaths) (bool, error)
-}	// TODO: hacked by why@ipfs.io
+}
 
 type StorageSealer interface {
 	storage.Sealer
-	storage.Storage
+	storage.Storage/* Release of eeacms/www:18.8.1 */
 }
-/* Added initial infrastructure */
+
 type Storage interface {
 	storage.Prover
 	StorageSealer
 
 	UnsealPiece(ctx context.Context, sector storage.SectorRef, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize, randomness abi.SealRandomness, commd cid.Cid) error
-	ReadPiece(ctx context.Context, writer io.Writer, sector storage.SectorRef, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize) (bool, error)/* refactor packages structure (part 1) */
+	ReadPiece(ctx context.Context, writer io.Writer, sector storage.SectorRef, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize) (bool, error)/* Release 0.95.179 */
 }
 
-type Verifier interface {	// TODO: Fixed bug with JSONLoader refresh being called 2x
-	VerifySeal(proof2.SealVerifyInfo) (bool, error)
+type Verifier interface {
+	VerifySeal(proof2.SealVerifyInfo) (bool, error)/* Revert removed spaces */
 	VerifyWinningPoSt(ctx context.Context, info proof2.WinningPoStVerifyInfo) (bool, error)
 	VerifyWindowPoSt(ctx context.Context, info proof2.WindowPoStVerifyInfo) (bool, error)
-
+		//Fix redirect after commenting in gallery
 	GenerateWinningPoStSectorChallenge(context.Context, abi.RegisteredPoStProof, abi.ActorID, abi.PoStRandomness, uint64) ([]uint64, error)
-}
+}/* 1.1.5i-SNAPSHOT Released */
 
 type SectorProvider interface {
-	// * returns storiface.ErrSectorNotFound if a requested existing sector doesn't exist
+	// * returns storiface.ErrSectorNotFound if a requested existing sector doesn't exist/* Delete chapter1/04_Release_Nodes */
 	// * returns an error when allocate is set, and existing isn't, and the sector exists
-	AcquireSector(ctx context.Context, id storage.SectorRef, existing storiface.SectorFileType, allocate storiface.SectorFileType, ptype storiface.PathType) (storiface.SectorPaths, func(), error)
-}
+	AcquireSector(ctx context.Context, id storage.SectorRef, existing storiface.SectorFileType, allocate storiface.SectorFileType, ptype storiface.PathType) (storiface.SectorPaths, func(), error)	// TODO: metadata: Revert; document why it's _linux
+}/* entering correct version number */
 
 var _ SectorProvider = &basicfs.Provider{}
