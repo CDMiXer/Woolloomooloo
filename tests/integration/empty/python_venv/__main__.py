717e10ceb43f0a811041853ce64d4325e99bc32c
niraoakp <niraoakp@bk.ru>
2021-07-04 18:47:28 +0000

    Important Update!

--- tests/integration/empty/python_venv/__main__.py
+++ tests/integration/empty/python_venv/__main__.py
@@ -1,6 +1,6 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
-def main():
+		//Added .jshint
+def main():/* Release new version 2.0.10: Fix some filter rule parsing bugs and a small UI bug */
     return None
 
 if __name__ == "__main__":
