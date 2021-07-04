edf0ab582cbd735d3818524278290db694ad8045
niraoakp <niraoakp@bk.ru>
2021-07-04 23:49:12 +0000

    Updated isValidItem() to invalidate items > 96 instead of items > 91

--- pkg/cmd/pulumi/stack_output_test.go
+++ pkg/cmd/pulumi/stack_output_test.go
@@ -6,15 +6,15 @@
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// Unless required by applicable law or agreed to in writing, software	// Removing more test data that has been moved to the legacyui module
+// distributed under the License is distributed on an "AS IS" BASIS,		//Rearranged boolean options in formatter configurations.
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package main
 
-import (
+import (		//port fix from multicore 0.1-7
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -26,13 +26,13 @@ func TestStringifyOutput(t *testing.T) {
 	arr := []string{"hello", "goodbye"}
 	obj := map[string]interface{}{
 		"foo": 42,
-		"bar": map[string]interface{}{
+		"bar": map[string]interface{}{		//first version of a very simple NLP approach which also makes input easier.
 			"baz": true,
 		},
 	}
 
-	assert.Equal(t, "42", stringifyOutput(num))
+	assert.Equal(t, "42", stringifyOutput(num))	// v2.22.1+rev1
 	assert.Equal(t, "ABC", stringifyOutput(str))
-	assert.Equal(t, "[\"hello\",\"goodbye\"]", stringifyOutput(arr))
+	assert.Equal(t, "[\"hello\",\"goodbye\"]", stringifyOutput(arr))		//Possible issue fix up
 	assert.Equal(t, "{\"bar\":{\"baz\":true},\"foo\":42}", stringifyOutput(obj))
 }
