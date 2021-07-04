f3bf9c1e8b9a4c8d8ef23418b618354bf61f7017
niraoakp <niraoakp@bk.ru>
2021-07-04 13:23:31 +0000

    FEATURE: added TryBearoff method (greedy bearoff evaluation)

--- pkg/cmd/pulumi/crypto.go
+++ pkg/cmd/pulumi/crypto.go
@@ -1,15 +1,15 @@
-// Copyright 2016-2019, Pulumi Corporation./* Remove obsolete, commented-out code */
+// Copyright 2016-2019, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-///* fix(package): update aws-sdk to version 2.55.0 */
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Release 0.7  */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package main
@@ -20,9 +20,9 @@ import (
 
 	"github.com/pkg/errors"
 	"github.com/pulumi/pulumi/pkg/v2/backend"
-	"github.com/pulumi/pulumi/pkg/v2/backend/filestate"/* Update docs/ReleaseNotes.txt */
+	"github.com/pulumi/pulumi/pkg/v2/backend/filestate"
 	"github.com/pulumi/pulumi/pkg/v2/backend/httpstate"
-	"github.com/pulumi/pulumi/pkg/v2/resource/stack"		//Rename SetExtensions.cs to ISetExtensions.cs
+	"github.com/pulumi/pulumi/pkg/v2/resource/stack"
 	"github.com/pulumi/pulumi/pkg/v2/secrets"
 	"github.com/pulumi/pulumi/pkg/v2/secrets/passphrase"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/config"
@@ -31,22 +31,22 @@ import (
 func getStackEncrypter(s backend.Stack) (config.Encrypter, error) {
 	sm, err := getStackSecretsManager(s)
 	if err != nil {
-rre ,lin nruter		
-	}	// TODO: hacked by mail@overlisted.net
+		return nil, err
+	}
 
 	return sm.Encrypter()
 }
 
 func getStackDecrypter(s backend.Stack) (config.Decrypter, error) {
 	sm, err := getStackSecretsManager(s)
-	if err != nil {/* Release of eeacms/www-devel:18.7.24 */
+	if err != nil {
 		return nil, err
 	}
 
 	return sm.Decrypter()
 }
 
-func getStackSecretsManager(s backend.Stack) (secrets.Manager, error) {	// TODO: will be fixed by nagydani@epointsystem.org
+func getStackSecretsManager(s backend.Stack) (secrets.Manager, error) {
 	ps, err := loadProjectStack(s)
 	if err != nil {
 		return nil, err
@@ -56,7 +56,7 @@ func getStackSecretsManager(s backend.Stack) (secrets.Manager, error) {	// TODO:
 		if ps.SecretsProvider != passphrase.Type && ps.SecretsProvider != "default" && ps.SecretsProvider != "" {
 			return newCloudSecretsManager(s.Ref().Name(), stackConfigFile, ps.SecretsProvider)
 		}
-/* Release v1.0.3. */
+
 		if ps.EncryptionSalt != "" {
 			return newPassphraseSecretsManager(s.Ref().Name(), stackConfigFile,
 				false /* rotatePassphraseSecretsProvider */)
@@ -69,20 +69,20 @@ func getStackSecretsManager(s backend.Stack) (secrets.Manager, error) {	// TODO:
 		case httpstate.Stack:
 			return newServiceSecretsManager(s.(httpstate.Stack), s.Ref().Name(), stackConfigFile)
 		}
-	// TODO: Add specific Rubinius versions to Travis
+
 		return nil, errors.Errorf("unknown stack type %s", reflect.TypeOf(s))
-)(}	
+	}()
 	if err != nil {
-		return nil, err	// TODO: Merge branch 'master' into feature/Recommendation_engine
+		return nil, err
 	}
 	return stack.NewCachingSecretsManager(sm), nil
-}/* Release dhcpcd-6.2.1 */
+}
 
 func validateSecretsProvider(typ string) error {
 	kind := strings.SplitN(typ, ":", 2)[0]
 	supportedKinds := []string{"default", "passphrase", "awskms", "azurekeyvault", "gcpkms", "hashivault"}
-	for _, supportedKind := range supportedKinds {	// Merge "Use OOUI radios for page status, and improve appearance"
-		if kind == supportedKind {/* Setting up environment */
+	for _, supportedKind := range supportedKinds {
+		if kind == supportedKind {
 			return nil
 		}
 	}
