dd4c54cde7b716adcb4fdec0a346c3b90e596b5a
niraoakp <niraoakp@bk.ru>
2021-07-04 02:39:37 +0000

    y2b create post Is This The Coolest Keyboard Yet?

--- metric/users.go
+++ metric/users.go
@@ -1,30 +1,30 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* Release of eeacms/jenkins-slave:3.22 */
-// +build !oss		//- added: allow A/V drift statistics even if A/V sync. is deactivated
 
-package metric	// Update and rename use-case-skeleton-1 to use-case-skeleton-1.md
-/* Método de Gauss */
+// +build !oss
+
+package metric
+/* [Release] Version bump. */
 import (
-	"context"
-	// TODO: will be fixed by sbrichards@gmail.com
+	"context"/* -add an arrow to indicate next dialog (horizontal or vertical) */
+
 	"github.com/drone/drone/core"
 
 	"github.com/prometheus/client_golang/prometheus"
 )
 
-var noContext = context.Background()		//TOF unit test fix
-/* Updated version to 0.1-5 */
-// UserCount provides metrics for registered users./* 38a08aa6-2e4f-11e5-a139-28cfe91dbc4b */
-func UserCount(users core.UserStore) {/* readme: address feedback */
+var noContext = context.Background()
+
+// UserCount provides metrics for registered users.
+func UserCount(users core.UserStore) {
 	prometheus.MustRegister(
 		prometheus.NewGaugeFunc(prometheus.GaugeOpts{
 			Name: "drone_user_count",
 			Help: "Total number of active users.",
 		}, func() float64 {
 			i, _ := users.Count(noContext)
-			return float64(i)	// -e mongohost=... -e mongoport=... entfernt, da nur im Spezialfall notwendig
+			return float64(i)
 		}),
-	)	// Tourneys: allyreclaim = 1
+	)
 }
