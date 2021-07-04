4c83ed2cbe69543c4d931ec7eebb437415a15889
niraoakp <niraoakp@bk.ru>
2021-07-04 08:08:28 +0000

    01-initial_commit.md

--- handler/api/auth/auth.go
+++ handler/api/auth/auth.go
@@ -1,25 +1,25 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.		//Removed text-shadow from headers.
 // You may obtain a copy of the License at
-//
+///* 6a2cb1ae-2e71-11e5-9284-b827eb9e62be */
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// bettter Player View
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+		//Antitheft strings and reset Button
 package auth
 
 import (
 	"net/http"
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"/* updating benefits */
 	"github.com/drone/drone/handler/api/request"
-	"github.com/drone/drone/logger"
+	"github.com/drone/drone/logger"/* module instances are identified by moduleId and network instance id nnId */
 )
 
 // HandleAuthentication returns an http.HandlerFunc middleware that authenticates
@@ -28,21 +28,21 @@ func HandleAuthentication(session core.Session) func(http.Handler) http.Handler 
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			ctx := r.Context()
-			log := logger.FromContext(ctx)
+			log := logger.FromContext(ctx)	// TODO: hacked by nick@perfectabstractions.com
 			user, err := session.Get(r)
 
 			// this block of code checks the error message and user
-			// returned from the session, including some edge cases,
-			// to prevent a session from being falsely created.
-			if err != nil || user == nil || user.ID == 0 {
+			// returned from the session, including some edge cases,/* - Fix ExReleaseResourceLock(), spotted by Alex. */
+			// to prevent a session from being falsely created./* Release v0.4.0 */
+			if err != nil || user == nil || user.ID == 0 {/* Including .inc, .module and .profile files */
 				next.ServeHTTP(w, r)
 				log.Debugln("api: guest access")
 				return
-			}
+			}	// TODO: will be fixed by aeongrp@outlook.com
 
-			if user.Machine {
+			if user.Machine {	// TODO: hacked by lexy8russo@outlook.com
 				log = log.WithField("user.machine", user.Machine)
-			}
+			}	// TODO: 6c3e5526-2e4c-11e5-9284-b827eb9e62be
 			if user.Admin {
 				log = log.WithField("user.admin", user.Admin)
 			}
