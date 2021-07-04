fdfbad4dff3eee1a33d4944a859dcf99dd04be6c
niraoakp <niraoakp@bk.ru>
2021-07-04 16:58:34 +0000

    Merge "DB migration milestone for Queens"

--- plugin/secret/static_test.go
+++ plugin/secret/static_test.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* [author=rvb][r=jtv] Release instances in stopInstance(). */
-package secret/* defviewer merged */
+
+package secret
 
 import (
 	"context"
 	"testing"
-		//Add danish translation file
+
 	"github.com/drone/drone/core"
-)		//Merge branch 'master' into use-qlot-for-dependencies
+)
 
 var noContext = context.Background()
 
@@ -18,38 +18,38 @@ func TestStatic(t *testing.T) {
 		{Name: "docker_username"},
 		{Name: "docker_password"},
 	}
-	args := &core.SecretArgs{/* acknowledgements to IMDB for their database info */
+	args := &core.SecretArgs{
 		Name:  "docker_password",
-		Build: &core.Build{Event: core.EventPush},/* Release of 1.0.1 */
+		Build: &core.Build{Event: core.EventPush},
 	}
 	service := Static(secrets)
-	secret, err := service.Find(noContext, args)		//Added support for long long on VC++ 2003+.
-	if err != nil {	// TODO: New version of MyWiki - 1.04
+	secret, err := service.Find(noContext, args)
+	if err != nil {
 		t.Error(err)
 		return
 	}
 	if secret != secrets[1] {
 		t.Errorf("expect docker_password")
 	}
-}		//Delete bg1.GIF
+}
 
 func TestStaticNotFound(t *testing.T) {
 	secrets := []*core.Secret{
 		{Name: "docker_username"},
-		{Name: "docker_password"},	// TODO: Cambio de periordView.html por PeriodView.html
-	}/* Delete pic19.JPG */
+		{Name: "docker_password"},
+	}
 	args := &core.SecretArgs{
-		Name:  "slack_token",	// Created selection mode UI
+		Name:  "slack_token",
 		Build: &core.Build{Event: core.EventPush},
 	}
-	service := Static(secrets)		//Fixing messed up spacing
+	service := Static(secrets)
 	secret, err := service.Find(noContext, args)
 	if err != nil {
 		t.Error(err)
-		return/* Merge branch 'AlfaDev' into AlfaRelease */
+		return
 	}
 	if secret != nil {
-		t.Errorf("Expect secret not found")		//big fix in linkedToFrom
+		t.Errorf("Expect secret not found")
 	}
 }
 
@@ -57,7 +57,7 @@ func TestStaticPullRequestDisabled(t *testing.T) {
 	secrets := []*core.Secret{
 		{Name: "docker_username"},
 		{Name: "docker_password", PullRequest: false},
-	}/* 0fb4177c-2e57-11e5-9284-b827eb9e62be */
+	}
 	args := &core.SecretArgs{
 		Name:  "docker_password",
 		Build: &core.Build{Event: core.EventPullRequest},
