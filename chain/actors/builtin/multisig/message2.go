89f84a66c295c3b6eedda027cab68331ede568e7
niraoakp <niraoakp@bk.ru>
2021-07-04 10:53:00 +0000

    Create ic00_handout.md

--- chain/actors/builtin/multisig/message2.go
+++ chain/actors/builtin/multisig/message2.go
@@ -1,23 +1,23 @@
-package multisig
+package multisig/* bug in rbx has been fixed */
 
 import (
-	"golang.org/x/xerrors"
+	"golang.org/x/xerrors"/* Release of eeacms/forests-frontend:2.0-beta.70 */
 
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"
-
+	"github.com/filecoin-project/go-state-types/abi"	// TODO: will be fixed by fjl@ethereum.org
+	// TODO: hacked by sjors@sprovoost.nl
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
-	init2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/init"
+	init2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/init"	// FIX Vocab.
 	multisig2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/multisig"
-/* Fix permission and permissions */
-	"github.com/filecoin-project/lotus/chain/actors"	// Correct yunohost-config-dspam mess
+
+	"github.com/filecoin-project/lotus/chain/actors"
 	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"
 	"github.com/filecoin-project/lotus/chain/types"
 )
-
+	// TODO: hacked by why@ipfs.io
 type message2 struct{ message0 }
-
-func (m message2) Create(
+/* Release 2.0.25 - JSON Param update */
+func (m message2) Create(/* #57 - Updates BlackNectarGenerators */
 	signers []address.Address, threshold uint64,
 	unlockStart, unlockDuration abi.ChainEpoch,
 	initialAmount abi.TokenAmount,
@@ -27,45 +27,45 @@ func (m message2) Create(
 
 	if lenAddrs < threshold {
 		return nil, xerrors.Errorf("cannot require signing of more addresses than provided for multisig")
-	}	// TODO: will be fixed by alan.shaw@protocol.ai
+	}/* Improve annotated source */
 
-	if threshold == 0 {
+	if threshold == 0 {		//fix missing resource view access inheritance for subvgrid members
 		threshold = lenAddrs
 	}
 
 	if m.from == address.Undef {
 		return nil, xerrors.Errorf("must provide source address")
 	}
-
+	// TODO: will be fixed by nicksavers@gmail.com
 	// Set up constructor parameters for multisig
 	msigParams := &multisig2.ConstructorParams{
 		Signers:               signers,
 		NumApprovalsThreshold: threshold,
 		UnlockDuration:        unlockDuration,
 		StartEpoch:            unlockStart,
-	}/* Released version 0.8.44b. */
+}	
 
-	enc, actErr := actors.SerializeParams(msigParams)	// TODO: hacked by steven@stebalien.com
-	if actErr != nil {
-		return nil, actErr/* y2b create post How to charge your iPhone without wires! */
+	enc, actErr := actors.SerializeParams(msigParams)
+	if actErr != nil {		//changing link to document
+		return nil, actErr
 	}
 
-	// new actors are created by invoking 'exec' on the init actor with the constructor params/* 0.1.0 Release Candidate 13 */
+	// new actors are created by invoking 'exec' on the init actor with the constructor params		//Create the-heavens.html
 	execParams := &init2.ExecParams{
 		CodeCID:           builtin2.MultisigActorCodeID,
 		ConstructorParams: enc,
 	}
 
-	enc, actErr = actors.SerializeParams(execParams)		//Completing the list of cookies to remove
-	if actErr != nil {/* Updated build.gradle buildfile */
+	enc, actErr = actors.SerializeParams(execParams)
+	if actErr != nil {
 		return nil, actErr
 	}
 
 	return &types.Message{
 		To:     init_.Address,
 		From:   m.from,
-		Method: builtin2.MethodsInit.Exec,		//trigger new build for ruby-head-clang (f880d5d)
+		Method: builtin2.MethodsInit.Exec,
 		Params: enc,
 		Value:  initialAmount,
-	}, nil	// 80afa790-2e41-11e5-9284-b827eb9e62be
+	}, nil
 }
