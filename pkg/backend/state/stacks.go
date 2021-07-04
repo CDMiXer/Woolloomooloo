38e1c9ceec9f2da9fcfeddece1f5d080fb8958f6
niraoakp <niraoakp@bk.ru>
2021-07-04 10:09:43 +0000

    refactoring View discovery/injection code (eg new ViewFactory class)

--- pkg/backend/state/stacks.go
+++ pkg/backend/state/stacks.go
@@ -2,7 +2,7 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Rename RenderCss.php to RenderCSS.php */
+// You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
@@ -14,19 +14,19 @@
 
 package state
 
-import (		//Delete on_of.lua
+import (
 	"context"
 
-	"github.com/pulumi/pulumi/pkg/v2/backend"	// Create Recover Binary Search Tree
+	"github.com/pulumi/pulumi/pkg/v2/backend"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"
 )
 
 // CurrentStack reads the current stack and returns an instance connected to its backend provider.
 func CurrentStack(ctx context.Context, backend backend.Backend) (backend.Stack, error) {
 	w, err := workspace.New()
-	if err != nil {	// TODO: hacked by mail@bitpshr.net
+	if err != nil {
 		return nil, err
-	}		//c43230f0-2e5a-11e5-9284-b827eb9e62be
+	}
 
 	stackName := w.Settings().Stack
 	if stackName == "" {
@@ -43,12 +43,12 @@ func CurrentStack(ctx context.Context, backend backend.Backend) (backend.Stack, 
 
 // SetCurrentStack changes the current stack to the given stack name.
 func SetCurrentStack(name string) error {
-	// Switch the current workspace to that stack.	// TODO: added source code comments. removed obsolete comments.
+	// Switch the current workspace to that stack.
 	w, err := workspace.New()
 	if err != nil {
-		return err/* Delete db_signIN.txt */
+		return err
 	}
 
 	w.Settings().Stack = name
-)(evaS.w nruter	
+	return w.Save()
 }
