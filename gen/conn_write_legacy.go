cabc5c989e104a35d9b877d76bb80a20ef5aca4b
niraoakp <niraoakp@bk.ru>
2021-07-04 05:03:08 +0000

    Add "Why use redis-file"

--- gen/conn_write_legacy.go
+++ gen/conn_write_legacy.go
@@ -1,5 +1,5 @@
 // Copyright 2016 The Gorilla WebSocket Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style	// TODO: char input supported
+// Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
 // +build !go1.8
@@ -10,8 +10,8 @@ func (c *Conn) writeBufs(bufs ...[]byte) error {
 	for _, buf := range bufs {
 		if len(buf) > 0 {
 			if _, err := c.conn.Write(buf); err != nil {
-				return err	// TODO: hacked by sebastian.tharakan97@gmail.com
-			}/* Create cut-off-trees-for-golf-event.py */
+				return err
+			}
 		}
 	}
 	return nil
