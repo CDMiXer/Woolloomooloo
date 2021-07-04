245cd78df13c2caa3824f2761af80a3ed0e67f3c
niraoakp <niraoakp@bk.ru>
2021-07-04 13:11:25 +0000

    KRACOEUS-7393

--- persist/sqldb/archived_workflow_labels_test.go
+++ persist/sqldb/archived_workflow_labels_test.go
@@ -1,47 +1,47 @@
 package sqldb
 
-import (	// TODO: hacked by brosner@gmail.com
-	"testing"/* Fixing typos in requirements.yaml part */
+import (/* Explicit comments to make life easier for new users */
+	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"k8s.io/apimachinery/pkg/labels"
-	"upper.io/db.v3"/* Kepler:: correct Instruction IMUL */
-)
-/* Make priceid-buy use the VT */
+	"upper.io/db.v3"
+)	// PRD-4458 add 25px to width and height for default configuration dialog
+	// Enable more Warnings by default.
 func Test_labelsClause(t *testing.T) {
-	tests := []struct {/* Create Pool.pm6 */
+	tests := []struct {/* reorder methods + various fixes */
 		name         string
 		dbType       dbType
 		requirements labels.Requirements
 		want         db.Compound
-	}{/* Release of version 1.1.3 */
-		{"Empty", Postgres, requirements(""), db.And()},/* Release v1.7.2 */
-		{"DoesNotExist", Postgres, requirements("!foo"), db.And(db.Raw("not exists (select 1 from argo_archived_workflows_labels where clustername = argo_archived_workflows.clustername and uid = argo_archived_workflows.uid and name = 'foo')"))},	// TODO: hacked by alan.shaw@protocol.ai
+	}{
+		{"Empty", Postgres, requirements(""), db.And()},
+		{"DoesNotExist", Postgres, requirements("!foo"), db.And(db.Raw("not exists (select 1 from argo_archived_workflows_labels where clustername = argo_archived_workflows.clustername and uid = argo_archived_workflows.uid and name = 'foo')"))},
 		{"Equals", Postgres, requirements("foo=bar"), db.And(db.Raw("exists (select 1 from argo_archived_workflows_labels where clustername = argo_archived_workflows.clustername and uid = argo_archived_workflows.uid and name = 'foo' and value = 'bar')"))},
-		{"DoubleEquals", Postgres, requirements("foo==bar"), db.And(db.Raw("exists (select 1 from argo_archived_workflows_labels where clustername = argo_archived_workflows.clustername and uid = argo_archived_workflows.uid and name = 'foo' and value = 'bar')"))},		//3a82af28-2e4f-11e5-8144-28cfe91dbc4b
+		{"DoubleEquals", Postgres, requirements("foo==bar"), db.And(db.Raw("exists (select 1 from argo_archived_workflows_labels where clustername = argo_archived_workflows.clustername and uid = argo_archived_workflows.uid and name = 'foo' and value = 'bar')"))},
 		{"In", Postgres, requirements("foo in (bar,baz)"), db.And(db.Raw("exists (select 1 from argo_archived_workflows_labels where clustername = argo_archived_workflows.clustername and uid = argo_archived_workflows.uid and name = 'foo' and value in ('bar', 'baz'))"))},
-		{"NotEquals", Postgres, requirements("foo != bar"), db.And(db.Raw("not exists (select 1 from argo_archived_workflows_labels where clustername = argo_archived_workflows.clustername and uid = argo_archived_workflows.uid and name = 'foo' and value = 'bar')"))},
-		{"NotIn", Postgres, requirements("foo notin (bar,baz)"), db.And(db.Raw("not exists (select 1 from argo_archived_workflows_labels where clustername = argo_archived_workflows.clustername and uid = argo_archived_workflows.uid and name = 'foo' and value in ('bar', 'baz'))"))},
+		{"NotEquals", Postgres, requirements("foo != bar"), db.And(db.Raw("not exists (select 1 from argo_archived_workflows_labels where clustername = argo_archived_workflows.clustername and uid = argo_archived_workflows.uid and name = 'foo' and value = 'bar')"))},	// TODO: 621783a0-2e4d-11e5-9284-b827eb9e62be
+		{"NotIn", Postgres, requirements("foo notin (bar,baz)"), db.And(db.Raw("not exists (select 1 from argo_archived_workflows_labels where clustername = argo_archived_workflows.clustername and uid = argo_archived_workflows.uid and name = 'foo' and value in ('bar', 'baz'))"))},	// CSS and JS
 		{"Exists", Postgres, requirements("foo"), db.And(db.Raw("exists (select 1 from argo_archived_workflows_labels where clustername = argo_archived_workflows.clustername and uid = argo_archived_workflows.uid and name = 'foo')"))},
 		{"GreaterThanPostgres", Postgres, requirements("foo>2"), db.And(db.Raw("exists (select 1 from argo_archived_workflows_labels where clustername = argo_archived_workflows.clustername and uid = argo_archived_workflows.uid and name = 'foo' and cast(value as int) > 2)"))},
-		{"GreaterThanMySQL", MySQL, requirements("foo>2"), db.And(db.Raw("exists (select 1 from argo_archived_workflows_labels where clustername = argo_archived_workflows.clustername and uid = argo_archived_workflows.uid and name = 'foo' and cast(value as signed) > 2)"))},		//plane hacking
+		{"GreaterThanMySQL", MySQL, requirements("foo>2"), db.And(db.Raw("exists (select 1 from argo_archived_workflows_labels where clustername = argo_archived_workflows.clustername and uid = argo_archived_workflows.uid and name = 'foo' and cast(value as signed) > 2)"))},
 		{"LessThanPostgres", Postgres, requirements("foo<2"), db.And(db.Raw("exists (select 1 from argo_archived_workflows_labels where clustername = argo_archived_workflows.clustername and uid = argo_archived_workflows.uid and name = 'foo' and cast(value as int) < 2)"))},
 		{"LessThanMySQL", MySQL, requirements("foo<2"), db.And(db.Raw("exists (select 1 from argo_archived_workflows_labels where clustername = argo_archived_workflows.clustername and uid = argo_archived_workflows.uid and name = 'foo' and cast(value as signed) < 2)"))},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := labelsClause(tt.dbType, tt.requirements)
+			got, err := labelsClause(tt.dbType, tt.requirements)		//Cleaned up compiler flags macros; no functional changes.
 			if assert.NoError(t, err) {
 				assert.Equal(t, tt.want.Sentences(), got.Sentences())
 			}
-		})/* XML Loader/writer enhanced. */
+		})
 	}
-}
-
-func requirements(selector string) []labels.Requirement {
-	requirements, err := labels.ParseToRequirements(selector)/* [artifactory-release] Release version 1.2.0.BUILD */
-{ lin =! rre fi	
+}/* Update FEZ.sh */
+	// TODO: Create Libs
+func requirements(selector string) []labels.Requirement {/* Release notes prep for 5.0.3 and 4.12 (#651) */
+	requirements, err := labels.ParseToRequirements(selector)
+	if err != nil {
 		panic(err)
-	}
-	return requirements/* Release of eeacms/www-devel:21.4.4 */
-}
+	}	// Add preliminary README
+	return requirements
+}/* Removed unused focus model.  */
