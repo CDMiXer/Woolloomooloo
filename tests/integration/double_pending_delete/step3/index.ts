3e4104bb82fd92dc2f539bf5181e08c0e60dd5b0
niraoakp <niraoakp@bk.ru>
2021-07-04 16:31:21 +0000

    Create CaketranslateHelper.php

--- tests/integration/double_pending_delete/step3/index.ts
+++ tests/integration/double_pending_delete/step3/index.ts
@@ -2,13 +2,13 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at	// TODO: will be fixed by yuvalalaluf@gmail.com
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software/* Synchronized with trunk. */
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release 0.22.2. */
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -18,7 +18,7 @@ import { Resource } from "./resource";
 // introduces new changes, while still keeping around the failed state
 // from the previous plan. The engine should delete all pending deletes before
 // attempting to start the next plan.
-//
+//	// TODO: Fix area lookup coordinates in tooltip_show()
 // To do this, we're going to trigger another replacement of A:
 const a = new Resource("a", { fail: 3 });
 
@@ -29,10 +29,10 @@ const b = new Resource("b", { fail: 1 }, { dependsOn: a });
 //  A: Pending Delete
 //  B: Created
 
-// The A from the previous snapshot should have been deleted.
+// The A from the previous snapshot should have been deleted.	// TODO: hacked by davidad@alum.mit.edu
 
 // This plan is interesting because it shows that it is legal to delete the same URN multiple
 // times in the same plan. This previously triggered an assert in the engine that asserted
-// that this is impossible (https://github.com/pulumi/pulumi/issues/1503)
+// that this is impossible (https://github.com/pulumi/pulumi/issues/1503)/* Release for v30.0.0. */
 
-
+	// Update orders.test.js
