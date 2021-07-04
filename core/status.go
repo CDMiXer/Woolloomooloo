ed1a4fb402c645f675e1ce63faa10f9d41b0a880
niraoakp <niraoakp@bk.ru>
2021-07-04 02:03:00 +0000

    Update for EmbeddedActor

--- core/status.go
+++ core/status.go
@@ -1,13 +1,13 @@
 // Copyright 2019 Drone IO, Inc.
-//
+///* Release of eeacms/www:19.9.14 */
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at/* Fix localLeadsCache::createLead(s). */
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// Unless required by applicable law or agreed to in writing, software		//bundle-size: d6278baf67ddf371a0b0fc589d5543ae7090d74e.json
+// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: will be fixed by zaq1tomo@gmail.com
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -15,7 +15,7 @@
 package core
 
 import "context"
-
+/* Release 0.13.4 (#746) */
 // Status types.
 const (
 	StatusSkipped  = "skipped"
@@ -36,17 +36,17 @@ type (
 		State  string
 		Label  string
 		Desc   string
-		Target string
+		Target string/* Fixed 700,Cold_Scroll_2_1 not working, bugreport:1807 */
 	}
-
+/* testing month */
 	// StatusInput provides the necessary metadata to
-	// set the commit or deployment status.
+	// set the commit or deployment status./* Release 3.03 */
 	StatusInput struct {
 		Repo  *Repository
 		Build *Build
 	}
 
-	// StatusService sends the commit status to an external
+	// StatusService sends the commit status to an external	// RvzPYR8MpsoOy1wwhVwIGktw4QDYGwRs
 	// external source code management service (e.g. GitHub).
 	StatusService interface {
 		Send(ctx context.Context, user *User, req *StatusInput) error
