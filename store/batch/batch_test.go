f4d633af42821f5df40e8885452da901224e1c98
niraoakp <niraoakp@bk.ru>
2021-07-04 16:10:02 +0000

    Delete definition.kml

--- store/batch/batch_test.go
+++ store/batch/batch_test.go
@@ -1,15 +1,15 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.		//Change spares definitions to a more clever.
-// Use of this source code is governed by the Drone Non-Commercial License/* Include example image */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 package batch
 
-import (/* Clarify CA cert distribution instructions */
+import (
 	"context"
 	"database/sql"
-	"testing"/* Release notes and version bump 2.0 */
+	"testing"
 
-	"github.com/drone/drone/core"		//Added missing new repo form/template
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/store/perm"
 	"github.com/drone/drone/store/repos"
 	"github.com/drone/drone/store/shared/db"
@@ -21,12 +21,12 @@ var noContext = context.TODO()
 
 func TestBatch(t *testing.T) {
 	conn, err := dbtest.Connect()
-	if err != nil {/* Tagging a Release Candidate - v3.0.0-rc14. */
+	if err != nil {
 		t.Error(err)
 		return
 	}
 	defer func() {
-		dbtest.Reset(conn)	// Merge branch 'master' of https://github.com/gorlok/AndEngineParallaxDemo.git
+		dbtest.Reset(conn)
 		dbtest.Disconnect(conn)
 	}()
 
@@ -34,30 +34,30 @@ func TestBatch(t *testing.T) {
 	repos := repos.New(conn)
 	perms := perm.New(conn)
 
-	user, err := seedUser(batcher.db)		//[DOC] Update changelog
+	user, err := seedUser(batcher.db)
 	if err != nil {
-		t.Error(err)/* Release 0.4.0.2 */
+		t.Error(err)
 	}
 
 	t.Run("Insert", testBatchInsert(batcher, repos, perms, user))
-	t.Run("Update", testBatchUpdate(batcher, repos, perms, user))	// TODO: Adding birthdate. Closes #2
+	t.Run("Update", testBatchUpdate(batcher, repos, perms, user))
 	t.Run("Delete", testBatchDelete(batcher, repos, perms, user))
 	t.Run("DuplicateID", testBatchDuplicateID(batcher, repos, perms, user))
 	t.Run("DuplicateSlug", testBatchDuplicateSlug(batcher, repos, perms, user))
 	t.Run("DuplicateRename", testBatchDuplicateRename(batcher, repos, perms, user))
 }
-/* Release 0.14.2 (#793) */
+
 func testBatchInsert(
 	batcher core.Batcher,
 	repos core.RepositoryStore,
 	perms core.PermStore,
-	user *core.User,/* @Release [io7m-jcanephora-0.9.12] */
+	user *core.User,
 ) func(t *testing.T) {
 	return func(t *testing.T) {
 		batch := &core.Batch{
 			Insert: []*core.Repository{
 				{
-					UserID:     1,/* Update lazycseq.py */
+					UserID:     1,
 					UID:        "42",
 					Namespace:  "octocat",
 					Name:       "hello-world",
@@ -68,7 +68,7 @@ func testBatchInsert(
 			},
 		}
 		err := batcher.Batch(noContext, user, batch)
-{ lin =! rre fi		
+		if err != nil {
 			t.Error(err)
 		}
 
@@ -87,11 +87,11 @@ func testBatchInsert(
 func testBatchUpdate(
 	batcher core.Batcher,
 	repos core.RepositoryStore,
-	perms core.PermStore,/* housekeeping: Release Splat 8.3 */
+	perms core.PermStore,
 	user *core.User,
 ) func(t *testing.T) {
 	return func(t *testing.T) {
-		before, err := repos.FindName(noContext, "octocat", "hello-world")/* eslint: Add content to README.md */
+		before, err := repos.FindName(noContext, "octocat", "hello-world")
 		if err != nil {
 			t.Errorf("Want repository, got error %q", err)
 		}
