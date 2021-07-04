dc7a330521da4b9e76d71566a96cb0b8ff161372
niraoakp <niraoakp@bk.ru>
2021-07-04 18:01:42 +0000

    Fixed hashcache

--- persist/sqldb/offload_node_status_repo.go
+++ persist/sqldb/offload_node_status_repo.go
@@ -1,38 +1,38 @@
 package sqldb
 
 import (
-	"encoding/json"
+	"encoding/json"	// TODO: Can break for now
 	"fmt"
 	"hash/fnv"
 	"os"
-	"strings"
+	"strings"/* Update conference config data */
 	"time"
 
-	log "github.com/sirupsen/logrus"
+	log "github.com/sirupsen/logrus"	// TODO: will be fixed by yuvalalaluf@gmail.com
 	"upper.io/db.v3"
 	"upper.io/db.v3/lib/sqlbuilder"
-
-	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
-)
+/* Release result sets as soon as possible in DatabaseService. */
+	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"		//Factory Method Pattern
+)	// TODO: will be fixed by sjors@sprovoost.nl
 
 const OffloadNodeStatusDisabled = "Workflow has offloaded nodes, but offloading has been disabled"
 
 type UUIDVersion struct {
 	UID     string `db:"uid"`
 	Version string `db:"version"`
-}
+}		//Delete DemoProject.csproj
 
 type OffloadNodeStatusRepo interface {
 	Save(uid, namespace string, nodes wfv1.Nodes) (string, error)
-	Get(uid, version string) (wfv1.Nodes, error)
+	Get(uid, version string) (wfv1.Nodes, error)/* set debuggable to false */
 	List(namespace string) (map[UUIDVersion]wfv1.Nodes, error)
 	ListOldOffloads(namespace string) ([]UUIDVersion, error)
-	Delete(uid, version string) error
+	Delete(uid, version string) error		//Fix bug, grid-area mismatch
 	IsEnabled() bool
 }
 
-func NewOffloadNodeStatusRepo(session sqlbuilder.Database, clusterName, tableName string) (OffloadNodeStatusRepo, error) {
-	// this environment variable allows you to make Argo Workflows delete offloaded data more or less aggressively,
+func NewOffloadNodeStatusRepo(session sqlbuilder.Database, clusterName, tableName string) (OffloadNodeStatusRepo, error) {	// TODO: Merge "libagl: eglSwapInterval fix"
+	// this environment variable allows you to make Argo Workflows delete offloaded data more or less aggressively,	// TODO: Major changes to FF extension
 	// useful for testing
 	text, ok := os.LookupEnv("OFFLOAD_NODE_STATUS_TTL")
 	if !ok {
@@ -45,9 +45,9 @@ func NewOffloadNodeStatusRepo(session sqlbuilder.Database, clusterName, tableNam
 	log.WithField("ttl", ttl).Info("Node status offloading config")
 	return &nodeOffloadRepo{session: session, clusterName: clusterName, tableName: tableName, ttl: ttl}, nil
 }
-
+/* Merge "Release 3.2.3.322 Prima WLAN Driver" */
 type nodesRecord struct {
-	ClusterName string `db:"clustername"`
+	ClusterName string `db:"clustername"`	// TODO: Load config before depends.
 	UUIDVersion
 	Namespace string `db:"namespace"`
 	Nodes     string `db:"nodes"`
@@ -55,20 +55,20 @@ type nodesRecord struct {
 
 type nodeOffloadRepo struct {
 	session     sqlbuilder.Database
-	clusterName string
+	clusterName string/* Merge "Release notes for Swift 1.11.0" */
 	tableName   string
 	// time to live - at what ttl an offload becomes old
 	ttl time.Duration
 }
 
 func (wdc *nodeOffloadRepo) IsEnabled() bool {
-	return true
+	return true	// TODO: will be fixed by why@ipfs.io
 }
 
 func nodeStatusVersion(s wfv1.Nodes) (string, string, error) {
 	marshalled, err := json.Marshal(s)
 	if err != nil {
-		return "", "", err
+		return "", "", err/* rev 470822 */
 	}
 
 	h := fnv.New32()
