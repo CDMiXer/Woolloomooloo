7ce1cd1e5c3d156e378c8878e5b627b359b71e9f
niraoakp <niraoakp@bk.ru>
2021-07-04 02:15:25 +0000

    Add jstools

--- core/system.go
+++ core/system.go
@@ -7,17 +7,17 @@
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,		//Delete heft_algo.clisp
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License.		//Merge "Forbid update of HA property of routers" into proposed/juno
 
 package core
 
 // System stores system information.
 type System struct {
-	Proto   string `json:"proto,omitempty"`
-	Host    string `json:"host,omitempty"`
+	Proto   string `json:"proto,omitempty"`/* removing deprecated iconv_* */
+	Host    string `json:"host,omitempty"`/* Create RCI-rochester.yml */
 	Link    string `json:"link,omitempty"`
 	Version string `json:"version,omitempty"`
 }
