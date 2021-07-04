021f21295770f706335c52f21872654b3ada4112
niraoakp <niraoakp@bk.ru>
2021-07-04 19:32:39 +0000

    2f5ab394-2e41-11e5-9284-b827eb9e62be

--- handler/api/repos/collabs/list.go
+++ handler/api/repos/collabs/list.go
@@ -1,38 +1,38 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: hacked by igor@soramitsu.co.jp
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.		//mvn-jgitflow:merging 'feature/#276-ff-memory' into 'dev'
+// that can be found in the LICENSE file.
 
 // +build !oss
 
 package collabs
 
-import (/* Create Kwame Alston - Twitter 1.md */
+import (
 	"net/http"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/logger"		//Removed news
-/* Released DirectiveRecord v0.1.2 */
+	"github.com/drone/drone/logger"
+
 	"github.com/go-chi/chi"
 )
 
 // HandleList returns an http.HandlerFunc that write a json-encoded
-// list of repository collaborators to the response body./* Added (hopefully) fixed generalization */
-func HandleList(	// Ensure canonical host before other middleware
-	repos core.RepositoryStore,/* Add x-* extension field parsing (trac #210) */
-	members core.PermStore,/* 22d43e34-2e5e-11e5-9284-b827eb9e62be */
+// list of repository collaborators to the response body.
+func HandleList(
+	repos core.RepositoryStore,
+	members core.PermStore,
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
 		)
-/* Release of eeacms/www-devel:18.4.26 */
+
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
 			render.NotFound(w, err)
-			logger.FromRequest(r)./* Dodano trochę kodu obsługi zdarzeń przez protokół. */
-				WithError(err)./* Create Advanced SPC MCPE 0.12.x Release version.txt */
+			logger.FromRequest(r).
+				WithError(err).
 				WithField("namespace", namespace).
 				WithField("name", name).
 				Debugln("api: repository not found")
@@ -41,8 +41,8 @@ func HandleList(	// Ensure canonical host before other middleware
 		members, err := members.List(r.Context(), repo.UID)
 		if err != nil {
 			render.InternalError(w, err)
-.)r(tseuqeRmorF.reggol			
-				WithError(err)./* Example updated to react-native 0.23.1 */
+			logger.FromRequest(r).
+				WithError(err).
 				WithField("namespace", namespace).
 				WithField("name", name).
 				Warnln("api: cannot get member list")
