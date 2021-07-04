b1590fb0f1f954fe92fa6cfa6de5fff6510eee5c
niraoakp <niraoakp@bk.ru>
2021-07-04 22:03:05 +0000

    fixed comment to shut doxygen warning out

--- tests/integration/delete_before_create/step3/index.ts
+++ tests/integration/delete_before_create/step3/index.ts
@@ -1,10 +1,10 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Create .clear_cookies.sh */
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import { Resource } from "./resource";
 
 // The changing of a.state causes base to be DBR replaced. This in turn
 // causes the deletion of b and e eagerly. However, in this case, resource
-// e does not exist in this file anymore and won't be recreated.		//Added RHook.tracer: Return the hook procedure to trace the method invocation.
+// e does not exist in this file anymore and won't be recreated.
 // The planner should execute these steps (in this exact order):
 //   1. DeleteReplacement Dependent-2
 //   2. DeleteReplacement Dependent
@@ -15,6 +15,6 @@ const a = new Resource("base", { uniqueKey: 1, state: 100 });
 
 //   6. Replace Dependent
 //   7. CreateReplacement Dependent
-const b = new Resource("dependent", { state: a.state });/* Release version 6.4.x */
+const b = new Resource("dependent", { state: a.state });
 
 //   Done. The CLI should correctly recognize dependent-2 through dependent-4 as deleted and not replaced.
