1ed751773f2ee2086e0abb94f22564cdee31757f
niraoakp <niraoakp@bk.ru>
2021-07-04 18:40:09 +0000

    Work on GraphPath test and implementation

--- tests/integration/cloud_secrets_provider/index.ts
+++ tests/integration/cloud_secrets_provider/index.ts
@@ -1,5 +1,5 @@
 import * as pulumi from "@pulumi/pulumi";
 
 const config = new pulumi.Config();
-
+	// FIX: Problems reading XML data from previous versions
 export const out = config.requireSecret("mysecret");
