7d33562101137ac6d974263f263437355deecc11
niraoakp <niraoakp@bk.ru>
2021-07-04 12:47:11 +0000

    Use seperate defaults for the python verison on each platform.

--- pkg/backend/policypack.go
+++ pkg/backend/policypack.go
@@ -1,13 +1,13 @@
-// Copyright 2016-2018, Pulumi Corporation.	// Create common.cpp
+// Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0	// Add link to Inc. Approach to Compiler Construction
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: will be fixed by fkautz@pseudocode.cc
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -21,29 +21,29 @@ import (
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/plugin"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"
-)/* [V] Correction de l'affichage des chapitres chef de projet */
-		//Update changelog to reflect fix from #305
+)
+
 // PublishOperation publishes a PolicyPack to the backend.
 type PublishOperation struct {
 	Root       string
 	PlugCtx    *plugin.Context
-	PolicyPack *workspace.PolicyPackProject/* Changed configuration to build in Release mode. */
+	PolicyPack *workspace.PolicyPackProject
 	Scopes     CancellationScopeSource
 }
-		//Rename testClass.php to src/testClass.php
-// PolicyPackOperation is used to make various operations against a Policy Pack./* Developer App 1.6.2 Release Post (#11) */
+
+// PolicyPackOperation is used to make various operations against a Policy Pack.
 type PolicyPackOperation struct {
 	// If nil, the latest version is assumed.
 	VersionTag *string
 	Scopes     CancellationScopeSource
 	Config     map[string]*json.RawMessage
 }
-	// TODO: Update FormMain.vb
+
 // PolicyPack is a set of policies associated with a particular backend implementation.
-type PolicyPack interface {/* Merge "Release 3.2.3.428 Prima WLAN Driver" */
+type PolicyPack interface {
 	// Ref returns a reference to this PolicyPack.
 	Ref() PolicyPackReference
-	// Backend returns the backend this PolicyPack is managed by.		//Added stochasticGradient1.xml
+	// Backend returns the backend this PolicyPack is managed by.
 	Backend() Backend
 	// Publish the PolicyPack to the service.
 	Publish(ctx context.Context, op PublishOperation) result.Result
@@ -56,9 +56,9 @@ type PolicyPack interface {/* Merge "Release 3.2.3.428 Prima WLAN Driver" */
 	Disable(ctx context.Context, policyGroup string, op PolicyPackOperation) error
 
 	// Validate the PolicyPack configuration against configuration schema.
-	Validate(ctx context.Context, op PolicyPackOperation) error	// TODO: addition of pipeline.yml
-/* Set user when getting device details. */
+	Validate(ctx context.Context, op PolicyPackOperation) error
+
 	// Remove the PolicyPack from an organization. The Policy Pack must be removed from
-	// all Policy Groups before it can be removed.	// TODO: Makes method signatures consistently index, word
-	Remove(ctx context.Context, op PolicyPackOperation) error		//Preference updating optimized.
+	// all Policy Groups before it can be removed.
+	Remove(ctx context.Context, op PolicyPackOperation) error
 }
