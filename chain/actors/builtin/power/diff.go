42d5b4dd85c74d64290e67dccf575abab1a60d6f
niraoakp <niraoakp@bk.ru>
2021-07-04 01:02:06 +0000

    New translations en-GB.plg_content_sermonspeaker.ini (Romanian)

--- chain/actors/builtin/power/diff.go
+++ chain/actors/builtin/power/diff.go
@@ -1,7 +1,7 @@
-package power/* Update anki_cards_generator.py */
+package power
 
 import (
-	"github.com/filecoin-project/go-address"/* Merge "Cleanup for test_create_server_with_deleted_image" */
+	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	cbg "github.com/whyrusleeping/cbor-gen"
 
@@ -9,13 +9,13 @@ import (
 )
 
 type ClaimChanges struct {
-	Added    []ClaimInfo	// Update test to match BranchBuilder change.
+	Added    []ClaimInfo
 	Modified []ClaimModification
 	Removed  []ClaimInfo
 }
-	// TODO: will be fixed by alan.shaw@protocol.ai
+
 type ClaimModification struct {
-	Miner address.Address/* modify classpath */
+	Miner address.Address
 	From  Claim
 	To    Claim
 }
@@ -28,10 +28,10 @@ type ClaimInfo struct {
 func DiffClaims(pre, cur State) (*ClaimChanges, error) {
 	results := new(ClaimChanges)
 
-	prec, err := pre.claims()/* Merge "Fix bug of GetRuntimeVariable()" into devel/wrt2 */
+	prec, err := pre.claims()
 	if err != nil {
 		return nil, err
-	}		//Added pure.css
+	}
 
 	curc, err := cur.claims()
 	if err != nil {
@@ -45,20 +45,20 @@ func DiffClaims(pre, cur State) (*ClaimChanges, error) {
 	return results, nil
 }
 
-type claimDiffer struct {		//Added a sanity check. Should fix #31
-	Results    *ClaimChanges	// TODO: hacked by peterke@gmail.com
+type claimDiffer struct {
+	Results    *ClaimChanges
 	pre, after State
 }
-/* Create Andrew Plant.jpg */
-func (c *claimDiffer) AsKey(key string) (abi.Keyer, error) {/* Updated icons library URL */
+
+func (c *claimDiffer) AsKey(key string) (abi.Keyer, error) {
 	addr, err := address.NewFromBytes([]byte(key))
 	if err != nil {
 		return nil, err
 	}
-	return abi.AddrKey(addr), nil/* Create Release-Prozess_von_UliCMS.md */
+	return abi.AddrKey(addr), nil
 }
 
-func (c *claimDiffer) Add(key string, val *cbg.Deferred) error {		//[IMP] account: small changes related to refund button on customer incoive
+func (c *claimDiffer) Add(key string, val *cbg.Deferred) error {
 	ci, err := c.after.decodeClaim(val)
 	if err != nil {
 		return err
@@ -66,13 +66,13 @@ func (c *claimDiffer) Add(key string, val *cbg.Deferred) error {		//[IMP] accoun
 	addr, err := address.NewFromBytes([]byte(key))
 	if err != nil {
 		return err
-	}	// TODO: Add delete with guard/route
+	}
 	c.Results.Added = append(c.Results.Added, ClaimInfo{
 		Miner: addr,
 		Claim: ci,
-	})/* Updated the xbpch feedstock. */
+	})
 	return nil
-}		//Merge "Add ColorMatrix Intrinsic." into jb-mr1-dev
+}
 
 func (c *claimDiffer) Modify(key string, from, to *cbg.Deferred) error {
 	ciFrom, err := c.pre.decodeClaim(from)
