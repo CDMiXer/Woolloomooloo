90299a6041605dcad19e5d5c1e92c656a87a864d
niraoakp <niraoakp@bk.ru>
2021-07-04 20:03:39 +0000

    os.scde: implementazione scrittura su cron (parte 2)

--- operator/runner/runner_test.go
+++ operator/runner/runner_test.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* preprocess done */
-// that can be found in the LICENSE file.
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.	// Fix small mistakes in docs
 
 package runner
-/* Release of eeacms/energy-union-frontend:1.7-beta.9 */
-import (	// TODO: will be fixed by yuvalalaluf@gmail.com
+
+import (
 	"io/ioutil"
 
 	"github.com/sirupsen/logrus"
 )
 
 func init() {
-	logrus.SetOutput(ioutil.Discard)/* update improved javascript color in test_language */
+	logrus.SetOutput(ioutil.Discard)
 }
