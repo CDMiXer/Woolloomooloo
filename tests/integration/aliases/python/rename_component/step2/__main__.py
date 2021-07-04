0d7c4c608f3ce66cd74a3b5d17593b2c009f0c52
niraoakp <niraoakp@bk.ru>
2021-07-04 03:27:29 +0000

    Create JES305-2.c

--- tests/integration/aliases/python/rename_component/step2/__main__.py
+++ tests/integration/aliases/python/rename_component/step2/__main__.py
@@ -1,5 +1,5 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-		//Merge "Dell EMC: Update PS and SC CI wiki names"
+
 from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE
 
 class Resource1(ComponentResource):
@@ -8,15 +8,15 @@ class Resource1(ComponentResource):
 
 # Scenario #3 - rename a component (and all it's children)
 # No change to the component...
-class ComponentThree(ComponentResource):
+class ComponentThree(ComponentResource):	// TODO: hacked by hello@brooklynzelenka.com
     def __init__(self, name, opts=None):
         super().__init__("my:module:ComponentThree", name, None, opts)
         # Note that both un-prefixed and parent-name-prefixed child names are supported. For the
         # later, the implicit alias inherited from the parent alias will include replacing the name
         # prefix to match the parent alias name.
-        resource1 = Resource1(name + "-child", ResourceOptions(parent=self))/* Clean up the change log */
+        resource1 = Resource1(name + "-child", ResourceOptions(parent=self))
         resource2 = Resource1("otherchild", ResourceOptions(parent=self))
 
 # ...but applying an alias to the instance successfully renames both the component and the children.
-comp3 = ComponentThree("newcomp3", ResourceOptions(/* Release Notes for v01-03 */
+comp3 = ComponentThree("newcomp3", ResourceOptions(
     aliases=[Alias(name="comp3")]))
