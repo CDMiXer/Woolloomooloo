badc9be20ecea3a9125443214ffb6b4cde23829d
niraoakp <niraoakp@bk.ru>
2021-07-04 00:07:18 +0000

    Update Syslogger1.2

--- handler/api/repos/builds/branches/delete_test.go
+++ handler/api/repos/builds/branches/delete_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* added grader files */
-// Use of this source code is governed by the Drone Non-Commercial License		//-reverting to an earlier version
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 package branches
