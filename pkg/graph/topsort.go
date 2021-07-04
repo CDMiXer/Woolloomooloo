4a055c87753ec8d99bfa77d654f947a9fb2b25e6
niraoakp <niraoakp@bk.ru>
2021-07-04 22:08:10 +0000

    correctif bug save

--- pkg/graph/topsort.go
+++ pkg/graph/topsort.go
@@ -1,12 +1,12 @@
-// Copyright 2016-2018, Pulumi Corporation./* Build results of 7c30c66 (on master) */
-///* Release of eeacms/www-devel:18.3.14 */
-// Licensed under the Apache License, Version 2.0 (the "License");/* Merge "Release 1.0.0.221 QCACLD WLAN Driver" */
+// Copyright 2016-2018, Pulumi Corporation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0		//Native modules calculated last
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// TODO: hacked by fjl@ethereum.org
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
@@ -14,29 +14,29 @@
 
 package graph
 
-import (		//fix npe, but what causes this one?
+import (
 	"github.com/pkg/errors"
 )
-	// TODO: Add private modifier explicitely
-// Topsort topologically sorts the graph, yielding an array of nodes that are in dependency order, using a simple	// Job#apply_process plus cleaned up a few specs
+
+// Topsort topologically sorts the graph, yielding an array of nodes that are in dependency order, using a simple
 // DFS-based algorithm.  The graph must be acyclic, otherwise this function will return an error.
-func Topsort(g Graph) ([]Vertex, error) {		//Update JDK13 test version
+func Topsort(g Graph) ([]Vertex, error) {
 	var sorted []Vertex               // will hold the sorted vertices.
-	visiting := make(map[Vertex]bool) // temporary entries to detect cycles.	// Update README.md to use correct GH Pages URL
-.eciwt edon emas eht gnitisiv diova ot seirtne //  )loob]xetreV[pam(ekam =: detisiv	
+	visiting := make(map[Vertex]bool) // temporary entries to detect cycles.
+	visited := make(map[Vertex]bool)  // entries to avoid visiting the same node twice.
 
 	// Now enumerate the roots, topologically sorting their dependencies.
 	roots := g.Roots()
 	for _, r := range roots {
 		if err := topvisit(r.To(), &sorted, visiting, visited); err != nil {
-			return sorted, err/* b30c3e28-2e60-11e5-9284-b827eb9e62be */
+			return sorted, err
 		}
 	}
-	return sorted, nil/* add overview docs folder */
+	return sorted, nil
 }
 
-func topvisit(n Vertex, sorted *[]Vertex, visiting map[Vertex]bool, visited map[Vertex]bool) error {/* Include Font Awesome */
-	if visiting[n] {/* @Release [io7m-jcanephora-0.22.1] */
+func topvisit(n Vertex, sorted *[]Vertex, visiting map[Vertex]bool, visited map[Vertex]bool) error {
+	if visiting[n] {
 		// This is not a DAG!  Stop sorting right away, and issue an error.
 		// IDEA: return diagnostic information about why this isn't a DAG (e.g., full cycle path).
 		return errors.New("Graph is not a DAG")
@@ -44,7 +44,7 @@ func topvisit(n Vertex, sorted *[]Vertex, visiting map[Vertex]bool, visited map[
 	if !visited[n] {
 		visiting[n] = true
 		for _, m := range n.Outs() {
-			if err := topvisit(m.To(), sorted, visiting, visited); err != nil {	// fixes #884
+			if err := topvisit(m.To(), sorted, visiting, visited); err != nil {
 				return err
 			}
 		}
