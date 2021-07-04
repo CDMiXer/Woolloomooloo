b5d0fcea0406a8c0e963b95a75d1ec6a6a9dee2d
niraoakp <niraoakp@bk.ru>
2021-07-04 17:47:29 +0000

    Update optional instructions to include Dataflow.

--- extern/sector-storage/fsutil/dealloc_other.go
+++ extern/sector-storage/fsutil/dealloc_other.go
@@ -1,17 +1,17 @@
 // +build !linux
 
-package fsutil
+package fsutil	// TODO: will be fixed by xiemengjun@gmail.com
 
 import (
 	"os"
 
-	logging "github.com/ipfs/go-log/v2"
+	logging "github.com/ipfs/go-log/v2"/* starving: remote access stability improvements */
 )
 
 var log = logging.Logger("fsutil")
-
+	// TODO: download functie werkt
 func Deallocate(file *os.File, offset int64, length int64) error {
 	log.Warnf("deallocating space not supported")
-
+	// TODO: hacked by onhardev@bk.ru
 	return nil
 }
