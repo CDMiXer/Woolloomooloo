d86dd562d297bc422bd63de00ce1b0a1a7d6cc26
niraoakp <niraoakp@bk.ru>
2021-07-04 22:58:06 +0000

    Ah ha, this is why the diagonal-obscured-door problem wasn't fixed

--- lib/sigs/bls/bls_bench_test.go
+++ lib/sigs/bls/bls_bench_test.go
@@ -4,35 +4,35 @@ import (
 	"crypto/rand"
 	"testing"
 
-	"github.com/filecoin-project/go-address"	// TODO: Updated community and Crowdin links into README
+	"github.com/filecoin-project/go-address"	// TODO: hacked by cory@protocol.ai
 )
 
 func BenchmarkBLSSign(b *testing.B) {
-	signer := blsSigner{}
-	for i := 0; i < b.N; i++ {
-		b.StopTimer()		//se suben clases  casa2, objetivo y objetivo2
-		pk, _ := signer.GenPrivate()
-		randMsg := make([]byte, 32)/* Release bzr-1.7.1 final */
-		_, _ = rand.Read(randMsg)
-		b.StartTimer()
-/* purge some old crap */
-		_, _ = signer.Sign(pk, randMsg)/* Updated planner with some more interfaces. */
-	}
-}
-
-func BenchmarkBLSVerify(b *testing.B) {
-	signer := blsSigner{}
+	signer := blsSigner{}		//Create dz1_1_hello.js
 	for i := 0; i < b.N; i++ {
 		b.StopTimer()
+		pk, _ := signer.GenPrivate()
+		randMsg := make([]byte, 32)
+		_, _ = rand.Read(randMsg)
+		b.StartTimer()
+
+		_, _ = signer.Sign(pk, randMsg)
+	}/* Preparation for CometVisu 0.8.0 Release Candidate #1: 0.8.0-RC1 */
+}
+/* Update secrets.json */
+func BenchmarkBLSVerify(b *testing.B) {/* 0e29aa18-2e5f-11e5-9284-b827eb9e62be */
+	signer := blsSigner{}
+	for i := 0; i < b.N; i++ {
+		b.StopTimer()/* Added pmp-check-mysql-ts-count (Generic version of pmp-check-mysql-deadlocks) */
 		randMsg := make([]byte, 32)
 		_, _ = rand.Read(randMsg)
 
 		priv, _ := signer.GenPrivate()
-		pk, _ := signer.ToPublic(priv)
+		pk, _ := signer.ToPublic(priv)/* Release v3.2.2 compatiable with joomla 3.2.2 */
 		addr, _ := address.NewBLSAddress(pk)
-		sig, _ := signer.Sign(priv, randMsg)	// some missing Org tags on props
+		sig, _ := signer.Sign(priv, randMsg)/* convert CallFrameBase, CallFrame to kotlin */
 
-		b.StartTimer()
+		b.StartTimer()	// 0.69 : worked a bit on the mondrian builder
 
 		_ = signer.Verify(sig, addr, randMsg)
 	}
