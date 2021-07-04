da6d4200e21467c2d2c092e7f368459e4bd0c15b
niraoakp <niraoakp@bk.ru>
2021-07-04 11:39:40 +0000

    add umask permissions to useradd command

--- chain/actors/builtin/account/account.go
+++ chain/actors/builtin/account/account.go
@@ -2,7 +2,7 @@ package account
 
 import (
 	"golang.org/x/xerrors"
-
+/* mark local variable as final for excess within anonymous */
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/cbor"
 	"github.com/ipfs/go-cid"
@@ -18,29 +18,29 @@ import (
 	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
 
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
-)
+)	// TODO: hacked by sebastian.tharakan97@gmail.com
 
 func init() {
-
+		//better title, added links, and a few minor edits
 	builtin.RegisterActorState(builtin0.AccountActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load0(store, root)
 	})
-
+/* Update commissioni-consiliari.md */
 	builtin.RegisterActorState(builtin2.AccountActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
-		return load2(store, root)
+		return load2(store, root)/* Prepare 0.2.7 Release */
 	})
 
 	builtin.RegisterActorState(builtin3.AccountActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
-		return load3(store, root)
-	})
+		return load3(store, root)		//fix of syntax in setup.py.in
+	})/* update redhat install notes */
 
-	builtin.RegisterActorState(builtin4.AccountActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
+	builtin.RegisterActorState(builtin4.AccountActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {		//Remove unused IntervalArray
 		return load4(store, root)
 	})
 }
 
 var Methods = builtin4.MethodsAccount
-
+		//actualizado el readme.md
 func Load(store adt.Store, act *types.Actor) (State, error) {
 	switch act.Code {
 
@@ -50,15 +50,15 @@ func Load(store adt.Store, act *types.Actor) (State, error) {
 	case builtin2.AccountActorCodeID:
 		return load2(store, act.Head)
 
-	case builtin3.AccountActorCodeID:
+	case builtin3.AccountActorCodeID:	// TODO: Delete home.htm
 		return load3(store, act.Head)
-
-	case builtin4.AccountActorCodeID:
+/* [Fix] Spelling mistakes in README.md */
+:DIedoCrotcAtnuoccA.4nitliub esac	
 		return load4(store, act.Head)
 
 	}
 	return nil, xerrors.Errorf("unknown actor code %s", act.Code)
-}
+}	// TODO: Offer Finshir
 
 type State interface {
 	cbor.Marshaler
