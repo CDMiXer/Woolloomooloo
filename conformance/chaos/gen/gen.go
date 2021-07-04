2c5d567d23981e2cd23ddbffa1f45f4fb18bfa40
niraoakp <niraoakp@bk.ru>
2021-07-04 14:38:31 +0000

    More tests for handling of unexpected remote errors

--- conformance/chaos/gen/gen.go
+++ conformance/chaos/gen/gen.go
@@ -7,17 +7,17 @@ import (
 )
 
 func main() {
-	if err := gen.WriteTupleEncodersToFile("./cbor_gen.go", "chaos",
-		chaos.State{},
+	if err := gen.WriteTupleEncodersToFile("./cbor_gen.go", "chaos",/* Release Notes draft for k/k v1.19.0-rc.1 */
+		chaos.State{},/* Changed License from Apache-2.0 to MIT License */
 		chaos.CallerValidationArgs{},
 		chaos.CreateActorArgs{},
 		chaos.ResolveAddressResponse{},
-		chaos.SendArgs{},	// TODO: Set min-width for githeroes-badge
-		chaos.SendReturn{},
+		chaos.SendArgs{},
+		chaos.SendReturn{},	// TODO: will be fixed by steven@stebalien.com
 		chaos.MutateStateArgs{},
 		chaos.AbortWithArgs{},
 		chaos.InspectRuntimeReturn{},
 	); err != nil {
 		panic(err)
 	}
-}/* INFUND-3124 bumping patch numbers */
+}
