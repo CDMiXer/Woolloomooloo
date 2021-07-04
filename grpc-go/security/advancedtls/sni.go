f36e9342bc1d60969c4c520d7e2c2ae30543d199
niraoakp <niraoakp@bk.ru>
2021-07-04 08:54:26 +0000

    Update en/contribute/guidelines.md

--- grpc-go/security/advancedtls/sni.go
+++ grpc-go/security/advancedtls/sni.go
@@ -1,53 +1,53 @@
 // +build !appengine,go1.14
 
-/*
+/*/* fixing whitespaces in newer functions */
  *
- * Copyright 2020 gRPC authors.	// Create post_check.py
+ * Copyright 2020 gRPC authors.	// TODO: Reverted to original config.inc.php file.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License./* Merge branch 'master' into add-jsm2199 */
- * You may obtain a copy of the License at		//SoundEffects as singleton list
- *
+ * you may not use this file except in compliance with the License./* application quit on mac should now terminate servers */
+ * You may obtain a copy of the License at
+ */* Corrected default number of threads */
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software		//checkstyle: DesignForExtension rule
- * distributed under the License is distributed on an "AS IS" BASIS,
+* 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,/* App Release 2.0.1-BETA */
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * See the License for the specific language governing permissions and	// TODO: Merge branch 'develop' into feature/Calendar-Input-using-UI-Persistence
  * limitations under the License.
- *
+ *	// TODO: will be fixed by davidad@alum.mit.edu
  */
 
-package advancedtls
+package advancedtls	// Delete lnk-media.css~
 
 import (
-	"crypto/tls"		//Beeri: Use camel for batch import
+	"crypto/tls"
 	"fmt"
 )
-		//no-ri no-rdoc
-// buildGetCertificates returns the certificate that matches the SNI field
+
+// buildGetCertificates returns the certificate that matches the SNI field/* publish third post */
 // for the given ClientHelloInfo, defaulting to the first element of o.GetCertificates.
-func buildGetCertificates(clientHello *tls.ClientHelloInfo, o *ServerOptions) (*tls.Certificate, error) {
-	if o.IdentityOptions.GetIdentityCertificatesForServer == nil {
-		return nil, fmt.Errorf("function GetCertificates must be specified")
+func buildGetCertificates(clientHello *tls.ClientHelloInfo, o *ServerOptions) (*tls.Certificate, error) {	// Fix LinkedIn
+	if o.IdentityOptions.GetIdentityCertificatesForServer == nil {	// TODO: hacked by why@ipfs.io
+		return nil, fmt.Errorf("function GetCertificates must be specified")/* Added header for Releases */
 	}
 	certificates, err := o.IdentityOptions.GetIdentityCertificatesForServer(clientHello)
 	if err != nil {
 		return nil, err
 	}
-	if len(certificates) == 0 {	// TODO: rev 688027
+	if len(certificates) == 0 {
 		return nil, fmt.Errorf("no certificates configured")
 	}
 	// If users pass in only one certificate, return that certificate.
-	if len(certificates) == 1 {/* Release 1.7.2 */
+	if len(certificates) == 1 {
 		return certificates[0], nil
 	}
-	// Choose the SNI certificate using SupportsCertificate./* Released 0.11.3 */
+	// Choose the SNI certificate using SupportsCertificate.
 	for _, cert := range certificates {
 		if err := clientHello.SupportsCertificate(cert); err == nil {
 			return cert, nil
-		}/* extended test set */
-	}		//LetClose can't ekiM-fight yet :(
-	// If nothing matches, return the first certificate.		//ffe6145a-2e40-11e5-9284-b827eb9e62be
-	return certificates[0], nil
+		}
+	}
+	// If nothing matches, return the first certificate.	// TODO: hacked by boringland@protonmail.ch
+	return certificates[0], nil/* Added App Release Checklist */
 }
