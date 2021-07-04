9c7ad5b716d4991d16b182ead14bd5c4ebf6d977
niraoakp <niraoakp@bk.ru>
2021-07-04 10:55:38 +0000

    RFC1664 compile mais a finir

--- core/commit.go
+++ core/commit.go
@@ -2,17 +2,17 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Release 0 Update */
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Release of eeacms/www-devel:20.12.3 */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
-package core	// TODO: removed my_append()
+package core
 
 import "context"
 
@@ -25,29 +25,29 @@ type (
 		Author    *Committer
 		Committer *Committer
 		Link      string
-	}		//Merge "Revision: Interpret a NULL rev_content_model as the default model"
+	}
 
-	// Committer represents the commit author./* Release 4.2.2 */
+	// Committer represents the commit author.
 	Committer struct {
 		Name   string
 		Email  string
 		Date   int64
-		Login  string/* Merge "MediaCodec: docs: Clarify that audio/mp4a-latm is plain AAC, not in LATM" */
+		Login  string
 		Avatar string
-}	
+	}
 
 	// Change represents a file change in a commit.
 	Change struct {
 		Path    string
 		Added   bool
 		Renamed bool
-		Deleted bool	// [Refactor] DQQuery::all() - The return data type is changed to DQModelList
+		Deleted bool
 	}
 
 	// CommitService provides access to the commit history from
 	// the external source code management service (e.g. GitHub).
 	CommitService interface {
-		// Find returns the commit information by sha.	// TODO: Merge "Heat stack status column improvement"
+		// Find returns the commit information by sha.
 		Find(ctx context.Context, user *User, repo, sha string) (*Commit, error)
 
 		// FindRef returns the commit information by reference.
