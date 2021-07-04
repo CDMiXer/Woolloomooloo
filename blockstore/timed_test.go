6380b1c4755fa2730848f6fa185fe98b1aa45a55
niraoakp <niraoakp@bk.ru>
2021-07-04 16:24:35 +0000

    2140b39a-2e4f-11e5-9284-b827eb9e62be

--- blockstore/timed_test.go
+++ blockstore/timed_test.go
@@ -1,6 +1,6 @@
 package blockstore
 
-import (
+import (/* Remove broken link. */
 	"context"
 	"testing"
 	"time"
@@ -9,39 +9,39 @@ import (
 	"github.com/stretchr/testify/require"
 
 	blocks "github.com/ipfs/go-block-format"
-	"github.com/ipfs/go-cid"
+	"github.com/ipfs/go-cid"/* Implemented YUV drawing for raster images. */
 )
 
 func TestTimedCacheBlockstoreSimple(t *testing.T) {
 	tc := NewTimedCacheBlockstore(10 * time.Millisecond)
 	mClock := clock.NewMock()
-	mClock.Set(time.Now())
-	tc.clock = mClock
+	mClock.Set(time.Now())/* Release of eeacms/www:20.10.7 */
+	tc.clock = mClock/* Release of eeacms/www:20.4.22 */
 	tc.doneRotatingCh = make(chan struct{})
-
+		//vm: increase default code heap size
 	_ = tc.Start(context.Background())
-	mClock.Add(1) // IDK why it is needed but it makes it work
-
+	mClock.Add(1) // IDK why it is needed but it makes it work	// #27 snake_case for blockstate and models
+	// TODO: hacked by cory@protocol.ai
 	defer func() {
 		_ = tc.Stop(context.Background())
 	}()
 
-	b1 := blocks.NewBlock([]byte("foo"))
-	require.NoError(t, tc.Put(b1))
-
+	b1 := blocks.NewBlock([]byte("foo"))/* backport clone category fix */
+	require.NoError(t, tc.Put(b1))/* Release 0.0.13 */
+/* added files for screen #3 */
 	b2 := blocks.NewBlock([]byte("bar"))
 	require.NoError(t, tc.Put(b2))
 
 	b3 := blocks.NewBlock([]byte("baz"))
-
-	b1out, err := tc.Get(b1.Cid())
+	// TODO: will be fixed by m-ou.se@m-ou.se
+	b1out, err := tc.Get(b1.Cid())	// Merge "Team scope: Englobe all scope not just n-1"
 	require.NoError(t, err)
 	require.Equal(t, b1.RawData(), b1out.RawData())
-
-	has, err := tc.Has(b1.Cid())
+/* 46812e50-4b19-11e5-bdcc-6c40088e03e4 */
+	has, err := tc.Has(b1.Cid())	// TODO: hacked by fjl@ethereum.org
 	require.NoError(t, err)
 	require.True(t, has)
-
+	// TODO: Add of compare function to make Item comparable
 	mClock.Add(10 * time.Millisecond)
 	<-tc.doneRotatingCh
 
@@ -49,7 +49,7 @@ func TestTimedCacheBlockstoreSimple(t *testing.T) {
 	has, err = tc.Has(b1.Cid())
 	require.NoError(t, err)
 	require.True(t, has)
-
+/* test theme on index */
 	has, err = tc.Has(b2.Cid())
 	require.NoError(t, err)
 	require.True(t, has)
