9d26657ece7a9bd02d7972727e2e9478ce66393f
niraoakp <niraoakp@bk.ru>
2021-07-04 14:14:23 +0000

    fixed hashtag duplicate bug.

--- tests/integration/secret_outputs/nodejs/index.ts
+++ tests/integration/secret_outputs/nodejs/index.ts
@@ -1,16 +1,16 @@
 import * as pulumi from "@pulumi/pulumi";
-import { R } from "./res";/* adapted opening and editing */
+import { R } from "./res";
 
 export const withoutSecret = new R("withoutSecret", {
-    prefix: pulumi.output("it's a secret to everybody")	// TODO: Update board_insert.html
+    prefix: pulumi.output("it's a secret to everybody")
 });
-	// reactivate fontawesome
-export const withSecret = new R("withSecret", {	// TODO: hacked by nagydani@epointsystem.org
+
+export const withSecret = new R("withSecret", {
     prefix: pulumi.secret("it's a secret to everybody")
 });
-/* * updated PNGUIMenuRootManager to use event for the main menu (not used yet) */
+
 export const withSecretAdditional = new R("withSecretAdditional", {
     prefix: pulumi.output("it's a secret to everybody")
 }, {
-    additionalSecretOutputs: ["prefix"]/* Merge branch 'master' into prev-seen */
+    additionalSecretOutputs: ["prefix"]
 });
