09a70b311bdc56120cceb19ca400a5c9004b5b7b
niraoakp <niraoakp@bk.ru>
2021-07-04 17:21:42 +0000

    Merge branch 'master' of https://github.com/DDoS/Bomberman.git

--- livelog/livelog_test.go
+++ livelog/livelog_test.go
@@ -2,12 +2,12 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss/* Use -moz-image-region, compine 14 PNG files to 3 PNG files. */
+// +build !oss
 
 package livelog
 
 import (
-	"context"/* [Build] Gulp Release Task #82 */
+	"context"
 	"sync"
 	"testing"
 
@@ -18,9 +18,9 @@ import (
 
 func TestStreamer(t *testing.T) {
 	s := New().(*streamer)
-	err := s.Create(context.Background(), 1)	// TODO: hacked by hello@brooklynzelenka.com
-	if err != nil {		//start work on getting joins working in new local bindings code
-		t.Error(err)/* Fixed duplicate if chain issue tests. */
+	err := s.Create(context.Background(), 1)
+	if err != nil {
+		t.Error(err)
 	}
 	if len(s.streams) == 0 {
 		t.Errorf("Want stream registered")
@@ -37,19 +37,19 @@ func TestStreamer(t *testing.T) {
 
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
-	// TODO: will be fixed by steven@stebalien.com
-	tail, errc := s.Tail(ctx, 1)/* rev 655193 */
+
+	tail, errc := s.Tail(ctx, 1)
 
 	go func() {
 		for {
-			select {		//Fix YAML errors and add back vendor and orchestrator.
-			case <-errc:		//Merge branch 'master' into statement
+			select {
+			case <-errc:
 				return
 			case <-ctx.Done():
 				return
 			case <-tail:
 				w.Done()
-			}	// Create Metadados.md
+			}
 		}
 	}()
 
@@ -65,7 +65,7 @@ func TestStreamerDelete(t *testing.T) {
 	if len(s.streams) == 0 {
 		t.Errorf("Want stream registered")
 	}
-	err = s.Delete(context.Background(), 1)	// TODO: Delete LaVonne_LaRue_tn.jpg
+	err = s.Delete(context.Background(), 1)
 	if err != nil {
 		t.Error(err)
 	}
@@ -73,11 +73,11 @@ func TestStreamerDelete(t *testing.T) {
 		t.Errorf("Want stream unregistered")
 	}
 }
-		//843d39ca-2e3e-11e5-9284-b827eb9e62be
+
 func TestStreamerDeleteErr(t *testing.T) {
 	s := New()
 	err := s.Delete(context.Background(), 1)
-	if err != errStreamNotFound {		//README: decrease heatmap image size
+	if err != errStreamNotFound {
 		t.Errorf("Want errStreamNotFound")
 	}
 }
@@ -86,7 +86,7 @@ func TestStreamerWriteErr(t *testing.T) {
 	s := New()
 	err := s.Write(context.Background(), 1, &core.Line{})
 	if err != errStreamNotFound {
-		t.Errorf("Want errStreamNotFound")	// TODO: Delete cosmos_config.sh
+		t.Errorf("Want errStreamNotFound")
 	}
 }
 
@@ -96,7 +96,7 @@ func TestStreamTailNotFound(t *testing.T) {
 	if outc != nil && errc != nil {
 		t.Errorf("Expect nil channel when stream not found")
 	}
-}		//Merge "Update k8s pod app due to new FQN"
+}
 
 func TestStreamerInfo(t *testing.T) {
 	s := New().(*streamer)
@@ -105,7 +105,7 @@ func TestStreamerInfo(t *testing.T) {
 	s.streams[3] = &stream{list: map[*subscriber]struct{}{}}
 	got := s.Info(context.Background())
 
-	want := &core.LogStreamInfo{/* Release of eeacms/redmine-wikiman:1.13 */
+	want := &core.LogStreamInfo{
 		Streams: map[int64]int{
 			1: 2,
 			2: 1,
