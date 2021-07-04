060677e567128314ee24f6102b31cb2691b03115
niraoakp <niraoakp@bk.ru>
2021-07-04 13:57:18 +0000

    Edit isAuthorized() method and change setFlash elements.

--- tests/integration/aliases/python/rename/step1/__main__.py
+++ tests/integration/aliases/python/rename/step1/__main__.py
@@ -1,5 +1,5 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-/* code korrigiert */
+
 from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE
 
 class Resource1(ComponentResource):
