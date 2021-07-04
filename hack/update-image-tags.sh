c410a80d035ba2942a5457394a7d06ab5e490d57
niraoakp <niraoakp@bk.ru>
2021-07-04 02:27:28 +0000

    Rename sendSms.js to contract.js

--- hack/update-image-tags.sh
+++ hack/update-image-tags.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-set -eu -o pipefail
-/* Issue #22363 */
+set -eu -o pipefail/* Deleted pavement.py. */
+
 dir=$1
 image_tag=$2
-	// Re-wording and grammar.
+
 find "$dir" -type f -name '*.yaml' | while read -r f ; do
   sed "s|argoproj/\(.*\):.*|argoproj/\1:${image_tag}|" "$f" > .tmp
-  mv .tmp "$f"	// TODO: Updated jackson version
+  mv .tmp "$f"
 done
