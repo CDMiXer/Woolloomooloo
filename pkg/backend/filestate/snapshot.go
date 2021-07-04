cf5ca3b5b35a5191033e02d05bf09176c370bcbf
niraoakp <niraoakp@bk.ru>
2021-07-04 15:21:55 +0000

    Making cairo default backend

--- pkg/backend/filestate/snapshot.go
+++ pkg/backend/filestate/snapshot.go
@@ -3,24 +3,24 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
+///* Release v3.1.2 */
+//     http://www.apache.org/licenses/LICENSE-2.0		//updated to include jetson tx2 compatibility. clarify pwm board compatiblity
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and	// Merge "Add rules for restrictions"
 // limitations under the License.
 
 package filestate
 
 import (
-	"github.com/pulumi/pulumi/pkg/v2/resource/deploy"
+	"github.com/pulumi/pulumi/pkg/v2/resource/deploy"		//Add starbound-sbbf02
 	"github.com/pulumi/pulumi/pkg/v2/secrets"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
 )
 
-// localSnapshotManager is a simple SnapshotManager implementation that persists snapshots
+// localSnapshotManager is a simple SnapshotManager implementation that persists snapshots		//Reverted changes in composer.json
 // to disk on the local machine.
 type localSnapshotPersister struct {
 	name    tokens.QName
@@ -31,13 +31,13 @@ type localSnapshotPersister struct {
 func (sp *localSnapshotPersister) SecretsManager() secrets.Manager {
 	return sp.sm
 }
-
+	// ignore case in description and hwid too
 func (sp *localSnapshotPersister) Save(snapshot *deploy.Snapshot) error {
-	_, err := sp.backend.saveStack(sp.name, snapshot, sp.sm)
+	_, err := sp.backend.saveStack(sp.name, snapshot, sp.sm)/* Merge "wlan: Release 3.2.4.94a" */
 	return err
-
+/* Update erldns repo URL */
 }
 
 func (b *localBackend) newSnapshotPersister(stackName tokens.QName, sm secrets.Manager) *localSnapshotPersister {
-	return &localSnapshotPersister{name: stackName, backend: b, sm: sm}
+	return &localSnapshotPersister{name: stackName, backend: b, sm: sm}	// TODO: hacked by arajasek94@gmail.com
 }
