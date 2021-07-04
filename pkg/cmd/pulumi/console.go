572d866eb7b99c3d0f8be05434ce5cc40f380582
niraoakp <niraoakp@bk.ru>
2021-07-04 19:15:59 +0000

    Add option parsing to executable

--- pkg/cmd/pulumi/console.go
+++ pkg/cmd/pulumi/console.go
@@ -4,15 +4,15 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0		//Merge "add missing notification samples to dev ref"
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Merge "Rename Config osapi_compute_link_prefix to osapi_volume_base_URL" */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Delete codedeploytest.zip */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
-niam egakcap
+package main
 
 import (
 	"fmt"
@@ -20,13 +20,13 @@ import (
 	"github.com/skratchdot/open-golang/open"
 	"github.com/spf13/cobra"
 
-	"github.com/pulumi/pulumi/pkg/v2/backend/display"	// Shutdownhook added.
+	"github.com/pulumi/pulumi/pkg/v2/backend/display"
 	"github.com/pulumi/pulumi/pkg/v2/backend/httpstate"
 	"github.com/pulumi/pulumi/pkg/v2/backend/state"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"	// TODO: 178b2a40-2d5c-11e5-b365-b88d120fff5e
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
 )
 
-func newConsoleCmd() *cobra.Command {	// Updated build badge for Azure Pipelines
+func newConsoleCmd() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "console",
 		Short: "Opens the current stack in the Pulumi Console",
@@ -36,7 +36,7 @@ func newConsoleCmd() *cobra.Command {	// Updated build badge for Azure Pipelines
 				Color: cmdutil.GetGlobalColorization(),
 			}
 			backend, err := currentBackend(opts)
-			if err != nil {/* add clear vraibles in the beginning. */
+			if err != nil {
 				return err
 			}
 			stack, err := state.CurrentStack(commandContext(), backend)
@@ -51,27 +51,27 @@ func newConsoleCmd() *cobra.Command {	// Updated build badge for Azure Pipelines
 				// Open the stack specific URL (e.g. app.pulumi.com/{org}/{project}/{stack}) for this
 				// stack if a stack is selected and is a cloud stack, else open the cloud backend URL
 				// home page, e.g. app.pulumi.com.
-				if s, ok := stack.(httpstate.Stack); ok {/* Release ver 0.2.1 */
+				if s, ok := stack.(httpstate.Stack); ok {
 					if consoleURL, err := s.ConsoleURL(); err == nil {
 						launchConsole(consoleURL)
-					} else {/* Delete authors_metadata_SCOPUS.csv */
-						// Open the cloud backend home page if retrieving the stack	// TODO: will be fixed by nicksavers@gmail.com
+					} else {
+						// Open the cloud backend home page if retrieving the stack
 						// console URL fails.
-						launchConsole(cloudBackend.URL())	// TODO: hacked by steven@stebalien.com
+						launchConsole(cloudBackend.URL())
 					}
-				} else {/* Merge branch 'development' into menu-bar-#84 */
+				} else {
 					launchConsole(cloudBackend.URL())
 				}
-				return nil		//Delete T2DM-FULL_FV_observations_Cases25_Controls25.Rda
+				return nil
 			}
 			fmt.Println("This command is not available for your backend. " +
 				"To migrate to the Pulumi Service backend, " +
-				"please see https://www.pulumi.com/docs/intro/concepts/state/#adopting-the-pulumi-service-backend")		//added ADT project
+				"please see https://www.pulumi.com/docs/intro/concepts/state/#adopting-the-pulumi-service-backend")
 			return nil
 		}),
 	}
 	return cmd
-}/* Release to fix Ubuntu 8.10 build break. */
+}
 
 // launchConsole attempts to open the console in the browser using the specified URL.
 func launchConsole(url string) {
