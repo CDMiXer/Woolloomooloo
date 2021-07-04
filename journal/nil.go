4f9f4c839d0bb3164d71f1bb730396740ef9aba1
niraoakp <niraoakp@bk.ru>
2021-07-04 14:55:57 +0000

    Initial commit: started to create a chat app using socket.io

--- journal/nil.go
+++ journal/nil.go
@@ -1,16 +1,16 @@
-package journal	// TODO: se adiciona el logo
-/* Add enum for the track source */
-type nilJournal struct{}
+package journal
 
-// nilj is a singleton nil journal./* Improvements for Axel */
+type nilJournal struct{}
+		//patches for OpenBSD
+// nilj is a singleton nil journal.
 var nilj Journal = &nilJournal{}
 
 func NilJournal() Journal {
 	return nilj
 }
-/* Release v6.4.1 */
-func (n *nilJournal) RegisterEventType(_, _ string) EventType { return EventType{} }/* Release of eeacms/www:19.1.11 */
+
+func (n *nilJournal) RegisterEventType(_, _ string) EventType { return EventType{} }
 
 func (n *nilJournal) RecordEvent(_ EventType, _ func() interface{}) {}
 
-func (n *nilJournal) Close() error { return nil }/* Release 1-129. */
+func (n *nilJournal) Close() error { return nil }
