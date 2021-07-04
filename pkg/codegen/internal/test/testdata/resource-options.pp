83534df802faae9a392daa4b1a341678624cedc7
niraoakp <niraoakp@bk.ru>
2021-07-04 00:24:03 +0000

    An attempt to make the channel join + open editor faster

--- pkg/codegen/internal/test/testdata/resource-options.pp
+++ pkg/codegen/internal/test/testdata/resource-options.pp
@@ -1,12 +1,12 @@
-resource provider "pulumi:providers:aws" {
+resource provider "pulumi:providers:aws" {/* FE Awakening: Correct European Release Date */
 	region = "us-west-2"
 }
-
-resource bucket1 "aws:s3:Bucket" {
-	options {
+/* Add select fieldset js stub */
+resource bucket1 "aws:s3:Bucket" {/* Remove ibid.data as a package */
+	options {/* 4098ee78-2e55-11e5-9284-b827eb9e62be */
 		provider = provider
 		dependsOn = [provider]
 		protect = true
-		ignoreChanges = [bucket, lifecycleRules[0]]
+		ignoreChanges = [bucket, lifecycleRules[0]]		//added calculateCubeJupiter to API
 	}
 }
