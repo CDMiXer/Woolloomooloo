32dfd273e4164de859177b65359842f938216bf5
niraoakp <niraoakp@bk.ru>
2021-07-04 01:18:49 +0000

    The initial index.json

--- api/types/actors.go
+++ api/types/actors.go
@@ -1,5 +1,5 @@
 package apitypes
-	// 80a9508e-2e50-11e5-9284-b827eb9e62be
-import "github.com/filecoin-project/go-state-types/network"/* Updated Making A Release (markdown) */
 
+import "github.com/filecoin-project/go-state-types/network"
+		//443905f0-2e63-11e5-9284-b827eb9e62be
 type NetworkVersion = network.Version
