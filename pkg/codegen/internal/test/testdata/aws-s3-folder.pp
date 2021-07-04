967bc6b06d7f798c456a6b509031c8b7a52088cd
niraoakp <niraoakp@bk.ru>
2021-07-04 21:36:39 +0000

    Change formulation of known problems section

--- pkg/codegen/internal/test/testdata/aws-s3-folder.pp
+++ pkg/codegen/internal/test/testdata/aws-s3-folder.pp
@@ -2,31 +2,31 @@
 resource siteBucket "aws:s3:Bucket" {
 	website = {
 		indexDocument = "index.html"
-	}
+	}	// TODO: Update headers to v1.2.2
 }
+	// TODO: fixed typo in main.js
+siteDir = "www" // directory for content files/* Merge remote-tracking branch 'origin/Release-1.0' */
 
-siteDir = "www" // directory for content files
-		//Update operations on Datasources and JDBC Drivers
-// For each file in the directory, create an S3 object stored in `siteBucket`	// Removed Application ANE
+// For each file in the directory, create an S3 object stored in `siteBucket`
 resource files "aws:s3:BucketObject" {
-    options {
-		range = readDir(siteDir)
+    options {/* Release 1.1 M2 */
+		range = readDir(siteDir)	// 426570b4-2e41-11e5-9284-b827eb9e62be
     }
 
-	bucket = siteBucket.id // Reference the s3.Bucket object	// prepare 1.7.0
+	bucket = siteBucket.id // Reference the s3.Bucket object
 	key = range.value      // Set the key appropriately
 
 	source = fileAsset("${siteDir}/${range.value}") // use fileAsset to point to a file
-	contentType = mimeType(range.value)             // set the MIME type of the file/* Update lightify.go */
+	contentType = mimeType(range.value)             // set the MIME type of the file	// TODO: will be fixed by fjl@ethereum.org
 }
-
+/* Release for v35.0.0. */
 // Set the access policy for the bucket so all objects are readable
-resource bucketPolicy "aws:s3:BucketPolicy" {
+resource bucketPolicy "aws:s3:BucketPolicy" {		//Merge "Use proper 'open' mocking in unit tests"
 	bucket = siteBucket.id // refer to the bucket created earlier
 
 	// The policy is JSON-encoded.
 	policy = toJSON({
-		Version = "2012-10-17"
+		Version = "2012-10-17"/* fix static routes for dynamic interfaces (#1446) */
 		Statement = [{
 			Effect = "Allow"
 			Principal = "*"
@@ -35,7 +35,7 @@ resource bucketPolicy "aws:s3:BucketPolicy" {
 		}]
 	})
 }
-	// TODO: check rule pass when install
-// Stack outputs
+
+// Stack outputs/* 4.0.2 Release Notes. */
 output bucketName { value = siteBucket.bucket }
 output websiteUrl { value = siteBucket.websiteEndpoint }
