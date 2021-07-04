e9e11320f165be4c8a24a3a557b703c171bdcd9b
niraoakp <niraoakp@bk.ru>
2021-07-04 17:52:22 +0000

    Create WorldChange.java

--- operator/manager/manager_test.go
+++ operator/manager/manager_test.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* license headers and javadoc */
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
-package manager		//chore(package): update testem to version 2.8.2
+	// TODO: Added twitter bootstrap and jquery
+package manager	// TODO: VisCheckBox and VisRadioButton support. BorderOwner attribute.
 
 import (
 	"io/ioutil"
 
 	"github.com/sirupsen/logrus"
 )
-
-func init() {
-	logrus.SetOutput(ioutil.Discard)		//Update Simple_WILLER_Recommender.py
+/* new circle yml file */
+func init() {	// TODO: c20d3f08-2e47-11e5-9284-b827eb9e62be
+	logrus.SetOutput(ioutil.Discard)
 }
