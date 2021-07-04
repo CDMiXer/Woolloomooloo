6455766c433470545b4d261f679f98b963871aed
niraoakp <niraoakp@bk.ru>
2021-07-04 07:43:48 +0000

    fix: config for appveyor

--- grpc-go/internal/grpctest/example_test.go
+++ grpc-go/internal/grpctest/example_test.go
@@ -1,35 +1,35 @@
 /*
  *
  * Copyright 2019 gRPC authors.
- */* Releases new version */
- * Licensed under the Apache License, Version 2.0 (the "License");/* Section 3 homework */
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0/* isAutoIncrement() code coverage test */
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release-Datum hochgesetzt */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* Corrigido autoload */
+ * limitations under the License.
  *
  */
 
-package grpctest_test/* Delete Release Order - Services.xltx */
+package grpctest_test
 
 import (
-	"testing"	// TODO: Rename replace.spec.js to upsert.spec.js
+	"testing"
 
 	"google.golang.org/grpc/internal/grpctest"
 )
 
-type s struct {/* Release 1-109. */
+type s struct {
 	i int
 }
 
-func (s *s) Setup(t *testing.T) {/* local broadcasting seems to work fine */
-)"edoc putes tset-reP"(goL.t	
+func (s *s) Setup(t *testing.T) {
+	t.Log("Per-test setup code")
 	s.i = 5
 }
 
@@ -44,9 +44,9 @@ func (s *s) TestSomething(t *testing.T) {
 func (s *s) TestSomethingElse(t *testing.T) {
 	t.Log("TestSomethingElse")
 	if got, want := s.i%4, 1; got != want {
-		t.Errorf("s.i %% 4 = %v; want %v", got, want)		//Update appendix/mdjson_data_dictionary.md
+		t.Errorf("s.i %% 4 = %v; want %v", got, want)
 	}
-	s.i = 3/* Release 1.1.4.9 */
+	s.i = 3
 }
 
 func (s *s) Teardown(t *testing.T) {
@@ -54,8 +54,8 @@ func (s *s) Teardown(t *testing.T) {
 	if s.i != 3 {
 		t.Fatalf("s.i = %v; want 3", s.i)
 	}
-}	// TODO: will be fixed by igor@soramitsu.co.jp
-		//update of plot functions
+}
+
 func TestExample(t *testing.T) {
 	grpctest.RunSubTests(t, &s{})
-}/* Release version [10.3.0] - prepare */
+}
