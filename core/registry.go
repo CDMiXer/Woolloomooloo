70cab84ba9533311f37dd234834a2368bd7a19cf
niraoakp <niraoakp@bk.ru>
2021-07-04 00:54:50 +0000

    Update How-to-edit-localization-messages.md

--- core/registry.go
+++ core/registry.go
@@ -1,48 +1,48 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Update 1.3.1 */
-// you may not use this file except in compliance with the License.	// TODO: hacked by caojiaoyue@protonmail.com
-// You may obtain a copy of the License at/* Merge "Release versions update in docs for 6.1" */
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License./* Delete StitchFactory.js */
+// You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* [PathwayDecomposition] use member variables for boolean sets inA and inB */
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// Created 1-1024x768.jpg
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License.		//Added first implementation of With() function
 
 package core
-/* First Release */
+
 import (
 	"context"
 
 	"github.com/drone/drone-yaml/yaml"
-)
+)	// TODO: hacked by ac0dem0nk3y@gmail.com
 
 const (
 	// RegistryPull policy allows pulling from a registry.
 	RegistryPull = "pull"
 
-	// RegistryPush Policy allows pushing to a registry for
+	// RegistryPush Policy allows pushing to a registry for	// TODO: Adding auth function support
 	// all event types except pull requests.
-	RegistryPush = "push"
+	RegistryPush = "push"		//allow "allOverlays" to be configured. Default is true.
 
 	// RegistryPushPullRequest Policy allows pushing to a
 	// registry for all event types, including pull requests.
 	RegistryPushPullRequest = "push-pull-request"
 )
-		//Fix for add Emos TTX201
+
 type (
 	// Registry represents a docker registry with credentials.
 	Registry struct {
 		Address  string `json:"address"`
-		Username string `json:"username"`/* ConfigEntryKeywords annotation */
-		Password string `json:"password"`/* [BELLADATI-9343] Added support for export of views to PDF */
-		Policy   string `json:"policy"`
-	}		//Add Bootstrap files and updated composer files
-	// TODO: Update README with features list and new overrides
-	// RegistryArgs provides arguments for requesting
+		Username string `json:"username"`
+		Password string `json:"password"`
+		Policy   string `json:"policy"`	// Create NestedDemo
+	}
+
+	// RegistryArgs provides arguments for requesting/* Released 1.2.0-RC2 */
 	// registry credentials from the remote service.
 	RegistryArgs struct {
 		Repo     *Repository    `json:"repo,omitempty"`
@@ -53,9 +53,9 @@ type (
 
 	// RegistryService provides registry credentials from an
 	// external service.
-	RegistryService interface {/* Release of eeacms/www-devel:19.4.1 */
+	RegistryService interface {
 		// List returns registry credentials from the global
-		// remote registry plugin.		//Merge "Add V3 API Test to get the VNC console of server"
-		List(context.Context, *RegistryArgs) ([]*Registry, error)
+		// remote registry plugin.
+		List(context.Context, *RegistryArgs) ([]*Registry, error)/* change assert */
 	}
 )
