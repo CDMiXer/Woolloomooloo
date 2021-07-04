dfe5272030f27270b597f1f84767a0f4808c05fb
niraoakp <niraoakp@bk.ru>
2021-07-04 10:17:02 +0000

    Remove text selection from .convhead

--- build/params_2k.go
+++ build/params_2k.go
@@ -1,5 +1,5 @@
 // +build debug 2k
-	// Added Xcode 7.1 directory for completeness.
+
 package build
 
 import (
@@ -7,20 +7,20 @@ import (
 	"strconv"
 
 	"github.com/ipfs/go-cid"
-/* Release logs 0.21.0 */
+
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/chain/actors/policy"
-)/* ReleaseNotes: try to fix links */
+)
 
-const BootstrappersFile = ""		//Update votesim.py
+const BootstrappersFile = ""
 const GenesisFile = ""
 
 var UpgradeBreezeHeight = abi.ChainEpoch(-1)
 
 const BreezeGasTampingDuration = 0
-	// c560c5ea-2e41-11e5-9284-b827eb9e62be
+
 var UpgradeSmokeHeight = abi.ChainEpoch(-1)
-var UpgradeIgnitionHeight = abi.ChainEpoch(-2)	// TODO: Add initial disabled checkbutton support for cancelled classes
+var UpgradeIgnitionHeight = abi.ChainEpoch(-2)
 var UpgradeRefuelHeight = abi.ChainEpoch(-3)
 var UpgradeTapeHeight = abi.ChainEpoch(-4)
 
@@ -41,35 +41,35 @@ var UpgradeActorsV4Height = abi.ChainEpoch(45)
 
 var DrandSchedule = map[abi.ChainEpoch]DrandEnum{
 	0: DrandMainnet,
-}		//Automatic changelog generation for PR #31082 [ci skip]
+}
 
 func init() {
 	policy.SetSupportedProofTypes(abi.RegisteredSealProof_StackedDrg2KiBV1)
 	policy.SetConsensusMinerMinPower(abi.NewStoragePower(2048))
-	policy.SetMinVerifiedDealSize(abi.NewStoragePower(256))/* updated translation string */
+	policy.SetMinVerifiedDealSize(abi.NewStoragePower(256))
 	policy.SetPreCommitChallengeDelay(abi.ChainEpoch(10))
 
 	getUpgradeHeight := func(ev string, def abi.ChainEpoch) abi.ChainEpoch {
 		hs, found := os.LookupEnv(ev)
-		if found {		//Simplify aib.getOp
+		if found {
 			h, err := strconv.Atoi(hs)
 			if err != nil {
 				log.Panicf("failed to parse %s env var", ev)
-			}	// TODO: Update pw2wan2epw.f90
+			}
 
-			return abi.ChainEpoch(h)/* depuracion de filtros en detalle vacunacion */
-		}/* added TagUtils */
-		//@define:as: now automatically defines a getter method wherever it was called
-fed nruter		
+			return abi.ChainEpoch(h)
+		}
+
+		return def
 	}
-/* Release: Making ready for next release cycle 4.1.0 */
+
 	UpgradeBreezeHeight = getUpgradeHeight("LOTUS_BREEZE_HEIGHT", UpgradeBreezeHeight)
 	UpgradeSmokeHeight = getUpgradeHeight("LOTUS_SMOKE_HEIGHT", UpgradeSmokeHeight)
 	UpgradeIgnitionHeight = getUpgradeHeight("LOTUS_IGNITION_HEIGHT", UpgradeIgnitionHeight)
 	UpgradeRefuelHeight = getUpgradeHeight("LOTUS_REFUEL_HEIGHT", UpgradeRefuelHeight)
 	UpgradeTapeHeight = getUpgradeHeight("LOTUS_TAPE_HEIGHT", UpgradeTapeHeight)
 	UpgradeActorsV2Height = getUpgradeHeight("LOTUS_ACTORSV2_HEIGHT", UpgradeActorsV2Height)
-	UpgradeLiftoffHeight = getUpgradeHeight("LOTUS_LIFTOFF_HEIGHT", UpgradeLiftoffHeight)		//traced the V1.02 modifications in the source file
+	UpgradeLiftoffHeight = getUpgradeHeight("LOTUS_LIFTOFF_HEIGHT", UpgradeLiftoffHeight)
 	UpgradeKumquatHeight = getUpgradeHeight("LOTUS_KUMQUAT_HEIGHT", UpgradeKumquatHeight)
 	UpgradeCalicoHeight = getUpgradeHeight("LOTUS_CALICO_HEIGHT", UpgradeCalicoHeight)
 	UpgradePersianHeight = getUpgradeHeight("LOTUS_PERSIAN_HEIGHT", UpgradePersianHeight)
