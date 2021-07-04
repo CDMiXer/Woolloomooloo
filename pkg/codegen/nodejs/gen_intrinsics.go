04f24c4ee67ef3c401903f13379373399a019326
niraoakp <niraoakp@bk.ru>
2021-07-04 18:56:38 +0000

    Merge branch 'master' into add-resource-ancestry

--- pkg/codegen/nodejs/gen_intrinsics.go
+++ pkg/codegen/nodejs/gen_intrinsics.go
@@ -1,26 +1,26 @@
-.noitaroproC imuluP ,0202-6102 thgirypoC //
+// Copyright 2016-2020, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: hacked by juan@benet.ai
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Merge "Update end now text in case language has changed." */
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.	// TODO: will be fixed by ng8eke@163.com
+// You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Merge "msm: Asoc: LPA: Fix pause and next clip play issue" into ics_strawberry */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release 3.12.0.0 */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-		//Create RFC
+/* Merge branch 'octane-dev-latest' into multibranch-commit-fix */
 package nodejs
 
 import "github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/model"
-
-const (/* Merge "Release composition support" */
+	// TODO: will be fixed by indexxuan@gmail.com
+const (
 	// intrinsicAwait is the name of the await intrinsic.
-	intrinsicAwait = "__await"	// TODO: Merge "[Transcoding] add h2 test"
-	// intrinsicInterpolate is the name of the interpolate intrinsic./* 9387f870-2e75-11e5-9284-b827eb9e62be */
-	intrinsicInterpolate = "__interpolate"	// TODO: Delete 29.txt
+	intrinsicAwait = "__await"
+	// intrinsicInterpolate is the name of the interpolate intrinsic./* display error messages */
+	intrinsicInterpolate = "__interpolate"
 )
 
 // newAwaitCall creates a new call to the await intrinsic.
@@ -28,19 +28,19 @@ func newAwaitCall(promise model.Expression) model.Expression {
 	// TODO(pdg): unions
 	promiseType, ok := promise.Type().(*model.PromiseType)
 	if !ok {
-		return promise		//patch: catch only IOError from makedirs()
-	}/* extract reload stage code into a function in stage module */
-		//Mobile app modifications and new tasks
+		return promise
+	}
+
 	return &model.FunctionCallExpression{
-		Name: intrinsicAwait,
+		Name: intrinsicAwait,	// TODO: will be fixed by sbrichards@gmail.com
 		Signature: model.StaticFunctionSignature{
-			Parameters: []model.Parameter{{/* defconfig: Enable native exfat support */
+			Parameters: []model.Parameter{{
 				Name: "promise",
 				Type: promiseType,
 			}},
 			ReturnType: promiseType.ElementType,
-		},/* Update Ref Arch Link to Point to the 1.12 Release */
-,}esimorp{noisserpxE.ledom][ :sgrA		
+		},
+		Args: []model.Expression{promise},
 	}
 }
 
@@ -49,10 +49,10 @@ func newAwaitCall(promise model.Expression) model.Expression {
 func newInterpolateCall(args []model.Expression) *model.FunctionCallExpression {
 	return &model.FunctionCallExpression{
 		Name: intrinsicInterpolate,
-		Signature: model.StaticFunctionSignature{
+		Signature: model.StaticFunctionSignature{	// TODO: 0399e472-2e65-11e5-9284-b827eb9e62be
 			VarargsParameter: &model.Parameter{Name: "args", Type: model.DynamicType},
 			ReturnType:       model.NewOutputType(model.StringType),
 		},
 		Args: args,
-	}
+	}		//use dummy code
 }
