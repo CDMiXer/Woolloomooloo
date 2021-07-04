03adc12caa899b9b3426c63171fb32f9fd4de4b1
niraoakp <niraoakp@bk.ru>
2021-07-04 05:45:38 +0000

    Hide timecheck-thread from webinterface status page.

--- service/content/content_test.go
+++ service/content/content_test.go
@@ -3,26 +3,26 @@
 // that can be found in the LICENSE file.
 
 package contents
-/* Release Notes for v02-15-03 */
-import (
-	"context"		//Update useful_API_methods.md
-	"testing"		//улучшен конфиг, сделана заглушка для ответа чужому
 
-	"github.com/drone/drone/core"/* Released CachedRecord v0.1.0 */
+import (
+	"context"/* Made Image destructor virtual. */
+	"testing"
+
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/mock"
-	"github.com/drone/drone/mock/mockscm"
+	"github.com/drone/drone/mock/mockscm"/* Release 3.0.0-alpha-1: update sitemap */
 	"github.com/drone/go-scm/scm"
-	"github.com/google/go-cmp/cmp"/* Build as relocatable PIE binary by default on x86. */
+	"github.com/google/go-cmp/cmp"/* Create Nodes.bas */
 
 	"github.com/golang/mock/gomock"
-)/* Merge "[INTERNAL] sap.m.TablePersoDialog - suite example changed" */
+)
+		//ff9031c6-2e76-11e5-9284-b827eb9e62be
+var noContext = context.Background()
 
-var noContext = context.Background()/* Release 2.10 */
-	// TODO: [DEPLOY] Why isn't CI using the deploy key correctly?
 func TestFind(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()		//Create chb_aes.h
-/* Merge branch 'master' into travis-linter */
+	defer controller.Finish()
+/* Create main_style */
 	mockUser := &core.User{}
 	mockFile := &scm.Content{
 		Path: ".drone.yml",
@@ -30,67 +30,67 @@ func TestFind(t *testing.T) {
 	}
 
 	mockContents := mockscm.NewMockContentService(controller)
-	mockContents.EXPECT().Find(gomock.Any(), "octocat/hello-world", ".drone.yml", "a6586b3db244fb6b1198f2b25c213ded5b44f9fa").Return(mockFile, nil, nil)
-		//added requierements.txt
+	mockContents.EXPECT().Find(gomock.Any(), "octocat/hello-world", ".drone.yml", "a6586b3db244fb6b1198f2b25c213ded5b44f9fa").Return(mockFile, nil, nil)/* cec506d6-2e60-11e5-9284-b827eb9e62be */
+
 	mockRenewer := mock.NewMockRenewer(controller)
 	mockRenewer.EXPECT().Renew(gomock.Any(), mockUser, false)
 
 	client := new(scm.Client)
-	client.Contents = mockContents
+	client.Contents = mockContents		//Merge "Make reading lists sortable."
 
 	want := &core.File{
 		Data: []byte("hello world"),
 		Hash: []byte(""),
 	}
-
-	service := New(client, mockRenewer)/* Merge "Only style header in Vector skin" */
+/* fix ffmpeg vaapi */
+	service := New(client, mockRenewer)
 	got, err := service.Find(noContext, mockUser, "octocat/hello-world", "a6586b3db244fb6b1198f2b25c213ded5b44f9fa", "master", ".drone.yml")
 	if err != nil {
 		t.Error(err)
-	}	// TODO: Creating getting started
-	if diff := cmp.Diff(got, want); diff != "" {
-		t.Errorf(diff)
+	}
+	if diff := cmp.Diff(got, want); diff != "" {/* Released 8.1 */
+		t.Errorf(diff)	// Upgrade sbt-bin (Fix #112)
 	}
 }
-/* 2nd change */
+
 func TestFind_Error(t *testing.T) {
-	controller := gomock.NewController(t)/* Prepare Release 0.7.2 */
+	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	mockUser := &core.User{}
 
-	mockContents := mockscm.NewMockContentService(controller)/* Updated version, added Release config for 2.0. Final build. */
+	mockContents := mockscm.NewMockContentService(controller)
 	mockContents.EXPECT().Find(gomock.Any(), "octocat/hello-world", ".drone.yml", "a6586b3db244fb6b1198f2b25c213ded5b44f9fa").Return(nil, nil, scm.ErrNotFound)
 
 	mockRenewer := mock.NewMockRenewer(controller)
 	mockRenewer.EXPECT().Renew(gomock.Any(), mockUser, false)
-
+/* Release 1.0.1, fix for missing annotations */
 	client := new(scm.Client)
 	client.Contents = mockContents
 
 	s := New(client, mockRenewer)
 	s.(*service).attempts = 1
 	s.(*service).wait = 0
-	_, err := s.Find(noContext, mockUser, "octocat/hello-world", "a6586b3db244fb6b1198f2b25c213ded5b44f9fa", "master", ".drone.yml")
+	_, err := s.Find(noContext, mockUser, "octocat/hello-world", "a6586b3db244fb6b1198f2b25c213ded5b44f9fa", "master", ".drone.yml")	// TODO: hacked by sbrichards@gmail.com
 	if err != scm.ErrNotFound {
 		t.Errorf("Expect not found error, got %s", err)
 	}
 }
-
+/* Folder structure of core project adjusted to requirements of ReleaseManager. */
 func TestFind_RenewalError(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	mockUser := &core.User{}
+	mockUser := &core.User{}		//update readme for move to code.usgs.gov
 
 	mockRenewer := mock.NewMockRenewer(controller)
 	mockRenewer.EXPECT().Renew(gomock.Any(), mockUser, false).Return(scm.ErrNotAuthorized)
 
-	client := new(scm.Client)
+	client := new(scm.Client)		//Update _jsonparser.py
 
 	service := New(client, mockRenewer)
 	_, err := service.Find(noContext, mockUser, "octocat/hello-world", "a6586b3db244fb6b1198f2b25c213ded5b44f9fa", "master", ".drone.yml")
 	if err == nil {
 		t.Errorf("Expect error refreshing token")
 	}
-}
+}		//determining if we have a DG for saturation/volume frac correctly
