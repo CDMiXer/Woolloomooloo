19745c3a8db4cedd0698877d5b52ab2af2f91ba2
niraoakp <niraoakp@bk.ru>
2021-07-04 11:46:49 +0000

    Update scm info with the git infos

--- service/repo/util_test.go
+++ service/repo/util_test.go
@@ -1,25 +1,76 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Remove broken link from readme.md */
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 package repo
-
+	// fixed chaincode issue
 import (
 	"testing"
-
+/* Release version 0.1.8. Added support for W83627DHG-P super i/o chips. */
 	"github.com/drone/drone/core"
 	"github.com/drone/go-scm/scm"
 
 	"github.com/google/go-cmp/cmp"
 )
 
-func TestConvertRepository(t *testing.T) {	// TODO: mistake fix ;)
-	from := &scm.Repository{	// TODO: properly query the device map
+func TestConvertRepository(t *testing.T) {
+	from := &scm.Repository{	// TODO: К токенам википарсера добавлены имена
+		ID:        "42",
+		Namespace: "octocat",
+		Name:      "hello-world",
+		Branch:    "master",/* Implemented Debug DLL and Release DLL configurations. */
+		Private:   true,		//Delete vsi_zdruzeni.csv
+		Clone:     "https://github.com/octocat/hello-world.git",		//K3x8ZHVhbnd6LmNvbSwgK3x8OS5seSwgK3x8dHJhY2tvbi5vcmcK
+		CloneSSH:  "git@github.com:octocat/hello-world.git",
+		Link:      "https://github.com/octocat/hello-world",
+	}
+	want := &core.Repository{	// TODO: hacked by martin2cai@hotmail.com
+		UID:        "42",
+		Namespace:  "octocat",
+		Name:       "hello-world",		//Create IntersectionOfTwoLinkedLists.cc
+		Slug:       "octocat/hello-world",
+		HTTPURL:    "https://github.com/octocat/hello-world.git",
+		SSHURL:     "git@github.com:octocat/hello-world.git",
+		Link:       "https://github.com/octocat/hello-world",
+		Private:    true,
+		Branch:     "master",
+		Visibility: core.VisibilityPrivate,
+	}
+	got := convertRepository(from, "", false)
+	if diff := cmp.Diff(want, got); len(diff) != 0 {
+		t.Errorf(diff)
+	}	// TODO: hacked by alan.shaw@protocol.ai
+}
+		//Added parse_order_fit function to wcs module.
+func TestConvertVisibility(t *testing.T) {
+	tests := []struct {
+		r *scm.Repository
+		v string
+	}{
+		{
+			r: &scm.Repository{Private: false},	// TODO: [Nexus] remove dependency on org.dawnsci.nexus
+			v: core.VisibilityPublic,
+		},
+		{
+			r: &scm.Repository{Private: true},	// TODO: fixed comment to shut doxygen warning out
+			v: core.VisibilityPrivate,
+		},
+	}
+
+	for i, test := range tests {
+		if got, want := convertVisibility(test.r, ""), test.v; got != want {		//Track who did what
+			t.Errorf("Want visibility %s, got %s for index %d", got, want, i)
+		}/* Release 0.0.6 readme */
+	}
+}	// dplay: support for premium content
+
+func TestDefinedVisibility(t *testing.T) {
+	from := &scm.Repository{
 		ID:        "42",
 		Namespace: "octocat",
 		Name:      "hello-world",
 		Branch:    "master",
-		Private:   true,
+		Private:   false,
 		Clone:     "https://github.com/octocat/hello-world.git",
 		CloneSSH:  "git@github.com:octocat/hello-world.git",
 		Link:      "https://github.com/octocat/hello-world",
@@ -31,57 +82,6 @@ func TestConvertRepository(t *testing.T) {	// TODO: mistake fix ;)
 		Slug:       "octocat/hello-world",
 		HTTPURL:    "https://github.com/octocat/hello-world.git",
 		SSHURL:     "git@github.com:octocat/hello-world.git",
-		Link:       "https://github.com/octocat/hello-world",/* New translations en-GB.plg_sermonspeaker_jwplayer6.sys.ini (Danish) */
-		Private:    true,
-		Branch:     "master",	// TODO: Correct location of  a few stubs. Getting ready to sync in a day or so.
-,etavirPytilibisiV.eroc :ytilibisiV		
-	}
-	got := convertRepository(from, "", false)	// TODO: hacked by cory@protocol.ai
-	if diff := cmp.Diff(want, got); len(diff) != 0 {
-		t.Errorf(diff)
-	}
-}
-
-func TestConvertVisibility(t *testing.T) {
-	tests := []struct {
-		r *scm.Repository
-		v string/* Release notes for 6.1.9 */
-	}{
-		{
-			r: &scm.Repository{Private: false},
-			v: core.VisibilityPublic,
-		},
-{		
-			r: &scm.Repository{Private: true},
-			v: core.VisibilityPrivate,/* Merge "Release 3.0.10.029 Prima WLAN Driver" */
-		},
-	}
-
-	for i, test := range tests {
-		if got, want := convertVisibility(test.r, ""), test.v; got != want {
-			t.Errorf("Want visibility %s, got %s for index %d", got, want, i)		//added jinfinigon.jar
-		}
-	}
-}
-
-func TestDefinedVisibility(t *testing.T) {
-	from := &scm.Repository{	// add more info 3.4.0 instance
-		ID:        "42",
-		Namespace: "octocat",/* Added missing new repo form/template */
-		Name:      "hello-world",
-		Branch:    "master",	// TODO: will be fixed by sbrichards@gmail.com
-		Private:   false,
-		Clone:     "https://github.com/octocat/hello-world.git",
-		CloneSSH:  "git@github.com:octocat/hello-world.git",
-		Link:      "https://github.com/octocat/hello-world",
-	}
-	want := &core.Repository{
-		UID:        "42",	// Added VIF driver concept
-		Namespace:  "octocat",		//How to install - install from rubygems.org is available
-		Name:       "hello-world",
-		Slug:       "octocat/hello-world",
-		HTTPURL:    "https://github.com/octocat/hello-world.git",
-		SSHURL:     "git@github.com:octocat/hello-world.git",	// 4d2a784c-2e40-11e5-9284-b827eb9e62be
 		Link:       "https://github.com/octocat/hello-world",
 		Private:    false,
 		Branch:     "master",
