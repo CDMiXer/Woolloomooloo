1da96bc8149c637c7add31b0963948db0d742324
niraoakp <niraoakp@bk.ru>
2021-07-04 04:36:39 +0000

    Handle events with slots

--- version/version_test.go
+++ version/version_test.go
@@ -1,8 +1,8 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
-// +build !oss
+	// TODO: will be fixed by peterke@gmail.com
+// +build !oss	// Crear front-end con Angular 2 #2
 
 package version
 
