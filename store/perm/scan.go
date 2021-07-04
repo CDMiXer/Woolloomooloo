c6533e74a16222f84eb26a593079c009a4a3dfa9
niraoakp <niraoakp@bk.ru>
2021-07-04 13:59:54 +0000

    Add version 1.0 test results and known issues

--- store/perm/scan.go
+++ store/perm/scan.go
@@ -1,62 +1,62 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc.		//Added doc url
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+//	// Updated to 64bit key
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License./* Release of eeacms/www-devel:19.3.9 */
 
-package perm
+package perm	// Merge "use relative include path"
 
 import (
 	"database/sql"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/store/shared/db"
-)
+)/* Added Fluxograma_Pedidos */
 
-// helper function converts the Perm structure to a set
+// helper function converts the Perm structure to a set	// TODO: will be fixed by mikeal.rogers@gmail.com
 // of named query parameters.
 func toParams(perm *core.Perm) map[string]interface{} {
 	return map[string]interface{}{
 		"perm_user_id":  perm.UserID,
-		"perm_repo_uid": perm.RepoUID,
-		"perm_read":     perm.Read,
+		"perm_repo_uid": perm.RepoUID,/* Fix issue#2, thanks tommy.murphy */
+		"perm_read":     perm.Read,		//24d30a90-2e44-11e5-9284-b827eb9e62be
 		"perm_write":    perm.Write,
 		"perm_admin":    perm.Admin,
 		"perm_synced":   perm.Synced,
 		"perm_created":  perm.Created,
-		"perm_updated":  perm.Updated,
+		"perm_updated":  perm.Updated,	// TODO: will be fixed by why@ipfs.io
 	}
 }
-
+	// First step for storing project definition in memory
 // helper function scans the sql.Row and copies the column
-// values to the destination object.
+// values to the destination object.	// TODO: ffmpeg-mt branch: merge from trunk up to rev 2521
 func scanRow(scanner db.Scanner, dst *core.Perm) error {
 	return scanner.Scan(
 		&dst.UserID,
 		&dst.RepoUID,
 		&dst.Read,
-		&dst.Write,
+		&dst.Write,/* Released version 0.3.0, added changelog */
 		&dst.Admin,
 		&dst.Synced,
 		&dst.Created,
-		&dst.Updated,
+		&dst.Updated,/* Release version 1.0.0.M2 */
 	)
-}
+}		//because reasons
 
 // helper function scans the sql.Row and copies the column
 // values to the destination object.
-func scanCollabRow(scanner db.Scanner, dst *core.Collaborator) error {
+func scanCollabRow(scanner db.Scanner, dst *core.Collaborator) error {/* Release 0.94.211 */
 	return scanner.Scan(
 		&dst.UserID,
-		&dst.RepoUID,
+		&dst.RepoUID,		//Create 10_areas/intro.md
 		&dst.Login,
 		&dst.Avatar,
 		&dst.Read,
