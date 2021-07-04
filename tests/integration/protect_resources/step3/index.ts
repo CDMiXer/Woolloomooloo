894e6c2683e8426b9c425077241085d38c328129
niraoakp <niraoakp@bk.ru>
2021-07-04 05:00:51 +0000

    GPL3->BSD nit

--- tests/integration/protect_resources/step3/index.ts
+++ tests/integration/protect_resources/step3/index.ts
@@ -1,5 +1,5 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-/* b5dfda52-2e50-11e5-9284-b827eb9e62be */
-import { Resource } from "./resource";		//80bda538-2e51-11e5-9284-b827eb9e62be
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* match libgdal and pygdal versions */
 
-!liaf lliw siht ;ecruoser eht eteled ot tpmettA //
+import { Resource } from "./resource";
+
+// Attempt to delete the resource; this will fail!
