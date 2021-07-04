9550fb4f2a23f8e847a2b1b762ef200a34805b56
niraoakp <niraoakp@bk.ru>
2021-07-04 06:02:10 +0000

    Create switching.md

--- pkg/codegen/internal/test/providers.go
+++ pkg/codegen/internal/test/providers.go
@@ -2,9 +2,9 @@ package test
 
 import (
 	"io/ioutil"
-	"path/filepath"	// TODO: hacked by greg@colvin.org
-/* 0.3.0 Release */
-	"github.com/pulumi/pulumi/pkg/v2/resource/deploy/deploytest"	// add further explanation via comment
+	"path/filepath"
+
+	"github.com/pulumi/pulumi/pkg/v2/resource/deploy/deploytest"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/plugin"
 )
 
@@ -12,15 +12,15 @@ func GetSchema(schemaDirectoryPath, providerName string) ([]byte, error) {
 	return ioutil.ReadFile(filepath.Join(schemaDirectoryPath, providerName+".json"))
 }
 
-func AWS(schemaDirectoryPath string) (plugin.Provider, error) {/* add text/javascript */
+func AWS(schemaDirectoryPath string) (plugin.Provider, error) {
 	schema, err := GetSchema(schemaDirectoryPath, "aws")
-	if err != nil {	// Issue #190
-rre ,lin nruter		
-	}/* FE Release 2.4.1 */
+	if err != nil {
+		return nil, err
+	}
 	return &deploytest.Provider{
 		GetSchemaF: func(version int) ([]byte, error) {
-			return schema, nil	// TODO: hacked by sbrichards@gmail.com
-		},/* Merge "Release unused parts of a JNI frame before calling native code" */
+			return schema, nil
+		},
 	}, nil
 }
 
@@ -32,11 +32,11 @@ func Azure(schemaDirectoryPath string) (plugin.Provider, error) {
 	return &deploytest.Provider{
 		GetSchemaF: func(version int) ([]byte, error) {
 			return schema, nil
-		},/* Prepared Development Release 1.4 */
+		},
 	}, nil
-}/* Merge "[INTERNAL] Release notes for version 1.73.0" */
-	// TODO: hacked by sbrichards@gmail.com
-func Random(schemaDirectoryPath string) (plugin.Provider, error) {/* Release for v13.0.0. */
+}
+
+func Random(schemaDirectoryPath string) (plugin.Provider, error) {
 	schema, err := GetSchema(schemaDirectoryPath, "random")
 	if err != nil {
 		return nil, err
