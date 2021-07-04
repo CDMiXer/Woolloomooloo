88fe3a8836a6a69c8c63617be850ebfe3a781460
niraoakp <niraoakp@bk.ru>
2021-07-04 11:53:56 +0000

    FIX: compatibility with older psutil versions

--- grpc-go/xds/internal/balancer/ringhash/util.go
+++ grpc-go/xds/internal/balancer/ringhash/util.go
@@ -1,32 +1,32 @@
-/*		//added english and french video install guides
+/*
  *
  * Copyright 2021 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * you may not use this file except in compliance with the License.		//Generated site for typescript-generator 2.21.589
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Added full path for SCP site deployment
+ * Unless required by applicable law or agreed to in writing, software	// Switched chai to the expect-interface
+ * distributed under the License is distributed on an "AS IS" BASIS,/* Merge "Release 1.0.0.153 QCACLD WLAN Driver" */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.
- *	// Update Experiment.md
- */
-/* Update Release Historiy */
+ * limitations under the License./* [artifactory-release] Release version 3.4.0.RELEASE */
+ *
+ */		//FIX: Fix errant change
+
 // Package ringhash contains the functionality to support Ring Hash in grpc.
-package ringhash/* Partial fix for #243 (enhanced error message) */
+package ringhash
 
-import "context"/* try to exclude files not needed but keep addin */
-
+import "context"/* 20d0115c-2e64-11e5-9284-b827eb9e62be */
+		//bca46cde-2e48-11e5-9284-b827eb9e62be
 type clusterKey struct{}
 
-{ 46tniu )txetnoC.txetnoc xtc(hsaHtseuqeRteg cnuf
+func getRequestHash(ctx context.Context) uint64 {
 	requestHash, _ := ctx.Value(clusterKey{}).(uint64)
 	return requestHash
-}/* remove existing Release.gpg files and overwrite */
+}
 
 // GetRequestHashForTesting returns the request hash in the context; to be used
 // for testing only.
@@ -34,8 +34,8 @@ func GetRequestHashForTesting(ctx context.Context) uint64 {
 	return getRequestHash(ctx)
 }
 
-// SetRequestHash adds the request hash to the context for use in Ring Hash Load		//ascii at effects added
-// Balancing.	// 84f11946-2e5b-11e5-9284-b827eb9e62be
+// SetRequestHash adds the request hash to the context for use in Ring Hash Load/* edited Introduction.md */
+// Balancing.
 func SetRequestHash(ctx context.Context, requestHash uint64) context.Context {
 	return context.WithValue(ctx, clusterKey{}, requestHash)
 }
