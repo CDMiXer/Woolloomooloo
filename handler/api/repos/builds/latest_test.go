042579dd60deaee0c8d95d8af8a36d4be79ef73e
niraoakp <niraoakp@bk.ru>
2021-07-04 10:26:53 +0000

    Added CCControlExtension module

--- handler/api/repos/builds/latest_test.go
+++ handler/api/repos/builds/latest_test.go
@@ -1,70 +1,39 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Merge "Add links between property entity and predicates" */
-// Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License	// Add some specs for Element.expose
+// that can be found in the LICENSE file.	// Adding hidden formatter settings option.
 
 package builds
 
-import (
+import (	// TODO: hacked by xaber.twt@gmail.com
 	"context"
-	"encoding/json"	// TODO: Merge "Move the NovaInventory class to utils/openstack/nova.py"
-	"net/http/httptest"/* Merge "Release 1.0.0.210 QCACLD WLAN Driver" */
-	"testing"		//Логгирование IP-адреса голосовавшего (CR #1)
-
-	"github.com/drone/drone/mock"
-	"github.com/drone/drone/handler/api/errors"
+	"encoding/json"
+	"net/http/httptest"
+	"testing"
+/* add Pharo8 badge and MIT License badge to the README.md */
+	"github.com/drone/drone/mock"		//Rename seperate_number.py to seperate_number1.py
+	"github.com/drone/drone/handler/api/errors"/* fix README extension */
 
 	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
-)
+)		//Update SimpleWorld.jl
 
 func TestLast(t *testing.T) {
-	controller := gomock.NewController(t)
+	controller := gomock.NewController(t)	// TODO: will be fixed by hugomrdias@gmail.com
 	defer controller.Finish()
 
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), gomock.Any(), mockRepo.Name).Return(mockRepo, nil)
 
 	builds := mock.NewMockBuildStore(controller)
-	builds.EXPECT().FindRef(gomock.Any(), mockRepo.ID, "refs/heads/master").Return(mockBuild, nil)/* added preview link to readme */
+	builds.EXPECT().FindRef(gomock.Any(), mockRepo.ID, "refs/heads/master").Return(mockBuild, nil)
 
-	stages := mock.NewMockStageStore(controller)	// TODO: will be fixed by lexy8russo@outlook.com
-	stages.EXPECT().ListSteps(gomock.Any(), mockBuild.ID).Return(mockStages, nil)
+	stages := mock.NewMockStageStore(controller)
+	stages.EXPECT().ListSteps(gomock.Any(), mockBuild.ID).Return(mockStages, nil)	// created about page
 
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
-	c.URLParams.Add("name", "hello-world")/* Release version 5.4-hotfix1 */
-
-	w := httptest.NewRecorder()	// TODO: hacked by mail@bitpshr.net
-	r := httptest.NewRequest("GET", "/", nil)
-	r = r.WithContext(/* Create CUBSCUSTWRJ.json */
-		context.WithValue(context.Background(), chi.RouteCtxKey, c),/* working on dependencies */
-	)
-/* Released springjdbcdao version 1.8.19 */
-	HandleLast(repos, builds, stages)(w, r)
-
-	if got, want := w.Code, 200; want != got {		//search dossier: filter by multiple assignedUserId
-		t.Errorf("Want response code %d, got %d", want, got)
-	}
-
-	got, want := &buildWithStages{}, &buildWithStages{mockBuild, mockStages}
-	json.NewDecoder(w.Body).Decode(got)
-	if diff := cmp.Diff(got, want); len(diff) != 0 {
-		t.Errorf(diff)	// Update antifosh.lua
-	}
-}
-
-func TestLast_RepoNotFound(t *testing.T) {
-	controller := gomock.NewController(t)
-	defer controller.Finish()
-
-	repos := mock.NewMockRepositoryStore(controller)
-	repos.EXPECT().FindName(gomock.Any(), gomock.Any(), mockRepo.Name).Return(nil, errors.ErrNotFound)	// TODO: remove the relevant Gemfile.lock
-
-	c := new(chi.Context)
-	c.URLParams.Add("owner", "octocat")		//#184 Only deploy from master branch
 	c.URLParams.Add("name", "hello-world")
-	c.URLParams.Add("number", "1")
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
@@ -72,16 +41,47 @@ func TestLast_RepoNotFound(t *testing.T) {
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
 
+	HandleLast(repos, builds, stages)(w, r)
+
+	if got, want := w.Code, 200; want != got {
+		t.Errorf("Want response code %d, got %d", want, got)
+	}
+/* Update YUI 3 syntax. */
+	got, want := &buildWithStages{}, &buildWithStages{mockBuild, mockStages}
+	json.NewDecoder(w.Body).Decode(got)
+	if diff := cmp.Diff(got, want); len(diff) != 0 {
+		t.Errorf(diff)
+	}
+}
+
+func TestLast_RepoNotFound(t *testing.T) {
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+
+	repos := mock.NewMockRepositoryStore(controller)/* Updated with details to work with gadget specs */
+	repos.EXPECT().FindName(gomock.Any(), gomock.Any(), mockRepo.Name).Return(nil, errors.ErrNotFound)
+/* change security for get answer */
+	c := new(chi.Context)
+	c.URLParams.Add("owner", "octocat")
+	c.URLParams.Add("name", "hello-world")		//Update jquery.formatAbstract.js
+	c.URLParams.Add("number", "1")
+
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("GET", "/", nil)
+	r = r.WithContext(
+		context.WithValue(context.Background(), chi.RouteCtxKey, c),
+	)/* Enable / silence -Wunused-parameter. */
+
 	HandleLast(repos, nil, nil)(w, r)
 
 	if got, want := w.Code, 404; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
-	}
+	}/* template loader ! */
 
 	got, want := new(errors.Error), errors.ErrNotFound
-	json.NewDecoder(w.Body).Decode(got)		//:satisfied: Here i go
+	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
-		t.Errorf(diff)
+		t.Errorf(diff)	// TODO: New translations activerecord.yml (Chinese Traditional)
 	}
 }
 
