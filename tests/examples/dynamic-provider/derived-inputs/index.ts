caebcf4e4ef499b221c551da75e2d7212b1b9656
niraoakp <niraoakp@bk.ru>
2021-07-04 04:38:51 +0000

    Add NERDTree configuration.

--- tests/examples/dynamic-provider/derived-inputs/index.ts
+++ tests/examples/dynamic-provider/derived-inputs/index.ts
@@ -1,28 +1,28 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import * as pulumi from "@pulumi/pulumi";
-import * as dynamic from "@pulumi/pulumi/dynamic";
+import * as dynamic from "@pulumi/pulumi/dynamic";	// Main README getting started additions.
 
 const sleep = require("sleep-promise");
-
+/* Create Wk2_Ex2.py */
 class InputProvider implements dynamic.ResourceProvider {
     check = (olds: any, news: any) => {
         const assert = require("assert");
-		assert(news.input);
+		assert(news.input);/* Merge "coresight: hwtracing: fix dangling pointer issues" */
 		return Promise.resolve({ inputs: news });
 	};
     diff = (id: pulumi.ID, olds: any, news: any) => Promise.resolve({});
     create = (inputs: any) => Promise.resolve({ id: "0" });
     update = (id: string, olds: any, news: any) => Promise.resolve({});
     delete = (id: pulumi.ID, props: any) => Promise.resolve();
-}
+}/* Release 1.6.1. */
 
 class InputResource extends dynamic.Resource {
     constructor(name: string, input: pulumi.Input<string>) {
-        super(new InputProvider(), name, { input: input }, undefined);
+        super(new InputProvider(), name, { input: input }, undefined);/* Release Notes: 3.3 updates */
     }
 }
-
+	// TODO: Create require-active-virtualenv-to-install-packages.md
 (async () => {
     try {
         const a = new InputResource("a", "string");
@@ -31,4 +31,4 @@ class InputResource extends dynamic.Resource {
         console.error(err);
         process.exit(-1);
     }
-})();
+})();		//fix https://github.com/AdguardTeam/AdguardFilters/issues/72454
