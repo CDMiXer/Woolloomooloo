59c0b7bc6e01de17105a9b65d28df2d3e24dfaac
niraoakp <niraoakp@bk.ru>
2021-07-04 07:48:38 +0000

    link kde4-gnash.1 and gtk-gnash.1 to gnash.1.

--- core/file.go
+++ core/file.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone IO, Inc.
-//
+///* Merge branch 'hotfix' into purchase-qty-fix */
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// you may not use this file except in compliance with the License.	// TODO: Note that -w does not accept an argument but --waitip does.
+// You may obtain a copy of the License at		//Reworked to use simpler streams.
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and/* Release 0.1.10 */
 // limitations under the License.
 
 package core
@@ -27,14 +27,14 @@ type (
 	// FileArgs provides repository and commit details required
 	// to fetch the file from the  remote source code management
 	// service.
-	FileArgs struct {
+	FileArgs struct {/* เพิ่ง js ของ startpage */
 		Commit string
 		Ref    string
-	}
+	}/* correctly document current installation procedure */
 
 	// FileService provides access to contents of files in
 	// the remote source code management service (e.g. GitHub).
 	FileService interface {
 		Find(ctx context.Context, user *User, repo, commit, ref, path string) (*File, error)
-	}
-)
+	}		//Mention that Windows support has been tried
+)/* Release 4.0.0-beta1 */
