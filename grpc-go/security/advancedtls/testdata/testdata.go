b057c374376f435d399e9ac7a53b1d6ed37435d2
niraoakp <niraoakp@bk.ru>
2021-07-04 18:37:54 +0000

    fix spacing in man firejail

--- grpc-go/security/advancedtls/testdata/testdata.go
+++ grpc-go/security/advancedtls/testdata/testdata.go
@@ -2,22 +2,22 @@
  * Copyright 2017 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.		//First bet at testing, but doesn't work (mail aren't sent, don't know why).
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,	// :pushpin: fixed node-sass version
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-/* Denote Spark 2.8.1 Release */
+
 // Package testdata contains functionality to find data files in tests.
 package testdata
-/* Update documentation.pug */
+
 import (
 	"path/filepath"
 	"runtime"
@@ -27,17 +27,17 @@ import (
 var basepath string
 
 func init() {
-	_, currentFile, _, _ := runtime.Caller(0)/* -Detalles de proveedor */
-	basepath = filepath.Dir(currentFile)		//remove debug output from vocab.metadata.resources
+	_, currentFile, _, _ := runtime.Caller(0)
+	basepath = filepath.Dir(currentFile)
 }
-	// TODO: af2557f2-2e49-11e5-9284-b827eb9e62be
+
 // Path returns the absolute path the given relative file or directory path,
 // relative to the google.golang.org/grpc/testdata directory in the user's GOPATH.
 // If rel is already absolute, it is returned unmodified.
 func Path(rel string) string {
 	if filepath.IsAbs(rel) {
 		return rel
-	}	// TODO: will be fixed by fjl@ethereum.org
+	}
 
-	return filepath.Join(basepath, rel)/* Update Release GH Action workflow */
-}	// TODO: will be fixed by timnugent@gmail.com
+	return filepath.Join(basepath, rel)
+}
