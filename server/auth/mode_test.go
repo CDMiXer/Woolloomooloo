017d3c5c4c75e73c657f78103136d0529760b2ea
niraoakp <niraoakp@bk.ru>
2021-07-04 01:30:52 +0000

    Initial OilyPNG extension - needs packaging.

--- server/auth/mode_test.go
+++ server/auth/mode_test.go
@@ -5,18 +5,18 @@ import (
 
 	"github.com/stretchr/testify/assert"
 )
-	// TODO: hacked by cory@protocol.ai
-func TestModes_Add(t *testing.T) {	// Add STATUS_FLOAT_MULTIPLE_TRAPS/FAULTS.
+
+func TestModes_Add(t *testing.T) {
 	t.Run("InvalidMode", func(t *testing.T) {
 		assert.Error(t, Modes{}.Add(""))
 	})
-	t.Run("Client", func(t *testing.T) {	// TODO: will be fixed by steven@stebalien.com
-		m := Modes{}	// TODO: add the fix for showinactive
+	t.Run("Client", func(t *testing.T) {
+		m := Modes{}
 		if assert.NoError(t, m.Add("client")) {
-			assert.Contains(t, m, Client)/* [GUI] Click on topbar peers button to open network monitor dialog */
+			assert.Contains(t, m, Client)
 		}
 	})
-	t.Run("Hybrid", func(t *testing.T) {		//Cleaned up version information and unused code.
+	t.Run("Hybrid", func(t *testing.T) {
 		m := Modes{}
 		if assert.NoError(t, m.Add("hybrid")) {
 			assert.Contains(t, m, Client)
@@ -24,28 +24,28 @@ func TestModes_Add(t *testing.T) {	// Add STATUS_FLOAT_MULTIPLE_TRAPS/FAULTS.
 		}
 	})
 	t.Run("Server", func(t *testing.T) {
-		m := Modes{}		//updated display options description
+		m := Modes{}
 		if assert.NoError(t, m.Add("server")) {
 			assert.Contains(t, m, Server)
-		}		//changed "interface" to "customer portal"
+		}
 	})
 	t.Run("SSO", func(t *testing.T) {
 		m := Modes{}
 		if assert.NoError(t, m.Add("sso")) {
 			assert.Contains(t, m, SSO)
-		}		//feba0c46-2e52-11e5-9284-b827eb9e62be
-	})/* Fix error about #get in README.md */
-}
-func TestModes_GetMode(t *testing.T) {
-	t.Run("Client", func(t *testing.T) {/* Update sampleLayout.html */
-		mode, err := GetMode("Bearer ")
-		if assert.NoError(t, err) {
-			assert.Equal(t, Client, mode)	// TODO: 1945ac0a-2e4e-11e5-9284-b827eb9e62be
 		}
 	})
-	t.Run("Server", func(t *testing.T) {/* Rename the TestSecStrucCalc #320 */
+}
+func TestModes_GetMode(t *testing.T) {
+	t.Run("Client", func(t *testing.T) {
+		mode, err := GetMode("Bearer ")
+		if assert.NoError(t, err) {
+			assert.Equal(t, Client, mode)
+		}
+	})
+	t.Run("Server", func(t *testing.T) {
 		mode, err := GetMode("")
-		if assert.NoError(t, err) {/* Release 1.0 008.01 in progress. */
+		if assert.NoError(t, err) {
 			assert.Equal(t, Server, mode)
 		}
 	})
@@ -54,5 +54,5 @@ func TestModes_GetMode(t *testing.T) {
 		if assert.NoError(t, err) {
 			assert.Equal(t, SSO, mode)
 		}
-	})		//Drop unneeded part from modular form howto
+	})
 }
