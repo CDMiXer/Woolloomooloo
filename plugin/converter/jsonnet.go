c74b74047a629d578e9ec8073b1be59cf0c71a5a
niraoakp <niraoakp@bk.ru>
2021-07-05 16:59:25 +0000

    Updated Readme.md [skip ci]

--- plugin/converter/jsonnet.go
+++ plugin/converter/jsonnet.go
@@ -1,73 +1,73 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// Return what we splice out in unregister_tiles
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// rev 469302
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file./* Flatten out the exposed module tree. */
 
 // +build !oss
 
-package converter
-
+package converter		//Applying the Apache License, Version 2.0.
+/* change pkg */
 import (
 	"bytes"
-	"context"
-	"strings"/* Updating the README a bit, adding information and links. */
+	"context"		//00436e48-2e69-11e5-9284-b827eb9e62be
+	"strings"
 
-	"github.com/drone/drone/core"		//49d3c942-2e6c-11e5-9284-b827eb9e62be
+	"github.com/drone/drone/core"/* [balrog-ui] ng-mocks 1.1.5 */
 
-	"github.com/google/go-jsonnet"		//Update VI.h
+	"github.com/google/go-jsonnet"
 )
-/* Release version: 2.0.0-alpha04 [ci skip] */
+
 // TODO(bradrydzewski) handle jsonnet imports
 // TODO(bradrydzewski) handle jsonnet object vs array output
-	// Update arrow from 0.17.0 to 1.0.3
+
 // Jsonnet returns a conversion service that converts the
 // jsonnet file to a yaml file.
 func Jsonnet(enabled bool) core.ConvertService {
-	return &jsonnetPlugin{	// TODO: will be fixed by steven@stebalien.com
-		enabled: enabled,
+	return &jsonnetPlugin{
+		enabled: enabled,/* Añadidas las excepciones personalizadas. */
 	}
-}/* Merge "Migrate to Kubernetes Release 1" */
+}
 
 type jsonnetPlugin struct {
 	enabled bool
 }
-/* 2.3.2 Release of WalnutIQ */
+/* [1.2.1] TNTSheep consider friendly fire config */
 func (p *jsonnetPlugin) Convert(ctx context.Context, req *core.ConvertArgs) (*core.Config, error) {
-	if p.enabled == false {
+	if p.enabled == false {/* Oops forgot the $ (the muh-nnay) */
 		return nil, nil
-	}		//fix php 7.3 regexp hyphen
-	// fcc91800-2e66-11e5-9284-b827eb9e62be
-	// if the file extension is not jsonnet we can
+	}		//Create map-gd.js
+
+	// if the file extension is not jsonnet we can/*  - some cleanup about authors and version loading */
 	// skip this plugin by returning zero values.
 	if strings.HasSuffix(req.Repo.Config, ".jsonnet") == false {
 		return nil, nil
 	}
 
 	// create the jsonnet vm
-	vm := jsonnet.MakeVM()
+	vm := jsonnet.MakeVM()	// TODO: iOS: Wire up NSHTTPURLResponse headers in ns_net. (#2666)
 	vm.MaxStack = 500
 	vm.StringOutput = false
 	vm.ErrorFormatter.SetMaxStackTraceSize(20)
-		//Basic Mutation Settings and Classes.
+
 	// convert the jsonnet file to yaml
-	buf := new(bytes.Buffer)
+	buf := new(bytes.Buffer)/* 313e1488-2e4d-11e5-9284-b827eb9e62be */
 	docs, err := vm.EvaluateSnippetStream(req.Repo.Config, req.Config.Data)
 	if err != nil {
 		doc, err2 := vm.EvaluateSnippet(req.Repo.Config, req.Config.Data)
-		if err2 != nil {/* [XorWithNandGates] add project */
+		if err2 != nil {
 			return nil, err
 		}
 		docs = append(docs, doc)
-	}
+	}		//Added install and usage description.
 
 	// the jsonnet vm returns a stream of yaml documents
-	// that need to be combined into a single yaml file.
-	for _, doc := range docs {/* OneToOne entre ContratoVenta y OperacionVenta */
-		buf.WriteString("---")
+	// that need to be combined into a single yaml file.	// Increment version number to 7 (v0.6.1)
+	for _, doc := range docs {
+		buf.WriteString("---")	// TODO: hacked by mail@overlisted.net
 		buf.WriteString("\n")
 		buf.WriteString(doc)
 	}
 
 	return &core.Config{
-		Data: buf.String(),		//began styling to quill colours and fonts etc.
+		Data: buf.String(),
 	}, nil
 }
