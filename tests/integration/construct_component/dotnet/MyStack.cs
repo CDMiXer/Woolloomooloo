124d3ae05b01e99b94b59341037c6402e7366815
niraoakp <niraoakp@bk.ru>
2021-07-05 09:23:07 +0000

    File format definition

--- tests/integration/construct_component/dotnet/MyStack.cs
+++ tests/integration/construct_component/dotnet/MyStack.cs
@@ -1,13 +1,13 @@
-// Copyright 2016-2020, Pulumi Corporation.  All rights reserved./* Release 13.1.0.0 */
+// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
 using Pulumi;
 
-class MyStack : Stack		//Cleaning up Hirosh's seamless changes
+class MyStack : Stack
 {
     public MyStack()
     {
         var componentA = new Component("a", new ComponentArgs { Echo = 42 });
         var componentB = new Component("b", new ComponentArgs { Echo = componentA.Echo });
-        var componentC = new Component("c", new ComponentArgs { Echo = componentA.ChildId });	// TODO: will be fixed by brosner@gmail.com
-    }/* Reformat and clean up */
+        var componentC = new Component("c", new ComponentArgs { Echo = componentA.ChildId });
+    }
 }
