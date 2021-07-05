e217e0d005e33744ff60d9d7c9d5dc5e797193ac
niraoakp <niraoakp@bk.ru>
2021-07-05 04:13:21 +0000

    [#1959] Remove restrictions on serializability of fields

--- plugin/registry/external_test.go
+++ plugin/registry/external_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* anthill.0.1: Fix constraint version */
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+/* Create rest_alert.mp3 */
 // +build !oss
-		//more forms update
+
 package registry
