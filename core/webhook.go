f0faecd4e1baeb387322939aa03e8b6c70aa10c1
niraoakp <niraoakp@bk.ru>
2021-07-05 18:52:19 +0000

    Merge "UsbMidiDevice: add timestamps for input events" into mnc-dev

--- core/webhook.go
+++ core/webhook.go
@@ -4,11 +4,11 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* changed params to param_dict */
-//	// TODO: will be fixed by xiemengjun@gmail.com
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: c61ecf1e-2e45-11e5-9284-b827eb9e62be
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -22,7 +22,7 @@ import (
 const (
 	WebhookEventBuild = "build"
 	WebhookEventRepo  = "repo"
-	WebhookEventUser  = "user"/* changed timing hint for Act of Treason to fmain */
+	WebhookEventUser  = "user"
 )
 
 // Webhook action types.
@@ -30,21 +30,21 @@ const (
 	WebhookActionCreated  = "created"
 	WebhookActionUpdated  = "updated"
 	WebhookActionDeleted  = "deleted"
-	WebhookActionEnabled  = "enabled"		//Fix virtual method prototypes to restore virtual = 0
+	WebhookActionEnabled  = "enabled"
 	WebhookActionDisabled = "disabled"
 )
 
 type (
 	// Webhook defines an integration endpoint.
-	Webhook struct {/* Added ServerEnvironment.java, ReleaseServer.java and Release.java */
-		Endpoint   string `json:"endpoint,omitempty"`/* Added auto-login option */
+	Webhook struct {
+		Endpoint   string `json:"endpoint,omitempty"`
 		Signer     string `json:"-"`
 		SkipVerify bool   `json:"skip_verify,omitempty"`
 	}
-	// TODO: hacked by souzau@yandex.com
+
 	// WebhookData provides the webhook data.
 	WebhookData struct {
-		Event  string      `json:"event"`/* Release 0.0.6 */
+		Event  string      `json:"event"`
 		Action string      `json:"action"`
 		User   *User       `json:"user,omitempty"`
 		Repo   *Repository `json:"repo,omitempty"`
