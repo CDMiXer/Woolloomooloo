80d6a226987bf49d0ba7d0380e299ed9fd94aec4
niraoakp <niraoakp@bk.ru>
2021-07-05 22:18:28 +0000

    add npm run test:debug

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,2 +1,2 @@
-$env:CGO_ENABLED="0"	// TODO: will be fixed by igor@soramitsu.co.jp
+$env:CGO_ENABLED="0"/* Release 2.0.5. */
 go build -o release/windows/amd64/drone-agent.exe github.com/drone/drone/cmd/drone-agent
