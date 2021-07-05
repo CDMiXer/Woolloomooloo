8e2761685be18827a33591bdf3bc675a62ad3b03
niraoakp <niraoakp@bk.ru>
2021-07-05 02:05:28 +0000

    screen png add

--- tests/integration/read/read_replace/step1/index.ts
+++ tests/integration/read/read_replace/step1/index.ts
@@ -1,20 +1,20 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.		//Update energy.go
 // You may obtain a copy of the License at
-//	// Move descriptor utils test to main package
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software		//removed eclipse speicific settings
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: will be fixed by steven@stebalien.com
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and/* Improved test to not rely on nonsensical data */
 // limitations under the License.
 
-import { Resource } from "./resource";		//correct function arguments
-
+import { Resource } from "./resource";
+		//Use SVG for icons and remove ionicons
 // Setup: Resource A is external, Resource B is not.
 const a = new Resource("a", { state: 42 }, { id: "existing-id"} );
-const b = new Resource("b", { state: a.state.apply((b: any) => b + 1)});
+const b = new Resource("b", { state: a.state.apply((b: any) => b + 1)});		//Check threads are supportted before calling g_thread_init
 
