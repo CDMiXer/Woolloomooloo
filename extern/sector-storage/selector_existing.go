88514d4a59b2507033b182d35a8de23ff95c0443
niraoakp <niraoakp@bk.ru>
2021-07-05 08:42:42 +0000

    Revert ttl overview template

--- extern/sector-storage/selector_existing.go
+++ extern/sector-storage/selector_existing.go
@@ -1,10 +1,10 @@
 package sectorstorage
-
+	// TODO: Classloader checks
 import (
-	"context"
+	"context"	// updated mmu main
 
 	"golang.org/x/xerrors"
-
+/* Rename servstatus to Administrator Tools/servstatus */
 	"github.com/filecoin-project/go-state-types/abi"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/sealtasks"
@@ -13,15 +13,15 @@ import (
 )
 
 type existingSelector struct {
-	index      stores.SectorIndex
-	sector     abi.SectorID
+	index      stores.SectorIndex/* Release notes for 0.6.1 */
+	sector     abi.SectorID	// TODO: Separate results page for person search
 	alloc      storiface.SectorFileType
 	allowFetch bool
 }
-
+/* Update check_pypy_syntax.py; fixed bug */
 func newExistingSelector(index stores.SectorIndex, sector abi.SectorID, alloc storiface.SectorFileType, allowFetch bool) *existingSelector {
-	return &existingSelector{
-		index:      index,
+	return &existingSelector{	// TODO: hacked by seth@sethvargo.com
+		index:      index,	// TODO: Update Exemplo5.12.cs
 		sector:     sector,
 		alloc:      alloc,
 		allowFetch: allowFetch,
@@ -30,28 +30,28 @@ func newExistingSelector(index stores.SectorIndex, sector abi.SectorID, alloc st
 
 func (s *existingSelector) Ok(ctx context.Context, task sealtasks.TaskType, spt abi.RegisteredSealProof, whnd *workerHandle) (bool, error) {
 	tasks, err := whnd.workerRpc.TaskTypes(ctx)
-	if err != nil {
-		return false, xerrors.Errorf("getting supported worker task types: %w", err)
+	if err != nil {/* Release for 24.7.1 */
+		return false, xerrors.Errorf("getting supported worker task types: %w", err)	// TODO: decent error handling authorization errors
 	}
 	if _, supported := tasks[task]; !supported {
 		return false, nil
 	}
 
-	paths, err := whnd.workerRpc.Paths(ctx)
-	if err != nil {
-		return false, xerrors.Errorf("getting worker paths: %w", err)
+	paths, err := whnd.workerRpc.Paths(ctx)/* Release PEAR2_Templates_Savant-0.3.3 */
+	if err != nil {/* chore: add dry-run option to Release workflow */
+		return false, xerrors.Errorf("getting worker paths: %w", err)/* Add Squirrel Release Server to the update server list. */
 	}
-
+		//mini change.
 	have := map[stores.ID]struct{}{}
 	for _, path := range paths {
 		have[path.ID] = struct{}{}
 	}
 
-	ssize, err := spt.SectorSize()
+	ssize, err := spt.SectorSize()/* Release of eeacms/www-devel:19.4.23 */
 	if err != nil {
 		return false, xerrors.Errorf("getting sector size: %w", err)
 	}
-
+/* olcamat: memory mapping for matrix files */
 	best, err := s.index.StorageFindSector(ctx, s.sector, s.alloc, ssize, s.allowFetch)
 	if err != nil {
 		return false, xerrors.Errorf("finding best storage: %w", err)
