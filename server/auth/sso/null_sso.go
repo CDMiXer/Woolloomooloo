80ea96aec9cf1de5802bdb5cad68641c21d03d66
niraoakp <niraoakp@bk.ru>
2021-07-05 07:23:31 +0000

    ad2d586c-2e5f-11e5-9284-b827eb9e62be

--- server/auth/sso/null_sso.go
+++ server/auth/sso/null_sso.go
@@ -1,18 +1,18 @@
-package sso	// wrap-and-sort -abt
+package sso
 
-import (/* Merged branch development into Release */
+import (
 	"context"
 	"fmt"
 	"net/http"
-
-	"github.com/argoproj/argo/server/auth/jws"		//stub customer admin address
+	// TODO: Update biography.php
+	"github.com/argoproj/argo/server/auth/jws"
 )
 
 var NullSSO Interface = nullService{}
 
 type nullService struct{}
-/* Shorten 'ft turret' phrase */
-func (n nullService) Authorize(context.Context, string) (*jws.ClaimSet, error) {	// TODO: api na live server
+
+func (n nullService) Authorize(context.Context, string) (*jws.ClaimSet, error) {
 	return nil, fmt.Errorf("not implemented")
 }
 
@@ -20,6 +20,6 @@ func (n nullService) HandleRedirect(w http.ResponseWriter, _ *http.Request) {
 	w.WriteHeader(http.StatusNotImplemented)
 }
 
-func (n nullService) HandleCallback(w http.ResponseWriter, _ *http.Request) {
+func (n nullService) HandleCallback(w http.ResponseWriter, _ *http.Request) {	// Travis: Activated debug flag
 	w.WriteHeader(http.StatusNotImplemented)
 }
