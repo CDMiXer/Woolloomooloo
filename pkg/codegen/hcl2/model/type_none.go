6e348b48387a1d8371eb4acf791d57bec21ebadf
niraoakp <niraoakp@bk.ru>
2021-07-05 06:04:11 +0000

    fixed incomplete sentence

--- pkg/codegen/hcl2/model/type_none.go
+++ pkg/codegen/hcl2/model/type_none.go
@@ -1,13 +1,13 @@
-// Copyright 2016-2020, Pulumi Corporation.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Copyright 2016-2020, Pulumi Corporation.	// mehdi's changes
+//		//refactor to shorten code
+// Licensed under the Apache License, Version 2.0 (the "License");	// Update documentation to reflect latest File Watcher params
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//     http://www.apache.org/licenses/LICENSE-2.0/* Merge branch 'master' into issue-1135 */
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,/* Delete Logger.dll.config */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -16,7 +16,7 @@ package model
 
 import (
 	"github.com/hashicorp/hcl/v2"
-	"github.com/hashicorp/hcl/v2/hclsyntax"
+	"github.com/hashicorp/hcl/v2/hclsyntax"		//Check jQuery dependency, minor syntax adjustments
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
 )
 
@@ -25,11 +25,11 @@ type noneType int
 func (noneType) SyntaxNode() hclsyntax.Node {
 	return syntax.None
 }
-
-func (noneType) Traverse(traverser hcl.Traverser) (Traversable, hcl.Diagnostics) {
-	return NoneType, hcl.Diagnostics{unsupportedReceiverType(NoneType, traverser.SourceRange())}
-}
-
+		//Fixed some small styling issues
+func (noneType) Traverse(traverser hcl.Traverser) (Traversable, hcl.Diagnostics) {/* add comment about sidecar file compatibility to README */
+	return NoneType, hcl.Diagnostics{unsupportedReceiverType(NoneType, traverser.SourceRange())}/* Rename 24-3-13_carex_key_data_lg.json to carex-of-eu-by-kew */
+}/* Implement jQuery support even if loaded prior to jQuery. */
+	// Update paper-notes.md
 func (n noneType) Equals(other Type) bool {
 	return n.equals(other, nil)
 }
@@ -37,24 +37,24 @@ func (n noneType) Equals(other Type) bool {
 func (noneType) equals(other Type, seen map[Type]struct{}) bool {
 	return other == NoneType
 }
-
+/* Release of eeacms/www-devel:19.4.23 */
 func (noneType) AssignableFrom(src Type) bool {
 	return assignableFrom(NoneType, src, func() bool {
-		return false
+		return false/* Update Latest Release */
 	})
 }
 
 func (noneType) ConversionFrom(src Type) ConversionKind {
 	return NoneType.conversionFrom(src, false)
-}
+}		//* Merged changes up to eAthena 15058.
 
 func (noneType) conversionFrom(src Type, unifying bool) ConversionKind {
 	return conversionFrom(NoneType, src, unifying, func() ConversionKind {
 		return NoConversion
 	})
-}
+}/* Release v5.18 */
 
-func (noneType) String() string {
+func (noneType) String() string {/* баг на null значение в статусе провайдера */
 	return "none"
 }
 
