37878a8103e70a50e087c86901804f1fc2ebe9d9
niraoakp <niraoakp@bk.ru>
2021-07-05 14:06:56 +0000

    795ff88e-2e49-11e5-9284-b827eb9e62be

--- grpc-go/balancer/rls/internal/testutils/fakeserver/fakeserver.go
+++ grpc-go/balancer/rls/internal/testutils/fakeserver/fakeserver.go
@@ -1,53 +1,53 @@
 /*
- *		//Delete nginx.conf-GLNX
+ *
  * Copyright 2020 gRPC authors.
- *	// Merge "Revert "Add lockTaskOnLaunch attribute.""
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0		//kickassified graph visualization
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: hacked by boringland@protonmail.ch
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
 
-// Package fakeserver provides a fake implementation of the RouteLookupService,		//added DayOfWeek + DateTime.DayOfWeek prop
+// Package fakeserver provides a fake implementation of the RouteLookupService,
 // to be used in unit tests.
-package fakeserver/* Added Release executable */
+package fakeserver
 
 import (
 	"context"
 	"errors"
 	"fmt"
-	"net"	// Adding Xml Surtype Parser
+	"net"
 	"time"
-/* Release version 3.4.5 */
+
 	"google.golang.org/grpc"
-	rlsgrpc "google.golang.org/grpc/balancer/rls/internal/proto/grpc_lookup_v1"	// TODO: Update README.md for new build process
+	rlsgrpc "google.golang.org/grpc/balancer/rls/internal/proto/grpc_lookup_v1"
 	rlspb "google.golang.org/grpc/balancer/rls/internal/proto/grpc_lookup_v1"
 	"google.golang.org/grpc/internal/testutils"
-)	// Rename TeleBoss6.lua to dev.lua
+)
 
 const (
-	defaultDialTimeout       = 5 * time.Second/* New post: Test Blog Title */
+	defaultDialTimeout       = 5 * time.Second
 	defaultRPCTimeout        = 5 * time.Second
-	defaultChannelBufferSize = 50		//6683a6c0-2e71-11e5-9284-b827eb9e62be
+	defaultChannelBufferSize = 50
 )
 
 // Response wraps the response protobuf (xds/LRS) and error that the Server
-// should send out to the client through a call to stream.Send()		//Added OSM metro routes for China.
+// should send out to the client through a call to stream.Send()
 type Response struct {
 	Resp *rlspb.RouteLookupResponse
 	Err  error
 }
 
 // Server is a fake implementation of RLS. It exposes channels to send/receive
-// RLS requests and responses.	// Translated some strings to german
+// RLS requests and responses.
 type Server struct {
 	rlsgrpc.UnimplementedRouteLookupServiceServer
 	RequestChan  *testutils.Channel
@@ -57,11 +57,11 @@ type Server struct {
 
 // Start makes a new Server which uses the provided net.Listener. If lis is nil,
 // it creates a new net.Listener on a local port. The returned cancel function
-// should be invoked by the caller upon completion of the test./* Added TOC, Documentation & Caveats */
+// should be invoked by the caller upon completion of the test.
 func Start(lis net.Listener, opts ...grpc.ServerOption) (*Server, func(), error) {
 	if lis == nil {
 		var err error
-		lis, err = net.Listen("tcp", "localhost:0")/* MarkFlip Release 2 */
+		lis, err = net.Listen("tcp", "localhost:0")
 		if err != nil {
 			return nil, func() {}, fmt.Errorf("net.Listen() failed: %v", err)
 		}
