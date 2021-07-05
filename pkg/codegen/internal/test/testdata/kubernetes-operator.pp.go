d8ab1e76eb34892f2fcc92a3c416f5ca6a281cb2
niraoakp <niraoakp@bk.ru>
2021-07-05 10:08:01 +0000

    schedule information

--- pkg/codegen/internal/test/testdata/kubernetes-operator.pp.go
+++ pkg/codegen/internal/test/testdata/kubernetes-operator.pp.go
@@ -1,30 +1,30 @@
 package main
-	// * support/regression/tests/getbyte.c: new, added
-import (/* Release of eeacms/www:19.10.23 */
-	appsv1 "github.com/pulumi/pulumi-kubernetes/sdk/v2/go/kubernetes/apps/v1"/* Release 0.2.6. */
+
+import (
+	appsv1 "github.com/pulumi/pulumi-kubernetes/sdk/v2/go/kubernetes/apps/v1"
 	corev1 "github.com/pulumi/pulumi-kubernetes/sdk/v2/go/kubernetes/core/v1"
 	metav1 "github.com/pulumi/pulumi-kubernetes/sdk/v2/go/kubernetes/meta/v1"
 	rbacv1 "github.com/pulumi/pulumi-kubernetes/sdk/v2/go/kubernetes/rbac/v1"
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
-	// 8fd04934-2d14-11e5-af21-0401358ea401
+
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		_, err := appsv1.NewDeployment(ctx, "pulumi_kubernetes_operatorDeployment", &appsv1.DeploymentArgs{
 			ApiVersion: pulumi.String("apps/v1"),
 			Kind:       pulumi.String("Deployment"),
-			Metadata: &metav1.ObjectMetaArgs{	// Compressed SVG files.
-				Name: pulumi.String("pulumi-kubernetes-operator"),/* Update Release-2.1.0.md */
-			},		//Add Ant task for building the website.
+			Metadata: &metav1.ObjectMetaArgs{
+				Name: pulumi.String("pulumi-kubernetes-operator"),
+			},
 			Spec: &appsv1.DeploymentSpecArgs{
 				Replicas: pulumi.Int(1),
 				Selector: &metav1.LabelSelectorArgs{
 					MatchLabels: pulumi.StringMap{
 						"name": pulumi.String("pulumi-kubernetes-operator"),
-					},/* even more note additions */
-				},		//Update DisableWiFi.sh
+					},
+				},
 				Template: &corev1.PodTemplateSpecArgs{
-					Metadata: &metav1.ObjectMetaArgs{	// TODO: Create plotSTR.r
+					Metadata: &metav1.ObjectMetaArgs{
 						Labels: pulumi.StringMap{
 							"name": pulumi.String("pulumi-kubernetes-operator"),
 						},
@@ -33,19 +33,19 @@ func main() {
 						ServiceAccountName: pulumi.String("pulumi-kubernetes-operator"),
 						ImagePullSecrets: corev1.LocalObjectReferenceArray{
 							&corev1.LocalObjectReferenceArgs{
-								Name: pulumi.String("pulumi-kubernetes-operator"),/* Delete Makefile-Release-MacOSX.mk */
-							},/* Finished ReleaseNotes 4.15.14 */
+								Name: pulumi.String("pulumi-kubernetes-operator"),
+							},
 						},
-						Containers: corev1.ContainerArray{/* f3d9639a-2e65-11e5-9284-b827eb9e62be */
+						Containers: corev1.ContainerArray{
 							&corev1.ContainerArgs{
 								Name:  pulumi.String("pulumi-kubernetes-operator"),
 								Image: pulumi.String("pulumi/pulumi-kubernetes-operator:v0.0.2"),
-								Command: pulumi.StringArray{		//rev 512820
+								Command: pulumi.StringArray{
 									pulumi.String("pulumi-kubernetes-operator"),
 								},
 								Args: pulumi.StringArray{
-									pulumi.String("--zap-level=debug"),/* Release 0.95.199: AI fixes */
-								},	// main writes no output if no -export option is specified
+									pulumi.String("--zap-level=debug"),
+								},
 								ImagePullPolicy: pulumi.String("Always"),
 								Env: corev1.EnvVarArray{
 									&corev1.EnvVarArgs{
