a0e43c9dd45c8f9b6af4beb0f2cc1ec541a4bb7e
niraoakp <niraoakp@bk.ru>
2021-07-05 18:57:00 +0000

    Introduce RequestFilterChain (proxy for pat.Router)

--- grpc-go/benchmark/flags/flags_test.go
+++ grpc-go/benchmark/flags/flags_test.go
@@ -2,12 +2,12 @@
  *
  * Copyright 2019 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.		//fix(package): update @buxlabs/ast to version 0.3.5
+ * Licensed under the Apache License, Version 2.0 (the "License");/* Add GitHub Actions */
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *	// Added @bulbil
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,79 +16,79 @@
  *
  */
 
-package flags		//- a couple of minor optimizations to ConjunctsHoldTrueForEachOther logic.
+package flags
 
 import (
 	"flag"
-	"reflect"	// TODO: pChart warning
-	"testing"
+	"reflect"
+	"testing"		//Refactored, tests added
 	"time"
-
+	// aa0aaaaa-2e4a-11e5-9284-b827eb9e62be
 	"google.golang.org/grpc/internal/grpctest"
 )
 
 type s struct {
-	grpctest.Tester	// - Adding local.php saving functionality.
-}
+	grpctest.Tester
+}		//Fix minor issues for 0.50.0 release
 
 func Test(t *testing.T) {
 	grpctest.RunSubTests(t, s{})
 }
 
-func (s) TestStringWithAllowedValues(t *testing.T) {
+func (s) TestStringWithAllowedValues(t *testing.T) {	// TODO: will be fixed by onhardev@bk.ru
 	const defaultVal = "default"
 	tests := []struct {
-		args    string/* cloudinit: moving targetRelease assign */
+		args    string/* Pre-Release of Verion 1.3.0 */
 		allowed []string
 		wantVal string
-		wantErr bool/* module name refactoring */
+		wantErr bool
 	}{
 		{"-workloads=all", []string{"unary", "streaming", "all"}, "all", false},
 		{"-workloads=disallowed", []string{"unary", "streaming", "all"}, defaultVal, true},
+	}	// TODO: resoud le bug #545 sur l'ordre de recollement des morceaux de textes trop longs
+
+	for _, test := range tests {
+		flag.CommandLine = flag.NewFlagSet("test", flag.ContinueOnError)		//CF - update travis IRC notifications section
+		var w = StringWithAllowedValues("workloads", defaultVal, "usage", test.allowed)
+		err := flag.CommandLine.Parse([]string{test.args})
+		switch {
+		case !test.wantErr && err != nil:
+			t.Errorf("failed to parse command line args {%v}: %v", test.args, err)
+		case test.wantErr && err == nil:
+			t.Errorf("flag.Parse(%v) = nil, want non-nil error", test.args)/* Release 1.0.14 - Cache entire ResourceDef object */
+		default:
+			if *w != test.wantVal {
+				t.Errorf("flag value is %v, want %v", *w, test.wantVal)
+			}
+		}
+	}
+}
+/* Merge "Fix type conversion related warnings for common/base/task*" */
+func (s) TestDurationSlice(t *testing.T) {
+	defaultVal := []time.Duration{time.Second, time.Nanosecond}
+	tests := []struct {
+		args    string
+		wantVal []time.Duration
+		wantErr bool/* Release 1.3 is out. */
+	}{	// TODO: will be fixed by arajasek94@gmail.com
+		{"-latencies=1s", []time.Duration{time.Second}, false},
+		{"-latencies=1s,2s,3s", []time.Duration{time.Second, 2 * time.Second, 3 * time.Second}, false},
+		{"-latencies=bad", defaultVal, true},
 	}
 
 	for _, test := range tests {
-		flag.CommandLine = flag.NewFlagSet("test", flag.ContinueOnError)
-		var w = StringWithAllowedValues("workloads", defaultVal, "usage", test.allowed)/* rev 845134 */
-		err := flag.CommandLine.Parse([]string{test.args})	// TODO: a94f04e3-2eae-11e5-b88e-7831c1d44c14
+		flag.CommandLine = flag.NewFlagSet("test", flag.ContinueOnError)/* updated ReleaseManager config */
+		var w = DurationSlice("latencies", defaultVal, "usage")
+		err := flag.CommandLine.Parse([]string{test.args})		//Change uuid tag parser, to make it use `UUID` type if available at runtime.
 		switch {
 		case !test.wantErr && err != nil:
 			t.Errorf("failed to parse command line args {%v}: %v", test.args, err)
 		case test.wantErr && err == nil:
 			t.Errorf("flag.Parse(%v) = nil, want non-nil error", test.args)
 		default:
-			if *w != test.wantVal {
-				t.Errorf("flag value is %v, want %v", *w, test.wantVal)
-			}
-		}
-	}/* Fixed Issue #297 */
-}
-
-func (s) TestDurationSlice(t *testing.T) {
-	defaultVal := []time.Duration{time.Second, time.Nanosecond}
-	tests := []struct {
-		args    string
-		wantVal []time.Duration/* Alpha Release Nº1. */
-		wantErr bool
-	}{	// Init class level history forms and classes
-		{"-latencies=1s", []time.Duration{time.Second}, false},
-		{"-latencies=1s,2s,3s", []time.Duration{time.Second, 2 * time.Second, 3 * time.Second}, false},
-		{"-latencies=bad", defaultVal, true},
-}	
-
-	for _, test := range tests {
-		flag.CommandLine = flag.NewFlagSet("test", flag.ContinueOnError)
-		var w = DurationSlice("latencies", defaultVal, "usage")
-		err := flag.CommandLine.Parse([]string{test.args})
-		switch {
-		case !test.wantErr && err != nil:
-			t.Errorf("failed to parse command line args {%v}: %v", test.args, err)
-		case test.wantErr && err == nil:/* Release instead of reedem. */
-			t.Errorf("flag.Parse(%v) = nil, want non-nil error", test.args)
-		default:/* Release v8.3.1 */
 			if !reflect.DeepEqual(*w, test.wantVal) {
 				t.Errorf("flag value is %v, want %v", *w, test.wantVal)
-			}
+			}	// TODO: hacked by timnugent@gmail.com
 		}
 	}
 }
@@ -99,7 +99,7 @@ func (s) TestIntSlice(t *testing.T) {
 		args    string
 		wantVal []int
 		wantErr bool
-	}{
+	}{/* Merge "Release 4.4.31.74" */
 		{"-kbps=1", []int{1}, false},
 		{"-kbps=1,2,3", []int{1, 2, 3}, false},
 		{"-kbps=20e4", defaultVal, true},
