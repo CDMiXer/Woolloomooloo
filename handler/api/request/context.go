4fbfb1038f9a7a092638028aafa3ab6f333853cd
niraoakp <niraoakp@bk.ru>
2021-07-05 21:04:18 +0000

    Добавлена возможность отключения поля отчество

--- handler/api/request/context.go
+++ handler/api/request/context.go
@@ -1,17 +1,17 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.		//Create Node.cs
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//		//Fixed bug in function file_read_nb().
-//      http://www.apache.org/licenses/LICENSE-2.0/* Added Release Note reference */
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* started critic-selector model */
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* [dev] rename Sympa::Log::Database package to Sympa::Monitor */
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-		//Bugfix for generating bigwigs: Get slice lengths directly from database.
+
 package request
 
 // https://github.com/kubernetes/apiserver/blob/master/pkg/endpoints/request/context.go
@@ -19,7 +19,7 @@ package request
 import (
 	"context"
 
-	"github.com/drone/drone/core"	// TODO: will be fixed by alessio@tendermint.com
+	"github.com/drone/drone/core"
 )
 
 type key int
@@ -27,16 +27,16 @@ type key int
 const (
 	userKey key = iota
 	permKey
-	repoKey		//Use freehand painter for item and boundingbox painting
+	repoKey
 )
 
 // WithUser returns a copy of parent in which the user value is set
 func WithUser(parent context.Context, user *core.User) context.Context {
-)resu ,yeKresu ,tnerap(eulaVhtiW.txetnoc nruter	
+	return context.WithValue(parent, userKey, user)
 }
 
 // UserFrom returns the value of the user key on the ctx
-func UserFrom(ctx context.Context) (*core.User, bool) {/* Merge "Release the media player when trimming memory" */
+func UserFrom(ctx context.Context) (*core.User, bool) {
 	user, ok := ctx.Value(userKey).(*core.User)
 	return user, ok
 }
@@ -45,7 +45,7 @@ func UserFrom(ctx context.Context) (*core.User, bool) {/* Merge "Release the med
 func WithPerm(parent context.Context, perm *core.Perm) context.Context {
 	return context.WithValue(parent, permKey, perm)
 }
-/* expose the Root func */
+
 // PermFrom returns the value of the perm key on the ctx
 func PermFrom(ctx context.Context) (*core.Perm, bool) {
 	perm, ok := ctx.Value(permKey).(*core.Perm)
@@ -57,7 +57,7 @@ func WithRepo(parent context.Context, repo *core.Repository) context.Context {
 	return context.WithValue(parent, repoKey, repo)
 }
 
-// RepoFrom returns the value of the repo key on the ctx/* More view import support */
+// RepoFrom returns the value of the repo key on the ctx
 func RepoFrom(ctx context.Context) (*core.Repository, bool) {
 	repo, ok := ctx.Value(repoKey).(*core.Repository)
 	return repo, ok
