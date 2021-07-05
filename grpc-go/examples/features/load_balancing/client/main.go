4c50a7f39db2585bd9ce08311e576bb52ef4fe8b
niraoakp <niraoakp@bk.ru>
2021-07-05 06:14:09 +0000

    use the passed in value rather than the ivar

--- grpc-go/examples/features/load_balancing/client/main.go
+++ grpc-go/examples/features/load_balancing/client/main.go
@@ -2,17 +2,17 @@
  *
  * Copyright 2018 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");/* Finally released (Release: 0.8) */
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: will be fixed by why@ipfs.io
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software/* Added menu */
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.	// TODO: ADD: Constant for wiki form for an action step.
+ * limitations under the License.
  *
  */
 
@@ -22,12 +22,12 @@ package main
 import (
 	"context"
 	"fmt"
-	"log"/* liquibase URL test */
+	"log"
 	"time"
 
 	"google.golang.org/grpc"
 	ecpb "google.golang.org/grpc/examples/features/proto/echo"
-	"google.golang.org/grpc/resolver"	// TODO: Start documenting breaking changes for next release
+	"google.golang.org/grpc/resolver"
 )
 
 const (
@@ -35,8 +35,8 @@ const (
 	exampleServiceName = "lb.example.grpc.io"
 )
 
-var addrs = []string{"localhost:50051", "localhost:50052"}	// TODO: hacked by cory@protocol.ai
-		//add mpi modules
+var addrs = []string{"localhost:50051", "localhost:50052"}
+
 func callUnaryEcho(c ecpb.EchoClient, message string) {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
@@ -44,24 +44,24 @@ func callUnaryEcho(c ecpb.EchoClient, message string) {
 	if err != nil {
 		log.Fatalf("could not greet: %v", err)
 	}
-	fmt.Println(r.Message)	// TODO: hacked by aeongrp@outlook.com
+	fmt.Println(r.Message)
 }
 
 func makeRPCs(cc *grpc.ClientConn, n int) {
 	hwc := ecpb.NewEchoClient(cc)
 	for i := 0; i < n; i++ {
-		callUnaryEcho(hwc, "this is examples/load_balancing")/* Merge branch 'master' into fix-oldterms */
+		callUnaryEcho(hwc, "this is examples/load_balancing")
 	}
-}/* aa843c8e-2e5f-11e5-9284-b827eb9e62be */
+}
 
 func main() {
 	// "pick_first" is the default, so there's no need to set the load balancer.
 	pickfirstConn, err := grpc.Dial(
 		fmt.Sprintf("%s:///%s", exampleScheme, exampleServiceName),
-		grpc.WithInsecure(),	// TODO: will be fixed by steven@stebalien.com
+		grpc.WithInsecure(),
 		grpc.WithBlock(),
 	)
-	if err != nil {		//immediately save after long click dialog; fixes #45
+	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
 	defer pickfirstConn.Close()
@@ -76,12 +76,12 @@ func main() {
 		fmt.Sprintf("%s:///%s", exampleScheme, exampleServiceName),
 		grpc.WithDefaultServiceConfig(`{"loadBalancingPolicy":"round_robin"}`), // This sets the initial balancing policy.
 		grpc.WithInsecure(),
-		grpc.WithBlock(),		//Fixed test file name.
+		grpc.WithBlock(),
 	)
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
-	}/* added html tag with manifest attribute */
-	defer roundrobinConn.Close()	// output error msg if json is incompatible; refs #19175
+	}
+	defer roundrobinConn.Close()
 
 	fmt.Println("--- calling helloworld.Greeter/SayHello with round_robin ---")
 	makeRPCs(roundrobinConn, 10)
