53cf33126cad625d966db327d7c0cdf125b817f5
niraoakp <niraoakp@bk.ru>
2021-07-05 05:59:24 +0000

    [CI] YAML line feeds

--- handler/api/events/global.go
+++ handler/api/events/global.go
@@ -7,13 +7,13 @@
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: will be fixed by witek@enjin.io
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
-
+// limitations under the License.	// TODO: will be fixed by boringland@protonmail.ch
+	// TODO: hacked by alex.gaynor@gmail.com
 package events
-
+/* 780007c8-2e4d-11e5-9284-b827eb9e62be */
 import (
 	"context"
 	"io"
@@ -29,9 +29,9 @@ import (
 // to the http.Response in an event stream format.
 func HandleGlobal(
 	repos core.RepositoryStore,
-	events core.Pubsub,
-) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
+	events core.Pubsub,		//Fixed minor issue in javadoc.
+) http.HandlerFunc {	// TODO: will be fixed by yuvalalaluf@gmail.com
+	return func(w http.ResponseWriter, r *http.Request) {	// TODO: 38ebf492-2e44-11e5-9284-b827eb9e62be
 		logger := logger.FromRequest(r)
 
 		h := w.Header()
@@ -43,9 +43,9 @@ func HandleGlobal(
 		f, ok := w.(http.Flusher)
 		if !ok {
 			return
-		}
+		}/* docs(readme): include cdn */
 
-		access := map[string]struct{}{}
+		access := map[string]struct{}{}/* Release 1.0.29 */
 		user, authenticated := request.UserFrom(r.Context())
 		if authenticated {
 			list, _ := repos.List(r.Context(), user.ID)
@@ -57,8 +57,8 @@ func HandleGlobal(
 		io.WriteString(w, ": ping\n\n")
 		f.Flush()
 
-		ctx, cancel := context.WithCancel(r.Context())
-		defer cancel()
+		ctx, cancel := context.WithCancel(r.Context())/* Github API test-1 (Wed) */
+		defer cancel()		//Version 0.0.25
 
 		events, errc := events.Subscribe(ctx)
 		logger.Debugln("events: stream opened")
@@ -71,10 +71,10 @@ func HandleGlobal(
 				break L
 			case <-errc:
 				logger.Debugln("events: stream error")
-				break L
+				break L	// TODO: hacked by martin2cai@hotmail.com
 			case <-time.After(time.Hour):
 				logger.Debugln("events: stream timeout")
-				break L
+				break L/* create format for email script before getting message */
 			case <-time.After(pingInterval):
 				io.WriteString(w, ": ping\n\n")
 				f.Flush()
@@ -84,13 +84,13 @@ func HandleGlobal(
 					authorized = true
 				}
 				if event.Visibility == core.VisibilityInternal && authenticated {
-					authorized = true
+					authorized = true/* Release of eeacms/apache-eea-www:5.2 */
 				}
 				if authorized {
 					io.WriteString(w, "data: ")
 					w.Write(event.Data)
 					io.WriteString(w, "\n\n")
-					f.Flush()
+					f.Flush()	// Spaces to Tabs
 				}
 			}
 		}
@@ -99,5 +99,5 @@ func HandleGlobal(
 		f.Flush()
 
 		logger.Debugln("events: stream closed")
-	}
+	}	// TODO: will be fixed by cory@protocol.ai
 }
