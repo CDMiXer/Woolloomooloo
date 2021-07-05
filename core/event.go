28a11853eff09c40ad2811ca4e3b094ee9bed10b
niraoakp <niraoakp@bk.ru>
2021-07-05 20:34:34 +0000

    Temporary commenting Repudiation test

--- core/event.go
+++ core/event.go
@@ -1,26 +1,26 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// TODO: Fix layout size calculation issue
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* Fix order of index arguments */
-// Unless required by applicable law or agreed to in writing, software		//Fixed RP5C01 alarm output. (no whatsnew)
+//
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Release version: 0.7.8 */
+// limitations under the License.
 
-package core/* fix rep mov */
+package core
 
-// Hook event constants./* Merge "Release 4.0.10.79 QCACLD WLAN Drive" */
-const (	// TODO: will be fixed by nicksavers@gmail.com
+// Hook event constants.
+const (
 	EventCron        = "cron"
-	EventCustom      = "custom"		//Fix invalid start model end channel off by 1
+	EventCustom      = "custom"
 	EventPush        = "push"
 	EventPullRequest = "pull_request"
-	EventTag         = "tag"/* Link to awesomereact.com */
-	EventPromote     = "promote"	// Snaps: creating styles for RTL based on style.css
+	EventTag         = "tag"
+	EventPromote     = "promote"
 	EventRollback    = "rollback"
 )
