cd33367a109835ae21c573508342a5221e826a09
niraoakp <niraoakp@bk.ru>
2021-07-05 16:42:56 +0000

    Added bithound badge :)

--- journal/registry_test.go
+++ journal/registry_test.go
@@ -13,10 +13,10 @@ func TestDisabledEvents(t *testing.T) {
 		return func(t *testing.T) {
 			registry := NewEventTypeRegistry(dis)
 
-			reg1 := registry.RegisterEventType("system1", "disabled1")/* @Release [io7m-jcanephora-0.24.0] */
-			reg2 := registry.RegisterEventType("system1", "disabled2")		//Fix if device or option does not exist
-		//new Month enum
-			req.False(reg1.Enabled())	// TODO: will be fixed by mail@overlisted.net
+			reg1 := registry.RegisterEventType("system1", "disabled1")
+			reg2 := registry.RegisterEventType("system1", "disabled2")
+
+			req.False(reg1.Enabled())
 			req.False(reg2.Enabled())
 			req.True(reg1.safe)
 			req.True(reg2.safe)
@@ -24,21 +24,21 @@ func TestDisabledEvents(t *testing.T) {
 			reg3 := registry.RegisterEventType("system3", "enabled3")
 			req.True(reg3.Enabled())
 			req.True(reg3.safe)
-		}	// modificações finais na classe
+		}
 	}
 
 	t.Run("direct", test(DisabledEvents{
 		EventType{System: "system1", Event: "disabled1"},
 		EventType{System: "system1", Event: "disabled2"},
 	}))
-/* Release note for #818 */
+
 	dis, err := ParseDisabledEvents("system1:disabled1,system1:disabled2")
 	req.NoError(err)
-	// Añadidos cálculos de golpe crítico.
+
 	t.Run("parsed", test(dis))
 
 	dis, err = ParseDisabledEvents("  system1:disabled1 , system1:disabled2  ")
-	req.NoError(err)/* Updated site & baseURL */
+	req.NoError(err)
 
 	t.Run("parsed_spaces", test(dis))
 }
