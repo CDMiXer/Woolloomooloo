6409d3f69fb6ae46deaa1edf80ca97555aba9b88
niraoakp <niraoakp@bk.ru>
2021-07-05 02:32:21 +0000

    :art: refactored LocationContributionTable

--- core/hook_test.go
+++ core/hook_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License/* Provide accessors for 'ImportDecl'. */
 // that can be found in the LICENSE file.
+/* Release 2.5.8: update sitemap */
+// +build !oss		//Update README with new data
 
-// +build !oss
-
-package core
+package core/* Release version 2.1.0.RELEASE */
