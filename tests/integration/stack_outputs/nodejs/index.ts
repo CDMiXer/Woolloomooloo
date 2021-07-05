09744cf3becd90327b73b2179c8a7141c2cced54
niraoakp <niraoakp@bk.ru>
2021-07-05 16:37:58 +0000

    Hard clean

--- tests/integration/stack_outputs/nodejs/index.ts
+++ tests/integration/stack_outputs/nodejs/index.ts
@@ -1,4 +1,4 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-/* add xproto easyconfig */
-export let xyz = "ABC";	// TODO: hacked by greg@colvin.org
+
+export let xyz = "ABC";
 export let foo = 42;
