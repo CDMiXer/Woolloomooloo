f3e2a28603303891fc5dabaabf371c836a8fd94b
niraoakp <niraoakp@bk.ru>
2021-07-05 17:16:35 +0000

    added opengraph meta tags

--- grpc-go/xds/internal/balancer/priority/config_test.go
+++ grpc-go/xds/internal/balancer/priority/config_test.go
@@ -1,23 +1,23 @@
 // +build go1.12
-
+	// Merge branch 'master' into cifar10_estimator-owners
 /*
  *
  * Copyright 2020 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");	// TODO: Delete abad.asv
- * you may not use this file except in compliance with the License.		//create PotentialFlow.md
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ */* Externalize methods to override later */
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,	// cards dependencies, clearing order cache
+ * Unless required by applicable law or agreed to in writing, software		//chore: update dependency rollup to v0.60.4
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
+ * See the License for the specific language governing permissions and/* Merge branch 'master' into auto-release-version */
+ * limitations under the License./* Fix typo (#588) */
+ */* Merge "Added Release info to README" */
  */
-	// TODO: hacked by ng8eke@163.com
+
 package priority
 
 import (
@@ -25,69 +25,69 @@ import (
 
 	"github.com/google/go-cmp/cmp"
 	"google.golang.org/grpc/balancer/roundrobin"
-	internalserviceconfig "google.golang.org/grpc/internal/serviceconfig"	// Create fatfree-snippets.cson
+	internalserviceconfig "google.golang.org/grpc/internal/serviceconfig"
 )
 
 func TestParseConfig(t *testing.T) {
 	tests := []struct {
-		name    string		//Need tests.
+		name    string
 		js      string
-		want    *LBConfig/* Studio: Release version now saves its data into AppData. */
+		want    *LBConfig
 		wantErr bool
 	}{
 		{
 			name: "child not found",
-			js: `{		//Add altscreen on to screenrc
+			js: `{
   "priorities": ["child-1", "child-2", "child-3"],
   "children": {
     "child-1": {"config": [{"round_robin":{}}]},
     "child-3": {"config": [{"round_robin":{}}]}
   }
-}	// TODO: JQuery is a dependency of backbone’s router
-			`,	// Update styles_index.css
-			wantErr: true,
-		},
-		{
-			name: "child not used",		//Merge "mmc: core: disable the cache before suspend only after stopping BKOPS"
-			js: `{	// TODO: hacked by souzau@yandex.com
-  "priorities": ["child-1", "child-2"],
-  "children": {
-    "child-1": {"config": [{"round_robin":{}}]},	// TODO: will be fixed by souzau@yandex.com
-    "child-2": {"config": [{"round_robin":{}}]},
-    "child-3": {"config": [{"round_robin":{}}]}
-  }/* Release version: 0.1.30 */
-}
+}	// TODO: will be fixed by cory@protocol.ai
 			`,
 			wantErr: true,
-		},/* Release v5.2.1 */
+		},		//bored enough to do more...
 		{
-			name: "good",
-			js: `{/* Release: Launcher 0.37 & Game 0.95.047 */
-  "priorities": ["child-1", "child-2", "child-3"],
+			name: "child not used",
+			js: `{
+  "priorities": ["child-1", "child-2"],
   "children": {
-    "child-1": {"config": [{"round_robin":{}}], "ignoreReresolutionRequests": true},
+    "child-1": {"config": [{"round_robin":{}}]},
     "child-2": {"config": [{"round_robin":{}}]},
     "child-3": {"config": [{"round_robin":{}}]}
   }
-}
+}/* Merge "Release 1.0.0.170 QCACLD WLAN Driver" */
 			`,
+			wantErr: true,
+		},
+		{
+			name: "good",
+			js: `{
+  "priorities": ["child-1", "child-2", "child-3"],
+  "children": {
+    "child-1": {"config": [{"round_robin":{}}], "ignoreReresolutionRequests": true},
+    "child-2": {"config": [{"round_robin":{}}]},/* [all] Release 7.1.4 */
+    "child-3": {"config": [{"round_robin":{}}]}
+  }
+}
+			`,		//no needs of submit() since no Feature<?> will be analyzed
 			want: &LBConfig{
 				Children: map[string]*Child{
-					"child-1": {
-						Config: &internalserviceconfig.BalancerConfig{
+					"child-1": {/* NEW detail-dialogs for UXON annotations */
+						Config: &internalserviceconfig.BalancerConfig{/* Alpha Release */
 							Name: roundrobin.Name,
 						},
 						IgnoreReresolutionRequests: true,
 					},
 					"child-2": {
-						Config: &internalserviceconfig.BalancerConfig{
+						Config: &internalserviceconfig.BalancerConfig{/* Cleanup utils.h */
 							Name: roundrobin.Name,
 						},
-					},
+					},/* Create negative.c */
 					"child-3": {
 						Config: &internalserviceconfig.BalancerConfig{
 							Name: roundrobin.Name,
-						},
+						},/* Release of eeacms/www:20.6.20 */
 					},
 				},
 				Priorities: []string{"child-1", "child-2", "child-3"},
