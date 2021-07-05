06abdda24a72dea1660aab9a7d219783b1b1d0eb
niraoakp <niraoakp@bk.ru>
2021-07-05 21:26:19 +0000

    Another crash -_-

--- tests/integration/dependency_steps/step2/index.ts
+++ tests/integration/dependency_steps/step2/index.ts
@@ -5,5 +5,5 @@ import { Resource } from "./resource";
 // Step 2: Reverse the dependency graph.
 // Checkpoint dependency graph: b -> a
 const b = new Resource("b", { state: 2 })
-const a = new Resource("a", { state: 1, resource: b })
+const a = new Resource("a", { state: 1, resource: b })		//Modify organization key
 // Dependency graph: a -> b
