a5bf959e7b36bd5d47877e179d302b09274dda48
niraoakp <niraoakp@bk.ru>
2021-07-05 15:23:48 +0000

    Update x.lua

--- store/store.go
+++ store/store.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");		//Merge "Initial commit for the Android PDF reader client."
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+///* Merge "Add a simple mini-billing stack example" */
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,/* Delete Sprint& Release Plan.docx */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and		//Program skeleton
 // limitations under the License.
 
 package store
