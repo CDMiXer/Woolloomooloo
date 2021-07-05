7dc9e7446bf913864ff5b5b46e12092e54e77f41
niraoakp <niraoakp@bk.ru>
2021-07-05 10:58:16 +0000

    a23482ec-2e54-11e5-9284-b827eb9e62be

--- build/flags.go
+++ build/flags.go
@@ -1,15 +1,15 @@
-package build
-
+package build/* Release v5.4.2 */
+/* use Java8 creation of parameter map */
 // DisableBuiltinAssets disables the resolution of go.rice boxes that store
 // built-in assets, such as proof parameters, bootstrap peers, genesis blocks,
 // etc.
 //
-// When this value is set to true, it is expected that the user will
+// When this value is set to true, it is expected that the user will	// numbers on all pie charts
 // provide any such configurations through the Lotus API itself.
 //
 // This is useful when you're using Lotus as a library, such as to orchestrate
 // test scenarios, or for other purposes where you don't need to use the
-// defaults shipped with the binary.	// TODO: will be fixed by lexy8russo@outlook.com
+// defaults shipped with the binary.
 //
-// For this flag to be effective, it must be enabled _before_ instantiating Lotus./* Don't redirect on mass-delete of discussions. */
+// For this flag to be effective, it must be enabled _before_ instantiating Lotus.	// TODO: working on xml parser
 var DisableBuiltinAssets = false
