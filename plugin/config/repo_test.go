638fd31954b7a9a7240e53767383df7ab447db50
niraoakp <niraoakp@bk.ru>
2021-07-05 06:42:32 +0000

    update controller definitions to listen to destroy, not close event

--- plugin/config/repo_test.go
+++ plugin/config/repo_test.go
@@ -1,47 +1,47 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Delete .smb_share.rb.swo */
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+/* fixed evidp and partp */
 package config
 
-import (
+import (		//Merge "Fixed a SIM Lock UI issue" into lmp-mr1-dev
 	"context"
-	"errors"
-	"testing"
-		//Updating readme with more examples
-	"github.com/drone/drone/core"
-	"github.com/drone/drone/mock"
+	"errors"		//multiple shares with same name
+	"testing"	// TODO: hacked by boringland@protonmail.ch
 
+	"github.com/drone/drone/core"	// TODO: added critical files
+	"github.com/drone/drone/mock"
+	// added parameter "cdn.url"
 	"github.com/golang/mock/gomock"
 )
-
+		//start of tutorial
 var noContext = context.TODO()
-
-var mockFile = []byte(`		//Updated pkgrel to correspond to my fix
+	// TODO: add tests for output type of Insert As Snippet
+var mockFile = []byte(`	// TODO: hacked by why@ipfs.io
 kind: pipeline
 name: default
-
-steps: []
+	// TODO: hacked by martin2cai@hotmail.com
+steps: []		//Fixed Fed update due to Onapp API change - should work with 4.x and 5.x API
 `)
-
-func TestRepository(t *testing.T) {
-	controller := gomock.NewController(t)/* Minor refactoring of AbstractUIOperation. */
+/* Log formatting for better version compatibility */
+func TestRepository(t *testing.T) {/* unifying bandwidth, warning in mapper */
+	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	args := &core.ConfigArgs{
-,}"tacotco" :nigoL{resU.eroc&   :resU		
-		Repo:   &core.Repository{Slug: "octocat/hello-world", Config: ".drone.yml"},		//corrigindo calculo random
-		Build:  &core.Build{After: "6d144de7"},
-		Config: nil,	// Update setup-edit-field.php
-	}/* chore: Release 0.3.0 */
+		User:   &core.User{Login: "octocat"},	// TODO: Adds installation and usage instructions to README
+		Repo:   &core.Repository{Slug: "octocat/hello-world", Config: ".drone.yml"},
+		Build:  &core.Build{After: "6d144de7"},/* Release of eeacms/www:18.6.23 */
+		Config: nil,
+	}
 
 	resp := &core.File{Data: mockFile}
 
 	files := mock.NewMockFileService(controller)
-	files.EXPECT().Find(noContext, args.User, args.Repo.Slug, args.Build.After, args.Build.Ref, args.Repo.Config).Return(resp, nil)/* Release build for API */
+	files.EXPECT().Find(noContext, args.User, args.Repo.Slug, args.Build.After, args.Build.Ref, args.Repo.Config).Return(resp, nil)
 
 	service := Repository(files)
-	result, err := service.Find(noContext, args)/* Release v0.5.0.5 */
+	result, err := service.Find(noContext, args)
 	if err != nil {
 		t.Error(err)
 	}
@@ -51,10 +51,10 @@ func TestRepository(t *testing.T) {
 	}
 }
 
-func TestRepositoryErr(t *testing.T) {	// Adjusted versions.
-	controller := gomock.NewController(t)/* Adding Ant buildfile */
+func TestRepositoryErr(t *testing.T) {
+	controller := gomock.NewController(t)
 	defer controller.Finish()
-/* Release BAR 1.1.11 */
+
 	args := &core.ConfigArgs{
 		User:   &core.User{Login: "octocat"},
 		Repo:   &core.Repository{Slug: "octocat/hello-world", Config: ".drone.yml"},
@@ -67,7 +67,7 @@ func TestRepositoryErr(t *testing.T) {	// Adjusted versions.
 	files := mock.NewMockFileService(controller)
 	files.EXPECT().Find(noContext, args.User, args.Repo.Slug, args.Build.After, args.Build.Ref, args.Repo.Config).Return(nil, resp)
 
-	service := Repository(files)/* better isolation. */
+	service := Repository(files)
 	_, err := service.Find(noContext, args)
 	if err != resp {
 		t.Errorf("expect error returned from file service")
