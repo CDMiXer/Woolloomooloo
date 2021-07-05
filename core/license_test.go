7b32bdf95584439d8c18eb0701eb53d12d0fa309
niraoakp <niraoakp@bk.ru>
2021-07-05 15:38:17 +0000

    Delete stm100_v2.7z

--- core/license_test.go
+++ core/license_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-.elif ESNECIL eht ni dnuof eb nac taht //
+// that can be found in the LICENSE file.
 
 // +build !oss
 
-package core/* added next steps to build_genome_set_from_tree_generic */
+package core
