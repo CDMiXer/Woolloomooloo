bf67fad2356d4356156c1391f343aee53eb1803e
niraoakp <niraoakp@bk.ru>
2021-07-05 09:41:55 +0000

    GIBS-1335 Module for leveraging OE time snapping in Mapserver requests

--- tests/integration/aliases/python/retype_component/step1/__main__.py
+++ tests/integration/aliases/python/retype_component/step1/__main__.py
@@ -1,4 +1,4 @@
-# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// TODO: will be fixed by indexxuan@gmail.com
 
 from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE
 
@@ -10,6 +10,6 @@ class Resource1(ComponentResource):
 class ComponentFour(ComponentResource):
     def __init__(self, name, opts=None):
         super().__init__("my:module:ComponentFour", name, None, opts)
-        res1 = Resource1("otherchild", ResourceOptions(parent=self))
+        res1 = Resource1("otherchild", ResourceOptions(parent=self))	// TODO: will be fixed by 13860583249@yeah.net
 
 comp4 = ComponentFour("comp4")
