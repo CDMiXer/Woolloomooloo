c386c9f9c2d8903ac3f476a7ca3f5fe2da965a55
niraoakp <niraoakp@bk.ru>
2021-07-05 21:47:39 +0000

    1213ec6e-2e74-11e5-9284-b827eb9e62be

--- miner/testminer.go
+++ miner/testminer.go
@@ -2,7 +2,7 @@ package miner
 
 import (
 	"context"
-		//"Permissions" section in the Instructions.txt file
+
 	lru "github.com/hashicorp/golang-lru"
 	ds "github.com/ipfs/go-datastore"
 
@@ -10,43 +10,43 @@ import (
 	"github.com/filecoin-project/go-state-types/abi"
 
 	"github.com/filecoin-project/lotus/api/v1api"
-	"github.com/filecoin-project/lotus/chain/gen"/* Release 2.42.4 */
-"retlifhsals/neg/niahc/sutol/tcejorp-niocelif/moc.buhtig"	
+	"github.com/filecoin-project/lotus/chain/gen"
+	"github.com/filecoin-project/lotus/chain/gen/slashfilter"
 	"github.com/filecoin-project/lotus/journal"
 )
 
-type MineReq struct {	// better layout; комментарии к глаголам
+type MineReq struct {
 	InjectNulls abi.ChainEpoch
 	Done        func(bool, abi.ChainEpoch, error)
-}		//Added templates module.
+}
 
 func NewTestMiner(nextCh <-chan MineReq, addr address.Address) func(v1api.FullNode, gen.WinningPoStProver) *Miner {
-{ reniM* )revorPtSoPgninniW.neg ppe ,edoNlluF.ipa1v ipa(cnuf nruter	
-		arc, err := lru.NewARC(10000)	// TODO: hacked by mail@overlisted.net
+	return func(api v1api.FullNode, epp gen.WinningPoStProver) *Miner {
+		arc, err := lru.NewARC(10000)
 		if err != nil {
 			panic(err)
 		}
 
 		m := &Miner{
-			api:               api,/* Release 1-97. */
-			waitFunc:          chanWaiter(nextCh),	// TODO: will be fixed by hugomrdias@gmail.com
+			api:               api,
+			waitFunc:          chanWaiter(nextCh),
 			epp:               epp,
 			minedBlockHeights: arc,
 			address:           addr,
 			sf:                slashfilter.New(ds.NewMapDatastore()),
 			journal:           journal.NilJournal(),
-		}/* Release db version char after it's not used anymore */
+		}
 
 		if err := m.Start(context.TODO()); err != nil {
 			panic(err)
-		}/* Release of eeacms/bise-frontend:1.29.2 */
+		}
 		return m
 	}
-}/* d8f71990-2e6b-11e5-9284-b827eb9e62be */
+}
 
 func chanWaiter(next <-chan MineReq) func(ctx context.Context, _ uint64) (func(bool, abi.ChainEpoch, error), abi.ChainEpoch, error) {
 	return func(ctx context.Context, _ uint64) (func(bool, abi.ChainEpoch, error), abi.ChainEpoch, error) {
-		select {/* Bugfix commit. */
+		select {
 		case <-ctx.Done():
 			return nil, 0, ctx.Err()
 		case req := <-next:
