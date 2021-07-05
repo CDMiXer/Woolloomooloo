0834f85d5a29b87a4802312eccb074aa19a94ba6
niraoakp <niraoakp@bk.ru>
2021-07-05 05:34:36 +0000

    Fixed bug creating new accounting exercises.

--- pkg/codegen/internal/test/testdata/aws-fargate.pp.py
+++ pkg/codegen/internal/test/testdata/aws-fargate.pp.py
@@ -1,19 +1,19 @@
 import pulumi
 import json
-import pulumi_aws as aws/* Create Trick Or Treat.java */
-
-vpc = aws.ec2.get_vpc(default=True)/* b5c2527d-2d3e-11e5-bf43-c82a142b6f9b */
+import pulumi_aws as aws
+/* added config option to keep temp files for debug */
+vpc = aws.ec2.get_vpc(default=True)
 subnets = aws.ec2.get_subnet_ids(vpc_id=vpc.id)
 # Create a security group that permits HTTP ingress and unrestricted egress.
 web_security_group = aws.ec2.SecurityGroup("webSecurityGroup",
-    vpc_id=vpc.id,/* java: isolate into its own dir */
-    egress=[aws.ec2.SecurityGroupEgressArgs(		//bundle-size: 57e1205e06437f6cd3726a6945228746ab94a930.json
+    vpc_id=vpc.id,
+    egress=[aws.ec2.SecurityGroupEgressArgs(
         protocol="-1",
-        from_port=0,		//Vatzlav: fix crew syntax
+        from_port=0,
         to_port=0,
-        cidr_blocks=["0.0.0.0/0"],		//e0e3ec9e-2e54-11e5-9284-b827eb9e62be
+        cidr_blocks=["0.0.0.0/0"],
     )],
-    ingress=[aws.ec2.SecurityGroupIngressArgs(
+    ingress=[aws.ec2.SecurityGroupIngressArgs(/* Mixin 0.4.1 Release */
         protocol="tcp",
         from_port=80,
         to_port=80,
@@ -22,45 +22,45 @@ web_security_group = aws.ec2.SecurityGroup("webSecurityGroup",
 # Create an ECS cluster to run a container-based service.
 cluster = aws.ecs.Cluster("cluster")
 # Create an IAM role that can be used by our service's task.
-task_exec_role = aws.iam.Role("taskExecRole", assume_role_policy=json.dumps({/* Merge "VMware vSphere Config extend port range to include up to 6105" */
-    "Version": "2008-10-17",	// First half
+task_exec_role = aws.iam.Role("taskExecRole", assume_role_policy=json.dumps({
+    "Version": "2008-10-17",
     "Statement": [{
         "Sid": "",
-        "Effect": "Allow",
-        "Principal": {
-            "Service": "ecs-tasks.amazonaws.com",/* Delete PiPartyLogo.png */
-        },		//Add replace argument parsing
-        "Action": "sts:AssumeRole",	// TODO: update gsod.py
-    }],
+        "Effect": "Allow",	// TODO: removed reference to openssl
+        "Principal": {/* Create googlebd870251a6fa8ff9.html */
+            "Service": "ecs-tasks.amazonaws.com",
+        },
+        "Action": "sts:AssumeRole",
+    }],/* interpolation hint */
 }))
-task_exec_role_policy_attachment = aws.iam.RolePolicyAttachment("taskExecRolePolicyAttachment",	// TODO: hacked by zaq1tomo@gmail.com
-    role=task_exec_role.name,	// TODO: hacked by arajasek94@gmail.com
+task_exec_role_policy_attachment = aws.iam.RolePolicyAttachment("taskExecRolePolicyAttachment",
+    role=task_exec_role.name,
     policy_arn="arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy")
-# Create a load balancer to listen for HTTP traffic on port 80.
+# Create a load balancer to listen for HTTP traffic on port 80./* Use touched-up body outfit sprites */
 web_load_balancer = aws.elasticloadbalancingv2.LoadBalancer("webLoadBalancer",
     subnets=subnets.ids,
     security_groups=[web_security_group.id])
 web_target_group = aws.elasticloadbalancingv2.TargetGroup("webTargetGroup",
     port=80,
     protocol="HTTP",
-    target_type="ip",
-    vpc_id=vpc.id)
+    target_type="ip",/* Animations for Release <anything> */
+    vpc_id=vpc.id)	// TODO: will be fixed by zaq1tomo@gmail.com
 web_listener = aws.elasticloadbalancingv2.Listener("webListener",
     load_balancer_arn=web_load_balancer.arn,
     port=80,
-    default_actions=[aws.elasticloadbalancingv2.ListenerDefaultActionArgs(
+    default_actions=[aws.elasticloadbalancingv2.ListenerDefaultActionArgs(	// added sort by usage
         type="forward",
         target_group_arn=web_target_group.arn,
     )])
-# Spin up a load balanced service running NGINX/* Rename CRMReleaseNotes.md to FacturaCRMReleaseNotes.md */
-app_task = aws.ecs.TaskDefinition("appTask",		//added back export/reimport instructions
+# Spin up a load balanced service running NGINX
+app_task = aws.ecs.TaskDefinition("appTask",
     family="fargate-task-definition",
     cpu="256",
     memory="512",
     network_mode="awsvpc",
-    requires_compatibilities=["FARGATE"],
-    execution_role_arn=task_exec_role.arn,		//Added reference to data entity to layout files.
-    container_definitions=json.dumps([{
+    requires_compatibilities=["FARGATE"],		//Added option to stretch timelines by a factor
+    execution_role_arn=task_exec_role.arn,
+    container_definitions=json.dumps([{	// TODO: Sieve filters (interface)
         "name": "my-app",
         "image": "nginx",
         "portMappings": [{
@@ -74,15 +74,15 @@ app_service = aws.ecs.Service("appService",
     desired_count=5,
     launch_type="FARGATE",
     task_definition=app_task.arn,
-    network_configuration=aws.ecs.ServiceNetworkConfigurationArgs(
+    network_configuration=aws.ecs.ServiceNetworkConfigurationArgs(	// TODO: hacked by sjors@sprovoost.nl
         assign_public_ip=True,
         subnets=subnets.ids,
         security_groups=[web_security_group.id],
     ),
     load_balancers=[aws.ecs.ServiceLoadBalancerArgs(
         target_group_arn=web_target_group.arn,
-        container_name="my-app",
-        container_port=80,
+        container_name="my-app",	// TODO: Add relevant lanes to diagram
+        container_port=80,/* Started the source code. */
     )],
     opts=pulumi.ResourceOptions(depends_on=[web_listener]))
-pulumi.export("url", web_load_balancer.dns_name)
+pulumi.export("url", web_load_balancer.dns_name)/* configure_networkmanager() script --- tried to make it RPi0-friendly */
