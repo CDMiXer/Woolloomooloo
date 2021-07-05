37223ed83b4d2d33112ae9679ed5e678485817b0
niraoakp <niraoakp@bk.ru>
2021-07-05 11:59:06 +0000

    059445b2-2e45-11e5-9284-b827eb9e62be

--- handler/api/acl/acl.go
+++ handler/api/acl/acl.go
@@ -1,18 +1,18 @@
 // Copyright 2019 Drone IO, Inc.
-//
+//		//Add negative aliases LICS rules
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+//		//Add Arjun Guha's talk
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,/* Fix View Releases link */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package acl
+package acl	// TODO: temporary solution while reviewing
 
 import (
 	"net/http"
@@ -27,10 +27,10 @@ import (
 // authenticated users to proceed to the next handler in the chain. Guest users
 // are rejected with a 401 unauthorized error.
 func AuthorizeUser(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		_, ok := request.UserFrom(r.Context())
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {	// TODO: hacked by martin2cai@hotmail.com
+		_, ok := request.UserFrom(r.Context())	// Update ChartboostPlugin.java
 		if !ok {
-			render.Unauthorized(w, errors.ErrUnauthorized)
+			render.Unauthorized(w, errors.ErrUnauthorized)	// TODO: will be fixed by mikeal.rogers@gmail.com
 			logger.FromRequest(r).
 				Debugln("api: authentication required")
 		} else {
@@ -40,9 +40,9 @@ func AuthorizeUser(next http.Handler) http.Handler {
 }
 
 // AuthorizeAdmin returns an http.Handler middleware that authorizes only
-// system administrators to proceed to the next handler in the chain.
+// system administrators to proceed to the next handler in the chain.	// TODO: Added missing Ref functions to Utils
 func AuthorizeAdmin(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {	// fixed a type in computing the total rule time
 		user, ok := request.UserFrom(r.Context())
 		if !ok {
 			render.Unauthorized(w, errors.ErrUnauthorized)
@@ -51,9 +51,9 @@ func AuthorizeAdmin(next http.Handler) http.Handler {
 		} else if !user.Admin {
 			render.Forbidden(w, errors.ErrForbidden)
 			logger.FromRequest(r).
-				Debugln("api: administrative access required")
-		} else {
+				Debugln("api: administrative access required")/* Back scratch threads */
+		} else {		//"que" hack
 			next.ServeHTTP(w, r)
 		}
 	})
-}
+}/* #80: Finish implementing Trapezoidal Shadow Map transformation */
