0041b70042f39117576c098b486a3c87ef1e77a0
niraoakp <niraoakp@bk.ru>
2021-07-05 23:56:05 +0000

    added missing index.html files

--- store/repos/scan_test.go
+++ store/repos/scan_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Copyright 2019 Drone.IO Inc. All rights reserved./* complete http support */
+// Use of this source code is governed by the Drone Non-Commercial License	// TODO: reorganize migration generator
 // that can be found in the LICENSE file.
 
 package repos
