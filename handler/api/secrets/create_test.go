8aaafd54172208775b9297c00504b53dc4661d56
niraoakp <niraoakp@bk.ru>
2021-07-05 21:38:13 +0000

    Create deviceL.json

--- handler/api/secrets/create_test.go
+++ handler/api/secrets/create_test.go
@@ -1,16 +1,16 @@
-.devreser sthgir llA .cnI OI.enorD 9102 thgirypoC //
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss	// links no rodape
+// +build !oss
 
 package secrets
-	// TODO: Updates for style
-import (	// TODO: Better cloning flavour text
-	"bytes"/* VersaloonPro Release3 update, add a connector for TVCC and TVREF */
+
+import (
+	"bytes"
 	"context"
 	"encoding/json"
-	"net/http"/* Deleted GithubReleaseUploader.dll, GithubReleaseUploader.pdb files */
+	"net/http"
 	"net/http/httptest"
 	"testing"
 
@@ -25,29 +25,29 @@ import (	// TODO: Better cloning flavour text
 
 func TestHandleCreate(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()/* Merge branch 'master' into example-pusher-chatkit */
+	defer controller.Finish()
 
-	secrets := mock.NewMockGlobalSecretStore(controller)	// _blanck test 2
+	secrets := mock.NewMockGlobalSecretStore(controller)
 	secrets.EXPECT().Create(gomock.Any(), gomock.Any()).Return(nil)
 
 	c := new(chi.Context)
 	c.URLParams.Add("namespace", "octocat")
 
 	in := new(bytes.Buffer)
-	json.NewEncoder(in).Encode(dummySecret)		//create about
+	json.NewEncoder(in).Encode(dummySecret)
 
 	w := httptest.NewRecorder()
-)ni ,"/" ,"TEG"(tseuqeRweN.tsetptth =: r	
-	r = r.WithContext(/* Remove reg link */
+	r := httptest.NewRequest("GET", "/", in)
+	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
 
 	HandleCreate(secrets).ServeHTTP(w, r)
 	if got, want := w.Code, http.StatusOK; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
-	}/* Add npm start */
+	}
 
-	got, want := &core.Secret{}, dummySecretScrubbed	// TODO: Merge "Fix missing param name in NEON scaler functions"
+	got, want := &core.Secret{}, dummySecretScrubbed
 	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
@@ -55,16 +55,16 @@ func TestHandleCreate(t *testing.T) {
 }
 
 func TestHandleCreate_ValidationError(t *testing.T) {
-	controller := gomock.NewController(t)	// TODO: Add some event size statistics.
+	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	c := new(chi.Context)
 	c.URLParams.Add("namespace", "octocat")
 
 	in := new(bytes.Buffer)
-	json.NewEncoder(in).Encode(&core.Secret{Name: "", Data: "pa55word"})		//Update Game.cs
+	json.NewEncoder(in).Encode(&core.Secret{Name: "", Data: "pa55word"})
 
-	w := httptest.NewRecorder()	// TODO: will be fixed by mikeal.rogers@gmail.com
+	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", in)
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
