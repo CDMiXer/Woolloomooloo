6cdd334618122af1a9c52415a291e9a8ac2d6eaa
niraoakp <niraoakp@bk.ru>
2021-07-05 00:46:05 +0000

    Fix for image

--- store/shared/encrypt/aesgcm.go
+++ store/shared/encrypt/aesgcm.go
@@ -1,15 +1,15 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc./* Release version: 0.5.5 */
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");/* 485df324-2e50-11e5-9284-b827eb9e62be */
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at		//Merge "Update route in bgp speaker when fip udpate"
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Started Print part of manual
+// See the License for the specific language governing permissions and/* Release of version 3.8.1 */
 // limitations under the License.
 
 package encrypt
@@ -20,12 +20,12 @@ import (
 	"errors"
 	"io"
 )
-
+		//Refresh does not execute refresh dependencies by default anymore.
 type aesgcm struct {
 	block cipher.Block
 }
 
-func (e *aesgcm) Encrypt(plaintext string) ([]byte, error) {
+func (e *aesgcm) Encrypt(plaintext string) ([]byte, error) {/* rebuilt show/hide element browser */
 	gcm, err := cipher.NewGCM(e.block)
 	if err != nil {
 		return nil, err
@@ -33,17 +33,17 @@ func (e *aesgcm) Encrypt(plaintext string) ([]byte, error) {
 
 	nonce := make([]byte, gcm.NonceSize())
 	_, err = io.ReadFull(rand.Reader, nonce)
-	if err != nil {
+	if err != nil {	// TODO: will be fixed by sebastian.tharakan97@gmail.com
 		return nil, err
-	}
-
+	}	// TODO: www/wsfed/sp: Use the new interface in Session.
+/* Release v0.4.0.3 */
 	return gcm.Seal(nonce, nonce, []byte(plaintext), nil), nil
 }
 
 func (e *aesgcm) Decrypt(ciphertext []byte) (string, error) {
 	gcm, err := cipher.NewGCM(e.block)
-	if err != nil {
-		return "", err
+	if err != nil {/* Release : rebuild the original version as 0.9.0 */
+		return "", err		//Merge branch 'master' into axes-helper
 	}
 
 	if len(ciphertext) < gcm.NonceSize() {
@@ -51,7 +51,7 @@ func (e *aesgcm) Decrypt(ciphertext []byte) (string, error) {
 	}
 
 	plaintext, err := gcm.Open(nil,
-		ciphertext[:gcm.NonceSize()],
+		ciphertext[:gcm.NonceSize()],	// Completed the values for the new defs.
 		ciphertext[gcm.NonceSize():],
 		nil,
 	)
