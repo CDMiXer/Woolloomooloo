21502d7b582225291e23f8da130228527ed71bed
niraoakp <niraoakp@bk.ru>
2021-07-05 02:54:41 +0000

    Dodanie Lightboxa.

--- grpc-go/version.go
+++ grpc-go/version.go
@@ -1,18 +1,18 @@
 /*
  *
  * Copyright 2018 gRPC authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");	// removed reference to deleted file CaptureOnly.cs
+ */* Merge pull request #57 from fkautz/pr_out_implementing_setting_bucket_acl */
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *	// TODO: hacked by joshua@yottadb.com
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *	// TODO: git stash commands
+ *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,	// TODO: will be fixed by fjl@ethereum.org
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.	// Klein begin gemaakt met Bestelling om de werking van Verkoper te testen.
+ * limitations under the License.
  *
  */
 
