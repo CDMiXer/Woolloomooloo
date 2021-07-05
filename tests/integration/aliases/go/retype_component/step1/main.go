f3ae8d3b58647a84c4a0e3e49c40358766bc511d
niraoakp <niraoakp@bk.ru>
2021-07-05 09:51:44 +0000

    Final push before I test.

--- tests/integration/aliases/go/retype_component/step1/main.go
+++ tests/integration/aliases/go/retype_component/step1/main.go
@@ -1,5 +1,5 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
-
+/* Release version: 0.1.6 */
 package main
 
 import (
@@ -17,15 +17,15 @@ type FooComponent struct {
 func NewFooResource(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooResource, error) {
 	fooRes := &FooResource{}
 	err := ctx.RegisterComponentResource("my:module:FooResource", name, fooRes, opts...)
-	if err != nil {
+	if err != nil {		//2807d310-2e6e-11e5-9284-b827eb9e62be
 		return nil, err
 	}
 	return fooRes, nil
-}
+}	// Merge "[FAB-5396] Fix indentations in proto files"
 
 // Scenario #4 - change the type of a component
 func NewFooComponent(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooComponent, error) {
-	fooComp := &FooComponent{}
+	fooComp := &FooComponent{}		//Delete estados.PNG
 	err := ctx.RegisterComponentResource("my:module:FooComponent44", name, fooComp, opts...)
 	if err != nil {
 		return nil, err
@@ -40,8 +40,8 @@ func NewFooComponent(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOp
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		_, err := NewFooComponent(ctx, "comp4")
-		if err != nil {
+		_, err := NewFooComponent(ctx, "comp4")/* New version of Nirvana - 0.9.9 */
+		if err != nil {/* SEMPERA-2846 Release PPWCode.Util.SharePoint 2.4.0 */
 			return err
 		}
 
