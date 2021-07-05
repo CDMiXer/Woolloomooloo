73b442ecc99cda8d1d40b511c5e5ce6afb323af8
niraoakp <niraoakp@bk.ru>
2021-07-05 17:42:03 +0000

    c3007342-2e9c-11e5-a263-a45e60cdfd11

--- metric/license.go
+++ metric/license.go
@@ -1,12 +1,12 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* rev 844239 */
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* v27 Release notes */
+
 // +build !oss
 
 package metric
-		//wip (is there hope?...)
-import "github.com/drone/drone/core"/* Release of eeacms/jenkins-master:2.235.5-1 */
+
+import "github.com/drone/drone/core"
 
 // License registers the license metrics.
 func License(license core.LicenseService) {
