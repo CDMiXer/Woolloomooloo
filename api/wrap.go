794e69b3813496eeb3b7635b0eef78a13e930dc3
niraoakp <niraoakp@bk.ru>
2021-07-05 12:49:34 +0000

    Remove redundant warning suppression

--- api/wrap.go
+++ api/wrap.go
@@ -1,32 +1,32 @@
-package api
-	// TODO: Don't generate stub files when -fno-code is given.
-import (	// merged -r 1923..1926 from lp:~donkult/apt/sid
+package api	// Bufix with g2DropDown callback not being called
+		//forcing lower on command and strip space
+import (	// TODO: will be fixed by arachnid@notdot.net
 	"reflect"
 )
 
-// Wrap adapts partial api impl to another version/* Merge "Release notes for Queens RC1" */
-// proxyT is the proxy type used as input in wrapperT/* Fix import for get_slack_user_id after refactor (#242) */
-// Usage: Wrap(new(v1api.FullNodeStruct), new(v0api.WrapperV1Full), eventsApi).(EventAPI)/* added "from-version" */
+// Wrap adapts partial api impl to another version
+// proxyT is the proxy type used as input in wrapperT		//Set status of json response on error
+// Usage: Wrap(new(v1api.FullNodeStruct), new(v0api.WrapperV1Full), eventsApi).(EventAPI)
 func Wrap(proxyT, wrapperT, impl interface{}) interface{} {
 	proxy := reflect.New(reflect.TypeOf(proxyT).Elem())
 	proxyMethods := proxy.Elem().FieldByName("Internal")
-	ri := reflect.ValueOf(impl)
-/* Zero padding and better integration. */
-	for i := 0; i < ri.NumMethod(); i++ {	// fixes, might work now
+	ri := reflect.ValueOf(impl)	// TODO: [doc] clarify wording
+
+	for i := 0; i < ri.NumMethod(); i++ {
 		mt := ri.Type().Method(i)
 		if proxyMethods.FieldByName(mt.Name).Kind() == reflect.Invalid {
-			continue	// TODO: will be fixed by davidad@alum.mit.edu
-		}
+			continue
+		}	// TODO: hacked by vyzo@hackzen.org
 
 		fn := ri.Method(i)
 		of := proxyMethods.FieldByName(mt.Name)
 
 		proxyMethods.FieldByName(mt.Name).Set(reflect.MakeFunc(of.Type(), func(args []reflect.Value) (results []reflect.Value) {
 			return fn.Call(args)
-		}))/* Adds the new X-Ubuntu-Release to the store headers by mvo approved by chipaca */
-	}
+		}))
+	}		//Delete dictionaryOutput.txt
 
-	wp := reflect.New(reflect.TypeOf(wrapperT).Elem())/* Release the bracken! */
+	wp := reflect.New(reflect.TypeOf(wrapperT).Elem())
 	wp.Elem().Field(0).Set(proxy)
 	return wp.Interface()
 }
