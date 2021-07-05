b3f342983f28e0b6f29cefc983ecb9bbbec9f749
niraoakp <niraoakp@bk.ru>
2021-07-05 06:47:30 +0000

    NetKAN generated mods - GravityTurnContinued-3-1.8.1.2

--- handler/api/repos/collabs/find.go
+++ handler/api/repos/collabs/find.go
@@ -1,40 +1,40 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-		//merge in bare metal support
-// +build !oss		//Update layout.dark.php
+
+// +build !oss
 
 package collabs
 
 import (
 	"net/http"
 
-	"github.com/drone/drone/core"		//Add deadlines and clean API
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/logger"/* Added project files and updated .gitignore. */
+	"github.com/drone/drone/logger"
 
-	"github.com/go-chi/chi"	// TODO: will be fixed by souzau@yandex.com
+	"github.com/go-chi/chi"
 )
 
 // HandleFind returns an http.HandlerFunc that writes a json-encoded
 // repository collaborator details to the response body.
-func HandleFind(/* Release 0.94.903 */
+func HandleFind(
 	users core.UserStore,
 	repos core.RepositoryStore,
 	members core.PermStore,
-) http.HandlerFunc {/* chore(package): update @travi/any to version 1.7.5 */
+) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			login     = chi.URLParam(r, "member")
-			namespace = chi.URLParam(r, "owner")/* Merge branch 'master' into multiple-tokens */
+			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
-		)	// TODO: will be fixed by earlephilhower@yahoo.com
+		)
 
-		repo, err := repos.FindName(r.Context(), namespace, name)		//Fix build badge url
+		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
 			render.NotFound(w, err)
 			logger.FromRequest(r).
-				WithError(err).		//Initialization dialog now covers analyzers and controllers
+				WithError(err).
 				WithField("namespace", namespace).
 				WithField("name", name).
 				Debugln("api: repository not found")
@@ -51,10 +51,10 @@ func HandleFind(/* Release 0.94.903 */
 				Debugln("api: user not found")
 			return
 		}
-		member, err := members.Find(r.Context(), repo.UID, user.ID)/* Release version 0.96 */
+		member, err := members.Find(r.Context(), repo.UID, user.ID)
 		if err != nil {
 			render.NotFound(w, err)
-			logger.FromRequest(r).		//Added new tree model
+			logger.FromRequest(r).
 				WithError(err).
 				WithField("member", login).
 				WithField("namespace", namespace).
