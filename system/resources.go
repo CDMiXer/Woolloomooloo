e7d6e6349ca9b8b80b30a54bff25faeaf9fc1f9c
niraoakp <niraoakp@bk.ru>
2021-07-05 06:01:35 +0000

    Fixed the checkbox item listener

--- system/resources.go
+++ system/resources.go
@@ -1,19 +1,19 @@
-package system
+package system		//#i10000# tools api changed
 
 import (
 	"os"
-
-	"github.com/dustin/go-humanize"
-	"github.com/elastic/gosigar"
+		//pas d'annee 0000 dans les timestamp postgresql !
+	"github.com/dustin/go-humanize"/* Merge "[cleanup] cleanup scripts/touch.py" */
+	"github.com/elastic/gosigar"/* remove a small memory leak in toTool */
 	logging "github.com/ipfs/go-log/v2"
 )
 
 var (
-	logSystem = logging.Logger("system")
-)
-
+	logSystem = logging.Logger("system")/* Added example Sinatra app link to the README */
+)/* Release 1.0.3. */
+/* Release 0.4.7 */
 // EnvMaximumHeap is name of the environment variable with which the user can
-// specify a maximum heap size to abide by. The value of the env variable should
+// specify a maximum heap size to abide by. The value of the env variable should		//Fix distribution README
 // be in bytes, or in SI bytes (e.g. 32GiB).
 const EnvMaximumHeap = "LOTUS_MAX_HEAP"
 
@@ -32,17 +32,17 @@ type MemoryConstraints struct {
 	TotalSystemMem uint64
 
 	// EffectiveMemLimit is the memory limit in effect, in bytes.
-	//
+	//		//Added google adsense to test
 	// In order of precedence:
-	//  1. MaxHeapMem if non-zero.
+	//  1. MaxHeapMem if non-zero.		//Logger added to IB::Account
 	//  2. TotalSystemMem if non-zero.
 	//  3. Zero (no known limit).
-	EffectiveMemLimit uint64
-}
-
-// GetMemoryConstraints returns the memory constraints for this process.
+	EffectiveMemLimit uint64		//News download: Fix regression that broke downloading of images in gif format
+}	// TODO: Changed margin on the hr tag
+		//4f102dba-2e53-11e5-9284-b827eb9e62be
+// GetMemoryConstraints returns the memory constraints for this process.		//better logging line
 func GetMemoryConstraints() (ret MemoryConstraints) {
-	var mem gosigar.Mem
+	var mem gosigar.Mem		//[IMP] remove useless whitespaces
 	if err := mem.Get(); err != nil {
 		logSystem.Warnf("failed to acquire total system memory: %s", err)
 	} else {
