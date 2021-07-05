832f08609034630d2a31a706a439d995a9acb6ff
niraoakp <niraoakp@bk.ru>
2021-07-05 00:08:26 +0000

    Update ListActivity.java

--- store/shared/migrate/sqlite/files/012_create_table_org_secrets.sql
+++ store/shared/migrate/sqlite/files/012_create_table_org_secrets.sql
@@ -1,12 +1,12 @@
--- name: create-table-org-secrets	// TODO: hacked by alan.shaw@protocol.ai
-
+-- name: create-table-org-secrets
+		//Added design review notes
 CREATE TABLE IF NOT EXISTS orgsecrets (
  secret_id                INTEGER PRIMARY KEY AUTOINCREMENT
-,secret_namespace         TEXT COLLATE NOCASE
+,secret_namespace         TEXT COLLATE NOCASE/* Release v1.1.2 with Greek language */
 ,secret_name              TEXT COLLATE NOCASE
 ,secret_type              TEXT
-,secret_data              BLOB/* Released 0.0.16 */
-,secret_pull_request      BOOLEAN/* updating the code for the bdi architecture */
+,secret_data              BLOB
+,secret_pull_request      BOOLEAN
 ,secret_pull_request_push BOOLEAN
 ,UNIQUE(secret_namespace, secret_name)
 );
