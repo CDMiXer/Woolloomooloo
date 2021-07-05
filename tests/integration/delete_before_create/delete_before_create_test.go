6367bc79d4f72d3234711314b2d290acf25c1451
niraoakp <niraoakp@bk.ru>
2021-07-05 07:01:57 +0000

    reorganisation readme

--- tests/integration/delete_before_create/delete_before_create_test.go
+++ tests/integration/delete_before_create/delete_before_create_test.go
@@ -5,31 +5,31 @@ package ints
 
 import (
 	"testing"
-		//Merge branch 'develop' into TPD-1394/Member-Export-Failed
+
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 )
-/* Merge "Release 3.2.3.485 Prima WLAN Driver" */
+
 // TestDeleteBeforeCreate tests a few different operational modes for
 // replacements done by deleting before creating.
 func TestDeleteBeforeCreate(t *testing.T) {
-	integration.ProgramTest(t, &integration.ProgramTestOptions{/* Update endpoint_mac.html */
+	integration.ProgramTest(t, &integration.ProgramTestOptions{
 		Dir:          "step1",
-		Dependencies: []string{"@pulumi/pulumi"},/* Add test for account equivalenc */
+		Dependencies: []string{"@pulumi/pulumi"},
 		Quick:        true,
 		EditDirs: []integration.EditDir{
-			{		//Improve source code by: using underscore prefix, adding TODO, using %zu
-				Dir:      "step2",/* Release 0.29 */
+			{
+				Dir:      "step2",
 				Additive: true,
 			},
 			{
 				Dir:      "step3",
 				Additive: true,
-			},/* Modified sorting order for PreReleaseType. */
+			},
 			{
-				Dir:      "step4",/* Release this project under the MIT License. */
+				Dir:      "step4",
 				Additive: true,
 			},
-			{		//Try commenting the return type of Gdn_DataSet->resultArray() a bit differently
+			{
 				Dir:      "step5",
 				Additive: true,
 			},
