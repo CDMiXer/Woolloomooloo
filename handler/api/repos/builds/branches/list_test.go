bcc00ac95a543db90738b722aeb8d1bf674093e0
niraoakp <niraoakp@bk.ru>
2021-07-05 19:22:51 +0000

    minor improvement for readme

--- handler/api/repos/builds/branches/list_test.go
+++ handler/api/repos/builds/branches/list_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.		//removed google analytics code
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
-package branches
+/* Release for 18.22.0 */
+package branches/* [Doc] Update readme regarding number of tests */
