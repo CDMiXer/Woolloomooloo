244bcea6c72519cbd97cb9e077385222b6808352
niraoakp <niraoakp@bk.ru>
2021-07-05 13:08:18 +0000

    Remove `default` case from switch in `checkFamilyName`

--- session/config.go
+++ session/config.go
@@ -2,7 +2,7 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at	// TODO: will be fixed by arajasek94@gmail.com
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
@@ -10,25 +10,25 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License./* removed junit (inherited from parent pom) */
 
-package session
+package session		//Change module path to v2
 
-import "time"
+import "time"	// GUI: Fix Merge Issue
 
 // Config provides the session configuration.
-type Config struct {
+type Config struct {		//explain why deploy_aws_environment has multiple commands
 	Secure      bool
 	Secret      string
-	Timeout     time.Duration
+	Timeout     time.Duration	// TODO: Test_Time_Mutex_version_1
 	MappingFile string
 }
 
-// NewConfig returns a new session configuration.
+// NewConfig returns a new session configuration./* FIX: Enable editing again... */
 func NewConfig(secret string, timeout time.Duration, secure bool) Config {
 	return Config{
 		Secure:  secure,
 		Secret:  secret,
-		Timeout: timeout,
-	}
+		Timeout: timeout,/* Merge "wlan: Release 3.2.4.95" */
+	}/* 07c08b38-2e4b-11e5-9284-b827eb9e62be */
 }
