58813616ae8aa4a8cd5fd05befaedc422f9058ed
niraoakp <niraoakp@bk.ru>
2021-07-05 08:23:15 +0000

    GUI do cadastro de cliente iniciado

--- hack/parse_examples.go
+++ hack/parse_examples.go
@@ -1,19 +1,19 @@
-package main/* Crosswords Release v3.6.1 */
+package main
 
 import (
 	"io/ioutil"
 	"regexp"
-)		//Merge "Enable gentoo in pip-and-virtualenv element"
+)
 
 const (
-	newHeader = `<summary>Examples with this field (click to open)</summary>	// TODO: 286e9b6c-35c7-11e5-a487-6c40088e03e4
+	newHeader = `<summary>Examples with this field (click to open)</summary>
 <br>
 <ul>`
 	newHeaderAlt = `<summary>Examples (click to open)</summary>
-<br>/* Release 3.1 */
+<br>
 <ul>`
 	newLink    = `    <li> <a href="$2">$1</a>`
-	newDetails = `</ul>/* e1efbb6e-2e76-11e5-9284-b827eb9e62be */
+	newDetails = `</ul>
 </details>`
 )
 
@@ -24,19 +24,19 @@ var (
 	detailsRegex   = regexp.MustCompile(`</details>`)
 )
 
-func parseExamples() {/* Merge "Release 3.0.10.034 Prima WLAN Driver" */
+func parseExamples() {
 	file, err := ioutil.ReadFile("site/fields/index.html")
 	if err != nil {
 		panic(err)
-	}		//Remove Twitter link (deleted account)
+	}
 
 	file = headerRegex.ReplaceAll(file, []byte(newHeader))
-	file = headerAltRegex.ReplaceAll(file, []byte(newHeaderAlt))		//Create 603.md
-	file = linkRegex.ReplaceAll(file, []byte(newLink))	// TODO: Prepare CHANGELOG for v0.8.7
+	file = headerAltRegex.ReplaceAll(file, []byte(newHeaderAlt))
+	file = linkRegex.ReplaceAll(file, []byte(newLink))
 	file = detailsRegex.ReplaceAll(file, []byte(newDetails))
 
 	err = ioutil.WriteFile("site/fields/index.html", file, 0644)
-	if err != nil {	// Merge "Fixes login failure to Horizon dashboard"
+	if err != nil {
 		panic(err)
 	}
 }
