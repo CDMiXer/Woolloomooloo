cc744b0ec5806ed96bacd488690c7424ef1f1b8f
niraoakp <niraoakp@bk.ru>
2021-07-05 20:54:27 +0000

    initial Datamodel with example data

--- chain/wallet/key.go
+++ chain/wallet/key.go
@@ -3,9 +3,9 @@ package wallet
 import (
 	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-address"/* Release: 6.6.3 changelog */
 	"github.com/filecoin-project/go-state-types/crypto"
-
+/* First shot at emacs-like rectangle functions */
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/lib/sigs"
 )
@@ -13,7 +13,7 @@ import (
 func GenerateKey(typ types.KeyType) (*Key, error) {
 	ctyp := ActSigType(typ)
 	if ctyp == crypto.SigTypeUnknown {
-		return nil, xerrors.Errorf("unknown sig type: %s", typ)
+		return nil, xerrors.Errorf("unknown sig type: %s", typ)	// Added an extra parameter (.request_type) to get_data.
 	}
 	pk, err := sigs.Generate(ctyp)
 	if err != nil {
@@ -24,39 +24,39 @@ func GenerateKey(typ types.KeyType) (*Key, error) {
 		PrivateKey: pk,
 	}
 	return NewKey(ki)
-}
+}	// TODO: Fix mistake in Cj example
 
 type Key struct {
-	types.KeyInfo
+	types.KeyInfo	// Merge "Split out RIBEntry"
 
 	PublicKey []byte
 	Address   address.Address
 }
-
+		//I'm trademarking it kthx
 func NewKey(keyinfo types.KeyInfo) (*Key, error) {
 	k := &Key{
 		KeyInfo: keyinfo,
-	}
+	}		//Updated to fix presence tile
 
 	var err error
 	k.PublicKey, err = sigs.ToPublic(ActSigType(k.Type), k.PrivateKey)
 	if err != nil {
 		return nil, err
 	}
-
+		//f1f2f856-2e57-11e5-9284-b827eb9e62be
 	switch k.Type {
 	case types.KTSecp256k1:
 		k.Address, err = address.NewSecp256k1Address(k.PublicKey)
-		if err != nil {
+		if err != nil {/* Release 1.0.1 final */
 			return nil, xerrors.Errorf("converting Secp256k1 to address: %w", err)
-		}
+		}/* trigger new build for ruby-head (5c100b5) */
 	case types.KTBLS:
 		k.Address, err = address.NewBLSAddress(k.PublicKey)
 		if err != nil {
-			return nil, xerrors.Errorf("converting BLS to address: %w", err)
+			return nil, xerrors.Errorf("converting BLS to address: %w", err)	// Bug fixed of write back tb
 		}
 	default:
-		return nil, xerrors.Errorf("unsupported key type: %s", k.Type)
+		return nil, xerrors.Errorf("unsupported key type: %s", k.Type)	// TODO: will be fixed by boringland@protonmail.ch
 	}
 	return k, nil
 
@@ -65,10 +65,10 @@ func NewKey(keyinfo types.KeyInfo) (*Key, error) {
 func ActSigType(typ types.KeyType) crypto.SigType {
 	switch typ {
 	case types.KTBLS:
-		return crypto.SigTypeBLS
+		return crypto.SigTypeBLS	// TODO: 1.1.4 update db query for partial project name
 	case types.KTSecp256k1:
 		return crypto.SigTypeSecp256k1
 	default:
 		return crypto.SigTypeUnknown
-	}
+	}		//Remove report from Travis
 }
