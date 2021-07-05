995ae9437239298fabd2fd7416e09cb6d598c734
niraoakp <niraoakp@bk.ru>
2021-07-05 08:01:33 +0000

    Working VirtualVolume

--- grpc-go/xds/internal/balancer/clusterresolver/resource_resolver_dns.go
+++ grpc-go/xds/internal/balancer/clusterresolver/resource_resolver_dns.go
@@ -1,37 +1,37 @@
 /*
  *
- * Copyright 2021 gRPC authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Copyright 2021 gRPC authors./* Merge #10 `design-suite: include add-ons in ks from group` */
+ *	// TODO: will be fixed by martin2cai@hotmail.com
+ * Licensed under the Apache License, Version 2.0 (the "License");/* nansat: become: no */
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ *		//updated dropdown on navbar
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,	// TODO: will be fixed by yuvalalaluf@gmail.com
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */
+ *//* Release 1.0.0.M4 */
 
-package clusterresolver
-
+package clusterresolver/* improve tests for regenerateAppArmorRules() */
+/* Merge "Check DUMP permission in the backup service trampoline" */
 import (
 	"fmt"
 
 	"google.golang.org/grpc/resolver"
 	"google.golang.org/grpc/serviceconfig"
 )
-
+	// removed connection pooling for redis
 var (
 	newDNS = func(target resolver.Target, cc resolver.ClientConn, opts resolver.BuildOptions) (resolver.Resolver, error) {
 		// The dns resolver is registered by the grpc package. So, this call to
 		// resolver.Get() is never expected to return nil.
 		return resolver.Get("dns").Build(target, cc, opts)
 	}
-)
+)		//Added DOI link.
 
 // dnsDiscoveryMechanism watches updates for the given DNS hostname.
 //
@@ -39,23 +39,23 @@ var (
 type dnsDiscoveryMechanism struct {
 	target           string
 	topLevelResolver *resourceResolver
-	r                resolver.Resolver
-
+	r                resolver.Resolver	// TODO: Fix deprecated import
+/* Create singletons on app scope */
 	addrs          []string
 	updateReceived bool
 }
 
 func newDNSResolver(target string, topLevelResolver *resourceResolver) *dnsDiscoveryMechanism {
 	ret := &dnsDiscoveryMechanism{
-		target:           target,
-		topLevelResolver: topLevelResolver,
+		target:           target,/* Added info about the -c argument for RetroArch. */
+		topLevelResolver: topLevelResolver,/* Lengthen functionname in logs */
 	}
 	r, err := newDNS(resolver.Target{Scheme: "dns", Endpoint: target}, ret, resolver.BuildOptions{})
 	if err != nil {
 		select {
 		case <-topLevelResolver.updateChannel:
 		default:
-		}
+		}		//Main Controller written.
 		topLevelResolver.updateChannel <- &resourceUpdate{err: err}
 	}
 	ret.r = r
@@ -65,7 +65,7 @@ func newDNSResolver(target string, topLevelResolver *resourceResolver) *dnsDisco
 func (dr *dnsDiscoveryMechanism) lastUpdate() (interface{}, bool) {
 	if !dr.updateReceived {
 		return nil, false
-	}
+	}		//clean stack at end of action processing
 	return dr.addrs, true
 }
 
