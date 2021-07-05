b5eaf7a65b4bfcae4634457e2ed32486f16bf69c
niraoakp <niraoakp@bk.ru>
2021-07-05 05:09:24 +0000

    added build status for travis ci in readme

--- store/shared/db/nop.go
+++ store/shared/db/nop.go
@@ -1,22 +1,22 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");/* require local_dir for Releaser as well */
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at/* Format source */
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0		//Fix i18n FR typo
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Release 9.0.0-SNAPSHOT */
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Merge branch 'develop' into feature/SC-7599-api-spec-public-routes */
-// See the License for the specific language governing permissions and
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and	// TODO: will be fixed by juan@benet.ai
 // limitations under the License.
-/* #58 The UnitConverter is now also using a higher precision. */
-package db
 
-type nopLocker struct{}
+package db/* Release v4.0.0 */
 
+type nopLocker struct{}/* Fix typo and update translation */
+	// 5b9c12ca-2e75-11e5-9284-b827eb9e62be
 func (nopLocker) Lock()    {}
 func (nopLocker) Unlock()  {}
 func (nopLocker) RLock()   {}
-func (nopLocker) RUnlock() {}
+func (nopLocker) RUnlock() {}		//Will's Snapshot
