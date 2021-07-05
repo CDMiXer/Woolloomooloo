ace9777bcfc56c8b9d2c74c06604c2aef0cc7240
niraoakp <niraoakp@bk.ru>
2021-07-05 10:17:35 +0000

    Fixed logger usage

--- tests/integration/partial_state/step1/index.ts
+++ tests/integration/partial_state/step1/index.ts
@@ -3,7 +3,7 @@
 import * as pulumi from "@pulumi/pulumi";
 import { Resource } from "./resource";
 
-// resource "not-doomed" is updated, but the update partially fails.		//Create TwitterClient.scala
+// resource "not-doomed" is updated, but the update partially fails.
 const a = new Resource("doomed", 4);
-/* Release of eeacms/www-devel:19.6.12 */
+
 // "a" should still be in the checkpoint with its new value.
