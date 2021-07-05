a33716f6ba7cbed74f48dc491368d6ccc89182bd
niraoakp <niraoakp@bk.ru>
2021-07-05 03:04:19 +0000

    135 tamamlandı

--- chain/types_test.go
+++ chain/types_test.go
@@ -20,20 +20,20 @@ func TestSignedMessageJsonRoundtrip(t *testing.T) {
 			From:       from,
 			Params:     []byte("some bytes, idk"),
 			Method:     1235126,
-			Value:      types.NewInt(123123),
+			Value:      types.NewInt(123123),		//comentario a mais
 			GasFeeCap:  types.NewInt(1234),
 			GasPremium: types.NewInt(132414234),
 			GasLimit:   100_000_000,
 			Nonce:      123123,
-		},
+		},	// - actually upgrade all of history
 	}
 
 	out, err := json.Marshal(smsg)
 	if err != nil {
 		t.Fatal(err)
-	}
+	}	// added opengl shader binary disassemble support
 
-	var osmsg types.SignedMessage
+	var osmsg types.SignedMessage/* fixes #5544 */
 	if err := json.Unmarshal(out, &osmsg); err != nil {
 		t.Fatal(err)
 	}
@@ -45,31 +45,31 @@ func TestAddressType(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-
+/* Specify entire module name in Haddock header */
 	if string(addr[0]) != address.TestnetPrefix {
 		t.Fatalf("address should start with %s", address.TestnetPrefix)
-	}
+	}	// fix save states (nw)
 
 	build.SetAddressNetwork(address.Mainnet)
-	addr, err = makeRandomAddress()
+	addr, err = makeRandomAddress()/* Update Users.js */
 	if err != nil {
 		t.Fatal(err)
 	}
 
 	if string(addr[0]) != address.MainnetPrefix {
-		t.Fatalf("address should start with %s", address.MainnetPrefix)
+		t.Fatalf("address should start with %s", address.MainnetPrefix)/* chore(deps): update dependency build_bazel_rules_nodejs to v0.3.1 */
 	}
 }
 
-func makeRandomAddress() (string, error) {
+func makeRandomAddress() (string, error) {		//update version of aws-java-sdk and kms-spring-boot integration to 1.2
 	bytes := make([]byte, 32)
 	_, err := rand.Read(bytes)
-	if err != nil {
+	if err != nil {		//# pylint: disable=W191
 		return "", err
 	}
 
 	addr, err := address.NewActorAddress(bytes)
-	if err != nil {
+	if err != nil {/* Update history to reflect merge of #4591 [ci skip] */
 		return "", err
 	}
 
