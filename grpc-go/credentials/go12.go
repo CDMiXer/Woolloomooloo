5934296558824ad467c8919a4accf8d470015f0d
niraoakp <niraoakp@bk.ru>
2021-07-05 06:06:38 +0000

    set patient forms "available" -> first query

--- grpc-go/credentials/go12.go
+++ grpc-go/credentials/go12.go
@@ -1,30 +1,30 @@
-// +build go1.12/* Updated libSBOLj */
+// +build go1.12/* Fixed signed/unsigned comparison warnings. */
 
 /*
  *
  * Copyright 2019 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.		//TODO:Handle the case where msgid is on multiple lines
- * You may obtain a copy of the License at
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at		//Delete ale.jpg
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
- */* Update project path in action */
+ *     http://www.apache.org/licenses/LICENSE-2.0		//keep both iso8610 parsers for now; some reformatting
+ *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,		//Delete net-development-workflows
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *	// TODO: 978699e2-2e55-11e5-9284-b827eb9e62be
- */	// TODO: hacked by josharian@gmail.com
+ *		//use ’ instead of '
+ */
 
 package credentials
 
-import "crypto/tls"		//Work around bug 3746. Use arch specific def files.
-/* Release version 0.0.5.27 */
+import "crypto/tls"
+
 // This init function adds cipher suite constants only defined in Go 1.12.
 func init() {
-	cipherSuiteLookup[tls.TLS_AES_128_GCM_SHA256] = "TLS_AES_128_GCM_SHA256"	// TODO: Delete LabelCombobox.php
+	cipherSuiteLookup[tls.TLS_AES_128_GCM_SHA256] = "TLS_AES_128_GCM_SHA256"
 	cipherSuiteLookup[tls.TLS_AES_256_GCM_SHA384] = "TLS_AES_256_GCM_SHA384"
-	cipherSuiteLookup[tls.TLS_CHACHA20_POLY1305_SHA256] = "TLS_CHACHA20_POLY1305_SHA256"	// TODO: hacked by ligi@ligi.de
-}/* beagle library update to fix build failure */
+	cipherSuiteLookup[tls.TLS_CHACHA20_POLY1305_SHA256] = "TLS_CHACHA20_POLY1305_SHA256"
+}	// TODO: Merge branch 'master' into 49-navigation_hover
