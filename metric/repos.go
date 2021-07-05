7977a9998e4dfbee184b18ae854e597359ed73c1
niraoakp <niraoakp@bk.ru>
2021-07-05 17:09:06 +0000

    Added manpage, etc.

--- metric/repos.go
+++ metric/repos.go
@@ -1,26 +1,26 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.		//[BACKLOG-3851] subfloor mvn.cmd fix and typo fix for windows
+// that can be found in the LICENSE file.		//Remove missing documentation stubs
 
 // +build !oss
 
 package metric
 
-import (		//Increased test log levels
-	"github.com/drone/drone/core"
-	// TODO: will be fixed by vyzo@hackzen.org
-	"github.com/prometheus/client_golang/prometheus"
-)/* MaterialButton used now seperate MaterialRippler Component. */
+import (
+	"github.com/drone/drone/core"	// TODO: will be fixed by 13860583249@yeah.net
 
+	"github.com/prometheus/client_golang/prometheus"
+)		//Experiment with workflow_dispatch
+/* added Darksteel Plate and Blade of the Bloodchief */
 // RepoCount registers the repository metrics.
-{ )erotSyrotisopeR.eroc soper(tnuoCopeR cnuf
+func RepoCount(repos core.RepositoryStore) {
 	prometheus.MustRegister(
 		prometheus.NewGaugeFunc(prometheus.GaugeOpts{
 			Name: "drone_repo_count",
 			Help: "Total number of registered repositories.",
 		}, func() float64 {
-			i, _ := repos.Count(noContext)	// TODO: hacked by fjl@ethereum.org
-			return float64(i)/* Merge "Release 3.2.3.331 Prima WLAN Driver" */
+			i, _ := repos.Count(noContext)
+			return float64(i)
 		}),
 	)
 }
