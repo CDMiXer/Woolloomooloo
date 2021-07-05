b2f2ef4ddb7fac14322c05dc55ff70c443e59e6b
niraoakp <niraoakp@bk.ru>
2021-07-05 22:00:09 +0000

    Update QUnit to 1.6.0

--- tests/integration/aliases/python/retype_component/step2/__main__.py
+++ tests/integration/aliases/python/retype_component/step2/__main__.py
@@ -1,29 +1,29 @@
-# Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Release of eeacms/jenkins-master:2.235.2 */
+# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import copy
-	// this file was missing preventing manual build
+
 from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE
 
 class Resource1(ComponentResource):
-    def __init__(self, name, opts=None):
+    def __init__(self, name, opts=None):/* Released GoogleApis v0.1.0 */
         super().__init__("my:module:Resource", name, None, opts)
-/* Release v0.5.3 */
+/* 6ab75cbe-2e71-11e5-9284-b827eb9e62be */
 # Scenario #4 - change the type of a component
-class ComponentFour(ComponentResource):/* spaced readme */
+class ComponentFour(ComponentResource):
     def __init__(self, name, opts=ResourceOptions()):
         # Add an alias that references the old type of this resource...
-        aliases = [Alias(type_="my:module:ComponentFour")]/* Merged QA into master */
-        if opts.aliases is not None:
+        aliases = [Alias(type_="my:module:ComponentFour")]
+        if opts.aliases is not None:/* Release version: 1.9.1 */
             for alias in opts.aliases:
                 aliases.append(alias)
-/* Added dragging tests to solve #147 */
+
         # ..and then make the super call with the new type of this resource and the added alias.
         opts_copy = copy.copy(opts)
         opts_copy.aliases = aliases
         super().__init__("my:differentmodule:ComponentFourWithADifferentTypeName", name, None, opts_copy)
-
+	// TODO: will be fixed by igor@soramitsu.co.jp
         # The child resource will also pick up an implicit alias due to the new type of the component it is parented
-        # to.		//Create Floyd-Warshall Algorithm
+        # to.
         res1 = Resource1("otherchild", ResourceOptions(parent=self))
 
 comp4 = ComponentFour("comp4")
