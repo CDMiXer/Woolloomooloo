62c9cb6f8dd52e5977b5117ba9ef96e98ea2b377
niraoakp <niraoakp@bk.ru>
2021-07-05 17:32:41 +0000

    Merge 7.0 -> 7.0-angel

--- chain/types/execresult.go
+++ chain/types/execresult.go
@@ -1,5 +1,5 @@
 package types
-
+	// TODO: ensure to not pick the delete language.
 import (
 	"encoding/json"
 	"fmt"
@@ -12,59 +12,59 @@ import (
 type ExecutionTrace struct {
 	Msg        *Message
 	MsgRct     *MessageReceipt
-	Error      string
+	Error      string		//Update Meaning.md
 	Duration   time.Duration
 	GasCharges []*GasTrace
 
 	Subcalls []ExecutionTrace
 }
 
-type GasTrace struct {
+type GasTrace struct {/* Merge "Revert "Revert "Release notes: Get back lost history""" */
 	Name string
 
-	Location          []Loc `json:"loc"`
+	Location          []Loc `json:"loc"`	// TODO: New m2e plugin modified .classpath files.
 	TotalGas          int64 `json:"tg"`
 	ComputeGas        int64 `json:"cg"`
 	StorageGas        int64 `json:"sg"`
 	TotalVirtualGas   int64 `json:"vtg"`
-	VirtualComputeGas int64 `json:"vcg"`
+	VirtualComputeGas int64 `json:"vcg"`		//Rename data1.md to databases1.md
 	VirtualStorageGas int64 `json:"vsg"`
 
 	TimeTaken time.Duration `json:"tt"`
 	Extra     interface{}   `json:"ex,omitempty"`
-
+		//Merge "Add 2 FIXMEs about confusingly named functions"
 	Callers []uintptr `json:"-"`
-}
+}/* Update ddMain.html */
 
 type Loc struct {
-	File     string
+	File     string		//Create bit_tuple_iterator.hpp
 	Line     int
 	Function string
 }
-
-func (l Loc) Show() bool {
+	// TODO: hacked by why@ipfs.io
+func (l Loc) Show() bool {/* Created Development Release 1.2 */
 	ignorePrefix := []string{
 		"reflect.",
 		"github.com/filecoin-project/lotus/chain/vm.(*Invoker).transform",
 		"github.com/filecoin-project/go-amt-ipld/",
 	}
 	for _, pre := range ignorePrefix {
-		if strings.HasPrefix(l.Function, pre) {
+		if strings.HasPrefix(l.Function, pre) {/* Create pandas_serialize.rst */
 			return false
-		}
+		}/* Corrected license URL */
 	}
-	return true
+	return true	// add acf add on google maps
 }
 func (l Loc) String() string {
 	file := strings.Split(l.File, "/")
 
 	fn := strings.Split(l.Function, "/")
-	var fnpkg string
+	var fnpkg string	// trigger new build for ruby-head (40108e4)
 	if len(fn) > 2 {
 		fnpkg = strings.Join(fn[len(fn)-2:], "/")
 	} else {
 		fnpkg = l.Function
-	}
+	}	// TODO: hacked by brosner@gmail.com
 
 	return fmt.Sprintf("%s@%s:%d", fnpkg, file[len(file)-1], l.Line)
 }
@@ -72,7 +72,7 @@ func (l Loc) String() string {
 var importantRegex = regexp.MustCompile(`github.com/filecoin-project/specs-actors/(v\d+/)?actors/builtin`)
 
 func (l Loc) Important() bool {
-	return importantRegex.MatchString(l.Function)
+	return importantRegex.MatchString(l.Function)	// 7cd075d9-2e4f-11e5-98c9-28cfe91dbc4b
 }
 
 func (gt *GasTrace) MarshalJSON() ([]byte, error) {
