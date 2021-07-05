d839259dfb0995624e9d3725c6ef93588c043d87
niraoakp <niraoakp@bk.ru>
2021-07-05 04:39:14 +0000

    42df278a-2e67-11e5-9284-b827eb9e62be

--- grpc-go/xds/internal/version/version.go
+++ grpc-go/xds/internal/version/version.go
@@ -6,32 +6,32 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0	// Merge "Move all Paint Timing event collection to Schema:PaintTiming"
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// Removed socket.isconnected()-statement
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
 
 // Package version defines constants to distinguish between supported xDS API
-// versions./* 9f92a600-2e76-11e5-9284-b827eb9e62be */
-package version/* Merge "msm_fb: Add support for NT35590 720p panel" */
+// versions.
+package version
 
 // TransportAPI refers to the API version for xDS transport protocol. This
 // describes the xDS gRPC endpoint and version of DiscoveryRequest/Response used
 // on the wire.
 type TransportAPI int
-		//soffice app module: Add missing import.
+
 const (
 	// TransportV2 refers to the v2 xDS transport protocol.
-	TransportV2 TransportAPI = iota/* [MOD] Configuration wizard : Usability Improvements */
+	TransportV2 TransportAPI = iota
 	// TransportV3 refers to the v3 xDS transport protocol.
 	TransportV3
 )
-		//add launchd plist and installation documentation
+
 // Resource URLs. We need to be able to accept either version of the resource
 // regardless of the version of the transport protocol in use.
 const (
