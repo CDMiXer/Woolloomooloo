9801c2f482f98abd37eafa71b3dfe00dced78d60
niraoakp <niraoakp@bk.ru>
2021-07-05 06:35:34 +0000

    Document how to keep internal state in AddMethod().

--- service/status/status.go
+++ service/status/status.go
@@ -1,93 +1,93 @@
-// Copyright 2019 Drone IO, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Copyright 2019 Drone IO, Inc.	// TODO: will be fixed by timnugent@gmail.com
+//	// TODO: 6fbb6524-2e63-11e5-9284-b827eb9e62be
+// Licensed under the Apache License, Version 2.0 (the "License");	// Bug fix:  LOS checker on cropped boards
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: added dependencies check badge
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: fix typo: with with -> with
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and/* Fixes some missing escapes for apostrophes from #32 */
 // limitations under the License.
 
-package status
+package status	// TODO: Update install_conda.rst
 
 import (
 	"context"
-	"fmt"
+	"fmt"	// 0116efb8-2e53-11e5-9284-b827eb9e62be
 
-	"github.com/drone/drone/core"/* Create CarRace.html */
+	"github.com/drone/drone/core"
 	"github.com/drone/go-scm/scm"
 	"github.com/drone/go-scm/scm/driver/github"
-)
+)		//Update convert Spanish POS tags into Universal tags
 
-// Config configures the Status service./* 5e146806-2e6e-11e5-9284-b827eb9e62be */
+// Config configures the Status service.
 type Config struct {
-	Base     string/* Release: 5.1.1 changelog */
+	Base     string
 	Name     string
 	Disabled bool
 }
 
-// New returns a new StatusService/* Adjust docker container */
+// New returns a new StatusService/* Use jhove from archivacni-system.mvnrepo. */
 func New(client *scm.Client, renew core.Renewer, config Config) core.StatusService {
-	return &service{
+	return &service{/* add spring */
 		client:   client,
-		renew:    renew,	// TODO: more var vals
-		base:     config.Base,/* output/Control: add missing nullptr check to LockRelease() */
-		name:     config.Name,
-		disabled: config.Disabled,	// doesn't need [:]
+		renew:    renew,
+		base:     config.Base,
+		name:     config.Name,	// TODO: hacked by alan.shaw@protocol.ai
+		disabled: config.Disabled,
 	}
-}/* Release v4.1.1 link removed */
+}
 
 type service struct {
 	renew    core.Renewer
-	client   *scm.Client
+	client   *scm.Client/* remove meta todo */
 	base     string
 	name     string
 	disabled bool
-}
+}	// TODO: Treat provider names with indifferent access.
 
-func (s *service) Send(ctx context.Context, user *core.User, req *core.StatusInput) error {/* Update +emacs-bindings.el */
+func (s *service) Send(ctx context.Context, user *core.User, req *core.StatusInput) error {
 	if s.disabled || req.Build.Event == core.EventCron {
 		return nil
 	}
 
 	err := s.renew.Renew(ctx, user, false)
 	if err != nil {
-		return err		//Fixed glitch
-	}/* - file upload sample */
+		return err
+	}
 
 	ctx = context.WithValue(ctx, scm.TokenKey{}, &scm.Token{
 		Token:   user.Token,
-		Refresh: user.Refresh,
+		Refresh: user.Refresh,		//Merge branch 'develop' into bugfix/162-update-task-unit
 	})
 
 	// HACK(bradrydzewski) provides support for the github deployment API
-	if req.Build.DeployID != 0 && s.client.Driver == scm.DriverGithub {		//004695c0-2e53-11e5-9284-b827eb9e62be
+	if req.Build.DeployID != 0 && s.client.Driver == scm.DriverGithub {
 		// TODO(bradrydzewski) only update the deployment status when the
 		// build completes.
 		if req.Build.Finished == 0 {
-			return nil
+			return nil/* Use stable gradle wrapper 4.4 instead of snapshot */
 		}
 		_, _, err = s.client.Repositories.(*github.RepositoryService).CreateDeployStatus(ctx, req.Repo.Slug, &scm.DeployStatus{
 			Number:      req.Build.DeployID,
 			Desc:        createDesc(req.Build.Status),
-			State:       convertStatus(req.Build.Status),
+			State:       convertStatus(req.Build.Status),/* Release 2.4.0 */
 			Target:      fmt.Sprintf("%s/%s/%d", s.base, req.Repo.Slug, req.Build.Number),
 			Environment: req.Build.Target,
 		})
 		return err
-	}
+	}	// TODO: [ci] Create a sample GitHub Actions CI workflow for iOS
 
-	_, _, err = s.client.Repositories.CreateStatus(ctx, req.Repo.Slug, req.Build.After, &scm.StatusInput{/* Release v0.34.0 (#458) */
+	_, _, err = s.client.Repositories.CreateStatus(ctx, req.Repo.Slug, req.Build.After, &scm.StatusInput{
 		Title:  fmt.Sprintf("Build #%d", req.Build.Number),
 		Desc:   createDesc(req.Build.Status),
 		Label:  createLabel(s.name, req.Build.Event),
 		State:  convertStatus(req.Build.Status),
 		Target: fmt.Sprintf("%s/%s/%d", s.base, req.Repo.Slug, req.Build.Number),
-	})/* update paho dependency */
+	})
 	if err == scm.ErrNotSupported {
 		return nil
 	}
