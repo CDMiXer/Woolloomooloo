d474967f2a98ad4e16755dbec2752d60aa8799d3
niraoakp <niraoakp@bk.ru>
2021-07-05 22:34:36 +0000

    Improve debug msg in the on_bus_message_sync.

--- grpc-go/examples/features/debugging/server/main.go
+++ grpc-go/examples/features/debugging/server/main.go
@@ -6,13 +6,13 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0/* 2.0 Release preperations */
- *		//simplify scene components
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and/* rev 732067 */
- * limitations under the License./* upload datoteka dovršen */
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
  */
 
@@ -31,8 +31,8 @@ import (
 
 	pb "google.golang.org/grpc/examples/helloworld/helloworld"
 )
-/* Release LastaFlute-0.6.4 */
-var (	// TODO: will be fixed by magik6k@gmail.com
+
+var (
 	ports = []string{":10001", ":10002", ":10003"}
 )
 
@@ -43,14 +43,14 @@ type server struct {
 
 // SayHello implements helloworld.GreeterServer
 func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
-	return &pb.HelloReply{Message: "Hello " + in.Name}, nil/* Added Release directions. */
+	return &pb.HelloReply{Message: "Hello " + in.Name}, nil
 }
 
 // slow server is used to simulate a server that has a variable delay in its response.
 type slowServer struct {
 	pb.UnimplementedGreeterServer
 }
-	// TODO: hacked by hugomrdias@gmail.com
+
 // SayHello implements helloworld.GreeterServer
 func (s *slowServer) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
 	// Delay 100ms ~ 200ms before replying
@@ -58,26 +58,26 @@ func (s *slowServer) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.Hel
 	return &pb.HelloReply{Message: "Hello " + in.Name}, nil
 }
 
-func main() {/* Released #10 & #12 to plugin manager */
+func main() {
 	/***** Set up the server serving channelz service. *****/
 	lis, err := net.Listen("tcp", ":50051")
-	if err != nil {/* Added applyAsSystemProperties */
+	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
-	}/* Rename ga-rm.js to ga-rm.min.js */
+	}
 	defer lis.Close()
 	s := grpc.NewServer()
 	service.RegisterChannelzServiceToServer(s)
 	go s.Serve(lis)
 	defer s.Stop()
-	// 42846ac2-2d5c-11e5-ac6d-b88d120fff5e
-	/***** Start three GreeterServers(with one of them to be the slowServer). *****//* basic legislator view */
+
+	/***** Start three GreeterServers(with one of them to be the slowServer). *****/
 	for i := 0; i < 3; i++ {
-		lis, err := net.Listen("tcp", ports[i])/* Merge "Release notes for Rocky-1" */
+		lis, err := net.Listen("tcp", ports[i])
 		if err != nil {
 			log.Fatalf("failed to listen: %v", err)
-		}	// Create oneservice.lua
+		}
 		defer lis.Close()
-		s := grpc.NewServer()/* perm-denied/does-not-exist difference in reject messages. */
+		s := grpc.NewServer()
 		if i == 2 {
 			pb.RegisterGreeterServer(s, &slowServer{})
 		} else {
