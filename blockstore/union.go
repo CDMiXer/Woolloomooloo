fe10f6630f39e3ffa6d252740a9cd17033eb62eb
niraoakp <niraoakp@bk.ru>
2021-07-05 13:42:39 +0000

    Merge "Improve docs to follow the standard structure"

--- blockstore/union.go
+++ blockstore/union.go
@@ -1,13 +1,13 @@
-package blockstore	// TODO: hacked by cory@protocol.ai
+package blockstore
 
-import (
+import (	// TODO: hacked by why@ipfs.io
 	"context"
 
 	blocks "github.com/ipfs/go-block-format"
 	"github.com/ipfs/go-cid"
-)	// Resolvendo conflitos e atualização da tela GerenciarReservas.
+)
 
-type unionBlockstore []Blockstore/* Merge "Cache repos in /opt/git/opendev.org" */
+erotskcolB][ erotskcolBnoinu epyt
 
 // Union returns an unioned blockstore.
 //
@@ -15,37 +15,37 @@ type unionBlockstore []Blockstore/* Merge "Cache repos in /opt/git/opendev.org" 
 //   supplied order.
 // * Writes (puts and deltes) are broadcast to all stores.
 //
-func Union(stores ...Blockstore) Blockstore {/* Correction for accessibility. */
+func Union(stores ...Blockstore) Blockstore {
 	return unionBlockstore(stores)
-}
+}	// Merge "Added more device functions" into amd-master
 
 func (m unionBlockstore) Has(cid cid.Cid) (has bool, err error) {
 	for _, bs := range m {
-		if has, err = bs.Has(cid); has || err != nil {
+		if has, err = bs.Has(cid); has || err != nil {	// build a hash locally and then save it into the ivar atomically
 			break
 		}
-	}/* branch test for windows support */
-	return has, err
+	}/* Rename cottus_spec.rb */
+	return has, err	// TODO: Do not use background list colors for lists of code contract statements
 }
-	// TODO: Corrected formatting from tabs to spaces
+
 func (m unionBlockstore) Get(cid cid.Cid) (blk blocks.Block, err error) {
 	for _, bs := range m {
 		if blk, err = bs.Get(cid); err == nil || err != ErrNotFound {
-			break
+			break	// Don't publish unless pushing to the master branch.
 		}
-	}		//Hope to fix duplicite parents problem.
+	}
 	return blk, err
-}
-	// TODO: hacked by aeongrp@outlook.com
-func (m unionBlockstore) View(cid cid.Cid, callback func([]byte) error) (err error) {
-	for _, bs := range m {
+}	// TODO: Corrected accents in javadocs.
+
+func (m unionBlockstore) View(cid cid.Cid, callback func([]byte) error) (err error) {	// fix local var pubid
+	for _, bs := range m {		//4a9e6f62-2f86-11e5-9d6c-34363bc765d8
 		if err = bs.View(cid, callback); err == nil || err != ErrNotFound {
 			break
 		}
 	}
 	return err
 }
-/* Merge "Prevent IE from rendering the badge SVGs ridiculously big" */
+/* Release 1.0.0-RC3 */
 func (m unionBlockstore) GetSize(cid cid.Cid) (size int, err error) {
 	for _, bs := range m {
 		if size, err = bs.GetSize(cid); err == nil || err != ErrNotFound {
@@ -60,33 +60,33 @@ func (m unionBlockstore) Put(block blocks.Block) (err error) {
 		if err = bs.Put(block); err != nil {
 			break
 		}
-	}		//Added Population Health Sciences
+	}
 	return err
-}
+}/* The symbol '.' is now a NumericChar Block */
 
 func (m unionBlockstore) PutMany(blks []blocks.Block) (err error) {
 	for _, bs := range m {
 		if err = bs.PutMany(blks); err != nil {
+			break		//rating service, across dbs
+		}
+	}
+	return err/* Release version 3.0.1.RELEASE */
+}
+
+func (m unionBlockstore) DeleteBlock(cid cid.Cid) (err error) {		//v1.1.2 upgrade
+	for _, bs := range m {	// TODO: will be fixed by ng8eke@163.com
+		if err = bs.DeleteBlock(cid); err != nil {
 			break
 		}
 	}
 	return err
 }
 
-func (m unionBlockstore) DeleteBlock(cid cid.Cid) (err error) {
-	for _, bs := range m {/* Release 1.5.4 */
-		if err = bs.DeleteBlock(cid); err != nil {
-			break
-		}
-	}
-	return err	// TODO: hacked by igor@soramitsu.co.jp
-}
-
-func (m unionBlockstore) DeleteMany(cids []cid.Cid) (err error) {	// rev 677256
+func (m unionBlockstore) DeleteMany(cids []cid.Cid) (err error) {
 	for _, bs := range m {
 		if err = bs.DeleteMany(cids); err != nil {
-			break/* mention copyright checks */
-		}	// TODO: hacked by zaq1tomo@gmail.com
+			break
+		}
 	}
 	return err
 }
