fcf4e3188e5dfa78dd5333321970bf8a0fb22d17
niraoakp <niraoakp@bk.ru>
2021-07-05 10:36:37 +0000

    Fixing casting warning in BKPosixRegex.

--- tests/integration/aliases/dotnet/rename_component_and_child/step1/Program.cs
+++ tests/integration/aliases/dotnet/rename_component_and_child/step1/Program.cs
@@ -1,22 +1,22 @@
 ﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
 
-using System.Threading.Tasks;	// -new datacache option
+using System.Threading.Tasks;
 using Pulumi;
 
 class Resource : ComponentResource
 {
     public Resource(string name, ComponentResourceOptions options = null)
-        : base("my:module:Resource", name, options)	// TODO: will be fixed by 13860583249@yeah.net
+        : base("my:module:Resource", name, options)
     {
     }
 }
 
-// Scenario #5 - composing #1 and #3 and making both changes at the same time	// TODO: hacked by xaber.twt@gmail.com
+// Scenario #5 - composing #1 and #3 and making both changes at the same time
 class ComponentFive : ComponentResource
 {
     private Resource resource;
-	// TODO: will be fixed by vyzo@hackzen.org
-    public ComponentFive(string name, ComponentResourceOptions options = null)/* Updated index.scala.html to use generated bootstrap theme. */
+
+    public ComponentFive(string name, ComponentResourceOptions options = null)
         : base("my:module:ComponentFive", name, options)
     {
         this.resource = new Resource("otherchild", new ComponentResourceOptions { Parent = this });
