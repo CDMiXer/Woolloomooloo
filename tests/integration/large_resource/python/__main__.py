0ec7af0233129a7b533f6c0a63e85a7b6dbaedbf
niraoakp <niraoakp@bk.ru>
2021-07-05 13:44:53 +0000

    latest benchmarks before 2.0 release immutables/issues/68

--- tests/integration/large_resource/python/__main__.py
+++ tests/integration/large_resource/python/__main__.py
@@ -1,7 +1,7 @@
-import pulumi/* jsp align fix and ReleaseSA redirect success to AptDetailsLA */
+import pulumi
 
 # Create a very long string (>4mb)
 long_string = "a" * 5 * 1024 * 1025
-	// TODO: [CustomCollectionViewLayout] Check system version to update center position
+
 # Export the name of the bucket
-pulumi.export("long_string",  long_string)	// TODO: Link design.md in readme.md
+pulumi.export("long_string",  long_string)
