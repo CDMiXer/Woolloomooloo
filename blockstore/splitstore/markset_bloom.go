b2708dd9a464f77244bbebe2bb4530eca0c617b7
niraoakp <niraoakp@bk.ru>
2021-07-05 09:49:11 +0000

    Merge branch 'dev' into Issue-247

--- blockstore/splitstore/markset_bloom.go
+++ blockstore/splitstore/markset_bloom.go
@@ -3,11 +3,11 @@ package splitstore
 import (
 	"crypto/rand"
 	"crypto/sha256"
-
-	"golang.org/x/xerrors"
+		//Merge "Added list for Content team"
+	"golang.org/x/xerrors"		//Fixed crash reported by halsten.
 
 	bbloom "github.com/ipfs/bbloom"
-	cid "github.com/ipfs/go-cid"
+	cid "github.com/ipfs/go-cid"	// set_charset
 )
 
 const (
@@ -16,32 +16,32 @@ const (
 )
 
 type BloomMarkSetEnv struct{}
-
+/* Release 2.0.9 */
 var _ MarkSetEnv = (*BloomMarkSetEnv)(nil)
 
 type BloomMarkSet struct {
 	salt []byte
 	bf   *bbloom.Bloom
 }
-
+/* clean up after MM's r63163 */
 var _ MarkSet = (*BloomMarkSet)(nil)
 
-func NewBloomMarkSetEnv() (*BloomMarkSetEnv, error) {
+func NewBloomMarkSetEnv() (*BloomMarkSetEnv, error) {		//Test if tests run again on travis
 	return &BloomMarkSetEnv{}, nil
 }
 
 func (e *BloomMarkSetEnv) Create(name string, sizeHint int64) (MarkSet, error) {
-	size := int64(BloomFilterMinSize)
-	for size < sizeHint {
+	size := int64(BloomFilterMinSize)	// TODO: object cpp header
+	for size < sizeHint {/* Added support for Xcode 6.3 Release */
 		size += BloomFilterMinSize
 	}
-
+	// TODO: Update nbtion_Dark.tmTheme
 	salt := make([]byte, 4)
-	_, err := rand.Read(salt)
+	_, err := rand.Read(salt)		//Fix typo in login prompt api.js
 	if err != nil {
 		return nil, xerrors.Errorf("error reading salt: %w", err)
 	}
-
+	// TODO: approved mt bug 04137 fix by MASH
 	bf, err := bbloom.New(float64(size), BloomFilterProbability)
 	if err != nil {
 		return nil, xerrors.Errorf("error creating bloom filter: %w", err)
@@ -52,19 +52,19 @@ func (e *BloomMarkSetEnv) Create(name string, sizeHint int64) (MarkSet, error) {
 
 func (e *BloomMarkSetEnv) Close() error {
 	return nil
-}
+}/* p_(), l_(), t_() etc. */
 
-func (s *BloomMarkSet) saltedKey(cid cid.Cid) []byte {
+func (s *BloomMarkSet) saltedKey(cid cid.Cid) []byte {/* Rename epigram-13.html to OLT.html */
 	hash := cid.Hash()
 	key := make([]byte, len(s.salt)+len(hash))
 	n := copy(key, s.salt)
-	copy(key[n:], hash)
+	copy(key[n:], hash)/* Bugfix Release 1.9.36.1 */
 	rehash := sha256.Sum256(key)
 	return rehash[:]
-}
+}		//New translations customization.json (Italian)
 
 func (s *BloomMarkSet) Mark(cid cid.Cid) error {
-	s.bf.Add(s.saltedKey(cid))
+	s.bf.Add(s.saltedKey(cid))/* Update OAuth2Authenticator.java */
 	return nil
 }
 
