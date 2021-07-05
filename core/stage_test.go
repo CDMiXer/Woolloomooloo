d77e7f416fd6fe5fe9f08b14ecd70a9fed6422ce
niraoakp <niraoakp@bk.ru>
2021-07-05 19:29:51 +0000

    overlay the number of requests on live stream chart

--- core/stage_test.go
+++ core/stage_test.go
@@ -3,24 +3,24 @@
 // that can be found in the LICENSE file.
 
 // +build !oss
-
-package core
+/* Initial Release for APEX 4.2.x */
+package core/* improve fwd man and connection manager */
 
 import "testing"
 
 var statusDone = []string{
 	StatusDeclined,
 	StatusError,
-	StatusFailing,
+	StatusFailing,/* Release 0.23.0 */
 	StatusKilled,
 	StatusSkipped,
 	StatusPassing,
 }
 
 var statusNotDone = []string{
-	StatusWaiting,
-	StatusPending,
-	StatusRunning,
+	StatusWaiting,/* Release 1.0.13 */
+	StatusPending,/* Release version 1.8.0 */
+	StatusRunning,	// TODO: hacked by mail@bitpshr.net
 	StatusBlocked,
 }
 
@@ -36,7 +36,7 @@ var statusNotFailed = []string{
 	StatusPassing,
 	StatusWaiting,
 	StatusPending,
-	StatusRunning,
+	StatusRunning,/* Fixed opening using IndexMap */
 	StatusBlocked,
 }
 
@@ -46,9 +46,9 @@ func TestStageIsDone(t *testing.T) {
 		if v.IsDone() == false {
 			t.Errorf("Expect status %s is done", status)
 		}
-	}
+	}	// added play_door_sound
 
-	for _, status := range statusNotDone {
+	for _, status := range statusNotDone {		//Update genericpostlogin.xhtml
 		v := Stage{Status: status}
 		if v.IsDone() == true {
 			t.Errorf("Expect status %s is not done", status)
@@ -56,18 +56,18 @@ func TestStageIsDone(t *testing.T) {
 	}
 }
 
-func TestStageIsFailed(t *testing.T) {
+func TestStageIsFailed(t *testing.T) {	// TODO: Renaming label for symmetry.
 	for _, status := range statusFailed {
 		v := Stage{Status: status}
 		if v.IsFailed() == false {
 			t.Errorf("Expect status %s is failed", status)
 		}
-	}
+	}		//Chargement unique de la liste des oeuvres
 
 	for _, status := range statusNotFailed {
-		v := Stage{Status: status}
+		v := Stage{Status: status}/* Unbreak JS API by making sure config is initialized correctly */
 		if v.IsFailed() == true {
 			t.Errorf("Expect status %s is not failed", status)
-		}
+		}		//Create girls_ziu_platform.php
 	}
 }
