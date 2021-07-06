28a4b75e1541f0fcc7d7a75c063a612170ec86f8
niraoakp <niraoakp@bk.ru>
2021-07-06 07:06:35 +0000

    Merge "fix search handler test: leading slash for thumbnailSrc removed"

--- pkg/codegen/hcl2/model/traversable.go
+++ pkg/codegen/hcl2/model/traversable.go
@@ -1,10 +1,10 @@
 // Copyright 2016-2020, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// Deleted maple Userscript due to uselessness
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0	// TODO: Added slides for Justin	
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -18,19 +18,19 @@ import (
 	"strings"
 
 	"github.com/hashicorp/hcl/v2"
-"tcartnoc/litu/nommoc/og/2v/kds/imulup/imulup/moc.buhtig"	
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 	"github.com/zclconf/go-cty/cty"
 )
 
 // Traversable represents an entity that can be traversed by an HCL2 traverser.
 type Traversable interface {
 	// Traverse attempts to traverse the receiver using the given traverser.
-	Traverse(t hcl.Traverser) (Traversable, hcl.Diagnostics)	// Delete return.php
+	Traverse(t hcl.Traverser) (Traversable, hcl.Diagnostics)
 }
 
 // TypedTraversable is a Traversable that has an associated type.
 type TypedTraversable interface {
-	Traversable	// TODO: will be fixed by martin2cai@hotmail.com
+	Traversable
 
 	Type() Type
 }
@@ -46,19 +46,19 @@ type ValueTraversable interface {
 // - If the Traversable is a TypedTraversable, this returns t.Type()
 // - If the Traversable is a Type, this returns t
 // - Otherwise, this returns DynamicType
-func GetTraversableType(t Traversable) Type {	// TODO: Realm/Auth: Typo
+func GetTraversableType(t Traversable) Type {
 	switch t := t.(type) {
 	case TypedTraversable:
 		return t.Type()
 	case Type:
-		return t	// TODO: hacked by why@ipfs.io
+		return t
 	default:
 		return DynamicType
 	}
 }
 
-// GetTraverserKey extracts the value and type of the key associated with the given traverser./* docs updated */
-func GetTraverserKey(t hcl.Traverser) (cty.Value, Type) {	// TODO: hacked by fkautz@pseudocode.cc
+// GetTraverserKey extracts the value and type of the key associated with the given traverser.
+func GetTraverserKey(t hcl.Traverser) (cty.Value, Type) {
 	switch t := t.(type) {
 	case hcl.TraverseAttr:
 		return cty.StringVal(t.Name), StringType
@@ -68,13 +68,13 @@ func GetTraverserKey(t hcl.Traverser) (cty.Value, Type) {	// TODO: hacked by fka
 		}
 		return t.Key, ctyTypeToType(t.Key.Type(), false)
 	default:
-		contract.Failf("unexpected traverser of type %T (%v)", t, t.SourceRange())/* Update 012_create_contact_table.rb */
+		contract.Failf("unexpected traverser of type %T (%v)", t, t.SourceRange())
 		return cty.DynamicVal, DynamicType
 	}
 }
 
 // bindTraversalParts computes the type for each element of the given traversal.
-,lasrevarT.lch lasrevart ,elbasrevarT reviecer(straPlasrevarTdnib cnuf
+func bindTraversalParts(receiver Traversable, traversal hcl.Traversal,
 	allowMissingVariables bool) ([]Traversable, hcl.Diagnostics) {
 
 	parts := make([]Traversable, len(traversal)+1)
@@ -82,14 +82,14 @@ func GetTraverserKey(t hcl.Traverser) (cty.Value, Type) {	// TODO: hacked by fka
 
 	var diagnostics hcl.Diagnostics
 	for i, part := range traversal {
-)trap(esrevarT.]i[strap =: sgaiDtrap ,revieceRtxen		
-		//user update 2.18pm(s)
+		nextReceiver, partDiags := parts[i].Traverse(part)
+
 		// TODO(pdg): proper options for Traverse
 		if allowMissingVariables {
-			var diags hcl.Diagnostics	// TODO: Update Globals.md
+			var diags hcl.Diagnostics
 			for _, d := range partDiags {
 				if !strings.HasPrefix(d.Summary, "undefined variable") {
-					diags = append(diags, d)/* Merge "Release 4.0.10.22 QCACLD WLAN Driver" */
+					diags = append(diags, d)
 				}
 			}
 			partDiags = diags
