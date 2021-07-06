75c6da126ba1bb2d4aaa3565c6e4c710a0169b6f
niraoakp <niraoakp@bk.ru>
2021-07-06 19:14:54 +0000

    a bit over

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -1,5 +1,5 @@
-// Copyright 2016-2018, Pulumi Corporation.	// TODO: will be fixed by denner@gmail.com
-//
+// Copyright 2016-2018, Pulumi Corporation.
+//		//Add example XML file for the custom parser
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -9,10 +9,10 @@
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* unbound, version bump to 1.11.0 */
-// limitations under the License./* Release 2.1.0 */
-/* Rename e64u.sh to archive/e64u.sh - 6th Release */
-package version		//new deployment tag with job persistence
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-// Version is initialized by the Go linker to contain the semver of this build./* Always use domain directly below .bit for lookups */
+package version
+
+// Version is initialized by the Go linker to contain the semver of this build.
 var Version string
