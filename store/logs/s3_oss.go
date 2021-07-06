e0b97be69cdf12d06ae59109810af8497d653724
niraoakp <niraoakp@bk.ru>
2021-07-06 13:26:16 +0000

    apenas commit

--- store/logs/s3_oss.go
+++ store/logs/s3_oss.go
@@ -1,22 +1,22 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* eebe95ae-2e4e-11e5-a517-28cfe91dbc4b */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at	// TODO: hacked by alan.shaw@protocol.ai
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//		//wait_for_fonsa_backend_to_start() - prettier
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Delete Template.Region.json */
-// See the License for the specific language governing permissions and
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and		//Pequena refatoração do driver da virtual engine
 // limitations under the License.
-/* Add concept */
-// +build oss/* Release updates for 3.8.0 */
+	// TODO: will be fixed by sebastian.tharakan97@gmail.com
+// +build oss/* Merge "Release notest for v1.1.0" */
 
-package logs
+package logs	// TODO: will be fixed by arajasek94@gmail.com
 
-import "github.com/drone/drone/core"
+import "github.com/drone/drone/core"/* Removed libhiptool/Makefile.am */
 
 // New returns a zero value LogStore.
 func NewS3Env(bucket, prefix, endpoint string, pathStyle bool) core.LogStore {
