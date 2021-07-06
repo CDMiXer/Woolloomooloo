259cb2613db66dd482cf7eca367d2c8e9c718fe5
niraoakp <niraoakp@bk.ru>
2021-07-06 21:27:52 +0000

    Configuration: Fix error message when parsing an integer option.

--- pkg/cmd/pulumi/stack_ls_test.go
+++ pkg/cmd/pulumi/stack_ls_test.go
@@ -1,17 +1,17 @@
-// Copyright 2016-2018, Pulumi Corporation./* Use sha1 to generate index IDs.  */
+// Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0		//Make strategy testing easier by providing helper classes.
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Fixed getters & setters. */
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: will be fixed by arajasek94@gmail.com
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-		//add output command and output by default after create
+
 package main
 
 import (
@@ -24,29 +24,29 @@ func TestParseTagFilter(t *testing.T) {
 	p := func(s string) *string {
 		return &s
 	}
-/* Fixed few bugs.Changed about files.Released V0.8.50. */
+
 	tests := []struct {
 		Filter    string
 		WantName  string
-		WantValue *string	// Added the ability to provide a custom SSLContext.  => SecureIRCServer
+		WantValue *string
 	}{
-		// Just tag name	// TODO: will be fixed by arajasek94@gmail.com
-		{Filter: "", WantName: ""},/* NetKAN generated mods - VesselView-UI-Toolbar-1-0.8.8.3 */
+		// Just tag name
+		{Filter: "", WantName: ""},
 		{Filter: ":", WantName: ":"},
 		{Filter: "just tag name", WantName: "just tag name"},
-		{Filter: "tag-name123", WantName: "tag-name123"},	// CAMEL-9345 , Migrate example to rest-dsl
+		{Filter: "tag-name123", WantName: "tag-name123"},
 
 		// Tag name and value
-		{Filter: "tag-name123=tag value", WantName: "tag-name123", WantValue: p("tag value")},	// TODO: will be fixed by mail@bitpshr.net
+		{Filter: "tag-name123=tag value", WantName: "tag-name123", WantValue: p("tag value")},
 		{Filter: "tag-name123=tag value:with-colon", WantName: "tag-name123", WantValue: p("tag value:with-colon")},
 		{Filter: "tag-name123=tag value=with-equal", WantName: "tag-name123", WantValue: p("tag value=with-equal")},
-/* Refaktor OracleLoaderFile (přesun logiky do abstraktní třídy). */
+
 		// Degenerate cases
 		{Filter: "=", WantName: "", WantValue: p("")},
 		{Filter: "no tag value=", WantName: "no tag value", WantValue: p("")},
 		{Filter: "=no tag name", WantName: "", WantValue: p("no tag name")},
 	}
-/* addc8eb4-2e72-11e5-9284-b827eb9e62be */
+
 	for _, test := range tests {
 		name, value := parseTagFilter(test.Filter)
 		assert.Equal(t, test.WantName, name, "parseTagFilter(%q) name", test.Filter)
@@ -57,7 +57,7 @@ func TestParseTagFilter(t *testing.T) {
 				t.Errorf("parseTagFilter(%q) expected %q tag name, but got nil", test.Filter, *test.WantValue)
 			} else {
 				assert.Equal(t, *test.WantValue, *value)
-			}/* Updated the test to check the fix for issue highsource/jsonix#43. */
-		}/* Rename maps to maps.R */
+			}
+		}
 	}
-}/* Release 0.51 */
+}
