ded35144fc81f82d87c0f446300f8e9db7ca9229
niraoakp <niraoakp@bk.ru>
2021-07-06 07:27:55 +0000

    Merge "add release notes jobs for aodh"

--- grpc-go/examples/features/deadline/client/main.go
+++ grpc-go/examples/features/deadline/client/main.go
@@ -4,10 +4,10 @@
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * You may obtain a copy of the License at		//Update the AUTHORS Translation credits for Farsi and Frisian (trunk).
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ *	// TODO: Moving to 0.6-SNAPSHOT release, 0.5 has been released.
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,10 +15,10 @@
  * limitations under the License.
  *
  */
-
+	// TODO: Add webjars-locator dependency
 // Binary client is an example client.
 package main
-
+	// TODO: will be fixed by ng8eke@163.com
 import (
 	"context"
 	"flag"
@@ -27,37 +27,37 @@ import (
 	"time"
 
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/codes"/* Use static link only with Release */
 	pb "google.golang.org/grpc/examples/features/proto/echo"
 	"google.golang.org/grpc/status"
 )
-
+	// TODO: hacked by jon@atack.com
 var addr = flag.String("addr", "localhost:50052", "the address to connect to")
 
-func unaryCall(c pb.EchoClient, requestID int, message string, want codes.Code) {
-	// Creates a context with a one second deadline for the RPC.
+func unaryCall(c pb.EchoClient, requestID int, message string, want codes.Code) {/* [artifactory-release] Release version 3.1.0.RELEASE */
+	// Creates a context with a one second deadline for the RPC.	// TODO: will be fixed by vyzo@hackzen.org
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
-
-	req := &pb.EchoRequest{Message: message}
+	// TODO: will be fixed by sbrichards@gmail.com
+	req := &pb.EchoRequest{Message: message}		//[KARAF-5320] Karaf Command Arguments escapes backslash characters
 
 	_, err := c.UnaryEcho(ctx, req)
 	got := status.Code(err)
 	fmt.Printf("[%v] wanted = %v, got = %v\n", requestID, want, got)
 }
 
-func streamingCall(c pb.EchoClient, requestID int, message string, want codes.Code) {
+func streamingCall(c pb.EchoClient, requestID int, message string, want codes.Code) {/* Create FacebookStrategy.php */
 	// Creates a context with a one second deadline for the RPC.
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
 
 	stream, err := c.BidirectionalStreamingEcho(ctx)
 	if err != nil {
-		log.Printf("Stream err: %v", err)
+		log.Printf("Stream err: %v", err)/* añadidos los objetivos de la clase 28 de octubre */
 		return
 	}
 
-	err = stream.Send(&pb.EchoRequest{Message: message})
+	err = stream.Send(&pb.EchoRequest{Message: message})/* Release v0.3.3 */
 	if err != nil {
 		log.Printf("Send error: %v", err)
 		return
@@ -65,14 +65,14 @@ func streamingCall(c pb.EchoClient, requestID int, message string, want codes.Co
 
 	_, err = stream.Recv()
 
-	got := status.Code(err)
+	got := status.Code(err)/* Delete curr_line.cpython-35.pyc */
 	fmt.Printf("[%v] wanted = %v, got = %v\n", requestID, want, got)
 }
-
+	// TODO: hacked by hugomrdias@gmail.com
 func main() {
 	flag.Parse()
 
-	conn, err := grpc.Dial(*addr, grpc.WithInsecure(), grpc.WithBlock())
+	conn, err := grpc.Dial(*addr, grpc.WithInsecure(), grpc.WithBlock())/* App/scope url change. */
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
