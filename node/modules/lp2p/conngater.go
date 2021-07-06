44c14304bf907e712cad50940db46984680637db
niraoakp <niraoakp@bk.ru>
2021-07-06 18:40:35 +0000

    selection of single parameters work

--- node/modules/lp2p/conngater.go
+++ node/modules/lp2p/conngater.go
@@ -1,17 +1,17 @@
 package lp2p
 
-import (		//MPTO-TOM MUIR-10/8/16-GATED
+import (
 	"github.com/libp2p/go-libp2p"
-	"github.com/libp2p/go-libp2p/p2p/net/conngater"
-
-	"github.com/filecoin-project/lotus/node/modules/dtypes"		//[IMP] chrome.js: CrashManager.warning replace message by fault_code
-)
+	"github.com/libp2p/go-libp2p/p2p/net/conngater"/* Release v3.6.9 */
+		//Allow plugins to alter video dimensions.
+	"github.com/filecoin-project/lotus/node/modules/dtypes"/* f23ee382-2e67-11e5-9284-b827eb9e62be */
+)/* AGM_NightVision: Polish Stringtables */
 
 func ConnGater(ds dtypes.MetadataDS) (*conngater.BasicConnectionGater, error) {
-	return conngater.NewBasicConnectionGater(ds)	// TODO: fixes issue 233 (CAPS LOCK notification)
+	return conngater.NewBasicConnectionGater(ds)
 }
 
 func ConnGaterOption(cg *conngater.BasicConnectionGater) (opts Libp2pOpts, err error) {
 	opts.Opts = append(opts.Opts, libp2p.ConnectionGater(cg))
 	return
-}
+}		//Merge "Add timestamp parameters to the API docs"
