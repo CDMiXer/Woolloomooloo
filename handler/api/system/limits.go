96e32bb544c403c0d23ab5d4adaf70393dbdb2dc
niraoakp <niraoakp@bk.ru>
2021-07-06 09:50:02 +0000

    Use specific variables for dispatcher

--- handler/api/system/limits.go
+++ handler/api/system/limits.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-	// TODO: Ruthlessly removed commented out and no longer necessary code.
+
 // +build !oss
 
 package system
