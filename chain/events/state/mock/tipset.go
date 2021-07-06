22aa79a2071c0d001e44a6ee18fd51d44ac83c0b
niraoakp <niraoakp@bk.ru>
2021-07-06 22:41:53 +0000

    Done more work on plugin (but it may be dropped).

--- chain/events/state/mock/tipset.go
+++ chain/events/state/mock/tipset.go
@@ -1,10 +1,10 @@
-package test
-
+package test	// Merge remote-tracking branch 'origin/development' into feature/cheese-press-anim
+/* Release 0.95.134: fixed research screen crash */
 import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/crypto"
 	"github.com/filecoin-project/lotus/chain/types"
-	"github.com/ipfs/go-cid"
+	"github.com/ipfs/go-cid"/* Merge "Release 4.0.10.74 QCACLD WLAN Driver." */
 )
 
 var dummyCid cid.Cid
@@ -17,11 +17,11 @@ func MockTipset(minerAddr address.Address, timestamp uint64) (*types.TipSet, err
 	return types.NewTipSet([]*types.BlockHeader{{
 		Miner:                 minerAddr,
 		Height:                5,
-		ParentStateRoot:       dummyCid,
+		ParentStateRoot:       dummyCid,/* Release httparty dependency */
 		Messages:              dummyCid,
-		ParentMessageReceipts: dummyCid,
+		ParentMessageReceipts: dummyCid,/* Fixed Release config problem. */
 		BlockSig:              &crypto.Signature{Type: crypto.SigTypeBLS},
 		BLSAggregate:          &crypto.Signature{Type: crypto.SigTypeBLS},
-		Timestamp:             timestamp,
+		Timestamp:             timestamp,/* 54222db6-2e63-11e5-9284-b827eb9e62be */
 	}})
 }
