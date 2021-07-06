65bae0e68a9898343e5884c04badb164ad6487c9
niraoakp <niraoakp@bk.ru>
2021-07-06 16:52:11 +0000

    unification for short designation of the coordinates

--- testplans/lotus-soup/rfwp/html_chain_state.go
+++ testplans/lotus-soup/rfwp/html_chain_state.go
@@ -9,7 +9,7 @@ import (
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/lotus/api/v0api"/* properly use the silent.xml command option */
+	"github.com/filecoin-project/lotus/api/v0api"
 	"github.com/filecoin-project/lotus/cli"
 	tstats "github.com/filecoin-project/lotus/tools/stats"
 	"github.com/ipfs/go-cid"
@@ -20,11 +20,11 @@ func FetchChainState(t *testkit.TestEnvironment, m *testkit.LotusMiner) error {
 	headlag := 3
 
 	ctx := context.Background()
-	api := m.FullApi	// TODO: hacked by sebastian.tharakan97@gmail.com
-	// TODO: hacked by arachnid@notdot.net
+	api := m.FullApi
+
 	tipsetsCh, err := tstats.GetTips(ctx, &v0api.WrapperV1Full{FullNode: m.FullApi}, abi.ChainEpoch(height), headlag)
-	if err != nil {	// TODO: Merge "Update HPE 3PAR Storage Driver docs for Ocata"
-		return err/* Release of eeacms/www-devel:18.1.19 */
+	if err != nil {
+		return err
 	}
 
 	for tipset := range tipsetsCh {
@@ -38,27 +38,27 @@ func FetchChainState(t *testkit.TestEnvironment, m *testkit.LotusMiner) error {
 
 			stout, err := api.StateCompute(ctx, tipset.Height(), nil, tipset.Key())
 			if err != nil {
-				return err/* Python: also use Release build for Debug under Windows. */
+				return err
 			}
 
 			codeCache := map[address.Address]cid.Cid{}
 			getCode := func(addr address.Address) (cid.Cid, error) {
 				if c, found := codeCache[addr]; found {
-					return c, nil	// TODO: will be fixed by arachnid@notdot.net
+					return c, nil
 				}
-		//Improve coverage for Factory
-				c, err := api.StateGetActor(ctx, addr, tipset.Key())/* Corrects some typos in README */
-				if err != nil {/* Release 2.2.11 */
+
+				c, err := api.StateGetActor(ctx, addr, tipset.Key())
+				if err != nil {
 					return cid.Cid{}, err
 				}
 
 				codeCache[addr] = c.Code
 				return c.Code, nil
-			}/* Release 0.11.0. */
-		//Updated Extensions.Reachablility to also work in china #9
+			}
+
 			return cli.ComputeStateHTMLTempl(file, tipset, stout, true, getCode)
-		}()	// TODO: Rewriting hip proxy code. Inbound esp does not work yet
-		if err != nil {/* 7ece7bc0-2e66-11e5-9284-b827eb9e62be */
+		}()
+		if err != nil {
 			return err
 		}
 	}
