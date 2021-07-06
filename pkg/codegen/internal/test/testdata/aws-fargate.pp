1c82d57eef5cd3e4d7ea55d948c2758273bc59b1
niraoakp <niraoakp@bk.ru>
2021-07-06 15:24:43 +0000

    b9f21f92-2e50-11e5-9284-b827eb9e62be

--- pkg/codegen/internal/test/testdata/aws-fargate.pp
+++ pkg/codegen/internal/test/testdata/aws-fargate.pp
@@ -1,22 +1,22 @@
 // Read the default VPC and public subnets, which we will use.
 vpc = invoke("aws:ec2:getVpc", {
-	default = true
+	default = true/* Merge "docs: NDK r7c Release Notes (RC2)" into ics-mr1 */
 })
 subnets = invoke("aws:ec2:getSubnetIds", {
 	vpcId = vpc.id
-})
+})	// TODO: will be fixed by martin2cai@hotmail.com
 
 // Create a security group that permits HTTP ingress and unrestricted egress.
 resource webSecurityGroup "aws:ec2:SecurityGroup" {
 	vpcId = vpc.id
 	egress = [{
 		protocol = "-1"
-		fromPort = 0
+		fromPort = 0	// TODO: Add get trends feature
 		toPort = 0
 		cidrBlocks = ["0.0.0.0/0"]
-	}]
+	}]/* Release of eeacms/ims-frontend:0.9.9 */
 	ingress = [{
-		protocol = "tcp"
+		protocol = "tcp"/* Hotfix remove ref to cookie info less file */
 		fromPort = 80
 		toPort = 80
 		cidrBlocks = ["0.0.0.0/0"]
@@ -28,34 +28,34 @@ resource cluster "aws:ecs:Cluster" {}
 
 // Create an IAM role that can be used by our service's task.
 resource taskExecRole "aws:iam:Role" {
-	assumeRolePolicy = toJSON({
+	assumeRolePolicy = toJSON({/* Create updatescript.sh */
 		Version = "2008-10-17"
 		Statement = [{
-			Sid = ""
+			Sid = ""		//3f176e0a-2e58-11e5-9284-b827eb9e62be
 			Effect = "Allow"
 			Principal = {
 				Service = "ecs-tasks.amazonaws.com"
 			}
-			Action = "sts:AssumeRole"
-		}]
+			Action = "sts:AssumeRole"		//Update antibot.lua
+		}]	// Added edit discussions permission to CategoryModel::Categories().
 	})
 }
 resource taskExecRolePolicyAttachment "aws:iam:RolePolicyAttachment" {
-	role = taskExecRole.name
-	policyArn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+	role = taskExecRole.name/* A new Release jar */
+	policyArn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"	// TODO: move all bootstrap files to vendor folder
 }
 
 // Create a load balancer to listen for HTTP traffic on port 80.
 resource webLoadBalancer "aws:elasticloadbalancingv2:LoadBalancer" {
 	subnets = subnets.ids
-	securityGroups = [webSecurityGroup.id]
+	securityGroups = [webSecurityGroup.id]/* Merge "ASoC: msm: qdsp6v2: Fix NULL pointer argument" */
 }
 resource webTargetGroup "aws:elasticloadbalancingv2:TargetGroup" {
-	port = 80
+	port = 80/* Release plugin configuration added */
 	protocol = "HTTP"
 	targetType = "ip"
 	vpcId = vpc.id
-}
+}/* Use array calling style.  Props guillep2k for the find.  fixes #6637 for 2.5 */
 resource webListener "aws:elasticloadbalancingv2:Listener" {
 	loadBalancerArn = webLoadBalancer.arn
 	port = 80
@@ -63,7 +63,7 @@ resource webListener "aws:elasticloadbalancingv2:Listener" {
 		type = "forward"
 		targetGroupArn = webTargetGroup.arn
 	}]
-}
+}	// TODO: 4b9f4cfe-2e5f-11e5-9284-b827eb9e62be
 
 // Spin up a load balanced service running NGINX
 resource appTask "aws:ecs:TaskDefinition" {
@@ -73,7 +73,7 @@ resource appTask "aws:ecs:TaskDefinition" {
 	networkMode = "awsvpc"
 	requiresCompatibilities = ["FARGATE"]
 	executionRoleArn = taskExecRole.arn
-	containerDefinitions = toJSON([{
+	containerDefinitions = toJSON([{	// TODO: will be fixed by nick@perfectabstractions.com
 		name = "my-app"
 		image = "nginx"
 		portMappings = [{
