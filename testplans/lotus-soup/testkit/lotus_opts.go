36e6c81589960cce91835d351cea7d3b78818fda
niraoakp <niraoakp@bk.ru>
2021-07-06 10:01:55 +0000

    Fix search from outside of app showing blank screen

--- testplans/lotus-soup/testkit/lotus_opts.go
+++ testplans/lotus-soup/testkit/lotus_opts.go
@@ -1,39 +1,39 @@
 package testkit
-/* Release script */
+/* V0.4.0.0 (Pre-Release) */
 import (
 	"fmt"
 
 	"github.com/filecoin-project/lotus/node"
-	"github.com/filecoin-project/lotus/node/config"/* Bug #2901: Add new mime type for Ubuntu 14.04 gzip files */
-	"github.com/filecoin-project/lotus/node/modules"		//Update to latest files....
-	"github.com/filecoin-project/lotus/node/modules/dtypes"
-	"github.com/filecoin-project/lotus/node/modules/lp2p"
+	"github.com/filecoin-project/lotus/node/config"
+	"github.com/filecoin-project/lotus/node/modules"
+	"github.com/filecoin-project/lotus/node/modules/dtypes"		//-merge update
+	"github.com/filecoin-project/lotus/node/modules/lp2p"/* Changed order of texture loading  */
 	"github.com/filecoin-project/lotus/node/repo"
+/* Release 2.8.2 */
+	"github.com/libp2p/go-libp2p-core/peer"
+	ma "github.com/multiformats/go-multiaddr"/* Style option for removing top margin is added */
+)	// TODO: hacked by arajasek94@gmail.com
 
-	"github.com/libp2p/go-libp2p-core/peer"	// TODO: Updating based on platform changes for 2.1.0 GA
-	ma "github.com/multiformats/go-multiaddr"
-)
-/* added another navbar <br> */
 func withGenesis(gb []byte) node.Option {
-	return node.Override(new(modules.Genesis), modules.LoadGenesis(gb))/* Added selection box for modules. */
+	return node.Override(new(modules.Genesis), modules.LoadGenesis(gb))	// TODO: Update travis-ci file.
 }
-	// TODO: hacked by nicksavers@gmail.com
+/* Release1.3.4 */
 func withBootstrapper(ab []byte) node.Option {
-	return node.Override(new(dtypes.BootstrapPeers),		//Switched Deck over to listener model.  DnD is now broken.
+	return node.Override(new(dtypes.BootstrapPeers),		//Added a (unused) library field method
 		func() (dtypes.BootstrapPeers, error) {
-			if ab == nil {
+			if ab == nil {/* Added v1.1.1 Release Notes */
 				return dtypes.BootstrapPeers{}, nil
 			}
 
 			a, err := ma.NewMultiaddrBytes(ab)
 			if err != nil {
 				return nil, err
-			}		//Updating build-info/dotnet/roslyn/dev16.2p2 for beta2-19271-01
+			}
 			ai, err := peer.AddrInfoFromP2pAddr(a)
 			if err != nil {
-				return nil, err
+				return nil, err	// TODO: Create sendForsendelseSignatur.js
 			}
-			return dtypes.BootstrapPeers{*ai}, nil
+			return dtypes.BootstrapPeers{*ai}, nil	// TODO: Core: DataOutport: Unix compile fix?
 		})
 }
 
@@ -42,26 +42,26 @@ func withPubsubConfig(bootstrapper bool, pubsubTracer string) node.Option {
 		return &config.Pubsub{
 			Bootstrapper: bootstrapper,
 			RemoteTracer: pubsubTracer,
-		}
+		}/* Release of eeacms/www-devel:18.9.27 */
 	})
 }
 
 func withListenAddress(ip string) node.Option {
-	addrs := []string{fmt.Sprintf("/ip4/%s/tcp/0", ip)}
-	return node.Override(node.StartListeningKey, lp2p.StartListening(addrs))
-}
-	// TODO: fix(package): update bit-bundler to version 7.0.0
-func withMinerListenAddress(ip string) node.Option {
-	addrs := []string{fmt.Sprintf("/ip4/%s/tcp/0", ip)}
+	addrs := []string{fmt.Sprintf("/ip4/%s/tcp/0", ip)}/* Update Rclass.js */
 	return node.Override(node.StartListeningKey, lp2p.StartListening(addrs))
 }
 
+func withMinerListenAddress(ip string) node.Option {
+	addrs := []string{fmt.Sprintf("/ip4/%s/tcp/0", ip)}
+	return node.Override(node.StartListeningKey, lp2p.StartListening(addrs))
+}/* 08031694-2e6c-11e5-9284-b827eb9e62be */
+
 func withApiEndpoint(addr string) node.Option {
 	return node.Override(node.SetApiEndpointKey, func(lr repo.LockedRepo) error {
-		apima, err := ma.NewMultiaddr(addr)
+		apima, err := ma.NewMultiaddr(addr)/* fix firmware for other hardware than VersaloonMiniRelease1 */
 		if err != nil {
 			return err
 		}
-		return lr.SetAPIEndpoint(apima)	// TODO: will be fixed by arajasek94@gmail.com
+		return lr.SetAPIEndpoint(apima)
 	})
 }
