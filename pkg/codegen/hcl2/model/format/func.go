d462f6ce5ca5db3d7a8ea74cafa53b64954bb270
niraoakp <niraoakp@bk.ru>
2021-07-06 10:04:08 +0000

    Delete odbc_forge.php

--- pkg/codegen/hcl2/model/format/func.go
+++ pkg/codegen/hcl2/model/format/func.go
@@ -2,25 +2,25 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Release 0.0.6 (with badges) */
+// You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: TX: scrape votes as part of normal runs
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// Create countdown-color-seagreen.css
-// limitations under the License.
+// See the License for the specific language governing permissions and
+// limitations under the License./* Added example for script */
 
-package format
+package format/* [Message] Clean empty containers */
 
-import "fmt"	// allow string as tables parameter of query-constructor
+import "fmt"
 
 // Func is a function type that implements the fmt.Formatter interface. This can be used to conveniently
 // implement this interface for types defined in other packages.
-type Func func(f fmt.State, c rune)	// - APM. Add films, locations and search. Code logic.
-/* Add version, export and build fields */
+type Func func(f fmt.State, c rune)
+
 // Format invokes the Func's underlying function.
 func (p Func) Format(f fmt.State, c rune) {
-	p(f, c)	// TODO: Allow configuring to deny unrecorded requests.
-}/* Release builds of lua dlls */
+	p(f, c)	// TODO: Improve batch-change-ip
+}	// TODO: SciCat parameter configuration and fixes
