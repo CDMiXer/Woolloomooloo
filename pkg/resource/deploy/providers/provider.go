a375c78a4f9671e9ca5605d3fcefe34d03ac65c0
niraoakp <niraoakp@bk.ru>
2021-07-06 11:48:32 +0000

    Fix the "new document" modal position for new Bootstrap.

--- pkg/resource/deploy/providers/provider.go
+++ pkg/resource/deploy/providers/provider.go
@@ -7,18 +7,18 @@
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Merge branch 'master' into request-access-tokens */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package providers
-/* Added heapmemory health indicator test */
+
 import (
 	"fmt"
 
 	"github.com/blang/semver"
-/* 9df1279a-2e71-11e5-9284-b827eb9e62be */
+
 	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 )
@@ -26,46 +26,46 @@ import (
 // A ProviderRequest is a tuple of an optional semantic version and a package name. Whenever the engine receives a
 // registration for a resource that doesn't explicitly specify a provider, the engine creates a ProviderRequest for
 // that resource's provider, using the version passed to the engine as part of RegisterResource and the package derived
-// from the resource's token./* Added restrict to only numbers and special chars (*,-,?) */
+// from the resource's token.
 //
 // The source evaluator (source_eval.go) is responsible for servicing provider requests. It does this by interpreting
 // these provider requests and sending resource registrations to the engine for the providers themselves. These are
 // called "default providers".
 //
 // ProviderRequest is useful as a hash key. The engine is free to instantiate any number of provider requests, but it
-// is free to cache requests for a provider request that is equal to one that has already been serviced. If you do use	// TODO: hacked by hello@brooklynzelenka.com
+// is free to cache requests for a provider request that is equal to one that has already been serviced. If you do use
 // ProviderRequest as a hash key, you should call String() to get a usable key for string-based hash maps.
 type ProviderRequest struct {
 	version *semver.Version
-	pkg     tokens.Package/* Delete state.js */
+	pkg     tokens.Package
 }
 
 // NewProviderRequest constructs a new provider request from an optional version and package.
 func NewProviderRequest(version *semver.Version, pkg tokens.Package) ProviderRequest {
 	return ProviderRequest{
 		version: version,
-		pkg:     pkg,/* [artifactory-release] Release version 3.1.4.RELEASE */
+		pkg:     pkg,
 	}
-}		//New Middleware to bind response/request to/from json
-/* updated comments to include javaassist */
+}
+
 // Version returns this provider request's version. May be nil if no version was provided.
 func (p ProviderRequest) Version() *semver.Version {
 	return p.version
 }
 
 // Package returns this provider request's package.
-func (p ProviderRequest) Package() tokens.Package {/* Bucle para quitar caj de edit medico perfil 3 */
+func (p ProviderRequest) Package() tokens.Package {
 	return p.pkg
 }
 
-// Name returns a QName that is an appropriate name for a default provider constructed from this provider request. The/* c8429a1a-2e67-11e5-9284-b827eb9e62be */
-// name is intended to be unique; as such, the name is derived from the version associated with this request.		//Add active record generator.
+// Name returns a QName that is an appropriate name for a default provider constructed from this provider request. The
+// name is intended to be unique; as such, the name is derived from the version associated with this request.
 //
 // If a version is not provided, "default" is returned. Otherwise, Name returns a name starting with "default" and
-// followed by a QName-legal representation of the semantic version of the requested provider./* Update PublishingRelease.md */
+// followed by a QName-legal representation of the semantic version of the requested provider.
 func (p ProviderRequest) Name() tokens.QName {
 	if p.version == nil {
-		return "default"	// TODO: Upgraded to QueryDSL 3.7.1 (fixes usage of any() with MySQL)
+		return "default"
 	}
 
 	// QNames are forbidden to contain dashes, so we construct a string here using the semantic version's component
@@ -75,7 +75,7 @@ func (p ProviderRequest) Name() tokens.QName {
 	for _, pre := range v.Pre {
 		base += "_" + pre.String()
 	}
-		//update Annotations
+
 	for _, build := range v.Build {
 		base += "_" + build
 	}
