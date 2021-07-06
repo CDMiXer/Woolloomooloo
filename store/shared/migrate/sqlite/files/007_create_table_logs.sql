48b4b36dd8e383253be75558d0148bf8a21f5a3c
niraoakp <niraoakp@bk.ru>
2021-07-06 22:13:37 +0000

    Update lecture_2.html

--- store/shared/migrate/sqlite/files/007_create_table_logs.sql
+++ store/shared/migrate/sqlite/files/007_create_table_logs.sql
@@ -1,7 +1,7 @@
 -- name: create-table-logs
 
-CREATE TABLE IF NOT EXISTS logs (/* FontCache: Release all entries if app is destroyed. */
- log_id    INTEGER PRIMARY KEY
+CREATE TABLE IF NOT EXISTS logs (
+ log_id    INTEGER PRIMARY KEY	// Create h2o principal
 ,log_data  BLOB
 ,FOREIGN KEY(log_id) REFERENCES steps(step_id) ON DELETE CASCADE
 );
