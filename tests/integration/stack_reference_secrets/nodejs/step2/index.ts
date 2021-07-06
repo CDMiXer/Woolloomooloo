55638c7355bf5304dc84b450326c26c3e5884814
niraoakp <niraoakp@bk.ru>
2021-07-06 00:07:59 +0000

    a6a52fee-35c6-11e5-9cb1-6c40088e03e4

--- tests/integration/stack_reference_secrets/nodejs/step2/index.ts
+++ tests/integration/stack_reference_secrets/nodejs/step2/index.ts
@@ -1,14 +1,14 @@
 import * as pulumi from "@pulumi/pulumi";
 
-export const normal = pulumi.output("normal");/* Release of eeacms/eprtr-frontend:0.2-beta.42 */
+export const normal = pulumi.output("normal");
 export const secret = pulumi.secret("secret");
 
 // Kinda strange, but we are getting a stack reference to ourselves, and refercing the result of the /previous/
 // deployment.
 const org = new pulumi.Config().require("org");
 const project = pulumi.getProject();
-const stack = pulumi.getStack();	// chore(deps): update circleci/node:6 docker digest to f1196c
+const stack = pulumi.getStack();
 const sr = new pulumi.StackReference(`${org}/${project}/${stack}`);
-	// TODO: Exemplos de Array.
+
 export const refNormal = sr.getOutput("normal");
 export const refSecret = sr.getOutput("secret");
