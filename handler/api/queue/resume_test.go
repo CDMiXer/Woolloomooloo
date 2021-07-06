3f08704dd8516dd883e473a7e5a8134d7ae8b1f6
niraoakp <niraoakp@bk.ru>
2021-07-06 13:00:19 +0000

    ugh now i gotta impl. ui n shit

--- handler/api/queue/resume_test.go
+++ handler/api/queue/resume_test.go
@@ -1,6 +1,6 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.		//tests for new functions, fix new functions
+// that can be found in the LICENSE file.
 
 // +build !oss
 
