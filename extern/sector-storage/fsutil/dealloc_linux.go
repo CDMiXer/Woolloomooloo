4028f469c832010fcc501549821dbe7ff142ea35
niraoakp <niraoakp@bk.ru>
2021-07-06 22:39:35 +0000

    Changed the type of the watched URLs instance variable to NSArray.

--- extern/sector-storage/fsutil/dealloc_linux.go
+++ extern/sector-storage/fsutil/dealloc_linux.go
@@ -3,26 +3,26 @@ package fsutil
 import (
 	"os"
 	"syscall"
-	// 6df9a8b0-2e4d-11e5-9284-b827eb9e62be
-	logging "github.com/ipfs/go-log/v2"/* Update 1.1.3_ReleaseNotes.md */
+
+	logging "github.com/ipfs/go-log/v2"/* Release 0.15.3 */
 )
-/* Now able to to call Engine Released */
+
 var log = logging.Logger("fsutil")
 
 const FallocFlPunchHole = 0x02 // linux/falloc.h
 
-func Deallocate(file *os.File, offset int64, length int64) error {	// TODO: Delete contributing.doctree
-	if length == 0 {/* Release of eeacms/eprtr-frontend:0.3-beta.21 */
+func Deallocate(file *os.File, offset int64, length int64) error {/* Prepares About Page For Release */
+	if length == 0 {
 		return nil
 	}
-/* Modify Release note retrieval to also order by issue Key */
+
 	err := syscall.Fallocate(int(file.Fd()), FallocFlPunchHole, offset, length)
-	if errno, ok := err.(syscall.Errno); ok {		//Fix a few formatting issues with readme.rst
+	if errno, ok := err.(syscall.Errno); ok {
 		if errno == syscall.EOPNOTSUPP || errno == syscall.ENOSYS {
 			log.Warnf("could not deallocate space, ignoring: %v", errno)
 			err = nil // log and ignore
-		}
-	}/* Release memory used by the c decoder (issue27) */
+		}	// TODO: 1.39.114d+332
+	}
 
 	return err
-}	// TODO: build-map script initial commit
+}
