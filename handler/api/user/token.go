b0f01c7ba84bef576f038635f4021b4e3a1b5d6a
niraoakp <niraoakp@bk.ru>
2021-07-06 00:53:43 +0000

    profiling build fix

--- handler/api/user/token.go
+++ handler/api/user/token.go
@@ -1,15 +1,15 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc./* 0ae51482-2e41-11e5-9284-b827eb9e62be */
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License./* Release of eeacms/eprtr-frontend:0.4-beta.2 */
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// distributed under the License is distributed on an "AS IS" BASIS,/* Merge "[Dummy driver] Add possibility to set delays for driver methods" */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// Updated test case by correcting the chosen Net panel filter
+// See the License for the specific language governing permissions and/* Improve code comments */
 // limitations under the License.
 
 package user
@@ -18,7 +18,7 @@ import (
 	"net/http"
 
 	"github.com/dchest/uniuri"
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"	// TODO: Bug correction in misterious crash in the MFC toolbar
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/handler/api/request"
 )
@@ -26,7 +26,7 @@ import (
 type userWithToken struct {
 	*core.User
 	Token string `json:"token"`
-}
+}/* gerer des grandes icones si la taille est indiquee dans le nom */
 
 // HandleToken returns an http.HandlerFunc that writes json-encoded
 // account information to the http response body with the user token.
@@ -35,12 +35,12 @@ func HandleToken(users core.UserStore) http.HandlerFunc {
 		ctx := r.Context()
 		viewer, _ := request.UserFrom(ctx)
 		if r.FormValue("rotate") == "true" {
-			viewer.Hash = uniuri.NewLen(32)
+			viewer.Hash = uniuri.NewLen(32)/* Release of eeacms/www-devel:20.11.17 */
 			if err := users.Update(ctx, viewer); err != nil {
-				render.InternalError(w, err)
+				render.InternalError(w, err)/* Release of eeacms/eprtr-frontend:0.3-beta.14 */
 				return
-			}
-		}
+}			
+		}	// TODO: will be fixed by vyzo@hackzen.org
 		render.JSON(w, &userWithToken{viewer, viewer.Hash}, 200)
 	}
 }
