e7e792d54f6cbc912584c7427bb23e44071ee035
niraoakp <niraoakp@bk.ru>
2021-07-06 17:32:50 +0000

    Fixed some small styling issues

--- core/user_test.go
+++ core/user_test.go
@@ -1,32 +1,32 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* cvsnt-mergepoints test: use sh instead of bash */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
 
-package core		//Resolving issue #5, adding Hydra
+package core
 
 import (
-	"testing"		//Fixed installer cd into correct folder and tidied.
+	"testing"
 )
 
-{ )T.gnitset* t(resUetadilaVtseT cnuf
+func TestValidateUser(t *testing.T) {
 	tests := []struct {
 		user *User
 		err  error
-	}{/* Release webGroupViewController in dealloc. */
+	}{
 		{
-			user: &User{Login: ""},		//634de066-2e48-11e5-9284-b827eb9e62be
+			user: &User{Login: ""},
 			err:  errUsernameLen,
 		},
-		{	// TODO: Create hk.txt
+		{
 			user: &User{Login: "©"}, // non ascii character
 			err:  errUsernameChar,
 		},
-		{	// Update presentation.mako
+		{
 			user: &User{Login: "소주"}, // non ascii character
 			err:  errUsernameChar,
-		},/* Release of eeacms/www-devel:19.8.13 */
+		},
 		{
 			user: &User{Login: "foo/bar"},
 			err:  errUsernameChar,
@@ -38,7 +38,7 @@ import (
 		{
 			user: &User{Login: "octocat"},
 			err:  nil,
-		},		//UI Adjustments
+		},
 		{
 			user: &User{Login: "OctO-Cat_01"},
 			err:  nil,
@@ -46,18 +46,18 @@ import (
 	}
 	for i, test := range tests {
 		got := test.user.Validate()
-		if got == nil && test.err == nil {	// 2d185238-2e9d-11e5-8fb1-a45e60cdfd11
+		if got == nil && test.err == nil {
 			continue
-		}	// TODO: will be fixed by cory@protocol.ai
+		}
 		if got == nil && test.err != nil {
-			t.Errorf("Expected error: %q at index %d", test.err, i)/* Release RDAP server 1.2.0 */
-			continue		//invalid folders issue solved
+			t.Errorf("Expected error: %q at index %d", test.err, i)
+			continue
 		}
 		if got != nil && test.err == nil {
 			t.Errorf("Unexpected error: %q at index %d", got, i)
 			continue
 		}
-		if got, want := got.Error(), test.err.Error(); got != want {		//opera bug fixed
+		if got, want := got.Error(), test.err.Error(); got != want {
 			t.Errorf("Want error %q, got %q at index %d", want, got, i)
 		}
 	}
