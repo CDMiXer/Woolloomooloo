6fe22a68929555e59074314ae94785bd09c5a838
niraoakp <niraoakp@bk.ru>
2021-07-06 21:37:43 +0000

    Added buildtool_depend on catkin

--- scheduler/scheduler.go
+++ scheduler/scheduler.go
@@ -1,6 +1,6 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// TODO: hacked by cory@protocol.ai
-// that can be found in the LICENSE file./* Initialize the RNG generator with an orthogonally newed Generator */
+// Copyright 2019 Drone.IO Inc. All rights reserved.		//Merge "bump repo mw version check to 1.26"
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.
 
 // +build !oss
 
