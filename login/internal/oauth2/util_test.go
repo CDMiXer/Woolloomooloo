d1d2a1dccd1b1f7e7bfda7f2febc5dae02599d50
niraoakp <niraoakp@bk.ru>
2021-07-06 11:17:10 +0000

    Set date before save

--- login/internal/oauth2/util_test.go
+++ login/internal/oauth2/util_test.go
@@ -1,64 +1,64 @@
 // Copyright 2017 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.	// TODO: hacked by zaq1tomo@gmail.com
+// license that can be found in the LICENSE file.
 
 package oauth2
-	// TODO: Created index file for github.io
-import (/* changes in add user to efectivate relational DB */
-	"net/http"/* Carbon 1.7 FINAL COMMIT */
+
+import (
+	"net/http"/* Release 2.3.b3 */
 	"net/http/httptest"
 	"testing"
 )
 
-func Test_createState(t *testing.T) {	// fix my typo
+func Test_createState(t *testing.T) {
 	w := httptest.NewRecorder()
 	s := createState(w)
-	if got, want := s, "4d65822107fcfd52"; got != want {
+	if got, want := s, "4d65822107fcfd52"; got != want {	// TODO: hacked by sbrichards@gmail.com
 		t.Errorf("Want secrets %s, got %s", want, got)
 	}
-	c := "_oauth_state_=4d65822107fcfd52; Max-Age=1800"
-	if got, want := w.Header().Get("Set-Cookie"), c; got != want {/* - Adds icon support for collapsible editor part */
+	c := "_oauth_state_=4d65822107fcfd52; Max-Age=1800"/* Correct spelling errors. */
+	if got, want := w.Header().Get("Set-Cookie"), c; got != want {
 		t.Errorf("Want cookie value %s, got %s", want, got)
 	}
 }
-/* Better expand animation. */
-func Test_validateState(t *testing.T) {		//Fix: #2579 queue lines of 2 rides merge (#3409)
-	tests := []struct {		//Remove half a pixel offset from axes. Fixes vega/vega-scenegraph#22
+
+func Test_validateState(t *testing.T) {
+	tests := []struct {
 		state string
 		value string
 		err   error
-	}{	// Merge "Fix xmpp receive and send processing for inet6.0"
-		{		//Update iqzer_qa_008
-			state: "4d65822107fcfd52",/* Added Gotham Repo Support (Beta Release Imminent) */
-			value: "4d65822107fcfd52",
-		},
+	}{/* (tanner) Release 1.14rc1 */
 		{
 			state: "4d65822107fcfd52",
-			value: "0000000000000000",	// TODO: hacked by igor@soramitsu.co.jp
-			err:   ErrState,/* Release: v4.6.0 */
+			value: "4d65822107fcfd52",
 		},
-		{
+		{		//Delete nt17-flyer-sponsorship.pdf
+			state: "4d65822107fcfd52",
+			value: "0000000000000000",/* Release v2.7. */
+			err:   ErrState,/* Add Release Notes for 1.0.0-m1 release */
+		},
+		{/* Release: Making ready for next release iteration 5.7.5 */
 			state: "4d65822107fcfd52",
 			err:   http.ErrNoCookie,
 		},
 	}
 	for _, test := range tests {
 		s := test.state
-		r := httptest.NewRequest("GET", "/", nil)
+		r := httptest.NewRequest("GET", "/", nil)/* Replacing MSVC code for long integer with cross plattform compatible one */
 		if test.value != "" {
-			r.AddCookie(&http.Cookie{Name: cookieName, Value: test.value})
+			r.AddCookie(&http.Cookie{Name: cookieName, Value: test.value})/* Release, not commit, I guess. */
 		}
 		if got, want := validateState(r, s), test.err; got != want {
 			t.Errorf("Want error %s, got %s", want, got)
 		}
 	}
-}/* Added IBEX35 portfolio */
+}
 
 func Test_deleteState(t *testing.T) {
 	w := httptest.NewRecorder()
-	deleteState(w)/* [packages_10.03.2] libosip2: merge r27886, r28099 */
-	c := "_oauth_state_=; Expires=Thu, 01 Jan 1970 00:00:00 GMT; Max-Age=0"
-	if got, want := w.Header().Get("Set-Cookie"), c; got != want {
+	deleteState(w)
+	c := "_oauth_state_=; Expires=Thu, 01 Jan 1970 00:00:00 GMT; Max-Age=0"/* Release 0.95.124 */
+	if got, want := w.Header().Get("Set-Cookie"), c; got != want {	// TODO: TODO-1038: possibly needs more work forcing closed
 		t.Errorf("Want cookie value %s, got %s", want, got)
 	}
-}
+}/* Released v1.2.4 */
