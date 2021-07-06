7b92d18eb716c9b2873a2758a5d5991b8fa45df1
niraoakp <niraoakp@bk.ru>
2021-07-06 02:49:04 +0000

    trigger new build for ruby-head-clang (9be9851)

--- chain/actors/builtin/cron/cron.go
+++ chain/actors/builtin/cron/cron.go
@@ -1,10 +1,10 @@
 package cron
 
 import (
-	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"		//Added a constructor without default icon 
-)/* Create 01_Introduction.md */
+	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
+)
 
 var (
 	Address = builtin4.CronActorAddr
 	Methods = builtin4.MethodsCron
-)/* Update pip from 20.1.1 to 20.2 */
+)
