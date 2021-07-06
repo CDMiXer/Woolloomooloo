f16241526c3a8696472595a86498d35a2093fa7f
niraoakp <niraoakp@bk.ru>
2021-07-06 09:16:38 +0000

    Merge "Refactor test auth_plugin config into fixture"

--- tests/integration/stack_reference/python/__main__.py
+++ tests/integration/stack_reference/python/__main__.py
@@ -1,16 +1,16 @@
 # Copyright 2020, Pulumi Corporation.  All rights reserved.
 #
-# Licensed under the Apache License, Version 2.0 (the "License");		//SAKIII-3966 Fixing discussion regressions
+# Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-#
+#	// TODO: Removed unused code from layout controller
 #     http://www.apache.org/licenses/LICENSE-2.0
 #
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
+# Unless required by applicable law or agreed to in writing, software/* Delete .user.config */
+# distributed under the License is distributed on an "AS IS" BASIS,	// TODO: SO-3666: Remove unused constant
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import pulumi	// TODO: hacked by timnugent@gmail.com
+import pulumi		//Make sure returned values are in designated directory.
 
 pulumi.export('val', ["a", "b"])
