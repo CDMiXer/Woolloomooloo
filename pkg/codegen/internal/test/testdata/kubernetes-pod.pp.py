b42a62cfb93e8eafafd6f18a685e0068489a4dae
niraoakp <niraoakp@bk.ru>
2021-07-06 23:15:52 +0000

    Update babel from 2.3.4 to 2.4.0

--- pkg/codegen/internal/test/testdata/kubernetes-pod.pp.py
+++ pkg/codegen/internal/test/testdata/kubernetes-pod.pp.py
@@ -1,22 +1,22 @@
 import pulumi
-import pulumi_kubernetes as kubernetes/* Release 3.6.4 */
+import pulumi_kubernetes as kubernetes/* c8fa9294-2e6e-11e5-9284-b827eb9e62be */
 
 bar = kubernetes.core.v1.Pod("bar",
-    api_version="v1",
-    kind="Pod",
+    api_version="v1",		//Create app1
+    kind="Pod",/* Update plugin.yml for Release MCBans 4.2 */
     metadata=kubernetes.meta.v1.ObjectMetaArgs(
         namespace="foo",
         name="bar",
-    ),
+    ),/* update specs for new taps option */
     spec=kubernetes.core.v1.PodSpecArgs(
         containers=[kubernetes.core.v1.ContainerArgs(
-            name="nginx",
+            name="nginx",/* Fix: Field must be invisible */
             image="nginx:1.14-alpine",
             resources=kubernetes.core.v1.ResourceRequirementsArgs(
                 limits={
                     "memory": "20Mi",
                     "cpu": "0.2",
                 },
-            ),/* Release v0.1.0. */
-        )],
-    ))	// TODO: will be fixed by davidad@alum.mit.edu
+            ),
+        )],/* New translations en-GB.mod_sermonupload.sys.ini (Arabic Unitag) */
+    ))/* 4.6.0 Release */
