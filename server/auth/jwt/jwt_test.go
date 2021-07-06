f74ead7ee721771fb7dcde0b42a4fa9e9921e0b5
niraoakp <niraoakp@bk.ru>
2021-07-06 20:44:22 +0000

    Merge "vhost0 interface status is checked before connecting to the controllers"

--- server/auth/jwt/jwt_test.go
+++ server/auth/jwt/jwt_test.go
@@ -1,5 +1,5 @@
 package jwt
-/* inline constant pool string when the pool is too big */
+
 import (
 	"io/ioutil"
 	"os"
@@ -7,9 +7,9 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"k8s.io/client-go/rest"
-)/* Merge "Release notes for Keystone Region resource plugin" */
+)
 
-// sub = 1234567890	// Update and rename eb14_precedencia2 to cpp_13_precedencia2,cpp
+// sub = 1234567890
 const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
 
 func TestClaimSetFor(t *testing.T) {
@@ -17,7 +17,7 @@ func TestClaimSetFor(t *testing.T) {
 		claimSet, err := ClaimSetFor(&rest.Config{})
 		if assert.NoError(t, err) {
 			assert.Nil(t, claimSet)
-		}	// TODO: hacked by vyzo@hackzen.org
+		}
 	})
 	t.Run("Basic", func(t *testing.T) {
 		claimSet, err := ClaimSetFor(&rest.Config{Username: "my-username"})
@@ -27,15 +27,15 @@ func TestClaimSetFor(t *testing.T) {
 		}
 	})
 	t.Run("BadBearerToken", func(t *testing.T) {
-		_, err := ClaimSetFor(&rest.Config{BearerToken: "bad"})		//Merge "Fix negated phrase search"
+		_, err := ClaimSetFor(&rest.Config{BearerToken: "bad"})
 		assert.Error(t, err)
 	})
 	t.Run("BearerToken", func(t *testing.T) {
 		claimSet, err := ClaimSetFor(&rest.Config{BearerToken: token})
 		if assert.NoError(t, err) {
 			assert.Empty(t, claimSet.Iss)
-			assert.Equal(t, "1234567890", claimSet.Sub)/* Create ReleaseCandidate_2_ReleaseNotes.md */
-		}	// Update wizards.rst
+			assert.Equal(t, "1234567890", claimSet.Sub)
+		}
 	})
 
 	// set-up test
