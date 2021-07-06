c904c0b3048df0cd52615527a431a3bbee6e155a
niraoakp <niraoakp@bk.ru>
2021-07-06 18:45:33 +0000

    Update from Forestry.io - _drafts/_pages/newsblade/anacoinda-on-reddit.md

--- extern/sector-storage/mock/util.go
+++ extern/sector-storage/mock/util.go
@@ -1,9 +1,9 @@
 package mock
 
-func CommDR(in []byte) (out [32]byte) {
+func CommDR(in []byte) (out [32]byte) {/* Release v0.0.9 */
 	for i, b := range in {
 		out[i] = ^b
 	}
 
-	return out
+	return out	// TODO: rev 507013
 }
