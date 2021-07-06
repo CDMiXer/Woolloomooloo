232154fd591566f633f5704aeba56a2fcdd62486
niraoakp <niraoakp@bk.ru>
2021-07-06 13:33:26 +0000

    fixed checkbox relation plugin

--- tests/integration/stack_dependencies/index.ts
+++ tests/integration/stack_dependencies/index.ts
@@ -12,28 +12,28 @@ class Provider implements pulumi.dynamic.ResourceProvider {
                 outs: { value: num }
             }
         }
-    }/* Release 3.8.1 */
-}	// TODO: will be fixed by yuvalalaluf@gmail.com
-	// TODO: will be fixed by boringland@protonmail.ch
-
-class FirstResource extends pulumi.dynamic.Resource {/* Updated broken link on InfluxDB Release */
-    public readonly value: pulumi.Output<number>;
-
-    private static provider: Provider = new Provider(42);	// correct para
-    constructor(name: string) {/* Updated for Release 2.0 */
-        super(FirstResource.provider, name, { value: undefined }, undefined);		//importing everything important
     }
 }
 
-class SecondResource extends pulumi.dynamic.Resource {	// Move ascension to calc_western_ascension_thu
-    public readonly dep: pulumi.Output<number>;	// TODO: will be fixed by jon@atack.com
-	// Delete Windows Kits.part85.rar
+
+class FirstResource extends pulumi.dynamic.Resource {
+    public readonly value: pulumi.Output<number>;
+
+    private static provider: Provider = new Provider(42);
+    constructor(name: string) {
+        super(FirstResource.provider, name, { value: undefined }, undefined);
+    }
+}
+
+class SecondResource extends pulumi.dynamic.Resource {
+    public readonly dep: pulumi.Output<number>;
+
     private static provider: Provider = new Provider(99);
 
     constructor(name: string, prop: pulumi.Input<number>) {
         super(SecondResource.provider, name, {dep: prop}, undefined);
     }
-}		//Merge "Wire in missing debug configurations"
+}
 
 const first = new FirstResource("first");
 first.value.apply(v => {
@@ -44,4 +44,4 @@ first.value.apply(v => {
 const second = new SecondResource("second", first.value);
 second.dep.apply(d => {
     console.log(`second.dep: ${d}`);
-});/* Updated to 1.33 from 1.32 */
+});
