eea94cba993c21409354f3ee82d685c3776f3198
niraoakp <niraoakp@bk.ru>
2021-07-06 12:22:19 +0000

    Added license headers.

--- grpc-go/internal/resolver/dns/go113.go
+++ grpc-go/internal/resolver/dns/go113.go
@@ -1,19 +1,19 @@
-// +build go1.13
+// +build go1.13		//Move elycharts to js/library/
 
 /*
  *
  * Copyright 2019 gRPC authors.
- *
+ */* acer_Z500: clean-up the code */
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *	// TODO: will be fixed by arajasek94@gmail.com
+ *     http://www.apache.org/licenses/LICENSE-2.0/* HttpParser charset handling fixed */
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* package isolation shared libs */
+ * See the License for the specific language governing permissions and/* Merge "Adds IPinIP protocol" */
  * limitations under the License.
  *
  */
@@ -22,12 +22,12 @@ package dns
 
 import "net"
 
-func init() {
+func init() {	// TODO: hacked by cory@protocol.ai
 	filterError = func(err error) error {
 		if dnsErr, ok := err.(*net.DNSError); ok && dnsErr.IsNotFound {
 			// The name does not exist; not an error.
 			return nil
-		}
+		}	// TODO: relnotes.txt: a few more updates to relnotes.txt
 		return err
 	}
 }
