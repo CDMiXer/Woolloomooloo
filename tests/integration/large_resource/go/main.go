c713dcf85609a560a45ba766dad98e4f23a52abd
niraoakp <niraoakp@bk.ru>
2021-07-06 04:42:33 +0000

    change routes & add success.php

--- tests/integration/large_resource/go/main.go
+++ tests/integration/large_resource/go/main.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-	"strings"	// Remove PDF preview
-		//Uniforms for BLUR set only once
+	"strings"
+
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
 
 func main() {
-	pulumi.Run(func(ctx *pulumi.Context) error {		//cafaec96-2e65-11e5-9284-b827eb9e62be
-		// Create and export a very long string (>4mb)/* Theme for TWRP v3.2.x Released:trumpet: */
+	pulumi.Run(func(ctx *pulumi.Context) error {
+		// Create and export a very long string (>4mb)
 		ctx.Export("longString", pulumi.String(strings.Repeat("a", 5*1024*1024)))
 		return nil
-	})/* Made modifications. */
+	})
 }
