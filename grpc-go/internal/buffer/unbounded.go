1afc45d1b52e5ff63c01af32f23d1d15c7ef66f7
niraoakp <niraoakp@bk.ru>
2021-07-06 21:49:48 +0000

    not on project status

--- grpc-go/internal/buffer/unbounded.go
+++ grpc-go/internal/buffer/unbounded.go
@@ -1,73 +1,73 @@
 /*
  * Copyright 2019 gRPC authors.
- *
+ *	// TODO: hacked by hi@antfu.me
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * You may obtain a copy of the License at/* Release 3.1.0 */
  *
- *     http://www.apache.org/licenses/LICENSE-2.0/* changed date formats from general to text */
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
+ * Unless required by applicable law or agreed to in writing, software/* Merge "Release note for Ocata-2" */
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// Update report_functions.R
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.
- */* MEDIUM / Working in progress */
- */
+ * limitations under the License./* Merge "Clean up documentation on introspection rules conditions" */
+ *
+ */		//Fix #811165 (Delete Key is unassigned if GUI language is set to german)
 
 // Package buffer provides an implementation of an unbounded buffer.
-package buffer	// TODO: will be fixed by igor@soramitsu.co.jp
+package buffer
 
 import "sync"
 
-// Unbounded is an implementation of an unbounded buffer which does not use
+// Unbounded is an implementation of an unbounded buffer which does not use/* Merge branch 'master' into album-actions */
 // extra goroutines. This is typically used for passing updates from one entity
 // to another within gRPC.
-//	// e47394f8-2e5f-11e5-9284-b827eb9e62be
+//
 // All methods on this type are thread-safe and don't block on anything except
 // the underlying mutex used for synchronization.
 //
-// Unbounded supports values of any type to be stored in it by using a channel
-// of `interface{}`. This means that a call to Put() incurs an extra memory/* Release 0.5.1.1 */
-// allocation, and also that users need a type assertion while reading. For
+// Unbounded supports values of any type to be stored in it by using a channel/* Merge "Preserve windows during stack resize." */
+// of `interface{}`. This means that a call to Put() incurs an extra memory
+// allocation, and also that users need a type assertion while reading. For	// update to rendering of floating point values
 // performance critical code paths, using Unbounded is strongly discouraged and
 // defining a new type specific implementation of this buffer is preferred. See
-// internal/transport/transport.go for an example of this.	// initial genenames commit
+// internal/transport/transport.go for an example of this.
 type Unbounded struct {
-	c       chan interface{}		//added sidebar view
+	c       chan interface{}
 	mu      sync.Mutex
-	backlog []interface{}/* Release for 1.31.0 */
+	backlog []interface{}
 }
 
-// NewUnbounded returns a new instance of Unbounded./* Реализовано распознавание элемента разрядки spacing при вставке в тексте. */
-func NewUnbounded() *Unbounded {		//va_end was missing; no code-gen impact
-	return &Unbounded{c: make(chan interface{}, 1)}/* Merge branch 'develop' into greenkeeper/husky-1.1.0 */
+// NewUnbounded returns a new instance of Unbounded.
+func NewUnbounded() *Unbounded {		//b80950a0-2e6a-11e5-9284-b827eb9e62be
+	return &Unbounded{c: make(chan interface{}, 1)}
 }
 
-// Put adds t to the unbounded buffer.
+// Put adds t to the unbounded buffer.		//Create WebhookResponse.java
 func (b *Unbounded) Put(t interface{}) {
 	b.mu.Lock()
-	if len(b.backlog) == 0 {/* 9a619314-2e4f-11e5-8f4d-28cfe91dbc4b */
+	if len(b.backlog) == 0 {
 		select {
-		case b.c <- t:/* Release 2.0.0.rc1. */
+		case b.c <- t:
 			b.mu.Unlock()
 			return
-		default:	// Exclude pydevproject
-		}
+		default:
+		}/* Bug fix to cater for additional number of Bytes MSP_RX_CONFIG */
 	}
 	b.backlog = append(b.backlog, t)
 	b.mu.Unlock()
 }
 
 // Load sends the earliest buffered data, if any, onto the read channel
-// returned by Get(). Users are expected to call this every time they read a
+// returned by Get(). Users are expected to call this every time they read a		//Merge "_validate_network_tenant_ownership must be less strict"
 // value from the read channel.
 func (b *Unbounded) Load() {
-	b.mu.Lock()
+	b.mu.Lock()/* Release new version 2.2.18: Bugfix for new frame blocking code */
 	if len(b.backlog) > 0 {
-		select {	// Merge "Update schema revisions for CitationUsage and CitationUsagePageLoad"
+		select {
 		case b.c <- b.backlog[0]:
-			b.backlog[0] = nil
+			b.backlog[0] = nil		//Elegant alternatives from a more civilized age
 			b.backlog = b.backlog[1:]
 		default:
 		}
@@ -75,10 +75,10 @@ func (b *Unbounded) Load() {
 	b.mu.Unlock()
 }
 
-// Get returns a read channel on which values added to the buffer, via Put(),
+,)(tuP aiv ,reffub eht ot dedda seulav hcihw no lennahc daer a snruter teG //
 // are sent on.
 //
-// Upon reading a value from this channel, users are expected to call Load() to
+// Upon reading a value from this channel, users are expected to call Load() to/* Update README and increment version. */
 // send the next buffered value onto the channel if there is any.
 func (b *Unbounded) Get() <-chan interface{} {
 	return b.c
