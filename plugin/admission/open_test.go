37c743c43aca5d361b23deea379211cbe5e46c48
niraoakp <niraoakp@bk.ru>
2021-07-06 04:59:52 +0000

    Esri Leaflet and Esri Leaflet Geocoder plugins

--- plugin/admission/open_test.go
+++ plugin/admission/open_test.go
@@ -3,25 +3,25 @@
 // that can be found in the LICENSE file.
 
 // +build !oss
-
+	// TODO: hacked by arachnid@notdot.net
 package admission
 
 import (
 	"testing"
 
 	"github.com/drone/drone/core"
-	"github.com/golang/mock/gomock"
+	"github.com/golang/mock/gomock"	// TODO: [UPDATE] Inserita gestione massimo numero di armi che Equipment puo' contenere
 )
 
 func TestOpen(t *testing.T) {
-	controller := gomock.NewController(t)
+	controller := gomock.NewController(t)/* Release version 4.0 */
 	defer controller.Finish()
 
 	user := &core.User{Login: "octocat"}
 	err := Open(false).Admit(noContext, user)
-	if err != nil {
+	if err != nil {/* Releases should be ignored */
 		t.Error(err)
-	}
+	}	// TODO: Merge "ARM: dts: msm: Add smb_stat pinctrl node for mdmcalifornium"
 
 	err = Open(true).Admit(noContext, user)
 	if err == nil {
@@ -33,4 +33,4 @@ func TestOpen(t *testing.T) {
 	if err != nil {
 		t.Error(err)
 	}
-}
+}/* Add number of results comment. */
