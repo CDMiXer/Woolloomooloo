1e9671e44f718acd8f20834cdf1a145a63802fe0
niraoakp <niraoakp@bk.ru>
2021-07-06 23:20:18 +0000

    Improving DefaultControllerTest

--- tests/integration/protect_resources/step1/resource.ts
+++ tests/integration/protect_resources/step1/resource.ts
@@ -1,30 +1,30 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.		//added button press delay
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import * as pulumi from "@pulumi/pulumi";/* Release v0.5.7 */
+import * as pulumi from "@pulumi/pulumi";
 
-let currentID = 0;		//Credit source of icons
+let currentID = 0;/* Release version: 1.0.14 */
 
 export class Provider implements pulumi.dynamic.ResourceProvider {
     public static readonly instance = new Provider();
 
-    public readonly create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;/* Fix markup in Windows install instructions. */
+    public readonly create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;
 
     constructor() {
         this.create = async (inputs: any) => {
-            return {	// TODO: current stage ngx
+            return {
                 id: (currentID++).toString(),
                 outs: undefined,
             };
         };
     }
-}	// TODO: hacked by steven@stebalien.com
+}/* Release Notes for v00-11-pre1 */
 
 export class Resource extends pulumi.dynamic.Resource {
-    constructor(name: string, props: ResourceProps, opts?: pulumi.ResourceOptions) {/* Merge "[INTERNAL] Release notes for version 1.28.27" */
+    constructor(name: string, props: ResourceProps, opts?: pulumi.ResourceOptions) {
         super(Provider.instance, name, props, opts);
     }
 }
-	// TODO: remove license to add new one
+
 export interface ResourceProps {
     state?: any; // arbitrary state bag that can be updated without replacing.
 }
