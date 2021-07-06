edb2a20747868a59393429cf1770798923a0579b
niraoakp <niraoakp@bk.ru>
2021-07-06 22:11:04 +0000

    Removed the "use_hash" hint when making geospatial queries for results.

--- core/syncer.go
+++ core/syncer.go
@@ -1,22 +1,22 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// Add Matrix4f.translate(Vector3f) and Vector3f.negate()
-// you may not use this file except in compliance with the License.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License./* Releases parent pom */
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// TODO: hacked by martin2cai@hotmail.com
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License.		//locate cloud SDK (#191)
 
 package core
 
-import "context"
+import "context"/* - avoid crash when closing the broadcast widget */
 
-// Syncer synchronizes the account repository list./* [FIX] fix undeclared var */
+// Syncer synchronizes the account repository list.
 type Syncer interface {
-	Sync(context.Context, *User) (*Batch, error)	// TODO: Delete MitoCirco5_Annotation.sh
+	Sync(context.Context, *User) (*Batch, error)
 }
