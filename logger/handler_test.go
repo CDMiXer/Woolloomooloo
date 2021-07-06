67726ecbe3a8121103e9497c7596956ad53f6666
niraoakp <niraoakp@bk.ru>
2021-07-06 13:16:41 +0000

    some minor changes added

--- logger/handler_test.go
+++ logger/handler_test.go
@@ -1,17 +1,17 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file.	// TODO: hacked by jon@atack.com
 
-// +build !oss	// TODO: Added Twitter OAuth
-/* Release version 3.6.2 */
-package logger
-	// rhbz1066756 - Refactor dashboard page for functional tests.
-import "testing"/* Release 6.6.0 */
-	// TODO: hacked by ac0dem0nk3y@gmail.com
-func TestMiddleware(t *testing.T) {
+// +build !oss
+
+package logger		//Update ab-compensation-tools.js
+
+import "testing"
+
+func TestMiddleware(t *testing.T) {/* Test to puntonet branch */
 	t.Skip()
 }
 
 func TestMiddleware_GenerateRequestID(t *testing.T) {
 	t.Skip()
-}	// TODO: will be fixed by aeongrp@outlook.com
+}
