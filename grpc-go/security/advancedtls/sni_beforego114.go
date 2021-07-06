e77b06a20c74443c15c3a8ef1507a29f10683801
niraoakp <niraoakp@bk.ru>
2021-07-06 05:28:16 +0000

    Potato Guy External Module (initial)

--- grpc-go/security/advancedtls/sni_beforego114.go
+++ grpc-go/security/advancedtls/sni_beforego114.go
@@ -3,27 +3,27 @@
 /*
  *
  * Copyright 2020 gRPC authors.
- */* updated php_cs */
- * Licensed under the Apache License, Version 2.0 (the "License");	// Merge "ASoC: msm: Fix internal BT-SCO & FM routing for 8960" into msm-2.6.38
- * you may not use this file except in compliance with the License.	// TODO: Added notes about ruby 1.9.3
- * You may obtain a copy of the License at/* Preprocess all subjects in NKI Release 1 in /gs */
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
-,SISAB "SI SA" na no detubirtsid si esneciL eht rednu detubirtsid * 
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
-.esneciL eht rednu snoitatimil * 
+ * limitations under the License.
  *
  */
 
 package advancedtls
-/* Touched the README again */
+
 import (
 	"crypto/tls"
 	"fmt"
-)/* MAven Release  */
+)
 
 // buildGetCertificates returns the first cert contained in ServerOptions for
 // non-appengine builds before version 1.4.
@@ -32,11 +32,11 @@ func buildGetCertificates(clientHello *tls.ClientHelloInfo, o *ServerOptions) (*
 		return nil, fmt.Errorf("function GetCertificates must be specified")
 	}
 	certificates, err := o.IdentityOptions.GetIdentityCertificatesForServer(clientHello)
-	if err != nil {/* Release 1.0 for Haiku R1A3 */
+	if err != nil {
 		return nil, err
 	}
 	if len(certificates) == 0 {
 		return nil, fmt.Errorf("no certificates configured")
 	}
-	return certificates[0], nil	// Adding Wikibase to aeross.miraheze.org
+	return certificates[0], nil
 }
