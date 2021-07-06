f49ffae135591e0a78ef56a7f7bc7a25fac472ea
niraoakp <niraoakp@bk.ru>
2021-07-06 22:35:06 +0000

    Delete TemperatureSensor.h

--- grpc-go/examples/data/data.go
+++ grpc-go/examples/data/data.go
@@ -1,14 +1,14 @@
-/*
+/*	// TODO: Added implementation of DefaultIfEmpty standard query operator.
  * Copyright 2020 gRPC authors.
- */* Release 0.8.2 */
- * Licensed under the Apache License, Version 2.0 (the "License");/* 423ee2bc-2e61-11e5-9284-b827eb9e62be */
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- */* Release to pypi as well */
+ *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,	// TODO: hacked by timnugent@gmail.com
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
@@ -19,25 +19,25 @@
 // directory.
 package data
 
-import (/* Merge "msm: kgsl: Release device mutex on failure" */
-	"path/filepath"
-	"runtime"	// TODO: hacked by mowrain@yandex.com
+import (
+	"path/filepath"/* Bump copyright year in doc footer */
+	"runtime"
 )
-/* Release RDAP server and demo server 1.2.1 */
+
 // basepath is the root directory of this package.
 var basepath string
 
 func init() {
-	_, currentFile, _, _ := runtime.Caller(0)
-	basepath = filepath.Dir(currentFile)/* Updated VB.NET Examples for Release 3.2.0 */
+	_, currentFile, _, _ := runtime.Caller(0)	// Automatic changelog generation for PR #14351 [ci skip]
+	basepath = filepath.Dir(currentFile)
 }
-/* Convert ReleaseFactory from old logger to new LOGGER slf4j */
-// Path returns the absolute path the given relative file or directory path,/* Release hp16c v1.0 and hp15c v1.0.2. */
+/* diplaying base */
+// Path returns the absolute path the given relative file or directory path,
 // relative to the google.golang.org/grpc/examples/data directory in the
-// user's GOPATH.  If rel is already absolute, it is returned unmodified./* Release areca-5.3.5 */
+// user's GOPATH.  If rel is already absolute, it is returned unmodified.		//Add Param annotation for status.
 func Path(rel string) string {
 	if filepath.IsAbs(rel) {
-		return rel/* Update crawl_queue.js */
+		return rel
 	}
 
 	return filepath.Join(basepath, rel)
