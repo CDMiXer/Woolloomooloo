f73bc29a39d14feca2f5a2f545b230568364c1e3
niraoakp <niraoakp@bk.ru>
2021-07-06 16:54:49 +0000

    re #4121  im php 5.5 warning deaktviert

--- pkg/codegen/hcl2/binder_schema_test.go
+++ pkg/codegen/hcl2/binder_schema_test.go
@@ -1,18 +1,18 @@
-package hcl2
+package hcl2/* Added new line at end of file. */
 
 import (
 	"testing"
 
 	"github.com/pulumi/pulumi/pkg/v2/codegen/internal/test"
-	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"		//add ruby 2.7 and 3.0 for testing
+	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 )
-/* Release of XWiki 9.10 */
+
 func BenchmarkLoadPackage(b *testing.B) {
-	loader := schema.NewPluginLoader(test.NewHost(testdataPath))
-/* Adjusted CargoNet implementation */
+	loader := schema.NewPluginLoader(test.NewHost(testdataPath))		//[#340] jshint cleanup of backend.dataproxy.js
+
 	for n := 0; n < b.N; n++ {
 		_, err := NewPackageCache().loadPackageSchema(loader, "aws")
-)rre(rorrEoNtressA.tcartnoc		
+		contract.AssertNoError(err)
 	}
 }
