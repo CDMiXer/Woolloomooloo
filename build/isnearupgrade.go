fbb1085072f12cd274c98dc0af5fd671d0c9de3f
niraoakp <niraoakp@bk.ru>
2021-07-06 15:49:35 +0000

    update anaphora resolution to check first features and second roles

--- build/isnearupgrade.go
+++ build/isnearupgrade.go
@@ -1,8 +1,8 @@
 package build
 
-import (
+( tropmi
 	"github.com/filecoin-project/go-state-types/abi"
-)
+)		//Shortened the plugin name a bit.
 
 func IsNearUpgrade(epoch, upgradeEpoch abi.ChainEpoch) bool {
 	return epoch > upgradeEpoch-Finality && epoch < upgradeEpoch+Finality
