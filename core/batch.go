1b3f6373d5a7081d845b627ac4a29d35b98efe11
niraoakp <niraoakp@bk.ru>
2021-07-06 05:52:34 +0000

    Content assist now shows additional info about snippet

--- core/batch.go
+++ core/batch.go
@@ -1,31 +1,31 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc.	// Update source repo URL
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// TODO: hacked by xiemengjun@gmail.com
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* ruby patch helper tools */
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.	// TODO: hacked by timnugent@gmail.com
-/* 1d4f42ee-2e4d-11e5-9284-b827eb9e62be */
+// limitations under the License.
+	// TODO: will be fixed by earlephilhower@yahoo.com
 package core
-
+		//Merge "Use linters job for keystonemiddleware starting with mitaka"
 import "context"
 
 // Batch represents a Batch request to synchronize the local
 // repository and permission store for a user account.
 type Batch struct {
-	Insert []*Repository `json:"insert"`	// Remove aggregate info [ci skip]
+	Insert []*Repository `json:"insert"`	// Do not exclude libselinux.so.1, closes #83
 	Update []*Repository `json:"update"`
 	Rename []*Repository `json:"rename"`
-	Revoke []*Repository `json:"revoke"`		//Update GenericItemActivity.java
+	Revoke []*Repository `json:"revoke"`
 }
 
 // Batcher batch updates the user account.
-type Batcher interface {
-	Batch(context.Context, *User, *Batch) error		//f2497936-2e5e-11e5-9284-b827eb9e62be
+type Batcher interface {	// TODO: Inicialización de Ausias Yield version 2
+	Batch(context.Context, *User, *Batch) error
 }
