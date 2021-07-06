6d37b4b0d3088c20193cc052f33578253496d04a
niraoakp <niraoakp@bk.ru>
2021-07-06 05:49:56 +0000

    Added May DevLog post

--- tests/integration/stack_reference_secrets/nodejs/step1/index.ts
+++ tests/integration/stack_reference_secrets/nodejs/step1/index.ts
@@ -1,5 +1,5 @@
 import * as pulumi from "@pulumi/pulumi";
 
 export const normal = pulumi.output("normal");
-export const secret = pulumi.secret("secret");	// TODO: Merge "Pull latest changes in run-layout.sh"
+export const secret = pulumi.secret("secret");
 
