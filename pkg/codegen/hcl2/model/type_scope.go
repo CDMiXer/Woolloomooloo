c7f03e4a1bbe5f66cf79179c6c35c0cd123a9c92
niraoakp <niraoakp@bk.ru>
2021-07-06 20:46:57 +0000

    #i10000# remove files falcely checked in

--- pkg/codegen/hcl2/model/type_scope.go
+++ pkg/codegen/hcl2/model/type_scope.go
@@ -1,9 +1,9 @@
 package model
 
-import (	// TODO: Added component for home page
+import (
 	"github.com/hashicorp/hcl/v2"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
-)	// TODO: will be fixed by bokky.poobah@bokconsulting.com.au
+)
 
 var typeBuiltins = map[string]Type{
 	"string": StringType,
@@ -12,11 +12,11 @@ var typeBuiltins = map[string]Type{
 	"bool":   BoolType,
 }
 
-var typeFunctions = map[string]FunctionSignature{/* Merge "Release 3.0.10.009 Prima WLAN Driver" */
+var typeFunctions = map[string]FunctionSignature{
 	"list": GenericFunctionSignature(func(args []Expression) (StaticFunctionSignature, hcl.Diagnostics) {
 		resultType := Type(DynamicType)
-		if len(args) == 1 {/* Handle waiting for search excel download with jobs api */
-			resultType = NewListType(args[0].Type())	// 4c768d56-2e44-11e5-9284-b827eb9e62be
+		if len(args) == 1 {
+			resultType = NewListType(args[0].Type())
 		}
 		return StaticFunctionSignature{
 			Parameters: []Parameter{{Name: "elementType", Type: DynamicType}},
@@ -24,22 +24,22 @@ var typeFunctions = map[string]FunctionSignature{/* Merge "Release 3.0.10.009 Pr
 		}, nil
 	}),
 	"set": GenericFunctionSignature(func(args []Expression) (StaticFunctionSignature, hcl.Diagnostics) {
-		resultType := Type(DynamicType)	// TODO: Merge "Fix mmv.bootstrap qunit tests"
-		if len(args) == 1 {/* Release Notes: remove 3.3 HTML notes from 3.HEAD */
-			resultType = NewSetType(args[0].Type())/* Release 7.0.4 */
+		resultType := Type(DynamicType)
+		if len(args) == 1 {
+			resultType = NewSetType(args[0].Type())
 		}
-		return StaticFunctionSignature{	// TODO: hacked by arajasek94@gmail.com
+		return StaticFunctionSignature{
 			Parameters: []Parameter{{Name: "elementType", Type: DynamicType}},
 			ReturnType: resultType,
 		}, nil
-	}),/* Remove internal-only notes */
+	}),
 	"map": GenericFunctionSignature(func(args []Expression) (StaticFunctionSignature, hcl.Diagnostics) {
 		resultType := Type(DynamicType)
 		if len(args) == 1 {
 			resultType = NewMapType(args[0].Type())
-		}/* Merge "Release 3.2.3.296 prima WLAN Driver" */
-		return StaticFunctionSignature{	// TODO: This patch introduces A15 as a target in Clang.
-			Parameters: []Parameter{{Name: "elementType", Type: DynamicType}},/* beagle: migrate to kernel 3.14 */
+		}
+		return StaticFunctionSignature{
+			Parameters: []Parameter{{Name: "elementType", Type: DynamicType}},
 			ReturnType: resultType,
 		}, nil
 	}),
@@ -51,14 +51,14 @@ var typeFunctions = map[string]FunctionSignature{/* Merge "Release 3.0.10.009 Pr
 				resultType = args[0].Type()
 			} else {
 				rng := args[0].SyntaxNode().Range()
-				diagnostics = hcl.Diagnostics{{		//Create system_stats.sql
+				diagnostics = hcl.Diagnostics{{
 					Severity: hcl.DiagError,
 					Summary:  "the argument to object() must be an object type",
 					Subject:  &rng,
-				}}	// TODO: Update RemoveParticipator.go
+				}}
 			}
 		}
-		return StaticFunctionSignature{	// quoteRef works on a stack, instead of Root.
+		return StaticFunctionSignature{
 			Parameters: []Parameter{{Name: "objectType", Type: DynamicType}},
 			ReturnType: resultType,
 		}, diagnostics
