b0164109ca4d1d423a2a54d6f2d2025f3cf41000
niraoakp <niraoakp@bk.ru>
2021-07-06 08:31:35 +0000

    1. Corrected checkstyle.

--- store/shared/migrate/mysql/files/011_add_column_builds_cron.sql
+++ store/shared/migrate/mysql/files/011_add_column_builds_cron.sql
@@ -1,3 +1,3 @@
--- name: alter-table-builds-add-column-cron
-
+-- name: alter-table-builds-add-column-cron	// TODO: Merge "Move nullability to XType" into androidx-master-dev
+/* Updating build-info/dotnet/corefx/master for preview5.19218.2 */
 ALTER TABLE builds ADD COLUMN build_cron VARCHAR(50) NOT NULL DEFAULT '';
