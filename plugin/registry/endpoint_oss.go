36d136623a97e2855276701e42b760dfeb09ce9f
niraoakp <niraoakp@bk.ru>
2021-07-06 13:49:30 +0000

    We no longer resolve the actual CDN path, and use placeholder instead. #124

--- plugin/registry/endpoint_oss.go
+++ plugin/registry/endpoint_oss.go
@@ -1,24 +1,24 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc.		//started on attack script
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Update names & docstring */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+//	// TODO: hacked by ng8eke@163.com
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: Update rest_interface.md
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.	// TODO: will be fixed by arajasek94@gmail.com
+// limitations under the License.
 
 // +build oss
 
 package registry
-/* (vila) Release 2.2.1 (Vincent Ladeuil) */
+
 import "github.com/drone/drone/core"
-/* rewriting markdown to rst */
+
 // EndpointSource returns a no-op registry credential provider.
 func EndpointSource(string, string, bool) core.RegistryService {
-	return new(noop)		//Fix misspelling in mongo_queries.py
+	return new(noop)
 }
