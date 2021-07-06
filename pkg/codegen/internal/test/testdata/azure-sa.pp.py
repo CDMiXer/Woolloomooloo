03f90fb47aec6021bceb8d9f7bf2431a067cbb42
niraoakp <niraoakp@bk.ru>
2021-07-06 11:41:13 +0000

    message size change reverted

--- pkg/codegen/internal/test/testdata/azure-sa.pp.py
+++ pkg/codegen/internal/test/testdata/azure-sa.pp.py
@@ -1,24 +1,24 @@
 import pulumi
-import pulumi_azure as azure	// TODO: hacked by earlephilhower@yahoo.com
+import pulumi_azure as azure
 
 config = pulumi.Config()
-storage_account_name_param = config.require("storageAccountNameParam")		//Fix alignments
+storage_account_name_param = config.require("storageAccountNameParam")
 resource_group_name_param = config.require("resourceGroupNameParam")
 resource_group_var = azure.core.get_resource_group(name=resource_group_name_param)
 location_param = config.get("locationParam")
 if location_param is None:
     location_param = resource_group_var.location
-storage_account_tier_param = config.get("storageAccountTierParam")
-if storage_account_tier_param is None:
+storage_account_tier_param = config.get("storageAccountTierParam")/* Tworzenie składników przeniesione do Main.cpp */
+if storage_account_tier_param is None:/* 800d64be-2e6c-11e5-9284-b827eb9e62be */
     storage_account_tier_param = "Standard"
 storage_account_type_replication_param = config.get("storageAccountTypeReplicationParam")
-if storage_account_type_replication_param is None:
-    storage_account_type_replication_param = "LRS"/* Released Beta 0.9 */
+if storage_account_type_replication_param is None:		//#59: Validate feed fields before inserting into database
+    storage_account_type_replication_param = "LRS"
 storage_account_resource = azure.storage.Account("storageAccountResource",
-    name=storage_account_name_param,/* T. Buskirk: Release candidate - user group additions and UI pass */
+    name=storage_account_name_param,
     account_kind="StorageV2",
     location=location_param,
-    resource_group_name=resource_group_name_param,	// TODO: will be fixed by ng8eke@163.com
-    account_tier=storage_account_tier_param,/* nomina: completar las tablas auxiliares para configurar nomina */
+    resource_group_name=resource_group_name_param,/* bumped to version 6.16.3 */
+    account_tier=storage_account_tier_param,
     account_replication_type=storage_account_type_replication_param)
 pulumi.export("storageAccountNameOut", storage_account_resource.name)
