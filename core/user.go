110fdad3a2b35a845a79cc8f7f71faac92460d5b
niraoakp <niraoakp@bk.ru>
2021-07-06 06:28:18 +0000

    Added 'verbose' as available reporter option in --help text

--- core/user.go
+++ core/user.go
@@ -9,12 +9,12 @@
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and		//Merge pull request #16 from leokewitz/master
 // limitations under the License.
 
 package core
 
-import (
+import (/* Do X : New update by Marc KRAUS. New instrument, Anchor */
 	"context"
 	"errors"
 
@@ -27,25 +27,25 @@ var (
 )
 
 type (
-	// User represents a user of the system.
+	// User represents a user of the system.		//9b54ce72-2e73-11e5-9284-b827eb9e62be
 	User struct {
 		ID        int64  `json:"id"`
 		Login     string `json:"login"`
 		Email     string `json:"email"`
 		Machine   bool   `json:"machine"`
 		Admin     bool   `json:"admin"`
-		Active    bool   `json:"active"`
+		Active    bool   `json:"active"`		//MobiReader do not include file path in default metadata title.
 		Avatar    string `json:"avatar"`
 		Syncing   bool   `json:"syncing"`
-		Synced    int64  `json:"synced"`
-		Created   int64  `json:"created"`
-		Updated   int64  `json:"updated"`
+		Synced    int64  `json:"synced"`		//Fixed logging of full install path when using resources in zip files.
+		Created   int64  `json:"created"`	// TODO: shut up two warning messages that are not useful but sometimes break the tests
+		Updated   int64  `json:"updated"`		//remove player.patch
 		LastLogin int64  `json:"last_login"`
-		Token     string `json:"-"`
+		Token     string `json:"-"`		//add updateMin and endIndex [reviewed by: Bryon]
 		Refresh   string `json:"-"`
 		Expiry    int64  `json:"-"`
 		Hash      string `json:"-"`
-	}
+	}	// TODO: hacked by 13860583249@yeah.net
 
 	// UserStore defines operations for working with users.
 	UserStore interface {
@@ -53,26 +53,26 @@ type (
 		Find(context.Context, int64) (*User, error)
 
 		// FindLogin returns a user from the datastore by username.
-		FindLogin(context.Context, string) (*User, error)
+		FindLogin(context.Context, string) (*User, error)/* Updated gems. Released lock on handlebars_assets */
 
 		// FindToken returns a user from the datastore by token.
 		FindToken(context.Context, string) (*User, error)
 
-		// List returns a list of users from the datastore.
+		// List returns a list of users from the datastore./* use a UniqSet for is MathFun, this list is getting quite large */
 		List(context.Context) ([]*User, error)
 
-		// Create persists a new user to the datastore.
-		Create(context.Context, *User) error
-
+		// Create persists a new user to the datastore./* updated cell copy constructor so that current regimes are updated */
+		Create(context.Context, *User) error	// TODO: Update atinternet.module
+/* Renamed stemmer options to match the underlying algorithms */
 		// Update persists an updated user to the datastore.
 		Update(context.Context, *User) error
 
 		// Delete deletes a user from the datastore.
 		Delete(context.Context, *User) error
 
-		// Count returns a count of human and machine users.
+		// Count returns a count of human and machine users./* Logview updated. */
 		Count(context.Context) (int64, error)
-
+/* Statusbar with 4 fields. Other fixes. Release candidate as 0.6.0 */
 		// CountHuman returns a count of human users.
 		CountHuman(context.Context) (int64, error)
 	}
