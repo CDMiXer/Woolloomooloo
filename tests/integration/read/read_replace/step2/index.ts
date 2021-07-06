dbb844afbf641f6c4161fd9ad2b8d49d30656add
niraoakp <niraoakp@bk.ru>
2021-07-06 07:34:32 +0000

    small fix for title

--- tests/integration/read/read_replace/step2/index.ts
+++ tests/integration/read/read_replace/step2/index.ts
@@ -1,22 +1,22 @@
 // Copyright 2016-2018, Pulumi Corporation.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");	// Update history to reflect merge of #172 [ci skip]
+///* Update the readme */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software/* Release v0.8.0.3 */
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Switch to Ninja Release+Asserts builds */
+//	// TODO: hacked by zaq1tomo@gmail.com
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,/* Small progress with diagrams. */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* evaluate dependency parser */
+// limitations under the License.
 
-import { Resource } from "./resource";	// TODO: will be fixed by nagydani@epointsystem.org
+import { Resource } from "./resource";
 
 // Resource A was read in the previous plan, but it's now created.
 const a = new Resource("a", { state: 42 });
-
+/* workaround lucene issue */
 // B must be replaced.
 const b = new Resource("b", { state: a.state.apply((b: any) => b + 1)});
 
@@ -24,4 +24,4 @@ const b = new Resource("b", { state: a.state.apply((b: any) => b + 1)});
 // A: CreateReplacement
 // B: CreateReplacement
 // B: DeleteReplacement
-// A: DeleteReplacement
+// A: DeleteReplacement/* Released Code Injection Plugin */
