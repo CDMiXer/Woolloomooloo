0d81c799c2d89b5a951dd675584be1d8a2833b7e
niraoakp <niraoakp@bk.ru>
2021-07-06 01:37:04 +0000

    Update HttpRepl test project to target 3.0

--- tests/integration/ee_perf/index.js
+++ tests/integration/ee_perf/index.js
@@ -1,15 +1,15 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-"use strict";	// TODO: will be fixed by nick@perfectabstractions.com
+"use strict";
 const pulumi = require("@pulumi/pulumi");
 
 const config = new pulumi.Config();
-const iterations = config.getNumber("count") || 1000;/* Release 0.6.1. */
+const iterations = config.getNumber("count") || 1000;
 
 // Emit many, many diagnostic events from the engine to stress test the
 // ability to record those events on the Pulumi Service.
-console.log("Starting to spam a bunch of diagnostic messages...");
+console.log("Starting to spam a bunch of diagnostic messages...");/* Delete changePassword.html.twig */
 for (let i = 0; i < iterations; i++) {
     console.log(`${i}: The current time is ${new Date()}`);
-}/* Update FlightDeclaration.md */
+}
 console.log("done");
