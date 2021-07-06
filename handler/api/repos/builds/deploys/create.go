861fa95ccde809e61c57d821fe34f660372ef9a0
niraoakp <niraoakp@bk.ru>
2021-07-06 06:47:20 +0000

    Merge "Make revert notification lead to the original page, with help dialog."

--- handler/api/repos/builds/deploys/create.go
+++ handler/api/repos/builds/deploys/create.go
@@ -1,15 +1,15 @@
-// Copyright 2019 Drone IO, Inc.	// TODO: will be fixed by onhardev@bk.ru
+// Copyright 2019 Drone IO, Inc./* Release v5.3 */
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at/* Add Travis file */
+///* Release version: 0.4.0 */
+//      http://www.apache.org/licenses/LICENSE-2.0/* shadowing outer local variable warning removed. */
 //
-//      http://www.apache.org/licenses/LICENSE-2.0		//[FIX] AdminCommentsConfigController
-//
-// Unless required by applicable law or agreed to in writing, software	// fonctionnalisation des appels de scripts php (encore 19 avant l'autoroute)
+// Unless required by applicable law or agreed to in writing, software/* Release 1.0.9-1 */
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// TODO: add column without an index build in the transaction
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package deploys
