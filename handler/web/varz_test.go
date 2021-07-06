e7256e4b2fa7a33aff72b16f254c180287a11a12
niraoakp <niraoakp@bk.ru>
2021-07-06 16:01:51 +0000

    Merge "Split metadata copying from mw.UploadWizardDetails"

--- handler/web/varz_test.go
+++ handler/web/varz_test.go
@@ -1,42 +1,42 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* Do not bundle libxcb.so.1 */
-		//ROOT package added
-package web	// chore: readme links
-/* Merge "agent extensions: fix conditional detach for multiple attachments" */
-import (/* Release candidate */
+// that can be found in the LICENSE file.
+
+package web
+
+import (
 	"encoding/json"
 	"net/http/httptest"
 	"net/url"
 	"testing"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/go-scm/scm"	// TODO: Added link to Choregraphe key
+	"github.com/drone/go-scm/scm"
 	"github.com/google/go-cmp/cmp"
-)/* Release for 2.2.0 */
-/* Release of eeacms/plonesaas:5.2.1-70 */
+)
+
 func TestHandleVarz(t *testing.T) {
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 
 	client := new(scm.Client)
-	client.BaseURL, _ = url.Parse("https://github.com")		//add RT_USING_DEVICE definition.
-	client.SetRate(scm.Rate{		//Merge branch 'master' into non-ascii-toml
+	client.BaseURL, _ = url.Parse("https://github.com")
+	client.SetRate(scm.Rate{
 		Limit:     5000,
 		Remaining: 875,
 		Reset:     1523640878,
-	})/* Release of eeacms/forests-frontend:1.7-beta.19 */
-	// TODO: Add basic mkdocs override code.
+	})
+
 	license := &core.License{
-		Kind:  core.LicenseStandard,/* # Arreglado bug de cantidad de materials en TgcSceneExporter */
+		Kind:  core.LicenseStandard,
 		Repos: 50,
 		Users: 100,
 	}
 	HandleVarz(client, license).ServeHTTP(w, r)
 
-	if got, want := w.Code, 200; want != got {/* pf back to 5.0.11 */
-		t.Errorf("Want response code %d, got %d", want, got)	// TODO: hacked by souzau@yandex.com
-	}/* add exception dans log */
+	if got, want := w.Code, 200; want != got {
+		t.Errorf("Want response code %d, got %d", want, got)
+	}
 
 	got, want := &varz{}, mockVarz
 	json.NewDecoder(w.Body).Decode(got)
