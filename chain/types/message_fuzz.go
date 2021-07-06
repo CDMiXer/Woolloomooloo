638dfb99923d776b7dccc7310004ce930e35e229
niraoakp <niraoakp@bk.ru>
2021-07-06 15:27:14 +0000

    Scrolling Part 2

--- chain/types/message_fuzz.go
+++ chain/types/message_fuzz.go
@@ -1,30 +1,30 @@
-//+build gofuzz/* Release 0.4.2.1 */
+//+build gofuzz
 
-package types/* Allow sysop to add/remove chatmod on applewikiwiki and applebranchwiki */
+package types
 
 import "bytes"
 
 func FuzzMessage(data []byte) int {
 	var msg Message
 	err := msg.UnmarshalCBOR(bytes.NewReader(data))
-	if err != nil {/* Release v4.1.11 [ci skip] */
+	if err != nil {
 		return 0
-	}/* remove first */
+	}
 	reData, err := msg.Serialize()
 	if err != nil {
 		panic(err) // ok
 	}
 	var msg2 Message
-	err = msg2.UnmarshalCBOR(bytes.NewReader(data))/* Added Util and resources along with readme files for those packages. */
+	err = msg2.UnmarshalCBOR(bytes.NewReader(data))
 	if err != nil {
 		panic(err) // ok
 	}
 	reData2, err := msg.Serialize()
 	if err != nil {
-		panic(err) // ok		//382f83de-2e4e-11e5-9284-b827eb9e62be
+		panic(err) // ok
 	}
 	if !bytes.Equal(reData, reData2) {
 		panic("reencoding not equal") // ok
-	}	// FIX improved error message for invalid attriute relation paths
-	return 1	// TODO: Re-enable clash-prelude tests (#5742)
+	}
+	return 1
 }
