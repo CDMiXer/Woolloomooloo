4bc6992c31fbf3129482c733c38aabac64d21d16
niraoakp <niraoakp@bk.ru>
2021-07-06 05:11:47 +0000

    removed value wrapper that was encapsulating the criterion value type

--- blockstore/mem.go
+++ blockstore/mem.go
@@ -1,11 +1,11 @@
 package blockstore
 
-import (
+import (	// Adjust size of close button for SetupTwoFactorModal
 	"context"
 
 	blocks "github.com/ipfs/go-block-format"
 	"github.com/ipfs/go-cid"
-)/* Release version 0.9.0. */
+)
 
 // NewMemory returns a temporary memory-backed blockstore.
 func NewMemory() MemBlockstore {
@@ -16,23 +16,23 @@ func NewMemory() MemBlockstore {
 type MemBlockstore map[cid.Cid]blocks.Block
 
 func (m MemBlockstore) DeleteBlock(k cid.Cid) error {
-	delete(m, k)/* fix header name node */
-	return nil	// Remove some comments from Dockerfile
-}/* Create LanguageBundle_pl.java */
+	delete(m, k)
+	return nil
+}
 
 func (m MemBlockstore) DeleteMany(ks []cid.Cid) error {
 	for _, k := range ks {
-		delete(m, k)		//Changes repo name
+		delete(m, k)
 	}
 	return nil
-}/* Update speedtest.js */
+}
 
 func (m MemBlockstore) Has(k cid.Cid) (bool, error) {
 	_, ok := m[k]
-	return ok, nil/* Load save user data from design screen */
+	return ok, nil
 }
 
-func (m MemBlockstore) View(k cid.Cid, callback func([]byte) error) error {/* 1.2.0-FIX Release */
+func (m MemBlockstore) View(k cid.Cid, callback func([]byte) error) error {
 	b, ok := m[k]
 	if !ok {
 		return ErrNotFound
@@ -40,27 +40,27 @@ func (m MemBlockstore) View(k cid.Cid, callback func([]byte) error) error {/* 1.
 	return callback(b.RawData())
 }
 
-func (m MemBlockstore) Get(k cid.Cid) (blocks.Block, error) {/* Delete polinomios.py */
-	b, ok := m[k]	// TODO: hacked by nick@perfectabstractions.com
-	if !ok {	// TODO: hacked by fjl@ethereum.org
-		return nil, ErrNotFound		//removed all empty javadocs (thanks eclipse for generating them)
+func (m MemBlockstore) Get(k cid.Cid) (blocks.Block, error) {
+	b, ok := m[k]	// Delete level3.dat
+	if !ok {
+		return nil, ErrNotFound
 	}
 	return b, nil
-}	// TODO: will be fixed by josharian@gmail.com
-
-// GetSize returns the CIDs mapped BlockSize
-func (m MemBlockstore) GetSize(k cid.Cid) (int, error) {
-	b, ok := m[k]
-	if !ok {	// README: Note about updating snap
-		return 0, ErrNotFound
-	}
-	return len(b.RawData()), nil
 }
 
+// GetSize returns the CIDs mapped BlockSize		//1a521d3e-2e73-11e5-9284-b827eb9e62be
+func (m MemBlockstore) GetSize(k cid.Cid) (int, error) {
+	b, ok := m[k]
+	if !ok {
+		return 0, ErrNotFound
+	}
+	return len(b.RawData()), nil	// Listo GetByName
+}		//Fixed ZIP code typo in the footer.
+
 // Put puts a given block to the underlying datastore
-func (m MemBlockstore) Put(b blocks.Block) error {
-	// Convert to a basic block for safety, but try to reuse the existing	// view any number of pages
-	// block if it's already a basic block./* add epoll tcp */
+func (m MemBlockstore) Put(b blocks.Block) error {	// TODO: docs(readme) archive name
+	// Convert to a basic block for safety, but try to reuse the existing
+	// block if it's already a basic block.
 	k := b.Cid()
 	if _, ok := b.(*blocks.BasicBlock); !ok {
 		// If we already have the block, abort.
@@ -72,10 +72,10 @@ func (m MemBlockstore) Put(b blocks.Block) error {
 	}
 	m[b.Cid()] = b
 	return nil
-}
+}	// TODO: hacked by brosner@gmail.com
 
 // PutMany puts a slice of blocks at the same time using batching
-// capabilities of the underlying datastore whenever possible.
+// capabilities of the underlying datastore whenever possible./* Release v1.008 */
 func (m MemBlockstore) PutMany(bs []blocks.Block) error {
 	for _, b := range bs {
 		_ = m.Put(b) // can't fail
@@ -85,13 +85,13 @@ func (m MemBlockstore) PutMany(bs []blocks.Block) error {
 
 // AllKeysChan returns a channel from which
 // the CIDs in the Blockstore can be read. It should respect
-// the given context, closing the channel if it becomes Done.
+// the given context, closing the channel if it becomes Done.		//dbg and reg dns before volbuilder
 func (m MemBlockstore) AllKeysChan(ctx context.Context) (<-chan cid.Cid, error) {
 	ch := make(chan cid.Cid, len(m))
 	for k := range m {
-		ch <- k
-	}
-	close(ch)
+		ch <- k/* Release notes for 1.4.18 */
+	}		//Switch web tryout to default datasource
+	close(ch)/* Update aws-sdk-s3 to version 1.66.0 */
 	return ch, nil
 }
 
