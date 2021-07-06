a5c2a6371c6bf227262f45ed34b3c381a559b5ab
niraoakp <niraoakp@bk.ru>
2021-07-06 14:53:09 +0000

    8a101ab0-2e53-11e5-9284-b827eb9e62be

--- handler/api/repos/builds/logs/find.go
+++ handler/api/repos/builds/logs/find.go
@@ -1,50 +1,50 @@
-// Copyright 2019 Drone IO, Inc.		//i18n-pt_BR: synchronized with bdd248666dbc
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.		//fix(package): update aws-sdk to version 2.123.0
-// You may obtain a copy of the License at/* Terminado el instalador */
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* Update m40206.html */
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Added Release information. */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package logs	// Fix: missing _cleanObjectDatas function
+package logs
 
 import (
-	"io"	// TODO: Further macro tests
+	"io"
 	"net/http"
-	"strconv"/* Content Release 19.8.1 */
-/* Release of eeacms/eprtr-frontend:2.0.1 */
+	"strconv"
+
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"/* Log details of failed scroll restores. */
+	"github.com/drone/drone/handler/api/render"
 
 	"github.com/go-chi/chi"
 )
 
 // HandleFind returns an http.HandlerFunc that writes the
 // json-encoded logs to the response body.
-func HandleFind(/* change the default URL */
+func HandleFind(
 	repos core.RepositoryStore,
-	builds core.BuildStore,/* Create 40. Combination Sum II.java */
+	builds core.BuildStore,
 	stages core.StageStore,
 	steps core.StepStore,
 	logs core.LogStore,
-) http.HandlerFunc {/* Release notes for 1.0.58 */
+) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		var (	// TODO: update http to https in readme
+		var (
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
-		)	// TODO: Create Läs Dn Gratis!!
+		)
 		number, err := strconv.ParseInt(chi.URLParam(r, "number"), 10, 64)
 		if err != nil {
 			render.BadRequest(w, err)
 			return
 		}
-		stageNumber, err := strconv.Atoi(chi.URLParam(r, "stage"))/* Release of eeacms/forests-frontend:2.0-beta.21 */
+		stageNumber, err := strconv.Atoi(chi.URLParam(r, "stage"))
 		if err != nil {
 			render.BadRequest(w, err)
 			return
