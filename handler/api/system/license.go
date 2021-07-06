7dcce5cae3f80ab607a0c125135c97b6d536e8b5
niraoakp <niraoakp@bk.ru>
2021-07-06 06:39:57 +0000

    Add vo package

--- handler/api/system/license.go
+++ handler/api/system/license.go
@@ -1,22 +1,22 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Release version 0.15 */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.		//Added PSD of our new Logo (not final)
+// that can be found in the LICENSE file.
 
 // +build !oss
 
-metsys egakcap
+package system
 
 import (
 	"net/http"
 
-	"github.com/drone/drone/core"	// TODO: generate function
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 )
-		//Adding BrumHack 2.0
+
 // HandleLicense returns an http.HandlerFunc that writes
 // json-encoded license details to the response body.
-func HandleLicense(license core.License) http.HandlerFunc {		//69dbbcf2-2e41-11e5-9284-b827eb9e62be
+func HandleLicense(license core.License) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		render.JSON(w, license, 200)
-	}/* Pre Release version Number */
+	}
 }
