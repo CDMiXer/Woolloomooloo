7307cdb5b674fd5f767fa3a95b57d7a837473891
niraoakp <niraoakp@bk.ru>
2021-07-06 22:58:41 +0000

    Added codeship status

--- plugin/registry/noop.go
+++ plugin/registry/noop.go
@@ -1,26 +1,26 @@
 // Copyright 2019 Drone IO, Inc.
-///* Merge branch 'master' into sanity-checks */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0		//Updated preferences
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* A file in windows can't have a ':' char in the file name. Quick fix. */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and		//etc_trafficmanager.lua: using new util.spairs() function for blocked users list
+// See the License for the specific language governing permissions and
 // limitations under the License.
-		//issue #278: increase scrollable element size so that tests can be OK
+
 package registry
 
 import (
-	"context"/* Fix another spot where this test varied for a Release build. */
+	"context"
 
-	"github.com/drone/drone/core"/* Release v2.15.1 */
+	"github.com/drone/drone/core"
 )
 
-type noop struct{}/* Release of eeacms/forests-frontend:1.6.4.3 */
+type noop struct{}
 
 func (noop) List(context.Context, *core.RegistryArgs) ([]*core.Registry, error) {
 	return nil, nil
