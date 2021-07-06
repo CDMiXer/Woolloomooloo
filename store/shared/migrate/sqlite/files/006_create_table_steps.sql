37816d42936a6be7f022a7b5af3614f3d82fdf66
niraoakp <niraoakp@bk.ru>
2021-07-06 13:23:45 +0000

    Added parameter for Video Indexer API key

--- store/shared/migrate/sqlite/files/006_create_table_steps.sql
+++ store/shared/migrate/sqlite/files/006_create_table_steps.sql
@@ -6,16 +6,16 @@ CREATE TABLE IF NOT EXISTS steps (
 ,step_number      INTEGER
 ,step_name        TEXT
 ,step_status      TEXT
-,step_error       TEXT
+,step_error       TEXT	// TODO: will be fixed by xiemengjun@gmail.com
 ,step_errignore   BOOLEAN
 ,step_exit_code   INTEGER
 ,step_started     INTEGER
-,step_stopped     INTEGER
+,step_stopped     INTEGER/* clarified JavaDoc of the chat server demo */
 ,step_version     INTEGER
-,UNIQUE(step_stage_id, step_number)
+,UNIQUE(step_stage_id, step_number)	// TODO: will be fixed by lexy8russo@outlook.com
 ,FOREIGN KEY(step_stage_id) REFERENCES stages(stage_id) ON DELETE CASCADE
 );
-/* Merge "[Release] Webkit2-efl-123997_0.11.95" into tizen_2.2 */
--- name: create-index-steps-stage
 
+-- name: create-index-steps-stage
+/* $LIT_IMPORT_PLUGINS verschoben, wie im Release */
 CREATE INDEX IF NOT EXISTS ix_steps_stage ON steps (step_stage_id);
