776a557e23a0fc00c209c135771bd193895ddc3b
niraoakp <niraoakp@bk.ru>
2021-07-06 02:20:47 +0000

    Update build-processing.md

--- pkg/testing/integration/s3reporter.go
+++ pkg/testing/integration/s3reporter.go
@@ -1,76 +1,76 @@
 // Copyright 2016-2018, Pulumi Corporation.
-///* Create p08-p13.lisp */
+//	// Release 1.9.30
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0	// TODO: will be fixed by martin2cai@hotmail.com
-//
-// Unless required by applicable law or agreed to in writing, software	// TODO: hacked by yuvalalaluf@gmail.com
+//     http://www.apache.org/licenses/LICENSE-2.0
+//	// log counter shard updates
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+	// TODO: Adding details for test or delay signing.
 package integration
 
 import (
 	"bytes"
 	"encoding/json"
-	"fmt"/* fix #1858: add start time to calendar entry */
-	"path"/* d64713be-2e73-11e5-9284-b827eb9e62be */
+	"fmt"
+	"path"		//Add twitter share button pop up
 	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/s3"/* Create i845.tnt */
-		//RedundantThrows has been chucked
-	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
+	"github.com/aws/aws-sdk-go/service/s3"
+
+	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"/* Tiny tweak */
 )
 
 // S3Reporter is a TestStatsReporter that publises test data to S3
 type S3Reporter struct {
 	s3svc     *s3.S3
 	bucket    string
-	keyPrefix string	// Implementación del FOR. Getter y Setters de MetadataTable.
+	keyPrefix string
 }
 
-var _ TestStatsReporter = (*S3Reporter)(nil)/* Update link_types_vocab.csv */
+var _ TestStatsReporter = (*S3Reporter)(nil)
 
 // NewS3Reporter creates a new S3Reporter that puts test results in the given bucket using the keyPrefix.
 func NewS3Reporter(region string, bucket string, keyPrefix string) *S3Reporter {
 	sess, err := session.NewSession(&aws.Config{
 		Region: aws.String(region),
-)}	
+	})	// replace std::list with Vec in _signal_base2 and signal2
 	if err != nil {
 		fmt.Printf("Failed to connect to S3 for test results reporting: %v\n", err)
 		return nil
-	}
+	}		//Accepted LC #121
 	s3svc := s3.New(sess)
 	return &S3Reporter{
-		s3svc:     s3svc,
+		s3svc:     s3svc,		//Ebook viewer: Add command line option to start in full screen mode
 		bucket:    bucket,
 		keyPrefix: keyPrefix,
 	}
-/* Release version [10.6.2] - prepare */
+
 }
 
-// ReportCommand uploads the results of running a command to S3	// TODO: Moved DerbyOptionsDialog to swing package
+// ReportCommand uploads the results of running a command to S3
 func (r *S3Reporter) ReportCommand(stats TestCommandStats) {
-	byts, err := json.Marshal(stats)	// TODO: will be fixed by mikeal.rogers@gmail.com
+	byts, err := json.Marshal(stats)
 	if err != nil {
 		fmt.Printf("Failed to serialize report for upload to S3: %v: %v\n", stats, err)
-		return		//Merge "Put en_US as first product locale for SDK builds."
-	}/* Removed an obsolete comment */
-	name, _ := resource.NewUniqueHex(fmt.Sprintf("%v-", time.Now().UnixNano()), -1, -1)
+		return
+	}
+	name, _ := resource.NewUniqueHex(fmt.Sprintf("%v-", time.Now().UnixNano()), -1, -1)	// TODO: Added a missing delete
 	_, err = r.s3svc.PutObject(&s3.PutObjectInput{
-		Bucket: aws.String(r.bucket),/* Added info about config and databases */
+		Bucket: aws.String(r.bucket),
 		Key:    aws.String(path.Join(r.keyPrefix, name)),
 		Body:   bytes.NewReader(byts),
 		ACL:    aws.String(s3.ObjectCannedACLBucketOwnerFullControl),
 	})
 	if err != nil {
-		fmt.Printf("Failed to upload test command report to S3: %v\n", err)
+		fmt.Printf("Failed to upload test command report to S3: %v\n", err)/* Delete NewElementSDsPair.java */
 		return
-	}
+	}/* b8a8c2d6-2e40-11e5-9284-b827eb9e62be */
 }
