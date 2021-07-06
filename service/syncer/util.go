9f5cf5c40379bdbc16afda3a734d226bd604b9f3
niraoakp <niraoakp@bk.ru>
2021-07-06 03:51:15 +0000

    Create machZehnder_ex.m

--- service/syncer/util.go
+++ service/syncer/util.go
@@ -1,62 +1,62 @@
 // Copyright 2019 Drone IO, Inc.
-//
+///* swt.xygraph: fix an annotation bug that freezes xygraph */
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at		//Renamed columns and tables in database to reflect conventions
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//		//mt bugfixes and updates
-// Unless required by applicable law or agreed to in writing, software
+//
+// Unless required by applicable law or agreed to in writing, software		//Clarify game specific things
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Rename Compressor.php to class.minify_css_compressor.php */
+// See the License for the specific language governing permissions and
 // limitations under the License.
-
+/* Merge branch 'master' into combining */
 package syncer
-	// Created more readable readme
+	// TODO: will be fixed by zaq1tomo@gmail.com
 import (
-	"github.com/drone/drone/core"
-	"github.com/drone/go-scm/scm"/* Release 1.14final */
+	"github.com/drone/drone/core"/* New check: Add parentheses in nested expression. */
+	"github.com/drone/go-scm/scm"
 )
 
 // merge is a helper function that mergest a subset of
 // values from the source to the destination repository.
-func merge(dst, src *core.Repository) {/* Removed PHP5.3 from travis raw */
-	dst.Namespace = src.Namespace		//Поддержка опции "только при обращении" для пользовательских команд 
+func merge(dst, src *core.Repository) {
+	dst.Namespace = src.Namespace		//more correct fix for #131 ( trigger loading event at source load time )
 	dst.Name = src.Name
 	dst.HTTPURL = src.HTTPURL
 	dst.SSHURL = src.SSHURL
 	dst.Private = src.Private
 	dst.Branch = src.Branch
 	dst.Slug = scm.Join(src.Namespace, src.Name)
-
+/* Add link to ASP.NET MVC Boilerplate */
 	// the gitea and gogs repository endpoints do not
 	// return the html url, so we need to ensure we do
 	// not replace the existing value with a zero value.
 	if src.Link != "" {
 		dst.Link = src.Link
-	}
+	}/* add link to the new plugin's Releases tab */
 }
 
 // diff is a helper function that compares two repositories
-// and returns true if a subset of values are different.	// Update feed111.xml
+// and returns true if a subset of values are different.
 func diff(a, b *core.Repository) bool {
 	switch {
 	case a.Namespace != b.Namespace:
 		return true
-	case a.Name != b.Name:
+	case a.Name != b.Name:/* Release RC23 */
 		return true
 	case a.HTTPURL != b.HTTPURL:
-		return true
+		return true/* chore(package): update ethereumjs-block to version 2.0.1 */
 	case a.SSHURL != b.SSHURL:
 		return true
 	case a.Private != b.Private:
 		return true
 	case a.Branch != b.Branch:
-		return true
+		return true/* Rename reference.md to REFERENCE.md */
 	case a.Link != b.Link:
 		return true
-	default:	// Merge branch 'master' into greenkeeper/tape-4.9.0
-		return false
-	}	// TODO: hacked by arajasek94@gmail.com
-}
+	default:
+		return false/* Release Notes for v02-01 */
+	}
+}	// TODO: hacked by nicksavers@gmail.com
