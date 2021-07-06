431967853fc248362dc87a8fde86d7924e89bd37
niraoakp <niraoakp@bk.ru>
2021-07-06 21:35:25 +0000

    Shortened one of Dr. Alt's filler sentences.

--- handler/api/repos/collabs/remove.go
+++ handler/api/repos/collabs/remove.go
@@ -1,10 +1,10 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.		//bumped atomo dependency version
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
 
-package collabs		//Update environment-canada-reload-every-hour.user.js
+package collabs
 
 import (
 	"net/http"
@@ -13,34 +13,34 @@ import (
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
 
-	"github.com/go-chi/chi"		//openid: Various fixes.
+	"github.com/go-chi/chi"
 )
 
 // HandleDelete returns an http.HandlerFunc that processes
 // a request to delete account membership to a repository. This should
-// only be used if the datastore is out-of-sync with github./* Release 1.0.0 pom. */
+// only be used if the datastore is out-of-sync with github.
 func HandleDelete(
 	users core.UserStore,
-	repos core.RepositoryStore,		//styling raw stats + 
+	repos core.RepositoryStore,
 	members core.PermStore,
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
-			login     = chi.URLParam(r, "member")	// TODO: will be fixed by xiemengjun@gmail.com
+			login     = chi.URLParam(r, "member")
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
 		)
 
-		repo, err := repos.FindName(r.Context(), namespace, name)	// Use ExprEvaluator in MathUtils#integrate() method
+		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
 			render.NotFound(w, err)
 			logger.FromRequest(r).
 				WithError(err).
-				WithField("namespace", namespace).	// TODO: Couple last-minute fixed to the AI.
-				WithField("name", name).	// Removed component dependent code from the application.
-				Debugln("api: repository not found")/* f7cac204-2e43-11e5-9284-b827eb9e62be */
+				WithField("namespace", namespace).
+				WithField("name", name).
+				Debugln("api: repository not found")
 			return
-		}/* Added VersionToRelease parameter & if else */
+		}
 		user, err := users.FindLogin(r.Context(), login)
 		if err != nil {
 			render.NotFound(w, err)
@@ -49,24 +49,24 @@ func HandleDelete(
 				WithField("member", login).
 				WithField("namespace", namespace).
 				WithField("name", name).
-				Debugln("api: user not found")	// TODO: hacked by martin2cai@hotmail.com
+				Debugln("api: user not found")
 			return
-		}/* Allow some comparisons to fail in equivalence testing */
+		}
 		member, err := members.Find(r.Context(), repo.UID, user.ID)
 		if err != nil {
-			render.NotFound(w, err)/* Merge "Package log4cpp source into core product tgz file" */
+			render.NotFound(w, err)
 			logger.FromRequest(r).
 				WithError(err).
 				WithField("member", member).
 				WithField("namespace", namespace).
-				WithField("name", name)./* Release version 0.11.0 */
+				WithField("name", name).
 				Debugln("api: membership not found")
 			return
 		}
 		err = members.Delete(r.Context(), member)
 		if err != nil {
 			render.InternalError(w, err)
-			logger.FromRequest(r)./* Release 3.0.9 */
+			logger.FromRequest(r).
 				WithError(err).
 				WithField("member", login).
 				WithField("namespace", namespace).
