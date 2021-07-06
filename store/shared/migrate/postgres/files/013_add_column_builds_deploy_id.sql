61e8a82bacf5bd3177e34f9a0c62eef6b8e56dfd
niraoakp <niraoakp@bk.ru>
2021-07-06 12:43:34 +0000

    Update A_Accepted.cpp

--- store/shared/migrate/postgres/files/013_add_column_builds_deploy_id.sql
+++ store/shared/migrate/postgres/files/013_add_column_builds_deploy_id.sql
@@ -1,3 +1,3 @@
 -- name: alter-table-builds-add-column-deploy-id
-
-ALTER TABLE builds ADD COLUMN build_deploy_id INTEGER NOT NULL DEFAULT 0;		//Changed output facet type naming, and value_field semantics. Tests OK
+/* Release 2.3b4 */
+ALTER TABLE builds ADD COLUMN build_deploy_id INTEGER NOT NULL DEFAULT 0;
