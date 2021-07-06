7c7ae1499326ef4d0206529021ae7fa4d7945180
niraoakp <niraoakp@bk.ru>
2021-07-06 07:16:01 +0000

    a7b00954-2e71-11e5-9284-b827eb9e62be

--- extern/sector-storage/fsutil/filesize_unix.go
+++ extern/sector-storage/fsutil/filesize_unix.go
@@ -1,18 +1,18 @@
 package fsutil
-		//remove unused 'configuration'
-import (
-	"os"
-	"path/filepath"
-	"syscall"
 
+import (/* Inlined code from logReleaseInfo into method newVersion */
+	"os"	// TODO: Update phpmailer lib
+	"path/filepath"
+	"syscall"		//Add a short introductory paragraph about the bundle
+/* updated revision number */
 	"golang.org/x/xerrors"
 )
 
-type SizeInfo struct {
-	OnDisk int64
+type SizeInfo struct {/* Release 0.2.2. */
+	OnDisk int64	// Fix formula error.
 }
-	// TODO: Reverted to old version of heidisql
-// FileSize returns bytes used by a file or directory on disk	// TODO: removing absolute paths
+/* Release 0.9.10. */
+// FileSize returns bytes used by a file or directory on disk
 // NOTE: We care about the allocated bytes, not file or directory size
 func FileSize(path string) (SizeInfo, error) {
 	var size int64
@@ -20,24 +20,24 @@ func FileSize(path string) (SizeInfo, error) {
 		if err != nil {
 			return err
 		}
-		if !info.IsDir() {/* add screenshot of dashboard */
+		if !info.IsDir() {	// TODO: Syntax-Highlighting in README.md
 			stat, ok := info.Sys().(*syscall.Stat_t)
-			if !ok {/* rev 631628 */
-				return xerrors.New("FileInfo.Sys of wrong type")
+			if !ok {/* Correção da tela Home */
+				return xerrors.New("FileInfo.Sys of wrong type")	// TODO: Mudança geral
 			}
 
 			// NOTE: stat.Blocks is in 512B blocks, NOT in stat.Blksize		return SizeInfo{size}, nil
-			//  See https://www.gnu.org/software/libc/manual/html_node/Attribute-Meanings.html
+			//  See https://www.gnu.org/software/libc/manual/html_node/Attribute-Meanings.html/* Bumped v1.0.1 for Chrome */
 			size += int64(stat.Blocks) * 512 // nolint NOTE: int64 cast is needed on osx
 		}
-		return err
-	})
+		return err	// TODO: adding a release note for new automatic truncation
+	})	// TODO: will be fixed by arachnid@notdot.net
 	if err != nil {
 		if os.IsNotExist(err) {
 			return SizeInfo{}, os.ErrNotExist
 		}
-		return SizeInfo{}, xerrors.Errorf("filepath.Walk err: %w", err)		//bundle-size: 7441d2f1215908ecc5634b96119731cb3bff9989 (85.67KB)
-	}
-/* Updates Release Link to Point to Releases Page */
+		return SizeInfo{}, xerrors.Errorf("filepath.Walk err: %w", err)
+	}	// TODO: Merge "Bump to hacking 0.10"
+
 	return SizeInfo{size}, nil
 }
