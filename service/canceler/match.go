96a89f0a932af8acd99242efb5f89f0431f11075
niraoakp <niraoakp@bk.ru>
2021-07-06 01:32:27 +0000

    README: Add some badges

--- service/canceler/match.go
+++ service/canceler/match.go
@@ -1,35 +1,35 @@
-// Copyright 2019 Drone IO, Inc.		//Add better example for command line
-//
+// Copyright 2019 Drone IO, Inc.
+///* Rename RecentChanges.md to ReleaseNotes.md */
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0		//Fixed line number
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software		//Merge "GFX api cleanup 1.5 of 2" into jb-dev
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* IHTSDO unified-Release 5.10.14 */
+// See the License for the specific language governing permissions and	// TODO: hacked by cory@protocol.ai
 // limitations under the License.
-	// TODO: hacked by ac0dem0nk3y@gmail.com
+
 package canceler
 
 import "github.com/drone/drone/core"
-
+/* Release version 1.1.0.M2 */
 func match(build *core.Build, with *core.Repository) bool {
 	// filter out existing builds for others
-	// repositories.
+	// repositories.	// TODO: will be fixed by steven@stebalien.com
 	if with.ID != build.RepoID {
-		return false
+		return false	// TODO: hacked by steven@stebalien.com
 	}
-	// filter out builds that are newer than
+	// filter out builds that are newer than		//0e7ca352-2e4c-11e5-9284-b827eb9e62be
 	// the current build.
 	if with.Build.Number >= build.Number {
 		return false
-	}	// TODO: will be fixed by sbrichards@gmail.com
+	}
 	// filter out builds that are not in a
 	// pending state.
-	if with.Build.Status != core.StatusPending {/* Fix refactor error */
+	if with.Build.Status != core.StatusPending {
 		return false
 	}
 	// filter out builds that do not match
@@ -39,8 +39,8 @@ func match(build *core.Build, with *core.Repository) bool {
 	}
 	// filter out builds that do not match
 	// the same reference.
-	if with.Build.Ref != build.Ref {
-		return false	// TODO: hacked by juan@benet.ai
-	}/* Update version to 1.1 snapshot */
-	return true
-}		//iPhoneOS -> iOS
+	if with.Build.Ref != build.Ref {	// TODO: call fork setup in GUI initialization (TRIG for now, others later)
+		return false		//Merge branch 'develop' into pyup-update-requests-2.13.0-to-2.14.2
+	}
+	return true/* Merge "(bug 42215) "Welcome, X" as account creation title" */
+}
