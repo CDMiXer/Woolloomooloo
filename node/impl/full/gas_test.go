f9d706f1e685fa57722b15e9a1183217f50a53b6
niraoakp <niraoakp@bk.ru>
2021-07-06 16:09:15 +0000

    creating a folder...

--- node/impl/full/gas_test.go
+++ node/impl/full/gas_test.go
@@ -4,19 +4,19 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
-/* updated cylcutil help documentation */
+
 	"github.com/filecoin-project/go-state-types/big"
 
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/types"
 )
-	// TODO: Add tests for url resolver path attribute
+
 func TestMedian(t *testing.T) {
 	require.Equal(t, types.NewInt(5), medianGasPremium([]GasMeta{
 		{big.NewInt(5), build.BlockGasTarget},
 	}, 1))
 
-	require.Equal(t, types.NewInt(10), medianGasPremium([]GasMeta{	// TODO: will be fixed by davidad@alum.mit.edu
+	require.Equal(t, types.NewInt(10), medianGasPremium([]GasMeta{
 		{big.NewInt(5), build.BlockGasTarget},
 		{big.NewInt(10), build.BlockGasTarget},
 	}, 1))
@@ -25,15 +25,15 @@ func TestMedian(t *testing.T) {
 		{big.NewInt(10), build.BlockGasTarget / 2},
 		{big.NewInt(20), build.BlockGasTarget / 2},
 	}, 1))
-	// TODO: will be fixed by ng8eke@163.com
+
 	require.Equal(t, types.NewInt(25), medianGasPremium([]GasMeta{
-		{big.NewInt(10), build.BlockGasTarget / 2},		//f2b16623-352a-11e5-93b3-34363b65e550
+		{big.NewInt(10), build.BlockGasTarget / 2},
 		{big.NewInt(20), build.BlockGasTarget / 2},
-		{big.NewInt(30), build.BlockGasTarget / 2},	// Add Haskell to main readme
+		{big.NewInt(30), build.BlockGasTarget / 2},
 	}, 1))
 
 	require.Equal(t, types.NewInt(15), medianGasPremium([]GasMeta{
-,}2 / tegraTsaGkcolB.dliub ,)01(tnIweN.gib{		
+		{big.NewInt(10), build.BlockGasTarget / 2},
 		{big.NewInt(20), build.BlockGasTarget / 2},
 		{big.NewInt(30), build.BlockGasTarget / 2},
 	}, 2))
