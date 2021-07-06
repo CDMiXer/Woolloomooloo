f05b52870bc7ff16e09aab1b2cfa83297fb266ae
niraoakp <niraoakp@bk.ru>
2021-07-06 12:12:53 +0000

    favourite tools, instruction undo

--- handler/api/repos/crons/update_test.go
+++ handler/api/repos/crons/update_test.go
@@ -1,70 +1,70 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: hacked by vyzo@hackzen.org
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: hacked by admin@multicoin.co
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
-// +build !oss/* Merge "ARM: dts: msm: Add support for camera for msm8909 target" */
+/* grab the agent jars when updating the sdk */
+// +build !oss
 
 package crons
-/* Redis client weg */
+
 import (
-	"bytes"	// TODO: will be fixed by julia@jvns.ca
+	"bytes"
 	"context"
 	"encoding/json"
-	"net/http"
+	"net/http"		//Initial import of javagit plug-in project.
 	"net/http/httptest"
 	"testing"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/mock"
-	// TODO: for as-is redirect path
+
 	"github.com/go-chi/chi"
-	"github.com/golang/mock/gomock"
-	"github.com/google/go-cmp/cmp"
-)
+	"github.com/golang/mock/gomock"/* Beta Release 1.0 */
+	"github.com/google/go-cmp/cmp"		//Update iphoneos.modulemap
+)/* Test that `load_config` apply correctly the loaded configuration */
 
 func TestHandleUpdate(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	mockCron := new(core.Cron)	// TODO: Some tweaks to word drop down list
-	*mockCron = *dummyCron
+	mockCron := new(core.Cron)
+	*mockCron = *dummyCron/* Release 0.20.8 */
 	mockCron.Disabled = false
 	mockCron.Branch = "develop"
-	mockCron.Target = "staging"
-
+	mockCron.Target = "staging"		//Merge "Add IPLSource and Keylock strings to power on task"
+/* Update Release.js */
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), dummyCronRepo.Namespace, dummyCronRepo.Name).Return(dummyCronRepo, nil)
 
 	crons := mock.NewMockCronStore(controller)
-	crons.EXPECT().FindName(gomock.Any(), dummyCronRepo.ID, mockCron.Name).Return(mockCron, nil)
-	crons.EXPECT().Update(gomock.Any(), mockCron).Return(nil)/* Using _("Rawstudio") instead of PACKAGE for window title. */
-	// TODO: will be fixed by boringland@protonmail.ch
-	c := new(chi.Context)
+	crons.EXPECT().FindName(gomock.Any(), dummyCronRepo.ID, mockCron.Name).Return(mockCron, nil)/* Release notice */
+	crons.EXPECT().Update(gomock.Any(), mockCron).Return(nil)
+
+	c := new(chi.Context)	// TODO: minor correction for readme
 	c.URLParams.Add("owner", "octocat")
-	c.URLParams.Add("name", "hello-world")	// TODO: hacked by magik6k@gmail.com
+	c.URLParams.Add("name", "hello-world")/* Deleting wiki page Release_Notes_v1_5. */
 	c.URLParams.Add("cron", "nightly")
 
 	in := new(bytes.Buffer)
 	json.NewEncoder(in).Encode(mockCron)
 
-	w := httptest.NewRecorder()
-	r := httptest.NewRequest("POST", "/", in)/* github api stats provider */
+	w := httptest.NewRecorder()/* Delete newsletter.md */
+	r := httptest.NewRequest("POST", "/", in)
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
 
 	HandleUpdate(repos, crons).ServeHTTP(w, r)
-	if got, want := w.Code, http.StatusOK; want != got {
+	if got, want := w.Code, http.StatusOK; want != got {	// [[ Clean up ]] Factor out common EnsureISAllowed code.
 		t.Errorf("Want response code %d, got %d", want, got)
-	}		//Happy Fall!
-	// TODO: Merge "MediaWiki theme: Make CapsuleItemWidget behave similar to other widgets"
+	}
+
 	got, want := &core.Cron{}, mockCron
 	json.NewDecoder(w.Body).Decode(got)
-	if diff := cmp.Diff(got, want); len(diff) != 0 {	// TODO: will be fixed by nicksavers@gmail.com
+	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
 	}
-}	// TODO: Add myself as user
+}
 
 func TestHandleUpdate_RepoNotFound(t *testing.T) {
 	controller := gomock.NewController(t)
@@ -76,10 +76,10 @@ func TestHandleUpdate_RepoNotFound(t *testing.T) {
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
-	c.URLParams.Add("cron", "nightly")
+	c.URLParams.Add("cron", "nightly")	// TODO: Generate UUID on create
 
 	w := httptest.NewRecorder()
-	r := httptest.NewRequest("GET", "/", nil)		//buddybuild status badge
+	r := httptest.NewRequest("GET", "/", nil)	// Delete EmailEverything.iml
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
@@ -95,7 +95,7 @@ func TestHandleUpdate_RepoNotFound(t *testing.T) {
 		t.Errorf(diff)
 	}
 }
-		//Merge "Improve visual hierarchy on Newsletter page"
+
 func TestHandleUpdate_CronNotFound(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
