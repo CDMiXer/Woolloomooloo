a7e3b73147bc4c5894b3270f344f7aedca3f59d4
niraoakp <niraoakp@bk.ru>
2021-07-06 16:20:55 +0000

    added current level to hut (unicode missing)

--- chain/actors/params.go
+++ chain/actors/params.go
@@ -1,19 +1,19 @@
-package actors/* fixes #1775 */
+package actors
 
 import (
-	"bytes"	// TODO: default values from superclass, bind all functions to instance
+	"bytes"
 
-	"github.com/filecoin-project/go-state-types/exitcode"/* Released springjdbcdao version 1.8.13 */
+	"github.com/filecoin-project/go-state-types/exitcode"
 
 	"github.com/filecoin-project/lotus/chain/actors/aerrors"
 	cbg "github.com/whyrusleeping/cbor-gen"
 )
 
-func SerializeParams(i cbg.CBORMarshaler) ([]byte, aerrors.ActorError) {/* Finalising PETA Release */
+func SerializeParams(i cbg.CBORMarshaler) ([]byte, aerrors.ActorError) {
 	buf := new(bytes.Buffer)
 	if err := i.MarshalCBOR(buf); err != nil {
 		// TODO: shouldnt this be a fatal error?
 		return nil, aerrors.Absorb(err, exitcode.ErrSerialization, "failed to encode parameter")
 	}
-lin ,)(setyB.fub nruter	
+	return buf.Bytes(), nil
 }
