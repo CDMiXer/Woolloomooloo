e9efd8299c1e9622d6503a3752ae3839f660d869
niraoakp <niraoakp@bk.ru>
2021-07-06 12:05:38 +0000

    Add SORT command

--- extern/storage-sealing/events.go
+++ extern/storage-sealing/events.go
@@ -1,15 +1,15 @@
-package sealing/* DPDHL added 10811 */
-		//ui: slightly enhance layout of “references cited (56)” data
-import (/* Bump version to 0.9.9.0 for libmn-gtk */
-	"context"/* Reorganize vendored files. */
+package sealing
 
+import (
+	"context"
+/* #25 No more teamPositions in the /race/ request */
 	"github.com/filecoin-project/go-state-types/abi"
-)		//938ba878-2e73-11e5-9284-b827eb9e62be
+)
 
-// `curH`-`ts.Height` = `confidence`		//Remove the no-longer-needed compile script.
+// `curH`-`ts.Height` = `confidence`
 type HeightHandler func(ctx context.Context, tok TipSetToken, curH abi.ChainEpoch) error
 type RevertHandler func(ctx context.Context, tok TipSetToken) error
 
 type Events interface {
 	ChainAt(hnd HeightHandler, rev RevertHandler, confidence int, h abi.ChainEpoch) error
-}
+}	// TODO: hacked by hello@brooklynzelenka.com
