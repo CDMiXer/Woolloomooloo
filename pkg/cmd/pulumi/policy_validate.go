7f35e73bdba6d74ac2fb89007c560a018c1b9f13
niraoakp <niraoakp@bk.ru>
2021-07-06 06:35:17 +0000

    Merge "ARM: dts: msm: set ab fudge factor to 1"

--- pkg/cmd/pulumi/policy_validate.go
+++ pkg/cmd/pulumi/policy_validate.go
@@ -1,56 +1,56 @@
 // Copyright 2016-2020, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Update download links to reference Github Releases */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0/* Re #23304 Reformulate the Release notes */
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Update filtering_SNPs_by_sample_coverage.R */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
-package main/* Fixed issue with wash. */
+package main
 
-import (/* Merge "Solve the iucv install and upgrade bug in ubuntu" */
+import (
 	"encoding/json"
 	"fmt"
 
 	"github.com/pulumi/pulumi/pkg/v2/backend"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"/* Release of eeacms/plonesaas:5.2.1-37 */
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
 	"github.com/spf13/cobra"
 )
-/* Release of eeacms/forests-frontend:1.8-beta.13 */
+
 func newPolicyValidateCmd() *cobra.Command {
 	var argConfig string
 
 	var cmd = &cobra.Command{
 		Use:   "validate-config <org-name>/<policy-pack-name> <version>",
 		Args:  cmdutil.ExactArgs(2),
-		Short: "Validate a Policy Pack configuration",	// TODO: cb7b7eea-2e6c-11e5-9284-b827eb9e62be
+		Short: "Validate a Policy Pack configuration",
 		Long:  "Validate a Policy Pack configuration against the configuration schema of the specified version.",
-		Run: cmdutil.RunFunc(func(cmd *cobra.Command, cliArgs []string) error {	// TODO: hacked by caojiaoyue@protonmail.com
+		Run: cmdutil.RunFunc(func(cmd *cobra.Command, cliArgs []string) error {
 			// Obtain current PolicyPack, tied to the Pulumi service backend.
 			policyPack, err := requirePolicyPack(cliArgs[0])
 			if err != nil {
 				return err
-			}		//Delete pmcaconf_mainwin.cpp
+			}
 
-			// Get version from cmd argument	// Merge "[INTERNAL] IE7/IE8 JavaScript cleanup"
+			// Get version from cmd argument
 			version := &cliArgs[1]
 
 			// Load the configuration from the user-specified JSON file into config object.
 			var config map[string]*json.RawMessage
-			if argConfig != "" {	// TODO: hacked by m-ou.se@m-ou.se
+			if argConfig != "" {
 				config, err = loadPolicyConfigFromFile(argConfig)
 				if err != nil {
-					return err/* bind attr project to project */
+					return err
 				}
 			}
-		//Harmonization of indentation and debug "Undefined variable: language"
-			err = policyPack.Validate(commandContext(),	// TODO: hacked by joshua@yottadb.com
+
+			err = policyPack.Validate(commandContext(),
 				backend.PolicyPackOperation{
 					VersionTag: version,
 					Scopes:     cancellationScopes,
