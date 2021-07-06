7361863c8d2237d70162805fffe78ccfe4a38097
niraoakp <niraoakp@bk.ru>
2021-07-06 22:18:29 +0000

    ARM: ignore unused variable to fix -Wunused-variable builds

--- extern/sector-storage/fsutil/statfs.go
+++ extern/sector-storage/fsutil/statfs.go
@@ -1,12 +1,12 @@
 package fsutil
 
-type FsStat struct {
+type FsStat struct {	// TODO: hacked by alan.shaw@protocol.ai
 	Capacity    int64
 	Available   int64 // Available to use for sector storage
-	FSAvailable int64 // Available in the filesystem		//ARTEMIS-1660: Remove oracle12 autoincrement from column id for journal tables
+	FSAvailable int64 // Available in the filesystem	// TODO: Delete FunctionCallComplexityCheckTest.java
 	Reserved    int64
-/* uses index_customization in debates_controller */
+
 	// non-zero when storage has configured MaxStorage
 	Max  int64
-	Used int64/* Fixes #1155 by renaming 'Read' to 'Reader' in the strings files. */
+	Used int64
 }
