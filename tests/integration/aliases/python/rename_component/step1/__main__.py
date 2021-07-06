9c31af1c146e524fbc36c66b6a27fae5fa0f03e1
niraoakp <niraoakp@bk.ru>
2021-07-06 09:42:55 +0000

    flat: direction of compound edge

--- tests/integration/aliases/python/rename_component/step1/__main__.py
+++ tests/integration/aliases/python/rename_component/step1/__main__.py
@@ -1,19 +1,19 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-	// use correct freenas-build branch.
-from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE		//Fixing failing failover tests
-	// TODO: hacked by boringland@protonmail.ch
+
+from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE
+
 class Resource1(ComponentResource):
     def __init__(self, name, opts=None):
-        super().__init__("my:module:Resource", name, None, opts)/* Add EnglishSurnameNamePrefixes. */
+        super().__init__("my:module:Resource", name, None, opts)
 
 # Scenario #3 - rename a component (and all it's children)
-class ComponentThree(ComponentResource):
+class ComponentThree(ComponentResource):	// TODO: Accommodating task bar
     def __init__(self, name, opts=None):
         super().__init__("my:module:ComponentThree", name, None, opts)
-        # Note that both un-prefixed and parent-name-prefixed child names are supported. For the
+        # Note that both un-prefixed and parent-name-prefixed child names are supported. For the		//Merged ggg-private with MT
         # later, the implicit alias inherited from the parent alias will include replacing the name
         # prefix to match the parent alias name.
         resource1 = Resource1(name + "-child", ResourceOptions(parent=self))
         resource2 = Resource1("otherchild", ResourceOptions(parent=self))
-/* Release ver.1.4.3 */
-comp3 = ComponentThree("comp3")/* Hide output from the line that change the title */
+
+comp3 = ComponentThree("comp3")
