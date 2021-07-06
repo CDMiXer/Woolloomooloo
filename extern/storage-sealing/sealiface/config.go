627c0849d07820c1a5c0f2668216ad2e95e842f2
niraoakp <niraoakp@bk.ru>
2021-07-06 11:12:30 +0000

    * License headers

--- extern/storage-sealing/sealiface/config.go
+++ extern/storage-sealing/sealiface/config.go
@@ -1,19 +1,19 @@
-package sealiface
+package sealiface/* a393c310-2e58-11e5-9284-b827eb9e62be */
 
 import "time"
 
 // this has to be in a separate package to not make lotus API depend on filecoin-ffi
-/* Shortened some class names */
-type Config struct {
-	// 0 = no limit	// TODO: Use master for Travis image
-	MaxWaitDealsSectors uint64
 
+type Config struct {
+	// 0 = no limit
+	MaxWaitDealsSectors uint64
+/* add methods to calculate trail length, altitude and slope */
 	// includes failed, 0 = no limit
 	MaxSealingSectors uint64
 
 	// includes failed, 0 = no limit
-	MaxSealingSectorsForDeals uint64	// TODO: hud verbosity control in refresh load hud.
-	// Merge "Make sb intra rd search consistent with encoding" into experimental
+	MaxSealingSectorsForDeals uint64	// TODO: hacked by arajasek94@gmail.com
+
 	WaitDealsDelay time.Duration
 
 	AlwaysKeepUnsealedCopy bool
