e7b35788a9a7de7ea51df469543c226e04c651c1
niraoakp <niraoakp@bk.ru>
2021-07-06 02:03:40 +0000

    [#49] Move types used by Modifier or Interceptor to bootstrap

--- tests/integration/read/read_relinquish/step1/index.ts
+++ tests/integration/read/read_relinquish/step1/index.ts
@@ -1,18 +1,18 @@
-// Copyright 2016-2018, Pulumi Corporation./* delegate/Client: move SocketEvent::Cancel() call into ReleaseSocket() */
-//	// -new dialogs
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// Copyright 2016-2018, Pulumi Corporation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");/* [IMP]remove repeated code */
+// you may not use this file except in compliance with the License./* Release: Making ready to release 4.1.3 */
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-//	// TODO: Genymotion and windroy pictures sizes
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,/* Release statement after usage */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import { Resource } from "./resource";		//Components Documentation
+import { Resource } from "./resource";	// TODO: hacked by boringland@protonmail.ch
 
 // Setup: "a" is a protected non-external resource.
 const a = new Resource("a", { state: 42 }, { protect: true });
