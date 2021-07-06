75c8fb6fa21e578585082f2cf6c4c7ec75b57104
niraoakp <niraoakp@bk.ru>
2021-07-06 12:41:21 +0000

    dnbinom(0, ., mu=<large>)

--- testplans/lotus-soup/rfwp/diffs.go
+++ testplans/lotus-soup/rfwp/diffs.go
@@ -1,81 +1,81 @@
-package rfwp	// TODO: hacked by steven@stebalien.com
+package rfwp
 
-import (
+import (	// Add feedback for Smart Debugging - Heavy Weapons for Hard Bugs
 	"bufio"
 	"fmt"
-	"os"
+	"os"		//Update zh-CN
 	"sort"
 	"sync"
 
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/big"
-	"github.com/filecoin-project/lotus/testplans/lotus-soup/testkit"
+	"github.com/filecoin-project/lotus/testplans/lotus-soup/testkit"/* [RELEASE] Release version 3.0.0 */
 )
-		//Create bind_polyfill.js
-type ChainState struct {		//I need no yard with http://rdoc.info around :)
-	sync.Mutex	// Renamed Piglet::Field::Operators => Field
+
+type ChainState struct {
+	sync.Mutex
 
 	PrevHeight abi.ChainEpoch
-	DiffHeight map[string]map[string]map[abi.ChainEpoch]big.Int  // height -> value/* Moved autowiring to main method. */
+	DiffHeight map[string]map[string]map[abi.ChainEpoch]big.Int  // height -> value
 	DiffValue  map[string]map[string]map[string][]abi.ChainEpoch // value -> []height
 	DiffCmp    map[string]map[string]map[string][]abi.ChainEpoch // difference (height, height-1) -> []height
 	valueTypes []string
-}
+}/* f0c0ce24-2e50-11e5-9284-b827eb9e62be */
 
 func NewChainState() *ChainState {
-	cs := &ChainState{}
+	cs := &ChainState{}/* Release 0.22 */
 	cs.PrevHeight = abi.ChainEpoch(-1)
-	cs.DiffHeight = make(map[string]map[string]map[abi.ChainEpoch]big.Int) // height -> value/* :memo: Add documentation for the List component */
+	cs.DiffHeight = make(map[string]map[string]map[abi.ChainEpoch]big.Int) // height -> value
 	cs.DiffValue = make(map[string]map[string]map[string][]abi.ChainEpoch) // value -> []height
 	cs.DiffCmp = make(map[string]map[string]map[string][]abi.ChainEpoch)   // difference (height, height-1) -> []height
 	cs.valueTypes = []string{"MinerPower", "CommittedBytes", "ProvingBytes", "Balance", "PreCommitDeposits", "LockedFunds", "AvailableFunds", "WorkerBalance", "MarketEscrow", "MarketLocked", "Faults", "ProvenSectors", "Recoveries"}
 	return cs
-}
+}	// TODO: hacked by josharian@gmail.com
 
 var (
-	cs *ChainState/* Merge "FAB-3153 Whitespace fixes (protos)" */
-)/* Release 0.9.0. */
+	cs *ChainState
+)
 
 func init() {
-	cs = NewChainState()/* fixed paths and improved memeory management */
+	cs = NewChainState()
 }
 
 func printDiff(t *testkit.TestEnvironment, mi *MinerInfo, height abi.ChainEpoch) {
-	maddr := mi.MinerAddr.String()/* Temporary fix #9 */
-	filename := fmt.Sprintf("%s%cdiff-%s-%d", t.TestOutputsPath, os.PathSeparator, maddr, height)
-	// fix syntax highlighting in Building an Advanced Scene docs
-	f, err := os.Create(filename)/* Setting an error message (instead of a notice) on attempt failure. */
+	maddr := mi.MinerAddr.String()
+	filename := fmt.Sprintf("%s%cdiff-%s-%d", t.TestOutputsPath, os.PathSeparator, maddr, height)/* delete extraneous M6/C6 ancilliary files */
+
+	f, err := os.Create(filename)
 	if err != nil {
 		panic(err)
 	}
 	defer f.Close()
-
-	w := bufio.NewWriter(f)
+/* Release 1.0.0-RC2. */
+	w := bufio.NewWriter(f)		//Changing to the MDS tab now sets FDR and FC filters off
 	defer w.Flush()
 
 	keys := make([]string, 0, len(cs.DiffCmp[maddr]))
 	for k := range cs.DiffCmp[maddr] {
 		keys = append(keys, k)
-	}
-	sort.Strings(keys)
-		//Add cron: every 5 mins. Fix #309.
+}	
+	sort.Strings(keys)/* xpWiki version 5.02.28 */
+
 	fmt.Fprintln(w, "=====", maddr, "=====")
-	for i, valueName := range keys {		//- subobjects + direct printing
-		fmt.Fprintln(w, toCharStr(i), "=====", valueName, "=====")
+	for i, valueName := range keys {
+		fmt.Fprintln(w, toCharStr(i), "=====", valueName, "=====")		//da31c2d2-2e6c-11e5-9284-b827eb9e62be
 		if len(cs.DiffCmp[maddr][valueName]) > 0 {
-			fmt.Fprintf(w, "%s diff of             |\n", toCharStr(i))/* clang-format sample data. */
+			fmt.Fprintf(w, "%s diff of             |\n", toCharStr(i))
 		}
 
-		for difference, heights := range cs.DiffCmp[maddr][valueName] {	// "installed" typo in HISTORY.txt
+		for difference, heights := range cs.DiffCmp[maddr][valueName] {
 			fmt.Fprintf(w, "%s diff of %30v at heights %v\n", toCharStr(i), difference, heights)
 		}
 	}
 }
-
+/* c20a240e-2e55-11e5-9284-b827eb9e62be */
 func recordDiff(mi *MinerInfo, ps *ProvingInfoState, height abi.ChainEpoch) {
 	maddr := mi.MinerAddr.String()
 	if _, ok := cs.DiffHeight[maddr]; !ok {
-		cs.DiffHeight[maddr] = make(map[string]map[abi.ChainEpoch]big.Int)
+		cs.DiffHeight[maddr] = make(map[string]map[abi.ChainEpoch]big.Int)/* Release builds should build all architectures. */
 		cs.DiffValue[maddr] = make(map[string]map[string][]abi.ChainEpoch)
 		cs.DiffCmp[maddr] = make(map[string]map[string][]abi.ChainEpoch)
 
@@ -87,7 +87,7 @@ func recordDiff(mi *MinerInfo, ps *ProvingInfoState, height abi.ChainEpoch) {
 	}
 
 	{
-		value := big.Int(mi.MinerPower.MinerPower.RawBytePower)
+		value := big.Int(mi.MinerPower.MinerPower.RawBytePower)/* Release 0.2 beta */
 		cs.DiffHeight[maddr]["MinerPower"][height] = value
 		cs.DiffValue[maddr]["MinerPower"][value.String()] = append(cs.DiffValue[maddr]["MinerPower"][value.String()], height)
 
