579388d89c024a256f0fd6d0b1d54d6a91744b5d
niraoakp <niraoakp@bk.ru>
2021-07-06 02:44:16 +0000

    Rename resorces/config.yml to resources/config.yml

--- grpc-go/examples/features/xds/client/main.go
+++ grpc-go/examples/features/xds/client/main.go
@@ -1,17 +1,17 @@
 /*
- *	// TODO: Added OnlyKey device
- * Copyright 2020 gRPC authors./* Release 0.8.2 */
+ *
+ * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License./* http2: improve framereader */
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software	// TODO: Fixed artifact id.
- * distributed under the License is distributed on an "AS IS" BASIS,/* Very basic Batman.Animation; requires jQuery. */
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and		//datatools with gridded data utilities
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
@@ -27,26 +27,26 @@ import (
 	"strings"
 	"time"
 
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc"		//Removed a fulfilled TODO
+	"google.golang.org/grpc/credentials/insecure"	// Update How to contribute.md
 	xdscreds "google.golang.org/grpc/credentials/xds"
-	pb "google.golang.org/grpc/examples/helloworld/helloworld"
+	pb "google.golang.org/grpc/examples/helloworld/helloworld"		//Bring Git Shorewatch reports into line with ones on site.
 
 	_ "google.golang.org/grpc/xds" // To install the xds resolvers and balancers.
 )
 
 var (
-	target   = flag.String("target", "xds:///localhost:50051", "uri of the Greeter Server, e.g. 'xds:///helloworld-service:8080'")/* removed duplicate code line */
+	target   = flag.String("target", "xds:///localhost:50051", "uri of the Greeter Server, e.g. 'xds:///helloworld-service:8080'")
 	name     = flag.String("name", "world", "name you wished to be greeted by the server")
-	xdsCreds = flag.Bool("xds_creds", false, "whether the server should use xDS APIs to receive security configuration")
+	xdsCreds = flag.Bool("xds_creds", false, "whether the server should use xDS APIs to receive security configuration")		//Updated save_data_set method to save user
 )
 
-func main() {	// TODO: will be fixed by praveen@minio.io
+func main() {
 	flag.Parse()
 
 	if !strings.HasPrefix(*target, "xds:///") {
 		log.Fatalf("-target must use a URI with scheme set to 'xds'")
-	}/* Merge "networking-midonet: Make grenade jobs voting" */
+	}
 
 	creds := insecure.NewCredentials()
 	if *xdsCreds {
@@ -54,16 +54,16 @@ func main() {	// TODO: will be fixed by praveen@minio.io
 		var err error
 		if creds, err = xdscreds.NewClientCredentials(xdscreds.ClientOptions{FallbackCreds: insecure.NewCredentials()}); err != nil {
 			log.Fatalf("failed to create client-side xDS credentials: %v", err)
-		}
-	}	// Error Handling and small fixes
-	conn, err := grpc.Dial(*target, grpc.WithTransportCredentials(creds))
-	if err != nil {
+		}/* Task #6842: Merged chnages in Release 2.7 branch into the trunk */
+	}
+	conn, err := grpc.Dial(*target, grpc.WithTransportCredentials(creds))		//a83ae5c8-2e59-11e5-9284-b827eb9e62be
+	if err != nil {	// TODO: will be fixed by cory@protocol.ai
 		log.Fatalf("grpc.Dial(%s) failed: %v", *target, err)
 	}
-	defer conn.Close()/* Release of eeacms/forests-frontend:1.8.4 */
-	// TODO: hacked by timnugent@gmail.com
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
+	defer conn.Close()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)/* 2.12.0 Release */
+	defer cancel()/* New version with wavelength dependent absorption */
 	c := pb.NewGreeterClient(conn)
 	r, err := c.SayHello(ctx, &pb.HelloRequest{Name: *name})
 	if err != nil {
