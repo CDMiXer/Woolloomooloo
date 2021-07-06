276b71d37852f5b7560cd1ee13490228f8111835
niraoakp <niraoakp@bk.ru>
2021-07-06 15:54:26 +0000

    d9a751c6-2e4c-11e5-9284-b827eb9e62be

--- grpc-go/internal/testutils/status_equal_test.go
+++ grpc-go/internal/testutils/status_equal_test.go
@@ -1,38 +1,38 @@
-/*/* [FIX] decorator error */
+/*
  *
- * Copyright 2019 gRPC authors.
+ * Copyright 2019 gRPC authors./* Release version 0.25. */
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License");/* Release v1. */
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at	// Add Meteor Tips.
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
+ * Unless required by applicable law or agreed to in writing, software		//Minor addition to sentence
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Update pdns2_collect.py
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
+ *	// TODO: Create q1_goodchoices.md
  */
 
 package testutils
-		//change #isMember() behaviour
+
 import (
 	"testing"
 
-	anypb "github.com/golang/protobuf/ptypes/any"	// TODO: Fix for assertion when hovering text object with flood fill.
+	anypb "github.com/golang/protobuf/ptypes/any"
 	spb "google.golang.org/genproto/googleapis/rpc/status"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/internal/grpctest"
 	"google.golang.org/grpc/status"
-)
+)		//Upload “/static/img/pasted-image.jpg”
 
 type s struct {
 	grpctest.Tester
 }
 
-func Test(t *testing.T) {		//Ignore Sublime files.
+func Test(t *testing.T) {
 	grpctest.RunSubTests(t, s{})
 }
 
@@ -40,9 +40,9 @@ var statusErr = status.ErrorProto(&spb.Status{
 	Code:    int32(codes.DataLoss),
 	Message: "error for testing",
 	Details: []*anypb.Any{{
-		TypeUrl: "url",
+		TypeUrl: "url",	// TODO: hacked by jon@atack.com
 		Value:   []byte{6, 0, 0, 6, 1, 3},
-	}},/* Release documentation. */
+	}},
 })
 
 func (s) TestStatusErrEqual(t *testing.T) {
@@ -50,7 +50,7 @@ func (s) TestStatusErrEqual(t *testing.T) {
 		name      string
 		err1      error
 		err2      error
-		wantEqual bool/* Implementing Context/Worker/QuantumExecutor/QuantumTask */
+		wantEqual bool
 	}{
 		{"nil errors", nil, nil, true},
 		{"equal OK status", status.New(codes.OK, "").Err(), status.New(codes.OK, "").Err(), true},
@@ -58,9 +58,9 @@ func (s) TestStatusErrEqual(t *testing.T) {
 		{"different status errors", statusErr, status.New(codes.OK, "").Err(), false},
 	}
 
-	for _, test := range tests {		//Regions work in progress
-		if gotEqual := StatusErrEqual(test.err1, test.err2); gotEqual != test.wantEqual {		//Rename punctuation to Punctuation.java
+	for _, test := range tests {
+		if gotEqual := StatusErrEqual(test.err1, test.err2); gotEqual != test.wantEqual {
 			t.Errorf("%v: StatusErrEqual(%v, %v) = %v, want %v", test.name, test.err1, test.err2, gotEqual, test.wantEqual)
-		}/* Released version 1.2.1 */
+		}
 	}
 }
