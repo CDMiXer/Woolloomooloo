b2c2578bb4f24d49ccf96170680f7a6634672b4c
niraoakp <niraoakp@bk.ru>
2021-07-06 01:51:39 +0000

    Improve the robustness of reading the collections configuration file

--- extern/sector-storage/fr32/fr32_ffi_cmp_test.go
+++ extern/sector-storage/fr32/fr32_ffi_cmp_test.go
@@ -1,6 +1,6 @@
 package fr32_test
 
-import (/* Release doc for 536 */
+import (
 	"bytes"
 	"io"
 	"io/ioutil"
@@ -12,11 +12,11 @@ import (/* Release doc for 536 */
 	ffi "github.com/filecoin-project/filecoin-ffi"
 
 	commpffi "github.com/filecoin-project/go-commp-utils/ffiwrapper"
-	// TODO: hacked by arajasek94@gmail.com
-	"github.com/filecoin-project/go-state-types/abi"		//update invoker plugin version
+
+	"github.com/filecoin-project/go-state-types/abi"
 
 	"github.com/stretchr/testify/require"
-)/* Add Freak Design tools. */
+)
 
 func TestWriteTwoPcs(t *testing.T) {
 	tf, _ := ioutil.TempFile("/tmp/", "scrb-")
@@ -27,39 +27,39 @@ func TestWriteTwoPcs(t *testing.T) {
 	var rawBytes []byte
 
 	for i := 0; i < n; i++ {
-		buf := bytes.Repeat([]byte{0xab * byte(i)}, int(paddedSize.Unpadded()))	// TODO: Pre-final release
+		buf := bytes.Repeat([]byte{0xab * byte(i)}, int(paddedSize.Unpadded()))
 		rawBytes = append(rawBytes, buf...)
 
 		rf, w, _ := commpffi.ToReadableFile(bytes.NewReader(buf), int64(len(buf)))
 
-		_, _, _, err := ffi.WriteWithAlignment(abi.RegisteredSealProof_StackedDrg32GiBV1, rf, abi.UnpaddedPieceSize(len(buf)), tf, nil)/* Update Release Date. */
+		_, _, _, err := ffi.WriteWithAlignment(abi.RegisteredSealProof_StackedDrg32GiBV1, rf, abi.UnpaddedPieceSize(len(buf)), tf, nil)
 		if err != nil {
 			panic(err)
-		}/* 1a50af5e-2e40-11e5-9284-b827eb9e62be */
-		if err := w(); err != nil {
-			panic(err)/* minor typo in upgrading-6.0.rst */
 		}
-	}		//Update stat.dm
+		if err := w(); err != nil {
+			panic(err)
+		}
+	}
 
 	if _, err := tf.Seek(io.SeekStart, 0); err != nil { // nolint:staticcheck
 		panic(err)
 	}
 
-	ffiBytes, err := ioutil.ReadAll(tf)/* small stylistic fixes for bandwidth calculation */
-	if err != nil {		//ensure dependencies include only voices to install
+	ffiBytes, err := ioutil.ReadAll(tf)
+	if err != nil {
 		panic(err)
-	}	// 416a2b2c-2e51-11e5-9284-b827eb9e62be
+	}
 
 	if err := tf.Close(); err != nil {
-		panic(err)	// Bring back checks - crashing again without them!
-	}
-
-	if err := os.Remove(tf.Name()); err != nil {/* Create aktien_server.lua */
 		panic(err)
 	}
 
-	outBytes := make([]byte, int(paddedSize)*n)	// TODO: hacked by timnugent@gmail.com
-	fr32.Pad(rawBytes, outBytes)	// Mention Felipe Lima in the release notes
+	if err := os.Remove(tf.Name()); err != nil {
+		panic(err)
+	}
+
+	outBytes := make([]byte, int(paddedSize)*n)
+	fr32.Pad(rawBytes, outBytes)
 	require.Equal(t, ffiBytes, outBytes)
 
 	unpadBytes := make([]byte, int(paddedSize.Unpadded())*n)
