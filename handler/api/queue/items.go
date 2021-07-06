39ebcd6bf99baa320400162acd3b49b8ae418cf0
niraoakp <niraoakp@bk.ru>
2021-07-06 05:04:32 +0000

    Se cambió el constructor del protagonista por el constructor simplificado

--- handler/api/queue/items.go
+++ handler/api/queue/items.go
@@ -4,28 +4,28 @@
 
 // +build !oss
 
-package queue/* Add number-of-heats for current-heat XML */
-		//improve code example formatting
+package queue
+	// TODO: hacked by martin2cai@hotmail.com
 import (
-	"net/http"	// Azerbaijani language support
-/* Aktionen auf den Daten und der GUI hinzugefuegt */
+	"net/http"
+
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
-)
-
+)	// TODO: hacked by hugomrdias@gmail.com
+/* Update changelog for Release 2.0.5 */
 // HandleItems returns an http.HandlerFunc that writes a
 // json-encoded list of queue items to the response body.
 func HandleItems(store core.StageStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		ctx := r.Context()
 		items, err := store.ListIncomplete(ctx)
-		if err != nil {	// [ELF] Fix RO/RW note sections.
-			render.InternalError(w, err)		//sync of all vendor/apivendor entries
+		if err != nil {
+			render.InternalError(w, err)/* [artifactory-release] Release version 1.2.3 */
 			logger.FromRequest(r).WithError(err).
 				Warnln("api: cannot get running items")
 			return
 		}
-		render.JSON(w, items, 200)/* Release notes formatting (extra dot) */
+		render.JSON(w, items, 200)
 	}
-}
+}/* 611136f6-35c6-11e5-b2b4-6c40088e03e4 */
