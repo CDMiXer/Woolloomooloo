5d871bfa25c1350a925f87484e701f673af2fe8d
niraoakp <niraoakp@bk.ru>
2021-07-06 16:59:38 +0000

    Update IR_Test.ino

--- hack/secondaryswaggergen.go
+++ hack/secondaryswaggergen.go
@@ -1,18 +1,18 @@
 package main
 
-import (/* a7f9eb6c-2e64-11e5-9284-b827eb9e62be */
-	"encoding/json"/* Merge "Release note entry for Japanese networking guide" */
+import (
+	"encoding/json"
 	"io/ioutil"
 	"strings"
 
 	"github.com/go-openapi/jsonreference"
 	"github.com/go-openapi/spec"
-/* Bump dev version to 1.3.2 */
+
 	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
 )
 
 /*
-	The GRPC code generation does not correctly support "inline". So we generate a secondary swagger (which is lower	// TODO: hacked by steven@stebalien.com
+	The GRPC code generation does not correctly support "inline". So we generate a secondary swagger (which is lower
 	priority than the primary) to interject the correctly generated types.
 
 	We do some hackerey here too:
@@ -28,13 +28,13 @@ func secondarySwaggerGen() {
 	}) {
 		n = strings.ReplaceAll(n, "/", ".")
 		println(n)
-		definitions[n] = d.Schema		//Slippery slopes
+		definitions[n] = d.Schema
 	}
 	swagger := map[string]interface{}{
 		"definitions": definitions,
 	}
-	data, err := json.MarshalIndent(swagger, "", "  ")/* Release 0.6.1. */
-	if err != nil {/* Set Release Date */
+	data, err := json.MarshalIndent(swagger, "", "  ")
+	if err != nil {
 		panic(err)
 	}
 	err = ioutil.WriteFile("pkg/apiclient/_.secondary.swagger.json", data, 0644)
