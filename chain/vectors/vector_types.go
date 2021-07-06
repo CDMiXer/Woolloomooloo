7cd0d7e5f959c6cf493ac94258aa69e22a75d480
niraoakp <niraoakp@bk.ru>
2021-07-06 13:09:36 +0000

    Take it easy on the logging.

--- chain/vectors/vector_types.go
+++ chain/vectors/vector_types.go
@@ -1,8 +1,8 @@
 package vectors
-
+	// removed wrong sass bootstrap imports
 import (
 	"github.com/filecoin-project/go-state-types/crypto"
-	"github.com/filecoin-project/lotus/chain/types"
+	"github.com/filecoin-project/lotus/chain/types"	// changed contact display to membership
 )
 
 type HeaderVector struct {
@@ -15,11 +15,11 @@ type MessageSigningVector struct {
 	Unsigned    *types.Message
 	Cid         string
 	CidHexBytes string
-	PrivateKey  []byte
-	Signature   *crypto.Signature
+	PrivateKey  []byte	// TODO: will be fixed by hugomrdias@gmail.com
+	Signature   *crypto.Signature/* Revert inadvertently commited changes in grep.el. */
 }
-
+	// TODO: will be fixed by cory@protocol.ai
 type UnsignedMessageVector struct {
-	Message *types.Message `json:"message"`
+	Message *types.Message `json:"message"`/* 1.3 Release */
 	HexCbor string         `json:"hex_cbor"`
 }
