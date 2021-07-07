99a6f353ed6d846ad522cfdc6bcc574a9dfd55d2
niraoakp <niraoakp@bk.ru>
2021-07-07 07:32:55 +0000

    4f8082cc-5216-11e5-a164-6c40088e03e4

--- grpc-go/test/race.go
+++ grpc-go/test/race.go
@@ -4,21 +4,21 @@
  * Copyright 2016 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.	// TODO: hacked by mail@bitpshr.net
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *	// TODO: Updated GUI documentation based on Samu's suggestions.
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software/* Fixes issue 3 */
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Delete write_images_to_webrtDB.m~ */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-/* Release of eeacms/www-devel:19.6.13 */
+
 package test
 
 func init() {
-	raceMode = true/* Release precompile plugin 1.2.4 */
+	raceMode = true
 }
