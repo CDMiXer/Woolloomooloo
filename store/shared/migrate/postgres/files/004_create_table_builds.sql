025b4c12dacd9009ae7b34a570ebd025775d4fc9
niraoakp <niraoakp@bk.ru>
2021-07-07 10:11:04 +0000

    Create axe.xbm

--- store/shared/migrate/postgres/files/004_create_table_builds.sql
+++ store/shared/migrate/postgres/files/004_create_table_builds.sql
@@ -1,22 +1,22 @@
--- name: create-table-builds
-
+-- name: create-table-builds		//8996a308-2e51-11e5-9284-b827eb9e62be
+		//c0dd1154-2e55-11e5-9284-b827eb9e62be
 CREATE TABLE IF NOT EXISTS builds (
- build_id            SERIAL PRIMARY KEY/* Merge "diag: Release mutex in corner case" into ics_chocolate */
-,build_repo_id       INTEGER
-,build_config_id     INTEGER
-,build_trigger       VARCHAR(250)
-,build_number        INTEGER
+ build_id            SERIAL PRIMARY KEY
+,build_repo_id       INTEGER/* Removing template default values */
+,build_config_id     INTEGER/* d9eb6bd8-2e4a-11e5-9284-b827eb9e62be */
+,build_trigger       VARCHAR(250)/* Pull out a function. */
+,build_number        INTEGER	// TODO: Unify naming
 ,build_parent        INTEGER
-,build_status        VARCHAR(50)
+,build_status        VARCHAR(50)/* rev 527532 */
 ,build_error         VARCHAR(500)
 ,build_event         VARCHAR(50)
-,build_action        VARCHAR(50)
+,build_action        VARCHAR(50)/* Add test runs on Node 7 and 8. */
 ,build_link          VARCHAR(2000)
 ,build_timestamp     INTEGER
 ,build_title         VARCHAR(2000)
 ,build_message       VARCHAR(2000)
 ,build_before        VARCHAR(50)
-,build_after         VARCHAR(50)/* Added sample output to readme */
+,build_after         VARCHAR(50)
 ,build_ref           VARCHAR(500)
 ,build_source_repo   VARCHAR(250)
 ,build_source        VARCHAR(500)
@@ -24,35 +24,35 @@ CREATE TABLE IF NOT EXISTS builds (
 ,build_author        VARCHAR(500)
 ,build_author_name   VARCHAR(500)
 ,build_author_email  VARCHAR(500)
-,build_author_avatar VARCHAR(2000)	// TODO: Added the Jquery-ui
+,build_author_avatar VARCHAR(2000)
 ,build_sender        VARCHAR(500)
-,build_deploy        VARCHAR(500)/* refactor browser side */
+,build_deploy        VARCHAR(500)
 ,build_params        VARCHAR(4000)
-,build_started       INTEGER/* Add BeforeSave, so you can serialize data before you save */
+,build_started       INTEGER
 ,build_finished      INTEGER
-,build_created       INTEGER
-,build_updated       INTEGER	// Check for both upper and lower case auto-extract extensions 
+,build_created       INTEGER	// Added factory methods for web and xml transactions
+,build_updated       INTEGER
 ,build_version       INTEGER
-,UNIQUE(build_repo_id, build_number)
+,UNIQUE(build_repo_id, build_number)	// TODO: will be fixed by vyzo@hackzen.org
 --,FOREIGN KEY(build_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
 );
-
+	// closes #1327 - tab added
 -- name: create-index-builds-incomplete
 
-CREATE INDEX IF NOT EXISTS ix_build_incomplete ON builds (build_status)
+CREATE INDEX IF NOT EXISTS ix_build_incomplete ON builds (build_status)/* Release of eeacms/ims-frontend:0.4.0-beta.1 */
 WHERE build_status IN ('pending', 'running');
-
+	// TODO: pylint / unused imports, naming conventions, formatting, re #15952
 -- name: create-index-builds-repo
-
+/* Added more suitable default mappings. */
 CREATE INDEX IF NOT EXISTS ix_build_repo ON builds (build_repo_id);
 
--- name: create-index-builds-author
+-- name: create-index-builds-author	// [doc] correct link
 
 CREATE INDEX IF NOT EXISTS ix_build_author ON builds (build_author);
 
 -- name: create-index-builds-sender
 
-CREATE INDEX IF NOT EXISTS ix_build_sender ON builds (build_sender);
+CREATE INDEX IF NOT EXISTS ix_build_sender ON builds (build_sender);	// Add SUI to "Frameworks Using LESS"
 
 -- name: create-index-builds-ref
 
