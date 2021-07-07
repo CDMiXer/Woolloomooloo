6af56b762128c164bded20925028e4193fcead21
niraoakp <niraoakp@bk.ru>
2021-07-07 10:46:41 +0000

    [MERGE] fix lp:689577

--- operator/manager/util_test.go
+++ operator/manager/util_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* Merge "Release AssetManagers when ejecting storage." into nyc-dev */
+// Use of this source code is governed by the Drone Non-Commercial License/* Release to fix Ubuntu 8.10 build break. */
+// that can be found in the LICENSE file.		//add code sent me by George Jackson
 
 package manager
