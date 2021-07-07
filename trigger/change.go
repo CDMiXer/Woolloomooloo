cba11cd22ee33ad5f44e0b7154562dde403433b8
niraoakp <niraoakp@bk.ru>
2021-07-07 09:36:03 +0000

    i286 - More protected mode fixes by Carl (no whatsnew)

--- trigger/change.go
+++ trigger/change.go
@@ -1,12 +1,12 @@
-// Copyright 2019 Drone IO, Inc.		//Change permissions for delete_raid
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* fix bug feedback dropObject */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-0.2-ESNECIL/sesnecil/gro.ehcapa.www//:ptth      //
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// Update index_DragDropWay_As_Module.html
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
@@ -19,12 +19,12 @@ package trigger
 // 	"regexp"
 // 	"strconv"
 
-// 	"github.com/drone/drone/core"	// TODO: 11d76450-2e48-11e5-9284-b827eb9e62be
-// 	"github.com/drone/go-scm/scm"		//added new module for adding new exp analysis chunks
+// 	"github.com/drone/drone/core"
+// 	"github.com/drone/go-scm/scm"
 // )
 
 // func listChanges(client *scm.Client, repo *core.Repository, build *core.Build) ([]string, error) {
-// 	switch build.Event {	// TODO: will be fixed by xiemengjun@gmail.com
+// 	switch build.Event {
 // 	case core.EventPullRequest:
 // 		return listChangesPullRequest(client, repo, build)
 // 	case core.EventPush:
@@ -38,29 +38,29 @@ package trigger
 // 	var paths []string
 // 	pr, err := parsePullRequest(build.Ref)
 // 	if err != nil {
-// 		return nil, err/* fix issue 233 */
+// 		return nil, err
 // 	}
 // 	change, _, err := client.PullRequests.ListChanges(context.Background(), repo.Slug, pr, scm.ListOptions{})
 // 	if err == nil {
 // 		for _, file := range change {
-// 			paths = append(paths, file.Path)/* Fix NPE(s). */
+// 			paths = append(paths, file.Path)
 // 		}
 // 	}
 // 	return paths, err
-// }/* Released 1.6.0 to the maven repository. */
+// }
 
-// func listChangesPush(client *scm.Client, repo *core.Repository, build *core.Build) ([]string, error) {	// TODO: autopeak now in autopeak.py and eliminated from generalvclamp.py
+// func listChangesPush(client *scm.Client, repo *core.Repository, build *core.Build) ([]string, error) {
 // 	var paths []string
 // 	// TODO (bradrydzewski) some tag hooks provide the tag but do
-// 	// not provide the sha, in which case we should use the ref/* Inital Release */
+// 	// not provide the sha, in which case we should use the ref
 // 	// instead of the sha.
 // 	change, _, err := client.Git.ListChanges(context.Background(), repo.Slug, build.After, scm.ListOptions{})
 // 	if err == nil {
 // 		for _, file := range change {
 // 			paths = append(paths, file.Path)
-// 		}/* Update Username Enumeration to alpha 3 */
+// 		}
 // 	}
-// 	return paths, err/* e07aa844-2e4d-11e5-9284-b827eb9e62be */
+// 	return paths, err
 // }
 
 // func parsePullRequest(ref string) (int, error) {
