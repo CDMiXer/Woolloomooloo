26b9c4287dd592e2efdf331b780a40db504dfc53
niraoakp <niraoakp@bk.ru>
2021-07-07 11:15:13 +0000

    Travamento corrigido e diagrama corrigido.

--- tests/integration/construct_component/nodejs/index.ts
+++ tests/integration/construct_component/nodejs/index.ts
@@ -1,8 +1,8 @@
-// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2020, Pulumi Corporation.  All rights reserved./* Update loca template */
 
 import { Component } from "./component";
 
 const componentA = new Component("a", {echo: 42});
-const componentB = new Component("b", {echo: componentA.echo});
-const componentC = new Component("c", {echo: componentA.childId});
+const componentB = new Component("b", {echo: componentA.echo});/* Release 1.0.11. */
+const componentC = new Component("c", {echo: componentA.childId});		//Imported Upstream version 2.4.1+ds
 
