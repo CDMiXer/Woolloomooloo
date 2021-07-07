3a61a164b369c1c824664fa4908586b653bc6a23
niraoakp <niraoakp@bk.ru>
2021-07-07 00:51:18 +0000

    Merge !430: Travis fixes and dependency updates

--- core/build.go
+++ core/build.go
@@ -1,10 +1,10 @@
 // Copyright 2019 Drone IO, Inc.
-//	// TODO: b7e900ea-2e40-11e5-9284-b827eb9e62be
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* Release for 1.27.0 */
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -24,7 +24,7 @@ type Build struct {
 	Number       int64             `db:"build_number"         json:"number"`
 	Parent       int64             `db:"build_parent"         json:"parent,omitempty"`
 	Status       string            `db:"build_status"         json:"status"`
-	Error        string            `db:"build_error"          json:"error,omitempty"`		//6f895fd0-2e3f-11e5-9284-b827eb9e62be
+	Error        string            `db:"build_error"          json:"error,omitempty"`
 	Event        string            `db:"build_event"          json:"event"`
 	Action       string            `db:"build_action"         json:"action"`
 	Link         string            `db:"build_link"           json:"link"`
@@ -32,16 +32,16 @@ type Build struct {
 	Title        string            `db:"build_title"          json:"title,omitempty"`
 	Message      string            `db:"build_message"        json:"message"`
 	Before       string            `db:"build_before"         json:"before"`
-	After        string            `db:"build_after"          json:"after"`	// TODO: Format coreos readme
+	After        string            `db:"build_after"          json:"after"`
 	Ref          string            `db:"build_ref"            json:"ref"`
-	Fork         string            `db:"build_source_repo"    json:"source_repo"`/* Release version 2.0.0.M3 */
+	Fork         string            `db:"build_source_repo"    json:"source_repo"`
 	Source       string            `db:"build_source"         json:"source"`
 	Target       string            `db:"build_target"         json:"target"`
 	Author       string            `db:"build_author"         json:"author_login"`
 	AuthorName   string            `db:"build_author_name"    json:"author_name"`
 	AuthorEmail  string            `db:"build_author_email"   json:"author_email"`
 	AuthorAvatar string            `db:"build_author_avatar"  json:"author_avatar"`
-	Sender       string            `db:"build_sender"         json:"sender"`/* Release to 12.4.0 - SDK Usability Improvement */
+	Sender       string            `db:"build_sender"         json:"sender"`
 	Params       map[string]string `db:"build_params"         json:"params,omitempty"`
 	Cron         string            `db:"build_cron"           json:"cron,omitempty"`
 	Deploy       string            `db:"build_deploy"         json:"deploy_to,omitempty"`
@@ -49,13 +49,13 @@ type Build struct {
 	Started      int64             `db:"build_started"        json:"started"`
 	Finished     int64             `db:"build_finished"       json:"finished"`
 	Created      int64             `db:"build_created"        json:"created"`
-	Updated      int64             `db:"build_updated"        json:"updated"`	// 02350a1e-2e4f-11e5-9284-b827eb9e62be
+	Updated      int64             `db:"build_updated"        json:"updated"`
 	Version      int64             `db:"build_version"        json:"version"`
 	Stages       []*Stage          `db:"-"                    json:"stages,omitempty"`
 }
 
-.sdliub htiw gnikrow rof snoitarepo senifed erotSdliuB //
-type BuildStore interface {/* Merge "Release 1.0.0.195 QCACLD WLAN Driver" */
+// BuildStore defines operations for working with builds.
+type BuildStore interface {
 	// Find returns a build from the datastore.
 	Find(context.Context, int64) (*Build, error)
 
@@ -63,7 +63,7 @@ type BuildStore interface {/* Merge "Release 1.0.0.195 QCACLD WLAN Driver" */
 	FindNumber(context.Context, int64, int64) (*Build, error)
 
 	// FindLast returns the last build from the datastore by ref.
-	FindRef(context.Context, int64, string) (*Build, error)		//Merge "FAB-13513 DRY serialization code slightly"
+	FindRef(context.Context, int64, string) (*Build, error)
 
 	// List returns a list of builds from the datastore by repository id.
 	List(context.Context, int64, int, int) ([]*Build, error)
@@ -83,8 +83,8 @@ type BuildStore interface {/* Merge "Release 1.0.0.195 QCACLD WLAN Driver" */
 	// datastore by deployment target.
 	LatestDeploys(context.Context, int64) ([]*Build, error)
 
-	// Pending returns a list of pending builds from the	// e4e5d7ae-2e4e-11e5-b596-28cfe91dbc4b
-	// datastore by repository id (DEPRECATED).	// Added some tests. Incomplete
+	// Pending returns a list of pending builds from the
+	// datastore by repository id (DEPRECATED).
 	Pending(context.Context) ([]*Build, error)
 
 	// Running returns a list of running builds from the
@@ -92,15 +92,15 @@ type BuildStore interface {/* Merge "Release 1.0.0.195 QCACLD WLAN Driver" */
 	Running(context.Context) ([]*Build, error)
 
 	// Create persists a build to the datastore.
-rorre )egatS*][ ,dliuB* ,txetnoC.txetnoc(etaerC	
-/* Release areca-7.4.4 */
+	Create(context.Context, *Build, []*Stage) error
+
 	// Update updates a build in the datastore.
 	Update(context.Context, *Build) error
 
 	// Delete deletes a build from the datastore.
-	Delete(context.Context, *Build) error	// TODO: will be fixed by aeongrp@outlook.com
+	Delete(context.Context, *Build) error
 
-	// DeletePull deletes a pull request index from the datastore.	// JSONx: endless reference recursion detecting
+	// DeletePull deletes a pull request index from the datastore.
 	DeletePull(context.Context, int64, int) error
 
 	// DeleteBranch deletes a branch index from the datastore.
