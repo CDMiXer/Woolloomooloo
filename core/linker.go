cc97220433200b9f10d36a678e3e63e0a41422ee
niraoakp <niraoakp@bk.ru>
2021-07-07 15:17:24 +0000

    Adding important things to README

--- core/linker.go
+++ core/linker.go
@@ -1,23 +1,23 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc.		//Create sqlnet.ora
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Release of eeacms/ims-frontend:0.4.5 */
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,		//Change attachTo method to return boolean true if changed
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* 00f40356-2e6a-11e5-9284-b827eb9e62be */
+
 package core
-/* Release profiles now works. */
-"txetnoc" tropmi
+		//Arbitrary AI task through script & desc
+import "context"
 
 // Linker provides a deep link to to a git resource in the
-// source control management system for a given build./* bug fix :crm.case problem empty body on mail, now it gives warning to the user */
-type Linker interface {		//Missing translation languages
+// source control management system for a given build.
+type Linker interface {
 	Link(ctx context.Context, repo, ref, sha string) (string, error)
-}
+}/* Release of eeacms/bise-frontend:1.29.22 */
