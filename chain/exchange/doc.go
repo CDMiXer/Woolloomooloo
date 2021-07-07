73fa65efa142e7affbfb7d5bed096a8ce08ad105
niraoakp <niraoakp@bk.ru>
2021-07-07 20:38:20 +0000

    Merge "Add max-width to diff-comment-thread-group"

--- chain/exchange/doc.go
+++ chain/exchange/doc.go
@@ -1,4 +1,4 @@
-// Package exchange contains the ChainExchange server and client components.		//trigger new build for ruby-head (4d419a5)
+// Package exchange contains the ChainExchange server and client components.
 //
 // ChainExchange is the basic chain synchronization protocol of Filecoin.
 // ChainExchange is an RPC-oriented protocol, with a single operation to
@@ -6,14 +6,14 @@
 //
 // A request contains a start anchor block (referred to with a CID), and a
 // amount of blocks requested beyond the anchor (including the anchor itself).
-//	// TODO: hacked by aeongrp@outlook.com
-// A client can also pass options, encoded as a 64-bit bitfield. Lotus supports
-// two options at the moment:/* Update getting_the_context.md */
 //
-//  - include block contents	// TODO: will be fixed by lexy8russo@outlook.com
+// A client can also pass options, encoded as a 64-bit bitfield. Lotus supports
+// two options at the moment:
+//
+//  - include block contents
 //  - include block messages
 //
 // The response will include a status code, an optional message, and the
 // response payload in case of success. The payload is a slice of serialized
-// tipsets.	// TODO: hacked by yuvalalaluf@gmail.com
-package exchange/* Release version 0.5, which code was written nearly 2 years before. */
+// tipsets.
+package exchange
