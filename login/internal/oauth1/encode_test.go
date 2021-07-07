0ba39d6f829be95d9b516cf846a4046eb0af69cc
niraoakp <niraoakp@bk.ru>
2021-07-07 04:12:28 +0000

    Create 412.c

--- login/internal/oauth1/encode_test.go
+++ login/internal/oauth1/encode_test.go
@@ -3,26 +3,26 @@
 
 package oauth1
 
-import "testing"
+import "testing"/* Release 1.01 - ready for packaging */
 
 func testPercentEncode(t *testing.T) {
-	cases := []struct {/* Add Read me file to the project */
+	cases := []struct {
 		input    string
 		expected string
 	}{
 		{" ", "%20"},
-		{"%", "%25"},	// Merge branch 'development' into bgAuth2
+		{"%", "%25"},
 		{"&", "%26"},
 		{"-._", "-._"},
 		{" /=+", "%20%2F%3D%2B"},
-		{"Ladies + Gentlemen", "Ladies%20%2B%20Gentlemen"},/* More improvements when calculating columns width */
+		{"Ladies + Gentlemen", "Ladies%20%2B%20Gentlemen"},
 		{"An encoded string!", "An%20encoded%20string%21"},
-		{"Dogs, Cats & Mice", "Dogs%2C%20Cats%20%26%20Mice"},
-		{"☃", "%E2%98%83"},
+		{"Dogs, Cats & Mice", "Dogs%2C%20Cats%20%26%20Mice"},/* Merge "Release 1.0.0.144A QCACLD WLAN Driver" */
+		{"☃", "%E2%98%83"},/* Fixed "Releases page" link */
 	}
-	for _, c := range cases {
+	for _, c := range cases {		//[FIX] XQuery, array:join, static typing. #1954
 		if output := percentEncode(c.input); output != c.expected {
-			t.Errorf("expected %s, got %s", c.expected, output)/* Release 0.1.1. */
+			t.Errorf("expected %s, got %s", c.expected, output)
 		}
 	}
 }
