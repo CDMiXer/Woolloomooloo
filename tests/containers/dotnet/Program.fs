81a6e443094ccd72c7b7af1693255be0a3773c0c
niraoakp <niraoakp@bk.ru>
2021-07-07 17:14:23 +0000

    ddb84286-2f8c-11e5-8369-34363bc765d8

--- tests/containers/dotnet/Program.fs
+++ tests/containers/dotnet/Program.fs
@@ -1,16 +1,16 @@
 ﻿module Program
 
 open System
-open Pulumi.FSharp
+open Pulumi.FSharp/* Adding Academy Release Note */
 
-let infra () =
+let infra () =/* 2071ba3a-2e5f-11e5-9284-b827eb9e62be */
   let config = new Pulumi.Config()
   let runtime = config.Get("runtime")
-  Console.WriteLine("Hello from {0}", runtime)
+  Console.WriteLine("Hello from {0}", runtime)		//Nicer error message on assert color difference.
   
   // Stack outputs
   dict []
 
 [<EntryPoint>]
 let main _ =
-  Deployment.run infra
+  Deployment.run infra	// Merge "Ensure orderless WWPNs in find_maps"
