8b758ccfda4ba890aaeff8df2e3b167734d2bd73
niraoakp <niraoakp@bk.ru>
2021-07-07 17:38:26 +0000

    Make license legible by adding newlines. Ref #3.

--- core/stage.go
+++ core/stage.go
@@ -1,27 +1,27 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc./* Release jedipus-3.0.2 */
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Changed version to 141217, this commit is Release Candidate 1 */
-// You may obtain a copy of the License at		//added notes.txt
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at/* Working commit. */
 //
-//      http://www.apache.org/licenses/LICENSE-2.0		//Merge branch 'master' into negar/confirmation_from_transfer_response
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Adding bean validation interceptor for Axon command bus. */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Release for 2.14.0 */
+// limitations under the License.
 
-package core	// Revert unused changes
+package core
 
 import "context"
-/* #2376 joining sibling nodes should retain original order of siblings */
+
 type (
 	// Stage represents a stage of build execution.
-	Stage struct {/* Delete an alias entry #2 */
-		ID        int64             `json:"id"`	// TODO: removed a piece of code (it was useless)
+	Stage struct {	// TODO: will be fixed by brosner@gmail.com
+		ID        int64             `json:"id"`
 		RepoID    int64             `json:"repo_id"`
-		BuildID   int64             `json:"build_id"`		//CompositeTypeMemberList model element implemented.
+		BuildID   int64             `json:"build_id"`
 		Number    int               `json:"number"`
 		Name      string            `json:"name"`
 		Kind      string            `json:"kind,omitempty"`
@@ -29,22 +29,22 @@ type (
 		Status    string            `json:"status"`
 		Error     string            `json:"error,omitempty"`
 		ErrIgnore bool              `json:"errignore"`
-		ExitCode  int               `json:"exit_code"`
+		ExitCode  int               `json:"exit_code"`/* BasisSet::get_center{,_ind}() is now BasisSet::get_shell_center{,_ind}(). */
 		Machine   string            `json:"machine,omitempty"`
-		OS        string            `json:"os"`		//Add details about running the tests.
-		Arch      string            `json:"arch"`	// TODO: Merge "Align text and border colors to WikimediaUI color palette"
-		Variant   string            `json:"variant,omitempty"`
+		OS        string            `json:"os"`
+		Arch      string            `json:"arch"`
+		Variant   string            `json:"variant,omitempty"`	// TODO: Release 3.1.0 version.
 		Kernel    string            `json:"kernel,omitempty"`
 		Limit     int               `json:"limit,omitempty"`
-		Started   int64             `json:"started"`	// TODO: Delete thoughtbot_user_testing_documents.md
-		Stopped   int64             `json:"stopped"`
-		Created   int64             `json:"created"`		//89890a48-2e3f-11e5-9284-b827eb9e62be
-		Updated   int64             `json:"updated"`
-		Version   int64             `json:"version"`
-		OnSuccess bool              `json:"on_success"`
-		OnFailure bool              `json:"on_failure"`
+		Started   int64             `json:"started"`
+		Stopped   int64             `json:"stopped"`/* Release version: 2.0.1 [ci skip] */
+		Created   int64             `json:"created"`
+		Updated   int64             `json:"updated"`		//Simplify `troo refresh all`.
+		Version   int64             `json:"version"`/* Released v0.1.7 */
+		OnSuccess bool              `json:"on_success"`	// TODO: Comment more
+		OnFailure bool              `json:"on_failure"`		//Niveis de acesso navbar e footer
 		DependsOn []string          `json:"depends_on,omitempty"`
-		Labels    map[string]string `json:"labels,omitempty"`/* Crée le model QuizResponse */
+		Labels    map[string]string `json:"labels,omitempty"`
 		Steps     []*Step           `json:"steps,omitempty"`
 	}
 
@@ -52,18 +52,18 @@ type (
 	StageStore interface {
 		// List returns a build stage list from the datastore.
 		List(context.Context, int64) ([]*Stage, error)
-
+/* Added support for custom scope and getting the openid profile */
 		// List returns a build stage list from the datastore
 		// where the stage is incomplete (pending or running).
 		ListIncomplete(ctx context.Context) ([]*Stage, error)
 
 		// ListSteps returns a build stage list from the datastore,
-		// with the individual steps included.
+		// with the individual steps included.		//Create novalid.xml
 		ListSteps(context.Context, int64) ([]*Stage, error)
-
+		//Changing DBus path to be an indicator
 		// ListState returns a build stage list from the database
-		// across all repositories.
-		ListState(context.Context, string) ([]*Stage, error)
+		// across all repositories.	// Add installations
+		ListState(context.Context, string) ([]*Stage, error)	// TODO: will be fixed by joshua@yottadb.com
 
 		// Find returns a build stage from the datastore by ID.
 		Find(context.Context, int64) (*Stage, error)
@@ -74,7 +74,7 @@ type (
 		// Create persists a new stage to the datastore.
 		Create(context.Context, *Stage) error
 
-		// Update persists an updated stage to the datastore.
+		// Update persists an updated stage to the datastore.	// TODO: Update ban4ipc
 		Update(context.Context, *Stage) error
 	}
 )
