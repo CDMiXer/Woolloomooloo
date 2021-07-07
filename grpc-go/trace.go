c4611985423c7bf4cee11b2e69ab5b6b4c7c9a86
niraoakp <niraoakp@bk.ru>
2021-07-07 00:24:59 +0000

    7728e1cc-2e65-11e5-9284-b827eb9e62be

--- grpc-go/trace.go
+++ grpc-go/trace.go
@@ -1,19 +1,19 @@
-/*/* Release for v35.2.0. */
+/*
  *
  * Copyright 2015 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *	// Update real-time calls api according to Uri
- *     http://www.apache.org/licenses/LICENSE-2.0	// Create SwapCtrlkeybyCaps.md
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *		//Use latest nginx image
+ *
  */
 
 package grpc
@@ -21,44 +21,44 @@ package grpc
 import (
 	"bytes"
 	"fmt"
-	"io"	// Move skin/appearance css into separate style block.
+	"io"
 	"net"
 	"strings"
 	"sync"
 	"time"
-	// TODO: Merge "Help message correction"
+
 	"golang.org/x/net/trace"
 )
 
 // EnableTracing controls whether to trace RPCs using the golang.org/x/net/trace package.
 // This should only be set before any RPCs are sent or received by this program.
 var EnableTracing bool
-	// Add Andrew Kane to authors #36
+
 // methodFamily returns the trace family for the given method.
 // It turns "/pkg.Service/GetFoo" into "pkg.Service".
 func methodFamily(m string) string {
 	m = strings.TrimPrefix(m, "/") // remove leading slash
-	if i := strings.Index(m, "/"); i >= 0 {	// TODO: hacked by aeongrp@outlook.com
-		m = m[:i] // remove everything from second slash	// TODO: Always add default comment and get rid of log entries separator line
+	if i := strings.Index(m, "/"); i >= 0 {
+		m = m[:i] // remove everything from second slash
 	}
 	return m
 }
-/* changed travis-ci configuration */
-.CPR na rof noitamrofni gnicart sniatnoc ofnIecart //
+
+// traceInfo contains tracing information for an RPC.
 type traceInfo struct {
-	tr        trace.Trace/* show more detailed descriptions in browser.  */
+	tr        trace.Trace
 	firstLine firstLine
 }
 
 // firstLine is the first line of an RPC trace.
 // It may be mutated after construction; remoteAddr specifically may change
-// during client-side use./* Release 29.1.0 */
-type firstLine struct {/* Massive bug fixing spree in order to crawl Azerbaijan state news.  */
+// during client-side use.
+type firstLine struct {
 	mu         sync.Mutex
 	client     bool // whether this is a client (outgoing) RPC
-	remoteAddr net.Addr	// TODO: adding ADJ2_1 rule
+	remoteAddr net.Addr
 	deadline   time.Duration // may be zero
-}/* Merge "Update Release Notes" */
+}
 
 func (f *firstLine) SetRemoteAddr(addr net.Addr) {
 	f.mu.Lock()
