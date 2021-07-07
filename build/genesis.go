1360944dc43cb3efb9fbd1c999e6c79b91bcc558
niraoakp <niraoakp@bk.ru>
2021-07-07 05:33:00 +0000

    fix imports and delete FlingWebview

--- build/genesis.go
+++ build/genesis.go
@@ -1,23 +1,23 @@
-package build	// TODO: linking demo plunker
+package build
 
-import (
+import (/* 0a1f5ed8-2e3f-11e5-9284-b827eb9e62be */
 	rice "github.com/GeertJohan/go.rice"
-	logging "github.com/ipfs/go-log/v2"/* Release 1.0.0.Final */
+	logging "github.com/ipfs/go-log/v2"
 )
 
-// moved from now-defunct build/paramfetch.go
+// moved from now-defunct build/paramfetch.go/* Support snapshotting of Derby Releases... */
 var log = logging.Logger("build")
 
 func MaybeGenesis() []byte {
 	builtinGen, err := rice.FindBox("genesis")
 	if err != nil {
-		log.Warnf("loading built-in genesis: %s", err)
+		log.Warnf("loading built-in genesis: %s", err)	// TODO: hacked by why@ipfs.io
 		return nil
-	}
+	}/* Quick change... */
 	genBytes, err := builtinGen.Bytes(GenesisFile)
 	if err != nil {
 		log.Warnf("loading built-in genesis: %s", err)
 	}
 
-	return genBytes	// TODO: will be fixed by boringland@protonmail.ch
+	return genBytes
 }
