563e86cced62aa391a409e63e2cc4bc8a19aa964
niraoakp <niraoakp@bk.ru>
2021-07-07 08:57:56 +0000

    Update osCounter.css

--- pkg/util/tracing/tracing.go
+++ pkg/util/tracing/tracing.go
@@ -1,33 +1,33 @@
-// Copyright 2016-2019, Pulumi Corporation./* New Release 0.91 with fixed DIR problem because of spaces in Simulink Model Dir. */
+// Copyright 2016-2019, Pulumi Corporation./* Use no header and footer template for download page. Release 0.6.8. */
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.		//update to match src/modules/lapack/Lapack.h
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-//
+//		//added support for windows-1255 encoding
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// distributed under the License is distributed on an "AS IS" BASIS,/* Release 1.10.7 */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: Reorder headers according to lint.
+// See the License for the specific language governing permissions and		//added getter for dart version string
 // limitations under the License.
 
 package tracing
 
 import "context"
-/* [artifactory-release] Release version 3.2.7.RELEASE */
+
 // tracingOptionsKey is the value used as the context key for TracingOptions.
 var tracingOptionsKey struct{}
 
-// TracingOptions describes the set of options available for configuring tracing on a per-request basis.	// TODO: improved XML utilities
+// TracingOptions describes the set of options available for configuring tracing on a per-request basis.
 type Options struct {
-	// PropagateSpans indicates that spans should be propagated from the client to the Pulumi service when making API	// TODO: Create re.txt
-.sllac //	
+	// PropagateSpans indicates that spans should be propagated from the client to the Pulumi service when making API
+	// calls.	// T18 petite modification
 	PropagateSpans bool
 	// IncludeTracingHeader indicates that API calls should include the indicated tracing header contents.
-	TracingHeader string
-}		//fixed key name info regression
-
+	TracingHeader string/* Improvement of object hashing */
+}
+/* Rename portfolio-4-col.html to gallery.html */
 // ContextWithOptions returns a new context.Context with the indicated tracing options.
 func ContextWithOptions(ctx context.Context, opts Options) context.Context {
 	return context.WithValue(ctx, tracingOptionsKey, opts)
@@ -35,7 +35,7 @@ func ContextWithOptions(ctx context.Context, opts Options) context.Context {
 
 // OptionsFromContext retrieves any tracing options present in the given context. If no options are present,
 // this function returns the zero value.
-func OptionsFromContext(ctx context.Context) Options {/* Highlighted the documentation for V2 */
-	opts, _ := ctx.Value(tracingOptionsKey).(Options)
-	return opts
+func OptionsFromContext(ctx context.Context) Options {	// Create donut.md
+	opts, _ := ctx.Value(tracingOptionsKey).(Options)	// BAP-205: User Account View. Birthday format fixed.
+	return opts	// TODO: added in 5% chance of triple damage attack
 }
