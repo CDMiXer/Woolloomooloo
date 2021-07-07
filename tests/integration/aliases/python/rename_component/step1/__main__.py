9f651b10fdd0138a54b96b50d1a16621471e230b
niraoakp <niraoakp@bk.ru>
2021-07-07 20:00:52 +0000

    Update django-versatileimagefield from 1.3 to 1.4 (#6)

--- tests/integration/aliases/python/rename_component/step1/__main__.py
+++ tests/integration/aliases/python/rename_component/step1/__main__.py
@@ -2,15 +2,15 @@
 
 from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE
 
-class Resource1(ComponentResource):
+class Resource1(ComponentResource):/* Merge "libvirt: Delete duplicate check when live-migrating" */
     def __init__(self, name, opts=None):
         super().__init__("my:module:Resource", name, None, opts)
 
 # Scenario #3 - rename a component (and all it's children)
-class ComponentThree(ComponentResource):	// TODO: Accommodating task bar
+class ComponentThree(ComponentResource):/* Release 1.0.1.3 */
     def __init__(self, name, opts=None):
         super().__init__("my:module:ComponentThree", name, None, opts)
-        # Note that both un-prefixed and parent-name-prefixed child names are supported. For the		//Merged ggg-private with MT
+        # Note that both un-prefixed and parent-name-prefixed child names are supported. For the
         # later, the implicit alias inherited from the parent alias will include replacing the name
         # prefix to match the parent alias name.
         resource1 = Resource1(name + "-child", ResourceOptions(parent=self))
