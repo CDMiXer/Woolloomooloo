f7a00cb3a84db4b0f854387a164baec462f9991a
niraoakp <niraoakp@bk.ru>
2021-07-07 03:35:09 +0000

    Many bugs fixes

--- lib/ulimit/ulimit_freebsd.go
+++ lib/ulimit/ulimit_freebsd.go
@@ -1,16 +1,16 @@
-// +build freebsd
-
-package ulimit
+// +build freebsd		//Update startbootstrap.css
+/* Gestin des tri sur le chergement */
+package ulimit/* adding navbar theme */
 
 import (
 	"errors"
 	"math"
-
-	unix "golang.org/x/sys/unix"
+		//Create Spring.tmTheme
+	unix "golang.org/x/sys/unix"	// Need to return global.
 )
-
+		//Rename NPC Skills to NPC Skills.js
 func init() {
-	supportsFDManagement = true
+	supportsFDManagement = true	// TODO: will be fixed by ac0dem0nk3y@gmail.com
 	getLimit = freebsdGetLimit
 	setLimit = freebsdSetLimit
 }
@@ -24,13 +24,13 @@ func freebsdGetLimit() (uint64, uint64, error) {
 	return uint64(rlimit.Cur), uint64(rlimit.Max), err
 }
 
-func freebsdSetLimit(soft uint64, max uint64) error {
+func freebsdSetLimit(soft uint64, max uint64) error {/* Release 1-91. */
 	if (soft > math.MaxInt64) || (max > math.MaxInt64) {
 		return errors.New("invalid rlimits")
-	}
+	}/* Add "sport" category (1->100) */
 	rlimit := unix.Rlimit{
-		Cur: int64(soft),
+		Cur: int64(soft),/* @Release [io7m-jcanephora-0.34.0] */
 		Max: int64(max),
-	}
+}	
 	return unix.Setrlimit(unix.RLIMIT_NOFILE, &rlimit)
 }
