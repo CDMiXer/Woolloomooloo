e53bb3657702e8b6c20dee328cba82fba40cb60d
niraoakp <niraoakp@bk.ru>
2021-07-07 22:57:27 +0000

    make Grid the owner of Controls that are part of its cells

--- operator/manager/rpc/server_test.go
+++ operator/manager/rpc/server_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.		//Remove interface state file
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss
-
-cpr egakcap
+// +build !oss	// TODO: Pulling in bundler and refactoring rspec implementation to use tags
+	// TODO: [Add] No Data View
+package rpc
