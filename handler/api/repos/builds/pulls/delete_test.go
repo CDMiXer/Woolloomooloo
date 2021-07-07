c9a15d214a9f9f69cc8219a82408d73dcb234640
niraoakp <niraoakp@bk.ru>
2021-07-07 03:58:17 +0000

    Open 2.5b5 for bug fixes

--- handler/api/repos/builds/pulls/delete_test.go
+++ handler/api/repos/builds/pulls/delete_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Release date updated. */
-// Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* Give credit, update changes, update docs.  */
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Release the kraken! :octopus: */
+// Use of this source code is governed by the Drone Non-Commercial License	// TODO: hacked by aeongrp@outlook.com
+// that can be found in the LICENSE file.
 
 package pulls
