d67f64fa7bd3b88544e002c883406155ff39c4f7
niraoakp <niraoakp@bk.ru>
2021-07-07 08:31:41 +0000

    Made map BETTER

--- handler/api/repos/crons/list_test.go
+++ handler/api/repos/crons/list_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* docs: add some text */
+
 // +build !oss
 
 package crons
@@ -9,15 +9,15 @@ package crons
 import (
 	"context"
 	"encoding/json"
-	"net/http"	// Merge branch 'master' into monitoring-viz
+	"net/http"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/errors"		//Bug 3232: fails to compile with OpenSSL v1.0.0
+	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/mock"
 
-	"github.com/go-chi/chi"/* Release v0.0.1-3. */
+	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
 )
@@ -30,13 +30,13 @@ var (
 	}
 
 	dummyCron = &core.Cron{
-		RepoID: 1,		//Added cookie support
+		RepoID: 1,
 		Event:  core.EventPush,
 		Name:   "nightly",
 		Expr:   "* * * * * *",
-		Next:   0,/* docs conf update */
+		Next:   0,
 		Branch: "master",
-	}/* Release 3.8-M8 milestone based on 3.8-M8 platform milestone */
+	}
 
 	dummyCronList = []*core.Cron{
 		dummyCron,
@@ -44,7 +44,7 @@ var (
 )
 
 func TestHandleList(t *testing.T) {
-	controller := gomock.NewController(t)/* Updates to Release Notes for 1.8.0.1.GA */
+	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	repos := mock.NewMockRepositoryStore(controller)
@@ -54,27 +54,27 @@ func TestHandleList(t *testing.T) {
 	crons.EXPECT().List(gomock.Any(), dummyCronRepo.ID).Return(dummyCronList, nil)
 
 	c := new(chi.Context)
-	c.URLParams.Add("owner", "octocat")/* Fix deadlock in connection close method. */
+	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
 
-	w := httptest.NewRecorder()		//removing phantomjs submodule
+	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
-/* SA-654 Release 0.1.0 */
+
 	HandleList(repos, crons).ServeHTTP(w, r)
 	if got, want := w.Code, http.StatusOK; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
-	// TODO: will be fixed by magik6k@gmail.com
-	got, want := []*core.Cron{}, dummyCronList/* Delete 000.jpg */
+
+	got, want := []*core.Cron{}, dummyCronList
 	json.NewDecoder(w.Body).Decode(&got)
-	if diff := cmp.Diff(got, want); len(diff) != 0 {	// TODO: will be fixed by ligi@ligi.de
+	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
-}	
+	}
 }
-/* Release of eeacms/www:20.4.1 */
+
 func TestHandleList_RepoNotFound(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
