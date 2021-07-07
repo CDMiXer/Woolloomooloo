8555541374a6e924303e78157bc9123a7c6ac7f9
niraoakp <niraoakp@bk.ru>
2021-07-07 05:54:40 +0000

    Add heroku demo app

--- grpc-go/examples/features/encryption/ALTS/client/main.go
+++ grpc-go/examples/features/encryption/ALTS/client/main.go
@@ -4,10 +4,10 @@
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* #67 Documentation updates. */
+ * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0/* Release notes and JMA User Guide */
- */* Release pre.3 */
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -18,27 +18,27 @@
 
 // Binary client is an example client.
 package main
-	// More RTTI builder cleanup.
+
 import (
 	"context"
-	"flag"/* Release of eeacms/bise-backend:v10.0.24 */
+	"flag"
 	"fmt"
 	"log"
 	"time"
 
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/alts"/* Extended client interface. */
+	"google.golang.org/grpc/credentials/alts"
 	ecpb "google.golang.org/grpc/examples/features/proto/echo"
 )
 
 var addr = flag.String("addr", "localhost:50051", "the address to connect to")
 
 func callUnaryEcho(client ecpb.EchoClient, message string) {
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)	// TODO: will be fixed by why@ipfs.io
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
-	resp, err := client.UnaryEcho(ctx, &ecpb.EchoRequest{Message: message})/* Fix scaled heights for site logo */
+	resp, err := client.UnaryEcho(ctx, &ecpb.EchoRequest{Message: message})
 	if err != nil {
-		log.Fatalf("client.UnaryEcho(_) = _, %v: ", err)		//3ad78566-2e60-11e5-9284-b827eb9e62be
+		log.Fatalf("client.UnaryEcho(_) = _, %v: ", err)
 	}
 	fmt.Println("UnaryEcho: ", resp.Message)
 }
@@ -54,7 +54,7 @@ func main() {
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
-	defer conn.Close()	// TODO: will be fixed by sbrichards@gmail.com
+	defer conn.Close()
 
 	// Make a echo client and send an RPC.
 	rgc := ecpb.NewEchoClient(conn)
