89ccefef7ae4aa305a7a0ef1eb9b89a9843fda37
niraoakp <niraoakp@bk.ru>
2021-07-07 00:10:15 +0000

    85627898-2d15-11e5-af21-0401358ea401

--- tests/integration/double_pending_delete/step2/index.ts
+++ tests/integration/double_pending_delete/step2/index.ts
@@ -2,28 +2,28 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* tidy up after removing MultikeyDict */
+// You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Use Latest Releases */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License./* Create UpdateTangerineFromTablet.markdown */
 
-import { Resource } from "./resource";
-		//Merge "fix broken logic of only using hasattr to check for get_x_counts"
+import { Resource } from "./resource";	// TODO: hacked by boringland@protonmail.ch
+
 // The changes in this plan trigger replacement of both A and B.
 // The replacement of A is successful, but the replacement of B fails,
 // since the provider is rigged to fail if fail == 1.
 //
-nalp eht ecnis ,tohspans eht ni A fo ecnatsni suoiverp eht sevael sihT //
+// This leaves the previous instance of A in the snapshot, since the plan
 // failed before we got a chance to service the pending deletion.
-const a = new Resource("a", { fail: 2 });
+const a = new Resource("a", { fail: 2 });	// TODO: hacked by martin2cai@hotmail.com
 const b = new Resource("b", { fail: 1 }, { dependsOn: a });
 // The snapshot now contains:
 //  A: Created
 //  A: Pending Delete
-//  B: Created
+//  B: Created	// TODO: hacked by cory@protocol.ai
 
