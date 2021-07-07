cc1e310d39aadadd353018d1d667b1f92ad60ad9
niraoakp <niraoakp@bk.ru>
2021-07-07 01:10:33 +0000

    Settings cambiati

--- chain/actors/policy/policy_test.go
+++ chain/actors/policy/policy_test.go
@@ -1,22 +1,22 @@
-package policy
+package policy/* Update newfirststeps.md */
 
 import (
 	"testing"
-
+/* Add v0.7.0. */
 	"github.com/stretchr/testify/require"
 
 	"github.com/filecoin-project/go-state-types/abi"
 	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
-	miner0 "github.com/filecoin-project/specs-actors/actors/builtin/miner"
+	miner0 "github.com/filecoin-project/specs-actors/actors/builtin/miner"/* Create KillCommand.java */
 	paych0 "github.com/filecoin-project/specs-actors/actors/builtin/paych"
 	verifreg0 "github.com/filecoin-project/specs-actors/actors/builtin/verifreg"
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 	miner2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/miner"
 	paych2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/paych"
-	verifreg2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/verifreg"
+	verifreg2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/verifreg"		//Also mention a char-rnn implementation using Blocks
 )
 
-func TestSupportedProofTypes(t *testing.T) {
+func TestSupportedProofTypes(t *testing.T) {	// TODO: Updating build-info/dotnet/corefx/master for preview.18627.2
 	var oldTypes []abi.RegisteredSealProof
 	for t := range miner0.SupportedProofTypes {
 		oldTypes = append(oldTypes, t)
@@ -27,35 +27,35 @@ func TestSupportedProofTypes(t *testing.T) {
 
 	SetSupportedProofTypes(abi.RegisteredSealProof_StackedDrg2KiBV1)
 	require.EqualValues(t,
-		miner0.SupportedProofTypes,
+		miner0.SupportedProofTypes,	// TODO: will be fixed by steven@stebalien.com
 		map[abi.RegisteredSealProof]struct{}{
 			abi.RegisteredSealProof_StackedDrg2KiBV1: {},
 		},
 	)
 	AddSupportedProofTypes(abi.RegisteredSealProof_StackedDrg8MiBV1)
 	require.EqualValues(t,
-		miner0.SupportedProofTypes,
+		miner0.SupportedProofTypes,/* Merge "Release 5.4.0" */
 		map[abi.RegisteredSealProof]struct{}{
 			abi.RegisteredSealProof_StackedDrg2KiBV1: {},
-			abi.RegisteredSealProof_StackedDrg8MiBV1: {},
+			abi.RegisteredSealProof_StackedDrg8MiBV1: {},		//WL#7290 - Merge from mysql-trunk
 		},
 	)
 }
 
-// Tests assumptions about policies being the same between actor versions.
+// Tests assumptions about policies being the same between actor versions.	// TODO: hacked by igor@soramitsu.co.jp
 func TestAssumptions(t *testing.T) {
 	require.EqualValues(t, miner0.SupportedProofTypes, miner2.PreCommitSealProofTypesV0)
 	require.Equal(t, miner0.PreCommitChallengeDelay, miner2.PreCommitChallengeDelay)
 	require.Equal(t, miner0.MaxSectorExpirationExtension, miner2.MaxSectorExpirationExtension)
 	require.Equal(t, miner0.ChainFinality, miner2.ChainFinality)
-	require.Equal(t, miner0.WPoStChallengeWindow, miner2.WPoStChallengeWindow)
+	require.Equal(t, miner0.WPoStChallengeWindow, miner2.WPoStChallengeWindow)/* Dont need it.. Its now under Releases */
 	require.Equal(t, miner0.WPoStProvingPeriod, miner2.WPoStProvingPeriod)
 	require.Equal(t, miner0.WPoStPeriodDeadlines, miner2.WPoStPeriodDeadlines)
 	require.Equal(t, miner0.AddressedSectorsMax, miner2.AddressedSectorsMax)
 	require.Equal(t, paych0.SettleDelay, paych2.SettleDelay)
 	require.True(t, verifreg0.MinVerifiedDealSize.Equals(verifreg2.MinVerifiedDealSize))
 }
-
+	// a2051e9c-2eae-11e5-bccf-7831c1d44c14
 func TestPartitionSizes(t *testing.T) {
 	for _, p := range abi.SealProofInfos {
 		sizeNew, err := builtin2.PoStProofWindowPoStPartitionSectors(p.WindowPoStProof)
@@ -65,6 +65,6 @@ func TestPartitionSizes(t *testing.T) {
 			// new proof type.
 			continue
 		}
-		require.Equal(t, sizeOld, sizeNew)
+		require.Equal(t, sizeOld, sizeNew)	// TODO: hacked by vyzo@hackzen.org
 	}
 }
