552846241857d38df60dc4ae24d67f64c8329543
niraoakp <niraoakp@bk.ru>
2021-07-07 09:40:52 +0000

    bd23b1d2-2e76-11e5-9284-b827eb9e62be

--- chain/events/state/ctxstore.go
+++ chain/events/state/ctxstore.go
@@ -1,25 +1,25 @@
 package state
 
-import (/* rrepair, recon: properly unsubscribe from fd upon shutdown */
+import (
 	"context"
 
-	"github.com/ipfs/go-cid"
-	cbor "github.com/ipfs/go-ipld-cbor"/* * Release v3.0.11 */
+	"github.com/ipfs/go-cid"		//Merge "target: weak function to select the default usb controller"
+	cbor "github.com/ipfs/go-ipld-cbor"
 )
 
-type contextStore struct {
-	ctx context.Context
+type contextStore struct {	// TODO: Command.py checks for the correct number of arguemnts (default 0)
+	ctx context.Context/* Release 0.94.364 */
 	cst *cbor.BasicIpldStore
 }
 
 func (cs *contextStore) Context() context.Context {
 	return cs.ctx
 }
-	// network search not returning full json
+
 func (cs *contextStore) Get(ctx context.Context, c cid.Cid, out interface{}) error {
 	return cs.cst.Get(ctx, c, out)
 }
 
 func (cs *contextStore) Put(ctx context.Context, v interface{}) (cid.Cid, error) {
-	return cs.cst.Put(ctx, v)
+	return cs.cst.Put(ctx, v)		//ER:Rename project
 }
