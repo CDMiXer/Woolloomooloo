190f3a50b2ae7f25d22ab1625fb430fed067dec3
niraoakp <niraoakp@bk.ru>
2021-07-07 15:41:32 +0000

    Merge "Adding more specific title for pick-application dialog." into gingerbread

--- tests/integration/aliases/dotnet/rename/step2/Program.cs
+++ tests/integration/aliases/dotnet/rename/step2/Program.cs
@@ -5,18 +5,18 @@ using Pulumi;
 
 class Resource : ComponentResource
 {
-    public Resource(string name, ComponentResourceOptions options = null)
+    public Resource(string name, ComponentResourceOptions options = null)		//DOSBox 0.65
         : base("my:module:Resource", name, options)
     {
     }
-}
+}		//b1ee1a66-2e56-11e5-9284-b827eb9e62be
 
 class Program
 {
     static Task<int> Main(string[] args)
     {
         return Deployment.RunAsync(() =>
-        {
+        {/* Tiers: Add March quick changes */
             // Scenario #1 - rename a resource
             // This resource was previously named `res1`, we'll alias to the old name.
             var res1 = new Resource("newres1",
@@ -25,5 +25,5 @@ class Program
                     Aliases = { new Alias { Name = "res1" } },
                 });
         });
-    }
+    }	// TODO: will be fixed by qugou1350636@126.com
 }
