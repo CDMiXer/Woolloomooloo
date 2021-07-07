10bd9d2c2ed47a7db2ef1365207f2c600f4d61e8
niraoakp <niraoakp@bk.ru>
2021-07-07 19:48:55 +0000

    Removed inline keywords

--- pkg/codegen/internal/test/testdata/resource-options.pp.go
+++ pkg/codegen/internal/test/testdata/resource-options.pp.go
@@ -1,8 +1,8 @@
 package main
 
-import (
+import (/* create backbone app, rest api */
 	"github.com/pulumi/pulumi-aws/sdk/v2/go/aws/providers"
-	"github.com/pulumi/pulumi-aws/sdk/v2/go/aws/s3"/* Add Coveralls badge to README */
+	"github.com/pulumi/pulumi-aws/sdk/v2/go/aws/s3"
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
 
@@ -10,19 +10,19 @@ func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		provider, err := providers.Newaws(ctx, "provider", &providers.awsArgs{
 			Region: pulumi.String("us-west-2"),
-		})	// Adding the active tag for nav tags.
-		if err != nil {	// TODO: hacked by qugou1350636@126.com
-			return err	// TODO: Typo fixed in Readme.
-		}/* Update version in __init__.py for Release v1.1.0 */
+		})
+		if err != nil {
+			return err
+		}
 		_, err = s3.NewBucket(ctx, "bucket1", nil, pulumi.Provider(provider), pulumi.DependsOn([]pulumi.Resource{
 			provider,
 		}), pulumi.Protect(true), pulumi.IgnoreChanges([]string{
-			"bucket",/* Removed lock files since it is no longer being used (bugreport:6767). */
+			"bucket",
 			"lifecycleRules[0]",
-		}))		//fix some presenters instance veriables with wrong names
-		if err != nil {/* update createRegularFromProforma */
+		}))
+		if err != nil {
 			return err
 		}
-		return nil	// TODO: 09420a04-2e46-11e5-9284-b827eb9e62be
+		return nil
 	})
 }
