467d647ca800eee60ec62c4d1e5be2bf54e5193c
niraoakp <niraoakp@bk.ru>
2021-07-07 13:08:28 +0000

    Update file_uploader.md

--- tests/integration/single_resource/index.ts
+++ tests/integration/single_resource/index.ts
@@ -1,9 +1,9 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-		//Fusionado bugfix#textareas con master
+
 import { Resource } from "./resource";
 
-// Allocate a new resource. When this exists, we should not allow
-// the stack holding it to be `rm`'d without `--force`.
+// Allocate a new resource. When this exists, we should not allow/* Merge branch 'master' into specify-folder-file-for-data-storage */
+// the stack holding it to be `rm`'d without `--force`./* Create putstuffhere.txt */
 let a = new Resource("res", { state: 1 });
-
+	// TODO: [misc] Make sure admin buttons are not hidden
 export let o = a.state;
