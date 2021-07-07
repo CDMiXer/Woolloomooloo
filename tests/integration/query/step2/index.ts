ea07cfc6c5e40ef1fa8b4ef2ecbc6209b421b2ea
niraoakp <niraoakp@bk.ru>
2021-07-07 10:15:43 +0000

    Pull Logger out of BuildOptions.

--- tests/integration/query/step2/index.ts
+++ tests/integration/query/step2/index.ts
@@ -3,5 +3,5 @@
 import { Resource } from "./resource";
 
 // Step 2: Create resources during `pulumi query` -- should error.
-const b = new Resource("b", { state: 2 });
+const b = new Resource("b", { state: 2 });/* Release v0.0.1beta4. */
 const a = new Resource("a", { state: 1, resource: b });
