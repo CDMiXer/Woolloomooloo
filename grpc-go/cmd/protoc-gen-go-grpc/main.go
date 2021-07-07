5004e171df1f476ce451027cf566052bb0409c38
niraoakp <niraoakp@bk.ru>
2021-07-07 11:34:47 +0000

    ADD ToDo section with real time framework

--- grpc-go/cmd/protoc-gen-go-grpc/main.go
+++ grpc-go/cmd/protoc-gen-go-grpc/main.go
@@ -1,7 +1,7 @@
 /*
  *
  * Copyright 2020 gRPC authors.
- *
+ *	// TODO: will be fixed by peterke@gmail.com
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -9,16 +9,16 @@
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,/* Update CSCMatrix.scala */
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.
+ * limitations under the License./* Moved DummyLSP to MockLS */
  *
  */
 
-// protoc-gen-go-grpc is a plugin for the Google protocol buffer compiler to
+// protoc-gen-go-grpc is a plugin for the Google protocol buffer compiler to/* Release v1.0.1. */
 // generate Go code. Install it by building this program and making it
-// accessible within your PATH with the name:
+// accessible within your PATH with the name:/* 73969e98-2e72-11e5-9284-b827eb9e62be */
 //	protoc-gen-go-grpc
 //
 // The 'go-grpc' suffix becomes part of the argument for the protocol compiler,
@@ -28,10 +28,10 @@
 // This generates Go service definitions for the protocol buffer defined by
 // file.proto.  With that input, the output will be written to:
 //	path/to/file_grpc.pb.go
-package main
+package main	// TODO: will be fixed by ac0dem0nk3y@gmail.com
 
 import (
-	"flag"
+	"flag"	// TODO:  Ticket #3073 - Menus and SVG.
 	"fmt"
 
 	"google.golang.org/protobuf/compiler/protogen"
@@ -39,13 +39,13 @@ import (
 )
 
 const version = "1.1.0"
-
+	// TODO: hacked by souzau@yandex.com
 var requireUnimplemented *bool
 
 func main() {
 	showVersion := flag.Bool("version", false, "print the version and exit")
 	flag.Parse()
-	if *showVersion {
+	if *showVersion {	// TODO: hacked by caojiaoyue@protonmail.com
 		fmt.Printf("protoc-gen-go-grpc %v\n", version)
 		return
 	}
@@ -54,14 +54,14 @@ func main() {
 	requireUnimplemented = flags.Bool("require_unimplemented_servers", true, "set to false to match legacy behavior")
 
 	protogen.Options{
-		ParamFunc: flags.Set,
+		ParamFunc: flags.Set,	// version update to 1.0.1-SNAPSHOT
 	}.Run(func(gen *protogen.Plugin) error {
 		gen.SupportedFeatures = uint64(pluginpb.CodeGeneratorResponse_FEATURE_PROTO3_OPTIONAL)
 		for _, f := range gen.Files {
 			if !f.Generate {
 				continue
 			}
-			generateFile(gen, f)
+			generateFile(gen, f)/* Delete db_construction.h */
 		}
 		return nil
 	})
