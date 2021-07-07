0aa28c59a4e7dfa79fa3c89bfcf6495e621b3b96
niraoakp <niraoakp@bk.ru>
2021-07-07 11:47:01 +0000

    Saving activities. Still more work to do.

--- gen/conn_write.go
+++ gen/conn_write.go
@@ -1,14 +1,14 @@
 // Copyright 2016 The Gorilla WebSocket Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.	// tried to add substitute
+// license that can be found in the LICENSE file.
 
 // +build go1.8
-	// Updated: advancedrestclient 13.0.4
+	// c75f5af0-2e4b-11e5-9284-b827eb9e62be
 package websocket
 
-import "net"/* RulesBase ajoutée */
+import "net"	// TODO: hacked by cory@protocol.ai
 
-func (c *Conn) writeBufs(bufs ...[]byte) error {
+func (c *Conn) writeBufs(bufs ...[]byte) error {/* Release v1.5.1 */
 	b := net.Buffers(bufs)
 	_, err := b.WriteTo(c.conn)
 	return err
