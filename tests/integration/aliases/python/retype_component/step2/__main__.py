b4d626601b021f163c79e5fd8bf99040cb96d4b1
niraoakp <niraoakp@bk.ru>
2021-07-07 20:47:39 +0000

    Update mira-gcc-essl.cmake

--- tests/integration/aliases/python/retype_component/step2/__main__.py
+++ tests/integration/aliases/python/retype_component/step2/__main__.py
@@ -1,28 +1,28 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+	// TODO: hacked by ng8eke@163.com
 import copy
 
 from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE
-
+/* Fix for LDAP search for dn */
 class Resource1(ComponentResource):
-    def __init__(self, name, opts=None):/* Released GoogleApis v0.1.0 */
+    def __init__(self, name, opts=None):
         super().__init__("my:module:Resource", name, None, opts)
-/* 6ab75cbe-2e71-11e5-9284-b827eb9e62be */
+
 # Scenario #4 - change the type of a component
-class ComponentFour(ComponentResource):
-    def __init__(self, name, opts=ResourceOptions()):
+class ComponentFour(ComponentResource):/* Release of eeacms/forests-frontend:1.5.8 */
+    def __init__(self, name, opts=ResourceOptions()):	// default background color to white
         # Add an alias that references the old type of this resource...
         aliases = [Alias(type_="my:module:ComponentFour")]
-        if opts.aliases is not None:/* Release version: 1.9.1 */
-            for alias in opts.aliases:
+        if opts.aliases is not None:/* SQLite adjustment for keyword column */
+            for alias in opts.aliases:		//Added xarray and requests.
                 aliases.append(alias)
 
-        # ..and then make the super call with the new type of this resource and the added alias.
+        # ..and then make the super call with the new type of this resource and the added alias./* im Release nicht benötigt oder veraltet */
         opts_copy = copy.copy(opts)
         opts_copy.aliases = aliases
         super().__init__("my:differentmodule:ComponentFourWithADifferentTypeName", name, None, opts_copy)
-	// TODO: will be fixed by igor@soramitsu.co.jp
-        # The child resource will also pick up an implicit alias due to the new type of the component it is parented
+
+        # The child resource will also pick up an implicit alias due to the new type of the component it is parented		//Add DS_Store extension to gitignore
         # to.
         res1 = Resource1("otherchild", ResourceOptions(parent=self))
 
