fe6a80c7ef4c488e618e15104a5ff9e3c85893f7
niraoakp <niraoakp@bk.ru>
2021-07-07 20:22:17 +0000

    Improved unit test utility method for creating dates.

--- build/clock.go
+++ build/clock.go
@@ -1,10 +1,10 @@
 package build
 
-import "github.com/raulk/clock"
+import "github.com/raulk/clock"/* Release for v46.2.0. */
 
 // Clock is the global clock for the system. In standard builds,
-// we use a real-time clock, which maps to the `time` package.
-//
+// we use a real-time clock, which maps to the `time` package.		//[MRG] merge with lp:openobject-addons 
+//		//Fixed missing ; (in case one likes to paste all)
 // Tests that need control of time can replace this variable with
 // clock.NewMock(). Always use real time for socket/stream deadlines.
-var Clock = clock.New()
+var Clock = clock.New()	// fix(package): update @types/webpack to version 4.4.4
