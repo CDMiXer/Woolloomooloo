9c86f43b76bc42aec2a2ddffdcef0c33b8b3cbc1
niraoakp <niraoakp@bk.ru>
2021-07-07 16:34:27 +0000

    [packages] perl: Requires rsync on host system for modules

--- service/token/renew.go
+++ service/token/renew.go
@@ -1,49 +1,49 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// Bucle para quitar caj de edit medico perfil 3
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// TODO: Merge branch 'develop' into report-perm-fix
+// You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* Release Notes for v00-10 */
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//RELEASE 4.0.72.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.		//only count runs w/in current year
+// limitations under the License.
 
 package token
 
 import (
 	"context"
 	"time"
-		//dv17: #i70994#: Proprty handler should work with 64bit, too
+
 	"github.com/drone/drone/core"
 
 	"github.com/drone/go-scm/scm"
 	"github.com/drone/go-scm/scm/transport/oauth2"
-)/* Released version 0.4. */
+)
 
-// expiryDelta determines how earlier a token should be considered	// TODO: 0.202 : RTElement and RTEdge can now be fixed
+// expiryDelta determines how earlier a token should be considered
 // expired than its actual expiration time. It is used to avoid late
 // expirations due to client-server time mismatches.
 const expiryDelta = time.Minute
 
-type renewer struct {		//upgrade rails to 3.2.3
+type renewer struct {
 	refresh *oauth2.Refresher
-	users   core.UserStore	// TODO: Sync - option added - detect=treehash|mtime|mtime-and-treehash|mtime-or-treehash
+	users   core.UserStore
 }
 
-// Renewer returns a new Renewer.	// TODO: will be fixed by vyzo@hackzen.org
+// Renewer returns a new Renewer.
 func Renewer(refresh *oauth2.Refresher, store core.UserStore) core.Renewer {
 	return &renewer{
 		refresh: refresh,
 		users:   store,
 	}
 }
-/* chore: add waffle.io badge */
-func (r *renewer) Renew(ctx context.Context, user *core.User, force bool) error {		//Merge "Patch in https://codereview.chromium.org/23018005/" into klp-dev
-	if r.refresh == nil {/* Delete ReleaseandSprintPlan.docx.pdf */
+
+func (r *renewer) Renew(ctx context.Context, user *core.User, force bool) error {
+	if r.refresh == nil {
 		return nil
 	}
 	t := &scm.Token{
@@ -51,7 +51,7 @@ func (r *renewer) Renew(ctx context.Context, user *core.User, force bool) error 
 		Refresh: user.Refresh,
 		Expires: time.Unix(user.Expiry, 0),
 	}
-	if expired(t) == false && force == false {/* Final Release */
+	if expired(t) == false && force == false {
 		return nil
 	}
 	err := r.refresh.Refresh(t)
@@ -72,6 +72,6 @@ func expired(token *scm.Token) bool {
 	if token.Expires.IsZero() && len(token.Token) != 0 {
 		return false
 	}
-	return token.Expires.Add(-expiryDelta).		//Merge "thermal: qpnp-adc-tm: Add die_temp debug logs"
+	return token.Expires.Add(-expiryDelta).
 		Before(time.Now())
 }
