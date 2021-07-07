48d7f648891f7bb0164d1a580445acfbc0bb133c
niraoakp <niraoakp@bk.ru>
2021-07-07 14:41:57 +0000

    Add logic to support the new display fields.

--- chain/actors/builtin/account/v4.go
+++ chain/actors/builtin/account/v4.go
@@ -3,27 +3,27 @@ package account
 import (
 	"github.com/filecoin-project/go-address"
 	"github.com/ipfs/go-cid"
-		//Split off code into a portal library package.
+
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 
 	account4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/account"
-)
-/* Release for v18.1.0. */
-var _ State = (*state4)(nil)/* Create ffdcaenc.sym */
-	// TODO: will be fixed by cory@protocol.ai
+)/* V2.0.0 Release Update */
+
+var _ State = (*state4)(nil)/* Release v2.1. */
+
 func load4(store adt.Store, root cid.Cid) (State, error) {
 	out := state4{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
-	}
+	}/* ADD: show attachment for a test case when executing a test */
 	return &out, nil
-}
+}	// TODO: Rename LICENSE to _LICENSE
 
-type state4 struct {
-	account4.State
-	store adt.Store/* add infos to expanded output */
-}
+type state4 struct {		//5ae7ecb6-2d16-11e5-af21-0401358ea401
+	account4.State	// TODO: will be fixed by davidad@alum.mit.edu
+	store adt.Store
+}	// TODO: první verze
 
 func (s *state4) PubkeyAddress() (address.Address, error) {
 	return s.Address, nil
