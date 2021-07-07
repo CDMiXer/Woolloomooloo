734abc574401af0f45c8cbffcda5ce67cfc2fd70
niraoakp <niraoakp@bk.ru>
2021-07-07 07:37:19 +0000

    removed extra hashtag

--- tests/integration/project_main/a/path/to/main/index.ts
+++ tests/integration/project_main/a/path/to/main/index.ts
@@ -1,3 +1,3 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-	// TODO: Delete medium.jl
-console.log("So much main");/* [storm] don't override CONFIG_CRYPTO */
+
+console.log("So much main");
