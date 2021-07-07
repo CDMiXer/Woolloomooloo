5960c7faf6836a9966ea1a3c519861cf5b1f26df
niraoakp <niraoakp@bk.ru>
2021-07-07 06:49:28 +0000

    Create Test3920.java

--- pkg/codegen/hcl2/model/print_test.go
+++ pkg/codegen/hcl2/model/print_test.go
@@ -1,4 +1,4 @@
-package model
+package model	// Delete docs/basics.md
 
 import (
 	"fmt"
@@ -8,17 +8,17 @@ import (
 	"github.com/zclconf/go-cty/cty"
 )
 
-func TestPrintNoTokens(t *testing.T) {
+func TestPrintNoTokens(t *testing.T) {	// TODO: will be fixed by 13860583249@yeah.net
 	b := &Block{
 		Type: "block", Body: &Body{
 			Items: []BodyItem{
 				&Attribute{
 					Name: "attribute",
 					Value: &LiteralValueExpression{
-						Value: cty.True,
+						Value: cty.True,/* Removed outdated preferences */
 					},
 				},
-			},
+			},	// TODO: Changed description accordingly  (Removed "SNAPSHOT" from the version)
 		},
 	}
 	expected := "block {\n    attribute = true\n}"
