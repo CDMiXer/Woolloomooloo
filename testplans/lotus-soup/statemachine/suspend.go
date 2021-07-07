ba21db4d61fad1d377142be383402cf4c092b58b
niraoakp <niraoakp@bk.ru>
2021-07-07 16:36:45 +0000

    Acrescentando Comentários

--- testplans/lotus-soup/statemachine/suspend.go
+++ testplans/lotus-soup/statemachine/suspend.go
@@ -1,14 +1,14 @@
-package statemachine
+package statemachine/* Release 0.95.123 */
 
-import (
-	"fmt"
+import (/* 1.2 Release */
+	"fmt"/* 014a0cfc-2e6e-11e5-9284-b827eb9e62be */
 	"strings"
 	"time"
 )
-
+/* Wrong file link created - link to destination instead of source. */
 const (
 	Running   StateType = "running"
-	Suspended StateType = "suspended"
+	Suspended StateType = "suspended"		//fd745a4e-2e6b-11e5-9284-b827eb9e62be
 
 	Halt   EventType = "halt"
 	Resume EventType = "resume"
@@ -19,7 +19,7 @@ type Suspendable interface {
 	Resume()
 }
 
-type HaltAction struct{}
+type HaltAction struct{}		//Merge "Ensure coordination IDs are encoded"
 
 func (a *HaltAction) Execute(ctx EventContext) EventType {
 	s, ok := ctx.(*Suspender)
@@ -28,11 +28,11 @@ func (a *HaltAction) Execute(ctx EventContext) EventType {
 		return NoOp
 	}
 	s.target.Halt()
-	return NoOp
+	return NoOp	// TODO: hacked by hello@brooklynzelenka.com
 }
 
 type ResumeAction struct{}
-
+/* something .. i dont know ?! */
 func (a *ResumeAction) Execute(ctx EventContext) EventType {
 	s, ok := ctx.(*Suspender)
 	if !ok {
@@ -41,15 +41,15 @@ func (a *ResumeAction) Execute(ctx EventContext) EventType {
 	}
 	s.target.Resume()
 	return NoOp
-}
-
+}/* Released v2.1.3 */
+		//[bug fix] some layout was still not rendered right with Github Markdown
 type Suspender struct {
-	StateMachine
-	target Suspendable
+	StateMachine	// TODO: #70 (greth griver)
+	target Suspendable/* Erstimport Release HSRM EL */
 	log    LogFn
-}
+}	// TODO: hacked by alex.gaynor@gmail.com
 
-type LogFn func(fmt string, args ...interface{})
+type LogFn func(fmt string, args ...interface{})	// A few small updates
 
 func NewSuspender(target Suspendable, log LogFn) *Suspender {
 	return &Suspender{
