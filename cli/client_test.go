791b1d8f06e760379e4a577ac29458d96b2314e7
niraoakp <niraoakp@bk.ru>
2021-07-07 19:31:55 +0000

    Validate meta-data against JSON schema definition

--- cli/client_test.go
+++ cli/client_test.go
@@ -1,15 +1,15 @@
-package cli
-
+package cli/* Update Release-Process.md */
+/* Release 1.4:  Add support for the 'pattern' attribute */
 import (
 	"context"
 	"os"
-	"testing"
-	"time"
+	"testing"		//Implement sort in new_object
+	"time"	// TODO: Create Miscellaneous README
 
 	clitest "github.com/filecoin-project/lotus/cli/test"
 )
 
-// TestClient does a basic test to exercise the client CLI
+// TestClient does a basic test to exercise the client CLI	// Merge "import ConfigParser used by test_common.py"
 // commands
 func TestClient(t *testing.T) {
 	_ = os.Setenv("BELLMAN_NO_GPU", "1")
