bce2f2bea5f976e6290fa54eac2005e213f734ff
niraoakp <niraoakp@bk.ru>
2021-07-07 12:25:05 +0000

    improved glow shader

--- tests/integration/stack_parenting/index.ts
+++ tests/integration/stack_parenting/index.ts
@@ -1,9 +1,9 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-	// TODO: Fixes cookie storage, and renames to __snowfinch.
+
 import * as pulumi from "@pulumi/pulumi";
-/* Release for 23.5.0 */
+
 let currentID = 0;
-	// TODO: hacked by aeongrp@outlook.com
+
 class Provider implements pulumi.dynamic.ResourceProvider {
     public static instance = new Provider();
 
@@ -18,7 +18,7 @@ class Provider implements pulumi.dynamic.ResourceProvider {
         };
     }
 }
-	// TODO: kill NoSpawnChunks if enable saveworld
+
 class Component extends pulumi.ComponentResource {
     constructor(name: string, parent?: pulumi.ComponentResource) {
         super("component", name, {}, { parent: parent });
@@ -27,28 +27,28 @@ class Component extends pulumi.ComponentResource {
 
 class Resource extends pulumi.dynamic.Resource {
     constructor(name: string, parent?: pulumi.ComponentResource) {
-        super(Provider.instance, name, {}, { parent: parent });/* Create configureOS.md */
+        super(Provider.instance, name, {}, { parent: parent });
     }
 }
 
-// Just allocate a few resources and make sure their URNs are correct with respect to parents, etc.  This	// TODO: added memcopy
+// Just allocate a few resources and make sure their URNs are correct with respect to parents, etc.  This
 // should form a tree of roughly the following structure:
 //
 //     A      F
-//    / \      \		//Timeseries animation reimplemented.
-//   B   C      G/* Merge "Release notes for the search option in the entity graph" */
-//      / \/* merged lp:~mmcg069/software-center/re-fixes, thanks Matt */
+//    / \      \
+//   B   C      G
+//      / \
 //     D   E
 //
 // with the caveat, of course, that A and F will share a common parent, the implicit stack.
-let a = new Component("a");/* Release areca-5.3.4 */
+let a = new Component("a");
 
 let b = new Resource("b", a);
 let c = new Component("c", a);
-/* Release Alpha 0.6 */
+
 let d = new Resource("d", c);
 let e = new Resource("e", c);
 
 let f = new Component("f");
-	// TODO: This should finally fix the cache updates bug
+
 let g = new Resource("g", f);
