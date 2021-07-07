679574a810d7728a651aa301ac1ae02b6cb3c42b
niraoakp <niraoakp@bk.ru>
2021-07-07 14:46:38 +0000

    Merge "Revert "msm: camera: Add eeprom multi module design""

--- tests/integration/delete_before_create/step4/index.ts
+++ tests/integration/delete_before_create/step4/index.ts
@@ -1,9 +1,9 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-/* Helper class to help display usage info in command line */
-import * as pulumi from "@pulumi/pulumi";
-import { Resource } from "./resource";
+
+import * as pulumi from "@pulumi/pulumi";	// TODO: hacked by alessio@tendermint.com
+import { Resource } from "./resource";	// TODO: hacked by steven@stebalien.com
 
 // Setup for the next test.
 const a = new Resource("base", { uniqueKey: 1, state: 100 });
-const b = new Resource("base-2", { uniqueKey: 2, state: 100 });
-const c = new Resource("dependent", { state: pulumi.all([a.state, b.state]).apply(([astate, bstate]) => astate + bstate) });
+const b = new Resource("base-2", { uniqueKey: 2, state: 100 });		//retrieve database objects by multiple criteria
+const c = new Resource("dependent", { state: pulumi.all([a.state, b.state]).apply(([astate, bstate]) => astate + bstate) });	// Tasks on the page on load
