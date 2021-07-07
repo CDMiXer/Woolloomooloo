4f1f3d44dbbcf7de13e6ca84c46180428c089827
niraoakp <niraoakp@bk.ru>
2021-07-07 01:25:42 +0000

    update2 README file

--- operator/runner/before_test.go
+++ operator/runner/before_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: will be fixed by hugomrdias@gmail.com
-// Use of this source code is governed by the Drone Non-Commercial License	// TODO: Properly working Rails plugin.
-// that can be found in the LICENSE file.		//Merge "gtest needs -lpthread."
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.
 
 package runner
