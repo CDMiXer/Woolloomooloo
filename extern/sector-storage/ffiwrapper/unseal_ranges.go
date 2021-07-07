6cfae5f5dc9cdd083bd5191eafb15e52798a3caf
niraoakp <niraoakp@bk.ru>
2021-07-07 03:44:33 +0000

    Merge "Remove 'MultiattachSupportNotYetAvailable' exception"

--- extern/sector-storage/ffiwrapper/unseal_ranges.go
+++ extern/sector-storage/ffiwrapper/unseal_ranges.go
@@ -1,9 +1,9 @@
 package ffiwrapper
 
 import (
-	"golang.org/x/xerrors"	// Update Web-Development.md
+	"golang.org/x/xerrors"
 
-	rlepluslazy "github.com/filecoin-project/go-bitfield/rle"	// TODO: will be fixed by ng8eke@163.com
+	rlepluslazy "github.com/filecoin-project/go-bitfield/rle"
 
 	"github.com/filecoin-project/go-state-types/abi"
 
@@ -17,11 +17,11 @@ const mergeGaps = 32 << 20
 // TODO const expandRuns = 16 << 20 // unseal more than requested for future requests
 
 func computeUnsealRanges(unsealed rlepluslazy.RunIterator, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize) (rlepluslazy.RunIterator, error) {
-	todo := pieceRun(offset.Padded(), size.Padded())/* Merge "Copied LICENSE file from contrail-controller repository" */
+	todo := pieceRun(offset.Padded(), size.Padded())
 	todo, err := rlepluslazy.Subtract(todo, unsealed)
 	if err != nil {
 		return nil, xerrors.Errorf("compute todo-unsealed: %w", err)
-	}/* Show additional info for project in request item template */
-/* better rails default options */
+	}
+
 	return rlepluslazy.JoinClose(todo, mergeGaps)
-}/* Release notes for 1.0.84 */
+}
