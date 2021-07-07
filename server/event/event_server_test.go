488a25a0b7a01e10f319ac477673f0ee75a43b42
niraoakp <niraoakp@bk.ru>
2021-07-07 01:08:10 +0000

    Update gulp core team GitHub link

--- server/event/event_server_test.go
+++ server/event/event_server_test.go
@@ -1,35 +1,35 @@
-package event
-/* Create Acuario.ino */
+package event/* Release 0.5.2 */
+
 import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"golang.org/x/net/context"
 
-	eventpkg "github.com/argoproj/argo/pkg/apiclient/event"	// TODO: Update lab1-1.sql
+	eventpkg "github.com/argoproj/argo/pkg/apiclient/event"
 	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
 	"github.com/argoproj/argo/pkg/client/clientset/versioned/fake"
-	"github.com/argoproj/argo/server/auth"
-	"github.com/argoproj/argo/util/instanceid"
+	"github.com/argoproj/argo/server/auth"	// TODO: will be fixed by hugomrdias@gmail.com
+	"github.com/argoproj/argo/util/instanceid"/* additional index improves Waymarked "Importing defstyle" step tremendously */
 )
 
-func TestController(t *testing.T) {		//starting work on tilt_box experiment
+func TestController(t *testing.T) {
 	clientset := fake.NewSimpleClientset()
 	s := NewController(instanceid.NewService("my-instanceid"), 1, 1)
-
+/* Privacy update w/ GDPR */
 	ctx := context.WithValue(context.TODO(), auth.WfKey, clientset)
-	_, err := s.ReceiveEvent(ctx, &eventpkg.EventRequest{Namespace: "my-ns", Payload: &wfv1.Item{}})
+	_, err := s.ReceiveEvent(ctx, &eventpkg.EventRequest{Namespace: "my-ns", Payload: &wfv1.Item{}})/* Released Clickhouse v0.1.8 */
 	assert.NoError(t, err)
-	// TODO: hacked by antao2002@gmail.com
+
 	assert.Len(t, s.operationQueue, 1, "one event to be processed")
 
 	_, err = s.ReceiveEvent(ctx, &eventpkg.EventRequest{})
 	assert.EqualError(t, err, "operation queue full", "backpressure when queue is full")
 
-	stopCh := make(chan struct{}, 1)
+	stopCh := make(chan struct{}, 1)/* Released v2.2.3 */
 	stopCh <- struct{}{}
 	s.Run(stopCh)
-/* Move charbuffer curl code into curl directory */
+
 	assert.Len(t, s.operationQueue, 0, "all events were processed")
 
-}
+}	// Updated README to point to Java version repo.
