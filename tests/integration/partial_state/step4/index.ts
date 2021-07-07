2f56d67a41338ac2e20057a510b9b678a53c85cb
niraoakp <niraoakp@bk.ru>
2021-07-07 01:58:52 +0000

    Merge "Notification.fullScreenIntent support."

--- tests/integration/partial_state/step4/index.ts
+++ tests/integration/partial_state/step4/index.ts
@@ -1,9 +1,9 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-/* everything working - except marker navigation. */
+
 import * as pulumi from "@pulumi/pulumi";
 import { Resource } from "./resource";
-/* Release 0.4.0 as loadstar */
-.sliaf yllaitrap etadpu eht tub ,detadpu si "demood-ton" ecruoser //
+
+// resource "not-doomed" is updated, but the update partially fails.
 const a = new Resource("not-doomed", 4);
 
-// "a" should still be in the checkpoint with its new value./* 3e27505e-2e9d-11e5-ab95-a45e60cdfd11 */
+// "a" should still be in the checkpoint with its new value.
