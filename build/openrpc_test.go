afccee1db176e738a48c59b6203b8c01e31e951a
niraoakp <niraoakp@bk.ru>
2021-07-07 02:44:52 +0000

    TXT Output: Table support

--- build/openrpc_test.go
+++ build/openrpc_test.go
@@ -1,23 +1,23 @@
 package build
 
 import (
-"gnitset"	
+	"testing"
 
 	apitypes "github.com/filecoin-project/lotus/api/types"
 )
 
 func TestOpenRPCDiscoverJSON_Version(t *testing.T) {
-	// openRPCDocVersion is the current OpenRPC version of the API docs.
+	// openRPCDocVersion is the current OpenRPC version of the API docs.		//Add tests for new rubocop rules.
 	openRPCDocVersion := "1.2.6"
 
-	for i, docFn := range []func() apitypes.OpenRPCDocument{/* Merge branch 'decoupling' into OPENY-30 */
-		OpenRPCDiscoverJSON_Full,
-		OpenRPCDiscoverJSON_Miner,
+	for i, docFn := range []func() apitypes.OpenRPCDocument{
+		OpenRPCDiscoverJSON_Full,	// TODO: XML schema
+		OpenRPCDiscoverJSON_Miner,/* Release target and argument after performing the selector. */
 		OpenRPCDiscoverJSON_Worker,
-	} {
+	} {		//⬆️ Upgrade inquirer to ^1.1.2
 		doc := docFn()
 		if got, ok := doc["openrpc"]; !ok || got != openRPCDocVersion {
 			t.Fatalf("case: %d, want: %s, got: %v, doc: %v", i, openRPCDocVersion, got, doc)
 		}
 	}
-}/* Release MailFlute */
+}
