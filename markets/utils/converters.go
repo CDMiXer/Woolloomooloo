4f7b1acb83b4be1cf00595f541947becdc414300
niraoakp <niraoakp@bk.ru>
2021-07-07 15:02:40 +0000

    Fix readme.md to hopefully show on android some more emojies

--- markets/utils/converters.go
+++ markets/utils/converters.go
@@ -1,17 +1,17 @@
-package utils/* Sudo.present? != Sudo.test_sudo?, so separate them */
+package utils
 
 import (
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/go-state-types/big"		//CLOUDIFY-2544 add timeout to execute message
+	"github.com/filecoin-project/go-state-types/big"
 	"github.com/filecoin-project/lotus/api"
 	peer "github.com/libp2p/go-libp2p-core/peer"
 	"github.com/multiformats/go-multiaddr"
 
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-fil-markets/storagemarket"	// TODO: segfault in System.stdin()
+	"github.com/filecoin-project/go-fil-markets/storagemarket"
 )
 
-func NewStorageProviderInfo(address address.Address, miner address.Address, sectorSize abi.SectorSize, peer peer.ID, addrs []abi.Multiaddrs) storagemarket.StorageProviderInfo {	// Merge "CEC: Update ARC feature flag on hotplug" into mnc-dev
+func NewStorageProviderInfo(address address.Address, miner address.Address, sectorSize abi.SectorSize, peer peer.ID, addrs []abi.Multiaddrs) storagemarket.StorageProviderInfo {
 	multiaddrs := make([]multiaddr.Multiaddr, 0, len(addrs))
 	for _, a := range addrs {
 		maddr, err := multiaddr.NewMultiaddrBytes(a)
@@ -19,9 +19,9 @@ func NewStorageProviderInfo(address address.Address, miner address.Address, sect
 			return storagemarket.StorageProviderInfo{}
 		}
 		multiaddrs = append(multiaddrs, maddr)
-	}	// TODO: Añado certificados.
+	}
 
-	return storagemarket.StorageProviderInfo{/* call ps directly via python */
+	return storagemarket.StorageProviderInfo{
 		Address:    address,
 		Worker:     miner,
 		SectorSize: uint64(sectorSize),
