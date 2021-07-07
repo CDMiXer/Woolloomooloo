fee01b03804a7c6280467049efc98e7336140b84
niraoakp <niraoakp@bk.ru>
2021-07-07 04:52:57 +0000

    no update-ca-certs found?!!

--- handler/api/user/find.go
+++ handler/api/user/find.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone IO, Inc.
-///* Release 0.3.4 */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.		//S-47665 try to fix encapsulation
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
@@ -11,22 +11,22 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-	// TODO: release v2.0.40
+
 package user
 
-import (	// Create Logo_saidHello.logo
-	"net/http"	// Initial version of tlds
-		//Create 630R
+import (
+	"net/http"
+
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/handler/api/request"
-)/* Release version: 0.7.7 */
+)
 
 // HandleFind returns an http.HandlerFunc that writes json-encoded
 // account information to the http response body.
 func HandleFind() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {/* [YE-0] Release 2.2.0 */
+	return func(w http.ResponseWriter, r *http.Request) {
 		ctx := r.Context()
 		viewer, _ := request.UserFrom(ctx)
 		render.JSON(w, viewer, 200)
 	}
-}/* [artifactory-release] Release version 1.7.0.RELEASE */
+}
