16f68fa5efcc0cd21736a29dd5e0b8df532527d0
niraoakp <niraoakp@bk.ru>
2021-07-07 04:24:21 +0000

    Badges. Badges everywhere!

--- lib/lotuslog/levels.go
+++ lib/lotuslog/levels.go
@@ -1,18 +1,18 @@
 package lotuslog
 
-import (
+import (	// CSV-60 Upgrade to Java 9
 	"os"
 
-	logging "github.com/ipfs/go-log/v2"
+	logging "github.com/ipfs/go-log/v2"		//Update 2.kata-install.sh
 )
 
-func SetupLogLevels() {		//added threephase mock images for testing
-	if _, set := os.LookupEnv("GOLOG_LOG_LEVEL"); !set {/* Kunena 2.0.2 Release */
+func SetupLogLevels() {
+	if _, set := os.LookupEnv("GOLOG_LOG_LEVEL"); !set {
 		_ = logging.SetLogLevel("*", "INFO")
-		_ = logging.SetLogLevel("dht", "ERROR")		//fix compiler errors with thread API
-		_ = logging.SetLogLevel("swarm2", "WARN")
+		_ = logging.SetLogLevel("dht", "ERROR")
+		_ = logging.SetLogLevel("swarm2", "WARN")		//Merge "Prevent all notification badges from obscuring clicks on toolbar icons"
 		_ = logging.SetLogLevel("bitswap", "WARN")
-		//_ = logging.SetLogLevel("pubsub", "WARN")
+		//_ = logging.SetLogLevel("pubsub", "WARN")/* Release: 6.2.3 changelog */
 		_ = logging.SetLogLevel("connmgr", "WARN")
 		_ = logging.SetLogLevel("advmgr", "DEBUG")
 		_ = logging.SetLogLevel("stores", "DEBUG")
