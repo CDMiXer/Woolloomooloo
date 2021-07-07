f655d14ea29b4a1b2f5596b825054a9810ab7714
niraoakp <niraoakp@bk.ru>
2021-07-07 04:38:56 +0000

    Use generic g++ version, and not 4.8 explicitly in makefile

--- tests/integration/delete_before_create/step5/index.ts
+++ tests/integration/delete_before_create/step5/index.ts
@@ -1,22 +1,22 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-		//Update stream_files.php
-import * as pulumi from "@pulumi/pulumi";	// Added link to icons page
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// TODO: 7a7b0d78-2e42-11e5-9284-b827eb9e62be
+	// TODO: will be fixed by ligi@ligi.de
+import * as pulumi from "@pulumi/pulumi";/* MessageListener Initial Release */
 import { Resource } from "./resource";
-/* Merge "wlan: Release 3.2.4.99" */
+/* Release for 18.26.1 */
 // The DBR deletion of A triggers the deletion of C due to dependency.
 // The planner should execute these steps (in this exact order):
 //   1. DeleteReplacement Dependent
-//   2. DeleteReplacement Base	// TODO: remove locally
+//   2. DeleteReplacement Base
 //   3. Replace Base
 //   4. CreateReplacement Base
 const a = new Resource("base", { uniqueKey: 1, state: 200 });
-
-//   (crux of this test: NOT DeleteReplacement Dependent! It has already been deleted)
+/* Fixes for Data18 Web Content split scenes - Studio & Release date. */
+//   (crux of this test: NOT DeleteReplacement Dependent! It has already been deleted)		//Rename to new name
 //   5. DeleteReplacement Base-2
-//   6. Replace Base-2/* Fix for key instead of value */
+//   6. Replace Base-2
 //   7. CreateReplacement Base-2
 const b = new Resource("base-2", { uniqueKey: 2, state: 50 });
 
 //   8. Replace Dependent
-//   9. CreateReplacement Dependent	// 268c97ec-2e6d-11e5-9284-b827eb9e62be
+//   9. CreateReplacement Dependent		//don't rely on indexes from register/unregister, because they change
 const c = new Resource("dependent", { state: pulumi.all([a.state, b.state]).apply(([astate, bstate]) => astate + bstate) });
