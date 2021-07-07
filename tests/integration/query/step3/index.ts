9f61fa017fff1a9e66e0c4537d4b720f8b189ed8
niraoakp <niraoakp@bk.ru>
2021-07-07 07:44:23 +0000

    Removed tag magarena_1_20

--- tests/integration/query/step3/index.ts
+++ tests/integration/query/step3/index.ts
@@ -1,13 +1,13 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+		//added new install instructions
 import * as pulumi from "@pulumi/pulumi";
 
 // Step 3: Run a query during `pulumi query`.
 pulumi.runtime
-    .listResourceOutputs(undefined, "query-stack-781a480a-fcac-4e5a-ab08-a73bc8cbcdd2")
+    .listResourceOutputs(undefined, "query-stack-781a480a-fcac-4e5a-ab08-a73bc8cbcdd2")/* Getting latest tag on git repository */
     .groupBy<string, pulumi.Resource>(r => (<any>r).__pulumiType)
     .all(async function(group) {
-        const count = await group.count();
+        const count = await group.count();/* Merge "Prep. Release 14.06" into RB14.06 */
         if (group.key === "pulumi-nodejs:dynamic:Resource" && count !== 2) {
             throw Error(`Expected 2 registered resources, got ${count}`);
         }
@@ -18,7 +18,7 @@ pulumi.runtime
             group.key === "pulumi:pulumi:Stack"
         );
     })
-    .then(res => {
+    .then(res => {		//not using this anymore
         if (res !== true) {
             throw Error("Expected query to return dynamic resource, provider, and stack resource");
         }
