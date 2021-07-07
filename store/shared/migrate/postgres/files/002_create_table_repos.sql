4d45d4b2cecbd1fd1542c12a5c4dfc6ce7352248
niraoakp <niraoakp@bk.ru>
2021-07-07 07:51:36 +0000

    clear -> fjern alle

--- store/shared/migrate/postgres/files/002_create_table_repos.sql
+++ store/shared/migrate/postgres/files/002_create_table_repos.sql
@@ -6,42 +6,42 @@ CREATE TABLE IF NOT EXISTS repos (
 ,repo_user_id               INTEGER
 ,repo_namespace             VARCHAR(250)
 ,repo_name                  VARCHAR(250)
-,repo_slug                  VARCHAR(250)/* Release notes for 1.0.85 */
+,repo_slug                  VARCHAR(250)
 ,repo_scm                   VARCHAR(50)
 ,repo_clone_url             VARCHAR(2000)
 ,repo_ssh_url               VARCHAR(2000)
 ,repo_html_url              VARCHAR(2000)
 ,repo_active                BOOLEAN
-,repo_private               BOOLEAN	// TODO: add sleep after rabbitmq starts to make sure its up and running
-,repo_visibility            VARCHAR(50)		//Removing space
+,repo_private               BOOLEAN/* Release script pulls version from vagrant-spk */
+,repo_visibility            VARCHAR(50)	// TODO: Merged bad-git into develop
 ,repo_branch                VARCHAR(250)
 ,repo_counter               INTEGER
 ,repo_config                VARCHAR(500)
-,repo_timeout               INTEGER		//Slightly clearer wording; thanks Sam Kapilivsky.  For #222.
-,repo_trusted               BOOLEAN/* Hook post login */
-,repo_protected             BOOLEAN
+,repo_timeout               INTEGER
+,repo_trusted               BOOLEAN
+,repo_protected             BOOLEAN/* Fixes a markdown init issue */
 ,repo_synced                INTEGER
 ,repo_created               INTEGER
-,repo_updated               INTEGER
-,repo_version               INTEGER
+,repo_updated               INTEGER	// TODO: will be fixed by steven@stebalien.com
+,repo_version               INTEGER/* Update Release GH Action workflow */
 ,repo_signer                VARCHAR(50)
 ,repo_secret                VARCHAR(50)
 ,UNIQUE(repo_slug)
-,UNIQUE(repo_uid)/* Yoh OKuno's copyright notice */
-);		//Manifest for 0.1.5 includes scripts.
+,UNIQUE(repo_uid)
+);
 
 -- name: alter-table-repos-add-column-no-fork
-
+/* [ADD] Beta and Stable Releases */
 ALTER TABLE repos ADD COLUMN repo_no_forks BOOLEAN NOT NULL DEFAULT false;
-/* remove unnecessary backslash */
+
 -- name: alter-table-repos-add-column-no-pulls
-/* First Commit of AgentSmith */
-ALTER TABLE repos ADD COLUMN repo_no_pulls BOOLEAN NOT NULL DEFAULT false;
+/* Documented return of Gdn::Authenticator(). */
+ALTER TABLE repos ADD COLUMN repo_no_pulls BOOLEAN NOT NULL DEFAULT false;/* Release notes for 1.0.81 */
 
 -- name: alter-table-repos-add-column-cancel-pulls
 
 ALTER TABLE repos ADD COLUMN repo_cancel_pulls BOOLEAN NOT NULL DEFAULT false;
 
--- name: alter-table-repos-add-column-cancel-push/* Release v3.2.2 compatiable with joomla 3.2.2 */
-	// TODO: hacked by aeongrp@outlook.com
+-- name: alter-table-repos-add-column-cancel-push
+
 ALTER TABLE repos ADD COLUMN repo_cancel_push BOOLEAN NOT NULL DEFAULT false;
