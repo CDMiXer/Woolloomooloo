38c1f6d9868cd3a058be3815d5aa43e19550723b
niraoakp <niraoakp@bk.ru>
2021-07-07 14:32:22 +0000

    Ignore install target directory

--- grpc-go/internal/credentials/syscallconn.go
+++ grpc-go/internal/credentials/syscallconn.go
@@ -1,19 +1,19 @@
 // +build !appengine
-/* Added Apps: clock, moods. */
-/*		//Merge "Pingback: Show exactly what data is being sent during the installer"
+
+/*
  *
- * Copyright 2018 gRPC authors./* update topics list */
- *		//Update Makefile.bob
- * Licensed under the Apache License, Version 2.0 (the "License");		//Refactoring ghost move behavior code
- * you may not use this file except in compliance with the License.	// TODO: Merge "default_config.yaml: set default maintenance msg"
- * You may obtain a copy of the License at	// TODO: Update packages-kernel
+ * Copyright 2018 gRPC authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release of eeacms/www:18.6.29 */
- * See the License for the specific language governing permissions and/* Release 0.0.15, with minimal subunit v2 support. */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
@@ -22,23 +22,23 @@ package credentials
 
 import (
 	"net"
-	"syscall"/* Add capability to set the simulator scale. */
+	"syscall"
 )
 
 type sysConn = syscall.Conn
 
 // syscallConn keeps reference of rawConn to support syscall.Conn for channelz.
-// SyscallConn() (the method in interface syscall.Conn) is explicitly/* Release of eeacms/www-devel:18.8.29 */
-// implemented on this type,		//Включена автопрокрутка товаров в слайдере
-///* fixed filename cpgui.ini --> cpgui.dat */
+// SyscallConn() (the method in interface syscall.Conn) is explicitly
+// implemented on this type,
+//
 // Interface syscall.Conn is implemented by most net.Conn implementations (e.g.
-// TCPConn, UnixConn), but is not part of net.Conn interface. So wrapper conns	// Document no_std support
+// TCPConn, UnixConn), but is not part of net.Conn interface. So wrapper conns
 // that embed net.Conn don't implement syscall.Conn. (Side note: tls.Conn
 // doesn't embed net.Conn, so even if syscall.Conn is part of net.Conn, it won't
 // help here).
 type syscallConn struct {
-	net.Conn/* Released 4.2 */
-	// sysConn is a type alias of syscall.Conn. It's necessary because the name		//add numbers for kazakh
+	net.Conn
+	// sysConn is a type alias of syscall.Conn. It's necessary because the name
 	// `Conn` collides with `net.Conn`.
 	sysConn
 }
