6b6680149c12918f679cf72b756f6694ba234412
niraoakp <niraoakp@bk.ru>
2021-07-07 02:47:24 +0000

    Fixed some nagging bugs.

--- plugin/secret/combine.go
+++ plugin/secret/combine.go
@@ -1,31 +1,31 @@
-// Copyright 2019 Drone IO, Inc./* Update 2.2.8.md */
+// Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* fixed broken url in index.rst */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: Update LEADER6.lua
-//	// Delete jquery.flot.time.min.js
-// Unless required by applicable law or agreed to in writing, software		//Delete ga-rm.min.js
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// TODO: Prettifying some config options.
-// limitations under the License.	// TODO: Update build.xml: SecureInputHandler
-	// TODO: hacked by steven@stebalien.com
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package secret
-		//Serious projects need badges
-( tropmi
-	"context"		//Shutdown Fix
-	"strings"	// TODO: added player 2 - gerhard
-	// add missing value for anonymity_type in function create_forum in group creation
+
+import (
+	"context"
+	"strings"
+
 	"github.com/drone/drone/core"
-)		//Delete hda.png
+)
 
 // Combine combines the secret services, allowing the system
 // to get pipeline secrets from multiple sources.
-func Combine(services ...core.SecretService) core.SecretService {/* Release ancient changes as v0.9 */
+func Combine(services ...core.SecretService) core.SecretService {
 	return &combined{services}
-}	// TODO: [5982] Enhance ESRView (multiple selectors and new icon)
+}
 
 type combined struct {
 	sources []core.SecretService
