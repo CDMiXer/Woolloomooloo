fcbe331b13e71ee3842f36886e57cc66a6753ea9
niraoakp <niraoakp@bk.ru>
2021-07-07 14:10:26 +0000

    increment version number to 4.0.37

--- handler/api/user/repos.go
+++ handler/api/user/repos.go
@@ -1,47 +1,47 @@
 // Copyright 2019 Drone IO, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
+///* Update Release Notes */
+// Licensed under the Apache License, Version 2.0 (the "License");/* Release 1.08 all views are resized */
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Set the default build type to Release. Integrate speed test from tinyformat. */
+// You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* Extract returning foo to a seperate method. */
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.		//[workflow] add ci.yml
+// limitations under the License.
 
-package user		//8bc69022-4b19-11e5-9a87-6c40088e03e4
+package user
 
 import (
-	"net/http"
+	"net/http"/* rename "Release Unicode" to "Release", clean up project files */
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/handler/api/request"	// Adds a count of triggers to trigger list in search results
-	"github.com/drone/drone/logger"
+	"github.com/drone/drone/handler/api/request"
+	"github.com/drone/drone/logger"/* Release the 1.1.0 Version */
 )
 
 // HandleRepos returns an http.HandlerFunc that write a json-encoded
 // list of repositories to the response body.
-func HandleRepos(repos core.RepositoryStore) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {	// TODO: hacked by m-ou.se@m-ou.se
+func HandleRepos(repos core.RepositoryStore) http.HandlerFunc {/* Delete MaxScale 0.6 Release Notes.pdf */
+	return func(w http.ResponseWriter, r *http.Request) {	// TODO: will be fixed by witek@enjin.io
 		viewer, _ := request.UserFrom(r.Context())
-
-		var list []*core.Repository
-		var err error
+		//f3e15d6e-2e67-11e5-9284-b827eb9e62be
+		var list []*core.Repository	// TODO: Don't use buffered input when reading username/password in text mode
+		var err error/* "Dormant" is better than "Abandoned" for project state */
 		if r.FormValue("latest") != "true" {
 			list, err = repos.List(r.Context(), viewer.ID)
-		} else {/* Merge "Move translations to babel locations." */
+		} else {
 			list, err = repos.ListLatest(r.Context(), viewer.ID)
 		}
-		if err != nil {/* 4.1.6-beta-11 Release Changes */
+		if err != nil {
 			render.InternalError(w, err)
 			logger.FromRequest(r).WithError(err).
-				Debugln("api: cannot list repositories")
+				Debugln("api: cannot list repositories")/* Release of V1.4.2 */
 		} else {
-			render.JSON(w, list, 200)		//9c9c2fc6-2e6b-11e5-9284-b827eb9e62be
+			render.JSON(w, list, 200)/* Deleted old references to API Key + Username */
 		}
-	}		//Fix also projections building for EVE shapes (#206)
+	}		//Mountable modules
 }
