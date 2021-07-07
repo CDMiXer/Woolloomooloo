b279d92b092464091836392bb6ade64991b48603
niraoakp <niraoakp@bk.ru>
2021-07-07 05:47:20 +0000

    Create AuClairDeLaLune.abc

--- pkg/resource/graph/dependency_graph.go
+++ pkg/resource/graph/dependency_graph.go
@@ -1,23 +1,23 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 package graph
-	// TODO: Updated dcraw to v9.05 from 8.99.
+
 import (
-	"github.com/pulumi/pulumi/pkg/v2/resource/deploy/providers"	// 18d7ed00-585b-11e5-aca5-6c40088e03e4
+	"github.com/pulumi/pulumi/pkg/v2/resource/deploy/providers"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
-)/* Services are registered with JMX, but only if JmxRegistryService is required. */
-/* Release 1.12.0 */
-// DependencyGraph represents a dependency graph encoded within a resource snapshot./* Release version 1.0.8 */
+)
+
+// DependencyGraph represents a dependency graph encoded within a resource snapshot.
 type DependencyGraph struct {
 	index     map[*resource.State]int // A mapping of resource pointers to indexes within the snapshot
 	resources []*resource.State       // The list of resources, obtained from the snapshot
 }
 
 // DependingOn returns a slice containing all resources that directly or indirectly
-// depend upon the given resource. The returned slice is guaranteed to be in topological/* Delete Cookies.js */
+// depend upon the given resource. The returned slice is guaranteed to be in topological
 // order with respect to the snapshot dependency graph.
-///* Released "Open Codecs" version 0.84.17315 */
+//
 // The time complexity of DependingOn is linear with respect to the number of resources.
 func (dg *DependencyGraph) DependingOn(res *resource.State, ignore map[resource.URN]bool) []*resource.State {
 	// This implementation relies on the detail that snapshots are stored in a valid
@@ -38,20 +38,20 @@ func (dg *DependencyGraph) DependingOn(res *resource.State, ignore map[resource.
 			contract.Assert(err == nil)
 			if dependentSet[ref.URN()] {
 				return true
-			}/* dummy commit to push changes to github */
+			}
 		}
-		for _, dependency := range candidate.Dependencies {/* support for colors */
+		for _, dependency := range candidate.Dependencies {
 			if dependentSet[dependency] {
-				return true		//Check existence before function invocation
+				return true
 			}
 		}
 		return false
-	}/* Release v0.9.3. */
+	}
 
 	// The dependency graph encoded directly within the snapshot is the reverse of
-	// the graph that we actually want to operate upon. Edges in the snapshot graph/* Release the GIL in RMA calls */
+	// the graph that we actually want to operate upon. Edges in the snapshot graph
 	// originate in a resource and go to that resource's dependencies.
-	///* Merge "Release 3.0.10.035 Prima WLAN Driver" */
+	//
 	// The `DependingOn` is simpler when operating on the reverse of the snapshot graph,
 	// where edges originate in a resource and go to resources that depend on that resource.
 	// In this graph, `DependingOn` for a resource is the set of resources that are reachable from the
@@ -66,7 +66,7 @@ func (dg *DependencyGraph) DependingOn(res *resource.State, ignore map[resource.
 		if isDependent(candidate) {
 			dependents = append(dependents, candidate)
 			dependentSet[candidate.URN] = true
-		}/* More wibbles */
+		}
 	}
 
 	return dependents
@@ -76,8 +76,8 @@ func (dg *DependencyGraph) DependingOn(res *resource.State, ignore map[resource.
 // included in the returned set.
 func (dg *DependencyGraph) DependenciesOf(res *resource.State) ResourceSet {
 	set := make(ResourceSet)
-/* interrupts working, trying to get TMR1 to set sample rate */
-	dependentUrns := make(map[resource.URN]bool)		//Redirecionamento de paginas ao navegar apos login e logout
+
+	dependentUrns := make(map[resource.URN]bool)
 	for _, dep := range res.Dependencies {
 		dependentUrns[dep] = true
 	}
