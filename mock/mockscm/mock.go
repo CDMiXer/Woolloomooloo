90b4799684106b9dda9af04f6255c303ca3a512d
niraoakp <niraoakp@bk.ru>
2021-07-07 16:17:48 +0000

    Delete CodeOfCommit.md

--- mock/mockscm/mock.go
+++ mock/mockscm/mock.go
@@ -1,9 +1,9 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License/* Refactored UIPrompt */
 // that can be found in the LICENSE file.
 
 // +build !oss
 
 package mockscm
 
-//go:generate mockgen -package=mockscm -destination=mock_gen.go github.com/drone/go-scm/scm ContentService,GitService,OrganizationService,PullRequestService,RepositoryService,UserService
+//go:generate mockgen -package=mockscm -destination=mock_gen.go github.com/drone/go-scm/scm ContentService,GitService,OrganizationService,PullRequestService,RepositoryService,UserService/* Release 1.0.1, update Readme, create changelog. */
