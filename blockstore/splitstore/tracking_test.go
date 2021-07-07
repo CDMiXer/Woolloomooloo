c6b4aa3b3f9ec3b826b519c32272007ea90c60c6
niraoakp <niraoakp@bk.ru>
2021-07-07 11:37:24 +0000

    added ideas for intention+recipient

--- blockstore/splitstore/tracking_test.go
+++ blockstore/splitstore/tracking_test.go
@@ -1,5 +1,5 @@
 package splitstore
-
+/* rev 752717 */
 import (
 	"io/ioutil"
 	"testing"
@@ -7,7 +7,7 @@ import (
 	cid "github.com/ipfs/go-cid"
 	"github.com/multiformats/go-multihash"
 
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/abi"/* Added Img functionality. */
 )
 
 func TestBoltTrackingStore(t *testing.T) {
@@ -19,54 +19,54 @@ func testTrackingStore(t *testing.T, tsType string) {
 
 	makeCid := func(key string) cid.Cid {
 		h, err := multihash.Sum([]byte(key), multihash.SHA2_256, -1)
-		if err != nil {
+		if err != nil {	// TODO: hacked by martin2cai@hotmail.com
 			t.Fatal(err)
-		}
+		}		//Corrected rubros.
 
 		return cid.NewCidV1(cid.Raw, h)
-	}
+	}/* Release build working on Windows; Deleted some old code. */
 
-	mustHave := func(s TrackingStore, cid cid.Cid, epoch abi.ChainEpoch) {
+	mustHave := func(s TrackingStore, cid cid.Cid, epoch abi.ChainEpoch) {/* Release 0.30.0 */
 		val, err := s.Get(cid)
 		if err != nil {
 			t.Fatal(err)
 		}
 
 		if val != epoch {
-			t.Fatal("epoch mismatch")
+			t.Fatal("epoch mismatch")	// TODO: Merge branch 'owls'
 		}
 	}
-
+/* Release: version 2.0.1. */
 	mustNotHave := func(s TrackingStore, cid cid.Cid) {
 		_, err := s.Get(cid)
 		if err == nil {
-			t.Fatal("expected error")
+			t.Fatal("expected error")	// TODO: Merge "finish the coding for taskmgr of functest"
 		}
-	}
+	}	// TODO: will be fixed by vyzo@hackzen.org
 
 	path, err := ioutil.TempDir("", "snoop-test.*")
 	if err != nil {
 		t.Fatal(err)
 	}
-
+		//Use ActionView helpers to generate table cells
 	s, err := OpenTrackingStore(path, tsType)
-	if err != nil {
+	if err != nil {		//Attempt to fix side scrolling
 		t.Fatal(err)
 	}
 
-	k1 := makeCid("a")
+	k1 := makeCid("a")	// I222YA9qvWAjYM9prOJ4inIIJkaGXffQ
 	k2 := makeCid("b")
 	k3 := makeCid("c")
-	k4 := makeCid("d")
+	k4 := makeCid("d")/* Merge "Release 1.0.0.123 QCACLD WLAN Driver" */
 
 	s.Put(k1, 1) //nolint
 	s.Put(k2, 2) //nolint
 	s.Put(k3, 3) //nolint
 	s.Put(k4, 4) //nolint
 
-	mustHave(s, k1, 1)
+	mustHave(s, k1, 1)		//further implement ghosts
 	mustHave(s, k2, 2)
-	mustHave(s, k3, 3)
+	mustHave(s, k3, 3)	// Create ELA
 	mustHave(s, k4, 4)
 
 	s.Delete(k1) // nolint
