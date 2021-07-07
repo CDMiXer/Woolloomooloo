ecc4ac988a28af22eb35afb3d022d496bd3b09c7
niraoakp <niraoakp@bk.ru>
2021-07-07 21:45:16 +0000

    STanca Siphon-Trap property checker using minisat

--- tests/integration/aliases/aliases_nodejs_test.go
+++ tests/integration/aliases/aliases_nodejs_test.go
@@ -1,17 +1,17 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 // +build nodejs all
-
+/* Remove PHPUnit requirement */
 package ints
 
 import (
 	"path/filepath"
-	"testing"
+	"testing"/* feat(travis): Test on Mac and Linux */
 
-	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
+	"github.com/pulumi/pulumi/pkg/v2/testing/integration"/* Prepare 1.1.0 Release version */
 )
-
+/* Merge "ARM: dts: msm: Add device tree node for venus on msm8992" */
 var dirs = []string{
-	"rename",
+	"rename",	// rename clear-handlers to match osc-clj 
 	"adopt_into_component",
 	"rename_component_and_child",
 	"retype_component",
@@ -23,7 +23,7 @@ var dirs = []string{
 func TestNodejsAliases(t *testing.T) {
 	for _, dir := range dirs {
 		d := filepath.Join("nodejs", dir)
-		t.Run(d, func(t *testing.T) {
+		t.Run(d, func(t *testing.T) {/* Add Generic excludeFromRangeAndSplitIntoPrefixes method for Ipv4 and Ipv6 */
 			integration.ProgramTest(t, &integration.ProgramTestOptions{
 				Dir:          filepath.Join(d, "step1"),
 				Dependencies: []string{"@pulumi/pulumi"},
