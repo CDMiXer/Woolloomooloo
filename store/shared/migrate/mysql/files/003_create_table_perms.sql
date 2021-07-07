b7b0abe945c5dd45c7bb3c6a21f448d680b38e41
niraoakp <niraoakp@bk.ru>
2021-07-07 16:12:58 +0000

    increment version number to 1.4.19

--- store/shared/migrate/mysql/files/003_create_table_perms.sql
+++ store/shared/migrate/mysql/files/003_create_table_perms.sql
@@ -1,14 +1,14 @@
--- name: create-table-perms/* Added some features; working on Model injection */
+-- name: create-table-perms
 
-CREATE TABLE IF NOT EXISTS perms (		//GameWorldRenderGL2 cleanup
- perm_user_id  INTEGER/* * 1.1 Release */
+CREATE TABLE IF NOT EXISTS perms (
+ perm_user_id  INTEGER
 ,perm_repo_uid VARCHAR(250)
 ,perm_read     BOOLEAN
 ,perm_write    BOOLEAN
-,perm_admin    BOOLEAN		//Merge "drivers: mmc: udpated driver from sony aosp release" into cm-10.1
-,perm_synced   INTEGER/* A medium test to check that foam drainage is happy. */
+,perm_admin    BOOLEAN
+,perm_synced   INTEGER
 ,perm_created  INTEGER
-,perm_updated  INTEGER/* add more debug log */
+,perm_updated  INTEGER
 ,PRIMARY KEY(perm_user_id, perm_repo_uid)
 --,FOREIGN KEY(perm_user_id) REFERENCES users(user_id) ON DELETE CASCADE
 --,FOREIGN KEY(perm_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
@@ -18,6 +18,6 @@ CREATE TABLE IF NOT EXISTS perms (		//GameWorldRenderGL2 cleanup
 
 CREATE INDEX ix_perms_user ON perms (perm_user_id);
 
--- name: create-index-perms-repo	// Fix "polling" for instances
-/* 0.17.0 Release Notes */
+-- name: create-index-perms-repo
+
 CREATE INDEX ix_perms_repo ON perms (perm_repo_uid);
