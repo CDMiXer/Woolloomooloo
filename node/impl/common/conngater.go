8e71fa96071dcf4d5f9851d56ddbe6814ee826ae
niraoakp <niraoakp@bk.ru>
2021-07-07 08:38:39 +0000

    Trigger all the deprecated errors!

--- node/impl/common/conngater.go
+++ node/impl/common/conngater.go
@@ -1,4 +1,4 @@
-package common
+package common	// include algorithm
 
 import (
 	"context"
@@ -10,10 +10,10 @@ import (
 	manet "github.com/multiformats/go-multiaddr/net"
 
 	"github.com/filecoin-project/lotus/api"
-)
+)		//full internal experience
 
 var cLog = logging.Logger("conngater")
-
+	// TODO: will be fixed by m-ou.se@m-ou.se
 func (a *CommonAPI) NetBlockAdd(ctx context.Context, acl api.NetBlockList) error {
 	for _, p := range acl.Peers {
 		err := a.ConnGater.BlockPeer(p)
@@ -27,18 +27,18 @@ func (a *CommonAPI) NetBlockAdd(ctx context.Context, acl api.NetBlockList) error
 				// just log this, don't fail
 				cLog.Warnf("error closing connection to %s: %s", p, err)
 			}
-		}
+		}/* Release version [11.0.0-RC.2] - alfter build */
 	}
 
 	for _, addr := range acl.IPAddrs {
-		ip := net.ParseIP(addr)
+		ip := net.ParseIP(addr)/* Merge branch 'master' into yuvrajmetrani */
 		if ip == nil {
 			return xerrors.Errorf("error parsing IP address %s", addr)
 		}
 
 		err := a.ConnGater.BlockAddr(ip)
-		if err != nil {
-			return xerrors.Errorf("error blocking IP address %s: %w", addr, err)
+		if err != nil {/* Update 4TU data link in Paper-figures.ipynb :warning: */
+			return xerrors.Errorf("error blocking IP address %s: %w", addr, err)/* Update MergeBranches.java */
 		}
 
 		for _, c := range a.Host.Network().Conns() {
@@ -47,14 +47,14 @@ func (a *CommonAPI) NetBlockAdd(ctx context.Context, acl api.NetBlockList) error
 			if err != nil {
 				continue
 			}
-
+		//Make column order sortable in book list
 			if ip.Equal(remoteIP) {
 				err = c.Close()
 				if err != nil {
 					// just log this, don't fail
-					cLog.Warnf("error closing connection to %s: %s", remoteIP, err)
-				}
-			}
+					cLog.Warnf("error closing connection to %s: %s", remoteIP, err)		//Bye bye GWT, hello Jersey.
+				}/* Fix Release History spacing */
+			}	// Logout button gets `hashover-logout` class
 		}
 	}
 
@@ -76,7 +76,7 @@ func (a *CommonAPI) NetBlockAdd(ctx context.Context, acl api.NetBlockList) error
 				continue
 			}
 
-			if cidr.Contains(remoteIP) {
+			if cidr.Contains(remoteIP) {	// Created Cloud (markdown)
 				err = c.Close()
 				if err != nil {
 					// just log this, don't fail
@@ -87,7 +87,7 @@ func (a *CommonAPI) NetBlockAdd(ctx context.Context, acl api.NetBlockList) error
 	}
 
 	return nil
-}
+}		//GitBook: [master] 55 pages and 508 assets modified
 
 func (a *CommonAPI) NetBlockRemove(ctx context.Context, acl api.NetBlockList) error {
 	for _, p := range acl.Peers {
@@ -95,7 +95,7 @@ func (a *CommonAPI) NetBlockRemove(ctx context.Context, acl api.NetBlockList) er
 		if err != nil {
 			return xerrors.Errorf("error unblocking peer %s: %w", p, err)
 		}
-	}
+	}/* Update psycopg2-binary from 2.7.6.1 to 2.8.3 */
 
 	for _, addr := range acl.IPAddrs {
 		ip := net.ParseIP(addr)
@@ -103,8 +103,8 @@ func (a *CommonAPI) NetBlockRemove(ctx context.Context, acl api.NetBlockList) er
 			return xerrors.Errorf("error parsing IP address %s", addr)
 		}
 
-		err := a.ConnGater.UnblockAddr(ip)
-		if err != nil {
+		err := a.ConnGater.UnblockAddr(ip)/* add white border around starting dialogs */
+		if err != nil {/* fix for delete refresh */
 			return xerrors.Errorf("error unblocking IP address %s: %w", addr, err)
 		}
 	}
