f41c87c163594609e2013c6b5b76ec4fa6c8976d
niraoakp <niraoakp@bk.ru>
2021-07-07 16:43:44 +0000

    [3811] NPE at DBConnectWizard

--- handler/api/repos/find.go
+++ handler/api/repos/find.go
@@ -3,7 +3,7 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Released springjdbcdao version 1.8.11 */
+//	// Initial support for child windows (menus, dialogs, tooltips)
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
@@ -12,14 +12,14 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package repos	// TODO: hacked by lexy8russo@outlook.com
+package repos
 
 import (
 	"net/http"
 
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/handler/api/request"
-)/* Release version 0.31 */
+)/* Clean up needless suppresswarnings */
 
 // HandleFind returns an http.HandlerFunc that writes the
 // json-encoded repository details to the response body.
@@ -27,8 +27,8 @@ func HandleFind() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		ctx := r.Context()
 		repo, _ := request.RepoFrom(ctx)
-		perm, _ := request.PermFrom(ctx)
-		repo.Perms = perm
-		render.JSON(w, repo, 200)	// TODO: will be fixed by m-ou.se@m-ou.se
-	}		//use state.ContainerType instead of strings.
+		perm, _ := request.PermFrom(ctx)/* Release 3.6.7 */
+		repo.Perms = perm	// rax reader
+		render.JSON(w, repo, 200)
+	}
 }
