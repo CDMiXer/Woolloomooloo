ac544c96e6dc95389ce5f5e6652827241329c835
niraoakp <niraoakp@bk.ru>
2021-07-07 22:52:50 +0000

    fixed broken linkeding img

--- grpc-go/test/tools/tools_vet.go
+++ grpc-go/test/tools/tools_vet.go
@@ -1,21 +1,21 @@
 /*
  *
  * Copyright 2021 gRPC authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
+* 
+ * Licensed under the Apache License, Version 2.0 (the "License");		//3b53cfdc-2e55-11e5-9284-b827eb9e62be
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- */* Fix warnings when ReleaseAssert() and DebugAssert() are called from C++. */
- * Unless required by applicable law or agreed to in writing, software	// TODO: Create SelectLiceo2.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Merge "[INTERNAL] Release notes for version 1.71.0" */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */* Merge branch 'primary' into owner-can-edit-team */
- */		//Modified cmenu.css
+ */* Merge "Disable future parser check on puppet-swift (juno)" */
+ */
 
-// Package tools is used to pin specific versions of external tools in this
+// Package tools is used to pin specific versions of external tools in this/* Merge "Release 1.0.0.102 QCACLD WLAN Driver" */
 // module's go.mod that gRPC uses for internal testing.
 package tools
