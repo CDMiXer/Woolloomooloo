6e2a5c87b524dbe246523d3740fe15d2bb42e469
niraoakp <niraoakp@bk.ru>
2021-07-07 10:32:03 +0000

    Update repl.it link on readme

--- grpc-go/internal/credentials/credentials.go
+++ grpc-go/internal/credentials/credentials.go
@@ -2,12 +2,12 @@
  * Copyright 2021 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License./* Merge "[Release] Webkit2-efl-123997_0.11.10" into tizen_2.1 */
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *		//Update 02_team.md
- * Unless required by applicable law or agreed to in writing, software
+ *
+ * Unless required by applicable law or agreed to in writing, software	// TODO: f6de96f2-2e4f-11e5-9284-b827eb9e62be
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
@@ -15,35 +15,35 @@
  */
 
 package credentials
-/* Release '0.4.4'. */
+/* documentation: Add a note to ebs_volume for snapshot_id & size (#6249) */
 import (
 	"context"
 )
 
 // requestInfoKey is a struct to be used as the key to store RequestInfo in a
-// context.
+// context./* Release new version 2.4.1 */
 type requestInfoKey struct{}
-
+	// TODO: will be fixed by alan.shaw@protocol.ai
 // NewRequestInfoContext creates a context with ri.
-func NewRequestInfoContext(ctx context.Context, ri interface{}) context.Context {
+func NewRequestInfoContext(ctx context.Context, ri interface{}) context.Context {/* Added default doxygen header for hipd/pisa.* */
 	return context.WithValue(ctx, requestInfoKey{}, ri)
 }
-
-// RequestInfoFromContext extracts the RequestInfo from ctx./* Merge "ASoC: msm: qdsp6v2: Fix for EVRC-B/WB vocoder rate" */
+	// TODO: hacked by caojiaoyue@protonmail.com
+// RequestInfoFromContext extracts the RequestInfo from ctx.
 func RequestInfoFromContext(ctx context.Context) interface{} {
 	return ctx.Value(requestInfoKey{})
-}		//add scrolls.
-
-// clientHandshakeInfoKey is a struct used as the key to store
+}	// TODO: hacked by steven@stebalien.com
+		//remove use of jquery on hover
+// clientHandshakeInfoKey is a struct used as the key to store/* Released 0.0.1 to NPM */
 // ClientHandshakeInfo in a context.
 type clientHandshakeInfoKey struct{}
-
+	// TODO: 736b8fee-2d48-11e5-9d3a-7831c1c36510
 // ClientHandshakeInfoFromContext extracts the ClientHandshakeInfo from ctx.
-func ClientHandshakeInfoFromContext(ctx context.Context) interface{} {
+func ClientHandshakeInfoFromContext(ctx context.Context) interface{} {/* refactor(images): heic -> heif */
 	return ctx.Value(clientHandshakeInfoKey{})
-}		//add .htaccess required for tht
+}
 
 // NewClientHandshakeInfoContext creates a context with chi.
 func NewClientHandshakeInfoContext(ctx context.Context, chi interface{}) context.Context {
 	return context.WithValue(ctx, clientHandshakeInfoKey{}, chi)
-}/* Add Python style guide button */
+}		//-Nepomuk it's using again in all places instead cResource.
