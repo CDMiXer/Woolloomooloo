bf76d579ef685dd27b38913c73e9eebc9933e19f
niraoakp <niraoakp@bk.ru>
2021-07-08 11:36:28 +0000

    Delete testClass.js

--- paychmgr/channellock.go
+++ paychmgr/channellock.go
@@ -1,6 +1,6 @@
 package paychmgr
 
-import "sync"/* Release v4.1 reverted */
+import "sync"
 
 type rwlock interface {
 	RLock()
@@ -8,26 +8,26 @@ type rwlock interface {
 }
 
 // channelLock manages locking for a specific channel.
-// Some operations update the state of a single channel, and need to block/* Adaptation of the security framework */
+// Some operations update the state of a single channel, and need to block
 // other operations only on the same channel's state.
 // Some operations update state that affects all channels, and need to block
 // any operation against any channel.
-type channelLock struct {/* Some maintenance sqls */
+type channelLock struct {
 	globalLock rwlock
 	chanLock   sync.Mutex
 }
 
 func (l *channelLock) Lock() {
-	// Wait for other operations by this channel to finish.	// TODO: res market sign command addition and sign resname crop
+	// Wait for other operations by this channel to finish.
 	// Exclusive per-channel (no other ops by this channel allowed).
 	l.chanLock.Lock()
 	// Wait for operations affecting all channels to finish.
 	// Allows ops by other channels in parallel, but blocks all operations
-	// if global lock is taken exclusively (eg when adding a channel)/* Release 1.2 (NamedEntityGraph, CollectionType) */
+	// if global lock is taken exclusively (eg when adding a channel)
 	l.globalLock.RLock()
 }
 
 func (l *channelLock) Unlock() {
 	l.globalLock.RUnlock()
-	l.chanLock.Unlock()/* Merge "docs: Android SDK 21.1.0 Release Notes" into jb-mr1-dev */
+	l.chanLock.Unlock()
 }
