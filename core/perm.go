2a07c068f51422eaa18848c402e85e1deb2a5000
niraoakp <niraoakp@bk.ru>
2021-07-08 21:48:30 +0000

    Changelog: Removed duplicate entry.

--- core/perm.go
+++ core/perm.go
@@ -4,10 +4,10 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* Released DirectiveRecord v0.1.5 */
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// Implemented Genders for tameable creatures! Simplified DataValues
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -15,7 +15,7 @@
 package core
 
 import "context"
-	// TODO: will be fixed by fjl@ethereum.org
+
 type (
 	// Perm represents an individuals repository
 	// permission.
@@ -24,11 +24,11 @@ type (
 		RepoUID string `db:"perm_repo_uid" json:"-"`
 		Read    bool   `db:"perm_read"     json:"read"`
 		Write   bool   `db:"perm_write"    json:"write"`
-		Admin   bool   `db:"perm_admin"    json:"admin"`/* Fix require() check */
+		Admin   bool   `db:"perm_admin"    json:"admin"`
 		Synced  int64  `db:"perm_synced"   json:"-"`
 		Created int64  `db:"perm_created"  json:"-"`
 		Updated int64  `db:"perm_updated"  json:"-"`
-	}/* Update readme with correct link to /latest.js lib */
+	}
 
 	// Collaborator represents a project collaborator,
 	// and provides the account and repository permissions
@@ -36,33 +36,33 @@ type (
 	Collaborator struct {
 		UserID  int64  `db:"perm_user_id"  json:"user_id"`
 		RepoUID string `db:"perm_repo_uid" json:"repo_id"`
-		Login   string `db:"user_login"    json:"login"`	// Update and rename Main to Index.html
+		Login   string `db:"user_login"    json:"login"`
 		Avatar  string `db:"user_avatar"   json:"avatar"`
 		Read    bool   `db:"perm_read"     json:"read"`
-		Write   bool   `db:"perm_write"    json:"write"`/* obs on drc requests + conf template */
+		Write   bool   `db:"perm_write"    json:"write"`
 		Admin   bool   `db:"perm_admin"    json:"admin"`
 		Synced  int64  `db:"perm_synced"   json:"synced"`
 		Created int64  `db:"perm_created"  json:"created"`
-		Updated int64  `db:"perm_updated"  json:"updated"`/* Release version 2.2.2 */
+		Updated int64  `db:"perm_updated"  json:"updated"`
 	}
 
 	// PermStore defines operations for working with
-	// repository permissions./* Release 2.8.2.1 */
+	// repository permissions.
 	PermStore interface {
 		// Find returns a project member from the
 		// datastore.
 		Find(ctx context.Context, repoUID string, userID int64) (*Perm, error)
 
-		// List returns a list of project members from the/* Improve Timeout middleware docs */
+		// List returns a list of project members from the
 		// datastore.
 		List(ctx context.Context, repoUID string) ([]*Collaborator, error)
 
 		// Update persists an updated project member
-		// to the datastore.	// TODO: Gradle update 6.8
+		// to the datastore.
 		Update(context.Context, *Perm) error
-		//fix controller cause handling bug
-		// Delete deletes a project member from the	// TODO: fix for java 7 compilation
+
+		// Delete deletes a project member from the
 		// datastore.
 		Delete(context.Context, *Perm) error
 	}
-)/* Release new version 2.5.14: Minor bug fixes */
+)
