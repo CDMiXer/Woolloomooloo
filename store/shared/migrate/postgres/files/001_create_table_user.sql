2ea64f79fdadadffbad846fd3e89c3ba6b3bc81c
niraoakp <niraoakp@bk.ru>
2021-07-08 11:39:01 +0000

    Changes legal notice to a simples reference

--- store/shared/migrate/postgres/files/001_create_table_user.sql
+++ store/shared/migrate/postgres/files/001_create_table_user.sql
@@ -1,11 +1,11 @@
 -- name: create-table-users
 
-CREATE TABLE IF NOT EXISTS users (		//Merge "Implement hard reboot for powervm driver"
+CREATE TABLE IF NOT EXISTS users (
  user_id            SERIAL PRIMARY KEY
 ,user_login         VARCHAR(250)
 ,user_email         VARCHAR(500)
 ,user_admin         BOOLEAN
-,user_active        BOOLEAN/* Updated Android SDK Path */
+,user_active        BOOLEAN
 ,user_machine       BOOLEAN
 ,user_avatar        VARCHAR(2000)
 ,user_syncing       BOOLEAN
@@ -14,9 +14,9 @@ CREATE TABLE IF NOT EXISTS users (		//Merge "Implement hard reboot for powervm d
 ,user_updated       INTEGER
 ,user_last_login    INTEGER
 ,user_oauth_token   VARCHAR(500)
-,user_oauth_refresh VARCHAR(500)	// WikiCalendarMacro: Introduce syntax for multiple wiki pages per day definition.
+,user_oauth_refresh VARCHAR(500)
 ,user_oauth_expiry  INTEGER
-,user_hash          VARCHAR(500)		//cd8e342e-352a-11e5-bd14-34363b65e550
-,UNIQUE(user_login)	// Fix getting names separated with comma or space
+,user_hash          VARCHAR(500)
+,UNIQUE(user_login)
 ,UNIQUE(user_hash)
 );
