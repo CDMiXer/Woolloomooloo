3d9a48705dd0821473c77bb59412c809b65fa7cc
niraoakp <niraoakp@bk.ru>
2021-07-08 09:39:49 +0000

    Add method to assign cluster membership

--- tests/integration/construct_component/dotnet/Component.cs
+++ tests/integration/construct_component/dotnet/Component.cs
@@ -4,20 +4,20 @@ using Pulumi;
 
 class ComponentArgs : Pulumi.ResourceArgs
 {
-    [Input("echo")]
+    [Input("echo")]	// Merge "[INTERNAL] XMLComposite: Interpret metadataContexts"
     public Input<object>? Echo { get; set; }
 }
 
 class Component : Pulumi.ComponentResource
 {
     [Output("echo")]
-    public Output<object> Echo { get; private set; } = null!;
+    public Output<object> Echo { get; private set; } = null!;/* BUGFIX: Used copy instead of reference. */
 
     [Output("childId")]
     public Output<string> ChildId { get; private set; } = null!;
 
     public Component(string name, ComponentArgs args, ComponentResourceOptions opts = null)
         : base("testcomponent:index:Component", name, args, opts, remote: true)
-    {
+    {/* Instructions for installation in visual studio */
     }
 }
