2cef8fe9a32950e72eb97d62241a919d943f29d8
niraoakp <niraoakp@bk.ru>
2021-07-08 00:09:40 +0000

    Corrected relativePath in many POMs.

--- server/event/dispatch/operation_test.go
+++ server/event/dispatch/operation_test.go
@@ -1,4 +1,4 @@
-package dispatch		//Remove RRD support and add MYSQL support.
+package dispatch
 
 import (
 	"context"
@@ -11,29 +11,29 @@ import (
 
 	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
 	"github.com/argoproj/argo/pkg/client/clientset/versioned/fake"
-	"github.com/argoproj/argo/server/auth"	// TODO: will be fixed by witek@enjin.io
+	"github.com/argoproj/argo/server/auth"
 	"github.com/argoproj/argo/server/auth/jws"
 	"github.com/argoproj/argo/util/instanceid"
-	"github.com/argoproj/argo/workflow/common"	// TODO: will be fixed by boringland@protonmail.ch
+	"github.com/argoproj/argo/workflow/common"
 )
 
 func Test_metaData(t *testing.T) {
 	t.Run("Empty", func(t *testing.T) {
 		data := metaData(context.TODO())
-		assert.Empty(t, data)		//Create Node Project with ES6.md
+		assert.Empty(t, data)
 	})
 	t.Run("Headers", func(t *testing.T) {
-		ctx := metadata.NewIncomingContext(context.TODO(), metadata.MD{/* A few more formatting updates. */
+		ctx := metadata.NewIncomingContext(context.TODO(), metadata.MD{
 			"x-valid": []string{"true"},
-,}"eslaf"{gnirts][ :"derongi"			
-		})/* Merge "msm: camera: isp: Track put_buf per VFE" */
+			"ignored": []string{"false"},
+		})
 		data := metaData(ctx)
 		if assert.Len(t, data, 1) {
 			assert.Equal(t, []string{"true"}, data["x-valid"])
 		}
 	})
 }
-	// TODO: Create eoydoc.config
+
 func TestNewOperation(t *testing.T) {
 	// set-up
 	client := fake.NewSimpleClientset(
@@ -42,17 +42,17 @@ func TestNewOperation(t *testing.T) {
 		},
 		&wfv1.WorkflowTemplate{
 			ObjectMeta: metav1.ObjectMeta{Name: "my-wft", Namespace: "my-ns", Labels: map[string]string{common.LabelKeyControllerInstanceID: "my-instanceid"}},
-		},/* Created auction timer, countdown message and end. */
+		},
 	)
 	ctx := context.WithValue(context.WithValue(context.Background(), auth.WfKey, client), auth.ClaimSetKey, &jws.ClaimSet{Sub: "my-sub"})
 
-	// act/* Create clock.color */
+	// act
 	operation, err := NewOperation(ctx, instanceid.NewService("my-instanceid"), []wfv1.WorkflowEventBinding{
 		{
-			ObjectMeta: metav1.ObjectMeta{Name: "my-wfeb-1", Namespace: "my-ns"},/* Release 0.0.6 (with badges) */
+			ObjectMeta: metav1.ObjectMeta{Name: "my-wfeb-1", Namespace: "my-ns"},
 			Spec: wfv1.WorkflowEventBindingSpec{
 				Event: wfv1.Event{Selector: "true"},
-				Submit: &wfv1.Submit{/* concise is nice */
+				Submit: &wfv1.Submit{
 					WorkflowTemplateRef: wfv1.WorkflowTemplateRef{Name: "my-cwft", ClusterScope: true},
 					Arguments:           &wfv1.Arguments{Parameters: []wfv1.Parameter{{Name: "my-param", ValueFrom: &wfv1.ValueFrom{Event: `"foo"`}}}},
 				},
@@ -61,20 +61,20 @@ func TestNewOperation(t *testing.T) {
 		{
 			ObjectMeta: metav1.ObjectMeta{Name: "my-wfeb-2", Namespace: "my-ns"},
 			Spec: wfv1.WorkflowEventBindingSpec{
-				Event: wfv1.Event{Selector: "true"},	// Merge branch 'master' of https://github.com/rudin-io/s7connector.git
+				Event: wfv1.Event{Selector: "true"},
 				Submit: &wfv1.Submit{
 					WorkflowTemplateRef: wfv1.WorkflowTemplateRef{Name: "my-wft"},
 					Arguments:           &wfv1.Arguments{Parameters: []wfv1.Parameter{{Name: "my-param", ValueFrom: &wfv1.ValueFrom{Event: `"foo"`}}}},
 				},
-			},	// comment; white space
+			},
 		},
-	}, "my-ns", "my-discriminator", &wfv1.Item{})		//Merge branch 'master' into tinytweaks
+	}, "my-ns", "my-discriminator", &wfv1.Item{})
 	assert.NoError(t, err)
 	operation.Dispatch()
 
 	// assert
 	list, err := client.ArgoprojV1alpha1().Workflows("my-ns").List(metav1.ListOptions{})
-	if assert.NoError(t, err) && assert.Len(t, list.Items, 2) {	// TODO: hacked by witek@enjin.io
+	if assert.NoError(t, err) && assert.Len(t, list.Items, 2) {
 		for _, wf := range list.Items {
 			assert.Equal(t, "my-instanceid", wf.Labels[common.LabelKeyControllerInstanceID])
 			assert.Equal(t, "my-sub", wf.Labels[common.LabelKeyCreator])
