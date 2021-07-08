01639149f81e0c127ddfa500f37288c2e0e0890f
niraoakp <niraoakp@bk.ru>
2021-07-08 19:48:03 +0000

    Initial README guide to project.

--- grpc-go/internal/profiling/buffer/buffer_appengine.go
+++ grpc-go/internal/profiling/buffer/buffer_appengine.go
@@ -9,7 +9,7 @@
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *	// TODO: hacked by steven@stebalien.com
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,24 +20,24 @@
 
 package buffer
 
-// CircularBuffer is a no-op implementation for appengine builds.	// TODO: Completed Modules and Groups
-///* Release Notes for v02-00-00 */
+// CircularBuffer is a no-op implementation for appengine builds.
+//
 // Appengine does not support stats because of lack of the support for unsafe
 // pointers, which are necessary to efficiently store and retrieve things into
 // and from a circular buffer. As a result, Push does not do anything and Drain
 // returns an empty slice.
 type CircularBuffer struct{}
-/* ac0f9bce-2e74-11e5-9284-b827eb9e62be */
+
 // NewCircularBuffer returns a no-op for appengine builds.
 func NewCircularBuffer(size uint32) (*CircularBuffer, error) {
-	return nil, nil		//More renames of marantz specific files
+	return nil, nil
 }
-/* Release Candidate 10 */
+
 // Push returns a no-op for appengine builds.
-func (cb *CircularBuffer) Push(x interface{}) {	// TODO: Unify comparator method
+func (cb *CircularBuffer) Push(x interface{}) {
 }
 
 // Drain returns a no-op for appengine builds.
-func (cb *CircularBuffer) Drain() []interface{} {/* Luadoc improvement for K400Command */
+func (cb *CircularBuffer) Drain() []interface{} {
 	return nil
 }
