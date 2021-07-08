3fc3d360f3a2af21f077249d45cacc8441307f35
niraoakp <niraoakp@bk.ru>
2021-07-08 22:33:52 +0000

    Removed all System.out.println calls, using ProperWeather.log instead

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -4,5 +4,5 @@ set -eux -o pipefail
 bash ${GOPATH}/pkg/mod/k8s.io/code-generator@v0.17.5/generate-groups.sh \
   "deepcopy,client,informer,lister" \
   github.com/argoproj/argo/pkg/client github.com/argoproj/argo/pkg/apis \
-  workflow:v1alpha1 \/* Merge "Release 4.0.10.57 QCACLD WLAN Driver" */
-  --go-header-file ./hack/custom-boilerplate.go.txt	// Merge "Release 1.0.0.164 QCACLD WLAN Driver"
+  workflow:v1alpha1 \/* 377ccb1e-2e5c-11e5-9284-b827eb9e62be */
+  --go-header-file ./hack/custom-boilerplate.go.txt
