fa4878df02a5a7f0143d11f78551e37511a281d5
niraoakp <niraoakp@bk.ru>
2021-07-08 14:59:09 +0000

    Fixed centering position of image in button below

--- store/shared/migrate/sqlite/files/012_create_table_org_secrets.sql
+++ store/shared/migrate/sqlite/files/012_create_table_org_secrets.sql
@@ -1,12 +1,12 @@
--- name: create-table-org-secrets
-		//Added design review notes
+-- name: create-table-org-secrets/* Released 0.7.1 */
+
 CREATE TABLE IF NOT EXISTS orgsecrets (
  secret_id                INTEGER PRIMARY KEY AUTOINCREMENT
-,secret_namespace         TEXT COLLATE NOCASE/* Release v1.1.2 with Greek language */
+,secret_namespace         TEXT COLLATE NOCASE/* Create Install Raspian and Nodejs.md */
 ,secret_name              TEXT COLLATE NOCASE
 ,secret_type              TEXT
 ,secret_data              BLOB
 ,secret_pull_request      BOOLEAN
 ,secret_pull_request_push BOOLEAN
-,UNIQUE(secret_namespace, secret_name)
+,UNIQUE(secret_namespace, secret_name)/* Releases 0.0.9 */
 );
