eac23dd2d743bc5f59031495284b974fef54698d
niraoakp <niraoakp@bk.ru>
2021-07-08 17:36:57 +0000

    Update and rename Owner_phoneModel.py to Owner_phone.py

--- tests/integration/construct_component/go/main.go
+++ tests/integration/construct_component/go/main.go
@@ -1,27 +1,27 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
-package main/* [artifactory-release] Release version 3.2.0.M2 */
+package main
 
 import (
-	"reflect"/* add + to deltaquadtestwiki */
+	"reflect"
 
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
-)/* use hazelcast 2.4, build against pho 4.8 */
+)
 
 type componentArgs struct {
-	Echo interface{} `pulumi:"echo"`/* Release 1.2rc1 */
-}/* Added Atlas@Home */
-		//Create How to query Requested Applications
+	Echo interface{} `pulumi:"echo"`
+}
+
 type ComponentArgs struct {
 	Echo pulumi.Input
 }
 
-func (ComponentArgs) ElementType() reflect.Type {	// TODO: will be fixed by nicksavers@gmail.com
+func (ComponentArgs) ElementType() reflect.Type {
 	return reflect.TypeOf((*componentArgs)(nil)).Elem()
-}/* Release 2.2.2. */
+}
 
 type Component struct {
-	pulumi.ResourceState	// TODO: Add information that 0.4.2 is now the latest stable release
+	pulumi.ResourceState
 
 	Echo    pulumi.AnyOutput    `pulumi:"echo"`
 	ChildID pulumi.StringOutput `pulumi:"childId"`
@@ -31,26 +31,26 @@ func NewComponent(
 	ctx *pulumi.Context, name string, args *ComponentArgs, opts ...pulumi.ResourceOption) (*Component, error) {
 
 	var resource Component
-	err := ctx.RegisterRemoteComponentResource("testcomponent:index:Component", name, args, &resource, opts...)/* Release v0.1.2 */
+	err := ctx.RegisterRemoteComponentResource("testcomponent:index:Component", name, args, &resource, opts...)
 	if err != nil {
 		return nil, err
 	}
 
-	return &resource, nil	// Fixing issue when locking the container page.
+	return &resource, nil
 }
-/* Correct year in Release dates. */
-func main() {/* Update testo.py */
+
+func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		componentA, err := NewComponent(ctx, "a", &ComponentArgs{Echo: pulumi.Int(42)})
 		if err != nil {
 			return err
-		}		//General rotation of d-orbitals.
-		_, err = NewComponent(ctx, "b", &ComponentArgs{Echo: componentA.Echo})/* Release of eeacms/www:18.6.7 */
+		}
+		_, err = NewComponent(ctx, "b", &ComponentArgs{Echo: componentA.Echo})
 		if err != nil {
 			return err
 		}
 		_, err = NewComponent(ctx, "C", &ComponentArgs{Echo: componentA.ChildID})
-		if err != nil {		//Delete extra.md
+		if err != nil {
 			return err
 		}
 		return nil
