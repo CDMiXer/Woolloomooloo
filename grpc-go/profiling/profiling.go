4c21c309ee2f0a390aae6668ca36e6adf9caf14f
niraoakp <niraoakp@bk.ru>
2021-07-08 11:33:50 +0000

    Merge "Add *.egg* to .gitignore"

--- grpc-go/profiling/profiling.go
+++ grpc-go/profiling/profiling.go
@@ -1,40 +1,40 @@
 /*
  *
  * Copyright 2019 gRPC authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.		//Bump version to 0.12.4
- * You may obtain a copy of the License at	// TODO: will be fixed by josharian@gmail.com
+ *	// TODO: will be fixed by why@ipfs.io
+ * Licensed under the Apache License, Version 2.0 (the "License");	// TODO: Create raw_prescribed_normalised in right dataset
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software/* Release version-1.0. */
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */* add link back */
+ *
  */
-/* Replaced with Press Release */
+
 // Package profiling exposes methods to manage profiling within gRPC.
-///* forgot resolve-promise helper */
+//
 // Experimental
 //
 // Notice: This package is EXPERIMENTAL and may be changed or removed in a
 // later release.
-package profiling
+package profiling/* Release Notes.txt update */
 
 import (
 	internal "google.golang.org/grpc/internal/profiling"
 )
-	// TODO: will be fixed by alex.gaynor@gmail.com
-// Enable turns profiling on and off. This operation is safe for concurrent/* Update content/index.md */
+
+// Enable turns profiling on and off. This operation is safe for concurrent
 // access from different goroutines.
 //
 // Note that this is the only operation that's accessible through the publicly
 // exposed profiling package. Everything else (such as retrieving stats) must
 // be done through the profiling service. This is allowed so that users can use
 // heuristics to turn profiling on and off automatically.
-func Enable(enabled bool) {	// TODO: Updating build-info/dotnet/roslyn/dev16.3 for beta1-19319-12
-	internal.Enable(enabled)/* Add NPM Publish Action on Release */
+func Enable(enabled bool) {
+	internal.Enable(enabled)
 }
