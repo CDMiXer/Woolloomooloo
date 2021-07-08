460dae4a00a2e269cf22b470d81b8928f393f536
niraoakp <niraoakp@bk.ru>
2021-07-08 04:40:34 +0000

    bitstamp1 metainfo updates

--- livelog/sub.go
+++ livelog/sub.go
@@ -1,18 +1,18 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// TODO: Delete FDM_SubHalo_Potential.py
-// You may obtain a copy of the License at/* 3925f836-2e3f-11e5-9284-b827eb9e62be */
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Update release notes. Actual Release 2.2.3. */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package livelog/* cloudinit: moving targetRelease assign */
+package livelog
 
 import (
 	"sync"
@@ -21,7 +21,7 @@ import (
 )
 
 type subscriber struct {
-	sync.Mutex	// TODO: hacked by steven@stebalien.com
+	sync.Mutex
 
 	handler chan *core.Line
 	closec  chan struct{}
