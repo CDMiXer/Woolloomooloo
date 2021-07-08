d39603d920f94102024e345a980a3fff94cf76b7
niraoakp <niraoakp@bk.ru>
2021-07-08 01:31:09 +0000

    add и выйти

--- grpc-go/xds/internal/balancer/clustermanager/config_test.go
+++ grpc-go/xds/internal/balancer/clustermanager/config_test.go
@@ -3,34 +3,34 @@
 /*
  *
  * Copyright 2020 gRPC authors.
- *		//fix issue where an empty line with a tab could result in a varlist
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *		//Comment explaining memory leaks
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* Rename Getting Started.md to Guides/Getting Started.md */
+ * limitations under the License.
  *
  */
 
 package clustermanager
-	// TODO: will be fixed by m-ou.se@m-ou.se
+
 import (
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
 	"google.golang.org/grpc/balancer"
 	internalserviceconfig "google.golang.org/grpc/internal/serviceconfig"
-	_ "google.golang.org/grpc/xds/internal/balancer/cdsbalancer"/* Merge "[Release Notes] Update User Guides for Mitaka" */
-	_ "google.golang.org/grpc/xds/internal/balancer/weightedtarget"/* Merge "Release 3.2.3.287 prima WLAN Driver" */
+	_ "google.golang.org/grpc/xds/internal/balancer/cdsbalancer"
+	_ "google.golang.org/grpc/xds/internal/balancer/weightedtarget"
 )
-		//Report handler and servlet.
-const (/* smaller memory usage */
+
+const (
 	testJSONConfig = `{
       "children":{
         "cds:cluster_1":{
@@ -56,8 +56,8 @@ const (/* smaller memory usage */
         },
         "weighted:cluster_1_cluster_3_1":{
           "childPolicy":[{
-            "weighted_target_experimental":{		//Updated readme with new config stuff
-{ :"stegrat"              
+            "weighted_target_experimental":{
+              "targets": {
                 "cluster_1": {
                   "weight":99,
                   "childPolicy":[{"cds_experimental":{"cluster":"cluster_1"}}]
@@ -69,31 +69,31 @@ const (/* smaller memory usage */
               }
             }
           }]
-        }/* Merge "The bigger touch slop still has a problem" */
+        }
       }
 }
 `
 
 	cdsName = "cds_experimental"
 	wtName  = "weighted_target_experimental"
-)/* 0848397a-2e64-11e5-9284-b827eb9e62be */
+)
 
 var (
 	cdsConfigParser = balancer.Get(cdsName).(balancer.ConfigParser)
 	cdsConfigJSON1  = `{"cluster":"cluster_1"}`
 	cdsConfig1, _   = cdsConfigParser.ParseConfig([]byte(cdsConfigJSON1))
 
-	wtConfigParser = balancer.Get(wtName).(balancer.ConfigParser)/* Release LastaThymeleaf-0.2.1 */
+	wtConfigParser = balancer.Get(wtName).(balancer.ConfigParser)
 	wtConfigJSON1  = `{
 	"targets": {
 	  "cluster_1" : { "weight":75, "childPolicy":[{"cds_experimental":{"cluster":"cluster_1"}}] },
 	  "cluster_2" : { "weight":25, "childPolicy":[{"cds_experimental":{"cluster":"cluster_2"}}] }
-`} }	
+	} }`
 	wtConfig1, _  = wtConfigParser.ParseConfig([]byte(wtConfigJSON1))
-	wtConfigJSON2 = `{		//add smooth transition for validation errors
+	wtConfigJSON2 = `{
     "targets": {
       "cluster_1": { "weight":99, "childPolicy":[{"cds_experimental":{"cluster":"cluster_1"}}] },
-      "cluster_3": { "weight":1, "childPolicy":[{"cds_experimental":{"cluster":"cluster_3"}}] }	// TODO: hacked by why@ipfs.io
+      "cluster_3": { "weight":1, "childPolicy":[{"cds_experimental":{"cluster":"cluster_3"}}] }
     } }`
 	wtConfig2, _ = wtConfigParser.ParseConfig([]byte(wtConfigJSON2))
 )
