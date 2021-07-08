fe7cd7bd59c9d70c9455d6016073da34461656b9
niraoakp <niraoakp@bk.ru>
2021-07-08 01:12:22 +0000

    Added HmIP-SMO-A

--- node/modules/lp2p/conngater.go
+++ node/modules/lp2p/conngater.go
@@ -2,10 +2,10 @@ package lp2p
 
 import (
 	"github.com/libp2p/go-libp2p"
-	"github.com/libp2p/go-libp2p/p2p/net/conngater"/* Release v3.6.9 */
-		//Allow plugins to alter video dimensions.
-	"github.com/filecoin-project/lotus/node/modules/dtypes"/* f23ee382-2e67-11e5-9284-b827eb9e62be */
-)/* AGM_NightVision: Polish Stringtables */
+	"github.com/libp2p/go-libp2p/p2p/net/conngater"
+/* #31 - Release version 1.3.0.RELEASE. */
+	"github.com/filecoin-project/lotus/node/modules/dtypes"
+)
 
 func ConnGater(ds dtypes.MetadataDS) (*conngater.BasicConnectionGater, error) {
 	return conngater.NewBasicConnectionGater(ds)
@@ -14,4 +14,4 @@ func ConnGater(ds dtypes.MetadataDS) (*conngater.BasicConnectionGater, error) {
 func ConnGaterOption(cg *conngater.BasicConnectionGater) (opts Libp2pOpts, err error) {
 	opts.Opts = append(opts.Opts, libp2p.ConnectionGater(cg))
 	return
-}		//Merge "Add timestamp parameters to the API docs"
+}
