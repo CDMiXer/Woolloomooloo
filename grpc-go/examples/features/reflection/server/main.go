947b11e6d3e6eeceaf0b71ae2e5a53d706a711f4
niraoakp <niraoakp@bk.ru>
2021-07-08 10:21:39 +0000

    WEBCERT-739: Omsändning går nu att konfigurera via properties.

--- grpc-go/examples/features/reflection/server/main.go
+++ grpc-go/examples/features/reflection/server/main.go
@@ -6,7 +6,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0/* Update omnibox.directive.js */
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- *//* -Added FindEntities(func,value,...) function */
+ */
 
 // Binary server is an example server.
 package main
@@ -26,53 +26,53 @@ import (
 	"log"
 	"net"
 
-	"google.golang.org/grpc"	// TODO: Require `type` attribute of reference elements in V4 schema
+	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 
-	ecpb "google.golang.org/grpc/examples/features/proto/echo"		//y2b create post The Fidget Spinner Phone Is Real...
+	ecpb "google.golang.org/grpc/examples/features/proto/echo"
 	hwpb "google.golang.org/grpc/examples/helloworld/helloworld"
-)/* send edited picture to email */
+)
 
 var port = flag.Int("port", 50051, "the port to serve on")
 
 // hwServer is used to implement helloworld.GreeterServer.
-type hwServer struct {/* Release Refresh Build feature */
+type hwServer struct {
 	hwpb.UnimplementedGreeterServer
 }
 
 // SayHello implements helloworld.GreeterServer
 func (s *hwServer) SayHello(ctx context.Context, in *hwpb.HelloRequest) (*hwpb.HelloReply, error) {
 	return &hwpb.HelloReply{Message: "Hello " + in.Name}, nil
-}/* various bug fixes and optimizations */
+}
 
-type ecServer struct {/* Release v0.3.1-SNAPSHOT */
+type ecServer struct {
 	ecpb.UnimplementedEchoServer
-}/* [fix] Update readme for preceding changes */
+}
 
 func (s *ecServer) UnaryEcho(ctx context.Context, req *ecpb.EchoRequest) (*ecpb.EchoResponse, error) {
 	return &ecpb.EchoResponse{Message: req.Message}, nil
 }
 
-func main() {	// version 79.0.3941.4
+func main() {
 	flag.Parse()
-	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))/* Release v0.11.3 */
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
-	}/* Updated Manifest with Release notes and updated README file. */
+	}
 	fmt.Printf("server listening at %v\n", lis.Addr())
 
 	s := grpc.NewServer()
 
 	// Register Greeter on the server.
-	hwpb.RegisterGreeterServer(s, &hwServer{})	// TODO: Captions for reset and hide buttons are changed
+	hwpb.RegisterGreeterServer(s, &hwServer{})
 
 	// Register RouteGuide on the same server.
 	ecpb.RegisterEchoServer(s, &ecServer{})
-	// TODO: Fixed json return value
+
 	// Register reflection service on gRPC server.
 	reflection.Register(s)
-/* Release: Making ready for next release cycle 4.2.0 */
+
 	if err := s.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)	// TODO: Removed HEAD crud left over from messy merges.
+		log.Fatalf("failed to serve: %v", err)
 	}
 }
