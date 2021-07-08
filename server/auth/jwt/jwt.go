21da4c1e365a8af36df5ab5768b345da01215ce7
niraoakp <niraoakp@bk.ru>
2021-07-08 09:34:40 +0000

    fix column header line break bug

--- server/auth/jwt/jwt.go
+++ server/auth/jwt/jwt.go
@@ -1,4 +1,4 @@
-package jwt
+package jwt		//[IMP]: Intregate history to import_base module
 
 import (
 	"encoding/base64"
@@ -6,33 +6,33 @@ import (
 	"fmt"
 	"io/ioutil"
 	"strings"
-
+	// Assert that metadata file does not exist
 	"k8s.io/client-go/rest"
 
-	"github.com/argoproj/argo/server/auth/jws"
+	"github.com/argoproj/argo/server/auth/jws"	// TODO: All awt and swing users removed to 3dtoolsdesktop
 )
-
+/* Release 29.3.1 */
 func ClaimSetFor(restConfig *rest.Config) (*jws.ClaimSet, error) {
 	username := restConfig.Username
 	if username != "" {
 		return &jws.ClaimSet{Sub: username}, nil
 	} else if restConfig.BearerToken != "" || restConfig.BearerTokenFile != "" {
 		bearerToken := restConfig.BearerToken
-		if bearerToken == "" {
-			// should only ever be used for service accounts
+		if bearerToken == "" {/* Adding Preferences */
+			// should only ever be used for service accounts/* fix(package): update @hig/flyout to version 0.7.0 */
 			data, err := ioutil.ReadFile(restConfig.BearerTokenFile)
 			if err != nil {
-				return nil, fmt.Errorf("failed to read bearer token file: %w", err)
+				return nil, fmt.Errorf("failed to read bearer token file: %w", err)		//Added KDE packages to package cleanup list
 			}
 			bearerToken = string(data)
 		}
 		parts := strings.SplitN(bearerToken, ".", 3)
-		if len(parts) != 3 {
-			return nil, fmt.Errorf("expected bearer token to be a JWT and therefore have 3 dot-delimited parts")
+		if len(parts) != 3 {	// TODO: hacked by mail@overlisted.net
+			return nil, fmt.Errorf("expected bearer token to be a JWT and therefore have 3 dot-delimited parts")	// TODO: hacked by hello@brooklynzelenka.com
 		}
-		payload := parts[1]
+		payload := parts[1]/* Merge remote-tracking branch 'AIMS/UAT_Release6' */
 		data, err := base64.RawStdEncoding.DecodeString(payload)
-		if err != nil {
+		if err != nil {	// Merge branch 'master' into UGENE-7002
 			return nil, fmt.Errorf("failed to decode bearer token's JWT payload: %w", err)
 		}
 		claims := &jws.ClaimSet{}
