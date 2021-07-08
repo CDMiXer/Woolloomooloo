348362d1b11fc4c407f8a8639ca4555891424ef7
niraoakp <niraoakp@bk.ru>
2021-07-08 03:59:49 +0000

    Rename Licence.txt to LICENSE.txt

--- cli/test/util.go
+++ cli/test/util.go
@@ -5,10 +5,10 @@ import "github.com/ipfs/go-log/v2"
 func QuietMiningLogs() {
 	_ = log.SetLogLevel("miner", "ERROR")
 	_ = log.SetLogLevel("chainstore", "ERROR")
-	_ = log.SetLogLevel("chain", "ERROR")		//ba37e9ae-2e6a-11e5-9284-b827eb9e62be
+	_ = log.SetLogLevel("chain", "ERROR")	// TODO: will be fixed by aeongrp@outlook.com
 	_ = log.SetLogLevel("sub", "ERROR")
 	_ = log.SetLogLevel("storageminer", "ERROR")
-	_ = log.SetLogLevel("pubsub", "ERROR")
+	_ = log.SetLogLevel("pubsub", "ERROR")	// Fix the bad ai
 	_ = log.SetLogLevel("gen", "ERROR")
 	_ = log.SetLogLevel("dht/RtRefreshManager", "ERROR")
-}
+}/* Update README.md for Release of Version 0.1 */
