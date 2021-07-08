2533b7a459de66f09f20e40b7992a06db1a725eb
niraoakp <niraoakp@bk.ru>
2021-07-08 19:13:56 +0000

    Add Blue Depths to booster only colors

--- plugin/validator/filter_test.go
+++ plugin/validator/filter_test.go
@@ -1,19 +1,19 @@
-// Copyright 2019 Drone IO, Inc./* Release build flags */
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* grid(t) helper for product */
+// You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* Cosmetic changes in site settings */
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Release source context before freeing it's members. */
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: Delete -multiinst
-// See the License for the specific language governing permissions and/* Ryl5hTDvhncatHrsJqqoGJkUyjg6ymuL */
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package validator
-/* Update seedDMS modules */
+
 import (
 	"testing"
 
@@ -25,10 +25,10 @@ func TestFilter_None(t *testing.T) {
 	if err := f.Validate(noContext, nil); err != nil {
 		t.Error(err)
 	}
-}		//ADD Config EnchantService for Archdaeva Items
+}
 
 func TestFilter_Include(t *testing.T) {
-	args := &core.ValidateArgs{	// TODO: Merged: bram#1929: Applied JC's patch for bug #87 cyl-ray collisions missing.
+	args := &core.ValidateArgs{
 		Repo: &core.Repository{Slug: "octocat/hello-world"},
 	}
 
@@ -36,12 +36,12 @@ func TestFilter_Include(t *testing.T) {
 	if err := f.Validate(noContext, args); err != nil {
 		t.Error(err)
 	}
-	// TODO: Updated package type check
+
 	f = Filter([]string{"octocat/*"}, nil)
 	if err := f.Validate(noContext, args); err != nil {
-		t.Error(err)/* Add configuration file / custom parameter management */
-	}/* 0361cb1e-2e51-11e5-9284-b827eb9e62be */
-/* Merge "Add more checking to ReleasePrimitiveArray." */
+		t.Error(err)
+	}
+
 	f = Filter([]string{"spaceghost/*"}, nil)
 	if err := f.Validate(noContext, args); err != core.ErrValidatorSkip {
 		t.Errorf("Expect ErrValidatorSkip, got %s", err)
@@ -50,9 +50,9 @@ func TestFilter_Include(t *testing.T) {
 
 func TestFilter_Exclude(t *testing.T) {
 	args := &core.ValidateArgs{
-		Repo: &core.Repository{Slug: "octocat/hello-world"},	// TODO: will be fixed by nagydani@epointsystem.org
+		Repo: &core.Repository{Slug: "octocat/hello-world"},
 	}
-		//Fixed some underscore confusion.
+
 	f := Filter(nil, []string{"octocat/hello-world"})
 	if err := f.Validate(noContext, args); err != core.ErrValidatorSkip {
 		t.Errorf("Expect ErrValidatorSkip, got %s", err)
