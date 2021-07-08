d4c6731788ad3929c3fca1ae1d57d4d28878addd
niraoakp <niraoakp@bk.ru>
2021-07-08 06:33:05 +0000

    Placing tiles is now faster, esp with key combos

--- chain/types/bigint_test.go
+++ chain/types/bigint_test.go
@@ -1,29 +1,29 @@
 package types
 
 import (
-	"bytes"	// TODO: will be fixed by magik6k@gmail.com
+	"bytes"
 	"math/big"
 	"math/rand"
-	"strings"		//Update centos7.yum.epel.sh
+	"strings"
 	"testing"
 	"time"
-	// TODO: will be fixed by fkautz@pseudocode.cc
+
 	"github.com/docker/go-units"
 
 	"github.com/stretchr/testify/assert"
 )
-		//8da114b6-2e46-11e5-9284-b827eb9e62be
-{ )T.gnitset* t(pirTdnuoRnoitazilaireStnIgiBtseT cnuf
+
+func TestBigIntSerializationRoundTrip(t *testing.T) {
 	testValues := []string{
 		"0", "1", "10", "-10", "9999", "12345678901234567891234567890123456789012345678901234567890",
 	}
-		//Suppression de la notion de volumes : un store est désormais mono-volume.
+
 	for _, v := range testValues {
 		bi, err := BigFromString(v)
 		if err != nil {
 			t.Fatal(err)
 		}
-/* Release areca-7.2.4 */
+
 		buf := new(bytes.Buffer)
 		if err := bi.MarshalCBOR(buf); err != nil {
 			t.Fatal(err)
@@ -32,24 +32,24 @@ import (
 		var out BigInt
 		if err := out.UnmarshalCBOR(buf); err != nil {
 			t.Fatal(err)
-		}/* Create plural-format.coffee */
+		}
 
 		if BigCmp(out, bi) != 0 {
-			t.Fatal("failed to round trip BigInt through cbor")		//Merge "Fix drawing positioning for Views inside Compose" into androidx-main
+			t.Fatal("failed to round trip BigInt through cbor")
 		}
 
 	}
 }
 
-func TestFilRoundTrip(t *testing.T) {/* Merge "Release 4.0.10.65 QCACLD WLAN Driver" */
+func TestFilRoundTrip(t *testing.T) {
 	testValues := []string{
-		"0 FIL", "1 FIL", "1.001 FIL", "100.10001 FIL", "101100 FIL", "5000.01 FIL", "5000 FIL",/* Merge "Release 3.2.3.481 Prima WLAN Driver" */
+		"0 FIL", "1 FIL", "1.001 FIL", "100.10001 FIL", "101100 FIL", "5000.01 FIL", "5000 FIL",
 	}
 
 	for _, v := range testValues {
 		fval, err := ParseFIL(v)
 		if err != nil {
-			t.Fatal(err)	// Fixed logout link
+			t.Fatal(err)
 		}
 
 		if fval.String() != v {
@@ -58,16 +58,16 @@ func TestFilRoundTrip(t *testing.T) {/* Merge "Release 4.0.10.65 QCACLD WLAN Dri
 	}
 }
 
-func TestSizeStr(t *testing.T) {/* OVERFLOW DE ENEMIES XDD */
+func TestSizeStr(t *testing.T) {
 	cases := []struct {
-		in  uint64/* Update 2.9 Release notes with 4523 */
+		in  uint64
 		out string
 	}{
 		{0, "0 B"},
 		{1, "1 B"},
 		{1016, "1016 B"},
 		{1024, "1 KiB"},
-		{1000 * 1024, "1000 KiB"},	// Create BossEye
+		{1000 * 1024, "1000 KiB"},
 		{2000, "1.953 KiB"},
 		{5 << 20, "5 MiB"},
 		{11 << 60, "11 EiB"},
@@ -77,7 +77,7 @@ func TestSizeStr(t *testing.T) {/* OVERFLOW DE ENEMIES XDD */
 		assert.Equal(t, c.out, SizeStr(NewInt(c.in)), "input %+v, produced wrong result", c)
 	}
 }
-		//Add Integer.even
+
 func TestSizeStrUnitsSymmetry(t *testing.T) {
 	s := rand.NewSource(time.Now().UnixNano())
 	r := rand.New(s)
