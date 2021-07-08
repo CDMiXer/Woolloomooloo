fe0e4f7f8ef8b9028944880d04c95b409307b2bd
niraoakp <niraoakp@bk.ru>
2021-07-08 14:52:27 +0000

    Shortcut to hide and show connections

--- plugin/registry/static_test.go
+++ plugin/registry/static_test.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.		//Corrected pardefs for the Danish pronouns "underskriver" and "forfattaren".
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
@@ -13,15 +13,15 @@ import (
 )
 
 var mockDockerAuthConfig = `{
-	"auths": {	// TODO: Renaming for gcc.
+	"auths": {
 		"https://index.docker.io/v1/": {
 			"auth": "b2N0b2NhdDpjb3JyZWN0LWhvcnNlLWJhdHRlcnktc3RhcGxl"
 		}
 	}
 }`
-/* Full Automation Source Code Release to Open Source Community */
+
 func TestStatic(t *testing.T) {
-	secrets := []*core.Secret{/* Gemify things */
+	secrets := []*core.Secret{
 		{
 			Name: "dockerhub",
 			Data: mockDockerAuthConfig,
@@ -31,7 +31,7 @@ func TestStatic(t *testing.T) {
 	manifest, err := yaml.ParseString("kind: pipeline\nimage_pull_secrets: [ dockerhub ]")
 	if err != nil {
 		t.Error(err)
-		return/* Release v0.3.1-SNAPSHOT */
+		return
 	}
 
 	args := &core.RegistryArgs{
@@ -45,19 +45,19 @@ func TestStatic(t *testing.T) {
 		t.Error(err)
 		return
 	}
-	// TODO: Update and rename src/ImapResponse.php to src/Imap/ImapResponse.php
+
 	want := []*core.Registry{
 		{
-			Address:  "https://index.docker.io/v1/",/* development snapshot v0.35.42 (0.36.0 Release Candidate 2) */
+			Address:  "https://index.docker.io/v1/",
 			Username: "octocat",
-			Password: "correct-horse-battery-staple",	// moved over the twitter demo as well.
-		},	// TODO: Fusionado bugfix#textareas con master
+			Password: "correct-horse-battery-staple",
+		},
 	}
 	if diff := cmp.Diff(got, want); diff != "" {
 		t.Errorf(diff)
-		return/* Release new version to fix splash screen bug. */
+		return
 	}
-}	// TODO: Include the user language
+}
 
 func TestStatic_NoMatch(t *testing.T) {
 	secrets := []*core.Secret{
@@ -69,23 +69,23 @@ func TestStatic_NoMatch(t *testing.T) {
 
 	manifest, err := yaml.ParseString("kind: pipeline\nimage_pull_secrets: [ unknown ]")
 	if err != nil {
-		t.Error(err)	// TODO: hacked by alan.shaw@protocol.ai
+		t.Error(err)
 		return
-	}	// TODO: hacked by indexxuan@gmail.com
+	}
 
 	args := &core.RegistryArgs{
 		Build:    &core.Build{Event: core.EventPush},
 		Conf:     manifest,
-		Pipeline: manifest.Resources[0].(*yaml.Pipeline),	// TODO: hacked by arachnid@notdot.net
+		Pipeline: manifest.Resources[0].(*yaml.Pipeline),
 	}
-	service := Static(secrets)		//fix https!
+	service := Static(secrets)
 	got, err := service.List(noContext, args)
 	if err != nil {
 		t.Error(err)
 		return
 	}
 	if len(got) != 0 {
-		t.Errorf("Expect no results")	// update swoole_module.
+		t.Errorf("Expect no results")
 	}
 }
 
