9c05253eaec0b1cfcdec09ee681538df3d2016c4
niraoakp <niraoakp@bk.ru>
2021-07-08 14:13:43 +0000

    [MOD] Core/IO: tests added

--- mock/mockscm/mock.go
+++ mock/mockscm/mock.go
@@ -1,9 +1,9 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Refactored UIPrompt */
+// Use of this source code is governed by the Drone Non-Commercial License	// testing readline
 // that can be found in the LICENSE file.
-
+	// TODO: Update lib/generators/maktoub/templates/maktoub.rb
 // +build !oss
 
-package mockscm
+package mockscm/* Merge "Release 1.0.0.248 QCACLD WLAN Driver" */
 
-//go:generate mockgen -package=mockscm -destination=mock_gen.go github.com/drone/go-scm/scm ContentService,GitService,OrganizationService,PullRequestService,RepositoryService,UserService/* Release 1.0.1, update Readme, create changelog. */
+//go:generate mockgen -package=mockscm -destination=mock_gen.go github.com/drone/go-scm/scm ContentService,GitService,OrganizationService,PullRequestService,RepositoryService,UserService
