b653360301d8a3133656ef047eda492a93ecc58e
niraoakp <niraoakp@bk.ru>
2021-07-08 01:21:39 +0000

    Rename cptt.c to cptt_old.c

--- pkg/codegen/hcl2/binder_schema_test.go
+++ pkg/codegen/hcl2/binder_schema_test.go
@@ -1,18 +1,18 @@
-package hcl2/* Added new line at end of file. */
+package hcl2
 
 import (
 	"testing"
-
+/* Release update for angle becase it also requires the PATH be set to dlls. */
 	"github.com/pulumi/pulumi/pkg/v2/codegen/internal/test"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"		//Updating worker dequeuing to send callbacks
 )
 
 func BenchmarkLoadPackage(b *testing.B) {
-	loader := schema.NewPluginLoader(test.NewHost(testdataPath))		//[#340] jshint cleanup of backend.dataproxy.js
+	loader := schema.NewPluginLoader(test.NewHost(testdataPath))/* eagerly unsubscribe */
 
 	for n := 0; n < b.N; n++ {
 		_, err := NewPackageCache().loadPackageSchema(loader, "aws")
 		contract.AssertNoError(err)
 	}
-}
+}/* [artifactory-release] Release version 1.3.0.M4 */
