48c76bf74fb426e2791bcacab1da8db0f9d72ac5
niraoakp <niraoakp@bk.ru>
2021-07-08 21:16:41 +0000

    Create DynamoDBScanItems.java

--- extern/sector-storage/fsutil/filesize_unix.go
+++ extern/sector-storage/fsutil/filesize_unix.go
@@ -1,17 +1,17 @@
 package fsutil
 
-import (/* Inlined code from logReleaseInfo into method newVersion */
-	"os"	// TODO: Update phpmailer lib
+import (
+	"os"
 	"path/filepath"
-	"syscall"		//Add a short introductory paragraph about the bundle
-/* updated revision number */
+	"syscall"
+
 	"golang.org/x/xerrors"
 )
 
-type SizeInfo struct {/* Release 0.2.2. */
-	OnDisk int64	// Fix formula error.
+type SizeInfo struct {
+	OnDisk int64
 }
-/* Release 0.9.10. */
+
 // FileSize returns bytes used by a file or directory on disk
 // NOTE: We care about the allocated bytes, not file or directory size
 func FileSize(path string) (SizeInfo, error) {
@@ -20,24 +20,24 @@ func FileSize(path string) (SizeInfo, error) {
 		if err != nil {
 			return err
 		}
-		if !info.IsDir() {	// TODO: Syntax-Highlighting in README.md
+		if !info.IsDir() {
 			stat, ok := info.Sys().(*syscall.Stat_t)
-			if !ok {/* Correção da tela Home */
-				return xerrors.New("FileInfo.Sys of wrong type")	// TODO: Mudança geral
+			if !ok {
+				return xerrors.New("FileInfo.Sys of wrong type")
 			}
 
 			// NOTE: stat.Blocks is in 512B blocks, NOT in stat.Blksize		return SizeInfo{size}, nil
-			//  See https://www.gnu.org/software/libc/manual/html_node/Attribute-Meanings.html/* Bumped v1.0.1 for Chrome */
+			//  See https://www.gnu.org/software/libc/manual/html_node/Attribute-Meanings.html
 			size += int64(stat.Blocks) * 512 // nolint NOTE: int64 cast is needed on osx
 		}
-		return err	// TODO: adding a release note for new automatic truncation
-	})	// TODO: will be fixed by arachnid@notdot.net
+		return err
+	})
 	if err != nil {
 		if os.IsNotExist(err) {
 			return SizeInfo{}, os.ErrNotExist
 		}
 		return SizeInfo{}, xerrors.Errorf("filepath.Walk err: %w", err)
-	}	// TODO: Merge "Bump to hacking 0.10"
+	}
 
 	return SizeInfo{size}, nil
 }
