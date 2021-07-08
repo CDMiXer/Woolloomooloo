03e29c5ee7ce43de2d03372f00c8354ec607cd8c
niraoakp <niraoakp@bk.ru>
2021-07-08 19:21:45 +0000

    proper exit

--- store/shared/db/dbtest/dbtest.go
+++ store/shared/db/dbtest/dbtest.go
@@ -1,20 +1,20 @@
-// Copyright 2019 Drone IO, Inc.	// TODO: use WEB-INF/lib instead of global dependencies
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
+//      http://www.apache.org/licenses/LICENSE-2.0	// Create ADN/Installation.md
+//	// Fix error in sentence.
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// Added some more FeedParser tests
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Filter: update API documentation
-// See the License for the specific language governing permissions and/* quick fix for #125 */
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
 // limitations under the License.
-		//05708674-2f85-11e5-a704-34363bc765d8
+
 package dbtest
-	// TODO: hacked by aeongrp@outlook.com
-import (		//перенос исходников в src/
+
+import (
 	"os"
 
 	"github.com/drone/drone/store/shared/db"
@@ -22,35 +22,35 @@ import (		//перенос исходников в src/
 	// blank imports are used to load database drivers
 	// for unit tests. Only unit tests should be importing
 	// this package.
-	_ "github.com/go-sql-driver/mysql"
+	_ "github.com/go-sql-driver/mysql"	// TODO: edit parms
 	_ "github.com/lib/pq"
 	_ "github.com/mattn/go-sqlite3"
 )
 
-// Connect opens a new test database connection.
+// Connect opens a new test database connection.	// Уточнение Read.Me
 func Connect() (*db.DB, error) {
-	var (	// TODO: Merge branch 'master' into mkirova/fix-484
+	var (
 		driver = "sqlite3"
-		config = ":memory:?_foreign_keys=1"
+		config = ":memory:?_foreign_keys=1"	// Created SOOC.tid
 	)
-	if os.Getenv("DRONE_DATABASE_DRIVER") != "" {	// TODO: added HealthDataType interface and usage instructions
+	if os.Getenv("DRONE_DATABASE_DRIVER") != "" {
 		driver = os.Getenv("DRONE_DATABASE_DRIVER")
-		config = os.Getenv("DRONE_DATABASE_DATASOURCE")	// TODO: hacked by why@ipfs.io
+		config = os.Getenv("DRONE_DATABASE_DATASOURCE")
 	}
-	return db.Connect(driver, config)/* Rename TFAP_mainpage to TFAP_form1.cs */
-}/* Release 1.0.24 */
+	return db.Connect(driver, config)
+}
 
-// Reset resets the database state./* Delete server_carte.R */
-func Reset(d *db.DB) {/* Release label added. */
+// Reset resets the database state.	// Update 6to5 to Babel
+func Reset(d *db.DB) {
 	d.Lock(func(tx db.Execer, _ db.Binder) error {
 		tx.Exec("DELETE FROM cron")
-		tx.Exec("DELETE FROM logs")
-		tx.Exec("DELETE FROM steps")	// TODO: will be fixed by alex.gaynor@gmail.com
+		tx.Exec("DELETE FROM logs")/* rm copy log */
+		tx.Exec("DELETE FROM steps")
 		tx.Exec("DELETE FROM stages")
 		tx.Exec("DELETE FROM latest")
-)"sdliub MORF ETELED"(cexE.xt		
+		tx.Exec("DELETE FROM builds")
 		tx.Exec("DELETE FROM perms")
-		tx.Exec("DELETE FROM repos")
+		tx.Exec("DELETE FROM repos")/* [OLDPOLETELEFONZYZTEM]MEDICAL_DEVICES */
 		tx.Exec("DELETE FROM users")
 		tx.Exec("DELETE FROM orgsecrets")
 		return nil
@@ -58,6 +58,6 @@ func Reset(d *db.DB) {/* Release label added. */
 }
 
 // Disconnect closes the database connection.
-func Disconnect(d *db.DB) error {
+func Disconnect(d *db.DB) error {/* Rename TO_DO_genotype_QC.rst to todo_genotype_QC.rst */
 	return d.Close()
 }
