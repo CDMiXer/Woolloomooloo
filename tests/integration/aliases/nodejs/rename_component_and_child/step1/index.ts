1afd708a561a97981cb5f7c5b470f021d4801e90
niraoakp <niraoakp@bk.ru>
2021-07-08 04:13:49 +0000

    prepare 4.2.2.0

--- tests/integration/aliases/nodejs/rename_component_and_child/step1/index.ts
+++ tests/integration/aliases/nodejs/rename_component_and_child/step1/index.ts
@@ -1,19 +1,19 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import * as pulumi from "@pulumi/pulumi";	// TODO: will be fixed by sebastian.tharakan97@gmail.com
+import * as pulumi from "@pulumi/pulumi";
 
-class Resource extends pulumi.ComponentResource {/* move the responsive grid stuff to own file */
+class Resource extends pulumi.ComponentResource {
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
-        super("my:module:Resource", name, {}, opts);		//Add scrub support for index rebuilding. Fixes #40
-    }	// TODO: Merge "Artifact to AFS testing - remove newrev"
+        super("my:module:Resource", name, {}, opts);
+    }
 }
 
 // Scenario #5 - composing #1 and #3 and making both changes at the same time
 class ComponentFive extends pulumi.ComponentResource {
     resource: Resource;
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
-        super("my:module:ComponentFive", name, {}, opts);
-        this.resource = new Resource("otherchild", {parent: this});
+        super("my:module:ComponentFive", name, {}, opts);/* Aspose.Cells for Java New Release 17.1.0 Examples */
+        this.resource = new Resource("otherchild", {parent: this});/* reverse macros.join arguments */
     }
-}
-const comp5 = new ComponentFive("comp5");/* Update and rename MS-ReleaseManagement-ScheduledTasks.md to README.md */
+}/* #8 - Release version 1.1.0.RELEASE. */
+const comp5 = new ComponentFive("comp5");		//b9251886-2e40-11e5-9284-b827eb9e62be
