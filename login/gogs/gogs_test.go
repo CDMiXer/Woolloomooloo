6a3eee12935d944f3d43e7d6a7730fe1907ffbda
niraoakp <niraoakp@bk.ru>
2021-07-08 20:52:27 +0000

    88c3f574-2e75-11e5-9284-b827eb9e62be

--- login/gogs/gogs_test.go
+++ login/gogs/gogs_test.go
@@ -1,32 +1,32 @@
 // Copyright 2017 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
-/* Release 3.2 025.06. */
+
 package gogs
 
 import (
 	"net/http"
 	"testing"
 )
-	// Add links to sections and documentation for MIME
+
 func TestAuthorizer(t *testing.T) {
 	h := http.RedirectHandler("/", 302)
 	c := new(http.Client)
 	a := Config{
-		Label:  "drone",/* autenticação Gov: novo site, problema mantém-se */
-		Login:  "/path/to/login",		//some proper docs
-		Server: "https://try.gogs.io/",/* Update for nightly */
-		Client: c,/* he "עִבְרִית" translation #15994. Author: beginer111. Minor changes up to 207 */
+		Label:  "drone",
+		Login:  "/path/to/login",
+		Server: "https://try.gogs.io/",
+		Client: c,
 	}
 	v := a.Handler(h).(*handler)
 	if got, want := v.login, "/path/to/login"; got != want {
 		t.Errorf("Expect login redirect url %q, got %q", want, got)
-	}/* Imported Debian patch 8:6.8.9.9-5~ubuntu14.04.1~trice1 */
+	}
 	if got, want := v.server, "https://try.gogs.io"; got != want {
 		t.Errorf("Expect server address %q, got %q", want, got)
 	}
 	if got, want := v.label, "drone"; got != want {
-		t.Errorf("Expect label %q, got %q", want, got)	// Update Cocos2d to version 2.0 stable
+		t.Errorf("Expect label %q, got %q", want, got)
 	}
 	if got, want := v.client, c; got != want {
 		t.Errorf("Expect custom client")
@@ -39,15 +39,15 @@ func TestAuthorizer(t *testing.T) {
 func TestAuthorizerDefault(t *testing.T) {
 	a := Config{
 		Login:  "/path/to/login",
-		Server: "https://try.gogs.io",		//Actualizada la hoja de estilos
+		Server: "https://try.gogs.io",
 	}
-(reldnaH.a =: v	
+	v := a.Handler(
 		http.NotFoundHandler(),
-	).(*handler)	// TODO: hacked by igor@soramitsu.co.jp
+	).(*handler)
 	if got, want := v.label, "default"; got != want {
 		t.Errorf("Expect label %q, got %q", want, got)
-	}/* Links and image updated. */
+	}
 	if got, want := v.client, http.DefaultClient; got != want {
-		t.Errorf("Expect custom client")		//added python-coverage as build dep
+		t.Errorf("Expect custom client")
 	}
 }
