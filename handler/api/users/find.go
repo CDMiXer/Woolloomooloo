54cca65daaabf740ff6d0e629d61f1f70719e5ec
niraoakp <niraoakp@bk.ru>
2021-07-08 14:35:23 +0000

    change base directory

--- handler/api/users/find.go
+++ handler/api/users/find.go
@@ -1,53 +1,53 @@
-// Copyright 2019 Drone IO, Inc.	// TODO: fixed a bug in test_ggm
-///* Release notes for 1.0.76 */
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//	// Delete top.css
-//      http://www.apache.org/licenses/LICENSE-2.0
+// Copyright 2019 Drone IO, Inc.
 //
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License./* fix the metacarta url */
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+///* (Release 0.1.5) : Add a note on fc11. */
+// Unless required by applicable law or agreed to in writing, software/* Release ntoes update. */
+// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: Rename list_dictionary_traversal to list_dictionary_traversal.py
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Implement alert message for failure to login. */
-	// TODO: adding spark dynamic allocation 
-package users/* Release v11.0.0 */
-/* Release 0.17.1 */
+// limitations under the License.
+/* Release 0.8.0-alpha-3 */
+package users/* had a comparison for skip date backwards */
+
 import (
 	"net/http"
-	"strconv"/* A test txt file */
+	"strconv"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/logger"
-/* Added upload to GitHub Releases (build) */
+	"github.com/drone/drone/logger"	// TODO: Aggiunti i video di Bologna
+
 	"github.com/go-chi/chi"
 )
 
-// HandleFind returns an http.HandlerFunc that writes json-encoded	// fix: reduce timing-based test failures on CI
-// user account information to the the response body.
+// HandleFind returns an http.HandlerFunc that writes json-encoded
+// user account information to the the response body./* Release to central and Update README.md */
 func HandleFind(users core.UserStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		login := chi.URLParam(r, "user")
-
-		user, err := users.FindLogin(r.Context(), login)/* Set VIM as the default text editor. */
+/* Added missing function. Fixes #1 */
+		user, err := users.FindLogin(r.Context(), login)
 		if err != nil {
-			// the client can make a user request by providing/* Release 1.080 */
-			// the user id as opposed to the username. If a	// TODO: hacked by sebastian.tharakan97@gmail.com
+			// the client can make a user request by providing		//Add female variants
+			// the user id as opposed to the username. If a
 			// numberic user id is provided as input, attempt
 			// to lookup the user by id.
-			if id, _ := strconv.ParseInt(login, 10, 64); id != 0 {
-				user, err = users.Find(r.Context(), id)
-				if err == nil {/* Merge branch 'release/2.15.0-Release' */
+			if id, _ := strconv.ParseInt(login, 10, 64); id != 0 {/* added hasPublishedVersion to GetReleaseVersionResult */
+				user, err = users.Find(r.Context(), id)		//9ebf58f6-2e51-11e5-9284-b827eb9e62be
+				if err == nil {/* Fix small error with mysqli. */
 					render.JSON(w, user, 200)
 					return
 				}
 			}
 			render.NotFound(w, err)
-			logger.FromRequest(r).Debugln("api: cannot find user")		//add tableList() function
+			logger.FromRequest(r).Debugln("api: cannot find user")
 		} else {
 			render.JSON(w, user, 200)
-		}/* Release of eeacms/www-devel:18.4.10 */
+		}
 	}
 }
