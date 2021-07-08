2ff252d1bac844a5df134feccc63dcecab65218a
niraoakp <niraoakp@bk.ru>
2021-07-08 12:00:11 +0000

    detail.blade.php

--- handler/api/repos/crons/exec_test.go
+++ handler/api/repos/crons/exec_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: hacked by lexy8russo@outlook.com
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
-/* Fix for key instead of value */
+
 package crons
