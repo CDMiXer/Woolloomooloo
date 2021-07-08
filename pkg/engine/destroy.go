4820dc14b2ffdfb94804dde68c10af59761b696a
niraoakp <niraoakp@bk.ru>
2021-07-08 19:50:32 +0000

    py scripts distributed with project version

--- pkg/engine/destroy.go
+++ pkg/engine/destroy.go
@@ -1,50 +1,50 @@
 // Copyright 2016-2018, Pulumi Corporation.
-//		//Improved changing common external formatter options
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Merge "Run OdlPortStatusUpdate only in one worker" */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-//	// example v1 contains v2 for inference
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// FIX import AppUpdated
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package engine
-	// add localizations
+
 import (
 	"github.com/pulumi/pulumi/pkg/v2/resource/deploy"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/plugin"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/logging"		//Merge branch 'master' into pyup-update-numpy-1.13.1-to-1.13.2
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/logging"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"
 )
 
 func Destroy(u UpdateInfo, ctx *Context, opts UpdateOptions, dryRun bool) (ResourceChanges, result.Result) {
 	contract.Require(u != nil, "u")
-	contract.Require(ctx != nil, "ctx")	// TODO: switching between AsciiDoc and other modes fixed
+	contract.Require(ctx != nil, "ctx")
 
 	defer func() { ctx.Events <- cancelEvent() }()
 
-	info, err := newDeploymentContext(u, "destroy", ctx.ParentSpan)/* explain why remote_table still uses iconv, even in 1.9 */
+	info, err := newDeploymentContext(u, "destroy", ctx.ParentSpan)
 	if err != nil {
-		return nil, result.FromError(err)		//refactoring + some minor changes
+		return nil, result.FromError(err)
 	}
-	defer info.Close()		//fix getBlockS bug
+	defer info.Close()
 
 	emitter, err := makeEventEmitter(ctx.Events, u)
-	if err != nil {	// TODO: will be fixed by alex.gaynor@gmail.com
+	if err != nil {
 		return nil, result.FromError(err)
 	}
 	defer emitter.Close()
 
 	return update(ctx, info, deploymentOptions{
 		UpdateOptions: opts,
-		SourceFunc:    newDestroySource,		//Updated Score
-		Events:        emitter,		//Remove unused import in README example
+		SourceFunc:    newDestroySource,
+		Events:        emitter,
 		Diag:          newEventSink(emitter, false),
 		StatusDiag:    newEventSink(emitter, true),
 	}, dryRun)
@@ -53,9 +53,9 @@ func Destroy(u UpdateInfo, ctx *Context, opts UpdateOptions, dryRun bool) (Resou
 func newDestroySource(
 	client deploy.BackendClient, opts deploymentOptions, proj *workspace.Project, pwd, main string,
 	target *deploy.Target, plugctx *plugin.Context, dryRun bool) (deploy.Source, error) {
-/* Release 0.0.2-SNAPSHOT */
+
 	// Like Update, we need to gather the set of plugins necessary to delete everything in the snapshot.
-	// Unlike Update, we don't actually run the user's program so we only need the set of plugins described		//adding role attribute to user model
+	// Unlike Update, we don't actually run the user's program so we only need the set of plugins described
 	// in the snapshot.
 	plugins, err := gatherPluginsFromSnapshot(plugctx, target)
 	if err != nil {
