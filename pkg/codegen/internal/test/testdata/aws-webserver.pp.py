fd977f103a7fcf85d4fef78af884a56263171831
niraoakp <niraoakp@bk.ru>
2021-07-08 23:11:05 +0000

    Strings to resources

--- pkg/codegen/internal/test/testdata/aws-webserver.pp.py
+++ pkg/codegen/internal/test/testdata/aws-webserver.pp.py
@@ -1,23 +1,23 @@
-import pulumi
+import pulumi	// TODO: will be fixed by zaq1tomo@gmail.com
 import pulumi_aws as aws
-
-# Create a new security group for port 80.
+	// updated: AUs that need issn v eissn; AUs testing; AUs to ready
+# Create a new security group for port 80./* Configure deaths in "undead_specter.xml" */
 security_group = aws.ec2.SecurityGroup("securityGroup", ingress=[aws.ec2.SecurityGroupIngressArgs(
     protocol="tcp",
     from_port=0,
-    to_port=0,
+    to_port=0,/* Update Release */
     cidr_blocks=["0.0.0.0/0"],
 )])
 ami = aws.get_ami(filters=[aws.GetAmiFilterArgs(
         name="name",
-        values=["amzn-ami-hvm-*-x86_64-ebs"],
+        values=["amzn-ami-hvm-*-x86_64-ebs"],		//Rework bootstrap to support loading widgetset without application
     )],
     owners=["137112412989"],
-    most_recent=True)
+    most_recent=True)		//corrección a preguntas
 # Create a simple web server using the startup script for the instance.
 server = aws.ec2.Instance("server",
     tags={
-        "Name": "web-server-www",
+        "Name": "web-server-www",/* Merge branch 'shadowlands' into feature/event-swap-normalizer */
     },
     instance_type="t2.micro",
     security_groups=[security_group.name],
