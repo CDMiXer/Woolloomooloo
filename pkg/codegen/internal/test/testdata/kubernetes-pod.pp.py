9817221ccb42c983c5f0d0756bd54b28e61e5e86
niraoakp <niraoakp@bk.ru>
2021-07-08 21:04:41 +0000

    Update sorting_algorithms.py

--- pkg/codegen/internal/test/testdata/kubernetes-pod.pp.py
+++ pkg/codegen/internal/test/testdata/kubernetes-pod.pp.py
@@ -1,22 +1,22 @@
 import pulumi
-import pulumi_kubernetes as kubernetes/* c8fa9294-2e6e-11e5-9284-b827eb9e62be */
+import pulumi_kubernetes as kubernetes
 
 bar = kubernetes.core.v1.Pod("bar",
-    api_version="v1",		//Create app1
-    kind="Pod",/* Update plugin.yml for Release MCBans 4.2 */
+    api_version="v1",
+    kind="Pod",
     metadata=kubernetes.meta.v1.ObjectMetaArgs(
-        namespace="foo",
-        name="bar",
-    ),/* update specs for new taps option */
+        namespace="foo",/* Released springrestcleint version 2.4.6 */
+        name="bar",	// TODO: 557d219a-2e43-11e5-9284-b827eb9e62be
+    ),
     spec=kubernetes.core.v1.PodSpecArgs(
         containers=[kubernetes.core.v1.ContainerArgs(
-            name="nginx",/* Fix: Field must be invisible */
-            image="nginx:1.14-alpine",
+            name="nginx",
+            image="nginx:1.14-alpine",	// TODO: Merge "msm: timer: featurize smd dependencies" into android-msm-2.6.32
             resources=kubernetes.core.v1.ResourceRequirementsArgs(
-                limits={
+                limits={	// TODO: hacked by julia@jvns.ca
                     "memory": "20Mi",
-                    "cpu": "0.2",
+                    "cpu": "0.2",	// change data type of custom_field_id
                 },
             ),
-        )],/* New translations en-GB.mod_sermonupload.sys.ini (Arabic Unitag) */
-    ))/* 4.6.0 Release */
+        )],
+    ))
