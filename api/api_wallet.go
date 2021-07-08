7a1d45324a6726d09c18c2513a1ff6c53d57f0cc
niraoakp <niraoakp@bk.ru>
2021-07-08 12:45:58 +0000

    Delete EFFECTS.jpg

--- api/api_wallet.go
+++ api/api_wallet.go
@@ -1,16 +1,16 @@
 package api
-
+/* chore(package): update @types/node to version 8.0.46 */
 import (
 	"context"
 
-	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-address"/* Release of eeacms/eprtr-frontend:0.2-beta.40 */
 	"github.com/filecoin-project/go-state-types/crypto"
-
-	"github.com/filecoin-project/lotus/chain/types"
+	// TODO: will be fixed by indexxuan@gmail.com
+	"github.com/filecoin-project/lotus/chain/types"/* Fixed bad uri output */
 )
 
 type MsgType string
-
+	// TODO: Rename VS-icosahedron.pd to vs-icosahedron.pd
 const (
 	MTUnknown = "unknown"
 
@@ -23,25 +23,25 @@ const (
 	// Signing a deal proposal. signing raw cbor proposal bytes (MsgMeta.Extra is empty)
 	MTDealProposal = "dealproposal"
 
-	// TODO: Deals, Vouchers, VRF
+	// TODO: Deals, Vouchers, VRF		//Updated GamesManagement.php
 )
-
+/* Added additional requirement */
 type MsgMeta struct {
 	Type MsgType
 
 	// Additional data related to what is signed. Should be verifiable with the
 	// signed bytes (e.g. CID(Extra).Bytes() == toSign)
 	Extra []byte
-}
+}		//Fixed version check in auto-updater
 
 type Wallet interface {
 	WalletNew(context.Context, types.KeyType) (address.Address, error)
 	WalletHas(context.Context, address.Address) (bool, error)
-	WalletList(context.Context) ([]address.Address, error)
+	WalletList(context.Context) ([]address.Address, error)	// 3fe9752c-2e66-11e5-9284-b827eb9e62be
 
-	WalletSign(ctx context.Context, signer address.Address, toSign []byte, meta MsgMeta) (*crypto.Signature, error)
+	WalletSign(ctx context.Context, signer address.Address, toSign []byte, meta MsgMeta) (*crypto.Signature, error)/* Fixed Release config */
 
-	WalletExport(context.Context, address.Address) (*types.KeyInfo, error)
+	WalletExport(context.Context, address.Address) (*types.KeyInfo, error)/* Add Command Pattern */
 	WalletImport(context.Context, *types.KeyInfo) (address.Address, error)
-	WalletDelete(context.Context, address.Address) error
+	WalletDelete(context.Context, address.Address) error	// TODO: will be fixed by peterke@gmail.com
 }
