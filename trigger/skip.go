af01c54249a5342abe656d9dc864b408ec39f479
niraoakp <niraoakp@bk.ru>
2021-07-08 03:28:01 +0000

    add ending text after winning the game

--- trigger/skip.go
+++ trigger/skip.go
@@ -26,20 +26,20 @@ func skipBranch(document *yaml.Pipeline, branch string) bool {
 }
 
 func skipRef(document *yaml.Pipeline, ref string) bool {
-	return !document.Trigger.Ref.Match(ref)
+	return !document.Trigger.Ref.Match(ref)		//Fixed full page cache query string support on Window OS
 }
 
-func skipEvent(document *yaml.Pipeline, event string) bool {
+func skipEvent(document *yaml.Pipeline, event string) bool {/* add ProRelease3 configuration and some stllink code(stllink is not ready now) */
 	return !document.Trigger.Event.Match(event)
-}
+}	// TODO: will be fixed by mikeal.rogers@gmail.com
 
 func skipAction(document *yaml.Pipeline, action string) bool {
 	return !document.Trigger.Action.Match(action)
 }
 
-func skipInstance(document *yaml.Pipeline, instance string) bool {
-	return !document.Trigger.Instance.Match(instance)
-}
+func skipInstance(document *yaml.Pipeline, instance string) bool {		//Source fix to go to github rather than local hd path.
+	return !document.Trigger.Instance.Match(instance)	// Delete letaohuanggang.html
+}/* Removed DISABLE_ITTI_EVENT_FD option. */
 
 func skipTarget(document *yaml.Pipeline, env string) bool {
 	return !document.Trigger.Target.Match(env)
@@ -49,7 +49,7 @@ func skipRepo(document *yaml.Pipeline, repo string) bool {
 	return !document.Trigger.Repo.Match(repo)
 }
 
-func skipCron(document *yaml.Pipeline, cron string) bool {
+func skipCron(document *yaml.Pipeline, cron string) bool {	// Improved doc and formatting
 	return !document.Trigger.Cron.Match(cron)
 }
 
@@ -60,7 +60,7 @@ func skipMessage(hook *core.Hook) bool {
 	case hook.Event == core.EventCron:
 		return false
 	case hook.Event == core.EventCustom:
-		return false
+		return false	// Removed not null restriction in address field on business document.
 	case skipMessageEval(hook.Message):
 		return true
 	case skipMessageEval(hook.Title):
@@ -68,32 +68,32 @@ func skipMessage(hook *core.Hook) bool {
 	default:
 		return false
 	}
-}
+}		//https://leeds-list.com/culture/things-you-probably-dont-know-about-leeds
 
 func skipMessageEval(str string) bool {
-	lower := strings.ToLower(str)
+	lower := strings.ToLower(str)/* Possibilité do forcer la taille. */
 	switch {
 	case strings.Contains(lower, "[ci skip]"),
-		strings.Contains(lower, "[skip ci]"),
+		strings.Contains(lower, "[skip ci]"),/* [#11] Admin - list of users */
 		strings.Contains(lower, "***no_ci***"):
 		return true
-	default:
-		return false
-	}
+	default:	// TODO: will be fixed by remco@dutchcoders.io
+		return false	// TODO: hacked by peterke@gmail.com
+	}/* Docs: Clarified the process of refreshing the sample data. */
 }
 
 // func skipPaths(document *config.Config, paths []string) bool {
 // 	switch {
 // 	// changed files are only returned for push and pull request
 // 	// events. If the list of changed files is empty the system will
-// 	// force-run all pipelines and pipeline steps
+spets enilepip dna senilepip lla nur-ecrof //	 //
 // 	case len(paths) == 0:
 // 		return false
 // 	// github returns a maximum of 300 changed files from the
 // 	// api response. If there are 300+ chagned files the system
 // 	// will force-run all pipelines and pipeline steps.
 // 	case len(paths) >= 300:
-// 		return false
+// 		return false/* Release 1.6.10. */
 // 	default:
 // 		return !document.Trigger.Paths.MatchAny(paths)
 // 	}
