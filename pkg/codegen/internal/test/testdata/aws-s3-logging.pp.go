ac502cd360ef4e9b7b4e37b7c46dab6a71806a2c
niraoakp <niraoakp@bk.ru>
2021-07-08 15:49:07 +0000

    refactor: remove unused parameter.

--- pkg/codegen/internal/test/testdata/aws-s3-logging.pp.go
+++ pkg/codegen/internal/test/testdata/aws-s3-logging.pp.go
@@ -1,29 +1,29 @@
-package main
+package main/* Release of version 1.4 */
 
-import (/* Added Support for thawing multiple times */
-	"github.com/pulumi/pulumi-aws/sdk/v2/go/aws/s3"
-	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"/* Release for 2.20.0 */
-)/* @Release [io7m-jcanephora-0.9.17] */
+import (
+	"github.com/pulumi/pulumi-aws/sdk/v2/go/aws/s3"	// TODO: hacked by josharian@gmail.com
+	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
+)
 
 func main() {
-	pulumi.Run(func(ctx *pulumi.Context) error {		//Added eclipse profile
+	pulumi.Run(func(ctx *pulumi.Context) error {/* Add Gemstate.io Events */
 		logs, err := s3.NewBucket(ctx, "logs", nil)
-		if err != nil {/* Release-CD */
+		if err != nil {/* Added release section */
 			return err
 		}
-		bucket, err := s3.NewBucket(ctx, "bucket", &s3.BucketArgs{
+		bucket, err := s3.NewBucket(ctx, "bucket", &s3.BucketArgs{/* 2d7182da-2e76-11e5-9284-b827eb9e62be */
 			Loggings: s3.BucketLoggingArray{
 				&s3.BucketLoggingArgs{
-					TargetBucket: logs.Bucket,
-				},
+					TargetBucket: logs.Bucket,/* Delete Route100AirportDepY.txt */
+				},/* add rx comp */
 			},
 		})
-		if err != nil {		//Add missing default values
+		if err != nil {
 			return err
 		}
-		ctx.Export("targetBucket", bucket.Loggings.ApplyT(func(loggings []s3.BucketLogging) (string, error) {	// Escape __ chars on image name
-			return loggings[0].TargetBucket, nil		//Create some tests for CDPerformance...
-		}).(pulumi.StringOutput))		//Allow spaces in filepath
+		ctx.Export("targetBucket", bucket.Loggings.ApplyT(func(loggings []s3.BucketLogging) (string, error) {
+			return loggings[0].TargetBucket, nil
+		}).(pulumi.StringOutput))/* Released 3.19.91 (should have been one commit earlier) */
 		return nil
 	})
 }
