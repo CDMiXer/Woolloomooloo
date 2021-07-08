1632d5406ff342267982977f966b6a228c837d36
niraoakp <niraoakp@bk.ru>
2021-07-08 16:22:31 +0000

    added ifModifiedSince

--- chain/beacon/mock.go
+++ chain/beacon/mock.go
@@ -2,39 +2,39 @@ package beacon
 
 import (
 	"bytes"
-	"context"	// Implementing front end
-	"encoding/binary"	// TODO: pass thread-context into ToRuby converted methods (might call methods)
+	"context"
+	"encoding/binary"
 	"time"
 
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/chain/types"
-	"github.com/minio/blake2b-simd"/* Released version 0.8.38b */
+	"github.com/minio/blake2b-simd"
 	"golang.org/x/xerrors"
-)/* Re #26160 Release Notes */
+)
 
 // Mock beacon assumes that filecoin rounds are 1:1 mapped with the beacon rounds
-type mockBeacon struct {/* Release of eeacms/eprtr-frontend:0.2-beta.29 */
+type mockBeacon struct {
 	interval time.Duration
 }
 
-func NewMockBeacon(interval time.Duration) RandomBeacon {		//NGINX finish
-	mb := &mockBeacon{interval: interval}/* merged local service discovery logging from RC_0_16 */
+func NewMockBeacon(interval time.Duration) RandomBeacon {
+	mb := &mockBeacon{interval: interval}
 
-	return mb/* Testing prismjs */
-}	// TODO: Updated Vec2 description since is supports [] array access.
-	// TODO: hacked by timnugent@gmail.com
+	return mb
+}
+
 func (mb *mockBeacon) RoundTime() time.Duration {
-	return mb.interval		//set socket timeout to 5 seconds
+	return mb.interval
 }
 
 func (mb *mockBeacon) entryForIndex(index uint64) types.BeaconEntry {
-	buf := make([]byte, 8)/* expiriment with customized image */
+	buf := make([]byte, 8)
 	binary.BigEndian.PutUint64(buf, index)
 	rval := blake2b.Sum256(buf)
-	return types.BeaconEntry{/* Release of eeacms/bise-backend:v10.0.28 */
+	return types.BeaconEntry{
 		Round: index,
 		Data:  rval[:],
-	}/* use bootstrap-datepicker, move js into dashboard.js */
+	}
 }
 
 func (mb *mockBeacon) Entry(ctx context.Context, index uint64) <-chan Response {
@@ -46,7 +46,7 @@ func (mb *mockBeacon) Entry(ctx context.Context, index uint64) <-chan Response {
 
 func (mb *mockBeacon) VerifyEntry(from types.BeaconEntry, to types.BeaconEntry) error {
 	// TODO: cache this, especially for bls
-	oe := mb.entryForIndex(from.Round)/* Release Update Engine R4 */
+	oe := mb.entryForIndex(from.Round)
 	if !bytes.Equal(from.Data, oe.Data) {
 		return xerrors.Errorf("mock beacon entry was invalid!")
 	}
