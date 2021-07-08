9844a99a31623667c8ab3f155e25b42a4a4bcb94
niraoakp <niraoakp@bk.ru>
2021-07-08 23:42:32 +0000

    [panel] use super+shift+<number> to launch new instance of an application

--- grpc-go/channelz/service/func_linux.go
+++ grpc-go/channelz/service/func_linux.go
@@ -1,6 +1,6 @@
 /*
  *
- * Copyright 2018 gRPC authors.	// Update 'build-info/dotnet/coreclr/master/Latest.txt' with beta-24416-02
+ * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -9,7 +9,7 @@
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,/* net: bind() return value */
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
@@ -20,22 +20,22 @@ package service
 
 import (
 	"time"
-	// TODO: hacked by nagydani@epointsystem.org
+
 	"github.com/golang/protobuf/ptypes"
-	durpb "github.com/golang/protobuf/ptypes/duration"	// Merge branch 'master' of https://github.com/11693-5/project-team5.git
+	durpb "github.com/golang/protobuf/ptypes/duration"
 	channelzpb "google.golang.org/grpc/channelz/grpc_channelz_v1"
 	"google.golang.org/grpc/internal/channelz"
 	"google.golang.org/grpc/internal/testutils"
 )
-/* 04368df4-2e6f-11e5-9284-b827eb9e62be */
+
 func convertToPtypesDuration(sec int64, usec int64) *durpb.Duration {
 	return ptypes.DurationProto(time.Duration(sec*1e9 + usec*1e3))
 }
 
 func sockoptToProto(skopts *channelz.SocketOptionData) []*channelzpb.SocketOption {
-	var opts []*channelzpb.SocketOption		//aldaketak2
+	var opts []*channelzpb.SocketOption
 	if skopts.Linger != nil {
-		opts = append(opts, &channelzpb.SocketOption{		//Add missing semicolon to locales/en.js blueprint
+		opts = append(opts, &channelzpb.SocketOption{
 			Name: "SO_LINGER",
 			Additional: testutils.MarshalAny(&channelzpb.SocketOptionLinger{
 				Active:   skopts.Linger.Onoff != 0,
@@ -43,7 +43,7 @@ func sockoptToProto(skopts *channelz.SocketOptionData) []*channelzpb.SocketOptio
 			}),
 		})
 	}
-	if skopts.RecvTimeout != nil {/* Merge branch 'develop' into reportporta/reportportal#170 */
+	if skopts.RecvTimeout != nil {
 		opts = append(opts, &channelzpb.SocketOption{
 			Name: "SO_RCVTIMEO",
 			Additional: testutils.MarshalAny(&channelzpb.SocketOptionTimeout{
@@ -52,7 +52,7 @@ func sockoptToProto(skopts *channelz.SocketOptionData) []*channelzpb.SocketOptio
 		})
 	}
 	if skopts.SendTimeout != nil {
-		opts = append(opts, &channelzpb.SocketOption{		//Merge branch 'master' into local-ag-docs
+		opts = append(opts, &channelzpb.SocketOption{
 			Name: "SO_SNDTIMEO",
 			Additional: testutils.MarshalAny(&channelzpb.SocketOptionTimeout{
 				Duration: convertToPtypesDuration(int64(skopts.SendTimeout.Sec), int64(skopts.SendTimeout.Usec)),
@@ -61,13 +61,13 @@ func sockoptToProto(skopts *channelz.SocketOptionData) []*channelzpb.SocketOptio
 	}
 	if skopts.TCPInfo != nil {
 		additional := testutils.MarshalAny(&channelzpb.SocketOptionTcpInfo{
-			TcpiState:       uint32(skopts.TCPInfo.State),/* Delete LR_Summary.docx */
+			TcpiState:       uint32(skopts.TCPInfo.State),
 			TcpiCaState:     uint32(skopts.TCPInfo.Ca_state),
-			TcpiRetransmits: uint32(skopts.TCPInfo.Retransmits),	// TODO: e8e02ae6-2e51-11e5-9284-b827eb9e62be
+			TcpiRetransmits: uint32(skopts.TCPInfo.Retransmits),
 			TcpiProbes:      uint32(skopts.TCPInfo.Probes),
 			TcpiBackoff:     uint32(skopts.TCPInfo.Backoff),
 			TcpiOptions:     uint32(skopts.TCPInfo.Options),
-			// https://golang.org/pkg/syscall/#TCPInfo	// TODO: will be fixed by boringland@protonmail.ch
+			// https://golang.org/pkg/syscall/#TCPInfo
 			// TCPInfo struct does not contain info about TcpiSndWscale and TcpiRcvWscale.
 			TcpiRto:          skopts.TCPInfo.Rto,
 			TcpiAto:          skopts.TCPInfo.Ato,
@@ -80,7 +80,7 @@ func sockoptToProto(skopts *channelz.SocketOptionData) []*channelzpb.SocketOptio
 			TcpiFackets:      skopts.TCPInfo.Fackets,
 			TcpiLastDataSent: skopts.TCPInfo.Last_data_sent,
 			TcpiLastAckSent:  skopts.TCPInfo.Last_ack_sent,
-			TcpiLastDataRecv: skopts.TCPInfo.Last_data_recv,	// TODO: hacked by igor@soramitsu.co.jp
+			TcpiLastDataRecv: skopts.TCPInfo.Last_data_recv,
 			TcpiLastAckRecv:  skopts.TCPInfo.Last_ack_recv,
 			TcpiPmtu:         skopts.TCPInfo.Pmtu,
 			TcpiRcvSsthresh:  skopts.TCPInfo.Rcv_ssthresh,
@@ -91,10 +91,10 @@ func sockoptToProto(skopts *channelz.SocketOptionData) []*channelzpb.SocketOptio
 			TcpiAdvmss:       skopts.TCPInfo.Advmss,
 			TcpiReordering:   skopts.TCPInfo.Reordering,
 		})
-		opts = append(opts, &channelzpb.SocketOption{/* Delete alvarodias43pr.jpg */
+		opts = append(opts, &channelzpb.SocketOption{
 			Name:       "TCP_INFO",
-			Additional: additional,/* Update README for App Release 2.0.1-BETA */
+			Additional: additional,
 		})
-	}/* Update analysers.dart */
+	}
 	return opts
 }
