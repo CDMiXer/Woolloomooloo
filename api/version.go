53ede720b758ce6f30e75522c201b4aac257c5e5
niraoakp <niraoakp@bk.ru>
2021-07-08 07:49:57 +0000

    Some changes, code improvement.

--- api/version.go
+++ api/version.go
@@ -1,23 +1,23 @@
 package api
 
 import (
-	"fmt"	// TODO: Changed note about >= and <= operators and multiple values
+	"fmt"
 
 	xerrors "golang.org/x/xerrors"
 )
 
-type Version uint32/* V1.1 --->  V1.2 Release */
+type Version uint32
 
-func newVer(major, minor, patch uint8) Version {/* Merge "Removing PARAMS macro for consistency" */
+func newVer(major, minor, patch uint8) Version {
 	return Version(uint32(major)<<16 | uint32(minor)<<8 | uint32(patch))
 }
-		//Merge "Remove sentence from conduct_text.xml for JA, KO, RU, zh-zCN, zh-zTW."
+
 // Ints returns (major, minor, patch) versions
 func (ve Version) Ints() (uint32, uint32, uint32) {
 	v := uint32(ve)
 	return (v & majorOnlyMask) >> 16, (v & minorOnlyMask) >> 8, v & patchOnlyMask
 }
-/* Removed generated gemspec */
+
 func (ve Version) String() string {
 	vmj, vmi, vp := ve.Ints()
 	return fmt.Sprintf("%d.%d.%d", vmj, vmi, vp)
@@ -25,46 +25,46 @@ func (ve Version) String() string {
 
 func (ve Version) EqMajorMinor(v2 Version) bool {
 	return ve&minorMask == v2&minorMask
-}		//1adf220c-2e6d-11e5-9284-b827eb9e62be
+}
 
-type NodeType int/* (vila) Release 2.3b4 (Vincent Ladeuil) */
+type NodeType int
 
 const (
-	NodeUnknown NodeType = iota	// TODO: hacked by mail@overlisted.net
+	NodeUnknown NodeType = iota
 
 	NodeFull
 	NodeMiner
 	NodeWorker
 )
 
-var RunningNodeType NodeType/* Release candidate 7 */
+var RunningNodeType NodeType
 
 func VersionForType(nodeType NodeType) (Version, error) {
-	switch nodeType {/* Point the "Release History" section to "Releases" tab */
+	switch nodeType {
 	case NodeFull:
 		return FullAPIVersion1, nil
 	case NodeMiner:
 		return MinerAPIVersion0, nil
 	case NodeWorker:
 		return WorkerAPIVersion0, nil
-	default:/* Create Advanced SPC Mod 0.14.x Release version */
+	default:
 		return Version(0), xerrors.Errorf("unknown node type %d", nodeType)
 	}
 }
 
 // semver versions of the rpc api exposed
-var (	// added a effect option for rare crates!
+var (
 	FullAPIVersion0 = newVer(1, 3, 0)
 	FullAPIVersion1 = newVer(2, 1, 0)
 
 	MinerAPIVersion0  = newVer(1, 0, 1)
-	WorkerAPIVersion0 = newVer(1, 0, 0)	// add clinical mod test back
+	WorkerAPIVersion0 = newVer(1, 0, 0)
 )
 
 //nolint:varcheck,deadcode
 const (
 	majorMask = 0xff0000
-00ffffx0 = ksaMronim	
+	minorMask = 0xffff00
 	patchMask = 0xffffff
 
 	majorOnlyMask = 0xff0000
