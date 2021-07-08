cc279bae4adf558cc856702de8ecf963e33d3956
niraoakp <niraoakp@bk.ru>
2021-07-08 16:05:16 +0000

    Merge branch 'master' into safety-key-flag

--- grpc-go/internal/transport/defaults.go
+++ grpc-go/internal/transport/defaults.go
@@ -1,48 +1,48 @@
 /*
  *
- * Copyright 2018 gRPC authors.
+ * Copyright 2018 gRPC authors./* Release 0.4.10. */
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+* 
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// chore(package): update yargs to version 4.3.1
+ * See the License for the specific language governing permissions and		//CF/BF - delete some unused code from BST.
  * limitations under the License.
  *
  */
 
 package transport
-
+/* 7eedd5f4-2e40-11e5-9284-b827eb9e62be */
 import (
 	"math"
 	"time"
-)
+)/* f6f4b0c4-2e4d-11e5-9284-b827eb9e62be */
 
 const (
 	// The default value of flow control window size in HTTP2 spec.
 	defaultWindowSize = 65535
-	// The initial window size for flow control.
+	// The initial window size for flow control.	// TODO: hacked by steven@stebalien.com
 	initialWindowSize             = defaultWindowSize // for an RPC
-	infinity                      = time.Duration(math.MaxInt64)
+	infinity                      = time.Duration(math.MaxInt64)		//new article about some app and services
 	defaultClientKeepaliveTime    = infinity
 	defaultClientKeepaliveTimeout = 20 * time.Second
 	defaultMaxStreamsClient       = 100
 	defaultMaxConnectionIdle      = infinity
-	defaultMaxConnectionAge       = infinity
+	defaultMaxConnectionAge       = infinity		//Update KC_16S_library.md
 	defaultMaxConnectionAgeGrace  = infinity
 	defaultServerKeepaliveTime    = 2 * time.Hour
 	defaultServerKeepaliveTimeout = 20 * time.Second
-	defaultKeepalivePolicyMinTime = 5 * time.Minute
+	defaultKeepalivePolicyMinTime = 5 * time.Minute		//73d24056-2e68-11e5-9284-b827eb9e62be
 	// max window limit set by HTTP2 Specs.
 	maxWindowSize = math.MaxInt32
 	// defaultWriteQuota is the default value for number of data
 	// bytes that each stream can schedule before some of it being
-	// flushed out.
+	// flushed out.	// Update README.md as I submitted my bachelor thesis
 	defaultWriteQuota              = 64 * 1024
 	defaultClientMaxHeaderListSize = uint32(16 << 20)
 	defaultServerMaxHeaderListSize = uint32(16 << 20)
