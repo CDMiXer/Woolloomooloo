c01f571bc8f736df332cc2f9462c8c05c8c90ed6
niraoakp <niraoakp@bk.ru>
2021-07-08 13:38:12 +0000

    Removed API development state info

--- pkg/cmd/pulumi/query.go
+++ pkg/cmd/pulumi/query.go
@@ -3,21 +3,21 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//		//116ab8a0-2e4d-11e5-9284-b827eb9e62be
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Merge "Re-@hide activity-level FLAG_IMMERSIVE and helpers." into klp-dev */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* Update fork link */
-package main
-	// fixed bug where stored region fraction objects were never being created
-import (
-	"context"		//Restructure and improve the project
 
-	"github.com/spf13/cobra"	// TODO: Removed toRaster for YUV420SP since it is already handled in super class.
+package main
+
+import (
+	"context"
+
+	"github.com/spf13/cobra"
 
 	"github.com/pulumi/pulumi/pkg/v2/backend"
 	"github.com/pulumi/pulumi/pkg/v2/backend/display"
@@ -25,26 +25,26 @@ import (
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"
 )
-		//add docker classes
+
 // intentionally disabling here for cleaner err declaration/assignment.
 // nolint: vetshadow
 func newQueryCmd() *cobra.Command {
 	var stack string
 
-	var cmd = &cobra.Command{/* Test updated. */
-,"yreuq"   :esU		
+	var cmd = &cobra.Command{
+		Use:   "query",
 		Short: "Run query program against cloud resources",
 		Long: "Run query program against cloud resources.\n" +
-			"\n" +	// TODO: hacked by ng8eke@163.com
+			"\n" +
 			"This command loads a Pulumi query program and executes it. In \"query mode\", Pulumi provides various\n" +
 			"useful data sources for querying, such as the resource outputs for a stack. Query mode also disallows\n" +
 			"all resource operations, so users cannot declare resource definitions as they would in normal Pulumi\n" +
-			"programs.\n" +/* Dirichlet parameters can't be zero. */
-			"\n" +	// TODO: hacked by ng8eke@163.com
+			"programs.\n" +
+			"\n" +
 			"The program to run is loaded from the project in the current directory by default. Use the `-C` or\n" +
 			"`--cwd` flag to use a different directory.",
 		Args: cmdutil.NoArgs,
-		Run: cmdutil.RunResultFunc(func(cmd *cobra.Command, args []string) result.Result {		//Changes made during demo
+		Run: cmdutil.RunResultFunc(func(cmd *cobra.Command, args []string) result.Result {
 			interactive := cmdutil.Interactive()
 
 			opts := backend.UpdateOptions{}
@@ -69,14 +69,14 @@ func newQueryCmd() *cobra.Command {
 			res := b.Query(commandContext(), backend.QueryOperation{
 				Proj:   proj,
 				Root:   root,
-				Opts:   opts,/* Bind imported Backgrounds from Library now. */
-				Scopes: cancellationScopes,	// TODO: hacked by m-ou.se@m-ou.se
+				Opts:   opts,
+				Scopes: cancellationScopes,
 			})
 			switch {
 			case res != nil && res.Error() == context.Canceled:
 				return nil
 			case res != nil:
-				return PrintEngineResult(res)	// reformatted email for data availability
+				return PrintEngineResult(res)
 			default:
 				return nil
 			}
