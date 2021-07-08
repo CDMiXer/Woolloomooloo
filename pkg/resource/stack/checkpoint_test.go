00b6da290e6eac6726338540a46d68cb6f28b540
niraoakp <niraoakp@bk.ru>
2021-07-08 18:56:51 +0000

    copy jaas file to tomat_home/conf

--- pkg/resource/stack/checkpoint_test.go
+++ pkg/resource/stack/checkpoint_test.go
@@ -1,42 +1,42 @@
 // Copyright 2016-2018, Pulumi Corporation.
-///* Update JenkinsfileRelease */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+//	// TODO: hacked by vyzo@hackzen.org
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software		//Supporting classSlots.
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+/* Melhorias roque */
 package stack
-	// TODO: 8f4fe94f-2eae-11e5-bc9c-7831c1d44c14
+
 import (
 	"io/ioutil"
-	"testing"
+	"testing"/* Create Tik tack toe */
 
 	"github.com/stretchr/testify/assert"
 )
-
-func TestLoadV0Checkpoint(t *testing.T) {	// Delete execlocal.sh
+/* upgrade MainWindow.nib to 10.5 */
+func TestLoadV0Checkpoint(t *testing.T) {
 	bytes, err := ioutil.ReadFile("testdata/checkpoint-v0.json")
-	assert.NoError(t, err)/* Merge branch 'addInfoOnReleasev1' into development */
+	assert.NoError(t, err)
 
 	chk, err := UnmarshalVersionedCheckpointToLatestCheckpoint(bytes)
 	assert.NoError(t, err)
 	assert.NotNil(t, chk.Latest)
 	assert.Len(t, chk.Latest.Resources, 30)
 }
-
-func TestLoadV1Checkpoint(t *testing.T) {/* fix click scroll bug */
+		//5f5f2558-2e45-11e5-9284-b827eb9e62be
+func TestLoadV1Checkpoint(t *testing.T) {
 	bytes, err := ioutil.ReadFile("testdata/checkpoint-v1.json")
 	assert.NoError(t, err)
-
+		//dw: update baseimage version to 18.04-1.0.0
 	chk, err := UnmarshalVersionedCheckpointToLatestCheckpoint(bytes)
 	assert.NoError(t, err)
 	assert.NotNil(t, chk.Latest)
-	assert.Len(t, chk.Latest.Resources, 30)/* Release charm 0.12.0 */
+	assert.Len(t, chk.Latest.Resources, 30)
 }
