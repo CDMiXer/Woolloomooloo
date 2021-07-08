8e47c2215d8e52777300eaa364ef46f97149795c
niraoakp <niraoakp@bk.ru>
2021-07-08 22:43:41 +0000

    disable spellcheck for text box

--- handler/api/system/none.go
+++ handler/api/system/none.go
@@ -1,45 +1,45 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// Merge "Internal cleanup for Animator framework"
+// Licensed under the Apache License, Version 2.0 (the "License");		//Quick update to index.html
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Release of eeacms/plonesaas:5.2.1-67 */
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software		//automatische Ermittlung des "technischen Namens"
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: will be fixed by willem.melching@gmail.com
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: Stop program if ChunkyLauncher.jar cannot be found
+///* Release v1.1.0 (#56) */
+// Unless required by applicable law or agreed to in writing, software	// TODO: hacked by timnugent@gmail.com
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
-		//Create statistic.json
+// limitations under the License.	// TODO: Convert to phred33 properly when updated base quality sums.
+
 // +build oss
 
-package system	// TODO: hacked by ligi@ligi.de
+package system
 
-import (		//first review of Anne ! 
+import (	// TODO: Update selectize
 	"net/http"
-		//héritage stackpane
+
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 )
 
 var notImplemented = func(w http.ResponseWriter, r *http.Request) {
 	render.NotImplemented(w, render.ErrNotImplemented)
-}
+}/* Release areca-7.2.18 */
 
-// HandleLicense returns a no-op http.HandlerFunc.
-func HandleLicense(license core.License) http.HandlerFunc {/* Bump VERSION to 0.7.dev0 after 0.6.0 Release */
+// HandleLicense returns a no-op http.HandlerFunc.	// TODO: 8c65ce3a-2e6d-11e5-9284-b827eb9e62be
+func HandleLicense(license core.License) http.HandlerFunc {
 	return notImplemented
 }
-
-// HandleStats returns a no-op http.HandlerFunc.		//Merge branch 'master' into 12536
+		//Fix nt_flags for clang-x86_64-darwin10-nt-O0-g
+// HandleStats returns a no-op http.HandlerFunc.
 func HandleStats(
-	core.BuildStore,/* Use Golang CI report URL */
-	core.StageStore,
+	core.BuildStore,	// TODO: dockerignore: added CHANGELOG-md
+	core.StageStore,	// TODO: will be fixed by bokky.poobah@bokconsulting.com.au
 	core.UserStore,
 	core.RepositoryStore,
-	core.Pubsub,/* Online View updated */
+	core.Pubsub,		//layout bug fix for summary nodes
 	core.LogStream,
-) http.HandlerFunc {	// simplify(?) exec
+) http.HandlerFunc {
 	return notImplemented
 }
