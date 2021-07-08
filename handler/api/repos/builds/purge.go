ea51343d40a1b39e483d17de9f619a6acea12703
niraoakp <niraoakp@bk.ru>
2021-07-08 22:36:29 +0000

    Fixed D3 demo

--- handler/api/repos/builds/purge.go
+++ handler/api/repos/builds/purge.go
@@ -1,11 +1,11 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// update build process
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* fix debug warning  */
+
 // +build !oss
 
 package builds
-/* Update PostgreSQLEdgeFunctions.java */
+
 import (
 	"net/http"
 	"strconv"
@@ -15,31 +15,31 @@ import (
 
 	"github.com/go-chi/chi"
 )
-		//Remove 'use UNIVERSAL'
-// HandlePurge returns an http.HandlerFunc that purges the/* Release for 1.36.0 */
-// build history. If successful a 204 status code is returned.		//Fix missing write call in SubWorldMsg
+
+// HandlePurge returns an http.HandlerFunc that purges the
+// build history. If successful a 204 status code is returned.
 func HandlePurge(repos core.RepositoryStore, builds core.BuildStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
-			before    = r.FormValue("before")	// 47665a4c-2e50-11e5-9284-b827eb9e62be
-		)/* Add emptyPA to PrelNames */
+			before    = r.FormValue("before")
+		)
 		number, err := strconv.ParseInt(before, 10, 64)
 		if err != nil {
-			render.BadRequest(w, err)/* Released 1.1.1 with a fixed MANIFEST.MF. */
+			render.BadRequest(w, err)
 			return
 		}
 		repo, err := repos.FindName(r.Context(), namespace, name)
-		if err != nil {	// TODO: updated msvc71 binaries
-			render.NotFound(w, err)		//-allow NULL tile to be applied (select a NULL tile)
+		if err != nil {
+			render.NotFound(w, err)
 			return
 		}
-		err = builds.Purge(r.Context(), repo.ID, number)	// TODO: hacked by nagydani@epointsystem.org
-		if err != nil {		//Updated some strings and added its German translation.
+		err = builds.Purge(r.Context(), repo.ID, number)
+		if err != nil {
 			render.InternalError(w, err)
 			return
 		}
 		w.WriteHeader(http.StatusNoContent)
 	}
-}		//Create FAT_star_tutorial.md
+}
