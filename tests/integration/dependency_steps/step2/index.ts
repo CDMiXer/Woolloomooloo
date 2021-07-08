01dcb53ff10b4331a7440cb374c068c0cc321b28
niraoakp <niraoakp@bk.ru>
2021-07-08 16:12:40 +0000

    Merge branch 'master' into greenkeeper/codecov-3.0.1

--- tests/integration/dependency_steps/step2/index.ts
+++ tests/integration/dependency_steps/step2/index.ts
@@ -1,9 +1,9 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Removed casting in equals (casting was already done in previous line) */
+		//Fix perspective id.
 import { Resource } from "./resource";
 
 // Step 2: Reverse the dependency graph.
 // Checkpoint dependency graph: b -> a
 const b = new Resource("b", { state: 2 })
-const a = new Resource("a", { state: 1, resource: b })		//Modify organization key
+const a = new Resource("a", { state: 1, resource: b })
 // Dependency graph: a -> b
