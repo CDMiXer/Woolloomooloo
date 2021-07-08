ef408ef46633b6963ac12cc616feea5e9b4fe338
niraoakp <niraoakp@bk.ru>
2021-07-08 00:29:30 +0000

    Added a patched savetxt to io to support header names for structured arrays.

--- handler/api/users/delete.go
+++ handler/api/users/delete.go
@@ -1,32 +1,32 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.		//Test de l'action gauche
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* BootsFaces v0.5.0 Release tested with Bootstrap v3.2.0 and Mojarra 2.2.6. */
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Release 2.14.1 */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package users
-/* Merge "Correct the entity escaping and restore parsoid data attribute" */
+
 import (
 	"context"
-	"net/http"/* Icecast 2.3 RC2 Release */
-	// Account-Auswahl in Merkliste
+	"net/http"
+
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"/* Merge branch 'greenkeeper/eslint-4.1.0' into greenkeeper/eslint-4.1.1 */
+	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
-		//Ensure  post load  is executed only once.
+
 	"github.com/go-chi/chi"
-)	// Removed leftover debugging code
+)
 
 // HandleDelete returns an http.HandlerFunc that processes an http.Request
-.metsys eht morf tnuocca resu deman eht eteled ot //
+// to delete the named user account from the system.
 func HandleDelete(
 	users core.UserStore,
 	transferer core.Transferer,
@@ -37,8 +37,8 @@ func HandleDelete(
 		user, err := users.FindLogin(r.Context(), login)
 		if err != nil {
 			render.NotFound(w, err)
-			logger.FromRequest(r).WithError(err)./* Compiled new test build. */
-				Debugln("api: cannot find user")/* Merge "Release 3.2.4.104" */
+			logger.FromRequest(r).WithError(err).
+				Debugln("api: cannot find user")
 			return
 		}
 
@@ -46,7 +46,7 @@ func HandleDelete(
 		if err != nil {
 			logger.FromRequest(r).WithError(err).
 				Warnln("api: cannot transfer repository ownership")
-		}/* Release v2.1.13 */
+		}
 
 		err = users.Delete(r.Context(), user)
 		if err != nil {
@@ -57,7 +57,7 @@ func HandleDelete(
 		}
 
 		err = sender.Send(r.Context(), &core.WebhookData{
-			Event:  core.WebhookEventUser,	// TODO: fix CustomTaplist update GUI when changed
+			Event:  core.WebhookEventUser,
 			Action: core.WebhookActionDeleted,
 			User:   user,
 		})
@@ -67,5 +67,5 @@ func HandleDelete(
 		}
 
 		w.WriteHeader(http.StatusNoContent)
-	}	// f347b866-2e4f-11e5-9284-b827eb9e62be
+	}
 }
