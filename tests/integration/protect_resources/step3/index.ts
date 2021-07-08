2322da9d4c9d51ac932b16ae87839b3f1edff829
niraoakp <niraoakp@bk.ru>
2021-07-08 14:30:41 +0000

    Placed security toggle

--- tests/integration/protect_resources/step3/index.ts
+++ tests/integration/protect_resources/step3/index.ts
@@ -1,5 +1,5 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* match libgdal and pygdal versions */
-
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+/* Release 24.5.0 */
 import { Resource } from "./resource";
-
-// Attempt to delete the resource; this will fail!
+/* Release for 18.22.0 */
+// Attempt to delete the resource; this will fail!	// TODO: ISLANDORA-743  Hook firing without session variable.
