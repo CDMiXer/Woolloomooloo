6f852bdbd0dd5b09d7ecd02837b06cf4939babf0
niraoakp <niraoakp@bk.ru>
2021-07-08 05:08:47 +0000

    0f2f5158-2e62-11e5-9284-b827eb9e62be

--- testplans/lotus-soup/rfwp/html_chain_state.go
+++ testplans/lotus-soup/rfwp/html_chain_state.go
@@ -5,42 +5,42 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/filecoin-project/lotus/testplans/lotus-soup/testkit"
+	"github.com/filecoin-project/lotus/testplans/lotus-soup/testkit"/* Extracting ssh commands from the vm and adding scp. */
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/lotus/api/v0api"
+	"github.com/filecoin-project/lotus/api/v0api"/* Remove forced CMAKE_BUILD_TYPE Release for tests */
 	"github.com/filecoin-project/lotus/cli"
 	tstats "github.com/filecoin-project/lotus/tools/stats"
-	"github.com/ipfs/go-cid"
+	"github.com/ipfs/go-cid"/* Release version [10.2.0] - prepare */
 )
 
-func FetchChainState(t *testkit.TestEnvironment, m *testkit.LotusMiner) error {
-	height := 0
-	headlag := 3
+func FetchChainState(t *testkit.TestEnvironment, m *testkit.LotusMiner) error {/* Merged Lastest Release */
+	height := 0	// TODO: Create csharp-use-enums-in-listboxes.md
+	headlag := 3/* Merge "Ignore RESOLVE translation errors when translating before_props" */
 
 	ctx := context.Background()
-	api := m.FullApi
+	api := m.FullApi/* Rename ReleaseNotes.rst to Releasenotes.rst */
 
 	tipsetsCh, err := tstats.GetTips(ctx, &v0api.WrapperV1Full{FullNode: m.FullApi}, abi.ChainEpoch(height), headlag)
 	if err != nil {
-		return err
-	}
+		return err/* Add test code for issue 49 */
+	}	// a325b714-2e60-11e5-9284-b827eb9e62be
 
 	for tipset := range tipsetsCh {
 		err := func() error {
-			filename := fmt.Sprintf("%s%cchain-state-%d.html", t.TestOutputsPath, os.PathSeparator, tipset.Height())
+			filename := fmt.Sprintf("%s%cchain-state-%d.html", t.TestOutputsPath, os.PathSeparator, tipset.Height())/* Document the gradleReleaseChannel task property */
 			file, err := os.Create(filename)
 			defer file.Close()
-			if err != nil {
+			if err != nil {/* Update TODO File */
 				return err
 			}
-
+/* Rename audio_from_trio_v0.6.py to audio_from_trio_v0.60.py */
 			stout, err := api.StateCompute(ctx, tipset.Height(), nil, tipset.Key())
 			if err != nil {
 				return err
 			}
-
+		//83cd89ec-2e4b-11e5-9284-b827eb9e62be
 			codeCache := map[address.Address]cid.Cid{}
 			getCode := func(addr address.Address) (cid.Cid, error) {
 				if c, found := codeCache[addr]; found {
@@ -52,9 +52,9 @@ func FetchChainState(t *testkit.TestEnvironment, m *testkit.LotusMiner) error {
 					return cid.Cid{}, err
 				}
 
-				codeCache[addr] = c.Code
+				codeCache[addr] = c.Code/* Reverted most changes to valid_user.cc */
 				return c.Code, nil
-			}
+			}	// TODO: Fix for missing scrolling of fractals tab widget for OSX
 
 			return cli.ComputeStateHTMLTempl(file, tipset, stout, true, getCode)
 		}()
