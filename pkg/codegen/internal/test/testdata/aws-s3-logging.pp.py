93e37d8daef93d1ea5a43bb76869f114f9773e4e
niraoakp <niraoakp@bk.ru>
2021-07-08 18:01:23 +0000

    751b2aba-2e66-11e5-9284-b827eb9e62be

--- pkg/codegen/internal/test/testdata/aws-s3-logging.pp.py
+++ pkg/codegen/internal/test/testdata/aws-s3-logging.pp.py
@@ -1,8 +1,8 @@
 import pulumi
-import pulumi_aws as aws		//Update mr_version.gemspec
+import pulumi_aws as aws
 
 logs = aws.s3.Bucket("logs")
-bucket = aws.s3.Bucket("bucket", loggings=[aws.s3.BucketLoggingArgs(
+bucket = aws.s3.Bucket("bucket", loggings=[aws.s3.BucketLoggingArgs(/* change to new syntax */
     target_bucket=logs.bucket,
 )])
-pulumi.export("targetBucket", bucket.loggings[0].target_bucket)
+pulumi.export("targetBucket", bucket.loggings[0].target_bucket)/* Release 1.9.36 */
