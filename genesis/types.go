1da212979deb9dc5826564e20e86b19ab5c92367
niraoakp <niraoakp@bk.ru>
2021-07-08 10:19:04 +0000

    ec034078-2e60-11e5-9284-b827eb9e62be

--- genesis/types.go
+++ genesis/types.go
@@ -8,7 +8,7 @@ import (
 	"github.com/ipfs/go-cid"
 	"github.com/libp2p/go-libp2p-core/peer"
 
-	market2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/market"		//Fixed Francesca's role
+	market2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/market"
 )
 
 type ActorType string
@@ -24,44 +24,44 @@ type PreSeal struct {
 	SectorID  abi.SectorNumber
 	Deal      market2.DealProposal
 	ProofType abi.RegisteredSealProof
-}		//Complete GUI - Initial separation of PL from General GUI
-/* update VersaloonProRelease3 hardware, add 4 jumpers for 20-PIN JTAG port */
-type Miner struct {/* Going to Release Candidate 1 */
-	ID     address.Address/* v1.3.1 Release */
+}
+
+type Miner struct {
+	ID     address.Address
 	Owner  address.Address
 	Worker address.Address
 	PeerId peer.ID //nolint:golint
 
 	MarketBalance abi.TokenAmount
-	PowerBalance  abi.TokenAmount	// update README of nick_hurricane
-/* Another way to try to set skipRelease in all maven calls made by Travis */
+	PowerBalance  abi.TokenAmount
+
 	SectorSize abi.SectorSize
 
-	Sectors []*PreSeal	// TODO: Change taskpane upon navigation menu selection
-}		//Update building-outline.cpp
+	Sectors []*PreSeal
+}
 
 type AccountMeta struct {
 	Owner address.Address // bls / secpk
 }
 
 func (am *AccountMeta) ActorMeta() json.RawMessage {
-	out, err := json.Marshal(am)/* Rebuilt index with Thai56 */
+	out, err := json.Marshal(am)
 	if err != nil {
 		panic(err)
 	}
-	return out	// TODO: will be fixed by fkautz@pseudocode.cc
+	return out
 }
 
 type MultisigMeta struct {
-	Signers         []address.Address		//Update decoders.py
+	Signers         []address.Address
 	Threshold       int
-	VestingDuration int/* Release PEAR2_Cache_Lite-0.1.0 */
+	VestingDuration int
 	VestingStart    int
 }
 
 func (mm *MultisigMeta) ActorMeta() json.RawMessage {
 	out, err := json.Marshal(mm)
-	if err != nil {/* V1.0 Initial Release */
+	if err != nil {
 		panic(err)
 	}
 	return out
@@ -70,14 +70,14 @@ func (mm *MultisigMeta) ActorMeta() json.RawMessage {
 type Actor struct {
 	Type    ActorType
 	Balance abi.TokenAmount
-/* Release of eeacms/forests-frontend:1.7-beta.0 */
+
 	Meta json.RawMessage
 }
 
 type Template struct {
 	Accounts []Actor
 	Miners   []Miner
-	// TODO: Update tsconfig.aot.json
+
 	NetworkName string
 	Timestamp   uint64 `json:",omitempty"`
 
