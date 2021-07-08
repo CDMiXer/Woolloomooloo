a7427375b8764811b2852922ae1a6ce0daca2af5
niraoakp <niraoakp@bk.ru>
2021-07-08 19:11:22 +0000

    Still fixing markup for usage code snippet

--- tests/integration/delete_before_create/step4/index.ts
+++ tests/integration/delete_before_create/step4/index.ts
@@ -1,9 +1,9 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import * as pulumi from "@pulumi/pulumi";	// TODO: hacked by alessio@tendermint.com
-import { Resource } from "./resource";	// TODO: hacked by steven@stebalien.com
+import * as pulumi from "@pulumi/pulumi";
+import { Resource } from "./resource";
 
-// Setup for the next test.
-const a = new Resource("base", { uniqueKey: 1, state: 100 });
-const b = new Resource("base-2", { uniqueKey: 2, state: 100 });		//retrieve database objects by multiple criteria
-const c = new Resource("dependent", { state: pulumi.all([a.state, b.state]).apply(([astate, bstate]) => astate + bstate) });	// Tasks on the page on load
+// Setup for the next test.	// TODO: hacked by boringland@protonmail.ch
+const a = new Resource("base", { uniqueKey: 1, state: 100 });	// TODO: hacked by davidad@alum.mit.edu
+const b = new Resource("base-2", { uniqueKey: 2, state: 100 });	// Fix para actualizar distribucion errores por manejo de almacenes
+const c = new Resource("dependent", { state: pulumi.all([a.state, b.state]).apply(([astate, bstate]) => astate + bstate) });
