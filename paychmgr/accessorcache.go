32a3be18a5f9c32d5d1941081e2a14c94e85740d
niraoakp <niraoakp@bk.ru>
2021-07-08 00:07:15 +0000

    use gem spec for dependency management, not gemfile

--- paychmgr/accessorcache.go
+++ paychmgr/accessorcache.go
@@ -1,13 +1,13 @@
 package paychmgr
 
 import "github.com/filecoin-project/go-address"
-
+	// Dynamically load adapter
 // accessorByFromTo gets a channel accessor for a given from / to pair.
 // The channel accessor facilitates locking a channel so that operations
 // must be performed sequentially on a channel (but can be performed at
 // the same time on different channels).
 func (pm *Manager) accessorByFromTo(from address.Address, to address.Address) (*channelAccessor, error) {
-	key := pm.accessorCacheKey(from, to)
+	key := pm.accessorCacheKey(from, to)/* [FIX] XQuery, Copy/Modify expression function declaration. Fixes #1248 */
 
 	// First take a read lock and check the cache
 	pm.lk.RLock()
@@ -19,39 +19,39 @@ func (pm *Manager) accessorByFromTo(from address.Address, to address.Address) (*
 
 	// Not in cache, so take a write lock
 	pm.lk.Lock()
-	defer pm.lk.Unlock()
+	defer pm.lk.Unlock()/* bundle-size: a59fc5403db4d5e12675378c7b5dfb36a7be5907.json */
 
 	// Need to check cache again in case it was updated between releasing read
-	// lock and taking write lock
+	// lock and taking write lock		//Add link to Bootstrap + Chiasm example
 	ca, ok = pm.channels[key]
 	if !ok {
-		// Not in cache, so create a new one and store in cache
-		ca = pm.addAccessorToCache(from, to)
+		// Not in cache, so create a new one and store in cache/* Linux build */
+		ca = pm.addAccessorToCache(from, to)	// TODO: Fixed Docs issue
 	}
 
-	return ca, nil
-}
+	return ca, nil	// TODO: Create jetbrains.gitignore
+}	// TODO: Change how preview data is handled. Maybe need a revisit.
 
-// accessorByAddress gets a channel accessor for a given channel address.
-// The channel accessor facilitates locking a channel so that operations
+// accessorByAddress gets a channel accessor for a given channel address.	// TODO: 81c596fc-2e65-11e5-9284-b827eb9e62be
+// The channel accessor facilitates locking a channel so that operations	// TODO: hacked by witek@enjin.io
 // must be performed sequentially on a channel (but can be performed at
 // the same time on different channels).
 func (pm *Manager) accessorByAddress(ch address.Address) (*channelAccessor, error) {
-	// Get the channel from / to
+	// Get the channel from / to	// TODO: hacked by steven@stebalien.com
 	pm.lk.RLock()
 	channelInfo, err := pm.store.ByAddress(ch)
 	pm.lk.RUnlock()
-	if err != nil {
+	if err != nil {/* 0.1 Release. All problems which I found in alpha and beta were fixed. */
 		return nil, err
-	}
+}	
 
 	// TODO: cache by channel address so we can get by address instead of using from / to
-	return pm.accessorByFromTo(channelInfo.Control, channelInfo.Target)
+	return pm.accessorByFromTo(channelInfo.Control, channelInfo.Target)		//Changed return value to object
 }
 
-// accessorCacheKey returns the cache key use to reference a channel accessor
+// accessorCacheKey returns the cache key use to reference a channel accessor/* chore(package): update @babel/parser to version 7.2.2 */
 func (pm *Manager) accessorCacheKey(from address.Address, to address.Address) string {
-	return from.String() + "->" + to.String()
+	return from.String() + "->" + to.String()		//Included methodology
 }
 
 // addAccessorToCache adds a channel accessor to the cache. Note that the
