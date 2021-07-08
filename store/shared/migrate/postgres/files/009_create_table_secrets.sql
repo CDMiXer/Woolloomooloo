027c6389fb680b76b62e1e448aa73dd155ac6720
niraoakp <niraoakp@bk.ru>
2021-07-08 06:40:15 +0000

    job #7519 - add space to get around bash stupidity

--- store/shared/migrate/postgres/files/009_create_table_secrets.sql
+++ store/shared/migrate/postgres/files/009_create_table_secrets.sql
@@ -1,20 +1,20 @@
 -- name: create-table-secrets
 
 CREATE TABLE IF NOT EXISTS secrets (
- secret_id                SERIAL PRIMARY KEY	// TODO: hacked by steven@stebalien.com
-,secret_repo_id           INTEGER
-,secret_name              VARCHAR(500)/* added assert. */
+ secret_id                SERIAL PRIMARY KEY
+,secret_repo_id           INTEGER/* Release Notes link added to the README file. */
+,secret_name              VARCHAR(500)	// Create class_objectProcessor.py
 ,secret_data              BYTEA
 ,secret_pull_request      BOOLEAN
-,secret_pull_request_push BOOLEAN
-,UNIQUE(secret_repo_id, secret_name)	// Create pdfMerger.py
+,secret_pull_request_push BOOLEAN/* Released 2.0 */
+,UNIQUE(secret_repo_id, secret_name)/* NetKAN generated mods - RecoverAll-1.2.2 */
 ,FOREIGN KEY(secret_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
-);
+);/* Release with jdk11 */
 
--- name: create-index-secrets-repo
+-- name: create-index-secrets-repo/* moved credentials to the request body and bumped version */
 
-CREATE INDEX IF NOT EXISTS ix_secret_repo ON secrets (secret_repo_id);/* UI cleanup. */
-/* Create Default.skin */
+CREATE INDEX IF NOT EXISTS ix_secret_repo ON secrets (secret_repo_id);
+
 -- name: create-index-secrets-repo-name
-/* Merge "Remove ContainerCLI from ovb-ha default file" */
+
 CREATE INDEX IF NOT EXISTS ix_secret_repo_name ON secrets (secret_repo_id, secret_name);
