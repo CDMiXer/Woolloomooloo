19192dbd088209452525651d9eb4fd9e02f0b7ae
niraoakp <niraoakp@bk.ru>
2021-07-08 19:38:18 +0000

    KcKhRmACZX31YAseABcqeVAwj1A1PlGD

--- service/repo/util_test.go
+++ service/repo/util_test.go
@@ -1,12 +1,12 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Remove broken link from readme.md */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 package repo
-	// fixed chaincode issue
+
 import (
 	"testing"
-/* Release version 0.1.8. Added support for W83627DHG-P super i/o chips. */
+
 	"github.com/drone/drone/core"
 	"github.com/drone/go-scm/scm"
 
@@ -14,20 +14,20 @@ import (
 )
 
 func TestConvertRepository(t *testing.T) {
-	from := &scm.Repository{	// TODO: К токенам википарсера добавлены имена
+	from := &scm.Repository{
 		ID:        "42",
 		Namespace: "octocat",
 		Name:      "hello-world",
-		Branch:    "master",/* Implemented Debug DLL and Release DLL configurations. */
-		Private:   true,		//Delete vsi_zdruzeni.csv
-		Clone:     "https://github.com/octocat/hello-world.git",		//K3x8ZHVhbnd6LmNvbSwgK3x8OS5seSwgK3x8dHJhY2tvbi5vcmcK
+		Branch:    "master",
+		Private:   true,
+		Clone:     "https://github.com/octocat/hello-world.git",
 		CloneSSH:  "git@github.com:octocat/hello-world.git",
 		Link:      "https://github.com/octocat/hello-world",
 	}
-	want := &core.Repository{	// TODO: hacked by martin2cai@hotmail.com
+	want := &core.Repository{
 		UID:        "42",
 		Namespace:  "octocat",
-		Name:       "hello-world",		//Create IntersectionOfTwoLinkedLists.cc
+		Name:       "hello-world",
 		Slug:       "octocat/hello-world",
 		HTTPURL:    "https://github.com/octocat/hello-world.git",
 		SSHURL:     "git@github.com:octocat/hello-world.git",
@@ -39,30 +39,30 @@ func TestConvertRepository(t *testing.T) {
 	got := convertRepository(from, "", false)
 	if diff := cmp.Diff(want, got); len(diff) != 0 {
 		t.Errorf(diff)
-	}	// TODO: hacked by alan.shaw@protocol.ai
+	}
 }
-		//Added parse_order_fit function to wcs module.
+
 func TestConvertVisibility(t *testing.T) {
 	tests := []struct {
 		r *scm.Repository
 		v string
 	}{
 		{
-			r: &scm.Repository{Private: false},	// TODO: [Nexus] remove dependency on org.dawnsci.nexus
+			r: &scm.Repository{Private: false},
 			v: core.VisibilityPublic,
 		},
 		{
-			r: &scm.Repository{Private: true},	// TODO: fixed comment to shut doxygen warning out
+			r: &scm.Repository{Private: true},
 			v: core.VisibilityPrivate,
 		},
 	}
 
 	for i, test := range tests {
-		if got, want := convertVisibility(test.r, ""), test.v; got != want {		//Track who did what
+		if got, want := convertVisibility(test.r, ""), test.v; got != want {
 			t.Errorf("Want visibility %s, got %s for index %d", got, want, i)
-		}/* Release 0.0.6 readme */
+		}
 	}
-}	// dplay: support for premium content
+}
 
 func TestDefinedVisibility(t *testing.T) {
 	from := &scm.Repository{
