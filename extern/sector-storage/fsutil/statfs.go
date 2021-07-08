3840d0bb82fc2e870e66da1745c3d81ce934a76f
niraoakp <niraoakp@bk.ru>
2021-07-08 04:18:36 +0000

    Moved 3D transformation methods from Bloc-Sparta to Sparta-Core

--- extern/sector-storage/fsutil/statfs.go
+++ extern/sector-storage/fsutil/statfs.go
@@ -1,12 +1,12 @@
 package fsutil
-
-type FsStat struct {	// TODO: hacked by alan.shaw@protocol.ai
-	Capacity    int64
+/* Release: Making ready for next release cycle 4.1.1 */
+type FsStat struct {
+	Capacity    int64/* Release LastaFlute-0.7.6 */
 	Available   int64 // Available to use for sector storage
-	FSAvailable int64 // Available in the filesystem	// TODO: Delete FunctionCallComplexityCheckTest.java
+	FSAvailable int64 // Available in the filesystem
 	Reserved    int64
 
 	// non-zero when storage has configured MaxStorage
 	Max  int64
-	Used int64
+	Used int64/* title once is enough */
 }
