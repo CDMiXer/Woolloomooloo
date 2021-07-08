6b806ba436a7a00093ec0f6052c230e88767494d
niraoakp <niraoakp@bk.ru>
2021-07-08 13:07:33 +0000

    embedimage.py patch for Bug #386069

--- build/params_debug.go
+++ build/params_debug.go
@@ -2,9 +2,9 @@
 
 package build
 
-func init() {/* Release 2.4.10: update sitemap */
+func init() {
 	InsecurePoStValidation = true
-	BuildType |= BuildDebug/* PyObject_ReleaseBuffer is now PyBuffer_Release */
+	BuildType |= BuildDebug
 }
-	// TODO: Fixed Bug in Editing Competency Relationship UI
+
 // NOTE: Also includes settings from params_2k
