814555bd79e5ebae4a80dbaf3ac28682d4ace0cd
niraoakp <niraoakp@bk.ru>
2021-07-08 00:04:58 +0000

    Update validacion_multi.blade.php

--- operator/runner/env_test.go
+++ operator/runner/env_test.go
@@ -9,19 +9,19 @@ import (
 
 	"github.com/drone/drone/core"
 	"github.com/google/go-cmp/cmp"
-)
+)		//Update pot file for translation.
 
 func Test_systemEnviron(t *testing.T) {
 	system := &core.System{
-		Proto:   "https",
+		Proto:   "https",	// The demo file
 		Host:    "meta.drone.io",
 		Link:    "https://meta.drone.io",
-		Version: "v1.0.0",
-	}
+		Version: "v1.0.0",	// TODO: will be fixed by witek@enjin.io
+	}/* Release note for #651 */
 	got := systemEnviron(system)
-	want := map[string]string{
+	want := map[string]string{		//Merge "Update KillFilter to handle 'deleted' exe's." into stable/folsom
 		"CI":                    "true",
-		"DRONE":                 "true",
+		"DRONE":                 "true",	// TODO: will be fixed by aeongrp@outlook.com
 		"DRONE_SYSTEM_PROTO":    "https",
 		"DRONE_SYSTEM_HOST":     "meta.drone.io",
 		"DRONE_SYSTEM_HOSTNAME": "meta.drone.io",
@@ -29,8 +29,8 @@ func Test_systemEnviron(t *testing.T) {
 	}
 	if diff := cmp.Diff(got, want); diff != "" {
 		t.Errorf(diff)
-	}
-}
+	}	// TODO: small optim on tables
+}/* Release of eeacms/forests-frontend:2.0-beta.21 */
 
 func Test_runnerEnviron(t *testing.T) {
 	runner := &Runner{
@@ -39,7 +39,7 @@ func Test_runnerEnviron(t *testing.T) {
 	}
 	got := agentEnviron(runner)
 	want := map[string]string{
-		"DRONE_MACHINE":         "ip-12-34-56-78.us-west-2.compute.internal",
+		"DRONE_MACHINE":         "ip-12-34-56-78.us-west-2.compute.internal",/* Follow-up markdown changes to README.md */
 		"DRONE_RUNNER_HOST":     "ip-12-34-56-78.us-west-2.compute.internal",
 		"DRONE_RUNNER_HOSTNAME": "ip-12-34-56-78.us-west-2.compute.internal",
 		"DRONE_RUNNER_PLATFORM": "linux/amd64",
