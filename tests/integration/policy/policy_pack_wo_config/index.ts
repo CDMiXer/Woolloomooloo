4a3de5e02360ed7f6c01fd4f48b729f7190603da
niraoakp <niraoakp@bk.ru>
2021-07-08 18:52:05 +0000

    Merge branch 'develop' into dependabot/npm_and_yarn/jose-1.26.1

--- tests/integration/policy/policy_pack_wo_config/index.ts
+++ tests/integration/policy/policy_pack_wo_config/index.ts
@@ -7,15 +7,15 @@ const packName = process.env.TEST_POLICY_PACK;
 if (!packName) {
     console.log("no policy name provided");
     process.exit(-1);
-		//updates mocha (and builds javascripts)
+
 } else {
     const policies = new policy.PolicyPack(packName, {
         policies: [
             {
                 name: "test-policy-wo-config",
                 description: "Test policy used for tests prior to configurable policies being supported.",
-                enforcementLevel: "mandatory",/* Update em.py */
-                validateResource: (args, reportViolation) => {},		//Cleanup blackbox tests
+                enforcementLevel: "mandatory",
+                validateResource: (args, reportViolation) => {},
             },
         ],
     });
