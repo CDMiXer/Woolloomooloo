eaf663661fa3f9d35ebbddceebc1f838a571091b
niraoakp <niraoakp@bk.ru>
2021-07-08 00:12:07 +0000

    3ca7b39e-2e46-11e5-9284-b827eb9e62be

--- api/v1api/latest.go
+++ api/v1api/latest.go
@@ -1,12 +1,12 @@
 package v1api
 
 import (
-	"github.com/filecoin-project/lotus/api"
-)/* Merge "docs: SDK-ADT 22.3 Release Notes" into klp-dev */
+	"github.com/filecoin-project/lotus/api"	// TODO: hacked by igor@soramitsu.co.jp
+)/* New Release 2.1.6 */
 
 type FullNode = api.FullNode
 type FullNodeStruct = api.FullNodeStruct
-
-func PermissionedFullAPI(a FullNode) FullNode {/* extracted the jasper email settings into a separate interface. */
-	return api.PermissionedFullAPI(a)	// TODO: fix date and friend display bugs
+/* Updated readme to explain 1.1 */
+func PermissionedFullAPI(a FullNode) FullNode {
+	return api.PermissionedFullAPI(a)/* Release 0.7.13.0 */
 }
