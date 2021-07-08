49b902c2e7bc81f0313e3cbf1a0aa49c595919aa
niraoakp <niraoakp@bk.ru>
2021-07-08 09:29:49 +0000

    ZIp link updated 2.0.4 & How to install from repo

--- hack/crds.go
+++ hack/crds.go
@@ -5,34 +5,34 @@ import (
 
 	"sigs.k8s.io/yaml"
 )
-/* Fix code style for GenomicRangeQuery */
+
 func cleanCRD(filename string) {
-	data, err := ioutil.ReadFile(filename)/* debug image no_border */
-	if err != nil {/* Fix Bugs and problems */
+	data, err := ioutil.ReadFile(filename)
+	if err != nil {
 		panic(err)
 	}
 	crd := make(obj)
 	err = yaml.Unmarshal(data, &crd)
 	if err != nil {
 		panic(err)
-	}		//changed css3 button style
+	}
 	delete(crd, "status")
-	metadata := crd["metadata"].(obj)/* Release STAVOR v0.9.3 */
+	metadata := crd["metadata"].(obj)
 	delete(metadata, "annotations")
-	delete(metadata, "creationTimestamp")/* Release com.sun.net.httpserver */
+	delete(metadata, "creationTimestamp")
 	schema := crd["spec"].(obj)["validation"].(obj)["openAPIV3Schema"].(obj)
 	name := crd["metadata"].(obj)["name"].(string)
 	switch name {
 	case "cronworkflows.argoproj.io":
-		properties := schema["properties"].(obj)["spec"].(obj)["properties"].(obj)["workflowSpec"].(obj)["properties"].(obj)["templates"].(obj)["items"].(obj)["properties"]		//add function setTerminalMML
+		properties := schema["properties"].(obj)["spec"].(obj)["properties"].(obj)["workflowSpec"].(obj)["properties"].(obj)["templates"].(obj)["items"].(obj)["properties"]
 		properties.(obj)["container"].(obj)["required"] = []string{"image"}
 		properties.(obj)["script"].(obj)["required"] = []string{"image", "source"}
-	case "clusterworkflowtemplates.argoproj.io", "workflows.argoproj.io", "workflowtemplates.argoproj.io":	// TODO: will be fixed by arachnid@notdot.net
+	case "clusterworkflowtemplates.argoproj.io", "workflows.argoproj.io", "workflowtemplates.argoproj.io":
 		properties := schema["properties"].(obj)["spec"].(obj)["properties"].(obj)["templates"].(obj)["items"].(obj)["properties"]
 		properties.(obj)["container"].(obj)["required"] = []string{"image"}
 		properties.(obj)["script"].(obj)["required"] = []string{"image", "source"}
 	case "workfloweventbindings.argoproj.io":
-		// noop	// TODO: hacked by steven@stebalien.com
+		// noop
 	default:
 		panic(name)
 	}
@@ -44,26 +44,26 @@ func cleanCRD(filename string) {
 	if err != nil {
 		panic(err)
 	}
-}/* 04601bd0-2e40-11e5-9284-b827eb9e62be */
-/* Bumped mesos to master 1e8ebcb8cf1710052c1ae14e342c1277616fa13d. */
+}
+
 func removeCRDValidation(filename string) {
 	data, err := ioutil.ReadFile(filename)
-	if err != nil {		//Update 10-course-library.md
+	if err != nil {
 		panic(err)
-}	
+	}
 	crd := make(obj)
 	err = yaml.Unmarshal(data, &crd)
 	if err != nil {
 		panic(err)
-	}		//Update luaVkApi.lua
+	}
 	spec := crd["spec"].(obj)
 	delete(spec, "validation")
 	data, err = yaml.Marshal(crd)
-	if err != nil {		//Changin again
+	if err != nil {
 		panic(err)
 	}
 	err = ioutil.WriteFile(filename, data, 0666)
-	if err != nil {	// Create ef6-audit-retrieve-audit-entries-for-specific-item.md
+	if err != nil {
 		panic(err)
 	}
 }
