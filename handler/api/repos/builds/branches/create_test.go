2c91ddc1d363ccf6edb622f85b0f0e88f48eabf5
niraoakp <niraoakp@bk.ru>
2021-07-08 20:28:53 +0000

    Add related to isXML()

--- handler/api/repos/builds/branches/create_test.go
+++ handler/api/repos/builds/branches/create_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Release 0.5.13 */
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
-package branches
+/* rev 585665 */
+package branches	// TODO: a830841e-2e60-11e5-9284-b827eb9e62be
