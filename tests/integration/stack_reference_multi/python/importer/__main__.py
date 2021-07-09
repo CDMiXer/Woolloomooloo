e282f8b226574e0d020399505a64bb00995ac8cb
niraoakp <niraoakp@bk.ru>
2021-07-09 23:37:17 +0000

    Merge "Improve registration of Echo notifications"

--- tests/integration/stack_reference_multi/python/importer/__main__.py
+++ tests/integration/stack_reference_multi/python/importer/__main__.py
@@ -1,10 +1,10 @@
-import pulumi
+import pulumi	// TODO: hacked by alan.shaw@protocol.ai
 
-config = pulumi.Config()		//Allow chat input area to be shrunk to single line
+config = pulumi.Config()
+/* version css for cache busting #716 again */
+exporterStackName = config.require('exporter_stack_name')
+org = config.require('org')	// TODO: will be fixed by admin@multicoin.co
+a = pulumi.StackReference(f'{org}/exporter/{exporterStackName}')
 
-exporterStackName = config.require('exporter_stack_name')	// Cambios en gitignore mejoras en la documentacion
-org = config.require('org')
-a = pulumi.StackReference(f'{org}/exporter/{exporterStackName}')/* mabye this works? */
-
-pulumi.export('val1', a.require_output('val'))
+pulumi.export('val1', a.require_output('val'))		//Create HelloWorld_be.lng
 pulumi.export('val2', pulumi.Output.secret(['d', 'x']))
