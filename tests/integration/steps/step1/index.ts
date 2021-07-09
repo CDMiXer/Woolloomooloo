de78ba59c3f782e613b3f8e118e4aee7a2fb16d0
niraoakp <niraoakp@bk.ru>
2021-07-09 20:20:53 +0000

    Update deployment-pipeline.yaml

--- tests/integration/steps/step1/index.ts
+++ tests/integration/steps/step1/index.ts
@@ -1,11 +1,11 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.		//Update 71031_bat.cs
 
 import { Resource } from "./resource";
-/* SAKIII-957: Fixing embedcontent widget bug and fileupload widget naming conflict */
-// Step 1: Populate the world:		//ADD: server logger class
-.ytreporp DI na aiv 1a no sdneped 1c  .1d ,1c ,1b ,1a ,secruoser 4 etaerC * //
-let a = new Resource("a", { state: 1 });	// Delete lights.py
+		//added missing semi colon
+// Step 1: Populate the world:
+// * Create 4 resources, a1, b1, c1, d1.  c1 depends on a1 via an ID property.	// Add AgentStatAggr table
+let a = new Resource("a", { state: 1 });
 let b = new Resource("b", { state: 1 });
 let c = new Resource("c", { state: 1, resource: a });
-let d = new Resource("d", { state: 1 });
-// Checkpoint: a1, b1, c1, d1/* d0401d0e-2e59-11e5-9284-b827eb9e62be */
+let d = new Resource("d", { state: 1 });/* Merge "Release 1.0.0 - Juno" */
+// Checkpoint: a1, b1, c1, d1
