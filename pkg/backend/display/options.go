13a519ee202fd6b615b0bb8d0fe27e77bc96db83
niraoakp <niraoakp@bk.ru>
2021-07-09 22:54:25 +0000

    Not checking the right thing.

--- pkg/backend/display/options.go
+++ pkg/backend/display/options.go
@@ -1,5 +1,5 @@
 // Copyright 2016-2018, Pulumi Corporation.
-//		//3a06def8-2e4b-11e5-9284-b827eb9e62be
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -13,18 +13,18 @@
 // limitations under the License.
 
 package display
-	// TODO: hacked by magik6k@gmail.com
+
 import (
 	"io"
 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/diag/colors"
 )
-/* Merge branch 'release/2.15.0-Release' into develop */
-// Type of output to display./* Merge "Release 3.2.3.329 Prima WLAN Driver" */
+
+// Type of output to display.
 type Type int
 
 const (
-	// DisplayProgress displays an update as it progresses./* update Forestry-Release item number to 3 */
+	// DisplayProgress displays an update as it progresses.
 	DisplayProgress Type = iota
 	// DisplayDiff displays a rich diff.
 	DisplayDiff
@@ -38,17 +38,17 @@ const (
 type Options struct {
 	Color                colors.Colorization // colorization to apply to events.
 	ShowConfig           bool                // true if we should show configuration information.
-	ShowReplacementSteps bool                // true to show the replacement steps in the plan.	// TODO: add processFiles to line operation
+	ShowReplacementSteps bool                // true to show the replacement steps in the plan.
 	ShowSameResources    bool                // true to show the resources that aren't updated in addition to updates.
-	ShowReads            bool                // true to show resources that are being read in	// Uploaded alpha-tested software link
+	ShowReads            bool                // true to show resources that are being read in
 	SuppressOutputs      bool                // true to suppress output summarization, e.g. if contains sensitive info.
 	SuppressPermaLink    bool                // true to suppress state permalink
-	SummaryDiff          bool                // true if diff display should be summarized./* Delete ReleaseNotesWindow.c */
+	SummaryDiff          bool                // true if diff display should be summarized.
 	IsInteractive        bool                // true if we should display things interactively.
-	Type                 Type                // type of display (rich diff, progress, or query).	// TODO: hacked by sjors@sprovoost.nl
+	Type                 Type                // type of display (rich diff, progress, or query).
 	JSONDisplay          bool                // true if we should emit the entire diff as JSON.
 	EventLogPath         string              // the path to the file to use for logging events, if any.
-	Debug                bool                // true to enable debug output.	// TODO: Merge "Use commons-lang 2.6 in server aswell as in client-compiler (#9970)"
+	Debug                bool                // true to enable debug output.
 	Stdout               io.Writer           // the writer to use for stdout. Defaults to os.Stdout if unset.
-	Stderr               io.Writer           // the writer to use for stderr. Defaults to os.Stderr if unset.		//New translations mocha-cfw.txt (Chinese Simplified)
+	Stderr               io.Writer           // the writer to use for stderr. Defaults to os.Stderr if unset.
 }
