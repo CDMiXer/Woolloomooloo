1f445ba96c65c62a9fc160af3c58af594038db26
niraoakp <niraoakp@bk.ru>
2021-07-09 20:23:11 +0000

    Added Jaeger link.

--- grpc-go/test/tools/tools.go
+++ grpc-go/test/tools/tools.go
@@ -1,33 +1,33 @@
 // +build tools
-		//8247ef4c-2e4e-11e5-9284-b827eb9e62be
-/*
+
+/*	// Merge "Change schema hypervisor.cpu_info from string to object in 2.28"
  *
  * Copyright 2018 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");	// Update IRC example to reflect new room ID
- * you may not use this file except in compliance with the License./* Remoção do campo data_abertura da modal de adicionar solicitação. */
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.	// Delete DADOS.CERTIF.txt
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software/* Workaround on some URL constructions */
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and	// TODO: Create DISPLAYQ.basic
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// fix depth test, remove getGlMatrixPerspective
+ * See the License for the specific language governing permissions and
  * limitations under the License.
- *		//[IMP] Improved message when applicant hired with/without employee.
- */
+ *
+ */		//Create a content categories enum
 
 // This file is not intended to be compiled.  Because some of these imports are
 // not actual go packages, we use a build constraint at the top of this file to
-// prevent tools from inspecting the imports.
+// prevent tools from inspecting the imports./* Release of eeacms/www:19.11.8 */
 
-package tools	// TODO: default task
+package tools
 
 import (
-	_ "github.com/client9/misspell/cmd/misspell"/* temp fix gem for deprecation warnings */
-	_ "github.com/golang/protobuf/protoc-gen-go"
+	_ "github.com/client9/misspell/cmd/misspell"	// Make CGSize unboxable
+	_ "github.com/golang/protobuf/protoc-gen-go"	// Rename index.md to Doing_a_PhD_Msc.md
 	_ "golang.org/x/lint/golint"
 	_ "golang.org/x/tools/cmd/goimports"
-	_ "honnef.co/go/tools/cmd/staticcheck"		//Fixed uncaught InterruptedException
+	_ "honnef.co/go/tools/cmd/staticcheck"
 )
