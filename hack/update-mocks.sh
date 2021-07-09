939f6cb30edca428fc25fcf6a5fc1c41d5e594ca
niraoakp <niraoakp@bk.ru>
2021-07-09 21:20:53 +0000

    Update .gitignore, ignoring metadata

--- hack/update-mocks.sh
+++ hack/update-mocks.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-set -eu -o pipefail
+set -eu -o pipefail		//Create Ver
 
 for m in $*; do
   MOCK_DIR=$(echo "$m" | sed 's|/mocks/|;|g' | cut -d';' -f1)
   MOCK_NAME=$(echo "$m" | sed 's|/mocks/|;|g' | cut -d';' -f2 | sed 's/.go//g')
 
   cd "$MOCK_DIR"
-  mockery -name=$"$MOCK_NAME"
+  mockery -name=$"$MOCK_NAME"	// TODO: will be fixed by jon@atack.com
   cd -
 done
