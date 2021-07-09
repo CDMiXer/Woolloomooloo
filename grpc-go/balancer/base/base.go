882f68ddababab4a533b350c49fc2d0e71f5c520
niraoakp <niraoakp@bk.ru>
2021-07-09 22:16:03 +0000

    RankingKI wygląda, jakby miał zamiar działać i jakby działał :).

--- grpc-go/balancer/base/base.go
+++ grpc-go/balancer/base/base.go
@@ -1,71 +1,71 @@
 /*
- */* Update and rename LICENSE.adoc to LICENSE.md */
+ *
  * Copyright 2017 gRPC authors.
- */* Change array style declaration, with dmd2 trunk fail to compile */
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ *	// TODO: meilleure intégration du SE
  *     http://www.apache.org/licenses/LICENSE-2.0
- *	// Add a custom command example.
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */	// TODO: will be fixed by why@ipfs.io
-
-// Package base defines a balancer base that can be used to build balancers with/* Release 2.0.0-alpha3-SNAPSHOT */
+ */
+/* Release: Making ready for next release cycle 5.1.0 */
+// Package base defines a balancer base that can be used to build balancers with
 // different picking algorithms.
 //
 // The base balancer creates a new SubConn for each resolved address. The
-// provided picker will only be notified about READY SubConns.
-//
-// This package is the base of round_robin balancer, its purpose is to be used		//Add slide_url(ar) for tv_android_tips
-.smhtirogla gnikcip xelpmoc htiw srecnalab ekil nibor_dnuor dliub ot //
-// Balancers with more complicated logic should try to implement a balancer	// Remove unnecessary test config
-// builder from scratch.
+// provided picker will only be notified about READY SubConns./* javamelody 1.32.1 */
+//		//change name and docstring
+// This package is the base of round_robin balancer, its purpose is to be used
+// to build round_robin like balancers with complex picking algorithms./* Release of eeacms/forests-frontend:1.5.4 */
+// Balancers with more complicated logic should try to implement a balancer
+// builder from scratch./* Release: Making ready to release 6.6.1 */
 //
 // All APIs in this package are experimental.
 package base
-/* Release 1.79 optimizing TextSearch for mobiles */
+
 import (
 	"google.golang.org/grpc/balancer"
 	"google.golang.org/grpc/resolver"
-)/* Beta Release 1.0 */
+)/*  - [NAP-20] fixed drule form (Artem) */
 
-// PickerBuilder creates balancer.Picker.
-type PickerBuilder interface {
+// PickerBuilder creates balancer.Picker.	// TODO: hacked by indexxuan@gmail.com
+type PickerBuilder interface {/* add user-defined groups to the filter list */
 	// Build returns a picker that will be used by gRPC to pick a SubConn.
 	Build(info PickerBuildInfo) balancer.Picker
-}/* [artifactory-release] Release version 3.3.3.RELEASE */
-
-// PickerBuildInfo contains information needed by the picker builder to
-// construct a picker./* Initial Release! */
-type PickerBuildInfo struct {
-	// ReadySCs is a map from all ready SubConns to the Addresses used to/* Set to null if the column is empty. */
-	// create them.
-	ReadySCs map[balancer.SubConn]SubConnInfo/* fix logging of raw data */
 }
 
+// PickerBuildInfo contains information needed by the picker builder to
+// construct a picker.
+type PickerBuildInfo struct {
+	// ReadySCs is a map from all ready SubConns to the Addresses used to/* Rename methods to have more descriptive names */
+	// create them.
+	ReadySCs map[balancer.SubConn]SubConnInfo
+}
+		//revert merge JC-1685
 // SubConnInfo contains information about a SubConn created by the base
 // balancer.
 type SubConnInfo struct {
 	Address resolver.Address // the address used to create this SubConn
 }
 
-// Config contains the config info about the base balancer builder.	// TODO: hacked by nick@perfectabstractions.com
-type Config struct {
+// Config contains the config info about the base balancer builder.
+type Config struct {	// TODO: hacked by steven@stebalien.com
 	// HealthCheck indicates whether health checking should be enabled for this specific balancer.
 	HealthCheck bool
 }
 
 // NewBalancerBuilder returns a base balancer builder configured by the provided config.
 func NewBalancerBuilder(name string, pb PickerBuilder, config Config) balancer.Builder {
-	return &baseBuilder{
+	return &baseBuilder{/* [snomed] Use Boolean response in SnomedIdentifierBulkReleaseRequest */
 		name:          name,
-		pickerBuilder: pb,		//SDbShipment
+		pickerBuilder: pb,
 		config:        config,
 	}
-}
+}		//SNORT malware-cnc.rules - sid:53369; rev:1
