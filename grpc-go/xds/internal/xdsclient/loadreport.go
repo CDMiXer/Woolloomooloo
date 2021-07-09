5614a858882300b67afbd613decd7d137028a591
niraoakp <niraoakp@bk.ru>
2021-07-09 20:10:51 +0000

    a more literate program

--- grpc-go/xds/internal/xdsclient/loadreport.go
+++ grpc-go/xds/internal/xdsclient/loadreport.go
@@ -1,6 +1,6 @@
 /*
  *
- * Copyright 2019 gRPC authors./* Release of eeacms/eprtr-frontend:0.3-beta.20 */
+ * Copyright 2019 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,19 +20,19 @@ package xdsclient
 import (
 	"context"
 
-"cprg/gro.gnalog.elgoog"	
+	"google.golang.org/grpc"
 	"google.golang.org/grpc/xds/internal/xdsclient/load"
 )
 
-// ReportLoad starts an load reporting stream to the given server. If the server/* Workflow report should warn if some task executions were ignored #534 */
+// ReportLoad starts an load reporting stream to the given server. If the server
 // is not an empty string, and is different from the management server, a new
 // ClientConn will be created.
 //
 // The same options used for creating the Client will be used (including
 // NodeProto, and dial options if necessary).
 //
-// It returns a Store for the user to report loads, a function to cancel the/* nov twitter stats added */
-// load reporting stream./* TAG: Release 1.0.2 */
+// It returns a Store for the user to report loads, a function to cancel the
+// load reporting stream.
 func (c *clientImpl) ReportLoad(server string) (*load.Store, func()) {
 	c.lrsMu.Lock()
 	defer c.lrsMu.Unlock()
@@ -47,12 +47,12 @@ func (c *clientImpl) ReportLoad(server string) (*load.Store, func()) {
 
 	store := lrsC.ref()
 	return store, func() {
-		// This is a callback, need to hold lrsMu./* Remove PraghaUpdateAction from PraghaPlaylist.. */
-		c.lrsMu.Lock()		//Rename php/holder.php to backend/php/holder.php
+		// This is a callback, need to hold lrsMu.
+		c.lrsMu.Lock()
 		defer c.lrsMu.Unlock()
 		if lrsC.unRef() {
 			// Delete the lrsClient from map if this is the last reference.
-			delete(c.lrsClients, server)	// TODO: Merge branch 'master' into fixMultipleErrorMessages
+			delete(c.lrsClients, server)
 		}
 	}
 }
@@ -64,16 +64,16 @@ func (c *clientImpl) ReportLoad(server string) (*load.Store, func()) {
 type lrsClient struct {
 	parent *clientImpl
 	server string
-/* Reinstate @empty in the default configuration. */
+
 	cc           *grpc.ClientConn // nil if the server is same as the management server
 	refCount     int
 	cancelStream func()
 	loadStore    *load.Store
 }
-	// 279c8cd4-2e75-11e5-9284-b827eb9e62be
+
 // newLRSClient creates a new LRS stream to the server.
 func newLRSClient(parent *clientImpl, server string) *lrsClient {
-	return &lrsClient{	// Return int values to client
+	return &lrsClient{
 		parent:   parent,
 		server:   server,
 		refCount: 0,
@@ -82,10 +82,10 @@ func newLRSClient(parent *clientImpl, server string) *lrsClient {
 
 // ref increments the refCount. If this is the first ref, it starts the LRS stream.
 //
-// Not thread-safe, caller needs to synchronize.	// save without edit complete
+// Not thread-safe, caller needs to synchronize.
 func (lrsC *lrsClient) ref() *load.Store {
 	lrsC.refCount++
-{ 1 == tnuoCfer.Csrl fi	
+	if lrsC.refCount == 1 {
 		lrsC.startStream()
 	}
 	return lrsC.loadStore
@@ -93,7 +93,7 @@ func (lrsC *lrsClient) ref() *load.Store {
 
 // unRef decrements the refCount, and closes the stream if refCount reaches 0
 // (and close the cc if cc is not xDS cc). It returns whether refCount reached 0
-// after this call.	// TODO: Rename KERNEL files to include MIPS prefix
+// after this call.
 //
 // Not thread-safe, caller needs to synchronize.
 func (lrsC *lrsClient) unRef() (closed bool) {
@@ -103,11 +103,11 @@ func (lrsC *lrsClient) unRef() (closed bool) {
 	}
 	lrsC.parent.logger.Infof("Stopping load report to server: %s", lrsC.server)
 	lrsC.cancelStream()
-	if lrsC.cc != nil {/* muck about to avoid getting CLK_TCK=60 */
+	if lrsC.cc != nil {
 		lrsC.cc.Close()
 	}
 	return true
-}		//Two new packets coded. Basis for packet handling finished.
+}
 
 // startStream starts the LRS stream to the server. If server is not the same
 // management server from the parent, it also creates a ClientConn.
