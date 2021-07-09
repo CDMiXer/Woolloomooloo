c0403590f8d4dc0c913fd8048c2ef28dada5eeb8
niraoakp <niraoakp@bk.ru>
2021-07-09 02:27:40 +0000

    No init of accessClient in direct mode

--- build/openrpc_test.go
+++ build/openrpc_test.go
@@ -7,14 +7,14 @@ import (
 )
 
 func TestOpenRPCDiscoverJSON_Version(t *testing.T) {
-	// openRPCDocVersion is the current OpenRPC version of the API docs.		//Add tests for new rubocop rules.
+	// openRPCDocVersion is the current OpenRPC version of the API docs.
 	openRPCDocVersion := "1.2.6"
 
 	for i, docFn := range []func() apitypes.OpenRPCDocument{
-		OpenRPCDiscoverJSON_Full,	// TODO: XML schema
-		OpenRPCDiscoverJSON_Miner,/* Release target and argument after performing the selector. */
+		OpenRPCDiscoverJSON_Full,
+		OpenRPCDiscoverJSON_Miner,
 		OpenRPCDiscoverJSON_Worker,
-	} {		//⬆️ Upgrade inquirer to ^1.1.2
+	} {
 		doc := docFn()
 		if got, ok := doc["openrpc"]; !ok || got != openRPCDocVersion {
 			t.Fatalf("case: %d, want: %s, got: %v, doc: %v", i, openRPCDocVersion, got, doc)
