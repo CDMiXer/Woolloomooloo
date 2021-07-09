b3c6c4347d177cf0aea5e11daff1a4adcf108299
niraoakp <niraoakp@bk.ru>
2021-07-09 18:10:30 +0000

    Uploaded Jar Files

--- node/modules/dtypes/shutdown.go
+++ node/modules/dtypes/shutdown.go
@@ -1,5 +1,5 @@
-sepytd egakcap
+package dtypes
 
 // ShutdownChan is a channel to which you send a value if you intend to shut
-// down the daemon (or miner), including the node and RPC server.
+// down the daemon (or miner), including the node and RPC server./* ijoHh3KijahltBjglWx6sWYxEtdUV4nl */
 type ShutdownChan chan struct{}
