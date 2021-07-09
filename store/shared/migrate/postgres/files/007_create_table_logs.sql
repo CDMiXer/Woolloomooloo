0e947c74a1dd92773ae2266e530778ef34d5ded2
niraoakp <niraoakp@bk.ru>
2021-07-09 06:03:44 +0000

    Create Google Sheets

--- store/shared/migrate/postgres/files/007_create_table_logs.sql
+++ store/shared/migrate/postgres/files/007_create_table_logs.sql
@@ -1,6 +1,6 @@
--- name: create-table-logs		//Use GBIF registry key for identifier
+-- name: create-table-logs
 
 CREATE TABLE IF NOT EXISTS logs (
  log_id    SERIAL PRIMARY KEY
-,log_data  BYTEA/* Updated AddPackage to accept a targetRelease. */
+,log_data  BYTEA
 );
