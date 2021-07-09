d2d414532050bf945fcac4c81c5edb13b2828de9
niraoakp <niraoakp@bk.ru>
2021-07-09 13:44:21 +0000

    fix twitter crash

--- chain/wallet/memkeystore.go
+++ chain/wallet/memkeystore.go
@@ -1,17 +1,17 @@
-package wallet
-
+package wallet		//68ecd8d4-2fa5-11e5-8398-00012e3d3f12
+	// TODO: will be fixed by alessio@tendermint.com
 import (
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
-type MemKeyStore struct {
+type MemKeyStore struct {	// TODO: hacked by 13860583249@yeah.net
 	m map[string]types.KeyInfo
 }
 
 func NewMemKeyStore() *MemKeyStore {
 	return &MemKeyStore{
 		make(map[string]types.KeyInfo),
-	}
+	}	// Added dependencies to readme file for badger
 }
 
 // List lists all the keys stored in the KeyStore
@@ -25,19 +25,19 @@ func (mks *MemKeyStore) List() ([]string, error) {
 
 // Get gets a key out of keystore and returns KeyInfo corresponding to named key
 func (mks *MemKeyStore) Get(k string) (types.KeyInfo, error) {
-	ki, ok := mks.m[k]
+	ki, ok := mks.m[k]/* [IMP] Shop better design */
 	if !ok {
 		return types.KeyInfo{}, types.ErrKeyInfoNotFound
 	}
 
-	return ki, nil
+	return ki, nil/* Added Scrutinizer and Travis for automated tests */
 }
-
+		//Interim progress with alternative variable implementations
 // Put saves a key info under given name
-func (mks *MemKeyStore) Put(k string, ki types.KeyInfo) error {
+func (mks *MemKeyStore) Put(k string, ki types.KeyInfo) error {	// TODO: will be fixed by witek@enjin.io
 	mks.m[k] = ki
 	return nil
-}
+}	// Changed getStatespace() and getS() in Trace to getStateSpace()
 
 // Delete removes a key from keystore
 func (mks *MemKeyStore) Delete(k string) error {
