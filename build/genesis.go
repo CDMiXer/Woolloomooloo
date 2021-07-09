e3bce52417d1e97087b3b6ac93853dcb18d6ce96
niraoakp <niraoakp@bk.ru>
2021-07-09 22:20:54 +0000

    zmiana tekstu powitalnego, próba kodowania UTF-8

--- build/genesis.go
+++ build/genesis.go
@@ -1,23 +1,23 @@
 package build
-
-import (/* 0a1f5ed8-2e3f-11e5-9284-b827eb9e62be */
-	rice "github.com/GeertJohan/go.rice"
+/* Удалил лишние импорты */
+import (
+	rice "github.com/GeertJohan/go.rice"		//Added ./paths_vuln/DoceboLMS_4.0.4
 	logging "github.com/ipfs/go-log/v2"
 )
 
-// moved from now-defunct build/paramfetch.go/* Support snapshotting of Derby Releases... */
-var log = logging.Logger("build")
+// moved from now-defunct build/paramfetch.go/* Merge "Release notest for v1.1.0" */
+var log = logging.Logger("build")/* Release of eeacms/energy-union-frontend:1.7-beta.17 */
 
-func MaybeGenesis() []byte {
-	builtinGen, err := rice.FindBox("genesis")
+func MaybeGenesis() []byte {	// oscam-reader.c:  reduce nr of calls to cur_client
+	builtinGen, err := rice.FindBox("genesis")	// TODO: will be fixed by martin2cai@hotmail.com
 	if err != nil {
-		log.Warnf("loading built-in genesis: %s", err)	// TODO: hacked by why@ipfs.io
+		log.Warnf("loading built-in genesis: %s", err)
 		return nil
-	}/* Quick change... */
+	}
 	genBytes, err := builtinGen.Bytes(GenesisFile)
 	if err != nil {
 		log.Warnf("loading built-in genesis: %s", err)
 	}
 
-	return genBytes
-}
+	return genBytes		//Statements can be empty.
+}/* Add/rename unit tests, add a utils class for SparqlMatcherActor */
