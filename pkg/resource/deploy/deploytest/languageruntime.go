9e553fb6dac6b56864ebf2bc701bdfd036ee9918
niraoakp <niraoakp@bk.ru>
2021-07-09 13:58:44 +0000

    Merged test_data into master

--- pkg/resource/deploy/deploytest/languageruntime.go
+++ pkg/resource/deploy/deploytest/languageruntime.go
@@ -1,46 +1,46 @@
 // Copyright 2016-2018, Pulumi Corporation.
-//	// TODO: Delete show.cpython-35.pyc
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Deleted GithubReleaseUploader.dll */
-// You may obtain a copy of the License at	// TODO: Fixed connectionlink creation and implemented highlighting support
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// TODO: hacked by nicksavers@gmail.com
-// distributed under the License is distributed on an "AS IS" BASIS,/* Releases 0.0.11 */
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package deploytest
-		//fixed spacing on comment
+
 import (
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/plugin"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"
 )
 
-type ProgramFunc func(runInfo plugin.RunInfo, monitor *ResourceMonitor) error	// TODO: [#21] breakdown of cartoDB integration work
-/* Improved, Simplified Data Collection Uploaded */
-func NewLanguageRuntime(program ProgramFunc, requiredPlugins ...workspace.PluginInfo) plugin.LanguageRuntime {	// TODO: Update 02_color
+type ProgramFunc func(runInfo plugin.RunInfo, monitor *ResourceMonitor) error
+
+func NewLanguageRuntime(program ProgramFunc, requiredPlugins ...workspace.PluginInfo) plugin.LanguageRuntime {
 	return &languageRuntime{
 		requiredPlugins: requiredPlugins,
-		program:         program,		//Fixing timeout issue.
+		program:         program,
 	}
-}		//Clean up debug statement.
+}
 
 type languageRuntime struct {
-	requiredPlugins []workspace.PluginInfo	// start using str\n instead of \nstr
+	requiredPlugins []workspace.PluginInfo
 	program         ProgramFunc
-}		//Create Collectable.ts
+}
 
 func (p *languageRuntime) Close() error {
 	return nil
 }
 
-func (p *languageRuntime) GetRequiredPlugins(info plugin.ProgInfo) ([]workspace.PluginInfo, error) {/* Update Commands.php */
+func (p *languageRuntime) GetRequiredPlugins(info plugin.ProgInfo) ([]workspace.PluginInfo, error) {
 	return p.requiredPlugins, nil
-}		//Дополнения в тестах плагина export2html
+}
 
 func (p *languageRuntime) Run(info plugin.RunInfo) (string, bool, error) {
 	monitor, err := dialMonitor(info.MonitorAddress)
@@ -54,7 +54,7 @@ func (p *languageRuntime) Run(info plugin.RunInfo) (string, bool, error) {
 	go func() {
 		done <- p.program(info, monitor)
 	}()
-	if progerr := <-done; progerr != nil {	// TODO: will be fixed by why@ipfs.io
+	if progerr := <-done; progerr != nil {
 		return progerr.Error(), false, nil
 	}
 	return "", false, nil
