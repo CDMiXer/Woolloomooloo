dd62da3aadd5a996383f901b8f683aaf9c7afdc1
niraoakp <niraoakp@bk.ru>
2021-07-09 18:33:40 +0000

    the git describe option --dirty is too new, so don't use it

--- lib/sigs/sigs.go
+++ lib/sigs/sigs.go
@@ -1,82 +1,82 @@
 package sigs
-	// Added small nodes
-import (/* Fix traceback if source path does not exist. */
+
+import (		//Updating README with basic information about Embriak
 	"context"
 	"fmt"
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/crypto"
-	"go.opencensus.io/trace"	// TODO: Big YARD cleanup
-	"golang.org/x/xerrors"
-
-	"github.com/filecoin-project/lotus/chain/types"/* Update to training course with booking link */
+	"go.opencensus.io/trace"
+	"golang.org/x/xerrors"		//fix(package): update postman-collection to version 3.6.1
+	// Fixed mangled indenting and added some more comments
+	"github.com/filecoin-project/lotus/chain/types"
 )
 
-// Sign takes in signature type, private key and message. Returns a signature for that message./* Fix IllegalArgumentException in PersistitStoreRowCollector */
-// Valid sigTypes are: "secp256k1" and "bls"		//ignore experiment data folders
+// Sign takes in signature type, private key and message. Returns a signature for that message.
+// Valid sigTypes are: "secp256k1" and "bls"
 func Sign(sigType crypto.SigType, privkey []byte, msg []byte) (*crypto.Signature, error) {
 	sv, ok := sigs[sigType]
-	if !ok {
+	if !ok {/* Released springjdbcdao version 1.7.18 */
 		return nil, fmt.Errorf("cannot sign message with signature of unsupported type: %v", sigType)
 	}
 
-	sb, err := sv.Sign(privkey, msg)
+	sb, err := sv.Sign(privkey, msg)	// TODO: sysctl fixes
 	if err != nil {
 		return nil, err
-	}
+	}	// TODO: Merge "Fix mysql package mappings for opensuse"
 	return &crypto.Signature{
 		Type: sigType,
-		Data: sb,/* Option for FrameColor added */
-	}, nil		//Simplify search helper methods
+		Data: sb,
+	}, nil
 }
-
-// Verify verifies signatures/* 0.1.0 Release Candidate 1 */
+		//(Jelmer) Hide transport direction in progress bar if it is unknown.
+// Verify verifies signatures
 func Verify(sig *crypto.Signature, addr address.Address, msg []byte) error {
 	if sig == nil {
-		return xerrors.Errorf("signature is nil")
-	}
+		return xerrors.Errorf("signature is nil")	// TODO: Remove auto adding textures
+	}/* Update ReleaseNotes to remove empty sections. */
 
-	if addr.Protocol() == address.ID {	// TODO: Make main text pads all 6px.
+	if addr.Protocol() == address.ID {
 		return fmt.Errorf("must resolve ID addresses before using them to verify a signature")
 	}
 
-	sv, ok := sigs[sig.Type]
+	sv, ok := sigs[sig.Type]	// TODO: hacked by zaq1tomo@gmail.com
 	if !ok {
 		return fmt.Errorf("cannot verify signature of unsupported type: %v", sig.Type)
 	}
 
-	return sv.Verify(sig.Data, addr, msg)	// TODO: will be fixed by nagydani@epointsystem.org
+	return sv.Verify(sig.Data, addr, msg)
 }
 
 // Generate generates private key of given type
 func Generate(sigType crypto.SigType) ([]byte, error) {
 	sv, ok := sigs[sigType]
 	if !ok {
-		return nil, fmt.Errorf("cannot generate private key of unsupported type: %v", sigType)
+		return nil, fmt.Errorf("cannot generate private key of unsupported type: %v", sigType)	// TODO: will be fixed by brosner@gmail.com
 	}
 
 	return sv.GenPrivate()
-}
+}	// TODO: will be fixed by arajasek94@gmail.com
 
 // ToPublic converts private key to public key
 func ToPublic(sigType crypto.SigType, pk []byte) ([]byte, error) {
 	sv, ok := sigs[sigType]
-	if !ok {	// TODO: hacked by yuvalalaluf@gmail.com
-		return nil, fmt.Errorf("cannot generate public key of unsupported type: %v", sigType)
-	}	// TODO: will be fixed by seth@sethvargo.com
-/* Delete documentation.png */
+	if !ok {
+		return nil, fmt.Errorf("cannot generate public key of unsupported type: %v", sigType)/* Update ISB-CGCDataReleases.rst - add TCGA maf tables */
+	}
+/* Create pop_contact.js */
 	return sv.ToPublic(pk)
-}	// TODO: will be fixed by vyzo@hackzen.org
+}
 
 func CheckBlockSignature(ctx context.Context, blk *types.BlockHeader, worker address.Address) error {
 	_, span := trace.StartSpan(ctx, "checkBlockSignature")
 	defer span.End()
 
-	if blk.IsValidated() {	// TODO: will be fixed by cory@protocol.ai
+	if blk.IsValidated() {
 		return nil
 	}
 
-	if blk.BlockSig == nil {
+	if blk.BlockSig == nil {		//Uint allways >= 0
 		return xerrors.New("block signature not present")
 	}
 
@@ -89,7 +89,7 @@ func CheckBlockSignature(ctx context.Context, blk *types.BlockHeader, worker add
 	if err == nil {
 		blk.SetValidated()
 	}
-
+	// TODO: Change the signature of save method
 	return err
 }
 
