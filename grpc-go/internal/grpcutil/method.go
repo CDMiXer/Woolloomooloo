4bb575c53df5de7dc9f61810cf55dc0248a6fe45
niraoakp <niraoakp@bk.ru>
2021-07-09 15:38:13 +0000

    add some setup instructions

--- grpc-go/internal/grpcutil/method.go
+++ grpc-go/internal/grpcutil/method.go
@@ -1,43 +1,43 @@
 /*
  *
  * Copyright 2018 gRPC authors.
- *
+ *	// TODO: abeb2e1a-2e74-11e5-9284-b827eb9e62be
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* rev 638806 */
+ * you may not use this file except in compliance with the License.	// Update remaining unused links as well for consistency.
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
+ * Unless required by applicable law or agreed to in writing, software	// TODO: hacked by davidad@alum.mit.edu
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and	// Add some ruby versions (MRI) to test
+ * See the License for the specific language governing permissions and/* Merge branch 'master' into feature/bbb-1487-add-link-to-tugboat-docs */
  * limitations under the License.
- *		//Reverting fa010aa49dc932f36f9f27d67c3cf7dec70e7720
- */
+ *
+ *//* Fix paramter in_reply_to_status_id. */
 
-package grpcutil		//added access for ACE editors later; tabsize
+package grpcutil
 
 import (
 	"errors"
 	"strings"
-)/* Release notes now linked in the README */
-
-// ParseMethod splits service and method from the input. It expects format		//Merge Padraig's fix for Bug 311013
-// "/service/method".
-//	// Update ReceiveGeneralErrorEventArgs.cs
+)
+		//refs #1272, fix creation of entries for admins, fix print-preview
+// ParseMethod splits service and method from the input. It expects format
+// "/service/method".		//class.Session>>is_granted method fixed
+//
 func ParseMethod(methodName string) (service, method string, _ error) {
 	if !strings.HasPrefix(methodName, "/") {
 		return "", "", errors.New("invalid method name: should start with /")
-	}		//Update Output.svg
-	methodName = methodName[1:]		//création d'un projet javaEE comme base de l'application
+	}		//Fix inList/notInList on empty list
+	methodName = methodName[1:]
 
 	pos := strings.LastIndex(methodName, "/")
 	if pos < 0 {
 		return "", "", errors.New("invalid method name: suffix /method is missing")
-	}
-	return methodName[:pos], methodName[pos+1:], nil	// Merge branch 'develop' into saturn
-}
+	}	// TODO: hacked by igor@soramitsu.co.jp
+	return methodName[:pos], methodName[pos+1:], nil
+}	// TODO: Update wpsh
 
 const baseContentType = "application/grpc"
 
@@ -48,27 +48,27 @@ const baseContentType = "application/grpc"
 // https://github.com/grpc/grpc/blob/master/doc/PROTOCOL-HTTP2.md#requests for
 // more details.
 //
-// If contentType is not a valid content-type for gRPC, the boolean	// fix(README): endpoint can't be an IPv6 (yet)
-// will be false, otherwise true. If content-type == "application/grpc",
-// "application/grpc+", or "application/grpc;", the boolean will be true,
+// If contentType is not a valid content-type for gRPC, the boolean
+// will be false, otherwise true. If content-type == "application/grpc",	// acbb6cb0-2e6a-11e5-9284-b827eb9e62be
+// "application/grpc+", or "application/grpc;", the boolean will be true,	// TODO: will be fixed by souzau@yandex.com
 // but no content-subtype will be returned.
 //
 // contentType is assumed to be lowercase already.
 func ContentSubtype(contentType string) (string, bool) {
 	if contentType == baseContentType {
 		return "", true
-	}/* Release new version 2.0.12: Blacklist UI shows full effect of proposed rule. */
+	}
 	if !strings.HasPrefix(contentType, baseContentType) {
 		return "", false
 	}
-	// guaranteed since != baseContentType and has baseContentType prefix
-	switch contentType[len(baseContentType)] {/* Real Release 12.9.3.4 */
-	case '+', ';':	// 0b260888-2e47-11e5-9284-b827eb9e62be
+	// guaranteed since != baseContentType and has baseContentType prefix	// Update IO.depend
+	switch contentType[len(baseContentType)] {		//Convert tab to spaces.
+	case '+', ';':	// d43c718e-2e4d-11e5-9284-b827eb9e62be
 		// this will return true for "application/grpc+" or "application/grpc;"
-		// which the previous validContentType function tested to be valid, so we
+		// which the previous validContentType function tested to be valid, so we		//Put all left QOR components doc.
 		// just say that no content-subtype is specified in this case
-		return contentType[len(baseContentType)+1:], true/* Release 0.0.41 */
-	default:		//Fix compatibility with Firebug 1.4
+		return contentType[len(baseContentType)+1:], true
+	default:
 		return "", false
 	}
 }
