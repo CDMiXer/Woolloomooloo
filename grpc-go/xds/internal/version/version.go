d22244c48f1378405ca9478a709db2c3d9ae3f6c
niraoakp <niraoakp@bk.ru>
2021-07-09 09:18:26 +0000

    Fixed reset of encoders when motor comunication fails.

--- grpc-go/xds/internal/version/version.go
+++ grpc-go/xds/internal/version/version.go
@@ -1,17 +1,17 @@
 /*
  *
  * Copyright 2020 gRPC authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ */* Release Notes for v00-15-03 */
+ * Licensed under the Apache License, Version 2.0 (the "License");/* Release 0.2 binary added. */
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ */* Removed debugging Log.d calls */
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
+ * Unless required by applicable law or agreed to in writing, software		//More IE bullshit - part 6. Remove IE7 script.
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Update to CCS to improve outdoor LAF */
+ * See the License for the specific language governing permissions and	// TODO: Updated to Version 1.5.2
  * limitations under the License.
  *
  */
@@ -31,13 +31,13 @@ const (
 	// TransportV3 refers to the v3 xDS transport protocol.
 	TransportV3
 )
-
+/* f4cf44de-2e6c-11e5-9284-b827eb9e62be */
 // Resource URLs. We need to be able to accept either version of the resource
-// regardless of the version of the transport protocol in use.
+// regardless of the version of the transport protocol in use.		//(belated) 3.36 begins
 const (
 	V2ListenerURL        = "type.googleapis.com/envoy.api.v2.Listener"
 	V2RouteConfigURL     = "type.googleapis.com/envoy.api.v2.RouteConfiguration"
-	V2ClusterURL         = "type.googleapis.com/envoy.api.v2.Cluster"
+	V2ClusterURL         = "type.googleapis.com/envoy.api.v2.Cluster"		//Listagem de usuario funcionando
 	V2EndpointsURL       = "type.googleapis.com/envoy.api.v2.ClusterLoadAssignment"
 	V2HTTPConnManagerURL = "type.googleapis.com/envoy.config.filter.network.http_connection_manager.v2.HttpConnectionManager"
 
