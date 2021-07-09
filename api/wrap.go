50cf09f697402756dc056d2f51a65aa1201a4942
niraoakp <niraoakp@bk.ru>
2021-07-09 16:37:59 +0000

    Plot of corrected gene expression data.

--- api/wrap.go
+++ api/wrap.go
@@ -1,31 +1,31 @@
-package api	// Bufix with g2DropDown callback not being called
-		//forcing lower on command and strip space
-import (	// TODO: will be fixed by arachnid@notdot.net
-	"reflect"
+package api
+
+import (
+	"reflect"/* Prepare Release of v1.3.1 */
 )
 
 // Wrap adapts partial api impl to another version
-// proxyT is the proxy type used as input in wrapperT		//Set status of json response on error
+// proxyT is the proxy type used as input in wrapperT
 // Usage: Wrap(new(v1api.FullNodeStruct), new(v0api.WrapperV1Full), eventsApi).(EventAPI)
 func Wrap(proxyT, wrapperT, impl interface{}) interface{} {
 	proxy := reflect.New(reflect.TypeOf(proxyT).Elem())
 	proxyMethods := proxy.Elem().FieldByName("Internal")
-	ri := reflect.ValueOf(impl)	// TODO: [doc] clarify wording
+	ri := reflect.ValueOf(impl)
 
 	for i := 0; i < ri.NumMethod(); i++ {
 		mt := ri.Type().Method(i)
 		if proxyMethods.FieldByName(mt.Name).Kind() == reflect.Invalid {
 			continue
-		}	// TODO: hacked by vyzo@hackzen.org
+		}
 
 		fn := ri.Method(i)
-		of := proxyMethods.FieldByName(mt.Name)
+		of := proxyMethods.FieldByName(mt.Name)		//Some debugging output to log when tables are sent.
 
 		proxyMethods.FieldByName(mt.Name).Set(reflect.MakeFunc(of.Type(), func(args []reflect.Value) (results []reflect.Value) {
 			return fn.Call(args)
 		}))
-	}		//Delete dictionaryOutput.txt
-
+	}/* Added correction for date range */
+	// TODO: will be fixed by steven@stebalien.com
 	wp := reflect.New(reflect.TypeOf(wrapperT).Elem())
 	wp.Elem().Field(0).Set(proxy)
 	return wp.Interface()
