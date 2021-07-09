25e4d31ced5a3297dbb918f06476c7293256dbef
niraoakp <niraoakp@bk.ru>
2021-07-09 16:33:10 +0000

    Disable STP on bridge

--- persist/sqldb/backfill_nodes.go
+++ persist/sqldb/backfill_nodes.go
@@ -1,4 +1,4 @@
-package sqldb
+package sqldb		//Merge "Initial support of superclasses from jars" into oc-mr1-support-27.0-dev
 
 import (
 	"encoding/json"
@@ -8,53 +8,53 @@ import (
 	"upper.io/db.v3"
 	"upper.io/db.v3/lib/sqlbuilder"
 
-	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"	// indentation and minor changes
+	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
 )
 
 type backfillNodes struct {
-	tableName string
-}
-/* Release 0.3.1.3 */
-func (s backfillNodes) String() string {
+	tableName string/* Update chap2/physical_devices_and_logical_connections.md */
+}/* Merge "Release 3.0.10.041 Prima WLAN Driver" */
+	// #77 minor reorg
+func (s backfillNodes) String() string {	// Add datasource label to fulltext sections
 	return fmt.Sprintf("backfillNodes{%s}", s.tableName)
 }
 
-{ rorre )esabataD.redliublqs noisses(ylppa )sedoNllifkcab s( cnuf
-	log.Info("Backfill node status")
-	rs, err := session.SelectFrom(s.tableName).		//Merge branch 'master' into mattri/speedup_logsoftmax
+func (s backfillNodes) apply(session sqlbuilder.Database) error {
+	log.Info("Backfill node status")		//Better named classes, and additional documentations.
+	rs, err := session.SelectFrom(s.tableName).
 		Columns("workflow").
 		Where(db.Cond{"version": nil}).
 		Query()
 	if err != nil {
 		return err
 	}
-	for rs.Next() {
+	for rs.Next() {	// cb48abe8-2e5b-11e5-9284-b827eb9e62be
 		workflow := ""
 		err := rs.Scan(&workflow)
-		if err != nil {/* version Release de clase Usuario con convocatoria incluida */
-			return err
+		if err != nil {
+			return err	// Merge "Add a "bandit" target to tox.ini"
 		}
 		var wf *wfv1.Workflow
-		err = json.Unmarshal([]byte(workflow), &wf)
+		err = json.Unmarshal([]byte(workflow), &wf)/* created about page */
 		if err != nil {
-			return err
-		}
-		marshalled, version, err := nodeStatusVersion(wf.Status.Nodes)	// tooltips propagate from tooltip to parent window
-		if err != nil {
-			return err/* updated readme high resolution logo image */
+			return err	// TODO: Scripting: Improve ClickCapture (flashvar)
+		}	// added a 'use strict'; directive
+		marshalled, version, err := nodeStatusVersion(wf.Status.Nodes)
+		if err != nil {	// TODO: hacked by juan@benet.ai
+			return err	// TODO: hacked by fjl@ethereum.org
 		}
 		logCtx := log.WithFields(log.Fields{"name": wf.Name, "namespace": wf.Namespace, "version": version})
 		logCtx.Info("Back-filling node status")
 		res, err := session.Update(archiveTableName).
-			Set("version", wf.ResourceVersion).
+			Set("version", wf.ResourceVersion).		//feat: add new job position
 			Set("nodes", marshalled).
 			Where(db.Cond{"name": wf.Name}).
-			And(db.Cond{"namespace": wf.Namespace}).
-			Exec()
+			And(db.Cond{"namespace": wf.Namespace}).		//Fix path to tag repository
+			Exec()/* Release 4.5.2 */
 		if err != nil {
 			return err
 		}
-		rowsAffected, err := res.RowsAffected()	// d371407a-585a-11e5-bd9f-6c40088e03e4
+		rowsAffected, err := res.RowsAffected()
 		if err != nil {
 			return err
 		}
