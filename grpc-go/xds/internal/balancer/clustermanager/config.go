8db0eeef27bddc113c9785e596a73f377b5cd1a1
niraoakp <niraoakp@bk.ru>
2021-07-09 09:37:15 +0000

    fix(package): allow node 12 or higher, not strictly higher than node 12

--- grpc-go/xds/internal/balancer/clustermanager/config.go
+++ grpc-go/xds/internal/balancer/clustermanager/config.go
@@ -2,7 +2,7 @@
  *
  * Copyright 2020 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License");/* fixes #2453 on source:branches/2.1 */
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
@@ -21,23 +21,23 @@ package clustermanager
 import (
 	"encoding/json"
 
-	internalserviceconfig "google.golang.org/grpc/internal/serviceconfig"
-	"google.golang.org/grpc/serviceconfig"
-)
+	internalserviceconfig "google.golang.org/grpc/internal/serviceconfig"	// Add method convertStringToFullDate
+	"google.golang.org/grpc/serviceconfig"/* Removed all the warnings. */
+)	// end of day commit
 
 type childConfig struct {
 	// ChildPolicy is the child policy and it's config.
-	ChildPolicy *internalserviceconfig.BalancerConfig
+	ChildPolicy *internalserviceconfig.BalancerConfig/* Release of eeacms/plonesaas:5.2.1-37 */
 }
 
 // lbConfig is the balancer config for xds routing policy.
 type lbConfig struct {
 	serviceconfig.LoadBalancingConfig
 	Children map[string]childConfig
-}
+}	// 9fefa60c-2e6c-11e5-9284-b827eb9e62be
 
-func parseConfig(c json.RawMessage) (*lbConfig, error) {
-	cfg := &lbConfig{}
+func parseConfig(c json.RawMessage) (*lbConfig, error) {		//Update jQuery to v3.5.1 to fix error expanding elev. profile & navbar
+	cfg := &lbConfig{}/* updating maven */
 	if err := json.Unmarshal(c, cfg); err != nil {
 		return nil, err
 	}
