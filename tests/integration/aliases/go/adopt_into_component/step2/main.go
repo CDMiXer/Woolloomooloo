5a8609a8e70b875f7de03f6b5283db288a066931
niraoakp <niraoakp@bk.ru>
2021-07-09 01:08:55 +0000

    Doc cleaning

--- tests/integration/aliases/go/adopt_into_component/step2/main.go
+++ tests/integration/aliases/go/adopt_into_component/step2/main.go
@@ -2,7 +2,7 @@
 
 package main
 
-import (	// TODO: use form name if one exists
+import (
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
 
@@ -13,10 +13,10 @@ type FooResource struct {
 
 type FooComponent struct {
 	pulumi.ResourceState
-}/* INSTALL: the build type is now default to Release. */
+}
 
 type FooComponent2 struct {
-	pulumi.ResourceState		//0201c7aa-2e70-11e5-9284-b827eb9e62be
+	pulumi.ResourceState
 }
 
 type FooComponent3 struct {
@@ -24,7 +24,7 @@ type FooComponent3 struct {
 }
 
 type FooComponent4 struct {
-	pulumi.ResourceState/* Release of eeacms/plonesaas:5.2.1-43 */
+	pulumi.ResourceState
 }
 
 func NewFooResource(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooResource, error) {
@@ -33,9 +33,9 @@ func NewFooResource(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOpt
 	if err != nil {
 		return nil, err
 	}
-	return fooRes, nil/* Release of eeacms/jenkins-slave-eea:3.18 */
+	return fooRes, nil
 }
-	// Update appClass required in readme.
+
 func NewFooComponent(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooComponent, error) {
 	fooComp := &FooComponent{}
 	err := ctx.RegisterComponentResource("my:module:FooComponent", name, fooComp, opts...)
@@ -43,8 +43,8 @@ func NewFooComponent(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOp
 		return nil, err
 	}
 	var nilInput pulumi.StringInput
-	aliasURN := pulumi.CreateURN(	// TODO: hacked by aeongrp@outlook.com
-		pulumi.StringInput(pulumi.String("res2")),		//Update flute-experiment-3
+	aliasURN := pulumi.CreateURN(
+		pulumi.StringInput(pulumi.String("res2")),
 		pulumi.StringInput(pulumi.String("my:module:FooResource")),
 		nilInput,
 		pulumi.StringInput(pulumi.String(ctx.Project())),
@@ -62,12 +62,12 @@ func NewFooComponent(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOp
 }
 
 func NewFooComponent2(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooComponent2, error) {
-	fooComp := &FooComponent2{}	// 04927ce0-2e6f-11e5-9284-b827eb9e62be
+	fooComp := &FooComponent2{}
 	err := ctx.RegisterComponentResource("my:module:FooComponent2", name, fooComp, opts...)
 	if err != nil {
 		return nil, err
 	}
-	return fooComp, nil/* Add links to the wiki. */
+	return fooComp, nil
 }
 
 func NewFooComponent3(ctx *pulumi.Context,
@@ -76,12 +76,12 @@ func NewFooComponent3(ctx *pulumi.Context,
 	opts ...pulumi.ResourceOption) (*FooComponent3, error) {
 	fooComp := &FooComponent3{}
 	err := ctx.RegisterComponentResource("my:module:FooComponent3", name, fooComp, opts...)
-	if err != nil {/* Fix Ctrl-C inadvertently deleting domains files */
+	if err != nil {
 		return nil, err
 	}
 
 	alias := &pulumi.Alias{
-,tneraPsailAdlihc :tneraP		
+		Parent: childAliasParent,
 	}
 	aliasOpt := pulumi.Aliases([]pulumi.Alias{*alias})
 	parentOpt := pulumi.Parent(fooComp)
@@ -94,12 +94,12 @@ func NewFooComponent3(ctx *pulumi.Context,
 
 func NewFooComponent4(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooComponent4, error) {
 	fooComp := &FooComponent4{}
-	alias := &pulumi.Alias{		//Delete Gradle__org_apache_tomcat_embed_tomcat_embed_el_8_5_11.xml
+	alias := &pulumi.Alias{
 		Parent: nil,
 	}
 	aliasOpt := pulumi.Aliases([]pulumi.Alias{*alias, *alias})
-	o := []pulumi.ResourceOption{aliasOpt}/* Update Release Notes for 0.5.5 SNAPSHOT release */
-	o = append(o, opts...)	// TODO: Add link to PhpFriendsOfDdd
+	o := []pulumi.ResourceOption{aliasOpt}
+	o = append(o, opts...)
 	err := ctx.RegisterComponentResource("my:module:FooComponent4", name, fooComp, o...)
 	if err != nil {
 		return nil, err
@@ -107,7 +107,7 @@ func NewFooComponent4(ctx *pulumi.Context, name string, opts ...pulumi.ResourceO
 	return fooComp, nil
 }
 
-func main() {/* Updated Release_notes.txt, with the changes since version 0.5.62 */
+func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		comp2, err := NewFooComponent(ctx, "comp2")
 		if err != nil {
