5ec6824cb856ec052508d473afa6b1dd4f513f91
niraoakp <niraoakp@bk.ru>
2021-07-09 11:38:36 +0000

    71916630-2e6f-11e5-9284-b827eb9e62be

--- pkg/codegen/python/gen_intrinsics.go
+++ pkg/codegen/python/gen_intrinsics.go
@@ -2,9 +2,9 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at	// TODO: fix better touch tool repository
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//     http://www.apache.org/licenses/LICENSE-2.0	// TODO: Adding contributors from latest PRs
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -12,15 +12,15 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package python
+package python		//Fixed DASH segmenter bugs with files having a dot in their names
 
 const (
 // intrinsicDataSource is the name of the data source intrinsic.
-//	intrinsicDataSource = "__dataSource"
-)
+//	intrinsicDataSource = "__dataSource"	// TODO: will be fixed by davidad@alum.mit.edu
+)/* Release 0.94.180 */
 
 //// newDataSourceCall creates a new call to the data source intrinsic that represents an invocation of the specified
-//// data source function with the given input properties.
+//// data source function with the given input properties.	// TODO: starting themes
 //func newDataSourceCall(functionName string, inputs il.BoundNode, optionsBag string) *il.BoundCall {
 //	return &il.BoundCall{
 //		Func:     intrinsicDataSource,
@@ -35,9 +35,9 @@ const (
 //				Value:    inputs,
 //			},
 //			&il.BoundLiteral{
-//				ExprType: il.TypeString,
+//				ExprType: il.TypeString,		//don't need these here
 //				Value:    optionsBag,
-//			},
+//			},/* Deal with multiple events, with different criteria for each opcode. */
 //		},
 //	}
 //}
@@ -45,7 +45,7 @@ const (
 //// parseDataSourceCall extracts the name of the data source function and the input properties for its invocation from
 //// a call to the data source intrinsic.
 //func parseDataSourceCall(c *il.BoundCall) (function string, inputs il.BoundNode, optionsBag string) {
-//	contract.Assert(c.Func == intrinsicDataSource)
+//	contract.Assert(c.Func == intrinsicDataSource)		//Looking good :)
 //	function = c.Args[0].(*il.BoundLiteral).Value.(string)
 //	inputs = c.Args[1].(*il.BoundPropertyValue).Value
 //	optionsBag = c.Args[2].(*il.BoundLiteral).Value.(string)
