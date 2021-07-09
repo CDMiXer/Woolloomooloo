cc9be632c736dff75cc78f6646f5a9c7d2314b18
niraoakp <niraoakp@bk.ru>
2021-07-09 02:49:15 +0000

    Modif commentaire

--- plugin/converter/starlark_oss.go
+++ plugin/converter/starlark_oss.go
@@ -1,27 +1,27 @@
 // Copyright 2019 Drone IO, Inc.
-//
+//	// TODO: will be fixed by sebastian.tharakan97@gmail.com
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-///* 9d767194-2e67-11e5-9284-b827eb9e62be */
-//      http://www.apache.org/licenses/LICENSE-2.0/* Update testingMarkdown.md */
+// You may obtain a copy of the License at	// TODO: Create fetch.gs
+//
+//      http://www.apache.org/licenses/LICENSE-2.0/* Added backends */
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,	// move block from preparser into parser
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// TODO: 82e46a48-2e67-11e5-9284-b827eb9e62be
-// limitations under the License./* Rename prepareRelease to prepareRelease.yml */
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-// +build oss/* Release 1.1.0-CI00271 */
+// +build oss
 
-package converter		//Scheduler.py - http://www.eventghost.net/forum/viewtopic.php?f=10&t=6145
-
-import (
-	"github.com/drone/drone/core"
+package converter/* Merge "Release 3.2.3.383 Prima WLAN Driver" */
+		//Create CountryAutocomplete.js
+import (/* Add title-lines mixin */
+	"github.com/drone/drone/core"/* Release areca-7.3.2 */
 )
 
-// Starlark returns a conversion service that converts the
-// starlark file to a yaml file.
-func Starlark(enabled bool) core.ConvertService {/* link-highlight */
-	return new(noop)		//Merge branch 'master' into dougsch/cdk-0340
+// Starlark returns a conversion service that converts the		//Add tests for Alert component
+// starlark file to a yaml file.	// TODO: "Qui sommes-nous" -> "A propos"
+func Starlark(enabled bool) core.ConvertService {
+	return new(noop)
 }
