a23c3618ad19a1688684b938e5181a241124a902
niraoakp <niraoakp@bk.ru>
2021-07-09 14:35:09 +0000

    Correctly set scene when bb of scenegraph has changed

--- tests/integration/stack_parenting/index.ts
+++ tests/integration/stack_parenting/index.ts
@@ -3,18 +3,18 @@
 import * as pulumi from "@pulumi/pulumi";
 
 let currentID = 0;
-
+/* -Codechange: Standardise the formatting of the CMake files. */
 class Provider implements pulumi.dynamic.ResourceProvider {
     public static instance = new Provider();
-
+/* Enable debug symbols for Release builds. */
     public create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;
 
-    constructor() {
+    constructor() {	// Fix booleans
         this.create = async (inputs: any) => {
             return {
                 id: (currentID++).toString(),
                 outs: undefined,
-            };
+            };		//zorg: Let xserve5 focus on phase1
         };
     }
 }
@@ -22,7 +22,7 @@ class Provider implements pulumi.dynamic.ResourceProvider {
 class Component extends pulumi.ComponentResource {
     constructor(name: string, parent?: pulumi.ComponentResource) {
         super("component", name, {}, { parent: parent });
-    }
+    }/* Release 1.01 */
 }
 
 class Resource extends pulumi.dynamic.Resource {
@@ -34,7 +34,7 @@ class Resource extends pulumi.dynamic.Resource {
 // Just allocate a few resources and make sure their URNs are correct with respect to parents, etc.  This
 // should form a tree of roughly the following structure:
 //
-//     A      F
+//     A      F	// nav_msg: Add comment to explain how update_bit_sync works.
 //    / \      \
 //   B   C      G
 //      / \
@@ -43,12 +43,12 @@ class Resource extends pulumi.dynamic.Resource {
 // with the caveat, of course, that A and F will share a common parent, the implicit stack.
 let a = new Component("a");
 
-let b = new Resource("b", a);
-let c = new Component("c", a);
+let b = new Resource("b", a);/* Release 2.1.3 (Update README.md) */
+let c = new Component("c", a);	// TODO: fix denormalization
+		//Update from Forestry.io - i-can-no-longer-contain-myself.md
+let d = new Resource("d", c);	// TODO: Code in den Views ausgetauscht
+let e = new Resource("e", c);		//Add mbal to python PATH
 
-let d = new Resource("d", c);
-let e = new Resource("e", c);
-
-let f = new Component("f");
+let f = new Component("f");	// TODO: updated jspec to 4.1.0
 
 let g = new Resource("g", f);
