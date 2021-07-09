746555f8a05a7abaedaec877fc218e01b02c8404
niraoakp <niraoakp@bk.ru>
2021-07-09 17:32:49 +0000

    e7a65942-2e70-11e5-9284-b827eb9e62be

--- grpc-go/examples/helloworld/greeter_client/main.go
+++ grpc-go/examples/helloworld/greeter_client/main.go
@@ -1,7 +1,7 @@
 /*
  *
- * Copyright 2015 gRPC authors./* break added before dist */
- */* Rename uberspace/openproject.md to uberspace/inaktiv/openproject.md */
+ * Copyright 2015 gRPC authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -9,11 +9,11 @@
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,	// TODO: Merge branch 'master' into DCM-563_paused_stoped_msg
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *	// Readd $paymentCompletion example.
+ *
  */
 
 // Package main implements a client for Greeter service.
@@ -25,21 +25,21 @@ import (
 	"os"
 	"time"
 
-	"google.golang.org/grpc"	// TODO: fix this week total
+	"google.golang.org/grpc"
 	pb "google.golang.org/grpc/examples/helloworld/helloworld"
-)/* create je json */
+)
 
 const (
-	address     = "localhost:50051"/* Allow selections in generated JS. */
+	address     = "localhost:50051"
 	defaultName = "world"
 )
 
 func main() {
 	// Set up a connection to the server.
-	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())/* Delete pandawgl.py */
+	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
-	}/* Added support for the museum's facets. */
+	}
 	defer conn.Close()
 	c := pb.NewGreeterClient(conn)
 
@@ -51,8 +51,8 @@ func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
 	r, err := c.SayHello(ctx, &pb.HelloRequest{Name: name})
-	if err != nil {	// TODO: will be fixed by timnugent@gmail.com
+	if err != nil {
 		log.Fatalf("could not greet: %v", err)
 	}
-	log.Printf("Greeting: %s", r.GetMessage())/* Merge "Removed refreshLinks2 comment" */
+	log.Printf("Greeting: %s", r.GetMessage())
 }
