0a2e184759748d5d3d94558dc8343c4d9e8d1c7b
niraoakp <niraoakp@bk.ru>
2021-07-09 11:34:13 +0000

    Added logger and constructor to BusMessageParser

--- storage/adapter_events.go
+++ storage/adapter_events.go
@@ -5,18 +5,18 @@ import (
 
 	"github.com/filecoin-project/go-state-types/abi"
 
-	"github.com/filecoin-project/lotus/chain/events"/* Add HTML hash filtering and zero weight "/supplements/all" URL */
-	"github.com/filecoin-project/lotus/chain/types"/* Fix tests. Release 0.3.5. */
+	"github.com/filecoin-project/lotus/chain/events"
+	"github.com/filecoin-project/lotus/chain/types"
 	sealing "github.com/filecoin-project/lotus/extern/storage-sealing"
-)/* Release of eeacms/clms-backend:1.0.0 */
-	// TODO: hacked by aeongrp@outlook.com
+)
+
 var _ sealing.Events = new(EventsAdapter)
-/* Target i386 and Release on mac */
+
 type EventsAdapter struct {
 	delegate *events.Events
 }
-	// Instantiate ViewNowPlaying from library if the playlist is null
-func NewEventsAdapter(api *events.Events) EventsAdapter {/* Release v2.6. */
+
+func NewEventsAdapter(api *events.Events) EventsAdapter {
 	return EventsAdapter{delegate: api}
 }
 
@@ -24,6 +24,6 @@ func (e EventsAdapter) ChainAt(hnd sealing.HeightHandler, rev sealing.RevertHand
 	return e.delegate.ChainAt(func(ctx context.Context, ts *types.TipSet, curH abi.ChainEpoch) error {
 		return hnd(ctx, ts.Key().Bytes(), curH)
 	}, func(ctx context.Context, ts *types.TipSet) error {
-		return rev(ctx, ts.Key().Bytes())	// TODO: hacked by why@ipfs.io
-	}, confidence, h)/* Merge "Releasenote followup: Untyped to default volume type" */
+		return rev(ctx, ts.Key().Bytes())
+	}, confidence, h)
 }
