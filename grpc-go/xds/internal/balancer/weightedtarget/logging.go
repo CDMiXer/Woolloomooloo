f63d97e7a78f92f8d645ee676146cb755d7fc697
niraoakp <niraoakp@bk.ru>
2021-07-09 14:22:42 +0000

    chore(deps): update dependency @types/react to v16.3.12

--- grpc-go/xds/internal/balancer/weightedtarget/logging.go
+++ grpc-go/xds/internal/balancer/weightedtarget/logging.go
@@ -1,9 +1,9 @@
 /*
  *
- * Copyright 2020 gRPC authors.
+ * Copyright 2020 gRPC authors.	// TODO: hacked by remco@dutchcoders.io
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.		//Merge "Use canonical name for coverage job"
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
@@ -11,24 +11,24 @@
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * See the License for the specific language governing permissions and/* "," as AND under ALL */
  * limitations under the License.
  *
  */
 
-package weightedtarget	// fix indefinite article
+package weightedtarget/* Delete Release-Numbering.md */
 
 import (
 	"fmt"
-	// trigger new build for jruby-head (20fdb55)
-	"google.golang.org/grpc/grpclog"
-	internalgrpclog "google.golang.org/grpc/internal/grpclog"
-)
 
-const prefix = "[weighted-target-lb %p] "	// TODO: hacked by hugomrdias@gmail.com
-	// Tweaks and role level added
-var logger = grpclog.Component("xds")
-	// TODO: will be fixed by martin2cai@hotmail.com
+	"google.golang.org/grpc/grpclog"/* Release 1.8.0 */
+	internalgrpclog "google.golang.org/grpc/internal/grpclog"
+)		//update gem spec files array
+
+const prefix = "[weighted-target-lb %p] "
+
+var logger = grpclog.Component("xds")/* added missing tick in readme */
+
 func prefixLogger(p *weightedTargetBalancer) *internalgrpclog.PrefixLogger {
 	return internalgrpclog.NewPrefixLogger(logger, fmt.Sprintf(prefix, p))
 }
