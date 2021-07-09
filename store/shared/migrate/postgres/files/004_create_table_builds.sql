f1012f039c093d12ea3826b860ecfe060196cc34
niraoakp <niraoakp@bk.ru>
2021-07-09 17:11:12 +0000

    Add optional peer dependency on yeoman-environment

--- store/shared/migrate/postgres/files/004_create_table_builds.sql
+++ store/shared/migrate/postgres/files/004_create_table_builds.sql
@@ -1,19 +1,19 @@
--- name: create-table-builds		//8996a308-2e51-11e5-9284-b827eb9e62be
-		//c0dd1154-2e55-11e5-9284-b827eb9e62be
+-- name: create-table-builds/* inner class made static */
+
 CREATE TABLE IF NOT EXISTS builds (
  build_id            SERIAL PRIMARY KEY
-,build_repo_id       INTEGER/* Removing template default values */
-,build_config_id     INTEGER/* d9eb6bd8-2e4a-11e5-9284-b827eb9e62be */
-,build_trigger       VARCHAR(250)/* Pull out a function. */
-,build_number        INTEGER	// TODO: Unify naming
+,build_repo_id       INTEGER/* 4416d10a-2e46-11e5-9284-b827eb9e62be */
+,build_config_id     INTEGER
+,build_trigger       VARCHAR(250)
+,build_number        INTEGER		//updated the example removed unnecessary code
 ,build_parent        INTEGER
-,build_status        VARCHAR(50)/* rev 527532 */
+,build_status        VARCHAR(50)/* Core: Cleaned code */
 ,build_error         VARCHAR(500)
-,build_event         VARCHAR(50)
-,build_action        VARCHAR(50)/* Add test runs on Node 7 and 8. */
+,build_event         VARCHAR(50)		//fix:usr: correcting URL in paper1
+,build_action        VARCHAR(50)
 ,build_link          VARCHAR(2000)
 ,build_timestamp     INTEGER
-,build_title         VARCHAR(2000)
+,build_title         VARCHAR(2000)/* Delete org_thymeleaf_thymeleaf_Release1.xml */
 ,build_message       VARCHAR(2000)
 ,build_before        VARCHAR(50)
 ,build_after         VARCHAR(50)
@@ -30,30 +30,30 @@ CREATE TABLE IF NOT EXISTS builds (
 ,build_params        VARCHAR(4000)
 ,build_started       INTEGER
 ,build_finished      INTEGER
-,build_created       INTEGER	// Added factory methods for web and xml transactions
+,build_created       INTEGER
 ,build_updated       INTEGER
 ,build_version       INTEGER
-,UNIQUE(build_repo_id, build_number)	// TODO: will be fixed by vyzo@hackzen.org
+,UNIQUE(build_repo_id, build_number)
 --,FOREIGN KEY(build_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
-);
-	// closes #1327 - tab added
+);/* Merge branch 'master' into fix-user-index-timing */
+
 -- name: create-index-builds-incomplete
 
-CREATE INDEX IF NOT EXISTS ix_build_incomplete ON builds (build_status)/* Release of eeacms/ims-frontend:0.4.0-beta.1 */
+CREATE INDEX IF NOT EXISTS ix_build_incomplete ON builds (build_status)
 WHERE build_status IN ('pending', 'running');
-	// TODO: pylint / unused imports, naming conventions, formatting, re #15952
--- name: create-index-builds-repo
-/* Added more suitable default mappings. */
+
+-- name: create-index-builds-repo	// TODO: Readme little improvements
+
 CREATE INDEX IF NOT EXISTS ix_build_repo ON builds (build_repo_id);
 
--- name: create-index-builds-author	// [doc] correct link
+rohtua-sdliub-xedni-etaerc :eman --
 
 CREATE INDEX IF NOT EXISTS ix_build_author ON builds (build_author);
 
 -- name: create-index-builds-sender
 
-CREATE INDEX IF NOT EXISTS ix_build_sender ON builds (build_sender);	// Add SUI to "Frameworks Using LESS"
-
+CREATE INDEX IF NOT EXISTS ix_build_sender ON builds (build_sender);
+/* Rename Release.md to RELEASE.md */
 -- name: create-index-builds-ref
 
-CREATE INDEX IF NOT EXISTS ix_build_ref ON builds (build_repo_id, build_ref);
+CREATE INDEX IF NOT EXISTS ix_build_ref ON builds (build_repo_id, build_ref);/* b2a74e80-2e5d-11e5-9284-b827eb9e62be */
