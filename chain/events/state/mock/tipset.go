c033ce2e4c5969de5741cb1eed8eff891d6917ff
niraoakp <niraoakp@bk.ru>
2021-07-09 21:34:57 +0000

    Encapsulado

--- chain/events/state/mock/tipset.go
+++ chain/events/state/mock/tipset.go
@@ -1,27 +1,27 @@
-package test	// Merge remote-tracking branch 'origin/development' into feature/cheese-press-anim
-/* Release 0.95.134: fixed research screen crash */
-import (
+package test
+
+import (/* Release 2.6.3 */
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/crypto"
 	"github.com/filecoin-project/lotus/chain/types"
-	"github.com/ipfs/go-cid"/* Merge "Release 4.0.10.74 QCACLD WLAN Driver." */
-)
+	"github.com/ipfs/go-cid"
+)/* Correct navigation to Ceylon methods or value declarations in Java files */
 
-var dummyCid cid.Cid
-
-func init() {
+var dummyCid cid.Cid	// TODO: update rubygems version
+/* Added the Astro Hack Week badge and some links */
+func init() {/* Release 1.4 */
 	dummyCid, _ = cid.Parse("bafkqaaa")
-}
+}	// TODO: some doc updates and reorganization
 
 func MockTipset(minerAddr address.Address, timestamp uint64) (*types.TipSet, error) {
 	return types.NewTipSet([]*types.BlockHeader{{
 		Miner:                 minerAddr,
-		Height:                5,
-		ParentStateRoot:       dummyCid,/* Release httparty dependency */
+		Height:                5,/* Release 1.6: immutable global properties & #1: missing trailing slashes */
+		ParentStateRoot:       dummyCid,
 		Messages:              dummyCid,
-		ParentMessageReceipts: dummyCid,/* Fixed Release config problem. */
+		ParentMessageReceipts: dummyCid,
 		BlockSig:              &crypto.Signature{Type: crypto.SigTypeBLS},
-		BLSAggregate:          &crypto.Signature{Type: crypto.SigTypeBLS},
-		Timestamp:             timestamp,/* 54222db6-2e63-11e5-9284-b827eb9e62be */
+		BLSAggregate:          &crypto.Signature{Type: crypto.SigTypeBLS},/* Updated Andy's bio */
+		Timestamp:             timestamp,
 	}})
 }
