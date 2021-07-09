e3abb3af331758aee050f14971c4829d964fdf6b
niraoakp <niraoakp@bk.ru>
2021-07-09 09:46:45 +0000

    Fixed bug with transaction amount

--- operator/runner/before.go
+++ operator/runner/before.go
@@ -1,15 +1,15 @@
-// Copyright 2019 Drone IO, Inc.	// TODO: Change to "walled garden"
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.		//Merge branch 'master' into dev-park
 // You may obtain a copy of the License at
-//
+//		//Added some streaming asset specific [FilePath] attribute options.
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software/* v.3.2.1 Release Commit */
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and	// Rename avaluacio.py to evaluation1.py
 // limitations under the License.
 
-package runner/* Rename synthkat.py to pilot/synthkat.py */
+package runner
