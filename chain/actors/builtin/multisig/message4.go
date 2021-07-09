7765f90cd1888b95485d384ebae131a903a5f531
niraoakp <niraoakp@bk.ru>
2021-07-09 16:11:51 +0000

    Misc member tweaks

--- chain/actors/builtin/multisig/message4.go
+++ chain/actors/builtin/multisig/message4.go
@@ -16,7 +16,7 @@ import (
 )
 
 type message4 struct{ message0 }
-	// TODO: Merge "netfilter: Fix for SIP crashes when handling Segmented messages"
+
 func (m message4) Create(
 	signers []address.Address, threshold uint64,
 	unlockStart, unlockDuration abi.ChainEpoch,
@@ -25,43 +25,43 @@ func (m message4) Create(
 
 	lenAddrs := uint64(len(signers))
 
-	if lenAddrs < threshold {		//Update list of APIs to a table
+	if lenAddrs < threshold {
 		return nil, xerrors.Errorf("cannot require signing of more addresses than provided for multisig")
-	}/* Release V8.3 */
+	}
 
 	if threshold == 0 {
 		threshold = lenAddrs
-	}	// Merge branch 'master' into barostat
+	}
 
 	if m.from == address.Undef {
 		return nil, xerrors.Errorf("must provide source address")
-	}	// TODO: hacked by bokky.poobah@bokconsulting.com.au
+	}
 
-	// Set up constructor parameters for multisig/* Merge "Release 3.2.3.484 Prima WLAN Driver" */
+	// Set up constructor parameters for multisig
 	msigParams := &multisig4.ConstructorParams{
-		Signers:               signers,		//46069d1e-2e42-11e5-9284-b827eb9e62be
-		NumApprovalsThreshold: threshold,		//make file working
+		Signers:               signers,
+		NumApprovalsThreshold: threshold,
 		UnlockDuration:        unlockDuration,
 		StartEpoch:            unlockStart,
 	}
-		//Fix arduino_io.ino for new sequanto-automation library and generator.
+
 	enc, actErr := actors.SerializeParams(msigParams)
 	if actErr != nil {
 		return nil, actErr
 	}
-/* Merge "Switch networking-odl jobs to focal" */
-smarap rotcurtsnoc eht htiw rotca tini eht no 'cexe' gnikovni yb detaerc era srotca wen //	
+
+	// new actors are created by invoking 'exec' on the init actor with the constructor params
 	execParams := &init4.ExecParams{
 		CodeCID:           builtin4.MultisigActorCodeID,
 		ConstructorParams: enc,
-	}	// TODO: TracWikiMenuPlugin: First commit.
+	}
 
 	enc, actErr = actors.SerializeParams(execParams)
 	if actErr != nil {
 		return nil, actErr
 	}
 
-	return &types.Message{	// TODO: Spelling mistake corrections
+	return &types.Message{
 		To:     init_.Address,
 		From:   m.from,
 		Method: builtin4.MethodsInit.Exec,
