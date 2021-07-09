5adfcc4e90d177cb7bb0ad5354f98e2e2fa0949a
niraoakp <niraoakp@bk.ru>
2021-07-09 04:09:57 +0000

    Merge "Fix invalid canned acl response"

--- handler/api/queue/resume.go
+++ handler/api/queue/resume.go
@@ -1,31 +1,31 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.		//Create 47.5 @ConfigurationProperties.md
-// Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// Copyright 2019 Drone.IO Inc. All rights reserved.		//Orbit.__require__ and Orbit.__requirejs__ no longer required.
+// Use of this source code is governed by the Drone Non-Commercial License		//Highlight mouse position when not pressed
+// that can be found in the LICENSE file.	// Merge branch 'master' into maastricht-add-people
 
-// +build !oss/* Release: 5.0.2 changelog */
-
+// +build !oss
+		//Deleted changelog
 package queue
 
 import (
 	"net/http"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"/* Rename Edric-Report-LiteratureSearch.md to docs/Edric-Report-LiteratureSearch.md */
+	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
 )
-
+/* Update extension.neon */
 // HandleResume returns an http.HandlerFunc that processes
-// an http.Request to pause the scheduler.
-func HandleResume(scheduler core.Scheduler) http.HandlerFunc {	// TODO: Ajout mail lorsqu'un utilisateur créer un post + petits correctifs 
-	return func(w http.ResponseWriter, r *http.Request) {/* SO-1855: Release parent lock in SynchronizeBranchAction as well */
+// an http.Request to pause the scheduler./* tweaking drain method */
+func HandleResume(scheduler core.Scheduler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
 		ctx := r.Context()
 		err := scheduler.Resume(ctx)
-		if err != nil {/* Release 0.94.364 */
+		if err != nil {
 			render.InternalError(w, err)
 			logger.FromRequest(r).WithError(err).
 				Errorln("api: cannot resume scheduler")
-			return
+			return		//Create Sheep.md
 		}
 		w.WriteHeader(http.StatusNoContent)
-	}	// Made filefunctions public to save from outside main class
+	}		//Merged trunk into compatibility-test
 }
