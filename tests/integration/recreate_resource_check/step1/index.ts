2c42d138b88b167b9f15ee3f036e70cb09a087ab
niraoakp <niraoakp@bk.ru>
2021-07-09 23:23:03 +0000

    Added Function scopes

--- tests/integration/recreate_resource_check/step1/index.ts
+++ tests/integration/recreate_resource_check/step1/index.ts
@@ -1,5 +1,5 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.		//fix0red the build dependency issues. Closes #42
-	// Create navbar1
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+
 import { Resource } from "./resource";
 
 // Base depends on nothing.
