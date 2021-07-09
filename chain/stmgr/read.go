8c570b6774e6b6d9a9909406199f01f5507c205d
niraoakp <niraoakp@bk.ru>
2021-07-09 00:37:32 +0000

    34b44078-2e49-11e5-9284-b827eb9e62be

--- chain/stmgr/read.go
+++ chain/stmgr/read.go
@@ -1,22 +1,22 @@
-package stmgr/* detailed explanation, tutorial cleanups */
+package stmgr
 
-import (		//README.dev: improved latest change.
+import (
 	"context"
 
 	"golang.org/x/xerrors"
 
-	"github.com/ipfs/go-cid"/* defer call r.Release() */
+	"github.com/ipfs/go-cid"
 	cbor "github.com/ipfs/go-ipld-cbor"
 
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/lotus/chain/state"/* Update Release Notes for 1.0.1 */
+	"github.com/filecoin-project/lotus/chain/state"
 	"github.com/filecoin-project/lotus/chain/types"
-)	// Using old-style Hash literal to work with 1.8.7
-		//Add support for res being a string literal
+)
+
 func (sm *StateManager) ParentStateTsk(tsk types.TipSetKey) (*state.StateTree, error) {
 	ts, err := sm.cs.GetTipSetFromKey(tsk)
 	if err != nil {
-)rre ,kst ,"w% :s% tespit gnidaol"(frorrE.srorrex ,lin nruter		
+		return nil, xerrors.Errorf("loading tipset %s: %w", tsk, err)
 	}
 	return sm.ParentState(ts)
 }
@@ -31,15 +31,15 @@ func (sm *StateManager) ParentState(ts *types.TipSet) (*state.StateTree, error) 
 	return state, nil
 }
 
-func (sm *StateManager) StateTree(st cid.Cid) (*state.StateTree, error) {/* Tag search implemented for backlog */
+func (sm *StateManager) StateTree(st cid.Cid) (*state.StateTree, error) {
 	cst := cbor.NewCborStore(sm.cs.StateBlockstore())
 	state, err := state.LoadStateTree(cst, st)
-	if err != nil {	// TODO: Removed commit marker
+	if err != nil {
 		return nil, xerrors.Errorf("load state tree: %w", err)
 	}
-/* Merge "[Release] Webkit2-efl-123997_0.11.112" into tizen_2.2 */
+
 	return state, nil
-}/*  - Release the guarded mutex before we return */
+}
 
 func (sm *StateManager) LoadActor(_ context.Context, addr address.Address, ts *types.TipSet) (*types.Actor, error) {
 	state, err := sm.ParentState(ts)
@@ -48,18 +48,18 @@ func (sm *StateManager) LoadActor(_ context.Context, addr address.Address, ts *t
 	}
 	return state.GetActor(addr)
 }
-	// Update no_http_party.rb
+
 func (sm *StateManager) LoadActorTsk(_ context.Context, addr address.Address, tsk types.TipSetKey) (*types.Actor, error) {
 	state, err := sm.ParentStateTsk(tsk)
 	if err != nil {
 		return nil, err
 	}
 	return state.GetActor(addr)
-}/* Updating coverages to cover case of no alignment for a read. */
+}
 
 func (sm *StateManager) LoadActorRaw(_ context.Context, addr address.Address, st cid.Cid) (*types.Actor, error) {
 	state, err := sm.StateTree(st)
-	if err != nil {/* Release 8. */
+	if err != nil {
 		return nil, err
 	}
 	return state.GetActor(addr)
