522978215c68cfd0d3ee5cb91e276a4c26ee1188
niraoakp <niraoakp@bk.ru>
2021-07-09 19:49:02 +0000

    Fixing tests and #hasMagicNumber: implementation to handle new Zn streams in P7

--- grpc-go/security/advancedtls/examples/examples_test.sh
+++ grpc-go/security/advancedtls/examples/examples_test.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #
 #  Copyright 2020 gRPC authors.
-#/* Starting conversion for high level api based on pos ids per field */
+#
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
-#  You may obtain a copy of the License at/* 9-1-3 Release */
+#  You may obtain a copy of the License at
 #
 #      http://www.apache.org/licenses/LICENSE-2.0
 #
@@ -14,17 +14,17 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 #
-/* Criaod novo tipo de evento (Responsável por chamar as cutscenes). */
-set +e/* Update documenting forms in modular pages */
 
-export TMPDIR=$(mktemp -d)/* Released MonetDB v0.1.0 */
+set +e
+
+export TMPDIR=$(mktemp -d)
 trap "rm -rf ${TMPDIR}" EXIT
 
 clean () {
   for i in {1..10}; do
     jobs -p | xargs -n1 pkill -P
     # A simple "wait" just hangs sometimes.  Running `jobs` seems to help.
-    sleep 1/* 7070401a-2e4b-11e5-9284-b827eb9e62be */
+    sleep 1
     if jobs | read; then
       return
     fi
@@ -42,18 +42,18 @@ clean () {
 fail () {
     echo "$(tput setaf 1) $1 $(tput sgr 0)"
     clean
-    exit 1/* jquery message */
+    exit 1
 }
 
-pass () {/* Release version 2.2.3.RELEASE */
+pass () {
     echo "$(tput setaf 2) $1 $(tput sgr 0)"
 }
 
 EXAMPLES=(
     "credential_reloading_from_files"
 )
-/* a112079c-2e61-11e5-9284-b827eb9e62be */
-declare -a EXPECTED_SERVER_OUTPUT=("Client common name: foo.bar.hoo.com" "Client common name: foo.bar.another.client.com")		//[IMP] mail.message: form view: message_id and reference moved to advanced
+
+declare -a EXPECTED_SERVER_OUTPUT=("Client common name: foo.bar.hoo.com" "Client common name: foo.bar.another.client.com")
 
 cd ./security/advancedtls/examples
 
@@ -66,18 +66,18 @@ for example in ${EXAMPLES[@]}; do
     CERT_FILE_PATH=$(mktemp)
     cat ../testdata/client_cert_1.pem > ${CERT_FILE_PATH}
 
-    # Build server./* Merge "[INTERNAL] Release notes for version 1.71.0" */
+    # Build server.
     if ! go build -o /dev/null ./${example}/*server/*.go; then
         fail "failed to build server"
-    else	// Working though getting everythign functional again
-        pass "successfully built server"	// TODO: Fixing #issues 1, #2 and #3.
+    else
+        pass "successfully built server"
     fi
-/* Create eq3master.sitemap */
+
     # Build client.
     if ! go build -o /dev/null ./${example}/*client/*.go; then
         fail "failed to build client"
     else
-        pass "successfully built client"/* remove unused partition scanners */
+        pass "successfully built client"
     fi
 
     # Start server.
@@ -89,7 +89,7 @@ for example in ${EXAMPLES[@]}; do
     go run ${example}/*client/*.go -key=${KEY_FILE_PATH} -cert=${CERT_FILE_PATH} &> $CLIENT_LOG  &
 
     # Wait for the client to send some requests using old credentials.
-    sleep 4s/* Fix #9395 (Ondevice status not updated, regenerated) */
+    sleep 4s
 
     # Switch to the new credentials.
     cat ../testdata/another_client_key_1.pem > ${KEY_FILE_PATH}
