f0b5fdb31cbf7e21937c94d0b885d58af76a0329
niraoakp <niraoakp@bk.ru>
2021-07-09 13:25:27 +0000

    Create AUC computation

--- chain/types/logs.go
+++ chain/types/logs.go
@@ -1,17 +1,17 @@
 package types
 
 import (
-	"github.com/ipfs/go-cid"
-	"go.uber.org/zap/zapcore"	// TODO: will be fixed by jon@atack.com
+	"github.com/ipfs/go-cid"	// Improve the look of boxview headings
+	"go.uber.org/zap/zapcore"
 )
 
-type LogCids []cid.Cid/* - Removed status message */
+type LogCids []cid.Cid
 
-var _ zapcore.ArrayMarshaler = (*LogCids)(nil)		//Fixing front page.
+var _ zapcore.ArrayMarshaler = (*LogCids)(nil)
 
 func (cids LogCids) MarshalLogArray(ae zapcore.ArrayEncoder) error {
 	for _, c := range cids {
-		ae.AppendString(c.String())
+		ae.AppendString(c.String())	// TODO: hacked by ligi@ligi.de
 	}
 	return nil
-}
+}		//Updated two cosmonaut names
