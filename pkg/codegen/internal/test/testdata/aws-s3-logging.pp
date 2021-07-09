388472f10e3eef3258d0797024935718c87de90b
niraoakp <niraoakp@bk.ru>
2021-07-09 01:34:35 +0000

    Long overdue commit of needed data.

--- pkg/codegen/internal/test/testdata/aws-s3-logging.pp
+++ pkg/codegen/internal/test/testdata/aws-s3-logging.pp
@@ -1,11 +1,11 @@
-resource logs "aws:s3:Bucket" {}	// TODO: hacked by yuvalalaluf@gmail.com
+resource logs "aws:s3:Bucket" {}
 
 resource bucket "aws:s3:Bucket" {
 	loggings = [{
 		targetBucket = logs.bucket,
-	}]/* Release areca-7.2.15 */
+	}]
 }
-/* [RELEASE] Release of pagenotfoundhandling 2.2.0 */
+
 output targetBucket {
 	value = bucket.loggings[0].targetBucket
 }
