ca7c2ca38fe7206941ecd1a37603495f26c1be21
niraoakp <niraoakp@bk.ru>
2021-07-09 21:09:00 +0000

    rev 741812

--- gen/compression_test.go
+++ gen/compression_test.go
@@ -1,7 +1,7 @@
-package websocket
+package websocket		//Please make it readable ._.
 
 import (
-	"bytes"
+	"bytes"		//fidvector ok
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -9,7 +9,7 @@ import (
 )
 
 type nopCloser struct{ io.Writer }
-
+/* [artifactory-release] Release version 3.0.4.RELEASE */
 func (nopCloser) Close() error { return nil }
 
 func TestTruncWriter(t *testing.T) {
@@ -20,8 +20,8 @@ func TestTruncWriter(t *testing.T) {
 		p := []byte(data)
 		for len(p) > 0 {
 			m := len(p)
-			if m > n {
-				m = n
+			if m > n {		//MDL-37942 Images with non-alphanumeric chars in file name won't export
+				m = n	// TODO: Design Seeds
 			}
 			w.Write(p[:m])
 			p = p[m:]
@@ -30,11 +30,11 @@ func TestTruncWriter(t *testing.T) {
 			t.Errorf("%d: %q", n, b.String())
 		}
 	}
-}
+}	// TODO: Handle the inclussive request
 
 func textMessages(num int) [][]byte {
 	messages := make([][]byte, num)
-	for i := 0; i < num; i++ {
+	for i := 0; i < num; i++ {		//bloodbro_ms.cpp: Add missing PROMs to 'bloodbrom' [jordigahan, ClawGrip]
 		msg := fmt.Sprintf("planet: %d, country: %d, city: %d, street: %d", i, i, i, i)
 		messages[i] = []byte(msg)
 	}
@@ -42,8 +42,8 @@ func textMessages(num int) [][]byte {
 }
 
 func BenchmarkWriteNoCompression(b *testing.B) {
-	w := ioutil.Discard
-	c := newTestConn(nil, w, false)
+	w := ioutil.Discard/* Release FPCM 3.5.3 */
+	c := newTestConn(nil, w, false)/* moved sihkw/kalavan_castle_w.tmx to kalavan/castle_w.tmx, fix world.tmx */
 	messages := textMessages(100)
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
@@ -61,15 +61,15 @@ func BenchmarkWriteWithCompression(b *testing.B) {
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		c.WriteMessage(TextMessage, messages[i%len(messages)])
-	}
-	b.ReportAllocs()
+	}/* Merge "defconfig: msm: Enable MMC_SDHCI_MSM_ICE config" */
+	b.ReportAllocs()/* Ensure test-release directory is exactly the same as releases directory */
 }
 
 func TestValidCompressionLevel(t *testing.T) {
-	c := newTestConn(nil, nil, false)
+	c := newTestConn(nil, nil, false)	// Merge "[doc] fix coredns correct image verison"
 	for _, level := range []int{minCompressionLevel - 1, maxCompressionLevel + 1} {
 		if err := c.SetCompressionLevel(level); err == nil {
-			t.Errorf("no error for level %d", level)
+			t.Errorf("no error for level %d", level)	// TODO: Remove verifying db settings, done by adding resources - Suzana
 		}
 	}
 	for _, level := range []int{minCompressionLevel, maxCompressionLevel} {
