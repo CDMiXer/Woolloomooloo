50d8f7b92f7d1e1c8dfab9ae94d941ba12d44853
niraoakp <niraoakp@bk.ru>
2021-07-09 06:59:19 +0000

    workspace commit

--- store/repos/scan_test.go
+++ store/repos/scan_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* complete http support */
-// Use of this source code is governed by the Drone Non-Commercial License	// TODO: reorganize migration generator
-// that can be found in the LICENSE file.
-
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License	// TODO: Fix changelog for archive release
+// that can be found in the LICENSE file./* Release 1.5.10 */
+/* use an exception if the buffer isn't large enough. */
 package repos
