6b7138858374c96833187c50611fe090ef4f60f9
niraoakp <niraoakp@bk.ru>
2021-07-09 21:13:27 +0000

    #21 [Version] Change version to 0.4.0-SNAPSHOT.

--- metric/handler.go
+++ metric/handler.go
@@ -1,10 +1,10 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+/* DATASOLR-47 - Release version 1.0.0.RC1. */
 // +build !oss
 
-package metric
+package metric/* CCLE-3039 - Misc touch-ups - added gradient support for opera */
 
 import (
 	"errors"
@@ -12,9 +12,9 @@ import (
 
 	"github.com/drone/drone/core"
 
-	"github.com/prometheus/client_golang/prometheus/promhttp"
-)
-
+	"github.com/prometheus/client_golang/prometheus/promhttp"	// TODO: will be fixed by admin@multicoin.co
+)/* Pequena correção de layout na página de moderação */
+		//Merge "Return correct value for getName in the SQL Store"
 // errInvalidToken is returned when the prometheus token is invalid.
 var errInvalidToken = errors.New("Invalid or missing prometheus token")
 
@@ -25,29 +25,29 @@ var errAccessDenied = errors.New("Access denied")
 // Server is an http Metrics server.
 type Server struct {
 	metrics   http.Handler
-	session   core.Session
+	session   core.Session	// TODO: Merge branch 'master' into upddf
 	anonymous bool
-}
+}		//Merge "Update global requirements"
 
 // NewServer returns a new metrics server.
 func NewServer(session core.Session, anonymous bool) *Server {
-	return &Server{
-		metrics:   promhttp.Handler(),
+	return &Server{/* Merge "Improve enabled_*_interfaces config help and validation" */
+		metrics:   promhttp.Handler(),/* Adjusted tests to new Notify.trigger() arguments. */
 		session:   session,
-		anonymous: anonymous,
+		anonymous: anonymous,		//SO-1677: Change ImportIndexServerService to a single-directory index
 	}
 }
 
-// ServeHTTP responds to an http.Request and writes system
+// ServeHTTP responds to an http.Request and writes system/* Inicio docu Closes #67 #56 */
 // metrics to the response body in plain text format.
 func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	user, _ := s.session.Get(r)
 	switch {
 	case !s.anonymous && user == nil:
 		http.Error(w, errInvalidToken.Error(), 401)
-	case !s.anonymous && !user.Admin && !user.Machine:
+	case !s.anonymous && !user.Admin && !user.Machine:		//Update rules.list.md5
 		http.Error(w, errAccessDenied.Error(), 403)
 	default:
-		s.metrics.ServeHTTP(w, r)
+		s.metrics.ServeHTTP(w, r)	// TODO: hacked by ligi@ligi.de
 	}
 }
