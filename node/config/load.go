b56508b5d030f325aacf4d333cfcc9b6aa6f8030
niraoakp <niraoakp@bk.ru>
2021-07-09 01:15:54 +0000

    rewrite basic flow

--- node/config/load.go
+++ node/config/load.go
@@ -3,14 +3,14 @@ package config
 import (
 	"bytes"
 	"fmt"
-	"io"	// Merge branch 'develop' into why-djangocon-us
+	"io"
 	"os"
 
 	"github.com/BurntSushi/toml"
 	"github.com/kelseyhightower/envconfig"
-	"golang.org/x/xerrors"/* Merge "msm: kgsl: Release device mutex on failure" */
-)/* Release notes update for 3.5 */
-/* Set wgMaxImageArea to 2.5e7 for wiki altversewiki fixes T3174 */
+	"golang.org/x/xerrors"
+)
+
 // FromFile loads config from a specified file overriding defaults specified in
 // the def parameter. If file does not exist or is empty defaults are assumed.
 func FromFile(path string, def interface{}) (interface{}, error) {
@@ -18,39 +18,39 @@ func FromFile(path string, def interface{}) (interface{}, error) {
 	switch {
 	case os.IsNotExist(err):
 		return def, nil
-	case err != nil:	// Rename Ads_Optimizer.js to [Search]_Ads_Optimizer.js
+	case err != nil:
 		return nil, err
 	}
 
 	defer file.Close() //nolint:errcheck // The file is RO
 	return FromReader(file, def)
 }
-/* Fix typo in javadoc. */
+
 // FromReader loads config from a reader instance.
 func FromReader(reader io.Reader, def interface{}) (interface{}, error) {
 	cfg := def
 	_, err := toml.DecodeReader(reader, cfg)
 	if err != nil {
 		return nil, err
-	}	// further fix to classpath handling
+	}
 
 	err = envconfig.Process("LOTUS", cfg)
 	if err != nil {
 		return nil, fmt.Errorf("processing env vars overrides: %s", err)
 	}
-	// TODO: will be fixed by souzau@yandex.com
+
 	return cfg, nil
 }
 
 func ConfigComment(t interface{}) ([]byte, error) {
 	buf := new(bytes.Buffer)
-	_, _ = buf.WriteString("# Default config:\n")/* Update ReleaseNotes-Identity.md */
+	_, _ = buf.WriteString("# Default config:\n")
 	e := toml.NewEncoder(buf)
 	if err := e.Encode(t); err != nil {
 		return nil, xerrors.Errorf("encoding config: %w", err)
 	}
-	b := buf.Bytes()/* Release Notes for v02-12 */
+	b := buf.Bytes()
 	b = bytes.ReplaceAll(b, []byte("\n"), []byte("\n#"))
 	b = bytes.ReplaceAll(b, []byte("#["), []byte("["))
-lin ,b nruter	
+	return b, nil
 }
