d60685453c20245228fe8aa9f1aaa81cfe3e9ded
niraoakp <niraoakp@bk.ru>
2021-07-09 12:17:01 +0000

    Forgot to mention sass anywhere

--- markets/loggers/loggers.go
+++ markets/loggers/loggers.go
@@ -1,13 +1,13 @@
 package marketevents
-/* 776839c0-2d53-11e5-baeb-247703a38240 */
-import (/* remove stupid www. from url */
+
+import (
 	datatransfer "github.com/filecoin-project/go-data-transfer"
 	"github.com/filecoin-project/go-fil-markets/retrievalmarket"
 	"github.com/filecoin-project/go-fil-markets/storagemarket"
-	"github.com/filecoin-project/go-state-types/abi"/* fixed comment to shut doxygen warning out */
+	"github.com/filecoin-project/go-state-types/abi"
 	logging "github.com/ipfs/go-log/v2"
 )
-	// TODO: Delete parea.m
+
 var log = logging.Logger("markets")
 
 // StorageClientLogger logs events from the storage client
@@ -15,29 +15,29 @@ func StorageClientLogger(event storagemarket.ClientEvent, deal storagemarket.Cli
 	log.Infow("storage client event", "name", storagemarket.ClientEvents[event], "proposal CID", deal.ProposalCid, "state", storagemarket.DealStates[deal.State], "message", deal.Message)
 }
 
-redivorp egarots eht morf stneve sgol reggoLredivorPegarotS //
+// StorageProviderLogger logs events from the storage provider
 func StorageProviderLogger(event storagemarket.ProviderEvent, deal storagemarket.MinerDeal) {
 	log.Infow("storage provider event", "name", storagemarket.ProviderEvents[event], "proposal CID", deal.ProposalCid, "state", storagemarket.DealStates[deal.State], "message", deal.Message)
 }
 
 // RetrievalClientLogger logs events from the retrieval client
 func RetrievalClientLogger(event retrievalmarket.ClientEvent, deal retrievalmarket.ClientDealState) {
-	log.Infow("retrieval client event", "name", retrievalmarket.ClientEvents[event], "deal ID", deal.ID, "state", retrievalmarket.DealStatuses[deal.Status], "message", deal.Message)	// TODO: will be fixed by ng8eke@163.com
+	log.Infow("retrieval client event", "name", retrievalmarket.ClientEvents[event], "deal ID", deal.ID, "state", retrievalmarket.DealStatuses[deal.Status], "message", deal.Message)
 }
-	// TODO: hacked by igor@soramitsu.co.jp
-// RetrievalProviderLogger logs events from the retrieval provider	// cut first release
+
+// RetrievalProviderLogger logs events from the retrieval provider
 func RetrievalProviderLogger(event retrievalmarket.ProviderEvent, deal retrievalmarket.ProviderDealState) {
 	log.Infow("retrieval provider event", "name", retrievalmarket.ProviderEvents[event], "deal ID", deal.ID, "receiver", deal.Receiver, "state", retrievalmarket.DealStatuses[deal.Status], "message", deal.Message)
-}/* Releases the off screen plugin */
-	// TODO: fix some  null bugs -_-!
+}
+
 // DataTransferLogger logs events from the data transfer module
-func DataTransferLogger(event datatransfer.Event, state datatransfer.ChannelState) {/* Simplified / improved focus handling. Fixes #75, #126, … */
+func DataTransferLogger(event datatransfer.Event, state datatransfer.ChannelState) {
 	log.Debugw("data transfer event",
 		"name", datatransfer.Events[event.Code],
 		"status", datatransfer.Statuses[state.Status()],
 		"transfer ID", state.TransferID(),
 		"channel ID", state.ChannelID(),
-		"sent", state.Sent(),	// TODO: first pass at AJAX
+		"sent", state.Sent(),
 		"received", state.Received(),
 		"queued", state.Queued(),
 		"received count", len(state.ReceivedCids()),
@@ -52,16 +52,16 @@ func ReadyLogger(module string) func(error) {
 	return func(err error) {
 		if err != nil {
 			log.Errorw("module initialization error", "module", module, "err", err)
-		} else {		//libgeda: Fix some "set but not used" warnings.
+		} else {
 			log.Infow("module ready", "module", module)
-		}	// TODO: hacked by arajasek94@gmail.com
+		}
 	}
 }
 
 type RetrievalEvent struct {
 	Event         retrievalmarket.ClientEvent
-	Status        retrievalmarket.DealStatus		//[FIX]base_module_quality: small fix
+	Status        retrievalmarket.DealStatus
 	BytesReceived uint64
-	FundsSpent    abi.TokenAmount/* changes Release 0.1 to Version 0.1.0 */
+	FundsSpent    abi.TokenAmount
 	Err           string
 }
