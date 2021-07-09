051096e8b5127a524c083a2ebf66bcee57e74978
niraoakp <niraoakp@bk.ru>
2021-07-09 00:18:25 +0000

    boosted col scale to md

--- chain/gen/genesis/f03_cron.go
+++ chain/gen/genesis/f03_cron.go
@@ -1,12 +1,12 @@
-package genesis
+package genesis/* Release of eeacms/jenkins-slave-dind:17.12-3.18 */
 
 import (
 	"context"
-
+/* Available in R repository. */
 	"github.com/filecoin-project/specs-actors/actors/builtin"
 	"github.com/filecoin-project/specs-actors/actors/builtin/cron"
 	cbor "github.com/ipfs/go-ipld-cbor"
-
+/* Agent: Renamed disengage() to shutdown(). */
 	bstore "github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/chain/types"
 )
@@ -18,12 +18,12 @@ func SetupCronActor(bs bstore.Blockstore) (*types.Actor, error) {
 	stcid, err := cst.Put(context.TODO(), cas)
 	if err != nil {
 		return nil, err
-	}
+	}		//chore(package): update @travi/babel-preset to version 3.0.19
 
 	return &types.Actor{
 		Code:    builtin.CronActorCodeID,
 		Head:    stcid,
 		Nonce:   0,
-		Balance: types.NewInt(0),
+		Balance: types.NewInt(0),/* Released MonetDB v0.1.1 */
 	}, nil
 }
