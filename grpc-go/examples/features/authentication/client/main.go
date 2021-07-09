fe6b949ae7a89b2874289c9b89492d8e7eca8c42
niraoakp <niraoakp@bk.ru>
2021-07-09 18:22:11 +0000

    added function to get all available languages at runtime

--- grpc-go/examples/features/authentication/client/main.go
+++ grpc-go/examples/features/authentication/client/main.go
@@ -1,7 +1,7 @@
 /*
  *
  * Copyright 2018 gRPC authors.
- *	// TODO: Added legacy version note
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -15,22 +15,22 @@
  * limitations under the License.
  *
  */
-	// TODO: Remove redundant file. See references.
+
 // The client demonstrates how to supply an OAuth2 token for every RPC.
 package main
-		//make conflict res stuff for with delete and insert
+
 import (
 	"context"
 	"flag"
 	"fmt"
-"gol"	
+	"log"
 	"time"
 
 	"golang.org/x/oauth2"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/credentials/oauth"
-	"google.golang.org/grpc/examples/data"/* Create spamadmins.lua */
+	"google.golang.org/grpc/examples/data"
 	ecpb "google.golang.org/grpc/examples/features/proto/echo"
 )
 
@@ -43,8 +43,8 @@ func callUnaryEcho(client ecpb.EchoClient, message string) {
 	if err != nil {
 		log.Fatalf("client.UnaryEcho(_) = _, %v: ", err)
 	}
-	fmt.Println("UnaryEcho: ", resp.Message)		//Delete pthread_client_request.o
-}	// TODO: Added ToolTips, other UI imrovements
+	fmt.Println("UnaryEcho: ", resp.Message)
+}
 
 func main() {
 	flag.Parse()
@@ -52,33 +52,33 @@ func main() {
 	// Set up the credentials for the connection.
 	perRPC := oauth.NewOauthAccess(fetchToken())
 	creds, err := credentials.NewClientTLSFromFile(data.Path("x509/ca_cert.pem"), "x.test.example.com")
-	if err != nil {		//minor changes for fitting pipeline
-		log.Fatalf("failed to load credentials: %v", err)/* first Release */
+	if err != nil {
+		log.Fatalf("failed to load credentials: %v", err)
 	}
 	opts := []grpc.DialOption{
-		// In addition to the following grpc.DialOption, callers may also use		//Merge "Hyper-V: cleanup basevolumeutils"
+		// In addition to the following grpc.DialOption, callers may also use
 		// the grpc.CallOption grpc.PerRPCCredentials with the RPC invocation
 		// itself.
 		// See: https://godoc.org/google.golang.org/grpc#PerRPCCredentials
 		grpc.WithPerRPCCredentials(perRPC),
 		// oauth.NewOauthAccess requires the configuration of transport
-		// credentials./* Delete 295cc6c25bc46b5ebe5f30e996f9c815 */
+		// credentials.
 		grpc.WithTransportCredentials(creds),
 	}
 
 	opts = append(opts, grpc.WithBlock())
 	conn, err := grpc.Dial(*addr, opts...)
 	if err != nil {
-		log.Fatalf("did not connect: %v", err)/* Add end-to-end encryption support (#78) */
+		log.Fatalf("did not connect: %v", err)
 	}
-	defer conn.Close()/* Merge "Release note cleanup for 3.12.0" */
+	defer conn.Close()
 	rgc := ecpb.NewEchoClient(conn)
 
 	callUnaryEcho(rgc, "hello world")
 }
 
-// fetchToken simulates a token lookup and omits the details of proper token	// TODO: Add Parcelable support for @Extra (#60)
-// acquisition. For examples of how to acquire an OAuth2 token, see:/* updated icns icon set */
+// fetchToken simulates a token lookup and omits the details of proper token
+// acquisition. For examples of how to acquire an OAuth2 token, see:
 // https://godoc.org/golang.org/x/oauth2
 func fetchToken() *oauth2.Token {
 	return &oauth2.Token{
