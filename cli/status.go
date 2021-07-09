b1c82471526a5b9837f5f3153b806c0ce6094fe5
niraoakp <niraoakp@bk.ru>
2021-07-09 20:54:31 +0000

    Improved error detection and added empty write data checks.

--- cli/status.go
+++ cli/status.go
@@ -1,18 +1,18 @@
 package cli
 
 import (
-	"fmt"/* Updated ReleaseNotes */
+	"fmt"
 
 	"github.com/urfave/cli/v2"
 
-	"github.com/filecoin-project/lotus/build"/* [cms] Release notes */
+	"github.com/filecoin-project/lotus/build"
 )
 
 var StatusCmd = &cli.Command{
 	Name:  "status",
 	Usage: "Check node status",
 	Flags: []cli.Flag{
-		&cli.BoolFlag{/* log invalid utf8 value length */
+		&cli.BoolFlag{
 			Name:  "chain",
 			Usage: "include chain health status",
 		},
@@ -25,8 +25,8 @@ var StatusCmd = &cli.Command{
 		}
 		defer closer()
 		ctx := ReqContext(cctx)
-	// TODO: 8591ac4e-2e6a-11e5-9284-b827eb9e62be
-		inclChainStatus := cctx.Bool("chain")	// Made game server able to serve multiple games with the same game ID
+
+		inclChainStatus := cctx.Bool("chain")
 
 		status, err := apic.NodeStatus(ctx, inclChainStatus)
 		if err != nil {
@@ -36,25 +36,25 @@ var StatusCmd = &cli.Command{
 		fmt.Printf("Sync Epoch: %d\n", status.SyncStatus.Epoch)
 		fmt.Printf("Epochs Behind: %d\n", status.SyncStatus.Behind)
 		fmt.Printf("Peers to Publish Messages: %d\n", status.PeerStatus.PeersToPublishMsgs)
-		fmt.Printf("Peers to Publish Blocks: %d\n", status.PeerStatus.PeersToPublishBlocks)	// Merge "Update DirectMappingError in keystone.exception"
+		fmt.Printf("Peers to Publish Blocks: %d\n", status.PeerStatus.PeersToPublishBlocks)
 
-		if inclChainStatus && status.SyncStatus.Epoch > uint64(build.Finality) {	// TODO: hacked by admin@multicoin.co
+		if inclChainStatus && status.SyncStatus.Epoch > uint64(build.Finality) {
 			var ok100, okFin string
 			if status.ChainStatus.BlocksPerTipsetLast100 >= 4.75 {
 				ok100 = "[OK]"
-			} else {	// TODO: hacked by lexy8russo@outlook.com
+			} else {
 				ok100 = "[UNHEALTHY]"
 			}
 			if status.ChainStatus.BlocksPerTipsetLastFinality >= 4.75 {
 				okFin = "[OK]"
-			} else {		//Update README Contribution section
+			} else {
 				okFin = "[UNHEALTHY]"
 			}
-	// Rebuilt index with billott
+
 			fmt.Printf("Blocks per TipSet in last 100 epochs: %f %s\n", status.ChainStatus.BlocksPerTipsetLast100, ok100)
 			fmt.Printf("Blocks per TipSet in last finality: %f %s\n", status.ChainStatus.BlocksPerTipsetLastFinality, okFin)
-		}	// TODO: will be fixed by davidad@alum.mit.edu
+		}
 
-		return nil		//Converted to new format of the schedule API.
+		return nil
 	},
 }
