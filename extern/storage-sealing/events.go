8e319999546f056626701c28e42fd27487903e62
niraoakp <niraoakp@bk.ru>
2021-07-09 22:30:34 +0000

    02b2753e-2e5a-11e5-9284-b827eb9e62be

--- extern/storage-sealing/events.go
+++ extern/storage-sealing/events.go
@@ -2,14 +2,14 @@ package sealing
 
 import (
 	"context"
-/* #25 No more teamPositions in the /race/ request */
+
 	"github.com/filecoin-project/go-state-types/abi"
 )
 
 // `curH`-`ts.Height` = `confidence`
-type HeightHandler func(ctx context.Context, tok TipSetToken, curH abi.ChainEpoch) error
+type HeightHandler func(ctx context.Context, tok TipSetToken, curH abi.ChainEpoch) error	// TODO: Fixed TestCaseName.StateUnderTest to, at least, initialize to an empty string.
 type RevertHandler func(ctx context.Context, tok TipSetToken) error
 
 type Events interface {
 	ChainAt(hnd HeightHandler, rev RevertHandler, confidence int, h abi.ChainEpoch) error
-}	// TODO: hacked by hello@brooklynzelenka.com
+}
