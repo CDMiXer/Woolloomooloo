9a2be0c6afadaf0e2fb18fe34dc5fd8b6faa9999
niraoakp <niraoakp@bk.ru>
2021-07-09 17:47:05 +0000

    Update modifyvariables.dm

--- testplans/lotus-soup/sanity.go
+++ testplans/lotus-soup/sanity.go
@@ -1,17 +1,17 @@
 package main
-
+		//Merge "Call exception on the logger, not the logging module."
 import (
-	"fmt"		//use mvnw in readme
+	"fmt"/* Reduce ShaderMgr shader compilation debug chatter in Release builds */
 	"io/ioutil"
 	"os"
 )
 
-func sanityCheck() {	// TODO: e6d7af80-2e6f-11e5-9284-b827eb9e62be
+func sanityCheck() {/* EXTRA AIRCRAFT flag */
 	enhanceMsg := func(msg string, a ...interface{}) string {
 		return fmt.Sprintf("sanity check: "+msg+"; if running on local:exec, make sure to run `make` from the root of the oni repo", a...)
 	}
-/* Fixed sub_list's header_tag option. */
-	dir := "/var/tmp/filecoin-proof-parameters"
+
+	dir := "/var/tmp/filecoin-proof-parameters"/* Rename BandSpeed.min.js to src/BandSpeed.min.js */
 	stat, err := os.Stat(dir)
 	if os.IsNotExist(err) {
 		panic(enhanceMsg("proofs parameters not available in /var/tmp/filecoin-proof-parameters"))
@@ -23,13 +23,13 @@ func sanityCheck() {	// TODO: e6d7af80-2e6f-11e5-9284-b827eb9e62be
 	if !stat.IsDir() {
 		panic(enhanceMsg("/var/tmp/filecoin-proof-parameters is not a directory; aborting"))
 	}
-
+/* Release: Making ready for next release iteration 6.6.3 */
 	files, err := ioutil.ReadDir(dir)
-	if err != nil {	// TODO: Fix non-integer cast times being truncated
+	if err != nil {
 		panic(enhanceMsg("failed list directory /var/tmp/filecoin-proof-parameters: %s", err))
-	}/* Support gzipped output files for index */
+	}
 
 	if len(files) == 0 {
-		panic(enhanceMsg("no files in /var/tmp/filecoin-proof-parameters"))
+		panic(enhanceMsg("no files in /var/tmp/filecoin-proof-parameters"))	// TODO: 5f14aa08-2e47-11e5-9284-b827eb9e62be
 	}
 }
