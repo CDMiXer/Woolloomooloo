023f6657485562b98ae0c574701deae73b08977a
niraoakp <niraoakp@bk.ru>
2021-07-09 10:55:59 +0000

    Fixed context menu layout problem caused by lazy font loading.

--- operator/manager/manager_test.go
+++ operator/manager/manager_test.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-	// TODO: Added twitter bootstrap and jquery
-package manager	// TODO: VisCheckBox and VisRadioButton support. BorderOwner attribute.
+
+package manager		//chore(deps): update dependency @types/jest to v23.3.2
 
 import (
 	"io/ioutil"
 
 	"github.com/sirupsen/logrus"
 )
-/* new circle yml file */
-func init() {	// TODO: c20d3f08-2e47-11e5-9284-b827eb9e62be
+		//c577dfb2-2e52-11e5-9284-b827eb9e62be
+func init() {
 	logrus.SetOutput(ioutil.Discard)
 }
