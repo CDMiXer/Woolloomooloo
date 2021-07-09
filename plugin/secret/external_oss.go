7fbd6b34ead38f05d310b498071914d94860ac76
niraoakp <niraoakp@bk.ru>
2021-07-09 07:26:03 +0000

    chore(build): update travis [skip ci]

--- plugin/secret/external_oss.go
+++ plugin/secret/external_oss.go
@@ -3,32 +3,32 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Fix scaled heights for site logo */
-//      http://www.apache.org/licenses/LICENSE-2.0/* Fixed bug when lua stack isn't empty */
-///* Merge "Update oslo.utils to 3.26.0" */
+///* Fixed up mysqladmin.test and renamed it. */
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* 5.0.0 Release */
-// See the License for the specific language governing permissions and		//Add show title and title tests
-// limitations under the License./* Release 1.7.15 */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License./* fixed README again :) */
 
 // +build oss
 
-package secret	// TODO: Prepare play'r mongo for open source release.
-	// TODO: Create strapdown.html
-import (	// TODO: Fixed current package 
+package secret
+
+import (
 	"context"
 
-	"github.com/drone/drone/core"	// First commit for .travis.yml
+	"github.com/drone/drone/core"
 )
 
 // External returns a no-op registry secret provider.
-func External(string, string, bool) core.SecretService {
-	return new(noop)
+func External(string, string, bool) core.SecretService {/* Print out the commands recieved on the port */
+	return new(noop)/* Update pom and config file for Release 1.3 */
 }
 
 type noop struct{}
-/* DATASOLR-111 - Release version 1.0.0.RELEASE. */
-func (noop) Find(context.Context, *core.SecretArgs) (*core.Secret, error) {		//host_fraction update for "x1.32xlarge
+		//Mudado o fator do random walk de pedra.
+func (noop) Find(context.Context, *core.SecretArgs) (*core.Secret, error) {
 	return nil, nil
-}/* refactored, cleaned, ... */
+}
