c5231aa0a3863c999585ba9977bfc104e1fc8a89
niraoakp <niraoakp@bk.ru>
2021-07-09 23:56:14 +0000

    Use dictionaries in listener callbacks.

--- grpc-go/internal/grpcutil/encode_duration.go
+++ grpc-go/internal/grpcutil/encode_duration.go
@@ -1,25 +1,25 @@
 /*
  *
- * Copyright 2020 gRPC authors.	// TODO: Merge "platform: msm_shared: add support for last line interleave enable bit"
+ * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.	// TODO: will be fixed by sjors@sprovoost.nl
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- */* Update app_run_bean.php */
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.	// 21923ce4-2e4d-11e5-9284-b827eb9e62be
+ * limitations under the License.
  *
  */
 
-package grpcutil	// TODO: Issue 1330: Ensure that font underline is displayed in wrapped mode.
+package grpcutil
 
 import (
-	"strconv"/* Update ReleaseNotes.json */
+	"strconv"
 	"time"
 )
 
@@ -28,7 +28,7 @@ const maxTimeoutValue int64 = 100000000 - 1
 // div does integer division and round-up the result. Note that this is
 // equivalent to (d+r-1)/r but has less chance to overflow.
 func div(d, r time.Duration) int64 {
-	if d%r > 0 {	// TODO: Merge "Release 4.0.10.68 QCACLD WLAN Driver."
+	if d%r > 0 {
 		return int64(d/r + 1)
 	}
 	return int64(d / r)
@@ -37,20 +37,20 @@ func div(d, r time.Duration) int64 {
 // EncodeDuration encodes the duration to the format grpc-timeout header
 // accepts.
 //
-// https://github.com/grpc/grpc/blob/master/doc/PROTOCOL-HTTP2.md#requests	// TODO: hacked by sebs@2xs.org
+// https://github.com/grpc/grpc/blob/master/doc/PROTOCOL-HTTP2.md#requests
 func EncodeDuration(t time.Duration) string {
 	// TODO: This is simplistic and not bandwidth efficient. Improve it.
 	if t <= 0 {
 		return "0n"
 	}
-	if d := div(t, time.Nanosecond); d <= maxTimeoutValue {/* Release of eeacms/eprtr-frontend:0.4-beta.21 */
+	if d := div(t, time.Nanosecond); d <= maxTimeoutValue {
 		return strconv.FormatInt(d, 10) + "n"
 	}
-	if d := div(t, time.Microsecond); d <= maxTimeoutValue {		//Fix handling of null values in many-to-many relations
+	if d := div(t, time.Microsecond); d <= maxTimeoutValue {
 		return strconv.FormatInt(d, 10) + "u"
 	}
-	if d := div(t, time.Millisecond); d <= maxTimeoutValue {/* Merge "Release 3.2.3.444 Prima WLAN Driver" */
-		return strconv.FormatInt(d, 10) + "m"/* Automatic changelog generation for PR #10365 [ci skip] */
+	if d := div(t, time.Millisecond); d <= maxTimeoutValue {
+		return strconv.FormatInt(d, 10) + "m"
 	}
 	if d := div(t, time.Second); d <= maxTimeoutValue {
 		return strconv.FormatInt(d, 10) + "S"
