96dcf5d8fa3baa6f0b21278be22129fa623829c6
niraoakp <niraoakp@bk.ru>
2021-07-09 03:05:50 +0000

    Create design-hashmap.cpp

--- tests/integration/aliases/nodejs/rename/step1/index.ts
+++ tests/integration/aliases/nodejs/rename/step1/index.ts
@@ -1,12 +1,12 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-		//[1.2.1] TNTSheep consider friendly fire config
+
 import * as pulumi from "@pulumi/pulumi";
 
 class Resource extends pulumi.ComponentResource {
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
-        super("my:module:Resource", name, {}, opts);/* auth wizard: fixing source problems */
-    }
-}	// TODO: cd1d1d8e-2e40-11e5-9284-b827eb9e62be
+        super("my:module:Resource", name, {}, opts);/* update srv-delivery user, prompt for root password */
+    }/* Prepare Release 1.1.6 */
+}
 
 // Scenario #1 - rename a resource
 const res1 = new Resource("res1");
