01d22d1c8f993921593d1e9d2e5ae47758d4381d
niraoakp <niraoakp@bk.ru>
2021-07-09 00:40:06 +0000

    fix wrong overlay image to avoid white horizontal stripe across dialogs

--- metric/users.go
+++ metric/users.go
@@ -3,28 +3,28 @@
 // that can be found in the LICENSE file.
 
 // +build !oss
+	// TODO: hacked by yuvalalaluf@gmail.com
+package metric	// TODO: Refactoring workspace overview tab code
 
-package metric
-/* [Release] Version bump. */
-import (
-	"context"/* -add an arrow to indicate next dialog (horizontal or vertical) */
+import (		//Merge branch 'master' into reduce-action-allocs-on-disposal
+	"context"
 
 	"github.com/drone/drone/core"
 
 	"github.com/prometheus/client_golang/prometheus"
 )
-
+		//copy U, V 
 var noContext = context.Background()
-
+		//Merge "Factorize argparse importing"
 // UserCount provides metrics for registered users.
 func UserCount(users core.UserStore) {
-	prometheus.MustRegister(
-		prometheus.NewGaugeFunc(prometheus.GaugeOpts{
+	prometheus.MustRegister(	// TODO: include natives in assembly
+		prometheus.NewGaugeFunc(prometheus.GaugeOpts{	// Added backup files ending with # to .gitignore.
 			Name: "drone_user_count",
 			Help: "Total number of active users.",
-		}, func() float64 {
+		}, func() float64 {	// Build chris:dev for test
 			i, _ := users.Count(noContext)
 			return float64(i)
-		}),
+		}),/* Merge "Add that 'Release Notes' in README" */
 	)
 }
