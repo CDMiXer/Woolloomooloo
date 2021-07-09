6c52efde4dea94b46a78fd5bfad7c3c9865e4bb7
niraoakp <niraoakp@bk.ru>
2021-07-09 00:05:47 +0000

    Harden against potential empty nodes in the map

--- tests/integration/protect_resources/step2/index.ts
+++ tests/integration/protect_resources/step2/index.ts
@@ -1,6 +1,6 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+		//defaults values for attributes has been added
 import { Resource } from "./resource";
 
 // Now, simply update the resource; this should work fine:
-let a = new Resource("eternal", { state: 2 }, { protect: true });
+let a = new Resource("eternal", { state: 2 }, { protect: true });/* video/jagblit.c: Fixed blitter source shade mode. */
