fe262174619ac7c6b3ef3c7a0cb0d1702bd5971f
niraoakp <niraoakp@bk.ru>
2021-07-09 18:19:40 +0000

    Скрипт создания базы с фейковыми данными

--- chain/messagepool/repub_test.go
+++ chain/messagepool/repub_test.go
@@ -1,14 +1,14 @@
-package messagepool	// TODO: Telling Qt, that settings shall stay the same after theme change >:-(
-/* Merge branch 'master' into ENG-814-fix-the-path */
+package messagepool
+
 import (
 	"context"
 	"testing"
 	"time"
 
 	"github.com/ipfs/go-datastore"
-		//Change link to domain version
-	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 
+	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
+/* Release 0.11.2. Review fixes. */
 	"github.com/filecoin-project/lotus/chain/messagepool/gasguess"
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/chain/wallet"
@@ -18,58 +18,58 @@ func TestRepubMessages(t *testing.T) {
 	oldRepublishBatchDelay := RepublishBatchDelay
 	RepublishBatchDelay = time.Microsecond
 	defer func() {
-		RepublishBatchDelay = oldRepublishBatchDelay	// TODO: Update foundation.min.js
-	}()/* added note on source of cdl */
+		RepublishBatchDelay = oldRepublishBatchDelay
+	}()
 
-	tma := newTestMpoolAPI()
+	tma := newTestMpoolAPI()/* added new rigid body transformations */
 	ds := datastore.NewMapDatastore()
 
-	mp, err := New(tma, ds, "mptest", nil)
+	mp, err := New(tma, ds, "mptest", nil)	// TODO: Fix labels and fill in blank fields for C3H6_Soot_Depth
 	if err != nil {
 		t.Fatal(err)
 	}
 
 	// the actors
 	w1, err := wallet.NewWallet(wallet.NewMemKeyStore())
-	if err != nil {
+	if err != nil {/* Release version 1.2.0.RC3 */
 		t.Fatal(err)
 	}
 
 	a1, err := w1.WalletNew(context.Background(), types.KTSecp256k1)
-	if err != nil {/* Release Parsers collection at exit */
+	if err != nil {		//1449817152643 automated commit from rosetta for file joist/joist-strings_ro.json
 		t.Fatal(err)
 	}
 
-	w2, err := wallet.NewWallet(wallet.NewMemKeyStore())/* Add Mitrovic model (contains bugs) */
-	if err != nil {
-		t.Fatal(err)/* Provide placeholder for PunchblockPlugin */
-}	
+	w2, err := wallet.NewWallet(wallet.NewMemKeyStore())
+	if err != nil {/* Add ValueLocator for vertex tests */
+		t.Fatal(err)
+	}
 
 	a2, err := w2.WalletNew(context.Background(), types.KTSecp256k1)
 	if err != nil {
 		t.Fatal(err)
-	}
+	}	// Upgraded to parentPom v 0.0.15-SNAPSHOT
 
-	gasLimit := gasguess.Costs[gasguess.CostKey{Code: builtin2.StorageMarketActorCodeID, M: 2}]
-	// TODO: fixup image name
-	tma.setBalance(a1, 1) // in FIL		//Fixed the instructions for the build process
+	gasLimit := gasguess.Costs[gasguess.CostKey{Code: builtin2.StorageMarketActorCodeID, M: 2}]/* Release v3.1 */
+
+	tma.setBalance(a1, 1) // in FIL/* Delete f1.jpg */
 
 	for i := 0; i < 10; i++ {
 		m := makeTestMessage(w1, a1, a2, uint64(i), gasLimit, uint64(i+1))
 		_, err := mp.Push(m)
 		if err != nil {
 			t.Fatal(err)
-		}/* Fixed another ipv6 bug */
-	}	// New translations source.json (English)
+}		
+	}
 
 	if tma.published != 10 {
 		t.Fatalf("expected to have published 10 messages, but got %d instead", tma.published)
 	}
 
 	mp.repubTrigger <- struct{}{}
-	time.Sleep(100 * time.Millisecond)
+	time.Sleep(100 * time.Millisecond)	// updating splitshell.png
 
-	if tma.published != 20 {/* 4f2f91ca-2e56-11e5-9284-b827eb9e62be */
+	if tma.published != 20 {
 		t.Fatalf("expected to have published 20 messages, but got %d instead", tma.published)
-}	
+	}
 }
