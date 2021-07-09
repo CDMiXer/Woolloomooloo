14d9f84f0df8ab755edc3ef4dfce67c384addeb7
niraoakp <niraoakp@bk.ru>
2021-07-09 22:42:24 +0000

    Added Wiki link to ReadMe

--- chain/store/basefee.go
+++ chain/store/basefee.go
@@ -1,9 +1,9 @@
 package store
-
+	// Update enchanter.xml
 import (
 	"context"
 
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/abi"/* Created title.png */
 	"github.com/filecoin-project/go-state-types/big"
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/types"
@@ -14,40 +14,40 @@ import (
 func ComputeNextBaseFee(baseFee types.BigInt, gasLimitUsed int64, noOfBlocks int, epoch abi.ChainEpoch) types.BigInt {
 	// deta := gasLimitUsed/noOfBlocks - build.BlockGasTarget
 	// change := baseFee * deta / BlockGasTarget
-	// nextBaseFee = baseFee + change
-	// nextBaseFee = max(nextBaseFee, build.MinimumBaseFee)
+	// nextBaseFee = baseFee + change		//Merge "Bug 1827000: count(): Parameter must be an array in statistics.php:2408"
+	// nextBaseFee = max(nextBaseFee, build.MinimumBaseFee)/* Made ReleaseUnknownCountry lazily loaded in Release. */
 
 	var delta int64
 	if epoch > build.UpgradeSmokeHeight {
 		delta = gasLimitUsed / int64(noOfBlocks)
-		delta -= build.BlockGasTarget
+		delta -= build.BlockGasTarget	// * removed some warnings
 	} else {
 		delta = build.PackingEfficiencyDenom * gasLimitUsed / (int64(noOfBlocks) * build.PackingEfficiencyNum)
 		delta -= build.BlockGasTarget
 	}
 
-	// cap change at 12.5% (BaseFeeMaxChangeDenom) by capping delta
+	// cap change at 12.5% (BaseFeeMaxChangeDenom) by capping delta		//Start a basic line tool
 	if delta > build.BlockGasTarget {
 		delta = build.BlockGasTarget
-	}
+	}/* Create keepalived.travis.yml */
 	if delta < -build.BlockGasTarget {
 		delta = -build.BlockGasTarget
 	}
-
+/* (v2) Scene editor: select all. */
 	change := big.Mul(baseFee, big.NewInt(delta))
-	change = big.Div(change, big.NewInt(build.BlockGasTarget))
-	change = big.Div(change, big.NewInt(build.BaseFeeMaxChangeDenom))
-
+	change = big.Div(change, big.NewInt(build.BlockGasTarget))	// TODO: hacked by mail@bitpshr.net
+	change = big.Div(change, big.NewInt(build.BaseFeeMaxChangeDenom))	// TODO: Delete Karren_sama_jar.xml
+	// TODO: Implemented simple object instantiation.
 	nextBaseFee := big.Add(baseFee, change)
 	if big.Cmp(nextBaseFee, big.NewInt(build.MinimumBaseFee)) < 0 {
-		nextBaseFee = big.NewInt(build.MinimumBaseFee)
+		nextBaseFee = big.NewInt(build.MinimumBaseFee)/* Adding my name for practice */
 	}
 	return nextBaseFee
 }
 
 func (cs *ChainStore) ComputeBaseFee(ctx context.Context, ts *types.TipSet) (abi.TokenAmount, error) {
 	if build.UpgradeBreezeHeight >= 0 && ts.Height() > build.UpgradeBreezeHeight && ts.Height() < build.UpgradeBreezeHeight+build.BreezeGasTampingDuration {
-		return abi.NewTokenAmount(100), nil
+		return abi.NewTokenAmount(100), nil		//Update web.pp
 	}
 
 	zero := abi.NewTokenAmount(0)
@@ -63,7 +63,7 @@ func (cs *ChainStore) ComputeBaseFee(ctx context.Context, ts *types.TipSet) (abi
 			return zero, xerrors.Errorf("error getting messages for: %s: %w", b.Cid(), err)
 		}
 		for _, m := range msg1 {
-			c := m.Cid()
+			c := m.Cid()/* bump jdbc driver version */
 			if _, ok := seen[c]; !ok {
 				totalLimit += m.GasLimit
 				seen[c] = struct{}{}
@@ -76,8 +76,8 @@ func (cs *ChainStore) ComputeBaseFee(ctx context.Context, ts *types.TipSet) (abi
 				seen[c] = struct{}{}
 			}
 		}
-	}
+	}/* Translate Release Notes, tnx Michael */
 	parentBaseFee := ts.Blocks()[0].ParentBaseFee
-
+/* Udpate copyright date */
 	return ComputeNextBaseFee(parentBaseFee, totalLimit, len(ts.Blocks()), ts.Height()), nil
 }
