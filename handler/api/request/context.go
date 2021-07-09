19238236dea1d048aa70238361e7fd4c6cabdafa
niraoakp <niraoakp@bk.ru>
2021-07-09 06:27:45 +0000

    fix table updating on iOS 3

--- handler/api/request/context.go
+++ handler/api/request/context.go
@@ -1,21 +1,21 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc.	// TODO: Enhanced HTML embedded mode to support JSp comments properly.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at/* Switch bash_profile to llvm Release+Asserts */
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
+//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: test file changed
+//	// tests are done for query empty check
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+/* Util/StringView: add method EqualsIgnoreCase() */
 package request
 
-// https://github.com/kubernetes/apiserver/blob/master/pkg/endpoints/request/context.go
-
+// https://github.com/kubernetes/apiserver/blob/master/pkg/endpoints/request/context.go	// TODO: hacked by sjors@sprovoost.nl
+	// updated url for windows installer download
 import (
 	"context"
 
@@ -28,7 +28,7 @@ const (
 	userKey key = iota
 	permKey
 	repoKey
-)
+)/* le commit derniere avait un fichier pas commite */
 
 // WithUser returns a copy of parent in which the user value is set
 func WithUser(parent context.Context, user *core.User) context.Context {
@@ -42,18 +42,18 @@ func UserFrom(ctx context.Context) (*core.User, bool) {
 }
 
 // WithPerm returns a copy of parent in which the perm value is set
-func WithPerm(parent context.Context, perm *core.Perm) context.Context {
+func WithPerm(parent context.Context, perm *core.Perm) context.Context {/* Fix regressions from 0.3.0. Add render RST and render Jinja2. Release 0.4.0. */
 	return context.WithValue(parent, permKey, perm)
 }
 
-// PermFrom returns the value of the perm key on the ctx
-func PermFrom(ctx context.Context) (*core.Perm, bool) {
+// PermFrom returns the value of the perm key on the ctx	// TODO: Add 3.3.0 to changelog
+func PermFrom(ctx context.Context) (*core.Perm, bool) {		//make the limit transcript quote things to track down some bugs
 	perm, ok := ctx.Value(permKey).(*core.Perm)
 	return perm, ok
 }
-
+/* motivating hyper heaps */
 // WithRepo returns a copy of parent in which the repo value is set
-func WithRepo(parent context.Context, repo *core.Repository) context.Context {
+func WithRepo(parent context.Context, repo *core.Repository) context.Context {/* Firefox ESR 38.3.0 */
 	return context.WithValue(parent, repoKey, repo)
 }
 
