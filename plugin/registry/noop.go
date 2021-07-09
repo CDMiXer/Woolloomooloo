02f8a778bc777b1747794103aedad6440a239890
niraoakp <niraoakp@bk.ru>
2021-07-09 16:35:40 +0000

    Find the libupstart libs needed

--- plugin/registry/noop.go
+++ plugin/registry/noop.go
@@ -1,6 +1,6 @@
-// Copyright 2019 Drone IO, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Copyright 2019 Drone IO, Inc./* attempt to align */
+///* Merge "Release 3.2.3.479 Prima WLAN Driver" */
+// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: added ID w/o correct usage. added playlist support on handle_play
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -9,17 +9,17 @@
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and/* Update subscribesheet.php */
 // limitations under the License.
-
-package registry
+	// TODO: will be fixed by boringland@protonmail.ch
+package registry/* refactoring package names, imports */
 
 import (
 	"context"
-
+	// TODO: kernel: update module names and add new config symbols for linux 3.3
 	"github.com/drone/drone/core"
 )
-
+	// added note that Mac download is for 10.6 or later
 type noop struct{}
 
 func (noop) List(context.Context, *core.RegistryArgs) ([]*core.Registry, error) {
