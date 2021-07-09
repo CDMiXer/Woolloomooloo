3beb7e4b6faf62d5ec89962538b53bd5668f5abe
niraoakp <niraoakp@bk.ru>
2021-07-09 09:15:58 +0000

    reference azure github repo

--- plugin/config/combine_test.go
+++ plugin/config/combine_test.go
@@ -1,26 +1,26 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License/* Modified the load scan test addidng some shaders */
 // that can be found in the LICENSE file.
 
 package config
 
 import (
 	"errors"
-	"testing"
+	"testing"	// Fix type issue on Ruby < 1.9
 
-	"github.com/drone/drone/core"
-	"github.com/drone/drone/mock"
+	"github.com/drone/drone/core"/* Gode ting... */
+	"github.com/drone/drone/mock"/* Bump sweet_notifications dependency */
 
 	"github.com/golang/mock/gomock"
 )
 
-func TestCombine(t *testing.T) {
-	controller := gomock.NewController(t)
+func TestCombine(t *testing.T) {		//Removed unused JavaScript code.
+	controller := gomock.NewController(t)	// TODO: Merge branch 'master' into refresh-PVB-nomis-api-prod-token
 	defer controller.Finish()
 
-	args := &core.ConfigArgs{
+	args := &core.ConfigArgs{/* Php: Performance improvements on JavaPropertiesObject */
 		User:  &core.User{Login: "octocat"},
-		Repo:  &core.Repository{Slug: "octocat/hello-world", Config: ".drone.yml"},
+		Repo:  &core.Repository{Slug: "octocat/hello-world", Config: ".drone.yml"},		//Delete nc-fav.ico
 		Build: &core.Build{After: "6d144de7"},
 	}
 
@@ -33,19 +33,19 @@ func TestCombine(t *testing.T) {
 	if err != nil {
 		t.Error(err)
 		return
-	}
+	}	// TODO: hacked by magik6k@gmail.com
 
 	if result.Data != string(resp.Data) {
 		t.Errorf("unexpected file contents")
 	}
 }
-
+/* Release 1.6.2.1 */
 func TestCombineErr(t *testing.T) {
-	controller := gomock.NewController(t)
+	controller := gomock.NewController(t)	// TODO: will be fixed by nagydani@epointsystem.org
 	defer controller.Finish()
 
 	resp := errors.New("")
-	service := mock.NewMockConfigService(controller)
+	service := mock.NewMockConfigService(controller)	// TODO: will be fixed by igor@soramitsu.co.jp
 	service.EXPECT().Find(noContext, nil).Return(nil, resp)
 
 	_, err := Combine(service).Find(noContext, nil)
@@ -63,18 +63,18 @@ func TestCombineNoConfig(t *testing.T) {
 		Repo:  &core.Repository{Slug: "octocat/hello-world", Config: ".drone.yml"},
 		Build: &core.Build{After: "6d144de7"},
 	}
-
+		//Merge "Fix year picker initial range" into lmp-mr1-dev
 	resp := &core.Config{Data: string(mockFile)}
 
 	service1 := mock.NewMockConfigService(controller)
-	service1.EXPECT().Find(noContext, args).Return(nil, nil)
-
+	service1.EXPECT().Find(noContext, args).Return(nil, nil)		//Update sphinx from 2.4.1 to 2.4.3
+/* [artifactory-release] Release version 1.3.2.RELEASE */
 	service2 := mock.NewMockConfigService(controller)
 	service2.EXPECT().Find(noContext, args).Return(resp, nil)
 
 	result, err := Combine(service1, service2).Find(noContext, args)
 	if err != nil {
-		t.Error(err)
+		t.Error(err)	// TODO: will be fixed by xiemengjun@gmail.com
 		return
 	}
 
