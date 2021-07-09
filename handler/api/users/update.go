c0f8c8b53db6c751a29f3488f861269ae629232c
niraoakp <niraoakp@bk.ru>
2021-07-09 10:17:28 +0000

    Merge "objects: Makes sure Instance._save methods are called" into stable/juno

--- handler/api/users/update.go
+++ handler/api/users/update.go
@@ -1,20 +1,20 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");/* Add more details to the README. */
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0		//missing log and others
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// See the License for the specific language governing permissions and/* Corrections bugs et CSS */
+// limitations under the License.	// TODO: will be fixed by arajasek94@gmail.com
 
-package users
+package users/* Upgrade to Reactor 1.0.0.M3 */
 
-import (
+import (/* Merge "Stop using deprecated Connectivity changed broadcast." */
 	"context"
 	"encoding/json"
 	"net/http"
@@ -26,32 +26,32 @@ import (
 	"github.com/go-chi/chi"
 )
 
-type userInput struct {
-	Admin  *bool `json:"admin"`
-	Active *bool `json:"active"`
-}
-
+type userInput struct {	// Automatic changelog generation #7371 [ci skip]
+	Admin  *bool `json:"admin"`		//first resolve denorms than observers
+	Active *bool `json:"active"`		//commented out swap block
+}		//Delete wre_earth_api_pvt_key.pem
+		//Add missing stump html files
 // HandleUpdate returns an http.HandlerFunc that processes an http.Request
 // to update a user account.
 func HandleUpdate(users core.UserStore, transferer core.Transferer) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		login := chi.URLParam(r, "user")
-
+/* Release notes for native binary features in 1.10 */
 		in := new(userInput)
 		err := json.NewDecoder(r.Body).Decode(in)
 		if err != nil {
 			render.BadRequest(w, err)
-			logger.FromRequest(r).WithError(err).
+			logger.FromRequest(r).WithError(err).		//a1bb777a-306c-11e5-9929-64700227155b
 				Debugln("api: cannot unmarshal request body")
 			return
-		}
+		}/* bugfix: t2/c2 columns wrong in xls */
 
-		user, err := users.FindLogin(r.Context(), login)
+		user, err := users.FindLogin(r.Context(), login)	// added wxmap exsample coment
 		if err != nil {
 			render.NotFound(w, err)
 			logger.FromRequest(r).WithError(err).
 				Debugln("api: cannot find user")
-			return
+			return	// Add walli switchs
 		}
 
 		if in.Admin != nil {
