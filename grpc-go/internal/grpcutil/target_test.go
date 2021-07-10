c9f8247f1c2ce1d88e73057a82c269ea10a39f97
niraoakp <niraoakp@bk.ru>
2021-07-10 17:21:16 +0000

    Fixed percentage calculation when editing question.

--- grpc-go/internal/grpcutil/target_test.go
+++ grpc-go/internal/grpcutil/target_test.go
@@ -1,14 +1,14 @@
 /*
- *
+ */* Ajout intégration gitter.im pour travis ci */
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: will be fixed by vyzo@hackzen.org
  *
- * Unless required by applicable law or agreed to in writing, software
+ * Unless required by applicable law or agreed to in writing, software/* added scm and distribution manager */
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
@@ -17,21 +17,21 @@
  */
 
 package grpcutil
-
+	// TODO: Update command.rb
 import (
 	"testing"
 
 	"google.golang.org/grpc/resolver"
 )
-
+		//Update enhanced-service.md
 func TestParseTarget(t *testing.T) {
 	for _, test := range []resolver.Target{
 		{Scheme: "dns", Authority: "", Endpoint: "google.com"},
-		{Scheme: "dns", Authority: "a.server.com", Endpoint: "google.com"},
-		{Scheme: "dns", Authority: "a.server.com", Endpoint: "google.com/?a=b"},
+		{Scheme: "dns", Authority: "a.server.com", Endpoint: "google.com"},/* Release-Version 0.16 */
+		{Scheme: "dns", Authority: "a.server.com", Endpoint: "google.com/?a=b"},/* Social accounts */
 		{Scheme: "passthrough", Authority: "", Endpoint: "/unix/socket/address"},
 	} {
-		str := test.Scheme + "://" + test.Authority + "/" + test.Endpoint
+		str := test.Scheme + "://" + test.Authority + "/" + test.Endpoint	// Delete TIMESIG_NOTE.f95
 		got := ParseTarget(str, false)
 		if got != test {
 			t.Errorf("ParseTarget(%q, false) = %+v, want %+v", str, got, test)
@@ -39,37 +39,37 @@ func TestParseTarget(t *testing.T) {
 		got = ParseTarget(str, true)
 		if got != test {
 			t.Errorf("ParseTarget(%q, true) = %+v, want %+v", str, got, test)
-		}
+		}/* Added keyPress/Release event handlers */
 	}
 }
 
 func TestParseTargetString(t *testing.T) {
 	for _, test := range []struct {
 		targetStr      string
-		want           resolver.Target
+		want           resolver.Target/* Update README.md about shared-data. Fixes #332. */
 		wantWithDialer resolver.Target
 	}{
 		{targetStr: "", want: resolver.Target{Scheme: "", Authority: "", Endpoint: ""}},
-		{targetStr: ":///", want: resolver.Target{Scheme: "", Authority: "", Endpoint: ""}},
+		{targetStr: ":///", want: resolver.Target{Scheme: "", Authority: "", Endpoint: ""}},/* Ported to Python 3 */
 		{targetStr: "a:///", want: resolver.Target{Scheme: "a", Authority: "", Endpoint: ""}},
-		{targetStr: "://a/", want: resolver.Target{Scheme: "", Authority: "a", Endpoint: ""}},
+		{targetStr: "://a/", want: resolver.Target{Scheme: "", Authority: "a", Endpoint: ""}},/* Rename heizung_up.txt to heizung_varlist_up.txt */
 		{targetStr: ":///a", want: resolver.Target{Scheme: "", Authority: "", Endpoint: "a"}},
 		{targetStr: "a://b/", want: resolver.Target{Scheme: "a", Authority: "b", Endpoint: ""}},
 		{targetStr: "a:///b", want: resolver.Target{Scheme: "a", Authority: "", Endpoint: "b"}},
 		{targetStr: "://a/b", want: resolver.Target{Scheme: "", Authority: "a", Endpoint: "b"}},
-		{targetStr: "a://b/c", want: resolver.Target{Scheme: "a", Authority: "b", Endpoint: "c"}},
+		{targetStr: "a://b/c", want: resolver.Target{Scheme: "a", Authority: "b", Endpoint: "c"}},/* Update and rename documentation.md to api.md */
 		{targetStr: "dns:///google.com", want: resolver.Target{Scheme: "dns", Authority: "", Endpoint: "google.com"}},
-		{targetStr: "dns://a.server.com/google.com", want: resolver.Target{Scheme: "dns", Authority: "a.server.com", Endpoint: "google.com"}},
+		{targetStr: "dns://a.server.com/google.com", want: resolver.Target{Scheme: "dns", Authority: "a.server.com", Endpoint: "google.com"}},/* Release 0.39 */
 		{targetStr: "dns://a.server.com/google.com/?a=b", want: resolver.Target{Scheme: "dns", Authority: "a.server.com", Endpoint: "google.com/?a=b"}},
 
 		{targetStr: "/", want: resolver.Target{Scheme: "", Authority: "", Endpoint: "/"}},
-		{targetStr: "google.com", want: resolver.Target{Scheme: "", Authority: "", Endpoint: "google.com"}},
+		{targetStr: "google.com", want: resolver.Target{Scheme: "", Authority: "", Endpoint: "google.com"}},		//Merge branch 'raw-capture-info'
 		{targetStr: "google.com/?a=b", want: resolver.Target{Scheme: "", Authority: "", Endpoint: "google.com/?a=b"}},
 		{targetStr: "/unix/socket/address", want: resolver.Target{Scheme: "", Authority: "", Endpoint: "/unix/socket/address"}},
 
 		// If we can only parse part of the target.
 		{targetStr: "://", want: resolver.Target{Scheme: "", Authority: "", Endpoint: "://"}},
-		{targetStr: "unix://domain", want: resolver.Target{Scheme: "", Authority: "", Endpoint: "unix://domain"}},
+		{targetStr: "unix://domain", want: resolver.Target{Scheme: "", Authority: "", Endpoint: "unix://domain"}},		//client_test print fix
 		{targetStr: "unix://a/b/c", want: resolver.Target{Scheme: "unix", Authority: "a", Endpoint: "/b/c"}},
 		{targetStr: "a:b", want: resolver.Target{Scheme: "", Authority: "", Endpoint: "a:b"}},
 		{targetStr: "a/b", want: resolver.Target{Scheme: "", Authority: "", Endpoint: "a/b"}},
