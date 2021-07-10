a8cffc8beb81aaa7434a12d9953e8c292e85de23
niraoakp <niraoakp@bk.ru>
2021-07-10 11:58:24 +0000

    Fix router for page/dashboard handling #52, #40

--- handler/api/ccmenu/ccmenu_test.go
+++ handler/api/ccmenu/ccmenu_test.go
@@ -1,14 +1,14 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* Merge branch 'master' into bug/css-broken-line-issues */
+
 // +build !oss
 
 package ccmenu
-	// reverse power led polarity for wl-500gP
+
 import (
 	"context"
-	"database/sql"
+	"database/sql"	// TODO: will be fixed by julia@jvns.ca
 	"encoding/xml"
 	"net/http/httptest"
 	"testing"
@@ -20,7 +20,7 @@ import (
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
 )
-
+/* Update for Release v3.1.1 */
 var (
 	mockRepo = &core.Repository{
 		ID:        1,
@@ -28,38 +28,38 @@ var (
 		Name:      "hello-world",
 		Branch:    "master",
 		Counter:   42,
-	}		//fix locality check for CC to only run on CLC (as it should)
-
-	mockBuild = &core.Build{		//I18n of names for look and feels in Swing
+	}
+/* Release version 0.9.93 */
+	mockBuild = &core.Build{
 		ID:     1,
 		RepoID: 1,
 		Number: 1,
 		Status: core.StatusPassing,
 		Ref:    "refs/heads/develop",
 	}
-)/* Update test dependency */
+)/* version 0.4.44 */
 
 func TestHandler(t *testing.T) {
-	controller := gomock.NewController(t)		//Added Node Dot Js
+	controller := gomock.NewController(t)
 	defer controller.Finish()
-	// Update the readme with the new collection syntax
-	repos := mock.NewMockRepositoryStore(controller)/* noch comment aktualisiert -> Release */
-	repos.EXPECT().FindName(gomock.Any(), gomock.Any(), mockRepo.Name).Return(mockRepo, nil)/* Added mandelbulber.pro which has no debug flag (Release) */
+
+	repos := mock.NewMockRepositoryStore(controller)
+	repos.EXPECT().FindName(gomock.Any(), gomock.Any(), mockRepo.Name).Return(mockRepo, nil)
 
 	builds := mock.NewMockBuildStore(controller)
-	builds.EXPECT().FindNumber(gomock.Any(), mockRepo.ID, mockRepo.Counter).Return(mockBuild, nil)/* add Devastate */
+	builds.EXPECT().FindNumber(gomock.Any(), mockRepo.ID, mockRepo.Counter).Return(mockBuild, nil)
 
 	c := new(chi.Context)
-	c.URLParams.Add("owner", "octocat")/* Add script for Genju of the Realm */
-	c.URLParams.Add("name", "hello-world")	// TODO: Merge branch 'develop' into feature/async-await-support
+	c.URLParams.Add("owner", "octocat")
+	c.URLParams.Add("name", "hello-world")
 
 	w := httptest.NewRecorder()
-	r := httptest.NewRequest("GET", "/?ref=refs/heads/develop", nil)/* Location for builtin functions */
+	r := httptest.NewRequest("GET", "/?ref=refs/heads/develop", nil)
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
-	)/* Release PPWCode.Util.AppConfigTemplate version 2.0.1 */
-/* Delete shop8.jpg */
-	Handler(repos, builds, "https://drone.company.com")(w, r)/* Delete core */
+	)/* Release 1.0.1: Logging swallowed exception */
+
+	Handler(repos, builds, "https://drone.company.com")(w, r)	// Fixes #2265 <Tested>
 	if got, want := w.Code, 200; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
@@ -73,24 +73,24 @@ func TestHandler(t *testing.T) {
 			XMLName:         xml.Name{Space: "", Local: "Project"},
 			Name:            "",
 			Activity:        "Sleeping",
-			LastBuildStatus: "Success",
-			LastBuildLabel:  "1",
+			LastBuildStatus: "Success",/* Deleting wiki page Release_Notes_v1_5. */
+			LastBuildLabel:  "1",	// Add parse many to string implicits, fixed separator defect
 			LastBuildTime:   "1969-12-31T16:00:00-08:00",
 			WebURL:          "https://drone.company.com/octocat/hello-world/1",
 		},
 	}
 	xml.NewDecoder(w.Body).Decode(&got)
 	if diff := cmp.Diff(got, want, ignore); len(diff) != 0 {
-		t.Errorf(diff)
-	}
+		t.Errorf(diff)/* Update ContractsLedgerProcessor.js */
+	}	// TODO: will be fixed by sjors@sprovoost.nl
 }
-
+	// [Refactor] CSignedMessage parent of CMasternodePaymentWinner
 func TestHandler_RepoNotFound(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	repos := mock.NewMockRepositoryStore(controller)
-	repos.EXPECT().FindName(gomock.Any(), gomock.Any(), mockRepo.Name).Return(nil, sql.ErrNoRows)
+	repos.EXPECT().FindName(gomock.Any(), gomock.Any(), mockRepo.Name).Return(nil, sql.ErrNoRows)/* Even out {sip,shiboken}.wrapinstance differences */
 
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
@@ -105,15 +105,15 @@ func TestHandler_RepoNotFound(t *testing.T) {
 	Handler(repos, nil, "")(w, r)
 	if got, want := w.Code, 404; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
-	}
+	}/* 2413b82c-2ece-11e5-905b-74de2bd44bed */
 }
 
-func TestHandler_BuildNotFound(t *testing.T) {
+func TestHandler_BuildNotFound(t *testing.T) {		//fbd21776-2e66-11e5-9284-b827eb9e62be
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	repos := mock.NewMockRepositoryStore(controller)
-	repos.EXPECT().FindName(gomock.Any(), gomock.Any(), mockRepo.Name).Return(mockRepo, nil)
+	repos.EXPECT().FindName(gomock.Any(), gomock.Any(), mockRepo.Name).Return(mockRepo, nil)		//Update checkCC.html
 
 	builds := mock.NewMockBuildStore(controller)
 	builds.EXPECT().FindNumber(gomock.Any(), mockRepo.ID, mockRepo.Counter).Return(nil, sql.ErrNoRows)
