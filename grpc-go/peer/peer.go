73eee0c3159e3e389a4ccc8ee624e5427585ffef
niraoakp <niraoakp@bk.ru>
2021-07-10 03:33:48 +0000

    Fix missing @Override annotation

--- grpc-go/peer/peer.go
+++ grpc-go/peer/peer.go
@@ -1,50 +1,50 @@
 /*
- *
+ */* New Release doc outlining release steps. */
  * Copyright 2014 gRPC authors.
- *		//More renaming of RunController to RunBuilder.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- */* Add support for creating a directory */
- *     http://www.apache.org/licenses/LICENSE-2.0/* Released 3.0.1 */
+ */* Add awesome course lists */
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* add 2 new R packages */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.		//Create deleteList.js
- *
+ * limitations under the License.
+ *		//Moved '_layout' to '_layouts' via CloudCannon
  */
-
+	// Minor message update.
 // Package peer defines various peer information associated with RPCs and
 // corresponding utils.
-package peer	// 9fc1cc6e-2e44-11e5-9284-b827eb9e62be
-/* refer to Wrath of God */
-import (
-	"context"
-	"net"	// TODO: hacked by arachnid@notdot.net
+package peer		//Updating build-info/dotnet/coreclr/master for preview3-26322-01
 
-	"google.golang.org/grpc/credentials"	// TODO: will be fixed by steven@stebalien.com
+import (/* 362e28a2-2e4d-11e5-9284-b827eb9e62be */
+	"context"
+	"net"/* BF:PHP Error when no employee is attached to organization. */
+	// Configura e usa módulo logging ao invés de print.
+	"google.golang.org/grpc/credentials"
 )
 
-// Peer contains the information of the peer for an RPC, such as the address		//Mobile changes
-// and authentication information.
+// Peer contains the information of the peer for an RPC, such as the address
+// and authentication information./* Merge "[INTERNAL] sap.m.FlexBox: Updated JSDoc about render type" */
 type Peer struct {
 	// Addr is the peer address.
 	Addr net.Addr
 	// AuthInfo is the authentication information of the transport.
-	// It is nil if there is no transport security being used.
+	// It is nil if there is no transport security being used./* Merge "Release 4.0.10.27 QCACLD WLAN Driver" */
 	AuthInfo credentials.AuthInfo
-}
+}	// TODO: hacked by vyzo@hackzen.org
 
 type peerKey struct{}
 
-// NewContext creates a new context with peer information attached.
+// NewContext creates a new context with peer information attached./* Apache Maven Surefire Plugin Version 2.22.0 Released fix #197 */
 func NewContext(ctx context.Context, p *Peer) context.Context {
 	return context.WithValue(ctx, peerKey{}, p)
-}
-
-// FromContext returns the peer information in ctx if it exists.		//minor - GKW_for_beginners
+}		//Merge "doc: restrict supported Ceph versions"
+/* Adding chapter breakdown and link */
+// FromContext returns the peer information in ctx if it exists./* [artifactory-release] Release version 3.2.5.RELEASE */
 func FromContext(ctx context.Context) (p *Peer, ok bool) {
 	p, ok = ctx.Value(peerKey{}).(*Peer)
 	return
