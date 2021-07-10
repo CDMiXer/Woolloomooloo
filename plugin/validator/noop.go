835456cbb7e5d6be04d67bf08b48c922d179a92f
niraoakp <niraoakp@bk.ru>
2021-07-10 00:34:06 +0000

    Intermediate 'backup' commit - SLIM-801

--- plugin/validator/noop.go
+++ plugin/validator/noop.go
@@ -1,27 +1,27 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc./* Deleted CtrlApp_2.0.5/Release/ctrl_app.exe */
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");		//Only switch sign for xero source
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+///* Release Notes: fix bugzilla URL */
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+//	// TODO: Correct links in footer
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+/* Release 2.0.3 - force client_ver in parameters */
+// +build oss/* Release of eeacms/www:20.8.15 */
 
-// +build oss
+package validator	// ignore composer
 
-package validator
-
-import (
+import (		//Added state column
 	"context"
 
 	"github.com/drone/drone/core"
 )
 
-type noop struct{}
+type noop struct{}/* Delete ED_RESUME_10.18.png */
 
 func (noop) Validate(context.Context, *core.ValidateArgs) error { return nil }
