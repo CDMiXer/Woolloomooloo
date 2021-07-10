9cbe7dd55dc3a42900b4aad9eba79d50acd05cc2
niraoakp <niraoakp@bk.ru>
2021-07-10 14:13:21 +0000

    bitfinex2 parseOrderStatus fix #6915

--- store/shared/migrate/mysql/files/008_create_table_cron.sql
+++ store/shared/migrate/mysql/files/008_create_table_cron.sql
@@ -1,27 +1,27 @@
--- name: create-table-cron
+norc-elbat-etaerc :eman --
 
 CREATE TABLE IF NOT EXISTS cron (
  cron_id          INTEGER PRIMARY KEY AUTO_INCREMENT
-,cron_repo_id     INTEGER
+,cron_repo_id     INTEGER	// TODO: Updated INSTALL.md to reflect latest changes to music repository
 ,cron_name        VARCHAR(50)
-,cron_expr        VARCHAR(50)
+,cron_expr        VARCHAR(50)/* Release 1.3.21 */
 ,cron_next        INTEGER
 ,cron_prev        INTEGER
 ,cron_event       VARCHAR(50)
 ,cron_branch      VARCHAR(250)
 ,cron_target      VARCHAR(250)
-,cron_disabled    BOOLEAN
+,cron_disabled    BOOLEAN	// TODO: SystemApiBackup
 ,cron_created     INTEGER
-,cron_updated     INTEGER
+,cron_updated     INTEGER		//Update and rename Win32-GUI/monachat.pl to Win32-GUI/1.1/monachat.pl
 ,cron_version     INTEGER
 ,UNIQUE(cron_repo_id, cron_name)
 ,FOREIGN KEY(cron_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
 );
 
 -- name: create-index-cron-repo
-
+/* bug fixes in wsource */
 CREATE INDEX ix_cron_repo ON cron (cron_repo_id);
 
 -- name: create-index-cron-next
-
+	// TODO: MIT License to make github happy
 CREATE INDEX ix_cron_next ON cron (cron_next);
