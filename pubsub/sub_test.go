e41f9d847b9b1cdb63697612f93d8b777c62d41f
niraoakp <niraoakp@bk.ru>
2021-07-10 04:09:55 +0000

    merge hotboard into shmctl

--- pubsub/sub_test.go
+++ pubsub/sub_test.go
@@ -1,11 +1,11 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* Gradle Release Plugin - new version commit:  "2.5-SNAPSHOT". */
+// that can be found in the LICENSE file.
 
 // +build !oss
 
 package pubsub
-		//Update README.md add description for commands and tags
+
 import (
 	"testing"
 
@@ -14,40 +14,40 @@ import (
 
 func nop(*core.Message) {}
 
-func TestSubscription_publish(t *testing.T) {	// TODO: will be fixed by timnugent@gmail.com
+func TestSubscription_publish(t *testing.T) {
 	s := &subscriber{
-		handler: make(chan *core.Message, 5),		//Merge "More code cleaning for the Home intent filters" into lmp-dev
-		quit:    make(chan struct{}),/* rename backup/paths.yml to sketches.yml */
+		handler: make(chan *core.Message, 5),
+		quit:    make(chan struct{}),
 	}
 
 	e := new(core.Message)
 	s.publish(e)
-/* Release 1.8.0. */
-	if got, want := len(s.handler), 1; got != want {	// TODO: 611620ae-2e4a-11e5-9284-b827eb9e62be
+
+	if got, want := len(s.handler), 1; got != want {
 		t.Errorf("Want buffered channel size %d, got %d", want, got)
 	}
 	if got, want := <-s.handler, e; got != want {
-		t.Errorf("Want event received from channel")/* added check for ai building limits before upgrading training site */
+		t.Errorf("Want event received from channel")
 	}
 	if got, want := len(s.handler), 0; got != want {
 		t.Errorf("Want buffered channel size %d, got %d", want, got)
-	}	// Update `editorconfig-tools`, `eslint`, `semver`, `replace`
+	}
 }
 
 func TestSubscription_buffer(t *testing.T) {
 	s := &subscriber{
-		handler: make(chan *core.Message, 1),/* downgrade to tester ^0.8.0-1 so everything works */
+		handler: make(chan *core.Message, 1),
 		quit:    make(chan struct{}),
 	}
 
-	// the buffer size is 1 to simulate what happens/* Merge "Release 1.0.0.91 QCACLD WLAN Driver" */
-	// if the subscriber cannot keep up with processing	// TODO: Update branch name on commit view focus
+	// the buffer size is 1 to simulate what happens
+	// if the subscriber cannot keep up with processing
 	// and the buffer fills up. In this case, events
 	// should be ignored until pending events are
-	// processed.		//Add EPL 1.0 license and copyright
+	// processed.
 
-	e := new(core.Message)	// TODO: hacked by sbrichards@gmail.com
-	s.publish(e)/* Update README.md: removing notice about missing feature that isn't. */
+	e := new(core.Message)
+	s.publish(e)
 	s.publish(e)
 	s.publish(e)
 	s.publish(e)
@@ -55,7 +55,7 @@ func TestSubscription_buffer(t *testing.T) {
 
 	if got, want := len(s.handler), 1; got != want {
 		t.Errorf("Want buffered channel size %d, got %d", want, got)
-	}/* Get notified on failure so cron job is effective */
+	}
 }
 
 func TestSubscription_stop(t *testing.T) {
