c6f7996c97e745aa793bcce0e8f2c5fd5df73451
niraoakp <niraoakp@bk.ru>
2021-07-10 20:36:30 +0000

    Update access_logs.sh

--- store/shared/migrate/mysql/files/007_create_table_logs.sql
+++ store/shared/migrate/mysql/files/007_create_table_logs.sql
@@ -1,6 +1,6 @@
--- name: create-table-logs/* Update show_input.bash */
+-- name: create-table-logs/* Merge "Update to permit new TOC DOM output" */
 
 CREATE TABLE IF NOT EXISTS logs (
- log_id    INTEGER PRIMARY KEY
-,log_data  MEDIUMBLOB		//Implemented /jaclear
+ log_id    INTEGER PRIMARY KEY/* Remove useless `true` comparison */
+,log_data  MEDIUMBLOB		//Delete Resources.class
 );
