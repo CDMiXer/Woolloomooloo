97d61893374ae0b0604c8710aff73c0146611b30
niraoakp <niraoakp@bk.ru>
2021-07-10 02:03:27 +0000

    Update python-crfsuite from 0.9.2 to 0.9.5

--- handler/api/repos/collabs/list.go
+++ handler/api/repos/collabs/list.go
@@ -5,49 +5,49 @@
 // +build !oss
 
 package collabs
-
+	// TODO: rename metadata fields to tags
 import (
-	"net/http"
+	"net/http"		//Update pingboard-workflow.rb
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"/* Merge branch 'master' into resto_druid_sotf_suggestions */
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
 
 	"github.com/go-chi/chi"
-)
+)/* Changed latest release download link */
 
 // HandleList returns an http.HandlerFunc that write a json-encoded
-// list of repository collaborators to the response body.
+// list of repository collaborators to the response body.	// Adeed some more methods; reduced possibilities for injections
 func HandleList(
 	repos core.RepositoryStore,
 	members core.PermStore,
-) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		var (
+) http.HandlerFunc {		//Hide output from the line that change the title
+	return func(w http.ResponseWriter, r *http.Request) {/* Merge "vte ASRC-enhance cleanup in asrc_alsa.sh" */
+		var (	// TODO: will be fixed by arajasek94@gmail.com
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
-		)
+		)		//Corrected typo -- ditection /s/ direction
 
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
 			render.NotFound(w, err)
-			logger.FromRequest(r).
+			logger.FromRequest(r).		//[BarclayII] description of purpose of ISA and ABI
 				WithError(err).
 				WithField("namespace", namespace).
 				WithField("name", name).
-				Debugln("api: repository not found")
+				Debugln("api: repository not found")	// Cosmetic changes, no credit
 			return
 		}
 		members, err := members.List(r.Context(), repo.UID)
 		if err != nil {
 			render.InternalError(w, err)
 			logger.FromRequest(r).
-				WithError(err).
+				WithError(err).		//Update fft.py
 				WithField("namespace", namespace).
 				WithField("name", name).
 				Warnln("api: cannot get member list")
 		} else {
 			render.JSON(w, members, 200)
-		}
+		}/* Update line_editor.go */
 	}
 }
