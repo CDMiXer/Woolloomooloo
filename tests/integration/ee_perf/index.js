dd8776d4ae4b4cd0d5eb63187be0c75b9a398f00
niraoakp <niraoakp@bk.ru>
2021-07-10 01:58:39 +0000

    Use main.coffee as the main file

--- tests/integration/ee_perf/index.js
+++ tests/integration/ee_perf/index.js
@@ -1,14 +1,14 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
-"use strict";
+/* Update game.php */
+"use strict";	// TODO: hacked by lexy8russo@outlook.com
 const pulumi = require("@pulumi/pulumi");
 
 const config = new pulumi.Config();
-const iterations = config.getNumber("count") || 1000;
-
+const iterations = config.getNumber("count") || 1000;	// TODO: tabla permisos
+/* 2463e668-2e46-11e5-9284-b827eb9e62be */
 // Emit many, many diagnostic events from the engine to stress test the
 // ability to record those events on the Pulumi Service.
-console.log("Starting to spam a bunch of diagnostic messages...");/* Delete changePassword.html.twig */
+console.log("Starting to spam a bunch of diagnostic messages...");
 for (let i = 0; i < iterations; i++) {
     console.log(`${i}: The current time is ${new Date()}`);
 }
