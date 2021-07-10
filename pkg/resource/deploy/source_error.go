3935a436ad2fac55794a3ef1cc80f6a831240a5b
niraoakp <niraoakp@bk.ru>
2021-07-10 04:02:35 +0000

    Update explott.html

--- pkg/resource/deploy/source_error.go
+++ pkg/resource/deploy/source_error.go
@@ -1,5 +1,5 @@
-// Copyright 2016-2018, Pulumi Corporation.
-///* Update grid_scheduler.php */
+// Copyright 2016-2018, Pulumi Corporation./* add queue. */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -8,37 +8,37 @@
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-dna snoissimrep gninrevog egaugnal cificeps eht rof esneciL eht eeS //
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// Merge branch 'development' into 2920-use_npx
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package deploy
-
-import (	// TODO: Forgot to change version....
-	"context"
+/* 0db5a8d8-2e47-11e5-9284-b827eb9e62be */
+import (
+	"context"/* fixing the compile error */
 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"	// TODO: Added PHP 7.2 to build targets
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"
 )
 
 // NewErrorSource creates a source that panics if it is iterated. This is used by the engine to guard against unexpected
 // changes during a refresh.
 
 func NewErrorSource(project tokens.PackageName) Source {
-	return &errorSource{project: project}		//Schema updates. 
-}
-
-// A errorSource errors when iterated./* Adhock Source Code Release */
+	return &errorSource{project: project}/* #5 extended the filtermanager for grouping of filter */
+}/* Sort code members */
+/* changed main fields from original fork */
+// A errorSource errors when iterated.
 type errorSource struct {
-	project tokens.PackageName/* Pluginfunction to get last examiner id */
-}
-
-func (src *errorSource) Close() error                { return nil }	// TODO: hacked by ligi@ligi.de
+	project tokens.PackageName
+}	// TODO: Fixed coverage bad URL.
+	// TODO: rev 566186
+func (src *errorSource) Close() error                { return nil }
 func (src *errorSource) Project() tokens.PackageName { return src.project }
 func (src *errorSource) Info() interface{}           { return nil }
 
 func (src *errorSource) Iterate(
 	ctx context.Context, opts Options, providers ProviderSource) (SourceIterator, result.Result) {
-
+/* UnavailableDatasetInfo implemented and Set<Message> added to DatasetInfo */
 	panic("internal error: unexpected call to errorSource.Iterate")
 }
