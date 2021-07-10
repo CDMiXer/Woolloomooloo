38a6394522ae07640e545bb4efdebe3a4c49e980
niraoakp <niraoakp@bk.ru>
2021-07-10 13:41:45 +0000

    Revert r198979 - accidental commit.

--- plugin/validator/remote_test.go
+++ plugin/validator/remote_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Update target definitions following the KNIME 3.6 Release */
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
 
-package validator	// TODO: will be fixed by 13860583249@yeah.net
+package validator
