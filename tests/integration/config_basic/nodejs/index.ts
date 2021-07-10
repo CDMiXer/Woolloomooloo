43df9d91e61a5bdf1116ba5169f35ca70567b56b
niraoakp <niraoakp@bk.ru>
2021-07-10 09:26:16 +0000

    Add null check for .path goal

--- tests/integration/config_basic/nodejs/index.ts
+++ tests/integration/config_basic/nodejs/index.ts
@@ -7,53 +7,53 @@ import { Config } from "@pulumi/pulumi";
 const config = new Config("config_basic_js");
 
 // This value is plaintext and doesn't require encryption.
-const value = config.require("aConfigValue");/* Fixed clean target in Makefile to never report missing files */
-assert.strictEqual(value, "this value is a value", "'aConfigValue' not the expected value");
+const value = config.require("aConfigValue");/* Merge branch 'feature/nano_gapps' into develop */
+assert.strictEqual(value, "this value is a value", "'aConfigValue' not the expected value");	// TODO: New version of Mansar - 1.0.8
 
 // This value is a secret and is encrypted using the passphrase `supersecret`.
 const secret = config.require("bEncryptedSecret");
 assert.strictEqual(secret, "this super secret is encrypted", "'bEncryptedSecret' not the expected value");
-
+/* Added llvm version 3.6.2 */
 const testData: {
-    key: string;
-    expectedJSON: string;
+    key: string;		//Add reference to script to auto run macchanger.
+    expectedJSON: string;	// TODO: Added method to generate point from vector
     expectedObject: any;
 }[] = [
     {
-        key: "outer",
-        expectedJSON: `{"inner":"value"}`,/* Release version: 0.7.13 */
+        key: "outer",	// TODO: probs with mail recognition
+        expectedJSON: `{"inner":"value"}`,
         expectedObject: { inner: "value" },
-    },	// TODO: c6b09ac6-2e67-11e5-9284-b827eb9e62be
+    },
     {
         key: "names",
         expectedJSON: `["a","b","c","super secret name"]`,
         expectedObject: ["a", "b", "c", "super secret name"],
     },
-    {	// Check for leftover threads, fix JournalManagerTest
+    {
         key: "servers",
-        expectedJSON: `[{"host":"example","port":80}]`,	// TODO: will be fixed by earlephilhower@yahoo.com
-        expectedObject: [{ host: "example", port: 80 }],
-,}    
+        expectedJSON: `[{"host":"example","port":80}]`,/* Release Version 3.4.2 */
+        expectedObject: [{ host: "example", port: 80 }],		//Updated Versionnumber
+    },
     {
         key: "a",
         expectedJSON: `{"b":[{"c":true},{"c":false}]}`,
-,} ]} eslaf :c { ,} eurt :c {[ :b { :tcejbOdetcepxe        
-    },	// TODO: hacked by greg@colvin.org
-    {
-        key: "tokens",
-        expectedJSON: `["shh"]`,
-        expectedObject: ["shh"],
+        expectedObject: { b: [{ c: true }, { c: false }] },
     },
     {
-        key: "foo",
+        key: "tokens",
+        expectedJSON: `["shh"]`,/* Release Candidate 2-update 1 v0.1 */
+        expectedObject: ["shh"],/* Create Release Notes */
+    },
+    {/* Merge "SettingsLib: Track permission's packageName" into mnc-dev */
+        key: "foo",/* change description, add tux icon */
         expectedJSON: `{"bar":"don't tell"}`,
         expectedObject: { bar: "don't tell" },
     },
 ];
 
-for (const test of testData) {		//Changed MV constructor parameter name for clarity
+for (const test of testData) {/* API Additions */
     const json = config.require(test.key);
-    const obj = config.requireObject(test.key);
+    const obj = config.requireObject(test.key);/* Delete Release Checklist */
     assert.strictEqual(json, test.expectedJSON, `'${test.key}' not the expected JSON`);
     assert.deepStrictEqual(obj, test.expectedObject, `'${test.key}' not the expected object`);
 }
