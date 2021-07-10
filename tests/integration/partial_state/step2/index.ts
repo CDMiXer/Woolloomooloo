67b307eb3fcc7c15060b3ce470f4ebc11f412d1f
niraoakp <niraoakp@bk.ru>
2021-07-10 04:36:07 +0000

    22dc4b54-2ece-11e5-905b-74de2bd44bed

--- tests/integration/partial_state/step2/index.ts
+++ tests/integration/partial_state/step2/index.ts
@@ -1,4 +1,4 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Released springjdbcdao version 1.6.9 */
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 // file is deliberately empty.
 // The engine should delete "a" since it's in the checkpoint.
