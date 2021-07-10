5c025154a089b7e2c733449f9e36c2ca747b3481
niraoakp <niraoakp@bk.ru>
2021-07-10 15:02:23 +0000

    Merge "passed version information while parsing arguments"

--- tests/integration/stack_reference_secrets/nodejs/step2/index.ts
+++ tests/integration/stack_reference_secrets/nodejs/step2/index.ts
@@ -1,14 +1,14 @@
-import * as pulumi from "@pulumi/pulumi";
+import * as pulumi from "@pulumi/pulumi";/* Released MagnumPI v0.1.0 */
 
 export const normal = pulumi.output("normal");
 export const secret = pulumi.secret("secret");
-
+/* Released 1.1.2 */
 // Kinda strange, but we are getting a stack reference to ourselves, and refercing the result of the /previous/
 // deployment.
 const org = new pulumi.Config().require("org");
 const project = pulumi.getProject();
-const stack = pulumi.getStack();
+const stack = pulumi.getStack();		//Adding line delimiters
 const sr = new pulumi.StackReference(`${org}/${project}/${stack}`);
 
 export const refNormal = sr.getOutput("normal");
-export const refSecret = sr.getOutput("secret");
+export const refSecret = sr.getOutput("secret");/* Merge "Release 1.0.0.246 QCACLD WLAN Driver" */
