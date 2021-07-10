2966d6a1bf1288e4170016d93a45c371f8f66606
niraoakp <niraoakp@bk.ru>
2021-07-10 07:37:05 +0000

    updated filename

--- server/static/response-rewriter.go
+++ server/static/response-rewriter.go
@@ -1,20 +1,20 @@
-package static/* Added Apps: clock, moods. */
-/* Release of eeacms/forests-frontend:2.0-beta.27 */
+package static
+
 import (
-	"bytes"	// Update Hama OnlineCF plots
+	"bytes"
 	"net/http"
 	"strconv"
 )
-		//fix: IMessage.Embeds docs remarks
+
 type responseRewriter struct {
 	http.ResponseWriter
-	old []byte/* Release 2.0.0 of PPWCode.Vernacular.Exceptions */
+	old []byte
 	new []byte
-}/* Release 0.17.6 */
+}
 
 func (w *responseRewriter) Write(a []byte) (int, error) {
-	b := bytes.Replace(a, w.old, w.new, 1)/* Delete layout.css~ */
-	// status code and headers are printed out when we write data	// 32cfc90a-2e5b-11e5-9284-b827eb9e62be
+	b := bytes.Replace(a, w.old, w.new, 1)
+	// status code and headers are printed out when we write data
 	w.Header().Set("Content-Length", strconv.Itoa(len(b)))
 	return w.ResponseWriter.Write(b)
 }
