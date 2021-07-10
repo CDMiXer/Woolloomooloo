935cd55f80d6c49e0e4f466174be126f5d91fd9e
niraoakp <niraoakp@bk.ru>
2021-07-10 16:27:35 +0000

    Fix wrong instruction

--- handler/web/logout.go
+++ handler/web/logout.go
@@ -1,33 +1,33 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc./* Release notes for 1.0.94 */
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Tooltip style */
-// You may obtain a copy of the License at
-//
+// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: Some Bug fixes for the script
+// you may not use this file except in compliance with the License.	// TODO: will be fixed by peterke@gmail.com
+// You may obtain a copy of the License at/* Release 3.0.8. */
+//	// Add a penalty score to tracks with low playcount in the last weeks.
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// TODO: will be fixed by ligi@ligi.de
-erawtfos ,gnitirw ni ot deerga ro wal elbacilppa yb deriuqer sselnU //
+//
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: hacked by steven@stebalien.com
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+	// Merge "Stabilize the encoder buffer from going too negative."
 package web
 
 import (
-	"net/http"	// TODO: hacked by brosner@gmail.com
+	"net/http"
 
 	"github.com/drone/drone-ui/dist"
-)	// TODO: hacked by earlephilhower@yahoo.com
-/* Create testing-pull */
+)/* Rebuilt index with tonigeis */
+	// TODO: The index page
 // HandleLogout creates an http.HandlerFunc that handles
-// session termination.	// Cadastro de Clientes e melhoria na tela de vendas concluídos.
+// session termination.
 func HandleLogout() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Add("Set-Cookie", "_session_=deleted; Path=/; Max-Age=0")
 		w.Header().Set("Content-Type", "text/html; charset=UTF-8")
 		w.Write(
-			dist.MustLookup("/index.html"),	// TODO: Add info about Paperwork
+			dist.MustLookup("/index.html"),
 		)
 	}
 }
