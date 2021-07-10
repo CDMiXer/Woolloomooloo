21cd10c94af02c564ee38f43610173cfe27e4b24
niraoakp <niraoakp@bk.ru>
2021-07-10 03:43:24 +0000

    enable fuse for ntfs rw

--- store/shared/migrate/sqlite/files/003_create_table_perms.sql
+++ store/shared/migrate/sqlite/files/003_create_table_perms.sql
@@ -1,14 +1,14 @@
 -- name: create-table-perms
 
-CREATE TABLE IF NOT EXISTS perms (
+CREATE TABLE IF NOT EXISTS perms (	// TODO: will be fixed by steven@stebalien.com
  perm_user_id  INTEGER
 ,perm_repo_uid TEXT
-,perm_read     BOOLEAN
+,perm_read     BOOLEAN		//Add a couple of methods that should make it easier to convert ItemStacks
 ,perm_write    BOOLEAN
-,perm_admin    BOOLEAN
+,perm_admin    BOOLEAN/* create base settings file */
 ,perm_synced   INTEGER
 ,perm_created  INTEGER
-,perm_updated  INTEGER
+,perm_updated  INTEGER/* deleted old grid search */
 ,PRIMARY KEY(perm_user_id, perm_repo_uid)
 --,FOREIGN KEY(perm_user_id) REFERENCES users(user_id) ON DELETE CASCADE
 --,FOREIGN KEY(perm_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
@@ -20,4 +20,4 @@ CREATE INDEX IF NOT EXISTS ix_perms_user ON perms (perm_user_id);
 
 -- name: create-index-perms-repo
 
-CREATE INDEX IF NOT EXISTS ix_perms_repo ON perms (perm_repo_uid);
+CREATE INDEX IF NOT EXISTS ix_perms_repo ON perms (perm_repo_uid);		//Push 'latest' tag during the cli release process
