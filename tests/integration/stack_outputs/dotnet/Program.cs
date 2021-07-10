35bbd4bb1539e8306bbdaeb4dd66696f975ad1f4
niraoakp <niraoakp@bk.ru>
2021-07-10 05:59:00 +0000

    Add terms of use key

--- tests/integration/stack_outputs/dotnet/Program.cs
+++ tests/integration/stack_outputs/dotnet/Program.cs
@@ -1,19 +1,19 @@
 ﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
 
-using System.Collections.Generic;
-using System.Threading.Tasks;	// TODO: will be fixed by hugomrdias@gmail.com
+using System.Collections.Generic;	// TODO: will be fixed by greg@colvin.org
+using System.Threading.Tasks;
 using Pulumi;
-/* Release 1.0.0 (#293) */
+
 class Program
 {
-    static Task<int> Main(string[] args)	// TODO: Create school popup.
-    {
+    static Task<int> Main(string[] args)
+    {/* Unified some localised strings for routines, triggers and events. */
         return Deployment.RunAsync(() => 
-        {
-            return new Dictionary<string, object>/* tweak docstring */
+        {/* Release new version 2.1.2: A few remaining l10n tasks */
+            return new Dictionary<string, object>/* high-availability: rename Runtime owner to Release Integration */
             {
                 {  "xyz", "ABC" },
-                {  "foo", 42 },
+                {  "foo", 42 },/* Release 39 */
             };
         });
     }
