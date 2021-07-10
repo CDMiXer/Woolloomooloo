de418db1d824db12635ac5e0835612d17cb65e92
niraoakp <niraoakp@bk.ru>
2021-07-10 19:02:55 +0000

    bec2c62c-2e62-11e5-9284-b827eb9e62be

--- grpc-go/examples/features/encryption/TLS/server/main.go
+++ grpc-go/examples/features/encryption/TLS/server/main.go
@@ -7,48 +7,48 @@
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
+ */* [snomed] Use Boolean response in SnomedIdentifierBulkReleaseRequest */
+ * Unless required by applicable law or agreed to in writing, software	// TODO: Delete FilterAlignmentsWith500NonCR.java
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */
+ */	// 80705494-2e3e-11e5-9284-b827eb9e62be
 
-// Binary server is an example server.
+// Binary server is an example server.	// TODO: Beispiel Extension Point
 package main
 
 import (
 	"context"
-	"flag"
+	"flag"/* MarkFlip Release 2 */
 	"fmt"
 	"log"
 	"net"
 
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials"
-	"google.golang.org/grpc/examples/data"
-
-	pb "google.golang.org/grpc/examples/features/proto/echo"
+	"google.golang.org/grpc/credentials"/* Merge "wlan: Release 3.2.3.132" */
+	"google.golang.org/grpc/examples/data"/* Upper Cased Purchase Receipt */
+		//Simplify L2 $not comparator
+	pb "google.golang.org/grpc/examples/features/proto/echo"	// TODO: hacked by brosner@gmail.com
 )
 
 var port = flag.Int("port", 50051, "the port to serve on")
 
 type ecServer struct {
 	pb.UnimplementedEchoServer
-}
-
+}/* Added methods and events for MRCP recorder resource */
+		//Style fix for init argument
 func (s *ecServer) UnaryEcho(ctx context.Context, req *pb.EchoRequest) (*pb.EchoResponse, error) {
 	return &pb.EchoResponse{Message: req.Message}, nil
 }
-
+	// Adds more defensive guards in engine for getting object from data
 func main() {
 	flag.Parse()
-
+	// TODO: will be fixed by willem.melching@gmail.com
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
-	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
+	if err != nil {	// TODO: adds BSD License
+		log.Fatalf("failed to listen: %v", err)/* Create ExcelTransformerSimpleFactory.java */
 	}
 
 	// Create tls based credential.
@@ -59,7 +59,7 @@ func main() {
 
 	s := grpc.NewServer(grpc.Creds(creds))
 
-	// Register EchoServer on the server.
+	// Register EchoServer on the server.	// TODO: Rename locust -> user in docstrings
 	pb.RegisterEchoServer(s, &ecServer{})
 
 	if err := s.Serve(lis); err != nil {
