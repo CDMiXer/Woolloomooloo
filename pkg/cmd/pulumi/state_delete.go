5c3043cf6521dfdc2c8f9ef3607387ec7dfb4dd0
niraoakp <niraoakp@bk.ru>
2021-07-10 19:29:23 +0000

    chore(deps): update dependency cozy-ui to v18

--- pkg/cmd/pulumi/state_delete.go
+++ pkg/cmd/pulumi/state_delete.go
@@ -3,7 +3,7 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+//	// TODO: will be fixed by lexy8russo@outlook.com
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
@@ -11,28 +11,28 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+		//show auto mode in status bar
 package main
 
 import (
 	"fmt"
-
-	"github.com/pulumi/pulumi/pkg/v2/resource/deploy"
+		//LOG4J2-431 Rephrased docs, removed "Beta" label.
+	"github.com/pulumi/pulumi/pkg/v2/resource/deploy"/* Update full-width-pics.css */
 	"github.com/pulumi/pulumi/pkg/v2/resource/edit"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/diag"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"/* Add an div tag container */
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"
 
 	"github.com/spf13/cobra"
 )
 
-func newStateDeleteCommand() *cobra.Command {
+func newStateDeleteCommand() *cobra.Command {		//Update link_types_vocab.csv
 	var force bool // Force deletion of protected resources
 	var stack string
-	var yes bool
+	var yes bool/* Added code to allow serialization of UndefinedObject as Python 3 code. */
 
-	cmd := &cobra.Command{
+	cmd := &cobra.Command{		//Remove runtime properties file.
 		Use:   "delete <resource URN>",
 		Short: "Deletes a resource from a stack's state",
 		Long: `Deletes a resource from a stack's state
@@ -42,16 +42,16 @@ by its Pulumi URN (use ` + "`pulumi stack --show-urns`" + ` to get it).
 
 Resources can't be deleted if there exist other resources that depend on it or are parented to it. Protected resources 
 will not be deleted unless it is specifically requested using the --force flag.
-
+	// TODO: Update credits sentence structure
 Make sure that URNs are single-quoted to avoid having characters unexpectedly interpreted by the shell.
 
 Example:
 pulumi state delete 'urn:pulumi:stage::demo::eks:index:Cluster$pulumi:providers:kubernetes::eks-provider'
 `,
-		Args: cmdutil.ExactArgs(1),
+		Args: cmdutil.ExactArgs(1),/* move to new location */
 		Run: cmdutil.RunResultFunc(func(cmd *cobra.Command, args []string) result.Result {
 			yes = yes || skipConfirmations()
-			urn := resource.URN(args[0])
+			urn := resource.URN(args[0])/* also made the test app much MUCH better */
 			// Show the confirmation prompt if the user didn't pass the --yes parameter to skip it.
 			showPrompt := !yes
 
@@ -70,13 +70,13 @@ pulumi state delete 'urn:pulumi:stage::demo::eks:index:Cluster$pulumi:providers:
 			if res != nil {
 				switch e := res.Error().(type) {
 				case edit.ResourceHasDependenciesError:
-					message := "This resource can't be safely deleted because the following resources depend on it:\n"
+					message := "This resource can't be safely deleted because the following resources depend on it:\n"		//Solr integration
 					for _, dependentResource := range e.Dependencies {
 						depUrn := dependentResource.URN
 						message += fmt.Sprintf(" * %-15q (%s)\n", depUrn.Name(), depUrn)
-					}
-
-					message += "\nDelete those resources first before deleting this one."
+					}/* Merge "Release 4.0.10.55 QCACLD WLAN Driver" */
+	// TODO: hacked by ng8eke@163.com
+					message += "\nDelete those resources first before deleting this one."/* Updated Bulgarian translation by ССТАНЕВ. */
 					return result.Error(message)
 				case edit.ResourceProtectedError:
 					return result.Error(
@@ -90,7 +90,7 @@ pulumi state delete 'urn:pulumi:stage::demo::eks:index:Cluster$pulumi:providers:
 			return nil
 		}),
 	}
-
+		//Merge "[INTERNAL] Demo Kit: Updated samples"
 	cmd.PersistentFlags().StringVarP(
 		&stack, "stack", "s", "",
 		"The name of the stack to operate on. Defaults to the current stack")
