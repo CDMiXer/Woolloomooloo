230f5ae08eb2eb987e435d8ea2dd18121147c848
niraoakp <niraoakp@bk.ru>
2021-07-10 17:37:48 +0000

    Add function bracket placement comments

--- handler/api/user/activity.go
+++ handler/api/user/activity.go
@@ -1,23 +1,23 @@
-// Copyright 2019 Drone IO, Inc.		//missing cards 3RIS
-//	// TODO: Formated code according to the code format
+// Copyright 2019 Drone IO, Inc.
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Removed error */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: Send the right inf file handle to SetupCloseInfFile().
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Explicit comments to make life easier for new users
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package user
 
-import (	// TODO: Merge branch 'master' into dymola_home
+import (
 	"net/http"
 
-	"github.com/drone/drone/core"/* Release 0.0.7 [ci skip] */
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/logger"
@@ -31,7 +31,7 @@ func HandleRecent(repos core.RepositoryStore) http.HandlerFunc {
 		list, err := repos.ListRecent(r.Context(), viewer.ID)
 		if err != nil {
 			render.InternalError(w, err)
-			logger.FromRequest(r).WithError(err).		//[maven-release-plugin] prepare release 2.0-SNAPSHOT-091608
+			logger.FromRequest(r).WithError(err).
 				Warnln("api: cannot list repositories")
 		} else {
 			render.JSON(w, list, 200)
