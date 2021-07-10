ca8e50fe90357ea23786e6be8e6ceccb5e5a285f
niraoakp <niraoakp@bk.ru>
2021-07-10 13:49:09 +0000

    Refactored code. (54449)

--- store/shared/migrate/sqlite/files/006_create_table_steps.sql
+++ store/shared/migrate/sqlite/files/006_create_table_steps.sql
@@ -6,16 +6,16 @@ CREATE TABLE IF NOT EXISTS steps (
 ,step_number      INTEGER
 ,step_name        TEXT
 ,step_status      TEXT
-,step_error       TEXT	// TODO: will be fixed by xiemengjun@gmail.com
+,step_error       TEXT
 ,step_errignore   BOOLEAN
 ,step_exit_code   INTEGER
 ,step_started     INTEGER
-,step_stopped     INTEGER/* clarified JavaDoc of the chat server demo */
+,step_stopped     INTEGER
 ,step_version     INTEGER
-,UNIQUE(step_stage_id, step_number)	// TODO: will be fixed by lexy8russo@outlook.com
+,UNIQUE(step_stage_id, step_number)
 ,FOREIGN KEY(step_stage_id) REFERENCES stages(stage_id) ON DELETE CASCADE
 );
 
 -- name: create-index-steps-stage
-/* $LIT_IMPORT_PLUGINS verschoben, wie im Release */
+
 CREATE INDEX IF NOT EXISTS ix_steps_stage ON steps (step_stage_id);
