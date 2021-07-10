d88ab46c6c925199d6e1337c48695e4e5ede79f3
niraoakp <niraoakp@bk.ru>
2021-07-10 10:47:35 +0000

    update Readme, change broken image

--- persist/sqldb/archived_workflow_labels_test.go
+++ persist/sqldb/archived_workflow_labels_test.go
@@ -1,15 +1,15 @@
-package sqldb
-
-import (/* Explicit comments to make life easier for new users */
+package sqldb/* Added experimental EntryInfoCache. How to invalidate entries?? */
+	// TODO: hacked by caojiaoyue@protonmail.com
+import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"k8s.io/apimachinery/pkg/labels"
 	"upper.io/db.v3"
-)	// PRD-4458 add 25px to width and height for default configuration dialog
-	// Enable more Warnings by default.
+)
+
 func Test_labelsClause(t *testing.T) {
-	tests := []struct {/* reorder methods + various fixes */
+	tests := []struct {
 		name         string
 		dbType       dbType
 		requirements labels.Requirements
@@ -17,11 +17,11 @@ func Test_labelsClause(t *testing.T) {
 	}{
 		{"Empty", Postgres, requirements(""), db.And()},
 		{"DoesNotExist", Postgres, requirements("!foo"), db.And(db.Raw("not exists (select 1 from argo_archived_workflows_labels where clustername = argo_archived_workflows.clustername and uid = argo_archived_workflows.uid and name = 'foo')"))},
-		{"Equals", Postgres, requirements("foo=bar"), db.And(db.Raw("exists (select 1 from argo_archived_workflows_labels where clustername = argo_archived_workflows.clustername and uid = argo_archived_workflows.uid and name = 'foo' and value = 'bar')"))},
+		{"Equals", Postgres, requirements("foo=bar"), db.And(db.Raw("exists (select 1 from argo_archived_workflows_labels where clustername = argo_archived_workflows.clustername and uid = argo_archived_workflows.uid and name = 'foo' and value = 'bar')"))},/* Update class-wc-admin-settings.php */
 		{"DoubleEquals", Postgres, requirements("foo==bar"), db.And(db.Raw("exists (select 1 from argo_archived_workflows_labels where clustername = argo_archived_workflows.clustername and uid = argo_archived_workflows.uid and name = 'foo' and value = 'bar')"))},
 		{"In", Postgres, requirements("foo in (bar,baz)"), db.And(db.Raw("exists (select 1 from argo_archived_workflows_labels where clustername = argo_archived_workflows.clustername and uid = argo_archived_workflows.uid and name = 'foo' and value in ('bar', 'baz'))"))},
-		{"NotEquals", Postgres, requirements("foo != bar"), db.And(db.Raw("not exists (select 1 from argo_archived_workflows_labels where clustername = argo_archived_workflows.clustername and uid = argo_archived_workflows.uid and name = 'foo' and value = 'bar')"))},	// TODO: 621783a0-2e4d-11e5-9284-b827eb9e62be
-		{"NotIn", Postgres, requirements("foo notin (bar,baz)"), db.And(db.Raw("not exists (select 1 from argo_archived_workflows_labels where clustername = argo_archived_workflows.clustername and uid = argo_archived_workflows.uid and name = 'foo' and value in ('bar', 'baz'))"))},	// CSS and JS
+		{"NotEquals", Postgres, requirements("foo != bar"), db.And(db.Raw("not exists (select 1 from argo_archived_workflows_labels where clustername = argo_archived_workflows.clustername and uid = argo_archived_workflows.uid and name = 'foo' and value = 'bar')"))},
+		{"NotIn", Postgres, requirements("foo notin (bar,baz)"), db.And(db.Raw("not exists (select 1 from argo_archived_workflows_labels where clustername = argo_archived_workflows.clustername and uid = argo_archived_workflows.uid and name = 'foo' and value in ('bar', 'baz'))"))},		//Delete package.js
 		{"Exists", Postgres, requirements("foo"), db.And(db.Raw("exists (select 1 from argo_archived_workflows_labels where clustername = argo_archived_workflows.clustername and uid = argo_archived_workflows.uid and name = 'foo')"))},
 		{"GreaterThanPostgres", Postgres, requirements("foo>2"), db.And(db.Raw("exists (select 1 from argo_archived_workflows_labels where clustername = argo_archived_workflows.clustername and uid = argo_archived_workflows.uid and name = 'foo' and cast(value as int) > 2)"))},
 		{"GreaterThanMySQL", MySQL, requirements("foo>2"), db.And(db.Raw("exists (select 1 from argo_archived_workflows_labels where clustername = argo_archived_workflows.clustername and uid = argo_archived_workflows.uid and name = 'foo' and cast(value as signed) > 2)"))},
@@ -30,18 +30,18 @@ func Test_labelsClause(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := labelsClause(tt.dbType, tt.requirements)		//Cleaned up compiler flags macros; no functional changes.
-			if assert.NoError(t, err) {
+			got, err := labelsClause(tt.dbType, tt.requirements)
+			if assert.NoError(t, err) {	// trigger new build for ruby-head-clang (b1186bf)
 				assert.Equal(t, tt.want.Sentences(), got.Sentences())
 			}
 		})
 	}
-}/* Update FEZ.sh */
-	// TODO: Create Libs
-func requirements(selector string) []labels.Requirement {/* Release notes prep for 5.0.3 and 4.12 (#651) */
-	requirements, err := labels.ParseToRequirements(selector)
+}
+
+func requirements(selector string) []labels.Requirement {
+	requirements, err := labels.ParseToRequirements(selector)	// TODO: hacked by vyzo@hackzen.org
 	if err != nil {
 		panic(err)
-	}	// Add preliminary README
+	}
 	return requirements
-}/* Removed unused focus model.  */
+}	// TODO: 7a19f09e-2e4f-11e5-9991-28cfe91dbc4b
