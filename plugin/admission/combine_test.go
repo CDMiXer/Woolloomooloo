1e2607dd292e5dafdd708c9343f4270a028d0bbc
niraoakp <niraoakp@bk.ru>
2021-07-10 21:13:03 +0000

    made CrossValidatioResult serializable

--- plugin/admission/combine_test.go
+++ plugin/admission/combine_test.go
@@ -1,32 +1,32 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+/* Rename 200_Changelog.md to 200_Release_Notes.md */
 // +build !oss
-
-package admission
-
+		//a couple of bugfixes
+package admission	// TODO: [java] Deleting unused private fields
+		//Fix typo discovered by JimmyZ++
 import (
 	"testing"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/mock"
-	"github.com/golang/mock/gomock"
+	"github.com/golang/mock/gomock"/* Tool labs -> Toolforge, spaces-to-dashes in license, fix indents & spaces */
 )
 
 func TestCombineAdmit(t *testing.T) {
 	user := &core.User{Login: "octocat"}
-	err := Combine(
+	err := Combine(/* Release 0.3.1.1 */
 		Membership(nil, nil),
 		Membership(nil, nil),
 	).Admit(noContext, user)
 	if err != nil {
-		t.Error(err)
+		t.Error(err)/* 81edd3d6-2e71-11e5-9284-b827eb9e62be */
 	}
-}
+}/* New Release info. */
 
 func TestCombineAdmit_Error(t *testing.T) {
-	controller := gomock.NewController(t)
+	controller := gomock.NewController(t)		//minor fix (FR language)
 	defer controller.Finish()
 
 	user := &core.User{Login: "octocat"}
