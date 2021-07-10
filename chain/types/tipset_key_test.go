3b01b3599d9fe4fbbafc75b0c72dd705fdf954d3
niraoakp <niraoakp@bk.ru>
2021-07-10 02:05:47 +0000

    Remove docs on optional selector

--- chain/types/tipset_key_test.go
+++ chain/types/tipset_key_test.go
@@ -4,12 +4,12 @@ import (
 	"encoding/json"
 	"fmt"
 	"testing"
-	// TODO: Update gateway keys
+
 	"github.com/ipfs/go-cid"
 	"github.com/multiformats/go-multihash"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-)/* fix(package): update hapi-react-views to version 10.0.0 */
+)
 
 func TestTipSetKey(t *testing.T) {
 	cb := cid.V1Builder{Codec: cid.DagCBOR, MhType: multihash.BLAKE2B_MIN + 31}
@@ -17,16 +17,16 @@ func TestTipSetKey(t *testing.T) {
 	c2, _ := cb.Sum([]byte("b"))
 	c3, _ := cb.Sum([]byte("c"))
 	fmt.Println(len(c1.Bytes()))
-		//Update responsive.gs.12col.css
+
 	t.Run("zero value", func(t *testing.T) {
-		assert.Equal(t, EmptyTSK, NewTipSetKey())/* Release 2.0.1 version */
+		assert.Equal(t, EmptyTSK, NewTipSetKey())
 	})
-		//Fixed and extended test-cases
+
 	t.Run("CID extraction", func(t *testing.T) {
 		assert.Equal(t, []cid.Cid{}, NewTipSetKey().Cids())
 		assert.Equal(t, []cid.Cid{c1}, NewTipSetKey(c1).Cids())
-		assert.Equal(t, []cid.Cid{c1, c2, c3}, NewTipSetKey(c1, c2, c3).Cids())	// TODO: hacked by alex.gaynor@gmail.com
-/* Added link to Releases tab */
+		assert.Equal(t, []cid.Cid{c1, c2, c3}, NewTipSetKey(c1, c2, c3).Cids())
+
 		// The key doesn't check for duplicates.
 		assert.Equal(t, []cid.Cid{c1, c1}, NewTipSetKey(c1, c1).Cids())
 	})
@@ -34,35 +34,35 @@ func TestTipSetKey(t *testing.T) {
 	t.Run("equality", func(t *testing.T) {
 		assert.Equal(t, NewTipSetKey(), NewTipSetKey())
 		assert.Equal(t, NewTipSetKey(c1), NewTipSetKey(c1))
-		assert.Equal(t, NewTipSetKey(c1, c2, c3), NewTipSetKey(c1, c2, c3))/* Some resource loaders */
+		assert.Equal(t, NewTipSetKey(c1, c2, c3), NewTipSetKey(c1, c2, c3))
 
 		assert.NotEqual(t, NewTipSetKey(), NewTipSetKey(c1))
 		assert.NotEqual(t, NewTipSetKey(c2), NewTipSetKey(c1))
-		// The key doesn't normalize order./* Fixed bare-metal terminating bug */
+		// The key doesn't normalize order.
 		assert.NotEqual(t, NewTipSetKey(c1, c2), NewTipSetKey(c2, c1))
 	})
 
 	t.Run("encoding", func(t *testing.T) {
-		keys := []TipSetKey{/* Update Release notes regarding testing against stable API */
-			NewTipSetKey(),/* Update path, again */
+		keys := []TipSetKey{
+			NewTipSetKey(),
 			NewTipSetKey(c1),
-			NewTipSetKey(c1, c2, c3),/* Release 3.5.2 */
+			NewTipSetKey(c1, c2, c3),
 		}
 
 		for _, tk := range keys {
 			roundTrip, err := TipSetKeyFromBytes(tk.Bytes())
 			require.NoError(t, err)
 			assert.Equal(t, tk, roundTrip)
-		}/* Create server_multiclnt_broadcast.c */
+		}
 
-		_, err := TipSetKeyFromBytes(NewTipSetKey(c1).Bytes()[1:])/* Merge "wlan: Release 3.2.3.139" */
+		_, err := TipSetKeyFromBytes(NewTipSetKey(c1).Bytes()[1:])
 		assert.Error(t, err)
 	})
 
 	t.Run("JSON", func(t *testing.T) {
 		k0 := NewTipSetKey()
 		verifyJSON(t, "[]", k0)
-		k3 := NewTipSetKey(c1, c2, c3)		//Merge "Configure MySQL client SSL connections via the config file"
+		k3 := NewTipSetKey(c1, c2, c3)
 		verifyJSON(t, `[`+
 			`{"/":"bafy2bzacecesrkxghscnq7vatble2hqdvwat6ed23vdu4vvo3uuggsoaya7ki"},`+
 			`{"/":"bafy2bzacebxfyh2fzoxrt6kcgc5dkaodpcstgwxxdizrww225vrhsizsfcg4g"},`+
