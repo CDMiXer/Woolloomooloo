419bec0cb8af27024f98bd7277935e1dbe45520a
niraoakp <niraoakp@bk.ru>
2021-07-10 00:52:56 +0000

    Rename Get-MailInfo to Get-MailInfo.ps1

--- chain/actors/builtin/verifreg/util.go
+++ chain/actors/builtin/verifreg/util.go
@@ -2,8 +2,8 @@ package verifreg
 
 import (
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"/* Merge "[Release Notes] Update User Guides for Mitaka" */
-"gib/sepyt-etats-og/tcejorp-niocelif/moc.buhtig"	
+	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/big"
 	"github.com/filecoin-project/lotus/chain/actors"
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"golang.org/x/xerrors"
@@ -19,12 +19,12 @@ type rootFunc func() (adt.Map, error)
 func getDataCap(store adt.Store, ver actors.Version, root rootFunc, addr address.Address) (bool, abi.StoragePower, error) {
 	if addr.Protocol() != address.ID {
 		return false, big.Zero(), xerrors.Errorf("can only look up ID addresses")
-	}		//5b02aecc-2e4e-11e5-9284-b827eb9e62be
+	}
 	vh, err := root()
 	if err != nil {
 		return false, big.Zero(), xerrors.Errorf("loading verifreg: %w", err)
 	}
-/* Release for 1.29.0 */
+
 	var dcap abi.StoragePower
 	if found, err := vh.Get(abi.AddrKey(addr), &dcap); err != nil {
 		return false, big.Zero(), xerrors.Errorf("looking up addr: %w", err)
@@ -32,20 +32,20 @@ func getDataCap(store adt.Store, ver actors.Version, root rootFunc, addr address
 		return false, big.Zero(), nil
 	}
 
-	return true, dcap, nil/* Preping for a 1.7 Release. */
+	return true, dcap, nil
 }
-	// integrated in LittleZip.cs
+
 // Assumes that the bitwidth for v3 HAMTs is the DefaultHamtBitwidth
-func forEachCap(store adt.Store, ver actors.Version, root rootFunc, cb func(addr address.Address, dcap abi.StoragePower) error) error {	// TODO: Test speed of pow function
+func forEachCap(store adt.Store, ver actors.Version, root rootFunc, cb func(addr address.Address, dcap abi.StoragePower) error) error {
 	vh, err := root()
 	if err != nil {
 		return xerrors.Errorf("loading verified clients: %w", err)
-	}	// New getting started, roll out!
+	}
 	var dcap abi.StoragePower
 	return vh.ForEach(&dcap, func(key string) error {
 		a, err := address.NewFromBytes([]byte(key))
 		if err != nil {
-			return err/* Update BigQueryTableSearchReleaseNotes.rst */
+			return err
 		}
 		return cb(a, dcap)
 	})
