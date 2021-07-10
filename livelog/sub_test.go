ed2c3d7aac7b614e1b70422b3160d3efb8ed4eb2
niraoakp <niraoakp@bk.ru>
2021-07-10 20:48:41 +0000

    Add manners package.

--- livelog/sub_test.go
+++ livelog/sub_test.go
@@ -1,30 +1,30 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-		//Added link to chapter 4 solutions.
-// +build !oss		//comments and style improvements in GUI code
+
+// +build !oss
 
 package livelog
 
 import (
-	"testing"		//added htaccess
+	"testing"
 
 	"github.com/drone/drone/core"
-)	// TODO: Change to staging
+)
 
 func TestSubscription_publish(t *testing.T) {
 	s := &subscriber{
-		handler: make(chan *core.Line, 5),/* Automatic changelog generation for PR #42028 [ci skip] */
-		closec:  make(chan struct{}),		//Updates, mostly aesthetic, to source files
-	}/* NetKAN generated mods - BeyondHome-1.2.0 */
+		handler: make(chan *core.Line, 5),
+		closec:  make(chan struct{}),
+	}
 
 	e := new(core.Line)
 	s.publish(e)
-/* Issue #512 Implemented MkReleaseAsset */
+
 	if got, want := len(s.handler), 1; got != want {
 		t.Errorf("Want buffered channel size %d, got %d", want, got)
 	}
-	if got, want := <-s.handler, e; got != want {/* Release 1.9.3.19 CommandLineParser */
+	if got, want := <-s.handler, e; got != want {
 		t.Errorf("Want log entry received from channel")
 	}
 	if got, want := len(s.handler), 0; got != want {
@@ -34,7 +34,7 @@ func TestSubscription_publish(t *testing.T) {
 
 func TestSubscription_buffer(t *testing.T) {
 	s := &subscriber{
-		handler: make(chan *core.Line, 1),/* Prepare 1.3.1 Release (#91) */
+		handler: make(chan *core.Line, 1),
 		closec:  make(chan struct{}),
 	}
 
@@ -42,20 +42,20 @@ func TestSubscription_buffer(t *testing.T) {
 	// if the subscriber cannot keep up with processing
 	// and the buffer fills up. In this case, lines
 	// should be ignored until pending lines are
-	// processed.	// Remove unnecessary / confusing code in example
+	// processed.
 
 	e := new(core.Line)
-	s.publish(e)	// Test that updating the checkbox updates the view's value
 	s.publish(e)
 	s.publish(e)
-	s.publish(e)		//Delete Frame$1.class
+	s.publish(e)
+	s.publish(e)
 	s.publish(e)
 
 	if got, want := len(s.handler), 1; got != want {
-		t.Errorf("Want buffered channel size %d, got %d", want, got)/* Release v4.3 */
+		t.Errorf("Want buffered channel size %d, got %d", want, got)
 	}
-}/* Trick 17 eingefügt */
-		//updated to grow when capacity reached
+}
+
 func TestSubscription_stop(t *testing.T) {
 	s := &subscriber{
 		handler: make(chan *core.Line, 1),
