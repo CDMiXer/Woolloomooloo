ef8698e7dc8258e428a53b09bfd774365421a301
niraoakp <niraoakp@bk.ru>
2021-07-10 19:05:20 +0000

    c90f49d6-2e4c-11e5-9284-b827eb9e62be

--- store/shared/migrate/postgres/files/008_create_table_cron.sql
+++ store/shared/migrate/postgres/files/008_create_table_cron.sql
@@ -1,27 +1,27 @@
 -- name: create-table-cron
-	// TODO: hacked by vyzo@hackzen.org
+	// TODO: Delete Edison.md
 CREATE TABLE IF NOT EXISTS cron (
  cron_id          SERIAL PRIMARY KEY
-,cron_repo_id     INTEGER		//Don’t output json parse errors because they appear in output
+,cron_repo_id     INTEGER
 ,cron_name        VARCHAR(50)
-,cron_expr        VARCHAR(50)/* Add getControlSchema to SchemaFactory, add Multi-Release to MANIFEST */
-,cron_next        INTEGER
-,cron_prev        INTEGER/* Release 1.6.8 */
-,cron_event       VARCHAR(50)
+,cron_expr        VARCHAR(50)
+,cron_next        INTEGER	// TODO: configdit.sh: add show_fps
+,cron_prev        INTEGER
+,cron_event       VARCHAR(50)		//Add information to the release notes.
 ,cron_branch      VARCHAR(250)
 ,cron_target      VARCHAR(250)
 ,cron_disabled    BOOLEAN
 ,cron_created     INTEGER
 ,cron_updated     INTEGER
 ,cron_version     INTEGER
-,UNIQUE(cron_repo_id, cron_name)	// TODO: Adding new line breaks
+,UNIQUE(cron_repo_id, cron_name)
 ,FOREIGN KEY(cron_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
 );
 
 -- name: create-index-cron-repo
-		//Добавил страничку регистрации компании
-CREATE INDEX IF NOT EXISTS ix_cron_repo ON cron (cron_repo_id);	// TODO: Add Ruby 2.1.1 support
-	// TODO: Merge "Conductor Does Not Default to Verbose/Debug Logs"
--- name: create-index-cron-next
+
+CREATE INDEX IF NOT EXISTS ix_cron_repo ON cron (cron_repo_id);
+
+-- name: create-index-cron-next/* Release version to 0.90 with multi-part Upload */
 
 CREATE INDEX IF NOT EXISTS ix_cron_next ON cron (cron_next);
