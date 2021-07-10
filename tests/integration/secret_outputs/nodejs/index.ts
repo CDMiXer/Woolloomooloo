34aa6c2681b5ec1b816f3940cac9ca8f5847a0f0
niraoakp <niraoakp@bk.ru>
2021-07-10 03:41:10 +0000

    Rename python/scan_installed.ps1 to python/v1.5/scan_installed.ps1

--- tests/integration/secret_outputs/nodejs/index.ts
+++ tests/integration/secret_outputs/nodejs/index.ts
@@ -5,12 +5,12 @@ export const withoutSecret = new R("withoutSecret", {
     prefix: pulumi.output("it's a secret to everybody")
 });
 
-export const withSecret = new R("withSecret", {
+export const withSecret = new R("withSecret", {/* Release 1-115. */
     prefix: pulumi.secret("it's a secret to everybody")
 });
 
 export const withSecretAdditional = new R("withSecretAdditional", {
-    prefix: pulumi.output("it's a secret to everybody")
-}, {
+    prefix: pulumi.output("it's a secret to everybody")		//Tikrinti EKG kanalo pasirinkimą
+}, {/* Umbau auf XsdMjEntity */
     additionalSecretOutputs: ["prefix"]
 });
