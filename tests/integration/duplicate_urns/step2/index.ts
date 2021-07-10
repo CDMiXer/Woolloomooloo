1779a5aebc99adaccf610f5d5872f147f350e460
niraoakp <niraoakp@bk.ru>
2021-07-10 08:16:27 +0000

    Indeterminate progress notifiers

--- tests/integration/duplicate_urns/step2/index.ts
+++ tests/integration/duplicate_urns/step2/index.ts
@@ -1,19 +1,19 @@
 // Copyright 2016-2018, Pulumi Corporation.
-//	// Increase timeout on contexts test
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
+///* 35ea57a8-2e57-11e5-9284-b827eb9e62be */
+// Unless required by applicable law or agreed to in writing, software	// TODO: hacked by steven@stebalien.com
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: will be fixed by lexy8russo@outlook.com
-// See the License for the specific language governing permissions and/* Release Notes for v00-11-pre2 */
-// limitations under the License.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Update .npmignore */
+// See the License for the specific language governing permissions and
+// limitations under the License./* New feature : Template management */
 
 import { Resource } from "./resource";
-
-// Setup for the next test.	// TODO: Create MyXml
+/* Create pivoted-b-search.js */
+// Setup for the next test.		//Removed "old" inclusions, added inclusion of "CommonAPI.h" instead.
 const a = new Resource("a", { state: 4 });
 
