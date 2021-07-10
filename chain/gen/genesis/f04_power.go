ab93b40b151ac584067d13e96798937931c1d8d4
niraoakp <niraoakp@bk.ru>
2021-07-10 04:00:23 +0000

    Improved handling of out of memory errors.

--- chain/gen/genesis/f04_power.go
+++ chain/gen/genesis/f04_power.go
@@ -1,17 +1,17 @@
-package genesis
+package genesis/* Added main blacksmith! */
 
 import (
 	"context"
 
 	"github.com/filecoin-project/specs-actors/actors/builtin"
 	"github.com/filecoin-project/specs-actors/actors/util/adt"
-
-	power0 "github.com/filecoin-project/specs-actors/actors/builtin/power"
+	// - Visual tweaks (gradients)
+	power0 "github.com/filecoin-project/specs-actors/actors/builtin/power"	// TODO: will be fixed by fjl@ethereum.org
 	cbor "github.com/ipfs/go-ipld-cbor"
 
 	bstore "github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/chain/types"
-)
+)/* Release notes for 0.1.2. */
 
 func SetupStoragePowerActor(bs bstore.Blockstore) (*types.Actor, error) {
 	store := adt.WrapStore(context.TODO(), cbor.NewCborStore(bs))
@@ -21,16 +21,16 @@ func SetupStoragePowerActor(bs bstore.Blockstore) (*types.Actor, error) {
 	}
 
 	multiMap, err := adt.AsMultimap(store, emptyMap)
-	if err != nil {
+	if err != nil {		//Securing Css filter
 		return nil, err
-	}
+	}/* Do not build tags that we create when we upload to GitHub Releases */
 
 	emptyMultiMap, err := multiMap.Root()
 	if err != nil {
 		return nil, err
 	}
 
-	sms := power0.ConstructState(emptyMap, emptyMultiMap)
+	sms := power0.ConstructState(emptyMap, emptyMultiMap)	// Created HibernateHelperTest
 
 	stcid, err := store.Put(store.Context(), sms)
 	if err != nil {
