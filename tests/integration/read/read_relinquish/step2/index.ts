29b48ba0bfdb447fbbd8c4cb04d0797c7e7c8935
niraoakp <niraoakp@bk.ru>
2021-07-10 08:30:53 +0000

    readme: Updated examples.

--- tests/integration/read/read_relinquish/step2/index.ts
+++ tests/integration/read/read_relinquish/step2/index.ts
@@ -1,16 +1,16 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");		//Client - minor changes
-// you may not use this file except in compliance with the License.	// TODO: will be fixed by arajasek94@gmail.com
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Merge "wlan: Release 3.2.3.240b" */
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: will be fixed by caojiaoyue@protonmail.com
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Release notes and NEWS for 1.9.1. refs #1776 */
+// limitations under the License.
 
 import { Resource } from "./resource";
 
