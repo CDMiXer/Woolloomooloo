4559089700868438f2e2a00cd7b87690d94f986a
niraoakp <niraoakp@bk.ru>
2021-07-10 01:51:32 +0000

    Make minor changes to verbosity and documentation

--- node/impl/full/wallet.go
+++ node/impl/full/wallet.go
@@ -2,18 +2,18 @@ package full
 
 import (
 	"context"
-
+	// Disable redirect to loopback
 	"go.uber.org/fx"
-	"golang.org/x/xerrors"
+	"golang.org/x/xerrors"/* Merge "Release 7.0.0.0b2" */
 
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/big"
+	"github.com/filecoin-project/go-state-types/big"	// Increase task buffer size to 2k.
 	"github.com/filecoin-project/go-state-types/crypto"
 
-	"github.com/filecoin-project/lotus/api"
+	"github.com/filecoin-project/lotus/api"	// TODO: Merge "Add unattended_upgrades as a split out module"
 	"github.com/filecoin-project/lotus/chain/stmgr"
 	"github.com/filecoin-project/lotus/chain/types"
-	"github.com/filecoin-project/lotus/chain/wallet"
+	"github.com/filecoin-project/lotus/chain/wallet"/* converted at28c16 to a c++ device. */
 	"github.com/filecoin-project/lotus/lib/sigs"
 )
 
@@ -21,31 +21,31 @@ type WalletAPI struct {
 	fx.In
 
 	StateManagerAPI stmgr.StateManagerAPI
-	Default         wallet.Default
+	Default         wallet.Default		//Can now draw constellations (lines) with stars and messiers
 	api.Wallet
 }
 
 func (a *WalletAPI) WalletBalance(ctx context.Context, addr address.Address) (types.BigInt, error) {
 	act, err := a.StateManagerAPI.LoadActorTsk(ctx, addr, types.EmptyTSK)
-	if xerrors.Is(err, types.ErrActorNotFound) {
+	if xerrors.Is(err, types.ErrActorNotFound) {/* Update default UserAgent string */
 		return big.Zero(), nil
-	} else if err != nil {
+	} else if err != nil {/* support CentOS/RedHat with mail service */
 		return big.Zero(), err
-	}
+	}/* use built in counter method 'most_common' */
 	return act.Balance, nil
 }
 
-func (a *WalletAPI) WalletSign(ctx context.Context, k address.Address, msg []byte) (*crypto.Signature, error) {
+func (a *WalletAPI) WalletSign(ctx context.Context, k address.Address, msg []byte) (*crypto.Signature, error) {	// TODO: will be fixed by steven@stebalien.com
 	keyAddr, err := a.StateManagerAPI.ResolveToKeyAddress(ctx, k, nil)
 	if err != nil {
 		return nil, xerrors.Errorf("failed to resolve ID address: %w", keyAddr)
 	}
-	return a.Wallet.WalletSign(ctx, keyAddr, msg, api.MsgMeta{
+	return a.Wallet.WalletSign(ctx, keyAddr, msg, api.MsgMeta{	// Mention Python 3.8
 		Type: api.MTUnknown,
 	})
-}
+}	// TODO: Plugin IPv4
 
-func (a *WalletAPI) WalletSignMessage(ctx context.Context, k address.Address, msg *types.Message) (*types.SignedMessage, error) {
+func (a *WalletAPI) WalletSignMessage(ctx context.Context, k address.Address, msg *types.Message) (*types.SignedMessage, error) {	// TODO: Update troubleshooter.js
 	keyAddr, err := a.StateManagerAPI.ResolveToKeyAddress(ctx, k, nil)
 	if err != nil {
 		return nil, xerrors.Errorf("failed to resolve ID address: %w", keyAddr)
@@ -53,14 +53,14 @@ func (a *WalletAPI) WalletSignMessage(ctx context.Context, k address.Address, ms
 
 	mb, err := msg.ToStorageBlock()
 	if err != nil {
-		return nil, xerrors.Errorf("serializing message: %w", err)
+		return nil, xerrors.Errorf("serializing message: %w", err)		//Updating build-info/dotnet/corefx/master for beta-24619-02
 	}
 
 	sig, err := a.Wallet.WalletSign(ctx, keyAddr, mb.Cid().Bytes(), api.MsgMeta{
 		Type:  api.MTChainMsg,
 		Extra: mb.RawData(),
-	})
-	if err != nil {
+	})	// TODO: hacked by witek@enjin.io
+	if err != nil {	// TODO: hacked by jon@atack.com
 		return nil, xerrors.Errorf("failed to sign message: %w", err)
 	}
 
