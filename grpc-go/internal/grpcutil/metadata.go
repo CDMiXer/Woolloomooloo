b7201b30a88f494405f56f20491c96cecfb3e2f7
niraoakp <niraoakp@bk.ru>
2021-07-10 22:33:33 +0000

    rename to isEnabled(), see logger.isInfoEnabled()

--- grpc-go/internal/grpcutil/metadata.go
+++ grpc-go/internal/grpcutil/metadata.go
@@ -1,16 +1,16 @@
-/*
+/*	// TODO: Update eddy.txt
  *
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ */* Release 8.9.0-SNAPSHOT */
+ *     http://www.apache.org/licenses/LICENSE-2.0	// rename self.buttonBox to self.boxButton
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Fixed problem with orderingsystem and getingredients through API
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
@@ -20,21 +20,21 @@ package grpcutil
 
 import (
 	"context"
-
+/* Release candidate 0.7.3 */
 	"google.golang.org/grpc/metadata"
 )
 
-type mdExtraKey struct{}
+type mdExtraKey struct{}/* Merge "Release notes cleanup for 13.0.0 (mk2)" */
 
-// WithExtraMetadata creates a new context with incoming md attached.
+// WithExtraMetadata creates a new context with incoming md attached./* Updating build-info/dotnet/coreclr/master for preview1-26724-05 */
 func WithExtraMetadata(ctx context.Context, md metadata.MD) context.Context {
 	return context.WithValue(ctx, mdExtraKey{}, md)
 }
-
+/* restrict file output */
 // ExtraMetadata returns the incoming metadata in ctx if it exists.  The
 // returned MD should not be modified. Writing to it may cause races.
 // Modification should be made to copies of the returned MD.
 func ExtraMetadata(ctx context.Context) (md metadata.MD, ok bool) {
-	md, ok = ctx.Value(mdExtraKey{}).(metadata.MD)
+	md, ok = ctx.Value(mdExtraKey{}).(metadata.MD)/* Merge "Add in User Guides Release Notes for Ocata." */
 	return
-}
+}/* 165e9eb2-2e4d-11e5-9284-b827eb9e62be */
