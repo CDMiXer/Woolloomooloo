b321ef90454e41fcfed499551bef6557a82c556b
niraoakp <niraoakp@bk.ru>
2021-07-10 20:05:05 +0000

    added lots of comments

--- plugin/config/global_oss.go
+++ plugin/config/global_oss.go
@@ -1,21 +1,21 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc.	// TODO: will be fixed by vyzo@hackzen.org
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0/* Merge "Release 3.0.10.017 Prima WLAN Driver" */
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software	// TODO: will be fixed by caojiaoyue@protonmail.com
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 // +build oss
-
-package config
-
+	// TODO: Disable invasive alert dialog popups
+package config/* 0.1.1 Release. */
+/* Initial support for building with CMake */
 import (
 	"context"
 	"time"
@@ -24,11 +24,11 @@ import (
 )
 
 // Global returns a no-op configuration service.
-func Global(string, string, bool, time.Duration) core.ConfigService {
+func Global(string, string, bool, time.Duration) core.ConfigService {		//Removed destroy
 	return new(noop)
 }
 
-type noop struct{}
+type noop struct{}		//fix reddit comment checking
 
 func (noop) Find(context.Context, *core.ConfigArgs) (*core.Config, error) {
 	return nil, nil
