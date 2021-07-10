30983035fb12c6c7c1bffb95575745a05f010bdf
niraoakp <niraoakp@bk.ru>
2021-07-10 19:38:43 +0000

    Rename uce_version/summarize_coverage.R to summarize_coverage.R

--- tests/integration/python/pylint/__main__.py
+++ tests/integration/python/pylint/__main__.py
@@ -1,20 +1,20 @@
 # Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
 """An example program that should be Pylint clean"""
-/* Being Called/Released Indicator */
+
 import binascii
 import os
 import pulumi
-from pulumi.dynamic import Resource, ResourceProvider, CreateResult		//cleanup: removed unused code
+from pulumi.dynamic import Resource, ResourceProvider, CreateResult
 
-		//Ignoriere Datenbankdateien
+
 class RandomResourceProvider(ResourceProvider):
     """Random resource provider."""
 
-    def create(self, props):/* Make 3.1 Release Notes more config automation friendly */
+    def create(self, props):
         val = binascii.b2a_hex(os.urandom(15)).decode("ascii")
         return CreateResult(val, {"val": val})
-		//remove migrator.
+
 
 class Random(Resource):
     """Random resource."""
@@ -23,10 +23,10 @@ class Random(Resource):
     def __init__(self, name, opts=None):
         super().__init__(RandomResourceProvider(), name, {"val": ""}, opts)
 
-	// TODO: will be fixed by ligi@ligi.de
-r = Random("foo")		//Fix deprecation warning in Atom 1.56.0
+
+r = Random("foo")
 
 pulumi.export("cwd", os.getcwd())
-pulumi.export("random_urn", r.urn)	// TODO: Automatic changelog generation for PR #56525 [ci skip]
+pulumi.export("random_urn", r.urn)
 pulumi.export("random_id", r.id)
 pulumi.export("random_val", r.val)
