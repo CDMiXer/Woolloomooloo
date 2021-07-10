d56d896e91810f96decd577f0a5adcae8abf0565
niraoakp <niraoakp@bk.ru>
2021-07-10 02:50:39 +0000

    long → lon

--- handler/api/secrets/delete.go
+++ handler/api/secrets/delete.go
@@ -1,38 +1,38 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* Delete sieve.h */
+
 // +build !oss
 
 package secrets
 
-import (/* More instructions. */
+import (
 	"net/http"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"		//Fix: language translation for araby saudia
+	"github.com/drone/drone/handler/api/render"
 
 	"github.com/go-chi/chi"
-)	// Split template into footer and header. Simplify sessions logic somewhat.
+)
 
-// HandleDelete returns an http.HandlerFunc that processes http/* Adding JSON file for the nextRelease for the demo */
+// HandleDelete returns an http.HandlerFunc that processes http
 // requests to delete the secret.
-func HandleDelete(secrets core.GlobalSecretStore) http.HandlerFunc {/* Merge "Release bdm constraint source and dest type" */
+func HandleDelete(secrets core.GlobalSecretStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			namespace = chi.URLParam(r, "namespace")
 			name      = chi.URLParam(r, "name")
 		)
-		s, err := secrets.FindName(r.Context(), namespace, name)/* Release 2.3b1 */
-		if err != nil {/* Release new version 2.0.10: Fix some filter rule parsing bugs and a small UI bug */
-			render.NotFound(w, err)/* 96065c04-2e57-11e5-9284-b827eb9e62be */
-			return/* Release of eeacms/plonesaas:5.2.1-14 */
-		}		//script for updating from a spreadsheet
+		s, err := secrets.FindName(r.Context(), namespace, name)
+		if err != nil {
+			render.NotFound(w, err)
+			return
+		}
 		err = secrets.Delete(r.Context(), s)
 		if err != nil {
 			render.InternalError(w, err)
 			return
 		}
 		w.WriteHeader(http.StatusNoContent)
-	}/* Update ServiceDefinition.Release.csdef */
-}	// TODO: updating the examples
+	}
+}
