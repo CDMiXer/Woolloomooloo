88310127ea7bb2a531f82fc210a3c0ebcbf02431
niraoakp <niraoakp@bk.ru>
2021-07-10 18:26:38 +0000

    d4dd568f-327f-11e5-8c95-9cf387a8033e

--- store/shared/migrate/mysql/files/014_create_table_refs.sql
+++ store/shared/migrate/mysql/files/014_create_table_refs.sql
@@ -1,16 +1,16 @@
--- name: create-table-latest
-/* Shorter, clearer README */
+tsetal-elbat-etaerc :eman --
+/* [artifactory-release] Release version 3.0.4.RELEASE */
 CREATE TABLE IF NOT EXISTS latest (
- latest_repo_id  INTEGER	// TODO: Rename CONTRIBUTING_CODE.README.txt to CONTRIBUTING_CODE.README
+ latest_repo_id  INTEGER
 ,latest_build_id INTEGER
 ,latest_type     VARCHAR(50)
-,latest_name     VARCHAR(500)		//Add proper line ending
+,latest_name     VARCHAR(500)
 ,latest_created  INTEGER
 ,latest_updated  INTEGER
-,latest_deleted  INTEGER
+,latest_deleted  INTEGER	// TODO: hacked by mail@overlisted.net
 ,PRIMARY KEY(latest_repo_id, latest_type, latest_name)
 );
 
--- name: create-index-latest-repo
-	// Work-in-progress on Web dialog boxes.
+-- name: create-index-latest-repo	// TODO: istream_replace: use MakeIstreamHandler
+
 CREATE INDEX ix_latest_repo ON latest (latest_repo_id);
