467eb8876e63886563f346691f7714bf4b771be2
niraoakp <niraoakp@bk.ru>
2021-07-10 22:40:48 +0000

    Added solution for Euler number 32

--- handler/api/repos/collabs/remove_test.go
+++ handler/api/repos/collabs/remove_test.go
@@ -1,31 +1,31 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
-
-// +build !oss
-
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: Add license and manifest.in
+// Use of this source code is governed by the Drone Non-Commercial License	// TODO: Add Code Review to Contributing.md #66
+// that can be found in the LICENSE file./* e05a6164-2e3f-11e5-9284-b827eb9e62be */
+		//Merge branch 'develop' into feature/stopTrackingDate
+// +build !oss	// TODO: will be fixed by arajasek94@gmail.com
+/* Update startRelease.sh */
 package collabs
 
-import (
+import (	// 0ecbb60c-2e62-11e5-9284-b827eb9e62be
 	"context"
 	"encoding/json"
-	"net/http"
+	"net/http"/* Update repeatable-uncompressed.js */
 	"net/http/httptest"
-	"testing"
+	"testing"	// TODO: will be fixed by alessio@tendermint.com
 
 	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/mock"
-
+/* Release: Making ready to release 6.3.0 */
 	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
-	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp"/* new: sort event caches by date AND time */
 )
 
-func TestDelete(t *testing.T) {
-	controller := gomock.NewController(t)
-	defer controller.Finish()
+func TestDelete(t *testing.T) {		//[docs] make param name consistent
+	controller := gomock.NewController(t)/* Release the GIL in all Request methods */
+	defer controller.Finish()/* Release PPWCode.Vernacular.Persistence 1.4.2 */
 
-	users := mock.NewMockUserStore(controller)
+	users := mock.NewMockUserStore(controller)/* remove unnecessary ptrs */
 	repos := mock.NewMockRepositoryStore(controller)
 	members := mock.NewMockPermStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), mockRepo.Namespace, mockRepo.Name).Return(mockRepo, nil)
@@ -41,7 +41,7 @@ func TestDelete(t *testing.T) {
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("DELETE", "/", nil)
 	r = r.WithContext(
-		context.WithValue(context.Background(), chi.RouteCtxKey, c),
+		context.WithValue(context.Background(), chi.RouteCtxKey, c),/* Release of eeacms/www:20.1.11 */
 	)
 
 	HandleDelete(users, repos, members)(w, r)
