c1c6f4f68914945f17e41bb08d8f327befa44abf
niraoakp <niraoakp@bk.ru>
2021-07-10 23:57:45 +0000

    Update build instruction

--- plugin/secret/external_test.go
+++ plugin/secret/external_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.		//Fixed incorrect link to "Who Is Using Orleans"
-// Use of this source code is governed by the Drone Non-Commercial License		//Update bin/installOnWindows.bat
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-	// TODO: da4e32ce-2e66-11e5-9284-b827eb9e62be
-package secret/* Minor fix in RPST - one edge case. */
+
+package secret
