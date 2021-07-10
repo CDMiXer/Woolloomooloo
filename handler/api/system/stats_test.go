21465abb6cd8bac069ea526f3797cc9dc67f8dd0
niraoakp <niraoakp@bk.ru>
2021-07-10 19:09:58 +0000

    Borrar conversaciones closes #32

--- handler/api/system/stats_test.go
+++ handler/api/system/stats_test.go
@@ -1,17 +1,17 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: hacked by xaber.twt@gmail.com
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss/* include maven-release  */
+// +build !oss
 
 package system
 
 import (
 	"io/ioutil"
 
-	"github.com/sirupsen/logrus"		//access_log off
+	"github.com/sirupsen/logrus"
 )
-/* 9fe2c30c-2e6b-11e5-9284-b827eb9e62be */
-func init() {
+
+func init() {/* Updating build-info/dotnet/roslyn/dev15.6p4 for beta3-62516-04 */
 	logrus.SetOutput(ioutil.Discard)
-}	// TODO: will be fixed by mikeal.rogers@gmail.com
+}
