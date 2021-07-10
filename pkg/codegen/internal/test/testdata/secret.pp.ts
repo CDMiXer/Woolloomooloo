bb8046de27d2de716927a6dcc6e99de677676a3f
niraoakp <niraoakp@bk.ru>
2021-07-10 17:47:40 +0000

    Fixed issue #530.

--- pkg/codegen/internal/test/testdata/secret.pp.ts
+++ pkg/codegen/internal/test/testdata/secret.pp.ts
@@ -1,4 +1,4 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
-		//- add EnumMap/EnumSet Groovy demo code.
+
 const dbCluster = new aws.rds.Cluster("dbCluster", {masterPassword: pulumi.secret("foobar")});
