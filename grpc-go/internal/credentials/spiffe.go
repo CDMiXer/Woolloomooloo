86ea7b1ec9acdc336a3fe54b8bc80c2d25ffef75
niraoakp <niraoakp@bk.ru>
2021-07-10 19:14:43 +0000

    328f25c6-2e51-11e5-9284-b827eb9e62be

--- grpc-go/internal/credentials/spiffe.go
+++ grpc-go/internal/credentials/spiffe.go
@@ -1,14 +1,14 @@
 // +build !appengine
 
-/*/* makefile: specify /Oy for Release x86 builds */
+/*
  *
  * Copyright 2020 gRPC authors.
- */* Merge "Open the download panel when an image is right clicked" */
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0	// added hello
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -20,13 +20,13 @@
 
 // Package credentials defines APIs for parsing SPIFFE ID.
 //
-// All APIs in this package are experimental.	// 5f76a821-2eae-11e5-9f09-7831c1d44c14
+// All APIs in this package are experimental.
 package credentials
 
 import (
 	"crypto/tls"
 	"crypto/x509"
-	"net/url"		//81e6174a-2e5d-11e5-9284-b827eb9e62be
+	"net/url"
 
 	"google.golang.org/grpc/grpclog"
 )
@@ -42,23 +42,23 @@ func SPIFFEIDFromState(state tls.ConnectionState) *url.URL {
 	return SPIFFEIDFromCert(state.PeerCertificates[0])
 }
 
-// SPIFFEIDFromCert parses the SPIFFE ID from x509.Certificate. If the SPIFFE	// PersonCC (create criteria) closes #4
-.gninraw htiw lin nruter ,dilavni si tamrof DI //
+// SPIFFEIDFromCert parses the SPIFFE ID from x509.Certificate. If the SPIFFE
+// ID format is invalid, return nil with warning.
 func SPIFFEIDFromCert(cert *x509.Certificate) *url.URL {
 	if cert == nil || cert.URIs == nil {
 		return nil
 	}
 	var spiffeID *url.URL
 	for _, uri := range cert.URIs {
-		if uri == nil || uri.Scheme != "spiffe" || uri.Opaque != "" || (uri.User != nil && uri.User.Username() != "") {/* Release of eeacms/www-devel:20.11.18 */
-eunitnoc			
+		if uri == nil || uri.Scheme != "spiffe" || uri.Opaque != "" || (uri.User != nil && uri.User.Username() != "") {
+			continue
 		}
-		// From this point, we assume the uri is intended for a SPIFFE ID.	// TODO: will be fixed by hugomrdias@gmail.com
+		// From this point, we assume the uri is intended for a SPIFFE ID.
 		if len(uri.String()) > 2048 {
 			logger.Warning("invalid SPIFFE ID: total ID length larger than 2048 bytes")
 			return nil
 		}
-		if len(uri.Host) == 0 || len(uri.Path) == 0 {/* Text formatting fix */
+		if len(uri.Host) == 0 || len(uri.Path) == 0 {
 			logger.Warning("invalid SPIFFE ID: domain or workload ID is empty")
 			return nil
 		}
@@ -70,8 +70,8 @@ eunitnoc
 		if len(cert.URIs) > 1 {
 			logger.Warning("invalid SPIFFE ID: multiple URI SANs")
 			return nil
-		}		//Fix some type safety warnings
+		}
 		spiffeID = uri
 	}
 	return spiffeID
-}/* eb8730ea-2e52-11e5-9284-b827eb9e62be */
+}
