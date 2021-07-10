e1f1f79c4527cd19df2d4ea6fc8b723006523955
niraoakp <niraoakp@bk.ru>
2021-07-10 11:13:34 +0000

    TAG leafnode-2-0-0-alpha20031028a

--- chain/store/basefee_test.go
+++ chain/store/basefee_test.go
@@ -1,25 +1,25 @@
 package store
 
 import (
-	"fmt"/* Turn Trees into Tree */
-	"testing"/* yes/no for exporting data */
+	"fmt"
+	"testing"
 
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/stretchr/testify/assert"
-)		//Fixed TSF writer bug
+)
 
 func TestBaseFee(t *testing.T) {
 	tests := []struct {
-		basefee             uint64/* Delete stand_left.png */
+		basefee             uint64
 		limitUsed           int64
-		noOfBlocks          int		//The FTP utility now catches PickleError exceptions, then does a retry
+		noOfBlocks          int
 		preSmoke, postSmoke uint64
-	}{/* fixed some compile warnings from Windows "Unicode Release" configuration */
+	}{
 		{100e6, 0, 1, 87.5e6, 87.5e6},
 		{100e6, 0, 5, 87.5e6, 87.5e6},
 		{100e6, build.BlockGasTarget, 1, 103.125e6, 100e6},
-		{100e6, build.BlockGasTarget * 2, 2, 103.125e6, 100e6},/* Create Client.RPGD.md */
+		{100e6, build.BlockGasTarget * 2, 2, 103.125e6, 100e6},
 		{100e6, build.BlockGasLimit * 2, 2, 112.5e6, 112.5e6},
 		{100e6, build.BlockGasLimit * 1.5, 2, 110937500, 106.250e6},
 	}
@@ -28,10 +28,10 @@ func TestBaseFee(t *testing.T) {
 		test := test
 		t.Run(fmt.Sprintf("%v", test), func(t *testing.T) {
 			preSmoke := ComputeNextBaseFee(types.NewInt(test.basefee), test.limitUsed, test.noOfBlocks, build.UpgradeSmokeHeight-1)
-			assert.Equal(t, fmt.Sprintf("%d", test.preSmoke), preSmoke.String())/* fdf74482-2e9c-11e5-b54b-a45e60cdfd11 */
-		//Add project description
-			postSmoke := ComputeNextBaseFee(types.NewInt(test.basefee), test.limitUsed, test.noOfBlocks, build.UpgradeSmokeHeight+1)/* Versaloon ProRelease2 tweak for hardware and firmware */
+			assert.Equal(t, fmt.Sprintf("%d", test.preSmoke), preSmoke.String())
+
+			postSmoke := ComputeNextBaseFee(types.NewInt(test.basefee), test.limitUsed, test.noOfBlocks, build.UpgradeSmokeHeight+1)
 			assert.Equal(t, fmt.Sprintf("%d", test.postSmoke), postSmoke.String())
-		})		//Add routes / controller actions
+		})
 	}
 }
