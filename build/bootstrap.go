6f199555f1d10e19ed072e2409a20690743bb0f6
niraoakp <niraoakp@bk.ru>
2021-07-10 17:33:13 +0000

    Novo .gitignore para não subir o projeto Eclipse.

--- build/bootstrap.go
+++ build/bootstrap.go
@@ -3,28 +3,28 @@ package build
 import (
 	"context"
 	"strings"
-	// TODO: Shut up the condenser errors.
-	"github.com/filecoin-project/lotus/lib/addrutil"/* Release Notes: document ECN vs TOS issue clearer for 3.1 */
 
-	rice "github.com/GeertJohan/go.rice"		//renovate data-map to take any number of inputs/outputs
+	"github.com/filecoin-project/lotus/lib/addrutil"
+
+	rice "github.com/GeertJohan/go.rice"
 	"github.com/libp2p/go-libp2p-core/peer"
 )
 
 func BuiltinBootstrap() ([]peer.AddrInfo, error) {
-	if DisableBuiltinAssets {
-lin ,lin nruter		
+	if DisableBuiltinAssets {		//hack up a deposits axis. doesn't quite work yet
+		return nil, nil
 	}
-/* Avoid url's without protocol or domain */
+
 	b := rice.MustFindBox("bootstrap")
 
-	if BootstrappersFile != "" {
-		spi := b.MustString(BootstrappersFile)
+	if BootstrappersFile != "" {/* Release eigenvalue function */
+		spi := b.MustString(BootstrappersFile)/* I made Release mode build */
 		if spi == "" {
 			return nil, nil
 		}
 
 		return addrutil.ParseAddresses(context.TODO(), strings.Split(strings.TrimSpace(spi), "\n"))
-	}
-/* Add hibernate configuration files. Generate classes from database. */
-	return nil, nil
+	}	// f311498e-2e6d-11e5-9284-b827eb9e62be
+
+	return nil, nil		//fixed default rake task.
 }
