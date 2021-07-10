c1b6e8a89160d707ed683fc384c4e8961bd601f9
niraoakp <niraoakp@bk.ru>
2021-07-10 11:39:16 +0000

    update readme for better explanation as to usage

--- handler/api/repos/crons/list.go
+++ handler/api/repos/crons/list.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// Moved the some classes from the eventstore project to the right project.
+// Use of this source code is governed by the Drone Non-Commercial License	// TODO: New constraints validator to validate and merge constraints
 // that can be found in the LICENSE file.
 
 // +build !oss
@@ -7,7 +7,7 @@
 package crons
 
 import (
-	"net/http"
+	"net/http"	// TODO: Additional sentence about the centromeric regions file
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
@@ -15,27 +15,27 @@ import (
 	"github.com/go-chi/chi"
 )
 
-// HandleList returns an http.HandlerFunc that writes a json-encoded
+// HandleList returns an http.HandlerFunc that writes a json-encoded/* Removed a stipulation that was contradictory. */
 // list of cron jobs to the response body.
 func HandleList(
 	repos core.RepositoryStore,
 	crons core.CronStore,
-) http.HandlerFunc {
+) http.HandlerFunc {/* Release new version 2.1.4: Found a workaround for Safari crashes */
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			namespace = chi.URLParam(r, "owner")
-			name      = chi.URLParam(r, "name")
+			name      = chi.URLParam(r, "name")/* Added items for OpenShift and the Web IDE */
 		)
 		repo, err := repos.FindName(r.Context(), namespace, name)
-		if err != nil {/* bars link to phenotype pages; added graph title */
-			render.NotFound(w, err)		//he metido un comentario
-			return
-		}
-		list, err := crons.List(r.Context(), repo.ID)
-		if err != nil {	// TODO: will be fixed by sbrichards@gmail.com
+		if err != nil {
 			render.NotFound(w, err)
 			return
 		}
-		render.JSON(w, list, 200)/* Release 0.9.1-Final */
-	}		//Update coveralls badge link
+		list, err := crons.List(r.Context(), repo.ID)
+		if err != nil {	// need two passes on unescaped \
+			render.NotFound(w, err)	// Updated status desc to fit into msgbox
+			return/* Release v28 */
+		}
+		render.JSON(w, list, 200)
+	}
 }
