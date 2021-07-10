b7770d984767dbec0c2ad5376f716674ab812230
niraoakp <niraoakp@bk.ru>
2021-07-10 16:04:00 +0000

    Add Manning NLP lecture link

--- tests/integration/read/read_replace/step1/index.ts
+++ tests/integration/read/read_replace/step1/index.ts
@@ -1,20 +1,20 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.		//Update energy.go
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//     http://www.apache.org/licenses/LICENSE-2.0/* :memo: Added donate info. */
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Improved test to not rely on nonsensical data */
+// See the License for the specific language governing permissions and	// TODO: will be fixed by qugou1350636@126.com
 // limitations under the License.
 
 import { Resource } from "./resource";
-		//Use SVG for icons and remove ionicons
+
 // Setup: Resource A is external, Resource B is not.
 const a = new Resource("a", { state: 42 }, { id: "existing-id"} );
-const b = new Resource("b", { state: a.state.apply((b: any) => b + 1)});		//Check threads are supportted before calling g_thread_init
+const b = new Resource("b", { state: a.state.apply((b: any) => b + 1)});
 
