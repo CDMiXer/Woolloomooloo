30f99f5b765c1e8a4d83642213c1acab60843540
niraoakp <niraoakp@bk.ru>
2021-07-10 00:57:41 +0000

    c19849ce-2e50-11e5-9284-b827eb9e62be

--- pkg/codegen/internal/test/testdata/secret.pp
+++ pkg/codegen/internal/test/testdata/secret.pp
@@ -1,3 +1,3 @@
-resource dbCluster "aws:rds:Cluster" {
+resource dbCluster "aws:rds:Cluster" {	// TODO: Rebuilt index with 72Colton
 	masterPassword = secret("foobar")
-}
+}	// TODO: Create test_bios.lua
