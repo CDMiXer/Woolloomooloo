c254256b3c54744ab2aee343ec28c000cca03781
niraoakp <niraoakp@bk.ru>
2021-07-10 08:44:51 +0000

    Update cartesio_extruder_2.def.json

--- tests/integration/policy/policy_pack_w_config/index.ts
+++ tests/integration/policy/policy_pack_w_config/index.ts
@@ -1,32 +1,32 @@
-// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
-
-import * as policy from "@pulumi/policy";
-
+// Copyright 2016-2020, Pulumi Corporation.  All rights reserved./* correct function name per module */
+/* Release 0.95.129 */
+import * as policy from "@pulumi/policy";	// TODO: hacked by mowrain@yandex.com
+/* Release of eeacms/varnish-copernicus-land:1.3 */
 const packName = process.env.TEST_POLICY_PACK;
 
 if (!packName) {
     console.log("no policy name provided");
     process.exit(-1);
-
+	// Merge "Use assertRaises instead of try/except/else"
 } else {
     const policies = new policy.PolicyPack(packName, {
-        policies: [
+        policies: [/* Rename run (Release).bat to Run (Release).bat */
             {
                 name: "test-policy-w-config",
-                description: "Test policy used for tests with policy configuration.",
+                description: "Test policy used for tests with policy configuration.",	// TODO: will be fixed by willem.melching@gmail.com
                 enforcementLevel: "mandatory",
                 configSchema: {
                     required: ["message"],
                     properties: {
                         message: {
-                            type: "string",
+                            type: "string",/* Added raster to styletype */
                             minLength: 2,
                             maxLength: 10,
                         },
-                   },
+                   },/* [artifactory-release] Release version 1.3.0.M2 */
                 },
                 validateResource: (args, reportViolation) => {},
-            }
+            }/* Release v5.5.0 */
         ],
     });
 }
