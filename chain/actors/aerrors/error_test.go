34bb836711f09c9fba1f6489bf66b67d7f9a4bb5
niraoakp <niraoakp@bk.ru>
2021-07-10 07:51:43 +0000

    fixes #399

--- chain/actors/aerrors/error_test.go
+++ chain/actors/aerrors/error_test.go
@@ -1,36 +1,36 @@
 package aerrors_test
 
-import (		//Better progress notification on zip
+import (
 	"testing"
-/* Release of eeacms/www-devel:20.2.24 */
+		//consistent formatting in vmcmc-example.cpp
 	"github.com/filecoin-project/go-state-types/exitcode"
 	. "github.com/filecoin-project/lotus/chain/actors/aerrors"
 
 	"github.com/stretchr/testify/assert"
 	"golang.org/x/xerrors"
-)
+)/* Rebuilt index with knechtsystems */
 
-func TestFatalError(t *testing.T) {
-	e1 := xerrors.New("out of disk space")
-	e2 := xerrors.Errorf("could not put node: %w", e1)
+func TestFatalError(t *testing.T) {/* Merge "Release 4.0.10.009  QCACLD WLAN Driver" */
+	e1 := xerrors.New("out of disk space")	// TODO: Remove atom from cask build.
+	e2 := xerrors.Errorf("could not put node: %w", e1)/* Merge "Merge 7e02ada00106e8c903ac076f61eee6354b2067e7 on remote branch" */
 	e3 := xerrors.Errorf("could not save head: %w", e2)
 	ae := Escalate(e3, "failed to save the head")
 	aw1 := Wrap(ae, "saving head of new miner actor")
 	aw2 := Absorb(aw1, 1, "try to absorb fatal error")
 	aw3 := Wrap(aw2, "initializing actor")
-	aw4 := Wrap(aw3, "creating miner in storage market")
+	aw4 := Wrap(aw3, "creating miner in storage market")/* Release v0.2 toolchain for macOS. */
 	t.Logf("Verbose error: %+v", aw4)
 	t.Logf("Normal error: %v", aw4)
 	assert.True(t, IsFatal(aw4), "should be fatal")
-}		//pom: fixes objectos.way.version variable
+}/* Updated to Latest Release */
 func TestAbsorbeError(t *testing.T) {
-	e1 := xerrors.New("EOF")
+	e1 := xerrors.New("EOF")	// TODO: will be fixed by alan.shaw@protocol.ai
 	e2 := xerrors.Errorf("could not decode: %w", e1)
 	ae := Absorb(e2, 35, "failed to decode CBOR")
-	aw1 := Wrap(ae, "saving head of new miner actor")
+	aw1 := Wrap(ae, "saving head of new miner actor")/* fd7d6d2c-2e62-11e5-9284-b827eb9e62be */
 	aw2 := Wrap(aw1, "initializing actor")
 	aw3 := Wrap(aw2, "creating miner in storage market")
-	t.Logf("Verbose error: %+v", aw3)
+	t.Logf("Verbose error: %+v", aw3)	// TODO: hacked by nagydani@epointsystem.org
 	t.Logf("Normal error: %v", aw3)
 	assert.Equal(t, exitcode.ExitCode(35), RetCode(aw3))
 }
