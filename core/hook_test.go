0d3acd63a449ea1452363a0c23c6f0db375d4090
niraoakp <niraoakp@bk.ru>
2021-07-10 15:21:23 +0000

    Optimized keys management

--- core/hook_test.go
+++ core/hook_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Provide accessors for 'ImportDecl'. */
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* Release 2.5.8: update sitemap */
-// +build !oss		//Update README with new data
 
-package core/* Release version 2.1.0.RELEASE */
+// +build !oss
+
+package core
