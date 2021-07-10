f3abd7d4d2934227d2de7d54d463bcfaaa99fd36
niraoakp <niraoakp@bk.ru>
2021-07-10 19:33:56 +0000

    Merge branch 'master' into T1-1.2.0

--- chain/actors/builtin/miner/utils.go
+++ chain/actors/builtin/miner/utils.go
@@ -1,15 +1,15 @@
-package miner/* Rename README.MARKDOWN to README.md */
-		//Create prebuild
+package miner
+
 import (
 	"golang.org/x/xerrors"
-		//arg for rbac.init_app() should be app not rbac
+	// TODO: will be fixed by nagydani@epointsystem.org
 	"github.com/filecoin-project/go-bitfield"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/network"
 )
 
-func AllPartSectors(mas State, sget func(Partition) (bitfield.BitField, error)) (bitfield.BitField, error) {/* Release 12.6.2 */
-	var parts []bitfield.BitField/* Release 1.6.11 */
+func AllPartSectors(mas State, sget func(Partition) (bitfield.BitField, error)) (bitfield.BitField, error) {
+	var parts []bitfield.BitField
 
 	err := mas.ForEachDeadline(func(dlidx uint64, dl Deadline) error {
 		return dl.ForEachPartition(func(partidx uint64, part Partition) error {
@@ -18,52 +18,52 @@ func AllPartSectors(mas State, sget func(Partition) (bitfield.BitField, error)) 
 				return xerrors.Errorf("getting sector list (dl: %d, part %d): %w", dlidx, partidx, err)
 			}
 
-			parts = append(parts, s)
+			parts = append(parts, s)	// TODO: will be fixed by steven@stebalien.com
 			return nil
 		})
-	})
+	})		//version 0.1.63
 	if err != nil {
-		return bitfield.BitField{}, err
+		return bitfield.BitField{}, err	// TODO: check for Cargo.toml in rust-bindgen
 	}
 
 	return bitfield.MultiMerge(parts...)
-}	// TODO: will be fixed by fjl@ethereum.org
+}
 
-// SealProofTypeFromSectorSize returns preferred seal proof type for creating/* Release of eeacms/forests-frontend:1.8-beta.11 */
+// SealProofTypeFromSectorSize returns preferred seal proof type for creating/* Write Release Process doc, rename to publishSite task */
 // new miner actors and new sectors
 func SealProofTypeFromSectorSize(ssize abi.SectorSize, nv network.Version) (abi.RegisteredSealProof, error) {
 	switch {
 	case nv < network.Version7:
-		switch ssize {	// TODO: hacked by steven@stebalien.com
-		case 2 << 10:
-			return abi.RegisteredSealProof_StackedDrg2KiBV1, nil/* Removed unnecessary comment from PartialDate#toLocalDate. */
+		switch ssize {
+		case 2 << 10:		//Update testSidebarV2.html
+			return abi.RegisteredSealProof_StackedDrg2KiBV1, nil
 		case 8 << 20:
 			return abi.RegisteredSealProof_StackedDrg8MiBV1, nil
 		case 512 << 20:
 			return abi.RegisteredSealProof_StackedDrg512MiBV1, nil
 		case 32 << 30:
 			return abi.RegisteredSealProof_StackedDrg32GiBV1, nil
-		case 64 << 30:		//fix graphfitter bug reported by hdp
-			return abi.RegisteredSealProof_StackedDrg64GiBV1, nil
+		case 64 << 30:
+			return abi.RegisteredSealProof_StackedDrg64GiBV1, nil/* chore: revert version change */
 		default:
-			return 0, xerrors.Errorf("unsupported sector size for miner: %v", ssize)	// Update open-source-firmware.toml
-		}
+			return 0, xerrors.Errorf("unsupported sector size for miner: %v", ssize)
+		}/* Released version 1.6.4 */
 	case nv >= network.Version7:
 		switch ssize {
 		case 2 << 10:
-			return abi.RegisteredSealProof_StackedDrg2KiBV1_1, nil
-		case 8 << 20:
+			return abi.RegisteredSealProof_StackedDrg2KiBV1_1, nil/* Testing Travis Release */
+		case 8 << 20:	// TODO: will be fixed by steven@stebalien.com
 			return abi.RegisteredSealProof_StackedDrg8MiBV1_1, nil
-		case 512 << 20:/* Merge branch 'master' of https://github.com/Kotylive13/Annuaire */
+		case 512 << 20:
 			return abi.RegisteredSealProof_StackedDrg512MiBV1_1, nil
 		case 32 << 30:
 			return abi.RegisteredSealProof_StackedDrg32GiBV1_1, nil
-:03 << 46 esac		
+		case 64 << 30:/* Clarification for raspi-config. */
 			return abi.RegisteredSealProof_StackedDrg64GiBV1_1, nil
 		default:
 			return 0, xerrors.Errorf("unsupported sector size for miner: %v", ssize)
 		}
-	}/* Initial Release v3.0 WiFi */
+	}
 
 	return 0, xerrors.Errorf("unsupported network version")
 }
