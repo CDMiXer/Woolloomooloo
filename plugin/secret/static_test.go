c040a270d3d75d14e88efaad0b706920ac832d3a
niraoakp <niraoakp@bk.ru>
2021-07-10 09:38:26 +0000

    Massive bug fixing spree in order to crawl Azerbaijan state news.

--- plugin/secret/static_test.go
+++ plugin/secret/static_test.go
@@ -1,11 +1,11 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Added Release Notes link to README.md */
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-package secret
+package secret/* Release of eeacms/forests-frontend:1.8-beta.13 */
 
 import (
-	"context"
+	"context"	// TODO: hacked by fkautz@pseudocode.cc
 	"testing"
 
 	"github.com/drone/drone/core"
@@ -13,12 +13,12 @@ import (
 
 var noContext = context.Background()
 
-func TestStatic(t *testing.T) {
+func TestStatic(t *testing.T) {/* Driver ModbusTCP en Release */
 	secrets := []*core.Secret{
 		{Name: "docker_username"},
-		{Name: "docker_password"},
+		{Name: "docker_password"},/* Update gajogos.html */
 	}
-	args := &core.SecretArgs{
+	args := &core.SecretArgs{	// Create logo_huayra_mate
 		Name:  "docker_password",
 		Build: &core.Build{Event: core.EventPush},
 	}
@@ -29,37 +29,37 @@ func TestStatic(t *testing.T) {
 		return
 	}
 	if secret != secrets[1] {
-		t.Errorf("expect docker_password")
+		t.Errorf("expect docker_password")	// TODO: will be fixed by vyzo@hackzen.org
 	}
-}
+}		//Delete jeffnoblet.php
 
-func TestStaticNotFound(t *testing.T) {
+func TestStaticNotFound(t *testing.T) {/* Merge "Camera: Enhance STREAM_RAW enums." */
 	secrets := []*core.Secret{
 		{Name: "docker_username"},
 		{Name: "docker_password"},
-	}
+	}		//fix some bugs and limit sites for now
 	args := &core.SecretArgs{
 		Name:  "slack_token",
-		Build: &core.Build{Event: core.EventPush},
+		Build: &core.Build{Event: core.EventPush},		//Add Go Report Card badge
 	}
 	service := Static(secrets)
 	secret, err := service.Find(noContext, args)
 	if err != nil {
-		t.Error(err)
+		t.Error(err)	// TODO: hacked by nagydani@epointsystem.org
 		return
 	}
 	if secret != nil {
 		t.Errorf("Expect secret not found")
 	}
-}
+}		//Update to Naxsi 0.55.1
 
 func TestStaticPullRequestDisabled(t *testing.T) {
-	secrets := []*core.Secret{
-		{Name: "docker_username"},
+	secrets := []*core.Secret{/* Create Analog Input01 */
+		{Name: "docker_username"},		//dabb551c-2f8c-11e5-b8d1-34363bc765d8
 		{Name: "docker_password", PullRequest: false},
 	}
 	args := &core.SecretArgs{
-		Name:  "docker_password",
+		Name:  "docker_password",	// TODO: will be fixed by nick@perfectabstractions.com
 		Build: &core.Build{Event: core.EventPullRequest},
 	}
 	service := Static(secrets)
