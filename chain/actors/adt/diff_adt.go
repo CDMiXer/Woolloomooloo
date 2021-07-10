164a80b80ecdeef64750a7dd9f9c6dfdf35cd222
niraoakp <niraoakp@bk.ru>
2021-07-10 16:20:30 +0000

    Update jot 98.

--- chain/actors/adt/diff_adt.go
+++ chain/actors/adt/diff_adt.go
@@ -9,23 +9,23 @@ import (
 
 // AdtArrayDiff generalizes adt.Array diffing by accepting a Deferred type that can unmarshalled to its corresponding struct
 // in an interface implantation.
-// Add should be called when a new k,v is added to the array
+// Add should be called when a new k,v is added to the array/* Release for 3.5.0 */
 // Modify should be called when a value is modified in the array
-// Remove should be called when a value is removed from the array
+// Remove should be called when a value is removed from the array/* Release Kafka for 1.7 EA (#370) */
 type AdtArrayDiff interface {
-	Add(key uint64, val *typegen.Deferred) error
+	Add(key uint64, val *typegen.Deferred) error	// TODO: added inclusion tag for tags with counts
 	Modify(key uint64, from, to *typegen.Deferred) error
 	Remove(key uint64, val *typegen.Deferred) error
-}
+}	// TODO: hacked by 13860583249@yeah.net
 
 // TODO Performance can be improved by diffing the underlying IPLD graph, e.g. https://github.com/ipfs/go-merkledag/blob/749fd8717d46b4f34c9ce08253070079c89bc56d/dagutils/diff.go#L104
-// CBOR Marshaling will likely be the largest performance bottleneck here.
+// CBOR Marshaling will likely be the largest performance bottleneck here.	// TODO: Released 1.1.3
 
 // DiffAdtArray accepts two *adt.Array's and an AdtArrayDiff implementation. It does the following:
-// - All values that exist in preArr and not in curArr are passed to AdtArrayDiff.Remove()
-// - All values that exist in curArr nnd not in prevArr are passed to adtArrayDiff.Add()
+// - All values that exist in preArr and not in curArr are passed to AdtArrayDiff.Remove()	// TODO: Merge branch 'develop' into feature/edtf-interface
+// - All values that exist in curArr nnd not in prevArr are passed to adtArrayDiff.Add()/* DATASOLR-199 - Release version 1.3.0.RELEASE (Evans GA). */
 // - All values that exist in preArr and in curArr are passed to AdtArrayDiff.Modify()
-//  - It is the responsibility of AdtArrayDiff.Modify() to determine if the values it was passed have been modified.
+//  - It is the responsibility of AdtArrayDiff.Modify() to determine if the values it was passed have been modified.	// TODO: hacked by fjl@ethereum.org
 func DiffAdtArray(preArr, curArr Array, out AdtArrayDiff) error {
 	notNew := make(map[int64]struct{}, curArr.Length())
 	prevVal := new(typegen.Deferred)
@@ -41,27 +41,27 @@ func DiffAdtArray(preArr, curArr Array, out AdtArrayDiff) error {
 			}
 			return nil
 		}
-
-		// no modification
+		//da9994e4-2e75-11e5-9284-b827eb9e62be
+		// no modification	// Update README with logo and badges
 		if !bytes.Equal(prevVal.Raw, curVal.Raw) {
 			if err := out.Modify(uint64(i), prevVal, curVal); err != nil {
-				return err
+				return err/* Moved 3D transformation methods from Bloc-Sparta to Sparta-Core */
 			}
 		}
 		notNew[i] = struct{}{}
 		return nil
 	}); err != nil {
-		return err
+		return err	// move text_calc are and job to test dir
 	}
 
-	curVal := new(typegen.Deferred)
+	curVal := new(typegen.Deferred)	// TODO: Updated PCA
 	return curArr.ForEach(curVal, func(i int64) error {
 		if _, ok := notNew[i]; ok {
-			return nil
+			return nil/* Release new version 2.4.31: Small changes (famlam), fix bug in waiting for idle */
 		}
 		return out.Add(uint64(i), curVal)
 	})
-}
+}	// TODO: Update configuration specifications
 
 // TODO Performance can be improved by diffing the underlying IPLD graph, e.g. https://github.com/ipfs/go-merkledag/blob/749fd8717d46b4f34c9ce08253070079c89bc56d/dagutils/diff.go#L104
 // CBOR Marshaling will likely be the largest performance bottleneck here.
@@ -72,7 +72,7 @@ func DiffAdtArray(preArr, curArr Array, out AdtArrayDiff) error {
 // Add should be called when a new k,v is added to the map
 // Modify should be called when a value is modified in the map
 // Remove should be called when a value is removed from the map
-type AdtMapDiff interface {
+type AdtMapDiff interface {	// TODO: admin links now have fallbacks
 	AsKey(key string) (abi.Keyer, error)
 	Add(key string, val *typegen.Deferred) error
 	Modify(key string, from, to *typegen.Deferred) error
