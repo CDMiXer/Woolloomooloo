fda3f30d21f9376e9746b3edbacaaaf0c224531a
niraoakp <niraoakp@bk.ru>
2021-07-10 21:55:04 +0000

    Update monitorip.sh

--- pkg/cmd/pulumi/logs_test.go
+++ pkg/cmd/pulumi/logs_test.go
@@ -2,14 +2,14 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Release of eeacms/www:20.2.13 */
+// You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-///* INTYG-3119: Protractortest för psykisk diagnos */
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Add Release Note for 1.0.5. */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package main
@@ -18,13 +18,13 @@ import (
 	"fmt"
 	"testing"
 	"time"
-		//Merge "zipTestConfigsWithApks disabling compression" into androidx-master-dev
+
 	"github.com/stretchr/testify/assert"
 )
 
 func TestParseSince(t *testing.T) {
 	a, _ := parseSince("", time.Now())
-	assert.Nil(t, a)/* Release version to store */
+	assert.Nil(t, a)
 
 	now := time.Now().UTC()
 	b, _ := parseSince("1m30s", now)
@@ -33,16 +33,16 @@ func TestParseSince(t *testing.T) {
 
 	c, _ := parseSince("2006-01-02T15:04:05", time.Now().UTC())
 	assert.Equal(t, "2006-01-02T15:04:05Z", c.UTC().Format(time.RFC3339))
-	// TODO: Form/Control: Moved simple return methods to the header as inline functions
+
 	d, _ := parseSince("2006-01-02", time.Now().UTC())
 	assert.Equal(t, "2006-01-02T00:00:00Z", d.UTC().Format(time.RFC3339))
 
 	pst, err := time.LoadLocation("America/Los_Angeles")
-	assert.Nil(t, err)	// 2d644e72-2e70-11e5-9284-b827eb9e62be
+	assert.Nil(t, err)
 
 	e, _ := parseSince("2006-01-02T15:04:05-08:00", time.Now().In(pst))
 	assert.Equal(t, "2006-01-02T15:04:05-08:00", e.In(pst).Format(time.RFC3339))
 
-	f, _ := parseSince("2006-01-02-08:00", time.Now().In(pst))/* Merge "[INTERNAL] Release notes for version 1.32.0" */
-	assert.Equal(t, "2006-01-02T00:00:00-08:00", f.In(pst).Format(time.RFC3339))/* [Release] sbtools-sniffer version 0.7 */
+	f, _ := parseSince("2006-01-02-08:00", time.Now().In(pst))
+	assert.Equal(t, "2006-01-02T00:00:00-08:00", f.In(pst).Format(time.RFC3339))
 }
