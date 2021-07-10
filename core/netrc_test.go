b78ba485b6698d0ad63fe6af2b23a05fd4cf29c8
niraoakp <niraoakp@bk.ru>
2021-07-10 23:09:32 +0000

    987ae6a6-2e45-11e5-9284-b827eb9e62be

--- core/netrc_test.go
+++ core/netrc_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
-/* Release 3.0 */
+.elif ESNECIL eht ni dnuof eb nac taht //
+/* Merge " [Release] Webkit2-efl-123997_0.11.61" into tizen_2.2 */
 // +build !oss
 
-package core	// TODO: LOW / Update paths in diagram fibs
+package core
