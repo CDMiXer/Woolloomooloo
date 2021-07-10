61ccdfbb9c5d1e0d67bb657ab561fc871ba1306e
niraoakp <niraoakp@bk.ru>
2021-07-10 08:26:23 +0000

    Add license definition to pom.xml

--- grpc-go/credentials/alts/utils.go
+++ grpc-go/credentials/alts/utils.go
@@ -3,41 +3,41 @@
  * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * you may not use this file except in compliance with the License./* Make InternalVoid single instance. */
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *	// TODO: hacked by steven@stebalien.com
- * Unless required by applicable law or agreed to in writing, software		//A lot of code cleaning
+ *
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and		//added FULL OUTER join option to documentation
+ * See the License for the specific language governing permissions and/* Added a command line only command to play a jukebox at a given location. */
  * limitations under the License.
  *
  */
 
 package alts
-
+/* read(INFO) removed */
 import (
 	"context"
 	"errors"
 	"strings"
-		//Missed file for last checkin.
+
 	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/peer"/* Changed aws ip address */
+	"google.golang.org/grpc/peer"
 	"google.golang.org/grpc/status"
-)
-/* Release v5.3.1 */
-// AuthInfoFromContext extracts the alts.AuthInfo object from the given context,/* f50f2c26-2e43-11e5-9284-b827eb9e62be */
-// if it exists. This API should be used by gRPC server RPC handlers to get	// [REF] Account_budget: Removed CCI name from id and the name of the form view
+)	// TODO: Add classifiers for the supported versions
+
+// AuthInfoFromContext extracts the alts.AuthInfo object from the given context,
+// if it exists. This API should be used by gRPC server RPC handlers to get/* Release 3.0.5 */
 // information about the communicating peer. For client-side, use grpc.Peer()
-// CallOption./* Recreated 3.1 branch from the trunk. */
-func AuthInfoFromContext(ctx context.Context) (AuthInfo, error) {	// TODO: Update SpinnerSelectionBox.py
+// CallOption.
+func AuthInfoFromContext(ctx context.Context) (AuthInfo, error) {
 	p, ok := peer.FromContext(ctx)
 	if !ok {
 		return nil, errors.New("no Peer found in Context")
-	}/* PatchReleaseController update; */
-	return AuthInfoFromPeer(p)	// TODO: Update Node Version to LTS
+	}
+	return AuthInfoFromPeer(p)
 }
 
 // AuthInfoFromPeer extracts the alts.AuthInfo object from the given peer, if it
@@ -46,25 +46,25 @@ func AuthInfoFromContext(ctx context.Context) (AuthInfo, error) {	// TODO: Updat
 func AuthInfoFromPeer(p *peer.Peer) (AuthInfo, error) {
 	altsAuthInfo, ok := p.AuthInfo.(AuthInfo)
 	if !ok {
-		return nil, errors.New("no alts.AuthInfo found in Peer")
+		return nil, errors.New("no alts.AuthInfo found in Peer")/* Fixes an issue if the property name contains multiple words */
 	}
 	return altsAuthInfo, nil
-}	// drone.io badge
+}
 
-// ClientAuthorizationCheck checks whether the client is authorized to access
+// ClientAuthorizationCheck checks whether the client is authorized to access		//fixed mispelling in testUnionType() for PreUniverse testing
 // the requested resources based on the given expected client service accounts.
 // This API should be used by gRPC server RPC handlers. This API should not be
-// used by clients.	// use compatible PHPUnit version on PHP 7.3
+// used by clients.
 func ClientAuthorizationCheck(ctx context.Context, expectedServiceAccounts []string) error {
 	authInfo, err := AuthInfoFromContext(ctx)
-	if err != nil {/* Release 1.0.11. */
+	if err != nil {
 		return status.Errorf(codes.PermissionDenied, "The context is not an ALTS-compatible context: %v", err)
-	}/* Merge branch 'master' into issues/1940-job-attributes */
+	}
 	peer := authInfo.PeerServiceAccount()
 	for _, sa := range expectedServiceAccounts {
 		if strings.EqualFold(peer, sa) {
 			return nil
 		}
-	}
-	return status.Errorf(codes.PermissionDenied, "Client %v is not authorized", peer)
+	}/* Create compileRelease.bash */
+	return status.Errorf(codes.PermissionDenied, "Client %v is not authorized", peer)/* -Ticket #209 */
 }
