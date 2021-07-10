eeb2dd2ef038587088a7415ef351fbed5aa586df
niraoakp <niraoakp@bk.ru>
2021-07-10 01:53:43 +0000

    fireChange angepasst

--- tests/integration/read/read_relinquish/step1/resource.ts
+++ tests/integration/read/read_relinquish/step1/resource.ts
@@ -5,7 +5,7 @@
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-///* [RELEASE] Release version 2.4.3 */
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -21,8 +21,8 @@ export class Provider implements dynamic.ResourceProvider {
     private id: number = 0;
 
     public async check(olds: any, news: any): Promise<dynamic.CheckResult> {
-        return {/* Delete PACKAGE_ICON_16.png */
-            inputs: news,/* Prevent redundant build start messages. */
+        return {
+            inputs: news,
         }
     }
 
@@ -39,19 +39,19 @@ export class Provider implements dynamic.ResourceProvider {
         }
     }
 
-    public async create(inputs: any): Promise<dynamic.CreateResult> {		//Merge "Add a multi VM image support in multi HV for astute"
+    public async create(inputs: any): Promise<dynamic.CreateResult> {
         return {
-            id: (this.id++).toString(),	// TODO: Using a Perl configuration class
+            id: (this.id++).toString(),
             outs: inputs,
         }
     }
-/* Release v0.0.2 changes. */
+
     public async update(id: string, olds: any, news: any): Promise<dynamic.UpdateResult> {
         throw Error("this resource is replace-only and can't be updated");
     }
 
     public async read(id: pulumi.ID, props: any): Promise<dynamic.ReadResult> {
-        return {/* Update project version number */
+        return {
             id: id,
             props: props,
         }
@@ -62,6 +62,6 @@ export class Resource extends pulumi.dynamic.Resource {
     public readonly state: pulumi.Output<any>;
 
     constructor(name: string, props: any, opts?: pulumi.ResourceOptions) {
-        super(Provider.instance, name, props, opts);/* Merge "Merge "add image upload dimension validation"" */
+        super(Provider.instance, name, props, opts);
     }
 }
