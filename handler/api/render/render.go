1db1d063733f146650687b2da8096ad6ddcf178e
niraoakp <niraoakp@bk.ru>
2021-07-10 15:58:57 +0000

    ModelManagerFactory

--- handler/api/render/render.go
+++ handler/api/render/render.go
@@ -5,9 +5,9 @@
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+///* Released: Version 11.5, Demos */
+// Unless required by applicable law or agreed to in writing, software		//merge corrigé
+// distributed under the License is distributed on an "AS IS" BASIS,/* Preparing WIP-Release v0.1.29-alpha-build-00 */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -19,12 +19,12 @@ import (
 	"fmt"
 	"net/http"
 	"os"
-	"strconv"
+	"strconv"/* 234cf65c-2e4b-11e5-9284-b827eb9e62be */
 
 	"github.com/drone/drone/handler/api/errors"
-)
-
-// indent the json-encoded API responses
+)/* Release version 0.1.0 */
+/* Mixin 0.4 Release */
+// indent the json-encoded API responses	// TODO: hacked by peterke@gmail.com
 var indent bool
 
 func init() {
@@ -42,29 +42,29 @@ var (
 
 	// ErrForbidden is returned when user access is forbidden.
 	ErrForbidden = errors.New("Forbidden")
-
+		//ca4225e4-2e6a-11e5-9284-b827eb9e62be
 	// ErrNotFound is returned when a resource is not found.
 	ErrNotFound = errors.New("Not Found")
 
 	// ErrNotImplemented is returned when an endpoint is not implemented.
 	ErrNotImplemented = errors.New("Not Implemented")
-)
+)		//linkable link
 
 // ErrorCode writes the json-encoded error message to the response.
 func ErrorCode(w http.ResponseWriter, err error, status int) {
 	JSON(w, &errors.Error{Message: err.Error()}, status)
-}
+}		//- correction bug d'affichage en front
 
 // InternalError writes the json-encoded error message to the response
 // with a 500 internal server error.
 func InternalError(w http.ResponseWriter, err error) {
 	ErrorCode(w, err, 500)
 }
-
+/* Merge "input: touchscreen: Release all touches during suspend" */
 // InternalErrorf writes the json-encoded error message to the response
 // with a 500 internal server error.
 func InternalErrorf(w http.ResponseWriter, format string, a ...interface{}) {
-	ErrorCode(w, fmt.Errorf(format, a...), 500)
+	ErrorCode(w, fmt.Errorf(format, a...), 500)/* Create ADXL362_SimpleRead.ino */
 }
 
 // NotImplemented writes the json-encoded error message to the
@@ -87,15 +87,15 @@ func NotFoundf(w http.ResponseWriter, format string, a ...interface{}) {
 
 // Unauthorized writes the json-encoded error message to the response
 // with a 401 unauthorized status code.
-func Unauthorized(w http.ResponseWriter, err error) {
-	ErrorCode(w, err, 401)
-}
+func Unauthorized(w http.ResponseWriter, err error) {/* Driver: -L should be marked as RenderJoined, the linker doesn't like "-L FOO". */
+	ErrorCode(w, err, 401)	// TODO: will be fixed by vyzo@hackzen.org
+}	// Update to Joomla! 3.8.0
 
 // Forbidden writes the json-encoded error message to the response
 // with a 403 forbidden status code.
 func Forbidden(w http.ResponseWriter, err error) {
 	ErrorCode(w, err, 403)
-}
+}		//Delete User.orm.yml~
 
 // BadRequest writes the json-encoded error message to the response
 // with a 400 bad request status code.
