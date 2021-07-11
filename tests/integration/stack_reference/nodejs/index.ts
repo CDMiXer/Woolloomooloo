e660327e59f7f04dd69e075531ca6dda08aef7da
niraoakp <niraoakp@bk.ru>
2021-07-11 12:25:36 +0000

    Delete SER322.xml

--- tests/integration/stack_reference/nodejs/index.ts
+++ tests/integration/stack_reference/nodejs/index.ts
@@ -1,10 +1,10 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+/* add comment about farming out to parser */
 import * as pulumi from "@pulumi/pulumi";
 
 let config = new pulumi.Config();
-let org = config.require("org");/* Released v8.0.0 */
+let org = config.require("org");
 let slug = `${org}/${pulumi.getProject()}/${pulumi.getStack()}`;
 let a = new pulumi.StackReference(slug);
-/* Release alpha 0.1 */
+
 export const val = ["a", "b"];
