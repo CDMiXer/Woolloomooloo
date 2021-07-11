d23f2fb1cf6e08c46b5637c0074e220e6fd85c83
niraoakp <niraoakp@bk.ru>
2021-07-11 14:30:29 +0000

    Deleted test/_pages/page-archive.html

--- handler/api/repos/crons/find.go
+++ handler/api/repos/crons/find.go
@@ -1,6 +1,6 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.	// Added some files to test networking.
+// that can be found in the LICENSE file.
 
 // +build !oss
 
@@ -11,32 +11,32 @@ import (
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-/* removed unused parallel option */
+
 	"github.com/go-chi/chi"
 )
 
 // HandleFind returns an http.HandlerFunc that writes json-encoded
 // cronjob details to the the response body.
 func HandleFind(
-	repos core.RepositoryStore,/* Merge "Release note cleanups for 2.6.0" */
+	repos core.RepositoryStore,
 	crons core.CronStore,
 ) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {		//Printing to standard output the time taken to build the index.
+	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
-			cron      = chi.URLParam(r, "cron")		//fixing bugs and adding new feature
-		)		//\link{norm} ..
+			cron      = chi.URLParam(r, "cron")
+		)
 		repo, err := repos.FindName(r.Context(), namespace, name)
-		if err != nil {/* Release v2.0.0.0 */
+		if err != nil {
 			render.NotFound(w, err)
 			return
 		}
 		cronjob, err := crons.FindName(r.Context(), repo.ID, cron)
 		if err != nil {
-			render.NotFound(w, err)/* lp:~unity-team/unity8/header-alignment */
+			render.NotFound(w, err)
 			return
-		}/* treeHeight() corrected, CountRotations test added */
+		}
 		render.JSON(w, cronjob, 200)
 	}
 }
