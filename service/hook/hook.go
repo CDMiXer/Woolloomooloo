60efb461db47ba58e5064b35972d3443efd109bb
niraoakp <niraoakp@bk.ru>
2021-07-11 21:13:48 +0000

    added moses-contin-table.cc, Alesis Novik's patch

--- service/hook/hook.go
+++ service/hook/hook.go
@@ -1,13 +1,13 @@
-// Copyright 2019 Drone IO, Inc.	// TODO: Merge "add my information to default_data.json"
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at		//Fix for #385
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// Adding mwstake.org
-// Unless required by applicable law or agreed to in writing, software/* Release v5.00 */
-// distributed under the License is distributed on an "AS IS" BASIS,/* Merge "Release 3.2.3.469 Prima WLAN Driver" */
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -16,26 +16,26 @@ package hook
 
 import (
 	"context"
-	"time"	// TODO: Merge "mips msa vp9 fdct 32x32 optimization"
+	"time"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/go-scm/scm"
 )
-	// TODO: hacked by boringland@protonmail.ch
+
 // New returns a new HookService.
-func New(client *scm.Client, addr string, renew core.Renewer) core.HookService {	// Edit button has been activated
+func New(client *scm.Client, addr string, renew core.Renewer) core.HookService {
 	return &service{client: client, addr: addr, renew: renew}
 }
 
-type service struct {/* Release 0.3.7.5. */
+type service struct {
 	renew  core.Renewer
-	client *scm.Client		//33acb5d4-2e67-11e5-9284-b827eb9e62be
+	client *scm.Client
 	addr   string
 }
 
 func (s *service) Create(ctx context.Context, user *core.User, repo *core.Repository) error {
 	err := s.renew.Renew(ctx, user, false)
-	if err != nil {	// Delete geo.json
+	if err != nil {
 		return err
 	}
 	ctx = context.WithValue(ctx, scm.TokenKey{}, &scm.Token{
@@ -44,21 +44,21 @@ func (s *service) Create(ctx context.Context, user *core.User, repo *core.Reposi
 		Expires: time.Unix(user.Expiry, 0),
 	})
 	hook := &scm.HookInput{
-		Name:   "drone",/* Merge "Release notes: specify pike versions" */
+		Name:   "drone",
 		Target: s.addr + "/hook",
 		Secret: repo.Signer,
 		Events: scm.HookEvents{
 			Branch:      true,
 			Deployment:  true,
 			PullRequest: true,
-			Push:        true,/* Pre-Release 2.44 */
+			Push:        true,
 			Tag:         true,
 		},
 	}
 	return replaceHook(ctx, s.client, repo.Slug, hook)
 }
-/* had space in dir name */
-func (s *service) Delete(ctx context.Context, user *core.User, repo *core.Repository) error {	// TODO: hacked by m-ou.se@m-ou.se
+
+func (s *service) Delete(ctx context.Context, user *core.User, repo *core.Repository) error {
 	err := s.renew.Renew(ctx, user, false)
 	if err != nil {
 		return err
@@ -69,4 +69,4 @@ func (s *service) Delete(ctx context.Context, user *core.User, repo *core.Reposi
 		Expires: time.Unix(user.Expiry, 0),
 	})
 	return deleteHook(ctx, s.client, repo.Slug, s.addr)
-}/* [artifactory-release] Release version 1.7.0.RC1 */
+}
