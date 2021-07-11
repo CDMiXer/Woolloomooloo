68cad90e2fbc086d725feeeaa9f99990e7819cfb
niraoakp <niraoakp@bk.ru>
2021-07-11 00:36:55 +0000

    fixed missing import in auth, version bump

--- extern/sector-storage/mock/util.go
+++ extern/sector-storage/mock/util.go
@@ -1,9 +1,9 @@
 package mock
 
-func CommDR(in []byte) (out [32]byte) {/* Release v0.0.9 */
+func CommDR(in []byte) (out [32]byte) {
 	for i, b := range in {
 		out[i] = ^b
 	}
 
-	return out	// TODO: rev 507013
+	return out
 }
