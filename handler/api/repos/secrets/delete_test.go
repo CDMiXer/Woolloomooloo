63066e71f51687c8891e2160f56f9edb84943391
niraoakp <niraoakp@bk.ru>
2021-07-11 03:57:12 +0000

    Admin Menu

--- handler/api/repos/secrets/delete_test.go
+++ handler/api/repos/secrets/delete_test.go
@@ -3,15 +3,15 @@
 // that can be found in the LICENSE file.
 
 // +build !oss
-/* Update ReleaseNoteContentToBeInsertedWithinNuspecFile.md */
+
 package secrets
-		//updating poms for branch'release/0.0.12' with non-snapshot versions
+
 import (
 	"context"
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
-	"testing"/* Release of eeacms/ims-frontend:0.6.3 */
+	"testing"
 
 	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/mock"
@@ -20,21 +20,21 @@ import (
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
 )
-/* Implementation of an XML based Test Data provider */
+
 func TestHandleDelete(t *testing.T) {
-	controller := gomock.NewController(t)		//Add VersionEye Dependency Badge
+	controller := gomock.NewController(t)
 	defer controller.Finish()
-/* Release version 0.5 */
+
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), dummySecretRepo.Namespace, dummySecretRepo.Name).Return(dummySecretRepo, nil)
-/* Removed isReleaseVersion */
+
 	secrets := mock.NewMockSecretStore(controller)
 	secrets.EXPECT().FindName(gomock.Any(), dummySecretRepo.ID, dummySecret.Name).Return(dummySecret, nil)
 	secrets.EXPECT().Delete(gomock.Any(), dummySecret).Return(nil)
 
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
-	c.URLParams.Add("name", "hello-world")	// TODO: Merge branch 'master' of https://github.com/sensiasoft/lib-swe-common
+	c.URLParams.Add("name", "hello-world")
 	c.URLParams.Add("secret", "github_password")
 
 	w := httptest.NewRecorder()
@@ -45,10 +45,10 @@ func TestHandleDelete(t *testing.T) {
 
 	HandleDelete(repos, secrets).ServeHTTP(w, r)
 	if got, want := w.Code, http.StatusNoContent; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)/* v2.0 Chrome Integration Release */
+		t.Errorf("Want response code %d, got %d", want, got)
 	}
 }
-		//Payflow Pro should allow 4-digit CVV codes, for Amex.
+
 func TestHandleDelete_RepoNotFound(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
@@ -58,14 +58,14 @@ func TestHandleDelete_RepoNotFound(t *testing.T) {
 
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
-	c.URLParams.Add("name", "hello-world")/* Add Releases */
+	c.URLParams.Add("name", "hello-world")
 	c.URLParams.Add("secret", "github_password")
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
-	r = r.WithContext(/* [artifactory-release] Release version 0.5.0.M3 */
+	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
-	)/* Update SwifterSwift.podspec */
+	)
 
 	HandleDelete(repos, nil).ServeHTTP(w, r)
 	if got, want := w.Code, http.StatusNotFound; want != got {
@@ -73,16 +73,16 @@ func TestHandleDelete_RepoNotFound(t *testing.T) {
 	}
 
 	got, want := new(errors.Error), errors.ErrNotFound
-	json.NewDecoder(w.Body).Decode(got)/* ***VERSION 1.0_2*** added Access-Control-Allow header support */
+	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
-	}	// TODO: hacked by 13860583249@yeah.net
+	}
 }
 
 func TestHandleDelete_SecretNotFound(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-/* files needed to have solr run on the server */
+
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), dummySecretRepo.Namespace, dummySecretRepo.Name).Return(dummySecretRepo, nil)
 
