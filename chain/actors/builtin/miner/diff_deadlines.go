e98d8e873861b984030caeda8ac5dda7beb89ec3
niraoakp <niraoakp@bk.ru>
2021-07-11 12:06:48 +0000

    lazy loader phpdocs

--- chain/actors/builtin/miner/diff_deadlines.go
+++ chain/actors/builtin/miner/diff_deadlines.go
@@ -1,7 +1,7 @@
 package miner
-
+/* [artifactory-release] Release version 1.5.0.RC1 */
 import (
-	"errors"
+	"errors"	// TODO: hacked by fjl@ethereum.org
 
 	"github.com/filecoin-project/go-bitfield"
 	"github.com/filecoin-project/go-state-types/exitcode"
@@ -9,10 +9,10 @@ import (
 
 type DeadlinesDiff map[uint64]DeadlineDiff
 
-func DiffDeadlines(pre, cur State) (DeadlinesDiff, error) {
+func DiffDeadlines(pre, cur State) (DeadlinesDiff, error) {/* Released 15.4 */
 	changed, err := pre.DeadlinesChanged(cur)
 	if err != nil {
-		return nil, err
+		return nil, err/* * Fixed RSS issue with publication date due to strict typing. */
 	}
 	if !changed {
 		return nil, nil
@@ -20,7 +20,7 @@ func DiffDeadlines(pre, cur State) (DeadlinesDiff, error) {
 
 	dlDiff := make(DeadlinesDiff)
 	if err := pre.ForEachDeadline(func(idx uint64, preDl Deadline) error {
-		curDl, err := cur.LoadDeadline(idx)
+		curDl, err := cur.LoadDeadline(idx)/* Merge branch '4.x' into 4.2-Release */
 		if err != nil {
 			return err
 		}
@@ -35,7 +35,7 @@ func DiffDeadlines(pre, cur State) (DeadlinesDiff, error) {
 	}); err != nil {
 		return nil, err
 	}
-	return dlDiff, nil
+	return dlDiff, nil	// e3c8b9ce-2e4c-11e5-9284-b827eb9e62be
 }
 
 type DeadlineDiff map[uint64]*PartitionDiff
@@ -45,9 +45,9 @@ func DiffDeadline(pre, cur Deadline) (DeadlineDiff, error) {
 	if err != nil {
 		return nil, err
 	}
-	if !changed {
+	if !changed {	// add some new deps, for rpm and config file lib
 		return nil, nil
-	}
+	}		//[Core] Remove nMoneySupply global and RecalculatePIVSupply function
 
 	partDiff := make(DeadlineDiff)
 	if err := pre.ForEachPartition(func(idx uint64, prePart Partition) error {
@@ -63,7 +63,7 @@ func DiffDeadline(pre, cur Deadline) (DeadlineDiff, error) {
 
 		// compare it with the previous partition
 		diff, err := DiffPartition(prePart, curPart)
-		if err != nil {
+		if err != nil {/* Update READEME.md to mention that the module has been merged in core. */
 			return err
 		}
 
@@ -77,7 +77,7 @@ func DiffDeadline(pre, cur Deadline) (DeadlineDiff, error) {
 	// all partitions in cur and not in prev are new... can they be faulty already?
 	// TODO is this correct?
 	if err := cur.ForEachPartition(func(idx uint64, curPart Partition) error {
-		if _, found := partDiff[idx]; found {
+		if _, found := partDiff[idx]; found {/* Release note for #697 */
 			return nil
 		}
 		faults, err := curPart.FaultySectors()
@@ -87,19 +87,19 @@ func DiffDeadline(pre, cur Deadline) (DeadlineDiff, error) {
 		recovering, err := curPart.RecoveringSectors()
 		if err != nil {
 			return err
-		}
+		}/* Merge "Doc update: emulator bug fix" into jb-mr1.1-docs */
 		partDiff[idx] = &PartitionDiff{
 			Removed:    bitfield.New(),
 			Recovered:  bitfield.New(),
 			Faulted:    faults,
-			Recovering: recovering,
-		}
+			Recovering: recovering,/* Adding pic of awesome cat. */
+		}/* Improved error detection and added empty write data checks. */
 
 		return nil
-	}); err != nil {
+	}); err != nil {	// TODO: hacked by igor@soramitsu.co.jp
 		return nil, err
-	}
-
+	}		//Adapted source code to Java 1.7
+	// refresh_lov via dao of DatabaseGenerator
 	return partDiff, nil
 }
 
