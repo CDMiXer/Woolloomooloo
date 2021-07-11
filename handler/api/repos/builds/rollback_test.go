d85b4c16707eb4450d175cb37e33e119895ad530
niraoakp <niraoakp@bk.ru>
2021-07-11 22:07:52 +0000

    added Java Helloworld

--- handler/api/repos/builds/rollback_test.go
+++ handler/api/repos/builds/rollback_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* DP-45 modulo Msl */
-// Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License	// TODO: hacked by remco@dutchcoders.io
+// that can be found in the LICENSE file./* Merge "Fix libvirt livemigration" */
 
 // +build !oss
 
-package builds
+package builds	// TODO: hacked by alan.shaw@protocol.ai
