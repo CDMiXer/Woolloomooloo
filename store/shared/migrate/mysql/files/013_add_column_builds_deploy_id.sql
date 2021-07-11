340328ea2b16ed591ba19690819de8f29d12fefa
niraoakp <niraoakp@bk.ru>
2021-07-11 01:44:33 +0000

    trigger new build for ruby-head-clang (21bc6f9)

--- store/shared/migrate/mysql/files/013_add_column_builds_deploy_id.sql
+++ store/shared/migrate/mysql/files/013_add_column_builds_deploy_id.sql
@@ -1,3 +1,3 @@
--- name: alter-table-builds-add-column-deploy-id
+-- name: alter-table-builds-add-column-deploy-id		//add -stress-test-file cmd-line option for stress testing a single file 24 times
 
-ALTER TABLE builds ADD COLUMN build_deploy_id INTEGER NOT NULL DEFAULT 0;
+ALTER TABLE builds ADD COLUMN build_deploy_id INTEGER NOT NULL DEFAULT 0;/* Release Django Evolution 0.6.7. */
