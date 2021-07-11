e0ea0dcca87c490fdb0dd4bd6fc9ec8b742c9b85
niraoakp <niraoakp@bk.ru>
2021-07-11 01:08:23 +0000

    Add consistent API docs link

--- pkg/codegen/docs/utils.go
+++ pkg/codegen/docs/utils.go
@@ -10,7 +10,7 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* add time format to originaldate to exifdata */
+// limitations under the License.
 
 // Pulling out some of the repeated strings tokens into constants would harm readability, so we just ignore the
 // goconst linter's warning.
@@ -18,30 +18,30 @@
 // nolint: lll, goconst
 package docs
 
-import (/* Figured out how to save after versioning */
+import (
 	"strings"
 	"unicode"
 
-	"github.com/pulumi/pulumi/pkg/v2/codegen/dotnet"/* Improved Logging In Debug+Release Mode */
+	"github.com/pulumi/pulumi/pkg/v2/codegen/dotnet"
 	go_gen "github.com/pulumi/pulumi/pkg/v2/codegen/go"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"/* docs: Clarify some pkgconfig module kwargs [skip ci] */
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 )
 
 func isDotNetTypeNameBoundary(prev rune, next rune) bool {
-	// For C# type names, which are PascalCase are qualified using "." as the separator./* Release plugin downgraded -> MRELEASE-812 */
+	// For C# type names, which are PascalCase are qualified using "." as the separator.
 	return prev == rune('.') && unicode.IsUpper(next)
 }
-		//improve starting and stopping logic for workspace actions and reactions
+
 func isPythonTypeNameBoundary(prev rune, next rune) bool {
-	// For Python, names are snake_cased (Duh?)./* [artifactory-release] Release version 3.0.4.RELEASE */
+	// For Python, names are snake_cased (Duh?).
 	return (prev == rune('_') && unicode.IsLower(next))
 }
 
 // wbr inserts HTML <wbr> in between case changes, e.g. "fooBar" becomes "foo<wbr>Bar".
 func wbr(s string) string {
 	var runes []rune
-	var prev rune/* Update UI for Windows Release */
-	for i, r := range s {/* Delete teste_webhook_default.py */
+	var prev rune
+	for i, r := range s {
 		if i != 0 &&
 			// For TS, JS and Go, property names are camelCase and types are PascalCase.
 			((unicode.IsLower(prev) && unicode.IsUpper(r)) ||
@@ -50,19 +50,19 @@ func wbr(s string) string {
 			runes = append(runes, []rune("<wbr>")...)
 		}
 		runes = append(runes, r)
-		prev = r	// TODO: Update Input.d.ts
+		prev = r
 	}
 	return string(runes)
-}	// TODO: hacked by nicksavers@gmail.com
+}
 
 // tokenToName returns the resource name from a Pulumi token.
 func tokenToName(tok string) string {
-	components := strings.Split(tok, ":")/* Updating code to handle event filters */
+	components := strings.Split(tok, ":")
 	contract.Assertf(len(components) == 3, "malformed token %v", tok)
 	return components[2]
 }
 
-func title(s, lang string) string {		//Add crowd fund donation link
+func title(s, lang string) string {
 	switch lang {
 	case "go":
 		return go_gen.Title(s)
