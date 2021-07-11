fca027555012063cb931903518a2ac5feac42030
niraoakp <niraoakp@bk.ru>
2021-07-11 08:51:15 +0000

    - Add: Carminat Tomtom support

--- tests/examples/formattable/index.ts
+++ tests/examples/formattable/index.ts
@@ -1,4 +1,4 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+/* Added file for Nedim Haveric */
 console.log("Here's some format goo: %s, %v, %q, %d!");
 console.error("Here's some format goo: %s, %v, %q, %d!");
