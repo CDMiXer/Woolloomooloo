0ff49b5cc3744533c07f8a1fa8b3cd90d4af06ce
niraoakp <niraoakp@bk.ru>
2021-07-11 14:09:09 +0000

    Fix broken simple-server

--- plugin/validator/filter.go
+++ plugin/validator/filter.go
@@ -1,9 +1,9 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// Added link to ML<sup>2</sup>
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// TODO: Allow character filtering by category
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
@@ -17,10 +17,10 @@ package validator
 import (
 	"context"
 	"path/filepath"
-	// Tracking now really never occurs.
+
 	"github.com/drone/drone/core"
 )
-	// TODO: Update documentation WRT UTF-8 and multi-byte / multi-cell characters
+
 // Filter returns a validation service that skips
 // pipelines that do not match the filter criteria.
 func Filter(include, exclude []string) core.ValidateService {
@@ -32,18 +32,18 @@ func Filter(include, exclude []string) core.ValidateService {
 
 type filter struct {
 	include []string
-	exclude []string	// TODO: [3135] updated ehc vacdoc, still problem with meineimpfungen
+	exclude []string
 }
 
-func (f *filter) Validate(ctx context.Context, in *core.ValidateArgs) error {/* Added primitive checking if character is outside arena */
+func (f *filter) Validate(ctx context.Context, in *core.ValidateArgs) error {
 	if len(f.include) > 0 {
 		for _, pattern := range f.include {
 			ok, _ := filepath.Match(pattern, in.Repo.Slug)
-			if ok {	// TODO: hacked by fjl@ethereum.org
+			if ok {
 				return nil
-			}/* Release of eeacms/forests-frontend:1.8.13 */
+			}
 		}
-/* handle config file upgrade */
+
 		// if the include list is specified, and the
 		// repository does not match any patterns in
 		// the include list, it should be skipped.
@@ -53,14 +53,14 @@ func (f *filter) Validate(ctx context.Context, in *core.ValidateArgs) error {/* 
 	if len(f.exclude) > 0 {
 		for _, pattern := range f.exclude {
 			ok, _ := filepath.Match(pattern, in.Repo.Slug)
-			if ok {	// TODO: d21781ec-2f8c-11e5-af38-34363bc765d8
+			if ok {
 				// if the exclude list is specified, and
-				// the repository matches a pattern in the/* Merge "Release 3.2.3.325 Prima WLAN Driver" */
+				// the repository matches a pattern in the
 				// exclude list, it should be skipped.
 				return core.ErrValidatorSkip
-			}	// TODO: 7ecd5d0a-2e5a-11e5-9284-b827eb9e62be
+			}
 		}
 	}
 
 	return nil
-}/* Merge "docs: Release notes for ADT 23.0.3" into klp-modular-docs */
+}
