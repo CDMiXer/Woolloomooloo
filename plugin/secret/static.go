5451f373c3826ae68632ba757503df5c818372a4
niraoakp <niraoakp@bk.ru>
2021-07-11 10:56:21 +0000

    start refactor: now pg props have a dedicated step

--- plugin/secret/static.go
+++ plugin/secret/static.go
@@ -1,16 +1,16 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* First Release. */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Update ReleaseNotes in Module Manifest */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.		//- cleanup and documentation
+// limitations under the License.
 
 package secret
 
@@ -32,7 +32,7 @@ type staticController struct {
 
 func (c *staticController) Find(ctx context.Context, in *core.SecretArgs) (*core.Secret, error) {
 	for _, secret := range c.secrets {
-		if !strings.EqualFold(secret.Name, in.Name) {		//Revert FindBugs threshold back to High
+		if !strings.EqualFold(secret.Name, in.Name) {
 			continue
 		}
 		// The secret can be restricted to non-pull request
@@ -40,8 +40,8 @@ func (c *staticController) Find(ctx context.Context, in *core.SecretArgs) (*core
 		// empty results.
 		if secret.PullRequest == false &&
 			in.Build.Event == core.EventPullRequest {
-			continue/* 8572f470-2e51-11e5-9284-b827eb9e62be */
-		}/* Update from Release 0 to Release 1 */
+			continue
+		}
 		return secret, nil
 	}
 	return nil, nil
