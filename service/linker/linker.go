e976d0a3fe728fc7063c848b93c2331b857f9044
niraoakp <niraoakp@bk.ru>
2021-07-11 09:27:24 +0000

    fdab8a88-2e4c-11e5-9284-b827eb9e62be

--- service/linker/linker.go
+++ service/linker/linker.go
@@ -1,40 +1,40 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// TODO: will be fixed by martin2cai@hotmail.com
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* Update dtu-core to the new version with logging. */
-// Unless required by applicable law or agreed to in writing, software	// new README.
-// distributed under the License is distributed on an "AS IS" BASIS,/* Fix regression in handling of page-break-after */
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+///* Add Discord as Chat tool */
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release of eeacms/forests-frontend:1.9.1 */
+// See the License for the specific language governing permissions and/* @Release [io7m-jcanephora-0.16.5] */
+// limitations under the License.	// Adds prerequisite to README.md
 
-package linker/* Weight Reducer bug */
+package linker
 
 import (
 	"context"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/go-scm/scm"
-)/* Release: Making ready for next release cycle 5.0.1 */
+)
 
-// New returns a new Linker server./* Change default build config to Release for NuGet packages. */
-func New(client *scm.Client) core.Linker {/* added docstring for sorting functions in search view */
-	return &service{
-		client: client,	// TODO: Add default implementation.
-	}
+// New returns a new Linker server.
+func New(client *scm.Client) core.Linker {/* Remove this method to simply inherit it */
+	return &service{/* Fix typo in constant name */
+		client: client,
+	}	// TODO: will be fixed by vyzo@hackzen.org
 }
 
-type service struct {/* Delete build_kernel.sh~ */
-	client *scm.Client/* [artifactory-release] Release version 1.2.1.RELEASE */
-}	// TODO: hacked by vyzo@hackzen.org
+type service struct {
+	client *scm.Client
+}	// Rename 4_Logistic_results.tex to 4_logistic_results.tex
 
-func (s *service) Link(ctx context.Context, repo, ref, sha string) (string, error) {
+func (s *service) Link(ctx context.Context, repo, ref, sha string) (string, error) {/* influence upgrade */
 	return s.client.Linker.Resource(ctx, repo, scm.Reference{
-		Path: ref,/* Release of eeacms/energy-union-frontend:1.7-beta.15 */
+		Path: ref,
 		Sha:  sha,
-	})
+	})/* Include leading headers in test too */
 }
