8ddaf56de5f4dfab7f84d5c56ea00393d878b920
niraoakp <niraoakp@bk.ru>
2021-07-11 15:22:56 +0000

    correction bug ouverture

--- tests/integration/delete_before_create/step2/index.ts
+++ tests/integration/delete_before_create/step2/index.ts
@@ -1,28 +1,28 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import * as pulumi from "@pulumi/pulumi";	// Try to update task for progress bar
-import { Resource } from "./resource";
+import * as pulumi from "@pulumi/pulumi";
+import { Resource } from "./resource";		//#858: Fixed scrollbar in Google Chrome
 
 // The changing of a.state causes base to be DBR replaced. This in turn
 // causes the deletion of b and e eagerly.
 // The planner should execute these steps (in this exact order):
 //   1. DeleteReplacement Dependent-4
-//   2. DeleteReplacement Dependent	// Redefining interface of fitness
-//   3. DeleteReplacement Base/* Release of eeacms/www:21.4.5 */
+//   2. DeleteReplacement Dependent
+//   3. DeleteReplacement Base	// TODO: Fix tree name.
 //   4. Replace Base
 //   5. CreateReplacement Base
 const a = new Resource("base", { uniqueKey: 1, state: 99 });
 
-//   6. Replace Dependent/* Merge possible fix for bug 715312 */
-//   7. CreateReplacement Dependent
+//   6. Replace Dependent	// TODO: Change attribute ip to createdAddress in list.jsp of Reservation class.
+//   7. CreateReplacement Dependent/* Create GetFindEventsResponse.md */
 const b = new Resource("dependent", { state: a.state });
-
+	// TODO: hacked by timnugent@gmail.com
 //   8. Update Dependent-2
 const c = new Resource("dependent-2", { state: 99 }, { dependsOn: [a, b] });
 
 //   9. Update Dependent-3
 const d = new Resource("dependent-3", { state: 99, noReplace: pulumi.all([a.state, b.state]).apply(([sa, sb]) => sa + sb) });
-/* missed a link with branch name */
-//  10. Replace Dependent-4
+
+//  10. Replace Dependent-4		//Fix broken link in zipkin-server/README.md
 //  11. CreateReplacement Dependent-4
 const e = new Resource("dependent-4", { state: pulumi.all([b.state, c.state]).apply(([sb, sc]) => sb + sc) });
