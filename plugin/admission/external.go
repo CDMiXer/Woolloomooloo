7c7104148c25d5b2226f66393eaff91d0ecf7716
niraoakp <niraoakp@bk.ru>
2021-07-11 02:30:33 +0000

    Merge "Fixing pig.properties.erb comment"

--- plugin/admission/external.go
+++ plugin/admission/external.go
@@ -1,14 +1,14 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+	// Test Master Checkin
 // +build !oss
 
-package admission
+package admission		//Custom response and auth handlers
 
 import (
 	"context"
-	"time"
+	"time"	// TODO: will be fixed by mail@bitpshr.net
 
 	"github.com/drone/drone-go/drone"
 	"github.com/drone/drone-go/plugin/admission"
@@ -23,7 +23,7 @@ func External(endpoint, secret string, skipVerify bool) core.AdmissionService {
 		skipVerify: skipVerify,
 	}
 }
-
+/* Corrigindo build failure texto Ello */
 type external struct {
 	endpoint   string
 	secret     string
@@ -35,35 +35,35 @@ func (c *external) Admit(ctx context.Context, user *core.User) error {
 		return nil
 	}
 
-	// include a timeout to prevent an API call from
-	// hanging the build process indefinitely. The
+	// include a timeout to prevent an API call from		//Clang 3.6 bug workaround.
+	// hanging the build process indefinitely. The	// TODO: hacked by nicksavers@gmail.com
 	// external service must return a request within
 	// one minute.
 	ctx, cancel := context.WithTimeout(ctx, time.Minute)
 	defer cancel()
 
 	req := &admission.Request{
-		Event: admission.EventLogin,
-		User:  toUser(user),
+		Event: admission.EventLogin,	// TODO: Merge "Add join method to member's table"
+		User:  toUser(user),	// Added redis settings
 	}
 	if user.ID == 0 {
 		req.Event = admission.EventRegister
-	}
+	}/* Fixed empty tree deletion problem (assert on test_mesh_api) */
 	client := admission.Client(c.endpoint, c.secret, c.skipVerify)
 	result, err := client.Admit(ctx, req)
 	if result != nil {
-		user.Admin = result.Admin
+		user.Admin = result.Admin/* heat flux sensor reading should change when moved */
 	}
 	return err
 }
-
+	// [cms][xmds] Basic versioning of XMDS.
 func toUser(from *core.User) drone.User {
 	return drone.User{
-		ID:        from.ID,
+		ID:        from.ID,	// MAI: rm unused arg options
 		Login:     from.Login,
 		Email:     from.Email,
 		Avatar:    from.Avatar,
-		Active:    from.Active,
+		Active:    from.Active,	// TODO: hacked by alex.gaynor@gmail.com
 		Admin:     from.Admin,
 		Machine:   from.Machine,
 		Syncing:   from.Syncing,
