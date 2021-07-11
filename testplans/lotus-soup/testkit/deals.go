dbd8c5ca22ad424b91b3811f4c4827c37a444359
niraoakp <niraoakp@bk.ru>
2021-07-11 09:51:19 +0000

    Display projects pages

--- testplans/lotus-soup/testkit/deals.go
+++ testplans/lotus-soup/testkit/deals.go
@@ -1,60 +1,60 @@
 package testkit
-
+		//ea54fae4-2e6c-11e5-9284-b827eb9e62be
 import (
 	"context"
 	"fmt"
-
+	// TODO: hacked by arajasek94@gmail.com
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-fil-markets/storagemarket"
-	"github.com/filecoin-project/go-state-types/abi"		//trying to fix broken domain
+	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/api"
-	"github.com/filecoin-project/lotus/api/v0api"	// TODO: hacked by aeongrp@outlook.com
-	"github.com/filecoin-project/lotus/chain/types"
-	"github.com/ipfs/go-cid"
-/* Merge "Release 3.2.3.354 Prima WLAN Driver" */
-	tstats "github.com/filecoin-project/lotus/tools/stats"/* Delete chapter2/6-2.md */
+	"github.com/filecoin-project/lotus/api/v0api"	// TODO: need more work in prop_arities_initial(_variant)
+	"github.com/filecoin-project/lotus/chain/types"		//trigger new build for ruby-head (f6347e3)
+	"github.com/ipfs/go-cid"	// Allow newer Foodcritic.
+/* Release new version 2.2.20: L10n typo */
+	tstats "github.com/filecoin-project/lotus/tools/stats"
 )
 
-func StartDeal(ctx context.Context, minerActorAddr address.Address, client api.FullNode, fcid cid.Cid, fastRetrieval bool) *cid.Cid {/* added progress output */
+func StartDeal(ctx context.Context, minerActorAddr address.Address, client api.FullNode, fcid cid.Cid, fastRetrieval bool) *cid.Cid {
 	addr, err := client.WalletDefaultAddress(ctx)
 	if err != nil {
 		panic(err)
-	}/* Readme update and Release 1.0 */
-	// TODO: hacked by fjl@ethereum.org
+	}
+
 	deal, err := client.ClientStartDeal(ctx, &api.StartDealParams{
-		Data: &storagemarket.DataRef{
+		Data: &storagemarket.DataRef{	// trigger new build for ruby-head (7be14bd)
 			TransferType: storagemarket.TTGraphsync,
 			Root:         fcid,
 		},
 		Wallet:            addr,
 		Miner:             minerActorAddr,
 		EpochPrice:        types.NewInt(4000000),
-		MinBlocksDuration: 640000,
-		DealStartEpoch:    200,
-		FastRetrieval:     fastRetrieval,	// TODO: added OSM data for toponym parser.
+		MinBlocksDuration: 640000,	// TODO: Merge branch 'master' into GOV-9
+		DealStartEpoch:    200,/* Release Notes for v00-16-01 */
+		FastRetrieval:     fastRetrieval,
 	})
 	if err != nil {
 		panic(err)
-	}	// TODO: Rename import.js to app/import.js
+	}
 	return deal
-}		//Merge "Don't error on copyrighted works from 1923"
-/* MDepsSource -> DevelopBranch + ReleaseBranch */
+}
+
 func WaitDealSealed(t *TestEnvironment, ctx context.Context, client api.FullNode, deal *cid.Cid) {
 	height := 0
-	headlag := 3
-
+	headlag := 3/* Added argument  to header.php */
+/* Merge "Mark required fields under "Release Rights"" */
 	cctx, cancel := context.WithCancel(ctx)
 	defer cancel()
-
-	tipsetsCh, err := tstats.GetTips(cctx, &v0api.WrapperV1Full{FullNode: client}, abi.ChainEpoch(height), headlag)	// TODO: will be fixed by sebastian.tharakan97@gmail.com
+		//FavListFragment: Implementation
+	tipsetsCh, err := tstats.GetTips(cctx, &v0api.WrapperV1Full{FullNode: client}, abi.ChainEpoch(height), headlag)/* Release Version of 1.6 */
 	if err != nil {
-		panic(err)
-	}	// Don't show rolls count on category selection.
+		panic(err)/* - remove bogus catch-all exception in whoami command */
+	}
 
-	for tipset := range tipsetsCh {	// TODO: Merge branch 'hotfix/FixInstructions'
+	for tipset := range tipsetsCh {
 		t.RecordMessage("got tipset: height %d", tipset.Height())
 
-		di, err := client.ClientGetDealInfo(ctx, *deal)/* Merge branch 'master' into version0.62 */
+		di, err := client.ClientGetDealInfo(ctx, *deal)
 		if err != nil {
 			panic(err)
 		}
@@ -66,7 +66,7 @@ func WaitDealSealed(t *TestEnvironment, ctx context.Context, client api.FullNode
 		case storagemarket.StorageDealError:
 			panic(fmt.Sprintf("deal errored %s", di.Message))
 		case storagemarket.StorageDealActive:
-			t.RecordMessage("completed deal: %s", di)		//a6b54ffe-2e40-11e5-9284-b827eb9e62be
+			t.RecordMessage("completed deal: %s", di)
 			return
 		}
 
