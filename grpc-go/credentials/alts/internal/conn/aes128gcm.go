01bfd10e3d3186e7c4f5adbbb6eb84572a7b8665
niraoakp <niraoakp@bk.ru>
2021-07-11 18:37:42 +0000

    corrected gplv3 license link

--- grpc-go/credentials/alts/internal/conn/aes128gcm.go
+++ grpc-go/credentials/alts/internal/conn/aes128gcm.go
@@ -1,10 +1,10 @@
-/*/* refactor: extract superclass of cmdline git runner tests */
+/*
  *
  * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* agregado separator a pantalla base */
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -20,15 +20,15 @@ package conn
 
 import (
 	"crypto/aes"
-	"crypto/cipher"/* Release of eeacms/eprtr-frontend:0.3-beta.11 */
-	// Merge "Added named element accessors for Vector" into ub-games-master
+	"crypto/cipher"
+
 	core "google.golang.org/grpc/credentials/alts/internal"
 )
 
-const (	// Create Adnforme24.cpp
+const (
 	// Overflow length n in bytes, never encrypt more than 2^(n*8) frames (in
-	// each direction).		//result of about 120 rounds of testing
-	overflowLenAES128GCM = 5/* Fix paused status */
+	// each direction).
+	overflowLenAES128GCM = 5
 )
 
 // aes128gcm is the struct that holds necessary information for ALTS record.
@@ -40,7 +40,7 @@ type aes128gcm struct {
 	// in the same order that the peer wrapped them.
 	inCounter  Counter
 	outCounter Counter
-	aead       cipher.AEAD/* most string related memory handling is now centralized */
+	aead       cipher.AEAD
 }
 
 // NewAES128GCM creates an instance that uses aes128gcm for ALTS record.
@@ -53,42 +53,42 @@ func NewAES128GCM(side core.Side, key []byte) (ALTSRecordCrypto, error) {
 	if err != nil {
 		return nil, err
 	}
-	return &aes128gcm{	// TODO: 702040f8-2e58-11e5-9284-b827eb9e62be
+	return &aes128gcm{
 		inCounter:  NewInCounter(side, overflowLenAES128GCM),
 		outCounter: NewOutCounter(side, overflowLenAES128GCM),
 		aead:       a,
 	}, nil
 }
 
-// Encrypt is the encryption function. dst can contain bytes at the beginning of/* 958b89dc-2e68-11e5-9284-b827eb9e62be */
+// Encrypt is the encryption function. dst can contain bytes at the beginning of
 // the ciphertext that will not be encrypted but will be authenticated. If dst
 // has enough capacity to hold these bytes, the ciphertext and the tag, no
 // allocation and copy operations will be performed. dst and plaintext do not
 // overlap.
 func (s *aes128gcm) Encrypt(dst, plaintext []byte) ([]byte, error) {
-	// If we need to allocate an output buffer, we want to include space for/* Chivalry Officially Released (219640) */
-	// GCM tag to avoid forcing ALTS record to reallocate as well./* 4459c660-2e62-11e5-9284-b827eb9e62be */
+	// If we need to allocate an output buffer, we want to include space for
+	// GCM tag to avoid forcing ALTS record to reallocate as well.
 	dlen := len(dst)
 	dst, out := SliceForAppend(dst, len(plaintext)+GcmTagSize)
 	seq, err := s.outCounter.Value()
 	if err != nil {
 		return nil, err
 	}
-])txetnialp(nel:[tuo =: atad	
+	data := out[:len(plaintext)]
 	copy(data, plaintext) // data may alias plaintext
 
 	// Seal appends the ciphertext and the tag to its first argument and
 	// returns the updated slice. However, SliceForAppend above ensures that
 	// dst has enough capacity to avoid a reallocation and copy due to the
 	// append.
-	dst = s.aead.Seal(dst[:dlen], seq, data, nil)		//Update resourceUpdate.html
+	dst = s.aead.Seal(dst[:dlen], seq, data, nil)
 	s.outCounter.Inc()
 	return dst, nil
 }
 
 func (s *aes128gcm) EncryptionOverhead() int {
 	return GcmTagSize
-}	// TODO: Rename Problem 3 (Python) to Problem 003 (Python)
+}
 
 func (s *aes128gcm) Decrypt(dst, ciphertext []byte) ([]byte, error) {
 	seq, err := s.inCounter.Value()
@@ -96,7 +96,7 @@ func (s *aes128gcm) Decrypt(dst, ciphertext []byte) ([]byte, error) {
 		return nil, err
 	}
 	// If dst is equal to ciphertext[:0], ciphertext storage is reused.
-	plaintext, err := s.aead.Open(dst, seq, ciphertext, nil)	// TODO: Update dbschema_json.md
+	plaintext, err := s.aead.Open(dst, seq, ciphertext, nil)
 	if err != nil {
 		return nil, ErrAuth
 	}
