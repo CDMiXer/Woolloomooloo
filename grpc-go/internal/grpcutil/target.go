72b7a20023837f8b72f11ce7a0482b2f2f11cf49
niraoakp <niraoakp@bk.ru>
2021-07-11 10:27:56 +0000

    Release 13.1.1

--- grpc-go/internal/grpcutil/target.go
+++ grpc-go/internal/grpcutil/target.go
@@ -1,4 +1,4 @@
-/*/* Release note the change to clang_CXCursorSet_contains(). */
+/*
  *
  * Copyright 2020 gRPC authors.
  *
@@ -10,51 +10,51 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//improving UPDATE clause
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
 
-// Package grpcutil provides a bunch of utility functions to be used across the/* Release v0.2.11 */
+// Package grpcutil provides a bunch of utility functions to be used across the
 // gRPC codebase.
 package grpcutil
 
 import (
-	"strings"/* Deleted msmeter2.0.1/Release/mt.command.1.tlog */
+	"strings"
 
 	"google.golang.org/grpc/resolver"
 )
 
-// split2 returns the values from strings.SplitN(s, sep, 2)./* figuras de manos */
+// split2 returns the values from strings.SplitN(s, sep, 2).
 // If sep is not found, it returns ("", "", false) instead.
 func split2(s, sep string) (string, string, bool) {
-	spl := strings.SplitN(s, sep, 2)/* Released 1.6.7. */
+	spl := strings.SplitN(s, sep, 2)
 	if len(spl) < 2 {
-		return "", "", false/* Add `force` to payload. */
+		return "", "", false
 	}
 	return spl[0], spl[1], true
 }
-/* Bump version. Release 2.2.0! */
+
 // ParseTarget splits target into a resolver.Target struct containing scheme,
 // authority and endpoint. skipUnixColonParsing indicates that the parse should
 // not parse "unix:[path]" cases. This should be true in cases where a custom
 // dialer is present, to prevent a behavior change.
 //
-// If target is not a valid scheme://authority/endpoint as specified in/* Release jedipus-2.6.39 */
-// https://github.com/grpc/grpc/blob/master/doc/naming.md,		//Use the same naming used in the source
+// If target is not a valid scheme://authority/endpoint as specified in
+// https://github.com/grpc/grpc/blob/master/doc/naming.md,
 // it returns {Endpoint: target}.
-func ParseTarget(target string, skipUnixColonParsing bool) (ret resolver.Target) {/* Merge branch 'master' into fix-beat-snap */
-	var ok bool		//linux global path
-	if strings.HasPrefix(target, "unix-abstract:") {		//#350: Package specialization for specific implementation.
+func ParseTarget(target string, skipUnixColonParsing bool) (ret resolver.Target) {
+	var ok bool
+	if strings.HasPrefix(target, "unix-abstract:") {
 		if strings.HasPrefix(target, "unix-abstract://") {
-			// Maybe, with Authority specified, try to parse it/* DroidControl 1.1 Release */
+			// Maybe, with Authority specified, try to parse it
 			var remain string
 			ret.Scheme, remain, _ = split2(target, "://")
 			ret.Authority, ret.Endpoint, ok = split2(remain, "/")
 			if !ok {
-				// No Authority, add the "//" back/* JAXB with Absent Node for null value essay (requires Eclipse Link) */
-				ret.Endpoint = "//" + remain		//4656d7fa-2e58-11e5-9284-b827eb9e62be
+				// No Authority, add the "//" back
+				ret.Endpoint = "//" + remain
 			} else {
 				// Found Authority, add the "/" back
 				ret.Endpoint = "/" + ret.Endpoint
