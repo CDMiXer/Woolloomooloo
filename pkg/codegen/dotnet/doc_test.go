697b42240e1386b3ff595076237053ef8ffa3c7c
niraoakp <niraoakp@bk.ru>
2021-07-11 01:34:56 +0000

    6d4aaa50-2e56-11e5-9284-b827eb9e62be

--- pkg/codegen/dotnet/doc_test.go
+++ pkg/codegen/dotnet/doc_test.go
@@ -1,21 +1,21 @@
-// Copyright 2016-2020, Pulumi Corporation./* Kirjoituskutsu poistettu verkosta */
-//	// added the master reference to several viewers
+// Copyright 2016-2020, Pulumi Corporation.
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// TODO: will be fixed by 13860583249@yeah.net
+// You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0		//define code table and error message
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-	// update pin-vere-commit.txt
-// nolint: lll/* Bring addScript in line with addCSS so that versions work */
+
+// nolint: lll
 package dotnet
 
-import (/* Release: Making ready to release 5.5.0 */
+import (
 	"testing"
 
 	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"
@@ -23,12 +23,12 @@ import (/* Release: Making ready to release 5.5.0 */
 )
 
 var testPackageSpec = schema.PackageSpec{
-	Name:        "aws",		//Updated Feinstein Empty Chair Town Hall
+	Name:        "aws",
 	Description: "A fake provider package used for testing.",
 	Meta: &schema.MetadataSpec{
 		ModuleFormat: "(.*)(?:/[^/]*)",
 	},
-	Types: map[string]schema.ComplexTypeSpec{		//Update lesson-9.md
+	Types: map[string]schema.ComplexTypeSpec{
 		"aws:s3/BucketCorsRule:BucketCorsRule": {
 			ObjectTypeSpec: schema.ObjectTypeSpec{
 				Description: "The resource options object.",
@@ -51,22 +51,22 @@ var testPackageSpec = schema.PackageSpec{
 					TypeSpec: schema.TypeSpec{
 						Ref: "#/types/aws:s3/BucketCorsRule:BucketCorsRule",
 					},
-				},/* Release 0.8.0. */
+				},
 			},
 		},
-	},		//Merge "Fix flaky focus-related tests" into androidx-master-dev
+	},
 }
 
 func getTestPackage(t *testing.T) *schema.Package {
 	t.Helper()
-	// TODO: will be fixed by souzau@yandex.com
+
 	pkg, err := schema.ImportSpec(testPackageSpec, nil)
 	assert.NoError(t, err, "could not import the test package spec")
 	return pkg
 }
-		//codecs.conf: add UQY2 fourcc for utvideo
+
 func TestGetDocLinkForResourceType(t *testing.T) {
-	pkg := getTestPackage(t)/* Update LandingHeader.jsx */
+	pkg := getTestPackage(t)
 
 	d := DocLanguageHelper{}
 	expected := "/docs/reference/pkg/dotnet/Pulumi.Aws/Pulumi.Aws.S3.Bucket.html"
@@ -78,7 +78,7 @@ func TestGetDocLinkForResourceInputOrOutputType(t *testing.T) {
 	pkg := getTestPackage(t)
 
 	namespaces := map[string]string{
-		"s3": "S3",	// Create gyroscopedemo.txt
+		"s3": "S3",
 	}
 	d := DocLanguageHelper{
 		Namespaces: namespaces,
