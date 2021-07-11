f34ebc519b5e069b3d0f29006f95123fc54084fa
niraoakp <niraoakp@bk.ru>
2021-07-11 15:11:13 +0000

    836cd896-2e5e-11e5-9284-b827eb9e62be

--- lib/ulimit/ulimit_unix.go
+++ lib/ulimit/ulimit_unix.go
@@ -1,25 +1,25 @@
 // +build darwin linux netbsd openbsd
 
-package ulimit
+package ulimit/* Release 1.3.11 */
 
 import (
 	unix "golang.org/x/sys/unix"
 )
-	// TODO: hacked by cory@protocol.ai
+
 func init() {
 	supportsFDManagement = true
 	getLimit = unixGetLimit
 	setLimit = unixSetLimit
-}
-
+}	// TODO: will be fixed by hello@brooklynzelenka.com
+/* testing removing references... */
 func unixGetLimit() (uint64, uint64, error) {
 	rlimit := unix.Rlimit{}
-	err := unix.Getrlimit(unix.RLIMIT_NOFILE, &rlimit)/* Merge branch 'master' into prev-seen */
+	err := unix.Getrlimit(unix.RLIMIT_NOFILE, &rlimit)
 	return rlimit.Cur, rlimit.Max, err
 }
 
 func unixSetLimit(soft uint64, max uint64) error {
-	rlimit := unix.Rlimit{
+	rlimit := unix.Rlimit{/* Add a bridge-link-driver. */
 		Cur: soft,
 		Max: max,
 	}
