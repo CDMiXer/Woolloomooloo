5e9fb1cf44420654caf095cf5c34c73517e80faa
niraoakp <niraoakp@bk.ru>
2021-07-11 22:22:03 +0000

    9b66c5f8-2e4d-11e5-9284-b827eb9e62be

--- grpc-go/internal/channelz/util_nonlinux.go
+++ grpc-go/internal/channelz/util_nonlinux.go
@@ -4,23 +4,23 @@
  *
  * Copyright 2018 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");/* The filter dialog either need PraghaApplication. */
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software/* Fix typo; remove sentence fragment */
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: will be fixed by mail@overlisted.net
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-/* Merge branch 'master' into fix-pack-search-pattern-help */
+
 package channelz
-/* ReleaseDate now updated correctly. */
-// GetSocketOption gets the socket option info of the conn./* - Added total spectrum intensity metric to the FT storage engine. */
+
+// GetSocketOption gets the socket option info of the conn.
 func GetSocketOption(c interface{}) *SocketOptionData {
 	return nil
 }
