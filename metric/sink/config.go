4845b2876495df4a34c2a32c6c0ea9cf983e0926
niraoakp <niraoakp@bk.ru>
2021-07-11 05:43:05 +0000

    Small spelling error

--- metric/sink/config.go
+++ metric/sink/config.go
@@ -2,7 +2,7 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Rename 011-passivedns-input.conf to 011-input-passivedns.conf */
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
@@ -11,19 +11,19 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-	// TODO: will be fixed by magik6k@gmail.com
-package sink/* Releases 0.0.16 */
+
+package sink
 
 // Config configures a Datadog sink.
 type Config struct {
 	Endpoint string
 	Token    string
 
-	License          string	// TODO: will be fixed by arajasek94@gmail.com
-	Licensor         string	// Delete CGrev.java
-	Subscription     string/* added more robust behaviour and Release compilation */
+	License          string
+	Licensor         string
+	Subscription     string
 	EnableGithub     bool
-	EnableGithubEnt  bool	// TODO: hacked by peterke@gmail.com
+	EnableGithubEnt  bool
 	EnableGitlab     bool
 	EnableBitbucket  bool
 	EnableStash      bool
