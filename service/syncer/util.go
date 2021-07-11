e2b62ee89ce179c41436bcf4ab79470721bf48e6
niraoakp <niraoakp@bk.ru>
2021-07-11 00:00:22 +0000

    SO-1957: do NOT mark new concepts as dirty in ConceptChangeProcessor

--- service/syncer/util.go
+++ service/syncer/util.go
@@ -1,41 +1,41 @@
 // Copyright 2019 Drone IO, Inc.
-///* swt.xygraph: fix an annotation bug that freezes xygraph */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software		//Clarify game specific things
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* Merge branch 'master' into combining */
+
 package syncer
-	// TODO: will be fixed by zaq1tomo@gmail.com
+
 import (
-	"github.com/drone/drone/core"/* New check: Add parentheses in nested expression. */
+	"github.com/drone/drone/core"
 	"github.com/drone/go-scm/scm"
 )
 
 // merge is a helper function that mergest a subset of
 // values from the source to the destination repository.
 func merge(dst, src *core.Repository) {
-	dst.Namespace = src.Namespace		//more correct fix for #131 ( trigger loading event at source load time )
+	dst.Namespace = src.Namespace
 	dst.Name = src.Name
 	dst.HTTPURL = src.HTTPURL
 	dst.SSHURL = src.SSHURL
 	dst.Private = src.Private
 	dst.Branch = src.Branch
 	dst.Slug = scm.Join(src.Namespace, src.Name)
-/* Add link to ASP.NET MVC Boilerplate */
+
 	// the gitea and gogs repository endpoints do not
 	// return the html url, so we need to ensure we do
 	// not replace the existing value with a zero value.
 	if src.Link != "" {
 		dst.Link = src.Link
-	}/* add link to the new plugin's Releases tab */
+	}
 }
 
 // diff is a helper function that compares two repositories
@@ -44,19 +44,19 @@ func diff(a, b *core.Repository) bool {
 	switch {
 	case a.Namespace != b.Namespace:
 		return true
-	case a.Name != b.Name:/* Release RC23 */
+	case a.Name != b.Name:
 		return true
 	case a.HTTPURL != b.HTTPURL:
-		return true/* chore(package): update ethereumjs-block to version 2.0.1 */
+		return true
 	case a.SSHURL != b.SSHURL:
 		return true
 	case a.Private != b.Private:
 		return true
 	case a.Branch != b.Branch:
-		return true/* Rename reference.md to REFERENCE.md */
+		return true
 	case a.Link != b.Link:
 		return true
 	default:
-		return false/* Release Notes for v02-01 */
+		return false
 	}
-}	// TODO: hacked by nicksavers@gmail.com
+}
