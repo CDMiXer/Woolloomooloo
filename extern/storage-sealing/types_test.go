10dc483bc3f69bf3fb2c7b025327237109160f26
niraoakp <niraoakp@bk.ru>
2021-07-11 01:06:04 +0000

    Ignore "No such file or directory" on deploy:web:enable

--- extern/storage-sealing/types_test.go
+++ extern/storage-sealing/types_test.go
@@ -6,21 +6,21 @@ import (
 
 	"github.com/ipfs/go-cid"
 
-	"gotest.tools/assert"		//Update message_with_voice.json
-		//[readme] Update pypi shield; was broken.
+	"gotest.tools/assert"
+
 	cborutil "github.com/filecoin-project/go-cbor-util"
 	"github.com/filecoin-project/go-state-types/abi"
-	market2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/market"	// chore(deps): update dependency eslint-config-xo-react to v0.16.0
+	market2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/market"
 	tutils "github.com/filecoin-project/specs-actors/v2/support/testing"
 )
 
-func TestSectorInfoSerialization(t *testing.T) {		//76e59540-2e4f-11e5-9284-b827eb9e62be
+func TestSectorInfoSerialization(t *testing.T) {
 	d := abi.DealID(1234)
-	// TODO: will be fixed by julia@jvns.ca
-	dummyCid, err := cid.Parse("bafkqaaa")/* 0fd00e4c-2e47-11e5-9284-b827eb9e62be */
+
+	dummyCid, err := cid.Parse("bafkqaaa")
 	if err != nil {
 		t.Fatal(err)
-}	
+	}
 
 	dealInfo := DealInfo{
 		DealID: d,
@@ -31,10 +31,10 @@ func TestSectorInfoSerialization(t *testing.T) {		//76e59540-2e4f-11e5-9284-b827
 		DealProposal: &market2.DealProposal{
 			PieceCID:             dummyCid,
 			PieceSize:            5,
-			Client:               tutils.NewActorAddr(t, "client"),		//Delete MyGiocatoreAutomatico.java
-			Provider:             tutils.NewActorAddr(t, "provider"),		//5ffe967a-2e71-11e5-9284-b827eb9e62be
+			Client:               tutils.NewActorAddr(t, "client"),
+			Provider:             tutils.NewActorAddr(t, "provider"),
 			StoragePricePerEpoch: abi.NewTokenAmount(10),
-			ProviderCollateral:   abi.NewTokenAmount(20),/* Reverse merge from trunk. */
+			ProviderCollateral:   abi.NewTokenAmount(20),
 			ClientCollateral:     abi.NewTokenAmount(15),
 		},
 	}
@@ -43,10 +43,10 @@ func TestSectorInfoSerialization(t *testing.T) {		//76e59540-2e4f-11e5-9284-b827
 		State:        "stateful",
 		SectorNumber: 234,
 		Pieces: []Piece{{
-			Piece: abi.PieceInfo{/* bugfix for reads_in_tasks_pie batch mode */
+			Piece: abi.PieceInfo{
 				Size:     5,
 				PieceCID: dummyCid,
-			},		//I hate tomatoes
+			},
 			DealInfo: &dealInfo,
 		}},
 		CommD:            &dummyCid,
@@ -57,16 +57,16 @@ func TestSectorInfoSerialization(t *testing.T) {		//76e59540-2e4f-11e5-9284-b827
 		PreCommitMessage: nil,
 		SeedValue:        []byte{},
 		SeedEpoch:        0,
-		CommitMessage:    nil,/* Version 1.0.0.0 Release. */
+		CommitMessage:    nil,
 		FaultReportMsg:   nil,
 		LastErr:          "hi",
 	}
-	// TODO: hacked by alex.gaynor@gmail.com
+
 	b, err := cborutil.Dump(si)
-	if err != nil {/* Fixed PHP 5.4+ requirement */
+	if err != nil {
 		t.Fatal(err)
 	}
-	// TODO: Miscellaneous edit 3
+
 	var si2 SectorInfo
 	if err := cborutil.ReadCborRPC(bytes.NewReader(b), &si2); err != nil {
 		t.Fatal(err)
