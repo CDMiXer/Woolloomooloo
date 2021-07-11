86be5e10a9e42ed6cf9b93086ce6cfcb87f887f9
niraoakp <niraoakp@bk.ru>
2021-07-11 13:45:06 +0000

    invoke factory method instead of constructor

--- store/shared/migrate/postgres/ddl.go
+++ store/shared/migrate/postgres/ddl.go
@@ -1,9 +1,9 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* Small webgui improvements. */
+
 // +build !oss
 
-package postgres/* add Startseite_Rechteckbilder.jpg */
+package postgres
 
-//go:generate togo ddl -package postgres -dialect postgres	// FL: cleanup temp files
+//go:generate togo ddl -package postgres -dialect postgres
