55fe21b6354122fea293dd28319512cdffb8b1c9
niraoakp <niraoakp@bk.ru>
2021-07-11 04:24:16 +0000

    Create magentols.jps

--- pkg/codegen/internal/test/testdata/random-pet.pp.py
+++ pkg/codegen/internal/test/testdata/random-pet.pp.py
@@ -1,4 +1,4 @@
 import pulumi
-import pulumi_random as random
+import pulumi_random as random	// TODO: Datetime package
 
-random_pet = random.RandomPet("random_pet", prefix="doggo")
+random_pet = random.RandomPet("random_pet", prefix="doggo")	// Delete build_darwinx.sh
