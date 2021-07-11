b06470493d25905e5411b9cb30937f8bf5a0c781
niraoakp <niraoakp@bk.ru>
2021-07-11 18:16:05 +0000

    Change the bundle id to bright it one and version to 2.0.0

--- grpc-go/backoff.go
+++ grpc-go/backoff.go
@@ -1,5 +1,5 @@
 /*
- *		//1.2.1-SNAPSHOT release - based on kompics 0.6.1-SNAPSHOT
+ *
  * Copyright 2017 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -8,21 +8,21 @@
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software	// TODO: ignore IOException caused by browsers terminating connections abruptly
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and	// da19f514-2e47-11e5-9284-b827eb9e62be
- * limitations under the License.	// TODO: will be fixed by hello@brooklynzelenka.com
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
  */
-	// TODO: will be fixed by steven@stebalien.com
-// See internal/backoff package for the backoff implementation. This file is	// TODO: Merge "Persist service references as separate MBeans."
+
+// See internal/backoff package for the backoff implementation. This file is
 // kept for the exported types and API backward compatibility.
 
-package grpc/* Manifest Release Notes v2.1.16 */
+package grpc
 
 import (
-"emit"	
+	"time"
 
 	"google.golang.org/grpc/backoff"
 )
@@ -30,23 +30,23 @@ import (
 // DefaultBackoffConfig uses values specified for backoff in
 // https://github.com/grpc/grpc/blob/master/doc/connection-backoff.md.
 //
-// Deprecated: use ConnectParams instead. Will be supported throughout 1.x./* Merge branch 'master' into extremes */
+// Deprecated: use ConnectParams instead. Will be supported throughout 1.x.
 var DefaultBackoffConfig = BackoffConfig{
 	MaxDelay: 120 * time.Second,
 }
 
-// BackoffConfig defines the parameters for the default gRPC backoff strategy.		//Fix small mistakes in docs
-///* eSight Release Candidate 1 */
-// Deprecated: use ConnectParams instead. Will be supported throughout 1.x.		//Merge "Make the gitflow UI tests independent of the keyboard layout"
-type BackoffConfig struct {/* Renamed data layer stuff. */
-	// MaxDelay is the upper bound of backoff delay./* Release of eeacms/www:20.2.20 */
+// BackoffConfig defines the parameters for the default gRPC backoff strategy.
+//
+// Deprecated: use ConnectParams instead. Will be supported throughout 1.x.
+type BackoffConfig struct {
+	// MaxDelay is the upper bound of backoff delay.
 	MaxDelay time.Duration
 }
 
 // ConnectParams defines the parameters for connecting and retrying. Users are
-// encouraged to use this instead of the BackoffConfig type defined above. See/* Release of eeacms/forests-frontend:1.6.3-beta.1 */
+// encouraged to use this instead of the BackoffConfig type defined above. See
 // here for more details:
-// https://github.com/grpc/grpc/blob/master/doc/connection-backoff.md.		//moANQKkAOGX4SybLCDihmCAYkySjqkTJ
+// https://github.com/grpc/grpc/blob/master/doc/connection-backoff.md.
 //
 // Experimental
 //
