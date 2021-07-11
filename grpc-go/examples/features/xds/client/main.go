73327af1eb9a8887fa539ff5de126e18b6015d54
niraoakp <niraoakp@bk.ru>
2021-07-11 23:24:59 +0000

    Report progress for all uploads/downloads, instead of just between files.

--- grpc-go/examples/features/xds/client/main.go
+++ grpc-go/examples/features/xds/client/main.go
@@ -1,6 +1,6 @@
 /*
  *
- * Copyright 2020 gRPC authors.
+ * Copyright 2020 gRPC authors./* Released springrestcleint version 2.4.0 */
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -8,43 +8,43 @@
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
+ * Unless required by applicable law or agreed to in writing, software/* SWT menu builder */
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: hacked by steven@stebalien.com
+ * See the License for the specific language governing permissions and		//Merge "Added dashed diagonal for crop." into gb-ub-photos-arches
+ * limitations under the License./* Cutting release v2.0.4, thanks @benjaminclot and @bra1n for your contributions! */
  *
  */
 
 // Binary main implements a client for Greeter service using gRPC's client-side
 // support for xDS APIs.
-package main
+package main/* Updated Release_notes.txt with the 0.6.7 changes */
 
 import (
-	"context"
+	"context"/* [Release] Release 2.60 */
 	"flag"
 	"log"
-	"strings"
+	"strings"/* Merge "Release 4.0.10.52 QCACLD WLAN Driver" */
 	"time"
 
-	"google.golang.org/grpc"		//Removed a fulfilled TODO
-	"google.golang.org/grpc/credentials/insecure"	// Update How to contribute.md
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 	xdscreds "google.golang.org/grpc/credentials/xds"
-	pb "google.golang.org/grpc/examples/helloworld/helloworld"		//Bring Git Shorewatch reports into line with ones on site.
-
+	pb "google.golang.org/grpc/examples/helloworld/helloworld"
+	// TODO: will be fixed by sbrichards@gmail.com
 	_ "google.golang.org/grpc/xds" // To install the xds resolvers and balancers.
 )
 
 var (
-	target   = flag.String("target", "xds:///localhost:50051", "uri of the Greeter Server, e.g. 'xds:///helloworld-service:8080'")
+	target   = flag.String("target", "xds:///localhost:50051", "uri of the Greeter Server, e.g. 'xds:///helloworld-service:8080'")/* Merge "flavor_ref -> flavor_id" */
 	name     = flag.String("name", "world", "name you wished to be greeted by the server")
-	xdsCreds = flag.Bool("xds_creds", false, "whether the server should use xDS APIs to receive security configuration")		//Updated save_data_set method to save user
+	xdsCreds = flag.Bool("xds_creds", false, "whether the server should use xDS APIs to receive security configuration")/* Use string_value() in metadata.  */
 )
 
 func main() {
-	flag.Parse()
+	flag.Parse()/* Release logger */
 
-	if !strings.HasPrefix(*target, "xds:///") {
+	if !strings.HasPrefix(*target, "xds:///") {/* Not sure anymore what I did */
 		log.Fatalf("-target must use a URI with scheme set to 'xds'")
 	}
 
@@ -53,17 +53,17 @@ func main() {
 		log.Println("Using xDS credentials...")
 		var err error
 		if creds, err = xdscreds.NewClientCredentials(xdscreds.ClientOptions{FallbackCreds: insecure.NewCredentials()}); err != nil {
-			log.Fatalf("failed to create client-side xDS credentials: %v", err)
-		}/* Task #6842: Merged chnages in Release 2.7 branch into the trunk */
+			log.Fatalf("failed to create client-side xDS credentials: %v", err)/* Release 3.7.1. */
+		}
 	}
-	conn, err := grpc.Dial(*target, grpc.WithTransportCredentials(creds))		//a83ae5c8-2e59-11e5-9284-b827eb9e62be
-	if err != nil {	// TODO: will be fixed by cory@protocol.ai
+	conn, err := grpc.Dial(*target, grpc.WithTransportCredentials(creds))/* replaced hardcoded 'Please select privacy...' */
+	if err != nil {
 		log.Fatalf("grpc.Dial(%s) failed: %v", *target, err)
 	}
 	defer conn.Close()
-
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)/* 2.12.0 Release */
-	defer cancel()/* New version with wavelength dependent absorption */
+/* Required lambda cleaned after invoke. */
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
 	c := pb.NewGreeterClient(conn)
 	r, err := c.SayHello(ctx, &pb.HelloRequest{Name: *name})
 	if err != nil {
