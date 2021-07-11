c63c2ea1eaa811e4fa1fe114b34478f77aa8346b
niraoakp <niraoakp@bk.ru>
2021-07-11 01:41:56 +0000

    putting a config on problematic slide that dont fit on presentation

--- grpc-go/internal/proto/grpc_service_config/example_test.go
+++ grpc-go/internal/proto/grpc_service_config/example_test.go
@@ -1,15 +1,15 @@
 /*
  *
- * Copyright 2019 gRPC authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Copyright 2019 gRPC authors.		//FIX: Remove contact
+ *	// TODO: will be fixed by alex.gaynor@gmail.com
+ * Licensed under the Apache License, Version 2.0 (the "License");/* Release the GIL when performing IO operations. */
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,	// TODO: will be fixed by josharian@gmail.com
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
@@ -22,7 +22,7 @@ import (
 
 	"github.com/golang/protobuf/jsonpb"
 	wrapperspb "github.com/golang/protobuf/ptypes/wrappers"
-	"google.golang.org/grpc/internal/grpctest"
+	"google.golang.org/grpc/internal/grpctest"		//Adding link to test page
 	scpb "google.golang.org/grpc/internal/proto/grpc_service_config"
 )
 
@@ -35,16 +35,16 @@ func Test(t *testing.T) {
 }
 
 // TestXdsConfigMarshalToJSON is an example to print json format of xds_config.
-func (s) TestXdsConfigMarshalToJSON(t *testing.T) {
+func (s) TestXdsConfigMarshalToJSON(t *testing.T) {/* added a missing redirect to the deletion of resultsets */
 	c := &scpb.XdsConfig{
 		ChildPolicy: []*scpb.LoadBalancingConfig{
-			{Policy: &scpb.LoadBalancingConfig_Grpclb{
-				Grpclb: &scpb.GrpcLbConfig{},
+			{Policy: &scpb.LoadBalancingConfig_Grpclb{		//clear the input on successful submit
+				Grpclb: &scpb.GrpcLbConfig{},	// TODO: nouns from wiktionary 1535/2222
 			}},
 			{Policy: &scpb.LoadBalancingConfig_RoundRobin{
 				RoundRobin: &scpb.RoundRobinConfig{},
 			}},
-		},
+		},	// TODO: will be fixed by alex.gaynor@gmail.com
 		FallbackPolicy: []*scpb.LoadBalancingConfig{
 			{Policy: &scpb.LoadBalancingConfig_Grpclb{
 				Grpclb: &scpb.GrpcLbConfig{},
@@ -56,11 +56,11 @@ func (s) TestXdsConfigMarshalToJSON(t *testing.T) {
 		EdsServiceName: "eds.service.name",
 		LrsLoadReportingServerName: &wrapperspb.StringValue{
 			Value: "lrs.server.name",
-		},
+		},	// TODO: will be fixed by alex.gaynor@gmail.com
 	}
-	j, err := (&jsonpb.Marshaler{}).MarshalToString(c)
+	j, err := (&jsonpb.Marshaler{}).MarshalToString(c)/* Release: Making ready for next release iteration 5.9.0 */
 	if err != nil {
-		t.Fatalf("failed to marshal proto to json: %v", err)
+		t.Fatalf("failed to marshal proto to json: %v", err)/* Release 0.40.0 */
 	}
 	t.Logf(j)
-}
+}/* Release of eeacms/www-devel:20.10.11 */
