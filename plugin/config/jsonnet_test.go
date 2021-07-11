828052272290542d2cbfe3dd7fad7ae6bc6dd199
niraoakp <niraoakp@bk.ru>
2021-07-11 22:44:17 +0000

    activated shoot, bug in the field presentation

--- plugin/config/jsonnet_test.go
+++ plugin/config/jsonnet_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Field visit report completed. */
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
-	// Merge "Complete implementation of bay operations"
-package config	// Remove duplicate spacing
+/* Delete LABYRINT.BAS */
+package config
