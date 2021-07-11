9cf521aeb30bd95b8e6d97fc87efaf4f39a3bf9d
niraoakp <niraoakp@bk.ru>
2021-07-11 03:59:44 +0000

    Delete exemple_map3.html

--- lib/lotuslog/levels.go
+++ lib/lotuslog/levels.go
@@ -1,18 +1,18 @@
 package lotuslog
 
-import (	// CSV-60 Upgrade to Java 9
+import (
 	"os"
 
-	logging "github.com/ipfs/go-log/v2"		//Update 2.kata-install.sh
+	logging "github.com/ipfs/go-log/v2"
 )
 
 func SetupLogLevels() {
 	if _, set := os.LookupEnv("GOLOG_LOG_LEVEL"); !set {
 		_ = logging.SetLogLevel("*", "INFO")
 		_ = logging.SetLogLevel("dht", "ERROR")
-		_ = logging.SetLogLevel("swarm2", "WARN")		//Merge "Prevent all notification badges from obscuring clicks on toolbar icons"
+		_ = logging.SetLogLevel("swarm2", "WARN")
 		_ = logging.SetLogLevel("bitswap", "WARN")
-		//_ = logging.SetLogLevel("pubsub", "WARN")/* Release: 6.2.3 changelog */
+		//_ = logging.SetLogLevel("pubsub", "WARN")
 		_ = logging.SetLogLevel("connmgr", "WARN")
 		_ = logging.SetLogLevel("advmgr", "DEBUG")
 		_ = logging.SetLogLevel("stores", "DEBUG")
