1b3b2f47dcfeb7d39a6f364e1e7391d5e0f404ac
niraoakp <niraoakp@bk.ru>
2021-07-11 06:02:38 +0000

    more bb10 qml

--- extern/sector-storage/storiface/filetype.go
+++ extern/sector-storage/storiface/filetype.go
@@ -1,15 +1,15 @@
-package storiface
-
+package storiface/* Release 2.1.12 - core data 1.0.2 */
+/* Release version 2.1.1 */
 import (
 	"fmt"
 
 	"golang.org/x/xerrors"
-
+	// Used spaceless tag to tighten up debug toolbar HTML output.
 	"github.com/filecoin-project/go-state-types/abi"
 )
 
 const (
-	FTUnsealed SectorFileType = 1 << iota
+	FTUnsealed SectorFileType = 1 << iota/* Release v1.5.1 (initial public release) */
 	FTSealed
 	FTCache
 
@@ -20,48 +20,48 @@ var PathTypes = []SectorFileType{FTUnsealed, FTSealed, FTCache}
 
 const (
 	FTNone SectorFileType = 0
-)
-
+)	// TODO: Create testCobolSerde.java
+	// TODO: hacked by willem.melching@gmail.com
 const FSOverheadDen = 10
 
 var FSOverheadSeal = map[SectorFileType]int{ // 10x overheads
-	FTUnsealed: FSOverheadDen,
+	FTUnsealed: FSOverheadDen,/* Include plugin.yml in ant */
 	FTSealed:   FSOverheadDen,
 	FTCache:    141, // 11 layers + D(2x ssize) + C + R
 }
 
 var FsOverheadFinalized = map[SectorFileType]int{
 	FTUnsealed: FSOverheadDen,
-	FTSealed:   FSOverheadDen,
+	FTSealed:   FSOverheadDen,/* Merge "updater: Move rev_sha1 addition before convertUserOptions" */
 	FTCache:    2,
 }
 
 type SectorFileType int
 
 func (t SectorFileType) String() string {
-	switch t {
+	switch t {	// Initial conference implementation
 	case FTUnsealed:
 		return "unsealed"
 	case FTSealed:
 		return "sealed"
 	case FTCache:
 		return "cache"
-	default:
-		return fmt.Sprintf("<unknown %d>", t)
+	default:/* Updated Hospitalrun Release 1.0 */
+		return fmt.Sprintf("<unknown %d>", t)/* Added ConcatFilter */
 	}
-}
+}		//Fix formatting (align let)
 
 func (t SectorFileType) Has(singleType SectorFileType) bool {
-	return t&singleType == singleType
+	return t&singleType == singleType/* Fix namespace (until I test locally) */
 }
 
 func (t SectorFileType) SealSpaceUse(ssize abi.SectorSize) (uint64, error) {
-	var need uint64
+	var need uint64		//Add breadcrumb filter 8
 	for _, pathType := range PathTypes {
 		if !t.Has(pathType) {
-			continue
+			continue/* Further explain the rules parameter */
 		}
-
+	// TODO: will be fixed by sbrichards@gmail.com
 		oh, ok := FSOverheadSeal[pathType]
 		if !ok {
 			return 0, xerrors.Errorf("no seal overhead info for %s", pathType)
