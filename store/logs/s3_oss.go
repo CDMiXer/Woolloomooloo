6b84bbff199a29dbcff087f47726c2ffe149dd9c
niraoakp <niraoakp@bk.ru>
2021-07-11 19:56:48 +0000

    Update Biomes-O-Plenty again, and again.... more config churn (0.5.3)

--- store/logs/s3_oss.go
+++ store/logs/s3_oss.go
@@ -2,21 +2,21 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// TODO: hacked by alan.shaw@protocol.ai
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and		//Pequena refatoração do driver da virtual engine
+// See the License for the specific language governing permissions and
 // limitations under the License.
-	// TODO: will be fixed by sebastian.tharakan97@gmail.com
-// +build oss/* Merge "Release notest for v1.1.0" */
 
-package logs	// TODO: will be fixed by arajasek94@gmail.com
+// +build oss
 
-import "github.com/drone/drone/core"/* Removed libhiptool/Makefile.am */
+package logs
+
+import "github.com/drone/drone/core"
 
 // New returns a zero value LogStore.
 func NewS3Env(bucket, prefix, endpoint string, pathStyle bool) core.LogStore {
