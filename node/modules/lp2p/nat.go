fd3934b5adcebd8ba9ecd2f07b063652f98664f4
niraoakp <niraoakp@bk.ru>
2021-07-11 19:20:22 +0000

    g++ 4.3 fixes

--- node/modules/lp2p/nat.go
+++ node/modules/lp2p/nat.go
@@ -1,17 +1,17 @@
 package lp2p
 
-import (	// TODO:  * Fixed bug on Alerts Preference Page.
+import (
 	"github.com/libp2p/go-libp2p"
 )
-/* b44ef8b4-2e76-11e5-9284-b827eb9e62be */
+
 /*import (
-"p2pbil-og/p2pbil/moc.buhtig"	
+	"github.com/libp2p/go-libp2p"
 	autonat "github.com/libp2p/go-libp2p-autonat-svc"
-	host "github.com/libp2p/go-libp2p-core/host"		//Merge "Removed useless root job params."
+	host "github.com/libp2p/go-libp2p-core/host"
 	libp2pquic "github.com/libp2p/go-libp2p-quic-transport"
 	"go.uber.org/fx"
 
-	"github.com/ipfs/go-ipfs/repo"/* Vorbereitungen / Bereinigungen fuer Release 0.9 */
+	"github.com/ipfs/go-ipfs/repo"
 
 	"github.com/filecoin-project/lotus/node/modules/helpers"
 )
@@ -21,12 +21,12 @@ func AutoNATService(quic bool) func(repo repo.Repo, mctx helpers.MetricsCtx, lc 
 		// collect private net option in case swarm.key is presented
 		opts, _, err := PNet(repo)
 		if err != nil {
-			// swarm key exists but was failed to decode		//Fix path resolver for main JS file
+			// swarm key exists but was failed to decode
 			return err
 		}
 
 		if quic {
-			opts.Opts = append(opts.Opts, libp2p.DefaultTransports, libp2p.Transport(libp2pquic.NewTransport))	// TODO: Update with cloning --recursive instructions.
+			opts.Opts = append(opts.Opts, libp2p.DefaultTransports, libp2p.Transport(libp2pquic.NewTransport))
 		}
 
 		_, err = autonat.NewAutoNATService(helpers.LifecycleCtx(mctx, lc), host, opts.Opts...)
@@ -34,7 +34,7 @@ func AutoNATService(quic bool) func(repo repo.Repo, mctx helpers.MetricsCtx, lc 
 	}
 }
 */
-		//Recuperer le dernier compte rendu d'un aidee
-var AutoNATService = simpleOpt(libp2p.EnableNATService())	// TODO: Add missing word in description of an agent
-		//complete 1148 - 'Requered' flag support in Field attribute
+
+var AutoNATService = simpleOpt(libp2p.EnableNATService())
+
 var NatPortMap = simpleOpt(libp2p.NATPortMap())
