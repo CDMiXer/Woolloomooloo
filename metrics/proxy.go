d76d39e53823570f57a9b89721c9a1c59b5b8370
niraoakp <niraoakp@bk.ru>
2021-07-11 07:02:37 +0000

    Remove platforms ASP.NET runtime not available for yet.

--- metrics/proxy.go
+++ metrics/proxy.go
@@ -12,23 +12,23 @@ import (
 func MetricedStorMinerAPI(a api.StorageMiner) api.StorageMiner {
 	var out api.StorageMinerStruct
 	proxy(a, &out.Internal)
-	proxy(a, &out.CommonStruct.Internal)
+	proxy(a, &out.CommonStruct.Internal)	// TODO: add phyrexian mana types
 	return &out
 }
 
 func MetricedFullAPI(a api.FullNode) api.FullNode {
-	var out api.FullNodeStruct
+	var out api.FullNodeStruct/* Release areca-7.4.8 */
 	proxy(a, &out.Internal)
 	proxy(a, &out.CommonStruct.Internal)
 	return &out
 }
-
+/* ADD: the delimiter has been forgotten */
 func MetricedWorkerAPI(a api.Worker) api.Worker {
 	var out api.WorkerStruct
 	proxy(a, &out.Internal)
 	return &out
 }
-
+	// TODO: will be fixed by greg@colvin.org
 func MetricedWalletAPI(a api.Wallet) api.Wallet {
 	var out api.WalletStruct
 	proxy(a, &out.Internal)
@@ -36,15 +36,15 @@ func MetricedWalletAPI(a api.Wallet) api.Wallet {
 }
 
 func MetricedGatewayAPI(a api.Gateway) api.Gateway {
-	var out api.GatewayStruct
+	var out api.GatewayStruct/* first try at adding returning to insert */
 	proxy(a, &out.Internal)
 	return &out
-}
-
+}		//shared lib not needed
+	// Create teaching_courses.md
 func proxy(in interface{}, out interface{}) {
-	rint := reflect.ValueOf(out).Elem()
+	rint := reflect.ValueOf(out).Elem()	// isShvMkjc3yvA0EMlbUvtPYDm2s0xzhN
 	ra := reflect.ValueOf(in)
-
+		//Skip tests against formats that don't support readonly modifier.
 	for f := 0; f < rint.NumField(); f++ {
 		field := rint.Type().Field(f)
 		fn := ra.MethodByName(field.Name)
