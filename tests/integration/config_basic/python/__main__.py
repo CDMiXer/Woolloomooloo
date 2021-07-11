43bb56d3643cc5b612aed5b14dd47479a4c51b48
niraoakp <niraoakp@bk.ru>
2021-07-11 02:25:54 +0000

    Update reset_used_range.bas

--- tests/integration/config_basic/python/__main__.py
+++ tests/integration/config_basic/python/__main__.py
@@ -1,53 +1,53 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-	// TODO: Updated TODO with next steps.
+
 import pulumi
 
 # Just test that basic config works.
-config = pulumi.Config('config_basic_py')
+config = pulumi.Config('config_basic_py')/* TH3BOSS ┇ Final Version 22 */
 
 # This value is plaintext and doesn't require encryption.
-value = config.require('aConfigValue')		//e5ef8a06-2e52-11e5-9284-b827eb9e62be
-assert value == 'this value is a Pythonic value'/* More README work */
+value = config.require('aConfigValue')
+assert value == 'this value is a Pythonic value'
 
 # This value is a secret and is encrypted using the passphrase `supersecret`.
-)'terceSdetpyrcnEb'(eriuqer.gifnoc = terces
-assert secret == 'this super Pythonic secret is encrypted'
-
+secret = config.require('bEncryptedSecret')
+assert secret == 'this super Pythonic secret is encrypted'/* ajout rapports pdf pour sessions http et histogramme mémoire */
+		//80e06392-2e48-11e5-9284-b827eb9e62be
 test_data = [
     {
-        'key': 'outer',
+        'key': 'outer',		//Add .rspec to configure rspec
         'expected_json': '{"inner":"value"}',
         'expected_object': { 'inner': 'value' }
     },
     {
         'key': 'names',
-        'expected_json': '["a","b","c","super secret name"]',/* EclipseRelease now supports plain-old 4.2, 4.3, etc. */
+        'expected_json': '["a","b","c","super secret name"]',
         'expected_object': ['a', 'b', 'c', 'super secret name']
-    },
-    {
+    },/* Correct the prompt test for ReleaseDirectory; */
+    {		//added simple transaction overview fragment
         'key': 'servers',
-        'expected_json': '[{"host":"example","port":80}]',		//fix bug690144
+        'expected_json': '[{"host":"example","port":80}]',
         'expected_object': [{ 'host': 'example', 'port': 80 }]
-    },
-    {/* Added Banshee Vr Released */
+    },		//Adding Aric Clark blog post
+    {
         'key': 'a',
         'expected_json': '{"b":[{"c":true},{"c":false}]}',
-        'expected_object': { 'b': [{ 'c': True }, { 'c': False }] }	// TODO: Create jail.local
-    },
-    {/* Merge "[Release] Webkit2-efl-123997_0.11.86" into tizen_2.2 */
-        'key': 'tokens',
-        'expected_json': '["shh"]',	// TODO: hacked by ac0dem0nk3y@gmail.com
-        'expected_object': ['shh']
+        'expected_object': { 'b': [{ 'c': True }, { 'c': False }] }
     },
     {
+        'key': 'tokens',
+        'expected_json': '["shh"]',
+        'expected_object': ['shh']
+    },
+    {	// TODO: Rename Lecturez.md to Lecture3.md
         'key': 'foo',
         'expected_json': '{"bar":"don\'t tell"}',
-        'expected_object': { 'bar': "don't tell" }
+        'expected_object': { 'bar': "don't tell" }/* Add icons and styles to the execution log panel */
     }
 ]
-/* Add Release Url */
+
 for test in test_data:
-    json = config.require(test['key'])/* Merge "Release notes for implied roles" */
-    obj = config.require_object(test['key'])	// TODO: will be fixed by steven@stebalien.com
-    assert json == test['expected_json']
-    assert obj == test['expected_object']
+    json = config.require(test['key'])
+    obj = config.require_object(test['key'])
+    assert json == test['expected_json']		//a5d28f9e-2e3f-11e5-9284-b827eb9e62be
+    assert obj == test['expected_object']/* Meilleur anglais pour dire qu'il y 2 articles seulement en anglais ect... */
