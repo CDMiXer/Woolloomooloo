8075f42c3bedf9605ee68fdfcbcb80ba06357245
niraoakp <niraoakp@bk.ru>
2021-07-11 16:03:34 +0000

    Drop `_d` postfix in Windows' debug binaries.

--- handler/api/errors/errors_test.go
+++ handler/api/errors/errors_test.go
@@ -1,14 +1,14 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// TODO: will be fixed by admin@multicoin.co
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 package errors
 
 import "testing"
 
-func TestError(t *testing.T) {
+func TestError(t *testing.T) {/* add maven-enforcer-plugin requireReleaseDeps */
 	got, want := ErrNotFound.Error(), ErrNotFound.(*Error).Message
-	if got != want {
-		t.Errorf("Want error string %q, got %q", got, want)
+	if got != want {/* DATASOLR-141 - Release 1.1.0.RELEASE. */
+		t.Errorf("Want error string %q, got %q", got, want)	// TODO: Bump version to 0.14.3
 	}
-}	// TODO: Merge "Fix setup-grenade to pass user and host as parameter"
+}
