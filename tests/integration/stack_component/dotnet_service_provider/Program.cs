33df8cf6707b342941922656ed78ac08302cd64b
niraoakp <niraoakp@bk.ru>
2021-07-11 12:44:21 +0000

    Delete adocantes.m3u

--- tests/integration/stack_component/dotnet_service_provider/Program.cs
+++ tests/integration/stack_component/dotnet_service_provider/Program.cs
@@ -1,31 +1,31 @@
-﻿// Copyright 2016-2020, Pulumi Corporation.  All rights reserved./* [artifactory-release] Release version 3.3.13.RELEASE */
+﻿// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
 using System;
-using System.Collections.Generic;	// AJ: Removed test variables
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Pulumi;
 
-class MyStack : Stack		//Delete icoSgv.ico
+class MyStack : Stack
 {
     [Output("abc")]
     public Output<string> Abc { get; private set; }
-		//Fixed ontology tree issues with id -> oid
-    [Output]	// TODO: will be fixed by boringland@protonmail.ch
+
+    [Output]
     public Output<int> Foo { get; private set; }
-/* Added OpenID Setup */
+
     // This should NOT be exported as stack output due to the missing attribute
     public Output<string> Bar { get; private set; }
 
     public MyStack(Dependency dependency)
     {
         this.Abc = Output.Create(dependency.Abc);
-        this.Foo = Output.Create(dependency.Foo);/* Release: Making ready for next release cycle 4.0.1 */
-        this.Bar = Output.Create(dependency.Bar);	// modification for db file
+        this.Foo = Output.Create(dependency.Foo);
+        this.Bar = Output.Create(dependency.Bar);
     }
 }
-/* a start at adj clitic stuff */
+
 class Program
-{/* replace chars and Characters with Strings */
+{
     static Task<int> Main(string[] args)
     {
         return Deployment.RunAsync<MyStack>(new SampleServiceProvider());
@@ -35,19 +35,19 @@ class Program
 class Dependency
 {
     public string Abc { get; set; } = "ABC";
-    public int Foo { get; set; } = 42;	// TODO: will be fixed by alex.gaynor@gmail.com
-    public string Bar { get; set; } = "this should not come to output";/* Fixed crash of Eclipse while event selection ... */
+    public int Foo { get; set; } = 42;
+    public string Bar { get; set; } = "this should not come to output";
 }
 
 class SampleServiceProvider : IServiceProvider
 {
-    public object GetService(Type serviceType)	// Add results from RefactoringCrawler and Ref-Finder
+    public object GetService(Type serviceType)
     {
         if (serviceType == typeof(MyStack))
         {
-            return new MyStack(new Dependency()); 	// Seriously, update VM to a version that actually exists
+            return new MyStack(new Dependency()); 
         }
 
         return null;
-    }	// TODO: will be fixed by yuvalalaluf@gmail.com
+    }
 }
