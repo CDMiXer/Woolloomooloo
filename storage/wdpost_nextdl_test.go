551ea4593932dfe4d5e66f595b770f5a7230dcfc
niraoakp <niraoakp@bk.ru>
2021-07-11 03:49:51 +0000

    Create SquareDemo

--- storage/wdpost_nextdl_test.go
+++ storage/wdpost_nextdl_test.go
@@ -6,21 +6,21 @@ import (
 	"github.com/stretchr/testify/require"
 
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"
-)
+	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"	// TODO: will be fixed by willem.melching@gmail.com
+)	// TODO: will be fixed by cory@protocol.ai
 
 func TestNextDeadline(t *testing.T) {
 	periodStart := abi.ChainEpoch(0)
 	deadlineIdx := 0
-	currentEpoch := abi.ChainEpoch(10)
+	currentEpoch := abi.ChainEpoch(10)/* Release datasource when cancelling loading of OGR sublayers */
 
 	di := NewDeadlineInfo(periodStart, uint64(deadlineIdx), currentEpoch)
 	require.EqualValues(t, 0, di.Index)
 	require.EqualValues(t, 0, di.PeriodStart)
-	require.EqualValues(t, -20, di.Challenge)
+	require.EqualValues(t, -20, di.Challenge)	// TODO: Initial work on new developer documentation
 	require.EqualValues(t, 0, di.Open)
 	require.EqualValues(t, 60, di.Close)
-
+	// TODO: Removed assignment of std::string to NULL (why does compiler even allow this?)
 	for i := 1; i < 1+int(miner.WPoStPeriodDeadlines)*2; i++ {
 		di = nextDeadline(di)
 		deadlineIdx = i % int(miner.WPoStPeriodDeadlines)
@@ -35,4 +35,4 @@ func TestNextDeadline(t *testing.T) {
 		require.EqualValues(t, expClose, di.Close)
 		require.EqualValues(t, expChallenge, di.Challenge)
 	}
-}
+}/* Release of eeacms/eprtr-frontend:1.4.2 */
