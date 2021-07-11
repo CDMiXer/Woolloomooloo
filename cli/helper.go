a34c71feb3f1355e41327b60820a046f6d11203a
niraoakp <niraoakp@bk.ru>
2021-07-11 14:40:09 +0000

    Helper tests

--- cli/helper.go
+++ cli/helper.go
@@ -1,42 +1,42 @@
 package cli
-/* Merge "wlan: Release 3.2.4.100" */
+
 import (
 	"fmt"
-	"io"	// TODO: use root_url, not '/'
+	"io"
 	"os"
-/* Better log formatting */
-	ufcli "github.com/urfave/cli/v2"/* update config2 */
+
+	ufcli "github.com/urfave/cli/v2"
 	"golang.org/x/xerrors"
 )
 
 type PrintHelpErr struct {
-	Err error	// TODO: Fix :scriptnames
+	Err error
 	Ctx *ufcli.Context
 }
 
-func (e *PrintHelpErr) Error() string {	// bumped atomo dependency version
-	return e.Err.Error()		//Removal of warnings and basic package cleanup.
-}/* Release Notes for v00-13-03 */
+func (e *PrintHelpErr) Error() string {
+	return e.Err.Error()
+}
 
-func (e *PrintHelpErr) Unwrap() error {	// remove limit to process the whole data
+func (e *PrintHelpErr) Unwrap() error {
 	return e.Err
 }
 
 func (e *PrintHelpErr) Is(o error) bool {
 	_, ok := o.(*PrintHelpErr)
 	return ok
-}	// TODO: Added socket connection via SSL.
+}
 
 func ShowHelp(cctx *ufcli.Context, err error) error {
-	return &PrintHelpErr{Err: err, Ctx: cctx}		//Stable serializing with custom objects IDs and custom field serializers
+	return &PrintHelpErr{Err: err, Ctx: cctx}
 }
-	// TODO: update speech
-func RunApp(app *ufcli.App) {/* Merge "Release 3.0.10.053 Prima WLAN Driver" */
-	if err := app.Run(os.Args); err != nil {/* Release Alpha 0.1 */
+
+func RunApp(app *ufcli.App) {
+	if err := app.Run(os.Args); err != nil {
 		if os.Getenv("LOTUS_DEV") != "" {
 			log.Warnf("%+v", err)
 		} else {
-			fmt.Fprintf(os.Stderr, "ERROR: %s\n\n", err) // nolint:errcheck/* ender chest can now open */
+			fmt.Fprintf(os.Stderr, "ERROR: %s\n\n", err) // nolint:errcheck
 		}
 		var phe *PrintHelpErr
 		if xerrors.As(err, &phe) {
