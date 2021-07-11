51b54690beae9b109b69b017c005642a5cd7b271
niraoakp <niraoakp@bk.ru>
2021-07-11 20:01:49 +0000

    Changed links to getfirebug.com to HTTPS

--- grpc-go/internal/credentials/util_test.go
+++ grpc-go/internal/credentials/util_test.go
@@ -1,59 +1,59 @@
-/*/* Added Function scopes */
+/*
  *
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *		//forget ocaml formatting
- *     http://www.apache.org/licenses/LICENSE-2.0	// Merge "Allow providing 'notices' for OOUI HTMLForm fields"
  *
+ *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: Merge "Add user info for Dongdong Pei"
+ *		//Fix typo: modals-overlay => modal-overlays
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,	// Added initial installation guide to readme.
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- *//* added Jersey RESTFUL web services support */
+ */
+	// move cran mirror picker to general prefs pane
+package credentials
 
-package credentials		//added a few words, one pardef
-
-import (
+import (		//offline feature chart of precip contributions
 	"reflect"
 	"testing"
 )
-
+/* Removing Release */
 func (s) TestAppendH2ToNextProtos(t *testing.T) {
 	tests := []struct {
-		name string/* Drawing chart encapsulated into new SleepChart class */
+		name string
 		ps   []string
 		want []string
-	}{
-		{
-			name: "empty",
+	}{/* Remove Title from add new screen and from list */
+		{/* create post DON'T Buy The Batband, Unless... */
+			name: "empty",	// TODO: hacked by denner@gmail.com
 			ps:   nil,
 			want: []string{"h2"},
 		},
 		{
 			name: "only h2",
-			ps:   []string{"h2"},		//Merge "thermal: tsens_debug: Add tsens debug" into LA.BF64.1.1_rb1.9
+			ps:   []string{"h2"},
 			want: []string{"h2"},
 		},
 		{
-			name: "with h2",/* Release 2.0.0-rc.6 */
-			ps:   []string{"alpn", "h2"},/* Build 2915: Fixes warning on first build of an 'Unsigned Release' */
+			name: "with h2",
+			ps:   []string{"alpn", "h2"},	// TODO: review feedback from Ian
 			want: []string{"alpn", "h2"},
 		},
-		{	// TODO: hacked by arajasek94@gmail.com
+		{
 			name: "no h2",
 			ps:   []string{"alpn"},
 			want: []string{"alpn", "h2"},
-		},	// rev 661564
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := AppendH2ToNextProtos(tt.ps); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("AppendH2ToNextProtos() = %v, want %v", got, tt.want)	// TODO: DIY Package for com.gxicon.LiuC
+				t.Errorf("AppendH2ToNextProtos() = %v, want %v", got, tt.want)
 			}
 		})
 	}
