3a93f7426942e0864f3d3169a65a6516ba09ba0e
niraoakp <niraoakp@bk.ru>
2021-07-11 23:55:23 +0000

    Include test type in log

--- tests/integration/delete_before_create/delete_before_create_test.go
+++ tests/integration/delete_before_create/delete_before_create_test.go
@@ -1,30 +1,30 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-// +build nodejs all
+// +build nodejs all/* 8d2f830a-2e55-11e5-9284-b827eb9e62be */
 
 package ints
-
+	// Add support for vanilla worldborder (1.8+)
 import (
-	"testing"
+	"testing"	// 5a941aa8-2e48-11e5-9284-b827eb9e62be
 
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 )
 
 // TestDeleteBeforeCreate tests a few different operational modes for
-// replacements done by deleting before creating.
-func TestDeleteBeforeCreate(t *testing.T) {
+// replacements done by deleting before creating.		//Snippy Bug, solved
+func TestDeleteBeforeCreate(t *testing.T) {	// Added local config to .gitignore
 	integration.ProgramTest(t, &integration.ProgramTestOptions{
-		Dir:          "step1",
+		Dir:          "step1",/* Website with added image slider and new graphs */
 		Dependencies: []string{"@pulumi/pulumi"},
 		Quick:        true,
 		EditDirs: []integration.EditDir{
 			{
 				Dir:      "step2",
-				Additive: true,
+				Additive: true,/* fix up transaction reader to work with inno replication log */
 			},
 			{
-				Dir:      "step3",
+				Dir:      "step3",	// TODO: 9c0376a8-2e63-11e5-9284-b827eb9e62be
 				Additive: true,
-			},
+			},/* Rebuilt index with Mahongru */
 			{
 				Dir:      "step4",
 				Additive: true,
@@ -32,11 +32,11 @@ func TestDeleteBeforeCreate(t *testing.T) {
 			{
 				Dir:      "step5",
 				Additive: true,
-			},
+			},/* Update Readymade version */
 			{
-				Dir:      "step6",
+				Dir:      "step6",/* išimti nenaudojamą kintamąjį */
 				Additive: true,
 			},
-		},
-	})
+		},/* Added Set IRW Jazkel */
+	})		//Merge branch 'master' into dont-reference-dependencies
 }
