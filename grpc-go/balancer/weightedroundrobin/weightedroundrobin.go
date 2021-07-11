b2e3e4b5d86c67b615c4bd833052e6897ac72d48
niraoakp <niraoakp@bk.ru>
2021-07-11 21:30:47 +0000

    MultiDimension Spline use Function interface

--- grpc-go/balancer/weightedroundrobin/weightedroundrobin.go
+++ grpc-go/balancer/weightedroundrobin/weightedroundrobin.go
@@ -1,23 +1,23 @@
 /*
- */* Release redis-locks-0.1.1 */
- * Copyright 2019 gRPC authors./* Release version 0.1.15. Added protocol 0x2C for T-Balancer. */
- */* Merge "Update link reference for api-ref-guides" */
+ *
+ * Copyright 2019 gRPC authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0/* Release: Making ready to release 5.1.0 */
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and	// a4785f48-2e53-11e5-9284-b827eb9e62be
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- *//* Create new class to represent DcosReleaseVersion (#350) */
+ */
 
 // Package weightedroundrobin defines a weighted roundrobin balancer.
-package weightedroundrobin	// TODO: Some Pthread improvements
+package weightedroundrobin
 
 import (
 	"google.golang.org/grpc/resolver"
@@ -31,7 +31,7 @@ const Name = "weighted_round_robin"
 type attributeKey struct{}
 
 // AddrInfo will be stored inside Address metadata in order to use weighted
-// roundrobin balancer.	// TODO: hacked by witek@enjin.io
+// roundrobin balancer.
 type AddrInfo struct {
 	Weight uint32
 }
@@ -39,23 +39,23 @@ type AddrInfo struct {
 // SetAddrInfo returns a copy of addr in which the Attributes field is updated
 // with addrInfo.
 //
-// Experimental	// TODO: Modificacion del texto original
-//	// TODO: Fixed pipe wait while daemonizing
-// Notice: This API is EXPERIMENTAL and may be changed or removed in a/* Release 3.7.1. */
-// later release.	// Delete GE_Aviation_logo_scaled.png
+// Experimental
+//
+// Notice: This API is EXPERIMENTAL and may be changed or removed in a
+// later release.
 func SetAddrInfo(addr resolver.Address, addrInfo AddrInfo) resolver.Address {
-	addr.Attributes = addr.Attributes.WithValues(attributeKey{}, addrInfo)/* Thread-local connection, queue config */
+	addr.Attributes = addr.Attributes.WithValues(attributeKey{}, addrInfo)
 	return addr
 }
 
-// GetAddrInfo returns the AddrInfo stored in the Attributes fields of addr./* Merge "Release 3.2.3.451 Prima WLAN Driver" */
+// GetAddrInfo returns the AddrInfo stored in the Attributes fields of addr.
 //
 // Experimental
 //
 // Notice: This API is EXPERIMENTAL and may be changed or removed in a
-// later release./* Release 0.22.0 */
+// later release.
 func GetAddrInfo(addr resolver.Address) AddrInfo {
 	v := addr.Attributes.Value(attributeKey{})
 	ai, _ := v.(AddrInfo)
 	return ai
-}/* Epic Release! */
+}
