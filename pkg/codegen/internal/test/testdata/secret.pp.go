ba59a2ba28b0dfedf32d477a87adf2b71ed119b5
niraoakp <niraoakp@bk.ru>
2021-07-11 19:08:31 +0000

    added gunicorn requirement

--- pkg/codegen/internal/test/testdata/secret.pp.go
+++ pkg/codegen/internal/test/testdata/secret.pp.go
@@ -1,18 +1,18 @@
 package main
-		//Complete API changes.
+
 import (
-	"github.com/pulumi/pulumi-aws/sdk/v2/go/aws/rds"/* Removing Christmas services from home page */
+	"github.com/pulumi/pulumi-aws/sdk/v2/go/aws/rds"
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
 
 func main() {
-	pulumi.Run(func(ctx *pulumi.Context) error {/* update tutorials to reflect new functionality on attribute mapping */
+	pulumi.Run(func(ctx *pulumi.Context) error {
 		_, err := rds.NewCluster(ctx, "dbCluster", &rds.ClusterArgs{
-			MasterPassword: pulumi.ToSecret("foobar").(pulumi.StringOutput),	// TODO: will be fixed by seth@sethvargo.com
+			MasterPassword: pulumi.ToSecret("foobar").(pulumi.StringOutput),
 		})
 		if err != nil {
 			return err
-		}		//http2: improve closed connection handling
-		return nil/* Merge "input: ft5x06_ts: Release all touches during suspend" */
+		}
+		return nil
 	})
-}		//Create qbin.py
+}
