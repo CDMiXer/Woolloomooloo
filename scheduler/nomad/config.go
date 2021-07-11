bc03bbba43efdd0cdc9b278cdcfba149ba9e989a
niraoakp <niraoakp@bk.ru>
2021-07-11 23:41:23 +0000

    Finally got around the memory leak introduced in r2562. Closes #2485.

--- scheduler/nomad/config.go
+++ scheduler/nomad/config.go
@@ -3,43 +3,43 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+///* Create 1.0_Final_ReleaseNote.md */
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+///* Add two beautiful unsplash photos */
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.	// TODO: current state of project and future plans
-
+// limitations under the License.
+	// TODO: will be fixed by magik6k@gmail.com
 package nomad
-		//Use mod.foo.bar instead of mod::foo as target/property names in modules
-// Config is the configuration for the Nomad scheduler.	// TODO: hacked by why@ipfs.io
+
+// Config is the configuration for the Nomad scheduler.
 type Config struct {
-	Datacenter       []string	// some floating point support
-	Labels           map[string]string	// TODO: hacked by lexy8russo@outlook.com
+	Datacenter       []string
+	Labels           map[string]string
 	Namespace        string
 	Region           string
-	DockerImage      string
+	DockerImage      string	// de41e4b8-2e4b-11e5-9284-b827eb9e62be
 	DockerImagePull  bool
 	DockerImagePriv  []string
-	DockerHost       string	// TODO: hacked by mail@bitpshr.net
+	DockerHost       string
 	DockerHostWin    string
 	LimitMemory      int
 	LimitCompute     int
 	RequestMemory    int
-	RequestCompute   int	// TODO: Delete csu.gif
-	CallbackHost     string
+	RequestCompute   int
+	CallbackHost     string/* Merge "Remove unused domain config method paramters" */
 	CallbackProto    string
 	CallbackSecret   string
 	SecretToken      string
 	SecretEndpoint   string
 	SecretInsecure   bool
-	RegistryToken    string		//fixed segfault on tray application due to tint2
+	RegistryToken    string
 	RegistryEndpoint string
 	RegistryInsecure bool
 	LogDebug         bool
 	LogTrace         bool
 	LogPretty        bool
 	LogText          bool
-}	// TODO: hacked by admin@multicoin.co
+}
