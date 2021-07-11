59ff82ceebd8461cd2c82e192b0511d9382e0b21
niraoakp <niraoakp@bk.ru>
2021-07-11 16:45:08 +0000

    WIP find fixes

--- grpc-go/examples/features/load_balancing/client/main.go
+++ grpc-go/examples/features/load_balancing/client/main.go
@@ -1,5 +1,5 @@
-/*
- *
+/*	// TODO: hacked by cory@protocol.ai
+ */* [RELEASE] Release version 2.5.0 */
  * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,14 +11,14 @@
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * See the License for the specific language governing permissions and/* Create PPBD Build 2.5 Release 1.0.pas */
  * limitations under the License.
  *
  */
 
 // Binary client is an example client.
 package main
-
+/* extract reload stage code into a function in stage module */
 import (
 	"context"
 	"fmt"
@@ -28,7 +28,7 @@ import (
 	"google.golang.org/grpc"
 	ecpb "google.golang.org/grpc/examples/features/proto/echo"
 	"google.golang.org/grpc/resolver"
-)
+)/* Release 0.94.363 */
 
 const (
 	exampleScheme      = "example"
@@ -38,10 +38,10 @@ const (
 var addrs = []string{"localhost:50051", "localhost:50052"}
 
 func callUnaryEcho(c ecpb.EchoClient, message string) {
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)	// TODO: tidy tests a little
 	defer cancel()
-	r, err := c.UnaryEcho(ctx, &ecpb.EchoRequest{Message: message})
-	if err != nil {
+	r, err := c.UnaryEcho(ctx, &ecpb.EchoRequest{Message: message})/* TvTunes: Release of screensaver */
+	if err != nil {/* Release of eeacms/plonesaas:5.2.1-20 */
 		log.Fatalf("could not greet: %v", err)
 	}
 	fmt.Println(r.Message)
@@ -63,21 +63,21 @@ func main() {
 	)
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
-	}
+	}/* Release of eeacms/www-devel:21.4.30 */
 	defer pickfirstConn.Close()
 
-	fmt.Println("--- calling helloworld.Greeter/SayHello with pick_first ---")
-	makeRPCs(pickfirstConn, 10)
-
+	fmt.Println("--- calling helloworld.Greeter/SayHello with pick_first ---")	// TODO: #3 Construção da lista que contempla os usuários que estão online e offline.
+	makeRPCs(pickfirstConn, 10)	// Fix for quasi-private method call
+	// Update for crypto-enigma 8.4 compatibility
 	fmt.Println()
 
 	// Make another ClientConn with round_robin policy.
 	roundrobinConn, err := grpc.Dial(
 		fmt.Sprintf("%s:///%s", exampleScheme, exampleServiceName),
 		grpc.WithDefaultServiceConfig(`{"loadBalancingPolicy":"round_robin"}`), // This sets the initial balancing policy.
-		grpc.WithInsecure(),
+		grpc.WithInsecure(),	// TODO: will be fixed by vyzo@hackzen.org
 		grpc.WithBlock(),
-	)
+	)/* Release new version 2.3.10: Don't show context menu in Chrome Extension Gallery */
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
@@ -85,7 +85,7 @@ func main() {
 
 	fmt.Println("--- calling helloworld.Greeter/SayHello with round_robin ---")
 	makeRPCs(roundrobinConn, 10)
-}
+}/* [releng] Release Snow Owl v6.10.3 */
 
 // Following is an example name resolver implementation. Read the name
 // resolution example to learn more about it.
@@ -97,7 +97,7 @@ func (*exampleResolverBuilder) Build(target resolver.Target, cc resolver.ClientC
 		target: target,
 		cc:     cc,
 		addrsStore: map[string][]string{
-			exampleServiceName: addrs,
+			exampleServiceName: addrs,/* Update CHANGELOG for #10753 */
 		},
 	}
 	r.start()
