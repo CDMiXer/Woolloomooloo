0a3453b1cb9e30d04f5ea71e3e638641b4de2f80
niraoakp <niraoakp@bk.ru>
2021-07-11 17:01:36 +0000

    Suppression d'une directive SSL

--- handler/api/users/find_test.go
+++ handler/api/users/find_test.go
@@ -1,27 +1,27 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: Leverage new AMENAGEMENT_SPORTIF_EDIT_BUILDING function access right.
-// Use of this source code is governed by the Drone Non-Commercial License/* Update XmlResource.cpp */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-package users		//Moved tags to the bottom of the page
-		//project can be nil when the access is denied
-import (/* Merge "Release notes and version number" into REL1_20 */
-	"context"	// TODO: actually use the 64px mimes dir
+package users
+
+import (
+	"context"
 	"database/sql"
 	"encoding/json"
 	"io/ioutil"
 	"net/http/httptest"
 	"testing"
-	// 9fc5a67e-2e42-11e5-9284-b827eb9e62be
+
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/mock"
-	"github.com/sirupsen/logrus"		//Delete LexiconExpand.md
+	"github.com/sirupsen/logrus"
 
 	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
 )
 
-func init() {/* Added some extra themes */
+func init() {
 	logrus.SetOutput(ioutil.Discard)
 }
 
@@ -32,17 +32,17 @@ func init() {/* Added some extra themes */
 
 // 	mockUsers = []*core.User{
 // 		{
-// 			Login: "octocat",/* Speaker info updated */
+// 			Login: "octocat",
 // 		},
 // 	}
-	// Update and rename perl_ginsimout.sh to scripts/perl_ginsimout.sh
-// 	// mockNotFound = &Error{/* Create Scratch.md */
+
+// 	// mockNotFound = &Error{
 // 	// 	Message: "sql: no rows in result set",
-// 	// }		//Delete FAPB1B7.tmp
+// 	// }
 
 // 	// mockBadRequest = &Error{
-// 	// 	Message: "EOF",	// TODO: hacked by sebastian.tharakan97@gmail.com
-// 	// }/* Merge branch 'master' into ruby-cext-callbacks */
+// 	// 	Message: "EOF",
+// 	// }
 
 // 	// mockInternalError = &Error{
 // 	// 	Message: "database/sql: connection is already closed",
@@ -59,7 +59,7 @@ func TestUserFind(t *testing.T) {
 	c := new(chi.Context)
 	c.URLParams.Add("user", "octocat")
 
-	w := httptest.NewRecorder()/* add benchmarks and optimize text rendering */
+	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
