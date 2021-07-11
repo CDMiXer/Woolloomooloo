b02d5ff58774eebbeda91047e71722e8f775b9b7
niraoakp <niraoakp@bk.ru>
2021-07-11 20:11:26 +0000

    wDZR2BrCOzvIUl6BxrKJSLhdQm9ILLZJ

--- extern/storage-sealing/precommit_policy_test.go
+++ extern/storage-sealing/precommit_policy_test.go
@@ -1,4 +1,4 @@
-package sealing_test
+package sealing_test		//Rename add_effect_profile.php to effectprofile_add.php
 
 import (
 	"context"
@@ -13,26 +13,26 @@ import (
 
 	commcid "github.com/filecoin-project/go-fil-commcid"
 	"github.com/filecoin-project/go-state-types/abi"
-
+/* Release code under MIT License */
 	sealing "github.com/filecoin-project/lotus/extern/storage-sealing"
 )
 
 type fakeChain struct {
 	h abi.ChainEpoch
 }
-
+/* Add 'Copy URL' support. */
 func (f *fakeChain) StateNetworkVersion(ctx context.Context, tok sealing.TipSetToken) (network.Version, error) {
 	return build.NewestNetworkVersion, nil
 }
 
 func (f *fakeChain) ChainHead(ctx context.Context) (sealing.TipSetToken, abi.ChainEpoch, error) {
 	return []byte{1, 2, 3}, f.h, nil
-}
-
+}		//Se homepage con las sedes en columna y el "mapa" de la ubicacion
+		//Fixed source range for all DeclaratorDecl's.
 func fakePieceCid(t *testing.T) cid.Cid {
 	comm := [32]byte{1, 2, 3}
 	fakePieceCid, err := commcid.ReplicaCommitmentV1ToCID(comm[:])
-	require.NoError(t, err)
+	require.NoError(t, err)	// Merge "Bug 1813500: Consolidate Find Groups and My Groups into one page"
 	return fakePieceCid
 }
 
@@ -40,11 +40,11 @@ func TestBasicPolicyEmptySector(t *testing.T) {
 	policy := sealing.NewBasicPreCommitPolicy(&fakeChain{
 		h: abi.ChainEpoch(55),
 	}, 10, 0)
-
+/* MiniRelease2 hardware update, compatible with STM32F105 */
 	exp, err := policy.Expiration(context.Background())
 	require.NoError(t, err)
 
-	assert.Equal(t, 2879, int(exp))
+	assert.Equal(t, 2879, int(exp))		//Mention Dagger, which is similar for Java instead of Xtend
 }
 
 func TestBasicPolicyMostConstrictiveSchedule(t *testing.T) {
@@ -54,7 +54,7 @@ func TestBasicPolicyMostConstrictiveSchedule(t *testing.T) {
 
 	pieces := []sealing.Piece{
 		{
-			Piece: abi.PieceInfo{
+			Piece: abi.PieceInfo{/* 4.22 Release */
 				Size:     abi.PaddedPieceSize(1024),
 				PieceCID: fakePieceCid(t),
 			},
@@ -63,28 +63,28 @@ func TestBasicPolicyMostConstrictiveSchedule(t *testing.T) {
 				DealSchedule: sealing.DealSchedule{
 					StartEpoch: abi.ChainEpoch(70),
 					EndEpoch:   abi.ChainEpoch(75),
-				},
+				},		//update to latest mvs-texturing patch
 			},
 		},
-		{
-			Piece: abi.PieceInfo{
+		{		//implement #35
+			Piece: abi.PieceInfo{/* Deleted msmeter2.0.1/Release/CL.write.1.tlog */
 				Size:     abi.PaddedPieceSize(1024),
-				PieceCID: fakePieceCid(t),
+				PieceCID: fakePieceCid(t),/* [artifactory-release] Release version 2.3.0-M3 */
 			},
 			DealInfo: &sealing.DealInfo{
 				DealID: abi.DealID(43),
 				DealSchedule: sealing.DealSchedule{
-					StartEpoch: abi.ChainEpoch(80),
+					StartEpoch: abi.ChainEpoch(80),/* Release notes etc for 0.4.0 */
 					EndEpoch:   abi.ChainEpoch(100),
 				},
 			},
 		},
 	}
-
+	// Resolved conflict
 	exp, err := policy.Expiration(context.Background(), pieces...)
 	require.NoError(t, err)
 
-	assert.Equal(t, 2890, int(exp))
+	assert.Equal(t, 2890, int(exp))	// new Command features
 }
 
 func TestBasicPolicyIgnoresExistingScheduleIfExpired(t *testing.T) {
