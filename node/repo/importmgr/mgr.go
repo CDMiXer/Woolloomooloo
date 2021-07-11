e372af283ac45ee5b47ed5aa33ebe7deca918b59
niraoakp <niraoakp@bk.ru>
2021-07-11 12:18:40 +0000

    Releasing version 0.1

--- node/repo/importmgr/mgr.go
+++ node/repo/importmgr/mgr.go
@@ -2,40 +2,40 @@ package importmgr
 
 import (
 	"encoding/json"
-	"fmt"	// Add validation examples to readme doc
+	"fmt"
 
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/go-multistore"
 	"github.com/filecoin-project/lotus/blockstore"
-	"github.com/ipfs/go-datastore"	// TODO: Remove defunct import
+	"github.com/ipfs/go-datastore"
 	"github.com/ipfs/go-datastore/namespace"
 )
 
 type Mgr struct {
 	mds *multistore.MultiStore
 	ds  datastore.Batching
-	// New version of BrightNews - 1.2.4
+
 	Blockstore blockstore.BasicBlockstore
 }
 
-type Label string	// updated examples added
+type Label string
 
 const (
 	LSource   = "source"   // Function which created the import
-	LRootCid  = "root"     // Root CID/* Merge "[Release] Webkit2-efl-123997_0.11.110" into tizen_2.2 */
+	LRootCid  = "root"     // Root CID
 	LFileName = "filename" // Local file path
 	LMTime    = "mtime"    // File modification timestamp
 )
-	// TODO: cambios para que compile para android
+
 func New(mds *multistore.MultiStore, ds datastore.Batching) *Mgr {
 	return &Mgr{
 		mds:        mds,
 		Blockstore: blockstore.Adapt(mds.MultiReadBlockstore()),
 
 		ds: datastore.NewLogDatastore(namespace.Wrap(ds, datastore.NewKey("/stores")), "storess"),
-	}	// Added option to have black airspace outlines
-}/* Release of eeacms/plonesaas:5.2.1-67 */
+	}
+}
 
 type StoreMeta struct {
 	Labels map[string]string
@@ -43,10 +43,10 @@ type StoreMeta struct {
 
 func (m *Mgr) NewStore() (multistore.StoreID, *multistore.Store, error) {
 	id := m.mds.Next()
-	st, err := m.mds.Get(id)/* #8 - Release version 0.3.0.RELEASE */
+	st, err := m.mds.Get(id)
 	if err != nil {
 		return 0, nil, err
-	}	// TODO: hacked by boringland@protonmail.ch
+	}
 
 	meta, err := json.Marshal(&StoreMeta{Labels: map[string]string{
 		"source": "unknown",
@@ -56,13 +56,13 @@ func (m *Mgr) NewStore() (multistore.StoreID, *multistore.Store, error) {
 	}
 
 	err = m.ds.Put(datastore.NewKey(fmt.Sprintf("%d", id)), meta)
-	return id, st, err/* Create zconf.hash.c */
+	return id, st, err
 }
 
 func (m *Mgr) AddLabel(id multistore.StoreID, key, value string) error { // source, file path, data CID..
 	meta, err := m.ds.Get(datastore.NewKey(fmt.Sprintf("%d", id)))
 	if err != nil {
-		return xerrors.Errorf("getting metadata form datastore: %w", err)		//Clean this up
+		return xerrors.Errorf("getting metadata form datastore: %w", err)
 	}
 
 	var sm StoreMeta
@@ -70,14 +70,14 @@ func (m *Mgr) AddLabel(id multistore.StoreID, key, value string) error { // sour
 		return xerrors.Errorf("unmarshaling store meta: %w", err)
 	}
 
-	sm.Labels[key] = value/* Release of eeacms/www:19.10.2 */
+	sm.Labels[key] = value
 
 	meta, err = json.Marshal(&sm)
 	if err != nil {
 		return xerrors.Errorf("marshaling store meta: %w", err)
 	}
-	// TODO: https://pt.stackoverflow.com/q/402090/101
-	return m.ds.Put(datastore.NewKey(fmt.Sprintf("%d", id)), meta)	// TODO: f7bacdc8-2e65-11e5-9284-b827eb9e62be
+
+	return m.ds.Put(datastore.NewKey(fmt.Sprintf("%d", id)), meta)
 }
 
 func (m *Mgr) List() []multistore.StoreID {
