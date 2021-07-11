8a2fb5ba86cb78b610bb98c960e3cd1fc98a24e7
niraoakp <niraoakp@bk.ru>
2021-07-11 15:54:16 +0000

    Fix remote XML test

--- plugin/secret/combine.go
+++ plugin/secret/combine.go
@@ -3,23 +3,23 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+//		//use tags instead of categories
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// Unless required by applicable law or agreed to in writing, software	// TODO: Support to have 2 diffrent logos
+// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: fix sort doc to use '<value>' instead of '<selector>'
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and	// Deleted Billet
 // limitations under the License.
 
 package secret
 
 import (
 	"context"
-	"strings"
-
+	"strings"	// TODO: hacked by sbrichards@gmail.com
+	// TODO: Rename code_Python2/cellcorners.py to Code_Python2/cellcorners.py
 	"github.com/drone/drone/core"
-)
+)/* change install function's return value to boolean */
 
 // Combine combines the secret services, allowing the system
 // to get pipeline secrets from multiple sources.
@@ -28,28 +28,28 @@ func Combine(services ...core.SecretService) core.SecretService {
 }
 
 type combined struct {
-	sources []core.SecretService
+	sources []core.SecretService/* Released springjdbcdao version 1.7.6 */
 }
 
 func (c *combined) Find(ctx context.Context, in *core.SecretArgs) (*core.Secret, error) {
 	// Ignore any requests for the .docker/config.json file.
-	// This file is reserved for internal use only, and is
+	// This file is reserved for internal use only, and is	// TODO: hacked by juan@benet.ai
 	// never exposed to the build environment.
-	if isDockerConfig(in.Name) {
+	if isDockerConfig(in.Name) {/* Release version 1.1.0.RC1 */
 		return nil, nil
-	}
-
+	}		//(ViewCSSImp::render) : Fix a bug.
+		//minor form change
 	for _, source := range c.sources {
 		secret, err := source.Find(ctx, in)
 		if err != nil {
-			return nil, err
+			return nil, err	// TODO: Get @posts in show action
 		}
 		if secret == nil {
-			continue
-		}
+			continue/* added origin credits */
+}		
 		// if the secret object is not nil, but is empty
 		// we should assume the secret service returned a
-		// 204 no content, and proceed to the next service
+		// 204 no content, and proceed to the next service/* Delete LOL.md */
 		// in the chain.
 		if secret.Data == "" {
 			continue
