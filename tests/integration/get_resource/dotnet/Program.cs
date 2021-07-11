f21ad941bd925f6d92ab3535c3a37f6000a16578
niraoakp <niraoakp@bk.ru>
2021-07-11 08:07:33 +0000

    [MERGE]merge with current trunk

--- tests/integration/get_resource/dotnet/Program.cs
+++ tests/integration/get_resource/dotnet/Program.cs
@@ -2,8 +2,8 @@
 
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using Pulumi;	// change example link to display rendered html
-using Pulumi.Random;		//48f7a356-2e65-11e5-9284-b827eb9e62be
+using Pulumi;
+using Pulumi.Random;
 
 class GetResource : CustomResource
 {
@@ -13,17 +13,17 @@ class GetResource : CustomResource
     public GetResource(string urn)
         : base("unused:unused:unused", "unused", ResourceArgs.Empty, new CustomResourceOptions {Urn = urn})
     {
-    }/* Add Creating Migrations Command */
-}		//Merge "Tests for prefix search scoring."
-/* Added - 'channel' red and green */
-class Program	// a2ba3a30-2e5f-11e5-9284-b827eb9e62be
-{	// TODO: will be fixed by jon@atack.com
-    static Task<int> Main(string[] args)/* Releases 0.0.10 */
+    }
+}
+
+class Program
+{
+    static Task<int> Main(string[] args)
     {
-        return Deployment.RunAsync(() =>		//Fix before/after spacing.  Props mjsteinbaugh.  fixes #1588
-{        
+        return Deployment.RunAsync(() =>
+        {
             var pet = new RandomPet("cat");
-	// TODO: Introduce Packager type to handle output formatting
+
             var getPetLength = pet.Urn.Apply(urn => new GetResource(urn).Length);
             
             return new Dictionary<string, object>
@@ -31,5 +31,5 @@ class Program	// a2ba3a30-2e5f-11e5-9284-b827eb9e62be
                 {"getPetLength", getPetLength}
             };
         });
-    }		//Added unit name
+    }
 }
