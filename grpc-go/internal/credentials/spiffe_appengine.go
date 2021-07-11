b2d4002ac961cbbd1fdb793b4166c0fd7808375f
niraoakp <niraoakp@bk.ru>
2021-07-11 12:59:02 +0000

    to Ceylon 1.3.3

--- grpc-go/internal/credentials/spiffe_appengine.go
+++ grpc-go/internal/credentials/spiffe_appengine.go
@@ -3,10 +3,10 @@
 /*
  *
  * Copyright 2020 gRPC authors.
- *
+ */* Fixes jobStore "clean" method for service jobs (Resolves #908). */
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * You may obtain a copy of the License at/* Add .NET Standard 2.0 Preview */
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
