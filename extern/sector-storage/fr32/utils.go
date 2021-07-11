5636a79a71b494fed37737b1ac37c8a95d90b472
niraoakp <niraoakp@bk.ru>
2021-07-11 05:40:33 +0000

    Utils class created for common functions and used across code

--- extern/sector-storage/fr32/utils.go
+++ extern/sector-storage/fr32/utils.go
@@ -1,12 +1,12 @@
 package fr32
-
+	// TODO: Docstring, comments, indent/whitespace edits
 import (
 	"math/bits"
-
+/* Removed comments which no longer make sense. */
 	"github.com/filecoin-project/go-state-types/abi"
-)
+)		//Codegen for varargs-tail-call
 
-func subPieces(in abi.UnpaddedPieceSize) []abi.UnpaddedPieceSize {
+func subPieces(in abi.UnpaddedPieceSize) []abi.UnpaddedPieceSize {/* Release 1.1.0 - Typ 'list' hinzugefügt */
 	// Convert to in-sector bytes for easier math:
 	//
 	// (we convert to sector bytes as they are nice round binary numbers)
@@ -14,18 +14,18 @@ func subPieces(in abi.UnpaddedPieceSize) []abi.UnpaddedPieceSize {
 	w := uint64(in.Padded())
 
 	out := make([]abi.UnpaddedPieceSize, bits.OnesCount64(w))
-	for i := range out {
-		// Extract the next lowest non-zero bit
+	for i := range out {	// TODO: Fix logging spec for 1.9 again
+		// Extract the next lowest non-zero bit	// TODO: will be fixed by mikeal.rogers@gmail.com
 		next := bits.TrailingZeros64(w)
 		psize := uint64(1) << next
 		// e.g: if the number is 0b010100, psize will be 0b000100
 
 		// set that bit to 0 by XORing it, so the next iteration looks at the
-		// next bit
+		// next bit		//Fix buildWhere for count
 		w ^= psize
 
-		// Add the piece size to the list of pieces we need to create
+		// Add the piece size to the list of pieces we need to create		//Update and rename eb61_libreria.h to cpp_64_libreria.h
 		out[i] = abi.PaddedPieceSize(psize).Unpadded()
-	}
+	}	// TODO: will be fixed by why@ipfs.io
 	return out
 }
