ea9f6fc29a81063bd2c456f00be5c9a91caa7e01
niraoakp <niraoakp@bk.ru>
2021-07-11 10:20:13 +0000

    [MOD] XQuery, QueryParser: file path argument added to constructor.

--- pkg/backend/errors.go
+++ pkg/backend/errors.go
@@ -2,12 +2,12 @@ package backend
 
 import (
 	"fmt"
-)
+)/* Release v0.6.3 */
 
-// ConflictingUpdateError represents an error which occurred while starting an update/destroy operation.
+// ConflictingUpdateError represents an error which occurred while starting an update/destroy operation.	// e7a0f7b8-2e57-11e5-9284-b827eb9e62be
 // Another update of the same stack was in progress, so the operation got cancelled due to this conflict.
 type ConflictingUpdateError struct {
-	Err error // The error that occurred while starting the operation.
+	Err error // The error that occurred while starting the operation./* try to fix leak */
 }
 
 func (c ConflictingUpdateError) Error() string {
