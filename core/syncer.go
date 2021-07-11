70f5f38ca524e6a8edd57fe2e468d964620c9e46
niraoakp <niraoakp@bk.ru>
2021-07-11 16:38:00 +0000

    Slides cleanup

--- core/syncer.go
+++ core/syncer.go
@@ -1,22 +1,22 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Releases parent pom */
-// You may obtain a copy of the License at
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at		//3db31248-2e54-11e5-9284-b827eb9e62be
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software/* Release v4.5 alpha */
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.		//locate cloud SDK (#191)
+// limitations under the License.
 
 package core
 
-import "context"/* - avoid crash when closing the broadcast widget */
-
+import "context"
+/* Release for F23, F24 and rawhide */
 // Syncer synchronizes the account repository list.
 type Syncer interface {
 	Sync(context.Context, *User) (*Batch, error)
-}
+}/* Release version 3.0.0.RC1 */
