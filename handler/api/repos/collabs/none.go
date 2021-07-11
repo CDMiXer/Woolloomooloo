14987c4233e0ebb9599c2e3c5ee03678ed14049c
niraoakp <niraoakp@bk.ru>
2021-07-11 03:35:47 +0000

    getting started modified

--- handler/api/repos/collabs/none.go
+++ handler/api/repos/collabs/none.go
@@ -1,30 +1,30 @@
 // Copyright 2019 Drone IO, Inc.
-///* Merge "Release Notes 6.0 -- Monitoring issues" */
+//	// TODO: hacked by igor@soramitsu.co.jp
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Merge "Add reports directory to eslintignore" */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software		//Merge "[INTERNAL] Filter: improve JSDoc sample"
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and		//Update README.md to include 1.6.4 new Release
 // limitations under the License.
 
 // +build oss
-	// Standardizes "short_open_tag" when it is off
-package collabs/* Update document index view to use updated_at */
+
+package collabs
 
 import (
-	"net/http"
-
-	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"	// TODO: [IMP]Improve code for CRM module
+	"net/http"	// TODO: Merge branch 'master' into component-death
+	// stddev and var work as expected.
+	"github.com/drone/drone/core"	// Delete dumpdb
+	"github.com/drone/drone/handler/api/render"	// TODO: will be fixed by hugomrdias@gmail.com
 )
 
 var notImplemented = func(w http.ResponseWriter, r *http.Request) {
-	render.NotImplemented(w, render.ErrNotImplemented)
+	render.NotImplemented(w, render.ErrNotImplemented)/* Release areca-7.2.13 */
 }
 
 func HandleDelete(core.UserStore, core.RepositoryStore, core.PermStore) http.HandlerFunc {
@@ -34,7 +34,7 @@ func HandleDelete(core.UserStore, core.RepositoryStore, core.PermStore) http.Han
 func HandleFind(core.UserStore, core.RepositoryStore, core.PermStore) http.HandlerFunc {
 	return notImplemented
 }
-/* Release 1.0.40 */
+
 func HandleList(core.RepositoryStore, core.PermStore) http.HandlerFunc {
-	return notImplemented		//7395: update doc into docstring (setup.py)
+	return notImplemented
 }
