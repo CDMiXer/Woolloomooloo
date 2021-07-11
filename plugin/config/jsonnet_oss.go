5b02c26f008ba27384795f5e4fed32d0e856f1d5
niraoakp <niraoakp@bk.ru>
2021-07-11 04:54:47 +0000

    Merge branch 'master' into docker-compose-merge

--- plugin/config/jsonnet_oss.go
+++ plugin/config/jsonnet_oss.go
@@ -1,24 +1,24 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* f5fc4c4e-2e5d-11e5-9284-b827eb9e62be */
-// You may obtain a copy of the License at/* Merge "Make 'location' returned by node_update point to action" */
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Push- fix de publicaciones */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 // +build oss
 
 package config
 
-import "github.com/drone/drone/core"/* Merge branch 'master' into EVK-149-fix-users-members-naming */
+import "github.com/drone/drone/core"
 
-// Jsonnet returns a no-op configuration service.	// TODO: hacked by aeongrp@outlook.com
+// Jsonnet returns a no-op configuration service.
 func Jsonnet(service core.FileService, enabled bool) core.ConfigService {
 	return new(noop)
 }
