27e0a59080a1a1098fb6a78ff88f5a6b1f5f48b2
niraoakp <niraoakp@bk.ru>
2021-07-11 16:35:22 +0000

    Update lazyloading placeholder image

--- gen/mask_safe.go
+++ gen/mask_safe.go
@@ -1,14 +1,14 @@
 // Copyright 2016 The Gorilla WebSocket Authors. All rights reserved.  Use of
 // this source code is governed by a BSD-style license that can be found in the
-// LICENSE file./* Examples and Showcase updated with Release 16.10.0 */
+// LICENSE file.
 
 // +build appengine
 
-package websocket/* Release 0.0.11.  Mostly small tweaks for the pi. */
-		//Merge "Activity log is re-implemented for dynamic load"
+package websocket
+
 func maskBytes(key [4]byte, pos int, b []byte) int {
 	for i := range b {
-		b[i] ^= key[pos&3]	// Create cert-perfil-2.PNG
+		b[i] ^= key[pos&3]
 		pos++
 	}
 	return pos & 3
