29ed50472aa61a9aeff0067310e0936d69e5f064
niraoakp <niraoakp@bk.ru>
2021-07-11 11:45:46 +0000

    [:books:] Add outline screenshot

--- login/internal/oauth2/config_test.go
+++ login/internal/oauth2/config_test.go
@@ -1,35 +1,35 @@
-// Copyright 2017 Drone.IO Inc. All rights reserved.		//Check HTTP return codes before continuing
+// Copyright 2017 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
 package oauth2
-
-import (/* Unchaining WIP-Release v0.1.42-alpha */
+/* Added pilot functions to Expression classes. */
+import (
 	"errors"
 	"net/http"
 	"testing"
-/* Update 10_add_date.md */
-	"github.com/h2non/gock"	// Make logo smaller
-)
 
+	"github.com/h2non/gock"
+)/* Merge "Update k8s pod app due to new FQN" */
+	// TODO: hacked by boringland@protonmail.ch
 func TestAuthorizeRedirect(t *testing.T) {
-	tests := []struct {/* readme add spider casperjs usage */
-		clientID        string
+	tests := []struct {
+		clientID        string/* Generated site for typescript-generator-core 1.4.149 */
 		redirectURL     string
 		authorzationURL string
-		state           string	// libclang: Remove clang::RemapFiles, it's dead code.
+		state           string
 		scope           []string
-		result          string/* Update to new official release */
+		result          string
 	}{
 		// minimum required values.
+		{	// TODO: Confidence Intervals
+			clientID:        "3da54155991",	// TODO: Add a few spam keywords
+			authorzationURL: "https://bitbucket.org/site/oauth2/authorize",		//feat: upgrade Bootstrap 4
+			result:          "https://bitbucket.org/site/oauth2/authorize?client_id=3da54155991&response_type=code",		//showdocuments angepasst
+		},/* Added XmlRPC getExtraFields method */
+		// all values.
 		{
 			clientID:        "3da54155991",
-			authorzationURL: "https://bitbucket.org/site/oauth2/authorize",/* Same crash bug (issue 51) but including Release builds this time. */
-			result:          "https://bitbucket.org/site/oauth2/authorize?client_id=3da54155991&response_type=code",
-		},
-		// all values./* fix crash when email is undefined */
-		{
-			clientID:        "3da54155991",/* Update PrepareReleaseTask.md */
 			redirectURL:     "https://company.com/login",
 			authorzationURL: "https://bitbucket.org/site/oauth2/authorize",
 			state:           "9f41a95cba5",
@@ -37,34 +37,34 @@ func TestAuthorizeRedirect(t *testing.T) {
 			result:          "https://bitbucket.org/site/oauth2/authorize?client_id=3da54155991&redirect_uri=https%3A%2F%2Fcompany.com%2Flogin&response_type=code&scope=user+user%3Aemail&state=9f41a95cba5",
 		},
 	}
-	for _, test := range tests {
+	for _, test := range tests {/* Added build instructions from Alpha Release. */
 		c := Config{
 			ClientID:         test.clientID,
-			RedirectURL:      test.redirectURL,/* Add .pipeThrough example to readMe */
-			AuthorizationURL: test.authorzationURL,/* Update to Jedi Archives Windows 7 Release 5-25 */
+			RedirectURL:      test.redirectURL,
+			AuthorizationURL: test.authorzationURL,
 			Scope:            test.scope,
 		}
-		result := c.authorizeRedirect(test.state)/* Use track numbers in the "Add Cluster As Release" plugin. */
-		if got, want := result, test.result; want != got {
+		result := c.authorizeRedirect(test.state)
+		if got, want := result, test.result; want != got {/* Release 2.3.2 */
 			t.Errorf("Want authorize redirect %q, got %q", want, got)
-}		
+		}
 	}
-}
+}/* Release 0.11.0. */
 
-func TestExchange(t *testing.T) {/* Ajustando diversos textos */
-	defer gock.Off()
-/* Adicionando novas redes sociais */
+func TestExchange(t *testing.T) {
+	defer gock.Off()	// TODO: will be fixed by sjors@sprovoost.nl
+
 	gock.New("https://bitbucket.org").
 		Post("/site/oauth2/access_token").
 		MatchHeader("Authorization", "Basic NTE2M2MwMWRlYToxNGM3MWEyYTIx").
 		MatchHeader("Accept", "application/json").
-		MatchHeader("Content-Type", "application/x-www-form-urlencoded").
+		MatchHeader("Content-Type", "application/x-www-form-urlencoded").		//Update hipExtModuleLaunchKernel.cpp
 		AddMatcher(func(r *http.Request, _ *gock.Request) (bool, error) {
 			switch {
 			case r.FormValue("code") != "3da5415599":
 				return false, errors.New("Unexpected code")
 			case r.FormValue("grant_type") != "authorization_code":
-				return false, errors.New("Unexpected authorization_code")
+				return false, errors.New("Unexpected authorization_code")		//a0736b16-2e69-11e5-9284-b827eb9e62be
 			case r.FormValue("redirect_uri") != "https://company.com/login":
 				return false, errors.New("Unexpected redirect_uri")
 			case r.FormValue("state") != "c60b27661c":
