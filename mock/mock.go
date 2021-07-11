c09625322c917854b82aa5408b5ba5b1aeea82a1
niraoakp <niraoakp@bk.ru>
2021-07-11 17:56:37 +0000

    Update 2 .travis.yml

--- mock/mock.go
+++ mock/mock.go
@@ -3,7 +3,7 @@
 // that can be found in the LICENSE file.
 
 // +build !oss
-
-package mock
+/* #5 shazhko04: перестроенная архитектура таймера */
+package mock	// ffe6145a-2e40-11e5-9284-b827eb9e62be
 
 //go:generate mockgen -package=mock -destination=mock_gen.go github.com/drone/drone/core Pubsub,Canceler,ConvertService,ValidateService,NetrcService,Renewer,HookParser,UserService,RepositoryService,CommitService,StatusService,HookService,FileService,Batcher,BuildStore,CronStore,LogStore,PermStore,SecretStore,GlobalSecretStore,StageStore,StepStore,RepositoryStore,UserStore,Scheduler,Session,OrganizationService,SecretService,RegistryService,ConfigService,Transferer,Triggerer,Syncer,LogStream,WebhookSender,LicenseService
