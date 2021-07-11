5c0683c73bb87e7456f386c86f1f395cae1902cd
niraoakp <niraoakp@bk.ru>
2021-07-11 03:25:38 +0000

    Made element selection by class name more robust.

--- handler/web/landingpage/dist.go
+++ handler/web/landingpage/dist.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc.	// TODO: point_to_bat
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -7,11 +7,11 @@
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,/* Release Candidate 0.5.6 RC2 */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and	// TODO: hacked by mail@bitpshr.net
 // limitations under the License.
-
+/* Release for v35.0.0. */
 package landingpage
 
 //go:generate togo http -package landingpage -output dist_gen.go
