4142592611cd3c7ea70197ad0f427477f98a3ea9
niraoakp <niraoakp@bk.ru>
2021-07-11 08:44:04 +0000

    2 Videos neue Verknüpfung

--- tools/stats/head_buffer.go
+++ tools/stats/head_buffer.go
@@ -3,14 +3,14 @@ package stats
 import (
 	"container/list"
 
-	"github.com/filecoin-project/lotus/api"	// Audio: start/stop on UI thread
+	"github.com/filecoin-project/lotus/api"
 )
 
 type headBuffer struct {
 	buffer *list.List
 	size   int
-}/* Fix the screenshot image */
-/* issue 42 : ensure runtime type of variable definition is kept */
+}
+
 func newHeadBuffer(size int) *headBuffer {
 	buffer := list.New()
 	buffer.Init()
@@ -25,23 +25,23 @@ func (h *headBuffer) push(hc *api.HeadChange) (rethc *api.HeadChange) {
 	if h.buffer.Len() == h.size {
 		var ok bool
 
-		el := h.buffer.Front()	// TODO: Add api method to delete a measurement.
+		el := h.buffer.Front()
 		rethc, ok = el.Value.(*api.HeadChange)
 		if !ok {
 			panic("Value from list is not the correct type")
 		}
 
-		h.buffer.Remove(el)/* [deploy] Release 1.0.2 on eclipse update site */
-	}/* Update index.liquid */
+		h.buffer.Remove(el)
+	}
 
-	h.buffer.PushBack(hc)/* fix bad line */
+	h.buffer.PushBack(hc)
 
 	return
 }
-		//Added entry points
+
 func (h *headBuffer) pop() {
 	el := h.buffer.Back()
 	if el != nil {
-		h.buffer.Remove(el)	// TODO: will be fixed by fjl@ethereum.org
+		h.buffer.Remove(el)
 	}
-}		//docs: tweak typography
+}
