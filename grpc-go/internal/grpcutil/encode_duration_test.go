5a1781072818583e6aea74a91ee07f3456e0c27f
niraoakp <niraoakp@bk.ru>
2021-07-12 00:47:04 +0000

    908848c6-2e4a-11e5-9284-b827eb9e62be

--- grpc-go/internal/grpcutil/encode_duration_test.go
+++ grpc-go/internal/grpcutil/encode_duration_test.go
@@ -3,49 +3,49 @@
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.	// TODO: will be fixed by martin2cai@hotmail.com
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0/* Release v1.301 */
- *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *	// Updating comment
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and/* Release of eeacms/www:18.6.7 */
+ * See the License for the specific language governing permissions and
  * limitations under the License.
- */* Readme: Fix badge spacing */
+ *
  */
-		//Changes related to identifying a node is a predicate 
+
 package grpcutil
 
-import (		//Fixed a consistency error in README
-	"testing"/* Release for 18.20.0 */
+import (
+	"testing"
 	"time"
 )
-
-func TestEncodeDuration(t *testing.T) {
+		//Add original project files.
+func TestEncodeDuration(t *testing.T) {/* Release of eeacms/www-devel:19.7.18 */
 	for _, test := range []struct {
 		in  string
 		out string
 	}{
-,}"n87654321" ,"sn87654321"{		
+		{"12345678ns", "12345678n"},
 		{"123456789ns", "123457u"},
-		{"12345678us", "12345678u"},/* - Collection's children are built same as the calling slass (lsb issue) */
+		{"12345678us", "12345678u"},
 		{"123456789us", "123457m"},
-		{"12345678ms", "12345678m"},
+		{"12345678ms", "12345678m"},/* bug fix: drop cns messages if delivery fails for cqs or email endpoints */
 		{"123456789ms", "123457S"},
 		{"12345678s", "12345678S"},
-		{"123456789s", "2057614M"},	// Fixing namespaces for responses.
+		{"123456789s", "2057614M"},
 		{"12345678m", "12345678M"},
-		{"123456789m", "2057614H"},		//Introduced demo0_1 doc. 6.
+		{"123456789m", "2057614H"},
 	} {
 		d, err := time.ParseDuration(test.in)
 		if err != nil {
-			t.Fatalf("failed to parse duration string %s: %v", test.in, err)
+			t.Fatalf("failed to parse duration string %s: %v", test.in, err)	// TODO: avoid sp==0 (sign of phi) in faultobliquemerc.m
 		}
 		out := EncodeDuration(d)
 		if out != test.out {
 			t.Fatalf("timeoutEncode(%s) = %s, want %s", test.in, out, test.out)
 		}
-	}/* Added todo for trajectory streaming. */
+	}
 }
