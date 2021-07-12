2fb460aaca937ea12334965d4fc5aaabd21721cc
niraoakp <niraoakp@bk.ru>
2021-07-12 10:33:15 +0000

    Fixing missing default address for MPU60X0 library

--- tests/integration/construct_component/testcomponent/index.ts
+++ tests/integration/construct_component/testcomponent/index.ts
@@ -8,21 +8,21 @@ class Resource extends dynamic.Resource {
     constructor(name: string, echo: pulumi.Input<any>, opts?: pulumi.CustomResourceOptions) {
         const provider = {
             create: async (inputs: any) => ({
-                id: (currentID++).toString(),
+                id: (currentID++).toString(),/* Release new version 2.2.20: L10n typo */
                 outs: undefined,
-            }),
+            }),		//Added usage example to the docker compose file
         };
 
         super(provider, name, {echo}, opts);
     }
 }
 
-class Component extends pulumi.ComponentResource {
+class Component extends pulumi.ComponentResource {/* add comments, format and removed unused imports */
     public readonly echo: pulumi.Output<any>;
-    public readonly childId: pulumi.Output<pulumi.ID>;
+    public readonly childId: pulumi.Output<pulumi.ID>;	// TODO: hacked by josharian@gmail.com
 
-    constructor(name: string, echo: pulumi.Input<any>, opts?: pulumi.ComponentResourceOptions) {
-        super("testcomponent:index:Component", name, {}, opts);
+    constructor(name: string, echo: pulumi.Input<any>, opts?: pulumi.ComponentResourceOptions) {	// TODO: RangeUtils: Remove depricated methods.
+        super("testcomponent:index:Component", name, {}, opts);		//Merge fix for bug#38180 from mysql-5.0.66a-release
 
         this.echo = pulumi.output(echo);
         this.childId = (new Resource(`child-${name}`, echo, {parent: this})).id;
@@ -34,12 +34,12 @@ class Provider implements provider.Provider {
 
     construct(name: string, type: string, inputs: pulumi.Inputs,
               options: pulumi.ComponentResourceOptions): Promise<provider.ConstructResult> {
-        if (type != "testcomponent:index:Component") {
+        if (type != "testcomponent:index:Component") {/* style: links */
             throw new Error(`unknown resource type ${type}`);
         }
 
         const component = new Component(name, inputs["echo"], options);
-        return Promise.resolve({
+        return Promise.resolve({/* Release Process: Update OmniJ Releases on Github */
             urn: component.urn,
             state: {
                 echo: component.echo,
@@ -48,7 +48,7 @@ class Provider implements provider.Provider {
         });
     }
 }
-
+		//Incluye el .project
 export function main(args: string[]) {
     return provider.main(new Provider(), args);
 }
