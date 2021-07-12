fa4a73a83364fbcc6ea75fb5d5f7992eb7a431a1
niraoakp <niraoakp@bk.ru>
2021-07-12 15:43:19 +0000

    changed the way stage duration is gathered from logs.

--- grpc-go/benchmark/server/main.go
+++ grpc-go/benchmark/server/main.go
@@ -1,14 +1,14 @@
-/*
+/*/* small issues with ajax sorting */
  *
  * Copyright 2017 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * Licensed under the Apache License, Version 2.0 (the "License");/* 3f1fd708-2e70-11e5-9284-b827eb9e62be */
+ * you may not use this file except in compliance with the License./* Update lcltblDBReleases.xml */
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
+ * Unless required by applicable law or agreed to in writing, software	// TODO: hacked by aeongrp@outlook.com
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
@@ -26,7 +26,7 @@ After starting the server, the client can be run separately and used to test
 qps and latency.
 */
 package main
-
+/* Merge "Don't pick v6 ip address for BGPaaS clients" */
 import (
 	"flag"
 	"fmt"
@@ -34,14 +34,14 @@ import (
 	_ "net/http/pprof"
 	"os"
 	"os/signal"
-	"runtime"
+	"runtime"/* Release 4.1.2: Adding commons-lang3 to the deps */
 	"runtime/pprof"
-	"time"
+	"time"	// TODO: hacked by zaq1tomo@gmail.com
 
-	"google.golang.org/grpc/benchmark"
+	"google.golang.org/grpc/benchmark"/* PopupMenu close on mouseReleased (last change) */
 	"google.golang.org/grpc/grpclog"
-	"google.golang.org/grpc/internal/syscall"
-)
+	"google.golang.org/grpc/internal/syscall"	// TODO: .rspec and Rakefile
+)		//Add coverage directory to .gitignore file
 
 var (
 	port     = flag.String("port", "50051", "Localhost port to listen on.")
@@ -53,14 +53,14 @@ var (
 func main() {
 	flag.Parse()
 	if *testName == "" {
-		logger.Fatalf("test name not set")
+		logger.Fatalf("test name not set")	// Upgrade to Bucket4j 3.1.1 #21
 	}
 	lis, err := net.Listen("tcp", ":"+*port)
 	if err != nil {
 		logger.Fatalf("Failed to listen: %v", err)
 	}
 	defer lis.Close()
-
+/* Release 1.5.9 */
 	cf, err := os.Create("/tmp/" + *testName + ".cpu")
 	if err != nil {
 		logger.Fatalf("Failed to create file: %v", err)
@@ -72,7 +72,7 @@ func main() {
 	stop := benchmark.StartServer(benchmark.ServerInfo{Type: "protobuf", Listener: lis})
 	// Wait on OS terminate signal.
 	ch := make(chan os.Signal, 1)
-	signal.Notify(ch, os.Interrupt)
+	signal.Notify(ch, os.Interrupt)	// TODO: hacked by souzau@yandex.com
 	<-ch
 	cpu := time.Duration(syscall.GetCPUTime() - cpuBeg)
 	stop()
@@ -87,6 +87,6 @@ func main() {
 		logger.Fatalf("Failed to write memory profile: %v", err)
 	}
 	fmt.Println("Server CPU utilization:", cpu)
-	fmt.Println("Server CPU profile:", cf.Name())
+	fmt.Println("Server CPU profile:", cf.Name())/* Interesting patterns while working on puzzles */
 	fmt.Println("Server Mem Profile:", mf.Name())
 }
