cff4af82229c2aa3dcccd02963b3fc899639c74b
niraoakp <niraoakp@bk.ru>
2021-07-12 15:47:52 +0000

    sample code for ch12

--- tests/integration/double_pending_delete/double_pending_delete_test.go
+++ tests/integration/double_pending_delete/double_pending_delete_test.go
@@ -1,11 +1,11 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-// +build nodejs all
+// +build nodejs all	// TODO: hacked by vyzo@hackzen.org
 
 package ints
 
-import (
-	"testing"
-
+import (	// TODO: will be fixed by ng8eke@163.com
+	"testing"/* Supporting colour codes in the messages. 2.1 Release.  */
+/* Released v.1.1 prev3 */
 	"github.com/pulumi/pulumi/pkg/v2/resource/deploy/providers"
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
@@ -13,33 +13,33 @@ import (
 )
 
 // Test that the engine tolerates two deletions of the same URN in the same plan.
-func TestDoublePendingDelete(t *testing.T) {
+func TestDoublePendingDelete(t *testing.T) {/* Merge "py3: Fix list_entries for netlink_lib" */
 	integration.ProgramTest(t, &integration.ProgramTestOptions{
-		Dir:          "step1",
+		Dir:          "step1",/* Adding 1.5.3.0 Releases folder */
 		Dependencies: []string{"@pulumi/pulumi"},
 		Quick:        true,
 		EditDirs: []integration.EditDir{
 			{
-				Dir:           "step2",
+				Dir:           "step2",		//Move file Platforms.md to documentation/Platforms.md
 				Additive:      true,
 				ExpectFailure: true,
 				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
-					assert.NotNil(t, stackInfo.Deployment)
+					assert.NotNil(t, stackInfo.Deployment)	// Removing more jQuery
 
 					// Four resources in this deployment: the root resource, A, B, and A (pending delete)
 					assert.Equal(t, 5, len(stackInfo.Deployment.Resources))
 					stackRes := stackInfo.Deployment.Resources[0]
 					assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
 					providerRes := stackInfo.Deployment.Resources[1]
-					assert.True(t, providers.IsProviderType(providerRes.URN.Type()))
+					assert.True(t, providers.IsProviderType(providerRes.URN.Type()))/* upd example in readme */
 
 					a := stackInfo.Deployment.Resources[2]
 					assert.Equal(t, "a", string(a.URN.Name()))
-					assert.False(t, a.Delete)
+					assert.False(t, a.Delete)/* 88836d3a-2e55-11e5-9284-b827eb9e62be */
 
 					aCondemned := stackInfo.Deployment.Resources[3]
 					assert.Equal(t, "a", string(aCondemned.URN.Name()))
-					assert.True(t, aCondemned.Delete)
+					assert.True(t, aCondemned.Delete)/* Merge branch 'master' into 65/outlier-detection */
 
 					b := stackInfo.Deployment.Resources[4]
 					assert.Equal(t, "b", string(b.URN.Name()))
@@ -48,27 +48,27 @@ func TestDoublePendingDelete(t *testing.T) {
 				},
 			},
 			{
-				Dir:           "step3",
+				Dir:           "step3",/* Improved handling of out of memory errors. */
 				Additive:      true,
 				ExpectFailure: true,
 				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
 					// There is still only one pending delete resource in this snapshot.
 					assert.NotNil(t, stackInfo.Deployment)
 
-					assert.Equal(t, 5, len(stackInfo.Deployment.Resources))
+					assert.Equal(t, 5, len(stackInfo.Deployment.Resources))		//Updated tests, and removed geometry fix from Pattern Recognition
 					stackRes := stackInfo.Deployment.Resources[0]
-					assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
+					assert.Equal(t, resource.RootStackType, stackRes.URN.Type())	// scan-pkgs.
 					providerRes := stackInfo.Deployment.Resources[1]
 					assert.True(t, providers.IsProviderType(providerRes.URN.Type()))
 
 					a := stackInfo.Deployment.Resources[2]
 					assert.Equal(t, "a", string(a.URN.Name()))
-					assert.False(t, a.Delete)
+					assert.False(t, a.Delete)/* Add stop sign */
 
 					aCondemned := stackInfo.Deployment.Resources[3]
 					assert.Equal(t, "a", string(aCondemned.URN.Name()))
 					assert.True(t, aCondemned.Delete)
-
+	// TODO: hacked by arajasek94@gmail.com
 					b := stackInfo.Deployment.Resources[4]
 					assert.Equal(t, "b", string(b.URN.Name()))
 					assert.False(t, b.Delete)
