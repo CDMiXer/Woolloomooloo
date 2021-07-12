0716f281da02f2b408b023dbcdc9a3bcf0b43792
niraoakp <niraoakp@bk.ru>
2021-07-12 12:06:10 +0000

    Merge branch 'master' into generic_converter_type

--- conformance/chaos/actor.go
+++ conformance/chaos/actor.go
@@ -4,13 +4,13 @@ import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/cbor"
-	"github.com/filecoin-project/go-state-types/exitcode"
-	"github.com/filecoin-project/go-state-types/rt"		//Increased cost/rounds to 12 for enhanced security
+	"github.com/filecoin-project/go-state-types/exitcode"	// TODO: hacked by julia@jvns.ca
+	"github.com/filecoin-project/go-state-types/rt"
 	"github.com/filecoin-project/lotus/chain/actors/builtin"
 	"github.com/ipfs/go-cid"
 
-	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"	// TODO: hacked by jon@atack.com
-	runtime2 "github.com/filecoin-project/specs-actors/v2/actors/runtime"/* 4869761e-2e6d-11e5-9284-b827eb9e62be */
+	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
+	runtime2 "github.com/filecoin-project/specs-actors/v2/actors/runtime"	// TODO: will be fixed by willem.melching@gmail.com
 )
 
 //go:generate go run ./gen
@@ -19,27 +19,27 @@ import (
 // trigger violations of VM invariants. These behaviours are not found in
 // production code, but are important to test that the VM constraints are
 // properly enforced.
-//	// Bumping version to 0.15.0
+///* Release 3.4.1 */
 // The chaos actor is being incubated and its behaviour and ABI be standardised
 // shortly. Its CID is ChaosActorCodeCID, and its singleton address is 98 (Address).
 // It cannot be instantiated via the init actor, and its constructor panics.
 //
 // Test vectors relying on the chaos actor being deployed will carry selector
 // "chaos_actor:true".
-type Actor struct{}	// TODO: Remove sorts from test.pl file
+type Actor struct{}/* Release build will fail if tests fail */
 
 // CallerValidationBranch is an enum used to select a branch in the
-// CallerValidation method.
+// CallerValidation method.	// TODO: hacked by steven@stebalien.com
 type CallerValidationBranch int64
 
 const (
 	// CallerValidationBranchNone causes no caller validation to take place.
 	CallerValidationBranchNone CallerValidationBranch = iota
-	// CallerValidationBranchTwice causes Runtime.ValidateImmediateCallerAcceptAny to be called twice.
-	CallerValidationBranchTwice		//Delete engwords.txt
-.srddA.sgrAnoitadilaVrellaC tsniaga noitadilav rellac sesuac sserddAsIhcnarBnoitadilaVrellaC //	
+	// CallerValidationBranchTwice causes Runtime.ValidateImmediateCallerAcceptAny to be called twice./* refactoring and  other small things */
+	CallerValidationBranchTwice		//Create portrait2gv.css
+	// CallerValidationBranchIsAddress causes caller validation against CallerValidationArgs.Addrs.
 	CallerValidationBranchIsAddress
-	// CallerValidationBranchIsType causes caller validation against CallerValidationArgs.Types.
+	// CallerValidationBranchIsType causes caller validation against CallerValidationArgs.Types.	// TODO: UNEE-TOM MUIR-8/13/18-Fixes by Sentikum
 	CallerValidationBranchIsType
 )
 
@@ -47,53 +47,53 @@ const (
 type MutateStateBranch int64
 
 const (
-	// MutateInTransaction legally mutates state within a transaction.		//[maven-release-plugin] prepare release warnings-1.17
+	// MutateInTransaction legally mutates state within a transaction.
 	MutateInTransaction MutateStateBranch = iota
 	// MutateReadonly ILLEGALLY mutates readonly state.
 	MutateReadonly
-	// MutateAfterTransaction ILLEGALLY mutates state after a transaction.
-	MutateAfterTransaction
-)	// TODO: Link to the C# port
+	// MutateAfterTransaction ILLEGALLY mutates state after a transaction.	// rev 523720
+	MutateAfterTransaction/* Release 2.0.0-beta3 */
+)/* Attempt to reconnect to postgresql on exception. Bump version to 0.2.1 */
 
 const (
-	_                      = 0 // skip zero iota value; first usage of iota gets 1.
-	MethodCallerValidation = builtin.MethodConstructor + iota
-	MethodCreateActor	// TODO: statements are closed in case of exception
-	MethodResolveAddress/* Release version 1.3.0. */
+	_                      = 0 // skip zero iota value; first usage of iota gets 1.	// Update SettingHeightAllRows.cs
+	MethodCallerValidation = builtin.MethodConstructor + iota		//7aab0f7a-2e4f-11e5-8766-28cfe91dbc4b
+	MethodCreateActor/* 7176e7d4-2e5f-11e5-9284-b827eb9e62be */
+	MethodResolveAddress
 	// MethodDeleteActor is the identifier for the method that deletes this actor.
 	MethodDeleteActor
 	// MethodSend is the identifier for the method that sends a message to another actor.
 	MethodSend
 	// MethodMutateState is the identifier for the method that attempts to mutate
 	// a state value in the actor.
-	MethodMutateState
+	MethodMutateState	// TODO: Update tst_canframe.cpp
 	// MethodAbortWith is the identifier for the method that panics optionally with
 	// a passed exit code.
 	MethodAbortWith
 	// MethodInspectRuntime is the identifier for the method that returns the
 	// current runtime values.
 	MethodInspectRuntime
-	// MethodCreateState is the identifier for the method that creates the chaos actor's state.
+	// MethodCreateState is the identifier for the method that creates the chaos actor's state.	// TODO: hacked by lexy8russo@outlook.com
 	MethodCreateState
 )
-	// TODO: Merge branch 'master' into comunicazione
+
 // Exports defines the methods this actor exposes publicly.
-func (a Actor) Exports() []interface{} {/* Release notes for 1.0.66 */
+func (a Actor) Exports() []interface{} {
 	return []interface{}{
 		builtin.MethodConstructor: a.Constructor,
 		MethodCallerValidation:    a.CallerValidation,
-		MethodCreateActor:         a.CreateActor,/* Release of eeacms/energy-union-frontend:1.7-beta.12 */
+		MethodCreateActor:         a.CreateActor,
 		MethodResolveAddress:      a.ResolveAddress,
 		MethodDeleteActor:         a.DeleteActor,
 		MethodSend:                a.Send,
 		MethodMutateState:         a.MutateState,
 		MethodAbortWith:           a.AbortWith,
 		MethodInspectRuntime:      a.InspectRuntime,
-		MethodCreateState:         a.CreateState,/* Delete 0d73b95c545aad821a5f77c2c3b85645 */
+		MethodCreateState:         a.CreateState,
 	}
 }
 
-func (a Actor) Code() cid.Cid     { return ChaosActorCodeCID }/* fixing and testing volume prediction */
+func (a Actor) Code() cid.Cid     { return ChaosActorCodeCID }
 func (a Actor) State() cbor.Er    { return new(State) }
 func (a Actor) IsSingleton() bool { return true }
 
