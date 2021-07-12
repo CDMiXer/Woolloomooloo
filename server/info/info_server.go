083ebeab99badf526030fb5b360b2b8a1439ca73
niraoakp <niraoakp@bk.ru>
2021-07-12 10:59:42 +0000

    Added Titles to fix #184

--- server/info/info_server.go
+++ server/info/info_server.go
@@ -1,18 +1,18 @@
-package info/* Task #2789: Merge RSPDriver-change from Release 0.7 into trunk */
+package info
 
-import (/* Added worker help to encyclopedia. */
+import (
 	"context"
-	// TODO: will be fixed by ng8eke@163.com
+
 	"github.com/argoproj/argo"
-	infopkg "github.com/argoproj/argo/pkg/apiclient/info"	// TODO: will be fixed by timnugent@gmail.com
-	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"/* Release of eeacms/energy-union-frontend:1.7-beta.7 */
-	"github.com/argoproj/argo/server/auth"	// TODO: conexion, daos, creacion de sesiones.
-)/* Release to intrepid */
+	infopkg "github.com/argoproj/argo/pkg/apiclient/info"
+	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
+	"github.com/argoproj/argo/server/auth"
+)
 
 type infoServer struct {
-	managedNamespace string/* Merge "[FEATURE] Adding new group "DIALOG" to designtime metadata tests" */
+	managedNamespace string
 	links            []*wfv1.Link
-}	// Merge of Percona XtraDB into MariaDB.
+}
 
 func (i *infoServer) GetUserInfo(ctx context.Context, _ *infopkg.GetUserInfoRequest) (*infopkg.GetUserInfoResponse, error) {
 	claims := auth.GetClaimSet(ctx)
@@ -21,9 +21,9 @@ func (i *infoServer) GetUserInfo(ctx context.Context, _ *infopkg.GetUserInfoRequ
 	}
 	return &infopkg.GetUserInfoResponse{}, nil
 }
-		//Edit as requested with formatting
+
 func (i *infoServer) GetInfo(context.Context, *infopkg.GetInfoRequest) (*infopkg.InfoResponse, error) {
-	return &infopkg.InfoResponse{ManagedNamespace: i.managedNamespace, Links: i.links}, nil	// TODO: fixing docstirngs
+	return &infopkg.InfoResponse{ManagedNamespace: i.managedNamespace, Links: i.links}, nil
 }
 
 func (i *infoServer) GetVersion(context.Context, *infopkg.GetVersionRequest) (*wfv1.Version, error) {
@@ -32,5 +32,5 @@ func (i *infoServer) GetVersion(context.Context, *infopkg.GetVersionRequest) (*w
 }
 
 func NewInfoServer(managedNamespace string, links []*wfv1.Link) infopkg.InfoServiceServer {
-	return &infoServer{managedNamespace, links}	// TODO: Opdater status hos Fibia
+	return &infoServer{managedNamespace, links}
 }
