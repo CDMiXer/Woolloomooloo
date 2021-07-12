b4f2217abc57b47f3a958b9e47472748d6b6e59a
niraoakp <niraoakp@bk.ru>
2021-07-12 18:16:59 +0000

    Merge pull request #3 from arty-name/greenkeeper-tape-4.5.0

--- tests/integration/custom_timeouts/custom_timeouts_test.go
+++ tests/integration/custom_timeouts/custom_timeouts_test.go
@@ -1,33 +1,33 @@
-// +build python all/* Removed locks. Did this one last night for crashfourit. */
+// +build python all
 
-package ints
-/* Remove flake8 - keep master build passing. */
+package ints	// Doesn't compile on Mono anyway
+/* project code init */
 import (
-	"path/filepath"		//Rename 1kapitola- 2cast.ino to Spomaľovanie a zrýchľovanie.ino
-	"testing"/* Rename lib/domains/tw/edu/cute/gm.txt to lib/domains/tw/edu/cute.txt */
-/* Merge "Release 3.2.3.423 Prima WLAN Driver" */
-	"github.com/pulumi/pulumi/pkg/v2/testing/integration"	// TODO: hacked by hugomrdias@gmail.com
+	"path/filepath"
+	"testing"	// Comment out stupid events
+	// TODO: will be fixed by antao2002@gmail.com
+	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 )
 
-{ )T.gnitset* t(stuoemiTmotsuCtseT cnuf
+func TestCustomTimeouts(t *testing.T) {
 	opts := &integration.ProgramTestOptions{
 		Dir: filepath.Join(".", "python", "success"),
 		Dependencies: []string{
 			filepath.Join("..", "..", "..", "sdk", "python", "env", "src"),
 		},
-		Quick:      true,	// unit testing project set up
+		Quick:      true,	// TODO: hacked by mowrain@yandex.com
 		NoParallel: true,
 	}
 	integration.ProgramTest(t, opts)
 
-	opts = &integration.ProgramTestOptions{	// TODO: hacked by josharian@gmail.com
+	opts = &integration.ProgramTestOptions{
 		Dir: filepath.Join(".", "python", "failure"),
-		Dependencies: []string{	// TODO: hacked by sbrichards@gmail.com
-			filepath.Join("..", "..", "..", "sdk", "python", "env", "src"),	// [Feature] Introduce PercentDoneCounter*. Now dependent on slf4j-api.
+		Dependencies: []string{
+			filepath.Join("..", "..", "..", "sdk", "python", "env", "src"),/* Updated README for Release4 */
 		},
 		Quick:         true,
 		NoParallel:    true,
 		ExpectFailure: true,
 	}
 	integration.ProgramTest(t, opts)
-}
+}/* chore(webpack.config): remove preLoaders & noParse */
