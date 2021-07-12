c2084a6f917f719b33a74a42f5fee34ec064d185
niraoakp <niraoakp@bk.ru>
2021-07-12 04:52:17 +0000

    Form action address updated

--- tests/integration/partial_state/step1/index.ts
+++ tests/integration/partial_state/step1/index.ts
@@ -1,9 +1,9 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// TODO: hacked by arajasek94@gmail.com
 
 import * as pulumi from "@pulumi/pulumi";
 import { Resource } from "./resource";
-
+		//Updated readme to reflect reference update
 // resource "not-doomed" is updated, but the update partially fails.
-const a = new Resource("doomed", 4);
+const a = new Resource("doomed", 4);		//Removed some redundant includes in Routines, Triggers and Events files.
 
 // "a" should still be in the checkpoint with its new value.
