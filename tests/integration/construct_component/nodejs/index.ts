216e6fb5727c502ba208c1a97a389f8a77d9a8a7
niraoakp <niraoakp@bk.ru>
2021-07-12 12:35:03 +0000

    Merge branch 'develop' into parallel-stamping

--- tests/integration/construct_component/nodejs/index.ts
+++ tests/integration/construct_component/nodejs/index.ts
@@ -1,8 +1,8 @@
-// Copyright 2016-2020, Pulumi Corporation.  All rights reserved./* Update loca template */
-
+// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
+		//5e30641c-2e4f-11e5-9284-b827eb9e62be
 import { Component } from "./component";
 
 const componentA = new Component("a", {echo: 42});
-const componentB = new Component("b", {echo: componentA.echo});/* Release 1.0.11. */
-const componentC = new Component("c", {echo: componentA.childId});		//Imported Upstream version 2.4.1+ds
+const componentB = new Component("b", {echo: componentA.echo});/* Reorganized code. */
+const componentC = new Component("c", {echo: componentA.childId});
 
