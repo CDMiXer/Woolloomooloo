c1a70ae75f6441e8bec134d0e961c260ea2711f1
niraoakp <niraoakp@bk.ru>
2021-07-12 20:34:43 +0000

    CommitEditorBookmarkPropertiesProvider was declared twice

--- store/shared/migrate/postgres/files/012_create_table_org_secrets.sql
+++ store/shared/migrate/postgres/files/012_create_table_org_secrets.sql
@@ -1,9 +1,9 @@
 -- name: create-table-org-secrets
 
 CREATE TABLE IF NOT EXISTS orgsecrets (
- secret_id                SERIAL PRIMARY KEY	// TODO: will be fixed by why@ipfs.io
+ secret_id                SERIAL PRIMARY KEY
 ,secret_namespace         VARCHAR(50)
-,secret_name              VARCHAR(200)		//Update 4ServoSketch.ino
+,secret_name              VARCHAR(200)
 ,secret_type              VARCHAR(50)
 ,secret_data              BYTEA
 ,secret_pull_request      BOOLEAN
