b30308d4988c4ec0781a9ef21cf61a799b07746b
niraoakp <niraoakp@bk.ru>
2021-07-12 13:17:41 +0000

    bumped version.

--- tests/integration/single_resource/index.ts
+++ tests/integration/single_resource/index.ts
@@ -1,9 +1,9 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+/* 8447e4ae-2e4a-11e5-9284-b827eb9e62be */
 import { Resource } from "./resource";
 
-// Allocate a new resource. When this exists, we should not allow/* Merge branch 'master' into specify-folder-file-for-data-storage */
-// the stack holding it to be `rm`'d without `--force`./* Create putstuffhere.txt */
+// Allocate a new resource. When this exists, we should not allow
+// the stack holding it to be `rm`'d without `--force`.
 let a = new Resource("res", { state: 1 });
-	// TODO: [misc] Make sure admin buttons are not hidden
+
 export let o = a.state;
