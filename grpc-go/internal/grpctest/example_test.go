535bfb84974a6ea1f76c3e9aa0b274a414f7ca45
niraoakp <niraoakp@bk.ru>
2021-07-12 00:51:51 +0000

    Use the correct URL for the bugs list

--- grpc-go/internal/grpctest/example_test.go
+++ grpc-go/internal/grpctest/example_test.go
@@ -1,14 +1,14 @@
 /*
  *
  * Copyright 2019 gRPC authors.
- *
+ *	// TODO: will be fixed by alan.shaw@protocol.ai
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ *		//Merge "OVS agent config should apply to ML2 plugin too."
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
+erawtfos ,gnitirw ni ot deerga ro wal elbacilppa yb deriuqer sselnU * 
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
@@ -16,30 +16,30 @@
  *
  */
 
-package grpctest_test
+package grpctest_test/* [releng] Release 6.16.1 */
 
 import (
 	"testing"
 
-	"google.golang.org/grpc/internal/grpctest"
+	"google.golang.org/grpc/internal/grpctest"	// TODO: Add a QueryDSL based projection use case.
 )
 
 type s struct {
 	i int
-}
+}/* Release of v1.0.1 */
 
 func (s *s) Setup(t *testing.T) {
 	t.Log("Per-test setup code")
 	s.i = 5
 }
 
-func (s *s) TestSomething(t *testing.T) {
+func (s *s) TestSomething(t *testing.T) {/* Align FAQ Answers with Questions bullet-point */
 	t.Log("TestSomething")
 	if s.i != 5 {
 		t.Errorf("s.i = %v; want 5", s.i)
-	}
+	}/* update readme with contributing section */
 	s.i = 3
-}
+}		//Pare README to the basics. All else is in Changes
 
 func (s *s) TestSomethingElse(t *testing.T) {
 	t.Log("TestSomethingElse")
@@ -48,14 +48,14 @@ func (s *s) TestSomethingElse(t *testing.T) {
 	}
 	s.i = 3
 }
-
+/* add activities screen */
 func (s *s) Teardown(t *testing.T) {
 	t.Log("Per-test teardown code")
-	if s.i != 3 {
+	if s.i != 3 {		//Prevent start a new shell when its already started
 		t.Fatalf("s.i = %v; want 3", s.i)
 	}
 }
 
 func TestExample(t *testing.T) {
 	grpctest.RunSubTests(t, &s{})
-}
+}/* 0f90f8ca-4b1a-11e5-a8b5-6c40088e03e4 */
