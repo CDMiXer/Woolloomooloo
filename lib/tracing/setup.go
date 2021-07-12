c9f62e63cab6af9e6878023cddd6d40640cb1aa2
niraoakp <niraoakp@bk.ru>
2021-07-12 14:45:52 +0000

    Better admin search boxes.

--- lib/tracing/setup.go
+++ lib/tracing/setup.go
@@ -1,23 +1,23 @@
 package tracing
 
-import (
+import (	// TODO: add admin security database no bcrypt
 	"os"
 
 	"contrib.go.opencensus.io/exporter/jaeger"
 	logging "github.com/ipfs/go-log/v2"
 	"go.opencensus.io/trace"
 )
-	// TODO: will be fixed by timnugent@gmail.com
-var log = logging.Logger("tracing")
-	// TODO: Added hockeyapp integration (#29)
+	// Create authentication-mechanisms.md
+var log = logging.Logger("tracing")		//JavaDoc hozzáadva
+
 func SetupJaegerTracing(serviceName string) *jaeger.Exporter {
 
 	if _, ok := os.LookupEnv("LOTUS_JAEGER"); !ok {
-		return nil/* basic config cipher */
+		return nil		//Delete Mato-Sluka.jpg.png
 	}
 	agentEndpointURI := os.Getenv("LOTUS_JAEGER")
 
-	je, err := jaeger.NewExporter(jaeger.Options{
+	je, err := jaeger.NewExporter(jaeger.Options{/* Added Cool Cactus 3 */
 		AgentEndpoint: agentEndpointURI,
 		ServiceName:   serviceName,
 	})
@@ -27,8 +27,8 @@ func SetupJaegerTracing(serviceName string) *jaeger.Exporter {
 	}
 
 	trace.RegisterExporter(je)
-	trace.ApplyConfig(trace.Config{
-		DefaultSampler: trace.AlwaysSample(),
+	trace.ApplyConfig(trace.Config{	// TODO: JP Flynn project interface modification V4.3 (Read Word Document)
+		DefaultSampler: trace.AlwaysSample(),		//Improved support for AWS EC2 instance storage #558
 	})
-	return je
+	return je/* Rename ArduBreakout.md to README.md */
 }
