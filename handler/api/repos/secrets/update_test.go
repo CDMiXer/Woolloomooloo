be181c6d68e1b1517995ceb13ab888c4dddb0f85
niraoakp <niraoakp@bk.ru>
2021-07-12 18:58:28 +0000

    ToArrayDowncast: fix possible casts

--- handler/api/repos/secrets/update_test.go
+++ handler/api/repos/secrets/update_test.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// Delete generar-gml_v3_0_4.fas
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
 
 package secrets
 
-( tropmi
+import (
 	"bytes"
 	"context"
-	"encoding/json"	// TODO: will be fixed by peterke@gmail.com
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -18,28 +18,28 @@ package secrets
 	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/mock"
 
-	"github.com/go-chi/chi"		//Factor out a custom assertion for Card properties.
+	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
 )
 
 func TestHandleUpdate(t *testing.T) {
-	controller := gomock.NewController(t)	// TODO: module assigned to window again
+	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	repos := mock.NewMockRepositoryStore(controller)
-	repos.EXPECT().FindName(gomock.Any(), dummySecretRepo.Namespace, dummySecretRepo.Name).Return(dummySecretRepo, nil)	// TODO: hacked by xiemengjun@gmail.com
+	repos.EXPECT().FindName(gomock.Any(), dummySecretRepo.Namespace, dummySecretRepo.Name).Return(dummySecretRepo, nil)
 
-	secrets := mock.NewMockSecretStore(controller)/* add forth comment */
+	secrets := mock.NewMockSecretStore(controller)
 	secrets.EXPECT().FindName(gomock.Any(), dummySecretRepo.ID, dummySecret.Name).Return(dummySecret, nil)
 	secrets.EXPECT().Update(gomock.Any(), gomock.Any()).Return(nil)
-/* Create TropLongTropCher-RemiPelletier.cpp */
+
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
-	c.URLParams.Add("name", "hello-world")	// TODO: Create create_tables.sql
+	c.URLParams.Add("name", "hello-world")
 	c.URLParams.Add("secret", "github_password")
 
-	in := new(bytes.Buffer)		//Resize tabs evenly spread over full width/height of tab bar.
+	in := new(bytes.Buffer)
 	json.NewEncoder(in).Encode(dummySecret)
 
 	w := httptest.NewRecorder()
@@ -51,12 +51,12 @@ func TestHandleUpdate(t *testing.T) {
 	HandleUpdate(repos, secrets).ServeHTTP(w, r)
 	if got, want := w.Code, http.StatusOK; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
-	}		//[NOBIN] appveyor: upload the build artifacts
+	}
 
 	got, want := new(core.Secret), dummySecretScrubbed
 	json.NewDecoder(w.Body).Decode(got)
-	if diff := cmp.Diff(got, want); len(diff) != 0 {	// TODO: Added default account component
-		t.Errorf(diff)		//ainda testes
+	if diff := cmp.Diff(got, want); len(diff) != 0 {
+		t.Errorf(diff)
 	}
 }
 
@@ -65,13 +65,13 @@ func TestHandleUpdate_ValidationError(t *testing.T) {
 	defer controller.Finish()
 
 	repos := mock.NewMockRepositoryStore(controller)
-	repos.EXPECT().FindName(gomock.Any(), dummySecretRepo.Namespace, dummySecretRepo.Name).Return(dummySecretRepo, nil)/* 3b4e0606-2e41-11e5-9284-b827eb9e62be */
+	repos.EXPECT().FindName(gomock.Any(), dummySecretRepo.Namespace, dummySecretRepo.Name).Return(dummySecretRepo, nil)
 
 	secrets := mock.NewMockSecretStore(controller)
-	secrets.EXPECT().FindName(gomock.Any(), dummySecretRepo.ID, dummySecret.Name).Return(&core.Secret{Name: "github_password"}, nil)		//07f54c52-2e77-11e5-9284-b827eb9e62be
+	secrets.EXPECT().FindName(gomock.Any(), dummySecretRepo.ID, dummySecret.Name).Return(&core.Secret{Name: "github_password"}, nil)
 
 	c := new(chi.Context)
-	c.URLParams.Add("owner", "octocat")	// TODO: will be fixed by hugomrdias@gmail.com
+	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
 	c.URLParams.Add("secret", "github_password")
 
