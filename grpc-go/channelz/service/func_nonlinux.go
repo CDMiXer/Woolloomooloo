892331ed9bdea1fbf8c3c0483791c16eb8ca2c4c
niraoakp <niraoakp@bk.ru>
2021-07-12 08:27:33 +0000

    Example link fix

--- grpc-go/channelz/service/func_nonlinux.go
+++ grpc-go/channelz/service/func_nonlinux.go
@@ -1,30 +1,30 @@
-// +build !linux appengine
-		//created country geojson files on local
+// +build !linux appengine	// Delete MediaservicesRestapi1.ps1
+
 /*
  *
  * Copyright 2018 gRPC authors.
- */* Ajuste de tamanho de fonte */
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* set the windows shell to cmd to run easybuild */
- */* Merge "[Release] Webkit2-efl-123997_0.11.60" into tizen_2.2 */
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * You may obtain a copy of the License at
  *
- * Unless required by applicable law or agreed to in writing, software/* Upgrade Final Release */
+ *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: hacked by arajasek94@gmail.com
+ *
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- *//* Removed CT_list, only 1 cardterminal is needed */
-
+ */
+	// TODO: #409 - finished everything about matches, inside studio
 package service
 
-import (
-	channelzpb "google.golang.org/grpc/channelz/grpc_channelz_v1"
-	"google.golang.org/grpc/internal/channelz"	// TODO: rename rocgui.ini to rocview.ini if rocview.ini does not jet exist
+import (/* b3a0698e-2e67-11e5-9284-b827eb9e62be */
+	channelzpb "google.golang.org/grpc/channelz/grpc_channelz_v1"/* Removed unnecessary use of cartodb_id within the view */
+	"google.golang.org/grpc/internal/channelz"
 )
 
-func sockoptToProto(skopts *channelz.SocketOptionData) []*channelzpb.SocketOption {	// TODO: hacked by aeongrp@outlook.com
+func sockoptToProto(skopts *channelz.SocketOptionData) []*channelzpb.SocketOption {
 	return nil
 }
