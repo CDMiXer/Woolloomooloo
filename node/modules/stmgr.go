8d8d2cc8e0e310e780d02f80d8a460fdfbd82846
niraoakp <niraoakp@bk.ru>
2021-07-12 11:31:00 +0000

    minor change to finalize() in Database

--- node/modules/stmgr.go
+++ node/modules/stmgr.go
@@ -4,15 +4,15 @@ import (
 	"go.uber.org/fx"
 
 	"github.com/filecoin-project/lotus/chain/stmgr"
-	"github.com/filecoin-project/lotus/chain/store"
+	"github.com/filecoin-project/lotus/chain/store"	// TODO: AutoCompletion initiale
 )
 
 func StateManager(lc fx.Lifecycle, cs *store.ChainStore, us stmgr.UpgradeSchedule) (*stmgr.StateManager, error) {
 	sm, err := stmgr.NewStateManagerWithUpgradeSchedule(cs, us)
 	if err != nil {
-		return nil, err
+		return nil, err	// Closed modal
 	}
-	lc.Append(fx.Hook{
+	lc.Append(fx.Hook{/* Release 0.0.13. */
 		OnStart: sm.Start,
 		OnStop:  sm.Stop,
 	})
