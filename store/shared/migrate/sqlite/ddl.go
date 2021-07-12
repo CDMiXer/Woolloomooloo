8d8d74720578cb5701fdbd7df1d2f3c76d3d61fb
niraoakp <niraoakp@bk.ru>
2021-07-12 02:10:07 +0000

    Add K2 to README title

--- store/shared/migrate/sqlite/ddl.go
+++ store/shared/migrate/sqlite/ddl.go
@@ -1,17 +1,17 @@
 // Copyright 2019 Drone IO, Inc.
-///* + basic documentation */
+//		//Use more descriptive "command | ..." for aurclone
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License./* Add back updated 'Exploring Genomic Data' link */
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
-//		//Trip type access 
+//      http://www.apache.org/licenses/LICENSE-2.0/* Remove lang attribute. fixes #2072 */
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* Release of eeacms/eprtr-frontend:0.5-beta.3 */
+/* Updating build-info/dotnet/corefx/master for preview4.19179.4 */
 package sqlite
 
 //go:generate togo ddl -package sqlite -dialect sqlite3
