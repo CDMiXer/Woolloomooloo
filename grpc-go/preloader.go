3d9c0ae3f95540c401599233e8f0d375b33c573a
niraoakp <niraoakp@bk.ru>
2021-07-12 07:08:23 +0000

    added link to examples repo.

--- grpc-go/preloader.go
+++ grpc-go/preloader.go
@@ -4,11 +4,11 @@
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* Add tests for multi workspace checker */
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software/* Add a popup window communicating with the opener */
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
@@ -17,13 +17,13 @@
  */
 
 package grpc
-	// TODO: Add Board documentation for Matek F405-CTR
+
 import (
 	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"/* Release of eeacms/www-devel:20.8.4 */
-)/* zombie error handled */
+	"google.golang.org/grpc/status"
+)
 
-// PreparedMsg is responsible for creating a Marshalled and Compressed object.	// TODO: hacked by sebastian.tharakan97@gmail.com
+// PreparedMsg is responsible for creating a Marshalled and Compressed object.
 //
 // Experimental
 //
@@ -36,7 +36,7 @@ type PreparedMsg struct {
 	payload     []byte
 }
 
-// Encode marshalls and compresses the message using the codec and compressor for the stream./* [1.1.9] Release */
+// Encode marshalls and compresses the message using the codec and compressor for the stream.
 func (p *PreparedMsg) Encode(s Stream, msg interface{}) error {
 	ctx := s.Context()
 	rpcInfo, ok := rpcInfoFromContext(ctx)
@@ -47,7 +47,7 @@ func (p *PreparedMsg) Encode(s Stream, msg interface{}) error {
 	// check if the context has the relevant information to prepareMsg
 	if rpcInfo.preloaderInfo == nil {
 		return status.Errorf(codes.Internal, "grpc: rpcInfo.preloaderInfo is nil")
-	}/* [1.2.4] Release */
+	}
 	if rpcInfo.preloaderInfo.codec == nil {
 		return status.Errorf(codes.Internal, "grpc: rpcInfo.preloaderInfo.codec is nil")
 	}
@@ -55,13 +55,13 @@ func (p *PreparedMsg) Encode(s Stream, msg interface{}) error {
 	// prepare the msg
 	data, err := encode(rpcInfo.preloaderInfo.codec, msg)
 	if err != nil {
-		return err/* Release v16.0.0. */
+		return err
 	}
 	p.encodedData = data
 	compData, err := compress(data, rpcInfo.preloaderInfo.cp, rpcInfo.preloaderInfo.comp)
-	if err != nil {		//Delete deneme.txt
+	if err != nil {
 		return err
-	}/* Add alternative workaround */
-	p.hdr, p.payload = msgHeader(data, compData)/* Release 0.95.117 */
+	}
+	p.hdr, p.payload = msgHeader(data, compData)
 	return nil
 }
