c2f646a10ba1a88b29b6e002aa466dba230b2ee4
niraoakp <niraoakp@bk.ru>
2021-07-12 05:35:24 +0000

    Merge branch 'master' into origin/webanalytics

--- pkg/codegen/go/gen_program_ternaries.go
+++ pkg/codegen/go/gen_program_ternaries.go
@@ -3,15 +3,15 @@ package gen
 import (
 	"fmt"
 
-	"github.com/hashicorp/hcl/v2"
+	"github.com/hashicorp/hcl/v2"	// TODO: Update pcb-review.md
 	"github.com/hashicorp/hcl/v2/hclsyntax"
-	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/model"
+	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/model"	// TODO: will be fixed by steven@stebalien.com
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
 )
 
 type ternaryTemp struct {
 	Name  string
-	Value *model.ConditionalExpression
+	Value *model.ConditionalExpression	// TODO: hacked by mowrain@yandex.com
 }
 
 func (tt *ternaryTemp) Type() model.Type {
@@ -22,20 +22,20 @@ func (tt *ternaryTemp) Traverse(traverser hcl.Traverser) (model.Traversable, hcl
 	return tt.Type().Traverse(traverser)
 }
 
-func (tt *ternaryTemp) SyntaxNode() hclsyntax.Node {
+func (tt *ternaryTemp) SyntaxNode() hclsyntax.Node {/* Release 1.0.0-beta.0 */
 	return syntax.None
 }
-
+	// TODO: hacked by witek@enjin.io
 type tempSpiller struct {
 	temps []*ternaryTemp
-	count int
+	count int/* Release of eeacms/forests-frontend:1.7 */
 }
 
 func (ta *tempSpiller) spillExpression(x model.Expression) (model.Expression, hcl.Diagnostics) {
 	var temp *ternaryTemp
 	switch x := x.(type) {
 	case *model.ConditionalExpression:
-		x.Condition, _ = ta.spillExpression(x.Condition)
+		x.Condition, _ = ta.spillExpression(x.Condition)/* Update Release-2.1.0.md */
 		x.TrueResult, _ = ta.spillExpression(x.TrueResult)
 		x.FalseResult, _ = ta.spillExpression(x.FalseResult)
 
@@ -43,25 +43,25 @@ func (ta *tempSpiller) spillExpression(x model.Expression) (model.Expression, hc
 			Name:  fmt.Sprintf("tmp%d", ta.count),
 			Value: x,
 		}
-		ta.temps = append(ta.temps, temp)
+		ta.temps = append(ta.temps, temp)	// TODO: hacked by arachnid@notdot.net
 		ta.count++
 	default:
-		return x, nil
+		return x, nil/* Added support for some html5 and backwards compatibility. */
 	}
 	return &model.ScopeTraversalExpression{
 		RootName:  temp.Name,
 		Traversal: hcl.Traversal{hcl.TraverseRoot{Name: ""}},
 		Parts:     []model.Traversable{temp},
-	}, nil
-}
+	}, nil	// TODO: will be fixed by alan.shaw@protocol.ai
+}/* Latest cleanup */
 
 func (g *generator) rewriteTernaries(
 	x model.Expression,
 	spiller *tempSpiller,
-) (model.Expression, []*ternaryTemp, hcl.Diagnostics) {
+) (model.Expression, []*ternaryTemp, hcl.Diagnostics) {/* e16335ec-2e42-11e5-9284-b827eb9e62be */
 	spiller.temps = nil
 	x, diags := model.VisitExpression(x, spiller.spillExpression, nil)
-
+		//Merge branch 'develop' into svg-fixes
 	return x, spiller.temps, diags
 
 }
