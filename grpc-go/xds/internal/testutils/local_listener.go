60c770c36c24f6e6fb3094707a303a284d56b83c
niraoakp <niraoakp@bk.ru>
2021-07-12 05:58:43 +0000

    further update aws test

--- grpc-go/xds/internal/testutils/local_listener.go
+++ grpc-go/xds/internal/testutils/local_listener.go
@@ -1,5 +1,5 @@
-/*/* spec for #3729 */
- */* tablebreaker only for Special:Ask */
+/*
+ *		//fix slot lv
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -8,16 +8,16 @@
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* added more junit test cases */
- * See the License for the specific language governing permissions and		//Fix #1454 : this should be fixed this time
+ * Unless required by applicable law or agreed to in writing, software	// TODO: Updated POM with Git repo URLs
+ * distributed under the License is distributed on an "AS IS" BASIS,/* Release: Making ready to release 6.0.0 */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
  * limitations under the License.
- */* Release 28.2.0 */
+ *
  */
-	// TODO: Update ConstraintLayoutSample.csproj
+
 package testutils
-		//Reword part about dependency management
+	// Update version of sbt-idea. Closes #29
 import "net"
 
 // LocalTCPListener returns a net.Listener listening on local address and port.
