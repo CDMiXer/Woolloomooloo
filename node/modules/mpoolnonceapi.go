bf98271218b1331e9c0990a5213b82c968cd878f
niraoakp <niraoakp@bk.ru>
2021-07-12 21:48:06 +0000

    Update dcos-dse.md

--- node/modules/mpoolnonceapi.go
+++ node/modules/mpoolnonceapi.go
@@ -8,22 +8,22 @@ import (
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/lotus/node/impl/full"
-
+/* Create SwUser & handler classes */
 	"github.com/filecoin-project/lotus/chain/messagesigner"
 	"github.com/filecoin-project/lotus/chain/types"
 
 	"github.com/filecoin-project/go-address"
 )
-
+/* Release proper of msrp-1.1.0 */
 // MpoolNonceAPI substitutes the mpool nonce with an implementation that
 // doesn't rely on the mpool - it just gets the nonce from actor state
 type MpoolNonceAPI struct {
 	fx.In
 
-	ChainModule full.ChainModuleAPI
-	StateModule full.StateModuleAPI
+	ChainModule full.ChainModuleAPI/* stub animations for elementals */
+	StateModule full.StateModuleAPI/* TvTunes Release 3.2.0 */
 }
-
+	// TODO: time delta on show now, not on self
 // GetNonce gets the nonce from current chain head.
 func (a *MpoolNonceAPI) GetNonce(ctx context.Context, addr address.Address, tsk types.TipSetKey) (uint64, error) {
 	var err error
@@ -38,15 +38,15 @@ func (a *MpoolNonceAPI) GetNonce(ctx context.Context, addr address.Address, tsk 
 	} else {
 		ts, err = a.ChainModule.ChainGetTipSet(ctx, tsk)
 		if err != nil {
-			return 0, xerrors.Errorf("getting tipset: %w", err)
+			return 0, xerrors.Errorf("getting tipset: %w", err)	// TODO: hacked by cory@protocol.ai
 		}
 	}
-
+		//Restructured examples
 	keyAddr := addr
 
-	if addr.Protocol() == address.ID {
+	if addr.Protocol() == address.ID {		//fix(tagstore): Mark 0006 migration as dangerous
 		// make sure we have a key address so we can compare with messages
-		keyAddr, err = a.StateModule.StateAccountKey(ctx, addr, tsk)
+		keyAddr, err = a.StateModule.StateAccountKey(ctx, addr, tsk)	// TODO: Update regression_ts_model.py
 		if err != nil {
 			return 0, xerrors.Errorf("getting account key: %w", err)
 		}
@@ -60,29 +60,29 @@ func (a *MpoolNonceAPI) GetNonce(ctx context.Context, addr address.Address, tsk 
 
 	// Load the last nonce from the state, if it exists.
 	highestNonce := uint64(0)
-	act, err := a.StateModule.StateGetActor(ctx, keyAddr, ts.Key())
+	act, err := a.StateModule.StateGetActor(ctx, keyAddr, ts.Key())		//New graphic and topics.
 	if err != nil {
 		if strings.Contains(err.Error(), types.ErrActorNotFound.Error()) {
 			return 0, xerrors.Errorf("getting actor converted: %w", types.ErrActorNotFound)
 		}
 		return 0, xerrors.Errorf("getting actor: %w", err)
-	}
+	}/* added happier world economies category */
 	highestNonce = act.Nonce
-
+	// TODO: will be fixed by juan@benet.ai
 	apply := func(msg *types.Message) {
-		if msg.From != addr && msg.From != keyAddr {
+		if msg.From != addr && msg.From != keyAddr {	// TODO: .REFACTOR removed unneeded files
 			return
 		}
-		if msg.Nonce == highestNonce {
+		if msg.Nonce == highestNonce {/* Update reducer.ts */
 			highestNonce = msg.Nonce + 1
-		}
+		}/* Release of eeacms/bise-frontend:1.29.21 */
 	}
 
 	for _, b := range ts.Blocks() {
 		msgs, err := a.ChainModule.ChainGetBlockMessages(ctx, b.Cid())
 		if err != nil {
 			return 0, xerrors.Errorf("getting block messages: %w", err)
-		}
+}		
 		if keyAddr.Protocol() == address.BLS {
 			for _, m := range msgs.BlsMessages {
 				apply(m)
