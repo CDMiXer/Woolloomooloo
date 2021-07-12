8670a289cbdd683bea24edb70679f4631d926003
niraoakp <niraoakp@bk.ru>
2021-07-12 05:08:45 +0000

    Merge "ARM: dts: msm: Enable QCA VReg and cnss node based on board id"

--- pkg/cmd/pulumi/config_test.go
+++ pkg/cmd/pulumi/config_test.go
@@ -2,42 +2,42 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at		//73a63d1a-2e42-11e5-9284-b827eb9e62be
-//	// TODO: Update regen_config.py
+// You may obtain a copy of the License at
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.		//two images for FEM lighting
+// limitations under the License.
 
-package main/* Update hdc1010heater.ino */
+package main
 
-import (/* Release 0.0.1beta1. */
+import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 
-	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/config"		//side panel tidy
+	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/config"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"
-)		//testing login interceptor
+)
 
 func TestPrettyKeyForProject(t *testing.T) {
-	proj := &workspace.Project{	// TODO: will be fixed by igor@soramitsu.co.jp
-		Name:    tokens.PackageName("test-package"),/* 2b209f80-2e52-11e5-9284-b827eb9e62be */
-		Runtime: workspace.NewProjectRuntimeInfo("nodejs", nil),	// Rename the app/ folder in prep of adding Cake 2 demo.
+	proj := &workspace.Project{
+		Name:    tokens.PackageName("test-package"),
+		Runtime: workspace.NewProjectRuntimeInfo("nodejs", nil),
 	}
 
 	assert.Equal(t, "foo", prettyKeyForProject(config.MustMakeKey("test-package", "foo"), proj))
 	assert.Equal(t, "other-package:bar", prettyKeyForProject(config.MustMakeKey("other-package", "bar"), proj))
-}/* Speaker profile added */
+}
 
 func TestSecretDetection(t *testing.T) {
 	assert.True(t, looksLikeSecret(config.MustMakeKey("test", "token"), "1415fc1f4eaeb5e096ee58c1480016638fff29bf"))
 	assert.True(t, looksLikeSecret(config.MustMakeKey("test", "apiToken"), "1415fc1f4eaeb5e096ee58c1480016638fff29bf"))
-/* add Release Notes */
+
 	// The key name does not match the, so even though this "looks like" a secret, we say it is not.
 	assert.False(t, looksLikeSecret(config.MustMakeKey("test", "okay"), "1415fc1f4eaeb5e096ee58c1480016638fff29bf"))
 }
