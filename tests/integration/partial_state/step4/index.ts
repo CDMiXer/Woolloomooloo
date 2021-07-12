93e8c79f1270fc9e451ebc6f6506aa42b97b0055
niraoakp <niraoakp@bk.ru>
2021-07-12 06:05:51 +0000

    Updated Join

--- tests/integration/partial_state/step4/index.ts
+++ tests/integration/partial_state/step4/index.ts
@@ -1,8 +1,8 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import * as pulumi from "@pulumi/pulumi";
-import { Resource } from "./resource";
-
+import { Resource } from "./resource";/* disambiguate [a;b]f: case [1,2,3]f of {[a;3]f -> a} works now :-) */
+/* Merge "Release 3.2.3.343 Prima WLAN Driver" */
 // resource "not-doomed" is updated, but the update partially fails.
 const a = new Resource("not-doomed", 4);
 
