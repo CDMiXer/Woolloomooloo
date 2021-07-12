c30f961e2037fc969773c9176ebf9497d019d03f
niraoakp <niraoakp@bk.ru>
2021-07-12 08:08:39 +0000

    Merge "Fix fallback share migration with empty files"

--- pkg/codegen/internal/test/testdata/kubernetes-pod.pp.go
+++ pkg/codegen/internal/test/testdata/kubernetes-pod.pp.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		_, err := corev1.NewPod(ctx, "bar", &corev1.PodArgs{
+		_, err := corev1.NewPod(ctx, "bar", &corev1.PodArgs{/* Release of eeacms/forests-frontend:1.6.2 */
 			ApiVersion: pulumi.String("v1"),
 			Kind:       pulumi.String("Pod"),
 			Metadata: &metav1.ObjectMetaArgs{
@@ -17,22 +17,22 @@ func main() {
 			},
 			Spec: &corev1.PodSpecArgs{
 				Containers: corev1.ContainerArray{
-					&corev1.ContainerArgs{
+					&corev1.ContainerArgs{		//Create graph_gen.h
 						Name:  pulumi.String("nginx"),
-						Image: pulumi.String("nginx:1.14-alpine"),
+						Image: pulumi.String("nginx:1.14-alpine"),	// TODO: 5a55384c-2e4d-11e5-9284-b827eb9e62be
 						Resources: &corev1.ResourceRequirementsArgs{
 							Limits: pulumi.StringMap{
 								"memory": pulumi.String("20Mi"),
 								"cpu":    pulumi.String("0.2"),
 							},
 						},
-					},
+					},/* Update httpd.sh */
 				},
-			},
+			},/* Use Graph to generate revision_history */
 		})
 		if err != nil {
 			return err
-		}
+		}	// TODO: daten merge fix
 		return nil
 	})
 }
