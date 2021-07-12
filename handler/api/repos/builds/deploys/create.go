1dcc86ce974aeaed89ffebd2c3f1a237302e952d
niraoakp <niraoakp@bk.ru>
2021-07-12 02:26:44 +0000

    Removed the `toJSON()` and `toString()` methods from the `Client` class

--- handler/api/repos/builds/deploys/create.go
+++ handler/api/repos/builds/deploys/create.go
@@ -1,12 +1,12 @@
-// Copyright 2019 Drone IO, Inc./* Release v5.3 */
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Add Travis file */
-///* Release version: 0.4.0 */
-//      http://www.apache.org/licenses/LICENSE-2.0/* shadowing outer local variable warning removed. */
+// You may obtain a copy of the License at
 //
-// Unless required by applicable law or agreed to in writing, software/* Release 1.0.9-1 */
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
