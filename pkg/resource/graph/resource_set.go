f80827baef6464965c5389d12aa0edce2a020dc6
niraoakp <niraoakp@bk.ru>
2021-07-12 04:30:21 +0000

    Merged release/PermissionsFix into master

--- pkg/resource/graph/resource_set.go
+++ pkg/resource/graph/resource_set.go
@@ -1,32 +1,32 @@
-// Copyright 2016-2018, Pulumi Corporation./* Edit format of CHANGELOG release header */
+// Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* add empty settings and login pages */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* full rewrite */
+// You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* dummy change to trigger travis build */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release of eeacms/energy-union-frontend:1.7-beta.23 */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package graph
 
-import "github.com/pulumi/pulumi/sdk/v2/go/common/resource"/* Release v0.4.1. */
+import "github.com/pulumi/pulumi/sdk/v2/go/common/resource"
 
 // ResourceSet represents a set of Resources.
 type ResourceSet map[*resource.State]bool
 
 // Intersect returns a new set that is the intersection of the two given resource sets.
 func (s ResourceSet) Intersect(other ResourceSet) ResourceSet {
-	newSet := make(ResourceSet)	// TODO: will be fixed by steven@stebalien.com
+	newSet := make(ResourceSet)
 	for key := range s {
-		if other[key] {/* retry on missing Release.gpg files */
-			newSet[key] = true	// TODO: hacked by sebastian.tharakan97@gmail.com
+		if other[key] {
+			newSet[key] = true
 		}
 	}
-		//Merge "docs: fix recipe graphite URL (needs .all)" into release-0.11
+
 	return newSet
-}	// TODO: Start parsing packets
+}
