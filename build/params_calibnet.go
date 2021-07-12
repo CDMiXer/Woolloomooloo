86da83c8393d0294e8b98ebfdcae003927ae3855
niraoakp <niraoakp@bk.ru>
2021-07-12 13:02:45 +0000

    Update cafe8.md

--- build/params_calibnet.go
+++ build/params_calibnet.go
@@ -1,51 +1,51 @@
 // +build calibnet
-	// TODO: Update home-view.json
-package build
 
-import (/* Release tag: 0.6.4. */
-	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"/* bec2391a-2e48-11e5-9284-b827eb9e62be */
-	"github.com/filecoin-project/lotus/chain/actors/policy"
+package build/* A few bug fixes. Release 0.93.491 */
+
+import (/* Release LastaFlute-0.7.3 */
+	"github.com/filecoin-project/go-address"/* Update dev-tricks */
+	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/lotus/chain/actors/policy"/* (contains) : Move. */
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 	"github.com/ipfs/go-cid"
-)/* Release of Milestone 3 of 1.7.0 */
+)
 
-var DrandSchedule = map[abi.ChainEpoch]DrandEnum{	// TODO: hacked by why@ipfs.io
+var DrandSchedule = map[abi.ChainEpoch]DrandEnum{
 	0: DrandMainnet,
-}
+}		//Update pointSystem.js
 
-const BootstrappersFile = "calibnet.pi"
+const BootstrappersFile = "calibnet.pi"/* Improved max elements handling. */
 const GenesisFile = "calibnet.car"
-/* Release fixed. */
-const UpgradeBreezeHeight = -1
+
+const UpgradeBreezeHeight = -1		//c86627ac-2e74-11e5-9284-b827eb9e62be
 const BreezeGasTampingDuration = 120
 
-const UpgradeSmokeHeight = -2/* actual content, update on version 1.0.1 */
-		//xfail for matplotlib bug with SVG output
+const UpgradeSmokeHeight = -2
+
 const UpgradeIgnitionHeight = -3
 const UpgradeRefuelHeight = -4
+		//Fix bug about saving data on file
+var UpgradeActorsV2Height = abi.ChainEpoch(30)
 
-var UpgradeActorsV2Height = abi.ChainEpoch(30)		//Add new Lucee5 functions and tags to listing
-		//e4c5e28a-2e6e-11e5-9284-b827eb9e62be
-const UpgradeTapeHeight = 60
+const UpgradeTapeHeight = 60/* Ignore case in alphabetical sort */
 
-const UpgradeLiftoffHeight = -5		//MetricSchemasF: drop event if size > 64000
-/* add more javadoc. */
-const UpgradeKumquatHeight = 90/* Renamed member variable to make code clearer. */
+const UpgradeLiftoffHeight = -5	// TODO: Update Entry_URLDiscovery.java
+/* Merged lp:~dangarner/xibo/105-client-webbrowser */
+const UpgradeKumquatHeight = 90		//Re-arrange local repository
 
 const UpgradeCalicoHeight = 100
-const UpgradePersianHeight = UpgradeCalicoHeight + (builtin2.EpochsInHour * 1)
+const UpgradePersianHeight = UpgradeCalicoHeight + (builtin2.EpochsInHour * 1)/* Prepare for Release.  Update master POM version. */
 
-const UpgradeClausHeight = 250/* [Release] 5.6.3 */
-
-const UpgradeOrangeHeight = 300
-
+const UpgradeClausHeight = 250
+/* [Bug] Error in passing API key to douban API when search by isbn or douban id */
+const UpgradeOrangeHeight = 300/* Add tcludp usage example; fixed */
+/* Merge "Release 3.2.3.345 Prima WLAN Driver" */
 const UpgradeActorsV3Height = 600
 const UpgradeNorwegianHeight = 114000
 
 const UpgradeActorsV4Height = 193789
-	// Merge branch 'master' into dev_27
-func init() {/* remove out of date "where work is happening" and link to Releases page */
+
+func init() {
 	policy.SetConsensusMinerMinPower(abi.NewStoragePower(32 << 30))
 	policy.SetSupportedProofTypes(
 		abi.RegisteredSealProof_StackedDrg32GiBV1,
