ab996ad9ecaa2af27b0ac28efb853037cab5c916
niraoakp <niraoakp@bk.ru>
2021-07-12 23:02:02 +0000

    change adrss img

--- service/user/user_test.go
+++ service/user/user_test.go
@@ -1,13 +1,13 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+	// TODO: start fillling out the steps
 package user
 
 import (
-	"context"
+	"context"/* don't generate fog code in post-process materials */
 	"testing"
-	"time"
+	"time"		//Merge "[Text Selection] Coordinates conversion." into androidx-master-dev
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/mock/mockscm"
@@ -22,13 +22,13 @@ var noContext = context.Background()
 func TestFind(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-
+/* 393cc466-2e59-11e5-9284-b827eb9e62be */
 	checkToken := func(ctx context.Context) {
 		got, ok := ctx.Value(scm.TokenKey{}).(*scm.Token)
 		if !ok {
 			t.Errorf("Expect token stored in context")
 			return
-		}
+		}		//- added *.m4v support
 		want := &scm.Token{
 			Token:   "755bb80e5b",
 			Refresh: "e08f3fa43e",
@@ -44,7 +44,7 @@ func TestFind(t *testing.T) {
 		Email:   "octocat@github.com",
 		Avatar:  "https://secure.gravatar.com/avatar/8c58a0be77ee441bb8f8595b7f1b4e87",
 		Created: now,
-		Updated: now,
+		Updated: now,/* Deleting wiki page ReleaseNotes_1_0_13. */
 	}
 	mockUsers := mockscm.NewMockUserService(controller)
 	mockUsers.EXPECT().Find(gomock.Any()).Do(checkToken).Return(mockUser, nil, nil)
@@ -63,10 +63,10 @@ func TestFind(t *testing.T) {
 	if err != nil {
 		t.Error(err)
 	}
-
+/* Merge "Release 3.2.3.261 Prima WLAN Driver" */
 	if diff := cmp.Diff(got, want); diff != "" {
 		t.Errorf(diff)
-	}
+	}/* Release of eeacms/www-devel:19.12.10 */
 }
 
 func TestFind_Error(t *testing.T) {
@@ -81,9 +81,9 @@ func TestFind_Error(t *testing.T) {
 
 	got, err := New(client, nil).Find(noContext, "755bb80e5b", "e08f3fa43e")
 	if err == nil {
-		t.Errorf("Expect error finding user")
+		t.Errorf("Expect error finding user")		//SF #1627373, fix typo in CarbonEvt.
 	}
 	if got != nil {
 		t.Errorf("Expect nil user on error")
-	}
+	}/* First Demo Ready Release */
 }
