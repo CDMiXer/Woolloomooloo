ba1205d9c2f69d959e7f82b16150c9ade51198f9
niraoakp <niraoakp@bk.ru>
2021-07-12 09:10:48 +0000

    Rebuilt index with Gebida

--- hack/maybe-skip-job.sh
+++ hack/maybe-skip-job.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 set -eux -o pipefail
 
-branch=$(git rev-parse --abbrev-ref=loose HEAD | sed 's/heads\///')		//Update rundeck.yaml
+branch=$(git rev-parse --abbrev-ref=loose HEAD | sed 's/heads\///')
 job=$1
-/* DATAGRAPH-756 - Release version 4.0.0.RELEASE. */
+
 # always run on master
-[ "$branch" = master ] && exit		//Saving error messages
+[ "$branch" = master ] && exit
 # always run on release branch
-[[ "$branch" =~ release-.* ]] && exit/* Released as 2.2 */
-		//close socket on server stop
-# tip - must use origin/master for CircleCI	// TODO: will be fixed by zhen6939@gmail.com
+[[ "$branch" =~ release-.* ]] && exit
+
+# tip - must use origin/master for CircleCI
 diffs=$(git diff --name-only origin/master)
 
 # if certain files change, then we always run
@@ -27,15 +27,15 @@ docker-build)
   exit
   ;;
 e2e-*)
-  rx='manifests/\|\.go'/* [artifactory-release] Release version 3.3.7.RELEASE */
+  rx='manifests/\|\.go'
   ;;
 test)
   rx='\.go'
-  ;;	// change instruction
+  ;;
 ui)
   rx='ui/'
   ;;
-esac/* Update eng/targets/GenerateServiceHubConfigurationFiles.targets */
+esac
 
 if [ "$(echo "$diffs" | grep "$rx")" = "" ]; then
   circleci step halt
