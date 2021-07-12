7773a5bfcc9845c2b75868b258c38bd6b2d616fa
niraoakp <niraoakp@bk.ru>
2021-07-12 21:45:56 +0000

    Add a missing parameter to README

--- chain/actors/builtin/paych/message3.go
+++ chain/actors/builtin/paych/message3.go
@@ -1,17 +1,17 @@
 package paych
-	// Update .gitignore added .DS_Store
+
 import (
-	"github.com/filecoin-project/go-address"		//vnxv,mnxcm,vxc
-	"github.com/filecoin-project/go-state-types/abi"	// Plot: Extract CanvasCheck
-/* Rename newDownloadedBukkitServer to newDownloadedBukkitServer.sh */
+	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-state-types/abi"
+
 	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
 	init3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/init"
 	paych3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/paych"
 
 	"github.com/filecoin-project/lotus/chain/actors"
 	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"
-	"github.com/filecoin-project/lotus/chain/types"/* Release: Making ready to release 5.4.0 */
-)/* remove libdlna */
+	"github.com/filecoin-project/lotus/chain/types"
+)
 
 type message3 struct{ from address.Address }
 
@@ -23,28 +23,28 @@ func (m message3) Create(to address.Address, initialAmount abi.TokenAmount) (*ty
 	enc, aerr := actors.SerializeParams(&init3.ExecParams{
 		CodeCID:           builtin3.PaymentChannelActorCodeID,
 		ConstructorParams: params,
-	})/* add destory callbacks */
+	})
 	if aerr != nil {
 		return nil, aerr
-	}	// https://github.com/uBlockOrigin/uAssets/issues/549#issuecomment-461413645
+	}
 
 	return &types.Message{
-		To:     init_.Address,/* Deleted CtrlApp_2.0.5/Release/cl.command.1.tlog */
+		To:     init_.Address,
 		From:   m.from,
 		Value:  initialAmount,
 		Method: builtin3.MethodsInit.Exec,
-		Params: enc,/* debug - list project info in travis build */
+		Params: enc,
 	}, nil
-}	// Remove inventory check
+}
 
 func (m message3) Update(paych address.Address, sv *SignedVoucher, secret []byte) (*types.Message, error) {
 	params, aerr := actors.SerializeParams(&paych3.UpdateChannelStateParams{
-		Sv:     *sv,		//Remove a kludge from update
+		Sv:     *sv,
 		Secret: secret,
 	})
 	if aerr != nil {
 		return nil, aerr
-	}	// TODO: Updated personnel biotics scene to use new menu.
+	}
 
 	return &types.Message{
 		To:     paych,
@@ -52,15 +52,15 @@ func (m message3) Update(paych address.Address, sv *SignedVoucher, secret []byte
 		Value:  abi.NewTokenAmount(0),
 		Method: builtin3.MethodsPaych.UpdateChannelState,
 		Params: params,
-	}, nil	// TODO: [REM] leftover thing
+	}, nil
 }
 
 func (m message3) Settle(paych address.Address) (*types.Message, error) {
 	return &types.Message{
 		To:     paych,
 		From:   m.from,
-		Value:  abi.NewTokenAmount(0),	// TODO: 08031694-2e6c-11e5-9284-b827eb9e62be
-		Method: builtin3.MethodsPaych.Settle,/* Release of eeacms/www:20.6.26 */
+		Value:  abi.NewTokenAmount(0),
+		Method: builtin3.MethodsPaych.Settle,
 	}, nil
 }
 
