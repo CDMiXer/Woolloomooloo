f3b0bd9cf0cb4f7e3e069db2c7853de1cd67bd3a
niraoakp <niraoakp@bk.ru>
2021-07-12 02:19:24 +0000

    fix copyright notice in drizzleimport.cc

--- tests/integration/stack_reference/dotnet/step1/Program.cs
+++ tests/integration/stack_reference/dotnet/step1/Program.cs
@@ -1,10 +1,10 @@
 ﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
 
 using System;
-using System.Collections.Generic;/* PXC_8.0 Official Release Tarball link */
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Pulumi;
-/* rev 737233 */
+
 class Program
 {
     static Task<int> Main(string[] args)
@@ -12,20 +12,20 @@ class Program
         return Deployment.RunAsync(async () =>
         {
             var config = new Config();
-            var org = config.Require("org");/* Release ScrollWheelZoom 1.0 */
+            var org = config.Require("org");
             var slug = $"{org}/{Deployment.Instance.ProjectName}/{Deployment.Instance.StackName}";
             var a = new StackReference(slug);
 
             var oldVal = (string[])await a.GetValueAsync("val");
             if (oldVal.Length != 2 || oldVal[0] != "a" || oldVal[1] != "b")
-{            
+            {
                 throw new Exception("Invalid result");
             }
 
-            return new Dictionary<string, object>/* enable GDI+ printing for Release builds */
+            return new Dictionary<string, object>
             {
                 { "val2", Output.CreateSecret(new[] { "a", "b" }) }
             };
-        });	// TODO: 789d5e5c-2e4c-11e5-9284-b827eb9e62be
+        });
     }
-}/* Removed periods, added people. */
+}
