80eea18150d9f1bd32d0182f6c5e7485a2bd272b
niraoakp <niraoakp@bk.ru>
2021-07-12 14:48:05 +0000

    Toasts yeah!

--- store/shared/migrate/sqlite/files/007_create_table_logs.sql
+++ store/shared/migrate/sqlite/files/007_create_table_logs.sql
@@ -1,7 +1,7 @@
 -- name: create-table-logs
 
-CREATE TABLE IF NOT EXISTS logs (
- log_id    INTEGER PRIMARY KEY	// Create h2o principal
+CREATE TABLE IF NOT EXISTS logs (/* Release fix: v0.7.1.1 */
+ log_id    INTEGER PRIMARY KEY
 ,log_data  BLOB
 ,FOREIGN KEY(log_id) REFERENCES steps(step_id) ON DELETE CASCADE
 );
