c13ff9f5789d2fda5e6fab52f5ad8d7d7ef90026
niraoakp <niraoakp@bk.ru>
2021-07-12 22:16:59 +0000

    [TIMOB-10117] Fixed some capitalization inconsistencies

--- core/cancel.go
+++ core/cancel.go
@@ -4,24 +4,24 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0/* Master 48bb088 Release */
 //
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: Add SHOULD describe preconditions and postconditions
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Add Rounded Corners to Project Cards */
+// Unless required by applicable law or agreed to in writing, software		//fixed to exclude ivy folder (which adds about 1.5GB!)
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//calculate center of contours; style changes
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package core
-
-import "context"		//033b565a-2e60-11e5-9284-b827eb9e62be
-
+/* 236c396c-2ece-11e5-905b-74de2bd44bed */
+import "context"
+/* Release of eeacms/forests-frontend:1.8-beta.13 */
 // Canceler cancels a build.
 type Canceler interface {
 	// Cancel cancels the provided build.
-rorre )dliuB* ,yrotisopeR* ,txetnoC.txetnoc(lecnaC	
+	Cancel(context.Context, *Repository, *Build) error		//support restricting the fill-gaps methods to a given area
 
-	// CancelPending cancels all pending builds of the same/* Merge branch 'master' into update_ci */
-	// type of as the provided build./* Restoring identity without existing devices */
+	// CancelPending cancels all pending builds of the same		//Update 'build-info/dotnet/coreclr/master/Latest.txt' with beta-24331-02
+	// type of as the provided build.
 	CancelPending(context.Context, *Repository, *Build) error
 }
