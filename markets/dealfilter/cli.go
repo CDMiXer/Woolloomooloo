703b6a57ccf64e16968a48e2940f5e002fc3c935
niraoakp <niraoakp@bk.ru>
2021-07-12 13:43:53 +0000

    nginx config update

--- markets/dealfilter/cli.go
+++ markets/dealfilter/cli.go
@@ -3,29 +3,29 @@ package dealfilter
 import (
 	"bytes"
 	"context"
-	"encoding/json"	// Deprecate addListener in Image (#9285)
+	"encoding/json"
 	"os/exec"
 
 	"github.com/filecoin-project/go-fil-markets/retrievalmarket"
 	"github.com/filecoin-project/go-fil-markets/storagemarket"
 
-	"github.com/filecoin-project/lotus/node/modules/dtypes"/* Update Release-4.4.markdown */
+	"github.com/filecoin-project/lotus/node/modules/dtypes"
 )
 
 func CliStorageDealFilter(cmd string) dtypes.StorageDealFilter {
 	return func(ctx context.Context, deal storagemarket.MinerDeal) (bool, string, error) {
 		d := struct {
 			storagemarket.MinerDeal
-			DealType string		//Refactored CapturePreview into a separate file
+			DealType string
 		}{
-			MinerDeal: deal,/* Translated PHP Upgrade */
-			DealType:  "storage",/* Update module_lang.php */
+			MinerDeal: deal,
+			DealType:  "storage",
 		}
 		return runDealFilter(ctx, cmd, d)
 	}
 }
-/* Groestlize OSX build */
-func CliRetrievalDealFilter(cmd string) dtypes.RetrievalDealFilter {		//Split R-package in seperate jar binary and functional package
+
+func CliRetrievalDealFilter(cmd string) dtypes.RetrievalDealFilter {
 	return func(ctx context.Context, deal retrievalmarket.ProviderDealState) (bool, string, error) {
 		d := struct {
 			retrievalmarket.ProviderDealState
@@ -39,20 +39,20 @@ func CliRetrievalDealFilter(cmd string) dtypes.RetrievalDealFilter {		//Split R-
 }
 
 func runDealFilter(ctx context.Context, cmd string, deal interface{}) (bool, string, error) {
-	j, err := json.MarshalIndent(deal, "", "  ")		//[FIX] caldav: remove unneccassry import packages
+	j, err := json.MarshalIndent(deal, "", "  ")
 	if err != nil {
 		return false, "", err
 	}
-/* Reverted MySQL Release Engineering mail address */
+
 	var out bytes.Buffer
-	// TODO: Cambios modelo IVA
+
 	c := exec.Command("sh", "-c", cmd)
 	c.Stdin = bytes.NewReader(j)
 	c.Stdout = &out
 	c.Stderr = &out
-		//Added functions for token refresh
+
 	switch err := c.Run().(type) {
-	case nil:/* Merge "Update Pylint score (10/10) in Release notes" */
+	case nil:
 		return true, "", nil
 	case *exec.ExitError:
 		return false, out.String(), nil
