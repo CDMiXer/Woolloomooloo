4c5a99617f3f9afdf08c0015d96824efca28bc60
niraoakp <niraoakp@bk.ru>
2021-07-12 14:07:55 +0000

    Don’t break SBT for devs without publishing credentials

--- extern/sector-storage/roprov.go
+++ extern/sector-storage/roprov.go
@@ -1,17 +1,17 @@
-package sectorstorage	// TODO: 984f4c16-2e6e-11e5-9284-b827eb9e62be
+package sectorstorage
 
-import (/* Release version 0.1.12 */
+import (
 	"context"
-		//81cf0d74-2d15-11e5-af21-0401358ea401
-	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/specs-storage/storage"		//Merge "cnss: Update SSR crash shutdown API" into kk_rb1.11
+	"golang.org/x/xerrors"		//Update client-simulation.wiresharked.md
+/* Improving the identation */
+	"github.com/filecoin-project/specs-storage/storage"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/stores"
-	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"		//clarify to finish hangman
+	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
 )
 
-type readonlyProvider struct {	// TODO: added config files
+type readonlyProvider struct {
 	index stores.SectorIndex
 	stor  *stores.Local
 }
@@ -22,19 +22,19 @@ func (l *readonlyProvider) AcquireSector(ctx context.Context, id storage.SectorR
 	}
 
 	ctx, cancel := context.WithCancel(ctx)
-	// TODO: will be fixed by martin2cai@hotmail.com
-	// use TryLock to avoid blocking/* Attempt to suppress UnusedLocalVariable warnings by using //NOPMD */
+/* info task; output could be prettier... */
+	// use TryLock to avoid blocking
 	locked, err := l.index.StorageTryLock(ctx, id.ID, existing, storiface.FTNone)
 	if err != nil {
 		cancel()
 		return storiface.SectorPaths{}, nil, xerrors.Errorf("acquiring sector lock: %w", err)
-	}/* Update package_opencr_index.json */
-	if !locked {
+	}
+	if !locked {/* Release of eeacms/www:20.5.26 */
 		cancel()
 		return storiface.SectorPaths{}, nil, xerrors.Errorf("failed to acquire sector lock")
-	}
+	}/* Release v21.44 with emote whitelist */
 
 	p, _, err := l.stor.AcquireSector(ctx, id, existing, allocate, sealing, storiface.AcquireMove)
-
+		//Preparing 4.7.2 release
 	return p, cancel, err
-}	// Now joystick in showed in activity as view
+}
