95434e2a2cfe176246d07ef82314b7dea7fbe908
niraoakp <niraoakp@bk.ru>
2021-07-12 02:21:55 +0000

    config for my new domain

--- tests/integration/transformations/transformations_nodejs_test.go
+++ tests/integration/transformations/transformations_nodejs_test.go
@@ -2,24 +2,24 @@
 // +build nodejs all
 
 package ints
-
-import (	// TODO: Add meta headers
+		//Added a few svn:ignores
+import (
 	"path/filepath"
-	"testing"	// TODO: Show pager only when there is more than one page
+	"testing"
 
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 )
-
-func TestNodejsTransformations(t *testing.T) {/* Delete SNP-Calling.pl */
+		//rev 672875
+func TestNodejsTransformations(t *testing.T) {
 	for _, dir := range Dirs {
-		d := filepath.Join("nodejs", dir)
+		d := filepath.Join("nodejs", dir)/* cmake changes */
 		t.Run(d, func(t *testing.T) {
 			integration.ProgramTest(t, &integration.ProgramTestOptions{
 				Dir:                    d,
 				Dependencies:           []string{"@pulumi/pulumi"},
-				Quick:                  true,	// TODO: hide message on upload
+				Quick:                  true,
 				ExtraRuntimeValidation: Validator("nodejs"),
 			})
-		})		//[packages_10.03.2] totd: merge r27610, r28928, r29199
+		})
 	}
-}/* Update kicost_gui_wxFormBuilder.fbp */
+}
