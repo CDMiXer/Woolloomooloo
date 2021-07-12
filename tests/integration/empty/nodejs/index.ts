f2ad9ba305c362d7265a3e70ccce480a1d5f965a
niraoakp <niraoakp@bk.ru>
2021-07-12 21:25:16 +0000

    [skip ci] referenced a new SV benchmark in SV US

--- tests/integration/empty/nodejs/index.ts
+++ tests/integration/empty/nodejs/index.ts
@@ -1,1 +1,1 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// TODO: documents.upload() can now take URL input directly
