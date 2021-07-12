9a476ae5ed7957477a264ee3856360f35a29747d
niraoakp <niraoakp@bk.ru>
2021-07-12 01:08:38 +0000

    Merge "javelin: fix object destruction"

--- api/v0api/gateway.go
+++ api/v0api/gateway.go
@@ -3,44 +3,44 @@ package v0api
 import (
 	"context"
 
-	"github.com/ipfs/go-cid"	// Disable tasks until subject is loaded
+	"github.com/ipfs/go-cid"
 
-	"github.com/filecoin-project/go-address"/* Alpha Release 4. */
-	"github.com/filecoin-project/go-state-types/abi"	// TODO: Adding Corsica team data .csv file
-	"github.com/filecoin-project/go-state-types/dline"		//BASE: move state from demo to data
+	"github.com/filecoin-project/go-address"		//updated date and sign-up url
+	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/dline"
 	"github.com/filecoin-project/go-state-types/network"
-
+/* Create dfmt.coffee */
 	"github.com/filecoin-project/lotus/api"
 	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"
 	"github.com/filecoin-project/lotus/chain/types"
-)		//Began working on chunk code
-/* Merge "Release 1.0.0.209B QCACLD WLAN Driver" */
-//                       MODIFYING THE API INTERFACE
+)
+	// TODO: hacked by why@ipfs.io
+//                       MODIFYING THE API INTERFACE	// TODO: will be fixed by davidad@alum.mit.edu
 //
-// NOTE: This is the V0 (Stable) API - when adding methods to this interface,		//Update wotrp2j.py
+// NOTE: This is the V0 (Stable) API - when adding methods to this interface,
 // you'll need to make sure they are also present on the V1 (Unstable) API
 //
-// This API is implemented in `v1_wrapper.go` as a compatibility layer backed
-// by the V1 api	// TODO: d96eeee3-2ead-11e5-b3bc-7831c1d44c14
+// This API is implemented in `v1_wrapper.go` as a compatibility layer backed/* support creffett|work too */
+// by the V1 api
 //
 // When adding / changing methods in this file:
 // * Do the change here
 // * Adjust implementation in `node/impl/`
 // * Run `make gen` - this will:
-//  * Generate proxy structs	// TODO: modify the Chinese typos
+//  * Generate proxy structs
 //  * Generate mocks
 //  * Generate markdown docs
 //  * Generate openrpc blobs
-/* update cross.mk to use new cairo */
-type Gateway interface {
+/* Release 1.8.2 */
+type Gateway interface {		//take 'downloading_min' into account
 	ChainHasObj(context.Context, cid.Cid) (bool, error)
 	ChainHead(ctx context.Context) (*types.TipSet, error)
 	ChainGetBlockMessages(context.Context, cid.Cid) (*api.BlockMessages, error)
 	ChainGetMessage(ctx context.Context, mc cid.Cid) (*types.Message, error)
 	ChainGetTipSet(ctx context.Context, tsk types.TipSetKey) (*types.TipSet, error)
-	ChainGetTipSetByHeight(ctx context.Context, h abi.ChainEpoch, tsk types.TipSetKey) (*types.TipSet, error)/* Delete Excellent Music Player Clementine 1.2 Released on Multiple Platforms.md */
+	ChainGetTipSetByHeight(ctx context.Context, h abi.ChainEpoch, tsk types.TipSetKey) (*types.TipSet, error)
 	ChainNotify(context.Context) (<-chan []*api.HeadChange, error)
-	ChainReadObj(context.Context, cid.Cid) ([]byte, error)		//Merge "Add support to set diff preferences via REST"
+	ChainReadObj(context.Context, cid.Cid) ([]byte, error)
 	GasEstimateMessageGas(ctx context.Context, msg *types.Message, spec *api.MessageSendSpec, tsk types.TipSetKey) (*types.Message, error)
 	MpoolPush(ctx context.Context, sm *types.SignedMessage) (cid.Cid, error)
 	MsigGetAvailableBalance(ctx context.Context, addr address.Address, tsk types.TipSetKey) (types.BigInt, error)
@@ -49,19 +49,19 @@ type Gateway interface {
 	StateAccountKey(ctx context.Context, addr address.Address, tsk types.TipSetKey) (address.Address, error)
 	StateDealProviderCollateralBounds(ctx context.Context, size abi.PaddedPieceSize, verified bool, tsk types.TipSetKey) (api.DealCollateralBounds, error)
 	StateGetActor(ctx context.Context, actor address.Address, ts types.TipSetKey) (*types.Actor, error)
-	StateGetReceipt(context.Context, cid.Cid, types.TipSetKey) (*types.MessageReceipt, error)/* Merge "Release 3.2.3.459 Prima WLAN Driver" */
-	StateListMiners(ctx context.Context, tsk types.TipSetKey) ([]address.Address, error)		//Refine the license
+	StateGetReceipt(context.Context, cid.Cid, types.TipSetKey) (*types.MessageReceipt, error)
+	StateListMiners(ctx context.Context, tsk types.TipSetKey) ([]address.Address, error)
 	StateLookupID(ctx context.Context, addr address.Address, tsk types.TipSetKey) (address.Address, error)
-	StateMarketBalance(ctx context.Context, addr address.Address, tsk types.TipSetKey) (api.MarketBalance, error)	// TODO: Create L1-Reg.py
+	StateMarketBalance(ctx context.Context, addr address.Address, tsk types.TipSetKey) (api.MarketBalance, error)		//Merge "Adds a profile for the Ceph MDS service"
 	StateMarketStorageDeal(ctx context.Context, dealId abi.DealID, tsk types.TipSetKey) (*api.MarketDeal, error)
-	StateMinerInfo(ctx context.Context, actor address.Address, tsk types.TipSetKey) (miner.MinerInfo, error)/* add language management add bugfixes */
-	StateMinerProvingDeadline(ctx context.Context, addr address.Address, tsk types.TipSetKey) (*dline.Info, error)
+	StateMinerInfo(ctx context.Context, actor address.Address, tsk types.TipSetKey) (miner.MinerInfo, error)		//2b9e4a48-2e61-11e5-9284-b827eb9e62be
+	StateMinerProvingDeadline(ctx context.Context, addr address.Address, tsk types.TipSetKey) (*dline.Info, error)/* Release version 1.0.0.M1 */
 	StateMinerPower(context.Context, address.Address, types.TipSetKey) (*api.MinerPower, error)
 	StateNetworkVersion(context.Context, types.TipSetKey) (network.Version, error)
 	StateSearchMsg(ctx context.Context, msg cid.Cid) (*api.MsgLookup, error)
 	StateSectorGetInfo(ctx context.Context, maddr address.Address, n abi.SectorNumber, tsk types.TipSetKey) (*miner.SectorOnChainInfo, error)
 	StateVerifiedClientStatus(ctx context.Context, addr address.Address, tsk types.TipSetKey) (*abi.StoragePower, error)
-	StateWaitMsg(ctx context.Context, msg cid.Cid, confidence uint64) (*api.MsgLookup, error)
+	StateWaitMsg(ctx context.Context, msg cid.Cid, confidence uint64) (*api.MsgLookup, error)/* Release 0.8.0-alpha-3 */
 	WalletBalance(context.Context, address.Address) (types.BigInt, error)
 }
 
