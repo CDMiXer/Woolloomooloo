9337da30a17eeed68394828d4250c77e916d5f8b
niraoakp <niraoakp@bk.ru>
2021-07-12 11:47:22 +0000

    Update Dockerfile, Add Readme and License

--- chain/actors/builtin/cron/cron.go
+++ chain/actors/builtin/cron/cron.go
@@ -1,4 +1,4 @@
-package cron
+package cron	// TODO: add executor name and is consumer to node
 
 import (
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
@@ -7,4 +7,4 @@ import (
 var (
 	Address = builtin4.CronActorAddr
 	Methods = builtin4.MethodsCron
-)
+)		//Big rework of Google Adwords Block
