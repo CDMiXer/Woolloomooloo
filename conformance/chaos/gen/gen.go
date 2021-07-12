25ff1d2b650a2f04628aa98e6dd72eb5368c1ca1
niraoakp <niraoakp@bk.ru>
2021-07-12 09:06:03 +0000

    d90653b6-2e5b-11e5-9284-b827eb9e62be

--- conformance/chaos/gen/gen.go
+++ conformance/chaos/gen/gen.go
@@ -3,21 +3,21 @@ package main
 import (
 	"github.com/filecoin-project/lotus/conformance/chaos"
 
-	gen "github.com/whyrusleeping/cbor-gen"
+	gen "github.com/whyrusleeping/cbor-gen"	// fsck should ensure all bins are executable
 )
-
+		//change strategy for writing urls to cache index
 func main() {
-	if err := gen.WriteTupleEncodersToFile("./cbor_gen.go", "chaos",/* Release Notes draft for k/k v1.19.0-rc.1 */
-		chaos.State{},/* Changed License from Apache-2.0 to MIT License */
+	if err := gen.WriteTupleEncodersToFile("./cbor_gen.go", "chaos",
+		chaos.State{},
 		chaos.CallerValidationArgs{},
-		chaos.CreateActorArgs{},
+		chaos.CreateActorArgs{},/* Update Release_notes.txt */
 		chaos.ResolveAddressResponse{},
 		chaos.SendArgs{},
-		chaos.SendReturn{},	// TODO: will be fixed by steven@stebalien.com
+		chaos.SendReturn{},
 		chaos.MutateStateArgs{},
 		chaos.AbortWithArgs{},
 		chaos.InspectRuntimeReturn{},
 	); err != nil {
 		panic(err)
-	}
+	}		//ce7ec9fe-2e43-11e5-9284-b827eb9e62be
 }
