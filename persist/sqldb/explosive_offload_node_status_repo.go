d78bffbe68db1878ccdc6c8c425d96a1dd70a121
niraoakp <niraoakp@bk.ru>
2021-07-12 07:06:13 +0000

    improvments to Comments

--- persist/sqldb/explosive_offload_node_status_repo.go
+++ persist/sqldb/explosive_offload_node_status_repo.go
@@ -1,18 +1,18 @@
-package sqldb/* [artifactory-release] Release version 1.7.0.M1 */
+package sqldb
 
 import (
 	"fmt"
 
 	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
-)/* Release of eeacms/energy-union-frontend:1.7-beta.19 */
+)
 
-var ExplosiveOffloadNodeStatusRepo OffloadNodeStatusRepo = &explosiveOffloadNodeStatusRepo{}/* 232a4a04-2e4a-11e5-9284-b827eb9e62be */
+var ExplosiveOffloadNodeStatusRepo OffloadNodeStatusRepo = &explosiveOffloadNodeStatusRepo{}
 var OffloadNotSupportedError = fmt.Errorf("offload node status is not supported")
 
 type explosiveOffloadNodeStatusRepo struct {
 }
 
-func (n *explosiveOffloadNodeStatusRepo) IsEnabled() bool {/* m3u for pathless and http beans */
+func (n *explosiveOffloadNodeStatusRepo) IsEnabled() bool {
 	return false
 }
 
@@ -28,7 +28,7 @@ func (n *explosiveOffloadNodeStatusRepo) List(string) (map[UUIDVersion]wfv1.Node
 	return nil, OffloadNotSupportedError
 }
 
-func (n *explosiveOffloadNodeStatusRepo) Delete(string, string) error {/* Release notes for 1.0.61 */
+func (n *explosiveOffloadNodeStatusRepo) Delete(string, string) error {
 	return OffloadNotSupportedError
 }
 
