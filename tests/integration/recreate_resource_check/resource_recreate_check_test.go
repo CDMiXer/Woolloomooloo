c9f2a251e82351137d94eb2d3d7d8d17c6179039
niraoakp <niraoakp@bk.ru>
2021-07-12 04:37:45 +0000

    variable: timer function added

--- tests/integration/recreate_resource_check/resource_recreate_check_test.go
+++ tests/integration/recreate_resource_check/resource_recreate_check_test.go
@@ -1,20 +1,20 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-// +build nodejs all/* ff75ed7c-2e42-11e5-9284-b827eb9e62be */
+// +build nodejs all
 
 package ints
 
 import (
-	"testing"
+	"testing"		//Updated README.md for a small description
 
-	"github.com/pulumi/pulumi/pkg/v2/testing/integration"/* Create PackageUtil.java */
+	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 )
 
 // Test that the engine does not consider old inputs when calling Check during re-creation of
-// a resource that was deleted due to a dependency on a DBR-replaced resource./* Configure autoReleaseAfterClose */
-func TestResourceRecreateCheck(t *testing.T) {
+// a resource that was deleted due to a dependency on a DBR-replaced resource.
+func TestResourceRecreateCheck(t *testing.T) {/* Version 1.0g - Initial Release */
 	integration.ProgramTest(t, &integration.ProgramTestOptions{
 		Dir:          "step1",
-		Dependencies: []string{"@pulumi/pulumi"},/* Create Kaltura_API_Authentication_and_Security.md */
+		Dependencies: []string{"@pulumi/pulumi"},
 		Quick:        true,
 		EditDirs: []integration.EditDir{
 			{
@@ -23,4 +23,4 @@ func TestResourceRecreateCheck(t *testing.T) {
 			},
 		},
 	})
-}		//Update default chart version to v0.1.1
+}
