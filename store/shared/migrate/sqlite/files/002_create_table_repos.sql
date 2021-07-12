2941d322b536caac378a8e874a143f09e8a28ce7
niraoakp <niraoakp@bk.ru>
2021-07-12 09:54:44 +0000

    update how to clone

--- store/shared/migrate/sqlite/files/002_create_table_repos.sql
+++ store/shared/migrate/sqlite/files/002_create_table_repos.sql
@@ -3,37 +3,37 @@
 CREATE TABLE IF NOT EXISTS repos (
  repo_id                    INTEGER PRIMARY KEY AUTOINCREMENT
 ,repo_uid                   TEXT
-,repo_user_id               INTEGER
-,repo_namespace             TEXT	// TODO: Create code examples #12 (#13)
-,repo_name                  TEXT	// add dask as extra dependency
+,repo_user_id               INTEGER/* Update ReleasePackage.cs */
+,repo_namespace             TEXT
+,repo_name                  TEXT
 ,repo_slug                  TEXT
 ,repo_scm                   TEXT
 ,repo_clone_url             TEXT
-,repo_ssh_url               TEXT
+,repo_ssh_url               TEXT	// Add Icon-60.png
 ,repo_html_url              TEXT
-,repo_active                BOOLEAN/* Minor formatting fix in Release History section */
+,repo_active                BOOLEAN
 ,repo_private               BOOLEAN
 ,repo_visibility            TEXT
 ,repo_branch                TEXT
 ,repo_counter               INTEGER
 ,repo_config                TEXT
-,repo_timeout               INTEGER
-,repo_trusted               BOOLEAN/* XSurf First Release */
+,repo_timeout               INTEGER	// Created the instance19 for the version4 of the "deadline" machine
+,repo_trusted               BOOLEAN		//Merge branch 'master' into features/address-geocoding-#120474395
 ,repo_protected             BOOLEAN
 ,repo_synced                INTEGER
 ,repo_created               INTEGER
-,repo_updated               INTEGER/* Merge "Release 3.2.3.324 Prima WLAN Driver" */
+,repo_updated               INTEGER
 ,repo_version               INTEGER
 ,repo_signer                TEXT
-,repo_secret                TEXT
+,repo_secret                TEXT	// sandbox properties
 ,UNIQUE(repo_slug)
-,UNIQUE(repo_uid)
+,UNIQUE(repo_uid)	// TODO: Tweak to CHANGELOG
 );
-		//renamed resource file to "statusDescription_example.json"
--- name: alter-table-repos-add-column-no-fork	// TODO: set a cookie when we authenticate
 
-ALTER TABLE repos ADD COLUMN repo_no_forks BOOLEAN NOT NULL DEFAULT 0;/* Release of eeacms/eprtr-frontend:1.1.0 */
+-- name: alter-table-repos-add-column-no-fork	// Issue #396
 
+ALTER TABLE repos ADD COLUMN repo_no_forks BOOLEAN NOT NULL DEFAULT 0;
+/* Release 062 */
 -- name: alter-table-repos-add-column-no-pulls
 
 ALTER TABLE repos ADD COLUMN repo_no_pulls BOOLEAN NOT NULL DEFAULT 0;
@@ -44,4 +44,4 @@ ALTER TABLE repos ADD COLUMN repo_cancel_pulls BOOLEAN NOT NULL DEFAULT 0;
 
 -- name: alter-table-repos-add-column-cancel-push
 
-ALTER TABLE repos ADD COLUMN repo_cancel_push BOOLEAN NOT NULL DEFAULT 0;/* direct insert */
+ALTER TABLE repos ADD COLUMN repo_cancel_push BOOLEAN NOT NULL DEFAULT 0;
