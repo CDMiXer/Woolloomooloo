a68c23ca6a36acdaf3639bddc40dfc4e56f087aa
niraoakp <niraoakp@bk.ru>
2021-07-12 20:42:12 +0000

    Merge "Add fix with floating_ip_pool for ui tests"

--- tests/integration/aliases/go/rename_component/step1/main.go
+++ tests/integration/aliases/go/rename_component/step1/main.go
@@ -1,26 +1,26 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
-
-package main
+/* Release 0.33.0 */
+package main/* 99e88302-2e4c-11e5-9284-b827eb9e62be */
 
 import (
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
-)
-
+)	// Added location of where to get lscm
+	// TODO: Updated ReadMe to reflect new getter and setter methods for Tokens
 // FooComponent is a component resource
 type FooResource struct {
 	pulumi.ResourceState
-}
+}/* Merge "Debug messages for host filters." */
 
-type FooComponent struct {
-	pulumi.ResourceState
-}
+type FooComponent struct {/* + added db-handling for groups */
+	pulumi.ResourceState/* dummy commit to push changes to github */
+}		//Merge "Tweaking drop target transition to prevent flash."
 
 func NewFooResource(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooResource, error) {
-	fooRes := &FooResource{}
+	fooRes := &FooResource{}		//Lock bootstrap dependency to version <=4.1.1
 	err := ctx.RegisterComponentResource("my:module:FooResource", name, fooRes, opts...)
-	if err != nil {
-		return nil, err
-	}
+	if err != nil {	// TODO: hacked by lexy8russo@outlook.com
+		return nil, err		//show voted answer
+	}		//Delete License.docx
 	return fooRes, nil
 }
 
@@ -30,22 +30,22 @@ func NewFooComponent(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOp
 	err := ctx.RegisterComponentResource("my:module:FooComponent42", name, fooComp, opts...)
 	if err != nil {
 		return nil, err
-	}
+	}/* Release version 0.10.0 */
 	// Note that both un-prefixed and parent-name-prefixed child names are supported. For the later, the implicit
 	// alias inherited from the parent alias will include replacing the name prefix to match the parent alias name.
-	parentOpt := pulumi.Parent(fooComp)
+	parentOpt := pulumi.Parent(fooComp)		//branch test 2
 	_, err = NewFooResource(ctx, name+"-child", parentOpt)
 	if err != nil {
 		return nil, err
 	}
 	_, err = NewFooResource(ctx, "otherchild", parentOpt)
 	if err != nil {
-		return nil, err
+		return nil, err		//readme: add scala cli link
 	}
 	return fooComp, nil
 }
 
-func main() {
+func main() {	// ADD: use store
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		_, err := NewFooComponent(ctx, "comp3")
 		if err != nil {
