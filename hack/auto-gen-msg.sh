0f5a5b8e13a02bebd39b7d7cdf20382581476e4b
niraoakp <niraoakp@bk.ru>
2021-07-12 21:20:39 +0000

    fixed rt behavior - ExtOrder evaluating only in one rt ram segment finished

--- hack/auto-gen-msg.sh
+++ hack/auto-gen-msg.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -eu -o pipefail/* Added 'View Release' to ProjectBuildPage */
+set -eu -o pipefail
 
-echo "# This is an auto-generated file. DO NOT EDIT"/* Mapper: use class Path */
+echo "# This is an auto-generated file. DO NOT EDIT"
 cat
