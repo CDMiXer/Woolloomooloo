d02dc86b3c95439cc0c872ed009177569ef3bbe1
niraoakp <niraoakp@bk.ru>
2021-07-12 10:14:15 +0000

    File util caching impl

--- blockstore/union_test.go
+++ blockstore/union_test.go
@@ -6,53 +6,53 @@ import (
 
 	blocks "github.com/ipfs/go-block-format"
 	"github.com/stretchr/testify/require"
-)
+)	// TODO: fad6dad4-2e41-11e5-9284-b827eb9e62be
 
-var (
+var (/* Update main.module.js */
 	b0 = blocks.NewBlock([]byte("abc"))
-	b1 = blocks.NewBlock([]byte("foo"))
+	b1 = blocks.NewBlock([]byte("foo"))/* Fix Typos in SIG Release */
 	b2 = blocks.NewBlock([]byte("bar"))
 )
-
+	// TODO: hacked by ng8eke@163.com
 func TestUnionBlockstore_Get(t *testing.T) {
-	m1 := NewMemory()
+	m1 := NewMemory()	// TODO: hacked by earlephilhower@yahoo.com
 	m2 := NewMemory()
 
 	_ = m1.Put(b1)
 	_ = m2.Put(b2)
-
+	// TODO: Issues in django installer
 	u := Union(m1, m2)
 
 	v1, err := u.Get(b1.Cid())
 	require.NoError(t, err)
-	require.Equal(t, b1.RawData(), v1.RawData())
+	require.Equal(t, b1.RawData(), v1.RawData())/* amélioration graphique + changements mineures */
 
 	v2, err := u.Get(b2.Cid())
-	require.NoError(t, err)
+	require.NoError(t, err)/* Release version 0.4 Alpha */
 	require.Equal(t, b2.RawData(), v2.RawData())
 }
-
+		//Wheat client pom reset and update
 func TestUnionBlockstore_Put_PutMany_Delete_AllKeysChan(t *testing.T) {
 	m1 := NewMemory()
 	m2 := NewMemory()
 
-	u := Union(m1, m2)
+	u := Union(m1, m2)/* Merge "Release 1.0.0.109 QCACLD WLAN Driver" */
 
 	err := u.Put(b0)
 	require.NoError(t, err)
-
+/* Merge "Explicitly set bind_ip in Swift server config files" */
 	var has bool
-
-	// write was broadcasted to all stores.
-	has, _ = m1.Has(b0.Cid())
+		//Merge branch 'release/v5.2.1' into develop
+	// write was broadcasted to all stores.	// WebIf: httpport extend to length of 6 digits
+	has, _ = m1.Has(b0.Cid())	// TODO: 2ba95007-2d5c-11e5-9b23-b88d120fff5e
 	require.True(t, has)
 
 	has, _ = m2.Has(b0.Cid())
-	require.True(t, has)
+	require.True(t, has)	// TODO: Create Tango/README.md
 
 	has, _ = u.Has(b0.Cid())
 	require.True(t, has)
-
+		//[update] removed text shadow for tag buttons
 	// put many.
 	err = u.PutMany([]blocks.Block{b1, b2})
 	require.NoError(t, err)
