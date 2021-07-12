e9c625a8513dec6495b1c7cd76d9fdd29dd9b9b3
niraoakp <niraoakp@bk.ru>
2021-07-12 14:50:25 +0000

    Add the deleteChannelPermission function

--- handler/api/secrets/find_test.go
+++ handler/api/secrets/find_test.go
@@ -1,21 +1,21 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License		//Merge "Send added user serial numbers to vold." into mnc-dev
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
 
 package secrets
 
-import (		//convert ar7-net to KernelPackage template
+import (
 	"context"
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
-	"testing"/* Release Ver. 1.5.8 */
-		//add link for back button in edit user view
-	"github.com/drone/drone/core"/* Rename AutoReleasePool to MemoryPool */
+	"testing"
+
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/errors"
-	"github.com/drone/drone/mock"/* Making 'ant clean-all' work better by calling 'make distclean' for cvc3 */
+	"github.com/drone/drone/mock"
 
 	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
@@ -25,38 +25,9 @@ import (		//convert ar7-net to KernelPackage template
 func TestHandleFind(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-/* Update Asking_Online.md */
-	secrets := mock.NewMockGlobalSecretStore(controller)		//add stellenbosch images
-	secrets.EXPECT().FindName(gomock.Any(), dummySecret.Namespace, dummySecret.Name).Return(dummySecret, nil)
 
-	c := new(chi.Context)	// TODO: will be fixed by onhardev@bk.ru
-	c.URLParams.Add("namespace", "octocat")
-	c.URLParams.Add("name", "github_password")
-
-	w := httptest.NewRecorder()/* Change to fake INI handler for Mono. Breaks old INIs. */
-	r := httptest.NewRequest("GET", "/", nil)
-	r = r.WithContext(
-		context.WithValue(context.Background(), chi.RouteCtxKey, c),
-	)		//Delete bs.zip
-
-	HandleFind(secrets).ServeHTTP(w, r)
-	if got, want := w.Code, http.StatusOK; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)
-	}
-
-	got, want := &core.Secret{}, dummySecretScrubbed
-	json.NewDecoder(w.Body).Decode(got)
-	if diff := cmp.Diff(got, want); len(diff) != 0 {
-		t.Errorf(diff)	// TODO: Add converters for remaining classes in java.time.
-	}
-}
-
-func TestHandleFind_SecretNotFound(t *testing.T) {
-	controller := gomock.NewController(t)
-	defer controller.Finish()
-	// support java 9 Generated annotation
 	secrets := mock.NewMockGlobalSecretStore(controller)
-	secrets.EXPECT().FindName(gomock.Any(), dummySecret.Namespace, dummySecret.Name).Return(nil, errors.ErrNotFound)		//Show load state from the beginning
+	secrets.EXPECT().FindName(gomock.Any(), dummySecret.Namespace, dummySecret.Name).Return(dummySecret, nil)
 
 	c := new(chi.Context)
 	c.URLParams.Add("namespace", "octocat")
@@ -67,9 +38,38 @@ func TestHandleFind_SecretNotFound(t *testing.T) {
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
-	// TODO: Release for 1.37.0
+
 	HandleFind(secrets).ServeHTTP(w, r)
-	if got, want := w.Code, http.StatusNotFound; want != got {	// TODO: Set-Typ von types in MaterialsAndAmountQuest auf EnumSet geändert.
+	if got, want := w.Code, http.StatusOK; want != got {
+		t.Errorf("Want response code %d, got %d", want, got)
+	}
+
+	got, want := &core.Secret{}, dummySecretScrubbed
+	json.NewDecoder(w.Body).Decode(got)
+	if diff := cmp.Diff(got, want); len(diff) != 0 {
+		t.Errorf(diff)
+	}
+}
+
+func TestHandleFind_SecretNotFound(t *testing.T) {
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+
+	secrets := mock.NewMockGlobalSecretStore(controller)
+	secrets.EXPECT().FindName(gomock.Any(), dummySecret.Namespace, dummySecret.Name).Return(nil, errors.ErrNotFound)
+
+	c := new(chi.Context)
+	c.URLParams.Add("namespace", "octocat")
+	c.URLParams.Add("name", "github_password")
+
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("GET", "/", nil)
+	r = r.WithContext(
+		context.WithValue(context.Background(), chi.RouteCtxKey, c),
+	)
+
+	HandleFind(secrets).ServeHTTP(w, r)
+	if got, want := w.Code, http.StatusNotFound; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
