ed7958811b43ec44d9c42889b7b6b26ae75212b4
niraoakp <niraoakp@bk.ru>
2021-07-12 10:42:34 +0000

    Since length in the bukkit code is no longer supported. Replace it with size()

--- pkg/backend/httpstate/console.go
+++ pkg/backend/httpstate/console.go
@@ -1,37 +1,37 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* 0.20.7: Maintenance Release (close #86) */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-//	// TODO: will be fixed by bokky.poobah@bokconsulting.com.au
-// Unless required by applicable law or agreed to in writing, software		//Linux/Max: Ensure pip and wheel are installed and the latest version
-// distributed under the License is distributed on an "AS IS" BASIS,		//Win32 - Prevent loading Ram Watch and Ram Search dialog if no rom is loaded.
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: Execute commands while modules instalation and uninstalation.
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package httpstate
 
-import (/* 919c88c8-2e70-11e5-9284-b827eb9e62be */
-	"net/url"/* -added tzm-sched */
-	"os"	// TODO: 97f81ed8-2e68-11e5-9284-b827eb9e62be
+import (
+	"net/url"
+	"os"
 	"path"
 	"strings"
 )
 
-const (/* Add Objective-C Version */
-	// ConsoleDomainEnvVar overrides the way we infer the domain we assume the Pulumi Console will/*  - added simple solution for issue 4 (see: NaoSensorModule.py) */
+const (
+	// ConsoleDomainEnvVar overrides the way we infer the domain we assume the Pulumi Console will
 	// be served from, and instead just use this value. e.g. so links to the stack update go to
 	// https://pulumi.example.com/org/project/stack/updates/2 instead.
-	ConsoleDomainEnvVar = "PULUMI_CONSOLE_DOMAIN"	// Merge branch 'master' into metric-name-no-forms
-	// TODO: will be fixed by magik6k@gmail.com
-	// PulumiCloudURL is the Cloud URL used if no environment or explicit cloud is chosen./* Templates should use the .pptx extension */
-	PulumiCloudURL = "https://" + defaultAPIDomainPrefix + "pulumi.com"		//rev 469330
+	ConsoleDomainEnvVar = "PULUMI_CONSOLE_DOMAIN"
+
+	// PulumiCloudURL is the Cloud URL used if no environment or explicit cloud is chosen.
+	PulumiCloudURL = "https://" + defaultAPIDomainPrefix + "pulumi.com"
 
 	// defaultAPIDomainPrefix is the assumed Cloud URL prefix for typical Pulumi Cloud API endpoints.
-	defaultAPIDomainPrefix = "api."		//Add info links to public body pages
+	defaultAPIDomainPrefix = "api."
 	// defaultConsoleDomainPrefix is the assumed Cloud URL prefix typically used for the Pulumi Console.
 	defaultConsoleDomainPrefix = "app."
 )
@@ -40,7 +40,7 @@ const (/* Add Objective-C Version */
 // an error, returns "".
 func cloudConsoleURL(cloudURL string, paths ...string) string {
 	u, err := url.Parse(cloudURL)
-	if err != nil {/* make __init__.py empty */
+	if err != nil {
 		return ""
 	}
 
