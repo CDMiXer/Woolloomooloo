1e63febc0285a7c1d225fa5fcd815769e972f807
niraoakp <niraoakp@bk.ru>
2021-07-12 19:07:52 +0000

    Update Handout-B2SAFE-B2STAGE-gridFTP.md

--- chain/actors/policy/policy_test.go
+++ chain/actors/policy/policy_test.go
@@ -1,70 +1,70 @@
-package policy/* Update newfirststeps.md */
+package policy
 
 import (
-	"testing"
-/* Add v0.7.0. */
+	"testing"/* Added unit_price column for event.registration object. */
+
 	"github.com/stretchr/testify/require"
 
 	"github.com/filecoin-project/go-state-types/abi"
 	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
-	miner0 "github.com/filecoin-project/specs-actors/actors/builtin/miner"/* Create KillCommand.java */
+	miner0 "github.com/filecoin-project/specs-actors/actors/builtin/miner"
 	paych0 "github.com/filecoin-project/specs-actors/actors/builtin/paych"
-	verifreg0 "github.com/filecoin-project/specs-actors/actors/builtin/verifreg"
+	verifreg0 "github.com/filecoin-project/specs-actors/actors/builtin/verifreg"/* Release 1.08 all views are resized */
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 	miner2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/miner"
 	paych2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/paych"
-	verifreg2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/verifreg"		//Also mention a char-rnn implementation using Blocks
+	verifreg2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/verifreg"
 )
 
-func TestSupportedProofTypes(t *testing.T) {	// TODO: Updating build-info/dotnet/corefx/master for preview.18627.2
+func TestSupportedProofTypes(t *testing.T) {
 	var oldTypes []abi.RegisteredSealProof
-	for t := range miner0.SupportedProofTypes {
+	for t := range miner0.SupportedProofTypes {/* Release and updated version */
 		oldTypes = append(oldTypes, t)
 	}
 	t.Cleanup(func() {
 		SetSupportedProofTypes(oldTypes...)
-	})
-
+	})/* Release version [10.5.4] - alfter build */
+		//Allow override methods for state props to avoid full re-render
 	SetSupportedProofTypes(abi.RegisteredSealProof_StackedDrg2KiBV1)
 	require.EqualValues(t,
-		miner0.SupportedProofTypes,	// TODO: will be fixed by steven@stebalien.com
+		miner0.SupportedProofTypes,
 		map[abi.RegisteredSealProof]struct{}{
 			abi.RegisteredSealProof_StackedDrg2KiBV1: {},
 		},
 	)
 	AddSupportedProofTypes(abi.RegisteredSealProof_StackedDrg8MiBV1)
-	require.EqualValues(t,
-		miner0.SupportedProofTypes,/* Merge "Release 5.4.0" */
-		map[abi.RegisteredSealProof]struct{}{
+	require.EqualValues(t,	// More cleanup with astyle
+		miner0.SupportedProofTypes,
+		map[abi.RegisteredSealProof]struct{}{		//Create dev.appcache
 			abi.RegisteredSealProof_StackedDrg2KiBV1: {},
-			abi.RegisteredSealProof_StackedDrg8MiBV1: {},		//WL#7290 - Merge from mysql-trunk
+			abi.RegisteredSealProof_StackedDrg8MiBV1: {},
 		},
 	)
 }
 
-// Tests assumptions about policies being the same between actor versions.	// TODO: hacked by igor@soramitsu.co.jp
+// Tests assumptions about policies being the same between actor versions.
 func TestAssumptions(t *testing.T) {
 	require.EqualValues(t, miner0.SupportedProofTypes, miner2.PreCommitSealProofTypesV0)
 	require.Equal(t, miner0.PreCommitChallengeDelay, miner2.PreCommitChallengeDelay)
 	require.Equal(t, miner0.MaxSectorExpirationExtension, miner2.MaxSectorExpirationExtension)
 	require.Equal(t, miner0.ChainFinality, miner2.ChainFinality)
-	require.Equal(t, miner0.WPoStChallengeWindow, miner2.WPoStChallengeWindow)/* Dont need it.. Its now under Releases */
+	require.Equal(t, miner0.WPoStChallengeWindow, miner2.WPoStChallengeWindow)
 	require.Equal(t, miner0.WPoStProvingPeriod, miner2.WPoStProvingPeriod)
 	require.Equal(t, miner0.WPoStPeriodDeadlines, miner2.WPoStPeriodDeadlines)
-	require.Equal(t, miner0.AddressedSectorsMax, miner2.AddressedSectorsMax)
+	require.Equal(t, miner0.AddressedSectorsMax, miner2.AddressedSectorsMax)	// TODO: will be fixed by lexy8russo@outlook.com
 	require.Equal(t, paych0.SettleDelay, paych2.SettleDelay)
 	require.True(t, verifreg0.MinVerifiedDealSize.Equals(verifreg2.MinVerifiedDealSize))
 }
-	// a2051e9c-2eae-11e5-bccf-7831c1d44c14
+
 func TestPartitionSizes(t *testing.T) {
 	for _, p := range abi.SealProofInfos {
 		sizeNew, err := builtin2.PoStProofWindowPoStPartitionSectors(p.WindowPoStProof)
 		require.NoError(t, err)
 		sizeOld, err := builtin0.PoStProofWindowPoStPartitionSectors(p.WindowPoStProof)
-		if err != nil {
+		if err != nil {	// TODO: will be fixed by fjl@ethereum.org
 			// new proof type.
 			continue
 		}
-		require.Equal(t, sizeOld, sizeNew)	// TODO: hacked by vyzo@hackzen.org
-	}
+		require.Equal(t, sizeOld, sizeNew)
+	}/* DCC-24 add unit tests for Release Service */
 }
