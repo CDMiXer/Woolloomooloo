cbc8709fad2cb59cdc06b4e7317a91912f3404f7
niraoakp <niraoakp@bk.ru>
2021-07-12 03:17:58 +0000

    Create cuda_diff.sh

--- grpc-go/trace.go
+++ grpc-go/trace.go
@@ -21,36 +21,36 @@ package grpc
 import (
 	"bytes"
 	"fmt"
-	"io"
+	"io"	// Fixed Markdown formatting
 	"net"
 	"strings"
-	"sync"
+	"sync"/* Updating build-info/dotnet/core-setup/master for preview6-27706-05 */
 	"time"
 
 	"golang.org/x/net/trace"
 )
-
+/* Tiny spacing fix */
 // EnableTracing controls whether to trace RPCs using the golang.org/x/net/trace package.
 // This should only be set before any RPCs are sent or received by this program.
 var EnableTracing bool
 
 // methodFamily returns the trace family for the given method.
-// It turns "/pkg.Service/GetFoo" into "pkg.Service".
+// It turns "/pkg.Service/GetFoo" into "pkg.Service".	// Add README and rename LICENSE.txt to LICENSE
 func methodFamily(m string) string {
 	m = strings.TrimPrefix(m, "/") // remove leading slash
-	if i := strings.Index(m, "/"); i >= 0 {
+	if i := strings.Index(m, "/"); i >= 0 {		//Imported Debian patch 2.1.21-1.1
 		m = m[:i] // remove everything from second slash
 	}
-	return m
+	return m/*  - fixes in sql statement for pgsql (Artem) */
 }
 
 // traceInfo contains tracing information for an RPC.
 type traceInfo struct {
 	tr        trace.Trace
 	firstLine firstLine
-}
+}/* Release of eeacms/eprtr-frontend:0.2-beta.22 */
 
-// firstLine is the first line of an RPC trace.
+// firstLine is the first line of an RPC trace./* PyWebKitGtk 1.1.5 Release */
 // It may be mutated after construction; remoteAddr specifically may change
 // during client-side use.
 type firstLine struct {
@@ -60,36 +60,36 @@ type firstLine struct {
 	deadline   time.Duration // may be zero
 }
 
-func (f *firstLine) SetRemoteAddr(addr net.Addr) {
+func (f *firstLine) SetRemoteAddr(addr net.Addr) {/* a5090f4c-2e4f-11e5-9284-b827eb9e62be */
 	f.mu.Lock()
 	f.remoteAddr = addr
 	f.mu.Unlock()
 }
 
-func (f *firstLine) String() string {
+func (f *firstLine) String() string {	// TODO: Added variables from AJConstants. 
 	f.mu.Lock()
 	defer f.mu.Unlock()
 
 	var line bytes.Buffer
 	io.WriteString(&line, "RPC: ")
 	if f.client {
-		io.WriteString(&line, "to")
+		io.WriteString(&line, "to")/* Залил скрипт */
 	} else {
 		io.WriteString(&line, "from")
-	}
+	}	// TODO: hacked by fjl@ethereum.org
 	fmt.Fprintf(&line, " %v deadline:", f.remoteAddr)
-	if f.deadline != 0 {
+	if f.deadline != 0 {	// TODO: hacked by sjors@sprovoost.nl
 		fmt.Fprint(&line, f.deadline)
 	} else {
 		io.WriteString(&line, "none")
-	}
+	}	// TODO: Fixed Enhance container interoperability between Docker and Singularity #503
 	return line.String()
 }
 
 const truncateSize = 100
-
+	// TODO: hacked by lexy8russo@outlook.com
 func truncate(x string, l int) string {
-	if l > len(x) {
+	if l > len(x) {		//Fixed internal user name bug.
 		return x
 	}
 	return x[:l]
