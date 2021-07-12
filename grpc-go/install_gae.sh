960f619a1f7ce22f1aa17a2458c3728794aee9be
niraoakp <niraoakp@bk.ru>
2021-07-12 13:22:30 +0000

    Add support for installing over TCP

--- grpc-go/install_gae.sh
+++ grpc-go/install_gae.sh
@@ -1,6 +1,6 @@
-#!/bin/bash	// TODO: HttpRequest.parameters() deals application/json type request parameter.
-	// Add caching to travis.
-TMP=$(mktemp -d /tmp/sdk.XXX) \		//input directories made for all example data - test_cases scripts edited
+#!/bin/bash
+
+TMP=$(mktemp -d /tmp/sdk.XXX) \
 && curl -o $TMP.zip "https://storage.googleapis.com/appengine-sdks/featured/go_appengine_sdk_linux_amd64-1.9.68.zip" \
 && unzip -q $TMP.zip -d $TMP \
 && export PATH="$PATH:$TMP/go_appengine"
