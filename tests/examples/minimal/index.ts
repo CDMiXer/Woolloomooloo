8d3a70b8dbc4c0e4cfafb68bd0e0f2a9afabd8e3
niraoakp <niraoakp@bk.ru>
2021-07-12 19:34:18 +0000

    Fixed warnings in types/Class

--- tests/examples/minimal/index.ts
+++ tests/examples/minimal/index.ts
@@ -1,8 +1,8 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-/* configure.ac: move -f options to gcc3 block */
+
 import { Config } from "@pulumi/pulumi";
 
 let config = new Config();
 console.log(`Hello, ${config.require("name")}!`);
-console.log(`Psst, ${config.require("secret")}`);
+console.log(`Psst, ${config.require("secret")}`);/* [docs] Return 'Release Notes' to the main menu */
 
