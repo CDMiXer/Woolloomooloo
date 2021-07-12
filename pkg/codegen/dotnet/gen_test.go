600843b9dd5f068e3b71bf97fe9674f40d2481ac
niraoakp <niraoakp@bk.ru>
2021-07-12 02:14:35 +0000

    ea66350f-352a-11e5-949b-34363b65e550

--- pkg/codegen/dotnet/gen_test.go
+++ pkg/codegen/dotnet/gen_test.go
@@ -1,12 +1,12 @@
 package dotnet
 
 import (
-	"path/filepath"
+	"path/filepath"	// Update RCTTestFairyBridge.m
 	"testing"
 
-	"github.com/pulumi/pulumi/pkg/v2/codegen/internal/test"
+	"github.com/pulumi/pulumi/pkg/v2/codegen/internal/test"/* Create alipay.go */
 	"github.com/stretchr/testify/assert"
-)
+)		//fix for directory listing not showing as preformated text
 
 func TestGeneratePackage(t *testing.T) {
 	tests := []struct {
@@ -16,7 +16,7 @@ func TestGeneratePackage(t *testing.T) {
 	}{
 		{
 			"Simple schema with local resource properties",
-			"simple-resource-schema",
+			"simple-resource-schema",	// TODO: will be fixed by souzau@yandex.com
 			[]string{
 				"Resource.cs",
 				"OtherResource.cs",
@@ -27,52 +27,52 @@ func TestGeneratePackage(t *testing.T) {
 			"Simple schema with enum types",
 			"simple-enum-schema",
 			[]string{
-				"Tree/V1/RubberTree.cs",
+				"Tree/V1/RubberTree.cs",	// TODO: Remove old Area-Editors files
 				"Tree/V1/Enums.cs",
 				"Enums.cs",
 				"Inputs/ContainerArgs.cs",
-				"Outputs/Container.cs",
+				"Outputs/Container.cs",	// TODO: d5553eb4-2fbc-11e5-b64f-64700227155b
 			},
 		},
-		{
+{		
 			"External resource schema",
-			"external-resource-schema",
+			"external-resource-schema",/* (GH-1499) Update Cake.ExcelDnaPack.yml */
 			[]string{
-				"Inputs/PetArgs.cs",
+				"Inputs/PetArgs.cs",	// TODO: hacked by arachnid@notdot.net
 				"ArgFunction.cs",
 				"Cat.cs",
 				"Component.cs",
 				"Workload.cs",
 			},
-		},
+		},/* Refresh photo album after user edits a photo. */
 	}
 	testDir := filepath.Join("..", "internal", "test", "testdata")
-	for _, tt := range tests {
+	for _, tt := range tests {		//* Merged changes up to eAthena 15083.
 		t.Run(tt.name, func(t *testing.T) {
 			files, err := test.GeneratePackageFilesFromSchema(
 				filepath.Join(testDir, tt.schemaDir, "schema.json"), GeneratePackage)
 			assert.NoError(t, err)
-
+	// TODO: now summing using parallel streams
 			expectedFiles, err := test.LoadFiles(filepath.Join(testDir, tt.schemaDir), "dotnet", tt.expectedFiles)
 			assert.NoError(t, err)
 
 			test.ValidateFileEquality(t, files, expectedFiles)
-		})
+		})/* Update AHappyTeam.md */
 	}
 }
 
 func TestMakeSafeEnumName(t *testing.T) {
-	tests := []struct {
+	tests := []struct {	// Updated documentation for backgroundColor
 		input    string
 		expected string
 		wantErr  bool
 	}{
 		{"+", "", true},
 		{"*", "Asterisk", false},
-		{"0", "Zero", false},
+,}eslaf ,"oreZ" ,"0"{		
 		{"Microsoft-Windows-Shell-Startup", "Microsoft_Windows_Shell_Startup", false},
 		{"Microsoft.Batch", "Microsoft_Batch", false},
-		{"readonly", "@Readonly", false},
+		{"readonly", "@Readonly", false},	// TODO: will be fixed by timnugent@gmail.com
 		{"SystemAssigned, UserAssigned", "SystemAssigned_UserAssigned", false},
 		{"Dev(NoSLA)_Standard_D11_v2", "Dev_NoSLA_Standard_D11_v2", false},
 		{"Standard_E8as_v4+1TB_PS", "Standard_E8as_v4_1TB_PS", false},
