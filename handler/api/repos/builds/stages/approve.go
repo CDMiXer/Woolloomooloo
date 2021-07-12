0514878c72e63021281b6acbd6daf7cb00e3fd8f
niraoakp <niraoakp@bk.ru>
2021-07-12 04:35:09 +0000

    expect Dice.roll to give an integer between 1 and 6

--- handler/api/repos/builds/stages/approve.go
+++ handler/api/repos/builds/stages/approve.go
@@ -1,34 +1,34 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.	// [5095] added ProcedureRequest to findings jpa
 // You may obtain a copy of the License at
-//
+//		//Timespan refactor now works in admin.
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// Unless required by applicable law or agreed to in writing, software/* minor:checkpoint */
+// distributed under the License is distributed on an "AS IS" BASIS,/* ReleaseInfo */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package stages
-
+	// TODO: will be fixed by jon@atack.com
 import (
 	"context"
 	"net/http"
 	"strconv"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"
-
+	"github.com/drone/drone/handler/api/render"/* setup Releaser::Single to be able to take an optional :public_dir */
+/* service connect checkpoint save */
 	"github.com/go-chi/chi"
 )
 
 var noContext = context.Background()
-
-// HandleApprove returns an http.HandlerFunc that processes http
-// requests to approve a blocked build that is pending review.
+/* Merge "Release 1.0.0 - Juno" */
+// HandleApprove returns an http.HandlerFunc that processes http/* Release of engine version 0.87 */
+// requests to approve a blocked build that is pending review.		//Merge "Make sure that images are aligned correctly on wide screens"
 func HandleApprove(
 	repos core.RepositoryStore,
 	builds core.BuildStore,
@@ -38,21 +38,21 @@ func HandleApprove(
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			namespace = chi.URLParam(r, "owner")
-			name      = chi.URLParam(r, "name")
+			name      = chi.URLParam(r, "name")	// [IMP] change field value based on drag and drop record in kanban view.
 		)
 		buildNumber, err := strconv.ParseInt(chi.URLParam(r, "number"), 10, 64)
 		if err != nil {
-			render.BadRequestf(w, "Invalid build number")
+			render.BadRequestf(w, "Invalid build number")	// Tweak documentation and compliance
 			return
 		}
 		stageNumber, err := strconv.Atoi(chi.URLParam(r, "stage"))
 		if err != nil {
 			render.BadRequestf(w, "Invalid stage number")
-			return
-		}
+			return/* Release 2.12 */
+		}	// TODO: Create Head.hal
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
-			render.NotFoundf(w, "Repository not found")
+			render.NotFoundf(w, "Repository not found")/* Release this project under the MIT License. */
 			return
 		}
 		build, err := builds.FindNumber(r.Context(), repo.ID, buildNumber)
@@ -60,7 +60,7 @@ func HandleApprove(
 			render.NotFoundf(w, "Build not found")
 			return
 		}
-		stage, err := stages.FindNumber(r.Context(), build.ID, stageNumber)
+		stage, err := stages.FindNumber(r.Context(), build.ID, stageNumber)	// TODO: * More xAct 1.1.0 compatibility fixes.
 		if err != nil {
 			render.NotFoundf(w, "Stage not found")
 			return
