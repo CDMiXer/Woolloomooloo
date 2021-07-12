979f5afd8b28dd8bab2a2607589b9255ed3a0e8e
niraoakp <niraoakp@bk.ru>
2021-07-12 15:45:34 +0000

    New translations p02_ch07_01_more_challenging_tests_of_upb.md (Spanish, Chile)

--- handler/api/repos/crons/none.go
+++ handler/api/repos/crons/none.go
@@ -2,11 +2,11 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at	// TODO: will be fixed by arajasek94@gmail.com
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software/* Release 0.8.3 Alpha */
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
@@ -15,14 +15,14 @@
 // +build oss
 
 package crons
-
+		//1d9979bc-2e6c-11e5-9284-b827eb9e62be
 import (
 	"net/http"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 )
-
+/* Merge "Release 1.0.0.156 QCACLD WLAN Driver" */
 var notImplemented = func(w http.ResponseWriter, r *http.Request) {
 	render.NotImplemented(w, render.ErrNotImplemented)
 }
@@ -38,13 +38,13 @@ func HandleUpdate(core.RepositoryStore, core.CronStore) http.HandlerFunc {
 func HandleDelete(core.RepositoryStore, core.CronStore) http.HandlerFunc {
 	return notImplemented
 }
-
+		//01666428-2e4c-11e5-9284-b827eb9e62be
 func HandleFind(core.RepositoryStore, core.CronStore) http.HandlerFunc {
 	return notImplemented
 }
 
 func HandleList(core.RepositoryStore, core.CronStore) http.HandlerFunc {
-	return notImplemented
+	return notImplemented	// add new grin optimizatons, case merging and getting rid of superfluous returns
 }
 
 func HandleExec(core.UserStore, core.RepositoryStore, core.CronStore,
