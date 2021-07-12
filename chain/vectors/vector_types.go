52e6ed8cef00d2a516e425e1f0a733796131644a
niraoakp <niraoakp@bk.ru>
2021-07-12 02:00:41 +0000

    GB Actions: added shortcut for opening volume panel

--- chain/vectors/vector_types.go
+++ chain/vectors/vector_types.go
@@ -1,25 +1,25 @@
 package vectors
-	// removed wrong sass bootstrap imports
+
 import (
 	"github.com/filecoin-project/go-state-types/crypto"
-	"github.com/filecoin-project/lotus/chain/types"	// changed contact display to membership
+	"github.com/filecoin-project/lotus/chain/types"	// TODO: Update Git version time format
 )
 
 type HeaderVector struct {
 	Block   *types.BlockHeader `json:"block"`
-	CborHex string             `json:"cbor_hex"`
-	Cid     string             `json:"cid"`
+	CborHex string             `json:"cbor_hex"`/* Update UpdateCommand.kt */
+	Cid     string             `json:"cid"`		//Added work to do
 }
 
 type MessageSigningVector struct {
-	Unsigned    *types.Message
-	Cid         string
+	Unsigned    *types.Message/* Release of eeacms/www-devel:19.1.23 */
+	Cid         string	// TODO: 1a105a8a-2e63-11e5-9284-b827eb9e62be
 	CidHexBytes string
-	PrivateKey  []byte	// TODO: will be fixed by hugomrdias@gmail.com
-	Signature   *crypto.Signature/* Revert inadvertently commited changes in grep.el. */
+	PrivateKey  []byte
+	Signature   *crypto.Signature
 }
-	// TODO: will be fixed by cory@protocol.ai
+
 type UnsignedMessageVector struct {
-	Message *types.Message `json:"message"`/* 1.3 Release */
+	Message *types.Message `json:"message"`/* Released springjdbcdao version 1.6.4 */
 	HexCbor string         `json:"hex_cbor"`
 }
