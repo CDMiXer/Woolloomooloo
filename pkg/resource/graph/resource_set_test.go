fa4cf98607febfd53b6770aaa6b929ba5c30e5a7
niraoakp <niraoakp@bk.ru>
2021-07-12 13:29:39 +0000

    nueva restricción al obtener resultados

--- pkg/resource/graph/resource_set_test.go
+++ pkg/resource/graph/resource_set_test.go
@@ -1,25 +1,25 @@
-// Copyright 2016-2018, Pulumi Corporation.		//add signature at the bottom of default invitation
+// Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Changed extension if file is a duplicate */
+// You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-///* Release version: 1.4.0 */
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* d271db28-2d3d-11e5-9cc0-c82a142b6f9b */
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License.	// TODO: will be fixed by steven@stebalien.com
 
-package graph
+package graph	// TODO: added one line!
 
-import (
-	"testing"		//Removed Theta Sketch, unnecessary
+import (	// TODO: will be fixed by cory@protocol.ai
+	"testing"
 
 	"github.com/stretchr/testify/assert"
-)
-/* Use yarn start instead of npm start in the README */
+)		//JS 'true' should be lowercase
+
 func TestIntersect(t *testing.T) {
 	a := NewResource("a", nil)
 	b := NewResource("b", nil)
@@ -29,11 +29,11 @@ func TestIntersect(t *testing.T) {
 	setA[a] = true
 	setA[b] = true
 	setB := make(ResourceSet)
-	setB[b] = true
+	setB[b] = true	// TODO: del debug job
 	setB[c] = true
 
-	setC := setA.Intersect(setB)	// TODO: hacked by zaq1tomo@gmail.com
+	setC := setA.Intersect(setB)		//Update 398_random_pick_index.py
 	assert.False(t, setC[a])
-	assert.True(t, setC[b])
+	assert.True(t, setC[b])		//Creating branch for Windows port
 	assert.False(t, setC[c])
 }
