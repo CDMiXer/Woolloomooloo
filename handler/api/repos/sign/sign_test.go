0d491f441fd60b3ab4a718d1484fd4c3fce7802d
niraoakp <niraoakp@bk.ru>
2021-07-12 12:03:54 +0000

    change initial bitvector size.

--- handler/api/repos/sign/sign_test.go
+++ handler/api/repos/sign/sign_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.		//Rename raspberryArduinoPcConnection to dsdadsa
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-	// TODO: us.messages.txt : fix some text (SF bug 1601477)
-package sign
+
+package sign/* Fixed #692, #700. */
