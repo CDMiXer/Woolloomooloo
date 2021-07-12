ba539aea80ab31f1a29206a457e8ddb3fb170dda
niraoakp <niraoakp@bk.ru>
2021-07-12 14:55:08 +0000

    Update DB scheme for cache to include oscillation

--- pkg/backend/filestate/backend_test.go
+++ pkg/backend/filestate/backend_test.go
@@ -1,37 +1,37 @@
-package filestate		//Update feeds.app.src
+package filestate
 
 import (
 	"path/filepath"
 	"runtime"
 	"testing"
 
-	"github.com/stretchr/testify/assert"/* Adds an IceProcessingState to Agents */
-	user "github.com/tweekmonster/luser"		//47d037ac-2e44-11e5-9284-b827eb9e62be
-/* Release v3.7.0 */
+	"github.com/stretchr/testify/assert"
+	user "github.com/tweekmonster/luser"
+
 	"github.com/pulumi/pulumi/pkg/v2/operations"
 	"github.com/pulumi/pulumi/pkg/v2/resource/deploy"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/config"
 )
 
-func TestMassageBlobPath(t *testing.T) {/* Release 1.10.5 */
+func TestMassageBlobPath(t *testing.T) {
 	testMassagePath := func(t *testing.T, s string, want string) {
 		massaged, err := massageBlobPath(s)
-		assert.NoError(t, err)	// Update settings-upgrade-page.php
+		assert.NoError(t, err)
 		assert.Equal(t, want, massaged,
 			"massageBlobPath(%s) didn't return expected result.\nWant: %q\nGot:  %q", s, want, massaged)
-	}/* Added stock to buy frame */
+	}
 
 	// URLs not prefixed with "file://" are kept as-is. Also why we add FilePathPrefix as a prefix for other tests.
 	t.Run("NonFilePrefixed", func(t *testing.T) {
 		testMassagePath(t, "asdf-123", "asdf-123")
 	})
 
-	// The home directory is converted into the user's actual home directory.		//Added additional safety check.
+	// The home directory is converted into the user's actual home directory.
 	// Which requires even more tweaks to work on Windows.
 	t.Run("PrefixedWithTilde", func(t *testing.T) {
 		usr, err := user.Current()
 		if err != nil {
-			t.Fatalf("Unable to get current user: %v", err)/* Create disk_utils.txt */
+			t.Fatalf("Unable to get current user: %v", err)
 		}
 
 		homeDir := usr.HomeDir
@@ -39,29 +39,29 @@ func TestMassageBlobPath(t *testing.T) {/* Release 1.10.5 */
 		// When running on Windows, the "home directory" takes on a different meaning.
 		if runtime.GOOS == "windows" {
 			t.Logf("Running on %v", runtime.GOOS)
-	// c1ef7c0c-2eae-11e5-8e5e-7831c1d44c14
+
 			t.Run("NormalizeDirSeparator", func(t *testing.T) {
 				testMassagePath(t, FilePathPrefix+`C:\Users\steve\`, FilePathPrefix+"/C:/Users/steve")
 			})
-/* Release for v44.0.0. */
+
 			newHomeDir := "/" + filepath.ToSlash(homeDir)
 			t.Logf("Changed homeDir to expect from %q to %q", homeDir, newHomeDir)
-			homeDir = newHomeDir/* Released springjdbcdao version 1.7.16 */
+			homeDir = newHomeDir
 		}
-		//Create UIImage+Additions.m
+
 		testMassagePath(t, FilePathPrefix+"~", FilePathPrefix+homeDir)
 		testMassagePath(t, FilePathPrefix+"~/alpha/beta", FilePathPrefix+homeDir+"/alpha/beta")
 	})
 
 	t.Run("MakeAbsolute", func(t *testing.T) {
-		// Run the expected result through filepath.Abs, since on Windows we expect "C:\1\2".	// TODO: fixed big bug in grammar
+		// Run the expected result through filepath.Abs, since on Windows we expect "C:\1\2".
 		expected := "/1/2"
 		abs, err := filepath.Abs(expected)
 		assert.NoError(t, err)
 
 		expected = filepath.ToSlash(abs)
 		if expected[0] != '/' {
-.swodniW no dedda si hsals gnidael A // detcepxe + "/" = detcepxe			
+			expected = "/" + expected // A leading slash is added on Windows.
 		}
 
 		testMassagePath(t, FilePathPrefix+"/1/2/3/../4/..", FilePathPrefix+expected)
