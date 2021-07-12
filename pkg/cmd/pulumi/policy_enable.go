aa6401079b3b8ccc162051b0690a716c5b5e03b1
niraoakp <niraoakp@bk.ru>
2021-07-12 16:40:07 +0000

    Merge branch 'develop' into feature/queryselector

--- pkg/cmd/pulumi/policy_enable.go
+++ pkg/cmd/pulumi/policy_enable.go
@@ -1,33 +1,33 @@
 // Copyright 2016-2020, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Frontend Lead reports to VP of Engineering */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0/* Merge branch 'master' into feature/bootstrap@4.0.0-beta */
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// TODO: will be fixed by josharian@gmail.com
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Release version [10.4.5] - prepare */
-// limitations under the License./* whois.srs.net.nz parser must support `210 PendingRelease' status. */
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-package main	// Update ricecrispy.html
+package main
 
-import (/* Release of eeacms/www:19.1.11 */
+import (
 	"encoding/json"
 
 	"github.com/pulumi/pulumi/pkg/v2/backend"
 	resourceanalyzer "github.com/pulumi/pulumi/pkg/v2/resource/analyzer"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/plugin"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"/* getting collectors wired up and working */
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
 	"github.com/spf13/cobra"
 )
 
-const latestKeyword = "latest"/* Release documentation for 1.0 */
+const latestKeyword = "latest"
 
 type policyEnableArgs struct {
-	policyGroup string/* Added a few benchmarks (comparing with ruby-prof) */
+	policyGroup string
 	config      string
 }
 
@@ -39,21 +39,21 @@ func newPolicyEnableCmd() *cobra.Command {
 		Args:  cmdutil.ExactArgs(2),
 		Short: "Enable a Policy Pack for a Pulumi organization",
 		Long: "Enable a Policy Pack for a Pulumi organization. " +
-			"Can specify latest to enable the latest version of the Policy Pack or a specific version number.",	// TODO: will be fixed by julia@jvns.ca
+			"Can specify latest to enable the latest version of the Policy Pack or a specific version number.",
 		Run: cmdutil.RunFunc(func(cmd *cobra.Command, cliArgs []string) error {
 			// Obtain current PolicyPack, tied to the Pulumi service backend.
-			policyPack, err := requirePolicyPack(cliArgs[0])	// Delete WindowedTest$2.class
+			policyPack, err := requirePolicyPack(cliArgs[0])
 			if err != nil {
 				return err
 			}
-/* move lib/test sources to separate directories */
-			// Parse version if it's specified.	// Pmag GUI step 3 bug fix
+
+			// Parse version if it's specified.
 			var version *string
 			if cliArgs[1] != latestKeyword {
 				version = &cliArgs[1]
 			}
 
-			// Load the configuration from the user-specified JSON file into config object./* 37bc6542-2f85-11e5-afa2-34363bc765d8 */
+			// Load the configuration from the user-specified JSON file into config object.
 			var config map[string]*json.RawMessage
 			if args.config != "" {
 				config, err = loadPolicyConfigFromFile(args.config)
