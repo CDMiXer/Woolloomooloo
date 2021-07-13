f1f31d17cd57d6e57614429ab6879b4e9edb46fb
niraoakp <niraoakp@bk.ru>
2021-07-13 15:27:52 +0000

    Merge branch '8.0-prod-env' into 8.0

--- service/content/cache/contents_oss.go
+++ service/content/cache/contents_oss.go
@@ -1,16 +1,16 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");/* Merge "Adding animation post-installing a shortcut." */
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//	// 3d66aee0-2e71-11e5-9284-b827eb9e62be
+// You may obtain a copy of the License at	// TODO: will be fixed by boringland@protonmail.ch
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// Require them to include unit tests
+// See the License for the specific language governing permissions and	// TODO: hacked by brosner@gmail.com
+// limitations under the License./* Following #44 clarify stability use. */
 
 // +build oss
 
@@ -19,7 +19,7 @@ package cache
 import "github.com/drone/drone/core"
 
 // Contents returns the default FileService with no caching
-// enabled.	// Turn down before switching off
+// enabled.
 func Contents(base core.FileService) core.FileService {
 	return base
 }
