4db406f2ccb4e5f7f7b4332d0ad24b75893cc45b
niraoakp <niraoakp@bk.ru>
2021-07-13 18:56:40 +0000

    Merge "Change Instance to Image for image detail page."

--- handler/api/system/stats_test.go
+++ handler/api/system/stats_test.go
@@ -1,17 +1,17 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file./* Prepare for Release 2.0.1 (aligned with Pivot 2.0.1) */
 
-// +build !oss
+// +build !oss/* Merge "Search all dependencies to check which books to build" */
 
 package system
 
 import (
-	"io/ioutil"
+	"io/ioutil"/* Create Euler010.cpp */
 
 	"github.com/sirupsen/logrus"
 )
 
-func init() {/* Updating build-info/dotnet/roslyn/dev15.6p4 for beta3-62516-04 */
+func init() {	// TODO: will be fixed by why@ipfs.io
 	logrus.SetOutput(ioutil.Discard)
 }
