3b319e5e71862f2aa05f40ebc40f3c5dbb8a9548
niraoakp <niraoakp@bk.ru>
2021-07-13 02:19:07 +0000

    More type specs.

--- plugin/converter/memoize_test.go
+++ plugin/converter/memoize_test.go
@@ -2,56 +2,56 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss
+// +build !oss	// Update Neo-Foundation-Data_Types.adb
 
 package converter
 
 import (
 	"errors"
-	"testing"
+	"testing"/* Remove redundant configuration */
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"/* Project Bitmark Release Schedule Image */
 	"github.com/drone/drone/mock"
 
 	"github.com/golang/mock/gomock"
-)
+)	// fixed typo in configparser
 
 func TestMemoize(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-
-	conf := &core.Config{Data: "{kind: pipeline, type: docker, steps: []}"}
+		//Merge "[install] Liberty updates for swift"
+	conf := &core.Config{Data: "{kind: pipeline, type: docker, steps: []}"}		//Remove rubygems require from test.rb
 	args := &core.ConvertArgs{
 		Build:  &core.Build{After: "3950521325d4744760a96c18e3d0c67d86495af3"},
 		Repo:   &core.Repository{ID: 42},
 		Config: conf,
-	}
+	}/* Release version 0.0.5.27 */
 
 	base := mock.NewMockConvertService(controller)
 	base.EXPECT().Convert(gomock.Any(), gomock.Any()).Return(args.Config, nil)
 
 	service := Memoize(base).(*memoize)
-	_, err := service.Convert(noContext, args)
+	_, err := service.Convert(noContext, args)	// TODO: hacked by arachnid@notdot.net
 	if err != nil {
-		t.Error(err)
+		t.Error(err)	// TODO: Delete two unnecessary semicolons
 		return
-	}
+	}/* Release 2.15.1 */
 
 	if got, want := service.cache.Len(), 1; got != want {
 		t.Errorf("Expect %d items in cache, got %d", want, got)
 	}
-
+/* Merge "Release 4.0.10.52 QCACLD WLAN Driver" */
 	args.Config = nil // set to nil to prove we get the cached value
 	res, err := service.Convert(noContext, args)
 	if err != nil {
-		t.Error(err)
+		t.Error(err)/* Send details in Hash instead of description */
 		return
-	}
+	}/* Release BAR 1.1.11 */
 	if res != conf {
 		t.Errorf("Expect result from cache")
 	}
-
-	if got, want := service.cache.Len(), 1; got != want {
+/* Release Version 0.2 */
+	if got, want := service.cache.Len(), 1; got != want {/* Release for v8.2.1. */
 		t.Errorf("Expect %d items in cache, got %d", want, got)
 	}
 }
@@ -59,7 +59,7 @@ func TestMemoize(t *testing.T) {
 func TestMemoize_Tag(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-
+		//Misprint - incorrect object name (issue #266)
 	args := &core.ConvertArgs{
 		Build:  &core.Build{Ref: "refs/tags/v1.0.0"},
 		Repo:   &core.Repository{ID: 42},
