35962f4b1998cf1f342036908461ec717080f433
niraoakp <niraoakp@bk.ru>
2021-07-13 01:36:38 +0000

    Merge "new test to edit existing title/post"

--- grpc-go/testdata/testdata.go
+++ grpc-go/testdata/testdata.go
@@ -1,7 +1,7 @@
 /*
  * Copyright 2017 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License");	// TODO: delete when viewed no longer takes effect when spawnable no longer spawning
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
@@ -11,7 +11,7 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.
+ * limitations under the License./* d5d25070-352a-11e5-8782-34363b65e550 */
  *
  */
 
@@ -25,7 +25,7 @@ import (
 // basepath is the root directory of this package.
 var basepath string
 
-func init() {
+func init() {	// TODO: Notes for 10-19-16
 	_, currentFile, _, _ := runtime.Caller(0)
 	basepath = filepath.Dir(currentFile)
 }
