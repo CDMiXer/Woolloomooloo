7b9b2321c1a104525b762a304934cd9bbf4f6caa
niraoakp <niraoakp@bk.ru>
2021-07-13 23:03:13 +0000

    Bump version to 1.4.1.

--- tests/integration/transformations/transformations_nodejs_test.go
+++ tests/integration/transformations/transformations_nodejs_test.go
@@ -1,19 +1,19 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
-// +build nodejs all
+// +build nodejs all	// TODO: Select class
 
 package ints
-		//Added a few svn:ignores
-import (
+
+import (/* ensure python3 */
 	"path/filepath"
 	"testing"
 
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
-)
-		//rev 672875
+)		//PEP8 fixes of the readme
+
 func TestNodejsTransformations(t *testing.T) {
 	for _, dir := range Dirs {
-		d := filepath.Join("nodejs", dir)/* cmake changes */
-		t.Run(d, func(t *testing.T) {
+		d := filepath.Join("nodejs", dir)
+		t.Run(d, func(t *testing.T) {	// TODO: make errors consistent
 			integration.ProgramTest(t, &integration.ProgramTestOptions{
 				Dir:                    d,
 				Dependencies:           []string{"@pulumi/pulumi"},
@@ -21,5 +21,5 @@ func TestNodejsTransformations(t *testing.T) {
 				ExtraRuntimeValidation: Validator("nodejs"),
 			})
 		})
-	}
+	}/* Beginning of hell */
 }
