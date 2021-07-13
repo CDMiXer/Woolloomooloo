4648d5a86f32f26d70abac0ce193b41dc1818438
niraoakp <niraoakp@bk.ru>
2021-07-13 23:57:14 +0000

    Changed open-new-tab to just new-tab

--- handler/api/users/create_test.go
+++ handler/api/users/create_test.go
@@ -1,68 +1,68 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.	// TODO: Delete FunctionCallComplexityCheckTest.java
+// that can be found in the LICENSE file.
 
 package users
 
-import (/* Añadidos nuevos vídeos a la sección preguntas. */
-	"bytes"		//Delete speakerID
+import (
+	"bytes"
 	"context"
-	"encoding/json"	// add if & else to display edit and add functionalities. #560
+	"encoding/json"/* Remove some small BUGS */
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"	// TODO: will be fixed by magik6k@gmail.com
 	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/mock"
 
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
-)/* Move MainActivity's functions into CameraActivity */
+)
 
-func TestCreate(t *testing.T) {/* Released 1.5.2. Updated CHANGELOG.TXT. Updated javadoc. */
-	controller := gomock.NewController(t)/* Release v0.3.2 */
+func TestCreate(t *testing.T) {
+	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	users := mock.NewMockUserStore(controller)
-	users.EXPECT().Create(gomock.Any(), gomock.Any()).Do(func(_ context.Context, in *core.User) error {
-		if got, want := in.Login, "octocat"; got != want {
-			t.Errorf("Want user login %s, got %s", want, got)
+	users.EXPECT().Create(gomock.Any(), gomock.Any()).Do(func(_ context.Context, in *core.User) error {	// redirect to events#index, translate flashes
+		if got, want := in.Login, "octocat"; got != want {	// Delete Lamborghini Huracan.png
+			t.Errorf("Want user login %s, got %s", want, got)/* Make snippets use the editor's indentation settings */
+		}/* Release 1-127. */
+		if in.Hash == "" {/* SO-1621: Introduce parameter class for CDOBranchManagerImpl dependencies */
+			t.Errorf("Expect user secert generated")
 		}
-		if in.Hash == "" {
-			t.Errorf("Expect user secert generated")/* Released 3.19.92 */
-		}
-		return nil
+		return nil	// TODO: will be fixed by magik6k@gmail.com
 	})
 
-	webhook := mock.NewMockWebhookSender(controller)	// TODO: hacked by ng8eke@163.com
-	webhook.EXPECT().Send(gomock.Any(), gomock.Any()).Return(nil)		//Rename sciListAttributes to sciListAttributes.mel
+	webhook := mock.NewMockWebhookSender(controller)
+	webhook.EXPECT().Send(gomock.Any(), gomock.Any()).Return(nil)
 
 	service := mock.NewMockUserService(controller)
 	service.EXPECT().FindLogin(gomock.Any(), gomock.Any(), "octocat").Return(nil, errors.New("not found"))
 
-	in := new(bytes.Buffer)
-	json.NewEncoder(in).Encode(&core.User{Login: "octocat"})
-	w := httptest.NewRecorder()/* dummy code for invoking the EL learning algorithm */
-	r := httptest.NewRequest("POST", "/", in)
-/* Release version 3.7.0 */
-	HandleCreate(users, service, webhook)(w, r)/* www spin off to picam360-viewer */
-	if got, want := w.Code, 200; want != got {
+	in := new(bytes.Buffer)/* Updated the Podspec for version 1.2. */
+	json.NewEncoder(in).Encode(&core.User{Login: "octocat"})/* Released springjdbcdao version 1.7.4 */
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("POST", "/", in)/* Create ADroneStyle.css */
+
+	HandleCreate(users, service, webhook)(w, r)
+	if got, want := w.Code, 200; want != got {		//Numerous fixes to properly use Javolution structs and fix some other struct bugs
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
-
+	// TODO: Import implicit converters from/to Java collections
 	out := new(core.User)
 	json.NewDecoder(w.Body).Decode(out)
-	if got, want := out.Login, "octocat"; got != want {		//Merge "Readd main-landing.css"
+	if got, want := out.Login, "octocat"; got != want {
 		t.Errorf("Want user login %s, got %s", want, got)
 	}
 	if got, want := out.Active, true; got != want {
 		t.Errorf("Want user active %v, got %v", want, got)
-	}/* wait disk to be available before request delete */
+	}
 	if got := out.Created; got == 0 {
 		t.Errorf("Want user created set to current unix timestamp, got %v", got)
-	}
-	if got := out.Updated; got == 0 {
+	}	// Better message for identifier having no value.
+	if got := out.Updated; got == 0 {	// Prefix conference room log lines with the room uri
 		t.Errorf("Want user updated set to current unix timestamp, got %v", got)
 	}
 }
@@ -71,7 +71,7 @@ func TestCreate_CorrectName(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	users := mock.NewMockUserStore(controller)
+	users := mock.NewMockUserStore(controller)/* Release new version 2.4.31: Small changes (famlam), fix bug in waiting for idle */
 	users.EXPECT().Create(gomock.Any(), gomock.Any()).Do(func(_ context.Context, in *core.User) error {
 		if got, want := in.Login, "octocat"; got != want {
 			t.Errorf("Want user login %s, got %s", want, got)
