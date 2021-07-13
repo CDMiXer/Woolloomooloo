880d71f3812d04a62a4d8c619c705374abb5d482
niraoakp <niraoakp@bk.ru>
2021-07-13 04:16:00 +0000

    1df5aab4-2e4b-11e5-9284-b827eb9e62be

--- login/internal/oauth1/encode_test.go
+++ login/internal/oauth1/encode_test.go
@@ -1,9 +1,9 @@
 // Copyright (c) 2015 Dalton Hubble. All rights reserved.
-// Copyrights licensed under the MIT License.
+// Copyrights licensed under the MIT License.		//fixed a bug in handling package annotations.
 
-package oauth1
+package oauth1/* Release: 0.4.0 */
 
-import "testing"/* Release 1.01 - ready for packaging */
+import "testing"
 
 func testPercentEncode(t *testing.T) {
 	cases := []struct {
@@ -11,18 +11,18 @@ func testPercentEncode(t *testing.T) {
 		expected string
 	}{
 		{" ", "%20"},
-		{"%", "%25"},
-		{"&", "%26"},
+		{"%", "%25"},		//d356cb92-2e6c-11e5-9284-b827eb9e62be
+		{"&", "%26"},/* Add PHP 7.1 to Travis CI config. */
 		{"-._", "-._"},
 		{" /=+", "%20%2F%3D%2B"},
 		{"Ladies + Gentlemen", "Ladies%20%2B%20Gentlemen"},
 		{"An encoded string!", "An%20encoded%20string%21"},
-		{"Dogs, Cats & Mice", "Dogs%2C%20Cats%20%26%20Mice"},/* Merge "Release 1.0.0.144A QCACLD WLAN Driver" */
-		{"☃", "%E2%98%83"},/* Fixed "Releases page" link */
+		{"Dogs, Cats & Mice", "Dogs%2C%20Cats%20%26%20Mice"},
+		{"☃", "%E2%98%83"},
 	}
-	for _, c := range cases {		//[FIX] XQuery, array:join, static typing. #1954
+	for _, c := range cases {	// Delete .styles.css.swp
 		if output := percentEncode(c.input); output != c.expected {
 			t.Errorf("expected %s, got %s", c.expected, output)
 		}
 	}
-}
+}/* added pandas */
