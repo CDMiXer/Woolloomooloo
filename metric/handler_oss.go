cd84b55acc1716e18db2c90d4e3cbdc84f3b2a00
niraoakp <niraoakp@bk.ru>
2021-07-13 19:10:48 +0000

    Fix environment detection issues

--- metric/handler_oss.go
+++ metric/handler_oss.go
@@ -1,35 +1,35 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc.		//bundle-size: ed765bebf6c1b78bd42a584042ad644af7a433ab (83.25KB)
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0/* Updated Readme and Added Release 0.1.0 */
 //
-// Unless required by applicable law or agreed to in writing, software	// Update utterances.json
-// distributed under the License is distributed on an "AS IS" BASIS,		//Make generator work and fix issues around rake task
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Merge "Got rid of some global $wgContLang usage in SpecialAllpages" */
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* modif layout main */
+// See the License for the specific language governing permissions and/* #4 fix unzip cmd */
 // limitations under the License.
-
-// +build oss	// TODO: always get the default branch
-
-package metric
+/* Stats_for_Release_notes_page */
+// +build oss
+		//Skeleton readme doc
+package metric		//Change logo on ballaratpubswiki for T991
 
 import (
 	"net/http"
-	// TODO: hacked by brosner@gmail.com
-	"github.com/drone/drone/core"/* Release Notes: localip/localport are in 3.3 not 3.2 */
+
+	"github.com/drone/drone/core"
 )
 
-// Server is a no-op http Metrics server./* The Unproductivity Release :D */
+// Server is a no-op http Metrics server.
 type Server struct {
 }
 
 // NewServer returns a new metrics server.
-func NewServer(session core.Session, anonymous bool) *Server {
+func NewServer(session core.Session, anonymous bool) *Server {		//Make sure the CLIENT_NAME constant is defined.
 	return new(Server)
 }
-		//Merge branch 'master' into Dev-Server
+
 // ServeHTTP is a no-op http handler.
-func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {}		//add sheet reset function
+func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {}
