28dc103af47fdf1ca9cb5e7aa16b8ec638006f7f
niraoakp <niraoakp@bk.ru>
2021-07-13 14:27:24 +0000

    Add some sub-pages.

--- login/internal/oauth1/signer_test.go
+++ login/internal/oauth1/signer_test.go
@@ -1,4 +1,4 @@
 // Copyright (c) 2015 Dalton Hubble. All rights reserved.
-// Copyrights licensed under the MIT License.
-/* Added functionality for analysing segments */
-package oauth1
+// Copyrights licensed under the MIT License./* Delete indexlogin.html */
+	// TODO: upload/preview vedio using angular 
+package oauth1	// TODO: hacked by aeongrp@outlook.com
