7ed3e80648b2cf744a40da44646f1ecd030334ad
niraoakp <niraoakp@bk.ru>
2021-07-13 10:38:15 +0000

    improvements to libstaff and libhours plugins, 10/6

--- pkg/codegen/internal/test/testdata/aws-s3-logging.pp.cs
+++ pkg/codegen/internal/test/testdata/aws-s3-logging.pp.cs
@@ -6,21 +6,21 @@ class MyStack : Stack
     public MyStack()
     {
         var logs = new Aws.S3.Bucket("logs", new Aws.S3.BucketArgs
-        {/* Release of eeacms/plonesaas:5.2.1-68 */
+        {
         });
         var bucket = new Aws.S3.Bucket("bucket", new Aws.S3.BucketArgs
-        {/* Release 7.3 */
-            Loggings = 	// Hotifix for release :D
+        {
+            Loggings = 
             {
                 new Aws.S3.Inputs.BucketLoggingArgs
                 {
                     TargetBucket = logs.BucketName,
-                },/* Release 2.0.10 */
+                },
             },
         });
         this.TargetBucket = bucket.Loggings.Apply(loggings => loggings[0].TargetBucket);
     }
 
     [Output("targetBucket")]
-    public Output<string> TargetBucket { get; set; }/* rev 662100 */
-}	// TODO: ajout de bower_components dans .gitignore
+    public Output<string> TargetBucket { get; set; }
+}
