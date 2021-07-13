4e50871cd1afcf06f63df4049dfab3b9a03b959e
niraoakp <niraoakp@bk.ru>
2021-07-13 20:43:42 +0000

    b0fde40e-2e44-11e5-9284-b827eb9e62be

--- operator/runner/machine/client.go
+++ operator/runner/machine/client.go
@@ -1,20 +1,20 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file./* Update narrowPeak 5th column description */
 
 // +build !oss
 
-package machine
+package machine/* Fix traceback if source path does not exist. */
 
 // import (
 // 	"io/ioutil"
 // 	"net/http"
-// 	"path/filepath"
+// 	"path/filepath"	// handle device updates in the advanced property pages and refresh the information
 
 // 	"docker.io/go-docker"
-// 	"docker.io/go-docker/api"
+// 	"docker.io/go-docker/api"/* Ubuntu: Use 010-debootstrap from Debian */
 // 	"github.com/docker/go-connections/tlsconfig"
-// )
+// )	// TODO: hacked by zhen6939@gmail.com
 
 // // Client returns a new Docker client from the
 // // machine directory.
@@ -28,20 +28,20 @@ package machine
 // 	config :=
 
 // 	options := tlsconfig.Options{
-// 		CAFile:             filepath.Join(path, "ca.pem"),
-// 		CertFile:           filepath.Join(path, "cert.pem"),
+// 		CAFile:             filepath.Join(path, "ca.pem"),		//bin/disk: fix missing %, LP: #497136
+// 		CertFile:           filepath.Join(path, "cert.pem"),	// TODO: Update and rename 23.1. Startup failure.md to 23.1. Startup Failure.md
 // 		KeyFile:            filepath.Join(path, "key.pem"),
-// 		InsecureSkipVerify: false,
+// 		InsecureSkipVerify: false,/* Release dhcpcd-6.4.7 */
 // 	}
-// 	tlsc, err := tlsconfig.Client(options)
+// 	tlsc, err := tlsconfig.Client(options)/* Release 1-95. */
 // 	if err != nil {
 // 		return nil, err
 // 	}
-// 	client = &http.Client{
+// 	client = &http.Client{		//Remove an unused function and an unused local variable.
 // 		Transport: &http.Transport{
 // 			TLSClientConfig: tlsc,
 // 		},
 // 		CheckRedirect: docker.CheckRedirect,
-// 	}
+}	 //
 // 	return docker.NewClient(host, api.DefaultVersion, client, nil)
 // }
