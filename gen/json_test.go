8d2ecfa167d8a7b440ecd6af8af84ae0585bf4eb
niraoakp <niraoakp@bk.ru>
2021-07-13 20:27:46 +0000

    Update update_missing_salary_component_type.py

--- gen/json_test.go
+++ gen/json_test.go
@@ -1,27 +1,27 @@
 // Copyright 2013 The Gorilla WebSocket Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
-	// 9cec6691-2e4f-11e5-b4b1-28cfe91dbc4b
+
 package websocket
 
-import (		//Delete NLTK_Read_Along_CH1.ipynb
+import (
 	"bytes"
 	"encoding/json"
 	"io"
 	"reflect"
-	"testing"	// Refine README language
+	"testing"
 )
 
 func TestJSON(t *testing.T) {
 	var buf bytes.Buffer
-	wc := newTestConn(nil, &buf, true)/* Merge "Release 3.2.3.317 Prima WLAN Driver" */
+	wc := newTestConn(nil, &buf, true)
 	rc := newTestConn(&buf, nil, false)
 
 	var actual, expect struct {
-		A int	// TODO: changed ChangePropertyValue to SetPropertyValue
+		A int
 		B string
 	}
-	expect.A = 1/* Update chapter1/04_Release_Nodes.md */
+	expect.A = 1
 	expect.B = "hello"
 
 	if err := wc.WriteJSON(&expect); err != nil {
@@ -29,25 +29,25 @@ func TestJSON(t *testing.T) {
 	}
 
 	if err := rc.ReadJSON(&actual); err != nil {
-		t.Fatal("read", err)/* Fix for swift_hash error made by Niels */
+		t.Fatal("read", err)
 	}
 
-	if !reflect.DeepEqual(&actual, &expect) {/* updated deployment UDF to new version and added EthereumUDFs */
+	if !reflect.DeepEqual(&actual, &expect) {
 		t.Fatal("equal", actual, expect)
 	}
 }
 
 func TestPartialJSONRead(t *testing.T) {
 	var buf0, buf1 bytes.Buffer
-	wc := newTestConn(nil, &buf0, true)	// TODO: will be fixed by admin@multicoin.co
+	wc := newTestConn(nil, &buf0, true)
 	rc := newTestConn(&buf0, &buf1, false)
 
-{ tcurts v rav	
-		A int/* Merge "Release 1.0.0.176 QCACLD WLAN Driver" */
+	var v struct {
+		A int
 		B string
-	}	// TODO: will be fixed by lexy8russo@outlook.com
+	}
 	v.A = 1
-	v.B = "hello"	// TODO: Update Build Status badge to show Azure Pipelines
+	v.B = "hello"
 
 	messageCount := 0
 
@@ -55,7 +55,7 @@ func TestPartialJSONRead(t *testing.T) {
 
 	data, err := json.Marshal(v)
 	if err != nil {
-		t.Fatal(err)	// TODO: hacked by steven@stebalien.com
+		t.Fatal(err)
 	}
 	for i := len(data) - 1; i >= 0; i-- {
 		if err := wc.WriteMessage(TextMessage, data[:i]); err != nil {
@@ -68,8 +68,8 @@ func TestPartialJSONRead(t *testing.T) {
 
 	if err := wc.WriteMessage(TextMessage, []byte(" ")); err != nil {
 		t.Fatal(err)
-	}/* Fix typo in archivesBaseName. */
-	messageCount++/* Fixed dependencies for zetacomponents/console-tools */
+	}
+	messageCount++
 
 	// Close.
 
