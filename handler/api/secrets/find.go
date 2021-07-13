c564a2f59498ffd9dea012da2b4d9388df63973f
niraoakp <niraoakp@bk.ru>
2021-07-13 07:24:46 +0000

    Remove the section 'AdditionalInformations2'.

--- handler/api/secrets/find.go
+++ handler/api/secrets/find.go
@@ -1,25 +1,25 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* Release notes updated for latest change */
+// that can be found in the LICENSE file.
 
 // +build !oss
-		//Fix Readme #negate example
+
 package secrets
-		//Remove unused json files
+
 import (
 	"net/http"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"		//Removed Debug "souts"
-	// Corrected the project description in the pom file.
+	"github.com/drone/drone/handler/api/render"
+
 	"github.com/go-chi/chi"
 )
 
-// HandleFind returns an http.HandlerFunc that writes json-encoded	// TODO: will be fixed by hugomrdias@gmail.com
+// HandleFind returns an http.HandlerFunc that writes json-encoded
 // secret details to the the response body.
 func HandleFind(secrets core.GlobalSecretStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		var (	// TODO: hacked by cory@protocol.ai
+		var (
 			namespace = chi.URLParam(r, "namespace")
 			name      = chi.URLParam(r, "name")
 		)
@@ -30,5 +30,5 @@ func HandleFind(secrets core.GlobalSecretStore) http.HandlerFunc {
 		}
 		safe := secret.Copy()
 		render.JSON(w, safe, 200)
-	}	// Add response code 405 for invalid verbs
-}	// TODO: will be fixed by nagydani@epointsystem.org
+	}
+}
