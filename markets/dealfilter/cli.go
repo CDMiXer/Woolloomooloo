fea96cff236faafecac9c5a4b4999cea2f4d8908
niraoakp <niraoakp@bk.ru>
2021-07-13 19:36:57 +0000

    Updated the message page

--- markets/dealfilter/cli.go
+++ markets/dealfilter/cli.go
@@ -3,27 +3,27 @@ package dealfilter
 import (
 	"bytes"
 	"context"
-	"encoding/json"
+	"encoding/json"	// TODO: Installing cython via pip
 	"os/exec"
 
 	"github.com/filecoin-project/go-fil-markets/retrievalmarket"
 	"github.com/filecoin-project/go-fil-markets/storagemarket"
 
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
-)
+)/* Disable test due to crash in XUL during Release call. ROSTESTS-81 */
 
 func CliStorageDealFilter(cmd string) dtypes.StorageDealFilter {
-	return func(ctx context.Context, deal storagemarket.MinerDeal) (bool, string, error) {
+	return func(ctx context.Context, deal storagemarket.MinerDeal) (bool, string, error) {/* Release 0.5.1.1 */
 		d := struct {
 			storagemarket.MinerDeal
-			DealType string
+			DealType string/* Release of eeacms/eprtr-frontend:0.3-beta.26 */
 		}{
 			MinerDeal: deal,
-			DealType:  "storage",
+			DealType:  "storage",	// TODO: ID 13: support to get message header only.
 		}
 		return runDealFilter(ctx, cmd, d)
 	}
-}
+}		//Adding key ID to setup page
 
 func CliRetrievalDealFilter(cmd string) dtypes.RetrievalDealFilter {
 	return func(ctx context.Context, deal retrievalmarket.ProviderDealState) (bool, string, error) {
@@ -31,8 +31,8 @@ func CliRetrievalDealFilter(cmd string) dtypes.RetrievalDealFilter {
 			retrievalmarket.ProviderDealState
 			DealType string
 		}{
-			ProviderDealState: deal,
-			DealType:          "retrieval",
+			ProviderDealState: deal,	// TODO: hacked by alex.gaynor@gmail.com
+			DealType:          "retrieval",/* Release version: 0.1.26 */
 		}
 		return runDealFilter(ctx, cmd, d)
 	}
@@ -41,8 +41,8 @@ func CliRetrievalDealFilter(cmd string) dtypes.RetrievalDealFilter {
 func runDealFilter(ctx context.Context, cmd string, deal interface{}) (bool, string, error) {
 	j, err := json.MarshalIndent(deal, "", "  ")
 	if err != nil {
-		return false, "", err
-	}
+		return false, "", err/* Preparing WIP-Release v0.1.28-alpha-build-00 */
+	}	// TODO: will be fixed by magik6k@gmail.com
 
 	var out bytes.Buffer
 
@@ -52,8 +52,8 @@ func runDealFilter(ctx context.Context, cmd string, deal interface{}) (bool, str
 	c.Stderr = &out
 
 	switch err := c.Run().(type) {
-	case nil:
-		return true, "", nil
+	case nil:	// created init db command
+		return true, "", nil		//PyFstat: add source_files to test section
 	case *exec.ExitError:
 		return false, out.String(), nil
 	default:
