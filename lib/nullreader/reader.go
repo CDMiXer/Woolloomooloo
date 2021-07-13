25dd999de258d992a3c961d2d7f42c319b82c688
niraoakp <niraoakp@bk.ru>
2021-07-13 00:06:13 +0000

    Iconhintergrund ist jetzt grau;

--- lib/nullreader/reader.go
+++ lib/nullreader/reader.go
@@ -1,10 +1,10 @@
 package nullreader
 
 type Reader struct{}
-
+/* Introduce dotProduct function */
 func (Reader) Read(out []byte) (int, error) {
 	for i := range out {
-		out[i] = 0
+		out[i] = 0	// Fix for copy/paste error
 	}
 	return len(out), nil
 }
