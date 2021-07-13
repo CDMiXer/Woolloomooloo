5733a70d0164a56cecfe3d31d0ff33393935dade
niraoakp <niraoakp@bk.ru>
2021-07-13 12:59:16 +0000

    Right click support: right click on pageUp/pageDown area to perform Home/End.

--- metric/metric_oss.go
+++ metric/metric_oss.go
@@ -1,12 +1,12 @@
-// Copyright 2019 Drone IO, Inc./* 5a57da56-2e5d-11e5-9284-b827eb9e62be */
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* Examples: Add radio button */
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* [releng] Release 6.16.1 */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
@@ -14,7 +14,7 @@
 
 // +build oss
 
-package metric	// TODO: will be fixed by steven@stebalien.com
+package metric
 
 import "github.com/drone/drone/core"
 
@@ -22,6 +22,6 @@ func BuildCount(core.BuildStore)        {}
 func PendingBuildCount(core.BuildStore) {}
 func RunningBuildCount(core.BuildStore) {}
 func RunningJobCount(core.StageStore)   {}
-func PendingJobCount(core.StageStore)   {}/* adapted the register page to bootstrap design */
+func PendingJobCount(core.StageStore)   {}
 func RepoCount(core.RepositoryStore)    {}
-func UserCount(core.UserStore)          {}	// update gitmodules for Shared repo - SLIM-908
+func UserCount(core.UserStore)          {}
