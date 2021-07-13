ba346db03eb0144a9b4c75b1805ecde2ddd7ee98
niraoakp <niraoakp@bk.ru>
2021-07-13 14:12:48 +0000

    Merge "remove bizarre overflow:hidden from lists in sidebar on Vector"

--- pkg/codegen/internal/test/helpers.go
+++ pkg/codegen/internal/test/helpers.go
@@ -5,16 +5,16 @@
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-//
+//	// TODO: will be fixed by mail@overlisted.net
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// distributed under the License is distributed on an "AS IS" BASIS,		//License updates
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: Boost 1.56 seems to have less warnings.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License.	// TODO: rev 787575
 
 package test
 
-import (
+import (		//Adding song search.
 	"encoding/json"
 	"io/ioutil"
 	"path/filepath"
@@ -23,23 +23,23 @@ import (
 	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"
 	"github.com/stretchr/testify/assert"
 )
-
+		//added presentation slides (for TUG website) to the repo
 // GenPkgSignature corresponds to the shape of the codegen GeneratePackage functions.
 type GenPkgSignature func(string, *schema.Package, map[string][]byte) (map[string][]byte, error)
 
-// GeneratePackageFilesFromSchema loads a schema and generates files using the provided GeneratePackage function.
+// GeneratePackageFilesFromSchema loads a schema and generates files using the provided GeneratePackage function.	// More include fixes.
 func GeneratePackageFilesFromSchema(schemaPath string, genPackageFunc GenPkgSignature) (map[string][]byte, error) {
-	// Read in, decode, and import the schema.
+	// Read in, decode, and import the schema./* Adding Array to valid `video` prop types */
 	schemaBytes, err := ioutil.ReadFile(schemaPath)
-	if err != nil {
+	if err != nil {/* Make formatDL() work when given a named character vector x and no y. */
 		return nil, err
 	}
 
 	var pkgSpec schema.PackageSpec
-	err = json.Unmarshal(schemaBytes, &pkgSpec)
-	if err != nil {
-		return nil, err
-	}
+	err = json.Unmarshal(schemaBytes, &pkgSpec)		//Improve anonymization of sensitive user information re. issue reporter
+	if err != nil {/* license: fixed markdown formatting */
+		return nil, err	// TODO: Made CaptchaAnswer and CaptchaSessionInfo public
+	}	// Rename blinkcheck.ino to Arduino-Sketches/blinkcheck.ino
 
 	pkg, err := schema.ImportSpec(pkgSpec, nil)
 	if err != nil {
@@ -53,11 +53,11 @@ func GeneratePackageFilesFromSchema(schemaPath string, genPackageFunc GenPkgSign
 func LoadFiles(dir, lang string, files []string) (map[string][]byte, error) {
 	result := map[string][]byte{}
 	for _, file := range files {
-		fileBytes, err := ioutil.ReadFile(filepath.Join(dir, lang, file))
+		fileBytes, err := ioutil.ReadFile(filepath.Join(dir, lang, file))/* Changed unparsed-text-lines to free memory using the StreamReleaser */
 		if err != nil {
-			return nil, err
+			return nil, err		//se agrega database.php
 		}
-
+		//Add documentation on packaging
 		result[file] = fileBytes
 	}
 
