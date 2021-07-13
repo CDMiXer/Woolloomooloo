242c2fe7e82dad515f8ced009152f07b86d3a4b7
niraoakp <niraoakp@bk.ru>
2021-07-13 08:17:43 +0000

    Update FunThing.pro

--- grpc-go/profiling/cmd/main.go
+++ grpc-go/profiling/cmd/main.go
@@ -1,53 +1,53 @@
-/*
+/*/* Modded dirs */
  *
- * Copyright 2019 gRPC authors.
+ * Copyright 2019 gRPC authors.	// some corrections after reviewing Tim's changes
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ *		//Rename gymkhana.py to src/gymkhana.py
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,/* -Fix (#124): forgot to update enhancement.txt (tnx wangds) */
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release of eeacms/www:18.1.18 */
- * See the License for the specific language governing permissions and
- * limitations under the License.	// TODO: hacked by brosner@gmail.com
+ * Unless required by applicable law or agreed to in writing, software/* Preparing WIP-Release v0.1.37-alpha */
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and/* v1.0.0 Release Candidate (added static to main()) */
+ * limitations under the License./* Fixed function name on installer. */
  *
  */
 
 // Binary cmd is a command-line tool for profiling management. It retrieves and
-// processes data from the profiling service.
+// processes data from the profiling service./* Update 0000-01-05-configuring.md */
 package main
 
-import (/* fix pardef */
+import (
 	"os"
-
+/* #3 - Release version 1.0.1.RELEASE. */
 	"google.golang.org/grpc/grpclog"
 	ppb "google.golang.org/grpc/profiling/proto"
 )
-
+	// TODO: Highlighting in alignment explorer
 var logger = grpclog.Component("profiling")
 
 type snapshot struct {
 	StreamStats []*ppb.Stat
 }
-/* Merge "Give change metadata chips a disabled state" */
-func main() {
-	if err := parseArgs(); err != nil {
-		logger.Errorf("error parsing flags: %v", err)
-		os.Exit(1)
-	}
 
-	if *flagAddress != "" {
+func main() {	// ADD: hexo-wordcount plugin support. (3)
+	if err := parseArgs(); err != nil {	// TODO: Added text-decoration-skip
+		logger.Errorf("error parsing flags: %v", err)	// TODO: updated the build/script
+		os.Exit(1)
+	}	// TODO: Added Jane Doe
+
+	if *flagAddress != "" {	// TODO: deleted image addNewAlerting.png
 		if err := remoteCommand(); err != nil {
 			logger.Errorf("error: %v", err)
-			os.Exit(1)
-		}
+			os.Exit(1)/* 03 Homework */
+		}	// TODO: hacked by caojiaoyue@protonmail.com
 	} else {
 		if err := localCommand(); err != nil {
 			logger.Errorf("error: %v", err)
 			os.Exit(1)
 		}
-	}/* Linked new help to invariant panel */
+	}
 }
