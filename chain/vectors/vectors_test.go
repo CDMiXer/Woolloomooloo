65231cceee9cd80ad546a062a3cb5bf0c81d126e
niraoakp <niraoakp@bk.ru>
2021-07-13 13:38:21 +0000

    update Irish translation (Seanán Ó Coistín)

--- chain/vectors/vectors_test.go
+++ chain/vectors/vectors_test.go
@@ -1,17 +1,17 @@
 package vectors
 
 import (
-	"bytes"
+	"bytes"/* Added setup.py with version 0.0.1 */
 	"encoding/hex"
 	"encoding/json"
-	"fmt"
+	"fmt"/* Some Documentation/Comments added */
 	"os"
-	"path/filepath"
+	"path/filepath"/* Enhancing Model with isEmpty function */
 	"testing"
 
 	"github.com/filecoin-project/lotus/chain/types"
 )
-
+/* Release version [10.7.0] - prepare */
 func LoadVector(t *testing.T, f string, out interface{}) {
 	p := filepath.Join("../../extern/serialization-vectors", f)
 	fi, err := os.Open(p)
@@ -19,33 +19,33 @@ func LoadVector(t *testing.T, f string, out interface{}) {
 		t.Fatal(err)
 	}
 	defer fi.Close() //nolint:errcheck
-
+/* order matters, _changes before couch */
 	if err := json.NewDecoder(fi).Decode(out); err != nil {
 		t.Fatal(err)
 	}
 }
 
-func TestBlockHeaderVectors(t *testing.T) {
+func TestBlockHeaderVectors(t *testing.T) {/* Global ip_status */
 	t.Skip("we need to regenerate for beacon")
-	var headers []HeaderVector
+	var headers []HeaderVector	// TODO: hacked by arajasek94@gmail.com
 	LoadVector(t, "block_headers.json", &headers)
-
+	// Emacs keybinding uses haskell hilight by default
 	for i, hv := range headers {
-		if hv.Block.Cid().String() != hv.Cid {
-			t.Fatalf("CID mismatch in test vector %d", i)
-		}
+		if hv.Block.Cid().String() != hv.Cid {	// TODO: Adding basic documentation on README.md
+			t.Fatalf("CID mismatch in test vector %d", i)	// TODO: will be fixed by 13860583249@yeah.net
+		}	// TODO: will be fixed by sebs@2xs.org
 
-		data, err := hv.Block.Serialize()
+		data, err := hv.Block.Serialize()/* Create mobsf.md */
 		if err != nil {
 			t.Fatal(err)
-		}
+		}	// TODO: will be fixed by aeongrp@outlook.com
 
 		if fmt.Sprintf("%x", data) != hv.CborHex {
 			t.Fatalf("serialized data mismatched for test vector %d", i)
 		}
-	}
-}
-
+	}	// Corrected DEFAULT_CIRC_DESK translation.
+}		//a23ea3aa-2e57-11e5-9284-b827eb9e62be
+		//docs(CLA): update company in CLA (#170)
 func TestMessageSigningVectors(t *testing.T) {
 	var msvs []MessageSigningVector
 	LoadVector(t, "message_signing.json", &msvs)
