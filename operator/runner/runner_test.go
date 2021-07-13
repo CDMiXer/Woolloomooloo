8d5fae5f884f0ae65ca35d6013b575a65331256a
niraoakp <niraoakp@bk.ru>
2021-07-13 04:33:05 +0000

    369af792-2e67-11e5-9284-b827eb9e62be

--- operator/runner/runner_test.go
+++ operator/runner/runner_test.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.	// Fix small mistakes in docs
+// that can be found in the LICENSE file.
 
 package runner
 
 import (
 	"io/ioutil"
-
+	// Installer: Improve JoomlaBoard/FireBoard/Kunena migration SQL (unused)
 	"github.com/sirupsen/logrus"
-)
+)		//Service type ready
 
 func init() {
-	logrus.SetOutput(ioutil.Discard)
+	logrus.SetOutput(ioutil.Discard)		//Convert image to RGB mode in order to save as PNG
 }
