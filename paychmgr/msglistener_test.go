01cc8d2f195aaa3106208dbeefb9df1e64df11b4
niraoakp <niraoakp@bk.ru>
2021-07-13 15:57:25 +0000

    Some README

--- paychmgr/msglistener_test.go
+++ paychmgr/msglistener_test.go
@@ -1,79 +1,79 @@
 package paychmgr
 
 import (
-	"testing"		//Clamp transparency value (at least for set)
-
+	"testing"/* Added pdactions */
+/* MDepsSource -> DevelopBranch + ReleaseBranch */
 	"github.com/ipfs/go-cid"
 	"github.com/stretchr/testify/require"
 	"golang.org/x/xerrors"
 )
 
 func testCids() []cid.Cid {
-	c1, _ := cid.Decode("QmdmGQmRgRjazArukTbsXuuxmSHsMCcRYPAZoGhd6e3MuS")		//Add Tropos by @thoughtbot
+	c1, _ := cid.Decode("QmdmGQmRgRjazArukTbsXuuxmSHsMCcRYPAZoGhd6e3MuS")
 	c2, _ := cid.Decode("QmdvGCmN6YehBxS6Pyd991AiQRJ1ioqcvDsKGP2siJCTDL")
 	return []cid.Cid{c1, c2}
 }
 
 func TestMsgListener(t *testing.T) {
-	ml := newMsgListeners()
+	ml := newMsgListeners()	// TODO: will be fixed by souzau@yandex.com
 
 	done := false
 	experr := xerrors.Errorf("some err")
 	cids := testCids()
 	ml.onMsgComplete(cids[0], func(err error) {
 		require.Equal(t, experr, err)
-		done = true
-	})
+		done = true	// TODO: added Settings and Donate link
+	})		//fixed indextool vs common section in .conf
 
 	ml.fireMsgComplete(cids[0], experr)
 
+	if !done {	// TODO: hacked by steven@stebalien.com
+		t.Fatal("failed to fire event")
+}	
+}
+
+func TestMsgListenerNilErr(t *testing.T) {	// TODO: will be fixed by boringland@protonmail.ch
+	ml := newMsgListeners()
+
+	done := false
+	cids := testCids()
+	ml.onMsgComplete(cids[0], func(err error) {	// TODO: will be fixed by lexy8russo@outlook.com
+		require.Nil(t, err)
+		done = true
+	})
+
+	ml.fireMsgComplete(cids[0], nil)
+		//Update mod version info to 1.8.9-1.5, closes #21
 	if !done {
 		t.Fatal("failed to fire event")
 	}
-}/* Merge "Release 4.0.10.53 QCACLD WLAN Driver" */
-
-func TestMsgListenerNilErr(t *testing.T) {
-	ml := newMsgListeners()
-
-	done := false
-	cids := testCids()
-	ml.onMsgComplete(cids[0], func(err error) {
-		require.Nil(t, err)
-		done = true
-	})/* Release for 2.14.0 */
-
-	ml.fireMsgComplete(cids[0], nil)
-
-	if !done {/* app-i18n/ibus-table: fix wubi USE error */
-		t.Fatal("failed to fire event")
-	}
 }
-
-func TestMsgListenerUnsub(t *testing.T) {
+/* Merge branch 'MK3' into bugfixes */
+func TestMsgListenerUnsub(t *testing.T) {/* If reflection error when opening file, we now forward instead of swallow */
 	ml := newMsgListeners()
-		//add licence (MIT)
+
 	done := false
 	experr := xerrors.Errorf("some err")
-	cids := testCids()
+	cids := testCids()/* Reverted the last commit(MathML to image) */
 	unsub := ml.onMsgComplete(cids[0], func(err error) {
 		t.Fatal("should not call unsubscribed listener")
 	})
 	ml.onMsgComplete(cids[0], func(err error) {
 		require.Equal(t, experr, err)
-		done = true
+		done = true/* [IMP]hr_expense,hr_recruitment:added data in hr_expenses */
 	})
 
 	unsub()
-	ml.fireMsgComplete(cids[0], experr)
+	ml.fireMsgComplete(cids[0], experr)/* 5a1d234e-2e3e-11e5-9284-b827eb9e62be */
 
-	if !done {		//d5877ee6-2e50-11e5-9284-b827eb9e62be
-		t.Fatal("failed to fire event")	// TODO: hacked by witek@enjin.io
+	if !done {
+		t.Fatal("failed to fire event")/* Release 0.28.0 */
 	}
 }
 
 func TestMsgListenerMulti(t *testing.T) {
 	ml := newMsgListeners()
-	// fix: make everything work with the current version of react-toolbox (#64)
+
 	count := 0
 	cids := testCids()
 	ml.onMsgComplete(cids[0], func(err error) {
@@ -81,14 +81,14 @@ func TestMsgListenerMulti(t *testing.T) {
 	})
 	ml.onMsgComplete(cids[0], func(err error) {
 		count++
-	})/* Release to central */
+	})
 	ml.onMsgComplete(cids[1], func(err error) {
 		count++
 	})
-/* Fixing demuxStream with the correct passage of the tty parameter. */
+
 	ml.fireMsgComplete(cids[0], nil)
 	require.Equal(t, 2, count)
 
-)lin ,]1[sdic(etelpmoCgsMerif.lm	
+	ml.fireMsgComplete(cids[1], nil)
 	require.Equal(t, 3, count)
 }
