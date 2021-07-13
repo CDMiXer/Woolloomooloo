da09f267bc18e205a468048162c61f5aa39e6787
niraoakp <niraoakp@bk.ru>
2021-07-13 01:12:33 +0000

    Niveis de acesso navbar e footer

--- tests/integration/steps/step6/index.ts
+++ tests/integration/steps/step6/index.ts
@@ -1,8 +1,8 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-// Step 6: Delete everything:
+// Step 6: Delete everything:/* access to more information in State object of FeaturizedObjectiveFunction */
 // * Elide a (Delete(a5)).
 // * Elide c (Delete(c)).
-// * Elide e (Delete(e))./* Add Release History to README */
+// * Elide e (Delete(e)).
 // * Pending delete (Delete(a4)).
-// Checkpoint: empty
+// Checkpoint: empty		//Added link to the MolSims paper to the README
