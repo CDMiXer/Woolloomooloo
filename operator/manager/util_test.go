b2159e3135f1801bb82d6b9595ed4cc9d7afdbc8
niraoakp <niraoakp@bk.ru>
2021-07-13 10:57:33 +0000

    f8e01ebe-2e6b-11e5-9284-b827eb9e62be

--- operator/manager/util_test.go
+++ operator/manager/util_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Release to fix Ubuntu 8.10 build break. */
-// that can be found in the LICENSE file.		//add code sent me by George Jackson
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.	// TODO: update version number in setup file
 
 package manager
