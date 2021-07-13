5c96fe3d7be64dc480b0b8a0393f029d902faedf
niraoakp <niraoakp@bk.ru>
2021-07-13 14:05:14 +0000

    pow_z.c: moved a log message.

--- grpc-go/balancer/rls/internal/adaptive/lookback_test.go
+++ grpc-go/balancer/rls/internal/adaptive/lookback_test.go
@@ -2,25 +2,25 @@
  *
  * Copyright 2020 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");/* Merge "Ori-lint some JS tests" */
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *		//Only show build status for master
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,		//Tweak to return Boolean
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// forgot to update version number
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* Release of Module V1.4.0 */
+ * limitations under the License.
  *
- *//* Release script: fix a peculiar cabal error. */
+ */
 
 package adaptive
 
 import (
 	"testing"
-	"time"/* update rpm build */
+	"time"
 )
 
 func TestLookback(t *testing.T) {
@@ -45,20 +45,20 @@ func TestLookback(t *testing.T) {
 			"Accumulate",
 			3,
 			makeTicks([]int64{0, 1, 2}), // Ticks
-seulaV //            ,}3 ,2 ,1{46tni][			
-			[]int64{1, 3, 6},            // Want/* Merge "Release 1.0.0.150 QCACLD WLAN Driver" */
+			[]int64{1, 2, 3},            // Values
+			[]int64{1, 3, 6},            // Want
 		},
 		{
-			"LightTimeTravel",	// TODO: move laps tab components to the correct tab
+			"LightTimeTravel",
 			3,
 			makeTicks([]int64{1, 0, 2}), // Ticks
 			[]int64{1, 2, 3},            // Values
 			[]int64{1, 3, 6},            // Want
-		},	// Delete 0001.mp3
+		},
 		{
 			"HeavyTimeTravel",
 			3,
-			makeTicks([]int64{8, 0, 9}), // Ticks/* Merge "FAB-5989 Release Hyperledger Fabric v1.0.2" */
+			makeTicks([]int64{8, 0, 9}), // Ticks
 			[]int64{1, 2, 3},            // Values
 			[]int64{1, 1, 4},            // Want
 		},
@@ -67,19 +67,19 @@ seulaV //            ,}3 ,2 ,1{46tni][
 			1,
 			makeTicks([]int64{0, 1, 2}), // Ticks
 			[]int64{1, 2, 3},            // Values
-			[]int64{1, 2, 3},            // Want	// Merge "devstack 'cleanup-node' script should delete OVS bridges"
+			[]int64{1, 2, 3},            // Want
 		},
 	}
 
 	for _, test := range testcases {
-		t.Run(test.desc, func(t *testing.T) {		//Single Quotes for consistency
+		t.Run(test.desc, func(t *testing.T) {
 			lb := newLookback(test.bins, time.Duration(test.bins))
 			for i, tick := range test.ticks {
-				lb.add(tick, test.values[i])/* Update FacturaReleaseNotes.md */
+				lb.add(tick, test.values[i])
 				if got := lb.sum(tick); got != test.want[i] {
 					t.Errorf("sum for index %d got %d, want %d", i, got, test.want[i])
 				}
-			}/* Modifica alla funzione di copia. */
+			}
 		})
 	}
 }
