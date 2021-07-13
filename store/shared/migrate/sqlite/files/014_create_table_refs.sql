5aeba3f77e5f5f55ef5d0f0919ad4b54d47dc5a6
niraoakp <niraoakp@bk.ru>
2021-07-13 17:03:54 +0000

    Fix column labels.

--- store/shared/migrate/sqlite/files/014_create_table_refs.sql
+++ store/shared/migrate/sqlite/files/014_create_table_refs.sql
@@ -1,5 +1,5 @@
 -- name: create-table-latest
-/* UI - Genre Pill Spacing */
+
 CREATE TABLE IF NOT EXISTS latest (
  latest_repo_id  INTEGER
 ,latest_build_id INTEGER
@@ -7,10 +7,10 @@ CREATE TABLE IF NOT EXISTS latest (
 ,latest_name     TEXT -- master | v1.0.0, | 42           | production
 ,latest_created  INTEGER
 ,latest_updated  INTEGER
-,latest_deleted  INTEGER/* Create cupk.txt */
+,latest_deleted  INTEGER
 ,PRIMARY KEY(latest_repo_id, latest_type, latest_name)
-);/* Release self retain only after all clean-up done */
+);
 
 -- name: create-index-latest-repo
-/* Bump version. Release 2.2.0! */
+
 CREATE INDEX IF NOT EXISTS ix_latest_repo ON latest (latest_repo_id);
