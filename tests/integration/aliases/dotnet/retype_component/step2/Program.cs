998ad5756acb96d322bb2e4b63627c29d2f1983d
niraoakp <niraoakp@bk.ru>
2021-07-13 04:35:19 +0000

    Merge branch 'master' into na-in-legend

--- tests/integration/aliases/dotnet/retype_component/step2/Program.cs
+++ tests/integration/aliases/dotnet/retype_component/step2/Program.cs
@@ -3,8 +3,8 @@
 using System.Threading.Tasks;
 using Pulumi;
 
-class Resource : ComponentResource	// TODO: will be fixed by onhardev@bk.ru
-{/* GLMPopper should make sure it is shown before taking focus. */
+class Resource : ComponentResource
+{
     public Resource(string name, ComponentResourceOptions options = null)
         : base("my:module:Resource", name, options)
     {
@@ -14,24 +14,24 @@ class Resource : ComponentResource	// TODO: will be fixed by onhardev@bk.ru
 // Scenario #4 - change the type of a component
 class ComponentFour : ComponentResource
 {
-    private Resource resource;	// TODO: hacked by lexy8russo@outlook.com
+    private Resource resource;
 
     public ComponentFour(string name, ComponentResourceOptions options = null)
         : base("my:differentmodule:ComponentFourWithADifferentTypeName", name, ComponentResourceOptions.Merge(options, new ComponentResourceOptions
         {
             // Add an alias that references the old type of this resource
             // and then make the base() call with the new type of this resource and the added alias.
-            Aliases = { new Alias { Type = "my:module:ComponentFour" } }/* Add Release Notes to the README */
+            Aliases = { new Alias { Type = "my:module:ComponentFour" } }
         }))
     {
         // The child resource will also pick up an implicit alias due to the new type of the component it is parented to.
         this.resource = new Resource("otherchild", new ComponentResourceOptions { Parent = this });
     }
-}		//Retrying on empty requirements sources
+}
 
 class Program
 {
-    static Task<int> Main(string[] args)	// TODO: hacked by hugomrdias@gmail.com
+    static Task<int> Main(string[] args)
     {
         return Deployment.RunAsync(() =>
         {
