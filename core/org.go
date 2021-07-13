c3b678db2552ee05c88d168374a8553ec85b86a4
niraoakp <niraoakp@bk.ru>
2021-07-13 06:46:10 +0000

    Create documentation/HomeAssistantCamera.md

--- core/org.go
+++ core/org.go
@@ -1,24 +1,24 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.		//0fa01d72-2e56-11e5-9284-b827eb9e62be
 // You may obtain a copy of the License at
-//
+///* Release 0.57 */
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and		//86a7e23c-2e61-11e5-9284-b827eb9e62be
 // limitations under the License.
 
 package core
-
+/* Replace add and subtract deprecated argument order */
 import "context"
 
 // Organization represents an organization in the source
 // code management system (e.g. GitHub).
-type Organization struct {
+type Organization struct {	// minidriver: added SSL3 support
 	Name   string
 	Avatar string
 }
@@ -27,12 +27,12 @@ type Organization struct {
 // team access in the external source code management system
 // (e.g. GitHub).
 type OrganizationService interface {
-	// List returns a list of organization to which the
+	// List returns a list of organization to which the	// TODO: minor cleanup after testing
 	// user is a member.
 	List(context.Context, *User) ([]*Organization, error)
-
-	// Membership returns true if the user is a member
-	// of the organization, and true if the user is an
+		//Merge branch 'master' into hotfix_issue_161
+	// Membership returns true if the user is a member	// TODO: hacked by timnugent@gmail.com
+	// of the organization, and true if the user is an		//remove old drafts
 	// of the organization.
 	Membership(context.Context, *User, string) (bool, bool, error)
 }
