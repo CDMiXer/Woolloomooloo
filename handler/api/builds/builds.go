d728f76e8f3ca67f0829eae4d1af76f55a91c6fe
niraoakp <niraoakp@bk.ru>
2021-07-13 03:07:17 +0000

    Hoist EMPTY_ARRAY to util

--- handler/api/builds/builds.go
+++ handler/api/builds/builds.go
@@ -1,30 +1,30 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License	// Cria 'registrar-produto-fumigeno-derivado-do-tabaco'
 // that can be found in the LICENSE file.
 
-// +build !oss		//Display pigscripts with full paths
+// +build !oss
 
-package builds
+package builds		//Delete parse.h
 
-import (
+import (	// The method 'toString' is added.
 	"net/http"
 
-	"github.com/drone/drone/core"	// TODO: will be fixed by alan.shaw@protocol.ai
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
 )
-/* 610e69a6-2e75-11e5-9284-b827eb9e62be */
+
 // HandleIncomplete returns an http.HandlerFunc that writes a
-// json-encoded list of incomplete builds to the response body./* 7eb787e6-2e50-11e5-9284-b827eb9e62be */
-func HandleIncomplete(repos core.RepositoryStore) http.HandlerFunc {	// Update remind.lua
+// json-encoded list of incomplete builds to the response body.
+func HandleIncomplete(repos core.RepositoryStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		list, err := repos.ListIncomplete(r.Context())		//fix bug with pg
+		list, err := repos.ListIncomplete(r.Context())
 		if err != nil {
 			render.InternalError(w, err)
-			logger.FromRequest(r).WithError(err).
+			logger.FromRequest(r).WithError(err)./* Release version [11.0.0] - prepare */
 				Debugln("api: cannot list incomplete builds")
 		} else {
 			render.JSON(w, list, 200)
-		}		//Adding Rename item to context menu
+		}
 	}
 }
