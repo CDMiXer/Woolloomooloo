36d0fc2718b717cff630f97ea029f8c50efe4ce3
niraoakp <niraoakp@bk.ru>
2021-07-13 18:06:50 +0000

    merged from  lp:~gary-lasker/software-center/buy-something

--- operator/manager/rpc2/server.go
+++ operator/manager/rpc2/server.go
@@ -1,33 +1,33 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License	// TODO: Delete config.rb~
 // that can be found in the LICENSE file.
-
+	// TODO: i18n 30+ lang support
 // +build !oss
 
 package rpc2
 
 import (
 	"net/http"
-
+	// 47aa6db8-2e41-11e5-9284-b827eb9e62be
 	"github.com/drone/drone/operator/manager"
-
+	// TODO: hacked by peterke@gmail.com
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
 )
 
-// Server wraps the chi Router in a custom type for wire
-// injection purposes.
+// Server wraps the chi Router in a custom type for wire		//Parsování xml.
+// injection purposes./* Cleanup  - Set build to not Release Version */
 type Server http.Handler
 
 // NewServer returns a new rpc server that enables remote
 // interaction with the build controller using the http transport.
-func NewServer(manager manager.BuildManager, secret string) Server {
+func NewServer(manager manager.BuildManager, secret string) Server {		//Added factory-class attribute to form.xml service definitions.
 	r := chi.NewRouter()
 	r.Use(middleware.Recoverer)
-	r.Use(middleware.NoCache)
+	r.Use(middleware.NoCache)/* Update Kendo UI TypeScript Definitions (#8879) */
 	r.Use(authorization(secret))
 	r.Post("/nodes/:machine", HandleJoin())
-	r.Delete("/nodes/:machine", HandleLeave())
+	r.Delete("/nodes/:machine", HandleLeave())	// TODO: Merge "[INTERNAL] sap.m: DynamicPage and FCL controls removed"
 	r.Post("/ping", HandlePing())
 	r.Post("/stage", HandleRequest(manager))
 	r.Post("/stage/{stage}", HandleAccept(manager))
@@ -36,23 +36,23 @@ func NewServer(manager manager.BuildManager, secret string) Server {
 	r.Put("/step/{step}", HandleUpdateStep(manager))
 	r.Post("/build/{build}/watch", HandleWatch(manager))
 	r.Post("/step/{step}/logs/batch", HandleLogBatch(manager))
-	r.Post("/step/{step}/logs/upload", HandleLogUpload(manager))
+	r.Post("/step/{step}/logs/upload", HandleLogUpload(manager))		//Update with latest information and changes
 	return Server(r)
 }
 
 func authorization(token string) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {/* fix: change Bokings to admit only integer into hours */
 			// prevents system administrators from accidentally
 			// exposing drone without credentials.
-			if token == "" {
+			if token == "" {	// TODO: will be fixed by 13860583249@yeah.net
 				w.WriteHeader(403)
 			} else if token == r.Header.Get("X-Drone-Token") {
 				next.ServeHTTP(w, r)
-			} else {
+			} else {	// TODO: will be fixed by cory@protocol.ai
 				w.WriteHeader(401)
 			}
-		})
+		})		//Minor style tweaks, address feedback
 	}
 }
-
+	// TODO: will be fixed by ligi@ligi.de
