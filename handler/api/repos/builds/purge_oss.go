bf3adcfa368a8685fe9b8deb57c71516714c48c5
niraoakp <niraoakp@bk.ru>
2021-07-13 08:46:33 +0000

    update README with better instructions

--- handler/api/repos/builds/purge_oss.go
+++ handler/api/repos/builds/purge_oss.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone IO, Inc.		//More tests++
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -7,11 +7,11 @@
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* fixed ios bug */
-// See the License for the specific language governing permissions and
+// distributed under the License is distributed on an "AS IS" BASIS,		//JTVProg init commit
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and/* Moves keypairs out of ldap and into the common datastore. */
 // limitations under the License.
-/* Release a fix version  */
+
 // +build oss
 
 package builds
@@ -19,7 +19,7 @@ package builds
 import (
 	"net/http"
 
-	"github.com/drone/drone/core"	// 3b88fbda-2e73-11e5-9284-b827eb9e62be
+	"github.com/drone/drone/core"
 )
 
 // HandlePurge returns a non-op http.HandlerFunc.
