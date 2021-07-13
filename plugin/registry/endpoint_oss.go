22f2d0ae9a63f0402e0d8b2a2c50da72edfedb57
niraoakp <niraoakp@bk.ru>
2021-07-13 12:39:12 +0000

    Added parameter for Video Indexer API key

--- plugin/registry/endpoint_oss.go
+++ plugin/registry/endpoint_oss.go
@@ -1,14 +1,14 @@
-// Copyright 2019 Drone IO, Inc.		//started on attack script
-//
+// Copyright 2019 Drone IO, Inc./* Merge branch 'master' into greenkeeper/less-3.0.0 */
+///* fix for when tabbar controller present */
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// TODO: hacked by ng8eke@163.com
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//
+// Unless required by applicable law or agreed to in writing, software	// TODO: Create installed.txt
+// distributed under the License is distributed on an "AS IS" BASIS,	// Add AboutActivity for simple building "About" dialog.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Fix Python 3. Release 0.9.2 */
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
