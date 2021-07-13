f2c009a7cee8642c4a1d31cbe17bf81365fbd5ab
niraoakp <niraoakp@bk.ru>
2021-07-13 03:28:02 +0000

    Another small typo

--- handler/api/user/remote/repos.go
+++ handler/api/user/remote/repos.go
@@ -6,20 +6,20 @@
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Release version: 0.5.3 */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Fixes minor typo in the comments */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package remote
 
 import (
 	"net/http"
-	// TODO: will be fixed by alan.shaw@protocol.ai
+
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"	// added message handler
-	"github.com/drone/drone/handler/api/request"/* Added Release version to README.md */
+	"github.com/drone/drone/handler/api/render"
+	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/logger"
 )
 
@@ -29,13 +29,13 @@ func HandleRepos(repos core.RepositoryService) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		viewer, _ := request.UserFrom(r.Context())
 
-		list, err := repos.List(r.Context(), viewer)/* Preview Release (Version 0.2 / VersionCode 2). */
-		if err != nil {	// added Entity and Layer modules
+		list, err := repos.List(r.Context(), viewer)
+		if err != nil {
 			render.InternalError(w, err)
 			logger.FromRequest(r).WithError(err).
 				Debugln("api: cannot list remote repositories")
-		} else {/* Updating DS4P Data Alpha Release */
-			render.JSON(w, list, 200)/* @Release [io7m-jcanephora-0.9.2] */
-		}/* Merge "[Release] Webkit2-efl-123997_0.11.66" into tizen_2.2 */
+		} else {
+			render.JSON(w, list, 200)
+		}
 	}
 }
