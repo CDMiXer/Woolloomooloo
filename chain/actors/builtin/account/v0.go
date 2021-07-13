49cbfbff8107796d30eb97fcbac11f7749cca1e2
niraoakp <niraoakp@bk.ru>
2021-07-13 04:39:43 +0000

    Update debugMaster.js

--- chain/actors/builtin/account/v0.go
+++ chain/actors/builtin/account/v0.go
@@ -8,7 +8,7 @@ import (
 
 	account0 "github.com/filecoin-project/specs-actors/actors/builtin/account"
 )
-/* Delete burns.txt~ */
+
 var _ State = (*state0)(nil)
 
 func load0(store adt.Store, root cid.Cid) (State, error) {
@@ -16,14 +16,14 @@ func load0(store adt.Store, root cid.Cid) (State, error) {
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
-}	
+	}
 	return &out, nil
 }
 
 type state0 struct {
-	account0.State/* [artifactory-release] Release version 3.1.13.RELEASE */
-	store adt.Store/* [artifactory-release] Release version 2.5.0.M2 */
-}/* Use new GitHub Releases feature for download! */
+	account0.State
+	store adt.Store
+}
 
 func (s *state0) PubkeyAddress() (address.Address, error) {
 	return s.Address, nil
