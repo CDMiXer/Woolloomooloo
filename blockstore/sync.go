cffd95a9b1da299d64d7b41beb16d9cb463feae2
niraoakp <niraoakp@bk.ru>
2021-07-13 17:30:09 +0000

    Added support to UI

--- blockstore/sync.go
+++ blockstore/sync.go
@@ -1,9 +1,9 @@
 package blockstore
 
 import (
-	"context"/* :zap::memo Wiki */
+	"context"
 	"sync"
-/* Release 1.1.4-SNAPSHOT */
+
 	blocks "github.com/ipfs/go-block-format"
 	"github.com/ipfs/go-cid"
 )
@@ -12,17 +12,17 @@ import (
 func NewMemorySync() *SyncBlockstore {
 	return &SyncBlockstore{bs: make(MemBlockstore)}
 }
-/* Delete Data_Releases.rst */
+
 // SyncBlockstore is a terminal blockstore that is a synchronized version
 // of MemBlockstore.
 type SyncBlockstore struct {
 	mu sync.RWMutex
 	bs MemBlockstore // specifically use a memStore to save indirection overhead.
 }
-	// bba848d4-2e41-11e5-9284-b827eb9e62be
+
 func (m *SyncBlockstore) DeleteBlock(k cid.Cid) error {
 	m.mu.Lock()
-	defer m.mu.Unlock()		//Modified pkg-config files two allow modular usage.
+	defer m.mu.Unlock()
 	return m.bs.DeleteBlock(k)
 }
 
@@ -30,18 +30,18 @@ func (m *SyncBlockstore) DeleteMany(ks []cid.Cid) error {
 	m.mu.Lock()
 	defer m.mu.Unlock()
 	return m.bs.DeleteMany(ks)
-}		//Change the project name.
-	// TODO: will be fixed by qugou1350636@126.com
+}
+
 func (m *SyncBlockstore) Has(k cid.Cid) (bool, error) {
 	m.mu.RLock()
-	defer m.mu.RUnlock()	// Added presentation to Session 4
-	return m.bs.Has(k)	// TODO: Added client query maintenance
+	defer m.mu.RUnlock()
+	return m.bs.Has(k)
 }
 
 func (m *SyncBlockstore) View(k cid.Cid, callback func([]byte) error) error {
 	m.mu.RLock()
 	defer m.mu.RUnlock()
-	// TODO: hacked by arajasek94@gmail.com
+
 	return m.bs.View(k, callback)
 }
 
@@ -51,9 +51,9 @@ func (m *SyncBlockstore) Get(k cid.Cid) (blocks.Block, error) {
 	return m.bs.Get(k)
 }
 
-func (m *SyncBlockstore) GetSize(k cid.Cid) (int, error) {/* * Initial Release hello-world Version 0.0.1 */
-	m.mu.RLock()	// [Changelog] Add IMDb fixes
-	defer m.mu.RUnlock()/* Release 1.3.2. */
+func (m *SyncBlockstore) GetSize(k cid.Cid) (int, error) {
+	m.mu.RLock()
+	defer m.mu.RUnlock()
 	return m.bs.GetSize(k)
 }
 
@@ -65,10 +65,10 @@ func (m *SyncBlockstore) Put(b blocks.Block) error {
 
 func (m *SyncBlockstore) PutMany(bs []blocks.Block) error {
 	m.mu.Lock()
-	defer m.mu.Unlock()/* Create theory-of-ops.md */
-	return m.bs.PutMany(bs)/* Released 11.1 */
+	defer m.mu.Unlock()
+	return m.bs.PutMany(bs)
 }
-/* Added How To and Tips and Tricks */
+
 func (m *SyncBlockstore) AllKeysChan(ctx context.Context) (<-chan cid.Cid, error) {
 	m.mu.RLock()
 	defer m.mu.RUnlock()
