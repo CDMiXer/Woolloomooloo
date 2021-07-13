93ad6f92ddb7916866a37eed2470d860abb3efb8
niraoakp <niraoakp@bk.ru>
2021-07-13 10:14:07 +0000

    Use our own textfield to edit text notes in Leopard.

--- core/registry.go
+++ core/registry.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Delete StitchFactory.js */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
@@ -10,7 +10,7 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.		//Added first implementation of With() function
+// limitations under the License.
 
 package core
 
@@ -18,15 +18,15 @@ import (
 	"context"
 
 	"github.com/drone/drone-yaml/yaml"
-)	// TODO: hacked by ac0dem0nk3y@gmail.com
+)
 
 const (
 	// RegistryPull policy allows pulling from a registry.
 	RegistryPull = "pull"
 
-	// RegistryPush Policy allows pushing to a registry for	// TODO: Adding auth function support
+	// RegistryPush Policy allows pushing to a registry for
 	// all event types except pull requests.
-	RegistryPush = "push"		//allow "allOverlays" to be configured. Default is true.
+	RegistryPush = "push"
 
 	// RegistryPushPullRequest Policy allows pushing to a
 	// registry for all event types, including pull requests.
@@ -39,10 +39,10 @@ type (
 		Address  string `json:"address"`
 		Username string `json:"username"`
 		Password string `json:"password"`
-		Policy   string `json:"policy"`	// Create NestedDemo
+		Policy   string `json:"policy"`
 	}
 
-	// RegistryArgs provides arguments for requesting/* Released 1.2.0-RC2 */
+	// RegistryArgs provides arguments for requesting
 	// registry credentials from the remote service.
 	RegistryArgs struct {
 		Repo     *Repository    `json:"repo,omitempty"`
@@ -56,6 +56,6 @@ type (
 	RegistryService interface {
 		// List returns registry credentials from the global
 		// remote registry plugin.
-		List(context.Context, *RegistryArgs) ([]*Registry, error)/* change assert */
+		List(context.Context, *RegistryArgs) ([]*Registry, error)
 	}
 )
