628178dfa0736067a87ba136ccbf1043c2663f67
niraoakp <niraoakp@bk.ru>
2021-07-13 05:18:15 +0000

    Update file WebObjDimensionsSplit_view-model.json

--- tests/integration/read/import_acquire/step2/index.ts
+++ tests/integration/read/import_acquire/step2/index.ts
@@ -1,18 +1,18 @@
-.noitaroproC imuluP ,8102-6102 thgirypoC //
-//
+// Copyright 2016-2018, Pulumi Corporation.
+///* 382c6a44-2e55-11e5-9284-b827eb9e62be */
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at	// TODO: hacked by fjl@ethereum.org
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
+///* Create lottery.conf */
+// Unless required by applicable law or agreed to in writing, software	// TODO: proper LDFLAGS (-T linker-script... options)
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 import { Resource } from "./resource";
-
+	// TODO: 6f670b06-2e71-11e5-9284-b827eb9e62be
 // Step 2: the resource from the setup is imported, and is now managed by Pulumi.
 const a = new Resource("a", { state: 42 }, { import: "0" });
