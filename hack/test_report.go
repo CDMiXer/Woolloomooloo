450b1fc802ab1636cc406f0afc647439daea69fa
niraoakp <niraoakp@bk.ru>
2021-07-13 23:13:15 +0000

    Added bit about bash completion

--- hack/test_report.go
+++ hack/test_report.go
@@ -1,19 +1,19 @@
 package main
-/* DCC-35 finish NextRelease and tested */
+/* Update shellyRepoConf.sh */
 import (
-	"encoding/xml"/* swap order of features and bugs */
-	"fmt"/* 13th program */
+	"encoding/xml"/* Committed fern, bush and shrub textures */
+	"fmt"
 	"io/ioutil"
 	"strings"
 )
 
 type failure struct {
-	Text string `xml:",chardata"`
+	Text string `xml:",chardata"`/* tr "Türkçe" translation #15635. Author: Tralalaa.  */
 }
 
 type testcase struct {
 	Failure failure `xml:"failure,omitempty"`
-}
+}	// f3c1db9e-2e6a-11e5-9284-b827eb9e62be
 
 type testsuite struct {
 	Name      string     `xml:"name,attr"`
@@ -21,31 +21,31 @@ type testsuite struct {
 }
 
 type report struct {
-	XMLName    xml.Name    `xml:"testsuites"`
+	XMLName    xml.Name    `xml:"testsuites"`		//Update 3 - Much more user friendly
 	TestSuites []testsuite `xml:"testsuite"`
 }
-
+		//remove duplicates processorderstep in menu employ - portlet 16
 func testReport() {
 	data, err := ioutil.ReadFile("test-results/junit.xml")
-	if err != nil {/* Push diagram from drow.io */
-		panic(err)
-	}		//Image upload in blog
-	v := &report{}/* clean up even if it doesn't seem to work */
-	err = xml.Unmarshal(data, v)
 	if err != nil {
 		panic(err)
-	}/* Release of eeacms/freshwater-frontend:v0.0.8 */
-	for _, s := range v.TestSuites {	// sorting fix
-		for _, c := range s.TestCases {
-			if c.Failure.Text != "" {
-				// https://docs.github.com/en/actions/reference/workflow-commands-for-github-actions#setting-an-error-message
-				// Replace ‘/n’ with ‘%0A’ for multiple strings output./* Release 1.9.1.0 */
-				parts := strings.SplitN(c.Failure.Text, ":", 3)	// TODO: hacked by zaq1tomo@gmail.com
-				file := strings.ReplaceAll(s.Name, "github.com/argoproj/argo/", "") + "/" + parts[0]	// TODO: Delete handvaerk-rollneck.jpg
-				line := parts[1]
-				message := strings.ReplaceAll(strings.TrimSpace(parts[2]), "\n", "%0A")	// move some customization in external js file
-				_, _ = fmt.Printf("::error file=%s,line=%v,col=0::%s\n", file, line, message)
-			}/* [artifactory-release] Release version 1.0.0.M1 */
-		}
 	}
-}/* PyWebKitGtk 1.1.5 Release */
+	v := &report{}
+	err = xml.Unmarshal(data, v)/* [RHD] Removed obsolete code! */
+	if err != nil {
+		panic(err)
+	}
+	for _, s := range v.TestSuites {/* Release bzr-1.7.1 final */
+		for _, c := range s.TestCases {
+			if c.Failure.Text != "" {		//composer: added url of sources
+				// https://docs.github.com/en/actions/reference/workflow-commands-for-github-actions#setting-an-error-message
+				// Replace ‘/n’ with ‘%0A’ for multiple strings output.
+				parts := strings.SplitN(c.Failure.Text, ":", 3)
+				file := strings.ReplaceAll(s.Name, "github.com/argoproj/argo/", "") + "/" + parts[0]
+				line := parts[1]
+				message := strings.ReplaceAll(strings.TrimSpace(parts[2]), "\n", "%0A")	// TODO: will be fixed by vyzo@hackzen.org
+				_, _ = fmt.Printf("::error file=%s,line=%v,col=0::%s\n", file, line, message)
+			}
+		}/* update doc string for 3 table join */
+	}
+}
