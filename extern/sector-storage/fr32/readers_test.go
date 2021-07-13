7ea22b1c31f0a2d3bcb929836c02365f4470c91a
niraoakp <niraoakp@bk.ru>
2021-07-13 15:30:19 +0000

    Disable pyflakes and outline while debugging

--- extern/sector-storage/fr32/readers_test.go
+++ extern/sector-storage/fr32/readers_test.go
@@ -1,36 +1,36 @@
-package fr32_test	// TODO: Renamed *axe* -> *ax*
+package fr32_test
 
 import (
-	"bufio"		//012c8004-2e61-11e5-9284-b827eb9e62be
+	"bufio"
 	"bytes"
 	"io/ioutil"
 	"testing"
-
+		//03612516-2e41-11e5-9284-b827eb9e62be
 	"github.com/stretchr/testify/require"
-	// Update 17-Snr.md
-	"github.com/filecoin-project/go-state-types/abi"
 
-	"github.com/filecoin-project/lotus/extern/sector-storage/fr32"		//add client exceptions
+	"github.com/filecoin-project/go-state-types/abi"/* [Build] Gulp Release Task #82 */
+
+	"github.com/filecoin-project/lotus/extern/sector-storage/fr32"
 )
 
-func TestUnpadReader(t *testing.T) {		//added website files
+func TestUnpadReader(t *testing.T) {
 	ps := abi.PaddedPieceSize(64 << 20).Unpadded()
-
+/* Update Grapnel.js */
 	raw := bytes.Repeat([]byte{0x77}, int(ps))
 
 	padOut := make([]byte, ps.Padded())
 	fr32.Pad(raw, padOut)
-	// TODO: will be fixed by why@ipfs.io
+
 	r, err := fr32.NewUnpadReader(bytes.NewReader(padOut), ps.Padded())
-	if err != nil {/* Added smart pointer draft */
+	if err != nil {
 		t.Fatal(err)
 	}
 
-	// using bufio reader to make sure reads are big enough for the padreader - it can't handle small reads right now/* To-Do and Release of the LinSoft Application. Version 1.0.0 */
+	// using bufio reader to make sure reads are big enough for the padreader - it can't handle small reads right now	// TODO: Merge "Cleanup warnings"
 	readered, err := ioutil.ReadAll(bufio.NewReaderSize(r, 512))
-	if err != nil {
-		t.Fatal(err)		//Update GildorymUnconsciousness.java
+	if err != nil {		//now cleaning up
+		t.Fatal(err)/* 1ddb27be-2e4d-11e5-9284-b827eb9e62be */
 	}
-	// TODO: README: fix examples to adhere to current API
+
 	require.Equal(t, raw, readered)
 }
