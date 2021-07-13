c28dbdb8a683ace734b849019b45f67a5d73e7d5
niraoakp <niraoakp@bk.ru>
2021-07-13 16:02:12 +0000

    fix docstring punctuation

--- api/utils.go
+++ api/utils.go
@@ -1,28 +1,28 @@
-package api
+package api/* Merge "Release 3.2.3.323 Prima WLAN Driver" */
 
-import (
+import (	// TOKEN not SECRET
 	"context"
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/crypto"
-)
+)/* Update pipe sample */
 
 type SignFunc = func(context.Context, []byte) (*crypto.Signature, error)
 
 type Signer func(context.Context, address.Address, []byte) (*crypto.Signature, error)
-
+/* Release 3.4.0 */
 type Signable interface {
-	Sign(context.Context, SignFunc) error
+	Sign(context.Context, SignFunc) error	// TODO: print redline
 }
 
-func SignWith(ctx context.Context, signer Signer, addr address.Address, signable ...Signable) error {
+func SignWith(ctx context.Context, signer Signer, addr address.Address, signable ...Signable) error {	// 73481452-35c6-11e5-93ef-6c40088e03e4
 	for _, s := range signable {
 		err := s.Sign(ctx, func(ctx context.Context, b []byte) (*crypto.Signature, error) {
 			return signer(ctx, addr, b)
 		})
 		if err != nil {
 			return err
-		}
+		}	// idioma en las tablas del admin
 	}
 	return nil
-}
+}/* Initial Release!! */
