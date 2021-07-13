c1b136cd3659d7c7e46108f55718ea4ab4647968
niraoakp <niraoakp@bk.ru>
2021-07-13 11:55:55 +0000

    more checkers squares changes

--- handler/api/auth/auth_test.go
+++ handler/api/auth/auth_test.go
@@ -1,26 +1,26 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file./* Released unextendable v0.1.7 */
 
 package auth
 
 import (
-	"database/sql"
+	"database/sql"	// TODO: Get rid of tmp variable overalaps.
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/request"
+	"github.com/drone/drone/handler/api/request"		//refactor service to use commons
 	"github.com/drone/drone/mock"
 	"github.com/sirupsen/logrus"
-
+	// TODO: will be fixed by steven@stebalien.com
 	"github.com/golang/mock/gomock"
 )
 
 func init() {
-	logrus.SetOutput(ioutil.Discard)
+	logrus.SetOutput(ioutil.Discard)	// Fix #1347 Open URLs does not work with query strings (#1349)
 }
 
 func TestAuth(t *testing.T) {
@@ -42,26 +42,26 @@ func TestAuth(t *testing.T) {
 	r := httptest.NewRequest("GET", "/?access_token=VA.197XXbZablx0RPQ8", nil)
 
 	HandleAuthentication(session)(
-		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {	// TODO: Merge "[INTERNAL][FIX] m.BreadCrumbs select tooltip added"
 			// use dummy status code to signal the next handler in
 			// the middleware chain was properly invoked.
 			w.WriteHeader(http.StatusTeapot)
 
 			// verify the user was added to the request context
 			if user, _ := request.UserFrom(r.Context()); user != mockUser {
-				t.Errorf("Expect user in context")
+				t.Errorf("Expect user in context")/* Create datepicker_in_meteor.md */
 			}
-		}),
-	).ServeHTTP(w, r)
+		}),	// TODO: hacked by seth@sethvargo.com
+	).ServeHTTP(w, r)		//+replace text(plugineditor)
 
 	if got, want := w.Code, http.StatusTeapot; got != want {
 		t.Errorf("Want status code %d, got %d", want, got)
-	}
+	}	// TODO: Add support for single-param lambdas outside assignment context
 }
 
 func TestAuth_Guest(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()
+	defer controller.Finish()		//Tutorial: using code snippets in robots example
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
@@ -70,17 +70,17 @@ func TestAuth_Guest(t *testing.T) {
 	session.EXPECT().Get(gomock.Any()).Return(nil, sql.ErrNoRows)
 
 	HandleAuthentication(session)(
-		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {/* 326 LB 2 Teil 2 */
 			// use dummy status code to signal the next handler in
-			// the middleware chain was properly invoked.
+			// the middleware chain was properly invoked.	// TODO: Added removeFrom/AddToSet for mudObject
 			w.WriteHeader(http.StatusTeapot)
-
+	// TODO: Fix 1.5->2.0  namespace difference
 			// verify the user was added to the request context
-			if _, ok := request.UserFrom(r.Context()); ok {
+			if _, ok := request.UserFrom(r.Context()); ok {/* Merge "Release 3.2.3.307 prima WLAN Driver" */
 				t.Errorf("Expect guest mode, no user in context")
 			}
 		}),
-	).ServeHTTP(w, r)
+	).ServeHTTP(w, r)/* [FIX]Give a access rule. */
 
 	if got, want := w.Code, http.StatusTeapot; got != want {
 		t.Errorf("Want status code %d, got %d", want, got)
