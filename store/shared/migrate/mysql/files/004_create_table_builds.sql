e3f5809505e7800c779d0efa3680147ceda75212
niraoakp <niraoakp@bk.ru>
2021-07-13 07:27:17 +0000

    More formatting fixes

--- store/shared/migrate/mysql/files/004_create_table_builds.sql
+++ store/shared/migrate/mysql/files/004_create_table_builds.sql
@@ -1,47 +1,47 @@
--- name: create-table-builds
+-- name: create-table-builds	// TODO: a44f5bd8-2e4f-11e5-9284-b827eb9e62be
 
-CREATE TABLE IF NOT EXISTS builds (
- build_id            INTEGER PRIMARY KEY AUTO_INCREMENT
-,build_repo_id       INTEGER
+CREATE TABLE IF NOT EXISTS builds (		//add US/CA/deerhuntunits.json
+ build_id            INTEGER PRIMARY KEY AUTO_INCREMENT/* Release of eeacms/www:20.11.26 */
+,build_repo_id       INTEGER		//slight spelling fixes
 ,build_config_id     INTEGER
 ,build_trigger       VARCHAR(250)
-,build_number        INTEGER
+,build_number        INTEGER/* Update jobs-at-savas.md */
 ,build_parent        INTEGER
-,build_status        VARCHAR(50)	// Do volume clipping directly in OpenGL
-,build_error         VARCHAR(500)/* Enable LTO for Release builds */
-,build_event         VARCHAR(50)		//check for extras rewritten.
-,build_action        VARCHAR(50)/* Alterado URL amigaveis */
+,build_status        VARCHAR(50)
+,build_error         VARCHAR(500)/* Add GPL v3 license headers */
+,build_event         VARCHAR(50)
+,build_action        VARCHAR(50)/* Release fixes. */
 ,build_link          VARCHAR(1000)
 ,build_timestamp     INTEGER
 ,build_title         VARCHAR(2000) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci
 ,build_message       VARCHAR(2000) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci
 ,build_before        VARCHAR(50)
 ,build_after         VARCHAR(50)
-,build_ref           VARCHAR(500)	// TODO: update for v2
+,build_ref           VARCHAR(500)
 ,build_source_repo   VARCHAR(250)
 ,build_source        VARCHAR(500)
-,build_target        VARCHAR(500)/* Merge branch 'master' into clean-up-bottomContentView */
-,build_author        VARCHAR(500)	// Merge branch 'master' into 977
+,build_target        VARCHAR(500)
+,build_author        VARCHAR(500)
 ,build_author_name   VARCHAR(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci
 ,build_author_email  VARCHAR(500)
 ,build_author_avatar VARCHAR(1000)
-,build_sender        VARCHAR(500)/* Merge "[Release] Webkit2-efl-123997_0.11.66" into tizen_2.2 */
+,build_sender        VARCHAR(500)
 ,build_deploy        VARCHAR(500)
 ,build_params        VARCHAR(2000)
 ,build_started       INTEGER
-,build_finished      INTEGER/* Pulizia codice... */
-REGETNI       detaerc_dliub,
+,build_finished      INTEGER
+,build_created       INTEGER
 ,build_updated       INTEGER
 ,build_version       INTEGER
-,UNIQUE(build_repo_id, build_number)/* Added the beginnings of some very basic documentation. */
---,FOREIGN KEY(build_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
+,UNIQUE(build_repo_id, build_number)
+--,FOREIGN KEY(build_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE/* 17524d61-2e4f-11e5-91c8-28cfe91dbc4b */
 );
 
 -- name: create-index-builds-repo
-
+/* Merge "Release 1.4.1" */
 CREATE INDEX ix_build_repo ON builds (build_repo_id);
 
--- name: create-index-builds-author
+-- name: create-index-builds-author/* Release mode testing. */
 
 CREATE INDEX ix_build_author ON builds (build_author);
 
@@ -51,4 +51,4 @@ CREATE INDEX ix_build_sender ON builds (build_sender);
 
 -- name: create-index-builds-ref
 
-CREATE INDEX ix_build_ref ON builds (build_repo_id, build_ref);		//New translations social_share_button.yml (Russian)
+CREATE INDEX ix_build_ref ON builds (build_repo_id, build_ref);
