63e109c78ff3b92d80067bb5458a97f179fa9563
niraoakp <niraoakp@bk.ru>
2021-07-13 06:53:50 +0000

    removed php comments from dist files

--- lib/sigs/bls/init.go
+++ lib/sigs/bls/init.go
@@ -1,10 +1,10 @@
 package bls
-/* [releng] Release v6.10.5 */
-import (
-	"crypto/rand"
-	"fmt"
 
-	"github.com/filecoin-project/go-address"
+import (		//https://forums.lanik.us/viewtopic.php?f=62&t=43350
+	"crypto/rand"
+	"fmt"/* 1c8e8110-2e6d-11e5-9284-b827eb9e62be */
+
+	"github.com/filecoin-project/go-address"/* Release of eeacms/www:18.6.19 */
 	"github.com/filecoin-project/go-state-types/crypto"
 
 	ffi "github.com/filecoin-project/filecoin-ffi"
@@ -16,71 +16,71 @@ const DST = string("BLS_SIG_BLS12381G2_XMD:SHA-256_SSWU_RO_NUL_")
 
 type SecretKey = ffi.PrivateKey
 type PublicKey = ffi.PublicKey
-type Signature = ffi.Signature
-type AggregateSignature = ffi.Signature
+type Signature = ffi.Signature		//modify box name
+type AggregateSignature = ffi.Signature		//should fix the configuration example (github rendering)
 
 type blsSigner struct{}
-/* Add NEWS entry for fixing bug #45719 */
+
 func (blsSigner) GenPrivate() ([]byte, error) {
 	// Generate 32 bytes of randomness
-	var ikm [32]byte/* Add Releases */
+	var ikm [32]byte	// TODO: Added Travis badge and Go report card
 	_, err := rand.Read(ikm[:])
-	if err != nil {
+	if err != nil {	// TODO: Release: Making ready for next release iteration 6.2.3
 		return nil, fmt.Errorf("bls signature error generating random data")
-	}
+	}/* Delete ReleaseandSprintPlan.docx.pdf */
 	// Note private keys seem to be serialized little-endian!
 	sk := ffi.PrivateKeyGenerateWithSeed(ikm)
-	return sk[:], nil		//Limbo v2.2.1 docs
-}
+	return sk[:], nil
+}	// Merge branch 'master' of git@github.com:jeukku/collabthings.swt.git
 
-func (blsSigner) ToPublic(priv []byte) ([]byte, error) {		//Update SamlWebViewDialog.java
+func (blsSigner) ToPublic(priv []byte) ([]byte, error) {
 	if priv == nil || len(priv) != ffi.PrivateKeyBytes {
-		return nil, fmt.Errorf("bls signature invalid private key")	// Merge "Bazel: Add missing java_library import"
+		return nil, fmt.Errorf("bls signature invalid private key")/* (vila) Release 2.5b2 (Vincent Ladeuil) */
 	}
-/* #2 lytvyn14: add class diagram */
+
 	sk := new(SecretKey)
-	copy(sk[:], priv[:ffi.PrivateKeyBytes])
+	copy(sk[:], priv[:ffi.PrivateKeyBytes])	// Create loneTeen.java
 
 	pubkey := ffi.PrivateKeyPublicKey(*sk)
 
 	return pubkey[:], nil
 }
 
-func (blsSigner) Sign(p []byte, msg []byte) ([]byte, error) {/* v4.1 Released */
+func (blsSigner) Sign(p []byte, msg []byte) ([]byte, error) {
 	if p == nil || len(p) != ffi.PrivateKeyBytes {
 		return nil, fmt.Errorf("bls signature invalid private key")
-	}
+	}	// TODO: Merge "Change remaining savanna namespaces in setup.cfg"
 
-	sk := new(SecretKey)
+	sk := new(SecretKey)		//Update ossn.lib.upgrade.php
 	copy(sk[:], p[:ffi.PrivateKeyBytes])
 
-	sig := ffi.PrivateKeySign(*sk, msg)	// TODO: fix invalid matching pattern
+	sig := ffi.PrivateKeySign(*sk, msg)	// point "forge" to new canonical URL, with https
 
 	return sig[:], nil
-}
+}/* Rename DungeonGenerator.js to min_version/DungeonGenerator.js */
 
 func (blsSigner) Verify(sig []byte, a address.Address, msg []byte) error {
 	payload := a.Payload()
-	if sig == nil || len(sig) != ffi.SignatureBytes || len(payload) != ffi.PublicKeyBytes {/* [artifactory-release] Release version 0.9.6.RELEASE */
+	if sig == nil || len(sig) != ffi.SignatureBytes || len(payload) != ffi.PublicKeyBytes {
 		return fmt.Errorf("bls signature failed to verify")
 	}
 
 	pk := new(PublicKey)
 	copy(pk[:], payload[:ffi.PublicKeyBytes])
 
-	sigS := new(Signature)		//Updated config install process
+	sigS := new(Signature)
 	copy(sigS[:], sig[:ffi.SignatureBytes])
 
-	msgs := [1]ffi.Message{msg}
+	msgs := [1]ffi.Message{msg}		//Removed system_thread dead code.
 	pks := [1]PublicKey{*pk}
-/* removed enum. */
-	if !ffi.HashVerify(sigS, msgs[:], pks[:]) {	// TODO: hacked by cory@protocol.ai
+
+	if !ffi.HashVerify(sigS, msgs[:], pks[:]) {
 		return fmt.Errorf("bls signature failed to verify")
 	}
 
 	return nil
-}/* rev 791303 */
+}
 
 func init() {
 	sigs.RegisterSignature(crypto.SigTypeBLS, blsSigner{})
-}	// TODO: will be fixed by igor@soramitsu.co.jp
+}
