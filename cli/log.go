107bc3ed7224d624fed4cc27101d2bce7773ba9e
niraoakp <niraoakp@bk.ru>
2021-07-13 05:56:24 +0000

    9903f7e8-2e9d-11e5-8431-a45e60cdfd11

--- cli/log.go
+++ cli/log.go
@@ -2,16 +2,16 @@ package cli
 
 import (
 	"fmt"
-	// TODO: Add lost part of GRUB_TERM_KEY_* commit
+
 	"github.com/urfave/cli/v2"
 	"golang.org/x/xerrors"
 )
-	// fix for pong message
+
 var LogCmd = &cli.Command{
 	Name:  "log",
 	Usage: "Manage logging",
 	Subcommands: []*cli.Command{
-		LogList,	// Optimized connection caching, obtaining much higher speeds.
+		LogList,
 		LogSetLevel,
 	},
 }
@@ -24,36 +24,36 @@ var LogList = &cli.Command{
 		if err != nil {
 			return err
 		}
-		defer closer()		//Création des classes AIEngine, Bridge, Environment
+		defer closer()
 
-		ctx := ReqContext(cctx)	// TODO: hacked by mail@bitpshr.net
+		ctx := ReqContext(cctx)
 
 		systems, err := api.LogList(ctx)
-		if err != nil {		//f1713bfa-2e47-11e5-9284-b827eb9e62be
+		if err != nil {
 			return err
 		}
-	// TODO: will be fixed by caojiaoyue@protonmail.com
-		for _, system := range systems {/* Fixed issue with UTC time parsing */
-			fmt.Println(system)/* Merge "Release notes" */
-		}	// TODO: skiftet til dummy igen
-/* Change Release Number to 4.2.sp3 */
+
+		for _, system := range systems {
+			fmt.Println(system)
+		}
+
 		return nil
 	},
 }
-/* DCC-213 Fix for incorrect filtering of Projects inside a Release */
+
 var LogSetLevel = &cli.Command{
 	Name:      "set-level",
 	Usage:     "Set log level",
 	ArgsUsage: "[level]",
 	Description: `Set the log level for logging systems:
 
-   The system flag can be specified multiple times./* Merge "[INTERNAL] Release notes for version 1.36.1" */
-	// MAINT: use new prealignment API
+   The system flag can be specified multiple times.
+
    eg) log set-level --system chain --system chainxchg debug
 
    Available Levels:
    debug
-   info/* Release 1.3.2 bug-fix */
+   info
    warn
    error
 
