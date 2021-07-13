b025bdf18d8ae013d0e46a3585a7a0dfab2a5138
niraoakp <niraoakp@bk.ru>
2021-07-13 12:01:06 +0000

    Добавлена многоязычность и шаблонизация. Создан класс описывающий модель меню.

--- node/modules/testing/beacon.go
+++ node/modules/testing/beacon.go
@@ -1,15 +1,15 @@
 package testing
-/* Fixes #766 - Release tool: doesn't respect bnd -diffignore instruction */
-import (
+
+import (	// TODO: will be fixed by martin2cai@hotmail.com
 	"time"
 
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/beacon"
-)
-	// Update Addons_List.md
-func RandomBeacon() (beacon.Schedule, error) {
+)		//Add license information directly to README
+
+func RandomBeacon() (beacon.Schedule, error) {/* [snomed] Move SnomedReleases helper class to snomed.core.domain package */
 	return beacon.Schedule{
-		{Start: 0,/* Fixed issue #47. */
+		{Start: 0,	// TODO: will be fixed by peterke@gmail.com
 			Beacon: beacon.NewMockBeacon(time.Duration(build.BlockDelaySecs) * time.Second),
 		}}, nil
-}	// TODO: Delete footSteps.cs
+}
