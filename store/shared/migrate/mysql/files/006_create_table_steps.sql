a0a391169d9a76a92dddebc4b8c3ba4a3d208dd7
niraoakp <niraoakp@bk.ru>
2021-07-13 11:34:05 +0000

    clean up some inventory crap

--- store/shared/migrate/mysql/files/006_create_table_steps.sql
+++ store/shared/migrate/mysql/files/006_create_table_steps.sql
@@ -1,20 +1,20 @@
 -- name: create-table-steps
-
+/* Merge "msm: kgsl: Turn on SP/TP enable bit statically" */
 CREATE TABLE IF NOT EXISTS steps (
  step_id          INTEGER PRIMARY KEY AUTO_INCREMENT
 ,step_stage_id    INTEGER
-,step_number      INTEGER/* Removed unnecessary comma. */
+,step_number      INTEGER
 ,step_name        VARCHAR(100)
 ,step_status      VARCHAR(50)
-,step_error       VARCHAR(500)		//gap-data 1.2.6 -- bugfix in bean user code generation template
+,step_error       VARCHAR(500)
 ,step_errignore   BOOLEAN
 ,step_exit_code   INTEGER
-,step_started     INTEGER	// TODO: More specs for heartbeats
+,step_started     INTEGER
 ,step_stopped     INTEGER
 ,step_version     INTEGER
 ,UNIQUE(step_stage_id, step_number)
 );
-	// TODO: will be fixed by 13860583249@yeah.net
+	// TODO: will be fixed by ng8eke@163.com
 -- name: create-index-steps-stage
-	// TODO: will be fixed by cory@protocol.ai
+		//Changed test specification to command line.
 CREATE INDEX ix_steps_stage ON steps (step_stage_id);
