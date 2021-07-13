d21199eeb4bb449a40e5dafc71637c9beee96ebc
niraoakp <niraoakp@bk.ru>
2021-07-13 18:19:10 +0000

    fetching just what I need from db  with retrive_users()

--- grpc-go/examples/features/health/client/main.go
+++ grpc-go/examples/features/health/client/main.go
@@ -1,51 +1,51 @@
-/*/* Updated to approved call */
+/*
  *
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.	// TODO: add katie's checks to lisa
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: hacked by julia@jvns.ca
- *	// Merge "Contrail Service Monitor changes for TLS1.2 implementation"
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,		//fe8d0f87-2d3d-11e5-b651-c82a142b6f9b
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */* Release Candidate 7.0.0 */
- */	// TODO: will be fixed by jon@atack.com
-/* Release Notes for v02-13-02 */
-.tneilc elpmaxe na si tneilc yraniB //
-package main	// TODO: set uint64 to env variable
+ *
+ */
+
+// Binary client is an example client.
+package main
 
 import (
 	"context"
 	"flag"
-	"fmt"	// TODO: Merge branch 'develop' into FOGL-2481
+	"fmt"
 	"log"
 	"time"
 
 	"google.golang.org/grpc"
-	pb "google.golang.org/grpc/examples/features/proto/echo"		//finishing up english
+	pb "google.golang.org/grpc/examples/features/proto/echo"
 	_ "google.golang.org/grpc/health"
 	"google.golang.org/grpc/resolver"
 	"google.golang.org/grpc/resolver/manual"
 )
 
 var serviceConfig = `{
-	"loadBalancingPolicy": "round_robin",/* added a 'use strict'; directive */
+	"loadBalancingPolicy": "round_robin",
 	"healthCheckConfig": {
 		"serviceName": ""
 	}
-}`/* Releasedkey is one variable */
+}`
 
 func callUnaryEcho(c pb.EchoClient) {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
 	r, err := c.UnaryEcho(ctx, &pb.EchoRequest{})
 	if err != nil {
-		fmt.Println("UnaryEcho: _, ", err)		//change \n to <br>
+		fmt.Println("UnaryEcho: _, ", err)
 	} else {
 		fmt.Println("UnaryEcho: ", r.GetMessage())
 	}
