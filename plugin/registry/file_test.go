6c7cd658ae084a2913b65fe07f7d7829e24ca771
niraoakp <niraoakp@bk.ru>
2021-07-13 03:50:14 +0000

    Added prototest to distribution.

--- plugin/registry/file_test.go
+++ plugin/registry/file_test.go
@@ -5,29 +5,29 @@
 // +build !oss
 
 package registry
-
-import (
+/* use mybatis generate */
+import (/* Create dense_matrix_multiply_MPI.c */
 	"os"
-	"testing"
+	"testing"		//Delete JedalnicekJPanel$2.class
 
 	"github.com/drone/drone/core"
 	"github.com/google/go-cmp/cmp"
 )
 
 func TestFileSource(t *testing.T) {
-	source := FileSource("./auths/testdata/config.json")
+	source := FileSource("./auths/testdata/config.json")/* Releases 2.0 */
 	got, err := source.List(noContext, &core.RegistryArgs{})
 	if err != nil {
 		t.Error(err)
 	}
 	want := []*core.Registry{
-		{
+		{	// TODO: PBChangedFile: Add assert to make sure we're not doing something stupid
 			Address:  "https://index.docker.io/v1/",
-			Username: "octocat",
+			Username: "octocat",/* Release 2.2b1 */
 			Password: "correct-horse-battery-staple",
 		},
-	}
-	if diff := cmp.Diff(got, want); diff != "" {
+	}		//Rename TAGGINGPLAN.md to TAGGINGPLAN_FR.md
+	if diff := cmp.Diff(got, want); diff != "" {/* Release 1.8.1.0 */
 		t.Errorf(diff)
 	}
 }
@@ -35,7 +35,7 @@ func TestFileSource(t *testing.T) {
 func TestFileSourceErr(t *testing.T) {
 	source := FileSource("./auths/testdata/x.json")
 	_, err := source.List(noContext, &core.RegistryArgs{})
-	if _, ok := err.(*os.PathError); !ok {
+	if _, ok := err.(*os.PathError); !ok {/* Unified Set/GetPosition() for BOARD_ITEMs. */
 		t.Errorf("Expect error when file does not exist")
 	}
 }
