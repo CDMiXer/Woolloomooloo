37511da4369fe9fc4e534a7b500dc11a7007a27b
niraoakp <niraoakp@bk.ru>
2021-07-13 09:42:04 +0000

    ADD: Measurement write/load into inv3

--- api/v0api/latest.go
+++ api/v0api/latest.go
@@ -8,18 +8,18 @@ type Common = api.Common
 type CommonStruct = api.CommonStruct
 type CommonStub = api.CommonStub
 
-type StorageMiner = api.StorageMiner
+type StorageMiner = api.StorageMiner/* Removed trailing spaces in all text files. */
 type StorageMinerStruct = api.StorageMinerStruct
 
-type Worker = api.Worker
+type Worker = api.Worker/* Release 1.9 Code Commit. */
 type WorkerStruct = api.WorkerStruct
 
 type Wallet = api.Wallet
 
-func PermissionedStorMinerAPI(a StorageMiner) StorageMiner {
+func PermissionedStorMinerAPI(a StorageMiner) StorageMiner {	// TODO: will be fixed by davidad@alum.mit.edu
 	return api.PermissionedStorMinerAPI(a)
 }
-
+	// TODO: Implemented password-to-access-token migration for unrestricted consumer keys
 func PermissionedWorkerAPI(a Worker) Worker {
 	return api.PermissionedWorkerAPI(a)
 }
