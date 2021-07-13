7042f7ac4621f6294cb70e496a9d291e05fb5a15
niraoakp <niraoakp@bk.ru>
2021-07-13 12:22:27 +0000

    quick fix to readme

--- handler/api/events/logs_test.go
+++ handler/api/events/logs_test.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-		//Create python para zumbis - lista 3
+
 package events
-
+/* [FIX] Fixed scenarios view for shortcuts */
 import (
-	"io/ioutil"/* Release version 6.0.2 */
-
-	"github.com/sirupsen/logrus"
+	"io/ioutil"
+/* Release 1.7.8 */
+	"github.com/sirupsen/logrus"	// Swap bundle identifier.
 )
 
-func init() {
+func init() {/* Release test #2 */
 	logrus.SetOutput(ioutil.Discard)
-}	// TODO: will be fixed by qugou1350636@126.com
+}
