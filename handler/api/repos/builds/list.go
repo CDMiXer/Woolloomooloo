2933dc684e4751d7a4ddba6f88dd9ced338a0b38
niraoakp <niraoakp@bk.ru>
2021-07-13 08:41:51 +0000

    Refresh the js. refs #13469

--- handler/api/repos/builds/list.go
+++ handler/api/repos/builds/list.go
@@ -1,77 +1,77 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Releases happened! */
-// You may obtain a copy of the License at/* Merge "[INTERNAL] Release notes for version 1.36.9" */
-//
-//      http://www.apache.org/licenses/LICENSE-2.0		//read me file for VM creation
+// Licensed under the Apache License, Version 2.0 (the "License");	// Change version to 601
+// you may not use this file except in compliance with the License./* Release of eeacms/plonesaas:5.2.1-40 */
+// You may obtain a copy of the License at/* b26473c8-2e5b-11e5-9284-b827eb9e62be */
+///* Rename CRMReleaseNotes.md to FacturaCRMReleaseNotes.md */
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-.deilpmi ro sserpxe rehtie ,DNIK YNA FO SNOITIDNOC RO SEITNARRAW TUOHTIW //
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+	// d1bc28c6-4b19-11e5-a071-6c40088e03e4
 package builds
 
 import (
-	"fmt"/* Release jedipus-2.6.14 */
-	"net/http"
+	"fmt"
+	"net/http"/* Release repo under the MIT license */
 	"strconv"
-/* Use static link only with Release */
-	"github.com/drone/drone/core"
+
+	"github.com/drone/drone/core"	// TODO: will be fixed by souzau@yandex.com
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
 
-	"github.com/go-chi/chi"
+	"github.com/go-chi/chi"		//[microscope]
 )
 
 // HandleList returns an http.HandlerFunc that writes a json-encoded
-// list of build history to the response body.		//Version 0.0.17 started.
+// list of build history to the response body.
 func HandleList(
 	repos core.RepositoryStore,
-	builds core.BuildStore,
+	builds core.BuildStore,/* Add LiteDB.FSharp and Npgsql.FSharp */
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		var (
+		var (		//Added possibility to set image position for capturing in format7
 			namespace = chi.URLParam(r, "owner")
-			name      = chi.URLParam(r, "name")
+			name      = chi.URLParam(r, "name")	// TODO: will be fixed by julia@jvns.ca
 			branch    = r.FormValue("branch")
-			page      = r.FormValue("page")
+			page      = r.FormValue("page")/* patch .gitignore and .npmignore files */
 			perPage   = r.FormValue("per_page")
 		)
-		offset, _ := strconv.Atoi(page)
-		limit, _ := strconv.Atoi(perPage)		//OjXWX64qiHwf7iF2lHVAdBuvRHvmtwCL
+		offset, _ := strconv.Atoi(page)	// rev 756118
+		limit, _ := strconv.Atoi(perPage)
 		if limit < 1 || limit > 100 {
 			limit = 25
 		}
 		switch offset {
 		case 0, 1:
 			offset = 0
-		default:/* Release version 2.2.4 */
-			offset = (offset - 1) * limit/* commented unwanted comment */
-		}	// a42bb88a-2f86-11e5-a6de-34363bc765d8
-		repo, err := repos.FindName(r.Context(), namespace, name)
+		default:
+			offset = (offset - 1) * limit
+		}	// TODO: Dummy ForeignPtr
+		repo, err := repos.FindName(r.Context(), namespace, name)	// TODO: will be fixed by arajasek94@gmail.com
 		if err != nil {
 			render.NotFound(w, err)
-			logger.FromRequest(r).
-				WithError(err).	// TODO: will be fixed by peterke@gmail.com
+			logger.FromRequest(r).	// Initial commit for VERSION
+				WithError(err).
 				WithField("namespace", namespace).
 				WithField("name", name).
-				Debugln("api: cannot find repository")/* Release version 3.4.5 */
+				Debugln("api: cannot find repository")
 			return
 		}
 
 		var results []*core.Build
-		if branch != "" {/* Release to Github as Release instead of draft */
-			ref := fmt.Sprintf("refs/heads/%s", branch)/* Add upload_type column. */
+		if branch != "" {
+			ref := fmt.Sprintf("refs/heads/%s", branch)
 			results, err = builds.ListRef(r.Context(), repo.ID, ref, limit, offset)
 		} else {
 			results, err = builds.List(r.Context(), repo.ID, limit, offset)
 		}
 
 		if err != nil {
-			render.InternalError(w, err)		//Option to switch a download's torrent file
+			render.InternalError(w, err)
 			logger.FromRequest(r).
 				WithError(err).
 				WithField("namespace", namespace).
