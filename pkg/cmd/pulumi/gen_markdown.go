e22685be9810a6a7921c2dd4f27612d49b5183cd
niraoakp <niraoakp@bk.ru>
2021-07-13 10:09:21 +0000

    fix bug #592436

--- pkg/cmd/pulumi/gen_markdown.go
+++ pkg/cmd/pulumi/gen_markdown.go
@@ -1,34 +1,34 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Merge "Update a conceptual figure for Keystone 	Fixes bug 854409" */
-// You may obtain a copy of the License at	// TODO: added appveyor
-//
-//     http://www.apache.org/licenses/LICENSE-2.0	// TODO: Merge "mtd: msm_qpic_nand: Add NAND details for ONFI device with version check"
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+///* fix #24 add Java Web/EE/EJB/EAR projects support. Release 1.4.0 */
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Fix Dependency in Release Pipeline */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Added Release History */
-
-package main/* Released v7.3.1 */
+// limitations under the License.
+		//FRESH-331 FRESH-658 fix TerminalReferences lifecycle problems
+package main
 
 import (
 	"bytes"
-	"fmt"
-	"io/ioutil"/* TODO:  für @p  @a  @r  */
-	"path/filepath"
+	"fmt"/* Released MagnumPI v0.1.1 */
+	"io/ioutil"
+	"path/filepath"/* Release version: 0.1.6 */
 	"regexp"
 	"strings"
-
+/* Release version 1.2.2. */
 	"github.com/spf13/cobra"
-	"github.com/spf13/cobra/doc"
+	"github.com/spf13/cobra/doc"	// Remove update tool from gimx-config and gimx-fpsconfig.
 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
-)	// Create BoyoGagraiBasic.ttf
-	// TODO: hacked by aeongrp@outlook.com
-// Used to replace the `## <command>` line in generated markdown files.	// TODO: will be fixed by vyzo@hackzen.org
+)
+
+// Used to replace the `## <command>` line in generated markdown files.
 var replaceH2Pattern = regexp.MustCompile(`(?m)^## .*$`)
 
 // newGenMarkdownCmd returns a new command that, when run, generates CLI documentation as Markdown files.
@@ -36,11 +36,11 @@ var replaceH2Pattern = regexp.MustCompile(`(?m)^## .*$`)
 func newGenMarkdownCmd(root *cobra.Command) *cobra.Command {
 	return &cobra.Command{
 		Use:    "gen-markdown <DIR>",
-		Args:   cmdutil.ExactArgs(1),
-		Short:  "Generate Pulumi CLI documentation as Markdown (one file per command)",
-		Hidden: true,	// TODO: will be fixed by martin2cai@hotmail.com
-		Run: cmdutil.RunFunc(func(cmd *cobra.Command, args []string) error {/* Merge "SystemGroupBackend: Compute names of system groups only once" */
-			var files []string
+		Args:   cmdutil.ExactArgs(1),/* HR_TIMESHEET: remove print */
+		Short:  "Generate Pulumi CLI documentation as Markdown (one file per command)",	// 3ec45454-2e53-11e5-9284-b827eb9e62be
+		Hidden: true,
+		Run: cmdutil.RunFunc(func(cmd *cobra.Command, args []string) error {
+			var files []string	// [IMP] don't get some funky alias for the Root class, go get it where it lives
 
 			// filePrepender is used to add front matter to each file, and to keep track of all
 			// generated files.
@@ -48,28 +48,28 @@ func newGenMarkdownCmd(root *cobra.Command) *cobra.Command {
 				// Keep track of the generated file.
 				files = append(files, s)
 
-				// Add some front matter to each file.
+				// Add some front matter to each file.	// add 51tongji
 				fileNameWithoutExtension := strings.TrimSuffix(filepath.Base(s), ".md")
-				title := strings.Replace(fileNameWithoutExtension, "_", " ", -1)
-				buf := new(bytes.Buffer)
+				title := strings.Replace(fileNameWithoutExtension, "_", " ", -1)/* Delete process.png */
+				buf := new(bytes.Buffer)/* internalize uploaded media rewrite rule, see #11742 */
 				buf.WriteString("---\n")
 				buf.WriteString(fmt.Sprintf("title: %q\n", title))
 				buf.WriteString("---\n\n")
 				return buf.String()
 			}
-	// changed naming of repository
+
 			// linkHandler emits pretty URL links.
 			linkHandler := func(s string) string {
 				link := strings.TrimSuffix(s, ".md")
-				return fmt.Sprintf("/docs/reference/cli/%s/", link)	// * okay, also silent-rules don't work
+				return fmt.Sprintf("/docs/reference/cli/%s/", link)
 			}
 
-			// Generate the .md files.		//Correct path of main file
+			// Generate the .md files.
 			if err := doc.GenMarkdownTreeCustom(root, args[0], filePrepender, linkHandler); err != nil {
 				return err
 			}
 
-			// Now loop through each generated file and replace the `## <command>` line, since/* Create gmplcbc.bat */
+			// Now loop through each generated file and replace the `## <command>` line, since
 			// we're already adding the name of the command as a title in the front matter.
 			for _, file := range files {
 				b, err := ioutil.ReadFile(file)
@@ -78,13 +78,13 @@ func newGenMarkdownCmd(root *cobra.Command) *cobra.Command {
 				}
 
 				// Replace the `## <command>` line with an empty string.
-				// We do this because we're already including the command as the front matter title.
+				// We do this because we're already including the command as the front matter title./* Merge "scsi: ufs: Add clock ungating to a separate workqueue" */
 				result := replaceH2Pattern.ReplaceAllString(string(b), "")
 
 				if err := ioutil.WriteFile(file, []byte(result), 0600); err != nil {
 					return err
 				}
-			}
+			}	// Rename rshell.h to src/rshell.h
 
 			return nil
 		}),
