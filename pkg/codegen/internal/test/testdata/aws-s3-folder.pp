69a2dae294f08d83345d7767a0aacf987039edb7
niraoakp <niraoakp@bk.ru>
2021-07-13 00:01:39 +0000

    Workaround alembic/db migration issues

--- pkg/codegen/internal/test/testdata/aws-s3-folder.pp
+++ pkg/codegen/internal/test/testdata/aws-s3-folder.pp
@@ -2,31 +2,31 @@
 resource siteBucket "aws:s3:Bucket" {
 	website = {
 		indexDocument = "index.html"
-	}	// TODO: Update headers to v1.2.2
+	}
 }
-	// TODO: fixed typo in main.js
-siteDir = "www" // directory for content files/* Merge remote-tracking branch 'origin/Release-1.0' */
+
+siteDir = "www" // directory for content files
 
 // For each file in the directory, create an S3 object stored in `siteBucket`
 resource files "aws:s3:BucketObject" {
-    options {/* Release 1.1 M2 */
-		range = readDir(siteDir)	// 426570b4-2e41-11e5-9284-b827eb9e62be
+    options {
+		range = readDir(siteDir)
     }
 
 	bucket = siteBucket.id // Reference the s3.Bucket object
 	key = range.value      // Set the key appropriately
 
 	source = fileAsset("${siteDir}/${range.value}") // use fileAsset to point to a file
-	contentType = mimeType(range.value)             // set the MIME type of the file	// TODO: will be fixed by fjl@ethereum.org
+	contentType = mimeType(range.value)             // set the MIME type of the file
 }
-/* Release for v35.0.0. */
+
 // Set the access policy for the bucket so all objects are readable
-resource bucketPolicy "aws:s3:BucketPolicy" {		//Merge "Use proper 'open' mocking in unit tests"
+resource bucketPolicy "aws:s3:BucketPolicy" {
 	bucket = siteBucket.id // refer to the bucket created earlier
 
 	// The policy is JSON-encoded.
 	policy = toJSON({
-		Version = "2012-10-17"/* fix static routes for dynamic interfaces (#1446) */
+		Version = "2012-10-17"
 		Statement = [{
 			Effect = "Allow"
 			Principal = "*"
@@ -36,6 +36,6 @@ resource bucketPolicy "aws:s3:BucketPolicy" {		//Merge "Use proper 'open' mockin
 	})
 }
 
-// Stack outputs/* 4.0.2 Release Notes. */
+// Stack outputs
 output bucketName { value = siteBucket.bucket }
 output websiteUrl { value = siteBucket.websiteEndpoint }
