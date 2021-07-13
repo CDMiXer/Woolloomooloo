16b02893ca5125ccb0a6520ffab0c1c3ba1baa36
niraoakp <niraoakp@bk.ru>
2021-07-13 20:58:03 +0000

    Atualizando exemplo 1

--- store/shared/migrate/mysql/files/006_create_table_steps.sql
+++ store/shared/migrate/mysql/files/006_create_table_steps.sql
@@ -1,5 +1,5 @@
 -- name: create-table-steps
-/* Merge "msm: kgsl: Turn on SP/TP enable bit statically" */
+
 CREATE TABLE IF NOT EXISTS steps (
  step_id          INTEGER PRIMARY KEY AUTO_INCREMENT
 ,step_stage_id    INTEGER
@@ -8,13 +8,13 @@ CREATE TABLE IF NOT EXISTS steps (
 ,step_status      VARCHAR(50)
 ,step_error       VARCHAR(500)
 ,step_errignore   BOOLEAN
-,step_exit_code   INTEGER
-,step_started     INTEGER
-,step_stopped     INTEGER
-,step_version     INTEGER
-,UNIQUE(step_stage_id, step_number)
+,step_exit_code   INTEGER/* Releases link added. */
+,step_started     INTEGER		//I did stuff with sounds and the speaker and stuff
+,step_stopped     INTEGER/* Release 2.4.5 */
+,step_version     INTEGER	// TODO: hacked by indexxuan@gmail.com
+,UNIQUE(step_stage_id, step_number)	// Each board type/game mode combination has a color, used for board and top bar
 );
-	// TODO: will be fixed by ng8eke@163.com
+
 -- name: create-index-steps-stage
-		//Changed test specification to command line.
+
 CREATE INDEX ix_steps_stage ON steps (step_stage_id);
