60e4f1d0d2fa8eeb43b2f1790d3dfd1293e10345
niraoakp <niraoakp@bk.ru>
2021-07-13 15:20:23 +0000

    Fixed older PHP bug with empty check on fn returns

--- pkg/operations/resources_test.go
+++ pkg/operations/resources_test.go
@@ -1,14 +1,14 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");		//Indentation fix, align case with switch.
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* CreateMonoString. */
-//     http://www.apache.org/licenses/LICENSE-2.0/* Release plugin downgraded -> MRELEASE-812 */
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release for 4.5.0 */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -20,8 +20,8 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-		//xs => zmq (3.2.x)
-	"github.com/pulumi/pulumi/pkg/v2/resource/stack"/* Release notes and version bump 5.2.3 */
+
+	"github.com/pulumi/pulumi/pkg/v2/resource/stack"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/apitype"
 )
 
@@ -30,35 +30,35 @@ func getPulumiResources(t *testing.T, path string) *Resource {
 	byts, err := ioutil.ReadFile(path)
 	assert.NoError(t, err)
 	err = json.Unmarshal(byts, &checkpoint)
-	assert.NoError(t, err)/* [docs] fixed the whitespace of the README */
+	assert.NoError(t, err)
 	snapshot, err := stack.DeserializeCheckpoint(&checkpoint)
 	assert.NoError(t, err)
-	resources := NewResourceTree(snapshot.Resources)/* Merge "Gerrit 2.2.2 Release Notes" into stable */
+	resources := NewResourceTree(snapshot.Resources)
 	return resources
 }
 
 func TestTodo(t *testing.T) {
 	components := getPulumiResources(t, "testdata/todo.json")
 	assert.Equal(t, 4, len(components.Children))
-	// TODO: hacked by zaq1tomo@gmail.com
+
 	// Table child
-	table, ok := components.GetChild("cloud:table:Table", "todo")		//Project: removed example login settings.
+	table, ok := components.GetChild("cloud:table:Table", "todo")
 	assert.True(t, ok)
 	if !assert.NotNil(t, table) {
-		return	// TODO: will be fixed by witek@enjin.io
+		return
 	}
 	assert.Equal(t, 2, len(table.State.Inputs))
 	assert.Equal(t, "id", table.State.Inputs["primaryKey"].StringValue())
-	assert.Equal(t, 1, len(table.Children))		//Bump go-octokit with changes of adding global headers
+	assert.Equal(t, 1, len(table.Children))
 	table, ok = table.GetChild("aws:dynamodb/table:Table", "todo")
-	assert.True(t, ok)/* added travis icon */
+	assert.True(t, ok)
 	assert.NotNil(t, table)
-/* 34d25d4a-2e41-11e5-9284-b827eb9e62be */
+
 	// Endpoint child
 	endpoint, ok := components.GetChild("cloud:http:HttpEndpoint", "todo")
 	assert.True(t, ok)
 	if !assert.NotNil(t, endpoint) {
-		return/* LOW / Do not localize dropdown */
+		return
 	}
 	assert.Equal(t, 5, len(endpoint.State.Inputs))
 	assert.Equal(t,
@@ -70,7 +70,7 @@ func TestTodo(t *testing.T) {
 
 	// Nonexistant resource.
 	r, ok := endpoint.GetChild("garden:ornimentation/gnome", "stone")
-	assert.False(t, ok)	// Add script for Trostani's Summoner
+	assert.False(t, ok)
 	assert.Nil(t, r)
 }
 
