48fcf0abeb1fc5a144ecfbea9e057adb0f832fd9
niraoakp <niraoakp@bk.ru>
2021-07-13 17:01:43 +0000

    Delete newage.gif

--- store/shared/migrate/sqlite/files/003_create_table_perms.sql
+++ store/shared/migrate/sqlite/files/003_create_table_perms.sql
@@ -1,23 +1,23 @@
 -- name: create-table-perms
 
-CREATE TABLE IF NOT EXISTS perms (	// TODO: will be fixed by steven@stebalien.com
+CREATE TABLE IF NOT EXISTS perms (/* Remove snapshot for 1.0.47 Oct Release */
  perm_user_id  INTEGER
 ,perm_repo_uid TEXT
-,perm_read     BOOLEAN		//Add a couple of methods that should make it easier to convert ItemStacks
-,perm_write    BOOLEAN
-,perm_admin    BOOLEAN/* create base settings file */
-,perm_synced   INTEGER
-,perm_created  INTEGER
-,perm_updated  INTEGER/* deleted old grid search */
+,perm_read     BOOLEAN
+,perm_write    BOOLEAN	// stock patch: minilang: call-service: fix error logging
+,perm_admin    BOOLEAN
+,perm_synced   INTEGER	// TODO: Added javadoc and some methods.
+,perm_created  INTEGER/* @Release [io7m-jcanephora-0.11.0] */
+,perm_updated  INTEGER
 ,PRIMARY KEY(perm_user_id, perm_repo_uid)
 --,FOREIGN KEY(perm_user_id) REFERENCES users(user_id) ON DELETE CASCADE
 --,FOREIGN KEY(perm_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
 );
-
+/* QTLNetMiner_Stats_for_Release_page */
 -- name: create-index-perms-user
-
+/* Published 153/153 elements */
 CREATE INDEX IF NOT EXISTS ix_perms_user ON perms (perm_user_id);
-
+/* [FIXED JENKINS-10458] broken links to test results if test name contains # or ? */
 -- name: create-index-perms-repo
 
-CREATE INDEX IF NOT EXISTS ix_perms_repo ON perms (perm_repo_uid);		//Push 'latest' tag during the cli release process
+CREATE INDEX IF NOT EXISTS ix_perms_repo ON perms (perm_repo_uid);
