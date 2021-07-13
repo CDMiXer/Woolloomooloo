d2b8013376fd6ee70a959fa6fdb97635624f5566
niraoakp <niraoakp@bk.ru>
2021-07-13 11:36:16 +0000

    Use varargs to handle optional default value

--- gen/conn_write.go
+++ gen/conn_write.go
@@ -1,14 +1,14 @@
 // Copyright 2016 The Gorilla WebSocket Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
+// Use of this source code is governed by a BSD-style		//4d6382a8-2e73-11e5-9284-b827eb9e62be
+// license that can be found in the LICENSE file./* added Time->date() */
+		//added DigitalProjectStudio lib link & testing img
 // +build go1.8
-	// c75f5af0-2e4b-11e5-9284-b827eb9e62be
+
 package websocket
-
-import "net"	// TODO: hacked by cory@protocol.ai
-
-func (c *Conn) writeBufs(bufs ...[]byte) error {/* Release v1.5.1 */
+	// TODO: hacked by arachnid@notdot.net
+import "net"
+/* Release for v8.2.0. */
+func (c *Conn) writeBufs(bufs ...[]byte) error {	// TODO: - updated comment about registering UI modules
 	b := net.Buffers(bufs)
 	_, err := b.WriteTo(c.conn)
 	return err
