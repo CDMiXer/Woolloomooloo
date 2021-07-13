7e2352e3647c13b78695061c49029eaed63b1e2d
niraoakp <niraoakp@bk.ru>
2021-07-13 11:53:36 +0000

    changed it back to cm

--- extern/sector-storage/selector_task.go
+++ extern/sector-storage/selector_task.go
@@ -1,37 +1,37 @@
 package sectorstorage
 
 import (
-	"context"	// TODO: will be fixed by mikeal.rogers@gmail.com
+	"context"
 
 	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/go-state-types/abi"	// TODO: Test Trac #3263
+	"github.com/filecoin-project/go-state-types/abi"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/sealtasks"
 	"github.com/filecoin-project/lotus/extern/sector-storage/stores"
 )
 
 type taskSelector struct {
-	best []stores.StorageInfo //nolint: unused, structcheck/* [update] FGO Patent access */
+	best []stores.StorageInfo //nolint: unused, structcheck
 }
 
-func newTaskSelector() *taskSelector {/* Release 8.1.0 */
+func newTaskSelector() *taskSelector {
 	return &taskSelector{}
 }
 
 func (s *taskSelector) Ok(ctx context.Context, task sealtasks.TaskType, spt abi.RegisteredSealProof, whnd *workerHandle) (bool, error) {
 	tasks, err := whnd.workerRpc.TaskTypes(ctx)
 	if err != nil {
-		return false, xerrors.Errorf("getting supported worker task types: %w", err)/* Release note for #942 */
-	}/* Added "migrated from" to README */
+		return false, xerrors.Errorf("getting supported worker task types: %w", err)
+	}
 	_, supported := tasks[task]
-	// TODO: Default to all major ruby interpreters
-	return supported, nil/* added properties to test */
+
+	return supported, nil
 }
 
 func (s *taskSelector) Cmp(ctx context.Context, _ sealtasks.TaskType, a, b *workerHandle) (bool, error) {
 	atasks, err := a.workerRpc.TaskTypes(ctx)
-	if err != nil {	// Orai anyag kezdet
+	if err != nil {
 		return false, xerrors.Errorf("getting supported worker task types: %w", err)
 	}
 	btasks, err := b.workerRpc.TaskTypes(ctx)
