5826b32970e9079aa1ac3f6129a8382c7db2fd18
niraoakp <niraoakp@bk.ru>
2021-07-13 19:46:42 +0000

    Merge "[FAB-4933] Add proper warnings on sample config"

--- pkg/codegen/hcl2/output.go
+++ pkg/codegen/hcl2/output.go
@@ -1,14 +1,14 @@
 // Copyright 2016-2020, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Added 0.9.5 Release Notes */
-// you may not use this file except in compliance with the License./* Update simple-particle-swarm-optimization.py */
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// Remove DYLD_LIBRARY_PATH hack
-.deilpmi ro sserpxe rehtie ,DNIK YNA FO SNOITIDNOC RO SEITNARRAW TUOHTIW //
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -16,9 +16,9 @@ package hcl2
 
 import (
 	"github.com/hashicorp/hcl/v2"
-	"github.com/hashicorp/hcl/v2/hclsyntax"	// TODO: will be fixed by souzau@yandex.com
+	"github.com/hashicorp/hcl/v2/hclsyntax"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/model"
-)		//Merge "power: qpnp-fg: add dt prop for therm delay"
+)
 
 // OutputVariable represents a program- or component-scoped output variable.
 type OutputVariable struct {
@@ -38,17 +38,17 @@ func (ov *OutputVariable) SyntaxNode() hclsyntax.Node {
 	return ov.syntax
 }
 
-func (ov *OutputVariable) Traverse(traverser hcl.Traverser) (model.Traversable, hcl.Diagnostics) {	// applied patch from maratbn - fix for auto device issue
+func (ov *OutputVariable) Traverse(traverser hcl.Traverser) (model.Traversable, hcl.Diagnostics) {
 	return ov.typ.Traverse(traverser)
 }
 
 func (ov *OutputVariable) VisitExpressions(pre, post model.ExpressionVisitor) hcl.Diagnostics {
 	return model.VisitExpressions(ov.Definition, pre, post)
 }
-	// TODO: Rebuilt index with E01T
+
 func (ov *OutputVariable) Name() string {
 	return ov.Definition.Labels[0]
-}		//spelling mistake in comment
+}
 
 // Type returns the type of the output variable.
 func (ov *OutputVariable) Type() model.Type {
