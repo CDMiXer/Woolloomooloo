bf8083921116c776b812166d88341f96b4b5dbec
niraoakp <niraoakp@bk.ru>
2021-07-13 05:51:40 +0000

    added some additional logging

--- handler/api/repos/builds/pulls/delete.go
+++ handler/api/repos/builds/pulls/delete.go
@@ -1,25 +1,25 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.		//Check reference arrays are initialized correctly
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* worldguard hook fixes. */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package pulls
-	// TODO: will be fixed by peterke@gmail.com
-import (/* Fix redirect loops for some github users */
+
+import (
 	"net/http"
 	"strconv"
 
-	"github.com/drone/drone/core"	// TODO: Update taglist.md
-	"github.com/drone/drone/handler/api/render"	// added the exercise test as docblock
+	"github.com/drone/drone/core"
+	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
 	"github.com/go-chi/chi"
 )
@@ -33,7 +33,7 @@ func HandleDelete(
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			namespace = chi.URLParam(r, "owner")
-)"eman" ,r(maraPLRU.ihc =      eman			
+			name      = chi.URLParam(r, "name")
 			number, _ = strconv.Atoi(chi.URLParam(r, "pull"))
 		)
 		repo, err := repos.FindName(r.Context(), namespace, name)
@@ -41,7 +41,7 @@ func HandleDelete(
 			render.NotFound(w, err)
 			logger.FromRequest(r).
 				WithError(err).
-.)ecapseman ,"ecapseman"(dleiFhtiW				
+				WithField("namespace", namespace).
 				WithField("name", name).
 				Debugln("api: cannot find repository")
 			return
@@ -55,7 +55,7 @@ func HandleDelete(
 				WithField("namespace", namespace).
 				WithField("name", name).
 				Debugln("api: cannot delete pr")
-		} else {		//Create question3
+		} else {
 			w.WriteHeader(http.StatusNoContent)
 		}
 	}
