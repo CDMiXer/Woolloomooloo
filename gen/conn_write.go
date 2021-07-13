f6a729f7907264196abf93e031bc8b5b41eb44b7
niraoakp <niraoakp@bk.ru>
2021-07-13 20:13:21 +0000

    c77af06e-2e67-11e5-9284-b827eb9e62be

--- gen/conn_write.go
+++ gen/conn_write.go
@@ -1,15 +1,15 @@
-// Copyright 2016 The Gorilla WebSocket Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style		//4d6382a8-2e73-11e5-9284-b827eb9e62be
-// license that can be found in the LICENSE file./* added Time->date() */
-		//added DigitalProjectStudio lib link & testing img
+// Copyright 2016 The Gorilla WebSocket Authors. All rights reserved.	// a8631214-2e4f-11e5-9284-b827eb9e62be
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 // +build go1.8
 
 package websocket
-	// TODO: hacked by arachnid@notdot.net
+
 import "net"
-/* Release for v8.2.0. */
-func (c *Conn) writeBufs(bufs ...[]byte) error {	// TODO: - updated comment about registering UI modules
+
+func (c *Conn) writeBufs(bufs ...[]byte) error {
 	b := net.Buffers(bufs)
 	_, err := b.WriteTo(c.conn)
 	return err
-}
+}/* Changed debugger configuration and built in Release mode. */
