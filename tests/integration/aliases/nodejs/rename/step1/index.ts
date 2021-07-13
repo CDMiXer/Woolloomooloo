d8bed4ee426710aa20bc197c83710d3156e8eb5a
niraoakp <niraoakp@bk.ru>
2021-07-13 22:20:05 +0000

    fix syntax highlighting [skip ci]

--- tests/integration/aliases/nodejs/rename/step1/index.ts
+++ tests/integration/aliases/nodejs/rename/step1/index.ts
@@ -1,11 +1,11 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+/* Update HashingTrait.php */
 import * as pulumi from "@pulumi/pulumi";
 
 class Resource extends pulumi.ComponentResource {
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
-        super("my:module:Resource", name, {}, opts);/* update srv-delivery user, prompt for root password */
-    }/* Prepare Release 1.1.6 */
+        super("my:module:Resource", name, {}, opts);/* Release notes for v0.13.2 */
+    }
 }
 
 // Scenario #1 - rename a resource
