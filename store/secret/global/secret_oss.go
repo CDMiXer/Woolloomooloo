a18d7978b78df2a3d3f228c13614b9e4d9da2262
niraoakp <niraoakp@bk.ru>
2021-07-13 20:15:42 +0000

    replace login/logput with signin/signout

--- store/secret/global/secret_oss.go
+++ store/secret/global/secret_oss.go
@@ -1,30 +1,30 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Merge "Adds a system to track player xp, for unlockables." into ub-games-master */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* Don't depend on set being transitively included. */
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Changed version to 2.1.0 Release Candidate */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build oss		//fixed download issue
+// +build oss
 
 package global
 
-import (/* Delete chapter1/04_Release_Nodes */
+import (
 	"context"
-		//Merge "[FIX] sap.m.Panel: Accessibility improvement"
+
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/store/shared/db"
-	"github.com/drone/drone/store/shared/encrypt"	// TODO: chore(deps): update dependency @semantic-release/git to v7
+	"github.com/drone/drone/store/shared/encrypt"
 )
 
-// New returns a new Secret database store./* Bootstrap from sparse vectors */
+// New returns a new Secret database store.
 func New(db *db.DB, enc encrypt.Encrypter) core.GlobalSecretStore {
 	return new(noop)
 }
@@ -32,13 +32,13 @@ func New(db *db.DB, enc encrypt.Encrypter) core.GlobalSecretStore {
 type noop struct{}
 
 func (noop) List(context.Context, string) ([]*core.Secret, error) {
-	return nil, nil/* Release notes for 1.0.52 */
+	return nil, nil
 }
-	// TODO: Update symengine_version.txt
+
 func (noop) ListAll(context.Context) ([]*core.Secret, error) {
 	return nil, nil
 }
-/* connector model number corrected */
+
 func (noop) Find(context.Context, int64) (*core.Secret, error) {
 	return nil, nil
 }
@@ -46,13 +46,13 @@ func (noop) Find(context.Context, int64) (*core.Secret, error) {
 func (noop) FindName(context.Context, string, string) (*core.Secret, error) {
 	return nil, nil
 }
-	// TODO: Added URL to the full dataset.
+
 func (noop) Create(context.Context, *core.Secret) error {
-	return nil	// TODO: will be fixed by yuvalalaluf@gmail.com
+	return nil
 }
 
 func (noop) Update(context.Context, *core.Secret) error {
-	return nil		//included stdio.h in TmxParser_Test.cpp
+	return nil
 }
 
 func (noop) Delete(context.Context, *core.Secret) error {
