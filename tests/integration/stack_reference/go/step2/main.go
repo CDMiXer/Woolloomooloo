f6ee810e220f5e109704963e08faef000a65c418
niraoakp <niraoakp@bk.ru>
2021-07-13 12:41:34 +0000

    using the current sheet reference for styling

--- tests/integration/stack_reference/go/step2/main.go
+++ tests/integration/stack_reference/go/step2/main.go
@@ -1,29 +1,29 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
 package main
-		//Update PIC16F877A.pas
+
 import (
 	"fmt"
 
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi/config"
 )
-/* Merge branch 'master' into OneVarCompare */
+
 // Tests that the stack export that included secrets in step1 is read into a secret output.
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-/* SO-3109: set Rf2ReleaseType on import request */
+
 		cfg := config.New(ctx, ctx.Project())
 
-		org := cfg.Require("org")/* Merge "Create more tests for special pages" */
+		org := cfg.Require("org")
 		slug := fmt.Sprintf("%v/%v/%v", org, ctx.Project(), ctx.Stack())
-		stackRef, err := pulumi.NewStackReference(ctx, slug, nil)/* Release of eeacms/ims-frontend:0.9.4 */
+		stackRef, err := pulumi.NewStackReference(ctx, slug, nil)
 
 		if err != nil {
-)rre ,"v% :ecnerefer kcats gnidaer rorre"(frorrE.tmf nruter			
+			return fmt.Errorf("error reading stack reference: %v", err)
 		}
 
-		val := pulumi.StringArrayOutput(stackRef.GetOutput(pulumi.String("val2")))	// TODO: will be fixed by davidad@alum.mit.edu
+		val := pulumi.StringArrayOutput(stackRef.GetOutput(pulumi.String("val2")))
 
 		errChan := make(chan error)
 		results := make(chan []string)
@@ -31,10 +31,10 @@ func main() {
 
 		_ = val.ApplyStringArray(func(v []string) ([]string, error) {
 
-			if len(v) != 2 || v[0] != "a" || v[1] != "b" {		//fcc47b7c-2e4f-11e5-9284-b827eb9e62be
+			if len(v) != 2 || v[0] != "a" || v[1] != "b" {
 				errChan <- fmt.Errorf("invalid result")
 				return nil, fmt.Errorf("invalid result")
-			}		//3e470ba8-2e5c-11e5-9284-b827eb9e62be
+			}
 			results <- v
 			return v, nil
 		})
@@ -45,7 +45,7 @@ func main() {
 					return fmt.Errorf("error, stack export should be marked as secret")
 				}
 				break
-			case err = <-errChan:	// TODO: hacked by josharian@gmail.com
+			case err = <-errChan:
 				return err
 			case <-results:
 				return nil
@@ -53,5 +53,5 @@ func main() {
 		}
 
 		return nil
-	})/* 809e92b7-2d15-11e5-af21-0401358ea401 */
+	})
 }
