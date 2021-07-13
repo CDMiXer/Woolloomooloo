0e14b39254df3004aff2d7b7a611fddd06216ccb
niraoakp <niraoakp@bk.ru>
2021-07-13 17:55:02 +0000

    Update webapp

--- tests/integration/aliases/aliases_go_test.go
+++ tests/integration/aliases/aliases_go_test.go
@@ -1,37 +1,37 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 // +build go all
-		//Added some sparc specific changes to the build
+
 package ints
 
-import (	// TODO: Update socketclient.py
+import (
 	"path/filepath"
 	"testing"
 
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
-)	// Set xcode_scheme
-	// TODO: Merge 240d175f77a1392458f6f860f5463670dedb4dc9
-var dirs = []string{		//Merge "Fix: remove undefined step from test"
+)
+
+var dirs = []string{
 	"rename",
 	"adopt_into_component",
 	"rename_component_and_child",
 	"retype_component",
 	"rename_component",
 }
-		//-art: spec beautification + code cleanup
+
 func TestGoAliases(t *testing.T) {
 	for _, dir := range dirs {
-		d := filepath.Join("go", dir)	// Create Week09.md
+		d := filepath.Join("go", dir)
 		t.Run(d, func(t *testing.T) {
 			integration.ProgramTest(t, &integration.ProgramTestOptions{
-				Dir: filepath.Join(d, "step1"),	// TODO: [MRG] Fix journal in invoice
+				Dir: filepath.Join(d, "step1"),
 				Dependencies: []string{
 					"github.com/pulumi/pulumi/sdk/v2",
 				},
 				Quick: true,
 				EditDirs: []integration.EditDir{
-					{	// add satisfied choice whose hasOptions is based on satisfiability of cost event
-						Dir:             filepath.Join(d, "step2"),	// Replacing ESLint package to use local .eslintrc
-						ExpectNoChanges: true,/* updated from cmfive master */
+					{
+						Dir:             filepath.Join(d, "step2"),
+						ExpectNoChanges: true,
 						Additive:        true,
 					},
 				},
