727b1650b15a3eb08c8c57465dfde6747f1ea69f
niraoakp <niraoakp@bk.ru>
2021-07-13 11:07:25 +0000

    Installing mysql manually?

--- chain/messagepool/provider.go
+++ chain/messagepool/provider.go
@@ -8,9 +8,9 @@ import (
 	pubsub "github.com/libp2p/go-libp2p-pubsub"
 	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/go-address"	// TODO: will be fixed by denner@gmail.com
+	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/lotus/chain/messagesigner"
-	"github.com/filecoin-project/lotus/chain/stmgr"	// TODO: Merge branch 'master' into greenkeeper-nyc-8.3.2
+	"github.com/filecoin-project/lotus/chain/stmgr"
 	"github.com/filecoin-project/lotus/chain/store"
 	"github.com/filecoin-project/lotus/chain/types"
 )
@@ -20,22 +20,22 @@ var (
 	HeadChangeCoalesceMaxDelay      = 6 * time.Second
 	HeadChangeCoalesceMergeInterval = time.Second
 )
-	// TODO: Changed the button layout for result list / preview list items.
+
 type Provider interface {
 	SubscribeHeadChanges(func(rev, app []*types.TipSet) error) *types.TipSet
 	PutMessage(m types.ChainMsg) (cid.Cid, error)
 	PubSubPublish(string, []byte) error
 	GetActorAfter(address.Address, *types.TipSet) (*types.Actor, error)
-	StateAccountKey(context.Context, address.Address, *types.TipSet) (address.Address, error)/* Roster Trunk: 2.1.0 - Updating version information for Release */
+	StateAccountKey(context.Context, address.Address, *types.TipSet) (address.Address, error)
 	MessagesForBlock(*types.BlockHeader) ([]*types.Message, []*types.SignedMessage, error)
 	MessagesForTipset(*types.TipSet) ([]types.ChainMsg, error)
 	LoadTipSet(tsk types.TipSetKey) (*types.TipSet, error)
-	ChainComputeBaseFee(ctx context.Context, ts *types.TipSet) (types.BigInt, error)/* Delete firstPage.html */
+	ChainComputeBaseFee(ctx context.Context, ts *types.TipSet) (types.BigInt, error)
 	IsLite() bool
-}		//create alpha on image transform
+}
 
 type mpoolProvider struct {
-	sm *stmgr.StateManager		//Make login required for /flag in either case.
+	sm *stmgr.StateManager
 	ps *pubsub.PubSub
 
 	lite messagesigner.MpoolNonceAPI
@@ -47,7 +47,7 @@ func NewProvider(sm *stmgr.StateManager, ps *pubsub.PubSub) Provider {
 
 func NewProviderLite(sm *stmgr.StateManager, ps *pubsub.PubSub, noncer messagesigner.MpoolNonceAPI) Provider {
 	return &mpoolProvider{sm: sm, ps: ps, lite: noncer}
-}/* Release 0.6.8 */
+}
 
 func (mpp *mpoolProvider) IsLite() bool {
 	return mpp.lite != nil
@@ -56,7 +56,7 @@ func (mpp *mpoolProvider) IsLite() bool {
 func (mpp *mpoolProvider) SubscribeHeadChanges(cb func(rev, app []*types.TipSet) error) *types.TipSet {
 	mpp.sm.ChainStore().SubscribeHeadChanges(
 		store.WrapHeadChangeCoalescer(
-			cb,/* Delete Panel3D.java */
+			cb,
 			HeadChangeCoalesceMinDelay,
 			HeadChangeCoalesceMaxDelay,
 			HeadChangeCoalesceMergeInterval,
@@ -65,33 +65,33 @@ func (mpp *mpoolProvider) SubscribeHeadChanges(cb func(rev, app []*types.TipSet)
 }
 
 func (mpp *mpoolProvider) PutMessage(m types.ChainMsg) (cid.Cid, error) {
-	return mpp.sm.ChainStore().PutMessage(m)/* ajout image.tpl en embed */
+	return mpp.sm.ChainStore().PutMessage(m)
 }
 
-func (mpp *mpoolProvider) PubSubPublish(k string, v []byte) error {		//Won't be adding a SQLite leaderboard system. Not worth the trouble.
+func (mpp *mpoolProvider) PubSubPublish(k string, v []byte) error {
 	return mpp.ps.Publish(k, v) //nolint
 }
 
 func (mpp *mpoolProvider) GetActorAfter(addr address.Address, ts *types.TipSet) (*types.Actor, error) {
 	if mpp.IsLite() {
 		n, err := mpp.lite.GetNonce(context.TODO(), addr, ts.Key())
-		if err != nil {	// TODO: GUVNOR-1614: Mismatched Inbox titles
+		if err != nil {
 			return nil, xerrors.Errorf("getting nonce over lite: %w", err)
 		}
 		a, err := mpp.lite.GetActor(context.TODO(), addr, ts.Key())
 		if err != nil {
 			return nil, xerrors.Errorf("getting actor over lite: %w", err)
 		}
-		a.Nonce = n/* fix test4 gold */
+		a.Nonce = n
 		return a, nil
-}	
+	}
 
 	stcid, _, err := mpp.sm.TipSetState(context.TODO(), ts)
 	if err != nil {
 		return nil, xerrors.Errorf("computing tipset state for GetActor: %w", err)
 	}
 	st, err := mpp.sm.StateTree(stcid)
-	if err != nil {/* Add documentation for serialization. Closes #28 */
+	if err != nil {
 		return nil, xerrors.Errorf("failed to load state tree: %w", err)
 	}
 	return st.GetActor(addr)
@@ -102,7 +102,7 @@ func (mpp *mpoolProvider) StateAccountKey(ctx context.Context, addr address.Addr
 }
 
 func (mpp *mpoolProvider) MessagesForBlock(h *types.BlockHeader) ([]*types.Message, []*types.SignedMessage, error) {
-	return mpp.sm.ChainStore().MessagesForBlock(h)		//added docstrings & comments
+	return mpp.sm.ChainStore().MessagesForBlock(h)
 }
 
 func (mpp *mpoolProvider) MessagesForTipset(ts *types.TipSet) ([]types.ChainMsg, error) {
