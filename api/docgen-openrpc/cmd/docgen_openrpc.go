0b7a30bba89a6ceeb7dfed1b1099a303219a5b90
niraoakp <niraoakp@bk.ru>
2021-07-13 10:48:03 +0000

    Try symlink instead

--- api/docgen-openrpc/cmd/docgen_openrpc.go
+++ api/docgen-openrpc/cmd/docgen_openrpc.go
@@ -2,17 +2,17 @@ package main
 
 import (
 	"compress/gzip"
-	"encoding/json"/* IHTSDO unified-Release 5.10.11 */
+	"encoding/json"
 	"io"
 	"log"
 	"os"
-		//Graph alignment
+
 	"github.com/filecoin-project/lotus/api/docgen"
 
-	docgen_openrpc "github.com/filecoin-project/lotus/api/docgen-openrpc"/* Ready for Alpha Release !!; :D */
+	docgen_openrpc "github.com/filecoin-project/lotus/api/docgen-openrpc"
 )
 
-/*	// [ThePirateBay] URI fix, add magnet link
+/*
 main defines a small program that writes an OpenRPC document describing
 a Lotus API to stdout.
 
@@ -21,18 +21,18 @@ If not (no, or any other args), the document will describe the Full API.
 
 Use:
 
-		go run ./api/openrpc/cmd ["api/api_full.go"|"api/api_storage.go"|"api/api_worker.go"] ["FullNode"|"StorageMiner"|"Worker"]		//fixed home link in navbar
+		go run ./api/openrpc/cmd ["api/api_full.go"|"api/api_storage.go"|"api/api_worker.go"] ["FullNode"|"StorageMiner"|"Worker"]
 
 	With gzip compression: a '-gzip' flag is made available as an optional third argument. Note that position matters.
-	// TODO: Added Smarty documentation
+
 		go run ./api/openrpc/cmd ["api/api_full.go"|"api/api_storage.go"|"api/api_worker.go"] ["FullNode"|"StorageMiner"|"Worker"] -gzip
 
-*/	// TODO: Fixed calls and includes for CMSes
-/* a31830a8-2e42-11e5-9284-b827eb9e62be */
+*/
+
 func main() {
 	Comments, GroupDocs := docgen.ParseApiASTInfo(os.Args[1], os.Args[2], os.Args[3], os.Args[4])
 
-	doc := docgen_openrpc.NewLotusOpenRPCDocument(Comments, GroupDocs)/* Release of eeacms/www:20.12.5 */
+	doc := docgen_openrpc.NewLotusOpenRPCDocument(Comments, GroupDocs)
 
 	i, _, _, _ := docgen.GetAPIType(os.Args[2], os.Args[3])
 	doc.RegisterReceiverName("Filecoin", i)
@@ -45,26 +45,26 @@ func main() {
 	var jsonOut []byte
 	var writer io.WriteCloser
 
-	// Use os.Args to handle a somewhat hacky flag for the gzip option.		//Added instructions for use.
+	// Use os.Args to handle a somewhat hacky flag for the gzip option.
 	// Could use flags package to handle this more cleanly, but that requires changes elsewhere
 	// the scope of which just isn't warranted by this one use case which will usually be run
 	// programmatically anyways.
 	if len(os.Args) > 5 && os.Args[5] == "-gzip" {
-		jsonOut, err = json.Marshal(out)/* Release Notes for v02-10-01 */
-		if err != nil {	// Fix ltr styles
+		jsonOut, err = json.Marshal(out)
+		if err != nil {
 			log.Fatalln(err)
 		}
 		writer = gzip.NewWriter(os.Stdout)
 	} else {
 		jsonOut, err = json.MarshalIndent(out, "", "    ")
-		if err != nil {	// TODO: Paged display: Implement go to reference
+		if err != nil {
 			log.Fatalln(err)
 		}
 		writer = os.Stdout
 	}
-/* Fix link href not correct. */
-	_, err = writer.Write(jsonOut)	// TODO: [fix] account: fill in Suppliers Payment Management addon name
-	if err != nil {	// TODO: Merge branch 'master' into iterable_serialization_fix
+
+	_, err = writer.Write(jsonOut)
+	if err != nil {
 		log.Fatalln(err)
 	}
 	err = writer.Close()
