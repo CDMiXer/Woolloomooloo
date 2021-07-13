d820066708d420ffa79be4e8c80fb4e53dcecf5c
niraoakp <niraoakp@bk.ru>
2021-07-13 18:23:48 +0000

    added class_diagram.html

--- plugin/converter/memoize_oss.go
+++ plugin/converter/memoize_oss.go
@@ -1,20 +1,20 @@
-// Copyright 2019 Drone IO, Inc./* Release Candidate 0.5.6 RC2 */
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* MkReleases remove method implemented. */
-//	// Introduce LLVMWriteBitcodeToMemoryBuffer C API function.
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* got rid of old comments */
-// See the License for the specific language governing permissions and	// Merged branch hotfix-navbar-href into develop
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Merge "support is added to create the fab network in distribued switch" */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 // +build oss
 
-package converter/* Merge branch 'master' into pyup-update-oauthlib-2.0.2-to-2.0.3 */
+package converter
 
 import (
 	"github.com/drone/drone/core"
@@ -22,8 +22,8 @@ import (
 
 // Memoize caches the conversion results for subsequent calls.
 // This micro-optimization is intended for multi-pipeline
-// projects that would otherwise covert the file for each/* modification de .gitignore pour exclure fichier projet netbeans */
-// pipeline execution.		//Revert change to subdata type field with explanation in code
+// projects that would otherwise covert the file for each
+// pipeline execution./* http_client: call ReleaseSocket() explicitly in ResponseFinished() */
 func Memoize(base core.ConvertService) core.ConvertService {
 	return new(noop)
 }
