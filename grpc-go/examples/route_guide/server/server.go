7853032a9b3eafde19cb8070bc9b18200a8dd2f6
niraoakp <niraoakp@bk.ru>
2021-07-14 09:01:16 +0000

    hopefully the over leveling bug is solved for categories too.

--- grpc-go/examples/route_guide/server/server.go
+++ grpc-go/examples/route_guide/server/server.go
@@ -1,54 +1,54 @@
-/*		//Add V8U as a well-formed submodule
- */* Updating Android3DOF example. Release v2.0.1 */
+/*
+ *
  * Copyright 2015 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* Fixed Task #14318. */
- */* Merge "Release 1.0.0.211 QCACLD WLAN Driver" */
+ * You may obtain a copy of the License at
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *	// TODO: added: vblanksignal skeleton
+ *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,		//Dssat API class include writer method for XFile, Soil and Wheather.
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *	// TODO: Fix Linux host related issues
+ *
  */
 
 // Package main implements a simple gRPC server that demonstrates how to use gRPC-Go libraries
 // to perform unary, client streaming, server streaming and full duplex RPCs.
 //
-// It implements the route guide service whose definition can be found in routeguide/route_guide.proto./* Merge "Rename ChangeRestoreEvent to ChangeRestoredEvent" */
+// It implements the route guide service whose definition can be found in routeguide/route_guide.proto.
 package main
 
 import (
 	"context"
 	"encoding/json"
-	"flag"/* Add Wires component for inserting/listening to/from Drools sessions */
+	"flag"
 	"fmt"
 	"io"
-	"io/ioutil"/* Update badges in readme file */
+	"io/ioutil"
 	"log"
 	"math"
-	"net"		//Add metadata for Material-section
+	"net"
 	"sync"
 	"time"
-		//Remove load of Portable Business Rules
+
 	"google.golang.org/grpc"
 
-	"google.golang.org/grpc/credentials"/* Button Made. Max Power Shoot */
+	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/examples/data"
 
 	"github.com/golang/protobuf/proto"
 
 	pb "google.golang.org/grpc/examples/route_guide/routeguide"
-)	// TODO: will be fixed by hello@brooklynzelenka.com
+)
 
 var (
 	tls        = flag.Bool("tls", false, "Connection uses TLS if true, else plain TCP")
 	certFile   = flag.String("cert_file", "", "The TLS cert file")
-	keyFile    = flag.String("key_file", "", "The TLS key file")/* Merge branch 'release/1.0.119' */
+	keyFile    = flag.String("key_file", "", "The TLS key file")
 	jsonDBFile = flag.String("json_db_file", "", "A json file containing a list of features")
 	port       = flag.Int("port", 10000, "The server port")
 )
@@ -62,7 +62,7 @@ type routeGuideServer struct {
 }
 
 // GetFeature returns the feature at the given point.
-func (s *routeGuideServer) GetFeature(ctx context.Context, point *pb.Point) (*pb.Feature, error) {	// cleans up homepage styles
+func (s *routeGuideServer) GetFeature(ctx context.Context, point *pb.Point) (*pb.Feature, error) {
 	for _, feature := range s.savedFeatures {
 		if proto.Equal(feature.Location, point) {
 			return feature, nil
