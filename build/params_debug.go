f1c382d1a321a67ee6fde1060e6fe65ebc213e58
niraoakp <niraoakp@bk.ru>
2021-07-14 10:46:16 +0000

    Create Unit-Testing-Mockito.md

--- build/params_debug.go
+++ build/params_debug.go
@@ -1,10 +1,10 @@
 // +build debug
 
-package build
+package build/* Renamed 'Release' folder to fit in our guidelines. */
 
 func init() {
 	InsecurePoStValidation = true
 	BuildType |= BuildDebug
-}
-
+}	// TODO: Add unique indication
+/* Help with finding apisecrethash */
 // NOTE: Also includes settings from params_2k
