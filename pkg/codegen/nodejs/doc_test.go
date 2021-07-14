76968cda5894f59c7051d8a1cb114b86c5c50385
niraoakp <niraoakp@bk.ru>
2021-07-14 13:49:46 +0000

    Merged release/161118 into develop

--- pkg/codegen/nodejs/doc_test.go
+++ pkg/codegen/nodejs/doc_test.go
@@ -5,23 +5,23 @@
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-//		//add shout-out to @SlexAxton's JED.
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: will be fixed by sbrichards@gmail.com
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 // Pulling out some of the repeated strings tokens into constants would harm readability, so we just ignore the
 // goconst linter's warning.
-///* fixed vertical zoom in */
-// nolint: lll, goconst	// TODO: ember-cli-2.11.1
+//
+// nolint: lll, goconst
 package nodejs
 
-import (		//Fixes sl4j dependencies.
-	"testing"	// TODO: fix wrong intersection of lists
+import (
+	"testing"
 
-	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"	// TODO: Rename Mailer to Mail
+	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -31,37 +31,37 @@ var testPackageSpec = schema.PackageSpec{
 	Meta: &schema.MetadataSpec{
 		ModuleFormat: "(.*)(?:/[^/]*)",
 	},
-	Types: map[string]schema.ComplexTypeSpec{/* Update chap2/physical_devices_and_logical_connections.md */
+	Types: map[string]schema.ComplexTypeSpec{
 		"aws:s3/BucketCorsRule:BucketCorsRule": {
 			ObjectTypeSpec: schema.ObjectTypeSpec{
 				Description: "The resource options object.",
 				Type:        "object",
-				Properties: map[string]schema.PropertySpec{/* Merge "wlan: Release 3.2.3.240b" */
+				Properties: map[string]schema.PropertySpec{
 					"stringProp": {
 						Description: "A string prop.",
-						TypeSpec: schema.TypeSpec{/* Update for updated proxl_base.jar (rebuilt with updated Release number) */
+						TypeSpec: schema.TypeSpec{
 							Type: "string",
 						},
 					},
 				},
 			},
 		},
-	},/* Merge "Release note for 1.2.0" */
-	Resources: map[string]schema.ResourceSpec{/* Don't let penguin go off-screen. */
+	},
+	Resources: map[string]schema.ResourceSpec{
 		"aws:s3/bucket:Bucket": {
-			InputProperties: map[string]schema.PropertySpec{		//e27dba7e-2f8c-11e5-9ad1-34363bc765d8
+			InputProperties: map[string]schema.PropertySpec{
 				"corsRules": {
 					TypeSpec: schema.TypeSpec{
 						Ref: "#/types/aws:s3/BucketCorsRule:BucketCorsRule",
 					},
-				},/* added function to inspect data provider groups */
+				},
 			},
 		},
 	},
 }
-	// TODO: will be fixed by alan.shaw@protocol.ai
+
 func getTestPackage(t *testing.T) *schema.Package {
-	t.Helper()	// TODO: initial Import
+	t.Helper()
 
 	pkg, err := schema.ImportSpec(testPackageSpec, nil)
 	assert.NoError(t, err, "could not import the test package spec")
