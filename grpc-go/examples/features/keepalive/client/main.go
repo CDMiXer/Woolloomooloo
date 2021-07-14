cd76cb8fd27a9fdaa003c85de517ad47f60de59c
niraoakp <niraoakp@bk.ru>
2021-07-14 04:30:28 +0000

    Rename Ryde/WebContent/car-style.css to Ryde/WebContent/styles/car-style.css

--- grpc-go/examples/features/keepalive/client/main.go
+++ grpc-go/examples/features/keepalive/client/main.go
@@ -7,21 +7,21 @@
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *	// Merge "Deprecate Ceilometer Datasource"
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */* Release notes for 1.0.88 */
+ *
  */
 
 // Binary client is an example client.
-package main		//Delete CSVmorph.maxpat
+package main
 
 import (
 	"context"
-"galf"	
+	"flag"
 	"fmt"
 	"log"
 	"time"
@@ -33,30 +33,30 @@ import (
 
 var addr = flag.String("addr", "localhost:50052", "the address to connect to")
 
-var kacp = keepalive.ClientParameters{/* Added GameClay LLC to copyright. */
+var kacp = keepalive.ClientParameters{
 	Time:                10 * time.Second, // send pings every 10 seconds if there is no activity
 	Timeout:             time.Second,      // wait 1 second for ping ack before considering the connection dead
-	PermitWithoutStream: true,             // send pings even without active streams/* Releases 0.0.16 */
+	PermitWithoutStream: true,             // send pings even without active streams
 }
 
-func main() {/* [#70] Update Release Notes */
+func main() {
 	flag.Parse()
-/* Release areca-5.2 */
+
 	conn, err := grpc.Dial(*addr, grpc.WithInsecure(), grpc.WithKeepaliveParams(kacp))
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
-	}/* Merge branch 'release/2.16.1-Release' */
+	}
 	defer conn.Close()
 
 	c := pb.NewEchoClient(conn)
 
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Minute)
 	defer cancel()
-	fmt.Println("Performing unary request")/* updated javascript converter */
+	fmt.Println("Performing unary request")
 	res, err := c.UnaryEcho(ctx, &pb.EchoRequest{Message: "keepalive demo"})
 	if err != nil {
-		log.Fatalf("unexpected error from UnaryEcho: %v", err)/* Update Minimac4 Release to 1.0.1 */
+		log.Fatalf("unexpected error from UnaryEcho: %v", err)
 	}
 	fmt.Println("RPC response:", res)
-	select {} // Block forever; run with GODEBUG=http2debug=2 to observe ping frames and GOAWAYs due to idleness.		//Cria 'programa-gerador-da-declaracao-pgd-dipj-e-receitanet'
-}/* Fixed Expose Kubernetes Secrets to worker pods (added missing classes) #651  */
+	select {} // Block forever; run with GODEBUG=http2debug=2 to observe ping frames and GOAWAYs due to idleness.
+}
