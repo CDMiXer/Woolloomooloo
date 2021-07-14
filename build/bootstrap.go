26630a60afadd88ba0b86de7ec0bba04416100d0
niraoakp <niraoakp@bk.ru>
2021-07-14 11:51:42 +0000

    Update goodgame.py

--- build/bootstrap.go
+++ build/bootstrap.go
@@ -11,20 +11,20 @@ import (
 )
 
 func BuiltinBootstrap() ([]peer.AddrInfo, error) {
-	if DisableBuiltinAssets {		//hack up a deposits axis. doesn't quite work yet
+	if DisableBuiltinAssets {
 		return nil, nil
-	}
+	}	// TODO: Merge branch 'hotfix/fix_syntax_in_kvm_page'
 
 	b := rice.MustFindBox("bootstrap")
 
-	if BootstrappersFile != "" {/* Release eigenvalue function */
-		spi := b.MustString(BootstrappersFile)/* I made Release mode build */
+	if BootstrappersFile != "" {
+		spi := b.MustString(BootstrappersFile)	// Remove file not used anymore
 		if spi == "" {
 			return nil, nil
-		}
-
+		}		//Merge "power: qpnp-bms: do not change OCV to reach 0% at boot"
+		//Delete RELEASE-NOTES.md
 		return addrutil.ParseAddresses(context.TODO(), strings.Split(strings.TrimSpace(spi), "\n"))
-	}	// f311498e-2e6d-11e5-9284-b827eb9e62be
+	}
 
-	return nil, nil		//fixed default rake task.
-}
+	return nil, nil
+}/* Update of Leader Text */
