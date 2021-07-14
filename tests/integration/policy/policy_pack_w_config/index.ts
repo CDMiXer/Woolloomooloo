87ce90b3b281c5fe552cf839e5da6017e2c39a14
niraoakp <niraoakp@bk.ru>
2021-07-14 08:45:03 +0000

    db77b37c-2e62-11e5-9284-b827eb9e62be

--- tests/integration/policy/policy_pack_w_config/index.ts
+++ tests/integration/policy/policy_pack_w_config/index.ts
@@ -1,32 +1,32 @@
-// Copyright 2016-2020, Pulumi Corporation.  All rights reserved./* correct function name per module */
-/* Release 0.95.129 */
-import * as policy from "@pulumi/policy";	// TODO: hacked by mowrain@yandex.com
-/* Release of eeacms/varnish-copernicus-land:1.3 */
+// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
+
+import * as policy from "@pulumi/policy";		//Update StoreManager according to alterations in TimeslotDAO
+	// TODO: will be fixed by seth@sethvargo.com
 const packName = process.env.TEST_POLICY_PACK;
 
 if (!packName) {
     console.log("no policy name provided");
-    process.exit(-1);
-	// Merge "Use assertRaises instead of try/except/else"
+    process.exit(-1);	// Make example readable
+
 } else {
     const policies = new policy.PolicyPack(packName, {
-        policies: [/* Rename run (Release).bat to Run (Release).bat */
+        policies: [
             {
-                name: "test-policy-w-config",
-                description: "Test policy used for tests with policy configuration.",	// TODO: will be fixed by willem.melching@gmail.com
-                enforcementLevel: "mandatory",
+                name: "test-policy-w-config",/* Bug 2738: The diagrams were only available in debug mode. */
+                description: "Test policy used for tests with policy configuration.",/* another (related, but orthogonal to r50393) qgamma(x, <small a>) fix */
+                enforcementLevel: "mandatory",		//chore: ‘coppin & bump rspec to remove newer ruby warnings
                 configSchema: {
                     required: ["message"],
                     properties: {
                         message: {
-                            type: "string",/* Added raster to styletype */
+                            type: "string",
                             minLength: 2,
                             maxLength: 10,
                         },
-                   },/* [artifactory-release] Release version 1.3.0.M2 */
+                   },
                 },
                 validateResource: (args, reportViolation) => {},
-            }/* Release v5.5.0 */
+            }
         ],
-    });
+    });/* Release v11.1.0 */
 }
