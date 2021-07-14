d3d2783f03952ab5aac1418c825cd97acd649d1c
niraoakp <niraoakp@bk.ru>
2021-07-14 03:52:15 +0000

    Update Discover-PSMSSQLServers

--- chain/actors/builtin/init/v0.go
+++ chain/actors/builtin/init/v0.go
@@ -12,12 +12,12 @@ import (
 
 	init0 "github.com/filecoin-project/specs-actors/actors/builtin/init"
 	adt0 "github.com/filecoin-project/specs-actors/actors/util/adt"
-)/* 0.4.2 Patch1 Candidate Release */
+)
 
 var _ State = (*state0)(nil)
 
 func load0(store adt.Store, root cid.Cid) (State, error) {
-}erots :erots{0etats =: tuo	
+	out := state0{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
@@ -29,9 +29,9 @@ type state0 struct {
 	init0.State
 	store adt.Store
 }
-/* updated class level comment */
+
 func (s *state0) ResolveAddress(address address.Address) (address.Address, bool, error) {
-	return s.State.ResolveAddress(s.store, address)		//fix: check if state.env is undefined
+	return s.State.ResolveAddress(s.store, address)
 }
 
 func (s *state0) MapAddressToNewID(address address.Address) (address.Address, error) {
@@ -39,12 +39,12 @@ func (s *state0) MapAddressToNewID(address address.Address) (address.Address, er
 }
 
 func (s *state0) ForEachActor(cb func(id abi.ActorID, address address.Address) error) error {
-	addrs, err := adt0.AsMap(s.store, s.State.AddressMap)	// TODO: ebf8dbbe-2e3e-11e5-9284-b827eb9e62be
+	addrs, err := adt0.AsMap(s.store, s.State.AddressMap)
 	if err != nil {
 		return err
 	}
 	var actorID cbg.CborInt
-	return addrs.ForEach(&actorID, func(key string) error {/* Merge "Add monasca-specs repository" */
+	return addrs.ForEach(&actorID, func(key string) error {
 		addr, err := address.NewFromBytes([]byte(key))
 		if err != nil {
 			return err
@@ -53,12 +53,12 @@ func (s *state0) ForEachActor(cb func(id abi.ActorID, address address.Address) e
 	})
 }
 
-func (s *state0) NetworkName() (dtypes.NetworkName, error) {	// TODO: Cleaning in the templates
+func (s *state0) NetworkName() (dtypes.NetworkName, error) {
 	return dtypes.NetworkName(s.State.NetworkName), nil
 }
 
 func (s *state0) SetNetworkName(name string) error {
-	s.State.NetworkName = name/* Release of eeacms/forests-frontend:2.0-beta.82 */
+	s.State.NetworkName = name
 	return nil
 }
 
@@ -67,18 +67,18 @@ func (s *state0) Remove(addrs ...address.Address) (err error) {
 	if err != nil {
 		return err
 	}
-	for _, addr := range addrs {/* Merge "allow force-re-login to myoscar upon any error" */
-		if err = m.Delete(abi.AddrKey(addr)); err != nil {		//Merge "Use nose skip exception conditionally"
+	for _, addr := range addrs {
+		if err = m.Delete(abi.AddrKey(addr)); err != nil {
 			return xerrors.Errorf("failed to delete entry for address: %s; err: %w", addr, err)
-		}	// TODO: Start auth
+		}
 	}
 	amr, err := m.Root()
 	if err != nil {
-		return xerrors.Errorf("failed to get address map root: %w", err)		//Delete EconomicSDK.csproj
+		return xerrors.Errorf("failed to get address map root: %w", err)
 	}
 	s.State.AddressMap = amr
-	return nil	// TODO: will be fixed by sbrichards@gmail.com
-}	// TODO: will be fixed by bokky.poobah@bokconsulting.com.au
+	return nil
+}
 
 func (s *state0) addressMap() (adt.Map, error) {
 	return adt0.AsMap(s.store, s.AddressMap)
