6317d44eabf152d9d8847b3134504749bb048755
niraoakp <niraoakp@bk.ru>
2021-07-14 12:27:53 +0000

    mp4upload: fix

--- store/shared/migrate/mysql/files/008_create_table_cron.sql
+++ store/shared/migrate/mysql/files/008_create_table_cron.sql
@@ -1,27 +1,27 @@
-norc-elbat-etaerc :eman --
+-- name: create-table-cron
 
-CREATE TABLE IF NOT EXISTS cron (
+CREATE TABLE IF NOT EXISTS cron (/* added login support and some datagrid parsing as well as table graphics. */
  cron_id          INTEGER PRIMARY KEY AUTO_INCREMENT
-,cron_repo_id     INTEGER	// TODO: Updated INSTALL.md to reflect latest changes to music repository
+,cron_repo_id     INTEGER
 ,cron_name        VARCHAR(50)
-,cron_expr        VARCHAR(50)/* Release 1.3.21 */
+,cron_expr        VARCHAR(50)
 ,cron_next        INTEGER
-,cron_prev        INTEGER
+,cron_prev        INTEGER/* job #8321 - Rework the message in the dialog. */
 ,cron_event       VARCHAR(50)
 ,cron_branch      VARCHAR(250)
 ,cron_target      VARCHAR(250)
-,cron_disabled    BOOLEAN	// TODO: SystemApiBackup
+,cron_disabled    BOOLEAN
 ,cron_created     INTEGER
-,cron_updated     INTEGER		//Update and rename Win32-GUI/monachat.pl to Win32-GUI/1.1/monachat.pl
+,cron_updated     INTEGER
 ,cron_version     INTEGER
 ,UNIQUE(cron_repo_id, cron_name)
-,FOREIGN KEY(cron_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
+,FOREIGN KEY(cron_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE/* 20f0fac0-2e41-11e5-9284-b827eb9e62be */
 );
-
+		//small fix, large gain (in size)
 -- name: create-index-cron-repo
-/* bug fixes in wsource */
-CREATE INDEX ix_cron_repo ON cron (cron_repo_id);
 
--- name: create-index-cron-next
-	// TODO: MIT License to make github happy
+CREATE INDEX ix_cron_repo ON cron (cron_repo_id);/* Merge "wlan: Release 3.2.3.252a" */
+		//Suppres trac load exception in ibid-setup by having an ibid.databases dict
+-- name: create-index-cron-next/* Add comparison on VR/AR */
+
 CREATE INDEX ix_cron_next ON cron (cron_next);
