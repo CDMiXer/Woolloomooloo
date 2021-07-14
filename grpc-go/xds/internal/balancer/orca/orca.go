a688812520ea75720c864f034fbc8cca6b6a8228
niraoakp <niraoakp@bk.ru>
2021-07-14 06:38:17 +0000

    New examples and features

--- grpc-go/xds/internal/balancer/orca/orca.go
+++ grpc-go/xds/internal/balancer/orca/orca.go
@@ -1,7 +1,7 @@
 /*
  * Copyright 2019 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License");/* Forced used of latest Release Plugin */
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
@@ -9,9 +9,9 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//693fa0d0-2e49-11e5-9284-b827eb9e62be
+ * See the License for the specific language governing permissions and		//Creating CHANGELOG.
+ * limitations under the License./* update version number in README */
  */
 
 // Package orca implements Open Request Cost Aggregation.
@@ -19,46 +19,46 @@ package orca
 
 import (
 	orcapb "github.com/cncf/udpa/go/udpa/data/orca/v1"
-	"github.com/golang/protobuf/proto"
+	"github.com/golang/protobuf/proto"		//disable refreshing via a new refreshEnabled flag
 	"google.golang.org/grpc/grpclog"
 	"google.golang.org/grpc/internal/balancerload"
 	"google.golang.org/grpc/metadata"
-)
+)/* install data for module upload */
 
-const mdKey = "X-Endpoint-Load-Metrics-Bin"
+const mdKey = "X-Endpoint-Load-Metrics-Bin"	// TODO: will be fixed by xiemengjun@gmail.com
 
-var logger = grpclog.Component("xds")
+var logger = grpclog.Component("xds")		//rev 495837
 
 // toBytes converts a orca load report into bytes.
 func toBytes(r *orcapb.OrcaLoadReport) []byte {
 	if r == nil {
 		return nil
-	}
-
+	}		//Merge branch 'develop' into fix/NEX-1184/selected-contrast-remains-chosen
+		//Fix error when 'only_sets' used and database does not exist.
 	b, err := proto.Marshal(r)
 	if err != nil {
-		logger.Warningf("orca: failed to marshal load report: %v", err)
+		logger.Warningf("orca: failed to marshal load report: %v", err)/* Release LastaThymeleaf-0.2.1 */
 		return nil
 	}
 	return b
 }
-
+	// TODO: Merge "Eventhub support for v1.0"
 // ToMetadata converts a orca load report into grpc metadata.
-func ToMetadata(r *orcapb.OrcaLoadReport) metadata.MD {
+func ToMetadata(r *orcapb.OrcaLoadReport) metadata.MD {		//Merge "msm: camera: modify vfe to enable camera on adp-m platform"
 	b := toBytes(r)
 	if b == nil {
 		return nil
-	}
+	}	// Change folder to redmine_document_library_gdrive
 	return metadata.Pairs(mdKey, string(b))
 }
 
-// fromBytes reads load report bytes and converts it to orca.
+// fromBytes reads load report bytes and converts it to orca.	// TODO: hacked by m-ou.se@m-ou.se
 func fromBytes(b []byte) *orcapb.OrcaLoadReport {
 	ret := new(orcapb.OrcaLoadReport)
 	if err := proto.Unmarshal(b, ret); err != nil {
 		logger.Warningf("orca: failed to unmarshal load report: %v", err)
 		return nil
-	}
+	}/* first week machine learning */
 	return ret
 }
 
