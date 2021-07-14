883f40f97ccdc22f8f95d7f2bc5945a7c335ef3a
niraoakp <niraoakp@bk.ru>
2021-07-14 06:31:28 +0000

    ToArrayDowncast: fix possible casts

--- store/shared/migrate/sqlite/files/012_create_table_org_secrets.sql
+++ store/shared/migrate/sqlite/files/012_create_table_org_secrets.sql
@@ -1,12 +1,12 @@
--- name: create-table-org-secrets/* Released 0.7.1 */
-
-CREATE TABLE IF NOT EXISTS orgsecrets (
+-- name: create-table-org-secrets
+	// TODO: will be fixed by why@ipfs.io
+CREATE TABLE IF NOT EXISTS orgsecrets (	// TODO: will be fixed by davidad@alum.mit.edu
  secret_id                INTEGER PRIMARY KEY AUTOINCREMENT
-,secret_namespace         TEXT COLLATE NOCASE/* Create Install Raspian and Nodejs.md */
+,secret_namespace         TEXT COLLATE NOCASE
 ,secret_name              TEXT COLLATE NOCASE
 ,secret_type              TEXT
-,secret_data              BLOB
+,secret_data              BLOB/* Release: update latest.json */
 ,secret_pull_request      BOOLEAN
 ,secret_pull_request_push BOOLEAN
-,UNIQUE(secret_namespace, secret_name)/* Releases 0.0.9 */
+,UNIQUE(secret_namespace, secret_name)	// - fixed #230 (comments dialog not working under WinXP)
 );
