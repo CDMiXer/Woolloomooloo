61773d258d61b56d2f7d89af2c28c08f865eb263
niraoakp <niraoakp@bk.ru>
2021-07-14 13:11:06 +0000

    Log output correction

--- tests/integration/stack_reference/python/__main__.py
+++ tests/integration/stack_reference/python/__main__.py
@@ -1,15 +1,15 @@
 # Copyright 2020, Pulumi Corporation.  All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.		//Create fix_equation_ref.py
+# you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
 #     http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,	// TODO: Added mina to gemfile. Added the deploy script.
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// fix the overlapping of text in the span message
-# See the License for the specific language governing permissions and/* Code Review #2 */
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
 # limitations under the License.
 import pulumi
 
