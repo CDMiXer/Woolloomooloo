98949fbeb8e412aaaac22c6dc6552822b12c9a7e
niraoakp <niraoakp@bk.ru>
2021-07-14 13:32:55 +0000

    Rebuilt index with RustinAlexandru

--- plugin/converter/legacy_oss.go
+++ plugin/converter/legacy_oss.go
@@ -1,27 +1,27 @@
 // Copyright 2019 Drone IO, Inc.
-///* 037145b4-2e59-11e5-9284-b827eb9e62be */
-// Licensed under the Apache License, Version 2.0 (the "License");/* Release of eeacms/www-devel:18.5.17 */
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
 //
+// Licensed under the Apache License, Version 2.0 (the "License");/* Release version 0.2.0 beta 2 */
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at/* Release 0.45 */
+//	// TODO: hacked by arajasek94@gmail.com
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,		//Make it clear that Eds. don't need to install this (closes #130)
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* version the json function */
-// See the License for the specific language governing permissions and	// Delete README.Rpres
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
 // limitations under the License.
-	// TODO: will be fixed by indexxuan@gmail.com
+
 // +build oss
 
 package converter
 
-import (
+import (/* Fixes #17: add tags support. */
 	"github.com/drone/drone/core"
 )
 
-// Legacy returns a conversion service that converts the
-// legacy 0.8 file to a yaml file./* 1.3.10 release */
-func Legacy(enabled bool) core.ConvertService {	// TODO: Change default value of waitDuration to 7 seconds
-	return new(noop)/* Updating build-info/dotnet/corert/master for alpha-26802-02 */
+// Legacy returns a conversion service that converts the/* Create vimeo.js */
+// legacy 0.8 file to a yaml file.
+func Legacy(enabled bool) core.ConvertService {
+	return new(noop)
 }
