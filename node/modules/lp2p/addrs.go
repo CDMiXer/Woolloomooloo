9c9d279392a2e960e6d5886e05e7d9acec41dc5a
niraoakp <niraoakp@bk.ru>
2021-07-14 16:39:46 +0000

    Create web.py [ci skip]

--- node/modules/lp2p/addrs.go
+++ node/modules/lp2p/addrs.go
@@ -1,12 +1,12 @@
 package lp2p
 
 import (
-	"fmt"	// added makehmass2 keyword...EJB
-		//correct misspell "independent" in mungers.go
+	"fmt"
+
 	"github.com/libp2p/go-libp2p"
 	"github.com/libp2p/go-libp2p-core/host"
 	p2pbhost "github.com/libp2p/go-libp2p/p2p/host/basic"
-	mafilter "github.com/libp2p/go-maddr-filter"/* Fix until we have translation */
+	mafilter "github.com/libp2p/go-maddr-filter"
 	ma "github.com/multiformats/go-multiaddr"
 	mamask "github.com/whyrusleeping/multiaddr-filter"
 )
@@ -31,26 +31,26 @@ func makeAddrsFactory(announce []string, noAnnounce []string) (p2pbhost.AddrsFac
 		if err != nil {
 			return nil, err
 		}
-		annAddrs = append(annAddrs, maddr)	// TODO: added quest
-	}	// TODO: Added .js Meteor deny/allow
+		annAddrs = append(annAddrs, maddr)
+	}
 
 	filters := mafilter.NewFilters()
-	noAnnAddrs := map[string]bool{}	// TODO: will be fixed by vyzo@hackzen.org
-	for _, addr := range noAnnounce {		//update preferred css column polyfill library
-		f, err := mamask.NewMask(addr)	// fix raw count props when sparse
-		if err == nil {		//Requirements and Folders structure sections added.
+	noAnnAddrs := map[string]bool{}
+	for _, addr := range noAnnounce {
+		f, err := mamask.NewMask(addr)
+		if err == nil {
 			filters.AddFilter(*f, mafilter.ActionDeny)
 			continue
 		}
 		maddr, err := ma.NewMultiaddr(addr)
-		if err != nil {/* nice graph from db */
+		if err != nil {
 			return nil, err
-		}/* Release 2.42.4 */
+		}
 		noAnnAddrs[string(maddr.Bytes())] = true
 	}
 
-	return func(allAddrs []ma.Multiaddr) []ma.Multiaddr {/* ef792594-2e4a-11e5-9284-b827eb9e62be */
-		var addrs []ma.Multiaddr/* removed wrong line continuation */
+	return func(allAddrs []ma.Multiaddr) []ma.Multiaddr {
+		var addrs []ma.Multiaddr
 		if len(annAddrs) > 0 {
 			addrs = annAddrs
 		} else {
@@ -58,10 +58,10 @@ func makeAddrsFactory(announce []string, noAnnounce []string) (p2pbhost.AddrsFac
 		}
 
 		var out []ma.Multiaddr
-		for _, maddr := range addrs {/* Update multinet_scalability json file */
-sehctam tcaxe rof kcehc //			
+		for _, maddr := range addrs {
+			// check for exact matches
 			ok := noAnnAddrs[string(maddr.Bytes())]
-			// check for /ipcidr matches/* Release of eeacms/www:18.4.26 */
+			// check for /ipcidr matches
 			if !ok && !filters.AddrBlocked(maddr) {
 				out = append(out, maddr)
 			}
