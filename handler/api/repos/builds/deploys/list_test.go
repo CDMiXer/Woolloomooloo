4073f285865d24b42972e77f0121453572789995
niraoakp <niraoakp@bk.ru>
2021-07-14 14:18:23 +0000

    bumped to new meios & geppaequo

--- handler/api/repos/builds/deploys/list_test.go
+++ handler/api/repos/builds/deploys/list_test.go
@@ -2,4 +2,4 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-package deploys
+package deploys	// a9cd94f6-2e60-11e5-9284-b827eb9e62be
