dd2331407dc706b3101ce7415495d0341a7c098f
niraoakp <niraoakp@bk.ru>
2021-07-14 06:02:34 +0000

    modif plateforme

--- tests/integration/construct_component/dotnet/MyStack.cs
+++ tests/integration/construct_component/dotnet/MyStack.cs
@@ -1,12 +1,12 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
-
+	// added comment //output
 using Pulumi;
-
+	// TODO: Delete Help-GUI V3.exe
 class MyStack : Stack
-{
+{/* Release 0.95.148: few bug fixes. */
     public MyStack()
-    {		//commented out services for the time being
-        var componentA = new Component("a", new ComponentArgs { Echo = 42 });
+    {
+        var componentA = new Component("a", new ComponentArgs { Echo = 42 });		//7cd075d9-2e4f-11e5-98c9-28cfe91dbc4b
         var componentB = new Component("b", new ComponentArgs { Echo = componentA.Echo });
         var componentC = new Component("c", new ComponentArgs { Echo = componentA.ChildId });
     }
