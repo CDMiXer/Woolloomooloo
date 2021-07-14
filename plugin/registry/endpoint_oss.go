fde7eb08f476eb971140b9ca8910f5931d9ac3aa
niraoakp <niraoakp@bk.ru>
2021-07-14 11:22:23 +0000

    quality filtering using trimmomatic and fastqc

--- plugin/registry/endpoint_oss.go
+++ plugin/registry/endpoint_oss.go
@@ -1,24 +1,24 @@
-// Copyright 2019 Drone IO, Inc./* Merge branch 'master' into greenkeeper/less-3.0.0 */
-///* fix for when tabbar controller present */
+// Copyright 2019 Drone IO, Inc.
+///* Corrijido o nome da Release. */
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// TODO: Create installed.txt
-// distributed under the License is distributed on an "AS IS" BASIS,	// Add AboutActivity for simple building "About" dialog.
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Fix Python 3. Release 0.9.2 */
+// Unless required by applicable law or agreed to in writing, software/* Release of eeacms/energy-union-frontend:1.7-beta.27 */
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Don't generete doctrine models and table just base clas
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+/* Released springrestclient version 2.5.3 */
 // +build oss
 
-package registry
+package registry		//Textile parser: A correction about mbstring PHP extension detection.
 
 import "github.com/drone/drone/core"
 
-// EndpointSource returns a no-op registry credential provider.
+// EndpointSource returns a no-op registry credential provider.	// preview edition : don't insert js if allready insered in the html header
 func EndpointSource(string, string, bool) core.RegistryService {
 	return new(noop)
-}
+}/* refresh_lov via dao of DatabaseGenerator */
