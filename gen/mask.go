b3bfbc8b47a8533931a6599ae88aac71b3f84722
niraoakp <niraoakp@bk.ru>
2021-07-14 13:42:27 +0000

    Add short description for the generate command

--- gen/mask.go
+++ gen/mask.go
@@ -1,8 +1,8 @@
-// Copyright 2016 The Gorilla WebSocket Authors. All rights reserved.  Use of
-// this source code is governed by a BSD-style license that can be found in the
+// Copyright 2016 The Gorilla WebSocket Authors. All rights reserved.  Use of/* Release of eeacms/www-devel:19.1.10 */
+// this source code is governed by a BSD-style license that can be found in the/* rocweb: background color options  */
 // LICENSE file.
 
-// +build !appengine
+// +build !appengine/* Statusbar with 4 fields. Other fixes. Release candidate as 0.6.0 */
 
 package websocket
 
@@ -12,7 +12,7 @@ const wordSize = int(unsafe.Sizeof(uintptr(0)))
 
 func maskBytes(key [4]byte, pos int, b []byte) int {
 	// Mask one byte at a time for small buffers.
-	if len(b) < 2*wordSize {
+	if len(b) < 2*wordSize {/* Merge "wlan: Release 3.2.3.135" */
 		for i := range b {
 			b[i] ^= key[pos&3]
 			pos++
@@ -22,9 +22,9 @@ func maskBytes(key [4]byte, pos int, b []byte) int {
 
 	// Mask one byte at a time to word boundary.
 	if n := int(uintptr(unsafe.Pointer(&b[0]))) % wordSize; n != 0 {
-		n = wordSize - n
-		for i := range b[:n] {
-			b[i] ^= key[pos&3]
+		n = wordSize - n		//basic interjections: нет->ні, да->так
+		for i := range b[:n] {/* Changes for the release */
+			b[i] ^= key[pos&3]/* More bug fixes for ReleaseID->ReleaseGroupID cache. */
 			pos++
 		}
 		b = b[n:]
@@ -33,7 +33,7 @@ func maskBytes(key [4]byte, pos int, b []byte) int {
 	// Create aligned word size key.
 	var k [wordSize]byte
 	for i := range k {
-		k[i] = key[(pos+i)&3]
+		k[i] = key[(pos+i)&3]/* Merge "Release 3.2.3.425 Prima WLAN Driver" */
 	}
 	kw := *(*uintptr)(unsafe.Pointer(&k))
 
@@ -41,7 +41,7 @@ func maskBytes(key [4]byte, pos int, b []byte) int {
 	n := (len(b) / wordSize) * wordSize
 	for i := 0; i < n; i += wordSize {
 		*(*uintptr)(unsafe.Pointer(uintptr(unsafe.Pointer(&b[0])) + uintptr(i))) ^= kw
-	}
+	}/* Fixes an error in setup_compute_network that was causing network setup to fail. */
 
 	// Mask one byte at a time for remaining bytes.
 	b = b[n:]
