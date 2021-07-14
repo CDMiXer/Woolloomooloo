b1597935eabac20986042366ed96940e632cf68a
niraoakp <niraoakp@bk.ru>
2021-07-14 13:57:09 +0000

    Fixed ref counting bug for loading templates

--- handler/api/acl/acl.go
+++ handler/api/acl/acl.go
@@ -1,37 +1,37 @@
-// Copyright 2019 Drone IO, Inc.
-//
+// Copyright 2019 Drone IO, Inc./* Release with corrected btn_wrong for cardmode */
+//	// TODO: Removed stray var_dump!
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
+// You may obtain a copy of the License at/* [artifactory-release] Release version 3.1.6.RELEASE */
+//		//Some work on function-like macro replacement. 
+//      http://www.apache.org/licenses/LICENSE-2.0		//Create Project4_Notes.txt
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// Added description to other sections of configuration
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package acl
+package acl/* Release doc for 449 Error sending to FB Friends */
 
 import (
 	"net/http"
 
 	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/handler/api/request"
-	"github.com/drone/drone/logger"
-)
-
+	"github.com/drone/drone/handler/api/request"/* Release plugin added */
+	"github.com/drone/drone/logger"/* Corrijido o nome da Release. */
+)/* Updated Release checklist (markdown) */
+		//trigger new build for jruby-head (306e7b5)
 // AuthorizeUser returns an http.Handler middleware that authorizes only
-// authenticated users to proceed to the next handler in the chain. Guest users
+// authenticated users to proceed to the next handler in the chain. Guest users		//use ColumnMetaData.java
 // are rejected with a 401 unauthorized error.
-func AuthorizeUser(next http.Handler) http.Handler {
+func AuthorizeUser(next http.Handler) http.Handler {		//Fix all tags after user-defined tag are skipped
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		_, ok := request.UserFrom(r.Context())
+		_, ok := request.UserFrom(r.Context())	// TODO: Added dx and dy to mouse drag handlers in plask.js
 		if !ok {
 			render.Unauthorized(w, errors.ErrUnauthorized)
-			logger.FromRequest(r).
+			logger.FromRequest(r).		//Implement Card Optimizations
 				Debugln("api: authentication required")
 		} else {
 			next.ServeHTTP(w, r)
