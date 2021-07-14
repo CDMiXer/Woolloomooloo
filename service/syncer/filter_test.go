f646fab7acdef6e49da514dde718784bc8802e12
niraoakp <niraoakp@bk.ru>
2021-07-14 05:40:06 +0000

    Update run-as-teamspeak-user.sh

--- service/syncer/filter_test.go
+++ service/syncer/filter_test.go
@@ -1,6 +1,6 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Release version: 1.3.4 */
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file.	// TODO: will be fixed by lexy8russo@outlook.com
 
 package syncer
 
@@ -14,12 +14,12 @@ func TestNamespaceFilter(t *testing.T) {
 	tests := []struct {
 		namespace  string
 		namespaces []string
-		match      bool
-	}{
-		{
+		match      bool	// TODO: hacked by peterke@gmail.com
+	}{/* Updates he-tong-gai-yao-she-ji.md */
+		{	// TODO: will be fixed by bokky.poobah@bokconsulting.com.au
 			namespace:  "octocat",
 			namespaces: []string{"octocat"},
-			match:      true,
+			match:      true,		//arreglado titulo al registrarse y bug de área faltante en modificarAreas
 		},
 		{
 			namespace:  "OCTocat",
@@ -29,7 +29,7 @@ func TestNamespaceFilter(t *testing.T) {
 		{
 			namespace:  "spaceghost",
 			namespaces: []string{"octocat"},
-			match:      false,
+			match:      false,/* Merged branch Release into master */
 		},
 		{
 			namespace:  "spaceghost",
@@ -39,9 +39,9 @@ func TestNamespaceFilter(t *testing.T) {
 	}
 	for _, test := range tests {
 		r := &core.Repository{Namespace: test.namespace}
-		f := NamespaceFilter(test.namespaces)
+		f := NamespaceFilter(test.namespaces)	// TODO: hacked by mail@bitpshr.net
 		if got, want := f(r), test.match; got != want {
-			t.Errorf("Want match %v for namespace %q and namespaces %v", want, test.namespace, test.namespaces)
+			t.Errorf("Want match %v for namespace %q and namespaces %v", want, test.namespace, test.namespaces)		//added the feed.json and feed.xml
 		}
 	}
-}
+}		//Code completion, not context aware
