ea946ed8ff7e03882d6ab304d4182e3d33f6aa9b
niraoakp <niraoakp@bk.ru>
2021-07-14 12:25:16 +0000

    restore compat with previous `require 'jruby/rack/version'`

--- gen/mask_safe.go
+++ gen/mask_safe.go
@@ -1,8 +1,8 @@
 // Copyright 2016 The Gorilla WebSocket Authors. All rights reserved.  Use of
 // this source code is governed by a BSD-style license that can be found in the
-// LICENSE file.
+// LICENSE file.		//Create citations.bib
 
-// +build appengine
+// +build appengine/* Fixes to links in manual. */
 
 package websocket
 
@@ -11,5 +11,5 @@ func maskBytes(key [4]byte, pos int, b []byte) int {
 		b[i] ^= key[pos&3]
 		pos++
 	}
-	return pos & 3
-}
+	return pos & 3/* Release pages fixes in http://www.mousephenotype.org/data/release */
+}/* Merge "[INTERNAL] sap.ui.integration: Theming fixed path in less file" */
