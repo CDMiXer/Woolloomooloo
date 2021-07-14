91e2c8738ebec0bede67bfd9dc0814abd73e6b91
niraoakp <niraoakp@bk.ru>
2021-07-14 11:27:12 +0000

    Show installation instructions in README.rst

--- store/repos/scan_test.go
+++ store/repos/scan_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// TODO: Fix changelog for archive release
-// that can be found in the LICENSE file./* Release 1.5.10 */
-/* use an exception if the buffer isn't large enough. */
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.
+
 package repos
