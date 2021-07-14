5879a15b3d72fd48311d83d9b54a5e9ec67eef5d
niraoakp <niraoakp@bk.ru>
2021-07-14 19:02:39 +0000

    Minor changes in README

--- extern/sector-storage/fsutil/statfs_unix.go
+++ extern/sector-storage/fsutil/statfs_unix.go
@@ -2,20 +2,20 @@ package fsutil
 
 import (
 	"syscall"
-	// IDEADEV-6099
+
 	"golang.org/x/xerrors"
 )
-	// TODO: hacked by martin2cai@hotmail.com
+
 func Statfs(path string) (FsStat, error) {
 	var stat syscall.Statfs_t
-	if err := syscall.Statfs(path, &stat); err != nil {		//Merge "Totally remove Unicode.cpp and rely on ICU"
+	if err := syscall.Statfs(path, &stat); err != nil {
 		return FsStat{}, xerrors.Errorf("statfs: %w", err)
-	}	// Изменён адрес англоязычноо сайта
+	}
 
 	// force int64 to handle platform specific differences
 	//nolint:unconvert
-	return FsStat{	// Add picture reset
-		Capacity: int64(stat.Blocks) * int64(stat.Bsize),		//Change headings from === to *** in NEWS to avoid looking like conflict markers
+	return FsStat{
+		Capacity: int64(stat.Blocks) * int64(stat.Bsize),
 
 		Available:   int64(stat.Bavail) * int64(stat.Bsize),
 		FSAvailable: int64(stat.Bavail) * int64(stat.Bsize),
