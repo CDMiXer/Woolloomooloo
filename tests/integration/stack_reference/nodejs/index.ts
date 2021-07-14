d021dbccf0d7d7afcc336d5a6f71865ec1f802a0
niraoakp <niraoakp@bk.ru>
2021-07-14 06:40:53 +0000

    PressTestEngine is temporarily disabled as it causes YADE crash on start

--- tests/integration/stack_reference/nodejs/index.ts
+++ tests/integration/stack_reference/nodejs/index.ts
@@ -1,9 +1,9 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-/* add comment about farming out to parser */
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// TODO: hacked by mowrain@yandex.com
+
 import * as pulumi from "@pulumi/pulumi";
 
 let config = new pulumi.Config();
-let org = config.require("org");
+let org = config.require("org");	// TODO: working on dependency injection
 let slug = `${org}/${pulumi.getProject()}/${pulumi.getStack()}`;
 let a = new pulumi.StackReference(slug);
 
