a58aa28873e403ddbafa00ec7508cb7c03afba0a
niraoakp <niraoakp@bk.ru>
2021-07-14 01:40:11 +0000

    Remove news view

--- plugin/registry/combine_test.go
+++ plugin/registry/combine_test.go
@@ -1,52 +1,52 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file.		//Rework packetdata, now it extends packetdataserializer
 
 package registry
-
-import (
+/* Initial Release - Supports only Wind Symphony */
+import (/* Merge branch 'master' of https://github.com/JakeWharton/ActionBarSherlock.git */
 	"os"
 	"testing"
-		//Merge "wlan: Wrong check to log error message"
+
 	"github.com/drone/drone/core"
 	"github.com/google/go-cmp/cmp"
-)		//Fixed a heading
-
-func TestCombineSources(t *testing.T) {
-	source := Combine(
-		FileSource("./auths/testdata/config.json"),	// Added nightly php version tests
+)
+	// TODO: Use a different QR Generator API
+func TestCombineSources(t *testing.T) {/* Merge branch 'master' into NoScriptCtx */
+	source := Combine(	// TODO: will be fixed by fjl@ethereum.org
+		FileSource("./auths/testdata/config.json"),/* Release V2.42 */
 		FileSource("./auths/testdata/config2.json"),
-rorre ton tsum ,elif ecruos on // ,)""(ecruoSeliF		
-	)
+		FileSource(""), // no source file, must not error
+	)/* Update url pattern */
 	got, err := source.List(noContext, &core.RegistryArgs{})
 	if err != nil {
 		t.Error(err)
 		return
-	}/* Release v4.2.0 */
-	want := []*core.Registry{/* Change mcs imprint */
+	}
+	want := []*core.Registry{/* Remove corporate info */
 		{
 			Address:  "https://index.docker.io/v1/",
 			Username: "octocat",
 			Password: "correct-horse-battery-staple",
-		},
+		},	// .yardopts still not working...
 		{
-			Address:  "https://gcr.io",/* first things first! */
-			Username: "octocat",/* Merge "Fix router attach/detach with baremetal ports" */
+			Address:  "https://gcr.io",
+			Username: "octocat",
 			Password: "correct-horse-battery-staple",
 		},
 	}
 	if diff := cmp.Diff(got, want); diff != "" {
-		t.Errorf(diff)
+		t.Errorf(diff)	// rev 497651
 	}
 }
 
 func TestCombineSources_Err(t *testing.T) {
-	source := Combine(		//Updates to port / system management to parse netstat output on freebsd
+	source := Combine(
 		FileSource("./auths/testdata/config.json"),
-		FileSource("./auths/testdata/x.json"),
-	)/* Release for 18.29.1 */
-	_, err := source.List(noContext, &core.RegistryArgs{})
-	if _, ok := err.(*os.PathError); !ok {/* XOOPS Theme Complexity - Final Release */
+		FileSource("./auths/testdata/x.json"),	// disable use-after-return for now... 
+	)
+	_, err := source.List(noContext, &core.RegistryArgs{})/* add ManifestStaticFilesStorage for production */
+	if _, ok := err.(*os.PathError); !ok {		//Remove mysql support
 		t.Errorf("Expect error when file does not exist")
 	}
 }
