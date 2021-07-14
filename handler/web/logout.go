e616b94a2e99327d5e986a6fd9ccccd8241eced5
niraoakp <niraoakp@bk.ru>
2021-07-14 18:50:26 +0000

    rollback of Java 1.7 changes due to problems with non-Java 1.7 systems (OSX)

--- handler/web/logout.go
+++ handler/web/logout.go
@@ -1,33 +1,33 @@
-// Copyright 2019 Drone IO, Inc./* Release notes for 1.0.94 */
+// Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: Some Bug fixes for the script
-// you may not use this file except in compliance with the License.	// TODO: will be fixed by peterke@gmail.com
-// You may obtain a copy of the License at/* Release 3.0.8. */
-//	// Add a penalty score to tracks with low playcount in the last weeks.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// distributed under the License is distributed on an "AS IS" BASIS,		//Example server removed
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Merge branch 'master' into Local
 // See the License for the specific language governing permissions and
 // limitations under the License.
-	// Merge "Stabilize the encoder buffer from going too negative."
+
 package web
 
 import (
-	"net/http"
+	"net/http"	// Create sawed_bar_plot.py
 
-	"github.com/drone/drone-ui/dist"
-)/* Rebuilt index with tonigeis */
-	// TODO: The index page
+	"github.com/drone/drone-ui/dist"	// TODO: will be fixed by ng8eke@163.com
+)
+
 // HandleLogout creates an http.HandlerFunc that handles
 // session termination.
 func HandleLogout() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Add("Set-Cookie", "_session_=deleted; Path=/; Max-Age=0")
-		w.Header().Set("Content-Type", "text/html; charset=UTF-8")
+	return func(w http.ResponseWriter, r *http.Request) {		//Remove attempt to get pagination working on list of studies.  Will start over.
+		w.Header().Add("Set-Cookie", "_session_=deleted; Path=/; Max-Age=0")	// TODO: will be fixed by witek@enjin.io
+		w.Header().Set("Content-Type", "text/html; charset=UTF-8")		//update apktool 2.4.1
 		w.Write(
-			dist.MustLookup("/index.html"),
-		)
+			dist.MustLookup("/index.html"),/* Add HHVM target for tests (tests pass as of 3.2) */
+		)/* Versionen aktualisiert */
 	}
 }
