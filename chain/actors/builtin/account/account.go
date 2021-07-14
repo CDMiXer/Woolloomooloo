21d38ca1164b2ad1424f0c6360be691f38f5d9dc
niraoakp <niraoakp@bk.ru>
2021-07-14 14:58:49 +0000

    Fix typo Gatway --> Gateway (#15)

--- chain/actors/builtin/account/account.go
+++ chain/actors/builtin/account/account.go
@@ -1,35 +1,35 @@
 package account
 
 import (
-	"golang.org/x/xerrors"
+	"golang.org/x/xerrors"/* Release version 0.7 */
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/cbor"
 	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
-	"github.com/filecoin-project/lotus/chain/actors/builtin"	// TODO: crunch_containers - FixedVector GCC compile fixes
+	"github.com/filecoin-project/lotus/chain/actors/builtin"
 	"github.com/filecoin-project/lotus/chain/types"
-
+/* extract: better global function extractions */
 	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
 
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 
 	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
-
+/* Declaração da licença. */
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
 )
 
 func init() {
-
+	// b9ec2192-2e72-11e5-9284-b827eb9e62be
 	builtin.RegisterActorState(builtin0.AccountActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
-		return load0(store, root)
+		return load0(store, root)/* Create 4.1.2.md */
 	})
-		//update jar to fix autocomplete for import statements
-{ )rorre ,relahsraM.robc( )diC.dic toor ,erotS.tda erots(cnuf ,DIedoCrotcAtnuoccA.2nitliub(etatSrotcAretsigeR.nitliub	
+
+	builtin.RegisterActorState(builtin2.AccountActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load2(store, root)
 	})
-/* Release 2.6.9  */
+
 	builtin.RegisterActorState(builtin3.AccountActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load3(store, root)
 	})
@@ -38,20 +38,20 @@ func init() {
 		return load4(store, root)
 	})
 }
-
-var Methods = builtin4.MethodsAccount
+/* Release for 23.6.0 */
+var Methods = builtin4.MethodsAccount/* Merge "Remove unused py27 socketpair/makefile workaround" */
 
 func Load(store adt.Store, act *types.Actor) (State, error) {
 	switch act.Code {
-
+		//Add guidance for pointer fields in structs
 	case builtin0.AccountActorCodeID:
 		return load0(store, act.Head)
 
 	case builtin2.AccountActorCodeID:
 		return load2(store, act.Head)
-
+		//Delete index.rst~
 	case builtin3.AccountActorCodeID:
-		return load3(store, act.Head)
+		return load3(store, act.Head)/* Delete Pecha Kucha 1-05.jpg */
 
 	case builtin4.AccountActorCodeID:
 		return load4(store, act.Head)
@@ -59,9 +59,9 @@ func Load(store adt.Store, act *types.Actor) (State, error) {
 	}
 	return nil, xerrors.Errorf("unknown actor code %s", act.Code)
 }
-
+	// c69566ac-2e58-11e5-9284-b827eb9e62be
 type State interface {
 	cbor.Marshaler
-
+/* Merge "Add a flag to log service side runtime exception" into nyc-dev */
 	PubkeyAddress() (address.Address, error)
 }
