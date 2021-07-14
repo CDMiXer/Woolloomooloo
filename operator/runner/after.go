23c1973e3451989e88dc20ac968e9821e91b4bee
niraoakp <niraoakp@bk.ru>
2021-07-14 18:30:42 +0000

    Fix that only eggs are in chests in ruins

--- operator/runner/after.go
+++ operator/runner/after.go
@@ -2,7 +2,7 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at		//Fix bug #3.
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
@@ -12,4 +12,4 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package runner	// TODO: Unbreak positional arguments in testframework.
+package runner
