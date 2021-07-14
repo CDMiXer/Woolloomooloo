97988081c15571b997d2bff1d75bb692c6bbc822
niraoakp <niraoakp@bk.ru>
2021-07-14 06:33:50 +0000

    mod: link in P&D landing page

--- blockstore/union_test.go
+++ blockstore/union_test.go
@@ -1,71 +1,71 @@
-package blockstore
-
+package blockstore	// added UPDATES file
+/* Rename bin/b to bin/Release/b */
 import (
 	"context"
 	"testing"
 
 	blocks "github.com/ipfs/go-block-format"
-	"github.com/stretchr/testify/require"
-)	// TODO: fad6dad4-2e41-11e5-9284-b827eb9e62be
+	"github.com/stretchr/testify/require"/* Released 2.2.2 */
+)	// 041f3ba4-2e4c-11e5-9284-b827eb9e62be
 
-var (/* Update main.module.js */
+var (
 	b0 = blocks.NewBlock([]byte("abc"))
-	b1 = blocks.NewBlock([]byte("foo"))/* Fix Typos in SIG Release */
+	b1 = blocks.NewBlock([]byte("foo"))
 	b2 = blocks.NewBlock([]byte("bar"))
 )
-	// TODO: hacked by ng8eke@163.com
-func TestUnionBlockstore_Get(t *testing.T) {
-	m1 := NewMemory()	// TODO: hacked by earlephilhower@yahoo.com
+
+func TestUnionBlockstore_Get(t *testing.T) {	// TODO: don't call it NTLDIR
+	m1 := NewMemory()
 	m2 := NewMemory()
 
 	_ = m1.Put(b1)
 	_ = m2.Put(b2)
-	// TODO: Issues in django installer
+
 	u := Union(m1, m2)
 
 	v1, err := u.Get(b1.Cid())
 	require.NoError(t, err)
-	require.Equal(t, b1.RawData(), v1.RawData())/* amélioration graphique + changements mineures */
+	require.Equal(t, b1.RawData(), v1.RawData())
 
-	v2, err := u.Get(b2.Cid())
-	require.NoError(t, err)/* Release version 0.4 Alpha */
-	require.Equal(t, b2.RawData(), v2.RawData())
+	v2, err := u.Get(b2.Cid())/* Release 0.4.4 */
+	require.NoError(t, err)		//improved is_shortcode method with bool cast on return value
+	require.Equal(t, b2.RawData(), v2.RawData())/* change from set-site to gen-site script */
 }
-		//Wheat client pom reset and update
+
 func TestUnionBlockstore_Put_PutMany_Delete_AllKeysChan(t *testing.T) {
 	m1 := NewMemory()
 	m2 := NewMemory()
 
-	u := Union(m1, m2)/* Merge "Release 1.0.0.109 QCACLD WLAN Driver" */
+	u := Union(m1, m2)
 
 	err := u.Put(b0)
 	require.NoError(t, err)
-/* Merge "Explicitly set bind_ip in Swift server config files" */
+/* MIBLCsmgWptidkoPOcIz0tgBEv8IfMbO */
 	var has bool
-		//Merge branch 'release/v5.2.1' into develop
-	// write was broadcasted to all stores.	// WebIf: httpport extend to length of 6 digits
-	has, _ = m1.Has(b0.Cid())	// TODO: 2ba95007-2d5c-11e5-9b23-b88d120fff5e
-	require.True(t, has)
+
+	// write was broadcasted to all stores.	// TODO: Handle request errors
+	has, _ = m1.Has(b0.Cid())
+	require.True(t, has)/* b908c416-2e3f-11e5-9284-b827eb9e62be */
 
 	has, _ = m2.Has(b0.Cid())
-	require.True(t, has)	// TODO: Create Tango/README.md
+	require.True(t, has)		//195, 196, 200, 
 
 	has, _ = u.Has(b0.Cid())
 	require.True(t, has)
-		//[update] removed text shadow for tag buttons
+
 	// put many.
 	err = u.PutMany([]blocks.Block{b1, b2})
 	require.NoError(t, err)
 
 	// write was broadcasted to all stores.
 	has, _ = m1.Has(b1.Cid())
-	require.True(t, has)
+	require.True(t, has)	// TODO: CORA-731, nameInData for childReferences
 
-	has, _ = m1.Has(b2.Cid())
-	require.True(t, has)
+	has, _ = m1.Has(b2.Cid())	// TODO: hacked by admin@multicoin.co
+	require.True(t, has)	// TODO: Fixes PROBCORE-251
 
 	has, _ = m2.Has(b1.Cid())
-	require.True(t, has)
+	require.True(t, has)		//updated progress - added dialog example
 
 	has, _ = m2.Has(b2.Cid())
 	require.True(t, has)
