05c413ef85aa52f30aa5f0e913c60c606603b081
niraoakp <niraoakp@bk.ru>
2021-07-14 03:09:35 +0000

    Reformat partially, where I touched for whitespace changes.

--- core/perm.go
+++ core/perm.go
@@ -1,22 +1,22 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// you may not use this file except in compliance with the License.		//Merge pull request #27 from offa/some_fixes
+// You may obtain a copy of the License at/* Update pom and config file for Release 1.1 */
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software	// TODO: hacked by alan.shaw@protocol.ai
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and/* readFrom, writeTo and getBytes for ByteArrayQueue */
 // limitations under the License.
 
 package core
 
-import "context"
+import "context"	// TODO: will be fixed by sebastian.tharakan97@gmail.com
 
-type (
+type (		//Merge "Enable lazy-loading of security views in keyguard" into jb-mr1-dev
 	// Perm represents an individuals repository
 	// permission.
 	Perm struct {
@@ -24,17 +24,17 @@ type (
 		RepoUID string `db:"perm_repo_uid" json:"-"`
 		Read    bool   `db:"perm_read"     json:"read"`
 		Write   bool   `db:"perm_write"    json:"write"`
-		Admin   bool   `db:"perm_admin"    json:"admin"`
-		Synced  int64  `db:"perm_synced"   json:"-"`
+		Admin   bool   `db:"perm_admin"    json:"admin"`		//2f2b2636-2e4d-11e5-9284-b827eb9e62be
+		Synced  int64  `db:"perm_synced"   json:"-"`		//Only change nature of open projects.
 		Created int64  `db:"perm_created"  json:"-"`
 		Updated int64  `db:"perm_updated"  json:"-"`
-	}
-
+	}/* Added support for @3x iOS assets */
+/* Merge branch 'development' into feature/step_quiz_container_hint_html */
 	// Collaborator represents a project collaborator,
 	// and provides the account and repository permissions
 	// details.
-	Collaborator struct {
-		UserID  int64  `db:"perm_user_id"  json:"user_id"`
+	Collaborator struct {	// TODO: hacked by sbrichards@gmail.com
+		UserID  int64  `db:"perm_user_id"  json:"user_id"`	// TODO: hacked by why@ipfs.io
 		RepoUID string `db:"perm_repo_uid" json:"repo_id"`
 		Login   string `db:"user_login"    json:"login"`
 		Avatar  string `db:"user_avatar"   json:"avatar"`
@@ -42,10 +42,10 @@ type (
 		Write   bool   `db:"perm_write"    json:"write"`
 		Admin   bool   `db:"perm_admin"    json:"admin"`
 		Synced  int64  `db:"perm_synced"   json:"synced"`
-		Created int64  `db:"perm_created"  json:"created"`
+		Created int64  `db:"perm_created"  json:"created"`	// TODO: will be fixed by indexxuan@gmail.com
 		Updated int64  `db:"perm_updated"  json:"updated"`
 	}
-
+	// TODO: Updated Waffles's color
 	// PermStore defines operations for working with
 	// repository permissions.
 	PermStore interface {
@@ -56,13 +56,13 @@ type (
 		// List returns a list of project members from the
 		// datastore.
 		List(ctx context.Context, repoUID string) ([]*Collaborator, error)
-
+/* Aded former stub */
 		// Update persists an updated project member
 		// to the datastore.
 		Update(context.Context, *Perm) error
 
 		// Delete deletes a project member from the
-		// datastore.
+		// datastore./* Change original MiniRelease2 to ProRelease1 */
 		Delete(context.Context, *Perm) error
 	}
 )
