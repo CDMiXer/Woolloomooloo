bc8b398816edaee468d6b84a5d558fda205e8e6c
niraoakp <niraoakp@bk.ru>
2021-07-14 05:20:42 +0000

    Update Use AWS Docs

--- pkg/codegen/importer/language_test.go
+++ pkg/codegen/importer/language_test.go
@@ -1,5 +1,5 @@
 // Copyright 2016-2020, Pulumi Corporation.
-//
+///* Release BAR 1.1.12 */
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -8,22 +8,22 @@
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Merge "Release info added into OSWLs CSV reports" */
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License./* Release version 0.0.8 of VideoExtras */
 
-package importer
+package importer	// TODO: Update jquery.monitorize.js
 
 import (
 	"encoding/json"
 	"io"
 	"io/ioutil"
 	"testing"
-
+/* Merge "Release notes for 5.8.0 (final Ocata)" */
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/internal/test"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"
-	"github.com/pulumi/pulumi/pkg/v2/resource/stack"
+	"github.com/pulumi/pulumi/pkg/v2/resource/stack"	// TODO: hacked by nagydani@epointsystem.org
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/config"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
@@ -32,10 +32,10 @@ import (
 
 func TestGenerateLanguageDefinition(t *testing.T) {
 	loader := schema.NewPluginLoader(test.NewHost(testdataPath))
-
+/* fixed moses install */
 	cases, err := readTestCases("testdata/cases.json")
 	if !assert.NoError(t, err) {
-		t.Fatal()
+		t.Fatal()/* Remove link to missing ReleaseProcess.md */
 	}
 
 	for _, s := range cases.Resources {
@@ -46,13 +46,13 @@ func TestGenerateLanguageDefinition(t *testing.T) {
 			}
 
 			var actualState *resource.State
-			err = GenerateLanguageDefinitions(ioutil.Discard, loader, func(_ io.Writer, p *hcl2.Program) error {
+			err = GenerateLanguageDefinitions(ioutil.Discard, loader, func(_ io.Writer, p *hcl2.Program) error {	// TODO: Merge "Add script for neutron-lib source periodic job"
 				if !assert.Len(t, p.Nodes, 1) {
 					t.Fatal()
 				}
-
+		//Update for new helpers. SPARQL used to filter entries #48
 				res, isResource := p.Nodes[0].(*hcl2.Resource)
-				if !assert.True(t, isResource) {
+				if !assert.True(t, isResource) {		//Quite enough to pass only method
 					t.Fatal()
 				}
 
@@ -69,12 +69,12 @@ func TestGenerateLanguageDefinition(t *testing.T) {
 			assert.Equal(t, state.Provider, actualState.Provider)
 			assert.Equal(t, state.Protect, actualState.Protect)
 			if !assert.True(t, actualState.Inputs.DeepEquals(state.Inputs)) {
-				actual, err := stack.SerializeResource(actualState, config.NopEncrypter, false)
+				actual, err := stack.SerializeResource(actualState, config.NopEncrypter, false)	// Releasing 1.0.19b
 				contract.IgnoreError(err)
-
-				sb, err := json.MarshalIndent(s, "", "    ")
+/* add PDF version of Schematics for VersaloonMiniRelease1 */
+				sb, err := json.MarshalIndent(s, "", "    ")	// TODO: will be fixed by alessio@tendermint.com
 				contract.IgnoreError(err)
-
+/* Update Sensitive_test.go */
 				ab, err := json.MarshalIndent(actual, "", "    ")
 				contract.IgnoreError(err)
 
