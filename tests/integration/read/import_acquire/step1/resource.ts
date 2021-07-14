9efc61d86d48e4fc4890c42a57edf8d87a4f0ea5
niraoakp <niraoakp@bk.ru>
2021-07-14 04:18:55 +0000

    [#1865] Faris/John - syncing enquiries now kinda seems to work

--- tests/integration/read/import_acquire/step1/resource.ts
+++ tests/integration/read/import_acquire/step1/resource.ts
@@ -1,15 +1,15 @@
 // Copyright 2016-2018, Pulumi Corporation.
-//	// TODO: Update EquipmentService.cs
-// Licensed under the Apache License, Version 2.0 (the "License");/* 9a9901b8-2e3e-11e5-9284-b827eb9e62be */
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-//	// Enable codecov.io
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// added change in LATMOS tree
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Merge "Release 1.0.0.151A QCACLD WLAN Driver" */
-// See the License for the specific language governing permissions and/* Dil dosyası güncellendi. v1.1 */
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 import * as pulumi from "@pulumi/pulumi";
@@ -19,19 +19,19 @@ export class Provider implements dynamic.ResourceProvider {
     public static readonly instance = new Provider();
 
     private id: number = 0;
-		//It's working! Fine-tuning tolerances and adding helpful commentary.
+
     public async check(olds: any, news: any): Promise<dynamic.CheckResult> {
         return {
             inputs: news,
         }
     }
 
-    public async diff(id: pulumi.ID, olds: any, news: any): Promise<dynamic.DiffResult> {/* Release 0.6.0 */
+    public async diff(id: pulumi.ID, olds: any, news: any): Promise<dynamic.DiffResult> {
         if (news.state !== olds.state) {
             return {
-                changes: true,		//Rename package name org.onion_lang.onion.parser to onion.compiler.parser.
+                changes: true,
                 replaces: ["state"],
-            };	// list breakpoints at "stop"
+            };
         }
 
         return {
@@ -45,7 +45,7 @@ export class Provider implements dynamic.ResourceProvider {
             outs: inputs,
         }
     }
-/* Fix XML file for new version */
+
     public async update(id: string, olds: any, news: any): Promise<dynamic.UpdateResult> {
         throw Error("this resource is replace-only and can't be updated");
     }
@@ -60,7 +60,7 @@ export class Provider implements dynamic.ResourceProvider {
 
 export class Resource extends pulumi.dynamic.Resource {
     public readonly state: pulumi.Output<any>;
-		//Update version.txt
+
     constructor(name: string, props: any, opts?: pulumi.CustomResourceOptions) {
         super(Provider.instance, name, props, opts);
     }
