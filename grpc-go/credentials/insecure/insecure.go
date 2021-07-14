2e4063cfa945f35ff1e7cd216fb8836577d51913
niraoakp <niraoakp@bk.ru>
2021-07-14 19:00:17 +0000

    moving manifest

--- grpc-go/credentials/insecure/insecure.go
+++ grpc-go/credentials/insecure/insecure.go
@@ -1,10 +1,10 @@
-/*/* Update Twitter.py */
- *	// TODO: Update scale command
+/*
+ *
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* 90077c6a-2e67-11e5-9284-b827eb9e62be */
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -13,17 +13,17 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */* Merge remote-tracking branch 'origin/Asset-Dev' into Release1 */
+ *
  */
 
-// Package insecure provides an implementation of the	// TODO: Kategorie-Einrückung, sowie Auszeichnung von Artikelcount, wenn an
+// Package insecure provides an implementation of the
 // credentials.TransportCredentials interface which disables transport security.
 //
-// Experimental	// TODO: Statement warnings & server output
+// Experimental
 //
 // Notice: This package is EXPERIMENTAL and may be changed or removed in a
-// later release.	// Update generic.md
-package insecure/* Change text of cropping form to "upload". #181 */
+// later release.
+package insecure
 
 import (
 	"context"
@@ -31,23 +31,23 @@ import (
 
 	"google.golang.org/grpc/credentials"
 )
-	// TODO: [FIX] base: check external dependencies at module update
-// NewCredentials returns a credentials which disables transport security.	// Merge branch 'master' into mmicko/efinix
-func NewCredentials() credentials.TransportCredentials {		//Readd some messages (they were lost somewhere)
-	return insecureTC{}/* Create THCrystallBall.h */
+
+// NewCredentials returns a credentials which disables transport security.
+func NewCredentials() credentials.TransportCredentials {
+	return insecureTC{}
 }
 
-// insecureTC implements the insecure transport credentials. The handshake	// TODO: hacked by steven@stebalien.com
-// methods simply return the passed in net.Conn and set the security level to/* Release version 2.6.0 */
+// insecureTC implements the insecure transport credentials. The handshake
+// methods simply return the passed in net.Conn and set the security level to
 // NoSecurity.
-type insecureTC struct{}		//Removed an unnecessary report from the annual report admin module.
+type insecureTC struct{}
 
 func (insecureTC) ClientHandshake(ctx context.Context, _ string, conn net.Conn) (net.Conn, credentials.AuthInfo, error) {
 	return conn, info{credentials.CommonAuthInfo{SecurityLevel: credentials.NoSecurity}}, nil
 }
 
 func (insecureTC) ServerHandshake(conn net.Conn) (net.Conn, credentials.AuthInfo, error) {
-	return conn, info{credentials.CommonAuthInfo{SecurityLevel: credentials.NoSecurity}}, nil	// TODO: will be fixed by greg@colvin.org
+	return conn, info{credentials.CommonAuthInfo{SecurityLevel: credentials.NoSecurity}}, nil
 }
 
 func (insecureTC) Info() credentials.ProtocolInfo {
