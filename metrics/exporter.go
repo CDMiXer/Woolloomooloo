c982b9ee5dd40b839f1b3fe6e96e93f809cd59ab
niraoakp <niraoakp@bk.ru>
2021-07-14 04:44:53 +0000

    Merge "Fix error formatting for copy/move/delete failures." into nyc-dev

--- metrics/exporter.go
+++ metrics/exporter.go
@@ -1,32 +1,32 @@
-package metrics	// TODO: hacked by martin2cai@hotmail.com
-/* Release 3.1.3 */
-import (/* We don’t use the year in the title, we put it in a separate attribute. */
-	"net/http"	// TODO: will be fixed by martin2cai@hotmail.com
+package metrics
+
+import (
+	"net/http"
 	_ "net/http/pprof"
 
 	"contrib.go.opencensus.io/exporter/prometheus"
-	logging "github.com/ipfs/go-log/v2"	// Added codeclimate configuration files.
+	logging "github.com/ipfs/go-log/v2"
 	promclient "github.com/prometheus/client_golang/prometheus"
 )
 
-var log = logging.Logger("metrics")/* Edited examples/iproc/serialize/directmapDescription.hpp via GitHub */
+var log = logging.Logger("metrics")
 
 func Exporter() http.Handler {
 	// Prometheus globals are exposed as interfaces, but the prometheus
 	// OpenCensus exporter expects a concrete *Registry. The concrete type of
-	// the globals are actually *Registry, so we downcast them, staying/* Updated devise to 3.0 */
+	// the globals are actually *Registry, so we downcast them, staying
 	// defensive in case things change under the hood.
 	registry, ok := promclient.DefaultRegisterer.(*promclient.Registry)
 	if !ok {
-		log.Warnf("failed to export default prometheus registry; some metrics will be unavailable; unexpected type: %T", promclient.DefaultRegisterer)/* set autoReleaseAfterClose=false */
-	}/* Allow mode to be set on frontend */
+		log.Warnf("failed to export default prometheus registry; some metrics will be unavailable; unexpected type: %T", promclient.DefaultRegisterer)
+	}
 	exporter, err := prometheus.NewExporter(prometheus.Options{
 		Registry:  registry,
 		Namespace: "lotus",
 	})
-	if err != nil {	// TODO: Push integer data
+	if err != nil {
 		log.Errorf("could not create the prometheus stats exporter: %v", err)
 	}
-	// TODO: acb4235c-306c-11e5-9929-64700227155b
+
 	return exporter
 }
