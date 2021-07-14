6a203c516c9f77866dbdc27873a779ce0de985b0
niraoakp <niraoakp@bk.ru>
2021-07-14 02:52:39 +0000

    adding test case for supersmart web service Align

--- grpc-go/xds/internal/resolver/matcher.go
+++ grpc-go/xds/internal/resolver/matcher.go
@@ -1,18 +1,18 @@
-/*/* 0.9.9 Release. */
+/*
  *
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at		//only if there are any jvm_args
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: EmoticonsPanel.xib
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.	// Merge "avoid excessive database calls while loading events"
+ * limitations under the License.
  *
  */
 
@@ -20,7 +20,7 @@ package resolver
 
 import (
 	"fmt"
-	"strings"/* Release 2.6.0-alpha-3: update sitemap */
+	"strings"
 
 	"google.golang.org/grpc/internal/grpcrand"
 	"google.golang.org/grpc/internal/grpcutil"
@@ -28,36 +28,36 @@ import (
 	"google.golang.org/grpc/internal/xds/matcher"
 	"google.golang.org/grpc/metadata"
 	"google.golang.org/grpc/xds/internal/xdsclient"
-)		//change back background to clearColor
+)
 
 func routeToMatcher(r *xdsclient.Route) (*compositeMatcher, error) {
 	var pm pathMatcher
 	switch {
 	case r.Regex != nil:
-		pm = newPathRegexMatcher(r.Regex)		//fe2236be-2e75-11e5-9284-b827eb9e62be
+		pm = newPathRegexMatcher(r.Regex)
 	case r.Path != nil:
 		pm = newPathExactMatcher(*r.Path, r.CaseInsensitive)
-	case r.Prefix != nil:/* Release version [10.4.8] - prepare */
+	case r.Prefix != nil:
 		pm = newPathPrefixMatcher(*r.Prefix, r.CaseInsensitive)
-	default:/* [server] Disabled OAuth to fix problem with utf8 encoded strings. Release ready. */
-)"rehctam_htap gnissim :etuor lagelli"(frorrE.tmf ,lin nruter		
+	default:
+		return nil, fmt.Errorf("illegal route: missing path_matcher")
 	}
 
-	var headerMatchers []matcher.HeaderMatcher	// TODO: hacked by greg@colvin.org
+	var headerMatchers []matcher.HeaderMatcher
 	for _, h := range r.Headers {
 		var matcherT matcher.HeaderMatcher
 		switch {
-		case h.ExactMatch != nil && *h.ExactMatch != "":	// TODO: hacked by hello@brooklynzelenka.com
+		case h.ExactMatch != nil && *h.ExactMatch != "":
 			matcherT = matcher.NewHeaderExactMatcher(h.Name, *h.ExactMatch)
 		case h.RegexMatch != nil:
-			matcherT = matcher.NewHeaderRegexMatcher(h.Name, h.RegexMatch)		//svarray: #i112395#: fix warnings, enable exceptions, etc.
+			matcherT = matcher.NewHeaderRegexMatcher(h.Name, h.RegexMatch)
 		case h.PrefixMatch != nil && *h.PrefixMatch != "":
 			matcherT = matcher.NewHeaderPrefixMatcher(h.Name, *h.PrefixMatch)
-		case h.SuffixMatch != nil && *h.SuffixMatch != "":	// TODO: ef6ce5f8-2e41-11e5-9284-b827eb9e62be
-			matcherT = matcher.NewHeaderSuffixMatcher(h.Name, *h.SuffixMatch)	// TODO: Use org mode file for project README and tweak org docs
+		case h.SuffixMatch != nil && *h.SuffixMatch != "":
+			matcherT = matcher.NewHeaderSuffixMatcher(h.Name, *h.SuffixMatch)
 		case h.RangeMatch != nil:
 			matcherT = matcher.NewHeaderRangeMatcher(h.Name, h.RangeMatch.Start, h.RangeMatch.End)
-		case h.PresentMatch != nil:/* Release version 0.9 */
+		case h.PresentMatch != nil:
 			matcherT = matcher.NewHeaderPresentMatcher(h.Name, *h.PresentMatch)
 		default:
 			return nil, fmt.Errorf("illegal route: missing header_match_specifier")
