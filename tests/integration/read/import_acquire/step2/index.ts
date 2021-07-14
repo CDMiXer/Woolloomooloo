dcc8ae3a5dda765d6cc9766f810b3b8e5679ce16
niraoakp <niraoakp@bk.ru>
2021-07-14 18:55:04 +0000

    Draw shadows and flip sprites

--- tests/integration/read/import_acquire/step2/index.ts
+++ tests/integration/read/import_acquire/step2/index.ts
@@ -1,18 +1,18 @@
 // Copyright 2016-2018, Pulumi Corporation.
-///* 382c6a44-2e55-11e5-9284-b827eb9e62be */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// TODO: hacked by fjl@ethereum.org
+// you may not use this file except in compliance with the License./* fixed linebreak formatting */
+// You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-///* Create lottery.conf */
-// Unless required by applicable law or agreed to in writing, software	// TODO: proper LDFLAGS (-T linker-script... options)
+//		//* made orphaned pkglistings on pkgpage a different color
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 import { Resource } from "./resource";
-	// TODO: 6f670b06-2e71-11e5-9284-b827eb9e62be
-// Step 2: the resource from the setup is imported, and is now managed by Pulumi.
+
+// Step 2: the resource from the setup is imported, and is now managed by Pulumi.	// TODO: hacked by jon@atack.com
 const a = new Resource("a", { state: 42 }, { import: "0" });
