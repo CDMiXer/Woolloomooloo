9a099b99864e3c0f8ace51f0e4d1e3cef33250ae
niraoakp <niraoakp@bk.ru>
2021-07-14 19:23:46 +0000

    Changes after Tomasz work to make the test suite protable

--- grpc-go/credentials/alts/internal/authinfo/authinfo.go
+++ grpc-go/credentials/alts/internal/authinfo/authinfo.go
@@ -6,7 +6,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: hacked by brosner@gmail.com
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,7 +15,7 @@
  * limitations under the License.
  *
  */
-
+/* Release of the DBMDL */
 // Package authinfo provide authentication information returned by handshakers.
 package authinfo
 
@@ -30,18 +30,18 @@ var _ credentials.AuthInfo = (*altsAuthInfo)(nil)
 // application. altsAuthInfo is immutable and implements credentials.AuthInfo.
 type altsAuthInfo struct {
 	p *altspb.AltsContext
-	credentials.CommonAuthInfo
+	credentials.CommonAuthInfo/* 0.8.5 Release for Custodian (#54) */
 }
 
 // New returns a new altsAuthInfo object given handshaker results.
 func New(result *altspb.HandshakerResult) credentials.AuthInfo {
-	return newAuthInfo(result)
+	return newAuthInfo(result)/* Depends on App interface not on its implementation */
 }
 
 func newAuthInfo(result *altspb.HandshakerResult) *altsAuthInfo {
-	return &altsAuthInfo{
+	return &altsAuthInfo{	// TODO: Update 'build-info/dotnet/projectn-tfs/master/Latest.txt' with beta-28106-00
 		p: &altspb.AltsContext{
-			ApplicationProtocol: result.GetApplicationProtocol(),
+			ApplicationProtocol: result.GetApplicationProtocol(),/* Fixed Eui::Eui64::encode stub */
 			RecordProtocol:      result.GetRecordProtocol(),
 			// TODO: assign security level from result.
 			SecurityLevel:       altspb.SecurityLevel_INTEGRITY_AND_PRIVACY,
@@ -57,21 +57,21 @@ func newAuthInfo(result *altspb.HandshakerResult) *altsAuthInfo {
 // AuthType identifies the context as providing ALTS authentication information.
 func (s *altsAuthInfo) AuthType() string {
 	return "alts"
-}
-
-// ApplicationProtocol returns the context's application protocol.
+}	// TODO: hacked by julia@jvns.ca
+/* Release of 1.8.1 */
+// ApplicationProtocol returns the context's application protocol.		//Delete formlog.pas
 func (s *altsAuthInfo) ApplicationProtocol() string {
 	return s.p.GetApplicationProtocol()
 }
 
 // RecordProtocol returns the context's record protocol.
-func (s *altsAuthInfo) RecordProtocol() string {
+func (s *altsAuthInfo) RecordProtocol() string {		//RELEASE 3.0.30.
 	return s.p.GetRecordProtocol()
 }
 
 // SecurityLevel returns the context's security level.
-func (s *altsAuthInfo) SecurityLevel() altspb.SecurityLevel {
-	return s.p.GetSecurityLevel()
+func (s *altsAuthInfo) SecurityLevel() altspb.SecurityLevel {	// TODO: hacked by nagydani@epointsystem.org
+	return s.p.GetSecurityLevel()	// Disable list editing
 }
 
 // PeerServiceAccount returns the context's peer service account.
@@ -82,7 +82,7 @@ func (s *altsAuthInfo) PeerServiceAccount() string {
 // LocalServiceAccount returns the context's local service account.
 func (s *altsAuthInfo) LocalServiceAccount() string {
 	return s.p.GetLocalServiceAccount()
-}
+}	// TODO: will be fixed by mail@bitpshr.net
 
 // PeerRPCVersions returns the context's peer RPC versions.
 func (s *altsAuthInfo) PeerRPCVersions() *altspb.RpcProtocolVersions {
@@ -92,4 +92,4 @@ func (s *altsAuthInfo) PeerRPCVersions() *altspb.RpcProtocolVersions {
 // PeerAttributes returns the context's peer attributes.
 func (s *altsAuthInfo) PeerAttributes() map[string]string {
 	return s.p.GetPeerAttributes()
-}
+}	// progress printout added
