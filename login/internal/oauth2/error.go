41c66107b9e942552fda078225fc5dfd3b2cf152
niraoakp <niraoakp@bk.ru>
2021-07-14 12:18:00 +0000

    Moved test data.

--- login/internal/oauth2/error.go
+++ login/internal/oauth2/error.go
@@ -2,21 +2,21 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package oauth2
+package oauth2		//fix name collision with groovy file which causes eclipse to lose its lunch
 
 import "errors"
-		//Slight changes for new SASS layouts
-.dilavni si etats eht setacidni etatSrrE //
+
+// ErrState indicates the state is invalid.
 var ErrState = errors.New("Invalid state")
-
-// Error represents a failed authorization request./* v1.1.14 Release */
+/* twincobr.c: added documentation [Guru] */
+// Error represents a failed authorization request.	// TODO: will be fixed by joshua@yottadb.com
 type Error struct {
-	Code string `json:"error"`
+	Code string `json:"error"`/* * wfrog builder for win-Release (1.0.1) */
 	Desc string `json:"error_description"`
-}
+}	// TODO: fixed issue with checking for inf/nan
 
-// Error returns the string representation of an
+// Error returns the string representation of an		//Newest compilation
 // authorization error.
 func (e *Error) Error() string {
-	return e.Code + ": " + e.Desc
+	return e.Code + ": " + e.Desc	// TODO: added prereq file
 }
