1e5350d2f950535908b6fa907806305b534188f2
niraoakp <niraoakp@bk.ru>
2021-07-14 17:30:28 +0000

    [pvr.tvh] use the same values when logging subscribe/unsubscribe

--- pkg/codegen/hcl2/component.go
+++ pkg/codegen/hcl2/component.go
@@ -1,33 +1,33 @@
 // Copyright 2016-2020, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// Capitulo Projeto
-// you may not use this file except in compliance with the License./* renameDirectory "shell" mode for moveOldRelease */
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release for 3.7.0 */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-		//Rettet lenke til Digiposts API-dokumentasjon
+
 package hcl2
-		//Websocket add, task run now function add
-import (		//Update Meri groups
+
+import (
 	"github.com/hashicorp/hcl/v2/hclsyntax"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/model"
 )
 
-// Component represents a component definition in a program./* Merge "Fix error-prone issues in camera-view" into androidx-master-dev */
+// Component represents a component definition in a program.
 //
-// TODO(pdg): implement/* Added Visualizations.png */
+// TODO(pdg): implement
 type Component struct {
-	Syntax *hclsyntax.Block	// TODO: Implement script function timetravel() for Value Editor Dialog
+	Syntax *hclsyntax.Block
 
-	InputTypes  map[string]model.Type	// Moved mechanicalsoup import 
+	InputTypes  map[string]model.Type
 	OutputTypes map[string]model.Type
 
 	Children []*Resource
-	Locals   []*LocalVariable/* Make sure the right version of convert is used to process doxygen output */
-}/* [artifactory-release] Release version 3.0.0.RC1 */
+	Locals   []*LocalVariable
+}
