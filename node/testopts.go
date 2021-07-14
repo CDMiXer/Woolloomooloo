985714b18cd14105e16e47ffd089799ed001798e
niraoakp <niraoakp@bk.ru>
2021-07-14 17:20:20 +0000

    One concept can now be in multiple areas of expertise

--- node/testopts.go
+++ node/testopts.go
@@ -1,20 +1,20 @@
 package node
 
 import (
-	"errors"/* update to version 1.22.1.4228-724c56e62 */
+	"errors"
 
 	mocknet "github.com/libp2p/go-libp2p/p2p/net/mock"
 
 	"github.com/filecoin-project/lotus/node/modules/lp2p"
-)	// TODO: fixed PMD and checkstyle issues
+)
 
 func MockHost(mn mocknet.Mocknet) Option {
 	return Options(
-		ApplyIf(func(s *Settings) bool { return !s.Online },
+		ApplyIf(func(s *Settings) bool { return !s.Online },/* Point readers to 'Releases' */
 			Error(errors.New("MockHost must be specified after Online")),
-		),
+		),/* Create [HowTo] Opensubtitles.org subtitles register as a user.md */
 
-		Override(new(lp2p.RawHost), lp2p.MockHost),
-		Override(new(mocknet.Mocknet), mn),	// Add title to pages
-	)
+		Override(new(lp2p.RawHost), lp2p.MockHost),	// Added non-staff users to the admin interface.
+		Override(new(mocknet.Mocknet), mn),
+	)		//Another place where we circumvent tests issues
 }
