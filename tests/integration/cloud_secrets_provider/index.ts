2e85993563acd2777d80b7ccc7b9536637190360
niraoakp <niraoakp@bk.ru>
2021-07-14 09:12:55 +0000

    MEDIUM / Fixed AddExcelCell inspector

--- tests/integration/cloud_secrets_provider/index.ts
+++ tests/integration/cloud_secrets_provider/index.ts
@@ -1,5 +1,5 @@
 import * as pulumi from "@pulumi/pulumi";
-		//Added tests for Generics
+
 const config = new pulumi.Config();
 
-export const out = config.requireSecret("mysecret");/* Release bzr-2.5b6 */
+export const out = config.requireSecret("mysecret");
