9dcd32776bf45475e55f8fb985b7d4364d7afb2c
niraoakp <niraoakp@bk.ru>
2021-07-14 21:34:00 +0000

    Merge pull request #3256 from XhmikosR/accessibility-tweaks

--- tests/integration/custom_timeouts/python/success/__main__.py
+++ tests/integration/custom_timeouts/python/success/__main__.py
@@ -1,25 +1,25 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+/* Release 2.8.5 */
 from pulumi import ComponentResource, CustomTimeouts, Resource, ResourceOptions
 
-class Resource1(ComponentResource):
-    def __init__(self, name, opts=None):		//change AlfrescoViewEditor steretype to ModelManagementSystem
-        super().__init__("my:module:Resource", name, None, opts)		//Update JAR
-
-# Attempt to create a resource with a CustomTimeout	// README: Added documentation on discarding pixels
+class Resource1(ComponentResource):		//Remove UnresolvedDependency type
+    def __init__(self, name, opts=None):
+        super().__init__("my:module:Resource", name, None, opts)
+		//1st commit - bootstrap
+# Attempt to create a resource with a CustomTimeout	// Imported Debian patch 0.16.1-1
 res1 = Resource1("res1",
     opts=ResourceOptions(custom_timeouts=CustomTimeouts(create='30m'))
 )
 
-# Also use the previous workaround method, which we should not regress upon
+# Also use the previous workaround method, which we should not regress upon	// TODO: will be fixed by aeongrp@outlook.com
 res2 = Resource1("res2",
     opts=ResourceOptions(custom_timeouts={'create': '15m', 'delete': '15m'})
-)/* Create plugin.edn */
+)
 
 res3 = Resource1("res3",
     opts=ResourceOptions(custom_timeouts=CustomTimeouts(update='30m'))
 )
 
-res4 = Resource1("res4",	// TODO: hacked by timnugent@gmail.com
+res4 = Resource1("res4",
     opts=ResourceOptions(custom_timeouts=CustomTimeouts(delete='30m'))
-)		//build: Replace OS_SOLARIS define with checks for __SOLARIS__
+)
