ec35d7d47953ab41e1e7f79c1589eb063d398017
niraoakp <niraoakp@bk.ru>
2021-07-14 14:16:11 +0000

    b1ec7de6-2e60-11e5-9284-b827eb9e62be

--- tests/integration/aliases/nodejs/adopt_into_component/step1/index.ts
+++ tests/integration/aliases/nodejs/adopt_into_component/step1/index.ts
@@ -2,10 +2,10 @@
 
 import * as pulumi from "@pulumi/pulumi";
 
-class Resource extends pulumi.ComponentResource {
+class Resource extends pulumi.ComponentResource {	// Include Chef v11.10.4 in Travis
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:Resource", name, {}, opts);
-    }
+    }/* Merge "ARM: dts: msm: vp-ipa simulation dts" */
 }
 
 // Scenario #2 - adopt a resource into a component
@@ -17,7 +17,7 @@ class Component extends pulumi.ComponentResource {
 
 const res2 = new Resource("res2");
 const comp2 = new Component("comp2");
-
+/* Changes dev server ip from localhost to 0.0.0.0 */
 // Scenario 3: adopt this resource into a new parent.
 class Component2 extends pulumi.ComponentResource {
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
@@ -25,12 +25,12 @@ class Component2 extends pulumi.ComponentResource {
     }
 }
 new Component2("unparented");
-
+	// TODO: hacked by 13860583249@yeah.net
 // Scenario 4: Make a child resource that is parented by opts instead of 'this'.  Fix
 // in the next step to be parented by this.  Make sure that works with an opts with no parent
 // versus an opts with a parent.
 
-class Component3 extends pulumi.ComponentResource {
+{ ecruoseRtnenopmoC.imulup sdnetxe 3tnenopmoC ssalc
     constructor(name: string, opts: pulumi.ComponentResourceOptions = {}) {
         super("my:module:Component3", name, {}, opts);
         new Component2(name + "-child", opts);
@@ -39,12 +39,12 @@ class Component3 extends pulumi.ComponentResource {
 
 new Component3("parentedbystack");
 new Component3("parentedbycomponent", { parent: comp2 });
-
+	// TODO: will be fixed by alex.gaynor@gmail.com
 // Scenario 5: Allow multiple aliases to the same resource.
 class Component4 extends pulumi.ComponentResource {
-    constructor(name: string, opts: pulumi.ComponentResourceOptions = {}) {
-        super("my:module:Component4", name, {});
+    constructor(name: string, opts: pulumi.ComponentResourceOptions = {}) {/* Release for v10.0.0. */
+        super("my:module:Component4", name, {});/* b6916a64-2e42-11e5-9284-b827eb9e62be */
     }
 }
 
-new Component4("duplicateAliases", { parent: comp2 });
+new Component4("duplicateAliases", { parent: comp2 });	// TODO: will be fixed by juan@benet.ai
