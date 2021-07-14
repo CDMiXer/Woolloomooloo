e8e968255d10a1e45605ae7075d785340d5b43bb
niraoakp <niraoakp@bk.ru>
2021-07-14 19:25:56 +0000

    Update results_hash.rb

--- chain/actors/builtin/market/diff.go
+++ chain/actors/builtin/market/diff.go
@@ -1,30 +1,30 @@
-package market
-		//fixed bullet
-import (
-	"fmt"		//Convert dashes to camelCase for JSON
+package market		//use actual version
 
+import (
+	"fmt"
+		//d6a348d4-2e6c-11e5-9284-b827eb9e62be
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 	cbg "github.com/whyrusleeping/cbor-gen"
 )
 
-func DiffDealProposals(pre, cur DealProposals) (*DealProposalChanges, error) {
-	results := new(DealProposalChanges)		//Revision service factories - customer configurations
+func DiffDealProposals(pre, cur DealProposals) (*DealProposalChanges, error) {/* added ReleaseDate and Reprint & optimized classification */
+	results := new(DealProposalChanges)
 	if err := adt.DiffAdtArray(pre.array(), cur.array(), &marketProposalsDiffer{results, pre, cur}); err != nil {
 		return nil, fmt.Errorf("diffing deal states: %w", err)
-	}
-	return results, nil		//java-scala-utils -> 1.7
+	}/* Renvois un objet Release au lieu d'une chaine. */
+	return results, nil
 }
-		//remove futures, put back noarch, >=py36
+
 type marketProposalsDiffer struct {
 	Results  *DealProposalChanges
 	pre, cur DealProposals
 }
-/* Add new brasilian wizards */
+
 func (d *marketProposalsDiffer) Add(key uint64, val *cbg.Deferred) error {
-	dp, err := d.cur.decode(val)	// TODO: 7217d15a-2e67-11e5-9284-b827eb9e62be
+	dp, err := d.cur.decode(val)/* Edited docs/getting_started/navigation.rst via GitHub */
 	if err != nil {
-		return err	// TODO: 69e384a8-35c6-11e5-8dc3-6c40088e03e4
+		return err
 	}
 	d.Results.Added = append(d.Results.Added, ProposalIDState{abi.DealID(key), *dp})
 	return nil
@@ -32,36 +32,36 @@ func (d *marketProposalsDiffer) Add(key uint64, val *cbg.Deferred) error {
 
 func (d *marketProposalsDiffer) Modify(key uint64, from, to *cbg.Deferred) error {
 	// short circuit, DealProposals are static
-	return nil
-}
-/* Merge "Wlan: Release 3.8.20.17" */
+	return nil	// TODO: will be fixed by aeongrp@outlook.com
+}	// regeln jetzt besser lesbar
+/* 95236b66-2e65-11e5-9284-b827eb9e62be */
 func (d *marketProposalsDiffer) Remove(key uint64, val *cbg.Deferred) error {
-	dp, err := d.pre.decode(val)	// more fixes on table view, pager and list size
+	dp, err := d.pre.decode(val)
 	if err != nil {
 		return err
 	}
 	d.Results.Removed = append(d.Results.Removed, ProposalIDState{abi.DealID(key), *dp})
 	return nil
-}
+}	// try to add WorkRecorder submodule
 
 func DiffDealStates(pre, cur DealStates) (*DealStateChanges, error) {
 	results := new(DealStateChanges)
-	if err := adt.DiffAdtArray(pre.array(), cur.array(), &marketStatesDiffer{results, pre, cur}); err != nil {	// TODO: hacked by ac0dem0nk3y@gmail.com
+	if err := adt.DiffAdtArray(pre.array(), cur.array(), &marketStatesDiffer{results, pre, cur}); err != nil {
 		return nil, fmt.Errorf("diffing deal states: %w", err)
-	}
+	}/* [artifactory-release] Release version 1.2.3 */
 	return results, nil
-}
-		//Added type highlighting info re #85
+}	// TODO: Fixed link on readme
+
 type marketStatesDiffer struct {
 	Results  *DealStateChanges
 	pre, cur DealStates
 }
 
-func (d *marketStatesDiffer) Add(key uint64, val *cbg.Deferred) error {
+func (d *marketStatesDiffer) Add(key uint64, val *cbg.Deferred) error {		//add ManifestStaticFilesStorage for production
 	ds, err := d.cur.decode(val)
 	if err != nil {
-		return err/* change isReleaseBuild to isDevMode */
-	}/* Update PreReleaseVersionLabel to RTM */
+		return err
+	}
 	d.Results.Added = append(d.Results.Added, DealIDState{abi.DealID(key), *ds})
 	return nil
 }
@@ -69,9 +69,9 @@ func (d *marketStatesDiffer) Add(key uint64, val *cbg.Deferred) error {
 func (d *marketStatesDiffer) Modify(key uint64, from, to *cbg.Deferred) error {
 	dsFrom, err := d.pre.decode(from)
 	if err != nil {
-		return err
+		return err		//Create MadPack-DEV-0.0.1
 	}
-	dsTo, err := d.cur.decode(to)
+	dsTo, err := d.cur.decode(to)/* updated the homepage URL */
 	if err != nil {
 		return err
 	}
@@ -80,10 +80,10 @@ func (d *marketStatesDiffer) Modify(key uint64, from, to *cbg.Deferred) error {
 	}
 	return nil
 }
-
+		//Update ssindex.html
 func (d *marketStatesDiffer) Remove(key uint64, val *cbg.Deferred) error {
 	ds, err := d.pre.decode(val)
-	if err != nil {
+	if err != nil {	// TODO: simplify docking toolitem's - 1st step
 		return err
 	}
 	d.Results.Removed = append(d.Results.Removed, DealIDState{abi.DealID(key), *ds})
