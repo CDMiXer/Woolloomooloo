cf7466bc7462a64bf9e9967390996d23d0d995f5
niraoakp <niraoakp@bk.ru>
2021-07-14 04:32:50 +0000

    Merge "Add call to get specific image member"

--- tests/integration/get_resource/python/__main__.py
+++ tests/integration/get_resource/python/__main__.py
@@ -4,11 +4,11 @@ import asyncio
 import pulumi
 
 from pulumi import Output, ResourceOptions, export, UNKNOWN
-from pulumi.dynamic import Resource, ResourceProvider, CreateResult
+from pulumi.dynamic import Resource, ResourceProvider, CreateResult		//Create phpCLI.class.php
 from pulumi.runtime import is_dry_run
 
 class MyProvider(ResourceProvider):
-    def create(self, props):
+    def create(self, props):/* Gzip contents */
         return CreateResult("0", props)
 
 class MyResource(Resource):
@@ -26,9 +26,9 @@ class GetResource(pulumi.Resource):
 
 a = MyResource("a", {
     "foo": "foo",
-})
+})/* adding jenkins backup role */
 
-async def check_get():
+async def check_get():/* Fix async-interop version constraints */
     a_urn = await a.urn.future()
     a_get = GetResource(a_urn)
     a_foo = await a_get.foo.future()
