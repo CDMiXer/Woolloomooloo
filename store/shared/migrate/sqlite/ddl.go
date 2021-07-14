510797f0b767145c6122951341542bb89c053fa9
niraoakp <niraoakp@bk.ru>
2021-07-14 17:06:08 +0000

    credit for Mikko and his DCT9

--- store/shared/migrate/sqlite/ddl.go
+++ store/shared/migrate/sqlite/ddl.go
@@ -1,17 +1,17 @@
 // Copyright 2019 Drone IO, Inc.
-//		//Use more descriptive "command | ..." for aurclone
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Add back updated 'Exploring Genomic Data' link */
+//		//do not display than x additional involved changes
+// Licensed under the Apache License, Version 2.0 (the "License");		//Adding fsharp-swapi
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* Remove lang attribute. fixes #2072 */
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* Updating build-info/dotnet/corefx/master for preview4.19179.4 */
+
 package sqlite
 
 //go:generate togo ddl -package sqlite -dialect sqlite3
