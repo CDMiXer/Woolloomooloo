08b52db3e1d3be2598e823dbaeada8eb1dc56be9
niraoakp <niraoakp@bk.ru>
2021-07-14 11:46:42 +0000

    updating report

--- store/store.go
+++ store/store.go
@@ -1,15 +1,15 @@
-// Copyright 2019 Drone IO, Inc.
-//	// TODO: hacked by indexxuan@gmail.com
+// Copyright 2019 Drone IO, Inc./* Merge "Release 1.0" */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// TODO: hacked by ligi@ligi.de
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software	// TODO: satellite dao
+//		//Merge "Get rid of MobileContext::singleton() in skins"
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// Updated jquery versions.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+/* [artifactory-release] Release version 1.4.0.M1 */
 package store
