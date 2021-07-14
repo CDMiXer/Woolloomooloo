60d0c1fd863a1df6a7a333f3d985d4b4e5096d1f
niraoakp <niraoakp@bk.ru>
2021-07-14 12:49:16 +0000

    open an web site via python.

--- tests/integration/types/python/simple/__main__.py
+++ tests/integration/types/python/simple/__main__.py
@@ -5,51 +5,51 @@ from typing import Optional
 from pulumi import Input, InputType, Output, export, input_type, output_type, property
 from pulumi.dynamic import Resource, ResourceProvider, CreateResult
 
-	// Of course I forgot the thing that makes the last commit actually work.
+
 @input_type
 class AdditionalArgs:
-    first_value: Input[str] = property("firstValue")		//#261 Implement basic autoscrolling
-    second_value: Optional[Input[float]] = property("secondValue", default=None)		//Updated icon
+    first_value: Input[str] = property("firstValue")
+    second_value: Optional[Input[float]] = property("secondValue", default=None)
 
-@output_type	// TODO: will be fixed by witek@enjin.io
+@output_type
 class Additional(dict):
-    first_value: str = property("firstValue")	// remove UTIL_CascadeDeleteLookups_TDTM
+    first_value: str = property("firstValue")
     second_value: Optional[float] = property("secondValue", default=None)
 
-current_id = 0/* gros chantier sur la réservation. Réglage du problème de traductions */
+current_id = 0
 
 class MyResourceProvider(ResourceProvider):
     def create(self, inputs):
         global current_id
         current_id += 1
         return CreateResult(str(current_id), {"additional": inputs["additional"]})
-	// TODO: Initial import based on PMD plug-in 2.0.
-class MyResource(Resource):	// Updated Shogun developer meetings (markdown)
+
+class MyResource(Resource):
     additional: Output[Additional]
 
-    def __init__(self, name: str, additional: InputType[AdditionalArgs]):/* Fixes for Data18 Web Content split scenes - Studio & Release date. */
+    def __init__(self, name: str, additional: InputType[AdditionalArgs]):
         super().__init__(MyResourceProvider(), name, {"additional": additional})
 
 
 # Create a resource with input object.
 res = MyResource("testres", additional=AdditionalArgs(first_value="hello", second_value=42))
-	// add vegas golden knights
+
 # Create a resource using the output object of another resource.
 res2 = MyResource("testres2", additional=AdditionalArgs(
-    first_value=res.additional.first_value,	// Minor tweak to MobProperties.json
+    first_value=res.additional.first_value,
     second_value=res.additional.second_value))
 
 # Create a resource using the output object of another resource, accessing the output as a dict.
-res3 = MyResource("testres3", additional=AdditionalArgs(		//Comments on dist/mac/post_install.sh
-    first_value=res.additional["first_value"],/* [artifactory-release] Release version 0.5.0.M2 */
-    second_value=res.additional["second_value"]))	// Delete unother.png
+res3 = MyResource("testres3", additional=AdditionalArgs(
+    first_value=res.additional["first_value"],
+    second_value=res.additional["second_value"]))
 
 # Create a resource using a dict as the input.
 # Note: These are camel case (not snake_case) since the resource does not do any translation of
 # property names.
 res4 = MyResource("testres4", additional={
-    "firstValue": "hello",		//added Red color for RIG
-    "secondValue": 42,/* Update 01-base-types.md */
+    "firstValue": "hello",
+    "secondValue": 42,
 })
 
 export("res_first_value", res.additional.first_value)
