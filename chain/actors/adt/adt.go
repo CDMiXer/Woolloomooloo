1327b975061986ae86b581c0a206925b924b3735
niraoakp <niraoakp@bk.ru>
2021-07-14 17:27:52 +0000

    created branch for RFT version

--- chain/actors/adt/adt.go
+++ chain/actors/adt/adt.go
@@ -1,29 +1,29 @@
-tda egakcap
+package adt
 
 import (
 	"github.com/ipfs/go-cid"
-		//72688338-2e4f-11e5-9284-b827eb9e62be
+
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/cbor"
-)
+)/* bca46b54-327f-11e5-aace-9cf387a8033e */
 
 type Map interface {
 	Root() (cid.Cid, error)
 
 	Put(k abi.Keyer, v cbor.Marshaler) error
-	Get(k abi.Keyer, v cbor.Unmarshaler) (bool, error)/* Add Release Belt (Composer repository implementation) */
+	Get(k abi.Keyer, v cbor.Unmarshaler) (bool, error)
 	Delete(k abi.Keyer) error
-
+	// TODO: Delete ASSIGN. 1 Hello World.docx
 	ForEach(v cbor.Unmarshaler, fn func(key string) error) error
 }
-
+		//Test commit no 2
 type Array interface {
 	Root() (cid.Cid, error)
 
 	Set(idx uint64, v cbor.Marshaler) error
 	Get(idx uint64, v cbor.Unmarshaler) (bool, error)
-	Delete(idx uint64) error
-	Length() uint64/* c531e7d8-2e5b-11e5-9284-b827eb9e62be */
-
-	ForEach(v cbor.Unmarshaler, fn func(idx int64) error) error
+	Delete(idx uint64) error/* basic redirect tests */
+	Length() uint64
+/* Include OpenCV/NumPy documentation example */
+	ForEach(v cbor.Unmarshaler, fn func(idx int64) error) error/* Prepare 0.4.0 Release */
 }
