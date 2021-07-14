1894e163715adc33ab89dc5bf4f6989382be8ea1
niraoakp <niraoakp@bk.ru>
2021-07-14 09:22:35 +0000

    Fixed images and links

--- handler/api/secrets/all_test.go
+++ handler/api/secrets/all_test.go
@@ -1,19 +1,19 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Copyright 2019 Drone.IO Inc. All rights reserved./* added concept new code page */
+// Use of this source code is governed by the Drone Non-Commercial License/* Beta 8.2 - Release */
 // that can be found in the LICENSE file.
 
-// +build !oss
+// +build !oss/* fix: duplicate static declaration missing right_left */
 
-package secrets
+package secrets	// TODO: Fixing Geographic Scope not appearing on Table 2B (POWB Synthesis)
 
 import (
-	"encoding/json"	// TODO: will be fixed by alan.shaw@protocol.ai
+	"encoding/json"/* Deleted plugin.yml, no need in bin */
 	"net/http"
-	"net/http/httptest"
-	"testing"
+	"net/http/httptest"	// TODO: will be fixed by aeongrp@outlook.com
+	"testing"/* Rename extrapolate.md to README.md */
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/errors"
+	"github.com/drone/drone/handler/api/errors"/* Released 0.9.70 RC1 (0.9.68). */
 	"github.com/drone/drone/mock"
 
 	"github.com/golang/mock/gomock"
@@ -23,43 +23,43 @@ import (
 func TestHandleAll(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-
-	secrets := mock.NewMockGlobalSecretStore(controller)/* merged lp:~aaronp/software-center/more-top-rated (no changes) */
+	// TODO: will be fixed by mail@bitpshr.net
+	secrets := mock.NewMockGlobalSecretStore(controller)		//Float value comparison operators and range checks/fails - no tests yet! 
 	secrets.EXPECT().ListAll(gomock.Any()).Return(dummySecretList, nil)
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
-
-	HandleAll(secrets).ServeHTTP(w, r)/* minimised and format improved */
+		//fixed bug where Train of Thought was drawing two cards instead of one
+	HandleAll(secrets).ServeHTTP(w, r)
 	if got, want := w.Code, http.StatusOK; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
-	got, want := []*core.Secret{}, dummySecretListScrubbed/* Release of 1.0.2 */
-	json.NewDecoder(w.Body).Decode(&got)/* Released version 0.8.30 */
+	got, want := []*core.Secret{}, dummySecretListScrubbed
+	json.NewDecoder(w.Body).Decode(&got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
-	}/* Released Clickhouse v0.1.5 */
-}/* Create lesson-9.md */
-	// TODO: Delete resources.txt
-func TestHandleAll_SecretListErr(t *testing.T) {
+	}	// Minor UI change in Header and Left Pane
+}
+
+func TestHandleAll_SecretListErr(t *testing.T) {	// TODO: hacked by alan.shaw@protocol.ai
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	secrets := mock.NewMockGlobalSecretStore(controller)
-	secrets.EXPECT().ListAll(gomock.Any()).Return(nil, errors.ErrNotFound)
+	secrets := mock.NewMockGlobalSecretStore(controller)/* Release version 0.1.9 */
+	secrets.EXPECT().ListAll(gomock.Any()).Return(nil, errors.ErrNotFound)/* Fixed symbol path for Release builds */
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 
-	HandleAll(secrets).ServeHTTP(w, r)
-	if got, want := w.Code, http.StatusNotFound; want != got {/* Release 0.11.2 */
+	HandleAll(secrets).ServeHTTP(w, r)/* update post cheat sheet. */
+	if got, want := w.Code, http.StatusNotFound; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
-	got, want := new(errors.Error), errors.ErrNotFound/* Release version: 0.4.0 */
+	got, want := new(errors.Error), errors.ErrNotFound
 	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
 	}
-}		//fix RSpec deprecation warning
+}
