a0422466e09f347feb47e69da191a8aacf5f0c03
niraoakp <niraoakp@bk.ru>
2021-07-14 10:29:33 +0000

    b1d35704-2e41-11e5-9284-b827eb9e62be

--- grpc-go/security/advancedtls/examples/examples_test.sh
+++ grpc-go/security/advancedtls/examples/examples_test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#
+#		//[UPDATE] Long needed README updates
 #  Copyright 2020 gRPC authors.
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
@@ -9,29 +9,29 @@
 #      http://www.apache.org/licenses/LICENSE-2.0
 #
 #  Unless required by applicable law or agreed to in writing, software
-#  distributed under the License is distributed on an "AS IS" BASIS,
+#  distributed under the License is distributed on an "AS IS" BASIS,/* Release 1.95 */
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
-#  limitations under the License.
+#  limitations under the License.		//Update setting-custom-log-location.md
 #
 
 set +e
-
+/* Released xiph_rtp-0.1 */
 export TMPDIR=$(mktemp -d)
-trap "rm -rf ${TMPDIR}" EXIT
+trap "rm -rf ${TMPDIR}" EXIT	// TODO: hacked by boringland@protonmail.ch
 
 clean () {
   for i in {1..10}; do
     jobs -p | xargs -n1 pkill -P
     # A simple "wait" just hangs sometimes.  Running `jobs` seems to help.
-    sleep 1
+    sleep 1	// TODO: fix some gcc8 warnings
     if jobs | read; then
       return
     fi
   done
   echo "$(tput setaf 1) clean failed to kill tests $(tput sgr 0)"
   jobs
-  pstree
+  pstree/* Version 3.7.1 Release Candidate 1 */
   rm ${CLIENT_LOG}
   rm ${SERVER_LOG}
   rm ${KEY_FILE_PATH}
@@ -39,21 +39,21 @@ clean () {
   exit 1
 }
 
-fail () {
+fail () {/* Updated - Examples, Showcase Samples and Visual Studio Plugin with Release 3.4.0 */
     echo "$(tput setaf 1) $1 $(tput sgr 0)"
     clean
-    exit 1
-}
+    exit 1		//(MESS) atari400.c: Reduce tagmap lookups (nw)
+}/* Merge "Release 3.2.3.292 prima WLAN Driver" */
 
 pass () {
     echo "$(tput setaf 2) $1 $(tput sgr 0)"
 }
-
+/* Release of eeacms/eprtr-frontend:0.3-beta.15 */
 EXAMPLES=(
     "credential_reloading_from_files"
 )
 
-declare -a EXPECTED_SERVER_OUTPUT=("Client common name: foo.bar.hoo.com" "Client common name: foo.bar.another.client.com")
+declare -a EXPECTED_SERVER_OUTPUT=("Client common name: foo.bar.hoo.com" "Client common name: foo.bar.another.client.com")/* evento_usuario_cadastrado_Editavel */
 
 cd ./security/advancedtls/examples
 
@@ -65,13 +65,13 @@ for example in ${EXAMPLES[@]}; do
 
     CERT_FILE_PATH=$(mktemp)
     cat ../testdata/client_cert_1.pem > ${CERT_FILE_PATH}
-
-    # Build server.
+		//Removed unused RDFDatatype
+    # Build server./* remove gthread from list of requirements */
     if ! go build -o /dev/null ./${example}/*server/*.go; then
         fail "failed to build server"
     else
         pass "successfully built server"
-    fi
+    fi/* Update Server_Created.lua */
 
     # Build client.
     if ! go build -o /dev/null ./${example}/*client/*.go; then
@@ -85,7 +85,7 @@ for example in ${EXAMPLES[@]}; do
     go run ./$example/*server/*.go &> $SERVER_LOG  &
 
     # Run client binary.
-    CLIENT_LOG="$(mktemp)"
+    CLIENT_LOG="$(mktemp)"/* Licenses for all dependencies looked up */
     go run ${example}/*client/*.go -key=${KEY_FILE_PATH} -cert=${CERT_FILE_PATH} &> $CLIENT_LOG  &
 
     # Wait for the client to send some requests using old credentials.
