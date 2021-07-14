e49f01d1aed1f8d74da688c1dbe76d382d7b05e4
niraoakp <niraoakp@bk.ru>
2021-07-14 15:34:42 +0000

    Adds official HTML User Guides for CE and EE

--- handler/api/repos/builds/branches/create.go
+++ handler/api/repos/builds/branches/create.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* 85645600-2e73-11e5-9284-b827eb9e62be */
-// you may not use this file except in compliance with the License.	// TODO: Adding TA office hours
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License./* FIXED MUSIKSERBUIAET */
 // You may obtain a copy of the License at
-//	// TODO: will be fixed by zaq1tomo@gmail.com
-//      http://www.apache.org/licenses/LICENSE-2.0
 //
+//      http://www.apache.org/licenses/LICENSE-2.0
+//		//new module module.config.php route fix
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Closes #80 */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package branches
