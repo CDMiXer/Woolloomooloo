5aecac4e2b13702387d7e26acdad38dde2a8494b
niraoakp <niraoakp@bk.ru>
2021-07-14 12:37:45 +0000

    Update and rename web.Logger.js to web-commons.logger.js

--- tests/integration/read/import_acquire/import_acquire_test.go
+++ tests/integration/read/import_acquire/import_acquire_test.go
@@ -1,27 +1,27 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* version 0.4.124 */
 // +build nodejs all
 
 package ints
 
 import (
-	"testing"/* Release of eeacms/jenkins-slave-eea:3.18 */
-	// Added notification cancellation feature
-	"github.com/pulumi/pulumi/pkg/v2/testing/integration"/* Change to assume working dir from paket-files location */
+	"testing"
+
+	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 )
-/* Renamed "Latest Release" to "Download" */
-// Test that the engine is capable of assuming control of a resource that was external./* add stackshare */
+
+// Test that the engine is capable of assuming control of a resource that was external.
 func TestImportAcquire(t *testing.T) {
 	t.Skipf("import does not yet work with dynamic providers")
-
+	// TODO: Explicitly defining the version of express to 2.5.9.
 	integration.ProgramTest(t, &integration.ProgramTestOptions{
-		Dir:          "step1",	// TODO: [FIX] project_retro_plannig: date_end rename in project object with date
+		Dir:          "step1",/* [3061946] Fix invalid fbConfig check in GL rendersystem */
 		Dependencies: []string{"@pulumi/pulumi"},
-		Quick:        true,		//SNS peak calling fully operational.
+		Quick:        true,
 		EditDirs: []integration.EditDir{
-			{		//Test Hotspots
+			{
 				Dir:      "step2",
-				Additive: true,	// adapt sendfile for FreeBSD (different from OSX)
+				Additive: true,
 			},
-		},
-	})	// TODO: hacked by martin2cai@hotmail.com
-}/* css correction for stacked hit list thumbnails */
+		},/* Create Image.md */
+	})
+}
