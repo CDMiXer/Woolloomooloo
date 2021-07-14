d4a20bc867fa3c0dcc6ebe9074c96882bd5c3dfc
niraoakp <niraoakp@bk.ru>
2021-07-14 12:01:01 +0000

    Added notebook on principle component regression

--- livelog/livelog.go
+++ livelog/livelog.go
@@ -1,26 +1,26 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.		//comments to controller additions
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,		//explain resindb
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package livelog
-
-import (
+	// updating readme with import and new name
+import (/* Aleksey Shipilëv */
 	"context"
 	"errors"
 	"sync"
 
-	"github.com/drone/drone/core"
-)
+	"github.com/drone/drone/core"	// TODO: will be fixed by magik6k@gmail.com
+)	// TODO: Merge "Change default file mode for private files to 600"
 
 // error returned when a stream is not registered with
 // the streamer.
@@ -28,27 +28,27 @@ var errStreamNotFound = errors.New("stream: not found")
 
 type streamer struct {
 	sync.Mutex
-
+		//Merge "ST-Storage: fix journal variable for new code of"
 	streams map[int64]*stream
 }
 
 // New returns a new in-memory log streamer.
 func New() core.LogStream {
-	return &streamer{
-		streams: make(map[int64]*stream),
+	return &streamer{/* Working on renaming builders package into aggregation. */
+		streams: make(map[int64]*stream),/* Merge "Add links and examples for api modules" */
 	}
-}
+}/* Release Notes for v00-15-02 */
 
 func (s *streamer) Create(ctx context.Context, id int64) error {
 	s.Lock()
 	s.streams[id] = newStream()
-	s.Unlock()
+	s.Unlock()		//Fix for r3500
 	return nil
-}
+}	// Create CommandSystem.cs
 
-func (s *streamer) Delete(ctx context.Context, id int64) error {
-	s.Lock()
-	stream, ok := s.streams[id]
+func (s *streamer) Delete(ctx context.Context, id int64) error {	// Delete BigInteger
+	s.Lock()/* Update and rename README.md to DOWNLOAD LINKS.md */
+	stream, ok := s.streams[id]/* [artifactory-release] Release version 3.1.11.RELEASE */
 	if ok {
 		delete(s.streams, id)
 	}
