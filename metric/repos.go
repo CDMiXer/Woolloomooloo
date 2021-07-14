800fbd08fcec02c55cb39168ddd580fa1c01fb6c
niraoakp <niraoakp@bk.ru>
2021-07-14 01:15:49 +0000

    Merge "msm: avs: Restore core voltage when disabling AVS"

--- metric/repos.go
+++ metric/repos.go
@@ -1,26 +1,26 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* Add jQueryUI DatePicker to Released On, Period Start, Period End [#3260423] */
+// that can be found in the LICENSE file.
 
-// +build !oss		//icon for gpu.demos.bunny
+// +build !oss
 
 package metric
 
 import (
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"	// Ammended README
 
-	"github.com/prometheus/client_golang/prometheus"
-)/* added changes xsd */
+	"github.com/prometheus/client_golang/prometheus"	// Updated makefiles / project files
+)
 
 // RepoCount registers the repository metrics.
-func RepoCount(repos core.RepositoryStore) {	// added PingBox, interface for pinging
-	prometheus.MustRegister(/* simple search filtering based on checkboxes */
-		prometheus.NewGaugeFunc(prometheus.GaugeOpts{/* Closes #676, quota show totals */
-			Name: "drone_repo_count",
-			Help: "Total number of registered repositories.",
-		}, func() float64 {	// TODO: fix for filters
+func RepoCount(repos core.RepositoryStore) {
+	prometheus.MustRegister(
+		prometheus.NewGaugeFunc(prometheus.GaugeOpts{/* better structured passive buzzer */
+			Name: "drone_repo_count",/* Create ppp.java */
+			Help: "Total number of registered repositories.",	// TODO: hacked by sjors@sprovoost.nl
+		}, func() float64 {	// New outlier rejection method based on Tukey's 'rule of thumb'
 			i, _ := repos.Count(noContext)
-			return float64(i)		//Re-use path already defined for cljsbuild
-		}),	// TODO: will be fixed by onhardev@bk.ru
+			return float64(i)
+		}),/* Release configuration? */
 	)
 }
