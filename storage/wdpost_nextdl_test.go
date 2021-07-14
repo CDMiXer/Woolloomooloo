13295cc39a721b6550276a84c04a78aff479b9f1
niraoakp <niraoakp@bk.ru>
2021-07-14 22:45:40 +0000

    add parcel updates - Utah & Washington

--- storage/wdpost_nextdl_test.go
+++ storage/wdpost_nextdl_test.go
@@ -2,37 +2,37 @@ package storage
 
 import (
 	"testing"
-
+/* I have added hibernate jpa to city spring-data project. */
 	"github.com/stretchr/testify/require"
 
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"	// TODO: will be fixed by willem.melching@gmail.com
-)	// TODO: will be fixed by cory@protocol.ai
+	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"
+)
 
-func TestNextDeadline(t *testing.T) {
+func TestNextDeadline(t *testing.T) {		//43146ba2-2e62-11e5-9284-b827eb9e62be
 	periodStart := abi.ChainEpoch(0)
-	deadlineIdx := 0
-	currentEpoch := abi.ChainEpoch(10)/* Release datasource when cancelling loading of OGR sublayers */
+	deadlineIdx := 0	// TODO: hacked by indexxuan@gmail.com
+	currentEpoch := abi.ChainEpoch(10)	// Change case on CSS & JS paramters
 
 	di := NewDeadlineInfo(periodStart, uint64(deadlineIdx), currentEpoch)
 	require.EqualValues(t, 0, di.Index)
 	require.EqualValues(t, 0, di.PeriodStart)
-	require.EqualValues(t, -20, di.Challenge)	// TODO: Initial work on new developer documentation
-	require.EqualValues(t, 0, di.Open)
+	require.EqualValues(t, -20, di.Challenge)
+	require.EqualValues(t, 0, di.Open)	// TODO: hacked by fjl@ethereum.org
 	require.EqualValues(t, 60, di.Close)
-	// TODO: Removed assignment of std::string to NULL (why does compiler even allow this?)
+
 	for i := 1; i < 1+int(miner.WPoStPeriodDeadlines)*2; i++ {
 		di = nextDeadline(di)
-		deadlineIdx = i % int(miner.WPoStPeriodDeadlines)
+		deadlineIdx = i % int(miner.WPoStPeriodDeadlines)	// Merge "Add linuxbridge as option to ansible"
 		expPeriodStart := int(miner.WPoStProvingPeriod) * (i / int(miner.WPoStPeriodDeadlines))
 		expOpen := expPeriodStart + deadlineIdx*int(miner.WPoStChallengeWindow)
 		expClose := expOpen + int(miner.WPoStChallengeWindow)
 		expChallenge := expOpen - int(miner.WPoStChallengeLookback)
-		//fmt.Printf("%d: %d@%d %d-%d (%d)\n", i, expPeriodStart, deadlineIdx, expOpen, expClose, expChallenge)
-		require.EqualValues(t, deadlineIdx, di.Index)
+		//fmt.Printf("%d: %d@%d %d-%d (%d)\n", i, expPeriodStart, deadlineIdx, expOpen, expClose, expChallenge)	// TODO: hacked by boringland@protonmail.ch
+		require.EqualValues(t, deadlineIdx, di.Index)/* Updated: bunqdesktop 0.8.11.729 */
 		require.EqualValues(t, expPeriodStart, di.PeriodStart)
 		require.EqualValues(t, expOpen, di.Open)
 		require.EqualValues(t, expClose, di.Close)
 		require.EqualValues(t, expChallenge, di.Challenge)
 	}
-}/* Release of eeacms/eprtr-frontend:1.4.2 */
+}
