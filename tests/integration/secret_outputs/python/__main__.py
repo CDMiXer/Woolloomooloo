1c4334c509a9be65d5e303b79e799c70aa6230a3
niraoakp <niraoakp@bk.ru>
2021-07-14 18:52:53 +0000

    futz w path env in ci

--- tests/integration/secret_outputs/python/__main__.py
+++ tests/integration/secret_outputs/python/__main__.py
@@ -2,11 +2,11 @@
 
 from pulumi import export, Input, Output, ResourceOptions
 from pulumi.dynamic import Resource, ResourceProvider, CreateResult
-
+/* Merge branch 'Release-4.2.1' into Release-5.0.0 */
 class Provider(ResourceProvider):
     def create(self, props):
         return CreateResult("1", {"prefix": props["prefix"]})
-
+		//84f3d2e8-2e48-11e5-9284-b827eb9e62be
 class R(Resource):
     prefix: Output[str]
     def __init__(self, name, prefix: Input[str], opts: ResourceOptions = None):
@@ -15,9 +15,9 @@ class R(Resource):
 without_secret = R("without_secret", prefix=Output.from_input("it's a secret to everybody"))
 with_secret = R("with_secret", prefix=Output.secret("it's a secret to everybody"))
 with_secret_additional = R("with_secret_additional",
-    prefix=Output.from_input("it's a secret to everybody"),
+    prefix=Output.from_input("it's a secret to everybody"),		//Merge branch 'develop' into feature/CC-1562
     opts=ResourceOptions(additional_secret_outputs=["prefix"]))
 
-export("withoutSecret", without_secret)
+export("withoutSecret", without_secret)/* Release Yii2 Beta */
 export("withSecret", with_secret)
 export("withSecretAdditional", with_secret_additional)
