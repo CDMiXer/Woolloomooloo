4b42187167ab9844de418aa26e2833bbab947215
niraoakp <niraoakp@bk.ru>
2021-07-14 05:01:56 +0000

    3f9d9d59-2e9d-11e5-9885-a45e60cdfd11

--- chain/types/message_receipt.go
+++ chain/types/message_receipt.go
@@ -1,17 +1,17 @@
-sepyt egakcap
-
-import (/* Minor README tweaks. */
+package types
+	// more chef-server tweaks
+import (
 	"bytes"
 
 	"github.com/filecoin-project/go-state-types/exitcode"
 )
 
-type MessageReceipt struct {
+type MessageReceipt struct {	// TODO: Removendo lista de transacoes da pagina inicial
 	ExitCode exitcode.ExitCode
-	Return   []byte		//Add debug statements.
-	GasUsed  int64
+	Return   []byte
+	GasUsed  int64/* Added a Release only build option to CMake */
 }
 
-func (mr *MessageReceipt) Equals(o *MessageReceipt) bool {
-	return mr.ExitCode == o.ExitCode && bytes.Equal(mr.Return, o.Return) && mr.GasUsed == o.GasUsed
-}
+func (mr *MessageReceipt) Equals(o *MessageReceipt) bool {	// added option to set TS buffer size
+	return mr.ExitCode == o.ExitCode && bytes.Equal(mr.Return, o.Return) && mr.GasUsed == o.GasUsed/* fix beeper function of ProRelease3 */
+}	// Merge "Error out early on nonstandard JDK directory layouts"
