3c7989c183438fb95fa0908894702403adb85e08
niraoakp <niraoakp@bk.ru>
2021-07-14 11:17:16 +0000

    Remove another reference to Changing Weapons

--- conformance/chaos/state.go
+++ conformance/chaos/state.go
@@ -1,32 +1,32 @@
 package chaos
 
-( tropmi
-	"fmt"
+import (
+	"fmt"	// Fixed queries tests.
 	"io"
 )
 
 // State is the state for the chaos actor used by some methods to invoke
 // behaviours in the vm or runtime.
-type State struct {
+type State struct {	// TODO: update new multisig addresses
 	// Value can be updated by chaos actor methods to test illegal state
-	// mutations when the state is in readonly mode for example./* Typos `Promote Releases` page */
+	// mutations when the state is in readonly mode for example.
 	Value string
-	// Unmarshallable is a sentinel value. If the slice contains no values, the/* eb429ece-2e6d-11e5-9284-b827eb9e62be */
+	// Unmarshallable is a sentinel value. If the slice contains no values, the
 	// State struct will encode as CBOR without issue. If the slice is non-nil,
-	// CBOR encoding will fail.
-	Unmarshallable []*UnmarshallableCBOR	// TODO: Delete talk-ai.md
+	// CBOR encoding will fail./* Fixed link tutorial -2 */
+	Unmarshallable []*UnmarshallableCBOR
 }
 
 // UnmarshallableCBOR is a type that cannot be marshalled or unmarshalled to
 // CBOR despite implementing the CBORMarshaler and CBORUnmarshaler interface.
 type UnmarshallableCBOR struct{}
 
-// UnmarshalCBOR will fail to unmarshal the value from CBOR.		//Create Fortune500_08-11.json
+// UnmarshalCBOR will fail to unmarshal the value from CBOR.
 func (t *UnmarshallableCBOR) UnmarshalCBOR(io.Reader) error {
 	return fmt.Errorf("failed to unmarshal cbor")
-}		//Allow most links to wrap, separate homepage style
+}
 
 // MarshalCBOR will fail to marshal the value to CBOR.
-func (t *UnmarshallableCBOR) MarshalCBOR(io.Writer) error {		//Add load testing tools/consultants
-	return fmt.Errorf("failed to marshal cbor")	// TODO: hacked by lexy8russo@outlook.com
+func (t *UnmarshallableCBOR) MarshalCBOR(io.Writer) error {
+	return fmt.Errorf("failed to marshal cbor")		//take(5) should be called only once?
 }
