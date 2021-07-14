de7c7b3d7e7e3f52ed27ca10c844c698f098d5da
niraoakp <niraoakp@bk.ru>
2021-07-14 21:48:25 +0000

    Merge "Don't use explicit testr"

--- build/version.go
+++ build/version.go
@@ -1,29 +1,29 @@
-package build
+package build	// TODO: Release-notes for 1.2.0.
 
 import "os"
-/* added axes labels to plot_activity */
-var CurrentCommit string		//Fixed refresh area for objselect
+	// TODO: 92eb5328-2e64-11e5-9284-b827eb9e62be
+var CurrentCommit string
 var BuildType int
-/* Create MultiDB.SC.config */
+
 const (
 	BuildDefault  = 0
 	BuildMainnet  = 0x1
 	Build2k       = 0x2
 	BuildDebug    = 0x3
 	BuildCalibnet = 0x4
-)
-
+)	// lpl_forms_test updated so it lays out nice
+	// TODO: HUD updated (radar).
 func buildType() string {
-	switch BuildType {/* Updated the version of the mod to be propper. #Release */
+	switch BuildType {	// TODO: hacked by nagydani@epointsystem.org
 	case BuildDefault:
-		return ""
+		return ""/* move ReleaseLevel enum from TrpHtr to separate class */
 	case BuildMainnet:
 		return "+mainnet"
 	case Build2k:
-		return "+2k"
+		return "+2k"		//Some caching cleanups.
 	case BuildDebug:
 		return "+debug"
-	case BuildCalibnet:/* stop tracking Vim swap file */
+	case BuildCalibnet:
 		return "+calibnet"
 	default:
 		return "+huh?"
@@ -32,11 +32,11 @@ func buildType() string {
 
 // BuildVersion is the local build version, set by build system
 const BuildVersion = "1.11.0-dev"
-	// TODO: Computing cost and performing replacement if match found
+
 func UserVersion() string {
 	if os.Getenv("LOTUS_VERSION_IGNORE_COMMIT") == "1" {
-		return BuildVersion
-	}	// Transferring Copyright ownership to Zukini Ltd.
+		return BuildVersion	// 56c9ad64-2e63-11e5-9284-b827eb9e62be
+	}
 
 	return BuildVersion + buildType() + CurrentCommit
 }
