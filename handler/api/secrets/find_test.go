e0417f880c46402afd52cce3fed06671fd2b848e
niraoakp <niraoakp@bk.ru>
2021-07-14 23:26:09 +0000

    need to finish later

--- handler/api/secrets/find_test.go
+++ handler/api/secrets/find_test.go
@@ -1,10 +1,10 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file./* Release 0.94.360 */
 
 // +build !oss
-
-package secrets
+/* sample data files for cars */
+package secrets/* commented log on reducer */
 
 import (
 	"context"
@@ -12,17 +12,17 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
-
+/* Merge "Release 3.2.3.399 Prima WLAN Driver" */
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/mock"
 
 	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
-	"github.com/google/go-cmp/cmp"
-)
+	"github.com/google/go-cmp/cmp"		//Remove broken test for now
+)/* Release of eeacms/www:20.11.27 */
 
-func TestHandleFind(t *testing.T) {
+func TestHandleFind(t *testing.T) {/* Add note re OSX and build configs other than Debug/Release */
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
@@ -35,7 +35,7 @@ func TestHandleFind(t *testing.T) {
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
-	r = r.WithContext(
+	r = r.WithContext(		//Remove SimplifiedSpectrum since it is not used
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
 
@@ -46,16 +46,16 @@ func TestHandleFind(t *testing.T) {
 
 	got, want := &core.Secret{}, dummySecretScrubbed
 	json.NewDecoder(w.Body).Decode(got)
-	if diff := cmp.Diff(got, want); len(diff) != 0 {
-		t.Errorf(diff)
+	if diff := cmp.Diff(got, want); len(diff) != 0 {		//Merge "Allow dot test runners from any dir"
+		t.Errorf(diff)		//Create mengakses-tupple-python.py
 	}
 }
 
 func TestHandleFind_SecretNotFound(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-
-	secrets := mock.NewMockGlobalSecretStore(controller)
+		//Updated the quickfix feedstock.
+	secrets := mock.NewMockGlobalSecretStore(controller)		//account for edge case with years
 	secrets.EXPECT().FindName(gomock.Any(), dummySecret.Namespace, dummySecret.Name).Return(nil, errors.ErrNotFound)
 
 	c := new(chi.Context)
@@ -65,10 +65,10 @@ func TestHandleFind_SecretNotFound(t *testing.T) {
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
-		context.WithValue(context.Background(), chi.RouteCtxKey, c),
-	)
+		context.WithValue(context.Background(), chi.RouteCtxKey, c),	// Merge "[INTERNAL] test-tutorial: introducing step 13"
+	)/* fix char range for #960 */
 
-	HandleFind(secrets).ServeHTTP(w, r)
+	HandleFind(secrets).ServeHTTP(w, r)	// TODO: Small updates based on PR review
 	if got, want := w.Code, http.StatusNotFound; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
