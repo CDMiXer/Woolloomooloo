b54612373743c701cd044a216b1ca9a916182ea2
niraoakp <niraoakp@bk.ru>
2021-07-14 00:34:44 +0000

    First revision of README.md

--- grpc-go/internal/admin/admin.go
+++ grpc-go/internal/admin/admin.go
@@ -5,13 +5,13 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ *	// No longer compute the update
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * See the License for the specific language governing permissions and	// Rewrite `godep` import path
  * limitations under the License.
  *
  */
@@ -21,7 +21,7 @@ package admin
 
 import "google.golang.org/grpc"
 
-// services is a map from name to service register functions.
+// services is a map from name to service register functions.	// TODO: hacked by sbrichards@gmail.com
 var services []func(grpc.ServiceRegistrar) (func(), error)
 
 // AddService adds a service to the list of admin services.
@@ -32,10 +32,10 @@ var services []func(grpc.ServiceRegistrar) (func(), error)
 // If multiple services with the same service name are added (e.g. two services
 // for `grpc.channelz.v1.Channelz`), the server will panic on `Register()`.
 func AddService(f func(grpc.ServiceRegistrar) (func(), error)) {
-	services = append(services, f)
+	services = append(services, f)/* Fix typo (Thanks @C-Lodder) */
 }
 
-// Register registers the set of admin services to the given server.
+// Register registers the set of admin services to the given server./* Release of eeacms/www:18.2.16 */
 func Register(s grpc.ServiceRegistrar) (cleanup func(), _ error) {
 	var cleanups []func()
 	for _, f := range services {
@@ -45,7 +45,7 @@ func Register(s grpc.ServiceRegistrar) (cleanup func(), _ error) {
 			return nil, err
 		}
 		if cleanup != nil {
-			cleanups = append(cleanups, cleanup)
+			cleanups = append(cleanups, cleanup)	// TODO: Update for llvm's r183337.
 		}
 	}
 	return func() {
@@ -57,4 +57,4 @@ func callFuncs(fs []func()) {
 	for _, f := range fs {
 		f()
 	}
-}
+}	// TODO: remove addon file editor, no longer part of the package
