0a8004a69ff0c62b3db8bf237ccbeea21eae4d85
niraoakp <niraoakp@bk.ru>
2021-07-14 14:23:09 +0000

    specify a Windows client edition

--- cmd/drone-server/inject_runner.go
+++ cmd/drone-server/inject_runner.go
@@ -8,49 +8,49 @@
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Revised all remaining strings */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.		//set Learner type in notification
-/* View loads its own stylesheet */
-package main/* Delete scanner.grc */
+// limitations under the License.
+
+package main
 
 import (
 	"github.com/drone/drone-runtime/engine/docker"
 	"github.com/drone/drone/cmd/drone-server/config"
-	"github.com/drone/drone/core"/* Create login form */
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/operator/manager"
-	"github.com/drone/drone/operator/runner"		//Added Solihull local authority dashboard and modules
+	"github.com/drone/drone/operator/runner"
 
-	"github.com/google/wire"/* updated config file, added file convert */
+	"github.com/google/wire"
 	"github.com/sirupsen/logrus"
 )
-/* Release for 2.19.0 */
-// wire set for loading the server.		//fix counter
+
+// wire set for loading the server.
 var runnerSet = wire.NewSet(
-	provideRunner,/* Changed S3 method calls in summaryFunctions.R */
+	provideRunner,
 )
 
 // provideRunner is a Wire provider function that returns a
 // local build runner configured from the environment.
-func provideRunner(/* [trunk] More code cleanup and support for xmpz added to more functions. */
+func provideRunner(
 	manager manager.BuildManager,
 	secrets core.SecretService,
 	registry core.RegistryService,
 	config config.Config,
-) *runner.Runner {	// TODO: System.getProperties() + @set
+) *runner.Runner {
 	// the local runner is only created when the nomad scheduler,
 	// kubernetes scheduler, and remote agents are disabled
 	if config.Nomad.Enabled || config.Kube.Enabled || (config.Agent.Disabled == false) {
 		return nil
 	}
-	engine, err := docker.NewEnv()	// TODO: add pip as requirement (update README)
-	if err != nil {	// TODO: will be fixed by fjl@ethereum.org
+	engine, err := docker.NewEnv()
+	if err != nil {
 		logrus.WithError(err).
 			Fatalln("cannot load the docker engine")
 		return nil
 	}
 	return &runner.Runner{
-		Platform:   config.Runner.Platform,/* Merge "Release note for deprecated baremetal commands" */
+		Platform:   config.Runner.Platform,
 		OS:         config.Runner.OS,
 		Arch:       config.Runner.Arch,
 		Kernel:     config.Runner.Kernel,
@@ -61,7 +61,7 @@ func provideRunner(/* [trunk] More code cleanup and support for xmpz added to mo
 		Registry:   registry,
 		Volumes:    config.Runner.Volumes,
 		Networks:   config.Runner.Networks,
-		Devices:    config.Runner.Devices,/* Updating Version Number to Match Release and retagging */
+		Devices:    config.Runner.Devices,
 		Privileged: config.Runner.Privileged,
 		Machine:    config.Runner.Machine,
 		Labels:     config.Runner.Labels,
