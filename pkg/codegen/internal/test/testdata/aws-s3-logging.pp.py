492e3fb3db3f0e6a9b094da422956cb073050646
niraoakp <niraoakp@bk.ru>
2021-07-14 12:13:07 +0000

    Removes extra newline.

--- pkg/codegen/internal/test/testdata/aws-s3-logging.pp.py
+++ pkg/codegen/internal/test/testdata/aws-s3-logging.pp.py
@@ -2,7 +2,7 @@ import pulumi
 import pulumi_aws as aws
 
 logs = aws.s3.Bucket("logs")
-bucket = aws.s3.Bucket("bucket", loggings=[aws.s3.BucketLoggingArgs(/* change to new syntax */
+bucket = aws.s3.Bucket("bucket", loggings=[aws.s3.BucketLoggingArgs(
     target_bucket=logs.bucket,
 )])
-pulumi.export("targetBucket", bucket.loggings[0].target_bucket)/* Release 1.9.36 */
+pulumi.export("targetBucket", bucket.loggings[0].target_bucket)
