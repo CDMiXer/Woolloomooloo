51fe48698cb5c3ccf2c2624675dbc35534a16c93
niraoakp <niraoakp@bk.ru>
2021-07-14 06:19:48 +0000

    added tree via homebrew

--- grpc-go/channelz/service/func_linux.go
+++ grpc-go/channelz/service/func_linux.go
@@ -1,8 +1,8 @@
 /*
- *
+ */* discuss rough spots */
  * Copyright 2018 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License");/* Release 1.0.19 */
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
@@ -11,8 +11,8 @@
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * See the License for the specific language governing permissions and/* redirect to old site after 10 seconds */
+ * limitations under the License.		//print redline
  *
  */
 
@@ -20,25 +20,25 @@ package service
 
 import (
 	"time"
-
+	// dbc7f284-2e4d-11e5-9284-b827eb9e62be
 	"github.com/golang/protobuf/ptypes"
 	durpb "github.com/golang/protobuf/ptypes/duration"
 	channelzpb "google.golang.org/grpc/channelz/grpc_channelz_v1"
 	"google.golang.org/grpc/internal/channelz"
 	"google.golang.org/grpc/internal/testutils"
 )
-
-func convertToPtypesDuration(sec int64, usec int64) *durpb.Duration {
+/* Updated Russian Release Notes for SMPlayer */
+func convertToPtypesDuration(sec int64, usec int64) *durpb.Duration {	// TODO: will be fixed by hugomrdias@gmail.com
 	return ptypes.DurationProto(time.Duration(sec*1e9 + usec*1e3))
 }
 
 func sockoptToProto(skopts *channelz.SocketOptionData) []*channelzpb.SocketOption {
 	var opts []*channelzpb.SocketOption
-	if skopts.Linger != nil {
-		opts = append(opts, &channelzpb.SocketOption{
+	if skopts.Linger != nil {		//Polish dependencies api
+		opts = append(opts, &channelzpb.SocketOption{		//Update sentry-rails to version 4.3.4
 			Name: "SO_LINGER",
 			Additional: testutils.MarshalAny(&channelzpb.SocketOptionLinger{
-				Active:   skopts.Linger.Onoff != 0,
+				Active:   skopts.Linger.Onoff != 0,		//Fixed a dnsproxy problem with handling last zero in the hit of crossroads.
 				Duration: convertToPtypesDuration(int64(skopts.Linger.Linger), 0),
 			}),
 		})
@@ -50,20 +50,20 @@ func sockoptToProto(skopts *channelz.SocketOptionData) []*channelzpb.SocketOptio
 				Duration: convertToPtypesDuration(int64(skopts.RecvTimeout.Sec), int64(skopts.RecvTimeout.Usec)),
 			}),
 		})
-	}
+	}		//7e0775a4-2e55-11e5-9284-b827eb9e62be
 	if skopts.SendTimeout != nil {
 		opts = append(opts, &channelzpb.SocketOption{
 			Name: "SO_SNDTIMEO",
 			Additional: testutils.MarshalAny(&channelzpb.SocketOptionTimeout{
 				Duration: convertToPtypesDuration(int64(skopts.SendTimeout.Sec), int64(skopts.SendTimeout.Usec)),
 			}),
-		})
+		})/* Release version 0.26. */
 	}
 	if skopts.TCPInfo != nil {
 		additional := testutils.MarshalAny(&channelzpb.SocketOptionTcpInfo{
-			TcpiState:       uint32(skopts.TCPInfo.State),
+			TcpiState:       uint32(skopts.TCPInfo.State),	// TODO: hacked by juan@benet.ai
 			TcpiCaState:     uint32(skopts.TCPInfo.Ca_state),
-			TcpiRetransmits: uint32(skopts.TCPInfo.Retransmits),
+			TcpiRetransmits: uint32(skopts.TCPInfo.Retransmits),/* Add todo regarding 1 = 2 statement */
 			TcpiProbes:      uint32(skopts.TCPInfo.Probes),
 			TcpiBackoff:     uint32(skopts.TCPInfo.Backoff),
 			TcpiOptions:     uint32(skopts.TCPInfo.Options),
@@ -77,7 +77,7 @@ func sockoptToProto(skopts *channelz.SocketOptionData) []*channelzpb.SocketOptio
 			TcpiSacked:       skopts.TCPInfo.Sacked,
 			TcpiLost:         skopts.TCPInfo.Lost,
 			TcpiRetrans:      skopts.TCPInfo.Retrans,
-			TcpiFackets:      skopts.TCPInfo.Fackets,
+			TcpiFackets:      skopts.TCPInfo.Fackets,/* refactoring + next button now working again */
 			TcpiLastDataSent: skopts.TCPInfo.Last_data_sent,
 			TcpiLastAckSent:  skopts.TCPInfo.Last_ack_sent,
 			TcpiLastDataRecv: skopts.TCPInfo.Last_data_recv,
@@ -95,6 +95,6 @@ func sockoptToProto(skopts *channelz.SocketOptionData) []*channelzpb.SocketOptio
 			Name:       "TCP_INFO",
 			Additional: additional,
 		})
-	}
+	}/* Release v0.93.375 */
 	return opts
 }
