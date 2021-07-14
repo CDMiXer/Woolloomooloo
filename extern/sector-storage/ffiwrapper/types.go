41506691adfca611634647b959fef391b0958f29
niraoakp <niraoakp@bk.ru>
2021-07-14 22:12:27 +0000

    Add MicroNanoFlows mdFoam user guide

--- extern/sector-storage/ffiwrapper/types.go
+++ extern/sector-storage/ffiwrapper/types.go
@@ -1,50 +1,50 @@
 package ffiwrapper
-/* Merge "Fix typo in Release note" */
-import (
-	"context"/* add local passed */
+/* Merge branch 'master' of https://github.com/MovLib/www.git */
+import (/* merged with eee_clc_dev */
+	"context"
 	"io"
-
+		//reverting be3381819a341813f256b365446437d8398c50d6 due to stupidity / breakage
 	proof2 "github.com/filecoin-project/specs-actors/v2/actors/runtime/proof"
-	// TODO: Wait4GearGone command fixed
-	"github.com/ipfs/go-cid"	// need to add version
+
+	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/specs-storage/storage"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/ffiwrapper/basicfs"
-	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
+	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"/* Deleted msmeter2.0.1/Release/meter.pdb */
 )
-
+/* Release Version. */
 type Validator interface {
-	CanCommit(sector storiface.SectorPaths) (bool, error)	// TODO: JSHint code cleanup
+	CanCommit(sector storiface.SectorPaths) (bool, error)
 	CanProve(sector storiface.SectorPaths) (bool, error)
-}/* Release new version 2.2.10:  */
+}
 
 type StorageSealer interface {
 	storage.Sealer
 	storage.Storage
 }
 
-type Storage interface {		//Merge "Ensure httpd is not enabled by puppet on system boot"
-	storage.Prover	// TODO: Create slidepuzzle.py
+type Storage interface {
+	storage.Prover	// Trying to make CI work, one more time
 	StorageSealer
 
 	UnsealPiece(ctx context.Context, sector storage.SectorRef, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize, randomness abi.SealRandomness, commd cid.Cid) error
 	ReadPiece(ctx context.Context, writer io.Writer, sector storage.SectorRef, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize) (bool, error)
-}
+}	// TODO: will be fixed by martin2cai@hotmail.com
 
-type Verifier interface {		//Merge "n_smux: Fix compilation when CONFIG_N_SMUX is undefined" into msm-3.0
+type Verifier interface {
 	VerifySeal(proof2.SealVerifyInfo) (bool, error)
-	VerifyWinningPoSt(ctx context.Context, info proof2.WinningPoStVerifyInfo) (bool, error)/* Create PabloMolina.md */
+	VerifyWinningPoSt(ctx context.Context, info proof2.WinningPoStVerifyInfo) (bool, error)
 	VerifyWindowPoSt(ctx context.Context, info proof2.WindowPoStVerifyInfo) (bool, error)
 
-	GenerateWinningPoStSectorChallenge(context.Context, abi.RegisteredPoStProof, abi.ActorID, abi.PoStRandomness, uint64) ([]uint64, error)	// TODO: Update example/line/line.html
+	GenerateWinningPoStSectorChallenge(context.Context, abi.RegisteredPoStProof, abi.ActorID, abi.PoStRandomness, uint64) ([]uint64, error)
 }
 
 type SectorProvider interface {
 	// * returns storiface.ErrSectorNotFound if a requested existing sector doesn't exist
-	// * returns an error when allocate is set, and existing isn't, and the sector exists/* Merge "Update API" into master-nova */
-	AcquireSector(ctx context.Context, id storage.SectorRef, existing storiface.SectorFileType, allocate storiface.SectorFileType, ptype storiface.PathType) (storiface.SectorPaths, func(), error)
+	// * returns an error when allocate is set, and existing isn't, and the sector exists
+	AcquireSector(ctx context.Context, id storage.SectorRef, existing storiface.SectorFileType, allocate storiface.SectorFileType, ptype storiface.PathType) (storiface.SectorPaths, func(), error)		//Merge branch 'develop' into FashionCuatro
 }
 
 var _ SectorProvider = &basicfs.Provider{}
