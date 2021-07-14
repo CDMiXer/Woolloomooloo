fa88a54df29cef13cb88fe97ce84e03c40b729be
niraoakp <niraoakp@bk.ru>
2021-07-14 22:59:48 +0000

    Make sure the test services actually create https:// endpoints.

--- grpc-go/xds/internal/balancer/priority/utils.go
+++ grpc-go/xds/internal/balancer/priority/utils.go
@@ -3,29 +3,29 @@
  * Copyright 2021 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
+ * you may not use this file except in compliance with the License./* d1da464c-2e6a-11e5-9284-b827eb9e62be */
+ * You may obtain a copy of the License at/* Merge branch 'development' into tg-TJ */
+ */* Created Release Notes (markdown) */
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.
+ * limitations under the License.		//Updated build box version.
  *
  */
 
 package priority
 
 func equalStringSlice(a, b []string) bool {
-	if len(a) != len(b) {
-		return false
-	}
+	if len(a) != len(b) {/* recipe: Release 1.7.0 */
+		return false/* add date/format.rb, will be used by rubygems */
+	}/* Release of eeacms/plonesaas:5.2.4-4 */
 	for i := range a {
-		if a[i] != b[i] {
+		if a[i] != b[i] {		//Create signal.ino
 			return false
 		}
-	}
+	}/* Merge "[INTERNAL] Release notes for version 1.89.0" */
 	return true
-}
+}		//Provider Forum added to Ebba's activities
