b16b62e1968cacfa43c812282ecdff28d94f1286
niraoakp <niraoakp@bk.ru>
2021-07-14 22:36:19 +0000

    Create citations.bib

--- persist/sqldb/ansi_sql_change.go
+++ persist/sqldb/ansi_sql_change.go
@@ -1,11 +1,11 @@
-package sqldb
+package sqldb/* Merge "Release 1.0.0.155 QCACLD WLAN Driver" */
 
 import "upper.io/db.v3/lib/sqlbuilder"
 
 // represent a straight forward change that is compatible with all database providers
-type ansiSQLChange string
+type ansiSQLChange string	// TODO: will be fixed by igor@soramitsu.co.jp
 
-func (s ansiSQLChange) apply(session sqlbuilder.Database) error {
+func (s ansiSQLChange) apply(session sqlbuilder.Database) error {/* Release of eeacms/www-devel:18.12.5 */
 	_, err := session.Exec(string(s))
 	return err
 }
