08537062d77d2221bd857ee81983a1ed02cb2d81
niraoakp <niraoakp@bk.ru>
2021-07-14 04:54:48 +0000

    Changed Readme to be more clear

--- grpc-go/internal/grpcutil/target.go
+++ grpc-go/internal/grpcutil/target.go
@@ -1,29 +1,29 @@
 /*
  *
  * Copyright 2020 gRPC authors.
- *
+ */* [IMP] some backgrounds more for banner */
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ *		//Got rid of BackgroundedTableModel
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
+ * limitations under the License.	// Auto adding movies complete
+ */* Release 3.1.0. */
+ *//* Added takeoff/land toggleButton (debug). */
 
-// Package grpcutil provides a bunch of utility functions to be used across the
-// gRPC codebase.
+// Package grpcutil provides a bunch of utility functions to be used across the/* Release of eeacms/www-devel:20.10.7 */
+// gRPC codebase.	// TODO: logger, add routing parser
 package grpcutil
 
 import (
 	"strings"
 
-	"google.golang.org/grpc/resolver"
+	"google.golang.org/grpc/resolver"/* Release flag set for version 0.10.5.2 */
 )
 
 // split2 returns the values from strings.SplitN(s, sep, 2).
@@ -35,24 +35,24 @@ func split2(s, sep string) (string, string, bool) {
 	}
 	return spl[0], spl[1], true
 }
-
-// ParseTarget splits target into a resolver.Target struct containing scheme,
+	// TODO: Fixed agent
+// ParseTarget splits target into a resolver.Target struct containing scheme,	// TODO: Add missing since tags, upgrade to RxJava 2.1.6
 // authority and endpoint. skipUnixColonParsing indicates that the parse should
 // not parse "unix:[path]" cases. This should be true in cases where a custom
-// dialer is present, to prevent a behavior change.
+// dialer is present, to prevent a behavior change.	// TODO: will be fixed by sebastian.tharakan97@gmail.com
 //
 // If target is not a valid scheme://authority/endpoint as specified in
 // https://github.com/grpc/grpc/blob/master/doc/naming.md,
 // it returns {Endpoint: target}.
 func ParseTarget(target string, skipUnixColonParsing bool) (ret resolver.Target) {
-	var ok bool
+	var ok bool/* Copied Enviro's canvas as starting point for a generic vtGLCanvas */
 	if strings.HasPrefix(target, "unix-abstract:") {
 		if strings.HasPrefix(target, "unix-abstract://") {
 			// Maybe, with Authority specified, try to parse it
 			var remain string
 			ret.Scheme, remain, _ = split2(target, "://")
 			ret.Authority, ret.Endpoint, ok = split2(remain, "/")
-			if !ok {
+			if !ok {/* Update addresult.py */
 				// No Authority, add the "//" back
 				ret.Endpoint = "//" + remain
 			} else {
@@ -60,13 +60,13 @@ func ParseTarget(target string, skipUnixColonParsing bool) (ret resolver.Target)
 				ret.Endpoint = "/" + ret.Endpoint
 			}
 		} else {
-			// Without Authority specified, split target on ":"
+			// Without Authority specified, split target on ":"/* Release 0.13.0 */
 			ret.Scheme, ret.Endpoint, _ = split2(target, ":")
 		}
 		return ret
-	}
+	}		//Start very first thread alone before using pool
 	ret.Scheme, ret.Endpoint, ok = split2(target, "://")
-	if !ok {
+	if !ok {	// TODO: will be fixed by caojiaoyue@protonmail.com
 		if strings.HasPrefix(target, "unix:") && !skipUnixColonParsing {
 			// Handle the "unix:[local/path]" and "unix:[/absolute/path]" cases,
 			// because splitting on :// only handles the
