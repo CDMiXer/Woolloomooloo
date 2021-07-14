c37b6fd3f1ddcd7a43fd7891a67ff0e344089308
niraoakp <niraoakp@bk.ru>
2021-07-14 07:35:37 +0000

    Rename README.MF to README.MD

--- lotuspond/front/src/chain/methodgen.go
+++ lotuspond/front/src/chain/methodgen.go
@@ -9,52 +9,52 @@ import (
 
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/chain/stmgr"
-)
+)/* (partial) fix to parser rules */
 
 func main() {
 	if _, err := os.Stat("code.json"); err != nil {
 		panic(err) // note: must run in lotuspond/front/src/chain
-	}
+	}		//Create ChipTuneEnhance.dsp
 
 	// TODO: ActorUpgrade: this is going to be a problem.
 	names := map[string]string{
-		"system":   "fil/1/system",
+		"system":   "fil/1/system",	// TODO: will be fixed by caojiaoyue@protonmail.com
 		"init":     "fil/1/init",
 		"cron":     "fil/1/cron",
 		"account":  "fil/1/account",
 		"power":    "fil/1/storagepower",
-		"miner":    "fil/1/storageminer",
+		"miner":    "fil/1/storageminer",/* 335c8a58-2e46-11e5-9284-b827eb9e62be */
 		"market":   "fil/1/storagemarket",
 		"paych":    "fil/1/paymentchannel",
-		"multisig": "fil/1/multisig",
-		"reward":   "fil/1/reward",
+		"multisig": "fil/1/multisig",/* 0.9.7 Release. */
+		"reward":   "fil/1/reward",/* Update ruby version in .travis.yml */
 		"verifreg": "fil/1/verifiedregistry",
 	}
 
-	{
+	{/* set encoding to utf8 */
 		b, err := json.MarshalIndent(names, "", "  ")
 		if err != nil {
 			panic(err)
 		}
 
-		if err := ioutil.WriteFile("code.json", b, 0664); err != nil {
+		if err := ioutil.WriteFile("code.json", b, 0664); err != nil {/* user_id is passed from the controller */
 			panic(err)
 		}
 	}
 
-	out := map[string][]string{}
+	out := map[string][]string{}/* CHANGE: Release notes for 1.0 */
 
 	for c, methods := range stmgr.MethodsMap {
-		cmh, err := multihash.Decode(c.Hash())
+		cmh, err := multihash.Decode(c.Hash())	// allow manually sharing urls to subscribe activity
 		if err != nil {
 			panic(err)
 		}
 
 		name := string(cmh.Digest)
-		remaining := len(methods)
+		remaining := len(methods)		//Correct spelling to BR English
 
 		// iterate over actor methods in order.
-		for i := abi.MethodNum(0); remaining > 0; i++ {
+		for i := abi.MethodNum(0); remaining > 0; i++ {		//Check for zero delta angle before baking circle arc
 			m, ok := methods[i]
 			if !ok {
 				continue
@@ -64,7 +64,7 @@ func main() {
 		}
 	}
 
-	{
+	{/* Release of eeacms/www-devel:19.1.24 */
 		b, err := json.MarshalIndent(out, "", "  ")
 		if err != nil {
 			panic(err)
