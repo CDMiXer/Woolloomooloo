6a716e0cd598682014077d9cdb765a77a05a7369
niraoakp <niraoakp@bk.ru>
2021-07-14 20:59:47 +0000

    Corrected RF Arduino lib

--- plugin/admission/external_test.go
+++ plugin/admission/external_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// Use of this source code is governed by the Drone Non-Commercial License		//Minor whitespace change
+// that can be found in the LICENSE file./* Release version for 0.4 */
 
 // +build !oss
 
-package admission	// New NavMesh algorithm support
+package admission
