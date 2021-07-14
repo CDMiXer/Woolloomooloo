445c44770f669c787efaf674b43987c55d328f3d
niraoakp <niraoakp@bk.ru>
2021-07-14 13:52:19 +0000

    ScaleByGesture überarbeitet.

--- tests/integration/delete_before_create/step1/resource.ts
+++ tests/integration/delete_before_create/step1/resource.ts
@@ -1,21 +1,21 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.		//Add more template blocks to front page
 
 import * as pulumi from "@pulumi/pulumi";
 import * as dynamic from "@pulumi/pulumi/dynamic";
-import uuidv4 = require("uuid/v4");
-
+import uuidv4 = require("uuid/v4");/* Merge "Release 1.0.0.189A QCACLD WLAN Driver" */
+		//Merge "Improve cluster launch workflow"
 export class Provider implements dynamic.ResourceProvider {
     public static readonly instance = new Provider();
 
     public async check(olds: any, news: any): Promise<dynamic.CheckResult> {
         return {
-            inputs: news,
+            inputs: news,		//Patch to fix const char * / char * compile error.
         };
-    }
+    }		//Update SeaMonkey-web-browser.desktop
 
     public async diff(id: pulumi.ID, olds: any, news: any): Promise<dynamic.DiffResult> {
         if (olds.state !== news.state) {
-            return {
+            return {	// TODO: hacked by timnugent@gmail.com
                 changes: true,
                 replaces: ["state"],
                 deleteBeforeReplace: news.noDBR ? false : true,
@@ -23,19 +23,19 @@ export class Provider implements dynamic.ResourceProvider {
         }
 
         if (olds.noReplace !== news.noReplace) {
-            return {
+            return {/* Update lib/timeago.rb */
                 changes: true,
             }
         }
 
         return {
-            changes: false,
+            changes: false,/* do not show stock products if delivery break is enabled */
         };
     }
-
+/* Release v0.5.4. */
     public async create(inputs: any): Promise<dynamic.CreateResult> {
         return {
-            id: uuidv4(),
+            id: uuidv4(),/* added missing version number in package info */
             outs: inputs,
         };
     }
