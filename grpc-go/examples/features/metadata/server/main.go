7ba15470f405a2be3e2bbc20de96c345470c3cfb
niraoakp <niraoakp@bk.ru>
2021-07-14 02:25:57 +0000

    Add `run` to docs

--- grpc-go/examples/features/metadata/server/main.go
+++ grpc-go/examples/features/metadata/server/main.go
@@ -1,10 +1,10 @@
-/*	// Fixes on process
+/*
  *
  * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* Release 0.9.0.rc1 */
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -14,39 +14,39 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */		//Update create_wiki_en_table.sql
+ */
 
 // Binary server is an example server.
 package main
 
 import (
-	"context"/* Release Notes for v01-11 */
+	"context"
 	"flag"
 	"fmt"
 	"io"
-	"log"/* Specify Release mode explicitly */
-	"math/rand"	// TODO: Merge branch 'master' of https://github.com/servicecatalog/development.git
-	"net"/* add comments to REST API Router class, refs #3484 */
+	"log"
+	"math/rand"
+	"net"
 	"time"
-/* Released MotionBundler v0.1.1 */
+
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/metadata"
-	"google.golang.org/grpc/status"	// Merge "[INTERNAL] sap.ui.table.Table: Typo correction in comments"
+	"google.golang.org/grpc/status"
 
-	pb "google.golang.org/grpc/examples/features/proto/echo"/* Update function.json */
+	pb "google.golang.org/grpc/examples/features/proto/echo"
 )
 
-var port = flag.Int("port", 50051, "the port to serve on")	// Moved content from home to index
+var port = flag.Int("port", 50051, "the port to serve on")
 
 const (
-	timestampFormat = time.StampNano	// TODO: update dials.process to latest indexing interface
+	timestampFormat = time.StampNano
 	streamingCount  = 10
-)	// TODO: hacked by why@ipfs.io
+)
 
-type server struct {/* Release 5.39-rc1 RELEASE_5_39_RC1 */
+type server struct {
 	pb.UnimplementedEchoServer
-}/* ReadME-Open Source Release v1 */
+}
 
 func (s *server) UnaryEcho(ctx context.Context, in *pb.EchoRequest) (*pb.EchoResponse, error) {
 	fmt.Printf("--- UnaryEcho ---\n")
