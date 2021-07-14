7d34ad2b2290bd87d91a4f03d570fd5e33ff9448
niraoakp <niraoakp@bk.ru>
2021-07-14 01:28:11 +0000

    Added not with current conditions of script development

--- tests/integration/get_resource/go/main.go
+++ tests/integration/get_resource/go/main.go
@@ -1,5 +1,5 @@
 package main
-/* Release v0.91 */
+
 import (
 	"reflect"
 
@@ -11,8 +11,8 @@ type MyResource struct {
 	pulumi.ResourceState
 
 	Length pulumi.IntOutput `pulumi:"length"`
-}		//[Videos] Red Hat Summit Youtube Channel
-		//[gui-components] bugfix: complete name of train is now translated
+}
+
 type myResourceArgs struct{}
 type MyResourceArgs struct{}
 
@@ -20,21 +20,21 @@ func (MyResourceArgs) ElementType() reflect.Type {
 	return reflect.TypeOf((*myResourceArgs)(nil)).Elem()
 }
 
-func GetResource(ctx *pulumi.Context, urn pulumi.URN) (*MyResource, error) {/* Update SmallAppliances.cs */
-	var resource MyResource/* update 3.md */
+func GetResource(ctx *pulumi.Context, urn pulumi.URN) (*MyResource, error) {
+	var resource MyResource
 	err := ctx.RegisterResource("unused:unused:unused", "unused", &MyResourceArgs{}, &resource,
 		pulumi.URN_(string(urn)))
 	if err != nil {
 		return nil, err
 	}
-	return &resource, nil/* Some improvements in the manangement of variables */
+	return &resource, nil
 }
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-	// TODO: will be fixed by willem.melching@gmail.com
+
 		pet, err := random.NewRandomPet(ctx, "cat", &random.RandomPetArgs{
-			Length: pulumi.Int(2),/* Added support for Xcode 6.3 Release */
+			Length: pulumi.Int(2),
 		})
 		if err != nil {
 			return err
