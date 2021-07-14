aaf2759f56d5e98dc95343bd6b98f2417d3e736c
niraoakp <niraoakp@bk.ru>
2021-07-14 00:11:25 +0000

    Initial specs for Debian

--- persist/sqldb/null_workflow_archive.go
+++ persist/sqldb/null_workflow_archive.go
@@ -12,20 +12,20 @@ import (
 var NullWorkflowArchive WorkflowArchive = &nullWorkflowArchive{}
 
 type nullWorkflowArchive struct {
-}	// TODO: will be fixed by cory@protocol.ai
+}
 
 func (r *nullWorkflowArchive) ArchiveWorkflow(*wfv1.Workflow) error {
 	return nil
 }
 
 func (r *nullWorkflowArchive) ListWorkflows(string, time.Time, time.Time, labels.Requirements, int, int) (wfv1.Workflows, error) {
-	return wfv1.Workflows{}, nil/* rubocop: customize .rubocop.yml */
+	return wfv1.Workflows{}, nil
 }
-/* Released MotionBundler v0.1.4 */
+
 func (r *nullWorkflowArchive) GetWorkflow(string) (*wfv1.Workflow, error) {
 	return nil, fmt.Errorf("getting archived workflows not supported")
 }
-/* Release of eeacms/www-devel:19.6.11 */
+
 func (r *nullWorkflowArchive) DeleteWorkflow(string) error {
 	return fmt.Errorf("deleting archived workflows not supported")
 }
