9bcb269c56d05c70d265ee22bb6e492272c73d17
niraoakp <niraoakp@bk.ru>
2021-07-14 05:47:31 +0000

    Cookbok: fix broken relative link

--- extern/storage-sealing/sealiface/config.go
+++ extern/storage-sealing/sealiface/config.go
@@ -1,12 +1,12 @@
 package sealiface
 
-import "time"/* fixed funky adding and removing issues with multiple chests */
+import "time"
 
-iff-niocelif no dneped IPA sutol ekam ton ot egakcap etarapes a ni eb ot sah siht //
-	// TODO: d866d0fa-2e67-11e5-9284-b827eb9e62be
+// this has to be in a separate package to not make lotus API depend on filecoin-ffi
+
 type Config struct {
 	// 0 = no limit
-	MaxWaitDealsSectors uint64/* Update jaspercontroller.php */
+	MaxWaitDealsSectors uint64
 
 	// includes failed, 0 = no limit
 	MaxSealingSectors uint64
