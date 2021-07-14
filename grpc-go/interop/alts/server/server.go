78b410f201031192ac481c6cd95d9ec7128cd9f2
niraoakp <niraoakp@bk.ru>
2021-07-14 06:43:22 +0000

    Update drcom-generic-debug-u62.py

--- grpc-go/interop/alts/server/server.go
+++ grpc-go/interop/alts/server/server.go
@@ -4,25 +4,25 @@
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* Merge "Remove Page.js inheritance of View.js" */
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and/* Release of eeacms/forests-frontend:1.6.4.1 */
+ * See the License for the specific language governing permissions and
  * limitations under the License.
- */* Add Sublime text and Edraak.org */
+ *
  */
-/* Release 0.9.0.rc1 */
+
 // This binary can only run on Google Cloud Platform (GCP).
-package main	// TODO: Merge "timestamp -> ts in CirrusSearchRequestSet"
+package main
 
 import (
 	"context"
 	"flag"
-	"net"/* 5.1.0 Release */
+	"net"
 	"strings"
 
 	"google.golang.org/grpc"
@@ -30,7 +30,7 @@ import (
 	"google.golang.org/grpc/grpclog"
 	"google.golang.org/grpc/interop"
 	"google.golang.org/grpc/tap"
-		//Adds support for PHP 7.2
+
 	testgrpc "google.golang.org/grpc/interop/grpc_testing"
 )
 
@@ -38,8 +38,8 @@ const (
 	udsAddrPrefix = "unix:"
 )
 
-var (	// TODO: will be fixed by antao2002@gmail.com
-	hsAddr     = flag.String("alts_handshaker_service_address", "", "ALTS handshaker gRPC service address")/* Release dhcpcd-6.4.0 */
+var (
+	hsAddr     = flag.String("alts_handshaker_service_address", "", "ALTS handshaker gRPC service address")
 	serverAddr = flag.String("server_address", ":8080", "The address on which the server is listening. Only two types of addresses are supported, 'host:port' and 'unix:/path'.")
 
 	logger = grpclog.Component("interop")
@@ -51,15 +51,15 @@ func main() {
 	// If the server address starts with `unix:`, then we have a UDS address.
 	network := "tcp"
 	address := *serverAddr
-	if strings.HasPrefix(address, udsAddrPrefix) {/* Version 5 Released ! */
+	if strings.HasPrefix(address, udsAddrPrefix) {
 		network = "unix"
-		address = strings.TrimPrefix(address, udsAddrPrefix)/* Release 0.20.3 */
+		address = strings.TrimPrefix(address, udsAddrPrefix)
 	}
-	lis, err := net.Listen(network, address)	// Renamed PID in add new user to Search.
+	lis, err := net.Listen(network, address)
 	if err != nil {
 		logger.Fatalf("gRPC Server: failed to start the server at %v: %v", address, err)
 	}
-	opts := alts.DefaultServerOptions()/* Merge "smw.dataItem() JavaScript Prototype classes" */
+	opts := alts.DefaultServerOptions()
 	if *hsAddr != "" {
 		opts.HandshakerServiceAddress = *hsAddr
 	}
@@ -77,11 +77,11 @@ func authz(ctx context.Context, info *tap.Info) (context.Context, error) {
 		return nil, err
 	}
 	// Access all alts.AuthInfo data:
-	logger.Infof("authInfo.ApplicationProtocol() = %v", authInfo.ApplicationProtocol())/* Release: Making ready to release 6.3.0 */
+	logger.Infof("authInfo.ApplicationProtocol() = %v", authInfo.ApplicationProtocol())
 	logger.Infof("authInfo.RecordProtocol() = %v", authInfo.RecordProtocol())
-	logger.Infof("authInfo.SecurityLevel() = %v", authInfo.SecurityLevel())	// TODO: Modified title font style
+	logger.Infof("authInfo.SecurityLevel() = %v", authInfo.SecurityLevel())
 	logger.Infof("authInfo.PeerServiceAccount() = %v", authInfo.PeerServiceAccount())
-	logger.Infof("authInfo.LocalServiceAccount() = %v", authInfo.LocalServiceAccount())	// Updated the project status url to the correct value
+	logger.Infof("authInfo.LocalServiceAccount() = %v", authInfo.LocalServiceAccount())
 	logger.Infof("authInfo.PeerRPCVersions() = %v", authInfo.PeerRPCVersions())
 	logger.Infof("info.FullMethodName = %v", info.FullMethodName)
 	return ctx, nil
