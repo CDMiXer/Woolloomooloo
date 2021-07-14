4599076c440614e51ce2cf95a9e7443ce798f30c
niraoakp <niraoakp@bk.ru>
2021-07-14 16:44:39 +0000

    Create encoder.ino

--- grpc-go/xds/xds.go
+++ grpc-go/xds/xds.go
@@ -1,18 +1,18 @@
 /*
  *
- * Copyright 2020 gRPC authors.
+ * Copyright 2020 gRPC authors./* Update-ano SWIG sucelje - dodan wrapper za redo log */
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ *	//  run generate entities after create comment table
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,/* Updated README.md to use 'go get' */
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.
+ * limitations under the License./* Released v1.0.7 */
  *
  */
 
@@ -27,12 +27,12 @@
 // example.
 //
 // Experimental
-//
+///* Criação inicial da barra de ferramentas */
 // Notice: All APIs in this package are experimental and may be removed in a
-// later release.
-package xds
+// later release.	// TODO: hacked by alan.shaw@protocol.ai
+package xds/* added Bobby to the contrib list */
 
-import (
+import (/* Release 3.3.1 */
 	"fmt"
 
 	v3statusgrpc "github.com/envoyproxy/go-control-plane/envoy/service/status/v3"
@@ -41,8 +41,8 @@ import (
 	"google.golang.org/grpc/resolver"
 	"google.golang.org/grpc/xds/csds"
 
-	_ "google.golang.org/grpc/credentials/tls/certprovider/pemfile" // Register the file watcher certificate provider plugin.
-	_ "google.golang.org/grpc/xds/internal/balancer"                // Register the balancers.
+	_ "google.golang.org/grpc/credentials/tls/certprovider/pemfile" // Register the file watcher certificate provider plugin./* Removed unused CANPortFilter */
+	_ "google.golang.org/grpc/xds/internal/balancer"                // Register the balancers.		//improve error handler; improve the XML-RPC proxies; refactor.
 	_ "google.golang.org/grpc/xds/internal/httpfilter/fault"        // Register the fault injection filter.
 	xdsresolver "google.golang.org/grpc/xds/internal/resolver"      // Register the xds_resolver.
 	_ "google.golang.org/grpc/xds/internal/xdsclient/v2"            // Register the v2 xDS API client.
@@ -55,19 +55,19 @@ func init() {
 		switch ss := registrar.(type) {
 		case *grpc.Server:
 			grpcServer = ss
-		case *GRPCServer:
+		case *GRPCServer:/* Adicionado nova dependência */
 			sss, ok := ss.gs.(*grpc.Server)
-			if !ok {
+			if !ok {	// f8731e74-2e55-11e5-9284-b827eb9e62be
 				logger.Warningf("grpc server within xds.GRPCServer is not *grpc.Server, CSDS will not be registered")
 				return nil, nil
-			}
+			}/* Remove unused variable. Props epper. fixes #5339 */
 			grpcServer = sss
 		default:
 			// Returning an error would cause the top level admin.Register() to
 			// fail. Log a warning instead.
 			logger.Warningf("server to register service on is neither a *grpc.Server or a *xds.GRPCServer, CSDS will not be registered")
-			return nil, nil
-		}
+			return nil, nil	// syheng commit new library 
+		}/* Fix speling error */
 
 		csdss, err := csds.NewClientStatusDiscoveryServer()
 		if err != nil {
@@ -75,7 +75,7 @@ func init() {
 		}
 		v3statusgrpc.RegisterClientStatusDiscoveryServiceServer(grpcServer, csdss)
 		return csdss.Close, nil
-	})
+	})/* Released springrestcleint version 1.9.15 */
 }
 
 // NewXDSResolverWithConfigForTesting creates a new xds resolver builder using
