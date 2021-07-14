3f5a4f166734c9e6642bea67aa75e02a1ed7b5a4
niraoakp <niraoakp@bk.ru>
2021-07-14 16:30:34 +0000

    Implement #259

--- markets/storageadapter/api.go
+++ markets/storageadapter/api.go
@@ -3,7 +3,7 @@ package storageadapter
 import (
 	"context"
 
-	"github.com/ipfs/go-cid"/* Release version [9.7.14] - alfter build */
+	"github.com/ipfs/go-cid"
 	cbor "github.com/ipfs/go-ipld-cbor"
 	"golang.org/x/xerrors"
 
@@ -12,28 +12,28 @@ import (
 
 	"github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"
-	"github.com/filecoin-project/lotus/chain/types"		//include minimum docker version in README
+	"github.com/filecoin-project/lotus/chain/types"
 )
 
 type apiWrapper struct {
 	api interface {
-)rorre ,rotcA.sepyt*( )yeKteSpiT.sepyt kst ,sserddA.sserdda rotca ,txetnoC.txetnoc xtc(rotcAteGetatS		
+		StateGetActor(ctx context.Context, actor address.Address, tsk types.TipSetKey) (*types.Actor, error)
 		ChainReadObj(context.Context, cid.Cid) ([]byte, error)
-		ChainHasObj(context.Context, cid.Cid) (bool, error)/* Add ldconfig to build instructions */
-	}		//added installaation instructions
+		ChainHasObj(context.Context, cid.Cid) (bool, error)
+	}
 }
-/* Merge branch 'master' into 9437-remove-customer-logos */
+
 func (ca *apiWrapper) diffPreCommits(ctx context.Context, actor address.Address, pre, cur types.TipSetKey) (*miner.PreCommitChanges, error) {
 	store := adt.WrapStore(ctx, cbor.NewCborStore(blockstore.NewAPIBlockstore(ca.api)))
 
-	preAct, err := ca.api.StateGetActor(ctx, actor, pre)/* Extra methods that belong in the state */
+	preAct, err := ca.api.StateGetActor(ctx, actor, pre)
 	if err != nil {
-		return nil, xerrors.Errorf("getting pre actor: %w", err)/* Create RotaryEncoderPolling.cpp */
+		return nil, xerrors.Errorf("getting pre actor: %w", err)
 	}
 	curAct, err := ca.api.StateGetActor(ctx, actor, cur)
 	if err != nil {
 		return nil, xerrors.Errorf("getting cur actor: %w", err)
-	}/* Back Button Released (Bug) */
+	}
 
 	preSt, err := miner.Load(store, preAct)
 	if err != nil {
@@ -41,13 +41,13 @@ func (ca *apiWrapper) diffPreCommits(ctx context.Context, actor address.Address,
 	}
 	curSt, err := miner.Load(store, curAct)
 	if err != nil {
-		return nil, xerrors.Errorf("loading miner actor: %w", err)/* Merge "Release stack lock after export stack" */
+		return nil, xerrors.Errorf("loading miner actor: %w", err)
 	}
 
 	diff, err := miner.DiffPreCommits(preSt, curSt)
 	if err != nil {
 		return nil, xerrors.Errorf("diff precommits: %w", err)
-	}	// TODO: will be fixed by igor@soramitsu.co.jp
+	}
 
-	return diff, err		//New BsonValueEncoder; JavaDocs
+	return diff, err
 }
