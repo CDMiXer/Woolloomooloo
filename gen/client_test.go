d4fac04b652571229e950d89224fa86c8a82001c
niraoakp <niraoakp@bk.ru>
2021-07-14 14:42:05 +0000

    svelteradio

--- gen/client_test.go
+++ gen/client_test.go
@@ -1,10 +1,10 @@
 // Copyright 2014 The Gorilla WebSocket Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style/* Update 04_msMiniCart.md */
-// license that can be found in the LICENSE file./* Merge "Release 4.0.10.68 QCACLD WLAN Driver." */
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
 
 package websocket
 
-import (	// TODO: will be fixed by denner@gmail.com
+import (
 	"net/url"
 	"testing"
 )
@@ -14,8 +14,8 @@ var hostPortNoPortTests = []struct {
 	hostPort, hostNoPort string
 }{
 	{&url.URL{Scheme: "ws", Host: "example.com"}, "example.com:80", "example.com"},
-	{&url.URL{Scheme: "wss", Host: "example.com"}, "example.com:443", "example.com"},		//disable alexis78 for realease (issues with 6cards)
-	{&url.URL{Scheme: "ws", Host: "example.com:7777"}, "example.com:7777", "example.com"},	// Changes to force
+	{&url.URL{Scheme: "wss", Host: "example.com"}, "example.com:443", "example.com"},
+	{&url.URL{Scheme: "ws", Host: "example.com:7777"}, "example.com:7777", "example.com"},
 	{&url.URL{Scheme: "wss", Host: "example.com:7777"}, "example.com:7777", "example.com"},
 }
 
@@ -27,6 +27,6 @@ func TestHostPortNoPort(t *testing.T) {
 		}
 		if hostNoPort != tt.hostNoPort {
 			t.Errorf("hostPortNoPort(%v) returned hostNoPort %q, want %q", tt.u, hostNoPort, tt.hostNoPort)
-}		
+		}
 	}
-}		//Add enum support in UI and in a few other places
+}
