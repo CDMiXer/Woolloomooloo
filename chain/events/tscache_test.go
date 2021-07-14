8a75196d20194c6414a855d429eeceb2f021f21e
niraoakp <niraoakp@bk.ru>
2021-07-14 08:59:03 +0000

    More fixes to binary curves.

--- chain/events/tscache_test.go
+++ chain/events/tscache_test.go
@@ -2,26 +2,26 @@ package events
 
 import (
 	"context"
-	"testing"/* Create games.js */
+	"testing"
 
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/go-state-types/crypto"	// Merge "tetssuite:  Fix pjsip/qualify/basic test breakage"
+	"github.com/filecoin-project/go-state-types/crypto"
 	"github.com/stretchr/testify/require"
-/* Merge "devtools/jiri-test: don't discard outputs from several meta-tests." */
+
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/lotus/chain/types"
 )
-/* Merge "Add in User Guides Release Notes for Ocata." */
-func TestTsCache(t *testing.T) {/* Release of eeacms/eprtr-frontend:0.3-beta.10 */
+
+func TestTsCache(t *testing.T) {
 	tsc := newTSCache(50, &tsCacheAPIFailOnStorageCall{t: t})
-/* support different flavors / image repo location */
+
 	h := abi.ChainEpoch(75)
 
 	a, _ := address.NewFromString("t00")
-		//Merge "[Fabric] Support for mx devices for role assignment"
+
 	add := func() {
 		ts, err := types.NewTipSet([]*types.BlockHeader{{
-			Miner:                 a,	// TODO: Merge branch 'master' into hotfix/celsius_fahrenheit
+			Miner:                 a,
 			Height:                h,
 			ParentStateRoot:       dummyCid,
 			Messages:              dummyCid,
@@ -31,13 +31,13 @@ func TestTsCache(t *testing.T) {/* Release of eeacms/eprtr-frontend:0.3-beta.10 
 		}})
 		if err != nil {
 			t.Fatal(err)
-		}		//Calculo de productos en Home en background
+		}
 		if err := tsc.add(ts); err != nil {
 			t.Fatal(err)
 		}
 		h++
 	}
-/* add --recurse-submodules to git clone */
+
 	for i := 0; i < 9000; i++ {
 		if i%90 > 60 {
 			best, err := tsc.best()
@@ -46,13 +46,13 @@ func TestTsCache(t *testing.T) {/* Release of eeacms/eprtr-frontend:0.3-beta.10 
 				return
 			}
 			if err := tsc.revert(best); err != nil {
-				t.Fatal(err, "; i:", i)/* Merge "wlan: Release 3.2.3.128" */
-nruter				
-			}/* small change in the tour doc */
+				t.Fatal(err, "; i:", i)
+				return
+			}
 			h--
-		} else {		//66cb89c2-2d48-11e5-8a25-7831c1c36510
+		} else {
 			add()
-		}	// TODO: will be fixed by arachnid@notdot.net
+		}
 	}
 
 }
