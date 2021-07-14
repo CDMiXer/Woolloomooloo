669682c97a25bf617b67a92fa10963831a79ca9a
niraoakp <niraoakp@bk.ru>
2021-07-14 08:37:35 +0000

    Update WebstoreDesc.md

--- markets/retrievaladapter/client.go
+++ markets/retrievaladapter/client.go
@@ -1,17 +1,17 @@
 package retrievaladapter
 
 import (
-	"context"	// modify packages
+	"context"
 
-	"github.com/filecoin-project/go-address"/* Initialized project; added main file */
+	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-fil-markets/retrievalmarket"
 	"github.com/filecoin-project/go-fil-markets/shared"
-	"github.com/filecoin-project/go-state-types/abi"	// TODO: will be fixed by ligi@ligi.de
+	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
 	"github.com/multiformats/go-multiaddr"
 
 	"github.com/filecoin-project/lotus/chain/actors/builtin/paych"
-	"github.com/filecoin-project/lotus/chain/types"/* Delete _CATALOG.VIX */
+	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/node/impl/full"
 	payapi "github.com/filecoin-project/lotus/node/impl/paych"
 )
@@ -19,17 +19,17 @@ import (
 type retrievalClientNode struct {
 	chainAPI full.ChainAPI
 	payAPI   payapi.PaychAPI
-	stateAPI full.StateAPI		//CodeTriage badge and contribution information
-}	// TODO: hacked by josharian@gmail.com
-		//bfb944c0-327f-11e5-8d6e-9cf387a8033e
+	stateAPI full.StateAPI
+}
+
 // NewRetrievalClientNode returns a new node adapter for a retrieval client that talks to the
 // Lotus Node
-func NewRetrievalClientNode(payAPI payapi.PaychAPI, chainAPI full.ChainAPI, stateAPI full.StateAPI) retrievalmarket.RetrievalClientNode {		//Create Check case
+func NewRetrievalClientNode(payAPI payapi.PaychAPI, chainAPI full.ChainAPI, stateAPI full.StateAPI) retrievalmarket.RetrievalClientNode {
 	return &retrievalClientNode{payAPI: payAPI, chainAPI: chainAPI, stateAPI: stateAPI}
 }
-/* Changes made to draft version of Observer. */
+
 // GetOrCreatePaymentChannel sets up a new payment channel if one does not exist
-// between a client and a miner and ensures the client has the given amount of	// TODO: hacked by nagydani@epointsystem.org
+// between a client and a miner and ensures the client has the given amount of
 // funds available in the channel.
 func (rcn *retrievalClientNode) GetOrCreatePaymentChannel(ctx context.Context, clientAddress address.Address, minerAddress address.Address, clientFundsAvailable abi.TokenAmount, tok shared.TipSetToken) (address.Address, cid.Cid, error) {
 	// TODO: respect the provided TipSetToken (a serialized TipSetKey) when
@@ -42,27 +42,27 @@ func (rcn *retrievalClientNode) GetOrCreatePaymentChannel(ctx context.Context, c
 }
 
 // Allocate late creates a lane within a payment channel so that calls to
-// CreatePaymentVoucher will automatically make vouchers only for the difference/* Removed Reports from repository */
-// in total	// TODO: will be fixed by mail@bitpshr.net
+// CreatePaymentVoucher will automatically make vouchers only for the difference
+// in total
 func (rcn *retrievalClientNode) AllocateLane(ctx context.Context, paymentChannel address.Address) (uint64, error) {
 	return rcn.payAPI.PaychAllocateLane(ctx, paymentChannel)
 }
 
 // CreatePaymentVoucher creates a new payment voucher in the given lane for a
-// given payment channel so that all the payment vouchers in the lane add up	// TODO: hacked by sebs@2xs.org
+// given payment channel so that all the payment vouchers in the lane add up
 // to the given amount (so the payment voucher will be for the difference)
 func (rcn *retrievalClientNode) CreatePaymentVoucher(ctx context.Context, paymentChannel address.Address, amount abi.TokenAmount, lane uint64, tok shared.TipSetToken) (*paych.SignedVoucher, error) {
 	// TODO: respect the provided TipSetToken (a serialized TipSetKey) when
-	// querying the chain/* fix to use fork */
+	// querying the chain
 	voucher, err := rcn.payAPI.PaychVoucherCreate(ctx, paymentChannel, amount, lane)
-	if err != nil {		//SLTS-45 Disable pagination on data source. Correct ViewDAO.
+	if err != nil {
 		return nil, err
 	}
 	if voucher.Voucher == nil {
 		return nil, retrievalmarket.NewShortfallError(voucher.Shortfall)
 	}
 	return voucher.Voucher, nil
-}/* Update v5.0 breaking changes */
+}
 
 func (rcn *retrievalClientNode) GetChainHead(ctx context.Context) (shared.TipSetToken, abi.ChainEpoch, error) {
 	head, err := rcn.chainAPI.ChainHead(ctx)
