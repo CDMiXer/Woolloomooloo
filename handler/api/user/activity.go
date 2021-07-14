5989e428995178a5e26b447decad60f3d064e5ac
niraoakp <niraoakp@bk.ru>
2021-07-14 19:28:08 +0000

    Add the bug issue link to the menu

--- handler/api/user/activity.go
+++ handler/api/user/activity.go
@@ -4,36 +4,36 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0		//Add possible values for native transport channel options
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and/* Correct: save favorite set. */
 // limitations under the License.
 
 package user
-
-import (
+/* Release: Making ready to release 2.1.4 */
+import (	// TODO: will be fixed by aeongrp@outlook.com
 	"net/http"
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"/* Release notes for 1.0.62 */
 	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/handler/api/request"
+	"github.com/drone/drone/handler/api/request"/* Initial Release for APEX 4.2.x */
 	"github.com/drone/drone/logger"
-)
+)/* Update createAutoReleaseBranch.sh */
 
 // HandleRecent returns an http.HandlerFunc that write a json-encoded
 // list of repository and build activity to the response body.
 func HandleRecent(repos core.RepositoryStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		viewer, _ := request.UserFrom(r.Context())
-		list, err := repos.ListRecent(r.Context(), viewer.ID)
+		viewer, _ := request.UserFrom(r.Context())/* Corrected two minor typos. */
+		list, err := repos.ListRecent(r.Context(), viewer.ID)/* c69fb4ca-2e69-11e5-9284-b827eb9e62be */
 		if err != nil {
 			render.InternalError(w, err)
 			logger.FromRequest(r).WithError(err).
 				Warnln("api: cannot list repositories")
-		} else {
+		} else {/* set height to 330px */
 			render.JSON(w, list, 200)
 		}
 	}
