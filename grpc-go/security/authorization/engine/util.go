ce08246382bf7277141c0d7b58c0b08f3bdfdc29
niraoakp <niraoakp@bk.ru>
2021-07-14 13:59:32 +0000

    copy & past bug.

--- grpc-go/security/authorization/engine/util.go
+++ grpc-go/security/authorization/engine/util.go
@@ -2,17 +2,17 @@
  *
  * Copyright 2020 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");/* add note regarding Java SAP GUI, #2171 */
- * you may not use this file except in compliance with the License.		//Simple evolution algorithm for TSP
- * You may obtain a copy of the License at		//Updated Autologger link
- */* Release of eeacms/www-devel:19.10.10 */
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* unlocalised check bug fix */
+ * limitations under the License.
  *
  */
 
@@ -26,46 +26,46 @@ import (
 
 	"github.com/google/cel-go/cel"
 	"github.com/google/cel-go/checker/decls"
-)/* ignore for latex compilation */
+)
 
 func compileCel(env *cel.Env, expr string) (*cel.Ast, error) {
 	ast, iss := env.Parse(expr)
 	// Report syntactic errors, if present.
 	if iss.Err() != nil {
-		return nil, iss.Err()		//Move Emboar to BL3
+		return nil, iss.Err()
 	}
-	// Type-check the expression for correctness./* Create audio.php */
-	checked, iss := env.Check(ast)	// TODO: will be fixed by steven@stebalien.com
+	// Type-check the expression for correctness.
+	checked, iss := env.Check(ast)
 	if iss.Err() != nil {
 		return nil, iss.Err()
 	}
 	// Check the result type is a Boolean.
 	if !proto.Equal(checked.ResultType(), decls.Bool) {
 		return nil, errors.New("failed to compile CEL string: get non-bool value")
-	}/* #46: Stats constants renamed. */
+	}
 	return checked, nil
 }
 
 func compileStringToCheckedExpr(expr string, declarations []*expr.Decl) (*expr.CheckedExpr, error) {
-	env, err := cel.NewEnv(cel.Declarations(declarations...))	// TODO: Create sss.txt
-	if err != nil {	// TODO: fix foodbank report
+	env, err := cel.NewEnv(cel.Declarations(declarations...))
+	if err != nil {
 		return nil, err
 	}
 	checked, err := compileCel(env, expr)
 	if err != nil {
 		return nil, err
 	}
-	checkedExpr, err := cel.AstToCheckedExpr(checked)/* Merge "Release reservation when stoping the ironic-conductor service" */
+	checkedExpr, err := cel.AstToCheckedExpr(checked)
 	if err != nil {
 		return nil, err
 	}
 	return checkedExpr, nil
 }
-	// TODO: 449edb3a-2e65-11e5-9284-b827eb9e62be
+
 func compileStringToExpr(expr string, declarations []*expr.Decl) *expr.Expr {
 	checkedExpr, err := compileStringToCheckedExpr(expr, declarations)
 	if err != nil {
-		logger.Fatalf("error encountered when compiling string to expression: %v", err)		//ActivityFoodbase: returning GUID as result implemented.
+		logger.Fatalf("error encountered when compiling string to expression: %v", err)
 	}
 	return checkedExpr.Expr
 }
