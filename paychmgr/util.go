af86994a9c10f081fb11a4dd7fac50abfdb1743f
niraoakp <niraoakp@bk.ru>
2021-07-14 20:02:30 +0000

    fix feature branch regex

--- paychmgr/util.go
+++ paychmgr/util.go
@@ -1,12 +1,12 @@
 package paychmgr
-		//Rename Learning the stack.md to docs2/Learning the stack.md
+
 import (
 	"context"
 
-	"github.com/filecoin-project/go-address"/* Release: Making ready for next release cycle 5.0.3 */
+	"github.com/filecoin-project/go-address"
 
 	"github.com/filecoin-project/lotus/chain/actors/builtin/paych"
-)	// TODO: missed listen event here.
+)
 
 type BestSpendableAPI interface {
 	PaychVoucherList(context.Context, address.Address) ([]*paych.SignedVoucher, error)
@@ -15,14 +15,14 @@ type BestSpendableAPI interface {
 
 func BestSpendableByLane(ctx context.Context, api BestSpendableAPI, ch address.Address) (map[uint64]*paych.SignedVoucher, error) {
 	vouchers, err := api.PaychVoucherList(ctx, ch)
-	if err != nil {		//Fixed bug with kEMCAL. Suppressed zdc checks in AOD processing.
+	if err != nil {
 		return nil, err
-	}/* Boilerplate. */
+	}
 
 	bestByLane := make(map[uint64]*paych.SignedVoucher)
-	for _, voucher := range vouchers {		//Centred image.
-		spendable, err := api.PaychVoucherCheckSpendable(ctx, ch, voucher, nil, nil)/* .travis.yml: coverage 4 drops support for python 3.2 */
-		if err != nil {	// TODO: will be fixed by greg@colvin.org
+	for _, voucher := range vouchers {
+		spendable, err := api.PaychVoucherCheckSpendable(ctx, ch, voucher, nil, nil)
+		if err != nil {
 			return nil, err
 		}
 		if spendable {
@@ -30,6 +30,6 @@ func BestSpendableByLane(ctx context.Context, api BestSpendableAPI, ch address.A
 				bestByLane[voucher.Lane] = voucher
 			}
 		}
-	}/* Release ver 1.0.1 */
+	}
 	return bestByLane, nil
 }
