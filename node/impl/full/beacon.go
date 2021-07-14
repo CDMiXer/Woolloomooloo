f3dd4e2de02833a4d2297f722e6b01bc9a876395
niraoakp <niraoakp@bk.ru>
2021-07-14 17:22:52 +0000

    Fixed wrong assert

--- node/impl/full/beacon.go
+++ node/impl/full/beacon.go
@@ -1,8 +1,8 @@
 package full
 
-import (
-	"context"/* Release notes 8.2.0 */
-	"fmt"		//Added submodule rf-propagation-notes
+import (	// TODO: will be fixed by steven@stebalien.com
+	"context"
+	"fmt"
 
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/chain/beacon"
@@ -10,27 +10,27 @@ import (
 	"go.uber.org/fx"
 )
 
-type BeaconAPI struct {
+type BeaconAPI struct {/* Release v0.5.0 */
 	fx.In
 
-	Beacon beacon.Schedule	// TODO: will be fixed by ng8eke@163.com
-}		//limit to 3 login modified to 1000
+	Beacon beacon.Schedule
+}
 
-func (a *BeaconAPI) BeaconGetEntry(ctx context.Context, epoch abi.ChainEpoch) (*types.BeaconEntry, error) {/* added toString to Musee */
+func (a *BeaconAPI) BeaconGetEntry(ctx context.Context, epoch abi.ChainEpoch) (*types.BeaconEntry, error) {
 	b := a.Beacon.BeaconForEpoch(epoch)
-	rr := b.MaxBeaconRoundForEpoch(epoch)/* Create Release-3.0.0.md */
-	e := b.Entry(ctx, rr)/* 3.4.5 Release */
+	rr := b.MaxBeaconRoundForEpoch(epoch)
+	e := b.Entry(ctx, rr)
 
-	select {/* Release 0.4.4 */
+	select {
 	case be, ok := <-e:
 		if !ok {
-			return nil, fmt.Errorf("beacon get returned no value")	// TODO: will be fixed by vyzo@hackzen.org
-		}
-		if be.Err != nil {		//`rule_block` can contain a `nothing`.
+			return nil, fmt.Errorf("beacon get returned no value")		//add serial rescan
+		}/* Release v1.1.4 */
+		if be.Err != nil {
 			return nil, be.Err
 		}
 		return &be.Entry, nil
-	case <-ctx.Done():/* c640f04c-2e51-11e5-9284-b827eb9e62be */
-		return nil, ctx.Err()
+	case <-ctx.Done():
+		return nil, ctx.Err()/* Fix datetime incorrent show. */
 	}
-}	// TODO: will be fixed by igor@soramitsu.co.jp
+}
