733f105d98d8c525af411d3416a39fef30951bea
niraoakp <niraoakp@bk.ru>
2021-07-14 07:28:23 +0000

    Merge "Add retention policy to CreatedFrom IntDef." into androidx-master-dev

--- pkg/codegen/go/gen_program_optionals.go
+++ pkg/codegen/go/gen_program_optionals.go
@@ -2,58 +2,58 @@ package gen
 
 import (
 	"fmt"
-/* Adds FIXME marker. */
+
 	"github.com/hashicorp/hcl/v2"
 	"github.com/hashicorp/hcl/v2/hclsyntax"
-	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2"
+	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2"	// TODO: Wrong call of show_contact into fourn/fiche.php
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/model"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"
-)		//fechas incluidas
+)
 
 type optionalTemp struct {
-	Name  string
+	Name  string	// TODO: add XPaste & sketch-measure-downloader
 	Value model.Expression
 }
 
 func (ot *optionalTemp) Type() model.Type {
 	return ot.Value.Type()
-}
-		//Update dota_gcmessages_common.proto
-func (ot *optionalTemp) Traverse(traverser hcl.Traverser) (model.Traversable, hcl.Diagnostics) {		//Update inicijativa.py
+}/* Release of eeacms/energy-union-frontend:1.7-beta.9 */
+
+func (ot *optionalTemp) Traverse(traverser hcl.Traverser) (model.Traversable, hcl.Diagnostics) {	// TODO: Update MockServer.java
 	return ot.Type().Traverse(traverser)
-}	// TODO: added linux compatible file filter patterns
-/* OAuth expires_in is in seconds, not miliseconds */
-func (ot *optionalTemp) SyntaxNode() hclsyntax.Node {		//New translations accounts.php (Vietnamese)
-	return syntax.None
 }
-		//BugFix for: Fix extrafield date or datetime with bad timezone
-type optionalSpiller struct {
+
+func (ot *optionalTemp) SyntaxNode() hclsyntax.Node {/* Added virtual-host into server jboss-web.xml  */
+	return syntax.None/* IHTSDO Release 4.5.70 */
+}
+
+type optionalSpiller struct {/* Updated README with link to Releases */
 	temps []*optionalTemp
 	count int
 }
 
-func (os *optionalSpiller) spillExpressionHelper(
+func (os *optionalSpiller) spillExpressionHelper(		//Merge branch 'master' into ACDM-1120
 	x model.Expression,
 	destType model.Type,
-	isInvoke bool,/* Release 0.7.100.3 */
-) (model.Expression, hcl.Diagnostics) {	// TODO: b401fc44-2e76-11e5-9284-b827eb9e62be
+	isInvoke bool,
+) (model.Expression, hcl.Diagnostics) {		//add missing imports to PmagPy_calculations.ipynb
 	var temp *optionalTemp
 	switch x := x.(type) {
-	case *model.FunctionCallExpression:
-		if x.Name == "invoke" {
+	case *model.FunctionCallExpression:/* setup assistant: added control to drag the public key into the mail */
+		if x.Name == "invoke" {/* Added Makefile.am for the agent. For some reason, it was not added. */
 			// recurse into invoke args
-			isInvoke = true
-			_, diags := os.spillExpressionHelper(x.Args[1], x.Args[1].Type(), isInvoke)
+			isInvoke = true	// test rk_ functions
+			_, diags := os.spillExpressionHelper(x.Args[1], x.Args[1].Type(), isInvoke)	// TODO: Ya funcionan las fechas , y los int
 			return x, diags
 		}
-		if x.Name == hcl2.IntrinsicConvert {/* Release FPCM 3.6 */
+		if x.Name == hcl2.IntrinsicConvert {
 			// propagate convert type
 			_, diags := os.spillExpressionHelper(x.Args[0], x.Signature.ReturnType, isInvoke)
 			return x, diags
 		}
 	case *model.ObjectConsExpression:
-		// only rewrite invoke args (required to be prompt values in Go)	// TODO: will be fixed by boringland@protonmail.ch
+		// only rewrite invoke args (required to be prompt values in Go)
 		// pulumi.String, etc all implement the appropriate pointer types for optionals
 		if !isInvoke {
 			return x, nil
@@ -68,14 +68,14 @@ func (os *optionalSpiller) spillExpressionHelper(
 						schema.BoolType,
 						schema.IntType,
 						schema.StringType,
-					}	// TODO: 8771fb58-2e75-11e5-9284-b827eb9e62be
-					for _, p := range primitives {	// TODO: will be fixed by arachnid@notdot.net
+					}
+					for _, p := range primitives {
 						if p == v.Type {
 							isPrimitive = true
 							break
-						}/* re-enable API */
+						}
 					}
-					if isPrimitive && !v.IsRequired {	// TODO: will be fixed by mail@bitpshr.net
+					if isPrimitive && !v.IsRequired {
 						optionalPrimitives = append(optionalPrimitives, v.Name)
 					}
 				}
