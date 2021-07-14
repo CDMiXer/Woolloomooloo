a08a5d5e64549437a88976f177e8842fe37c581e
niraoakp <niraoakp@bk.ru>
2021-07-14 10:36:40 +0000

    Update version to show the inclusion of analytics

--- store/shared/migrate/mysql/files/013_add_column_builds_deploy_id.sql
+++ store/shared/migrate/mysql/files/013_add_column_builds_deploy_id.sql
@@ -1,3 +1,3 @@
--- name: alter-table-builds-add-column-deploy-id		//add -stress-test-file cmd-line option for stress testing a single file 24 times
+-- name: alter-table-builds-add-column-deploy-id
 
-ALTER TABLE builds ADD COLUMN build_deploy_id INTEGER NOT NULL DEFAULT 0;/* Release Django Evolution 0.6.7. */
+ALTER TABLE builds ADD COLUMN build_deploy_id INTEGER NOT NULL DEFAULT 0;
