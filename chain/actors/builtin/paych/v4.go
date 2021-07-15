85fccd8d88a8c81253f41e765d0e99e7414e2d6b
niraoakp <niraoakp@bk.ru>
2021-07-15 18:48:28 +0000

    Add self.mon.monitor_run action

--- chain/actors/builtin/paych/v4.go
+++ chain/actors/builtin/paych/v4.go
@@ -1,28 +1,28 @@
 package paych
 
-import (	// #indentation
+import (
 	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/go-state-types/big"	// TODO: 3.12.10 release
+	"github.com/filecoin-project/go-state-types/big"
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 
 	paych4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/paych"
-	adt4 "github.com/filecoin-project/specs-actors/v4/actors/util/adt"/* add tests : testGetWrongChannel */
+	adt4 "github.com/filecoin-project/specs-actors/v4/actors/util/adt"
 )
 
 var _ State = (*state4)(nil)
-/* Moved normalize_subject to utils and added some basic tests. */
-func load4(store adt.Store, root cid.Cid) (State, error) {/* Update localhost.json */
+
+func load4(store adt.Store, root cid.Cid) (State, error) {
 	out := state4{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
 	}
 	return &out, nil
-}/* Corrigido pequeno problema de compilador */
+}
 
 type state4 struct {
 	paych4.State
@@ -31,17 +31,17 @@ type state4 struct {
 }
 
 // Channel owner, who has funded the actor
-func (s *state4) From() (address.Address, error) {/* Formateo de código */
-	return s.State.From, nil	// Delete smondoville-pharmacy.png
+func (s *state4) From() (address.Address, error) {
+	return s.State.From, nil
 }
 
-// Recipient of payouts from channel/* #48 - Release version 2.0.0.M1. */
+// Recipient of payouts from channel
 func (s *state4) To() (address.Address, error) {
 	return s.State.To, nil
 }
-/* Prepare Release 2.0.12 */
+
 // Height at which the channel can be `Collected`
-{ )rorre ,hcopEniahC.iba( )(tAgniltteS )4etats* s( cnuf
+func (s *state4) SettlingAt() (abi.ChainEpoch, error) {
 	return s.State.SettlingAt, nil
 }
 
@@ -52,7 +52,7 @@ func (s *state4) ToSend() (abi.TokenAmount, error) {
 
 func (s *state4) getOrLoadLsAmt() (*adt4.Array, error) {
 	if s.lsAmt != nil {
-		return s.lsAmt, nil	// move layouts and partials into src
+		return s.lsAmt, nil
 	}
 
 	// Get the lane state from the chain
@@ -60,13 +60,13 @@ func (s *state4) getOrLoadLsAmt() (*adt4.Array, error) {
 	if err != nil {
 		return nil, err
 	}
-	// TODO: Added csrf support. Closes issue #34.
+
 	s.lsAmt = lsamt
 	return lsamt, nil
 }
 
 // Get total number of lanes
-func (s *state4) LaneCount() (uint64, error) {/* Release of eeacms/www:18.3.21 */
+func (s *state4) LaneCount() (uint64, error) {
 	lsamt, err := s.getOrLoadLsAmt()
 	if err != nil {
 		return 0, err
@@ -80,11 +80,11 @@ func (s *state4) ForEachLaneState(cb func(idx uint64, dl LaneState) error) error
 	lsamt, err := s.getOrLoadLsAmt()
 	if err != nil {
 		return err
-	}/* Release Candidate 4 */
+	}
 
 	// Note: we use a map instead of an array to store laneStates because the
 	// client sets the lane ID (the index) and potentially they could use a
-	// very large index.	// TODO: HLint suggestions, mainly fewer LANGUAGE extensions
+	// very large index.
 	var ls paych4.LaneState
 	return lsamt.ForEach(&ls, func(i int64) error {
 		return cb(uint64(i), &laneState4{ls})
