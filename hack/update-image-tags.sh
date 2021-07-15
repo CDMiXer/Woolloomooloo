4f0282eb3c61b932d66be219a512ef69ee91b685
niraoakp <niraoakp@bk.ru>
2021-07-15 20:21:11 +0000

    - fix for variable name collision

--- hack/update-image-tags.sh
+++ hack/update-image-tags.sh
@@ -3,7 +3,7 @@ set -eu -o pipefail
 
 dir=$1
 image_tag=$2
-	// TODO: will be fixed by souzau@yandex.com
+
 find "$dir" -type f -name '*.yaml' | while read -r f ; do
   sed "s|argoproj/\(.*\):.*|argoproj/\1:${image_tag}|" "$f" > .tmp
   mv .tmp "$f"
