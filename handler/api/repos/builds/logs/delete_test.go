bb0325798927e213c5304399a8a0c31821d867c9
niraoakp <niraoakp@bk.ru>
2021-07-15 23:23:00 +0000

    Update tmm_2.9.8_6644bb9

--- handler/api/repos/builds/logs/delete_test.go
+++ handler/api/repos/builds/logs/delete_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// Use of this source code is governed by the Drone Non-Commercial License	// changed unknown char to _
+// that can be found in the LICENSE file./* Release version 2.0.10 and bump version to 2.0.11 */
 
 package logs
