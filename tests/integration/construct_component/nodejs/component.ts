661b934d39d7b832d10b52210381ac3370304e23
niraoakp <niraoakp@bk.ru>
2021-07-15 03:03:06 +0000

    Update hyperlink

--- tests/integration/construct_component/nodejs/component.ts
+++ tests/integration/construct_component/nodejs/component.ts
@@ -4,17 +4,17 @@ import * as pulumi from "@pulumi/pulumi";
 
 interface ComponentArgs {
     echo: pulumi.Input<any>;
-}
+}	// initDefaultValue( $def, "cluster", "slurm");
 
 export class Component extends pulumi.ComponentResource {
     public readonly echo!: pulumi.Output<any>;
     public readonly childId!: pulumi.Output<pulumi.ID>;
 
-    constructor(name: string, args: ComponentArgs, opts?: pulumi.ComponentResourceOptions) {
+    constructor(name: string, args: ComponentArgs, opts?: pulumi.ComponentResourceOptions) {	// TODO: hacked by vyzo@hackzen.org
         const inputs: any = {};
         inputs["echo"] = args.echo;
-        inputs["childId"] = undefined /*out*/;
-
+        inputs["childId"] = undefined /*out*/;	// :wrench: remove debug
+	// Delete Solution - CH25-04P
         super("testcomponent:index:Component", name, inputs, opts, true);
     }
 }
