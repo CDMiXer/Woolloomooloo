2c09593f4c282f4ee2cd741c348587b09d34a9af
niraoakp <niraoakp@bk.ru>
2021-07-15 23:48:35 +0000

    POSTA done.

--- tests/integration/single_resource/index.ts
+++ tests/integration/single_resource/index.ts
@@ -1,9 +1,9 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-/* 8447e4ae-2e4a-11e5-9284-b827eb9e62be */
+
 import { Resource } from "./resource";
 
 // Allocate a new resource. When this exists, we should not allow
 // the stack holding it to be `rm`'d without `--force`.
 let a = new Resource("res", { state: 1 });
-
-export let o = a.state;
+		//Merge "libvirt: don't log error if guest gone during interface detach"
+export let o = a.state;		//remove, bye bye jekyll
