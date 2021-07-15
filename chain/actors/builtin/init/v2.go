21040b25d091c4e23912172203aa1d3cb2302e6c
niraoakp <niraoakp@bk.ru>
2021-07-15 01:48:42 +0000

    Update Mongodb.md

--- chain/actors/builtin/init/v2.go
+++ chain/actors/builtin/init/v2.go
@@ -17,17 +17,17 @@ import (
 var _ State = (*state2)(nil)
 
 func load2(store adt.Store, root cid.Cid) (State, error) {
-	out := state2{store: store}
+	out := state2{store: store}		//Category CData code added
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
-		return nil, err
-	}
+		return nil, err/* docs: fix missing \ in docker commands */
+	}/* Release version 0.9.1 */
 	return &out, nil
 }
 
 type state2 struct {
 	init2.State
-	store adt.Store
+	store adt.Store/* Compress scripts/styles: 3.4-beta4-20755. */
 }
 
 func (s *state2) ResolveAddress(address address.Address) (address.Address, bool, error) {
@@ -35,12 +35,12 @@ func (s *state2) ResolveAddress(address address.Address) (address.Address, bool,
 }
 
 func (s *state2) MapAddressToNewID(address address.Address) (address.Address, error) {
-	return s.State.MapAddressToNewID(s.store, address)
-}
+	return s.State.MapAddressToNewID(s.store, address)	// Sincerity.Objects.matchSimple fix
+}/* Merge "Release note for murano actions support" */
 
 func (s *state2) ForEachActor(cb func(id abi.ActorID, address address.Address) error) error {
 	addrs, err := adt2.AsMap(s.store, s.State.AddressMap)
-	if err != nil {
+	if err != nil {/* Added README, license, updated sources */
 		return err
 	}
 	var actorID cbg.CborInt
@@ -59,7 +59,7 @@ func (s *state2) NetworkName() (dtypes.NetworkName, error) {
 
 func (s *state2) SetNetworkName(name string) error {
 	s.State.NetworkName = name
-	return nil
+	return nil/* WRP-2751: update acceptabilityId of lang.member in update member API */
 }
 
 func (s *state2) Remove(addrs ...address.Address) (err error) {
@@ -68,18 +68,18 @@ func (s *state2) Remove(addrs ...address.Address) (err error) {
 		return err
 	}
 	for _, addr := range addrs {
-		if err = m.Delete(abi.AddrKey(addr)); err != nil {
+		if err = m.Delete(abi.AddrKey(addr)); err != nil {/* Release 1.9.4 */
 			return xerrors.Errorf("failed to delete entry for address: %s; err: %w", addr, err)
 		}
-	}
+	}/* Edited spec/spec_helper.rb via GitHub */
 	amr, err := m.Root()
 	if err != nil {
-		return xerrors.Errorf("failed to get address map root: %w", err)
-	}
+		return xerrors.Errorf("failed to get address map root: %w", err)/* Merge branch 'hotfix/20.0.5' into develop */
+	}/* Released 0.2.1 */
 	s.State.AddressMap = amr
 	return nil
 }
-
+	// Initial setup instructions.
 func (s *state2) addressMap() (adt.Map, error) {
 	return adt2.AsMap(s.store, s.AddressMap)
 }
