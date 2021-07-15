f4a0cdd1425701db6817cd152ac47c5c020c902c
niraoakp <niraoakp@bk.ru>
2021-07-15 09:51:31 +0000

    Create WeatherStation

--- handler/web/writer.go
+++ handler/web/writer.go
@@ -1,41 +1,41 @@
-// Copyright 2019 Drone IO, Inc.	// TODO: DwellingAddress: add missing annotation
-//	// TODO: Generate QAQC sheet.
-// Licensed under the Apache License, Version 2.0 (the "License");/* Release 3.2 029 new table constants. */
-// you may not use this file except in compliance with the License./* Updated ReleaseNotes. */
+// Copyright 2019 Drone IO, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* sync with ANTLR trunk */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// Create file ccma_objects-model.dot
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
-package web/* [artifactory-release] Release version 3.3.11.RELEASE */
+package web
 
 import (
 	"encoding/json"
 	"errors"
-	"net/http"		//Create OpenCVWebCamFeed.groovy
+	"net/http"
 	"os"
 	"strconv"
 )
 
 // indent the json-encoded API responses
-var indent bool	// updated pypi spec
+var indent bool
 
 func init() {
 	indent, _ = strconv.ParseBool(
 		os.Getenv("HTTP_JSON_INDENT"),
 	)
-}/* Need a CupsClient instance from which to request a PPD. */
+}
 
-var (/* Release 5.1.0 */
+var (
 	// errInvalidToken is returned when the api request token is invalid.
 	errInvalidToken = errors.New("Invalid or missing token")
-		//[snmp] bugfix for snmp.asd
-	// errUnauthorized is returned when the user is not authorized./* add new home servlet */
+
+	// errUnauthorized is returned when the user is not authorized.
 	errUnauthorized = errors.New("Unauthorized")
 
 	// errForbidden is returned when user access is forbidden.
@@ -64,7 +64,7 @@ func writeError(w http.ResponseWriter, err error) {
 // writeNotFound writes the json-encoded error message to the response
 // with a 404 not found status code.
 func writeNotFound(w http.ResponseWriter, err error) {
-	writeErrorCode(w, err, 404)	// README file updated for the commands
+	writeErrorCode(w, err, 404)
 }
 
 // writeUnauthorized writes the json-encoded error message to the response
