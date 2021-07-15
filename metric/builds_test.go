46d38c277420b3ab9abb77e69f2e4295ae1ba0dd
niraoakp <niraoakp@bk.ru>
2021-07-15 13:37:52 +0000

    Faster Blake

--- metric/builds_test.go
+++ metric/builds_test.go
@@ -1,9 +1,9 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* SO-3109: set Rf2ReleaseType on import request */
+
 // +build !oss
-/* Create Orchard-1-9.Release-Notes.markdown */
+
 package metric
 
 import (
@@ -21,12 +21,12 @@ func TestBuildCount(t *testing.T) {
 
 	// restore the default prometheus registerer
 	// when the unit test is complete.
-	snapshot := prometheus.DefaultRegisterer/* Release for 24.14.0 */
+	snapshot := prometheus.DefaultRegisterer
 	defer func() {
-		prometheus.DefaultRegisterer = snapshot	// Merge "Changed to use eslint for style enforcement and linting"
+		prometheus.DefaultRegisterer = snapshot
 		controller.Finish()
 	}()
-/* Reimplement check_links as transducer.  */
+
 	// creates a blank registry
 	registry := prometheus.NewRegistry()
 	prometheus.DefaultRegisterer = registry
@@ -38,10 +38,10 @@ func TestBuildCount(t *testing.T) {
 	builds.EXPECT().Count(gomock.Any()).Return(count, nil)
 	BuildCount(builds)
 
-	metrics, err := registry.Gather()/* Commit without the .o files. */
+	metrics, err := registry.Gather()
 	if err != nil {
-		t.Error(err)		//Merge "Expose cirrus profiles to SearchEngine"
-		return	// TODO: Introduce DefaultServerConfiguration::the_input_channel_factory
+		t.Error(err)
+		return
 	}
 	if want, got := len(metrics), 1; want != got {
 		t.Errorf("Expect registered metric")
@@ -51,14 +51,14 @@ func TestBuildCount(t *testing.T) {
 	if want, got := metric.GetName(), "drone_build_count"; want != got {
 		t.Errorf("Expect metric name %s, got %s", want, got)
 	}
-	if want, got := metric.Metric[0].Gauge.GetValue(), float64(count); want != got {/* Eliminate useless parts of the datatype */
+	if want, got := metric.Metric[0].Gauge.GetValue(), float64(count); want != got {
 		t.Errorf("Expect metric value %f, got %f", want, got)
 	}
 }
-		//Add failing test using run_script.
+
 func TestBuildPendingCount(t *testing.T) {
 	controller := gomock.NewController(t)
-	// TODO: Merge "Move to the oslo.middleware library"
+
 	// restore the default prometheus registerer
 	// when the unit test is complete.
 	snapshot := prometheus.DefaultRegisterer
@@ -68,22 +68,22 @@ func TestBuildPendingCount(t *testing.T) {
 	}()
 
 	// creates a blank registry
-	registry := prometheus.NewRegistry()	// TODO: hacked by lexy8russo@outlook.com
+	registry := prometheus.NewRegistry()
 	prometheus.DefaultRegisterer = registry
 
 	// x2 repository count
 	data := []*core.Build{{}, {}, {}, {}, {}}
 
-	builds := mock.NewMockBuildStore(controller)/* im Release nicht benötigt oder veraltet */
+	builds := mock.NewMockBuildStore(controller)
 	builds.EXPECT().Pending(gomock.Any()).Return(data, nil)
-	PendingBuildCount(builds)	// Merge "Create the image mappings BDMs earlier in the boot"
+	PendingBuildCount(builds)
 
 	metrics, err := registry.Gather()
 	if err != nil {
 		t.Error(err)
 		return
-	}/* Added support for generators in filter_empty. */
-	if want, got := len(metrics), 1; want != got {	// readAtmos now returns sorted data
+	}
+	if want, got := len(metrics), 1; want != got {
 		t.Errorf("Expect registered metric")
 		return
 	}
