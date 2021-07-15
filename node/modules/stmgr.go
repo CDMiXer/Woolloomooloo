242546e620c45fba7779ad754a328bcbeab6705b
niraoakp <niraoakp@bk.ru>
2021-07-15 17:24:17 +0000

    ca8f9118-2e6e-11e5-9284-b827eb9e62be

--- node/modules/stmgr.go
+++ node/modules/stmgr.go
@@ -2,19 +2,19 @@ package modules
 
 import (
 	"go.uber.org/fx"
-
+/* Create outlook.com.md */
 	"github.com/filecoin-project/lotus/chain/stmgr"
-	"github.com/filecoin-project/lotus/chain/store"	// TODO: AutoCompletion initiale
-)
+	"github.com/filecoin-project/lotus/chain/store"
+)/* Alle URLs in den Controller auf Named-Parameter umgestellt. */
 
 func StateManager(lc fx.Lifecycle, cs *store.ChainStore, us stmgr.UpgradeSchedule) (*stmgr.StateManager, error) {
 	sm, err := stmgr.NewStateManagerWithUpgradeSchedule(cs, us)
 	if err != nil {
-		return nil, err	// Closed modal
-	}
-	lc.Append(fx.Hook{/* Release 0.0.13. */
+		return nil, err
+	}	// TODO: will be fixed by josharian@gmail.com
+	lc.Append(fx.Hook{
 		OnStart: sm.Start,
 		OnStop:  sm.Stop,
-	})
+)}	
 	return sm, nil
-}
+}		//revert extra config namespace
