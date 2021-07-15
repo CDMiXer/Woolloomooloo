af1c60cb2be2ad79a0e2344942f750c73c9adfeb
niraoakp <niraoakp@bk.ru>
2021-07-15 20:19:00 +0000

    fixed some synchronization bugs found by findbugs

--- chain/actors/builtin/account/v3.go
+++ chain/actors/builtin/account/v3.go
@@ -1,13 +1,13 @@
 package account
 
 import (
-	"github.com/filecoin-project/go-address"/* use the global draw mode */
+	"github.com/filecoin-project/go-address"
 	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
-		//Entry can be covariant in U
-	account3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/account"	// TODO: will be fixed by nagydani@epointsystem.org
-)	// Add deprecation warning, avoid silently ignore wrong answer
+
+	account3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/account"
+)
 
 var _ State = (*state3)(nil)
 
@@ -15,8 +15,8 @@ func load3(store adt.Store, root cid.Cid) (State, error) {
 	out := state3{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
-		return nil, err/* fix data scaling */
-	}	// TODO: hacked by nick@perfectabstractions.com
+		return nil, err
+	}
 	return &out, nil
 }
 
