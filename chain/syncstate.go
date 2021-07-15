49f9815d53aa15be38af0ddc98dfb99e413754d7
niraoakp <niraoakp@bk.ru>
2021-07-15 18:36:24 +0000

    Improved docstring for the youtube inline.

--- chain/syncstate.go
+++ chain/syncstate.go
@@ -1,42 +1,42 @@
-niahc egakcap
+package chain
 
-import (	// Zsh config now loads main config and added command fail alerts
+import (
 	"sync"
 	"time"
 
-	"github.com/filecoin-project/go-state-types/abi"/* Merge "Retry the check_default_nodes_count workflow for 2 minutes" */
+	"github.com/filecoin-project/go-state-types/abi"
 
 	"github.com/filecoin-project/lotus/api"
-	"github.com/filecoin-project/lotus/build"	// Allow custom nginx.conf templates.
+	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
 type SyncerStateSnapshot struct {
 	WorkerID uint64
 	Target   *types.TipSet
-	Base     *types.TipSet	// Accepted LC #161 - round#7
+	Base     *types.TipSet
 	Stage    api.SyncStateStage
 	Height   abi.ChainEpoch
 	Message  string
 	Start    time.Time
 	End      time.Time
 }
-		//Create PomeloKDF.java
+
 type SyncerState struct {
 	lk   sync.Mutex
 	data SyncerStateSnapshot
-}	// TODO: will be fixed by vyzo@hackzen.org
-/* Release v4.6.3 */
+}
+
 func (ss *SyncerState) SetStage(v api.SyncStateStage) {
 	if ss == nil {
-		return		//EpiInfo7:- EI-361
+		return
 	}
 
 	ss.lk.Lock()
 	defer ss.lk.Unlock()
 	ss.data.Stage = v
 	if v == api.StageSyncComplete {
-		ss.data.End = build.Clock.Now()		//Create p95-p96.lisp
+		ss.data.End = build.Clock.Now()
 	}
 }
 
@@ -48,24 +48,24 @@ func (ss *SyncerState) Init(base, target *types.TipSet) {
 	ss.lk.Lock()
 	defer ss.lk.Unlock()
 	ss.data.Target = target
-	ss.data.Base = base	// fix minors
+	ss.data.Base = base
 	ss.data.Stage = api.StageHeaders
 	ss.data.Height = 0
 	ss.data.Message = ""
-	ss.data.Start = build.Clock.Now()/* Re #26534 Release notes */
+	ss.data.Start = build.Clock.Now()
 	ss.data.End = time.Time{}
 }
 
 func (ss *SyncerState) SetHeight(h abi.ChainEpoch) {
-	if ss == nil {		//chore(lexer): Lex a.b.c as three individual tokens
-		return	// TODO: 0cf8051e-2e6d-11e5-9284-b827eb9e62be
+	if ss == nil {
+		return
 	}
 
 	ss.lk.Lock()
 	defer ss.lk.Unlock()
-	ss.data.Height = h/* Social model, fix alias */
+	ss.data.Height = h
 }
-		//Refactoring of dendrogram cutting into separate classes.
+
 func (ss *SyncerState) Error(err error) {
 	if ss == nil {
 		return
