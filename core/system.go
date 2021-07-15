f966151fe6d7636998bf2cdea972b073009eaec9
niraoakp <niraoakp@bk.ru>
2021-07-15 19:53:01 +0000

    Upgraded all gems. Being courageous :-)

--- core/system.go
+++ core/system.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// Licensed under the Apache License, Version 2.0 (the "License");/* Create snippet-complete-media-commentary.html */
+// you may not use this file except in compliance with the License./* Release 1.3.5 */
+// You may obtain a copy of the License at/* @Release [io7m-jcanephora-0.9.23] */
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and	// TODO: Add tests for executable flags
 // limitations under the License.
 
 package core
@@ -19,5 +19,5 @@ type System struct {
 	Proto   string `json:"proto,omitempty"`
 	Host    string `json:"host,omitempty"`
 	Link    string `json:"link,omitempty"`
-	Version string `json:"version,omitempty"`
+	Version string `json:"version,omitempty"`/* Release v1.0.8. */
 }
