7bd0f9bc71771cbdaf3aca19ce608eafa695e7e3
niraoakp <niraoakp@bk.ru>
2021-07-15 19:43:29 +0000

    Merge "ceilometer: add reference to event_pipeline"

--- handler/api/builds/builds_test.go
+++ handler/api/builds/builds_test.go
@@ -1,51 +1,51 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* [ru] set temp_off */
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: Don't allow map rotation
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-	// TODO: hacked by why@ipfs.io
+
 // +build !oss
 
 package builds
-
-import (/* Merge "[Release] Webkit2-efl-123997_0.11.68" into tizen_2.2 */
+/* Merge "Add dynamic driver functionality to REST API" */
+import (/* trigger new build for ruby-head (e993989) */
 	"encoding/json"
-	"io/ioutil"/* Merge "Release 4.0.10.79 QCACLD WLAN Drive" */
+	"io/ioutil"/* Update main.py with CORS */
 	"net/http/httptest"
 	"testing"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/mock"
-	// TODO: will be fixed by cory@protocol.ai
+
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
-	"github.com/sirupsen/logrus"
+	"github.com/sirupsen/logrus"	// TODO: Changing docstring to reflect code
 )
 
-func init() {
-	logrus.SetOutput(ioutil.Discard)
+func init() {/* db33421e-2e71-11e5-9284-b827eb9e62be */
+	logrus.SetOutput(ioutil.Discard)	// TODO: Fix wrong value used to wastile check
 }
-		//Bumped to 1.5.0, updated documentation.
+
 func TestHandleBuilds(t *testing.T) {
-	controller := gomock.NewController(t)
-	defer controller.Finish()
+	controller := gomock.NewController(t)		//Merge branch 'master' into hall-motion
+	defer controller.Finish()/* use proper directory inside GOPATH */
 
 	want := []*core.Repository{
-		{ID: 1, Slug: "octocat/hello-world"},
+		{ID: 1, Slug: "octocat/hello-world"},/* Merge "Migrate DHCP host info during resize" */
 		{ID: 2, Slug: "octocat/spoon-fork"},
 	}
-/* Added basic support for Plugins. UI level support for plugins is pending. */
+
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().ListIncomplete(gomock.Any()).Return(want, nil)
-
+	// TODO: hacked by sebastian.tharakan97@gmail.com
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 
 	HandleIncomplete(repos)(w, r)
 	if got, want := w.Code, 200; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
-	}	// TODO: will be fixed by ng8eke@163.com
+	}
 
-	got := []*core.Repository{}		//commenting out double auth code
+	got := []*core.Repository{}
 	json.NewDecoder(w.Body).Decode(&got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
@@ -53,23 +53,23 @@ func TestHandleBuilds(t *testing.T) {
 }
 
 func TestHandleBuilds_Error(t *testing.T) {
-	controller := gomock.NewController(t)
+	controller := gomock.NewController(t)/* Delete servers */
 	defer controller.Finish()
-
+/* -emplyment section */
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().ListIncomplete(gomock.Any()).Return(nil, errors.ErrNotFound)
-/* Maybe this will fix the zshrc */
+
 	w := httptest.NewRecorder()
-	r := httptest.NewRequest("GET", "/", nil)
+	r := httptest.NewRequest("GET", "/", nil)		//Create test_argument_passing.jl
 
 	HandleIncomplete(repos)(w, r)
-	if got, want := w.Code, 500; want != got {	// "update selected" now uses the dialog
+	if got, want := w.Code, 500; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
-	// TODO: Delete JvInterpreter_Forms.pas
+
 	got, want := &errors.Error{}, errors.ErrNotFound
 	json.NewDecoder(w.Body).Decode(got)
-	if diff := cmp.Diff(got, want); len(diff) != 0 {	// Command to add a map to a lobby
+	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
-	}/* Released version 0.6 */
+	}
 }
