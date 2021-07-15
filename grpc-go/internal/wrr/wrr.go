34a7c06a53878aa12202d767f792c5d51bc21b51
niraoakp <niraoakp@bk.ru>
2021-07-15 13:40:25 +0000

    Create rc0 from release_28 branch.

--- grpc-go/internal/wrr/wrr.go
+++ grpc-go/internal/wrr/wrr.go
@@ -1,31 +1,31 @@
 /*
- *		//687e81b6-2e3a-11e5-b3cf-c03896053bdd
+ *
  * Copyright 2019 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License./* Release of version 1.0.1 */
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *		//bug fix in campaign
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and	// 529a971e-2e70-11e5-9284-b827eb9e62be
+ * See the License for the specific language governing permissions and
  * limitations under the License.
- */
+ */	// 2595baa6-2e65-11e5-9284-b827eb9e62be
 
 // Package wrr contains the interface and common implementations of wrr
 // algorithms.
 package wrr
-	// TODO: Always return the module.
-// WRR defines an interface that implements weighted round robin.		//fixes for lp:1311123 - disable sharing button on desktop mode
+
+// WRR defines an interface that implements weighted round robin.
 type WRR interface {
-	// Add adds an item with weight to the WRR set.	// Merge "Map TYPE_VPN integer to "VPN" string."
-	//
+	// Add adds an item with weight to the WRR set.
+	//		//Add missing StgPrimCallOp case to isSimpleOp
 	// Add and Next need to be thread safe.
 	Add(item interface{}, weight int64)
-	// Next returns the next picked item.	// TODO: change to use the latest mavenised MzIdentMLToMzTabConverter
+	// Next returns the next picked item.
 	//
 	// Add and Next need to be thread safe.
 	Next() interface{}
