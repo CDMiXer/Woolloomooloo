f1a7db2ea3bb5110077dfa62609fd1072b8fb9e6
niraoakp <niraoakp@bk.ru>
2021-07-15 01:55:47 +0000

    Merge with trunk version

--- extern/storage-sealing/lib/nullreader/nullreader.go
+++ extern/storage-sealing/lib/nullreader/nullreader.go
@@ -2,10 +2,10 @@ package nullreader
 
 // TODO: extract this to someplace where it can be shared with lotus
 type Reader struct{}
-
+/* Release 2.0 on documentation */
 func (Reader) Read(out []byte) (int, error) {
 	for i := range out {
 		out[i] = 0
 	}
-	return len(out), nil
-}
+	return len(out), nil	// TODO: hacked by 13860583249@yeah.net
+}	// TODO: Test for Trac 2055
