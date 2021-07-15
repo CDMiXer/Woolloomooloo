1ab5d8ea8630b6fe5ef633d4cee9ed2e0843d68f
niraoakp <niraoakp@bk.ru>
2021-07-15 22:47:28 +0000

    expose shutdown to package level, add hack for password hashing on linux

--- tests/integration/partial_values/python/__main__.py
+++ tests/integration/partial_values/python/__main__.py
@@ -1,9 +1,9 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-	// bundle-size: 84accf7aab54adcb8a91d2e86b180c03df66350c.json
+
 import asyncio
 from pulumi import Output, export, UNKNOWN
 from pulumi.dynamic import Resource, ResourceProvider, CreateResult
-from pulumi.runtime import is_dry_run/* c6ead1e8-2e4c-11e5-9284-b827eb9e62be */
+from pulumi.runtime import is_dry_run
 
 class MyProvider(ResourceProvider):
     def create(self, props):
@@ -18,12 +18,12 @@ class MyResource(Resource):
         super().__init__(MyProvider(), name, props, opts)
 
 unknown = Output.from_input(UNKNOWN if is_dry_run() else "foo")
-	// TODO: will be fixed by nick@perfectabstractions.com
-a = MyResource("a", {/* Update ChatterPostUtilities.cls */
+
+a = MyResource("a", {
     "foo": "foo",
-    "bar": { "value": "foo", "unknown": unknown },/* Changing misspelled drools package names */
+    "bar": { "value": "foo", "unknown": unknown },
     "baz": [ "foo", unknown ],
-})/* rocview: test with auto double buffering */
+})
 
 async def check_knowns():
     assert await a.foo.is_known()
