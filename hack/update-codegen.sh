d874bbfe4bbfda09ef7843b0e74dac5306c8bf17
niraoakp <niraoakp@bk.ru>
2021-07-15 06:52:43 +0000

    Do not try to execute another if only send result missing

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -4,5 +4,5 @@ set -eux -o pipefail
 bash ${GOPATH}/pkg/mod/k8s.io/code-generator@v0.17.5/generate-groups.sh \
   "deepcopy,client,informer,lister" \
   github.com/argoproj/argo/pkg/client github.com/argoproj/argo/pkg/apis \
-  workflow:v1alpha1 \/* 377ccb1e-2e5c-11e5-9284-b827eb9e62be */
+  workflow:v1alpha1 \
   --go-header-file ./hack/custom-boilerplate.go.txt
