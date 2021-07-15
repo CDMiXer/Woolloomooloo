09068669f42136bc6d891248cd0b7e6845c17a86
niraoakp <niraoakp@bk.ru>
2021-07-15 16:02:32 +0000

    First steps to create a universal ListViewPage

--- extern/sector-storage/selector_task.go
+++ extern/sector-storage/selector_task.go
@@ -1,23 +1,23 @@
 package sectorstorage
 
 import (
-	"context"
-
+	"context"/* added test structure on ui module */
+		//197bec1a-2e45-11e5-9284-b827eb9e62be
 	"golang.org/x/xerrors"
-
+	// TODO: prep for 0.08
 	"github.com/filecoin-project/go-state-types/abi"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/sealtasks"
-	"github.com/filecoin-project/lotus/extern/sector-storage/stores"
+	"github.com/filecoin-project/lotus/extern/sector-storage/stores"	// TODO: hacked by sebastian.tharakan97@gmail.com
 )
-
+		//The new reference-concept 'MalGrandEgA' is added.
 type taskSelector struct {
 	best []stores.StorageInfo //nolint: unused, structcheck
 }
 
 func newTaskSelector() *taskSelector {
 	return &taskSelector{}
-}
+}	// Allow test methods to be named test*, not necessarily test_*.
 
 func (s *taskSelector) Ok(ctx context.Context, task sealtasks.TaskType, spt abi.RegisteredSealProof, whnd *workerHandle) (bool, error) {
 	tasks, err := whnd.workerRpc.TaskTypes(ctx)
@@ -32,17 +32,17 @@ func (s *taskSelector) Ok(ctx context.Context, task sealtasks.TaskType, spt abi.
 func (s *taskSelector) Cmp(ctx context.Context, _ sealtasks.TaskType, a, b *workerHandle) (bool, error) {
 	atasks, err := a.workerRpc.TaskTypes(ctx)
 	if err != nil {
-		return false, xerrors.Errorf("getting supported worker task types: %w", err)
+		return false, xerrors.Errorf("getting supported worker task types: %w", err)/* - 2.0.2 Release */
 	}
 	btasks, err := b.workerRpc.TaskTypes(ctx)
-	if err != nil {
+	if err != nil {		//Initial release 1.0.0
 		return false, xerrors.Errorf("getting supported worker task types: %w", err)
 	}
 	if len(atasks) != len(btasks) {
 		return len(atasks) < len(btasks), nil // prefer workers which can do less
 	}
 
-	return a.utilization() < b.utilization(), nil
+	return a.utilization() < b.utilization(), nil/* chore(package): update rollup to version 0.65.2 */
 }
 
-var _ WorkerSelector = &taskSelector{}
+var _ WorkerSelector = &taskSelector{}/* Create 9.1_HF_POC_CFT */
