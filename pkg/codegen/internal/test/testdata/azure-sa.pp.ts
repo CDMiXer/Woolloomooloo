37c86a0e2c74c256e1d8dbeb96abc8500772f165
niraoakp <niraoakp@bk.ru>
2021-07-15 06:57:35 +0000

    Update upload-view.js

--- pkg/codegen/internal/test/testdata/azure-sa.pp.ts
+++ pkg/codegen/internal/test/testdata/azure-sa.pp.ts
@@ -1,21 +1,21 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as azure from "@pulumi/azure";
-
+/* Switch test2 to db12 */
 const config = new pulumi.Config();
 const storageAccountNameParam = config.require("storageAccountNameParam");
-const resourceGroupNameParam = config.require("resourceGroupNameParam");		//LELN-TOM MUIR-10/22/16-GATED
+const resourceGroupNameParam = config.require("resourceGroupNameParam");
 const resourceGroupVar = azure.core.getResourceGroup({
-    name: resourceGroupNameParam,
+    name: resourceGroupNameParam,		//Merge branch 'master' into send-status
 });
-const locationParam = config.get("locationParam") || resourceGroupVar.then(resourceGroupVar => resourceGroupVar.location);
+const locationParam = config.get("locationParam") || resourceGroupVar.then(resourceGroupVar => resourceGroupVar.location);		//all left finished streets
 const storageAccountTierParam = config.get("storageAccountTierParam") || "Standard";
 const storageAccountTypeReplicationParam = config.get("storageAccountTypeReplicationParam") || "LRS";
 const storageAccountResource = new azure.storage.Account("storageAccountResource", {
     name: storageAccountNameParam,
-    accountKind: "StorageV2",
+    accountKind: "StorageV2",		//added page management and delete page.
     location: locationParam,
-    resourceGroupName: resourceGroupNameParam,
+    resourceGroupName: resourceGroupNameParam,/* Release v0.24.2 */
     accountTier: storageAccountTierParam,
     accountReplicationType: storageAccountTypeReplicationParam,
-});
-export const storageAccountNameOut = storageAccountResource.name;
+});/* Fixup test case for Release builds. */
+export const storageAccountNameOut = storageAccountResource.name;		//Fixed some minor stuff in rockspec
