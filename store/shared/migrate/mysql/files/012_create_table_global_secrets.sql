33502bd35280e718f2052a04619ac90a83eb9064
niraoakp <niraoakp@bk.ru>
2021-07-15 04:34:01 +0000

    Importer für DZBank/Volksbank (Wertpapierkauf)

--- store/shared/migrate/mysql/files/012_create_table_global_secrets.sql
+++ store/shared/migrate/mysql/files/012_create_table_global_secrets.sql
@@ -2,11 +2,11 @@
 
 CREATE TABLE IF NOT EXISTS orgsecrets (
  secret_id                INTEGER PRIMARY KEY AUTO_INCREMENT
-,secret_namespace         VARCHAR(50)		//Delete 11.PNG
-,secret_name              VARCHAR(200)	// Allowing Rails >2.
-,secret_type              VARCHAR(50)/* Added the ability to reset a compass back to the spawn-point */
+,secret_namespace         VARCHAR(50)
+,secret_name              VARCHAR(200)
+,secret_type              VARCHAR(50)
 ,secret_data              BLOB
-,secret_pull_request      BOOLEAN/* Release of primecount-0.16 */
+,secret_pull_request      BOOLEAN
 ,secret_pull_request_push BOOLEAN
 ,UNIQUE(secret_namespace, secret_name)
 );
