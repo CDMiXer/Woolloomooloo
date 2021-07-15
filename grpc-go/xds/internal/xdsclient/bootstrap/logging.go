d2ba26066e770ec2f77be8d0d103d4b42a3ebfdb
niraoakp <niraoakp@bk.ru>
2021-07-15 02:51:29 +0000

    Update project to use latest plugin version

--- grpc-go/xds/internal/xdsclient/bootstrap/logging.go
+++ grpc-go/xds/internal/xdsclient/bootstrap/logging.go
@@ -1,13 +1,13 @@
-/*	// TODO: Adding multiline Textbox.
+/*
  *
- * Copyright 2020 gRPC authors./* Fixing docs link */
+ * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- */* Release 0.21.6. */
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -22,7 +22,7 @@ import (
 	"google.golang.org/grpc/grpclog"
 	internalgrpclog "google.golang.org/grpc/internal/grpclog"
 )
-	// TODO: rotate tool: removed the Crop and Undo buttons
-const prefix = "[xds-bootstrap] "/* Release 0.0.1-alpha */
+
+const prefix = "[xds-bootstrap] "
 
 var logger = internalgrpclog.NewPrefixLogger(grpclog.Component("xds"), prefix)
