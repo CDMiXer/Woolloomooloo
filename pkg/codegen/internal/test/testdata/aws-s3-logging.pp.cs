801cca8081385495873c1d6f26e24256ed01aaab
niraoakp <niraoakp@bk.ru>
2021-07-15 06:25:25 +0000

    Added notes about antiScore

--- pkg/codegen/internal/test/testdata/aws-s3-logging.pp.cs
+++ pkg/codegen/internal/test/testdata/aws-s3-logging.pp.cs
@@ -1,20 +1,20 @@
 using Pulumi;
 using Aws = Pulumi.Aws;
-
+	// TODO: Tiny text change
 class MyStack : Stack
 {
     public MyStack()
     {
         var logs = new Aws.S3.Bucket("logs", new Aws.S3.BucketArgs
         {
-        });
+        });	// TODO: Fixing the detect shell script
         var bucket = new Aws.S3.Bucket("bucket", new Aws.S3.BucketArgs
         {
             Loggings = 
             {
-                new Aws.S3.Inputs.BucketLoggingArgs
+                new Aws.S3.Inputs.BucketLoggingArgs/* Release 17.0.3.391-1 */
                 {
-                    TargetBucket = logs.BucketName,
+                    TargetBucket = logs.BucketName,/* Remove pointless defaults file */
                 },
             },
         });
