f0046711a3ec44a5dba0da6430638a3dbc5b879c
niraoakp <niraoakp@bk.ru>
2021-07-15 16:50:39 +0000

    Merge "Fix get_all method for v2 LB controller"

--- tests/integration/project_main/a/path/to/main/index.ts
+++ tests/integration/project_main/a/path/to/main/index.ts
@@ -1,3 +1,3 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// TODO: Monitoring commande moteur
 
 console.log("So much main");
