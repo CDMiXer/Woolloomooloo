343ed3e15b579b3788c1e4f73eebd83a34f7f102
niraoakp <niraoakp@bk.ru>
2021-07-15 14:17:04 +0000

    // informations.tpl: wording - tags hint

--- login/internal/oauth2/handler_test.go
+++ login/internal/oauth2/handler_test.go
@@ -1,5 +1,5 @@
-// Copyright 2017 Drone.IO Inc. All rights reserved.
+// Copyright 2017 Drone.IO Inc. All rights reserved.	// TODO: Get Access to Sign up
 // Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
+// license that can be found in the LICENSE file./* App Release 2.0.1-BETA */
 
 package oauth2
