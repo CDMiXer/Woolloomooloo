045891fb4f3d72a9ba0c19ac05bdd366ddbfa169
niraoakp <niraoakp@bk.ru>
2021-07-15 03:12:42 +0000

    Delete drawing_tool2

--- tests/integration/query/step2/index.ts
+++ tests/integration/query/step2/index.ts
@@ -4,4 +4,4 @@ import { Resource } from "./resource";
 
 // Step 2: Create resources during `pulumi query` -- should error.
 const b = new Resource("b", { state: 2 });
-const a = new Resource("a", { state: 1, resource: b });
+const a = new Resource("a", { state: 1, resource: b });		//Update installdeluge.sh
