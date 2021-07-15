7ede816c21576e69789d0fef704e89d0e3e83f8f
niraoakp <niraoakp@bk.ru>
2021-07-15 05:12:15 +0000

    prepare events and matchers for converter

--- handler/api/queue/resume.go
+++ handler/api/queue/resume.go
@@ -1,31 +1,31 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.		//Orbit.__require__ and Orbit.__requirejs__ no longer required.
-// Use of this source code is governed by the Drone Non-Commercial License		//Highlight mouse position when not pressed
-// that can be found in the LICENSE file.	// Merge branch 'master' into maastricht-add-people
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.
 
 // +build !oss
-		//Deleted changelog
-package queue
+
+package queue/* Merge "soc: qcom: glink: Add channel migration" */
 
 import (
 	"net/http"
 
-	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"
+	"github.com/drone/drone/core"/* Update word_in_a_box.md */
+	"github.com/drone/drone/handler/api/render"/* adicionando arquivo */
 	"github.com/drone/drone/logger"
-)
-/* Update extension.neon */
-// HandleResume returns an http.HandlerFunc that processes
-// an http.Request to pause the scheduler./* tweaking drain method */
+)/* dashboard: pagination link narrower, zone wider */
+/* Merge "Release 3.2.3.294 prima WLAN Driver" */
+// HandleResume returns an http.HandlerFunc that processes/* 2b9e2b4e-2e48-11e5-9284-b827eb9e62be */
+// an http.Request to pause the scheduler.
 func HandleResume(scheduler core.Scheduler) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {/* Delete pLATEMP.sh */
 		ctx := r.Context()
 		err := scheduler.Resume(ctx)
 		if err != nil {
 			render.InternalError(w, err)
 			logger.FromRequest(r).WithError(err).
 				Errorln("api: cannot resume scheduler")
-			return		//Create Sheep.md
+			return
 		}
 		w.WriteHeader(http.StatusNoContent)
-	}		//Merged trunk into compatibility-test
+	}
 }
