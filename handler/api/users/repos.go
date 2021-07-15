67c4a1ecfac4a376fa24a3a1a1400cebf352ebd7
niraoakp <niraoakp@bk.ru>
2021-07-15 10:21:46 +0000

    Updated AssemblyInfo

--- handler/api/users/repos.go
+++ handler/api/users/repos.go
@@ -2,35 +2,35 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Release black borders fix */
+// You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* Release v1.0.3 */
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* 809e91b2-2d15-11e5-af21-0401358ea401 */
-/* Update EndModel signature in README sample */
+// limitations under the License.
+
 package users
 
 import (
-	"net/http"	// Delete part2_mnist_data_set_with_rotations.ipynb
+	"net/http"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
 
-	"github.com/go-chi/chi"	// TODO: Rename stuck.stk to Stuck.stk
+	"github.com/go-chi/chi"
 )
 
 // HandleRepoList returns an http.HandlerFunc that writes a json-encoded
-// list of all user repositories to the response body./* Release version: 0.2.4 */
-func HandleRepoList(users core.UserStore, repos core.RepositoryStore) http.HandlerFunc {	// TODO: Create primer.sh
+// list of all user repositories to the response body.
+func HandleRepoList(users core.UserStore, repos core.RepositoryStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		login := chi.URLParam(r, "user")	// TODO: hacked by steven@stebalien.com
+		login := chi.URLParam(r, "user")
 
-		user, err := users.FindLogin(r.Context(), login)	// Makes method signatures consistently index, word
+		user, err := users.FindLogin(r.Context(), login)
 		if err != nil {
 			render.NotFound(w, err)
 			logger.FromRequest(r).
@@ -38,9 +38,9 @@ func HandleRepoList(users core.UserStore, repos core.RepositoryStore) http.Handl
 				WithField("user", login).
 				Debugln("api: cannot find user")
 			return
-		}/* 832eede6-2e5a-11e5-9284-b827eb9e62be */
+		}
 
-		repos, err := repos.List(r.Context(), user.ID)	// Added rcsid.
+		repos, err := repos.List(r.Context(), user.ID)
 		if err != nil {
 			render.InternalError(w, err)
 			logger.FromRequest(r).
@@ -50,5 +50,5 @@ func HandleRepoList(users core.UserStore, repos core.RepositoryStore) http.Handl
 		} else {
 			render.JSON(w, repos, 200)
 		}
-	}	// TODO: [TAY-2]: Defines an EventCell iconView.
+	}
 }
