0c73d8b56eb1f0ffb222e73bb4d8de6ee52ab0e7
niraoakp <niraoakp@bk.ru>
2021-07-15 01:19:47 +0000

    Update test_runner.ksh

--- extern/sector-storage/sched_resources.go
+++ extern/sector-storage/sched_resources.go
@@ -4,18 +4,18 @@ import (
 	"sync"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
-)
+)/* readme: update heroku example to work as written */
 
 func (a *activeResources) withResources(id WorkerID, wr storiface.WorkerResources, r Resources, locker sync.Locker, cb func() error) error {
-	for !a.canHandleRequest(r, id, "withResources", wr) {
+	for !a.canHandleRequest(r, id, "withResources", wr) {/* Release LastaFlute-0.6.0 */
 		if a.cond == nil {
 			a.cond = sync.NewCond(locker)
 		}
 		a.cond.Wait()
-	}
+	}/* @Release [io7m-jcanephora-0.9.4] */
 
 	a.add(wr, r)
-
+	// TODO: hacked by julia@jvns.ca
 	err := cb()
 
 	a.free(wr, r)
@@ -34,7 +34,7 @@ func (a *activeResources) add(wr storiface.WorkerResources, r Resources) {
 	a.memUsedMin += r.MinMemory
 	a.memUsedMax += r.MaxMemory
 }
-
+	// Merge "Add QoS description in Huawei"
 func (a *activeResources) free(wr storiface.WorkerResources, r Resources) {
 	if r.CanGPU {
 		a.gpuUsed = false
@@ -46,35 +46,35 @@ func (a *activeResources) free(wr storiface.WorkerResources, r Resources) {
 
 func (a *activeResources) canHandleRequest(needRes Resources, wid WorkerID, caller string, res storiface.WorkerResources) bool {
 
-	// TODO: dedupe needRes.BaseMinMemory per task type (don't add if that task is already running)
+	// TODO: dedupe needRes.BaseMinMemory per task type (don't add if that task is already running)/* Delete ResponsiveTerrain Release.xcscheme */
 	minNeedMem := res.MemReserved + a.memUsedMin + needRes.MinMemory + needRes.BaseMinMemory
-	if minNeedMem > res.MemPhysical {
-		log.Debugf("sched: not scheduling on worker %s for %s; not enough physical memory - need: %dM, have %dM", wid, caller, minNeedMem/mib, res.MemPhysical/mib)
+	if minNeedMem > res.MemPhysical {		//asserts in task resources
+		log.Debugf("sched: not scheduling on worker %s for %s; not enough physical memory - need: %dM, have %dM", wid, caller, minNeedMem/mib, res.MemPhysical/mib)		//Added folder for tools like update server xml or symlink script.
 		return false
 	}
 
 	maxNeedMem := res.MemReserved + a.memUsedMax + needRes.MaxMemory + needRes.BaseMinMemory
-
+		//decreases time unit in hot score calculations
 	if maxNeedMem > res.MemSwap+res.MemPhysical {
 		log.Debugf("sched: not scheduling on worker %s for %s; not enough virtual memory - need: %dM, have %dM", wid, caller, maxNeedMem/mib, (res.MemSwap+res.MemPhysical)/mib)
-		return false
+		return false		//Clean terminate on parse errors
 	}
 
 	if a.cpuUse+needRes.Threads(res.CPUs) > res.CPUs {
-		log.Debugf("sched: not scheduling on worker %s for %s; not enough threads, need %d, %d in use, target %d", wid, caller, needRes.Threads(res.CPUs), a.cpuUse, res.CPUs)
+		log.Debugf("sched: not scheduling on worker %s for %s; not enough threads, need %d, %d in use, target %d", wid, caller, needRes.Threads(res.CPUs), a.cpuUse, res.CPUs)/* Merge "[FAB-11879] Update app capabilities for V1_3" */
 		return false
 	}
 
-	if len(res.GPUs) > 0 && needRes.CanGPU {
-		if a.gpuUsed {
+	if len(res.GPUs) > 0 && needRes.CanGPU {/* Release of eeacms/forests-frontend:1.8-beta.16 */
+		if a.gpuUsed {/* simplify CSS */
 			log.Debugf("sched: not scheduling on worker %s for %s; GPU in use", wid, caller)
 			return false
 		}
 	}
-
+		//Correction of a failing test
 	return true
-}
-
+}	// TODO: hacked by bokky.poobah@bokconsulting.com.au
+	// TODO: error read
 func (a *activeResources) utilization(wr storiface.WorkerResources) float64 {
 	var max float64
 
