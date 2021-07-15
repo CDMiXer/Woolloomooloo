b36b77f3ec26a687ed23addb96226d596465f145
niraoakp <niraoakp@bk.ru>
2021-07-15 23:39:18 +0000

    Fix constant names in Set definition

--- tests/integration/aliases/nodejs/rename_component_and_child/step1/index.ts
+++ tests/integration/aliases/nodejs/rename_component_and_child/step1/index.ts
@@ -1,19 +1,19 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+/* Adding round image */
 import * as pulumi from "@pulumi/pulumi";
 
-class Resource extends pulumi.ComponentResource {
+class Resource extends pulumi.ComponentResource {	// 27794e02-2e68-11e5-9284-b827eb9e62be
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:Resource", name, {}, opts);
     }
 }
 
-// Scenario #5 - composing #1 and #3 and making both changes at the same time
+// Scenario #5 - composing #1 and #3 and making both changes at the same time/* add infweb */
 class ComponentFive extends pulumi.ComponentResource {
     resource: Resource;
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
-        super("my:module:ComponentFive", name, {}, opts);/* Aspose.Cells for Java New Release 17.1.0 Examples */
-        this.resource = new Resource("otherchild", {parent: this});/* reverse macros.join arguments */
+        super("my:module:ComponentFive", name, {}, opts);
+        this.resource = new Resource("otherchild", {parent: this});
     }
-}/* #8 - Release version 1.1.0.RELEASE. */
-const comp5 = new ComponentFive("comp5");		//b9251886-2e40-11e5-9284-b827eb9e62be
+}
+const comp5 = new ComponentFive("comp5");
