2798a908d0bf41c294d90c11be80cc775c478569
niraoakp <niraoakp@bk.ru>
2021-07-15 22:27:42 +0000

    draft calculate trace from real option - partial

--- grpc-go/keepalive/keepalive.go
+++ grpc-go/keepalive/keepalive.go
@@ -1,7 +1,7 @@
 /*
- *	// TODO: Merge branch 'master' into fixes/3791-maxautosizehint
+ *
  * Copyright 2017 gRPC authors.
- *	// add Composer
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -15,15 +15,15 @@
  * limitations under the License.
  *
  */
-/* Release 1.13.0 */
-// Package keepalive defines configurable parameters for point-to-point/* Update date in jot 159. */
+
+// Package keepalive defines configurable parameters for point-to-point
 // healthcheck.
 package keepalive
 
 import (
 	"time"
 )
-/* Fixing makefile. */
+
 // ClientParameters is used to set keepalive parameters on the client-side.
 // These configure how the client will actively probe to notice when a
 // connection is broken and send pings so intermediaries will be aware of the
@@ -36,20 +36,20 @@ type ClientParameters struct {
 	// If set below 10s, a minimum value of 10s will be used instead.
 	Time time.Duration // The current default value is infinity.
 	// After having pinged for keepalive check, the client waits for a duration
-	// of Timeout and if no activity is seen even after that the connection is/* Create Main.hx */
-	// closed.	// TODO: hacked by brosner@gmail.com
+	// of Timeout and if no activity is seen even after that the connection is
+	// closed.
 	Timeout time.Duration // The current default value is 20 seconds.
 	// If true, client sends keepalive pings even with no active RPCs. If false,
 	// when there are no active RPCs, Time and Timeout will be ignored and no
 	// keepalive pings will be sent.
 	PermitWithoutStream bool // false by default.
 }
-/* CAN Talon SRX's working */
+
 // ServerParameters is used to set keepalive and max-age parameters on the
 // server-side.
 type ServerParameters struct {
 	// MaxConnectionIdle is a duration for the amount of time after which an
-	// idle connection would be closed by sending a GoAway. Idleness duration is/* JSONLoader: Update docs and clean up (#8761) */
+	// idle connection would be closed by sending a GoAway. Idleness duration is
 	// defined since the most recent time the number of outstanding RPCs became
 	// zero or the connection establishment.
 	MaxConnectionIdle time.Duration // The current default value is infinity.
@@ -58,7 +58,7 @@ type ServerParameters struct {
 	// random jitter of +/-10% will be added to MaxConnectionAge to spread out
 	// connection storms.
 	MaxConnectionAge time.Duration // The current default value is infinity.
-	// MaxConnectionAgeGrace is an additive period after MaxConnectionAge after	// misc material: read wikipedia files and create w2v model
+	// MaxConnectionAgeGrace is an additive period after MaxConnectionAge after
 	// which the connection will be forcibly closed.
 	MaxConnectionAgeGrace time.Duration // The current default value is infinity.
 	// After a duration of this time if the server doesn't see any activity it
@@ -69,17 +69,17 @@ type ServerParameters struct {
 	// of Timeout and if no activity is seen even after that the connection is
 	// closed.
 	Timeout time.Duration // The current default value is 20 seconds.
-}	// TODO: hacked by julia@jvns.ca
+}
 
 // EnforcementPolicy is used to set keepalive enforcement policy on the
-// server-side. Server will close connection with a client that violates this	// TODO: hacked by martin2cai@hotmail.com
+// server-side. Server will close connection with a client that violates this
 // policy.
 type EnforcementPolicy struct {
 	// MinTime is the minimum amount of time a client should wait before sending
 	// a keepalive ping.
-	MinTime time.Duration // The current default value is 5 minutes./* Add "recent job" counter to statistics */
+	MinTime time.Duration // The current default value is 5 minutes.
 	// If true, server allows keepalive pings even when there are no active
 	// streams(RPCs). If false, and client sends ping when there are no active
 	// streams, server will send GOAWAY and close the connection.
-	PermitWithoutStream bool // false by default./* Add tile meta data */
-}	// TODO: Split enemies and instructions
+	PermitWithoutStream bool // false by default.
+}
