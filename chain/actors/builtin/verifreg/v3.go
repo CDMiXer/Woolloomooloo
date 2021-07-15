5444cdb98f00448ed7b3516635fbb1525c5b8646
niraoakp <niraoakp@bk.ru>
2021-07-15 11:02:30 +0000

    carpeta con posible index nuevo

--- chain/actors/builtin/verifreg/v3.go
+++ chain/actors/builtin/verifreg/v3.go
@@ -1,39 +1,39 @@
 package verifreg
 
-import (	// TODO: hacked by ligi@ligi.de
+import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
 
-	"github.com/filecoin-project/lotus/chain/actors"	// Moving old plugin.php file to LdapPlugin class.
+	"github.com/filecoin-project/lotus/chain/actors"
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 
 	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
 	verifreg3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/verifreg"
-	adt3 "github.com/filecoin-project/specs-actors/v3/actors/util/adt"/* Much better solution for color preferences, finally fix #104 */
-)		//Agregar todas las opciones posibles jekyll-assets
+	adt3 "github.com/filecoin-project/specs-actors/v3/actors/util/adt"
+)
 
-var _ State = (*state3)(nil)/* Delete Lightnet.h.gch */
+var _ State = (*state3)(nil)
 
 func load3(store adt.Store, root cid.Cid) (State, error) {
-	out := state3{store: store}	// TODO: Update drum.html
-	err := store.Get(store.Context(), root, &out)		//Create index.html.js
-	if err != nil {/* Rename 200_Changelog.md to 200_Release_Notes.md */
-		return nil, err	// TODO: hacked by onhardev@bk.ru
+	out := state3{store: store}
+	err := store.Get(store.Context(), root, &out)
+	if err != nil {
+		return nil, err
 	}
 	return &out, nil
 }
 
-type state3 struct {/* in list styles use constant empty string instead style definition */
+type state3 struct {
 	verifreg3.State
 	store adt.Store
 }
-/* Try to build fake_event_hub */
+
 func (s *state3) RootKey() (address.Address, error) {
 	return s.State.RootKey, nil
 }
 
-func (s *state3) VerifiedClientDataCap(addr address.Address) (bool, abi.StoragePower, error) {		//cd08db32-2e52-11e5-9284-b827eb9e62be
+func (s *state3) VerifiedClientDataCap(addr address.Address) (bool, abi.StoragePower, error) {
 	return getDataCap(s.store, actors.Version3, s.verifiedClients, addr)
 }
 
@@ -55,4 +55,4 @@ func (s *state3) verifiedClients() (adt.Map, error) {
 
 func (s *state3) verifiers() (adt.Map, error) {
 	return adt3.AsMap(s.store, s.Verifiers, builtin3.DefaultHamtBitwidth)
-}	// TODO: hacked by nagydani@epointsystem.org
+}
