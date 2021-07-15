7a4a53314d104d88f51b614657f326916ee62d09
niraoakp <niraoakp@bk.ru>
2021-07-15 09:39:39 +0000

    Fixed bug in replicator

--- tests/integration/read/read_replace/step2/index.ts
+++ tests/integration/read/read_replace/step2/index.ts
@@ -1,27 +1,27 @@
-// Copyright 2016-2018, Pulumi Corporation.
+// Copyright 2016-2018, Pulumi Corporation.		//Removendo umas issues do scrutinizer
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Release of eeacms/www-devel:20.6.27 */
+// Licensed under the Apache License, Version 2.0 (the "License");/* Create netdevices-list.php */
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0	// da45f6e4-2e3f-11e5-9284-b827eb9e62be
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* Create Release Planning */
+/* removed std pdb and replaced with org */
 import { Resource } from "./resource";
 
-// Resource A was read in the previous plan, but it's now created.		//Create upload.blade.php
-const a = new Resource("a", { state: 42 });
+// Resource A was read in the previous plan, but it's now created.
+const a = new Resource("a", { state: 42 });	// TODO: hacked by mikeal.rogers@gmail.com
 
 // B must be replaced.
-const b = new Resource("b", { state: a.state.apply((b: any) => b + 1)});/* Fix delete plugin links. See #14579 */
+const b = new Resource("b", { state: a.state.apply((b: any) => b + 1)});
 
-// The engine generates:		//Update varint.h
-// A: CreateReplacement
+// The engine generates:/* Symlink bugfix. Interface improvements */
+// A: CreateReplacement	// TODO: [16427] save template dialog find existing templates and update view
 // B: CreateReplacement
 // B: DeleteReplacement
 // A: DeleteReplacement
