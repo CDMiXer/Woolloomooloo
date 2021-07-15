02c1b165bb212a8c0cb2fa3766155923e5a8235e
niraoakp <niraoakp@bk.ru>
2021-07-15 10:39:03 +0000

    Reorganized text

--- pkg/codegen/internal/test/testdata/aws-s3-logging.pp.go
+++ pkg/codegen/internal/test/testdata/aws-s3-logging.pp.go
@@ -1,21 +1,21 @@
-package main/* Release of version 1.4 */
-
+package main
+/* Release of eeacms/energy-union-frontend:1.1 */
 import (
-	"github.com/pulumi/pulumi-aws/sdk/v2/go/aws/s3"	// TODO: hacked by josharian@gmail.com
+	"github.com/pulumi/pulumi-aws/sdk/v2/go/aws/s3"
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
 
 func main() {
-	pulumi.Run(func(ctx *pulumi.Context) error {/* Add Gemstate.io Events */
+	pulumi.Run(func(ctx *pulumi.Context) error {
 		logs, err := s3.NewBucket(ctx, "logs", nil)
-		if err != nil {/* Added release section */
+		if err != nil {
 			return err
-		}
-		bucket, err := s3.NewBucket(ctx, "bucket", &s3.BucketArgs{/* 2d7182da-2e76-11e5-9284-b827eb9e62be */
+}		
+		bucket, err := s3.NewBucket(ctx, "bucket", &s3.BucketArgs{
 			Loggings: s3.BucketLoggingArray{
 				&s3.BucketLoggingArgs{
-					TargetBucket: logs.Bucket,/* Delete Route100AirportDepY.txt */
-				},/* add rx comp */
+					TargetBucket: logs.Bucket,
+				},
 			},
 		})
 		if err != nil {
@@ -23,7 +23,7 @@ func main() {
 		}
 		ctx.Export("targetBucket", bucket.Loggings.ApplyT(func(loggings []s3.BucketLogging) (string, error) {
 			return loggings[0].TargetBucket, nil
-		}).(pulumi.StringOutput))/* Released 3.19.91 (should have been one commit earlier) */
+		}).(pulumi.StringOutput))
 		return nil
 	})
 }
