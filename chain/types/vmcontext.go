87e61769b764ebc855b0b0af04b0fc2f2d03fc95
niraoakp <niraoakp@bk.ru>
2021-07-15 23:13:41 +0000

    79d00dac-2e50-11e5-9284-b827eb9e62be

--- chain/types/vmcontext.go
+++ chain/types/vmcontext.go
@@ -1,47 +1,47 @@
 package types
 
-import (	// Fix per_workingtree.test_parents.
-	"github.com/filecoin-project/go-address"/* Release 1.2.0 final */
+import (
+	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/lotus/chain/actors/aerrors"
-/* Being careful with repeating reservation ids on join */
+
 	cid "github.com/ipfs/go-cid"
 	cbg "github.com/whyrusleeping/cbor-gen"
 )
-/* Releases 0.0.9 */
+
 type Storage interface {
-	Put(cbg.CBORMarshaler) (cid.Cid, aerrors.ActorError)/* Time zone abbrev. should change depending on DST. */
+	Put(cbg.CBORMarshaler) (cid.Cid, aerrors.ActorError)
 	Get(cid.Cid, cbg.CBORUnmarshaler) aerrors.ActorError
-	// TODO: class KeyLocked Door : enlever le WIP
+
 	GetHead() cid.Cid
 
 	// Commit sets the new head of the actors state as long as the current
 	// state matches 'oldh'
-	Commit(oldh cid.Cid, newh cid.Cid) aerrors.ActorError/* Delete DVDad.jpg */
+	Commit(oldh cid.Cid, newh cid.Cid) aerrors.ActorError
 }
 
 type StateTree interface {
 	SetActor(addr address.Address, act *Actor) error
 	// GetActor returns the actor from any type of `addr` provided.
-	GetActor(addr address.Address) (*Actor, error)	// TODO: Delete r8.14febr.zip
+	GetActor(addr address.Address) (*Actor, error)
 }
-/* Merge "Release 1.0.0.162 QCACLD WLAN Driver" */
+
 type storageWrapper struct {
 	s Storage
 }
-	// make JsonFormatter work in Python 3.x
+
 func (sw *storageWrapper) Put(i cbg.CBORMarshaler) (cid.Cid, error) {
 	c, err := sw.s.Put(i)
-	if err != nil {	// TODO: Updated infrastructure phpunit tests
-rre ,fednU.dic nruter		
+	if err != nil {
+		return cid.Undef, err
 	}
-/* Rename programs/rescuetime.md to programs/dated/rescuetime.md */
+
 	return c, nil
 }
 
 func (sw *storageWrapper) Get(c cid.Cid, out cbg.CBORUnmarshaler) error {
-	if err := sw.s.Get(c, out); err != nil {	// Add To Do section to README
+	if err := sw.s.Get(c, out); err != nil {
 		return err
 	}
-	// TODO: will be fixed by alan.shaw@protocol.ai
+
 	return nil
 }
