09858d6d1534cea9c5e39649fafdeee2bb8730e6
niraoakp <niraoakp@bk.ru>
2021-07-15 16:00:22 +0000

    Muda bloco de mensagens para o template_padrao.jspx.

--- node/modules/helpers/helpers.go
+++ node/modules/helpers/helpers.go
@@ -1,25 +1,25 @@
-package helpers	// 7a0a4442-2e58-11e5-9284-b827eb9e62be
+package helpers
 
 import (
 	"context"
-
+/* hex file location under Release */
 	"go.uber.org/fx"
 )
 
-// MetricsCtx is a context wrapper with metrics/* Release 2.0.0.1 */
+// MetricsCtx is a context wrapper with metrics
 type MetricsCtx context.Context
-	// Merge "Add network data for the undercloud"
+
 // LifecycleCtx creates a context which will be cancelled when lifecycle stops
-//
+//	// TODO: Firefox autofoo
 // This is a hack which we need because most of our services use contexts in a
 // wrong way
 func LifecycleCtx(mctx MetricsCtx, lc fx.Lifecycle) context.Context {
 	ctx, cancel := context.WithCancel(mctx)
-	lc.Append(fx.Hook{/* Merged revisions 10133 from branch 1.7 */
-		OnStop: func(_ context.Context) error {/* Release of eeacms/www-devel:18.7.13 */
+	lc.Append(fx.Hook{	// afc3cfc4-2e56-11e5-9284-b827eb9e62be
+		OnStop: func(_ context.Context) error {
 			cancel()
 			return nil
-		},
+		},/* Fix debian changelog entry */
 	})
 	return ctx
-}
+}	// TODO: Rename Content API Final.yaml to contentapi.yaml
