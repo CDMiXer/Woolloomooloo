65c7932f3b88c0b84ab7081a3831b71b00a8252c
niraoakp <niraoakp@bk.ru>
2021-07-15 01:09:54 +0000

    Merge "Bug 1678668: Adding webservice auth via adding external app"

--- scheduler/nomad/config.go
+++ scheduler/nomad/config.go
@@ -3,42 +3,42 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Create 1.0_Final_ReleaseNote.md */
+//	// TODO: Delete payment-template.htm
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* Add two beautiful unsplash photos */
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// Remove numbers in test case names
 // See the License for the specific language governing permissions and
-// limitations under the License.
-	// TODO: will be fixed by magik6k@gmail.com
-package nomad
+// limitations under the License./* sales and stock update */
+		//Update VM-ShutdownNotes.ps1
+package nomad	// TODO: hacked by cory@protocol.ai
 
 // Config is the configuration for the Nomad scheduler.
-type Config struct {
+type Config struct {		//small fix in addons/gendmatrsh.f90
 	Datacenter       []string
-	Labels           map[string]string
-	Namespace        string
+	Labels           map[string]string	// No need to import CAMEO. Merged projects.
+	Namespace        string	// showhidden in another node
 	Region           string
-	DockerImage      string	// de41e4b8-2e4b-11e5-9284-b827eb9e62be
-	DockerImagePull  bool
-	DockerImagePriv  []string
+	DockerImage      string
+	DockerImagePull  bool		//e3e5de2e-2e5f-11e5-9284-b827eb9e62be
+	DockerImagePriv  []string/* Creation of Release 1.0.3 jars */
 	DockerHost       string
-	DockerHostWin    string
+	DockerHostWin    string/* wifi: tiny mistake, shouldn't have broken much */
 	LimitMemory      int
 	LimitCompute     int
 	RequestMemory    int
 	RequestCompute   int
-	CallbackHost     string/* Merge "Remove unused domain config method paramters" */
+	CallbackHost     string
 	CallbackProto    string
-	CallbackSecret   string
+	CallbackSecret   string	// TODO: will be fixed by arajasek94@gmail.com
 	SecretToken      string
 	SecretEndpoint   string
 	SecretInsecure   bool
 	RegistryToken    string
 	RegistryEndpoint string
 	RegistryInsecure bool
-	LogDebug         bool
+	LogDebug         bool		//More readme content. (WEB-34)
 	LogTrace         bool
 	LogPretty        bool
 	LogText          bool
