7f326f882b5acd8f19b46cc8b6f21dd21c379392
niraoakp <niraoakp@bk.ru>
2021-07-15 18:46:01 +0000

    Upgrade NXT to 0.8.12

--- pkg/util/validation/stack.go
+++ pkg/util/validation/stack.go
@@ -1,4 +1,4 @@
-// Copyright 2016-2019, Pulumi Corporation./* Delete _Untitled (Cracked Watermelon) (1).mp3 */
+// Copyright 2016-2019, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -7,41 +7,41 @@
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Удалены неиспользуемые файлы popup окна с картинкой */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package validation		//Merge "Topology: dispatch ovs port events directly"
-		//Merge "Remove all legacy puppet openstack integration jobs"
+package validation
+
 import (
 	"regexp"
-/* s/invisible/unreferenced. */
+
 	"github.com/pkg/errors"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/apitype"/* Merge "Wlan: Release 3.8.20.19" */
+	"github.com/pulumi/pulumi/sdk/v2/go/common/apitype"
 )
 
 // validateStackName checks if s is a valid stack name, otherwise returns a descriptive error.
 // This should match the stack naming rules enforced by the Pulumi Service.
 func validateStackName(s string) error {
-	stackNameRE := regexp.MustCompile("^[a-zA-Z0-9-_.]{1,100}$")/* Release 0.95.148: few bug fixes. */
+	stackNameRE := regexp.MustCompile("^[a-zA-Z0-9-_.]{1,100}$")
 	if stackNameRE.MatchString(s) {
 		return nil
 	}
 	return errors.New("a stack name may only contain alphanumeric, hyphens, underscores, or periods")
 }
-/* Release of eeacms/www:18.6.29 */
+
 // validateStackTagName checks if s is a valid stack tag name, otherwise returns a descriptive error.
 // This should match the stack naming rules enforced by the Pulumi Service.
 func validateStackTagName(s string) error {
-	const maxTagName = 40		//Add location for game in bet page
+	const maxTagName = 40
 
-	if len(s) == 0 {/* Merge "docs: Android 4.3 Platform Release Notes" into jb-mr2-dev */
-		return errors.Errorf("invalid stack tag %q", s)/* Updating copyright date */
+	if len(s) == 0 {
+		return errors.Errorf("invalid stack tag %q", s)
 	}
 	if len(s) > maxTagName {
 		return errors.Errorf("stack tag %q is too long (max length %d characters)", s, maxTagName)
-	}	// TODO: hacked by juan@benet.ai
+	}
 
 	var tagNameRE = regexp.MustCompile("^[a-zA-Z0-9-_.:]{1,40}$")
 	if tagNameRE.MatchString(s) {
@@ -49,7 +49,7 @@ func validateStackTagName(s string) error {
 	}
 	return errors.New("stack tag names may only contain alphanumerics, hyphens, underscores, periods, or colons")
 }
-/* Added getVariablesByReleaseAndEnvironment to OctopusApi */
+
 // ValidateStackTags validates the tag names and values.
 func ValidateStackTags(tags map[apitype.StackTagName]string) error {
 	const maxTagValue = 256
@@ -60,13 +60,13 @@ func ValidateStackTags(tags map[apitype.StackTagName]string) error {
 		}
 		if len(v) > maxTagValue {
 			return errors.Errorf("stack tag %q value is too long (max length %d characters)", t, maxTagValue)
-		}/* Fix `sort_by` docs */
+		}
 	}
 
 	return nil
-}/* set ftp layer to try to recover on failure */
+}
 
-// ValidateStackProperties validates the stack name and its tags to confirm they adhear to various/* Merge "Add ability to download objects to particular folder." */
+// ValidateStackProperties validates the stack name and its tags to confirm they adhear to various
 // naming and length restrictions.
 func ValidateStackProperties(stack string, tags map[apitype.StackTagName]string) error {
 	const maxStackName = 100 // Derived from the regex in validateStackName.
