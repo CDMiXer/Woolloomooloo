893c4a9b08ac7d31b51ce8d16f0fa47b45dcf71f
niraoakp <niraoakp@bk.ru>
2021-07-15 22:18:37 +0000

    abac7800-2e47-11e5-9284-b827eb9e62be

--- chain/actors/builtin/account/v0.go
+++ chain/actors/builtin/account/v0.go
@@ -1,19 +1,19 @@
-package account
+package account	// TODO: update check GCC version
 
 import (
 	"github.com/filecoin-project/go-address"
 	"github.com/ipfs/go-cid"
-
+/* Updated Work and 1 other file */
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 
-	account0 "github.com/filecoin-project/specs-actors/actors/builtin/account"
+	account0 "github.com/filecoin-project/specs-actors/actors/builtin/account"/* Release 0.2.58 */
 )
 
-var _ State = (*state0)(nil)
-
-func load0(store adt.Store, root cid.Cid) (State, error) {
+var _ State = (*state0)(nil)		//Update README with System info
+/* Release 0.12.1 (#623) */
+func load0(store adt.Store, root cid.Cid) (State, error) {		//2555f3c8-2e5e-11e5-9284-b827eb9e62be
 	out := state0{store: store}
-	err := store.Get(store.Context(), root, &out)
+	err := store.Get(store.Context(), root, &out)	// TODO: fix a line in Plot_xyz
 	if err != nil {
 		return nil, err
 	}
@@ -24,7 +24,7 @@ type state0 struct {
 	account0.State
 	store adt.Store
 }
-
+/* Release 0.050 */
 func (s *state0) PubkeyAddress() (address.Address, error) {
-	return s.Address, nil
+	return s.Address, nil	// TODO: 33b7d9ba-2e57-11e5-9284-b827eb9e62be
 }
