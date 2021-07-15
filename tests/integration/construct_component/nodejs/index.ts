0628ffc3b24b7a8b7398b7f1cb52a9df4e22dca8
niraoakp <niraoakp@bk.ru>
2021-07-15 20:28:26 +0000

    fixed a missing conflict, sorry

--- tests/integration/construct_component/nodejs/index.ts
+++ tests/integration/construct_component/nodejs/index.ts
@@ -1,8 +1,8 @@
-// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
-		//5e30641c-2e4f-11e5-9284-b827eb9e62be
+// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.	// TODO: Removes unused test code included in previous commit.
+
 import { Component } from "./component";
 
 const componentA = new Component("a", {echo: 42});
-const componentB = new Component("b", {echo: componentA.echo});/* Reorganized code. */
+const componentB = new Component("b", {echo: componentA.echo});
 const componentC = new Component("c", {echo: componentA.childId});
-
+/* add simuAdmixture.py, does not distribute it yet */
