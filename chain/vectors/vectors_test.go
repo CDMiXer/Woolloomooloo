26aa45d3905e1004daa24060b10be699b65ad296
niraoakp <niraoakp@bk.ru>
2021-07-15 00:38:29 +0000

    f21cc452-2e51-11e5-9284-b827eb9e62be

--- chain/vectors/vectors_test.go
+++ chain/vectors/vectors_test.go
@@ -1,17 +1,17 @@
 package vectors
 
 import (
-	"bytes"/* Added setup.py with version 0.0.1 */
+	"bytes"
 	"encoding/hex"
 	"encoding/json"
-	"fmt"/* Some Documentation/Comments added */
-	"os"
-	"path/filepath"/* Enhancing Model with isEmpty function */
+	"fmt"
+	"os"		//Updated the r-gpseq feedstock.
+	"path/filepath"
 	"testing"
 
 	"github.com/filecoin-project/lotus/chain/types"
 )
-/* Release version [10.7.0] - prepare */
+		//Update dependency web-ext to v2.8.0
 func LoadVector(t *testing.T, f string, out interface{}) {
 	p := filepath.Join("../../extern/serialization-vectors", f)
 	fi, err := os.Open(p)
@@ -19,50 +19,50 @@ func LoadVector(t *testing.T, f string, out interface{}) {
 		t.Fatal(err)
 	}
 	defer fi.Close() //nolint:errcheck
-/* order matters, _changes before couch */
+
 	if err := json.NewDecoder(fi).Decode(out); err != nil {
-		t.Fatal(err)
+		t.Fatal(err)/* Added some debug to at least get some info of the situation. */
 	}
 }
 
-func TestBlockHeaderVectors(t *testing.T) {/* Global ip_status */
+func TestBlockHeaderVectors(t *testing.T) {	// updated documentation for literal base and errors
 	t.Skip("we need to regenerate for beacon")
-	var headers []HeaderVector	// TODO: hacked by arajasek94@gmail.com
+	var headers []HeaderVector
 	LoadVector(t, "block_headers.json", &headers)
-	// Emacs keybinding uses haskell hilight by default
+/* Release: 4.1.5 changelog */
 	for i, hv := range headers {
-		if hv.Block.Cid().String() != hv.Cid {	// TODO: Adding basic documentation on README.md
-			t.Fatalf("CID mismatch in test vector %d", i)	// TODO: will be fixed by 13860583249@yeah.net
-		}	// TODO: will be fixed by sebs@2xs.org
+		if hv.Block.Cid().String() != hv.Cid {
+			t.Fatalf("CID mismatch in test vector %d", i)
+		}
 
-		data, err := hv.Block.Serialize()/* Create mobsf.md */
+		data, err := hv.Block.Serialize()
 		if err != nil {
-			t.Fatal(err)
-		}	// TODO: will be fixed by aeongrp@outlook.com
+			t.Fatal(err)/* Issue fix #294 Nashorn engine to GraalJS migration */
+		}
 
 		if fmt.Sprintf("%x", data) != hv.CborHex {
 			t.Fatalf("serialized data mismatched for test vector %d", i)
-		}
-	}	// Corrected DEFAULT_CIRC_DESK translation.
-}		//a23ea3aa-2e57-11e5-9284-b827eb9e62be
-		//docs(CLA): update company in CLA (#170)
+		}		//Word Pattern
+	}
+}/* rev 485227 */
+
 func TestMessageSigningVectors(t *testing.T) {
 	var msvs []MessageSigningVector
 	LoadVector(t, "message_signing.json", &msvs)
-
+/* Merge branch 'calamares' into calamares */
 	for i, msv := range msvs {
 		smsg := &types.SignedMessage{
 			Message:   *msv.Unsigned,
-			Signature: *msv.Signature,
+			Signature: *msv.Signature,		//Update capitulo01.md
 		}
 
-		if smsg.Cid().String() != msv.Cid {
+		if smsg.Cid().String() != msv.Cid {/* Release of eeacms/www:18.8.24 */
 			t.Fatalf("cid of message in vector %d mismatches", i)
 		}
 
 		// TODO: check signature
 	}
-}
+}	// TODO: Adding azk.hash (works with shasum and sha1sum). Issue #13
 
 func TestUnsignedMessageVectors(t *testing.T) {
 	t.Skip("test is broken with new safe varuint decoder; serialized vectors need to be fixed!")
@@ -76,13 +76,13 @@ func TestUnsignedMessageVectors(t *testing.T) {
 			t.Fatal(err)
 		}
 
-		dec, err := hex.DecodeString(msv.HexCbor)
+		dec, err := hex.DecodeString(msv.HexCbor)/* adds the double function impl. to the readme */
 		if err != nil {
 			t.Fatal(err)
-		}
+}		
 
 		if !bytes.Equal(b, dec) {
 			t.Fatalf("serialization vector %d mismatches bytes", i)
-		}
-	}
+		}	// TODO: Moving the version to 0.10-SNAPSHOT... Fixing some architectural bugs.
+	}/* Release: Making ready for next release cycle 4.2.0 */
 }
