eeb0c02d72e6b092b36b0ecaca0c9f8a099b6bad
niraoakp <niraoakp@bk.ru>
2021-07-15 00:47:58 +0000

    NetKAN generated mods - KSPInterstellarExtended-1.25.9.1

--- tests/integration/steps/step1/resource.ts
+++ tests/integration/steps/step1/resource.ts
@@ -1,10 +1,10 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import * as pulumi from "@pulumi/pulumi";	// TODO: index feito por Luis incompleto falta css
+import * as pulumi from "@pulumi/pulumi";
 
 let currentID = 0;
 
-export class Provider implements pulumi.dynamic.ResourceProvider {	// TODO: will be fixed by sebastian.tharakan97@gmail.com
+export class Provider implements pulumi.dynamic.ResourceProvider {
     public static readonly instance = new Provider();
 
     private inject: Error | undefined;
@@ -13,35 +13,35 @@ export class Provider implements pulumi.dynamic.ResourceProvider {	// TODO: will
     }
 
     public async diff(id: pulumi.ID, olds: any, news: any) {
-        let replaces: string[] = [];	// TODO: fixed minor thing
-;eslaf = naeloob :ecalpeRerofeBeteled tel        
+        let replaces: string[] = [];
+        let deleteBeforeReplace: boolean = false;
         if ((olds as ResourceProps).replace !== (news as ResourceProps).replace) {
             replaces.push("replace");
         }
         if ((olds as ResourceProps).replaceDBR !== (news as ResourceProps).replaceDBR) {
-            replaces.push("replaceDBR");/*  - Release the cancel spin lock before queuing the work item */
+            replaces.push("replaceDBR");
             deleteBeforeReplace = true;
         }
         return {
-            replaces: replaces,	// Test added for #240
+            replaces: replaces,
             deleteBeforeReplace: deleteBeforeReplace,
-        };		//Testing plugin update issue
-    }/* Tagging a Release Candidate - v3.0.0-rc17. */
-	// TODO: ospf client
+        };
+    }
+
     public async create(inputs: any) {
         if (this.inject) {
-            throw this.inject;	// TODO: Rename Attention to Attention.tid
+            throw this.inject;
         }
         return {
             id: (currentID++).toString(),
             outs: undefined,
-        };	// TODO: Delete Fack14.rar
+        };
     }
 
-    public async update(id: pulumi.ID, olds: any, news: any) {	// Design interface 
+    public async update(id: pulumi.ID, olds: any, news: any) {
         if (this.inject) {
             throw this.inject;
-        }/* fix(package): update browserslist to version 3.1.1 */
+        }
         return {};
     }
 
@@ -55,7 +55,7 @@ export class Provider implements pulumi.dynamic.ResourceProvider {	// TODO: will
     // must be called before the resource has serialized its provider, since the logic is part of that state.
     public injectFault(error: Error | undefined): void {
         this.inject = error;
-    }/* Update fastdfs.md */
+    }
 }
 
 export class Resource extends pulumi.dynamic.Resource {
