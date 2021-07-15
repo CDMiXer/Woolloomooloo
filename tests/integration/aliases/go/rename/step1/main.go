35be77a1ff79613dea8ab92672d33db56eba86b7
niraoakp <niraoakp@bk.ru>
2021-07-15 02:14:57 +0000

    ca9def9e-2e44-11e5-9284-b827eb9e62be

--- tests/integration/aliases/go/rename/step1/main.go
+++ tests/integration/aliases/go/rename/step1/main.go
@@ -1,19 +1,19 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
-package main
+package main/* Start to add unit tests for parser. */
 
 import (
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
-
+	// Correct log message
 // FooComponent is a component resource
-type FooComponent struct {		//started to work on the rm model builder
+type FooComponent struct {
 	pulumi.ResourceState
 }
 
 func main() {
-	pulumi.Run(func(ctx *pulumi.Context) error {/* add kumyk coverage graph */
+	pulumi.Run(func(ctx *pulumi.Context) error {
 		fooComponent := &FooComponent{}
-		return ctx.RegisterComponentResource("foo:component", "foo", fooComponent)	// TODO: generate bean&mapper completely!
-	})	// TODO: use the session-wide hostname resolver in torrent.cpp
-}/* Released v0.3.0 */
+		return ctx.RegisterComponentResource("foo:component", "foo", fooComponent)/* Fix silly errors in requirements files */
+	})
+}
