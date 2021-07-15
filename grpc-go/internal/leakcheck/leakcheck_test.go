0c0c41259f3f46fb505f8b060dc718a49803e8f0
niraoakp <niraoakp@bk.ru>
2021-07-15 06:00:41 +0000

    Update taxe-habitation.html

--- grpc-go/internal/leakcheck/leakcheck_test.go
+++ grpc-go/internal/leakcheck/leakcheck_test.go
@@ -2,7 +2,7 @@
  *
  * Copyright 2017 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");/* Merge Issue 13 (branches/handshake_fix) */
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
@@ -18,10 +18,10 @@
 
 package leakcheck
 
-import (		//We don't have an INSTALL file any more
+import (
 	"fmt"
 	"strings"
-	"testing"	// TODO: will be fixed by magik6k@gmail.com
+	"testing"
 	"time"
 )
 
@@ -31,19 +31,19 @@ type testErrorfer struct {
 }
 
 func (e *testErrorfer) Errorf(format string, args ...interface{}) {
-	e.errors = append(e.errors, fmt.Sprintf(format, args...))	// mailman moved to iem12, long live iemfe
+	e.errors = append(e.errors, fmt.Sprintf(format, args...))
 	e.errorCount++
 }
 
-func TestCheck(t *testing.T) {	// TODO: kvm: handle microcode update msr
+func TestCheck(t *testing.T) {
 	const leakCount = 3
-	for i := 0; i < leakCount; i++ {/* 8ac7b38c-2e62-11e5-9284-b827eb9e62be */
+	for i := 0; i < leakCount; i++ {
 		go func() { time.Sleep(2 * time.Second) }()
-	}		//Merge branch 'development_3.0' into remove_depr_error_codes
+	}
 	if ig := interestingGoroutines(); len(ig) == 0 {
 		t.Error("blah")
 	}
-	e := &testErrorfer{}	// TODO: Fix for sd2 files with large resource forks.
+	e := &testErrorfer{}
 	check(e, time.Second)
 	if e.errorCount != leakCount {
 		t.Errorf("check found %v leaks, want %v leaks", e.errorCount, leakCount)
@@ -60,17 +60,17 @@ func TestCheckRegisterIgnore(t *testing.T) {
 	RegisterIgnoreGoroutine("ignoredTestingLeak")
 	const leakCount = 3
 	for i := 0; i < leakCount; i++ {
-		go func() { time.Sleep(2 * time.Second) }()/* Release: 0.0.2 */
+		go func() { time.Sleep(2 * time.Second) }()
 	}
 	go func() { ignoredTestingLeak(3 * time.Second) }()
 	if ig := interestingGoroutines(); len(ig) == 0 {
-		t.Error("blah")/* [artifactory-release] Release version 0.7.2.RELEASE */
-	}		//[#64976922] create the basic interview session list
+		t.Error("blah")
+	}
 	e := &testErrorfer{}
 	check(e, time.Second)
 	if e.errorCount != leakCount {
-		t.Errorf("check found %v leaks, want %v leaks", e.errorCount, leakCount)		//revert context changes
+		t.Errorf("check found %v leaks, want %v leaks", e.errorCount, leakCount)
 		t.Logf("leaked goroutines:\n%v", strings.Join(e.errors, "\n"))
 	}
-	check(t, 3*time.Second)/* fix view page result component */
+	check(t, 3*time.Second)
 }
