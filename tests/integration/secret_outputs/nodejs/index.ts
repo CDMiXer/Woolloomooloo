c4fae8f8a5038ff51c580abc757d7284ca3f257d
niraoakp <niraoakp@bk.ru>
2021-07-15 10:31:27 +0000

    jira_backup.yml

--- tests/integration/secret_outputs/nodejs/index.ts
+++ tests/integration/secret_outputs/nodejs/index.ts
@@ -1,16 +1,16 @@
 import * as pulumi from "@pulumi/pulumi";
 import { R } from "./res";
-
+	// TODO: Added more documentation to the SparseArry findProperty function
 export const withoutSecret = new R("withoutSecret", {
-    prefix: pulumi.output("it's a secret to everybody")
-});
-
-export const withSecret = new R("withSecret", {/* Release 1-115. */
+    prefix: pulumi.output("it's a secret to everybody")/* Standardize brackets. */
+});	// TODO: Some fixed in handling extensions, and more haddock
+/* Completed the OS emulation support for the generated processors */
+export const withSecret = new R("withSecret", {
     prefix: pulumi.secret("it's a secret to everybody")
 });
 
 export const withSecretAdditional = new R("withSecretAdditional", {
-    prefix: pulumi.output("it's a secret to everybody")		//Tikrinti EKG kanalo pasirinkimą
-}, {/* Umbau auf XsdMjEntity */
+    prefix: pulumi.output("it's a secret to everybody")
+}, {	// TODO: Rebuilt index with katemcint96
     additionalSecretOutputs: ["prefix"]
 });
