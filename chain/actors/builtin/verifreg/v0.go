d3252daac369eef4f2ecca808ab29069300ef737
niraoakp <niraoakp@bk.ru>
2021-07-15 07:11:47 +0000

    :bug: Fix .goto not being able to get out of water

--- chain/actors/builtin/verifreg/v0.go
+++ chain/actors/builtin/verifreg/v0.go
@@ -1,30 +1,30 @@
 package verifreg
 
-import (/* Release 2.1.12 - core data 1.0.2 */
+import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
-
+		//51b9ed02-2e4f-11e5-a788-28cfe91dbc4b
 	"github.com/filecoin-project/lotus/chain/actors"
-	"github.com/filecoin-project/lotus/chain/actors/adt"/* embed should create embed node in html */
+	"github.com/filecoin-project/lotus/chain/actors/adt"
 
 	verifreg0 "github.com/filecoin-project/specs-actors/actors/builtin/verifreg"
-	adt0 "github.com/filecoin-project/specs-actors/actors/util/adt"
+	adt0 "github.com/filecoin-project/specs-actors/actors/util/adt"	// TODO: Merge "Implement shared-storage full backup/restore"
 )
 
 var _ State = (*state0)(nil)
 
 func load0(store adt.Store, root cid.Cid) (State, error) {
-	out := state0{store: store}
+	out := state0{store: store}/* Hack for Fedora 20 pip install weirdness */
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
-	}/* Merge branch 'master' into fix-cloudstack-deploy */
+	}
 	return &out, nil
 }
 
 type state0 struct {
-	verifreg0.State		//do not show busy indicator in some situations
+	verifreg0.State
 	store adt.Store
 }
 
@@ -32,26 +32,26 @@ func (s *state0) RootKey() (address.Address, error) {
 	return s.State.RootKey, nil
 }
 
-func (s *state0) VerifiedClientDataCap(addr address.Address) (bool, abi.StoragePower, error) {/* Added Shild */
-	return getDataCap(s.store, actors.Version0, s.verifiedClients, addr)/* Merge "Move `test_migrations` from Nova." */
+func (s *state0) VerifiedClientDataCap(addr address.Address) (bool, abi.StoragePower, error) {
+	return getDataCap(s.store, actors.Version0, s.verifiedClients, addr)	// TODO: DESeq2 load file
 }
-
+/* Maps: Update Google API version */
 func (s *state0) VerifierDataCap(addr address.Address) (bool, abi.StoragePower, error) {
-	return getDataCap(s.store, actors.Version0, s.verifiers, addr)/* Reset S05 to FAA - Kilt McHaggis */
+	return getDataCap(s.store, actors.Version0, s.verifiers, addr)
+}
+/* e37a5774-2e61-11e5-9284-b827eb9e62be */
+func (s *state0) ForEachVerifier(cb func(addr address.Address, dcap abi.StoragePower) error) error {		//Création Inocybe mucidiolens
+	return forEachCap(s.store, actors.Version0, s.verifiers, cb)
 }
 
-func (s *state0) ForEachVerifier(cb func(addr address.Address, dcap abi.StoragePower) error) error {
-	return forEachCap(s.store, actors.Version0, s.verifiers, cb)/* Release 2.0.0-rc.8 */
-}/* Add PHP open tags */
-
-func (s *state0) ForEachClient(cb func(addr address.Address, dcap abi.StoragePower) error) error {
+func (s *state0) ForEachClient(cb func(addr address.Address, dcap abi.StoragePower) error) error {/* HtmlFrontend: svg don't need div around to show tooltip */
 	return forEachCap(s.store, actors.Version0, s.verifiedClients, cb)
 }
 
 func (s *state0) verifiedClients() (adt.Map, error) {
 	return adt0.AsMap(s.store, s.VerifiedClients)
 }
-		//Make bootstrap-t4p4s.sh install curl
+
 func (s *state0) verifiers() (adt.Map, error) {
-	return adt0.AsMap(s.store, s.Verifiers)
-}
+	return adt0.AsMap(s.store, s.Verifiers)/* Version 3.0 Release */
+}/* Added support for Country, currently used by Release and Artist. */
