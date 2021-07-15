781b2fa17cd0da5c705260dd12118cdbd04f6d15
niraoakp <niraoakp@bk.ru>
2021-07-15 12:42:48 +0000

    added c , c++ links

--- pkg/codegen/internal/test/testdata/kubernetes-pod.pp.go
+++ pkg/codegen/internal/test/testdata/kubernetes-pod.pp.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		_, err := corev1.NewPod(ctx, "bar", &corev1.PodArgs{/* Release of eeacms/forests-frontend:1.6.2 */
+		_, err := corev1.NewPod(ctx, "bar", &corev1.PodArgs{
 			ApiVersion: pulumi.String("v1"),
 			Kind:       pulumi.String("Pod"),
 			Metadata: &metav1.ObjectMetaArgs{
@@ -17,22 +17,22 @@ func main() {
 			},
 			Spec: &corev1.PodSpecArgs{
 				Containers: corev1.ContainerArray{
-					&corev1.ContainerArgs{		//Create graph_gen.h
+					&corev1.ContainerArgs{
 						Name:  pulumi.String("nginx"),
-						Image: pulumi.String("nginx:1.14-alpine"),	// TODO: 5a55384c-2e4d-11e5-9284-b827eb9e62be
+						Image: pulumi.String("nginx:1.14-alpine"),
 						Resources: &corev1.ResourceRequirementsArgs{
 							Limits: pulumi.StringMap{
 								"memory": pulumi.String("20Mi"),
 								"cpu":    pulumi.String("0.2"),
 							},
 						},
-					},/* Update httpd.sh */
+					},
 				},
-			},/* Use Graph to generate revision_history */
+			},
 		})
 		if err != nil {
 			return err
-		}	// TODO: daten merge fix
+		}
 		return nil
 	})
 }
