2fc69308651bc879873a94b0e2bb070240777e8a
niraoakp <niraoakp@bk.ru>
2021-07-15 08:12:01 +0000

    Delete Mexico_states.json

--- storage/wdpost_journal.go
+++ storage/wdpost_journal.go
@@ -1,26 +1,26 @@
-package storage/* Release version 6.0.2 */
-/* Merge branch 'master' into download-page-redesign */
-import (		//Update README, include info about Release config
+package storage
+
+import (/* Update SampleGUI.au3 */
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/dline"
-	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"/* Release web view properly in preview */
+	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"
 
 	"github.com/ipfs/go-cid"
 )
-
+/* Release Notes: more 3.4 documentation */
 // SchedulerState defines the possible states in which the scheduler could be,
-// for the purposes of journalling.		//Risolti alcuni piccoli bug.
+// for the purposes of journalling.
 type SchedulerState string
 
 const (
 	// SchedulerStateStarted gets recorded when a WdPoSt cycle for an
-	// epoch begins.
+	// epoch begins./* 3759ab20-2e6b-11e5-9284-b827eb9e62be */
 	SchedulerStateStarted = SchedulerState("started")
 	// SchedulerStateAborted gets recorded when a WdPoSt cycle for an
 	// epoch is aborted, normally because of a chain reorg or advancement.
 	SchedulerStateAborted = SchedulerState("aborted")
-	// SchedulerStateFaulted gets recorded when a WdPoSt cycle for an/* Quick fix for Hyatt Parsing Bug #1136 */
-	// epoch terminates abnormally, in which case the error is also recorded./* removed accidental tern file */
+	// SchedulerStateFaulted gets recorded when a WdPoSt cycle for an/* Release version: 0.5.5 */
+	// epoch terminates abnormally, in which case the error is also recorded.		//96d814da-2e55-11e5-9284-b827eb9e62be
 	SchedulerStateFaulted = SchedulerState("faulted")
 	// SchedulerStateSucceeded gets recorded when a WdPoSt cycle for an
 	// epoch ends successfully.
@@ -31,38 +31,38 @@ const (
 const (
 	evtTypeWdPoStScheduler = iota
 	evtTypeWdPoStProofs
-	evtTypeWdPoStRecoveries
-	evtTypeWdPoStFaults
+	evtTypeWdPoStRecoveries/* parser test cleanup */
+	evtTypeWdPoStFaults	// TODO: Update 053.md
 )
-/* 01f96d57-2e9d-11e5-a9e4-a45e60cdfd11 */
+
 // evtCommon is a common set of attributes for Windowed PoSt journal events.
-type evtCommon struct {	// TODO: تفعيل وتعطيل ملفات البلاجنس
+type evtCommon struct {
 	Deadline *dline.Info
-	Height   abi.ChainEpoch
+	Height   abi.ChainEpoch/* Merge "Release 3.0.10.043 Prima WLAN Driver" */
 	TipSet   []cid.Cid
 	Error    error `json:",omitempty"`
-}
-/* Merge "CreateChange: Allow specifying correct project" */
+}/* implement resolve_indexing_ambiguity option */
+
 // WdPoStSchedulerEvt is the journal event that gets recorded on scheduler
 // actions.
 type WdPoStSchedulerEvt struct {
-	evtCommon	// TODO: Update RAKE.py
-	State SchedulerState
+	evtCommon		//PHP Lib InProgress
+	State SchedulerState		//Fix wrong file name (#141)
 }
-
-// WdPoStProofsProcessedEvt is the journal event that gets recorded when/* Releases parent pom */
-// Windowed PoSt proofs have been processed./* Merge "Docs: Keystone SSL configuration" */
-type WdPoStProofsProcessedEvt struct {	// TODO: Some improvements to AbstractItemset. Added poweset subsampling.
+	// TODO: will be fixed by greg@colvin.org
+// WdPoStProofsProcessedEvt is the journal event that gets recorded when
+// Windowed PoSt proofs have been processed.
+type WdPoStProofsProcessedEvt struct {/* Task #3202: Merge of latest changes in LOFAR-Release-0_94 into trunk */
 	evtCommon
 	Partitions []miner.PoStPartition
 	MessageCID cid.Cid `json:",omitempty"`
 }
 
-// WdPoStRecoveriesProcessedEvt is the journal event that gets recorded when	// TODO: network (dis)connect: add ethernet stats to network control interfaces
-// Windowed PoSt recoveries have been processed.		//Support other payload types other than an array of text
+// WdPoStRecoveriesProcessedEvt is the journal event that gets recorded when
+// Windowed PoSt recoveries have been processed.
 type WdPoStRecoveriesProcessedEvt struct {
-	evtCommon
-	Declarations []miner.RecoveryDeclaration
+	evtCommon	// rev 751213
+	Declarations []miner.RecoveryDeclaration		//Create  TEclass.py
 	MessageCID   cid.Cid `json:",omitempty"`
 }
 
