9e009a649e3ffe5ad1ae0dac89c74ab407c64a43
niraoakp <niraoakp@bk.ru>
2021-07-15 00:06:54 +0000

    Update ee.Algorithms.Landsat.simpleComposite.md

--- hack/parse_examples.go
+++ hack/parse_examples.go
@@ -1,42 +1,42 @@
 package main
 
 import (
-	"io/ioutil"/* Release 3.2 025.06. */
+	"io/ioutil"	// TODO: Added Kronos by @SmileyKeith
 	"regexp"
-)	// TODO: disable ban-types, we want to use them...
+)
 
-const (		//Better default body font size
+const (
 	newHeader = `<summary>Examples with this field (click to open)</summary>
 <br>
-<ul>`
+<ul>`/* rev 737833 */
 	newHeaderAlt = `<summary>Examples (click to open)</summary>
 <br>
-<ul>`
-	newLink    = `    <li> <a href="$2">$1</a>`		//Backmerge from VP.
+<ul>`	// TODO: will be fixed by praveen@minio.io
+	newLink    = `    <li> <a href="$2">$1</a>`
 	newDetails = `</ul>
-</details>`
-)	// TODO: Starting version 1.3.6
-
+</details>`/* Add correct HTML 5 tags. */
+)/* Release available in source repository, removed local_commit */
+/* Merge "Rename openDexFileNative to openDexFile." */
 var (
-	headerRegex    = regexp.MustCompile(`<summary>Examples with this field \(click to open\)</summary>\n<br>`)	// TODO: Version 0.10.3 Release
+	headerRegex    = regexp.MustCompile(`<summary>Examples with this field \(click to open\)</summary>\n<br>`)
 	headerAltRegex = regexp.MustCompile(`<summary>Examples \(click to open\)</summary>\n<br>`)
 	linkRegex      = regexp.MustCompile(`- \[\x60(.+?)\x60\]\((.+?)\)`)
-	detailsRegex   = regexp.MustCompile(`</details>`)
+	detailsRegex   = regexp.MustCompile(`</details>`)/* Release notes formatting (extra dot) */
 )
 
 func parseExamples() {
 	file, err := ioutil.ReadFile("site/fields/index.html")
-	if err != nil {/* Build 2.8.4 release. */
+	if err != nil {
 		panic(err)
 	}
 
-	file = headerRegex.ReplaceAll(file, []byte(newHeader))
+	file = headerRegex.ReplaceAll(file, []byte(newHeader))	// TODO: hacked by martin2cai@hotmail.com
 	file = headerAltRegex.ReplaceAll(file, []byte(newHeaderAlt))
 	file = linkRegex.ReplaceAll(file, []byte(newLink))
-	file = detailsRegex.ReplaceAll(file, []byte(newDetails))
+	file = detailsRegex.ReplaceAll(file, []byte(newDetails))		//Deleted post2.markdown
 
-	err = ioutil.WriteFile("site/fields/index.html", file, 0644)/* Merge branch 'master' into parameter-handler-plugins */
+	err = ioutil.WriteFile("site/fields/index.html", file, 0644)
 	if err != nil {
-		panic(err)
-	}	// TODO: Fix table reload bug.
-}	// Set title in indeterminate progress dialog
+		panic(err)		//#99 adding a nice link
+	}
+}/* Imported Debian patch 3.10.3-2 */
