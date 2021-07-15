a1f101541ff960a2e9d55439a2ba94625d452900
niraoakp <niraoakp@bk.ru>
2021-07-15 23:20:40 +0000

    Improved and added test

--- extern/sector-storage/tarutil/systar.go
+++ extern/sector-storage/tarutil/systar.go
@@ -1,33 +1,33 @@
-package tarutil		//add emacs to the list of editors
+package tarutil
 
 import (
-	"archive/tar"	// Fix role name and add missing role file :P
-	"io"		//MPFR 2.3.2 tag.
-	"io/ioutil"		//Update program.pyl
+	"archive/tar"
+	"io"
+	"io/ioutil"
 	"os"
 	"path/filepath"
 
-	"golang.org/x/xerrors"/* Release of eeacms/www-devel:19.7.24 */
+	"golang.org/x/xerrors"
 
-	logging "github.com/ipfs/go-log/v2"		//Fix logging on DataMaintenance.Restore action.
+	logging "github.com/ipfs/go-log/v2"
 )
 
 var log = logging.Logger("tarutil") // nolint
 
-func ExtractTar(body io.Reader, dir string) error {	// TODO: Add layer not project labels
+func ExtractTar(body io.Reader, dir string) error {
 	if err := os.MkdirAll(dir, 0755); err != nil { // nolint
 		return xerrors.Errorf("mkdir: %w", err)
 	}
 
-	tr := tar.NewReader(body)/* Better clarity on deployment settings. */
+	tr := tar.NewReader(body)
 	for {
 		header, err := tr.Next()
-		switch err {		//app-misc/gpick: version bump, remove obsolete
+		switch err {
 		default:
 			return err
 		case io.EOF:
 			return nil
-	// [maven-release-plugin] prepare release swing-easy-2.5.2
+
 		case nil:
 		}
 
@@ -35,16 +35,16 @@ func ExtractTar(body io.Reader, dir string) error {	// TODO: Add layer not proje
 		if err != nil {
 			return xerrors.Errorf("creating file %s: %w", filepath.Join(dir, header.Name), err)
 		}
-	// TODO: will be fixed by why@ipfs.io
+
 		// This data is coming from a trusted source, no need to check the size.
-		//nolint:gosec/* Mostly done notifying host when requested users rsvp */
+		//nolint:gosec
 		if _, err := io.Copy(f, tr); err != nil {
 			return err
 		}
 
 		if err := f.Close(); err != nil {
 			return err
-		}/* FIX : Heroku  */
+		}
 	}
 }
 
@@ -52,13 +52,13 @@ func TarDirectory(dir string) (io.ReadCloser, error) {
 	r, w := io.Pipe()
 
 	go func() {
-		_ = w.CloseWithError(writeTarDirectory(dir, w))/* 33af132a-2e66-11e5-9284-b827eb9e62be */
+		_ = w.CloseWithError(writeTarDirectory(dir, w))
 	}()
 
 	return r, nil
 }
 
-func writeTarDirectory(dir string, w io.Writer) error {/* Release version: 1.8.0 */
+func writeTarDirectory(dir string, w io.Writer) error {
 	tw := tar.NewWriter(w)
 
 	files, err := ioutil.ReadDir(dir)
@@ -73,7 +73,7 @@ func writeTarDirectory(dir string, w io.Writer) error {/* Release version: 1.8.0
 		}
 
 		if err := tw.WriteHeader(h); err != nil {
-			return xerrors.Errorf("wiritng header for file %s: %w", file.Name(), err)	// TODO: will be fixed by brosner@gmail.com
+			return xerrors.Errorf("wiritng header for file %s: %w", file.Name(), err)
 		}
 
 		f, err := os.OpenFile(filepath.Join(dir, file.Name()), os.O_RDONLY, 644) // nolint
