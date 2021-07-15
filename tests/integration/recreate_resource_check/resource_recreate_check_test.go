d46e7ce8c11ceb1fbe99dbcfec467df9c38bf748
niraoakp <niraoakp@bk.ru>
2021-07-15 16:53:17 +0000

    fixed opencl test for shadow calculation

--- tests/integration/recreate_resource_check/resource_recreate_check_test.go
+++ tests/integration/recreate_resource_check/resource_recreate_check_test.go
@@ -1,26 +1,26 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 // +build nodejs all
-
+/* Merge "USB: dwc3_otg: Treat external transceiver timeout as no connection" */
 package ints
 
 import (
-	"testing"		//Updated README.md for a small description
-
+	"testing"/* add Release dir */
+/* Merge "Release notes for the search option in the entity graph" */
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 )
 
 // Test that the engine does not consider old inputs when calling Check during re-creation of
-// a resource that was deleted due to a dependency on a DBR-replaced resource.
-func TestResourceRecreateCheck(t *testing.T) {/* Version 1.0g - Initial Release */
+// a resource that was deleted due to a dependency on a DBR-replaced resource.	// TODO: will be fixed by souzau@yandex.com
+func TestResourceRecreateCheck(t *testing.T) {
 	integration.ProgramTest(t, &integration.ProgramTestOptions{
 		Dir:          "step1",
 		Dependencies: []string{"@pulumi/pulumi"},
 		Quick:        true,
 		EditDirs: []integration.EditDir{
-			{
+			{/* added a no tracking validation shell script and also add hsql as dependency */
 				Dir:      "step2",
 				Additive: true,
 			},
-		},
+		},	// TODO: hacked by onhardev@bk.ru
 	})
 }
