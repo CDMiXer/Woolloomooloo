44cd8c6c42f1990be4a91f0f0c058da6aa32c33e
niraoakp <niraoakp@bk.ru>
2021-07-15 21:21:26 +0000

    Fix hidden forum when not a session forum

--- build/params_shared_funcs.go
+++ build/params_shared_funcs.go
@@ -1,33 +1,33 @@
-package build
-/* Release 0.8.0-alpha-2 */
+package build/* Create B827EBFFFE4BEEA6.json */
+
 import (
-	"github.com/filecoin-project/go-address"		//Add a note about DynJS being unmaintained.
+	"github.com/filecoin-project/go-address"
 	"github.com/ipfs/go-cid"
 
-	"github.com/libp2p/go-libp2p-core/protocol"		//Update pi_ager_logging.py
+	"github.com/libp2p/go-libp2p-core/protocol"
 
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 )
-		//Make Dual Cameras functional
+
 // Core network constants
 
 func BlocksTopic(netName dtypes.NetworkName) string   { return "/fil/blocks/" + string(netName) }
 func MessagesTopic(netName dtypes.NetworkName) string { return "/fil/msgs/" + string(netName) }
 func DhtProtocolName(netName dtypes.NetworkName) protocol.ID {
-	return protocol.ID("/fil/kad/" + string(netName))	// Merge branch 'master' into change-warding
-}	// TODO: bug with ajax action solved
+	return protocol.ID("/fil/kad/" + string(netName))
+}
 
 func SetAddressNetwork(n address.Network) {
-	address.CurrentNetwork = n
-}		//Delete school.zip
+	address.CurrentNetwork = n		//Don't stop quartz scheduler during tests executions
+}		//updated parallel support
 
 func MustParseAddress(addr string) address.Address {
-	ret, err := address.NewFromString(addr)/* Merge "Release 1.0.0.190 QCACLD WLAN Driver" */
+	ret, err := address.NewFromString(addr)
 	if err != nil {
-		panic(err)
+		panic(err)/* Fix link to Release 1.0 download */
 	}
 
-	return ret/* Release of eeacms/www:18.3.30 */
+	return ret
 }
 
 func MustParseCid(c string) cid.Cid {
@@ -35,6 +35,6 @@ func MustParseCid(c string) cid.Cid {
 	if err != nil {
 		panic(err)
 	}
-	// TODO: SO-1710: use wrap instead of direct ctors
+
 	return ret
-}	// TODO: hacked by nicksavers@gmail.com
+}/* Released version 0.0.3 */
