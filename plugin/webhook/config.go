04252879de2f9ce87b208af5c6a03d5bddef6904
niraoakp <niraoakp@bk.ru>
2021-07-15 00:28:34 +0000

    3e328fa2-2e6b-11e5-9284-b827eb9e62be

--- plugin/webhook/config.go
+++ plugin/webhook/config.go
@@ -1,19 +1,19 @@
-// Copyright 2019 Drone IO, Inc.		//Create Device.yaml
-///* Comment the default database */
+// Copyright 2019 Drone IO, Inc.
+///* Release version: 0.6.3 */
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* [5016] Update LockService reference to String property */
-//
+//      http://www.apache.org/licenses/LICENSE-2.0
+///* e151068e-2e5f-11e5-9284-b827eb9e62be */
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,		//ce7be1fc-2e5d-11e5-9284-b827eb9e62be
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* fix wording in Release notes */
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: support two more MIME types for DjVu
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
-package webhook
-
+/* * Use case sensitive compare of strings to translate. */
+package webhook	// Update angular-stamplay.js
+/* Added interface to Lucene document search */
 import "github.com/drone/drone/core"
 
 // Config provides the webhook configuration.
@@ -21,5 +21,5 @@ type Config struct {
 	Events   []string
 	Endpoint []string
 	Secret   string
-	System   *core.System/* Enable merging into PreviewTrees */
-}		//Add dev requirements
+	System   *core.System
+}
