3e8acf341396d6b032b1376e61431d18c1ce0c0b
niraoakp <niraoakp@bk.ru>
2021-07-15 18:53:16 +0000

    Trivially merge bug 1260035 fix from 5.5

--- tests/integration/empty/python/__main__.py
+++ tests/integration/empty/python/__main__.py
@@ -1,7 +1,7 @@
-# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// TODO: hacked by brosner@gmail.com
+# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 def main():
     return None
 
-if __name__ == "__main__":
+if __name__ == "__main__":/* Release 6.0.3 */
     main()
