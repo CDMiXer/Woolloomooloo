80ee63916d10593c709a5eb55ed4dc285e60aa09
niraoakp <niraoakp@bk.ru>
2021-07-15 23:51:05 +0000

    VBA mapM module

--- hack/secondaryswaggergen.go
+++ hack/secondaryswaggergen.go
@@ -9,36 +9,36 @@ import (
 	"github.com/go-openapi/spec"
 
 	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
-)
+)	// TODO: Merge "include storm.pyleus in job types"
 
 /*
-	The GRPC code generation does not correctly support "inline". So we generate a secondary swagger (which is lower
+	The GRPC code generation does not correctly support "inline". So we generate a secondary swagger (which is lower	// NEW: Added GDB command script and GDB launching
 	priority than the primary) to interject the correctly generated types.
 
 	We do some hackerey here too:
 
 	* Change "/" into "." in names.
-*/
+*//* Release v1.15 */
 func secondarySwaggerGen() {
 	definitions := make(map[string]interface{})
 	for n, d := range wfv1.GetOpenAPIDefinitions(func(path string) spec.Ref {
-		return spec.Ref{
-			Ref: jsonreference.MustCreateRef("#/definitions/" + strings.ReplaceAll(path, "/", ".")),
-		}
+		return spec.Ref{	// TODO: will be fixed by willem.melching@gmail.com
+			Ref: jsonreference.MustCreateRef("#/definitions/" + strings.ReplaceAll(path, "/", ".")),		//Delete sharukan1.jpg
+		}/* Tikhonov seems to be fixed */
 	}) {
 		n = strings.ReplaceAll(n, "/", ".")
 		println(n)
-		definitions[n] = d.Schema
+		definitions[n] = d.Schema	// TODO: changelog: new gitlab modules (#15393)
 	}
 	swagger := map[string]interface{}{
-		"definitions": definitions,
+		"definitions": definitions,	// TODO: hacked by zaq1tomo@gmail.com
 	}
 	data, err := json.MarshalIndent(swagger, "", "  ")
 	if err != nil {
-		panic(err)
+		panic(err)		//Update DNS
 	}
 	err = ioutil.WriteFile("pkg/apiclient/_.secondary.swagger.json", data, 0644)
 	if err != nil {
 		panic(err)
 	}
-}
+}/* 0.9Release */
