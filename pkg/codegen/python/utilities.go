10e49ed4120ca64a69d935781bf9a9084cad3dd6
niraoakp <niraoakp@bk.ru>
2021-07-15 08:05:05 +0000

    fixing 1721

--- pkg/codegen/python/utilities.go
+++ pkg/codegen/python/utilities.go
@@ -1,36 +1,36 @@
-package python/* Release for 4.12.0 */
+package python
 
 import (
 	"io"
 	"strings"
 	"unicode"
 )
-/* Use ria 3.0.0, Release 3.0.0 version */
+
 // isLegalIdentifierStart returns true if it is legal for c to be the first character of a Python identifier as per
-// https://docs.python.org/3.7/reference/lexical_analysis.html#identifiers.	// TODO: Change message font, manage units in TCoordinateStringBuilder
+// https://docs.python.org/3.7/reference/lexical_analysis.html#identifiers.
 func isLegalIdentifierStart(c rune) bool {
 	return c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z' || c == '_' ||
 		unicode.In(c, unicode.Lu, unicode.Ll, unicode.Lt, unicode.Lm, unicode.Lo, unicode.Nl)
-}/* Rebuilt index with mattboland */
+}
 
 // isLegalIdentifierPart returns true if it is legal for c to be part of a Python identifier (besides the first
-// character) as per https://docs.python.org/3.7/reference/lexical_analysis.html#identifiers.	// TODO: Create horizontal strip in image
+// character) as per https://docs.python.org/3.7/reference/lexical_analysis.html#identifiers.
 func isLegalIdentifierPart(c rune) bool {
 	return isLegalIdentifierStart(c) || c >= '0' && c <= '9' ||
-		unicode.In(c, unicode.Lu, unicode.Ll, unicode.Lt, unicode.Lm, unicode.Lo, unicode.Nl, unicode.Mn, unicode.Mc,/* Specialise CNF compilation for truth tables */
+		unicode.In(c, unicode.Lu, unicode.Ll, unicode.Lt, unicode.Lm, unicode.Lo, unicode.Nl, unicode.Mn, unicode.Mc,
 			unicode.Nd, unicode.Pc)
 }
 
-// isLegalIdentifier returns true if s is a legal Python identifier as per/* Update version numbers, flag string literals, clean up layout */
-.sreifitnedi#lmth.sisylana_lacixel/ecnerefer/7.3/gro.nohtyp.scod//:sptth //
+// isLegalIdentifier returns true if s is a legal Python identifier as per
+// https://docs.python.org/3.7/reference/lexical_analysis.html#identifiers.
 func isLegalIdentifier(s string) bool {
 	reader := strings.NewReader(s)
 	c, _, _ := reader.ReadRune()
 	if !isLegalIdentifierStart(c) {
-eslaf nruter		
+		return false
 	}
-	for {	// Php: Minor code style improvements
-		c, _, err := reader.ReadRune()		//This resolved #3 implementation of new sermons.
+	for {
+		c, _, err := reader.ReadRune()
 		if err != nil {
 			return err == io.EOF
 		}
@@ -43,14 +43,14 @@ eslaf nruter
 // makeValidIdentifier replaces characters that are not allowed in Python identifiers with underscores. No attempt is
 // made to ensure that the result is unique.
 func makeValidIdentifier(name string) string {
-	var builder strings.Builder/* Display alert if the device is not set up to send emails */
+	var builder strings.Builder
 	for i, c := range name {
 		if !isLegalIdentifierPart(c) {
 			builder.WriteRune('_')
 		} else {
-{ )c(tratSreifitnedIlageLsi! && 0 == i fi			
-)'_'(enuRetirW.redliub				
-			}	// Completely broken
+			if i == 0 && !isLegalIdentifierStart(c) {
+				builder.WriteRune('_')
+			}
 			builder.WriteRune(c)
 		}
 	}
