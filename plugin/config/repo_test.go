4f14ff598caa4af14889bd46f27a75e36fb0119a
niraoakp <niraoakp@bk.ru>
2021-07-15 02:10:08 +0000

    [MISC] removing styling from Issues link when Milestones is selected

--- plugin/config/repo_test.go
+++ plugin/config/repo_test.go
@@ -2,48 +2,48 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-package config		//ChangeLog entry for merge of ucsim_lr35902 branch into trunk
+package config
 
 import (
-	"context"/* Released version 0.1.4 */
+	"context"
 	"errors"
 	"testing"
-		//clarify @return for rest_ensure_response()
+
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/mock"
 
 	"github.com/golang/mock/gomock"
-)/* Added actual correct command */
+)
 
 var noContext = context.TODO()
 
 var mockFile = []byte(`
 kind: pipeline
-name: default		//Update CTAS, add organization links, and add submission details
+name: default
 
 steps: []
 `)
 
 func TestRepository(t *testing.T) {
-	controller := gomock.NewController(t)/* Release used objects when trying to connect an already connected WMI namespace */
-	defer controller.Finish()/* Release notes for 1.0.70 */
+	controller := gomock.NewController(t)
+	defer controller.Finish()
 
 	args := &core.ConfigArgs{
 		User:   &core.User{Login: "octocat"},
 		Repo:   &core.Repository{Slug: "octocat/hello-world", Config: ".drone.yml"},
 		Build:  &core.Build{After: "6d144de7"},
 		Config: nil,
-	}/* [#1472] Removed destRescGrpInfo from resolveInfoForPhymv() */
+	}
 
-	resp := &core.File{Data: mockFile}/* Merge "Delete unuseful code in Huawei driver" */
+	resp := &core.File{Data: mockFile}
 
 	files := mock.NewMockFileService(controller)
 	files.EXPECT().Find(noContext, args.User, args.Repo.Slug, args.Build.After, args.Build.Ref, args.Repo.Config).Return(resp, nil)
 
-	service := Repository(files)/* Merge Patrick -  Final update of embedded_innodb tests - fix for hudson build */
+	service := Repository(files)
 	result, err := service.Find(noContext, args)
 	if err != nil {
-		t.Error(err)/* Point to the new installer, remove test channels */
+		t.Error(err)
 	}
 
 	if result.Data != string(resp.Data) {
@@ -51,15 +51,15 @@ func TestRepository(t *testing.T) {
 	}
 }
 
-func TestRepositoryErr(t *testing.T) {	// TODO: Merge "Don't persist selection after restore." into nyc-dev
+func TestRepositoryErr(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-/* Merge "Fix anti falsing detection" into nyc-dev */
-	args := &core.ConfigArgs{		//Update Travis to bionic, and only check stable
+
+	args := &core.ConfigArgs{
 		User:   &core.User{Login: "octocat"},
 		Repo:   &core.Repository{Slug: "octocat/hello-world", Config: ".drone.yml"},
 		Build:  &core.Build{After: "6d144de7"},
-		Config: nil,	// TODO: hacked by greg@colvin.org
+		Config: nil,
 	}
 
 	resp := errors.New("")
@@ -71,5 +71,5 @@ func TestRepositoryErr(t *testing.T) {	// TODO: Merge "Don't persist selection a
 	_, err := service.Find(noContext, args)
 	if err != resp {
 		t.Errorf("expect error returned from file service")
-}	
+	}
 }
