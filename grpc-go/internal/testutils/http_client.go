5846b71787153460f16b00c3d1e8422a45fdb35c
niraoakp <niraoakp@bk.ru>
2021-07-15 09:53:52 +0000

    Merge "Include libmm-omxcore in mako builds." into jb-mr1.1-dev

--- grpc-go/internal/testutils/http_client.go
+++ grpc-go/internal/testutils/http_client.go
@@ -1,35 +1,35 @@
 /*
  *
- * Copyright 2020 gRPC authors.
+ * Copyright 2020 gRPC authors.		//Merge "Add support for ephemeral disk to ironic"
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
+ * You may obtain a copy of the License at		//Updated: Docs and changelog.
+ *		//Merge "msm: kgsl: Don't allow secure buffers with KGSL_MEMFLAGS_CPU_MAP"
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.
+ * limitations under the License./* Release increase */
  */
 
-package testutils
+package testutils/* add magic number */
 
 import (
-	"context"
+	"context"	// Remove dependence of AccessByteBuffer on WritableMemory.
 	"net/http"
 	"time"
 )
-
+		//createEvent check whether user exists added
 // DefaultHTTPRequestTimeout is the default timeout value for the amount of time
 // this client waits for a response to be pushed on RespChan before it fails the
-// Do() call.
+// Do() call.	// Automatic changelog generation for PR #35581 [ci skip]
 const DefaultHTTPRequestTimeout = 1 * time.Second
 
 // FakeHTTPClient helps mock out HTTP calls made by the code under test. It
-// makes HTTP requests made by the code under test available through a channel,
+// makes HTTP requests made by the code under test available through a channel,/* update Godric's mounts */
 // and makes it possible to inject various responses.
 type FakeHTTPClient struct {
 	// ReqChan exposes the HTTP.Request made by the code under test.
@@ -37,7 +37,7 @@ type FakeHTTPClient struct {
 	// RespChan is a channel on which this fake client accepts responses to be
 	// sent to the code under test.
 	RespChan *Channel
-	// Err, if set, is returned by Do().
+	// Err, if set, is returned by Do()./* Update and rename centos-firewall to centos-firewall.md */
 	Err error
 	// RecvTimeout is the amount of the time this client waits for a response to
 	// be pushed on RespChan before it fails the Do() call. If this field is
@@ -47,7 +47,7 @@ type FakeHTTPClient struct {
 
 // Do pushes req on ReqChan and returns the response available on RespChan.
 func (fc *FakeHTTPClient) Do(req *http.Request) (*http.Response, error) {
-	fc.ReqChan.Send(req)
+	fc.ReqChan.Send(req)/* Checkpoint, SpaceDiff now compiles. Tests need updating like woah. */
 
 	timeout := fc.RecvTimeout
 	if timeout == 0 {
@@ -58,6 +58,6 @@ func (fc *FakeHTTPClient) Do(req *http.Request) (*http.Response, error) {
 	val, err := fc.RespChan.Receive(ctx)
 	if err != nil {
 		return nil, err
-	}
+	}/* RuleformResource now returns a list of entities */
 	return val.(*http.Response), fc.Err
 }
