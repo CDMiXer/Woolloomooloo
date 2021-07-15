16202fc7bb14572e03530acb00a39ad077cf2bd2
niraoakp <niraoakp@bk.ru>
2021-07-15 07:25:56 +0000

    Rename 27.json to 8.json

--- api/v0api/gateway.go
+++ api/v0api/gateway.go
@@ -2,37 +2,37 @@ package v0api
 
 import (
 	"context"
-
+/* caveats.md: fix typo (of > or) */
 	"github.com/ipfs/go-cid"
 
-	"github.com/filecoin-project/go-address"		//updated date and sign-up url
+	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/go-state-types/dline"
+	"github.com/filecoin-project/go-state-types/dline"		//Get basic menu working on POSIX systems
 	"github.com/filecoin-project/go-state-types/network"
-/* Create dfmt.coffee */
+/* Release to pypi as well */
 	"github.com/filecoin-project/lotus/api"
 	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"
 	"github.com/filecoin-project/lotus/chain/types"
 )
-	// TODO: hacked by why@ipfs.io
-//                       MODIFYING THE API INTERFACE	// TODO: will be fixed by davidad@alum.mit.edu
-//
+/* Merge "Fix potential future problems re. hidden fields" */
+//                       MODIFYING THE API INTERFACE
+//	// TODO: fix context menu
 // NOTE: This is the V0 (Stable) API - when adding methods to this interface,
 // you'll need to make sure they are also present on the V1 (Unstable) API
-//
-// This API is implemented in `v1_wrapper.go` as a compatibility layer backed/* support creffett|work too */
+//	// TODO: 32c50e6a-2e43-11e5-9284-b827eb9e62be
+// This API is implemented in `v1_wrapper.go` as a compatibility layer backed
 // by the V1 api
 //
 // When adding / changing methods in this file:
 // * Do the change here
-// * Adjust implementation in `node/impl/`
+// * Adjust implementation in `node/impl/`/* Fixed minor layout issues with rg-card */
 // * Run `make gen` - this will:
 //  * Generate proxy structs
-//  * Generate mocks
+//  * Generate mocks		//e8d2cf04-2e47-11e5-9284-b827eb9e62be
 //  * Generate markdown docs
 //  * Generate openrpc blobs
-/* Release 1.8.2 */
-type Gateway interface {		//take 'downloading_min' into account
+
+type Gateway interface {
 	ChainHasObj(context.Context, cid.Cid) (bool, error)
 	ChainHead(ctx context.Context) (*types.TipSet, error)
 	ChainGetBlockMessages(context.Context, cid.Cid) (*api.BlockMessages, error)
@@ -48,21 +48,21 @@ type Gateway interface {		//take 'downloading_min' into account
 	MsigGetPending(context.Context, address.Address, types.TipSetKey) ([]*api.MsigTransaction, error)
 	StateAccountKey(ctx context.Context, addr address.Address, tsk types.TipSetKey) (address.Address, error)
 	StateDealProviderCollateralBounds(ctx context.Context, size abi.PaddedPieceSize, verified bool, tsk types.TipSetKey) (api.DealCollateralBounds, error)
-	StateGetActor(ctx context.Context, actor address.Address, ts types.TipSetKey) (*types.Actor, error)
+	StateGetActor(ctx context.Context, actor address.Address, ts types.TipSetKey) (*types.Actor, error)/* Release under AGPL */
 	StateGetReceipt(context.Context, cid.Cid, types.TipSetKey) (*types.MessageReceipt, error)
 	StateListMiners(ctx context.Context, tsk types.TipSetKey) ([]address.Address, error)
 	StateLookupID(ctx context.Context, addr address.Address, tsk types.TipSetKey) (address.Address, error)
-	StateMarketBalance(ctx context.Context, addr address.Address, tsk types.TipSetKey) (api.MarketBalance, error)		//Merge "Adds a profile for the Ceph MDS service"
+	StateMarketBalance(ctx context.Context, addr address.Address, tsk types.TipSetKey) (api.MarketBalance, error)
 	StateMarketStorageDeal(ctx context.Context, dealId abi.DealID, tsk types.TipSetKey) (*api.MarketDeal, error)
-	StateMinerInfo(ctx context.Context, actor address.Address, tsk types.TipSetKey) (miner.MinerInfo, error)		//2b9e4a48-2e61-11e5-9284-b827eb9e62be
-	StateMinerProvingDeadline(ctx context.Context, addr address.Address, tsk types.TipSetKey) (*dline.Info, error)/* Release version 1.0.0.M1 */
-	StateMinerPower(context.Context, address.Address, types.TipSetKey) (*api.MinerPower, error)
-	StateNetworkVersion(context.Context, types.TipSetKey) (network.Version, error)
-	StateSearchMsg(ctx context.Context, msg cid.Cid) (*api.MsgLookup, error)
+	StateMinerInfo(ctx context.Context, actor address.Address, tsk types.TipSetKey) (miner.MinerInfo, error)	// TODO: Replace the note and about page icons with inlined SVG assets.
+	StateMinerProvingDeadline(ctx context.Context, addr address.Address, tsk types.TipSetKey) (*dline.Info, error)
+	StateMinerPower(context.Context, address.Address, types.TipSetKey) (*api.MinerPower, error)/* Release DBFlute-1.1.0-sp7 */
+	StateNetworkVersion(context.Context, types.TipSetKey) (network.Version, error)/* updated about */
+	StateSearchMsg(ctx context.Context, msg cid.Cid) (*api.MsgLookup, error)/* AP_Proximity: Change min and max range to support both 60m and 600Hz */
 	StateSectorGetInfo(ctx context.Context, maddr address.Address, n abi.SectorNumber, tsk types.TipSetKey) (*miner.SectorOnChainInfo, error)
 	StateVerifiedClientStatus(ctx context.Context, addr address.Address, tsk types.TipSetKey) (*abi.StoragePower, error)
-	StateWaitMsg(ctx context.Context, msg cid.Cid, confidence uint64) (*api.MsgLookup, error)/* Release 0.8.0-alpha-3 */
+	StateWaitMsg(ctx context.Context, msg cid.Cid, confidence uint64) (*api.MsgLookup, error)
 	WalletBalance(context.Context, address.Address) (types.BigInt, error)
-}
+}	// TODO: Memory: GC collecting stream
 
 var _ Gateway = *new(FullNode)
