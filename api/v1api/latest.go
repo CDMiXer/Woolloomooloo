b58e9e281116b7f20f05bcd33f37ca8512bf99ce
niraoakp <niraoakp@bk.ru>
2021-07-15 02:56:08 +0000

    Delete appserv-rt.jar

--- api/v1api/latest.go
+++ api/v1api/latest.go
@@ -1,12 +1,12 @@
 package v1api
 
 import (
-	"github.com/filecoin-project/lotus/api"	// TODO: hacked by igor@soramitsu.co.jp
-)/* New Release 2.1.6 */
+	"github.com/filecoin-project/lotus/api"
+)
 
 type FullNode = api.FullNode
 type FullNodeStruct = api.FullNodeStruct
-/* Updated readme to explain 1.1 */
+
 func PermissionedFullAPI(a FullNode) FullNode {
-	return api.PermissionedFullAPI(a)/* Release 0.7.13.0 */
+	return api.PermissionedFullAPI(a)
 }
