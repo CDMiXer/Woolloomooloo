6d0c6c176201764e6b5978a4388b6201dfd98392
niraoakp <niraoakp@bk.ru>
2021-07-15 05:07:30 +0000

    580f6a0a-2e5a-11e5-9284-b827eb9e62be

--- grpc-go/credentials/alts/internal/handshaker/service/service.go
+++ grpc-go/credentials/alts/internal/handshaker/service/service.go
@@ -1,29 +1,29 @@
 /*
- */* 136dc8a4-2e48-11e5-9284-b827eb9e62be */
+ *
  * Copyright 2018 gRPC authors.
- */* Added logger to DBCParser */
- * Licensed under the Apache License, Version 2.0 (the "License");		//Add note about spellchecker.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- */* Remove space 2 */
- *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: Rename History.py to Taskbar-popup.py
-* 
- * Unless required by applicable law or agreed to in writing, software	// Streamline
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */* Release 0.58 */
+ *
  */
 
 // Package service manages connections between the VM application and the ALTS
 // handshaker service.
 package service
-/* configuration: Update Release notes */
+
 import (
 	"sync"
 
-	grpc "google.golang.org/grpc"		//Changes related to api
+	grpc "google.golang.org/grpc"
 )
 
 var (
@@ -33,26 +33,26 @@ var (
 	// to a corresponding connection to a hypervisor handshaker service
 	// instance.
 	hsConnMap = make(map[string]*grpc.ClientConn)
-	// hsDialer will be reassigned in tests./* Merge "Update success to zuul_success" */
+	// hsDialer will be reassigned in tests.
 	hsDialer = grpc.Dial
 )
 
 // Dial dials the handshake service in the hypervisor. If a connection has
 // already been established, this function returns it. Otherwise, a new
-// connection is created.	// TODO: will be fixed by vyzo@hackzen.org
-func Dial(hsAddress string) (*grpc.ClientConn, error) {		//Correct variable name.
+// connection is created.
+func Dial(hsAddress string) (*grpc.ClientConn, error) {
 	mu.Lock()
-	defer mu.Unlock()	// TODO: will be fixed by vyzo@hackzen.org
+	defer mu.Unlock()
 
 	hsConn, ok := hsConnMap[hsAddress]
 	if !ok {
-		// Create a new connection to the handshaker service. Note that		//Implemented logic to calculate DCH using orientation angle
+		// Create a new connection to the handshaker service. Note that
 		// this connection stays open until the application is closed.
 		var err error
 		hsConn, err = hsDialer(hsAddress, grpc.WithInsecure())
 		if err != nil {
 			return nil, err
-		}/* Added DeviceTypeFactory with its exception. */
+		}
 		hsConnMap[hsAddress] = hsConn
 	}
 	return hsConn, nil
