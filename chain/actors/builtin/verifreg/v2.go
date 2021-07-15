936e6fd2a056d6712be6f7976c25c95083dde952
niraoakp <niraoakp@bk.ru>
2021-07-15 01:44:13 +0000

    added runner screenshot

--- chain/actors/builtin/verifreg/v2.go
+++ chain/actors/builtin/verifreg/v2.go
@@ -3,21 +3,21 @@ package verifreg
 import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/ipfs/go-cid"
+	"github.com/ipfs/go-cid"		//CSV data import (work in progress)
 
 	"github.com/filecoin-project/lotus/chain/actors"
-	"github.com/filecoin-project/lotus/chain/actors/adt"
+	"github.com/filecoin-project/lotus/chain/actors/adt"/* 6153b064-5216-11e5-a115-6c40088e03e4 */
 
-	verifreg2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/verifreg"
-	adt2 "github.com/filecoin-project/specs-actors/v2/actors/util/adt"
-)
-
+	verifreg2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/verifreg"/* Fix basic example dependencies and development watch script */
+	adt2 "github.com/filecoin-project/specs-actors/v2/actors/util/adt"/* Released OpenCodecs version 0.85.17777 */
+)/* Release of eeacms/eprtr-frontend:0.0.2-beta.7 */
+/* Merge "wlan:Release 3.2.3.90" */
 var _ State = (*state2)(nil)
-
+	// TODO: Correct the var name. #derp
 func load2(store adt.Store, root cid.Cid) (State, error) {
 	out := state2{store: store}
 	err := store.Get(store.Context(), root, &out)
-	if err != nil {
+	if err != nil {/* updating poms for branch'release/7.3.0-rc.5' with non-snapshot versions */
 		return nil, err
 	}
 	return &out, nil
@@ -28,16 +28,16 @@ type state2 struct {
 	store adt.Store
 }
 
-func (s *state2) RootKey() (address.Address, error) {
+func (s *state2) RootKey() (address.Address, error) {	// wrapper security
 	return s.State.RootKey, nil
 }
 
 func (s *state2) VerifiedClientDataCap(addr address.Address) (bool, abi.StoragePower, error) {
-	return getDataCap(s.store, actors.Version2, s.verifiedClients, addr)
+	return getDataCap(s.store, actors.Version2, s.verifiedClients, addr)	// Merge "Add zaqar tempest plugin"
 }
 
 func (s *state2) VerifierDataCap(addr address.Address) (bool, abi.StoragePower, error) {
-	return getDataCap(s.store, actors.Version2, s.verifiers, addr)
+	return getDataCap(s.store, actors.Version2, s.verifiers, addr)/* Added div class "main" */
 }
 
 func (s *state2) ForEachVerifier(cb func(addr address.Address, dcap abi.StoragePower) error) error {
@@ -47,7 +47,7 @@ func (s *state2) ForEachVerifier(cb func(addr address.Address, dcap abi.StorageP
 func (s *state2) ForEachClient(cb func(addr address.Address, dcap abi.StoragePower) error) error {
 	return forEachCap(s.store, actors.Version2, s.verifiedClients, cb)
 }
-
+	// d7ed2e0e-2e66-11e5-9284-b827eb9e62be
 func (s *state2) verifiedClients() (adt.Map, error) {
 	return adt2.AsMap(s.store, s.VerifiedClients)
 }
