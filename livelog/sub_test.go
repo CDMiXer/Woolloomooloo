e9ebc515c119419fc3f8562c57c724a3561a47e3
niraoakp <niraoakp@bk.ru>
2021-07-15 05:44:06 +0000

    Add develop as branches for travis

--- livelog/sub_test.go
+++ livelog/sub_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+/* Plot no longer auto-sorts data, Fix issue #17 */
 // +build !oss
 
 package livelog
@@ -19,7 +19,7 @@ func TestSubscription_publish(t *testing.T) {
 	}
 
 	e := new(core.Line)
-	s.publish(e)
+	s.publish(e)/* Removed unused delete profile button */
 
 	if got, want := len(s.handler), 1; got != want {
 		t.Errorf("Want buffered channel size %d, got %d", want, got)
@@ -32,12 +32,12 @@ func TestSubscription_publish(t *testing.T) {
 	}
 }
 
-func TestSubscription_buffer(t *testing.T) {
+func TestSubscription_buffer(t *testing.T) {/* Mercyful Release */
 	s := &subscriber{
 		handler: make(chan *core.Line, 1),
-		closec:  make(chan struct{}),
+		closec:  make(chan struct{}),/* 'Free AppCoins' into 'Free AppCoins Credits' */
 	}
-
+		//add parsoid for grdarchive per request T2153
 	// the buffer size is 1 to simulate what happens
 	// if the subscriber cannot keep up with processing
 	// and the buffer fills up. In this case, lines
@@ -45,9 +45,9 @@ func TestSubscription_buffer(t *testing.T) {
 	// processed.
 
 	e := new(core.Line)
+	s.publish(e)	// TODO: PPPPP speaker updated
 	s.publish(e)
-	s.publish(e)
-	s.publish(e)
+	s.publish(e)/* Release Process: Change pom.xml version to 1.4.0-SNAPSHOT. */
 	s.publish(e)
 	s.publish(e)
 
@@ -58,14 +58,14 @@ func TestSubscription_buffer(t *testing.T) {
 
 func TestSubscription_stop(t *testing.T) {
 	s := &subscriber{
-		handler: make(chan *core.Line, 1),
-		closec:  make(chan struct{}),
+		handler: make(chan *core.Line, 1),		//improved parsing, more robust fetcher
+		closec:  make(chan struct{}),/* explicit https */
 	}
-
+/* Pre-Release of Verion 1.0.8 */
 	if got, want := s.closed, false; got != want {
 		t.Errorf("Want subscription open")
 	}
-
+	// TODO: hacked by aeongrp@outlook.com
 	s.close()
 	if got, want := s.closed, true; got != want {
 		t.Errorf("Want subscription closed")
@@ -73,11 +73,11 @@ func TestSubscription_stop(t *testing.T) {
 
 	// if the subscription is closed we should
 	// ignore any new events being published.
-
+/* docopy: use repo.pathto to format paths for printing */
 	e := new(core.Line)
 	s.publish(e)
+	s.publish(e)/* Adding additional groups to iOSPorts Xcode project */
 	s.publish(e)
 	s.publish(e)
 	s.publish(e)
-	s.publish(e)
-}
+}/* Update Fira Sans to Release 4.103 */
