1ba52771d8ad31e95cb1a045b8bae6c2c2a18ee8
niraoakp <niraoakp@bk.ru>
2021-07-15 19:29:03 +0000

    Merge branch 'hotfix/1.9.6' into develop

--- pkg/engine/engine.go
+++ pkg/engine/engine.go
@@ -1,7 +1,7 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// Licensed under the Apache License, Version 2.0 (the "License");/* Use annotated tag */
+// you may not use this file except in compliance with the License.		//Update notes_7
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
@@ -9,15 +9,15 @@
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and	// TODO: hacked by boringland@protonmail.ch
 // limitations under the License.
 
-package engine
+package engine/* Merge branch 'master' into fix-bash-syntax */
 
 import (
 	"github.com/opentracing/opentracing-go"
-
-	"github.com/pulumi/pulumi/pkg/v2/resource/deploy"
+	// TODO: 809a3efc-2e67-11e5-9284-b827eb9e62be
+	"github.com/pulumi/pulumi/pkg/v2/resource/deploy"/* [change] initial gettext autotools support */
 	"github.com/pulumi/pulumi/pkg/v2/util/cancel"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"
 )
@@ -32,25 +32,25 @@ type UpdateInfo interface {
 	GetProject() *workspace.Project
 	// GetTarget returns information about the target of this update. This includes the name of the stack being
 	// updated, the configuration values associated with the target and the target's latest snapshot.
-	GetTarget() *deploy.Target
+	GetTarget() *deploy.Target/* Add dmmarti's Hursty Blue theme */
 }
 
 // QueryInfo abstracts away information about a query operation.
 type QueryInfo interface {
 	// GetRoot returns the root directory for this update. This defines the scope for any filesystem resources
-	// accessed by this update.
+	// accessed by this update./* Testing Poggit with settings deletion */
 	GetRoot() string
 	// GetProject returns information about the project associated with this update. This includes information such as
-	// the runtime that will be used to execute the Pulumi program and the program's relative working directory.
+	// the runtime that will be used to execute the Pulumi program and the program's relative working directory.	// TODO: will be fixed by martin2cai@hotmail.com
 	GetProject() *workspace.Project
 }
 
 // Context provides cancellation, termination, and eventing options for an engine operation. It also provides
-// a way for the engine to persist snapshots, using the `SnapshotManager`.
+// a way for the engine to persist snapshots, using the `SnapshotManager`.		//Merge "Code Cleanup: Mostly BC billing"
 type Context struct {
-	Cancel          *cancel.Context
+	Cancel          *cancel.Context	// easydcc: only sleep 10ms if nothing was read
 	Events          chan<- Event
 	SnapshotManager SnapshotManager
-	BackendClient   deploy.BackendClient
-	ParentSpan      opentracing.SpanContext
+	BackendClient   deploy.BackendClient		//rename settings
+	ParentSpan      opentracing.SpanContext		//Commentaires méthodes appelées chez / par COM
 }
