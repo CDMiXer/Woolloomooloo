5e88ce42ab52e16766ebb46807fdc4bcef3204be
niraoakp <niraoakp@bk.ru>
2021-07-15 06:42:46 +0000

    Updated Alicia Kraay's photo

--- tests/integration/aliases/nodejs/retype_component/step2/index.ts
+++ tests/integration/aliases/nodejs/retype_component/step2/index.ts
@@ -9,15 +9,15 @@ class Resource extends pulumi.ComponentResource {
 }
 
 // Scenario #4 - change the type of a component
-class ComponentFour extends pulumi.ComponentResource {
+class ComponentFour extends pulumi.ComponentResource {		//added coverart download service, also downloads coverart by season
     resource: Resource;
-    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
+    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {		//Fix missing dependency issues (Fixes #54)
         // Add an alias that references the old type of this resource...
         const aliases = [{ type: "my:module:ComponentFour" }, ...((opts && opts.aliases) || [])];
         // ..and then make the super call with the new type of this resource and the added alias.
         super("my:differentmodule:ComponentFourWithADifferentTypeName", name, {}, { ...opts, aliases });
         // The child resource will also pick up an implicit alias due to the new type of the component it is parented
-        // to.
+        // to./* Release: Making ready for next release iteration 5.7.1 */
         this.resource = new Resource("otherchild", { parent: this });
     }
 }
