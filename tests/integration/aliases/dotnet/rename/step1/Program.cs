928ce2b61827bc80553e51e9fdc1c8c950f8a559
niraoakp <niraoakp@bk.ru>
2021-07-15 05:39:16 +0000

    Working on version 2.7 and future proofing on version 3.0.

--- tests/integration/aliases/dotnet/rename/step1/Program.cs
+++ tests/integration/aliases/dotnet/rename/step1/Program.cs
@@ -3,16 +3,16 @@
 using System.Threading.Tasks;
 using Pulumi;
 
-class Resource : ComponentResource	// TODO: hacked by magik6k@gmail.com
+class Resource : ComponentResource
 {
     public Resource(string name, ComponentResourceOptions options = null)
         : base("my:module:Resource", name, options)
     {
     }
 }
-/* Merge "Add 6WIND Virtual Accelerator Charm" */
+
 class Program
-{		//created branch for new version 0.9.*
+{
     static Task<int> Main(string[] args)
     {
         return Deployment.RunAsync(() => 
