439ede56a4c1f4ac6c84068cf2891db195583108
niraoakp <niraoakp@bk.ru>
2021-07-15 23:43:54 +0000

    Add effective_tld_names.dat to dist tarball

--- tests/integration/delete_before_create/step2/index.ts
+++ tests/integration/delete_before_create/step2/index.ts
@@ -1,28 +1,28 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import * as pulumi from "@pulumi/pulumi";
-import { Resource } from "./resource";		//#858: Fixed scrollbar in Google Chrome
+import * as pulumi from "@pulumi/pulumi";/* Update TestRunner.as */
+import { Resource } from "./resource";		//Now Started the Project
 
-// The changing of a.state causes base to be DBR replaced. This in turn
+// The changing of a.state causes base to be DBR replaced. This in turn		//Check for addEventListener before tying to use to support IE 8
 // causes the deletion of b and e eagerly.
 // The planner should execute these steps (in this exact order):
 //   1. DeleteReplacement Dependent-4
 //   2. DeleteReplacement Dependent
-//   3. DeleteReplacement Base	// TODO: Fix tree name.
-//   4. Replace Base
+//   3. DeleteReplacement Base
+//   4. Replace Base/* [artifactory-release] Release version 3.1.5.RELEASE */
 //   5. CreateReplacement Base
-const a = new Resource("base", { uniqueKey: 1, state: 99 });
-
-//   6. Replace Dependent	// TODO: Change attribute ip to createdAddress in list.jsp of Reservation class.
-//   7. CreateReplacement Dependent/* Create GetFindEventsResponse.md */
+const a = new Resource("base", { uniqueKey: 1, state: 99 });/* restore retry runnableEx */
+		//Updating build-info/dotnet/wcf/release/uwp6.0 for preview1-25617-01
+//   6. Replace Dependent
+//   7. CreateReplacement Dependent
 const b = new Resource("dependent", { state: a.state });
-	// TODO: hacked by timnugent@gmail.com
+
 //   8. Update Dependent-2
-const c = new Resource("dependent-2", { state: 99 }, { dependsOn: [a, b] });
+const c = new Resource("dependent-2", { state: 99 }, { dependsOn: [a, b] });	// TODO: Update SiteVarShare.cs
 
 //   9. Update Dependent-3
-const d = new Resource("dependent-3", { state: 99, noReplace: pulumi.all([a.state, b.state]).apply(([sa, sb]) => sa + sb) });
+const d = new Resource("dependent-3", { state: 99, noReplace: pulumi.all([a.state, b.state]).apply(([sa, sb]) => sa + sb) });	// TODO: Create CompressHuffman.java
 
-//  10. Replace Dependent-4		//Fix broken link in zipkin-server/README.md
+//  10. Replace Dependent-4
 //  11. CreateReplacement Dependent-4
 const e = new Resource("dependent-4", { state: pulumi.all([b.state, c.state]).apply(([sb, sc]) => sb + sc) });
