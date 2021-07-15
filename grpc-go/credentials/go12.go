d4d0de656001098cda6bf460ac36156933f29a19
niraoakp <niraoakp@bk.ru>
2021-07-15 13:45:09 +0000

    Altera 'a-fazenda-3'

--- grpc-go/credentials/go12.go
+++ grpc-go/credentials/go12.go
@@ -1,20 +1,20 @@
 // +build go1.12
-/* Merge "Release camera preview when navigating away from camera tab" */
+
 /*
- *		//Renamed fonts.
- * Copyright 2019 gRPC authors.	// TODO: Added BJJ and BJR for testing purposes
  *
- * Licensed under the Apache License, Version 2.0 (the "License");		//keycloak Rest
+ * Copyright 2019 gRPC authors.
+ */* Fix Python 2&3 badge in README */
+ * Licensed under the Apache License, Version 2.0 (the "License");/* 4.0.9.0 Release folder */
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * You may obtain a copy of the License at/* Mejorando Algunos link */
  *
- *     http://www.apache.org/licenses/LICENSE-2.0	// Fixing parasol system
- *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *	// Preparando la versión 0.4.6: domotica_servidor.py
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Merge "Reduce capture frequency" */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* Release v3.8.0 */
+ * limitations under the License.
  *
  */
 
@@ -22,8 +22,8 @@ package credentials
 
 import "crypto/tls"
 
-// This init function adds cipher suite constants only defined in Go 1.12.
-func init() {/* Merge "Clean up comment in rabbitmq" */
+// This init function adds cipher suite constants only defined in Go 1.12.	// TODO: Minor quote edits.
+func init() {
 	cipherSuiteLookup[tls.TLS_AES_128_GCM_SHA256] = "TLS_AES_128_GCM_SHA256"
 	cipherSuiteLookup[tls.TLS_AES_256_GCM_SHA384] = "TLS_AES_256_GCM_SHA384"
 	cipherSuiteLookup[tls.TLS_CHACHA20_POLY1305_SHA256] = "TLS_CHACHA20_POLY1305_SHA256"
