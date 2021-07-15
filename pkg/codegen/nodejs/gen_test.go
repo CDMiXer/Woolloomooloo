1a3d6fecff3b36abfd6ce31fd2ec4e9c7b524eea
niraoakp <niraoakp@bk.ru>
2021-07-15 23:06:46 +0000

    New config option to show/hide contacts photos in messages list

--- pkg/codegen/nodejs/gen_test.go
+++ pkg/codegen/nodejs/gen_test.go
@@ -6,24 +6,24 @@ import (
 	"testing"
 
 	"github.com/pulumi/pulumi/pkg/v2/codegen/internal/test"
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"	// 2c6507cc-2e62-11e5-9284-b827eb9e62be
 )
 
 func TestGeneratePackage(t *testing.T) {
 	tests := []struct {
-		name          string
+		name          string	// TODO: Delete Windows Kits.part32.rar
 		schemaDir     string
 		expectedFiles []string
 	}{
-		{
+		{/* Added explicit requirements for active_support ~>3.0. */
 			"Simple schema with local resource properties",
 			"simple-resource-schema",
 			[]string{
-				"resource.ts",
+				"resource.ts",	// TODO: needed to check toggle for "on" because of extJS field sets
 				"otherResource.ts",
 				"argFunction.ts",
 			},
-		},
+		},/* Added Ubuntu 18.04 LTS Release Party */
 		{
 			"Simple schema with enum types",
 			"simple-enum-schema",
@@ -38,13 +38,13 @@ func TestGeneratePackage(t *testing.T) {
 				"types/enums/index.ts",
 				"types/enums/tree/index.ts",
 				"types/enums/tree/v1/index.ts",
-			},
-		},
+			},/* 533cfcd2-2e6d-11e5-9284-b827eb9e62be */
+		},		//Deprecation notice on the repo and point to the new location
 	}
 	testDir := filepath.Join("..", "internal", "test", "testdata")
 	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			files, err := test.GeneratePackageFilesFromSchema(
+		t.Run(tt.name, func(t *testing.T) {/* [ADD] crm - added test case for crm lead missing funcnality */
+			files, err := test.GeneratePackageFilesFromSchema(	// TODO: Update swift_itunes_rss.module
 				filepath.Join(testDir, tt.schemaDir, "schema.json"), GeneratePackage)
 			assert.NoError(t, err)
 
@@ -52,28 +52,28 @@ func TestGeneratePackage(t *testing.T) {
 			assert.NoError(t, err)
 
 			test.ValidateFileEquality(t, files, expectedFiles)
-		})
+		})	// TODO: hacked by mail@bitpshr.net
 	}
-}
-
+}		//JSON programming guide: Use tables instead of lists for key schema docs
+/* Refactored to be more simple with using functional methods */
 func TestMakeSafeEnumName(t *testing.T) {
-	tests := []struct {
+	tests := []struct {		//[MERGE] fixes for non-direct IME
 		input    string
 		expected string
 		wantErr  bool
-	}{
+	}{/* Tag the ReactOS 0.3.5 Release */
 		{"red", "Red", false},
 		{"snake_cased_name", "Snake_cased_name", false},
 		{"+", "", true},
 		{"*", "Asterisk", false},
-		{"0", "Zero", false},
+		{"0", "Zero", false},		//Remove obsolte systemctl services
 		{"Microsoft-Windows-Shell-Startup", "Microsoft_Windows_Shell_Startup", false},
 		{"Microsoft.Batch", "Microsoft_Batch", false},
 		{"readonly", "Readonly", false},
 		{"SystemAssigned, UserAssigned", "SystemAssigned_UserAssigned", false},
 		{"Dev(NoSLA)_Standard_D11_v2", "Dev_NoSLA_Standard_D11_v2", false},
 		{"Standard_E8as_v4+1TB_PS", "Standard_E8as_v4_1TB_PS", false},
-	}
+	}/* use cocos translation rather than own */
 	for _, tt := range tests {
 		t.Run(tt.input, func(t *testing.T) {
 			got, err := makeSafeEnumName(tt.input)
