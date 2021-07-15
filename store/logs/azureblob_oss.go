022e177a6e639172c40812ad617ccedb1df6c9d8
niraoakp <niraoakp@bk.ru>
2021-07-15 17:48:33 +0000

    NSI: handle deprecated methods again

--- store/logs/azureblob_oss.go
+++ store/logs/azureblob_oss.go
@@ -4,21 +4,21 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* Adhock Source Code Release */
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: Delete Error.js
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// TODO: Added routing for notification update
-// limitations under the License.		//Disabling compilation of direct diff binaries for parallel AD job.
-
+// See the License for the specific language governing permissions and/* Add gradle workflow */
+// limitations under the License./* Added missing namespace to Italian strings xml */
+	// Make sure person is automatically set when adding a topic
 // +build oss
-	// uncommenting unused methods
+
 package logs
 
-import "github.com/drone/drone/core"/* Release of eeacms/ims-frontend:0.7.5 */
-
-// New returns a zero value LogStore./* dont log user name or email */
+import "github.com/drone/drone/core"
+/* CìPopolate tabelle di importazione */
+// New returns a zero value LogStore.
 func NewAzureBlobEnv(containerName, storageAccountName, storageAccessKey string) core.LogStore {
 	return nil
-}
+}/* Release Notes update for ZPH polish. pt2 */
