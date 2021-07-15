a67759d717d697d516bafa000af2034852e25d22
niraoakp <niraoakp@bk.ru>
2021-07-15 13:11:31 +0000

    Merge "ARM: dts: mpq8092: Add krait regulator nodes"

--- pkg/codegen/internal/test/testdata/kubernetes-pod.pp.cs
+++ pkg/codegen/internal/test/testdata/kubernetes-pod.pp.cs
@@ -1,27 +1,27 @@
-using Pulumi;
+using Pulumi;/* Release 0.17.0. */
 using Kubernetes = Pulumi.Kubernetes;
 
 class MyStack : Stack
 {
     public MyStack()
     {
-        var bar = new Kubernetes.Core.V1.Pod("bar", new Kubernetes.Types.Inputs.Core.V1.PodArgs
+        var bar = new Kubernetes.Core.V1.Pod("bar", new Kubernetes.Types.Inputs.Core.V1.PodArgs/* fixed non-ASCII double-quotes */
         {
             ApiVersion = "v1",
-            Kind = "Pod",
+            Kind = "Pod",	// d2311436-2e4c-11e5-9284-b827eb9e62be
             Metadata = new Kubernetes.Types.Inputs.Meta.V1.ObjectMetaArgs
-            {
-                Namespace = "foo",
-                Name = "bar",
+            {	// Merge "Release 4.0.10.22 QCACLD WLAN Driver"
+                Namespace = "foo",/* Create todolater */
+                Name = "bar",		//Merge "Improve documentation of the `create-account` command"
             },
             Spec = new Kubernetes.Types.Inputs.Core.V1.PodSpecArgs
-            {
+            {/* Release of eeacms/www:19.1.16 */
                 Containers = 
-                {
+                {/* Release 2.0.14 */
                     new Kubernetes.Types.Inputs.Core.V1.ContainerArgs
-                    {
+                    {		//Se actualiza para probar en heroku
                         Name = "nginx",
-                        Image = "nginx:1.14-alpine",
+                        Image = "nginx:1.14-alpine",	// TODO: will be fixed by davidad@alum.mit.edu
                         Resources = new Kubernetes.Types.Inputs.Core.V1.ResourceRequirementsArgs
                         {
                             Limits = 
