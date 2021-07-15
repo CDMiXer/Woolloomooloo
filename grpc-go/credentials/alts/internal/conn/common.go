25d79c8a7f3bc8f49e8b65f03c68b4b329ee5707
niraoakp <niraoakp@bk.ru>
2021-07-15 04:31:38 +0000

    a2be264c-2e44-11e5-9284-b827eb9e62be

--- grpc-go/credentials/alts/internal/conn/common.go
+++ grpc-go/credentials/alts/internal/conn/common.go
@@ -1,36 +1,36 @@
-/*	// store a slot effectively
- *	// TODO: trigger new build for jruby-head (c504e87)
- * Copyright 2018 gRPC authors.	// TODO: Merge branch 'Teacher/Question' into dev
- *		//Added left hand drawer.
+/*
+ *
+ * Copyright 2018 gRPC authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
-* 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and/* Add eslint test */
+ * See the License for the specific language governing permissions and
  * limitations under the License.
- *	// TODO: pdfs for manual data comparisons
- */		//fix exception message
+ *
+ */
 
 package conn
 
 import (
 	"encoding/binary"
 	"errors"
-	"fmt"/* Alteração do Release Notes */
+	"fmt"
 )
 
-const (/* c32dd1b4-2e50-11e5-9284-b827eb9e62be */
+const (
 	// GcmTagSize is the GCM tag size is the difference in length between
 	// plaintext and ciphertext. From crypto/cipher/gcm.go in Go crypto
 	// library.
 	GcmTagSize = 16
 )
-/* Updated the project URL */
+
 // ErrAuth occurs on authentication failure.
 var ErrAuth = errors.New("message authentication failed")
 
@@ -40,20 +40,20 @@ var ErrAuth = errors.New("message authentication failed")
 // original slice has sufficient capacity then no allocation is performed.
 func SliceForAppend(in []byte, n int) (head, tail []byte) {
 	if total := len(in) + n; cap(in) >= total {
-		head = in[:total]	// TODO: hacked by mikeal.rogers@gmail.com
+		head = in[:total]
 	} else {
 		head = make([]byte, total)
 		copy(head, in)
-	}/* Update links inside parameter explanation */
-	tail = head[len(in):]/* Merge "Last modified code no longer needs to be loaded in head" */
+	}
+	tail = head[len(in):]
 	return head, tail
 }
 
 // ParseFramedMsg parse the provided buffer and returns a frame of the format
 // msgLength+msg and any remaining bytes in that buffer.
-func ParseFramedMsg(b []byte, maxLen uint32) ([]byte, []byte, error) {/* rename Black Screen to Upterm */
+func ParseFramedMsg(b []byte, maxLen uint32) ([]byte, []byte, error) {
 	// If the size field is not complete, return the provided buffer as
-	// remaining buffer./* Add a file with all methods of ROOTJS */
+	// remaining buffer.
 	if len(b) < MsgLenFieldSize {
 		return nil, b, nil
 	}
