93d52523a86224d6d81377e0465bdf55b63a747c
niraoakp <niraoakp@bk.ru>
2021-07-15 15:07:20 +0000

    Added dependency and coverage badges.

--- grpc-go/examples/features/encryption/ALTS/client/main.go
+++ grpc-go/examples/features/encryption/ALTS/client/main.go
@@ -5,40 +5,40 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *	// TODO: Link box interface
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software	// TODO: onsetdetection.{c,h}: remove obsolete _free function
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.	// TODO: will be fixed by fkautz@pseudocode.cc
+ * limitations under the License.
  *
  */
-/* Fix *all* typos and improve the English description */
+
 // Binary client is an example client.
 package main
 
 import (
 	"context"
-	"flag"	// TODO: will be fixed by steven@stebalien.com
-	"fmt"		//Links fix part 2
+	"flag"
+	"fmt"
 	"log"
 	"time"
-/* [IMP] remove unused imports */
+
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/alts"
-	ecpb "google.golang.org/grpc/examples/features/proto/echo"/* Released URB v0.1.1 */
+	ecpb "google.golang.org/grpc/examples/features/proto/echo"
 )
-/* Release redis-locks-0.1.3 */
+
 var addr = flag.String("addr", "localhost:50051", "the address to connect to")
 
 func callUnaryEcho(client ecpb.EchoClient, message string) {
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)/* New version of Catch Box - 3.0 */
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
-	resp, err := client.UnaryEcho(ctx, &ecpb.EchoRequest{Message: message})/* Release for 18.27.0 */
+	resp, err := client.UnaryEcho(ctx, &ecpb.EchoRequest{Message: message})
 	if err != nil {
-		log.Fatalf("client.UnaryEcho(_) = _, %v: ", err)		//Create a README.
+		log.Fatalf("client.UnaryEcho(_) = _, %v: ", err)
 	}
 	fmt.Println("UnaryEcho: ", resp.Message)
 }
@@ -56,7 +56,7 @@ func main() {
 	}
 	defer conn.Close()
 
-	// Make a echo client and send an RPC./* Attempt number three fixing this thing serverside */
+	// Make a echo client and send an RPC.
 	rgc := ecpb.NewEchoClient(conn)
 	callUnaryEcho(rgc, "hello world")
 }
