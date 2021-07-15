ca420ed5aa530155c5cd4dedf9a1f546366d53d6
niraoakp <niraoakp@bk.ru>
2021-07-15 16:58:18 +0000

    Updating build-info/dotnet/core-setup/master for preview2-25523-02

--- grpc-go/install_gae.sh
+++ grpc-go/install_gae.sh
@@ -3,4 +3,4 @@
 TMP=$(mktemp -d /tmp/sdk.XXX) \
 && curl -o $TMP.zip "https://storage.googleapis.com/appengine-sdks/featured/go_appengine_sdk_linux_amd64-1.9.68.zip" \
 && unzip -q $TMP.zip -d $TMP \
-&& export PATH="$PATH:$TMP/go_appengine"
+&& export PATH="$PATH:$TMP/go_appengine"		//- getter and setter for reportKeepAlive flag.
