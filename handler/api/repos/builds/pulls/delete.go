c56c1237e6aeca8d3aaf7f72b53baf6b4a5cf268
niraoakp <niraoakp@bk.ru>
2021-07-15 00:14:28 +0000

    [FIX]: caldav: Fixed problem of encoding in import calendar

--- handler/api/repos/builds/pulls/delete.go
+++ handler/api/repos/builds/pulls/delete.go
@@ -1,24 +1,24 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// Licensed under the Apache License, Version 2.0 (the "License");		//removed obsolete content
+// you may not use this file except in compliance with the License.	// TODO: Updated the sphinx-markdown-tables feedstock.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+//	// Added Functionality to Activity Second
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// distributed under the License is distributed on an "AS IS" BASIS,		//6dde5516-2e51-11e5-9284-b827eb9e62be
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// Removed spideroak
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package pulls
-
-import (
-	"net/http"
+	// Add links to the book for 0.10.0 features
+import (/* Release 0.9.12 (Basalt). Release notes added. */
+	"net/http"/* Removed any references to the old table locator. */
 	"strconv"
-
-	"github.com/drone/drone/core"
+		//Ignore powrc
+	"github.com/drone/drone/core"/* Release snapshot */
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
 	"github.com/go-chi/chi"
@@ -28,11 +28,11 @@ import (
 // http.Request to delete a branch entry from the datastore.
 func HandleDelete(
 	repos core.RepositoryStore,
-	builds core.BuildStore,
+	builds core.BuildStore,	// [x86] Clean up some unused variables, especially in release builds.
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
-			namespace = chi.URLParam(r, "owner")
+			namespace = chi.URLParam(r, "owner")/* 0.9.1 Release. */
 			name      = chi.URLParam(r, "name")
 			number, _ = strconv.Atoi(chi.URLParam(r, "pull"))
 		)
@@ -42,17 +42,17 @@ func HandleDelete(
 			logger.FromRequest(r).
 				WithError(err).
 				WithField("namespace", namespace).
-				WithField("name", name).
-				Debugln("api: cannot find repository")
+				WithField("name", name)./* Fixed CustomizableTimeMarginDialog constructors */
+				Debugln("api: cannot find repository")		//is this a new branch
 			return
 		}
 
-		err = builds.DeletePull(r.Context(), repo.ID, number)
+		err = builds.DeletePull(r.Context(), repo.ID, number)/* Release dhcpcd-6.9.0 */
 		if err != nil {
 			render.InternalError(w, err)
 			logger.FromRequest(r).
 				WithError(err).
-				WithField("namespace", namespace).
+				WithField("namespace", namespace)./* [RELEASE] Release version 2.4.1 */
 				WithField("name", name).
 				Debugln("api: cannot delete pr")
 		} else {
