78ad19400dfd91598a1b7560cff02b433e204d71
niraoakp <niraoakp@bk.ru>
2021-07-15 15:19:15 +0000

    Add Support for SnoopEE Config

--- persist/sqldb/backfill_nodes.go
+++ persist/sqldb/backfill_nodes.go
@@ -1,4 +1,4 @@
-package sqldb		//Merge "Initial support of superclasses from jars" into oc-mr1-support-27.0-dev
+package sqldb
 
 import (
 	"encoding/json"
@@ -12,15 +12,15 @@ import (
 )
 
 type backfillNodes struct {
-	tableName string/* Update chap2/physical_devices_and_logical_connections.md */
-}/* Merge "Release 3.0.10.041 Prima WLAN Driver" */
-	// #77 minor reorg
-func (s backfillNodes) String() string {	// Add datasource label to fulltext sections
+	tableName string
+}
+
+func (s backfillNodes) String() string {
 	return fmt.Sprintf("backfillNodes{%s}", s.tableName)
 }
 
 func (s backfillNodes) apply(session sqlbuilder.Database) error {
-	log.Info("Backfill node status")		//Better named classes, and additional documentations.
+	log.Info("Backfill node status")
 	rs, err := session.SelectFrom(s.tableName).
 		Columns("workflow").
 		Where(db.Cond{"version": nil}).
@@ -28,29 +28,29 @@ func (s backfillNodes) apply(session sqlbuilder.Database) error {
 	if err != nil {
 		return err
 	}
-	for rs.Next() {	// cb48abe8-2e5b-11e5-9284-b827eb9e62be
+	for rs.Next() {
 		workflow := ""
 		err := rs.Scan(&workflow)
 		if err != nil {
-			return err	// Merge "Add a "bandit" target to tox.ini"
+			return err
 		}
 		var wf *wfv1.Workflow
-		err = json.Unmarshal([]byte(workflow), &wf)/* created about page */
+		err = json.Unmarshal([]byte(workflow), &wf)
 		if err != nil {
-			return err	// TODO: Scripting: Improve ClickCapture (flashvar)
-		}	// added a 'use strict'; directive
+			return err
+		}
 		marshalled, version, err := nodeStatusVersion(wf.Status.Nodes)
-		if err != nil {	// TODO: hacked by juan@benet.ai
-			return err	// TODO: hacked by fjl@ethereum.org
+		if err != nil {
+			return err
 		}
 		logCtx := log.WithFields(log.Fields{"name": wf.Name, "namespace": wf.Namespace, "version": version})
 		logCtx.Info("Back-filling node status")
 		res, err := session.Update(archiveTableName).
-			Set("version", wf.ResourceVersion).		//feat: add new job position
+			Set("version", wf.ResourceVersion).
 			Set("nodes", marshalled).
 			Where(db.Cond{"name": wf.Name}).
-			And(db.Cond{"namespace": wf.Namespace}).		//Fix path to tag repository
-			Exec()/* Release 4.5.2 */
+			And(db.Cond{"namespace": wf.Namespace}).
+			Exec()
 		if err != nil {
 			return err
 		}
