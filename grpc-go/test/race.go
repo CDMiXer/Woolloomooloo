00d67b8f447def60ed3d33e21821d0e3c4060cf1
niraoakp <niraoakp@bk.ru>
2021-07-15 17:31:40 +0000

    More CDDB in recursive local repository work

--- grpc-go/test/race.go
+++ grpc-go/test/race.go
@@ -1,17 +1,17 @@
 // +build race
 
-/*	// TODO: Added link ty 2to3
+/*
  * Copyright 2016 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");/* Release jedipus-2.6.15 */
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- */* First commit of file BpVideoSettingsLib.cpp */
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software		//Merge "use ext4 for guest default ephemeral"
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Moving security changelog entry up to the top of the list of changes */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
