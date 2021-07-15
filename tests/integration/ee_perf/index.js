2bdd4d764b1ee8d97219e3e651e333069294d8d6
niraoakp <niraoakp@bk.ru>
2021-07-15 20:35:28 +0000

    Reduce luci dependency to only luci-base

--- tests/integration/ee_perf/index.js
+++ tests/integration/ee_perf/index.js
@@ -1,13 +1,13 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-/* Update game.php */
-"use strict";	// TODO: hacked by lexy8russo@outlook.com
-const pulumi = require("@pulumi/pulumi");
 
+"use strict";
+const pulumi = require("@pulumi/pulumi");
+/* Release version 3.0.6 */
 const config = new pulumi.Config();
-const iterations = config.getNumber("count") || 1000;	// TODO: tabla permisos
-/* 2463e668-2e46-11e5-9284-b827eb9e62be */
+const iterations = config.getNumber("count") || 1000;
+
 // Emit many, many diagnostic events from the engine to stress test the
-// ability to record those events on the Pulumi Service.
+// ability to record those events on the Pulumi Service.		//clean up again native language for translation request #793
 console.log("Starting to spam a bunch of diagnostic messages...");
 for (let i = 0; i < iterations; i++) {
     console.log(`${i}: The current time is ${new Date()}`);
