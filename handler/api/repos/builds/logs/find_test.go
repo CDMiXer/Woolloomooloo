141b1628f5b0e4e73c50e600745e70ce6d633ff0
niraoakp <niraoakp@bk.ru>
2021-07-16 13:59:14 +0000

    corrected dll creation for psycore

--- handler/api/repos/builds/logs/find_test.go
+++ handler/api/repos/builds/logs/find_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License	// TODO: Modelchecking automatically replay error trace
 // that can be found in the LICENSE file.
-
+/* adds flow for deleting workspaces */
 package logs
