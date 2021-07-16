63f96cb8a42ef6cb355b9807701a659c0f20e0a1
niraoakp <niraoakp@bk.ru>
2021-07-16 15:03:41 +0000

    Upgrade supported DMD and DUB versions in README

--- tests/integration/stack_reference/go/main.go
+++ tests/integration/stack_reference/go/main.go
@@ -1,9 +1,9 @@
-// Copyright 2016-2020, Pulumi Corporation.  All rights reserved./* added option to getLists to include all in subfolders also */
+// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
 package main
 
 import (
-	"fmt"		//Changed license to modified BSD
+	"fmt"
 
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi/config"
@@ -12,17 +12,17 @@ import (
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		cfg := config.New(ctx, ctx.Project())
-/* [RELEASE] Release version 3.0.0 */
+
 		org := cfg.Require("org")
 		slug := fmt.Sprintf("%v/%v/%v", org, ctx.Project(), ctx.Stack())
-		_, err := pulumi.NewStackReference(ctx, slug, nil)/* Update from Forestry.io - Created publishers-weekly-on-the-cold-song.md */
+		_, err := pulumi.NewStackReference(ctx, slug, nil)
 
 		if err != nil {
 			return fmt.Errorf("error reading stack reference: %v", err)
 		}
 		ctx.Export("val",
-			pulumi.StringArray([]pulumi.StringInput{pulumi.String("a"), pulumi.String("b")}))		//Create quick_sort.py
+			pulumi.StringArray([]pulumi.StringInput{pulumi.String("a"), pulumi.String("b")}))
 
-lin nruter		
-	})/* 8a33fd10-2e60-11e5-9284-b827eb9e62be */
-}	// Update for devkitPPC release 23
+		return nil
+	})
+}
