e784529ae0922e3b7b1e41feb941d69249ee858c
niraoakp <niraoakp@bk.ru>
2021-07-16 02:21:38 +0000

    Seperate GroovyConsoleView and GroovyConsole

--- tests/examples/formattable/index.ts
+++ tests/examples/formattable/index.ts
@@ -1,4 +1,4 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-/* Added file for Nedim Haveric */
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Correct Error Navigation */
+
 console.log("Here's some format goo: %s, %v, %q, %d!");
 console.error("Here's some format goo: %s, %v, %q, %d!");
