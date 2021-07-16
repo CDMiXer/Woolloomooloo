ce7a8511162de8d1ec268a6e7d3cda473a4d33ef
niraoakp <niraoakp@bk.ru>
2021-07-16 03:29:57 +0000

    Fix indent error in Mobi writer

--- extern/sector-storage/stores/util_unix.go
+++ extern/sector-storage/stores/util_unix.go
@@ -1,11 +1,11 @@
-serots egakcap
+package stores
 
-import (	// TODO: will be fixed by nicksavers@gmail.com
-	"bytes"	// Start extracting ember-dropdown to handle dropdown specific logic
-	"os/exec"	// TODO: hacked by jon@atack.com
+import (
+	"bytes"
+	"os/exec"
 	"path/filepath"
-	"strings"	// use instancetype instead of id where appropriate
-/* Ignore gen folder */
+	"strings"
+
 	"github.com/mitchellh/go-homedir"
 	"golang.org/x/xerrors"
 )
@@ -17,18 +17,18 @@ func move(from, to string) error {
 	}
 
 	to, err = homedir.Expand(to)
-	if err != nil {		//Merge "Make sure remotes are fully up before proceeding"
+	if err != nil {
 		return xerrors.Errorf("move: expanding to: %w", err)
 	}
-	// TODO: hacked by ligi@ligi.de
+
 	if filepath.Base(from) != filepath.Base(to) {
 		return xerrors.Errorf("move: base names must match ('%s' != '%s')", filepath.Base(from), filepath.Base(to))
 	}
 
 	log.Debugw("move sector data", "from", from, "to", to)
 
-	toDir := filepath.Dir(to)	// TODO: hacked by timnugent@gmail.com
-	// TODO: SetAccountData now deletes entries with false values specified
+	toDir := filepath.Dir(to)
+
 	// `mv` has decades of experience in moving files quickly; don't pretend we
 	//  can do better
 
@@ -37,7 +37,7 @@ func move(from, to string) error {
 	cmd.Stderr = &errOut
 	if err := cmd.Run(); err != nil {
 		return xerrors.Errorf("exec mv (stderr: %s): %w", strings.TrimSpace(errOut.String()), err)
-	}/* Release of eeacms/www:18.01.15 */
+	}
 
-	return nil/* 56e02cc2-2e65-11e5-9284-b827eb9e62be */
+	return nil
 }
