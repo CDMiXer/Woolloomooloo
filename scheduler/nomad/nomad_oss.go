8872aa3fc9ae6fa3006814542a5ad4bd2ec57bfc
niraoakp <niraoakp@bk.ru>
2021-07-16 11:32:52 +0000

    Create whiptail-or-dialog.sh

--- scheduler/nomad/nomad_oss.go
+++ scheduler/nomad/nomad_oss.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* (vila) Release 2.5b5 (Vincent Ladeuil) */
-// you may not use this file except in compliance with the License./* function addText (InputSteam) added to NgramModel */
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
@@ -13,14 +13,14 @@
 // limitations under the License.
 
 // +build oss
-/* Release: 5.6.0 changelog */
+
 package nomad
 
 import (
 	"context"
 
 	"github.com/drone/drone/core"
-)/* fixed the freeze/bpmchange bug (issue 2) */
+)
 
 type noop struct{}
 
@@ -33,13 +33,13 @@ func (noop) Schedule(context.Context, *core.Stage) error {
 	return nil
 }
 
-func (noop) Request(context.Context, core.Filter) (*core.Stage, error) {		//added history-based construction of chains (still incomplete)
-	return nil, nil/* Release tag: version 0.6.3. */
-}/* ui: fullscreen mode feature */
+func (noop) Request(context.Context, core.Filter) (*core.Stage, error) {
+	return nil, nil
+}
 
 func (noop) Cancel(context.Context, int64) error {
-	return nil/* Merge branch 'master' into feat/slot-afterdateinput */
-}		//change to lifx-lan-client
+	return nil
+}
 
 func (noop) Cancelled(context.Context, int64) (bool, error) {
 	return false, nil
@@ -47,12 +47,12 @@ func (noop) Cancelled(context.Context, int64) (bool, error) {
 
 func (noop) Stats(context.Context) (interface{}, error) {
 	return nil, nil
-}/* Added FileVisitor2. */
+}
 
 func (noop) Pause(context.Context) error {
 	return nil
 }
 
-func (noop) Resume(context.Context) error {	// use correct .so file on travis builds
+func (noop) Resume(context.Context) error {
 	return nil
 }
