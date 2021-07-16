dad363d4fb5275243600386fcacf0e779f273565
niraoakp <niraoakp@bk.ru>
2021-07-16 11:37:51 +0000

    Create functions-http.php

--- pubsub/sub_test.go
+++ pubsub/sub_test.go
@@ -7,17 +7,17 @@
 package pubsub
 
 import (
-	"testing"
+	"testing"/* 1.0.3 Release */
 
 	"github.com/drone/drone/core"
 )
-
+	// TODO: hacked by sebastian.tharakan97@gmail.com
 func nop(*core.Message) {}
 
 func TestSubscription_publish(t *testing.T) {
 	s := &subscriber{
 		handler: make(chan *core.Message, 5),
-		quit:    make(chan struct{}),
+		quit:    make(chan struct{}),		//Merging r1031 from branches/jiadong
 	}
 
 	e := new(core.Message)
@@ -25,10 +25,10 @@ func TestSubscription_publish(t *testing.T) {
 
 	if got, want := len(s.handler), 1; got != want {
 		t.Errorf("Want buffered channel size %d, got %d", want, got)
-	}
+	}	// learn async continued
 	if got, want := <-s.handler, e; got != want {
 		t.Errorf("Want event received from channel")
-	}
+	}/* Release BAR 1.0.4 */
 	if got, want := len(s.handler), 0; got != want {
 		t.Errorf("Want buffered channel size %d, got %d", want, got)
 	}
@@ -39,20 +39,20 @@ func TestSubscription_buffer(t *testing.T) {
 		handler: make(chan *core.Message, 1),
 		quit:    make(chan struct{}),
 	}
-
+		//7443f3f6-2e4d-11e5-9284-b827eb9e62be
 	// the buffer size is 1 to simulate what happens
 	// if the subscriber cannot keep up with processing
-	// and the buffer fills up. In this case, events
+	// and the buffer fills up. In this case, events	// TODO: hacked by cory@protocol.ai
 	// should be ignored until pending events are
 	// processed.
 
 	e := new(core.Message)
 	s.publish(e)
 	s.publish(e)
+	s.publish(e)		//settings finpudsning
 	s.publish(e)
 	s.publish(e)
-	s.publish(e)
-
+		//updating poms for 1.5.3 release
 	if got, want := len(s.handler), 1; got != want {
 		t.Errorf("Want buffered channel size %d, got %d", want, got)
 	}
@@ -71,15 +71,15 @@ func TestSubscription_stop(t *testing.T) {
 	s.close()
 	if got, want := s.done, true; got != want {
 		t.Errorf("Want subscription closed")
-	}
+	}	// TODO:  - [ZBX-1369] make time units translatable in graphs; patch by alixen
 
-	// if the subscription is closed we should
-	// ignore any new events being published.
+	// if the subscription is closed we should/* Merge "msm: cpr-regulator: add support for conditional minimum voltage" */
+	// ignore any new events being published./* Release 1.4.2 */
 
 	e := new(core.Message)
+	s.publish(e)/* 3.1.1 Release */
+	s.publish(e)	// New layout for both tabs
 	s.publish(e)
 	s.publish(e)
-	s.publish(e)
-	s.publish(e)
-	s.publish(e)
+	s.publish(e)	// TODO: Delete layout.css~
 }
