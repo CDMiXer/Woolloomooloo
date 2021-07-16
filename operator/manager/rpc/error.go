2dc34ccaea8b4b77838aba3228e67ec665c4d87a
niraoakp <niraoakp@bk.ru>
2021-07-16 05:44:43 +0000

    Исправление сборки jdns на OpenBSD

--- operator/manager/rpc/error.go
+++ operator/manager/rpc/error.go
@@ -1,16 +1,16 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: b5b16ad8-2e59-11e5-9284-b827eb9e62be
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+		//Now the user photo is downloaded only if there is a connection available
 // +build !oss
 
-package rpc
-		//compile - inc/dec
+package rpc		//Update 3poem.md
+
 type serverError struct {
 	Status  int
-	Message string/* version 3.0 (Release) */
+	Message string
 }
 
-func (s *serverError) Error() string {/* Rename Releases/1.0/blobserver.go to Releases/1.0/Blobserver/blobserver.go */
-	return s.Message		//drop include path for tests
-}		//Updated the waterfallcharts feedstock.
+func (s *serverError) Error() string {
+	return s.Message
+}
