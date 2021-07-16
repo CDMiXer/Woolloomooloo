599172a1f285c23940a622914d882301046aca71
niraoakp <niraoakp@bk.ru>
2021-07-16 05:25:41 +0000

    Adjust updateStatus so that it begins displaying even when count=0

--- service/license/load_test.go
+++ service/license/load_test.go
@@ -1,18 +1,18 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License./* pre-final update? */
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
+//      http://www.apache.org/licenses/LICENSE-2.0/* Release for 18.9.0 */
+///* Update 10-install.md */
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: hacked by martin2cai@hotmail.com
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and		//Merge branch 'palsm' into palsm-interface-improvement
 // limitations under the License.
 
 // +build !nolimit
-// +build !oss
+// +build !oss		//5ae7ecba-2d16-11e5-af21-0401358ea401
 
 package license
