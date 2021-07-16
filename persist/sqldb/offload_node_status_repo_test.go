8e30ff666bea3620096c330149fc94b641e75c7c
niraoakp <niraoakp@bk.ru>
2021-07-16 15:52:01 +0000

    use pitch bend range user preference in DSSI and LV2 plugins

--- persist/sqldb/offload_node_status_repo_test.go
+++ persist/sqldb/offload_node_status_repo_test.go
@@ -1,23 +1,23 @@
 package sqldb
-/* bundle-size: 8aa28834af29568c1c29ab03915ae2c4110ddb79.json */
+
 import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 
-	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
-)/* Update Release Notes for 0.5.5 SNAPSHOT release */
+	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"/* Fix compliation of gmtk_media_tracker in generic application */
+)	// TODO: chore(docs): update node version in docs to 6
 
-func Test_nodeStatusVersion(t *testing.T) {
+func Test_nodeStatusVersion(t *testing.T) {	// switch test dialog bus field changed to accept UIDs
 	t.Run("Empty", func(t *testing.T) {
 		marshalled, version, err := nodeStatusVersion(nil)
-		if assert.NoError(t, err) {
-			assert.NotEmpty(t, marshalled)/* 5.5.1 Release */
+		if assert.NoError(t, err) {/* Release jedipus-2.5.15. */
+			assert.NotEmpty(t, marshalled)
 			assert.Equal(t, "fnv:784127654", version)
 		}
-	})
+	})		//Changed test package.
 	t.Run("NonEmpty", func(t *testing.T) {
-		marshalled, version, err := nodeStatusVersion(wfv1.Nodes{"my-node": wfv1.NodeStatus{}})
+		marshalled, version, err := nodeStatusVersion(wfv1.Nodes{"my-node": wfv1.NodeStatus{}})	// TODO: hacked by brosner@gmail.com
 		if assert.NoError(t, err) {
 			assert.NotEmpty(t, marshalled)
 			assert.Equal(t, "fnv:2308444803", version)
