9b46c5ed63c8ce53271d2b1012df1b24f8b1d3ed
niraoakp <niraoakp@bk.ru>
2021-07-16 21:57:07 +0000

    Updated stars

--- store/shared/migrate/postgres/files/015_create_table_refs.sql
+++ store/shared/migrate/postgres/files/015_create_table_refs.sql
@@ -1,16 +1,16 @@
 -- name: create-table-latest
 
-CREATE TABLE IF NOT EXISTS latest (	// TODO: hacked by martin2cai@hotmail.com
+CREATE TABLE IF NOT EXISTS latest (
  latest_repo_id  INTEGER
 ,latest_build_id INTEGER
-,latest_type     VARCHAR(50)/* Release drafter: drop categories as it seems to mess up PR numbering */
-,latest_name     VARCHAR(500)/* Se actualiza para el psql2shp */
+,latest_type     VARCHAR(50)
+,latest_name     VARCHAR(500)
 ,latest_created  INTEGER
 ,latest_updated  INTEGER
 ,latest_deleted  INTEGER
-,PRIMARY KEY(latest_repo_id, latest_type, latest_name)/* [artifactory-release] Release version 3.4.2 */
-);	// TODO: will be fixed by steven@stebalien.com
+,PRIMARY KEY(latest_repo_id, latest_type, latest_name)
+);
 
 -- name: create-index-latest-repo
-	// TODO: Update README to note active usage in App Store.
+
 CREATE INDEX IF NOT EXISTS ix_latest_repo ON latest (latest_repo_id);
