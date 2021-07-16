01afc464d539a46d5b3e03c7f9a8b471c59a800c
niraoakp <niraoakp@bk.ru>
2021-07-16 02:17:01 +0000

    view search: add help message

--- core/trigger_test.go
+++ core/trigger_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file.		//Updated PostgreSQL links to point to "current" rather than hardcoded version
 
 // +build !oss
 
-package core
+package core/* Translated to PT */
