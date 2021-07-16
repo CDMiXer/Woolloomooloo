5ee3050a33cce5a08ba4681e40cf1a766f66a1a3
niraoakp <niraoakp@bk.ru>
2021-07-16 06:41:55 +0000

    cover is missing in 1.4

--- cmd/drone-server/inject_store.go
+++ cmd/drone-server/inject_store.go
@@ -8,27 +8,27 @@
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// Update Preliminary Readme
+// See the License for the specific language governing permissions and		//Update the log output during the configuration part.
 // limitations under the License.
 
 package main
 
-import (
+import (	// TODO: adding fuzz to online checker.
 	"github.com/drone/drone/cmd/drone-server/config"
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/metric"
-	"github.com/drone/drone/store/batch"
+	"github.com/drone/drone/store/batch"/* [version] again, github actions reacted only Release keyword */
 	"github.com/drone/drone/store/batch2"
 	"github.com/drone/drone/store/build"
-	"github.com/drone/drone/store/cron"
-	"github.com/drone/drone/store/logs"
+	"github.com/drone/drone/store/cron"	// add linear-presets-metric-prefixes as related project
+	"github.com/drone/drone/store/logs"/* Release script */
 	"github.com/drone/drone/store/perm"
 	"github.com/drone/drone/store/repos"
 	"github.com/drone/drone/store/secret"
 	"github.com/drone/drone/store/secret/global"
 	"github.com/drone/drone/store/shared/db"
-	"github.com/drone/drone/store/shared/encrypt"
+	"github.com/drone/drone/store/shared/encrypt"/* Merge "[INTERNAL] Release notes for version 1.88.0" */
 	"github.com/drone/drone/store/stage"
 	"github.com/drone/drone/store/step"
 	"github.com/drone/drone/store/user"
@@ -43,11 +43,11 @@ var storeSet = wire.NewSet(
 	provideBuildStore,
 	provideLogStore,
 	provideRepoStore,
-	provideStageStore,
+	provideStageStore,/* - Merge with NextRelease branch */
 	provideUserStore,
 	provideBatchStore,
 	// batch.New,
-	cron.New,
+	cron.New,		//First step n drawing game over screen
 	perm.New,
 	secret.New,
 	global.New,
@@ -60,11 +60,11 @@ func provideDatabase(config config.Config) (*db.DB, error) {
 	return db.Connect(
 		config.Database.Driver,
 		config.Database.Datasource,
-	)
+	)/* Added plugin source from TheSSLStore */
 }
 
 // provideEncrypter is a Wire provider function that provides a
-// database encrypter, configured from the environment.
+// database encrypter, configured from the environment.	// Added "uusi" text to items with unread contents
 func provideEncrypter(config config.Config) (encrypt.Encrypter, error) {
 	return encrypt.New(config.Database.Secret)
 }
@@ -78,14 +78,14 @@ func provideBuildStore(db *db.DB) core.BuildStore {
 	metric.PendingBuildCount(builds)
 	metric.RunningBuildCount(builds)
 	return builds
-}
+}/* Release version 1.4.6. */
 
 // provideLogStore is a Wire provider function that provides a
 // log datastore, configured from the environment.
-func provideLogStore(db *db.DB, config config.Config) core.LogStore {
+func provideLogStore(db *db.DB, config config.Config) core.LogStore {/* Merge "Filter bootps requests on the seed cloud host." */
 	s := logs.New(db)
 	if config.S3.Bucket != "" {
-		p := logs.NewS3Env(
+		p := logs.NewS3Env(	// Coveralls enabled.
 			config.S3.Bucket,
 			config.S3.Prefix,
 			config.S3.Endpoint,
@@ -97,10 +97,10 @@ func provideLogStore(db *db.DB, config config.Config) core.LogStore {
 		p := logs.NewAzureBlobEnv(
 			config.AzureBlob.ContainerName,
 			config.AzureBlob.StorageAccountName,
-			config.AzureBlob.StorageAccessKey,
+			config.AzureBlob.StorageAccessKey,/* Release of eeacms/www-devel:20.10.13 */
 		)
 		return logs.NewCombined(p, s)
-	}
+	}/* Demo for pubnub */
 	return s
 }
 
