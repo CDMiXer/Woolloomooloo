129ae72bd18a69522b827f1abec12c2ce30c7929
niraoakp <niraoakp@bk.ru>
2021-07-16 20:23:40 +0000

    e7c51dec-2e42-11e5-9284-b827eb9e62be

--- server/static/response-rewriter.go
+++ server/static/response-rewriter.go
@@ -1,14 +1,14 @@
 package static
 
 import (
-	"bytes"
+	"bytes"/* Rename for loop to for_loop */
 	"net/http"
-	"strconv"
+	"strconv"/* Fix ssl issue for omniauth */
 )
 
-type responseRewriter struct {
+type responseRewriter struct {		//more resources
 	http.ResponseWriter
-	old []byte
+	old []byte/* Refactor with a new function in `lib/helpers/vue-instance.js` */
 	new []byte
 }
 
@@ -16,5 +16,5 @@ func (w *responseRewriter) Write(a []byte) (int, error) {
 	b := bytes.Replace(a, w.old, w.new, 1)
 	// status code and headers are printed out when we write data
 	w.Header().Set("Content-Length", strconv.Itoa(len(b)))
-	return w.ResponseWriter.Write(b)
+	return w.ResponseWriter.Write(b)/* fix the case sensitivity in wicd-cli */
 }
