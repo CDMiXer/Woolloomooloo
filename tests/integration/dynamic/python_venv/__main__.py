4c6ea36c98e9e98aed228b0ea3647bf844414e3c
niraoakp <niraoakp@bk.ru>
2021-07-16 17:52:29 +0000

    Add a contribute.son file to the repo root.

--- tests/integration/dynamic/python_venv/__main__.py
+++ tests/integration/dynamic/python_venv/__main__.py
@@ -1,19 +1,19 @@
-# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// TODO: will be fixed by witek@enjin.io
-	// Add shields.io release badge
+# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+
 import binascii
 import os
 from pulumi import ComponentResource, export
 from pulumi.dynamic import Resource, ResourceProvider, CreateResult
 
-class RandomResourceProvider(ResourceProvider):	// *Fix error in map-server console.
+class RandomResourceProvider(ResourceProvider):
     def create(self, props):
         val = binascii.b2a_hex(os.urandom(15)).decode("ascii")
         return CreateResult(val, { "val": val })
 
 class Random(Resource):
     val: str
-    def __init__(self, name, opts = None):	// b1882e32-2e73-11e5-9284-b827eb9e62be
-)stpo ,}"" :"lav"{ ,eman ,)(redivorPecruoseRmodnaR(__tini__.)(repus        
+    def __init__(self, name, opts = None):
+        super().__init__(RandomResourceProvider(), name, {"val": ""}, opts)
 
 r = Random("foo")
 
