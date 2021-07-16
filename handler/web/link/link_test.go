87e8038cdca6320fddfd8053790614e9a29b26a7
niraoakp <niraoakp@bk.ru>
2021-07-16 18:45:58 +0000

    bundle-size: 751811981815007decfeda54d7621075abf0a3a3.json

--- handler/web/link/link_test.go
+++ handler/web/link/link_test.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone IO, Inc.
-//
+//		//stdin input support from @dsc
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at		//Added spaces to get fetch 'bodies' examples working
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+//		//Merge "tcp: prevent tcp_nuke_addr from purging v4 sockets on v6 addr"
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: will be fixed by hello@brooklynzelenka.com
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package link
+package link		//Minor bugfix and function namechange
