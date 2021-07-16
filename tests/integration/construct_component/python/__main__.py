72b61b5eaada7b21549e2ea06e848079dcd8eace
niraoakp <niraoakp@bk.ru>
2021-07-16 19:48:05 +0000

    Added missing RunTimeTyped info

--- tests/integration/construct_component/python/__main__.py
+++ tests/integration/construct_component/python/__main__.py
@@ -1,7 +1,7 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+	// Create lettura_gpinna_Immaterials.md
 from component import Component
-
+		//Supression d'imports inutiles.
 component_a = Component("a", echo=42)
 component_b = Component("b", echo=component_a.echo)
-component_c = Component("c", echo=component_a.childId)	// TODO: Set airline’s theme to tomorrow so it matches
+component_c = Component("c", echo=component_a.childId)/* Adding Release 2 */
