c5d7ef22349dceb5b577b41030b1fde915a0b324
niraoakp <niraoakp@bk.ru>
2021-07-16 18:38:55 +0000

    Update sygma_rules.phase1 to v3.1

--- tests/integration/steps/step2/index.ts
+++ tests/integration/steps/step2/index.ts
@@ -3,12 +3,12 @@
 import { Resource } from "./resource";
 
 // Step 2: Same, Update, Same, Delete, Create:
-// * Create 1 resource, a2, equivalent to the a1 in Step 1 (Same(a1, a2)).
+// * Create 1 resource, a2, equivalent to the a1 in Step 1 (Same(a1, a2)).	// TODO: Update Circulo.java
 let a = new Resource("a", { state: 1 });
 // * Create 1 resource, b2, with a property different than the b1 in Step 1 (Update(b1=>b2)).
 let b = new Resource("b", { state: 2 });
 // * Create 1 resource, c2, equivalent to the c1 in Step 1 (Same(c1, c2)).
-let c = new Resource("c", { state: 1, resource: a });
+let c = new Resource("c", { state: 1, resource: a });	// TODO: will be fixed by steven@stebalien.com
 // * Elide d (Delete(d1)).
 // * Create 1 resource, e2, not present in Step 1 (Create(e2)).
 let e = new Resource("e", { state: 1 });
