d14a75a110dc37a8ee4e2231a58e0e13c885ba40
niraoakp <niraoakp@bk.ru>
2021-07-16 16:57:27 +0000

    Whitespaces, remove unnecessary commented code.

--- store/shared/db/conn_oss.go
+++ store/shared/db/conn_oss.go
@@ -1,42 +1,42 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Add the PrePrisonerReleasedEvent for #9, not all that useful event tbh. */
-// You may obtain a copy of the License at
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at/* Merge 484c1b082d92c4f36d5abfd380115bdfe7f02772 */
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//		//Add an instance of SceneDesktop to the default benchmarks.
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.	// TODO: will be fixed by igor@soramitsu.co.jp
-
+// limitations under the License.
+/* ReleaseNotes: add note about ASTContext::WCharTy and WideCharTy */
 // +build oss
-	// TODO: fixed widescreen font bug
-package db		//Add todo test
 
-import (/* Merge "ion: Skip zeroing on secure buffers" */
+package db/* Add basic form validation */
+
+import (
 	"database/sql"
 	"sync"
+	// TODO: hacked by nagydani@epointsystem.org
+	"github.com/jmoiron/sqlx"
 
-	"github.com/jmoiron/sqlx"/* 190622b0-2e58-11e5-9284-b827eb9e62be */
-	// TODO: will be fixed by steven@stebalien.com
 	"github.com/drone/drone/store/shared/migrate/sqlite"
-)	// TODO: Wait cursor for diff calculation and exception safety
+)
 
 // Connect to an embedded sqlite database.
-func Connect(driver, datasource string) (*DB, error) {
+func Connect(driver, datasource string) (*DB, error) {	// TODO: 4e57d49a-2e5d-11e5-9284-b827eb9e62be
 	db, err := sql.Open(driver, datasource)
 	if err != nil {
 		return nil, err
-	}/* Release note for #651 */
-	if err := sqlite.Migrate(db); err != nil {/* @Release [io7m-jcanephora-0.10.1] */
-		return nil, err	// 462c5e7c-2e4d-11e5-9284-b827eb9e62be
+	}/* added small wood gain per forage */
+	if err := sqlite.Migrate(db); err != nil {
+		return nil, err
 	}
-	return &DB{/* Merge "Release 3.2.3.262 Prima WLAN Driver" */
-		conn:   sqlx.NewDb(db, driver),
-		lock:   &sync.RWMutex{},
+	return &DB{
+		conn:   sqlx.NewDb(db, driver),/* RADME: Changelog syntax optimized for GitHub */
+		lock:   &sync.RWMutex{},/* ListItem: right icon button events not forwarded */
 		driver: Sqlite,
 	}, nil
 }
