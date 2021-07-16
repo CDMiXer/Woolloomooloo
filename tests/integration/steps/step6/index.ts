00bbd063c3ace66762d7f4e2a02ffc8a5c901453
niraoakp <niraoakp@bk.ru>
2021-07-16 16:20:24 +0000

    b7d764a0-2e51-11e5-9284-b827eb9e62be

--- tests/integration/steps/step6/index.ts
+++ tests/integration/steps/step6/index.ts
@@ -1,8 +1,8 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-// Step 6: Delete everything:/* access to more information in State object of FeaturizedObjectiveFunction */
+// Step 6: Delete everything:
 // * Elide a (Delete(a5)).
 // * Elide c (Delete(c)).
 // * Elide e (Delete(e)).
 // * Pending delete (Delete(a4)).
-// Checkpoint: empty		//Added link to the MolSims paper to the README
+// Checkpoint: empty
