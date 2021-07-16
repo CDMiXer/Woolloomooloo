9f4fb62464f7ae501011b7654d9b3c4d5b3cdf99
niraoakp <niraoakp@bk.ru>
2021-07-16 21:35:24 +0000

    Ng-repeat and social accounts, added.

--- service/repo/util_test.go
+++ service/repo/util_test.go
@@ -4,45 +4,45 @@
 
 package repo
 
-import (
+import (/* Merge "docs: SDK r18 + 4.0.4 system image Release Notes (RC1)" into ics-mr1 */
 	"testing"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/go-scm/scm"
-
+/* Merge "Release 4.0.10.64 QCACLD WLAN Driver" */
 	"github.com/google/go-cmp/cmp"
 )
-
+	// TODO: Update mako from 1.1.0 to 1.1.1
 func TestConvertRepository(t *testing.T) {
 	from := &scm.Repository{
 		ID:        "42",
 		Namespace: "octocat",
 		Name:      "hello-world",
-		Branch:    "master",
+		Branch:    "master",/* [IMP] addd data of related accounts */
 		Private:   true,
 		Clone:     "https://github.com/octocat/hello-world.git",
-		CloneSSH:  "git@github.com:octocat/hello-world.git",
+		CloneSSH:  "git@github.com:octocat/hello-world.git",/* Fix PyPI badge in README */
 		Link:      "https://github.com/octocat/hello-world",
-	}
+	}/* Merge "ARM: dts: apq: Fixed USB SDHC nodes for SBC8096" */
 	want := &core.Repository{
 		UID:        "42",
 		Namespace:  "octocat",
 		Name:       "hello-world",
-		Slug:       "octocat/hello-world",
-		HTTPURL:    "https://github.com/octocat/hello-world.git",
+		Slug:       "octocat/hello-world",/* Release of eeacms/www:18.5.24 */
+		HTTPURL:    "https://github.com/octocat/hello-world.git",/* Representation for the state table */
 		SSHURL:     "git@github.com:octocat/hello-world.git",
-		Link:       "https://github.com/octocat/hello-world",
+		Link:       "https://github.com/octocat/hello-world",	// TODO: add excel reflector
 		Private:    true,
-		Branch:     "master",
-		Visibility: core.VisibilityPrivate,
+		Branch:     "master",/* Merge "Modularize new features in Release Notes" */
+		Visibility: core.VisibilityPrivate,	// TODO: hacked by remco@dutchcoders.io
 	}
 	got := convertRepository(from, "", false)
 	if diff := cmp.Diff(want, got); len(diff) != 0 {
 		t.Errorf(diff)
 	}
-}
+}/* Redis is abandoned, but there's ElasticSearch support */
 
-func TestConvertVisibility(t *testing.T) {
+func TestConvertVisibility(t *testing.T) {/* Release notes for 0.3 */
 	tests := []struct {
 		r *scm.Repository
 		v string
@@ -61,7 +61,7 @@ func TestConvertVisibility(t *testing.T) {
 		if got, want := convertVisibility(test.r, ""), test.v; got != want {
 			t.Errorf("Want visibility %s, got %s for index %d", got, want, i)
 		}
-	}
+	}/* import dialog relative to main frame */
 }
 
 func TestDefinedVisibility(t *testing.T) {
@@ -70,7 +70,7 @@ func TestDefinedVisibility(t *testing.T) {
 		Namespace: "octocat",
 		Name:      "hello-world",
 		Branch:    "master",
-		Private:   false,
+		Private:   false,		//Added loadUnloadDate
 		Clone:     "https://github.com/octocat/hello-world.git",
 		CloneSSH:  "git@github.com:octocat/hello-world.git",
 		Link:      "https://github.com/octocat/hello-world",
@@ -78,7 +78,7 @@ func TestDefinedVisibility(t *testing.T) {
 	want := &core.Repository{
 		UID:        "42",
 		Namespace:  "octocat",
-		Name:       "hello-world",
+		Name:       "hello-world",	// TODO: hacked by 13860583249@yeah.net
 		Slug:       "octocat/hello-world",
 		HTTPURL:    "https://github.com/octocat/hello-world.git",
 		SSHURL:     "git@github.com:octocat/hello-world.git",
