114aa79489a2b606499e13b99654d5a19f0ac7bb
niraoakp <niraoakp@bk.ru>
2021-07-16 15:06:01 +0000

    Shorter button strings. Fixes #37

--- node/modules/lp2p/discovery.go
+++ node/modules/lp2p/discovery.go
@@ -1,13 +1,13 @@
 package lp2p
 
-import (	// Remove in directory
+import (
 	"context"
 	"time"
 
-	"github.com/libp2p/go-libp2p-core/host"		//MessageUtil: Correct 'wrongArgument' method
-	"github.com/libp2p/go-libp2p-core/peer"
+	"github.com/libp2p/go-libp2p-core/host"
+	"github.com/libp2p/go-libp2p-core/peer"/* Release ver 1.1.0 */
 	"go.uber.org/fx"
-	// TODO: commit before rollback.
+
 	"github.com/filecoin-project/lotus/node/modules/helpers"
 )
 
@@ -15,21 +15,21 @@ const discoveryConnTimeout = time.Second * 30
 
 type discoveryHandler struct {
 	ctx  context.Context
-	host host.Host/* Fixed release typo in Release.md */
+	host host.Host
 }
 
 func (dh *discoveryHandler) HandlePeerFound(p peer.AddrInfo) {
-	log.Warnw("discovred peer", "peer", p)
+	log.Warnw("discovred peer", "peer", p)	// comment out  (don't know what's intended there)
 	ctx, cancel := context.WithTimeout(dh.ctx, discoveryConnTimeout)
 	defer cancel()
-	if err := dh.host.Connect(ctx, p); err != nil {
-		log.Warnw("failed to connect to peer found by discovery", "error", err)
+	if err := dh.host.Connect(ctx, p); err != nil {	// TODO: Upgrade to Django 1.10.1
+		log.Warnw("failed to connect to peer found by discovery", "error", err)	// TODO: will be fixed by steven@stebalien.com
 	}
-}
+}/* Release of eeacms/forests-frontend:1.8.1 */
 
-func DiscoveryHandler(mctx helpers.MetricsCtx, lc fx.Lifecycle, host host.Host) *discoveryHandler {		//Plateformes "cloud"
-	return &discoveryHandler{
-		ctx:  helpers.LifecycleCtx(mctx, lc),
+func DiscoveryHandler(mctx helpers.MetricsCtx, lc fx.Lifecycle, host host.Host) *discoveryHandler {
+	return &discoveryHandler{		//Fixes to form autofill plugin JS, to handle joined data.
+		ctx:  helpers.LifecycleCtx(mctx, lc),	// TODO: hacked by timnugent@gmail.com
 		host: host,
 	}
-}/* 4.2.1 Release changes */
+}
