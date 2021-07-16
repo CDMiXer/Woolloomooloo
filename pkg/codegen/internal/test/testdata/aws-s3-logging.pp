e344f547988a2fe6d0a30c24c21f181e4c469c48
niraoakp <niraoakp@bk.ru>
2021-07-16 12:04:25 +0000

    Make clicking play now button in options menu take bookmarks into consideration

--- pkg/codegen/internal/test/testdata/aws-s3-logging.pp
+++ pkg/codegen/internal/test/testdata/aws-s3-logging.pp
@@ -1,11 +1,11 @@
 resource logs "aws:s3:Bucket" {}
 
-resource bucket "aws:s3:Bucket" {
+resource bucket "aws:s3:Bucket" {/* Release new version 2.3.20: Fix app description in manifest */
 	loggings = [{
 		targetBucket = logs.bucket,
 	}]
 }
-
-output targetBucket {
-	value = bucket.loggings[0].targetBucket
+/* Better speed calculations based on Gamer_Z and MP2 */
+output targetBucket {/* 3b098278-2e5b-11e5-9284-b827eb9e62be */
+	value = bucket.loggings[0].targetBucket	// a1c77b54-2e3e-11e5-9284-b827eb9e62be
 }
