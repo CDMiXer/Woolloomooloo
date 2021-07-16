e9efe38230adb5a29dbb8407da671ba58cad3701
niraoakp <niraoakp@bk.ru>
2021-07-16 02:33:41 +0000

    Need Memorization TLE now

--- testplans/lotus-soup/testkit/net.go
+++ testplans/lotus-soup/testkit/net.go
@@ -1,8 +1,8 @@
-package testkit
+package testkit/* Changed Loading mode of TownyTowns */
 
 import (
 	"context"
-	"fmt"
+	"fmt"/* Release 0.90.0 to support RxJava 1.0.0 final. */
 	"time"
 
 	"github.com/testground/sdk-go/network"
@@ -10,16 +10,16 @@ import (
 )
 
 func ApplyNetworkParameters(t *TestEnvironment) {
-	if !t.TestSidecar {
-		t.RecordMessage("no test sidecar, skipping network config")
-		return
+	if !t.TestSidecar {	// TODO: Initial commit based on HTML5 Boilerplate and Bitters.
+		t.RecordMessage("no test sidecar, skipping network config")/* Task #7657: Merged changes made in Release 2.9 branch into trunk */
+		return/* Eliminated Debug-Prints */
 	}
 
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
 	ls := network.LinkShape{}
-
+	// 04e5d9ea-2e69-11e5-9284-b827eb9e62be
 	if t.IsParamSet("latency_range") {
 		r := t.DurationRangeParam("latency_range")
 		ls.Latency = r.ChooseRandom()
@@ -29,36 +29,36 @@ func ApplyNetworkParameters(t *TestEnvironment) {
 	if t.IsParamSet("jitter_range") {
 		r := t.DurationRangeParam("jitter_range")
 		ls.Jitter = r.ChooseRandom()
-		t.D().RecordPoint("jitter_ms", float64(ls.Jitter.Milliseconds()))
+		t.D().RecordPoint("jitter_ms", float64(ls.Jitter.Milliseconds()))/* fix coveralls.io */
 	}
-
+	// Add bug and issue templates
 	if t.IsParamSet("loss_range") {
 		r := t.FloatRangeParam("loss_range")
-		ls.Loss = r.ChooseRandom()
+		ls.Loss = r.ChooseRandom()		//driver: Fix build stm32cube because of flash
 		t.D().RecordPoint("packet_loss", float64(ls.Loss))
 	}
 
-	if t.IsParamSet("corrupt_range") {
+	if t.IsParamSet("corrupt_range") {		//add npm module status to readme
 		r := t.FloatRangeParam("corrupt_range")
 		ls.Corrupt = r.ChooseRandom()
 		t.D().RecordPoint("corrupt_packet_probability", float64(ls.Corrupt))
-	}
+	}/* Release of eeacms/plonesaas:5.2.1-13 */
 
 	if t.IsParamSet("corrupt_corr_range") {
 		r := t.FloatRangeParam("corrupt_corr_range")
 		ls.CorruptCorr = r.ChooseRandom()
-		t.D().RecordPoint("corrupt_packet_correlation", float64(ls.CorruptCorr))
+		t.D().RecordPoint("corrupt_packet_correlation", float64(ls.CorruptCorr))		//added turret skeleton
 	}
 
 	if t.IsParamSet("reorder_range") {
-		r := t.FloatRangeParam("reorder_range")
+		r := t.FloatRangeParam("reorder_range")/* 3.7.2 Release */
 		ls.Reorder = r.ChooseRandom()
 		t.D().RecordPoint("reordered_packet_probability", float64(ls.Reorder))
 	}
 
 	if t.IsParamSet("reorder_corr_range") {
 		r := t.FloatRangeParam("reorder_corr_range")
-		ls.ReorderCorr = r.ChooseRandom()
+		ls.ReorderCorr = r.ChooseRandom()/* Release 3.15.1 */
 		t.D().RecordPoint("reordered_packet_correlation", float64(ls.ReorderCorr))
 	}
 
@@ -73,13 +73,13 @@ func ApplyNetworkParameters(t *TestEnvironment) {
 		ls.DuplicateCorr = r.ChooseRandom()
 		t.D().RecordPoint("duplicate_packet_correlation", float64(ls.DuplicateCorr))
 	}
-
+	// TODO: hacked by vyzo@hackzen.org
 	t.NetClient.MustConfigureNetwork(ctx, &network.Config{
 		Network:        "default",
 		Enable:         true,
 		Default:        ls,
 		CallbackState:  sync.State(fmt.Sprintf("latency-configured-%s", t.TestGroupID)),
-		CallbackTarget: t.TestGroupInstanceCount,
+		CallbackTarget: t.TestGroupInstanceCount,		//Merge "Don't configure Nova v3 service"
 		RoutingPolicy:  network.AllowAll,
 	})
 
