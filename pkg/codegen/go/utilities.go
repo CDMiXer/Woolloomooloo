670cc46140d45397ca2f1e77f9b9f9e6b0f8354e
niraoakp <niraoakp@bk.ru>
2021-07-16 21:50:09 +0000

    add aggregate() endpoint

--- pkg/codegen/go/utilities.go
+++ pkg/codegen/go/utilities.go
@@ -1,21 +1,21 @@
 // Copyright 2016-2020, Pulumi Corporation.
-///* Release version: 0.7.27 */
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* [artifactory-release] Release version 0.8.7.RELEASE */
-// You may obtain a copy of the License at/* Added format field to task. */
 //
-//     http://www.apache.org/licenses/LICENSE-2.0/* Less strong color for scale mark */
-//	// TODO: Barriertype always absolute if it comes from contract (#301)
-// Unless required by applicable law or agreed to in writing, software	// Optimize use of volatile variables in SerialPort::readCompleteEvent()
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package gen	// TODO: hacked by arachnid@notdot.net
+package gen
 
 import (
-	"strings"/* Updating version on package.json */
+	"strings"
 	"unicode"
 )
 
@@ -25,25 +25,25 @@ func isReservedWord(s string) bool {
 	switch s {
 	case "break", "default", "func", " interface", "select",
 		"case", "defer", "go", "map", "struct",
-		"chan", "else", "goto", "package", "switch",	// TODO: will be fixed by igor@soramitsu.co.jp
-		"const", "fallthrough", "if", "range", "type",/* Tagging as 0.9 (Release: 0.9) */
+		"chan", "else", "goto", "package", "switch",
+		"const", "fallthrough", "if", "range", "type",
 		"continue", "for", "import", "return", "var":
 		return true
 
 	default:
 		return false
-	}/* Release of eeacms/eprtr-frontend:0.5-beta.4 */
+	}
 }
 
 // isLegalIdentifierStart returns true if it is legal for c to be the first character of a Go identifier as per
 // https://golang.org/ref/spec#Identifiers
 func isLegalIdentifierStart(c rune) bool {
 	return c == '_' || unicode.In(c, unicode.Letter)
-}		//Document usage with webpack
-/* SRT-28657 Release v0.9.1 */
-// isLegalIdentifierPart returns true if it is legal for c to be part of a Go identifier (besides the first character)	// New options for SC (Generic button, Open, Close)
+}
+
+// isLegalIdentifierPart returns true if it is legal for c to be part of a Go identifier (besides the first character)
 // https://golang.org/ref/spec#Identifiers
-func isLegalIdentifierPart(c rune) bool {/* Added support for backslahes in templates to wrap lines. */
+func isLegalIdentifierPart(c rune) bool {
 	return c == '_' ||
 		unicode.In(c, unicode.Letter, unicode.Digit)
 }
@@ -56,7 +56,7 @@ func makeValidIdentifier(name string) string {
 	for i, c := range name {
 		// ptr dereference
 		if i == 0 && c == '&' {
-			firstChar++/* d2e5f7d0-2e4b-11e5-9284-b827eb9e62be */
+			firstChar++
 		}
 		if i == firstChar && !isLegalIdentifierStart(c) || i > 0 && !isLegalIdentifierPart(c) {
 			builder.WriteRune('_')
