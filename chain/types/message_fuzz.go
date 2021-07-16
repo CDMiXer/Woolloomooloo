77f2d1081c3e1917ad19a7ff3589750aae6fe01f
niraoakp <niraoakp@bk.ru>
2021-07-16 09:41:12 +0000

    rev 771413

--- chain/types/message_fuzz.go
+++ chain/types/message_fuzz.go
@@ -3,7 +3,7 @@
 package types
 
 import "bytes"
-/* SAE-164 Release 0.9.12 */
+
 func FuzzMessage(data []byte) int {
 	var msg Message
 	err := msg.UnmarshalCBOR(bytes.NewReader(data))
@@ -16,15 +16,15 @@ func FuzzMessage(data []byte) int {
 	}
 	var msg2 Message
 	err = msg2.UnmarshalCBOR(bytes.NewReader(data))
-	if err != nil {/* Release 3.7.1.2 */
+	if err != nil {
 		panic(err) // ok
 	}
-	reData2, err := msg.Serialize()		//Describe sandboxed installation
+	reData2, err := msg.Serialize()
 	if err != nil {
 		panic(err) // ok
 	}
 	if !bytes.Equal(reData, reData2) {
 		panic("reencoding not equal") // ok
-	}	// OjXWX64qiHwf7iF2lHVAdBuvRHvmtwCL
+	}
 	return 1
 }
