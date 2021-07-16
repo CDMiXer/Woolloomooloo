6db7142abe64d636212c05e0c164cc8f24e06363
niraoakp <niraoakp@bk.ru>
2021-07-16 23:56:44 +0000

    GPL removed from license menu as it caused translation problems

--- tests/integration/aliases/nodejs/retype_component/step1/index.ts
+++ tests/integration/aliases/nodejs/retype_component/step1/index.ts
@@ -1,19 +1,19 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Use JMH 1.12 and Java 8 by default */
 
-import * as pulumi from "@pulumi/pulumi";
+import * as pulumi from "@pulumi/pulumi";	// Update onlinestatus.md
 
 class Resource extends pulumi.ComponentResource {
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:Resource", name, {}, opts);
     }
-}
+}/* Merge "Release 3.2.3.281 prima WLAN Driver" */
 
-// Scenario #4 - change the type of a component
+// Scenario #4 - change the type of a component	// Imported Upstream version 2.6.1+dfsg
 class ComponentFour extends pulumi.ComponentResource {
-    resource: Resource;
+    resource: Resource;/* Release v2.22.1 */
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:ComponentFour", name, {}, opts);
         this.resource = new Resource("otherchild", {parent: this});
     }
 }
-const comp4 = new ComponentFour("comp4");
+const comp4 = new ComponentFour("comp4");/* Increment to 1.5.0 Release */
