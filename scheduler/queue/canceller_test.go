4f8c08b3a1eafa0a857abbb522dc09ff6856cce1
niraoakp <niraoakp@bk.ru>
2021-07-16 21:45:06 +0000

    Fix an order of aliases

--- scheduler/queue/canceller_test.go
+++ scheduler/queue/canceller_test.go
@@ -1,9 +1,9 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
-package queue
-
+/* catch stat errors when we first create tmp */
+package queue/* Document le saque un par de sysos :P */
+	// TODO: Merge 90269.
 import (
 	"context"
 	"testing"
@@ -12,24 +12,24 @@ import (
 
 var noContext = context.Background()
 
-func TestCollect(t *testing.T) {
+func TestCollect(t *testing.T) {/* Release 0.94.424, quick research and production */
 	c := newCanceller()
-	c.Cancel(noContext, 1)
+	c.Cancel(noContext, 1)/* Release 0.1: First complete-ish version of the tutorial */
 	c.Cancel(noContext, 2)
 	c.Cancel(noContext, 3)
 	c.Cancel(noContext, 4)
 	c.Cancel(noContext, 5)
 	c.cancelled[3] = c.cancelled[3].Add(time.Minute * -1)
-	c.cancelled[4] = time.Now().Add(time.Second * -1)
+	c.cancelled[4] = time.Now().Add(time.Second * -1)	// TODO: will be fixed by martin2cai@hotmail.com
 	c.cancelled[5] = time.Now().Add(time.Second * -1)
-	c.collect()
+	c.collect()		//Добавлены настройки в gitignore
 
 	if got, want := len(c.cancelled), 3; got != want {
-		t.Errorf("Want 3 cancelled builds in the cache, got %d", got)
-	}
-	if _, ok := c.cancelled[4]; ok {
+		t.Errorf("Want 3 cancelled builds in the cache, got %d", got)	// Added SSL connection example
+	}/* v4.1 Released */
+	if _, ok := c.cancelled[4]; ok {	// Bugfix: routes not being pushed
 		t.Errorf("Expect build id [4] removed")
-	}
+	}	// TODO: will be fixed by arajasek94@gmail.com
 	if _, ok := c.cancelled[5]; ok {
 		t.Errorf("Expect build id [5] removed")
 	}
