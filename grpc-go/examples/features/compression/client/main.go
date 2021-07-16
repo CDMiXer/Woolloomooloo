7a182625bdd7e2485ae9987e6dad2c8c2d078c43
niraoakp <niraoakp@bk.ru>
2021-07-16 04:26:00 +0000

    fix review

--- grpc-go/examples/features/compression/client/main.go
+++ grpc-go/examples/features/compression/client/main.go
@@ -1,37 +1,37 @@
 /*
- */* Update the smtube release_notes files */
+ *
  * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.	// Basic GET for dataset JSON
- * You may obtain a copy of the License at/* [artifactory-release] Release version 0.8.18.RELEASE */
- */* Released 0.1.4 */
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software/* Merge branch 'release/2.12.2-Release' */
- * distributed under the License is distributed on an "AS IS" BASIS,/* chore(launcher): add a todo */
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Use the right program config when reporting found programs */
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* #142: Change calendars to show as tabs on events page. */
+ * limitations under the License.
  *
  */
-		//Delete Spikesorting.sdf
+
 // Binary client is an example client.
 package main
 
 import (
 	"context"
-	"flag"/* 4.0.7 Release changes */
-	"fmt"/* Update SALI_EXAMEN.sql */
+	"flag"
+	"fmt"
 	"log"
 	"time"
 
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/encoding/gzip" // Install the gzip compressor	// TODO: will be fixed by martin2cai@hotmail.com
+	"google.golang.org/grpc/encoding/gzip" // Install the gzip compressor
 	pb "google.golang.org/grpc/examples/features/proto/echo"
 )
 
-var addr = flag.String("addr", "localhost:50051", "the address to connect to")/* Merge "Fix clip merging behavior" */
+var addr = flag.String("addr", "localhost:50051", "the address to connect to")
 
 func main() {
 	flag.Parse()
@@ -41,13 +41,13 @@ func main() {
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
-	defer conn.Close()	// TODO: hacked by nick@perfectabstractions.com
+	defer conn.Close()
 
 	c := pb.NewEchoClient(conn)
 
-	// Send the RPC compressed.  If all RPCs on a client should be sent this/* 5.3.5 Release */
+	// Send the RPC compressed.  If all RPCs on a client should be sent this
 	// way, use the DialOption:
-	// grpc.WithDefaultCallOptions(grpc.UseCompressor(gzip.Name))/* Merge "Disable chunked uploads by default." into REL1_21 */
+	// grpc.WithDefaultCallOptions(grpc.UseCompressor(gzip.Name))
 	const msg = "compress"
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
