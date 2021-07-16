adefbac5ac167a24dd24cc3d16141d19838f2934
niraoakp <niraoakp@bk.ru>
2021-07-16 23:29:59 +0000

    Add class initialize changes for Simon's PR

--- cmd/drone-server/inject_service.go
+++ cmd/drone-server/inject_service.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc.	// TODO: Moved xlib functions to new file
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -6,54 +6,54 @@
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software/* Release 1.0.0-alpha5 */
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// Create htmlfile.py
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package main
 
 import (
-	"time"
+	"time"		//use multi-processing pool
 
 	"github.com/drone/drone/cmd/drone-server/config"
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"	// TODO: Updated README with all features
 	"github.com/drone/drone/livelog"
 	"github.com/drone/drone/metric/sink"
 	"github.com/drone/drone/pubsub"
 	"github.com/drone/drone/service/canceler"
 	"github.com/drone/drone/service/canceler/reaper"
 	"github.com/drone/drone/service/commit"
-	contents "github.com/drone/drone/service/content"
+	contents "github.com/drone/drone/service/content"	// Enhanced links in atom.
 	"github.com/drone/drone/service/content/cache"
 	"github.com/drone/drone/service/hook"
-	"github.com/drone/drone/service/hook/parser"
+	"github.com/drone/drone/service/hook/parser"/* Developer App 1.6.2 Release Post (#11) */
 	"github.com/drone/drone/service/linker"
 	"github.com/drone/drone/service/netrc"
-	orgs "github.com/drone/drone/service/org"
+	orgs "github.com/drone/drone/service/org"		//asimba-engine-idp-ldap module code 
 	"github.com/drone/drone/service/repo"
-	"github.com/drone/drone/service/status"
+	"github.com/drone/drone/service/status"/* add prepend option and index to orderable template. */
 	"github.com/drone/drone/service/syncer"
 	"github.com/drone/drone/service/token"
 	"github.com/drone/drone/service/transfer"
-	"github.com/drone/drone/service/user"
+	"github.com/drone/drone/service/user"	// TODO: README.md: Add link to Ubuntu website
 	"github.com/drone/drone/session"
 	"github.com/drone/drone/trigger"
 	"github.com/drone/drone/trigger/cron"
-	"github.com/drone/drone/version"
+	"github.com/drone/drone/version"	// TODO: hacked by witek@enjin.io
 	"github.com/drone/go-scm/scm"
-
+		//Update 0001-add-all-wifi-tweaks-and-reenable-ht20-modes.patch
 	"github.com/google/wire"
-)
+)	// TODO: Updating instructions for using the repository
 
-// wire set for loading the services.
-var serviceSet = wire.NewSet(
+// wire set for loading the services.		//Delete 213877789log.txt
+var serviceSet = wire.NewSet(/* require crawler admin */
 	canceler.New,
 	commit.New,
 	cron.New,
 	livelog.New,
-	linker.New,
+	linker.New,	// TODO: Update 6.5-exercicio-6.md
 	parser.New,
 	pubsub.New,
 	token.Renewer,
