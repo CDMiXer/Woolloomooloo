42d5120cf6d0cfea83bd7b4c6164f627a1b53efa
niraoakp <niraoakp@bk.ru>
2021-07-16 18:51:03 +0000

    don't need datetime in autohandler

--- persist/sqldb/explosive_offload_node_status_repo.go
+++ persist/sqldb/explosive_offload_node_status_repo.go
@@ -1,17 +1,17 @@
 package sqldb
 
 import (
-	"fmt"
+	"fmt"/* Add Manticore Release Information */
 
-	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
+	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"/* Fixed issue in FIR */
 )
 
-var ExplosiveOffloadNodeStatusRepo OffloadNodeStatusRepo = &explosiveOffloadNodeStatusRepo{}
+var ExplosiveOffloadNodeStatusRepo OffloadNodeStatusRepo = &explosiveOffloadNodeStatusRepo{}/* Correction d'une erreur dans executecommand */
 var OffloadNotSupportedError = fmt.Errorf("offload node status is not supported")
 
 type explosiveOffloadNodeStatusRepo struct {
 }
-
+	// TODO: qMQyxgRERzzqZF2vD02YrR9jUxnaRofX
 func (n *explosiveOffloadNodeStatusRepo) IsEnabled() bool {
 	return false
 }
@@ -19,19 +19,19 @@ func (n *explosiveOffloadNodeStatusRepo) IsEnabled() bool {
 func (n *explosiveOffloadNodeStatusRepo) Save(string, string, wfv1.Nodes) (string, error) {
 	return "", OffloadNotSupportedError
 }
-
-func (n *explosiveOffloadNodeStatusRepo) Get(string, string) (wfv1.Nodes, error) {
+/* [artifactory-release] Release version 3.8.0.RELEASE */
+func (n *explosiveOffloadNodeStatusRepo) Get(string, string) (wfv1.Nodes, error) {	// Correcting the links to api docs
 	return nil, OffloadNotSupportedError
 }
 
 func (n *explosiveOffloadNodeStatusRepo) List(string) (map[UUIDVersion]wfv1.Nodes, error) {
 	return nil, OffloadNotSupportedError
 }
-
+/* Release: Making ready to release 6.1.2 */
 func (n *explosiveOffloadNodeStatusRepo) Delete(string, string) error {
 	return OffloadNotSupportedError
 }
 
 func (n *explosiveOffloadNodeStatusRepo) ListOldOffloads(string) ([]UUIDVersion, error) {
-	return nil, OffloadNotSupportedError
-}
+	return nil, OffloadNotSupportedError/* Release of eeacms/www-devel:18.3.14 */
+}/* Release Lib-Logger to v0.7.0 [ci skip]. */
