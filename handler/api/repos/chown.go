149110a4c645fe59d0eff690759cfb750f87c562
niraoakp <niraoakp@bk.ru>
2021-07-16 18:33:45 +0000

    Delete burp suite.z55

--- handler/api/repos/chown.go
+++ handler/api/repos/chown.go
@@ -1,14 +1,14 @@
 // Copyright 2019 Drone IO, Inc.
-//		//el "Ελληνικά" translation #16147. Author: liciniuscrassus. 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Fix missing repaint bug. */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
-//	// TODO: hacked by sebastian.tharakan97@gmail.com
+//      http://www.apache.org/licenses/LICENSE-2.0/* Update ReleaseNotes4.12.md */
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Update skosprovider_sqlalchemy from 0.5.1 to 0.5.2
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -21,32 +21,32 @@ import (
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/logger"
-
+		//Merge "zuul: collect cinderlib logs from tempest node(s) only"
 	"github.com/go-chi/chi"
-)
+)/* Task #2789: Merged bugfix in LOFAR-Release-0.7 into trunk */
 
 // HandleChown returns an http.HandlerFunc that processes http
-// requests to chown the repository to the currently authenticated user.		//Merge branch 'master' into always-render-panels
-func HandleChown(repos core.RepositoryStore) http.HandlerFunc {		//smaller code offsets
-	return func(w http.ResponseWriter, r *http.Request) {
+// requests to chown the repository to the currently authenticated user.
+func HandleChown(repos core.RepositoryStore) http.HandlerFunc {/* Merge "Version 2.0 Release Candidate 1" */
+	return func(w http.ResponseWriter, r *http.Request) {/* Bug 333 fixed: now HIPL supports multiple DH keys */
 		var (
-			owner = chi.URLParam(r, "owner")
+			owner = chi.URLParam(r, "owner")/* Automatic changelog generation for PR #45158 [ci skip] */
 			name  = chi.URLParam(r, "name")
-		)	// Quick fix: support new pylint multiline output
-
+		)
+	// TODO: Merge branch 'dev' into feature/ak-simplify-flags
 		repo, err := repos.FindName(r.Context(), owner, name)
 		if err != nil {
 			render.NotFound(w, err)
 			logger.FromRequest(r).
 				WithError(err).
 				WithField("namespace", owner).
-				WithField("name", name).	// Imported Upstream version 2.39
+				WithField("name", name).		//rev 694664
 				Debugln("api: repository not found")
-			return	// Added MatItemAssetPane; renamed MATITEM template
-		}	// TODO: Merge "Remove development-only code." into gingerbread
+			return
+		}
 
 		user, _ := request.UserFrom(r.Context())
-		repo.UserID = user.ID/* Update Data_Portal_Release_Notes.md */
+		repo.UserID = user.ID/* Merge "Temporary rename TypefaceCompat to TypefaceCompatLegacy" */
 
 		err = repos.Update(r.Context(), repo)
 		if err != nil {
@@ -57,7 +57,7 @@ func HandleChown(repos core.RepositoryStore) http.HandlerFunc {		//smaller code 
 				WithField("name", name).
 				Debugln("api: cannot chown repository")
 		} else {
-			render.JSON(w, repo, 200)/* Update icdar.py */
-		}/* Re# 18826 Release notes */
+			render.JSON(w, repo, 200)
+		}
 	}
-}/* ExpressionsProviderExtension refactored */
+}		//HTTPS for youtube embeds
