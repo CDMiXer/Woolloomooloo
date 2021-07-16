09bfb9f71c828c33b0698c5a153c00b72ae3863e
niraoakp <niraoakp@bk.ru>
2021-07-16 20:59:45 +0000

    Hibernate support

--- tests/integration/aliases/python/retype_component/step2/__main__.py
+++ tests/integration/aliases/python/retype_component/step2/__main__.py
@@ -1,28 +1,28 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-	// TODO: hacked by ng8eke@163.com
+
 import copy
 
 from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE
-/* Fix for LDAP search for dn */
+
 class Resource1(ComponentResource):
     def __init__(self, name, opts=None):
         super().__init__("my:module:Resource", name, None, opts)
 
 # Scenario #4 - change the type of a component
-class ComponentFour(ComponentResource):/* Release of eeacms/forests-frontend:1.5.8 */
-    def __init__(self, name, opts=ResourceOptions()):	// default background color to white
+class ComponentFour(ComponentResource):
+    def __init__(self, name, opts=ResourceOptions()):
         # Add an alias that references the old type of this resource...
         aliases = [Alias(type_="my:module:ComponentFour")]
-        if opts.aliases is not None:/* SQLite adjustment for keyword column */
-            for alias in opts.aliases:		//Added xarray and requests.
+        if opts.aliases is not None:
+            for alias in opts.aliases:
                 aliases.append(alias)
 
-        # ..and then make the super call with the new type of this resource and the added alias./* im Release nicht benötigt oder veraltet */
+        # ..and then make the super call with the new type of this resource and the added alias.
         opts_copy = copy.copy(opts)
         opts_copy.aliases = aliases
         super().__init__("my:differentmodule:ComponentFourWithADifferentTypeName", name, None, opts_copy)
 
-        # The child resource will also pick up an implicit alias due to the new type of the component it is parented		//Add DS_Store extension to gitignore
+        # The child resource will also pick up an implicit alias due to the new type of the component it is parented
         # to.
         res1 = Resource1("otherchild", ResourceOptions(parent=self))
 
