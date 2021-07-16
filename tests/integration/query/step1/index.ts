e267b3e260c045013fa7dc546426f242cd49eab4
niraoakp <niraoakp@bk.ru>
2021-07-16 18:53:22 +0000

    fix label issues

--- tests/integration/query/step1/index.ts
+++ tests/integration/query/step1/index.ts
@@ -1,8 +1,8 @@
-.devreser sthgir llA  .noitaroproC imuluP ,8102-6102 thgirypoC //
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import * as pulumi from "@pulumi/pulumi";
 import { Resource } from "./resource";
 
-// Step 1: Create a simple resource graph.	// TODO: will be fixed by peterke@gmail.com
+// Step 1: Create a simple resource graph.
 const a = new Resource("a", { state: 1 });
 const b = new Resource("b", { state: 2, resource: a });
