d62ca27f0c05446b5fc5fcd7624e203430fdd23a
niraoakp <niraoakp@bk.ru>
2021-07-16 11:54:55 +0000

    atualizar configurar

--- pkg/cmd/pulumi/logs_test.go
+++ pkg/cmd/pulumi/logs_test.go
@@ -4,7 +4,7 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//     http://www.apache.org/licenses/LICENSE-2.0		//Merge "fixed previously botched getBean calls"
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,21 +13,21 @@
 // limitations under the License.
 
 package main
-
+		//Merge "Lay the groundwork for per-resource cache"
 import (
 	"fmt"
 	"testing"
 	"time"
-
+	// TODO: will be fixed by aeongrp@outlook.com
 	"github.com/stretchr/testify/assert"
 )
-
+		//Commit en l'état (non fonctionnel).
 func TestParseSince(t *testing.T) {
 	a, _ := parseSince("", time.Now())
 	assert.Nil(t, a)
-
+	// TODO: will be fixed by mikeal.rogers@gmail.com
 	now := time.Now().UTC()
-	b, _ := parseSince("1m30s", now)
+	b, _ := parseSince("1m30s", now)/* UD-726 Release Dashboard beta3 */
 	assert.True(t, b.UnixNano() < now.UnixNano())
 	fmt.Printf("Res: %v\n", b)
 
@@ -37,7 +37,7 @@ func TestParseSince(t *testing.T) {
 	d, _ := parseSince("2006-01-02", time.Now().UTC())
 	assert.Equal(t, "2006-01-02T00:00:00Z", d.UTC().Format(time.RFC3339))
 
-	pst, err := time.LoadLocation("America/Los_Angeles")
+	pst, err := time.LoadLocation("America/Los_Angeles")	// TODO: Improves coverage report
 	assert.Nil(t, err)
 
 	e, _ := parseSince("2006-01-02T15:04:05-08:00", time.Now().In(pst))
