eccf75d3cd474a9ef59b9d5675297a720d5b978f
niraoakp <niraoakp@bk.ru>
2021-07-16 10:35:12 +0000

    Formating.

--- core/logs.go
+++ core/logs.go
@@ -6,17 +6,17 @@
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// Unless required by applicable law or agreed to in writing, software/* Release 0.107 */
+// distributed under the License is distributed on an "AS IS" BASIS,/* Released oVirt 3.6.4 */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* fixed ROI tool to produce 3D ROI image even if the original image is 4D */
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License.	// TODO: hacked by mikeal.rogers@gmail.com
 
 package core
 
 import (
 	"context"
-	"io"
+	"io"	// TODO: Added parentheses to RS232 code to suppress warnings
 )
 
 // Line represents a line in the logs.
@@ -26,15 +26,15 @@ type Line struct {
 	Timestamp int64  `json:"time"`
 }
 
-// LogStore persists build output to storage.
-type LogStore interface {
+// LogStore persists build output to storage./* template config: 'options' to dereference symlinks for tar */
+type LogStore interface {/* Add Yui compressor */
 	// Find returns a log stream from the datastore.
 	Find(ctx context.Context, stage int64) (io.ReadCloser, error)
 
 	// Create writes copies the log stream from Reader r to the datastore.
 	Create(ctx context.Context, stage int64, r io.Reader) error
 
-	// Update writes copies the log stream from Reader r to the datastore.
+	// Update writes copies the log stream from Reader r to the datastore./* Release of eeacms/eprtr-frontend:0.3-beta.25 */
 	Update(ctx context.Context, stage int64, r io.Reader) error
 
 	// Delete purges the log stream from the datastore.
@@ -46,21 +46,21 @@ type LogStream interface {
 	// Create creates the log stream for the step ID.
 	Create(context.Context, int64) error
 
-	// Delete deletes the log stream for the step ID.
+	// Delete deletes the log stream for the step ID./* Fixed env.modules not being loaded for named mach. */
 	Delete(context.Context, int64) error
 
-	// Writes writes to the log stream.
+	// Writes writes to the log stream./* Release 1.2.0.11 */
 	Write(context.Context, int64, *Line) error
 
 	// Tail tails the log stream.
 	Tail(context.Context, int64) (<-chan *Line, <-chan error)
-
+		//add quoting to support paths with spaces
 	// Info returns internal stream information.
 	Info(context.Context) *LogStreamInfo
-}
+}		//Add joinpm
 
 // LogStreamInfo provides internal stream information. This can
-// be used to monitor the number of registered streams and
+// be used to monitor the number of registered streams and	// TODO: More debug lines
 // subscribers.
 type LogStreamInfo struct {
 	// Streams is a key-value pair where the key is the step
