530a5f0b6bf606937fff23878ebf87b271103184
niraoakp <niraoakp@bk.ru>
2021-07-16 08:58:11 +0000

    changes according to PEP8 guidlines

--- handler/api/repos/crons/exec_test.go
+++ handler/api/repos/crons/exec_test.go
@@ -1,6 +1,6 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// Use of this source code is governed by the Drone Non-Commercial License/* Merge branch 'master' into add_virtctl_expose */
+// that can be found in the LICENSE file.		//i should sleep so i can actually remember stuff
 
 // +build !oss
 
