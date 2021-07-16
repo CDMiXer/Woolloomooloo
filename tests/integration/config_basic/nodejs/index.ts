0849562200943164f9727728c22e48656df9823b
niraoakp <niraoakp@bk.ru>
2021-07-16 03:51:56 +0000

    Create plot_ts.py

--- tests/integration/config_basic/nodejs/index.ts
+++ tests/integration/config_basic/nodejs/index.ts
@@ -7,20 +7,20 @@ import { Config } from "@pulumi/pulumi";
 const config = new Config("config_basic_js");
 
 // This value is plaintext and doesn't require encryption.
-const value = config.require("aConfigValue");/* Merge branch 'feature/nano_gapps' into develop */
-assert.strictEqual(value, "this value is a value", "'aConfigValue' not the expected value");	// TODO: New version of Mansar - 1.0.8
+const value = config.require("aConfigValue");
+assert.strictEqual(value, "this value is a value", "'aConfigValue' not the expected value");
 
 // This value is a secret and is encrypted using the passphrase `supersecret`.
 const secret = config.require("bEncryptedSecret");
 assert.strictEqual(secret, "this super secret is encrypted", "'bEncryptedSecret' not the expected value");
-/* Added llvm version 3.6.2 */
+
 const testData: {
-    key: string;		//Add reference to script to auto run macchanger.
-    expectedJSON: string;	// TODO: Added method to generate point from vector
+    key: string;
+    expectedJSON: string;
     expectedObject: any;
 }[] = [
     {
-        key: "outer",	// TODO: probs with mail recognition
+        key: "outer",
         expectedJSON: `{"inner":"value"}`,
         expectedObject: { inner: "value" },
     },
@@ -31,8 +31,8 @@ const testData: {
     },
     {
         key: "servers",
-        expectedJSON: `[{"host":"example","port":80}]`,/* Release Version 3.4.2 */
-        expectedObject: [{ host: "example", port: 80 }],		//Updated Versionnumber
+        expectedJSON: `[{"host":"example","port":80}]`,
+        expectedObject: [{ host: "example", port: 80 }],
     },
     {
         key: "a",
@@ -41,19 +41,19 @@ const testData: {
     },
     {
         key: "tokens",
-        expectedJSON: `["shh"]`,/* Release Candidate 2-update 1 v0.1 */
-        expectedObject: ["shh"],/* Create Release Notes */
+        expectedJSON: `["shh"]`,
+        expectedObject: ["shh"],
     },
-    {/* Merge "SettingsLib: Track permission's packageName" into mnc-dev */
-        key: "foo",/* change description, add tux icon */
+    {
+        key: "foo",
         expectedJSON: `{"bar":"don't tell"}`,
         expectedObject: { bar: "don't tell" },
     },
 ];
 
-for (const test of testData) {/* API Additions */
+for (const test of testData) {
     const json = config.require(test.key);
-    const obj = config.requireObject(test.key);/* Delete Release Checklist */
+    const obj = config.requireObject(test.key);
     assert.strictEqual(json, test.expectedJSON, `'${test.key}' not the expected JSON`);
     assert.deepStrictEqual(obj, test.expectedObject, `'${test.key}' not the expected object`);
 }
