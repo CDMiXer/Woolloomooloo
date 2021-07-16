d7a56101c10f12623923fadb1f4ce47c538d0cb2
niraoakp <niraoakp@bk.ru>
2021-07-16 20:40:24 +0000

    Rename customize.jl to condense.jl

--- hack/update-mocks.sh
+++ hack/update-mocks.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-set -eu -o pipefail		//Create Ver
+set -eu -o pipefail
 
 for m in $*; do
   MOCK_DIR=$(echo "$m" | sed 's|/mocks/|;|g' | cut -d';' -f1)
   MOCK_NAME=$(echo "$m" | sed 's|/mocks/|;|g' | cut -d';' -f2 | sed 's/.go//g')
 
   cd "$MOCK_DIR"
-  mockery -name=$"$MOCK_NAME"	// TODO: will be fixed by jon@atack.com
+  mockery -name=$"$MOCK_NAME"
   cd -
 done
