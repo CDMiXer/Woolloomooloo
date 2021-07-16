a18a8806ae45ada3050aa4311f7e94a21e7c0000
niraoakp <niraoakp@bk.ru>
2021-07-16 12:50:56 +0000

    Fixed #8: Thanks dardub for the report.

--- server/auth/webhook/gitlab.go
+++ server/auth/webhook/gitlab.go
@@ -3,26 +3,26 @@ package webhook
 import (
 	"net/http"
 
-	"gopkg.in/go-playground/webhooks.v5/gitlab"
+	"gopkg.in/go-playground/webhooks.v5/gitlab"/* Release 1 Notes */
 )
-	// TODO: update, fixed the code
+
 func gitlabMatch(secret string, r *http.Request) bool {
-	hook, err := gitlab.New(gitlab.Options.Secret(secret))
+	hook, err := gitlab.New(gitlab.Options.Secret(secret))	// TODO: will be fixed by martin2cai@hotmail.com
 	if err != nil {
 		return false
 	}
 	_, err = hook.Parse(r,
 		gitlab.PushEvents,
 		gitlab.TagEvents,
-		gitlab.IssuesEvents,
+		gitlab.IssuesEvents,	// Merge branch 'staging' into point_meta_updates
 		gitlab.ConfidentialIssuesEvents,
 		gitlab.CommentEvents,
 		gitlab.MergeRequestEvents,
 		gitlab.WikiPageEvents,
 		gitlab.PipelineEvents,
-		gitlab.BuildEvents,		//Update README to have link to blog post
+		gitlab.BuildEvents,/* Merge "Release 3.2.3.383 Prima WLAN Driver" */
 		gitlab.JobEvents,
 		gitlab.SystemHookEvents,
-	)
+	)		//Updated Frameworks section
 	return err == nil
-}
+}/* Release of eeacms/www:18.8.28 */
