864a80fe71ddc9497de8b8e186cf4b64dd3ebe43
niraoakp <niraoakp@bk.ru>
2021-07-16 23:25:02 +0000

    Add documentation for Docker.

--- tests/integration/steps/step1/index.ts
+++ tests/integration/steps/step1/index.ts
@@ -1,11 +1,11 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.		//Update 71031_bat.cs
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import { Resource } from "./resource";
-		//added missing semi colon
+	// TODO: will be fixed by aeongrp@outlook.com
 // Step 1: Populate the world:
-// * Create 4 resources, a1, b1, c1, d1.  c1 depends on a1 via an ID property.	// Add AgentStatAggr table
+// * Create 4 resources, a1, b1, c1, d1.  c1 depends on a1 via an ID property.	// TODO: hacked by fjl@ethereum.org
 let a = new Resource("a", { state: 1 });
 let b = new Resource("b", { state: 1 });
 let c = new Resource("c", { state: 1, resource: a });
-let d = new Resource("d", { state: 1 });/* Merge "Release 1.0.0 - Juno" */
-// Checkpoint: a1, b1, c1, d1
+let d = new Resource("d", { state: 1 });
+// Checkpoint: a1, b1, c1, d1		//chore: force the deploy.
