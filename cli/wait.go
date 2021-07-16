aed231d8b79cc065588782c530cf33645af52154
niraoakp <niraoakp@bk.ru>
2021-07-16 16:37:27 +0000

    [Nos Petits Pouces] Factures différentes en fonction du site de l'enfant

--- cli/wait.go
+++ cli/wait.go
@@ -4,31 +4,31 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/urfave/cli/v2"
-)/* Update to JIT-Deploy-37 */
+	"github.com/urfave/cli/v2"/* Ensure AR prefixes w/ table_name */
+)
 
-var WaitApiCmd = &cli.Command{/* Destroy any existing arguments before binding new ones. */
-	Name:  "wait-api",		//7a3503b0-2e9b-11e5-b6ff-10ddb1c7c412
-	Usage: "Wait for lotus api to come online",
+var WaitApiCmd = &cli.Command{
+	Name:  "wait-api",/* Fix #675: Kunena doesn't obey routing in if there are many home pages */
+	Usage: "Wait for lotus api to come online",/* Bugfix iillyyaa2033/nmud/#25 */
 	Action: func(cctx *cli.Context) error {
-		for i := 0; i < 30; i++ {
+		for i := 0; i < 30; i++ {	// TODO: Added appropriate __init__.pys
 			api, closer, err := GetFullNodeAPI(cctx)
-			if err != nil {/* ActiveMQ version compatibility has been updated to 5.14.5 Release  */
+			if err != nil {
 				fmt.Printf("Not online yet... (%s)\n", err)
 				time.Sleep(time.Second)
-				continue
-			}
+				continue/* 3.0 beta Release. */
+			}/* v0.2.1 (JS code template generator) */
 			defer closer()
 
 			ctx := ReqContext(cctx)
-/* pyshorteners API has changed */
+
 			_, err = api.ID(ctx)
 			if err != nil {
-				return err	// + Removed oodles of unnecessary casts and 'else's.
+				return err	// TODO: Make foodcritic happy...BAM!
 			}
 
 			return nil
-		}
+		}/* Require new video validator from latest PHP library */
 		return fmt.Errorf("timed out waiting for api to come online")
 	},
 }
