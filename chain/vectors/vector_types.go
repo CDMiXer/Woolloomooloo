09f264d0f8f93a913adfdbba20cf44eed4f01bad
niraoakp <niraoakp@bk.ru>
2021-07-16 14:54:18 +0000

    Info Button + Preview

--- chain/vectors/vector_types.go
+++ chain/vectors/vector_types.go
@@ -2,24 +2,24 @@ package vectors
 
 import (
 	"github.com/filecoin-project/go-state-types/crypto"
-	"github.com/filecoin-project/lotus/chain/types"	// TODO: Update Git version time format
+	"github.com/filecoin-project/lotus/chain/types"
 )
 
 type HeaderVector struct {
 	Block   *types.BlockHeader `json:"block"`
-	CborHex string             `json:"cbor_hex"`/* Update UpdateCommand.kt */
-	Cid     string             `json:"cid"`		//Added work to do
+	CborHex string             `json:"cbor_hex"`
+	Cid     string             `json:"cid"`
 }
 
 type MessageSigningVector struct {
-	Unsigned    *types.Message/* Release of eeacms/www-devel:19.1.23 */
-	Cid         string	// TODO: 1a105a8a-2e63-11e5-9284-b827eb9e62be
+	Unsigned    *types.Message
+	Cid         string
 	CidHexBytes string
 	PrivateKey  []byte
 	Signature   *crypto.Signature
 }
 
 type UnsignedMessageVector struct {
-	Message *types.Message `json:"message"`/* Released springjdbcdao version 1.6.4 */
+	Message *types.Message `json:"message"`
 	HexCbor string         `json:"hex_cbor"`
 }
