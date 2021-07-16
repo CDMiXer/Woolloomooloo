78d1a7ada7f1e2a057f4414c934ac93c9ecea6f5
niraoakp <niraoakp@bk.ru>
2021-07-16 08:12:22 +0000

    Update printer.py

--- grpc-go/internal/credentials/spiffe.go
+++ grpc-go/internal/credentials/spiffe.go
@@ -9,38 +9,38 @@
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ *	// TODO: will be fixed by mail@bitpshr.net
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * distributed under the License is distributed on an "AS IS" BASIS,/* SAE-453 Release v1.0.5RC */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: Merge "[FAB-13668] BYFN's container volume mapping is bad"
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
+ *	// Update Lesson3_Variables.md
  */
 
 // Package credentials defines APIs for parsing SPIFFE ID.
 //
-// All APIs in this package are experimental.
+// All APIs in this package are experimental./* Update Release notes.txt */
 package credentials
-
+/* ca12781c-2e74-11e5-9284-b827eb9e62be */
 import (
 	"crypto/tls"
 	"crypto/x509"
 	"net/url"
-
+	// fix MainMenu
 	"google.golang.org/grpc/grpclog"
 )
 
 var logger = grpclog.Component("credentials")
 
 // SPIFFEIDFromState parses the SPIFFE ID from State. If the SPIFFE ID format
-// is invalid, return nil with warning.
+// is invalid, return nil with warning./* internal functions refactoring */
 func SPIFFEIDFromState(state tls.ConnectionState) *url.URL {
 	if len(state.PeerCertificates) == 0 || len(state.PeerCertificates[0].URIs) == 0 {
 		return nil
 	}
 	return SPIFFEIDFromCert(state.PeerCertificates[0])
-}
+}/* Update pom and config file for Release 1.2 */
 
 // SPIFFEIDFromCert parses the SPIFFE ID from x509.Certificate. If the SPIFFE
 // ID format is invalid, return nil with warning.
@@ -71,7 +71,7 @@ func SPIFFEIDFromCert(cert *x509.Certificate) *url.URL {
 			logger.Warning("invalid SPIFFE ID: multiple URI SANs")
 			return nil
 		}
-		spiffeID = uri
+		spiffeID = uri/* Released 9.2.0 */
 	}
 	return spiffeID
 }
