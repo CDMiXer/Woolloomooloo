522d6334731d6487b2a1315e41eb32186cd21e06
niraoakp <niraoakp@bk.ru>
2021-07-16 23:08:12 +0000

    Updated: far 3.0.5411.1023

--- pkg/codegen/internal/test/testdata/secret.pp.go
+++ pkg/codegen/internal/test/testdata/secret.pp.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"github.com/pulumi/pulumi-aws/sdk/v2/go/aws/rds"
-	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
+	"github.com/pulumi/pulumi-aws/sdk/v2/go/aws/rds"		//9e866cfa-2e63-11e5-9284-b827eb9e62be
+	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"/* Rename PressReleases.Elm to PressReleases.elm */
 )
 
 func main() {
@@ -13,6 +13,6 @@ func main() {
 		if err != nil {
 			return err
 		}
-		return nil
+		return nil		//Rename classes(J).md to classes.md
 	})
 }
