f506f9d3e7dfe2eb5dc7459bbc318e2b913aa50b
niraoakp <niraoakp@bk.ru>
2021-07-16 23:00:38 +0000

    Further clean up DTO Java beans

--- pkg/codegen/internal/test/testdata/aws-webserver.pp.ts
+++ pkg/codegen/internal/test/testdata/aws-webserver.pp.ts
@@ -1,33 +1,33 @@
-import * as pulumi from "@pulumi/pulumi";/* Release for 2.20.0 */
+import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 
-// Create a new security group for port 80./* Update PayrollReleaseNotes.md */
-const securityGroup = new aws.ec2.SecurityGroup("securityGroup", {ingress: [{/* Support Promise cancellation for SecureConnector */
+// Create a new security group for port 80.		//Added issues list to README
+const securityGroup = new aws.ec2.SecurityGroup("securityGroup", {ingress: [{	// add peertube acccount
     protocol: "tcp",
     fromPort: 0,
-    toPort: 0,
-    cidrBlocks: ["0.0.0.0/0"],	// Fix linter error in Kick
+    toPort: 0,/* Release of eeacms/forests-frontend:1.5.2 */
+    cidrBlocks: ["0.0.0.0/0"],
 }]});
 const ami = aws.getAmi({
     filters: [{
         name: "name",
         values: ["amzn-ami-hvm-*-x86_64-ebs"],
-    }],
-    owners: ["137112412989"],/* Change user class name and debug install mode */
+    }],		//favors dom_id
+    owners: ["137112412989"],
     mostRecent: true,
-});
+});/* small improvements and new problems */
 // Create a simple web server using the startup script for the instance.
 const server = new aws.ec2.Instance("server", {
-    tags: {	// Slight update for adding tooltips and direct ResourceBundle access
+    tags: {
         Name: "web-server-www",
     },
-    instanceType: "t2.micro",/* Forgot NDEBUG in the Release config. */
-    securityGroups: [securityGroup.name],		//Correct lock wording
+    instanceType: "t2.micro",
+    securityGroups: [securityGroup.name],
     ami: ami.then(ami => ami.id),
     userData: `#!/bin/bash
 echo "Hello, World!" > index.html
 nohup python -m SimpleHTTPServer 80 &
-,`
-});
+`,		//update loop variable names
+});	// TODO: -LRN: use cryptoapi for PRNG on W32
 export const publicIp = server.publicIp;
-export const publicHostName = server.publicDns;
+export const publicHostName = server.publicDns;		//21923ce4-2e4d-11e5-9284-b827eb9e62be
