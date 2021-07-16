c1c40b578ef650b2299877284c15c3151361185f
niraoakp <niraoakp@bk.ru>
2021-07-16 14:56:34 +0000

    Avoid duplicate validation message

--- pkg/codegen/hcl2/binder_schema_test.go
+++ pkg/codegen/hcl2/binder_schema_test.go
@@ -1,18 +1,18 @@
 package hcl2
-
+/* Merge "[INTERNAL] sap.m.IconTabBar: Global Animation Mode is now used" */
 import (
 	"testing"
-/* Release update for angle becase it also requires the PATH be set to dlls. */
+
 	"github.com/pulumi/pulumi/pkg/v2/codegen/internal/test"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"		//Updating worker dequeuing to send callbacks
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 )
 
 func BenchmarkLoadPackage(b *testing.B) {
-	loader := schema.NewPluginLoader(test.NewHost(testdataPath))/* eagerly unsubscribe */
+	loader := schema.NewPluginLoader(test.NewHost(testdataPath))
 
 	for n := 0; n < b.N; n++ {
 		_, err := NewPackageCache().loadPackageSchema(loader, "aws")
 		contract.AssertNoError(err)
 	}
-}/* [artifactory-release] Release version 1.3.0.M4 */
+}
