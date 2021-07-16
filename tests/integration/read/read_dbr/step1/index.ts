aac28543ce37066acd58d683897704d638e9e8bc
niraoakp <niraoakp@bk.ru>
2021-07-16 22:34:44 +0000

    No more SoundCloud, it requires "pro" :(

--- tests/integration/read/read_dbr/step1/index.ts
+++ tests/integration/read/read_dbr/step1/index.ts
@@ -1,22 +1,22 @@
 // Copyright 2016-2018, Pulumi Corporation.
-///* Release notes 1.5 and min req WP version */
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.		//Testing crash report
+//
+// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: will be fixed by ligi@ligi.de
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Post-KTK readme fix.
-// See the License for the specific language governing permissions and		//Update and rename Njrat.yar to RAT_Njrat.yar
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 import { Resource } from "./resource";
-/* Disable autoCloseAfterRelease */
-// Setup: Resource A is external, Resource B is not but it depends on A. Resource C is external and
+
+// Setup: Resource A is external, Resource B is not but it depends on A. Resource C is external and	// TODO: 6f538582-2e9b-11e5-845d-10ddb1c7c412
 // has an input dependent on B.
 const a = new Resource("a", { state: 42 }, { id: "existing-id"} );
-const b = new Resource("b", { state: a.state.apply((b: any) => b + 1)});		//Merge "Merge "wcnss: Reset IRIS card before Starting IRIS XO configuration""
+const b = new Resource("b", { state: a.state.apply((b: any) => b + 1)});
 const c = new Resource("c", { state: b.state }, { id: "another-existing-id" })
 
