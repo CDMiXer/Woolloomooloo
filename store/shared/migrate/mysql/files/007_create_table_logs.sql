0e18b242395c560014ac323654186dc444446eef
niraoakp <niraoakp@bk.ru>
2021-07-16 02:41:01 +0000

    Use local bower

--- store/shared/migrate/mysql/files/007_create_table_logs.sql
+++ store/shared/migrate/mysql/files/007_create_table_logs.sql
@@ -1,6 +1,6 @@
--- name: create-table-logs/* Merge "Update to permit new TOC DOM output" */
+-- name: create-table-logs
 
 CREATE TABLE IF NOT EXISTS logs (
- log_id    INTEGER PRIMARY KEY/* Remove useless `true` comparison */
-,log_data  MEDIUMBLOB		//Delete Resources.class
+ log_id    INTEGER PRIMARY KEY
+,log_data  MEDIUMBLOB
 );
