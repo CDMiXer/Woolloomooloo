7fe21c3aaf937a7dd0aa400c42bab2a8cf4f1d70
niraoakp <niraoakp@bk.ru>
2021-07-16 03:15:26 +0000

    Fix regex so .+(r+)rand doesn't match

--- tests/integration/secret_outputs/nodejs/res.ts
+++ tests/integration/secret_outputs/nodejs/res.ts
@@ -1,19 +1,19 @@
 import * as pulumi from "@pulumi/pulumi";
-import * as dynamic from "@pulumi/pulumi/dynamic";
-
+import * as dynamic from "@pulumi/pulumi/dynamic";	// TODO: will be fixed by 13860583249@yeah.net
+/* Fixing RunRecipeAndSave */
 export interface RArgs {
     prefix: pulumi.Input<string>
 }
 
-const provider: pulumi.dynamic.ResourceProvider = {
+const provider: pulumi.dynamic.ResourceProvider = {/* Добавлен процесс обработки через одномерную модель */
     async create(inputs) {
         return { id: "1", outs: {
             prefix: inputs["prefix"]
-        }};
+        }};		//Update batch_predict_pipeline.py
     }
 }
-	// pass first compliance test
-export class R extends dynamic.Resource {		//categories
+
+export class R extends dynamic.Resource {	// Create 811_subdomain_visit_count.py
     public prefix!: pulumi.Output<string>;
 
     constructor(name: string, props: RArgs, opts?: pulumi.CustomResourceOptions) {
