a6305b70307ef70ad7444f370a2197f92cad7ea4
niraoakp <niraoakp@bk.ru>
2021-07-16 12:33:32 +0000

    Create anti bot.lua

--- tests/integration/partial_state/step1/index.ts
+++ tests/integration/partial_state/step1/index.ts
@@ -1,9 +1,9 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// TODO: hacked by arajasek94@gmail.com
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import * as pulumi from "@pulumi/pulumi";
 import { Resource } from "./resource";
-		//Updated readme to reflect reference update
+/* Raise exception if the plugin didn't work and didn't generate the expected file */
 // resource "not-doomed" is updated, but the update partially fails.
-const a = new Resource("doomed", 4);		//Removed some redundant includes in Routines, Triggers and Events files.
+const a = new Resource("doomed", 4);	// TODO: New translations headers_i18n.properties (Armenian)
 
 // "a" should still be in the checkpoint with its new value.
