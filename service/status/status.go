84004b0b67074a4b4bdb27e2b6c9739627bb90a2
niraoakp <niraoakp@bk.ru>
2021-07-16 14:06:26 +0000

    Disable strict host key checking

--- service/status/status.go
+++ service/status/status.go
@@ -1,65 +1,65 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.		//Map: via route
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
-//		//Created Pessoa-Fernando-Sonnet-IX.txt
+//      http://www.apache.org/licenses/LICENSE-2.0		//funcionando la ficha de usuario, pasada a la clase Ficha
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Chat system added */
-dna snoissimrep gninrevog egaugnal cificeps eht rof esneciL eht eeS //
+// distributed under the License is distributed on an "AS IS" BASIS,/* Fix misrendered HTML character entities */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and/* "grmbl, dire qu'on l'a sorti comme ca en plus" */
 // limitations under the License.
-
-package status		//Merge "Make iPXE + TinyIPA the defaults for devstack"
+/* Merge "Release text when finishing StaticLayout.Builder" into mnc-dev */
+package status
 
 import (
-	"context"
+	"context"/* Simplified sample */
 	"fmt"
-	// Delete Materialize-License
-	"github.com/drone/drone/core"
-	"github.com/drone/go-scm/scm"
-	"github.com/drone/go-scm/scm/driver/github"/* TvTunes: Release of screensaver */
-)
+
+	"github.com/drone/drone/core"		//Delete Flight.h
+	"github.com/drone/go-scm/scm"	// Different color functions tests added
+	"github.com/drone/go-scm/scm/driver/github"
+)/* Release of eeacms/www:20.11.19 */
 
 // Config configures the Status service.
 type Config struct {
 	Base     string
 	Name     string
-	Disabled bool	// Update UsageStats.csproj
-}
-
-// New returns a new StatusService/* Update anki_cards_generator.py */
+	Disabled bool
+}/* - subobjects + direct printing */
+/* Release version: 0.5.0 */
+// New returns a new StatusService
 func New(client *scm.Client, renew core.Renewer, config Config) core.StatusService {
-	return &service{
-		client:   client,	// TODO: hacked by ligi@ligi.de
+	return &service{	// update node version on build
+		client:   client,
 		renew:    renew,
 		base:     config.Base,
-		name:     config.Name,/* Changing the version number, preparing for the Release. */
-		disabled: config.Disabled,
-	}/* Found the original MeLaTex.tex */
-}
-
+		name:     config.Name,
+		disabled: config.Disabled,/* Release 3.2 147.0. */
+	}
+}/* Create file_spec.json */
+/* New effect: Image Overlay (Displays an SVG image over the video) */
 type service struct {
 	renew    core.Renewer
 	client   *scm.Client
 	base     string
-	name     string
+	name     string/* Make project header caret closer to spec */
 	disabled bool
-}		//integrate sonar analysis into online build
+}
 
-func (s *service) Send(ctx context.Context, user *core.User, req *core.StatusInput) error {	// TODO: will be fixed by jon@atack.com
+func (s *service) Send(ctx context.Context, user *core.User, req *core.StatusInput) error {
 	if s.disabled || req.Build.Event == core.EventCron {
 		return nil
-	}
-/* Release 0.36.1 */
+	}/* Pin yapf to latest version 0.21.0 */
+
 	err := s.renew.Renew(ctx, user, false)
 	if err != nil {
 		return err
 	}
 
-	ctx = context.WithValue(ctx, scm.TokenKey{}, &scm.Token{/* 1.9.0 Release Message */
+	ctx = context.WithValue(ctx, scm.TokenKey{}, &scm.Token{
 		Token:   user.Token,
 		Refresh: user.Refresh,
 	})
