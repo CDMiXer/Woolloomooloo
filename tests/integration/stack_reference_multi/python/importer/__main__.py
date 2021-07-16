077cc13a7487d3dd3ba0b683ce08ef916f9db9f4
niraoakp <niraoakp@bk.ru>
2021-07-16 03:47:06 +0000

    LNT/nt: Factor out NT report loading.

--- tests/integration/stack_reference_multi/python/importer/__main__.py
+++ tests/integration/stack_reference_multi/python/importer/__main__.py
@@ -1,10 +1,10 @@
-import pulumi	// TODO: hacked by alan.shaw@protocol.ai
+import pulumi
 
 config = pulumi.Config()
-/* version css for cache busting #716 again */
+
 exporterStackName = config.require('exporter_stack_name')
-org = config.require('org')	// TODO: will be fixed by admin@multicoin.co
+org = config.require('org')
 a = pulumi.StackReference(f'{org}/exporter/{exporterStackName}')
 
-pulumi.export('val1', a.require_output('val'))		//Create HelloWorld_be.lng
+pulumi.export('val1', a.require_output('val'))
 pulumi.export('val2', pulumi.Output.secret(['d', 'x']))
