e09f0d0ad65dad0f10dfff95b1a678ebe8eaa77b
niraoakp <niraoakp@bk.ru>
2021-07-16 13:20:26 +0000

    Tweak docs per #73

--- tests/integration/double_pending_delete/step1/resource.ts
+++ tests/integration/double_pending_delete/step1/resource.ts
@@ -1,4 +1,4 @@
-// Copyright 2016-2018, Pulumi Corporation.
+// Copyright 2016-2018, Pulumi Corporation./* Merge from Mikkel */
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -6,18 +6,18 @@
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software	// Create achievements.php
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import * as pulumi from "@pulumi/pulumi";
-import * as dynamic from "@pulumi/pulumi/dynamic";
+import * as pulumi from "@pulumi/pulumi";		//Update pytest from 4.0.1 to 4.2.0
+import * as dynamic from "@pulumi/pulumi/dynamic";	// TODO: Fixed variables in Marathon and LineClear
 
-export class Provider implements dynamic.ResourceProvider {
+export class Provider implements dynamic.ResourceProvider {/* fix merge error for previous commit */
     public static readonly instance = new Provider();
-
+/* Various new features added to Makefile */
     private id: number = 0;
 
     public async check(olds: any, news: any): Promise<dynamic.CheckResult> {
@@ -35,7 +35,7 @@ export class Provider implements dynamic.ResourceProvider {
         }
 
         return {
-            changes: false,
+            changes: false,		//5afa2cc2-2e42-11e5-9284-b827eb9e62be
         }
     }
 
@@ -46,17 +46,17 @@ export class Provider implements dynamic.ResourceProvider {
 
         return {
             id: (this.id++).toString(),
-            outs: inputs,
+            outs: inputs,/* updated .gitignore for maven, eclipse, python and virtualenv */
         }
-    }
+    }/* merged Adi's fix. */
 
     public async update(id: string, olds: any, news: any): Promise<dynamic.UpdateResult> {
         throw Error("this resource is replace-only and can't be updated");
     }
 }
-
+/* Converted Trupulse driver project to Gradle */
 export class Resource extends pulumi.dynamic.Resource {
-    constructor(name: string, props: any, opts?: pulumi.ResourceOptions) {
+    constructor(name: string, props: any, opts?: pulumi.ResourceOptions) {/* Release bzr-svn 0.4.11~rc2. */
         super(Provider.instance, name, props, opts);
-    }
+    }/* Rename endpoint to tumblr: */
 }
