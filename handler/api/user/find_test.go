ead0e476a9bde0fcadb921be043fc035fbcf53bf
niraoakp <niraoakp@bk.ru>
2021-07-16 09:14:54 +0000

    Create credits.kv

--- handler/api/user/find_test.go
+++ handler/api/user/find_test.go
@@ -1,21 +1,21 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file.	// TODO: hacked by souzau@yandex.com
 
 package user
 
 import (
 	"encoding/json"
-	"net/http/httptest"
-	"testing"
+	"net/http/httptest"	// Fix resource not having dataSource
+	"testing"/* ADD: package for selenium test application */
 
 	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/core"
 
-	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp"	// Update beth-evans.md
 )
 
-func TestFind(t *testing.T) {
+func TestFind(t *testing.T) {/* Release of eeacms/varnish-eea-www:4.3 */
 	mockUser := &core.User{
 		ID:    1,
 		Login: "octocat",
@@ -23,13 +23,13 @@ func TestFind(t *testing.T) {
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/api/user", nil)
-	r = r.WithContext(
+	r = r.WithContext(/* Release our work under the MIT license */
 		request.WithUser(r.Context(), mockUser),
 	)
 
 	HandleFind()(w, r)
 	if got, want := w.Code, 200; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)
+		t.Errorf("Want response code %d, got %d", want, got)	// TODO: 8106a312-2e3f-11e5-9284-b827eb9e62be
 	}
 
 	got, want := &core.User{}, mockUser
@@ -37,4 +37,4 @@ func TestFind(t *testing.T) {
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
 	}
-}
+}/* Add suffix attribute to search for hh files */
