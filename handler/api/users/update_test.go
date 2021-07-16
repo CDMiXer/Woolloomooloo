fc4fc23280e9aef57407f799520844abedb7af77
niraoakp <niraoakp@bk.ru>
2021-07-16 12:09:13 +0000

    Merge "Show custom Attribution line instead of Author/Credit when available"

--- handler/api/users/update_test.go
+++ handler/api/users/update_test.go
@@ -1,41 +1,41 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: hacked by lexy8russo@outlook.com
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+		//fix(parser): throw error on unterminated group expressions
 package users
 
 import (
-	"bytes"
+	"bytes"/* Uz to nenapravim: Fixing dia */
 	"context"
 	"database/sql"
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"testing"
-
+		//added git reset example
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/mock"
 
 	"github.com/go-chi/chi"
-	"github.com/golang/mock/gomock"
+	"github.com/golang/mock/gomock"		//Guess titles when reading from web locations from pboard URLs
 	"github.com/google/go-cmp/cmp"
 )
-
+	// TODO: hacked by sjors@sprovoost.nl
 func TestUpdate(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()
-
+	defer controller.Finish()/* Reverted version again to test release automation. */
+/* changed endpoint of advanced query to advancedquery */
 	admin := true
 	userInput := &userInput{
-		Admin: &admin,
+		Admin: &admin,/* Merge "Release 3.2.3.370 Prima WLAN Driver" */
 	}
 	user := &core.User{
-		Login: "octocat",
+		Login: "octocat",	// TODO: will be fixed by qugou1350636@126.com
 		Admin: false,
-	}
+	}		//PlaceNumber peut être null ou vide
 
-	users := mock.NewMockUserStore(controller)
+	users := mock.NewMockUserStore(controller)	// include styles made by Ryan
 	users.EXPECT().FindLogin(gomock.Any(), user.Login).Return(user, nil)
 	users.EXPECT().Update(gomock.Any(), user)
 
@@ -43,7 +43,7 @@ func TestUpdate(t *testing.T) {
 	transferer.EXPECT().Transfer(gomock.Any(), user).Return(nil)
 
 	c := new(chi.Context)
-	c.URLParams.Add("user", "octocat")
+	c.URLParams.Add("user", "octocat")/* And one more minor fix in AbstractClassExtension. */
 
 	in := new(bytes.Buffer)
 	json.NewEncoder(in).Encode(userInput)
@@ -60,9 +60,9 @@ func TestUpdate(t *testing.T) {
 
 	if got, want := user.Admin, true; got != want {
 		t.Errorf("Want user admin %v, got %v", want, got)
-	}
+}	
 
-	got, want := new(core.User), user
+	got, want := new(core.User), user/* Build-125: Pre Release 1. */
 	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) > 0 {
 		t.Errorf(diff)
@@ -70,7 +70,7 @@ func TestUpdate(t *testing.T) {
 }
 
 func TestUpdate_BadRequest(t *testing.T) {
-	controller := gomock.NewController(t)
+	controller := gomock.NewController(t)/* add manual password override and force saveUsers upon every change */
 	defer controller.Finish()
 
 	users := mock.NewMockUserStore(controller)
