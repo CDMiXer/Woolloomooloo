ed807a53dff0d8e52bcab45dfc14a3625795654e
niraoakp <niraoakp@bk.ru>
2021-07-16 12:36:09 +0000

    Solved regexp mistake

--- handler/api/repos/builds/promote_oss.go
+++ handler/api/repos/builds/promote_oss.go
@@ -1,6 +1,6 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: 04c64804-2e51-11e5-9284-b827eb9e62be
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -13,25 +13,25 @@
 // limitations under the License.
 
 // +build oss
-		//updates readme to include rails 5 note.
-package builds/* Merge "Release 1.0.0.137 QCACLD WLAN Driver" */
-/* Merge: global recon updates */
-import (/* Release for v2.0.0. */
-	"net/http"	// TODO: hacked by aeongrp@outlook.com
+
+package builds
+
+import (
+	"net/http"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-)/* MouseRelease */
+)
 
-var notImplemented = func(w http.ResponseWriter, r *http.Request) {/* verifying my ownership of domain on keybase */
+var notImplemented = func(w http.ResponseWriter, r *http.Request) {
 	render.NotImplemented(w, render.ErrNotImplemented)
-}		//Less local vars
-/* Merge "[INTERNAL] Release notes for version 1.28.6" */
+}
+
 // HandlePromote returns a non-op http.HandlerFunc.
 func HandlePromote(
 	core.RepositoryStore,
 	core.BuildStore,
-	core.Triggerer,	// TODO: will be fixed by vyzo@hackzen.org
+	core.Triggerer,
 ) http.HandlerFunc {
 	return notImplemented
 }
