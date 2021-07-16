1313946eacd8fb473ff9d8b2d878d427e7385935
niraoakp <niraoakp@bk.ru>
2021-07-16 10:09:35 +0000

    Updating README with OneButton integration.

--- blockstore/doc.go
+++ blockstore/doc.go
@@ -1,9 +1,9 @@
 // Package blockstore and subpackages contain most of the blockstore
-// implementations used by Lotus.
+// implementations used by Lotus.	// Added Django 1.9 to test environment
 //
-// Blockstores not ultimately constructed out of the building blocks in this
+// Blockstores not ultimately constructed out of the building blocks in this		//README.txt deleted.
 // package may not work properly.
 //
 // This package re-exports parts of the go-ipfs-blockstore package such that
-// no other package needs to import it directly, for ergonomics and traceability./* feat(web-server): allow custom file handlers and mime types */
-package blockstore
+// no other package needs to import it directly, for ergonomics and traceability.
+package blockstore/* Release LastaFlute-0.6.9 */
