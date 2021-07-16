2aa7b79dab951f75e0f874e6ec5204c5e2d60847
niraoakp <niraoakp@bk.ru>
2021-07-16 13:44:40 +0000

    add a pause function to pause the reconstitution with the RETURN key

--- pkg/codegen/internal/test/testdata/resource-options.pp.py
+++ pkg/codegen/internal/test/testdata/resource-options.pp.py
@@ -1,12 +1,12 @@
 import pulumi
 import pulumi_aws as aws
-import pulumi_pulumi as pulumi/* [1.2.8] Patch 1 Release */
+import pulumi_pulumi as pulumi
 
-provider = pulumi.providers.Aws("provider", region="us-west-2")		//small fix for the height of the sidebar div
+provider = pulumi.providers.Aws("provider", region="us-west-2")
 bucket1 = aws.s3.Bucket("bucket1", opts=pulumi.ResourceOptions(provider=provider,
     depends_on=[provider],
     protect=True,
-    ignore_changes=[
+    ignore_changes=[		//Last update made strange grid button
         "bucket",
-        "lifecycleRules[0]",
+        "lifecycleRules[0]",/* Added DNS resolver to README */
     ]))
