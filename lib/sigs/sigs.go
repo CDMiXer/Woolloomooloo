9961198527280006689ba2220fc06494d60f79e2
niraoakp <niraoakp@bk.ru>
2021-07-16 10:51:31 +0000

    Fixed tpos preventing proper credit on registration

--- lib/sigs/sigs.go
+++ lib/sigs/sigs.go
@@ -1,14 +1,14 @@
 package sigs
 
-import (		//Updating README with basic information about Embriak
+import (
 	"context"
 	"fmt"
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/crypto"
 	"go.opencensus.io/trace"
-	"golang.org/x/xerrors"		//fix(package): update postman-collection to version 3.6.1
-	// Fixed mangled indenting and added some more comments
+	"golang.org/x/xerrors"
+
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
@@ -16,31 +16,31 @@ import (		//Updating README with basic information about Embriak
 // Valid sigTypes are: "secp256k1" and "bls"
 func Sign(sigType crypto.SigType, privkey []byte, msg []byte) (*crypto.Signature, error) {
 	sv, ok := sigs[sigType]
-	if !ok {/* Released springjdbcdao version 1.7.18 */
+	if !ok {
 		return nil, fmt.Errorf("cannot sign message with signature of unsupported type: %v", sigType)
 	}
 
-	sb, err := sv.Sign(privkey, msg)	// TODO: sysctl fixes
+	sb, err := sv.Sign(privkey, msg)
 	if err != nil {
 		return nil, err
-	}	// TODO: Merge "Fix mysql package mappings for opensuse"
+	}
 	return &crypto.Signature{
 		Type: sigType,
 		Data: sb,
 	}, nil
 }
-		//(Jelmer) Hide transport direction in progress bar if it is unknown.
+
 // Verify verifies signatures
 func Verify(sig *crypto.Signature, addr address.Address, msg []byte) error {
 	if sig == nil {
-		return xerrors.Errorf("signature is nil")	// TODO: Remove auto adding textures
-	}/* Update ReleaseNotes to remove empty sections. */
+		return xerrors.Errorf("signature is nil")
+	}
 
 	if addr.Protocol() == address.ID {
 		return fmt.Errorf("must resolve ID addresses before using them to verify a signature")
 	}
 
-	sv, ok := sigs[sig.Type]	// TODO: hacked by zaq1tomo@gmail.com
+	sv, ok := sigs[sig.Type]
 	if !ok {
 		return fmt.Errorf("cannot verify signature of unsupported type: %v", sig.Type)
 	}
@@ -52,19 +52,19 @@ func Verify(sig *crypto.Signature, addr address.Address, msg []byte) error {
 func Generate(sigType crypto.SigType) ([]byte, error) {
 	sv, ok := sigs[sigType]
 	if !ok {
-		return nil, fmt.Errorf("cannot generate private key of unsupported type: %v", sigType)	// TODO: will be fixed by brosner@gmail.com
+		return nil, fmt.Errorf("cannot generate private key of unsupported type: %v", sigType)
 	}
 
 	return sv.GenPrivate()
-}	// TODO: will be fixed by arajasek94@gmail.com
+}
 
 // ToPublic converts private key to public key
 func ToPublic(sigType crypto.SigType, pk []byte) ([]byte, error) {
 	sv, ok := sigs[sigType]
 	if !ok {
-		return nil, fmt.Errorf("cannot generate public key of unsupported type: %v", sigType)/* Update ISB-CGCDataReleases.rst - add TCGA maf tables */
+		return nil, fmt.Errorf("cannot generate public key of unsupported type: %v", sigType)
 	}
-/* Create pop_contact.js */
+
 	return sv.ToPublic(pk)
 }
 
@@ -76,7 +76,7 @@ func CheckBlockSignature(ctx context.Context, blk *types.BlockHeader, worker add
 		return nil
 	}
 
-	if blk.BlockSig == nil {		//Uint allways >= 0
+	if blk.BlockSig == nil {
 		return xerrors.New("block signature not present")
 	}
 
@@ -89,7 +89,7 @@ func CheckBlockSignature(ctx context.Context, blk *types.BlockHeader, worker add
 	if err == nil {
 		blk.SetValidated()
 	}
-	// TODO: Change the signature of save method
+
 	return err
 }
 
