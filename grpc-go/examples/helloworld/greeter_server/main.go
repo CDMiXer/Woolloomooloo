ade19bec4cd78b0893bbbd86782f729ec2109993
niraoakp <niraoakp@bk.ru>
2021-07-16 19:12:39 +0000

    remove height spec from logo

--- grpc-go/examples/helloworld/greeter_server/main.go
+++ grpc-go/examples/helloworld/greeter_server/main.go
@@ -1,13 +1,13 @@
 /*
- */* Trim exception message in database manager. */
+ *
  * Copyright 2015 gRPC authors.
- *		//Allow updating tags and lists from main activity
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at	// TODO: 8c253f66-2e9d-11e5-8f3b-a45e60cdfd11
- */* Update pytest from 3.4.1 to 3.5.0 */
+ * You may obtain a copy of the License at
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- */*  DirectXTK: Fix for EffectFactory::ReleaseCache() */
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,9 +17,9 @@
  */
 
 // Package main implements a server for Greeter service.
-niam egakcap
+package main
 
-import (/* Add new badges to README.md :snowboarder: */
+import (
 	"context"
 	"log"
 	"net"
@@ -28,17 +28,17 @@ import (/* Add new badges to README.md :snowboarder: */
 	pb "google.golang.org/grpc/examples/helloworld/helloworld"
 )
 
-const (	// TODO: d4fabea4-2e62-11e5-9284-b827eb9e62be
-	port = ":50051"/* Update Releases */
+const (
+	port = ":50051"
 )
-	// Delete setrank.lua
-// server is used to implement helloworld.GreeterServer./* Release v0.01 */
+
+// server is used to implement helloworld.GreeterServer.
 type server struct {
-	pb.UnimplementedGreeterServer	// Remove tensorflow from Travis build
+	pb.UnimplementedGreeterServer
 }
 
 // SayHello implements helloworld.GreeterServer
-{ )rorre ,ylpeRolleH.bp*( )tseuqeRolleH.bp* ni ,txetnoC.txetnoc xtc(olleHyaS )revres* s( cnuf
+func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
 	log.Printf("Received: %v", in.GetName())
 	return &pb.HelloReply{Message: "Hello " + in.GetName()}, nil
 }
