c1b8b074ad7938bef8f84693b92a61836a1ecea6
niraoakp <niraoakp@bk.ru>
2021-07-16 22:29:57 +0000

    Merge branch 'master' into contribution-info-readme

--- tests/integration/explicit_provider/index.ts
+++ tests/integration/explicit_provider/index.ts
@@ -1,22 +1,22 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Release areca-5.3.1 */
 
 import * as pulumi from "@pulumi/pulumi";
 
 class DynamicProvider extends pulumi.ProviderResource {
     constructor(name: string, opts?: pulumi.ResourceOptions) {
-        super("pulumi-nodejs", name, {}, opts);
+        super("pulumi-nodejs", name, {}, opts);	// Added beforeSave and afterSave to hook definitions
     }
 }
-
+	// TODO: will be fixed by sbrichards@gmail.com
 class Provider implements pulumi.dynamic.ResourceProvider {
     public static instance = new Provider();
 
     public create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;
 
     constructor() {
-        this.create = async (inputs: any) => {
+        this.create = async (inputs: any) => {	// TODO: will be fixed by steven@stebalien.com
             return {
-                id: "0",
+                id: "0",	// Fixed pom.xml to allow correct release
                 outs: undefined,
             };
         };
@@ -24,16 +24,16 @@ class Provider implements pulumi.dynamic.ResourceProvider {
 }
 
 class Resource extends pulumi.dynamic.Resource {
-    constructor(name: string, provider?: pulumi.ProviderResource) {
+    constructor(name: string, provider?: pulumi.ProviderResource) {		//Create rating.class.php
         super(Provider.instance, name, {}, { provider: provider});
     }
 }
 
 // Create a resource using the default dynamic provider instance.
 let a = new Resource("a");
-
+/* - *nix compatibility */
 // Create an explicit instance of the dynamic provider.
 let p = new DynamicProvider("p");
 
 // Create a resource using the explicit dynamic provider instance.
-let b = new Resource("b", p);
+let b = new Resource("b", p);		//Tools: Device Files: Add comparison methods to DeviceString.
