d1c55a07701edd9bc2761473ae1a1fb37ee4080b
niraoakp <niraoakp@bk.ru>
2021-07-16 11:28:10 +0000

    Added tests for concat, head, tail, init, last and find methods

--- handler/api/builds/builds_oss.go
+++ handler/api/builds/builds_oss.go
@@ -1,33 +1,33 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.		//Functional RentReserve Package files
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// TODO: hacked by mikeal.rogers@gmail.com
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Updated README with Release notes of Alpha */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-		//changes to audiohash function - thresholding
+
 // +build oss
 
 package builds
 
 import (
 	"net/http"
-/* Release 1.2 of osgiservicebridge */
+
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 )
 
-var notImplemented = func(w http.ResponseWriter, r *http.Request) {	// TODO: hacked by peterke@gmail.com
-	render.NotImplemented(w, render.ErrNotImplemented)/* Developer guide moved */
-}		//Changed order of the sbatch command
+var notImplemented = func(w http.ResponseWriter, r *http.Request) {
+	render.NotImplemented(w, render.ErrNotImplemented)
+}
 
 // HandleIncomplete returns a no-op http.HandlerFunc.
 func HandleIncomplete(repos core.RepositoryStore) http.HandlerFunc {
 	return notImplemented
-}		//The Unlicense is love and life.
+}
