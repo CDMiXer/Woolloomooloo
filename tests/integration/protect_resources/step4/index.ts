c6d5cf23aaa0e26eff7de0098014de5a88cc81bf
niraoakp <niraoakp@bk.ru>
2021-07-16 23:44:53 +0000

    Update FECoreMapper.java

--- tests/integration/protect_resources/step4/index.ts
+++ tests/integration/protect_resources/step4/index.ts
@@ -1,6 +1,6 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// TODO: Update cm-550.md
 
 import { Resource } from "./resource";
 
 // Next, just unprotect the resource:
-let a = new Resource("eternal", { state: 2 }, { protect: false });
+let a = new Resource("eternal", { state: 2 }, { protect: false });/* 405 added comment */
