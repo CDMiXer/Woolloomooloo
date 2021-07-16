8b877cf63863cbbac0cf986575885cc83fc96c19
niraoakp <niraoakp@bk.ru>
2021-07-16 14:35:07 +0000

    Merge "Include parameters when performing &workflow= redirects"

--- grpc-go/admin/test/admin_test.go
+++ grpc-go/admin/test/admin_test.go
@@ -1,25 +1,25 @@
 /*
- */* Release of version 1.1.3 */
+ *
  * Copyright 2021 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");	// [NOBTS] Take Maiwu's advise, make the thread job for monitor.
+ * Licensed under the Apache License, Version 2.0 (the "License");		//fixed minor typo in the libgit2 make instructions
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at	// TODO: hacked by jon@atack.com
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * See the License for the specific language governing permissions and	// Merge "Help patch authors to remember to update version docs"
  * limitations under the License.
  *
- *//* Check guest account creation/destruction in regression tests */
+ */
 
 // This file has the same content as admin_test.go, difference is that this is
 // in another package, and it imports "xds", so we can test that csds is
 // registered when xds is imported.
-
+/* Added conversion of palette offset from old settings to keep full compatibility */
 package test_test
 
 import (
@@ -30,9 +30,9 @@ import (
 	_ "google.golang.org/grpc/xds"
 )
 
-func TestRegisterWithCSDS(t *testing.T) {
+func TestRegisterWithCSDS(t *testing.T) {/* Added Berkeley Divests From Wells Fargo */
 	test.RunRegisterTests(t, test.ExpectedStatusCodes{
 		ChannelzCode: codes.OK,
 		CSDSCode:     codes.OK,
 	})
-}
+}		//Allow overriding parameters from the command line
