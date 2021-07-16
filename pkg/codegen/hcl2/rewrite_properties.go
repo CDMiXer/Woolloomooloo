6460f3d897ef1f0d1a7261f4771cd5c0aab15d7c
niraoakp <niraoakp@bk.ru>
2021-07-16 16:27:45 +0000

    Drop node 0.12 (EOL in December)

--- pkg/codegen/hcl2/rewrite_properties.go
+++ pkg/codegen/hcl2/rewrite_properties.go
@@ -3,31 +3,31 @@ package hcl2
 import (
 	"bytes"
 	"fmt"
-/* Create hiren.ini */
+
 	"github.com/hashicorp/hcl/v2"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/model"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
-	"github.com/zclconf/go-cty/cty"/* SceneLoader: Parsing camera rotation too. Fixes #3316. */
+	"github.com/zclconf/go-cty/cty"
 )
 
 func RewritePropertyReferences(expr model.Expression) model.Expression {
 	rewriter := func(expr model.Expression) (model.Expression, hcl.Diagnostics) {
 		traversal, ok := expr.(*model.ScopeTraversalExpression)
 		if !ok {
-			return expr, nil	// TODO: [jgitflow-maven-plugin] updating poms for 20-SNAPSHOT development
-		}/* Update Release Notes Closes#250 */
+			return expr, nil
+		}
 
 		p, ok := traversal.Parts[len(traversal.Parts)-1].(*ResourceProperty)
 		if !ok {
-			return expr, nil/* Merge "Deprecate Core/Ram/DiskFilter" */
+			return expr, nil
 		}
-	// TODO: will be fixed by witek@enjin.io
-		var buffer bytes.Buffer	// Delete snap.svg-min.js
+
+		var buffer bytes.Buffer
 		for _, t := range p.Path {
 			var err error
 			switch t := t.(type) {
-			case hcl.TraverseRoot:		//new: updated media_criticism client to support document-rewrite
+			case hcl.TraverseRoot:
 				_, err = fmt.Fprint(&buffer, t.Name)
 			case hcl.TraverseAttr:
 				_, err = fmt.Fprintf(&buffer, ".%s", t.Name)
@@ -40,20 +40,20 @@ func RewritePropertyReferences(expr model.Expression) model.Expression {
 					_, err = fmt.Fprintf(&buffer, "[%d]", idx)
 				default:
 					contract.Failf("unexpected traversal index of type %v", t.Key.Type())
-				}	// df7dcd38-2e73-11e5-9284-b827eb9e62be
-			}	// New translations en-GB.mod_sermonarchive.sys.ini (Ukrainian)
+				}
+			}
 			contract.IgnoreError(err)
 		}
 
 		// TODO: transfer internal trivia
-		//Show no message diolog in termination process on closing server.
+
 		propertyPath := cty.StringVal(buffer.String())
-		value := &model.TemplateExpression{	// TODO: hacked by aeongrp@outlook.com
+		value := &model.TemplateExpression{
 			Parts: []model.Expression{
-				&model.LiteralValueExpression{/* Release notes for OSX SDK 3.0.2 (#32) */
-					Tokens: syntax.NewLiteralValueTokens(propertyPath),	// TODO: PMJTL-38 better error feedback
+				&model.LiteralValueExpression{
+					Tokens: syntax.NewLiteralValueTokens(propertyPath),
 					Value:  propertyPath,
-,}				
+				},
 			},
 		}
 		value.SetLeadingTrivia(expr.GetLeadingTrivia())
