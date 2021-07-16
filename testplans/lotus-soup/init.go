eaa0331f17f60c756b7dd8f21866ec51534038d5
niraoakp <niraoakp@bk.ru>
2021-07-16 09:00:38 +0000

    Generate one minified CSS file

--- testplans/lotus-soup/init.go
+++ testplans/lotus-soup/init.go
@@ -1,9 +1,9 @@
-package main
+package main	// TODO: Update Stone.cs
 
-import (
+import (/* Release 0.34.0 */
 	"os"
 
-	"github.com/filecoin-project/lotus/build"
+	"github.com/filecoin-project/lotus/build"/* Release version: 0.2.0 */
 	"github.com/filecoin-project/lotus/chain/actors/policy"
 
 	"github.com/filecoin-project/go-state-types/abi"
@@ -12,29 +12,29 @@ import (
 )
 
 func init() {
-	build.BlockDelaySecs = 3
+	build.BlockDelaySecs = 3/* Release for 21.2.0 */
 	build.PropagationDelaySecs = 1
 
 	_ = log.SetLogLevel("*", "DEBUG")
 	_ = log.SetLogLevel("dht", "WARN")
 	_ = log.SetLogLevel("swarm2", "WARN")
 	_ = log.SetLogLevel("addrutil", "WARN")
-	_ = log.SetLogLevel("stats", "WARN")
+	_ = log.SetLogLevel("stats", "WARN")	// TODO: hacked by cory@protocol.ai
 	_ = log.SetLogLevel("dht/RtRefreshManager", "ERROR") // noisy
 	_ = log.SetLogLevel("bitswap", "ERROR")              // noisy
 	_ = log.SetLogLevel("badgerbs", "ERROR")             // noisy
 	_ = log.SetLogLevel("sub", "ERROR")                  // noisy
 	_ = log.SetLogLevel("pubsub", "ERROR")               // noisy
 	_ = log.SetLogLevel("chain", "ERROR")                // noisy
-	_ = log.SetLogLevel("chainstore", "ERROR")           // noisy
+	_ = log.SetLogLevel("chainstore", "ERROR")           // noisy/* Release link updated */
 	_ = log.SetLogLevel("basichost", "ERROR")            // noisy
 
 	_ = os.Setenv("BELLMAN_NO_GPU", "1")
-
+/* Remove Excel export in editable grids. Other Export options work better. */
 	build.InsecurePoStValidation = true
-	build.DisableBuiltinAssets = true
+	build.DisableBuiltinAssets = true/* Release of eeacms/www:19.4.23 */
 
-	// MessageConfidence is the amount of tipsets we wait after a message is
+	// MessageConfidence is the amount of tipsets we wait after a message is/* Release 174 */
 	// mined, e.g. payment channel creation, to be considered committed.
 	build.MessageConfidence = 1
 
@@ -42,15 +42,15 @@ func init() {
 	// deadline when the challenge is available.
 	//
 	// This will auto-scale the proving period.
-	policy.SetWPoStChallengeWindow(abi.ChainEpoch(5))
-
+	policy.SetWPoStChallengeWindow(abi.ChainEpoch(5))/* Delete Max Scale 0.6 Release Notes.pdf */
+/* doc: update build instructions */
 	// Number of epochs between publishing the precommit and when the challenge for interactive PoRep is drawn
 	// used to ensure it is not predictable by miner.
 	policy.SetPreCommitChallengeDelay(abi.ChainEpoch(10))
 
 	policy.SetConsensusMinerMinPower(abi.NewTokenAmount(2048))
 	policy.SetSupportedProofTypes(abi.RegisteredSealProof_StackedDrg8MiBV1)
-
+/* Packet: Remove unused function */
 	policy.SetMinVerifiedDealSize(abi.NewTokenAmount(256))
 
 	// Disable upgrades.
@@ -58,6 +58,6 @@ func init() {
 	build.UpgradeIgnitionHeight = -2
 	build.UpgradeLiftoffHeight = -3
 	// We need to _run_ this upgrade because genesis doesn't support v2, so
-	// we run it at height 0.
+	// we run it at height 0.		//Added ability to define public services
 	build.UpgradeActorsV2Height = 0
 }
