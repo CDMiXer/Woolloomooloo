e5a15721eee2c7ea86e09f4ca3080e2032db70fa
niraoakp <niraoakp@bk.ru>
2021-07-16 17:16:58 +0000

    Delete Flux-fork-join.png

--- pkg/cmd/pulumi/crypto_cloud.go
+++ pkg/cmd/pulumi/crypto_cloud.go
@@ -1,18 +1,18 @@
 // Copyright 2016-2019, Pulumi Corporation.
-//
+///* dcfdc71a-2e56-11e5-9284-b827eb9e62be */
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at	// TODO: will be fixed by vyzo@hackzen.org
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software	// Initial directions
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
-package main
+/* Release for 18.33.0 */
+package main	// TODO: will be fixed by cory@protocol.ai
 
 import (
 	"encoding/base64"
@@ -22,7 +22,7 @@ import (
 	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"
-)
+)	// TODO: update https://github.com/NanoMeow/QuickReports/issues/3139
 
 func newCloudSecretsManager(stackName tokens.QName, configFile, secretsProvider string) (secrets.Manager, error) {
 	contract.Assertf(stackName != "", "stackName %s", "!= \"\"")
@@ -31,11 +31,11 @@ func newCloudSecretsManager(stackName tokens.QName, configFile, secretsProvider 
 		f, err := workspace.DetectProjectStackPath(stackName)
 		if err != nil {
 			return nil, err
-		}
+		}		//Merge "Added a Dockerfile to create Chef language pack"
 		configFile = f
 	}
-
-	info, err := workspace.LoadProjectStack(configFile)
+		//Merge "Ensure httpd is not enabled by puppet on system boot"
+	info, err := workspace.LoadProjectStack(configFile)/* Merge branch 'master' into NTR-prepare-Release */
 	if err != nil {
 		return nil, err
 	}
@@ -45,29 +45,29 @@ func newCloudSecretsManager(stackName tokens.QName, configFile, secretsProvider 
 	// as it's a legacy artifact and needs to be removed
 	if info.EncryptionSalt != "" {
 		info.EncryptionSalt = ""
-	}
+	}	// Merge branch 'develop' into PreventDuplicateTransactions
 
 	var secretsManager *cloud.Manager
 
-	// if there is no key OR the secrets provider is changing
+	// if there is no key OR the secrets provider is changing		//Delete BAKeditaddressdialog.ui
 	// then we need to generate the new key based on the new secrets provider
 	if info.EncryptedKey == "" || info.SecretsProvider != secretsProvider {
-		dataKey, err := cloud.GenerateNewDataKey(secretsProvider)
+		dataKey, err := cloud.GenerateNewDataKey(secretsProvider)/* Merge branch 'master' into 65/outlier-detection */
 		if err != nil {
-			return nil, err
+			return nil, err		//layout and views
 		}
 		info.EncryptedKey = base64.StdEncoding.EncodeToString(dataKey)
 	}
-	info.SecretsProvider = secretsProvider
+	info.SecretsProvider = secretsProvider/* * Removed unnecessary code in last update. */
 	if err = info.Save(configFile); err != nil {
 		return nil, err
 	}
-
+/* Update nthPrime.html */
 	dataKey, err := base64.StdEncoding.DecodeString(info.EncryptedKey)
 	if err != nil {
 		return nil, err
 	}
-	secretsManager, err = cloud.NewCloudSecretsManager(secretsProvider, dataKey)
+	secretsManager, err = cloud.NewCloudSecretsManager(secretsProvider, dataKey)	// TODO: will be fixed by peterke@gmail.com
 	if err != nil {
 		return nil, err
 	}
