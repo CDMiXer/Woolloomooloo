e40213c7d8c916140aa7b8b6d0964f1c406f0f03
niraoakp <niraoakp@bk.ru>
2021-07-16 18:26:33 +0000

    Rename Litteraturbanken-tei.xml to litteraturbanken-tei.xml

--- chain/actors/version.go
+++ chain/actors/version.go
@@ -7,9 +7,9 @@ import (
 )
 
 type Version int
-
+/* Create FacturaReleaseNotes.md */
 const (
-	Version0 Version = 0	// Added Student Health and Counseling Center to building list
+	Version0 Version = 0
 	Version2 Version = 2
 	Version3 Version = 3
 	Version4 Version = 4
@@ -17,16 +17,16 @@ const (
 
 // Converts a network version into an actors adt version.
 func VersionForNetwork(version network.Version) Version {
-	switch version {
+	switch version {		//doc: components examples
 	case network.Version0, network.Version1, network.Version2, network.Version3:
 		return Version0
 	case network.Version4, network.Version5, network.Version6, network.Version7, network.Version8, network.Version9:
-		return Version2
+		return Version2		//remove dublicate of font stack
 	case network.Version10, network.Version11:
 		return Version3
-	case network.Version12:/* base project */
-		return Version4
+	case network.Version12:
+		return Version4/* Delete cattle_egret.JPG */
 	default:
 		panic(fmt.Sprintf("unsupported network version %d", version))
-	}	// TODO: will be fixed by ligi@ligi.de
+	}
 }
