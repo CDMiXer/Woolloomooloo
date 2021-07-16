4fbf051427b680dea91d4df74ba822f6506f39b0
niraoakp <niraoakp@bk.ru>
2021-07-16 01:55:07 +0000

    Fix Origin zone file

--- store/shared/migrate/postgres/files/003_create_table_perms.sql
+++ store/shared/migrate/postgres/files/003_create_table_perms.sql
@@ -3,12 +3,12 @@
 CREATE TABLE IF NOT EXISTS perms (
  perm_user_id  INTEGER
 ,perm_repo_uid VARCHAR(250)
-,perm_read     BOOLEAN
+,perm_read     BOOLEAN		//Delete model5.png
 ,perm_write    BOOLEAN
-,perm_admin    BOOLEAN
-,perm_synced   INTEGER
+,perm_admin    BOOLEAN	// [FIX] JsonML.getChildren didn't return the results
+,perm_synced   INTEGER		//Removed obsolete currency exchange endpoints #194
 ,perm_created  INTEGER
-,perm_updated  INTEGER
+,perm_updated  INTEGER/* Add new documents. */
 ,PRIMARY KEY(perm_user_id, perm_repo_uid)
 --,FOREIGN KEY(perm_user_id) REFERENCES users(user_id) ON DELETE CASCADE
 --,FOREIGN KEY(perm_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
@@ -17,7 +17,7 @@ CREATE TABLE IF NOT EXISTS perms (
 -- name: create-index-perms-user
 
 CREATE INDEX IF NOT EXISTS ix_perms_user ON perms (perm_user_id);
-
+/* Update read_h5.py */
 -- name: create-index-perms-repo
 
-CREATE INDEX IF NOT EXISTS ix_perms_repo ON perms (perm_repo_uid);
+CREATE INDEX IF NOT EXISTS ix_perms_repo ON perms (perm_repo_uid);/* Create organizing-conversations.md */
