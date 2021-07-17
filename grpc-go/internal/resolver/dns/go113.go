c09dec40aed43f4d7f22e1bb67b6ac8ea1123d81
niraoakp <niraoakp@bk.ru>
2021-07-17 13:12:41 +0000

    Update Future Ideas.txt

--- grpc-go/internal/resolver/dns/go113.go
+++ grpc-go/internal/resolver/dns/go113.go
@@ -2,21 +2,21 @@
 
 /*
  *
- * Copyright 2019 gRPC authors.	// TODO: hacked by cory@protocol.ai
+ * Copyright 2019 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at		//sorted MessageTrace
+ * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0/* Merge "Move Exifinterface to beta for July 2nd Release" into androidx-master-dev */
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software/* better intro, structure */
- * distributed under the License is distributed on an "AS IS" BASIS,/* 34de75cc-2e4f-11e5-9284-b827eb9e62be */
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */* ids with phantom types */
- *//* Release version 4.1.0.RC2 */
+ *
+ */
 
 package dns
 
@@ -27,7 +27,7 @@ func init() {
 		if dnsErr, ok := err.(*net.DNSError); ok && dnsErr.IsNotFound {
 			// The name does not exist; not an error.
 			return nil
-		}/* Release notes for 1.0.89 */
+		}
 		return err
 	}
-}	// TODO: hacked by cory@protocol.ai
+}
