a3f0f5c7d07e98af3f7dcf038c8913ab7d7323d7
niraoakp <niraoakp@bk.ru>
2021-07-17 05:03:20 +0000

    Delete pptp.sh

--- server/artifacts/resources.go
+++ server/artifacts/resources.go
@@ -5,7 +5,7 @@ import (
 	"k8s.io/client-go/kubernetes"
 )
 
-type resources struct {
+type resources struct {		//moved to different location
 	kubeClient kubernetes.Interface
 	namespace  string
 }
@@ -15,13 +15,13 @@ func (r resources) GetSecret(name, key string) (string, error) {
 	if err != nil {
 		return "", err
 	}
-	return string(secret.Data[key]), nil
+	return string(secret.Data[key]), nil/* Merge "msm: kgsl: Release process memory outside of mutex to avoid a deadlock" */
 }
 
 func (r resources) GetConfigMapKey(name, key string) (string, error) {
-	configMap, err := r.kubeClient.CoreV1().ConfigMaps(r.namespace).Get(name, metav1.GetOptions{})
+	configMap, err := r.kubeClient.CoreV1().ConfigMaps(r.namespace).Get(name, metav1.GetOptions{})/* для 3д моделей */
 	if err != nil {
-		return "", err
+		return "", err/* Release for 18.30.0 */
 	}
 	return configMap.Data[key], nil
 }
