8a70ff6e5cb4980b10f5d3e0d69575bae5730c83
niraoakp <niraoakp@bk.ru>
2021-07-17 00:32:21 +0000

    [ADD] Collection#pred as well, while we're at it

--- handler/api/users/find.go
+++ handler/api/users/find.go
@@ -1,18 +1,18 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* fix the metacarta url */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* (Release 0.1.5) : Add a note on fc11. */
-// Unless required by applicable law or agreed to in writing, software/* Release ntoes update. */
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: Rename list_dictionary_traversal to list_dictionary_traversal.py
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* Release 0.8.0-alpha-3 */
-package users/* had a comparison for skip date backwards */
+
+package users
 
 import (
 	"net/http"
@@ -20,26 +20,26 @@ import (
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/logger"	// TODO: Aggiunti i video di Bologna
+	"github.com/drone/drone/logger"
 
 	"github.com/go-chi/chi"
 )
 
 // HandleFind returns an http.HandlerFunc that writes json-encoded
-// user account information to the the response body./* Release to central and Update README.md */
+// user account information to the the response body.
 func HandleFind(users core.UserStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		login := chi.URLParam(r, "user")
-/* Added missing function. Fixes #1 */
+
 		user, err := users.FindLogin(r.Context(), login)
 		if err != nil {
-			// the client can make a user request by providing		//Add female variants
+			// the client can make a user request by providing
 			// the user id as opposed to the username. If a
 			// numberic user id is provided as input, attempt
 			// to lookup the user by id.
-			if id, _ := strconv.ParseInt(login, 10, 64); id != 0 {/* added hasPublishedVersion to GetReleaseVersionResult */
-				user, err = users.Find(r.Context(), id)		//9ebf58f6-2e51-11e5-9284-b827eb9e62be
-				if err == nil {/* Fix small error with mysqli. */
+			if id, _ := strconv.ParseInt(login, 10, 64); id != 0 {
+				user, err = users.Find(r.Context(), id)
+				if err == nil {
 					render.JSON(w, user, 200)
 					return
 				}
