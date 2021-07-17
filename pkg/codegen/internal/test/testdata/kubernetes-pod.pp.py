dc27a33a502c10096a77fdbf7e8817336feac019
niraoakp <niraoakp@bk.ru>
2021-07-17 16:54:58 +0000

    Update Switch-Full.sh

--- pkg/codegen/internal/test/testdata/kubernetes-pod.pp.py
+++ pkg/codegen/internal/test/testdata/kubernetes-pod.pp.py
@@ -5,17 +5,17 @@ bar = kubernetes.core.v1.Pod("bar",
     api_version="v1",
     kind="Pod",
     metadata=kubernetes.meta.v1.ObjectMetaArgs(
-        namespace="foo",/* Released springrestcleint version 2.4.6 */
-        name="bar",	// TODO: 557d219a-2e43-11e5-9284-b827eb9e62be
+        namespace="foo",
+        name="bar",
     ),
     spec=kubernetes.core.v1.PodSpecArgs(
         containers=[kubernetes.core.v1.ContainerArgs(
             name="nginx",
-            image="nginx:1.14-alpine",	// TODO: Merge "msm: timer: featurize smd dependencies" into android-msm-2.6.32
+            image="nginx:1.14-alpine",
             resources=kubernetes.core.v1.ResourceRequirementsArgs(
-                limits={	// TODO: hacked by julia@jvns.ca
+                limits={
                     "memory": "20Mi",
-                    "cpu": "0.2",	// change data type of custom_field_id
+                    "cpu": "0.2",
                 },
             ),
         )],
