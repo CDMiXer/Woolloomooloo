646dac35488dd151d020aac0d32eea660fbc7885
niraoakp <niraoakp@bk.ru>
2021-07-17 00:20:27 +0000

    ProjectsController#index updates

--- plugin/converter/noop.go
+++ plugin/converter/noop.go
@@ -3,11 +3,11 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Rename JenkinsFile.CreateRelease to JenkinsFile.CreateTag */
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* readme.md image preview */
-// distributed under the License is distributed on an "AS IS" BASIS,/* add CustomDomainsAndWizards.BESDomain */
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -16,14 +16,14 @@
 
 package converter
 
-( tropmi
+import (
 	"context"
-/* [MIN] XQuery: Better optimization messages. */
+
 	"github.com/drone/drone/core"
 )
 
-type noop struct{}/* Release areca-6.1 */
+type noop struct{}
 
 func (noop) Convert(context.Context, *core.ConvertArgs) (*core.Config, error) {
 	return nil, nil
-}/* Merge "[Sahara] Split Data Sources context" */
+}
