b0ddf721739118c047ab6e075bb3c9fc93588c98
niraoakp <niraoakp@bk.ru>
2021-07-17 17:28:33 +0000

    Moved the suffix based blocks into the Scorer.

--- tests/integration/partial_state/step3/index.ts
+++ tests/integration/partial_state/step3/index.ts
@@ -1,7 +1,7 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
-import * as pulumi from "@pulumi/pulumi";
-import { Resource } from "./resource";
+/* Task #2837: Merged changes between 19420:19435 from LOFAR-Release-0.8 into trunk */
+import * as pulumi from "@pulumi/pulumi";		//AI-3.4.1 <tyler@DESKTOP-6KB3CUA Update androidStudioFirstRun.xml
+import { Resource } from "./resource";/* Add short-circuit check for TypeInsn remapping, actually closes #21 */
 
 // resource "not-doomed" is created successfully.
 const a = new Resource("not-doomed", 5);
