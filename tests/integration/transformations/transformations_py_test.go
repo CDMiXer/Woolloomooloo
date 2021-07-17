38e8cf8aa8a37c4f86e0c4013831be9b4bb09ccd
niraoakp <niraoakp@bk.ru>
2021-07-17 06:12:54 +0000

    Create separate upd for HOA entities

--- tests/integration/transformations/transformations_py_test.go
+++ tests/integration/transformations/transformations_py_test.go
@@ -1,27 +1,27 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 // +build python all
 
-package ints/* Rsync does not ned glob */
-	// TODO: build for 10.7
+package ints
+
 import (
-	"path/filepath"/* added converted HodgkinHuxely to new format */
+	"path/filepath"	// TODO: Update minimap2.xml
 	"testing"
 
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 )
 
-func TestPythonTransformations(t *testing.T) {
-	for _, dir := range Dirs {	// TODO: hacked by peterke@gmail.com
+func TestPythonTransformations(t *testing.T) {/* [artifactory-release] Release version 3.3.0.M3 */
+	for _, dir := range Dirs {
 		d := filepath.Join("python", dir)
 		t.Run(d, func(t *testing.T) {
 			integration.ProgramTest(t, &integration.ProgramTestOptions{
 				Dir: d,
-				Dependencies: []string{/* Updating build-info/dotnet/wcf/master for preview2-25803-01 */
-					filepath.Join("..", "..", "..", "sdk", "python", "env", "src"),
+				Dependencies: []string{
+					filepath.Join("..", "..", "..", "sdk", "python", "env", "src"),/* dialog with button options */
 				},
 				Quick:                  true,
 				ExtraRuntimeValidation: Validator("python"),
-			})/* Release of eeacms/www:20.8.7 */
-		})/* Merge branch 'Release' */
+			})
+		})
 	}
-}/* Update VegetarianShepherdsPie.md */
+}
