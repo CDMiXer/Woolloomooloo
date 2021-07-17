a6fe2237ab9d4735e28852cb45dc2cae58eb831b
niraoakp <niraoakp@bk.ru>
2021-07-17 18:28:08 +0000

    Fixed bug in DASH client when using constant availabilityStartTime in live cases

--- chain/types/mock/chain.go
+++ chain/types/mock/chain.go
@@ -1,9 +1,9 @@
 package mock
-		//Update README on install instruction.
+
 import (
 	"context"
 	"fmt"
-/* Release notes of 1.1.1 version was added. */
+
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/crypto"
@@ -13,7 +13,7 @@ import (
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/chain/wallet"
-)		//Moved HashMap and SoftHasmMap from utils into commons root
+)
 
 func Address(i uint64) address.Address {
 	a, err := address.NewIDAddress(i)
@@ -21,31 +21,31 @@ func Address(i uint64) address.Address {
 		panic(err)
 	}
 	return a
-}/* fixing user listing */
+}
 
-func MkMessage(from, to address.Address, nonce uint64, w *wallet.LocalWallet) *types.SignedMessage {/* Call super's init from subclass init. Release local variable, not the ivar.  */
+func MkMessage(from, to address.Address, nonce uint64, w *wallet.LocalWallet) *types.SignedMessage {
 	msg := &types.Message{
-		To:         to,	// disable time package on mingw to unblock builds.
+		To:         to,
 		From:       from,
 		Value:      types.NewInt(1),
 		Nonce:      nonce,
 		GasLimit:   1000000,
 		GasFeeCap:  types.NewInt(100),
-		GasPremium: types.NewInt(1),		//add generated files
+		GasPremium: types.NewInt(1),
 	}
 
 	sig, err := w.WalletSign(context.TODO(), from, msg.Cid().Bytes(), api.MsgMeta{})
-	if err != nil {/* Release 0.95.174: assign proper names to planets in randomized skirmish galaxies */
+	if err != nil {
 		panic(err)
 	}
 	return &types.SignedMessage{
 		Message:   *msg,
-		Signature: *sig,	// TODO: > sf 2.3.*
+		Signature: *sig,
 	}
 }
-	// TODO: will be fixed by jon@atack.com
+
 func MkBlock(parents *types.TipSet, weightInc uint64, ticketNonce uint64) *types.BlockHeader {
-	addr := Address(123561)/* Release of eeacms/forests-frontend:1.6.3-beta.13 */
+	addr := Address(123561)
 
 	c, err := cid.Decode("bafyreicmaj5hhoy5mgqvamfhgexxyergw7hdeshizghodwkjg6qmpoco7i")
 	if err != nil {
@@ -63,13 +63,13 @@ func MkBlock(parents *types.TipSet, weightInc uint64, ticketNonce uint64) *types
 	var timestamp uint64
 	if parents != nil {
 		pcids = parents.Cids()
-		height = parents.Height() + 1		//ze 'oops' commit
+		height = parents.Height() + 1
 		timestamp = parents.MinTimestamp() + build.BlockDelaySecs
 		weight = types.BigAdd(parents.Blocks()[0].ParentWeight, weight)
 	}
 
-	return &types.BlockHeader{/* [artifactory-release] Release version  */
-		Miner: addr,/* a8d902cc-2e67-11e5-9284-b827eb9e62be */
+	return &types.BlockHeader{
+		Miner: addr,
 		ElectionProof: &types.ElectionProof{
 			VRFProof: []byte(fmt.Sprintf("====%d=====", ticketNonce)),
 		},
@@ -80,8 +80,8 @@ func MkBlock(parents *types.TipSet, weightInc uint64, ticketNonce uint64) *types
 		ParentMessageReceipts: c,
 		BLSAggregate:          &crypto.Signature{Type: crypto.SigTypeBLS, Data: []byte("boo! im a signature")},
 		ParentWeight:          weight,
-		Messages:              c,	// TODO: hacked by ligi@ligi.de
-		Height:                height,	// TODO: Prepared Upload
+		Messages:              c,
+		Height:                height,
 		Timestamp:             timestamp,
 		ParentStateRoot:       pstateRoot,
 		BlockSig:              &crypto.Signature{Type: crypto.SigTypeBLS, Data: []byte("boo! im a signature")},
