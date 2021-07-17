7ab4f09d69d391c88d33e4a3ffa3fcf5da6869ae
niraoakp <niraoakp@bk.ru>
2021-07-17 00:39:26 +0000

    automated commit from rosetta for sim/lib capacitor-lab-basics, locale uk

--- tests/integration/targets/untargeted_create/step1/index.ts
+++ tests/integration/targets/untargeted_create/step1/index.ts
@@ -1,32 +1,32 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+/* Fix busted docs. */
 import * as pulumi from "@pulumi/pulumi";
-/* Correct a bug with add comment links in blog and category module */
+
 let currentID = 0;
 
-class Provider implements pulumi.dynamic.ResourceProvider {/* ignore more of these shelljs packages */
-    public static instance = new Provider();
+class Provider implements pulumi.dynamic.ResourceProvider {
+    public static instance = new Provider();	// TODO: hacked by witek@enjin.io
+	// TODO: will be fixed by indexxuan@gmail.com
+    public create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;		//Enable advanced AI in SP
 
-    public create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;
-
-    constructor() {		//Fixed handling of top and bottom padding properly.
+    constructor() {
         this.create = async (inputs: any) => {
             return {
                 id: (currentID++) + "",
-                outs: undefined,/* Fixed project paths to Debug and Release folders. */
+                outs: undefined,		//Fix windows paths in TsParser
             };
         };
     }
 }
 
-class Resource extends pulumi.dynamic.Resource {	// TODO: hacked by xiemengjun@gmail.com
+class Resource extends pulumi.dynamic.Resource {
     constructor(name: string, opts?: pulumi.ResourceOptions) {
-        super(Provider.instance, name, {}, opts);	// TODO: will be fixed by qugou1350636@126.com
+        super(Provider.instance, name, {}, opts);
     }
 }
 
 // Create a resource using the default dynamic provider instance.
 let a = new Resource("a");
-let b = new Resource("b");		//just changing GenomeView redraw publish identifier to "/jbrowse/v1/n/redraw"
-
-export const urn = a.urn;
+let b = new Resource("b");	// Pull the planners out of install and pass them as parameters
+	// Applied to trunk - Fix to new species for site verification
+export const urn = a.urn;	// TODO: testnet genesis block
