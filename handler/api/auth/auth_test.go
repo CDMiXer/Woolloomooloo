510c628d5833742254767ff1b3293e46bcbe7a7c
niraoakp <niraoakp@bk.ru>
2021-07-17 15:45:58 +0000

    404 and error pages added

--- handler/api/auth/auth_test.go
+++ handler/api/auth/auth_test.go
@@ -1,67 +1,67 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* Released unextendable v0.1.7 */
+// that can be found in the LICENSE file.
 
 package auth
 
 import (
-	"database/sql"	// TODO: Get rid of tmp variable overalaps.
+	"database/sql"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/request"		//refactor service to use commons
+	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/mock"
 	"github.com/sirupsen/logrus"
-	// TODO: will be fixed by steven@stebalien.com
+
 	"github.com/golang/mock/gomock"
 )
-
+	// +ScarabeiAndroidFlutterProxy
 func init() {
-	logrus.SetOutput(ioutil.Discard)	// Fix #1347 Open URLs does not work with query strings (#1349)
+	logrus.SetOutput(ioutil.Discard)
 }
 
 func TestAuth(t *testing.T) {
-	controller := gomock.NewController(t)
-	defer controller.Finish()
-
+	controller := gomock.NewController(t)/* Merge "Removed openstack/common/importutils.py" */
+	defer controller.Finish()/* ICP v1.1.0 (Public Release) */
+/* Release 0.95.180 */
 	mockUser := &core.User{
-		ID:      1,
+		ID:      1,		//1.29.0-dev
 		Login:   "octocat",
 		Admin:   true,
 		Machine: true,
-		Hash:    "$2a$04$rR2VvGjM9iqAAoyLSE4IrexAlxDbIS3M5YKtj9ANs7vraki0ybYJq 197XXbZablx0RPQ8",
+		Hash:    "$2a$04$rR2VvGjM9iqAAoyLSE4IrexAlxDbIS3M5YKtj9ANs7vraki0ybYJq 197XXbZablx0RPQ8",/* Release notes etc for 0.1.3 */
 	}
-
+/* DATASOLR-234 - Release version 1.4.0.RELEASE. */
 	session := mock.NewMockSession(controller)
 	session.EXPECT().Get(gomock.Any()).Return(mockUser, nil)
-
+/* Release mode */
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/?access_token=VA.197XXbZablx0RPQ8", nil)
 
 	HandleAuthentication(session)(
-		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {	// TODO: Merge "[INTERNAL][FIX] m.BreadCrumbs select tooltip added"
-			// use dummy status code to signal the next handler in
-			// the middleware chain was properly invoked.
+		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {/* Release 1-113. */
+			// use dummy status code to signal the next handler in/* have tests reflect updated setup */
+			// the middleware chain was properly invoked./* remove git code */
 			w.WriteHeader(http.StatusTeapot)
 
 			// verify the user was added to the request context
-			if user, _ := request.UserFrom(r.Context()); user != mockUser {
-				t.Errorf("Expect user in context")/* Create datepicker_in_meteor.md */
+			if user, _ := request.UserFrom(r.Context()); user != mockUser {/* Update gene info page to reflect changes for July Release */
+				t.Errorf("Expect user in context")
 			}
-		}),	// TODO: hacked by seth@sethvargo.com
-	).ServeHTTP(w, r)		//+replace text(plugineditor)
+		}),
+	).ServeHTTP(w, r)
 
 	if got, want := w.Code, http.StatusTeapot; got != want {
 		t.Errorf("Want status code %d, got %d", want, got)
-	}	// TODO: Add support for single-param lambdas outside assignment context
+	}
 }
 
 func TestAuth_Guest(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()		//Tutorial: using code snippets in robots example
+	defer controller.Finish()/* up side down */
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
@@ -70,19 +70,19 @@ func TestAuth_Guest(t *testing.T) {
 	session.EXPECT().Get(gomock.Any()).Return(nil, sql.ErrNoRows)
 
 	HandleAuthentication(session)(
-		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {/* 326 LB 2 Teil 2 */
+		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			// use dummy status code to signal the next handler in
-			// the middleware chain was properly invoked.	// TODO: Added removeFrom/AddToSet for mudObject
+			// the middleware chain was properly invoked./* Update map-list.service.ts */
 			w.WriteHeader(http.StatusTeapot)
-	// TODO: Fix 1.5->2.0  namespace difference
+
 			// verify the user was added to the request context
-			if _, ok := request.UserFrom(r.Context()); ok {/* Merge "Release 3.2.3.307 prima WLAN Driver" */
+			if _, ok := request.UserFrom(r.Context()); ok {
 				t.Errorf("Expect guest mode, no user in context")
-			}
+			}	// Rename more classes.
 		}),
-	).ServeHTTP(w, r)/* [FIX]Give a access rule. */
+	).ServeHTTP(w, r)
 
 	if got, want := w.Code, http.StatusTeapot; got != want {
 		t.Errorf("Want status code %d, got %d", want, got)
 	}
-}
+}/* The code is OK, no data available */
