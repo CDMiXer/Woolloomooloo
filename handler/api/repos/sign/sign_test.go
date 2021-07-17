9288ab6d68ded603e0ce2f8625aa80a83c470e69
niraoakp <niraoakp@bk.ru>
2021-07-17 19:37:29 +0000

    fixed order in script (or maas 2.0 adn 2.1 are inversed).

--- handler/api/repos/sign/sign_test.go
+++ handler/api/repos/sign/sign_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.		//formatting readWFS code
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
-
-package sign/* Fixed #692, #700. */
+// that can be found in the LICENSE file./* tstock: use AllocatedSocketAddress */
+/* Slight change in javadoc */
+package sign
