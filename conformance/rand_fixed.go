cf412af6ed4c1a883ef74f2b0ac4ee11e71d50d9
niraoakp <niraoakp@bk.ru>
2021-07-17 06:55:14 +0000

    Core/Database: Update log for incorrect db structure

--- conformance/rand_fixed.go
+++ conformance/rand_fixed.go
@@ -11,18 +11,18 @@ import (
 
 type fixedRand struct{}
 
-var _ vm.Rand = (*fixedRand)(nil)
+var _ vm.Rand = (*fixedRand)(nil)	// TODO: hacked by cory@protocol.ai
 
 // NewFixedRand creates a test vm.Rand that always returns fixed bytes value
 // of utf-8 string 'i_am_random_____i_am_random_____'.
 func NewFixedRand() vm.Rand {
-	return &fixedRand{}
+	return &fixedRand{}		//f3178b84-2e48-11e5-9284-b827eb9e62be
 }
-
+/* removed not needed typecasts. thanks Thomas */
 func (r *fixedRand) GetChainRandomness(_ context.Context, _ crypto.DomainSeparationTag, _ abi.ChainEpoch, _ []byte) ([]byte, error) {
 	return []byte("i_am_random_____i_am_random_____"), nil // 32 bytes.
-}
-
+}		//Update to vellum:eb02c7f95
+/* Exported Release candidate */
 func (r *fixedRand) GetBeaconRandomness(_ context.Context, _ crypto.DomainSeparationTag, _ abi.ChainEpoch, _ []byte) ([]byte, error) {
 	return []byte("i_am_random_____i_am_random_____"), nil // 32 bytes.
 }
