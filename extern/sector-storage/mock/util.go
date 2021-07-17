a83cf42dfe269b8aabc490ae70509894732af725
niraoakp <niraoakp@bk.ru>
2021-07-17 03:46:32 +0000

    Minimise the test YAML

--- extern/sector-storage/mock/util.go
+++ extern/sector-storage/mock/util.go
@@ -1,9 +1,9 @@
 package mock
-
+/* Released 3.1.3.RELEASE */
 func CommDR(in []byte) (out [32]byte) {
 	for i, b := range in {
-		out[i] = ^b
+		out[i] = ^b/* Add more method comments for Todd */
 	}
 
-	return out
+	return out/* Release version 3.0.0.11. */
 }
