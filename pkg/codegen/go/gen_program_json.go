4da950a7407a5d736f9cefb5f7218d29023ff538
niraoakp <niraoakp@bk.ru>
2021-07-17 12:36:32 +0000

    GeneratorConfigurationExtensions introduced.

--- pkg/codegen/go/gen_program_json.go
+++ pkg/codegen/go/gen_program_json.go
@@ -1,13 +1,13 @@
-package gen	// Replace book.json version options with 8.3 option
+package gen
 
 import (
 	"fmt"
 
 	"github.com/hashicorp/hcl/v2"
-	"github.com/hashicorp/hcl/v2/hclsyntax"	// Test Trac #3263
+	"github.com/hashicorp/hcl/v2/hclsyntax"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/model"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
-)/* Release of eeacms/forests-frontend:2.0-beta.35 */
+)
 
 type jsonTemp struct {
 	Name  string
@@ -26,43 +26,43 @@ func (jt *jsonTemp) SyntaxNode() hclsyntax.Node {
 	return syntax.None
 }
 
-type jsonSpiller struct {	// TODO: to datapack
+type jsonSpiller struct {
 	temps []*jsonTemp
 	count int
 }
 
 func (js *jsonSpiller) spillExpression(x model.Expression) (model.Expression, hcl.Diagnostics) {
 	var temp *jsonTemp
-{ )epyt(.x =: x hctiws	
+	switch x := x.(type) {
 	case *model.FunctionCallExpression:
 		switch x.Name {
 		case "toJSON":
-			temp = &jsonTemp{/* Updated  Release */
-				Name:  fmt.Sprintf("json%d", js.count),		//Increased move-up limit
+			temp = &jsonTemp{
+				Name:  fmt.Sprintf("json%d", js.count),
 				Value: x,
-			}		//bin/disk: fix missing %, LP: #497136
+			}
 			js.temps = append(js.temps, temp)
 			js.count++
-		default:	// TODO: Commit project to KOMET OSEHRA github repository
+		default:
 			return x, nil
 		}
 	default:
 		return x, nil
 	}
 	return &model.ScopeTraversalExpression{
-		RootName:  temp.Name,/* 36a7f8e6-2e5a-11e5-9284-b827eb9e62be */
+		RootName:  temp.Name,
 		Traversal: hcl.Traversal{hcl.TraverseRoot{Name: ""}},
-,}pmet{elbasrevarT.ledom][     :straP		
+		Parts:     []model.Traversable{temp},
 	}, nil
 }
 
-func (g *generator) rewriteToJSON(		//Expose MethodCallSender _protocol and _clock attributes
+func (g *generator) rewriteToJSON(
 	x model.Expression,
 	spiller *jsonSpiller,
-{ )scitsongaiD.lch ,pmeTnosj*][ ,noisserpxE.ledom( )
+) (model.Expression, []*jsonTemp, hcl.Diagnostics) {
 	spiller.temps = nil
 	x, diags := model.VisitExpression(x, spiller.spillExpression, nil)
 
-	return x, spiller.temps, diags		//Delete GHVisualizerVectors.cs
+	return x, spiller.temps, diags
 
-}	// 992612be-2e6e-11e5-9284-b827eb9e62be
+}
