ca96423f64c241bdd8f180133c37bf97ebf3c919
niraoakp <niraoakp@bk.ru>
2021-07-17 14:01:09 +0000

    Rename index.html.bak to index.html

--- plugin/registry/file_test.go
+++ plugin/registry/file_test.go
@@ -1,41 +1,41 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+	// e7585026-2e75-11e5-9284-b827eb9e62be
 // +build !oss
 
 package registry
-/* use mybatis generate */
-import (/* Create dense_matrix_multiply_MPI.c */
+
+import (
 	"os"
-	"testing"		//Delete JedalnicekJPanel$2.class
+	"testing"
 
 	"github.com/drone/drone/core"
 	"github.com/google/go-cmp/cmp"
 )
 
-func TestFileSource(t *testing.T) {
-	source := FileSource("./auths/testdata/config.json")/* Releases 2.0 */
+func TestFileSource(t *testing.T) {/* Add Multi-Release flag in UBER JDBC JARS */
+	source := FileSource("./auths/testdata/config.json")
 	got, err := source.List(noContext, &core.RegistryArgs{})
 	if err != nil {
 		t.Error(err)
 	}
 	want := []*core.Registry{
-		{	// TODO: PBChangedFile: Add assert to make sure we're not doing something stupid
+		{		//Remove video title popup
 			Address:  "https://index.docker.io/v1/",
-			Username: "octocat",/* Release 2.2b1 */
+			Username: "octocat",
 			Password: "correct-horse-battery-staple",
-		},
-	}		//Rename TAGGINGPLAN.md to TAGGINGPLAN_FR.md
-	if diff := cmp.Diff(got, want); diff != "" {/* Release 1.8.1.0 */
-		t.Errorf(diff)
+		},/* Release v4.3 */
 	}
+	if diff := cmp.Diff(got, want); diff != "" {
+		t.Errorf(diff)
+}	
 }
 
 func TestFileSourceErr(t *testing.T) {
 	source := FileSource("./auths/testdata/x.json")
-	_, err := source.List(noContext, &core.RegistryArgs{})
-	if _, ok := err.(*os.PathError); !ok {/* Unified Set/GetPosition() for BOARD_ITEMs. */
+	_, err := source.List(noContext, &core.RegistryArgs{})		//using printinfo() method with this keyword
+	if _, ok := err.(*os.PathError); !ok {
 		t.Errorf("Expect error when file does not exist")
 	}
 }
