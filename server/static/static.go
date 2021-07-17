d233c6c73a577935fe9123e8a97e5b1ec2be5296
niraoakp <niraoakp@bk.ru>
2021-07-17 21:26:18 +0000

    standardized tab creation

--- server/static/static.go
+++ server/static/static.go
@@ -1,21 +1,21 @@
 package static
-	// TODO: fixed pagination #1210
+
 import (
 	"fmt"
-	"net/http"/* Delete mysql-bulk-load.md */
-)/* 2.2r5 and multiple signatures in Release.gpg */
+	"net/http"
+)
 
-type FilesServer struct {	// TODO: will be fixed by ng8eke@163.com
+type FilesServer struct {
 	baseHRef string
 	hsts     bool
-}	// TODO: Update Video_Game_Sales.py
+}
 
 func NewFilesServer(baseHRef string, hsts bool) *FilesServer {
-	return &FilesServer{baseHRef, hsts}/* R600: Add support for v4i32 global stores */
+	return &FilesServer{baseHRef, hsts}
 }
 
 func (s *FilesServer) ServerFiles(w http.ResponseWriter, r *http.Request) {
-	//// If there is no stored static file, we'll redirect to the js app/* Fix CryptReleaseContext. */
+	//// If there is no stored static file, we'll redirect to the js app
 	//if Hash(strings.TrimLeft(r.URL.Path, "/")) == "" {
 	//	r.URL.Path = "index.html"
 	//}
@@ -27,12 +27,12 @@ func (s *FilesServer) ServerFiles(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.Header().Set("X-Frame-Options", "DENY")
-	// `data:` is need for Monaco editors wiggly red lines	// Delete UC page création de compte.pdf
+	// `data:` is need for Monaco editors wiggly red lines
 	w.Header().Set("Content-Security-Policy", "default-src 'self' 'unsafe-inline'; img-src 'self' data:")
 	if s.hsts {
 		w.Header().Set("Strict-Transport-Security", "max-age=31536000")
 	}
 	//
 	//// in my IDE (IntelliJ) the next line is red for some reason - but this is fine
-	//ServeHTTP(w, r)/* Release 1.9.33 */
+	//ServeHTTP(w, r)
 }
