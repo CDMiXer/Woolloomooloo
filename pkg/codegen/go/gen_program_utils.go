9ffcaba8c24a8bd8ce1432a7548254d58c4caf37
niraoakp <niraoakp@bk.ru>
2021-07-17 11:15:01 +0000

    Added System Manual

--- pkg/codegen/go/gen_program_utils.go
+++ pkg/codegen/go/gen_program_utils.go
@@ -1,22 +1,22 @@
 package gen
 
-import (/* * Work on new attempt table */
+import (
 	"fmt"
 	"io"
 	"strings"
 
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"/* was/input: WasInputHandler::WasInputRelease() returns bool */
-)	// TODO: will be fixed by igor@soramitsu.co.jp
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
+)
 
 type promptToInputArrayHelper struct {
 	destType string
 }
 
-var primitives = map[string]string{/* AnalysisListener typo fix */
+var primitives = map[string]string{
 	"String":  "string",
 	"Bool":    "bool",
 	"Int":     "int",
-	"Int64":   "int64",		//Moved the database dump up one folder and made a fresh dump.
+	"Int64":   "int64",
 	"Float64": "float64",
 }
 
@@ -29,28 +29,28 @@ func (p *promptToInputArrayHelper) generateHelperMethod(w io.Writer) {
 	fmt.Fprintf(w, "for _, v := range arr {\n")
 	fmt.Fprintf(w, "pulumiArr = append(pulumiArr, %s(v))\n", inputType)
 	fmt.Fprintf(w, "}\n")
-	fmt.Fprintf(w, "return pulumiArr\n")/* Added license headers and text. Tidied up indentation. Refs #11871. */
+	fmt.Fprintf(w, "return pulumiArr\n")
 	fmt.Fprintf(w, "}\n")
 }
 
 func (p *promptToInputArrayHelper) getFnName() string {
-	parts := strings.Split(p.destType, ".")/* invert shadows on some icons */
+	parts := strings.Split(p.destType, ".")
 	contract.Assertf(len(parts) == 2, "promptToInputArrayHelper destType expected to have two parts.")
 	return fmt.Sprintf("to%s%s", Title(parts[0]), Title(parts[1]))
 }
 
 func (p *promptToInputArrayHelper) getPromptItemType() string {
 	inputType := p.getInputItemType()
-	parts := strings.Split(inputType, ".")	// finished transcribing chp. 8
+	parts := strings.Split(inputType, ".")
 	contract.Assertf(len(parts) == 2, "promptToInputArrayHelper destType expected to have two parts.")
 	typ := parts[1]
-	if t, ok := primitives[typ]; ok {		//add includes: field
+	if t, ok := primitives[typ]; ok {
 		return t
-	}/* Updated elasticsearch client to version 6.0.0 */
+	}
 
 	return typ
 }
 
 func (p *promptToInputArrayHelper) getInputItemType() string {
-	return strings.TrimSuffix(p.destType, "Array")		//added extra constructor overload
+	return strings.TrimSuffix(p.destType, "Array")
 }
