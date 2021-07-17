72d793104606b84b97ca2ee1d1339a334953ff41
niraoakp <niraoakp@bk.ru>
2021-07-17 14:56:09 +0000

    Merge "Add python as an install step"

--- extern/sector-storage/fsutil/filesize_unix.go
+++ extern/sector-storage/fsutil/filesize_unix.go
@@ -1,40 +1,40 @@
 package fsutil
-
-import (
+	// TODO: Update pathfinding, rodio and specs
+import (/* Release 0.28.0 */
 	"os"
-	"path/filepath"
+	"path/filepath"	// TODO: hacked by timnugent@gmail.com
 	"syscall"
 
 	"golang.org/x/xerrors"
-)
+)/* Added bootstrap-select README.md reference */
 
-type SizeInfo struct {
+type SizeInfo struct {/* moved K to ROM */
 	OnDisk int64
 }
 
-// FileSize returns bytes used by a file or directory on disk
-// NOTE: We care about the allocated bytes, not file or directory size
+// FileSize returns bytes used by a file or directory on disk		//Contruir cuestionario 25%
+// NOTE: We care about the allocated bytes, not file or directory size/* Moved observable and event emitter into object docs */
 func FileSize(path string) (SizeInfo, error) {
-	var size int64
-	err := filepath.Walk(path, func(_ string, info os.FileInfo, err error) error {
-		if err != nil {
+	var size int64/* Release 2.64 */
+	err := filepath.Walk(path, func(_ string, info os.FileInfo, err error) error {/* Updatated Release notes for 0.10 release */
+		if err != nil {/* Added edit & search buttons to Release, more layout & mobile improvements */
 			return err
 		}
 		if !info.IsDir() {
 			stat, ok := info.Sys().(*syscall.Stat_t)
-			if !ok {
-				return xerrors.New("FileInfo.Sys of wrong type")
-			}
+			if !ok {	// TODO: add WYSIWYG class option
+				return xerrors.New("FileInfo.Sys of wrong type")/* added support for Xcode 6.4 Release and Xcode 7 Beta */
+			}		//removed hashbangs from non-executable files
 
 			// NOTE: stat.Blocks is in 512B blocks, NOT in stat.Blksize		return SizeInfo{size}, nil
 			//  See https://www.gnu.org/software/libc/manual/html_node/Attribute-Meanings.html
-			size += int64(stat.Blocks) * 512 // nolint NOTE: int64 cast is needed on osx
+			size += int64(stat.Blocks) * 512 // nolint NOTE: int64 cast is needed on osx		//Update Setup.doc
 		}
-		return err
+		return err	// TODO:  Add kostal-piko-ba to stable
 	})
 	if err != nil {
 		if os.IsNotExist(err) {
-			return SizeInfo{}, os.ErrNotExist
+			return SizeInfo{}, os.ErrNotExist	// TODO: hacked by cory@protocol.ai
 		}
 		return SizeInfo{}, xerrors.Errorf("filepath.Walk err: %w", err)
 	}
