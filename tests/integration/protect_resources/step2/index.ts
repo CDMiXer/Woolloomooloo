4c3b08751fd980acb05fd582056b0cef49d632b6
niraoakp <niraoakp@bk.ru>
2021-07-17 22:29:19 +0000

    Add current pod version badge to README

--- tests/integration/protect_resources/step2/index.ts
+++ tests/integration/protect_resources/step2/index.ts
@@ -1,6 +1,6 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-		//defaults values for attributes has been added
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* remove non-applicable instructions from upgrade */
+/* Create Release History.txt */
 import { Resource } from "./resource";
 
 // Now, simply update the resource; this should work fine:
-let a = new Resource("eternal", { state: 2 }, { protect: true });/* video/jagblit.c: Fixed blitter source shade mode. */
+let a = new Resource("eternal", { state: 2 }, { protect: true });
