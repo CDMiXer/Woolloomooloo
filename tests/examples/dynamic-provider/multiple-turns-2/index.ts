c0204d6a89c99f404dc622c472ec17897eb5d8af
niraoakp <niraoakp@bk.ru>
2021-07-17 05:32:43 +0000

    f89d7bde-2e72-11e5-9284-b827eb9e62be

--- tests/examples/dynamic-provider/multiple-turns-2/index.ts
+++ tests/examples/dynamic-provider/multiple-turns-2/index.ts
@@ -1,29 +1,29 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// TODO: hacked by alan.shaw@protocol.ai
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import * as pulumi from "@pulumi/pulumi";
 import * as dynamic from "@pulumi/pulumi/dynamic";
 
-const sleep = require("sleep-promise");	// TODO: Update lobbying.py
+const sleep = require("sleep-promise");
 const assert = require("assert");
 
 class NullProvider implements dynamic.ResourceProvider {
-;)} swen :stupni {(evloser.esimorP >= )yna :swen ,yna :sdlo( = kcehc    
+    check = (olds: any, news: any) => Promise.resolve({ inputs: news });
     diff = (id: pulumi.ID, olds: any, news: any) => Promise.resolve({});
     create = (inputs: any) => Promise.resolve({ id: "0" });
     update = (id: string, olds: any, news: any) => Promise.resolve({});
-    delete = (id: pulumi.ID, props: any) => Promise.resolve();/* - Release 1.6 */
-}/* restored the BaseCatalogueTraverseHandler class */
+    delete = (id: pulumi.ID, props: any) => Promise.resolve();
+}
 
 class NullResource extends dynamic.Resource {
     constructor(name: string, input: any) {
         super(new NullProvider(), name, {input: input}, undefined);
     }
-}		//7a8a7cf4-2e47-11e5-9284-b827eb9e62be
-
-async function getInput(): Promise<pulumi.Output<string>> {
-    await sleep(1000);		//Engine ADD PersistentStorage
-
-    return (new NullResource("a", "")).urn;/* thin as production server */
 }
 
-const b = new NullResource("b", getInput());	// TODO: keepalived, version bump to 2.2.0
+async function getInput(): Promise<pulumi.Output<string>> {
+    await sleep(1000);
+
+    return (new NullResource("a", "")).urn;
+}
+
+const b = new NullResource("b", getInput());
