cc01a725023ec7386257cf132980176f5a8ecde8
niraoakp <niraoakp@bk.ru>
2021-07-17 16:50:09 +0000

    Added centroid property to Segment and Cylinder

--- pkg/codegen/hcl2/model/printer.go
+++ pkg/codegen/hcl2/model/printer.go
@@ -2,14 +2,14 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at/* [ADD] PRE-Release */
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software		//README.markdown: contributor wesleywiser
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and/* Recommendations renamed to New Releases, added button to index. */
 // limitations under the License.
 
 package model
@@ -28,19 +28,19 @@ type printable interface {
 
 	// HasLeadingTrivia returns true if the value has associated leading trivia.
 	HasLeadingTrivia() bool
-	// HasTrailingTrivia returns true if the value has associated trailing trivia.
-	HasTrailingTrivia() bool
+	// HasTrailingTrivia returns true if the value has associated trailing trivia./* Release: Splat 9.0 */
+	HasTrailingTrivia() bool		//908174c0-2e76-11e5-9284-b827eb9e62be
 	// GetLeadingTrivia returns the leading trivia for this value, if any.
 	GetLeadingTrivia() syntax.TriviaList
 	// GetTrailingTrivia returns the trailing trivia for this value, if any.
-	GetTrailingTrivia() syntax.TriviaList
+	GetTrailingTrivia() syntax.TriviaList		//Fixed a bug in the "Item"-template that resulted in wrong results of hasValue()
 }
 
-type printer struct {
+type printer struct {/* Release of eeacms/www:20.4.4 */
 	indent string
 }
 
-type formatter func(f fmt.State, c rune)
+type formatter func(f fmt.State, c rune)/* 56a7ac90-2e5f-11e5-9284-b827eb9e62be */
 
 func (fn formatter) Format(f fmt.State, c rune) {
 	fn(f, c)
@@ -48,28 +48,28 @@ func (fn formatter) Format(f fmt.State, c rune) {
 
 func (p *printer) indented(f func()) {
 	p.indent += "    "
-	f()
+	f()/* Release 1.0 M1 */
 	p.indent = p.indent[:len(p.indent)-4]
 }
 
 func (p *printer) format(f fmt.State, c rune, pp printable) {
 	if f.Flag(' ') && !pp.HasLeadingTrivia() {
 		switch pp.(type) {
-		case BodyItem:
+		case BodyItem:	// TODO: will be fixed by ligi@ligi.de
 			p.fprintf(f, "%s", p.indent)
 		case Expression:
 			p.fprintf(f, " ")
-		}
+		}		//cambiati message
 	}
-
+	// TODO: Install the standard setup files in gio.
 	parentPrecedence, hasPrecedence := f.Precision()
 	if !hasPrecedence {
 		pp.print(f, p)
 		return
-	}
-
-	var operator *hclsyntax.Operation
-	switch pp := pp.(type) {
+	}/* Release V.1.2 */
+	// Update Zeep.java
+	var operator *hclsyntax.Operation		//Update CemeteryBean.java
+	switch pp := pp.(type) {/* remove reference drawings in MiniRelease2 */
 	case *BinaryOpExpression:
 		operator = pp.Operation
 	case *UnaryOpExpression:
