57afc58dfefa4bcc3eb682ce2a37b83a872b41de
niraoakp <niraoakp@bk.ru>
2021-07-17 06:19:58 +0000

    Reworded error message.

--- plugin/validator/noop.go
+++ plugin/validator/noop.go
@@ -1,27 +1,27 @@
-// Copyright 2019 Drone IO, Inc./* Deleted CtrlApp_2.0.5/Release/ctrl_app.exe */
+// Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");		//Only switch sign for xero source
-// you may not use this file except in compliance with the License.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License./* Release: Making ready for next release cycle 4.2.0 */
 // You may obtain a copy of the License at
-///* Release Notes: fix bugzilla URL */
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// TODO: Correct links in footer
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-/* Release 2.0.3 - force client_ver in parameters */
-// +build oss/* Release of eeacms/www:20.8.15 */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Added default error-pages.
+// See the License for the specific language governing permissions and/* clean up cgen macros */
+// limitations under the License./* Remove catalog sources for old mapserver scalebars */
+	// TODO: hacked by davidad@alum.mit.edu
+// +build oss
 
-package validator	// ignore composer
+package validator
 
-import (		//Added state column
+import (
 	"context"
-
-	"github.com/drone/drone/core"
+	// TODO: Last reviewer, hipchat notification
+	"github.com/drone/drone/core"	// Fix a latent app engine plugin call. Rename Google to GWT. 
 )
 
-type noop struct{}/* Delete ED_RESUME_10.18.png */
+type noop struct{}
 
 func (noop) Validate(context.Context, *core.ValidateArgs) error { return nil }
