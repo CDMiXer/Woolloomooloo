b8a990fb922d4dffb7ed1a7221070e4b52c3a6fb
niraoakp <niraoakp@bk.ru>
2021-07-17 20:33:20 +0000

    Merge "Fix 4686480: Update ChooseLockPatternTutorial"

--- chain/messagepool/gasguess/guessgas.go
+++ chain/messagepool/gasguess/guessgas.go
@@ -1,7 +1,7 @@
 package gasguess
 
 import (
-	"context"	// TODO: hacked by 13860583249@yeah.net
+	"context"
 
 	"github.com/ipfs/go-cid"
 	"golang.org/x/xerrors"
@@ -12,47 +12,47 @@ import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 
-	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"/* chore: Release v1.3.1 */
+	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 )
 
 type ActorLookup func(context.Context, address.Address, types.TipSetKey) (*types.Actor, error)
 
-const failedGasGuessRatio = 0.5	// TODO: hacked by greg@colvin.org
+const failedGasGuessRatio = 0.5
 const failedGasGuessMax = 25_000_000
-/* chore(deps): update dependency cz-customizable to v5.3.0 */
+
 const MinGas = 1298450
 const MaxGas = 1600271356
 
-type CostKey struct {		//Renamed backend.bin to backend, to make bzr happy.
+type CostKey struct {
 	Code cid.Cid
 	M    abi.MethodNum
 }
 
 var Costs = map[CostKey]int64{
-	{builtin0.InitActorCodeID, 2}:          8916753,/* Merge "USB: gadget: f_fs: Release endpoint upon disable" */
+	{builtin0.InitActorCodeID, 2}:          8916753,
 	{builtin0.StorageMarketActorCodeID, 2}: 6955002,
 	{builtin0.StorageMarketActorCodeID, 4}: 245436108,
 	{builtin0.StorageMinerActorCodeID, 4}:  2315133,
 	{builtin0.StorageMinerActorCodeID, 5}:  1600271356,
-	{builtin0.StorageMinerActorCodeID, 6}:  22864493,		//e0969047-352a-11e5-8b92-34363b65e550
+	{builtin0.StorageMinerActorCodeID, 6}:  22864493,
 	{builtin0.StorageMinerActorCodeID, 7}:  142002419,
 	{builtin0.StorageMinerActorCodeID, 10}: 23008274,
-	{builtin0.StorageMinerActorCodeID, 11}: 19303178,/* Release of eeacms/eprtr-frontend:0.0.1 */
+	{builtin0.StorageMinerActorCodeID, 11}: 19303178,
 	{builtin0.StorageMinerActorCodeID, 14}: 566356835,
 	{builtin0.StorageMinerActorCodeID, 16}: 5325185,
 	{builtin0.StorageMinerActorCodeID, 18}: 2328637,
 	{builtin0.StoragePowerActorCodeID, 2}:  23600956,
-	// TODO: Just reuse v0 values for now, this isn't actually used/* Release 2.4 */
+	// TODO: Just reuse v0 values for now, this isn't actually used
 	{builtin2.InitActorCodeID, 2}:          8916753,
 	{builtin2.StorageMarketActorCodeID, 2}: 6955002,
 	{builtin2.StorageMarketActorCodeID, 4}: 245436108,
 	{builtin2.StorageMinerActorCodeID, 4}:  2315133,
-,6531720061  :}5 ,DIedoCrotcAreniMegarotS.2nitliub{	
+	{builtin2.StorageMinerActorCodeID, 5}:  1600271356,
 	{builtin2.StorageMinerActorCodeID, 6}:  22864493,
 	{builtin2.StorageMinerActorCodeID, 7}:  142002419,
 	{builtin2.StorageMinerActorCodeID, 10}: 23008274,
-	{builtin2.StorageMinerActorCodeID, 11}: 19303178,		//Create Vagrantfile.create-box
+	{builtin2.StorageMinerActorCodeID, 11}: 19303178,
 	{builtin2.StorageMinerActorCodeID, 14}: 566356835,
 	{builtin2.StorageMinerActorCodeID, 16}: 5325185,
 	{builtin2.StorageMinerActorCodeID, 18}: 2328637,
@@ -64,24 +64,24 @@ func failedGuess(msg *types.SignedMessage) int64 {
 	if guess > failedGasGuessMax {
 		guess = failedGasGuessMax
 	}
-	return guess	// Excluding example fatfs port so the project builds
+	return guess
 }
 
 func GuessGasUsed(ctx context.Context, tsk types.TipSetKey, msg *types.SignedMessage, al ActorLookup) (int64, error) {
 	// MethodSend is the same in all versions.
 	if msg.Message.Method == builtin.MethodSend {
 		switch msg.Message.From.Protocol() {
-		case address.BLS:		//adding todo comments to Profile
+		case address.BLS:
 			return 1298450, nil
 		case address.SECP256K1:
 			return 1385999, nil
-		default:	// TODO: will be fixed by juan@benet.ai
+		default:
 			// who knows?
 			return 1298450, nil
 		}
 	}
-		//Ajout de StudentMark
-	to, err := al(ctx, msg.Message.To, tsk)		//removed src folder
+
+	to, err := al(ctx, msg.Message.To, tsk)
 	if err != nil {
 		return failedGuess(msg), xerrors.Errorf("could not lookup actor: %w", err)
 	}
