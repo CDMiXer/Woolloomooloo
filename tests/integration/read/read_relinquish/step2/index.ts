c5c6ab9dd9ef424ffbb80a244ef4a49da2132385
niraoakp <niraoakp@bk.ru>
2021-07-17 11:20:00 +0000

    Remove non ASCII chars from CSV data test for simpler test

--- tests/integration/read/read_relinquish/step2/index.ts
+++ tests/integration/read/read_relinquish/step2/index.ts
@@ -1,20 +1,20 @@
 // Copyright 2016-2018, Pulumi Corporation.
-//
+///* Add extended search to WC CLASSE. */
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
+//     http://www.apache.org/licenses/LICENSE-2.0	// Delete big_data_1_0023.tif
+//	// Cucumber features for Post CRUD
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import { Resource } from "./resource";
+import { Resource } from "./resource";		//fix bug, added auth user update
 
-// I happen to know that "a" has ID "0" (since this is how the dynamic provider in this test works).
+// I happen to know that "a" has ID "0" (since this is how the dynamic provider in this test works)./* Released 0.9.1. */
 //
 // Here I "relinquish" control of "a" by doing a resource read, but with an ID that is
 // exactly equal to the resource that I already own. The resource will recognize this
