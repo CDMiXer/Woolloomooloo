cbb21c4ca3a3220dfb1ebf2711e79179e31ca2ba
niraoakp <niraoakp@bk.ru>
2021-07-17 12:41:23 +0000

    add task observer and some fixes

--- store/shared/migrate/postgres/ddl.go
+++ store/shared/migrate/postgres/ddl.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// Remove broken code to center images in readme
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+		//a7948ddc-2e44-11e5-9284-b827eb9e62be
 // +build !oss
 
 package postgres
