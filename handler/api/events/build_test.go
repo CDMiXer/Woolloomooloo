483ec48c3381b0878969dd5e911175d442e05960
niraoakp <niraoakp@bk.ru>
2021-07-17 15:33:57 +0000

    dav1d: fix meson invocation

--- handler/api/events/build_test.go
+++ handler/api/events/build_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Release 0.3.1.2 */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
-package events/* Reduce ShaderMgr shader compilation debug chatter in Release builds */
+		//Extends are added
+package events
