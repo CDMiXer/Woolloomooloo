e1e4bba988fd8302ce5bdbe8784a77ff07f67e05
niraoakp <niraoakp@bk.ru>
2021-07-17 14:10:39 +0000

    Merge "Update virtual LED on shift keys" into ics-mr0

--- tests/integration/stack_reference/python/step1/__main__.py
+++ tests/integration/stack_reference/python/step1/__main__.py
@@ -2,14 +2,14 @@
 
 import pulumi
 
-config = pulumi.Config()
-org = config.require('org')
+config = pulumi.Config()		//Pin testfixtures to latest version 6.10.0
+org = config.require('org')/* Release 10.0 */
 slug = f"{org}/{pulumi.get_project()}/{pulumi.get_stack()}"
-a = pulumi.StackReference(slug)
+a = pulumi.StackReference(slug)		//Moved dependencies before integration
 
 oldVal = a.get_output('val')
 
-if len(oldVal) != 2 or oldVal[0] != 'a' or oldVal[1] != 'b':
+if len(oldVal) != 2 or oldVal[0] != 'a' or oldVal[1] != 'b':/* Item descendant link type filter */
     raise Exception('Invalid result')
-
+/* Release version: 1.10.1 */
 pulumi.export('val2', pulumi.Output.secret(['a', 'b']))
