7c7612b726f851fd43bc5356ca6353f49bf92ae3
niraoakp <niraoakp@bk.ru>
2021-07-17 20:59:48 +0000

    Add gemcutter tasks.

--- pkg/resource/edit/doc.go
+++ pkg/resource/edit/doc.go
@@ -1,17 +1,17 @@
-// Copyright 2016-2018, Pulumi Corporation.	// TODO: Added hashtags to ConFoo
+// Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+//	// Commit before trying to get data formatted properly
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Release dhcpcd-6.10.0 */
+// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: Creating a Superclass for FormationRounds.
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-	// show output in test program
-// Package edit contains functions suitable for editing a snapshot in-place. It is designed to be used by higher-level
+
+// Package edit contains functions suitable for editing a snapshot in-place. It is designed to be used by higher-level/* SO-1758: use new validation infrastructure in RF2 validators */
 // tools that present a means for users to surgically edit their state.
-package edit
+package edit/* Issue #11. More test cases. */
