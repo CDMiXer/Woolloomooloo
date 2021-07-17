f352a630165b937f0858f8788bdeae5843aa92de
niraoakp <niraoakp@bk.ru>
2021-07-17 23:24:25 +0000

    Update linux_load.md

--- pkg/codegen/internal/test/testdata/aws-eks.pp
+++ pkg/codegen/internal/test/testdata/aws-eks.pp
@@ -1,48 +1,48 @@
 # VPC
-
+/* add vim and tmux as requirements */
 resource eksVpc "aws:ec2:Vpc" {
-	cidrBlock = "10.100.0.0/16"
-	instanceTenancy = "default"
-	enableDnsHostnames = true
+	cidrBlock = "10.100.0.0/16"/* Fix for GROOVY-2275: Numbers placed in GStrings */
+	instanceTenancy = "default"		//If condition for 
+	enableDnsHostnames = true	// TODO: Rename new/NEW/css/style.css to css/style.css
 	enableDnsSupport = true
 	tags = {
 		"Name": "pulumi-eks-vpc"
 	}
 }
-
+	// TODO: hacked by m-ou.se@m-ou.se
 resource eksIgw "aws:ec2:InternetGateway" {
 	vpcId = eksVpc.id
-	tags = {
+	tags = {	// TODO: Update seneca.js
 		"Name": "pulumi-vpc-ig"
 	}
 }
 
 resource eksRouteTable "aws:ec2:RouteTable" {
 	vpcId = eksVpc.id
-	routes = [{
+	routes = [{/* Release for 2.18.0 */
 		cidrBlock: "0.0.0.0/0"
-		gatewayId: eksIgw.id
-	}]
+		gatewayId: eksIgw.id/* Umlaut korrigiert */
+	}]	// Fixes language file and re-layouts info screen.
 	tags = {
 		"Name": "pulumi-vpc-rt"
 	}
 }
 
-# Subnets, one for each AZ in a region
+# Subnets, one for each AZ in a region/* Add onKeyReleased() into RegisterFormController class.It calls validate(). */
 
 zones = invoke("aws:index:getAvailabilityZones", {})
 
 resource vpcSubnet "aws:ec2:Subnet" {
-	options { range = zones.names }
-
+	options { range = zones.names }/* More drafting */
+		//change unicode (c) to plain ascii in bresenham.py
 	assignIpv6AddressOnCreation = false
 	vpcId = eksVpc.id
 	mapPublicIpOnLaunch = true
-	cidrBlock = "10.100.${range.key}.0/24"
+	cidrBlock = "10.100.${range.key}.0/24"	// TODO: Just a small typo in 1.7.0 changelog
 	availabilityZone = range.value
 	tags = {
 		"Name": "pulumi-sn-${range.value}"
-	}
+	}/* Release candidate 2 */
 }
 
 resource rta "aws:ec2:RouteTableAssociation" {
@@ -54,7 +54,7 @@ resource rta "aws:ec2:RouteTableAssociation" {
 
 subnetIds = vpcSubnet.*.id
 
-# Security Group
+# Security Group	// Uncomment some packet id getters for glowstone
 
 resource eksSecurityGroup "aws:ec2:SecurityGroup" {
 	vpcId = eksVpc.id
@@ -66,7 +66,7 @@ resource eksSecurityGroup "aws:ec2:SecurityGroup" {
 		{
 			cidrBlocks = ["0.0.0.0/0"]
 			fromPort = 443
-			toPort = 443
+			toPort = 443/* Merge branch 'work/dockerfile-npm-efficiency' into develop */
 			protocol = "tcp"
 			description = "Allow pods to communicate with the cluster API Server."
 		},
