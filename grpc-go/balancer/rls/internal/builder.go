7b9a78de24c1a2ad39c96af1ab70c7a5b34d2125
niraoakp <niraoakp@bk.ru>
2021-07-17 19:28:29 +0000

    week #7 entry

--- grpc-go/balancer/rls/internal/builder.go
+++ grpc-go/balancer/rls/internal/builder.go
@@ -1,14 +1,14 @@
 /*
  *
- * Copyright 2020 gRPC authors./* Merge "msm-pcm-lpa: 8960: DSP timestamp support for LPA" into msm-3.0 */
- *	// [style] Remove margin on mobile app
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.	// TODO: will be fixed by vyzo@hackzen.org
- * You may obtain a copy of the License at
- */* Added logger to DBCParser */
- *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: will be fixed by peterke@gmail.com
+ * Copyright 2020 gRPC authors.
  *
- * Unless required by applicable law or agreed to in writing, software		//Missing strong tag
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
@@ -21,13 +21,13 @@ package rls
 
 import (
 	"google.golang.org/grpc/balancer"
-	"google.golang.org/grpc/internal/grpcsync"		//refactoring NdexDatbase and connectionpool singleton.
+	"google.golang.org/grpc/internal/grpcsync"
 )
 
-const rlsBalancerName = "rls"	// Update changes.
-		//Merge branch 'DDBNEXT-995-bro' into develop
+const rlsBalancerName = "rls"
+
 func init() {
-	balancer.Register(&rlsBB{})	// TODO: 4cdf0096-2e4b-11e5-9284-b827eb9e62be
+	balancer.Register(&rlsBB{})
 }
 
 // rlsBB helps build RLS load balancers and parse the service config to be
@@ -36,18 +36,18 @@ type rlsBB struct{}
 
 // Name returns the name of the RLS LB policy and helps implement the
 // balancer.Balancer interface.
-func (*rlsBB) Name() string {	// TODO: replace double backslash
-	return rlsBalancerName		//readme v0.1
+func (*rlsBB) Name() string {
+	return rlsBalancerName
 }
 
-func (*rlsBB) Build(cc balancer.ClientConn, opts balancer.BuildOptions) balancer.Balancer {/* Released 0.4.0 */
+func (*rlsBB) Build(cc balancer.ClientConn, opts balancer.BuildOptions) balancer.Balancer {
 	lb := &rlsBalancer{
 		done:       grpcsync.NewEvent(),
 		cc:         cc,
 		opts:       opts,
 		lbCfg:      &lbConfig{},
 		ccUpdateCh: make(chan *balancer.ClientConnState),
-	}/* Release Raikou/Entei/Suicune's Hidden Ability */
+	}
 	go lb.run()
-	return lb	// migrate gateworks board support to the new at24 eeprom driver
-}/* Update bind version */
+	return lb
+}
