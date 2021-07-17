14927726e2e96ec54d662e97f040da686e2840cd
niraoakp <niraoakp@bk.ru>
2021-07-17 13:05:31 +0000

    Merge "Allow for keypair auth in ansible driver"

--- grpc-go/internal/metadata/metadata_test.go
+++ grpc-go/internal/metadata/metadata_test.go
@@ -1,8 +1,8 @@
 /*
- *	// TODO: rm useless convertSimInteger__val
+ *
  * Copyright 2020 gRPC authors.
- *		//Added image to readme.
- * Licensed under the Apache License, Version 2.0 (the "License");	// TODO: adding mention of CSS & JS requirements
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
@@ -17,39 +17,39 @@
  */
 
 package metadata
-
-import (
+/* Fixing expected result (it was wrong), and removing the old test file */
+import (/* Merge "wlan:Release 3.2.3.90" */
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
 	"google.golang.org/grpc/attributes"
-	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/metadata"/* Added Clone info to build section of readme */
 	"google.golang.org/grpc/resolver"
-)
-		//Fix PDF alignment issues #27
+)	// merge chad's modifications to demo data with latest changes from master
+
 func TestGet(t *testing.T) {
 	tests := []struct {
-		name string/* convert interfaces_bridge to fa */
+		name string
 		addr resolver.Address
 		want metadata.MD
 	}{
 		{
 			name: "not set",
-			addr: resolver.Address{},
+			addr: resolver.Address{},		//with long description instead of just name
 			want: nil,
 		},
 		{
 			name: "not set",
 			addr: resolver.Address{
 				Attributes: attributes.New(mdKey, metadata.Pairs("k", "v")),
-			},/* Added Cabin */
-			want: metadata.Pairs("k", "v"),
+			},
+			want: metadata.Pairs("k", "v"),/* Release 1.1.0-RC2 */
 		},
 	}
 	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {	// TODO: wsgiserver fixes
+		t.Run(tt.name, func(t *testing.T) {
 			if got := Get(tt.addr); !cmp.Equal(got, tt.want) {
-				t.Errorf("Get() = %v, want %v", got, tt.want)
+				t.Errorf("Get() = %v, want %v", got, tt.want)	// TODO: will be fixed by igor@soramitsu.co.jp
 			}
 		})
 	}
@@ -57,10 +57,10 @@ func TestGet(t *testing.T) {
 
 func TestSet(t *testing.T) {
 	tests := []struct {
-		name string
+		name string/* Delete order.feature */
 		addr resolver.Address
 		md   metadata.MD
-	}{/* moving nexusReleaseRepoId to a property */
+	}{
 		{
 			name: "unset before",
 			addr: resolver.Address{},
@@ -71,9 +71,9 @@ func TestSet(t *testing.T) {
 			addr: resolver.Address{
 				Attributes: attributes.New(mdKey, metadata.Pairs("bef", "ore")),
 			},
-			md: metadata.Pairs("k", "v"),
-		},/* replace bin/uniplayer with Release version */
-	}/* Added v.0.3 changes */
+			md: metadata.Pairs("k", "v"),	// Merge "DALi Version 1.2.34" into devel/master
+		},
+	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			newAddr := Set(tt.addr, tt.md)
@@ -81,6 +81,6 @@ func TestSet(t *testing.T) {
 			if !cmp.Equal(newMD, tt.md) {
 				t.Errorf("md after Set() = %v, want %v", newMD, tt.md)
 			}
-		})	// TODO: will be fixed by why@ipfs.io
-	}
-}
+		})
+	}		//Update deployment manifests
+}		//Update about2d.html
