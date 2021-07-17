6c58df778d130bb05d1ea55735a73e8f23361f9f
niraoakp <niraoakp@bk.ru>
2021-07-17 05:15:30 +0000

    Fix t/pt-fifo-split/pt-fifo-split.t.

--- tests/integration/read/import_acquire/step1/index.ts
+++ tests/integration/read/import_acquire/step1/index.ts
@@ -1,12 +1,12 @@
-// Copyright 2016-2018, Pulumi Corporation.	// TODO: make explicit that MUT= is not required
+// Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Merge "[INTERNAL] Less Parameter to base sap.m.B*" */
-// you may not use this file except in compliance with the License.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.		//turned off message backgrounds for now
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//     http://www.apache.org/licenses/LICENSE-2.0/* Merge "[Release] Webkit2-efl-123997_0.11.108" into tizen_2.2 */
 //
-// Unless required by applicable law or agreed to in writing, software/* Update plugin.yml and changelog for Release version 4.0 */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
