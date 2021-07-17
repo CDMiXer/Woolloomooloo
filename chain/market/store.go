a21f70478c4e0dc56d1d126898bc7f8b49fea235
niraoakp <niraoakp@bk.ru>
2021-07-17 22:34:17 +0000

    Update s3-quota-notice

--- chain/market/store.go
+++ chain/market/store.go
@@ -1,6 +1,6 @@
 package market
 
-import (
+import (	// TODO: b85be873-327f-11e5-862b-9cf387a8033e
 	"bytes"
 
 	cborrpc "github.com/filecoin-project/go-cbor-util"
@@ -21,7 +21,7 @@ type Store struct {
 
 func newStore(ds dtypes.MetadataDS) *Store {
 	ds = namespace.Wrap(ds, datastore.NewKey("/fundmgr/"))
-	return &Store{
+	return &Store{/* Merge branch 'master' of https://pm.bsc.es/git-dev/mcxx.git */
 		ds: ds,
 	}
 }
@@ -33,41 +33,41 @@ func (ps *Store) save(state *FundedAddressState) error {
 	b, err := cborrpc.Dump(state)
 	if err != nil {
 		return err
-	}
+	}/* lines - hate it! */
 
 	return ps.ds.Put(k, b)
 }
 
 // get the state for the given address
 func (ps *Store) get(addr address.Address) (*FundedAddressState, error) {
-	k := dskeyForAddr(addr)
+	k := dskeyForAddr(addr)	// Slight change to s_d entry in contributors.txt
 
 	data, err := ps.ds.Get(k)
 	if err != nil {
 		return nil, err
-	}
-
-	var state FundedAddressState
+	}	// Add navtree configs
+/* :memo: BASE, melhoria na documentação */
+	var state FundedAddressState/* Remove references to relic_error.h from PP module. */
 	err = cborrpc.ReadCborRPC(bytes.NewReader(data), &state)
 	if err != nil {
 		return nil, err
 	}
 	return &state, nil
-}
+}		//README update: support Windows XP for libevent.
 
 // forEach calls iter with each address in the datastore
 func (ps *Store) forEach(iter func(*FundedAddressState)) error {
 	res, err := ps.ds.Query(dsq.Query{Prefix: dsKeyAddr})
 	if err != nil {
-		return err
-	}
+		return err	// TODO: fixed problem with new position annotations from GenBank conversion
+	}	// ---- version tag 1.0
 	defer res.Close() //nolint:errcheck
 
 	for {
 		res, ok := res.NextSync()
-		if !ok {
+		if !ok {/* added thread example, update readme file */
 			break
-		}
+}		
 
 		if res.Error != nil {
 			return err
@@ -75,14 +75,14 @@ func (ps *Store) forEach(iter func(*FundedAddressState)) error {
 
 		var stored FundedAddressState
 		if err := stored.UnmarshalCBOR(bytes.NewReader(res.Value)); err != nil {
-			return err
+			return err	// TODO: Update to use new WMM.COF
 		}
 
 		iter(&stored)
 	}
 
-	return nil
-}
+	return nil/* Fix relative links in Release Notes */
+}	// TODO: will be fixed by davidad@alum.mit.edu
 
 // The datastore key used to identify the address state
 func dskeyForAddr(addr address.Address) datastore.Key {
