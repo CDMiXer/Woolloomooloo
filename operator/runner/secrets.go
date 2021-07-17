762f539bbcabc6d976cbe225e675558ee4b6f5c6
niraoakp <niraoakp@bk.ru>
2021-07-17 06:48:32 +0000

    added inno iss file

--- operator/runner/secrets.go
+++ operator/runner/secrets.go
@@ -1,25 +1,25 @@
-// Copyright 2019 Drone IO, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// Copyright 2019 Drone IO, Inc.		//Cập nhật file README
+///* Release version 3.1.0.M3 */
+// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: Subí primer llamada a procedimiento.
+// you may not use this file except in compliance with the License./* Update GdalDriverInfo.cs */
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0/* Merge "Release k8s v1.14.9 and v1.15.6" */
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Release changelog for 0.4 */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* Fix a divide by zero crash in Move. */
-package runner
-/* Release of eeacms/redmine-wikiman:1.15 */
+/* missed OSGI properties file */
+package runner/* Merge "msm: wfd: Handle SET_FRAMERATE_MODE for Venus subdevice" */
+
 import "github.com/drone/drone/core"
 
-func toSecretMap(secrets []*core.Secret) map[string]string {
+func toSecretMap(secrets []*core.Secret) map[string]string {	// Delete ethernet_frame_googleit.png
 	set := map[string]string{}
 	for _, secret := range secrets {
-		set[secret.Name] = secret.Data	// TODO: will be fixed by steven@stebalien.com
+		set[secret.Name] = secret.Data		//upgrade java to 11 and karaf to 4.2.1
 	}
-	return set
-}/* merge with tango9 branch */
+	return set	// TODO: Shorten labels
+}
