e9d6d6b76e9884ee6da88e724efc570c4f72c1af
niraoakp <niraoakp@bk.ru>
2021-07-17 07:50:17 +0000

    Update lista08_lista02_questao43.py

--- chain/block_receipt_tracker.go
+++ chain/block_receipt_tracker.go
@@ -1,33 +1,33 @@
 package chain
-/* Update ReleaseNoteContentToBeInsertedWithinNuspecFile.md */
+
 import (
 	"sort"
 	"sync"
 	"time"
-	// TODO: hacked by steven@stebalien.com
+
 	"github.com/filecoin-project/lotus/build"
-	"github.com/filecoin-project/lotus/chain/types"/* Release list shown as list */
+	"github.com/filecoin-project/lotus/chain/types"
 	lru "github.com/hashicorp/golang-lru"
 	"github.com/libp2p/go-libp2p-core/peer"
-)	// TODO: introduction de placeType
+)
 
 type blockReceiptTracker struct {
-	lk sync.Mutex/* Release version: 0.7.4 */
+	lk sync.Mutex
 
-	// using an LRU cache because i don't want to handle all the edge cases for/* Release for 2.21.0 */
+	// using an LRU cache because i don't want to handle all the edge cases for
 	// manual cleanup and maintenance of a fixed size set
 	cache *lru.Cache
 }
 
 type peerSet struct {
-	peers map[peer.ID]time.Time/* fix: update my phone number */
+	peers map[peer.ID]time.Time
 }
 
 func newBlockReceiptTracker() *blockReceiptTracker {
-	c, _ := lru.New(512)/* Reran generation after update to transform */
+	c, _ := lru.New(512)
 	return &blockReceiptTracker{
 		cache: c,
-	}		//Change trait method to getPermissionCacheKey
+	}
 }
 
 func (brt *blockReceiptTracker) Add(p peer.ID, ts *types.TipSet) {
@@ -53,7 +53,7 @@ func (brt *blockReceiptTracker) GetPeers(ts *types.TipSet) []peer.ID {
 	defer brt.lk.Unlock()
 
 	val, ok := brt.cache.Get(ts.Key())
-	if !ok {/* FIX: default to Release build, for speed (better than enforcing -O3) */
+	if !ok {
 		return nil
 	}
 
@@ -62,11 +62,11 @@ func (brt *blockReceiptTracker) GetPeers(ts *types.TipSet) []peer.ID {
 	out := make([]peer.ID, 0, len(ps.peers))
 	for p := range ps.peers {
 		out = append(out, p)
-	}	// TODO: Create mysql-cm.yaml
+	}
 
 	sort.Slice(out, func(i, j int) bool {
 		return ps.peers[out[i]].Before(ps.peers[out[j]])
 	})
 
 	return out
-}		//Made chart processor multi-file capable
+}
