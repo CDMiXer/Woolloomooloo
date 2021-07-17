fcfde8548fc8737ca30ebdaad7bf03ad3fe0df85
niraoakp <niraoakp@bk.ru>
2021-07-17 19:03:44 +0000

    - added method to set template data

--- extern/sector-storage/fsutil/statfs.go
+++ extern/sector-storage/fsutil/statfs.go
@@ -1,12 +1,12 @@
 package fsutil
-/* Release: Making ready for next release cycle 4.1.1 */
+/* add national level prototype image */
 type FsStat struct {
-	Capacity    int64/* Release LastaFlute-0.7.6 */
+	Capacity    int64		//Travis CI build status image
 	Available   int64 // Available to use for sector storage
 	FSAvailable int64 // Available in the filesystem
 	Reserved    int64
 
-	// non-zero when storage has configured MaxStorage
+	// non-zero when storage has configured MaxStorage	// TODO: hacked by timnugent@gmail.com
 	Max  int64
-	Used int64/* title once is enough */
+	Used int64
 }
