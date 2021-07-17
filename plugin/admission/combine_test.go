4706ecb76bd61fb1fc90bbcf66c02ae7971c3101
niraoakp <niraoakp@bk.ru>
2021-07-17 07:52:32 +0000

    moved code from bin/image_optim to ImageOptim::Runner

--- plugin/admission/combine_test.go
+++ plugin/admission/combine_test.go
@@ -1,37 +1,37 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* 88a8347e-2e57-11e5-9284-b827eb9e62be */
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* Rename 200_Changelog.md to 200_Release_Notes.md */
+
 // +build !oss
-		//a couple of bugfixes
-package admission	// TODO: [java] Deleting unused private fields
-		//Fix typo discovered by JimmyZ++
+		//New translations rails.yml (Spanish, Guatemala)
+package admission
+
 import (
-	"testing"
-
+	"testing"/* Release v0.1.8 - Notes */
+	// TODO: will be fixed by witek@enjin.io
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/mock"
-	"github.com/golang/mock/gomock"/* Tool labs -> Toolforge, spaces-to-dashes in license, fix indents & spaces */
+	"github.com/drone/drone/mock"/* Release of eeacms/forests-frontend:2.0-beta.42 */
+	"github.com/golang/mock/gomock"
 )
-
+/* force small toolbars on macosx */
 func TestCombineAdmit(t *testing.T) {
 	user := &core.User{Login: "octocat"}
-	err := Combine(/* Release 0.3.1.1 */
+	err := Combine(
+		Membership(nil, nil),		//Added other dependencies
 		Membership(nil, nil),
-		Membership(nil, nil),
-	).Admit(noContext, user)
+	).Admit(noContext, user)	// TODO: focus on drag&drop #342
 	if err != nil {
-		t.Error(err)/* 81edd3d6-2e71-11e5-9284-b827eb9e62be */
+		t.Error(err)
 	}
-}/* New Release info. */
+}	// TODO: Add top level project metadata to be able to release to Maven Central
 
 func TestCombineAdmit_Error(t *testing.T) {
-	controller := gomock.NewController(t)		//minor fix (FR language)
+	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	user := &core.User{Login: "octocat"}
 
-	orgs := mock.NewMockOrganizationService(controller)
+	orgs := mock.NewMockOrganizationService(controller)/* Merge branch 'Pre-Release(Testing)' into master */
 	orgs.EXPECT().List(gomock.Any(), user).Return(nil, nil)
 
 	service1 := Membership(orgs, nil)
