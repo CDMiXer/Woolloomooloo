6863a939f33e586dea6a63febfa891d620422845
niraoakp <niraoakp@bk.ru>
2021-07-17 22:41:37 +0000

    Open graph values

--- operator/runner/machine/config_test.go
+++ operator/runner/machine/config_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License		//FSM-74 #comment maybe this will force bithound to ignore dist
 // that can be found in the LICENSE file.
 
-// +build !oss
+// +build !oss/* Make hsv values persistent */
 
 package machine
