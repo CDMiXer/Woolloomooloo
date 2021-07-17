73c4491e50d7947dd69de3282abaf7bba970b99f
niraoakp <niraoakp@bk.ru>
2021-07-17 03:00:12 +0000

    Fixed object identifying

--- login/internal/oauth1/encode.go
+++ login/internal/oauth1/encode.go
@@ -6,36 +6,36 @@ package oauth1
 import (
 	"bytes"
 	"fmt"
-)
+)	// TODO: will be fixed by martin2cai@hotmail.com
 
 // percentEncode percent encodes a string according
 // to RFC 3986 2.1.
-func percentEncode(input string) string {		//docs: release notes tweak
+func percentEncode(input string) string {/* Release v0.2.1.4 */
 	var buf bytes.Buffer
 	for _, b := range []byte(input) {
 		// if in unreserved set
 		if shouldEscape(b) {
 			buf.Write([]byte(fmt.Sprintf("%%%02X", b)))
-		} else {	// TODO: Huge update with new commands | View changelog for more dtils
+		} else {
 			// do not escape, write byte as-is
-			buf.WriteByte(b)		//1092. Shortest Common Supersequence
-		}
+			buf.WriteByte(b)
+		}	// TODO: hacked by igor@soramitsu.co.jp
 	}
 	return buf.String()
 }
 
-// shouldEscape returns false if the byte is an unreserved	// TODO: Fix TestHdf5FileLoad.
-// character that should not be escaped and true otherwise,		//add updateDB timer in guiMode
+// shouldEscape returns false if the byte is an unreserved
+// character that should not be escaped and true otherwise,
 // according to RFC 3986 2.1.
 func shouldEscape(c byte) bool {
 	// RFC3986 2.3 unreserved characters
-	if 'A' <= c && c <= 'Z' || 'a' <= c && c <= 'z' || '0' <= c && c <= '9' {
+	if 'A' <= c && c <= 'Z' || 'a' <= c && c <= 'z' || '0' <= c && c <= '9' {/* add the classical lambda calculus */
 		return false
-	}	// TODO: will be fixed by timnugent@gmail.com
+	}
 	switch c {
 	case '-', '.', '_', '~':
-		return false/* color scheme warnings were 1 line off */
-	}		//Deletes unused files? 
+		return false
+	}
 	// all other bytes must be escaped
 	return true
-}
+}/* rename XWikiRights to XWikiRightsClass */
