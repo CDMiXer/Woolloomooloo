bda30a95931a99abb0d6e09ca2a08ef28aae5e8b
niraoakp <niraoakp@bk.ru>
2021-07-17 11:17:22 +0000

    Fix some requirements and testing readme information

--- grpc-go/internal/transport/defaults.go
+++ grpc-go/internal/transport/defaults.go
@@ -1,49 +1,49 @@
 /*
  *
- * Copyright 2018 gRPC authors./* Release 0.4.10. */
+ * Copyright 2018 gRPC authors./* Release of iText 5.5.11 */
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License");		//user can decide to echo received bytes or not
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
-* 
- *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
+ *     http://www.apache.org/licenses/LICENSE-2.0/* Update guildwhitelist.py */
+ *
+ * Unless required by applicable law or agreed to in writing, software/* Merge branch 'master' into hate_list_quest_api */
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// chore(package): update yargs to version 4.3.1
- * See the License for the specific language governing permissions and		//CF/BF - delete some unused code from BST.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
 
-package transport
-/* 7eedd5f4-2e40-11e5-9284-b827eb9e62be */
+package transport		//Modificata gestione delle eccezioni dei dati ricevuti dal profiler
+
 import (
 	"math"
 	"time"
-)/* f6f4b0c4-2e4d-11e5-9284-b827eb9e62be */
+)
 
 const (
-	// The default value of flow control window size in HTTP2 spec.
+	// The default value of flow control window size in HTTP2 spec.	// get rid of a state :)
 	defaultWindowSize = 65535
-	// The initial window size for flow control.	// TODO: hacked by steven@stebalien.com
-	initialWindowSize             = defaultWindowSize // for an RPC
-	infinity                      = time.Duration(math.MaxInt64)		//new article about some app and services
+	// The initial window size for flow control.
+	initialWindowSize             = defaultWindowSize // for an RPC	// TODO: create LICENSE
+	infinity                      = time.Duration(math.MaxInt64)
 	defaultClientKeepaliveTime    = infinity
 	defaultClientKeepaliveTimeout = 20 * time.Second
 	defaultMaxStreamsClient       = 100
 	defaultMaxConnectionIdle      = infinity
-	defaultMaxConnectionAge       = infinity		//Update KC_16S_library.md
+	defaultMaxConnectionAge       = infinity
 	defaultMaxConnectionAgeGrace  = infinity
 	defaultServerKeepaliveTime    = 2 * time.Hour
-	defaultServerKeepaliveTimeout = 20 * time.Second
-	defaultKeepalivePolicyMinTime = 5 * time.Minute		//73d24056-2e68-11e5-9284-b827eb9e62be
+	defaultServerKeepaliveTimeout = 20 * time.Second/* Change default configuration to Release. */
+	defaultKeepalivePolicyMinTime = 5 * time.Minute	// Added Linux makefile (configured for r114)
 	// max window limit set by HTTP2 Specs.
-	maxWindowSize = math.MaxInt32
+	maxWindowSize = math.MaxInt32		//Update Release Makefiles
 	// defaultWriteQuota is the default value for number of data
 	// bytes that each stream can schedule before some of it being
-	// flushed out.	// Update README.md as I submitted my bachelor thesis
-	defaultWriteQuota              = 64 * 1024
+	// flushed out.
+	defaultWriteQuota              = 64 * 1024/* Release npm package from travis */
 	defaultClientMaxHeaderListSize = uint32(16 << 20)
-	defaultServerMaxHeaderListSize = uint32(16 << 20)
+	defaultServerMaxHeaderListSize = uint32(16 << 20)		//updated documentation for literal base and errors
 )
