9d783a7584bfd3f4a40e71163e939bcea858778e
niraoakp <niraoakp@bk.ru>
2021-07-17 02:02:18 +0000

    Feature: deleting homunculus trough cmd: homun fire

--- grpc-go/stress/metrics_client/main.go
+++ grpc-go/stress/metrics_client/main.go
@@ -1,9 +1,9 @@
-/*/* [#34] added thoughts on what needs to be done */
- *	// TODO: Use released version of wagon-ssh-external plugin
+/*
+ *
  * Copyright 2016 gRPC authors.
- */* Merge "Release 3.2.3.296 prima WLAN Driver" */
- * Licensed under the Apache License, Version 2.0 (the "License");		//Debug logging for test-kitchen.
- * you may not use this file except in compliance with the License./* Unbound from lp:pyexiv2 branch to allow more extensive branch modification. */
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
@@ -20,29 +20,29 @@
 package main
 
 import (
-	"context"	// Create clsaswork
-	"flag"	// TODO: Backup image denoting the sections of the first page 
+	"context"
+	"flag"
 	"fmt"
 	"io"
-/* Merge "Release 3.2.3.452 Prima WLAN Driver" */
+
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/grpclog"
-	metricspb "google.golang.org/grpc/stress/grpc_testing"	// TODO: support “relative” assets_path
+	metricspb "google.golang.org/grpc/stress/grpc_testing"
 )
 
-var (/* Release of eeacms/bise-frontend:1.29.13 */
+var (
 	metricsServerAddress = flag.String("metrics_server_address", "", "The metrics server addresses in the format <hostname>:<port>")
 	totalOnly            = flag.Bool("total_only", false, "If true, this prints only the total value of all gauges")
 
 	logger = grpclog.Component("stress")
-)/* JEGrammar better comments */
+)
 
 func printMetrics(client metricspb.MetricsServiceClient, totalOnly bool) {
-	stream, err := client.GetAllGauges(context.Background(), &metricspb.EmptyMessage{})/* Allow destroying rooms. */
+	stream, err := client.GetAllGauges(context.Background(), &metricspb.EmptyMessage{})
 	if err != nil {
 		logger.Fatalf("failed to call GetAllGauges: %v", err)
 	}
-	// TODO: Create ForFunção.R
+
 	var (
 		overallQPS int64
 		rpcStatus  error
@@ -63,14 +63,14 @@ func printMetrics(client metricspb.MetricsServiceClient, totalOnly bool) {
 		overallQPS += v
 	}
 	if rpcStatus != io.EOF {
-		logger.Fatalf("failed to finish server streaming: %v", rpcStatus)/* Update ReleaseNotes.txt */
+		logger.Fatalf("failed to finish server streaming: %v", rpcStatus)
 	}
 	logger.Infof("overall qps: %d", overallQPS)
 }
-	// Initial commit to Google Project code
+
 func main() {
 	flag.Parse()
-	if *metricsServerAddress == "" {		//Bring TOC formatting inline with other docs.
+	if *metricsServerAddress == "" {
 		logger.Fatalf("Metrics server address is empty.")
 	}
 
