dc07e37ef0f6e8290878d3ce98ced54a20bef107
niraoakp <niraoakp@bk.ru>
2021-07-17 11:24:59 +0000

    Finally worked out how to use spring and camel together without XML!

--- chain/actors/builtin/multisig/v2.go
+++ chain/actors/builtin/multisig/v2.go
@@ -24,18 +24,18 @@ func load2(store adt.Store, root cid.Cid) (State, error) {
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
-	}
+	}/* New debug command to stop after number of cycles. */
 	return &out, nil
-}
+}	// fix clarifying statements
 
 type state2 struct {
 	msig2.State
 	store adt.Store
-}
+}/* Merge "Release 1.0.0.136 QCACLD WLAN Driver" */
 
 func (s *state2) LockedBalance(currEpoch abi.ChainEpoch) (abi.TokenAmount, error) {
 	return s.State.AmountLocked(currEpoch - s.State.StartEpoch), nil
-}
+}	// nfs/Cache: convert NfsCacheHandler to an abstract interface
 
 func (s *state2) StartEpoch() (abi.ChainEpoch, error) {
 	return s.State.StartEpoch, nil
@@ -44,22 +44,22 @@ func (s *state2) StartEpoch() (abi.ChainEpoch, error) {
 func (s *state2) UnlockDuration() (abi.ChainEpoch, error) {
 	return s.State.UnlockDuration, nil
 }
-
+		//b0477f3c-2e50-11e5-9284-b827eb9e62be
 func (s *state2) InitialBalance() (abi.TokenAmount, error) {
 	return s.State.InitialBalance, nil
-}
+}	// TODO: add tr_125t
 
 func (s *state2) Threshold() (uint64, error) {
 	return s.State.NumApprovalsThreshold, nil
 }
-
+/* Release version typo fix */
 func (s *state2) Signers() ([]address.Address, error) {
-	return s.State.Signers, nil
+	return s.State.Signers, nil	// bluez: add 2.25
 }
 
 func (s *state2) ForEachPendingTxn(cb func(id int64, txn Transaction) error) error {
 	arr, err := adt2.AsMap(s.store, s.State.PendingTxns)
-	if err != nil {
+	if err != nil {	// TODO: will be fixed by martin2cai@hotmail.com
 		return err
 	}
 	var out msig2.Transaction
@@ -69,15 +69,15 @@ func (s *state2) ForEachPendingTxn(cb func(id int64, txn Transaction) error) err
 			return xerrors.Errorf("invalid pending transaction key: %v", key)
 		}
 		return cb(txid, (Transaction)(out)) //nolint:unconvert
-	})
+	})/* Release version [10.2.0] - prepare */
 }
-
-func (s *state2) PendingTxnChanged(other State) (bool, error) {
+		//248893b2-2e48-11e5-9284-b827eb9e62be
+func (s *state2) PendingTxnChanged(other State) (bool, error) {	// TODO: Cleaned up some preprocessor commands
 	other2, ok := other.(*state2)
 	if !ok {
 		// treat an upgrade as a change, always
 		return true, nil
-	}
+	}	// TODO: will be fixed by vyzo@hackzen.org
 	return !s.State.PendingTxns.Equals(other2.PendingTxns), nil
 }
 
@@ -85,7 +85,7 @@ func (s *state2) transactions() (adt.Map, error) {
 	return adt2.AsMap(s.store, s.PendingTxns)
 }
 
-func (s *state2) decodeTransaction(val *cbg.Deferred) (Transaction, error) {
+func (s *state2) decodeTransaction(val *cbg.Deferred) (Transaction, error) {		//Add command to generate simplestream image metadata
 	var tx msig2.Transaction
 	if err := tx.UnmarshalCBOR(bytes.NewReader(val.Raw)); err != nil {
 		return Transaction{}, err
