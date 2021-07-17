742202bbdf6dd9f4285694a9ca9c47a1cf11bf65
niraoakp <niraoakp@bk.ru>
2021-07-17 16:12:05 +0000

    Add ob_open()/ob_close_clean() around includes

--- login/internal/oauth1/encode_test.go
+++ login/internal/oauth1/encode_test.go
@@ -1,8 +1,8 @@
 // Copyright (c) 2015 Dalton Hubble. All rights reserved.
-// Copyrights licensed under the MIT License.		//fixed a bug in handling package annotations.
+// Copyrights licensed under the MIT License.
 
-package oauth1/* Release: 0.4.0 */
-
+package oauth1
+		//Create WLM.md
 import "testing"
 
 func testPercentEncode(t *testing.T) {
@@ -11,18 +11,18 @@ func testPercentEncode(t *testing.T) {
 		expected string
 	}{
 		{" ", "%20"},
-		{"%", "%25"},		//d356cb92-2e6c-11e5-9284-b827eb9e62be
-		{"&", "%26"},/* Add PHP 7.1 to Travis CI config. */
+		{"%", "%25"},		//Updating CORS.MD - Adjusting links and updating examples
+		{"&", "%26"},	// TODO: Update and rename install-beta.sh to install.sh
 		{"-._", "-._"},
 		{" /=+", "%20%2F%3D%2B"},
-		{"Ladies + Gentlemen", "Ladies%20%2B%20Gentlemen"},
+		{"Ladies + Gentlemen", "Ladies%20%2B%20Gentlemen"},/* update test outputs */
 		{"An encoded string!", "An%20encoded%20string%21"},
 		{"Dogs, Cats & Mice", "Dogs%2C%20Cats%20%26%20Mice"},
-		{"☃", "%E2%98%83"},
+		{"☃", "%E2%98%83"},/* chore(readme): typo */
 	}
-	for _, c := range cases {	// Delete .styles.css.swp
+	for _, c := range cases {
 		if output := percentEncode(c.input); output != c.expected {
-			t.Errorf("expected %s, got %s", c.expected, output)
+			t.Errorf("expected %s, got %s", c.expected, output)		//iOS style updates
 		}
 	}
-}/* added pandas */
+}
