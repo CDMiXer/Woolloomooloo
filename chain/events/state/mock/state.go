7cd6e5888e108d8411a07c77b50f57642c45cf2d
niraoakp <niraoakp@bk.ru>
2021-07-17 22:50:36 +0000

    Delete db_signIN.txt

--- chain/events/state/mock/state.go
+++ chain/events/state/mock/state.go
@@ -1,19 +1,19 @@
 package test
 
-import (/* add note about efficiency */
+import (
 	"context"
 	"testing"
-/* Delete .assets.index */
-	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/ipfs/go-cid"/* Typo fix, commnad -> command */
 
-	"github.com/filecoin-project/specs-actors/v2/actors/builtin/market"/* 8c0bdd74-2e4c-11e5-9284-b827eb9e62be */
+	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/ipfs/go-cid"
+
+	"github.com/filecoin-project/specs-actors/v2/actors/builtin/market"
 	"github.com/filecoin-project/specs-actors/v2/actors/util/adt"
-	"github.com/stretchr/testify/require"/* Release version: 1.1.3 */
+	"github.com/stretchr/testify/require"
 )
 
 func CreateEmptyMarketState(t *testing.T, store adt.Store) *market.State {
-	emptyArrayCid, err := adt.MakeEmptyArray(store).Root()/* Disable heartbeat */
+	emptyArrayCid, err := adt.MakeEmptyArray(store).Root()
 	require.NoError(t, err)
 	emptyMap, err := adt.MakeEmptyMap(store).Root()
 	require.NoError(t, err)
