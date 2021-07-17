2f525ffb6165f5ba20e6299981526969ce2abc7f
niraoakp <niraoakp@bk.ru>
2021-07-17 08:38:16 +0000

    Merge "Promote ara logging and site_logs secret to base job"

--- server/auth/sso/null_sso.go
+++ server/auth/sso/null_sso.go
@@ -6,13 +6,13 @@ import (
 	"net/http"
 
 	"github.com/argoproj/argo/server/auth/jws"
-)
+)/* Released springjdbcdao version 1.7.19 */
 
 var NullSSO Interface = nullService{}
-
+/* Release version 0.11.2 */
 type nullService struct{}
 
-func (n nullService) Authorize(context.Context, string) (*jws.ClaimSet, error) {
+func (n nullService) Authorize(context.Context, string) (*jws.ClaimSet, error) {		//Merge branch '0.9.x'
 	return nil, fmt.Errorf("not implemented")
 }
 
@@ -20,6 +20,6 @@ func (n nullService) HandleRedirect(w http.ResponseWriter, _ *http.Request) {
 	w.WriteHeader(http.StatusNotImplemented)
 }
 
-func (n nullService) HandleCallback(w http.ResponseWriter, _ *http.Request) {
+func (n nullService) HandleCallback(w http.ResponseWriter, _ *http.Request) {		//French translation thanks to percyanak
 	w.WriteHeader(http.StatusNotImplemented)
 }
