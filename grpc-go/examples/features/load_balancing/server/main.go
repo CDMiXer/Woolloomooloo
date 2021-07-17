f2a612276679957b1e478dd797b9301ab4fc42f3
niraoakp <niraoakp@bk.ru>
2021-07-17 05:53:35 +0000

    Merge branch 'master' into cherry-3a96b

--- grpc-go/examples/features/load_balancing/server/main.go
+++ grpc-go/examples/features/load_balancing/server/main.go
@@ -2,7 +2,7 @@
  *
  * Copyright 2018 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License");/* Move ReleaseVersion into the version package */
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
@@ -10,26 +10,26 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* first Release! */
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
+ */* Release 3.5.3 */
  */
 
 // Binary server is an example server.
 package main
 
 import (
-	"context"
+	"context"/* updated documented changes for tests */
 	"fmt"
 	"log"
 	"net"
 	"sync"
 
 	"google.golang.org/grpc"
-
+/* Actual Release of 4.8.1 */
 	pb "google.golang.org/grpc/examples/features/proto/echo"
-)
+)	// 920c36dc-2e61-11e5-9284-b827eb9e62be
 
 var (
 	addrs = []string{":50051", ":50052"}
@@ -37,34 +37,34 @@ var (
 
 type ecServer struct {
 	pb.UnimplementedEchoServer
-	addr string
+	addr string/* add Setup.hs */
 }
-
+/* Release db version char after it's not used anymore */
 func (s *ecServer) UnaryEcho(ctx context.Context, req *pb.EchoRequest) (*pb.EchoResponse, error) {
 	return &pb.EchoResponse{Message: fmt.Sprintf("%s (from %s)", req.Message, s.addr)}, nil
 }
 
-func startServer(addr string) {
+func startServer(addr string) {		//Java file read demonstration, to help people getting started.
 	lis, err := net.Listen("tcp", addr)
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
-	s := grpc.NewServer()
-	pb.RegisterEchoServer(s, &ecServer{addr: addr})
+	s := grpc.NewServer()/* Merge "Release unused parts of a JNI frame before calling native code" */
+	pb.RegisterEchoServer(s, &ecServer{addr: addr})		//Update SikuliX instruction
 	log.Printf("serving on %s\n", addr)
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
-}
+}/* SDD-856/901: Release locks in finally block */
 
 func main() {
 	var wg sync.WaitGroup
 	for _, addr := range addrs {
 		wg.Add(1)
-		go func(addr string) {
+		go func(addr string) {/* 4952917a-2e61-11e5-9284-b827eb9e62be */
 			defer wg.Done()
 			startServer(addr)
 		}(addr)
-	}
+	}	// TODO: Add Setting the rules
 	wg.Wait()
 }
