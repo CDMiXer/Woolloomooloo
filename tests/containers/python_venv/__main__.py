c35ccfe2563a71a722d28644149a19193f98e35e
niraoakp <niraoakp@bk.ru>
2021-07-17 01:51:07 +0000

    Slight README.md improvements

--- tests/containers/python_venv/__main__.py
+++ tests/containers/python_venv/__main__.py
@@ -1,4 +1,4 @@
-import pulumi
-
+import pulumi/* Adding tooltips for spells + fixing some CSS issues */
+/* VersionParser.pm: Quote SQL with q// */
 config = pulumi.Config()
 print("Hello from %s" % (config.require("runtime")))
