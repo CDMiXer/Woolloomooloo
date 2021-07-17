ed7d8a4a9086f13453f0fd8105c1e50103196db1
niraoakp <niraoakp@bk.ru>
2021-07-17 23:48:26 +0000

    Further improved regimes selection

--- pkg/codegen/internal/test/testdata/resource-options.pp
+++ pkg/codegen/internal/test/testdata/resource-options.pp
@@ -1,12 +1,12 @@
 resource provider "pulumi:providers:aws" {
-	region = "us-west-2"	// TODO: Automatic changelog generation for PR #56202 [ci skip]
-}		//Update php_msafe.h
+	region = "us-west-2"
+}		//syntax highlight for shorten
 
 resource bucket1 "aws:s3:Bucket" {
 	options {
-		provider = provider	// xP90M39zYvfIZ1jZo9St1xoJKUg9rafC
+		provider = provider
 		dependsOn = [provider]
-		protect = true/* Delete Pool3.png */
+		protect = true
 		ignoreChanges = [bucket, lifecycleRules[0]]
 	}
 }
