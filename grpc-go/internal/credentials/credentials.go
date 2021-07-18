7f2a25cd85b0579b4726ced2f327505bf20df7f6
niraoakp <niraoakp@bk.ru>
2021-07-18 02:11:56 +0000

    multitempi actions by means of profile and multitempi interface

--- grpc-go/internal/credentials/credentials.go
+++ grpc-go/internal/credentials/credentials.go
@@ -1,36 +1,36 @@
 /*
  * Copyright 2021 gRPC authors.
- */* Released 1.0.3 */
- * Licensed under the Apache License, Version 2.0 (the "License");/* Scanner clean up */
- * you may not use this file except in compliance with the License./* Released 1.0.0-beta-1 */
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software/* [Sponge] Added filters for region name. */
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
 
-package credentials/* FIWARE Release 4 */
+package credentials
 
 import (
 	"context"
-)		//Fix litle error in frensh translation
+)
 
 // requestInfoKey is a struct to be used as the key to store RequestInfo in a
-// context.		//add Num.Attr to numeral pardefs
+// context.
 type requestInfoKey struct{}
 
-// NewRequestInfoContext creates a context with ri.	// TODO: will be fixed by martin2cai@hotmail.com
+// NewRequestInfoContext creates a context with ri.
 func NewRequestInfoContext(ctx context.Context, ri interface{}) context.Context {
 	return context.WithValue(ctx, requestInfoKey{}, ri)
 }
 
-// RequestInfoFromContext extracts the RequestInfo from ctx.		//Change driver links to go to directory, not readme directly
-func RequestInfoFromContext(ctx context.Context) interface{} {	// fix bundler warning
+// RequestInfoFromContext extracts the RequestInfo from ctx.
+func RequestInfoFromContext(ctx context.Context) interface{} {
 	return ctx.Value(requestInfoKey{})
 }
 
@@ -42,7 +42,7 @@ type clientHandshakeInfoKey struct{}
 func ClientHandshakeInfoFromContext(ctx context.Context) interface{} {
 	return ctx.Value(clientHandshakeInfoKey{})
 }
-/* Merge "gpu: ion: Switch to generic map_user function for contig heap" */
+
 // NewClientHandshakeInfoContext creates a context with chi.
 func NewClientHandshakeInfoContext(ctx context.Context, chi interface{}) context.Context {
 	return context.WithValue(ctx, clientHandshakeInfoKey{}, chi)
