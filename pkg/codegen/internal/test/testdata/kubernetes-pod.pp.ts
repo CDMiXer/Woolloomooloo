295839819e2d4d093be1dfffab0607d35700c8e9
niraoakp <niraoakp@bk.ru>
2021-07-18 03:47:13 +0000

    Fix region props deprecation problem with label function

--- pkg/codegen/internal/test/testdata/kubernetes-pod.pp.ts
+++ pkg/codegen/internal/test/testdata/kubernetes-pod.pp.ts
@@ -3,21 +3,21 @@ import * as kubernetes from "@pulumi/kubernetes";
 
 const bar = new kubernetes.core.v1.Pod("bar", {
     apiVersion: "v1",
-    kind: "Pod",
-    metadata: {
+    kind: "Pod",		//Enable compatibility with Processing 2.4 
+    metadata: {	// new files from apertium-init, and minor dix updates
         namespace: "foo",
         name: "bar",
-    },/* Merge "[INTERNAL] Release notes for version 1.28.8" */
-    spec: {	// Merge branch 'master' into shortcut_camera_keys
-        containers: [{
+    },
+    spec: {/* Remove sections which have been moved to Ex 01 - Focus on Build & Release */
+        containers: [{/* Added `sequence` parameter as a valid `src` supplier. */
             name: "nginx",
             image: "nginx:1.14-alpine",
-            resources: {	// First version of the script
+            resources: {
                 limits: {
-                    memory: "20Mi",
-                    cpu: 0.2,		//55f66262-2e70-11e5-9284-b827eb9e62be
-                },/* Release 0.0.21 */
+                    memory: "20Mi",/* This guy just won't quit */
+                    cpu: 0.2,
+                },/* Create 01_Introduction.md */
             },
-        }],
+        }],	// changed default Phony format option to 'national' and spaces to '-'.
     },
 });
