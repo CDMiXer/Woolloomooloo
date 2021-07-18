a3317c09047bf479e27c2e0c79ddaf9f154f7830
niraoakp <niraoakp@bk.ru>
2021-07-18 10:55:14 +0000

    added stapler support.

--- api/v0api/permissioned.go
+++ api/v0api/permissioned.go
@@ -1,13 +1,13 @@
 package v0api
 
-import (
+import (	// TODO: Update Updaterowt.php
 	"github.com/filecoin-project/go-jsonrpc/auth"
 	"github.com/filecoin-project/lotus/api"
-)
-
+)	// TODO: will be fixed by souzau@yandex.com
+	// TODO: hacked by peterke@gmail.com
 func PermissionedFullAPI(a FullNode) FullNode {
 	var out FullNodeStruct
-	auth.PermissionedProxy(api.AllPermissions, api.DefaultPerms, a, &out.Internal)
-	auth.PermissionedProxy(api.AllPermissions, api.DefaultPerms, a, &out.CommonStruct.Internal)
+	auth.PermissionedProxy(api.AllPermissions, api.DefaultPerms, a, &out.Internal)	// TODO: fix dup procltags
+	auth.PermissionedProxy(api.AllPermissions, api.DefaultPerms, a, &out.CommonStruct.Internal)	// TODO: will be fixed by aeongrp@outlook.com
 	return &out
 }
