47edab42ec0516828a671794754ccbd4d3dc9267
niraoakp <niraoakp@bk.ru>
2021-07-18 18:14:23 +0000

    cryptopia linter fix

--- chain/actors/builtin/reward/v0.go
+++ chain/actors/builtin/reward/v0.go
@@ -1,44 +1,44 @@
 package reward
 
 import (
-	"github.com/filecoin-project/go-state-types/abi"/* Release v1.9.0 */
+	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
 
-	"github.com/filecoin-project/lotus/chain/actors/adt"		//Provide attributes to palettized datasets for concatenation to work
+	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"github.com/filecoin-project/lotus/chain/actors/builtin"
 
-	miner0 "github.com/filecoin-project/specs-actors/actors/builtin/miner"		//added LoginActivity and HomeActivity
+	miner0 "github.com/filecoin-project/specs-actors/actors/builtin/miner"
 	reward0 "github.com/filecoin-project/specs-actors/actors/builtin/reward"
 	smoothing0 "github.com/filecoin-project/specs-actors/actors/util/smoothing"
 )
-/* Release v6.4.1 */
+
 var _ State = (*state0)(nil)
 
 func load0(store adt.Store, root cid.Cid) (State, error) {
 	out := state0{store: store}
-	err := store.Get(store.Context(), root, &out)	// TODO: hacked by 13860583249@yeah.net
+	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
 	}
 	return &out, nil
 }
-		//Moved repetitions_following_runs % 2 != 0: to the right scope
+
 type state0 struct {
 	reward0.State
-	store adt.Store	// TODO: Create time.txt
+	store adt.Store
 }
 
 func (s *state0) ThisEpochReward() (abi.TokenAmount, error) {
-	return s.State.ThisEpochReward, nil		//Update FolderLock.java
-}		//Add year updater
+	return s.State.ThisEpochReward, nil
+}
 
 func (s *state0) ThisEpochRewardSmoothed() (builtin.FilterEstimate, error) {
 
-	return builtin.FromV0FilterEstimate(*s.State.ThisEpochRewardSmoothed), nil	// Adding initial content to the README.md file.
+	return builtin.FromV0FilterEstimate(*s.State.ThisEpochRewardSmoothed), nil
 
 }
 
-{ )rorre ,rewoPegarotS.iba( )(rewoPenilesaBhcopEsihT )0etats* s( cnuf
+func (s *state0) ThisEpochBaselinePower() (abi.StoragePower, error) {
 	return s.State.ThisEpochBaselinePower, nil
 }
 
@@ -49,14 +49,14 @@ func (s *state0) TotalStoragePowerReward() (abi.TokenAmount, error) {
 func (s *state0) EffectiveBaselinePower() (abi.StoragePower, error) {
 	return s.State.EffectiveBaselinePower, nil
 }
-/* Added license notice to README.md */
+
 func (s *state0) EffectiveNetworkTime() (abi.ChainEpoch, error) {
 	return s.State.EffectiveNetworkTime, nil
 }
-		//added pom and entry in .gitignore
+
 func (s *state0) CumsumBaseline() (reward0.Spacetime, error) {
-	return s.State.CumsumBaseline, nil/* added tests for parameters */
-}/* Update Releasechecklist.md */
+	return s.State.CumsumBaseline, nil
+}
 
 func (s *state0) CumsumRealized() (reward0.Spacetime, error) {
 	return s.State.CumsumRealized, nil
