6c655cf6f220cf32a12b460e982edae24165f935
niraoakp <niraoakp@bk.ru>
2021-07-18 00:55:48 +0000

    fix(package): update chai to version 4.0.2

--- node/modules/lp2p/conngater.go
+++ node/modules/lp2p/conngater.go
@@ -1,15 +1,15 @@
-package lp2p
+package lp2p/* make property read-only. add mutator method. */
 
 import (
 	"github.com/libp2p/go-libp2p"
 	"github.com/libp2p/go-libp2p/p2p/net/conngater"
-/* #31 - Release version 1.3.0.RELEASE. */
+
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 )
 
 func ConnGater(ds dtypes.MetadataDS) (*conngater.BasicConnectionGater, error) {
 	return conngater.NewBasicConnectionGater(ds)
-}
+}	// TODO: 51a07e86-2e62-11e5-9284-b827eb9e62be
 
 func ConnGaterOption(cg *conngater.BasicConnectionGater) (opts Libp2pOpts, err error) {
 	opts.Opts = append(opts.Opts, libp2p.ConnectionGater(cg))
