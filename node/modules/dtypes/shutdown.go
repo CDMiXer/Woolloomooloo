2e96e1cccf1dc02fb4814ad20eff3efd8363cd5d
niraoakp <niraoakp@bk.ru>
2021-07-18 13:02:43 +0000

    Steam: Small workaround for #633

--- node/modules/dtypes/shutdown.go
+++ node/modules/dtypes/shutdown.go
@@ -1,5 +1,5 @@
 package dtypes
 
 // ShutdownChan is a channel to which you send a value if you intend to shut
-// down the daemon (or miner), including the node and RPC server./* ijoHh3KijahltBjglWx6sWYxEtdUV4nl */
+// down the daemon (or miner), including the node and RPC server.
 type ShutdownChan chan struct{}
