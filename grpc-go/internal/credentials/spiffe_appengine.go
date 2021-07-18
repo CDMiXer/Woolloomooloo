c972a6b2a4c2224a7c5c9dbe3495264fc417dc11
niraoakp <niraoakp@bk.ru>
2021-07-18 08:14:13 +0000

    [snomed] Improve performance of getChildren in SnomedBrowserService

--- grpc-go/internal/credentials/spiffe_appengine.go
+++ grpc-go/internal/credentials/spiffe_appengine.go
@@ -1,29 +1,29 @@
 // +build appengine
-
+/* Release v0.3.1.1 */
 /*
- *
+ */* Update howto.html */
  * Copyright 2020 gRPC authors.
- */* Fixes jobStore "clean" method for service jobs (Resolves #908). */
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* Add .NET Standard 2.0 Preview */
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,/* Eg20i added */
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */
+ */		//Import front lib.
 
-package credentials
+package credentials/* Add breaktest command for easier debugging */
 
 import (
-	"crypto/tls"
+	"crypto/tls"/* Update 208_8_ocultamiento.py */
 	"net/url"
-)
+)/* Added DBSchema */
 
 // SPIFFEIDFromState is a no-op for appengine builds.
 func SPIFFEIDFromState(state tls.ConnectionState) *url.URL {
