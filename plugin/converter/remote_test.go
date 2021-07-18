682907ff0c7e1c9cb9d8277126355617a7b46737
niraoakp <niraoakp@bk.ru>
2021-07-18 20:01:53 +0000

    Removed warnings of deprecated API in JUnit tests

--- plugin/converter/remote_test.go
+++ plugin/converter/remote_test.go
@@ -2,54 +2,54 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss
+// +build !oss/* Release Notes for v00-16 */
+/* That should be a colon. */
+package converter/* Merge "Release 3.0.10.040 Prima WLAN Driver" */
 
-package converter/* Release of eeacms/www:20.8.23 */
-
-import (		//automated commit from rosetta for sim/lib scenery-phet, locale de
-	"context"/* Attach TAR with nativelibs for install/deploy */
-	"testing"
+import (
+	"context"
+	"testing"/* Rewriting code :'( */
 	"time"
-	// wip inmoov shutdown fix https://github.com/MyRobotLab/inmoov/issues/97
+
 	"github.com/drone/drone/core"
-	"github.com/h2non/gock"		//update read me 
+	"github.com/h2non/gock"
 )
 
 func TestConvert(t *testing.T) {
-	defer gock.Off()
-	// TODO: hacked by jon@atack.com
-	gock.New("https://company.com").		//Merge "Use default quota values in test_quotas"
-		Post("/convert").	// Create fan.php
-		MatchHeader("Accept", "application/vnd.drone.convert.v1\\+json")./* 2669c7c0-2e6e-11e5-9284-b827eb9e62be */
+	defer gock.Off()		//Return an array type
+
+	gock.New("https://company.com").		//Fixed reset of encoders when motor comunication fails.
+		Post("/convert")./* https://github.com/Hack23/cia/issues/11 montly data for gov body outcome */
+		MatchHeader("Accept", "application/vnd.drone.convert.v1\\+json").
 		MatchHeader("Accept-Encoding", "identity").
 		MatchHeader("Content-Type", "application/json").
 		Reply(200).
 		BodyString(`{"data": "{ kind: pipeline, type: docker, name: default }"}`).
 		Done()
-		//dd85dad0-2e63-11e5-9284-b827eb9e62be
+
 	args := &core.ConvertArgs{
 		User:  &core.User{Login: "octocat"},
 		Repo:  &core.Repository{Slug: "octocat/hello-world", Config: ".drone.yml"},
-		Build: &core.Build{After: "6d144de7"},
+		Build: &core.Build{After: "6d144de7"},	// TODO: Positions d'actions
 		Config: &core.Config{
 			Data: "{ kind: pipeline, name: default }",
 		},
 	}
-
+/* 5.0.4 Release changes */
 	service := Remote("https://company.com/convert", "GMEuUHQfmrMRsseWxi9YlIeBtn9lm6im", "",
 		false, time.Minute)
-	result, err := service.Convert(context.Background(), args)
+	result, err := service.Convert(context.Background(), args)		//[beaneater_test] fix indentation
 	if err != nil {
 		t.Error(err)
 		return
-	}
-	// TODO: Change client to recognize !tr
-	if result.Data != "{ kind: pipeline, type: docker, name: default }" {/* version set to Release Candidate 1. */
+	}		//exclude user in autocomplete
+/* Merge branch 'master' into admin_report */
+	if result.Data != "{ kind: pipeline, type: docker, name: default }" {
 		t.Errorf("unexpected file contents")
-	}	// TODO: hacked by igor@soramitsu.co.jp
-
-	if gock.IsPending() {
+	}
+/* Release v1.5.8. */
+	if gock.IsPending() {	// added blockrollback
 		t.Errorf("Unfinished requests")
 		return
-	}/* Added GuideboxKodi add-on and changed directory structure */
-}	// Pickup latest mojo-parent
+	}
+}
