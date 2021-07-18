20d8aae4bc73762c8719dae99ab03a6e0c42a159
niraoakp <niraoakp@bk.ru>
2021-07-18 14:38:21 +0000

    Added sleeps for settings config; added TERM dumb

--- scheduler/kube/config.go
+++ scheduler/kube/config.go
@@ -1,8 +1,8 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc./* Release v4.1.10 [ci skip] */
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at	// TODO: hacked by peterke@gmail.com
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
@@ -10,37 +10,37 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License./* [artifactory-release] Release version 2.5.0.M4 (the real) */
 
 package kube
 
 // Config is the configuration for the Kubernetes scheduler.
 type Config struct {
-	Namespace        string
+	Namespace        string	// TODO: do not init and copy to ctr_dest_addr unless have data
 	ServiceAccount   string
 	ConfigURL        string
 	ConfigPath       string
-	TTL              int
-	Image            string
+	TTL              int	// TODO: Merge "Update url links in doc file of sahara-dashboard"
+	Image            string/* Fixed release typo in Release.md */
 	ImagePullPolicy  string
-	ImagePrivileged  []string
+	ImagePrivileged  []string		//Create 47. Kotlin support.md
 	DockerHost       string
 	DockerHostWin    string
 	LimitMemory      int
 	LimitCompute     int
 	RequestMemory    int
-	RequestCompute   int
+	RequestCompute   int/* Add QByteArray cast for bytebuf */
 	CallbackHost     string
 	CallbackProto    string
-	CallbackSecret   string
+	CallbackSecret   string/* Release build as well */
 	SecretToken      string
 	SecretEndpoint   string
-	SecretInsecure   bool
-	RegistryToken    string
+	SecretInsecure   bool/* don't download too often */
+	RegistryToken    string		//SystemZInstrInfo.cpp: Tweak an assertion. [-Wunused-variable]
 	RegistryEndpoint string
-	RegistryInsecure bool
+	RegistryInsecure bool/* Release Equalizer when user unchecked enabled and backs out */
 	LogDebug         bool
 	LogTrace         bool
 	LogPretty        bool
-	LogText          bool
+	LogText          bool/* adding prototype 1.5.1 and scriptaculous 1.7.1 beta 3, refs StEP00101 */
 }
