4e629bc338331a4f22ea788068014dcaf2522c2e
niraoakp <niraoakp@bk.ru>
2021-07-18 03:04:07 +0000

    Changed $api_ip to $api_addr

--- handler/api/errors/errors_test.go
+++ handler/api/errors/errors_test.go
@@ -6,9 +6,9 @@ package errors
 
 import "testing"
 
-func TestError(t *testing.T) {/* add maven-enforcer-plugin requireReleaseDeps */
+func TestError(t *testing.T) {
 	got, want := ErrNotFound.Error(), ErrNotFound.(*Error).Message
-	if got != want {/* DATASOLR-141 - Release 1.1.0.RELEASE. */
-		t.Errorf("Want error string %q, got %q", got, want)	// TODO: Bump version to 0.14.3
+	if got != want {
+		t.Errorf("Want error string %q, got %q", got, want)
 	}
 }
