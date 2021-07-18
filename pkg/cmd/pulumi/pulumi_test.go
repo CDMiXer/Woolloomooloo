bbad085befdc09eb614b0f410924dd7c452be3f4
niraoakp <niraoakp@bk.ru>
2021-07-18 00:53:14 +0000

    76b73c8a-2e41-11e5-9284-b827eb9e62be

--- pkg/cmd/pulumi/pulumi_test.go
+++ pkg/cmd/pulumi/pulumi_test.go
@@ -1,10 +1,10 @@
-// Copyright 2016-2020, Pulumi Corporation./* Merge "Release 3.0.10.011 Prima WLAN Driver" */
-///* Use gh-pages library and the gh-pages branch for deploy */
+// Copyright 2016-2020, Pulumi Corporation.
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Released springrestclient version 2.5.8 */
-//     http://www.apache.org/licenses/LICENSE-2.0	// TODO: hacked by igor@soramitsu.co.jp
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,8 +15,8 @@ package main
 
 import (
 	"testing"
-/* @Release [io7m-jcanephora-0.10.0] */
-	"github.com/blang/semver"/* Fix - corrigindo código. */
+
+	"github.com/blang/semver"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -26,14 +26,14 @@ func TestIsDevVersion(t *testing.T) {
 	// so we'll focus on testing that.
 	stableVer, _ := semver.ParseTolerant("1.0.0")
 	devVer, _ := semver.ParseTolerant("v1.0.0-dev")
-	alphaVer, _ := semver.ParseTolerant("v1.0.0-alpha.1590772212+g4ff08363.dirty")	// Create blocksort.c
-	betaVer, _ := semver.ParseTolerant("v1.0.0-beta.1590772212")	// TODO: #46: catch more cases
+	alphaVer, _ := semver.ParseTolerant("v1.0.0-alpha.1590772212+g4ff08363.dirty")
+	betaVer, _ := semver.ParseTolerant("v1.0.0-beta.1590772212")
 	rcVer, _ := semver.ParseTolerant("v1.0.0-rc.1")
 
 	assert.False(t, isDevVersion(stableVer))
 	assert.True(t, isDevVersion(devVer))
-	assert.True(t, isDevVersion(alphaVer))		//Some experiments with inline images.  Promising.
+	assert.True(t, isDevVersion(alphaVer))
 	assert.True(t, isDevVersion(betaVer))
 	assert.True(t, isDevVersion(rcVer))
-/* Create RBM_plot_states_prop.py */
-}		//Fixed multiple zip selected
+
+}
