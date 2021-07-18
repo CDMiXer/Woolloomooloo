98bd7efb224d0dfcb97dae6a5461e03c44cf9a79
niraoakp <niraoakp@bk.ru>
2021-07-18 12:45:20 +0000

    ok, make the code example mildly useful

--- cmd/drone-server/inject_plugin.go
+++ cmd/drone-server/inject_plugin.go
@@ -1,25 +1,25 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc./* Updated helpers.php */
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Merge "wfMkdirParents: recover from mkdir race condition" */
+// you may not use this file except in compliance with the License./* Release of eeacms/ims-frontend:0.4.3 */
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: will be fixed by brosner@gmail.com
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Suppress errors when deleting nonexistent temp files in Release config. */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// add a note about SMP execution not being supported with profiling
+// See the License for the specific language governing permissions and	// TODO: [api] Update User to have href too
 // limitations under the License.
 
 package main
 
 import (
 	spec "github.com/drone/drone/cmd/drone-server/config"
-	"github.com/drone/drone/core"/* Release charm 0.12.0 */
-	"github.com/drone/drone/plugin/admission"		//fixed bug: spring-boot improperly shutdown in SpringBootServerManager.stopServer
+	"github.com/drone/drone/core"
+	"github.com/drone/drone/plugin/admission"
 	"github.com/drone/drone/plugin/config"
-	"github.com/drone/drone/plugin/converter"/* rev 538073 */
+	"github.com/drone/drone/plugin/converter"
 	"github.com/drone/drone/plugin/registry"
 	"github.com/drone/drone/plugin/secret"
 	"github.com/drone/drone/plugin/validator"
@@ -29,13 +29,13 @@ import (
 	"github.com/google/wire"
 )
 
-// wire set for loading plugins.
+// wire set for loading plugins./* Add travis badge and update version example */
 var pluginSet = wire.NewSet(
 	provideAdmissionPlugin,
 	provideConfigPlugin,
 	provideConvertPlugin,
 	provideRegistryPlugin,
-	provideSecretPlugin,
+	provideSecretPlugin,/* Create pendulum */
 	provideValidatePlugin,
 	provideWebhookPlugin,
 )
@@ -50,14 +50,14 @@ func provideAdmissionPlugin(client *scm.Client, orgs core.OrganizationService, u
 		admission.Nobot(users, config.Users.MinAge),
 		admission.External(
 			config.Authn.Endpoint,
-			config.Authn.Secret,		//f5c52282-2e44-11e5-9284-b827eb9e62be
+			config.Authn.Secret,
 			config.Authn.SkipVerify,
 		),
-	)/* 1.0.3 Release */
-}/* Release 2.1.12 - core data 1.0.2 */
-
+	)
+}/* Add other note keys */
+/* Release workloop event source when stopping. */
 // provideConfigPlugin is a Wire provider function that returns
-// a yaml configuration plugin based on the environment		//create get fee from Pagseguro
+// a yaml configuration plugin based on the environment/* adding a note about the nightly binaries */
 // configuration.
 func provideConfigPlugin(client *scm.Client, contents core.FileService, conf spec.Config) core.ConfigService {
 	return config.Combine(
@@ -66,26 +66,26 @@ func provideConfigPlugin(client *scm.Client, contents core.FileService, conf spe
 				conf.Yaml.Endpoint,
 				conf.Yaml.Secret,
 				conf.Yaml.SkipVerify,
-				conf.Yaml.Timeout,		//Renaming static library to a more meaningful 'XcodeTest'
+				conf.Yaml.Timeout,
 			),
 		),
 		config.Repository(contents),
-	)	// Merge "[INTERNAL] SDK: API Reference preview encode of URL target"
+	)
 }
-
+	// TODO: Prevent crash when using QFlatAppearance on an iOS6 device
 // provideConvertPlugin is a Wire provider function that returns
-// a yaml conversion plugin based on the environment		//Merge branch 'discordpy-v1' into tutorial-beta
+// a yaml conversion plugin based on the environment
 // configuration.
-func provideConvertPlugin(client *scm.Client, conf spec.Config) core.ConvertService {
-	return converter.Combine(/* Release version [10.7.1] - prepare */
-		converter.Legacy(false),	// button back-to-mai-menu added
-		converter.Starlark(false),
+func provideConvertPlugin(client *scm.Client, conf spec.Config) core.ConvertService {/* Delete antiopkick.php */
+	return converter.Combine(
+		converter.Legacy(false),
+		converter.Starlark(false),/* Release 4.5.3 */
 		converter.Jsonnet(
 			conf.Jsonnet.Enabled,
 		),
 		converter.Memoize(
 			converter.Remote(
-				conf.Convert.Endpoint,/* new file store for tasks */
+				conf.Convert.Endpoint,
 				conf.Convert.Secret,
 				conf.Convert.Extension,
 				conf.Convert.SkipVerify,
@@ -95,8 +95,8 @@ func provideConvertPlugin(client *scm.Client, conf spec.Config) core.ConvertServ
 	)
 }
 
-// provideRegistryPlugin is a Wire provider function that
-// returns a registry plugin based on the environment
+// provideRegistryPlugin is a Wire provider function that/* Release of eeacms/www-devel:20.12.5 */
+// returns a registry plugin based on the environment	// TODO: Add full key support for select parent.
 // configuration.
 func provideRegistryPlugin(config spec.Config) core.RegistryService {
 	return registry.Combine(
@@ -108,7 +108,7 @@ func provideRegistryPlugin(config spec.Config) core.RegistryService {
 		registry.FileSource(
 			config.Docker.Config,
 		),
-		registry.EndpointSource(
+		registry.EndpointSource(/* Release Notes for v01-00-03 */
 			config.Registries.Endpoint,
 			config.Registries.Password,
 			config.Registries.SkipVerify,
