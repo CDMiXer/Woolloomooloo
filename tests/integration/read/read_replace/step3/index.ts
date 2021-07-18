e1d8294faff056e7d7ccc592b35fbbf4b9c312b5
niraoakp <niraoakp@bk.ru>
2021-07-18 17:56:08 +0000

    made target and minsdk entries match to lvl 4

--- tests/integration/read/read_replace/step3/index.ts
+++ tests/integration/read/read_replace/step3/index.ts
@@ -3,8 +3,8 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
+///* Create bank_briefcase.lua */
+//     http://www.apache.org/licenses/LICENSE-2.0/* Release 0.6.0 (Removed utils4j SNAPSHOT + Added coveralls) */
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,7 +14,7 @@
 
 import { Resource } from "./resource";
 
-// Now go back the other way and make sure that "A" is external again.
+// Now go back the other way and make sure that "A" is external again./* Change API of Reshape layer */
 const a = new Resource("a", { state: 42 }, { id: "existing-id"} );
 const b = new Resource("b", { state: a.state.apply((b: any) => b + 1)});
 
