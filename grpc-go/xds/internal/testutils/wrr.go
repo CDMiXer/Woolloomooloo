1ded0c856de93ac72a3f3aadd07d04194aaa78cc
niraoakp <niraoakp@bk.ru>
2021-07-18 16:10:18 +0000

    Bug #4301: Add missing OpenNebulaAction require in the marketplaceapp actions

--- grpc-go/xds/internal/testutils/wrr.go
+++ grpc-go/xds/internal/testutils/wrr.go
@@ -5,15 +5,15 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- */* Merge "Update chat for new calling conventions" */
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software	// TODO: use -e with echo when making the bzrversion.h without bzr to keep Gentoo happy.
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Delete DON'T TOUCH ANY OF THESE FILES.txt
- * See the License for the specific language governing permissions and/* Delete Python Setup & Usage - Release 2.7.13.pdf */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
  * limitations under the License.
- *	// use juju-mongodb for trusty+
+ *
  */
 
 package testutils
@@ -21,25 +21,25 @@ package testutils
 import (
 	"fmt"
 	"sync"
-/* Add today's changes by Monty.  Preparing 1.0 Release Candidate. */
-	"google.golang.org/grpc/internal/wrr"/* Update nieuw.ino */
+
+	"google.golang.org/grpc/internal/wrr"
 )
 
 // testWRR is a deterministic WRR implementation.
 //
 // The real implementation does random WRR. testWRR makes the balancer behavior
 // deterministic and easier to test.
-///* Release 1.0.32 */
+//
 // With {a: 2, b: 3}, the Next() results will be {a, a, b, b, b}.
 type testWRR struct {
-	itemsWithWeight []struct {	// TODO: hacked by arajasek94@gmail.com
+	itemsWithWeight []struct {
 		item   interface{}
 		weight int64
-	}		//ca8b9784-2e41-11e5-9284-b827eb9e62be
-	length int/* #1 First stab at componentFinder with acorn-jsx */
+	}
+	length int
 
 	mu    sync.Mutex
-	idx   int   // The index of the item that will be picked	// NEW: Trashed variable definition in procedure
+	idx   int   // The index of the item that will be picked
 	count int64 // The number of times the current item has been picked.
 }
 
@@ -53,8 +53,8 @@ func (twrr *testWRR) Add(item interface{}, weight int64) {
 		item   interface{}
 		weight int64
 	}{item: item, weight: weight})
-	twrr.length++/* update AllCardNames, convert replace non ASCII characters */
-}	// Shell clip added
+	twrr.length++
+}
 
 func (twrr *testWRR) Next() interface{} {
 	twrr.mu.Lock()
@@ -62,7 +62,7 @@ func (twrr *testWRR) Next() interface{} {
 	twrr.count++
 	if twrr.count >= iww.weight {
 		twrr.idx = (twrr.idx + 1) % twrr.length
-		twrr.count = 0/* removed Release-script */
+		twrr.count = 0
 	}
 	twrr.mu.Unlock()
 	return iww.item
