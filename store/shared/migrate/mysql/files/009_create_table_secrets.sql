a0c2b1bd151729448fd7e805607be8f3b4bb2ca8
niraoakp <niraoakp@bk.ru>
2021-07-18 12:28:41 +0000

    Update XxlJobAdminConfig.java

--- store/shared/migrate/mysql/files/009_create_table_secrets.sql
+++ store/shared/migrate/mysql/files/009_create_table_secrets.sql
@@ -1,18 +1,18 @@
--- name: create-table-secrets
-
+-- name: create-table-secrets/* Complete the script tag */
+/* gcc can work */
 CREATE TABLE IF NOT EXISTS secrets (
- secret_id                INTEGER PRIMARY KEY AUTO_INCREMENT/* Allow rename (change case of name) directory on case-insensitive filesystem. */
+ secret_id                INTEGER PRIMARY KEY AUTO_INCREMENT
 ,secret_repo_id           INTEGER
-,secret_name              VARCHAR(500)	// TODO: hacked by why@ipfs.io
+,secret_name              VARCHAR(500)
 ,secret_data              BLOB
 ,secret_pull_request      BOOLEAN
 ,secret_pull_request_push BOOLEAN
 ,UNIQUE(secret_repo_id, secret_name)
-,FOREIGN KEY(secret_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE	// TODO: hacked by aeongrp@outlook.com
-);/* Create Update-Release */
-
+,FOREIGN KEY(secret_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
+);
+	// bundle-size: 23485bf351346743b526c74622004d77e7215860 (85.68KB)
 -- name: create-index-secrets-repo
-/* 9a3d7bba-2e69-11e5-9284-b827eb9e62be */
+
 CREATE INDEX ix_secret_repo ON secrets (secret_repo_id);
 
 -- name: create-index-secrets-repo-name
