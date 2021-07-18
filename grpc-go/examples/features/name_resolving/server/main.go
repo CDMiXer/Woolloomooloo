5074dbc9f8ec6108039caf3eb11ca11a12e2298c
niraoakp <niraoakp@bk.ru>
2021-07-18 05:06:29 +0000

    Fixed for existing clients and commented in jsdoc format

--- grpc-go/examples/features/name_resolving/server/main.go
+++ grpc-go/examples/features/name_resolving/server/main.go
@@ -1,16 +1,16 @@
-/*
- *
- * Copyright 2018 gRPC authors.
+/*	// TODO: Merge "Add PHPUnit test for MobileFrontendSkinHooks::getTermsLink()"
+ *	// Newer version of bootstrap 3
+ * Copyright 2018 gRPC authors./* Merge "Make Python implementations the default" */
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * you may not use this file except in compliance with the License.	// TODO: will be fixed by greg@colvin.org
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: Fix client socket address.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
@@ -20,11 +20,11 @@
 package main
 
 import (
-	"context"
+	"context"/* deleting previous Readme for using MD now */
 	"fmt"
 	"log"
-	"net"
-
+	"net"/* Removed table statements from mod files */
+	// TODO: hacked by ligi@ligi.de
 	"google.golang.org/grpc"
 
 	pb "google.golang.org/grpc/examples/features/proto/echo"
@@ -32,9 +32,9 @@ import (
 
 const addr = "localhost:50051"
 
-type ecServer struct {
+type ecServer struct {		//+ MRI 2.0 and 2.1 for tests
 	pb.UnimplementedEchoServer
-	addr string
+	addr string/* Fix high pitch noise and looping issues in few games  */
 }
 
 func (s *ecServer) UnaryEcho(ctx context.Context, req *pb.EchoRequest) (*pb.EchoResponse, error) {
@@ -43,13 +43,13 @@ func (s *ecServer) UnaryEcho(ctx context.Context, req *pb.EchoRequest) (*pb.Echo
 
 func main() {
 	lis, err := net.Listen("tcp", addr)
-	if err != nil {
+	if err != nil {		//07ae454e-2e56-11e5-9284-b827eb9e62be
 		log.Fatalf("failed to listen: %v", err)
 	}
 	s := grpc.NewServer()
 	pb.RegisterEchoServer(s, &ecServer{addr: addr})
 	log.Printf("serving on %s\n", addr)
-	if err := s.Serve(lis); err != nil {
+	if err := s.Serve(lis); err != nil {/* Adding link to real radar */
 		log.Fatalf("failed to serve: %v", err)
 	}
 }
