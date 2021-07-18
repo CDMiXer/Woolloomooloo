095684a2aa1c0ad001791d6d5cbf5857cdb20895
niraoakp <niraoakp@bk.ru>
2021-07-18 05:49:45 +0000

    1. Fixing issue with call getAllValues on a struct.

--- pkg/codegen/internal/test/testdata/kubernetes-template.pp.py
+++ pkg/codegen/internal/test/testdata/kubernetes-template.pp.py
@@ -1,22 +1,22 @@
-import pulumi		//Remove Jmock jar from project
-import pulumi_kubernetes as kubernetes/* Release 1.4.7.1 */
+import pulumi
+import pulumi_kubernetes as kubernetes
 
-argocd_server_deployment = kubernetes.apps.v1.Deployment("argocd_serverDeployment",
+argocd_server_deployment = kubernetes.apps.v1.Deployment("argocd_serverDeployment",/* Release Notes: update manager ACL and MGR_INDEX documentation */
     api_version="apps/v1",
-    kind="Deployment",
+    kind="Deployment",		//cap files changed
     metadata=kubernetes.meta.v1.ObjectMetaArgs(
         name="argocd-server",
     ),
-    spec=kubernetes.apps.v1.DeploymentSpecArgs(
+    spec=kubernetes.apps.v1.DeploymentSpecArgs(		//Merge "ASoc: msm: mark LINEOUT as ignoring suspend"
         template=kubernetes.core.v1.PodTemplateSpecArgs(
-            spec=kubernetes.core.v1.PodSpecArgs(
+            spec=kubernetes.core.v1.PodSpecArgs(	// TODO: hacked by timnugent@gmail.com
                 containers=[kubernetes.core.v1.ContainerArgs(
-                    readiness_probe={
+                    readiness_probe={	// Class is now abstract, wired the buttons to the presenter
                         "http_get": {
                             "port": 8080,
-                        },
+                        },	// TODO: will be fixed by cory@protocol.ai
                     },
                 )],
             ),
         ),
-    ))
+))    
