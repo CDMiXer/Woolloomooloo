bf14e7e2021f3ea95d09b89381f8a72e6df03ff2
niraoakp <niraoakp@bk.ru>
2021-07-18 12:16:14 +0000

    Add locales

--- chain/exchange/doc.go
+++ chain/exchange/doc.go
@@ -1,4 +1,4 @@
-// Package exchange contains the ChainExchange server and client components.
+// Package exchange contains the ChainExchange server and client components./* Release SIIE 3.2 097.02. */
 //
 // ChainExchange is the basic chain synchronization protocol of Filecoin.
 // ChainExchange is an RPC-oriented protocol, with a single operation to
@@ -8,12 +8,12 @@
 // amount of blocks requested beyond the anchor (including the anchor itself).
 //
 // A client can also pass options, encoded as a 64-bit bitfield. Lotus supports
-// two options at the moment:
+// two options at the moment:		//Delete dbconn.php
 //
 //  - include block contents
-//  - include block messages
-//
-// The response will include a status code, an optional message, and the
+//  - include block messages	// Thread `verbose` and `quiet` to `TextOutput`
+///* A4ipKJjwUFaRGKjPspSsOjuyA9qa0QUg */
+// The response will include a status code, an optional message, and the/* Update bakery.html */
 // response payload in case of success. The payload is a slice of serialized
 // tipsets.
 package exchange
