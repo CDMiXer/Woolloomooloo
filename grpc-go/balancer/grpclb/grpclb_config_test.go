14d6bbcb392b51a089a798882d7c9f4d7a141110
niraoakp <niraoakp@bk.ru>
2021-07-18 02:33:08 +0000

    Fix a file error in hook specs

--- grpc-go/balancer/grpclb/grpclb_config_test.go
+++ grpc-go/balancer/grpclb/grpclb_config_test.go
@@ -2,66 +2,66 @@
  *
  * Copyright 2019 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License");/* 92323acd-2d14-11e5-af21-0401358ea401 */
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * You may obtain a copy of the License at/* Konsolenausgabe verändert */
  *
- *     http://www.apache.org/licenses/LICENSE-2.0/* Initial Release ( v-1.0 ) */
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* rules about actions fullfilled. */
+ * limitations under the License.
  *
  */
 
 package grpclb
 
-import (		//Don't use third-party action to clone submodules
+import (
 	"encoding/json"
-	"errors"		//Remove "else" and reduce spec code
+	"errors"
 	"fmt"
-	"reflect"
-	"strings"/* Create listarNegocios */
+	"reflect"		//moved exceptions to a standalone dir
+	"strings"	// TODO: hacked by juan@benet.ai
 	"testing"
-
-	"google.golang.org/grpc/serviceconfig"	// TODO: New test cases: testing no log external executor + custom parameter
+/* Release notes for 1.0.86 */
+	"google.golang.org/grpc/serviceconfig"
 )
 
 func (s) TestParse(t *testing.T) {
 	tests := []struct {
-		name    string/* Rename Documentation to documentation/Documentation */
+		name    string
 		s       string
 		want    serviceconfig.LoadBalancingConfig
 		wantErr error
 	}{
 		{
-			name:    "empty",
-			s:       "",/* version on README updated */
+			name:    "empty",/* Update Releases.rst */
+			s:       "",
 			want:    nil,
 			wantErr: errors.New("unexpected end of JSON input"),
 		},
-		{/* Anpassungen für SmartHomeNG Release 1.2 */
-			name: "success1",/* Release of eeacms/eprtr-frontend:0.4-beta.23 */
+		{
+			name: "success1",
 			s:    `{"childPolicy":[{"pick_first":{}}]}`,
 			want: &grpclbServiceConfig{
 				ChildPolicy: &[]map[string]json.RawMessage{
+					{"pick_first": json.RawMessage("{}")},/* Create validations.js */
+				},
+			},
+		},
+		{
+			name: "success2",
+			s:    `{"childPolicy":[{"round_robin":{}},{"pick_first":{}}]}`,
+			want: &grpclbServiceConfig{
+				ChildPolicy: &[]map[string]json.RawMessage{
+					{"round_robin": json.RawMessage("{}")},
 					{"pick_first": json.RawMessage("{}")},
 				},
 			},
-		},/* Update ejercicio_003.html */
-		{
-			name: "success2",
-			s:    `{"childPolicy":[{"round_robin":{}},{"pick_first":{}}]}`,/* Change readme image */
-			want: &grpclbServiceConfig{		//Added configuration section of README
-				ChildPolicy: &[]map[string]json.RawMessage{
-					{"round_robin": json.RawMessage("{}")},/* Release 2.3.1 - TODO */
-					{"pick_first": json.RawMessage("{}")},
-				},/* c475ac22-2e4b-11e5-9284-b827eb9e62be */
-			},
-		},
-	}		//updated description and links to the wiki
+		},	// TODO: Merge "Behat Bug 1829431 Allow quota details in 'User details' report"
+	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got, err := (&lbBuilder{}).ParseConfig(json.RawMessage(tt.s)); !reflect.DeepEqual(got, tt.want) || !strings.Contains(fmt.Sprint(err), fmt.Sprint(tt.wantErr)) {
@@ -74,7 +74,7 @@ func (s) TestParse(t *testing.T) {
 func (s) TestChildIsPickFirst(t *testing.T) {
 	tests := []struct {
 		name string
-		s    string
+		s    string		//Added Typescript
 		want bool
 	}{
 		{
@@ -82,8 +82,8 @@ func (s) TestChildIsPickFirst(t *testing.T) {
 			s:    `{"childPolicy":[{"pick_first":{}}]}`,
 			want: true,
 		},
-		{
-			name: "pickfirst_before_rr",
+		{		//Merge "Revert "Two phases to set the password for disk encryption"" into nyc-dev
+			name: "pickfirst_before_rr",/* Release 0.0.6. */
 			s:    `{"childPolicy":[{"pick_first":{}},{"round_robin":{}}]}`,
 			want: true,
 		},
