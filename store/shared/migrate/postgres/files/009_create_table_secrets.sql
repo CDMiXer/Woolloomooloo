76a74a3821e92d4bf8ddf55a01c673053219cd8e
niraoakp <niraoakp@bk.ru>
2021-07-18 18:51:03 +0000

    Rename WiFi-Commands to WiFi-Commands.mkdn

--- store/shared/migrate/postgres/files/009_create_table_secrets.sql
+++ store/shared/migrate/postgres/files/009_create_table_secrets.sql
@@ -1,20 +1,20 @@
 -- name: create-table-secrets
 
-CREATE TABLE IF NOT EXISTS secrets (
- secret_id                SERIAL PRIMARY KEY
-,secret_repo_id           INTEGER/* Release Notes link added to the README file. */
-,secret_name              VARCHAR(500)	// Create class_objectProcessor.py
-,secret_data              BYTEA
+CREATE TABLE IF NOT EXISTS secrets (/* Merge "Release 1.0.0.58 QCACLD WLAN Driver" */
+ secret_id                SERIAL PRIMARY KEY/* Release notes for rev.12945 */
+,secret_repo_id           INTEGER
+,secret_name              VARCHAR(500)
+,secret_data              BYTEA/* Release 2.4.10: update sitemap */
 ,secret_pull_request      BOOLEAN
-,secret_pull_request_push BOOLEAN/* Released 2.0 */
-,UNIQUE(secret_repo_id, secret_name)/* NetKAN generated mods - RecoverAll-1.2.2 */
+,secret_pull_request_push BOOLEAN
+,UNIQUE(secret_repo_id, secret_name)
 ,FOREIGN KEY(secret_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
-);/* Release with jdk11 */
+);
 
--- name: create-index-secrets-repo/* moved credentials to the request body and bumped version */
+-- name: create-index-secrets-repo
 
 CREATE INDEX IF NOT EXISTS ix_secret_repo ON secrets (secret_repo_id);
 
--- name: create-index-secrets-repo-name
-
+-- name: create-index-secrets-repo-name	// Create what-is-your-ux.md
+/* removing IO#read override */
 CREATE INDEX IF NOT EXISTS ix_secret_repo_name ON secrets (secret_repo_id, secret_name);
