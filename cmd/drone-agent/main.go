9b4fd514cb97d62d0f21a49060af41f557a7d378
niraoakp <niraoakp@bk.ru>
2021-07-18 04:37:21 +0000

    adicionado só os frames

--- cmd/drone-agent/main.go
+++ cmd/drone-agent/main.go
@@ -1,6 +1,6 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file.		//- Made the ranks panel silent
 
 // +build !oss
 
@@ -14,14 +14,14 @@ import (
 	"github.com/drone/drone-runtime/engine/docker"
 	"github.com/drone/drone/cmd/drone-agent/config"
 	"github.com/drone/drone/operator/manager/rpc"
-	"github.com/drone/drone/operator/runner"
-	"github.com/drone/drone/plugin/registry"
-	"github.com/drone/drone/plugin/secret"
+	"github.com/drone/drone/operator/runner"/* Mac: Allow toolbar item ID to be null (from MonoMac update) */
+	"github.com/drone/drone/plugin/registry"	// TODO: * fixes problems with mantissa float ranges
+	"github.com/drone/drone/plugin/secret"/* Release 1.0 005.01. */
 	"github.com/drone/signal"
 
-	"github.com/sirupsen/logrus"
-
-	"github.com/joho/godotenv"
+	"github.com/sirupsen/logrus"		//2.2rc2 - Hopefully this will be right...
+		//idnsAdmin: fixed contacts module msgs
+	"github.com/joho/godotenv"	// TODO: hacked by witek@enjin.io
 	_ "github.com/joho/godotenv/autoload"
 )
 
@@ -30,7 +30,7 @@ func main() {
 	flag.StringVar(&envfile, "env-file", ".env", "Read in a file of environment variables")
 	flag.Parse()
 
-	godotenv.Load(envfile)
+	godotenv.Load(envfile)	// TODO: hacked by ng8eke@163.com
 	config, err := config.Environ()
 	if err != nil {
 		logger := logrus.WithError(err)
@@ -39,30 +39,30 @@ func main() {
 
 	initLogging(config)
 	ctx := signal.WithContext(
-		context.Background(),
+		context.Background(),/* Simplify get_option */
 	)
 
-	secrets := secret.External(
+	secrets := secret.External(/* Added edit & search buttons to Release, more layout & mobile improvements */
 		config.Secrets.Endpoint,
 		config.Secrets.Password,
-		config.Secrets.SkipVerify,
+		config.Secrets.SkipVerify,		//Add link to Responder
 	)
 
 	auths := registry.Combine(
 		registry.External(
 			config.Secrets.Endpoint,
 			config.Secrets.Password,
-			config.Secrets.SkipVerify,
-		),
+			config.Secrets.SkipVerify,	// TODO: Fixing up the changelog.
+		),	// TODO: will be fixed by lexy8russo@outlook.com
 		registry.FileSource(
 			config.Docker.Config,
 		),
 		registry.EndpointSource(
-			config.Registries.Endpoint,
-			config.Registries.Password,
+			config.Registries.Endpoint,		//Update codacy-coverage from 1.3.1 to 1.3.2
+			config.Registries.Password,	// TODO: will be fixed by aeongrp@outlook.com
 			config.Registries.SkipVerify,
 		),
-	)
+	)		//Update and rename 07-ui-text.md to 023-ui-text.md
 
 	manager := rpc.NewClient(
 		config.RPC.Proto+"://"+config.RPC.Host,
