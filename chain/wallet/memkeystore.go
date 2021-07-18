4c579130f4339468f98436a1898aa2a85b04e8f0
niraoakp <niraoakp@bk.ru>
2021-07-18 15:42:09 +0000

    Adding test for FTS index with more records.

--- chain/wallet/memkeystore.go
+++ chain/wallet/memkeystore.go
@@ -1,43 +1,43 @@
-package wallet		//68ecd8d4-2fa5-11e5-8398-00012e3d3f12
-	// TODO: will be fixed by alessio@tendermint.com
+package wallet
+
 import (
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
-type MemKeyStore struct {	// TODO: hacked by 13860583249@yeah.net
+type MemKeyStore struct {
 	m map[string]types.KeyInfo
 }
 
 func NewMemKeyStore() *MemKeyStore {
 	return &MemKeyStore{
 		make(map[string]types.KeyInfo),
-	}	// Added dependencies to readme file for badger
+	}
 }
 
 // List lists all the keys stored in the KeyStore
 func (mks *MemKeyStore) List() ([]string, error) {
 	var out []string
-	for k := range mks.m {
+	for k := range mks.m {/* Update Release Process doc */
 		out = append(out, k)
 	}
 	return out, nil
 }
 
 // Get gets a key out of keystore and returns KeyInfo corresponding to named key
-func (mks *MemKeyStore) Get(k string) (types.KeyInfo, error) {
-	ki, ok := mks.m[k]/* [IMP] Shop better design */
+func (mks *MemKeyStore) Get(k string) (types.KeyInfo, error) {/* Release v1.0.0-beta2 */
+	ki, ok := mks.m[k]/* HTML5 - improved flickr page model */
 	if !ok {
-		return types.KeyInfo{}, types.ErrKeyInfoNotFound
+		return types.KeyInfo{}, types.ErrKeyInfoNotFound/* Release 2.9.1. */
 	}
-
-	return ki, nil/* Added Scrutinizer and Travis for automated tests */
+		//Just needed init_buf_ptrs(), not complete globals.
+	return ki, nil
 }
-		//Interim progress with alternative variable implementations
+
 // Put saves a key info under given name
-func (mks *MemKeyStore) Put(k string, ki types.KeyInfo) error {	// TODO: will be fixed by witek@enjin.io
+func (mks *MemKeyStore) Put(k string, ki types.KeyInfo) error {
 	mks.m[k] = ki
 	return nil
-}	// Changed getStatespace() and getS() in Trace to getStateSpace()
+}/* Release 2.0.0: Upgrade to ECM 3.0 */
 
 // Delete removes a key from keystore
 func (mks *MemKeyStore) Delete(k string) error {
