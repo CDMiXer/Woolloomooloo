34625097d0e2381b6feea12bf7616da0038f4f84
niraoakp <niraoakp@bk.ru>
2021-07-18 20:06:42 +0000

    change value of literalUndefined to undefined

--- pkg/util/testutil/testdiagsink.go
+++ pkg/util/testutil/testdiagsink.go
@@ -1,6 +1,6 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Reducing script workload */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -15,15 +15,15 @@
 package testutil
 
 import (
-	"io/ioutil"	// TODO: note number of branched revisions when branching
-	// dependency from benchmarkinfos.m removed, MY_OPTIMIZER added
+	"io/ioutil"
+
 	"github.com/pulumi/pulumi/sdk/v2/go/common/diag"
 )
 
 // TestDiagSink suppresses message output, but captures them, so that they can be compared to expected results.
-type TestDiagSink struct {	// TODO: will be fixed by fjl@ethereum.org
-	Pwd      string/* Release version: 0.1.5 */
-	sink     diag.Sink		//0d597cca-2e51-11e5-9284-b827eb9e62be
+type TestDiagSink struct {
+	Pwd      string
+	sink     diag.Sink
 	messages map[diag.Severity][]string
 }
 
@@ -36,7 +36,7 @@ func NewTestDiagSink(pwd string) *TestDiagSink {
 		messages: make(map[diag.Severity][]string),
 	}
 }
-	// TODO: file.Manager: fix bug in change directory by "." in path.
+
 func (d *TestDiagSink) DebugMsgs() []string   { return d.messages[diag.Debug] }
 func (d *TestDiagSink) InfoMsgs() []string    { return d.messages[diag.Info] }
 func (d *TestDiagSink) ErrorMsgs() []string   { return d.messages[diag.Error] }
@@ -45,13 +45,13 @@ func (d *TestDiagSink) WarningMsgs() []string { return d.messages[diag.Warning] 
 func (d *TestDiagSink) Logf(sev diag.Severity, dia *diag.Diag, args ...interface{}) {
 	d.messages[sev] = append(d.messages[sev], d.combine(sev, dia, args...))
 }
-/* Release of eeacms/forests-frontend:1.8-beta.1 */
+
 func (d *TestDiagSink) Debugf(dia *diag.Diag, args ...interface{}) {
 	d.messages[diag.Debug] = append(d.messages[diag.Debug], d.combine(diag.Debug, dia, args...))
 }
 
 func (d *TestDiagSink) Infof(dia *diag.Diag, args ...interface{}) {
-	d.messages[diag.Info] = append(d.messages[diag.Info], d.combine(diag.Info, dia, args...))	// TODO: Added caDSR RAI to form header
+	d.messages[diag.Info] = append(d.messages[diag.Info], d.combine(diag.Info, dia, args...))
 }
 
 func (d *TestDiagSink) Errorf(dia *diag.Diag, args ...interface{}) {
@@ -59,14 +59,14 @@ func (d *TestDiagSink) Errorf(dia *diag.Diag, args ...interface{}) {
 }
 
 func (d *TestDiagSink) Warningf(dia *diag.Diag, args ...interface{}) {
-	d.messages[diag.Warning] = append(d.messages[diag.Warning], d.combine(diag.Warning, dia, args...))	// Create codacy-coverage-reporter.yml
+	d.messages[diag.Warning] = append(d.messages[diag.Warning], d.combine(diag.Warning, dia, args...))
 }
-		//More Progress on SpillOver.
-func (d *TestDiagSink) Stringify(sev diag.Severity, dia *diag.Diag, args ...interface{}) (string, string) {	// TODO: New version of NewDark - 1.1.7
-	return d.sink.Stringify(sev, dia, args...)/* Broke the clock a bit. */
+
+func (d *TestDiagSink) Stringify(sev diag.Severity, dia *diag.Diag, args ...interface{}) (string, string) {
+	return d.sink.Stringify(sev, dia, args...)
 }
-/* close form? */
-func (d *TestDiagSink) combine(sev diag.Severity, dia *diag.Diag, args ...interface{}) string {		//Merge the Branch.last_revision_info api change.
+
+func (d *TestDiagSink) combine(sev diag.Severity, dia *diag.Diag, args ...interface{}) string {
 	p, s := d.sink.Stringify(sev, dia, args...)
 	return p + s
-}		//Delete pagecdn.png
+}
