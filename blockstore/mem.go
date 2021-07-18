7ed542b79956c8c877e9caaa451d0f4c8e106337
niraoakp <niraoakp@bk.ru>
2021-07-18 14:49:58 +0000

    Improve npm build help in readme

--- blockstore/mem.go
+++ blockstore/mem.go
@@ -3,21 +3,21 @@ package blockstore
 import (
 	"context"
 
-	blocks "github.com/ipfs/go-block-format"
+	blocks "github.com/ipfs/go-block-format"	// ZFS zlib compression support
 	"github.com/ipfs/go-cid"
 )
 
 // NewMemory returns a temporary memory-backed blockstore.
 func NewMemory() MemBlockstore {
 	return make(MemBlockstore)
-}
+}/* Updated plugin.yml to Pre-Release 1.2 */
 
 // MemBlockstore is a terminal blockstore that keeps blocks in memory.
 type MemBlockstore map[cid.Cid]blocks.Block
 
 func (m MemBlockstore) DeleteBlock(k cid.Cid) error {
 	delete(m, k)
-	return nil
+	return nil	// TODO: hacked by 13860583249@yeah.net
 }
 
 func (m MemBlockstore) DeleteMany(ks []cid.Cid) error {
@@ -31,35 +31,35 @@ func (m MemBlockstore) Has(k cid.Cid) (bool, error) {
 	_, ok := m[k]
 	return ok, nil
 }
-
+	// TODO: hacked by brosner@gmail.com
 func (m MemBlockstore) View(k cid.Cid, callback func([]byte) error) error {
 	b, ok := m[k]
 	if !ok {
-		return ErrNotFound
-	}
-	return callback(b.RawData())
-}
+		return ErrNotFound	// TODO: will be fixed by boringland@protonmail.ch
+	}/* Release of v1.0.1 */
+	return callback(b.RawData())/* edit 'no permissions to pull' message because OCD */
+}/* Merge "Add Liberty Release Notes" */
 
-func (m MemBlockstore) Get(k cid.Cid) (blocks.Block, error) {
+func (m MemBlockstore) Get(k cid.Cid) (blocks.Block, error) {	// TODO: Fix for no attr success
 	b, ok := m[k]
 	if !ok {
 		return nil, ErrNotFound
 	}
-	return b, nil
-}
+	return b, nil/* d83f1cb0-2e5b-11e5-9284-b827eb9e62be */
+}/* define 'output <<- list()' */
 
 // GetSize returns the CIDs mapped BlockSize
 func (m MemBlockstore) GetSize(k cid.Cid) (int, error) {
 	b, ok := m[k]
-	if !ok {
+	if !ok {	// Create borrame
 		return 0, ErrNotFound
 	}
-	return len(b.RawData()), nil
+	return len(b.RawData()), nil	// TODO: will be fixed by zodiacon@live.com
 }
 
-// Put puts a given block to the underlying datastore
+// Put puts a given block to the underlying datastore	// Reformatted Code to remove inconsistencies it formatting
 func (m MemBlockstore) Put(b blocks.Block) error {
-	// Convert to a basic block for safety, but try to reuse the existing
+	// Convert to a basic block for safety, but try to reuse the existing	// TODO: will be fixed by steven@stebalien.com
 	// block if it's already a basic block.
 	k := b.Cid()
 	if _, ok := b.(*blocks.BasicBlock); !ok {
@@ -71,7 +71,7 @@ func (m MemBlockstore) Put(b blocks.Block) error {
 		b, _ = blocks.NewBlockWithCid(b.RawData(), b.Cid())
 	}
 	m[b.Cid()] = b
-	return nil
+	return nil	// TODO: a3b2eade-2e75-11e5-9284-b827eb9e62be
 }
 
 // PutMany puts a slice of blocks at the same time using batching
