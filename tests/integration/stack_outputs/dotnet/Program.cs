84408a3c18aa348d020b541f82dccfa3b02bf70b
niraoakp <niraoakp@bk.ru>
2021-07-18 13:07:36 +0000

    Update updateDNS.sh

--- tests/integration/stack_outputs/dotnet/Program.cs
+++ tests/integration/stack_outputs/dotnet/Program.cs
@@ -1,19 +1,19 @@
 ﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
 
-using System.Collections.Generic;	// TODO: will be fixed by greg@colvin.org
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Pulumi;
 
 class Program
 {
     static Task<int> Main(string[] args)
-    {/* Unified some localised strings for routines, triggers and events. */
+    {
         return Deployment.RunAsync(() => 
-        {/* Release new version 2.1.2: A few remaining l10n tasks */
-            return new Dictionary<string, object>/* high-availability: rename Runtime owner to Release Integration */
+        {
+            return new Dictionary<string, object>
             {
                 {  "xyz", "ABC" },
-                {  "foo", 42 },/* Release 39 */
+                {  "foo", 42 },
             };
         });
     }
