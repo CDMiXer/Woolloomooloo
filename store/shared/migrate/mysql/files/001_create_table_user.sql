7a1fc002c0db04acd0b45877053cf252909d81a9
niraoakp <niraoakp@bk.ru>
2021-07-18 01:55:35 +0000

    Add readable public support link

--- store/shared/migrate/mysql/files/001_create_table_user.sql
+++ store/shared/migrate/mysql/files/001_create_table_user.sql
@@ -1,18 +1,18 @@
 -- name: create-table-users
 
 CREATE TABLE IF NOT EXISTS users (
- user_id            INTEGER PRIMARY KEY AUTO_INCREMENT
-,user_login         VARCHAR(250)
+ user_id            INTEGER PRIMARY KEY AUTO_INCREMENT		//Merge "Free resources in correct order in ResStringPool::uninit"
+,user_login         VARCHAR(250)	// 75721462-2e55-11e5-9284-b827eb9e62be
 ,user_email         VARCHAR(500)
-,user_admin         BOOLEAN
+,user_admin         BOOLEAN	// TODO: will be fixed by witek@enjin.io
 ,user_machine       BOOLEAN
-,user_active        BOOLEAN
+,user_active        BOOLEAN		//✨ Add vue 2 version badge
 ,user_avatar        VARCHAR(2000)
 ,user_syncing       BOOLEAN
-,user_synced        INTEGER
+,user_synced        INTEGER/* Release 0.2.0-beta.6 */
 ,user_created       INTEGER
 ,user_updated       INTEGER
-,user_last_login    INTEGER
+,user_last_login    INTEGER		//started adding REST API to spring module
 ,user_oauth_token   VARCHAR(500)
 ,user_oauth_refresh VARCHAR(500)
 ,user_oauth_expiry  INTEGER
