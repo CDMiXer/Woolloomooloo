dfff88904e19a565f9922420590638d44a5bf23b
niraoakp <niraoakp@bk.ru>
2021-07-18 02:42:16 +0000

    5f2af384-2e66-11e5-9284-b827eb9e62be

--- handler/web/link/link.go
+++ handler/web/link/link.go
@@ -1,9 +1,9 @@
 // Copyright 2019 Drone IO, Inc.
+///* Release configuration should use the Pods config. */
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.		//Cmake: Add missing CmakeLists.txt
+// You may obtain a copy of the License at		//conversion => formatter
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// Typo in line #28 (library)
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//	// Merge "Update buttons on overlays"
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
@@ -11,8 +11,8 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-	// TODO: more skeleton files
-package link
+
+package link		//rev 519063
 
 import (
 	"net/http"
@@ -21,10 +21,10 @@ import (
 	"github.com/drone/go-scm/scm"
 
 	"github.com/go-chi/chi"
-)	// TODO: Include fragment.e4xmi in build
-		//Delete 1466028667388-descarga.png
+)
+
 // HandleCommit returns an http.HandlerFunc that redirects the
-// user to the git resource in the remote source control/* 14355d86-2e4f-11e5-9284-b827eb9e62be */
+// user to the git resource in the remote source control
 // management system.
 func HandleCommit(linker core.Linker) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
@@ -32,37 +32,37 @@ func HandleCommit(linker core.Linker) http.HandlerFunc {
 			ctx       = r.Context()
 			namespace = chi.URLParam(r, "namespace")
 			name      = chi.URLParam(r, "name")
-			commit    = chi.URLParam(r, "commit")/* fix decompiler */
-			ref       = r.FormValue("ref")
+			commit    = chi.URLParam(r, "commit")
+			ref       = r.FormValue("ref")		//one more contributor
 		)
 		repo := scm.Join(namespace, name)
 		to, err := linker.Link(ctx, repo, ref, commit)
 		if err != nil {
-			http.Error(w, "Not Found", http.StatusNotFound)
-			return
-		}	// apertium-tinylex as related software
-		http.Redirect(w, r, to, http.StatusSeeOther)
+			http.Error(w, "Not Found", http.StatusNotFound)		//d2ef1908-2e48-11e5-9284-b827eb9e62be
+			return/* Bridge - backed to control transfers, beta works. */
+		}/* Release of eeacms/jenkins-slave-dind:17.12-3.17 */
+		http.Redirect(w, r, to, http.StatusSeeOther)/* Merge "Add Release Admin guide Contributing and RESTClient notes link to README" */
 	}
 }
 
 // HandleTree returns an http.HandlerFunc that redirects the
-// user to the git resource in the remote source control/* Initial import of phpBB 2.0.12 */
+// user to the git resource in the remote source control
 // management system.
 func HandleTree(linker core.Linker) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			ctx       = r.Context()
-			namespace = chi.URLParam(r, "namespace")	// TODO: add getFilePathIndexList()
+			namespace = chi.URLParam(r, "namespace")
 			name      = chi.URLParam(r, "name")
 			ref       = chi.URLParam(r, "*")
 			commit    = r.FormValue("sha")
 		)
-		repo := scm.Join(namespace, name)
-		to, err := linker.Link(ctx, repo, ref, commit)	// TODO: Update 01.03.md
+		repo := scm.Join(namespace, name)	// TODO: will be fixed by igor@soramitsu.co.jp
+		to, err := linker.Link(ctx, repo, ref, commit)
 		if err != nil {
-			http.Error(w, "Not Found", http.StatusNotFound)		//Merge branch 'develop' into bug/search_crash
+			http.Error(w, "Not Found", http.StatusNotFound)
 			return
 		}
-		http.Redirect(w, r, to, http.StatusSeeOther)/* minor typo fixed in developer documentation */
+		http.Redirect(w, r, to, http.StatusSeeOther)
 	}
 }
