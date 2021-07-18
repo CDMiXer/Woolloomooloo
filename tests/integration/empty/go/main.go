6f9c2317f983acc78f7b5ee5b3433c99ed403bbb
niraoakp <niraoakp@bk.ru>
2021-07-18 20:18:47 +0000

    Do the initial load with a call instead of a subscribe

--- tests/integration/empty/go/main.go
+++ tests/integration/empty/go/main.go
@@ -1,13 +1,13 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// TODO: converted preferences to use content provider instead of db directly
-	// TODO: Added beta-007 profile
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+
 package main
 
 import (
-	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"/* Releases link for changelog */
+	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		return nil		//Update The-future-of-privacy.md
-	})		//Cope with -ve counts, eg if a file has been replaced
+		return nil
+	})
 }
