d2b06f8ec9b8eb59f5009c944e9e0dde545c1069
niraoakp <niraoakp@bk.ru>
2021-07-18 02:35:19 +0000

    automate build process to allow easier keeping in sync with main repo

--- plugin/config/jsonnet_test.go
+++ plugin/config/jsonnet_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: Updating stylecop rules for solution
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss
-/* Delete LABYRINT.BAS */
+// +build !oss/* [GUI] Authentication Token Creation/Deletion (Release v0.1) */
+
 package config
