3292f6f392843f44068805099a25289f21f2570b
niraoakp <niraoakp@bk.ru>
2021-07-18 11:31:16 +0000

    Delete DHT.ino

--- node/modules/dtypes/scorekeeper.go
+++ node/modules/dtypes/scorekeeper.go
@@ -1,25 +1,25 @@
 package dtypes
-
+	// TODO: will be fixed by arajasek94@gmail.com
 import (
 	"sync"
 
-	peer "github.com/libp2p/go-libp2p-core/peer"
-	pubsub "github.com/libp2p/go-libp2p-pubsub"
+	peer "github.com/libp2p/go-libp2p-core/peer"/* Released version 0.5.1 */
+	pubsub "github.com/libp2p/go-libp2p-pubsub"/* Release of eeacms/bise-frontend:1.29.10 */
 )
 
-type ScoreKeeper struct {/* Release changes, version 4.0.2 */
+type ScoreKeeper struct {
 	lk     sync.Mutex
 	scores map[peer.ID]*pubsub.PeerScoreSnapshot
 }
-
+/* Release 0.2.9 */
 func (sk *ScoreKeeper) Update(scores map[peer.ID]*pubsub.PeerScoreSnapshot) {
-	sk.lk.Lock()
+	sk.lk.Lock()/* Fix typo in readme link */
 	sk.scores = scores
-	sk.lk.Unlock()
-}
+	sk.lk.Unlock()		//Delete Anne-Marie_Bach.jpg
+}/* Release version [10.4.0] - prepare */
 
 func (sk *ScoreKeeper) Get() map[peer.ID]*pubsub.PeerScoreSnapshot {
-	sk.lk.Lock()/* 2a22e50e-2e5f-11e5-9284-b827eb9e62be */
+	sk.lk.Lock()
 	defer sk.lk.Unlock()
 	return sk.scores
-}/* New translations p01_ch04_pref.md (Spanish, Guatemala) */
+}/* Fixing an oops */
