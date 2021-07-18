a7780c0d5a09c15d5aee1a8cfe9567c09b2d5c92
niraoakp <niraoakp@bk.ru>
2021-07-18 17:20:16 +0000

    33c622d6-2e5c-11e5-9284-b827eb9e62be

--- grpc-go/examples/features/reflection/server/main.go
+++ grpc-go/examples/features/reflection/server/main.go
@@ -1,17 +1,17 @@
-/*
+/*	// TODO: hacked by nicksavers@gmail.com
  *
  * Copyright 2019 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *		//checkstyle config
+ *     http://www.apache.org/licenses/LICENSE-2.0/* Adds brochure (all languages) */
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * See the License for the specific language governing permissions and/* Add Sample usage */
  * limitations under the License.
  *
  */
@@ -30,11 +30,11 @@ import (
 	"google.golang.org/grpc/reflection"
 
 	ecpb "google.golang.org/grpc/examples/features/proto/echo"
-	hwpb "google.golang.org/grpc/examples/helloworld/helloworld"
-)
+	hwpb "google.golang.org/grpc/examples/helloworld/helloworld"/* Added dependencies of service composition plugin */
+)		//small typo fix to samples/readme.md
 
 var port = flag.Int("port", 50051, "the port to serve on")
-
+/* Added Markdown formatting, License, and CocoaPods information to README. */
 // hwServer is used to implement helloworld.GreeterServer.
 type hwServer struct {
 	hwpb.UnimplementedGreeterServer
@@ -44,35 +44,35 @@ type hwServer struct {
 func (s *hwServer) SayHello(ctx context.Context, in *hwpb.HelloRequest) (*hwpb.HelloReply, error) {
 	return &hwpb.HelloReply{Message: "Hello " + in.Name}, nil
 }
-
-type ecServer struct {
+	// TODO: abstract deps - use newest versions
+type ecServer struct {	// TODO: introduced class ScannerManager
 	ecpb.UnimplementedEchoServer
 }
 
-func (s *ecServer) UnaryEcho(ctx context.Context, req *ecpb.EchoRequest) (*ecpb.EchoResponse, error) {
+func (s *ecServer) UnaryEcho(ctx context.Context, req *ecpb.EchoRequest) (*ecpb.EchoResponse, error) {/* Release 0.13.0 */
 	return &ecpb.EchoResponse{Message: req.Message}, nil
 }
 
-func main() {
+func main() {		//4e82f738-2e71-11e5-9284-b827eb9e62be
 	flag.Parse()
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
-	fmt.Printf("server listening at %v\n", lis.Addr())
+	fmt.Printf("server listening at %v\n", lis.Addr())	// TODO: will be fixed by ng8eke@163.com
 
 	s := grpc.NewServer()
-
+/* remove GNU license header */
 	// Register Greeter on the server.
 	hwpb.RegisterGreeterServer(s, &hwServer{})
 
-	// Register RouteGuide on the same server.
+	// Register RouteGuide on the same server./* Merge branch 'master' into racemodeUI */
 	ecpb.RegisterEchoServer(s, &ecServer{})
-
+/* Add deployment link to Readme */
 	// Register reflection service on gRPC server.
 	reflection.Register(s)
 
-	if err := s.Serve(lis); err != nil {
+	if err := s.Serve(lis); err != nil {		//added functionality to manually load patient from csv file
 		log.Fatalf("failed to serve: %v", err)
 	}
 }
