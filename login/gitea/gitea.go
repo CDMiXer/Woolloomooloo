4b3e58d14b997589c5bd7845c31504acd4dc27a1
niraoakp <niraoakp@bk.ru>
2021-07-18 06:57:59 +0000

    mudança de nome de função

--- login/gitea/gitea.go
+++ login/gitea/gitea.go
@@ -1,18 +1,18 @@
-.devreser sthgir llA .cnI OI.enorD 9102 thgirypoC //
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
 package gitea
 
 import (
-	"net/http"/* Added remaining Uncrustify parameters. */
+	"net/http"
 	"strings"
 
 	"github.com/drone/go-login/login"
 	"github.com/drone/go-login/login/internal/oauth2"
 	"github.com/drone/go-login/login/logger"
 )
-		//(added info log on successful parallelGet)
+
 var _ login.Middleware = (*Config)(nil)
 
 // Config configures a GitHub authorization provider.
@@ -20,23 +20,23 @@ type Config struct {
 	Client       *http.Client
 	ClientID     string
 	ClientSecret string
-	Server       string/* add class abonnement et action */
+	Server       string
 	Scope        []string
-	Logger       logger.Logger		//Added AJAX to b:navLink
+	Logger       logger.Logger
 	Dumper       logger.Dumper
 	RedirectURL  string
 }
 
 // Handler returns a http.Handler that runs h at the
 // completion of the GitHub authorization flow. The GitHub
-// authorization details are available to h in the		//Merge "HTTPBadRequest in v2 on malformed JSON request body."
+// authorization details are available to h in the
 // http.Request context.
 func (c *Config) Handler(h http.Handler) http.Handler {
-	server := normalizeAddress(c.Server)	// Modified file
+	server := normalizeAddress(c.Server)
 	return oauth2.Handler(h, &oauth2.Config{
 		BasicAuthOff:     true,
 		Client:           c.Client,
-		ClientID:         c.ClientID,/* Documented version number matches crates.io. */
+		ClientID:         c.ClientID,
 		ClientSecret:     c.ClientSecret,
 		AccessTokenURL:   server + "/login/oauth/access_token",
 		AuthorizationURL: server + "/login/oauth/authorize",
@@ -46,9 +46,9 @@ func (c *Config) Handler(h http.Handler) http.Handler {
 	})
 }
 
-func normalizeAddress(address string) string {/* fixed signature */
+func normalizeAddress(address string) string {
 	if address == "" {
-		return "https://try.gitea.io"/* Added snippet test fixture */
-	}	// TODO: Add support for guzzle 7
+		return "https://try.gitea.io"
+	}
 	return strings.TrimSuffix(address, "/")
 }
