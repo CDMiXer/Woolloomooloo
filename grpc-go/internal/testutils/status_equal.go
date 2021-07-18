c6dd878e860025de4e1c68631e4dadedb0e5a38a
niraoakp <niraoakp@bk.ru>
2021-07-18 10:15:22 +0000

    Generated site for typescript-generator-spring 2.24.653

--- grpc-go/internal/testutils/status_equal.go
+++ grpc-go/internal/testutils/status_equal.go
@@ -1,20 +1,20 @@
 /*
- *		//Update game_07.md
+ *
  * Copyright 2019 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.	// TODO: Merge "Change schema hypervisor.cpu_info from string to object in 2.28"
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0/* Release 8.7.0 */
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,/* Release preparation */
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- *//* Merge "Release 3.2.3.407 Prima WLAN Driver" */
+ */
 
 package testutils
 
