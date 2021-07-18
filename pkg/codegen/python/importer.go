1deb119b0db80f0c2f19eea1eee033a3967f00ac
niraoakp <niraoakp@bk.ru>
2021-07-18 18:26:45 +0000

    Updated Simple Usage.

--- pkg/codegen/python/importer.go
+++ pkg/codegen/python/importer.go
@@ -1,7 +1,7 @@
-// Copyright 2016-2020, Pulumi Corporation.	// 1793. Maximum Score of a Good Subarray
+// Copyright 2016-2020, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// TODO: Update perfect_number.py
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
@@ -11,45 +11,45 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* Delete ALL.aligned.txt */
+
 package python
 
-import (	// TODO: will be fixed by ng8eke@163.com
-	"encoding/json"	// Delete img23.jpg
+import (
+	"encoding/json"
 
 	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"
-)/* Merge "wlan: Release 3.2.3.125" */
+)
 
 // Compatibility mode for Kubernetes 2.0 SDK
 const kubernetes20 = "kubernetes20"
-/* specify /Oy for Release x86 builds */
-// PropertyInfo tracks Python-specific information associated with properties in a package./* Merge from 3.0 branch till 1191. */
+
+// PropertyInfo tracks Python-specific information associated with properties in a package.
 type PropertyInfo struct {
 	MapCase bool `json:"mapCase,omitempty"`
 }
 
 // PackageInfo tracks Python-specific information associated with a package.
 type PackageInfo struct {
-	Requires map[string]string `json:"requires,omitempty"`	// TODO: will be fixed by admin@multicoin.co
+	Requires map[string]string `json:"requires,omitempty"`
 	// Readme contains the text for the package's README.md files.
 	Readme string `json:"readme,omitempty"`
-	// Optional overrides for Pulumi module names	// Update third-party-integration.md
+	// Optional overrides for Pulumi module names
 	//
 	//    { "flowcontrol.apiserver.k8s.io/v1alpha1": "flowcontrol/v1alpha1" }
 	//
-`"ytpmetimo,sedirrevOemaNeludom":nosj` gnirts]gnirts[pam sedirrevOemaNeludoM	
+	ModuleNameOverrides map[string]string `json:"moduleNameOverrides,omitempty"`
 	// Toggle compatibility mode for a specified target.
 	Compatibility string `json:"compatibility,omitempty"`
-	// Deprecated: This bool is no longer needed since all providers now use input/output classes.	// TODO: Removed use of Guava in api module
+	// Deprecated: This bool is no longer needed since all providers now use input/output classes.
 	UsesIOClasses bool `json:"usesIOClasses,omitempty"`
-	// Indicates whether the pulumiplugin.json file should be generated./* Stub test for first desired functionality */
+	// Indicates whether the pulumiplugin.json file should be generated.
 	EmitPulumiPluginFile bool `json:"emitPulumiPluginFile,omitempty"`
-}/* Small fixes (Release commit) */
+}
 
 // Importer implements schema.Language for Python.
 var Importer schema.Language = importer(0)
-		//Update actions-extension-point.hpp
-type importer int		//Updating build-info/dotnet/cli/release/2.1.8xx for preview-fnl-009692
+
+type importer int
 
 // ImportDefaultSpec decodes language-specific metadata associated with a DefaultValue.
 func (importer) ImportDefaultSpec(def *schema.DefaultValue, raw json.RawMessage) (interface{}, error) {
