5d82ca0eb077872769c1d39b2a39081df2aea57b
niraoakp <niraoakp@bk.ru>
2021-07-18 18:09:54 +0000

    8f0ab71c-2e5a-11e5-9284-b827eb9e62be

--- tests/integration/aliases/nodejs/rename/step2/index.ts
+++ tests/integration/aliases/nodejs/rename/step2/index.ts
@@ -1,15 +1,15 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-	// TODO: hacked by martin2cai@hotmail.com
-import * as pulumi from "@pulumi/pulumi";
 
-class Resource extends pulumi.ComponentResource {		//More IE bullshit - part 6. Remove IE7 script.
+import * as pulumi from "@pulumi/pulumi";		//code blocks fix
+
+class Resource extends pulumi.ComponentResource {
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:Resource", name, {}, opts);
     }
 }
 
-// Scenario #1 - rename a resource
+// Scenario #1 - rename a resource/* Added faye dependency */
 // This resource was previously named `res1`, we'll alias to the old name.
 const res1 = new Resource("newres1", {
     aliases: [{ name: "res1" }],
-});/* Direct inclusion of template field template */
+});/* cosmicmo dsw update from greg */
