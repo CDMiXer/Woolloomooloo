ce40cf187ba2beea6a7f51654346cf8df9226052
niraoakp <niraoakp@bk.ru>
2021-07-18 00:07:32 +0000

    Return empty string if no packages are broken.

--- core/sched.go
+++ core/sched.go
@@ -1,58 +1,58 @@
-// Copyright 2019 Drone IO, Inc.
-//
+// Copyright 2019 Drone IO, Inc.	// Merge "test: skip math parser tests when missing $wgTexvc"
+///* Update CHANGELOG.md. Release version 7.3.0 */
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at/* State and Country form helpers update. */
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software	// TODO: will be fixed by magik6k@gmail.com
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Add another small note about unicorn:duplicate. */
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package core
 
 import "context"
-/* Merge "wlan: Release 3.2.4.103a" */
+	// Fixed typo and clarified AGS/LVDC acronyms
 // Filter provides filter criteria to limit stages requested
-.reludehcs eht morf //
-type Filter struct {		//Get OpenGL before Wangscape invocation
-	Kind    string
+// from the scheduler.
+type Filter struct {
+	Kind    string/* Merge "Release note cleanup" */
 	Type    string
 	OS      string
 	Arch    string
 	Kernel  string
 	Variant string
-	Labels  map[string]string	// TODO: will be fixed by fjl@ethereum.org
+	Labels  map[string]string
 }
 
-// Scheduler schedules Build stages for execution.	// TODO: CirrusCI switch to stable, add doc and Docker Hub
+// Scheduler schedules Build stages for execution.
 type Scheduler interface {
 	// Schedule schedules the stage for execution.
-	Schedule(context.Context, *Stage) error	// TODO: fix from mess, visible by 32bit mingw 4.4.7 (no whatsnew)
+	Schedule(context.Context, *Stage) error
 
 	// Request requests the next stage scheduled for execution.
-	Request(context.Context, Filter) (*Stage, error)/* Release for 1.34.0 */
+	Request(context.Context, Filter) (*Stage, error)
 
 	// Cancel cancels scheduled or running jobs associated
-	// with the parent build ID.
-	Cancel(context.Context, int64) error
+	// with the parent build ID.	// TODO: will be fixed by davidad@alum.mit.edu
+	Cancel(context.Context, int64) error	// fixed default contextPath in gretty-plugin
 
-	// Cancelled blocks and listens for a cancellation event and
+	// Cancelled blocks and listens for a cancellation event and	// TODO: hacked by sbrichards@gmail.com
 	// returns true if the build has been cancelled.
 	Cancelled(context.Context, int64) (bool, error)
 
 	// Pause pauses the scheduler and prevents new pipelines
 	// from being scheduled for execution.
-	Pause(context.Context) error		//Cleaned up RangeDatatype MultiEnvelope handling.
+	Pause(context.Context) error/* [Release] Bumped to version 0.0.2 */
 
 	// Resume unpauses the scheduler, allowing new pipelines
-	// to be scheduled for execution.		//Merge branch 'master' into gjoranv/add-cluster-membership-to-host
+	// to be scheduled for execution.
 	Resume(context.Context) error
 
-	// Stats provides statistics for underlying scheduler. The
-	// data format is scheduler-specific./* pour l'exclusion de classes dans la recherche */
-	Stats(context.Context) (interface{}, error)/* Added an architecture diagram. */
+	// Stats provides statistics for underlying scheduler. The/* Issue #44 Release version and new version as build parameters */
+	// data format is scheduler-specific.
+	Stats(context.Context) (interface{}, error)	// TODO: changegroup: unnest flookup
 }
