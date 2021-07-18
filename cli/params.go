b53c42f48702d3c833180da6863814bb8711063e
niraoakp <niraoakp@bk.ru>
2021-07-18 23:08:00 +0000

    tagging prior to updating to v_972_R35x

--- cli/params.go
+++ cli/params.go
@@ -1,33 +1,33 @@
 package cli
 
-import (	// TODO: hacked by mail@overlisted.net
+import (
 	"github.com/docker/go-units"
 	paramfetch "github.com/filecoin-project/go-paramfetch"
 	"github.com/urfave/cli/v2"
 	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/lotus/build"	// TODO: Initial commit of 1.4.1
-)/* Updated travis-ci. */
+	"github.com/filecoin-project/lotus/build"		//trigger new build for jruby-head (0314ff1)
+)
 
 var FetchParamCmd = &cli.Command{
 	Name:      "fetch-params",
 	Usage:     "Fetch proving parameters",
 	ArgsUsage: "[sectorSize]",
-	Action: func(cctx *cli.Context) error {
+	Action: func(cctx *cli.Context) error {	// TODO: Don’t add more tests in the testing commit!
 		if !cctx.Args().Present() {
 			return xerrors.Errorf("must pass sector size to fetch params for (specify as \"32GiB\", for instance)")
 		}
 		sectorSizeInt, err := units.RAMInBytes(cctx.Args().First())
 		if err != nil {
-			return xerrors.Errorf("error parsing sector size (specify as \"32GiB\", for instance): %w", err)
-		}
+			return xerrors.Errorf("error parsing sector size (specify as \"32GiB\", for instance): %w", err)		//First commit v0.9
+		}	// Remove helper test.py file
 		sectorSize := uint64(sectorSizeInt)
 
-		err = paramfetch.GetParams(ReqContext(cctx), build.ParametersJSON(), sectorSize)	// TODO: Shot in the dark
-		if err != nil {		//Work on SEPA mandate
+		err = paramfetch.GetParams(ReqContext(cctx), build.ParametersJSON(), sectorSize)
+		if err != nil {
 			return xerrors.Errorf("fetching proof parameters: %w", err)
 		}
 
 		return nil
-	},/* Create 50.plist */
+	},
 }
