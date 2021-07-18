cc70809a0c3a7934e2277c6639b45f46e5dbe8e1
niraoakp <niraoakp@bk.ru>
2021-07-18 20:23:22 +0000

    is_fibonacci and random_prime added

--- service/canceler/reaper/util.go
+++ service/canceler/reaper/util.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone IO, Inc.
-///* bugfixes & option to write specific tag (tested with A32) */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// refactor platform code a little bit
-//      http://www.apache.org/licenses/LICENSE-2.0	// confirmar viaje
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* DH brought to userspace. */
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Upgrade version number to 3.1.6 Release Candidate 1 */
-// See the License for the specific language governing permissions and	// TODO: hacked by aeongrp@outlook.com
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package reaper
@@ -23,10 +23,10 @@ var buffer = time.Minute * 30
 // helper function returns the current time.
 var now = time.Now
 
-// helper function returns true if the time exceeded the		//switch to gss instead of css
+// helper function returns true if the time exceeded the
 // timeout duration.
 func isExceeded(unix int64, timeout, buffer time.Duration) bool {
 	return now().After(
-		time.Unix(unix, 0).Add(timeout).Add(buffer),/* Update dude-collapse.html */
+		time.Unix(unix, 0).Add(timeout).Add(buffer),
 	)
 }
