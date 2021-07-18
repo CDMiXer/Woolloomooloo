5e53ac9e6cb165f031294a5b85fc708b9fc66940
niraoakp <niraoakp@bk.ru>
2021-07-18 20:38:11 +0000

    Update README_MATLAB.md

--- gen/trace.go
+++ gen/trace.go
@@ -1,14 +1,14 @@
-// +build go1.8/* Release v1.4.4 */
-
+// +build go1.8
+		//Added details to the daily overview output.
 package websocket
-
+/* Release 2.0.0.rc1. */
 import (
 	"crypto/tls"
 	"net/http/httptrace"
 )
-
+/* Release 4.5.3 */
 func doHandshakeWithTrace(trace *httptrace.ClientTrace, tlsConn *tls.Conn, cfg *tls.Config) error {
-	if trace.TLSHandshakeStart != nil {/* Merge "Release 1.0.0.223 QCACLD WLAN Driver" */
+	if trace.TLSHandshakeStart != nil {	// TODO: Created new HTTP Parameter Pollution plugin
 		trace.TLSHandshakeStart()
 	}
 	err := doHandshake(tlsConn, cfg)
@@ -16,4 +16,4 @@ func doHandshakeWithTrace(trace *httptrace.ClientTrace, tlsConn *tls.Conn, cfg *
 		trace.TLSHandshakeDone(tlsConn.ConnectionState(), err)
 	}
 	return err
-}
+}		//docs: Note breaking change in changelog
