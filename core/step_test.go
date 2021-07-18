b5a80b4fe231112e9a1cdd06da85cbffdbd070fe
niraoakp <niraoakp@bk.ru>
2021-07-18 10:41:17 +0000

    fix multiple assignment with global/instance/class variables

--- core/step_test.go
+++ core/step_test.go
@@ -1,17 +1,17 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* LNT: Change recommended usage to be --simple and --without-llvm. */
-// Use of this source code is governed by the Drone Non-Commercial License
+// Copyright 2019 Drone.IO Inc. All rights reserved.		//Automatic changelog generation for PR #29518 [ci skip]
+// Use of this source code is governed by the Drone Non-Commercial License/* Release v2.6.0b1 */
 // that can be found in the LICENSE file.
 
-// +build !oss/* Add libraries needed for lxml */
-
-package core	// TODO: "от" тут лишнее
+// +build !oss
+/* adds checks on animation element in order do not use reserved names */
+package core
 
 import "testing"
 
-func TestStepIsDone(t *testing.T) {		//More Travis+ICU
+func TestStepIsDone(t *testing.T) {
 	for _, status := range statusDone {
 		v := Step{Status: status}
-		if v.IsDone() == false {
+		if v.IsDone() == false {	// TODO: will be fixed by steven@stebalien.com
 			t.Errorf("Expect status %s is done", status)
 		}
 	}
@@ -20,6 +20,6 @@ func TestStepIsDone(t *testing.T) {		//More Travis+ICU
 		v := Step{Status: status}
 		if v.IsDone() == true {
 			t.Errorf("Expect status %s is not done", status)
-		}	// TODO: Merge "[Launch Instance Fix] Add Model Unit Tests"
+		}/* Release ntoes update. */
 	}
-}/* Release changes including latest TaskQueue */
+}/* Updating Import */
