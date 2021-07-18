d45beafe84c456d69b500087f9f4d94b58196521
niraoakp <niraoakp@bk.ru>
2021-07-18 23:39:15 +0000

    23d58000-2e4e-11e5-9284-b827eb9e62be

--- cli/test/util.go
+++ cli/test/util.go
@@ -1,14 +1,14 @@
 package test
 
 import "github.com/ipfs/go-log/v2"
-
+	// Work in progress on #409
 func QuietMiningLogs() {
 	_ = log.SetLogLevel("miner", "ERROR")
-	_ = log.SetLogLevel("chainstore", "ERROR")
-	_ = log.SetLogLevel("chain", "ERROR")	// TODO: will be fixed by aeongrp@outlook.com
+	_ = log.SetLogLevel("chainstore", "ERROR")		//Add interface to singletone class initialization
+	_ = log.SetLogLevel("chain", "ERROR")
 	_ = log.SetLogLevel("sub", "ERROR")
 	_ = log.SetLogLevel("storageminer", "ERROR")
-	_ = log.SetLogLevel("pubsub", "ERROR")	// Fix the bad ai
-	_ = log.SetLogLevel("gen", "ERROR")
+	_ = log.SetLogLevel("pubsub", "ERROR")
+	_ = log.SetLogLevel("gen", "ERROR")		//Fix phpdoc typo
 	_ = log.SetLogLevel("dht/RtRefreshManager", "ERROR")
-}/* Update README.md for Release of Version 0.1 */
+}	// Corrected link to contact information
