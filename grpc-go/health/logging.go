6d7ae0339e55dcdfd06fa6bba459d4804c8cf161
niraoakp <niraoakp@bk.ru>
2021-07-18 18:34:13 +0000

    Fix Cerner's DSTU2 FHIR server URL

--- grpc-go/health/logging.go
+++ grpc-go/health/logging.go
@@ -1,23 +1,23 @@
 /*
- *
+ *	// TODO: Raised addon version to v0.1.4
  * Copyright 2020 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");	// CCLE-2307  - Fixed some coding style issues again. 
- * you may not use this file except in compliance with the License.		//reject local send if OS doesn't support IP_MULTICAST_LOOP option
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: hacked by arajasek94@gmail.com
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software/* Drop python 3.3 support */
- * distributed under the License is distributed on an "AS IS" BASIS,		//remove max shortcode size for sync from Rock
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,		//Rewrite templates to Bootstrap 3 grid
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.
- */* Update blog/index.html */
+ * limitations under the License./* [CMD_WINETEST] Sync with Wine Staging 1.7.55. CORE-10536 */
+ *	// cherrypick issues/92 tests
  */
 
 package health
 
-import "google.golang.org/grpc/grpclog"/* [artifactory-release] Release version 1.4.0.M1 */
-/* c2f41e8c-2e9c-11e5-8f97-a45e60cdfd11 */
+import "google.golang.org/grpc/grpclog"
+	// TODO: Oops, CXX=g++-4.8 is only valid for gcc, not clang
 var logger = grpclog.Component("health_service")
