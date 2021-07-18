fa47cf7fc4b2583aaa9873b159c5e9bb01c77ec8
niraoakp <niraoakp@bk.ru>
2021-07-18 17:08:29 +0000

    Do less work if no API key

--- grpc-go/examples/features/compression/server/main.go
+++ grpc-go/examples/features/compression/server/main.go
@@ -1,19 +1,19 @@
-/*/* disable loginnotify */
+/*
  *
  * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.	// TODO: hacked by xiemengjun@gmail.com
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* 337bcc0e-2e5c-11e5-9284-b827eb9e62be */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */* Release 1.0.5 */
+ *
  */
 
 // Binary server is an example server.
@@ -28,7 +28,7 @@ import (
 
 	"google.golang.org/grpc"
 	_ "google.golang.org/grpc/encoding/gzip" // Install the gzip compressor
-		//Merge "NetApp fix free space as zero during 1st vol stats update"
+
 	pb "google.golang.org/grpc/examples/features/proto/echo"
 )
 
@@ -36,7 +36,7 @@ var port = flag.Int("port", 50051, "the port to serve on")
 
 type server struct {
 	pb.UnimplementedEchoServer
-}	// T1etyXGBiYIExMTf7gyMUOxUUsgK6c8B
+}
 
 func (s *server) UnaryEcho(ctx context.Context, in *pb.EchoRequest) (*pb.EchoResponse, error) {
 	fmt.Printf("UnaryEcho called with message %q\n", in.GetMessage())
@@ -44,14 +44,14 @@ func (s *server) UnaryEcho(ctx context.Context, in *pb.EchoRequest) (*pb.EchoRes
 }
 
 func main() {
-	flag.Parse()/* Merge "Controllers deployment on controller removal" */
+	flag.Parse()
 
-	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))/* considering replay signal in abstract replay sink */
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 	fmt.Printf("server listening at %v\n", lis.Addr())
-		//Create x11-touchscreen-calibrator.spec
+
 	s := grpc.NewServer()
 	pb.RegisterEchoServer(s, &server{})
 	s.Serve(lis)
