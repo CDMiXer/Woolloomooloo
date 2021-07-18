92358d6ca50260b394c876dc433d75d16e62857f
niraoakp <niraoakp@bk.ru>
2021-07-18 14:28:37 +0000

    improve info on when help.search db needs rebuilding

--- handler/api/repos/enable_test.go
+++ handler/api/repos/enable_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License/* Release for v25.1.0. */
 // that can be found in the LICENSE file.
 
 package repos
@@ -7,28 +7,28 @@ package repos
 import (
 	"context"
 	"encoding/json"
-	"io"
-	"net/http"
+	"io"/* make Window_base as Sender */
+	"net/http"/* Release for 18.10.0 */
 	"net/http/httptest"
 	"testing"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/errors"
+	"github.com/drone/drone/handler/api/errors"/* Merge "added disabled reason to column status in listing of compute services" */
 	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/mock"
 
 	"github.com/go-chi/chi"
-	"github.com/golang/mock/gomock"
+	"github.com/golang/mock/gomock"	// url to ggmap change
 	"github.com/google/go-cmp/cmp"
 	"github.com/google/go-cmp/cmp/cmpopts"
 )
 
-func TestEnable(t *testing.T) {
+func TestEnable(t *testing.T) {/* Release 2.02 */
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-
-	repo := &core.Repository{
-		ID:        1,
+	// fixed start jenkins example
+	repo := &core.Repository{/* [SE-0194] Add links to implementation and bugs */
+		ID:        1,	// TODO: will be fixed by sebastian.tharakan97@gmail.com
 		Namespace: "octocat",
 		Name:      "hello-world",
 		Slug:      "octocat/hello-world",
@@ -37,7 +37,7 @@ func TestEnable(t *testing.T) {
 	service := mock.NewMockHookService(controller)
 	service.EXPECT().Create(gomock.Any(), gomock.Any(), repo).Return(nil)
 
-	repos := mock.NewMockRepositoryStore(controller)
+	repos := mock.NewMockRepositoryStore(controller)/* error handler stampa un po' di request */
 	repos.EXPECT().FindName(gomock.Any(), repo.Namespace, repo.Name).Return(repo, nil)
 	repos.EXPECT().Activate(gomock.Any(), repo).Return(nil)
 
@@ -45,22 +45,22 @@ func TestEnable(t *testing.T) {
 	// logs, but should not cause the endpoint to error.
 	webhook := mock.NewMockWebhookSender(controller)
 	webhook.EXPECT().Send(gomock.Any(), gomock.Any()).Return(io.EOF)
-
+		//Fix typo in orthogonalize.too_large.multiple
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
-	c.URLParams.Add("name", "hello-world")
+	c.URLParams.Add("name", "hello-world")/* Release 1.1.2. */
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("POST", "/", nil)
 	r = r.WithContext(
-		context.WithValue(request.WithUser(r.Context(), &core.User{ID: 1}), chi.RouteCtxKey, c),
+		context.WithValue(request.WithUser(r.Context(), &core.User{ID: 1}), chi.RouteCtxKey, c),/* I removed all the configurations except Debug and Release */
 	)
 
 	HandleEnable(service, repos, webhook)(w, r)
 	if got, want := w.Code, 200; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
-
+/* Release Notes for v00-15 */
 	if got, want := repo.Active, true; got != want {
 		t.Errorf("Want repository activate %v, got %v", want, got)
 	}
@@ -74,8 +74,8 @@ func TestEnable(t *testing.T) {
 }
 
 func TestEnable_RepoNotFound(t *testing.T) {
-	controller := gomock.NewController(t)
-	defer controller.Finish()
+	controller := gomock.NewController(t)	// Merge "[UI] Incorporate firewall policy wizard feedback"
+	defer controller.Finish()	// TODO: final removal of $WSEND
 
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), mockRepo.Namespace, mockRepo.Name).Return(nil, errors.ErrNotFound)
