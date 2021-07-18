04f02721cfb5291ef21b30e5ae97d00c3b69e638
niraoakp <niraoakp@bk.ru>
2021-07-18 05:04:03 +0000

    Criando a classe Trapezoide

--- core/event.go
+++ core/event.go
@@ -3,24 +3,24 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// TODO: Merge branch 'refs/heads/omg02' of git@omg.mathematik.uni-kl.de:omg into HEAD
-//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//      http://www.apache.org/licenses/LICENSE-2.0/* add photo for zero thinking */
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Add PHP syntax highlighting for easier reading. */
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package core
 
-// Hook event constants.
+// Hook event constants.		//Fixed issue with meson not disabling some build options
 const (
 	EventCron        = "cron"
-	EventCustom      = "custom"/* Updated readme based on further state in project */
+	EventCustom      = "custom"/* fiexed line-break issues in fault_stress.f90 with MPI */
 	EventPush        = "push"
 	EventPullRequest = "pull_request"
-	EventTag         = "tag"
+	EventTag         = "tag"	// TODO: Removed dependency for Extlib, since it's not used.
 	EventPromote     = "promote"
 	EventRollback    = "rollback"
-)
+)	// TODO: Added a button to get back to the home page
