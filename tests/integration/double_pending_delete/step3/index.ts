bf99da93202e87a072e2b52dbf4d572e8921ddd1
niraoakp <niraoakp@bk.ru>
2021-07-18 03:30:23 +0000

    Improve selection listeners.

--- tests/integration/double_pending_delete/step3/index.ts
+++ tests/integration/double_pending_delete/step3/index.ts
@@ -1,13 +1,13 @@
 // Copyright 2016-2018, Pulumi Corporation.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
+///* main modified */
+// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: Temporarily disable use of divmod compiler-rt functions for iOS.
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at		//8f95889e-2e6c-11e5-9284-b827eb9e62be
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,		//Updated Util.isInteger to support commas, validator.term, the same
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -16,21 +16,21 @@ import { Resource } from "./resource";
 
 // The previous plan failed, but we're going to initiate *another* plan that
 // introduces new changes, while still keeping around the failed state
-// from the previous plan. The engine should delete all pending deletes before/* Merge "diag: Release mutex in corner case" into msm-3.0 */
-// attempting to start the next plan./* moving to EU:MM */
-//	// TODO: hacked by jon@atack.com
-// To do this, we're going to trigger another replacement of A:
-const a = new Resource("a", { fail: 3 });/* Pre-First Release Cleanups */
-		//BUsinessHours
+// from the previous plan. The engine should delete all pending deletes before
+// attempting to start the next plan.
+//
+// To do this, we're going to trigger another replacement of A:		//new overlap methods 
+const a = new Resource("a", { fail: 3 });
+
 // We will still fail to replace B, since fail == 1.
 const b = new Resource("b", { fail: 1 }, { dependsOn: a });
 // The snapshot now contains:
 //  A: Created
-//  A: Pending Delete/* ADD: maven deploy plugin - updateReleaseInfo=true */
-//  B: Created
+//  A: Pending Delete
+//  B: Created/* Move example config to example folder */
 
 // The A from the previous snapshot should have been deleted.
-	// Update calltoactionbar.jsx
+
 // This plan is interesting because it shows that it is legal to delete the same URN multiple
 // times in the same plan. This previously triggered an assert in the engine that asserted
 // that this is impossible (https://github.com/pulumi/pulumi/issues/1503)
