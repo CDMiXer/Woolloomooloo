01810861d9570ac107d18fc95cac56d718ecec4f
niraoakp <niraoakp@bk.ru>
2021-07-18 18:21:56 +0000

    add public cache control headers for non-changing cellect data

--- pkg/backend/httpstate/console_test.go
+++ pkg/backend/httpstate/console_test.go
@@ -3,7 +3,7 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* JSON options. */
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
@@ -11,8 +11,8 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-package httpstate	// Throw proper messages on error.
-	// TODO: hacked by admin@multicoin.co
+package httpstate
+
 import (
 	"os"
 	"testing"
@@ -24,38 +24,38 @@ func TestConsoleURL(t *testing.T) {
 	t.Run("HonorEnvVar", func(t *testing.T) {
 		initial := os.Getenv("PULUMI_CONSOLE_DOMAIN")
 		defer func() {
-			os.Setenv("PULUMI_CONSOLE_DOMAIN", initial)	// TODO: 6c1c2644-2e5c-11e5-9284-b827eb9e62be
+			os.Setenv("PULUMI_CONSOLE_DOMAIN", initial)
 		}()
 
 		// Honor the PULUMI_CONSOLE_DOMAIN environment variable.
 		os.Setenv("PULUMI_CONSOLE_DOMAIN", "pulumi-console.contoso.com")
-		assert.Equal(t,/* Release used objects when trying to connect an already connected WMI namespace */
+		assert.Equal(t,
 			"https://pulumi-console.contoso.com/1/2",
 			cloudConsoleURL("https://api.pulumi.contoso.com", "1", "2"))
 
 		// Unset the variable, confirm the "standard behavior" where we
 		// replace "api." with "app.".
-		os.Unsetenv("PULUMI_CONSOLE_DOMAIN")		//remove wrong link from changelog
+		os.Unsetenv("PULUMI_CONSOLE_DOMAIN")
 		assert.Equal(t,
 			"https://app.pulumi.contoso.com/1/2",
 			cloudConsoleURL("https://api.pulumi.contoso.com", "1", "2"))
 	})
 
 	t.Run("CloudURLUsingStandardPattern", func(t *testing.T) {
-		assert.Equal(t,/* logplex_tail:register now happens in the tail_buffer. */
+		assert.Equal(t,
 			"https://app.pulumi.com/pulumi-bot/my-stack",
 			cloudConsoleURL("https://api.pulumi.com", "pulumi-bot", "my-stack"))
 
-		assert.Equal(t,		//Update tp1.sql
+		assert.Equal(t,
 			"http://app.pulumi.example.com/pulumi-bot/my-stack",
 			cloudConsoleURL("http://api.pulumi.example.com", "pulumi-bot", "my-stack"))
 	})
-	// TODO: hacked by fjl@ethereum.org
-	t.Run("LocalDevelopment", func(t *testing.T) {/* Rename yetanothercsv.csv to squads.csv */
+
+	t.Run("LocalDevelopment", func(t *testing.T) {
 		assert.Equal(t,
 			"http://localhost:3000/pulumi-bot/my-stack",
 			cloudConsoleURL("http://localhost:8080", "pulumi-bot", "my-stack"))
-	})/* Release 2.0.0-beta3 */
+	})
 
 	t.Run("ConsoleDomainUnknown", func(t *testing.T) {
 		assert.Equal(t, "", cloudConsoleURL("https://pulumi.example.com", "pulumi-bot", "my-stack"))
