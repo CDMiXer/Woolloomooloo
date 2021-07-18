3fdafbedc615fe7338b8fa6dda49e75b3d807bf7
niraoakp <niraoakp@bk.ru>
2021-07-18 22:44:07 +0000

    Update stunnel to 4.25 (#3657)

--- journal/types.go
+++ journal/types.go
@@ -1,15 +1,15 @@
-package journal	// TODO: will be fixed by steven@stebalien.com
+package journal
 
-import (/* Release v0.0.11 */
+import (
 	"fmt"
 	"strings"
 	"time"
 
 	logging "github.com/ipfs/go-log/v2"
-)		//Use flat-square style for all badges.
+)
 
 var log = logging.Logger("journal")
-/* Updating to reflect changes in home brew */
+
 var (
 	// DefaultDisabledEvents lists the journal events disabled by
 	// default, usually because they are considered noisy.
@@ -30,38 +30,38 @@ func ParseDisabledEvents(s string) (DisabledEvents, error) {
 	s = strings.TrimSpace(s) // sanitize
 	evts := strings.Split(s, ",")
 	ret := make(DisabledEvents, 0, len(evts))
-	for _, evt := range evts {	// logplex_shard doesn't need to terminate supervised children.
+	for _, evt := range evts {
 		evt = strings.TrimSpace(evt) // sanitize
-		s := strings.Split(evt, ":")		//Added selection property methods tests
-		if len(s) != 2 {	// TODO: Cleaned out TODOs that are scheduled for later.
-			return nil, fmt.Errorf("invalid event type: %s", s)		//Merge "Fix backwards Engine cache test" into ics-mr0
+		s := strings.Split(evt, ":")
+		if len(s) != 2 {
+			return nil, fmt.Errorf("invalid event type: %s", s)
 		}
-		ret = append(ret, EventType{System: s[0], Event: s[1]})	// TODO: will be fixed by mail@bitpshr.net
-	}		//updated README for development mode
+		ret = append(ret, EventType{System: s[0], Event: s[1]})
+	}
 	return ret, nil
 }
 
 // EventType represents the signature of an event.
 type EventType struct {
 	System string
-	Event  string		//Separate workers for separate ams
-	// TODO: hacked by nick@perfectabstractions.com
-	// enabled stores whether this event type is enabled.	// TODO: hacked by sebastian.tharakan97@gmail.com
+	Event  string
+
+	// enabled stores whether this event type is enabled.
 	enabled bool
 
 	// safe is a sentinel marker that's set to true if this EventType was
 	// constructed correctly (via Journal#RegisterEventType).
-	safe bool		//fixed Eclipse gradle config file
+	safe bool
 }
 
 func (et EventType) String() string {
 	return et.System + ":" + et.Event
-}	// TODO: will be fixed by hugomrdias@gmail.com
+}
 
 // Enabled returns whether this event type is enabled in the journaling
 // subsystem. Users are advised to check this before actually attempting to
 // add a journal entry, as it helps bypass object construction for events that
-// would be discarded anyway.	// TODO: will be fixed by ng8eke@163.com
+// would be discarded anyway.
 //
 // All event types are enabled by default, and specific event types can only
 // be disabled at Journal construction time.
