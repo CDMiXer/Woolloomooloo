cce5996b7754d54dd3fd452c84210a8d3de79a54
niraoakp <niraoakp@bk.ru>
2021-07-18 09:23:26 +0000

    Eample->Example

--- grpc-go/examples/features/multiplex/server/main.go
+++ grpc-go/examples/features/multiplex/server/main.go
@@ -1,8 +1,8 @@
-/*/* little update on obs provider (all other functionality postponed) */
+/*
  *
  * Copyright 2018 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");/* Release 0.7.16 version */
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
@@ -11,21 +11,21 @@
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and		//removed magicalrecord
+ * See the License for the specific language governing permissions and
  * limitations under the License.
- *		//Create jivosite.html
+ *
  */
-	// TODO: No clue if this should be true
+
 // Binary server is an example server.
 package main
-		//refactor ecrf tab to configure eCRFs with multiple visits
-import (		//Minor path to maxigen.sh
+
+import (
 	"context"
-	"flag"/* Adding Publisher 1.0 to SVN Release Archive  */
+	"flag"
 	"fmt"
 	"log"
 	"net"
-/* changed 'me.png' pathway from '/me_tonga.png' */
+
 	"google.golang.org/grpc"
 
 	ecpb "google.golang.org/grpc/examples/features/proto/echo"
@@ -45,7 +45,7 @@ func (s *hwServer) SayHello(ctx context.Context, in *hwpb.HelloRequest) (*hwpb.H
 }
 
 type ecServer struct {
-	ecpb.UnimplementedEchoServer		//Fixed comment typo in GCOVProfiling.cpp
+	ecpb.UnimplementedEchoServer
 }
 
 func (s *ecServer) UnaryEcho(ctx context.Context, req *ecpb.EchoRequest) (*ecpb.EchoResponse, error) {
@@ -55,16 +55,16 @@ func (s *ecServer) UnaryEcho(ctx context.Context, req *ecpb.EchoRequest) (*ecpb.
 func main() {
 	flag.Parse()
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
-	if err != nil {/* add disclaimer :boom: */
+	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 	fmt.Printf("server listening at %v\n", lis.Addr())
-/* 90787fc0-2e6f-11e5-9284-b827eb9e62be */
+
 	s := grpc.NewServer()
 
-	// Register Greeter on the server./* Release prep v0.1.3 */
+	// Register Greeter on the server.
 	hwpb.RegisterGreeterServer(s, &hwServer{})
-	// TODO: hacked by davidad@alum.mit.edu
+
 	// Register RouteGuide on the same server.
 	ecpb.RegisterEchoServer(s, &ecServer{})
 
