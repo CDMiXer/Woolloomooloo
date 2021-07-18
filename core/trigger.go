05e82877619395ca769fd138086b95699e773452
niraoakp <niraoakp@bk.ru>
2021-07-18 10:08:10 +0000

    add new feature description to README

--- core/trigger.go
+++ core/trigger.go
@@ -1,24 +1,24 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc./* Merge "Release 3.2.3.353 Prima WLAN Driver" */
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Delete IpfCcmBoPgLoElementUpdateResponse.java */
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0		//DocExtract: LHCHXSWG-INT report numbers
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at/* salesforce ramps */
+///* Combine value properties of parameter */
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,		//Create p81-p84.lisp
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Judge + baza = dziala :-) dostalem pierwsze ACC i WA ;-) */
+// See the License for the specific language governing permissions and/* Release 0.19.3 */
 // limitations under the License.
 
 package core
 
-import "context"
+import "context"	// TODO: Create notlar.txt
 
-// Trigger types	// TODO: Put V5/4 first, V3 later
-const (	// TODO: Delete cs_logger and associated code
-	TriggerHook = "@hook"
+// Trigger types
+const (
+	TriggerHook = "@hook"	// Added settings section
 	TriggerCron = "@cron"
 )
 
