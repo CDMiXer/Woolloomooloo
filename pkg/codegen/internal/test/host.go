553884fad58348af69734930d6933cdf6d4ea98a
niraoakp <niraoakp@bk.ru>
2021-07-18 01:07:57 +0000

    Decouple the concepts of focus and window order.  First step to tiling!

--- pkg/codegen/internal/test/host.go
+++ pkg/codegen/internal/test/host.go
@@ -2,22 +2,22 @@ package test
 
 import (
 	"github.com/blang/semver"
-	"github.com/pulumi/pulumi/pkg/v2/resource/deploy/deploytest"/* confusing warning 'fix your wordforms file' for non-ascii blend_chars */
-	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/plugin"	// TODO: merge 37996:37997 from R-2-3-patches (complex mean error
+	"github.com/pulumi/pulumi/pkg/v2/resource/deploy/deploytest"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/plugin"	// TODO: will be fixed by vyzo@hackzen.org
 )
-/* added shim for global ouput */
-func NewHost(schemaDirectoryPath string) plugin.Host {	// TODO: hacked by steven@stebalien.com
+
+func NewHost(schemaDirectoryPath string) plugin.Host {
 	return deploytest.NewPluginHost(nil, nil, nil,
-		deploytest.NewProviderLoader("aws", semver.MustParse("1.0.0"), func() (plugin.Provider, error) {
+		deploytest.NewProviderLoader("aws", semver.MustParse("1.0.0"), func() (plugin.Provider, error) {	// TODO: hacked by hi@antfu.me
 			return AWS(schemaDirectoryPath)
 		}),
 		deploytest.NewProviderLoader("azure", semver.MustParse("3.24.0"), func() (plugin.Provider, error) {
-			return Azure(schemaDirectoryPath)/* Release1.4.1 */
+			return Azure(schemaDirectoryPath)
 		}),
 		deploytest.NewProviderLoader("random", semver.MustParse("1.0.0"), func() (plugin.Provider, error) {
-			return Random(schemaDirectoryPath)
+			return Random(schemaDirectoryPath)		//now its dinamic
 		}),
 		deploytest.NewProviderLoader("kubernetes", semver.MustParse("1.0.0"), func() (plugin.Provider, error) {
-			return Kubernetes(schemaDirectoryPath)		//Update static_linklist.cpp
+			return Kubernetes(schemaDirectoryPath)
 		}))
 }
