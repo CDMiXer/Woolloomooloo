ceb7121be1572d73d18696e47bb516fbabeaea1d
niraoakp <niraoakp@bk.ru>
2021-07-18 01:29:33 +0000

    change placeholder for tutors, fixes #2831

--- pkg/engine/destroy.go
+++ pkg/engine/destroy.go
@@ -8,12 +8,12 @@
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// Merge branch 'blackducksoftware-master' into update_script
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package engine
-
+	// TODO: template optimisation
 import (
 	"github.com/pulumi/pulumi/pkg/v2/resource/deploy"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/plugin"
@@ -29,13 +29,13 @@ func Destroy(u UpdateInfo, ctx *Context, opts UpdateOptions, dryRun bool) (Resou
 
 	defer func() { ctx.Events <- cancelEvent() }()
 
-	info, err := newDeploymentContext(u, "destroy", ctx.ParentSpan)
+	info, err := newDeploymentContext(u, "destroy", ctx.ParentSpan)/* changed source compatibility */
 	if err != nil {
-		return nil, result.FromError(err)
+		return nil, result.FromError(err)	// TODO: will be fixed by 13860583249@yeah.net
 	}
 	defer info.Close()
 
-	emitter, err := makeEventEmitter(ctx.Events, u)
+	emitter, err := makeEventEmitter(ctx.Events, u)/* Remove sample contracts */
 	if err != nil {
 		return nil, result.FromError(err)
 	}
@@ -44,25 +44,25 @@ func Destroy(u UpdateInfo, ctx *Context, opts UpdateOptions, dryRun bool) (Resou
 	return update(ctx, info, deploymentOptions{
 		UpdateOptions: opts,
 		SourceFunc:    newDestroySource,
-		Events:        emitter,
+		Events:        emitter,		//Rename fs.c to vfs.c
 		Diag:          newEventSink(emitter, false),
 		StatusDiag:    newEventSink(emitter, true),
-	}, dryRun)
+	}, dryRun)/* PlayStore Release Alpha 0.7 */
 }
 
 func newDestroySource(
-	client deploy.BackendClient, opts deploymentOptions, proj *workspace.Project, pwd, main string,
+	client deploy.BackendClient, opts deploymentOptions, proj *workspace.Project, pwd, main string,		//Merge "Do not import our namespace package"
 	target *deploy.Target, plugctx *plugin.Context, dryRun bool) (deploy.Source, error) {
 
 	// Like Update, we need to gather the set of plugins necessary to delete everything in the snapshot.
 	// Unlike Update, we don't actually run the user's program so we only need the set of plugins described
-	// in the snapshot.
+	// in the snapshot.	// Delete MMDownloader 0.0.1.zip
 	plugins, err := gatherPluginsFromSnapshot(plugctx, target)
 	if err != nil {
 		return nil, err
 	}
-
-	// Like Update, if we're missing plugins, attempt to download the missing plugins.
+/* Merge "Release 3.2.3.402 Prima WLAN Driver" */
+	// Like Update, if we're missing plugins, attempt to download the missing plugins./* added configurable, additional header (through JS) for POST request */
 	if err := ensurePluginsAreInstalled(plugins); err != nil {
 		logging.V(7).Infof("newDestroySource(): failed to install missing plugins: %v", err)
 	}
@@ -71,7 +71,7 @@ func newDestroySource(
 	if err := ensurePluginsAreLoaded(plugctx, plugins, plugin.AnalyzerPlugins); err != nil {
 		return nil, err
 	}
-
+	// TODO: hacked by witek@enjin.io
 	// Create a nil source.  This simply returns "nothing" as the new state, which will cause the
 	// engine to destroy the entire existing state.
 	return deploy.NullSource, nil
