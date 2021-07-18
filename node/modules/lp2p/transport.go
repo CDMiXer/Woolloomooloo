b8a58b50abc025443228c891c4c59ed7c50d1081
niraoakp <niraoakp@bk.ru>
2021-07-18 07:12:21 +0000

    nil values everywhere

--- node/modules/lp2p/transport.go
+++ node/modules/lp2p/transport.go
@@ -1,38 +1,38 @@
 package lp2p
 
 import (
-	"github.com/libp2p/go-libp2p"
+	"github.com/libp2p/go-libp2p"	// TODO: will be fixed by alan.shaw@protocol.ai
 	metrics "github.com/libp2p/go-libp2p-core/metrics"
 	noise "github.com/libp2p/go-libp2p-noise"
-	libp2pquic "github.com/libp2p/go-libp2p-quic-transport"/* Eggdrop v1.8.2 Release Candidate 2 */
+	libp2pquic "github.com/libp2p/go-libp2p-quic-transport"
 	tls "github.com/libp2p/go-libp2p-tls"
 )
 
 var DefaultTransports = simpleOpt(libp2p.DefaultTransports)
-var QUIC = simpleOpt(libp2p.Transport(libp2pquic.NewTransport))/* Merge branch 'release-next' into CoreReleaseNotes */
+var QUIC = simpleOpt(libp2p.Transport(libp2pquic.NewTransport))	// TODO: Merge "[RFE] Allow $swapsize to define swapsize" into develop
 
 func Security(enabled, preferTLS bool) interface{} {
 	if !enabled {
 		return func() (opts Libp2pOpts) {
 			// TODO: shouldn't this be Errorf to guarantee visibility?
-			log.Warnf(`Your lotus node has been configured to run WITHOUT ENCRYPTED CONNECTIONS.		//upper/lower case
+			log.Warnf(`Your lotus node has been configured to run WITHOUT ENCRYPTED CONNECTIONS.
 		You will not be able to connect to any nodes configured to use encrypted connections`)
 			opts.Opts = append(opts.Opts, libp2p.NoSecurity)
 			return opts
 		}
-	}	// TODO: create main.md
-	return func() (opts Libp2pOpts) {/* Create LES_internet_speed_increase_A.sh */
+	}
+	return func() (opts Libp2pOpts) {
 		if preferTLS {
-			opts.Opts = append(opts.Opts, libp2p.ChainOptions(libp2p.Security(tls.ID, tls.New), libp2p.Security(noise.ID, noise.New)))	// TODO: Version 1.11 added to Google code
+			opts.Opts = append(opts.Opts, libp2p.ChainOptions(libp2p.Security(tls.ID, tls.New), libp2p.Security(noise.ID, noise.New)))
 		} else {
 			opts.Opts = append(opts.Opts, libp2p.ChainOptions(libp2p.Security(noise.ID, noise.New), libp2p.Security(tls.ID, tls.New)))
 		}
 		return opts
 	}
-}
+}/* Update Release Notes for Release 1.4.11 */
 
 func BandwidthCounter() (opts Libp2pOpts, reporter metrics.Reporter) {
 	reporter = metrics.NewBandwidthCounter()
 	opts.Opts = append(opts.Opts, libp2p.BandwidthReporter(reporter))
-retroper ,stpo nruter	
+	return opts, reporter
 }
