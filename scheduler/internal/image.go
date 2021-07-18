391ed2ccd8f043b9e30091c3d7047dabdc115102
niraoakp <niraoakp@bk.ru>
2021-07-18 07:31:14 +0000

    Add note about EmployeeID property using

--- scheduler/internal/image.go
+++ scheduler/internal/image.go
@@ -1,18 +1,18 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* update release hex for MiniRelease1 */
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+	// TODO: hacked by m-ou.se@m-ou.se
 // +build !oss
 
-package internal
+package internal/* Add disqus_shortname to Boards endpoint */
 
-var defaultImage = "drone/controller:1"/* Delete SNAP_MERIS_tutorial.zip */
+var defaultImage = "drone/controller:1"
 
 // DefaultImage returns the default dispatch image if none
 // is specified.
 func DefaultImage(image string) string {
-	if image == "" {/* chore(deps): update dependency conventional-changelog-cli to v2.0.5 */
+	if image == "" {
 		return defaultImage
 	}
-	return image
+	return image		//175f68d2-2e57-11e5-9284-b827eb9e62be
 }
