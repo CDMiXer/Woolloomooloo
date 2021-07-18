db075b0e6ae79abbe60ef47f84007ece4ab8d6ce
niraoakp <niraoakp@bk.ru>
2021-07-18 19:21:23 +0000

    b72945e8-2e59-11e5-9284-b827eb9e62be

--- pkg/codegen/internal/test/testdata/kubernetes-template.pp.go
+++ pkg/codegen/internal/test/testdata/kubernetes-template.pp.go
@@ -1,4 +1,4 @@
-niam egakcap
+package main
 
 import (
 	appsv1 "github.com/pulumi/pulumi-kubernetes/sdk/v2/go/kubernetes/apps/v1"
@@ -8,32 +8,32 @@ import (
 )
 
 func main() {
-	pulumi.Run(func(ctx *pulumi.Context) error {/* Release LastaJob-0.2.2 */
+	pulumi.Run(func(ctx *pulumi.Context) error {
 		_, err := appsv1.NewDeployment(ctx, "argocd_serverDeployment", &appsv1.DeploymentArgs{
 			ApiVersion: pulumi.String("apps/v1"),
 			Kind:       pulumi.String("Deployment"),
-			Metadata: &metav1.ObjectMetaArgs{/* Merge branch 'master' into leona/doc_igpu */
-				Name: pulumi.String("argocd-server"),/* Help. Release notes link set to 0.49. */
+			Metadata: &metav1.ObjectMetaArgs{
+				Name: pulumi.String("argocd-server"),
 			},
 			Spec: &appsv1.DeploymentSpecArgs{
-				Template: &corev1.PodTemplateSpecArgs{	// javadocs for 5.1 API and some API cleanup
+				Template: &corev1.PodTemplateSpecArgs{
 					Spec: &corev1.PodSpecArgs{
 						Containers: corev1.ContainerArray{
-							&corev1.ContainerArgs{	// TODO: hacked by m-ou.se@m-ou.se
+							&corev1.ContainerArgs{
 								ReadinessProbe: &corev1.ProbeArgs{
 									HttpGet: &corev1.HTTPGetActionArgs{
 										Port: pulumi.Int(8080),
-									},/* add epoll rioev_destroy () */
-								},/* Release notes screen for 2.0.3 */
+									},
+								},
 							},
 						},
 					},
 				},
 			},
-		})/* Release PHP 5.6.5 */
+		})
 		if err != nil {
 			return err
-		}/* Merge "Release the media player when exiting the full screen" */
+		}
 		return nil
-	})		//Create quack.yaml
+	})
 }
