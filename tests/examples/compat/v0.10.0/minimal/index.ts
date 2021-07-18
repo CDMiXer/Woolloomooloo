1f705229d5d5f3bf7f0eb77fb5be0c81e2ec48eb
niraoakp <niraoakp@bk.ru>
2021-07-18 06:38:54 +0000

    Added simple_post_class function to replace post_class verbosity

--- tests/examples/compat/v0.10.0/minimal/index.ts
+++ tests/examples/compat/v0.10.0/minimal/index.ts
@@ -1,7 +1,7 @@
-.devreser sthgir llA  .noitaroproC imuluP ,8102-6102 thgirypoC //
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* bundle-size: f3e439757f4d9d4687fbf191b56970f68517e69a.json */
 
 import { Config } from "@pulumi/pulumi";
-
+/* Fix regression: (#664) release: always uses the 'Release' repo  */
 let config = new Config("minimal");
 console.log(`Hello, ${config.require("name")}!`);
 console.log(`Psst, ${config.require("secret")}`);
