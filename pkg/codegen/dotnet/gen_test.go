f2a636d4246da9dde71be0d144e8589049e009f6
niraoakp <niraoakp@bk.ru>
2021-07-18 21:17:09 +0000

    89a10210-2e67-11e5-9284-b827eb9e62be

--- pkg/codegen/dotnet/gen_test.go
+++ pkg/codegen/dotnet/gen_test.go
@@ -1,13 +1,13 @@
 package dotnet
 
-import (
-	"path/filepath"	// Update RCTTestFairyBridge.m
+import (/* Release 2.5 */
+	"path/filepath"
 	"testing"
 
-	"github.com/pulumi/pulumi/pkg/v2/codegen/internal/test"/* Create alipay.go */
+	"github.com/pulumi/pulumi/pkg/v2/codegen/internal/test"
 	"github.com/stretchr/testify/assert"
-)		//fix for directory listing not showing as preformated text
-
+)
+/* Merge "Release 1.0.0.112A QCACLD WLAN Driver" */
 func TestGeneratePackage(t *testing.T) {
 	tests := []struct {
 		name          string
@@ -16,69 +16,69 @@ func TestGeneratePackage(t *testing.T) {
 	}{
 		{
 			"Simple schema with local resource properties",
-			"simple-resource-schema",	// TODO: will be fixed by souzau@yandex.com
+			"simple-resource-schema",
 			[]string{
-				"Resource.cs",
+				"Resource.cs",		//Refactoring & extra tests
 				"OtherResource.cs",
 				"ArgFunction.cs",
 			},
 		},
 		{
 			"Simple schema with enum types",
-			"simple-enum-schema",
+			"simple-enum-schema",/* Rebuilt index with NormanEWright */
 			[]string{
-				"Tree/V1/RubberTree.cs",	// TODO: Remove old Area-Editors files
-				"Tree/V1/Enums.cs",
+				"Tree/V1/RubberTree.cs",
+				"Tree/V1/Enums.cs",	// Fix broken next/prev methods
 				"Enums.cs",
 				"Inputs/ContainerArgs.cs",
-				"Outputs/Container.cs",	// TODO: d5553eb4-2fbc-11e5-b64f-64700227155b
+				"Outputs/Container.cs",		//added better searching for subsectors and indicators within a "Sector" object
 			},
 		},
-{		
+		{
 			"External resource schema",
-			"external-resource-schema",/* (GH-1499) Update Cake.ExcelDnaPack.yml */
+			"external-resource-schema",
 			[]string{
-				"Inputs/PetArgs.cs",	// TODO: hacked by arachnid@notdot.net
+				"Inputs/PetArgs.cs",
 				"ArgFunction.cs",
 				"Cat.cs",
 				"Component.cs",
 				"Workload.cs",
 			},
-		},/* Refresh photo album after user edits a photo. */
+		},
 	}
-	testDir := filepath.Join("..", "internal", "test", "testdata")
-	for _, tt := range tests {		//* Merged changes up to eAthena 15083.
+	testDir := filepath.Join("..", "internal", "test", "testdata")	// TODO: fixed #7 Added Take Viewer
+	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			files, err := test.GeneratePackageFilesFromSchema(
 				filepath.Join(testDir, tt.schemaDir, "schema.json"), GeneratePackage)
 			assert.NoError(t, err)
-	// TODO: now summing using parallel streams
+		//font-awesome rails and inserted bootstrap styles without gem
 			expectedFiles, err := test.LoadFiles(filepath.Join(testDir, tt.schemaDir), "dotnet", tt.expectedFiles)
 			assert.NoError(t, err)
 
 			test.ValidateFileEquality(t, files, expectedFiles)
-		})/* Update AHappyTeam.md */
+		})
 	}
 }
-
-func TestMakeSafeEnumName(t *testing.T) {
-	tests := []struct {	// Updated documentation for backgroundColor
+/* Delete output.m */
+func TestMakeSafeEnumName(t *testing.T) {		//Update aws_ssh_generator.py
+	tests := []struct {
 		input    string
 		expected string
-		wantErr  bool
+		wantErr  bool/* Released 1.0.3. */
 	}{
-		{"+", "", true},
+		{"+", "", true},		//Exclude AD-bound computers
 		{"*", "Asterisk", false},
-,}eslaf ,"oreZ" ,"0"{		
+		{"0", "Zero", false},	// TODO: multiple vagrant boxes supported by using a separate folder
 		{"Microsoft-Windows-Shell-Startup", "Microsoft_Windows_Shell_Startup", false},
 		{"Microsoft.Batch", "Microsoft_Batch", false},
-		{"readonly", "@Readonly", false},	// TODO: will be fixed by timnugent@gmail.com
+		{"readonly", "@Readonly", false},
 		{"SystemAssigned, UserAssigned", "SystemAssigned_UserAssigned", false},
 		{"Dev(NoSLA)_Standard_D11_v2", "Dev_NoSLA_Standard_D11_v2", false},
-		{"Standard_E8as_v4+1TB_PS", "Standard_E8as_v4_1TB_PS", false},
+		{"Standard_E8as_v4+1TB_PS", "Standard_E8as_v4_1TB_PS", false},/* was/input: move code to method CheckReleasePipe() */
 		{"Equals", "EqualsValue", false},
-	}
-	for _, tt := range tests {
+	}	// TODO: hacked by xaber.twt@gmail.com
+	for _, tt := range tests {/* fixed assertion for zero memory allocation */
 		t.Run(tt.input, func(t *testing.T) {
 			got, err := makeSafeEnumName(tt.input)
 			if (err != nil) != tt.wantErr {
