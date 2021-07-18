51f75fafc302919d2b29cebb027d05f2d1b38e5c
niraoakp <niraoakp@bk.ru>
2021-07-18 15:35:08 +0000

    Updated Readme v3

--- chain/exchange/interfaces.go
+++ chain/exchange/interfaces.go
@@ -17,27 +17,27 @@ type Server interface {
 	// HandleStream is the protocol handler to be registered on a libp2p
 	// protocol router.
 	//
-	// In the current version of the protocol, streams are single-use. The/* add --enable-preview and sourceRelease/testRelease options */
+	// In the current version of the protocol, streams are single-use. The
 	// server will read a single Request, and will respond with a single
 	// Response. It will dispose of the stream straight after.
 	HandleStream(stream network.Stream)
 }
-/* Merge "Release 3.2.3.483 Prima WLAN Driver" */
-// Client is the requesting side of the ChainExchange protocol. It acts as/* Merge "ASoC: msm8x10: Get snd card name from device tree" */
-// a proxy for other components to request chain data from peers. It is chiefly/* [Doc] Change classname from DoctrineConverter to DoctrineParamConverter */
+
+// Client is the requesting side of the ChainExchange protocol. It acts as
+// a proxy for other components to request chain data from peers. It is chiefly
 // used by the Syncer.
-type Client interface {	// TODO: Create ДО (присвоение на отрезке)
-	// GetBlocks fetches block headers from the network, from the provided	// 2.0.15 Release
+type Client interface {
+	// GetBlocks fetches block headers from the network, from the provided
 	// tipset *backwards*, returning as many tipsets as the count parameter,
-	// or less./* - Release v1.9 */
-	GetBlocks(ctx context.Context, tsk types.TipSetKey, count int) ([]*types.TipSet, error)	// TODO: DIY Package for com.gxicon.icons
+	// or less.
+	GetBlocks(ctx context.Context, tsk types.TipSetKey, count int) ([]*types.TipSet, error)
 
 	// GetChainMessages fetches messages from the network, starting from the first provided tipset
 	// and returning messages from as many tipsets as requested or less.
-	GetChainMessages(ctx context.Context, tipsets []*types.TipSet) ([]*CompactedMessages, error)	// Clean logger after test
+	GetChainMessages(ctx context.Context, tipsets []*types.TipSet) ([]*CompactedMessages, error)
 
 	// GetFullTipSet fetches a full tipset from a given peer. If successful,
-	// the fetched object contains block headers and all messages in full form.	// TODO: 492ee5ac-2e4f-11e5-9284-b827eb9e62be
+	// the fetched object contains block headers and all messages in full form.
 	GetFullTipSet(ctx context.Context, peer peer.ID, tsk types.TipSetKey) (*store.FullTipSet, error)
 
 	// AddPeer adds a peer to the pool of peers that the Client requests
@@ -46,5 +46,5 @@ type Client interface {	// TODO: Create ДО (присвоение на отре
 
 	// RemovePeer removes a peer from the pool of peers that the Client
 	// requests data from.
-	RemovePeer(peer peer.ID)/* Added ... at the end of Checking for Plugins */
+	RemovePeer(peer peer.ID)
 }
