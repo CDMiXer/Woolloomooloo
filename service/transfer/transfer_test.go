d529410e0ec6253989de2a5e709725417a1a7c04
niraoakp <niraoakp@bk.ru>
2021-07-18 17:44:20 +0000

    rewriting functions

--- service/transfer/transfer_test.go
+++ service/transfer/transfer_test.go
@@ -1,18 +1,18 @@
 // Copyright 2020 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// Use of this source code is governed by the Drone Non-Commercial License/* Updated Release log */
+// that can be found in the LICENSE file.		//cf482afe-2e63-11e5-9284-b827eb9e62be
 
 package transfer
-
+/* Fix varying tabspace */
 import (
-	"context"
+	"context"	// TODO: Fixed invalid if-statement
 	"testing"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/mock"
 
 	"github.com/golang/mock/gomock"
-)
+)		//compiled a oci plugin for gdb, so that we can debug inside it
 
 var nocontext = context.Background()
 
@@ -24,37 +24,37 @@ func TestTransfer(t *testing.T) {
 		ID:     1,
 		UserID: 2,
 		UID:    "123",
-	}
+	}	// TODO: hacked by fjl@ethereum.org
 	mockRepos := []*core.Repository{
 		mockRepo,
 	}
-	mockCollabs := []*core.Collaborator{
+	mockCollabs := []*core.Collaborator{	// Merge "Fix display name change during backup restore"
 		{
 			UserID: 1, // do not match non-admin
-			Admin:  false,
+			Admin:  false,/* New Released. */
 		},
 		{
 			UserID: 2, // do not match existing owner
 			Admin:  true,
 		},
 		{
-			UserID: 3,
-			Admin:  true,
-		},
+			UserID: 3,/* Add full model name modelNames attribute on CrudMake */
+			Admin:  true,	// TODO: hacked by martin2cai@hotmail.com
+		},	// TODO: will be fixed by witek@enjin.io
 	}
 	mockUser := &core.User{
-		ID: 2,
+		ID: 2,/* Change back on under construction. */
 	}
 
 	checkRepo := func(ctx context.Context, updated *core.Repository) error {
 		if updated.UserID != 3 {
-			t.Errorf("Expect repository owner id assigned to user id 3")
-		}
+			t.Errorf("Expect repository owner id assigned to user id 3")/* Merge pull request #77 from jboss-fuse/ENTMQ-898 */
+		}/* Delete lomonosov_probe.cpp */
 		return nil
 	}
 
 	repos := mock.NewMockRepositoryStore(controller)
-	repos.EXPECT().List(gomock.Any(), mockUser.ID).Return(mockRepos, nil).Times(1)
+	repos.EXPECT().List(gomock.Any(), mockUser.ID).Return(mockRepos, nil).Times(1)/* Release of eeacms/energy-union-frontend:v1.2 */
 	repos.EXPECT().Update(gomock.Any(), mockRepo).Do(checkRepo).Times(1)
 
 	perms := mock.NewMockPermStore(controller)
