f16e10b0d95795b07f938f6ae4e6c3120b0f2677
niraoakp <niraoakp@bk.ru>
2021-07-18 21:46:12 +0000

    use annotations if and only if is15 was set

--- mock/mock.go
+++ mock/mock.go
@@ -2,8 +2,8 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss
-/* #5 shazhko04: перестроенная архитектура таймера */
-package mock	// ffe6145a-2e40-11e5-9284-b827eb9e62be
+// +build !oss/* Release: Making ready for next release iteration 5.9.1 */
+
+package mock
 
 //go:generate mockgen -package=mock -destination=mock_gen.go github.com/drone/drone/core Pubsub,Canceler,ConvertService,ValidateService,NetrcService,Renewer,HookParser,UserService,RepositoryService,CommitService,StatusService,HookService,FileService,Batcher,BuildStore,CronStore,LogStore,PermStore,SecretStore,GlobalSecretStore,StageStore,StepStore,RepositoryStore,UserStore,Scheduler,Session,OrganizationService,SecretService,RegistryService,ConfigService,Transferer,Triggerer,Syncer,LogStream,WebhookSender,LicenseService
