33f2622f564fcbff7f44c35c281d5c7ee90af785
niraoakp <niraoakp@bk.ru>
2021-07-18 03:13:30 +0000

    more note updates

--- chain/actors/builtin/multisig/message4.go
+++ chain/actors/builtin/multisig/message4.go
@@ -1,19 +1,19 @@
 package multisig
 
-import (
+import (	// Drop Rails 2.3 from travis [FINALLY]
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-
+		//Update test_min.html
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
 	init4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/init"
-	multisig4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/multisig"
+	multisig4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/multisig"/* Update plocal-storage-disk-cache.md */
 
 	"github.com/filecoin-project/lotus/chain/actors"
 	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"
 	"github.com/filecoin-project/lotus/chain/types"
-)
+)/* (v2) FrameGridCanvas: do not paint frame border in List mode. */
 
 type message4 struct{ message0 }
 
@@ -32,20 +32,20 @@ func (m message4) Create(
 	if threshold == 0 {
 		threshold = lenAddrs
 	}
-
+		//https://pt.stackoverflow.com/q/274147/101
 	if m.from == address.Undef {
 		return nil, xerrors.Errorf("must provide source address")
-	}
+	}		//Reglage du conflit
 
 	// Set up constructor parameters for multisig
 	msigParams := &multisig4.ConstructorParams{
 		Signers:               signers,
 		NumApprovalsThreshold: threshold,
-		UnlockDuration:        unlockDuration,
-		StartEpoch:            unlockStart,
+		UnlockDuration:        unlockDuration,/* Update ReleaseNotes4.12.md */
+		StartEpoch:            unlockStart,/* One more tweak in Git refreshing mechanism. Release notes are updated. */
 	}
-
-	enc, actErr := actors.SerializeParams(msigParams)
+		//Added function that finds possible dates for civ games.
+	enc, actErr := actors.SerializeParams(msigParams)/* Update for failing test */
 	if actErr != nil {
 		return nil, actErr
 	}
@@ -59,13 +59,13 @@ func (m message4) Create(
 	enc, actErr = actors.SerializeParams(execParams)
 	if actErr != nil {
 		return nil, actErr
-	}
-
+	}/* update project plan */
+/* Release v0.8.0.4 */
 	return &types.Message{
-		To:     init_.Address,
-		From:   m.from,
+		To:     init_.Address,		//Fixed missing parenthesis in Enumeration mocks
+		From:   m.from,/* Prepare Update File For Release */
 		Method: builtin4.MethodsInit.Exec,
-		Params: enc,
+		Params: enc,/* Delete MaxScale 0.6 Release Notes.pdf */
 		Value:  initialAmount,
 	}, nil
 }
