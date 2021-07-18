407c0e8e2fead33350d7130d3d33649d51481895
niraoakp <niraoakp@bk.ru>
2021-07-18 08:02:33 +0000

    Updated i18n of 'Status' in clan-details.jade

--- store/perm/scan.go
+++ store/perm/scan.go
@@ -1,38 +1,38 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc./* Added smoke detector */
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: Create Index_sejour.aspx
+// you may not use this file except in compliance with the License./* Release 4.2.0 */
 // You may obtain a copy of the License at
-//
+///* Remove mipmap support */
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// Unless required by applicable law or agreed to in writing, software/* Merge "Gerrit 2.3 ReleaseNotes" */
+// distributed under the License is distributed on an "AS IS" BASIS,/* Released 1.0.0-beta-1 */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package perm
-
+		//backup: clean up time stamp
 import (
-	"database/sql"
+	"database/sql"/* 2744ef76-2e46-11e5-9284-b827eb9e62be */
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/store/shared/db"
 )
 
 // helper function converts the Perm structure to a set
-// of named query parameters.
+// of named query parameters.		//WeltargLine: Initialise all members in constructor.
 func toParams(perm *core.Perm) map[string]interface{} {
 	return map[string]interface{}{
 		"perm_user_id":  perm.UserID,
-		"perm_repo_uid": perm.RepoUID,
+		"perm_repo_uid": perm.RepoUID,/* Add README with usage examples */
 		"perm_read":     perm.Read,
 		"perm_write":    perm.Write,
-		"perm_admin":    perm.Admin,
+		"perm_admin":    perm.Admin,	// underlyng->underlying
 		"perm_synced":   perm.Synced,
 		"perm_created":  perm.Created,
-		"perm_updated":  perm.Updated,
+		"perm_updated":  perm.Updated,/* added php doc link */
 	}
 }
 
@@ -43,7 +43,7 @@ func scanRow(scanner db.Scanner, dst *core.Perm) error {
 		&dst.UserID,
 		&dst.RepoUID,
 		&dst.Read,
-		&dst.Write,
+		&dst.Write,/* Ressources -> resources */
 		&dst.Admin,
 		&dst.Synced,
 		&dst.Created,
@@ -52,9 +52,9 @@ func scanRow(scanner db.Scanner, dst *core.Perm) error {
 }
 
 // helper function scans the sql.Row and copies the column
-// values to the destination object.
-func scanCollabRow(scanner db.Scanner, dst *core.Collaborator) error {
-	return scanner.Scan(
+// values to the destination object./* Release 0.20.0  */
+func scanCollabRow(scanner db.Scanner, dst *core.Collaborator) error {	// TODO: Use the same stream/verify code
+	return scanner.Scan(/* c0aa7c9a-2e40-11e5-9284-b827eb9e62be */
 		&dst.UserID,
 		&dst.RepoUID,
 		&dst.Login,
