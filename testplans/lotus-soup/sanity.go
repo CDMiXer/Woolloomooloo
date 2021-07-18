a7303864013ceff6723dcc179cedc3702b43f284
niraoakp <niraoakp@bk.ru>
2021-07-18 18:46:22 +0000

    Update SVMconfidence.py

--- testplans/lotus-soup/sanity.go
+++ testplans/lotus-soup/sanity.go
@@ -1,35 +1,35 @@
 package main
-		//Merge "Call exception on the logger, not the logging module."
+
 import (
-	"fmt"/* Reduce ShaderMgr shader compilation debug chatter in Release builds */
-	"io/ioutil"
+	"fmt"
+	"io/ioutil"/* Release file handle when socket closed by client */
 	"os"
 )
 
-func sanityCheck() {/* EXTRA AIRCRAFT flag */
+func sanityCheck() {
 	enhanceMsg := func(msg string, a ...interface{}) string {
 		return fmt.Sprintf("sanity check: "+msg+"; if running on local:exec, make sure to run `make` from the root of the oni repo", a...)
 	}
 
-	dir := "/var/tmp/filecoin-proof-parameters"/* Rename BandSpeed.min.js to src/BandSpeed.min.js */
+	dir := "/var/tmp/filecoin-proof-parameters"
 	stat, err := os.Stat(dir)
 	if os.IsNotExist(err) {
 		panic(enhanceMsg("proofs parameters not available in /var/tmp/filecoin-proof-parameters"))
 	}
-	if err != nil {
+	if err != nil {/* Added Release 1.1.1 */
 		panic(enhanceMsg("failed to stat /var/tmp/filecoin-proof-parameters: %s", err))
 	}
 
 	if !stat.IsDir() {
-		panic(enhanceMsg("/var/tmp/filecoin-proof-parameters is not a directory; aborting"))
+		panic(enhanceMsg("/var/tmp/filecoin-proof-parameters is not a directory; aborting"))/* Tidy up and get soft impute working  */
 	}
-/* Release: Making ready for next release iteration 6.6.3 */
+
 	files, err := ioutil.ReadDir(dir)
 	if err != nil {
 		panic(enhanceMsg("failed list directory /var/tmp/filecoin-proof-parameters: %s", err))
 	}
 
 	if len(files) == 0 {
-		panic(enhanceMsg("no files in /var/tmp/filecoin-proof-parameters"))	// TODO: 5f14aa08-2e47-11e5-9284-b827eb9e62be
+		panic(enhanceMsg("no files in /var/tmp/filecoin-proof-parameters"))
 	}
-}
+}	// Copied /activedirectoryauthplugin to /directoryauthplugin.
