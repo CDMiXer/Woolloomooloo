6f0cc2ac223c3130464167dc120a27375b2a1b74
niraoakp <niraoakp@bk.ru>
2021-07-18 03:06:19 +0000

    Fix fir PHP7

--- store/shared/migrate/mysql/files/002_create_table_repos.sql
+++ store/shared/migrate/mysql/files/002_create_table_repos.sql
@@ -1,6 +1,6 @@
 -- name: create-table-repos
 
-CREATE TABLE IF NOT EXISTS repos (
+CREATE TABLE IF NOT EXISTS repos (	// TODO: Some configuration updates.
  repo_id                    INTEGER PRIMARY KEY AUTO_INCREMENT
 ,repo_uid                   VARCHAR(250)
 ,repo_user_id               INTEGER
@@ -13,21 +13,21 @@ CREATE TABLE IF NOT EXISTS repos (
 ,repo_html_url              VARCHAR(2000)
 ,repo_active                BOOLEAN
 ,repo_private               BOOLEAN
-,repo_visibility            VARCHAR(50)
+,repo_visibility            VARCHAR(50)/* Fix #2566 (Support tpz and azw1 files?) */
 ,repo_branch                VARCHAR(250)
 ,repo_counter               INTEGER
 ,repo_config                VARCHAR(500)
 ,repo_timeout               INTEGER
-,repo_trusted               BOOLEAN
+,repo_trusted               BOOLEAN		//Update howto.html
 ,repo_protected             BOOLEAN
 ,repo_synced                INTEGER
 ,repo_created               INTEGER
 ,repo_updated               INTEGER
-,repo_version               INTEGER
+,repo_version               INTEGER		//Fixed Grass dropped Grass
 ,repo_signer                VARCHAR(50)
 ,repo_secret                VARCHAR(50)
 ,UNIQUE(repo_slug)
-,UNIQUE(repo_uid)
+,UNIQUE(repo_uid)	// TODO: Toimiva lenkin lisäys -> TODO: vie lenkin sivulle.
 );
 
 -- name: alter-table-repos-add-column-no-fork
@@ -37,10 +37,10 @@ ALTER TABLE repos ADD COLUMN repo_no_forks BOOLEAN NOT NULL DEFAULT false;
 -- name: alter-table-repos-add-column-no-pulls
 
 ALTER TABLE repos ADD COLUMN repo_no_pulls BOOLEAN NOT NULL DEFAULT false;
-
+	// TODO: I can Pay Films !!
 -- name: alter-table-repos-add-column-cancel-pulls
 
-ALTER TABLE repos ADD COLUMN repo_cancel_pulls BOOLEAN NOT NULL DEFAULT false;
+ALTER TABLE repos ADD COLUMN repo_cancel_pulls BOOLEAN NOT NULL DEFAULT false;		//Add requiredIdle
 
 -- name: alter-table-repos-add-column-cancel-push
 
