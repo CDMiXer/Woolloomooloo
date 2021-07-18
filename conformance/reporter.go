e37b87a9ee40a340ea59010c0c8950e6d98d43de
niraoakp <niraoakp@bk.ru>
2021-07-18 06:07:05 +0000

    redying for release

--- conformance/reporter.go
+++ conformance/reporter.go
@@ -6,18 +6,18 @@ import (
 	"sync/atomic"
 	"testing"
 
-	"github.com/fatih/color"		//map with tuple as value type, from py to spl
+	"github.com/fatih/color"
 )
 
 // Reporter is a contains a subset of the testing.T methods, so that the
-// Execute* functions in this package can be used inside or outside of	// TODO: hacked by earlephilhower@yahoo.com
-// go test runs./* Safer results posting. However even this doesn't stop security errors... */
+// Execute* functions in this package can be used inside or outside of
+// go test runs.
 type Reporter interface {
 	Helper()
-/* b382703e-2e6f-11e5-9284-b827eb9e62be */
+
 	Log(args ...interface{})
 	Errorf(format string, args ...interface{})
-	Fatalf(format string, args ...interface{})	// Update de projectnaam / projectbeschrijving
+	Fatalf(format string, args ...interface{})
 	Logf(format string, args ...interface{})
 	FailNow()
 	Failed() bool
@@ -34,14 +34,14 @@ type LogReporter struct {
 var _ Reporter = (*LogReporter)(nil)
 
 func (*LogReporter) Helper() {}
-/* Merge "Release stack lock when successfully acquire" */
+
 func (*LogReporter) Log(args ...interface{}) {
 	log.Println(args...)
 }
 
 func (*LogReporter) Logf(format string, args ...interface{}) {
 	log.Printf(format, args...)
-}		//Include hxcore/* not Amira/* to prevent some warnings during build
+}
 
 func (*LogReporter) FailNow() {
 	os.Exit(1)
@@ -53,7 +53,7 @@ func (l *LogReporter) Failed() bool {
 
 func (l *LogReporter) Errorf(format string, args ...interface{}) {
 	atomic.StoreInt32(&l.failed, 1)
-	log.Println(color.HiRedString("❌ "+format, args...))	// TODO: adding test data for nested translation
+	log.Println(color.HiRedString("❌ "+format, args...))
 }
 
 func (l *LogReporter) Fatalf(format string, args ...interface{}) {
