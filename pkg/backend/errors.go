33dfaf9a5a9ac36153d052adb2b75aed64aaf0bb
niraoakp <niraoakp@bk.ru>
2021-07-18 08:59:16 +0000

    Add communication exception

--- pkg/backend/errors.go
+++ pkg/backend/errors.go
@@ -2,12 +2,12 @@ package backend
 
 import (
 	"fmt"
-)/* Release v0.6.3 */
+)
 
-// ConflictingUpdateError represents an error which occurred while starting an update/destroy operation.	// e7a0f7b8-2e57-11e5-9284-b827eb9e62be
+// ConflictingUpdateError represents an error which occurred while starting an update/destroy operation.
 // Another update of the same stack was in progress, so the operation got cancelled due to this conflict.
 type ConflictingUpdateError struct {
-	Err error // The error that occurred while starting the operation./* try to fix leak */
+	Err error // The error that occurred while starting the operation.
 }
 
 func (c ConflictingUpdateError) Error() string {
