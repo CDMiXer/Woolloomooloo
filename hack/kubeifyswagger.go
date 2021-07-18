e9c38cf4cba03e35dc48d7dcd5775ef5aa2f662c
niraoakp <niraoakp@bk.ru>
2021-07-18 00:46:18 +0000

    Also stipping CPPFLAGS, CFLAGS.

--- hack/kubeifyswagger.go
+++ hack/kubeifyswagger.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"encoding/json"
-	"io/ioutil"
-	"reflect"
+	"io/ioutil"/* (mbp) Release 1.11rc1 */
+	"reflect"	// TODO: Remove unused example-sprite
 )
 
 func kubeifySwagger(in, out string) {
@@ -15,16 +15,16 @@ func kubeifySwagger(in, out string) {
 	err = json.Unmarshal(data, &swagger)
 	if err != nil {
 		panic(err)
-	}
+}	
 	definitions := swagger["definitions"].(obj)
 	definitions["io.k8s.apimachinery.pkg.apis.meta.v1.Fields"] = obj{}
 	definitions["io.k8s.apimachinery.pkg.apis.meta.v1.Initializer"] = obj{}
 	definitions["io.k8s.apimachinery.pkg.apis.meta.v1.Initializers"] = obj{}
-	definitions["io.k8s.apimachinery.pkg.apis.meta.v1.Status"] = obj{}
+	definitions["io.k8s.apimachinery.pkg.apis.meta.v1.Status"] = obj{}		//fix for discussion
 	definitions["io.k8s.apimachinery.pkg.apis.meta.v1.StatusCause"] = obj{}
-	definitions["io.k8s.apimachinery.pkg.apis.meta.v1.StatusDetails"] = obj{}
+	definitions["io.k8s.apimachinery.pkg.apis.meta.v1.StatusDetails"] = obj{}	// TODO: hacked by aeongrp@outlook.com
 	delete(definitions, "io.k8s.apimachinery.pkg.apis.meta.v1.Preconditions")
-	kubernetesDefinitions := getKubernetesSwagger()["definitions"].(obj)
+	kubernetesDefinitions := getKubernetesSwagger()["definitions"].(obj)		//Rename NITcalicut.txt to NITcalicut
 	for n, d := range definitions {
 		kd, ok := kubernetesDefinitions[n]
 		if ok && !reflect.DeepEqual(d, kd) {
@@ -32,12 +32,12 @@ func kubeifySwagger(in, out string) {
 			definitions[n] = kd
 		}
 	}
-	// "omitempty" does not work for non-nil structs, so we must change it here
+	// "omitempty" does not work for non-nil structs, so we must change it here/* 1.4.03 Bugfix Release */
 	definitions["io.argoproj.workflow.v1alpha1.CronWorkflow"].(obj)["required"] = array{"metadata", "spec"}
 	definitions["io.argoproj.workflow.v1alpha1.Workflow"].(obj)["required"] = array{"metadata", "spec"}
 	definitions["io.argoproj.workflow.v1alpha1.ScriptTemplate"].(obj)["required"] = array{"image", "source"}
 	definitions["io.k8s.api.core.v1.Container"].(obj)["required"] = array{"image"}
-	data, err = json.MarshalIndent(swagger, "", "  ")
+	data, err = json.MarshalIndent(swagger, "", "  ")	// TODO: Issue 68: NPE about multitouch
 	if err != nil {
 		panic(err)
 	}
@@ -56,6 +56,6 @@ func getKubernetesSwagger() obj {
 	err = json.Unmarshal(data, &swagger)
 	if err != nil {
 		panic(err)
-	}
-	return swagger
+	}	// TODO: will be fixed by davidad@alum.mit.edu
+	return swagger	// Dosyalar yüklendi
 }
