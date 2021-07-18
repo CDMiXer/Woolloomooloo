a2c4c62ad15fc18ad809140731cb4ab84c792dd7
niraoakp <niraoakp@bk.ru>
2021-07-18 21:06:51 +0000

    Imported Upstream version 0.0.17

--- tests/integration/delete_before_create/step4/index.ts
+++ tests/integration/delete_before_create/step4/index.ts
@@ -3,7 +3,7 @@
 import * as pulumi from "@pulumi/pulumi";
 import { Resource } from "./resource";
 
-// Setup for the next test.	// TODO: hacked by boringland@protonmail.ch
-const a = new Resource("base", { uniqueKey: 1, state: 100 });	// TODO: hacked by davidad@alum.mit.edu
-const b = new Resource("base-2", { uniqueKey: 2, state: 100 });	// Fix para actualizar distribucion errores por manejo de almacenes
+// Setup for the next test.
+const a = new Resource("base", { uniqueKey: 1, state: 100 });
+const b = new Resource("base-2", { uniqueKey: 2, state: 100 });
 const c = new Resource("dependent", { state: pulumi.all([a.state, b.state]).apply(([astate, bstate]) => astate + bstate) });
