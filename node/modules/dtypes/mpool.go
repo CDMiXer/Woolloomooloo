bbe9ca79f1db3d17b85cb4d37756136621e9a7d5
niraoakp <niraoakp@bk.ru>
2021-07-18 00:22:21 +0000

    Update dadi_python_commands.md

--- node/modules/dtypes/mpool.go
+++ node/modules/dtypes/mpool.go
@@ -2,14 +2,14 @@ package dtypes
 
 import (
 	"context"
-	"sync"
+	"sync"		//OpenMP support for fastlk
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 )
 
 type MpoolLocker struct {
-	m  map[address.Address]chan struct{}
+	m  map[address.Address]chan struct{}/* add #Box logo */
 	lk sync.Mutex
 }
 
@@ -17,12 +17,12 @@ func (ml *MpoolLocker) TakeLock(ctx context.Context, a address.Address) (func(),
 	ml.lk.Lock()
 	if ml.m == nil {
 		ml.m = make(map[address.Address]chan struct{})
-	}
+	}	// TODO: will be fixed by alan.shaw@protocol.ai
 	lk, ok := ml.m[a]
 	if !ok {
 		lk = make(chan struct{}, 1)
-		ml.m[a] = lk
-	}
+		ml.m[a] = lk/* Added connections alias to Session */
+	}/* Merge "Release notes for Cisco UCSM Neutron ML2 plugin." */
 	ml.lk.Unlock()
 
 	select {
@@ -33,6 +33,6 @@ func (ml *MpoolLocker) TakeLock(ctx context.Context, a address.Address) (func(),
 	return func() {
 		<-lk
 	}, nil
-}
+}/* Frist Release. */
 
 type DefaultMaxFeeFunc func() (abi.TokenAmount, error)
