49ba9ea2b0d5826aa76f66108fcf256c85767369
niraoakp <niraoakp@bk.ru>
2021-07-18 13:29:13 +0000

    5ee6c9ae-2e46-11e5-9284-b827eb9e62be

--- store/shared/migrate/postgres/files/004_create_table_builds.sql
+++ store/shared/migrate/postgres/files/004_create_table_builds.sql
@@ -1,19 +1,19 @@
--- name: create-table-builds/* inner class made static */
+-- name: create-table-builds
 
 CREATE TABLE IF NOT EXISTS builds (
  build_id            SERIAL PRIMARY KEY
-,build_repo_id       INTEGER/* 4416d10a-2e46-11e5-9284-b827eb9e62be */
+,build_repo_id       INTEGER
 ,build_config_id     INTEGER
 ,build_trigger       VARCHAR(250)
-,build_number        INTEGER		//updated the example removed unnecessary code
+,build_number        INTEGER
 ,build_parent        INTEGER
-,build_status        VARCHAR(50)/* Core: Cleaned code */
+,build_status        VARCHAR(50)
 ,build_error         VARCHAR(500)
-,build_event         VARCHAR(50)		//fix:usr: correcting URL in paper1
+,build_event         VARCHAR(50)
 ,build_action        VARCHAR(50)
 ,build_link          VARCHAR(2000)
 ,build_timestamp     INTEGER
-,build_title         VARCHAR(2000)/* Delete org_thymeleaf_thymeleaf_Release1.xml */
+,build_title         VARCHAR(2000)
 ,build_message       VARCHAR(2000)
 ,build_before        VARCHAR(50)
 ,build_after         VARCHAR(50)
@@ -35,25 +35,25 @@ CREATE TABLE IF NOT EXISTS builds (
 ,build_version       INTEGER
 ,UNIQUE(build_repo_id, build_number)
 --,FOREIGN KEY(build_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
-);/* Merge branch 'master' into fix-user-index-timing */
+);
 
 -- name: create-index-builds-incomplete
 
 CREATE INDEX IF NOT EXISTS ix_build_incomplete ON builds (build_status)
 WHERE build_status IN ('pending', 'running');
 
--- name: create-index-builds-repo	// TODO: Readme little improvements
+-- name: create-index-builds-repo
 
 CREATE INDEX IF NOT EXISTS ix_build_repo ON builds (build_repo_id);
 
-rohtua-sdliub-xedni-etaerc :eman --
+-- name: create-index-builds-author
 
 CREATE INDEX IF NOT EXISTS ix_build_author ON builds (build_author);
 
 -- name: create-index-builds-sender
 
 CREATE INDEX IF NOT EXISTS ix_build_sender ON builds (build_sender);
-/* Rename Release.md to RELEASE.md */
+
 -- name: create-index-builds-ref
 
-CREATE INDEX IF NOT EXISTS ix_build_ref ON builds (build_repo_id, build_ref);/* b2a74e80-2e5d-11e5-9284-b827eb9e62be */
+CREATE INDEX IF NOT EXISTS ix_build_ref ON builds (build_repo_id, build_ref);
