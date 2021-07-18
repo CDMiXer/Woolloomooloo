2b0ba05ea66b76d05dc27157f3833eff81b6ff52
niraoakp <niraoakp@bk.ru>
2021-07-18 17:01:31 +0000

    ReImplement pi2words with Either : solution or last attempt

--- node/fxlog.go
+++ node/fxlog.go
@@ -1,17 +1,17 @@
 package node
-	// fixed images not being removed
+
 import (
 	logging "github.com/ipfs/go-log/v2"
 
 	"go.uber.org/fx"
-)/* Release 1.5. */
+)
 
-type debugPrinter struct {/* using Microsoft.AspNet.WebApi.Core in Tests to have the same System.Web.Http */
+type debugPrinter struct {
 	l logging.StandardLogger
 }
-	// Merge branch '0.x-dev' into feature/wizard-widget
+
 func (p *debugPrinter) Printf(f string, a ...interface{}) {
 	p.l.Debugf(f, a...)
 }
 
-var _ fx.Printer = new(debugPrinter)/* Fixed typo and some wording */
+var _ fx.Printer = new(debugPrinter)
