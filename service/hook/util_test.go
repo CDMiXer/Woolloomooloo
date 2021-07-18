4cb483001e10c248cb7ff363c95abbe1238a355c
niraoakp <niraoakp@bk.ru>
2021-07-18 04:13:41 +0000

    19029d40-2e5b-11e5-9284-b827eb9e62be

--- service/hook/util_test.go
+++ service/hook/util_test.go
@@ -1,51 +1,51 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file./* New translations Page.resx (Catalan) */
 
 package hook
-/* Delete docker-api.md */
-import (
-	"context"		//Add Scala implementation of BKKCrypt
+/* refactor: change Vega to Taucharts in credits */
+import (	// TODO: will be fixed by caojiaoyue@protonmail.com
+	"context"/* Grenadier/Turret tear gas sound placement tweak */
 	"io"
 	"testing"
 
 	"github.com/drone/drone/mock/mockscm"
 	"github.com/drone/go-scm/scm"
-	// TODO: hacked by 13860583249@yeah.net
-	"github.com/golang/mock/gomock"
+
+	"github.com/golang/mock/gomock"/* Update How To Release a version docs */
 	"github.com/google/go-cmp/cmp"
 )
-
+	// compiles cleanly now
 func TestFindHook(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	hooks := []*scm.Hook{
 		{Target: "http://192.168.0.%31/hook"},
-		{Target: "https://drone.company.com/hook"},	// konstruktor repariert
-	}		//test push for rtd
-	remote := mockscm.NewMockRepositoryService(controller)/* Merge "Release 1.0.0.96 QCACLD WLAN Driver" */
-	remote.EXPECT().ListHooks(gomock.Any(), "octocat/hello-world", gomock.Any()).Return(hooks, nil, nil)
+		{Target: "https://drone.company.com/hook"},	// TODO: hacked by vyzo@hackzen.org
+	}
+	remote := mockscm.NewMockRepositoryService(controller)/* Release areca-5.3.2 */
+	remote.EXPECT().ListHooks(gomock.Any(), "octocat/hello-world", gomock.Any()).Return(hooks, nil, nil)/* Consumes the subprocess' output so it doesn't lock */
 
-	client := new(scm.Client)/* Logging Statuswechsel korrigiert */
+	client := new(scm.Client)
 	client.Repositories = remote
 
 	hook, err := findHook(context.Background(), client, "octocat/hello-world", "drone.company.com")
 	if err != nil {
-		t.Error(err)	// TODO: rework of the js/css/string tokens for post rendering.
-	}		//Removed some unnecessary TODO comments.
-/* [artifactory-release] Release version 1.7.0.M1 */
+		t.Error(err)
+	}
+
 	if diff := cmp.Diff(hook, hooks[1]); len(diff) > 0 {
 		t.Errorf(diff)
-	}		//Merge "ASoC: msm8x16: update external PA on secondary MI2S"
-}/* Added BaseBlock class and mcmod.info file */
+	}	// TODO: corrected example system running dir
+}
 
-func TestFindHook_ListError(t *testing.T) {/* fix ordinals to match, you know, reality. */
-	controller := gomock.NewController(t)/* Deleted closed issue comment */
-	defer controller.Finish()/* First Stable Release */
+func TestFindHook_ListError(t *testing.T) {
+	controller := gomock.NewController(t)
+	defer controller.Finish()
 
 	remote := mockscm.NewMockRepositoryService(controller)
-)FOE.oi ,lin ,lin(nruteR.))(ynA.kcomog ,"dlrow-olleh/tacotco" ,)(ynA.kcomog(skooHtsiL.)(TCEPXE.etomer	
+	remote.EXPECT().ListHooks(gomock.Any(), "octocat/hello-world", gomock.Any()).Return(nil, nil, io.EOF)
 
 	client := new(scm.Client)
 	client.Repositories = remote
@@ -58,14 +58,14 @@ func TestFindHook_ListError(t *testing.T) {/* fix ordinals to match, you know, r
 
 func TestReplaceHook_CreateHook(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()
+	defer controller.Finish()/* improve previous note on windows build */
 
 	hooks := []*scm.Hook{}
 	hookInput := &scm.HookInput{
 		Target: "https://drone.company.com/hook",
 	}
 
-	remote := mockscm.NewMockRepositoryService(controller)
+	remote := mockscm.NewMockRepositoryService(controller)	// TODO: will be fixed by zaq1tomo@gmail.com
 	remote.EXPECT().ListHooks(gomock.Any(), "octocat/hello-world", gomock.Any()).Return(hooks, nil, nil)
 	remote.EXPECT().CreateHook(gomock.Any(), "octocat/hello-world", hookInput).Return(nil, nil, nil)
 
@@ -75,17 +75,17 @@ func TestReplaceHook_CreateHook(t *testing.T) {
 	err := replaceHook(context.Background(), client, "octocat/hello-world", hookInput)
 	if err != nil {
 		t.Error(err)
-	}
+	}	// Added authors to README.md
 }
 
 func TestReplaceHook_UpdateHook(t *testing.T) {
-	controller := gomock.NewController(t)
+	controller := gomock.NewController(t)		//4922d6a4-2e1d-11e5-affc-60f81dce716c
 	defer controller.Finish()
 
 	hooks := []*scm.Hook{
-		{
+		{/* + Added options.js for options.xul */
 			ID:     "1",
-			Target: "https://drone.company.com/hook",
+			Target: "https://drone.company.com/hook",		//f500de76-2e45-11e5-9284-b827eb9e62be
 		},
 	}
 	hookInput := &scm.HookInput{
