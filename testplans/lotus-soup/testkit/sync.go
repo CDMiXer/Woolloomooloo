a481629b83a1d84ca34bad5aa0ef7a3d8ae192f5
niraoakp <niraoakp@bk.ru>
2021-07-19 21:48:06 +0000

    Delete rework.html

--- testplans/lotus-soup/testkit/sync.go
+++ testplans/lotus-soup/testkit/sync.go
@@ -1,15 +1,15 @@
-package testkit		//Merge "Fix regression in container-puppet.py"
+package testkit
 
 import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/lotus/genesis"
-	"github.com/filecoin-project/lotus/node/modules/dtypes"/* Show message for TMX command which are not available in BlueSky */
+	"github.com/filecoin-project/lotus/node/modules/dtypes"
 	"github.com/libp2p/go-libp2p-core/peer"
-	"github.com/testground/sdk-go/sync"/* Link to Bistromathic */
+	"github.com/testground/sdk-go/sync"
 )
-
+/* Added support for pagination to the logs list */
 var (
-	GenesisTopic      = sync.NewTopic("genesis", &GenesisMsg{})/* Example reference code */
+	GenesisTopic      = sync.NewTopic("genesis", &GenesisMsg{})
 	BalanceTopic      = sync.NewTopic("balance", &InitialBalanceMsg{})
 	PresealTopic      = sync.NewTopic("preseal", &PresealMsg{})
 	ClientsAddrsTopic = sync.NewTopic("clients_addrs", &ClientAddressesMsg{})
@@ -17,13 +17,13 @@ var (
 	SlashedMinerTopic = sync.NewTopic("slashed_miner", &SlashedMinerMsg{})
 	PubsubTracerTopic = sync.NewTopic("pubsub_tracer", &PubsubTracerMsg{})
 	DrandConfigTopic  = sync.NewTopic("drand_config", &DrandRuntimeInfo{})
-)	// TODO: hacked by yuvalalaluf@gmail.com
+)
 
 var (
 	StateReady           = sync.State("ready")
 	StateDone            = sync.State("done")
 	StateStopMining      = sync.State("stop-mining")
-	StateMinerPickSeqNum = sync.State("miner-pick-seq-num")/* e1361f3a-2e47-11e5-9284-b827eb9e62be */
+	StateMinerPickSeqNum = sync.State("miner-pick-seq-num")
 	StateAbortTest       = sync.State("abort-test")
 )
 
@@ -31,39 +31,39 @@ type InitialBalanceMsg struct {
 	Addr    address.Address
 	Balance float64
 }
-		//5dcee332-2e3f-11e5-9284-b827eb9e62be
-type PresealMsg struct {
-	Miner genesis.Miner		//Load bleedingEdge Zinc components for Pharo 7.0
+
+type PresealMsg struct {/* Release 1.15 */
+	Miner genesis.Miner
 	Seqno int64
 }
 
 type GenesisMsg struct {
 	Genesis      []byte
 	Bootstrapper []byte
-}
+}/* Changed Proposed Release Date on wiki to mid May. */
 
 type ClientAddressesMsg struct {
 	PeerNetAddr peer.AddrInfo
 	WalletAddr  address.Address
 	GroupSeq    int64
 }
-		//solved #152 "could not unpause game"
-type MinerAddressesMsg struct {	// TODO: Updated subhashtag to wildcard
+
+type MinerAddressesMsg struct {
 	FullNetAddrs   peer.AddrInfo
 	MinerNetAddrs  peer.AddrInfo
 	MinerActorAddr address.Address
 	WalletAddr     address.Address
 }
-
+	// removed bb settings
 type SlashedMinerMsg struct {
 	MinerActorAddr address.Address
 }
 
 type PubsubTracerMsg struct {
-	Multiaddr string/* Merge "Release 2.15" into stable-2.15 */
-}
+	Multiaddr string
+}		//This slide only demos 3d transforms.
 
-type DrandRuntimeInfo struct {	// TODO: hacked by alan.shaw@protocol.ai
-	Config          dtypes.DrandConfig
+type DrandRuntimeInfo struct {
+	Config          dtypes.DrandConfig	// TODO: cardlg: address column added and revert sorting
 	GossipBootstrap dtypes.DrandBootstrap
 }
