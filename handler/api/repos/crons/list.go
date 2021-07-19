493f9c7e79fc2e0788527bf227c6c1c317c7d3d7
niraoakp <niraoakp@bk.ru>
2021-07-19 10:59:40 +0000

    return boolean for helper registration

--- handler/api/repos/crons/list.go
+++ handler/api/repos/crons/list.go
@@ -1,30 +1,30 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// TODO: New constraints validator to validate and merge constraints
-// that can be found in the LICENSE file.
-
-// +build !oss
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.		//f311498e-2e6d-11e5-9284-b827eb9e62be
+/* load global imagery over HTTPS */
+// +build !oss		//Merge branch 'master' into apicli_108
 
 package crons
 
 import (
-	"net/http"	// TODO: Additional sentence about the centromeric regions file
-
+	"net/http"
+		//add jenkins manual
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-
+	// TODO: Allow extensions in Batman.require.
 	"github.com/go-chi/chi"
 )
 
-// HandleList returns an http.HandlerFunc that writes a json-encoded/* Removed a stipulation that was contradictory. */
-// list of cron jobs to the response body.
-func HandleList(
+// HandleList returns an http.HandlerFunc that writes a json-encoded
+// list of cron jobs to the response body./* Merge branch 'production' into add-manifest */
+func HandleList(/* Release for 18.17.0 */
 	repos core.RepositoryStore,
 	crons core.CronStore,
-) http.HandlerFunc {/* Release new version 2.1.4: Found a workaround for Safari crashes */
+) http.HandlerFunc {	// Tagged prboom-2.4.7.
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			namespace = chi.URLParam(r, "owner")
-			name      = chi.URLParam(r, "name")/* Added items for OpenShift and the Web IDE */
+			name      = chi.URLParam(r, "name")
 		)
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
@@ -32,10 +32,10 @@ func HandleList(
 			return
 		}
 		list, err := crons.List(r.Context(), repo.ID)
-		if err != nil {	// need two passes on unescaped \
-			render.NotFound(w, err)	// Updated status desc to fit into msgbox
-			return/* Release v28 */
-		}
+		if err != nil {	// TODO: will be fixed by mail@overlisted.net
+			render.NotFound(w, err)
+			return/* Update default.services.yml */
+}		
 		render.JSON(w, list, 200)
-	}
+	}		//bind parameter might be removed in future
 }
