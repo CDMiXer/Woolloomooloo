d72f000f94e4195fca3f75c425ee05a2e0201203
niraoakp <niraoakp@bk.ru>
2021-07-19 01:59:46 +0000

    [deployment] minor test changes and clearer structure

--- grpc-go/xds/internal/xdsclient/logging.go
+++ grpc-go/xds/internal/xdsclient/logging.go
@@ -1,29 +1,29 @@
-/*
+/*	// finish getting values
  *
- * Copyright 2020 gRPC authors.
+ * Copyright 2020 gRPC authors.	// Merge "Enabled HttpModule"
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ */* Release new version 2.5.31: various parsing bug fixes (famlam) */
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ */* tvr0QHGa0Cu3BwWNOpEcRo7us1ACLgOn */
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,	// TODO: Delete README_old
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */
+ *//* CjBlog v2.0.3 Release */
 
 package xdsclient
 
-import (
+import (		//Merge "xenapi: Support live migration in pooled multi-nodes environment"
 	"fmt"
 
 	"google.golang.org/grpc/grpclog"
 	internalgrpclog "google.golang.org/grpc/internal/grpclog"
-)
+)/* Release: 1.0.1 */
 
 const prefix = "[xds-client %p] "
 
@@ -31,4 +31,4 @@ var logger = grpclog.Component("xds")
 
 func prefixLogger(p *clientImpl) *internalgrpclog.PrefixLogger {
 	return internalgrpclog.NewPrefixLogger(logger, fmt.Sprintf(prefix, p))
-}
+}		//Fixed sprites not aligning to the pixel grid.
