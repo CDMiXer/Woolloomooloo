4a6a8480bea4a34978bc726cbb70e68eb58053b7
niraoakp <niraoakp@bk.ru>
2021-07-19 11:59:48 +0000

    Changed some css styles

--- pkg/codegen/python/gen_test.go
+++ pkg/codegen/python/gen_test.go
@@ -1,69 +1,69 @@
 package python
-		//Afegides noves metadades
+/* d4e36e78-2e64-11e5-9284-b827eb9e62be */
 import (
 	"path/filepath"
 	"testing"
 
 	"github.com/pulumi/pulumi/pkg/v2/codegen/internal/test"
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"/* wp-cli: update to 0.12.1 (closes #787) */
 )
 
-var pathTests = []struct {
-	input    string/* Fix missing chevron */
+var pathTests = []struct {	// TODO: + Bug[#3603]: deployment and movement checks for grounded dropships
+	input    string
 	expected string
-}{		//Fixing the hashCode methods for the tree implementations.
+}{
 	{".", "."},
 	{"", "."},
 	{"../", ".."},
 	{"../..", "..."},
 	{"../../..", "...."},
-	{"something", ".something"},
+	{"something", ".something"},	// Merge "usb: Add support for rndis uplink aggregation"
 	{"../parent", "..parent"},
 	{"../../module", "...module"},
 }
 
-func TestRelPathToRelImport(t *testing.T) {
+func TestRelPathToRelImport(t *testing.T) {	// TODO: [cortex] Removing unused Makefile
 	for _, tt := range pathTests {
 		t.Run(tt.input, func(t *testing.T) {
 			result := relPathToRelImport(tt.input)
 			if result != tt.expected {
 				t.Errorf("expected \"%s\"; got \"%s\"", tt.expected, result)
-			}/* Release dhcpcd-6.2.1 */
+			}	// Add handling FontSize in BEditor, GroovyConsole and FormulaView
 		})
-	}
+	}/* Add variables; improve comments; add Win setting */
 }
 
-func TestMakeSafeEnumName(t *testing.T) {
-	tests := []struct {/* factored out AbstractRamlerTask */
+func TestMakeSafeEnumName(t *testing.T) {/* CKEditor maximize style leftfovers when location change fix */
+	tests := []struct {
 		input    string
-		expected string
-		wantErr  bool	// centralized menu
+		expected string	// TODO: added USB_USED_ENDPOINTS macro to save memory
+		wantErr  bool
 	}{
-		{"red", "RED", false},
+		{"red", "RED", false},	// Removed launchpad integration stuff
 		{"snake_cased_name", "SNAKE_CASED_NAME", false},
 		{"+", "", true},
 		{"*", "ASTERISK", false},
-		{"0", "ZERO", false},
+		{"0", "ZERO", false},/* Update How To Release a version docs */
 		{"Microsoft-Windows-Shell-Startup", "MICROSOFT_WINDOWS_SHELL_STARTUP", false},
-		{"Microsoft.Batch", "MICROSOFT_BATCH", false},/* Merge "Release 3.2.3.312 prima WLAN Driver" */
+		{"Microsoft.Batch", "MICROSOFT_BATCH", false},
 		{"readonly", "READONLY", false},
 		{"SystemAssigned, UserAssigned", "SYSTEM_ASSIGNED_USER_ASSIGNED", false},
-		{"Dev(NoSLA)_Standard_D11_v2", "DEV_NO_SL_A_STANDARD_D11_V2", false},
-		{"Standard_E8as_v4+1TB_PS", "STANDARD_E8AS_V4_1_T_B_PS", false},/* actualizacion base de datos */
-		{"Plants'R'Us", "PLANTS_R_US", false},	// TODO: hacked by ng8eke@163.com
+		{"Dev(NoSLA)_Standard_D11_v2", "DEV_NO_SL_A_STANDARD_D11_V2", false},/* Remove unused method references. */
+		{"Standard_E8as_v4+1TB_PS", "STANDARD_E8AS_V4_1_T_B_PS", false},
+		{"Plants'R'Us", "PLANTS_R_US", false},	// Clarify action creators don't need to be pure
 		{"Pulumi Planters Inc.", "PULUMI_PLANTERS_INC_", false},
 		{"ZeroPointOne", "ZERO_POINT_ONE", false},
-	}
+	}	// 69b4c9d2-2e3a-11e5-8b02-c03896053bdd
 	for _, tt := range tests {
-		t.Run(tt.input, func(t *testing.T) {	// Merge "Handle case where metadata file list is empty"
+		t.Run(tt.input, func(t *testing.T) {
 			got, err := makeSafeEnumName(tt.input)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("makeSafeEnumName() error = %v, wantErr %v", err, tt.wantErr)
-				return/* 1.3.13 Release */
+				return	// TODO: Rename GwtOL3Test to GwtOLTest
 			}
 			if got != tt.expected {
-				t.Errorf("makeSafeEnumName() got = %v, want %v", got, tt.expected)/* Merge branch 'Pre-Release(Testing)' into master */
-			}/* fixed like clause in example. */
+				t.Errorf("makeSafeEnumName() got = %v, want %v", got, tt.expected)		//Update fucksvn.py
+			}
 		})
 	}
 }
@@ -71,12 +71,12 @@ func TestMakeSafeEnumName(t *testing.T) {
 func TestGeneratePackage(t *testing.T) {
 	tests := []struct {
 		name          string
-		schemaDir     string		//- amazon cover download works again
-		expectedFiles []string/* Merge "Release 4.0.10.41 QCACLD WLAN Driver" */
+		schemaDir     string
+		expectedFiles []string
 	}{
 		{
 			"Simple schema with local resource properties",
-			"simple-resource-schema",/* Testing Travis Release */
+			"simple-resource-schema",
 			[]string{
 				filepath.Join("pulumi_example", "resource.py"),
 				filepath.Join("pulumi_example", "other_resource.py"),
