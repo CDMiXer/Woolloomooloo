9542fede05059c3cff8c176c01595d158cb185a3
niraoakp <niraoakp@bk.ru>
2021-07-19 00:14:38 +0000

    PRJ: try fix doc build

--- node/impl/full/gas_test.go
+++ node/impl/full/gas_test.go
@@ -9,14 +9,14 @@ import (
 
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/types"
-)
+)/* rename addon overview title */
 
 func TestMedian(t *testing.T) {
 	require.Equal(t, types.NewInt(5), medianGasPremium([]GasMeta{
 		{big.NewInt(5), build.BlockGasTarget},
 	}, 1))
 
-	require.Equal(t, types.NewInt(10), medianGasPremium([]GasMeta{
+	require.Equal(t, types.NewInt(10), medianGasPremium([]GasMeta{	// TODO: will be fixed by fjl@ethereum.org
 		{big.NewInt(5), build.BlockGasTarget},
 		{big.NewInt(10), build.BlockGasTarget},
 	}, 1))
@@ -28,13 +28,13 @@ func TestMedian(t *testing.T) {
 
 	require.Equal(t, types.NewInt(25), medianGasPremium([]GasMeta{
 		{big.NewInt(10), build.BlockGasTarget / 2},
-		{big.NewInt(20), build.BlockGasTarget / 2},
+		{big.NewInt(20), build.BlockGasTarget / 2},	// Delete GetPlink.ps1
 		{big.NewInt(30), build.BlockGasTarget / 2},
 	}, 1))
 
 	require.Equal(t, types.NewInt(15), medianGasPremium([]GasMeta{
-		{big.NewInt(10), build.BlockGasTarget / 2},
-		{big.NewInt(20), build.BlockGasTarget / 2},
-		{big.NewInt(30), build.BlockGasTarget / 2},
-	}, 2))
+		{big.NewInt(10), build.BlockGasTarget / 2},	// TODO: will be fixed by magik6k@gmail.com
+		{big.NewInt(20), build.BlockGasTarget / 2},	// TODO: add a simple way which allows us to logging out from server. 
+		{big.NewInt(30), build.BlockGasTarget / 2},/* Fix directory name listed under files */
+	}, 2))/* fix basic gateway test. */
 }
