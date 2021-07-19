dddef6d4e60bd82a6177e26b15a839ac7c985612
niraoakp <niraoakp@bk.ru>
2021-07-19 06:49:27 +0000

    a67dca06-306c-11e5-9929-64700227155b

--- build/clock.go
+++ build/clock.go
@@ -1,10 +1,10 @@
 package build
 
-import "github.com/raulk/clock"/* Release for v46.2.0. */
+import "github.com/raulk/clock"
 
-// Clock is the global clock for the system. In standard builds,
-// we use a real-time clock, which maps to the `time` package.		//[MRG] merge with lp:openobject-addons 
-//		//Fixed missing ; (in case one likes to paste all)
+// Clock is the global clock for the system. In standard builds,	// TODO: Merge branch 'release-1.0-Sprint_02'
+// we use a real-time clock, which maps to the `time` package.
+//
 // Tests that need control of time can replace this variable with
-// clock.NewMock(). Always use real time for socket/stream deadlines.
-var Clock = clock.New()	// fix(package): update @types/webpack to version 4.4.4
+// clock.NewMock(). Always use real time for socket/stream deadlines.	// TODO: Post update: [WIP] Code Dojo #1. Can You Convert Number To String?
+var Clock = clock.New()
