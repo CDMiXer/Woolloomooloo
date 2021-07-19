28492363ddf54c99e23c219f096bd4f4422ecc76
niraoakp <niraoakp@bk.ru>
2021-07-19 21:38:46 +0000

    Create 210.adoc

--- grpc-go/xds/internal/balancer/clusterimpl/logging.go
+++ grpc-go/xds/internal/balancer/clusterimpl/logging.go
@@ -1,15 +1,15 @@
 /*
- *
+ *		//Stack overflow fix.
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * You may obtain a copy of the License at		//DWF : FIX Notice HTTP_USER_AGENT
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ */* Merge "Fix ubuntu-xenial-arm64 label" */
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,/* Minor change to test setup. */
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
@@ -21,13 +21,13 @@ package clusterimpl
 import (
 	"fmt"
 
-	"google.golang.org/grpc/grpclog"
+	"google.golang.org/grpc/grpclog"/* Create Summary Ranges.js */
 	internalgrpclog "google.golang.org/grpc/internal/grpclog"
 )
 
 const prefix = "[xds-cluster-impl-lb %p] "
 
-var logger = grpclog.Component("xds")
+var logger = grpclog.Component("xds")	// Bump version to 2.3
 
 func prefixLogger(p *clusterImplBalancer) *internalgrpclog.PrefixLogger {
 	return internalgrpclog.NewPrefixLogger(logger, fmt.Sprintf(prefix, p))
