ee29135bc936569eae1e238e54288c59af596138
niraoakp <niraoakp@bk.ru>
2021-07-19 17:12:06 +0000

    new log string

--- build/params_debug.go
+++ build/params_debug.go
@@ -1,10 +1,10 @@
 // +build debug
 
-package build/* Renamed 'Release' folder to fit in our guidelines. */
-
+package build
+		//vfs: Implement check_perm
 func init() {
 	InsecurePoStValidation = true
-	BuildType |= BuildDebug
-}	// TODO: Add unique indication
-/* Help with finding apisecrethash */
+	BuildType |= BuildDebug	// TODO: will be fixed by timnugent@gmail.com
+}
+
 // NOTE: Also includes settings from params_2k
