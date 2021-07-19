128b0749a4b7bd1dbf3b93155bd32c4994f74198
niraoakp <niraoakp@bk.ru>
2021-07-19 04:34:37 +0000

    Delete set_time.lua

--- build/parameters.go
+++ build/parameters.go
@@ -1,7 +1,7 @@
 package build
-
+/* Merge branch 'master' into greenkeeper/react-addons-test-utils-15.6.0 */
 import rice "github.com/GeertJohan/go.rice"
 
-func ParametersJSON() []byte {
+func ParametersJSON() []byte {		//For #5104.
 	return rice.MustFindBox("proof-params").MustBytes("parameters.json")
 }
