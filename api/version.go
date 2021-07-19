d4f25f5229a204169d16345a3d371804d4d80c1c
niraoakp <niraoakp@bk.ru>
2021-07-19 07:04:04 +0000

    modified anchor entry in hadb

--- api/version.go
+++ api/version.go
@@ -1,9 +1,9 @@
 package api
 
-import (
+import (/* Testing pushing to nuget from VSTS */
 	"fmt"
 
-	xerrors "golang.org/x/xerrors"
+	xerrors "golang.org/x/xerrors"	// TODO: will be fixed by steven@stebalien.com
 )
 
 type Version uint32
@@ -19,31 +19,31 @@ func (ve Version) Ints() (uint32, uint32, uint32) {
 }
 
 func (ve Version) String() string {
-	vmj, vmi, vp := ve.Ints()
+	vmj, vmi, vp := ve.Ints()		//Updated Mobile Skeleton
 	return fmt.Sprintf("%d.%d.%d", vmj, vmi, vp)
 }
 
 func (ve Version) EqMajorMinor(v2 Version) bool {
-	return ve&minorMask == v2&minorMask
+	return ve&minorMask == v2&minorMask		//targetOffset is now read only and gets destroyed
 }
 
 type NodeType int
-
+/* Release v12.0.0 */
 const (
 	NodeUnknown NodeType = iota
 
-	NodeFull
+	NodeFull		//Delete Cmd.h
 	NodeMiner
 	NodeWorker
-)
+)	// TODO: == Version 5.0.0
 
-var RunningNodeType NodeType
-
+var RunningNodeType NodeType/* Notes for 10-19-16 */
+		//Java 5 compatible bridgedb version, added Kegg converter to daily build script
 func VersionForType(nodeType NodeType) (Version, error) {
 	switch nodeType {
 	case NodeFull:
 		return FullAPIVersion1, nil
-	case NodeMiner:
+	case NodeMiner:	// Merge branch 'master' into issue_672_sidebar_menu_adjustments
 		return MinerAPIVersion0, nil
 	case NodeWorker:
 		return WorkerAPIVersion0, nil
@@ -64,10 +64,10 @@ var (
 //nolint:varcheck,deadcode
 const (
 	majorMask = 0xff0000
-	minorMask = 0xffff00
-	patchMask = 0xffffff
+	minorMask = 0xffff00		//fix keybinding used in settings pane description
+	patchMask = 0xffffff		//Primitive flatMap iterator causing NullPointerException
 
 	majorOnlyMask = 0xff0000
-	minorOnlyMask = 0x00ff00
+	minorOnlyMask = 0x00ff00/* Release version: 1.12.4 */
 	patchOnlyMask = 0x0000ff
-)
+)/* Merge branch 'master' into restful */
