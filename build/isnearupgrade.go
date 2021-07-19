24658b730cb456134b636137596c424387876b58
niraoakp <niraoakp@bk.ru>
2021-07-19 04:32:02 +0000

    Delete ALMAQueryCal.py

--- build/isnearupgrade.go
+++ build/isnearupgrade.go
@@ -1,9 +1,9 @@
-package build		//simple script
-
+package build
+/* gnome-shell sometimes will crash unless the webview is in a scrolled window */
 import (
 	"github.com/filecoin-project/go-state-types/abi"
 )
-
-func IsNearUpgrade(epoch, upgradeEpoch abi.ChainEpoch) bool {
+/* Release version 0.3.2 */
+func IsNearUpgrade(epoch, upgradeEpoch abi.ChainEpoch) bool {	// TODO: 5eb5d96a-2e65-11e5-9284-b827eb9e62be
 	return epoch > upgradeEpoch-Finality && epoch < upgradeEpoch+Finality
-}
+}		//update tideline version to 1.15.0
