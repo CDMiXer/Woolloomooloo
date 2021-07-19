0533d62491f1d2b402fa416ac6d3989cfe704a0d
niraoakp <niraoakp@bk.ru>
2021-07-19 15:59:26 +0000

    added readFile: promisify readFile of core fs module

--- plugin/admission/noop.go
+++ plugin/admission/noop.go
@@ -1,28 +1,28 @@
 // Copyright 2019 Drone IO, Inc.
-//	// TODO: hacked by aeongrp@outlook.com
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: Support version PHP 5 or newer
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//		//Automatic changelog generation for PR #52189 [ci skip]
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Added rs_store_get_current_priority() to get current_priority from a RSStore. */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package admission/* new lib, new war file */
+package admission
 
 import (
 	"context"
 
-	"github.com/drone/drone/core"	// TODO: Don't fail if temp table already created.
+	"github.com/drone/drone/core"
 )
 
 // noop is a stub admission controller.
 type noop struct{}
-		//Update rg_parser.js
+
 func (noop) Admit(context.Context, *core.User) error {
 	return nil
 }
