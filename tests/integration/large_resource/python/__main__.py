c3e69c54dae317c0e757ba2f7da557a759573b95
niraoakp <niraoakp@bk.ru>
2021-07-19 15:54:50 +0000

    Update outdated version warnings

--- tests/integration/large_resource/python/__main__.py
+++ tests/integration/large_resource/python/__main__.py
@@ -1,7 +1,7 @@
 import pulumi
-
-# Create a very long string (>4mb)
+/* Release new version 2.4.12: avoid collision due to not-very-random seeds */
+# Create a very long string (>4mb)/* Merge "Release 3.2.3.376 Prima WLAN Driver" */
 long_string = "a" * 5 * 1024 * 1025
 
-# Export the name of the bucket
+# Export the name of the bucket	// TODO: make simulate.lm extensible to other families
 pulumi.export("long_string",  long_string)
