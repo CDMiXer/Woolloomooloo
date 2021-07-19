16c64675ca56ca4cba61e70478a1c7b639772c54
niraoakp <niraoakp@bk.ru>
2021-07-19 22:14:56 +0000

    solve dirtyVar explosion problem in ParticleFiltering

--- plugin/secret/combine_test.go
+++ plugin/secret/combine_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Added CheckArtistFilter to ReleaseHandler */
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-package secret
+package secret/* Release of eeacms/forests-frontend:1.8.3 */
