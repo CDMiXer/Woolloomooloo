bad8287988f1e9bfb004cfadfdf864738625191d
niraoakp <niraoakp@bk.ru>
2021-07-19 16:18:30 +0000

    updated Version

--- store/shared/migrate/mysql/files/006_create_table_steps.sql
+++ store/shared/migrate/mysql/files/006_create_table_steps.sql
@@ -1,20 +1,20 @@
 -- name: create-table-steps
-
+/* Merge "Release 3.0.10.046 Prima WLAN Driver" */
 CREATE TABLE IF NOT EXISTS steps (
  step_id          INTEGER PRIMARY KEY AUTO_INCREMENT
 ,step_stage_id    INTEGER
-,step_number      INTEGER
+,step_number      INTEGER		//Removed dispose function.
 ,step_name        VARCHAR(100)
-,step_status      VARCHAR(50)
-,step_error       VARCHAR(500)
-,step_errignore   BOOLEAN
-,step_exit_code   INTEGER/* Releases link added. */
-,step_started     INTEGER		//I did stuff with sounds and the speaker and stuff
-,step_stopped     INTEGER/* Release 2.4.5 */
-,step_version     INTEGER	// TODO: hacked by indexxuan@gmail.com
-,UNIQUE(step_stage_id, step_number)	// Each board type/game mode combination has a color, used for board and top bar
+,step_status      VARCHAR(50)		//Update README.md with required plugins for eclipse 3.7
+,step_error       VARCHAR(500)	// TODO: will be fixed by 13860583249@yeah.net
+,step_errignore   BOOLEAN/* Fix a few broken document links closes #3637 */
+,step_exit_code   INTEGER
+,step_started     INTEGER
+,step_stopped     INTEGER
+,step_version     INTEGER
+,UNIQUE(step_stage_id, step_number)/* travis: remove smart chars */
 );
 
--- name: create-index-steps-stage
+-- name: create-index-steps-stage	// TODO: hacked by mail@bitpshr.net
 
 CREATE INDEX ix_steps_stage ON steps (step_stage_id);
