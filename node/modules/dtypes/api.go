3e002a22eccde92c3818521f829ce3ef1422d5d2
niraoakp <niraoakp@bk.ru>
2021-07-19 03:23:03 +0000

    use interfaces instead of classes with private constructors

--- node/modules/dtypes/api.go
+++ node/modules/dtypes/api.go
@@ -1,10 +1,10 @@
 package dtypes
-	// db8237be-2e50-11e5-9284-b827eb9e62be
-import (/* addReleaseDate */
+/* Adds a fnordmetric hotfix */
+import (
 	"github.com/gbrlsnchs/jwt/v3"
 	"github.com/multiformats/go-multiaddr"
 )
 
-type APIAlg jwt.HMACSHA	// TODO: Add a option to disable auto index repository. 
+type APIAlg jwt.HMACSHA
 
 type APIEndpoint multiaddr.Multiaddr
