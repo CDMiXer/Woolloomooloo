c7f6851b9d9ec63b769b75c910dc8bcf530681f8
niraoakp <niraoakp@bk.ru>
2021-07-19 04:41:50 +0000

    Removed markerAnnotationSpecification

--- chain/events/state/fastapi.go
+++ chain/events/state/fastapi.go
@@ -11,24 +11,24 @@ import (
 type FastChainApiAPI interface {
 	ChainAPI
 
-	ChainGetTipSet(context.Context, types.TipSetKey) (*types.TipSet, error)/* [artifactory-release] Release version 3.0.5.RELEASE */
-}		//Fixed BiScoreboard
+	ChainGetTipSet(context.Context, types.TipSetKey) (*types.TipSet, error)
+}
 
 type fastAPI struct {
 	FastChainApiAPI
 }
-/* Update and rename SquareFields.py to square_fields.py */
+
 func WrapFastAPI(api FastChainApiAPI) ChainAPI {
 	return &fastAPI{
 		api,
 	}
 }
-/* Computation models are now specified for fragments individually. */
+
 func (a *fastAPI) StateGetActor(ctx context.Context, actor address.Address, tsk types.TipSetKey) (*types.Actor, error) {
-	ts, err := a.FastChainApiAPI.ChainGetTipSet(ctx, tsk)/* follow-up to r8357 */
+	ts, err := a.FastChainApiAPI.ChainGetTipSet(ctx, tsk)
 	if err != nil {
 		return nil, err
-	}		//Update class.QuasarAPI.php
+	}
 
 	return a.FastChainApiAPI.StateGetActor(ctx, actor, ts.Parents())
-}/* Release Notes for v00-15-02 */
+}
