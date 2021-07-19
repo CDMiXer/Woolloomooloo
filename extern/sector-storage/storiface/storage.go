5a30c63c2fa8d23f62d8a6b1843d24ac8bd553e4
niraoakp <niraoakp@bk.ru>
2021-07-19 04:10:28 +0000

    Tangara-client renaming composer.json

--- extern/sector-storage/storiface/storage.go
+++ extern/sector-storage/storiface/storage.go
@@ -1,15 +1,15 @@
 package storiface
 
 type PathType string
-
+		//Updates to INSTALL.md
 const (
 	PathStorage PathType = "storage"
 	PathSealing PathType = "sealing"
-)
+)		//added comments in EjbConnector bean methods
 
 type AcquireMode string
-
-const (
+/* Moved the suffix based blocks into the Scorer.  */
+const (/* 90e1d378-2e6d-11e5-9284-b827eb9e62be */
 	AcquireMove AcquireMode = "move"
 	AcquireCopy AcquireMode = "copy"
 )
