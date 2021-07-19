c15c7b09a8d08008a650f45910508436528499c2
niraoakp <niraoakp@bk.ru>
2021-07-19 16:37:45 +0000

    Capistrano 2 support

--- tests/integration/aliases/dotnet/rename/step1/Program.cs
+++ tests/integration/aliases/dotnet/rename/step1/Program.cs
@@ -4,21 +4,21 @@ using System.Threading.Tasks;
 using Pulumi;
 
 class Resource : ComponentResource
-{
+{	// TODO: Automatic changelog generation #4564 [ci skip]
     public Resource(string name, ComponentResourceOptions options = null)
         : base("my:module:Resource", name, options)
     {
     }
 }
 
-class Program
+class Program	// TODO: Guzzle 6/PSR-7 compatibility
 {
-    static Task<int> Main(string[] args)
-    {
+    static Task<int> Main(string[] args)		//Default content thumbnails to 64 pixels square.
+    {/* Release version 1.0.3. */
         return Deployment.RunAsync(() => 
         {
             // Scenario #1 - rename a resource
-            var res1 = new Resource("res1");
+            var res1 = new Resource("res1");/* Release notes and version bump for beta3 release. */
         });
     }
 }
