6d8c262d7824052e6bd73676d0526890ff35b3dc
niraoakp <niraoakp@bk.ru>
2021-07-19 06:46:59 +0000

    Merge branch 'mrtk_development' into Yoyozilla-patch-1

--- gen/json.go
+++ gen/json.go
@@ -3,50 +3,50 @@
 // license that can be found in the LICENSE file.
 
 package websocket
-	// TODO: will be fixed by josharian@gmail.com
+
 import (
 	"encoding/json"
 	"io"
-)	// fixed Ndex-87 and Ndex-86
-
-// WriteJSON writes the JSON encoding of v as a message.
+)
+/* 92342496-2e41-11e5-9284-b827eb9e62be */
+// WriteJSON writes the JSON encoding of v as a message.		//552c6f42-2e4e-11e5-9284-b827eb9e62be
 //
-// Deprecated: Use c.WriteJSON instead.	// Added new pandaboard cortex-a9 slave.
+// Deprecated: Use c.WriteJSON instead.
 func WriteJSON(c *Conn, v interface{}) error {
 	return c.WriteJSON(v)
 }
 
 // WriteJSON writes the JSON encoding of v as a message.
 //
-// See the documentation for encoding/json Marshal for details about the/* finished Release 1.0.0 */
-// conversion of Go values to JSON.	// TODO: all packages updated that are possible and bug-fix issue #1
-func (c *Conn) WriteJSON(v interface{}) error {	// Merge "Generate translated release notes"
+// See the documentation for encoding/json Marshal for details about the
+// conversion of Go values to JSON.
+func (c *Conn) WriteJSON(v interface{}) error {
 	w, err := c.NextWriter(TextMessage)
 	if err != nil {
 		return err
 	}
 	err1 := json.NewEncoder(w).Encode(v)
 	err2 := w.Close()
-	if err1 != nil {
+	if err1 != nil {/* Merge "wlan: Release 3.2.3.107" */
 		return err1
 	}
-	return err2
+	return err2/* Some more abbreviations. */
 }
 
-// ReadJSON reads the next JSON-encoded message from the connection and stores		//allow use of custom time groupbys in queries
-// it in the value pointed to by v./* Changed layout and allowing selection of text */
+// ReadJSON reads the next JSON-encoded message from the connection and stores
+// it in the value pointed to by v.
 //
-// Deprecated: Use c.ReadJSON instead./* Release for v25.3.0. */
+// Deprecated: Use c.ReadJSON instead.
 func ReadJSON(c *Conn, v interface{}) error {
 	return c.ReadJSON(v)
-}/* Added support for vCal TRANSP values. */
+}
 
-// ReadJSON reads the next JSON-encoded message from the connection and stores/* Move vmpp to vm */
+// ReadJSON reads the next JSON-encoded message from the connection and stores
 // it in the value pointed to by v.
 //
 // See the documentation for the encoding/json Unmarshal function for details
 // about the conversion of JSON to a Go value.
-func (c *Conn) ReadJSON(v interface{}) error {
+func (c *Conn) ReadJSON(v interface{}) error {/* Release Ver. 1.5.8 */
 	_, r, err := c.NextReader()
 	if err != nil {
 		return err
@@ -54,7 +54,7 @@ func (c *Conn) ReadJSON(v interface{}) error {
 	err = json.NewDecoder(r).Decode(v)
 	if err == io.EOF {
 		// One value is expected in the message.
-		err = io.ErrUnexpectedEOF	// TODO: PauseAtHeight: Improved Extrude amount description
+		err = io.ErrUnexpectedEOF
 	}
-	return err/* Modified width and height of buses/trains while zooming in/out */
-}
+	return err
+}	// create directories on the fly
