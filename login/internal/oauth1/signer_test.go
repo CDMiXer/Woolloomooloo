69981b2822c5b1a93b5e648394a4a0f9319a6217
niraoakp <niraoakp@bk.ru>
2021-07-19 19:00:43 +0000

    Add missing object names in classes.md

--- login/internal/oauth1/signer_test.go
+++ login/internal/oauth1/signer_test.go
@@ -1,4 +1,4 @@
 // Copyright (c) 2015 Dalton Hubble. All rights reserved.
 // Copyrights licensed under the MIT License.
-/* [pyclient] Released 1.4.2 */
-package oauth1	// TODO: [EPGView] Improved align of the now line: it was 10 minutes wrong
+
+package oauth1
