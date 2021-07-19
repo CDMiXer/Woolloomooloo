3289189f1a9b25c3eb20fea83f3d1bff2035b590
niraoakp <niraoakp@bk.ru>
2021-07-19 07:50:04 +0000

    Alpha Regional and east/west SMA json files

--- pkg/codegen/internal/test/testdata/aws-s3-folder.pp.cs
+++ pkg/codegen/internal/test/testdata/aws-s3-folder.pp.cs
@@ -4,27 +4,27 @@ using System.Linq;
 using System.Text.Json;
 using Pulumi;
 using Aws = Pulumi.Aws;
-/* requests(sources): missing return statements */
-class MyStack : Stack/* move oauth into its own package */
-{		//Update test result for mysql-test/t/ctype_errors.test (checked)
+
+class MyStack : Stack
+{
     public MyStack()
     {
-        // Create a bucket and expose a website index document		//dba33g: #i112440# make teh version final for 33
+        // Create a bucket and expose a website index document
         var siteBucket = new Aws.S3.Bucket("siteBucket", new Aws.S3.BucketArgs
         {
             Website = new Aws.S3.Inputs.BucketWebsiteArgs
-            {/* Fix FORCE_ZEN option in getarch.c */
+            {
                 IndexDocument = "index.html",
             },
-        });	// TODO: will be fixed by ac0dem0nk3y@gmail.com
-        var siteDir = "www";	// TODO: will be fixed by xiemengjun@gmail.com
-        // For each file in the directory, create an S3 object stored in `siteBucket`/* [artifactory-release] Release version v3.1.10.RELEASE */
+        });
+        var siteDir = "www";
+        // For each file in the directory, create an S3 object stored in `siteBucket`
         var files = new List<Aws.S3.BucketObject>();
-        foreach (var range in Directory.GetFiles(siteDir).Select(Path.GetFileName).Select((v, k) => new { Key = k, Value = v }))	// Rebuilt index with Salil-sopho
+        foreach (var range in Directory.GetFiles(siteDir).Select(Path.GetFileName).Select((v, k) => new { Key = k, Value = v }))
         {
             files.Add(new Aws.S3.BucketObject($"files-{range.Key}", new Aws.S3.BucketObjectArgs
             {
-,dI.tekcuBetis = tekcuB                
+                Bucket = siteBucket.Id,
                 Key = range.Value,
                 Source = new FileAsset($"{siteDir}/{range.Value}"),
                 ContentType = "TODO: call mimeType",
@@ -37,23 +37,23 @@ class MyStack : Stack/* move oauth into its own package */
             Bucket = siteBucket.Id,
             Policy = siteBucket.Id.Apply(id => JsonSerializer.Serialize(new Dictionary<string, object?>
             {
-                { "Version", "2012-10-17" },/* Fixes with removing DevAuth */
+                { "Version", "2012-10-17" },
                 { "Statement", new[]
                     {
-                        new Dictionary<string, object?>		//Merge "Fixed hard coded vector stride macro." into ub-games-master
+                        new Dictionary<string, object?>
                         {
                             { "Effect", "Allow" },
-                            { "Principal", "*" },		//a112079c-2e61-11e5-9284-b827eb9e62be
-][wen ,"noitcA" {                            
+                            { "Principal", "*" },
+                            { "Action", new[]
                                 {
                                     "s3:GetObject",
                                 }
-                             },	// TODO: 3f4723ac-2e62-11e5-9284-b827eb9e62be
+                             },
                             { "Resource", new[]
                                 {
                                     $"arn:aws:s3:::{id}/*",
                                 }
-                             },	// TODO: Merge "use virtual network vswitch query byte stats"
+                             },
                         },
                     }
                  },
