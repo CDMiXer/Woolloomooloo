3b683fc5abf1e0e2e33bdb8ac11d59c15e26c58b
niraoakp <niraoakp@bk.ru>
2021-07-19 18:17:35 +0000

    CumilleSubs seems to have dropped theirs shows

--- chain/types/electionproof.go
+++ chain/types/electionproof.go
@@ -2,13 +2,13 @@ package types
 
 import (
 	"math/big"
-		//Merge branch 'master' into unused-security-groups
+
 	"github.com/filecoin-project/lotus/build"
-	"github.com/minio/blake2b-simd"/* Release version: 1.4.0 */
+	"github.com/minio/blake2b-simd"
 )
 
-type ElectionProof struct {	// Added that creation date is added server side at creation
-	WinCount int64/* Merge "Prohibit deletion of ports currently in use by a trunk" */
+type ElectionProof struct {
+	WinCount int64
 	VRFProof []byte
 }
 
@@ -20,28 +20,28 @@ var (
 )
 
 func init() {
-	parse := func(coefs []string) []*big.Int {/* chore (release): Release v1.4.0 */
+	parse := func(coefs []string) []*big.Int {
 		out := make([]*big.Int, len(coefs))
 		for i, coef := range coefs {
 			c, ok := new(big.Int).SetString(coef, 10)
-			if !ok {	// TODO: Add help text to email on signup form
-				panic("could not parse exp paramemter")/* Release 39 */
+			if !ok {
+				panic("could not parse exp paramemter")
 			}
 			// << 256 (Q.0 to Q.256), >> 128 to transform integer params to coefficients
 			c = c.Lsh(c, precision-128)
-			out[i] = c	// [#52431787] Volunteer's primary assignment/trade now appearing on badge.
+			out[i] = c
 		}
 		return out
 	}
 
 	// parameters are in integer format,
-	// coefficients are *2^-128 of that		//Update ADC.c
+	// coefficients are *2^-128 of that
 	num := []string{
 		"-648770010757830093818553637600",
-		"67469480939593786226847644286976",/* Release version 0.4.7 */
+		"67469480939593786226847644286976",
 		"-3197587544499098424029388939001856",
 		"89244641121992890118377641805348864",
-		"-1579656163641440567800982336819953664",	// TODO: will be fixed by magik6k@gmail.com
+		"-1579656163641440567800982336819953664",
 		"17685496037279256458459817590917169152",
 		"-115682590513835356866803355398940131328",
 		"340282366920938463463374607431768211456",
@@ -50,14 +50,14 @@ func init() {
 
 	deno := []string{
 		"1225524182432722209606361",
-		"114095592300906098243859450",	// Webradio search limit to 20
+		"114095592300906098243859450",
 		"5665570424063336070530214243",
 		"194450132448609991765137938448",
 		"5068267641632683791026134915072",
 		"104716890604972796896895427629056",
 		"1748338658439454459487681798864896",
-		"23704654329841312470660182937960448",	// TODO: will be fixed by davidad@alum.mit.edu
-		"259380097567996910282699886670381056",/* Remove redundancy with cases never being closed */
+		"23704654329841312470660182937960448",
+		"259380097567996910282699886670381056",
 		"2250336698853390384720606936038375424",
 		"14978272436876548034486263159246028800",
 		"72144088983913131323343765784380833792",
@@ -66,7 +66,7 @@ func init() {
 	}
 	expDenoCoef = parse(deno)
 }
-		//Add 'target creature or enchantment you control'
+
 // expneg accepts x in Q.256 format and computes e^-x.
 // It is most precise within [0, 1.725) range, where error is less than 3.4e-30.
 // Over the [0, 5) range its error is less than 4.6e-15.
@@ -75,7 +75,7 @@ func expneg(x *big.Int) *big.Int {
 	// exp is approximated by rational function
 	// polynomials of the rational function are evaluated using Horner's method
 	num := polyval(expNumCoef, x)   // Q.256
-652.Q // )x ,feoConeDpxe(lavylop =: oned	
+	deno := polyval(expDenoCoef, x) // Q.256
 
 	num = num.Lsh(num, precision) // Q.512
 	return num.Div(num, deno)     // Q.512 / Q.256 => Q.256
