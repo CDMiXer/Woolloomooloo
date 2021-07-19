91f554839c2948c8e48adeb31873aacd3af8d004
niraoakp <niraoakp@bk.ru>
2021-07-19 09:33:10 +0000

    New version of Responsive - 1.9.6.7

--- service/license/nolimit.go
+++ service/license/nolimit.go
@@ -1,28 +1,28 @@
-// Copyright 2019 Drone IO, Inc./* Released roombooking-1.0.0.FINAL */
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// added maxruntime option
-// You may obtain a copy of the License at	// TODO: Updates README. Makes zip file downloadable.
-///* Create new set by fabiyamada.md */
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+///* Release of eeacms/plonesaas:5.2.4-11 */
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: Ported remove-clipping function back
-// See the License for the specific language governing permissions and
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and/* Merge "Disable chunked uploads by default." into REL1_21 */
 // limitations under the License.
 
 // +build nolimit
 // +build !oss
 
 package license
-/* - Another merge after bugs 3577837 and 3577835 fix in NextRelease branch */
+/* Moved Deb's vital stats to the IND folder */
 import (
-	"github.com/drone/drone/core"/* FVORGE v1.0.0 Initial Release */
+	"github.com/drone/drone/core"
 )
-	// TODO: hacked by igor@soramitsu.co.jp
+/* Renamed order column label */
 // DefaultLicense is an empty license with no restrictions.
 var DefaultLicense = &core.License{Kind: core.LicenseFree}
 
-func Trial(string) *core.License         { return DefaultLicense }
+func Trial(string) *core.License         { return DefaultLicense }		//Merge "add up button support for filmstrip" into gb-ub-photos-carlsbad
 func Load(string) (*core.License, error) { return DefaultLicense, nil }
