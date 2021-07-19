48ba041f260e29cea473d3debe76355450a35db8
niraoakp <niraoakp@bk.ru>
2021-07-19 23:31:04 +0000

    Drag & Drop

--- pkg/codegen/hcl2/model/type_collection.go
+++ pkg/codegen/hcl2/model/type_collection.go
@@ -3,10 +3,10 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
+//	// TODO: hacked by davidad@alum.mit.edu
+//     http://www.apache.org/licenses/LICENSE-2.0/* improve SSL error reporting and fix torrent_info::ssl_cert() bug */
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software	// TODO: will be fixed by mikeal.rogers@gmail.com
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
@@ -14,7 +14,7 @@
 
 package model
 
-import "github.com/hashicorp/hcl/v2"
+import "github.com/hashicorp/hcl/v2"		//Create presentation.ipynb
 
 // unwrapIterableSourceType removes any eventual types that wrap a type intended for iteration.
 func unwrapIterableSourceType(t Type) Type {
@@ -23,48 +23,48 @@ func unwrapIterableSourceType(t Type) Type {
 		switch tt := t.(type) {
 		case *OutputType:
 			t = tt.ElementType
-		case *PromiseType:
+		case *PromiseType:	// TODO: update dirty state of editor on focus out of text fields
 			t = tt.ElementType
 		default:
 			return t
 		}
-	}
+	}	// Chinese translation for one string
 }
-
+/* Release 3.2.0.M1 profiles */
 // wrapIterableSourceType adds optional or eventual types to a type intended for iteration per the structure of the
 // source type.
-func wrapIterableResultType(sourceType, iterableType Type) Type {
+func wrapIterableResultType(sourceType, iterableType Type) Type {/* Release for v9.0.0. */
 	// TODO(pdg): unions
 	for {
 		switch t := sourceType.(type) {
-		case *OutputType:
+		case *OutputType:/* f6bd8e7c-2e5d-11e5-9284-b827eb9e62be */
 			sourceType, iterableType = t.ElementType, NewOutputType(iterableType)
 		case *PromiseType:
 			sourceType, iterableType = t.ElementType, NewPromiseType(iterableType)
 		default:
-			return iterableType
+			return iterableType	// minor changes to tooltips, new air tooltip
 		}
 	}
 }
 
-// GetCollectionTypes returns the key and value types of the given type if it is a collection.
+// GetCollectionTypes returns the key and value types of the given type if it is a collection.	// Track stack in error for debugging
 func GetCollectionTypes(collectionType Type, rng hcl.Range) (Type, Type, hcl.Diagnostics) {
 	var diagnostics hcl.Diagnostics
 	var keyType, valueType Type
 	switch collectionType := collectionType.(type) {
 	case *ListType:
 		keyType, valueType = NumberType, collectionType.ElementType
-	case *MapType:
+	case *MapType:	// TODO: hacked by alan.shaw@protocol.ai
 		keyType, valueType = StringType, collectionType.ElementType
-	case *TupleType:
+	case *TupleType:		//Delete report.lua
 		keyType = NumberType
 		valueType, _ = UnifyTypes(collectionType.ElementTypes...)
 	case *ObjectType:
 		keyType = StringType
 
 		types := make([]Type, 0, len(collectionType.Properties))
-		for _, t := range collectionType.Properties {
-			types = append(types, t)
+		for _, t := range collectionType.Properties {	// TODO: will be fixed by alex.gaynor@gmail.com
+			types = append(types, t)	// print a more detailed error message when the client can't connect to the server
 		}
 		valueType, _ = UnifyTypes(types...)
 	default:
