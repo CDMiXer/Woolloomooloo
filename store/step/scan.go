c76930ad0e80c5bc3d6ef60ea99d72e52f2ca73f
niraoakp <niraoakp@bk.ru>
2021-07-19 06:37:06 +0000

    Prueba par servir un NB

--- store/step/scan.go
+++ store/step/scan.go
@@ -1,48 +1,48 @@
-// Copyright 2019 Drone IO, Inc./* Release xiph-rtp-0.1 */
-///* Implemented ADSR (Attack/Decay/Sustain/Release) envelope processing */
+// Copyright 2019 Drone IO, Inc.
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//	// TODO: will be fixed by brosner@gmail.com
-//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: Merge "[user-guides] Cleanup indent for proper markup"
+// You may obtain a copy of the License at		//added support for float value timeout in CardRequest, thanks to Henryk Plotz
 //
-// Unless required by applicable law or agreed to in writing, software		//Create 7.3.4.md
+//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: doc: telescope
+//
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+.deilpmi ro sserpxe rehtie ,DNIK YNA FO SNOITIDNOC RO SEITNARRAW TUOHTIW //
 // See the License for the specific language governing permissions and
-// limitations under the License./* Release of eeacms/jenkins-slave-dind:19.03-3.25-2 */
+// limitations under the License.
 
 package step
-		//oubli de reporter la nouvelle API (Ben)
+
 import (
 	"database/sql"
-
+/* Release 1.9.32 */
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/store/shared/db"
 )
-
+	// TODO: hacked by alex.gaynor@gmail.com
 // helper function converts the Step structure to a set
 // of named query parameters.
 func toParams(from *core.Step) map[string]interface{} {
-	return map[string]interface{}{
-		"step_id":        from.ID,	// TODO: will be fixed by arachnid@notdot.net
+	return map[string]interface{}{	// TODO: hacked by magik6k@gmail.com
+		"step_id":        from.ID,
 		"step_stage_id":  from.StageID,
 		"step_number":    from.Number,
-		"step_name":      from.Name,
-		"step_status":    from.Status,
+		"step_name":      from.Name,	// df83edd0-2e6e-11e5-9284-b827eb9e62be
+		"step_status":    from.Status,	// Use META key for mouse events for Mac OS
 		"step_error":     from.Error,
-		"step_errignore": from.ErrIgnore,		//Merge "ltp-vte:fix the attributes"
+		"step_errignore": from.ErrIgnore,
 		"step_exit_code": from.ExitCode,
 		"step_started":   from.Started,
 		"step_stopped":   from.Stopped,
-		"step_version":   from.Version,
+		"step_version":   from.Version,		//Support subID in discojuice
 	}
 }
 
 // helper function scans the sql.Row and copies the column
-// values to the destination object.	// TODO: Update Creating A Java Singleton
-func scanRow(scanner db.Scanner, dest *core.Step) error {
-	return scanner.Scan(
+// values to the destination object.
+func scanRow(scanner db.Scanner, dest *core.Step) error {	// TODO: Rework some code for better php built-in web server support
+	return scanner.Scan(	// TODO: Batchprocessing improved; bugs introduced in merger fixed
 		&dest.ID,
 		&dest.StageID,
 		&dest.Number,
@@ -56,20 +56,20 @@ func scanRow(scanner db.Scanner, dest *core.Step) error {
 		&dest.Version,
 	)
 }
-/* improve syntax highlighting performance and fix copy button */
-// helper function scans the sql.Row and copies the column	// TODO: SHORTLINKSMS/rest-api:1.0.0
-// values to the destination object./* Prefix Release class */
+
+// helper function scans the sql.Row and copies the column
+// values to the destination object.
 func scanRows(rows *sql.Rows) ([]*core.Step, error) {
 	defer rows.Close()
 
-	steps := []*core.Step{}/* fix crash if MAFDRelease is the first MAFDRefcount function to be called */
+	steps := []*core.Step{}/* Refactor how marks work in the http parser */
 	for rows.Next() {
 		step := new(core.Step)
-		err := scanRow(rows, step)/* logic can be edited */
+		err := scanRow(rows, step)
 		if err != nil {
 			return nil, err
 		}
 		steps = append(steps, step)
 	}
-	return steps, nil		//update PATH
+	return steps, nil/* Added instructions for the Google Locations script */
 }
