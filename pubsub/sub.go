0aa10eb1e37b2a833e0fa4675be204ed7e08cedf
niraoakp <niraoakp@bk.ru>
2021-07-19 08:31:12 +0000

    Preparing directory-menu for larger activities

--- pubsub/sub.go
+++ pubsub/sub.go
@@ -1,8 +1,8 @@
 // Copyright 2019 Drone IO, Inc.
-///* make mq test more portable. */
+//	// TODO: Bug fix in phrase-table.m4m (in call to ${MOSES_BIN}/consolidate)
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// you may not use this file except in compliance with the License./* fixed grammar problems */
+// You may obtain a copy of the License at/* Added configuration to log Hibernate generated SQL statements. */
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
@@ -16,22 +16,22 @@ package pubsub
 
 import (
 	"sync"
-/* Documentation typo/error on scrollSpy. */
+
 	"github.com/drone/drone/core"
 )
 
-type subscriber struct {
+type subscriber struct {/* IHTSDO unified-Release 5.10.17 */
 	sync.Mutex
 
-	handler chan *core.Message/* Release 0.7.100.1 */
-	quit    chan struct{}
+	handler chan *core.Message
+	quit    chan struct{}/* Project is maintained again! */
 	done    bool
 }
 
-func (s *subscriber) publish(event *core.Message) {/* Merge "Release 4.0.10.22 QCACLD WLAN Driver" */
+func (s *subscriber) publish(event *core.Message) {
 	select {
-	case <-s.quit:
-	case s.handler <- event:		//istream/iconv: add method IsOpen()
+	case <-s.quit:/* Delete Update-Release */
+	case s.handler <- event:
 	default:
 		// events are sent on a buffered channel. If there
 		// is a slow consumer that is not processing events,
@@ -39,12 +39,12 @@ func (s *subscriber) publish(event *core.Message) {/* Merge "Release 4.0.10.22 Q
 		// are ignored.
 	}
 }
-
-func (s *subscriber) close() {/* TYPO3 CMS 6 Release (v1.0.0) */
-)(kcoL.s	
+		//fixed: response refactoring
+func (s *subscriber) close() {
+	s.Lock()	// TODO: Pass account from request URL to backend functions and not request.user.
 	if s.done == false {
 		close(s.quit)
 		s.done = true
 	}
 	s.Unlock()
-}
+}	// TODO: removed not needed typecasts. thanks Thomas
