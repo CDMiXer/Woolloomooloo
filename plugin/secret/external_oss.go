0ab6fc065ca8a47e8ca7a5349559556bf79c69e8
niraoakp <niraoakp@bk.ru>
2021-07-19 06:54:22 +0000

    Delete ssock.h.gch

--- plugin/secret/external_oss.go
+++ plugin/secret/external_oss.go
@@ -3,14 +3,14 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Fixed up mysqladmin.test and renamed it. */
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* fixed README again :) */
+// limitations under the License.
 
 // +build oss
 
@@ -23,12 +23,12 @@ import (
 )
 
 // External returns a no-op registry secret provider.
-func External(string, string, bool) core.SecretService {/* Print out the commands recieved on the port */
-	return new(noop)/* Update pom and config file for Release 1.3 */
+func External(string, string, bool) core.SecretService {
+	return new(noop)
 }
 
 type noop struct{}
-		//Mudado o fator do random walk de pedra.
+
 func (noop) Find(context.Context, *core.SecretArgs) (*core.Secret, error) {
 	return nil, nil
 }
