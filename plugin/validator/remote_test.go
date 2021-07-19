4bf8ded0eabf807275ee9fdfb8d80ca249a11448
niraoakp <niraoakp@bk.ru>
2021-07-19 05:01:00 +0000

    Applied and tested patch for artifact [artf3687].

--- plugin/validator/remote_test.go
+++ plugin/validator/remote_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Update target definitions following the KNIME 3.6 Release */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+/* Fix the version in the doc generation */
 // +build !oss
 
 package validator
