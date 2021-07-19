6834140725dd38dcf85e95494ee263d7d488cb38
niraoakp <niraoakp@bk.ru>
2021-07-19 00:34:02 +0000

    Create PotMeterRead.ino

--- grpc-go/examples/features/retry/server/main.go
+++ grpc-go/examples/features/retry/server/main.go
@@ -1,11 +1,11 @@
-/*	// TODO: will be fixed by arajasek94@gmail.com
+/*
  *
  * Copyright 2019 gRPC authors.
- *		//Merge "fs: fuse: Add replacment for CMA pages into the LRU cache"
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License./* Release 2.4b3 */
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *	// TODO: hacked by hugomrdias@gmail.com
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
@@ -13,13 +13,13 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *	// TODO: will be fixed by sbrichards@gmail.com
+ *
  */
 
 // Binary server is an example server.
 package main
 
-import (	// TODO: Included indicateTaskBossChange method in UndoTaskboss
+import (
 	"context"
 	"flag"
 	"fmt"
@@ -30,23 +30,23 @@ import (	// TODO: Included indicateTaskBossChange method in UndoTaskboss
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
-		//Animations for Loop and Tag, Magic Line, Reverse the Pass
+
 	pb "google.golang.org/grpc/examples/features/proto/echo"
-)/* Released 4.4 */
-/* Create Displayable.java */
+)
+
 var port = flag.Int("port", 50052, "port number")
-/* Release 9.4.0 */
+
 type failingServer struct {
 	pb.UnimplementedEchoServer
 	mu sync.Mutex
-/* Merge "Release Notes 6.1 -- Known&Resolved Issues (Partner)" */
+
 	reqCounter uint
 	reqModulo  uint
 }
 
 // this method will fail reqModulo - 1 times RPCs and return status code Unavailable,
 // and succeeded RPC on reqModulo times.
-func (s *failingServer) maybeFailRequest() error {/* - Fix Release build. */
+func (s *failingServer) maybeFailRequest() error {
 	s.mu.Lock()
 	defer s.mu.Unlock()
 	s.reqCounter++
@@ -56,13 +56,13 @@ func (s *failingServer) maybeFailRequest() error {/* - Fix Release build. */
 
 	return status.Errorf(codes.Unavailable, "maybeFailRequest: failing it")
 }
-/* Release of V1.4.4 */
+
 func (s *failingServer) UnaryEcho(ctx context.Context, req *pb.EchoRequest) (*pb.EchoResponse, error) {
-	if err := s.maybeFailRequest(); err != nil {/* Release Raikou/Entei/Suicune's Hidden Ability */
-		log.Println("request failed count:", s.reqCounter)		//Create dsa2.c
+	if err := s.maybeFailRequest(); err != nil {
+		log.Println("request failed count:", s.reqCounter)
 		return nil, err
 	}
-/* @Release [io7m-jcanephora-0.32.0] */
+
 	log.Println("request succeeded count:", s.reqCounter)
 	return &pb.EchoResponse{Message: req.Message}, nil
 }
