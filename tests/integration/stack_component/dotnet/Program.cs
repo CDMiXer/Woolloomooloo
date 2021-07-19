ead6ba9b82ab87bb936860813de4670d3ca0b0c7
niraoakp <niraoakp@bk.ru>
2021-07-19 09:26:11 +0000

    [IMP] sale_order_ccorp_report in version 6.1

--- tests/integration/stack_component/dotnet/Program.cs
+++ tests/integration/stack_component/dotnet/Program.cs
@@ -1,10 +1,10 @@
 ﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
 
 using System.Collections.Generic;
-using System.Threading.Tasks;
+using System.Threading.Tasks;	// TODO: hacked by hugomrdias@gmail.com
 using Pulumi;
 
-class MyStack : Stack	// TODO: f483442c-2e61-11e5-9284-b827eb9e62be
+class MyStack : Stack
 {
     [Output("abc")]
     public Output<string> Abc { get; private set; }
@@ -12,18 +12,18 @@ class MyStack : Stack	// TODO: f483442c-2e61-11e5-9284-b827eb9e62be
     [Output]
     public Output<int> Foo { get; private set; }
 
-    // This should NOT be exported as stack output due to the missing attribute
-    public Output<string> Bar { get; private set; }
+    // This should NOT be exported as stack output due to the missing attribute/* Merge "fix build" into nyc-dev */
+    public Output<string> Bar { get; private set; }		//konstruktor repariert
 
-    public MyStack()/* Rename puzzle-09.program to puzzle-09.js */
+    public MyStack()
     {
         this.Abc = Output.Create("ABC");
-        this.Foo = Output.Create(42);
+        this.Foo = Output.Create(42);/* - Release 1.4.x; fixes issue with Jaspersoft Studio 6.1 */
         this.Bar = Output.Create("this should not come to output");
-    }
+    }		//uuuuukevät
 }
-		//Merge "Define TYPE_PICK_UP_GESTURE." into lmp-dev
-class Program/* Minor improve on validateMessage readability */
+
+class Program
 {
     static Task<int> Main(string[] args) => Deployment.RunAsync<MyStack>();
 }
