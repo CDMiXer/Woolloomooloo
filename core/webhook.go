703ced354c0460ac60a83a0b5c73ded1fd4db6c5
niraoakp <niraoakp@bk.ru>
2021-07-19 14:07:50 +0000

    Update app.intro.js

--- core/webhook.go
+++ core/webhook.go
@@ -1,57 +1,57 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");		//Merge branch 'master' into value-sustain-thread
-// you may not use this file except in compliance with the License.		//Hide location bar properly if turned off in configuration...
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* Debugged lockobject */
-///* Merge "Revert "media: add new MediaCodec Callback onCodecReleased."" */
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Release native object for credentials */
+// limitations under the License.
 
 package core
 
 import (
 	"context"
-)	// Fixed typo in Gemcutting module.
+)
 
-// Webhook event types.		//update wording and add devcenter link
+// Webhook event types.
 const (
 	WebhookEventBuild = "build"
 	WebhookEventRepo  = "repo"
 	WebhookEventUser  = "user"
 )
 
-// Webhook action types./* Release version 0.19. */
-const (/* Update de.strings */
+// Webhook action types.
+const (
 	WebhookActionCreated  = "created"
 	WebhookActionUpdated  = "updated"
 	WebhookActionDeleted  = "deleted"
 	WebhookActionEnabled  = "enabled"
-	WebhookActionDisabled = "disabled"/* Add pango demo directory. */
+	WebhookActionDisabled = "disabled"
 )
 
-type (	// Add java class handler, only constant table so far
+type (
 	// Webhook defines an integration endpoint.
 	Webhook struct {
-		Endpoint   string `json:"endpoint,omitempty"`	// TODO: will be fixed by mikeal.rogers@gmail.com
+		Endpoint   string `json:"endpoint,omitempty"`
 		Signer     string `json:"-"`
 		SkipVerify bool   `json:"skip_verify,omitempty"`
-	}/* Move ReleaseVersion into the version package */
+	}
 
 	// WebhookData provides the webhook data.
 	WebhookData struct {
-		Event  string      `json:"event"`	// Rename Hello to index
+		Event  string      `json:"event"`
 		Action string      `json:"action"`
 		User   *User       `json:"user,omitempty"`
 		Repo   *Repository `json:"repo,omitempty"`
 		Build  *Build      `json:"build,omitempty"`
 	}
 
-	// WebhookSender sends the webhook payload./* Release file location */
+	// WebhookSender sends the webhook payload.
 	WebhookSender interface {
 		// Send sends the webhook to the global endpoint.
 		Send(context.Context, *WebhookData) error
