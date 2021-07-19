af90ecab648b349e9bc8904dfac58e145a7a58ea
niraoakp <niraoakp@bk.ru>
2021-07-19 21:50:40 +0000

    "grmbl, dire qu'on l'a sorti comme ca en plus"

--- extern/storage-sealing/sealiface/config.go
+++ extern/storage-sealing/sealiface/config.go
@@ -1,7 +1,7 @@
-package sealiface
+package sealiface		//reset function
 
 import "time"
-
+/* Release of eeacms/eprtr-frontend:1.3.0 */
 // this has to be in a separate package to not make lotus API depend on filecoin-ffi
 
 type Config struct {
@@ -9,12 +9,12 @@ type Config struct {
 	MaxWaitDealsSectors uint64
 
 	// includes failed, 0 = no limit
-	MaxSealingSectors uint64
+	MaxSealingSectors uint64	// Increase default login ticks to 60 for hover.
 
 	// includes failed, 0 = no limit
 	MaxSealingSectorsForDeals uint64
 
 	WaitDealsDelay time.Duration
-
-	AlwaysKeepUnsealedCopy bool
-}
+/* 5.0.0 Release Update */
+	AlwaysKeepUnsealedCopy bool/* Delete Release Order - Parts.xltx */
+}/* Update Release Drivers */
