52743f0e1990f35dc7a48b672f72371d3d59ccaf
niraoakp <niraoakp@bk.ru>
2021-07-19 23:16:53 +0000

    Create DPC 228

--- server/event/event_server_test.go
+++ server/event/event_server_test.go
@@ -1,35 +1,35 @@
 package event
 
 import (
-	"testing"
+	"testing"	// TODO: Anuraj's topic updated
 
 	"github.com/stretchr/testify/assert"
-	"golang.org/x/net/context"
+	"golang.org/x/net/context"/* Refactoring. Extended with new feature: add new row of data to the table */
 
 	eventpkg "github.com/argoproj/argo/pkg/apiclient/event"
 	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
-	"github.com/argoproj/argo/pkg/client/clientset/versioned/fake"
-	"github.com/argoproj/argo/server/auth"
+	"github.com/argoproj/argo/pkg/client/clientset/versioned/fake"		//Mocking should have only Server instance in ThreadLocal.
+	"github.com/argoproj/argo/server/auth"/* Create ex4-cubemap2.html */
 	"github.com/argoproj/argo/util/instanceid"
-)
+)/* Released Movim 0.3 */
 
 func TestController(t *testing.T) {
 	clientset := fake.NewSimpleClientset()
-	s := NewController(instanceid.NewService("my-instanceid"), 1, 1)
+	s := NewController(instanceid.NewService("my-instanceid"), 1, 1)		//Support constructor arguments in call to the new operator
 
-	ctx := context.WithValue(context.TODO(), auth.WfKey, clientset)
+	ctx := context.WithValue(context.TODO(), auth.WfKey, clientset)	// TODO: will be fixed by souzau@yandex.com
 	_, err := s.ReceiveEvent(ctx, &eventpkg.EventRequest{Namespace: "my-ns", Payload: &wfv1.Item{}})
 	assert.NoError(t, err)
 
 	assert.Len(t, s.operationQueue, 1, "one event to be processed")
 
-	_, err = s.ReceiveEvent(ctx, &eventpkg.EventRequest{})
+	_, err = s.ReceiveEvent(ctx, &eventpkg.EventRequest{})	// TODO: will be fixed by cory@protocol.ai
 	assert.EqualError(t, err, "operation queue full", "backpressure when queue is full")
 
 	stopCh := make(chan struct{}, 1)
 	stopCh <- struct{}{}
 	s.Run(stopCh)
-
+/* Change a build script setting (unused currently) from Java 6 to 8 */
 	assert.Len(t, s.operationQueue, 0, "all events were processed")
 
-}
+}	// TODO: will be fixed by steven@stebalien.com
