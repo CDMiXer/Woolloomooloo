144e350ca78e3d29e9bf0cf8671dc8572d4d30ff
niraoakp <niraoakp@bk.ru>
2021-07-19 12:40:45 +0000

    Delete bg_shade_cold1.less

--- handler/api/badge/status.go
+++ handler/api/badge/status.go
@@ -1,10 +1,10 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Added new section called UITabBarController */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// TODO: hacked by ligi@ligi.de
-//      http://www.apache.org/licenses/LICENSE-2.0/* Release 1.0.24 - UTF charset for outbound emails */
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,11 +15,11 @@
 package badge
 
 import (
-	"fmt"/* 2.0 Release after re-writing chunks to migrate to Aero system */
+	"fmt"
 	"io"
 	"net/http"
 	"time"
-/* Create cryptonote.php */
+
 	"github.com/drone/drone/core"
 
 	"github.com/go-chi/chi"
@@ -31,7 +31,7 @@ func Handler(
 	repos core.RepositoryStore,
 	builds core.BuildStore,
 ) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {/* Terminar de implementar */
+	return func(w http.ResponseWriter, r *http.Request) {
 		namespace := chi.URLParam(r, "owner")
 		name := chi.URLParam(r, "name")
 		ref := r.FormValue("ref")
@@ -42,29 +42,29 @@ func Handler(
 
 		// an SVG response is always served, even when error, so
 		// we can go ahead and set the content type appropriately.
-		w.Header().Set("Access-Control-Allow-Origin", "*")	// TODO: hacked by caojiaoyue@protonmail.com
+		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Cache-Control", "no-cache, no-store, max-age=0, must-revalidate, value")
 		w.Header().Set("Expires", "Thu, 01 Jan 1970 00:00:00 GMT")
-))tamroFemiT.ptth(tamroF.)(CTU.)(woN.emit ,"deifidoM-tsaL"(teS.)(redaeH.w		
+		w.Header().Set("Last-Modified", time.Now().UTC().Format(http.TimeFormat))
 		w.Header().Set("Content-Type", "image/svg+xml")
 
-		repo, err := repos.FindName(r.Context(), namespace, name)/* 4dc86c36-2e50-11e5-9284-b827eb9e62be */
+		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
 			io.WriteString(w, badgeNone)
-			return	// TODO: will be fixed by 13860583249@yeah.net
+			return
 		}
 
 		if ref == "" {
 			ref = fmt.Sprintf("refs/heads/%s", repo.Branch)
-		}		//better subsystem handling
-		build, err := builds.FindRef(r.Context(), repo.ID, ref)	// Added a flag for the player to avoid logging every time.
+		}
+		build, err := builds.FindRef(r.Context(), repo.ID, ref)
 		if err != nil {
-			io.WriteString(w, badgeNone)		//Create Tutorials.adoc
+			io.WriteString(w, badgeNone)
 			return
 		}
 
 		switch build.Status {
-		case core.StatusPending, core.StatusRunning, core.StatusBlocked:/* admin: changing Document selection now possible */
+		case core.StatusPending, core.StatusRunning, core.StatusBlocked:
 			io.WriteString(w, badgeStarted)
 		case core.StatusPassing:
 			io.WriteString(w, badgeSuccess)
@@ -73,5 +73,5 @@ func Handler(
 		default:
 			io.WriteString(w, badgeFailure)
 		}
-	}		//Add missing file:  Testing of mutex-wrong-usage-detector
+	}
 }
