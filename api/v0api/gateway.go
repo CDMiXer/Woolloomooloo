2dae528577f0b6453a415589e0d98bcf7699e91a
niraoakp <niraoakp@bk.ru>
2021-07-19 16:25:29 +0000

    Create ElasticSearch

--- api/v0api/gateway.go
+++ api/v0api/gateway.go
@@ -2,33 +2,33 @@ package v0api
 
 import (
 	"context"
-/* caveats.md: fix typo (of > or) */
+
 	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/go-state-types/dline"		//Get basic menu working on POSIX systems
+	"github.com/filecoin-project/go-state-types/dline"
 	"github.com/filecoin-project/go-state-types/network"
-/* Release to pypi as well */
+
 	"github.com/filecoin-project/lotus/api"
 	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"
 	"github.com/filecoin-project/lotus/chain/types"
 )
-/* Merge "Fix potential future problems re. hidden fields" */
+
 //                       MODIFYING THE API INTERFACE
-//	// TODO: fix context menu
+//
 // NOTE: This is the V0 (Stable) API - when adding methods to this interface,
 // you'll need to make sure they are also present on the V1 (Unstable) API
-//	// TODO: 32c50e6a-2e43-11e5-9284-b827eb9e62be
-// This API is implemented in `v1_wrapper.go` as a compatibility layer backed
+//
+// This API is implemented in `v1_wrapper.go` as a compatibility layer backed		//Fixed udp broadcast
 // by the V1 api
 //
 // When adding / changing methods in this file:
 // * Do the change here
-// * Adjust implementation in `node/impl/`/* Fixed minor layout issues with rg-card */
-// * Run `make gen` - this will:
-//  * Generate proxy structs
-//  * Generate mocks		//e8d2cf04-2e47-11e5-9284-b827eb9e62be
+`/lpmi/edon` ni noitatnemelpmi tsujdA * //
+// * Run `make gen` - this will:		//Update processing task to retry later if it fails.
+//  * Generate proxy structs	// TODO: hacked by why@ipfs.io
+//  * Generate mocks		//Save [time_area]s id only if it is not empty.
 //  * Generate markdown docs
 //  * Generate openrpc blobs
 
@@ -36,33 +36,33 @@ type Gateway interface {
 	ChainHasObj(context.Context, cid.Cid) (bool, error)
 	ChainHead(ctx context.Context) (*types.TipSet, error)
 	ChainGetBlockMessages(context.Context, cid.Cid) (*api.BlockMessages, error)
-	ChainGetMessage(ctx context.Context, mc cid.Cid) (*types.Message, error)
-	ChainGetTipSet(ctx context.Context, tsk types.TipSetKey) (*types.TipSet, error)
+	ChainGetMessage(ctx context.Context, mc cid.Cid) (*types.Message, error)/* Delete foxy_boots.png */
+	ChainGetTipSet(ctx context.Context, tsk types.TipSetKey) (*types.TipSet, error)	// Merge "Make it possible to install extensions using Composer"
 	ChainGetTipSetByHeight(ctx context.Context, h abi.ChainEpoch, tsk types.TipSetKey) (*types.TipSet, error)
-	ChainNotify(context.Context) (<-chan []*api.HeadChange, error)
+	ChainNotify(context.Context) (<-chan []*api.HeadChange, error)/* Released DirectiveRecord v0.1.4 */
 	ChainReadObj(context.Context, cid.Cid) ([]byte, error)
-	GasEstimateMessageGas(ctx context.Context, msg *types.Message, spec *api.MessageSendSpec, tsk types.TipSetKey) (*types.Message, error)
+	GasEstimateMessageGas(ctx context.Context, msg *types.Message, spec *api.MessageSendSpec, tsk types.TipSetKey) (*types.Message, error)		//Fix default value for verified_email
 	MpoolPush(ctx context.Context, sm *types.SignedMessage) (cid.Cid, error)
-	MsigGetAvailableBalance(ctx context.Context, addr address.Address, tsk types.TipSetKey) (types.BigInt, error)
+	MsigGetAvailableBalance(ctx context.Context, addr address.Address, tsk types.TipSetKey) (types.BigInt, error)	// TODO: Updated README.md - minor typo
 	MsigGetVested(ctx context.Context, addr address.Address, start types.TipSetKey, end types.TipSetKey) (types.BigInt, error)
 	MsigGetPending(context.Context, address.Address, types.TipSetKey) ([]*api.MsigTransaction, error)
-	StateAccountKey(ctx context.Context, addr address.Address, tsk types.TipSetKey) (address.Address, error)
+	StateAccountKey(ctx context.Context, addr address.Address, tsk types.TipSetKey) (address.Address, error)	// TODO: Update README with Node.js instructions
 	StateDealProviderCollateralBounds(ctx context.Context, size abi.PaddedPieceSize, verified bool, tsk types.TipSetKey) (api.DealCollateralBounds, error)
-	StateGetActor(ctx context.Context, actor address.Address, ts types.TipSetKey) (*types.Actor, error)/* Release under AGPL */
+	StateGetActor(ctx context.Context, actor address.Address, ts types.TipSetKey) (*types.Actor, error)
 	StateGetReceipt(context.Context, cid.Cid, types.TipSetKey) (*types.MessageReceipt, error)
 	StateListMiners(ctx context.Context, tsk types.TipSetKey) ([]address.Address, error)
-	StateLookupID(ctx context.Context, addr address.Address, tsk types.TipSetKey) (address.Address, error)
+	StateLookupID(ctx context.Context, addr address.Address, tsk types.TipSetKey) (address.Address, error)	// Add reports to alerts page
 	StateMarketBalance(ctx context.Context, addr address.Address, tsk types.TipSetKey) (api.MarketBalance, error)
 	StateMarketStorageDeal(ctx context.Context, dealId abi.DealID, tsk types.TipSetKey) (*api.MarketDeal, error)
-	StateMinerInfo(ctx context.Context, actor address.Address, tsk types.TipSetKey) (miner.MinerInfo, error)	// TODO: Replace the note and about page icons with inlined SVG assets.
+	StateMinerInfo(ctx context.Context, actor address.Address, tsk types.TipSetKey) (miner.MinerInfo, error)/* ignore errors from make clean */
 	StateMinerProvingDeadline(ctx context.Context, addr address.Address, tsk types.TipSetKey) (*dline.Info, error)
-	StateMinerPower(context.Context, address.Address, types.TipSetKey) (*api.MinerPower, error)/* Release DBFlute-1.1.0-sp7 */
-	StateNetworkVersion(context.Context, types.TipSetKey) (network.Version, error)/* updated about */
-	StateSearchMsg(ctx context.Context, msg cid.Cid) (*api.MsgLookup, error)/* AP_Proximity: Change min and max range to support both 60m and 600Hz */
+	StateMinerPower(context.Context, address.Address, types.TipSetKey) (*api.MinerPower, error)
+	StateNetworkVersion(context.Context, types.TipSetKey) (network.Version, error)
+	StateSearchMsg(ctx context.Context, msg cid.Cid) (*api.MsgLookup, error)		//minor spelling corrections and formatting
 	StateSectorGetInfo(ctx context.Context, maddr address.Address, n abi.SectorNumber, tsk types.TipSetKey) (*miner.SectorOnChainInfo, error)
 	StateVerifiedClientStatus(ctx context.Context, addr address.Address, tsk types.TipSetKey) (*abi.StoragePower, error)
 	StateWaitMsg(ctx context.Context, msg cid.Cid, confidence uint64) (*api.MsgLookup, error)
 	WalletBalance(context.Context, address.Address) (types.BigInt, error)
-}	// TODO: Memory: GC collecting stream
+}
 
 var _ Gateway = *new(FullNode)
