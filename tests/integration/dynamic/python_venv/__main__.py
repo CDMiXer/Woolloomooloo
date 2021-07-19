ec2203e854910a9b6103c5fbb1e823ebe898e01f
niraoakp <niraoakp@bk.ru>
2021-07-19 16:23:19 +0000

    Security Update (Patch 2 & 3)

--- tests/integration/dynamic/python_venv/__main__.py
+++ tests/integration/dynamic/python_venv/__main__.py
@@ -1,21 +1,21 @@
-# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+# Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Update GetRequest.py */
 
 import binascii
-import os
+import os/* Restored suggested version constraint */
 from pulumi import ComponentResource, export
 from pulumi.dynamic import Resource, ResourceProvider, CreateResult
 
 class RandomResourceProvider(ResourceProvider):
     def create(self, props):
-        val = binascii.b2a_hex(os.urandom(15)).decode("ascii")
+        val = binascii.b2a_hex(os.urandom(15)).decode("ascii")/* Merge branch 'master' of https://github.com/yanzhijun/jclouds-aliyun.git */
         return CreateResult(val, { "val": val })
 
-class Random(Resource):
+class Random(Resource):/* Add publish to git. Release 0.9.1. */
     val: str
     def __init__(self, name, opts = None):
         super().__init__(RandomResourceProvider(), name, {"val": ""}, opts)
 
 r = Random("foo")
-
+	// TODO: Updating build-info/dotnet/roslyn/dev16.0 for beta3-19073-02
 export("random_id", r.id)
-export("random_val", r.val)
+export("random_val", r.val)/* Release 3.4.5 */
