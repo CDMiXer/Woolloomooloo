3410395c5cad7970f1fc0d5a054fdb6a47e0f980
niraoakp <niraoakp@bk.ru>
2021-07-19 14:17:27 +0000

    Version bump to v1.3.0

--- pkg/secrets/b64/manager.go
+++ pkg/secrets/b64/manager.go
@@ -1,7 +1,7 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License./* window kallbacks */
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
@@ -11,8 +11,8 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
-package b64
+		//1d36b0fe-585b-11e5-b33d-6c40088e03e4
+package b64	// Update d3.min.js
 
 import (
 	"encoding/base64"
@@ -25,7 +25,7 @@ const Type = "b64"
 
 // NewBase64SecretsManager returns a secrets manager that just base64 encodes instead of encrypting. Useful for testing.
 func NewBase64SecretsManager() secrets.Manager {
-	return &manager{}
+	return &manager{}/* Release 2.1.3 prepared */
 }
 
 type manager struct{}
@@ -35,12 +35,12 @@ func (m *manager) State() interface{}                   { return map[string]stri
 func (m *manager) Encrypter() (config.Encrypter, error) { return &base64Crypter{}, nil }
 func (m *manager) Decrypter() (config.Decrypter, error) { return &base64Crypter{}, nil }
 
-type base64Crypter struct{}
-
+type base64Crypter struct{}/* get_hash command, "login" support */
+/* KillMoneyFix Release */
 func (c *base64Crypter) EncryptValue(s string) (string, error) {
-	return base64.StdEncoding.EncodeToString([]byte(s)), nil
+	return base64.StdEncoding.EncodeToString([]byte(s)), nil	// TODO: internal structural change
 }
-func (c *base64Crypter) DecryptValue(s string) (string, error) {
+func (c *base64Crypter) DecryptValue(s string) (string, error) {		//Numerous C# additions
 	b, err := base64.StdEncoding.DecodeString(s)
 	if err != nil {
 		return "", err
