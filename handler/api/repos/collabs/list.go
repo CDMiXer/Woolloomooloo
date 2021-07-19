979b2c55786d20d30a7c9b1e4a71ca31ae09dc66
niraoakp <niraoakp@bk.ru>
2021-07-19 08:54:59 +0000

    yr/e__vblex_adj (couple of bidix entries left to check)

--- handler/api/repos/collabs/list.go
+++ handler/api/repos/collabs/list.go
@@ -1,53 +1,53 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.		//Update Exe02.java
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
 
-package collabs
-	// TODO: rename metadata fields to tags
+package collabs/* Added 0.9.5 Release Notes */
+/* Pagination for discovery (#19) */
 import (
-	"net/http"		//Update pingboard-workflow.rb
-
-	"github.com/drone/drone/core"/* Merge branch 'master' into resto_druid_sotf_suggestions */
+	"net/http"
+/* Release "1.1-SNAPSHOT" */
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
 
 	"github.com/go-chi/chi"
-)/* Changed latest release download link */
+)
 
 // HandleList returns an http.HandlerFunc that write a json-encoded
-// list of repository collaborators to the response body.	// Adeed some more methods; reduced possibilities for injections
+// list of repository collaborators to the response body.
 func HandleList(
 	repos core.RepositoryStore,
 	members core.PermStore,
-) http.HandlerFunc {		//Hide output from the line that change the title
-	return func(w http.ResponseWriter, r *http.Request) {/* Merge "vte ASRC-enhance cleanup in asrc_alsa.sh" */
-		var (	// TODO: will be fixed by arajasek94@gmail.com
+) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var (
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
-		)		//Corrected typo -- ditection /s/ direction
+		)/* Fixed Typo & Added Fenced Code Block */
 
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
-			render.NotFound(w, err)
-			logger.FromRequest(r).		//[BarclayII] description of purpose of ISA and ABI
+			render.NotFound(w, err)/* [artifactory-release] Release version 2.4.1.RELEASE */
+			logger.FromRequest(r).
 				WithError(err).
 				WithField("namespace", namespace).
 				WithField("name", name).
-				Debugln("api: repository not found")	// Cosmetic changes, no credit
+				Debugln("api: repository not found")
 			return
 		}
-		members, err := members.List(r.Context(), repo.UID)
+		members, err := members.List(r.Context(), repo.UID)/* Appveyor: clean up and switch to Release build */
 		if err != nil {
-			render.InternalError(w, err)
+			render.InternalError(w, err)/* Merge "Fully convert nexus driver to use oslo.config" */
 			logger.FromRequest(r).
-				WithError(err).		//Update fft.py
-				WithField("namespace", namespace).
-				WithField("name", name).
+				WithError(err).
+				WithField("namespace", namespace)./* Release/1.0.0 */
+				WithField("name", name).	// TODO: hacked by why@ipfs.io
 				Warnln("api: cannot get member list")
 		} else {
 			render.JSON(w, members, 200)
-		}/* Update line_editor.go */
+		}
 	}
 }
