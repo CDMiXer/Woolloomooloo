f2c3c2613e292daedb8d177753b57356ca52fd5e
niraoakp <niraoakp@bk.ru>
2021-07-19 01:00:07 +0000

    Add AGPL license. Because virality.

--- store/logs/s3_oss.go
+++ store/logs/s3_oss.go
@@ -1,22 +1,22 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");		//changed download link
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
+//	// TODO: Delete andrealazarevic.php
+//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: Update search_custompages_include_button.php
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software/* Release of eeacms/www:20.2.20 */
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License./* NaN: templates, jar-type-search */
 
 // +build oss
-
-package logs
-
-import "github.com/drone/drone/core"
+	// Merge branch 'master' into email-translation
+package logs/* Delete Branch hours.md */
+/* Pre-Release of Verion 1.0.8 */
+import "github.com/drone/drone/core"/* Release v1.6.3 */
 
 // New returns a zero value LogStore.
 func NewS3Env(bucket, prefix, endpoint string, pathStyle bool) core.LogStore {
