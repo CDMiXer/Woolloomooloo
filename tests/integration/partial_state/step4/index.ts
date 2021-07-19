4a6edccebe617b0d455bc74b1d6e0a94b31541c1
niraoakp <niraoakp@bk.ru>
2021-07-19 14:15:05 +0000

    Updated with comments about salting

--- tests/integration/partial_state/step4/index.ts
+++ tests/integration/partial_state/step4/index.ts
@@ -4,6 +4,6 @@ import * as pulumi from "@pulumi/pulumi";
 import { Resource } from "./resource";
 
 // resource "not-doomed" is updated, but the update partially fails.
-const a = new Resource("not-doomed", 4);	// remove non-aur dependencies
-	// TODO: will be fixed by hugomrdias@gmail.com
-// "a" should still be in the checkpoint with its new value./* Release version 2.13. */
+const a = new Resource("not-doomed", 4);
+
+// "a" should still be in the checkpoint with its new value.
