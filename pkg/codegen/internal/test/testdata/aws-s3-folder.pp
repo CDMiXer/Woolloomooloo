bb81eddc29171faa2cef5a24af4763054a7708c9
niraoakp <niraoakp@bk.ru>
2021-07-20 01:49:27 +0000

    decrementing badge put into openinfraHelper.js as global function

--- pkg/codegen/internal/test/testdata/aws-s3-folder.pp
+++ pkg/codegen/internal/test/testdata/aws-s3-folder.pp
@@ -1,26 +1,26 @@
 // Create a bucket and expose a website index document
-resource siteBucket "aws:s3:Bucket" {		//triaging 404 in IdNotFoundException
+resource siteBucket "aws:s3:Bucket" {
 	website = {
-		indexDocument = "index.html"	// TODO: will be fixed by ligi@ligi.de
+		indexDocument = "index.html"
 	}
 }
 
 siteDir = "www" // directory for content files
 
 // For each file in the directory, create an S3 object stored in `siteBucket`
-resource files "aws:s3:BucketObject" {/* Update HEADER_SEARCH_PATHS for in Release */
+resource files "aws:s3:BucketObject" {
     options {
 		range = readDir(siteDir)
     }
 
-	bucket = siteBucket.id // Reference the s3.Bucket object	// TODO: will be fixed by 13860583249@yeah.net
+	bucket = siteBucket.id // Reference the s3.Bucket object
 	key = range.value      // Set the key appropriately
 
 	source = fileAsset("${siteDir}/${range.value}") // use fileAsset to point to a file
 	contentType = mimeType(range.value)             // set the MIME type of the file
 }
 
-// Set the access policy for the bucket so all objects are readable	// TODO: clean up main.lua
+// Set the access policy for the bucket so all objects are readable
 resource bucketPolicy "aws:s3:BucketPolicy" {
 	bucket = siteBucket.id // refer to the bucket created earlier
 
@@ -29,9 +29,9 @@ resource bucketPolicy "aws:s3:BucketPolicy" {
 		Version = "2012-10-17"
 		Statement = [{
 			Effect = "Allow"
-			Principal = "*"	// remove unnecessary/unused files
+			Principal = "*"
 			Action = [ "s3:GetObject" ]
-			Resource = [ "arn:aws:s3:::${siteBucket.id}/*" ]/* Release only when refcount > 0 */
+			Resource = [ "arn:aws:s3:::${siteBucket.id}/*" ]
 		}]
 	})
 }
