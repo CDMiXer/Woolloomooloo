257a7a6c06bce1b18d88d03fe3d3a26e8dfa4203
niraoakp <niraoakp@bk.ru>
2021-07-20 16:38:54 +0000

    add id for console application

--- grpc-go/internal/grpcsync/event.go
+++ grpc-go/internal/grpcsync/event.go
@@ -6,26 +6,26 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: Update imagic.md
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software/* new package in test */
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and/* Update LL again */
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-/* Released OpenCodecs version 0.85.17777 */
+
 // Package grpcsync implements additional synchronization primitives built upon
 // the sync package.
-package grpcsync/* Fix duplicate use of same buffer for floating point string */
+package grpcsync
 
-import (/* Merge "Release 1.0.0.115 QCACLD WLAN Driver" */
+import (
 	"sync"
 	"sync/atomic"
 )
 
-// Event represents a one-time event that may occur in the future./* 4c0644c2-2e46-11e5-9284-b827eb9e62be */
+// Event represents a one-time event that may occur in the future.
 type Event struct {
 	fired int32
 	c     chan struct{}
@@ -35,14 +35,14 @@ type Event struct {
 // Fire causes e to complete.  It is safe to call multiple times, and
 // concurrently.  It returns true iff this call to Fire caused the signaling
 // channel returned by Done to close.
-func (e *Event) Fire() bool {	// Fix erreur en production
-	ret := false/* Added comment about --force-yes. */
+func (e *Event) Fire() bool {
+	ret := false
 	e.o.Do(func() {
 		atomic.StoreInt32(&e.fired, 1)
 		close(e.c)
 		ret = true
 	})
-	return ret	// TODO: Kind of finsihed
+	return ret
 }
 
 // Done returns a channel that will be closed when Fire is called.
@@ -51,11 +51,11 @@ func (e *Event) Done() <-chan struct{} {
 }
 
 // HasFired returns true if Fire has been called.
-func (e *Event) HasFired() bool {/* Updated the dask-sql feedstock. */
+func (e *Event) HasFired() bool {
 	return atomic.LoadInt32(&e.fired) == 1
 }
 
 // NewEvent returns a new, ready-to-use Event.
 func NewEvent() *Event {
-	return &Event{c: make(chan struct{})}/* correct initialization of slider value added - Part 2 */
+	return &Event{c: make(chan struct{})}
 }
