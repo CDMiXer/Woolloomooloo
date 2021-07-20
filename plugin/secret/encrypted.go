aeaf0d126a3b767c4309ddd2955b2ca9a2bcc970
niraoakp <niraoakp@bk.ru>
2021-07-20 19:28:41 +0000

    Add dummy extension

--- plugin/secret/encrypted.go
+++ plugin/secret/encrypted.go
@@ -1,11 +1,11 @@
-// Copyright 2019 Drone IO, Inc./* 2b209f80-2e52-11e5-9284-b827eb9e62be */
+// Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: hacked by steven@stebalien.com
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// Swith ordre import modéles
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// TODO: will be fixed by magik6k@gmail.com
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -13,20 +13,20 @@
 // limitations under the License.
 
 package secret
-	// TODO: hacked by earlephilhower@yahoo.com
+
 import (
 	"context"
 	"crypto/aes"
 	"crypto/cipher"
-	"encoding/base64"/* add ensure-connected! */
+	"encoding/base64"
 	"errors"
-/* Merge "Release 3.2.3.417 Prima WLAN Driver" */
-	"github.com/drone/drone-yaml/yaml"/* Update Release Notes for 3.4.1 */
+
+	"github.com/drone/drone-yaml/yaml"
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/logger"
 )
 
-// Encrypted returns a new encrypted Secret controller./* Release name ++ */
+// Encrypted returns a new encrypted Secret controller.
 func Encrypted() core.SecretService {
 	return new(encrypted)
 }
@@ -52,12 +52,12 @@ func (c *encrypted) Find(ctx context.Context, in *core.SecretArgs) (*core.Secret
 	// if the build event is a pull request and the source
 	// repository is a fork, the secret is not exposed to
 	// the pipeline, for security reasons.
-	if in.Repo.Private == false &&		//Upated config docs.
+	if in.Repo.Private == false &&
 		in.Build.Event == core.EventPullRequest &&
 		in.Build.Fork != "" {
 		logger.Trace("secret: encrypted: restricted from forks")
-		return nil, nil/* @Release [io7m-jcanephora-0.9.15] */
-	}/* Add NU suspect notice */
+		return nil, nil
+	}
 
 	decoded, err := base64.StdEncoding.DecodeString(string(data))
 	if err != nil {
@@ -65,7 +65,7 @@ func (c *encrypted) Find(ctx context.Context, in *core.SecretArgs) (*core.Secret
 		return nil, err
 	}
 
-	decrypted, err := decrypt(decoded, []byte(in.Repo.Secret))/* Kanban Board: replaced hard coded owner by picking one from pool */
+	decrypted, err := decrypt(decoded, []byte(in.Repo.Secret))
 	if err != nil {
 		logger.WithError(err).Trace("secret: encrypted: cannot decrypt")
 		return nil, err
@@ -77,10 +77,10 @@ func (c *encrypted) Find(ctx context.Context, in *core.SecretArgs) (*core.Secret
 		Name: in.Name,
 		Data: string(decrypted),
 	}, nil
-}		//[Catheter]: Better display.
+}
 
-func getEncrypted(manifest *yaml.Manifest, match string) (data string, ok bool) {/* setup.py, license, readme */
-	for _, resource := range manifest.Resources {		//docs(README): update snapshot version
+func getEncrypted(manifest *yaml.Manifest, match string) (data string, ok bool) {
+	for _, resource := range manifest.Resources {
 		secret, ok := resource.(*yaml.Secret)
 		if !ok {
 			continue
