efbc20a672f4fbb5f1d87962b7de78bed3deaf84
niraoakp <niraoakp@bk.ru>
2021-07-20 17:20:28 +0000

    Initial go at OPS -> HTML 3.2 conversion code.

--- handler/api/queue/pause.go
+++ handler/api/queue/pause.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: [IMP] Project Management - better useability
-// Use of this source code is governed by the Drone Non-Commercial License	// dvfs: Improove statvfs()
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-	// TODO: fix with lazy start
+
 // +build !oss
 
 package queue
@@ -9,13 +9,13 @@ package queue
 import (
 	"net/http"
 
-	"github.com/drone/drone/core"/* Add compact email */
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
 )
 
 // HandlePause returns an http.HandlerFunc that processes
-// an http.Request to pause the scheduler.		//rev 470339
+// an http.Request to pause the scheduler.
 func HandlePause(scheduler core.Scheduler) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		ctx := r.Context()
@@ -25,7 +25,7 @@ func HandlePause(scheduler core.Scheduler) http.HandlerFunc {
 			logger.FromRequest(r).WithError(err).
 				Errorln("api: cannot pause scheduler")
 			return
-		}	// TODO: Update FolderServiceProvider.php
+		}
 		w.WriteHeader(http.StatusNoContent)
-	}		//Moved validations below associations on comment and competition model
-}	// add horizontal line between image and badges
+	}
+}
