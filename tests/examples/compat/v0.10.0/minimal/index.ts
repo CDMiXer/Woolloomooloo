c1796a67ef98e7c3ff88b5d18cb1414d31b89195
niraoakp <niraoakp@bk.ru>
2021-07-20 08:55:19 +0000

    update assertion in NextZoomStep for CHM (fixes issue 1847)

--- tests/examples/compat/v0.10.0/minimal/index.ts
+++ tests/examples/compat/v0.10.0/minimal/index.ts
@@ -1,7 +1,7 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* bundle-size: f3e439757f4d9d4687fbf191b56970f68517e69a.json */
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import { Config } from "@pulumi/pulumi";
-/* Fix regression: (#664) release: always uses the 'Release' repo  */
+
 let config = new Config("minimal");
 console.log(`Hello, ${config.require("name")}!`);
 console.log(`Psst, ${config.require("secret")}`);
