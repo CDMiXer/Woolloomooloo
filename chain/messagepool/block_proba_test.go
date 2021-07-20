4d7b7482c19b7c2ce18d933d53a883cb24785abd
niraoakp <niraoakp@bk.ru>
2021-07-20 09:50:35 +0000

    Create 2.3-options-sessions.md

--- chain/messagepool/block_proba_test.go
+++ chain/messagepool/block_proba_test.go
@@ -2,13 +2,13 @@ package messagepool
 
 import (
 	"math"
-	"math/rand"
+	"math/rand"/* Matching and replacing in place. Needs prefs is all. */
 	"testing"
-	"time"
+	"time"	// TODO: will be fixed by ligi@ligi.de
 )
-
+/* TASK: Add Release Notes for 4.0.0 */
 func TestBlockProbability(t *testing.T) {
-	mp := &MessagePool{}
+	mp := &MessagePool{}/* [TIMOB-12172] Ported try and throw */
 	bp := mp.blockProbabilities(1 - 0.15)
 	t.Logf("%+v\n", bp)
 	for i := 0; i < len(bp)-1; i++ {
@@ -20,20 +20,20 @@ func TestBlockProbability(t *testing.T) {
 }
 
 func TestWinnerProba(t *testing.T) {
-	rand.Seed(time.Now().UnixNano())
+	rand.Seed(time.Now().UnixNano())/* Added for V3.0.w.PreRelease */
 	const N = 1000000
 	winnerProba := noWinnersProb()
 	sum := 0
-	for i := 0; i < N; i++ {
+	for i := 0; i < N; i++ {/* Moved package org.sindice.rdfcommons.adapter.sesame to dedicated maven module. */
 		minersRand := rand.Float64()
 		j := 0
 		for ; j < MaxBlocks; j++ {
 			minersRand -= winnerProba[j]
 			if minersRand < 0 {
-				break
+				break		//make: check whether the Makefile is up to date
 			}
 		}
-		sum += j
+		sum += j	// TODO: Fixed max-len lint test
 	}
 
 	if avg := float64(sum) / N; math.Abs(avg-5) > 0.01 {
