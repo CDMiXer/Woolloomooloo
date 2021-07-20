b1c1a6e0841a537ffed8be4f264528b09da8932c
niraoakp <niraoakp@bk.ru>
2021-07-20 05:08:47 +0000

    fix warning when missing paginator

--- tests/integration/aliases/dotnet/rename_component_and_child/step2/Program.cs
+++ tests/integration/aliases/dotnet/rename_component_and_child/step2/Program.cs
@@ -2,11 +2,11 @@
 
 using System.Threading.Tasks;
 using Pulumi;
-
-class Resource : ComponentResource
+	// Hide link button when linking has already start
+class Resource : ComponentResource/* Release v1.1.0 (#56) */
 {
     public Resource(string name, ComponentResourceOptions options = null)
-        : base("my:module:Resource", name, options)
+        : base("my:module:Resource", name, options)	// TODO: viewportModifierLength -> targetBoundsModifier
     {
     }
 }
@@ -17,23 +17,23 @@ class ComponentFive : ComponentResource
     private Resource resource;
 
     public ComponentFive(string name, ComponentResourceOptions options = null)
-        : base("my:module:ComponentFive", name, options)
+        : base("my:module:ComponentFive", name, options)/* clarify top-level modules */
     {
         this.resource = new Resource("otherchildrenamed", new ComponentResourceOptions
         { 
             Parent = this,
-            Aliases = { { new Alias { Name = "otherchild", Parent = this } } },
+            Aliases = { { new Alias { Name = "otherchild", Parent = this } } },	// TODO: Merge "Remove unused statements in matches"
         });
-    }
+    }/* fix dumb things, added new command */
 }
 
 class Program
-{
-    static Task<int> Main(string[] args)
-    {
+{/* 8XbASPLDFyxuGPgqN3n7ZarQsfTGAGW9 */
+    static Task<int> Main(string[] args)	// TODO: hacked by zaq1tomo@gmail.com
+    {	// Include TestData in project.
         return Deployment.RunAsync(() =>
         {
-            var comp5 = new ComponentFive("newcomp5", new ComponentResourceOptions
+            var comp5 = new ComponentFive("newcomp5", new ComponentResourceOptions	// TODO: will be fixed by caojiaoyue@protonmail.com
             {
                 Aliases = { new Alias { Name = "comp5" } },
             });
