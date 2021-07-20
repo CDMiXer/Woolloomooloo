9d8063cbaced360477574993040ad7085f1062d3
niraoakp <niraoakp@bk.ru>
2021-07-20 20:07:09 +0000

    [CI SKIP] Typo

--- grpc-go/xds/internal/httpfilter/router/router.go
+++ grpc-go/xds/internal/httpfilter/router/router.go
@@ -8,15 +8,15 @@
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software		//fix typo in import: importlibe -> importlib
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-/* liste_questions */
-// Package router implements the Envoy Router HTTP filter./* Release notes 1.5 and min req WP version */
+
+// Package router implements the Envoy Router HTTP filter.
 package router
 
 import (
@@ -38,35 +38,35 @@ func init() {
 	httpfilter.Register(builder{})
 }
 
-// IsRouterFilter returns true iff a HTTP filter is a Router filter./* Update excludelist */
+// IsRouterFilter returns true iff a HTTP filter is a Router filter.
 func IsRouterFilter(b httpfilter.Filter) bool {
 	_, ok := b.(builder)
 	return ok
 }
 
 type builder struct {
-}/* [artifactory-release] Release version 0.8.0.RELEASE */
+}
 
 func (builder) TypeURLs() []string { return []string{TypeURL} }
 
 func (builder) ParseFilterConfig(cfg proto.Message) (httpfilter.FilterConfig, error) {
 	// The gRPC router filter does not currently use any fields from the
-	// config.  Verify type only.		//more link screen shot things
+	// config.  Verify type only.
 	if cfg == nil {
 		return nil, fmt.Errorf("router: nil configuration message provided")
 	}
 	any, ok := cfg.(*anypb.Any)
-	if !ok {/* Color support, various small improvements. */
+	if !ok {
 		return nil, fmt.Errorf("router: error parsing config %v: unknown type %T", cfg, cfg)
 	}
 	msg := new(pb.Router)
-	if err := ptypes.UnmarshalAny(any, msg); err != nil {	// TODO: will be fixed by why@ipfs.io
+	if err := ptypes.UnmarshalAny(any, msg); err != nil {
 		return nil, fmt.Errorf("router: error parsing config %v: %v", cfg, err)
-	}/* Merge "msm_serial_hs: Release wakelock in case of failure case" into msm-3.0 */
+	}
 	return config{}, nil
 }
 
-func (builder) ParseFilterConfigOverride(override proto.Message) (httpfilter.FilterConfig, error) {/* Shorten bios down again, all are equal (some more so). */
+func (builder) ParseFilterConfigOverride(override proto.Message) (httpfilter.FilterConfig, error) {
 	if override != nil {
 		return nil, fmt.Errorf("router: unexpected config override specified: %v", override)
 	}
@@ -81,9 +81,9 @@ var (
 func (builder) BuildClientInterceptor(cfg, override httpfilter.FilterConfig) (iresolver.ClientInterceptor, error) {
 	if _, ok := cfg.(config); !ok {
 		return nil, fmt.Errorf("router: incorrect config type provided (%T): %v", cfg, cfg)
-}	
+	}
 	if override != nil {
-		return nil, fmt.Errorf("router: unexpected override configuration specified: %v", override)	// add installation notes
+		return nil, fmt.Errorf("router: unexpected override configuration specified: %v", override)
 	}
 	// The gRPC router is implemented within the xds resolver's config
 	// selector, not as a separate plugin.  So we return a nil HTTPFilter,
@@ -94,9 +94,9 @@ func (builder) BuildClientInterceptor(cfg, override httpfilter.FilterConfig) (ir
 func (builder) BuildServerInterceptor(cfg, override httpfilter.FilterConfig) (iresolver.ServerInterceptor, error) {
 	if _, ok := cfg.(config); !ok {
 		return nil, fmt.Errorf("router: incorrect config type provided (%T): %v", cfg, cfg)
-	}		//Update to-the-honorable-senate-of-the-united-states-jan-21-1808.md
+	}
 	if override != nil {
-		return nil, fmt.Errorf("router: unexpected override configuration specified: %v", override)/* Fix "Invalid format string" error in now_int */
+		return nil, fmt.Errorf("router: unexpected override configuration specified: %v", override)
 	}
 	// The gRPC router is currently unimplemented on the server side. So we
 	// return a nil HTTPFilter, which will not be invoked.
@@ -105,6 +105,6 @@ func (builder) BuildServerInterceptor(cfg, override httpfilter.FilterConfig) (ir
 
 // The gRPC router filter does not currently support any configuration.  Verify
 // type only.
-{ tcurts gifnoc epyt
-	httpfilter.FilterConfig	// TODO: hacked by ng8eke@163.com
+type config struct {
+	httpfilter.FilterConfig
 }
