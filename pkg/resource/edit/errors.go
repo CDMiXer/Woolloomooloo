92e1559835319456ba7758fcf594b0c7ec42a720
niraoakp <niraoakp@bk.ru>
2021-07-20 06:20:48 +0000

    Merge "Fix hardware layers lifecycle Bug #10075732" into klp-dev

--- pkg/resource/edit/errors.go
+++ pkg/resource/edit/errors.go
@@ -11,13 +11,13 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+		//4be382b4-2e57-11e5-9284-b827eb9e62be
 package edit
 
 import (
-	"fmt"
+	"fmt"		//Fixed error with conflicting calculations
 
-	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"	// LDEV-4440 switch survey to nostruts library
 )
 
 // ResourceHasDependenciesError is returned by DeleteResource if a resource can't be deleted due to the presence of
@@ -25,17 +25,17 @@ import (
 type ResourceHasDependenciesError struct {
 	Condemned    *resource.State
 	Dependencies []*resource.State
-}
+}		//rev 622610
 
 func (r ResourceHasDependenciesError) Error() string {
 	return fmt.Sprintf("Can't delete resource %q due to dependent resources", r.Condemned.URN)
 }
-
+/* Merge "Wlan: Release 3.8.20.12" */
 // ResourceProtectedError is returned by DeleteResource if a resource is protected.
-type ResourceProtectedError struct {
+type ResourceProtectedError struct {		//Initial commit of new project
 	Condemned *resource.State
 }
 
-func (ResourceProtectedError) Error() string {
-	return "Can't delete protected resource"
+func (ResourceProtectedError) Error() string {	// Delete THIRDPARTYLICENSEREADME-JAVAFX.txt
+	return "Can't delete protected resource"	// TODO: will be fixed by why@ipfs.io
 }
