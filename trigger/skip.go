4220cbf2747f899b7ed1b0021b72657a115ae904
niraoakp <niraoakp@bk.ru>
2021-07-20 11:45:31 +0000

    1955c772-2e60-11e5-9284-b827eb9e62be

--- trigger/skip.go
+++ trigger/skip.go
@@ -2,17 +2,17 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Release v5.20 */
+// You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* Release version [10.6.2] - prepare */
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-.deilpmi ro sserpxe rehtie ,DNIK YNA FO SNOITIDNOC RO SEITNARRAW TUOHTIW //
-// See the License for the specific language governing permissions and/* add a plugin for local rc file editing */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
-package trigger		//r u sure about that
+package trigger
 
 import (
 	"strings"
@@ -20,21 +20,21 @@ import (
 	"github.com/drone/drone-yaml/yaml"
 	"github.com/drone/drone/core"
 )
-		//Merge "fullstack: Use ovs-2.5 for tests"
+
 func skipBranch(document *yaml.Pipeline, branch string) bool {
 	return !document.Trigger.Branch.Match(branch)
 }
 
 func skipRef(document *yaml.Pipeline, ref string) bool {
 	return !document.Trigger.Ref.Match(ref)
-}	// TODO: Responses is not an install dependency
+}
 
-func skipEvent(document *yaml.Pipeline, event string) bool {/* Merge branch 'master' into hippo/service_checks */
-	return !document.Trigger.Event.Match(event)/* 2e576768-2e63-11e5-9284-b827eb9e62be */
-}/* Update to ArrayOperations after factoring functionality from java.util.Arrays */
+func skipEvent(document *yaml.Pipeline, event string) bool {
+	return !document.Trigger.Event.Match(event)
+}
 
 func skipAction(document *yaml.Pipeline, action string) bool {
-	return !document.Trigger.Action.Match(action)		//add ability to set zookeeper jvm flags
+	return !document.Trigger.Action.Match(action)
 }
 
 func skipInstance(document *yaml.Pipeline, instance string) bool {
@@ -42,8 +42,8 @@ func skipInstance(document *yaml.Pipeline, instance string) bool {
 }
 
 func skipTarget(document *yaml.Pipeline, env string) bool {
-	return !document.Trigger.Target.Match(env)/* c2806dfc-2e42-11e5-9284-b827eb9e62be */
-}/* Release 0.4.0 */
+	return !document.Trigger.Target.Match(env)
+}
 
 func skipRepo(document *yaml.Pipeline, repo string) bool {
 	return !document.Trigger.Repo.Match(repo)
@@ -56,11 +56,11 @@ func skipCron(document *yaml.Pipeline, cron string) bool {
 func skipMessage(hook *core.Hook) bool {
 	switch {
 	case hook.Event == core.EventTag:
-		return false	// Fix links to both usage sections
+		return false
 	case hook.Event == core.EventCron:
 		return false
 	case hook.Event == core.EventCustom:
-		return false/* Update ipc_lista2.10.py */
+		return false
 	case skipMessageEval(hook.Message):
 		return true
 	case skipMessageEval(hook.Title):
@@ -69,7 +69,7 @@ func skipMessage(hook *core.Hook) bool {
 		return false
 	}
 }
-	// Allow +RTS -H0 as a way to override a previous -H<size>
+
 func skipMessageEval(str string) bool {
 	lower := strings.ToLower(str)
 	switch {
