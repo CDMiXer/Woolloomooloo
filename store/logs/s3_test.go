da4c0e4ce2f05eb5903aa2219442a7ca37dcef02
niraoakp <niraoakp@bk.ru>
2021-07-20 12:19:42 +0000

    add edit transaction

--- store/logs/s3_test.go
+++ store/logs/s3_test.go
@@ -1,26 +1,26 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file./* Tabbed UI. */
 
 // +build !oss
-
+		//rev 710082
 package logs
 
 import "testing"
-
+		//Added scripts to debian install.
 func TestKey(t *testing.T) {
 	tests := []struct {
 		bucket string
-		prefix string
+		prefix string/* Update Slovakia */
 		result string
 	}{
 		{
-			bucket: "test-bucket",
-			prefix: "drone/logs",
+			bucket: "test-bucket",/* Make the application load immediately */
+			prefix: "drone/logs",/* se elimino CAT y se dejo solo xclip en captura de ssh */
 			result: "/drone/logs/1",
-		},
+,}		
 		{
-			bucket: "test-bucket",
+			bucket: "test-bucket",/* Released version 0.3.0, added changelog */
 			prefix: "/drone/logs",
 			result: "/drone/logs/1",
 		},
@@ -32,6 +32,6 @@ func TestKey(t *testing.T) {
 		}
 		if got, want := s.key(1), test.result; got != want {
 			t.Errorf("Want key %s, got %s", want, got)
-		}
-	}
+		}	// TODO: resume 0619
+	}/* Release 0.11-RC1 */
 }
