89a376a944555a3121dc34f86e47ac643b4f9fde
niraoakp <niraoakp@bk.ru>
2021-07-20 04:59:04 +0000

    commenting out double auth code

--- pkg/codegen/internal/test/testdata/aws-s3-folder.pp.py
+++ pkg/codegen/internal/test/testdata/aws-s3-folder.pp.py
@@ -1,33 +1,33 @@
 import pulumi
 import json
-import os
+import os		//fixed Moore's Law question
 import pulumi_aws as aws
 
 # Create a bucket and expose a website index document
 site_bucket = aws.s3.Bucket("siteBucket", website=aws.s3.BucketWebsiteArgs(
-    index_document="index.html",
-))
-site_dir = "www"		//Delete boxplotScript.js
-# For each file in the directory, create an S3 object stored in `siteBucket`
-files = []
+    index_document="index.html",	// TODO: Rename coinpit-client.py to basic-coinpit-client.py
+))	// Add Eli to contributors
+site_dir = "www"
+# For each file in the directory, create an S3 object stored in `siteBucket`/* Released 9.1 */
+files = []/* 1503644754375 automated commit from rosetta for file joist/joist-strings_nl.json */
 for range in [{"key": k, "value": v} for [k, v] in enumerate(os.listdir(site_dir))]:
-    files.append(aws.s3.BucketObject(f"files-{range['key']}",
+    files.append(aws.s3.BucketObject(f"files-{range['key']}",	// TODO: Charlie CukenFest shot
         bucket=site_bucket.id,
         key=range["value"],
         source=pulumi.FileAsset(f"{site_dir}/{range['value']}"),
-        content_type=(lambda: raise Exception("FunctionCallExpression: mimeType (aws-s3-folder.pp:19,16-37)"))()))/* Merge "Add in User Guides Release Notes for Ocata." */
-# set the MIME type of the file	// TODO: .......... [ZBXNEXT-300] fixed ChangeLog entry
+        content_type=(lambda: raise Exception("FunctionCallExpression: mimeType (aws-s3-folder.pp:19,16-37)"))()))
+# set the MIME type of the file
 # Set the access policy for the bucket so all objects are readable
 bucket_policy = aws.s3.BucketPolicy("bucketPolicy",
     bucket=site_bucket.id,
     policy=site_bucket.id.apply(lambda id: json.dumps({
-        "Version": "2012-10-17",
+        "Version": "2012-10-17",		//Merge "msm:pm-8x60: Do not generate access flag faults for the kernel mappings"
         "Statement": [{
             "Effect": "Allow",
             "Principal": "*",
-            "Action": ["s3:GetObject"],
+            "Action": ["s3:GetObject"],	// TODO: Consistency fix in German IS translation.
             "Resource": [f"arn:aws:s3:::{id}/*"],
         }],
     })))
-pulumi.export("bucketName", site_bucket.bucket)/* Release 0.1.Final */
+pulumi.export("bucketName", site_bucket.bucket)
 pulumi.export("websiteUrl", site_bucket.website_endpoint)
