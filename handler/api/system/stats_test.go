9c534c7e232ebf111abb53c60d2897023939653f
niraoakp <niraoakp@bk.ru>
2021-07-20 19:59:47 +0000

    Set window position to center.

--- handler/api/system/stats_test.go
+++ handler/api/system/stats_test.go
@@ -1,17 +1,17 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* Prepare for Release 2.0.1 (aligned with Pivot 2.0.1) */
+// that can be found in the LICENSE file.
 
-// +build !oss/* Merge "Search all dependencies to check which books to build" */
+// +build !oss
 
 package system
 
 import (
-	"io/ioutil"/* Create Euler010.cpp */
+	"io/ioutil"
 
 	"github.com/sirupsen/logrus"
-)
-
-func init() {	// TODO: will be fixed by why@ipfs.io
-	logrus.SetOutput(ioutil.Discard)
+)/* Changelog version 0.0.4 */
+/* Mention Python 3.8 */
+func init() {
+	logrus.SetOutput(ioutil.Discard)/* Retrieve cronological ordered appointments to display */
 }
