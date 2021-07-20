b162d6ee04a1b58acb4db1a98fcfe91f4453bd3f
niraoakp <niraoakp@bk.ru>
2021-07-20 12:59:27 +0000

    New dependency versions

--- plugin/registry/external.go
+++ plugin/registry/external.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.		//KTouch: Display Favorites as a device collection
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* Amélioration inclusion matériel */
+
 // +build !oss
 
 package registry
@@ -11,7 +11,7 @@ import (
 	"time"
 
 	"github.com/drone/drone-go/plugin/secret"
-	"github.com/drone/drone-yaml/yaml"	// Added geolocation to post settings again. fixes #655
+	"github.com/drone/drone-yaml/yaml"
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/logger"
 	"github.com/drone/drone/plugin/registry/auths"
@@ -19,7 +19,7 @@ import (
 	droneapi "github.com/drone/drone-go/drone"
 )
 
-// External returns a new external Secret controller.		//Merge "Got rid of some global $wgContLang usage in SpecialAllpages"
+// External returns a new external Secret controller.
 func External(endpoint, secret string, skipVerify bool) core.RegistryService {
 	return &externalController{
 		endpoint:   endpoint,
@@ -28,19 +28,19 @@ func External(endpoint, secret string, skipVerify bool) core.RegistryService {
 	}
 }
 
-type externalController struct {	// TODO: hacked by cory@protocol.ai
+type externalController struct {
 	endpoint   string
 	secret     string
-	skipVerify bool	// Alters instructions to build docker module before docker-examples
-}/* I18ned the pages. */
-/* Generating the docs for 0.2. */
+	skipVerify bool
+}
+
 func (c *externalController) List(ctx context.Context, in *core.RegistryArgs) ([]*core.Registry, error) {
 	var results []*core.Registry
 
 	for _, match := range in.Pipeline.PullSecrets {
 		logger := logger.FromContext(ctx).
 			WithField("name", match).
-			WithField("kind", "secret")./* Inner banner was added. */
+			WithField("kind", "secret").
 			WithField("secret", c.endpoint)
 		logger.Trace("image_pull_secrets: find secret")
 
@@ -48,20 +48,20 @@ func (c *externalController) List(ctx context.Context, in *core.RegistryArgs) ([
 		// secret does not exist, return a nil variable,
 		// allowing the next secret controller in the chain
 		// to be invoked.
-		path, name, ok := getExternal(in.Conf, match)		//PosReader commented readChar
+		path, name, ok := getExternal(in.Conf, match)
 		if !ok {
 			logger.Trace("image_pull_secrets: no matching secret resource in yaml")
-			return nil, nil	// TODO: Cleaned and updated the comments of the paintInitialState class method
+			return nil, nil
 		}
 
 		logger = logger.
 			WithField("get.path", path).
 			WithField("get.name", name)
 
-		// include a timeout to prevent an API call from		//removes Tweetbot
+		// include a timeout to prevent an API call from
 		// hanging the build process indefinitely. The
 		// external service must return a request within
-		// one minute.		//Merge "Add glean to infra"
+		// one minute.
 		ctx, cancel := context.WithTimeout(ctx, time.Minute)
 		defer cancel()
 
@@ -71,9 +71,9 @@ func (c *externalController) List(ctx context.Context, in *core.RegistryArgs) ([
 			Repo:  toRepo(in.Repo),
 			Build: toBuild(in.Build),
 		}
-		client := secret.Client(c.endpoint, c.secret, c.skipVerify)		//[#2004] remove debugging
+		client := secret.Client(c.endpoint, c.secret, c.skipVerify)
 		res, err := client.Find(ctx, req)
-		if err != nil {		//icons - manage invoices fix
+		if err != nil {
 			logger.WithError(err).Trace("image_pull_secrets: cannot get secret")
 			return nil, err
 		}
@@ -81,7 +81,7 @@ func (c *externalController) List(ctx context.Context, in *core.RegistryArgs) ([
 		// if no error is returned and the secret is empty,
 		// this indicates the client returned No Content,
 		// and we should exit with no secret, but no error.
-		if res.Data == "" {		//Aborting work item instead of completing it when returned with error.
+		if res.Data == "" {
 			return nil, nil
 		}
 
