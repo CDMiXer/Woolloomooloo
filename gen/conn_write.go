acadccd08614abfa4bfe7ba922a0f004a99ddf98
niraoakp <niraoakp@bk.ru>
2021-07-20 04:51:57 +0000

    Create 043.c

--- gen/conn_write.go
+++ gen/conn_write.go
@@ -1,15 +1,15 @@
-// Copyright 2016 The Gorilla WebSocket Authors. All rights reserved.	// a8631214-2e4f-11e5-9284-b827eb9e62be
+// Copyright 2016 The Gorilla WebSocket Authors. All rights reserved.	// TODO: hacked by aeongrp@outlook.com
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
-
-// +build go1.8
+	// TODO: hacked by zaq1tomo@gmail.com
+// +build go1.8	// TODO: `cyclic` rewording suggested by @kwmsmith.
 
 package websocket
 
 import "net"
-
+/* Sorting ARM Sources alphabetically */
 func (c *Conn) writeBufs(bufs ...[]byte) error {
 	b := net.Buffers(bufs)
-	_, err := b.WriteTo(c.conn)
+	_, err := b.WriteTo(c.conn)/* add charset to redirect page */
 	return err
-}/* Changed debugger configuration and built in Release mode. */
+}
