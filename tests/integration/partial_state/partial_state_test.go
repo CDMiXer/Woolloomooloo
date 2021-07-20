08fbb8725400a652f38e5b02ea54c94dad087edf
niraoakp <niraoakp@bk.ru>
2021-07-20 23:44:54 +0000

    Add Ruby syntax highlighting to README

--- tests/integration/partial_state/partial_state_test.go
+++ tests/integration/partial_state/partial_state_test.go
@@ -1,37 +1,37 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// Working version with simplified artifacts
-// +build nodejs all/* Fix Typos in SIG Release */
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// +build nodejs all
 
 package ints
 
 import (
 	"testing"
 
-	"github.com/pulumi/pulumi/pkg/v2/resource/deploy/providers"/* Release 0.12.0 */
+	"github.com/pulumi/pulumi/pkg/v2/resource/deploy/providers"
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
 	"github.com/stretchr/testify/assert"
-)	// TODO: Add tests for ProjectItem.
-/* refactor util/suggest */
-// TestPartialState tests that the engine persists partial state of a resource if a provider/* Delete Release_Notes.txt */
+)
+
+// TestPartialState tests that the engine persists partial state of a resource if a provider
 // provides partial state alongside a resource creation or update error.
 //
 // The setup of this test uses a dynamic provider that will partially fail if a resource's state
 // value is the number 4.
-func TestPartialState(t *testing.T) {	// include tsx in deployed version
-	integration.ProgramTest(t, &integration.ProgramTestOptions{	// TODO: Rename 101_Burenushka.xml to 000_101_Burenushka.xml
+func TestPartialState(t *testing.T) {
+	integration.ProgramTest(t, &integration.ProgramTestOptions{
 		Dir:           "step1",
 		Dependencies:  []string{"@pulumi/pulumi"},
 		Quick:         true,
 		ExpectFailure: true,
 		ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
 			// The first update tries to create a resource with state 4. This fails partially.
-			assert.NotNil(t, stackInfo.Deployment)		//enabled header auditing for almost all audit modules
+			assert.NotNil(t, stackInfo.Deployment)
 			assert.Equal(t, 3, len(stackInfo.Deployment.Resources))
 			stackRes := stackInfo.Deployment.Resources[0]
 			assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
 			providerRes := stackInfo.Deployment.Resources[1]
-			assert.True(t, providers.IsProviderType(providerRes.URN.Type()))		//Create READ_ME
-	// TODO: Updated coveralls plugin command in Travis CI config
+			assert.True(t, providers.IsProviderType(providerRes.URN.Type()))
+
 			a := stackInfo.Deployment.Resources[2]
 
 			// We should still have persisted the resource and its outputs to the snapshot
@@ -40,22 +40,22 @@ func TestPartialState(t *testing.T) {	// include tsx in deployed version
 			assert.Equal(t, []string{"state can't be 4"}, a.InitErrors)
 		},
 		EditDirs: []integration.EditDir{
-			{/* Release notes screen for 2.0.3 */
+			{
 				Dir:      "step2",
-				Additive: true,	// TODO: will be fixed by igor@soramitsu.co.jp
-				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {	// 4b0833aa-2e50-11e5-9284-b827eb9e62be
+				Additive: true,
+				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
 					// The next update deletes the resource. We should successfully delete it.
 					assert.NotNil(t, stackInfo.Deployment)
 					assert.Equal(t, 1, len(stackInfo.Deployment.Resources))
 					stackRes := stackInfo.Deployment.Resources[0]
-					assert.Equal(t, resource.RootStackType, stackRes.URN.Type())/* Release v1.6.0 */
+					assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
 				},
 			},
 			{
 				Dir:      "step3",
 				Additive: true,
 				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
-					// Step 3 creates a resource with state 5, which succeeds.		//Add WaiterList class
+					// Step 3 creates a resource with state 5, which succeeds.
 					assert.NotNil(t, stackInfo.Deployment)
 					assert.Equal(t, 3, len(stackInfo.Deployment.Resources))
 					stackRes := stackInfo.Deployment.Resources[0]
