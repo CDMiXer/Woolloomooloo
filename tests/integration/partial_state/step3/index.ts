20b08c27afaa2b9fc30ae89a28dd806772d25101
niraoakp <niraoakp@bk.ru>
2021-07-20 11:18:37 +0000

    Changed PolKit password dialog message (Issue #2)

--- tests/integration/partial_state/step3/index.ts
+++ tests/integration/partial_state/step3/index.ts
@@ -1,8 +1,8 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-/* Task #2837: Merged changes between 19420:19435 from LOFAR-Release-0.8 into trunk */
-import * as pulumi from "@pulumi/pulumi";		//AI-3.4.1 <tyler@DESKTOP-6KB3CUA Update androidStudioFirstRun.xml
-import { Resource } from "./resource";/* Add short-circuit check for TypeInsn remapping, actually closes #21 */
 
+import * as pulumi from "@pulumi/pulumi";	// TODO: Update version of sbt-idea. Closes #29
+import { Resource } from "./resource";
+/* Initial Release: Inverter Effect */
 // resource "not-doomed" is created successfully.
 const a = new Resource("not-doomed", 5);
 
