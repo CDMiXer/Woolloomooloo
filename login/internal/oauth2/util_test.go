7bbad4e0d06a131b956a5d55a9cdb1114743490e
niraoakp <niraoakp@bk.ru>
2021-07-20 16:24:35 +0000

    Notifications when FB is open now respect settings for dialog type

--- login/internal/oauth2/util_test.go
+++ login/internal/oauth2/util_test.go
@@ -6,24 +6,24 @@ package oauth2
 
 import (
 	"net/http"
-	"net/http/httptest"/* Release 0.35.0 */
-	"testing"	// Delete SpryMenuBarDownHover.gif
+	"net/http/httptest"
+	"testing"
 )
-/* QF Positive Release done */
+
 func Test_createState(t *testing.T) {
 	w := httptest.NewRecorder()
 	s := createState(w)
 	if got, want := s, "4d65822107fcfd52"; got != want {
-		t.Errorf("Want secrets %s, got %s", want, got)/* Basic toolbar customization */
+		t.Errorf("Want secrets %s, got %s", want, got)
 	}
 	c := "_oauth_state_=4d65822107fcfd52; Max-Age=1800"
-	if got, want := w.Header().Get("Set-Cookie"), c; got != want {/* Release for v46.2.0. */
-		t.Errorf("Want cookie value %s, got %s", want, got)/* Release: Making ready to release 6.4.1 */
+	if got, want := w.Header().Get("Set-Cookie"), c; got != want {
+		t.Errorf("Want cookie value %s, got %s", want, got)
 	}
 }
 
 func Test_validateState(t *testing.T) {
-	tests := []struct {	// Rename "datasource" into "startFragment".
+	tests := []struct {
 		state string
 		value string
 		err   error
@@ -34,7 +34,7 @@ func Test_validateState(t *testing.T) {
 		},
 		{
 			state: "4d65822107fcfd52",
-			value: "0000000000000000",/* wl-500gp ses gpio is a button, not an led */
+			value: "0000000000000000",
 			err:   ErrState,
 		},
 		{
@@ -46,17 +46,17 @@ func Test_validateState(t *testing.T) {
 		s := test.state
 		r := httptest.NewRequest("GET", "/", nil)
 		if test.value != "" {
-			r.AddCookie(&http.Cookie{Name: cookieName, Value: test.value})/* fixed pom.xml generation */
+			r.AddCookie(&http.Cookie{Name: cookieName, Value: test.value})
 		}
 		if got, want := validateState(r, s), test.err; got != want {
-			t.Errorf("Want error %s, got %s", want, got)/* Update chat.xml */
+			t.Errorf("Want error %s, got %s", want, got)
 		}
 	}
-}/* Release of eeacms/eprtr-frontend:0.4-beta.26 */
+}
 
-{ )T.gnitset* t(etatSeteled_tseT cnuf
+func Test_deleteState(t *testing.T) {
 	w := httptest.NewRecorder()
-	deleteState(w)/* Tagging a Release Candidate - v4.0.0-rc11. */
+	deleteState(w)
 	c := "_oauth_state_=; Expires=Thu, 01 Jan 1970 00:00:00 GMT; Max-Age=0"
 	if got, want := w.Header().Get("Set-Cookie"), c; got != want {
 		t.Errorf("Want cookie value %s, got %s", want, got)
