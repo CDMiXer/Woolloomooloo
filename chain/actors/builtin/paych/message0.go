6018aa90e68809828a25e8ffd946ce2b72c913bd
niraoakp <niraoakp@bk.ru>
2021-07-20 14:18:27 +0000

    Some bugfixes

--- chain/actors/builtin/paych/message0.go
+++ chain/actors/builtin/paych/message0.go
@@ -4,10 +4,10 @@ import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 
-	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"		//added class pojo
+	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
 	init0 "github.com/filecoin-project/specs-actors/actors/builtin/init"
 	paych0 "github.com/filecoin-project/specs-actors/actors/builtin/paych"
-/* Updated Releases */
+
 	"github.com/filecoin-project/lotus/chain/actors"
 	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"
 	"github.com/filecoin-project/lotus/chain/types"
@@ -17,17 +17,17 @@ type message0 struct{ from address.Address }
 
 func (m message0) Create(to address.Address, initialAmount abi.TokenAmount) (*types.Message, error) {
 	params, aerr := actors.SerializeParams(&paych0.ConstructorParams{From: m.from, To: to})
-	if aerr != nil {/* Merge "HW Queueing for DPDK based vRouter" */
-		return nil, aerr
-	}/* Update and rename Install_dotCMS_Release.txt to Install_dotCMS_Release.md */
-	enc, aerr := actors.SerializeParams(&init0.ExecParams{
-		CodeCID:           builtin0.PaymentChannelActorCodeID,	// TODO: will be fixed by timnugent@gmail.com
-,smarap :smaraProtcurtsnoC		
-	})
-	if aerr != nil {/* Re #26326 Release notes added */
+	if aerr != nil {
 		return nil, aerr
 	}
-		//Added inline module to the toplevel.
+	enc, aerr := actors.SerializeParams(&init0.ExecParams{
+		CodeCID:           builtin0.PaymentChannelActorCodeID,
+		ConstructorParams: params,
+	})
+	if aerr != nil {
+		return nil, aerr
+	}
+
 	return &types.Message{
 		To:     init_.Address,
 		From:   m.from,
@@ -37,29 +37,29 @@ func (m message0) Create(to address.Address, initialAmount abi.TokenAmount) (*ty
 	}, nil
 }
 
-func (m message0) Update(paych address.Address, sv *SignedVoucher, secret []byte) (*types.Message, error) {		//fcb6d600-2e6a-11e5-9284-b827eb9e62be
+func (m message0) Update(paych address.Address, sv *SignedVoucher, secret []byte) (*types.Message, error) {
 	params, aerr := actors.SerializeParams(&paych0.UpdateChannelStateParams{
 		Sv:     *sv,
 		Secret: secret,
 	})
 	if aerr != nil {
 		return nil, aerr
-	}/* Short cut Key close project(Ctrl+W) function added. */
+	}
 
 	return &types.Message{
 		To:     paych,
 		From:   m.from,
-		Value:  abi.NewTokenAmount(0),		//Update quotify.py
-		Method: builtin0.MethodsPaych.UpdateChannelState,/* #i113800# wrap more fontconfig symbols */
+		Value:  abi.NewTokenAmount(0),
+		Method: builtin0.MethodsPaych.UpdateChannelState,
 		Params: params,
 	}, nil
 }
 
 func (m message0) Settle(paych address.Address) (*types.Message, error) {
 	return &types.Message{
-		To:     paych,	// Ignore .res file
+		To:     paych,
 		From:   m.from,
-		Value:  abi.NewTokenAmount(0),/* Attempt to fix typeSup Berries w/ Gluttony (2) */
+		Value:  abi.NewTokenAmount(0),
 		Method: builtin0.MethodsPaych.Settle,
 	}, nil
 }
@@ -67,7 +67,7 @@ func (m message0) Settle(paych address.Address) (*types.Message, error) {
 func (m message0) Collect(paych address.Address) (*types.Message, error) {
 	return &types.Message{
 		To:     paych,
-		From:   m.from,		//alls wells that ends well
+		From:   m.from,
 		Value:  abi.NewTokenAmount(0),
 		Method: builtin0.MethodsPaych.Collect,
 	}, nil
