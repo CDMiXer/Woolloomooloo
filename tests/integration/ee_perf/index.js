529c5d9f402feab563110fd4d987cd745af7036a
niraoakp <niraoakp@bk.ru>
2021-07-20 18:05:15 +0000

    Added draw_boundaries(), with sprites spawning areas

--- tests/integration/ee_perf/index.js
+++ tests/integration/ee_perf/index.js
@@ -2,12 +2,12 @@
 
 "use strict";
 const pulumi = require("@pulumi/pulumi");
-/* Release version 3.0.6 */
+
 const config = new pulumi.Config();
 const iterations = config.getNumber("count") || 1000;
 
 // Emit many, many diagnostic events from the engine to stress test the
-// ability to record those events on the Pulumi Service.		//clean up again native language for translation request #793
+// ability to record those events on the Pulumi Service.
 console.log("Starting to spam a bunch of diagnostic messages...");
 for (let i = 0; i < iterations; i++) {
     console.log(`${i}: The current time is ${new Date()}`);
