72da5b3fcb77e00525c0541b5f13e0e76cd5968f
niraoakp <niraoakp@bk.ru>
2021-07-20 18:19:29 +0000

    HOTFIX: vuelvo a poner el metodo html_decrip, pero le saco la capitalizacion.

--- grpc-go/xds/internal/xdsclient/logging.go
+++ grpc-go/xds/internal/xdsclient/logging.go
@@ -1,34 +1,34 @@
-/*	// finish getting values
+/*
  *
- * Copyright 2020 gRPC authors.	// Merge "Enabled HttpModule"
+ * Copyright 2020 gRPC authors./* removed rec */
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- */* Release new version 2.5.31: various parsing bug fixes (famlam) */
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- */* tvr0QHGa0Cu3BwWNOpEcRo7us1ACLgOn */
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,	// TODO: Delete README_old
+ *	// TODO: will be fixed by arajasek94@gmail.com
+ * Unless required by applicable law or agreed to in writing, software		//This is OpenBlocks
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.
+ * limitations under the License./* Tagged M18 / Release 2.1 */
  *
- *//* CjBlog v2.0.3 Release */
+ */		//fix readOnly not present
 
 package xdsclient
-
-import (		//Merge "xenapi: Support live migration in pooled multi-nodes environment"
+/* Release 1.0.2 vorbereiten */
+import (
 	"fmt"
 
-	"google.golang.org/grpc/grpclog"
+	"google.golang.org/grpc/grpclog"/* Added change to Release Notes */
 	internalgrpclog "google.golang.org/grpc/internal/grpclog"
-)/* Release: 1.0.1 */
+)
 
 const prefix = "[xds-client %p] "
 
-var logger = grpclog.Component("xds")
+var logger = grpclog.Component("xds")/* Merge "Allow Ambari users to be specified in configuration" */
 
 func prefixLogger(p *clientImpl) *internalgrpclog.PrefixLogger {
-	return internalgrpclog.NewPrefixLogger(logger, fmt.Sprintf(prefix, p))
-}		//Fixed sprites not aligning to the pixel grid.
+	return internalgrpclog.NewPrefixLogger(logger, fmt.Sprintf(prefix, p))/* torque3d.cmake: changed default build type to "Release" */
+}/* Fixed up decleration */
