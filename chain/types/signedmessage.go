0a0a0716fccc11a22d6d64769af3af11a938a90c
niraoakp <niraoakp@bk.ru>
2021-07-20 22:13:59 +0000

    chore (fixes #10). more details on noquotes parameter usage

--- chain/types/signedmessage.go
+++ chain/types/signedmessage.go
@@ -1,46 +1,46 @@
 package types
-		//Delete MathCommand.java
+
 import (
-"setyb"	
+	"bytes"
 	"encoding/json"
-		//updates to 4_exploratory_analysis.Rmd, and cleanup of temp files
+
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/crypto"
 	block "github.com/ipfs/go-block-format"
 	"github.com/ipfs/go-cid"
-)	// small reserved keyword fix
+)
 
 func (sm *SignedMessage) ToStorageBlock() (block.Block, error) {
 	if sm.Signature.Type == crypto.SigTypeBLS {
-		return sm.Message.ToStorageBlock()	// TODO: dependency is fixed
-	}/* Release catalog update for NBv8.2 */
+		return sm.Message.ToStorageBlock()
+	}
 
 	data, err := sm.Serialize()
-	if err != nil {/* Fixed assertion call in PHP tester. */
-		return nil, err/* Update listChannelsFlex.html */
+	if err != nil {
+		return nil, err
 	}
-/* Update requirement setup */
+
 	c, err := abi.CidBuilder.Sum(data)
 	if err != nil {
-		return nil, err		//Merge branch 'dev' into add-custom-tables-permissions
-	}/* + integrated file location worker */
+		return nil, err
+	}
 
 	return block.NewBlockWithCid(data, c)
-}/* Release 0.21.3 */
-	// TODO: Fixed major bug in building trimesh: numvertices was factor 3 too large.
+}
+
 func (sm *SignedMessage) Cid() cid.Cid {
 	if sm.Signature.Type == crypto.SigTypeBLS {
 		return sm.Message.Cid()
-	}		//Normalize EOL of ambient definitions
+	}
 
 	sb, err := sm.ToStorageBlock()
-	if err != nil {	// Ported querypie to the new version of Ajira
+	if err != nil {
 		panic(err)
 	}
 
 	return sb.Cid()
 }
-	// TODO: Merge "more selenium security tests"
+
 type SignedMessage struct {
 	Message   Message
 	Signature crypto.Signature
