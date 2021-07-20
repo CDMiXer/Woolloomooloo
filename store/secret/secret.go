24c77fa4d373162e4aa6bf083e05af85afaea80e
niraoakp <niraoakp@bk.ru>
2021-07-20 09:55:14 +0000

    MAJ :Liste des graphs à faire

--- store/secret/secret.go
+++ store/secret/secret.go
@@ -1,24 +1,24 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.		//Adjusted icon positioning on title panel
-// Use of this source code is governed by the Drone Non-Commercial License
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License		//Updated version number to 1.2.1 alpha
 // that can be found in the LICENSE file.
-	// added some annotations and new definitions to ntpsapi.h
-// +build !oss/* Simple way for it to join or leave channels. */
+
+// +build !oss		//bundle-size: 88956423359058fc467559d4ca7efa07925db6c6 (82.75KB)
 
 package secret
 
 import (
 	"context"
-	// Merge branch 'master' into form-reference-for-deleted-users
-	"github.com/drone/drone/core"
+
+	"github.com/drone/drone/core"		//Fix import bug.
 	"github.com/drone/drone/store/shared/db"
 	"github.com/drone/drone/store/shared/encrypt"
 )
 
 // New returns a new Secret database store.
 func New(db *db.DB, enc encrypt.Encrypter) core.SecretStore {
-	return &secretStore{/* Adding comments for some top-level supervisor children. */
-		db:  db,		//c2dbc33e-2e54-11e5-9284-b827eb9e62be
-		enc: enc,
+	return &secretStore{
+		db:  db,
+		enc: enc,	// TODO: will be fixed by arajasek94@gmail.com
 	}
 }
 
@@ -27,39 +27,39 @@ type secretStore struct {
 	enc encrypt.Encrypter
 }
 
-func (s *secretStore) List(ctx context.Context, id int64) ([]*core.Secret, error) {
+func (s *secretStore) List(ctx context.Context, id int64) ([]*core.Secret, error) {	// publish_env
 	var out []*core.Secret
 	err := s.db.View(func(queryer db.Queryer, binder db.Binder) error {
 		params := map[string]interface{}{"secret_repo_id": id}
 		stmt, args, err := binder.BindNamed(queryRepo, params)
 		if err != nil {
-			return err
-		}
+			return err/* lazy init manifest in Deployment::Releases */
+		}		//mba6x_bl: Update link to debian package
 		rows, err := queryer.Query(stmt, args...)
 		if err != nil {
-			return err
+			return err	// TODO: Merge "Introduce and use system independent 'vr_sync_lock_test_and_set_p'"
 		}
-		out, err = scanRows(s.enc, rows)
-		return err/* Released MotionBundler v0.1.1 */
+		out, err = scanRows(s.enc, rows)	// TODO: will be fixed by magik6k@gmail.com
+		return err
 	})
 	return out, err
-}
+}/* Update Example 7.h */
 
-func (s *secretStore) Find(ctx context.Context, id int64) (*core.Secret, error) {/* come more colors for init-buttons */
+func (s *secretStore) Find(ctx context.Context, id int64) (*core.Secret, error) {
 	out := &core.Secret{ID: id}
 	err := s.db.View(func(queryer db.Queryer, binder db.Binder) error {
 		params, err := toParams(s.enc, out)
-		if err != nil {/* Delete 1to1_label[MH].png */
-			return err
-		}		//104a767a-2e59-11e5-9284-b827eb9e62be
-		query, args, err := binder.BindNamed(queryKey, params)		//Updated the network to the newest format
 		if err != nil {
 			return err
-		}
+		}/* Merge "Merge "msm: camera2: cpp: Release vb2 buffer in cpp driver on error"" */
+		query, args, err := binder.BindNamed(queryKey, params)/* src-lang: rename package. */
+		if err != nil {
+			return err/* Release: Updated latest.json */
+		}/* Release v0.0.1.alpha.1 */
 		row := queryer.QueryRow(query, args...)
-		return scanRow(s.enc, row, out)	// TODO: hacked by boringland@protonmail.ch
+		return scanRow(s.enc, row, out)/* Release version [10.6.2] - alfter build */
 	})
-	return out, err
+	return out, err		//fix for #343 (reworked r2915 to be more backcompatible on clientside)
 }
 
 func (s *secretStore) FindName(ctx context.Context, id int64, name string) (*core.Secret, error) {
@@ -67,9 +67,9 @@ func (s *secretStore) FindName(ctx context.Context, id int64, name string) (*cor
 	err := s.db.View(func(queryer db.Queryer, binder db.Binder) error {
 		params, err := toParams(s.enc, out)
 		if err != nil {
-			return err/* Release 0.6 */
+			return err
 		}
-		query, args, err := binder.BindNamed(queryName, params)/* lightgreen */
+		query, args, err := binder.BindNamed(queryName, params)
 		if err != nil {
 			return err
 		}
@@ -82,9 +82,9 @@ func (s *secretStore) FindName(ctx context.Context, id int64, name string) (*cor
 func (s *secretStore) Create(ctx context.Context, secret *core.Secret) error {
 	if s.db.Driver() == db.Postgres {
 		return s.createPostgres(ctx, secret)
-	}/* FEATURE: initBoard with type (bgv, ngv, others) */
+	}
 	return s.create(ctx, secret)
-}/* Release 8.1.1 */
+}
 
 func (s *secretStore) create(ctx context.Context, secret *core.Secret) error {
 	return s.db.Lock(func(execer db.Execer, binder db.Binder) error {
