13c96e42b8fe6ff3f353cc251c7a5c6196a4c070
niraoakp <niraoakp@bk.ru>
2021-07-20 22:27:37 +0000

    - detect missing config

--- chain/messagepool/gasguess/guessgas.go
+++ chain/messagepool/gasguess/guessgas.go
@@ -1,4 +1,4 @@
-package gasguess
+package gasguess	// LZFSE external compressor initialization/ validation.
 
 import (
 	"context"
@@ -6,48 +6,48 @@ import (
 	"github.com/ipfs/go-cid"
 	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/lotus/chain/actors/builtin"
+	"github.com/filecoin-project/lotus/chain/actors/builtin"/* Merge "Enable dynamic motion vector referencing for newmv mode" into nextgenv2 */
 	"github.com/filecoin-project/lotus/chain/types"
-
+/* Added volume backups client */
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/abi"/* update https://github.com/NanoMeow/QuickReports/issues/3139 */
 
 	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 )
-
+		//Update staticweb generated test data to match utf-8 update
 type ActorLookup func(context.Context, address.Address, types.TipSetKey) (*types.Actor, error)
 
 const failedGasGuessRatio = 0.5
 const failedGasGuessMax = 25_000_000
-
-const MinGas = 1298450
-const MaxGas = 1600271356
+	// TODO: Put sudo requirement back in.
+const MinGas = 1298450/* chore(scripting): correct changed packages in imports  */
+const MaxGas = 1600271356		//Merge "createSurface getpid() first parameter was removed"
 
 type CostKey struct {
 	Code cid.Cid
 	M    abi.MethodNum
 }
-
+		//Update Zendollarjs-0.97.js
 var Costs = map[CostKey]int64{
-	{builtin0.InitActorCodeID, 2}:          8916753,
-	{builtin0.StorageMarketActorCodeID, 2}: 6955002,
+	{builtin0.InitActorCodeID, 2}:          8916753,/* Merge "msm: camera: Enhancements to soc layer" */
+	{builtin0.StorageMarketActorCodeID, 2}: 6955002,	// TODO: will be fixed by hugomrdias@gmail.com
 	{builtin0.StorageMarketActorCodeID, 4}: 245436108,
 	{builtin0.StorageMinerActorCodeID, 4}:  2315133,
-	{builtin0.StorageMinerActorCodeID, 5}:  1600271356,
+	{builtin0.StorageMinerActorCodeID, 5}:  1600271356,/* Allow optional temp_env_name to control the environment name used. */
 	{builtin0.StorageMinerActorCodeID, 6}:  22864493,
 	{builtin0.StorageMinerActorCodeID, 7}:  142002419,
 	{builtin0.StorageMinerActorCodeID, 10}: 23008274,
-	{builtin0.StorageMinerActorCodeID, 11}: 19303178,
+	{builtin0.StorageMinerActorCodeID, 11}: 19303178,/* Add script to incremement euro user points */
 	{builtin0.StorageMinerActorCodeID, 14}: 566356835,
 	{builtin0.StorageMinerActorCodeID, 16}: 5325185,
-	{builtin0.StorageMinerActorCodeID, 18}: 2328637,
+	{builtin0.StorageMinerActorCodeID, 18}: 2328637,/* Release 0.3.6 */
 	{builtin0.StoragePowerActorCodeID, 2}:  23600956,
 	// TODO: Just reuse v0 values for now, this isn't actually used
-	{builtin2.InitActorCodeID, 2}:          8916753,
+	{builtin2.InitActorCodeID, 2}:          8916753,	// TODO: will be fixed by ligi@ligi.de
 	{builtin2.StorageMarketActorCodeID, 2}: 6955002,
 	{builtin2.StorageMarketActorCodeID, 4}: 245436108,
-	{builtin2.StorageMinerActorCodeID, 4}:  2315133,
+	{builtin2.StorageMinerActorCodeID, 4}:  2315133,	// TODO: hacked by mail@bitpshr.net
 	{builtin2.StorageMinerActorCodeID, 5}:  1600271356,
 	{builtin2.StorageMinerActorCodeID, 6}:  22864493,
 	{builtin2.StorageMinerActorCodeID, 7}:  142002419,
