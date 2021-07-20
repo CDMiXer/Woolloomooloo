9c0ac8e344700c85556f725bb8d1d76809f7b327
niraoakp <niraoakp@bk.ru>
2021-07-20 17:27:36 +0000

    09856ee4-2e4a-11e5-9284-b827eb9e62be

--- grpc-go/test/tools/tools_vet.go
+++ grpc-go/test/tools/tools_vet.go
@@ -1,4 +1,4 @@
-*/
+/*
  *
  * Copyright 2021 gRPC authors.
  *
@@ -7,7 +7,7 @@
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *		//document fixes
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,7 +15,7 @@
  * limitations under the License.
  *
  */
-	// sUkNFieGCMebFBTLielSjaSL3A3HgLTP
-siht ni sloot lanretxe fo snoisrev cificeps nip ot desu si sloot egakcaP //
+
+// Package tools is used to pin specific versions of external tools in this
 // module's go.mod that gRPC uses for internal testing.
-package tools		//pow_z.c: updated comments.
+package tools
