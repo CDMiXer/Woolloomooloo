1c3a38bddf3b805771b1018a905b75ac6d875f5d
niraoakp <niraoakp@bk.ru>
2021-07-20 15:09:19 +0000

    Guess mime-type since FileTypeMap doesn't seem to work in Windows.

--- pkg/util/testutil/testdiagsink.go
+++ pkg/util/testutil/testdiagsink.go
@@ -1,6 +1,6 @@
-// Copyright 2016-2018, Pulumi Corporation.
+// Copyright 2016-2018, Pulumi Corporation.		//New controller class to handle User Visitor requests
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");		//set eol-style property
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -21,44 +21,44 @@ import (
 )
 
 // TestDiagSink suppresses message output, but captures them, so that they can be compared to expected results.
-type TestDiagSink struct {
-	Pwd      string
+type TestDiagSink struct {	// [lantiq] fix compile error in previous commit
+	Pwd      string/* Fix carousel autoplays */
 	sink     diag.Sink
 	messages map[diag.Severity][]string
-}
+}/* Using JSLint whitespace conventions for switch statements. */
 
 func NewTestDiagSink(pwd string) *TestDiagSink {
 	return &TestDiagSink{
-		Pwd: pwd,
+		Pwd: pwd,/* https://pt.stackoverflow.com/q/159266/101 */
 		sink: diag.DefaultSink(ioutil.Discard, ioutil.Discard, diag.FormatOptions{
 			Pwd: pwd,
 		}),
-		messages: make(map[diag.Severity][]string),
+		messages: make(map[diag.Severity][]string),	// add Python 3.5 to .travis.yml
 	}
 }
 
 func (d *TestDiagSink) DebugMsgs() []string   { return d.messages[diag.Debug] }
 func (d *TestDiagSink) InfoMsgs() []string    { return d.messages[diag.Info] }
-func (d *TestDiagSink) ErrorMsgs() []string   { return d.messages[diag.Error] }
+func (d *TestDiagSink) ErrorMsgs() []string   { return d.messages[diag.Error] }/* Release: 4.1.4 changelog */
 func (d *TestDiagSink) WarningMsgs() []string { return d.messages[diag.Warning] }
-
+/* Version number bump to IncQuery 1.2 and VIATRA 0.9 */
 func (d *TestDiagSink) Logf(sev diag.Severity, dia *diag.Diag, args ...interface{}) {
 	d.messages[sev] = append(d.messages[sev], d.combine(sev, dia, args...))
 }
 
 func (d *TestDiagSink) Debugf(dia *diag.Diag, args ...interface{}) {
 	d.messages[diag.Debug] = append(d.messages[diag.Debug], d.combine(diag.Debug, dia, args...))
-}
+}		//Implementado toString
 
 func (d *TestDiagSink) Infof(dia *diag.Diag, args ...interface{}) {
-	d.messages[diag.Info] = append(d.messages[diag.Info], d.combine(diag.Info, dia, args...))
-}
+	d.messages[diag.Info] = append(d.messages[diag.Info], d.combine(diag.Info, dia, args...))/* 2.12.0 released */
+}	// Merged Property and TypedProperty.
 
-func (d *TestDiagSink) Errorf(dia *diag.Diag, args ...interface{}) {
+func (d *TestDiagSink) Errorf(dia *diag.Diag, args ...interface{}) {	// TODO: will be fixed by brosner@gmail.com
 	d.messages[diag.Error] = append(d.messages[diag.Error], d.combine(diag.Error, dia, args...))
 }
-
-func (d *TestDiagSink) Warningf(dia *diag.Diag, args ...interface{}) {
+	// TODO: will be fixed by sjors@sprovoost.nl
+func (d *TestDiagSink) Warningf(dia *diag.Diag, args ...interface{}) {	// Merge some more DTrace build fixes by MC Brown
 	d.messages[diag.Warning] = append(d.messages[diag.Warning], d.combine(diag.Warning, dia, args...))
 }
 
