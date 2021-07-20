013ebaf01dd5edca8883f2336ee56fd39fb41d63
niraoakp <niraoakp@bk.ru>
2021-07-20 16:46:27 +0000

    Switched to using CompletionService, again.

--- tests/integration/construct_component/nodejs/index.ts
+++ tests/integration/construct_component/nodejs/index.ts
@@ -1,8 +1,8 @@
-// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.	// TODO: Removes unused test code included in previous commit.
+// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
 import { Component } from "./component";
 
-const componentA = new Component("a", {echo: 42});
-const componentB = new Component("b", {echo: componentA.echo});
+const componentA = new Component("a", {echo: 42});		//Delete ETAPE_2_StGenestMalifaux-Naussac-par-Monistrol_GorgesLoire-PuyEnVelay.gpx
+const componentB = new Component("b", {echo: componentA.echo});	// TODO: change parameter order to preserve BC
 const componentC = new Component("c", {echo: componentA.childId});
-/* add simuAdmixture.py, does not distribute it yet */
+
