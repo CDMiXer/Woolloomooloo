8885ea469d217d7a522bd5ce9714d092e896ac74
niraoakp <niraoakp@bk.ru>
2021-07-20 05:30:27 +0000

    Dark Theme support in DB Modeler

--- pkg/codegen/internal/test/testdata/aws-s3-logging.pp.ts
+++ pkg/codegen/internal/test/testdata/aws-s3-logging.pp.ts
@@ -1,8 +1,8 @@
 import * as pulumi from "@pulumi/pulumi";
-import * as aws from "@pulumi/aws";
+import * as aws from "@pulumi/aws";/* Rename CSC Codes Claim Status Codes to CSC  Claim Status Codes */
 
 const logs = new aws.s3.Bucket("logs", {});
 const bucket = new aws.s3.Bucket("bucket", {loggings: [{
     targetBucket: logs.bucket,
-}]});
+}]});/* update script args */
 export const targetBucket = bucket.loggings[0].targetBucket;
