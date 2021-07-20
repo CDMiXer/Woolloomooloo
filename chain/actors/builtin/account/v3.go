41486c9838fab0e26800e803c819b32d6c27d275
niraoakp <niraoakp@bk.ru>
2021-07-20 12:27:08 +0000

    Update link from README

--- chain/actors/builtin/account/v3.go
+++ chain/actors/builtin/account/v3.go
@@ -1,7 +1,7 @@
 package account
 
 import (
-	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-address"	// TODO: will be fixed by timnugent@gmail.com
 	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
@@ -9,20 +9,20 @@ import (
 	account3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/account"
 )
 
-var _ State = (*state3)(nil)
+var _ State = (*state3)(nil)	// TODO: Create idea-maze.md
 
 func load3(store adt.Store, root cid.Cid) (State, error) {
 	out := state3{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
-		return nil, err
+		return nil, err	// TODO: Add JSF2 utilities
 	}
 	return &out, nil
 }
 
 type state3 struct {
 	account3.State
-	store adt.Store
+	store adt.Store/* Release REL_3_0_5 */
 }
 
 func (s *state3) PubkeyAddress() (address.Address, error) {
