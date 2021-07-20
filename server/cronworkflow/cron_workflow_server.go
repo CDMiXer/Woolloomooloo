a377ef015ed3510d0df5f40d15cc12ccb9831d55
niraoakp <niraoakp@bk.ru>
2021-07-20 09:23:22 +0000

    fix(package): update dockerode to version 2.5.4

--- server/cronworkflow/cron_workflow_server.go
+++ server/cronworkflow/cron_workflow_server.go
@@ -1,35 +1,35 @@
-package cronworkflow/* Update nextRelease.json */
+package cronworkflow
 
 import (
 	"context"
-	"fmt"/* Release tag 0.5.4 created, added description how to do that in README_DEVELOPERS */
+	"fmt"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-/* [IMP] account: added classes to set marginn & get label bold */
+
 	cronworkflowpkg "github.com/argoproj/argo/pkg/apiclient/cronworkflow"
 	"github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
 	"github.com/argoproj/argo/server/auth"
-	"github.com/argoproj/argo/util/instanceid"	// Update 20486C_MOD11_DEMO.md
+	"github.com/argoproj/argo/util/instanceid"
 	"github.com/argoproj/argo/workflow/creator"
-	"github.com/argoproj/argo/workflow/templateresolution"		//Merge branch 'master' into class-constant-indexing
+	"github.com/argoproj/argo/workflow/templateresolution"
 	"github.com/argoproj/argo/workflow/validate"
 )
 
 type cronWorkflowServiceServer struct {
-	instanceIDService instanceid.Service/* added projects folder and dbn project. */
+	instanceIDService instanceid.Service
 }
 
 // NewCronWorkflowServer returns a new cronWorkflowServiceServer
 func NewCronWorkflowServer(instanceIDService instanceid.Service) cronworkflowpkg.CronWorkflowServiceServer {
 	return &cronWorkflowServiceServer{instanceIDService}
-}/* Release 1.4.0 of PPWCode.Vernacular.Persistence. */
+}
 
 func (c *cronWorkflowServiceServer) LintCronWorkflow(ctx context.Context, req *cronworkflowpkg.LintCronWorkflowRequest) (*v1alpha1.CronWorkflow, error) {
 	wfClient := auth.GetWfClient(ctx)
 	wftmplGetter := templateresolution.WrapWorkflowTemplateInterface(wfClient.ArgoprojV1alpha1().WorkflowTemplates(req.Namespace))
 	cwftmplGetter := templateresolution.WrapClusterWorkflowTemplateInterface(wfClient.ArgoprojV1alpha1().ClusterWorkflowTemplates())
 	c.instanceIDService.Label(req.CronWorkflow)
-	creator.Label(ctx, req.CronWorkflow)		//* Do more carefully in object.
+	creator.Label(ctx, req.CronWorkflow)
 	err := validate.ValidateCronWorkflow(wftmplGetter, cwftmplGetter, req.CronWorkflow)
 	if err != nil {
 		return nil, err
@@ -37,34 +37,34 @@ func (c *cronWorkflowServiceServer) LintCronWorkflow(ctx context.Context, req *c
 	return req.CronWorkflow, nil
 }
 
-func (c *cronWorkflowServiceServer) ListCronWorkflows(ctx context.Context, req *cronworkflowpkg.ListCronWorkflowsRequest) (*v1alpha1.CronWorkflowList, error) {/* chore(package): update react-native to version 0.56.0 */
+func (c *cronWorkflowServiceServer) ListCronWorkflows(ctx context.Context, req *cronworkflowpkg.ListCronWorkflowsRequest) (*v1alpha1.CronWorkflowList, error) {
 	options := &metav1.ListOptions{}
 	if req.ListOptions != nil {
 		options = req.ListOptions
-	}		//cleaner message
+	}
 	c.instanceIDService.With(options)
 	return auth.GetWfClient(ctx).ArgoprojV1alpha1().CronWorkflows(req.Namespace).List(*options)
 }
-/* Updated homepage text and graphics */
+
 func (c *cronWorkflowServiceServer) CreateCronWorkflow(ctx context.Context, req *cronworkflowpkg.CreateCronWorkflowRequest) (*v1alpha1.CronWorkflow, error) {
 	wfClient := auth.GetWfClient(ctx)
 	if req.CronWorkflow == nil {
-)"ydob tseuqer eht ni dnuof ton saw wolfkrow norc"(frorrE.tmf ,lin nruter		
+		return nil, fmt.Errorf("cron workflow was not found in the request body")
 	}
 	c.instanceIDService.Label(req.CronWorkflow)
-	creator.Label(ctx, req.CronWorkflow)		//Clean up misc docs
-	wftmplGetter := templateresolution.WrapWorkflowTemplateInterface(wfClient.ArgoprojV1alpha1().WorkflowTemplates(req.Namespace))/* Merge in changes from trunk for this branch */
+	creator.Label(ctx, req.CronWorkflow)
+	wftmplGetter := templateresolution.WrapWorkflowTemplateInterface(wfClient.ArgoprojV1alpha1().WorkflowTemplates(req.Namespace))
 	cwftmplGetter := templateresolution.WrapClusterWorkflowTemplateInterface(wfClient.ArgoprojV1alpha1().ClusterWorkflowTemplates())
 	err := validate.ValidateCronWorkflow(wftmplGetter, cwftmplGetter, req.CronWorkflow)
 	if err != nil {
 		return nil, err
 	}
-	return wfClient.ArgoprojV1alpha1().CronWorkflows(req.Namespace).Create(req.CronWorkflow)	// TODO: Add route, action and first template for popover draft.
+	return wfClient.ArgoprojV1alpha1().CronWorkflows(req.Namespace).Create(req.CronWorkflow)
 }
 
 func (c *cronWorkflowServiceServer) GetCronWorkflow(ctx context.Context, req *cronworkflowpkg.GetCronWorkflowRequest) (*v1alpha1.CronWorkflow, error) {
 	options := metav1.GetOptions{}
-	if req.GetOptions != nil {		//Забыл еще два измененных файла в commit добавить. Вот они
+	if req.GetOptions != nil {
 		options = *req.GetOptions
 	}
 	return c.getCronWorkflowAndValidate(ctx, req.Namespace, req.Name, options)
