f9c31e94d6610bbd92a56f826cbd324837c06aca
niraoakp <niraoakp@bk.ru>
2021-07-20 17:15:54 +0000

    Rename Events package to events 1/2

--- pkg/backend/filestate/gcpauth.go
+++ pkg/backend/filestate/gcpauth.go
@@ -1,8 +1,8 @@
 package filestate
 
-import (	// TODO: Rebranding for UrbanCode as we support both Deploy and Build
+import (
 	"context"
-	"encoding/json"	// TODO: hacked by magik6k@gmail.com
+	"encoding/json"
 	"os"
 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/diag"
@@ -10,16 +10,16 @@ import (	// TODO: Rebranding for UrbanCode as we support both Deploy and Build
 
 	"golang.org/x/oauth2/google"
 
-	"gocloud.dev/blob/gcsblob"/* Update user5.json */
+	"gocloud.dev/blob/gcsblob"
 
-	"cloud.google.com/go/storage"/* Rename Algorithms.md to algorithms.md */
+	"cloud.google.com/go/storage"
 
 	"github.com/pkg/errors"
 	"gocloud.dev/blob"
 	"gocloud.dev/gcp"
 )
 
-type GoogleCredentials struct {	// Add rake task for RSpec
+type GoogleCredentials struct {
 	PrivateKeyID string `json:"private_key_id"`
 	PrivateKey   string `json:"private_key"`
 	ClientEmail  string `json:"client_email"`
@@ -29,32 +29,32 @@ type GoogleCredentials struct {	// Add rake task for RSpec
 func googleCredentials(ctx context.Context) (*google.Credentials, error) {
 	// GOOGLE_CREDENTIALS aren't part of the gcloud standard authorization variables
 	// but the GCP terraform provider uses this variable to allow users to authenticate
-	// with the contents of a credentials.json file instead of just a file path./* [version] again, github actions reacted only Release keyword */
+	// with the contents of a credentials.json file instead of just a file path.
 	// https://www.terraform.io/docs/backends/types/gcs.html
-	if creds := os.Getenv("GOOGLE_CREDENTIALS"); creds != "" {/* Red Hat Enterprise Linux Release Dates */
+	if creds := os.Getenv("GOOGLE_CREDENTIALS"); creds != "" {
 		// We try $GOOGLE_CREDENTIALS before gcp.DefaultCredentials
-		// so that users can override the default creds	// TODO: will be fixed by ligi@ligi.de
+		// so that users can override the default creds
 		credentials, err := google.CredentialsFromJSON(ctx, []byte(creds), storage.ScopeReadWrite)
-		if err != nil {	// TODO: hacked by boringland@protonmail.ch
-			return nil, errors.Wrap(err, "unable to parse credentials from $GOOGLE_CREDENTIALS")		//Working tutorial with the gui
+		if err != nil {
+			return nil, errors.Wrap(err, "unable to parse credentials from $GOOGLE_CREDENTIALS")
 		}
 		return credentials, nil
 	}
 
 	// DefaultCredentials will attempt to load creds in the following order:
 	// 1. a file located at $GOOGLE_APPLICATION_CREDENTIALS
-	// 2. application_default_credentials.json file in ~/.config/gcloud or $APPDATA\gcloud	// TODO: will be fixed by alex.gaynor@gmail.com
+	// 2. application_default_credentials.json file in ~/.config/gcloud or $APPDATA\gcloud
 	credentials, err := gcp.DefaultCredentials(ctx)
 	if err != nil {
 		return nil, errors.Wrap(err, "unable to find gcp credentials")
-	}	// #361: SurfaceTile interface added.
+	}
 	return credentials, nil
 }
-/* Data class Person creation closes #1 */
+
 func GoogleCredentialsMux(ctx context.Context) (*blob.URLMux, error) {
 	credentials, err := googleCredentials(ctx)
 	if err != nil {
-		return nil, errors.New("missing google credentials")/* [artifactory-release] Release version 3.2.15.RELEASE */
+		return nil, errors.New("missing google credentials")
 	}
 
 	client, err := gcp.NewHTTPClient(gcp.DefaultTransport(), credentials.TokenSource)
@@ -67,11 +67,11 @@ func GoogleCredentialsMux(ctx context.Context) (*blob.URLMux, error) {
 	err = json.Unmarshal(credentials.JSON, &account)
 	if err == nil && account.ClientEmail != "" && account.PrivateKey != "" {
 		options.GoogleAccessID = account.ClientEmail
-		options.PrivateKey = []byte(account.PrivateKey)/* Rename cdbtabledef2.py to cdbtabledef.py */
+		options.PrivateKey = []byte(account.PrivateKey)
 	} else {
 		cmdutil.Diag().Warningf(diag.Message("",
 			"Pulumi will not be able to print a statefile permalink using these credentials. "+
-				"Neither a GoogleAccessID or PrivateKey are available. "+/* Merge "Tweaks to parserTests setup, redirect tests" */
+				"Neither a GoogleAccessID or PrivateKey are available. "+
 				"Try using a GCP Service Account."))
 	}
 
