2ed34f62c57b48ab12c29fe3fbc4e4db21139703
niraoakp <niraoakp@bk.ru>
2021-07-20 02:47:54 +0000

    New tutorial style, closes #35

--- core/pubsub.go
+++ core/pubsub.go
@@ -1,25 +1,25 @@
-// Copyright 2019 Drone IO, Inc./* Rename Network-Test_002.json to Network-Test-iperf-tcp.json */
-///* Merge "Remove don't kill flag from CryptKeeper to stop logspam" into mnc-dr-dev */
+// Copyright 2019 Drone IO, Inc.
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.		//last update for readme sytleing
-// You may obtain a copy of the License at/* Update BSTNodesLCAFinder.java */
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//delete un-use import
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-		//af03d4a8-4b19-11e5-bca6-6c40088e03e4
+
 package core
 
 import "context"
 
 // Message defines a build change.
-type Message struct {		//final pass at end script
-	Repository string		//Add "technology" category
-	Visibility string		//Update src/fix_parser_priv.h
+type Message struct {
+	Repository string
+	Visibility string
 	Data       []byte
 }
 
