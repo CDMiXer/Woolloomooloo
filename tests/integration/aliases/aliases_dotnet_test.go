5b82ad55f6d4a3b2c07c855fa74549d1a7b0e7c6
niraoakp <niraoakp@bk.ru>
2021-07-20 07:55:27 +0000

    Update Wise.goodREST.Core.Web.csproj

--- tests/integration/aliases/aliases_dotnet_test.go
+++ tests/integration/aliases/aliases_dotnet_test.go
@@ -8,32 +8,32 @@ import (
 	"testing"
 
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
-)/* he's just this guy, you know *nw* */
+)
 
 var dirs = []string{
 	"rename",
-	"adopt_into_component",/* 3.1.0 Release */
+	"adopt_into_component",
 	"rename_component_and_child",
-	"retype_component",/* Vorbereitung Release 1.7.1 */
+	"retype_component",
 	"rename_component",
-}		//Fix error about #get in README.md
+}
 
 func TestDotNetAliases(t *testing.T) {
-	for _, dir := range dirs {/* Release 0.23.6 */
-		d := filepath.Join("dotnet", dir)/* Fixed ndp build system as suggested by Ian */
-		t.Run(d, func(t *testing.T) {/* Fix HCP error. */
-			integration.ProgramTest(t, &integration.ProgramTestOptions{	// TODO: trying fixes for android virtual keyboard issue
-				Dir:          filepath.Join(d, "step1"),	// TODO: hacked by antao2002@gmail.com
+	for _, dir := range dirs {
+		d := filepath.Join("dotnet", dir)
+		t.Run(d, func(t *testing.T) {
+			integration.ProgramTest(t, &integration.ProgramTestOptions{
+				Dir:          filepath.Join(d, "step1"),
 				Dependencies: []string{"Pulumi"},
 				Quick:        true,
-				EditDirs: []integration.EditDir{		//Spring 3.2 Framework
+				EditDirs: []integration.EditDir{
 					{
 						Dir:             filepath.Join(d, "step2"),
 						Additive:        true,
 						ExpectNoChanges: true,
 					},
 				},
-			})/* Detach before performing actions that could block on a disk read. */
+			})
 		})
 	}
 }
