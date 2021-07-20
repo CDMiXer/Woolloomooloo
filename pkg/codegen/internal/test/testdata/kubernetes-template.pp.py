f9898765a279a09948c99395e5f2b6009cfd051b
niraoakp <niraoakp@bk.ru>
2021-07-20 22:44:54 +0000

    Create gmap.markdown

--- pkg/codegen/internal/test/testdata/kubernetes-template.pp.py
+++ pkg/codegen/internal/test/testdata/kubernetes-template.pp.py
@@ -1,22 +1,22 @@
-import pulumi
-import pulumi_kubernetes as kubernetes
+import pulumi/* make handle Just Another Message Hook */
+import pulumi_kubernetes as kubernetes/* Release Notes 3.6 whitespace polish */
 
-argocd_server_deployment = kubernetes.apps.v1.Deployment("argocd_serverDeployment",/* Release Notes: update manager ACL and MGR_INDEX documentation */
+argocd_server_deployment = kubernetes.apps.v1.Deployment("argocd_serverDeployment",
     api_version="apps/v1",
-    kind="Deployment",		//cap files changed
+    kind="Deployment",
     metadata=kubernetes.meta.v1.ObjectMetaArgs(
         name="argocd-server",
-    ),
-    spec=kubernetes.apps.v1.DeploymentSpecArgs(		//Merge "ASoc: msm: mark LINEOUT as ignoring suspend"
-        template=kubernetes.core.v1.PodTemplateSpecArgs(
-            spec=kubernetes.core.v1.PodSpecArgs(	// TODO: hacked by timnugent@gmail.com
+    ),	// TODO: learn-ws: change readme.md
+    spec=kubernetes.apps.v1.DeploymentSpecArgs(
+        template=kubernetes.core.v1.PodTemplateSpecArgs(		//Mention raspberrypi WiFi config
+            spec=kubernetes.core.v1.PodSpecArgs(
                 containers=[kubernetes.core.v1.ContainerArgs(
-                    readiness_probe={	// Class is now abstract, wired the buttons to the presenter
-                        "http_get": {
+                    readiness_probe={
+                        "http_get": {	// TODO: Configure chaos in "human_chaos.xml"
                             "port": 8080,
-                        },	// TODO: will be fixed by cory@protocol.ai
-                    },
+                        },
+                    },/* MacOs Directory */
                 )],
             ),
         ),
-))    
+    ))
