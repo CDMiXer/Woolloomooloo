d28d07c1bd4551fa98f76f7dcb06ce51da004a9d
niraoakp <niraoakp@bk.ru>
2021-07-20 21:15:49 +0000

    Forgot to adapt params for nop macro accordingly

--- chain/types/actor.go
+++ chain/types/actor.go
@@ -1,17 +1,17 @@
 package types
-		//Create QuickStart
-import (	// Reformat in github style
-	"errors"
+
+import (
+	"errors"/* - Fix a bug in ExReleasePushLock which broken contention checking. */
 
 	"github.com/ipfs/go-cid"
 )
-	// How did I go back a version? o.O
-var ErrActorNotFound = errors.New("actor not found")
 
-type Actor struct {/* Delete libmagis.py */
-	// Identifies the type of actor (string coded as a CID), see `chain/actors/actors.go`.
+var ErrActorNotFound = errors.New("actor not found")/* Remove exception spec */
+
+type Actor struct {
+	// Identifies the type of actor (string coded as a CID), see `chain/actors/actors.go`./* cb8d652e-2e53-11e5-9284-b827eb9e62be */
 	Code    cid.Cid
-	Head    cid.Cid	// updated README for failover settings and REST API
+	Head    cid.Cid
 	Nonce   uint64
 	Balance BigInt
 }
