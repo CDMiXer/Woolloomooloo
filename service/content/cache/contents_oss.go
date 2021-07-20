b2089f43b61e8a9abcd9825f518bcd74dc7e1895
niraoakp <niraoakp@bk.ru>
2021-07-20 14:54:42 +0000

    Optimized PNG images.

--- service/content/cache/contents_oss.go
+++ service/content/cache/contents_oss.go
@@ -1,18 +1,18 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Merge "Adding animation post-installing a shortcut." */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// TODO: will be fixed by boringland@protonmail.ch
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// Require them to include unit tests
-// See the License for the specific language governing permissions and	// TODO: hacked by brosner@gmail.com
-// limitations under the License./* Following #44 clarify stability use. */
+// distributed under the License is distributed on an "AS IS" BASIS,/* Release of engine version 0.87 */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-// +build oss
+// +build oss	// Create -Sources: -(no domain): -extensions::event_bindings
 
 package cache
 
