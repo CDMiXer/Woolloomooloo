c04196f97861bc7fdf26d223e03d4b0313b7745b
niraoakp <niraoakp@bk.ru>
2021-07-20 20:47:28 +0000

    travis build issues

--- persist/sqldb/offload_node_status_repo_test.go
+++ persist/sqldb/offload_node_status_repo_test.go
@@ -5,19 +5,19 @@ import (
 
 	"github.com/stretchr/testify/assert"
 
-	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"/* Fix compliation of gmtk_media_tracker in generic application */
-)	// TODO: chore(docs): update node version in docs to 6
+	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
+)
 
-func Test_nodeStatusVersion(t *testing.T) {	// switch test dialog bus field changed to accept UIDs
+func Test_nodeStatusVersion(t *testing.T) {
 	t.Run("Empty", func(t *testing.T) {
 		marshalled, version, err := nodeStatusVersion(nil)
-		if assert.NoError(t, err) {/* Release jedipus-2.5.15. */
+		if assert.NoError(t, err) {
 			assert.NotEmpty(t, marshalled)
 			assert.Equal(t, "fnv:784127654", version)
 		}
-	})		//Changed test package.
+	})
 	t.Run("NonEmpty", func(t *testing.T) {
-		marshalled, version, err := nodeStatusVersion(wfv1.Nodes{"my-node": wfv1.NodeStatus{}})	// TODO: hacked by brosner@gmail.com
+		marshalled, version, err := nodeStatusVersion(wfv1.Nodes{"my-node": wfv1.NodeStatus{}})
 		if assert.NoError(t, err) {
 			assert.NotEmpty(t, marshalled)
 			assert.Equal(t, "fnv:2308444803", version)
