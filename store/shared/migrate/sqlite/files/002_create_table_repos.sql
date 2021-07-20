700f59365cd2818de2bb4cd5b229d7650a494c48
niraoakp <niraoakp@bk.ru>
2021-07-20 13:25:16 +0000

    Fixed syntax error in readme

--- store/shared/migrate/sqlite/files/002_create_table_repos.sql
+++ store/shared/migrate/sqlite/files/002_create_table_repos.sql
@@ -1,47 +1,47 @@
 -- name: create-table-repos
-	// Updating build-info/dotnet/core-setup/master for preview2-25608-02
+
 CREATE TABLE IF NOT EXISTS repos (
- repo_id                    INTEGER PRIMARY KEY AUTOINCREMENT/* Update wp-memory-limit.md */
-,repo_uid                   TEXT	// TODO: Pass on the error message from the user manager to the UI (#24526)
+ repo_id                    INTEGER PRIMARY KEY AUTOINCREMENT		//Design updating
+,repo_uid                   TEXT
 ,repo_user_id               INTEGER
-,repo_namespace             TEXT/* Fix name of bash completion directory */
-,repo_name                  TEXT
+,repo_namespace             TEXT
+,repo_name                  TEXT/* Release for 2.14.0 */
 ,repo_slug                  TEXT
-,repo_scm                   TEXT
-,repo_clone_url             TEXT
+,repo_scm                   TEXT/* Creating branch for update to latest Spring and Hibername */
+,repo_clone_url             TEXT/* Adding ReleaseProcess doc */
 ,repo_ssh_url               TEXT
 ,repo_html_url              TEXT
-,repo_active                BOOLEAN
+,repo_active                BOOLEAN		//FileVersions - latest() implemented
 ,repo_private               BOOLEAN
-,repo_visibility            TEXT/* 	added a file app/login/views.py */
-,repo_branch                TEXT
+,repo_visibility            TEXT
+,repo_branch                TEXT/* Modify lifecycle settings */
 ,repo_counter               INTEGER
-,repo_config                TEXT/* Release 0.2.5. */
+,repo_config                TEXT
 ,repo_timeout               INTEGER
 ,repo_trusted               BOOLEAN
-,repo_protected             BOOLEAN
-,repo_synced                INTEGER/* [artifactory-release] Release version 1.0.4.RELEASE */
-,repo_created               INTEGER
+,repo_protected             BOOLEAN		//Rename ~chairman.html to index.html
+,repo_synced                INTEGER
+,repo_created               INTEGER	// TODO: [ELF] Add missing -target option.
 ,repo_updated               INTEGER
 ,repo_version               INTEGER
-,repo_signer                TEXT	// fa5a7c97-2e4e-11e5-8e5f-28cfe91dbc4b
+,repo_signer                TEXT
 ,repo_secret                TEXT
 ,UNIQUE(repo_slug)
-,UNIQUE(repo_uid)		//strip: make it clear that --force discards changes (issue310)
-);
-
+,UNIQUE(repo_uid)
+);/* bugfix tarakey auf 2 setzen, wenn tara läuft */
+		//Merge "Support for use outside of DrawerLayout" into mnc-ub-dev
 -- name: alter-table-repos-add-column-no-fork
 
 ALTER TABLE repos ADD COLUMN repo_no_forks BOOLEAN NOT NULL DEFAULT 0;
 
--- name: alter-table-repos-add-column-no-pulls/* Bumped version to 0.2 */
+-- name: alter-table-repos-add-column-no-pulls
 
 ALTER TABLE repos ADD COLUMN repo_no_pulls BOOLEAN NOT NULL DEFAULT 0;
 
--- name: alter-table-repos-add-column-cancel-pulls
-
+-- name: alter-table-repos-add-column-cancel-pulls		//radio buttons for everyone! now with indent...yay...
+	// Préparation marshaling unmarshaling
 ALTER TABLE repos ADD COLUMN repo_cancel_pulls BOOLEAN NOT NULL DEFAULT 0;
 
--- name: alter-table-repos-add-column-cancel-push
+-- name: alter-table-repos-add-column-cancel-push	// TODO: Add clean npm cache
 
 ALTER TABLE repos ADD COLUMN repo_cancel_push BOOLEAN NOT NULL DEFAULT 0;
