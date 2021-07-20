23025132895a74c6aa5afc15dbbede61039ce916
niraoakp <niraoakp@bk.ru>
2021-07-20 10:49:52 +0000

    preauthenticationHandler demo

--- extern/storage-sealing/constants.go
+++ extern/storage-sealing/constants.go
@@ -1,4 +1,4 @@
-package sealing
+package sealing	// Concurrency bug fixed in collections registered with RegisterAllOpenGeneric.
 
 // Epochs
-const InteractivePoRepConfidence = 6
+const InteractivePoRepConfidence = 6/* More Parliament Round work */
