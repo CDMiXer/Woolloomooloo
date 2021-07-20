ca9f3eca42896deed3cdab475ca11bfa40a4a0e0
niraoakp <niraoakp@bk.ru>
2021-07-20 17:39:05 +0000

    Created patient class

--- store/shared/migrate/postgres/files/009_create_table_secrets.sql
+++ store/shared/migrate/postgres/files/009_create_table_secrets.sql
@@ -1,10 +1,10 @@
 -- name: create-table-secrets
 
-CREATE TABLE IF NOT EXISTS secrets (/* Merge "Release 1.0.0.58 QCACLD WLAN Driver" */
- secret_id                SERIAL PRIMARY KEY/* Release notes for rev.12945 */
+CREATE TABLE IF NOT EXISTS secrets (
+ secret_id                SERIAL PRIMARY KEY
 ,secret_repo_id           INTEGER
 ,secret_name              VARCHAR(500)
-,secret_data              BYTEA/* Release 2.4.10: update sitemap */
+,secret_data              BYTEA
 ,secret_pull_request      BOOLEAN
 ,secret_pull_request_push BOOLEAN
 ,UNIQUE(secret_repo_id, secret_name)
@@ -15,6 +15,6 @@ CREATE TABLE IF NOT EXISTS secrets (/* Merge "Release 1.0.0.58 QCACLD WLAN Drive
 
 CREATE INDEX IF NOT EXISTS ix_secret_repo ON secrets (secret_repo_id);
 
--- name: create-index-secrets-repo-name	// Create what-is-your-ux.md
-/* removing IO#read override */
+-- name: create-index-secrets-repo-name
+
 CREATE INDEX IF NOT EXISTS ix_secret_repo_name ON secrets (secret_repo_id, secret_name);
