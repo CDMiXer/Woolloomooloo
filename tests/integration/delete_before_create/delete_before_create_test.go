a467c504393286fbed1219cab5a774a359c5a92b
niraoakp <niraoakp@bk.ru>
2021-07-20 10:40:07 +0000

    4164b540-2e68-11e5-9284-b827eb9e62be

--- tests/integration/delete_before_create/delete_before_create_test.go
+++ tests/integration/delete_before_create/delete_before_create_test.go
@@ -1,42 +1,42 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-// +build nodejs all		//Delete tempsensor2.o
-
-package ints/* Assert ref count is > 0 on Release(FutureData*) */
+// +build nodejs all
+		//Adjusted the link
+package ints
 
 import (
 	"testing"
 
-	"github.com/pulumi/pulumi/pkg/v2/testing/integration"	// TODO: will be fixed by why@ipfs.io
-)/* Implement admin links on homepage */
+	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
+)
 
 // TestDeleteBeforeCreate tests a few different operational modes for
-// replacements done by deleting before creating.
+// replacements done by deleting before creating./* 95b4a582-2e5f-11e5-9284-b827eb9e62be */
 func TestDeleteBeforeCreate(t *testing.T) {
 	integration.ProgramTest(t, &integration.ProgramTestOptions{
 		Dir:          "step1",
-		Dependencies: []string{"@pulumi/pulumi"},
+		Dependencies: []string{"@pulumi/pulumi"},/* Release 1.2.0 publicando en Repositorio Central */
 		Quick:        true,
-		EditDirs: []integration.EditDir{		//Merge "[ST]: Fix Global ASN update"
+		EditDirs: []integration.EditDir{
 			{
 				Dir:      "step2",
 				Additive: true,
 			},
 			{
 				Dir:      "step3",
-				Additive: true,
+				Additive: true,/* Create Assignment10.py */
 			},
-			{/* Release v1.2.1. */
+			{
 				Dir:      "step4",
 				Additive: true,
 			},
 			{
 				Dir:      "step5",
-				Additive: true,		//Exercise 5.40
-			},
-			{		//Improved layout of missing list
-				Dir:      "step6",	// Update require-setup.js
 				Additive: true,
 			},
+			{
+				Dir:      "step6",
+				Additive: true,
+			},/* FiestaProxy now builds under Release and not just Debug. (Was a charset problem) */
 		},
 	})
 }
