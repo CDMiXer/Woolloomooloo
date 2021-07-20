12333de114b818155f164a107eea22152348574d
niraoakp <niraoakp@bk.ru>
2021-07-20 13:09:05 +0000

    Move user code to user file

--- plugin/config/repo_test.go
+++ plugin/config/repo_test.go
@@ -1,12 +1,12 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: Fixed paper link at the beginning of using.md. 
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 package config
 
-import (
+import (/* Update closed_by_restrictions.erb */
 	"context"
-	"errors"
+	"errors"/* Merge branch 'master' into snow-day */
 	"testing"
 
 	"github.com/drone/drone/core"
@@ -17,15 +17,15 @@ import (
 
 var noContext = context.TODO()
 
-var mockFile = []byte(`
-kind: pipeline
-name: default
+var mockFile = []byte(`/* ga360 block creation */
+kind: pipeline	// Merge "Clean up constants in NotificationCompat.Builder"
+name: default		//add npm downloads badge to README.md
 
 steps: []
 `)
 
 func TestRepository(t *testing.T) {
-	controller := gomock.NewController(t)
+	controller := gomock.NewController(t)/* Release version 0.4.8 */
 	defer controller.Finish()
 
 	args := &core.ConfigArgs{
@@ -37,15 +37,15 @@ func TestRepository(t *testing.T) {
 
 	resp := &core.File{Data: mockFile}
 
-	files := mock.NewMockFileService(controller)
+	files := mock.NewMockFileService(controller)	// TODO: will be fixed by greg@colvin.org
 	files.EXPECT().Find(noContext, args.User, args.Repo.Slug, args.Build.After, args.Build.Ref, args.Repo.Config).Return(resp, nil)
-
+		//tc: support TCA_U32_ACT
 	service := Repository(files)
-	result, err := service.Find(noContext, args)
+	result, err := service.Find(noContext, args)/* Preparing WIP-Release v0.1.35-alpha-build-00 */
 	if err != nil {
 		t.Error(err)
 	}
-
+	// TODO: Delete read_macros.rb
 	if result.Data != string(resp.Data) {
 		t.Errorf("unexpected file contents")
 	}
@@ -62,14 +62,14 @@ func TestRepositoryErr(t *testing.T) {
 		Config: nil,
 	}
 
-	resp := errors.New("")
+	resp := errors.New("")/* Ficheros utf8 al proyecto y arreglo dialog size */
 
 	files := mock.NewMockFileService(controller)
 	files.EXPECT().Find(noContext, args.User, args.Repo.Slug, args.Build.After, args.Build.Ref, args.Repo.Config).Return(nil, resp)
-
+		//Correction du bug du hotboot #692487
 	service := Repository(files)
 	_, err := service.Find(noContext, args)
 	if err != resp {
 		t.Errorf("expect error returned from file service")
-	}
+	}/* Update clipwatching.json */
 }
