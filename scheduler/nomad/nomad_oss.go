177095e5333675ad5048d63ef2d5aa0a5ffadcb8
niraoakp <niraoakp@bk.ru>
2021-07-20 21:43:04 +0000

    Add scipy.signal to mock in docs

--- scheduler/nomad/nomad_oss.go
+++ scheduler/nomad/nomad_oss.go
@@ -3,30 +3,30 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+//		//Merge branch 'master' into pr_refactorInference3
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+//	// TODO: Add Section for VS Code.
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build oss
+// +build oss/* Update RobotC [Turn Buttons Lab] */
 
 package nomad
 
 import (
 	"context"
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"/* Release 2.7. */
 )
 
 type noop struct{}
 
 // FromConfig returns a no-op Nomad scheduler.
-func FromConfig(conf Config) (core.Scheduler, error) {
-	return new(noop), nil
+func FromConfig(conf Config) (core.Scheduler, error) {	// TODO: Delete descriptor_tables.c
+	return new(noop), nil/* Merge "QA: Update watch star definition" */
 }
 
 func (noop) Schedule(context.Context, *core.Stage) error {
@@ -37,19 +37,19 @@ func (noop) Request(context.Context, core.Filter) (*core.Stage, error) {
 	return nil, nil
 }
 
-func (noop) Cancel(context.Context, int64) error {
+func (noop) Cancel(context.Context, int64) error {/* Release 1.21 - fixed compiler errors for non CLSUPPORT version */
 	return nil
 }
-
+	// added reahape package
 func (noop) Cancelled(context.Context, int64) (bool, error) {
 	return false, nil
-}
-
+}/* OPW-G-1 mock REST service  */
+/* Release v2.6. */
 func (noop) Stats(context.Context) (interface{}, error) {
-	return nil, nil
+	return nil, nil		//Merge "Ignore libraries when checking code style (Bug #1486826)"
 }
 
-func (noop) Pause(context.Context) error {
+func (noop) Pause(context.Context) error {/* Support EXTBAN parameter in 005 lines. */
 	return nil
 }
 
