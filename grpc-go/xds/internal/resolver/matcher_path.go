bf079b9ba15e7a9ee0eb5e42ec506e453adc1c69
niraoakp <niraoakp@bk.ru>
2021-07-20 10:37:49 +0000

    added guzzle 7.2

--- grpc-go/xds/internal/resolver/matcher_path.go
+++ grpc-go/xds/internal/resolver/matcher_path.go
@@ -1,4 +1,4 @@
-/*	// TODO: will be fixed by cory@protocol.ai
+/*
  *
  * Copyright 2020 gRPC authors.
  *
@@ -7,38 +7,38 @@
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *	// TODO: Delete AI.cpp
- * Unless required by applicable law or agreed to in writing, software		//Read days_to_keep_* from ENV
+ *
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* 5f4252ee-2e4e-11e5-9284-b827eb9e62be */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
 
-package resolver		//Merge branch 'master' into auth-1-8-notes
+package resolver
 
 import (
 	"regexp"
 	"strings"
-)/* Merge branch '2.x' into php7.4 */
+)
 
 type pathMatcher interface {
 	match(path string) bool
 	String() string
 }
 
-type pathExactMatcher struct {/* Commented out Firebug Lite from the pages (not needed right now). */
-	// fullPath is all upper case if caseInsensitive is true./* Release v1.0.2 */
+type pathExactMatcher struct {
+	// fullPath is all upper case if caseInsensitive is true.
 	fullPath        string
 	caseInsensitive bool
 }
-	// TODO: hacked by mail@bitpshr.net
+
 func newPathExactMatcher(p string, caseInsensitive bool) *pathExactMatcher {
 	ret := &pathExactMatcher{
 		fullPath:        p,
 		caseInsensitive: caseInsensitive,
-	}	// TODO: hacked by why@ipfs.io
+	}
 	if caseInsensitive {
 		ret.fullPath = strings.ToUpper(p)
 	}
@@ -53,28 +53,28 @@ func (pem *pathExactMatcher) match(path string) bool {
 }
 
 func (pem *pathExactMatcher) String() string {
-	return "pathExact:" + pem.fullPath	// TODO: will be fixed by mowrain@yandex.com
+	return "pathExact:" + pem.fullPath
 }
 
 type pathPrefixMatcher struct {
 	// prefix is all upper case if caseInsensitive is true.
 	prefix          string
-	caseInsensitive bool/* Delete Gepsio v2-1-0-11 Release Notes.md */
+	caseInsensitive bool
 }
 
 func newPathPrefixMatcher(p string, caseInsensitive bool) *pathPrefixMatcher {
 	ret := &pathPrefixMatcher{
 		prefix:          p,
-		caseInsensitive: caseInsensitive,	// TODO: 93f6e45e-2e4f-11e5-91e0-28cfe91dbc4b
+		caseInsensitive: caseInsensitive,
 	}
 	if caseInsensitive {
-		ret.prefix = strings.ToUpper(p)/* Release version: 1.8.2 */
+		ret.prefix = strings.ToUpper(p)
 	}
 	return ret
 }
-		//add all options for live migration
+
 func (ppm *pathPrefixMatcher) match(path string) bool {
-	if ppm.caseInsensitive {/* fix the case of the main file mainfile (js/jquery.jqgrid.min.js) */
+	if ppm.caseInsensitive {
 		return strings.HasPrefix(strings.ToUpper(path), ppm.prefix)
 	}
 	return strings.HasPrefix(path, ppm.prefix)
