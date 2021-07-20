cadc513adbf8b3008aef5933faeba4f5acda8324
niraoakp <niraoakp@bk.ru>
2021-07-20 16:29:15 +0000

    Merge branch 'master' into 240-release-candidate-1

--- core/netrc_test.go
+++ core/netrc_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
-	// README: change pahaz nickname
+// that can be found in the LICENSE file./* Update geo nameservers - Print out multiple nameservers */
+		//navigation list
 // +build !oss
 
-package core
+package core/* Release version: 1.10.0 */
