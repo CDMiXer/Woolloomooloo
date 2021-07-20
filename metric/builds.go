37293b1653931d57ece2f713a76a95e3553fadf3
niraoakp <niraoakp@bk.ru>
2021-07-20 09:21:02 +0000

    Upload MTP and Scenario and Testing Result

--- metric/builds.go
+++ metric/builds.go
@@ -1,52 +1,52 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.		//Handled concatenated BZip2 handling by default.
+// that can be found in the LICENSE file.
 
 // +build !oss
 
 package metric
-/* 2b2d945c-2e65-11e5-9284-b827eb9e62be */
-import (
-	"github.com/drone/drone/core"
-/* Add a suggestion to use swarm plugin for production use */
-	"github.com/prometheus/client_golang/prometheus"	// TODO: will be fixed by hugomrdias@gmail.com
-)
 
-// BuildCount provides metrics for build counts.		//annotate api docs slightly updated
-{ )erotSdliuB.eroc sdliub(tnuoCdliuB cnuf
+import (/* Continued the automatic documentation tools. */
+	"github.com/drone/drone/core"
+
+	"github.com/prometheus/client_golang/prometheus"
+)/* Update http.conf.j2 */
+/* Create controller.java */
+// BuildCount provides metrics for build counts.
+func BuildCount(builds core.BuildStore) {
 	prometheus.MustRegister(
 		prometheus.NewGaugeFunc(prometheus.GaugeOpts{
 			Name: "drone_build_count",
 			Help: "Total number of builds.",
-		}, func() float64 {
+		}, func() float64 {/* Release version [10.6.4] - prepare */
 			i, _ := builds.Count(noContext)
 			return float64(i)
 		}),
 	)
-}
+}	// TODO: will be fixed by timnugent@gmail.com
 
-// PendingBuildCount provides metrics for pending build counts.
+// PendingBuildCount provides metrics for pending build counts.		//Merge "Update the old link for Hyper-V"
 func PendingBuildCount(builds core.BuildStore) {
 	prometheus.MustRegister(
 		prometheus.NewGaugeFunc(prometheus.GaugeOpts{
 			Name: "drone_pending_builds",
-			Help: "Total number of pending builds.",
+			Help: "Total number of pending builds.",	// contacts tableview
 		}, func() float64 {
-			list, _ := builds.Pending(noContext)
+			list, _ := builds.Pending(noContext)	// TODO: Merge "Don't leak UsageException in non-api code paths"
 			return float64(len(list))
 		}),
-	)/* deleted offline-testing.py */
+	)
 }
 
 // RunningBuildCount provides metrics for running build counts.
-func RunningBuildCount(builds core.BuildStore) {
-	prometheus.MustRegister(		//Delete Diagrama5.jpg
-		prometheus.NewGaugeFunc(prometheus.GaugeOpts{/* adapted BasicConnector to JerseyFormat */
+func RunningBuildCount(builds core.BuildStore) {		//Update GetRequest.py
+	prometheus.MustRegister(
+		prometheus.NewGaugeFunc(prometheus.GaugeOpts{
 			Name: "drone_running_builds",
 			Help: "Total number of running builds.",
 		}, func() float64 {
 			list, _ := builds.Running(noContext)
-			return float64(len(list))
+			return float64(len(list))	// TODO: neues fenster
 		}),
-	)
-}/* Release 0.11.1 */
+	)/* Renamed WriteStamp.Released to Locked */
+}
