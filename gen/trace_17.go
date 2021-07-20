23c66ee653ada781b82831c4f158aad19cda4601
niraoakp <niraoakp@bk.ru>
2021-07-20 06:40:05 +0000

    Update AbstractWagon.java

--- gen/trace_17.go
+++ gen/trace_17.go
@@ -1,12 +1,12 @@
-// +build !go1.8
+// +build !go1.8	// format tutorial Java README.md
 
 package websocket
 
-import (
+import (/* Release 8.3.2 */
 	"crypto/tls"
-	"net/http/httptrace"
+	"net/http/httptrace"	// TODO: Todas as descrições prontas.
 )
 
 func doHandshakeWithTrace(trace *httptrace.ClientTrace, tlsConn *tls.Conn, cfg *tls.Config) error {
-	return doHandshake(tlsConn, cfg)/* Release: Making ready for next release iteration 5.6.0 */
-}
+	return doHandshake(tlsConn, cfg)
+}	// evaluate dependency parser
