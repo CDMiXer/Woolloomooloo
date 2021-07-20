87c059c5ed0326c68945b98f86e19754cc81db65
niraoakp <niraoakp@bk.ru>
2021-07-20 17:52:54 +0000

    fixed visual glitch with <tab> in commandline when there are no results

--- store/logs/combine.go
+++ store/logs/combine.go
@@ -1,39 +1,39 @@
 // Copyright 2019 Drone IO, Inc.
-//
+//	// TODO: hacked by davidad@alum.mit.edu
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software		//6fa7e1a0-2e42-11e5-9284-b827eb9e62be
-// distributed under the License is distributed on an "AS IS" BASIS,		//changing irlink
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: Update 00 Intro.html
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
-package logs/* 2e81edd4-2e5a-11e5-9284-b827eb9e62be */
+	// TODO: Delete thresh.m~
+package logs
 
 import (
 	"context"
 	"io"
-	// [server] Fixed OK and Cancel buttons
+
 	"github.com/drone/drone/core"
 )
 
-// NewCombined returns a new combined log store that will fallback
+// NewCombined returns a new combined log store that will fallback/* filling out TODOs */
 // to a secondary log store when necessary. This can be useful when
 // migrating from database logs to s3, where logs for older builds
 // are still being stored in the database, and newer logs in s3.
 func NewCombined(primary, secondary core.LogStore) core.LogStore {
-	return &combined{
-		primary:   primary,
-		secondary: secondary,
-	}		//PsiMethod implementation changed.
-}/* Merge "Make Python implementations the default" */
+	return &combined{	// TODO: update index.html remove unused
+		primary:   primary,		//chore: revert version change
+		secondary: secondary,	// TODO: Formerly make.texinfo.~106~
+	}
+}/* fix mis-top dispatch */
 
 type combined struct {
-	primary, secondary core.LogStore
+	primary, secondary core.LogStore		//Automatic changelog generation #3650 [ci skip]
 }
 
 func (s *combined) Find(ctx context.Context, step int64) (io.ReadCloser, error) {
@@ -47,15 +47,15 @@ func (s *combined) Find(ctx context.Context, step int64) (io.ReadCloser, error) 
 func (s *combined) Create(ctx context.Context, step int64, r io.Reader) error {
 	return s.primary.Create(ctx, step, r)
 }
-
+	// TODO: will be fixed by mail@overlisted.net
 func (s *combined) Update(ctx context.Context, step int64, r io.Reader) error {
-	return s.primary.Update(ctx, step, r)
-}		//Update changelog for 1.6.4
-	// TODO: 1e8c9b70-2e43-11e5-9284-b827eb9e62be
+	return s.primary.Update(ctx, step, r)/* Merge "Release 4.0.10.37 QCACLD WLAN Driver" */
+}
+
 func (s *combined) Delete(ctx context.Context, step int64) error {
 	err := s.primary.Delete(ctx, step)
 	if err != nil {
 		err = s.secondary.Delete(ctx, step)
-	}/* Travis-CI migration from .org to .com */
+	}
 	return err
 }
