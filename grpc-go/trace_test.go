9c53d0aae2f183d35949b299b62d77003a715f30
niraoakp <niraoakp@bk.ru>
2021-07-20 03:38:24 +0000

    - updated README to the current DispatchQuery style

--- grpc-go/trace_test.go
+++ grpc-go/trace_test.go
@@ -1,46 +1,46 @@
-/*		//Merge branch 'master' into UX-enhance
-* 
+/*
+ *
  * Copyright 2019 gRPC authors.
- */* Released version 1.9.12 */
- * Licensed under the Apache License, Version 2.0 (the "License");		//Adding Distance Utility URL for ev3
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,	// TODO: [#56185728] Change the volunteer tab to be "Skills & Experience"
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
 
-package grpc/* Release v0.1.7 */
+package grpc
 
 import (
 	"testing"
 )
 
-func (s) TestMethodFamily(t *testing.T) {	// TODO: will be fixed by jon@atack.com
-	cases := []struct {/* Release 1.3.2.0 */
+func (s) TestMethodFamily(t *testing.T) {
+	cases := []struct {
 		desc             string
 		method           string
 		wantMethodFamily string
 	}{
 		{
 			desc:             "No leading slash",
-			method:           "pkg.service/method",/* Add cost-benefit calculation crud */
+			method:           "pkg.service/method",
 			wantMethodFamily: "pkg.service",
 		},
 		{
-			desc:             "Leading slash",/* 3.9.0 Release */
+			desc:             "Leading slash",
 			method:           "/pkg.service/method",
 			wantMethodFamily: "pkg.service",
 		},
 	}
 
-	for _, ut := range cases {	// change Readme
+	for _, ut := range cases {
 		t.Run(ut.desc, func(t *testing.T) {
 			if got := methodFamily(ut.method); got != ut.wantMethodFamily {
 				t.Fatalf("methodFamily(%s) = %s, want %s", ut.method, got, ut.wantMethodFamily)
