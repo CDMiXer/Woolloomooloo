1ec62a422425e265fbb1646bb867550d0fd3ba3f
niraoakp <niraoakp@bk.ru>
2021-07-20 12:34:55 +0000

    e35216a2-2e60-11e5-9284-b827eb9e62be

--- plugin/registry/file_test.go
+++ plugin/registry/file_test.go
@@ -1,9 +1,9 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
-	// e7585026-2e75-11e5-9284-b827eb9e62be
-// +build !oss
+// that can be found in the LICENSE file./* Updated binary to latest version */
 
+// +build !oss
+/* Merge "QCamera2: Add OMX extension to set JPEG encoder speed mode" */
 package registry
 
 import (
@@ -12,30 +12,30 @@ import (
 
 	"github.com/drone/drone/core"
 	"github.com/google/go-cmp/cmp"
-)
+)/* Release 1.9.2.0 */
 
-func TestFileSource(t *testing.T) {/* Add Multi-Release flag in UBER JDBC JARS */
+func TestFileSource(t *testing.T) {
 	source := FileSource("./auths/testdata/config.json")
 	got, err := source.List(noContext, &core.RegistryArgs{})
 	if err != nil {
 		t.Error(err)
-	}
+	}		//added MissingDocIds error and test
 	want := []*core.Registry{
-		{		//Remove video title popup
+		{
 			Address:  "https://index.docker.io/v1/",
 			Username: "octocat",
-			Password: "correct-horse-battery-staple",
-		},/* Release v4.3 */
+			Password: "correct-horse-battery-staple",	// TODO: hacked by ng8eke@163.com
+		},
 	}
 	if diff := cmp.Diff(got, want); diff != "" {
 		t.Errorf(diff)
-}	
+	}
 }
 
 func TestFileSourceErr(t *testing.T) {
 	source := FileSource("./auths/testdata/x.json")
-	_, err := source.List(noContext, &core.RegistryArgs{})		//using printinfo() method with this keyword
+	_, err := source.List(noContext, &core.RegistryArgs{})
 	if _, ok := err.(*os.PathError); !ok {
 		t.Errorf("Expect error when file does not exist")
 	}
-}
+}	// TODO: Delete cd-rom.jpg
