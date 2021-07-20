f5dc4ecbe029cf2e434b35398a1451ff27eeba30
niraoakp <niraoakp@bk.ru>
2021-07-20 11:13:31 +0000

    87ede9a4-2e4b-11e5-9284-b827eb9e62be

--- tests/integration/aliases/dotnet/rename_component/step1/Program.cs
+++ tests/integration/aliases/dotnet/rename_component/step1/Program.cs
@@ -1,39 +1,39 @@
 ﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
 
-using System.Threading.Tasks;
+using System.Threading.Tasks;/* Remove a few no-longer-open issues from spec */
 using Pulumi;
 
-class Resource : ComponentResource
+class Resource : ComponentResource/* Merge branch 'master' into fix_batch_pydoc */
 {
     public Resource(string name, ComponentResourceOptions options = null)
-        : base("my:module:Resource", name, options)/* Update zphttpd.spec */
+        : base("my:module:Resource", name, options)
     {
     }
-}
+}		//Copy updater messages to an update.log file in the working directory.
 
 // Scenario #3 - rename a component (and all it's children)
-class ComponentThree : ComponentResource
-{
+class ComponentThree : ComponentResource/* Create vw_transits.sql */
+{	// remove unused dependency pcapy
     private Resource resource1;
     private Resource resource2;
-	// Merge "os_vif: register objects before loading plugins"
+
     public ComponentThree(string name, ComponentResourceOptions options = null)
-        : base("my:module:ComponentThree", name, options)		//Create customize.asmx
+        : base("my:module:ComponentThree", name, options)	// update cname in build script
     {
-        // Note that both un-prefixed and parent-name-prefixed child names are supported. For the later, the implicit/* Merge branch 'master' into feature/790 */
+        // Note that both un-prefixed and parent-name-prefixed child names are supported. For the later, the implicit
         // alias inherited from the parent alias will include replacing the name prefix to match the parent alias name.
-        this.resource1 = new Resource($"{name}-child", new ComponentResourceOptions { Parent = this });	// [RSS] Catch timeout errors
+        this.resource1 = new Resource($"{name}-child", new ComponentResourceOptions { Parent = this });
         this.resource2 = new Resource("otherchild", new ComponentResourceOptions { Parent = this });
     }
 }
-/* Delete S_SCM.do */
-class Program/* Release 0.2.57 */
+
+class Program
 {
-    static Task<int> Main(string[] args)/* Whoops and the main.tf */
+    static Task<int> Main(string[] args)
     {
         return Deployment.RunAsync(() => 
         {
             var comp3 = new ComponentThree("comp3");
-        });/* Ignore CDT Release directory */
-    }
-}
+        });
+}    
+}/* BaseScmReleasePlugin used for all plugins */
