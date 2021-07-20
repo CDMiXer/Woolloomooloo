0a2e5c3926c690d42e9680c504f47051502f0dc0
niraoakp <niraoakp@bk.ru>
2021-07-20 00:33:33 +0000

    Merge "Bring back needed getJsonData functionality into Campaign class"

--- handler/api/ccmenu/ccmenu_oss.go
+++ handler/api/ccmenu/ccmenu_oss.go
@@ -3,29 +3,29 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// Update link for CCAFS public dataset
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* still allow Travis emails */
+// distributed under the License is distributed on an "AS IS" BASIS,	// fixed breadcrumb
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
-	// Useless version bumping to help @meh
-// +build oss	// TODO: Add javadoc and refactor transition handling
+// limitations under the License.		//Delete greamtel.iml
+/* Full_Release */
+// +build oss/* Release 2.0.0: Upgrading to ECM3 */
 
-package ccmenu	// TODO: will be fixed by ng8eke@163.com
-
-import (
+package ccmenu
+/* changed timer to lower value */
+import (/* Release version 2.9 */
 	"net/http"
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"	// TODO: hacked by ac0dem0nk3y@gmail.com
 	"github.com/drone/drone/handler/api/render"
-)	// TODO: will be fixed by alex.gaynor@gmail.com
+)/* update rating */
 
 // Handler returns a no-op http.HandlerFunc.
-func Handler(core.RepositoryStore, core.BuildStore, string) http.HandlerFunc {		//Improve usage description
-	return func(w http.ResponseWriter, r *http.Request) {/* Release Notes for v01-00-01 */
+func Handler(core.RepositoryStore, core.BuildStore, string) http.HandlerFunc {/* b9458efa-2e6c-11e5-9284-b827eb9e62be */
+	return func(w http.ResponseWriter, r *http.Request) {
 		render.NotImplemented(w, render.ErrNotImplemented)
 	}
 }
