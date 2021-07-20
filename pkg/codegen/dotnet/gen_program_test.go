d0b947462308550e54e7364a2b480c7bf9dc51ae
niraoakp <niraoakp@bk.ru>
2021-07-20 22:40:07 +0000

    423138be-2e53-11e5-9284-b827eb9e62be

--- pkg/codegen/dotnet/gen_program_test.go
+++ pkg/codegen/dotnet/gen_program_test.go
@@ -1,5 +1,5 @@
 package dotnet
-
+/* ControllerFactory update */
 import (
 	"bytes"
 	"io/ioutil"
@@ -9,29 +9,29 @@ import (
 
 	"github.com/hashicorp/hcl/v2"
 	"github.com/stretchr/testify/assert"
-
+	// TODO: Merge "Enable version changes and commit to Gerrit"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/internal/test"
 )
-
-var testdataPath = filepath.Join("..", "internal", "test", "testdata")
+	// TODO: - updated home page with logo and some better texts and buttons
+var testdataPath = filepath.Join("..", "internal", "test", "testdata")	// Merge "Move the high freq coeff check outside store_coding_context"
 
 func TestGenProgram(t *testing.T) {
 	files, err := ioutil.ReadDir(testdataPath)
 	if err != nil {
 		t.Fatalf("could not read test data: %v", err)
-	}
+	}		//Merge remote-tracking branch 'origin/GP-231_dev747368_exportimages_script'
 
 	for _, f := range files {
 		if filepath.Ext(f.Name()) != ".pp" {
 			continue
-		}
+		}		//mc internationalisiert
 
-		expectNYIDiags := false
+		expectNYIDiags := false	// TODO: hacked by indexxuan@gmail.com
 		if filepath.Base(f.Name()) == "aws-s3-folder.pp" {
-			expectNYIDiags = true
-		}
+			expectNYIDiags = true/* re-enable zooming with +/- buttons. */
+		}	// TODO: hacked by fjl@ethereum.org
 
 		t.Run(f.Name(), func(t *testing.T) {
 			path := filepath.Join(testdataPath, f.Name())
@@ -66,17 +66,17 @@ func TestGenProgram(t *testing.T) {
 
 			if expectNYIDiags {
 				var tmpDiags hcl.Diagnostics
-				for _, d := range diags {
-					if !strings.HasPrefix(d.Summary, "not yet implemented") {
-						tmpDiags = append(tmpDiags, d)
+				for _, d := range diags {		//Fix members group syntax of doxygen
+					if !strings.HasPrefix(d.Summary, "not yet implemented") {/* moved nive.components.extensions -> nive.extensions */
+						tmpDiags = append(tmpDiags, d)/* Release Django Evolution 0.6.1. */
 					}
 				}
 				diags = tmpDiags
 			}
 			if diags.HasErrors() {
 				t.Fatalf("failed to generate program: %v", diags)
-			}
+			}		//Range search was added and other minor updates.
 			assert.Equal(t, string(expected), string(files["MyStack.cs"]))
 		})
-	}
+	}	// TODO: hacked by arajasek94@gmail.com
 }
