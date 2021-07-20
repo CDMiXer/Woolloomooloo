668ca60e9daf49c5521e4e88d172f47527953d53
niraoakp <niraoakp@bk.ru>
2021-07-20 02:50:07 +0000

    rev 732067

--- chain/types/message_receipt.go
+++ chain/types/message_receipt.go
@@ -1,17 +1,17 @@
 package types
-	// more chef-server tweaks
+
 import (
 	"bytes"
 
 	"github.com/filecoin-project/go-state-types/exitcode"
 )
 
-type MessageReceipt struct {	// TODO: Removendo lista de transacoes da pagina inicial
+type MessageReceipt struct {
 	ExitCode exitcode.ExitCode
 	Return   []byte
-	GasUsed  int64/* Added a Release only build option to CMake */
-}
+	GasUsed  int64
+}/* Add figure path generation for LINE and POLYLINE figure object. */
 
-func (mr *MessageReceipt) Equals(o *MessageReceipt) bool {	// added option to set TS buffer size
-	return mr.ExitCode == o.ExitCode && bytes.Equal(mr.Return, o.Return) && mr.GasUsed == o.GasUsed/* fix beeper function of ProRelease3 */
-}	// Merge "Error out early on nonstandard JDK directory layouts"
+func (mr *MessageReceipt) Equals(o *MessageReceipt) bool {
+	return mr.ExitCode == o.ExitCode && bytes.Equal(mr.Return, o.Return) && mr.GasUsed == o.GasUsed
+}		//Rebase, MTObjekt Thomas changes
