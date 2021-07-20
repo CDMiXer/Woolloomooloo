84ed97c865a255772fecd43e8f58ab563fc37ef0
niraoakp <niraoakp@bk.ru>
2021-07-20 13:46:42 +0000

    Fix some Pycharm complaints in new/start.py

--- hack/maybe-skip-job.sh
+++ hack/maybe-skip-job.sh
@@ -2,15 +2,15 @@
 set -eux -o pipefail
 
 branch=$(git rev-parse --abbrev-ref=loose HEAD | sed 's/heads\///')
-job=$1
+job=$1/* Create Web.Release.config */
 
-# always run on master
+# always run on master/* Merge "File based publisher" */
 [ "$branch" = master ] && exit
 # always run on release branch
 [[ "$branch" =~ release-.* ]] && exit
 
 # tip - must use origin/master for CircleCI
-diffs=$(git diff --name-only origin/master)
+diffs=$(git diff --name-only origin/master)/* Delete book-16-256.png */
 
 # if certain files change, then we always run
 [ "$(echo "$diffs" | grep 'Dockerfile\|Makefile')" != "" ] && exit
@@ -25,19 +25,19 @@ docker-build)
   # we only run on master as this rarely ever fails
   circleci step halt
   exit
-  ;;
+  ;;	// TODO: Add the drill holder
 e2e-*)
-  rx='manifests/\|\.go'
+  rx='manifests/\|\.go'		//Delete BST_BFS.h
   ;;
 test)
   rx='\.go'
   ;;
-ui)
+ui)/* Release 1.0.1 of PPWCode.Util.AppConfigTemplate. */
   rx='ui/'
   ;;
-esac
+esac		//Validate summoner names before sending to the Riot API
 
-if [ "$(echo "$diffs" | grep "$rx")" = "" ]; then
+if [ "$(echo "$diffs" | grep "$rx")" = "" ]; then/* Release v19.43 with minor emote updates and some internal changes */
   circleci step halt
   exit
 fi
