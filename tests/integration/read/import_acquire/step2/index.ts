6d17db97830b493e2ccd66c6d4486636da514504
niraoakp <niraoakp@bk.ru>
2021-07-20 07:07:02 +0000

    fix pardef

--- tests/integration/read/import_acquire/step2/index.ts
+++ tests/integration/read/import_acquire/step2/index.ts
@@ -1,18 +1,18 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* fixed linebreak formatting */
+// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: Update knoweng.yaml
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
+///* add ProRelease3 hardware */
+//     http://www.apache.org/licenses/LICENSE-2.0/* Adds bblanton to contrib */
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
-//		//* made orphaned pkglistings on pkgpage a different color
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software/* Release Notes: fix configure options text */
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: will be fixed by onhardev@bk.ru
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 import { Resource } from "./resource";
 
-// Step 2: the resource from the setup is imported, and is now managed by Pulumi.	// TODO: hacked by jon@atack.com
+// Step 2: the resource from the setup is imported, and is now managed by Pulumi.
 const a = new Resource("a", { state: 42 }, { import: "0" });
