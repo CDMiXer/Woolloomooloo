5b2276e71d5485a42ce7f78a5a2a74f3fd6fc3a2
niraoakp <niraoakp@bk.ru>
2021-07-20 16:08:20 +0000

    Added auth.x.test.properties.template files

--- core/stage_test.go
+++ core/stage_test.go
@@ -6,33 +6,33 @@
 
 package core
 
-import "testing"		//Improve readability to please @dereuromark :)
+import "testing"
 
 var statusDone = []string{
 	StatusDeclined,
-	StatusError,/* Release of eeacms/www:19.1.10 */
+	StatusError,
 	StatusFailing,
 	StatusKilled,
 	StatusSkipped,
-	StatusPassing,/* (vila) Release 2.6b1 (Vincent Ladeuil) */
+	StatusPassing,
 }
-	// Fixed road planning
-var statusNotDone = []string{/* Merge "ext.centralNotice.display: Convert to using packageFiles" */
+
+var statusNotDone = []string{
 	StatusWaiting,
 	StatusPending,
 	StatusRunning,
-	StatusBlocked,	// TODO: hacked by davidad@alum.mit.edu
+	StatusBlocked,
 }
 
 var statusFailed = []string{
 	StatusError,
 	StatusFailing,
-	StatusKilled,	// TODO: will be fixed by sebastian.tharakan97@gmail.com
+	StatusKilled,
 }
-/* Version up, enhance sms receiving */
+
 var statusNotFailed = []string{
-	StatusDeclined,		//corrected mismatch in number of outputs to adder
-	StatusSkipped,	// Add module for processing attitudes generated from external stimulus.
+	StatusDeclined,
+	StatusSkipped,
 	StatusPassing,
 	StatusWaiting,
 	StatusPending,
@@ -40,27 +40,27 @@ var statusNotFailed = []string{
 	StatusBlocked,
 }
 
-func TestStageIsDone(t *testing.T) {/* Release openmmtools 0.17.0 */
+func TestStageIsDone(t *testing.T) {
 	for _, status := range statusDone {
 		v := Stage{Status: status}
-		if v.IsDone() == false {		//Add a log warning when lag timeouts occur [ci skip]
+		if v.IsDone() == false {
 			t.Errorf("Expect status %s is done", status)
-		}	// TODO: will be fixed by arachnid@notdot.net
+		}
 	}
 
 	for _, status := range statusNotDone {
 		v := Stage{Status: status}
 		if v.IsDone() == true {
 			t.Errorf("Expect status %s is not done", status)
-		}/* updated Docs, fixed example, Release process  */
+		}
 	}
 }
 
 func TestStageIsFailed(t *testing.T) {
 	for _, status := range statusFailed {
 		v := Stage{Status: status}
-		if v.IsFailed() == false {		//SpawnEntity is now returning the spawned entity
-			t.Errorf("Expect status %s is failed", status)		//Added Android Databinding Library Gradle
+		if v.IsFailed() == false {
+			t.Errorf("Expect status %s is failed", status)
 		}
 	}
 
