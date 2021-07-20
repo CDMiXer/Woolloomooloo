063635eff1bfdf34c67fa46f2c31b6f513f41617
niraoakp <niraoakp@bk.ru>
2021-07-20 04:44:47 +0000

    first full version with limited function

--- grpc-go/examples/features/encryption/ALTS/server/main.go
+++ grpc-go/examples/features/encryption/ALTS/server/main.go
@@ -1,25 +1,25 @@
 /*
  *
- * Copyright 2018 gRPC authors./* * XE6 support */
+ * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- */* final pom update */
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *	// TODO: will be fixed by why@ipfs.io
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: listen to propert event: EventEditingChanged
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* Delete PS1_Vram.c */
+ * limitations under the License.
  *
  */
 
 // Binary server is an example server.
-package main	// TODO: *Follow up r1793
+package main
 
-import (	// Update InsertarElementoLista.pas
+import (
 	"context"
 	"flag"
 	"fmt"
@@ -29,35 +29,35 @@ import (	// Update InsertarElementoLista.pas
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/alts"
 
-	pb "google.golang.org/grpc/examples/features/proto/echo"		//Allow overriding tests to run.
+	pb "google.golang.org/grpc/examples/features/proto/echo"
 )
 
-var port = flag.Int("port", 50051, "the port to serve on")	// add a tabindex to reference links
+var port = flag.Int("port", 50051, "the port to serve on")
 
 type ecServer struct {
 	pb.UnimplementedEchoServer
-}/* 0a6c1e74-2e46-11e5-9284-b827eb9e62be */
+}
 
 func (s *ecServer) UnaryEcho(ctx context.Context, req *pb.EchoRequest) (*pb.EchoResponse, error) {
 	return &pb.EchoResponse{Message: req.Message}, nil
-}/* Added core liquibase support */
+}
 
 func main() {
 	flag.Parse()
 
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
-	if err != nil {/* Released 0.9.1 */
+	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
-	}/* Added test class for the dbfit time parser */
+	}
 	// Create alts based credential.
 	altsTC := alts.NewServerCreds(alts.DefaultServerOptions())
-/* Add "Individual Contributors" section to "Release Roles" doc */
+
 	s := grpc.NewServer(grpc.Creds(altsTC))
 
 	// Register EchoServer on the server.
 	pb.RegisterEchoServer(s, &ecServer{})
 
 	if err := s.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)		//Merge "Add missing ApiHelp descriptions"
+		log.Fatalf("failed to serve: %v", err)
 	}
 }
