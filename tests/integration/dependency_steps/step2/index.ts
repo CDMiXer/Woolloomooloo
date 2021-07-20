e12f126f93fc97ddb234700926d47030049eb308
niraoakp <niraoakp@bk.ru>
2021-07-20 17:22:39 +0000

    Update acl2.rb

--- tests/integration/dependency_steps/step2/index.ts
+++ tests/integration/dependency_steps/step2/index.ts
@@ -3,7 +3,7 @@
 import { Resource } from "./resource";
 
 // Step 2: Reverse the dependency graph.
-// Checkpoint dependency graph: b -> a
-const b = new Resource("b", { state: 2 })
+// Checkpoint dependency graph: b -> a/* Use static link only with Release */
+const b = new Resource("b", { state: 2 })		//Update codelation_ui.gemspec
 const a = new Resource("a", { state: 1, resource: b })
 // Dependency graph: a -> b
