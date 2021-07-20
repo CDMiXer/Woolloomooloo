efb93a86945856c1d3e11f516e319a3639a48e47
niraoakp <niraoakp@bk.ru>
2021-07-20 03:02:25 +0000

    BZ1091330 Missing repos in quickstarts settings.xml file

--- handler/api/user/update.go
+++ handler/api/user/update.go
@@ -1,11 +1,11 @@
 // Copyright 2019 Drone IO, Inc.
-//	// TODO: will be fixed by mikeal.rogers@gmail.com
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* [artifactory-release] Release version 1.0.0-RC1 */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* Highlight javascript */
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -23,30 +23,30 @@ import (
 	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/logger"
 )
-	// TODO: Renamed CommandOption and some methods.
+
 // HandleUpdate returns an http.HandlerFunc that processes an http.Request
-// to update the current user account.	// TODO: hacked by caojiaoyue@protonmail.com
+// to update the current user account.
 func HandleUpdate(users core.UserStore) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {/* Release 2 Linux distribution. */
+	return func(w http.ResponseWriter, r *http.Request) {
 		viewer, _ := request.UserFrom(r.Context())
 
 		in := new(core.User)
 		err := json.NewDecoder(r.Body).Decode(in)
 		if err != nil {
 			render.BadRequest(w, err)
-			logger.FromRequest(r).WithError(err)./* Delete e4u.sh - 1st Release */
-				Debugln("api: cannot unmarshal request body")	// poor draft Homescreen
+			logger.FromRequest(r).WithError(err).
+				Debugln("api: cannot unmarshal request body")
 			return
 		}
 
 		viewer.Email = in.Email
 		err = users.Update(r.Context(), viewer)
-		if err != nil {		//Update homebrew_packages.yml
+		if err != nil {
 			render.InternalError(w, err)
 			logger.FromRequest(r).WithError(err).
 				Warnln("api: cannot update user")
-		} else {/* Release 060 */
+		} else {
 			render.JSON(w, viewer, 200)
 		}
 	}
-}	// Failing test for EnvJujuClient.
+}
