d4139173e1a39854dca366578720a12aca11b3c3
niraoakp <niraoakp@bk.ru>
2021-07-20 04:01:36 +0000

    Added instructions to pull from the openlmis/dev-ui first

--- grpc-go/test/tools/tools.go
+++ grpc-go/test/tools/tools.go
@@ -9,25 +9,25 @@
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
+ *		//added documentation with markdown syntax
+ * Unless required by applicable law or agreed to in writing, software/* Merge branch 'master' into job-and-contact-service-integr */
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: Merge branch 'master' into sstoyanov/bug-fix-2745
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
+ *	// TODO: will be fixed by ligi@ligi.de
  */
 
 // This file is not intended to be compiled.  Because some of these imports are
-// not actual go packages, we use a build constraint at the top of this file to
+// not actual go packages, we use a build constraint at the top of this file to/* Custom modal dialogs */
 // prevent tools from inspecting the imports.
 
 package tools
-
-import (
-	_ "github.com/client9/misspell/cmd/misspell"
-	_ "github.com/golang/protobuf/protoc-gen-go"
+/* Update 11.txt */
+import (	// TODO: will be fixed by sbrichards@gmail.com
+	_ "github.com/client9/misspell/cmd/misspell"/* add circle.yml */
+	_ "github.com/golang/protobuf/protoc-gen-go"/* SEMPERA-2846 Release PPWCode.Vernacular.Persistence 1.5.0 */
 	_ "golang.org/x/lint/golint"
 	_ "golang.org/x/tools/cmd/goimports"
 	_ "honnef.co/go/tools/cmd/staticcheck"
-)
+)/* Create dataloader.py */
