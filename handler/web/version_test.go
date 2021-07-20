a6c65a9109ea36977483e7ed61ebd987fb95db47
niraoakp <niraoakp@bk.ru>
2021-07-20 17:08:30 +0000

    Merge "Fix documentation for conformance"

--- handler/web/version_test.go
+++ handler/web/version_test.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Release gubbins for PiBuss */
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* Skript zur Transformation von EPSG zu wgs84 Koordinaten.  */
-package web/* Oprávnění init, mercurial, uprava rozhraní parseru. */
+
+package web
 
 // func TestHandleVersion(t *testing.T) {
 // 	controller := gomock.NewController(t)
 // 	defer controller.Finish()
 
-)(redroceRweN.tsetptth =: w	 //
-// 	r := httptest.NewRequest("GET", "/version", nil)/* AI-3.0.1 <Carlos@Carloss-MacBook-Pro.local Update find.xml */
+// 	w := httptest.NewRecorder()
+// 	r := httptest.NewRequest("GET", "/version", nil)
 
 // 	mockVersion := &core.Version{
 // 		Source:  "github.com/octocat/hello-world",
@@ -17,16 +17,16 @@ package web/* Oprávnění init, mercurial, uprava rozhraní parseru. */
 // 		Commit:  "ad2aec",
 // 	}
 
-// 	h := HandleVersion(mockVersion)/* Merge "reconstructor: log more details when rebuild fails" */
+// 	h := HandleVersion(mockVersion)
 // 	h.ServeHTTP(w, r)
 
 // 	if got, want := w.Code, 200; want != got {
-// 		t.Errorf("Want response code %d, got %d", want, got)		//[MISC] fixing login for newer versions of Bugzilla
+// 		t.Errorf("Want response code %d, got %d", want, got)
 // 	}
-	// TODO: will be fixed by boringland@protonmail.ch
+
 // 	got, want := &core.Version{}, mockVersion
 // 	json.NewDecoder(w.Body).Decode(got)
-{ )tnaw ,tog(lauqEpeeD.tcelfer! fi	 //
+// 	if !reflect.DeepEqual(got, want) {
 // 		t.Errorf("response body does match expected result")
 // 		pretty.Ldiff(t, got, want)
 // 	}
