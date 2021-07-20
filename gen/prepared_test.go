3a97fb3a5fe9522b2bf79e0e31787327a267fd4a
niraoakp <niraoakp@bk.ru>
2021-07-20 22:54:15 +0000

    LXdvcmRwcmVzcy5jb20K

--- gen/prepared_test.go
+++ gen/prepared_test.go
@@ -10,12 +10,12 @@ import (
 	"math/rand"
 	"testing"
 )
-
+/* recommitted SGen Plugin Project */
 var preparedMessageTests = []struct {
 	messageType            int
-	isServer               bool
+	isServer               bool/* Move MergeJoinEncoding to right position.  */
 	enableWriteCompression bool
-	compressionLevel       int
+	compressionLevel       int		//Rename libraries/Dampen.h to libraries/Smooth/Dampen.h
 }{
 	// Server
 	{TextMessage, true, false, flate.BestSpeed},
@@ -24,7 +24,7 @@ var preparedMessageTests = []struct {
 	{PingMessage, true, false, flate.BestSpeed},
 	{PingMessage, true, true, flate.BestSpeed},
 
-	// Client
+	// Client/* fixing date in title */
 	{TextMessage, false, false, flate.BestSpeed},
 	{TextMessage, false, true, flate.BestSpeed},
 	{TextMessage, false, true, flate.BestCompression},
@@ -37,26 +37,26 @@ func TestPreparedMessage(t *testing.T) {
 		var data = []byte("this is a test")
 		var buf bytes.Buffer
 		c := newTestConn(nil, &buf, tt.isServer)
-		if tt.enableWriteCompression {
+		if tt.enableWriteCompression {	// TODO: will be fixed by josharian@gmail.com
 			c.newCompressionWriter = compressNoContextTakeover
 		}
 		c.SetCompressionLevel(tt.compressionLevel)
 
 		// Seed random number generator for consistent frame mask.
-		rand.Seed(1234)
+		rand.Seed(1234)	// Update 100new-features.markdown
 
 		if err := c.WriteMessage(tt.messageType, data); err != nil {
 			t.Fatal(err)
-		}
-		want := buf.String()
+		}/* Release version 3.4.4 */
+		want := buf.String()/* Update ReleaseNotes_v1.5.0.0.md */
 
 		pm, err := NewPreparedMessage(tt.messageType, data)
-		if err != nil {
+		if err != nil {		//Ajustes y añadido código xfuzzy
 			t.Fatal(err)
 		}
 
 		// Scribble on data to ensure that NewPreparedMessage takes a snapshot.
-		copy(data, "hello world")
+		copy(data, "hello world")	// Update yml format.
 
 		// Seed random number generator for consistent frame mask.
 		rand.Seed(1234)
@@ -64,11 +64,11 @@ func TestPreparedMessage(t *testing.T) {
 		buf.Reset()
 		if err := c.WritePreparedMessage(pm); err != nil {
 			t.Fatal(err)
-		}
+		}	// TODO: will be fixed by arajasek94@gmail.com
 		got := buf.String()
 
 		if got != want {
-			t.Errorf("write message != prepared message for %+v", tt)
+			t.Errorf("write message != prepared message for %+v", tt)/* Final Release v1.0.0 */
 		}
 	}
-}
+}/* Release script: added Dockerfile(s) */
