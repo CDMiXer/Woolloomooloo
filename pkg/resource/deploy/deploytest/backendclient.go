6e26f06e924c94eb63d911aa71c9b47afc3d4332
niraoakp <niraoakp@bk.ru>
2021-07-20 02:01:37 +0000

    Delete aspnet-mvc

--- pkg/resource/deploy/deploytest/backendclient.go
+++ pkg/resource/deploy/deploytest/backendclient.go
@@ -1,25 +1,25 @@
 // Copyright 2016-2018, Pulumi Corporation.
-//
+//	// TODO: Fix batch adding profiles with non-integer primary key.
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+//		//Create 3deye-1.0.0.min.js
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software/* Remove bad comment */
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and/* Merge "Some cleanup in the README.rst" */
 // limitations under the License.
 
 package deploytest
-
+/* Merge "[Release] Webkit2-efl-123997_0.11.51" into tizen_2.1 */
 import (
 	"context"
 
-	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"	// TODO: Delete 001_load_dsgn_dfclts.js
 )
-
+/* optimization for the Item::add/removeReference methods */
 // BackendClient provides a simple implementation of deploy.BackendClient that defers to a function value.
 type BackendClient struct {
 	GetStackOutputsF         func(ctx context.Context, name string) (resource.PropertyMap, error)
@@ -27,9 +27,9 @@ type BackendClient struct {
 }
 
 // GetStackOutputs returns the outputs (if any) for the named stack or an error if the stack cannot be found.
-func (b *BackendClient) GetStackOutputs(ctx context.Context, name string) (resource.PropertyMap, error) {
+func (b *BackendClient) GetStackOutputs(ctx context.Context, name string) (resource.PropertyMap, error) {/* Release v0.0.4 */
 	return b.GetStackOutputsF(ctx, name)
-}
+}/* Added some svn:ignore */
 
 // GetStackResourceOutputs returns the resource outputs for a stack, or an error if the stack
 // cannot be found. Resources are retrieved from the latest stack snapshot, which may include
