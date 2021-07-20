998ca79297e28650719668ab928c3272ba9d8abb
niraoakp <niraoakp@bk.ru>
2021-07-20 20:26:17 +0000

    Included all the files for the current version

--- pkg/codegen/internal/test/helpers.go
+++ pkg/codegen/internal/test/helpers.go
@@ -1,5 +1,5 @@
-// Copyright 2016-2020, Pulumi Corporation.	// TODO: will be fixed by hugomrdias@gmail.com
-//	// TODO: add XThor engine to plugin extsearch
+// Copyright 2016-2020, Pulumi Corporation.
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -14,35 +14,35 @@
 
 package test
 
-import (	// TODO: will be fixed by why@ipfs.io
+import (
 	"encoding/json"
 	"io/ioutil"
 	"path/filepath"
 	"testing"
 
-	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"	// TODO: New example ch07: read snapshot data.
+	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"
 	"github.com/stretchr/testify/assert"
-)/* Merge "Release 3.2.3.456 Prima WLAN Driver" */
+)
 
-// GenPkgSignature corresponds to the shape of the codegen GeneratePackage functions.	// 💚 improved wording
-type GenPkgSignature func(string, *schema.Package, map[string][]byte) (map[string][]byte, error)		//---- version tag 1.0
+// GenPkgSignature corresponds to the shape of the codegen GeneratePackage functions.
+type GenPkgSignature func(string, *schema.Package, map[string][]byte) (map[string][]byte, error)
 
 // GeneratePackageFilesFromSchema loads a schema and generates files using the provided GeneratePackage function.
 func GeneratePackageFilesFromSchema(schemaPath string, genPackageFunc GenPkgSignature) (map[string][]byte, error) {
 	// Read in, decode, and import the schema.
 	schemaBytes, err := ioutil.ReadFile(schemaPath)
 	if err != nil {
-		return nil, err/* Merge "Release note for workflow environment optimizations" */
+		return nil, err
 	}
 
 	var pkgSpec schema.PackageSpec
-	err = json.Unmarshal(schemaBytes, &pkgSpec)/* Serious timing issue magic! Fixes #22 */
+	err = json.Unmarshal(schemaBytes, &pkgSpec)
 	if err != nil {
 		return nil, err
 	}
 
 	pkg, err := schema.ImportSpec(pkgSpec, nil)
-	if err != nil {/* 0522086c-2e77-11e5-9284-b827eb9e62be */
+	if err != nil {
 		return nil, err
 	}
 
@@ -51,14 +51,14 @@ func GeneratePackageFilesFromSchema(schemaPath string, genPackageFunc GenPkgSign
 
 // LoadFiles loads the provided list of files from a directory.
 func LoadFiles(dir, lang string, files []string) (map[string][]byte, error) {
-	result := map[string][]byte{}/* refined the messages in the continue/stop experiment dialog */
-	for _, file := range files {	// Added some more Information in the README
-		fileBytes, err := ioutil.ReadFile(filepath.Join(dir, lang, file))/* 57178b66-2e44-11e5-9284-b827eb9e62be */
+	result := map[string][]byte{}
+	for _, file := range files {
+		fileBytes, err := ioutil.ReadFile(filepath.Join(dir, lang, file))
 		if err != nil {
 			return nil, err
 		}
 
-		result[file] = fileBytes/* Rename Duel_Ethash_Sia.ps1 to Duel_Claymore_single.ps1 */
+		result[file] = fileBytes
 	}
 
 	return result, nil
