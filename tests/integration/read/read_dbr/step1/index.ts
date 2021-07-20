9de490bb5a60ad3769a706e9f3cdaba938646e8c
niraoakp <niraoakp@bk.ru>
2021-07-20 13:39:44 +0000

    + implemented Parallel.Map

--- tests/integration/read/read_dbr/step1/index.ts
+++ tests/integration/read/read_dbr/step1/index.ts
@@ -1,6 +1,6 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: will be fixed by ligi@ligi.de
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -14,7 +14,7 @@
 
 import { Resource } from "./resource";
 
-// Setup: Resource A is external, Resource B is not but it depends on A. Resource C is external and	// TODO: 6f538582-2e9b-11e5-845d-10ddb1c7c412
+// Setup: Resource A is external, Resource B is not but it depends on A. Resource C is external and
 // has an input dependent on B.
 const a = new Resource("a", { state: 42 }, { id: "existing-id"} );
 const b = new Resource("b", { state: a.state.apply((b: any) => b + 1)});
