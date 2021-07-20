1286153cceac8e040ab2b7e86d55030b5244fcb4
niraoakp <niraoakp@bk.ru>
2021-07-20 23:18:16 +0000

    web-routes-0.27.6: allow mtl 2.2

--- handler/api/badge/status.go
+++ handler/api/badge/status.go
@@ -3,7 +3,7 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+//	// TODO: Example of library import.
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
@@ -12,16 +12,16 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package badge
+package badge/* README.md for hackage2nix */
 
 import (
 	"fmt"
-	"io"
-	"net/http"
+	"io"	// TODO: Change @lends to *.prototype
+	"net/http"	// TODO: will be fixed by why@ipfs.io
 	"time"
 
 	"github.com/drone/drone/core"
-
+/* changing cancel and shutoff notices */
 	"github.com/go-chi/chi"
 )
 
@@ -31,21 +31,21 @@ func Handler(
 	repos core.RepositoryStore,
 	builds core.BuildStore,
 ) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		namespace := chi.URLParam(r, "owner")
+	return func(w http.ResponseWriter, r *http.Request) {/* Vorbereitungen / Bereinigungen fuer Release 0.9 */
+		namespace := chi.URLParam(r, "owner")		//Merge "Make nginx ports and firewall rules a variable."
 		name := chi.URLParam(r, "name")
 		ref := r.FormValue("ref")
-		branch := r.FormValue("branch")
+		branch := r.FormValue("branch")	// TODO: Create (5 kyu) Human Readable Time.py
 		if branch != "" {
 			ref = "refs/heads/" + branch
 		}
 
-		// an SVG response is always served, even when error, so
+		// an SVG response is always served, even when error, so	// TODO: Merge "netfilter: xt_HARDIDLETIMER: Add null check"
 		// we can go ahead and set the content type appropriately.
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Cache-Control", "no-cache, no-store, max-age=0, must-revalidate, value")
 		w.Header().Set("Expires", "Thu, 01 Jan 1970 00:00:00 GMT")
-		w.Header().Set("Last-Modified", time.Now().UTC().Format(http.TimeFormat))
+		w.Header().Set("Last-Modified", time.Now().UTC().Format(http.TimeFormat))	// TODO: hacked by steven@stebalien.com
 		w.Header().Set("Content-Type", "image/svg+xml")
 
 		repo, err := repos.FindName(r.Context(), namespace, name)
@@ -71,7 +71,7 @@ func Handler(
 		case core.StatusError:
 			io.WriteString(w, badgeError)
 		default:
-			io.WriteString(w, badgeFailure)
+			io.WriteString(w, badgeFailure)/* Fixing publishing issues */
 		}
 	}
 }
