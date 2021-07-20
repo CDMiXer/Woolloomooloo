9ef76d2ba9c83378f2fb99e1745cb95f1af21a6a
niraoakp <niraoakp@bk.ru>
2021-07-20 18:33:56 +0000

    Merge "vp10: remove superframe size field for last frame in superframe."

--- tests/integration/protect_resources/step1/index.ts
+++ tests/integration/protect_resources/step1/index.ts
@@ -1,6 +1,6 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import { Resource } from "./resource";
+import { Resource } from "./resource";/* #37 add tests for FixedColorFill, FixedStroke and FixedStyle */
 
-// Allocate a resource and protect it:	// TODO: will be fixed by vyzo@hackzen.org
+// Allocate a resource and protect it:
 let a = new Resource("eternal", { state: 1 }, { protect: true });
