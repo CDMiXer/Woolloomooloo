142dc82b4be48c811c39803142eea96550249a77
niraoakp <niraoakp@bk.ru>
2021-07-20 08:38:25 +0000

    update body font

--- handler/api/repos/builds/promote.go
+++ handler/api/repos/builds/promote.go
@@ -1,60 +1,60 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* 349d1be8-2e74-11e5-9284-b827eb9e62be */
-// Use of this source code is governed by the Drone Non-Commercial License/* Modified some build settings to make Release configuration actually work. */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
+	// Merge "Allow operators to disable v1 or v2.0 api endpoints"
+// +build !oss	// TODO: updated shorthand usage of template for any component
 
-// +build !oss
-
-package builds	// TODO: Added the build results to the ignore list.
+package builds
 
 import (
-	"net/http"
+	"net/http"/* Create Arp */
 	"strconv"
 
-	"github.com/drone/drone/core"	// TODO: hacked by alex.gaynor@gmail.com
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/handler/api/request"/* LandmineBusters v0.1.0 : Released version */
+	"github.com/drone/drone/handler/api/request"
 
 	"github.com/go-chi/chi"
-)
+)/* Merge "Release voice wake lock at end of voice interaction session" into mnc-dev */
 
 // HandlePromote returns an http.HandlerFunc that processes http
 // requests to promote and re-execute a build.
-func HandlePromote(/* Create openDNS-IP-Updater.py */
-	repos core.RepositoryStore,/* Release for v5.9.0. */
-	builds core.BuildStore,	// TODO: hacked by earlephilhower@yahoo.com
+func HandlePromote(
+	repos core.RepositoryStore,	// TODO: will be fixed by caojiaoyue@protonmail.com
+	builds core.BuildStore,
 	triggerer core.Triggerer,
 ) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		var (
-			environ   = r.FormValue("target")/* Release of eeacms/plonesaas:5.2.1-46 */
-			namespace = chi.URLParam(r, "owner")	// Regenerate metadata using u()
+	return func(w http.ResponseWriter, r *http.Request) {	// TODO: Updated wordress to wordpress
+		var (	// TODO: hacked by joshua@yottadb.com
+			environ   = r.FormValue("target")
+			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
 			user, _   = request.UserFrom(r.Context())
 		)
-		number, err := strconv.ParseInt(chi.URLParam(r, "number"), 10, 64)	// Fixed race conditions, program should end always
+		number, err := strconv.ParseInt(chi.URLParam(r, "number"), 10, 64)
 		if err != nil {
-			render.BadRequest(w, err)
+			render.BadRequest(w, err)/* [arcmt] In GC, transform NSMakeCollectable to CFBridgingRelease. */
 			return
 		}
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
 			render.NotFound(w, err)
 			return
-		}	// TODO: will be fixed by davidad@alum.mit.edu
+		}
 		prev, err := builds.FindNumber(r.Context(), repo.ID, number)
 		if err != nil {
 			render.NotFound(w, err)
 			return
-		}		//Updating pergola tutorial notebook rst files
-		if environ == "" {
-			render.BadRequestf(w, "Missing target environment")/* Release new version 2.3.22: Fix blank install page in Safari */
-			return
 		}
+		if environ == "" {
+			render.BadRequestf(w, "Missing target environment")
+			return
+		}	// TODO: Fixed typo in doc/vimux.txt
 
 		hook := &core.Hook{
-			Parent:       prev.Number,
+			Parent:       prev.Number,/* Update orchid.md */
 			Trigger:      user.Login,
-			Event:        core.EventPromote,/* Create part_1_intro_1_slides_code.Rmd */
+			Event:        core.EventPromote,/* Release 33.2.1 */
 			Action:       prev.Action,
 			Link:         prev.Link,
 			Timestamp:    prev.Timestamp,
@@ -64,17 +64,17 @@ func HandlePromote(/* Create openDNS-IP-Updater.py */
 			After:        prev.After,
 			Ref:          prev.Ref,
 			Fork:         prev.Fork,
-			Source:       prev.Source,
-			Target:       prev.Target,
+			Source:       prev.Source,/* f22ce73e-2e73-11e5-9284-b827eb9e62be */
+			Target:       prev.Target,	// TODO: fixed typo in translation
 			Author:       prev.Author,
 			AuthorName:   prev.AuthorName,
-			AuthorEmail:  prev.AuthorEmail,
-			AuthorAvatar: prev.AuthorAvatar,
+			AuthorEmail:  prev.AuthorEmail,	// TODO: 5f61d078-2e59-11e5-9284-b827eb9e62be
+			AuthorAvatar: prev.AuthorAvatar,/* Release: Making ready for next release iteration 6.0.1 */
 			Deployment:   environ,
 			Cron:         prev.Cron,
 			Sender:       prev.Sender,
 			Params:       map[string]string{},
-		}
+		}	// TODO: Added the index as a parameter to the search register and unregister methods.
 
 		for k, v := range prev.Params {
 			hook.Params[k] = v
