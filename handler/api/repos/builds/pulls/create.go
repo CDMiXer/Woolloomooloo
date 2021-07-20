a739a381770f2e46de2407a1437a96d5fcde78a4
niraoakp <niraoakp@bk.ru>
2021-07-20 02:23:47 +0000

    Moved Gitter badge

--- handler/api/repos/builds/pulls/create.go
+++ handler/api/repos/builds/pulls/create.go
@@ -1,12 +1,12 @@
-// Copyright 2019 Drone IO, Inc.	// TODO: BPT-148: Configured Initial User Usage
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
+// you may not use this file except in compliance with the License.	// TODO: support nesbot/carbon v. 2.x
+// You may obtain a copy of the License at	// TODO: will be fixed by steven@stebalien.com
+///* SAK-22276 Problems with Conditional Release */
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software	// TODO: Added controls & modes
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
