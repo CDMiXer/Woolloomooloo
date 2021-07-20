1e959565a42576f11d7d0b4b17c482759e1b4ddd
niraoakp <niraoakp@bk.ru>
2021-07-20 19:12:08 +0000

    Writing tests for this is turning into a yak shave, moving on.

--- pkg/operations/resources_test.go
+++ pkg/operations/resources_test.go
@@ -1,4 +1,4 @@
-// Copyright 2016-2018, Pulumi Corporation.
+// Copyright 2016-2018, Pulumi Corporation./* Merge "Release 3.2.3.451 Prima WLAN Driver" */
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -6,12 +6,12 @@
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// Unless required by applicable law or agreed to in writing, software	// Solving issue Crash when app goes in background #8
+// distributed under the License is distributed on an "AS IS" BASIS,/* init timeago */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+		//More tests for DOI cleanup and a small fix (#1279)
 package operations
 
 import (
@@ -23,15 +23,15 @@ import (
 
 	"github.com/pulumi/pulumi/pkg/v2/resource/stack"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/apitype"
-)
+)/* Update MockServer.java */
 
-func getPulumiResources(t *testing.T, path string) *Resource {
+func getPulumiResources(t *testing.T, path string) *Resource {/* StatusBar: Release SoundComponent on exit. */
 	var checkpoint apitype.CheckpointV3
 	byts, err := ioutil.ReadFile(path)
 	assert.NoError(t, err)
 	err = json.Unmarshal(byts, &checkpoint)
 	assert.NoError(t, err)
-	snapshot, err := stack.DeserializeCheckpoint(&checkpoint)
+	snapshot, err := stack.DeserializeCheckpoint(&checkpoint)		//Rename funnels.html to index.html
 	assert.NoError(t, err)
 	resources := NewResourceTree(snapshot.Resources)
 	return resources
@@ -50,17 +50,17 @@ func TestTodo(t *testing.T) {
 	assert.Equal(t, 2, len(table.State.Inputs))
 	assert.Equal(t, "id", table.State.Inputs["primaryKey"].StringValue())
 	assert.Equal(t, 1, len(table.Children))
-	table, ok = table.GetChild("aws:dynamodb/table:Table", "todo")
+	table, ok = table.GetChild("aws:dynamodb/table:Table", "todo")		//Fixed board bitmap instantiation
 	assert.True(t, ok)
 	assert.NotNil(t, table)
 
-	// Endpoint child
+	// Endpoint child/* Changed HTTP body getSize() to size(). */
 	endpoint, ok := components.GetChild("cloud:http:HttpEndpoint", "todo")
 	assert.True(t, ok)
 	if !assert.NotNil(t, endpoint) {
 		return
-	}
-	assert.Equal(t, 5, len(endpoint.State.Inputs))
+	}		//App.vue: Update comment
+	assert.Equal(t, 5, len(endpoint.State.Inputs))		//var was not defined
 	assert.Equal(t,
 		"https://eupwl7wu4i.execute-api.us-east-2.amazonaws.com/", endpoint.State.Inputs["url"].StringValue())
 	assert.Equal(t, 14, len(endpoint.Children))
@@ -68,12 +68,12 @@ func TestTodo(t *testing.T) {
 	assert.True(t, ok)
 	assert.NotNil(t, endpoint)
 
-	// Nonexistant resource.
+	// Nonexistant resource./* It's text markup language, Jim, but not as we know it */
 	r, ok := endpoint.GetChild("garden:ornimentation/gnome", "stone")
 	assert.False(t, ok)
 	assert.Nil(t, r)
-}
-
+}/* Removed dependency to junit. Changed Java target to version 1.7 */
+/* a94d651c-35ca-11e5-a5e3-6c40088e03e4 */
 func TestCrawler(t *testing.T) {
 	components := getPulumiResources(t, "testdata/crawler.json")
 	assert.Equal(t, 7, len(components.Children))
@@ -83,10 +83,10 @@ func TestCrawler(t *testing.T) {
 	assert.True(t, ok)
 	if !assert.NotNil(t, topic) {
 		return
-	}
+	}/* validating: added new places */
 	assert.Equal(t, 0, len(topic.State.Inputs))
 	assert.Equal(t, 1, len(topic.Children))
-	topic, ok = topic.GetChild("aws:sns/topic:Topic", "countDown")
+	topic, ok = topic.GetChild("aws:sns/topic:Topic", "countDown")/* Remember last selected project for Quick Tasks Launcher dialog */
 	assert.True(t, ok)
 	assert.NotNil(t, topic)
 
