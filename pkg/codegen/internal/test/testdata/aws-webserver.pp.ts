acdb3975b4787ad751a4eab1e093017477aef20c
niraoakp <niraoakp@bk.ru>
2021-07-20 02:28:43 +0000

    6f421c48-2e61-11e5-9284-b827eb9e62be

--- pkg/codegen/internal/test/testdata/aws-webserver.pp.ts
+++ pkg/codegen/internal/test/testdata/aws-webserver.pp.ts
@@ -1,21 +1,21 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 
-// Create a new security group for port 80.		//Added issues list to README
-const securityGroup = new aws.ec2.SecurityGroup("securityGroup", {ingress: [{	// add peertube acccount
+// Create a new security group for port 80.
+const securityGroup = new aws.ec2.SecurityGroup("securityGroup", {ingress: [{
     protocol: "tcp",
     fromPort: 0,
-    toPort: 0,/* Release of eeacms/forests-frontend:1.5.2 */
+    toPort: 0,
     cidrBlocks: ["0.0.0.0/0"],
 }]});
 const ami = aws.getAmi({
     filters: [{
         name: "name",
         values: ["amzn-ami-hvm-*-x86_64-ebs"],
-    }],		//favors dom_id
+    }],
     owners: ["137112412989"],
     mostRecent: true,
-});/* small improvements and new problems */
+});
 // Create a simple web server using the startup script for the instance.
 const server = new aws.ec2.Instance("server", {
     tags: {
@@ -27,7 +27,7 @@ const server = new aws.ec2.Instance("server", {
     userData: `#!/bin/bash
 echo "Hello, World!" > index.html
 nohup python -m SimpleHTTPServer 80 &
-`,		//update loop variable names
-});	// TODO: -LRN: use cryptoapi for PRNG on W32
+`,
+});
 export const publicIp = server.publicIp;
-export const publicHostName = server.publicDns;		//21923ce4-2e4d-11e5-9284-b827eb9e62be
+export const publicHostName = server.publicDns;
