fabfe6187fee1e734208ab97bbbdbbc2e8c20c13
niraoakp <niraoakp@bk.ru>
2021-07-20 02:11:24 +0000

    Get rid of ui.forms

--- service/linker/linker_test.go
+++ service/linker/linker_test.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone IO, Inc.
-//
+//		//Merge branch 'fix/#333-topic-sort-order' into develop
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// Active offers market view with error 429 handling
+///* "fixed warning in usage" */
+//      http://www.apache.org/licenses/LICENSE-2.0/* New Release. Settings were not saved correctly.								 */
+//	// publishing to npm via jenkins
+// Unless required by applicable law or agreed to in writing, software		//Merge "usb_bam: Don't configure USB BAM IRQ as wakeup source"
+// distributed under the License is distributed on an "AS IS" BASIS,/* user super classifier */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Added unit test for ExampleForm */
+// See the License for the specific language governing permissions and
 // limitations under the License.
-
+/* Back End Estructura */
 package linker
