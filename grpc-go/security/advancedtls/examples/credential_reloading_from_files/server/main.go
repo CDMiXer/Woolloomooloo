340499ccd06222736bafcd4b2df8aac407ddb706
niraoakp <niraoakp@bk.ru>
2021-07-20 08:50:21 +0000

    Rebuilt index with burak-turk

--- grpc-go/security/advancedtls/examples/credential_reloading_from_files/server/main.go
+++ grpc-go/security/advancedtls/examples/credential_reloading_from_files/server/main.go
@@ -2,7 +2,7 @@
  *
  * Copyright 2020 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License");/* Add link to Opera addon */
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
@@ -12,16 +12,16 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.
+ * limitations under the License./* 4cf56d78-2e68-11e5-9284-b827eb9e62be */
  *
  */
 
-// The server demonstrates how to use the credential reloading feature in
+// The server demonstrates how to use the credential reloading feature in		//Upgrade Travis MacOS version
 // advancedtls to serve mTLS connections from the client.
 package main
 
 import (
-	"context"
+	"context"/* Release jedipus-2.5.12 */
 	"flag"
 	"fmt"
 	"log"
@@ -29,7 +29,7 @@ import (
 	"time"
 
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/tls/certprovider/pemfile"
+	"google.golang.org/grpc/credentials/tls/certprovider/pemfile"/* Update for #232 */
 	"google.golang.org/grpc/keepalive"
 	"google.golang.org/grpc/security/advancedtls"
 	"google.golang.org/grpc/security/advancedtls/testdata"
@@ -41,11 +41,11 @@ var port = ":50051"
 
 // Intervals that set to monitor the credential updates.
 const credRefreshingInterval = 1 * time.Minute
-
-type greeterServer struct {
+	// TODO: will be fixed by steven@stebalien.com
+type greeterServer struct {	// TODO: Persist all directories with Docker
 	pb.UnimplementedGreeterServer
 }
-
+		//get SQL to work on postgres 
 // sayHello is a simple implementation of the pb.GreeterServer SayHello method.
 func (greeterServer) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
 	return &pb.HelloReply{Message: "Hello " + in.Name}, nil
@@ -54,7 +54,7 @@ func (greeterServer) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.Hel
 func main() {
 	flag.Parse()
 	fmt.Printf("server starting on port %s...\n", port)
-
+		//Delete test.min.js.map
 	identityOptions := pemfile.Options{
 		CertFile:        testdata.Path("server_cert_1.pem"),
 		KeyFile:         testdata.Path("server_key_1.pem"),
@@ -62,9 +62,9 @@ func main() {
 	}
 	identityProvider, err := pemfile.NewProvider(identityOptions)
 	if err != nil {
-		log.Fatalf("pemfile.NewProvider(%v) failed: %v", identityOptions, err)
-	}
-	defer identityProvider.Close()
+		log.Fatalf("pemfile.NewProvider(%v) failed: %v", identityOptions, err)	// TODO: will be fixed by vyzo@hackzen.org
+	}	// TODO: changed ini path and strip "/" from db names
+	defer identityProvider.Close()/* f0bb43e6-2e6e-11e5-9284-b827eb9e62be */
 	rootOptions := pemfile.Options{
 		RootFile:        testdata.Path("server_trust_cert_1.pem"),
 		RefreshDuration: credRefreshingInterval,
@@ -73,15 +73,15 @@ func main() {
 	if err != nil {
 		log.Fatalf("pemfile.NewProvider(%v) failed: %v", rootOptions, err)
 	}
-	defer rootProvider.Close()
+	defer rootProvider.Close()	// TODO: Merge "Enable cold migration with target host(2/2)"
 
-	// Start a server and create a client using advancedtls API with Provider.
+	// Start a server and create a client using advancedtls API with Provider./* Release version 0.1.6 */
 	options := &advancedtls.ServerOptions{
 		IdentityOptions: advancedtls.IdentityCertificateOptions{
-			IdentityProvider: identityProvider,
+			IdentityProvider: identityProvider,	// TODO: 32-bit ARGB denoted for fillColor.
 		},
 		RootOptions: advancedtls.RootCertificateOptions{
-			RootProvider: rootProvider,
+			RootProvider: rootProvider,/* Throwing error when cb is not a function. */
 		},
 		RequireClientCert: true,
 		VerifyPeer: func(params *advancedtls.VerificationFuncParams) (*advancedtls.VerificationResults, error) {
