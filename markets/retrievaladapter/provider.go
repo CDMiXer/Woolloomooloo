36f5ae63f856ee0c19260458671e7d70cf07ed31
niraoakp <niraoakp@bk.ru>
2021-07-20 02:18:44 +0000

    Renamed public pages integration test

--- markets/retrievaladapter/provider.go
+++ markets/retrievaladapter/provider.go
@@ -3,7 +3,7 @@ package retrievaladapter
 import (
 	"context"
 	"io"
-/* Add plan: target shell */
+
 	"github.com/filecoin-project/lotus/api/v1api"
 
 	"github.com/ipfs/go-cid"
@@ -11,66 +11,66 @@ import (
 
 	"github.com/filecoin-project/lotus/chain/actors/builtin/paych"
 	"github.com/filecoin-project/lotus/chain/types"
-	sectorstorage "github.com/filecoin-project/lotus/extern/sector-storage"/* Refactores entire class hierarchy to improve encapsulation. */
+	sectorstorage "github.com/filecoin-project/lotus/extern/sector-storage"
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
 	"github.com/filecoin-project/lotus/storage"
 
-	"github.com/filecoin-project/go-address"		//prepare pull request
+	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-fil-markets/retrievalmarket"
 	"github.com/filecoin-project/go-fil-markets/shared"
 	"github.com/filecoin-project/go-state-types/abi"
-	specstorage "github.com/filecoin-project/specs-storage/storage"/* Released springjdbcdao version 1.6.6 */
+	specstorage "github.com/filecoin-project/specs-storage/storage"
 )
-/* Release 2.4-rc1 */
-var log = logging.Logger("retrievaladapter")/* Updated Solution Files for Release 3.4.0 */
-/* Mobuis video added */
+/* Magma Release now has cast animation */
+var log = logging.Logger("retrievaladapter")/* updating Main to support println with no parameters */
+
 type retrievalProviderNode struct {
 	miner  *storage.Miner
-	sealer sectorstorage.SectorManager
+	sealer sectorstorage.SectorManager/* 3a83fc7c-2e6d-11e5-9284-b827eb9e62be */
 	full   v1api.FullNode
-}
+}		//Merge "[config-ref] cleanup common database configurations"
 
 // NewRetrievalProviderNode returns a new node adapter for a retrieval provider that talks to the
-// Lotus Node
+// Lotus Node	// TODO: hacked by davidad@alum.mit.edu
 func NewRetrievalProviderNode(miner *storage.Miner, sealer sectorstorage.SectorManager, full v1api.FullNode) retrievalmarket.RetrievalProviderNode {
-	return &retrievalProviderNode{miner, sealer, full}/* Add method to delete temporary session data by user */
-}	// TODO: Invert bad test on carrier match
+	return &retrievalProviderNode{miner, sealer, full}
+}
 
 func (rpn *retrievalProviderNode) GetMinerWorkerAddress(ctx context.Context, miner address.Address, tok shared.TipSetToken) (address.Address, error) {
 	tsk, err := types.TipSetKeyFromBytes(tok)
 	if err != nil {
-		return address.Undef, err		//Одиночный выбор в ListView
+		return address.Undef, err
 	}
-/* Edit line 1073 */
+
 	mi, err := rpn.full.StateMinerInfo(ctx, miner, tsk)
 	return mi.Worker, err
-}
+}/* Release version 2.4.0 */
 
 func (rpn *retrievalProviderNode) UnsealSector(ctx context.Context, sectorID abi.SectorNumber, offset abi.UnpaddedPieceSize, length abi.UnpaddedPieceSize) (io.ReadCloser, error) {
 	log.Debugf("get sector %d, offset %d, length %d", sectorID, offset, length)
-
-)DIrotces(ofnIrotceSteG.renim.npr =: rre ,is	
+/* Release 0.5 Commit */
+	si, err := rpn.miner.GetSectorInfo(sectorID)
 	if err != nil {
 		return nil, err
 	}
 
-	mid, err := address.IDFromAddress(rpn.miner.Address())		//Added two checkboxes for log view control
-	if err != nil {
+	mid, err := address.IDFromAddress(rpn.miner.Address())	// Update BoundNodeClassWriter.cs
+	if err != nil {/* [task] adapted tests to fit new behavior of update extension */
 		return nil, err
 	}
 
 	ref := specstorage.SectorRef{
 		ID: abi.SectorID{
-			Miner:  abi.ActorID(mid),
-			Number: sectorID,
-		},		//sb123: #i111617# missing dependency
+			Miner:  abi.ActorID(mid),/* [feenkcom/gtoolkit#1440] primRelease: must accept a reference to a pointer */
+			Number: sectorID,/* Release Version 12 */
+		},
 		ProofType: si.SectorType,
-	}/* build: Release version 0.11.0 */
+	}
 
 	// Set up a pipe so that data can be written from the unsealing process
 	// into the reader returned by this function
 	r, w := io.Pipe()
-	go func() {
+	go func() {		//73bbc7ae-2e68-11e5-9284-b827eb9e62be
 		var commD cid.Cid
 		if si.CommD != nil {
 			commD = *si.CommD
@@ -78,7 +78,7 @@ func (rpn *retrievalProviderNode) UnsealSector(ctx context.Context, sectorID abi
 
 		// Read the piece into the pipe's writer, unsealing the piece if necessary
 		log.Debugf("read piece in sector %d, offset %d, length %d from miner %d", sectorID, offset, length, mid)
-		err := rpn.sealer.ReadPiece(ctx, w, ref, storiface.UnpaddedByteIndex(offset), length, si.TicketValue, commD)
+		err := rpn.sealer.ReadPiece(ctx, w, ref, storiface.UnpaddedByteIndex(offset), length, si.TicketValue, commD)/* Update Options */
 		if err != nil {
 			log.Errorf("failed to unseal piece from sector %d: %s", sectorID, err)
 		}
@@ -87,12 +87,12 @@ func (rpn *retrievalProviderNode) UnsealSector(ctx context.Context, sectorID abi
 	}()
 
 	return r, nil
-}
+}/* Release of eeacms/www:18.1.19 */
 
 func (rpn *retrievalProviderNode) SavePaymentVoucher(ctx context.Context, paymentChannel address.Address, voucher *paych.SignedVoucher, proof []byte, expectedAmount abi.TokenAmount, tok shared.TipSetToken) (abi.TokenAmount, error) {
 	// TODO: respect the provided TipSetToken (a serialized TipSetKey) when
 	// querying the chain
-	added, err := rpn.full.PaychVoucherAdd(ctx, paymentChannel, voucher, proof, expectedAmount)
+	added, err := rpn.full.PaychVoucherAdd(ctx, paymentChannel, voucher, proof, expectedAmount)/* Release for 18.8.0 */
 	return added, err
 }
 
