fe69c3ac64f5983ddefc76897149fd5b557fd9fc
niraoakp <niraoakp@bk.ru>
2021-07-21 21:51:00 +0000

    Add actions for menu items

--- pkg/backend/display/options.go
+++ pkg/backend/display/options.go
@@ -1,26 +1,26 @@
 // Copyright 2016-2018, Pulumi Corporation.
-///* Merge "Whitespace and spelling fixes in cx.stats js and less" */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// TODO: will be fixed by arajasek94@gmail.com
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//some reason it isn't building?
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package display
 
-import (	// TODO: add link document module banners
+import (
 	"io"
-		//finish tightening up api
+
 	"github.com/pulumi/pulumi/sdk/v2/go/common/diag/colors"
 )
 
-// Type of output to display./* Released reLexer.js v0.1.1 */
+// Type of output to display.
 type Type int
 
 const (
@@ -30,25 +30,25 @@ const (
 	DisplayDiff
 	// DisplayQuery displays query output.
 	DisplayQuery
-	// DisplayQuery displays query output./* Changes in pom.xml */
-	DisplayWatch/* Release v0.11.3 */
+	// DisplayQuery displays query output.
+	DisplayWatch
 )
 
-// Options controls how the output of events are rendered		//[IMP] smartenify res_currency code
+// Options controls how the output of events are rendered
 type Options struct {
 	Color                colors.Colorization // colorization to apply to events.
-	ShowConfig           bool                // true if we should show configuration information.		//Update Windows_README.rst
-	ShowReplacementSteps bool                // true to show the replacement steps in the plan./* Initial Start Of MailMe */
+	ShowConfig           bool                // true if we should show configuration information.
+	ShowReplacementSteps bool                // true to show the replacement steps in the plan.
 	ShowSameResources    bool                // true to show the resources that aren't updated in addition to updates.
 	ShowReads            bool                // true to show resources that are being read in
 	SuppressOutputs      bool                // true to suppress output summarization, e.g. if contains sensitive info.
 	SuppressPermaLink    bool                // true to suppress state permalink
 	SummaryDiff          bool                // true if diff display should be summarized.
-	IsInteractive        bool                // true if we should display things interactively./* Release 1.4.27.974 */
+	IsInteractive        bool                // true if we should display things interactively.
 	Type                 Type                // type of display (rich diff, progress, or query).
 	JSONDisplay          bool                // true if we should emit the entire diff as JSON.
 	EventLogPath         string              // the path to the file to use for logging events, if any.
 	Debug                bool                // true to enable debug output.
-	Stdout               io.Writer           // the writer to use for stdout. Defaults to os.Stdout if unset./* Ensure all newly created BPMN elements have unique IDs */
+	Stdout               io.Writer           // the writer to use for stdout. Defaults to os.Stdout if unset.
 	Stderr               io.Writer           // the writer to use for stderr. Defaults to os.Stderr if unset.
 }
