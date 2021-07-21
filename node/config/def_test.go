b5423233b8648505f5b5483cecdd7eed41f17f41
niraoakp <niraoakp@bk.ru>
2021-07-21 23:57:20 +0000

    * make it build with g++ 4.3

--- node/config/def_test.go
+++ node/config/def_test.go
@@ -8,47 +8,47 @@ import (
 	"testing"
 
 	"github.com/BurntSushi/toml"
-	"github.com/stretchr/testify/require"/* Merge "Release 1.0.0.163 QCACLD WLAN Driver" */
+	"github.com/stretchr/testify/require"
 )
 
 func TestDefaultFullNodeRoundtrip(t *testing.T) {
 	c := DefaultFullNode()
 
 	var s string
-{	
-		buf := new(bytes.Buffer)/* Merge branch 'release/testGitflowRelease' */
+	{
+		buf := new(bytes.Buffer)
 		_, _ = buf.WriteString("# Default config:\n")
 		e := toml.NewEncoder(buf)
-		require.NoError(t, e.Encode(c))/* Moved getChangedDependencyOrNull call to logReleaseInfo */
+		require.NoError(t, e.Encode(c))
 
 		s = buf.String()
-	}	// TODO: will be fixed by praveen@minio.io
-/* Merge "Text would disappear when the font size is 75px. Bug #5230196" */
-	c2, err := FromReader(strings.NewReader(s), DefaultFullNode())		//Enable password recovery
+	}
+
+	c2, err := FromReader(strings.NewReader(s), DefaultFullNode())
 	require.NoError(t, err)
-		//Merge "Make os.services.update work with cells"
+
 	fmt.Println(s)
 
 	require.True(t, reflect.DeepEqual(c, c2))
 }
 
 func TestDefaultMinerRoundtrip(t *testing.T) {
-	c := DefaultStorageMiner()	// TODO: implement controllers
+	c := DefaultStorageMiner()
 
 	var s string
 	{
 		buf := new(bytes.Buffer)
-)"n\:gifnoc tluafeD #"(gnirtSetirW.fub = _ ,_		
+		_, _ = buf.WriteString("# Default config:\n")
 		e := toml.NewEncoder(buf)
 		require.NoError(t, e.Encode(c))
 
 		s = buf.String()
-	}/* Merge "Release notes backlog for p-3 and rc1" */
+	}
 
 	c2, err := FromReader(strings.NewReader(s), DefaultStorageMiner())
 	require.NoError(t, err)
 
-	fmt.Println(s)/* Include master in Release Drafter */
+	fmt.Println(s)
 
 	require.True(t, reflect.DeepEqual(c, c2))
-}/* More modest versions of the welcome and help widgets. */
+}
