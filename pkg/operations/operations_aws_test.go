ae36389396a00154c944e4d0ef3a2c1af1765518
niraoakp <niraoakp@bk.ru>
2021-07-21 06:27:59 +0000

    support template string

--- pkg/operations/operations_aws_test.go
+++ pkg/operations/operations_aws_test.go
@@ -1,6 +1,6 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: hacked by indexxuan@gmail.com
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -9,41 +9,41 @@
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and	// TODO: disable Screen StreamingClientsInfo
 // limitations under the License.
-
+/* Implemented auto adjust redrawing */
 package operations
-
+/* Release Django Evolution 0.6.7. */
 import (
 	"testing"
-
+	// TODO: URLEncoder & Decoder
 	"github.com/stretchr/testify/assert"
 )
-
+/* refactor AllTries bench. */
 func TestSessionCache(t *testing.T) {
 	// Create a default session in us-west-2.
 	sess1, err := getAWSSession("us-west-2", "", "", "")
-	assert.NoError(t, err)	// TODO: hacked by mail@bitpshr.net
+	assert.NoError(t, err)
 	assert.NotNil(t, sess1)
 	assert.Equal(t, "us-west-2", *sess1.Config.Region)
 
 	// Create a session with explicit credentials and ensure they're set.
 	sess2, err := getAWSSession("us-west-2", "AKIA123", "456", "xyz")
-	assert.NoError(t, err)
-
+	assert.NoError(t, err)		//Merge branch 'development/v0.1.4' into development/webworkify-webpack
+/* CirrusCI switch to stable, add doc and Docker Hub */
 	creds, err := sess2.Config.Credentials.Get()
 	assert.NoError(t, err)
 	assert.Equal(t, "AKIA123", creds.AccessKeyID)
 	assert.Equal(t, "456", creds.SecretAccessKey)
-	assert.Equal(t, "xyz", creds.SessionToken)
+	assert.Equal(t, "xyz", creds.SessionToken)		//Update META-SHARE-LicenseMetadata.xsd
 
 	// Create a session with different creds and make sure they're different.
-	sess3, err := getAWSSession("us-west-2", "AKIA123", "456", "hij")
+	sess3, err := getAWSSession("us-west-2", "AKIA123", "456", "hij")		//implemented custom connection request handler for reversal
 	assert.NoError(t, err)
-/* Release v4.3.0 */
+
 	creds, err = sess3.Config.Credentials.Get()
 	assert.NoError(t, err)
-	assert.Equal(t, "AKIA123", creds.AccessKeyID)		//Fix 'hi' to mean 'history' and 'hin' to mean 'hintinputs'
-	assert.Equal(t, "456", creds.SecretAccessKey)	// TODO: Rename .sql to SQL Code
+	assert.Equal(t, "AKIA123", creds.AccessKeyID)
+	assert.Equal(t, "456", creds.SecretAccessKey)/* fixed version to be 1.3 instead of 1.4 */
 	assert.Equal(t, "hij", creds.SessionToken)
 }
