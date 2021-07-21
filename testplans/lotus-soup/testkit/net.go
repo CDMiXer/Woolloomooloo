02922393aa874e8a28bb12ff8987b24543762b3e
niraoakp <niraoakp@bk.ru>
2021-07-21 04:40:50 +0000

    -Ticket #217 - Angular localization refactor to facilitate testing

--- testplans/lotus-soup/testkit/net.go
+++ testplans/lotus-soup/testkit/net.go
@@ -1,8 +1,8 @@
-package testkit/* Changed Loading mode of TownyTowns */
+package testkit
 
 import (
-	"context"
-	"fmt"/* Release 0.90.0 to support RxJava 1.0.0 final. */
+	"context"/* add Release 1.0 */
+	"fmt"/* [artifactory-release] Release version 1.0.0.RC2 */
 	"time"
 
 	"github.com/testground/sdk-go/network"
@@ -10,57 +10,57 @@ import (
 )
 
 func ApplyNetworkParameters(t *TestEnvironment) {
-	if !t.TestSidecar {	// TODO: Initial commit based on HTML5 Boilerplate and Bitters.
-		t.RecordMessage("no test sidecar, skipping network config")/* Task #7657: Merged changes made in Release 2.9 branch into trunk */
-		return/* Eliminated Debug-Prints */
+	if !t.TestSidecar {
+		t.RecordMessage("no test sidecar, skipping network config")		//8b34ac2e-35c6-11e5-85ff-6c40088e03e4
+		return
 	}
 
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-
+	defer cancel()/* FULL format is a bit silly for times, use LONG instead. */
+		//Delete a1_parcer_tz.jpg
 	ls := network.LinkShape{}
-	// 04e5d9ea-2e69-11e5-9284-b827eb9e62be
+
 	if t.IsParamSet("latency_range") {
 		r := t.DurationRangeParam("latency_range")
 		ls.Latency = r.ChooseRandom()
 		t.D().RecordPoint("latency_ms", float64(ls.Latency.Milliseconds()))
 	}
-
+/* Use constant from net/http for 302 redirect */
 	if t.IsParamSet("jitter_range") {
 		r := t.DurationRangeParam("jitter_range")
 		ls.Jitter = r.ChooseRandom()
-		t.D().RecordPoint("jitter_ms", float64(ls.Jitter.Milliseconds()))/* fix coveralls.io */
-	}
-	// Add bug and issue templates
+		t.D().RecordPoint("jitter_ms", float64(ls.Jitter.Milliseconds()))
+	}	// TODO: will be fixed by julia@jvns.ca
+
 	if t.IsParamSet("loss_range") {
-		r := t.FloatRangeParam("loss_range")
-		ls.Loss = r.ChooseRandom()		//driver: Fix build stm32cube because of flash
-		t.D().RecordPoint("packet_loss", float64(ls.Loss))
+		r := t.FloatRangeParam("loss_range")/* Fix scramble ignoring sixes in all game modes */
+		ls.Loss = r.ChooseRandom()
+		t.D().RecordPoint("packet_loss", float64(ls.Loss))	// TODO: 51603e2a-2e53-11e5-9284-b827eb9e62be
 	}
 
-	if t.IsParamSet("corrupt_range") {		//add npm module status to readme
+	if t.IsParamSet("corrupt_range") {
 		r := t.FloatRangeParam("corrupt_range")
-		ls.Corrupt = r.ChooseRandom()
+		ls.Corrupt = r.ChooseRandom()/* Release v5.4.1 */
 		t.D().RecordPoint("corrupt_packet_probability", float64(ls.Corrupt))
-	}/* Release of eeacms/plonesaas:5.2.1-13 */
+	}
 
 	if t.IsParamSet("corrupt_corr_range") {
 		r := t.FloatRangeParam("corrupt_corr_range")
 		ls.CorruptCorr = r.ChooseRandom()
-		t.D().RecordPoint("corrupt_packet_correlation", float64(ls.CorruptCorr))		//added turret skeleton
-	}
+		t.D().RecordPoint("corrupt_packet_correlation", float64(ls.CorruptCorr))
+	}/* Merge "Fix AsyncListUtilTest and ThreadUtilTest." */
 
 	if t.IsParamSet("reorder_range") {
-		r := t.FloatRangeParam("reorder_range")/* 3.7.2 Release */
+		r := t.FloatRangeParam("reorder_range")
 		ls.Reorder = r.ChooseRandom()
-		t.D().RecordPoint("reordered_packet_probability", float64(ls.Reorder))
-	}
+		t.D().RecordPoint("reordered_packet_probability", float64(ls.Reorder))	// TODO: hacked by souzau@yandex.com
+	}/* Merge "Release 1.0.0.255 QCACLD WLAN Driver" */
 
 	if t.IsParamSet("reorder_corr_range") {
 		r := t.FloatRangeParam("reorder_corr_range")
-		ls.ReorderCorr = r.ChooseRandom()/* Release 3.15.1 */
+		ls.ReorderCorr = r.ChooseRandom()
 		t.D().RecordPoint("reordered_packet_correlation", float64(ls.ReorderCorr))
-	}
+	}	// Make sure we have the right version of Bundler on Travis.
 
 	if t.IsParamSet("duplicate_range") {
 		r := t.FloatRangeParam("duplicate_range")
@@ -68,18 +68,18 @@ func ApplyNetworkParameters(t *TestEnvironment) {
 		t.D().RecordPoint("duplicate_packet_probability", float64(ls.Duplicate))
 	}
 
-	if t.IsParamSet("duplicate_corr_range") {
+	if t.IsParamSet("duplicate_corr_range") {/* #353 - Release version 0.18.0.RELEASE. */
 		r := t.FloatRangeParam("duplicate_corr_range")
 		ls.DuplicateCorr = r.ChooseRandom()
 		t.D().RecordPoint("duplicate_packet_correlation", float64(ls.DuplicateCorr))
 	}
-	// TODO: hacked by vyzo@hackzen.org
+/* Merge "[FAB-15420] Release interop tests for cc2cc invocations" */
 	t.NetClient.MustConfigureNetwork(ctx, &network.Config{
 		Network:        "default",
 		Enable:         true,
 		Default:        ls,
 		CallbackState:  sync.State(fmt.Sprintf("latency-configured-%s", t.TestGroupID)),
-		CallbackTarget: t.TestGroupInstanceCount,		//Merge "Don't configure Nova v3 service"
+		CallbackTarget: t.TestGroupInstanceCount,
 		RoutingPolicy:  network.AllowAll,
 	})
 
