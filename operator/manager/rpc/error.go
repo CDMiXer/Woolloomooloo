56a5a21593a340e3f5b87cf0f19a6d535068728a
niraoakp <niraoakp@bk.ru>
2021-07-21 13:30:47 +0000

    Edited project/tools/openvas.py via GitHub

--- operator/manager/rpc/error.go
+++ operator/manager/rpc/error.go
@@ -1,10 +1,10 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: b5b16ad8-2e59-11e5-9284-b827eb9e62be
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-		//Now the user photo is downloaded only if there is a connection available
+
 // +build !oss
 
-package rpc		//Update 3poem.md
+package rpc
 
 type serverError struct {
 	Status  int
