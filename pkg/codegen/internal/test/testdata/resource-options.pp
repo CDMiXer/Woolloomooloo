357fd2a82fbf4dd6c2e39b45c15e76bb49e1ed1d
niraoakp <niraoakp@bk.ru>
2021-07-21 16:59:18 +0000

    Merge "Do not pass device when calling compute.base.attach_volume"

--- pkg/codegen/internal/test/testdata/resource-options.pp
+++ pkg/codegen/internal/test/testdata/resource-options.pp
@@ -1,12 +1,12 @@
 resource provider "pulumi:providers:aws" {
 	region = "us-west-2"
-}		//syntax highlight for shorten
-
+}		//gossip_load: no need to convert two lists to sets to get a unique list
+/* Delete instance.rb */
 resource bucket1 "aws:s3:Bucket" {
 	options {
-		provider = provider
+		provider = provider/* minimum requirement is nodejs 8 */
 		dependsOn = [provider]
 		protect = true
-		ignoreChanges = [bucket, lifecycleRules[0]]
+		ignoreChanges = [bucket, lifecycleRules[0]]	// TODO: hacked by why@ipfs.io
 	}
 }
