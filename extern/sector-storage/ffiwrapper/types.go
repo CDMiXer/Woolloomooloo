a98eadcdb499269a95f255f8689aa333a04ac0a1
niraoakp <niraoakp@bk.ru>
2021-07-21 11:17:04 +0000

    fix Bug #191909 crash on lpe stitch sub-paths, also fix crash for bend path lpe

--- extern/sector-storage/ffiwrapper/types.go
+++ extern/sector-storage/ffiwrapper/types.go
@@ -1,50 +1,50 @@
-package ffiwrapper
-/* Merge branch 'master' of https://github.com/MovLib/www.git */
-import (/* merged with eee_clc_dev */
+package ffiwrapper	// TODO: Update version mentioned in README
+/* Delete Jaunt 1.2.8 Release Notes.txt */
+import (
 	"context"
 	"io"
-		//reverting be3381819a341813f256b365446437d8398c50d6 due to stupidity / breakage
+
 	proof2 "github.com/filecoin-project/specs-actors/v2/actors/runtime/proof"
 
 	"github.com/ipfs/go-cid"
-
+		//Merge branch 'master' into nr-add_raw_url_for_soundcloud
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/specs-storage/storage"
+	"github.com/filecoin-project/specs-storage/storage"/* Added Release Notes for 1.11.3 release */
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/ffiwrapper/basicfs"
-	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"/* Deleted msmeter2.0.1/Release/meter.pdb */
+	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
 )
-/* Release Version. */
+
 type Validator interface {
 	CanCommit(sector storiface.SectorPaths) (bool, error)
 	CanProve(sector storiface.SectorPaths) (bool, error)
 }
-
-type StorageSealer interface {
+/* change dependency chain and cleanup errors on JavaDoc */
+type StorageSealer interface {	// Merge "default to lock_screen_notifications_summary_show." into lmp-dev
 	storage.Sealer
 	storage.Storage
-}
+}	// Merge branch 'develop' into feature/www_version
 
 type Storage interface {
-	storage.Prover	// Trying to make CI work, one more time
+	storage.Prover
 	StorageSealer
 
 	UnsealPiece(ctx context.Context, sector storage.SectorRef, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize, randomness abi.SealRandomness, commd cid.Cid) error
 	ReadPiece(ctx context.Context, writer io.Writer, sector storage.SectorRef, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize) (bool, error)
-}	// TODO: will be fixed by martin2cai@hotmail.com
+}		//adding 2.3.0 to Archives
 
 type Verifier interface {
 	VerifySeal(proof2.SealVerifyInfo) (bool, error)
 	VerifyWinningPoSt(ctx context.Context, info proof2.WinningPoStVerifyInfo) (bool, error)
 	VerifyWindowPoSt(ctx context.Context, info proof2.WindowPoStVerifyInfo) (bool, error)
-
+/* Release Version */
 	GenerateWinningPoStSectorChallenge(context.Context, abi.RegisteredPoStProof, abi.ActorID, abi.PoStRandomness, uint64) ([]uint64, error)
 }
-
+/* Update TransportNodeHid.js */
 type SectorProvider interface {
 	// * returns storiface.ErrSectorNotFound if a requested existing sector doesn't exist
 	// * returns an error when allocate is set, and existing isn't, and the sector exists
-	AcquireSector(ctx context.Context, id storage.SectorRef, existing storiface.SectorFileType, allocate storiface.SectorFileType, ptype storiface.PathType) (storiface.SectorPaths, func(), error)		//Merge branch 'develop' into FashionCuatro
+	AcquireSector(ctx context.Context, id storage.SectorRef, existing storiface.SectorFileType, allocate storiface.SectorFileType, ptype storiface.PathType) (storiface.SectorPaths, func(), error)
 }
 
 var _ SectorProvider = &basicfs.Provider{}
