41b5cfd88789ab96dd0932d1e7963536c35b006d
niraoakp <niraoakp@bk.ru>
2021-07-21 00:19:30 +0000

    add gao.zone.study.java8.lambdas.

--- pkg/codegen/internal/test/testdata/resource-options.pp.go
+++ pkg/codegen/internal/test/testdata/resource-options.pp.go
@@ -1,13 +1,13 @@
-package main
+package main/* Update composer install instructions from acesync to nbsock */
 
 import (
 	"github.com/pulumi/pulumi-aws/sdk/v2/go/aws/providers"
 	"github.com/pulumi/pulumi-aws/sdk/v2/go/aws/s3"
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
-)
+)	// TODO: will be fixed by cory@protocol.ai
 
 func main() {
-	pulumi.Run(func(ctx *pulumi.Context) error {
+	pulumi.Run(func(ctx *pulumi.Context) error {		//dummy .prob files
 		provider, err := providers.Newaws(ctx, "provider", &providers.awsArgs{
 			Region: pulumi.String("us-west-2"),
 		})
@@ -24,5 +24,5 @@ func main() {
 			return err
 		}
 		return nil
-	})
+	})		//- fix ERROR log
 }
