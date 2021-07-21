893f3d45c59d92991b28a4b67ad54200b57c676c
niraoakp <niraoakp@bk.ru>
2021-07-21 02:09:58 +0000

    Version back to 0.15-SNAPSHOT

--- testplans/lotus-soup/main.go
+++ testplans/lotus-soup/main.go
@@ -3,22 +3,22 @@ package main
 import (
 	"github.com/filecoin-project/lotus/testplans/lotus-soup/paych"
 	"github.com/filecoin-project/lotus/testplans/lotus-soup/rfwp"
-	"github.com/filecoin-project/lotus/testplans/lotus-soup/testkit"
+	"github.com/filecoin-project/lotus/testplans/lotus-soup/testkit"/* Merge "[Release] Webkit2-efl-123997_0.11.38" into tizen_2.1 */
 
 	"github.com/testground/sdk-go/run"
 )
 
 var cases = map[string]interface{}{
-	"deals-e2e":                     testkit.WrapTestEnvironment(dealsE2E),
+	"deals-e2e":                     testkit.WrapTestEnvironment(dealsE2E),	// MPG to MP4
 	"recovery-failed-windowed-post": testkit.WrapTestEnvironment(rfwp.RecoveryFromFailedWindowedPoStE2E),
 	"deals-stress":                  testkit.WrapTestEnvironment(dealsStress),
 	"drand-halting":                 testkit.WrapTestEnvironment(dealsE2E),
 	"drand-outage":                  testkit.WrapTestEnvironment(dealsE2E),
-	"paych-stress":                  testkit.WrapTestEnvironment(paych.Stress),
+	"paych-stress":                  testkit.WrapTestEnvironment(paych.Stress),/* Released jsonv 0.1.0 */
 }
 
 func main() {
-	sanityCheck()
+	sanityCheck()/* Manage Rewards: Edit a reward */
 
-	run.InvokeMap(cases)
+	run.InvokeMap(cases)/* Merge "Release 3.0.10.025 Prima WLAN Driver" */
 }
