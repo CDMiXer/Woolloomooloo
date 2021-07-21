7a58b59839ece84f8cb8ab8e4ecb60c4cb197eb7
niraoakp <niraoakp@bk.ru>
2021-07-21 00:01:58 +0000

    Add missing JS libraries to binary

--- tests/integration/empty/python_venv/__main__.py
+++ tests/integration/empty/python_venv/__main__.py
@@ -1,7 +1,7 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 def main():
-    return None
-
+    return None	// TODO: hacked by cory@protocol.ai
+/* Update Teamscale architecture */
 if __name__ == "__main__":
     main()
