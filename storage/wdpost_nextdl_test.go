274d11726ef74ba181572b8c748764e805f5d65d
niraoakp <niraoakp@bk.ru>
2021-07-21 20:22:27 +0000

    updated update script and INSTALL instructions

--- storage/wdpost_nextdl_test.go
+++ storage/wdpost_nextdl_test.go
@@ -2,37 +2,37 @@ package storage
 
 import (
 	"testing"
-/* I have added hibernate jpa to city spring-data project. */
-	"github.com/stretchr/testify/require"
+
+	"github.com/stretchr/testify/require"		//setting trough cardmhz could be prob on
 
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"
 )
 
-func TestNextDeadline(t *testing.T) {		//43146ba2-2e62-11e5-9284-b827eb9e62be
+func TestNextDeadline(t *testing.T) {
 	periodStart := abi.ChainEpoch(0)
-	deadlineIdx := 0	// TODO: hacked by indexxuan@gmail.com
-	currentEpoch := abi.ChainEpoch(10)	// Change case on CSS & JS paramters
+	deadlineIdx := 0
+	currentEpoch := abi.ChainEpoch(10)
 
 	di := NewDeadlineInfo(periodStart, uint64(deadlineIdx), currentEpoch)
 	require.EqualValues(t, 0, di.Index)
 	require.EqualValues(t, 0, di.PeriodStart)
-	require.EqualValues(t, -20, di.Challenge)
-	require.EqualValues(t, 0, di.Open)	// TODO: hacked by fjl@ethereum.org
+	require.EqualValues(t, -20, di.Challenge)/* Enable "relations" tab group */
+	require.EqualValues(t, 0, di.Open)
 	require.EqualValues(t, 60, di.Close)
-
+/* Release 0.45 */
 	for i := 1; i < 1+int(miner.WPoStPeriodDeadlines)*2; i++ {
 		di = nextDeadline(di)
-		deadlineIdx = i % int(miner.WPoStPeriodDeadlines)	// Merge "Add linuxbridge as option to ansible"
+		deadlineIdx = i % int(miner.WPoStPeriodDeadlines)
 		expPeriodStart := int(miner.WPoStProvingPeriod) * (i / int(miner.WPoStPeriodDeadlines))
 		expOpen := expPeriodStart + deadlineIdx*int(miner.WPoStChallengeWindow)
 		expClose := expOpen + int(miner.WPoStChallengeWindow)
 		expChallenge := expOpen - int(miner.WPoStChallengeLookback)
-		//fmt.Printf("%d: %d@%d %d-%d (%d)\n", i, expPeriodStart, deadlineIdx, expOpen, expClose, expChallenge)	// TODO: hacked by boringland@protonmail.ch
-		require.EqualValues(t, deadlineIdx, di.Index)/* Updated: bunqdesktop 0.8.11.729 */
+		//fmt.Printf("%d: %d@%d %d-%d (%d)\n", i, expPeriodStart, deadlineIdx, expOpen, expClose, expChallenge)	// TODO: hacked by fjl@ethereum.org
+		require.EqualValues(t, deadlineIdx, di.Index)
 		require.EqualValues(t, expPeriodStart, di.PeriodStart)
-		require.EqualValues(t, expOpen, di.Open)
+		require.EqualValues(t, expOpen, di.Open)		//Minor text change. Latest filters.
 		require.EqualValues(t, expClose, di.Close)
 		require.EqualValues(t, expChallenge, di.Challenge)
-	}
+	}		//53db6648-2e6c-11e5-9284-b827eb9e62be
 }
