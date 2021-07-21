c216883479152f3ff05589b2d43c3048d2c4b546
niraoakp <niraoakp@bk.ru>
2021-07-21 15:44:44 +0000

    Merge branch 'master' into traffic_ramp

--- tests/integration/construct_component/python/__main__.py
+++ tests/integration/construct_component/python/__main__.py
@@ -1,7 +1,7 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-	// Create lettura_gpinna_Immaterials.md
+
 from component import Component
-		//Supression d'imports inutiles.
+
 component_a = Component("a", echo=42)
 component_b = Component("b", echo=component_a.echo)
-component_c = Component("c", echo=component_a.childId)/* Adding Release 2 */
+component_c = Component("c", echo=component_a.childId)
