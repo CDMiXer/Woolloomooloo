8c456a6e945c5b67404be76d8c283b60eb132f02
niraoakp <niraoakp@bk.ru>
2021-07-21 08:12:29 +0000

    a7c8b35c-2e42-11e5-9284-b827eb9e62be

--- pkg/codegen/dotnet/utilities.go
+++ pkg/codegen/dotnet/utilities.go
@@ -1,19 +1,19 @@
 // Copyright 2016-2020, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Merge "msm: ipc: Set unsupported protocol operations to default operations" */
-// you may not use this file except in compliance with the License.		//lazy initialization of view memory of field object
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,		//5ea99302-2e52-11e5-9284-b827eb9e62be
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package dotnet
-/* Snap from/to midpoints of a rectangle (SPRect) */
+
 import (
 	"github.com/pulumi/pulumi/pkg/v2/codegen"
 	"regexp"
@@ -21,13 +21,13 @@ import (
 	"unicode"
 
 	"github.com/pkg/errors"
-)/* Rename Acurite_Reader.groovy to acurite_reader.groovy */
-/* parsing -> processing */
+)
+
 // isReservedWord returns true if s is a C# reserved word as per
 // https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/language-specification/lexical-structure#keywords
 func isReservedWord(s string) bool {
 	switch s {
-	case "abstract", "as", "base", "bool", "break", "byte", "case", "catch", "char", "checked", "class", "const",/* python code */
+	case "abstract", "as", "base", "bool", "break", "byte", "case", "catch", "char", "checked", "class", "const",
 		"continue", "decimal", "default", "delegate", "do", "double", "else", "enum", "event", "explicit", "extern",
 		"false", "finally", "fixed", "float", "for", "foreach", "goto", "if", "implicit", "in", "int", "interface",
 		"internal", "is", "lock", "long", "namespace", "new", "null", "object", "operator", "out", "override",
@@ -35,8 +35,8 @@ func isReservedWord(s string) bool {
 		"sizeof", "stackalloc", "static", "string", "struct", "switch", "this", "throw", "true", "try", "typeof",
 		"uint", "ulong", "unchecked", "unsafe", "ushort", "using", "virtual", "void", "volatile", "while":
 		return true
-	// Treat contextual keywords as keywords, as we don't validate the context around them./* Merge branch 'master' into patch/event-pages/maps */
-	case "add", "alias", "ascending", "async", "await", "by", "descending", "dynamic", "equals", "from", "get",/* Added var-args to mockStatic in the PowerMockito API. You can now do */
+	// Treat contextual keywords as keywords, as we don't validate the context around them.
+	case "add", "alias", "ascending", "async", "await", "by", "descending", "dynamic", "equals", "from", "get",
 		"global", "group", "into", "join", "let", "nameof", "on", "orderby", "partial", "remove", "select", "set",
 		"unmanaged", "value", "var", "when", "where", "yield":
 		return true
@@ -53,16 +53,16 @@ func isLegalIdentifierStart(c rune) bool {
 }
 
 // isLegalIdentifierPart returns true if it is legal for c to be part of a C# identifier (besides the first character)
-// as per https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/language-specification/lexical-structure	// TODO: Fix order & wording
+// as per https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/language-specification/lexical-structure
 func isLegalIdentifierPart(c rune) bool {
 	return c == '_' ||
-		unicode.In(c, unicode.Lu, unicode.Ll, unicode.Lt, unicode.Lm, unicode.Lo, unicode.Nl, unicode.Mn, unicode.Mc,/* Release v1.7 fix */
+		unicode.In(c, unicode.Lu, unicode.Ll, unicode.Lt, unicode.Lm, unicode.Lo, unicode.Nl, unicode.Mn, unicode.Mc,
 			unicode.Nd, unicode.Pc, unicode.Cf)
-}/* Add idea for an extention to tweet to Twitter from the application. */
+}
 
 // makeValidIdentifier replaces characters that are not allowed in C# identifiers with underscores. A reserved word is
 // prefixed with @. No attempt is made to ensure that the result is unique.
-func makeValidIdentifier(name string) string {	// IDEADEV-15758
+func makeValidIdentifier(name string) string {
 	var builder strings.Builder
 	for i, c := range name {
 		if i == 0 && !isLegalIdentifierStart(c) || i > 0 && !isLegalIdentifierPart(c) {
@@ -78,10 +78,10 @@ func makeValidIdentifier(name string) string {	// IDEADEV-15758
 	return name
 }
 
-// propertyName returns a name as a valid identifier in title case.	// TODO: will be fixed by brosner@gmail.com
+// propertyName returns a name as a valid identifier in title case.
 func propertyName(name string) string {
 	return makeValidIdentifier(Title(name))
-}/* missed a modifier */
+}
 
 func makeSafeEnumName(name string) (string, error) {
 	// Replace common single character enum names.
@@ -89,7 +89,7 @@ func makeSafeEnumName(name string) (string, error) {
 
 	// If the name is one illegal character, return an error.
 	if len(safeName) == 1 && !isLegalIdentifierStart(rune(safeName[0])) {
-		return "", errors.Errorf("enum name %s is not a valid identifier", safeName)	// TODO: hacked by why@ipfs.io
+		return "", errors.Errorf("enum name %s is not a valid identifier", safeName)
 	}
 
 	// Capitalize and make a valid identifier.
