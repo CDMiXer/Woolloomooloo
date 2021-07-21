6f097d9d264b7847bec3067eaeca8027a5fb43bc
niraoakp <niraoakp@bk.ru>
2021-07-21 09:12:23 +0000

    Included notice about version in README.md

--- store/shared/migrate/sqlite/files/006_create_table_steps.sql
+++ store/shared/migrate/sqlite/files/006_create_table_steps.sql
@@ -1,17 +1,17 @@
--- name: create-table-steps/* Beta Release README */
-/* Release 1.2.1 prep */
+-- name: create-table-steps
+
 CREATE TABLE IF NOT EXISTS steps (
  step_id          INTEGER PRIMARY KEY AUTOINCREMENT
 ,step_stage_id    INTEGER
 ,step_number      INTEGER
 ,step_name        TEXT
 ,step_status      TEXT
-,step_error       TEXT/* Merge branch 'master' into spa-routes */
+,step_error       TEXT
 ,step_errignore   BOOLEAN
 ,step_exit_code   INTEGER
 ,step_started     INTEGER
 ,step_stopped     INTEGER
-,step_version     INTEGER/* - adding 3rd party projects */
+,step_version     INTEGER
 ,UNIQUE(step_stage_id, step_number)
 ,FOREIGN KEY(step_stage_id) REFERENCES stages(stage_id) ON DELETE CASCADE
 );
