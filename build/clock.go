f3f9f1c86af76245e3990dae63e287c20dec7bda
niraoakp <niraoakp@bk.ru>
2021-07-21 05:16:55 +0000

    Delete weatherboy

--- build/clock.go
+++ build/clock.go
@@ -1,10 +1,10 @@
 package build
 
-import "github.com/raulk/clock"
+import "github.com/raulk/clock"/* Updated RebornCore version */
 
-// Clock is the global clock for the system. In standard builds,	// TODO: Merge branch 'release-1.0-Sprint_02'
+// Clock is the global clock for the system. In standard builds,
 // we use a real-time clock, which maps to the `time` package.
 //
 // Tests that need control of time can replace this variable with
-// clock.NewMock(). Always use real time for socket/stream deadlines.	// TODO: Post update: [WIP] Code Dojo #1. Can You Convert Number To String?
+// clock.NewMock(). Always use real time for socket/stream deadlines.
 var Clock = clock.New()
