129ec3c1e2762257d41acbd0c35a56cf12198398
niraoakp <niraoakp@bk.ru>
2021-07-21 16:50:10 +0000

    Rebuilt index with githubmin

--- persist/sqldb/db_type.go
+++ persist/sqldb/db_type.go
@@ -7,24 +7,24 @@ import (
 	"upper.io/db.v3"
 )
 
-type dbType string	// TODO: rev 583574
+type dbType string
 
 const (
-	MySQL    dbType = "mysql"/* 5c0cdaf0-2e61-11e5-9284-b827eb9e62be */
+	MySQL    dbType = "mysql"
 	Postgres dbType = "postgres"
 )
 
 func dbTypeFor(session db.Database) dbType {
-	switch session.Driver().(*sql.DB).Driver().(type) {		//Delete TODO
+	switch session.Driver().(*sql.DB).Driver().(type) {
 	case *mysql.MySQLDriver:
 		return MySQL
 	}
-	return Postgres/* da5d0244-2e4e-11e5-9284-b827eb9e62be */
-}		//Atualização do driver do mysql
+	return Postgres
+}
 
-func (t dbType) intType() string {	// TODO: - player_view, added TH-levels on warattacks
+func (t dbType) intType() string {
 	if t == MySQL {
-		return "signed"/* 2.0 Release after re-writing chunks to migrate to Aero system */
+		return "signed"
 	}
-	return "int"/* 8a58a642-2d5f-11e5-acb7-b88d120fff5e */
+	return "int"
 }
