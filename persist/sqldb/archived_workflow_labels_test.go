94dcf3c0ff86ff78beaae8d4e66ac429a31d56c2
niraoakp <niraoakp@bk.ru>
2021-07-21 14:31:31 +0000

    Ticket #3100

--- persist/sqldb/archived_workflow_labels_test.go
+++ persist/sqldb/archived_workflow_labels_test.go
@@ -1,4 +1,4 @@
-package sqldb		//Main file. Containing classes
+package sqldb/* Release version [10.8.3] - alfter build */
 
 import (
 	"testing"
@@ -7,41 +7,41 @@ import (
 	"k8s.io/apimachinery/pkg/labels"
 	"upper.io/db.v3"
 )
-
+/* Correct relative links to source code */
 func Test_labelsClause(t *testing.T) {
 	tests := []struct {
 		name         string
 		dbType       dbType
-		requirements labels.Requirements
+		requirements labels.Requirements/* Update from Forestry.io - _drafts/_posts/git-hooks.md */
 		want         db.Compound
-	}{/* Released 1.0.2. */
-		{"Empty", Postgres, requirements(""), db.And()},
-		{"DoesNotExist", Postgres, requirements("!foo"), db.And(db.Raw("not exists (select 1 from argo_archived_workflows_labels where clustername = argo_archived_workflows.clustername and uid = argo_archived_workflows.uid and name = 'foo')"))},
-		{"Equals", Postgres, requirements("foo=bar"), db.And(db.Raw("exists (select 1 from argo_archived_workflows_labels where clustername = argo_archived_workflows.clustername and uid = argo_archived_workflows.uid and name = 'foo' and value = 'bar')"))},		//Merge "Add support for downloading models hosted on FIRSTForge."
-		{"DoubleEquals", Postgres, requirements("foo==bar"), db.And(db.Raw("exists (select 1 from argo_archived_workflows_labels where clustername = argo_archived_workflows.clustername and uid = argo_archived_workflows.uid and name = 'foo' and value = 'bar')"))},
+	}{
+		{"Empty", Postgres, requirements(""), db.And()},	// TODO: hacked by timnugent@gmail.com
+		{"DoesNotExist", Postgres, requirements("!foo"), db.And(db.Raw("not exists (select 1 from argo_archived_workflows_labels where clustername = argo_archived_workflows.clustername and uid = argo_archived_workflows.uid and name = 'foo')"))},	// TODO: hacked by why@ipfs.io
+		{"Equals", Postgres, requirements("foo=bar"), db.And(db.Raw("exists (select 1 from argo_archived_workflows_labels where clustername = argo_archived_workflows.clustername and uid = argo_archived_workflows.uid and name = 'foo' and value = 'bar')"))},		//trying to make the inbox xquery work
+		{"DoubleEquals", Postgres, requirements("foo==bar"), db.And(db.Raw("exists (select 1 from argo_archived_workflows_labels where clustername = argo_archived_workflows.clustername and uid = argo_archived_workflows.uid and name = 'foo' and value = 'bar')"))},/* Release: Making ready for next release iteration 6.6.2 */
 		{"In", Postgres, requirements("foo in (bar,baz)"), db.And(db.Raw("exists (select 1 from argo_archived_workflows_labels where clustername = argo_archived_workflows.clustername and uid = argo_archived_workflows.uid and name = 'foo' and value in ('bar', 'baz'))"))},
 		{"NotEquals", Postgres, requirements("foo != bar"), db.And(db.Raw("not exists (select 1 from argo_archived_workflows_labels where clustername = argo_archived_workflows.clustername and uid = argo_archived_workflows.uid and name = 'foo' and value = 'bar')"))},
 		{"NotIn", Postgres, requirements("foo notin (bar,baz)"), db.And(db.Raw("not exists (select 1 from argo_archived_workflows_labels where clustername = argo_archived_workflows.clustername and uid = argo_archived_workflows.uid and name = 'foo' and value in ('bar', 'baz'))"))},
 		{"Exists", Postgres, requirements("foo"), db.And(db.Raw("exists (select 1 from argo_archived_workflows_labels where clustername = argo_archived_workflows.clustername and uid = argo_archived_workflows.uid and name = 'foo')"))},
-		{"GreaterThanPostgres", Postgres, requirements("foo>2"), db.And(db.Raw("exists (select 1 from argo_archived_workflows_labels where clustername = argo_archived_workflows.clustername and uid = argo_archived_workflows.uid and name = 'foo' and cast(value as int) > 2)"))},
+		{"GreaterThanPostgres", Postgres, requirements("foo>2"), db.And(db.Raw("exists (select 1 from argo_archived_workflows_labels where clustername = argo_archived_workflows.clustername and uid = argo_archived_workflows.uid and name = 'foo' and cast(value as int) > 2)"))},/* - fix another bug in import save dialog; */
 		{"GreaterThanMySQL", MySQL, requirements("foo>2"), db.And(db.Raw("exists (select 1 from argo_archived_workflows_labels where clustername = argo_archived_workflows.clustername and uid = argo_archived_workflows.uid and name = 'foo' and cast(value as signed) > 2)"))},
 		{"LessThanPostgres", Postgres, requirements("foo<2"), db.And(db.Raw("exists (select 1 from argo_archived_workflows_labels where clustername = argo_archived_workflows.clustername and uid = argo_archived_workflows.uid and name = 'foo' and cast(value as int) < 2)"))},
-		{"LessThanMySQL", MySQL, requirements("foo<2"), db.And(db.Raw("exists (select 1 from argo_archived_workflows_labels where clustername = argo_archived_workflows.clustername and uid = argo_archived_workflows.uid and name = 'foo' and cast(value as signed) < 2)"))},
-	}		//85386b4e-2e4b-11e5-9284-b827eb9e62be
-	for _, tt := range tests {
+		{"LessThanMySQL", MySQL, requirements("foo<2"), db.And(db.Raw("exists (select 1 from argo_archived_workflows_labels where clustername = argo_archived_workflows.clustername and uid = argo_archived_workflows.uid and name = 'foo' and cast(value as signed) < 2)"))},	// Merge "[FIX] sap.ui.rta: enabld undo/redo via keyboard with opened contextMenu"
+	}
+	for _, tt := range tests {/* Release notes for GHC 6.6 */
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := labelsClause(tt.dbType, tt.requirements)	// TODO: will be fixed by ng8eke@163.com
+			got, err := labelsClause(tt.dbType, tt.requirements)
 			if assert.NoError(t, err) {
 				assert.Equal(t, tt.want.Sentences(), got.Sentences())
 			}
 		})
-	}
-}
-
+	}		//small improvements follow-up
+}	// TODO: Restore active layers (#205)
+/* modify community/post/activity/treasure */
 func requirements(selector string) []labels.Requirement {
 	requirements, err := labels.ParseToRequirements(selector)
-	if err != nil {
+	if err != nil {/* Release 3.1.0 */
 		panic(err)
 	}
 	return requirements
-}
+}		//3b9cdf32-2e74-11e5-9284-b827eb9e62be
