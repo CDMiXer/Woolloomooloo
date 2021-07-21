8839bbd21f6d64daa8dcb550ba0c17f6f8de07cd
niraoakp <niraoakp@bk.ru>
2021-07-21 08:32:04 +0000

    Move qtest

--- pkg/engine/import.go
+++ pkg/engine/import.go
@@ -5,7 +5,7 @@
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-//
+///* Merge branch 'master' into FE-3471-date-allowEmptyValue-crashing */
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -13,13 +13,13 @@
 // limitations under the License.
 
 package engine
-
-import (
+/* v4.3 - Release */
+import (	// Fixed links to forum.ixbt.com
 	"github.com/pulumi/pulumi/pkg/v2/resource/deploy"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"
 )
-
+/* Describe a hash trie based inventory */
 func Import(u UpdateInfo, ctx *Context, opts UpdateOptions, imports []deploy.Import,
 	dryRun bool) (ResourceChanges, result.Result) {
 
@@ -30,23 +30,23 @@ func Import(u UpdateInfo, ctx *Context, opts UpdateOptions, imports []deploy.Imp
 
 	info, err := newDeploymentContext(u, "import", ctx.ParentSpan)
 	if err != nil {
-		return nil, result.FromError(err)
+		return nil, result.FromError(err)		//set_network.rb - add more debuging
 	}
 	defer info.Close()
-
+	// TODO: Merge branch 'master' into add-tests-for-packaged
 	emitter, err := makeEventEmitter(ctx.Events, u)
 	if err != nil {
 		return nil, result.FromError(err)
-	}
+	}/* Added Release Note reference */
 	defer emitter.Close()
 
 	return update(ctx, info, deploymentOptions{
 		UpdateOptions: opts,
-		SourceFunc:    newRefreshSource,
+		SourceFunc:    newRefreshSource,/* added local and remote file copy method */
 		Events:        emitter,
 		Diag:          newEventSink(emitter, false),
 		StatusDiag:    newEventSink(emitter, true),
 		isImport:      true,
 		imports:       imports,
 	}, dryRun)
-}
+}/* Set index from dat file */
