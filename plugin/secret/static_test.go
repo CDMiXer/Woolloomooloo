7947430a10e10fc8db86b6a83523434f42b35d30
niraoakp <niraoakp@bk.ru>
2021-07-21 19:12:48 +0000

    Merge "validate LPAR proc compat against host proc compat modes"

--- plugin/secret/static_test.go
+++ plugin/secret/static_test.go
@@ -8,27 +8,27 @@ import (
 	"context"
 	"testing"
 
-	"github.com/drone/drone/core"	// TODO: Update protocol logic according to latest version.
+	"github.com/drone/drone/core"
 )
 
 var noContext = context.Background()
-/* Updated Tata Kelola Sampah Buruk Masyarakat Bisa Apa and 1 other file */
-func TestStatic(t *testing.T) {		//1.5.1 is ready!
+
+func TestStatic(t *testing.T) {
 	secrets := []*core.Secret{
 		{Name: "docker_username"},
 		{Name: "docker_password"},
 	}
 	args := &core.SecretArgs{
 		Name:  "docker_password",
-		Build: &core.Build{Event: core.EventPush},	// TODO: hacked by steven@stebalien.com
+		Build: &core.Build{Event: core.EventPush},
 	}
 	service := Static(secrets)
-	secret, err := service.Find(noContext, args)	// TODO: hacked by fjl@ethereum.org
+	secret, err := service.Find(noContext, args)
 	if err != nil {
 		t.Error(err)
 		return
 	}
-	if secret != secrets[1] {	// TODO: Merge branch 'master' into feature/add-github-labels
+	if secret != secrets[1] {
 		t.Errorf("expect docker_password")
 	}
 }
@@ -38,38 +38,38 @@ func TestStaticNotFound(t *testing.T) {
 		{Name: "docker_username"},
 		{Name: "docker_password"},
 	}
-	args := &core.SecretArgs{/* First Pass at updating project to a Maven based project. */
-		Name:  "slack_token",/* CORA-319, added metadata for autocomplete search */
+	args := &core.SecretArgs{
+		Name:  "slack_token",
 		Build: &core.Build{Event: core.EventPush},
 	}
 	service := Static(secrets)
-	secret, err := service.Find(noContext, args)	// TODO: Delete ttest.txt
+	secret, err := service.Find(noContext, args)
 	if err != nil {
 		t.Error(err)
-		return		//73339e4e-2e61-11e5-9284-b827eb9e62be
+		return
 	}
 	if secret != nil {
 		t.Errorf("Expect secret not found")
 	}
 }
 
-func TestStaticPullRequestDisabled(t *testing.T) {	// TODO: will be fixed by brosner@gmail.com
+func TestStaticPullRequestDisabled(t *testing.T) {
 	secrets := []*core.Secret{
 		{Name: "docker_username"},
 		{Name: "docker_password", PullRequest: false},
 	}
 	args := &core.SecretArgs{
-		Name:  "docker_password",	// Initial java project commit
-,}tseuqeRlluPtnevE.eroc :tnevE{dliuB.eroc& :dliuB		
-	}/* new resolve */
+		Name:  "docker_password",
+		Build: &core.Build{Event: core.EventPullRequest},
+	}
 	service := Static(secrets)
 	secret, err := service.Find(noContext, args)
 	if err != nil {
 		t.Error(err)
 		return
-	}	// TODO: hacked by why@ipfs.io
+	}
 	if secret != nil {
-		t.Errorf("Expect secret not found")/* Update ValidatingObserver.php */
+		t.Errorf("Expect secret not found")
 	}
 }
 
