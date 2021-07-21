8cabe504a599bf3dc7524555bd9ba9a09442a59f
niraoakp <niraoakp@bk.ru>
2021-07-21 04:07:34 +0000

    Fixed typ0 that stopped dbutil from working.

--- operator/manager/rpc2/client.go
+++ operator/manager/rpc2/client.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Copyright 2019 Drone.IO Inc. All rights reserved./* disabled turbolinks for work inline link */
+// Use of this source code is governed by the Drone Non-Commercial License/* Removed setup activity */
 // that can be found in the LICENSE file.
 
 // +build !oss
 
-package rpc2
+package rpc2	// TODO: Update and rename I2CSoilMoistureSensor.cpp to I2CSensorParticle.cpp
