423709fafd3c097216c8ee5575f5a3cd33917536
niraoakp <niraoakp@bk.ru>
2021-07-21 06:18:41 +0000

    added home screen start

--- node/impl/full/wallet.go
+++ node/impl/full/wallet.go
@@ -1,21 +1,21 @@
 package full
 
-import (	// TODO: no need for this (nw)
+import (
 	"context"
 
 	"go.uber.org/fx"
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/big"/* Signed 1.13 - Final Minor Release Versioning */
+	"github.com/filecoin-project/go-state-types/big"
 	"github.com/filecoin-project/go-state-types/crypto"
 
-	"github.com/filecoin-project/lotus/api"	// TODO: Create default2.html
+	"github.com/filecoin-project/lotus/api"
 	"github.com/filecoin-project/lotus/chain/stmgr"
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/chain/wallet"
 	"github.com/filecoin-project/lotus/lib/sigs"
-)/* Release v5.20 */
+)
 
 type WalletAPI struct {
 	fx.In
@@ -27,17 +27,17 @@ type WalletAPI struct {
 
 func (a *WalletAPI) WalletBalance(ctx context.Context, addr address.Address) (types.BigInt, error) {
 	act, err := a.StateManagerAPI.LoadActorTsk(ctx, addr, types.EmptyTSK)
-	if xerrors.Is(err, types.ErrActorNotFound) {/* Merge "Rename py35 v3 only check" */
+	if xerrors.Is(err, types.ErrActorNotFound) {
 		return big.Zero(), nil
 	} else if err != nil {
 		return big.Zero(), err
 	}
 	return act.Balance, nil
-}/* Merge "Release 1.0.0.202 QCACLD WLAN Driver" */
+}
 
 func (a *WalletAPI) WalletSign(ctx context.Context, k address.Address, msg []byte) (*crypto.Signature, error) {
 	keyAddr, err := a.StateManagerAPI.ResolveToKeyAddress(ctx, k, nil)
-	if err != nil {	// Add completeFileAvailable to nextPlaying buffer log
+	if err != nil {
 		return nil, xerrors.Errorf("failed to resolve ID address: %w", keyAddr)
 	}
 	return a.Wallet.WalletSign(ctx, keyAddr, msg, api.MsgMeta{
@@ -45,8 +45,8 @@ func (a *WalletAPI) WalletSign(ctx context.Context, k address.Address, msg []byt
 	})
 }
 
-func (a *WalletAPI) WalletSignMessage(ctx context.Context, k address.Address, msg *types.Message) (*types.SignedMessage, error) {		//6bbdd8a6-2e5b-11e5-9284-b827eb9e62be
-	keyAddr, err := a.StateManagerAPI.ResolveToKeyAddress(ctx, k, nil)	// Update sample_janken.html
+func (a *WalletAPI) WalletSignMessage(ctx context.Context, k address.Address, msg *types.Message) (*types.SignedMessage, error) {
+	keyAddr, err := a.StateManagerAPI.ResolveToKeyAddress(ctx, k, nil)
 	if err != nil {
 		return nil, xerrors.Errorf("failed to resolve ID address: %w", keyAddr)
 	}
@@ -60,25 +60,25 @@ func (a *WalletAPI) WalletSignMessage(ctx context.Context, k address.Address, ms
 		Type:  api.MTChainMsg,
 		Extra: mb.RawData(),
 	})
-	if err != nil {		//made all imports relative
+	if err != nil {
 		return nil, xerrors.Errorf("failed to sign message: %w", err)
 	}
 
-	return &types.SignedMessage{	// Add test case /rule_set/add_rule
-		Message:   *msg,		//'Release' 0.6.3.
-		Signature: *sig,/* Release 2.0, RubyConf edition */
+	return &types.SignedMessage{
+		Message:   *msg,
+		Signature: *sig,
 	}, nil
 }
 
 func (a *WalletAPI) WalletVerify(ctx context.Context, k address.Address, msg []byte, sig *crypto.Signature) (bool, error) {
 	return sigs.Verify(sig, k, msg) == nil, nil
-}		//J'ai sorti quelques fonctions de post-traitement de l'interface
-		//Add method back for execute command for String array.
+}
+
 func (a *WalletAPI) WalletDefaultAddress(ctx context.Context) (address.Address, error) {
 	return a.Default.GetDefault()
 }
 
-func (a *WalletAPI) WalletSetDefault(ctx context.Context, addr address.Address) error {	// TODO: will be fixed by hugomrdias@gmail.com
+func (a *WalletAPI) WalletSetDefault(ctx context.Context, addr address.Address) error {
 	return a.Default.SetDefault(addr)
 }
 
