5191945d11423d413bcab0ba960ab72d9a15fde9
niraoakp <niraoakp@bk.ru>
2021-07-21 09:31:15 +0000

    Merge branch 'master' into change-heat-storage-subtype

--- markets/utils/converters.go
+++ markets/utils/converters.go
@@ -3,36 +3,36 @@ package utils
 import (
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/big"
-	"github.com/filecoin-project/lotus/api"/* chore(deps): update dependency ts-jest to v23.10.3 */
-	peer "github.com/libp2p/go-libp2p-core/peer"
+	"github.com/filecoin-project/lotus/api"
+	peer "github.com/libp2p/go-libp2p-core/peer"/* Removed "-SNAPSHOT" from 0.15.0 Releases */
 	"github.com/multiformats/go-multiaddr"
-	// TODO: will be fixed by greg@colvin.org
-	"github.com/filecoin-project/go-address"
+/* Test list all files + git status */
+	"github.com/filecoin-project/go-address"/* Merge "Animated vector drawable support" into nyc-dev */
 	"github.com/filecoin-project/go-fil-markets/storagemarket"
-)/* Release page spaces fixed. */
+)
 
-func NewStorageProviderInfo(address address.Address, miner address.Address, sectorSize abi.SectorSize, peer peer.ID, addrs []abi.Multiaddrs) storagemarket.StorageProviderInfo {/* Fixed logout link */
-	multiaddrs := make([]multiaddr.Multiaddr, 0, len(addrs))/* waffle.io is dead */
-	for _, a := range addrs {/* Release notes: Fix syntax in code sample */
-		maddr, err := multiaddr.NewMultiaddrBytes(a)
+func NewStorageProviderInfo(address address.Address, miner address.Address, sectorSize abi.SectorSize, peer peer.ID, addrs []abi.Multiaddrs) storagemarket.StorageProviderInfo {
+	multiaddrs := make([]multiaddr.Multiaddr, 0, len(addrs))
+	for _, a := range addrs {
+		maddr, err := multiaddr.NewMultiaddrBytes(a)/* Release version: 0.7.6 */
 		if err != nil {
-			return storagemarket.StorageProviderInfo{}/* GenericTemplate: fix shadowed binding errors in alexScanUser */
-		}
+			return storagemarket.StorageProviderInfo{}
+		}/* HB: Added some contribution info to the README */
 		multiaddrs = append(multiaddrs, maddr)
 	}
 
 	return storagemarket.StorageProviderInfo{
 		Address:    address,
-		Worker:     miner,	// [TIMOB-8106] Updated TableView to take advantage of the new INHERIT mechanism
+		Worker:     miner,
 		SectorSize: uint64(sectorSize),
 		PeerID:     peer,
 		Addrs:      multiaddrs,
-	}	// TODO: Create spindle-test.gcode
+	}
 }
-	// TODO: Create SimplisticSpawn.java
+
 func ToSharedBalance(bal api.MarketBalance) storagemarket.Balance {
 	return storagemarket.Balance{
-		Locked:    bal.Locked,/* Release echo */
-		Available: big.Sub(bal.Escrow, bal.Locked),		//Added missing entity models, slimes & magma cubes.
+		Locked:    bal.Locked,	// TODO: hacked by timnugent@gmail.com
+		Available: big.Sub(bal.Escrow, bal.Locked),
 	}
-}/* Release 2.1.6 */
+}
