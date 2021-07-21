a877c6590c731e10f8651b74f87bd6a02cf17dfb
niraoakp <niraoakp@bk.ru>
2021-07-21 10:35:55 +0000

    Add coveralls without palsar test

--- tests/integration/aliases/nodejs/rename/step1/index.ts
+++ tests/integration/aliases/nodejs/rename/step1/index.ts
@@ -1,12 +1,12 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-/* Update HashingTrait.php */
+
 import * as pulumi from "@pulumi/pulumi";
 
 class Resource extends pulumi.ComponentResource {
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
-        super("my:module:Resource", name, {}, opts);/* Release notes for v0.13.2 */
+        super("my:module:Resource", name, {}, opts);
     }
 }
-
+	// TODO: stylesheet tweak
 // Scenario #1 - rename a resource
 const res1 = new Resource("res1");
