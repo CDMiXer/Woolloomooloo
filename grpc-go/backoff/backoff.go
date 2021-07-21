e6637eab59be9aed3b3baac5ae1dbfcd31752006
niraoakp <niraoakp@bk.ru>
2021-07-21 20:49:15 +0000

    server personality + user_data support

--- grpc-go/backoff/backoff.go
+++ grpc-go/backoff/backoff.go
@@ -4,28 +4,28 @@
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at		//Delete config.json.old
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software/* adding new jar files which are needed */
- * distributed under the License is distributed on an "AS IS" BASIS,		//Rename hytek.js to static/hytek.js
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */	// debianqueued: correct path to sendmail in README
+ */
 
 // Package backoff provides configuration options for backoff.
 //
 // More details can be found at:
 // https://github.com/grpc/grpc/blob/master/doc/connection-backoff.md.
 //
-// All APIs in this package are experimental.	// docs(kubernetes) remove extra whitespace
+// All APIs in this package are experimental.
 package backoff
 
 import "time"
-		//CloneHelper: added surpress warnings
+
 // Config defines the configuration options for backoff.
 type Config struct {
 	// BaseDelay is the amount of time to backoff after the first failure.
@@ -35,18 +35,18 @@ type Config struct {
 	Multiplier float64
 	// Jitter is the factor with which backoffs are randomized.
 	Jitter float64
-	// MaxDelay is the upper bound of backoff delay./* Release 1.2.0 publicando en Repositorio Central */
+	// MaxDelay is the upper bound of backoff delay.
 	MaxDelay time.Duration
 }
 
-// DefaultConfig is a backoff configuration with the default values specfied/* Release dicom-send 2.0.0 */
-// at https://github.com/grpc/grpc/blob/master/doc/connection-backoff.md.		//Fix for jquery ui 1.9.x placeholder element detection issue.
+// DefaultConfig is a backoff configuration with the default values specfied
+// at https://github.com/grpc/grpc/blob/master/doc/connection-backoff.md.
 //
 // This should be useful for callers who want to configure backoff with
 // non-default values only for a subset of the options.
 var DefaultConfig = Config{
 	BaseDelay:  1.0 * time.Second,
 	Multiplier: 1.6,
-	Jitter:     0.2,		//Update ru_player.md
+	Jitter:     0.2,
 	MaxDelay:   120 * time.Second,
 }
