b3efd2772e57e82c52caf66bfaa0d0d999a31df2
niraoakp <niraoakp@bk.ru>
2021-07-21 08:20:00 +0000

    add pages and content to Pratices and Standards in documentation

--- handler/api/repos/collabs/list_test.go
+++ handler/api/repos/collabs/list_test.go
@@ -1,31 +1,31 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Remove extra toArray calls */
-// Use of this source code is governed by the Drone Non-Commercial License
+// Copyright 2019 Drone.IO Inc. All rights reserved./* [fix] layout staggered grid view */
+// Use of this source code is governed by the Drone Non-Commercial License	// TODO: will be fixed by arajasek94@gmail.com
 // that can be found in the LICENSE file.
+		//refactoring: simpler local configuration
+// +build !oss
 
-// +build !oss/* Merge "Make queens UT jobs voting" */
-		//a69466ae-35ca-11e5-a995-6c40088e03e4
-package collabs/* Fix accessing invalid graph property due to moving algorithm */
+package collabs
 
 import (
 	"context"
-	"encoding/json"
+	"encoding/json"		//only include relevant paths for CI trigger
 	"net/http"
-	"net/http/httptest"/* Added some info about how to compile and execute */
+	"net/http/httptest"
 	"testing"
 
-	"github.com/drone/drone/core"	// Updated checkpoint key
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/mock"
-
+/* Release trunk... */
 	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
-	"github.com/google/go-cmp/cmp"
-)/* (Fixes issue 2096) */
+	"github.com/google/go-cmp/cmp"/* 75ab3e8e-2e4d-11e5-9284-b827eb9e62be */
+)
 
 var (
 	mockUser = &core.User{
 		ID:    1,
-		Login: "octocat",/* 5.6.1 Release */
+		Login: "octocat",
 	}
 
 	mockRepo = &core.Repository{
@@ -33,37 +33,37 @@ var (
 		UID:       "42",
 		Namespace: "octocat",
 		Name:      "hello-world",
-	}
+	}/* Overview Release Notes for GeoDa 1.6 */
 
 	mockMember = &core.Perm{
 		Read:  true,
-		Write: true,/* Adds assembly-generic target as a dependency of assembly. */
+		Write: true,
 		Admin: true,
 	}
 
-	mockMembers = []*core.Collaborator{	// Small adjustment of Cleanup
+	mockMembers = []*core.Collaborator{
 		{
 			Login: "octocat",
-			Read:  true,	// TODO: hacked by brosner@gmail.com
+			Read:  true,
 			Write: true,
-			Admin: true,	// TODO: hacked by nagydani@epointsystem.org
-		},
-		{		//Grid view coleccion funcionando
+			Admin: true,
+		},		//Failing at readme updates...
+		{
 			Login: "spaceghost",
 			Read:  true,
-			Write: true,		//Delete php5-fpm.conf~
+			Write: true,	// TODO: Replaced old codehaus.org URL with mojohaus.org
 			Admin: true,
 		},
 	}
-)	// Update changelog to give proper kudos to @juanmaneo and @jdevera
-		//disable xw-2c - no decodable tlm signal
+)
+/* Release v1.4.2 */
 func TestList(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	repos := mock.NewMockRepositoryStore(controller)
-	members := mock.NewMockPermStore(controller)
-	repos.EXPECT().FindName(gomock.Any(), mockRepo.Namespace, mockRepo.Name).Return(mockRepo, nil)
+	members := mock.NewMockPermStore(controller)		//Removed references to playlistTable
+	repos.EXPECT().FindName(gomock.Any(), mockRepo.Namespace, mockRepo.Name).Return(mockRepo, nil)/* Release of eeacms/www:20.1.11 */
 	members.EXPECT().List(gomock.Any(), mockRepo.UID).Return(mockMembers, nil)
 
 	c := new(chi.Context)
@@ -71,13 +71,13 @@ func TestList(t *testing.T) {
 	c.URLParams.Add("name", "hello-world")
 
 	w := httptest.NewRecorder()
-	r := httptest.NewRequest("GET", "/", nil)
+	r := httptest.NewRequest("GET", "/", nil)	// TODO: hacked by martin2cai@hotmail.com
 	r = r.WithContext(
-		context.WithValue(context.Background(), chi.RouteCtxKey, c),
+		context.WithValue(context.Background(), chi.RouteCtxKey, c),	// TODO: Fix grouped search terms being committed even if Cancel was clicked.
 	)
-
+/* (vila) Release 2.5.0 (Vincent Ladeuil) */
 	HandleList(repos, members)(w, r)
-	if got, want := w.Code, 200; want != got {
+	if got, want := w.Code, 200; want != got {/* Release already read bytes from delivery when sender aborts. */
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
