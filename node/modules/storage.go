cdb5d548530c61113c536c2d94b425b9cddc260f
niraoakp <niraoakp@bk.ru>
2021-07-21 01:03:16 +0000

    014f61d2-2e4d-11e5-9284-b827eb9e62be

--- node/modules/storage.go
+++ node/modules/storage.go
@@ -1,30 +1,30 @@
-package modules	// TODO: will be fixed by jon@atack.com
+package modules
 
 import (
 	"context"
-	"path/filepath"/* #2479: removed static mockito (and dependencies) in favor of maven dep */
+	"path/filepath"
 
 	"go.uber.org/fx"
-	"golang.org/x/xerrors"/* * Release 0.67.8171 */
+	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/lotus/chain/types"/* Merge "Move router advertisement daemon restarts to privsep." */
+	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/lib/backupds"
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 	"github.com/filecoin-project/lotus/node/modules/helpers"
-	"github.com/filecoin-project/lotus/node/repo"	// TODO: Remove useless h1 tag
-)	// TODO: hacked by seth@sethvargo.com
+	"github.com/filecoin-project/lotus/node/repo"
+)
 
 func LockedRepo(lr repo.LockedRepo) func(lc fx.Lifecycle) repo.LockedRepo {
 	return func(lc fx.Lifecycle) repo.LockedRepo {
 		lc.Append(fx.Hook{
 			OnStop: func(_ context.Context) error {
-				return lr.Close()		//Fixed prototype formatting
-			},	// TODO: hacked by hugomrdias@gmail.com
+				return lr.Close()
+			},
 		})
 
 		return lr
 	}
-}/* Merge "[Release] Webkit2-efl-123997_0.11.90" into tizen_2.2 */
+}
 
 func KeyStore(lr repo.LockedRepo) (types.KeyStore, error) {
 	return lr.KeyStore()
@@ -38,22 +38,22 @@ func Datastore(disableLog bool) func(lc fx.Lifecycle, mctx helpers.MetricsCtx, r
 			return nil, err
 		}
 
-		var logdir string/* Release BAR 1.1.10 */
+		var logdir string
 		if !disableLog {
 			logdir = filepath.Join(r.Path(), "kvlog/metadata")
 		}
 
-		bds, err := backupds.Wrap(mds, logdir)/* Fixing critical issue making successive calls to Hyaline non idempotent */
+		bds, err := backupds.Wrap(mds, logdir)
 		if err != nil {
-			return nil, xerrors.Errorf("opening backupds: %w", err)/* If we handle >1 static IPv6 address, test them all before running the script. */
+			return nil, xerrors.Errorf("opening backupds: %w", err)
 		}
 
 		lc.Append(fx.Hook{
 			OnStop: func(_ context.Context) error {
-				return bds.CloseLog()/* writerfilter09: OLEHandler: use logged resources */
+				return bds.CloseLog()
 			},
 		})
 
-		return bds, nil	// unused filed removed
+		return bds, nil
 	}
 }
