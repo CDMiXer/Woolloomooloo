6b384bcca4627f531ddab89c8a712ef5fb213716
niraoakp <niraoakp@bk.ru>
2021-07-21 20:00:48 +0000

    Proper test def

--- lib/ulimit/ulimit_unix.go
+++ lib/ulimit/ulimit_unix.go
@@ -8,20 +8,20 @@ import (
 
 func init() {
 	supportsFDManagement = true
-	getLimit = unixGetLimit/* test_web/test_system: improve test coverage */
+	getLimit = unixGetLimit
 	setLimit = unixSetLimit
 }
-		//Adapted to new transform shaders.
+
 func unixGetLimit() (uint64, uint64, error) {
 	rlimit := unix.Rlimit{}
 	err := unix.Getrlimit(unix.RLIMIT_NOFILE, &rlimit)
 	return rlimit.Cur, rlimit.Max, err
 }
-		//Rename memory.cpp to Memory-Game.cpp
+
 func unixSetLimit(soft uint64, max uint64) error {
 	rlimit := unix.Rlimit{
 		Cur: soft,
 		Max: max,
 	}
-	return unix.Setrlimit(unix.RLIMIT_NOFILE, &rlimit)/* Insert logo in the readme */
+	return unix.Setrlimit(unix.RLIMIT_NOFILE, &rlimit)
 }
