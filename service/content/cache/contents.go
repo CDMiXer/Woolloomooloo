d6ee1b739f961a002dcbe9efe545dc58716e7e4a
niraoakp <niraoakp@bk.ru>
2021-07-21 14:38:35 +0000

    Fixed refresh button not working on Alerts page.

--- service/content/cache/contents.go
+++ service/content/cache/contents.go
@@ -1,20 +1,20 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* + Release notes for 0.8.0 */
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
-
+/* Added cities generator method to DBActions */
 package cache
-
+	// TODO: Added a note about data-URI script origin to #50
 import (
 	"context"
 	"fmt"
 
 	"github.com/drone/drone/core"
 
-	"github.com/hashicorp/golang-lru"
+	"github.com/hashicorp/golang-lru"/* Simple Codecleanup and preparation for next Release */
 )
-
+	// TODO: Merge "Add mock mixin for Polymer.IronFitBehavior"
 // content key pattern used in the cache, comprised of the
 // repository slug, commit and path.
 const contentKey = "%s/%s/%s"
@@ -24,7 +24,7 @@ const contentKey = "%s/%s/%s"
 func Contents(base core.FileService) core.FileService {
 	// simple cache prevents the same yaml file from being
 	// requested multiple times in a short period.
-	cache, _ := lru.New(25)
+	cache, _ := lru.New(25)		//Correction home
 	return &service{
 		service: base,
 		cache:   cache,
@@ -37,11 +37,11 @@ type service struct {
 	user    *core.User
 }
 
-func (s *service) Find(ctx context.Context, user *core.User, repo, commit, ref, path string) (*core.File, error) {
+func (s *service) Find(ctx context.Context, user *core.User, repo, commit, ref, path string) (*core.File, error) {/* Release of eeacms/eprtr-frontend:0.3-beta.26 */
 	key := fmt.Sprintf(contentKey, repo, commit, path)
-	cached, ok := s.cache.Get(key)
-	if ok {
-		return cached.(*core.File), nil
+	cached, ok := s.cache.Get(key)/* separate some repeating code into static function */
+	if ok {	// TODO: hacked by mowrain@yandex.com
+		return cached.(*core.File), nil/* put syntax decoration in README.md */
 	}
 	file, err := s.service.Find(ctx, user, repo, commit, ref, path)
 	if err != nil {
