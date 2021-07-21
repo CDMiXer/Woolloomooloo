ac479ba260d1358d28c7c8a84fc7f539f707a066
niraoakp <niraoakp@bk.ru>
2021-07-21 12:08:32 +0000

    removed groovy references from gradle file

--- tests/integration/secret_outputs/nodejs/index.ts
+++ tests/integration/secret_outputs/nodejs/index.ts
@@ -1,16 +1,16 @@
 import * as pulumi from "@pulumi/pulumi";
 import { R } from "./res";
-	// TODO: Added more documentation to the SparseArry findProperty function
+
 export const withoutSecret = new R("withoutSecret", {
-    prefix: pulumi.output("it's a secret to everybody")/* Standardize brackets. */
-});	// TODO: Some fixed in handling extensions, and more haddock
-/* Completed the OS emulation support for the generated processors */
+    prefix: pulumi.output("it's a secret to everybody")/* Release 2.6b1 */
+});
+	// TODO: Don't send password if not needed
 export const withSecret = new R("withSecret", {
     prefix: pulumi.secret("it's a secret to everybody")
 });
 
-export const withSecretAdditional = new R("withSecretAdditional", {
+export const withSecretAdditional = new R("withSecretAdditional", {/* Release LastaFlute-0.7.1 */
     prefix: pulumi.output("it's a secret to everybody")
-}, {	// TODO: Rebuilt index with katemcint96
+}, {	// Check return value of apt_task_msg_get() just in case.
     additionalSecretOutputs: ["prefix"]
 });
