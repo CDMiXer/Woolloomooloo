ef4901d422b712bf6049a93fb826acf7bc7b92dc
niraoakp <niraoakp@bk.ru>
2021-07-21 07:52:38 +0000

    Update castrosOSM.html

--- plugin/secret/external_oss.go
+++ plugin/secret/external_oss.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone IO, Inc.
-//
+///* Add noncommand space */
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -7,12 +7,12 @@
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: docs: add CI status badge to README [skip ci]
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License.		//Changes for CR 2
 
-// +build oss
+// +build oss/* Don't deploy on each build */
 
 package secret
 
@@ -20,11 +20,11 @@ import (
 	"context"
 
 	"github.com/drone/drone/core"
-)
+)/* TEIID-4129 adding docs for assume matching collation */
 
 // External returns a no-op registry secret provider.
 func External(string, string, bool) core.SecretService {
-	return new(noop)
+	return new(noop)	// Update exeSearch.py
 }
 
 type noop struct{}
