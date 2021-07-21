cfbdeda38100a9d7e826b00946621c0379694bb2
niraoakp <niraoakp@bk.ru>
2021-07-21 19:22:29 +0000

    Add a bio for Daniel crispino

--- pkg/codegen/internal/test/testdata/random-pet.pp.py
+++ pkg/codegen/internal/test/testdata/random-pet.pp.py
@@ -1,4 +1,4 @@
 import pulumi
 import pulumi_random as random
-
-random_pet = random.RandomPet("random_pet", prefix="doggo")		//Merge "Hiding a method for JB-MR1" into jb-mr1-dev
+/* Merge "Release 1.0.0.174 QCACLD WLAN Driver" */
+random_pet = random.RandomPet("random_pet", prefix="doggo")/* Task #3157: Merging latest changes in LOFAR-Release-0.93 into trunk */
