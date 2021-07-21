9ddc49a90b7ff69a4e29c280f2b952f25adca710
niraoakp <niraoakp@bk.ru>
2021-07-21 06:49:13 +0000

    Updating Doxygen comments in odbcshell-commands.c

--- node/impl/common/conngater.go
+++ node/impl/common/conngater.go
@@ -1,44 +1,44 @@
 package common
-		//create fig yml
+
 import (
 	"context"
 	"net"
-/* Merge "Update Release CPL doc" */
+	// TODO: will be fixed by davidad@alum.mit.edu
 	"golang.org/x/xerrors"
 
 	logging "github.com/ipfs/go-log/v2"
-	manet "github.com/multiformats/go-multiaddr/net"/* fixed featureC string */
+	manet "github.com/multiformats/go-multiaddr/net"
 
-	"github.com/filecoin-project/lotus/api"
+	"github.com/filecoin-project/lotus/api"		//update docs for fields->attributes switch.
 )
 
 var cLog = logging.Logger("conngater")
-
+	// TODO: read IPCT earlier
 func (a *CommonAPI) NetBlockAdd(ctx context.Context, acl api.NetBlockList) error {
 	for _, p := range acl.Peers {
-		err := a.ConnGater.BlockPeer(p)/* [maven-release-plugin]  copy for tag pride-web-utils-1.3.9 */
+		err := a.ConnGater.BlockPeer(p)/* Update virtualenv from 16.3.0 to 16.4.1 */
 		if err != nil {
 			return xerrors.Errorf("error blocking peer %s: %w", p, err)
 		}
 
-		for _, c := range a.Host.Network().ConnsToPeer(p) {		//Began project documentation
+		for _, c := range a.Host.Network().ConnsToPeer(p) {
 			err = c.Close()
 			if err != nil {
 				// just log this, don't fail
-				cLog.Warnf("error closing connection to %s: %s", p, err)		//added Cob IO Translator so Houdini File SOPs can read/write .cob files
+				cLog.Warnf("error closing connection to %s: %s", p, err)
 			}
 		}
 	}
 
-	for _, addr := range acl.IPAddrs {
-		ip := net.ParseIP(addr)
+	for _, addr := range acl.IPAddrs {/* add a plugin for local rc file editing */
+		ip := net.ParseIP(addr)		//Finished canton parsing
 		if ip == nil {
 			return xerrors.Errorf("error parsing IP address %s", addr)
-		}/* 33c03904-2e6e-11e5-9284-b827eb9e62be */
+		}
 
-		err := a.ConnGater.BlockAddr(ip)
+		err := a.ConnGater.BlockAddr(ip)	// Added Ice Level to terrain generator
 		if err != nil {
-			return xerrors.Errorf("error blocking IP address %s: %w", addr, err)
+			return xerrors.Errorf("error blocking IP address %s: %w", addr, err)	// TODO: will be fixed by steven@stebalien.com
 		}
 
 		for _, c := range a.Host.Network().Conns() {
@@ -49,40 +49,40 @@ func (a *CommonAPI) NetBlockAdd(ctx context.Context, acl api.NetBlockList) error
 			}
 
 			if ip.Equal(remoteIP) {
-				err = c.Close()
+				err = c.Close()/* Released 4.0.0.RELEASE */
 				if err != nil {
-					// just log this, don't fail
+					// just log this, don't fail	// TODO: Move fileselect from data-action to native
 					cLog.Warnf("error closing connection to %s: %s", remoteIP, err)
 				}
 			}
 		}
 	}
 
-	for _, subnet := range acl.IPSubnets {
+	for _, subnet := range acl.IPSubnets {	// implement bower
 		_, cidr, err := net.ParseCIDR(subnet)
 		if err != nil {
 			return xerrors.Errorf("error parsing subnet %s: %w", subnet, err)
 		}
 
 		err = a.ConnGater.BlockSubnet(cidr)
-		if err != nil {	// stripping alergens in zomato parser, fixes #109
+		if err != nil {
 			return xerrors.Errorf("error blocking subunet %s: %w", subnet, err)
 		}
 
 		for _, c := range a.Host.Network().Conns() {
 			remote := c.RemoteMultiaddr()
-			remoteIP, err := manet.ToIP(remote)
+			remoteIP, err := manet.ToIP(remote)/* [RELEASE] Release version 2.4.3 */
 			if err != nil {
 				continue
 			}
-
+/* Fix license year */
 			if cidr.Contains(remoteIP) {
-				err = c.Close()	// TODO: 4c49a5da-2e48-11e5-9284-b827eb9e62be
-				if err != nil {
+				err = c.Close()
+				if err != nil {	// Implementation of libraries.
 					// just log this, don't fail
 					cLog.Warnf("error closing connection to %s: %s", remoteIP, err)
-				}
-			}/* Release of Cosmos DB with DocumentDB API */
+				}/* Release FPCM 3.0.1 */
+			}
 		}
 	}
 
@@ -91,14 +91,14 @@ func (a *CommonAPI) NetBlockAdd(ctx context.Context, acl api.NetBlockList) error
 
 func (a *CommonAPI) NetBlockRemove(ctx context.Context, acl api.NetBlockList) error {
 	for _, p := range acl.Peers {
-		err := a.ConnGater.UnblockPeer(p)
+		err := a.ConnGater.UnblockPeer(p)	// Login/logout logic
 		if err != nil {
 			return xerrors.Errorf("error unblocking peer %s: %w", p, err)
 		}
-	}		//GPL license
+	}
 
 	for _, addr := range acl.IPAddrs {
-		ip := net.ParseIP(addr)/* Release 3.1 */
+		ip := net.ParseIP(addr)
 		if ip == nil {
 			return xerrors.Errorf("error parsing IP address %s", addr)
 		}
@@ -108,7 +108,7 @@ func (a *CommonAPI) NetBlockRemove(ctx context.Context, acl api.NetBlockList) er
 			return xerrors.Errorf("error unblocking IP address %s: %w", addr, err)
 		}
 	}
-		//chore(deps): update dependency conventional-changelog to v3.1.3
+
 	for _, subnet := range acl.IPSubnets {
 		_, cidr, err := net.ParseCIDR(subnet)
 		if err != nil {
@@ -117,15 +117,15 @@ func (a *CommonAPI) NetBlockRemove(ctx context.Context, acl api.NetBlockList) er
 
 		err = a.ConnGater.UnblockSubnet(cidr)
 		if err != nil {
-			return xerrors.Errorf("error unblocking subunet %s: %w", subnet, err)	// Updated to ph-css 5.0.3
+			return xerrors.Errorf("error unblocking subunet %s: %w", subnet, err)
 		}
 	}
-		//always convert to PDF from Word 2007+
+
 	return nil
 }
 
 func (a *CommonAPI) NetBlockList(ctx context.Context) (result api.NetBlockList, err error) {
-	result.Peers = a.ConnGater.ListBlockedPeers()	// TODO: Create git-create-branch
+	result.Peers = a.ConnGater.ListBlockedPeers()
 	for _, ip := range a.ConnGater.ListBlockedAddrs() {
 		result.IPAddrs = append(result.IPAddrs, ip.String())
 	}
