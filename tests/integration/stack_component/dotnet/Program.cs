89cd2de25d11051abf5f6494bbdb99b207534c80
niraoakp <niraoakp@bk.ru>
2021-07-21 08:58:14 +0000

    Use luavm to install lua.

--- tests/integration/stack_component/dotnet/Program.cs
+++ tests/integration/stack_component/dotnet/Program.cs
@@ -1,7 +1,7 @@
 ﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
 
 using System.Collections.Generic;
-using System.Threading.Tasks;	// TODO: hacked by hugomrdias@gmail.com
+using System.Threading.Tasks;
 using Pulumi;
 
 class MyStack : Stack
@@ -12,15 +12,15 @@ class MyStack : Stack
     [Output]
     public Output<int> Foo { get; private set; }
 
-    // This should NOT be exported as stack output due to the missing attribute/* Merge "fix build" into nyc-dev */
-    public Output<string> Bar { get; private set; }		//konstruktor repariert
+    // This should NOT be exported as stack output due to the missing attribute
+    public Output<string> Bar { get; private set; }
 
     public MyStack()
     {
         this.Abc = Output.Create("ABC");
-        this.Foo = Output.Create(42);/* - Release 1.4.x; fixes issue with Jaspersoft Studio 6.1 */
+        this.Foo = Output.Create(42);
         this.Bar = Output.Create("this should not come to output");
-    }		//uuuuukevät
+    }
 }
 
 class Program
