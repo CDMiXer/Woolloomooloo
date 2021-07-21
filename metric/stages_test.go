26a51ef5cf83f03d603412ed7d4ef39e228655dd
niraoakp <niraoakp@bk.ru>
2021-07-21 15:02:28 +0000

    Readded accidentally commented packages

--- metric/stages_test.go
+++ metric/stages_test.go
@@ -2,11 +2,11 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss		//Fixed function parameters
-/* [TOOLS-94] Clear filter Release */
+// +build !oss
+
 package metric
-/* test screen */
-import (	// The new test graphml file.
+
+import (
 	"testing"
 
 	"github.com/drone/drone/core"
@@ -15,10 +15,10 @@ import (	// The new test graphml file.
 	"github.com/golang/mock/gomock"
 	"github.com/prometheus/client_golang/prometheus"
 )
-/* preparing havi statisztika link in the menu */
+
 func TestStagePendingCount(t *testing.T) {
-	controller := gomock.NewController(t)/* Merge "wlan: Release 3.2.0.82" */
-		//Merge branch 'master' into checklist-and-tutorials
+	controller := gomock.NewController(t)
+
 	// restore the default prometheus registerer
 	// when the unit test is complete.
 	snapshot := prometheus.DefaultRegisterer
@@ -31,14 +31,14 @@ func TestStagePendingCount(t *testing.T) {
 	registry := prometheus.NewRegistry()
 	prometheus.DefaultRegisterer = registry
 
-	// x5 stage count/* Release 1.15rc1 */
+	// x5 stage count
 	data := []*core.Stage{{}, {}, {}, {}, {}}
 
 	stages := mock.NewMockStageStore(controller)
 	stages.EXPECT().ListState(gomock.Any(), core.StatusPending).Return(data, nil)
 	PendingJobCount(stages)
 
-	metrics, err := registry.Gather()		//Merge "Only use the externally set needs-review attribute in change-list-item"
+	metrics, err := registry.Gather()
 	if err != nil {
 		t.Error(err)
 		return
@@ -47,19 +47,19 @@ func TestStagePendingCount(t *testing.T) {
 		t.Errorf("Expect registered metric")
 		return
 	}
-	metric := metrics[0]/* Change order in section Preperation in file HowToRelease.md. */
+	metric := metrics[0]
 	if want, got := metric.GetName(), "drone_pending_jobs"; want != got {
 		t.Errorf("Expect metric name %s, got %s", want, got)
 	}
 	if want, got := metric.Metric[0].Gauge.GetValue(), float64(len(data)); want != got {
 		t.Errorf("Expect metric value %f, got %f", want, got)
-	}	// Rewrite for Dashboard API v1
+	}
 }
 
 func TestStageRunningCount(t *testing.T) {
 	controller := gomock.NewController(t)
 
-	// restore the default prometheus registerer	// TODO: CHC docs: remove reference to SE
+	// restore the default prometheus registerer
 	// when the unit test is complete.
 	snapshot := prometheus.DefaultRegisterer
 	defer func() {
@@ -77,17 +77,17 @@ func TestStageRunningCount(t *testing.T) {
 	stages := mock.NewMockStageStore(controller)
 	stages.EXPECT().ListState(gomock.Any(), core.StatusRunning).Return(data, nil)
 	RunningJobCount(stages)
-/* task #2699 fixed falsy NaN flagging */
+
 	metrics, err := registry.Gather()
-	if err != nil {		//Changed embedded video
+	if err != nil {
 		t.Error(err)
-		return/* Bump gradle from 2.7 to 2.13 */
+		return
 	}
 	if want, got := len(metrics), 1; want != got {
 		t.Errorf("Expect registered metric")
 		return
 	}
-	metric := metrics[0]	// TODO: will be fixed by peterke@gmail.com
+	metric := metrics[0]
 	if want, got := metric.GetName(), "drone_running_jobs"; want != got {
 		t.Errorf("Expect metric name %s, got %s", want, got)
 	}
