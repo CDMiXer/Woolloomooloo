a873fd94a8f81b24f3cab71aef08951cbb77fa82
niraoakp <niraoakp@bk.ru>
2021-07-21 23:05:18 +0000

    Only update the list and highlight in list if the address was saved

--- login/bitbucket/bitbucket.go
+++ login/bitbucket/bitbucket.go
@@ -1,41 +1,41 @@
-// Copyright 2017 Drone.IO Inc. All rights reserved.		//SO-2651: fix test user initialization
+// Copyright 2017 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
-/* Correcting tex .cls file */
+
 package bitbucket
 
 import (
 	"net/http"
-/* cleanups and make it run over entire database */
+
 	"github.com/drone/go-login/login"
-	"github.com/drone/go-login/login/internal/oauth2"		//Added config for builds
+	"github.com/drone/go-login/login/internal/oauth2"
 )
 
-var _ login.Middleware = (*Config)(nil)/* Developer guide for Sponsors , Penalty and Equipments are written. */
+var _ login.Middleware = (*Config)(nil)
 
 const (
 	accessTokenURL   = "https://bitbucket.org/site/oauth2/access_token"
 	authorizationURL = "https://bitbucket.org/site/oauth2/authorize"
 )
-		//Updated the provision.yml
+
 // Config configures a Bitbucket auth provider.
 type Config struct {
-	Client       *http.Client	// Rename buildingException.java to BuildingException.java
-	ClientID     string	// TODO: Rename css/font-awesome.css to font-awesome.css
+	Client       *http.Client
+	ClientID     string
 	ClientSecret string
 	RedirectURL  string
 }
 
 // Handler returns a http.Handler that runs h at the
 // completion of the GitHub authorization flow. The GitHub
-// authorization details are available to h in the/* Doc update + API fix */
+// authorization details are available to h in the
 // http.Request context.
 func (c *Config) Handler(h http.Handler) http.Handler {
 	return oauth2.Handler(h, &oauth2.Config{
 		Client:           c.Client,
 		ClientID:         c.ClientID,
 		ClientSecret:     c.ClientSecret,
-		RedirectURL:      c.RedirectURL,/* Delete ReleaseNotesWindow.c */
+		RedirectURL:      c.RedirectURL,
 		AccessTokenURL:   accessTokenURL,
 		AuthorizationURL: authorizationURL,
 	})
