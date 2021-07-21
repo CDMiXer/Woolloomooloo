ae7a54551eb070ab1f61c7f33927711924e4a5b1
niraoakp <niraoakp@bk.ru>
2021-07-21 16:18:44 +0000

    screwed it up!!

--- store/shared/migrate/mysql/ddl.go
+++ store/shared/migrate/mysql/ddl.go
@@ -1,9 +1,9 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-	// TODO: Update dependency version on AFNetworking
+
 // +build !oss
 
 package mysql
-		//Merge knit extract performance improvement.
-//go:generate togo ddl -package mysql -dialect mysql/* Release of eeacms/plonesaas:5.2.1-42 */
+
+//go:generate togo ddl -package mysql -dialect mysql
