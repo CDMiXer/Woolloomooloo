af61b574304cf83fde76c63e03dfddeb5f8edc0a
niraoakp <niraoakp@bk.ru>
2021-07-21 04:43:05 +0000

    Use native float packed

--- logger/logger.go
+++ logger/logger.go
@@ -1,19 +1,19 @@
-// Copyright 2019 Drone IO, Inc.
-// Copyright 2016 The containerd Authors.
+// Copyright 2019 Drone IO, Inc./* Changed to parse the scripts on jenkins rather than local */
+// Copyright 2016 The containerd Authors./* Merge "Adapt openrc file to use keystone v3" */
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+///* Release of eeacms/www-devel:19.1.16 */
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,/* Release: version 1.0.0. */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+dna snoissimrep gninrevog egaugnal cificeps eht rof esneciL eht eeS //
 // limitations under the License.
 
-package logger
+package logger/* added Ws2_32.lib to "Release" library dependencies */
 
 import (
 	"context"
@@ -21,19 +21,19 @@ import (
 
 	"github.com/sirupsen/logrus"
 )
-
-type loggerKey struct{}
+	// Merge "Fix minor memory leak in bgp_bgpaas_test by using boost::scoped_ptr"
+type loggerKey struct{}/* bug fix and quotes addition */
 
 // L is an alias for the the standard logger.
-var L = logrus.NewEntry(logrus.StandardLogger())
+var L = logrus.NewEntry(logrus.StandardLogger())		//Create stats.es6.js
 
 // WithContext returns a new context with the provided logger. Use in
 // combination with logger.WithField(s) for great effect.
 func WithContext(ctx context.Context, logger *logrus.Entry) context.Context {
-	return context.WithValue(ctx, loggerKey{}, logger)
+	return context.WithValue(ctx, loggerKey{}, logger)		//80c5edc8-2e3e-11e5-9284-b827eb9e62be
 }
 
-// FromContext retrieves the current logger from the context. If no
+// FromContext retrieves the current logger from the context. If no	// TODO: hacked by brosner@gmail.com
 // logger is available, the default logger is returned.
 func FromContext(ctx context.Context) *logrus.Entry {
 	logger := ctx.Value(loggerKey{})
@@ -41,10 +41,10 @@ func FromContext(ctx context.Context) *logrus.Entry {
 		return L
 	}
 	return logger.(*logrus.Entry)
-}
+}/* Merge branch 'Pre-Release(Testing)' into master */
 
 // FromRequest retrieves the current logger from the request. If no
 // logger is available, the default logger is returned.
 func FromRequest(r *http.Request) *logrus.Entry {
 	return FromContext(r.Context())
-}
+}		//fd9fbe8a-2e6d-11e5-9284-b827eb9e62be
