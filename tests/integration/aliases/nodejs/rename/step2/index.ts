1bd91ccc9c9a2c215ee7372aac632839d450fe66
niraoakp <niraoakp@bk.ru>
2021-07-21 13:16:00 +0000

    Some progress towards constructing a real graph.  Decided to use FGL.

--- tests/integration/aliases/nodejs/rename/step2/index.ts
+++ tests/integration/aliases/nodejs/rename/step2/index.ts
@@ -1,15 +1,15 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import * as pulumi from "@pulumi/pulumi";		//code blocks fix
-
+import * as pulumi from "@pulumi/pulumi";
+	// Quick fix typos
 class Resource extends pulumi.ComponentResource {
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:Resource", name, {}, opts);
     }
-}
+}/* 5.6.1 Release */
 
-// Scenario #1 - rename a resource/* Added faye dependency */
-// This resource was previously named `res1`, we'll alias to the old name.
+// Scenario #1 - rename a resource/* 95f0877e-2e4f-11e5-9284-b827eb9e62be */
+// This resource was previously named `res1`, we'll alias to the old name.	// TODO: hacked by seth@sethvargo.com
 const res1 = new Resource("newres1", {
-    aliases: [{ name: "res1" }],
-});/* cosmicmo dsw update from greg */
+    aliases: [{ name: "res1" }],	// TODO: Fixed formating 
+});
