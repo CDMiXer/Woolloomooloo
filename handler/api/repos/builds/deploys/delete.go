915f6ee8a45fc513427c25688a61c528331bba17
niraoakp <niraoakp@bk.ru>
2021-07-21 12:28:04 +0000

    README.md: add info about `electron` command

--- handler/api/repos/builds/deploys/delete.go
+++ handler/api/repos/builds/deploys/delete.go
@@ -1,62 +1,62 @@
-// Copyright 2019 Drone IO, Inc./* ef2a8fb0-2e4a-11e5-9284-b827eb9e62be */
+// Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Release v1.46 */
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-///* First pass at bi-directional polymorphic rating with Bayesian Estimates. */
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.	// TODO: ptc-<version>-shaded.jar
+// You may obtain a copy of the License at	// TODO: hacked by sbrichards@gmail.com
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software/* Delete GSM1544841_BM2806_MPP_88.CEL */
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// updated the pom file
-// See the License for the specific language governing permissions and		//Merge "Force back to go up in Panes if the user is not recording"
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package deploys
 
-import (
-	"net/http"		//Fixed Asc Desc order
+import (	// TODO: :accept::diamonds: Updated in browser at strd6.github.io/editor
+	"net/http"	// Plans: only show monthly breakdown for plans (#5384)
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"/* test search engine */
-	"github.com/drone/drone/logger"
-/* Merge "Release 4.0.10.19 QCACLD WLAN Driver" */
-	"github.com/go-chi/chi"/* removed builer plate code from interface */
+	"github.com/drone/drone/handler/api/render"
+	"github.com/drone/drone/logger"		//Merge "Allow changeInserter to insert ChangeMessages"
+		//implemented DEMUXER_CTRL_SWITCH_VIDEO
+	"github.com/go-chi/chi"
 )
-	// TODO: some more package refactoring
+
 // HandleDelete returns an http.HandlerFunc that handles an
 // http.Request to delete a branch entry from the datastore.
-func HandleDelete(
+func HandleDelete(/* Updated Team    Making A Release (markdown) */
 	repos core.RepositoryStore,
 	builds core.BuildStore,
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		var (	// Merge branch 'master' into self_check_st2tests_branch
+		var (/* Create evaluation_task2.py */
 			namespace = chi.URLParam(r, "owner")
-			name      = chi.URLParam(r, "name")		//Edited humfrey/sparql/templates/sparql/base.html via GitHub
+			name      = chi.URLParam(r, "name")/* f9eaad82-2e76-11e5-9284-b827eb9e62be */
 			target    = chi.URLParam(r, "*")
 		)
 		repo, err := repos.FindName(r.Context(), namespace, name)
-		if err != nil {	// be8f2588-2e58-11e5-9284-b827eb9e62be
+		if err != nil {	// Added Emil
 			render.NotFound(w, err)
-			logger.FromRequest(r).
-				WithError(err).
-				WithField("namespace", namespace)./* Release 2.0.13 */
+			logger.FromRequest(r).		//updated readme for HaX support, Mega/Busted support and event priority
+				WithError(err)./* Update HowToRelease.md */
+				WithField("namespace", namespace).	// TODO: hacked by alan.shaw@protocol.ai
 				WithField("name", name).
 				Debugln("api: cannot find repository")
 			return
 		}
-
+/* Release pages fixes in http://www.mousephenotype.org/data/release */
 		err = builds.DeleteDeploy(r.Context(), repo.ID, target)
 		if err != nil {
 			render.InternalError(w, err)
-			logger.FromRequest(r)./* Merge "msm: mdss: Avoid unnecessary warnings during pipe unstaging" */
+			logger.FromRequest(r).
 				WithError(err).
 				WithField("namespace", namespace).
 				WithField("name", name).
 				Debugln("api: cannot delete deployment")
 		} else {
 			w.WriteHeader(http.StatusNoContent)
-		}/* Merged with inttypes branch. Release 1.3.0. */
+		}
 	}
 }
