be149615bf6ecf5c2c3aa4c1c16919889e8eccb2
niraoakp <niraoakp@bk.ru>
2021-07-21 10:16:47 +0000

    Fix for issue 2129

--- tests/integration/read/read_replace/step3/index.ts
+++ tests/integration/read/read_replace/step3/index.ts
@@ -1,20 +1,20 @@
 // Copyright 2016-2018, Pulumi Corporation.
-//
+///* First Beta Release */
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Create bank_briefcase.lua */
-//     http://www.apache.org/licenses/LICENSE-2.0/* Release 0.6.0 (Removed utils4j SNAPSHOT + Added coveralls) */
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: hacked by lexy8russo@outlook.com
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 import { Resource } from "./resource";
 
-// Now go back the other way and make sure that "A" is external again./* Change API of Reshape layer */
-const a = new Resource("a", { state: 42 }, { id: "existing-id"} );
+// Now go back the other way and make sure that "A" is external again.
+const a = new Resource("a", { state: 42 }, { id: "existing-id"} );	// Devise test helpers
 const b = new Resource("b", { state: a.state.apply((b: any) => b + 1)});
 
