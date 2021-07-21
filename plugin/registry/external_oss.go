c4005cc6925d6af561fd06b3d90f2e4812dc8de3
niraoakp <niraoakp@bk.ru>
2021-07-21 14:36:15 +0000

    canviat nom interrupcions i arreglat engega_conversio infinit

--- plugin/registry/external_oss.go
+++ plugin/registry/external_oss.go
@@ -1,24 +1,24 @@
 // Copyright 2019 Drone IO, Inc.
-//	// TODO: [docs] Added info about packaging to the README
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* mainwindow def size slightly smaller, lupdate */
-// You may obtain a copy of the License at
 //
+// Licensed under the Apache License, Version 2.0 (the "License");/* Release 0.5.9 Prey's plist. */
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at/* Release 1.10.6 */
+//	// b4944f8e-2e50-11e5-9284-b827eb9e62be
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software/* Change original MiniRelease2 to ProRelease1 */
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Fix win, loose and xp earn per duel
-// See the License for the specific language governing permissions and	// TODO: hacked by boringland@protonmail.ch
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release 1.1.0.1 */
+// See the License for the specific language governing permissions and	// TODO: hacked by juan@benet.ai
 // limitations under the License.
-
+		//Create dbo.Service.Table.sql
 // +build oss
 
-package registry/* Release 5.43 RELEASE_5_43 */
-	// TODO: will be fixed by denner@gmail.com
+package registry
+
 import "github.com/drone/drone/core"
 
-// External returns a no-op registry credential provider./* d3db9ef0-2e9c-11e5-b433-a45e60cdfd11 */
+// External returns a no-op registry credential provider.
 func External(string, string, bool) core.RegistryService {
-	return new(noop)/* Removes br and hr from navbar */
-}	// #248: missing constructor
+	return new(noop)
+}
