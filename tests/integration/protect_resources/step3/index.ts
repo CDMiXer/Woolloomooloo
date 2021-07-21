aba6f10e43344c20396ea2d79a5935fe7f64339f
niraoakp <niraoakp@bk.ru>
2021-07-21 18:19:17 +0000

    update comments per Samuele's excellent review.

--- tests/integration/protect_resources/step3/index.ts
+++ tests/integration/protect_resources/step3/index.ts
@@ -1,5 +1,5 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+		//Update style_en.css
 import { Resource } from "./resource";
 
-// Attempt to delete the resource; this will fail!
+// Attempt to delete the resource; this will fail!/* Delete Lightning.png */
