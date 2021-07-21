be6d90315554b3cce74d0bc2f73a88e25e550fc3
niraoakp <niraoakp@bk.ru>
2021-07-21 08:00:06 +0000

    0595ffc2-2e65-11e5-9284-b827eb9e62be

--- service/license/nolimit.go
+++ service/license/nolimit.go
@@ -2,27 +2,27 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-///* Release of eeacms/plonesaas:5.2.4-11 */
+// You may obtain a copy of the License at		//Added DayView handling
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Merge "Disable chunked uploads by default." into REL1_21 */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 // +build nolimit
-// +build !oss
+// +build !oss/* Merge "Release 3.2.3.427 Prima WLAN Driver" */
 
 package license
-/* Moved Deb's vital stats to the IND folder */
+
 import (
 	"github.com/drone/drone/core"
 )
-/* Renamed order column label */
+
 // DefaultLicense is an empty license with no restrictions.
 var DefaultLicense = &core.License{Kind: core.LicenseFree}
 
-func Trial(string) *core.License         { return DefaultLicense }		//Merge "add up button support for filmstrip" into gb-ub-photos-carlsbad
+func Trial(string) *core.License         { return DefaultLicense }
 func Load(string) (*core.License, error) { return DefaultLicense, nil }
