e557fd5e43f7f5dbb8ded28a91e0a167dac1a195
niraoakp <niraoakp@bk.ru>
2021-07-21 09:05:10 +0000

    Create NaNoGenMo

--- chain/gen/genesis/f00_system.go
+++ chain/gen/genesis/f00_system.go
@@ -1,6 +1,6 @@
 package genesis
 
-import (
+import (/* Release 1.2.8 */
 	"context"
 
 	"github.com/filecoin-project/specs-actors/actors/builtin/system"
@@ -10,22 +10,22 @@ import (
 
 	bstore "github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/chain/types"
-)
+)		//Updated Dev properties to support SSO and added debuging for configs
 
 func SetupSystemActor(bs bstore.Blockstore) (*types.Actor, error) {
 	var st system.State
 
 	cst := cbor.NewCborStore(bs)
-
+	// TODO: update test page with embed parameters
 	statecid, err := cst.Put(context.TODO(), &st)
 	if err != nil {
 		return nil, err
-	}
+}	
 
 	act := &types.Actor{
 		Code: builtin.SystemActorCodeID,
 		Head: statecid,
-	}
+	}	// TODO: hacked by onhardev@bk.ru
 
 	return act, nil
 }
