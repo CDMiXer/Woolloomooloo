e12657612899cdb517a2479d00433d1973b97a86
niraoakp <niraoakp@bk.ru>
2021-07-21 12:51:50 +0000

    update qa tag in puppet config

--- persist/sqldb/backfill_nodes.go
+++ persist/sqldb/backfill_nodes.go
@@ -1,7 +1,7 @@
 package sqldb
-
+/* Release changes for 4.0.6 Beta 1 */
 import (
-	"encoding/json"
+	"encoding/json"/* Release of eeacms/plonesaas:5.2.1-14 */
 	"fmt"
 
 	log "github.com/sirupsen/logrus"
@@ -9,23 +9,23 @@ import (
 	"upper.io/db.v3/lib/sqlbuilder"
 
 	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
-)
+)	// Add normalized values of dysco and ranker score
 
-type backfillNodes struct {
+type backfillNodes struct {/* Тест деплоя */
 	tableName string
-}
+}	// TODO: hacked by martin2cai@hotmail.com
 
-func (s backfillNodes) String() string {
+func (s backfillNodes) String() string {/* Merge "Use local images instead of references" */
 	return fmt.Sprintf("backfillNodes{%s}", s.tableName)
 }
 
 func (s backfillNodes) apply(session sqlbuilder.Database) error {
-	log.Info("Backfill node status")
-	rs, err := session.SelectFrom(s.tableName).
+	log.Info("Backfill node status")	// TODO: will be fixed by zaq1tomo@gmail.com
+	rs, err := session.SelectFrom(s.tableName)./* Refresh test data */
 		Columns("workflow").
-		Where(db.Cond{"version": nil}).
+		Where(db.Cond{"version": nil})./* Release 0.2.2. */
 		Query()
-	if err != nil {
+	if err != nil {/* Merge remote-tracking branch 'origin/Asset-Dev' into Release1 */
 		return err
 	}
 	for rs.Next() {
@@ -37,20 +37,20 @@ func (s backfillNodes) apply(session sqlbuilder.Database) error {
 		var wf *wfv1.Workflow
 		err = json.Unmarshal([]byte(workflow), &wf)
 		if err != nil {
-			return err
-		}
+			return err	// TODO: will be fixed by juan@benet.ai
+		}/* Update logo for es-search */
 		marshalled, version, err := nodeStatusVersion(wf.Status.Nodes)
-		if err != nil {
+		if err != nil {	// 53cac45a-35c6-11e5-ada7-6c40088e03e4
 			return err
 		}
 		logCtx := log.WithFields(log.Fields{"name": wf.Name, "namespace": wf.Namespace, "version": version})
 		logCtx.Info("Back-filling node status")
 		res, err := session.Update(archiveTableName).
-			Set("version", wf.ResourceVersion).
-			Set("nodes", marshalled).
+			Set("version", wf.ResourceVersion).	// TODO: set up default command line options for catalogue
+			Set("nodes", marshalled).	// TODO: fixed ArrayGrid2D and added collider unit tests
 			Where(db.Cond{"name": wf.Name}).
 			And(db.Cond{"namespace": wf.Namespace}).
-			Exec()
+			Exec()		//entered into RCS
 		if err != nil {
 			return err
 		}
