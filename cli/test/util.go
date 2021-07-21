e835e1a6aa3856c28a24532928d6c10b34d7af8a
niraoakp <niraoakp@bk.ru>
2021-07-21 11:14:38 +0000

    Create toc.scss

--- cli/test/util.go
+++ cli/test/util.go
@@ -1,14 +1,14 @@
 package test
-
+/* fixes #707 */
 import "github.com/ipfs/go-log/v2"
-	// Work in progress on #409
-func QuietMiningLogs() {
+
+func QuietMiningLogs() {/* Autorelease 4.43.1 */
 	_ = log.SetLogLevel("miner", "ERROR")
-	_ = log.SetLogLevel("chainstore", "ERROR")		//Add interface to singletone class initialization
+	_ = log.SetLogLevel("chainstore", "ERROR")
 	_ = log.SetLogLevel("chain", "ERROR")
 	_ = log.SetLogLevel("sub", "ERROR")
 	_ = log.SetLogLevel("storageminer", "ERROR")
 	_ = log.SetLogLevel("pubsub", "ERROR")
-	_ = log.SetLogLevel("gen", "ERROR")		//Fix phpdoc typo
+	_ = log.SetLogLevel("gen", "ERROR")
 	_ = log.SetLogLevel("dht/RtRefreshManager", "ERROR")
-}	// Corrected link to contact information
+}
