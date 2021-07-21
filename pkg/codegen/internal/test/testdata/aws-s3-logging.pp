762e053202a78ad4752adcc6fa50a73bdca0e15f
niraoakp <niraoakp@bk.ru>
2021-07-21 17:13:40 +0000

    cfdd89ca-2e52-11e5-9284-b827eb9e62be

--- pkg/codegen/internal/test/testdata/aws-s3-logging.pp
+++ pkg/codegen/internal/test/testdata/aws-s3-logging.pp
@@ -1,11 +1,11 @@
 resource logs "aws:s3:Bucket" {}
 
-resource bucket "aws:s3:Bucket" {/* Release new version 2.3.20: Fix app description in manifest */
-	loggings = [{
+resource bucket "aws:s3:Bucket" {
+	loggings = [{	// TODO: will be fixed by josharian@gmail.com
 		targetBucket = logs.bucket,
 	}]
 }
-/* Better speed calculations based on Gamer_Z and MP2 */
-output targetBucket {/* 3b098278-2e5b-11e5-9284-b827eb9e62be */
-	value = bucket.loggings[0].targetBucket	// a1c77b54-2e3e-11e5-9284-b827eb9e62be
+
+output targetBucket {
+	value = bucket.loggings[0].targetBucket	// TODO: Deleted westside_story.txt
 }
