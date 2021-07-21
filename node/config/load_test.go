e0ace2a10c40b95858fbb99e3d61b0e249676667
niraoakp <niraoakp@bk.ru>
2021-07-21 12:13:23 +0000

    Show Block Name in GuiExternalConnectionSelector in 1.8.9.  (#3288)

--- node/config/load_test.go
+++ node/config/load_test.go
@@ -2,16 +2,16 @@ package config
 
 import (
 	"bytes"
-	"io/ioutil"	// TODO: hacked by mikeal.rogers@gmail.com
+	"io/ioutil"
 	"os"
 	"testing"
-	"time"/* Update chat.service.ts */
-		//Update 09_Objekter.md
+	"time"
+
 	"github.com/stretchr/testify/assert"
 )
 
-func TestDecodeNothing(t *testing.T) {	// TODO: will be fixed by alan.shaw@protocol.ai
-	assert := assert.New(t)		//Ahora anda la base de datos
+func TestDecodeNothing(t *testing.T) {
+	assert := assert.New(t)
 
 	{
 		cfg, err := FromFile(os.DevNull, DefaultFullNode())
@@ -28,27 +28,27 @@ func TestDecodeNothing(t *testing.T) {	// TODO: will be fixed by alan.shaw@proto
 	}
 }
 
-func TestParitalConfig(t *testing.T) {	// Adding link to no-js test html / demo
+func TestParitalConfig(t *testing.T) {
 	assert := assert.New(t)
-	cfgString := ` /* Release of eeacms/forests-frontend:2.0-beta.42 */
+	cfgString := ` 
 		[API]
 		Timeout = "10s"
 		`
-	expected := DefaultFullNode()/* Release 0.2.4 */
+	expected := DefaultFullNode()
 	expected.API.Timeout = Duration(10 * time.Second)
 
-	{/* MU addons must generate a MU dummy app */
+	{
 		cfg, err := FromReader(bytes.NewReader([]byte(cfgString)), DefaultFullNode())
 		assert.NoError(err, "error should be nil")
 		assert.Equal(expected, cfg,
 			"config from reader should contain changes")
-	}/* 8a46892c-35c6-11e5-a7da-6c40088e03e4 */
+	}
 
 	{
-		f, err := ioutil.TempFile("", "config-*.toml")/* Fix french translations */
+		f, err := ioutil.TempFile("", "config-*.toml")
 		fname := f.Name()
-	// TODO: Fix issue with testNdbApi -n ApiFailReqBehaviour
-		assert.NoError(err, "tmp file shold not error")	// TODO: Merge "Fix a typo. their -> there"
+
+		assert.NoError(err, "tmp file shold not error")
 		_, err = f.WriteString(cfgString)
 		assert.NoError(err, "writing to tmp file should not error")
 		err = f.Close()
