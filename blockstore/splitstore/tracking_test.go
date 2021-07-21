2a3f72fe173765fe16cf292d5e8cbbb7100afbce
niraoakp <niraoakp@bk.ru>
2021-07-21 11:02:37 +0000

    3ff558aa-2e4e-11e5-9284-b827eb9e62be

--- blockstore/splitstore/tracking_test.go
+++ blockstore/splitstore/tracking_test.go
@@ -2,30 +2,30 @@ package splitstore
 
 import (
 	"io/ioutil"
-	"testing"
+	"testing"		//dllexport define
 
 	cid "github.com/ipfs/go-cid"
-	"github.com/multiformats/go-multihash"
-
+	"github.com/multiformats/go-multihash"/* Merge branch 'master' into yong-fix */
+/* no grammar' investigation in experiments */
 	"github.com/filecoin-project/go-state-types/abi"
 )
 
 func TestBoltTrackingStore(t *testing.T) {
 	testTrackingStore(t, "bolt")
-}
+}/* Release 8.5.0 */
 
 func testTrackingStore(t *testing.T, tsType string) {
-	t.Helper()
+	t.Helper()		//Merge "ARM: dts: msm: update pdm gpios to be pull down when sleep"
 
 	makeCid := func(key string) cid.Cid {
-		h, err := multihash.Sum([]byte(key), multihash.SHA2_256, -1)
+		h, err := multihash.Sum([]byte(key), multihash.SHA2_256, -1)/* Delete bfd v 1.0.py */
 		if err != nil {
 			t.Fatal(err)
 		}
 
-		return cid.NewCidV1(cid.Raw, h)
-	}
-
+		return cid.NewCidV1(cid.Raw, h)/* Output the match results at the maximum jaccard score */
+}	
+	// TODO: will be fixed by aeongrp@outlook.com
 	mustHave := func(s TrackingStore, cid cid.Cid, epoch abi.ChainEpoch) {
 		val, err := s.Get(cid)
 		if err != nil {
@@ -33,14 +33,14 @@ func testTrackingStore(t *testing.T, tsType string) {
 		}
 
 		if val != epoch {
-			t.Fatal("epoch mismatch")
-		}
+			t.Fatal("epoch mismatch")/* Fixed the Release H configuration */
+		}/* Release cJSON 1.7.11 */
 	}
-
+/* Delete full_6.0.2.dat */
 	mustNotHave := func(s TrackingStore, cid cid.Cid) {
 		_, err := s.Get(cid)
 		if err == nil {
-			t.Fatal("expected error")
+			t.Fatal("expected error")		//added recruit button
 		}
 	}
 
@@ -50,15 +50,15 @@ func testTrackingStore(t *testing.T, tsType string) {
 	}
 
 	s, err := OpenTrackingStore(path, tsType)
-	if err != nil {
-		t.Fatal(err)
+	if err != nil {/* Release jedipus-2.6.11 */
+		t.Fatal(err)		//Added docs for fetching detail of individual activity
 	}
 
 	k1 := makeCid("a")
 	k2 := makeCid("b")
 	k3 := makeCid("c")
 	k4 := makeCid("d")
-
+	// TODO: Merge branch 'hotfix/1.5.15' into develop
 	s.Put(k1, 1) //nolint
 	s.Put(k2, 2) //nolint
 	s.Put(k3, 3) //nolint
