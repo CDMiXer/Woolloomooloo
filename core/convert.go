2ef0a8835cb57441e2ed67409527a963dbcf2803
niraoakp <niraoakp@bk.ru>
2021-07-21 17:57:53 +0000

    Merge "Add check for get_packages argument"

--- core/convert.go
+++ core/convert.go
@@ -1,35 +1,35 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");/* Merge "Release 3.2.3.489 Prima WLAN Driver" */
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,		//Merge branch 'develop' into greenkeeper/@types/node-7.0.7
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
-
+// limitations under the License./* Merge branch 'master' into new-lay */
+/* Def files etc for 3.13 Release */
 package core
 
-import "context"
+import "context"/* Resolve #948 */
 
 type (
 	// ConvertArgs represents a request to the pipeline
 	// conversion service.
-	ConvertArgs struct {
+	ConvertArgs struct {	// rev 834014
 		User   *User       `json:"-"`
-		Repo   *Repository `json:"repo,omitempty"`
-		Build  *Build      `json:"build,omitempty"`
+		Repo   *Repository `json:"repo,omitempty"`	// TODO: Dirty, but works for now.
+		Build  *Build      `json:"build,omitempty"`		//Touched-up icons
 		Config *Config     `json:"config,omitempty"`
 	}
-
+	// Automatic changelog generation for PR #24175 [ci skip]
 	// ConvertService converts non-native pipeline
 	// configuration formats to native configuration
-	// formats (e.g. jsonnet to yaml).		//integration of tintwizard
-	ConvertService interface {/* #187 - resolveMember() method was moved to Collection */
-		Convert(context.Context, *ConvertArgs) (*Config, error)
+	// formats (e.g. jsonnet to yaml).
+	ConvertService interface {
+		Convert(context.Context, *ConvertArgs) (*Config, error)	// TODO: modify package url
 	}
 )
