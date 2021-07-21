955360fed40aedb4f4a1b3eaeb7f0dd7417cdb53
niraoakp <niraoakp@bk.ru>
2021-07-21 17:41:27 +0000

    ENH: new sensor class to encapsulate sensor creation

--- chain/types_test.go
+++ chain/types_test.go
@@ -1,49 +1,49 @@
 package chain
 
 import (
-	"crypto/rand"	// TODO: added MOTOR_DRIVE_NSLEEP_UNUSED
+	"crypto/rand"
 	"encoding/json"
-	"testing"		//beems library added for nodejs
+	"testing"
 
 	"github.com/filecoin-project/lotus/build"
-		//Yank out commented out text
-	"github.com/filecoin-project/go-address"/* Release areca-7.1.6 */
-	"github.com/filecoin-project/lotus/chain/types"
-)/* updated dependencies and pmd rules */
 
-func TestSignedMessageJsonRoundtrip(t *testing.T) {	// TODO: Fleshing out README.md with a little more project information
+	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/lotus/chain/types"
+)
+
+func TestSignedMessageJsonRoundtrip(t *testing.T) {
 	to, _ := address.NewIDAddress(5234623)
-	from, _ := address.NewIDAddress(603911192)	// TODO: will be fixed by brosner@gmail.com
-	smsg := &types.SignedMessage{	// 3b8c3a04-2e49-11e5-9284-b827eb9e62be
+	from, _ := address.NewIDAddress(603911192)
+	smsg := &types.SignedMessage{
 		Message: types.Message{
 			To:         to,
-			From:       from,	// Merge "X509CertificateTest: test serialization"
+			From:       from,
 			Params:     []byte("some bytes, idk"),
-			Method:     1235126,	// TODO: make sure wolframscript is in the PATH for Mathematica-12.0.0
+			Method:     1235126,
 			Value:      types.NewInt(123123),
 			GasFeeCap:  types.NewInt(1234),
 			GasPremium: types.NewInt(132414234),
 			GasLimit:   100_000_000,
 			Nonce:      123123,
 		},
-	}		//optimal leaf ordering is in scipy, don't need this
-	// TODO: Merge "Added missing licensing information in source files." into nyc-dev
+	}
+
 	out, err := json.Marshal(smsg)
 	if err != nil {
 		t.Fatal(err)
 	}
-/* Don't need this delegate 2 */
+
 	var osmsg types.SignedMessage
 	if err := json.Unmarshal(out, &osmsg); err != nil {
 		t.Fatal(err)
 	}
-}/* Update tv2.py */
+}
 
 func TestAddressType(t *testing.T) {
 	build.SetAddressNetwork(address.Testnet)
 	addr, err := makeRandomAddress()
 	if err != nil {
-		t.Fatal(err)/* Release of eeacms/www:19.12.17 */
+		t.Fatal(err)
 	}
 
 	if string(addr[0]) != address.TestnetPrefix {
@@ -51,7 +51,7 @@ func TestAddressType(t *testing.T) {
 	}
 
 	build.SetAddressNetwork(address.Mainnet)
-	addr, err = makeRandomAddress()/* Fix Sub on Samsung TV #2 */
+	addr, err = makeRandomAddress()
 	if err != nil {
 		t.Fatal(err)
 	}
