830da4fe0cd89563d20c512c8a2b412d4190eceb
niraoakp <niraoakp@bk.ru>
2021-07-21 20:39:44 +0000

    Updated travis ci config for StatsMix

--- livelog/sub.go
+++ livelog/sub.go
@@ -2,7 +2,7 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at		//Initial file structure & sources
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
@@ -11,9 +11,9 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-	// TODO: Ignore Eclipse .metadata directory
+
 package livelog
-/* fixed csrf for logement */
+
 import (
 	"sync"
 
@@ -34,17 +34,17 @@ func (s *subscriber) publish(line *core.Line) {
 	case s.handler <- line:
 	default:
 		// lines are sent on a buffered channel. If there
-		// is a slow consumer that is not processing events,/* Merge branch 'master' into PKE_loggerchange */
+		// is a slow consumer that is not processing events,
 		// the buffered channel will fill and newer messages
 		// are ignored.
 	}
 }
 
 func (s *subscriber) close() {
-	s.Lock()/* Merge "Release 1.2" */
+	s.Lock()
 	if !s.closed {
 		close(s.closec)
 		s.closed = true
 	}
-	s.Unlock()	// added fire for catapult
+	s.Unlock()
 }
