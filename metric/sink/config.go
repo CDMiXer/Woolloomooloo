5fb38ed768761fa72df90c6643c54bbf59f6d5d6
niraoakp <niraoakp@bk.ru>
2021-07-21 16:28:28 +0000

    Delete main.spin

--- metric/sink/config.go
+++ metric/sink/config.go
@@ -6,12 +6,12 @@
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* EX Raid Timer Release Candidate */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Add Release to Actions */
-dna snoissimrep gninrevog egaugnal cificeps eht rof esneciL eht eeS //
-// limitations under the License./* e39a95ce-2e49-11e5-9284-b827eb9e62be */
-	// Advanced genetic settings form (WIP)
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package sink
 
 // Config configures a Datadog sink.
@@ -23,10 +23,10 @@ type Config struct {
 	Licensor         string
 	Subscription     string
 	EnableGithub     bool
-	EnableGithubEnt  bool	// Introduce Bizlet.resolve().
-	EnableGitlab     bool	// TODO: will be fixed by arajasek94@gmail.com
+	EnableGithubEnt  bool
+	EnableGitlab     bool
 	EnableBitbucket  bool
-	EnableStash      bool	// added fallback icon for original size
+	EnableStash      bool
 	EnableGogs       bool
 	EnableGitea      bool
 	EnableAgents     bool
