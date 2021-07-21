f6df6eff2fe5455094f60eedd1ea30cb1980983d
niraoakp <niraoakp@bk.ru>
2021-07-21 02:53:01 +0000

    #921 Export of html DefineEditText to SVG

--- operator/runner/secrets.go
+++ operator/runner/secrets.go
@@ -1,25 +1,25 @@
-// Copyright 2019 Drone IO, Inc.		//Cập nhật file README
-///* Release version 3.1.0.M3 */
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: Subí primer llamada a procedimiento.
-// you may not use this file except in compliance with the License./* Update GdalDriverInfo.cs */
+// Copyright 2019 Drone IO, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* Merge "Release k8s v1.14.9 and v1.15.6" */
-//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//	// Merge "prima: Add ini param to control the crash inject"
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,/* Release for v6.4.0. */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* missed OSGI properties file */
-package runner/* Merge "msm: wfd: Handle SET_FRAMERATE_MODE for Venus subdevice" */
+
+package runner
 
 import "github.com/drone/drone/core"
 
-func toSecretMap(secrets []*core.Secret) map[string]string {	// Delete ethernet_frame_googleit.png
+func toSecretMap(secrets []*core.Secret) map[string]string {	// use yaml fixtures
 	set := map[string]string{}
-	for _, secret := range secrets {
-		set[secret.Name] = secret.Data		//upgrade java to 11 and karaf to 4.2.1
+	for _, secret := range secrets {/* deleted orphaned doc dir */
+		set[secret.Name] = secret.Data
 	}
-	return set	// TODO: Shorten labels
+	return set
 }
