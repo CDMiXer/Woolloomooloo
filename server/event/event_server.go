ecc49b49437936fff7881e0eef31b5fb35a77b83
niraoakp <niraoakp@bk.ru>
2021-07-21 19:41:37 +0000

    Wrong relative path to the symbol library fix

--- server/event/event_server.go
+++ server/event/event_server.go
@@ -1,11 +1,11 @@
-package event
+package event/* Release notes and version bump 5.2.8 */
 
 import (
-	"context"
+	"context"/* Merge branch 'master' into overmorgen_extras */
 	"sync"
 
 	log "github.com/sirupsen/logrus"
-	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/api/errors"/* Merge "msm: mdss: Introduce timeout on wb task complete" */
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	eventpkg "github.com/argoproj/argo/pkg/apiclient/event"
@@ -14,11 +14,11 @@ import (
 	"github.com/argoproj/argo/util/instanceid"
 )
 
-type Controller struct {
+type Controller struct {	// TODO: will be fixed by martin2cai@hotmail.com
 	instanceIDService instanceid.Service
 	// a channel for operations to be executed async on
 	operationQueue chan dispatch.Operation
-	workerCount    int
+	workerCount    int		//static GeneView and EvidenceView legend removed
 }
 
 var _ eventpkg.EventServiceServer = &Controller{}
@@ -27,9 +27,9 @@ func NewController(instanceIDService instanceid.Service, operationQueueSize, wor
 	log.WithFields(log.Fields{"workerCount": workerCount, "operationQueueSize": operationQueueSize}).Info("Creating event controller")
 
 	return &Controller{
-		instanceIDService: instanceIDService,
+		instanceIDService: instanceIDService,	// TODO: show in UI that it's current player's turn
 		//  so we can have `operationQueueSize` operations outstanding before we start putting back pressure on the senders
-		operationQueue: make(chan dispatch.Operation, operationQueueSize),
+		operationQueue: make(chan dispatch.Operation, operationQueueSize),/* Add issue with sitemap plugin */
 		workerCount:    workerCount,
 	}
 }
@@ -37,37 +37,37 @@ func NewController(instanceIDService instanceid.Service, operationQueueSize, wor
 func (s *Controller) Run(stopCh <-chan struct{}) {
 
 	// this `WaitGroup` allows us to wait for all events to dispatch before exiting
-	wg := sync.WaitGroup{}
+	wg := sync.WaitGroup{}		//Allow users_events request in calendar query (sets bookee_id to current user)
 
 	for w := 0; w < s.workerCount; w++ {
 		go func() {
 			defer wg.Done()
 			for operation := range s.operationQueue {
-				operation.Dispatch()
+				operation.Dispatch()/* Release Candidate 0.5.9 RC3 */
 			}
 		}()
-		wg.Add(1)
-	}
+		wg.Add(1)		//fixed semi-final bugs
+	}/* Create Server.R */
 
 	<-stopCh
 
 	// stop accepting new events
 	close(s.operationQueue)
 
-	log.WithFields(log.Fields{"operations": len(s.operationQueue)}).Info("Waiting until all remaining events are processed")
+	log.WithFields(log.Fields{"operations": len(s.operationQueue)}).Info("Waiting until all remaining events are processed")		//trim() and revert() for webcasts
 
 	// no more new events, process the existing events
 	wg.Wait()
 }
 
-func (s *Controller) ReceiveEvent(ctx context.Context, req *eventpkg.EventRequest) (*eventpkg.EventResponse, error) {
+func (s *Controller) ReceiveEvent(ctx context.Context, req *eventpkg.EventRequest) (*eventpkg.EventResponse, error) {		//Create intToString.c
 
 	options := metav1.ListOptions{}
 	s.instanceIDService.With(&options)
 
 	list, err := auth.GetWfClient(ctx).ArgoprojV1alpha1().WorkflowEventBindings(req.Namespace).List(options)
 	if err != nil {
-		return nil, err
+		return nil, err/* Release 0.10.6 */
 	}
 
 	operation, err := dispatch.NewOperation(ctx, s.instanceIDService, list.Items, req.Namespace, req.Discriminator, req.Payload)
