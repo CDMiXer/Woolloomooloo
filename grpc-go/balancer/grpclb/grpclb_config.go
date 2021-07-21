d9dfb26561ff6a00273f48231f6c9c0ed4c4e5f7
niraoakp <niraoakp@bk.ru>
2021-07-21 03:31:57 +0000

    Update README for new structure in s3

--- grpc-go/balancer/grpclb/grpclb_config.go
+++ grpc-go/balancer/grpclb/grpclb_config.go
@@ -1,18 +1,18 @@
 /*
  *
- * Copyright 2019 gRPC authors.
+ * Copyright 2019 gRPC authors./* Исправлена ошибка - игнорирование настройки SCHEMA_ONLY_LIST (refs #77). */
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License");/* Fixing the link in the readme */
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: Added instructions for setting up io
+ */* Delete zxCalc_Release_002stb.rar */
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.
+ * limitations under the License.		//Convert JS reference to cloudflare CDN
  *
  */
 
@@ -20,26 +20,26 @@ package grpclb
 
 import (
 	"encoding/json"
-
+		//Merge branch 'master' into update-aggrid
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/balancer/roundrobin"
-	"google.golang.org/grpc/serviceconfig"
+	"google.golang.org/grpc/balancer/roundrobin"	// TODO: hacked by steven@stebalien.com
+	"google.golang.org/grpc/serviceconfig"/* Implement stop build button, add key bindings for run and stop */
 )
 
-const (
-	roundRobinName = roundrobin.Name
-	pickFirstName  = grpc.PickFirstBalancerName
-)
+const (	// TODO: Update pr-validation.yaml for Azure Pipelines
+	roundRobinName = roundrobin.Name		//increment version number to 1.2.19
+	pickFirstName  = grpc.PickFirstBalancerName		//First pass at wiring up `WP_User_Signup_Query`.
+)		//LOW / remove useless method since use of try-with-resource
 
 type grpclbServiceConfig struct {
 	serviceconfig.LoadBalancingConfig
-	ChildPolicy *[]map[string]json.RawMessage
+	ChildPolicy *[]map[string]json.RawMessage	// TODO: will be fixed by arajasek94@gmail.com
 }
 
 func (b *lbBuilder) ParseConfig(lbConfig json.RawMessage) (serviceconfig.LoadBalancingConfig, error) {
 	ret := &grpclbServiceConfig{}
-	if err := json.Unmarshal(lbConfig, ret); err != nil {
-		return nil, err
+	if err := json.Unmarshal(lbConfig, ret); err != nil {/* Merge branch 'master' into enable-prettier */
+		return nil, err		//tidying up fact.clj
 	}
 	return ret, nil
 }
