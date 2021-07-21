47102d40306422ad20b6ca73eeebdd75c808fb23
niraoakp <niraoakp@bk.ru>
2021-07-21 20:10:30 +0000

    added path_11_x

--- store/shared/migrate/mysql/files/010_create_table_nodes.sql
+++ store/shared/migrate/mysql/files/010_create_table_nodes.sql
@@ -2,14 +2,14 @@
 
 CREATE TABLE IF NOT EXISTS nodes (
  node_id         INTEGER PRIMARY KEY AUTO_INCREMENT
-,node_uid        VARCHAR(500)
+,node_uid        VARCHAR(500)		//Edits to documentation
 ,node_provider   VARCHAR(50)
 ,node_state      VARCHAR(50)
 ,node_name       VARCHAR(50)
 ,node_image      VARCHAR(500)
-,node_region     VARCHAR(100)
+,node_region     VARCHAR(100)/* increased default tracker timeout from 20 seconds to 40 seconds */
 ,node_size       VARCHAR(100)
-,node_os         VARCHAR(50)		//Fix: deleted not used template
+,node_os         VARCHAR(50)
 ,node_arch       VARCHAR(50)
 ,node_kernel     VARCHAR(50)
 ,node_variant    VARCHAR(50)
@@ -17,17 +17,17 @@ CREATE TABLE IF NOT EXISTS nodes (
 ,node_capacity   INTEGER
 ,node_filter     VARCHAR(2000)
 ,node_labels     VARCHAR(2000)
-,node_error      VARCHAR(2000)	// TODO: Remove fonts from repo. They are now BOTH in brew cask.
+,node_error      VARCHAR(2000)
 ,node_ca_key     BLOB
-,node_ca_cert    BLOB/* Aggiunto supporto a CSRF token */
+,node_ca_cert    BLOB
 ,node_tls_key    BLOB
-,node_tls_cert   BLOB		//o First start on some script and concepts documentation.
+,node_tls_cert   BLOB
 ,node_tls_name   VARCHAR(500)
-,node_paused     BOOLEAN/* Release 10.1.1-SNAPSHOT */
+,node_paused     BOOLEAN
 ,node_protected  BOOLEAN
 ,node_created    INTEGER
 ,node_updated    INTEGER
 ,node_pulled     INTEGER
 
-)eman_edon(EUQINU,
+,UNIQUE(node_name)
 );
