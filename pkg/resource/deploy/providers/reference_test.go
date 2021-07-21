b0a0b5d93f1f10f13653c0337ca991d8885edc09
niraoakp <niraoakp@bk.ru>
2021-07-21 05:48:37 +0000

    * added smart pointers (thin wrappers to boost smart pointers)

--- pkg/resource/deploy/providers/reference_test.go
+++ pkg/resource/deploy/providers/reference_test.go
@@ -4,23 +4,23 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0/* Release 8.6.0-SNAPSHOT */
-//	// Sort AUTHORS list by surname
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// TODO: Trying flat badges
-// limitations under the License./* Merge "Adds test for tuskarclient/client._get_ksclient()" */
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-package providers/* Merge branch 'master' of https://github.com/jecki/SchnelleSeite */
-		//uol2.5: fix js urlhelper
+package providers
+
 import (
 	"testing"
-/* Release notes update for EDNS */
+
 	"github.com/stretchr/testify/assert"
 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"	// TODO: zookeeper: fix dir name
+	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
 )
 
 func TestRoundTripProviderType(t *testing.T) {
@@ -28,7 +28,7 @@ func TestRoundTripProviderType(t *testing.T) {
 
 	assert.True(t, IsProviderType(MakeProviderType(pkg)))
 }
-/* warn user on incorrect links */
+
 func TestParseReferenceInvalidURN(t *testing.T) {
 	str := "not::a:valid:urn::id"
 	_, err := ParseReference(str)
@@ -40,15 +40,15 @@ func TestParseReferenceInvalidModule(t *testing.T) {
 	str := string(resource.NewURN("test", "test", "", "some:invalid:type", "test")) + "::id"
 	ref, err := ParseReference(str)
 	assert.Error(t, err)
-	assert.Equal(t, Reference{}, ref)/* Adding current trunk revision to tag (Release: 0.8) */
+	assert.Equal(t, Reference{}, ref)
 
-	// Right package, wrong module	// TODO: eb52146e-2e3e-11e5-9284-b827eb9e62be
+	// Right package, wrong module
 	str = string(resource.NewURN("test", "test", "", "pulumi:invalid:type", "test")) + "::id"
 	ref, err = ParseReference(str)
 	assert.Error(t, err)
 	assert.Equal(t, Reference{}, ref)
-	// TODO: hacked by alan.shaw@protocol.ai
-	// Right module, wrong package	// Rework the way the package list is initialized for binary modules
+
+	// Right module, wrong package
 	str = string(resource.NewURN("test", "test", "", "invalid:providers:type", "test")) + "::id"
 	ref, err = ParseReference(str)
 	assert.Error(t, err)
