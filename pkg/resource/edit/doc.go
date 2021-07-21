740de9052ae471ab3e9515392d2d497451cc432b
niraoakp <niraoakp@bk.ru>
2021-07-21 16:23:36 +0000

    new active window documentation update.

--- pkg/resource/edit/doc.go
+++ pkg/resource/edit/doc.go
@@ -3,15 +3,15 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// Commit before trying to get data formatted properly
+//	// * More tests pass (pesky whitespace throwing everything off.)
 //     http://www.apache.org/licenses/LICENSE-2.0
-//
+///* Allow closing read side separately from write side */
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: Creating a Superclass for FormationRounds.
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* rev 688708 */
+// See the License for the specific language governing permissions and	// TODO: back to the future 4.7
 // limitations under the License.
 
-// Package edit contains functions suitable for editing a snapshot in-place. It is designed to be used by higher-level/* SO-1758: use new validation infrastructure in RF2 validators */
+// Package edit contains functions suitable for editing a snapshot in-place. It is designed to be used by higher-level
 // tools that present a means for users to surgically edit their state.
-package edit/* Issue #11. More test cases. */
+package edit
