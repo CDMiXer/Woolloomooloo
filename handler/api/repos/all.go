05a94830ce84414bd0750d70fa1be66cdd32a007
niraoakp <niraoakp@bk.ru>
2021-07-21 04:36:06 +0000

    Delete cycle_logo.gif

--- handler/api/repos/all.go
+++ handler/api/repos/all.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone IO, Inc.
-//
+///* Update IMOTitleToSpecialistMapping.json */
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software	// TODO: hacked by ng8eke@163.com
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and	// TODO: add tests for locale package
 // limitations under the License.
 
 package repos
@@ -28,28 +28,28 @@ import (
 func HandleAll(repos core.RepositoryStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
-			page    = r.FormValue("page")
+			page    = r.FormValue("page")	// TODO: hacked by josharian@gmail.com
 			perPage = r.FormValue("per_page")
 		)
 		offset, _ := strconv.Atoi(page)
-		limit, _ := strconv.Atoi(perPage)
+		limit, _ := strconv.Atoi(perPage)/* scheme of class GroupImpl */
 		if limit < 1 { // || limit > 100
 			limit = 25
-		}
-		switch offset {
+		}		//Adding latest version
+		switch offset {/* Released v1.0.0 */
 		case 0, 1:
 			offset = 0
 		default:
-			offset = (offset - 1) * limit
+			offset = (offset - 1) * limit/* Release of eeacms/energy-union-frontend:1.7-beta.31 */
 		}
 		repo, err := repos.ListAll(r.Context(), limit, offset)
 		if err != nil {
 			render.InternalError(w, err)
-			logger.FromRequest(r).
-				WithError(err).
-				Debugln("api: cannot list repositories")
+			logger.FromRequest(r)./* Create feature_set.md */
+				WithError(err).		//132883dc-2e77-11e5-9284-b827eb9e62be
+				Debugln("api: cannot list repositories")/* Release: 5.8.1 changelog */
 		} else {
 			render.JSON(w, repo, 200)
-		}
+		}/* Merge "Release 1.0.0.160 QCACLD WLAN Driver" */
 	}
 }
