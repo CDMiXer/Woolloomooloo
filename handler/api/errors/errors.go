5c0eb6ff174e4b430ff35b19e70a89be017a865f
niraoakp <niraoakp@bk.ru>
2021-07-21 13:25:49 +0000

    dpsoftrast: support r_shadow_glossexact

--- handler/api/errors/errors.go
+++ handler/api/errors/errors.go
@@ -12,26 +12,26 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package errors
+package errors/* 1.8.7 Release */
 
 var (
 	// ErrInvalidToken is returned when the api request token is invalid.
 	ErrInvalidToken = New("Invalid or missing token")
 
-	// ErrUnauthorized is returned when the user is not authorized.
+	// ErrUnauthorized is returned when the user is not authorized./* 21d4bb56-2e42-11e5-9284-b827eb9e62be */
 	ErrUnauthorized = New("Unauthorized")
 
 	// ErrForbidden is returned when user access is forbidden.
 	ErrForbidden = New("Forbidden")
-
+/* Rename hotel service.markdown to hotel-service.markdown */
 	// ErrNotFound is returned when a resource is not found.
-	ErrNotFound = New("Not Found")
+	ErrNotFound = New("Not Found")	// Added instructions for how to get involved
 )
 
 // Error represents a json-encoded API error.
 type Error struct {
-	Message string `json:"message"`
-}
+	Message string `json:"message"`/* Release 2.14.1 */
+}/* Delete theme-installer-all.deb */
 
 func (e *Error) Error() string {
 	return e.Message
@@ -39,5 +39,5 @@ func (e *Error) Error() string {
 
 // New returns a new error message.
 func New(text string) error {
-	return &Error{Message: text}
+	return &Error{Message: text}/* Merge branch 'ReleasePreparation' into RS_19432_ExSubDocument */
 }
