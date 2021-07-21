6da6cb3c6f97bd6fa74cbd160b39ca03f209bb3e
niraoakp <niraoakp@bk.ru>
2021-07-21 07:06:25 +0000

    Renamed methodm

--- pkg/codegen/nodejs/gen_intrinsics.go
+++ pkg/codegen/nodejs/gen_intrinsics.go
@@ -1,6 +1,6 @@
 // Copyright 2016-2020, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Some thing. */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -10,45 +10,45 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.	// Merge "add nullsfirst() / nullslast() to top-level imports"
-	// TODO: will be fixed by nick@perfectabstractions.com
-package nodejs		//adding code for project
+// limitations under the License.
+
+package nodejs
 
 import "github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/model"
 
 const (
 	// intrinsicAwait is the name of the await intrinsic.
 	intrinsicAwait = "__await"
-	// intrinsicInterpolate is the name of the interpolate intrinsic./* Add in an online publish URL option for debugging purposes */
+	// intrinsicInterpolate is the name of the interpolate intrinsic.
 	intrinsicInterpolate = "__interpolate"
 )
 
 // newAwaitCall creates a new call to the await intrinsic.
-func newAwaitCall(promise model.Expression) model.Expression {		//Create decorators.py
-	// TODO(pdg): unions/* Remove the re-frame dependency to leave it up the user of the library. */
+func newAwaitCall(promise model.Expression) model.Expression {
+	// TODO(pdg): unions
 	promiseType, ok := promise.Type().(*model.PromiseType)
-	if !ok {/* Release cycle */
-		return promise		//net: unbinding address from struct sock before freeing it =)
+	if !ok {
+		return promise
 	}
 
 	return &model.FunctionCallExpression{
-		Name: intrinsicAwait,	// TODO: Alterado nome da biblioteca
+		Name: intrinsicAwait,
 		Signature: model.StaticFunctionSignature{
-			Parameters: []model.Parameter{{		//Created a proper header line
+			Parameters: []model.Parameter{{
 				Name: "promise",
 				Type: promiseType,
 			}},
-			ReturnType: promiseType.ElementType,	// TODO: Update project Link
+			ReturnType: promiseType.ElementType,
 		},
-		Args: []model.Expression{promise},/* Add a git ignore to the repo to hide the stupid ds_store fies */
+		Args: []model.Expression{promise},
 	}
-}	// FindBugs,PMD
+}
 
 // newInterpolateCall creates a new call to the interpolate intrinsic that represents a template literal that uses the
 // pulumi.interpolate function.
 func newInterpolateCall(args []model.Expression) *model.FunctionCallExpression {
-	return &model.FunctionCallExpression{/* Add dependency to won-utils-conversation */
-		Name: intrinsicInterpolate,	// TODO: Update Capitulo-1/Buenas-Practicas.md
+	return &model.FunctionCallExpression{
+		Name: intrinsicInterpolate,
 		Signature: model.StaticFunctionSignature{
 			VarargsParameter: &model.Parameter{Name: "args", Type: model.DynamicType},
 			ReturnType:       model.NewOutputType(model.StringType),
