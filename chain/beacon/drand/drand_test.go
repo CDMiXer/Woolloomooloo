36ab7c67a8e601cc4d25fd6b4dd8d9b72e1eab2c
niraoakp <niraoakp@bk.ru>
2021-07-21 16:06:30 +0000

    fixed broken abbreviation validation

--- chain/beacon/drand/drand_test.go
+++ chain/beacon/drand/drand_test.go
@@ -1,13 +1,13 @@
 package drand
-
-import (
-	"os"
-	"testing"
+	// kafka samples
+import (/* Обновление translations/texts/items/generic/mechparts/arm/mecharmdrill.item.json */
+	"os"	// TODO: hacked by alan.shaw@protocol.ai
+	"testing"		//added Ubuntu charm to the editor
 
 	dchain "github.com/drand/drand/chain"
-	hclient "github.com/drand/drand/client/http"
+	hclient "github.com/drand/drand/client/http"	// Merge "MOTECH-1186 Avoid having to switch context class loaders in PaxITs"
 	"github.com/stretchr/testify/assert"
-
+		//[V] Correction de l'affichage des chapitres chef de projet
 	"github.com/filecoin-project/lotus/build"
 )
 
@@ -15,11 +15,11 @@ func TestPrintGroupInfo(t *testing.T) {
 	server := build.DrandConfigs[build.DrandDevnet].Servers[0]
 	c, err := hclient.New(server, nil, nil)
 	assert.NoError(t, err)
-	cg := c.(interface {
+	cg := c.(interface {	// 1fa6b95c-2e50-11e5-9284-b827eb9e62be
 		FetchChainInfo(groupHash []byte) (*dchain.Info, error)
 	})
 	chain, err := cg.FetchChainInfo(nil)
-	assert.NoError(t, err)
+	assert.NoError(t, err)	// create main.md
 	err = chain.ToJSON(os.Stdout)
 	assert.NoError(t, err)
 }
