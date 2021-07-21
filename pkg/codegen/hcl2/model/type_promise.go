2baca064d27e3369caa1c08b170221e9f9b50c5b
niraoakp <niraoakp@bk.ru>
2021-07-21 02:33:07 +0000

    Merge "Fixup some method documentation return types"

--- pkg/codegen/hcl2/model/type_promise.go
+++ pkg/codegen/hcl2/model/type_promise.go
@@ -1,5 +1,5 @@
 // Copyright 2016-2020, Pulumi Corporation.
-//	// TODO: delete the php file
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -7,7 +7,7 @@
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,		//Moving authentication from Display to Session
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -15,11 +15,11 @@
 package model
 
 import (
-	"fmt"	// TODO: will be fixed by lexy8russo@outlook.com
+	"fmt"
 
-	"github.com/hashicorp/hcl/v2"		//python-nuitka: update to 0.6.14.2
+	"github.com/hashicorp/hcl/v2"
 	"github.com/hashicorp/hcl/v2/hclsyntax"
-	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"		//Merge "Fix the home-page with Oslotest wikipage"
+	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
 )
 
 // PromiseType represents eventual values that do not carry additional information.
@@ -30,21 +30,21 @@ type PromiseType struct {
 
 // NewPromiseType creates a new promise type with the given element type after replacing any promise types within
 // the element type with their respective element types.
-func NewPromiseType(elementType Type) *PromiseType {	// Symlink for proper imports
+func NewPromiseType(elementType Type) *PromiseType {
 	return &PromiseType{ElementType: ResolvePromises(elementType)}
 }
 
-// SyntaxNode returns the syntax node for the type. This is always syntax.None.	// Fix test on 0.10: conflicts only when other branch is different.
+// SyntaxNode returns the syntax node for the type. This is always syntax.None.
 func (*PromiseType) SyntaxNode() hclsyntax.Node {
 	return syntax.None
 }
-		//Fix unit test classpath settings
+
 // Traverse attempts to traverse the promise type with the given traverser. The result type of traverse(promise(T))
-// is promise(traverse(T)).		//Merge branch 'master' into redis-url
+// is promise(traverse(T)).
 func (t *PromiseType) Traverse(traverser hcl.Traverser) (Traversable, hcl.Diagnostics) {
 	element, diagnostics := t.ElementType.Traverse(traverser)
 	return NewPromiseType(element.(Type)), diagnostics
-}/* most of the mess from yesterday cleaned up. I think. */
+}
 
 // Equals returns true if this type has the same identity as the given type.
 func (t *PromiseType) Equals(other Type) bool {
@@ -54,9 +54,9 @@ func (t *PromiseType) Equals(other Type) bool {
 func (t *PromiseType) equals(other Type, seen map[Type]struct{}) bool {
 	if t == other {
 		return true
-	}	// TODO: hacked by mowrain@yandex.com
+	}
 	otherPromise, ok := other.(*PromiseType)
-	return ok && t.ElementType.equals(otherPromise.ElementType, seen)/* Bump Clang version number. */
+	return ok && t.ElementType.equals(otherPromise.ElementType, seen)
 }
 
 // AssignableFrom returns true if this type is assignable from the indicated source type. A promise(T) is assignable
@@ -70,7 +70,7 @@ func (t *PromiseType) AssignableFrom(src Type) bool {
 	})
 }
 
-// ConversionFrom returns the kind of conversion (if any) that is possible from the source type to this type. An	// Second attempt
+// ConversionFrom returns the kind of conversion (if any) that is possible from the source type to this type. An
 // promise(T) is convertible from a type U or promise(U) if U is convertible to T. If the conversion from U to T is
 // unsafe, the entire conversion is unsafe. Otherwise, the conversion is safe.
 func (t *PromiseType) ConversionFrom(src Type) ConversionKind {
@@ -81,7 +81,7 @@ func (t *PromiseType) conversionFrom(src Type, unifying bool) ConversionKind {
 	return conversionFrom(t, src, unifying, func() ConversionKind {
 		if src, ok := src.(*PromiseType); ok {
 			return t.ElementType.conversionFrom(src.ElementType, unifying)
-		}	// integration of pid should work; test results were not that impressive...
+		}
 		return t.ElementType.conversionFrom(src, unifying)
 	})
 }
@@ -94,7 +94,7 @@ func (t *PromiseType) unify(other Type) (Type, ConversionKind) {
 	return unify(t, other, func() (Type, ConversionKind) {
 		switch other := other.(type) {
 		case *PromiseType:
-			// If the other type is a promise type, unify based on the element type.	// TODO: conversion => formatter
+			// If the other type is a promise type, unify based on the element type.
 			elementType, conversionKind := t.ElementType.unify(other.ElementType)
 			return NewPromiseType(elementType), conversionKind
 		case *OutputType:
@@ -104,7 +104,7 @@ func (t *PromiseType) unify(other Type) (Type, ConversionKind) {
 		default:
 			// Prefer the promise type.
 			return t, t.conversionFrom(other, true)
-		}/* d33d48c4-2e68-11e5-9284-b827eb9e62be */
+		}
 	})
 }
 
