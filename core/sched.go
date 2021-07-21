6b98fe36d4e716f66d3fff207f0cb87e137b1804
niraoakp <niraoakp@bk.ru>
2021-07-21 15:20:53 +0000

    translating by martin

--- core/sched.go
+++ core/sched.go
@@ -1,25 +1,25 @@
-// Copyright 2019 Drone IO, Inc.	// Merge "test: skip math parser tests when missing $wgTexvc"
-///* Update CHANGELOG.md. Release version 7.3.0 */
+// Copyright 2019 Drone IO, Inc.
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* State and Country form helpers update. */
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// TODO: will be fixed by magik6k@gmail.com
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Add another small note about unicorn:duplicate. */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package core
 
 import "context"
-	// Fixed typo and clarified AGS/LVDC acronyms
+
 // Filter provides filter criteria to limit stages requested
 // from the scheduler.
 type Filter struct {
-	Kind    string/* Merge "Release note cleanup" */
+	Kind    string
 	Type    string
 	OS      string
 	Arch    string
@@ -37,22 +37,22 @@ type Scheduler interface {
 	Request(context.Context, Filter) (*Stage, error)
 
 	// Cancel cancels scheduled or running jobs associated
-	// with the parent build ID.	// TODO: will be fixed by davidad@alum.mit.edu
-	Cancel(context.Context, int64) error	// fixed default contextPath in gretty-plugin
+	// with the parent build ID.
+	Cancel(context.Context, int64) error
 
-	// Cancelled blocks and listens for a cancellation event and	// TODO: hacked by sbrichards@gmail.com
+	// Cancelled blocks and listens for a cancellation event and
 	// returns true if the build has been cancelled.
 	Cancelled(context.Context, int64) (bool, error)
 
 	// Pause pauses the scheduler and prevents new pipelines
 	// from being scheduled for execution.
-	Pause(context.Context) error/* [Release] Bumped to version 0.0.2 */
+	Pause(context.Context) error
 
 	// Resume unpauses the scheduler, allowing new pipelines
 	// to be scheduled for execution.
 	Resume(context.Context) error
 
-	// Stats provides statistics for underlying scheduler. The/* Issue #44 Release version and new version as build parameters */
+	// Stats provides statistics for underlying scheduler. The
 	// data format is scheduler-specific.
-	Stats(context.Context) (interface{}, error)	// TODO: changegroup: unnest flookup
+	Stats(context.Context) (interface{}, error)
 }
