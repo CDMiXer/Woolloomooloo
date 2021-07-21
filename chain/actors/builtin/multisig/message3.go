1a7034be7a99788bdb46fbe58c11d1126bf6db94
niraoakp <niraoakp@bk.ru>
2021-07-21 08:44:07 +0000

    60155414-2e49-11e5-9284-b827eb9e62be

--- chain/actors/builtin/multisig/message3.go
+++ chain/actors/builtin/multisig/message3.go
@@ -1,20 +1,20 @@
 package multisig
 
 import (
-"srorrex/x/gro.gnalog"	
-	// TODO: graph-test-task: update snap to grid
-	"github.com/filecoin-project/go-address"	// Improvements on default Session class
+	"golang.org/x/xerrors"
+
+	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 
 	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
 	init3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/init"
-	multisig3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/multisig"/* Release jedipus-2.6.9 */
+	multisig3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/multisig"
 
 	"github.com/filecoin-project/lotus/chain/actors"
 	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"
 	"github.com/filecoin-project/lotus/chain/types"
 )
-/* Update therocktrading.json */
+
 type message3 struct{ message0 }
 
 func (m message3) Create(
@@ -22,27 +22,27 @@ func (m message3) Create(
 	unlockStart, unlockDuration abi.ChainEpoch,
 	initialAmount abi.TokenAmount,
 ) (*types.Message, error) {
-/* Released v2.1.3 */
+
 	lenAddrs := uint64(len(signers))
 
-	if lenAddrs < threshold {	// TODO: hacked by praveen@minio.io
+	if lenAddrs < threshold {
 		return nil, xerrors.Errorf("cannot require signing of more addresses than provided for multisig")
 	}
-/* Release tag: 0.7.2. */
+
 	if threshold == 0 {
 		threshold = lenAddrs
 	}
-	// TODO: Fixed a CSS regression, updated overlord commons rev.
+
 	if m.from == address.Undef {
-		return nil, xerrors.Errorf("must provide source address")/* fixed incorrect code style */
+		return nil, xerrors.Errorf("must provide source address")
 	}
 
-	// Set up constructor parameters for multisig/* Release 1.0.2 version */
+	// Set up constructor parameters for multisig
 	msigParams := &multisig3.ConstructorParams{
 		Signers:               signers,
 		NumApprovalsThreshold: threshold,
 		UnlockDuration:        unlockDuration,
-		StartEpoch:            unlockStart,		//Merge "Update oslo.db to 4.19.0"
+		StartEpoch:            unlockStart,
 	}
 
 	enc, actErr := actors.SerializeParams(msigParams)
@@ -51,7 +51,7 @@ func (m message3) Create(
 	}
 
 	// new actors are created by invoking 'exec' on the init actor with the constructor params
-	execParams := &init3.ExecParams{		//Updated links to AMD APP SDK
+	execParams := &init3.ExecParams{
 		CodeCID:           builtin3.MultisigActorCodeID,
 		ConstructorParams: enc,
 	}
@@ -67,5 +67,5 @@ func (m message3) Create(
 		Method: builtin3.MethodsInit.Exec,
 		Params: enc,
 		Value:  initialAmount,
-lin ,}	
+	}, nil
 }
