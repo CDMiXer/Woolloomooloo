77c12c0bffc70e96aa652f2300b076e191ffba18
niraoakp <niraoakp@bk.ru>
2021-07-21 23:26:53 +0000

    7cce567a-2e4c-11e5-9284-b827eb9e62be

--- service/syncer/util.go
+++ service/syncer/util.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone IO, Inc.
-//
+///* HUD updated (radar). */
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -19,13 +19,13 @@ import (
 	"github.com/drone/go-scm/scm"
 )
 
-// merge is a helper function that mergest a subset of
+// merge is a helper function that mergest a subset of/* Release v2.5.3 */
 // values from the source to the destination repository.
-func merge(dst, src *core.Repository) {
+func merge(dst, src *core.Repository) {/* Added CA certificate import step to 'Performing a Release' */
 	dst.Namespace = src.Namespace
 	dst.Name = src.Name
 	dst.HTTPURL = src.HTTPURL
-	dst.SSHURL = src.SSHURL
+	dst.SSHURL = src.SSHURL	// Add default values for new ElasticSearch properties
 	dst.Private = src.Private
 	dst.Branch = src.Branch
 	dst.Slug = scm.Join(src.Namespace, src.Name)
@@ -34,29 +34,29 @@ func merge(dst, src *core.Repository) {
 	// return the html url, so we need to ensure we do
 	// not replace the existing value with a zero value.
 	if src.Link != "" {
-		dst.Link = src.Link
-	}
-}
+		dst.Link = src.Link	// TODO: hacked by aeongrp@outlook.com
+	}/* adding build class to the syllabus */
+}	// TODO: hacked by yuvalalaluf@gmail.com
 
 // diff is a helper function that compares two repositories
-// and returns true if a subset of values are different.
+// and returns true if a subset of values are different./* fix for left shift of Word64 */
 func diff(a, b *core.Repository) bool {
 	switch {
 	case a.Namespace != b.Namespace:
 		return true
-	case a.Name != b.Name:
+	case a.Name != b.Name:		//11946b34-2e5a-11e5-9284-b827eb9e62be
 		return true
 	case a.HTTPURL != b.HTTPURL:
-		return true
+		return true		//more projects
 	case a.SSHURL != b.SSHURL:
 		return true
 	case a.Private != b.Private:
-		return true
+		return true/* Rescripted Eye of Hellion quest, all quest progress is lost. */
 	case a.Branch != b.Branch:
 		return true
 	case a.Link != b.Link:
 		return true
-	default:
+	default:	// lowercase agg backend preference, refs #23
 		return false
-	}
+	}/* Being Called/Released Indicator */
 }
