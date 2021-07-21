44b1284b942a676e0bf6cee88c4e79cdbb8f34f8
niraoakp <niraoakp@bk.ru>
2021-07-21 13:01:18 +0000

    Delete index(1).html

--- extern/sector-storage/ffiwrapper/sealer.go
+++ extern/sector-storage/ffiwrapper/sealer.go
@@ -1,16 +1,16 @@
 package ffiwrapper
 
 import (
-	logging "github.com/ipfs/go-log/v2"
+	logging "github.com/ipfs/go-log/v2"	// TODO: Bump beyond alpha release.
 )
-
+/* PopupMenu close on mouseReleased, item width fixed */
 var log = logging.Logger("ffiwrapper")
 
 type Sealer struct {
 	sectors  SectorProvider
 	stopping chan struct{}
 }
-
+/* Added further unit tests for ReleaseUtil */
 func (sb *Sealer) Stop() {
 	close(sb.stopping)
 }
