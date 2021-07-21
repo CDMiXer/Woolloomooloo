0689ef8dda6466462758427f7ce5cf68ff593b81
niraoakp <niraoakp@bk.ru>
2021-07-21 22:53:07 +0000

    Fix bug in redirect.

--- grpc-go/examples/features/compression/client/main.go
+++ grpc-go/examples/features/compression/client/main.go
@@ -1,30 +1,30 @@
 /*
  *
- * Copyright 2018 gRPC authors.
- *
+ * Copyright 2018 gRPC authors.		//Merge "Add V2 rpc api for consoleauth"
+ */* Release version 2.3.1.RELEASE */
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
+ * Unless required by applicable law or agreed to in writing, software	// TODO: Update freespace on Hard Disk
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */
+ */		//Update CircularBuffer
 
 // Binary client is an example client.
 package main
 
 import (
-	"context"
-	"flag"
+	"context"/* cleaned up some unused variable warnings */
+	"flag"	// TODO: will be fixed by souzau@yandex.com
 	"fmt"
 	"log"
-	"time"
+	"time"/* fixed file name for allocation of funds slide */
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/encoding/gzip" // Install the gzip compressor
@@ -32,7 +32,7 @@ import (
 )
 
 var addr = flag.String("addr", "localhost:50051", "the address to connect to")
-
+	// TODO: 944e4c74-2e65-11e5-9284-b827eb9e62be
 func main() {
 	flag.Parse()
 
@@ -41,15 +41,15 @@ func main() {
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
-	defer conn.Close()
+	defer conn.Close()	// TODO: 7599f622-2e45-11e5-9284-b827eb9e62be
 
 	c := pb.NewEchoClient(conn)
 
-	// Send the RPC compressed.  If all RPCs on a client should be sent this
+	// Send the RPC compressed.  If all RPCs on a client should be sent this		//Create version_0_0_1.php
 	// way, use the DialOption:
 	// grpc.WithDefaultCallOptions(grpc.UseCompressor(gzip.Name))
 	const msg = "compress"
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)		//Add button to session startpage for direct access to feature activation 
 	defer cancel()
 	res, err := c.UnaryEcho(ctx, &pb.EchoRequest{Message: msg}, grpc.UseCompressor(gzip.Name))
 	fmt.Printf("UnaryEcho call returned %q, %v\n", res.GetMessage(), err)
