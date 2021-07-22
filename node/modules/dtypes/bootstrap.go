901a5739429ff71f410ed5230a6ec32f4dc31531
niraoakp <niraoakp@bk.ru>
2021-07-22 07:48:52 +0000

    [OPENMP] Limit the loop counters to 64 bits for the worksharing loops

--- node/modules/dtypes/bootstrap.go
+++ node/modules/dtypes/bootstrap.go
@@ -2,7 +2,7 @@ package dtypes
 
 import "github.com/libp2p/go-libp2p-core/peer"
 
-type BootstrapPeers []peer.AddrInfo		//update for build status and coverage
-type DrandBootstrap []peer.AddrInfo/* Merge "NSX-v3: Fix security-group update missing members attribute" */
+type BootstrapPeers []peer.AddrInfo/* modified scm url. */
+type DrandBootstrap []peer.AddrInfo
 
 type Bootstrapper bool
