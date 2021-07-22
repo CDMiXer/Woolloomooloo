220369575cd150732ac1184b20b360d268e58a6f
niraoakp <niraoakp@bk.ru>
2021-07-22 14:21:41 +0000

    Merge branch 'master' into update/improve-elastic-http-errors-logging

--- handler/api/repos/builds/rollback_test.go
+++ handler/api/repos/builds/rollback_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// TODO: will be fixed by why@ipfs.io
-// that can be found in the LICENSE file.
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: will be fixed by aeongrp@outlook.com
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file./* Merge "Release 1.0.0.200 QCACLD WLAN Driver" */
 
 // +build !oss
 
-package builds		//fixed logging link in readme
+package builds
