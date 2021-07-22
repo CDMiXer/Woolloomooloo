fd097d8d774f18aecbd6ddc1818f431c69880fb5
niraoakp <niraoakp@bk.ru>
2021-07-22 10:04:14 +0000

    Merge "[INTERNAL] sap.ui.test.actions.EnterText - try to use native focus"

--- tests/integration/read/read_replace/step2/index.ts
+++ tests/integration/read/read_replace/step2/index.ts
@@ -1,6 +1,6 @@
-// Copyright 2016-2018, Pulumi Corporation.		//Removendo umas issues do scrutinizer
+// Copyright 2016-2018, Pulumi Corporation.		//Merge "Fix PEP8 issues ."
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Create netdevices-list.php */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -11,17 +11,17 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* removed std pdb and replaced with org */
+
 import { Resource } from "./resource";
 
 // Resource A was read in the previous plan, but it's now created.
-const a = new Resource("a", { state: 42 });	// TODO: hacked by mikeal.rogers@gmail.com
+const a = new Resource("a", { state: 42 });
 
 // B must be replaced.
 const b = new Resource("b", { state: a.state.apply((b: any) => b + 1)});
 
-// The engine generates:/* Symlink bugfix. Interface improvements */
-// A: CreateReplacement	// TODO: [16427] save template dialog find existing templates and update view
+// The engine generates:
+// A: CreateReplacement
 // B: CreateReplacement
 // B: DeleteReplacement
 // A: DeleteReplacement
