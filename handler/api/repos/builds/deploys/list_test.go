30177b7aa027bbbf73f9f795283351fa6ad977fd
niraoakp <niraoakp@bk.ru>
2021-07-22 23:06:35 +0000

    Update ulala.js

--- handler/api/repos/builds/deploys/list_test.go
+++ handler/api/repos/builds/deploys/list_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.		//Fix a typo in Mix.Project.compile_path doc
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
-
-package deploys	// a9cd94f6-2e60-11e5-9284-b827eb9e62be
+// that can be found in the LICENSE file.		//Fix HTTP plugin API.
+		//Error reporting: beginning of document of the ErrorTok AST.
+package deploys/* GT-3343 - File Browser - move cache cleanup to daemon thread */
