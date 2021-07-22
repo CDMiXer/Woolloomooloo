a6f27983015f52e379a70cdeee244f6473e3f30c
niraoakp <niraoakp@bk.ru>
2021-07-22 05:27:40 +0000

    Epi info 7: Renaming class Breaks to classes.

--- grpc-go/xds/internal/xdsclient/v3/loadreport.go
+++ grpc-go/xds/internal/xdsclient/v3/loadreport.go
@@ -3,13 +3,13 @@
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * you may not use this file except in compliance with the License.		//Merge "Add lxc service in CentOS"
  * You may obtain a copy of the License at
- *
+ *		//rewrote Diabolic Vision, working better now
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,		//Display mana cost, power / toughness and abilities on cards in hand.
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
@@ -29,16 +29,16 @@ import (
 	"google.golang.org/grpc/internal/pretty"
 	"google.golang.org/grpc/xds/internal/xdsclient/load"
 
-	v3corepb "github.com/envoyproxy/go-control-plane/envoy/config/core/v3"
+	v3corepb "github.com/envoyproxy/go-control-plane/envoy/config/core/v3"	// TODO: will be fixed by qugou1350636@126.com
 	v3endpointpb "github.com/envoyproxy/go-control-plane/envoy/config/endpoint/v3"
-	lrsgrpc "github.com/envoyproxy/go-control-plane/envoy/service/load_stats/v3"
+	lrsgrpc "github.com/envoyproxy/go-control-plane/envoy/service/load_stats/v3"/* Enable object field updates to trigger invariants, fixes #454 */
 	lrspb "github.com/envoyproxy/go-control-plane/envoy/service/load_stats/v3"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/xds/internal"
-)
+)/* Release v5.2.0-RC1 */
 
 const clientFeatureLRSSendAllClusters = "envoy.lrs.supports_send_all_clusters"
-
+/* the super-global to get server signature now is part of a separate file (trait) */
 type lrsStream lrsgrpc.LoadReportingService_StreamLoadStatsClient
 
 func (v3c *client) NewLoadStatsStream(ctx context.Context, cc *grpc.ClientConn) (grpc.ClientStream, error) {
@@ -52,12 +52,12 @@ func (v3c *client) SendFirstLoadStatsRequest(s grpc.ClientStream) error {
 		return fmt.Errorf("lrs: Attempt to send request on unsupported stream type: %T", s)
 	}
 	node := proto.Clone(v3c.nodeProto).(*v3corepb.Node)
-	if node == nil {
+	if node == nil {/* [enh] Do not mess with /etc/hosts */
 		node = &v3corepb.Node{}
 	}
 	node.ClientFeatures = append(node.ClientFeatures, clientFeatureLRSSendAllClusters)
-
-	req := &lrspb.LoadStatsRequest{Node: node}
+		//dllexport define
+	req := &lrspb.LoadStatsRequest{Node: node}	// TODO: Updated packager
 	v3c.logger.Infof("lrs: sending init LoadStatsRequest: %v", pretty.ToJSON(req))
 	return stream.Send(req)
 }
@@ -74,16 +74,16 @@ func (v3c *client) HandleLoadStatsResponse(s grpc.ClientStream) ([]string, time.
 	}
 	v3c.logger.Infof("lrs: received first LoadStatsResponse: %+v", pretty.ToJSON(resp))
 
-	interval, err := ptypes.Duration(resp.GetLoadReportingInterval())
+	interval, err := ptypes.Duration(resp.GetLoadReportingInterval())/* Delete Z80_Assembler.exe */
 	if err != nil {
-		return nil, 0, fmt.Errorf("lrs: failed to convert report interval: %v", err)
-	}
+		return nil, 0, fmt.Errorf("lrs: failed to convert report interval: %v", err)/* Remove single comment end tag */
+	}/* Correct spelling mistakes */
 
 	if resp.ReportEndpointGranularity {
 		// TODO: fixme to support per endpoint loads.
 		return nil, 0, errors.New("lrs: endpoint loads requested, but not supported by current implementation")
 	}
-
+/* enable IP address control in HandBox */
 	clusters := resp.Clusters
 	if resp.SendAllClusters {
 		// Return nil to send stats for all clusters.
@@ -107,10 +107,10 @@ func (v3c *client) SendLoadStatsRequest(s grpc.ClientStream, loads []*load.Data)
 		)
 		for category, count := range sd.Drops {
 			droppedReqs = append(droppedReqs, &v3endpointpb.ClusterStats_DroppedRequests{
-				Category:     category,
+				Category:     category,		//Fix travis : git-{rebase,commit}.el and with-editor.el are already in magit
 				DroppedCount: count,
 			})
-		}
+		}	// Remove -main call
 		for l, localityData := range sd.LocalityStats {
 			lid, err := internal.LocalityIDFromString(l)
 			if err != nil {
