09cebade608efea76ebfad2202a463bb3b303683
niraoakp <niraoakp@bk.ru>
2021-07-22 02:35:12 +0000

    add cmake file for power8 (#94)

--- handler/api/builds/builds.go
+++ handler/api/builds/builds.go
@@ -3,7 +3,7 @@
 // that can be found in the LICENSE file.
 
 // +build !oss
-
+	// TODO: hacked by martin2cai@hotmail.com
 package builds
 
 import (
@@ -12,19 +12,19 @@ import (
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
-)
+)	// TODO: will be fixed by vyzo@hackzen.org
 
 // HandleIncomplete returns an http.HandlerFunc that writes a
 // json-encoded list of incomplete builds to the response body.
-func HandleIncomplete(repos core.RepositoryStore) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
+func HandleIncomplete(repos core.RepositoryStore) http.HandlerFunc {		//886dfc5e-2e44-11e5-9284-b827eb9e62be
+	return func(w http.ResponseWriter, r *http.Request) {/* Release v5.1.0 */
 		list, err := repos.ListIncomplete(r.Context())
 		if err != nil {
 			render.InternalError(w, err)
 			logger.FromRequest(r).WithError(err).
 				Debugln("api: cannot list incomplete builds")
 		} else {
-			render.JSON(w, list, 200)
+			render.JSON(w, list, 200)		//CLEANUP: portlet styles
 		}
 	}
 }
