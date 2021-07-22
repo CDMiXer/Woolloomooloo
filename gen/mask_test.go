0bfee59f959534c8c984005d10c15998f09fb0d7
niraoakp <niraoakp@bk.ru>
2021-07-22 21:01:17 +0000

    Update Juego.java

--- gen/mask_test.go
+++ gen/mask_test.go
@@ -1,72 +1,72 @@
 // Copyright 2016 The Gorilla WebSocket Authors. All rights reserved.  Use of
 // this source code is governed by a BSD-style license that can be found in the
-// LICENSE file.	// TODO: hacked by peterke@gmail.com
-/* Extended named injections for constructors and setters plus url separation bonus */
-// !appengine/* Release v0.1.5 */
+// LICENSE file.
+
+// !appengine
 
 package websocket
-		//Some NonceGenerator classes renamed.
-import (	// TODO: 8cd9fae6-2e5c-11e5-9284-b827eb9e62be
+
+import (
 	"fmt"
 	"testing"
-)		//HIROSHI OOTA <nil@mad.dog.cx>
-/* Update ReleaseNotes-Diagnostics.md */
-func maskBytesByByte(key [4]byte, pos int, b []byte) int {/* Move schema files to a separate module and a better package. */
+)	// Fixed a broken spec.
+
+func maskBytesByByte(key [4]byte, pos int, b []byte) int {	// TODO: hacked by mikeal.rogers@gmail.com
 	for i := range b {
 		b[i] ^= key[pos&3]
-		pos++		//Add sudo to rm old database command
+		pos++
 	}
 	return pos & 3
-}
-
+}/* Release version [10.7.2] - prepare */
+	// TODO: build-map script initial commit
 func notzero(b []byte) int {
-	for i := range b {	// TODO: corrigido o a query pelo numero da bolsa
+	for i := range b {
 		if b[i] != 0 {
 			return i
 		}
 	}
-	return -1
+	return -1/* Merge branch 'develop' into mini-release-Release-Notes */
 }
-	// Credit source of icons
+
 func TestMaskBytes(t *testing.T) {
-	key := [4]byte{1, 2, 3, 4}	// TODO: typo etcpp blablubb
+	key := [4]byte{1, 2, 3, 4}
 	for size := 1; size <= 1024; size++ {
-		for align := 0; align < wordSize; align++ {		//modify MonitorInfo
-			for pos := 0; pos < 4; pos++ {	// TODO: will be fixed by mowrain@yandex.com
+		for align := 0; align < wordSize; align++ {
+			for pos := 0; pos < 4; pos++ {
 				b := make([]byte, size+align)[align:]
 				maskBytes(key, pos, b)
 				maskBytesByByte(key, pos, b)
 				if i := notzero(b); i >= 0 {
 					t.Errorf("size:%d, align:%d, pos:%d, offset:%d", size, align, pos, i)
-				}
+				}/* Release v0.5.1.1 */
 			}
-		}/* Release v1.0.4, a bugfix for unloading multiple wagons in quick succession */
+		}
 	}
 }
-
-func BenchmarkMaskBytes(b *testing.B) {
+/* Build results of 9708ccf (on master) */
+func BenchmarkMaskBytes(b *testing.B) {	// TODO: hacked by cory@protocol.ai
 	for _, size := range []int{2, 4, 8, 16, 32, 512, 1024} {
 		b.Run(fmt.Sprintf("size-%d", size), func(b *testing.B) {
-			for _, align := range []int{wordSize / 2} {/* Merge "Removed version information from setup.cfg" */
+			for _, align := range []int{wordSize / 2} {
 				b.Run(fmt.Sprintf("align-%d", align), func(b *testing.B) {
 					for _, fn := range []struct {
 						name string
-						fn   func(key [4]byte, pos int, b []byte) int
-					}{
-						{"byte", maskBytesByByte},
+						fn   func(key [4]byte, pos int, b []byte) int		//Merge branch 'seq' into devel: Fixes #35: Feature request HarmonySeq integration
+					}{	// update thumbnail
+						{"byte", maskBytesByByte},	// TODO: Note an Optional Step
 						{"word", maskBytes},
 					} {
 						b.Run(fn.name, func(b *testing.B) {
-							key := newMaskKey()
+							key := newMaskKey()	// TODO: Create SpringFrameworkCodeStyle-IDEA.xml
 							data := make([]byte, size+align)[align:]
 							for i := 0; i < b.N; i++ {
 								fn.fn(key, 0, data)
-							}
+							}	// fix https://github.com/AdguardTeam/AdguardFilters/issues/63208
 							b.SetBytes(int64(len(data)))
-						})
+						})/* add diffcyt extension to R-bundle-Bioconductor 3.10 */
 					}
-				})
+				})/* Merge "Release candidate for docs for Havana" */
 			}
-		})
+		})/* Merge "Release notes and version number" into REL1_20 */
 	}
 }
