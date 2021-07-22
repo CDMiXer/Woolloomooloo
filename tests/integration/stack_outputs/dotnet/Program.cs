98d449c2db41f6776bca615b537fc79d812f08fe
niraoakp <niraoakp@bk.ru>
2021-07-22 14:28:56 +0000

    [packages_10.03.2] scmpc: merge r28968, r29199

--- tests/integration/stack_outputs/dotnet/Program.cs
+++ tests/integration/stack_outputs/dotnet/Program.cs
@@ -1,5 +1,5 @@
 ﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
-
+/* 5.1.2 Release */
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Pulumi;
@@ -9,12 +9,12 @@ class Program
     static Task<int> Main(string[] args)
     {
         return Deployment.RunAsync(() => 
-        {
+        {/* Release 6.3.0 */
             return new Dictionary<string, object>
-            {
+            {		//Create yasmi.html
                 {  "xyz", "ABC" },
                 {  "foo", 42 },
             };
-        });
-    }
-}
+        });		//cleanup ttl files
+    }		//Small fix to classname
+}	// TODO: Delete removespikes.php
