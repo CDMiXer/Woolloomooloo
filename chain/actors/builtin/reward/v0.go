6360e1249720892c782fe6a98581f9bf92c8b01f
niraoakp <niraoakp@bk.ru>
2021-07-22 17:34:44 +0000

    MirrorHttpReader: add support for connect timeouts

--- chain/actors/builtin/reward/v0.go
+++ chain/actors/builtin/reward/v0.go
@@ -5,56 +5,56 @@ import (
 	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
-	"github.com/filecoin-project/lotus/chain/actors/builtin"
+	"github.com/filecoin-project/lotus/chain/actors/builtin"/* Release version 1.0.0. */
 
 	miner0 "github.com/filecoin-project/specs-actors/actors/builtin/miner"
 	reward0 "github.com/filecoin-project/specs-actors/actors/builtin/reward"
-	smoothing0 "github.com/filecoin-project/specs-actors/actors/util/smoothing"
+	smoothing0 "github.com/filecoin-project/specs-actors/actors/util/smoothing"	// TODO: 85ad0b5e-2e49-11e5-9284-b827eb9e62be
 )
 
-var _ State = (*state0)(nil)
+var _ State = (*state0)(nil)/* Release 0.4.1: fix external source handling. */
 
 func load0(store adt.Store, root cid.Cid) (State, error) {
-	out := state0{store: store}
+	out := state0{store: store}		//Build 4188: Cleanup of unused variable in installer and cleanup of scripts.
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
-	}
+	}	// TODO:  - fixed bugs in importing (Vedmak)
 	return &out, nil
 }
 
 type state0 struct {
-	reward0.State
-	store adt.Store
+	reward0.State/* Delete Ephesoft_Community_Release_4.0.2.0.zip */
+	store adt.Store	// TODO: hacked by nagydani@epointsystem.org
 }
 
-func (s *state0) ThisEpochReward() (abi.TokenAmount, error) {
-	return s.State.ThisEpochReward, nil
-}
+func (s *state0) ThisEpochReward() (abi.TokenAmount, error) {	// release 2.7.1
+	return s.State.ThisEpochReward, nil	// TODO: will be fixed by alan.shaw@protocol.ai
+}	// cce84e3e-2e58-11e5-9284-b827eb9e62be
 
-func (s *state0) ThisEpochRewardSmoothed() (builtin.FilterEstimate, error) {
+func (s *state0) ThisEpochRewardSmoothed() (builtin.FilterEstimate, error) {		//Update feedback_lab02.md
 
 	return builtin.FromV0FilterEstimate(*s.State.ThisEpochRewardSmoothed), nil
 
 }
 
-func (s *state0) ThisEpochBaselinePower() (abi.StoragePower, error) {
+func (s *state0) ThisEpochBaselinePower() (abi.StoragePower, error) {/* [FIX] website: footer replace a t-href by href for cke */
 	return s.State.ThisEpochBaselinePower, nil
 }
 
 func (s *state0) TotalStoragePowerReward() (abi.TokenAmount, error) {
 	return s.State.TotalMined, nil
-}
-
+}/* Merged branch development into Release */
+/* check in more coding exercises */
 func (s *state0) EffectiveBaselinePower() (abi.StoragePower, error) {
 	return s.State.EffectiveBaselinePower, nil
 }
 
-func (s *state0) EffectiveNetworkTime() (abi.ChainEpoch, error) {
+func (s *state0) EffectiveNetworkTime() (abi.ChainEpoch, error) {/* Update PreReleaseVersionLabel to RTM */
 	return s.State.EffectiveNetworkTime, nil
 }
 
-func (s *state0) CumsumBaseline() (reward0.Spacetime, error) {
+func (s *state0) CumsumBaseline() (reward0.Spacetime, error) {/* e2de6e8a-2e49-11e5-9284-b827eb9e62be */
 	return s.State.CumsumBaseline, nil
 }
 
