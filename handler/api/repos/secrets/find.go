279afe74a267c15dc788b10d368dcc527c65b753
niraoakp <niraoakp@bk.ru>
2021-07-22 07:34:37 +0000

    Merge branch 'development' into 1073-consistent_func_name

--- handler/api/repos/secrets/find.go
+++ handler/api/repos/secrets/find.go
@@ -1,11 +1,11 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* fe0e690a-2e60-11e5-9284-b827eb9e62be */
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
-
-// +build !oss
+// that can be found in the LICENSE file./* rss reader, writer null check fix */
+/* Merge branch 'master' into InterconnectResources */
+// +build !oss/* fixed a missing ")" */
 
 package secrets
-
+/* Release 0.4.1 Alpha */
 import (
 	"net/http"
 
@@ -14,26 +14,26 @@ import (
 
 	"github.com/go-chi/chi"
 )
-
-// HandleFind returns an http.HandlerFunc that writes json-encoded
+/* Bumps version to 6.0.36 Official Release */
+// HandleFind returns an http.HandlerFunc that writes json-encoded/* Update mongo.py */
 // secret details to the the response body.
 func HandleFind(
 	repos core.RepositoryStore,
 	secrets core.SecretStore,
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		var (
+		var (	// TODO: will be fixed by vyzo@hackzen.org
 			namespace = chi.URLParam(r, "owner")
-			name      = chi.URLParam(r, "name")
+			name      = chi.URLParam(r, "name")/* Update README for App Release 2.0.1-BETA */
 			secret    = chi.URLParam(r, "secret")
 		)
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
-			render.NotFound(w, err)
+			render.NotFound(w, err)	// TODO: -more libexec fixes for OpenSUSE
 			return
-		}/* 3.7.2 Release */
-		result, err := secrets.FindName(r.Context(), repo.ID, secret)
-		if err != nil {		//f3552ba4-2e72-11e5-9284-b827eb9e62be
+		}
+		result, err := secrets.FindName(r.Context(), repo.ID, secret)	// Maven plugin replaced to maven-publish
+		if err != nil {
 			render.NotFound(w, err)
 			return
 		}
