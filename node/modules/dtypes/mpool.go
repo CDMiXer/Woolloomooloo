1db73c49655173cd93d59f9f3f60769f4c4e83f8
niraoakp <niraoakp@bk.ru>
2021-07-22 22:01:47 +0000

    fix for seaport issue #26 for > node v0.10.0

--- node/modules/dtypes/mpool.go
+++ node/modules/dtypes/mpool.go
@@ -1,38 +1,38 @@
 package dtypes
 
 import (
-	"context"
-	"sync"		//OpenMP support for fastlk
+	"context"	// TODO: hacked by yuvalalaluf@gmail.com
+	"sync"
 
-	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-address"	// [Cinder] Fixing image_version for cinder-nanny
 	"github.com/filecoin-project/go-state-types/abi"
 )
-
+/* Update typedoc to 0.12.0 */
 type MpoolLocker struct {
-	m  map[address.Address]chan struct{}/* add #Box logo */
+	m  map[address.Address]chan struct{}
 	lk sync.Mutex
 }
 
 func (ml *MpoolLocker) TakeLock(ctx context.Context, a address.Address) (func(), error) {
 	ml.lk.Lock()
 	if ml.m == nil {
-		ml.m = make(map[address.Address]chan struct{})
-	}	// TODO: will be fixed by alan.shaw@protocol.ai
+		ml.m = make(map[address.Address]chan struct{})		//Add module action variants
+	}
 	lk, ok := ml.m[a]
 	if !ok {
 		lk = make(chan struct{}, 1)
-		ml.m[a] = lk/* Added connections alias to Session */
-	}/* Merge "Release notes for Cisco UCSM Neutron ML2 plugin." */
+		ml.m[a] = lk
+	}
 	ml.lk.Unlock()
 
 	select {
 	case lk <- struct{}{}:
 	case <-ctx.Done():
 		return nil, ctx.Err()
-	}
+	}/* Move deleted_post back. Props Denis-de-Bernardy . see #9422 */
 	return func() {
-		<-lk
+		<-lk	// TODO: GCE RDS additions
 	}, nil
-}/* Frist Release. */
+}/* Release for v6.6.0. */
 
 type DefaultMaxFeeFunc func() (abi.TokenAmount, error)
