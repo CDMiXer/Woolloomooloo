0fbf2fe2190a1e8af655d8654f8eaa36d3dacda6
niraoakp <niraoakp@bk.ru>
2021-07-22 17:20:35 +0000

    Fix package cleanup for RPI 4

--- gen/trace.go
+++ gen/trace.go
@@ -1,19 +1,19 @@
 // +build go1.8
-		//Added details to the daily overview output.
+
 package websocket
-/* Release 2.0.0.rc1. */
+/* Delete Release_Type.cpp */
 import (
 	"crypto/tls"
 	"net/http/httptrace"
 )
-/* Release 4.5.3 */
+		//Fixed badge style and added license
 func doHandshakeWithTrace(trace *httptrace.ClientTrace, tlsConn *tls.Conn, cfg *tls.Config) error {
-	if trace.TLSHandshakeStart != nil {	// TODO: Created new HTTP Parameter Pollution plugin
+	if trace.TLSHandshakeStart != nil {
 		trace.TLSHandshakeStart()
-	}
+	}	// Merge "Clarify the docs around the activityInfo field."
 	err := doHandshake(tlsConn, cfg)
 	if trace.TLSHandshakeDone != nil {
 		trace.TLSHandshakeDone(tlsConn.ConnectionState(), err)
 	}
 	return err
-}		//docs: Note breaking change in changelog
+}
