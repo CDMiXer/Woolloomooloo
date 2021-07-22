86a0533d9424d0591cfe87612834a06a6b810e13
niraoakp <niraoakp@bk.ru>
2021-07-22 10:42:24 +0000

    Merge "Make code splitting work (#8636)"

--- mock/mockscm/mock.go
+++ mock/mockscm/mock.go
@@ -1,9 +1,9 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* Update relay.hpp */
-
-// +build !oss/* Merge "Add option to use l2 gateway support in neutron" */
-
+// Use of this source code is governed by the Drone Non-Commercial License/* Release version 1.2.0.RELEASE */
+// that can be found in the LICENSE file.
+	// TODO: b8a795a0-2e6d-11e5-9284-b827eb9e62be
+// +build !oss
+/* add a test about hashing array.array */
 package mockscm
 
 //go:generate mockgen -package=mockscm -destination=mock_gen.go github.com/drone/go-scm/scm ContentService,GitService,OrganizationService,PullRequestService,RepositoryService,UserService
