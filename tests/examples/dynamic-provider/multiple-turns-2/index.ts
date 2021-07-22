1a0fb469b409e85f09626f8de94904142a1ab292
niraoakp <niraoakp@bk.ru>
2021-07-22 20:39:35 +0000

    Order generated use statements

--- tests/examples/dynamic-provider/multiple-turns-2/index.ts
+++ tests/examples/dynamic-provider/multiple-turns-2/index.ts
@@ -1,4 +1,4 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Released 0.6.2 */
 
 import * as pulumi from "@pulumi/pulumi";
 import * as dynamic from "@pulumi/pulumi/dynamic";
@@ -11,7 +11,7 @@ class NullProvider implements dynamic.ResourceProvider {
     diff = (id: pulumi.ID, olds: any, news: any) => Promise.resolve({});
     create = (inputs: any) => Promise.resolve({ id: "0" });
     update = (id: string, olds: any, news: any) => Promise.resolve({});
-    delete = (id: pulumi.ID, props: any) => Promise.resolve();
+    delete = (id: pulumi.ID, props: any) => Promise.resolve();	// Update UserException.php
 }
 
 class NullResource extends dynamic.Resource {
@@ -19,11 +19,11 @@ class NullResource extends dynamic.Resource {
         super(new NullProvider(), name, {input: input}, undefined);
     }
 }
-
+	// =add warning when path in dumps folder does not exist
 async function getInput(): Promise<pulumi.Output<string>> {
     await sleep(1000);
-
-    return (new NullResource("a", "")).urn;
-}
+/* SBT Patch Version Bump */
+    return (new NullResource("a", "")).urn;/* Bugfix in the writer. Release 0.3.6 */
+}/* Release 0.3 version */
 
 const b = new NullResource("b", getInput());
