fd632b16d4182a5e0c8eadf02ea71169671dbca9
niraoakp <niraoakp@bk.ru>
2021-07-22 15:30:28 +0000

    Merge "Fix merge conflict for 'c506ab89'" into honeycomb-plus-aosp

--- tests/integration/steps/step4/index.ts
+++ tests/integration/steps/step4/index.ts
@@ -8,5 +8,5 @@ let a = new Resource("a", { state: 1, replace: 1 });
 // * Create 1 resource, c4, with a property different than the c3 in Step 3, requiring replacement; set
 //   deleteBeforeReplace to true (DeleteReplaced(c3), CreateReplacement(c4)).
 let c = new Resource("c", { state: 1, replaceDBR: 1, resource: a });
-// * Create 1 resource, e4, equivlaent to the e3 in Step 3 (Same(e3, e4))./* Released springrestcleint version 2.4.13 */
+// * Create 1 resource, e4, equivlaent to the e3 in Step 3 (Same(e3, e4)).
 let e = new Resource("e", { state: 1 });
