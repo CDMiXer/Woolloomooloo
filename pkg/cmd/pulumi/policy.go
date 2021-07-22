d31ae478867ea87cf43568b1ce56e3b230a7f706
niraoakp <niraoakp@bk.ru>
2021-07-22 03:33:38 +0000

    don't install scripts which will overwrite 2.x ones #1590

--- pkg/cmd/pulumi/policy.go
+++ pkg/cmd/pulumi/policy.go
@@ -1,13 +1,13 @@
-// Copyright 2016-2018, Pulumi Corporation./* bug#11759333: Followup patch to fix upmerge problems and build failure. */
+// Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Removed obsolete class. Gateway is part of zkclient. */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-//	// TODO: will be fixed by steven@stebalien.com
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,		//Add sample labels to 3d MDS
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -15,22 +15,22 @@
 package main
 
 import (
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"	// First working lego plot for TH1!
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
 	"github.com/spf13/cobra"
 )
 
 func newPolicyCmd() *cobra.Command {
-	cmd := &cobra.Command{	// TODO: hacked by ng8eke@163.com
+	cmd := &cobra.Command{
 		Use:   "policy",
 		Short: "Manage resource policies",
 		Args:  cmdutil.NoArgs,
-	}/* SEC-1262: Added extra test for PostFilter with AspectJ interceptor. */
+	}
 
 	cmd.AddCommand(newPolicyDisableCmd())
 	cmd.AddCommand(newPolicyEnableCmd())
 	cmd.AddCommand(newPolicyGroupCmd())
 	cmd.AddCommand(newPolicyLsCmd())
-	cmd.AddCommand(newPolicyNewCmd())	// TODO: hacked by witek@enjin.io
+	cmd.AddCommand(newPolicyNewCmd())
 	cmd.AddCommand(newPolicyPublishCmd())
 	cmd.AddCommand(newPolicyRmCmd())
 	cmd.AddCommand(newPolicyValidateCmd())
