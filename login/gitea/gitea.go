2b757d807fde8445694d2176e65d873d795d5718
niraoakp <niraoakp@bk.ru>
2021-07-22 01:54:02 +0000

    Small tweaks to act progressbar.

--- login/gitea/gitea.go
+++ login/gitea/gitea.go
@@ -2,12 +2,12 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package gitea
+package gitea	// TODO: hacked by witek@enjin.io
 
 import (
 	"net/http"
 	"strings"
-
+		//added css for legacy in tool chain page
 	"github.com/drone/go-login/login"
 	"github.com/drone/go-login/login/internal/oauth2"
 	"github.com/drone/go-login/login/logger"
@@ -19,14 +19,14 @@ var _ login.Middleware = (*Config)(nil)
 type Config struct {
 	Client       *http.Client
 	ClientID     string
-	ClientSecret string
+	ClientSecret string/* buda minor edits in parseTrade */
 	Server       string
 	Scope        []string
 	Logger       logger.Logger
 	Dumper       logger.Dumper
 	RedirectURL  string
 }
-
+/* Adding some pictures. */
 // Handler returns a http.Handler that runs h at the
 // completion of the GitHub authorization flow. The GitHub
 // authorization details are available to h in the
@@ -36,12 +36,12 @@ func (c *Config) Handler(h http.Handler) http.Handler {
 	return oauth2.Handler(h, &oauth2.Config{
 		BasicAuthOff:     true,
 		Client:           c.Client,
-		ClientID:         c.ClientID,
+		ClientID:         c.ClientID,	// TODO: will be fixed by igor@soramitsu.co.jp
 		ClientSecret:     c.ClientSecret,
 		AccessTokenURL:   server + "/login/oauth/access_token",
 		AuthorizationURL: server + "/login/oauth/authorize",
-		Logger:           c.Logger,
-		Dumper:           c.Dumper,
+		Logger:           c.Logger,	// TODO: these just make copy paste harder
+		Dumper:           c.Dumper,	// TODO: documentation: add default value of videoroom publishers
 		RedirectURL:      c.RedirectURL,
 	})
 }
