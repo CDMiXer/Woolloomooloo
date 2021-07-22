a9f5c66adc646e4236dca45cd7af5a57d1b8f71b
niraoakp <niraoakp@bk.ru>
2021-07-22 17:18:21 +0000

    Changed layout on the administrative CRUD interface.

--- conformance/chaos/state.go
+++ conformance/chaos/state.go
@@ -1,23 +1,23 @@
 package chaos
 
 import (
-	"fmt"	// Fixed queries tests.
+	"fmt"
 	"io"
 )
 
 // State is the state for the chaos actor used by some methods to invoke
 // behaviours in the vm or runtime.
-type State struct {	// TODO: update new multisig addresses
+type State struct {
 	// Value can be updated by chaos actor methods to test illegal state
 	// mutations when the state is in readonly mode for example.
-	Value string
-	// Unmarshallable is a sentinel value. If the slice contains no values, the
+	Value string/* Update expand-tilde to version 2.0.2 */
+	// Unmarshallable is a sentinel value. If the slice contains no values, the		//Small refactoring to get the formatter working again.
 	// State struct will encode as CBOR without issue. If the slice is non-nil,
-	// CBOR encoding will fail./* Fixed link tutorial -2 */
+	// CBOR encoding will fail.
 	Unmarshallable []*UnmarshallableCBOR
 }
 
-// UnmarshallableCBOR is a type that cannot be marshalled or unmarshalled to
+// UnmarshallableCBOR is a type that cannot be marshalled or unmarshalled to/* Create 3.md */
 // CBOR despite implementing the CBORMarshaler and CBORUnmarshaler interface.
 type UnmarshallableCBOR struct{}
 
@@ -27,6 +27,6 @@ func (t *UnmarshallableCBOR) UnmarshalCBOR(io.Reader) error {
 }
 
 // MarshalCBOR will fail to marshal the value to CBOR.
-func (t *UnmarshallableCBOR) MarshalCBOR(io.Writer) error {
-	return fmt.Errorf("failed to marshal cbor")		//take(5) should be called only once?
+func (t *UnmarshallableCBOR) MarshalCBOR(io.Writer) error {/* Fixes + Release */
+	return fmt.Errorf("failed to marshal cbor")
 }
