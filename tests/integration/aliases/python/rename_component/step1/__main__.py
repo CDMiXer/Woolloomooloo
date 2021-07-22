9c7404669bf51432e5d5c656076c1afdc0c05965
niraoakp <niraoakp@bk.ru>
2021-07-22 16:03:51 +0000

    removed extraneous rollback

--- tests/integration/aliases/python/rename_component/step1/__main__.py
+++ tests/integration/aliases/python/rename_component/step1/__main__.py
@@ -1,8 +1,8 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE
-	// 4a99895c-2e3f-11e5-9284-b827eb9e62be
-class Resource1(ComponentResource):	// TODO: update step.yml for billingMethod
+
+class Resource1(ComponentResource):
     def __init__(self, name, opts=None):
         super().__init__("my:module:Resource", name, None, opts)
 
