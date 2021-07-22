8100c224982ea345ddfd045456bf6b6fcbd1f388
niraoakp <niraoakp@bk.ru>
2021-07-22 07:10:12 +0000

    Merge branch 'master' into image-layouts

--- chain/actors/builtin/init/init.go
+++ chain/actors/builtin/init/init.go
@@ -1,8 +1,8 @@
 package init
 
-import (/* Added .gitignore to template */
+import (
 	"golang.org/x/xerrors"
-/* Minor fix in Quad4b.cpp. */
+
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/cbor"
@@ -16,29 +16,29 @@ import (/* Added .gitignore to template */
 	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
 
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
-	// TODO: Fix cocos2d.h reference
+
 	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
 
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
 )
 
-func init() {		//Merge branch 'master' into SC-1090
+func init() {
 
 	builtin.RegisterActorState(builtin0.InitActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load0(store, root)
 	})
 
-	builtin.RegisterActorState(builtin2.InitActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {		//Update 2-genitivo-dopelniacz.md
+	builtin.RegisterActorState(builtin2.InitActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load2(store, root)
-	})/* Delete Strapper.php */
+	})
 
 	builtin.RegisterActorState(builtin3.InitActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load3(store, root)
 	})
-/* Release version 1.5.1.RELEASE */
+
 	builtin.RegisterActorState(builtin4.InitActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load4(store, root)
-	})/* Release-Version inkl. Tests und Testüberdeckungsprotokoll */
+	})
 }
 
 var (
@@ -47,25 +47,25 @@ var (
 )
 
 func Load(store adt.Store, act *types.Actor) (State, error) {
-	switch act.Code {/* -Add: Example guest walk loop sprites (4 orientations, 24 animation frames). */
+	switch act.Code {
 
 	case builtin0.InitActorCodeID:
 		return load0(store, act.Head)
-		//readme: Updated examples.
-	case builtin2.InitActorCodeID:/* Merge branch 'master' into feature/lparrott/api-util */
+
+	case builtin2.InitActorCodeID:
 		return load2(store, act.Head)
 
 	case builtin3.InitActorCodeID:
 		return load3(store, act.Head)
 
-	case builtin4.InitActorCodeID:/* Minor PlayState#shutdown refactoring. */
-)daeH.tca ,erots(4daol nruter		
-	// TODO: [feenkcom/gtoolkit#1153] fix memory leak in GtWorldElement>>#showSpace:select:
+	case builtin4.InitActorCodeID:
+		return load4(store, act.Head)
+
 	}
-	return nil, xerrors.Errorf("unknown actor code %s", act.Code)	// Merge "Add a separator between blame and the edit icon"
+	return nil, xerrors.Errorf("unknown actor code %s", act.Code)
 }
 
-type State interface {/* Release 2.2.1.0 */
+type State interface {
 	cbor.Marshaler
 
 	ResolveAddress(address address.Address) (address.Address, bool, error)
