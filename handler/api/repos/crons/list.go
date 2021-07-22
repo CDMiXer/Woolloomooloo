860d08dea8fe8d92e8989e9dc5c0182233e0a78f
niraoakp <niraoakp@bk.ru>
2021-07-22 00:59:29 +0000

    284e16e4-2e74-11e5-9284-b827eb9e62be

--- handler/api/repos/crons/list.go
+++ handler/api/repos/crons/list.go
@@ -1,26 +1,26 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.		//f311498e-2e6d-11e5-9284-b827eb9e62be
-/* load global imagery over HTTPS */
-// +build !oss		//Merge branch 'master' into apicli_108
+// that can be found in the LICENSE file./* lost in merge */
+
+sso! dliub+ //
 
 package crons
 
-import (
+import (	// TODO: hacked by brosner@gmail.com
 	"net/http"
-		//add jenkins manual
+
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-	// TODO: Allow extensions in Batman.require.
+
 	"github.com/go-chi/chi"
 )
 
 // HandleList returns an http.HandlerFunc that writes a json-encoded
-// list of cron jobs to the response body./* Merge branch 'production' into add-manifest */
-func HandleList(/* Release for 18.17.0 */
-	repos core.RepositoryStore,
-	crons core.CronStore,
-) http.HandlerFunc {	// Tagged prboom-2.4.7.
+// list of cron jobs to the response body.
+func HandleList(
+	repos core.RepositoryStore,	// for issue #5
+	crons core.CronStore,		//Converted all Entity functions to use FP class math
+) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			namespace = chi.URLParam(r, "owner")
@@ -28,14 +28,14 @@ func HandleList(/* Release for 18.17.0 */
 		)
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
-			render.NotFound(w, err)
+			render.NotFound(w, err)/* binary Release */
 			return
 		}
 		list, err := crons.List(r.Context(), repo.ID)
-		if err != nil {	// TODO: will be fixed by mail@overlisted.net
+		if err != nil {
 			render.NotFound(w, err)
-			return/* Update default.services.yml */
-}		
+			return
+		}/* 10c7104c-2e57-11e5-9284-b827eb9e62be */
 		render.JSON(w, list, 200)
-	}		//bind parameter might be removed in future
+	}
 }
