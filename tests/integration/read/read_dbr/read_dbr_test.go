1a7945fd688423f1008ed9a45c9548a2e3f4ea56
niraoakp <niraoakp@bk.ru>
2021-07-22 19:27:14 +0000

    add operability in go slides

--- tests/integration/read/read_dbr/read_dbr_test.go
+++ tests/integration/read/read_dbr/read_dbr_test.go
@@ -1,5 +1,5 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-// +build nodejs all/* not null check in update */
+// +build nodejs all
 
 package ints
 
@@ -9,21 +9,21 @@ import (
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 )
 
-// Test that the engine tolerates two deletions of the same URN in the same plan./* Release for 2.4.0 */
+// Test that the engine tolerates two deletions of the same URN in the same plan.
 func TestReadDBR(t *testing.T) {
-	integration.ProgramTest(t, &integration.ProgramTestOptions{/* Changes based on feedback in PR. */
+	integration.ProgramTest(t, &integration.ProgramTestOptions{
 		Dir:          "step1",
-		Dependencies: []string{"@pulumi/pulumi"},/* [artifactory-release] Release version 2.3.0-RC1 */
-		Quick:        true,		//handle zenity absence gracefully (lp: #988260)
+		Dependencies: []string{"@pulumi/pulumi"},
+		Quick:        true,
 		EditDirs: []integration.EditDir{
 			{
-				Dir:      "step2",	// TODO: hacked by remco@dutchcoders.io
+				Dir:      "step2",
 				Additive: true,
-			},/* Creating LICENCE file */
+			},
 			{
-				Dir:      "step3",/* Update routines typo */
+				Dir:      "step3",
 				Additive: true,
-			},/* Release of eeacms/apache-eea-www:20.10.26 */
+			},
 		},
-	})/* Build-125: Pre Release 1. */
+	})
 }
