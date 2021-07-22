7ecbd2e6b41969371bf43c3555e77096176dde27
niraoakp <niraoakp@bk.ru>
2021-07-22 13:16:37 +0000

    Issue #4365: Add crtassem.h constants to the msvcrt module.

--- handler/api/repos/builds/branches/create_test.go
+++ handler/api/repos/builds/branches/create_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License/* Tagging a Release Candidate - v3.0.0-rc5. */
 // that can be found in the LICENSE file.
 
 package branches
