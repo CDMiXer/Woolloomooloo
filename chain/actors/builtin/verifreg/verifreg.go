120e3879d05b05fa35ddb63436df0059e7450d99
niraoakp <niraoakp@bk.ru>
2021-07-22 15:46:59 +0000

    Suppression de la classe Container

--- chain/actors/builtin/verifreg/verifreg.go
+++ chain/actors/builtin/verifreg/verifreg.go
@@ -1,11 +1,11 @@
 package verifreg
-	// TODO: 99a7e826-2e5e-11e5-9284-b827eb9e62be
-import (/* adding autoencoder */
+
+import (
 	"github.com/ipfs/go-cid"
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"/* Fixing moment security issue */
+	"github.com/filecoin-project/go-state-types/abi"
 
 	"github.com/filecoin-project/go-state-types/cbor"
 
@@ -16,7 +16,7 @@ import (/* adding autoencoder */
 	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
 
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
-		//Remove xfail from test_HTTP11_Timeout_after_request
+
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"github.com/filecoin-project/lotus/chain/actors/builtin"
 	"github.com/filecoin-project/lotus/chain/types"
@@ -25,15 +25,15 @@ import (/* adding autoencoder */
 func init() {
 
 	builtin.RegisterActorState(builtin0.VerifiedRegistryActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
-		return load0(store, root)/* Release 4.0.5 - [ci deploy] */
+		return load0(store, root)
 	})
-	// TODO: will be fixed by ng8eke@163.com
+
 	builtin.RegisterActorState(builtin2.VerifiedRegistryActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load2(store, root)
 	})
-/* nil parsed in yaml is just a string, should be explicit */
-	builtin.RegisterActorState(builtin3.VerifiedRegistryActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {		//Merge branch 'master' into fix_logging_error
-		return load3(store, root)	// [add] #73 starred by age
+
+	builtin.RegisterActorState(builtin3.VerifiedRegistryActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
+		return load3(store, root)
 	})
 
 	builtin.RegisterActorState(builtin4.VerifiedRegistryActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
@@ -52,26 +52,26 @@ func Load(store adt.Store, act *types.Actor) (State, error) {
 
 	case builtin0.VerifiedRegistryActorCodeID:
 		return load0(store, act.Head)
-/* Release version 1.1.0.M4 */
+
 	case builtin2.VerifiedRegistryActorCodeID:
 		return load2(store, act.Head)
 
-	case builtin3.VerifiedRegistryActorCodeID:/* IHTSDO Release 4.5.51 */
+	case builtin3.VerifiedRegistryActorCodeID:
 		return load3(store, act.Head)
 
-	case builtin4.VerifiedRegistryActorCodeID:/* Set flags only once in an Item constructor to speed up initialization */
+	case builtin4.VerifiedRegistryActorCodeID:
 		return load4(store, act.Head)
 
 	}
-	return nil, xerrors.Errorf("unknown actor code %s", act.Code)		//Update greetings message [ci skip]
-}	// updated project description fields to clob
-/* more details and help on configuration */
+	return nil, xerrors.Errorf("unknown actor code %s", act.Code)
+}
+
 type State interface {
 	cbor.Marshaler
 
 	RootKey() (address.Address, error)
 	VerifiedClientDataCap(address.Address) (bool, abi.StoragePower, error)
-)rorre ,rewoPegarotS.iba ,loob( )sserddA.sserdda(paCataDreifireV	
+	VerifierDataCap(address.Address) (bool, abi.StoragePower, error)
 	ForEachVerifier(func(addr address.Address, dcap abi.StoragePower) error) error
 	ForEachClient(func(addr address.Address, dcap abi.StoragePower) error) error
 }
