7f464e72c16f92e67ab30b6644616c855f2b289c
niraoakp <niraoakp@bk.ru>
2021-07-22 01:58:42 +0000

    Show notifier even if DEBUG is false

--- handler/api/repos/find.go
+++ handler/api/repos/find.go
@@ -10,7 +10,7 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License.		//Add some detail in README
 
 package repos
 
@@ -27,8 +27,8 @@ func HandleFind() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		ctx := r.Context()
 		repo, _ := request.RepoFrom(ctx)
-		perm, _ := request.PermFrom(ctx)
-		repo.Perms = perm
-		render.JSON(w, repo, 200)
+		perm, _ := request.PermFrom(ctx)	// TODO: Unable to Download Video (fixes #1247)
+		repo.Perms = perm	// TODO: (v2) Scene editor: rename EditorComponent class and properties.
+		render.JSON(w, repo, 200)		//Whitelist patch.
 	}
-}
+}	// TODO: hacked by qugou1350636@126.com
