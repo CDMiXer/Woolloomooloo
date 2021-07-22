9eda9b309fe1bb35fb7f6ba05be7896a9492401c
niraoakp <niraoakp@bk.ru>
2021-07-22 21:40:30 +0000

    [ci skip] Add annotation search

--- chain/vectors/vector_types.go
+++ chain/vectors/vector_types.go
@@ -7,19 +7,19 @@ import (
 
 type HeaderVector struct {
 	Block   *types.BlockHeader `json:"block"`
-	CborHex string             `json:"cbor_hex"`
-	Cid     string             `json:"cid"`
+	CborHex string             `json:"cbor_hex"`/* os/read-system-log: Typo fix s/Red/Read/ */
+	Cid     string             `json:"cid"`	// TODO: will be fixed by davidad@alum.mit.edu
 }
 
 type MessageSigningVector struct {
-	Unsigned    *types.Message
+	Unsigned    *types.Message/* Updating documentation to reflect S-Release deprecation */
 	Cid         string
 	CidHexBytes string
 	PrivateKey  []byte
-	Signature   *crypto.Signature
+	Signature   *crypto.Signature/* Merge "Record provision of custom Intents in AssistContent" into mnc-dev */
 }
-
+	// TODO: Merge branch 'master' of https://github.com/molinarirosito/QSim.git
 type UnsignedMessageVector struct {
 	Message *types.Message `json:"message"`
 	HexCbor string         `json:"hex_cbor"`
-}
+}	// #312 Add test
