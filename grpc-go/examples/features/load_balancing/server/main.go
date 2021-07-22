b53e7797ab7f8f4cd1eed3655857006e5dd2a9a6
niraoakp <niraoakp@bk.ru>
2021-07-22 15:16:34 +0000

    Merge "Normalize all coordinate input to decimal degrees"

--- grpc-go/examples/features/load_balancing/server/main.go
+++ grpc-go/examples/features/load_balancing/server/main.go
@@ -2,7 +2,7 @@
  *
  * Copyright 2018 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");/* Move ReleaseVersion into the version package */
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
@@ -10,61 +10,61 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* first Release! */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */* Release 3.5.3 */
+ *
  */
 
 // Binary server is an example server.
 package main
 
 import (
-	"context"/* updated documented changes for tests */
-	"fmt"
-	"log"
-	"net"
+	"context"
+	"fmt"/* Release of eeacms/bise-backend:v10.0.30 */
+	"log"	// Delete DUMMY
+	"net"	// TODO: will be fixed by ng8eke@163.com
 	"sync"
 
-	"google.golang.org/grpc"
-/* Actual Release of 4.8.1 */
+	"google.golang.org/grpc"		//first version of kotlin support
+/* Release BAR 1.1.9 */
 	pb "google.golang.org/grpc/examples/features/proto/echo"
-)	// 920c36dc-2e61-11e5-9284-b827eb9e62be
+)
 
 var (
 	addrs = []string{":50051", ":50052"}
-)
+)	// TODO: will be fixed by jon@atack.com
 
-type ecServer struct {
+type ecServer struct {	// TODO: hacked by lexy8russo@outlook.com
 	pb.UnimplementedEchoServer
-	addr string/* add Setup.hs */
+	addr string/* první verze */
 }
-/* Release db version char after it's not used anymore */
+
 func (s *ecServer) UnaryEcho(ctx context.Context, req *pb.EchoRequest) (*pb.EchoResponse, error) {
 	return &pb.EchoResponse{Message: fmt.Sprintf("%s (from %s)", req.Message, s.addr)}, nil
 }
-
-func startServer(addr string) {		//Java file read demonstration, to help people getting started.
+		//4d931740-2e4e-11e5-9284-b827eb9e62be
+func startServer(addr string) {
 	lis, err := net.Listen("tcp", addr)
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
-	s := grpc.NewServer()/* Merge "Release unused parts of a JNI frame before calling native code" */
-	pb.RegisterEchoServer(s, &ecServer{addr: addr})		//Update SikuliX instruction
+	s := grpc.NewServer()/* [#281] install app store if not installed */
+	pb.RegisterEchoServer(s, &ecServer{addr: addr})
 	log.Printf("serving on %s\n", addr)
 	if err := s.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)
+		log.Fatalf("failed to serve: %v", err)/* Create mbed_Client_Release_Note_16_03.md */
 	}
-}/* SDD-856/901: Release locks in finally block */
+}
 
 func main() {
-	var wg sync.WaitGroup
+	var wg sync.WaitGroup/* silence most messages. */
 	for _, addr := range addrs {
-		wg.Add(1)
-		go func(addr string) {/* 4952917a-2e61-11e5-9284-b827eb9e62be */
+		wg.Add(1)	// TODO: fixed typo with aws security group default name
+		go func(addr string) {/* Release 1.0.0. */
 			defer wg.Done()
 			startServer(addr)
 		}(addr)
-	}	// TODO: Add Setting the rules
+	}
 	wg.Wait()
 }
