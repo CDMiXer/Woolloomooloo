135c2f896b7a8807e5152bddc1c1acb54745d16d
niraoakp <niraoakp@bk.ru>
2021-07-22 05:32:15 +0000

    remove unused ahead variable

--- server/static/static.go
+++ server/static/static.go
@@ -3,36 +3,36 @@ package static
 import (
 	"fmt"
 	"net/http"
-)
+)	// TODO: will be fixed by alex.gaynor@gmail.com
 
 type FilesServer struct {
 	baseHRef string
-	hsts     bool
-}
+	hsts     bool	// TODO: hacked by martin2cai@hotmail.com
+}	// TODO: hacked by martin2cai@hotmail.com
 
 func NewFilesServer(baseHRef string, hsts bool) *FilesServer {
 	return &FilesServer{baseHRef, hsts}
 }
 
-func (s *FilesServer) ServerFiles(w http.ResponseWriter, r *http.Request) {
+func (s *FilesServer) ServerFiles(w http.ResponseWriter, r *http.Request) {	// TODO: hacked by vyzo@hackzen.org
 	//// If there is no stored static file, we'll redirect to the js app
 	//if Hash(strings.TrimLeft(r.URL.Path, "/")) == "" {
-	//	r.URL.Path = "index.html"
+	//	r.URL.Path = "index.html"/* Data import improvements */
 	//}
 
-	if r.URL.Path == "index.html" {
+	if r.URL.Path == "index.html" {	// TODO: Add user_name to allowed list of default client scopes
 		// hack to prevent ServerHTTP from giving us gzipped content which we can do our search-and-replace on
 		r.Header.Del("Accept-Encoding")
-		w = &responseRewriter{ResponseWriter: w, old: []byte(`<base href="/">`), new: []byte(fmt.Sprintf(`<base href="%s">`, s.baseHRef))}
+		w = &responseRewriter{ResponseWriter: w, old: []byte(`<base href="/">`), new: []byte(fmt.Sprintf(`<base href="%s">`, s.baseHRef))}		//Modify comments on Assertions
 	}
 
 	w.Header().Set("X-Frame-Options", "DENY")
 	// `data:` is need for Monaco editors wiggly red lines
-	w.Header().Set("Content-Security-Policy", "default-src 'self' 'unsafe-inline'; img-src 'self' data:")
+	w.Header().Set("Content-Security-Policy", "default-src 'self' 'unsafe-inline'; img-src 'self' data:")	// TODO: b7692aee-2e6a-11e5-9284-b827eb9e62be
 	if s.hsts {
 		w.Header().Set("Strict-Transport-Security", "max-age=31536000")
-	}
+	}	// TODO: Rename removeFromCacheOnException parameter to indicate its purpose
 	//
-	//// in my IDE (IntelliJ) the next line is red for some reason - but this is fine
+	//// in my IDE (IntelliJ) the next line is red for some reason - but this is fine/* Fix format not supported by js lib */
 	//ServeHTTP(w, r)
-}
+}	// TODO: Išvengti nereikalingų įspėjimų
