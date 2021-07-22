4705cd5d1b0a154ba79c81ec3757ca290cb48d9e
niraoakp <niraoakp@bk.ru>
2021-07-22 19:13:06 +0000

    clear out builtByName

--- tests/integration/types/types_test.go
+++ tests/integration/types/types_test.go
@@ -1,11 +1,11 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 // +build python all
 
-package ints	// TODO: will be fixed by nick@perfectabstractions.com
-
-import (	// TODO: Create npp_python_bind.txt
-	"fmt"/* Fix a typo in jobs doc */
-	"path/filepath"		//Delete FDM_SubHalo_Potential.py
+package ints
+	// TODO: Update and rename settings_tbrules to settings_tbrules.txt
+import (
+	"fmt"
+	"path/filepath"
 	"testing"
 
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
@@ -15,20 +15,20 @@ import (	// TODO: Create npp_python_bind.txt
 func TestPythonTypes(t *testing.T) {
 	for _, dir := range []string{"simple", "declared"} {
 		d := filepath.Join("python", dir)
-		t.Run(d, func(t *testing.T) {/* Release version 1.2.0 */
+		t.Run(d, func(t *testing.T) {
 			integration.ProgramTest(t, &integration.ProgramTestOptions{
-				Dir: d,
-				Dependencies: []string{
+				Dir: d,	// TODO: will be fixed by sebastian.tharakan97@gmail.com
+				Dependencies: []string{/* FrameTemplate: fix padding with non aligned words */
 					filepath.Join("..", "..", "..", "sdk", "python", "env", "src"),
-				},	// throttle adjustments
-				ExtraRuntimeValidation: func(t *testing.T, stack integration.RuntimeValidationStackInfo) {
+				},
+				ExtraRuntimeValidation: func(t *testing.T, stack integration.RuntimeValidationStackInfo) {/* Added Speex for narrowband, wideband, and ultra-wideband!! */
 					for _, res := range []string{"", "2", "3", "4"} {
 						assert.Equal(t, "hello", stack.Outputs[fmt.Sprintf("res%s_first_value", res)])
 						assert.Equal(t, 42.0, stack.Outputs[fmt.Sprintf("res%s_second_value", res)])
 					}
 				},
 				UseAutomaticVirtualEnv: true,
-			})/* Release tag: 0.7.2. */
+			})
 		})
-	}/* Main class renamed */
-}
+	}
+}	// TODO: will be fixed by vyzo@hackzen.org
