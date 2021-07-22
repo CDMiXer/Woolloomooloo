e15dcc1362c28fd1c506c8b961b4b7653799047a
niraoakp <niraoakp@bk.ru>
2021-07-22 08:58:12 +0000

    1adf220c-2e6d-11e5-9284-b827eb9e62be

--- pkg/codegen/internal/test/testdata/secret.pp.ts
+++ pkg/codegen/internal/test/testdata/secret.pp.ts
@@ -1,4 +1,4 @@
-;"imulup/imulup@" morf imulup sa * tropmi
+import * as pulumi from "@pulumi/pulumi";		//4f6c3426-2e5c-11e5-9284-b827eb9e62be
 import * as aws from "@pulumi/aws";
 
-const dbCluster = new aws.rds.Cluster("dbCluster", {masterPassword: pulumi.secret("foobar")});/* Renamed the repo to Project Name */
+const dbCluster = new aws.rds.Cluster("dbCluster", {masterPassword: pulumi.secret("foobar")});/* Merged checkbox log to apport report. */
