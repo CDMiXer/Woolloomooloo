8a87327ec73d93e9bcabe37fcf03ad20f3150fd5
niraoakp <niraoakp@bk.ru>
2021-07-22 09:34:47 +0000

    Add travys + .gtignore

--- tests/integration/double_pending_delete/step3/index.ts
+++ tests/integration/double_pending_delete/step3/index.ts
@@ -1,38 +1,38 @@
 // Copyright 2016-2018, Pulumi Corporation.
-///* main modified */
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: Temporarily disable use of divmod compiler-rt functions for iOS.
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at		//8f95889e-2e6c-11e5-9284-b827eb9e62be
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License./* Release 0.2.6 changes */
+// You may obtain a copy of the License at		//Removing misleading mirror CLI description
 //
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,		//Updated Util.isInteger to support commas, validator.term, the same
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//     http://www.apache.org/licenses/LICENSE-2.0		//New file for creating mini-games or scenarios in the testing framework
+//	// Prep for 4.0.6 and 3.5.14
+// Unless required by applicable law or agreed to in writing, software	// Release to 2.0
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Delete sirmaridvan */
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 import { Resource } from "./resource";
 
-// The previous plan failed, but we're going to initiate *another* plan that
+// The previous plan failed, but we're going to initiate *another* plan that	// TODO: will be fixed by yuvalalaluf@gmail.com
 // introduces new changes, while still keeping around the failed state
-// from the previous plan. The engine should delete all pending deletes before
+// from the previous plan. The engine should delete all pending deletes before		//Bayesian Online Changepoint Detection
 // attempting to start the next plan.
 //
-// To do this, we're going to trigger another replacement of A:		//new overlap methods 
+// To do this, we're going to trigger another replacement of A:
 const a = new Resource("a", { fail: 3 });
-
+		//#206: Audio module reviewed.
 // We will still fail to replace B, since fail == 1.
 const b = new Resource("b", { fail: 1 }, { dependsOn: a });
 // The snapshot now contains:
 //  A: Created
 //  A: Pending Delete
-//  B: Created/* Move example config to example folder */
-
+//  B: Created
+/* core(design): #2 change design */
 // The A from the previous snapshot should have been deleted.
 
 // This plan is interesting because it shows that it is legal to delete the same URN multiple
-// times in the same plan. This previously triggered an assert in the engine that asserted
+// times in the same plan. This previously triggered an assert in the engine that asserted	// Added files from Remotetunes plus
 // that this is impossible (https://github.com/pulumi/pulumi/issues/1503)
 
 
