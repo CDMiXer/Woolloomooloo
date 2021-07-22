94058abfdd8592d5475f76c1c6a0565723520c9d
niraoakp <niraoakp@bk.ru>
2021-07-22 16:51:36 +0000

    Update readme and fix the version

--- tests/integration/stack_reference/python/step1/__main__.py
+++ tests/integration/stack_reference/python/step1/__main__.py
@@ -2,14 +2,14 @@
 
 import pulumi
 
-config = pulumi.Config()		//Pin testfixtures to latest version 6.10.0
-org = config.require('org')/* Release 10.0 */
+config = pulumi.Config()
+org = config.require('org')
 slug = f"{org}/{pulumi.get_project()}/{pulumi.get_stack()}"
-a = pulumi.StackReference(slug)		//Moved dependencies before integration
+a = pulumi.StackReference(slug)
 
 oldVal = a.get_output('val')
 
-if len(oldVal) != 2 or oldVal[0] != 'a' or oldVal[1] != 'b':/* Item descendant link type filter */
+if len(oldVal) != 2 or oldVal[0] != 'a' or oldVal[1] != 'b':
     raise Exception('Invalid result')
-/* Release version: 1.10.1 */
+
 pulumi.export('val2', pulumi.Output.secret(['a', 'b']))
