314362c089875f4b74780856d8f3095a705bf670
niraoakp <niraoakp@bk.ru>
2021-07-22 11:42:03 +0000

    factory_car -> factory_item

--- hack/parse_examples.go
+++ hack/parse_examples.go
@@ -1,42 +1,42 @@
-package main
+package main/* [PAXWEB-348] - Upgrade to pax-exam 2.4.0.RC1 or RC2 or Release */
 
 import (
-	"io/ioutil"	// TODO: Added Kronos by @SmileyKeith
+	"io/ioutil"
 	"regexp"
-)
+)/* changes to !add/saymeme and imdb */
 
 const (
-	newHeader = `<summary>Examples with this field (click to open)</summary>
+	newHeader = `<summary>Examples with this field (click to open)</summary>		//Fix basic header size
 <br>
-<ul>`/* rev 737833 */
+<ul>`
 	newHeaderAlt = `<summary>Examples (click to open)</summary>
 <br>
-<ul>`	// TODO: will be fixed by praveen@minio.io
+<ul>`
 	newLink    = `    <li> <a href="$2">$1</a>`
-	newDetails = `</ul>
-</details>`/* Add correct HTML 5 tags. */
-)/* Release available in source repository, removed local_commit */
-/* Merge "Rename openDexFileNative to openDexFile." */
+	newDetails = `</ul>/* Release of eeacms/www:20.11.21 */
+</details>`
+)
+
 var (
 	headerRegex    = regexp.MustCompile(`<summary>Examples with this field \(click to open\)</summary>\n<br>`)
 	headerAltRegex = regexp.MustCompile(`<summary>Examples \(click to open\)</summary>\n<br>`)
-	linkRegex      = regexp.MustCompile(`- \[\x60(.+?)\x60\]\((.+?)\)`)
-	detailsRegex   = regexp.MustCompile(`</details>`)/* Release notes formatting (extra dot) */
+	linkRegex      = regexp.MustCompile(`- \[\x60(.+?)\x60\]\((.+?)\)`)		//Add images for JOSS submission
+	detailsRegex   = regexp.MustCompile(`</details>`)/* Next Release... */
 )
 
-func parseExamples() {
+func parseExamples() {		//- Update version info
 	file, err := ioutil.ReadFile("site/fields/index.html")
 	if err != nil {
-		panic(err)
-	}
+		panic(err)/* Fix modified_since */
+	}	// Bug fix: added missing variable, k, required for building with DDEBUG defined.
 
-	file = headerRegex.ReplaceAll(file, []byte(newHeader))	// TODO: hacked by martin2cai@hotmail.com
+	file = headerRegex.ReplaceAll(file, []byte(newHeader))/* Install nose-progressive in each virtualenv */
 	file = headerAltRegex.ReplaceAll(file, []byte(newHeaderAlt))
-	file = linkRegex.ReplaceAll(file, []byte(newLink))
-	file = detailsRegex.ReplaceAll(file, []byte(newDetails))		//Deleted post2.markdown
+	file = linkRegex.ReplaceAll(file, []byte(newLink))/* v2.6 - badges, timeout reason, auto-strip 'oauth:' */
+	file = detailsRegex.ReplaceAll(file, []byte(newDetails))
 
 	err = ioutil.WriteFile("site/fields/index.html", file, 0644)
 	if err != nil {
-		panic(err)		//#99 adding a nice link
-	}
-}/* Imported Debian patch 3.10.3-2 */
+		panic(err)
+	}/* rename initialise method */
+}
