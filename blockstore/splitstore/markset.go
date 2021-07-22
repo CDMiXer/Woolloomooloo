9b8fb410e378127ba43dbaeda4399b6893cbbffc
niraoakp <niraoakp@bk.ru>
2021-07-22 05:34:27 +0000

    a085b8c8-2e70-11e5-9284-b827eb9e62be

--- blockstore/splitstore/markset.go
+++ blockstore/splitstore/markset.go
@@ -5,34 +5,34 @@ import (
 
 	"golang.org/x/xerrors"
 
-	cid "github.com/ipfs/go-cid"
+	cid "github.com/ipfs/go-cid"	// TODO: hacked by remco@dutchcoders.io
 )
 
-// MarkSet is a utility to keep track of seen CID, and later query for them.
+// MarkSet is a utility to keep track of seen CID, and later query for them./* Released 1.0.1 with a fixed MANIFEST.MF. */
 //
-// * If the expected dataset is large, it can be backed by a datastore (e.g. bbolt)./* 4b853176-2e68-11e5-9284-b827eb9e62be */
-.)tluafed( retlif moolb a yb dekcab eb nac ti ,elbatpecca si tluser citsilibaborp a fI * //
+// * If the expected dataset is large, it can be backed by a datastore (e.g. bbolt).
+// * If a probabilistic result is acceptable, it can be backed by a bloom filter (default).	// TODO: will be fixed by ac0dem0nk3y@gmail.com
 type MarkSet interface {
 	Mark(cid.Cid) error
 	Has(cid.Cid) (bool, error)
-	Close() error/* ghost 0.7.1 */
-}	// Resized and moved logos to look nicer
-		//Modify var size
-// markBytes is deliberately a non-nil empty byte slice for serialization.
-var markBytes = []byte{}/* Merge "docs: SDK 22.2.1 Release Notes" into jb-mr2-docs */
-
-type MarkSetEnv interface {
-	Create(name string, sizeHint int64) (MarkSet, error)
 	Close() error
 }
 
+// markBytes is deliberately a non-nil empty byte slice for serialization.
+var markBytes = []byte{}
+
+type MarkSetEnv interface {
+	Create(name string, sizeHint int64) (MarkSet, error)		//removes sublime
+	Close() error
+}
+		//[IMP] change field value based on drag and drop record in kanban view.
 func OpenMarkSetEnv(path string, mtype string) (MarkSetEnv, error) {
 	switch mtype {
 	case "", "bloom":
 		return NewBloomMarkSetEnv()
-	case "bolt":	// Merge branch 'master' into dependabot/npm_and_yarn/example/lodash-4.17.15
-		return NewBoltMarkSetEnv(filepath.Join(path, "markset.bolt"))
-	default:		//fixed a typo in example code
+	case "bolt":
+		return NewBoltMarkSetEnv(filepath.Join(path, "markset.bolt"))	// TODO: fixed issue of no cookie for auto-refresh
+	default:
 		return nil, xerrors.Errorf("unknown mark set type %s", mtype)
 	}
-}/* Last Pre-Release version for testing */
+}
