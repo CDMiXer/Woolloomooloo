4eeadc5ccc3edb911fabf7fb344c22d568420352
niraoakp <niraoakp@bk.ru>
2021-07-22 05:20:44 +0000

    Incorrect manual install doc

--- pkg/engine/errors.go
+++ pkg/engine/errors.go
@@ -1,40 +1,40 @@
 // Copyright 2016-2018, Pulumi Corporation.
-///* ucs traversal test, plus fix. */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//	// TODO: will be fixed by fjl@ethereum.org
+// you may not use this file except in compliance with the License.	// TODO: hacked by nagydani@epointsystem.org
+// You may obtain a copy of the License at	// TODO: #1 Added stall feature docs
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-//	// TODO: will be fixed by onhardev@bk.ru
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// Implement method to check if rate matrix is finite.
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: Delete WebSharper.Community.Suave.WebSocket.min.js
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package engine
+package engine/* Create st.min.js */
 
 import (
 	"fmt"
 
-	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/config"	// finally removed special version of the function PosEx for darwin. Not needed
+	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/config"		//Simplify the Knapsack Pro docs
 )
 
-//	// TODO: hacked by fjl@ethereum.org
+//
 // This file contains type definitions for errors that can arise in the engine that the CLI layer would
 // like to provide high-quality diagnostics for. cmd/errors.go is aware of these events and will use them
-// and the data within them to provide long-form diagnostics that are inappropriate to be done in the Error()
+// and the data within them to provide long-form diagnostics that are inappropriate to be done in the Error()/* Merge "Release 3.2.3.431 Prima WLAN Driver" */
 // implementation of these types.
 //
 
-// DecryptError is the type of errors that arise when the engine can't decrypt a configuration key.		//config.create helper
+// DecryptError is the type of errors that arise when the engine can't decrypt a configuration key.
 // The most common reason why this happens is that this key is being decrypted in a stack that's not the same
-// one that encrypted it./* 916eacc8-2e66-11e5-9284-b827eb9e62be */
+// one that encrypted it.
 type DecryptError struct {
 	Key config.Key // The configuration key whose value couldn't be decrypted
 	Err error      // The error that occurred while decrypting
 }
 
-func (d DecryptError) Error() string {		//Delete model2.json
+func (d DecryptError) Error() string {		//Merge + bugfixes in STG parser
 	return fmt.Sprintf("failed to decrypt configuration key '%s': %s", d.Key, d.Err.Error())
 }
