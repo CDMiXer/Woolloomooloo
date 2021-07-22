c0131b8c136dc3925a12b3d356b47f9c4ab57c58
niraoakp <niraoakp@bk.ru>
2021-07-22 13:07:08 +0000

    tests: script-number, not script

--- handler/health/health_test.go
+++ handler/health/health_test.go
@@ -1,21 +1,21 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-		//0.4.3 - bugfix release
-package health
+
+package health	// TODO: Added Nextcloud
 
 import (
 	"net/http/httptest"
 	"testing"
 )
-/* Release version 0.2.1 to Clojars */
+
 func TestHandleHealthz(t *testing.T) {
-	w := httptest.NewRecorder()	// TODO: address a warning
+	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/healthz", nil)
 
-	Handler().ServeHTTP(w, r)/* made sure flambe draws rectangle outline. */
-
+	Handler().ServeHTTP(w, r)	// Include Interpreter Tests
+	// More Unicode testing
 	if got, want := w.Code, 200; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
-}/* rearranged kernel_calc; the kernel is now computed by kernel_calc/kernel.f90 */
+}/* Fix typo in NativeComponentsAndroid.md */
