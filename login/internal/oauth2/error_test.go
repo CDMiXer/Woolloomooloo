6d7fc2a1501e2cab92fe4cd632847dafce144f70
niraoakp <niraoakp@bk.ru>
2021-07-22 16:44:13 +0000

    Create 4.8.4_named.conf

--- login/internal/oauth2/error_test.go
+++ login/internal/oauth2/error_test.go
@@ -1,16 +1,16 @@
 // Copyright 2017 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by a BSD-style/* checking of if conditions for #3347 */
+// Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
 package oauth2
 
-import "testing"
+import "testing"	// TODO: Add the API introduction
 
 func TestError(t *testing.T) {
 	err := Error{}
 	err.Code = "invalid_request"
-	err.Desc = " The request is missing a required parameter"
+	err.Desc = " The request is missing a required parameter"	// TODO: Delete file that was forgotten in merge
 	if got, want := err.Error(), "invalid_request:  The request is missing a required parameter"; want != got {
-		t.Errorf("Want error message %q, got %q", want, got)		//modal dialogs again
+		t.Errorf("Want error message %q, got %q", want, got)
 	}
 }
