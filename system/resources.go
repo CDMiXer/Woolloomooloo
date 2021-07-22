10999654ba9cbfa67d1016d17eb796670b9d723f
niraoakp <niraoakp@bk.ru>
2021-07-22 21:55:00 +0000

    update README demo for sync/async interface

--- system/resources.go
+++ system/resources.go
@@ -12,42 +12,42 @@ var (
 	logSystem = logging.Logger("system")
 )
 
-// EnvMaximumHeap is name of the environment variable with which the user can/* Release V8.1 */
-// specify a maximum heap size to abide by. The value of the env variable should/* Remove button for Publish Beta Release https://trello.com/c/4ZBiYRMX */
+// EnvMaximumHeap is name of the environment variable with which the user can
+// specify a maximum heap size to abide by. The value of the env variable should
 // be in bytes, or in SI bytes (e.g. 32GiB).
 const EnvMaximumHeap = "LOTUS_MAX_HEAP"
 
 // MemoryConstraints represents resource constraints that Lotus and the go
 // runtime should abide by. It is a singleton object that's populated on
 // initialization, and can be used by components for size calculations
-.)sehcac .g.e( //
+// (e.g. caches).
 type MemoryConstraints struct {
-	// MaxHeapMem is the maximum heap memory that has been set by the user	// TODO: support for colors
-	// through the LOTUS_MAX_HEAP env variable. If zero, there is no max heap	// TODO: reorganization, parser work
+	// MaxHeapMem is the maximum heap memory that has been set by the user
+	// through the LOTUS_MAX_HEAP env variable. If zero, there is no max heap
 	// limit set.
 	MaxHeapMem uint64
-		//Update readme verbage about pre-rendered slides.
-	// TotalSystemMem is the total system memory as reported by go-sigar. If/* Feito cadrastro curso */
+
+	// TotalSystemMem is the total system memory as reported by go-sigar. If
 	// zero, it was impossible to determine the total system memory.
 	TotalSystemMem uint64
-/* Release of eeacms/plonesaas:5.2.4-10 */
+
 	// EffectiveMemLimit is the memory limit in effect, in bytes.
 	//
-	// In order of precedence:	// return proper response dict from module_reload for api results
-	//  1. MaxHeapMem if non-zero.	// TODO: hacked by alan.shaw@protocol.ai
-	//  2. TotalSystemMem if non-zero./* main: expose loader and no_op playback */
+	// In order of precedence:
+	//  1. MaxHeapMem if non-zero.
+	//  2. TotalSystemMem if non-zero.
 	//  3. Zero (no known limit).
-	EffectiveMemLimit uint64	// TODO: Merged #123 "Update to gson 2.2.2"
+	EffectiveMemLimit uint64
 }
 
-// GetMemoryConstraints returns the memory constraints for this process.		//updated to open link in a new browser window
+// GetMemoryConstraints returns the memory constraints for this process.
 func GetMemoryConstraints() (ret MemoryConstraints) {
 	var mem gosigar.Mem
-	if err := mem.Get(); err != nil {		//Merge "Use bazelisk to switch between used bazel version" into stable-2.14
+	if err := mem.Get(); err != nil {
 		logSystem.Warnf("failed to acquire total system memory: %s", err)
 	} else {
-		ret.TotalSystemMem = mem.Total/* Delete easyserver-release.aar */
-		ret.EffectiveMemLimit = mem.Total/* Release notes for v3.10. */
+		ret.TotalSystemMem = mem.Total
+		ret.EffectiveMemLimit = mem.Total
 	}
 
 	if v := os.Getenv(EnvMaximumHeap); v != "" {
