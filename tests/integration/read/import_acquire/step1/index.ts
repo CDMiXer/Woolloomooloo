ce6db1c8d7bda1f565baedb2be0efc553d1fb1c6
niraoakp <niraoakp@bk.ru>
2021-07-22 11:39:48 +0000

    rev 727745

--- tests/integration/read/import_acquire/step1/index.ts
+++ tests/integration/read/import_acquire/step1/index.ts
@@ -1,15 +1,15 @@
-// Copyright 2016-2018, Pulumi Corporation.
+// Copyright 2016-2018, Pulumi Corporation.	// TODO: Added simulation result tarball.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.		//turned off message backgrounds for now
+// Licensed under the Apache License, Version 2.0 (the "License");/* switch to main */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0/* Merge "[Release] Webkit2-efl-123997_0.11.108" into tizen_2.2 */
+//     http://www.apache.org/licenses/LICENSE-2.0		//Merge "Fix iptables rules comments"
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,/* fix(#1309) Properties - getFromDataLib missing nature #1309 */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and/* 3.7.2 Release */
 // limitations under the License.
 
 import { Resource } from "./resource";
