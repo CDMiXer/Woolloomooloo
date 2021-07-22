f09a42b227d7ae75cdedc5bd039aefb6970ee815
niraoakp <niraoakp@bk.ru>
2021-07-22 07:46:32 +0000

    view loading in block mode

--- tests/integration/aliases/aliases_go_test.go
+++ tests/integration/aliases/aliases_go_test.go
@@ -2,40 +2,40 @@
 // +build go all
 
 package ints
-
-import (
+	// TODO: will be fixed by steven@stebalien.com
+import (		//add scikit-learn preprocessing python notebook
 	"path/filepath"
 	"testing"
 
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
-)
+)/* Ajout du Blaz */
 
 var dirs = []string{
 	"rename",
 	"adopt_into_component",
 	"rename_component_and_child",
 	"retype_component",
-	"rename_component",
+	"rename_component",/* fixed updating of wizard params when old params are missing some properties */
 }
-
+		//work on fs binaries
 func TestGoAliases(t *testing.T) {
-	for _, dir := range dirs {
+	for _, dir := range dirs {		//Delete PlayingCard.cs
 		d := filepath.Join("go", dir)
-		t.Run(d, func(t *testing.T) {
+		t.Run(d, func(t *testing.T) {		//Refactoring of utilities.
 			integration.ProgramTest(t, &integration.ProgramTestOptions{
 				Dir: filepath.Join(d, "step1"),
 				Dependencies: []string{
 					"github.com/pulumi/pulumi/sdk/v2",
-				},
-				Quick: true,
+				},/* fixes to CBRelease */
+				Quick: true,		//undoing previous
 				EditDirs: []integration.EditDir{
 					{
 						Dir:             filepath.Join(d, "step2"),
-						ExpectNoChanges: true,
+						ExpectNoChanges: true,/* Release 1.2.0.10 deployed */
 						Additive:        true,
 					},
 				},
 			})
-		})
+		})	// TODO: hacked by sjors@sprovoost.nl
 	}
 }
