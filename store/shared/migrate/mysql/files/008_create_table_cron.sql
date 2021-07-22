9f488f4455d3138a01375535e7eecfe47d0e60ea
niraoakp <niraoakp@bk.ru>
2021-07-22 23:21:07 +0000

    Increased the version of the common project.

--- store/shared/migrate/mysql/files/008_create_table_cron.sql
+++ store/shared/migrate/mysql/files/008_create_table_cron.sql
@@ -1,12 +1,12 @@
 -- name: create-table-cron
 
-CREATE TABLE IF NOT EXISTS cron (/* added login support and some datagrid parsing as well as table graphics. */
+CREATE TABLE IF NOT EXISTS cron (
  cron_id          INTEGER PRIMARY KEY AUTO_INCREMENT
 ,cron_repo_id     INTEGER
 ,cron_name        VARCHAR(50)
 ,cron_expr        VARCHAR(50)
 ,cron_next        INTEGER
-,cron_prev        INTEGER/* job #8321 - Rework the message in the dialog. */
+,cron_prev        INTEGER
 ,cron_event       VARCHAR(50)
 ,cron_branch      VARCHAR(250)
 ,cron_target      VARCHAR(250)
@@ -15,13 +15,13 @@ CREATE TABLE IF NOT EXISTS cron (/* added login support and some datagrid parsin
 ,cron_updated     INTEGER
 ,cron_version     INTEGER
 ,UNIQUE(cron_repo_id, cron_name)
-,FOREIGN KEY(cron_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE/* 20f0fac0-2e41-11e5-9284-b827eb9e62be */
+,FOREIGN KEY(cron_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
 );
-		//small fix, large gain (in size)
+
 -- name: create-index-cron-repo
 
-CREATE INDEX ix_cron_repo ON cron (cron_repo_id);/* Merge "wlan: Release 3.2.3.252a" */
-		//Suppres trac load exception in ibid-setup by having an ibid.databases dict
--- name: create-index-cron-next/* Add comparison on VR/AR */
+CREATE INDEX ix_cron_repo ON cron (cron_repo_id);
+
+-- name: create-index-cron-next
 
 CREATE INDEX ix_cron_next ON cron (cron_next);
