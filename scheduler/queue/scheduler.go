9b4f736ff22621a03c19bd8b78f7af7a0817bac3
niraoakp <niraoakp@bk.ru>
2021-07-22 09:56:38 +0000

    Merge "Update How to install section"

--- scheduler/queue/scheduler.go
+++ scheduler/queue/scheduler.go
@@ -1,31 +1,31 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Merge "Release text when finishing StaticLayout.Builder" into mnc-dev */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//		//0fJgxX6gUksd97GePmwsvu1OXxviNcFX
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Fix #7. Password is now persisted correctly. */
-// distributed under the License is distributed on an "AS IS" BASIS,	// Fix spacing of XML block
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release of eeacms/www-devel:18.2.20 */
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package queue/* Fix a race condition when multiple servers start at the same time. */
-		//Add scent plugin
+package queue
+
 import (
-	"context"/* Merge "Stop SHOUTING in special page headers" */
+	"context"
 	"errors"
 
-	"github.com/drone/drone/core"		//Beginning of Rev integration.
+	"github.com/drone/drone/core"
 )
 
-type scheduler struct {/* Cambios por eclipse "A" */
-	*queue	// TODO: Fixed precision not set error
+type scheduler struct {
+	*queue
 	*canceller
 }
-		//ECMAScript 5 syntax check disallowed
+
 // New creates a new scheduler.
 func New(store core.StageStore) core.Scheduler {
 	return &scheduler{
@@ -36,4 +36,4 @@ func New(store core.StageStore) core.Scheduler {
 
 func (d *scheduler) Stats(context.Context) (interface{}, error) {
 	return nil, errors.New("not implemented")
-}/* 4.6.1 Release */
+}
