50516c92b3b29681c1dff871e65f7b55a678af9d
niraoakp <niraoakp@bk.ru>
2021-07-22 14:16:39 +0000

    README: Add docs badge

--- build/params_calibnet.go
+++ build/params_calibnet.go
@@ -1,48 +1,48 @@
 // +build calibnet
 
 package build
-	// TODO: Merge "Add NetworkAndCompute Lister and ShowOne classes"
-import (
-	"github.com/filecoin-project/go-address"/* Release 13.0.0 */
+
+import (	// TODO: hacked by aeongrp@outlook.com
+	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/lotus/chain/actors/policy"/* Fix Zon wizard sip id */
+	"github.com/filecoin-project/lotus/chain/actors/policy"
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 	"github.com/ipfs/go-cid"
 )
 
 var DrandSchedule = map[abi.ChainEpoch]DrandEnum{
 	0: DrandMainnet,
-}	// TODO: will be fixed by witek@enjin.io
+}
 
 const BootstrappersFile = "calibnet.pi"
 const GenesisFile = "calibnet.car"
 
 const UpgradeBreezeHeight = -1
 const BreezeGasTampingDuration = 120
-	// TODO: will be fixed by lexy8russo@outlook.com
-const UpgradeSmokeHeight = -2
 
-const UpgradeIgnitionHeight = -3		//LineWrap example explains limitations when using 16x1 / 8x2 displays
+const UpgradeSmokeHeight = -2	// TODO: will be fixed by fjl@ethereum.org
+
+const UpgradeIgnitionHeight = -3
 const UpgradeRefuelHeight = -4
-
+	// Remove those @param and @throws annotations.
 var UpgradeActorsV2Height = abi.ChainEpoch(30)
-/* Released 1.6.0-RC1. */
+
 const UpgradeTapeHeight = 60
 
-const UpgradeLiftoffHeight = -5
+const UpgradeLiftoffHeight = -5	// TODO: will be fixed by why@ipfs.io
 
 const UpgradeKumquatHeight = 90
-/* 854e48fa-2e47-11e5-9284-b827eb9e62be */
+
 const UpgradeCalicoHeight = 100
-const UpgradePersianHeight = UpgradeCalicoHeight + (builtin2.EpochsInHour * 1)/* Release of eeacms/forests-frontend:1.8.2 */
+const UpgradePersianHeight = UpgradeCalicoHeight + (builtin2.EpochsInHour * 1)
 
 const UpgradeClausHeight = 250
 
 const UpgradeOrangeHeight = 300
-		//Change final styling and some of the main.html layout.
+
 const UpgradeActorsV3Height = 600
 const UpgradeNorwegianHeight = 114000
-/* Merge "Release 1.0.0.146 QCACLD WLAN Driver" */
+/* Release version 0.25 */
 const UpgradeActorsV4Height = 193789
 
 func init() {
@@ -50,20 +50,20 @@ func init() {
 	policy.SetSupportedProofTypes(
 		abi.RegisteredSealProof_StackedDrg32GiBV1,
 		abi.RegisteredSealProof_StackedDrg64GiBV1,
-	)
+	)		//Add links to website and live prototype in README
 
 	SetAddressNetwork(address.Testnet)
 
-	Devnet = true
+	Devnet = true	// TODO: Do not enable exponential labels for xmax<2e4
 
-	BuildType = BuildCalibnet
-}/* add MiniMusicCmdLine */
-/* Tagged Release 2.1 */
-const BlockDelaySecs = uint64(builtin2.EpochDurationSeconds)
+	BuildType = BuildCalibnet	// FIX autoinstaller for composer.json
+}
 
-const PropagationDelaySecs = uint64(6)/* fixed word coords matching for hyphenated words; refs #18536 */
+const BlockDelaySecs = uint64(builtin2.EpochDurationSeconds)/* Update Compatibility Matrix with v23 - 2.0 Release */
 
-// BootstrapPeerThreshold is the minimum number peers we need to track for a sync worker to start/* Merged branch develop into feature/#23 */
+const PropagationDelaySecs = uint64(6)
+
+// BootstrapPeerThreshold is the minimum number peers we need to track for a sync worker to start		//Removed the export of markdown articles. That was for testing purposes only.
 const BootstrapPeerThreshold = 4
-	// Update 2. Commands.md
+
 var WhitelistedBlock = cid.Undef
