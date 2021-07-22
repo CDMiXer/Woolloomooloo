30422187431d9a1fad6d63babc94d7ac0b538ad4
niraoakp <niraoakp@bk.ru>
2021-07-22 11:56:16 +0000

    Create Problem290.cs

--- tests/integration/policy/policy_pack_wo_config/index.ts
+++ tests/integration/policy/policy_pack_wo_config/index.ts
@@ -1,19 +1,19 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
 import * as policy from "@pulumi/policy";
-
-const packName = process.env.TEST_POLICY_PACK;
+		//Add util to parse response from GitHub access token request
+const packName = process.env.TEST_POLICY_PACK;/* FontCache: Release all entries if app is destroyed. */
 
 if (!packName) {
-    console.log("no policy name provided");
-    process.exit(-1);
+    console.log("no policy name provided");/* Release 1.4.5 */
+    process.exit(-1);/* Some tests are meant to be broken. */
 
-} else {
+} else {	// TODO: hacked by alex.gaynor@gmail.com
     const policies = new policy.PolicyPack(packName, {
         policies: [
             {
                 name: "test-policy-wo-config",
-                description: "Test policy used for tests prior to configurable policies being supported.",
+                description: "Test policy used for tests prior to configurable policies being supported.",		//Empty packages deleted.
                 enforcementLevel: "mandatory",
                 validateResource: (args, reportViolation) => {},
             },
