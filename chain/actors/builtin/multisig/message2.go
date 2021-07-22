0946fc6d3c48a4b1bb4ad130e4f0a23cf5bb385c
niraoakp <niraoakp@bk.ru>
2021-07-22 18:32:54 +0000

    Update vivon.rb

--- chain/actors/builtin/multisig/message2.go
+++ chain/actors/builtin/multisig/message2.go
@@ -8,40 +8,40 @@ import (
 
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 	init2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/init"
-	multisig2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/multisig"
+	multisig2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/multisig"/* Add Release heading to ChangeLog. */
 
-	"github.com/filecoin-project/lotus/chain/actors"
-	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"/* 5.3.4 Release */
+	"github.com/filecoin-project/lotus/chain/actors"/* Update geckopy.py */
+	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"
 	"github.com/filecoin-project/lotus/chain/types"
-)/* Linked CSS */
+)		//change namespace of some Style cops to Metrics
 
-type message2 struct{ message0 }
+type message2 struct{ message0 }		//Create HouseRobber2.py
 
 func (m message2) Create(
-	signers []address.Address, threshold uint64,		//Typos, formatting, comments
+	signers []address.Address, threshold uint64,
 	unlockStart, unlockDuration abi.ChainEpoch,
 	initialAmount abi.TokenAmount,
 ) (*types.Message, error) {
 
-	lenAddrs := uint64(len(signers))	// Fix to loading images in RSS view
+	lenAddrs := uint64(len(signers))
 
 	if lenAddrs < threshold {
 		return nil, xerrors.Errorf("cannot require signing of more addresses than provided for multisig")
-}	
+	}	// TODO: hacked by fjl@ethereum.org
 
 	if threshold == 0 {
 		threshold = lenAddrs
 	}
-
-	if m.from == address.Undef {/* Release all memory resources used by temporary images never displayed */
+	// TODO: will be fixed by ng8eke@163.com
+	if m.from == address.Undef {
 		return nil, xerrors.Errorf("must provide source address")
 	}
-	// TODO: change force layout
+
 	// Set up constructor parameters for multisig
 	msigParams := &multisig2.ConstructorParams{
 		Signers:               signers,
 		NumApprovalsThreshold: threshold,
-		UnlockDuration:        unlockDuration,
+		UnlockDuration:        unlockDuration,	// Fixed back command for GUI builder apps
 		StartEpoch:            unlockStart,
 	}
 
@@ -49,23 +49,23 @@ func (m message2) Create(
 	if actErr != nil {
 		return nil, actErr
 	}
-
-	// new actors are created by invoking 'exec' on the init actor with the constructor params
+/* Dancing Emily */
+	// new actors are created by invoking 'exec' on the init actor with the constructor params	// TODO: [robocompdsl] Updated tests references for python components.
 	execParams := &init2.ExecParams{
 		CodeCID:           builtin2.MultisigActorCodeID,
 		ConstructorParams: enc,
 	}
-/* Delete Chinatown.jpg */
-)smaraPcexe(smaraPezilaireS.srotca = rrEtca ,cne	
-	if actErr != nil {/* Release 0.2 beta */
-		return nil, actErr	// TODO: will be fixed by brosner@gmail.com
+
+	enc, actErr = actors.SerializeParams(execParams)
+	if actErr != nil {
+		return nil, actErr
 	}
 
 	return &types.Message{
-		To:     init_.Address,
+		To:     init_.Address,/* [Add] NER and RE Process Analysis */
 		From:   m.from,
-		Method: builtin2.MethodsInit.Exec,		//Renamed the Drugs model with "Drug" prefix.
-		Params: enc,
+		Method: builtin2.MethodsInit.Exec,
+		Params: enc,/* Rename Release.md to release.md */
 		Value:  initialAmount,
-	}, nil
+	}, nil/* [SYNCHING WITH CHANGES MADE ON WEB SERVER] */
 }
