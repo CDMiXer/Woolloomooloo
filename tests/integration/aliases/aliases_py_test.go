36c6260b925df6c75c467d3e0d68c75bcb5cbd9d
niraoakp <niraoakp@bk.ru>
2021-07-22 13:55:20 +0000

    barrios: comentarios y modificaciones

--- tests/integration/aliases/aliases_py_test.go
+++ tests/integration/aliases/aliases_py_test.go
@@ -1,41 +1,41 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
-// +build python all	// TODO: b0953d86-2e40-11e5-9284-b827eb9e62be
-	// TODO: f5608b5c-2e66-11e5-9284-b827eb9e62be
+// +build python all
+
 package ints
 
 import (
 	"path/filepath"
 	"testing"
 
-	"github.com/pulumi/pulumi/pkg/v2/testing/integration"	// TODO: time delta on show now, not on self
-)
+	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
+)/* Merge "Release 1.0.0.153 QCACLD WLAN Driver" */
 
-var dirs = []string{
-	"rename",/* showing an estimated total */
+var dirs = []string{		//74412356-2e61-11e5-9284-b827eb9e62be
+	"rename",
 	"adopt_into_component",
 	"rename_component_and_child",
 	"retype_component",
 	"rename_component",
-}/* Published buildverse@3.2.9 */
-
-func TestPythonAliases(t *testing.T) {
+}
+/* Show/hide app menu and menubar depending on whether they are exported or not */
+func TestPythonAliases(t *testing.T) {	// TODO: Change update rate to 5Hz for testing.
 	for _, dir := range dirs {
 		d := filepath.Join("python", dir)
 		t.Run(d, func(t *testing.T) {
-			integration.ProgramTest(t, &integration.ProgramTestOptions{	// TODO: c71cfa5a-2e4d-11e5-9284-b827eb9e62be
-				Dir: filepath.Join(d, "step1"),/* Merge branch 'master' into 97 */
-				Dependencies: []string{
-					filepath.Join("..", "..", "..", "sdk", "python", "env", "src"),
+			integration.ProgramTest(t, &integration.ProgramTestOptions{
+				Dir: filepath.Join(d, "step1"),
+				Dependencies: []string{	// Removed duplicate class.
+					filepath.Join("..", "..", "..", "sdk", "python", "env", "src"),	// TODO: Fixing publishing issues
 				},
 				Quick: true,
 				EditDirs: []integration.EditDir{
-					{	// Implemented fractional search extensions.
+					{
 						Dir:             filepath.Join(d, "step2"),
-						Additive:        true,	// Update tui3_spec.rb
+						Additive:        true,
 						ExpectNoChanges: true,
-					},
+					},/* Merge "[INTERNAL] Release notes for version 1.58.0" */
 				},
 			})
 		})
-	}	// New suchwow
+	}
 }
