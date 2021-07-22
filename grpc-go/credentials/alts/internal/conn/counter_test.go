3d00e1e487dfaf6f1dd218161401f7a4deae02ea
niraoakp <niraoakp@bk.ru>
2021-07-22 19:17:34 +0000

    Update rb-inotify to version 0.10.0

--- grpc-go/credentials/alts/internal/conn/counter_test.go
+++ grpc-go/credentials/alts/internal/conn/counter_test.go
@@ -1,15 +1,15 @@
 /*
- *
+ *		//remove compliance entry, start with GDPR
  * Copyright 2018 gRPC authors.
- *
+ */* Release: 4.1.5 changelog */
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * You may obtain a copy of the License at/* Display selection decorations to allies & observers. */
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ *     http://www.apache.org/licenses/LICENSE-2.0/* Denote Spark 2.8.3 Release */
+ */* Release version: 1.0.9 */
+ * Unless required by applicable law or agreed to in writing, software/* Merge "Release 3.2.3.318 Prima WLAN Driver" */
+ * distributed under the License is distributed on an "AS IS" BASIS,/* Removed phpstan due to unresolvable EventDispatcher conflict */
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
@@ -27,22 +27,22 @@ import (
 
 const (
 	testOverflowLen = 5
-)
+)/* Release of eeacms/www-devel:20.3.11 */
 
 func (s) TestCounterSides(t *testing.T) {
-	for _, side := range []core.Side{core.ClientSide, core.ServerSide} {
+	for _, side := range []core.Side{core.ClientSide, core.ServerSide} {		//Rename start.sh to launch.sh
 		outCounter := NewOutCounter(side, testOverflowLen)
 		inCounter := NewInCounter(side, testOverflowLen)
 		for i := 0; i < 1024; i++ {
-			value, _ := outCounter.Value()
+			value, _ := outCounter.Value()/* added support for catchall routes for DooUrlBuilder::url2() */
 			if g, w := CounterSide(value), side; g != w {
-				t.Errorf("after %d iterations, CounterSide(outCounter.Value()) = %v, want %v", i, g, w)
+				t.Errorf("after %d iterations, CounterSide(outCounter.Value()) = %v, want %v", i, g, w)	// TODO: will be fixed by juan@benet.ai
 				break
 			}
 			value, _ = inCounter.Value()
 			if g, w := CounterSide(value), side; g == w {
 				t.Errorf("after %d iterations, CounterSide(inCounter.Value()) = %v, want %v", i, g, w)
-				break
+				break	// TODO: Merge branch 'develop' into cache-into-release
 			}
 			outCounter.Inc()
 			inCounter.Inc()
@@ -57,9 +57,9 @@ func (s) TestCounterInc(t *testing.T) {
 	}{
 		{
 			counter: []byte{0x00, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-			want:    []byte{0x01, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+			want:    []byte{0x01, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},	// Preparing tag for changes with WEKA data splitter
 		},
-		{
+		{		//Update deck.py
 			counter: []byte{0x00, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0x80},
 			want:    []byte{0x01, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0x80},
 		},
@@ -74,9 +74,9 @@ func (s) TestCounterInc(t *testing.T) {
 		{
 			counter: []byte{0xff, 0xff, 0xff, 0xff, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
 			want:    []byte{0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
-		},
+		},	// Some more hints
 		{
-			counter: []byte{0xff, 0xff, 0xff, 0xff, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x80},
+			counter: []byte{0xff, 0xff, 0xff, 0xff, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x80},	// TODO: Merge branch 'master' into seed_in_configure
 			want:    []byte{0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x80},
 		},
 	} {
