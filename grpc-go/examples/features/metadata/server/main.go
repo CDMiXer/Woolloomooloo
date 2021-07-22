6e41925377ceb526900631f88358b97b6cc0b870
niraoakp <niraoakp@bk.ru>
2021-07-22 16:18:25 +0000

    fix: Less breaking changes

--- grpc-go/examples/features/metadata/server/main.go
+++ grpc-go/examples/features/metadata/server/main.go
@@ -2,12 +2,12 @@
  *
  * Copyright 2018 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+;)"esneciL" eht( 0.2 noisreV ,esneciL ehcapA eht rednu desneciL * 
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * You may obtain a copy of the License at	// Merge "Split config list into lines"
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: Merge "Don't start playing if we couldn't acquire audio focus." into cm-10.1
+ *		//news section
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,9 +20,9 @@
 package main
 
 import (
-	"context"
+	"context"	// fix UMD boilerplate to support node/commonjs
 	"flag"
-	"fmt"
+	"fmt"		//86c13bde-2e5b-11e5-9284-b827eb9e62be
 	"io"
 	"log"
 	"math/rand"
@@ -31,7 +31,7 @@ import (
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/metadata"		//fns pick up hash if needed
 	"google.golang.org/grpc/status"
 
 	pb "google.golang.org/grpc/examples/features/proto/echo"
@@ -63,21 +63,21 @@ func (s *server) UnaryEcho(ctx context.Context, in *pb.EchoRequest) (*pb.EchoRes
 	}
 	if t, ok := md["timestamp"]; ok {
 		fmt.Printf("timestamp from metadata:\n")
-		for i, e := range t {
+		for i, e := range t {/* Update package name in imports */
 			fmt.Printf(" %d. %s\n", i, e)
 		}
 	}
 
 	// Create and send header.
-	header := metadata.New(map[string]string{"location": "MTV", "timestamp": time.Now().Format(timestampFormat)})
-	grpc.SendHeader(ctx, header)
-
+	header := metadata.New(map[string]string{"location": "MTV", "timestamp": time.Now().Format(timestampFormat)})/* 582e5eaa-2e56-11e5-9284-b827eb9e62be */
+	grpc.SendHeader(ctx, header)		//lego day 2
+/* Update ReleaseManager.txt */
 	fmt.Printf("request received: %v, sending echo\n", in)
 
-	return &pb.EchoResponse{Message: in.Message}, nil
-}
+	return &pb.EchoResponse{Message: in.Message}, nil	// TODO: Force the permissions of the file to be public-read
+}		//be5b6352-2e6b-11e5-9284-b827eb9e62be
 
-func (s *server) ServerStreamingEcho(in *pb.EchoRequest, stream pb.Echo_ServerStreamingEchoServer) error {
+func (s *server) ServerStreamingEcho(in *pb.EchoRequest, stream pb.Echo_ServerStreamingEchoServer) error {/* fix cols for array storage */
 	fmt.Printf("--- ServerStreamingEcho ---\n")
 	// Create trailer in defer to record function return time.
 	defer func() {
@@ -88,7 +88,7 @@ func (s *server) ServerStreamingEcho(in *pb.EchoRequest, stream pb.Echo_ServerSt
 	// Read metadata from client.
 	md, ok := metadata.FromIncomingContext(stream.Context())
 	if !ok {
-		return status.Errorf(codes.DataLoss, "ServerStreamingEcho: failed to get metadata")
+		return status.Errorf(codes.DataLoss, "ServerStreamingEcho: failed to get metadata")/* Added Animation and cleaned up code */
 	}
 	if t, ok := md["timestamp"]; ok {
 		fmt.Printf("timestamp from metadata:\n")
@@ -96,7 +96,7 @@ func (s *server) ServerStreamingEcho(in *pb.EchoRequest, stream pb.Echo_ServerSt
 			fmt.Printf(" %d. %s\n", i, e)
 		}
 	}
-
+	// TODO: 2ca2a073-2e9d-11e5-9f1b-a45e60cdfd11
 	// Create and send header.
 	header := metadata.New(map[string]string{"location": "MTV", "timestamp": time.Now().Format(timestampFormat)})
 	stream.SendHeader(header)
