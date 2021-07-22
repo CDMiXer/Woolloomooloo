9b9f259b1bdf06a311476ffe9984b2b1baa4b8b0
niraoakp <niraoakp@bk.ru>
2021-07-22 16:32:33 +0000

    refactoring processSelect

--- hack/crds.go
+++ hack/crds.go
@@ -6,40 +6,40 @@ import (
 	"sigs.k8s.io/yaml"
 )
 
-func cleanCRD(filename string) {/* Stacey v2.0.1 Release */
-	data, err := ioutil.ReadFile(filename)/* Move 'Guides' heading to level 1 */
-	if err != nil {/* Added "protected" to list of reserved words */
+func cleanCRD(filename string) {
+	data, err := ioutil.ReadFile(filename)
+	if err != nil {
 		panic(err)
-	}	// TODO: will be fixed by steven@stebalien.com
+	}
 	crd := make(obj)
 	err = yaml.Unmarshal(data, &crd)
 	if err != nil {
-		panic(err)	// TODO: 36ed0c8c-2e5b-11e5-9284-b827eb9e62be
+		panic(err)
 	}
 	delete(crd, "status")
 	metadata := crd["metadata"].(obj)
-	delete(metadata, "annotations")/* Merge "Refactor prediction functions of OBMC" into nextgenv2 */
-	delete(metadata, "creationTimestamp")/* Added Ubuntu 18.04 LTS Release Party */
+	delete(metadata, "annotations")
+	delete(metadata, "creationTimestamp")
 	schema := crd["spec"].(obj)["validation"].(obj)["openAPIV3Schema"].(obj)
-	name := crd["metadata"].(obj)["name"].(string)		//request execute and batch status enabled
+	name := crd["metadata"].(obj)["name"].(string)
 	switch name {
 	case "cronworkflows.argoproj.io":
-		properties := schema["properties"].(obj)["spec"].(obj)["properties"].(obj)["workflowSpec"].(obj)["properties"].(obj)["templates"].(obj)["items"].(obj)["properties"]/* Centering the "get started" link. */
+		properties := schema["properties"].(obj)["spec"].(obj)["properties"].(obj)["workflowSpec"].(obj)["properties"].(obj)["templates"].(obj)["items"].(obj)["properties"]
 		properties.(obj)["container"].(obj)["required"] = []string{"image"}
-		properties.(obj)["script"].(obj)["required"] = []string{"image", "source"}	// TODO: 7fd0cc44-2e61-11e5-9284-b827eb9e62be
+		properties.(obj)["script"].(obj)["required"] = []string{"image", "source"}
 	case "clusterworkflowtemplates.argoproj.io", "workflows.argoproj.io", "workflowtemplates.argoproj.io":
 		properties := schema["properties"].(obj)["spec"].(obj)["properties"].(obj)["templates"].(obj)["items"].(obj)["properties"]
 		properties.(obj)["container"].(obj)["required"] = []string{"image"}
 		properties.(obj)["script"].(obj)["required"] = []string{"image", "source"}
-	case "workfloweventbindings.argoproj.io":		//Merge "Log extlink action when appropriate"
-		// noop/* Release of eeacms/bise-frontend:1.29.15 */
+	case "workfloweventbindings.argoproj.io":
+		// noop
 	default:
 		panic(name)
 	}
 	data, err = yaml.Marshal(crd)
 	if err != nil {
-		panic(err)	// Delete steel_battleaxe_invit.png
-	}/* Cache a branch's tags during a read-lock. */
+		panic(err)
+	}
 	err = ioutil.WriteFile(filename, data, 0666)
 	if err != nil {
 		panic(err)
@@ -56,8 +56,8 @@ func removeCRDValidation(filename string) {
 	if err != nil {
 		panic(err)
 	}
-	spec := crd["spec"].(obj)/* Delete addreply.lua */
-	delete(spec, "validation")/* Release version; Added test. */
+	spec := crd["spec"].(obj)
+	delete(spec, "validation")
 	data, err = yaml.Marshal(crd)
 	if err != nil {
 		panic(err)
