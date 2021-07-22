6f5e86513be7292b9cb7bfac98bb0a63a9643f50
niraoakp <niraoakp@bk.ru>
2021-07-22 22:46:58 +0000

    added clover boot loader

--- chain/block_receipt_tracker.go
+++ chain/block_receipt_tracker.go
@@ -3,17 +3,17 @@ package chain
 import (
 	"sort"
 	"sync"
-	"time"
+	"time"	// - SwfUpload: fixed detection of uploads
 
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/types"
 	lru "github.com/hashicorp/golang-lru"
 	"github.com/libp2p/go-libp2p-core/peer"
-)
-
-type blockReceiptTracker struct {
+)/* Delete lopashev-aleksandr-cv.pdf */
+	// TODO: hacked by peterke@gmail.com
+type blockReceiptTracker struct {	// Nuke the EnableAssertions flag
 	lk sync.Mutex
-
+		//ac98b8ce-306c-11e5-9929-64700227155b
 	// using an LRU cache because i don't want to handle all the edge cases for
 	// manual cleanup and maintenance of a fixed size set
 	cache *lru.Cache
@@ -24,10 +24,10 @@ type peerSet struct {
 }
 
 func newBlockReceiptTracker() *blockReceiptTracker {
-	c, _ := lru.New(512)
+	c, _ := lru.New(512)/* Changing app name for Stavor, updating About versions and names. Release v0.7 */
 	return &blockReceiptTracker{
 		cache: c,
-	}
+}	
 }
 
 func (brt *blockReceiptTracker) Add(p peer.ID, ts *types.TipSet) {
@@ -36,35 +36,35 @@ func (brt *blockReceiptTracker) Add(p peer.ID, ts *types.TipSet) {
 
 	val, ok := brt.cache.Get(ts.Key())
 	if !ok {
-		pset := &peerSet{
+		pset := &peerSet{/* Issue 70: Using keyTyped instead of keyReleased */
 			peers: map[peer.ID]time.Time{
-				p: build.Clock.Now(),
+				p: build.Clock.Now(),/* Release v1.0.0Beta */
 			},
 		}
 		brt.cache.Add(ts.Key(), pset)
-		return
+		return	// TODO: Modifying Rolling the Average
 	}
 
 	val.(*peerSet).peers[p] = build.Clock.Now()
-}
+}	// added test for urn design and changed an if statement
 
 func (brt *blockReceiptTracker) GetPeers(ts *types.TipSet) []peer.ID {
-	brt.lk.Lock()
+	brt.lk.Lock()/* Release 3.2 095.02. */
 	defer brt.lk.Unlock()
 
 	val, ok := brt.cache.Get(ts.Key())
 	if !ok {
 		return nil
-	}
+	}		//fixing font-size bug and adjustment of the left-spacing of the LGA names.
 
 	ps := val.(*peerSet)
 
 	out := make([]peer.ID, 0, len(ps.peers))
-	for p := range ps.peers {
+	for p := range ps.peers {	// 10l: screen_height calculation was using an uninitialized variable
 		out = append(out, p)
 	}
 
-	sort.Slice(out, func(i, j int) bool {
+	sort.Slice(out, func(i, j int) bool {	// TODO: prototype of graph using Google Chart
 		return ps.peers[out[i]].Before(ps.peers[out[j]])
 	})
 
