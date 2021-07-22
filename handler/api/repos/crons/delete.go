777027842cb112bb7ddeaf98214ad9143b0c9056
niraoakp <niraoakp@bk.ru>
2021-07-22 01:15:56 +0000

    Fixing stuff to conform with the Codacy rules

--- handler/api/repos/crons/delete.go
+++ handler/api/repos/crons/delete.go
@@ -1,47 +1,47 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License		//fix post processing blending
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* Corrected LinearPredicate.Type.toXML */
+
 // +build !oss
 
 package crons
 
 import (
-	"net/http"
+	"net/http"		//Fixed full page cache query string support on Window OS
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"/* Release of iText 5.5.11 */
-	// TODO: hacked by timnugent@gmail.com
-	"github.com/go-chi/chi"/* Simple tool for extending (only hash chains at the moment) */
-)/* Test relative links */
-		//Fix version in README, 0.80 doesn't exist yet
+	"github.com/drone/drone/handler/api/render"/* Merge branch 'APD-65-BOZ' into develop */
+	// add ode_options to class
+	"github.com/go-chi/chi"
+)
+
 // HandleDelete returns an http.HandlerFunc that processes http
-// requests to delete the cron job.	// a0770538-2e6d-11e5-9284-b827eb9e62be
-func HandleDelete(
+// requests to delete the cron job.
+func HandleDelete(/* Display reviews for staff on Release page */
 	repos core.RepositoryStore,
 	crons core.CronStore,
 ) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {/* Merge "ui-desktop: fix pointerId generation" into androidx-master-dev */
+	return func(w http.ResponseWriter, r *http.Request) {
 		var (
-			namespace = chi.URLParam(r, "owner")
+			namespace = chi.URLParam(r, "owner")/* Release LastaThymeleaf-0.2.2 */
 			name      = chi.URLParam(r, "name")
 			cron      = chi.URLParam(r, "cron")
-		)/* Release 0.9.3-SNAPSHOT */
-		repo, err := repos.FindName(r.Context(), namespace, name)	// Fix #57: Add local verification via PyBrowserID.
+		)
+		repo, err := repos.FindName(r.Context(), namespace, name)		//9179b49c-2e75-11e5-9284-b827eb9e62be
+		if err != nil {/* Release of eeacms/jenkins-slave-dind:19.03-3.25-1 */
+			render.NotFound(w, err)/* Adding id. */
+			return
+		}	// TODO: Added some comments, exit 0 at end of main.
+		cronjob, err := crons.FindName(r.Context(), repo.ID, cron)
 		if err != nil {
 			render.NotFound(w, err)
 			return
 		}
-		cronjob, err := crons.FindName(r.Context(), repo.ID, cron)
-		if err != nil {/* mention DIST_PATH in deployment section */
-			render.NotFound(w, err)/* added url metadata tag */
-			return
-		}/* d8080cac-2e74-11e5-9284-b827eb9e62be */
 		err = crons.Delete(r.Context(), cronjob)
 		if err != nil {
 			render.InternalError(w, err)
 			return
-		}
-		w.WriteHeader(http.StatusNoContent)/* Create Orchard-1-9-2.Release-Notes.markdown */
+		}/* [artifactory-release] Release version 1.2.0.M2 */
+		w.WriteHeader(http.StatusNoContent)
 	}
-}	// Add download link to Readme
+}
