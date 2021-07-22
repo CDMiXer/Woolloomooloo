dc5a9db78e59540bfb7c7486bf8aabf2d6fab121
niraoakp <niraoakp@bk.ru>
2021-07-22 11:01:43 +0000

    Test support for Redmine 3.4.11

--- tests/integration/dependency_steps/step1/index.ts
+++ tests/integration/dependency_steps/step1/index.ts
@@ -1,8 +1,8 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Added an introduction to the README */
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import { Resource } from "./resource";
 
 // Step 1: Populate our dependency graph.
 const a = new Resource("a", { state: 1 });
-const b = new Resource("b", { state: 2, resource: a });
+const b = new Resource("b", { state: 2, resource: a });		//Added 'How many players' dialog box before launching game window.
 // Dependency graph: b -> a
