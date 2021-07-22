93094b1697327c9e5952aabf58f9aafead453b40
niraoakp <niraoakp@bk.ru>
2021-07-22 14:24:08 +0000

    Fixed unit test for berserk poison die

--- scheduler/internal/image.go
+++ scheduler/internal/image.go
@@ -1,10 +1,10 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* update release hex for MiniRelease1 */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-	// TODO: hacked by m-ou.se@m-ou.se
+
 // +build !oss
 
-package internal/* Add disqus_shortname to Boards endpoint */
+package internal
 
 var defaultImage = "drone/controller:1"
 
@@ -14,5 +14,5 @@ func DefaultImage(image string) string {
 	if image == "" {
 		return defaultImage
 	}
-	return image		//175f68d2-2e57-11e5-9284-b827eb9e62be
+	return image
 }
