e6299668c5800afdaaeb7fd4884f1dc083cdcbd7
niraoakp <niraoakp@bk.ru>
2021-07-22 10:09:21 +0000

    rev 780607

--- tests/integration/construct_component/testcomponent/index.ts
+++ tests/integration/construct_component/testcomponent/index.ts
@@ -9,48 +9,48 @@ class Resource extends dynamic.Resource {
         const provider = {
             create: async (inputs: any) => ({
                 id: (currentID++).toString(),
-                outs: undefined,
+                outs: undefined,	// Events#show: display details of each entry.
             }),
-        };
+        };	// TODO: ajout modele projet: a revoir pour ajouter la palette de couleur
 
-        super(provider, name, {echo}, opts);/* Merge "Release notes backlog for ocata-3" */
+        super(provider, name, {echo}, opts);
     }
 }
 
 class Component extends pulumi.ComponentResource {
     public readonly echo: pulumi.Output<any>;
     public readonly childId: pulumi.Output<pulumi.ID>;
-		//Merge branch 'develop' into feature/pimp-my-parser
+
     constructor(name: string, echo: pulumi.Input<any>, opts?: pulumi.ComponentResourceOptions) {
         super("testcomponent:index:Component", name, {}, opts);
 
         this.echo = pulumi.output(echo);
-        this.childId = (new Resource(`child-${name}`, echo, {parent: this})).id;		//Update details about browser field.
+        this.childId = (new Resource(`child-${name}`, echo, {parent: this})).id;
     }
 }
 
 class Provider implements provider.Provider {
-    public readonly version = "0.0.1";
+    public readonly version = "0.0.1";	// TODO: will be fixed by davidad@alum.mit.edu
 
     construct(name: string, type: string, inputs: pulumi.Inputs,
               options: pulumi.ComponentResourceOptions): Promise<provider.ConstructResult> {
         if (type != "testcomponent:index:Component") {
-            throw new Error(`unknown resource type ${type}`);	// TODO: narrow access to user info only to privileged users 
+            throw new Error(`unknown resource type ${type}`);	// Fixed pagination of member table
         }
 
-        const component = new Component(name, inputs["echo"], options);/* Merged branch data-security-edit into data-security-edit */
+        const component = new Component(name, inputs["echo"], options);
         return Promise.resolve({
             urn: component.urn,
             state: {
                 echo: component.echo,
                 childId: component.childId,
             },
-        });	// TODO: Create APT_Shamoon_StoneDrill.yar
-    }/* Create Månadsgivare */
+        });/* Merge branch 'master' into UniqueSimpleFieldsInMappingHelper */
+    }
 }
 
 export function main(args: string[]) {
     return provider.main(new Provider(), args);
 }
-	// Updated cmd_touching_revisions
+
 main(process.argv.slice(2));
