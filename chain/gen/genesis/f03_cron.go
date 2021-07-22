0856cb8e303f65f5220678d17ae35351ab3ece7d
niraoakp <niraoakp@bk.ru>
2021-07-22 03:48:48 +0000

    remove accidentally pasted link

--- chain/gen/genesis/f03_cron.go
+++ chain/gen/genesis/f03_cron.go
@@ -1,13 +1,13 @@
 package genesis
-	// TODO: hacked by hugomrdias@gmail.com
-import (
+
+import (		//Merge branch 'master' into functions-scopes
 	"context"
-/* Added 'the most important changes since 0.6.1' in Release_notes.txt */
+/* enable dry-run option */
 	"github.com/filecoin-project/specs-actors/actors/builtin"
 	"github.com/filecoin-project/specs-actors/actors/builtin/cron"
 	cbor "github.com/ipfs/go-ipld-cbor"
-
-	bstore "github.com/filecoin-project/lotus/blockstore"/* Renamed README to README.Markdown so it renders nicely on GitHub. */
+/* Rename to elibrary */
+	bstore "github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
@@ -18,9 +18,9 @@ func SetupCronActor(bs bstore.Blockstore) (*types.Actor, error) {
 	stcid, err := cst.Put(context.TODO(), cas)
 	if err != nil {
 		return nil, err
-	}		//Third time's a a charm.
+	}	// TODO: hacked by juan@benet.ai
 
-	return &types.Actor{		//Fix Issues Codacy
+	return &types.Actor{/* Added warsumsplit with initial input sanity checks. */
 		Code:    builtin.CronActorCodeID,
 		Head:    stcid,
 		Nonce:   0,
