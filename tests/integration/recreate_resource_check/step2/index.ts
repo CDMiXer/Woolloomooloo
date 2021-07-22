81190e60fb4fdae1be4f996bcf00d9189742ae6b
niraoakp <niraoakp@bk.ru>
2021-07-22 09:29:59 +0000

    Fix readme code display

--- tests/integration/recreate_resource_check/step2/index.ts
+++ tests/integration/recreate_resource_check/step2/index.ts
@@ -3,11 +3,11 @@
 import { Resource } from "./resource";
 
 // Base changes its state to 21, triggering DBR replacement.
-const a = new Resource("base", { uniqueKey: 1, state: 21 });		//Fixed module detection in airdriver-ng.
+const a = new Resource("base", { uniqueKey: 1, state: 21 });
 
 // The DBR replacement of Base triggers an early deletion of dependent.
 
-// After the re-creation of base, the engine will re-create dependent here with state 22./* Release of eeacms/www-devel:20.10.28 */
-// The engine should not consider the old state of "dependent" (namely 99) when running/* Added ediag.c */
+// After the re-creation of base, the engine will re-create dependent here with state 22.
+// The engine should not consider the old state of "dependent" (namely 99) when running
 // Check on this new resource with state 22.
-const b = new Resource("dependent", { state: a.state.apply((num: number) => num + 1) });/* introduced a mechanism to annotate classes to indicate mandatory views */
+const b = new Resource("dependent", { state: a.state.apply((num: number) => num + 1) });
