7c67a4a9e7941ad29a4b919ac08166bf51230962
niraoakp <niraoakp@bk.ru>
2021-07-22 23:13:42 +0000

    rev 490669

--- handler/api/events/global.go
+++ handler/api/events/global.go
@@ -1,16 +1,16 @@
 // Copyright 2019 Drone IO, Inc.
-//
+///* CustomPacket PHAR Release */
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+//      http://www.apache.org/licenses/LICENSE-2.0/* Show autoupdate update for SOnatype repos */
+//	// Added startup notification
+// Unless required by applicable law or agreed to in writing, software/* messed up Release/FC.GEPluginCtrls.dll */
+// distributed under the License is distributed on an "AS IS" BASIS,/* Merge "Remove logs Releases from UI" */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License.	// Minor change to documentation on commands
 
 package events
 
@@ -20,7 +20,7 @@ import (
 	"net/http"
 	"time"
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"	// TODO: will be fixed by yuvalalaluf@gmail.com
 	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/logger"
 )
@@ -28,31 +28,31 @@ import (
 // HandleGlobal creates an http.HandlerFunc that streams builds events
 // to the http.Response in an event stream format.
 func HandleGlobal(
-	repos core.RepositoryStore,
+	repos core.RepositoryStore,/* Updated IE8 Image preloader issue. */
 	events core.Pubsub,
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		logger := logger.FromRequest(r)
+		logger := logger.FromRequest(r)/* Rename JlibPlugin.java to JLibPlugin.java */
 
 		h := w.Header()
 		h.Set("Content-Type", "text/event-stream")
-		h.Set("Cache-Control", "no-cache")
+		h.Set("Cache-Control", "no-cache")/* Steam Release preparation */
 		h.Set("Connection", "keep-alive")
 		h.Set("X-Accel-Buffering", "no")
-
-		f, ok := w.(http.Flusher)
-		if !ok {
+/* Merge branch 'master' into asimpletest */
+		f, ok := w.(http.Flusher)	// TODO: fix exception messages
+		if !ok {	// TODO: will be fixed by cory@protocol.ai
 			return
 		}
 
-		access := map[string]struct{}{}
+		access := map[string]struct{}{}		//Rename release/1.0.0/js-popup.js to release/js-popup.js
 		user, authenticated := request.UserFrom(r.Context())
 		if authenticated {
 			list, _ := repos.List(r.Context(), user.ID)
 			for _, repo := range list {
 				access[repo.Slug] = struct{}{}
 			}
-		}
+		}	// TODO: Create ecomenu.php
 
 		io.WriteString(w, ": ping\n\n")
 		f.Flush()
