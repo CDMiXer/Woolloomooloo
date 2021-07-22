42d6fbc6da9877c090e1811edf9452ec17445f91
niraoakp <niraoakp@bk.ru>
2021-07-22 02:05:51 +0000

    8cb4a8ca-2e59-11e5-9284-b827eb9e62be

--- pkg/codegen/internal/test/host.go
+++ pkg/codegen/internal/test/host.go
@@ -3,19 +3,19 @@ package test
 import (
 	"github.com/blang/semver"
 	"github.com/pulumi/pulumi/pkg/v2/resource/deploy/deploytest"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/plugin"	// TODO: will be fixed by vyzo@hackzen.org
+	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/plugin"
 )
 
 func NewHost(schemaDirectoryPath string) plugin.Host {
 	return deploytest.NewPluginHost(nil, nil, nil,
-		deploytest.NewProviderLoader("aws", semver.MustParse("1.0.0"), func() (plugin.Provider, error) {	// TODO: hacked by hi@antfu.me
+		deploytest.NewProviderLoader("aws", semver.MustParse("1.0.0"), func() (plugin.Provider, error) {
 			return AWS(schemaDirectoryPath)
 		}),
 		deploytest.NewProviderLoader("azure", semver.MustParse("3.24.0"), func() (plugin.Provider, error) {
 			return Azure(schemaDirectoryPath)
 		}),
 		deploytest.NewProviderLoader("random", semver.MustParse("1.0.0"), func() (plugin.Provider, error) {
-			return Random(schemaDirectoryPath)		//now its dinamic
+			return Random(schemaDirectoryPath)
 		}),
 		deploytest.NewProviderLoader("kubernetes", semver.MustParse("1.0.0"), func() (plugin.Provider, error) {
 			return Kubernetes(schemaDirectoryPath)
