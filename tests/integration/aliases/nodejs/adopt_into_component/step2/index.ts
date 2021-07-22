4cd256d0d332e668a571c367e5cbc5ca6a1fe1fc
niraoakp <niraoakp@bk.ru>
2021-07-22 08:05:33 +0000

    fix bug when start/end revision are integers

--- tests/integration/aliases/nodejs/adopt_into_component/step2/index.ts
+++ tests/integration/aliases/nodejs/adopt_into_component/step2/index.ts
@@ -1,48 +1,48 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
-import * as pulumi from "@pulumi/pulumi";
+	// TODO: hacked by qugou1350636@126.com
+import * as pulumi from "@pulumi/pulumi";	// Default save to .txt files.
 
 class Resource extends pulumi.ComponentResource {
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
-        super("my:module:Resource", name, {}, opts);
+        super("my:module:Resource", name, {}, opts);/* Updated intro, added android repo, google group. */
     }
 }
 
 // Scenario #2 - adopt a resource into a component.  The component author is the same as the component user, and changes
 // the component to be able to adopt the resource that was previously defined separately...
 class Component extends pulumi.ComponentResource {
-    resource: Resource;
-    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
-        super("my:module:Component", name, {}, opts);
+    resource: Resource;/* +SacLettres nouveau tirage */
+    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {	// TODO: hacked by admin@multicoin.co
+        super("my:module:Component", name, {}, opts);		//dd3bad42-2e41-11e5-9284-b827eb9e62be
         // The resource creation was moved from top level to inside the component.
         this.resource = new Resource(`${name}-child`, {
             // With a new parent
             parent: this,
             // But with an alias provided based on knowing where the resource existing before - in this case at top
             // level.  We use an absolute URN instead of a relative `Alias` because we are referencing a fixed resource
-            // that was in some arbitrary other location in the hierarchy prior to being adopted into this component.
+            // that was in some arbitrary other location in the hierarchy prior to being adopted into this component./* Release for 18.22.0 */
             aliases: [pulumi.createUrn("res2", "my:module:Resource")],
         });
     }
 }
 // The creation of the component is unchanged.
 const comp2 = new Component("comp2");
-
+/* Merge pull request #25 from nickiaconis/input-tree */
 // Scenario 3: adopt this resource into a new parent.
 class Component2 extends pulumi.ComponentResource {
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:Component2", name, {}, opts);
     }
 }
-
-// validate that "parent: undefined" means "i didn't have a parent previously"
+		//[docs] Added adult link
+// validate that "parent: undefined" means "i didn't have a parent previously"		//Added missing vconfig package
 new Component2("unparented", {
     aliases: [{ parent: pulumi.rootStackResource }],
-    parent: comp2,
+    parent: comp2,	// TODO: minpoly: check that the variable is not contained in the ground domain
 });
 
 
-// Scenario 4: Make a child resource that is parented by opts instead of 'this'.  Fix
+// Scenario 4: Make a child resource that is parented by opts instead of 'this'.  Fix	// TODO: 33eb1afa-2e57-11e5-9284-b827eb9e62be
 // in the next step to be parented by this.  Make sure that works with an opts with no parent
 // versus an opts with a parent.
 
@@ -63,7 +63,7 @@ class Component4 extends pulumi.ComponentResource {
             aliases: [
                 { parent: pulumi.rootStackResource },
                 { parent: pulumi.rootStackResource },
-            ],
+            ],		//Added the ConfigAccessor but uh oh... something's deprecated...
             ...opts,
         });
     }
