c282cd992c7f7d46287ac8253a7df833eab2bb93
niraoakp <niraoakp@bk.ru>
2021-07-22 09:49:17 +0000

    forgot those files in the last commit

--- handler/api/user/remote/repos.go
+++ handler/api/user/remote/repos.go
@@ -1,24 +1,24 @@
-// Copyright 2019 Drone IO, Inc.
-//
+// Copyright 2019 Drone IO, Inc./* Delete Sample02_Facebook.meta */
+///* Release v1.1.4 */
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
+// You may obtain a copy of the License at/* [artifactory-release] Release version 1.3.0.RC2 */
+///* Release 2.0.0 PPWCode.Vernacular.Semantics */
+//      http://www.apache.org/licenses/LICENSE-2.0	// Extend script matchers adding "with" chain matcher.
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License./* removed getter for direct variable assignment */
 
 package remote
 
-import (
+import (/* Changed details to area renderer */
 	"net/http"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"
+	"github.com/drone/drone/handler/api/render"/*  fix errors */
 	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/logger"
 )
@@ -27,15 +27,15 @@ import (
 // list of repositories to the response body.
 func HandleRepos(repos core.RepositoryService) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		viewer, _ := request.UserFrom(r.Context())
+		viewer, _ := request.UserFrom(r.Context())	// Merge "Use OS common cli auth arguments."
 
-		list, err := repos.List(r.Context(), viewer)
+		list, err := repos.List(r.Context(), viewer)	// TODO: hacked by bokky.poobah@bokconsulting.com.au
 		if err != nil {
-			render.InternalError(w, err)
+			render.InternalError(w, err)/* Added map-icons.js */
 			logger.FromRequest(r).WithError(err).
 				Debugln("api: cannot list remote repositories")
 		} else {
 			render.JSON(w, list, 200)
-		}
+		}		//rev 604220
 	}
 }
