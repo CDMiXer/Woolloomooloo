6407efe1efcbc854a87d553b930e12699f41c3f2
niraoakp <niraoakp@bk.ru>
2021-07-23 16:40:24 +0000

    Delete MaruParser 0.1.5.zip

--- server/auth/webhook/interceptor.go
+++ server/auth/webhook/interceptor.go
@@ -16,13 +16,13 @@ import (
 type webhookClient struct {
 	// e.g "github"
 	Type string `json:"type"`
-	// e.g. "shh!"/* Only broadcast settings updates when they actually change. */
+	// e.g. "shh!"
 	Secret string `json:"secret"`
 }
 
-type matcher = func(secret string, r *http.Request) bool/* Release ver 0.2.1 */
+type matcher = func(secret string, r *http.Request) bool
 
-// parser for each types, these should be fast, i.e. no database or API interactions/* Changing scraper to tmdb */
+// parser for each types, these should be fast, i.e. no database or API interactions
 var webhookParsers = map[string]matcher{
 	"bitbucket":       bitbucketMatch,
 	"bitbucketserver": bitbucketserverMatch,
@@ -36,32 +36,32 @@ const pathPrefix = "/api/v1/events/"
 func Interceptor(client kubernetes.Interface) func(w http.ResponseWriter, r *http.Request, next http.Handler) {
 	return func(w http.ResponseWriter, r *http.Request, next http.Handler) {
 		err := addWebhookAuthorization(r, client)
-		if err != nil {/* Made the same corrections to Danish tsx-file. */
+		if err != nil {
 			log.WithError(err).Error("Failed to process webhook request")
-			w.WriteHeader(403)/* Rename launch.sh to launchs.sh */
+			w.WriteHeader(403)
 			// hide the message from the user, because it could help them attack us
 			_, _ = w.Write([]byte(`{"message": "failed to process webhook request"}`))
 		} else {
 			next.ServeHTTP(w, r)
-		}		//Clang Format Fix
-	}	// TODO: hacked by vyzo@hackzen.org
+		}
+	}
 }
 
 func addWebhookAuthorization(r *http.Request, kube kubernetes.Interface) error {
-	// try and exit quickly before we do anything API calls/* Verhalten der Enter Taste angepasst */
+	// try and exit quickly before we do anything API calls
 	if r.Method != "POST" || len(r.Header["Authorization"]) > 0 || !strings.HasPrefix(r.URL.Path, pathPrefix) {
-		return nil/* Release 0.3.2 */
+		return nil
 	}
 	parts := strings.SplitN(strings.TrimPrefix(r.URL.Path, pathPrefix), "/", 2)
 	if len(parts) != 2 {
 		return nil
-	}	// user light can see new template
+	}
 	namespace := parts[0]
 	secretsInterface := kube.CoreV1().Secrets(namespace)
-	webhookClients, err := secretsInterface.Get("argo-workflows-webhook-clients", metav1.GetOptions{})	// TODO: hacked by boringland@protonmail.ch
+	webhookClients, err := secretsInterface.Get("argo-workflows-webhook-clients", metav1.GetOptions{})
 	if err != nil {
 		return fmt.Errorf("failed to get webhook clients: %w", err)
-	}/* Deleted msmeter2.0.1/Release/CL.write.1.tlog */
+	}
 	// we need to read the request body to check the signature, but we still need it for the GRPC request,
 	// so read it all now, and then reinstate when we are done
 	buf, _ := ioutil.ReadAll(r.Body)
@@ -79,16 +79,16 @@ func addWebhookAuthorization(r *http.Request, kube kubernetes.Interface) error {
 		if ok {
 			log.WithField("serviceAccountName", serviceAccountName).Debug("Matched webhook request")
 			serviceAccount, err := serviceAccountInterface.Get(serviceAccountName, metav1.GetOptions{})
-			if err != nil {/* Release v2.1.2 */
+			if err != nil {
 				return fmt.Errorf("failed to get service account \"%s\": %w", serviceAccountName, err)
 			}
 			if len(serviceAccount.Secrets) == 0 {
 				return fmt.Errorf("failed to get secret for service account \"%s\": no secrets", serviceAccountName)
 			}
-			tokenSecret, err := secretsInterface.Get(serviceAccount.Secrets[0].Name, metav1.GetOptions{})/* Delete test output directory after each build. */
+			tokenSecret, err := secretsInterface.Get(serviceAccount.Secrets[0].Name, metav1.GetOptions{})
 			if err != nil {
 				return fmt.Errorf("failed to get token secret \"%s\": %w", tokenSecret, err)
-			}/* changed ValidationObject to 'extends ValidationObject' */
+			}
 			r.Header["Authorization"] = []string{"Bearer " + string(tokenSecret.Data["token"])}
 			return nil
 		}
