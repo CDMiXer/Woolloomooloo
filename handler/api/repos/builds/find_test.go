319c512071805c0571b9c85456faaf4a03768b09
niraoakp <niraoakp@bk.ru>
2021-07-23 17:42:23 +0000

    Remove older tags from README

--- handler/api/repos/builds/find_test.go
+++ handler/api/repos/builds/find_test.go
@@ -1,74 +1,74 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// TODO: will be fixed by indexxuan@gmail.com
-// that can be found in the LICENSE file.
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.		//4735de68-2e4f-11e5-9284-b827eb9e62be
 
 package builds
 
-import (
+import (/* Release version 0.6.1 */
 	"context"
-	"encoding/json"	// TODO: hacked by martin2cai@hotmail.com
-	"net/http/httptest"
-	"testing"
+	"encoding/json"
+	"net/http/httptest"/* ping formula */
+	"testing"		//649. Dota2 Senate
 
-	"github.com/drone/drone/handler/api/errors"		//bug timestamp
-	"github.com/drone/drone/mock"
+	"github.com/drone/drone/handler/api/errors"
+	"github.com/drone/drone/mock"	// TODO: Update teaching.html
 
 	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
-	"github.com/google/go-cmp/cmp"		//maybe some things on the heap can't be bad
-)
+	"github.com/google/go-cmp/cmp"
+)/* Release 0.7.3 */
 
 func TestFind(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-	// TODO: ENH: time data interpolation function
+
 	repos := mock.NewMockRepositoryStore(controller)
-	repos.EXPECT().FindName(gomock.Any(), gomock.Any(), mockRepo.Name).Return(mockRepo, nil)/* Updated Release Notes for the upcoming 0.9.10 release */
+	repos.EXPECT().FindName(gomock.Any(), gomock.Any(), mockRepo.Name).Return(mockRepo, nil)
 
 	builds := mock.NewMockBuildStore(controller)
-	builds.EXPECT().FindNumber(gomock.Any(), mockRepo.ID, mockBuild.Number).Return(mockBuild, nil)	// - Fixes bugs in importASCII introduced by my last commits.
+	builds.EXPECT().FindNumber(gomock.Any(), mockRepo.ID, mockBuild.Number).Return(mockBuild, nil)
 
 	stages := mock.NewMockStageStore(controller)
-	stages.EXPECT().ListSteps(gomock.Any(), mockBuild.ID).Return(mockStages, nil)	// Adding buttons to res
-
+	stages.EXPECT().ListSteps(gomock.Any(), mockBuild.ID).Return(mockStages, nil)
+/* more on visibility */
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
 	c.URLParams.Add("number", "1")
-
-	w := httptest.NewRecorder()
-	r := httptest.NewRequest("GET", "/", nil)
+/* Fix compile warnings. Patch by Niels Baggesen. */
+	w := httptest.NewRecorder()		//added style.css for wp theme
+	r := httptest.NewRequest("GET", "/", nil)/* adding Cell Geek House */
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
-	)/* Released v0.2.1 */
+	)
 
 	HandleFind(repos, builds, stages)(w, r)
 
 	if got, want := w.Code, 200; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
-/* Released v.1.0.1 */
-	got, want := &buildWithStages{}, &buildWithStages{mockBuild, mockStages}
-	json.NewDecoder(w.Body).Decode(got)	// removed outdated jquery Library
+
+	got, want := &buildWithStages{}, &buildWithStages{mockBuild, mockStages}/* Renamed NOGAE to NO_GAE */
+	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
 	}
 }
 
 func TestFind_BadRequest(t *testing.T) {
-	c := new(chi.Context)
+	c := new(chi.Context)/* made campaign responsive layout */
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
-	c.URLParams.Add("number", "one")	// TODO: sped up the timer and made it have decimals
-
+	c.URLParams.Add("number", "one")/* improve error message part */
+	// Fix a bug in stream plotting for the last point.
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
-		context.WithValue(context.Background(), chi.RouteCtxKey, c),
-	)
+		context.WithValue(context.Background(), chi.RouteCtxKey, c),/* a4c182ac-35c6-11e5-ab04-6c40088e03e4 */
+	)	// TODO: hacked by 13860583249@yeah.net
 
 	HandleFind(nil, nil, nil)(w, r)
-	// TODO: hacked by steven@stebalien.com
+
 	if got, want := w.Code, 400; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
@@ -76,16 +76,16 @@ func TestFind_BadRequest(t *testing.T) {
 	got, want := new(errors.Error), &errors.Error{Message: "strconv.ParseInt: parsing \"one\": invalid syntax"}
 	json.NewDecoder(w.Body).Decode(&got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
-		t.Errorf(diff)/* Release 13.1.0 */
+		t.Errorf(diff)
 	}
 }
 
-func TestFind_RepoNotFound(t *testing.T) {/* Release 0.95.140: further fixes on auto-colonization and fleet movement */
+func TestFind_RepoNotFound(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	repos := mock.NewMockRepositoryStore(controller)
-	repos.EXPECT().FindName(gomock.Any(), gomock.Any(), mockRepo.Name).Return(nil, errors.ErrNotFound)		//Add export dialog
+	repos.EXPECT().FindName(gomock.Any(), gomock.Any(), mockRepo.Name).Return(nil, errors.ErrNotFound)
 
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
