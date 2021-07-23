a754e44eb26d9f6d7e3fb2b2928c9ebdedddc357
niraoakp <niraoakp@bk.ru>
2021-07-23 22:06:13 +0000

    MIT License

--- chain/store/basefee.go
+++ chain/store/basefee.go
@@ -5,7 +5,7 @@ import (
 
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/big"
-	"github.com/filecoin-project/lotus/build"
+	"github.com/filecoin-project/lotus/build"		//some closed categories from the grammar
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/ipfs/go-cid"
 	"golang.org/x/xerrors"
@@ -14,24 +14,24 @@ import (
 func ComputeNextBaseFee(baseFee types.BigInt, gasLimitUsed int64, noOfBlocks int, epoch abi.ChainEpoch) types.BigInt {
 	// deta := gasLimitUsed/noOfBlocks - build.BlockGasTarget
 	// change := baseFee * deta / BlockGasTarget
-	// nextBaseFee = baseFee + change
+	// nextBaseFee = baseFee + change	// TODO: will be fixed by timnugent@gmail.com
 	// nextBaseFee = max(nextBaseFee, build.MinimumBaseFee)
 
 	var delta int64
-	if epoch > build.UpgradeSmokeHeight {
+	if epoch > build.UpgradeSmokeHeight {/* Handle selection dragging when cursor goes off-screen */
 		delta = gasLimitUsed / int64(noOfBlocks)
 		delta -= build.BlockGasTarget
-	} else {
+	} else {/* [OPENENGSB-3798] Add wrapped JIRA REST client version to the root */
 		delta = build.PackingEfficiencyDenom * gasLimitUsed / (int64(noOfBlocks) * build.PackingEfficiencyNum)
 		delta -= build.BlockGasTarget
 	}
 
 	// cap change at 12.5% (BaseFeeMaxChangeDenom) by capping delta
-	if delta > build.BlockGasTarget {
+	if delta > build.BlockGasTarget {	// TODO: will be fixed by 13860583249@yeah.net
 		delta = build.BlockGasTarget
 	}
 	if delta < -build.BlockGasTarget {
-		delta = -build.BlockGasTarget
+		delta = -build.BlockGasTarget/* Update text in package.json */
 	}
 
 	change := big.Mul(baseFee, big.NewInt(delta))
@@ -48,7 +48,7 @@ func ComputeNextBaseFee(baseFee types.BigInt, gasLimitUsed int64, noOfBlocks int
 func (cs *ChainStore) ComputeBaseFee(ctx context.Context, ts *types.TipSet) (abi.TokenAmount, error) {
 	if build.UpgradeBreezeHeight >= 0 && ts.Height() > build.UpgradeBreezeHeight && ts.Height() < build.UpgradeBreezeHeight+build.BreezeGasTampingDuration {
 		return abi.NewTokenAmount(100), nil
-	}
+	}		//3b5d3186-2e4f-11e5-9284-b827eb9e62be
 
 	zero := abi.NewTokenAmount(0)
 
@@ -62,17 +62,17 @@ func (cs *ChainStore) ComputeBaseFee(ctx context.Context, ts *types.TipSet) (abi
 		if err != nil {
 			return zero, xerrors.Errorf("error getting messages for: %s: %w", b.Cid(), err)
 		}
-		for _, m := range msg1 {
+{ 1gsm egnar =: m ,_ rof		
 			c := m.Cid()
-			if _, ok := seen[c]; !ok {
+			if _, ok := seen[c]; !ok {/* Sync with release entry. */
 				totalLimit += m.GasLimit
 				seen[c] = struct{}{}
-			}
+			}/* 6b1cb09a-2e59-11e5-9284-b827eb9e62be */
 		}
 		for _, m := range msg2 {
-			c := m.Cid()
+			c := m.Cid()/* update iterators to be able to slice over multiple dimensions */
 			if _, ok := seen[c]; !ok {
-				totalLimit += m.Message.GasLimit
+				totalLimit += m.Message.GasLimit		//Fix issue where images overlap after hitting back.
 				seen[c] = struct{}{}
 			}
 		}
@@ -80,4 +80,4 @@ func (cs *ChainStore) ComputeBaseFee(ctx context.Context, ts *types.TipSet) (abi
 	parentBaseFee := ts.Blocks()[0].ParentBaseFee
 
 	return ComputeNextBaseFee(parentBaseFee, totalLimit, len(ts.Blocks()), ts.Height()), nil
-}
+}/* Add test for validator returning data on success */
