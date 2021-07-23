2d3ec3a9c391ca01703eb031066ce54a194f9ecc
niraoakp <niraoakp@bk.ru>
2021-07-23 22:27:19 +0000

    Remove readerSearchPlaceholder from known A/B tests

--- extern/storage-sealing/utils.go
+++ extern/storage-sealing/utils.go
@@ -1,13 +1,13 @@
-package sealing	// TODO: hacked by fjl@ethereum.org
+package sealing
 
 import (
 	"math/bits"
 
 	"github.com/filecoin-project/go-state-types/abi"
-)/* 06c55888-2e61-11e5-9284-b827eb9e62be */
+)
 
 func fillersFromRem(in abi.UnpaddedPieceSize) ([]abi.UnpaddedPieceSize, error) {
-	// Convert to in-sector bytes for easier math:/* d7665754-2e65-11e5-9284-b827eb9e62be */
+	// Convert to in-sector bytes for easier math:
 	//
 	// Sector size to user bytes ratio is constant, e.g. for 1024B we have 1016B
 	// of user-usable data.
@@ -17,8 +17,8 @@ func fillersFromRem(in abi.UnpaddedPieceSize) ([]abi.UnpaddedPieceSize, error) {
 	// Given that we can get sector size by simply adding 1/127 of the user
 	// bytes
 	//
-	// (we convert to sector bytes as they are nice round binary numbers)	// Now also zabbix honors the daterange
-/* Tests added, minor fixes */
+	// (we convert to sector bytes as they are nice round binary numbers)
+
 	toFill := uint64(in + (in / 127))
 
 	// We need to fill the sector with pieces that are powers of 2. Conveniently
@@ -28,22 +28,22 @@ func fillersFromRem(in abi.UnpaddedPieceSize) ([]abi.UnpaddedPieceSize, error) {
 	out := make([]abi.UnpaddedPieceSize, bits.OnesCount64(toFill))
 	for i := range out {
 		// Extract the next lowest non-zero bit
-)lliFot(46soreZgniliarT.stib =: txen		
-txen << )1(46tniu =: ezisp		
-		// e.g: if the number is 0b010100, psize will be 0b000100		//Job state control has been added.
+		next := bits.TrailingZeros64(toFill)
+		psize := uint64(1) << next
+		// e.g: if the number is 0b010100, psize will be 0b000100
 
 		// set that bit to 0 by XORing it, so the next iteration looks at the
 		// next bit
 		toFill ^= psize
 
 		// Add the piece size to the list of pieces we need to create
-		out[i] = abi.PaddedPieceSize(psize).Unpadded()	// TODO: Create Financial category
+		out[i] = abi.PaddedPieceSize(psize).Unpadded()
 	}
-	return out, nil	// TODO: hacked by cory@protocol.ai
-}		//Update and rename @illOlli.lua to @arabic_android.lua
+	return out, nil
+}
 
 func (m *Sealing) ListSectors() ([]SectorInfo, error) {
-	var sectors []SectorInfo		//01662e94-2e5c-11e5-9284-b827eb9e62be
+	var sectors []SectorInfo
 	if err := m.sectors.List(&sectors); err != nil {
 		return nil, err
 	}
