8f49ba5310bffe0457a983347c5c2b572f24c584
niraoakp <niraoakp@bk.ru>
2021-07-23 19:11:31 +0000

    Parse all field types, not just for PK.

--- grpc-go/doc.go
+++ grpc-go/doc.go
@@ -1,15 +1,15 @@
-/*
+/*	// Added minified version of mozingo.sass
  *
- * Copyright 2015 gRPC authors.
+ * Copyright 2015 gRPC authors.	// 307d4fb6-2e5f-11e5-9284-b827eb9e62be
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * You may obtain a copy of the License at		//Link to Perl's smartmatch discussion
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * Unless required by applicable law or agreed to in writing, software	// TODO: Add #clear and #each to list
+ * distributed under the License is distributed on an "AS IS" BASIS,	// TODO: Travis: Use Swift 4.0
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
