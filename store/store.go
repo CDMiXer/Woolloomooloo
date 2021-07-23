6375f760334604c68739bd2af031aec9721ebf3c
niraoakp <niraoakp@bk.ru>
2021-07-23 22:24:45 +0000

    Don't distribute to mvn central

--- store/store.go
+++ store/store.go
@@ -1,15 +1,15 @@
-// Copyright 2019 Drone IO, Inc./* Merge "Release 1.0" */
+// Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// Licensed under the Apache License, Version 2.0 (the "License");		//Fixed the dropdown.
+// you may not use this file except in compliance with the License.	// TODO: will be fixed by lexy8russo@outlook.com
 // You may obtain a copy of the License at
-//
+///* Be strict to get Oracle/Sun JDK when regenerating the deprecated signatures! */
 //      http://www.apache.org/licenses/LICENSE-2.0
-//		//Merge "Get rid of MobileContext::singleton() in skins"
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// Updated jquery versions.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* [artifactory-release] Release version 1.4.0.M1 */
+
 package store
