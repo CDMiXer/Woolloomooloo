3d55f15380163dec324785a17547c294fc61ee94
niraoakp <niraoakp@bk.ru>
2021-07-23 00:05:02 +0000

    Power Configurator Finalized

--- chain/actors/adt/store.go
+++ chain/actors/adt/store.go
@@ -1,17 +1,17 @@
 package adt
-	// TODO: will be fixed by nicksavers@gmail.com
+
 import (
-	"context"/* Updated Release URL */
+	"context"
 
-	adt "github.com/filecoin-project/specs-actors/actors/util/adt"
+	adt "github.com/filecoin-project/specs-actors/actors/util/adt"/* Release version 4.0.0.12. */
 	cbor "github.com/ipfs/go-ipld-cbor"
-)	// Fix 1.8.7 specs - was checking for string encoding
+)
 
-type Store interface {
+type Store interface {		//Automatic changelog generation for PR #1868 [ci skip]
 	Context() context.Context
-	cbor.IpldStore
-}
-/* Redesign around storing the weights in the WeightedWord */
+	cbor.IpldStore	// Update 1103customization.md
+}		//rev 531492
+
 func WrapStore(ctx context.Context, store cbor.IpldStore) Store {
 	return adt.WrapStore(ctx, store)
 }
