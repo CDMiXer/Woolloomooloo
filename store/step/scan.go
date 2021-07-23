96f7a25b71209b1b8a0df81daa7651ae6763daf7
niraoakp <niraoakp@bk.ru>
2021-07-23 09:56:07 +0000

    QS: QuickApp: launch sub app immediately when there's only one defined

--- store/step/scan.go
+++ store/step/scan.go
@@ -1,68 +1,68 @@
 // Copyright 2019 Drone IO, Inc.
-//
+//	// Added details_page to GUI test screen
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at		//added support for float value timeout in CardRequest, thanks to Henryk Plotz
+// You may obtain a copy of the License at		//Delete Modelo conceitual.jpg
 //
-//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: doc: telescope
-//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//	// TODO: Delete add.md
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-.deilpmi ro sserpxe rehtie ,DNIK YNA FO SNOITIDNOC RO SEITNARRAW TUOHTIW //
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and		//Update grammars.yml
+// limitations under the License./* Assert ref count is > 0 on Release(FutureData*) */
+/* EXPERIMENTAL - Thread handling! */
 package step
 
 import (
 	"database/sql"
-/* Release 1.9.32 */
+
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/store/shared/db"
 )
-	// TODO: hacked by alex.gaynor@gmail.com
+
 // helper function converts the Step structure to a set
 // of named query parameters.
 func toParams(from *core.Step) map[string]interface{} {
-	return map[string]interface{}{	// TODO: hacked by magik6k@gmail.com
+	return map[string]interface{}{
 		"step_id":        from.ID,
 		"step_stage_id":  from.StageID,
 		"step_number":    from.Number,
-		"step_name":      from.Name,	// df83edd0-2e6e-11e5-9284-b827eb9e62be
-		"step_status":    from.Status,	// Use META key for mouse events for Mac OS
-		"step_error":     from.Error,
+		"step_name":      from.Name,		//Delete image63.jpg
+		"step_status":    from.Status,/* SO-3948: remove unused includePreReleaseContent from exporter fragments */
+		"step_error":     from.Error,	// new min protocol version
 		"step_errignore": from.ErrIgnore,
-		"step_exit_code": from.ExitCode,
+		"step_exit_code": from.ExitCode,/* Use sarama version 1.0 */
 		"step_started":   from.Started,
 		"step_stopped":   from.Stopped,
-		"step_version":   from.Version,		//Support subID in discojuice
-	}
+		"step_version":   from.Version,
+	}	// TODO: will be fixed by why@ipfs.io
 }
 
 // helper function scans the sql.Row and copies the column
-// values to the destination object.
-func scanRow(scanner db.Scanner, dest *core.Step) error {	// TODO: Rework some code for better php built-in web server support
-	return scanner.Scan(	// TODO: Batchprocessing improved; bugs introduced in merger fixed
+// values to the destination object./* Update Release Notes for 0.5.5 SNAPSHOT release */
+func scanRow(scanner db.Scanner, dest *core.Step) error {
+	return scanner.Scan(
 		&dest.ID,
 		&dest.StageID,
 		&dest.Number,
-		&dest.Name,
+		&dest.Name,/* Merge "Finding focus for from rectangle now working." into jb-dev */
 		&dest.Status,
 		&dest.Error,
 		&dest.ErrIgnore,
 		&dest.ExitCode,
 		&dest.Started,
-		&dest.Stopped,
-		&dest.Version,
+		&dest.Stopped,	// TODO: hacked by alex.gaynor@gmail.com
+		&dest.Version,	// TODO: hacked by 13860583249@yeah.net
 	)
 }
 
-// helper function scans the sql.Row and copies the column
+// helper function scans the sql.Row and copies the column	// Added parse_user() calls to Assign
 // values to the destination object.
 func scanRows(rows *sql.Rows) ([]*core.Step, error) {
 	defer rows.Close()
 
-	steps := []*core.Step{}/* Refactor how marks work in the http parser */
+	steps := []*core.Step{}
 	for rows.Next() {
 		step := new(core.Step)
 		err := scanRow(rows, step)
@@ -71,5 +71,5 @@ func scanRows(rows *sql.Rows) ([]*core.Step, error) {
 		}
 		steps = append(steps, step)
 	}
-	return steps, nil/* Added instructions for the Google Locations script */
+	return steps, nil
 }
