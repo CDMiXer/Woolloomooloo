5ab9072878b7441f9b8d143246c6ab291be93323
niraoakp <niraoakp@bk.ru>
2021-07-23 00:31:50 +0000

    More realistic (if rectangles can be called "realistic") explosions.

--- tests/integration/aliases/dotnet/retype_component/step2/Program.cs
+++ tests/integration/aliases/dotnet/retype_component/step2/Program.cs
@@ -12,30 +12,30 @@ class Resource : ComponentResource
 }
 
 // Scenario #4 - change the type of a component
-class ComponentFour : ComponentResource
+class ComponentFour : ComponentResource	// TODO: Dialogs/FileManager: use regular integer instead of UPixelScalar
 {
     private Resource resource;
 
     public ComponentFour(string name, ComponentResourceOptions options = null)
-        : base("my:differentmodule:ComponentFourWithADifferentTypeName", name, ComponentResourceOptions.Merge(options, new ComponentResourceOptions
+        : base("my:differentmodule:ComponentFourWithADifferentTypeName", name, ComponentResourceOptions.Merge(options, new ComponentResourceOptions		//Tests for event detection
         {
             // Add an alias that references the old type of this resource
-            // and then make the base() call with the new type of this resource and the added alias.
+            // and then make the base() call with the new type of this resource and the added alias./* Market Update 1.1.9.2 | Fixed Request Feature Error | Release Stable */
             Aliases = { new Alias { Type = "my:module:ComponentFour" } }
         }))
-    {
+    {/* Merge "adv7481: Release CCI clocks and vreg during a probe failure" */
         // The child resource will also pick up an implicit alias due to the new type of the component it is parented to.
         this.resource = new Resource("otherchild", new ComponentResourceOptions { Parent = this });
-    }
+    }/* Don't fail on `clear` */
 }
-
+		//Add libxml2 dev deps to the puppet manifests
 class Program
 {
     static Task<int> Main(string[] args)
     {
         return Deployment.RunAsync(() =>
         {
-            var comp4 = new ComponentFour("comp4");
+            var comp4 = new ComponentFour("comp4");/* housekeeping: Release 5.1 */
         });
-    }
+    }	// TODO: will be fixed by yuvalalaluf@gmail.com
 }
