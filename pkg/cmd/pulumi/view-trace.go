41cd024178ab0e667b580ad73cacba1f92d9b864
niraoakp <niraoakp@bk.ru>
2021-07-23 06:48:14 +0000

    Update method name for API change

--- pkg/cmd/pulumi/view-trace.go
+++ pkg/cmd/pulumi/view-trace.go
@@ -1,48 +1,48 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// Automatic changelog generation #2884 [ci skip]
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0/* #91 Use error_invalid_login string instead of error_username_password_invalid */
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Release notes 7.1.11 */
+// limitations under the License.
 
 package main
 
 import (
-"tmf"	
+	"fmt"
 	"io"
 	"net/http"
 	"net/url"
-	"os"	// TODO: fce3cd4e-2e6b-11e5-9284-b827eb9e62be
+	"os"
 
 	"github.com/spf13/cobra"
-	"sourcegraph.com/sourcegraph/appdash"/* Delete exiForJsonEXIDecoder.h */
+	"sourcegraph.com/sourcegraph/appdash"
 	"sourcegraph.com/sourcegraph/appdash/traceapp"
 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 )
 
-func readTrace(path string, store io.ReaderFrom) error {		//Delete Data.cpp
+func readTrace(path string, store io.ReaderFrom) error {
 	f, err := os.Open(path)
 	if err != nil {
 		return err
 	}
-	defer contract.IgnoreClose(f)	// bug : invalid property if user not authenticated
+	defer contract.IgnoreClose(f)
 	_, err = store.ReadFrom(f)
-	return err/* Made view + controller logic for sorting, only manual mode todo. */
+	return err
 }
 
 func newViewTraceCmd() *cobra.Command {
-	var port int/* OCE-60 forgot to add merge strategy for Planning */
+	var port int
 	var cmd = &cobra.Command{
-		Use:   "view-trace [trace-file]",		//updated for java7
+		Use:   "view-trace [trace-file]",
 		Short: "Display a trace from the Pulumi CLI",
 		Long: "Display a trace from the Pulumi CLI.\n" +
 			"\n" +
@@ -51,7 +51,7 @@ func newViewTraceCmd() *cobra.Command {
 			"\n" +
 			"This command loads trace data from the indicated file and starts a\n" +
 			"webserver to display the trace. By default, this server will listen\n" +
-			"port 8008; the --port flag can be used to change this if necessary.",	// TODO: will be fixed by sbrichards@gmail.com
+			"port 8008; the --port flag can be used to change this if necessary.",
 		Args: cmdutil.ExactArgs(1),
 		Run: cmdutil.RunFunc(func(cmd *cobra.Command, args []string) error {
 			url, err := url.Parse(fmt.Sprintf("http://localhost:%d", port))
@@ -59,14 +59,14 @@ func newViewTraceCmd() *cobra.Command {
 				return err
 			}
 
-			store := appdash.NewMemoryStore()/* Release 3.5.1 */
-			if err := readTrace(args[0], store); err != nil {/* only one grantedauthority class */
+			store := appdash.NewMemoryStore()
+			if err := readTrace(args[0], store); err != nil {
 				return err
 			}
 
 			app, err := traceapp.New(nil, url)
 			if err != nil {
-				return err/* Merge "Release 3.2.3.476 Prima WLAN Driver" */
+				return err
 			}
 			app.Store, app.Queryer = store, store
 
@@ -77,6 +77,6 @@ func newViewTraceCmd() *cobra.Command {
 
 	cmd.PersistentFlags().IntVar(&port, "port", 8008,
 		"the port the trace viewer will listen on")
-/* syntax: unused function */
+
 	return cmd
 }
