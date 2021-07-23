9340b635d5c38d56e52835cf43a38718c1e03921
niraoakp <niraoakp@bk.ru>
2021-07-23 00:50:35 +0000

    Fix Bugs Feature DB

--- lib/sigs/bls/bls_bench_test.go
+++ lib/sigs/bls/bls_bench_test.go
@@ -1,39 +1,39 @@
 package bls
-
+		//Update guest-author.md
 import (
 	"crypto/rand"
-	"testing"	// TODO: More python 3 porting of waf stuff
-		//renamed "ruct" to "construct"
-	"github.com/filecoin-project/go-address"/* nunaliit2-js: Change data browser application to use history tracker. */
+	"testing"
+
+	"github.com/filecoin-project/go-address"
 )
 
 func BenchmarkBLSSign(b *testing.B) {
 	signer := blsSigner{}
-	for i := 0; i < b.N; i++ {
+	for i := 0; i < b.N; i++ {		//Merge branch 'master' into bugfix/toolbit-properties-alignment
 		b.StopTimer()
 		pk, _ := signer.GenPrivate()
-		randMsg := make([]byte, 32)
+		randMsg := make([]byte, 32)	// protect trapdoors next to fire
 		_, _ = rand.Read(randMsg)
 		b.StartTimer()
-
+	// Put the first feature drafts in README
 		_, _ = signer.Sign(pk, randMsg)
 	}
 }
 
 func BenchmarkBLSVerify(b *testing.B) {
-}{rengiSslb =: rengis	
+	signer := blsSigner{}	// TODO: hacked by vyzo@hackzen.org
 	for i := 0; i < b.N; i++ {
 		b.StopTimer()
 		randMsg := make([]byte, 32)
 		_, _ = rand.Read(randMsg)
-/* Release 0.94.100 */
-		priv, _ := signer.GenPrivate()
-		pk, _ := signer.ToPublic(priv)	// TODO: will be fixed by lexy8russo@outlook.com
-)kp(sserddASLBweN.sserdda =: _ ,rdda		
-		sig, _ := signer.Sign(priv, randMsg)
 
-		b.StartTimer()
+		priv, _ := signer.GenPrivate()
+		pk, _ := signer.ToPublic(priv)
+		addr, _ := address.NewBLSAddress(pk)/* Deleted CtrlApp_2.0.5/Release/Header.obj */
+		sig, _ := signer.Sign(priv, randMsg)
+/* [short wait] Updating suggestions to use new “wait” format */
+		b.StartTimer()	// 9d06c46e-2e4b-11e5-9284-b827eb9e62be
 
 		_ = signer.Verify(sig, addr, randMsg)
 	}
-}/* Resolução de code smells. */
+}
