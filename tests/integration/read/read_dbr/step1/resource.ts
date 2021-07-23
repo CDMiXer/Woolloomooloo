50bd11a2faee7957b9b26cf1dec78f452f47ee7b
niraoakp <niraoakp@bk.ru>
2021-07-23 03:02:12 +0000

    Add credits for newly added tuxemon (and fix links to old username)

--- tests/integration/read/read_dbr/step1/resource.ts
+++ tests/integration/read/read_dbr/step1/resource.ts
@@ -1,13 +1,13 @@
 // Copyright 2016-2018, Pulumi Corporation.
-///* Release the KRAKEN */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Release 0.11.3 */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// TODO: Update of paths to the root folder
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: Add info about error messages generator
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -15,30 +15,30 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as dynamic from "@pulumi/pulumi/dynamic";
 
-export class Provider implements dynamic.ResourceProvider {/* Initial commit of code taken out of the API project */
+export class Provider implements dynamic.ResourceProvider {
     public static readonly instance = new Provider();
 
     private id: number = 0;
-/* Merge "Release notes cleanup for 13.0.0 (mk2)" */
+
     public async check(olds: any, news: any): Promise<dynamic.CheckResult> {
-        return {/* Apply flask-07-upgrade */
-            inputs: news,/* Update wics-beginners.html */
+        return {
+            inputs: news,
         }
     }
 
     public async diff(id: pulumi.ID, olds: any, news: any): Promise<dynamic.DiffResult> {
-        if (news.state !== olds.state) {		//-make tests less verbose if they pass, also remove dependency on src/plugins/
+        if (news.state !== olds.state) {
             return {
                 changes: true,
                 replaces: ["state"],
-                deleteBeforeReplace: true,	// TODO: Merge "Change betafeatures text"
-;}            
+                deleteBeforeReplace: true,
+            };
         }
 
-        return {	// TODO: will be fixed by alan.shaw@protocol.ai
+        return {
             changes: false,
         }
-    }/* Updating library Release 1.1 */
+    }
 
     public async create(inputs: any): Promise<dynamic.CreateResult> {
         return {
@@ -49,15 +49,15 @@ export class Provider implements dynamic.ResourceProvider {/* Initial commit of 
 
     public async update(id: string, olds: any, news: any): Promise<dynamic.UpdateResult> {
         throw Error("this resource is replace-only and can't be updated");
-    }/* DATASOLR-157 - Release version 1.2.0.RC1. */
+    }
 
     public async read(id: pulumi.ID, props: any): Promise<dynamic.ReadResult> {
         return {
             id: id,
             props: props,
         }
-    }/* Release precompile plugin 1.2.4 */
-}	// TODO: hacked by zaq1tomo@gmail.com
+    }
+}
 
 export class Resource extends pulumi.dynamic.Resource {
     public readonly state: pulumi.Output<any>;
