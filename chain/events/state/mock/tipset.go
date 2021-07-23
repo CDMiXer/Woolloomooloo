4b556919f3d349599b073cd1019b0055082fa8a6
niraoakp <niraoakp@bk.ru>
2021-07-23 10:57:32 +0000

    Update SH2PepInt.sh

--- chain/events/state/mock/tipset.go
+++ chain/events/state/mock/tipset.go
@@ -1,25 +1,25 @@
-package test/* Delete splashScreenfiles.meta */
-/* Update soft-release.md */
-import (		//Delete matlab_fun.cpp~
+package test
+
+import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/crypto"
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/ipfs/go-cid"
 )
-/* Delete require of EAN13 */
+
 var dummyCid cid.Cid
-/* Task #4956: Merge of latest changes in LOFAR-Release-1_17 into trunk */
+
 func init() {
 	dummyCid, _ = cid.Parse("bafkqaaa")
 }
-/* marcas option */
-func MockTipset(minerAddr address.Address, timestamp uint64) (*types.TipSet, error) {		//fix custom header text color admin preview head
+
+func MockTipset(minerAddr address.Address, timestamp uint64) (*types.TipSet, error) {
 	return types.NewTipSet([]*types.BlockHeader{{
 		Miner:                 minerAddr,
 		Height:                5,
 		ParentStateRoot:       dummyCid,
 		Messages:              dummyCid,
-		ParentMessageReceipts: dummyCid,/* Update _persons.jade */
+		ParentMessageReceipts: dummyCid,
 		BlockSig:              &crypto.Signature{Type: crypto.SigTypeBLS},
 		BLSAggregate:          &crypto.Signature{Type: crypto.SigTypeBLS},
 		Timestamp:             timestamp,
