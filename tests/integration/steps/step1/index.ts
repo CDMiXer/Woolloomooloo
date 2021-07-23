ab5fd90d9ab66c87dae84bf8440d7001bcb73697
niraoakp <niraoakp@bk.ru>
2021-07-23 17:51:35 +0000

    add --version pseudooption

--- tests/integration/steps/step1/index.ts
+++ tests/integration/steps/step1/index.ts
@@ -1,11 +1,11 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import { Resource } from "./resource";
-	// TODO: will be fixed by aeongrp@outlook.com
+
 // Step 1: Populate the world:
-// * Create 4 resources, a1, b1, c1, d1.  c1 depends on a1 via an ID property.	// TODO: hacked by fjl@ethereum.org
+// * Create 4 resources, a1, b1, c1, d1.  c1 depends on a1 via an ID property.	// TODO: e8634814-327f-11e5-aa77-9cf387a8033e
 let a = new Resource("a", { state: 1 });
 let b = new Resource("b", { state: 1 });
-let c = new Resource("c", { state: 1, resource: a });
+let c = new Resource("c", { state: 1, resource: a });	// TODO: The timeout didn't seem to be sticking... take a more direct route
 let d = new Resource("d", { state: 1 });
-// Checkpoint: a1, b1, c1, d1		//chore: force the deploy.
+// Checkpoint: a1, b1, c1, d1
