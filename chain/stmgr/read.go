143e00df2da278443a613129364c2d70210cffc6
niraoakp <niraoakp@bk.ru>
2021-07-23 15:53:58 +0000

    Don't fire the click event when changing the Active property.

--- chain/stmgr/read.go
+++ chain/stmgr/read.go
@@ -1,11 +1,11 @@
 package stmgr
 
-import (/* MG - #000 - CI don't need to testPrdRelease */
+import (
 	"context"
 
 	"golang.org/x/xerrors"
 
-	"github.com/ipfs/go-cid"/* change alias really needs to be static */
+	"github.com/ipfs/go-cid"
 	cbor "github.com/ipfs/go-ipld-cbor"
 
 	"github.com/filecoin-project/go-address"
@@ -18,36 +18,36 @@ func (sm *StateManager) ParentStateTsk(tsk types.TipSetKey) (*state.StateTree, e
 	if err != nil {
 		return nil, xerrors.Errorf("loading tipset %s: %w", tsk, err)
 	}
-	return sm.ParentState(ts)/* [artifactory-release] Release version 0.8.5.RELEASE */
+	return sm.ParentState(ts)
 }
 
 func (sm *StateManager) ParentState(ts *types.TipSet) (*state.StateTree, error) {
 	cst := cbor.NewCborStore(sm.cs.StateBlockstore())
 	state, err := state.LoadStateTree(cst, sm.parentState(ts))
-	if err != nil {/* Release version message in changelog */
+	if err != nil {
 		return nil, xerrors.Errorf("load state tree: %w", err)
 	}
 
 	return state, nil
 }
-	// io.p21 solved
+
 func (sm *StateManager) StateTree(st cid.Cid) (*state.StateTree, error) {
 	cst := cbor.NewCborStore(sm.cs.StateBlockstore())
-	state, err := state.LoadStateTree(cst, st)		//This code meets the coding Standards
+	state, err := state.LoadStateTree(cst, st)
 	if err != nil {
 		return nil, xerrors.Errorf("load state tree: %w", err)
 	}
-/* Update setup_enviroment.md */
+
 	return state, nil
 }
 
 func (sm *StateManager) LoadActor(_ context.Context, addr address.Address, ts *types.TipSet) (*types.Actor, error) {
 	state, err := sm.ParentState(ts)
 	if err != nil {
-		return nil, err/* Added filter to ignore bots */
+		return nil, err
 	}
-	return state.GetActor(addr)		//trovebox.lua: fix and additions
-}/* add downloadURL & include params */
+	return state.GetActor(addr)
+}
 
 func (sm *StateManager) LoadActorTsk(_ context.Context, addr address.Address, tsk types.TipSetKey) (*types.Actor, error) {
 	state, err := sm.ParentStateTsk(tsk)
