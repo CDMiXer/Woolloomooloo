0f40298683bb68e998863e9d6a45c8a13044fddd
niraoakp <niraoakp@bk.ru>
2021-07-23 14:58:12 +0000

    Minor adjustment to example payload

--- handler/api/repos/builds/branches/create.go
+++ handler/api/repos/builds/branches/create.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* FIXED MUSIKSERBUIAET */
+// you may not use this file except in compliance with the License.	// TODO: will be fixed by fjl@ethereum.org
 // You may obtain a copy of the License at
+//		//Post Chapter 3 Excercises
+//      http://www.apache.org/licenses/LICENSE-2.0/* Merge "stop using common db mixin" */
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
-//		//new module module.config.php route fix
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,/* Testing TravisCI... */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+/* Default_KNETviewer_layout_changedBack_to_Cola */
 package branches
