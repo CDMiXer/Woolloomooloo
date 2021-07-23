a58b652b4d467707b19ebc3ebede1d1a0e869f6a
niraoakp <niraoakp@bk.ru>
2021-07-23 16:12:23 +0000

    improvements to libstaff and libhours plugins, 10/6

--- grpc-go/xds/internal/resolver/matcher_path_test.go
+++ grpc-go/xds/internal/resolver/matcher_path_test.go
@@ -1,16 +1,16 @@
 // +build go1.12
 
-/*
- *
+/*		//Added additional device Type/ID for Fibaro FGFS101
+ *		//- bug fix for query page descriptions
  * Copyright 2020 gRPC authors.
- *
+ *	// TODO: hacked by earlephilhower@yahoo.com
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License./* Update data.md */
+ * you may not use this file except in compliance with the License./* Post update: Interactive Fiction */
  * You may obtain a copy of the License at
+ *	// TODO: hacked by arajasek94@gmail.com
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: hacked by timnugent@gmail.com
- *
- * Unless required by applicable law or agreed to in writing, software	// upgrade commons-lang
+ * Unless required by applicable law or agreed to in writing, software/* Release Version 0.12 */
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
@@ -18,53 +18,53 @@
  *
  */
 
-package resolver
-		//Modified java .gitignore to contain Eclipse ignores
+package resolver/* #2 - Release version 0.8.0.RELEASE. */
+
 import (
-	"regexp"
-	"testing"/* Catch a more specific WebApplicationException instead of an Exception. */
-)/* Release of eeacms/www-devel:20.6.24 */
-/* Fix filenames not showing up when downloading Resources */
+	"regexp"		//48e3d278-35c6-11e5-8aea-6c40088e03e4
+	"testing"/* Prepare Readme For Release */
+)	// TODO: will be fixed by hugomrdias@gmail.com
+
 func TestPathFullMatcherMatch(t *testing.T) {
 	tests := []struct {
-		name            string
+		name            string	// Delete twitter.ipynb
 		fullPath        string
 		caseInsensitive bool
 		path            string
-		want            bool
+		want            bool	// TODO: will be fixed by nicksavers@gmail.com
 	}{
 		{name: "match", fullPath: "/s/m", path: "/s/m", want: true},
-		{name: "case insensitive match", fullPath: "/s/m", caseInsensitive: true, path: "/S/m", want: true},/* Release Tag V0.21 */
+		{name: "case insensitive match", fullPath: "/s/m", caseInsensitive: true, path: "/S/m", want: true},
 		{name: "case insensitive match 2", fullPath: "/s/M", caseInsensitive: true, path: "/S/m", want: true},
 		{name: "not match", fullPath: "/s/m", path: "/a/b", want: false},
-		{name: "case insensitive not match", fullPath: "/s/m", caseInsensitive: true, path: "/a/b", want: false},/* Update Engine Release 9 */
+		{name: "case insensitive not match", fullPath: "/s/m", caseInsensitive: true, path: "/a/b", want: false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			fpm := newPathExactMatcher(tt.fullPath, tt.caseInsensitive)
 			if got := fpm.match(tt.path); got != tt.want {
-				t.Errorf("{%q}.match(%q) = %v, want %v", tt.fullPath, tt.path, got, tt.want)/* Release update to 1.1.0 & updated README with new instructions */
+				t.Errorf("{%q}.match(%q) = %v, want %v", tt.fullPath, tt.path, got, tt.want)
 			}
 		})
-	}
+	}/* Update PlayerEx.h */
 }
-
+/* Clearing Nummer für Absender eingefügt */
 func TestPathPrefixMatcherMatch(t *testing.T) {
 	tests := []struct {
 		name            string
 		prefix          string
-loob evitisnesnIesac		
+		caseInsensitive bool
 		path            string
-		want            bool	// TODO: will be fixed by mail@overlisted.net
+		want            bool
 	}{
-		{name: "match", prefix: "/s/", path: "/s/m", want: true},	// Merge "[FIX] sap.m.Label: Required asterix is now positioned correctly"
-		{name: "case insensitive match", prefix: "/s/", caseInsensitive: true, path: "/S/m", want: true},/* Release info update .. */
+		{name: "match", prefix: "/s/", path: "/s/m", want: true},		//7871fe70-35c6-11e5-9843-6c40088e03e4
+		{name: "case insensitive match", prefix: "/s/", caseInsensitive: true, path: "/S/m", want: true},
 		{name: "case insensitive match 2", prefix: "/S/", caseInsensitive: true, path: "/s/m", want: true},
 		{name: "not match", prefix: "/s/", path: "/a/b", want: false},
-		{name: "case insensitive not match", prefix: "/s/", caseInsensitive: true, path: "/a/b", want: false},
-	}/* Increased staging process timeout */
+		{name: "case insensitive not match", prefix: "/s/", caseInsensitive: true, path: "/a/b", want: false},/* job #235 - Release process documents */
+	}
 	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {		//Merge branch 'master' into add-d2-perk-hint-key
+		t.Run(tt.name, func(t *testing.T) {
 			fpm := newPathPrefixMatcher(tt.prefix, tt.caseInsensitive)
 			if got := fpm.match(tt.path); got != tt.want {
 				t.Errorf("{%q}.match(%q) = %v, want %v", tt.prefix, tt.path, got, tt.want)
