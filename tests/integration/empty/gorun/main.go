9b68f6c6f96fafc5fa795371365c6fdf6ca70feb
niraoakp <niraoakp@bk.ru>
2021-07-23 18:22:53 +0000

    Se agrega método para hacer reset a los datos de PaginationDataProvider

--- tests/integration/empty/gorun/main.go
+++ tests/integration/empty/gorun/main.go
@@ -1,13 +1,13 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-package main
+package main/* Fixup test case for Release builds. */
 
 import (
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
-
+	// TODO: will be fixed by 13860583249@yeah.net
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		return nil
+		return nil/* removed confusing btns. */
 	})
 }
