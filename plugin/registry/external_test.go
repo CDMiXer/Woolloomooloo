d5053e32100b6183b588ad57f76644239d7aebd4
niraoakp <niraoakp@bk.ru>
2021-07-23 14:17:15 +0000

    #3937 fix typo.

--- plugin/registry/external_test.go
+++ plugin/registry/external_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* - fixed: reduced frame buffer in case of hidden network streams */
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
 
-package registry	// updated chrome version
+package registry
