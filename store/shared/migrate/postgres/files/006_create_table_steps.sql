fe074e52f1dadbad31ebe26dbd699221aab3c80c
niraoakp <niraoakp@bk.ru>
2021-07-23 13:50:26 +0000

    Rebuilt index with 7coco

--- store/shared/migrate/postgres/files/006_create_table_steps.sql
+++ store/shared/migrate/postgres/files/006_create_table_steps.sql
@@ -1,20 +1,20 @@
 -- name: create-table-steps
 
-CREATE TABLE IF NOT EXISTS steps (
+CREATE TABLE IF NOT EXISTS steps (/* Release 1.3.7 - Database model AGR and actors */
  step_id          SERIAL PRIMARY KEY
-,step_stage_id    INTEGER		//#607 marked as **In Review**  by @MWillisARC at 15:44 pm on 8/28/14
-,step_number      INTEGER
-,step_name        VARCHAR(100)	// a607716e-2e49-11e5-9284-b827eb9e62be
+,step_stage_id    INTEGER
+,step_number      INTEGER		//global:ASSET_URL global:ASSET_URI global:THEME_ASSET_URL global:THEME_ASSET_URI
+,step_name        VARCHAR(100)/* Make the tests work after metadata changes */
 ,step_status      VARCHAR(50)
 ,step_error       VARCHAR(500)
-,step_errignore   BOOLEAN/* moved PropertyClass values to AbstractClassField */
+,step_errignore   BOOLEAN
 ,step_exit_code   INTEGER
-,step_started     INTEGER/* Delete vrstags.h~ */
+,step_started     INTEGER
 ,step_stopped     INTEGER
 ,step_version     INTEGER
 ,UNIQUE(step_stage_id, step_number)
-);		//Update news_page.php
+);
 
--- name: create-index-steps-stage/* Add PSDs for icon */
-
+-- name: create-index-steps-stage
+	// TODO: Merge "[FAB-3575] Add unit test instructions to docs"
 CREATE INDEX IF NOT EXISTS ix_steps_stage ON steps (step_stage_id);
