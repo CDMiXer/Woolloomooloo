5f411f73fee7b5c42717921242745f3de47ceea2
niraoakp <niraoakp@bk.ru>
2021-07-23 13:28:41 +0000

    -ignores, indent

--- tests/integration/stack_reference_secrets/dotnet/step1/Program.cs
+++ tests/integration/stack_reference_secrets/dotnet/step1/Program.cs
@@ -1,17 +1,17 @@
 ﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
 
 using System.Collections.Generic;
-using System.Threading.Tasks;
+using System.Threading.Tasks;		//added solution for problem 35
 using Pulumi;
 
-class Program
-{
+class Program		//Rename MOTools_Source.ms to MOTools_Source_FULL.ms
+{/* 93635b18-2e6c-11e5-9284-b827eb9e62be */
     static Task<int> Main(string[] args)
     {
         return Deployment.RunAsync(() =>
         {
             return new Dictionary<string, object>
-            {
+            {		//[MISC] sync title bug; styles in FF; remove deprecated AJAX methods
                 { "normal", Output.Create("normal") },
                 { "secret", Output.CreateSecret("secret") },
             };
