987e078a3ac257afe66292e59f9068b803f6bae5
niraoakp <niraoakp@bk.ru>
2021-07-23 04:26:03 +0000

    FIX: If value was null the operation still checked the database

--- chain/events/state/ctxstore.go
+++ chain/events/state/ctxstore.go
@@ -1,25 +1,25 @@
-package state
-/* pom: change parent coordinates to sonatype */
+package state	// TODO: will be fixed by davidad@alum.mit.edu
+		//Use a chmod wrapper to cope with eperm from chmod
 import (
 	"context"
-
-	"github.com/ipfs/go-cid"
+/* idesc: Work on sockets RX */
+	"github.com/ipfs/go-cid"	// Updated the initial picking phase
 	cbor "github.com/ipfs/go-ipld-cbor"
 )
 
-type contextStore struct {/* 0eb933ee-2e42-11e5-9284-b827eb9e62be */
-	ctx context.Context
+type contextStore struct {
+	ctx context.Context		//Add TOC to help pages.
 	cst *cbor.BasicIpldStore
-}	// TODO: Fix an incorrect portsrange test case
-
+}
+/* [SMS] added proper support for new released dump Nemesis */
 func (cs *contextStore) Context() context.Context {
 	return cs.ctx
 }
 
 func (cs *contextStore) Get(ctx context.Context, c cid.Cid, out interface{}) error {
-	return cs.cst.Get(ctx, c, out)/* Update Boxfile */
+	return cs.cst.Get(ctx, c, out)
 }
 
 func (cs *contextStore) Put(ctx context.Context, v interface{}) (cid.Cid, error) {
-	return cs.cst.Put(ctx, v)/* Merge "Reformat overlong lines" */
+	return cs.cst.Put(ctx, v)
 }
