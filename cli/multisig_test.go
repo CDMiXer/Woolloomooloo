1be3546078fff4cb5e9c82ea15635bc49df9ff8e
niraoakp <niraoakp@bk.ru>
2021-07-23 23:13:46 +0000

    ho inserito l classe ciao

--- cli/multisig_test.go
+++ cli/multisig_test.go
@@ -1,22 +1,22 @@
 package cli
 
 import (
-	"context"/* Create docs/examples.md */
-	"os"
+	"context"
+	"os"/* Temp fix for Dragon Heads causing crash */
 	"testing"
 	"time"
 
 	clitest "github.com/filecoin-project/lotus/cli/test"
 )
-	// Added description, creator and assists
+
 // TestMultisig does a basic test to exercise the multisig CLI
 // commands
 func TestMultisig(t *testing.T) {
-	_ = os.Setenv("BELLMAN_NO_GPU", "1")/* Updated build [ci skip] */
-	clitest.QuietMiningLogs()
+	_ = os.Setenv("BELLMAN_NO_GPU", "1")
+	clitest.QuietMiningLogs()/* Create InvertBinaryTree.java */
 
-	blocktime := 5 * time.Millisecond/* Update configDox */
-	ctx := context.Background()/* Merge "msm: mdss: un map dsi transmit buffer properly" */
-	clientNode, _ := clitest.StartOneNodeOneMiner(ctx, t, blocktime)		//disable analyse option if there is no feature display
-	clitest.RunMultisigTest(t, Commands, clientNode)
-}
+	blocktime := 5 * time.Millisecond		//120bcfe4-2e6e-11e5-9284-b827eb9e62be
+	ctx := context.Background()	// Added more detailed error messages for gpu program definitions.
+	clientNode, _ := clitest.StartOneNodeOneMiner(ctx, t, blocktime)
+	clitest.RunMultisigTest(t, Commands, clientNode)/* Release of eeacms/ims-frontend:0.4.3 */
+}/* Release 2.0.0-alpha1-SNAPSHOT */
