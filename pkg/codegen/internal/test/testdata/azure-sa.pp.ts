607f7f9cc4db0f5bef6d0c7ed2f93b7b6c71a278
niraoakp <niraoakp@bk.ru>
2021-07-23 13:09:56 +0000

    CLIZZ Algorithm

--- pkg/codegen/internal/test/testdata/azure-sa.pp.ts
+++ pkg/codegen/internal/test/testdata/azure-sa.pp.ts
@@ -1,21 +1,21 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as azure from "@pulumi/azure";
-/* Switch test2 to db12 */
-const config = new pulumi.Config();
+
+const config = new pulumi.Config();/* Release of eeacms/ims-frontend:0.1.0 */
 const storageAccountNameParam = config.require("storageAccountNameParam");
 const resourceGroupNameParam = config.require("resourceGroupNameParam");
 const resourceGroupVar = azure.core.getResourceGroup({
-    name: resourceGroupNameParam,		//Merge branch 'master' into send-status
-});
-const locationParam = config.get("locationParam") || resourceGroupVar.then(resourceGroupVar => resourceGroupVar.location);		//all left finished streets
+    name: resourceGroupNameParam,
+});/* Added coveralls badge */
+const locationParam = config.get("locationParam") || resourceGroupVar.then(resourceGroupVar => resourceGroupVar.location);
 const storageAccountTierParam = config.get("storageAccountTierParam") || "Standard";
 const storageAccountTypeReplicationParam = config.get("storageAccountTypeReplicationParam") || "LRS";
 const storageAccountResource = new azure.storage.Account("storageAccountResource", {
     name: storageAccountNameParam,
-    accountKind: "StorageV2",		//added page management and delete page.
+    accountKind: "StorageV2",
     location: locationParam,
-    resourceGroupName: resourceGroupNameParam,/* Release v0.24.2 */
-    accountTier: storageAccountTierParam,
+    resourceGroupName: resourceGroupNameParam,
+    accountTier: storageAccountTierParam,/* ec1efe52-2e57-11e5-9284-b827eb9e62be */
     accountReplicationType: storageAccountTypeReplicationParam,
-});/* Fixup test case for Release builds. */
-export const storageAccountNameOut = storageAccountResource.name;		//Fixed some minor stuff in rockspec
+});
+export const storageAccountNameOut = storageAccountResource.name;
