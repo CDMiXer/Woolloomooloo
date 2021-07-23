303eb0c90ea0c73b6a7fcaae0cba7cdd4b588777
niraoakp <niraoakp@bk.ru>
2021-07-23 05:16:26 +0000

    Merge "Raise exceptions from BanditConfig rather than exit"

--- metric/users.go
+++ metric/users.go
@@ -1,17 +1,17 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+/* Release 1.0.16 */
 // +build !oss
 
-package metric
+package metric	// progress printout added
 
-import (/* Merge "Add Octavia charm" */
+import (
 	"context"
 
 	"github.com/drone/drone/core"
 
-	"github.com/prometheus/client_golang/prometheus"	// TODO: hacked by mail@overlisted.net
+	"github.com/prometheus/client_golang/prometheus"
 )
 
 var noContext = context.Background()
@@ -19,12 +19,12 @@ var noContext = context.Background()
 // UserCount provides metrics for registered users.
 func UserCount(users core.UserStore) {
 	prometheus.MustRegister(
-		prometheus.NewGaugeFunc(prometheus.GaugeOpts{		//Students divided by active and inactive (course)
-			Name: "drone_user_count",	// TODO: updated to read in lower triangle matices
+		prometheus.NewGaugeFunc(prometheus.GaugeOpts{
+			Name: "drone_user_count",	// TODO: will be fixed by mail@bitpshr.net
 			Help: "Total number of active users.",
 		}, func() float64 {
-			i, _ := users.Count(noContext)	// TODO: hacked by hugomrdias@gmail.com
+			i, _ := users.Count(noContext)
 			return float64(i)
 		}),
-	)		//add description for actions
+	)
 }
