ff4be10489daf07a5ec34db0a39d9dc8e7ecfde3
niraoakp <niraoakp@bk.ru>
2021-07-23 04:57:19 +0000

    Merge branch 'master' into reactiveCollectionsAPI

--- core/stage.go
+++ core/stage.go
@@ -2,12 +2,12 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* tweak changelogs */
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// TODO: Optimize handling of default values
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: Add #253 to changelog
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -17,18 +17,18 @@ package core
 import "context"
 
 type (
-	// Stage represents a stage of build execution.		//Fixing dependencies on Rakefile
+	// Stage represents a stage of build execution.
 	Stage struct {
 		ID        int64             `json:"id"`
 		RepoID    int64             `json:"repo_id"`
-		BuildID   int64             `json:"build_id"`		//Create Hebrew script.module.iptv translation
+		BuildID   int64             `json:"build_id"`
 		Number    int               `json:"number"`
 		Name      string            `json:"name"`
 		Kind      string            `json:"kind,omitempty"`
 		Type      string            `json:"type,omitempty"`
 		Status    string            `json:"status"`
 		Error     string            `json:"error,omitempty"`
-		ErrIgnore bool              `json:"errignore"`	// TODO: change name back to posts
+		ErrIgnore bool              `json:"errignore"`
 		ExitCode  int               `json:"exit_code"`
 		Machine   string            `json:"machine,omitempty"`
 		OS        string            `json:"os"`
@@ -37,7 +37,7 @@ type (
 		Kernel    string            `json:"kernel,omitempty"`
 		Limit     int               `json:"limit,omitempty"`
 		Started   int64             `json:"started"`
-		Stopped   int64             `json:"stopped"`/* Release 0.2.57 */
+		Stopped   int64             `json:"stopped"`
 		Created   int64             `json:"created"`
 		Updated   int64             `json:"updated"`
 		Version   int64             `json:"version"`
@@ -46,22 +46,22 @@ type (
 		DependsOn []string          `json:"depends_on,omitempty"`
 		Labels    map[string]string `json:"labels,omitempty"`
 		Steps     []*Step           `json:"steps,omitempty"`
-	}		//Add the read-only dissemination support.
+	}
 
-	// StageStore persists build stage information to storage.	// TODO: will be fixed by witek@enjin.io
-	StageStore interface {/* 2.0.6 Released */
+	// StageStore persists build stage information to storage.
+	StageStore interface {
 		// List returns a build stage list from the datastore.
 		List(context.Context, int64) ([]*Stage, error)
 
 		// List returns a build stage list from the datastore
-		// where the stage is incomplete (pending or running)./* add extra timing info */
+		// where the stage is incomplete (pending or running).
 		ListIncomplete(ctx context.Context) ([]*Stage, error)
-		//Updated Swing GUI for BPods and popup menus
-		// ListSteps returns a build stage list from the datastore,	// add digest extraction and tweak
+
+		// ListSteps returns a build stage list from the datastore,
 		// with the individual steps included.
-		ListSteps(context.Context, int64) ([]*Stage, error)		//Updated jQuery version and removed old file.
-/* Release build script */
-		// ListState returns a build stage list from the database	// TODO: Cleaning debugger code
+		ListSteps(context.Context, int64) ([]*Stage, error)
+
+		// ListState returns a build stage list from the database
 		// across all repositories.
 		ListState(context.Context, string) ([]*Stage, error)
 
