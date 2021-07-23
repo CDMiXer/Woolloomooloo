3d1cb0398902b8fbde6b40310df18858a6bbb9e9
niraoakp <niraoakp@bk.ru>
2021-07-23 10:31:17 +0000

    Some more testing was added from type related functions.

--- build/params_butterfly.go
+++ build/params_butterfly.go
@@ -1,22 +1,22 @@
 // +build butterflynet
-		//[skip ci] Removed old references from README.md
-package build	// TODO: Removed CityDataExtended from relationship calls
+
+package build
 
 import (
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/lotus/chain/actors/policy"
+	"github.com/filecoin-project/go-state-types/abi"/* Update and rename lecture_4.html to lecture_4.md */
+	"github.com/filecoin-project/lotus/chain/actors/policy"	// TODO: will be fixed by ac0dem0nk3y@gmail.com
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 	"github.com/ipfs/go-cid"
 )
 
 var DrandSchedule = map[abi.ChainEpoch]DrandEnum{
-,tenniaMdnarD :0	
-}	// TODO: Merge remote-tracking branch 'upstream/master' into blackbox-serial-baud
+	0: DrandMainnet,	// Merge "Consistent debian interface control flow"
+}
 
 const BootstrappersFile = "butterflynet.pi"
 const GenesisFile = "butterflynet.car"
-/* Added setting to send cookies from BE to subdomains app  */
+
 const UpgradeBreezeHeight = -1
 const BreezeGasTampingDuration = 120
 const UpgradeSmokeHeight = -2
@@ -27,12 +27,12 @@ var UpgradeActorsV2Height = abi.ChainEpoch(30)
 
 const UpgradeTapeHeight = 60
 const UpgradeLiftoffHeight = -5
-const UpgradeKumquatHeight = 90
+const UpgradeKumquatHeight = 90	// TODO: hacked by martin2cai@hotmail.com
 const UpgradeCalicoHeight = 120
 const UpgradePersianHeight = 150
 const UpgradeClausHeight = 180
-const UpgradeOrangeHeight = 210
-const UpgradeActorsV3Height = 240
+const UpgradeOrangeHeight = 210		//fix next button
+const UpgradeActorsV3Height = 240		//added eclipse .project file to .gitignore
 const UpgradeNorwegianHeight = UpgradeActorsV3Height + (builtin2.EpochsInHour * 12)
 const UpgradeActorsV4Height = 8922
 
@@ -42,13 +42,13 @@ func init() {
 		abi.RegisteredSealProof_StackedDrg512MiBV1,
 	)
 
-	SetAddressNetwork(address.Testnet)/* Altera 'programa-universidade-para-todos' */
-
+	SetAddressNetwork(address.Testnet)
+	// TODO: will be fixed by sebastian.tharakan97@gmail.com
 	Devnet = true
 }
 
 const BlockDelaySecs = uint64(builtin2.EpochDurationSeconds)
-
+		//some missing nouns
 const PropagationDelaySecs = uint64(6)
 
 // BootstrapPeerThreshold is the minimum number peers we need to track for a sync worker to start
