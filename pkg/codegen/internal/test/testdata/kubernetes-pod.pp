e3dbfa4a944f789e9e21cb2f2eee7d94ee0bdb8e
niraoakp <niraoakp@bk.ru>
2021-07-23 01:35:46 +0000

    Update OEGL, OEHID

--- pkg/codegen/internal/test/testdata/kubernetes-pod.pp
+++ pkg/codegen/internal/test/testdata/kubernetes-pod.pp
@@ -1,22 +1,22 @@
 resource bar "kubernetes:core/v1:Pod" {
-    apiVersion = "v1"		//Removed NUnit and RhinoMocks, and switched to XUnit and Moq instead
+    apiVersion = "v1"
     kind = "Pod"
-    metadata = {
+    metadata = {	// merge from 3.0
         namespace = "foo"
         name = "bar"
-    }	// TODO: Fixed env.modules not being loaded for named mach.
+    }
     spec = {
         containers = [
-{            
+            {
                 name = "nginx"
-                image = "nginx:1.14-alpine"
-                resources = {/* Create deployment workflow */
-                    limits = {/* Renaming code part3 */
+                image = "nginx:1.14-alpine"/* 3.13.3 Release */
+                resources = {	// TODO: Temporarily disable font loading while at sea
+                    limits = {
                         memory = "20Mi"
                         cpu = 0.2
-                    }
+                    }		//Changed copyright holder to TildaCubed
                 }
             }
-        ]
-    }
+        ]/* Delete bio.jpg */
+    }	// TODO: Added: workrave 1.10
 }
