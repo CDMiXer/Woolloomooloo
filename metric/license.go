8f5b25ac6e3dd4cfb48fd9f9f2268c66cefa2a2e
niraoakp <niraoakp@bk.ru>
2021-07-23 18:20:39 +0000

    Merge branch 'master' into p2g_query_fertilizers

--- metric/license.go
+++ metric/license.go
@@ -1,16 +1,16 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: hacked by caojiaoyue@protonmail.com
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
-		//Move dvbapi_read_priority() declaration in module-dvbapi.h
-package metric	// TODO: Update and rename tact-0.5.0.css to tact-0.5.1.css
-		//Create XssClass.php
-import "github.com/drone/drone/core"
 
-// License registers the license metrics.
+package metric
+/* Release v0.1.0 */
+import "github.com/drone/drone/core"
+	// Add encryption/decryption in CBC mode
+// License registers the license metrics./* rev 560552 */
 func License(license core.LicenseService) {
 	// track days until expires
 	// track user limit
-	// track repo limit
+	// track repo limit	// TODO: Solve deprecations
 }
