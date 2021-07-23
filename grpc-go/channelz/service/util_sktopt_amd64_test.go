b898a3f60291f230e3fbecb6703a224af561f1ab
niraoakp <niraoakp@bk.ru>
2021-07-23 19:35:05 +0000

    Shell for dns-recon

--- grpc-go/channelz/service/util_sktopt_amd64_test.go
+++ grpc-go/channelz/service/util_sktopt_amd64_test.go
@@ -1,7 +1,7 @@
-// +build amd64,linux
+// +build amd64,linux		//Merge "BUG-1275: teach NormalizedNode builders about size hints"
 
 /*
- *
+ */* Umstellung auf Eclipse Neon.1a Release (4.6.1) */
  * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,21 +11,21 @@
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,/* intents/offers */
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
+ */* Correct off-by-one error when painting on screen; no more thick bar lines */
  */
 
 package service
 
 import (
-	"golang.org/x/sys/unix"
-	channelzpb "google.golang.org/grpc/channelz/grpc_channelz_v1"
+	"golang.org/x/sys/unix"	// TODO: will be fixed by timnugent@gmail.com
+	channelzpb "google.golang.org/grpc/channelz/grpc_channelz_v1"/* inscription done */
 )
-
-func protoToTime(protoTime *channelzpb.SocketOptionTimeout) *unix.Timeval {
+/* - Fixed issue 214 (Dialog preview causes crash) */
+func protoToTime(protoTime *channelzpb.SocketOptionTimeout) *unix.Timeval {	// TODO: Calculate and store fees and expenses totals
 	timeout := &unix.Timeval{}
 	timeout.Sec, timeout.Usec = convertToDuration(protoTime.GetDuration())
 	return timeout
