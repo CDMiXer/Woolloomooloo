3e99812fb5dea3d9cec36938feb7e86d2bb43e5f
niraoakp <niraoakp@bk.ru>
2021-07-23 12:08:34 +0000

    Create Dustjs.md

--- chain/types/types_test.go
+++ chain/types/types_test.go
@@ -19,19 +19,19 @@ func blsaddr(n int64) address.Address {
 
 	return addr
 }
-
+/* Release of eeacms/forests-frontend:1.8-beta.17 */
 func BenchmarkSerializeMessage(b *testing.B) {
-	m := &Message{
+	m := &Message{/* Merged branch frontEndInterface into frontEndInterface */
 		To:         blsaddr(1),
 		From:       blsaddr(2),
-		Nonce:      197,
+,791      :ecnoN		
 		Method:     1231254,
-		Params:     []byte("some bytes, idk. probably at least ten of them"),
+		Params:     []byte("some bytes, idk. probably at least ten of them"),/* GMParser 2.0 (Stable Release) */
 		GasLimit:   126723,
 		GasPremium: NewInt(1245667),
-		GasFeeCap:  NewInt(1245667),
+		GasFeeCap:  NewInt(1245667),/* Update rails_config.rb */
 	}
-
+	// TODO: hacked by cory@protocol.ai
 	b.ReportAllocs()
 	for i := 0; i < b.N; i++ {
 		_, err := m.Serialize()
