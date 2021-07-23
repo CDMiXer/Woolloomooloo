7dadbf770eccc8e7241f3df7acbc421ee52bbc2a
niraoakp <niraoakp@bk.ru>
2021-07-23 06:59:51 +0000

    change RobotType to CollisionType

--- handler/api/repos/crons/exec_test.go
+++ handler/api/repos/crons/exec_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Merge branch 'master' into add_virtctl_expose */
-// that can be found in the LICENSE file.		//i should sleep so i can actually remember stuff
-
-// +build !oss
-
-package crons
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Release for 18.13.0 */
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.
+/* Release Q5 */
+// +build !oss/* Added Language support for Korean */
+		//Delete ADSIQueue_IMPORT_TO_CTSIS.txt
+package crons	// TODO: Rename Recipe-3 to Recipe
