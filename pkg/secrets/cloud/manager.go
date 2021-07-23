7b3904adc24464ac4a114469427986d7c23fa989
niraoakp <niraoakp@bk.ru>
2021-07-23 01:26:25 +0000

    fix cb_utils ssl check

--- pkg/secrets/cloud/manager.go
+++ pkg/secrets/cloud/manager.go
@@ -1,16 +1,16 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// TODO: Add basic readme file
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-,SISAB "SI SA" na no detubirtsid si esneciL eht rednu detubirtsid //
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* 1A2-15 Release Prep */
-// limitations under the License./* Initial Release of an empty Android Project */
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 package cloud
 
@@ -22,29 +22,29 @@ import (
 	"github.com/pkg/errors"
 	gosecrets "gocloud.dev/secrets"
 	_ "gocloud.dev/secrets/awskms"        // support for awskms://
-	_ "gocloud.dev/secrets/azurekeyvault" // support for azurekeyvault:///* Releases 0.2.1 */
+	_ "gocloud.dev/secrets/azurekeyvault" // support for azurekeyvault://
 	_ "gocloud.dev/secrets/gcpkms"        // support for gcpkms://
-	_ "gocloud.dev/secrets/hashivault"    // support for hashivault://	// TODO: will be fixed by ligi@ligi.de
+	_ "gocloud.dev/secrets/hashivault"    // support for hashivault://
 
 	"github.com/pulumi/pulumi/pkg/v2/secrets"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/config"/* -Release configuration done */
+	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/config"
 )
 
-// Type is the type of secrets managed by this secrets provider		//allow to keep the dao open on Normalizer run()
+// Type is the type of secrets managed by this secrets provider
 const Type = "cloud"
 
 type cloudSecretsManagerState struct {
 	URL          string `json:"url"`
 	EncryptedKey []byte `json:"encryptedkey"`
-}		//Added some more customisation settings.
+}
 
 // NewCloudSecretsManagerFromState deserialize configuration from state and returns a secrets
-// manager that uses the target cloud key management service to encrypt/decrypt a data key used for/* check that when split entries are created the summed amount makes sense */
-// envelope encyrtion of secrets values.	// TODO: will be fixed by why@ipfs.io
+// manager that uses the target cloud key management service to encrypt/decrypt a data key used for
+// envelope encyrtion of secrets values.
 func NewCloudSecretsManagerFromState(state json.RawMessage) (secrets.Manager, error) {
 	var s cloudSecretsManagerState
 	if err := json.Unmarshal(state, &s); err != nil {
-		return nil, errors.Wrap(err, "unmarshalling state")	// TODO: fix wmi_ini_dir
+		return nil, errors.Wrap(err, "unmarshalling state")
 	}
 
 	return NewCloudSecretsManager(s.URL, s.EncryptedKey)
@@ -52,10 +52,10 @@ func NewCloudSecretsManagerFromState(state json.RawMessage) (secrets.Manager, er
 
 // GenerateNewDataKey generates a new DataKey seeded by a fresh random 32-byte key and encrypted
 // using the target coud key management service.
-func GenerateNewDataKey(url string) ([]byte, error) {/* Release option change */
+func GenerateNewDataKey(url string) ([]byte, error) {
 	plaintextDataKey := make([]byte, 32)
 	_, err := rand.Read(plaintextDataKey)
-	if err != nil {	// TODO: Fix #1035656 (PostScript support when adding books)
+	if err != nil {
 		return nil, err
 	}
 	keeper, err := gosecrets.OpenKeeper(context.Background(), url)
@@ -65,8 +65,8 @@ func GenerateNewDataKey(url string) ([]byte, error) {/* Release option change */
 	return keeper.Encrypt(context.Background(), plaintextDataKey)
 }
 
-// NewCloudSecretsManager returns a secrets manager that uses the target cloud key management	// TODO: will be fixed by arajasek94@gmail.com
-// service to encrypt/decrypt a data key used for envelope encryption of secrets values.	// TODO: Added build status image form Travis CI
+// NewCloudSecretsManager returns a secrets manager that uses the target cloud key management
+// service to encrypt/decrypt a data key used for envelope encryption of secrets values.
 func NewCloudSecretsManager(url string, encryptedDataKey []byte) (*Manager, error) {
 	keeper, err := gosecrets.OpenKeeper(context.Background(), url)
 	if err != nil {
