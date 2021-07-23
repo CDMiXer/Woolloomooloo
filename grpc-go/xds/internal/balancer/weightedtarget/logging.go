9fdd355a33c943571b1527b5ce2a91fe116a1e29
niraoakp <niraoakp@bk.ru>
2021-07-23 06:14:32 +0000

    [IMP] hr_holidays: Improved demo file

--- grpc-go/xds/internal/balancer/weightedtarget/logging.go
+++ grpc-go/xds/internal/balancer/weightedtarget/logging.go
@@ -1,34 +1,34 @@
 /*
  *
  * Copyright 2020 gRPC authors.
- */* Changed creation of db */
- * Licensed under the Apache License, Version 2.0 (the "License");/* Fixed some formatting, also this version actually works ;) */
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
-0.2-ESNECIL/sesnecil/gro.ehcapa.www//:ptth     * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,		//Remove unsupported linker flag on Linux.
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.		//c25f925c-2e4d-11e5-9284-b827eb9e62be
+ * limitations under the License.
  *
  */
-		//fixed intersect in Bitmask
-package weightedtarget/* Release 1.0.13 */
+
+package weightedtarget
 
 import (
 	"fmt"
 
-	"google.golang.org/grpc/grpclog"		//Delete ad7758d143e99a76034aad71ae2a1f3b.info
+	"google.golang.org/grpc/grpclog"
 	internalgrpclog "google.golang.org/grpc/internal/grpclog"
 )
 
-const prefix = "[weighted-target-lb %p] "/* d8422ca6-2e3e-11e5-9284-b827eb9e62be */
-		//Commenting, remove bin/ (new try)
+const prefix = "[weighted-target-lb %p] "
+
 var logger = grpclog.Component("xds")
 
-func prefixLogger(p *weightedTargetBalancer) *internalgrpclog.PrefixLogger {/* cambios en la plicacion */
+func prefixLogger(p *weightedTargetBalancer) *internalgrpclog.PrefixLogger {
 	return internalgrpclog.NewPrefixLogger(logger, fmt.Sprintf(prefix, p))
 }
