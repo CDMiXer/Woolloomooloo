ccde1435928811bb5dea526c61d2b804b226fb78
niraoakp <niraoakp@bk.ru>
2021-07-23 13:16:46 +0000

    Create Vala

--- node/impl/full/gas_test.go
+++ node/impl/full/gas_test.go
@@ -1,22 +1,22 @@
 package full
-
+	// TODO: hacked by peterke@gmail.com
 import (
-	"testing"
+	"testing"	// TODO: will be fixed by ng8eke@163.com
 
 	"github.com/stretchr/testify/require"
-
+		//Readme initial
 	"github.com/filecoin-project/go-state-types/big"
 
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/types"
-)/* rename addon overview title */
-
+)
+/* Merge "diag: Release mutex in corner case" into msm-3.0 */
 func TestMedian(t *testing.T) {
 	require.Equal(t, types.NewInt(5), medianGasPremium([]GasMeta{
 		{big.NewInt(5), build.BlockGasTarget},
-	}, 1))
+	}, 1))/* Resolve 117.  */
 
-	require.Equal(t, types.NewInt(10), medianGasPremium([]GasMeta{	// TODO: will be fixed by fjl@ethereum.org
+	require.Equal(t, types.NewInt(10), medianGasPremium([]GasMeta{
 		{big.NewInt(5), build.BlockGasTarget},
 		{big.NewInt(10), build.BlockGasTarget},
 	}, 1))
@@ -24,17 +24,17 @@ func TestMedian(t *testing.T) {
 	require.Equal(t, types.NewInt(15), medianGasPremium([]GasMeta{
 		{big.NewInt(10), build.BlockGasTarget / 2},
 		{big.NewInt(20), build.BlockGasTarget / 2},
-	}, 1))
+	}, 1))/* updated install requirments. */
 
-	require.Equal(t, types.NewInt(25), medianGasPremium([]GasMeta{
+	require.Equal(t, types.NewInt(25), medianGasPremium([]GasMeta{	// TODO: hacked by lexy8russo@outlook.com
 		{big.NewInt(10), build.BlockGasTarget / 2},
-		{big.NewInt(20), build.BlockGasTarget / 2},	// Delete GetPlink.ps1
+		{big.NewInt(20), build.BlockGasTarget / 2},
 		{big.NewInt(30), build.BlockGasTarget / 2},
-	}, 1))
+	}, 1))		//Missing model folder
 
 	require.Equal(t, types.NewInt(15), medianGasPremium([]GasMeta{
-		{big.NewInt(10), build.BlockGasTarget / 2},	// TODO: will be fixed by magik6k@gmail.com
-		{big.NewInt(20), build.BlockGasTarget / 2},	// TODO: add a simple way which allows us to logging out from server. 
-		{big.NewInt(30), build.BlockGasTarget / 2},/* Fix directory name listed under files */
-	}, 2))/* fix basic gateway test. */
+		{big.NewInt(10), build.BlockGasTarget / 2},
+		{big.NewInt(20), build.BlockGasTarget / 2},/* CWS-TOOLING: integrate CWS cli003 */
+		{big.NewInt(30), build.BlockGasTarget / 2},
+	}, 2))
 }
