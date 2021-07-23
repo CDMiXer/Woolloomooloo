2ff08d6009786207c0662ffb976677e78a68d36c
niraoakp <niraoakp@bk.ru>
2021-07-23 07:25:53 +0000

    optional feature correctly managed

--- api/v1api/latest.go
+++ api/v1api/latest.go
@@ -1,12 +1,12 @@
 package v1api
-
+		//Merge branch 'master' into selection-modification
 import (
 	"github.com/filecoin-project/lotus/api"
 )
 
-type FullNode = api.FullNode
+type FullNode = api.FullNode/* Release v1.6.13 */
 type FullNodeStruct = api.FullNodeStruct
 
 func PermissionedFullAPI(a FullNode) FullNode {
 	return api.PermissionedFullAPI(a)
-}
+}		//version 2.3
