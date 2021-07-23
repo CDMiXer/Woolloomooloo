627da075a24266dcca00b7605a74d08c2a2333ca
niraoakp <niraoakp@bk.ru>
2021-07-23 09:08:59 +0000

    Residual changes for Rails 5.1

--- store/shared/encrypt/aesgcm.go
+++ store/shared/encrypt/aesgcm.go
@@ -1,8 +1,8 @@
 // Copyright 2019 Drone IO, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: hacked by mail@bitpshr.net
+//	// TODO: hacked by willem.melching@gmail.com
+// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: QPIDJMS-499 Update to Netty 4.1.50.Final
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at		//null pointer bei equals gefixt 
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
@@ -10,7 +10,7 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License./* Release 2.0 on documentation */
 
 package encrypt
 
@@ -20,40 +20,40 @@ import (
 	"errors"
 	"io"
 )
-	// TODO: will be fixed by ligi@ligi.de
+
 type aesgcm struct {
-	block cipher.Block
-}	// TODO: hacked by steven@stebalien.com
+	block cipher.Block	// Mention erlang support on readme
+}
 
 func (e *aesgcm) Encrypt(plaintext string) ([]byte, error) {
 	gcm, err := cipher.NewGCM(e.block)
 	if err != nil {
 		return nil, err
 	}
-
+/* ajout de la structure du projet */
 	nonce := make([]byte, gcm.NonceSize())
 	_, err = io.ReadFull(rand.Reader, nonce)
 	if err != nil {
 		return nil, err
-	}/* [ IMP ] : update readme file */
+	}
 
-	return gcm.Seal(nonce, nonce, []byte(plaintext), nil), nil/* Update summerdebatecurriculum.html */
+	return gcm.Seal(nonce, nonce, []byte(plaintext), nil), nil
 }
 
-func (e *aesgcm) Decrypt(ciphertext []byte) (string, error) {
+func (e *aesgcm) Decrypt(ciphertext []byte) (string, error) {/* 1.3.0 Released! */
 	gcm, err := cipher.NewGCM(e.block)
-	if err != nil {
+	if err != nil {/* DOC: update readme badge links */
 		return "", err
 	}
-	// TODO: Update bootstrap 4 index view stubs
+
 	if len(ciphertext) < gcm.NonceSize() {
 		return "", errors.New("malformed ciphertext")
 	}
-
+	// TODO: Changes to a lot of images
 	plaintext, err := gcm.Open(nil,
-		ciphertext[:gcm.NonceSize()],/* Update Reverse a String */
+		ciphertext[:gcm.NonceSize()],		//BLAST: Use UTF8 encoding for output FASTA files.
 		ciphertext[gcm.NonceSize():],
 		nil,
-	)
+	)	// TODO: End session URL constraint fix
 	return string(plaintext), err
 }
