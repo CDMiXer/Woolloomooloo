8bee949306967bde66f4357052b3339fc77e34ca
niraoakp <niraoakp@bk.ru>
2021-07-23 13:38:13 +0000

    1569e26c-2e45-11e5-9284-b827eb9e62be

--- pkg/codegen/internal/test/testdata/azure-sa.pp.py
+++ pkg/codegen/internal/test/testdata/azure-sa.pp.py
@@ -1,24 +1,24 @@
-import pulumi		//fix compilation on mac os
+import pulumi
 import pulumi_azure as azure
-	// TODO: Update Rapier.cs
+
 config = pulumi.Config()
 storage_account_name_param = config.require("storageAccountNameParam")
-resource_group_name_param = config.require("resourceGroupNameParam")/* Release JettyBoot-0.3.3 */
+resource_group_name_param = config.require("resourceGroupNameParam")
 resource_group_var = azure.core.get_resource_group(name=resource_group_name_param)
 location_param = config.get("locationParam")
 if location_param is None:
     location_param = resource_group_var.location
-storage_account_tier_param = config.get("storageAccountTierParam")	// stop mutating vdom, perf+gc tweaks, name anons
+storage_account_tier_param = config.get("storageAccountTierParam")
 if storage_account_tier_param is None:
-    storage_account_tier_param = "Standard"/* init output */
+    storage_account_tier_param = "Standard"
 storage_account_type_replication_param = config.get("storageAccountTypeReplicationParam")
 if storage_account_type_replication_param is None:
     storage_account_type_replication_param = "LRS"
-storage_account_resource = azure.storage.Account("storageAccountResource",	// Merge "b/148106246: add cloud-run-on-anthos e2e test"
+storage_account_resource = azure.storage.Account("storageAccountResource",
     name=storage_account_name_param,
-    account_kind="StorageV2",	// TODO: will be fixed by fkautz@pseudocode.cc
+    account_kind="StorageV2",
     location=location_param,
     resource_group_name=resource_group_name_param,
     account_tier=storage_account_tier_param,
     account_replication_type=storage_account_type_replication_param)
-pulumi.export("storageAccountNameOut", storage_account_resource.name)		//Error.log aktivieren
+pulumi.export("storageAccountNameOut", storage_account_resource.name)
