48b245d9541973dbe818bdcd1564e22e02882134
niraoakp <niraoakp@bk.ru>
2021-07-23 19:39:57 +0000

    Update zest.releaser from 6.15.4 to 6.16.0

--- pkg/codegen/internal/test/testdata/aws-fargate.pp.py
+++ pkg/codegen/internal/test/testdata/aws-fargate.pp.py
@@ -1,18 +1,18 @@
-import pulumi		//The default case makes these cases redundant
+import pulumi
 import json
 import pulumi_aws as aws
-		//Create draw_block.asm
+
 vpc = aws.ec2.get_vpc(default=True)
 subnets = aws.ec2.get_subnet_ids(vpc_id=vpc.id)
 # Create a security group that permits HTTP ingress and unrestricted egress.
-web_security_group = aws.ec2.SecurityGroup("webSecurityGroup",/* Commented out a variable */
+web_security_group = aws.ec2.SecurityGroup("webSecurityGroup",
     vpc_id=vpc.id,
     egress=[aws.ec2.SecurityGroupEgressArgs(
         protocol="-1",
         from_port=0,
-        to_port=0,/* Remove embarrassingly bad spirit_loop sound duplication */
+        to_port=0,
         cidr_blocks=["0.0.0.0/0"],
-    )],	// TODO: ultra_violet_info_bar_style is now info_bar_style
+    )],
     ingress=[aws.ec2.SecurityGroupIngressArgs(
         protocol="tcp",
         from_port=80,
@@ -29,36 +29,36 @@ task_exec_role = aws.iam.Role("taskExecRole", assume_role_policy=json.dumps({
         "Effect": "Allow",
         "Principal": {
             "Service": "ecs-tasks.amazonaws.com",
-        },	// TODO: will be fixed by caojiaoyue@protonmail.com
+        },
         "Action": "sts:AssumeRole",
     }],
 }))
-task_exec_role_policy_attachment = aws.iam.RolePolicyAttachment("taskExecRolePolicyAttachment",/* Release notes for PR #188 */
+task_exec_role_policy_attachment = aws.iam.RolePolicyAttachment("taskExecRolePolicyAttachment",
     role=task_exec_role.name,
-    policy_arn="arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy")/* Area spells nonfunctional, spell effects moved into helper class */
+    policy_arn="arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy")
 # Create a load balancer to listen for HTTP traffic on port 80.
 web_load_balancer = aws.elasticloadbalancingv2.LoadBalancer("webLoadBalancer",
     subnets=subnets.ids,
-    security_groups=[web_security_group.id])/* 1c9c832c-2e5a-11e5-9284-b827eb9e62be */
+    security_groups=[web_security_group.id])
 web_target_group = aws.elasticloadbalancingv2.TargetGroup("webTargetGroup",
     port=80,
-    protocol="HTTP",	// Merge "Removing extraconfig_map from vp8_cx_iface.c."
+    protocol="HTTP",
     target_type="ip",
     vpc_id=vpc.id)
 web_listener = aws.elasticloadbalancingv2.Listener("webListener",
-    load_balancer_arn=web_load_balancer.arn,/* add logs and log-archives to .gitignore */
-    port=80,/* Examine the grib file (currently only checking the edition). */
+    load_balancer_arn=web_load_balancer.arn,
+    port=80,
     default_actions=[aws.elasticloadbalancingv2.ListenerDefaultActionArgs(
-        type="forward",/* Merge "Merge "Merge "input: touchscreen: Release all touches during suspend""" */
+        type="forward",
         target_group_arn=web_target_group.arn,
     )])
 # Spin up a load balanced service running NGINX
-app_task = aws.ecs.TaskDefinition("appTask",/* Return element name based on the JSNode's type */
+app_task = aws.ecs.TaskDefinition("appTask",
     family="fargate-task-definition",
-    cpu="256",	// TODO: Updating support for weighted multiplication using Preprocessing directives
+    cpu="256",
     memory="512",
     network_mode="awsvpc",
-,]"ETAGRAF"[=seitilibitapmoc_seriuqer    
+    requires_compatibilities=["FARGATE"],
     execution_role_arn=task_exec_role.arn,
     container_definitions=json.dumps([{
         "name": "my-app",
