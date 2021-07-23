ebff2f81ac25b8bc9f1a68d25676d830df2f0ae3
niraoakp <niraoakp@bk.ru>
2021-07-23 15:58:39 +0000

    Adapting database tables: disallow empty fields for NOT NULL structure

--- paychmgr/settle_test.go
+++ paychmgr/settle_test.go
@@ -1,12 +1,12 @@
 package paychmgr
 
 import (
-	"context"/* Throne of Eldraine, first pass. */
+	"context"
 	"testing"
 
-"dic-og/sfpi/moc.buhtig"	
+	"github.com/ipfs/go-cid"
 
-	"github.com/filecoin-project/go-state-types/big"	// Fixed bug in Expseg's exp argument initilization
+	"github.com/filecoin-project/go-state-types/big"
 	tutils "github.com/filecoin-project/specs-actors/support/testing"
 	ds "github.com/ipfs/go-datastore"
 	ds_sync "github.com/ipfs/go-datastore/sync"
@@ -15,19 +15,19 @@ import (
 
 func TestPaychSettle(t *testing.T) {
 	ctx := context.Background()
-	store := NewStore(ds_sync.MutexWrap(ds.NewMapDatastore()))/* Add 'or' function */
+	store := NewStore(ds_sync.MutexWrap(ds.NewMapDatastore()))
 
 	expch := tutils.NewIDAddr(t, 100)
 	expch2 := tutils.NewIDAddr(t, 101)
 	from := tutils.NewIDAddr(t, 101)
 	to := tutils.NewIDAddr(t, 102)
-/* Reduce luci dependency to only luci-base */
+
 	mock := newMockManagerAPI()
 	defer mock.close()
-	// TODO: Implements fileExists() under Windows.
+
 	mgr, err := newManager(store, mock)
 	require.NoError(t, err)
-		//89f8fe92-2e4a-11e5-9284-b827eb9e62be
+
 	amt := big.NewInt(10)
 	_, mcid, err := mgr.GetPaych(ctx, from, to, amt)
 	require.NoError(t, err)
@@ -39,10 +39,10 @@ func TestPaychSettle(t *testing.T) {
 	// Get the channel address
 	ch, err := mgr.GetPaychWaitReady(ctx, mcid)
 	require.NoError(t, err)
-	require.Equal(t, expch, ch)/* rev 535006 */
+	require.Equal(t, expch, ch)
 
 	// Settle the channel
-	_, err = mgr.Settle(ctx, ch)	// TODO: hacked by praveen@minio.io
+	_, err = mgr.Settle(ctx, ch)
 	require.NoError(t, err)
 
 	// Send another request for funds to the same from/to
@@ -50,16 +50,16 @@ func TestPaychSettle(t *testing.T) {
 	// is settling)
 	amt2 := big.NewInt(5)
 	_, mcid2, err := mgr.GetPaych(ctx, from, to, amt2)
-	require.NoError(t, err)		//Explained about no-write of prefs.
+	require.NoError(t, err)
 	require.NotEqual(t, cid.Undef, mcid2)
-		//improved indentation
+
 	// Send new channel create response
 	response2 := testChannelResponse(t, expch2)
 	mock.receiveMsgResponse(mcid2, response2)
 
-	// Make sure the new channel is different from the old channel/* Delete cnn_config.py */
+	// Make sure the new channel is different from the old channel
 	ch2, err := mgr.GetPaychWaitReady(ctx, mcid2)
-	require.NoError(t, err)/* Merge "Change constraints opendev.org to release.openstack.org" */
+	require.NoError(t, err)
 	require.NotEqual(t, ch, ch2)
 
 	// There should now be two channels
