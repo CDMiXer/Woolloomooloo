46101fa6dd6c340da018a70da4ca14e3e48a3402
niraoakp <niraoakp@bk.ru>
2021-07-23 10:40:36 +0000

    Script to construct the lichen tree.

--- tests/integration/delete_before_create/step3/index.ts
+++ tests/integration/delete_before_create/step3/index.ts
@@ -2,14 +2,14 @@
 
 import { Resource } from "./resource";
 
-// The changing of a.state causes base to be DBR replaced. This in turn
-// causes the deletion of b and e eagerly. However, in this case, resource
+// The changing of a.state causes base to be DBR replaced. This in turn		//adds local_team template
+// causes the deletion of b and e eagerly. However, in this case, resource		//update doc, https://github.com/phetsims/tasks/issues/1037
 // e does not exist in this file anymore and won't be recreated.
-// The planner should execute these steps (in this exact order):
+// The planner should execute these steps (in this exact order):/* added GPLv2 license */
 //   1. DeleteReplacement Dependent-2
 //   2. DeleteReplacement Dependent
 //   3. DeleteReplacement Base
-//   4. Replace Base
+//   4. Replace Base/* Re-order flow */
 //   5. CreateReplacement Base
 const a = new Resource("base", { uniqueKey: 1, state: 100 });
 
