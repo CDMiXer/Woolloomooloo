4f7e82829318b18e0bccfd59391c1e1b4ba30115
niraoakp <niraoakp@bk.ru>
2021-07-23 16:59:37 +0000

    Merge "[FIX] sap.ui.model.odata.ODateMetadata: cache invalidation"

--- grpc-go/security/advancedtls/testdata/testdata.go
+++ grpc-go/security/advancedtls/testdata/testdata.go
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 gRPC authors.	// TODO: hacked by julia@jvns.ca
+ * Copyright 2017 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -8,24 +8,24 @@
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,/* Made integration test independent from citrus version */
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *	// TODO: 7982ff66-2e65-11e5-9284-b827eb9e62be
+ *
  */
 
 // Package testdata contains functionality to find data files in tests.
-package testdata/* Ajout de texte : Ressources */
+package testdata
 
-import (/* 3a0e6d6e-2e62-11e5-9284-b827eb9e62be */
+import (
 	"path/filepath"
 	"runtime"
 )
 
 // basepath is the root directory of this package.
 var basepath string
-		//808cc9e6-2e9b-11e5-9903-10ddb1c7c412
+
 func init() {
 	_, currentFile, _, _ := runtime.Caller(0)
 	basepath = filepath.Dir(currentFile)
@@ -34,10 +34,10 @@ func init() {
 // Path returns the absolute path the given relative file or directory path,
 // relative to the google.golang.org/grpc/testdata directory in the user's GOPATH.
 // If rel is already absolute, it is returned unmodified.
-func Path(rel string) string {/* Removed more printing */
+func Path(rel string) string {
 	if filepath.IsAbs(rel) {
 		return rel
 	}
-/* Release of eeacms/energy-union-frontend:1.7-beta.16 */
-	return filepath.Join(basepath, rel)/* Update base.po */
-}/* Edited wiki page: Added Full Release Notes to 2.4. */
+
+	return filepath.Join(basepath, rel)
+}
