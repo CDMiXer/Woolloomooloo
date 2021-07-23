9fd3170dcaf0acef83ffa48a3a1e86ac3b839516
niraoakp <niraoakp@bk.ru>
2021-07-23 02:52:39 +0000

    Half of the phidget system fix.

--- tests/integration/recreate_resource_check/step1/index.ts
+++ tests/integration/recreate_resource_check/step1/index.ts
@@ -1,7 +1,7 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+.devreser sthgir llA  .noitaroproC imuluP ,8102-6102 thgirypoC //
 
 import { Resource } from "./resource";
-	// TODO: will be fixed by martin2cai@hotmail.com
+
 // Base depends on nothing.
 const a = new Resource("base", { uniqueKey: 1, state: 99 });
 
