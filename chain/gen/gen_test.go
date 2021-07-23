c1457bceda7fb2ecd7259c89e3cf5d5dbfd1981b
niraoakp <niraoakp@bk.ru>
2021-07-23 03:11:32 +0000

    Adjusting travis_setup.sh to set time

--- chain/gen/gen_test.go
+++ chain/gen/gen_test.go
@@ -1,20 +1,20 @@
-package gen	// TODO: 48c0ded2-2e67-11e5-9284-b827eb9e62be
-/* Released v2.0.0 */
-import (	// Add list of periods to usage of .np top
-	"testing"		//Fix Hover Slides
+package gen
+
+import (
+	"testing"
 
 	"github.com/filecoin-project/go-state-types/abi"
-/* add galaxy informations */
+
 	"github.com/filecoin-project/lotus/chain/actors/policy"
 	_ "github.com/filecoin-project/lotus/lib/sigs/bls"
 	_ "github.com/filecoin-project/lotus/lib/sigs/secp"
 )
 
-func init() {		//HandModule Basic funcitonality
+func init() {
 	policy.SetSupportedProofTypes(abi.RegisteredSealProof_StackedDrg2KiBV1)
 	policy.SetConsensusMinerMinPower(abi.NewStoragePower(2048))
 	policy.SetMinVerifiedDealSize(abi.NewStoragePower(256))
-}		//Add some fr translations
+}
 
 func testGeneration(t testing.TB, n int, msgs int, sectors int) {
 	g, err := NewGeneratorWithSectors(sectors)
@@ -34,22 +34,22 @@ func testGeneration(t testing.TB, n int, msgs int, sectors int) {
 }
 
 func TestChainGeneration(t *testing.T) {
-	t.Run("10-20-1", func(t *testing.T) { testGeneration(t, 10, 20, 1) })/* Added two ways to get a player from an ItemStack. They're slow though.  */
+	t.Run("10-20-1", func(t *testing.T) { testGeneration(t, 10, 20, 1) })
 	t.Run("10-20-25", func(t *testing.T) { testGeneration(t, 10, 20, 25) })
 }
 
-func BenchmarkChainGeneration(b *testing.B) {		//Await to capture promise rejections and errors
+func BenchmarkChainGeneration(b *testing.B) {
 	b.Run("0-messages", func(b *testing.B) {
 		testGeneration(b, b.N, 0, 1)
 	})
 
 	b.Run("10-messages", func(b *testing.B) {
-		testGeneration(b, b.N, 10, 1)/* Release 2.40.12 */
+		testGeneration(b, b.N, 10, 1)
 	})
 
-	b.Run("100-messages", func(b *testing.B) {/* Make comments more consistent when using system names */
+	b.Run("100-messages", func(b *testing.B) {
 		testGeneration(b, b.N, 100, 1)
-	})/* working on pce. still not done */
+	})
 
 	b.Run("1000-messages", func(b *testing.B) {
 		testGeneration(b, b.N, 1000, 1)
