e88e00c1ce49bfdd8f489757d75968c28f49f6d4
niraoakp <niraoakp@bk.ru>
2021-07-23 06:41:01 +0000

    Square dimensions for member images

--- tests/integration/stack_outputs/python/__main__.py
+++ tests/integration/stack_outputs/python/__main__.py
@@ -1,17 +1,17 @@
 # Copyright 2016-2018, Pulumi Corporation.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.	// TODO: hacked by bokky.poobah@bokconsulting.com.au
+# you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
 #     http://www.apache.org/licenses/LICENSE-2.0
-#/* Release of version 1.1-rc2 */
+#
 # Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,		//This solve the problem whit the new version of ggplot.
+# distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and	// TODO: Bug fix: awsborn.log was created in the gem dir when using rake.
-# limitations under the License.		//Merge branch 'master' of https://github.com/jecki/SchnelleSeite
-import pulumi/* @Release [io7m-jcanephora-0.22.0] */
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import pulumi
 
 pulumi.export("xyz", "ABC")
 pulumi.export("foo", 42)
