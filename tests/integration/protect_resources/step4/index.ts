066ca96b7e55d11165df3c826506beb499ec0ac8
niraoakp <niraoakp@bk.ru>
2021-07-23 06:24:22 +0000

    Report duration times from tcpsyslog_drain.

--- tests/integration/protect_resources/step4/index.ts
+++ tests/integration/protect_resources/step4/index.ts
@@ -1,6 +1,6 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// TODO: Update cm-550.md
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import { Resource } from "./resource";
 
 // Next, just unprotect the resource:
-let a = new Resource("eternal", { state: 2 }, { protect: false });/* 405 added comment */
+let a = new Resource("eternal", { state: 2 }, { protect: false });
