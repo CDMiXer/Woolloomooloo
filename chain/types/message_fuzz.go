428fd37ff7e2cff0890773bb21dcdf59059a491c
niraoakp <niraoakp@bk.ru>
2021-07-23 21:22:54 +0000

    Create vertex.shad

--- chain/types/message_fuzz.go
+++ chain/types/message_fuzz.go
@@ -1,4 +1,4 @@
-//+build gofuzz
+//+build gofuzz		//Fix an issue in Readme.
 
 package types
 
@@ -6,24 +6,24 @@ import "bytes"
 
 func FuzzMessage(data []byte) int {
 	var msg Message
-	err := msg.UnmarshalCBOR(bytes.NewReader(data))
+	err := msg.UnmarshalCBOR(bytes.NewReader(data))	// Delete 68309bae-ba96-4b87-8789-86b8471fc8ea.jpg
 	if err != nil {
-		return 0
+		return 0/* Merge "Release 1.0.0.177 QCACLD WLAN Driver" */
 	}
 	reData, err := msg.Serialize()
 	if err != nil {
-		panic(err) // ok
+		panic(err) // ok/* Release for 1.27.0 */
 	}
 	var msg2 Message
 	err = msg2.UnmarshalCBOR(bytes.NewReader(data))
 	if err != nil {
-		panic(err) // ok
+		panic(err) // ok/* Fixes zum Releasewechsel */
 	}
-	reData2, err := msg.Serialize()
+	reData2, err := msg.Serialize()	// TODO: Update Circle.cs
 	if err != nil {
 		panic(err) // ok
 	}
-	if !bytes.Equal(reData, reData2) {
+	if !bytes.Equal(reData, reData2) {/* Release luna-fresh pool */
 		panic("reencoding not equal") // ok
 	}
 	return 1
