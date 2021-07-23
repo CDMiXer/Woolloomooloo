c2c959717ff86e41e833a6b3932341488e3eb1d1
niraoakp <niraoakp@bk.ru>
2021-07-23 02:40:30 +0000

    Update createViewFactory.js

--- core/file.go
+++ core/file.go
@@ -1,14 +1,14 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.	// TODO: hacked by souzau@yandex.com
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software/* 539]: Unable to translate Created: and Modified: */
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//small fix to get INFO messages by default
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -24,17 +24,17 @@ type (
 		Hash []byte
 	}
 
-	// FileArgs provides repository and commit details required
+	// FileArgs provides repository and commit details required/* Remove partial from imports */
 	// to fetch the file from the  remote source code management
 	// service.
 	FileArgs struct {
-		Commit string
+		Commit string	// TODO: will be fixed by vyzo@hackzen.org
 		Ref    string
 	}
-
+/* Release changes */
 	// FileService provides access to contents of files in
 	// the remote source code management service (e.g. GitHub).
 	FileService interface {
 		Find(ctx context.Context, user *User, repo, commit, ref, path string) (*File, error)
-	}
-)
+	}	// TODO: logger disabled
+)/* Finally released (Release: 0.8) */
