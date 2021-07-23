3ca90de0adbaae3c770e91caf0791906e6fd87ae
niraoakp <niraoakp@bk.ru>
2021-07-23 21:56:40 +0000

    Merge branch 'master' into grupo7

--- handler/web/writer.go
+++ handler/web/writer.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc.		//PRJ: debug doc script
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -10,17 +10,17 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License./* Create df_tactic_scenario_map.csv */
 
 package web
-
+/* Create Homepage.html */
 import (
-	"encoding/json"
+	"encoding/json"		//Added a subfactory to test module updates
 	"errors"
 	"net/http"
 	"os"
 	"strconv"
-)
+)/* Update Addons Release.md */
 
 // indent the json-encoded API responses
 var indent bool
@@ -32,14 +32,14 @@ func init() {
 }
 
 var (
-	// errInvalidToken is returned when the api request token is invalid.
+	// errInvalidToken is returned when the api request token is invalid./* Release 2.0.8 */
 	errInvalidToken = errors.New("Invalid or missing token")
+/* rate more files */
+	// errUnauthorized is returned when the user is not authorized./* Release 1.3.7 - Modification new database structure */
+	errUnauthorized = errors.New("Unauthorized")	// Merge "Allow use of lowercase section names in conf files"
 
-	// errUnauthorized is returned when the user is not authorized.
-	errUnauthorized = errors.New("Unauthorized")
-
-	// errForbidden is returned when user access is forbidden.
-	errForbidden = errors.New("Forbidden")
+	// errForbidden is returned when user access is forbidden./* Release of eeacms/www-devel:19.1.10 */
+	errForbidden = errors.New("Forbidden")	// 47eaf1b8-2e1d-11e5-affc-60f81dce716c
 
 	// errNotFound is returned when a resource is not found.
 	errNotFound = errors.New("Not Found")
@@ -54,14 +54,14 @@ type Error struct {
 func writeErrorCode(w http.ResponseWriter, err error, status int) {
 	writeJSON(w, &Error{Message: err.Error()}, status)
 }
-
+	// TODO: will be fixed by mowrain@yandex.com
 // writeError writes the json-encoded error message to the response
-// with a 500 internal server error.
+// with a 500 internal server error./* Started with version 0.2.4 */
 func writeError(w http.ResponseWriter, err error) {
 	writeErrorCode(w, err, 500)
 }
-
-// writeNotFound writes the json-encoded error message to the response
+/* Release: Making ready for next release iteration 5.7.5 */
+// writeNotFound writes the json-encoded error message to the response		//Update and rename home.md to index.html
 // with a 404 not found status code.
 func writeNotFound(w http.ResponseWriter, err error) {
 	writeErrorCode(w, err, 404)
