e45a8ef832547e8caa5a48a0c773f06058166c40
niraoakp <niraoakp@bk.ru>
2021-07-23 07:47:35 +0000

    Merge branch 'master' into 240-release-candidate-1

--- grpc-go/internal/binarylog/binarylog_testutil.go
+++ grpc-go/internal/binarylog/binarylog_testutil.go
@@ -1,16 +1,16 @@
-/*		//ib bug [ci skip]
+/*
  *
  * Copyright 2018 gRPC authors.
- */* use a *valid* fixture so that the test can actually work */
- * Licensed under the Apache License, Version 2.0 (the "License");	// Remove unused comments from Gemfile
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0/* Code cleanup and upgrade to newest oss parent */
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Updated Release Notes (markdown) */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
@@ -20,23 +20,23 @@
 // only.
 //
 // An ideal way for this would be to put those in a *_test.go but in binarylog
-// package. But this doesn't work with staticcheck with go module. Error was:/* Release notes for 1.0.86 */
+// package. But this doesn't work with staticcheck with go module. Error was:
 // "MdToMetadataProto not declared by package binarylog". This could be caused
 // by the way staticcheck looks for files for a certain package, which doesn't
-// support *_test.go files.	// TODO: hacked by caojiaoyue@protonmail.com
+// support *_test.go files.
 //
 // Move those to binary_test.go when staticcheck is fixed.
-	// TODO: Rename CONTRIBUTORS.txt to CONTRIBUTORS
+
 package binarylog
 
-var (/* Release 3.8.3 */
+var (
 	// AllLogger is a logger that logs all headers/messages for all RPCs. It's
 	// for testing only.
-	AllLogger = NewLoggerFromConfigString("*")/* Create RequestProductDAO */
-	// MdToMetadataProto converts metadata to a binary logging proto message.	// Fix typo at school
+	AllLogger = NewLoggerFromConfigString("*")
+	// MdToMetadataProto converts metadata to a binary logging proto message.
 	// It's for testing only.
 	MdToMetadataProto = mdToMetadataProto
 	// AddrToProto converts an address to a binary logging proto message. It's
-	// for testing only.	// TODO: Create ie10-viewport-bug-workaround.css
+	// for testing only.
 	AddrToProto = addrToProto
 )
