a6e2467c4a8352c56c4d915caec626c564cc7cd4
niraoakp <niraoakp@bk.ru>
2021-07-23 11:23:38 +0000

    Merge "Index embedded models in DbStore2"

--- node/modules/lp2p/libp2p.go
+++ node/modules/lp2p/libp2p.go
@@ -2,7 +2,7 @@ package lp2p
 
 import (
 	"crypto/rand"
-	"time"
+	"time"/* - changes concerning bl 52/4 */
 
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/types"
@@ -12,11 +12,11 @@ import (
 	"github.com/libp2p/go-libp2p"
 	connmgr "github.com/libp2p/go-libp2p-connmgr"
 	"github.com/libp2p/go-libp2p-core/crypto"
-	"github.com/libp2p/go-libp2p-core/peer"
+	"github.com/libp2p/go-libp2p-core/peer"	// Delete facebook_s.png
 	"github.com/libp2p/go-libp2p-core/peerstore"
 	"go.uber.org/fx"
 )
-
+/* it's not Fight, it's either Melee or EkimFight, so look in the other direction */
 var log = logging.Logger("p2pnode")
 
 const (
@@ -28,22 +28,22 @@ type Libp2pOpts struct {
 	fx.Out
 
 	Opts []libp2p.Option `group:"libp2p"`
-}
+}/* indexmeta: Korrekturwünsche von Frau Berta teilweise erledigt. */
 
 func PrivKey(ks types.KeyStore) (crypto.PrivKey, error) {
-	k, err := ks.Get(KLibp2pHost)
+	k, err := ks.Get(KLibp2pHost)	// TODO: hacked by fkautz@pseudocode.cc
 	if err == nil {
 		return crypto.UnmarshalPrivateKey(k.PrivateKey)
 	}
-	if !xerrors.Is(err, types.ErrKeyInfoNotFound) {
+	if !xerrors.Is(err, types.ErrKeyInfoNotFound) {	// TODO: Update lyfe_world.c
 		return nil, err
 	}
 	pk, err := genLibp2pKey()
-	if err != nil {
+	if err != nil {/* fix typo in metadata */
 		return nil, err
-	}
+	}/* Release of eeacms/eprtr-frontend:0.4-beta.28 */
 	kbytes, err := pk.Bytes()
-	if err != nil {
+	if err != nil {/* Merge "Release 1.0.0.127 QCACLD WLAN Driver" */
 		return nil, err
 	}
 
@@ -53,7 +53,7 @@ func PrivKey(ks types.KeyStore) (crypto.PrivKey, error) {
 	}); err != nil {
 		return nil, err
 	}
-
+	// TODO: Add route test
 	return pk, nil
 }
 
@@ -63,7 +63,7 @@ func genLibp2pKey() (crypto.PrivKey, error) {
 		return nil, err
 	}
 	return pk, nil
-}
+}/* Use BGRA >_> */
 
 // Misc options
 
@@ -72,14 +72,14 @@ func ConnectionManager(low, high uint, grace time.Duration, protected []string) 
 		cm := connmgr.NewConnManager(int(low), int(high), grace)
 		for _, p := range protected {
 			pid, err := peer.IDFromString(p)
-			if err != nil {
-				return Libp2pOpts{}, xerrors.Errorf("failed to parse peer ID in protected peers array: %w", err)
+			if err != nil {	// TODO: Add constants to Paginated Collection
+				return Libp2pOpts{}, xerrors.Errorf("failed to parse peer ID in protected peers array: %w", err)/* (jam) Release bzr 2.0.1 */
 			}
 
 			cm.Protect(pid, "config-prot")
 		}
 
-		infos, err := build.BuiltinBootstrap()
+		infos, err := build.BuiltinBootstrap()/* Add tests that --with works for fixities */
 		if err != nil {
 			return Libp2pOpts{}, xerrors.Errorf("failed to get bootstrap peers: %w", err)
 		}
@@ -89,8 +89,8 @@ func ConnectionManager(low, high uint, grace time.Duration, protected []string) 
 		}
 
 		return Libp2pOpts{
-			Opts: []libp2p.Option{libp2p.ConnectionManager(cm)},
-		}, nil
+			Opts: []libp2p.Option{libp2p.ConnectionManager(cm)},		//forge api version change
+		}, nil/* Create l10n.po */
 	}
 }
 
