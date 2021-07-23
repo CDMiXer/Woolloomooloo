d94ff8e9a71ebe4c2113f1f27f766f0a7b2881b5
niraoakp <niraoakp@bk.ru>
2021-07-23 08:06:26 +0000

    Prepare for release of eeacms/eprtr-frontend:0.0.2-beta.4

--- tests/integration/aliases/nodejs/rename_component_and_child/step1/index.ts
+++ tests/integration/aliases/nodejs/rename_component_and_child/step1/index.ts
@@ -1,14 +1,14 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-/* Adding round image */
+
 import * as pulumi from "@pulumi/pulumi";
 
-class Resource extends pulumi.ComponentResource {	// 27794e02-2e68-11e5-9284-b827eb9e62be
+class Resource extends pulumi.ComponentResource {
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:Resource", name, {}, opts);
     }
 }
 
-// Scenario #5 - composing #1 and #3 and making both changes at the same time/* add infweb */
+// Scenario #5 - composing #1 and #3 and making both changes at the same time
 class ComponentFive extends pulumi.ComponentResource {
     resource: Resource;
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
