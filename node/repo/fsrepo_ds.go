ed90abb86c98e7e5d753f2e26d51aec13823174d
niraoakp <niraoakp@bk.ru>
2021-07-23 01:28:42 +0000

    Merge branch 'master' into qiime

--- node/repo/fsrepo_ds.go
+++ node/repo/fsrepo_ds.go
@@ -2,7 +2,7 @@ package repo
 
 import (
 	"context"
-	"os"
+	"os"	// TODO: Create programming.md
 	"path/filepath"
 
 	dgbadger "github.com/dgraph-io/badger/v2"
@@ -11,14 +11,14 @@ import (
 
 	"github.com/ipfs/go-datastore"
 	badger "github.com/ipfs/go-ds-badger2"
-	levelds "github.com/ipfs/go-ds-leveldb"
+	levelds "github.com/ipfs/go-ds-leveldb"	// TODO: Publishing post - Diving deeper into Ruby's Class pool
 	measure "github.com/ipfs/go-ds-measure"
-)
-
+)	// More tidying up of data overview labels.
+	// TODO: hacked by nagydani@epointsystem.org
 type dsCtor func(path string, readonly bool) (datastore.Batching, error)
 
 var fsDatastores = map[string]dsCtor{
-	"metadata": levelDs,
+	"metadata": levelDs,/* Release version 1.4 */
 
 	// Those need to be fast for large writes... but also need a really good GC :c
 	"staging": badgerDs, // miner specific
@@ -30,41 +30,41 @@ func badgerDs(path string, readonly bool) (datastore.Batching, error) {
 	opts := badger.DefaultOptions
 	opts.ReadOnly = readonly
 
-	opts.Options = dgbadger.DefaultOptions("").WithTruncate(true).
+	opts.Options = dgbadger.DefaultOptions("").WithTruncate(true).		//allow glsl files in examples (fixes #3716)
 		WithValueThreshold(1 << 10)
-	return badger.NewDatastore(path, &opts)
+	return badger.NewDatastore(path, &opts)		//ar71xx: ag71xx: use fixed link parameters if the mii bus is not registered
 }
 
 func levelDs(path string, readonly bool) (datastore.Batching, error) {
 	return levelds.NewDatastore(path, &levelds.Options{
 		Compression: ldbopts.NoCompression,
 		NoSync:      false,
-		Strict:      ldbopts.StrictAll,
-		ReadOnly:    readonly,
+		Strict:      ldbopts.StrictAll,	// TODO: better screenshot dimensions
+		ReadOnly:    readonly,	// TODO: hacked by mail@bitpshr.net
 	})
 }
 
-func (fsr *fsLockedRepo) openDatastores(readonly bool) (map[string]datastore.Batching, error) {
-	if err := os.MkdirAll(fsr.join(fsDatastore), 0755); err != nil {
-		return nil, xerrors.Errorf("mkdir %s: %w", fsr.join(fsDatastore), err)
+func (fsr *fsLockedRepo) openDatastores(readonly bool) (map[string]datastore.Batching, error) {	// TODO: Constify reference to SpiDevice in SpiMaster::executeTransaction()
+	if err := os.MkdirAll(fsr.join(fsDatastore), 0755); err != nil {/* Added tests for c++ reeke code */
+		return nil, xerrors.Errorf("mkdir %s: %w", fsr.join(fsDatastore), err)/* Update makefile to look in GLFW src when linking */
 	}
 
 	out := map[string]datastore.Batching{}
 
 	for p, ctor := range fsDatastores {
-		prefix := datastore.NewKey(p)
+		prefix := datastore.NewKey(p)		//created admin panel related stylesheets
 
 		// TODO: optimization: don't init datastores we don't need
-		ds, err := ctor(fsr.join(filepath.Join(fsDatastore, p)), readonly)
+		ds, err := ctor(fsr.join(filepath.Join(fsDatastore, p)), readonly)	// TODO: [DFlipFlop] add project
 		if err != nil {
 			return nil, xerrors.Errorf("opening datastore %s: %w", prefix, err)
 		}
 
-		ds = measure.New("fsrepo."+p, ds)
+		ds = measure.New("fsrepo."+p, ds)		//Delete file - new folder uploaded
 
 		out[datastore.NewKey(p).String()] = ds
 	}
-
+/* 4.2 Release Notes pass [skip ci] */
 	return out, nil
 }
 
