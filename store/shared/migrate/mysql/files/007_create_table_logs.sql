0dceeb46118da1a69cccdf694ed30b562a2e62f7
niraoakp <niraoakp@bk.ru>
2021-07-23 08:54:01 +0000

    Create Chorus Pro

--- store/shared/migrate/mysql/files/007_create_table_logs.sql
+++ store/shared/migrate/mysql/files/007_create_table_logs.sql
@@ -1,6 +1,6 @@
 -- name: create-table-logs
-
+	// 779383b4-2d53-11e5-baeb-247703a38240
 CREATE TABLE IF NOT EXISTS logs (
  log_id    INTEGER PRIMARY KEY
-,log_data  MEDIUMBLOB
+,log_data  MEDIUMBLOB/* Release to 12.4.0 - SDK Usability Improvement */
 );
