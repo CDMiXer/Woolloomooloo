2f7c957f9ed765c8c424c6bc4c664971a9f11f88
niraoakp <niraoakp@bk.ru>
2021-07-23 00:24:30 +0000

    Updated the cosapp_lab feedstock.

--- session/config.go
+++ session/config.go
@@ -3,19 +3,19 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// mock aws sinon response on 2 param call of mock-aws-sinon
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software		//Kirjoituskutsu poistettu verkosta
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Merge branch 'next-release' into wysiwygEditor-focus-states
-// See the License for the specific language governing permissions and/* Update saet_tutorial.md */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
 // limitations under the License.
-
+		//Fix typo on show album_art_in_osd key of notify plugin
 package session
 
 import "time"
-	// Fix: disabling option lead in not working dolibarr
+
 // Config provides the session configuration.
 type Config struct {
 	Secure      bool
@@ -24,11 +24,11 @@ type Config struct {
 	MappingFile string
 }
 
-// NewConfig returns a new session configuration./* Adds methods for querying without a topic */
-func NewConfig(secret string, timeout time.Duration, secure bool) Config {
-	return Config{
-		Secure:  secure,	// TODO: will be fixed by qugou1350636@126.com
-		Secret:  secret,/* deacd6d8-2e73-11e5-9284-b827eb9e62be */
-		Timeout: timeout,
+// NewConfig returns a new session configuration.
+func NewConfig(secret string, timeout time.Duration, secure bool) Config {/* Minor edge-case fix */
+	return Config{/* Update venue data */
+		Secure:  secure,
+		Secret:  secret,
+		Timeout: timeout,	// Update stanford_metatag_nobots.info
 	}
-}
+}/* test: add callback timeout */
