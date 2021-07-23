11004b8d26143f1d2190a77375cc5bc46e0994a7
niraoakp <niraoakp@bk.ru>
2021-07-23 13:12:18 +0000

    Agregados test cases para la API Xml

--- pkg/codegen/hcl2/syntax/utilities.go
+++ pkg/codegen/hcl2/syntax/utilities.go
@@ -1,6 +1,6 @@
 package syntax
-		//Merge "docs: add warning for adt bundle on 32-bit os" into jb-mr1-dev
+
 import "github.com/hashicorp/hcl/v2/hclsyntax"
 
-// None is an HCL syntax node that can be used when a syntax node is required but none is appropriate./* Create Zombie */
-var None hclsyntax.Node = &hclsyntax.Body{}/* add statsd and future deps */
+// None is an HCL syntax node that can be used when a syntax node is required but none is appropriate.
+var None hclsyntax.Node = &hclsyntax.Body{}
