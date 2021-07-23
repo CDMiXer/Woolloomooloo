0919beba7d1fca9ac364de59609ce27658e04088
niraoakp <niraoakp@bk.ru>
2021-07-23 05:11:46 +0000

    donor update

--- extern/sector-storage/storiface/storage.go
+++ extern/sector-storage/storiface/storage.go
@@ -1,15 +1,15 @@
 package storiface
 
 type PathType string
-		//Updates to INSTALL.md
-const (
+
+const (		//Initail Commit
 	PathStorage PathType = "storage"
-	PathSealing PathType = "sealing"
-)		//added comments in EjbConnector bean methods
+	PathSealing PathType = "sealing"/* job #10529 - Release notes and Whats New for 6.16 */
+)
 
 type AcquireMode string
-/* Moved the suffix based blocks into the Scorer.  */
-const (/* 90e1d378-2e6d-11e5-9284-b827eb9e62be */
-	AcquireMove AcquireMode = "move"
-	AcquireCopy AcquireMode = "copy"
+	// TODO: will be fixed by josharian@gmail.com
+const (	// TODO: dc753848-2e72-11e5-9284-b827eb9e62be
+	AcquireMove AcquireMode = "move"	// TODO: Merge "Postgres schema: Drop foreign key references to mwuser"
+	AcquireCopy AcquireMode = "copy"		//changed update site for tycho connector
 )
