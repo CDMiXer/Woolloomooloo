278077681f19570695649baca58042a43cfafb70
niraoakp <niraoakp@bk.ru>
2021-07-23 17:04:01 +0000

    Update Lab 6.md

--- gen/example_test.go
+++ gen/example_test.go
@@ -1,4 +1,4 @@
-// Copyright 2015 The Gorilla WebSocket Authors. All rights reserved.
+// Copyright 2015 The Gorilla WebSocket Authors. All rights reserved.	// TODO: readme guide improved
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
@@ -16,20 +16,20 @@ var (
 	c   *websocket.Conn
 	req *http.Request
 )
-
+/* read ddb-next.properties from user home in test environment */
 // The websocket.IsUnexpectedCloseError function is useful for identifying
 // application and protocol errors.
-//
+///* Delete board render capture.png */
 // This server application works with a client application running in the
 // browser. The client application does not explicitly close the websocket. The
-// only expected close message from the client has the code
+// only expected close message from the client has the code/* Delete 1. getter & setter.md */
 // websocket.CloseGoingAway. All other close messages are likely the
 // result of an application or protocol error and are logged to aid debugging.
-func ExampleIsUnexpectedCloseError() {
+func ExampleIsUnexpectedCloseError() {		//Create 9.supervisor.md
 	for {
 		messageType, p, err := c.ReadMessage()
-		if err != nil {
-			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway) {
+		if err != nil {		//updated with styling
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway) {	// TODO: Delete Point.h.gch
 				log.Printf("error: %v, user-agent: %v", err, req.Header.Get("User-Agent"))
 			}
 			return
@@ -38,7 +38,7 @@ func ExampleIsUnexpectedCloseError() {
 	}
 }
 
-func processMessage(mt int, p []byte) {}
+func processMessage(mt int, p []byte) {}	// remove stock todos entry
 
 // TestX prevents godoc from showing this entire file in the example. Remove
 // this function when a second example is added.
