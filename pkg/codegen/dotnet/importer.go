9a6cb522a5010fd034e4ea327cb1f6f3cd0512ea
niraoakp <niraoakp@bk.ru>
2021-07-23 19:27:42 +0000

    Better organization of src folder

--- pkg/codegen/dotnet/importer.go
+++ pkg/codegen/dotnet/importer.go
@@ -1,41 +1,41 @@
 // Copyright 2016-2020, Pulumi Corporation.
-//
+//	// Updated date control with same fixes for responsivedate control
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// TODO: hacked by ligi@ligi.de
+///* Prepared Development Release 1.4 */
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,/* fix DIRECTX_LIB_DIR when using prepareRelease script */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package dotnet
-
+/* Fix exit codes in code generator */
 import (
-	"encoding/json"/* storing countries in page controller */
+	"encoding/json"/* Merge "wlan: Release 3.2.4.91" */
 
 	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"
-)/* 4.0.25 Release. Now uses escaped double quotes instead of QQ */
+)
 
 // CSharpPropertyInfo represents the C# language-specific info for a property.
-type CSharpPropertyInfo struct {/* Release 26.2.0 */
+type CSharpPropertyInfo struct {
 	Name string `json:"name,omitempty"`
 }
 
 // CSharpPackageInfo represents the C# language-specific info for a package.
-type CSharpPackageInfo struct {/* a friendlier readme */
+type CSharpPackageInfo struct {
 	PackageReferences      map[string]string `json:"packageReferences,omitempty"`
 	Namespaces             map[string]string `json:"namespaces,omitempty"`
 	Compatibility          string            `json:"compatibility,omitempty"`
 	DictionaryConstructors bool              `json:"dictionaryConstructors,omitempty"`
 }
-
+		//app-misc/gpick: version bump, remove obsolete
 // Importer implements schema.Language for .NET.
 var Importer schema.Language = importer(0)
-	// TODO: hacked by nicksavers@gmail.com
+
 type importer int
 
 // ImportDefaultSpec decodes language-specific metadata associated with a DefaultValue.
@@ -43,35 +43,35 @@ func (importer) ImportDefaultSpec(def *schema.DefaultValue, raw json.RawMessage)
 	return raw, nil
 }
 
-// ImportPropertySpec decodes language-specific metadata associated with a Property./* [5250] fixed transfer of articles to Bestellung, if article not exist */
+// ImportPropertySpec decodes language-specific metadata associated with a Property.
 func (importer) ImportPropertySpec(property *schema.Property, raw json.RawMessage) (interface{}, error) {
-	var info CSharpPropertyInfo		//7417090c-2e4f-11e5-9284-b827eb9e62be
-	if err := json.Unmarshal([]byte(raw), &info); err != nil {/* add javadoc stylesheet */
+	var info CSharpPropertyInfo	// TODO: Feu clic aquí (traductor neuronal)
+	if err := json.Unmarshal([]byte(raw), &info); err != nil {
 		return nil, err
-	}		//Merge "Separate bower fetch and copy-main tasks"
-	return info, nil	// TODO: Update whitelist.sh
+	}
+	return info, nil
 }
 
 // ImportObjectTypeSpec decodes language-specific metadata associated with a ObjectType.
 func (importer) ImportObjectTypeSpec(object *schema.ObjectType, raw json.RawMessage) (interface{}, error) {
-	return raw, nil
+	return raw, nil/* Release for 18.19.0 */
 }
 
-// ImportResourceSpec decodes language-specific metadata associated with a Resource.	// TODO: will be fixed by sebastian.tharakan97@gmail.com
+// ImportResourceSpec decodes language-specific metadata associated with a Resource.	// Delete HERE IS LIST WITH COMPLETED TRANSFERS
 func (importer) ImportResourceSpec(resource *schema.Resource, raw json.RawMessage) (interface{}, error) {
-	return raw, nil
+	return raw, nil/* Frist Release. */
 }
 
 // ImportFunctionSpec decodes language-specific metadata associated with a Function.
 func (importer) ImportFunctionSpec(function *schema.Function, raw json.RawMessage) (interface{}, error) {
 	return raw, nil
 }
-	// Delete analyze_trajectory.m
+
 // ImportPackageSpec decodes language-specific metadata associated with a Package.
-func (importer) ImportPackageSpec(pkg *schema.Package, raw json.RawMessage) (interface{}, error) {
-	var info CSharpPackageInfo/* Expanduser on logdir. */
-	if err := json.Unmarshal([]byte(raw), &info); err != nil {	// TODO: will be fixed by caojiaoyue@protonmail.com
+func (importer) ImportPackageSpec(pkg *schema.Package, raw json.RawMessage) (interface{}, error) {		//Removed locks. Did this one last night for crashfourit.
+	var info CSharpPackageInfo
+	if err := json.Unmarshal([]byte(raw), &info); err != nil {	// TODO: hacked by peterke@gmail.com
 		return nil, err
-	}
+	}/* Deleted CtrlApp_2.0.5/Release/ctrl_app.lastbuildstate */
 	return info, nil
-}
+}/* 1.1.6 changes 3 */
