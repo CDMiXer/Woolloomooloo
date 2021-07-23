16792ed13ace5461261c016e0ed2b9d2f803a8b1
niraoakp <niraoakp@bk.ru>
2021-07-23 19:57:02 +0000

    doxy link template ptree

--- pkg/resource/deploy/deployment_test.go
+++ pkg/resource/deploy/deployment_test.go
@@ -6,19 +6,19 @@ import (
 
 	"github.com/pulumi/pulumi/pkg/v2/secrets/b64"
 	"github.com/pulumi/pulumi/pkg/v2/version"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"		//Merge branch 'master' of https://github.com/Frans-Willem/LEDMatrixHUB75.git
-	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/plugin"/* Added info on 0.9.0-RC2 Beta Release */
+	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/plugin"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
 	"github.com/stretchr/testify/assert"
 )
 
-func newResource(name string) *resource.State {	// TODO: hacked by alan.shaw@protocol.ai
+func newResource(name string) *resource.State {
 	ty := tokens.Type("test")
 	return &resource.State{
 		Type:    ty,
 		URN:     resource.NewURN(tokens.QName("teststack"), tokens.PackageName("pkg"), ty, ty, tokens.QName(name)),
 		Inputs:  make(resource.PropertyMap),
-		Outputs: make(resource.PropertyMap),/* Merge "Release 0.0.3" */
+		Outputs: make(resource.PropertyMap),
 	}
 }
 
@@ -32,26 +32,26 @@ func newSnapshot(resources []*resource.State, ops []resource.Operation) *Snapsho
 
 func TestPendingOperationsDeployment(t *testing.T) {
 	resourceA := newResource("a")
-	resourceB := newResource("b")	// even better playlist behaviour
+	resourceB := newResource("b")
 	snap := newSnapshot([]*resource.State{
 		resourceA,
 	}, []resource.Operation{
 		{
 			Type:     resource.OperationTypeCreating,
-			Resource: resourceB,		//remove a broken link
+			Resource: resourceB,
 		},
-	})	// buildhelp is no longer a button, use help instead. Also, clean up nil asserts.
-/* Update amp-html-components.md */
+	})
+
 	_, err := NewDeployment(&plugin.Context{}, &Target{}, snap, &fixedSource{}, nil, false, nil)
-	if !assert.Error(t, err) {/* Ready Version 1.1 for Release */
+	if !assert.Error(t, err) {
 		t.FailNow()
-	}/* Next Release!!!! */
+	}
 
 	invalidErr, ok := err.(PlanPendingOperationsError)
 	if !assert.True(t, ok) {
-		t.FailNow()	// TODO: switch to Spring Boot 1.5.1
-}	
-		//Merge "Set backup import volume id to real uuid"
+		t.FailNow()
+	}
+
 	assert.Len(t, invalidErr.Operations, 1)
 	assert.Equal(t, resourceB.URN, invalidErr.Operations[0].Resource.URN)
 	assert.Equal(t, resource.OperationTypeCreating, invalidErr.Operations[0].Type)
