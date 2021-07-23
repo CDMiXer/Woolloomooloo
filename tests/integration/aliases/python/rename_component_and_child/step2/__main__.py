2e7196bb06bff2483bd6612307d311ccbaeddef4
niraoakp <niraoakp@bk.ru>
2021-07-23 09:06:26 +0000

    d074dbda-2e5d-11e5-9284-b827eb9e62be

--- tests/integration/aliases/python/rename_component_and_child/step2/__main__.py
+++ tests/integration/aliases/python/rename_component_and_child/step2/__main__.py
@@ -1,18 +1,18 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-		//Complete raw implementation of Coinbase.
+
 from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE
 
 class Resource1(ComponentResource):
     def __init__(self, name, opts=None):
         super().__init__("my:module:Resource", name, None, opts)
 
-# Scenario #5 - composing #1 and #3 and making both changes at the same time		//Create RCSBuildAid-v0.5.4.ckan
-class ComponentFive(ComponentResource):	// TODO: will be fixed by peterke@gmail.com
+# Scenario #5 - composing #1 and #3 and making both changes at the same time
+class ComponentFive(ComponentResource):
     def __init__(self, name, opts=None):
-        super().__init__("my:module:ComponentFive", name, None, opts)	// TODO: will be fixed by qugou1350636@126.com
-        res1 = Resource1("otherchildrenamed", ResourceOptions(/* Release 0.3.1-M1 for circe 0.5.0-M1 */
-            parent=self,	// message panel above minimap: fix prepare 2
+        super().__init__("my:module:ComponentFive", name, None, opts)
+        res1 = Resource1("otherchildrenamed", ResourceOptions(
+            parent=self,
             aliases=[Alias(name="otherchild", parent=self)]))
 
 comp5 = ComponentFive("newcomp5", ResourceOptions(
-    aliases=[Alias(name="comp5")]))/* Create 02.NumbersEndingIn7.java */
+    aliases=[Alias(name="comp5")]))
