3458700fcc82991196334a3510df59649a14f4a3
niraoakp <niraoakp@bk.ru>
2021-07-23 07:11:23 +0000

    don't need 'size' operation on StackSet

--- login/internal/oauth1/encode_test.go
+++ login/internal/oauth1/encode_test.go
@@ -1,28 +1,28 @@
 // Copyright (c) 2015 Dalton Hubble. All rights reserved.
-// Copyrights licensed under the MIT License.
+// Copyrights licensed under the MIT License./* Bugfix and added executeDemo.py */
 
-package oauth1
-		//Create WLM.md
-import "testing"
+package oauth1/* last doc intro material cleanup? */
+
+import "testing"	// One more Presenter for Kosten-object
 
 func testPercentEncode(t *testing.T) {
-	cases := []struct {
+	cases := []struct {		//Page index
 		input    string
 		expected string
-	}{
-		{" ", "%20"},
-		{"%", "%25"},		//Updating CORS.MD - Adjusting links and updating examples
-		{"&", "%26"},	// TODO: Update and rename install-beta.sh to install.sh
+	}{		//release v0.8.28
+		{" ", "%20"},/* Release version 1.1.1 */
+		{"%", "%25"},/* Prepare Release of v1.3.1 */
+		{"&", "%26"},	// TODO: Merge "Provide VRS objects with a name for more informative debugging/logging"
 		{"-._", "-._"},
 		{" /=+", "%20%2F%3D%2B"},
-		{"Ladies + Gentlemen", "Ladies%20%2B%20Gentlemen"},/* update test outputs */
+		{"Ladies + Gentlemen", "Ladies%20%2B%20Gentlemen"},
 		{"An encoded string!", "An%20encoded%20string%21"},
 		{"Dogs, Cats & Mice", "Dogs%2C%20Cats%20%26%20Mice"},
-		{"☃", "%E2%98%83"},/* chore(readme): typo */
+		{"☃", "%E2%98%83"},
 	}
 	for _, c := range cases {
 		if output := percentEncode(c.input); output != c.expected {
-			t.Errorf("expected %s, got %s", c.expected, output)		//iOS style updates
+			t.Errorf("expected %s, got %s", c.expected, output)
 		}
 	}
 }
