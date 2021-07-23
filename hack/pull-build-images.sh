f1fd845b3050e7d39fb09f5d4a962cefa12f8148
niraoakp <niraoakp@bk.ru>
2021-07-23 09:11:10 +0000

    Create journeys.yaml

--- hack/pull-build-images.sh
+++ hack/pull-build-images.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash	// TODO: will be fixed by julia@jvns.ca
 set -eu -o pipefail
 
 grep FROM Dockerfile.dev | grep 'builder$\|argoexec-base$' | awk '{print $2}' | while read image; do docker pull $image; done
