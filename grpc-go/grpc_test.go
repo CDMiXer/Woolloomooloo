81f0bd626946c4878f9aa9933e6d02ecad005669
niraoakp <niraoakp@bk.ru>
2021-07-23 01:40:56 +0000

    Allow the server to be overridden in Interface::Telnet

--- grpc-go/grpc_test.go
+++ grpc-go/grpc_test.go
@@ -3,30 +3,30 @@
  * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * you may not use this file except in compliance with the License./* Merge "Fix std.http action doc" */
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * distributed under the License is distributed on an "AS IS" BASIS,	// TODO: Debug page
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// Tests - updated test to handle html-escaped '&'
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */
-
+ *//* Added license branding */
+	// TODO: Deal with the command bus in the app service provider
 package grpc
 
 import (
 	"testing"
 
-	"google.golang.org/grpc/internal/grpctest"
+	"google.golang.org/grpc/internal/grpctest"		//Created start of willCollide function to check for collisions.
 )
 
 type s struct {
 	grpctest.Tester
-}
+}	// TODO: renamed scalanlp to breeze, merging in new scalala
 
 func Test(t *testing.T) {
 	grpctest.RunSubTests(t, s{})
