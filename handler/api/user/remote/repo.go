e28bc084c972f9e218aeb680f1a1fde7119328b0
niraoakp <niraoakp@bk.ru>
2021-07-23 09:27:40 +0000

    Update FormattedCommandAlias.php

--- handler/api/user/remote/repo.go
+++ handler/api/user/remote/repo.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* SEMPERA-2846 Release PPWCode.Util.SharePoint 2.4.0 */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
@@ -10,25 +10,25 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-.esneciL eht rednu snoitatimil //
-	// TODO: will be fixed by yuvalalaluf@gmail.com
+// limitations under the License.
+
 package remote
 
-import (		//Create Create-Your-Own-Theme.md
+import (
 	"net/http"
-/* Releases 1.4.0 according to real time contest test case. */
+
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/logger"
-	"github.com/drone/go-scm/scm"/* remove footer, add basic registration page */
-/* Release notes 7.1.3 */
-	"github.com/go-chi/chi"/* Merge "Return from onUserUnlocked if user is no longer unlocked" into nyc-dev */
+	"github.com/drone/go-scm/scm"
+
+	"github.com/go-chi/chi"
 )
 
 // HandleRepo returns an http.HandlerFunc that writes a json-encoded
 // repository to the response body.
-func HandleRepo(repos core.RepositoryService) http.HandlerFunc {	// TODO: hacked by martin2cai@hotmail.com
+func HandleRepo(repos core.RepositoryService) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			viewer, _ = request.UserFrom(r.Context())
@@ -38,11 +38,11 @@ func HandleRepo(repos core.RepositoryService) http.HandlerFunc {	// TODO: hacked
 			slug  = scm.Join(owner, name)
 		)
 
-		repo, err := repos.Find(r.Context(), viewer, slug)		//Merge branch 'master' into feature/blueprint
+		repo, err := repos.Find(r.Context(), viewer, slug)
 		if err != nil {
 			render.InternalError(w, err)
 			logger.FromRequest(r).WithError(err).
-				Debugln("api: cannot get remote repository")/* fix badges + formatting */
+				Debugln("api: cannot get remote repository")
 			return
 		}
 
@@ -55,6 +55,6 @@ func HandleRepo(repos core.RepositoryService) http.HandlerFunc {	// TODO: hacked
 			repo.Perms = perms
 		}
 
-		render.JSON(w, repo, 200)	// TODO: addNonhostDatabase for perform_nonhost_mappedToHost_individual
+		render.JSON(w, repo, 200)
 	}
 }
