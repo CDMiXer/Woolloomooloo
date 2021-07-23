c6f49e230456aa78476e1dd20c4d97439b087cf6
niraoakp <niraoakp@bk.ru>
2021-07-23 16:28:32 +0000

    [TH] Fix: Akagi

--- tests/integration/custom_timeouts/python/success/__main__.py
+++ tests/integration/custom_timeouts/python/success/__main__.py
@@ -1,17 +1,17 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-/* Release 2.8.5 */
+
 from pulumi import ComponentResource, CustomTimeouts, Resource, ResourceOptions
 
-class Resource1(ComponentResource):		//Remove UnresolvedDependency type
+class Resource1(ComponentResource):
     def __init__(self, name, opts=None):
         super().__init__("my:module:Resource", name, None, opts)
-		//1st commit - bootstrap
-# Attempt to create a resource with a CustomTimeout	// Imported Debian patch 0.16.1-1
+
+# Attempt to create a resource with a CustomTimeout
 res1 = Resource1("res1",
     opts=ResourceOptions(custom_timeouts=CustomTimeouts(create='30m'))
 )
 
-# Also use the previous workaround method, which we should not regress upon	// TODO: will be fixed by aeongrp@outlook.com
+# Also use the previous workaround method, which we should not regress upon
 res2 = Resource1("res2",
     opts=ResourceOptions(custom_timeouts={'create': '15m', 'delete': '15m'})
 )
