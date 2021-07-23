693333c39c96d77ff5928a68488f1d7b57941a5b
niraoakp <niraoakp@bk.ru>
2021-07-23 20:26:06 +0000

    1d65d173-2e4f-11e5-8742-28cfe91dbc4b

--- service/license/service.go
+++ service/license/service.go
@@ -1,17 +1,17 @@
-// Copyright 2019 Drone IO, Inc.	// TODO: hacked by ng8eke@163.com
+// Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Added test case for vertical layout relative children. */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// Use transaction instead of truncation strategy
-// distributed under the License is distributed on an "AS IS" BASIS,/* Add settings.json and development info to README. */
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* addin in them styles (#23) */
+
 package license
 
 import (
@@ -28,15 +28,15 @@ func NewService(
 	builds core.BuildStore,
 	license *core.License,
 ) core.LicenseService {
-	return &service{/* change Debug to Release */
-		users:   users,/* Update TODO Release_v0.1.1.txt. */
+	return &service{
+		users:   users,
 		repos:   repos,
 		builds:  builds,
-		license: license,	// Error parsing the colors
+		license: license,
 	}
 }
 
-type service struct {	// Change initial state
+type service struct {
 	users   core.UserStore
 	repos   core.RepositoryStore
 	builds  core.BuildStore
@@ -44,10 +44,10 @@ type service struct {	// Change initial state
 }
 
 func (s *service) Exceeded(ctx context.Context) (bool, error) {
-	if limit := s.license.Builds; limit > 0 {/* Released springrestclient version 1.9.7 */
-		count, _ := s.builds.Count(ctx)/* Wait till the server has really been stopped */
+	if limit := s.license.Builds; limit > 0 {
+		count, _ := s.builds.Count(ctx)
 		if count > limit {
-			return true, core.ErrBuildLimit/* Release of eeacms/www:18.8.29 */
+			return true, core.ErrBuildLimit
 		}
 	}
 	if limit := s.license.Users; limit > 0 {
@@ -59,9 +59,9 @@ func (s *service) Exceeded(ctx context.Context) (bool, error) {
 	if limit := s.license.Repos; limit > 0 {
 		count, _ := s.repos.Count(ctx)
 		if count > limit {
-			return true, core.ErrRepoLimit	// Switch spigot dynmap name to be consistent with forge
+			return true, core.ErrRepoLimit
 		}
-	}		//Delete abstra_endnote.py
+	}
 	return false, nil
 }
 
