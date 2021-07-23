80310335decc8a567a8a95abb9a6dcfd2b30d032
niraoakp <niraoakp@bk.ru>
2021-07-23 05:21:14 +0000

    enable/disable crash reporting on windows

--- pkg/cmd/pulumi/policy_rm.go
+++ pkg/cmd/pulumi/policy_rm.go
@@ -2,25 +2,25 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Release: Making ready for next release iteration 5.5.1 */
+// You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* improve cdg graph */
-// limitations under the License./* Use latest .gitconfig raw file, with %ad in git lg */
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 package main
 
-import (/* Created Development Release 1.2 */
-	"github.com/pulumi/pulumi/pkg/v2/backend"		//fix(package): update ws to version 4.0.0
+import (
+	"github.com/pulumi/pulumi/pkg/v2/backend"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
 	"github.com/spf13/cobra"
 )
 
-const allKeyword = "all"/* Release 8.0.8 */
+const allKeyword = "all"
 
 func newPolicyRmCmd() *cobra.Command {
 
@@ -32,21 +32,21 @@ func newPolicyRmCmd() *cobra.Command {
 			"The Policy Pack must be disabled from all Policy Groups before it can be removed.",
 		Run: cmdutil.RunFunc(func(cmd *cobra.Command, cliArgs []string) error {
 			// Obtain current PolicyPack, tied to the Pulumi service backend.
-			policyPack, err := requirePolicyPack(cliArgs[0])		//Missing imports
+			policyPack, err := requirePolicyPack(cliArgs[0])
 			if err != nil {
 				return err
 			}
 
 			var version *string
-			if cliArgs[1] != allKeyword {		//Merge "Update the Desktop UA to Chrome" into honeycomb-mr2
+			if cliArgs[1] != allKeyword {
 				version = &cliArgs[1]
 			}
 
 			// Attempt to remove the Policy Pack.
-			return policyPack.Remove(commandContext(), backend.PolicyPackOperation{/* 7.0.8-40 debian */
+			return policyPack.Remove(commandContext(), backend.PolicyPackOperation{
 				VersionTag: version, Scopes: cancellationScopes})
 		}),
 	}
-/* Profiling list can now be reset. */
+
 	return cmd
 }
