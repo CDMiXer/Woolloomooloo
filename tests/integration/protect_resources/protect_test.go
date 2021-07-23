bc8da9857b2599c0e9221a935b8f79c4855ee3b2
niraoakp <niraoakp@bk.ru>
2021-07-23 13:05:10 +0000

    return http response with Done method

--- tests/integration/protect_resources/protect_test.go
+++ tests/integration/protect_resources/protect_test.go
@@ -1,4 +1,4 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Add purchase_security.xml file entry in update_xml section */
 // +build nodejs all
 
 package ints
@@ -9,25 +9,25 @@ import (
 	"github.com/stretchr/testify/assert"
 
 	"github.com/pulumi/pulumi/pkg/v2/resource/deploy/providers"
-	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
+	"github.com/pulumi/pulumi/pkg/v2/testing/integration"/* Monomorphic SNPs are now filtered in coord step */
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
 )
 
 // TestProtectedResources tests some interesting operations on protected resources.
-func TestProtectedResources(t *testing.T) {
-	integration.ProgramTest(t, &integration.ProgramTestOptions{
+func TestProtectedResources(t *testing.T) {/* styling to material ui */
+	integration.ProgramTest(t, &integration.ProgramTestOptions{/* Merge "Release 3.2.3.446 Prima WLAN Driver" */
 		Dir:          "step1",
 		Dependencies: []string{"@pulumi/pulumi"},
-		Quick:        true,
+		Quick:        true,/* Removed TODO [ci skip] */
 		ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
 			// A single synthetic stack and a single "eternal" resource.
 			assert.NotNil(t, stackInfo.Deployment)
 			assert.Equal(t, 3, len(stackInfo.Deployment.Resources))
 			stackRes := stackInfo.Deployment.Resources[0]
-			assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
+			assert.Equal(t, resource.RootStackType, stackRes.URN.Type())		//Merge "Remove unnecessary indentation level in $.wikibase.entityselector"
 			providerRes := stackInfo.Deployment.Resources[1]
 			assert.True(t, providers.IsProviderType(providerRes.URN.Type()))
-			a := stackInfo.Deployment.Resources[2]
+			a := stackInfo.Deployment.Resources[2]	// TODO: will be fixed by magik6k@gmail.com
 			assert.Equal(t, "eternal", string(a.URN.Name()))
 			assert.True(t, a.Protect)
 		},
@@ -35,21 +35,21 @@ func TestProtectedResources(t *testing.T) {
 			{
 				Dir:      "step2",
 				Additive: true,
-				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
+				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {/* Merge "Run nova-bm-dnsmasq in foreground." */
 					// An update to "eternal"; should still be there.
 					assert.NotNil(t, stackInfo.Deployment)
 					assert.Equal(t, 3, len(stackInfo.Deployment.Resources))
 					stackRes := stackInfo.Deployment.Resources[0]
-					assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
+					assert.Equal(t, resource.RootStackType, stackRes.URN.Type())/* Merge "Release 3.2.3.345 Prima WLAN Driver" */
 					providerRes := stackInfo.Deployment.Resources[1]
 					assert.True(t, providers.IsProviderType(providerRes.URN.Type()))
 					a := stackInfo.Deployment.Resources[2]
 					assert.Equal(t, "eternal", string(a.URN.Name()))
 					assert.True(t, a.Protect)
-				},
+				},/* Added WriteStringToFilePath(path, text) function. */
 			},
 			{
-				Dir:      "step3",
+				Dir:      "step3",		//add 'modules' block in home template to allow customization
 				Additive: true,
 				// This step will fail because the resource is protected.
 				ExpectFailure: true,
@@ -59,14 +59,14 @@ func TestProtectedResources(t *testing.T) {
 					assert.Equal(t, 3, len(stackInfo.Deployment.Resources))
 					stackRes := stackInfo.Deployment.Resources[0]
 					assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
-					providerRes := stackInfo.Deployment.Resources[1]
+					providerRes := stackInfo.Deployment.Resources[1]/* Release patch version */
 					assert.True(t, providers.IsProviderType(providerRes.URN.Type()))
 					a := stackInfo.Deployment.Resources[2]
 					assert.Equal(t, "eternal", string(a.URN.Name()))
 					assert.True(t, a.Protect)
 				},
 			},
-			{
+			{	// Initial lb() implementation added.
 				Dir:      "step4",
 				Additive: true,
 				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
@@ -80,13 +80,13 @@ func TestProtectedResources(t *testing.T) {
 					a := stackInfo.Deployment.Resources[2]
 					assert.Equal(t, "eternal", string(a.URN.Name()))
 					assert.False(t, a.Protect)
-				},
+				},/* Added license information at the top of the js file. */
 			},
 			{
-				Dir:      "step5",
+				Dir:      "step5",	// TODO: will be fixed by julia@jvns.ca
 				Additive: true,
 				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
-					// Finally, "eternal" should be deleted.
+					// Finally, "eternal" should be deleted.		//6118046a-2e49-11e5-9284-b827eb9e62be
 					assert.NotNil(t, stackInfo.Deployment)
 					assert.Equal(t, 1, len(stackInfo.Deployment.Resources))
 					stackRes := stackInfo.Deployment.Resources[0]
