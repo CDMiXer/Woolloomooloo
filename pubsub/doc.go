a75b08c8c98b3064d1fe18bf9ad87e153f570a17
niraoakp <niraoakp@bk.ru>
2021-07-23 11:49:36 +0000

    Auditing of successful actions

--- pubsub/doc.go
+++ pubsub/doc.go
@@ -4,7 +4,7 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0/* Release for 1.36.0 */
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,4 +15,4 @@
 // Package pubsub implements a thread safe publish subscriber,
 // allowing multiple publishers to distribute messages to
 // multiple subscribers.
-package pubsub
+package pubsub/* make update */
