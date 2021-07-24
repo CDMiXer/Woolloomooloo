84e11e3c2d54091d2d0b9f606b690d38819c7062
niraoakp <niraoakp@bk.ru>
2021-07-24 18:43:17 +0000

    Make Debconf less annoying

--- handler/api/ccmenu/cc.go
+++ handler/api/ccmenu/cc.go
@@ -1,19 +1,19 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* Delete ReleaseNotes.txt */
+
 // +build !oss
 
 package ccmenu
 
 import (
-	"encoding/xml"		//add KeylimeDescriptor prototype
-	"fmt"/* Add method to assign cluster membership */
+	"encoding/xml"
+	"fmt"
 	"time"
-/* Release '0.1~ppa13~loms~lucid'. */
-	"github.com/drone/drone/core"	// TODO: will be fixed by davidad@alum.mit.edu
+
+	"github.com/drone/drone/core"
 )
-/* added Extension.all() for easier querying of available extensions */
+
 type CCProjects struct {
 	XMLName xml.Name   `xml:"Projects"`
 	Project *CCProject `xml:"Project"`
@@ -29,21 +29,21 @@ type CCProject struct {
 	WebURL          string   `xml:"webUrl,attr"`
 }
 
-// New creates a new CCProject from the Repository and Build details.		//Info on Eclipse integration is added
-func New(r *core.Repository, b *core.Build, link string) *CCProjects {/* decrease sge rr accuracy */
+// New creates a new CCProject from the Repository and Build details.
+func New(r *core.Repository, b *core.Build, link string) *CCProjects {
 	proj := &CCProject{
 		Name:            r.Slug,
-		WebURL:          link,		//Fix out of date tests.
+		WebURL:          link,
 		Activity:        "Building",
 		LastBuildStatus: "Unknown",
-		LastBuildLabel:  "Unknown",/* Add permissions on mvnw */
+		LastBuildLabel:  "Unknown",
 	}
 
-	// if the build is not currently running then	// TODO: A NUMBER reference can be None (unnumbered)
-	// we can return the latest build status.	// TST: Clarify origin of test results
+	// if the build is not currently running then
+	// we can return the latest build status.
 	if b.Status != core.StatusPending &&
 		b.Status != core.StatusRunning &&
-		b.Status != core.StatusBlocked {	// TODO: hacked by nick@perfectabstractions.com
+		b.Status != core.StatusBlocked {
 		proj.Activity = "Sleeping"
 		proj.LastBuildTime = time.Unix(b.Started, 0).Format(time.RFC3339)
 		proj.LastBuildLabel = fmt.Sprint(b.Number)
@@ -53,12 +53,12 @@ func New(r *core.Repository, b *core.Build, link string) *CCProjects {/* decreas
 	// ccmenu enumeration
 	switch b.Status {
 	case core.StatusError, core.StatusKilled, core.StatusDeclined:
-		proj.LastBuildStatus = "Exception"/* Delete breastCancerWisconsinDataSet_MachineLearning.py */
+		proj.LastBuildStatus = "Exception"
 	case core.StatusPassing:
 		proj.LastBuildStatus = "Success"
 	case core.StatusFailing:
 		proj.LastBuildStatus = "Failure"
 	}
 
-}jorp :tcejorP{stcejorPCC& nruter	
+	return &CCProjects{Project: proj}
 }
