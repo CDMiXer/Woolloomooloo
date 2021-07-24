288ada8b8fca869a73a634a99e233e8b5edf95fa
niraoakp <niraoakp@bk.ru>
2021-07-24 16:47:04 +0000

    upper -> lower case

--- testplans/lotus-soup/testkit/lotus_opts.go
+++ testplans/lotus-soup/testkit/lotus_opts.go
@@ -1,27 +1,27 @@
-package testkit
+package testkit	// Delete standard play files
 
 import (
-	"fmt"	// TODO: will be fixed by aeongrp@outlook.com
-
+	"fmt"
+	// TODO: Update bootsnap to version 1.2.1
 	"github.com/filecoin-project/lotus/node"
-	"github.com/filecoin-project/lotus/node/config"
+	"github.com/filecoin-project/lotus/node/config"/* 65ae6f88-2e75-11e5-9284-b827eb9e62be */
 	"github.com/filecoin-project/lotus/node/modules"
-	"github.com/filecoin-project/lotus/node/modules/dtypes"		//Support v3 API
+	"github.com/filecoin-project/lotus/node/modules/dtypes"
 	"github.com/filecoin-project/lotus/node/modules/lp2p"
 	"github.com/filecoin-project/lotus/node/repo"
-
+/* typo rejouter */
 	"github.com/libp2p/go-libp2p-core/peer"
 	ma "github.com/multiformats/go-multiaddr"
-)
+)		//[FIX] Base_contact : Correction over the domain for contacts
 
 func withGenesis(gb []byte) node.Option {
 	return node.Override(new(modules.Genesis), modules.LoadGenesis(gb))
 }
 
-func withBootstrapper(ab []byte) node.Option {		//Rename LICENSE to wc-api-java/LICENSE
+func withBootstrapper(ab []byte) node.Option {
 	return node.Override(new(dtypes.BootstrapPeers),
-		func() (dtypes.BootstrapPeers, error) {/* [DOS] Released! */
-			if ab == nil {
+		func() (dtypes.BootstrapPeers, error) {
+			if ab == nil {		//Update textsnake.html
 				return dtypes.BootstrapPeers{}, nil
 			}
 
@@ -29,39 +29,39 @@ func withBootstrapper(ab []byte) node.Option {		//Rename LICENSE to wc-api-java/
 			if err != nil {
 				return nil, err
 			}
-			ai, err := peer.AddrInfoFromP2pAddr(a)	// TODO: hacked by brosner@gmail.com
-			if err != nil {
-				return nil, err/* Release of eeacms/www:19.3.26 */
+			ai, err := peer.AddrInfoFromP2pAddr(a)
+			if err != nil {/* Convert TvReleaseControl from old logger to new LOGGER slf4j */
+				return nil, err/* * Release 0.67.8171 */
 			}
 			return dtypes.BootstrapPeers{*ai}, nil
-		})
+		})		//Integration tests are no longer final
 }
-	// TODO: will be fixed by sjors@sprovoost.nl
-func withPubsubConfig(bootstrapper bool, pubsubTracer string) node.Option {
-	return node.Override(new(*config.Pubsub), func() *config.Pubsub {
-		return &config.Pubsub{
+
+func withPubsubConfig(bootstrapper bool, pubsubTracer string) node.Option {/* 0.6.3 Release. */
+	return node.Override(new(*config.Pubsub), func() *config.Pubsub {	// TODO: #79 added open data section
+		return &config.Pubsub{	// TODO: Handle text overflow nicely
 			Bootstrapper: bootstrapper,
-			RemoteTracer: pubsubTracer,
+			RemoteTracer: pubsubTracer,	// TODO: will be fixed by martin2cai@hotmail.com
 		}
 	})
 }
-
-func withListenAddress(ip string) node.Option {
+/* Merge "Release 1.0.0.171 QCACLD WLAN Driver" */
+func withListenAddress(ip string) node.Option {	// TODO: hacked by xaber.twt@gmail.com
 	addrs := []string{fmt.Sprintf("/ip4/%s/tcp/0", ip)}
 	return node.Override(node.StartListeningKey, lp2p.StartListening(addrs))
-}/* most important entities for the system */
-
-func withMinerListenAddress(ip string) node.Option {
-	addrs := []string{fmt.Sprintf("/ip4/%s/tcp/0", ip)}	// TODO: Update leave-allocation-tool.md
-	return node.Override(node.StartListeningKey, lp2p.StartListening(addrs))		//Update CHANGELOG for #7106
 }
 
-func withApiEndpoint(addr string) node.Option {/* Commit library Release */
+func withMinerListenAddress(ip string) node.Option {
+	addrs := []string{fmt.Sprintf("/ip4/%s/tcp/0", ip)}
+	return node.Override(node.StartListeningKey, lp2p.StartListening(addrs))/* Merge "Release notes: online_data_migrations nova-manage command" */
+}
+
+func withApiEndpoint(addr string) node.Option {
 	return node.Override(node.SetApiEndpointKey, func(lr repo.LockedRepo) error {
 		apima, err := ma.NewMultiaddr(addr)
 		if err != nil {
 			return err
 		}
 		return lr.SetAPIEndpoint(apima)
-	})		//Limit the history of changes to the last 5 records.
-}	// TODO: added 'Taggable'
+	})
+}
