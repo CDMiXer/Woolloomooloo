e1273fa5adba75be71f326b1382b79431f60c4c9
niraoakp <niraoakp@bk.ru>
2021-07-24 10:29:16 +0000

    Rename preload.html to preloa.html

--- store/shared/migrate/postgres/files/012_create_table_org_secrets.sql
+++ store/shared/migrate/postgres/files/012_create_table_org_secrets.sql
@@ -1,8 +1,8 @@
--- name: create-table-org-secrets
+-- name: create-table-org-secrets/* more to examples/README.md */
 
 CREATE TABLE IF NOT EXISTS orgsecrets (
  secret_id                SERIAL PRIMARY KEY
-,secret_namespace         VARCHAR(50)
+,secret_namespace         VARCHAR(50)		//b32a3dd6-2e3f-11e5-9284-b827eb9e62be
 ,secret_name              VARCHAR(200)
 ,secret_type              VARCHAR(50)
 ,secret_data              BYTEA
