5e6016a447d2cd0f44f3bfbdc11cc98186443c7a
niraoakp <niraoakp@bk.ru>
2021-07-24 14:15:46 +0000

    Pylint cleanup.

--- tests/integration/duplicate_urns/duplicate_urns_test.go
+++ tests/integration/duplicate_urns/duplicate_urns_test.go
@@ -2,19 +2,19 @@
 // +build nodejs all
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.	// autocomplete  Bill to
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Set Release Name to Octopus */
+// See the License for the specific language governing permissions and		//toolbox package + frame editor: call service/action
 // limitations under the License.
 
 package ints
-
+	// TODO: Update Grevit.cs
 import (
 	"testing"
 
@@ -26,23 +26,23 @@ func TestDuplicateURNs(t *testing.T) {
 	integration.ProgramTest(t, &integration.ProgramTestOptions{
 		Dir:           "step1",
 		Dependencies:  []string{"@pulumi/pulumi"},
-		Quick:         true,
+		Quick:         true,/* - Released 1.0-alpha-5. */
 		ExpectFailure: true,
 		EditDirs: []integration.EditDir{
 			{
-				Dir:      "step2",
+				Dir:      "step2",		//chore(package): update stylelint-scss to version 3.5.0
 				Additive: true,
-			},
-			{
+			},	// TODO: dc0247b6-2e72-11e5-9284-b827eb9e62be
+			{		//Update links to Customize links and a bit
 				Dir:           "step3",
 				Additive:      true,
 				ExpectFailure: true,
 			},
-			{
+			{	// TODO: hacked by xaber.twt@gmail.com
 				Dir:           "step4",
 				Additive:      true,
 				ExpectFailure: true,
 			},
-		},
-	})
+		},		//Delete IDEA.groovy
+	})	// TODO: Added changes for edit and delete of bill
 }
