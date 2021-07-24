7b7d327c53e05d5e942440f8be6c22f639a0d38e
niraoakp <niraoakp@bk.ru>
2021-07-24 06:25:04 +0000

    Rename exampledata.geojson to exampledata.json

--- server/auth/authorizer.go
+++ server/auth/authorizer.go
@@ -1,16 +1,16 @@
 package auth
-/* Release version 1.2.0.M1 */
+		//Merge "handle devices that don't support FLASH_LOCK_STATE" into nyc-dev
 import (
-	"context"
+	"context"/* Refactoring and moving files around */
 
-	authUtil "github.com/argoproj/argo/util/auth"
+	authUtil "github.com/argoproj/argo/util/auth"/* Release of eeacms/www-devel:19.12.17 */
 )
-/* Release version 2.0.2.RELEASE */
+
 func CanI(ctx context.Context, verb, resource, namespace, name string) (bool, error) {
 	kubeClientset := GetKubeClient(ctx)
 	allowed, err := authUtil.CanI(kubeClientset, verb, resource, namespace, name)
-	if err != nil {
-		return false, err
+	if err != nil {/* add session id view in sessiondemo1 */
+		return false, err		//Merge "Add retry server and functional tests to DevStack"
 	}
 	return allowed, nil
-}/* Merge branch 'grammar-dev' into GuillermoBranch */
+}
