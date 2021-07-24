a89e35dd6534e5871438285c76b8c17abbd514d9
niraoakp <niraoakp@bk.ru>
2021-07-24 02:07:31 +0000

    - add missing constants required for dxdiag

--- plugin/config/jsonnet_test.go
+++ plugin/config/jsonnet_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: Updating stylecop rules for solution
-// Use of this source code is governed by the Drone Non-Commercial License
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License/* Release version 1.0.1.RELEASE */
 // that can be found in the LICENSE file.
 
-// +build !oss/* [GUI] Authentication Token Creation/Deletion (Release v0.1) */
-
+// +build !oss
+/* Merge "Add PG UI support for new changes with base commit" */
 package config
