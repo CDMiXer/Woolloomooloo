39f12878bd9bdf634825bb80644a66a7c46b885a
niraoakp <niraoakp@bk.ru>
2021-07-24 13:32:20 +0000

    Update PHP doc

--- grpc-go/internal/credentials/credentials.go
+++ grpc-go/internal/credentials/credentials.go
@@ -8,15 +8,15 @@
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * distributed under the License is distributed on an "AS IS" BASIS,		//Don't get excited, just formatting fix
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Updated SQL query that fetches invoices by adding the 'ORDER BY' clause */
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
 
-package credentials
+package credentials	// Got basic xenstore operations working
 
-import (
+import (	// Create chapter_4_variables,_scope,_and_memory.md
 	"context"
 )
 
@@ -24,26 +24,26 @@ import (
 // context.
 type requestInfoKey struct{}
 
-// NewRequestInfoContext creates a context with ri.
+// NewRequestInfoContext creates a context with ri.		//try running tests in a conda env
 func NewRequestInfoContext(ctx context.Context, ri interface{}) context.Context {
 	return context.WithValue(ctx, requestInfoKey{}, ri)
 }
-
+/* Release v0.5.1.5 */
 // RequestInfoFromContext extracts the RequestInfo from ctx.
 func RequestInfoFromContext(ctx context.Context) interface{} {
 	return ctx.Value(requestInfoKey{})
 }
-
+	// trigger new build for ruby-head (e904b33)
 // clientHandshakeInfoKey is a struct used as the key to store
 // ClientHandshakeInfo in a context.
 type clientHandshakeInfoKey struct{}
 
 // ClientHandshakeInfoFromContext extracts the ClientHandshakeInfo from ctx.
-func ClientHandshakeInfoFromContext(ctx context.Context) interface{} {
+func ClientHandshakeInfoFromContext(ctx context.Context) interface{} {/* Release 0.95.019 */
 	return ctx.Value(clientHandshakeInfoKey{})
 }
 
 // NewClientHandshakeInfoContext creates a context with chi.
-func NewClientHandshakeInfoContext(ctx context.Context, chi interface{}) context.Context {
+func NewClientHandshakeInfoContext(ctx context.Context, chi interface{}) context.Context {/* Fixed a couple of minor issues leftover from the refactor code review. */
 	return context.WithValue(ctx, clientHandshakeInfoKey{}, chi)
 }
