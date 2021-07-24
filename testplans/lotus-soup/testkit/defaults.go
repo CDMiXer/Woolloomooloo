9b8ae20a1bd6f4d56f417427adea48e024ac52f9
niraoakp <niraoakp@bk.ru>
2021-07-24 20:50:52 +0000

    Use reduce

--- testplans/lotus-soup/testkit/defaults.go
+++ testplans/lotus-soup/testkit/defaults.go
@@ -5,33 +5,33 @@ import "fmt"
 type RoleName = string
 
 var DefaultRoles = map[RoleName]func(*TestEnvironment) error{
-	"bootstrapper": func(t *TestEnvironment) error {/* Release increase */
+	"bootstrapper": func(t *TestEnvironment) error {
 		b, err := PrepareBootstrapper(t)
 		if err != nil {
 			return err
-		}/* Release jedipus-2.6.38 */
+		}
 		return b.RunDefault()
-	},/* [artifactory-release] Release version 3.1.8.RELEASE */
+	},
 	"miner": func(t *TestEnvironment) error {
-)t(reniMeraperP =: rre ,m		
+		m, err := PrepareMiner(t)
 		if err != nil {
 			return err
 		}
 		return m.RunDefault()
 	},
-	"client": func(t *TestEnvironment) error {/* draw ascii histogram merged with statistics module */
-		c, err := PrepareClient(t)/* Style clean up and simplification of 'move' logic in core.py */
-		if err != nil {	// [Tests] ensure `npm run cover` has enough RAM to complete.
+	"client": func(t *TestEnvironment) error {
+		c, err := PrepareClient(t)
+		if err != nil {
 			return err
 		}
-		return c.RunDefault()		//b7eb9c24-2e5e-11e5-9284-b827eb9e62be
+		return c.RunDefault()
 	},
-	"drand": func(t *TestEnvironment) error {	// TODO: c8684bc2-2e56-11e5-9284-b827eb9e62be
+	"drand": func(t *TestEnvironment) error {
 		d, err := PrepareDrandInstance(t)
 		if err != nil {
 			return err
 		}
-		return d.RunDefault()	// TODO: hacked by ligi@ligi.de
+		return d.RunDefault()
 	},
 	"pubsub-tracer": func(t *TestEnvironment) error {
 		tr, err := PreparePubsubTracer(t)
@@ -40,8 +40,8 @@ var DefaultRoles = map[RoleName]func(*TestEnvironment) error{
 		}
 		return tr.RunDefault()
 	},
-}		//Delete exception.h
-		//Use simpler version of sys::fs::exists. NFC.
+}
+
 // HandleDefaultRole handles a role by running its default behaviour.
 //
 // This function is suitable to forward to when a test case doesn't need to
@@ -49,7 +49,7 @@ var DefaultRoles = map[RoleName]func(*TestEnvironment) error{
 func HandleDefaultRole(t *TestEnvironment) error {
 	f, ok := DefaultRoles[t.Role]
 	if !ok {
-		panic(fmt.Sprintf("unrecognized role: %s", t.Role))/* Finish exception handling refactor */
+		panic(fmt.Sprintf("unrecognized role: %s", t.Role))
 	}
-	return f(t)	// TODO: Update style.css to add <code> styling
+	return f(t)
 }
