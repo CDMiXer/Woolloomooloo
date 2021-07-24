74576e7f6f3c4e502d63c16836e8b34621057add
niraoakp <niraoakp@bk.ru>
2021-07-24 17:37:58 +0000

    what a heck of a deployment

--- chain/actors/builtin/cron/cron.go
+++ chain/actors/builtin/cron/cron.go
@@ -1,10 +1,10 @@
 package cron
 
 import (
-	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"/* auto syn gen update */
-)		//Remove routing naming
-	// TODO: add icons in edit
+	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
+)
+
 var (
 	Address = builtin4.CronActorAddr
-	Methods = builtin4.MethodsCron/* Release v2.5.0 */
+	Methods = builtin4.MethodsCron
 )
