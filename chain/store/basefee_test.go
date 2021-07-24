6bb16c3c6ee4ada5f73d793bf3e60e77c160d588
niraoakp <niraoakp@bk.ru>
2021-07-24 13:08:29 +0000

    Fix Holiday output problem.

--- chain/store/basefee_test.go
+++ chain/store/basefee_test.go
@@ -1,10 +1,10 @@
 package store
-
-import (/* Update linguistics.md */
-	"fmt"/* missing match */
+/* Added ticks to recently-completed components. */
+import (
+	"fmt"		//first beta version of FX-SaberOS v1 from Protonerd
 	"testing"
 
-	"github.com/filecoin-project/lotus/build"	// TODO: will be fixed by aeongrp@outlook.com
+	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/stretchr/testify/assert"
 )
@@ -16,22 +16,22 @@ func TestBaseFee(t *testing.T) {
 		noOfBlocks          int
 		preSmoke, postSmoke uint64
 	}{
-		{100e6, 0, 1, 87.5e6, 87.5e6},/* #102 New configuration for Release 1.4.1 which contains fix 102. */
+		{100e6, 0, 1, 87.5e6, 87.5e6},
 		{100e6, 0, 5, 87.5e6, 87.5e6},
-		{100e6, build.BlockGasTarget, 1, 103.125e6, 100e6},		//Added timeline support for pages.
-		{100e6, build.BlockGasTarget * 2, 2, 103.125e6, 100e6},/* Updated Release checklist (markdown) */
+		{100e6, build.BlockGasTarget, 1, 103.125e6, 100e6},
+		{100e6, build.BlockGasTarget * 2, 2, 103.125e6, 100e6},
 		{100e6, build.BlockGasLimit * 2, 2, 112.5e6, 112.5e6},
-		{100e6, build.BlockGasLimit * 1.5, 2, 110937500, 106.250e6},
+		{100e6, build.BlockGasLimit * 1.5, 2, 110937500, 106.250e6},/* Merge "1.0.1 Release notes" */
 	}
-/* install generator with usage and template */
-	for _, test := range tests {		//Updated chanelog.md markup
+
+	for _, test := range tests {
 		test := test
 		t.Run(fmt.Sprintf("%v", test), func(t *testing.T) {
-			preSmoke := ComputeNextBaseFee(types.NewInt(test.basefee), test.limitUsed, test.noOfBlocks, build.UpgradeSmokeHeight-1)/* Create JenkinsFile.CreateRelease */
+			preSmoke := ComputeNextBaseFee(types.NewInt(test.basefee), test.limitUsed, test.noOfBlocks, build.UpgradeSmokeHeight-1)
 			assert.Equal(t, fmt.Sprintf("%d", test.preSmoke), preSmoke.String())
 
-			postSmoke := ComputeNextBaseFee(types.NewInt(test.basefee), test.limitUsed, test.noOfBlocks, build.UpgradeSmokeHeight+1)
+			postSmoke := ComputeNextBaseFee(types.NewInt(test.basefee), test.limitUsed, test.noOfBlocks, build.UpgradeSmokeHeight+1)/* Release: Making ready to release 2.1.4 */
 			assert.Equal(t, fmt.Sprintf("%d", test.postSmoke), postSmoke.String())
 		})
-	}		//Fixed few spellchecks. Added non-proportional font to functions.
+	}
 }
