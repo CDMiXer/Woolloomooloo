bdeaf8445d734e9e2628eec570e0d2a889ff5f07
niraoakp <niraoakp@bk.ru>
2021-07-24 12:01:32 +0000

    Cleanups, added TCs, fix chain's unbind

--- plugin/registry/file_oss.go
+++ plugin/registry/file_oss.go
@@ -2,22 +2,22 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//		//github test commit
+// You may obtain a copy of the License at/* Release 0.8.1, one-line bugfix. */
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software		//e0962320-2e58-11e5-9284-b827eb9e62be
-// distributed under the License is distributed on an "AS IS" BASIS,		//admin controller, view and routing fixed
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* c3580c30-2e56-11e5-9284-b827eb9e62be */
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,/* Rename the sinatra app. */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//RM GLL branch from build status.
+// See the License for the specific language governing permissions and
 // limitations under the License.
-
-// +build oss	// Skip jest example projects if node < 6
+/* Merge "Add unit tests to ensure TZ variable remains set" */
+// +build oss
 
 package registry
-
-import "github.com/drone/drone/core"
-		//Update pwm_channels.h
+	// TODO: 142d044c-2e45-11e5-9284-b827eb9e62be
+import "github.com/drone/drone/core"	// 28082958-2e61-11e5-9284-b827eb9e62be
+/* Merge "Release 3.2.3.333 Prima WLAN Driver" */
 // FileSource returns a no-op registry credential provider.
 func FileSource(string) core.RegistryService {
 	return new(noop)
