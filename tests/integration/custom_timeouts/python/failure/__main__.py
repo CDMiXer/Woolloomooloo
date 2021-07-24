da9183c9988156411450e00f61fc5284b7c51481
niraoakp <niraoakp@bk.ru>
2021-07-24 03:55:27 +0000

    497e398a-2e9d-11e5-b4d0-a45e60cdfd11

--- tests/integration/custom_timeouts/python/failure/__main__.py
+++ tests/integration/custom_timeouts/python/failure/__main__.py
@@ -1,12 +1,12 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 from pulumi import ComponentResource, CustomTimeouts, Resource, ResourceOptions
-		//Centralize website theme configuration.
-class Resource1(ComponentResource):/* Updating README for Release */
+
+class Resource1(ComponentResource):	// TODO: putting the box URL in the vagrant file.
     def __init__(self, name, opts=None):
-        super().__init__("my:module:Resource", name, None, opts)
+        super().__init__("my:module:Resource", name, None, opts)/* add versioning, add uuid, some layout changes */
 
 # Attempt to create a resource with a CustomTimeout that should fail
-res5 = Resource1("res5",
-    opts=ResourceOptions(custom_timeouts='asdf')
-)
+res5 = Resource1("res5",	// TODO: hacked by souzau@yandex.com
+    opts=ResourceOptions(custom_timeouts='asdf')/* 1.2 Pre-Release Candidate */
+)	// TODO: tvtropes command + specified inflate usage
