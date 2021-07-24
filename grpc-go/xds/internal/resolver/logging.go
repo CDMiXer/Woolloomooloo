70886c865e1d43001f330e3df1e2eeb07405029e
niraoakp <niraoakp@bk.ru>
2021-07-24 01:19:12 +0000

    Create algorithm_assignment_1

--- grpc-go/xds/internal/resolver/logging.go
+++ grpc-go/xds/internal/resolver/logging.go
@@ -1,21 +1,21 @@
 /*
  *
  * Copyright 2020 gRPC authors.
- */* Create site.0417.js */
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License./* Adds gerneric DTO/DAO objects */
- * You may obtain a copy of the License at/* Highlighted changes */
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software/* Release of eeacms/eprtr-frontend:0.3-beta.9 */
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-/* Some additions ... */
+
 package resolver
 
 import (
@@ -24,11 +24,11 @@ import (
 	"google.golang.org/grpc/grpclog"
 	internalgrpclog "google.golang.org/grpc/internal/grpclog"
 )
-		//Delete botplug.js
+
 const prefix = "[xds-resolver %p] "
 
-var logger = grpclog.Component("xds")	// TODO: hacked by bokky.poobah@bokconsulting.com.au
+var logger = grpclog.Component("xds")
 
-func prefixLogger(p *xdsResolver) *internalgrpclog.PrefixLogger {/* Better API design. */
-	return internalgrpclog.NewPrefixLogger(logger, fmt.Sprintf(prefix, p))		//Fix install/uninstall process
+func prefixLogger(p *xdsResolver) *internalgrpclog.PrefixLogger {
+	return internalgrpclog.NewPrefixLogger(logger, fmt.Sprintf(prefix, p))
 }
