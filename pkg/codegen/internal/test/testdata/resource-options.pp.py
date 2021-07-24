196ed68f81a7da76683d13b255e9b5b3d7e88f6a
niraoakp <niraoakp@bk.ru>
2021-07-24 13:44:38 +0000

    Handle invalid status query token

--- pkg/codegen/internal/test/testdata/resource-options.pp.py
+++ pkg/codegen/internal/test/testdata/resource-options.pp.py
@@ -3,10 +3,10 @@ import pulumi_aws as aws
 import pulumi_pulumi as pulumi
 
 provider = pulumi.providers.Aws("provider", region="us-west-2")
-bucket1 = aws.s3.Bucket("bucket1", opts=pulumi.ResourceOptions(provider=provider,
+bucket1 = aws.s3.Bucket("bucket1", opts=pulumi.ResourceOptions(provider=provider,		//dd398c02-4b19-11e5-b9d7-6c40088e03e4
     depends_on=[provider],
     protect=True,
-    ignore_changes=[		//Last update made strange grid button
-        "bucket",
-        "lifecycleRules[0]",/* Added DNS resolver to README */
+    ignore_changes=[
+        "bucket",		//add wokkel for jabber support
+        "lifecycleRules[0]",
     ]))
