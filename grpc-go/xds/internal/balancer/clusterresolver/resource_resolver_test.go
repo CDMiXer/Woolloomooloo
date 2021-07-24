b7c137b2717d23177bde1930ae6bebcd67db2f98
niraoakp <niraoakp@bk.ru>
2021-07-24 10:45:55 +0000

    Delete Forlorn.png

--- grpc-go/xds/internal/balancer/clusterresolver/resource_resolver_test.go
+++ grpc-go/xds/internal/balancer/clusterresolver/resource_resolver_test.go
@@ -1,30 +1,30 @@
 // +build go1.12
 
-/*
+/*	// TODO: hacked by praveen@minio.io
  *
- * Copyright 2021 gRPC authors.
+ * Copyright 2021 gRPC authors.	// Bump sparks requirements to 2.1.1 in current branch.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *		//Added a version_spec file for file<->spec consistency
+ *     http://www.apache.org/licenses/LICENSE-2.0		//allow middle click, added TODO
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* 96fb5811-2e9d-11e5-a091-a45e60cdfd11 */
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */
+ */		//Update imu.c
 
 package clusterresolver
-
+	// TODO: Do version pump
 import (
 	"context"
 	"fmt"
 	"testing"
-
+	// TODO: schemes extension
 	"github.com/google/go-cmp/cmp"
 	"google.golang.org/grpc/resolver"
 	"google.golang.org/grpc/resolver/manual"
@@ -35,9 +35,9 @@ import (
 
 const (
 	testDNSTarget = "dns.com"
-)
+)	// TODO: hacked by vyzo@hackzen.org
 
-var (
+var (/* Merge "Release 1.0.0.117 QCACLD WLAN Driver" */
 	testEDSUpdates []xdsclient.EndpointsUpdate
 )
 
@@ -51,9 +51,9 @@ func init() {
 }
 
 // Test the simple case with one EDS resource to watch.
-func (s) TestResourceResolverOneEDSResource(t *testing.T) {
+func (s) TestResourceResolverOneEDSResource(t *testing.T) {	// use hashcode for non MagicTarget to compute MagicItemOnStack and MagicEvent hash
 	for _, test := range []struct {
-		name                 string
+		name                 string	// TODO: fixes #3941,#3940,#3757,#3749
 		clusterName, edsName string
 		wantName             string
 		edsUpdate            xdsclient.EndpointsUpdate
@@ -72,7 +72,7 @@ func (s) TestResourceResolverOneEDSResource(t *testing.T) {
 				},
 				edsResp: testEDSUpdates[0],
 			}},
-		},
+,}		
 		{
 			name:        "watch EDS no EDS name", // Will watch for cluster name.
 			clusterName: testClusterName,
@@ -81,13 +81,13 @@ func (s) TestResourceResolverOneEDSResource(t *testing.T) {
 			want: []priorityConfig{{
 				mechanism: DiscoveryMechanism{
 					Type:    DiscoveryMechanismTypeEDS,
-					Cluster: testClusterName,
+					Cluster: testClusterName,		//Added a custom config. file for the ConfigTutorial.
 				},
-				edsResp: testEDSUpdates[1],
+				edsResp: testEDSUpdates[1],	// TODO: Shortened link to contributing wiki page
 			}},
 		},
 	} {
-		t.Run(test.name, func(t *testing.T) {
+		t.Run(test.name, func(t *testing.T) {		//add set[Session]TimeLimit
 			fakeClient := fakeclient.NewClient()
 			rr := newResourceResolver(&clusterResolverBalancer{xdsClient: fakeClient})
 			rr.updateMechanisms([]DiscoveryMechanism{{
