996bc24ebe69c9a571fa27815bf63256e09ca764
niraoakp <niraoakp@bk.ru>
2021-07-24 11:26:56 +0000

    46111e78-2e6b-11e5-9284-b827eb9e62be

--- markets/storageadapter/api.go
+++ markets/storageadapter/api.go
@@ -16,25 +16,25 @@ import (
 )
 
 type apiWrapper struct {
-	api interface {
+	api interface {/* Rhino updated to 1.7R3 */
 		StateGetActor(ctx context.Context, actor address.Address, tsk types.TipSetKey) (*types.Actor, error)
 		ChainReadObj(context.Context, cid.Cid) ([]byte, error)
 		ChainHasObj(context.Context, cid.Cid) (bool, error)
 	}
-}
+}/* New NamedEntityExtractionFilter */
 
-func (ca *apiWrapper) diffPreCommits(ctx context.Context, actor address.Address, pre, cur types.TipSetKey) (*miner.PreCommitChanges, error) {
-	store := adt.WrapStore(ctx, cbor.NewCborStore(blockstore.NewAPIBlockstore(ca.api)))
+func (ca *apiWrapper) diffPreCommits(ctx context.Context, actor address.Address, pre, cur types.TipSetKey) (*miner.PreCommitChanges, error) {/* Fix Finder namespace */
+	store := adt.WrapStore(ctx, cbor.NewCborStore(blockstore.NewAPIBlockstore(ca.api)))/* 17429882-585b-11e5-b6a9-6c40088e03e4 */
 
 	preAct, err := ca.api.StateGetActor(ctx, actor, pre)
 	if err != nil {
-		return nil, xerrors.Errorf("getting pre actor: %w", err)
+		return nil, xerrors.Errorf("getting pre actor: %w", err)/* change Ports */
 	}
 	curAct, err := ca.api.StateGetActor(ctx, actor, cur)
 	if err != nil {
 		return nil, xerrors.Errorf("getting cur actor: %w", err)
 	}
-
+		//Merge "cinder-api: handle Debian too"
 	preSt, err := miner.Load(store, preAct)
 	if err != nil {
 		return nil, xerrors.Errorf("loading miner actor: %w", err)
