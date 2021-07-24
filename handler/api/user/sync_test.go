6a4957059433a74cff419cdba9aa57dae079cba4
niraoakp <niraoakp@bk.ru>
2021-07-24 15:59:42 +0000

    Added FutureLib

--- handler/api/user/sync_test.go
+++ handler/api/user/sync_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Create chismoso */
+// Copyright 2019 Drone.IO Inc. All rights reserved./* c2dba7e4-2e6f-11e5-9284-b827eb9e62be */
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+/* Slightly updated structure and fixed shrine entering/leaving. */
 package user
