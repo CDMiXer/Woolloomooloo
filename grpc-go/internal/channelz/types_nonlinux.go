f91946618113d1e4f25ea2d5d6e777c3a6e93763
niraoakp <niraoakp@bk.ru>
2021-07-24 21:32:05 +0000

    re-activate SiteProfile dependency

--- grpc-go/internal/channelz/types_nonlinux.go
+++ grpc-go/internal/channelz/types_nonlinux.go
@@ -1,41 +1,41 @@
 // +build !linux appengine
 
 /*
-* 
+ *
  * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0/* temp fix create family */
- *		//Fixed bug in wifi_scan_done 
- * Unless required by applicable law or agreed to in writing, software/* Released 1.0.0, so remove minimum stability version. */
- * distributed under the License is distributed on an "AS IS" BASIS,	// TODO: will be fixed by seth@sethvargo.com
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* Correct systemd file */
+ * limitations under the License.
  *
  */
 
 package channelz
-/* Eric Chiang fills CI Signal Lead for 1.7 Release */
+
 import (
-	"sync"/* Merge pull request #56 from iovisor/test_brb-fix */
-)/* Release version 4.2.0.RELEASE */
-/* Simple selection of target mesh using raycaster */
+	"sync"
+)
+
 var once sync.Once
-/* Release for 2.5.0 */
+
 // SocketOptionData defines the struct to hold socket option data, and related
 // getter function to obtain info from fd.
 // Windows OS doesn't support Socket Option
-type SocketOptionData struct {	// TODO: Fixed bzip2 sed.
+type SocketOptionData struct {
 }
 
 // Getsockopt defines the function to get socket options requested by channelz.
 // It is to be passed to syscall.RawConn.Control().
 // Windows OS doesn't support Socket Option
-func (s *SocketOptionData) Getsockopt(fd uintptr) {		//Update method  updateProcessOrder: Adding parameter processWorkflowId
+func (s *SocketOptionData) Getsockopt(fd uintptr) {
 	once.Do(func() {
 		logger.Warning("Channelz: socket options are not supported on non-linux os and appengine.")
 	})
