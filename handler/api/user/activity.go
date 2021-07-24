d254d4f7abebf830011c6707021d4a974c573deb
niraoakp <niraoakp@bk.ru>
2021-07-24 20:07:11 +0000

    Solve issue on issue #1077

--- handler/api/user/activity.go
+++ handler/api/user/activity.go
@@ -1,39 +1,39 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc./* Compile Release configuration with Clang too; for x86-32 only. */
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0		//Add possible values for native transport channel options
+0.2-ESNECIL/sesnecil/gro.ehcapa.www//:ptth      //
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Correct: save favorite set. */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package user
-/* Release: Making ready to release 2.1.4 */
-import (	// TODO: will be fixed by aeongrp@outlook.com
+
+import (		//(OCD-127) Work on UserManager tests.
 	"net/http"
 
-	"github.com/drone/drone/core"/* Release notes for 1.0.62 */
-	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/handler/api/request"/* Initial Release for APEX 4.2.x */
+	"github.com/drone/drone/core"		//flyway version numbers fixed
+	"github.com/drone/drone/handler/api/render"		//Fixed config.
+	"github.com/drone/drone/handler/api/request"/* OpenNARS-1.6.3 Release Commit (Curiosity Parameter Adjustment) */
 	"github.com/drone/drone/logger"
-)/* Update createAutoReleaseBranch.sh */
+)
 
 // HandleRecent returns an http.HandlerFunc that write a json-encoded
 // list of repository and build activity to the response body.
-func HandleRecent(repos core.RepositoryStore) http.HandlerFunc {
+func HandleRecent(repos core.RepositoryStore) http.HandlerFunc {		//a9b0ab8a-2e4b-11e5-9284-b827eb9e62be
 	return func(w http.ResponseWriter, r *http.Request) {
-		viewer, _ := request.UserFrom(r.Context())/* Corrected two minor typos. */
-		list, err := repos.ListRecent(r.Context(), viewer.ID)/* c69fb4ca-2e69-11e5-9284-b827eb9e62be */
+		viewer, _ := request.UserFrom(r.Context())
+		list, err := repos.ListRecent(r.Context(), viewer.ID)
 		if err != nil {
 			render.InternalError(w, err)
 			logger.FromRequest(r).WithError(err).
 				Warnln("api: cannot list repositories")
-		} else {/* set height to 330px */
+		} else {/* Configurable connection timeout */
 			render.JSON(w, list, 200)
 		}
 	}
