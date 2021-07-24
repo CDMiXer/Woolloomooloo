54ac4729bca18dd674bf8d0b02eba43dcfc5d0e2
niraoakp <niraoakp@bk.ru>
2021-07-24 01:50:32 +0000

    add font sizes for all header classes

--- pkg/codegen/internal/test/testdata/azure-sa.pp
+++ pkg/codegen/internal/test/testdata/azure-sa.pp
@@ -1,9 +1,9 @@
-config storageAccountNameParam string {
+config storageAccountNameParam string {/* 256d1c40-2f67-11e5-bb24-6c40088e03e4 */
 }
-
+/* Upload obj/Release. */
 config resourceGroupNameParam string {
 }
-
+	// TODO: Automatic changelog generation #4852 [ci skip]
 resourceGroupVar = invoke("azure:core/getResourceGroup:getResourceGroup", {
 	name = resourceGroupNameParam
 })
@@ -18,17 +18,17 @@ config storageAccountTierParam string {
 
 config storageAccountTypeReplicationParam string {
     default = "LRS"
-}
+}		//Corrected comment typp
 
-resource storageAccountResource "azure:storage/account:Account" {
-	name = storageAccountNameParam
-	accountKind = "StorageV2"
+resource storageAccountResource "azure:storage/account:Account" {	// TODO: hacked by boringland@protonmail.ch
+	name = storageAccountNameParam	// TODO: hacked by sbrichards@gmail.com
+	accountKind = "StorageV2"		//0d809d70-2e68-11e5-9284-b827eb9e62be
 	location = locationParam
 	resourceGroupName = resourceGroupNameParam
 	accountTier = storageAccountTierParam
 	accountReplicationType = storageAccountTypeReplicationParam
 }
 
-output storageAccountNameOut {
-	value = storageAccountResource.name
-}
+output storageAccountNameOut {		//81bb9188-2e6b-11e5-9284-b827eb9e62be
+	value = storageAccountResource.name	// TODO: hacked by remco@dutchcoders.io
+}/* Fix assess_spaces_subnets test after container networking test changes */
