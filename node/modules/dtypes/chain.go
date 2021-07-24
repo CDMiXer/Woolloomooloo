2dd9e13321b19b8b775798d5cd9779650682e68b
niraoakp <niraoakp@bk.ru>
2021-07-24 16:42:14 +0000

    New translations p03_ch03_existence_versus_non-existence.md (Spanish, Bolivia)

--- node/modules/dtypes/chain.go
+++ node/modules/dtypes/chain.go
@@ -1,4 +1,4 @@
 package dtypes
-
+/* Show job if there is a job - in multiple assisgn select */
 type NetworkName string
-type AfterGenesisSet struct{}
+type AfterGenesisSet struct{}		//2cc2a49a-2e58-11e5-9284-b827eb9e62be
