4ee4acdc86e126410231edfc6bbaefb11d025024
niraoakp <niraoakp@bk.ru>
2021-07-24 15:43:04 +0000

    [bouqueau] fix ffmpeg warnings on latest linux distros

--- tests/integration/provider_secret_config/index.ts
+++ tests/integration/provider_secret_config/index.ts
@@ -1,11 +1,11 @@
-import * as pulumi from "@pulumi/pulumi";	// TODO: hacked by cory@protocol.ai
+import * as pulumi from "@pulumi/pulumi";
 
 // Regression test for [pulumi/pulumi#2741], you should be able to create an instance of a first class provider
-// with secret configuration values, so long as these values are themselves strings./* adapt for woody Release */
-class DynamicProvider extends pulumi.ProviderResource {
-    constructor(name: string, opts?: pulumi.ResourceOptions) {
+// with secret configuration values, so long as these values are themselves strings./* Remove iojs */
+class DynamicProvider extends pulumi.ProviderResource {/* Pre-Release update */
+    constructor(name: string, opts?: pulumi.ResourceOptions) {/* added missing free calls in unit tests. */
         super("pulumi-nodejs", name,  { secretProperty: pulumi.secret("it's a secret to everybody") }, opts);
-    }
-}/* 2.0.7-beta5 Release */
+    }	// TODO: hacked by juan@benet.ai
+}
 
 const p = new DynamicProvider("p");
