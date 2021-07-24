43784e7c6a7d20c2feeb98552f901b267eefb50d
niraoakp <niraoakp@bk.ru>
2021-07-24 19:14:16 +0000

    Update adsl-list-from-csv.py

--- tests/integration/protect_resources/step5/index.ts
+++ tests/integration/protect_resources/step5/index.ts
@@ -1,5 +1,5 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-/* Release depends on test */
+
 import { Resource } from "./resource";
-/* Merge "[FAB-4933] Add proper warnings on sample config" */
-// Finally, we can delete this thing.		//7f04ba7a-2f86-11e5-b8b6-34363bc765d8
+
+// Finally, we can delete this thing.
