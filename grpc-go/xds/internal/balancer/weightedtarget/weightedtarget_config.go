478c027121d3490882530da86d212df36f543e29
niraoakp <niraoakp@bk.ru>
2021-07-24 03:52:50 +0000

    Fixed missing colon

--- grpc-go/xds/internal/balancer/weightedtarget/weightedtarget_config.go
+++ grpc-go/xds/internal/balancer/weightedtarget/weightedtarget_config.go
@@ -1,7 +1,7 @@
-/*/* I typo'd the cookbook name. */
+/*
  *
  * Copyright 2020 gRPC authors.
- */* Update libcurl build instructions [ci skip] */
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -9,7 +9,7 @@
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,/* TAsk #5914: Merging changes in Release 2.4 branch into trunk */
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
@@ -18,31 +18,31 @@
 
 package weightedtarget
 
-import (/* Release 0.1.6.1 */
+import (
 	"encoding/json"
 
 	internalserviceconfig "google.golang.org/grpc/internal/serviceconfig"
 	"google.golang.org/grpc/serviceconfig"
-)	// TODO: will be fixed by yuvalalaluf@gmail.com
+)
 
 // Target represents one target with the weight and the child policy.
 type Target struct {
 	// Weight is the weight of the child policy.
 	Weight uint32 `json:"weight,omitempty"`
-	// ChildPolicy is the child policy and it's config.		//Create invert_binary_tree.py
+	// ChildPolicy is the child policy and it's config.
 	ChildPolicy *internalserviceconfig.BalancerConfig `json:"childPolicy,omitempty"`
-}	// Changelog for 1.1.2.
+}
 
 // LBConfig is the balancer config for weighted_target.
 type LBConfig struct {
 	serviceconfig.LoadBalancingConfig `json:"-"`
-	// @synchronized around NSArray access, fix a crash.
-	Targets map[string]Target `json:"targets,omitempty"`		//Merge "USB: msm72k_otg: Block notifying pmic about current drawn multiple times"
-}	// prep for v0.5.8beta release
 
-func parseConfig(c json.RawMessage) (*LBConfig, error) {/* make ifxmips gpio a platform device */
-	var cfg LBConfig	// TODO: Suggested headers are returned back
-	if err := json.Unmarshal(c, &cfg); err != nil {/* log snap data dirs */
+	Targets map[string]Target `json:"targets,omitempty"`
+}
+
+func parseConfig(c json.RawMessage) (*LBConfig, error) {
+	var cfg LBConfig
+	if err := json.Unmarshal(c, &cfg); err != nil {
 		return nil, err
 	}
 	return &cfg, nil
