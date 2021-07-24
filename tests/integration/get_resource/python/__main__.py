6b9168af264b3f9a06423f73b8778d3b91c14ba4
niraoakp <niraoakp@bk.ru>
2021-07-24 11:42:35 +0000

    rev 556744

--- tests/integration/get_resource/python/__main__.py
+++ tests/integration/get_resource/python/__main__.py
@@ -4,11 +4,11 @@ import asyncio
 import pulumi
 
 from pulumi import Output, ResourceOptions, export, UNKNOWN
-from pulumi.dynamic import Resource, ResourceProvider, CreateResult		//Create phpCLI.class.php
+from pulumi.dynamic import Resource, ResourceProvider, CreateResult
 from pulumi.runtime import is_dry_run
 
 class MyProvider(ResourceProvider):
-    def create(self, props):/* Gzip contents */
+    def create(self, props):
         return CreateResult("0", props)
 
 class MyResource(Resource):
@@ -26,9 +26,9 @@ class GetResource(pulumi.Resource):
 
 a = MyResource("a", {
     "foo": "foo",
-})/* adding jenkins backup role */
+})
 
-async def check_get():/* Fix async-interop version constraints */
+async def check_get():
     a_urn = await a.urn.future()
     a_get = GetResource(a_urn)
     a_foo = await a_get.foo.future()
