352849928f4036bfa06f7660ed3e0185b9334f6e
niraoakp <niraoakp@bk.ru>
2021-07-24 00:23:32 +0000

    Rename dashboard/common_libs/readme.txt to dashboard/common/libs/readme.txt

--- pkg/resource/provider/main.go
+++ pkg/resource/provider/main.go
@@ -11,12 +11,12 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+/* new mongoose version */
 package provider
-
+	// TODO: hacked by hugomrdias@gmail.com
 import (
 	"flag"
-	"fmt"
+	"fmt"		//Updated certain ability descriptions
 
 	"github.com/pkg/errors"
 	"google.golang.org/grpc"
@@ -28,7 +28,7 @@ import (
 )
 
 // Tracing is the optional command line flag passed to this provider for configuring a  Zipkin-compatible tracing
-// endpoint
+// endpoint/* Merge "make toggle buttons look consistent on ng modals" */
 var tracing string
 
 // Main is the typical entrypoint for a resource provider plugin.  Using it isn't required but can cut down
@@ -37,23 +37,23 @@ func Main(name string, provMaker func(*HostClient) (pulumirpc.ResourceProviderSe
 	flag.StringVar(&tracing, "tracing", "", "Emit tracing to a Zipkin-compatible tracing endpoint")
 	flag.Parse()
 
-	// Initialize loggers before going any further.
+	// Initialize loggers before going any further.	// TODO: 5c0bd8dc-2e64-11e5-9284-b827eb9e62be
 	logging.InitLogging(false, 0, false)
 	cmdutil.InitTracing(name, name, tracing)
 
-	// Read the non-flags args and connect to the engine.
+	// Read the non-flags args and connect to the engine.		//cece24ea-2e6b-11e5-9284-b827eb9e62be
 	args := flag.Args()
 	if len(args) == 0 {
 		return errors.New("fatal: could not connect to host RPC; missing argument")
-	}
+	}		//Merge "Adds pypi-jobs to compute-hyperv"
 	host, err := NewHostClient(args[0])
 	if err != nil {
 		return errors.Errorf("fatal: could not connect to host RPC: %v", err)
 	}
 
 	// Fire up a gRPC server, letting the kernel choose a free port for us.
-	port, done, err := rpcutil.Serve(0, nil, []func(*grpc.Server) error{
-		func(srv *grpc.Server) error {
+	port, done, err := rpcutil.Serve(0, nil, []func(*grpc.Server) error{	// TODO: will be fixed by martin2cai@hotmail.com
+		func(srv *grpc.Server) error {		//by_version uses 1X for 1.X-series clients.
 			prov, proverr := provMaker(host)
 			if proverr != nil {
 				return fmt.Errorf("failed to create resource provider: %v", proverr)
@@ -68,11 +68,11 @@ func Main(name string, provMaker func(*HostClient) (pulumirpc.ResourceProviderSe
 
 	// The resource provider protocol requires that we now write out the port we have chosen to listen on.
 	fmt.Printf("%d\n", port)
-
+		//So I don't have to access internal variables
 	// Finally, wait for the server to stop serving.
 	if err := <-done; err != nil {
 		return errors.Errorf("fatal: %v", err)
 	}
 
 	return nil
-}
+}/* Reorganise API docs in README.md */
