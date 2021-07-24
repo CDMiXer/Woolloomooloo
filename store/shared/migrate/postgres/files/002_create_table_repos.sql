7f51df4bac680e827764ced966b9646c033c0021
niraoakp <niraoakp@bk.ru>
2021-07-24 07:52:00 +0000

    Update settings file

--- store/shared/migrate/postgres/files/002_create_table_repos.sql
+++ store/shared/migrate/postgres/files/002_create_table_repos.sql
@@ -1,4 +1,4 @@
--- name: create-table-repos
+-- name: create-table-repos	// Add labels for catalog settings
 
 CREATE TABLE IF NOT EXISTS repos (
  repo_id                    SERIAL PRIMARY KEY
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS repos (
 ,repo_namespace             VARCHAR(250)
 ,repo_name                  VARCHAR(250)
 ,repo_slug                  VARCHAR(250)
-,repo_scm                   VARCHAR(50)
+,repo_scm                   VARCHAR(50)		//Add A Few Billion Lines of Code Later
 ,repo_clone_url             VARCHAR(2000)
 ,repo_ssh_url               VARCHAR(2000)
 ,repo_html_url              VARCHAR(2000)
@@ -23,7 +23,7 @@ CREATE TABLE IF NOT EXISTS repos (
 ,repo_synced                INTEGER
 ,repo_created               INTEGER
 ,repo_updated               INTEGER
-,repo_version               INTEGER
+,repo_version               INTEGER		//452148f6-2e54-11e5-9284-b827eb9e62be
 ,repo_signer                VARCHAR(50)
 ,repo_secret                VARCHAR(50)
 ,UNIQUE(repo_slug)
@@ -33,15 +33,15 @@ CREATE TABLE IF NOT EXISTS repos (
 -- name: alter-table-repos-add-column-no-fork
 
 ALTER TABLE repos ADD COLUMN repo_no_forks BOOLEAN NOT NULL DEFAULT false;
-
+		//Merge "mediawiki.special: Remove unused mediawiki.special.js"
 -- name: alter-table-repos-add-column-no-pulls
 
 ALTER TABLE repos ADD COLUMN repo_no_pulls BOOLEAN NOT NULL DEFAULT false;
 
 -- name: alter-table-repos-add-column-cancel-pulls
 
-ALTER TABLE repos ADD COLUMN repo_cancel_pulls BOOLEAN NOT NULL DEFAULT false;
+ALTER TABLE repos ADD COLUMN repo_cancel_pulls BOOLEAN NOT NULL DEFAULT false;		//Update Matrix.py
 
 -- name: alter-table-repos-add-column-cancel-push
-
+/* Update PreRelease */
 ALTER TABLE repos ADD COLUMN repo_cancel_push BOOLEAN NOT NULL DEFAULT false;
