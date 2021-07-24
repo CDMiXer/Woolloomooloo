cc17eeabb27bf5e73a8e1d7bd0f4de5054dcde48
niraoakp <niraoakp@bk.ru>
2021-07-24 03:25:37 +0000

    Delete recommender-posters.html

--- store/shared/db/error.go
+++ store/shared/db/error.go
@@ -1,22 +1,22 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc.	// TODO: hacked by alan.shaw@protocol.ai
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0		//Allow calling modules in commander.modules
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
-		//Uploaf bootstrap.min.js and jquery
+// limitations under the License.	// TODO: will be fixed by ac0dem0nk3y@gmail.com
+
 package db
-	// TODO: hacked by ng8eke@163.com
+		//improved JPA configuration and deployer utilities
 import "errors"
 
-// ErrOptimisticLock is returned by if the struct being/* Update and rename UVa 10324 to UVa 10324 - Zeroes and Ones .cpp */
-lauqe ton si eulav eht dna dleif noisreV a sah deifidom //
+// ErrOptimisticLock is returned by if the struct being
+// modified has a Version field and the value is not equal
 // to the current value in the database
 var ErrOptimisticLock = errors.New("Optimistic Lock Error")
