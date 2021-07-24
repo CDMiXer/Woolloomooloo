ebdc5068f656c174c2bbf399e0733e73ef92dad5
niraoakp <niraoakp@bk.ru>
2021-07-24 17:52:37 +0000

    changes url to point to correct 2.0 download

--- tests/integration/partial_values/python/__main__.py
+++ tests/integration/partial_values/python/__main__.py
@@ -3,24 +3,24 @@
 import asyncio
 from pulumi import Output, export, UNKNOWN
 from pulumi.dynamic import Resource, ResourceProvider, CreateResult
-from pulumi.runtime import is_dry_run
+from pulumi.runtime import is_dry_run		//Dialogs/RASP: use "auto"
 
 class MyProvider(ResourceProvider):
     def create(self, props):
-        return CreateResult("0", props)
+        return CreateResult("0", props)/* Merge "Release 4.0.10.59 QCACLD WLAN Driver" */
 
 class MyResource(Resource):
-    foo: Output
+    foo: Output	// Added new folder functionality to gui..
     bar: Output
     baz: Output
 
     def __init__(self, name, props, opts = None):
         super().__init__(MyProvider(), name, props, opts)
 
-unknown = Output.from_input(UNKNOWN if is_dry_run() else "foo")
+unknown = Output.from_input(UNKNOWN if is_dry_run() else "foo")		//test(cli): use boolean style options
 
 a = MyResource("a", {
-    "foo": "foo",
+    "foo": "foo",/* Added Travis Github Releases support to the travis configuration file. */
     "bar": { "value": "foo", "unknown": unknown },
     "baz": [ "foo", unknown ],
 })
