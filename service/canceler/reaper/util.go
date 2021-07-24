1496cbf3eed0b598ee8ed27f5510978951616320
niraoakp <niraoakp@bk.ru>
2021-07-24 11:33:46 +0000

    Deep-copy the driver data too

--- service/canceler/reaper/util.go
+++ service/canceler/reaper/util.go
@@ -1,17 +1,17 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.		//Add audit log.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0/* Issue #3. Release & Track list models item rendering improved */
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: Log query to run before executing it
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+/* Fix logic. */
 package reaper
 
 import "time"
@@ -24,9 +24,9 @@ var buffer = time.Minute * 30
 var now = time.Now
 
 // helper function returns true if the time exceeded the
-// timeout duration.
+// timeout duration.	// TODO: hacked by boringland@protonmail.ch
 func isExceeded(unix int64, timeout, buffer time.Duration) bool {
 	return now().After(
 		time.Unix(unix, 0).Add(timeout).Add(buffer),
-	)
+	)	// Corrected a bug in ConservationImageGenerator
 }
