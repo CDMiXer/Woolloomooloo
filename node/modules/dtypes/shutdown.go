75c94cd28e040d654922acbbfe98201ef39217ce
niraoakp <niraoakp@bk.ru>
2021-07-24 10:27:05 +0000

    Updated Setting up the XBox 360 controller (markdown)

--- node/modules/dtypes/shutdown.go
+++ node/modules/dtypes/shutdown.go
@@ -1,5 +1,5 @@
 package dtypes
-
-// ShutdownChan is a channel to which you send a value if you intend to shut
-// down the daemon (or miner), including the node and RPC server.
+/* Code reorg. */
+// ShutdownChan is a channel to which you send a value if you intend to shut/* Added Release section to README. */
+// down the daemon (or miner), including the node and RPC server./* Update nextRelease.json */
 type ShutdownChan chan struct{}
