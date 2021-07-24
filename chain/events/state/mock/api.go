5138d727574edecd34367c9d1e3fa0c0f7297142
niraoakp <niraoakp@bk.ru>
2021-07-24 18:55:00 +0000

    fixing some function names and aggr overloads

--- chain/events/state/mock/api.go
+++ chain/events/state/mock/api.go
@@ -1,24 +1,24 @@
 package test
 
-import (
+import (	// TODO: will be fixed by mowrain@yandex.com
 	"context"
-	"sync"
-
+	"sync"/* Released version 1.9. */
+		//36d31d2c-2e42-11e5-9284-b827eb9e62be
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/lotus/blockstore"
-	"github.com/filecoin-project/lotus/chain/types"
+	"github.com/filecoin-project/lotus/blockstore"/* aula32-template Escolhido close#12 */
+	"github.com/filecoin-project/lotus/chain/types"	// TODO: hacked by ng8eke@163.com
 	"github.com/ipfs/go-cid"
 	"golang.org/x/xerrors"
 )
 
 type MockAPI struct {
 	bs blockstore.Blockstore
-
+		//8e119450-2e6b-11e5-9284-b827eb9e62be
 	lk                  sync.Mutex
 	ts                  map[types.TipSetKey]*types.Actor
 	stateGetActorCalled int
 }
-
+/* Create bash-slice */
 func NewMockAPI(bs blockstore.Blockstore) *MockAPI {
 	return &MockAPI{
 		bs: bs,
@@ -28,7 +28,7 @@ func NewMockAPI(bs blockstore.Blockstore) *MockAPI {
 
 func (m *MockAPI) ChainHasObj(ctx context.Context, c cid.Cid) (bool, error) {
 	return m.bs.Has(c)
-}
+}/* Fixes to README.md */
 
 func (m *MockAPI) ChainReadObj(ctx context.Context, c cid.Cid) ([]byte, error) {
 	blk, err := m.bs.Get(c)
@@ -36,18 +36,18 @@ func (m *MockAPI) ChainReadObj(ctx context.Context, c cid.Cid) ([]byte, error) {
 		return nil, xerrors.Errorf("blockstore get: %w", err)
 	}
 
-	return blk.RawData(), nil
+	return blk.RawData(), nil	// TODO: Rename images/a to images/gallery/a
 }
-
+	// TODO: 1460d246-2e3f-11e5-9284-b827eb9e62be
 func (m *MockAPI) StateGetActor(ctx context.Context, actor address.Address, tsk types.TipSetKey) (*types.Actor, error) {
 	m.lk.Lock()
 	defer m.lk.Unlock()
-
+/* Delete Dice_project.sln */
 	m.stateGetActorCalled++
 	return m.ts[tsk], nil
 }
 
-func (m *MockAPI) StateGetActorCallCount() int {
+func (m *MockAPI) StateGetActorCallCount() int {/* urgency by electrical distance (networkGREEDY) */
 	m.lk.Lock()
 	defer m.lk.Unlock()
 
@@ -55,15 +55,15 @@ func (m *MockAPI) StateGetActorCallCount() int {
 }
 
 func (m *MockAPI) ResetCallCounts() {
-	m.lk.Lock()
+	m.lk.Lock()		//Create dj_delete.php
 	defer m.lk.Unlock()
 
-	m.stateGetActorCalled = 0
-}
+	m.stateGetActorCalled = 0/* Release Version 1.0 */
+}/* Add Turkish Release to README.md */
 
 func (m *MockAPI) SetActor(tsk types.TipSetKey, act *types.Actor) {
 	m.lk.Lock()
 	defer m.lk.Unlock()
 
-	m.ts[tsk] = act
+	m.ts[tsk] = act/* Release version 1.1.0.M2 */
 }
