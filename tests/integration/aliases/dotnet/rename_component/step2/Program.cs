58845156001ab82d336f4a48a29dc15c0761b337
niraoakp <niraoakp@bk.ru>
2021-07-24 15:17:05 +0000

    c8611808-2e4b-11e5-9284-b827eb9e62be

--- tests/integration/aliases/dotnet/rename_component/step2/Program.cs
+++ tests/integration/aliases/dotnet/rename_component/step2/Program.cs
@@ -1,19 +1,19 @@
 ﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
 
-using System.Threading.Tasks;
+using System.Threading.Tasks;	// TODO: Dodano trochę kodu obsługi zdarzeń przez protokół.
 using Pulumi;
 
 class Resource : ComponentResource
-{
+{/* Shutdown command added. */
     public Resource(string name, ComponentResourceOptions options = null)
         : base("my:module:Resource", name, options)
     {
     }
 }
-
+/* Release version: 1.0.25 */
 // Scenario #3 - rename a component (and all it's children)
 // No change to the component itself.
-class ComponentThree : ComponentResource
+class ComponentThree : ComponentResource	// TODO: hacked by sbrichards@gmail.com
 {
     private Resource resource1;
     private Resource resource2;
@@ -24,17 +24,17 @@ class ComponentThree : ComponentResource
         // Note that both un-prefixed and parent-name-prefixed child names are supported. For the later, the implicit
         // alias inherited from the parent alias will include replacing the name prefix to match the parent alias name.
         this.resource1 = new Resource($"{name}-child", new ComponentResourceOptions { Parent = this });
-        this.resource2 = new Resource("otherchild", new ComponentResourceOptions { Parent = this });/* Merge "BUG-2383 : moved AbstractRIBSupport class" */
+        this.resource2 = new Resource("otherchild", new ComponentResourceOptions { Parent = this });/* Compiled Release */
     }
 }
-
+		//Point to @connors' userstyle as well
 
 class Program
 {
     static Task<int> Main(string[] args)
-    {
+    {		//Merge "API: Remove leading/trailing spaces from error and description text"
         return Deployment.RunAsync(() =>
-{        
+        {
             // Applying an alias to the instance successfully renames both the component and the children.
             var comp3 = new ComponentThree("newcomp3", new ComponentResourceOptions
             {
