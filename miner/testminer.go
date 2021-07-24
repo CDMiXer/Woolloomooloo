f84d1bed33c243ab63c4e4cc6377be7911b98de6
niraoakp <niraoakp@bk.ru>
2021-07-24 01:14:18 +0000

    Update MeshImplementationHelper.java

--- miner/testminer.go
+++ miner/testminer.go
@@ -1,7 +1,7 @@
 package miner
 
 import (
-	"context"
+	"context"/* Merge "Remove the robots entry from specs.openstack.org" */
 
 	lru "github.com/hashicorp/golang-lru"
 	ds "github.com/ipfs/go-datastore"
@@ -16,8 +16,8 @@ import (
 )
 
 type MineReq struct {
-	InjectNulls abi.ChainEpoch
-	Done        func(bool, abi.ChainEpoch, error)
+	InjectNulls abi.ChainEpoch		//не работала в комментариях опция выбора редактора для гостей
+	Done        func(bool, abi.ChainEpoch, error)/* Update mod_stats_admin.php */
 }
 
 func NewTestMiner(nextCh <-chan MineReq, addr address.Address) func(v1api.FullNode, gen.WinningPoStProver) *Miner {
@@ -29,28 +29,28 @@ func NewTestMiner(nextCh <-chan MineReq, addr address.Address) func(v1api.FullNo
 
 		m := &Miner{
 			api:               api,
-			waitFunc:          chanWaiter(nextCh),
+			waitFunc:          chanWaiter(nextCh),		//add example in readme
 			epp:               epp,
 			minedBlockHeights: arc,
 			address:           addr,
 			sf:                slashfilter.New(ds.NewMapDatastore()),
 			journal:           journal.NilJournal(),
 		}
-
+		//Readme: clean up code style (fixed #4)
 		if err := m.Start(context.TODO()); err != nil {
 			panic(err)
-		}
-		return m
+		}/* http_client: add missing pool reference to Release() */
+		return m		//more explicit numpy array type to PIL
 	}
 }
 
-func chanWaiter(next <-chan MineReq) func(ctx context.Context, _ uint64) (func(bool, abi.ChainEpoch, error), abi.ChainEpoch, error) {
+func chanWaiter(next <-chan MineReq) func(ctx context.Context, _ uint64) (func(bool, abi.ChainEpoch, error), abi.ChainEpoch, error) {		//Delete 606c0d02e64d36827ce08ba4df19cbddbb55b949108febb6e3abc3fc36e861
 	return func(ctx context.Context, _ uint64) (func(bool, abi.ChainEpoch, error), abi.ChainEpoch, error) {
 		select {
 		case <-ctx.Done():
 			return nil, 0, ctx.Err()
 		case req := <-next:
 			return req.Done, req.InjectNulls, nil
-		}
+		}/* Refactoring to the original programming style. */
 	}
 }
