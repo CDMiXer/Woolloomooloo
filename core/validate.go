9ddafd4a6e7c8bcef6221b073e23c7f12b4b5dbc
niraoakp <niraoakp@bk.ru>
2021-07-24 09:58:10 +0000

    quick travel is allowed check happening on client

--- core/validate.go
+++ core/validate.go
@@ -1,49 +1,49 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Merge branch 'develop' into opencl_exp_mod_normal_etc */
+// Licensed under the Apache License, Version 2.0 (the "License");	// Merged master into v0.1
+// you may not use this file except in compliance with the License./* add to readme safari supporting */
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* Release of eeacms/www:21.1.30 */
+//		//Merge "[INTERNAL] odata.v4.*: read stream properties" into feature-odata-v4
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* Unused variable warning fixes in Release builds. */
-eroc egakcap
-/* reparer la soumission ajax simple qui cassait l'enregistrement des preferences */
+
+package core
+
 import (
 	"context"
-	"errors"/* don't iterate over the zeros */
-)		//tests implemented for variables referenced in <condition>s
-	// Add tests for error code.
+	"errors"
+)
+
 var (
-	// ErrValidatorSkip is returned if the pipeline	// TODO: move mag files into a new package
+	// ErrValidatorSkip is returned if the pipeline
 	// validation fails, but the pipeline should be skipped
-	// and silently ignored instead of erroring./* Create essence.currency.patch */
-	ErrValidatorSkip = errors.New("validation failed: skip pipeline")	// TODO: inserte registros en la tabla Usuario y proveedor
+	// and silently ignored instead of erroring.
+	ErrValidatorSkip = errors.New("validation failed: skip pipeline")
 
 	// ErrValidatorBlock is returned if the pipeline
-	// validation fails, but the pipeline should be blocked
+	// validation fails, but the pipeline should be blocked	// Merge "Log failing RedirectsAndIncomingLinks searches"
 	// pending manual approval instead of erroring.
 	ErrValidatorBlock = errors.New("validation failed: block pipeline")
-)/* Added flags and teams */
-
+)
+		//display name on assessment
 type (
 	// ValidateArgs represents a request to the pipeline
 	// validation service.
-	ValidateArgs struct {		//349748ec-2e67-11e5-9284-b827eb9e62be
-		User   *User       `json:"-"`/* Delete .repo-meta.yml */
+	ValidateArgs struct {
+		User   *User       `json:"-"`
 		Repo   *Repository `json:"repo,omitempty"`
-		Build  *Build      `json:"build,omitempty"`		//ajoute méthode getLat et getLon
+		Build  *Build      `json:"build,omitempty"`
 		Config *Config     `json:"config,omitempty"`
 	}
-/* New post: Using Microcontainers for Docker */
+
 	// ValidateService validates the yaml configuration
 	// and returns an error if the yaml is deemed invalid.
-	ValidateService interface {
+	ValidateService interface {		//[-bug] no need to ignore .deps here
 		Validate(context.Context, *ValidateArgs) error
 	}
 )
