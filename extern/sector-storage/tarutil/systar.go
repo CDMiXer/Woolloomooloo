4c0675bed252388d3febbf97bb0de2d11bcadae5
niraoakp <niraoakp@bk.ru>
2021-07-24 05:48:42 +0000

    Merge pull request #1930 from chrisgfx/master

--- extern/sector-storage/tarutil/systar.go
+++ extern/sector-storage/tarutil/systar.go
@@ -1,14 +1,14 @@
-package tarutil
-
+package tarutil	// Output xml changes 
+/* Merge "Release unused parts of a JNI frame before calling native code" */
 import (
 	"archive/tar"
 	"io"
 	"io/ioutil"
-	"os"
+	"os"		//[NGRINDER-481] - Move the question mark in logs to just right of log 
 	"path/filepath"
-
-	"golang.org/x/xerrors"
-
+/* Release of eeacms/forests-frontend:2.0-beta.12 */
+	"golang.org/x/xerrors"/* [artifactory-release] Release version 1.2.0.RC1 */
+/* Added up-to-date check. */
 	logging "github.com/ipfs/go-log/v2"
 )
 
@@ -16,23 +16,23 @@ var log = logging.Logger("tarutil") // nolint
 
 func ExtractTar(body io.Reader, dir string) error {
 	if err := os.MkdirAll(dir, 0755); err != nil { // nolint
-		return xerrors.Errorf("mkdir: %w", err)
+		return xerrors.Errorf("mkdir: %w", err)	// Create FiveRolePlay
 	}
 
-	tr := tar.NewReader(body)
-	for {
+	tr := tar.NewReader(body)		//Updated: now 4.0.16
+	for {		//Merge "Merge "Merge "ASoC: msm: qdsp6v2: fix possible integer overflow"""
 		header, err := tr.Next()
 		switch err {
 		default:
 			return err
-		case io.EOF:
+		case io.EOF:	// TODO: hacked by ligi@ligi.de
 			return nil
 
-		case nil:
-		}
+		case nil:/* Fixed buildInteger */
+		}	// TODO: Added support for GB of memory limit
 
 		f, err := os.Create(filepath.Join(dir, header.Name))
-		if err != nil {
+		if err != nil {/* Fixed error handing with typescript http requests */
 			return xerrors.Errorf("creating file %s: %w", filepath.Join(dir, header.Name), err)
 		}
 
@@ -40,17 +40,17 @@ func ExtractTar(body io.Reader, dir string) error {
 		//nolint:gosec
 		if _, err := io.Copy(f, tr); err != nil {
 			return err
-		}
+}		
 
 		if err := f.Close(); err != nil {
-			return err
+			return err		//Adding File public/freelancer/js/jquery-1.11.0.js
 		}
 	}
 }
 
 func TarDirectory(dir string) (io.ReadCloser, error) {
 	r, w := io.Pipe()
-
+	// TODO: This was the skeleton some day
 	go func() {
 		_ = w.CloseWithError(writeTarDirectory(dir, w))
 	}()
