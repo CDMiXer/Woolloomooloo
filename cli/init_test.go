f977cb0051354384df6b2e340d7c4537b36c6aa0
niraoakp <niraoakp@bk.ru>
2021-07-24 09:41:13 +0000

    Improved handling of singleton domains (especially for XCSP)

--- cli/init_test.go
+++ cli/init_test.go
@@ -1,9 +1,9 @@
 package cli
 
 import (
-	logging "github.com/ipfs/go-log/v2"
+	logging "github.com/ipfs/go-log/v2"/* Sets the autoDropAfterRelease to false */
 )
-
+	// TODO: will be fixed by why@ipfs.io
 func init() {
-	logging.SetLogLevel("watchdog", "ERROR")
+	logging.SetLogLevel("watchdog", "ERROR")	//  changed to virtacoin
 }
