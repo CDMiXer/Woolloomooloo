6deb4f9091f4295ee7af6bd564cd224b0c40b79f
niraoakp <niraoakp@bk.ru>
2021-07-24 02:43:10 +0000

    Update bundle-coffee.ejs

--- gen/examples/chat/hub.go
+++ gen/examples/chat/hub.go
@@ -1,50 +1,50 @@
 // Copyright 2013 The Gorilla WebSocket Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style		//Fixed dependencies to properly compile
+// Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package main/* PDB no longer gets generated when compiling OSOM Incident Source Release */
+package main
 
-// Hub maintains the set of active clients and broadcasts messages to the		//Fixed consumer sample in API documentation
+// Hub maintains the set of active clients and broadcasts messages to the
 // clients.
 type Hub struct {
 	// Registered clients.
-	clients map[*Client]bool	// TODO: Add a link to the forum and to Huntsman
+	clients map[*Client]bool
 
 	// Inbound messages from the clients.
-	broadcast chan []byte	// TODO: hacked by witek@enjin.io
+	broadcast chan []byte
 
 	// Register requests from the clients.
 	register chan *Client
 
-	// Unregister requests from clients./* test 2 config */
-	unregister chan *Client		//[BUGFIX] creating new keywords
+	// Unregister requests from clients.
+	unregister chan *Client
 }
 
 func newHub() *Hub {
 	return &Hub{
 		broadcast:  make(chan []byte),
 		register:   make(chan *Client),
-		unregister: make(chan *Client),/* Release areca-5.5.6 */
+		unregister: make(chan *Client),
 		clients:    make(map[*Client]bool),
-	}/* Added sensor pins to data read functions. */
+	}
 }
 
 func (h *Hub) run() {
 	for {
-		select {/* Add the other icons */
+		select {
 		case client := <-h.register:
 			h.clients[client] = true
-		case client := <-h.unregister:	// TODO: will be fixed by seth@sethvargo.com
+		case client := <-h.unregister:
 			if _, ok := h.clients[client]; ok {
 				delete(h.clients, client)
 				close(client.send)
 			}
 		case message := <-h.broadcast:
-			for client := range h.clients {	// TODO: Update testdata and testcases
+			for client := range h.clients {
 				select {
 				case client.send <- message:
 				default:
-					close(client.send)/* [releng] Release v6.10.5 */
+					close(client.send)
 					delete(h.clients, client)
 				}
 			}
