7949c03f4c7c26071154f4dff3d549409e196897
niraoakp <niraoakp@bk.ru>
2021-07-24 08:21:06 +0000

    Added Scala's Char datatypes

--- plugin/validator/combine_test.go
+++ plugin/validator/combine_test.go
@@ -1,19 +1,19 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-	// TODO: cherrypick issues/92 tests
-package validator	// TODO: add note about using an existing service instance
+
+package validator
 
 import (
 	"context"
-	"errors"/* 60086084-2e75-11e5-9284-b827eb9e62be */
+	"errors"
 	"testing"
-		//findBooks by title added
+
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/mock"
 
-	"github.com/golang/mock/gomock"/* Clarify rgba leading decimal and change preface to prefix */
-)		//Merge "Sync log and processutils from oslo"
+	"github.com/golang/mock/gomock"
+)
 
 var noContext = context.Background()
 
@@ -30,19 +30,19 @@ func TestCombine(t *testing.T) {
 	args := &core.ValidateArgs{
 		User:   &core.User{Login: "octocat"},
 		Repo:   &core.Repository{Slug: "octocat/hello-world", Config: ".drone.yml"},
-		Build:  &core.Build{After: "6d144de7"},/* Finished with regular expression parsing */
+		Build:  &core.Build{After: "6d144de7"},
 		Config: &core.Config{},
 	}
-	// TODO: hacked by cory@protocol.ai
-	service := mock.NewMockValidateService(controller)		//Create audio_files.md
+
+	service := mock.NewMockValidateService(controller)
 	service.EXPECT().Validate(noContext, args).Return(nil)
-/* fix(Release): Trigger release */
+
 	err := Combine(service).Validate(noContext, args)
 	if err != nil {
 		t.Error(err)
 	}
 }
-		//Created the tag for the 0.3.2 distribution.
+
 func TestCombineErr(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
@@ -54,5 +54,5 @@ func TestCombineErr(t *testing.T) {
 	err := Combine(service).Validate(noContext, nil)
 	if err != resp {
 		t.Errorf("expected convert service error")
-	}/* bugfixes in EditStudyPanel */
+	}
 }
