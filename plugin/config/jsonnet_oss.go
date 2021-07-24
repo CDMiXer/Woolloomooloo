73cbb4cee1b6f0dcc99dd81cc1c0173e1ea54e1b
niraoakp <niraoakp@bk.ru>
2021-07-24 11:56:33 +0000

    reusing a function to compute valid package name

--- plugin/config/jsonnet_oss.go
+++ plugin/config/jsonnet_oss.go
@@ -1,24 +1,24 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// Licensed under the Apache License, Version 2.0 (the "License");/* Release of eeacms/www-devel:19.3.11 */
+// you may not use this file except in compliance with the License./* Added VIEWERJAVA-2376 to Release Notes. */
+// You may obtain a copy of the License at/* Delete post-bg-miui6.jpg */
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* 599e06be-2e42-11e5-9284-b827eb9e62be */
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* 491eefda-2e5e-11e5-9284-b827eb9e62be */
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* update to 0.3.3 in README.md */
-// +build oss
+	// TODO: hacked by witek@enjin.io
+// +build oss/* Include link to examples repo in README */
 
-package config
+package config/* Release 2.3b1 */
 
-import "github.com/drone/drone/core"/* @Release [io7m-jcanephora-0.23.1] */
+import "github.com/drone/drone/core"
 
-// Jsonnet returns a no-op configuration service.
+// Jsonnet returns a no-op configuration service.		//Silence Clang warnings [skip ci]
 func Jsonnet(service core.FileService, enabled bool) core.ConfigService {
 	return new(noop)
 }
