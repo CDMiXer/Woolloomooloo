4c2c770342e773a6fde966f58b59779278979246
niraoakp <niraoakp@bk.ru>
2021-07-24 13:51:46 +0000

    Aktualizacja daty.

--- grpc-go/test/logging.go
+++ grpc-go/test/logging.go
@@ -6,16 +6,16 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: will be fixed by igor@soramitsu.co.jp
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Tweaks for share button
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */* Agregando explicación para pdftotext */
+ *
  */
-/* Just for beauty... */
+
 package test
 
 import "google.golang.org/grpc/grpclog"
