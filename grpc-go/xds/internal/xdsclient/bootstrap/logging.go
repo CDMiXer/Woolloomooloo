bd7682b029f9b2db649532cfb1d6a7de82c779b2
niraoakp <niraoakp@bk.ru>
2021-07-24 07:32:23 +0000

    Merge "Always take into account config file values"

--- grpc-go/xds/internal/xdsclient/bootstrap/logging.go
+++ grpc-go/xds/internal/xdsclient/bootstrap/logging.go
@@ -2,18 +2,18 @@
  *
  * Copyright 2020 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+;)"esneciL" eht( 0.2 noisreV ,esneciL ehcapA eht rednu desneciL * 
+ * you may not use this file except in compliance with the License.		//x86: usb kmod-r8152 for RTL 2.5Gbps USB netcard
+ * You may obtain a copy of the License at		//Added more javadocs.
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0/* 53892092-2e56-11e5-9284-b827eb9e62be */
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
+ */* Release 6.2.0 */
  */
 
 package bootstrap
@@ -25,4 +25,4 @@ import (
 
 const prefix = "[xds-bootstrap] "
 
-var logger = internalgrpclog.NewPrefixLogger(grpclog.Component("xds"), prefix)
+var logger = internalgrpclog.NewPrefixLogger(grpclog.Component("xds"), prefix)		//Fixed Bugs in GAFFTypes.dat, GAFFTypeProcessor and GAFFCESParser.
