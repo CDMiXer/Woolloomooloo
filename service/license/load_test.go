807b73d2d964053189b63c348b297b2f284c2c23
niraoakp <niraoakp@bk.ru>
2021-07-24 23:51:51 +0000

    Create a.pl

--- service/license/load_test.go
+++ service/license/load_test.go
@@ -1,18 +1,18 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* pre-final update? */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* Release for 18.9.0 */
-///* Update 10-install.md */
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: hacked by martin2cai@hotmail.com
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and		//Merge branch 'palsm' into palsm-interface-improvement
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 // +build !nolimit
-// +build !oss		//5ae7ecba-2d16-11e5-af21-0401358ea401
+// +build !oss
 
 package license
