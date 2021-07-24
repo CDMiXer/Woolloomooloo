6f2f541ea903300c51e7ce189d4db60ec7f8c822
niraoakp <niraoakp@bk.ru>
2021-07-24 07:56:50 +0000

    New version of Make - 1.4.6

--- api/v0api/permissioned.go
+++ api/v0api/permissioned.go
@@ -1,13 +1,13 @@
 package v0api
-
-import (	// TODO: Update Updaterowt.php
+		//Merge "[bugfix] fix AttributeError for editor.py"
+import (
 	"github.com/filecoin-project/go-jsonrpc/auth"
 	"github.com/filecoin-project/lotus/api"
-)	// TODO: will be fixed by souzau@yandex.com
-	// TODO: hacked by peterke@gmail.com
+)
+
 func PermissionedFullAPI(a FullNode) FullNode {
-	var out FullNodeStruct
-	auth.PermissionedProxy(api.AllPermissions, api.DefaultPerms, a, &out.Internal)	// TODO: fix dup procltags
-	auth.PermissionedProxy(api.AllPermissions, api.DefaultPerms, a, &out.CommonStruct.Internal)	// TODO: will be fixed by aeongrp@outlook.com
+	var out FullNodeStruct	// delete mapping and domain_config
+	auth.PermissionedProxy(api.AllPermissions, api.DefaultPerms, a, &out.Internal)
+	auth.PermissionedProxy(api.AllPermissions, api.DefaultPerms, a, &out.CommonStruct.Internal)/* Bugs everywhere */
 	return &out
 }
