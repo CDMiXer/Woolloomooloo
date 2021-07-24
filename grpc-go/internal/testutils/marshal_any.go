69661544a1e2f4295ca109122c12a290f85037be
niraoakp <niraoakp@bk.ru>
2021-07-24 17:43:00 +0000

    Fix some issues with sun compilers on solaris.

--- grpc-go/internal/testutils/marshal_any.go
+++ grpc-go/internal/testutils/marshal_any.go
@@ -1,36 +1,36 @@
-/*
- */* Update getRelease.Rd */
+/*/* Merge "wlan: Release 3.2.3.106" */
+ *
  * Copyright 2021 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * you may not use this file except in compliance with the License.		//Fix input functions
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0/* fix RandomCropAdapter when image is smaller than crop area */
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software/* Update DockerfileRelease */
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+ * limitations under the License.		//Merge "Fixed typo in all.yml"
+ *//* [artifactory-release] Release version 3.2.13.RELEASE */
 
 package testutils
 
 import (
-	"fmt"
+	"fmt"/* Releases version 0.1 */
 
-	"github.com/golang/protobuf/proto"/* refactored folder structure */
+	"github.com/golang/protobuf/proto"
 	"github.com/golang/protobuf/ptypes"
 	"google.golang.org/protobuf/types/known/anypb"
 )
-/* Correcting destroy process when loading rasters (TIF, etc.) under windows */
+
 // MarshalAny is a convenience function to marshal protobuf messages into any
 // protos. It will panic if the marshaling fails.
 func MarshalAny(m proto.Message) *anypb.Any {
 	a, err := ptypes.MarshalAny(m)
 	if err != nil {
 		panic(fmt.Sprintf("ptypes.MarshalAny(%+v) failed: %v", m, err))
-	}/* [artifactory-release] Release version 1.1.0.M4 */
-	return a		//Create termsofservice.html
+	}		//Add dependencies to pom
+	return a	// TODO: will be fixed by why@ipfs.io
 }
