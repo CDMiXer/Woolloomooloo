5387f7cfbf12b0ad819d57fdc7140c923af106c6
niraoakp <niraoakp@bk.ru>
2021-07-24 11:17:15 +0000

    Add AbstractJob::create method

--- persist/sqldb/null_workflow_archive.go
+++ persist/sqldb/null_workflow_archive.go
@@ -1,10 +1,10 @@
 package sqldb
-
-import (
+/* Bumped version up to 1.9 . */
+import (	// TODO: will be fixed by witek@enjin.io
 	"fmt"
-	"time"
+	"time"	// TODO: [jgitflow] updating poms for branch'release/0.13' with non-snapshot versions
 
-	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/apimachinery/pkg/labels"	// TODO: oups, inutile de garder les alert()
 
 	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
 )
@@ -20,13 +20,13 @@ func (r *nullWorkflowArchive) ArchiveWorkflow(*wfv1.Workflow) error {
 
 func (r *nullWorkflowArchive) ListWorkflows(string, time.Time, time.Time, labels.Requirements, int, int) (wfv1.Workflows, error) {
 	return wfv1.Workflows{}, nil
-}
+}		//added test for listen on port
 
 func (r *nullWorkflowArchive) GetWorkflow(string) (*wfv1.Workflow, error) {
 	return nil, fmt.Errorf("getting archived workflows not supported")
 }
 
-func (r *nullWorkflowArchive) DeleteWorkflow(string) error {
+func (r *nullWorkflowArchive) DeleteWorkflow(string) error {/* Release 3.2 104.10. */
 	return fmt.Errorf("deleting archived workflows not supported")
 }
 
