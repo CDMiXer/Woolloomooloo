b99b02b564e0358dae9d917427b2dd76f6698bf2
niraoakp <niraoakp@bk.ru>
2021-07-24 22:07:10 +0000

    New version of Rambo - 1.2.2.1

--- tests/integration/aliases/nodejs/retype_component/step2/index.ts
+++ tests/integration/aliases/nodejs/retype_component/step2/index.ts
@@ -1,24 +1,24 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* [artifactory-release] Release version 2.1.0.M1 */
+/* Release of eeacms/ims-frontend:0.6.0 */
 import * as pulumi from "@pulumi/pulumi";
-
+/* use interfaces instead of classes with private constructors */
 class Resource extends pulumi.ComponentResource {
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:Resource", name, {}, opts);
-    }
+}    
 }
 
 // Scenario #4 - change the type of a component
-class ComponentFour extends pulumi.ComponentResource {		//added coverart download service, also downloads coverart by season
+class ComponentFour extends pulumi.ComponentResource {
     resource: Resource;
-    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {		//Fix missing dependency issues (Fixes #54)
+    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         // Add an alias that references the old type of this resource...
-        const aliases = [{ type: "my:module:ComponentFour" }, ...((opts && opts.aliases) || [])];
+        const aliases = [{ type: "my:module:ComponentFour" }, ...((opts && opts.aliases) || [])];		//Delete Teste+BI+no+R.html
         // ..and then make the super call with the new type of this resource and the added alias.
         super("my:differentmodule:ComponentFourWithADifferentTypeName", name, {}, { ...opts, aliases });
         // The child resource will also pick up an implicit alias due to the new type of the component it is parented
-        // to./* Release: Making ready for next release iteration 5.7.1 */
+        // to.
         this.resource = new Resource("otherchild", { parent: this });
     }
-}
-const comp4 = new ComponentFour("comp4");
+}	// add php 7 to tests
+const comp4 = new ComponentFour("comp4");/* [dev] Upgrade::upgrade: KeySpool would not be migrated into DB */
