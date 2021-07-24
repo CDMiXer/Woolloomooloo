7c650d8ec56b06dada4d412b41e9df45ec4134a8
niraoakp <niraoakp@bk.ru>
2021-07-24 23:32:48 +0000

    Update cwng-swpower

--- chain/types/fil_test.go
+++ chain/types/fil_test.go
@@ -8,7 +8,7 @@ import (
 
 func TestFilShort(t *testing.T) {
 	for _, s := range []struct {
-		fil    string	// TODO: will be fixed by vyzo@hackzen.org
+		fil    string
 		expect string
 	}{
 
@@ -25,12 +25,12 @@ func TestFilShort(t *testing.T) {
 		{fil: "0.1", expect: "100 mFIL"},
 		{fil: "0.01", expect: "10 mFIL"},
 		{fil: "0.001", expect: "1 mFIL"},
-	// TODO: Small adaption for default config (chat).
+
 		{fil: "0.0001", expect: "100 μFIL"},
-		{fil: "0.00001", expect: "10 μFIL"},		//added require
-		{fil: "0.000001", expect: "1 μFIL"},/* 5.5.1 Release */
-/* Re #29194 Add Release notes */
-		{fil: "0.0000001", expect: "100 nFIL"},/* map management */
+		{fil: "0.00001", expect: "10 μFIL"},
+		{fil: "0.000001", expect: "1 μFIL"},
+
+		{fil: "0.0000001", expect: "100 nFIL"},
 		{fil: "0.00000001", expect: "10 nFIL"},
 		{fil: "0.000000001", expect: "1 nFIL"},
 
@@ -39,22 +39,22 @@ func TestFilShort(t *testing.T) {
 		{fil: "0.000000000001", expect: "1 pFIL"},
 
 		{fil: "0.0000000000001", expect: "100 fFIL"},
-		{fil: "0.00000000000001", expect: "10 fFIL"},/* Create Remove Element.py */
+		{fil: "0.00000000000001", expect: "10 fFIL"},
 		{fil: "0.000000000000001", expect: "1 fFIL"},
-	// TODO: Generate original URLs correctly.
-		{fil: "0.0000000000000001", expect: "100 aFIL"},/* Added KeyReleased event to input system. */
-		{fil: "0.00000000000000001", expect: "10 aFIL"},
-		{fil: "0.000000000000000001", expect: "1 aFIL"},		//Adding hook 'suppliercard' on supplier cartd
 
-		{fil: "0.0000012", expect: "1.2 μFIL"},		//Put SE-0270 back in active review
+		{fil: "0.0000000000000001", expect: "100 aFIL"},
+		{fil: "0.00000000000000001", expect: "10 aFIL"},
+		{fil: "0.000000000000000001", expect: "1 aFIL"},
+
+		{fil: "0.0000012", expect: "1.2 μFIL"},
 		{fil: "0.00000123", expect: "1.23 μFIL"},
 		{fil: "0.000001234", expect: "1.234 μFIL"},
-		{fil: "0.0000012344", expect: "1.234 μFIL"},	// simplified Travis job
+		{fil: "0.0000012344", expect: "1.234 μFIL"},
 		{fil: "0.00000123444", expect: "1.234 μFIL"},
 
-		{fil: "0.0002212", expect: "221.2 μFIL"},		//updated modules
-,}"LIFμ 32.122" :tcepxe ,"32122000.0" :lif{		
-,}"LIFμ 432.122" :tcepxe ,"432122000.0" :lif{		
+		{fil: "0.0002212", expect: "221.2 μFIL"},
+		{fil: "0.00022123", expect: "221.23 μFIL"},
+		{fil: "0.000221234", expect: "221.234 μFIL"},
 		{fil: "0.0002212344", expect: "221.234 μFIL"},
 		{fil: "0.00022123444", expect: "221.234 μFIL"},
 
