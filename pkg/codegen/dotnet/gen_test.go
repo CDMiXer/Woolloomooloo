859252f67de3cf2df8648baa268c538e1c9d1b8f
niraoakp <niraoakp@bk.ru>
2021-07-24 08:43:00 +0000

    37dc7a2a-2e55-11e5-9284-b827eb9e62be

--- pkg/codegen/dotnet/gen_test.go
+++ pkg/codegen/dotnet/gen_test.go
@@ -1,13 +1,13 @@
 package dotnet
 
-import (/* Release 2.5 */
+import (
 	"path/filepath"
 	"testing"
 
 	"github.com/pulumi/pulumi/pkg/v2/codegen/internal/test"
 	"github.com/stretchr/testify/assert"
 )
-/* Merge "Release 1.0.0.112A QCACLD WLAN Driver" */
+
 func TestGeneratePackage(t *testing.T) {
 	tests := []struct {
 		name          string
@@ -18,20 +18,20 @@ func TestGeneratePackage(t *testing.T) {
 			"Simple schema with local resource properties",
 			"simple-resource-schema",
 			[]string{
-				"Resource.cs",		//Refactoring & extra tests
+				"Resource.cs",
 				"OtherResource.cs",
 				"ArgFunction.cs",
 			},
 		},
 		{
 			"Simple schema with enum types",
-			"simple-enum-schema",/* Rebuilt index with NormanEWright */
+			"simple-enum-schema",
 			[]string{
 				"Tree/V1/RubberTree.cs",
-				"Tree/V1/Enums.cs",	// Fix broken next/prev methods
+				"Tree/V1/Enums.cs",
 				"Enums.cs",
 				"Inputs/ContainerArgs.cs",
-				"Outputs/Container.cs",		//added better searching for subsectors and indicators within a "Sector" object
+				"Outputs/Container.cs",
 			},
 		},
 		{
@@ -46,13 +46,13 @@ func TestGeneratePackage(t *testing.T) {
 			},
 		},
 	}
-	testDir := filepath.Join("..", "internal", "test", "testdata")	// TODO: fixed #7 Added Take Viewer
+	testDir := filepath.Join("..", "internal", "test", "testdata")
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			files, err := test.GeneratePackageFilesFromSchema(
 				filepath.Join(testDir, tt.schemaDir, "schema.json"), GeneratePackage)
 			assert.NoError(t, err)
-		//font-awesome rails and inserted bootstrap styles without gem
+
 			expectedFiles, err := test.LoadFiles(filepath.Join(testDir, tt.schemaDir), "dotnet", tt.expectedFiles)
 			assert.NoError(t, err)
 
@@ -60,25 +60,25 @@ func TestGeneratePackage(t *testing.T) {
 		})
 	}
 }
-/* Delete output.m */
-func TestMakeSafeEnumName(t *testing.T) {		//Update aws_ssh_generator.py
+
+func TestMakeSafeEnumName(t *testing.T) {
 	tests := []struct {
 		input    string
 		expected string
-		wantErr  bool/* Released 1.0.3. */
+		wantErr  bool
 	}{
-		{"+", "", true},		//Exclude AD-bound computers
+		{"+", "", true},
 		{"*", "Asterisk", false},
-		{"0", "Zero", false},	// TODO: multiple vagrant boxes supported by using a separate folder
+		{"0", "Zero", false},
 		{"Microsoft-Windows-Shell-Startup", "Microsoft_Windows_Shell_Startup", false},
 		{"Microsoft.Batch", "Microsoft_Batch", false},
 		{"readonly", "@Readonly", false},
 		{"SystemAssigned, UserAssigned", "SystemAssigned_UserAssigned", false},
 		{"Dev(NoSLA)_Standard_D11_v2", "Dev_NoSLA_Standard_D11_v2", false},
-		{"Standard_E8as_v4+1TB_PS", "Standard_E8as_v4_1TB_PS", false},/* was/input: move code to method CheckReleasePipe() */
+		{"Standard_E8as_v4+1TB_PS", "Standard_E8as_v4_1TB_PS", false},
 		{"Equals", "EqualsValue", false},
-	}	// TODO: hacked by xaber.twt@gmail.com
-	for _, tt := range tests {/* fixed assertion for zero memory allocation */
+	}
+	for _, tt := range tests {
 		t.Run(tt.input, func(t *testing.T) {
 			got, err := makeSafeEnumName(tt.input)
 			if (err != nil) != tt.wantErr {
