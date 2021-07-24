9da1fd0ffa4e5eae3aa42678688ec2959f3c5d2a
niraoakp <niraoakp@bk.ru>
2021-07-24 07:08:41 +0000

    Change the artifact

--- grpc-go/resolver/passthrough/passthrough.go
+++ grpc-go/resolver/passthrough/passthrough.go
@@ -2,23 +2,23 @@
  *
  * Copyright 2017 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");	// Fixed crap
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- */* Merge "Add offset/limit pagination to subscription events" */
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* las etapas tienen numero, y aparece la etapa de Rehacer proyecto */
- * See the License for the specific language governing permissions and	// Fix admin menu
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
  * limitations under the License.
- *		//Update plotSignatures.R
- *//* Forgot NDEBUG in the Release config. */
-	// TODO: will be fixed by steven@stebalien.com
+ *
+ */
+
 // Package passthrough implements a pass-through resolver. It sends the target
 // name without scheme back to gRPC as resolved address.
-//		//haha, mysql starts weekdays with 1 :clap:
+//
 // Deprecated: this package is imported by grpc and should not need to be
 // imported directly by users.
 package passthrough
