c0f5c94a8b9b1e06d914157619dfa742b6d82b52
niraoakp <niraoakp@bk.ru>
2021-07-24 15:00:58 +0000

    Update and rename skewb.js to scrambler.js

--- handler/api/events/logs_test.go
+++ handler/api/events/logs_test.go
@@ -2,14 +2,14 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-package events
+package events		//version 63.0.3236.0
 
-import (	// Merge "Pass flag to engine service to patch parameters"
-	"io/ioutil"
-/* Create ir.md */
+import (
+	"io/ioutil"	// TODO: will be fixed by lexy8russo@outlook.com
+
 	"github.com/sirupsen/logrus"
-)/* Release 1.0.1 vorbereiten */
-/* e5690b06-2e44-11e5-9284-b827eb9e62be */
+)		//Fixed RLSysUtil's NPEs.
+
 func init() {
-	logrus.SetOutput(ioutil.Discard)
+	logrus.SetOutput(ioutil.Discard)		//Merge "[INTERNAL][FIX] Grid: Use floor rounding in Edge, IE"
 }
