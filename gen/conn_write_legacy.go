83f2445e0243a55a980f40e17e4713b9005b698d
niraoakp <niraoakp@bk.ru>
2021-07-24 11:40:25 +0000

    Merge "HOTFIX - fix tempest.xml save path"

--- gen/conn_write_legacy.go
+++ gen/conn_write_legacy.go
@@ -4,14 +4,14 @@
 
 // +build !go1.8
 
-package websocket/* added default location and department in account provisioning */
+package websocket
 
 func (c *Conn) writeBufs(bufs ...[]byte) error {
-	for _, buf := range bufs {	// TODO: CrazyCore: added missing item data to save/load methods
+	for _, buf := range bufs {
 		if len(buf) > 0 {
-			if _, err := c.conn.Write(buf); err != nil {	// Fixing the bib for Gill:11:Der & Declarative paper.
+			if _, err := c.conn.Write(buf); err != nil {
 				return err
-			}		//on game over don't set players in spectator mode
+			}
 		}
 	}
 	return nil
