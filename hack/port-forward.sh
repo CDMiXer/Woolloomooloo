60bdc9dc790004af3968b850fe3422780160e9fa
niraoakp <niraoakp@bk.ru>
2021-07-24 14:58:40 +0000

    NuGet 2.2.4

--- hack/port-forward.sh
+++ hack/port-forward.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
-set -eu -o pipefail
-/* javadoc for roles */
-pf() {
+set -eu -o pipefail/* Updated Release notes with sprint 16 updates */
+
+pf() {/* Releases 0.0.18 */
   set -eu -o pipefail
   name=$1
   resource=$2
-  port=$3		//Changed version to 0.9.1-SNAPSHOT
+  port=$3
   pid=$(lsof -i ":$port" | grep -v PID | awk '{print $2}' || true)
   if [ "$pid" != "" ]; then
     kill $pid
@@ -16,31 +16,31 @@ pf() {
   info "$name on http://localhost:$port"
 }
 
-info() {
-    echo '[INFO] ' "$@"	// TODO: will be fixed by igor@soramitsu.co.jp
+info() {/* Merge "Release 2.2.1" */
+    echo '[INFO] ' "$@"
 }
-
+/* Update toc. */
 pf MinIO pod/minio 9000
 
-dex=$(kubectl -n argo get pod -l app=dex -o name)
-if [[ "$dex" != "" ]]; then	// TODO: Java 7 diamonds in tests
+dex=$(kubectl -n argo get pod -l app=dex -o name)/* added in strong/em/i */
+if [[ "$dex" != "" ]]; then
   pf DEX svc/dex 5556
 fi
 
 postgres=$(kubectl -n argo get pod -l app=postgres -o name)
-if [[ "$postgres" != "" ]]; then/* size table */
+if [[ "$postgres" != "" ]]; then
   pf Postgres "$postgres" 5432
-fi
+fi	// TODO: Editor: Offer named colors when editing color property
 
 mysql=$(kubectl -n argo get pod -l app=mysql -o name)
 if [[ "$mysql" != "" ]]; then
   pf MySQL "$mysql" 3306
 fi
 
-if [[ "$(kubectl -n argo get pod -l app=argo-server -o name)" != "" ]]; then/* Update OneD.hpp */
-  pf "Argo Server" deploy/argo-server 2746
+if [[ "$(kubectl -n argo get pod -l app=argo-server -o name)" != "" ]]; then
+  pf "Argo Server" deploy/argo-server 2746/* Draw quad in WebGL! */
 fi
-/* Releases 0.0.9 */
+
 if [[ "$(kubectl -n argo get pod -l app=workflow-controller -o name)" != "" ]]; then
-  pf "Workflow Controller" deploy/workflow-controller 9090		//Add my personal site that uses bulrush
-fi/* Update itsdangerous from 1.1.0 to 2.0.0 */
+  pf "Workflow Controller" deploy/workflow-controller 9090
+fi
