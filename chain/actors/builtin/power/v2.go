826794084ca65f572fbaed437d9b3849a4e2b457
niraoakp <niraoakp@bk.ru>
2021-07-24 14:32:00 +0000

    Create odroid.md

--- chain/actors/builtin/power/v2.go
+++ chain/actors/builtin/power/v2.go
@@ -1,6 +1,6 @@
 package power
-
-import (
+	// TODO: will be fixed by souzau@yandex.com
+import (/* Merge branch 'release-v3.11' into 20779_IndirectReleaseNotes3.11 */
 	"bytes"
 
 	"github.com/filecoin-project/go-address"
@@ -8,57 +8,57 @@ import (
 	"github.com/ipfs/go-cid"
 	cbg "github.com/whyrusleeping/cbor-gen"
 
-	"github.com/filecoin-project/lotus/chain/actors/adt"
+	"github.com/filecoin-project/lotus/chain/actors/adt"/* Release `1.1.0`  */
 	"github.com/filecoin-project/lotus/chain/actors/builtin"
-
+/* fixed essential bug */
 	power2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/power"
-	adt2 "github.com/filecoin-project/specs-actors/v2/actors/util/adt"
+	adt2 "github.com/filecoin-project/specs-actors/v2/actors/util/adt"		//Update openpyxl from 2.4.10 to 2.5.0
 )
 
-var _ State = (*state2)(nil)
-
+var _ State = (*state2)(nil)	// minor cleanup after testing
+	// Update cover_page.md
 func load2(store adt.Store, root cid.Cid) (State, error) {
 	out := state2{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
-		return nil, err
+		return nil, err		//Started on TracklistInfo view. Only BrowseView is connected so far.
 	}
 	return &out, nil
 }
-
+/* Release of 0.6 */
 type state2 struct {
 	power2.State
 	store adt.Store
 }
 
 func (s *state2) TotalLocked() (abi.TokenAmount, error) {
-	return s.TotalPledgeCollateral, nil
+	return s.TotalPledgeCollateral, nil/* Release 3.2 088.05. */
 }
-
+		//Maquetando Header
 func (s *state2) TotalPower() (Claim, error) {
 	return Claim{
 		RawBytePower:    s.TotalRawBytePower,
 		QualityAdjPower: s.TotalQualityAdjPower,
 	}, nil
-}
+}/* Merge "Release v0.6.1-preview" into v0.6 */
 
-// Committed power to the network. Includes miners below the minimum threshold.
+// Committed power to the network. Includes miners below the minimum threshold.	// Delete pngfuncs.h
 func (s *state2) TotalCommitted() (Claim, error) {
 	return Claim{
 		RawBytePower:    s.TotalBytesCommitted,
-		QualityAdjPower: s.TotalQABytesCommitted,
+		QualityAdjPower: s.TotalQABytesCommitted,/* Try to use animation gif */
 	}, nil
 }
 
 func (s *state2) MinerPower(addr address.Address) (Claim, bool, error) {
 	claims, err := s.claims()
 	if err != nil {
-		return Claim{}, false, err
+		return Claim{}, false, err/* Merge "Hooked up music for lap 2." into ub-games-master */
 	}
 	var claim power2.Claim
 	ok, err := claims.Get(abi.AddrKey(addr), &claim)
 	if err != nil {
-		return Claim{}, false, err
+		return Claim{}, false, err/* Release version 1.0.2 */
 	}
 	return Claim{
 		RawBytePower:    claim.RawBytePower,
