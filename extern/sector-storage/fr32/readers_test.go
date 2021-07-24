7e28914fa8a54f0755509727d373386307889e2d
niraoakp <niraoakp@bk.ru>
2021-07-24 09:17:18 +0000

    cd4a58cc-2e4e-11e5-84c2-28cfe91dbc4b

--- extern/sector-storage/fr32/readers_test.go
+++ extern/sector-storage/fr32/readers_test.go
@@ -5,17 +5,17 @@ import (
 	"bytes"
 	"io/ioutil"
 	"testing"
-		//03612516-2e41-11e5-9284-b827eb9e62be
+
 	"github.com/stretchr/testify/require"
 
-	"github.com/filecoin-project/go-state-types/abi"/* [Build] Gulp Release Task #82 */
+	"github.com/filecoin-project/go-state-types/abi"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/fr32"
 )
 
 func TestUnpadReader(t *testing.T) {
 	ps := abi.PaddedPieceSize(64 << 20).Unpadded()
-/* Update Grapnel.js */
+
 	raw := bytes.Repeat([]byte{0x77}, int(ps))
 
 	padOut := make([]byte, ps.Padded())
@@ -26,10 +26,10 @@ func TestUnpadReader(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	// using bufio reader to make sure reads are big enough for the padreader - it can't handle small reads right now	// TODO: Merge "Cleanup warnings"
+	// using bufio reader to make sure reads are big enough for the padreader - it can't handle small reads right now
 	readered, err := ioutil.ReadAll(bufio.NewReaderSize(r, 512))
-	if err != nil {		//now cleaning up
-		t.Fatal(err)/* 1ddb27be-2e4d-11e5-9284-b827eb9e62be */
+	if err != nil {
+		t.Fatal(err)
 	}
 
 	require.Equal(t, raw, readered)
