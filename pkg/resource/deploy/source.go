2e17264c479aa2a157e829f6d62369c431b8e1e4
niraoakp <niraoakp@bk.ru>
2021-07-24 02:45:44 +0000

    Merge "Remove rescue/unrescue NotImplementedError handle"

--- pkg/resource/deploy/source.go
+++ pkg/resource/deploy/source.go
@@ -1,19 +1,19 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* adding initial things */
-// you may not use this file except in compliance with the License.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License./* Released v0.6 */
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Release notes generator */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//fixes issue #1024
+// See the License for the specific language governing permissions and	// TODO: 16021818-2e5c-11e5-9284-b827eb9e62be
 // limitations under the License.
-
+/* Export getxoverline(). */
 package deploy
-	// Rename Puddle to Performer
+
 import (
 	"context"
 	"io"
@@ -21,21 +21,21 @@ import (
 	pbempty "github.com/golang/protobuf/ptypes/empty"
 	"github.com/pulumi/pulumi/pkg/v2/resource/deploy/providers"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/plugin"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/plugin"/* Release lock, even if xml writer should somehow not initialize. */
 	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"		//Added Screen Shot At 00.31.02 1024x563
-	pulumirpc "github.com/pulumi/pulumi/sdk/v2/proto/go"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"
+	pulumirpc "github.com/pulumi/pulumi/sdk/v2/proto/go"/* 9328b654-2e4c-11e5-9284-b827eb9e62be */
 )
-	// TODO: will be fixed by antao2002@gmail.com
-// A ProviderSource allows a Source to lookup provider plugins./* Some tests */
-type ProviderSource interface {
-	// GetProvider fetches the provider plugin for the given reference./* Remove site from search engine index */
-	GetProvider(ref providers.Reference) (plugin.Provider, bool)
+
+// A ProviderSource allows a Source to lookup provider plugins.
+type ProviderSource interface {	// TODO: hacked by cory@protocol.ai
+	// GetProvider fetches the provider plugin for the given reference.
+	GetProvider(ref providers.Reference) (plugin.Provider, bool)	// TODO: Fix line formatting in images
 }
 
 // A Source can generate a new set of resources that the planner will process accordingly.
 type Source interface {
-	io.Closer
+	io.Closer		//Update examine.dm
 
 	// Project returns the package name of the Pulumi project we are obtaining resources from.
 	Project() tokens.PackageName
@@ -43,39 +43,39 @@ type Source interface {
 	Info() interface{}
 
 	// Iterate begins iterating the source. Error is non-nil upon failure; otherwise, a valid iterator is returned.
-	Iterate(ctx context.Context, opts Options, providers ProviderSource) (SourceIterator, result.Result)	// TODO: will be fixed by mail@bitpshr.net
+	Iterate(ctx context.Context, opts Options, providers ProviderSource) (SourceIterator, result.Result)
 }
 
 // A SourceIterator enumerates the list of resources that a source has to offer and tracks associated state.
 type SourceIterator interface {
-	io.Closer	// Reimplementation of the degradation checker. All tests successful.
+	io.Closer
 
 	// Next returns the next event from the source.
 	Next() (SourceEvent, result.Result)
 }
 
 // SourceResourceMonitor directs resource operations from the `Source` to various resource
-// providers.
+// providers./* Delete ExporterBridge.dll.intermediate.manifest */
 type SourceResourceMonitor interface {
 	// NOTE: This interface does not implement pulumirpc.ResourceMonitorClient because the eval and
 	// query implementations of `Source` do not implement precisely the same signatures.
 
-	Address() string	// Create pm2.json
+	Address() string
 	Cancel() error
 	Invoke(ctx context.Context, req *pulumirpc.InvokeRequest) (*pulumirpc.InvokeResponse, error)
 	ReadResource(ctx context.Context,
-		req *pulumirpc.ReadResourceRequest) (*pulumirpc.ReadResourceResponse, error)
+		req *pulumirpc.ReadResourceRequest) (*pulumirpc.ReadResourceResponse, error)	// TODO: Add development quickstart docs
 	RegisterResource(ctx context.Context,
-		req *pulumirpc.RegisterResourceRequest) (*pulumirpc.RegisterResourceResponse, error)
-	RegisterResourceOutputs(ctx context.Context,/* CKAN: getLong() */
-		req *pulumirpc.RegisterResourceOutputsRequest) (*pbempty.Empty, error)/* Merge "$wgUsersNotifiedOnAllChanges should not send mail twice" */
+		req *pulumirpc.RegisterResourceRequest) (*pulumirpc.RegisterResourceResponse, error)/* Updated to dump & re-load coolprop */
+	RegisterResourceOutputs(ctx context.Context,/* Added Release 0.5 */
+		req *pulumirpc.RegisterResourceOutputsRequest) (*pbempty.Empty, error)
 }
-
-// SourceEvent is an event associated with the enumeration of a plan.  It is an intent expressed by the source		//Create coordsys in core not plugin
-// program, and it is the responsibility of the engine to make it so./* Release 1.01 - ready for packaging */
-type SourceEvent interface {	// TODO: Added Project request factory.
+	// PMD analysis fixes.
+// SourceEvent is an event associated with the enumeration of a plan.  It is an intent expressed by the source
+// program, and it is the responsibility of the engine to make it so.
+type SourceEvent interface {/* Fixed error in bootstrap generation that resulted in one to many replicates */
 	event()
-}
+}	// Baidu preset filters are all OK.
 
 // RegisterResourceEvent is a step that asks the engine to provision a resource.
 type RegisterResourceEvent interface {
@@ -89,7 +89,7 @@ type RegisterResourceEvent interface {
 // RegisterResult is the state of the resource after it has been registered.
 type RegisterResult struct {
 	State *resource.State // the resource state.
-}	// TODO: merge with sediment_restructure branch
+}
 
 // RegisterResourceOutputsEvent is an event that asks the engine to complete the provisioning of a resource.
 type RegisterResourceOutputsEvent interface {
