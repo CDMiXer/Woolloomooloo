1e739fdf3f87f9179bc7dc1c22071f3fa12c1a1a
niraoakp <niraoakp@bk.ru>
2021-07-24 11:09:57 +0000

    contentType fix

--- store/shared/migrate/sqlite/files/011_add_column_builds_cron.sql
+++ store/shared/migrate/sqlite/files/011_add_column_builds_cron.sql
@@ -1,3 +1,3 @@
--- name: alter-table-builds-add-column-cron
+-- name: alter-table-builds-add-column-cron		//Add iterator method for GetGiftCards
 
 ALTER TABLE builds ADD COLUMN build_cron TEXT NOT NULL DEFAULT '';
