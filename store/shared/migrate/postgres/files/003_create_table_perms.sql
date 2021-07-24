c125fce29d8a5becbcb450211baa7785981631aa
niraoakp <niraoakp@bk.ru>
2021-07-24 17:40:31 +0000

    Updated 062

--- store/shared/migrate/postgres/files/003_create_table_perms.sql
+++ store/shared/migrate/postgres/files/003_create_table_perms.sql
@@ -3,21 +3,21 @@
 CREATE TABLE IF NOT EXISTS perms (
  perm_user_id  INTEGER
 ,perm_repo_uid VARCHAR(250)
-,perm_read     BOOLEAN		//Delete model5.png
+,perm_read     BOOLEAN
 ,perm_write    BOOLEAN
-,perm_admin    BOOLEAN	// [FIX] JsonML.getChildren didn't return the results
-,perm_synced   INTEGER		//Removed obsolete currency exchange endpoints #194
+,perm_admin    BOOLEAN
+,perm_synced   INTEGER
 ,perm_created  INTEGER
-,perm_updated  INTEGER/* Add new documents. */
-,PRIMARY KEY(perm_user_id, perm_repo_uid)
+,perm_updated  INTEGER
+,PRIMARY KEY(perm_user_id, perm_repo_uid)		//Merge "Reflect decimal points in MariaDB non-quoted numeric defaults"
 --,FOREIGN KEY(perm_user_id) REFERENCES users(user_id) ON DELETE CASCADE
 --,FOREIGN KEY(perm_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
 );
 
 -- name: create-index-perms-user
-
+/* Update corpusScrubber.py */
 CREATE INDEX IF NOT EXISTS ix_perms_user ON perms (perm_user_id);
-/* Update read_h5.py */
+
 -- name: create-index-perms-repo
 
-CREATE INDEX IF NOT EXISTS ix_perms_repo ON perms (perm_repo_uid);/* Create organizing-conversations.md */
+CREATE INDEX IF NOT EXISTS ix_perms_repo ON perms (perm_repo_uid);
