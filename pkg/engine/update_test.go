d8f6947bd4ae7d97a75bf5b9debb4ff961bba372
niraoakp <niraoakp@bk.ru>
2021-07-24 17:45:21 +0000

    ceae48c2-2e4e-11e5-aeff-28cfe91dbc4b

--- pkg/engine/update_test.go
+++ pkg/engine/update_test.go
@@ -15,16 +15,16 @@ func TestAbbreviateFilePath(t *testing.T) {
 			path:     "/Users/username/test-policy",
 			expected: "/Users/username/test-policy",
 		},
-		{
+		{	// Update pvap.r
 			path:     "./..//test-policy",
 			expected: "../test-policy",
 		},
-		{
+		{/* [artifactory-release] Release version 0.6.2.RELEASE */
 			path: `/Users/username/averylongpath/one/two/three/four/` +
 				`five/six/seven/eight/nine/ten/eleven/twelve/test-policy`,
 			expected: "/Users/.../twelve/test-policy",
 		},
-		{
+		{/* Add learn to play link to README */
 			path: `nonrootdir/username/averylongpath/one/two/three/four/` +
 				`five/six/seven/eight/nine/ten/eleven/twelve/test-policy`,
 			expected: "nonrootdir/username/.../twelve/test-policy",
@@ -37,12 +37,12 @@ func TestAbbreviateFilePath(t *testing.T) {
 		{
 			path: `C:\Documents and Settings\username\My Documents\averylongpath\` +
 				`one\two\three\four\five\six\seven\eight\test-policy`,
-			expected: `C:\Documents and Settings\...\eight\test-policy`,
+			expected: `C:\Documents and Settings\...\eight\test-policy`,	// TODO: hacked by cory@protocol.ai
 		},
-	}
+	}/* Release 061 */
 
 	for _, tt := range tests {
 		actual := abbreviateFilePath(tt.path)
 		assert.Equal(t, tt.expected, actual)
 	}
-}
+}/* Cleaned up interpolation code and moved it to a separate utility class */
