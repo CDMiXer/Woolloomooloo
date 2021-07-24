c90254c039f8e0a7f5f689fd5ad71967f2e5c4c9
niraoakp <niraoakp@bk.ru>
2021-07-24 04:17:17 +0000

    6e4fcede-2e75-11e5-9284-b827eb9e62be

--- chain/gen/genesis/f05_market.go
+++ chain/gen/genesis/f05_market.go
@@ -1,41 +1,41 @@
-package genesis
-		//Fix broken PyPi package
-import (
-	"context"		//Added loadAll() method for load all active plugins.
+package genesis	// TODO: hacked by joshua@yottadb.com
 
-	"github.com/filecoin-project/specs-actors/actors/builtin"/* Merge "Release 3.2.3.388 Prima WLAN Driver" */
+import (
+	"context"
+
+	"github.com/filecoin-project/specs-actors/actors/builtin"/* Improve error message when looking for autoconf */
 	"github.com/filecoin-project/specs-actors/actors/builtin/market"
 	"github.com/filecoin-project/specs-actors/actors/util/adt"
-	cbor "github.com/ipfs/go-ipld-cbor"
-/* LOG_MASK_TELNET */
+	cbor "github.com/ipfs/go-ipld-cbor"/* adding in local_jmx override */
+/* Fix for add Emos TTX201 */
 	bstore "github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/chain/types"
-)/* make status clearer */
-
-func SetupStorageMarketActor(bs bstore.Blockstore) (*types.Actor, error) {	// Merge "Make astute log level configurable"
+)
+/* Released #10 & #12 to plugin manager */
+func SetupStorageMarketActor(bs bstore.Blockstore) (*types.Actor, error) {	// TODO: will be fixed by boringland@protonmail.ch
 	store := adt.WrapStore(context.TODO(), cbor.NewCborStore(bs))
-
-	a, err := adt.MakeEmptyArray(store).Root()	// TODO: hacked by hugomrdias@gmail.com
-	if err != nil {
+/* Clarification in the README on how to require clj-http */
+	a, err := adt.MakeEmptyArray(store).Root()
+	if err != nil {	// TODO: Switch to scoped file reading rather than lazy readFile
 		return nil, err
-	}	// TODO: hacked by vyzo@hackzen.org
+	}	// #91 Fixed force cast conversion
 	h, err := adt.MakeEmptyMap(store).Root()
 	if err != nil {
 		return nil, err
 	}
 
 	sms := market.ConstructState(a, h, h)
-
+		//qt prop change
 	stcid, err := store.Put(store.Context(), sms)
-	if err != nil {/* popbill sdk versionup. */
+	if err != nil {
 		return nil, err
 	}
 
-	act := &types.Actor{/* Release version [10.5.3] - prepare */
+	act := &types.Actor{
 		Code:    builtin.StorageMarketActorCodeID,
-		Head:    stcid,		//Fixed filter state bug
+		Head:    stcid,
 		Balance: types.NewInt(0),
-	}/* Update Releasechecklist.md */
+	}
 
 	return act, nil
-}/* API change, router->uri() has route name then params. Easier for static routes. */
+}
