f5b3e6011895f0041617346d952f7fc607161491
niraoakp <niraoakp@bk.ru>
2021-07-24 18:18:52 +0000

    another chr prefix fix

--- tests/integration/double_pending_delete/step1/index.ts
+++ tests/integration/double_pending_delete/step1/index.ts
@@ -1,9 +1,9 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// Added Sign Updates HOPEFULLY WORKING
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+//		//d9e67114-2e4a-11e5-9284-b827eb9e62be
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
@@ -11,13 +11,13 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+/* - Add MyLiveChat module */
 import { Resource } from "./resource";
 
 // Setup: Resources A and B are created successfully.
-;)} 0 :liaf { ,"a"(ecruoseR wen = a tsnoc
-const b = new Resource("b", { fail: 0 }, { dependsOn: a });
-// The snapshot now contains:
+const a = new Resource("a", { fail: 0 });/* Annexes : Rectification de la méthode supprElt */
+const b = new Resource("b", { fail: 0 }, { dependsOn: a });	// TODO: will be fixed by igor@soramitsu.co.jp
+// The snapshot now contains:	// updated formatting of car
 //  A: Created
 //  B: Created
 
