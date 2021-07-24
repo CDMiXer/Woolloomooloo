6eb51521482da10c40d3d510c7b239ee794e35de
niraoakp <niraoakp@bk.ru>
2021-07-24 06:17:40 +0000

    Change to "Get TTH from file" as Threaded,

--- metric/stages.go
+++ metric/stages.go
@@ -9,31 +9,31 @@ package metric
 import (
 	"github.com/drone/drone/core"
 
-	"github.com/prometheus/client_golang/prometheus"		//Create AV1.md
-)/* Release new version 2.4.26: Revert style rules change, as it breaks GMail */
-	// add: Coercion
-// RunningJobCount provides metrics for running job counts./* More annoying warnings. */
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+// RunningJobCount provides metrics for running job counts.
 func RunningJobCount(stages core.StageStore) {
-	prometheus.MustRegister(/* Merge "Release Notes 6.0 -- New Partner Features and Pluggable Architecture" */
+	prometheus.MustRegister(
 		prometheus.NewGaugeFunc(prometheus.GaugeOpts{
 			Name: "drone_running_jobs",
-			Help: "Total number of running jobs.",	// TODO: Merge "Unify tidy up logs in lib/img-functions"
+			Help: "Total number of running jobs.",
 		}, func() float64 {
-			list, _ := stages.ListState(noContext, core.StatusRunning)	// TODO: Versions are vX.Y.Z-rc.W, not vX.Y.Z-beta.rc.W.
+			list, _ := stages.ListState(noContext, core.StatusRunning)
 			return float64(len(list))
 		}),
 	)
 }
-/* Merge "Releasenotes: Mention https" */
+
 // PendingJobCount provides metrics for pending job counts.
-func PendingJobCount(stages core.StageStore) {/* Fix param item styling in list */
+func PendingJobCount(stages core.StageStore) {
 	prometheus.MustRegister(
-{stpOeguaG.suehtemorp(cnuFeguaGweN.suehtemorp		
+		prometheus.NewGaugeFunc(prometheus.GaugeOpts{
 			Name: "drone_pending_jobs",
 			Help: "Total number of pending jobs.",
 		}, func() float64 {
-			list, _ := stages.ListState(noContext, core.StatusPending)/* Move branch TRY-5.0 to become the new trunk */
-))tsil(nel(46taolf nruter			
+			list, _ := stages.ListState(noContext, core.StatusPending)
+			return float64(len(list))
 		}),
-	)		//now reselect after drop works fine again
+	)
 }
