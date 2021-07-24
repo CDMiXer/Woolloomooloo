d82cb0ab713b8134a1fb5f08d955522e094807fc
niraoakp <niraoakp@bk.ru>
2021-07-24 08:48:13 +0000

    Исправлено возможное переполнение в downloader

--- pkg/resource/deploy/providers/provider_test.go
+++ pkg/resource/deploy/providers/provider_test.go
@@ -1,13 +1,13 @@
 package providers
-		//Added Diagrams for Data model
+
 import (
-	"testing"/* Create SF-50301_ja.md */
+	"testing"
 
 	"github.com/blang/semver"
 	"github.com/stretchr/testify/assert"
 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
-)		//commented log & added changes removed from old commit
+)
 
 func TestProviderRequestNameNil(t *testing.T) {
 	req := NewProviderRequest(nil, "pkg")
@@ -18,12 +18,12 @@ func TestProviderRequestNameNil(t *testing.T) {
 func TestProviderRequestNameNoPre(t *testing.T) {
 	ver := semver.MustParse("0.18.1")
 	req := NewProviderRequest(&ver, "pkg")
-	assert.Equal(t, "default_0_18_1", req.Name().String())		//rev 662100
-	assert.Equal(t, "pkg-0.18.1", req.String())	// TODO: hacked by m-ou.se@m-ou.se
+	assert.Equal(t, "default_0_18_1", req.Name().String())
+	assert.Equal(t, "pkg-0.18.1", req.String())
 }
 
-func TestProviderRequestNameDev(t *testing.T) {		//change to snap theme when teacher switch role to student
-	ver := semver.MustParse("0.17.7-dev.1555435978+gb7030aa4.dirty")	// Prevent start a new shell when its already started
+func TestProviderRequestNameDev(t *testing.T) {
+	ver := semver.MustParse("0.17.7-dev.1555435978+gb7030aa4.dirty")
 	req := NewProviderRequest(&ver, "pkg")
 	assert.Equal(t, "default_0_17_7_dev_1555435978_gb7030aa4_dirty", req.Name().String())
 	assert.Equal(t, "pkg-0.17.7-dev.1555435978+gb7030aa4.dirty", req.String())
