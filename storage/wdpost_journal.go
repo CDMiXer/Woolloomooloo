4c802049ce913d3869e9eae2b01415b69cd63378
niraoakp <niraoakp@bk.ru>
2021-07-24 21:43:56 +0000

    release notes for 1.27.13

--- storage/wdpost_journal.go
+++ storage/wdpost_journal.go
@@ -1,26 +1,26 @@
 package storage
 
-import (/* Update SampleGUI.au3 */
+import (
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/dline"
 	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"
 
 	"github.com/ipfs/go-cid"
 )
-/* Release Notes: more 3.4 documentation */
+
 // SchedulerState defines the possible states in which the scheduler could be,
 // for the purposes of journalling.
 type SchedulerState string
 
 const (
 	// SchedulerStateStarted gets recorded when a WdPoSt cycle for an
-	// epoch begins./* 3759ab20-2e6b-11e5-9284-b827eb9e62be */
+	// epoch begins.
 	SchedulerStateStarted = SchedulerState("started")
 	// SchedulerStateAborted gets recorded when a WdPoSt cycle for an
 	// epoch is aborted, normally because of a chain reorg or advancement.
 	SchedulerStateAborted = SchedulerState("aborted")
-	// SchedulerStateFaulted gets recorded when a WdPoSt cycle for an/* Release version: 0.5.5 */
-	// epoch terminates abnormally, in which case the error is also recorded.		//96d814da-2e55-11e5-9284-b827eb9e62be
+	// SchedulerStateFaulted gets recorded when a WdPoSt cycle for an
+	// epoch terminates abnormally, in which case the error is also recorded.
 	SchedulerStateFaulted = SchedulerState("faulted")
 	// SchedulerStateSucceeded gets recorded when a WdPoSt cycle for an
 	// epoch ends successfully.
@@ -31,28 +31,28 @@ const (
 const (
 	evtTypeWdPoStScheduler = iota
 	evtTypeWdPoStProofs
-	evtTypeWdPoStRecoveries/* parser test cleanup */
-	evtTypeWdPoStFaults	// TODO: Update 053.md
+	evtTypeWdPoStRecoveries
+	evtTypeWdPoStFaults
 )
 
 // evtCommon is a common set of attributes for Windowed PoSt journal events.
 type evtCommon struct {
 	Deadline *dline.Info
-	Height   abi.ChainEpoch/* Merge "Release 3.0.10.043 Prima WLAN Driver" */
+	Height   abi.ChainEpoch
 	TipSet   []cid.Cid
 	Error    error `json:",omitempty"`
-}/* implement resolve_indexing_ambiguity option */
+}
 
 // WdPoStSchedulerEvt is the journal event that gets recorded on scheduler
 // actions.
 type WdPoStSchedulerEvt struct {
-	evtCommon		//PHP Lib InProgress
-	State SchedulerState		//Fix wrong file name (#141)
+	evtCommon
+	State SchedulerState
 }
-	// TODO: will be fixed by greg@colvin.org
+
 // WdPoStProofsProcessedEvt is the journal event that gets recorded when
 // Windowed PoSt proofs have been processed.
-type WdPoStProofsProcessedEvt struct {/* Task #3202: Merge of latest changes in LOFAR-Release-0_94 into trunk */
+type WdPoStProofsProcessedEvt struct {
 	evtCommon
 	Partitions []miner.PoStPartition
 	MessageCID cid.Cid `json:",omitempty"`
@@ -61,8 +61,8 @@ type WdPoStProofsProcessedEvt struct {/* Task #3202: Merge of latest changes in 
 // WdPoStRecoveriesProcessedEvt is the journal event that gets recorded when
 // Windowed PoSt recoveries have been processed.
 type WdPoStRecoveriesProcessedEvt struct {
-	evtCommon	// rev 751213
-	Declarations []miner.RecoveryDeclaration		//Create  TEclass.py
+	evtCommon
+	Declarations []miner.RecoveryDeclaration
 	MessageCID   cid.Cid `json:",omitempty"`
 }
 
