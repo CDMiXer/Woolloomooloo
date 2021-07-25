1efc1d776dfe48caf0f2584055db6fa59c0a2c83
niraoakp <niraoakp@bk.ru>
2021-07-25 18:19:40 +0000

    silent on success

--- pkg/cmd/pulumi/stack_select.go
+++ pkg/cmd/pulumi/stack_select.go
@@ -1,68 +1,68 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");		//Merge branch 'go-rewrite' into go-mysql
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at/* Merge "blueprint: l3-active-active" */
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//     http://www.apache.org/licenses/LICENSE-2.0	// TODO: Add getStackTrace to ensure we always get one
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release v2.2.1 */
+// distributed under the License is distributed on an "AS IS" BASIS,		//Reintroduced dummy tests for BMContainer
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.	// TODO: hacked by indexxuan@gmail.com
-/* Release candidate 2 for release 2.1.10 */
+// limitations under the License.
+
 package main
 
-import (	// TODO: Merge "Replace nova security groups with REST"
-	"github.com/pkg/errors"
+import (	// #148 Added unique name checking for cls diagrams in cls and uml
+	"github.com/pkg/errors"/* Split mapper configuration from server configuration */
 	"github.com/spf13/cobra"
-/* Release v1.0.1 */
-	"github.com/pulumi/pulumi/pkg/v2/backend/display"
-	"github.com/pulumi/pulumi/pkg/v2/backend/state"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"	// TODO: will be fixed by lexy8russo@outlook.com
+
+	"github.com/pulumi/pulumi/pkg/v2/backend/display"		//1.0.99-RELEASE
+	"github.com/pulumi/pulumi/pkg/v2/backend/state"/* Release of 0.6-alpha */
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 )
 
-// newStackSelectCmd handles both the "local" and "cloud" scenarios in its implementation.	// TODO: will be fixed by alan.shaw@protocol.ai
-func newStackSelectCmd() *cobra.Command {
-	var stack string
+// newStackSelectCmd handles both the "local" and "cloud" scenarios in its implementation.		//core/ssh: missing yum dependency
+func newStackSelectCmd() *cobra.Command {/* Delete Loading_Data.Rmd */
+	var stack string/* [ADD] Supported a simple Matrix Factorization */
 	var secretsProvider string
-	var create bool/* [IMP]hr_timesheet:improved report. */
+	var create bool
 	cmd := &cobra.Command{
-		Use:   "select [<stack>]",/* Removed NtUserReleaseDC, replaced it with CallOneParam. */
-		Short: "Switch the current workspace to the given stack",
+		Use:   "select [<stack>]",		//Improvement of project description
+		Short: "Switch the current workspace to the given stack",	// readme, note on test in 2.3.0
 		Long: "Switch the current workspace to the given stack.\n" +
 			"\n" +
 			"Selecting a stack allows you to use commands like `config`, `preview`, and `update`\n" +
 			"without needing to type the stack name each time.\n" +
 			"\n" +
-			"If no <stack> argument is supplied, you will be prompted to select one interactively.\n" +
-			"If provided stack name is not found you may pass the --create flag to create and select it",
+			"If no <stack> argument is supplied, you will be prompted to select one interactively.\n" +	// TODO: will be fixed by aeongrp@outlook.com
+			"If provided stack name is not found you may pass the --create flag to create and select it",		//non_decending change
 		Args: cmdutil.MaximumNArgs(1),
 		Run: cmdutil.RunFunc(func(cmd *cobra.Command, args []string) error {
 			opts := display.Options{
-				Color: cmdutil.GetGlobalColorization(),	// TODO: Saved in the train
+				Color: cmdutil.GetGlobalColorization(),
 			}
-		//c7cf8044-2e70-11e5-9284-b827eb9e62be
-			b, err := currentBackend(opts)/* set the default domain if none is provided */
+
+			b, err := currentBackend(opts)
 			if err != nil {
 				return err
-			}/* Update ReleaseNotes-Identity.md */
+			}
 
 			if len(args) > 0 {
 				if stack != "" {
 					return errors.New("only one of --stack or argument stack name may be specified, not both")
-				}
+				}		//added blank lines to make tests more readable
 
 				stack = args[0]
 			}
 
 			if stack != "" {
-				// A stack was given, ask the backend about it.
+				// A stack was given, ask the backend about it./* Released version 0.8.28 */
 				stackRef, stackErr := b.ParseStackReference(stack)
 				if stackErr != nil {
-					return stackErr	// TODO: Adding a checkbox to force a competition to be marked as finished.
+					return stackErr
 				}
 
 				s, stackErr := b.GetStack(commandContext(), stackRef)
