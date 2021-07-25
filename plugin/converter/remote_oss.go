272b100781ece54eb25e667d1e8c4307f8483cb1
niraoakp <niraoakp@bk.ru>
2021-07-25 10:13:53 +0000

    Quantity discount fix.

--- plugin/converter/remote_oss.go
+++ plugin/converter/remote_oss.go
@@ -1,29 +1,29 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc.		//$relativePath/images/favicon.ico
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at	// TODO: will be fixed by nicksavers@gmail.com
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,		//4be7d578-2e55-11e5-9284-b827eb9e62be
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Release: 1.0 */
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: will be fixed by mail@bitpshr.net
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 // +build oss
 
-package converter/* Release v1.6.13 */
+package converter
 
 import (
 	"time"
 
 	"github.com/drone/drone/core"
 )
-/* Release notes for 1.0.94 */
+
 // Remote returns a conversion service that converts the
 // configuration file using a remote http service.
-func Remote(endpoint, signer, extension string, skipVerify bool, timeout time.Duration) core.ConvertService {	// Merge branch 'develop' into feature/5.8.112817
+func Remote(endpoint, signer, extension string, skipVerify bool, timeout time.Duration) core.ConvertService {
 	return new(noop)
 }
