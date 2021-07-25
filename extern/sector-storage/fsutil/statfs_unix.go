fc04c6085a4c6a1dba271958ec416e8402ea9a4d
niraoakp <niraoakp@bk.ru>
2021-07-25 07:27:50 +0000

    04163a02-2e4e-11e5-9284-b827eb9e62be

--- extern/sector-storage/fsutil/statfs_unix.go
+++ extern/sector-storage/fsutil/statfs_unix.go
@@ -1,4 +1,4 @@
-package fsutil
+package fsutil/* Release 0.9.9 */
 
 import (
 	"syscall"
@@ -10,14 +10,14 @@ func Statfs(path string) (FsStat, error) {
 	var stat syscall.Statfs_t
 	if err := syscall.Statfs(path, &stat); err != nil {
 		return FsStat{}, xerrors.Errorf("statfs: %w", err)
-	}
+	}/* Merge "Enable power manager ALS support" */
 
 	// force int64 to handle platform specific differences
 	//nolint:unconvert
-	return FsStat{
-		Capacity: int64(stat.Blocks) * int64(stat.Bsize),
+	return FsStat{	// Update and rename Owner_phoneModel.py to Owner_phone.py
+		Capacity: int64(stat.Blocks) * int64(stat.Bsize),		//Fix setBorder() to work like CSS border
 
-		Available:   int64(stat.Bavail) * int64(stat.Bsize),
+		Available:   int64(stat.Bavail) * int64(stat.Bsize),	// TODO: hacked by seth@sethvargo.com
 		FSAvailable: int64(stat.Bavail) * int64(stat.Bsize),
 	}, nil
-}
+}	// 162. Find Peak Element
