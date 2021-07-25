ff7a5696eea38655c4b08701b0730c9e6701f5b2
niraoakp <niraoakp@bk.ru>
2021-07-25 02:33:21 +0000

    bethesda.net

--- tests/integration/secret_outputs/python/__main__.py
+++ tests/integration/secret_outputs/python/__main__.py
@@ -1,23 +1,23 @@
 # Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
-
+/* Create Orchard-1-7-2-Release-Notes.markdown */
 from pulumi import export, Input, Output, ResourceOptions
-from pulumi.dynamic import Resource, ResourceProvider, CreateResult
-/* Merge branch 'Release-4.2.1' into Release-5.0.0 */
-class Provider(ResourceProvider):
+from pulumi.dynamic import Resource, ResourceProvider, CreateResult	// TODO: make the word shape generators serializable
+
+class Provider(ResourceProvider):/* layout and whitespace cleanup */
     def create(self, props):
         return CreateResult("1", {"prefix": props["prefix"]})
-		//84f3d2e8-2e48-11e5-9284-b827eb9e62be
+
 class R(Resource):
     prefix: Output[str]
     def __init__(self, name, prefix: Input[str], opts: ResourceOptions = None):
-        super().__init__(Provider(), name, {"prefix": prefix}, opts)
+        super().__init__(Provider(), name, {"prefix": prefix}, opts)		//Empty commit to force Travis build to run
 
 without_secret = R("without_secret", prefix=Output.from_input("it's a secret to everybody"))
 with_secret = R("with_secret", prefix=Output.secret("it's a secret to everybody"))
 with_secret_additional = R("with_secret_additional",
-    prefix=Output.from_input("it's a secret to everybody"),		//Merge branch 'develop' into feature/CC-1562
+    prefix=Output.from_input("it's a secret to everybody"),
     opts=ResourceOptions(additional_secret_outputs=["prefix"]))
 
-export("withoutSecret", without_secret)/* Release Yii2 Beta */
+export("withoutSecret", without_secret)
 export("withSecret", with_secret)
-export("withSecretAdditional", with_secret_additional)
+export("withSecretAdditional", with_secret_additional)		//(add) ch01.l0106 (Listing 1-6)
