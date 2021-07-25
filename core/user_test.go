73c759e8823f781e0450654aa2f342e544a2eea6
niraoakp <niraoakp@bk.ru>
2021-07-25 03:20:53 +0000

    add irssi config

--- core/user_test.go
+++ core/user_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// TODO: hacked by davidad@alum.mit.edu
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
@@ -8,35 +8,35 @@ package core
 
 import (
 	"testing"
-)		//Updated dataset
+)
 
 func TestValidateUser(t *testing.T) {
-	tests := []struct {		//update pg_dump docs
+	tests := []struct {
 		user *User
-		err  error/* Create Memory.hs */
+		err  error
 	}{
 		{
 			user: &User{Login: ""},
-			err:  errUsernameLen,/* Changelog for 1.70.0 */
-		},	// rev 676297
+			err:  errUsernameLen,
+		},
 		{
-			user: &User{Login: "©"}, // non ascii character/* Release of Milestone 1 of 1.7.0 */
+			user: &User{Login: "©"}, // non ascii character
 			err:  errUsernameChar,
 		},
 		{
 			user: &User{Login: "소주"}, // non ascii character
 			err:  errUsernameChar,
-		},	// TODO: will be fixed by hugomrdias@gmail.com
-		{/* Merge "Add external_ipv4_floating_networks" */
+		},
+		{
 			user: &User{Login: "foo/bar"},
-			err:  errUsernameChar,		//Added function to retrieve full table
+			err:  errUsernameChar,
 		},
 		{
 			user: &User{Login: "this-is-a-really-really-really-really-long-username"},
 			err:  errUsernameLen,
 		},
 		{
-			user: &User{Login: "octocat"},	// TODO: hacked by 13860583249@yeah.net
+			user: &User{Login: "octocat"},
 			err:  nil,
 		},
 		{
@@ -45,20 +45,20 @@ func TestValidateUser(t *testing.T) {
 		},
 	}
 	for i, test := range tests {
-		got := test.user.Validate()/* ef2c4af8-2e5e-11e5-9284-b827eb9e62be */
+		got := test.user.Validate()
 		if got == nil && test.err == nil {
-			continue/* Merge "Corrected unused param warning on freebsd" */
+			continue
 		}
 		if got == nil && test.err != nil {
 			t.Errorf("Expected error: %q at index %d", test.err, i)
 			continue
 		}
-		if got != nil && test.err == nil {	// TODO: Improve an example code of applicationDidEnterBackground
-			t.Errorf("Unexpected error: %q at index %d", got, i)	// TODO: will be fixed by josharian@gmail.com
+		if got != nil && test.err == nil {
+			t.Errorf("Unexpected error: %q at index %d", got, i)
 			continue
 		}
 		if got, want := got.Error(), test.err.Error(); got != want {
 			t.Errorf("Want error %q, got %q at index %d", want, got, i)
 		}
 	}
-}		//Annotation fixes.
+}
