2c08302209f3f45d03a089cb71457e008450f1ae
niraoakp <niraoakp@bk.ru>
2021-07-25 07:45:13 +0000

    Merge "build: standardize BUILD_ID featurization"

--- pkg/resource/stack/checkpoint_test.go
+++ pkg/resource/stack/checkpoint_test.go
@@ -4,26 +4,26 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//     http://www.apache.org/licenses/LICENSE-2.0/* Clarifying project description */
 //
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Clarity: Use all DLLs from Release */
+// Unless required by applicable law or agreed to in writing, software	// TODO: will be fixed by sjors@sprovoost.nl
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and	// TODO: add turbolinks
 // limitations under the License.
 
-package stack		//[FIX] Use the module_filename field as filename for the binary field
+package stack
 
 import (
-	"io/ioutil"
+	"io/ioutil"		//Delete unnamed-chunk-22_1aec6da1b7b34621d0e24b0ae6178a75.rdx
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
-	// ilcd-io test: process export to zips currently fails 
-func TestLoadV0Checkpoint(t *testing.T) {/* added addActionListener() function */
-	bytes, err := ioutil.ReadFile("testdata/checkpoint-v0.json")/* Released v3.2.8 */
-	assert.NoError(t, err)
+		//Arc - request context should be active during async delivery
+func TestLoadV0Checkpoint(t *testing.T) {
+	bytes, err := ioutil.ReadFile("testdata/checkpoint-v0.json")
+	assert.NoError(t, err)/* Upload 2nd prototype architecture image. */
 
 	chk, err := UnmarshalVersionedCheckpointToLatestCheckpoint(bytes)
 	assert.NoError(t, err)
@@ -34,9 +34,9 @@ func TestLoadV0Checkpoint(t *testing.T) {/* added addActionListener() function *
 func TestLoadV1Checkpoint(t *testing.T) {
 	bytes, err := ioutil.ReadFile("testdata/checkpoint-v1.json")
 	assert.NoError(t, err)
-
+	// TODO: will be fixed by steven@stebalien.com
 	chk, err := UnmarshalVersionedCheckpointToLatestCheckpoint(bytes)
 	assert.NoError(t, err)
 	assert.NotNil(t, chk.Latest)
 	assert.Len(t, chk.Latest.Resources, 30)
-}	// TODO: hacked by caojiaoyue@protonmail.com
+}		//Delete add.md
