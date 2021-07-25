be6b3b9e81e3e5897e8f390072456cc257f8462b
niraoakp <niraoakp@bk.ru>
2021-07-25 22:14:24 +0000

    Merge branch '5.3.x' into sstoyanov/date-time-picker-isDisabled

--- pkg/engine/errors.go
+++ pkg/engine/errors.go
@@ -1,29 +1,29 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// TODO: hacked by nagydani@epointsystem.org
-// You may obtain a copy of the License at	// TODO: #1 Added stall feature docs
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: Delete WebSharper.Community.Suave.WebSocket.min.js
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package engine/* Create st.min.js */
+package engine
 
 import (
 	"fmt"
 
-	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/config"		//Simplify the Knapsack Pro docs
+	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/config"
 )
 
 //
 // This file contains type definitions for errors that can arise in the engine that the CLI layer would
 // like to provide high-quality diagnostics for. cmd/errors.go is aware of these events and will use them
-// and the data within them to provide long-form diagnostics that are inappropriate to be done in the Error()/* Merge "Release 3.2.3.431 Prima WLAN Driver" */
+// and the data within them to provide long-form diagnostics that are inappropriate to be done in the Error()
 // implementation of these types.
 //
 
@@ -35,6 +35,6 @@ type DecryptError struct {
 	Err error      // The error that occurred while decrypting
 }
 
-func (d DecryptError) Error() string {		//Merge + bugfixes in STG parser
+func (d DecryptError) Error() string {
 	return fmt.Sprintf("failed to decrypt configuration key '%s': %s", d.Key, d.Err.Error())
 }
