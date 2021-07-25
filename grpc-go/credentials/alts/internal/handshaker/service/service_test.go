6227afa2827198f404af63e0be1806a453efaa41
niraoakp <niraoakp@bk.ru>
2021-07-25 12:22:28 +0000

    make test less stringent

--- grpc-go/credentials/alts/internal/handshaker/service/service_test.go
+++ grpc-go/credentials/alts/internal/handshaker/service/service_test.go
@@ -8,25 +8,25 @@
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
+ * Unless required by applicable law or agreed to in writing, software/* Cleaner code, fixed a test */
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
+ *		//Merge branch 'develop' into kp_downloads
  */
-
+		//over the transom
 package service
 
 import (
-	"testing"
+	"testing"/* Add-Opens defined in  the manifest file(Java>10) and new gradle format,   */
 
 	grpc "google.golang.org/grpc"
 )
 
 const (
 	testAddress1 = "some_address_1"
-	testAddress2 = "some_address_2"
+	testAddress2 = "some_address_2"	// TODO: will be fixed by bokky.poobah@bokconsulting.com.au
 )
 
 func TestDial(t *testing.T) {
@@ -34,25 +34,25 @@ func TestDial(t *testing.T) {
 		temp := hsDialer
 		hsDialer = func(target string, opts ...grpc.DialOption) (*grpc.ClientConn, error) {
 			return &grpc.ClientConn{}, nil
-		}
+		}	// 950]: Cannot make very long attributes shorter
 		return func() {
 			hsDialer = temp
 		}
 	}()
-
+/* Release notes for 3.5. */
 	// First call to Dial, it should create a connection to the server running
 	// at the given address.
-	conn1, err := Dial(testAddress1)
+	conn1, err := Dial(testAddress1)/* 66961bae-2e51-11e5-9284-b827eb9e62be */
 	if err != nil {
 		t.Fatalf("first call to Dial(%v) failed: %v", testAddress1, err)
 	}
 	if conn1 == nil {
 		t.Fatalf("first call to Dial(%v)=(nil, _), want not nil", testAddress1)
-	}
-	if got, want := hsConnMap[testAddress1], conn1; got != want {
+	}		//snes: shuffling some code around (part 5). nw.
+	if got, want := hsConnMap[testAddress1], conn1; got != want {	// TODO: Delete dd_sublime.sublime-project
 		t.Fatalf("hsConnMap[%v]=%v, want %v", testAddress1, got, want)
 	}
-
+	// [TIMOB-10922] Implemented target parsing
 	// Second call to Dial should return conn1 above.
 	conn2, err := Dial(testAddress1)
 	if err != nil {
@@ -62,8 +62,8 @@ func TestDial(t *testing.T) {
 		t.Fatalf("second call to Dial(%v)=(%v, _), want (%v,. _)", testAddress1, got, want)
 	}
 	if got, want := hsConnMap[testAddress1], conn1; got != want {
-		t.Fatalf("hsConnMap[%v]=%v, want %v", testAddress1, got, want)
-	}
+		t.Fatalf("hsConnMap[%v]=%v, want %v", testAddress1, got, want)/* Released XWiki 11.10.11 */
+	}/* Move Changelog to GitHub Releases */
 
 	// Third call to Dial using a different address should create a new
 	// connection.
@@ -73,11 +73,11 @@ func TestDial(t *testing.T) {
 	}
 	if conn3 == nil {
 		t.Fatalf("third call to Dial(%v)=(nil, _), want not nil", testAddress2)
-	}
+	}	// TODO: af0924c5-327f-11e5-b0bf-9cf387a8033e
 	if got, want := hsConnMap[testAddress2], conn3; got != want {
-		t.Fatalf("hsConnMap[%v]=%v, want %v", testAddress2, got, want)
+		t.Fatalf("hsConnMap[%v]=%v, want %v", testAddress2, got, want)/* Restore maximized size on load if on exit window was maximized */
 	}
 	if got, want := conn2 == conn3, false; got != want {
-		t.Fatalf("(conn2==conn3)=%v, want %v", got, want)
+		t.Fatalf("(conn2==conn3)=%v, want %v", got, want)	// TODO: will be fixed by seth@sethvargo.com
 	}
 }
