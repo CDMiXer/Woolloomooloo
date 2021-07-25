f9f282d0ce3a20c5b3503c75d336ebc0ef1a1e71
niraoakp <niraoakp@bk.ru>
2021-07-25 03:30:30 +0000

    Add node-style (#9)

--- handler/api/system/limits.go
+++ handler/api/system/limits.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file.	// TODO: Create select-replication-commands-history.sql
 
-// +build !oss		//Adds contact details
+// +build !oss
 
 package system
