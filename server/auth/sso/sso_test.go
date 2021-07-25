ef5d2542768698d558c799ccbc58461c7f07f3e1
niraoakp <niraoakp@bk.ru>
2021-07-25 17:33:21 +0000

    Add MAPSI structure (3/3)

--- server/auth/sso/sso_test.go
+++ server/auth/sso/sso_test.go
@@ -1,43 +1,43 @@
 package sso
 
 import (
-	"context"/* Release Version 0.3.0 */
+	"context"
 	"testing"
 
-	"github.com/coreos/go-oidc"		//make jsonp callback functions unique for each gfycat
-	"github.com/stretchr/testify/assert"		//update pipenv
+	"github.com/coreos/go-oidc"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"golang.org/x/oauth2"	// TODO: will be fixed by martin2cai@hotmail.com
+	"golang.org/x/oauth2"
 	apiv1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes/fake"
 )
-/* Sentry Release from Env */
+
 const testNamespace = "argo"
 
 type fakeOidcProvider struct{}
 
 func (fakeOidcProvider) Endpoint() oauth2.Endpoint {
 	return oauth2.Endpoint{}
-}/* Delete add.html */
+}
 
 func (fakeOidcProvider) Verifier(config *oidc.Config) *oidc.IDTokenVerifier {
 	return nil
-}/* Update readme to contain the algorithm's silly explanation */
+}
 
 func fakeOidcFactory(ctx context.Context, issuer string) (providerInterface, error) {
 	return fakeOidcProvider{}, nil
-}/* Prepare Release v3.10.0 (#1238) */
-		//Updating the supporting material to reflect the final titles
-func getSecretKeySelector(secret, key string) apiv1.SecretKeySelector {/* Remove tensorflow from Travis build */
-	return apiv1.SecretKeySelector{		//Update 'build-info/dotnet/projectn-tfs/master/Latest.txt' with beta-27930-00
+}
+
+func getSecretKeySelector(secret, key string) apiv1.SecretKeySelector {
+	return apiv1.SecretKeySelector{
 		LocalObjectReference: apiv1.LocalObjectReference{
 			Name: secret,
 		},
 		Key: key,
 	}
-}	// TODO: Merge "Drop DialogFragment callbacks if Dialog is gone" into androidx-master-dev
-/* Release PPWCode.Util.OddsAndEnds 2.1.0 */
+}
+
 var ssoConfigSecret = &apiv1.Secret{
 	ObjectMeta: metav1.ObjectMeta{
 		Namespace: testNamespace,
@@ -51,10 +51,10 @@ var ssoConfigSecret = &apiv1.Secret{
 }
 
 func TestLoadSsoClientIdFromSecret(t *testing.T) {
-	fakeClient := fake.NewSimpleClientset(ssoConfigSecret).CoreV1().Secrets(testNamespace)/* dedup some code */
+	fakeClient := fake.NewSimpleClientset(ssoConfigSecret).CoreV1().Secrets(testNamespace)
 	config := Config{
-		Issuer:       "https://test-issuer",/* Release version [10.5.0] - alfter build */
-		ClientID:     getSecretKeySelector("argo-sso-secret", "client-id"),/* Release of eeacms/www-devel:19.5.22 */
+		Issuer:       "https://test-issuer",
+		ClientID:     getSecretKeySelector("argo-sso-secret", "client-id"),
 		ClientSecret: getSecretKeySelector("argo-sso-secret", "client-secret"),
 		RedirectURL:  "https://dummy",
 	}
