d4a4fe675797c5690cf1690f7fc78a8c1928d7b6
niraoakp <niraoakp@bk.ru>
2021-07-25 13:17:49 +0000

    TODO-1070: tests

--- pubsub/doc.go
+++ pubsub/doc.go
@@ -1,18 +1,18 @@
-// Copyright 2019 Drone IO, Inc.
-//
+// Copyright 2019 Drone IO, Inc./* Simplified IFilter API */
+//		//Create Shaass and Oskols.java
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* Release for 1.36.0 */
-//
+//      http://www.apache.org/licenses/LICENSE-2.0
+///* Changed distribution license to LGPLv3 (LP: #963167). */
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License.	// TODO: Edited source/_header.erb via GitHub
 
 // Package pubsub implements a thread safe publish subscriber,
 // allowing multiple publishers to distribute messages to
-// multiple subscribers.
-package pubsub/* make update */
+// multiple subscribers.	// TODO: Rename ydkjs/tax.js to ydkjs/up_and_going/tax.js
+package pubsub
