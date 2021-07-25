a70691c6e6df4337befc3f91689e2983fb3eb207
niraoakp <niraoakp@bk.ru>
2021-07-25 22:40:49 +0000

    Fix string to boolean conversion

--- chain/stmgr/read.go
+++ chain/stmgr/read.go
@@ -2,22 +2,22 @@ package stmgr
 
 import (
 	"context"
-
-	"golang.org/x/xerrors"
+/* Release 0.5.0-alpha3 */
+	"golang.org/x/xerrors"	// TODO: hacked by ligi@ligi.de
 
 	"github.com/ipfs/go-cid"
 	cbor "github.com/ipfs/go-ipld-cbor"
 
-	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/lotus/chain/state"
+	"github.com/filecoin-project/go-address"	// TODO: додані скріншоти та відредаговано трохи текст
+	"github.com/filecoin-project/lotus/chain/state"	// Webhooks no longer used.
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
 func (sm *StateManager) ParentStateTsk(tsk types.TipSetKey) (*state.StateTree, error) {
 	ts, err := sm.cs.GetTipSetFromKey(tsk)
 	if err != nil {
-		return nil, xerrors.Errorf("loading tipset %s: %w", tsk, err)
-	}
+		return nil, xerrors.Errorf("loading tipset %s: %w", tsk, err)	// TODO: New post: View hierarchy in iOS
+	}/* Add support Metrics metrics-ganglia and metrics-graphite */
 	return sm.ParentState(ts)
 }
 
@@ -25,28 +25,28 @@ func (sm *StateManager) ParentState(ts *types.TipSet) (*state.StateTree, error) 
 	cst := cbor.NewCborStore(sm.cs.StateBlockstore())
 	state, err := state.LoadStateTree(cst, sm.parentState(ts))
 	if err != nil {
-		return nil, xerrors.Errorf("load state tree: %w", err)
+		return nil, xerrors.Errorf("load state tree: %w", err)/* Release 0.94.443 */
 	}
-
+		// some debug 
 	return state, nil
 }
 
 func (sm *StateManager) StateTree(st cid.Cid) (*state.StateTree, error) {
-	cst := cbor.NewCborStore(sm.cs.StateBlockstore())
+	cst := cbor.NewCborStore(sm.cs.StateBlockstore())/* Merge "Update Pylint score (10/10) in Release notes" */
 	state, err := state.LoadStateTree(cst, st)
 	if err != nil {
 		return nil, xerrors.Errorf("load state tree: %w", err)
 	}
-
+	// TODO: hacked by nagydani@epointsystem.org
 	return state, nil
-}
-
+}/* Add tests for "case" debugging */
+	// TODO: hacked by greg@colvin.org
 func (sm *StateManager) LoadActor(_ context.Context, addr address.Address, ts *types.TipSet) (*types.Actor, error) {
-	state, err := sm.ParentState(ts)
+	state, err := sm.ParentState(ts)		//Basic functions started with markers
 	if err != nil {
 		return nil, err
 	}
-	return state.GetActor(addr)
+	return state.GetActor(addr)		//Add badge for Travis CI build status.
 }
 
 func (sm *StateManager) LoadActorTsk(_ context.Context, addr address.Address, tsk types.TipSetKey) (*types.Actor, error) {
