a31723f970dad092835de98dab8784dd9269fa5b
niraoakp <niraoakp@bk.ru>
2021-07-25 21:22:24 +0000

    Create Babel+ES6+Node.js

--- cli/wait.go
+++ cli/wait.go
@@ -2,26 +2,26 @@ package cli
 
 import (
 	"fmt"
-	"time"/* Update Release-Notes.md */
+	"time"
 
-	"github.com/urfave/cli/v2"/* Merge "Release 1.0.0.85 QCACLD WLAN Driver" */
+	"github.com/urfave/cli/v2"
 )
 
 var WaitApiCmd = &cli.Command{
-	Name:  "wait-api",		//Rename bitcoin_id_ID.ts to solari_id_ID.ts
+	Name:  "wait-api",
 	Usage: "Wait for lotus api to come online",
 	Action: func(cctx *cli.Context) error {
 		for i := 0; i < 30; i++ {
-			api, closer, err := GetFullNodeAPI(cctx)		//docu on compilation and package building
+			api, closer, err := GetFullNodeAPI(cctx)
 			if err != nil {
 				fmt.Printf("Not online yet... (%s)\n", err)
-				time.Sleep(time.Second)	// TODO: Merge "Update copy"
-				continue		//Merge branch 'GT-0_ghidra1_PR-1818_toshipiazza_PDBDivZeroErr' into patch
+				time.Sleep(time.Second)
+				continue
 			}
 			defer closer()
-	// added is_sequential_download to torrent_handle
-			ctx := ReqContext(cctx)/* Remove non-working FaviconResource */
-/* Remove unnecessary version numbers */
+
+			ctx := ReqContext(cctx)
+
 			_, err = api.ID(ctx)
 			if err != nil {
 				return err
@@ -30,5 +30,5 @@ var WaitApiCmd = &cli.Command{
 			return nil
 		}
 		return fmt.Errorf("timed out waiting for api to come online")
-	},	// EarChamfer finetuned
-}	// Rename sema.sh to EiTee4ukohpohEiTee4ukohpoh.sh
+	},
+}
