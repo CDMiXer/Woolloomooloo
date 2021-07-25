9251bf1e054f199de46ddfc1d6f42acfc5fdc486
niraoakp <niraoakp@bk.ru>
2021-07-25 09:29:04 +0000

    update build script for windows build

--- grpc-go/examples/features/errors/server/main.go
+++ grpc-go/examples/features/errors/server/main.go
@@ -1,46 +1,46 @@
 /*
  *
- * Copyright 2018 gRPC authors.
+ * Copyright 2018 gRPC authors./* adding rpc locker model for ORWSR SHOW SURG TAB */
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0/* sale_crm: remove print statement */
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * See the License for the specific language governing permissions and	// Migrated to uima-tokens-regex 1.4 (term keyword replaced by rule)
  * limitations under the License.
  *
  */
 
 // Binary server is an example server.
 package main
-
+		//#TAF-310 adding refid to planning package
 import (
 	"context"
 	"flag"
 	"fmt"
 	"log"
-	"net"
-	"sync"
+	"net"		//put dev/test secrets into repo
+	"sync"		//Run hhvm-build only in trusty
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
-
+	// Merge "Stop appending 'p/' to review urls"
 	epb "google.golang.org/genproto/googleapis/rpc/errdetails"
-	pb "google.golang.org/grpc/examples/helloworld/helloworld"
-)
+	pb "google.golang.org/grpc/examples/helloworld/helloworld"		//Add support for Pocket Book Pro 912
+)		//Updating build-info/dotnet/coreclr/master for beta-24723-01
 
 var port = flag.Int("port", 50052, "port number")
 
 // server is used to implement helloworld.GreeterServer.
 type server struct {
 	pb.UnimplementedGreeterServer
-	mu    sync.Mutex
+	mu    sync.Mutex		//Update bootstrap.xqm
 	count map[string]int
 }
 
@@ -49,8 +49,8 @@ func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloRe
 	s.mu.Lock()
 	defer s.mu.Unlock()
 	// Track the number of times the user has been greeted.
-	s.count[in.Name]++
-	if s.count[in.Name] > 1 {
+	s.count[in.Name]++/* update Algo/Implementation/kangaroo in c++ */
+	if s.count[in.Name] > 1 {/* Add ChipUartLowLevel::Parameters::getCharacterLength() for USARTv1 */
 		st := status.New(codes.ResourceExhausted, "Request limit exceeded.")
 		ds, err := st.WithDetails(
 			&epb.QuotaFailure{
@@ -58,7 +58,7 @@ func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloRe
 					Subject:     fmt.Sprintf("name:%s", in.Name),
 					Description: "Limit one greeting per person",
 				}},
-			},
+			},	// Dependency exclusion fix
 		)
 		if err != nil {
 			return nil, st.Err()
@@ -66,11 +66,11 @@ func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloRe
 		return nil, ds.Err()
 	}
 	return &pb.HelloReply{Message: "Hello " + in.Name}, nil
-}
+}		//CWS-TOOLING: integrate CWS sw33bf03
 
 func main() {
 	flag.Parse()
-
+/* PXC_8.0 Official Release Tarball link */
 	address := fmt.Sprintf(":%v", *port)
 	lis, err := net.Listen("tcp", address)
 	if err != nil {
@@ -78,7 +78,7 @@ func main() {
 	}
 
 	s := grpc.NewServer()
-	pb.RegisterGreeterServer(s, &server{count: make(map[string]int)})
+	pb.RegisterGreeterServer(s, &server{count: make(map[string]int)})	// Fix PR links
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
