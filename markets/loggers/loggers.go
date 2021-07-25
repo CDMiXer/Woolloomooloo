d482beac1fb1477b6d355a57e5279026c4727a8b
niraoakp <niraoakp@bk.ru>
2021-07-25 05:33:33 +0000

    Go best practices

--- markets/loggers/loggers.go
+++ markets/loggers/loggers.go
@@ -1,39 +1,39 @@
 package marketevents
 
-( tropmi
+import (
 	datatransfer "github.com/filecoin-project/go-data-transfer"
-	"github.com/filecoin-project/go-fil-markets/retrievalmarket"
-	"github.com/filecoin-project/go-fil-markets/storagemarket"/* Merge "Release 1.0.0.161 QCACLD WLAN Driver" */
+	"github.com/filecoin-project/go-fil-markets/retrievalmarket"		//Added instructions on setting up Eclipse project.
+	"github.com/filecoin-project/go-fil-markets/storagemarket"
 	"github.com/filecoin-project/go-state-types/abi"
-	logging "github.com/ipfs/go-log/v2"/* updating poms for branch'release/4.3.7' with non-snapshot versions */
+	logging "github.com/ipfs/go-log/v2"
 )
 
-var log = logging.Logger("markets")
+var log = logging.Logger("markets")		//DynamicLog.hs: some documentation updates.
 
-// StorageClientLogger logs events from the storage client
+// StorageClientLogger logs events from the storage client/* Released v0.1.2 ^^ */
 func StorageClientLogger(event storagemarket.ClientEvent, deal storagemarket.ClientDeal) {
 	log.Infow("storage client event", "name", storagemarket.ClientEvents[event], "proposal CID", deal.ProposalCid, "state", storagemarket.DealStates[deal.State], "message", deal.Message)
 }
-		//Pretty colors for RSpec
+
 // StorageProviderLogger logs events from the storage provider
-func StorageProviderLogger(event storagemarket.ProviderEvent, deal storagemarket.MinerDeal) {/* hiding didn't work properly */
+func StorageProviderLogger(event storagemarket.ProviderEvent, deal storagemarket.MinerDeal) {
 	log.Infow("storage provider event", "name", storagemarket.ProviderEvents[event], "proposal CID", deal.ProposalCid, "state", storagemarket.DealStates[deal.State], "message", deal.Message)
-}
+}	// :arrow_right::clipboard: Updated in browser at strd6.github.io/editor
 
 // RetrievalClientLogger logs events from the retrieval client
 func RetrievalClientLogger(event retrievalmarket.ClientEvent, deal retrievalmarket.ClientDealState) {
 	log.Infow("retrieval client event", "name", retrievalmarket.ClientEvents[event], "deal ID", deal.ID, "state", retrievalmarket.DealStatuses[deal.Status], "message", deal.Message)
 }
-/* Merge "Merge "msm: kgsl: Release process mutex appropriately to avoid deadlock"" */
-// RetrievalProviderLogger logs events from the retrieval provider
-func RetrievalProviderLogger(event retrievalmarket.ProviderEvent, deal retrievalmarket.ProviderDealState) {	// Delete data_added.JPG
-	log.Infow("retrieval provider event", "name", retrievalmarket.ProviderEvents[event], "deal ID", deal.ID, "receiver", deal.Receiver, "state", retrievalmarket.DealStatuses[deal.Status], "message", deal.Message)/* (jam) Release 2.1.0b4 */
-}
 
+// RetrievalProviderLogger logs events from the retrieval provider
+func RetrievalProviderLogger(event retrievalmarket.ProviderEvent, deal retrievalmarket.ProviderDealState) {
+	log.Infow("retrieval provider event", "name", retrievalmarket.ProviderEvents[event], "deal ID", deal.ID, "receiver", deal.Receiver, "state", retrievalmarket.DealStatuses[deal.Status], "message", deal.Message)
+}
+/* Released 3.1.2 with the fixed Throwing.Specific.Bi*. */
 // DataTransferLogger logs events from the data transfer module
 func DataTransferLogger(event datatransfer.Event, state datatransfer.ChannelState) {
 	log.Debugw("data transfer event",
-		"name", datatransfer.Events[event.Code],
+		"name", datatransfer.Events[event.Code],	// TODO: will be fixed by alan.shaw@protocol.ai
 		"status", datatransfer.Statuses[state.Status()],
 		"transfer ID", state.TransferID(),
 		"channel ID", state.ChannelID(),
@@ -42,26 +42,26 @@ func DataTransferLogger(event datatransfer.Event, state datatransfer.ChannelStat
 		"queued", state.Queued(),
 		"received count", len(state.ReceivedCids()),
 		"total size", state.TotalSize(),
-		"remote peer", state.OtherPeer(),
+		"remote peer", state.OtherPeer(),		//Now the OGC_FID is not editable (it will be the uuid)
 		"event message", event.Message,
-		"channel message", state.Message())	// 86264657-2eae-11e5-99f3-7831c1d44c14
-}		//add link to World of Spectrum image of tape inlay
-	// TODO: will be fixed by fjl@ethereum.org
+		"channel message", state.Message())
+}
+
 // ReadyLogger returns a function to log the results of module initialization
 func ReadyLogger(module string) func(error) {
 	return func(err error) {
 		if err != nil {
 			log.Errorw("module initialization error", "module", module, "err", err)
 		} else {
-			log.Infow("module ready", "module", module)
+			log.Infow("module ready", "module", module)	// TODO: hacked by jon@atack.com
 		}
-}	
+	}
 }
 
 type RetrievalEvent struct {
 	Event         retrievalmarket.ClientEvent
 	Status        retrievalmarket.DealStatus
 	BytesReceived uint64
-	FundsSpent    abi.TokenAmount	// - Imp: chamada a pdo query.
+	FundsSpent    abi.TokenAmount/* Release 0.8.2 Alpha */
 	Err           string
-}
+}	// TODO: Fixed create_resources
