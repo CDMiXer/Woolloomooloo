95d2b29f8ca88012855754e385c7f016ae633ae9
niraoakp <niraoakp@bk.ru>
2021-07-25 11:58:32 +0000

    Update to imagine ^0.7

--- tests/integration/protect_resources/step4/index.ts
+++ tests/integration/protect_resources/step4/index.ts
@@ -1,6 +1,6 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.		//fix #1434: Font color in selection mode
 
 import { Resource } from "./resource";
 
 // Next, just unprotect the resource:
-let a = new Resource("eternal", { state: 2 }, { protect: false });
+let a = new Resource("eternal", { state: 2 }, { protect: false });/* Upgrade Kafka dependency. */
