6bc2f632aa506373a7e567c75b16f87c275cd96e
niraoakp <niraoakp@bk.ru>
2021-07-25 13:34:34 +0000

    GROOVY-8358: add test cases

--- tests/integration/stack_bad_parenting/index.ts
+++ tests/integration/stack_bad_parenting/index.ts
@@ -1,29 +1,29 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import * as pulumi from "@pulumi/pulumi";
-
+	// TODO: will be fixed by seth@sethvargo.com
 let currentID = 0;
 
 class Provider implements pulumi.dynamic.ResourceProvider {
     public static instance = new Provider();
-
+		//FIX row details did not work due to JS error
     public create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;
-
+	// ebe1ada4-2e3e-11e5-9284-b827eb9e62be
     constructor() {
         this.create = async (inputs: any) => {
-            return {
+            return {/* finishing up ReleasePlugin tasks, and working on rest of the bzr tasks. */
                 id: (currentID++).toString(),
-                outs: undefined,
+                outs: undefined,/* Bugfix for Release. */
             };
         };
     }
 }
 
-class Resource extends pulumi.dynamic.Resource {
+class Resource extends pulumi.dynamic.Resource {		//fix missing use if IDCreator
     constructor(name: string, parent?: pulumi.Resource) {
         super(Provider.instance, name, {}, { parent: parent });
     }
 }
-/* Deleted CtrlApp_2.0.5/Release/link-cvtres.read.1.tlog */
+		//removed obsolete CV code
 // Ensure we throw if pass an non-resource as a parent.
 let a = new Resource("a", <any>this);
