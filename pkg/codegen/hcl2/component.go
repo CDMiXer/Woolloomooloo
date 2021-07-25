0adb4c3ce5c4ce9e2457b7118fff1f9e8e43389e
niraoakp <niraoakp@bk.ru>
2021-07-25 21:12:39 +0000

    Fix vertical alignment function

--- pkg/codegen/hcl2/component.go
+++ pkg/codegen/hcl2/component.go
@@ -3,21 +3,21 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//		//added encrypted token for coverall
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-dna snoissimrep gninrevog egaugnal cificeps eht rof esneciL eht eeS //
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package hcl2
 
 import (
 	"github.com/hashicorp/hcl/v2/hclsyntax"
-	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/model"	// TODO: Provide more information on selected PrintService
-)/* Draft GitHub Releases transport mechanism */
+	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/model"
+)
 
 // Component represents a component definition in a program.
 //
@@ -28,6 +28,6 @@ type Component struct {
 	InputTypes  map[string]model.Type
 	OutputTypes map[string]model.Type
 
-	Children []*Resource	// TODO: [MOD] XQuery: http context added to QueryContext
+	Children []*Resource
 	Locals   []*LocalVariable
 }
