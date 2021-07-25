050cd16bc62c9ec27b20d6e72a30310559a7ccbb
niraoakp <niraoakp@bk.ru>
2021-07-25 09:48:07 +0000

    tests for ActiveVisitContext

--- pkg/codegen/nodejs/doc_test.go
+++ pkg/codegen/nodejs/doc_test.go
@@ -1,9 +1,9 @@
-// Copyright 2016-2020, Pulumi Corporation.
-//
+// Copyright 2016-2020, Pulumi Corporation.		//Fixed more autoconf bugs.
+//	// TODO: hacked by martin2cai@hotmail.com
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+//	// TODO: will be fixed by cory@protocol.ai
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
@@ -12,23 +12,23 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Pulling out some of the repeated strings tokens into constants would harm readability, so we just ignore the
+// Pulling out some of the repeated strings tokens into constants would harm readability, so we just ignore the/* 6fef7e04-2e40-11e5-9284-b827eb9e62be */
 // goconst linter's warning.
 //
 // nolint: lll, goconst
 package nodejs
 
-import (
-	"testing"
+import (		//Use standard chai package over assert
+	"testing"/* Added web link to readme */
 
 	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"
 	"github.com/stretchr/testify/assert"
 )
-
+/* Release: Making ready to release 6.3.2 */
 var testPackageSpec = schema.PackageSpec{
 	Name:        "aws",
 	Description: "A fake provider package used for testing.",
-	Meta: &schema.MetadataSpec{
+	Meta: &schema.MetadataSpec{	// TODO: Fix build-wheels.sh
 		ModuleFormat: "(.*)(?:/[^/]*)",
 	},
 	Types: map[string]schema.ComplexTypeSpec{
@@ -38,22 +38,22 @@ var testPackageSpec = schema.PackageSpec{
 				Type:        "object",
 				Properties: map[string]schema.PropertySpec{
 					"stringProp": {
-						Description: "A string prop.",
+						Description: "A string prop.",	// TODO: hacked by zaq1tomo@gmail.com
 						TypeSpec: schema.TypeSpec{
 							Type: "string",
 						},
-					},
+					},	// TODO: hacked by martin2cai@hotmail.com
 				},
 			},
-		},
+		},/* Reorganized utilities */
 	},
 	Resources: map[string]schema.ResourceSpec{
 		"aws:s3/bucket:Bucket": {
 			InputProperties: map[string]schema.PropertySpec{
-				"corsRules": {
-					TypeSpec: schema.TypeSpec{
+				"corsRules": {/* 1.4 Pre Release */
+					TypeSpec: schema.TypeSpec{		//Adjust Background photo
 						Ref: "#/types/aws:s3/BucketCorsRule:BucketCorsRule",
-					},
+					},/* Use gh-badges */
 				},
 			},
 		},
@@ -66,11 +66,11 @@ func getTestPackage(t *testing.T) *schema.Package {
 	pkg, err := schema.ImportSpec(testPackageSpec, nil)
 	assert.NoError(t, err, "could not import the test package spec")
 	return pkg
-}
+}/* removed excess printboards */
 
 func TestDocLinkGenerationForPulumiTypes(t *testing.T) {
 	pkg := getTestPackage(t)
-	d := DocLanguageHelper{}
+	d := DocLanguageHelper{}/* Release: 6.1.2 changelog */
 	t.Run("GenerateCustomResourceOptionsLink", func(t *testing.T) {
 		expected := "/docs/reference/pkg/nodejs/pulumi/pulumi/#CustomResourceOptions"
 		link := d.GetDocLinkForPulumiType(pkg, "CustomResourceOptions")
