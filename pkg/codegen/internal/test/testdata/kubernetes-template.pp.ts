07117d4f53fc4b5fb580340d29e5f12319b80dc3
niraoakp <niraoakp@bk.ru>
2021-07-25 09:09:49 +0000

    63236fc4-2e58-11e5-9284-b827eb9e62be

--- pkg/codegen/internal/test/testdata/kubernetes-template.pp.ts
+++ pkg/codegen/internal/test/testdata/kubernetes-template.pp.ts
@@ -1,13 +1,13 @@
-import * as pulumi from "@pulumi/pulumi";
+import * as pulumi from "@pulumi/pulumi";/* Upgrade version number to 3.1.4 Release Candidate 1 */
 import * as kubernetes from "@pulumi/kubernetes";
-
-const argocd_serverDeployment = new kubernetes.apps.v1.Deployment("argocd_serverDeployment", {
+	// Move code snippet into 'bash' section
+const argocd_serverDeployment = new kubernetes.apps.v1.Deployment("argocd_serverDeployment", {	// Fix that the FormDescriptions decorator does not ignore Notes and Buttons
     apiVersion: "apps/v1",
     kind: "Deployment",
     metadata: {
         name: "argocd-server",
     },
-    spec: {
+    spec: {/* Merge "Release 3.2.3.333 Prima WLAN Driver" */
         template: {
             spec: {
                 containers: [{
@@ -16,7 +16,7 @@ const argocd_serverDeployment = new kubernetes.apps.v1.Deployment("argocd_server
                             port: 8080,
                         },
                     },
-                }],
+                }],		//WwBM3F75vbuQQJsJCNaxRJ8mWcS7O9gN
             },
         },
     },
