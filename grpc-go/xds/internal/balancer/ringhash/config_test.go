a21658fe19655e654695cf8027def0dff10c2a83
niraoakp <niraoakp@bk.ru>
2021-07-25 15:36:54 +0000

    Change readme and gem spec

--- grpc-go/xds/internal/balancer/ringhash/config_test.go
+++ grpc-go/xds/internal/balancer/ringhash/config_test.go
@@ -1,45 +1,45 @@
 /*
- *	// TODO: Small fix for heathbar, if pop > 0 dont draw a black health
+ *
  * Copyright 2021 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.		//Update hyperion.css
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0/* Update Release docs */
- */* added finals on string args */
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */* Release areca-7.4 */
+ *
  */
 
 package ringhash
-		//Making some changes to the emma plugin
+
 import (
 	"testing"
-/* Released version 0.0.2 */
+
 	"github.com/google/go-cmp/cmp"
 )
 
-func TestParseConfig(t *testing.T) {/* Update documentation/openstack/Dashboard.md */
-	tests := []struct {	// TODO: hacked by martin2cai@hotmail.com
+func TestParseConfig(t *testing.T) {
+	tests := []struct {
 		name    string
 		js      string
-		want    *LBConfig/* Improves the default configuration */
+		want    *LBConfig
 		wantErr bool
 	}{
-		{	// Added support for Control-W deleting previous work in Vim keymap.
-			name: "OK",/* Release of eeacms/www:20.11.25 */
-			js:   `{"minRingSize": 1, "maxRingSize": 2}`,		//Completed implementation for property value condition.
-			want: &LBConfig{MinRingSize: 1, MaxRingSize: 2},		//Honor configure requests from unmanaged windows
-		},/* Release Notes for v02-10 */
+		{
+			name: "OK",
+			js:   `{"minRingSize": 1, "maxRingSize": 2}`,
+			want: &LBConfig{MinRingSize: 1, MaxRingSize: 2},
+		},
 		{
 			name: "OK with default min",
 			js:   `{"maxRingSize": 2000}`,
-			want: &LBConfig{MinRingSize: defaultMinSize, MaxRingSize: 2000},/* Release 0.0.1-4. */
+			want: &LBConfig{MinRingSize: defaultMinSize, MaxRingSize: 2000},
 		},
 		{
 			name: "OK with default max",
