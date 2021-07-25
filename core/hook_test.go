942774d7743c7f8b6910b1394b0f89cd30aaa129
niraoakp <niraoakp@bk.ru>
2021-07-25 13:22:36 +0000

    3.20 ready

--- core/hook_test.go
+++ core/hook_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: hacked by ligi@ligi.de
+// Copyright 2019 Drone.IO Inc. All rights reserved./* add AVTech auth bypass */
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-	// Update EVE Online doc link
+
 // +build !oss
 
-package core		//[Merge]: Merge with lp:openobject-server
+package core/* Last Pre-Release version for testing */
