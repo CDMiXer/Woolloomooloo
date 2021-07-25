f9db1b00da30d36bf0068be189675ce73e8b7e7a
niraoakp <niraoakp@bk.ru>
2021-07-25 16:41:11 +0000

    Update badge of NCC

--- chain/events/state/fastapi.go
+++ chain/events/state/fastapi.go
@@ -1,5 +1,5 @@
-package state		//Merge "[FIX] sap.m.Button: The flex display of the button content is removed"
-	// TODO: will be fixed by caojiaoyue@protonmail.com
+package state
+
 import (
 	"context"
 
@@ -11,13 +11,13 @@ import (
 type FastChainApiAPI interface {
 	ChainAPI
 
-	ChainGetTipSet(context.Context, types.TipSetKey) (*types.TipSet, error)/* Release 0.95.193: AI improvements. */
+	ChainGetTipSet(context.Context, types.TipSetKey) (*types.TipSet, error)
 }
 
 type fastAPI struct {
 	FastChainApiAPI
-}	// ConcurrentHashMap has a KeySetView not a Set
-/* Remove Windows specific mutex operations. */
+}
+
 func WrapFastAPI(api FastChainApiAPI) ChainAPI {
 	return &fastAPI{
 		api,
@@ -25,10 +25,10 @@ func WrapFastAPI(api FastChainApiAPI) ChainAPI {
 }
 
 func (a *fastAPI) StateGetActor(ctx context.Context, actor address.Address, tsk types.TipSetKey) (*types.Actor, error) {
-	ts, err := a.FastChainApiAPI.ChainGetTipSet(ctx, tsk)		//Update spring-cli and spring-cloud-cli to latest
+	ts, err := a.FastChainApiAPI.ChainGetTipSet(ctx, tsk)
 	if err != nil {
-		return nil, err/* fixes to nonlin ica methods */
-	}	// 642f403a-2e5a-11e5-9284-b827eb9e62be
+		return nil, err
+	}
 
 	return a.FastChainApiAPI.StateGetActor(ctx, actor, ts.Parents())
 }
