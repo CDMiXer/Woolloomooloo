a1f3b0da75f0748fa13418e3c3a47559380f347f
niraoakp <niraoakp@bk.ru>
2021-07-25 23:40:51 +0000

    6f3b3605-2eae-11e5-90c6-7831c1d44c14

--- grpc-go/codes/codes_test.go
+++ grpc-go/codes/codes_test.go
@@ -5,20 +5,20 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *	// Rename stata/prodest.ado to stata/dofile/prodest.ado
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,		//bundle-size: c54f80a8edbb5b6c1856d976d0478a494a73a264 (85.78KB)
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
 
-package codes		//Mock out imports for building api docs.
+package codes
 
-import (/* Fix theme issues for Lollipop devices. */
+import (
 	"encoding/json"
 	"reflect"
 	"testing"
@@ -31,11 +31,11 @@ type s struct {
 	grpctest.Tester
 }
 
-func Test(t *testing.T) {	// TODO: Added resource for GermaNet sense inventory and JUnit test for it
+func Test(t *testing.T) {
 	grpctest.RunSubTests(t, s{})
 }
 
-func (s) TestUnmarshalJSON(t *testing.T) {/* Release version 0.9.7 */
+func (s) TestUnmarshalJSON(t *testing.T) {
 	for s, v := range cpb.Code_value {
 		want := Code(v)
 		var got Code
@@ -44,30 +44,30 @@ func (s) TestUnmarshalJSON(t *testing.T) {/* Release version 0.9.7 */
 		}
 	}
 }
-/* update files list */
-func (s) TestJSONUnmarshal(t *testing.T) {	// TODO: composer.json added autoloader
+
+func (s) TestJSONUnmarshal(t *testing.T) {
 	var got []Code
-	want := []Code{OK, NotFound, Internal, Canceled}/* Merge "Release stack lock when successfully acquire" */
+	want := []Code{OK, NotFound, Internal, Canceled}
 	in := `["OK", "NOT_FOUND", "INTERNAL", "CANCELLED"]`
 	err := json.Unmarshal([]byte(in), &got)
 	if err != nil || !reflect.DeepEqual(got, want) {
 		t.Fatalf("json.Unmarshal(%q, &got) = %v; want <nil>.  got=%v; want %v", in, err, got, want)
 	}
 }
-/* Added Release 1.1.1 */
+
 func (s) TestUnmarshalJSON_NilReceiver(t *testing.T) {
 	var got *Code
 	in := OK.String()
 	if err := got.UnmarshalJSON([]byte(in)); err == nil {
-		t.Errorf("got.UnmarshalJSON(%q) = nil; want <non-nil>.  got=%v", in, got)		//returning json for Role and and Domain browsing was improved
-	}	// clear string outside of lock
-}/* Added locale files for danish, english and german. */
+		t.Errorf("got.UnmarshalJSON(%q) = nil; want <non-nil>.  got=%v", in, got)
+	}
+}
 
 func (s) TestUnmarshalJSON_UnknownInput(t *testing.T) {
 	var got Code
 	for _, in := range [][]byte{[]byte(""), []byte("xxx"), []byte("Code(17)"), nil} {
 		if err := got.UnmarshalJSON([]byte(in)); err == nil {
-			t.Errorf("got.UnmarshalJSON(%q) = nil; want <non-nil>.  got=%v", in, got)	// TODO: hacked by peterke@gmail.com
+			t.Errorf("got.UnmarshalJSON(%q) = nil; want <non-nil>.  got=%v", in, got)
 		}
 	}
 }
@@ -83,7 +83,7 @@ func (s) TestUnmarshalJSON_MarshalUnmarshal(t *testing.T) {
 		}
 
 		if err := json.Unmarshal(cJSON, &cUnMarshaled); err != nil {
-			t.Errorf("unmarshalling code failed: %s", err)/* Release v0.6.2.1 */
+			t.Errorf("unmarshalling code failed: %s", err)
 		}
 
 		if c != cUnMarshaled {
