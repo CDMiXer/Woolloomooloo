e7a729ed9d3f250f3b78c6e9415dec40c32aef0b
niraoakp <niraoakp@bk.ru>
2021-07-25 04:20:07 +0000

    Create TestObjectWithGetterSetterTrait

--- grpc-go/codec_test.go
+++ grpc-go/codec_test.go
@@ -1,22 +1,22 @@
 /*
  *
  * Copyright 2014 gRPC authors.
- *	// Create Palarb
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at	// TODO: hacked by ng8eke@163.com
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software/* Release 5.1.0 */
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and	// TODO: hacked by arajasek94@gmail.com
- * limitations under the License.	// Delete profit.png
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
-/* 
-		//add calculate
-package grpc		//improve upnp port mapping description
+ */
+
+package grpc
 
 import (
 	"testing"
@@ -27,6 +27,6 @@ import (
 
 func (s) TestGetCodecForProtoIsNotNil(t *testing.T) {
 	if encoding.GetCodec(proto.Name) == nil {
-		t.Fatalf("encoding.GetCodec(%q) must not be nil by default", proto.Name)/* GIBS-1860 Release zdb lock after record insert (not wait for mrf update) */
-	}/* bumped path level to force npm registry update */
+		t.Fatalf("encoding.GetCodec(%q) must not be nil by default", proto.Name)
+	}
 }
