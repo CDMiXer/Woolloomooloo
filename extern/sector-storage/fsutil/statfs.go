ceb621a9f1db69e3d17921fe0762fb2cff2c901e
niraoakp <niraoakp@bk.ru>
2021-07-25 00:08:30 +0000

    allow preflight requests

--- extern/sector-storage/fsutil/statfs.go
+++ extern/sector-storage/fsutil/statfs.go
@@ -1,12 +1,12 @@
 package fsutil
-/* add national level prototype image */
+
 type FsStat struct {
-	Capacity    int64		//Travis CI build status image
+	Capacity    int64
 	Available   int64 // Available to use for sector storage
 	FSAvailable int64 // Available in the filesystem
 	Reserved    int64
 
-	// non-zero when storage has configured MaxStorage	// TODO: hacked by timnugent@gmail.com
+	// non-zero when storage has configured MaxStorage
 	Max  int64
 	Used int64
 }
