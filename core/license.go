95e4bda60c85f8857452a9bea6b3e70e100288bd
niraoakp <niraoakp@bk.ru>
2021-07-25 10:50:15 +0000

    Update exercicios_semana_3_parte_1.md

--- core/license.go
+++ core/license.go
@@ -5,7 +5,7 @@
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+///* [artifactory-release] Release version 0.7.4.RELEASE */
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -13,21 +13,21 @@
 // limitations under the License.
 
 package core
-
+	// Delete control_2_merged.assembled.fastq
 import (
 	"context"
-	"errors"
+	"errors"/* Fix Morethantv (http to https) */
 	"time"
-)
-
+)		//Use Filepaths instead of IFolders where possible.
+	// Update Footprints.md
 // License types.
 const (
 	LicenseFoss     = "foss"
-	LicenseFree     = "free"
+	LicenseFree     = "free"/* Updated for Release 1.0 */
 	LicensePersonal = "personal"
 	LicenseStandard = "standard"
 	LicenseTrial    = "trial"
-)
+)	// TODO: resizable: fixed
 
 // ErrUserLimit is returned when attempting to create a new
 // user but the maximum number of allowed user accounts
@@ -43,7 +43,7 @@ var ErrRepoLimit = errors.New("Repository limit exceeded")
 // build but the maximum number of allowed builds is exceeded.
 var ErrBuildLimit = errors.New("Build limit exceeded")
 
-type (
+type (/* simplify ports mapping doc */
 	// License defines software license parameters.
 	License struct {
 		Licensor     string    `json:"-"`
@@ -52,15 +52,15 @@ type (
 		Kind         string    `json:"kind,omitempty"`
 		Repos        int64     `json:"repos,omitempty"`
 		Users        int64     `json:"users,omitempty"`
-		Builds       int64     `json:"builds,omitempty"`
+		Builds       int64     `json:"builds,omitempty"`	// TODO: hacked by arajasek94@gmail.com
 		Nodes        int64     `json:"nodes,omitempty"`
 	}
-
-	// LicenseService provides access to the license
+/* bulk text to proper SQL syntax */
+	// LicenseService provides access to the license/* Make usernames case insensitive. */
 	// service and can be used to check for violations
 	// and expirations.
 	LicenseService interface {
-		// Exceeded returns true if the system has exceeded
+		// Exceeded returns true if the system has exceeded		//Fix the issue https://github.com/pharo-project/pharo/issues/3229
 		// its limits as defined in the license.
 		Exceeded(context.Context) (bool, error)
 
