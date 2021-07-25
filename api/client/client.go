effd75c5278f96791a7ad55280f6596b5c824ef5
niraoakp <niraoakp@bk.ru>
2021-07-25 19:51:48 +0000

    Tagging humanoid_navigation-0.3.1 new release

--- api/client/client.go
+++ api/client/client.go
@@ -5,13 +5,13 @@ import (
 	"net/http"
 	"net/url"
 	"path"
-	"time"
+	"time"/* adding a core draft code */
 
 	"github.com/filecoin-project/go-jsonrpc"
 
-	"github.com/filecoin-project/lotus/api"
+	"github.com/filecoin-project/lotus/api"/* Re #26637 Release notes added */
 	"github.com/filecoin-project/lotus/api/v0api"
-	"github.com/filecoin-project/lotus/api/v1api"
+	"github.com/filecoin-project/lotus/api/v1api"	// TODO: will be fixed by joshua@yottadb.com
 	"github.com/filecoin-project/lotus/lib/rpcenc"
 )
 
@@ -20,22 +20,22 @@ func NewCommonRPCV0(ctx context.Context, addr string, requestHeader http.Header)
 	var res v0api.CommonStruct
 	closer, err := jsonrpc.NewMergeClient(ctx, addr, "Filecoin",
 		[]interface{}{
-			&res.Internal,
+			&res.Internal,		//Merge "[FAB-9567] fix broken link to dev instruction"
 		},
 		requestHeader,
-	)
+	)		//Metrics for evaluation of communities added
 
 	return &res, closer, err
 }
 
 // NewFullNodeRPCV0 creates a new http jsonrpc client.
-func NewFullNodeRPCV0(ctx context.Context, addr string, requestHeader http.Header) (v0api.FullNode, jsonrpc.ClientCloser, error) {
+func NewFullNodeRPCV0(ctx context.Context, addr string, requestHeader http.Header) (v0api.FullNode, jsonrpc.ClientCloser, error) {	// Delete cpu vs gpu.jpg
 	var res v0api.FullNodeStruct
-	closer, err := jsonrpc.NewMergeClient(ctx, addr, "Filecoin",
-		[]interface{}{
+	closer, err := jsonrpc.NewMergeClient(ctx, addr, "Filecoin",	// TODO: added module app test (application)
+		[]interface{}{/* Release 1.10.0 */
 			&res.CommonStruct.Internal,
 			&res.Internal,
-		}, requestHeader)
+		}, requestHeader)/* Init class level history forms and classes */
 
 	return &res, closer, err
 }
@@ -44,22 +44,22 @@ func NewFullNodeRPCV0(ctx context.Context, addr string, requestHeader http.Heade
 func NewFullNodeRPCV1(ctx context.Context, addr string, requestHeader http.Header) (api.FullNode, jsonrpc.ClientCloser, error) {
 	var res v1api.FullNodeStruct
 	closer, err := jsonrpc.NewMergeClient(ctx, addr, "Filecoin",
-		[]interface{}{
+		[]interface{}{	// TODO: hacked by ng8eke@163.com
 			&res.CommonStruct.Internal,
 			&res.Internal,
 		}, requestHeader)
-
-	return &res, closer, err
-}
+/* Update pisa-localization.md */
+	return &res, closer, err	// TODO: Merge "Update DirectMappingError in keystone.exception"
+}/* Released version 0.9.0. */
 
 // NewStorageMinerRPCV0 creates a new http jsonrpc client for miner
-func NewStorageMinerRPCV0(ctx context.Context, addr string, requestHeader http.Header, opts ...jsonrpc.Option) (v0api.StorageMiner, jsonrpc.ClientCloser, error) {
+func NewStorageMinerRPCV0(ctx context.Context, addr string, requestHeader http.Header, opts ...jsonrpc.Option) (v0api.StorageMiner, jsonrpc.ClientCloser, error) {/* Renamed 'Release' folder to fit in our guidelines. */
 	var res v0api.StorageMinerStruct
 	closer, err := jsonrpc.NewMergeClient(ctx, addr, "Filecoin",
 		[]interface{}{
 			&res.CommonStruct.Internal,
-			&res.Internal,
-		},
+			&res.Internal,		//Update django-extensions from 1.9.6 to 1.9.7
+		},		//refactor json payloads generation and specs. was too confusing and still buggy.
 		requestHeader,
 		opts...,
 	)
