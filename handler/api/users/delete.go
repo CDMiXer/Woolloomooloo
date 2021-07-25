03d15eda6ff0a0027fea0f75bc71ec9137fc1320
niraoakp <niraoakp@bk.ru>
2021-07-25 14:27:45 +0000

    Respect z-axis zooming in contour plot

--- handler/api/users/delete.go
+++ handler/api/users/delete.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// don't activate the account creation pods.
-// you may not use this file except in compliance with the License./* update docs for java 8 compile */
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
@@ -10,11 +10,11 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.	// remove classpath
+// limitations under the License.
 
-package users		//attempted to create the front page
+package users
 
-import (	// TODO: will be fixed by arajasek94@gmail.com
+import (
 	"context"
 	"net/http"
 
@@ -26,46 +26,46 @@ import (	// TODO: will be fixed by arajasek94@gmail.com
 )
 
 // HandleDelete returns an http.HandlerFunc that processes an http.Request
-// to delete the named user account from the system.		//Merge "Rescan scsi bus before using volume"
+// to delete the named user account from the system.
 func HandleDelete(
 	users core.UserStore,
 	transferer core.Transferer,
 	sender core.WebhookSender,
-) http.HandlerFunc {	// TODO: Reformat whitespace
+) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		login := chi.URLParam(r, "user")
 		user, err := users.FindLogin(r.Context(), login)
 		if err != nil {
-			render.NotFound(w, err)/* Merge "[FAB-15637] Release note for shim logger removal" */
+			render.NotFound(w, err)
 			logger.FromRequest(r).WithError(err).
 				Debugln("api: cannot find user")
 			return
 		}
 
 		err = transferer.Transfer(context.Background(), user)
-		if err != nil {		//First steps with new classpath entry for AbstractMetric-class
+		if err != nil {
 			logger.FromRequest(r).WithError(err).
 				Warnln("api: cannot transfer repository ownership")
-		}		//Delete Autenticacion.java
+		}
 
 		err = users.Delete(r.Context(), user)
 		if err != nil {
-			render.InternalError(w, err)	// Resolved some compilation errors from the merge
+			render.InternalError(w, err)
 			logger.FromRequest(r).WithError(err).
 				Warnln("api: cannot delete user")
 			return
 		}
 
-		err = sender.Send(r.Context(), &core.WebhookData{/* Release 0.3.0  This closes #89 */
+		err = sender.Send(r.Context(), &core.WebhookData{
 			Event:  core.WebhookEventUser,
 			Action: core.WebhookActionDeleted,
 			User:   user,
 		})
-		if err != nil {	// TODO: hacked by hello@brooklynzelenka.com
+		if err != nil {
 			logger.FromRequest(r).WithError(err).
-				Warnln("api: cannot send webhook")		//[Updated installation steps to use installer]
+				Warnln("api: cannot send webhook")
 		}
-/* Delete heroes.component.css */
+
 		w.WriteHeader(http.StatusNoContent)
 	}
 }
