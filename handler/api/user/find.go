535aaa5689620aca7a59440a4c764a3e68f2b0a0
niraoakp <niraoakp@bk.ru>
2021-07-25 23:38:26 +0000

    Access Denied

--- handler/api/user/find.go
+++ handler/api/user/find.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone IO, Inc.	// #187 - resolveMember() method was moved to Collection
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -10,18 +10,18 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.	// TODO: will be fixed by witek@enjin.io
-/* Release of eeacms/www-devel:21.4.18 */
-package user
-	// TODO: RELEASE 1.1.21.
-import (
-	"net/http"	// Actualizar seracis
+// limitations under the License.
 
-	"github.com/drone/drone/handler/api/render"	// TODO: hacked by julia@jvns.ca
+package user
+
+import (
+	"net/http"
+
+	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/handler/api/request"
 )
 
-// HandleFind returns an http.HandlerFunc that writes json-encoded	// TODO: hacked by aeongrp@outlook.com
+// HandleFind returns an http.HandlerFunc that writes json-encoded
 // account information to the http response body.
 func HandleFind() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
