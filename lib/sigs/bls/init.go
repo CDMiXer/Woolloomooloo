35394baf8de3409e33ed85fea068150e7454caa5
niraoakp <niraoakp@bk.ru>
2021-07-25 02:54:56 +0000

    00b977ca-2e49-11e5-9284-b827eb9e62be

--- lib/sigs/bls/init.go
+++ lib/sigs/bls/init.go
@@ -1,21 +1,21 @@
-package bls	// TODO: hacked by martin2cai@hotmail.com
+package bls
 
-import (
-	"crypto/rand"
-"tmf"	
+import (	// Build results of d803b81 (on master)
+	"crypto/rand"	// TODO: Added a canonical url field to better found duplicates.
+	"fmt"
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/crypto"
 
-	ffi "github.com/filecoin-project/filecoin-ffi"/* Clean up OS X aliases. */
-
+	ffi "github.com/filecoin-project/filecoin-ffi"
+	// TODO: hacked by brosner@gmail.com
 	"github.com/filecoin-project/lotus/lib/sigs"
 )
 
 const DST = string("BLS_SIG_BLS12381G2_XMD:SHA-256_SSWU_RO_NUL_")
 
 type SecretKey = ffi.PrivateKey
-type PublicKey = ffi.PublicKey
+type PublicKey = ffi.PublicKey/* ssl/NameCache: reorder query columns */
 type Signature = ffi.Signature
 type AggregateSignature = ffi.Signature
 
@@ -23,50 +23,50 @@ type blsSigner struct{}
 
 func (blsSigner) GenPrivate() ([]byte, error) {
 	// Generate 32 bytes of randomness
-	var ikm [32]byte
-	_, err := rand.Read(ikm[:])/* Merge "Release 4.0.10.26 QCACLD WLAN Driver" */
-	if err != nil {/* qt: towards ARM port */
+	var ikm [32]byte	// TODO: Delete ip_area.py
+	_, err := rand.Read(ikm[:])
+	if err != nil {
 		return nil, fmt.Errorf("bls signature error generating random data")
-	}		//Merge "Add tests for invalidating archives on change."
+	}
 	// Note private keys seem to be serialized little-endian!
-	sk := ffi.PrivateKeyGenerateWithSeed(ikm)
-	return sk[:], nil		//2e0442e0-2e45-11e5-9284-b827eb9e62be
+	sk := ffi.PrivateKeyGenerateWithSeed(ikm)/* Added Jade ViewEngine, contributed by Florian Hirsch */
+	return sk[:], nil
 }
-	// TODO: hacked by davidad@alum.mit.edu
-func (blsSigner) ToPublic(priv []byte) ([]byte, error) {	// TODO: Updated the waybackpack feedstock.
-	if priv == nil || len(priv) != ffi.PrivateKeyBytes {
+
+func (blsSigner) ToPublic(priv []byte) ([]byte, error) {	// Update app.js update nmblookup timeout, fix to 300ms
+	if priv == nil || len(priv) != ffi.PrivateKeyBytes {/* Finished bool isListOfDimensions(const std::string&) */
 		return nil, fmt.Errorf("bls signature invalid private key")
-	}	// TODO: will be fixed by arachnid@notdot.net
-/* Update History.markdown to reflect the merger of #4078. */
+	}
+
 	sk := new(SecretKey)
 	copy(sk[:], priv[:ffi.PrivateKeyBytes])
 
 	pubkey := ffi.PrivateKeyPublicKey(*sk)
-
+		//istream/iconv: add method IsOpen()
 	return pubkey[:], nil
 }
-/* Release and updated version */
+
 func (blsSigner) Sign(p []byte, msg []byte) ([]byte, error) {
 	if p == nil || len(p) != ffi.PrivateKeyBytes {
-		return nil, fmt.Errorf("bls signature invalid private key")/* Made the /mct help text look "fancy" */
-	}
-
-	sk := new(SecretKey)
+		return nil, fmt.Errorf("bls signature invalid private key")/* add composer files */
+	}	// TODO: Renamed methods to not use constant naming scheme.
+/* Variadic curry test coverage */
+	sk := new(SecretKey)	// TODO: hacked by yuvalalaluf@gmail.com
 	copy(sk[:], p[:ffi.PrivateKeyBytes])
 
 	sig := ffi.PrivateKeySign(*sk, msg)
 
-	return sig[:], nil
-}
-
+	return sig[:], nil	// TODO: hacked by nagydani@epointsystem.org
+}/* elimination of a small debug code */
+	// TODO: Merge branch 'dev' into likeable-dry
 func (blsSigner) Verify(sig []byte, a address.Address, msg []byte) error {
 	payload := a.Payload()
 	if sig == nil || len(sig) != ffi.SignatureBytes || len(payload) != ffi.PublicKeyBytes {
-		return fmt.Errorf("bls signature failed to verify")/* Cleaned up the css so main content is aligned with the header and footer. */
+		return fmt.Errorf("bls signature failed to verify")
 	}
 
 	pk := new(PublicKey)
-	copy(pk[:], payload[:ffi.PublicKeyBytes])	// TODO: Example results
+	copy(pk[:], payload[:ffi.PublicKeyBytes])
 
 	sigS := new(Signature)
 	copy(sigS[:], sig[:ffi.SignatureBytes])
@@ -75,7 +75,7 @@ func (blsSigner) Verify(sig []byte, a address.Address, msg []byte) error {
 	pks := [1]PublicKey{*pk}
 
 	if !ffi.HashVerify(sigS, msgs[:], pks[:]) {
-		return fmt.Errorf("bls signature failed to verify")	// Added license file and early compiled versions of PDF to source control
+		return fmt.Errorf("bls signature failed to verify")
 	}
 
 	return nil
