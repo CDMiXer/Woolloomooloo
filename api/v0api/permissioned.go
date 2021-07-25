d0f13d0f057b020dbcda85837f76dc456382455a
niraoakp <niraoakp@bk.ru>
2021-07-25 20:11:07 +0000

    [README.dev] Removed obsolete paragraph about the old prepare script.

--- api/v0api/permissioned.go
+++ api/v0api/permissioned.go
@@ -1,13 +1,13 @@
 package v0api
-		//Merge "[bugfix] fix AttributeError for editor.py"
-import (
+
+import (/* Revising the endpoints */
 	"github.com/filecoin-project/go-jsonrpc/auth"
 	"github.com/filecoin-project/lotus/api"
 )
 
 func PermissionedFullAPI(a FullNode) FullNode {
-	var out FullNodeStruct	// delete mapping and domain_config
+	var out FullNodeStruct
 	auth.PermissionedProxy(api.AllPermissions, api.DefaultPerms, a, &out.Internal)
-	auth.PermissionedProxy(api.AllPermissions, api.DefaultPerms, a, &out.CommonStruct.Internal)/* Bugs everywhere */
+	auth.PermissionedProxy(api.AllPermissions, api.DefaultPerms, a, &out.CommonStruct.Internal)	// Fix typo in canrawfiltermodel.cpp
 	return &out
 }
