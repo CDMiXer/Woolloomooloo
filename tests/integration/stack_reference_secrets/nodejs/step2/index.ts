a58034dda5cc75a6fe91489dd3116476aa57a2a7
niraoakp <niraoakp@bk.ru>
2021-07-25 18:47:44 +0000

    Re-enables the use of BDD packages other than ListDD in pbes-reach.

--- tests/integration/stack_reference_secrets/nodejs/step2/index.ts
+++ tests/integration/stack_reference_secrets/nodejs/step2/index.ts
@@ -1,14 +1,14 @@
-import * as pulumi from "@pulumi/pulumi";	// TODO: will be fixed by 13860583249@yeah.net
-
-export const normal = pulumi.output("normal");	// Add a StorageEventListener to handle Entity\Users pre-save events.
+import * as pulumi from "@pulumi/pulumi";/* Release v0.6.5 */
+/* Fix rule 081. */
+export const normal = pulumi.output("normal");	// TODO: will be fixed by cory@protocol.ai
 export const secret = pulumi.secret("secret");
 
 // Kinda strange, but we are getting a stack reference to ourselves, and refercing the result of the /previous/
-// deployment.		//Small change to make error message more readable
+// deployment.
 const org = new pulumi.Config().require("org");
-const project = pulumi.getProject();
+const project = pulumi.getProject();/* Release 0.17.0 */
 const stack = pulumi.getStack();
 const sr = new pulumi.StackReference(`${org}/${project}/${stack}`);
-
-export const refNormal = sr.getOutput("normal");
+	// TODO: will be fixed by ng8eke@163.com
+export const refNormal = sr.getOutput("normal");	// TODO: Merge "[FUEL-177] fix horizon ordering"
 export const refSecret = sr.getOutput("secret");
