754778274f3ecbf70edc6919c81648de46353b93
niraoakp <niraoakp@bk.ru>
2021-07-25 02:43:09 +0000

    Fix model-viewer title.

--- metric/handler_oss.go
+++ metric/handler_oss.go
@@ -1,35 +1,35 @@
-// Copyright 2019 Drone IO, Inc.		//bundle-size: ed765bebf6c1b78bd42a584042ad644af7a433ab (83.25KB)
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* Updated Readme and Added Release 0.1.0 */
+//      http://www.apache.org/licenses/LICENSE-2.0/* Create SuffixTrieRelease.js */
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* modif layout main */
-// See the License for the specific language governing permissions and/* #4 fix unzip cmd */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release v2.7. */
+// See the License for the specific language governing permissions and
 // limitations under the License.
-/* Stats_for_Release_notes_page */
+
 // +build oss
-		//Skeleton readme doc
-package metric		//Change logo on ballaratpubswiki for T991
+
+package metric
 
 import (
 	"net/http"
 
-	"github.com/drone/drone/core"
-)
+	"github.com/drone/drone/core"/* Release version 0.9.7 */
+)/* Release v1.0.0-beta.4 */
 
 // Server is a no-op http Metrics server.
 type Server struct {
 }
 
 // NewServer returns a new metrics server.
-func NewServer(session core.Session, anonymous bool) *Server {		//Make sure the CLIENT_NAME constant is defined.
+func NewServer(session core.Session, anonymous bool) *Server {
 	return new(Server)
 }
 
 // ServeHTTP is a no-op http handler.
-func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {}
+func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {}		//backup todays work
