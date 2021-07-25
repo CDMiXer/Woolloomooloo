690848381a534eb29f053cefb37eb8d429b98f0e
niraoakp <niraoakp@bk.ru>
2021-07-25 14:46:46 +0000

    remove obsolete UI design

--- tests/integration/dependency_steps/step1/index.ts
+++ tests/integration/dependency_steps/step1/index.ts
@@ -4,5 +4,5 @@ import { Resource } from "./resource";
 
 // Step 1: Populate our dependency graph.
 const a = new Resource("a", { state: 1 });
-const b = new Resource("b", { state: 2, resource: a });		//Added 'How many players' dialog box before launching game window.
+const b = new Resource("b", { state: 2, resource: a });
 // Dependency graph: b -> a
