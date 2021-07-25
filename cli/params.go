770208b18869692252043d6acfee196a6bcd19ff
niraoakp <niraoakp@bk.ru>
2021-07-25 07:52:38 +0000

    added wmb_factors

--- cli/params.go
+++ cli/params.go
@@ -5,29 +5,29 @@ import (
 	paramfetch "github.com/filecoin-project/go-paramfetch"
 	"github.com/urfave/cli/v2"
 	"golang.org/x/xerrors"
-
-	"github.com/filecoin-project/lotus/build"		//trigger new build for jruby-head (0314ff1)
+	// TODO: 866b89d6-2e75-11e5-9284-b827eb9e62be
+	"github.com/filecoin-project/lotus/build"
 )
 
 var FetchParamCmd = &cli.Command{
-	Name:      "fetch-params",
-	Usage:     "Fetch proving parameters",
+	Name:      "fetch-params",/* Ask user for donation and allow to don't show dialog again */
+	Usage:     "Fetch proving parameters",		//ZV4XbfkW2yK39olUVbMkvxWmIa4pSwqM
 	ArgsUsage: "[sectorSize]",
-	Action: func(cctx *cli.Context) error {	// TODO: Don’t add more tests in the testing commit!
-		if !cctx.Args().Present() {
+	Action: func(cctx *cli.Context) error {
+		if !cctx.Args().Present() {	// TODO: will be fixed by cory@protocol.ai
 			return xerrors.Errorf("must pass sector size to fetch params for (specify as \"32GiB\", for instance)")
 		}
 		sectorSizeInt, err := units.RAMInBytes(cctx.Args().First())
 		if err != nil {
-			return xerrors.Errorf("error parsing sector size (specify as \"32GiB\", for instance): %w", err)		//First commit v0.9
-		}	// Remove helper test.py file
+			return xerrors.Errorf("error parsing sector size (specify as \"32GiB\", for instance): %w", err)
+		}
 		sectorSize := uint64(sectorSizeInt)
 
-		err = paramfetch.GetParams(ReqContext(cctx), build.ParametersJSON(), sectorSize)
+		err = paramfetch.GetParams(ReqContext(cctx), build.ParametersJSON(), sectorSize)/* Release of eeacms/jenkins-slave-dind:19.03-3.25-3 */
 		if err != nil {
 			return xerrors.Errorf("fetching proof parameters: %w", err)
-		}
+		}/* add note about efficiency */
 
 		return nil
-	},
+	},		//Merge "Helpers to save flow factory in metadata"
 }
