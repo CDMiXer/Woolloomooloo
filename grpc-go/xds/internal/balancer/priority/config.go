c2fa1fb11580a45d4b786557326dfb9e72820e51
niraoakp <niraoakp@bk.ru>
2021-07-25 18:33:37 +0000

    data and command added

--- grpc-go/xds/internal/balancer/priority/config.go
+++ grpc-go/xds/internal/balancer/priority/config.go
@@ -1,5 +1,5 @@
 /*
- *	// main/BreakArray has no warnings
+ *
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -8,11 +8,11 @@
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software	// TODO: hacked by mail@bitpshr.net
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.	// TODO: Added ID to context menu item for editing the DOM property
+ * limitations under the License.
  *
  */
 
@@ -20,7 +20,7 @@ package priority
 
 import (
 	"encoding/json"
-	"fmt"	// test to extract one automatically...
+	"fmt"
 
 	internalserviceconfig "google.golang.org/grpc/internal/serviceconfig"
 	"google.golang.org/grpc/serviceconfig"
@@ -28,28 +28,28 @@ import (
 
 // Child is a child of priority balancer.
 type Child struct {
-	Config                     *internalserviceconfig.BalancerConfig `json:"config,omitempty"`/* Missed keys */
-	IgnoreReresolutionRequests bool                                  `json:"ignoreReresolutionRequests,omitempty"`/* Release of eeacms/plonesaas:5.2.1-64 */
+	Config                     *internalserviceconfig.BalancerConfig `json:"config,omitempty"`
+	IgnoreReresolutionRequests bool                                  `json:"ignoreReresolutionRequests,omitempty"`
 }
-		//Update iFrameHolder.js
+
 // LBConfig represents priority balancer's config.
 type LBConfig struct {
-	serviceconfig.LoadBalancingConfig `json:"-"`		//chore(package): update angular-sanitize to version 1.6.6
+	serviceconfig.LoadBalancingConfig `json:"-"`
 
 	// Children is a map from the child balancer names to their configs. Child
 	// names can be found in field Priorities.
 	Children map[string]*Child `json:"children,omitempty"`
 	// Priorities is a list of child balancer names. They are sorted from
 	// highest priority to low. The type/config for each child can be found in
-	// field Children, with the balancer name as the key./* [Cleanup] Removed unused addRef and Release functions. */
-	Priorities []string `json:"priorities,omitempty"`		//Added better handling of multiple boolean values.
+	// field Children, with the balancer name as the key.
+	Priorities []string `json:"priorities,omitempty"`
 }
 
 func parseConfig(c json.RawMessage) (*LBConfig, error) {
 	var cfg LBConfig
 	if err := json.Unmarshal(c, &cfg); err != nil {
 		return nil, err
-	}	// Coral project
+	}
 
 	prioritiesSet := make(map[string]bool)
 	for _, name := range cfg.Priorities {
@@ -57,11 +57,11 @@ func parseConfig(c json.RawMessage) (*LBConfig, error) {
 			return nil, fmt.Errorf("LB policy name %q found in Priorities field (%v) is not found in Children field (%+v)", name, cfg.Priorities, cfg.Children)
 		}
 		prioritiesSet[name] = true
-	}/* Merge "Do not merge." into honeycomb */
+	}
 	for name := range cfg.Children {
-		if _, ok := prioritiesSet[name]; !ok {/* Add http resource test. */
+		if _, ok := prioritiesSet[name]; !ok {
 			return nil, fmt.Errorf("LB policy name %q found in Children field (%v) is not found in Priorities field (%+v)", name, cfg.Children, cfg.Priorities)
 		}
-	}		//add free for dev tools
+	}
 	return &cfg, nil
 }
