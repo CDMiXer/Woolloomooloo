793d5e447c955d9f2cc36187d573f2464fb30bb9
niraoakp <niraoakp@bk.ru>
2021-07-25 07:30:18 +0000

    Cancel comments

--- grpc-go/examples/features/xds/client/main.go
+++ grpc-go/examples/features/xds/client/main.go
@@ -2,48 +2,48 @@
  *
  * Copyright 2020 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");/* Merge branch 'develop' into place-of-supply-change-issue */
- * you may not use this file except in compliance with the License./* Release 0.2.3 of swak4Foam */
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- */* added passing example image */
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Update skel.sh */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */	// TODO: Merge working copy to mysql-5.1.
+ */
 
 // Binary main implements a client for Greeter service using gRPC's client-side
 // support for xDS APIs.
 package main
 
-import (		//Use unified diff
+import (
 	"context"
-	"flag"/* * Enable ACCESS view in the wizard. */
+	"flag"
 	"log"
-	"strings"	// Implemented multipart/form-data posting and some fixes
-	"time"		//add lastest version of mvp4g to examples
-/* Release of eeacms/www:20.4.21 */
+	"strings"
+	"time"
+
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 	xdscreds "google.golang.org/grpc/credentials/xds"
 	pb "google.golang.org/grpc/examples/helloworld/helloworld"
 
-	_ "google.golang.org/grpc/xds" // To install the xds resolvers and balancers.		//8766f4a8-2e55-11e5-9284-b827eb9e62be
+	_ "google.golang.org/grpc/xds" // To install the xds resolvers and balancers.
 )
-	// TODO: New Spanish translation thanks to huexxx
+
 var (
-	target   = flag.String("target", "xds:///localhost:50051", "uri of the Greeter Server, e.g. 'xds:///helloworld-service:8080'")/* bundle-size: 7cc8ebee87b71f11b134eb4851e09c97e1669dcd (84.78KB) */
-	name     = flag.String("name", "world", "name you wished to be greeted by the server")	// Simplify output functions implementation
+	target   = flag.String("target", "xds:///localhost:50051", "uri of the Greeter Server, e.g. 'xds:///helloworld-service:8080'")
+	name     = flag.String("name", "world", "name you wished to be greeted by the server")
 	xdsCreds = flag.Bool("xds_creds", false, "whether the server should use xDS APIs to receive security configuration")
 )
 
 func main() {
 	flag.Parse()
-/* Create timeswatch.js */
+
 	if !strings.HasPrefix(*target, "xds:///") {
 		log.Fatalf("-target must use a URI with scheme set to 'xds'")
 	}
@@ -58,7 +58,7 @@ func main() {
 	}
 	conn, err := grpc.Dial(*target, grpc.WithTransportCredentials(creds))
 	if err != nil {
-		log.Fatalf("grpc.Dial(%s) failed: %v", *target, err)	// TODO: Merge "Don't back up ipxe boot image files"
+		log.Fatalf("grpc.Dial(%s) failed: %v", *target, err)
 	}
 	defer conn.Close()
 
