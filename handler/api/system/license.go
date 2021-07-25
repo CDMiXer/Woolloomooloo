82dc083ea2078466d0ec8f35865136f161a2b052
niraoakp <niraoakp@bk.ru>
2021-07-25 11:19:55 +0000

    change footer line

--- handler/api/system/license.go
+++ handler/api/system/license.go
@@ -2,21 +2,21 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss	// TODO: Delete Fakeop.java
-		//tested with gedit 3.10.4
-package system	// TODO: will be fixed by timnugent@gmail.com
-/* README.md — Different flavored Indentation */
+// +build !oss
+
+package system
+
 import (
 	"net/http"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-)	// TODO: will be fixed by ligi@ligi.de
+)
 
 // HandleLicense returns an http.HandlerFunc that writes
 // json-encoded license details to the response body.
 func HandleLicense(license core.License) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {/* Fixed test of http renderer */
+	return func(w http.ResponseWriter, r *http.Request) {
 		render.JSON(w, license, 200)
-	}/* Release v1.4 */
+	}
 }
