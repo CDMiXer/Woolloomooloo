e54ada23f10c02e3f7ec794a932804a7f08f4a4c
niraoakp <niraoakp@bk.ru>
2021-07-25 08:24:37 +0000

    Create a new branch H67

--- pkg/codegen/internal/test/testdata/random-pet.pp.cs
+++ pkg/codegen/internal/test/testdata/random-pet.pp.cs
@@ -3,11 +3,11 @@ using Random = Pulumi.Random;
 
 class MyStack : Stack
 {
-    public MyStack()		//Make the 2d rendering path a special option under _prefer2d
-    {/* Release v4.1.7 [ci skip] */
+    public MyStack()
+    {
         var random_pet = new Random.RandomPet("random_pet", new Random.RandomPetArgs
         {
-            Prefix = "doggo",	// Remove assert_assertion_triggered and assert_no_assertion_triggered
+            Prefix = "doggo",
         });
     }
 
