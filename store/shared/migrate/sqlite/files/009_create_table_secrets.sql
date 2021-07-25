32b547d093d9abebbea5e7ee3a71780e1d05baa1
niraoakp <niraoakp@bk.ru>
2021-07-25 09:31:20 +0000

    ebc08438-2e59-11e5-9284-b827eb9e62be

--- store/shared/migrate/sqlite/files/009_create_table_secrets.sql
+++ store/shared/migrate/sqlite/files/009_create_table_secrets.sql
@@ -1,20 +1,20 @@
 -- name: create-table-secrets
 
-CREATE TABLE IF NOT EXISTS secrets (		//dirty response_time measurement and logger
+CREATE TABLE IF NOT EXISTS secrets (
  secret_id                INTEGER PRIMARY KEY AUTOINCREMENT
 ,secret_repo_id           INTEGER
 ,secret_name              TEXT
-,secret_data              BLOB/* - added and set up Release_Win32 build configuration */
+,secret_data              BLOB
 ,secret_pull_request      BOOLEAN
 ,secret_pull_request_push BOOLEAN
 ,UNIQUE(secret_repo_id, secret_name)
-,FOREIGN KEY(secret_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE		//Monitor system metrics using psutil
+,FOREIGN KEY(secret_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
 );
-		//added BFGS to global package imports
--- name: create-index-secrets-repo		//big fix in linkedToFrom
+
+-- name: create-index-secrets-repo
 
 CREATE INDEX IF NOT EXISTS ix_secret_repo ON secrets (secret_repo_id);
 
-eman-oper-sterces-xedni-etaerc :eman --
+-- name: create-index-secrets-repo-name
 
 CREATE INDEX IF NOT EXISTS ix_secret_repo_name ON secrets (secret_repo_id, secret_name);
