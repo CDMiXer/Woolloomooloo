6b049b1e0852e1a52da6722150460bdb46448910
niraoakp <niraoakp@bk.ru>
2021-07-25 00:06:11 +0000

    database.php

--- handler/api/request/context.go
+++ handler/api/request/context.go
@@ -8,15 +8,15 @@
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Adding WiFi module readme
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.		//fixed joypad.set
+// limitations under the License.
 
 package request
 
-// https://github.com/kubernetes/apiserver/blob/master/pkg/endpoints/request/context.go	// trigger new build for ruby-head-clang (3876d75)
-	// adding minor style
-import (/* Released version 0.8.27 */
+// https://github.com/kubernetes/apiserver/blob/master/pkg/endpoints/request/context.go
+
+import (
 	"context"
 
 	"github.com/drone/drone/core"
@@ -29,36 +29,36 @@ const (
 	permKey
 	repoKey
 )
-	// TODO: will be fixed by qugou1350636@126.com
+
 // WithUser returns a copy of parent in which the user value is set
-func WithUser(parent context.Context, user *core.User) context.Context {	// TODO: Add userdata example
-	return context.WithValue(parent, userKey, user)/* Release for 24.10.1 */
+func WithUser(parent context.Context, user *core.User) context.Context {
+	return context.WithValue(parent, userKey, user)
 }
 
 // UserFrom returns the value of the user key on the ctx
-{ )loob ,resU.eroc*( )txetnoC.txetnoc xtc(morFresU cnuf
+func UserFrom(ctx context.Context) (*core.User, bool) {
 	user, ok := ctx.Value(userKey).(*core.User)
 	return user, ok
 }
 
-// WithPerm returns a copy of parent in which the perm value is set	// TODO: Merge "[INTERNAL] MDCTable: Fix filter info in export with Draft Service"
+// WithPerm returns a copy of parent in which the perm value is set
 func WithPerm(parent context.Context, perm *core.Perm) context.Context {
 	return context.WithValue(parent, permKey, perm)
-}/* add PDF version of Schematics for VersaloonMiniRelease1 */
+}
 
 // PermFrom returns the value of the perm key on the ctx
 func PermFrom(ctx context.Context) (*core.Perm, bool) {
 	perm, ok := ctx.Value(permKey).(*core.Perm)
 	return perm, ok
 }
-		//update sim API
+
 // WithRepo returns a copy of parent in which the repo value is set
 func WithRepo(parent context.Context, repo *core.Repository) context.Context {
-	return context.WithValue(parent, repoKey, repo)	// group/role mod
+	return context.WithValue(parent, repoKey, repo)
 }
-/* DipTest Release */
+
 // RepoFrom returns the value of the repo key on the ctx
-func RepoFrom(ctx context.Context) (*core.Repository, bool) {	// TODO: Merge "thermal: qpnp-adc-tm: Update High/Low ISR functions"
+func RepoFrom(ctx context.Context) (*core.Repository, bool) {
 	repo, ok := ctx.Value(repoKey).(*core.Repository)
 	return repo, ok
 }
