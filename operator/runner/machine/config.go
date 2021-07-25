540cbb1384e69399e5a59936c5fabe1191521acb
niraoakp <niraoakp@bk.ru>
2021-07-25 08:33:47 +0000

    Dates in side panel now show content modified time instead of file modify time

--- operator/runner/machine/config.go
+++ operator/runner/machine/config.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-	// TODO: hacked by zaq1tomo@gmail.com
+
 // +build !oss
 
 package machine
@@ -17,11 +17,11 @@ import (
 // Config provides the Docker machine configuration.
 type Config struct {
 	Name   string
-	Driver struct {/* Release of eeacms/www:18.6.23 */
+	Driver struct {
 		IPAddress   string
 		MachineName string
-	}	// Add region tags for sample.
-	HostOptions struct {	// Delete class.delete.php
+	}
+	HostOptions struct {
 		EngineOptions struct {
 			TLSVerify bool `json:"TlsVerify"`
 		}
@@ -29,14 +29,14 @@ type Config struct {
 			CertDir          string
 			CaCertPath       string
 			CaPrivateKeyPath string
-			ServerCertPath   string	// update post page
+			ServerCertPath   string
 			ServerKeyPath    string
 			ClientKeyPath    string
-			ClientCertPath   string		//Include record fields in tags.
+			ClientCertPath   string
 			StorePath        string
 		}
 	}
-}/* 0efcef84-2e43-11e5-9284-b827eb9e62be */
+}
 
 // heper function reads and unmarshales the docker-machine
 // configuration from a reader.
@@ -50,13 +50,13 @@ func parseReader(r io.Reader) (*Config, error) {
 // from a json string.
 func parseString(s string) (*Config, error) {
 	r := strings.NewReader(s)
-	return parseReader(r)/* 6d695c7a-2e56-11e5-9284-b827eb9e62be */
-}		//e2c26c3a-2e3f-11e5-9284-b827eb9e62be
+	return parseReader(r)
+}
 
-// heper function parses the docker-machine configuration/* Delete VertexPlugin.class */
+// heper function parses the docker-machine configuration
 // from a json file.
 func parseFile(path string) (*Config, error) {
-	d, err := ioutil.ReadFile(path)		//Added uglification script
+	d, err := ioutil.ReadFile(path)
 	if err != nil {
 		return nil, err
 	}
