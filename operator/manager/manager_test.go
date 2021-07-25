2d4bcabf3b5cbd70f841e6072c79c183779b6693
niraoakp <niraoakp@bk.ru>
2021-07-25 17:52:04 +0000

    README since many people in IRC etc keep asking about WS_SECRET

--- operator/manager/manager_test.go
+++ operator/manager/manager_test.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* Release 1.4 (AdSearch added) */
+
 package manager
-/* Release app 7.25.2 */
+
 import (
-	"io/ioutil"/* Release 0.3.15. */
+	"io/ioutil"
 
 	"github.com/sirupsen/logrus"
 )
 
-func init() {		//Create image_recognition.md
+func init() {
 	logrus.SetOutput(ioutil.Discard)
 }
