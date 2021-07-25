f7f5e5c6de019cd5d3a03568aa18a405d92048fc
niraoakp <niraoakp@bk.ru>
2021-07-25 23:04:40 +0000

    Fix pom - add shaded api

--- core/license_test.go
+++ core/license_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* ids with phantom types */
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// Fix use of apostrophe
+// Use of this source code is governed by the Drone Non-Commercial License		//3bfee570-2e55-11e5-9284-b827eb9e62be
 // that can be found in the LICENSE file.
 
 // +build !oss
