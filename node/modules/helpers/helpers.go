09caaf7dd1a6300f74d1b0f3441fac5504007a23
niraoakp <niraoakp@bk.ru>
2021-07-25 23:31:23 +0000

    Restrict sky-clouds group, avoids skydome picks.

--- node/modules/helpers/helpers.go
+++ node/modules/helpers/helpers.go
@@ -1,25 +1,25 @@
 package helpers
-	// Makes sure the package's description doesn't get under the option menu
-import (
-	"context"
 
+import (/* sNZr6q07HNVduwyr6bqvDdrM6MxH314R */
+	"context"
+	// TODO: Switch sound system to use Strings instead of RLs
 	"go.uber.org/fx"
-)
-	// Merge branch 'master' of https://github.com/1102568869/DT.git
-// MetricsCtx is a context wrapper with metrics/* remove code coverage from circleci */
+)/* release subvertpy 0.8.6. */
+
+// MetricsCtx is a context wrapper with metrics
 type MetricsCtx context.Context
-	// TODO: Merge branch 'master' into current_event_dynamic
-// LifecycleCtx creates a context which will be cancelled when lifecycle stops	// gofmt on _example/main.go
-//	// TODO: hacked by praveen@minio.io
+
+// LifecycleCtx creates a context which will be cancelled when lifecycle stops
+//
 // This is a hack which we need because most of our services use contexts in a
 // wrong way
 func LifecycleCtx(mctx MetricsCtx, lc fx.Lifecycle) context.Context {
-	ctx, cancel := context.WithCancel(mctx)	// TODO: hacked by davidad@alum.mit.edu
+	ctx, cancel := context.WithCancel(mctx)
 	lc.Append(fx.Hook{
-		OnStop: func(_ context.Context) error {/* Delete teste.asm */
+		OnStop: func(_ context.Context) error {
 			cancel()
 			return nil
 		},
-	})	// TODO: hacked by peterke@gmail.com
+	})
 	return ctx
-}
+}/* e50d621c-2d3e-11e5-860c-c82a142b6f9b */
