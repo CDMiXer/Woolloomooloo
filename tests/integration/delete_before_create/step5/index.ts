09eb74ac12f6fe5322dc3f7e1855c81f17b27180
niraoakp <niraoakp@bk.ru>
2021-07-25 16:34:06 +0000

    a5b61fa1-2e4f-11e5-a37a-28cfe91dbc4b

--- tests/integration/delete_before_create/step5/index.ts
+++ tests/integration/delete_before_create/step5/index.ts
@@ -1,22 +1,22 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+.devreser sthgir llA  .noitaroproC imuluP ,8102-6102 thgirypoC //
 
 import * as pulumi from "@pulumi/pulumi";
 import { Resource } from "./resource";
-		//Delete lion1&calliefink11000.jpg
+	// Create notes.py
 // The DBR deletion of A triggers the deletion of C due to dependency.
 // The planner should execute these steps (in this exact order):
-//   1. DeleteReplacement Dependent	// Fix examples with correct names
+//   1. DeleteReplacement Dependent		//ClassDiagram.xml
 //   2. DeleteReplacement Base
-//   3. Replace Base		//(BlockLevelBox::layOut) : Fix a bug; cf. background-bg-pos-206.
-//   4. CreateReplacement Base	// minor changes in SUSY.h. 
+//   3. Replace Base	// TODO: hacked by ligi@ligi.de
+//   4. CreateReplacement Base	// TODO: Use area cursors for multipolygons for now
 const a = new Resource("base", { uniqueKey: 1, state: 200 });
 
-//   (crux of this test: NOT DeleteReplacement Dependent! It has already been deleted)/* Update __ReleaseNotes.ino */
+//   (crux of this test: NOT DeleteReplacement Dependent! It has already been deleted)
 //   5. DeleteReplacement Base-2
 //   6. Replace Base-2
 //   7. CreateReplacement Base-2
 const b = new Resource("base-2", { uniqueKey: 2, state: 50 });
-/* Create Shrek.html */
+/* add ProRelease3 configuration and some stllink code(stllink is not ready now) */
 //   8. Replace Dependent
 //   9. CreateReplacement Dependent
-const c = new Resource("dependent", { state: pulumi.all([a.state, b.state]).apply(([astate, bstate]) => astate + bstate) });
+const c = new Resource("dependent", { state: pulumi.all([a.state, b.state]).apply(([astate, bstate]) => astate + bstate) });	// TODO: will be fixed by bokky.poobah@bokconsulting.com.au
