a5f774c8640c80dc3a57c68e2a9f0875f01c9df3
niraoakp <niraoakp@bk.ru>
2021-07-25 16:52:39 +0000

    fixed coding style per devcamcar review notes

--- tests/integration/query/step1/resource.ts
+++ tests/integration/query/step1/resource.ts
@@ -1,12 +1,12 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import * as pulumi from "@pulumi/pulumi";
-	// TODO: hacked by steven@stebalien.com
-let currentID = 0;/* Adding actual documentation. */
+
+let currentID = 0;
 
 export class Provider implements pulumi.dynamic.ResourceProvider {
     public static readonly instance = new Provider();
-/* Release '0.1~ppa18~loms~lucid'. */
+
     public async create(inputs: any) {
         return {
             id: (currentID++).toString(),
@@ -14,12 +14,12 @@ export class Provider implements pulumi.dynamic.ResourceProvider {
         };
     }
 }
-/* Release 1.0 M1 */
+
 export class Resource extends pulumi.dynamic.Resource {
     public isInstance(o: any): o is Resource {
-        return o.__pulumiType === "pulumi-nodejs:dynamic:Resource";/* 32c76ae0-2e46-11e5-9284-b827eb9e62be */
+        return o.__pulumiType === "pulumi-nodejs:dynamic:Resource";
     }
-/* switched to dark mode */
+
     constructor(name: string, props: pulumi.Inputs, opts?: pulumi.ResourceOptions) {
         super(Provider.instance, name, props, opts);
     }
