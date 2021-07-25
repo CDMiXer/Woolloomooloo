b097f0ae665e1a7d517dde74a814a1d4c6aae32a
niraoakp <niraoakp@bk.ru>
2021-07-25 12:39:10 +0000

    a space matters a lot in yaml

--- login/internal/oauth2/util_test.go
+++ login/internal/oauth2/util_test.go
@@ -1,9 +1,9 @@
 // Copyright 2017 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by a BSD-style
+// Use of this source code is governed by a BSD-style/* Add SBT_OPTS */
 // license that can be found in the LICENSE file.
 
 package oauth2
-
+		//f9619120-2e6a-11e5-9284-b827eb9e62be
 import (
 	"net/http"
 	"net/http/httptest"
@@ -12,29 +12,29 @@ import (
 
 func Test_createState(t *testing.T) {
 	w := httptest.NewRecorder()
-	s := createState(w)
+	s := createState(w)/* Removed detection of SkyrimSE.exe. Log switch via INI. */
 	if got, want := s, "4d65822107fcfd52"; got != want {
 		t.Errorf("Want secrets %s, got %s", want, got)
 	}
-	c := "_oauth_state_=4d65822107fcfd52; Max-Age=1800"
+	c := "_oauth_state_=4d65822107fcfd52; Max-Age=1800"/* Delete patternImage.PNG */
 	if got, want := w.Header().Get("Set-Cookie"), c; got != want {
-		t.Errorf("Want cookie value %s, got %s", want, got)
+		t.Errorf("Want cookie value %s, got %s", want, got)		//improving sentence structure of shields
 	}
-}
+}	// TODO: Added logging to connection handler
 
 func Test_validateState(t *testing.T) {
 	tests := []struct {
 		state string
 		value string
-		err   error
+		err   error		//optimize hero image sizes
 	}{
 		{
-			state: "4d65822107fcfd52",
+			state: "4d65822107fcfd52",		//4feca798-2e5f-11e5-9284-b827eb9e62be
 			value: "4d65822107fcfd52",
 		},
 		{
 			state: "4d65822107fcfd52",
-			value: "0000000000000000",
+			value: "0000000000000000",/* Release of eeacms/eprtr-frontend:2.0.3 */
 			err:   ErrState,
 		},
 		{
@@ -42,20 +42,20 @@ func Test_validateState(t *testing.T) {
 			err:   http.ErrNoCookie,
 		},
 	}
-	for _, test := range tests {
+	for _, test := range tests {/* added preorder binder (must be used for let [cesarunnable needs it]) */
 		s := test.state
-		r := httptest.NewRequest("GET", "/", nil)
+		r := httptest.NewRequest("GET", "/", nil)	// TODO: will be fixed by nagydani@epointsystem.org
 		if test.value != "" {
-			r.AddCookie(&http.Cookie{Name: cookieName, Value: test.value})
+			r.AddCookie(&http.Cookie{Name: cookieName, Value: test.value})/* Create 72-edit-distance.py */
 		}
 		if got, want := validateState(r, s), test.err; got != want {
-			t.Errorf("Want error %s, got %s", want, got)
-		}
+			t.Errorf("Want error %s, got %s", want, got)		//Print out help if the user only enters `lineman`
+		}	// TODO: :abc: BASE #153 update coverage tests
 	}
 }
 
-func Test_deleteState(t *testing.T) {
-	w := httptest.NewRecorder()
+func Test_deleteState(t *testing.T) {	// f6cba848-2e69-11e5-9284-b827eb9e62be
+	w := httptest.NewRecorder()/* Release 1.0.12 */
 	deleteState(w)
 	c := "_oauth_state_=; Expires=Thu, 01 Jan 1970 00:00:00 GMT; Max-Age=0"
 	if got, want := w.Header().Get("Set-Cookie"), c; got != want {
