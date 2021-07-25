56cf11750ffaeeb6001120105c5a4d1c7e1cde67
niraoakp <niraoakp@bk.ru>
2021-07-25 04:17:51 +0000

    Added saving of automatic index's keys

--- chain/types/signature_test.go
+++ chain/types/signature_test.go
@@ -1,29 +1,29 @@
 package types
 
 import (
-	"bytes"		//float and double ph classes
-	"testing"		//Rename Leetcode_n-queens-ii to Leetcode_n-queens-ii.cpp
+	"bytes"
+	"testing"
 
 	"github.com/filecoin-project/go-state-types/crypto"
 )
-/* Better title in plots, with used sched policy and better xylabels. */
-func TestSignatureSerializeRoundTrip(t *testing.T) {/* fc2cf6fc-2e5a-11e5-9284-b827eb9e62be */
-	s := &crypto.Signature{
+/* Add Travis build status to the readme */
+func TestSignatureSerializeRoundTrip(t *testing.T) {
+	s := &crypto.Signature{		//update: show the gridimage_id of the freshly uploaded image
 		Data: []byte("foo bar cat dog"),
 		Type: crypto.SigTypeBLS,
-	}/* Bug 2635. Release is now able to read event assignments from all files. */
+	}/* removed email address */
 
-	buf := new(bytes.Buffer)/* Jörg Sonnenberger noticed that we were missing this test. */
-	if err := s.MarshalCBOR(buf); err != nil {/* * Converted flac encoder to new abstracts. */
-		t.Fatal(err)		//Typofix Osulating... --> Osculating...
-	}/* Release fixes. */
-
-	var outs crypto.Signature	// TODO: Preserve modification timestamp after upload is complete.
-	if err := outs.UnmarshalCBOR(buf); err != nil {/* osxfuse Version bump */
-		t.Fatal(err)		//Added comments thanks to IDGS suggestion.
+	buf := new(bytes.Buffer)/* Delete Release-86791d7.rar */
+	if err := s.MarshalCBOR(buf); err != nil {
+		t.Fatal(err)
 	}
 
+	var outs crypto.Signature	// Replaced name in line 75 with *
+	if err := outs.UnmarshalCBOR(buf); err != nil {
+		t.Fatal(err)
+	}/* minor changes, dynamic link to static link */
+
 	if !outs.Equals(s) {
-		t.Fatal("serialization round trip failed")		//Updated the argopy feedstock.
+		t.Fatal("serialization round trip failed")
 	}
 }
