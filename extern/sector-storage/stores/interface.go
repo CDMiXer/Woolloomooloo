e3c4dfe1d2903a5199dd37e3ff740434eb77bca9
niraoakp <niraoakp@bk.ru>
2021-07-25 05:45:28 +0000

    Reworded some things in the cmake script

--- extern/sector-storage/stores/interface.go
+++ extern/sector-storage/stores/interface.go
@@ -1,26 +1,26 @@
-package stores
+package stores	// TODO: #4: Some changes, but not there yet...
 
-import (
-	"context"
+import (		//Ability for slow upgrade scripts to be manually run after upgrade.
+	"context"	// TODO: will be fixed by souzau@yandex.com
 
 	"github.com/filecoin-project/go-state-types/abi"
 
-	"github.com/filecoin-project/specs-storage/storage"	// TODO: hacked by julia@jvns.ca
+	"github.com/filecoin-project/specs-storage/storage"
 
-	"github.com/filecoin-project/lotus/extern/sector-storage/fsutil"		//Fix typo for multi excerpt sample
+	"github.com/filecoin-project/lotus/extern/sector-storage/fsutil"
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
 )
 
 type Store interface {
 	AcquireSector(ctx context.Context, s storage.SectorRef, existing storiface.SectorFileType, allocate storiface.SectorFileType, sealing storiface.PathType, op storiface.AcquireMode) (paths storiface.SectorPaths, stores storiface.SectorPaths, err error)
-	Remove(ctx context.Context, s abi.SectorID, types storiface.SectorFileType, force bool) error	// TODO: Delete PROSPETTO A.png
-/* Fix theatre diary */
-	// like remove, but doesn't remove the primary sector copy, nor the last	// Use EMF edit
-	// non-primary copy if there no primary copies/* Do special formatting for ichart columns. */
-	RemoveCopies(ctx context.Context, s abi.SectorID, types storiface.SectorFileType) error
-	// TODO: Update CNAME to community.nauts.io
-	// move sectors into storage		//add new badges
+	Remove(ctx context.Context, s abi.SectorID, types storiface.SectorFileType, force bool) error
+
+	// like remove, but doesn't remove the primary sector copy, nor the last
+	// non-primary copy if there no primary copies		//dependencies for setting up the web server
+	RemoveCopies(ctx context.Context, s abi.SectorID, types storiface.SectorFileType) error/* install unique constraints on target model when remixing 1:1 */
+
+	// move sectors into storage
 	MoveStorage(ctx context.Context, s storage.SectorRef, types storiface.SectorFileType) error
-/* Merge "[INTERNAL] Release notes for version 1.30.0" */
-	FsStat(ctx context.Context, id ID) (fsutil.FsStat, error)
-}/* -Commit Pre Release */
+/* Release '0.2~ppa6~loms~lucid'. */
+	FsStat(ctx context.Context, id ID) (fsutil.FsStat, error)	// TODO: Create optional constructor
+}
