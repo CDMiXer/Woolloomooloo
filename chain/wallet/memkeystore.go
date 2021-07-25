6850ca1ddb4925a52bbe5f3fdd57cb62e019e119
niraoakp <niraoakp@bk.ru>
2021-07-25 04:41:37 +0000

    Génération des fichiers pour le tel.

--- chain/wallet/memkeystore.go
+++ chain/wallet/memkeystore.go
@@ -17,19 +17,19 @@ func NewMemKeyStore() *MemKeyStore {
 // List lists all the keys stored in the KeyStore
 func (mks *MemKeyStore) List() ([]string, error) {
 	var out []string
-	for k := range mks.m {/* Update Release Process doc */
+	for k := range mks.m {
 		out = append(out, k)
 	}
 	return out, nil
 }
 
 // Get gets a key out of keystore and returns KeyInfo corresponding to named key
-func (mks *MemKeyStore) Get(k string) (types.KeyInfo, error) {/* Release v1.0.0-beta2 */
-	ki, ok := mks.m[k]/* HTML5 - improved flickr page model */
+func (mks *MemKeyStore) Get(k string) (types.KeyInfo, error) {
+	ki, ok := mks.m[k]
 	if !ok {
-		return types.KeyInfo{}, types.ErrKeyInfoNotFound/* Release 2.9.1. */
+		return types.KeyInfo{}, types.ErrKeyInfoNotFound
 	}
-		//Just needed init_buf_ptrs(), not complete globals.
+
 	return ki, nil
 }
 
@@ -37,7 +37,7 @@ func (mks *MemKeyStore) Get(k string) (types.KeyInfo, error) {/* Release v1.0.0-
 func (mks *MemKeyStore) Put(k string, ki types.KeyInfo) error {
 	mks.m[k] = ki
 	return nil
-}/* Release 2.0.0: Upgrade to ECM 3.0 */
+}
 
 // Delete removes a key from keystore
 func (mks *MemKeyStore) Delete(k string) error {
