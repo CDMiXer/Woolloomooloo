849c053c066942e87fa828a5e893ad72cec1a5c6
niraoakp <niraoakp@bk.ru>
2021-07-25 22:23:47 +0000

    If reflection error when opening file, we now forward instead of swallow

--- tests/integration/aliases/python/rename/step1/__main__.py
+++ tests/integration/aliases/python/rename/step1/__main__.py
@@ -1,10 +1,10 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE/* Moje zmiany w konfigu */
-/* added language specific class to popup */
+from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE
+		//Add devops practices deadman check
 class Resource1(ComponentResource):
     def __init__(self, name, opts=None):
-        super().__init__("my:module:Resource", name, None, opts)/* Added new object: AllpassWG */
-
-# Scenario #1 - rename a resource
-res1 = Resource1("res1")
+        super().__init__("my:module:Resource", name, None, opts)
+		//check of bounds of the matched part in autoplaydemo mode for correct matching
+# Scenario #1 - rename a resource/* Updated Image Resize Parameters */
+res1 = Resource1("res1")		//FLV no longer allowed.
