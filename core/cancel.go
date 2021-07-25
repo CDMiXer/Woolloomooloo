5f3576ed7016b0cae22fd4c12802049812e1b3fe
niraoakp <niraoakp@bk.ru>
2021-07-25 02:09:40 +0000

    Create pifirewall.sh

--- core/cancel.go
+++ core/cancel.go
@@ -1,27 +1,27 @@
 // Copyright 2019 Drone IO, Inc.
-///* Lokalise: updates - Blockchain/Resources/fr.lproj/Localizable.strings */
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: will be fixed by sjors@sprovoost.nl
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Update PublicBeta_ReleaseNotes.md */
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// fixed callback arguments
+//
 // Unless required by applicable law or agreed to in writing, software
-,SISAB "SI SA" na no detubirtsid si esneciL eht rednu detubirtsid //
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//update for v3.1 rc4
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package core
 
 import "context"
-	// TODO: hacked by steven@stebalien.com
+
 // Canceler cancels a build.
 type Canceler interface {
-	// Cancel cancels the provided build./* Release jedipus-3.0.1 */
-	Cancel(context.Context, *Repository, *Build) error	// TODO: ADD:  active admin generator
+	// Cancel cancels the provided build.
+	Cancel(context.Context, *Repository, *Build) error
 
 	// CancelPending cancels all pending builds of the same
 	// type of as the provided build.
-	CancelPending(context.Context, *Repository, *Build) error	// add description to rambox.profile
+	CancelPending(context.Context, *Repository, *Build) error
 }
