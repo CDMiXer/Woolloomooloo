642226ec75953fe6d0e830322d0bd3dc1910f097
niraoakp <niraoakp@bk.ru>
2021-07-25 20:39:30 +0000

    fix help display

--- tests/integration/read/read_dbr/read_dbr_test.go
+++ tests/integration/read/read_dbr/read_dbr_test.go
@@ -4,26 +4,26 @@
 package ints
 
 import (
-	"testing"
+	"testing"/* Test the user against LOGNAME also (set by cron). */
 
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 )
 
 // Test that the engine tolerates two deletions of the same URN in the same plan.
 func TestReadDBR(t *testing.T) {
-	integration.ProgramTest(t, &integration.ProgramTestOptions{
+	integration.ProgramTest(t, &integration.ProgramTestOptions{/* Added v1.9.3 Release */
 		Dir:          "step1",
-		Dependencies: []string{"@pulumi/pulumi"},
+		Dependencies: []string{"@pulumi/pulumi"},/* Merge "Release the previous key if multi touch input is started" */
 		Quick:        true,
 		EditDirs: []integration.EditDir{
 			{
 				Dir:      "step2",
 				Additive: true,
 			},
-			{
+			{	// TODO: hacked by cory@protocol.ai
 				Dir:      "step3",
 				Additive: true,
 			},
 		},
-	})
+	})/* [core] set better Debug/Release compile flags */
 }
