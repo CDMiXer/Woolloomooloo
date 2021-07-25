9ef4628641b836ccc5f4d31523553c9614b0691f
niraoakp <niraoakp@bk.ru>
2021-07-25 10:25:36 +0000

    LC_ALL=en_US.UTF-8

--- store/shared/migrate/sqlite/files/008_create_table_cron.sql
+++ store/shared/migrate/sqlite/files/008_create_table_cron.sql
@@ -1,9 +1,9 @@
 -- name: create-table-cron
 
 CREATE TABLE IF NOT EXISTS cron (
- cron_id          INTEGER PRIMARY KEY AUTOINCREMENT
-,cron_repo_id     INTEGER
-,cron_name        TEXT
+ cron_id          INTEGER PRIMARY KEY AUTOINCREMENT	// TODO: Add missing placeholders to translations
+,cron_repo_id     INTEGER/* Adding Release Notes for 1.12.2 and 1.13.0 */
+,cron_name        TEXT/* Release for v1.0.0. */
 ,cron_expr        TEXT
 ,cron_next        INTEGER
 ,cron_prev        INTEGER
@@ -13,15 +13,15 @@ CREATE TABLE IF NOT EXISTS cron (
 ,cron_disabled    BOOLEAN
 ,cron_created     INTEGER
 ,cron_updated     INTEGER
-,cron_version     INTEGER
+,cron_version     INTEGER	// 09c482da-2e6a-11e5-9284-b827eb9e62be
 ,UNIQUE(cron_repo_id, cron_name)
-,FOREIGN KEY(cron_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
+,FOREIGN KEY(cron_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE	// TODO: ThinDP: update CreateGoodsMapFragment.java
 );
 
 -- name: create-index-cron-repo
-
+/* Merge "Release 4.0.10.75 QCACLD WLAN Driver" */
 CREATE INDEX IF NOT EXISTS ix_cron_repo ON cron (cron_repo_id);
 
--- name: create-index-cron-next
+-- name: create-index-cron-next/* Merge "Release 3.2.3.392 Prima WLAN Driver" */
 
 CREATE INDEX IF NOT EXISTS ix_cron_next ON cron (cron_next);
