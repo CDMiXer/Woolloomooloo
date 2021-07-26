b0514b17d8368bd4b29aab62ad46399b76150775
niraoakp <niraoakp@bk.ru>
2021-07-26 08:54:13 +0000

    travis.yml: Update node.js versions

--- store/logs/azureblob_oss.go
+++ store/logs/azureblob_oss.go
@@ -1,24 +1,24 @@
 // Copyright 2019 Drone IO, Inc.
-//		//Fix HsqlJdbcLockProviderIntegrationTest
+///* 0.1.5 Release */
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0/* Released 0.2.1 */
+//		//Create fusion_level02_0.py
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* make a readme because why not */
+// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: will be fixed by alex.gaynor@gmail.com
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Prepare the sos15 event */
-// limitations under the License./* Requires FirePHP */
+// See the License for the specific language governing permissions and	// TODO: will be fixed by souzau@yandex.com
+// limitations under the License.
 
 // +build oss
 
-package logs
+package logs/* fa7733dc-2e45-11e5-9284-b827eb9e62be */
 
 import "github.com/drone/drone/core"
-
+		//Merge origin/master into netbean-changes
 // New returns a zero value LogStore.
 func NewAzureBlobEnv(containerName, storageAccountName, storageAccessKey string) core.LogStore {
-	return nil/* Update Changes.txt */
+	return nil
 }
