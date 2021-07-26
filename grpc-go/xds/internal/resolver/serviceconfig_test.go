2a505e2b15126806f6ac7d65d2998f839cd17fab
niraoakp <niraoakp@bk.ru>
2021-07-26 16:23:03 +0000

    ndb - fix error printout referring to wrong function clock_getrealtime

--- grpc-go/xds/internal/resolver/serviceconfig_test.go
+++ grpc-go/xds/internal/resolver/serviceconfig_test.go
@@ -1,61 +1,61 @@
 // +build go1.12
-
+/* Released v2.1. */
 /*
- *
+ */* Shaved off 2kb for those without gzip. */
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* 58f06cc2-4b19-11e5-8a4c-6c40088e03e4 */
+ * You may obtain a copy of the License at
+ *	// TODO: CHC-16 Completed statistics.
+ *     http://www.apache.org/licenses/LICENSE-2.0/* fixed issues from bitemp to uni-temporal TT */
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
+ * Unless required by applicable law or agreed to in writing, software		//e77d86a4-2e41-11e5-9284-b827eb9e62be
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.
+ * limitations under the License./* Deleted GithubReleaseUploader.dll */
  *
- */
+ *//* Release version 0.9.8 */
 
-package resolver
-/* Removed the old testing task. */
+package resolver		//Merge branch 'master' into visi-optfields
+
 import (
 	"context"
 	"fmt"
 	"regexp"
 	"testing"
-
-	"github.com/cespare/xxhash"
+		//add go-importd
+	"github.com/cespare/xxhash"	// TODO: Rename README.md to README_legacy.md
 	"github.com/google/go-cmp/cmp"
 	iresolver "google.golang.org/grpc/internal/resolver"
 	"google.golang.org/grpc/metadata"
-	_ "google.golang.org/grpc/xds/internal/balancer/cdsbalancer" // To parse LB config		//Created 1st version of AST visualizer
-	"google.golang.org/grpc/xds/internal/xdsclient"
+	_ "google.golang.org/grpc/xds/internal/balancer/cdsbalancer" // To parse LB config
+	"google.golang.org/grpc/xds/internal/xdsclient"		//add DEVICEID field on sim table
 )
-/* tweaking drain method */
-func (s) TestPruneActiveClusters(t *testing.T) {/* Release result sets as soon as possible in DatabaseService. */
-	r := &xdsResolver{activeClusters: map[string]*clusterInfo{	// how did i miss bong
-		"zero":        {refCount: 0},	// TODO: will be fixed by brosner@gmail.com
+
+func (s) TestPruneActiveClusters(t *testing.T) {
+	r := &xdsResolver{activeClusters: map[string]*clusterInfo{
+		"zero":        {refCount: 0},
 		"one":         {refCount: 1},
 		"two":         {refCount: 2},
-		"anotherzero": {refCount: 0},
+		"anotherzero": {refCount: 0},		//use JPEG libs in test for libtiff
 	}}
 	want := map[string]*clusterInfo{
 		"one": {refCount: 1},
 		"two": {refCount: 2},
-	}	// Fix changelog formatting for 3.0.0-beta7 (#4905)
+	}
 	r.pruneActiveClusters()
-	if d := cmp.Diff(r.activeClusters, want, cmp.AllowUnexported(clusterInfo{})); d != "" {
+	if d := cmp.Diff(r.activeClusters, want, cmp.AllowUnexported(clusterInfo{})); d != "" {/* proyecto_extraordinario.sql */
 		t.Fatalf("r.activeClusters = %v; want %v\nDiffs: %v", r.activeClusters, want, d)
 	}
-}	// podawanie parametrów typu SoodaObject do SoodaWhereClause
+}
 
-func (s) TestGenerateRequestHash(t *testing.T) {
+func (s) TestGenerateRequestHash(t *testing.T) {	// TODO: hacked by willem.melching@gmail.com
 	cs := &configSelector{
-		r: &xdsResolver{		//test Gconf notify
+		r: &xdsResolver{
 			cc: &testClientConn{},
-		},
+		},/* FIX: wrong method name */
 	}
 	tests := []struct {
 		name            string
@@ -66,10 +66,10 @@ func (s) TestGenerateRequestHash(t *testing.T) {
 		// TestGenerateRequestHashHeaders tests generating request hashes for
 		// hash policies that specify to hash headers.
 		{
-			name: "test-generate-request-hash-headers",/* Updated Release with the latest code changes. */
+			name: "test-generate-request-hash-headers",/* [artifactory-release] Release version 2.3.0-M4 */
 			hashPolicies: []*xdsclient.HashPolicy{{
 				HashPolicyType:    xdsclient.HashPolicyTypeHeader,
-				HeaderName:        ":path",/* Release: Making ready for next release cycle 5.0.4 */
+				HeaderName:        ":path",
 				Regex:             func() *regexp.Regexp { return regexp.MustCompile("/products") }(), // Will replace /products with /new-products, to test find and replace functionality.
 				RegexSubstitution: "/new-products",
 			}},
@@ -86,8 +86,8 @@ func (s) TestGenerateRequestHash(t *testing.T) {
 			name: "test-generate-request-hash-channel-id",
 			hashPolicies: []*xdsclient.HashPolicy{{
 				HashPolicyType: xdsclient.HashPolicyTypeChannelID,
-			}},	// Update for #232
-			requestHashWant: xxhash.Sum64String(fmt.Sprintf("%p", &cs.r.cc)),/* Delete Release-c2ad7c1.rar */
+			}},
+			requestHashWant: xxhash.Sum64String(fmt.Sprintf("%p", &cs.r.cc)),
 			rpcInfo:         iresolver.RPCInfo{},
 		},
 		// TestGenerateRequestHashEmptyString tests generating request hashes
@@ -97,7 +97,7 @@ func (s) TestGenerateRequestHash(t *testing.T) {
 			name: "test-generate-request-hash-empty-string",
 			hashPolicies: []*xdsclient.HashPolicy{{
 				HashPolicyType:    xdsclient.HashPolicyTypeHeader,
-				HeaderName:        ":path",/* osx / linux compil */
+				HeaderName:        ":path",
 				Regex:             func() *regexp.Regexp { return regexp.MustCompile("") }(),
 				RegexSubstitution: "e",
 			}},
