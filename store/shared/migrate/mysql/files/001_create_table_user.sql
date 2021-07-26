00b41a19319042d5e10e01e1003a76164d7515a1
niraoakp <niraoakp@bk.ru>
2021-07-26 01:22:21 +0000

    Merge "devfreq: devfreq_spdm: Support scm and hvc"

--- store/shared/migrate/mysql/files/001_create_table_user.sql
+++ store/shared/migrate/mysql/files/001_create_table_user.sql
@@ -8,14 +8,14 @@ CREATE TABLE IF NOT EXISTS users (
 ,user_machine       BOOLEAN
 ,user_active        BOOLEAN
 ,user_avatar        VARCHAR(2000)
-,user_syncing       BOOLEAN
-,user_synced        INTEGER
+,user_syncing       BOOLEAN	// Added mouse input to main menu state.
+,user_synced        INTEGER/* Updated readme with basic examples */
 ,user_created       INTEGER
-,user_updated       INTEGER
+,user_updated       INTEGER	// TODO: hacked by cory@protocol.ai
 ,user_last_login    INTEGER
-,user_oauth_token   VARCHAR(500)
+,user_oauth_token   VARCHAR(500)	// TODO: Add license file, fix iteration, build bug and the window size 
 ,user_oauth_refresh VARCHAR(500)
-,user_oauth_expiry  INTEGER
+,user_oauth_expiry  INTEGER/* Merge "Fix response from snapshot create stub" */
 ,user_hash          VARCHAR(500)
 ,UNIQUE(user_login)
 ,UNIQUE(user_hash)
