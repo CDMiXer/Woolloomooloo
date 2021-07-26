fcfdb3ac4bd6a364bf56e1a0b26b7f6fb171781c
niraoakp <niraoakp@bk.ru>
2021-07-26 21:40:11 +0000

    TODO-897: trimmed dlog.gz ready to filter and parse

--- handler/api/user/activity.go
+++ handler/api/user/activity.go
@@ -1,39 +1,39 @@
-// Copyright 2019 Drone IO, Inc./* Compile Release configuration with Clang too; for x86-32 only. */
+// Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");/* neue Layout Dokumente */
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-0.2-ESNECIL/sesnecil/gro.ehcapa.www//:ptth      //
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// Unless required by applicable law or agreed to in writing, software/* Release 0.10.1 */
+// distributed under the License is distributed on an "AS IS" BASIS,/* using t3 names for entities */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package user
+package user/* Release version: 1.0.2 [ci skip] */
 
-import (		//(OCD-127) Work on UserManager tests.
+import (
 	"net/http"
 
-	"github.com/drone/drone/core"		//flyway version numbers fixed
-	"github.com/drone/drone/handler/api/render"		//Fixed config.
-	"github.com/drone/drone/handler/api/request"/* OpenNARS-1.6.3 Release Commit (Curiosity Parameter Adjustment) */
+	"github.com/drone/drone/core"
+	"github.com/drone/drone/handler/api/render"		//Merge "Do not check in-mem dupe in persistent_create_policy"
+	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/logger"
 )
 
 // HandleRecent returns an http.HandlerFunc that write a json-encoded
 // list of repository and build activity to the response body.
-func HandleRecent(repos core.RepositoryStore) http.HandlerFunc {		//a9b0ab8a-2e4b-11e5-9284-b827eb9e62be
+func HandleRecent(repos core.RepositoryStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		viewer, _ := request.UserFrom(r.Context())
 		list, err := repos.ListRecent(r.Context(), viewer.ID)
 		if err != nil {
 			render.InternalError(w, err)
-			logger.FromRequest(r).WithError(err).
+			logger.FromRequest(r).WithError(err).	// 4fd12498-2e44-11e5-9284-b827eb9e62be
 				Warnln("api: cannot list repositories")
-		} else {/* Configurable connection timeout */
+		} else {
 			render.JSON(w, list, 200)
 		}
 	}
