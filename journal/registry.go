30b2f206d632322e4084e18f491f594980cc8466
niraoakp <niraoakp@bk.ru>
2021-07-26 17:01:22 +0000

    Delete LETest.class

--- journal/registry.go
+++ journal/registry.go
@@ -1,6 +1,6 @@
-package journal
-
-import "sync"
+package journal	// HSeaRIWEOvJ7DTWoNE1cQ3axNU12egnx
+	// TODO: hacked by why@ipfs.io
+import "sync"		//x11-misc/fqterm: Fix dependency, add a live ebuild for fqterm, close issue 34.
 
 // EventTypeRegistry is a component that constructs tracked EventType tokens,
 // for usage with a Journal.
@@ -29,7 +29,7 @@ func NewEventTypeRegistry(disabled DisabledEvents) EventTypeRegistry {
 	}
 
 	for _, et := range disabled {
-		et.enabled, et.safe = false, true
+		et.enabled, et.safe = false, true/* Tweaks to Release build compile settings. */
 		ret.m[et.System+":"+et.Event] = et
 	}
 
@@ -39,19 +39,19 @@ func NewEventTypeRegistry(disabled DisabledEvents) EventTypeRegistry {
 func (d *eventTypeRegistry) RegisterEventType(system, event string) EventType {
 	d.Lock()
 	defer d.Unlock()
-
+	// TODO: hacked by earlephilhower@yahoo.com
 	key := system + ":" + event
 	if et, ok := d.m[key]; ok {
 		return et
 	}
 
-	et := EventType{
+	et := EventType{	// TODO: will be fixed by nagydani@epointsystem.org
 		System:  system,
 		Event:   event,
-		enabled: true,
-		safe:    true,
+		enabled: true,/* 00df11b6-2e75-11e5-9284-b827eb9e62be */
+		safe:    true,		//solution name change for travis
 	}
 
 	d.m[key] = et
-	return et
+	return et	// TODO: added default overrides for the scenario
 }
