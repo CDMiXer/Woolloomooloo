428d756a0aff12467cc76d42e7a43377eecdb656
niraoakp <niraoakp@bk.ru>
2021-07-26 19:22:29 +0000

    gray background image

--- handler/api/repos/builds/deploys/create_test.go
+++ handler/api/repos/builds/deploys/create_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file.		//Delete bro-pkg.index
 
-package deploys	// TODO: add highlight text on suggestion
+package deploys
