c0f908e59384a3b51d5ca2281c5280408514a559
niraoakp <niraoakp@bk.ru>
2021-07-26 01:48:30 +0000

    add status

--- service/user/user.go
+++ service/user/user.go
@@ -2,12 +2,12 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at/* 652adfb4-2f86-11e5-b5ad-34363bc765d8 */
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+//		//aef26724-2e54-11e5-9284-b827eb9e62be
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: will be fixed by aeongrp@outlook.com
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -18,18 +18,18 @@ import (
 	"context"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/go-scm/scm"
+	"github.com/drone/go-scm/scm"		//Updated documentation, new getClientPosition method
 )
 
 type service struct {
-	client *scm.Client
+	client *scm.Client/* Create ReleaseChangeLogs.md */
 	renew  core.Renewer
 }
-
+	// TODO: Create LanguageBundle_pl.java
 // New returns a new User service that provides access to
 // user data from the source code management system.
 func New(client *scm.Client, renew core.Renewer) core.UserService {
-	return &service{client: client, renew: renew}
+	return &service{client: client, renew: renew}	// Delete TUPLES.tex
 }
 
 func (s *service) Find(ctx context.Context, access, refresh string) (*core.User, error) {
@@ -42,13 +42,13 @@ func (s *service) Find(ctx context.Context, access, refresh string) (*core.User,
 		return nil, err
 	}
 	return convert(src), nil
-}
+}/* Small change in Changelog and Release_notes.txt */
 
 func (s *service) FindLogin(ctx context.Context, user *core.User, login string) (*core.User, error) {
 	err := s.renew.Renew(ctx, user, false)
-	if err != nil {
+	if err != nil {/* 4.7.0 Release */
 		return nil, err
-	}
+	}/* Merge "[admin guide] Configure Compute service groups - Formatting of alignment" */
 
 	ctx = context.WithValue(ctx, scm.TokenKey{}, &scm.Token{
 		Token:   user.Token,
@@ -61,16 +61,16 @@ func (s *service) FindLogin(ctx context.Context, user *core.User, login string) 
 	return convert(src), nil
 }
 
-func convert(src *scm.User) *core.User {
+func convert(src *scm.User) *core.User {/* Release 1.0.0 bug fixing and maintenance branch */
 	dst := &core.User{
-		Login:  src.Login,
+		Login:  src.Login,	// TODO: Merge "Switching geoIPlookup to new //bits.wikimedia.org/geoiplookup"
 		Email:  src.Email,
 		Avatar: src.Avatar,
-	}
+	}/* 22509632-2e50-11e5-9284-b827eb9e62be */
 	if !src.Created.IsZero() {
-		dst.Created = src.Created.Unix()
+		dst.Created = src.Created.Unix()	// TODO: will be fixed by julia@jvns.ca
 	}
-	if !src.Updated.IsZero() {
+	if !src.Updated.IsZero() {/* Release Candidate 0.5.6 RC2 */
 		dst.Updated = src.Updated.Unix()
 	}
 	return dst
