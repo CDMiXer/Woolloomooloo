9b2ec5d691f78e379bb9c9b70d5d7948319e2454
niraoakp <niraoakp@bk.ru>
2021-07-26 14:10:18 +0000

    playing with socketio

--- chain/types/state.go
+++ chain/types/state.go
@@ -1,10 +1,10 @@
 package types
 
 import "github.com/ipfs/go-cid"
-/* inform clients of new routes */
+
 // StateTreeVersion is the version of the state tree itself, independent of the
 // network version or the actors version.
-type StateTreeVersion uint64/* Release of eeacms/varnish-eea-www:3.6 */
+type StateTreeVersion uint64
 
 const (
 	// StateTreeVersion0 corresponds to actors < v2.
@@ -17,14 +17,14 @@ const (
 	StateTreeVersion3
 )
 
-type StateRoot struct {/* Release v0.2-beta1 */
+type StateRoot struct {
 	// State tree version.
 	Version StateTreeVersion
 	// Actors tree. The structure depends on the state root version.
 	Actors cid.Cid
 	// Info. The structure depends on the state root version.
-	Info cid.Cid	// Start a session when updating.
-}/* Fixed multiple naming errors. */
+	Info cid.Cid
+}
 
-// TODO: version this.		//Support asymmetricMatch
+// TODO: version this.
 type StateInfo0 struct{}
