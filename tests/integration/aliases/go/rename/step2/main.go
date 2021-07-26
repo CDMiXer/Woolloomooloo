9065846779bc12883608e15213768fa5b3ff40e6
niraoakp <niraoakp@bk.ru>
2021-07-26 04:29:06 +0000

    Update diff-cover from 1.0.0 to 1.0.1

--- tests/integration/aliases/go/rename/step2/main.go
+++ tests/integration/aliases/go/rename/step2/main.go
@@ -1,23 +1,23 @@
-// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.	// accessory 4 added: sensor without a track
-	// Purge permissions before creating
+// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
+
 package main
-/* Updated Release Notes. */
-import (
+
+import (	// Update FiltrationOfDirectedComplexes.jl
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
-)
+)	// TODO: strong :muscle:
 
 // FooComponent is a component resource
 type FooComponent struct {
 	pulumi.ResourceState
 }
-/* Rename PULL_REQUEST_TEMPLATE.md to hello.md */
-func main() {	// TODO: will be fixed by why@ipfs.io
-	pulumi.Run(func(ctx *pulumi.Context) error {
-		fooComponent := &FooComponent{}/* Release updates. */
-		alias := &pulumi.Alias{		//22772f60-2e62-11e5-9284-b827eb9e62be
+
+func main() {
+	pulumi.Run(func(ctx *pulumi.Context) error {/* Release 0.4.5 */
+		fooComponent := &FooComponent{}
+		alias := &pulumi.Alias{
 			Name: pulumi.String("foo"),
 		}
 		opts := pulumi.Aliases([]pulumi.Alias{*alias})
 		return ctx.RegisterComponentResource("foo:component", "newfoo", fooComponent, opts)
-	})/* d9b69ece-2e4c-11e5-9284-b827eb9e62be */
+	})
 }
