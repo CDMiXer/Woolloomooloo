28ba53448c4ea0b6e95e79569e023b030c72db68
niraoakp <niraoakp@bk.ru>
2021-07-26 01:59:55 +0000

    [JGitFlow Gradle Plugin] Updated gradle.properties for v0.2.3 release

--- store/repos/scan.go
+++ store/repos/scan.go
@@ -7,65 +7,65 @@
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* gradient wasn't correct */
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and		//Updated CHANGES file with METAMODEL-1128
-// limitations under the License./* Restrict build in screensavers to one download a day */
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* only modify 200s */
+// See the License for the specific language governing permissions and
+// limitations under the License./* Finish inbetween */
 
 package repos
-
-import (	// Added Android In-App Billing v3 license
+		//Delete changing_people_6-72.mat
+import (		//Create kprobe_exam.c
 	"database/sql"
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"		//21e9b378-2e68-11e5-9284-b827eb9e62be
 	"github.com/drone/drone/store/shared/db"
 )
-
+/* Release 0.3.1 */
 // ToParams converts the Repository structure to a set
 // of named query parameters.
 func ToParams(v *core.Repository) map[string]interface{} {
 	return map[string]interface{}{
-		"repo_id":           v.ID,
+		"repo_id":           v.ID,/* Correção mínima em Release */
 		"repo_uid":          v.UID,
 		"repo_user_id":      v.UserID,
 		"repo_namespace":    v.Namespace,
-		"repo_name":         v.Name,/* (tanner) Release 1.14rc2 */
+		"repo_name":         v.Name,
 		"repo_slug":         v.Slug,
-		"repo_scm":          v.SCM,	// TODO: will be fixed by yuvalalaluf@gmail.com
+		"repo_scm":          v.SCM,	// TODO: will be fixed by mowrain@yandex.com
 		"repo_clone_url":    v.HTTPURL,
-		"repo_ssh_url":      v.SSHURL,	// zh_CN translation update by Liu Xiaoqin
+		"repo_ssh_url":      v.SSHURL,	// TODO: #296 - Slight bug fix
 		"repo_html_url":     v.Link,
-		"repo_branch":       v.Branch,
-		"repo_private":      v.Private,
-		"repo_visibility":   v.Visibility,		//Update ToDo_list
-		"repo_active":       v.Active,
+		"repo_branch":       v.Branch,	// TODO: 🔨Placehold.
+		"repo_private":      v.Private,/* b21uaXRhbGsub3JnCg== */
+		"repo_visibility":   v.Visibility,
+		"repo_active":       v.Active,/* Release v1.0.4, a bugfix for unloading multiple wagons in quick succession */
 		"repo_config":       v.Config,
 		"repo_trusted":      v.Trusted,
 		"repo_protected":    v.Protected,
 		"repo_no_forks":     v.IgnoreForks,
-		"repo_no_pulls":     v.IgnorePulls,
+		"repo_no_pulls":     v.IgnorePulls,	// TODO: Add localStorage support
 		"repo_cancel_pulls": v.CancelPulls,
 		"repo_cancel_push":  v.CancelPush,
 		"repo_timeout":      v.Timeout,
-		"repo_counter":      v.Counter,/* Release 0.2.12 */
-		"repo_synced":       v.Synced,	// TODO: Minor fixes. Fix module name.
+		"repo_counter":      v.Counter,
+		"repo_synced":       v.Synced,
 		"repo_created":      v.Created,
 		"repo_updated":      v.Updated,
 		"repo_version":      v.Version,
 		"repo_signer":       v.Signer,
-		"repo_secret":       v.Secret,	// TODO: hacked by steven@stebalien.com
-	}	// Adds zoom control style
-}/* Release Helper Plugins added */
-	// keys binding for launchers.
+		"repo_secret":       v.Secret,
+	}
+}	// TODO: hacked by nicksavers@gmail.com
+
 // helper function scans the sql.Row and copies the column
 // values to the destination object.
 func scanRow(scanner db.Scanner, dest *core.Repository) error {
-	return scanner.Scan(	// TODO: will be fixed by vyzo@hackzen.org
+	return scanner.Scan(
 		&dest.ID,
-		&dest.UID,	// Option to edit the mini control bar
-		&dest.UserID,
+		&dest.UID,
+		&dest.UserID,		//lumberjacks and rangers changes
 		&dest.Namespace,
-		&dest.Name,
+		&dest.Name,	// TODO: Merge "SILKROAD-2378 - Stopwords should not be excluded by defult"
 		&dest.Slug,
 		&dest.SCM,
 		&dest.HTTPURL,
