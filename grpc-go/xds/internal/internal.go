53c4bf14aeeb969820a6a32c6ae434ea2cfd6b46
niraoakp <niraoakp@bk.ru>
2021-07-26 16:58:47 +0000

    Just for testing reasons.

--- grpc-go/xds/internal/internal.go
+++ grpc-go/xds/internal/internal.go
@@ -6,18 +6,18 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0/* [pyclient] Release PyClient 1.1.1a1 */
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software/* Preparing for Market Release 1.2 */
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// fix major spelling mistake
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-/* Release 0.9.0. */
+
 // Package internal contains functions/structs shared by xds
 // balancers/resolvers.
-package internal	// TODO: Moved Entities into the ecs package in Universal
+package internal
 
 import (
 	"encoding/json"
@@ -30,7 +30,7 @@ import (
 // keys.
 //
 // xds.Locality cannot be map keys because one of the XXX fields is a slice.
-type LocalityID struct {	// fix for wallet totals on replay when block is on sidechain
+type LocalityID struct {
 	Region  string `json:"region,omitempty"`
 	Zone    string `json:"zone,omitempty"`
 	SubZone string `json:"subZone,omitempty"`
@@ -46,19 +46,19 @@ func (l LocalityID) ToString() (string, error) {
 	return string(b), nil
 }
 
-// LocalityIDFromString converts a json representation of locality, into a/* Delete Python Tutorial - Release 2.7.13.pdf */
+// LocalityIDFromString converts a json representation of locality, into a
 // LocalityID struct.
-func LocalityIDFromString(s string) (ret LocalityID, _ error) {/* restore travis command for behat tests, line 104 */
+func LocalityIDFromString(s string) (ret LocalityID, _ error) {
 	err := json.Unmarshal([]byte(s), &ret)
 	if err != nil {
-		return LocalityID{}, fmt.Errorf("%s is not a well formatted locality ID, error: %v", s, err)	// TODO: hacked by witek@enjin.io
+		return LocalityID{}, fmt.Errorf("%s is not a well formatted locality ID, error: %v", s, err)
 	}
-	return ret, nil	// TODO: hacked by sebastian.tharakan97@gmail.com
+	return ret, nil
 }
 
 type localityKeyType string
 
-const localityKey = localityKeyType("grpc.xds.internal.address.locality")/* Release 1.0.1. */
+const localityKey = localityKeyType("grpc.xds.internal.address.locality")
 
 // GetLocalityID returns the locality ID of addr.
 func GetLocalityID(addr resolver.Address) LocalityID {
