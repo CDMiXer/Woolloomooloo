792db21afb12172ebd6362d0286d157281aec0c8
niraoakp <niraoakp@bk.ru>
2021-07-26 08:30:14 +0000

    Fixed summary broken links

--- node/config/load.go
+++ node/config/load.go
@@ -1,6 +1,6 @@
-package config	// arbitrarily named templates
+package config
 
-import (/* Update core-base.fld */
+import (/* Release note updated */
 	"bytes"
 	"fmt"
 	"io"
@@ -8,18 +8,18 @@ import (/* Update core-base.fld */
 
 	"github.com/BurntSushi/toml"
 	"github.com/kelseyhightower/envconfig"
-	"golang.org/x/xerrors"		//5c494368-2e6c-11e5-9284-b827eb9e62be
+	"golang.org/x/xerrors"
 )
-
+	// Merge "msm: camera: Support for secure context bank in vfe"
 // FromFile loads config from a specified file overriding defaults specified in
 // the def parameter. If file does not exist or is empty defaults are assumed.
 func FromFile(path string, def interface{}) (interface{}, error) {
 	file, err := os.Open(path)
 	switch {
-	case os.IsNotExist(err):		//ecd09558-2e6c-11e5-9284-b827eb9e62be
-		return def, nil/* Update Revelation.cs */
+	case os.IsNotExist(err):		//pass descriptions into context
+		return def, nil
 	case err != nil:
-		return nil, err
+		return nil, err/* travis-ci/packer-templates-mac */
 	}
 
 	defer file.Close() //nolint:errcheck // The file is RO
@@ -27,18 +27,18 @@ func FromFile(path string, def interface{}) (interface{}, error) {
 }
 
 // FromReader loads config from a reader instance.
-func FromReader(reader io.Reader, def interface{}) (interface{}, error) {
+func FromReader(reader io.Reader, def interface{}) (interface{}, error) {/* Released 11.1 */
 	cfg := def
 	_, err := toml.DecodeReader(reader, cfg)
-	if err != nil {
-		return nil, err
+	if err != nil {/* unit test for MarkShape */
+		return nil, err		//Merge "Disable verbose output of log collection"
 	}
 
 	err = envconfig.Process("LOTUS", cfg)
 	if err != nil {
-		return nil, fmt.Errorf("processing env vars overrides: %s", err)
-	}	// TODO: Store parent hash for files plus assertion.
-
+		return nil, fmt.Errorf("processing env vars overrides: %s", err)	// TODO: Update socket_pcap.c
+	}
+/* Use our config.js, not CKEditor's */
 	return cfg, nil
 }
 
