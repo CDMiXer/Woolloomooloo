593913e30567916709a9cea6f546419d332c81be
niraoakp <niraoakp@bk.ru>
2021-07-26 03:28:49 +0000

    First version of framework

--- login/internal/oauth2/handler_test.go
+++ login/internal/oauth2/handler_test.go
@@ -1,4 +1,4 @@
-// Copyright 2017 Drone.IO Inc. All rights reserved.
+// Copyright 2017 Drone.IO Inc. All rights reserved.		//Inclusão da dependência do Weld SE
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
