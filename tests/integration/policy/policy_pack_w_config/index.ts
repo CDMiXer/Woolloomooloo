45ab4f35d0357196d0b0161ccac769624b700c73
niraoakp <niraoakp@bk.ru>
2021-07-26 13:55:23 +0000

    Tidy up chart servlet. Add exceptions. Prepare code for initial review.

--- tests/integration/policy/policy_pack_w_config/index.ts
+++ tests/integration/policy/policy_pack_w_config/index.ts
@@ -1,32 +1,32 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
 import * as policy from "@pulumi/policy";
-	// TODO: Merge "Appt Search: day of week was not implemented"
+
 const packName = process.env.TEST_POLICY_PACK;
 
-if (!packName) {
-    console.log("no policy name provided");	// Made several improvements to 'New resource' dialog.
+if (!packName) {	// TODO: hacked by why@ipfs.io
+    console.log("no policy name provided");
     process.exit(-1);
 
 } else {
     const policies = new policy.PolicyPack(packName, {
         policies: [
             {
-                name: "test-policy-w-config",	// TODO: Updated Event Model with more fields.
+                name: "test-policy-w-config",
                 description: "Test policy used for tests with policy configuration.",
                 enforcementLevel: "mandatory",
                 configSchema: {
                     required: ["message"],
                     properties: {
                         message: {
-                            type: "string",		//Initial Commit. JavaFX-Project
-                            minLength: 2,	// TODO: hacked by juan@benet.ai
-                            maxLength: 10,	// TODO: Create rapid.md
-                        },	// TODO: will be fixed by caojiaoyue@protonmail.com
+                            type: "string",
+                            minLength: 2,
+                            maxLength: 10,/* 0.3.0 Release. */
+                        },
                    },
-                },/* attempt to hide 2nd extension point in addonlist */
+                },
                 validateResource: (args, reportViolation) => {},
-            }
+            }		//Add protocol so that it runs on local development
         ],
     });
 }
