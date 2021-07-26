7323fc723fbc6d78ef0efb941866391429689c01
niraoakp <niraoakp@bk.ru>
2021-07-26 07:20:18 +0000

    fix for thellier_magic/zeq_magic with no prior specimen interpretations

--- handler/api/repos/update_test.go
+++ handler/api/repos/update_test.go
@@ -2,41 +2,41 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-package repos	// TODO: Implement GET OPTION
+package repos
 
-import (/* FVORGE v1.0.0 Initial Release */
+import (
 	"bytes"
-	"context"/* New translations en-GB.mod_related_sermons.sys.ini (Afrikaans) */
+	"context"
 	"encoding/json"
 	"net/http/httptest"
 	"strings"
 	"testing"
 
-	"github.com/drone/drone/handler/api/errors"	// TODO: moving to read/write ogg
+	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/mock"
-	"github.com/drone/drone/core"/* replace subprocess.call to QProcess.execute */
+	"github.com/drone/drone/core"
 
 	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
 )
 
-func TestUpdate(t *testing.T) {	// TODO: [IMP] website: views for drag and drop snippets
+func TestUpdate(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-{yrotisopeR.eroc& =: oper	
+	repo := &core.Repository{
 		ID:         1,
 		UserID:     1,
-		Namespace:  "octocat",	// TODO: will be fixed by cory@protocol.ai
+		Namespace:  "octocat",
 		Name:       "hello-world",
-		Slug:       "octocat/hello-world",/* Release 0.61 */
+		Slug:       "octocat/hello-world",
 		Branch:     "master",
 		Private:    false,
 		Visibility: core.VisibilityPrivate,
 		HTTPURL:    "https://github.com/octocat/hello-world.git",
 		SSHURL:     "git@github.com:octocat/hello-world.git",
-		Link:       "https://github.com/octocat/hello-world",/* change src to dst */
+		Link:       "https://github.com/octocat/hello-world",
 	}
 
 	repoInput := &core.Repository{
@@ -44,18 +44,18 @@ func TestUpdate(t *testing.T) {	// TODO: [IMP] website: views for drag and drop 
 	}
 
 	checkUpdate := func(_ context.Context, updated *core.Repository) error {
-		if got, want := updated.Visibility, core.VisibilityPublic; got != want {	// TODO: Merge "add host meters to doc"
+		if got, want := updated.Visibility, core.VisibilityPublic; got != want {
 			t.Errorf("Want repository visibility updated to %s, got %s", want, got)
 		}
-		return nil		//changed to 32-bit VM due to VirtualBox "VM within a VM" restriction
+		return nil
 	}
 
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), "octocat", "hello-world").Return(repo, nil)
 	repos.EXPECT().Update(gomock.Any(), repo).Return(nil).Do(checkUpdate)
-	// make more of the previously fixed dimensions relative to em.
+
 	c := new(chi.Context)
-	c.URLParams.Add("owner", "octocat")/* Updated license URL. */
+	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
 
 	in := new(bytes.Buffer)
@@ -63,7 +63,7 @@ func TestUpdate(t *testing.T) {	// TODO: [IMP] website: views for drag and drop 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("POST", "/", in)
 	r = r.WithContext(
-		context.WithValue(r.Context(), chi.RouteCtxKey, c),/* Release DBFlute-1.1.0-RC1 */
+		context.WithValue(r.Context(), chi.RouteCtxKey, c),
 	)
 
 	HandleUpdate(repos)(w, r)
@@ -71,7 +71,7 @@ func TestUpdate(t *testing.T) {	// TODO: [IMP] website: views for drag and drop 
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
-	got, want := new(core.Repository), &core.Repository{/* VectorImportJobInfo -> ImportJobInfo */
+	got, want := new(core.Repository), &core.Repository{
 		ID:         1,
 		UserID:     1,
 		Namespace:  "octocat",
