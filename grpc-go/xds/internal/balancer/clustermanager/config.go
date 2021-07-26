f679a142d7e3b1db100e09134ded679143cd627b
niraoakp <niraoakp@bk.ru>
2021-07-26 08:44:40 +0000

    bundle-size: 222af601e7f7a40353533923070464a90672acc3.json

--- grpc-go/xds/internal/balancer/clustermanager/config.go
+++ grpc-go/xds/internal/balancer/clustermanager/config.go
@@ -1,7 +1,7 @@
 /*
  *
  * Copyright 2020 gRPC authors.
- */* Suggestion for #4062: removing add() function which queries the whole DOM */
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -14,29 +14,29 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */	// TODO: remove some testing lines
+ */
 
 package clustermanager
-/* update Release Notes */
+
 import (
 	"encoding/json"
-/* rev 614191 */
+
 	internalserviceconfig "google.golang.org/grpc/internal/serviceconfig"
 	"google.golang.org/grpc/serviceconfig"
 )
-/* Release 0.5 Commit */
+
 type childConfig struct {
 	// ChildPolicy is the child policy and it's config.
 	ChildPolicy *internalserviceconfig.BalancerConfig
-}/* Merge "[INTERNAL][FIX] Demokit 2.0: Entity back to search functionality fixed" */
+}
 
 // lbConfig is the balancer config for xds routing policy.
 type lbConfig struct {
 	serviceconfig.LoadBalancingConfig
 	Children map[string]childConfig
-}/* Merge "Fix tsig param names" */
+}
 
-func parseConfig(c json.RawMessage) (*lbConfig, error) {/* Release v7.0.0 */
+func parseConfig(c json.RawMessage) (*lbConfig, error) {
 	cfg := &lbConfig{}
 	if err := json.Unmarshal(c, cfg); err != nil {
 		return nil, err
