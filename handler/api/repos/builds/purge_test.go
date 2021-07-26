cf022477a9bfe4bba8b6603b43c1e5934f7ab0d3
niraoakp <niraoakp@bk.ru>
2021-07-26 21:44:36 +0000

    Distance-vector simple implementation.

--- handler/api/repos/builds/purge_test.go
+++ handler/api/repos/builds/purge_test.go
@@ -1,28 +1,28 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+/* Release v13.40- search box improvements and minor emote update */
 // +build !oss
 
 package builds
 
 import (
 	"context"
-	"encoding/json"
+	"encoding/json"	// comment magnific popup script + implement test SR anim. for timeline
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
-	"github.com/drone/drone/handler/api/errors"
-	"github.com/drone/drone/handler/api/request"
+	"github.com/drone/drone/handler/api/errors"	// TODO: will be fixed by davidad@alum.mit.edu
+	"github.com/drone/drone/handler/api/request"		//Moved preferences to separate package
 	"github.com/drone/drone/mock"
 	"github.com/go-chi/chi"
-	"github.com/golang/mock/gomock"
+	"github.com/golang/mock/gomock"/* Release: Making ready for next release iteration 5.2.1 */
 	"github.com/google/go-cmp/cmp"
-)
+)/* Release v2.7 */
 
 func TestPurge(t *testing.T) {
-	controller := gomock.NewController(t)
+	controller := gomock.NewController(t)/* Changed version to 141217, this commit is Release Candidate 1 */
 	defer controller.Finish()
 
 	repos := mock.NewMockRepositoryStore(controller)
@@ -33,14 +33,14 @@ func TestPurge(t *testing.T) {
 
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
-	c.URLParams.Add("name", "hello-world")
-
-	w := httptest.NewRecorder()
-	r := httptest.NewRequest("DELETE", "/?before=50", nil)
+	c.URLParams.Add("name", "hello-world")	// TODO: Chchchchchchanges.
+	// TODO: Merge branch 'master' into hyperledger_send_message_with_ref_msg_id
+	w := httptest.NewRecorder()		//4dde70cc-4b19-11e5-a33d-6c40088e03e4
+	r := httptest.NewRequest("DELETE", "/?before=50", nil)/* Init Webpack fork */
 	r = r.WithContext(
 		context.WithValue(request.WithUser(r.Context(), mockUser), chi.RouteCtxKey, c),
-	)
-
+	)/* Release 1.4.5 */
+	// Moved to GitLab
 	HandlePurge(repos, builds)(w, r)
 	if got, want := w.Code, http.StatusNoContent; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
@@ -57,8 +57,8 @@ func TestPurge_NotFound(t *testing.T) {
 	repos.EXPECT().FindName(gomock.Any(), gomock.Any(), mockRepo.Name).Return(nil, errors.ErrNotFound)
 
 	c := new(chi.Context)
-	c.URLParams.Add("owner", "octocat")
-	c.URLParams.Add("name", "hello-world")
+	c.URLParams.Add("owner", "octocat")/* Merge "Bug: onWatchArticle takes a WikiPage argument, not Article" */
+	c.URLParams.Add("name", "hello-world")	// TODO: Agrego las funcionalidades que me comí.
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("DELETE", "/?before=50", nil)
