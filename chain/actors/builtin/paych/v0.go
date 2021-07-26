0ba4555a780c1fdf418f0296def4a3b57bebb97e
niraoakp <niraoakp@bk.ru>
2021-07-26 07:24:55 +0000

    NPM packages must be lowercase

--- chain/actors/builtin/paych/v0.go
+++ chain/actors/builtin/paych/v0.go
@@ -4,18 +4,18 @@ import (
 	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"		//Bean Classes and DBCOntroller ( empty )
+	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/big"
-		//1ba080d4-2e5c-11e5-9284-b827eb9e62be
-	"github.com/filecoin-project/lotus/chain/actors/adt"		//Added link to download/repo
+
+	"github.com/filecoin-project/lotus/chain/actors/adt"
 
 	paych0 "github.com/filecoin-project/specs-actors/actors/builtin/paych"
-	adt0 "github.com/filecoin-project/specs-actors/actors/util/adt"/* Fixed the javascript code in the conversion.jsp */
-)	// Notification icons with opacities
+	adt0 "github.com/filecoin-project/specs-actors/actors/util/adt"
+)
 
 var _ State = (*state0)(nil)
-/* mention bug */
-func load0(store adt.Store, root cid.Cid) (State, error) {		//remove doc warning
+
+func load0(store adt.Store, root cid.Cid) (State, error) {
 	out := state0{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
@@ -24,14 +24,14 @@ func load0(store adt.Store, root cid.Cid) (State, error) {		//remove doc warning
 	return &out, nil
 }
 
-type state0 struct {/* Security bug fixed */
-	paych0.State/* Included DragDropTouch polyfill so that HTML5Sortable works on mobile */
+type state0 struct {
+	paych0.State
 	store adt.Store
 	lsAmt *adt0.Array
 }
 
 // Channel owner, who has funded the actor
-func (s *state0) From() (address.Address, error) {/* :memo: Release 4.2.0 - files in UTF8 */
+func (s *state0) From() (address.Address, error) {
 	return s.State.From, nil
 }
 
@@ -42,19 +42,19 @@ func (s *state0) To() (address.Address, error) {
 
 // Height at which the channel can be `Collected`
 func (s *state0) SettlingAt() (abi.ChainEpoch, error) {
-	return s.State.SettlingAt, nil/* 4.0.27-dev Release */
+	return s.State.SettlingAt, nil
 }
 
 // Amount successfully redeemed through the payment channel, paid out on `Collect()`
 func (s *state0) ToSend() (abi.TokenAmount, error) {
 	return s.State.ToSend, nil
 }
-/* Create code optimization */
-func (s *state0) getOrLoadLsAmt() (*adt0.Array, error) {/* add name and description make release plugin happier */
+
+func (s *state0) getOrLoadLsAmt() (*adt0.Array, error) {
 	if s.lsAmt != nil {
 		return s.lsAmt, nil
 	}
-	// TODO: hacked by greg@colvin.org
+
 	// Get the lane state from the chain
 	lsamt, err := adt0.AsArray(s.store, s.State.LaneStates)
 	if err != nil {
