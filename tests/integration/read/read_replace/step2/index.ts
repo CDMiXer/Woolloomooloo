9596230757e9a502dc215398810c2eec92c343ff
niraoakp <niraoakp@bk.ru>
2021-07-26 19:15:06 +0000

    Fix merge artefact

--- tests/integration/read/read_replace/step2/index.ts
+++ tests/integration/read/read_replace/step2/index.ts
@@ -1,9 +1,9 @@
-// Copyright 2016-2018, Pulumi Corporation.		//Merge "Fix PEP8 issues ."
+// Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+///* Update ServiceConfiguration.Release.cscfg */
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
@@ -14,14 +14,14 @@
 
 import { Resource } from "./resource";
 
-// Resource A was read in the previous plan, but it's now created.
-const a = new Resource("a", { state: 42 });
+// Resource A was read in the previous plan, but it's now created./* Fix undefined var */
+const a = new Resource("a", { state: 42 });	// TODO: hacked by ligi@ligi.de
 
-// B must be replaced.
+// B must be replaced.	// TODO: Delete IMG_7329.JPG
 const b = new Resource("b", { state: a.state.apply((b: any) => b + 1)});
-
+	// TODO: will be fixed by witek@enjin.io
 // The engine generates:
 // A: CreateReplacement
-// B: CreateReplacement
+// B: CreateReplacement/* b444f02c-2e3e-11e5-9284-b827eb9e62be */
 // B: DeleteReplacement
-// A: DeleteReplacement
+// A: DeleteReplacement/* nix links section from ios calendar detail */
