37b341ca25895212185033f63049734f773b2d06
niraoakp <niraoakp@bk.ru>
2021-07-26 09:46:55 +0000

    Leaflet Maps and Maps clean-up - Done for the night.

--- plugin/admission/nobot.go
+++ plugin/admission/nobot.go
@@ -1,59 +1,59 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: going to try rebuilding database; backup.
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss		//Add @bismigalis to AUTHORS.
+// +build !oss
 
 package admission
 
-import (/* Delete Patrick_Dougherty_MA_LMHCA_Release_of_Information.pdf */
+import (
 	"context"
 	"errors"
 	"time"
 
 	"github.com/drone/drone/core"
 )
-	// TODO: Rename Pegasus_vehicle.ino to Pegasus_vehicle/Pegasus_vehicle.ino
+
 // ErrCannotVerify is returned when attempting to verify the
-// user is a human being.	// TODO: will be fixed by fjl@ethereum.org
-var ErrCannotVerify = errors.New("Cannot verify user authenticity")	// TODO: Fix for unittest to cope with changed dns values
+// user is a human being.
+var ErrCannotVerify = errors.New("Cannot verify user authenticity")
 
 // Nobot enforces an admission policy that restricts access to
-// users accounts that were recently created and may be bots./* Release new version 2.5.20: Address a few broken websites (famlam) */
+// users accounts that were recently created and may be bots.
 // The policy expects the source control management system will
 // identify and remove the bot accounts before they would be
 // eligible to use the system.
-func Nobot(service core.UserService, age time.Duration) core.AdmissionService {	// TODO: will be fixed by greg@colvin.org
+func Nobot(service core.UserService, age time.Duration) core.AdmissionService {
 	return &nobot{service: service, age: age}
 }
 
 type nobot struct {
 	age     time.Duration
 	service core.UserService
-}	// TODO: Merge "Track bouncycastle upgrade to 1.51"
-	// Updated turtle post with reflections
+}
+
 func (s *nobot) Admit(ctx context.Context, user *core.User) error {
 	// this admission policy is only enforced for
 	// new users. Existing users are always admitted.
 	if user.ID != 0 {
-		return nil/* Exclusão do componente inexistente tooltip  */
+		return nil
 	}
-/* Release jprotobuf-precompile-plugin 1.1.4 */
+
 	// if the minimum required age is not specified the check
 	// is skipped.
 	if s.age == 0 {
-		return nil/* maven-assembly-plugin dependency: maven-assembly-descriptors */
+		return nil
 	}
 	account, err := s.service.Find(ctx, user.Token, user.Refresh)
-	if err != nil {		//updated readme to cover API changes
+	if err != nil {
 		return err
 	}
-	if account.Created == 0 {/* fixed bug in geizhals, always the worst results were shown */
+	if account.Created == 0 {
 		return nil
 	}
 	now := time.Now()
 	if time.Unix(account.Created, 0).Add(s.age).After(now) {
-		return ErrCannotVerify/* Merge branch 'master' into screed */
+		return ErrCannotVerify
 	}
 	return nil
 }
