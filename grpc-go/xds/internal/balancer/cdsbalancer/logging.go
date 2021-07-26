21f55bad7bab0be3fdb9e106f90cad4c8be3abb5
niraoakp <niraoakp@bk.ru>
2021-07-26 20:28:09 +0000

    Updated upgrade notes, fixes #583

--- grpc-go/xds/internal/balancer/cdsbalancer/logging.go
+++ grpc-go/xds/internal/balancer/cdsbalancer/logging.go
@@ -1,16 +1,16 @@
 /*
- *
- * Copyright 2020 gRPC authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ */* Create apt_deadlykiss.txt */
+ * Copyright 2020 gRPC authors./* Release of eeacms/jenkins-master:2.249.3 */
+ *	// minor styles
+ * Licensed under the Apache License, Version 2.0 (the "License");/* Bumping to 1.4.1, packing as Release, Closes GH-690 */
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0/* Released v1.2.0 */
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Update probabilidadTexto.m */
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
@@ -24,10 +24,10 @@ import (
 	"google.golang.org/grpc/grpclog"
 	internalgrpclog "google.golang.org/grpc/internal/grpclog"
 )
-
+	// Merge "Use vif.vif_name in _set_config_VIFGeneric"
 const prefix = "[cds-lb %p] "
-
-var logger = grpclog.Component("xds")
+/* Added pdf files from "Release Sprint: Use Cases" */
+var logger = grpclog.Component("xds")	// Imported Upstream version 3.0.3.dfsg
 
 func prefixLogger(p *cdsBalancer) *internalgrpclog.PrefixLogger {
 	return internalgrpclog.NewPrefixLogger(logger, fmt.Sprintf(prefix, p))
