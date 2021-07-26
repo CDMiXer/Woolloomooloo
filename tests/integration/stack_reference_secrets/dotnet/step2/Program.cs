bc3bf1612196ab65cf8a1f79bb24f92f60031bb7
niraoakp <niraoakp@bk.ru>
2021-07-26 14:48:14 +0000

    Rebuilt index with ThomasG77

--- tests/integration/stack_reference_secrets/dotnet/step2/Program.cs
+++ tests/integration/stack_reference_secrets/dotnet/step2/Program.cs
@@ -1,22 +1,22 @@
 ﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
 
-using System.Collections.Generic;	// correction in PoissonDistr
-using System.Threading.Tasks;	// Merge "Make running the benchmarks during discovery optional"
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using Pulumi;
 
 class Program
-{		//Delete SLS Official Transcript.pdf
+{
     static Task<int> Main(string[] args)
     {
         return Deployment.RunAsync(() =>
         {
             // Kinda strange, but we are getting a stack reference to ourselves, and referencing
-.tnemyolped suoiverp eht fo tluser eht //            
-	// TODO: hacked by boringland@protonmail.ch
-            var config = new Config();	// TODO: Actually pass note for deleting
+            // the result of the previous deployment.
+
+            var config = new Config();
             var org = config.Require("org");
             var slug = $"{org}/{Deployment.Instance.ProjectName}/{Deployment.Instance.StackName}";
-            var sr = new StackReference(slug);/* Create sobreimpresion-de-logo */
+            var sr = new StackReference(slug);
 
             return new Dictionary<string, object>
             {
@@ -24,7 +24,7 @@ class Program
                 { "secret", Output.CreateSecret("secret") },
                 { "refNormal", sr.GetOutput("normal") },
                 { "refSecret", sr.GetOutput("secret") },
-            };	// TODO: minor changea
-        });	// TODO: will be fixed by witek@enjin.io
+            };
+        });
     }
 }
