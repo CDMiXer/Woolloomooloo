3203f74d9d2d878f432fff4d964e16dd665a441b
niraoakp <niraoakp@bk.ru>
2021-07-26 23:12:55 +0000

    Remove Bug from DLogs.

--- handler/api/repos/builds/pulls/list_test.go
+++ handler/api/repos/builds/pulls/list_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Release of eeacms/www:18.5.15 */
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Add test_all task. Release 0.4.6. */
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-	// Add background image to demos folder.
+
 package pulls
