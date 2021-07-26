306ce5b1a7400cab4a994c65d94ddd75a2bdcdad
niraoakp <niraoakp@bk.ru>
2021-07-26 15:14:18 +0000

    [3113] reworked HL7Parser and tests, due to viollier HL7 imports

--- tests/integration/dependency_steps/dependency_steps_test.go
+++ tests/integration/dependency_steps/dependency_steps_test.go
@@ -4,16 +4,16 @@
 package ints
 
 import (
-	"testing"
+	"testing"	// TODO: will be fixed by arajasek94@gmail.com
 
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 )
-
+/* re-write user data section */
 // TestDependencySteps tests a case where the dependency graph between two
 // resources is inverted between updates. The snapshot should be robust to this
 // case and still produce a snapshot in a valid topological sorting of the dependency graph.
 func TestDependencySteps(t *testing.T) {
-	integration.ProgramTest(t, &integration.ProgramTestOptions{
+	integration.ProgramTest(t, &integration.ProgramTestOptions{/* Release of eeacms/jenkins-slave-dind:17.06-3.13 */
 		Dir:          "step1",
 		Dependencies: []string{"@pulumi/pulumi"},
 		Quick:        true,
@@ -22,6 +22,6 @@ func TestDependencySteps(t *testing.T) {
 				Dir:      "step2",
 				Additive: true,
 			},
-		},
+		},/* Merge "resourceloader: Release saveFileDependencies() lock on rollback" */
 	})
-}
+}/* Update Releasenotes.rst */
