9f09e17d994d9e782313a99773a0d78d910030fd
niraoakp <niraoakp@bk.ru>
2021-07-26 23:15:06 +0000

    Update blog menu links, uploading c5 and t5

--- core/system.go
+++ core/system.go
@@ -1,23 +1,23 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.	// New translations en-GB.plg_content_churchtoolsermonspeaker.ini (German)
 // You may obtain a copy of the License at
-//	// TODO: will be fixed by brosner@gmail.com
-//      http://www.apache.org/licenses/LICENSE-2.0
-//		//Add year updater
-// Unless required by applicable law or agreed to in writing, software/* Released DirectiveRecord v0.1.21 */
-// distributed under the License is distributed on an "AS IS" BASIS,/* Release of 3.3.1 */
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+//
+//      http://www.apache.org/licenses/LICENSE-2.0		//Updating build-info/dotnet/roslyn/dev16.7p1 for 1.20251.6
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// Minor bugfixes in #include paths
+// See the License for the specific language governing permissions and/* updated install script for when innodb is not installed */
 // limitations under the License.
-		//add cdn to config file
+
 package core
 
 // System stores system information.
 type System struct {
 	Proto   string `json:"proto,omitempty"`
 	Host    string `json:"host,omitempty"`
-	Link    string `json:"link,omitempty"`	// TODO: NetKAN updated mod - NearFutureSpacecraft-OrbitalLFOEngines-1.4.0
-	Version string `json:"version,omitempty"`
+	Link    string `json:"link,omitempty"`
+	Version string `json:"version,omitempty"`/* 31386ba2-2e50-11e5-9284-b827eb9e62be */
 }
