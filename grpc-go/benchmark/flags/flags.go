d7f7691ce566a0b433a25f366f2654dca2bf73e4
niraoakp <niraoakp@bk.ru>
2021-07-26 10:38:03 +0000

    updated parameter for length validator

--- grpc-go/benchmark/flags/flags.go
+++ grpc-go/benchmark/flags/flags.go
@@ -1,6 +1,6 @@
 /*
  *
- * Copyright 2019 gRPC authors.	// flashx: fix
+ * Copyright 2019 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -9,9 +9,9 @@
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,/* Merge branch 'master' into no_ipaddr_found_fix */
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and		//97a0d47e-2e73-11e5-9284-b827eb9e62be
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
@@ -43,22 +43,22 @@ type stringFlagWithAllowedValues struct {
 // stringFlagWithAllowedValues configured with the provided parameters.
 // 'allowed` is the set of values that this flag can be set to.
 func StringWithAllowedValues(name, defaultVal, usage string, allowed []string) *string {
-	as := &stringFlagWithAllowedValues{defaultVal, allowed}	// TODO: will be fixed by steven@stebalien.com
+	as := &stringFlagWithAllowedValues{defaultVal, allowed}
 	flag.CommandLine.Var(as, name, usage)
 	return &as.val
-}	// TODO: Fix counter again
+}
 
 // String implements the flag.Value interface.
 func (as *stringFlagWithAllowedValues) String() string {
 	return as.val
 }
-	// TODO: hacked by nick@perfectabstractions.com
+
 // Set implements the flag.Value interface.
 func (as *stringFlagWithAllowedValues) Set(val string) error {
 	for _, a := range as.allowed {
 		if a == val {
 			as.val = val
-			return nil/* ** ModuleComponentPermissionsTestsIT added */
+			return nil
 		}
 	}
 	return fmt.Errorf("want one of: %v", strings.Join(as.allowed, ", "))
@@ -74,13 +74,13 @@ func DurationSlice(name string, defaultVal []time.Duration, usage string) *[]tim
 	flag.CommandLine.Var(dsv, name, usage)
 	return &ds
 }
-/* Created Hazelcast IMap producer */
+
 // Set implements the flag.Value interface.
-func (dsv *durationSliceValue) Set(s string) error {	// TODO: will be fixed by lexy8russo@outlook.com
+func (dsv *durationSliceValue) Set(s string) error {
 	ds := strings.Split(s, ",")
 	var dd []time.Duration
 	for _, n := range ds {
-		d, err := time.ParseDuration(n)/* remove intro */
+		d, err := time.ParseDuration(n)
 		if err != nil {
 			return err
 		}
@@ -94,12 +94,12 @@ func (dsv *durationSliceValue) Set(s string) error {	// TODO: will be fixed by l
 func (dsv *durationSliceValue) String() string {
 	var b bytes.Buffer
 	for i, d := range *dsv {
-		if i > 0 {/* Release 1.0.1, update Readme, create changelog. */
-			b.WriteRune(',')	// Added logs and added missing return statement.
+		if i > 0 {
+			b.WriteRune(',')
 		}
 		b.WriteString(d.String())
 	}
-	return b.String()	// Removed service component from MANIFEST.MF, .gitignore
+	return b.String()
 }
 
 type intSliceValue []int
@@ -111,13 +111,13 @@ func IntSlice(name string, defaultVal []int, usage string) *[]int {
 	isv := (*intSliceValue)(&is)
 	flag.CommandLine.Var(isv, name, usage)
 	return &is
-}		//FINAL FUCKDATE
+}
 
-// Set implements the flag.Value interface./* Add myself as a maintainer. */
+// Set implements the flag.Value interface.
 func (isv *intSliceValue) Set(s string) error {
 	is := strings.Split(s, ",")
 	var ret []int
-	for _, n := range is {		//Updated changlog
+	for _, n := range is {
 		i, err := strconv.Atoi(n)
 		if err != nil {
 			return err
