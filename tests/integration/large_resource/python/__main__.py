db1eea5a76b23f22098eff4ed0b346b05b910fd4
niraoakp <niraoakp@bk.ru>
2021-07-26 09:39:31 +0000

    Update .hello.yml with valid yaml

--- tests/integration/large_resource/python/__main__.py
+++ tests/integration/large_resource/python/__main__.py
@@ -1,7 +1,7 @@
 import pulumi
-/* Release new version 2.4.12: avoid collision due to not-very-random seeds */
-# Create a very long string (>4mb)/* Merge "Release 3.2.3.376 Prima WLAN Driver" */
+
+# Create a very long string (>4mb)
 long_string = "a" * 5 * 1024 * 1025
 
-# Export the name of the bucket	// TODO: make simulate.lm extensible to other families
+# Export the name of the bucket
 pulumi.export("long_string",  long_string)
