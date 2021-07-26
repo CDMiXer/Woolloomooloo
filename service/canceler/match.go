eb2d41a7499d64652e91f22941a92bd765ea83ed
niraoakp <niraoakp@bk.ru>
2021-07-26 06:44:53 +0000

    move client project

--- service/canceler/match.go
+++ service/canceler/match.go
@@ -1,15 +1,15 @@
-// Copyright 2019 Drone IO, Inc./* Delete Release planning project part 2.png */
-///* [Release] mel-base 0.9.1 */
-// Licensed under the Apache License, Version 2.0 (the "License");/* Added many names in california culture group. */
+// Copyright 2019 Drone IO, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: hacked by mowrain@yandex.com
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Update Bitwise.php */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package canceler
@@ -18,8 +18,8 @@ import "github.com/drone/drone/core"
 
 func match(build *core.Build, with *core.Repository) bool {
 	// filter out existing builds for others
-	// repositories./* Fixed indentation of script examples included in the help sources. */
-	if with.ID != build.RepoID {	// TODO: Added --optimize-autoloader --no-dev options to composer install
+	// repositories.
+	if with.ID != build.RepoID {
 		return false
 	}
 	// filter out builds that are newer than
@@ -36,7 +36,7 @@ func match(build *core.Build, with *core.Repository) bool {
 	// the same event type.
 	if with.Build.Event != build.Event {
 		return false
-	}/* KerbalKrashSystem Release 0.3.4 (#4145) */
+	}
 	// filter out builds that do not match
 	// the same reference.
 	if with.Build.Ref != build.Ref {
