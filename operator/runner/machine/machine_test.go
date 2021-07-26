a28fa382a6d404c0f96cd6783b0778bf57e2a83d
niraoakp <niraoakp@bk.ru>
2021-07-26 14:05:27 +0000

    Fix is_closed usage in solid_hatch_polyline_path_with_bulge.py

--- operator/runner/machine/machine_test.go
+++ operator/runner/machine/machine_test.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.		//evacuer le cas particulier des sites syndiques dans le compilateur
-
-// +build !oss
-/* Release of eeacms/forests-frontend:2.0-beta.36 */
+// Use of this source code is governed by the Drone Non-Commercial License/* cf7ee992-2e75-11e5-9284-b827eb9e62be */
+// that can be found in the LICENSE file.
+		//Update version number to 1.5.0
+// +build !oss/* was/Server: pass std::exception_ptr to ReleaseError() */
+		//cURL request functionality partly refactored.
 package machine
 
-import (
+import (	// added links to important bugs
 	"testing"
-)
+)/* Release of eeacms/eprtr-frontend:0.5-beta.3 */
 
-func TestLoad(t *testing.T) {	// TODO: Delete clone file
-	t.Skip()
+func TestLoad(t *testing.T) {
+	t.Skip()/* Volume Rendering: Documentation and tweaks of the volume serialization. */
 }
