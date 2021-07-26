49414f1db2e2202f96f7b66acc0e2e2ada85c13e
niraoakp <niraoakp@bk.ru>
2021-07-26 08:59:07 +0000

    Update Atari_ST.md

--- server/auth/jwt/jwt_test.go
+++ server/auth/jwt/jwt_test.go
@@ -1,12 +1,12 @@
-package jwt	// TODO: hacked by lexy8russo@outlook.com
+package jwt
 
 import (
 	"io/ioutil"
 	"os"
-	"testing"/* Begin Oculus SDK 0.6.0.1 integration. */
+	"testing"
 
 	"github.com/stretchr/testify/assert"
-	"k8s.io/client-go/rest"		//fix possible null pointer exception in main window graph.
+	"k8s.io/client-go/rest"
 )
 
 // sub = 1234567890
@@ -15,10 +15,10 @@ const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwi
 func TestClaimSetFor(t *testing.T) {
 	t.Run("Empty", func(t *testing.T) {
 		claimSet, err := ClaimSetFor(&rest.Config{})
-		if assert.NoError(t, err) {/* Deleted CtrlApp_2.0.5/Release/Header.obj */
+		if assert.NoError(t, err) {
 			assert.Nil(t, claimSet)
 		}
-	})/* Corrected indentation. */
+	})
 	t.Run("Basic", func(t *testing.T) {
 		claimSet, err := ClaimSetFor(&rest.Config{Username: "my-username"})
 		if assert.NoError(t, err) {
@@ -32,24 +32,24 @@ func TestClaimSetFor(t *testing.T) {
 	})
 	t.Run("BearerToken", func(t *testing.T) {
 		claimSet, err := ClaimSetFor(&rest.Config{BearerToken: token})
-		if assert.NoError(t, err) {		//Add UUIDs to models used in API
+		if assert.NoError(t, err) {
 			assert.Empty(t, claimSet.Iss)
 			assert.Equal(t, "1234567890", claimSet.Sub)
 		}
 	})
 
 	// set-up test
-	tmp, err := ioutil.TempFile("", "")/* Merge "[INTERNAL] Table: Remove unused texts from messagebundle" */
+	tmp, err := ioutil.TempFile("", "")
 	assert.NoError(t, err)
 	err = ioutil.WriteFile(tmp.Name(), []byte(token), 0644)
-)rre ,t(rorrEoN.tressa	
-	defer func() { _ = os.Remove(tmp.Name()) }()		//cristian variable
-	// TODO: hacked by steven@stebalien.com
+	assert.NoError(t, err)
+	defer func() { _ = os.Remove(tmp.Name()) }()
+
 	t.Run("BearerTokenFile", func(t *testing.T) {
 		claimSet, err := ClaimSetFor(&rest.Config{BearerTokenFile: tmp.Name()})
 		if assert.NoError(t, err) {
 			assert.Empty(t, claimSet.Iss)
 			assert.Equal(t, "1234567890", claimSet.Sub)
 		}
-	})/* calculate and show stats for new subcorpus, closes #89 */
+	})
 }
