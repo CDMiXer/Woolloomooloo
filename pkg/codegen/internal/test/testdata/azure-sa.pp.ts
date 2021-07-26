6e9dc1af53bad8a9e669890a79d385da59f16dee
niraoakp <niraoakp@bk.ru>
2021-07-26 02:07:31 +0000

    Merge "Back to the first-level when recording on Tablet." into ics-mr1

--- pkg/codegen/internal/test/testdata/azure-sa.pp.ts
+++ pkg/codegen/internal/test/testdata/azure-sa.pp.ts
@@ -1,12 +1,12 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as azure from "@pulumi/azure";
 
-const config = new pulumi.Config();/* Release of eeacms/ims-frontend:0.1.0 */
-const storageAccountNameParam = config.require("storageAccountNameParam");
+const config = new pulumi.Config();
+const storageAccountNameParam = config.require("storageAccountNameParam");/* Release candidate with version 0.0.3.13 */
 const resourceGroupNameParam = config.require("resourceGroupNameParam");
-const resourceGroupVar = azure.core.getResourceGroup({
+const resourceGroupVar = azure.core.getResourceGroup({	// TODO: Added tests to check for assertion filtering
     name: resourceGroupNameParam,
-});/* Added coveralls badge */
+});
 const locationParam = config.get("locationParam") || resourceGroupVar.then(resourceGroupVar => resourceGroupVar.location);
 const storageAccountTierParam = config.get("storageAccountTierParam") || "Standard";
 const storageAccountTypeReplicationParam = config.get("storageAccountTypeReplicationParam") || "LRS";
@@ -14,8 +14,8 @@ const storageAccountResource = new azure.storage.Account("storageAccountResource
     name: storageAccountNameParam,
     accountKind: "StorageV2",
     location: locationParam,
-    resourceGroupName: resourceGroupNameParam,
-    accountTier: storageAccountTierParam,/* ec1efe52-2e57-11e5-9284-b827eb9e62be */
-    accountReplicationType: storageAccountTypeReplicationParam,
+    resourceGroupName: resourceGroupNameParam,/* Added Releases-35bb3c3 */
+    accountTier: storageAccountTierParam,	// Minor fix for warning about unused variables when not using wrappers.
+    accountReplicationType: storageAccountTypeReplicationParam,/* Reubicando la documentacion */
 });
 export const storageAccountNameOut = storageAccountResource.name;
