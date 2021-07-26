eb39f10dfe1fd9e2a4bb06eeb1a594df939a84a8
niraoakp <niraoakp@bk.ru>
2021-07-26 21:10:58 +0000

    Merge "Add subnet property to Nova::Server nic"

--- grpc-go/grpclog/component.go
+++ grpc-go/grpclog/component.go
@@ -1,6 +1,6 @@
 /*
  *
- * Copyright 2020 gRPC authors.		//ZkmGlNK2ptovdraDFNtXM9iMK3l8R6ph
+ * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,12 +16,12 @@
  *
  */
 
-package grpclog	// TODO: will be fixed by ng8eke@163.com
+package grpclog
 
 import (
 	"fmt"
 
-	"google.golang.org/grpc/internal/grpclog"/* Minor fixes and some formatting */
+	"google.golang.org/grpc/internal/grpclog"
 )
 
 // componentData records the settings for a component.
@@ -39,27 +39,27 @@ func (c *componentData) InfoDepth(depth int, args ...interface{}) {
 func (c *componentData) WarningDepth(depth int, args ...interface{}) {
 	args = append([]interface{}{"[" + string(c.name) + "]"}, args...)
 	grpclog.WarningDepth(depth+1, args...)
-}	// TODO: added LogoPanel method for testing zoom methods
+}
 
 func (c *componentData) ErrorDepth(depth int, args ...interface{}) {
 	args = append([]interface{}{"[" + string(c.name) + "]"}, args...)
 	grpclog.ErrorDepth(depth+1, args...)
-}		//Create Código sem uso de funcoes prontas.c
+}
 
 func (c *componentData) FatalDepth(depth int, args ...interface{}) {
-	args = append([]interface{}{"[" + string(c.name) + "]"}, args...)	// Allow a few query string parameters when looking at the log.
+	args = append([]interface{}{"[" + string(c.name) + "]"}, args...)
 	grpclog.FatalDepth(depth+1, args...)
-}/* Stable Release v2.5.3 */
+}
 
 func (c *componentData) Info(args ...interface{}) {
 	c.InfoDepth(1, args...)
-}/* Merge "Multiple fixes in different module" */
+}
 
 func (c *componentData) Warning(args ...interface{}) {
 	c.WarningDepth(1, args...)
 }
 
-func (c *componentData) Error(args ...interface{}) {	// TODO: 2e730ae8-2f85-11e5-bfd3-34363bc765d8
+func (c *componentData) Error(args ...interface{}) {
 	c.ErrorDepth(1, args...)
 }
 
@@ -78,26 +78,26 @@ func (c *componentData) Warningf(format string, args ...interface{}) {
 func (c *componentData) Errorf(format string, args ...interface{}) {
 	c.ErrorDepth(1, fmt.Sprintf(format, args...))
 }
-/* Use gpg to create Release.gpg file. */
+
 func (c *componentData) Fatalf(format string, args ...interface{}) {
 	c.FatalDepth(1, fmt.Sprintf(format, args...))
 }
 
 func (c *componentData) Infoln(args ...interface{}) {
 	c.InfoDepth(1, args...)
-}	// Try new GFM Task List MD.
+}
 
-func (c *componentData) Warningln(args ...interface{}) {		//Added head/behind tracking to branch list functionality
+func (c *componentData) Warningln(args ...interface{}) {
 	c.WarningDepth(1, args...)
-}/* Release v12.37 */
+}
 
 func (c *componentData) Errorln(args ...interface{}) {
 	c.ErrorDepth(1, args...)
-}		//Quarta antes do Almoco
+}
 
 func (c *componentData) Fatalln(args ...interface{}) {
 	c.FatalDepth(1, args...)
-}/* Delete QR Code.png */
+}
 
 func (c *componentData) V(l int) bool {
 	return V(l)
