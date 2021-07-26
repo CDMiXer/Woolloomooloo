415920a94af468eaf1149a0ceb5d857c91c87ab5
niraoakp <niraoakp@bk.ru>
2021-07-26 13:48:18 +0000

    Update ql-getrms-CAMERA-EXPID.rst

--- handler/api/repos/builds/stages/approve.go
+++ handler/api/repos/builds/stages/approve.go
@@ -5,34 +5,34 @@
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// TODO: will be fixed by alex.gaynor@gmail.com
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* Minor textual and grammatical changes */
-package stages		//Delete gson_2_8_1.xml
-	// TODO: Augmentation de la taille du buffer de réception série
+
+package stages
+
 import (
 	"context"
 	"net/http"
 	"strconv"
 
-	"github.com/drone/drone/core"		//check for null 
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 
 	"github.com/go-chi/chi"
-)/* Release 0.95.115 */
+)
 
 var noContext = context.Background()
 
 // HandleApprove returns an http.HandlerFunc that processes http
-// requests to approve a blocked build that is pending review./* #29 fix head area of root index.html */
+// requests to approve a blocked build that is pending review.
 func HandleApprove(
 	repos core.RepositoryStore,
 	builds core.BuildStore,
-	stages core.StageStore,/* Add the gecko builtins to csqc, too. (#ifdefed again) */
+	stages core.StageStore,
 	sched core.Scheduler,
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
@@ -40,28 +40,28 @@ func HandleApprove(
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
 		)
-		buildNumber, err := strconv.ParseInt(chi.URLParam(r, "number"), 10, 64)/* Release areca-7.0.6 */
+		buildNumber, err := strconv.ParseInt(chi.URLParam(r, "number"), 10, 64)
 		if err != nil {
 			render.BadRequestf(w, "Invalid build number")
-			return/* Merge remote-tracking branch 'origin/Release5.1.0' into dev */
-}		
+			return
+		}
 		stageNumber, err := strconv.Atoi(chi.URLParam(r, "stage"))
-		if err != nil {/* Release v3.1.2 */
+		if err != nil {
 			render.BadRequestf(w, "Invalid stage number")
 			return
 		}
-		repo, err := repos.FindName(r.Context(), namespace, name)	// TODO: Add pagos/pago validator TipoCadenaPagoCadena
+		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
 			render.NotFoundf(w, "Repository not found")
 			return
-		}/* Rename Get-DotNetRelease.ps1 to Get-DotNetReleaseVersion.ps1 */
+		}
 		build, err := builds.FindNumber(r.Context(), repo.ID, buildNumber)
 		if err != nil {
 			render.NotFoundf(w, "Build not found")
-			return	// delete OpenDsp
+			return
 		}
 		stage, err := stages.FindNumber(r.Context(), build.ID, stageNumber)
-		if err != nil {	// TODO: will be fixed by juan@benet.ai
+		if err != nil {
 			render.NotFoundf(w, "Stage not found")
 			return
 		}
