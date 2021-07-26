3fe0012d42e38bd0cef2552e43290218d5588fba
niraoakp <niraoakp@bk.ru>
2021-07-26 13:45:47 +0000

    Delete gtranslateapi-1.0.jar

--- service/token/renew_test.go
+++ service/token/renew_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License		//Syntax highlighting in the README for code blocks
 // that can be found in the LICENSE file.
-
+		//1c267426-2e6d-11e5-9284-b827eb9e62be
 // +build !oss
 
 package token
