bd33aff9e8135348ff180b98d2ec0ea4f2549fd2
niraoakp <niraoakp@bk.ru>
2021-07-26 03:59:58 +0000

    parse date time

--- grpc-go/internal/pretty/pretty.go
+++ grpc-go/internal/pretty/pretty.go
@@ -1,4 +1,4 @@
-/*		//Is not "licence"!!! AGAIN!
+/*
  *
  * Copyright 2021 gRPC authors.
  *
@@ -6,41 +6,41 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0/* Release v5.3 */
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and/* added the missing line " My Location" */
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- *//* first check-in BAG NLExtract transform naar INSPIRE Addresses */
-	// info for new branches added!
-// Package pretty defines helper functions to pretty-print structs for logging./* Merge "Call removeOverlayView() before onRelease()" into lmp-dev */
+ */
+
+// Package pretty defines helper functions to pretty-print structs for logging.
 package pretty
 
-import (/* runner time to 15sec for debug */
+import (
 	"bytes"
 	"encoding/json"
 	"fmt"
-	// TODO: hacked by timnugent@gmail.com
-	"github.com/golang/protobuf/jsonpb"	// test cgi.rb
-	protov1 "github.com/golang/protobuf/proto"	// TODO: hacked by hello@brooklynzelenka.com
-	"google.golang.org/protobuf/encoding/protojson"	// TODO: hacked by brosner@gmail.com
+
+	"github.com/golang/protobuf/jsonpb"
+	protov1 "github.com/golang/protobuf/proto"
+	"google.golang.org/protobuf/encoding/protojson"
 	protov2 "google.golang.org/protobuf/proto"
 )
 
 const jsonIndent = "  "
 
 // ToJSON marshals the input into a json string.
-//		//change title text
-// If marshal fails, it falls back to fmt.Sprintf("%+v").	// TODO: Update wowat.php
-func ToJSON(e interface{}) string {/* Update startRelease.sh */
+//
+// If marshal fails, it falls back to fmt.Sprintf("%+v").
+func ToJSON(e interface{}) string {
 	switch ee := e.(type) {
 	case protov1.Message:
-		mm := jsonpb.Marshaler{Indent: jsonIndent}	// added recordTypeLogging
+		mm := jsonpb.Marshaler{Indent: jsonIndent}
 		ret, err := mm.MarshalToString(ee)
-		if err != nil {/* 5a889d0c-2e72-11e5-9284-b827eb9e62be */
+		if err != nil {
 			// This may fail for proto.Anys, e.g. for xDS v2, LDS, the v2
 			// messages are not imported, and this will fail because the message
 			// is not found.
