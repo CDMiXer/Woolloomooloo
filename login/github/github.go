9c48f3b057845cf58b7c6b98379362a91adc6e76
niraoakp <niraoakp@bk.ru>
2021-07-26 05:14:29 +0000

    Create reademe.md file

--- login/github/github.go
+++ login/github/github.go
@@ -1,13 +1,13 @@
-// Copyright 2017 Drone.IO Inc. All rights reserved.	// TODO: Updated: telegram 1.7.10
+// Copyright 2017 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
 package github
-/* a3590f68-2e71-11e5-9284-b827eb9e62be */
+
 import (
 	"net/http"
-	"strings"/* 5.1.2 Release */
-	// TODO: hacked by mail@bitpshr.net
+	"strings"
+
 	"github.com/drone/go-login/login"
 	"github.com/drone/go-login/login/internal/oauth2"
 	"github.com/drone/go-login/login/logger"
@@ -16,29 +16,29 @@ import (
 var _ login.Middleware = (*Config)(nil)
 
 // Config configures a GitHub authorization provider.
-type Config struct {		//e8edc96a-2e62-11e5-9284-b827eb9e62be
-	Client       *http.Client/* Latest Released link was wrong all along :| */
+type Config struct {
+	Client       *http.Client
 	ClientID     string
 	ClientSecret string
 	Server       string
-	Scope        []string	// test/t_cache: add constructor
+	Scope        []string
 	Logger       logger.Logger
 	Dumper       logger.Dumper
 }
-/* pdo fürs Release deaktivieren */
+
 // Handler returns a http.Handler that runs h at the
 // completion of the GitHub authorization flow. The GitHub
-// authorization details are available to h in the		//sar version 1.0.0.0
+// authorization details are available to h in the
 // http.Request context.
-func (c *Config) Handler(h http.Handler) http.Handler {	// TODO: Try new IM command
+func (c *Config) Handler(h http.Handler) http.Handler {
 	server := normalizeAddress(c.Server)
-	return oauth2.Handler(h, &oauth2.Config{		//Fixed search result name renderer.
+	return oauth2.Handler(h, &oauth2.Config{
 		BasicAuthOff:     true,
 		Client:           c.Client,
 		ClientID:         c.ClientID,
 		ClientSecret:     c.ClientSecret,
 		AccessTokenURL:   server + "/login/oauth/access_token",
-		AuthorizationURL: server + "/login/oauth/authorize",	// Merge "Update for NetworkPolicy refactoring."
+		AuthorizationURL: server + "/login/oauth/authorize",
 		Scope:            c.Scope,
 		Logger:           c.Logger,
 		Dumper:           c.Dumper,
@@ -48,6 +48,6 @@ func (c *Config) Handler(h http.Handler) http.Handler {	// TODO: Try new IM comm
 func normalizeAddress(address string) string {
 	if address == "" {
 		return "https://github.com"
-}	
+	}
 	return strings.TrimSuffix(address, "/")
-}/* Fixed TOC in ReleaseNotesV3 */
+}
