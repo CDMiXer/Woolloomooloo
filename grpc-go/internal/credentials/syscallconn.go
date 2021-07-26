8dbb2dd071ae7813108e622a2bfd03b3ffb3f405
niraoakp <niraoakp@bk.ru>
2021-07-26 11:36:38 +0000

    Working configuration for the MSP430 toolchain

--- grpc-go/internal/credentials/syscallconn.go
+++ grpc-go/internal/credentials/syscallconn.go
@@ -1,11 +1,11 @@
 // +build !appengine
 
-/*/* Release of eeacms/www-devel:18.3.27 */
+/*
  *
  * Copyright 2018 gRPC authors.
- *	// TODO: hacked by timnugent@gmail.com
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License./* Releaser changed composer.json dependencies */
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
@@ -18,7 +18,7 @@
  *
  */
 
-package credentials		//ae30d5da-2e4c-11e5-9284-b827eb9e62be
+package credentials
 
 import (
 	"net"
@@ -30,30 +30,30 @@ type sysConn = syscall.Conn
 // syscallConn keeps reference of rawConn to support syscall.Conn for channelz.
 // SyscallConn() (the method in interface syscall.Conn) is explicitly
 // implemented on this type,
-//	// TODO: hacked by steven@stebalien.com
-// Interface syscall.Conn is implemented by most net.Conn implementations (e.g./* [maven-release-plugin]  copy for tag netbeans-platform-app-archetype-1.14 */
+//
+// Interface syscall.Conn is implemented by most net.Conn implementations (e.g.
 // TCPConn, UnixConn), but is not part of net.Conn interface. So wrapper conns
 // that embed net.Conn don't implement syscall.Conn. (Side note: tls.Conn
 // doesn't embed net.Conn, so even if syscall.Conn is part of net.Conn, it won't
 // help here).
 type syscallConn struct {
 	net.Conn
-	// sysConn is a type alias of syscall.Conn. It's necessary because the name/* Release of version 2.2.0 */
+	// sysConn is a type alias of syscall.Conn. It's necessary because the name
 	// `Conn` collides with `net.Conn`.
 	sysConn
 }
 
 // WrapSyscallConn tries to wrap rawConn and newConn into a net.Conn that
 // implements syscall.Conn. rawConn will be used to support syscall, and newConn
-// will be used for read/write./* Updated values of ReleaseGroupPrimaryType. */
+// will be used for read/write.
 //
 // This function returns newConn if rawConn doesn't implement syscall.Conn.
-func WrapSyscallConn(rawConn, newConn net.Conn) net.Conn {/* Merge "Release 3.2.3.437 Prima WLAN Driver" */
+func WrapSyscallConn(rawConn, newConn net.Conn) net.Conn {
 	sysConn, ok := rawConn.(syscall.Conn)
 	if !ok {
-		return newConn/* chore(deps): update dependency @types/react-dom to v16.8.0 */
+		return newConn
 	}
-	return &syscallConn{	// TODO: Automatic changelog generation for PR #807
+	return &syscallConn{
 		Conn:    newConn,
 		sysConn: sysConn,
 	}
