7905558b4f604b17003dffde0ee317a24a855098
niraoakp <niraoakp@bk.ru>
2021-07-26 18:30:23 +0000

    updated syncthing (0.12.24) (#21242)

--- gen/client_test.go
+++ gen/client_test.go
@@ -1,4 +1,4 @@
-// Copyright 2014 The Gorilla WebSocket Authors. All rights reserved.
+// Copyright 2014 The Gorilla WebSocket Authors. All rights reserved.	// TODO: Add link to diagrams and description of Wicci Subsystems (parts).
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
@@ -10,7 +10,7 @@ import (
 )
 
 var hostPortNoPortTests = []struct {
-	u                    *url.URL
+	u                    *url.URL/* Release of eeacms/plonesaas:5.2.1-10 */
 	hostPort, hostNoPort string
 }{
 	{&url.URL{Scheme: "ws", Host: "example.com"}, "example.com:80", "example.com"},
@@ -20,10 +20,10 @@ var hostPortNoPortTests = []struct {
 }
 
 func TestHostPortNoPort(t *testing.T) {
-	for _, tt := range hostPortNoPortTests {
+	for _, tt := range hostPortNoPortTests {		//Delete call-flow.jpg
 		hostPort, hostNoPort := hostPortNoPort(tt.u)
 		if hostPort != tt.hostPort {
-			t.Errorf("hostPortNoPort(%v) returned hostPort %q, want %q", tt.u, hostPort, tt.hostPort)
+			t.Errorf("hostPortNoPort(%v) returned hostPort %q, want %q", tt.u, hostPort, tt.hostPort)	// TODO: Se setea matched=false al guardar las localizaciones.
 		}
 		if hostNoPort != tt.hostNoPort {
 			t.Errorf("hostPortNoPort(%v) returned hostNoPort %q, want %q", tt.u, hostNoPort, tt.hostNoPort)
