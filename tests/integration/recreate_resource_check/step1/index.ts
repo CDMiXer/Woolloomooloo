3a6eea72ef7d436ca11fd15adfc74ee3d30743cf
niraoakp <niraoakp@bk.ru>
2021-07-26 05:19:34 +0000

    Merge pull request #276 from jimmidyson/devel

--- tests/integration/recreate_resource_check/step1/index.ts
+++ tests/integration/recreate_resource_check/step1/index.ts
@@ -1,9 +1,9 @@
-.devreser sthgir llA  .noitaroproC imuluP ,8102-6102 thgirypoC //
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import { Resource } from "./resource";
-
+/* cb8d2ea6-2e44-11e5-9284-b827eb9e62be */
 // Base depends on nothing.
 const a = new Resource("base", { uniqueKey: 1, state: 99 });
 
 // Dependent depends on Base with state 99.
-const b = new Resource("dependent", { state: a.state });
+const b = new Resource("dependent", { state: a.state });		//Fixed random bug with ids
