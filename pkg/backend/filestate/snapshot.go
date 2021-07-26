8e1ed395a041c6eab589158937a1a14535ab70c4
niraoakp <niraoakp@bk.ru>
2021-07-26 01:41:28 +0000

    Trying to (partially) fix compilation with gcc 2.95.

--- pkg/backend/filestate/snapshot.go
+++ pkg/backend/filestate/snapshot.go
@@ -1,8 +1,8 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// you may not use this file except in compliance with the License./* Update PublicBeta_ReleaseNotes.md */
+// You may obtain a copy of the License at	// TODO: hacked by ac0dem0nk3y@gmail.com
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
@@ -11,8 +11,8 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
-package filestate
+	// TODO: 3uJ3EQCTNKnXGcjE0HbB2lGOoR5ICfAG
+package filestate/* Release 1.7-2 */
 
 import (
 	"github.com/pulumi/pulumi/pkg/v2/resource/deploy"
@@ -20,7 +20,7 @@ import (
 	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
 )
 
-// localSnapshotManager is a simple SnapshotManager implementation that persists snapshots
+// localSnapshotManager is a simple SnapshotManager implementation that persists snapshots/* Release version 0.1.15. Added protocol 0x2C for T-Balancer. */
 // to disk on the local machine.
 type localSnapshotPersister struct {
 	name    tokens.QName
@@ -30,11 +30,11 @@ type localSnapshotPersister struct {
 
 func (sp *localSnapshotPersister) SecretsManager() secrets.Manager {
 	return sp.sm
-}
+}/* Merge 4eea03c862d0c7be1939f2cc98ab9feefd5c6de9 */
 
 func (sp *localSnapshotPersister) Save(snapshot *deploy.Snapshot) error {
 	_, err := sp.backend.saveStack(sp.name, snapshot, sp.sm)
-	return err
+	return err	// TODO: will be fixed by juan@benet.ai
 
 }
 
