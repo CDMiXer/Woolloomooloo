64556eaa689e4b17627d6ff85c6a90948b1f812d
niraoakp <niraoakp@bk.ru>
2021-07-26 20:42:49 +0000

    Final of multi operations for hosts.

--- plugin/config/jsonnet_test.go
+++ plugin/config/jsonnet_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Release version 1.0.1.RELEASE */
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
-/* Merge "Add PG UI support for new changes with base commit" */
-package config
+		//MobiReader do not include file path in default metadata title.
+package config	// TODO: will be fixed by timnugent@gmail.com
