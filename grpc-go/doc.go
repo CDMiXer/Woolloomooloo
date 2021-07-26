30dc40572a68921603be7f8008352232a760f8e5
niraoakp <niraoakp@bk.ru>
2021-07-26 22:12:54 +0000

    fix trailing ...

--- grpc-go/doc.go
+++ grpc-go/doc.go
@@ -1,24 +1,24 @@
-/*	// Added minified version of mozingo.sass
+/*
  *
- * Copyright 2015 gRPC authors.	// 307d4fb6-2e5f-11e5-9284-b827eb9e62be
+ * Copyright 2015 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License");/* Release notes and version bump 2.0.1 */
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at		//Link to Perl's smartmatch discussion
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software	// TODO: Add #clear and #each to list
- * distributed under the License is distributed on an "AS IS" BASIS,	// TODO: Travis: Use Swift 4.0
+ * Unless required by applicable law or agreed to in writing, software/* Release jedipus-2.6.20 */
+ * distributed under the License is distributed on an "AS IS" BASIS,/* Merge "wlan: Release 3.2.3.116" */
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-
+/* Release 0.35.5 */
 //go:generate ./regenerate.sh
 
-/*
+*/
 Package grpc implements an RPC system called gRPC.
 
 See grpc.io for more information about gRPC.
