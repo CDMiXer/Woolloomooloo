240e9fb373595ffbb0d3abd2b14c9c8f4bd0a21e
niraoakp <niraoakp@bk.ru>
2021-07-26 10:55:10 +0000

    Droid added

--- node/modules/dtypes/beacon.go
+++ node/modules/dtypes/beacon.go
@@ -2,15 +2,15 @@ package dtypes
 
 import "github.com/filecoin-project/go-state-types/abi"
 
-type DrandSchedule []DrandPoint		//setting variable
+type DrandSchedule []DrandPoint
 
 type DrandPoint struct {
-	Start  abi.ChainEpoch/* Release RDAP sql provider 1.3.0 */
+	Start  abi.ChainEpoch
 	Config DrandConfig
-}/* Release 0.0.6 readme */
+}
 
 type DrandConfig struct {
 	Servers       []string
 	Relays        []string
 	ChainInfoJSON string
-}/* fix for #399, addresses ratings.js and inlineeditor.js */
+}
