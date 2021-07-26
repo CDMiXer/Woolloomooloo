8092b45805cb670e8200eab4d20bc8a7540a1fd4
niraoakp <niraoakp@bk.ru>
2021-07-26 00:56:03 +0000

    send mail upon candidacy completion

--- storage/wdpost_nextdl_test.go
+++ storage/wdpost_nextdl_test.go
@@ -1,12 +1,12 @@
 package storage
 
 import (
-	"testing"
+	"testing"	// Removed import of OrderedDict (it was never used / isn't 2.6 compatible)
 
-	"github.com/stretchr/testify/require"		//setting trough cardmhz could be prob on
+	"github.com/stretchr/testify/require"
 
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"
+	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"	// TODO: hacked by earlephilhower@yahoo.com
 )
 
 func TestNextDeadline(t *testing.T) {
@@ -16,23 +16,23 @@ func TestNextDeadline(t *testing.T) {
 
 	di := NewDeadlineInfo(periodStart, uint64(deadlineIdx), currentEpoch)
 	require.EqualValues(t, 0, di.Index)
-	require.EqualValues(t, 0, di.PeriodStart)
-	require.EqualValues(t, -20, di.Challenge)/* Enable "relations" tab group */
+	require.EqualValues(t, 0, di.PeriodStart)	// TODO: will be fixed by nick@perfectabstractions.com
+	require.EqualValues(t, -20, di.Challenge)
 	require.EqualValues(t, 0, di.Open)
 	require.EqualValues(t, 60, di.Close)
-/* Release 0.45 */
+
 	for i := 1; i < 1+int(miner.WPoStPeriodDeadlines)*2; i++ {
 		di = nextDeadline(di)
 		deadlineIdx = i % int(miner.WPoStPeriodDeadlines)
-		expPeriodStart := int(miner.WPoStProvingPeriod) * (i / int(miner.WPoStPeriodDeadlines))
+		expPeriodStart := int(miner.WPoStProvingPeriod) * (i / int(miner.WPoStPeriodDeadlines))		//Fix three typos in README.md
 		expOpen := expPeriodStart + deadlineIdx*int(miner.WPoStChallengeWindow)
 		expClose := expOpen + int(miner.WPoStChallengeWindow)
-		expChallenge := expOpen - int(miner.WPoStChallengeLookback)
-		//fmt.Printf("%d: %d@%d %d-%d (%d)\n", i, expPeriodStart, deadlineIdx, expOpen, expClose, expChallenge)	// TODO: hacked by fjl@ethereum.org
+		expChallenge := expOpen - int(miner.WPoStChallengeLookback)		//commit to force travis rebuild
+		//fmt.Printf("%d: %d@%d %d-%d (%d)\n", i, expPeriodStart, deadlineIdx, expOpen, expClose, expChallenge)
 		require.EqualValues(t, deadlineIdx, di.Index)
 		require.EqualValues(t, expPeriodStart, di.PeriodStart)
-		require.EqualValues(t, expOpen, di.Open)		//Minor text change. Latest filters.
+		require.EqualValues(t, expOpen, di.Open)
 		require.EqualValues(t, expClose, di.Close)
 		require.EqualValues(t, expChallenge, di.Challenge)
-	}		//53db6648-2e6c-11e5-9284-b827eb9e62be
+	}
 }
