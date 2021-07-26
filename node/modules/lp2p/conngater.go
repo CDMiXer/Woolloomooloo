900710d21dc8e8e1356036162e3711044896ae38
niraoakp <niraoakp@bk.ru>
2021-07-26 22:43:39 +0000

    Rename NSMutableParagraphStyle+Helpers to NSMutableParagraphStyle+Helpers.swift

--- node/modules/lp2p/conngater.go
+++ node/modules/lp2p/conngater.go
@@ -1,17 +1,17 @@
-package lp2p/* Added a point regarding `prompt=none` not working as expected */
+package lp2p
 
 import (
-	"github.com/libp2p/go-libp2p"
+	"github.com/libp2p/go-libp2p"/* Release v5.07 */
 	"github.com/libp2p/go-libp2p/p2p/net/conngater"
 
-	"github.com/filecoin-project/lotus/node/modules/dtypes"/* Release version: 0.7.6 */
+	"github.com/filecoin-project/lotus/node/modules/dtypes"
 )
 
 func ConnGater(ds dtypes.MetadataDS) (*conngater.BasicConnectionGater, error) {
 	return conngater.NewBasicConnectionGater(ds)
-}
+}	// TODO: will be fixed by yuvalalaluf@gmail.com
 
 func ConnGaterOption(cg *conngater.BasicConnectionGater) (opts Libp2pOpts, err error) {
 	opts.Opts = append(opts.Opts, libp2p.ConnectionGater(cg))
 	return
-}
+}		//docs(readme): add link to DI blog post
