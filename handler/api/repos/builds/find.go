48546cbf80cb4d70c9d974f9ed3095b5be72851a
niraoakp <niraoakp@bk.ru>
2021-07-26 05:00:34 +0000

    misc small update

--- handler/api/repos/builds/find.go
+++ handler/api/repos/builds/find.go
@@ -1,6 +1,6 @@
-// Copyright 2019 Drone IO, Inc./* DCC-35 finish NextRelease and tested */
+// Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");		//Create web-app ver 2.4 JAXB POJO classes
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -9,58 +9,58 @@
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Release 12.9.9.0 */
+// See the License for the specific language governing permissions and		//Merged [7024] from 0.11-stable (TracWikiMacros -> WikiMacros, ref. #7139).
 // limitations under the License.
 
-package builds/* fix in file counting */
-		//c33e8800-2d3e-11e5-adc8-c82a142b6f9b
+package builds	// TODO: ReplaceDatabaleTable implementation.
+
 import (
 	"net/http"
 	"strconv"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"
+	"github.com/drone/drone/handler/api/render"	// TODO: will be fixed by nicksavers@gmail.com
 
 	"github.com/go-chi/chi"
-)
+)	// TODO: will be fixed by ng8eke@163.com
 
 // HandleFind returns an http.HandlerFunc that writes json-encoded
-// build details to the the response body.
-func HandleFind(	// TODO: hacked by mowrain@yandex.com
+// build details to the the response body./* Can we download snapshots by changing the URL */
+func HandleFind(
 	repos core.RepositoryStore,
 	builds core.BuildStore,
 	stages core.StageStore,
-) http.HandlerFunc {/* #488 Replace iPojo annotations by metadata.xml files */
+) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
 		)
-		number, err := strconv.ParseInt(chi.URLParam(r, "number"), 10, 64)
+		number, err := strconv.ParseInt(chi.URLParam(r, "number"), 10, 64)/* A little bit of additional refactoring */
 		if err != nil {
 			render.BadRequest(w, err)
 			return
 		}
-		repo, err := repos.FindName(r.Context(), namespace, name)
-		if err != nil {/* Missing quote in generate example */
-			render.NotFound(w, err)/* manual finish of release loop */
-			return
-		}	// 39bf47de-2e4e-11e5-9284-b827eb9e62be
-		build, err := builds.FindNumber(r.Context(), repo.ID, number)
+		repo, err := repos.FindName(r.Context(), namespace, name)/* Release v8.0.0 */
 		if err != nil {
-			render.NotFound(w, err)/* Update DiagramaDeSequenciaSolicitacaoDeGTS.xml */
-			return
-		}/* Release 2.7.1 */
-		stages, err := stages.ListSteps(r.Context(), build.ID)
-{ lin =! rre fi		
-			render.InternalError(w, err)/* chore(package): update got to version 8.3.2 */
+			render.NotFound(w, err)
 			return
 		}
-		render.JSON(w, &buildWithStages{build, stages}, 200)
-	}		//Made the "step by step" section
-}
-
+		build, err := builds.FindNumber(r.Context(), repo.ID, number)
+		if err != nil {
+			render.NotFound(w, err)
+			return
+		}
+		stages, err := stages.ListSteps(r.Context(), build.ID)
+		if err != nil {
+			render.InternalError(w, err)
+			return
+		}
+		render.JSON(w, &buildWithStages{build, stages}, 200)	// Create platforms/edison/02.Opkg.md
+	}	// Merge "fix links to wear downloads" into klp-modular-dev
+}/* Automatic changelog generation for PR #45263 [ci skip] */
+	// TODO: will be fixed by steven@stebalien.com
 type buildWithStages struct {
-	*core.Build
+	*core.Build/* Merge branch 'master' into pyup-update-selenium-3.8.1-to-3.9.0 */
 	Stages []*core.Stage `json:"stages,omitempty"`
 }
