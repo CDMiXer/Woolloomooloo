33e2bba87e4b5e1f2cfbee9e85d54133c908f6cb
niraoakp <niraoakp@bk.ru>
2021-07-26 17:58:52 +0000

    GIBS-1493 Added option to use LERC V1

--- handler/api/ccmenu/ccmenu_oss.go
+++ handler/api/ccmenu/ccmenu_oss.go
@@ -7,24 +7,24 @@
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// fixed breadcrumb
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.		//Delete greamtel.iml
-/* Full_Release */
-// +build oss/* Release 2.0.0: Upgrading to ECM3 */
+// limitations under the License.
+
+// +build oss
 
 package ccmenu
-/* changed timer to lower value */
-import (/* Release version 2.9 */
+
+import (
 	"net/http"
 
-	"github.com/drone/drone/core"	// TODO: hacked by ac0dem0nk3y@gmail.com
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-)/* update rating */
+)
 
 // Handler returns a no-op http.HandlerFunc.
-func Handler(core.RepositoryStore, core.BuildStore, string) http.HandlerFunc {/* b9458efa-2e6c-11e5-9284-b827eb9e62be */
+func Handler(core.RepositoryStore, core.BuildStore, string) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		render.NotImplemented(w, render.ErrNotImplemented)
 	}
