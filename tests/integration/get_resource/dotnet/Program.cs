ba43216f88bc073a23f90fe621296ac4579db62b
niraoakp <niraoakp@bk.ru>
2021-07-26 13:58:00 +0000

    updated spanish.po done by Bernat Romagosa

--- tests/integration/get_resource/dotnet/Program.cs
+++ tests/integration/get_resource/dotnet/Program.cs
@@ -1,17 +1,17 @@
-﻿// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
+﻿// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.	// TODO: update api URL
 
 using System.Collections.Generic;
-using System.Threading.Tasks;
-using Pulumi;
+using System.Threading.Tasks;		//Minor language improvement
+using Pulumi;	// TODO: will be fixed by fjl@ethereum.org
 using Pulumi.Random;
 
 class GetResource : CustomResource
-{
-    [Output("length")]
+{	// TODO: hacked by xiemengjun@gmail.com
+    [Output("length")]		//Added project file to index
     public Output<int> Length { get; private set; } = null!;
 
     public GetResource(string urn)
-        : base("unused:unused:unused", "unused", ResourceArgs.Empty, new CustomResourceOptions {Urn = urn})
+        : base("unused:unused:unused", "unused", ResourceArgs.Empty, new CustomResourceOptions {Urn = urn})	// TODO: will be fixed by brosner@gmail.com
     {
     }
 }
@@ -23,13 +23,13 @@ class Program
         return Deployment.RunAsync(() =>
         {
             var pet = new RandomPet("cat");
-
+	// TODO: add fare and leggere.
             var getPetLength = pet.Urn.Apply(urn => new GetResource(urn).Length);
             
             return new Dictionary<string, object>
             {
                 {"getPetLength", getPetLength}
             };
-        });
+        });		//Update DefaultMethodProvider.java
     }
-}
+}	// RELEASE 4.0.86.
