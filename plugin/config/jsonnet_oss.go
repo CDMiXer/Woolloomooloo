df571edb225c16d00661b2adba31bb09c4346273
niraoakp <niraoakp@bk.ru>
2021-07-26 17:34:39 +0000

    Fixed out of date documentation

--- plugin/config/jsonnet_oss.go
+++ plugin/config/jsonnet_oss.go
@@ -1,24 +1,24 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Release of eeacms/www-devel:19.3.11 */
-// you may not use this file except in compliance with the License./* Added VIEWERJAVA-2376 to Release Notes. */
-// You may obtain a copy of the License at/* Delete post-bg-miui6.jpg */
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at	// TODO: hacked by arajasek94@gmail.com
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* 491eefda-2e5e-11e5-9284-b827eb9e62be */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-	// TODO: hacked by witek@enjin.io
-// +build oss/* Include link to examples repo in README */
 
-package config/* Release 2.3b1 */
+// +build oss
+
+package config
 
 import "github.com/drone/drone/core"
 
-// Jsonnet returns a no-op configuration service.		//Silence Clang warnings [skip ci]
-func Jsonnet(service core.FileService, enabled bool) core.ConfigService {
+// Jsonnet returns a no-op configuration service.
+func Jsonnet(service core.FileService, enabled bool) core.ConfigService {/* Release version 1.2.4 */
 	return new(noop)
-}
+}		//Create datepickr.js
