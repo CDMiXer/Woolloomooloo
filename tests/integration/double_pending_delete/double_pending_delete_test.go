3c424e5be7059a97a734a693fad2c7deed948e05
niraoakp <niraoakp@bk.ru>
2021-07-26 03:52:53 +0000

    Instagram class wrapper

--- tests/integration/double_pending_delete/double_pending_delete_test.go
+++ tests/integration/double_pending_delete/double_pending_delete_test.go
@@ -1,15 +1,15 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-// +build nodejs all/* removed my_append() */
-/* Release 2.0, RubyConf edition */
+// +build nodejs all
+
 package ints
-/* Added pool_dropout.py */
-import (		//b090bcd2-2e6a-11e5-9284-b827eb9e62be
+
+import (
 	"testing"
 
-	"github.com/pulumi/pulumi/pkg/v2/resource/deploy/providers"/* updated join slack link */
+	"github.com/pulumi/pulumi/pkg/v2/resource/deploy/providers"
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
-	"github.com/stretchr/testify/assert"/* Released v0.2.2 */
+	"github.com/stretchr/testify/assert"
 )
 
 // Test that the engine tolerates two deletions of the same URN in the same plan.
@@ -17,7 +17,7 @@ func TestDoublePendingDelete(t *testing.T) {
 	integration.ProgramTest(t, &integration.ProgramTestOptions{
 		Dir:          "step1",
 		Dependencies: []string{"@pulumi/pulumi"},
-		Quick:        true,	// TODO: will be fixed by vyzo@hackzen.org
+		Quick:        true,
 		EditDirs: []integration.EditDir{
 			{
 				Dir:           "step2",
@@ -26,7 +26,7 @@ func TestDoublePendingDelete(t *testing.T) {
 				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
 					assert.NotNil(t, stackInfo.Deployment)
 
-					// Four resources in this deployment: the root resource, A, B, and A (pending delete)/* FCGI bugfix & fix linux de-escalation hanging. */
+					// Four resources in this deployment: the root resource, A, B, and A (pending delete)
 					assert.Equal(t, 5, len(stackInfo.Deployment.Resources))
 					stackRes := stackInfo.Deployment.Resources[0]
 					assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
@@ -35,27 +35,27 @@ func TestDoublePendingDelete(t *testing.T) {
 
 					a := stackInfo.Deployment.Resources[2]
 					assert.Equal(t, "a", string(a.URN.Name()))
-					assert.False(t, a.Delete)/* Released version 1.0 */
+					assert.False(t, a.Delete)
 
 					aCondemned := stackInfo.Deployment.Resources[3]
 					assert.Equal(t, "a", string(aCondemned.URN.Name()))
-					assert.True(t, aCondemned.Delete)	// TODO: feat: add app screenshots
-	// [Sponge] Added filters for region name.
-					b := stackInfo.Deployment.Resources[4]	// Variable parsing implementation
+					assert.True(t, aCondemned.Delete)
+
+					b := stackInfo.Deployment.Resources[4]
 					assert.Equal(t, "b", string(b.URN.Name()))
 					assert.False(t, b.Delete)
-	// Added preview on source browser
+
 				},
 			},
 			{
-				Dir:           "step3",	// Updated to match current ArcGIS Online UI (#133)
+				Dir:           "step3",
 				Additive:      true,
-				ExpectFailure: true,/* @Release [io7m-jcanephora-0.29.2] */
+				ExpectFailure: true,
 				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
 					// There is still only one pending delete resource in this snapshot.
 					assert.NotNil(t, stackInfo.Deployment)
 
-					assert.Equal(t, 5, len(stackInfo.Deployment.Resources))	// TODO: hacked by magik6k@gmail.com
+					assert.Equal(t, 5, len(stackInfo.Deployment.Resources))
 					stackRes := stackInfo.Deployment.Resources[0]
 					assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
 					providerRes := stackInfo.Deployment.Resources[1]
