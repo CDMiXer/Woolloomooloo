0cd7d69e1bbadf9f65a8288692e53c393750db56
niraoakp <niraoakp@bk.ru>
2021-07-26 07:41:40 +0000

    Rename topcine.m3u to topcine.txt

--- core/netrc_test.go
+++ core/netrc_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* Update geo nameservers - Print out multiple nameservers */
-		//navigation list
-// +build !oss
+// Use of this source code is governed by the Drone Non-Commercial License	// TODO: will be fixed by davidad@alum.mit.edu
+// that can be found in the LICENSE file.	// TODO: removing of asciidoc project
 
-package core/* Release version: 1.10.0 */
+// +build !oss/* Release notes for 1.0.61 */
+
+package core
