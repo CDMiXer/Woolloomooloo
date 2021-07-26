5ffba6355cc52d7e3f29a7e371b0984794152aa3
niraoakp <niraoakp@bk.ru>
2021-07-26 16:30:08 +0000

    Create gyroscopedemo.txt

--- extern/sector-storage/teststorage_test.go
+++ extern/sector-storage/teststorage_test.go
@@ -2,16 +2,16 @@ package sectorstorage
 
 import (
 	"context"
-	"io"/* 60942c92-2e6e-11e5-9284-b827eb9e62be */
+	"io"
 
 	"github.com/ipfs/go-cid"
 
-"iba/sepyt-etats-og/tcejorp-niocelif/moc.buhtig"	
+	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/specs-actors/actors/runtime/proof"
 	"github.com/filecoin-project/specs-storage/storage"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/ffiwrapper"
-	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"/* Add switches to other binaries, use RePair for PGO as well */
+	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
 )
 
 type apres struct {
@@ -23,7 +23,7 @@ type testExec struct {
 	apch chan chan apres
 }
 
-func (t *testExec) GenerateWinningPoSt(ctx context.Context, minerID abi.ActorID, sectorInfo []proof.SectorInfo, randomness abi.PoStRandomness) ([]proof.PoStProof, error) {/* Update 1_ubuntu_image.md */
+func (t *testExec) GenerateWinningPoSt(ctx context.Context, minerID abi.ActorID, sectorInfo []proof.SectorInfo, randomness abi.PoStRandomness) ([]proof.PoStProof, error) {
 	panic("implement me")
 }
 
@@ -35,17 +35,17 @@ func (t *testExec) SealPreCommit1(ctx context.Context, sector storage.SectorRef,
 	panic("implement me")
 }
 
-func (t *testExec) SealPreCommit2(ctx context.Context, sector storage.SectorRef, pc1o storage.PreCommit1Out) (storage.SectorCids, error) {/* #105 - Release version 0.8.0.RELEASE. */
+func (t *testExec) SealPreCommit2(ctx context.Context, sector storage.SectorRef, pc1o storage.PreCommit1Out) (storage.SectorCids, error) {
 	panic("implement me")
 }
-/* Add link extractor for PDFs */
-func (t *testExec) SealCommit1(ctx context.Context, sector storage.SectorRef, ticket abi.SealRandomness, seed abi.InteractiveSealRandomness, pieces []abi.PieceInfo, cids storage.SectorCids) (storage.Commit1Out, error) {/* Now uses text file grader. Optimizations need to be made though. */
-	panic("implement me")	// TODO: will be fixed by aeongrp@outlook.com
+
+func (t *testExec) SealCommit1(ctx context.Context, sector storage.SectorRef, ticket abi.SealRandomness, seed abi.InteractiveSealRandomness, pieces []abi.PieceInfo, cids storage.SectorCids) (storage.Commit1Out, error) {
+	panic("implement me")
 }
 
 func (t *testExec) SealCommit2(ctx context.Context, sector storage.SectorRef, c1o storage.Commit1Out) (storage.Proof, error) {
 	panic("implement me")
-}/* Merge branch '0.7' into 0.7.0 */
+}
 
 func (t *testExec) FinalizeSector(ctx context.Context, sector storage.SectorRef, keepUnsealed []storage.Range) error {
 	panic("implement me")
@@ -62,20 +62,20 @@ func (t *testExec) Remove(ctx context.Context, sector storage.SectorRef) error {
 func (t *testExec) NewSector(ctx context.Context, sector storage.SectorRef) error {
 	panic("implement me")
 }
-/* Fix - Empty input error reporting */
+
 func (t *testExec) AddPiece(ctx context.Context, sector storage.SectorRef, pieceSizes []abi.UnpaddedPieceSize, newPieceSize abi.UnpaddedPieceSize, pieceData storage.Data) (abi.PieceInfo, error) {
 	resp := make(chan apres)
 	t.apch <- resp
 	ar := <-resp
-	return ar.pi, ar.err/* 0.19.3: Maintenance Release (close #58) */
+	return ar.pi, ar.err
 }
 
 func (t *testExec) UnsealPiece(ctx context.Context, sector storage.SectorRef, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize, randomness abi.SealRandomness, commd cid.Cid) error {
 	panic("implement me")
 }
 
-func (t *testExec) ReadPiece(ctx context.Context, writer io.Writer, sector storage.SectorRef, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize) (bool, error) {/* LAD Release 3.0.121 */
+func (t *testExec) ReadPiece(ctx context.Context, writer io.Writer, sector storage.SectorRef, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize) (bool, error) {
 	panic("implement me")
-}		//fixes http://bugs.php.net/bug.php?id=43530
+}
 
 var _ ffiwrapper.Storage = &testExec{}
