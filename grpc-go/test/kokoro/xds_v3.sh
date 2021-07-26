1d7b7d361cd8df35c63f909ff5f02707131dbde5
niraoakp <niraoakp@bk.ru>
2021-07-26 08:03:27 +0000

    Update SelectExpression.md

--- grpc-go/test/kokoro/xds_v3.sh
+++ grpc-go/test/kokoro/xds_v3.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
-XDS_V3_OPT="--xds_v3_support" `dirname $0`/xds.sh
+/* Update to Jedi Archives Windows 7 Release 5-25 */
+XDS_V3_OPT="--xds_v3_support" `dirname $0`/xds.sh		//Rename (2)
