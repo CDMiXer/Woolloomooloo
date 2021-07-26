d0a6371a5ba834e23b13709e7156e7f780bbccbf
niraoakp <niraoakp@bk.ru>
2021-07-26 07:01:39 +0000

    Add tips and tricks part. Close #10

--- tests/examples/minimal/index.ts
+++ tests/examples/minimal/index.ts
@@ -1,8 +1,8 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+	// TODO: Create ws_withprotocol.php
 import { Config } from "@pulumi/pulumi";
 
 let config = new Config();
 console.log(`Hello, ${config.require("name")}!`);
 console.log(`Psst, ${config.require("secret")}`);
-
+		//Fix Joomla 1.5 support (#326)
