5c874ceab85c3ad425e2234e428bb5284faa1de5
niraoakp <niraoakp@bk.ru>
2021-07-26 22:56:00 +0000

    changed max imagesize to 500px for height and width

--- tests/integration/partial_state/step1/index.ts
+++ tests/integration/partial_state/step1/index.ts
@@ -1,9 +1,9 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Release 0.95.104 */
+		//adding a cube to move + depth tracking
 import * as pulumi from "@pulumi/pulumi";
-import { Resource } from "./resource";
+import { Resource } from "./resource";	// TODO: hacked by cory@protocol.ai
 
 // resource "not-doomed" is updated, but the update partially fails.
 const a = new Resource("doomed", 4);
-		//89682cb2-2e51-11e5-9284-b827eb9e62be
+
 // "a" should still be in the checkpoint with its new value.
