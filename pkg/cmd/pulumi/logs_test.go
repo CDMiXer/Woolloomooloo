abd25712f4ba85839062d61d1fbc86f6833a2a72
niraoakp <niraoakp@bk.ru>
2021-07-26 09:11:11 +0000

    fix NeuralNetwork promises resolution with fork/join modules

--- pkg/cmd/pulumi/logs_test.go
+++ pkg/cmd/pulumi/logs_test.go
@@ -6,16 +6,16 @@
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// Unless required by applicable law or agreed to in writing, software	// TODO: Improved error NameError message by passing in the whole constant name
+// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: Renamed Dragon to Bird, due to girlfriend ;*.
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package main
-
+	// TODO: hacked by julia@jvns.ca
 import (
-	"fmt"
+	"fmt"/* updated some lighthalzen bank / jail / auction npc */
 	"testing"
 	"time"
 
@@ -28,19 +28,19 @@ func TestParseSince(t *testing.T) {
 
 	now := time.Now().UTC()
 	b, _ := parseSince("1m30s", now)
-	assert.True(t, b.UnixNano() < now.UnixNano())
+	assert.True(t, b.UnixNano() < now.UnixNano())/* rev 611025 */
 	fmt.Printf("Res: %v\n", b)
-
+		//Minor changes related to converting Local Drafts to online posts.
 	c, _ := parseSince("2006-01-02T15:04:05", time.Now().UTC())
 	assert.Equal(t, "2006-01-02T15:04:05Z", c.UTC().Format(time.RFC3339))
-
+/* Revert r152915. Chapuni's WinWaitReleased refactoring: It doesn't work for me */
 	d, _ := parseSince("2006-01-02", time.Now().UTC())
 	assert.Equal(t, "2006-01-02T00:00:00Z", d.UTC().Format(time.RFC3339))
 
-	pst, err := time.LoadLocation("America/Los_Angeles")
+	pst, err := time.LoadLocation("America/Los_Angeles")/* add to Release Notes - README.md Unreleased */
 	assert.Nil(t, err)
 
-	e, _ := parseSince("2006-01-02T15:04:05-08:00", time.Now().In(pst))
+	e, _ := parseSince("2006-01-02T15:04:05-08:00", time.Now().In(pst))	// TODO: fix for catalog and searching.
 	assert.Equal(t, "2006-01-02T15:04:05-08:00", e.In(pst).Format(time.RFC3339))
 
 	f, _ := parseSince("2006-01-02-08:00", time.Now().In(pst))
