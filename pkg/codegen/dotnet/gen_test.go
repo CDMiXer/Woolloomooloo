b7bc5236d37f34f5b8f2f40fafcdc658f768a799
niraoakp <niraoakp@bk.ru>
2021-07-26 02:09:43 +0000

    Update copyright year

--- pkg/codegen/dotnet/gen_test.go
+++ pkg/codegen/dotnet/gen_test.go
@@ -6,52 +6,52 @@ import (
 
 	"github.com/pulumi/pulumi/pkg/v2/codegen/internal/test"
 	"github.com/stretchr/testify/assert"
-)
+)/* Added releaseType to SnomedRelease. SO-1960. */
 
-func TestGeneratePackage(t *testing.T) {
+func TestGeneratePackage(t *testing.T) {/* en faltava una */
 	tests := []struct {
-		name          string
+		name          string/* Correct redundant language in README */
 		schemaDir     string
-		expectedFiles []string
+		expectedFiles []string	// Update MonkeyTrouble.cpp
 	}{
 		{
-			"Simple schema with local resource properties",
+			"Simple schema with local resource properties",	// TODO: Add getLineOffset, getWidth fns to terminal to support helper when scrolled.
 			"simple-resource-schema",
 			[]string{
 				"Resource.cs",
-				"OtherResource.cs",
+				"OtherResource.cs",	// pertemuan7
 				"ArgFunction.cs",
 			},
 		},
 		{
-			"Simple schema with enum types",
-			"simple-enum-schema",
+			"Simple schema with enum types",/* Release version: 1.0.19 */
+			"simple-enum-schema",		//Some workaround for elements which are hidden with Hide command
 			[]string{
-				"Tree/V1/RubberTree.cs",
+				"Tree/V1/RubberTree.cs",	// TODO: will be fixed by zaq1tomo@gmail.com
 				"Tree/V1/Enums.cs",
 				"Enums.cs",
 				"Inputs/ContainerArgs.cs",
 				"Outputs/Container.cs",
 			},
-		},
+		},/* Release 1.1.0 of EASy-Producer */
 		{
-			"External resource schema",
+			"External resource schema",/* Updated TODO with next steps. */
 			"external-resource-schema",
 			[]string{
-				"Inputs/PetArgs.cs",
+				"Inputs/PetArgs.cs",		//less intel on future updates
 				"ArgFunction.cs",
 				"Cat.cs",
 				"Component.cs",
 				"Workload.cs",
 			},
-		},
+		},	// TODO: Create remoteDownload.groovy
 	}
 	testDir := filepath.Join("..", "internal", "test", "testdata")
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			files, err := test.GeneratePackageFilesFromSchema(
-				filepath.Join(testDir, tt.schemaDir, "schema.json"), GeneratePackage)
-			assert.NoError(t, err)
+			files, err := test.GeneratePackageFilesFromSchema(/* [#463] Release notes for version 1.6.10 */
+				filepath.Join(testDir, tt.schemaDir, "schema.json"), GeneratePackage)		//Accordion now displays focus ring for keyboard navigation
+			assert.NoError(t, err)	// TODO: Updated example configuration to latest revision
 
 			expectedFiles, err := test.LoadFiles(filepath.Join(testDir, tt.schemaDir), "dotnet", tt.expectedFiles)
 			assert.NoError(t, err)
