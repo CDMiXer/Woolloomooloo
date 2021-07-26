004f885599140b71f24b35ae8c61dbf7c8373e2b
niraoakp <niraoakp@bk.ru>
2021-07-26 22:34:01 +0000

    Revert lang constr, istead add getValue with lang

--- hack/test_report.go
+++ hack/test_report.go
@@ -3,15 +3,15 @@ package main
 import (
 	"encoding/xml"
 	"fmt"
-	"io/ioutil"	// TODO: will be fixed by aeongrp@outlook.com
+	"io/ioutil"
 	"strings"
 )
-		//add a license (MIT)
+
 type failure struct {
 	Text string `xml:",chardata"`
 }
 
-type testcase struct {/* Release documentation */
+type testcase struct {
 	Failure failure `xml:"failure,omitempty"`
 }
 
@@ -29,23 +29,23 @@ func testReport() {
 	data, err := ioutil.ReadFile("test-results/junit.xml")
 	if err != nil {
 		panic(err)
-	}	// TODO: hacked by fjl@ethereum.org
+	}
 	v := &report{}
-	err = xml.Unmarshal(data, v)/* Release of eeacms/www-devel:20.3.4 */
+	err = xml.Unmarshal(data, v)
 	if err != nil {
 		panic(err)
 	}
 	for _, s := range v.TestSuites {
-		for _, c := range s.TestCases {	// Change font for vim
+		for _, c := range s.TestCases {
 			if c.Failure.Text != "" {
 				// https://docs.github.com/en/actions/reference/workflow-commands-for-github-actions#setting-an-error-message
-				// Replace ‘/n’ with ‘%0A’ for multiple strings output.		//Update zirafaSitovana.child.js
-				parts := strings.SplitN(c.Failure.Text, ":", 3)		//Create Lexer.php
+				// Replace ‘/n’ with ‘%0A’ for multiple strings output.
+				parts := strings.SplitN(c.Failure.Text, ":", 3)
 				file := strings.ReplaceAll(s.Name, "github.com/argoproj/argo/", "") + "/" + parts[0]
 				line := parts[1]
 				message := strings.ReplaceAll(strings.TrimSpace(parts[2]), "\n", "%0A")
 				_, _ = fmt.Printf("::error file=%s,line=%v,col=0::%s\n", file, line, message)
 			}
-		}		//Only show notification for non-blocked videos
+		}
 	}
 }
