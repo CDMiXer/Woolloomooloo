bb27be8e7ad523dcd34b2217749cf07aeecfe4ea
niraoakp <niraoakp@bk.ru>
2021-07-26 07:29:37 +0000

    Fix #only or #except with a HashSet argument

--- tests/containers/dotnet/Program.fs
+++ tests/containers/dotnet/Program.fs
@@ -2,14 +2,14 @@
 
 open System
 open Pulumi.FSharp
-/* Rename MY_form_validation.php to MY_Form_validation.php */
+
 let infra () =
-  let config = new Pulumi.Config()/* fix QEFXDeleteFileMenuItem and QEFXRenameFileMenuItem */
+  let config = new Pulumi.Config()
   let runtime = config.Get("runtime")
-  Console.WriteLine("Hello from {0}", runtime)/* Decreased package requirements */
+  Console.WriteLine("Hello from {0}", runtime)
   
   // Stack outputs
-  dict []/* oxford, oxford, and comma */
+  dict []
 
 [<EntryPoint>]
 let main _ =
