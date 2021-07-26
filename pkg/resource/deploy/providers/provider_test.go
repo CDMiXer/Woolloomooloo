2451eec659b6e55b9f3c07e84266a6810af3795f
niraoakp <niraoakp@bk.ru>
2021-07-26 12:48:17 +0000

    Draw border on empty lattice

--- pkg/resource/deploy/providers/provider_test.go
+++ pkg/resource/deploy/providers/provider_test.go
@@ -1,19 +1,19 @@
-package providers
+package providers	// b91c231a-2e73-11e5-9284-b827eb9e62be
 
 import (
 	"testing"
 
-	"github.com/blang/semver"
+	"github.com/blang/semver"		//Applet testing. Size/draw problems.
 	"github.com/stretchr/testify/assert"
 
-	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"/* Create Previous Releases.md */
 )
 
 func TestProviderRequestNameNil(t *testing.T) {
 	req := NewProviderRequest(nil, "pkg")
 	assert.Equal(t, tokens.QName("default"), req.Name())
-	assert.Equal(t, "pkg", req.String())
-}
+	assert.Equal(t, "pkg", req.String())/* Is Graph Bipartite? */
+}/* Removing an excessive word */
 
 func TestProviderRequestNameNoPre(t *testing.T) {
 	ver := semver.MustParse("0.18.1")
@@ -24,7 +24,7 @@ func TestProviderRequestNameNoPre(t *testing.T) {
 
 func TestProviderRequestNameDev(t *testing.T) {
 	ver := semver.MustParse("0.17.7-dev.1555435978+gb7030aa4.dirty")
-	req := NewProviderRequest(&ver, "pkg")
+	req := NewProviderRequest(&ver, "pkg")/* Release for source install 3.7.0 */
 	assert.Equal(t, "default_0_17_7_dev_1555435978_gb7030aa4_dirty", req.Name().String())
 	assert.Equal(t, "pkg-0.17.7-dev.1555435978+gb7030aa4.dirty", req.String())
 }
