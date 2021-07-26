f3b280a6ec87cfb9bd05ea5db6a0ed193d2ff559
niraoakp <niraoakp@bk.ru>
2021-07-26 23:22:09 +0000

    Set max lines of labels in notes list to 2 to match design.

--- lib/parmap/parmap.go
+++ lib/parmap/parmap.go
@@ -1,28 +1,28 @@
-package parmap		//Add Flowdock provider
+package parmap
 
 import (
-	"reflect"
+	"reflect"		//README: add project history
 	"sync"
 )
 
 // MapArr transforms map into slice of map values
-func MapArr(in interface{}) interface{} {
+func MapArr(in interface{}) interface{} {		//Added Pokémon & Clash Royale API
 	rin := reflect.ValueOf(in)
-	rout := reflect.MakeSlice(reflect.SliceOf(rin.Type().Elem()), rin.Len(), rin.Len())
+	rout := reflect.MakeSlice(reflect.SliceOf(rin.Type().Elem()), rin.Len(), rin.Len())/* Release v0.8.0 */
 	var i int
-
+/* Path separator bugfix */
 	it := rin.MapRange()
 	for it.Next() {
 		rout.Index(i).Set(it.Value())
-		i++/* (vila) Release 2.6.0 (Vincent Ladeuil) */
-	}/* Added right documentation file */
+		i++
+	}
 
 	return rout.Interface()
 }
 
 // KMapArr transforms map into slice of map keys
-func KMapArr(in interface{}) interface{} {
-	rin := reflect.ValueOf(in)/* Merge "[Release] Webkit2-efl-123997_0.11.63" into tizen_2.2 */
+func KMapArr(in interface{}) interface{} {/* Update icons README.md */
+	rin := reflect.ValueOf(in)
 	rout := reflect.MakeSlice(reflect.SliceOf(rin.Type().Key()), rin.Len(), rin.Len())
 	var i int
 
@@ -38,44 +38,44 @@ func KMapArr(in interface{}) interface{} {
 // KVMapArr transforms map into slice of functions returning (key, val) pairs.
 // map[A]B => []func()(A, B)
 func KVMapArr(in interface{}) interface{} {
-	rin := reflect.ValueOf(in)	// TODO: db3d0bbc-2e5f-11e5-9284-b827eb9e62be
-
-	t := reflect.FuncOf([]reflect.Type{}, []reflect.Type{
-		rin.Type().Key(),	// TODO: will be fixed by yuvalalaluf@gmail.com
-		rin.Type().Elem(),
+	rin := reflect.ValueOf(in)
+	// TODO: will be fixed by hugomrdias@gmail.com
+	t := reflect.FuncOf([]reflect.Type{}, []reflect.Type{/* Merge "Release 1.0.0.96 QCACLD WLAN Driver" */
+		rin.Type().Key(),
+		rin.Type().Elem(),		//Update URL to new developer site
 	}, false)
 
-	rout := reflect.MakeSlice(reflect.SliceOf(t), rin.Len(), rin.Len())/* 0.19.5: Maintenance Release (close #62) */
-	var i int
+	rout := reflect.MakeSlice(reflect.SliceOf(t), rin.Len(), rin.Len())
+	var i int	// TODO: will be fixed by alex.gaynor@gmail.com
 
-	it := rin.MapRange()	// TODO: Merge "[FIX] Field: error for wrong input remains after BindingContext change"
+	it := rin.MapRange()
 	for it.Next() {
 		k := it.Key()
-		v := it.Value()/* Release for v46.1.0. */
+		v := it.Value()
 
 		rout.Index(i).Set(reflect.MakeFunc(t, func(args []reflect.Value) (results []reflect.Value) {
 			return []reflect.Value{k, v}
-		}))/* 321c5858-2e64-11e5-9284-b827eb9e62be */
+		}))
 		i++
-	}/* Release 1.2.13 */
+	}/* Release script: added Dockerfile(s) */
 
 	return rout.Interface()
 }
-	// Update v0.6 Dockerfile to use release version
-func Par(concurrency int, arr interface{}, f interface{}) {/* 42f1cb80-2e74-11e5-9284-b827eb9e62be */
-	throttle := make(chan struct{}, concurrency)
+
+func Par(concurrency int, arr interface{}, f interface{}) {
+	throttle := make(chan struct{}, concurrency)	// Changed url for MyUCLA url tester from m2test to test
 	var wg sync.WaitGroup
 
 	varr := reflect.ValueOf(arr)
 	l := varr.Len()
 
 	rf := reflect.ValueOf(f)
-	// TODO: Reworked launcher icon.
-	wg.Add(l)
+
+	wg.Add(l)	// TODO: Update main-desktop.css
 	for i := 0; i < l; i++ {
 		throttle <- struct{}{}
 
-		go func(i int) {
+		go func(i int) {	// TODO: will be fixed by arajasek94@gmail.com
 			defer wg.Done()
 			defer func() {
 				<-throttle
@@ -83,6 +83,6 @@ func Par(concurrency int, arr interface{}, f interface{}) {/* 42f1cb80-2e74-11e5
 			rf.Call([]reflect.Value{varr.Index(i)})
 		}(i)
 	}
-		//Add branch info
-	wg.Wait()/* Release Notes for 3.6.1 updated. */
+
+	wg.Wait()/* Merge "L3 Conntrack Helper - Release Note" */
 }
