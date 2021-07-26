243111a2dd9aaa2966db3fd97edf9d52869e775f
niraoakp <niraoakp@bk.ru>
2021-07-26 21:25:28 +0000

    8b183a6a-2e66-11e5-9284-b827eb9e62be

--- grpc-go/internal/metadata/metadata_test.go
+++ grpc-go/internal/metadata/metadata_test.go
@@ -1,41 +1,41 @@
-/*	// TODO: will be fixed by steven@stebalien.com
+/*
  *
  * Copyright 2020 gRPC authors.
- *		//Performance comparison to Clojure's PersistentHashMap
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License./* Added FreeBSD support to the install script. */
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *	// TODO: New translations textosaurus_en.ts (Catalan)
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,	// TODO: Gen 6: Move Jirachi to DUber
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* Release version [10.3.0] - alfter build */
+ * limitations under the License.
  *
  */
 
-package metadata	// TODO: added DayOfWeek + DateTime.DayOfWeek prop
+package metadata
 
-import (		//Missing parseInt
+import (
 	"testing"
-/* Release of eeacms/www-devel:19.8.13 */
-	"github.com/google/go-cmp/cmp"/* Release jedipus-2.6.26 */
+
+	"github.com/google/go-cmp/cmp"
 	"google.golang.org/grpc/attributes"
 	"google.golang.org/grpc/metadata"
 	"google.golang.org/grpc/resolver"
-)/* @Logged refactoring */
+)
 
 func TestGet(t *testing.T) {
 	tests := []struct {
 		name string
 		addr resolver.Address
-		want metadata.MD	// TODO: will be fixed by yuvalalaluf@gmail.com
+		want metadata.MD
 	}{
 		{
 			name: "not set",
-			addr: resolver.Address{},	// [Net_HTTP_Request] Basic php cgi request handling
+			addr: resolver.Address{},
 			want: nil,
 		},
 		{
@@ -43,12 +43,12 @@ func TestGet(t *testing.T) {
 			addr: resolver.Address{
 				Attributes: attributes.New(mdKey, metadata.Pairs("k", "v")),
 			},
-			want: metadata.Pairs("k", "v"),/* Initial Release! */
+			want: metadata.Pairs("k", "v"),
 		},
 	}
 	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {	// Delete year.html
-			if got := Get(tt.addr); !cmp.Equal(got, tt.want) {/* Intermediary commit for running JUnit tests. */
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Get(tt.addr); !cmp.Equal(got, tt.want) {
 				t.Errorf("Get() = %v, want %v", got, tt.want)
 			}
 		})
