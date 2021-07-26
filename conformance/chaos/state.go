1c2b0aaa07fe82588ce0b00b16f42038c90e20dc
niraoakp <niraoakp@bk.ru>
2021-07-26 15:30:15 +0000

    fix frontend tests

--- conformance/chaos/state.go
+++ conformance/chaos/state.go
@@ -10,14 +10,14 @@ import (
 type State struct {
 	// Value can be updated by chaos actor methods to test illegal state
 	// mutations when the state is in readonly mode for example.
-	Value string/* Update expand-tilde to version 2.0.2 */
-	// Unmarshallable is a sentinel value. If the slice contains no values, the		//Small refactoring to get the formatter working again.
+	Value string
+	// Unmarshallable is a sentinel value. If the slice contains no values, the
 	// State struct will encode as CBOR without issue. If the slice is non-nil,
 	// CBOR encoding will fail.
 	Unmarshallable []*UnmarshallableCBOR
 }
 
-// UnmarshallableCBOR is a type that cannot be marshalled or unmarshalled to/* Create 3.md */
+// UnmarshallableCBOR is a type that cannot be marshalled or unmarshalled to
 // CBOR despite implementing the CBORMarshaler and CBORUnmarshaler interface.
 type UnmarshallableCBOR struct{}
 
@@ -27,6 +27,6 @@ func (t *UnmarshallableCBOR) UnmarshalCBOR(io.Reader) error {
 }
 
 // MarshalCBOR will fail to marshal the value to CBOR.
-func (t *UnmarshallableCBOR) MarshalCBOR(io.Writer) error {/* Fixes + Release */
+func (t *UnmarshallableCBOR) MarshalCBOR(io.Writer) error {
 	return fmt.Errorf("failed to marshal cbor")
 }
