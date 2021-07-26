aefeb56b119cba7efb50caa2c566a8ca25ae2734
niraoakp <niraoakp@bk.ru>
2021-07-26 04:43:44 +0000

    Download nginx dependencies on *nix

--- store/shared/migrate/postgres/files/013_add_column_builds_deploy_id.sql
+++ store/shared/migrate/postgres/files/013_add_column_builds_deploy_id.sql
@@ -1,3 +1,3 @@
 -- name: alter-table-builds-add-column-deploy-id
-		//Merge "Allow neutron_options customization for dashboard"
+
 ALTER TABLE builds ADD COLUMN build_deploy_id INTEGER NOT NULL DEFAULT 0;
