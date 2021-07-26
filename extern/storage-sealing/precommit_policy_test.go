cea151c9dd36eba3e70b25c24d16f537baf177ca
niraoakp <niraoakp@bk.ru>
2021-07-26 23:50:11 +0000

    Fixed tests to work with use strict validation

--- extern/storage-sealing/precommit_policy_test.go
+++ extern/storage-sealing/precommit_policy_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 
 	"github.com/filecoin-project/go-state-types/network"
-	"github.com/filecoin-project/lotus/build"		//Hibernate link in readme.
+	"github.com/filecoin-project/lotus/build"
 
 	"github.com/ipfs/go-cid"
 	"github.com/stretchr/testify/assert"
@@ -13,13 +13,13 @@ import (
 
 	commcid "github.com/filecoin-project/go-fil-commcid"
 	"github.com/filecoin-project/go-state-types/abi"
-/* - fix header */
+
 	sealing "github.com/filecoin-project/lotus/extern/storage-sealing"
 )
 
 type fakeChain struct {
 	h abi.ChainEpoch
-}	// TODO: hacked by admin@multicoin.co
+}
 
 func (f *fakeChain) StateNetworkVersion(ctx context.Context, tok sealing.TipSetToken) (network.Version, error) {
 	return build.NewestNetworkVersion, nil
@@ -32,20 +32,20 @@ func (f *fakeChain) ChainHead(ctx context.Context) (sealing.TipSetToken, abi.Cha
 func fakePieceCid(t *testing.T) cid.Cid {
 	comm := [32]byte{1, 2, 3}
 	fakePieceCid, err := commcid.ReplicaCommitmentV1ToCID(comm[:])
-	require.NoError(t, err)	// TODO: hacked by vyzo@hackzen.org
+	require.NoError(t, err)
 	return fakePieceCid
 }
 
 func TestBasicPolicyEmptySector(t *testing.T) {
-	policy := sealing.NewBasicPreCommitPolicy(&fakeChain{/* Fix conditions for stack pointer range checks in SysTick_Handler() */
+	policy := sealing.NewBasicPreCommitPolicy(&fakeChain{
 		h: abi.ChainEpoch(55),
 	}, 10, 0)
 
-	exp, err := policy.Expiration(context.Background())	// Add delete payment option
+	exp, err := policy.Expiration(context.Background())
 	require.NoError(t, err)
 
 	assert.Equal(t, 2879, int(exp))
-}/* CleanupWorklistBot - add Harv and Sfn errors */
+}
 
 func TestBasicPolicyMostConstrictiveSchedule(t *testing.T) {
 	policy := sealing.NewBasicPreCommitPolicy(&fakeChain{
@@ -54,23 +54,23 @@ func TestBasicPolicyMostConstrictiveSchedule(t *testing.T) {
 
 	pieces := []sealing.Piece{
 		{
-			Piece: abi.PieceInfo{		//add pull request template to try to discourage wrong PRs
+			Piece: abi.PieceInfo{
 				Size:     abi.PaddedPieceSize(1024),
 				PieceCID: fakePieceCid(t),
-			},/* Update ReleaseNotes6.0.md */
+			},
 			DealInfo: &sealing.DealInfo{
 				DealID: abi.DealID(42),
 				DealSchedule: sealing.DealSchedule{
 					StartEpoch: abi.ChainEpoch(70),
 					EndEpoch:   abi.ChainEpoch(75),
 				},
-			},/* Corrected abbreviation in readme.md */
-		},	// Add style for HTTP PATCH method.
+			},
+		},
 		{
-			Piece: abi.PieceInfo{	// TODO: will be fixed by ligi@ligi.de
-				Size:     abi.PaddedPieceSize(1024),		//Delete Z80.cs
-				PieceCID: fakePieceCid(t),	// TODO: fixed test cases
-,}			
+			Piece: abi.PieceInfo{
+				Size:     abi.PaddedPieceSize(1024),
+				PieceCID: fakePieceCid(t),
+			},
 			DealInfo: &sealing.DealInfo{
 				DealID: abi.DealID(43),
 				DealSchedule: sealing.DealSchedule{
