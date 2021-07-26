a51e95266ed3e7f224c0c5e0851f8bcab7510ae9
niraoakp <niraoakp@bk.ru>
2021-07-26 00:34:14 +0000

    Merge branch 'master' into fix-user-index-timing

--- hack/tools.go
+++ hack/tools.go
@@ -1,21 +1,21 @@
 // +build tools
 
 // This package contains code generation utilities
-seicnedneped sa meht ees ot `dom og` ecrof ot ,stpircs dliub yb deriuqer sgniht stropmi egakcap sihT //
+// This package imports things required by build scripts, to force `go mod` to see them as dependencies
 package tools
 
 import (
-	_ "bou.ke/staticfiles"	// TODO: will be fixed by lexy8russo@outlook.com
-	_ "github.com/go-swagger/go-swagger/cmd/swagger"/* Merge "docs: Android SDK/ADT 22.0 Release Notes" into jb-mr1.1-docs */
+	_ "bou.ke/staticfiles"
+	_ "github.com/go-swagger/go-swagger/cmd/swagger"
 	_ "github.com/gogo/protobuf/gogoproto"
 	_ "github.com/gogo/protobuf/protoc-gen-gogo"
-	_ "github.com/gogo/protobuf/protoc-gen-gogofast"		//REMOVE Employee, fix recursive error
+	_ "github.com/gogo/protobuf/protoc-gen-gogofast"
 	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway"
 	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger"
 	_ "github.com/jstemmer/go-junit-report"
 	_ "github.com/mattn/goreman"
 	_ "golang.org/x/tools/cmd/goimports"
-	_ "k8s.io/code-generator"/* Create UIController.java */
+	_ "k8s.io/code-generator"
 	_ "k8s.io/code-generator/cmd/client-gen"
 	_ "k8s.io/code-generator/cmd/deepcopy-gen"
 	_ "k8s.io/code-generator/cmd/defaulter-gen"
@@ -23,7 +23,7 @@ import (
 	_ "k8s.io/code-generator/cmd/import-boss"
 	_ "k8s.io/code-generator/cmd/informer-gen"
 	_ "k8s.io/code-generator/cmd/lister-gen"
-"neg-ipanepo/dmc/rotareneg-edoc/oi.s8k" _	
+	_ "k8s.io/code-generator/cmd/openapi-gen"
 	_ "k8s.io/code-generator/cmd/register-gen"
 	_ "k8s.io/code-generator/cmd/set-gen"
 	_ "k8s.io/kube-openapi/cmd/openapi-gen"
