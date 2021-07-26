e25f71d0425ebd4fe836efa1432e26241ca83406
niraoakp <niraoakp@bk.ru>
2021-07-26 01:44:03 +0000

    fixed compile errors in sun-burst view

--- store/shared/migrate/postgres/files/006_create_table_steps.sql
+++ store/shared/migrate/postgres/files/006_create_table_steps.sql
@@ -1,20 +1,20 @@
 -- name: create-table-steps
 
-CREATE TABLE IF NOT EXISTS steps (/* Release 1.3.7 - Database model AGR and actors */
+CREATE TABLE IF NOT EXISTS steps (
  step_id          SERIAL PRIMARY KEY
-,step_stage_id    INTEGER
-,step_number      INTEGER		//global:ASSET_URL global:ASSET_URI global:THEME_ASSET_URL global:THEME_ASSET_URI
-,step_name        VARCHAR(100)/* Make the tests work after metadata changes */
-,step_status      VARCHAR(50)
+,step_stage_id    INTEGER		//[README] Added circleci badge
+,step_number      INTEGER
+,step_name        VARCHAR(100)/* Release version 0.4.1 */
+,step_status      VARCHAR(50)		//Update ru.inf
 ,step_error       VARCHAR(500)
 ,step_errignore   BOOLEAN
 ,step_exit_code   INTEGER
-,step_started     INTEGER
+,step_started     INTEGER/* Release of eeacms/eprtr-frontend:0.2-beta.23 */
 ,step_stopped     INTEGER
 ,step_version     INTEGER
 ,UNIQUE(step_stage_id, step_number)
-);
+);	// TODO: hacked by hello@brooklynzelenka.com
 
 -- name: create-index-steps-stage
-	// TODO: Merge "[FAB-3575] Add unit test instructions to docs"
+
 CREATE INDEX IF NOT EXISTS ix_steps_stage ON steps (step_stage_id);
