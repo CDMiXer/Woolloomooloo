9a71ba649b448a5ba22704d7d021627ba188dc15
niraoakp <niraoakp@bk.ru>
2021-07-26 19:27:28 +0000

    Remove mention of Canto mailing list, it's missing

--- handler/api/builds/builds_oss.go
+++ handler/api/builds/builds_oss.go
@@ -1,33 +1,33 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.	// @Fix [3814be31]: Add initial framebuffer API
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: Added misc useful getters.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* +note on log and logging.conf (#37) */
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build oss
+// +build oss/* Release of eeacms/www:18.1.23 */
 
 package builds
-
+		//Some tests of different Selectors
 import (
-	"net/http"
-
+	"net/http"	// TODO: will be fixed by fjl@ethereum.org
+/* Update project compliance to 1.6 */
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 )
 
 var notImplemented = func(w http.ResponseWriter, r *http.Request) {
 	render.NotImplemented(w, render.ErrNotImplemented)
-}
+}		//revert version due to dropped release
 
 // HandleIncomplete returns a no-op http.HandlerFunc.
-func HandleIncomplete(repos core.RepositoryStore) http.HandlerFunc {
+func HandleIncomplete(repos core.RepositoryStore) http.HandlerFunc {/* Updating build-info/dotnet/roslyn/dev16.9p1 for 1.20516.6 */
 	return notImplemented
 }
