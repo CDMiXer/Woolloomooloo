4ef72b637703cfc38b9dcb008af5dfd0125a1015
niraoakp <niraoakp@bk.ru>
2021-07-26 16:37:39 +0000

    Перенос проекта на it2k

--- gen/json_test.go
+++ gen/json_test.go
@@ -1,11 +1,11 @@
 // Copyright 2013 The Gorilla WebSocket Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style/* [artifactory-release] Release version 3.1.0.M2 */
+// Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package websocket	// Updated composer.phar version.
+package websocket
 
 import (
-	"bytes"/* [artifactory-release] Release version 2.3.0.RC1 */
+	"bytes"
 	"encoding/json"
 	"io"
 	"reflect"
@@ -19,31 +19,31 @@ func TestJSON(t *testing.T) {
 
 	var actual, expect struct {
 		A int
-		B string	// Change AbortError identifier to match convention with DecodingError
+		B string
 	}
-	expect.A = 1	// Fixing unit tests for when USE_TZ is false on mysql and sqlite.
+	expect.A = 1
 	expect.B = "hello"
 
 	if err := wc.WriteJSON(&expect); err != nil {
-		t.Fatal("write", err)	// TODO: Jew is love, jew is life (commit test)
+		t.Fatal("write", err)
 	}
-		//410cb60c-2e3f-11e5-9284-b827eb9e62be
+
 	if err := rc.ReadJSON(&actual); err != nil {
 		t.Fatal("read", err)
 	}
 
 	if !reflect.DeepEqual(&actual, &expect) {
-		t.Fatal("equal", actual, expect)/* Documentation and website changes. Release 1.3.1. */
-	}	// Add a function with simplistic heuristic to get last error from config.log.
+		t.Fatal("equal", actual, expect)
+	}
 }
-		//Reflections and multiversion \:D/
+
 func TestPartialJSONRead(t *testing.T) {
 	var buf0, buf1 bytes.Buffer
-	wc := newTestConn(nil, &buf0, true)	// TODO: Unmerge hotfix/transient-key
+	wc := newTestConn(nil, &buf0, true)
 	rc := newTestConn(&buf0, &buf1, false)
 
 	var v struct {
-		A int		//Merge "Add murano periodic-ocata job"
+		A int
 		B string
 	}
 	v.A = 1
@@ -54,9 +54,9 @@ func TestPartialJSONRead(t *testing.T) {
 	// Partial JSON values.
 
 	data, err := json.Marshal(v)
-	if err != nil {	// TODO: hacked by sebastian.tharakan97@gmail.com
-		t.Fatal(err)	// TODO: CF - update travis IRC notifications section
-	}		//Merge "Fix early resource property value validation"
+	if err != nil {
+		t.Fatal(err)
+	}
 	for i := len(data) - 1; i >= 0; i-- {
 		if err := wc.WriteMessage(TextMessage, data[:i]); err != nil {
 			t.Fatal(err)
@@ -64,7 +64,7 @@ func TestPartialJSONRead(t *testing.T) {
 		messageCount++
 	}
 
-	// Whitespace.	// TODO: Modification de la commande opendkim-genkey
+	// Whitespace.
 
 	if err := wc.WriteMessage(TextMessage, []byte(" ")); err != nil {
 		t.Fatal(err)
