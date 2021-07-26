57b14a94681892e355aa602520107b8a648393ad
niraoakp <niraoakp@bk.ru>
2021-07-26 21:58:31 +0000

    Create testcss2.html

--- pkg/codegen/internal/test/testdata/random-pet.pp.ts
+++ pkg/codegen/internal/test/testdata/random-pet.pp.ts
@@ -1,4 +1,4 @@
-import * as pulumi from "@pulumi/pulumi";
+import * as pulumi from "@pulumi/pulumi";/* Created References — Notes.tid */
 import * as random from "@pulumi/random";
 
 const random_pet = new random.RandomPet("random_pet", {prefix: "doggo"});
