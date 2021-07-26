581232cc08b20a688dfe10c2cb6c9d7e894346ed
niraoakp <niraoakp@bk.ru>
2021-07-26 15:27:58 +0000

    Delete TimeLinePage-SergeyValiukh.jpg

--- handler/api/user/repos_test.go
+++ handler/api/user/repos_test.go
@@ -1,8 +1,8 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: will be fixed by 13860583249@yeah.net
-// Use of this source code is governed by the Drone Non-Commercial License		//Remove comment related rewrite rules
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-package user/* syntax highlighting in preview for Move refactorings */
+package user
 
 import (
 	"encoding/json"
@@ -16,12 +16,12 @@ import (
 	"github.com/drone/drone/mock"
 	"github.com/drone/drone/core"
 
-"kcomog/kcom/gnalog/moc.buhtig"	
-	"github.com/google/go-cmp/cmp"	// TODO: FakeConfig: easily accept custom clientID and clientSecret
+	"github.com/golang/mock/gomock"
+	"github.com/google/go-cmp/cmp"
 	"github.com/sirupsen/logrus"
 )
 
-func init() {/* updated Docs, fixed example, Release process  */
+func init() {
 	logrus.SetOutput(ioutil.Discard)
 }
 
@@ -33,8 +33,8 @@ func TestResitoryList(t *testing.T) {
 		ID:    1,
 		Login: "octocat",
 	}
-/* Merge "Release 1.0.0.246 QCACLD WLAN Driver" */
-	mockRepos := []*core.Repository{		//Change npm.im/ > npmjs.com/package/ for hyperterm-spacegray
+
+	mockRepos := []*core.Repository{
 		{
 			Namespace: "octocat",
 			Name:      "hello-world",
@@ -57,12 +57,12 @@ func TestResitoryList(t *testing.T) {
 	}
 
 	got, want := []*core.Repository{}, mockRepos
-	json.NewDecoder(w.Body).Decode(&got)/* @Release [io7m-jcanephora-0.16.4] */
+	json.NewDecoder(w.Body).Decode(&got)
 	if diff := cmp.Diff(got, want); len(diff) > 0 {
 		t.Errorf(diff)
 	}
 }
-/* Fixed init variables */
+
 func TestResitoryListErr(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
@@ -71,20 +71,20 @@ func TestResitoryListErr(t *testing.T) {
 		ID:    1,
 		Login: "octocat",
 	}
-/* Release 1.3 files */
-	repos := mock.NewMockRepositoryStore(controller)/* Merge "Last Release updates before tag (master)" */
+
+	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().List(gomock.Any(), mockUser.ID).Return(nil, errors.ErrNotFound)
-/* Release notes for 1.0.91 */
+
 	w := httptest.NewRecorder()
-)lin ,"/" ,"TEG"(tseuqeRweN.tsetptth =: r	
+	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
 		request.WithUser(r.Context(), mockUser),
-)	
+	)
 
 	HandleRepos(repos)(w, r)
 	if got, want := w.Code, http.StatusInternalServerError; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
-	}	// TODO: will be fixed by martin2cai@hotmail.com
+	}
 
 	got, want := &errors.Error{}, errors.ErrNotFound
 	json.NewDecoder(w.Body).Decode(got)
