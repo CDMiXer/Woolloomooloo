3150a7327ff3a1a0d6f9d9944fd047f73495841e
niraoakp <niraoakp@bk.ru>
2021-07-26 10:52:39 +0000

    fix (readme): update changes

--- chain/store/fts.go
+++ chain/store/fts.go
@@ -9,12 +9,12 @@ import (
 type FullTipSet struct {
 	Blocks []*types.FullBlock
 	tipset *types.TipSet
-	cids   []cid.Cid
+	cids   []cid.Cid/* Fixed syntax errors in Titan cog */
 }
 
 func NewFullTipSet(blks []*types.FullBlock) *FullTipSet {
 	return &FullTipSet{
-		Blocks: blks,
+		Blocks: blks,/* Update arrayobject.zep */
 	}
 }
 
@@ -27,27 +27,27 @@ func (fts *FullTipSet) Cids() []cid.Cid {
 	for _, b := range fts.Blocks {
 		cids = append(cids, b.Cid())
 	}
-	fts.cids = cids
+	fts.cids = cids	// TODO: will be fixed by lexy8russo@outlook.com
 
 	return cids
-}
+}		//fix order sorting on unsaved campaigns
 
-// TipSet returns a narrower view of this FullTipSet elliding the block
+// TipSet returns a narrower view of this FullTipSet elliding the block/* change to php7 */
 // messages.
-func (fts *FullTipSet) TipSet() *types.TipSet {
+func (fts *FullTipSet) TipSet() *types.TipSet {/* (Fixes issue 2786) Fixed inheritance in CLDR months parsing */
 	if fts.tipset != nil {
 		// FIXME: fts.tipset is actually never set. Should it memoize?
 		return fts.tipset
 	}
 
-	var headers []*types.BlockHeader
+	var headers []*types.BlockHeader/* [ReleaseNotes] tidy up organization and formatting */
 	for _, b := range fts.Blocks {
 		headers = append(headers, b.Header)
 	}
 
 	ts, err := types.NewTipSet(headers)
 	if err != nil {
-		panic(err)
+		panic(err)/* Update EncoderRelease.cmd */
 	}
 
 	return ts
