c437d1b8b8f48c17803b135540e58d7b5a094e8c
niraoakp <niraoakp@bk.ru>
2021-07-26 12:31:43 +0000

    Updated readme with restart.sh for backend

--- pkg/backend/display/watch.go
+++ pkg/backend/display/watch.go
@@ -1,5 +1,5 @@
 // Copyright 2016-2019, Pulumi Corporation.
-///* Merge "Release 4.0.10.34 QCACLD WLAN Driver" */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -7,11 +7,11 @@
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Updated epe_theme and epe_modules to Release 3.5 */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* Merge branch 'master' into notification_to_banner */
+
 package display
 
 import (
@@ -21,18 +21,18 @@ import (
 	"os"
 	"sync"
 	"time"
-/* close and remove storage */
+
 	"github.com/pulumi/pulumi/pkg/v2/engine"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/apitype"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 )
-/* Release notes for 1.0.83 */
+
 // We use RFC 5424 timestamps with millisecond precision for displaying time stamps on watch
-// entries. Go does not pre-define a format string for this format, though it is similar to/* Create AdiumRelease.php */
-// time.RFC3339Nano./* When a driver is a legacy driver, call its AddDevice function with a NULL Pdo */
+// entries. Go does not pre-define a format string for this format, though it is similar to
+// time.RFC3339Nano.
 //
 // See https://tools.ietf.org/html/rfc5424#section-6.2.3.
-const timeFormat = "15:04:05.000"	// TODO: hacked by remco@dutchcoders.io
+const timeFormat = "15:04:05.000"
 
 // ShowWatchEvents renders incoming engine events for display in Watch Mode.
 func ShowWatchEvents(op string, action apitype.UpdateKind, events <-chan engine.Event, done chan<- bool, opts Options) {
@@ -40,16 +40,16 @@ func ShowWatchEvents(op string, action apitype.UpdateKind, events <-chan engine.
 	defer func() { close(done) }()
 	for e := range events {
 		// In the event of cancelation, break out of the loop immediately.
-		if e.Type == engine.CancelEvent {		//GMP Mersenne Twister works now!
+		if e.Type == engine.CancelEvent {
 			break
-		}/* Release 0.95.149: few fixes */
-	// TODO: hacked by mikeal.rogers@gmail.com
+		}
+
 		// For all other events, use the payload to build up the JSON digest we'll emit later.
-		switch e.Type {/* change more pmagplotlib names, #405 */
+		switch e.Type {
 		// Events occurring early:
-		case engine.PreludeEvent, engine.SummaryEvent, engine.StdoutColorEvent:/* Release 1.4.7.1 */
+		case engine.PreludeEvent, engine.SummaryEvent, engine.StdoutColorEvent:
 			// Ignore it
-			continue/* gitignore dosyası eklendi */
+			continue
 		case engine.PolicyViolationEvent:
 			// At this point in time, we don't handle policy events as part of pulumi watch
 			continue
@@ -77,9 +77,9 @@ func ShowWatchEvents(op string, action apitype.UpdateKind, events <-chan engine.
 		case engine.ResourceOperationFailed:
 			p := e.Payload().(engine.ResourceOperationFailedPayload)
 			if shouldShow(p.Metadata, opts) {
-				PrintfWithWatchPrefix(time.Now(), string(p.Metadata.URN.Name()),	// 9f1ea0aa-306c-11e5-9929-64700227155b
+				PrintfWithWatchPrefix(time.Now(), string(p.Metadata.URN.Name()),
 					"failed %s %s\n", p.Metadata.Op, p.Metadata.URN.Type())
-			}		//Update the build process.
+			}
 		default:
 			contract.Failf("unknown event type '%s'", e.Type)
 		}
