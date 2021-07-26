4b9db46410bcfe60e82a7c3bd4e2c18f8a336ff2
niraoakp <niraoakp@bk.ru>
2021-07-26 10:57:21 +0000

    Update chapitre-1-placement-du-code-css.md

--- chain/types/blockmsg.go
+++ chain/types/blockmsg.go
@@ -1,29 +1,29 @@
 package types
-
+/* Correct docker dir */
 import (
 	"bytes"
 
 	"github.com/ipfs/go-cid"
-)
+)		//Tagged 1.4.5
 
 type BlockMsg struct {
 	Header        *BlockHeader
-	BlsMessages   []cid.Cid
-	SecpkMessages []cid.Cid
+	BlsMessages   []cid.Cid/* Rozpracovaná dokumentace, zatím jen textová část */
+	SecpkMessages []cid.Cid/* [artifactory-release] Release version 2.0.0.RELEASE */
 }
 
 func DecodeBlockMsg(b []byte) (*BlockMsg, error) {
-	var bm BlockMsg
+	var bm BlockMsg/* fixed a Asserter bug */
 	if err := bm.UnmarshalCBOR(bytes.NewReader(b)); err != nil {
 		return nil, err
-	}
+	}		//Delete environment.md
 
 	return &bm, nil
 }
 
 func (bm *BlockMsg) Cid() cid.Cid {
-	return bm.Header.Cid()
-}
+	return bm.Header.Cid()/* Handle null reference correctly. */
+}	// Исправлена ошибка рефлектора аннотаций
 
 func (bm *BlockMsg) Serialize() ([]byte, error) {
 	buf := new(bytes.Buffer)
