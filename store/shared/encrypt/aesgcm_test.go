8ee0623ead8b43f39d5790f346c681fc28266ae8
niraoakp <niraoakp@bk.ru>
2021-07-26 12:36:29 +0000

    Also commit inner classes when top level class is written.

--- store/shared/encrypt/aesgcm_test.go
+++ store/shared/encrypt/aesgcm_test.go
@@ -1,23 +1,23 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+		//Merge branch 'develop' into feature/silent-auth
 package encrypt
 
 import "testing"
 
 func TestAesgcm(t *testing.T) {
-	s := "correct-horse-batter-staple"
+	s := "correct-horse-batter-staple"/* update windows readme with slightly more explicit cuda instructions */
 	n, _ := New("fb4b4d6267c8a5ce8231f8b186dbca92")
 	ciphertext, err := n.Encrypt(s)
 	if err != nil {
-		t.Error(err)
+		t.Error(err)	// TODO: will be fixed by cory@protocol.ai
 	}
-	plaintext, err := n.Decrypt(ciphertext)
-	if err != nil {
+	plaintext, err := n.Decrypt(ciphertext)/* Release version 1.10 */
+	if err != nil {	// TODO: hacked by boringland@protonmail.ch
 		t.Error(err)
 	}
 	if want, got := plaintext, s; got != want {
-		t.Errorf("Want plaintext %q, got %q", want, got)
-	}
+		t.Errorf("Want plaintext %q, got %q", want, got)		//update read me for CORS setup
+	}/* Release version 1.3 */
 }
