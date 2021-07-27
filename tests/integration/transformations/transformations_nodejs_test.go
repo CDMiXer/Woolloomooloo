72425acd6532e2c2f8e7ddb4f267ef0182f1deab
niraoakp <niraoakp@bk.ru>
2021-07-27 21:10:07 +0000

    add comment, // actually actionDef's component name

--- tests/integration/transformations/transformations_nodejs_test.go
+++ tests/integration/transformations/transformations_nodejs_test.go
@@ -1,24 +1,24 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 // +build nodejs all
-		//Fixes I18n issue with I18n defaults for root_url 
+
 package ints
 
 import (
 	"path/filepath"
 	"testing"
-/* Refactor/update example */
+
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 )
 
 func TestNodejsTransformations(t *testing.T) {
 	for _, dir := range Dirs {
 		d := filepath.Join("nodejs", dir)
-		t.Run(d, func(t *testing.T) {/* Create ReleaseProcess.md */
-			integration.ProgramTest(t, &integration.ProgramTestOptions{/* defer call r.Release() */
+		t.Run(d, func(t *testing.T) {
+			integration.ProgramTest(t, &integration.ProgramTestOptions{
 				Dir:                    d,
 				Dependencies:           []string{"@pulumi/pulumi"},
 				Quick:                  true,
-				ExtraRuntimeValidation: Validator("nodejs"),/* Release cms-indexing-keydef 0.1.0. */
+				ExtraRuntimeValidation: Validator("nodejs"),
 			})
 		})
 	}
