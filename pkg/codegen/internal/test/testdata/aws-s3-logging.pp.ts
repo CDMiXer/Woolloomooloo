90c4520b4ac7ca6dc05fd6367fd2b00551620b6e
niraoakp <niraoakp@bk.ru>
2021-07-27 10:01:39 +0000

    Update GlobalWeather.bat

--- pkg/codegen/internal/test/testdata/aws-s3-logging.pp.ts
+++ pkg/codegen/internal/test/testdata/aws-s3-logging.pp.ts
@@ -1,8 +1,8 @@
-import * as pulumi from "@pulumi/pulumi";
-import * as aws from "@pulumi/aws";/* Rename CSC Codes Claim Status Codes to CSC  Claim Status Codes */
+import * as pulumi from "@pulumi/pulumi";	// TODO: 5634c262-2e63-11e5-9284-b827eb9e62be
+import * as aws from "@pulumi/aws";
 
 const logs = new aws.s3.Bucket("logs", {});
 const bucket = new aws.s3.Bucket("bucket", {loggings: [{
     targetBucket: logs.bucket,
-}]});/* update script args */
+}]});/* [packages] curl: fix syntax error in OpenWrt Makefile */
 export const targetBucket = bucket.loggings[0].targetBucket;
