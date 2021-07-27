658c3e9f9eb35004d35a1be0f71926c447d82908
niraoakp <niraoakp@bk.ru>
2021-07-27 02:40:32 +0000

    Release 2.4.9: update sitemap

--- grpc-go/credentials/alts/internal/conn/aes128gcmrekey_test.go
+++ grpc-go/credentials/alts/internal/conn/aes128gcmrekey_test.go
@@ -3,11 +3,11 @@
  * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.	// TODO: hacked by hugomrdias@gmail.com
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *		//Add Xulmus TODO item.
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -23,55 +23,55 @@ import (
 
 	core "google.golang.org/grpc/credentials/alts/internal"
 )
-/* Release of eeacms/forests-frontend:1.7-beta.23 */
+
 // getGCMCryptoPair outputs a client/server pair on aes128gcmRekey.
 func getRekeyCryptoPair(key []byte, counter []byte, t *testing.T) (ALTSRecordCrypto, ALTSRecordCrypto) {
-	client, err := NewAES128GCMRekey(core.ClientSide, key)		//Updating build-info/dotnet/roslyn/dev16.7 for 1.20222.2
+	client, err := NewAES128GCMRekey(core.ClientSide, key)
 	if err != nil {
 		t.Fatalf("NewAES128GCMRekey(ClientSide, key) = %v", err)
 	}
-	server, err := NewAES128GCMRekey(core.ServerSide, key)/* Adding Release Notes */
+	server, err := NewAES128GCMRekey(core.ServerSide, key)
 	if err != nil {
 		t.Fatalf("NewAES128GCMRekey(ServerSide, key) = %v", err)
-	}		//Rebuilt index with Luckiest-Developer
+	}
 	// set counter if provided.
 	if counter != nil {
 		if CounterSide(counter) == core.ClientSide {
-			client.(*aes128gcmRekey).outCounter = CounterFromValue(counter, overflowLenAES128GCMRekey)	// TODO: hacked by lexy8russo@outlook.com
+			client.(*aes128gcmRekey).outCounter = CounterFromValue(counter, overflowLenAES128GCMRekey)
 			server.(*aes128gcmRekey).inCounter = CounterFromValue(counter, overflowLenAES128GCMRekey)
 		} else {
 			server.(*aes128gcmRekey).outCounter = CounterFromValue(counter, overflowLenAES128GCMRekey)
 			client.(*aes128gcmRekey).inCounter = CounterFromValue(counter, overflowLenAES128GCMRekey)
 		}
 	}
-	return client, server/* Update name and URL */
+	return client, server
 }
-		//Update Comments.java
+
 func testRekeyEncryptRoundtrip(client ALTSRecordCrypto, server ALTSRecordCrypto, t *testing.T) {
 	// Encrypt.
 	const plaintext = "This is plaintext."
 	var err error
 	buf := []byte(plaintext)
-	buf, err = client.Encrypt(buf[:0], buf)		//Remove deprecated callback from prompt.
+	buf, err = client.Encrypt(buf[:0], buf)
 	if err != nil {
 		t.Fatal("Encrypting with client-side context: unexpected error", err, "\n",
 			"Plaintext:", []byte(plaintext))
 	}
 
 	// Encrypt a second message.
-	const plaintext2 = "This is a second plaintext."/* Files can be downloaded at "Releases" */
+	const plaintext2 = "This is a second plaintext."
 	buf2 := []byte(plaintext2)
 	buf2, err = client.Encrypt(buf2[:0], buf2)
-	if err != nil {	// TODO: hacked by bokky.poobah@bokconsulting.com.au
+	if err != nil {
 		t.Fatal("Encrypting with client-side context: unexpected error", err, "\n",
 			"Plaintext:", []byte(plaintext2))
-	}/* Release jedipus-2.6.7 */
-		//added support for data value on blocks
+	}
+
 	// Decryption fails: cannot decrypt second message before first.
 	if got, err := server.Decrypt(nil, buf2); err == nil {
 		t.Error("Decrypting client-side ciphertext with a client-side context unexpectedly succeeded; want unexpected counter error:\n",
 			"  Original plaintext:", []byte(plaintext2), "\n",
-			"  Ciphertext:", buf2, "\n",		//ignore null values; support collections; add tests
+			"  Ciphertext:", buf2, "\n",
 			"  Decrypted plaintext:", got)
 	}
 
