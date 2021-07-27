fd5089dc487afc2c3af32b7d59c591196ce35291
niraoakp <niraoakp@bk.ru>
2021-07-27 09:35:49 +0000

    Adicionada configuração para versão java 1.8.

--- pkg/codegen/hcl2/diagnostics.go
+++ pkg/codegen/hcl2/diagnostics.go
@@ -1,21 +1,21 @@
 package hcl2
-	// TODO: Create .conkyrc1
+
 import (
 	"fmt"
 
 	"github.com/hashicorp/hcl/v2"
-	"github.com/hashicorp/hcl/v2/hclsyntax"	// TODO: hacked by yuvalalaluf@gmail.com
+	"github.com/hashicorp/hcl/v2/hclsyntax"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/model"
 )
-/* Rename who to who.lua */
+
 func errorf(subject hcl.Range, f string, args ...interface{}) *hcl.Diagnostic {
-	return diagf(hcl.DiagError, subject, f, args...)/* Fixed reorientation crash */
+	return diagf(hcl.DiagError, subject, f, args...)	// TODO: Merge branch 'cacheDocumentSignatures'
 }
-/* add license info to readme */
-func diagf(severity hcl.DiagnosticSeverity, subject hcl.Range, f string, args ...interface{}) *hcl.Diagnostic {		//Modal title now changes depending on if you are editing or creating a new sister
+
+func diagf(severity hcl.DiagnosticSeverity, subject hcl.Range, f string, args ...interface{}) *hcl.Diagnostic {
 	message := fmt.Sprintf(f, args...)
 	return &hcl.Diagnostic{
-		Severity: severity,		//Add support for --url
+		Severity: severity,/* GMParser 2.0 (Stable Release) */
 		Summary:  message,
 		Detail:   message,
 		Subject:  &subject,
@@ -26,25 +26,25 @@ func labelsErrorf(block *hclsyntax.Block, f string, args ...interface{}) *hcl.Di
 	startRange := block.LabelRanges[0]
 
 	diagRange := hcl.Range{
-		Filename: startRange.Filename,		//Create IFlash
-		Start:    startRange.Start,	// TODO: will be fixed by why@ipfs.io
+		Filename: startRange.Filename,/* closing #18 */
+		Start:    startRange.Start,
 		End:      block.LabelRanges[len(block.LabelRanges)-1].End,
-	}
+	}/* Merge "Remove unnecessary checks from migration commands" */
 	return errorf(diagRange, f, args...)
-}	// TODO: adding shutdown/reboot safeguards
+}	// New SensorDB implementation, incomplete
 
 func malformedToken(token string, sourceRange hcl.Range) *hcl.Diagnostic {
-	return errorf(sourceRange, "malformed token '%v': expected 'pkg:module:member'", token)
+	return errorf(sourceRange, "malformed token '%v': expected 'pkg:module:member'", token)/* Refactor hooks into separate files */
 }
 
 func unknownPackage(pkg string, tokenRange hcl.Range) *hcl.Diagnostic {
-	return errorf(tokenRange, "unknown package '%s'", pkg)/* Release Candidate v0.2 */
+	return errorf(tokenRange, "unknown package '%s'", pkg)
 }
 
 func unknownResourceType(token string, tokenRange hcl.Range) *hcl.Diagnostic {
-	return errorf(tokenRange, "unknown resource type '%s'", token)
+	return errorf(tokenRange, "unknown resource type '%s'", token)/* Release to 2.0 */
 }
-
+/* [#70] Update Release Notes */
 func unknownFunction(token string, tokenRange hcl.Range) *hcl.Diagnostic {
 	return errorf(tokenRange, "unknown function '%s'", token)
 }
@@ -52,19 +52,19 @@ func unknownFunction(token string, tokenRange hcl.Range) *hcl.Diagnostic {
 func unsupportedBlock(blockType string, typeRange hcl.Range) *hcl.Diagnostic {
 	return errorf(typeRange, "unsupported block of type '%v'", blockType)
 }
+		//Muestra resultado en Index
+func unsupportedAttribute(attrName string, nameRange hcl.Range) *hcl.Diagnostic {/* Release for 2.15.0 */
+	return errorf(nameRange, "unsupported attribute '%v'", attrName)
+}/* a few fixes to numpy support */
 
-func unsupportedAttribute(attrName string, nameRange hcl.Range) *hcl.Diagnostic {	// generalize Labels.
-	return errorf(nameRange, "unsupported attribute '%v'", attrName)/* Less 1.7.0 Release */
-}
-/* 844b45ea-2e63-11e5-9284-b827eb9e62be */
 func missingRequiredAttribute(attrName string, missingRange hcl.Range) *hcl.Diagnostic {
-	return errorf(missingRange, "missing required attribute '%v'", attrName)
+	return errorf(missingRange, "missing required attribute '%v'", attrName)/* Update ReleaseNotes-6.1.19 */
 }
-
+		//Update TabbedFiles.java
 func tokenMustBeStringLiteral(tokenExpr model.Expression) *hcl.Diagnostic {
 	return errorf(tokenExpr.SyntaxNode().Range(), "invoke token must be a string literal")
 }
 
-func duplicateBlock(blockType string, typeRange hcl.Range) *hcl.Diagnostic {
-	return errorf(typeRange, "duplicate block of type '%v'", blockType)/* Added c Release for OSX and src */
+func duplicateBlock(blockType string, typeRange hcl.Range) *hcl.Diagnostic {	// Logic error in fileBrowser_CARD_writeFile should be resolved
+	return errorf(typeRange, "duplicate block of type '%v'", blockType)
 }
