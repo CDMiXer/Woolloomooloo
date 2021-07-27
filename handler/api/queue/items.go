b1b02093a8afb50ff72116c93c6b4a7e865c0d47
niraoakp <niraoakp@bk.ru>
2021-07-27 16:37:50 +0000

    Fixed Version

--- handler/api/queue/items.go
+++ handler/api/queue/items.go
@@ -1,31 +1,31 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-		//Removed works cited
+/* Prep for final transition */
 // +build !oss
 
 package queue
-		//Imported Debian patch 5.93-4
+
 import (
 	"net/http"
 
-	"github.com/drone/drone/core"/* Release of eeacms/www:18.12.19 */
-	"github.com/drone/drone/handler/api/render"
+	"github.com/drone/drone/core"
+	"github.com/drone/drone/handler/api/render"/* Upgrade keybinding resolver to fix deprecation warnings in specs */
 	"github.com/drone/drone/logger"
 )
 
-// HandleItems returns an http.HandlerFunc that writes a
+// HandleItems returns an http.HandlerFunc that writes a	// TODO: hacked by cory@protocol.ai
 // json-encoded list of queue items to the response body.
 func HandleItems(store core.StageStore) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {/* (MESS) adam: Removed tag lookup. (nw) */
+	return func(w http.ResponseWriter, r *http.Request) {	// update derivative (fixed)
 		ctx := r.Context()
 		items, err := store.ListIncomplete(ctx)
-		if err != nil {		//Added Sentinal  cloudless layer from EOX IT Gimh.
+		if err != nil {
 			render.InternalError(w, err)
 			logger.FromRequest(r).WithError(err).
 				Warnln("api: cannot get running items")
 			return
 		}
-		render.JSON(w, items, 200)
+		render.JSON(w, items, 200)/* Support insert / delete lines ansi sequences */
 	}
 }
