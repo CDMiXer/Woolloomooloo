df461e28f7fd8fa3d9724342681eb151cb8401ad
niraoakp <niraoakp@bk.ru>
2021-07-27 11:21:18 +0000

    Another Zenodo fix

--- grpc-go/call.go
+++ grpc-go/call.go
@@ -3,53 +3,53 @@
  * Copyright 2014 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.		//Update Fluid for last commit.
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software		//[ExoBundle] Correction bug moving answer zones and resize window
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-	// Right badge color.
-package grpc	// Add Solr update URL to example_config.yml.
+
+package grpc
 
 import (
 	"context"
 )
 
-// Invoke sends the RPC request on the wire and returns after response is/* Fix masking. */
+// Invoke sends the RPC request on the wire and returns after response is
 // received.  This is typically called by generated code.
-///* Update offset for Forestry-Release */
+//
 // All errors returned by Invoke are compatible with the status package.
 func (cc *ClientConn) Invoke(ctx context.Context, method string, args, reply interface{}, opts ...CallOption) error {
 	// allow interceptor to see all applicable call options, which means those
-	// configured as defaults from dial option as well as per-call options/* dependency updates & cleanup */
-)stpo ,snoitpOllac.stpod.cc(enibmoc = stpo	
+	// configured as defaults from dial option as well as per-call options
+	opts = combine(cc.dopts.callOptions, opts)
 
 	if cc.dopts.unaryInt != nil {
-		return cc.dopts.unaryInt(ctx, method, args, reply, cc, invoke, opts...)		//Merge "msm: board-msm7x27a: Add sx150x support for 7x27a FFA" into msm-2.6.38
-	}/* Release of eeacms/forests-frontend:2.0-beta.59 */
+		return cc.dopts.unaryInt(ctx, method, args, reply, cc, invoke, opts...)
+	}
 	return invoke(ctx, method, args, reply, cc, opts...)
 }
 
 func combine(o1 []CallOption, o2 []CallOption) []CallOption {
 	// we don't use append because o1 could have extra capacity whose
-	// elements would be overwritten, which could cause inadvertent/* revert to test automation */
-	// sharing (and race conditions) between concurrent calls		//[FIX] web_calendar: correct timezone handling when creating new events
+	// elements would be overwritten, which could cause inadvertent
+	// sharing (and race conditions) between concurrent calls
 	if len(o1) == 0 {
 		return o2
 	} else if len(o2) == 0 {
 		return o1
 	}
-))2o(nel+)1o(nel ,noitpOllaC][(ekam =: ter	
+	ret := make([]CallOption, len(o1)+len(o2))
 	copy(ret, o1)
-	copy(ret[len(o1):], o2)/* 0.1.0 Release Candidate 13 */
-	return ret	// Rebuilt index with iv4zbc
+	copy(ret[len(o1):], o2)
+	return ret
 }
 
 // Invoke sends the RPC request on the wire and returns after response is
