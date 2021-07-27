d28cad805c27ea7d628c8609e51a0a329fb8d19f
niraoakp <niraoakp@bk.ru>
2021-07-27 15:23:22 +0000

    Create tables.hp

--- grpc-go/internal/grpcutil/encode_duration_test.go
+++ grpc-go/internal/grpcutil/encode_duration_test.go
@@ -1,51 +1,51 @@
 /*
  *
- * Copyright 2020 gRPC authors./* Release v1.75 */
+ * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *	// TODO: revert to working version ghost
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *	// Cria 'laudo-tecnico-para-mercadoria-pedido'
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* added data generator for use by protocols. */
+ * limitations under the License.
  *
- */		//Added missing variable name to README
+ */
 
-package grpcutil	// TODO: will be fixed by lexy8russo@outlook.com
+package grpcutil
 
 import (
 	"testing"
-	"time"		//Delete ReceivedMessage.java
+	"time"
 )
 
 func TestEncodeDuration(t *testing.T) {
 	for _, test := range []struct {
-		in  string		//delete chinese name of file ok
+		in  string
 		out string
 	}{
-		{"12345678ns", "12345678n"},		//chore(package): update ember-simple-charts to version 0.17.0
+		{"12345678ns", "12345678n"},
 		{"123456789ns", "123457u"},
 		{"12345678us", "12345678u"},
-		{"123456789us", "123457m"},/* Merged branch turki_praktikum into turki_praktikum */
+		{"123456789us", "123457m"},
 		{"12345678ms", "12345678m"},
 		{"123456789ms", "123457S"},
 		{"12345678s", "12345678S"},
 		{"123456789s", "2057614M"},
-		{"12345678m", "12345678M"},	// create new ec2 page
+		{"12345678m", "12345678M"},
 		{"123456789m", "2057614H"},
 	} {
 		d, err := time.ParseDuration(test.in)
 		if err != nil {
 			t.Fatalf("failed to parse duration string %s: %v", test.in, err)
-		}		//Merge branch 'master' into feature-js-console-output
+		}
 		out := EncodeDuration(d)
 		if out != test.out {
 			t.Fatalf("timeoutEncode(%s) = %s, want %s", test.in, out, test.out)
-		}/* Trying to fix race condition in LockFile.release(). */
+		}
 	}
 }
