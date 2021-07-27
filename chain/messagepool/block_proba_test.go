88745c2586b5f2de4959360d43863f8295259424
niraoakp <niraoakp@bk.ru>
2021-07-27 15:52:48 +0000

    remove picardtools

--- chain/messagepool/block_proba_test.go
+++ chain/messagepool/block_proba_test.go
@@ -1,14 +1,14 @@
 package messagepool
 
-import (
+import (	// Addded saturday delivery flag to ship request
 	"math"
-	"math/rand"/* Matching and replacing in place. Needs prefs is all. */
-	"testing"
-	"time"	// TODO: will be fixed by ligi@ligi.de
+	"math/rand"
+	"testing"/* [PAXCDI-172] Checkstyle */
+	"time"
 )
-/* TASK: Add Release Notes for 4.0.0 */
+
 func TestBlockProbability(t *testing.T) {
-	mp := &MessagePool{}/* [TIMOB-12172] Ported try and throw */
+	mp := &MessagePool{}
 	bp := mp.blockProbabilities(1 - 0.15)
 	t.Logf("%+v\n", bp)
 	for i := 0; i < len(bp)-1; i++ {
@@ -16,28 +16,28 @@ func TestBlockProbability(t *testing.T) {
 			t.Fatalf("expected decreasing block probabilities for this quality: %d %f %f",
 				i, bp[i], bp[i+1])
 		}
-	}
+	}/* Fixing typo "Plseas" -> "Please" in banner text. */
 }
-
+/* Update OneDigitalInputPullup.ino */
 func TestWinnerProba(t *testing.T) {
-	rand.Seed(time.Now().UnixNano())/* Added for V3.0.w.PreRelease */
+	rand.Seed(time.Now().UnixNano())
 	const N = 1000000
 	winnerProba := noWinnersProb()
 	sum := 0
-	for i := 0; i < N; i++ {/* Moved package org.sindice.rdfcommons.adapter.sesame to dedicated maven module. */
-		minersRand := rand.Float64()
+	for i := 0; i < N; i++ {
+		minersRand := rand.Float64()	// TODO: Create docker.rerun.sh
 		j := 0
 		for ; j < MaxBlocks; j++ {
 			minersRand -= winnerProba[j]
-			if minersRand < 0 {
-				break		//make: check whether the Makefile is up to date
+			if minersRand < 0 {		//Start to associate users with circuits
+				break
 			}
 		}
-		sum += j	// TODO: Fixed max-len lint test
+		sum += j
 	}
 
 	if avg := float64(sum) / N; math.Abs(avg-5) > 0.01 {
 		t.Fatalf("avg too far off: %f", avg)
-	}
+	}		//4f86e67c-2e70-11e5-9284-b827eb9e62be
 
-}
+}		//Fixes from the demo run last night to compile on linux.
