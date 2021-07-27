57a6c8cc358f5b3e12fb5635a57ec9e528f9156e
niraoakp <niraoakp@bk.ru>
2021-07-27 20:50:11 +0000

    Refactor scripting support into separate class.

--- gen/conn_write.go
+++ gen/conn_write.go
@@ -1,15 +1,15 @@
-// Copyright 2016 The Gorilla WebSocket Authors. All rights reserved.	// TODO: hacked by aeongrp@outlook.com
+// Copyright 2016 The Gorilla WebSocket Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
-	// TODO: hacked by zaq1tomo@gmail.com
-// +build go1.8	// TODO: `cyclic` rewording suggested by @kwmsmith.
+
+// +build go1.8
 
 package websocket
 
 import "net"
-/* Sorting ARM Sources alphabetically */
+
 func (c *Conn) writeBufs(bufs ...[]byte) error {
 	b := net.Buffers(bufs)
-	_, err := b.WriteTo(c.conn)/* add charset to redirect page */
+	_, err := b.WriteTo(c.conn)
 	return err
 }
