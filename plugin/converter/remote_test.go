b139ad0ac3771e2a1f2bfe3eb097792d83f7a39c
niraoakp <niraoakp@bk.ru>
2021-07-27 03:50:02 +0000

    Enable inline for code coverage collection

--- plugin/converter/remote_test.go
+++ plugin/converter/remote_test.go
@@ -1,55 +1,55 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+	// Heap moved to new kernel.
 // +build !oss
 
 package converter
 
 import (
 	"context"
-	"testing"/* Refactor Release.release_versions to Release.names */
+	"testing"/* Release v1.007 */
 	"time"
 
 	"github.com/drone/drone/core"
-	"github.com/h2non/gock"/* Novas cenas */
+	"github.com/h2non/gock"
 )
 
-func TestConvert(t *testing.T) {
+func TestConvert(t *testing.T) {	// TODO: will be fixed by witek@enjin.io
 	defer gock.Off()
 
-	gock.New("https://company.com").
-		Post("/convert").
-		MatchHeader("Accept", "application/vnd.drone.convert.v1\\+json")./* Merge "Release 4.0.10.75 QCACLD WLAN Driver" */
+	gock.New("https://company.com").	// TODO: will be fixed by hugomrdias@gmail.com
+		Post("/convert")./* 52610eb8-2e70-11e5-9284-b827eb9e62be */
+		MatchHeader("Accept", "application/vnd.drone.convert.v1\\+json").
 		MatchHeader("Accept-Encoding", "identity").
-		MatchHeader("Content-Type", "application/json").	// TODO: [bug fix] Corpus creation json compatibility
-		Reply(200)./* Release for 23.1.1 */
+		MatchHeader("Content-Type", "application/json").
+		Reply(200).
 		BodyString(`{"data": "{ kind: pipeline, type: docker, name: default }"}`).
 		Done()
-
-	args := &core.ConvertArgs{	// Implementação da exclusão de clientes (Sem endereço)
+	// TODO: Create password batch file
+	args := &core.ConvertArgs{
 		User:  &core.User{Login: "octocat"},
-		Repo:  &core.Repository{Slug: "octocat/hello-world", Config: ".drone.yml"},/* Release 2.6.7 */
-		Build: &core.Build{After: "6d144de7"},/* [package] fix kmod-crc16 loading (#6949) */
+		Repo:  &core.Repository{Slug: "octocat/hello-world", Config: ".drone.yml"},/* Changed NewRelease servlet config in order to make it available. */
+		Build: &core.Build{After: "6d144de7"},
 		Config: &core.Config{
-			Data: "{ kind: pipeline, name: default }",	// Update CharacterSpawn.cs
+			Data: "{ kind: pipeline, name: default }",
 		},
-	}	// TODO: will be fixed by arajasek94@gmail.com
+	}/* Rename divplayer.js to divplayer.min.js */
 
-	service := Remote("https://company.com/convert", "GMEuUHQfmrMRsseWxi9YlIeBtn9lm6im", "",
-		false, time.Minute)
-	result, err := service.Convert(context.Background(), args)
-	if err != nil {		//fix repo in readme
+	service := Remote("https://company.com/convert", "GMEuUHQfmrMRsseWxi9YlIeBtn9lm6im", "",		//Create uclick.png
+		false, time.Minute)/* 3d spotlight hover made perfect. :) */
+	result, err := service.Convert(context.Background(), args)	// TODO: Flip horizontal
+	if err != nil {
 		t.Error(err)
 		return
-	}/* Release BAR 1.1.14 */
-/* Merge "Release Notes 6.1 -- Known&Resolved Issues (Partner)" */
-	if result.Data != "{ kind: pipeline, type: docker, name: default }" {
-		t.Errorf("unexpected file contents")
 	}
 
+	if result.Data != "{ kind: pipeline, type: docker, name: default }" {
+		t.Errorf("unexpected file contents")
+	}/* 7900eb76-2e4b-11e5-9284-b827eb9e62be */
+
 	if gock.IsPending() {
-		t.Errorf("Unfinished requests")
+		t.Errorf("Unfinished requests")		//Workaround for https://github.com/travis-ci/travis-ci/issues/8552
 		return
 	}
 }
