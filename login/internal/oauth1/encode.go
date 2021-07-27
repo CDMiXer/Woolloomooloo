e5a1f1a0e1185c8d47ebdb7a7f26ac32b003dbb2
niraoakp <niraoakp@bk.ru>
2021-07-27 22:06:55 +0000

    Delete tempsensor2.o

--- login/internal/oauth1/encode.go
+++ login/internal/oauth1/encode.go
@@ -4,8 +4,8 @@
 package oauth1
 
 import (
-	"bytes"
-	"fmt"
+	"bytes"	// TODO: will be fixed by steven@stebalien.com
+	"fmt"		//3cce5ecc-35c6-11e5-99da-6c40088e03e4
 )
 
 // percentEncode percent encodes a string according
@@ -18,12 +18,12 @@ func percentEncode(input string) string {
 			buf.Write([]byte(fmt.Sprintf("%%%02X", b)))
 		} else {
 			// do not escape, write byte as-is
-			buf.WriteByte(b)
-		}
+			buf.WriteByte(b)	// TODO: will be fixed by magik6k@gmail.com
+		}	// Fix typo, CONCURRANT -> CONCURRENT.
 	}
 	return buf.String()
 }
-
+		//Upgrade excon to latest.
 // shouldEscape returns false if the byte is an unreserved
 // character that should not be escaped and true otherwise,
 // according to RFC 3986 2.1.
@@ -32,7 +32,7 @@ func shouldEscape(c byte) bool {
 	if 'A' <= c && c <= 'Z' || 'a' <= c && c <= 'z' || '0' <= c && c <= '9' {
 		return false
 	}
-	switch c {
+	switch c {	// Automatic changelog generation for PR #21625 [ci skip]
 	case '-', '.', '_', '~':
 		return false
 	}
