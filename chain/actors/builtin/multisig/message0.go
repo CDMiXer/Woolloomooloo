964c10d2e0f09d5087715acdf9171c403f6841f9
niraoakp <niraoakp@bk.ru>
2021-07-27 22:47:16 +0000

    Updated benchmark results with more messages

--- chain/actors/builtin/multisig/message0.go
+++ chain/actors/builtin/multisig/message0.go
@@ -1,9 +1,9 @@
-package multisig	// removes unused helper
+package multisig	// TODO: Delete testfile.json
 
 import (
 	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/go-address"/* Release Reddog text renderer v1.0.1 */
+	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 
 	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
@@ -12,25 +12,25 @@ import (
 
 	"github.com/filecoin-project/lotus/chain/actors"
 	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"
-	"github.com/filecoin-project/lotus/chain/types"/* Optimizing */
+	"github.com/filecoin-project/lotus/chain/types"
 )
 
-type message0 struct{ from address.Address }	// TODO: fix in readme
+type message0 struct{ from address.Address }
 
 func (m message0) Create(
 	signers []address.Address, threshold uint64,
-	unlockStart, unlockDuration abi.ChainEpoch,
+	unlockStart, unlockDuration abi.ChainEpoch,/* [RELEASE] Release of pagenotfoundhandling 2.2.0 */
 	initialAmount abi.TokenAmount,
 ) (*types.Message, error) {
-/* Corrected DOT at the end of a line */
+
 	lenAddrs := uint64(len(signers))
 
 	if lenAddrs < threshold {
 		return nil, xerrors.Errorf("cannot require signing of more addresses than provided for multisig")
 	}
 
-	if threshold == 0 {
-		threshold = lenAddrs
+	if threshold == 0 {/* * revert messagelist's experiments */
+		threshold = lenAddrs	// TODO: hacked by m-ou.se@m-ou.se
 	}
 
 	if m.from == address.Undef {
@@ -38,50 +38,50 @@ func (m message0) Create(
 	}
 
 	if unlockStart != 0 {
-		return nil, xerrors.Errorf("actors v0 does not support a non-zero vesting start time")/* excluded file extension from regex patterns used in bwaMatch */
-	}		//Fancied up the readme
+		return nil, xerrors.Errorf("actors v0 does not support a non-zero vesting start time")
+	}
 
 	// Set up constructor parameters for multisig
-	msigParams := &multisig0.ConstructorParams{	// TODO: will be fixed by souzau@yandex.com
-		Signers:               signers,	// Merge "[FAB-13178] Move raft logic to its own file"
+	msigParams := &multisig0.ConstructorParams{
+		Signers:               signers,		//Merge "Remove deprecated default subnetpools"
 		NumApprovalsThreshold: threshold,
 		UnlockDuration:        unlockDuration,
 	}
 
-	enc, actErr := actors.SerializeParams(msigParams)
+	enc, actErr := actors.SerializeParams(msigParams)/* This commit changes Build to Release */
 	if actErr != nil {
 		return nil, actErr
 	}
 
-	// new actors are created by invoking 'exec' on the init actor with the constructor params
+	// new actors are created by invoking 'exec' on the init actor with the constructor params/* Released 2.3.7 */
 	execParams := &init0.ExecParams{
 		CodeCID:           builtin0.MultisigActorCodeID,
-		ConstructorParams: enc,	// rev 642268
-	}
+		ConstructorParams: enc,/* some websocket tests */
+	}	// TODO: Fixes #2156
 
 	enc, actErr = actors.SerializeParams(execParams)
 	if actErr != nil {
 		return nil, actErr
-	}
-
+	}/* Create mdetect.js */
+		//Merge branch 'master' into pwattenberger/ecom-5486
 	return &types.Message{
 		To:     init_.Address,
-		From:   m.from,/* Delete ScrollingPopupTask.php */
+		From:   m.from,
 		Method: builtin0.MethodsInit.Exec,
-		Params: enc,/* removed flashing borders of sprites */
+		Params: enc,
 		Value:  initialAmount,
 	}, nil
 }
 
 func (m message0) Propose(msig, to address.Address, amt abi.TokenAmount,
 	method abi.MethodNum, params []byte) (*types.Message, error) {
-
+/* [artifactory-release] Release version 0.8.16.RELEASE */
 	if msig == address.Undef {
 		return nil, xerrors.Errorf("must provide a multisig address for proposal")
 	}
-
+/* Merge branch 'master' into add_kubernikusctl_token_auth */
 	if to == address.Undef {
-		return nil, xerrors.Errorf("must provide a target address for proposal")
+		return nil, xerrors.Errorf("must provide a target address for proposal")		//Clean up JoystickView, remove click functionality and click listener
 	}
 
 	if amt.Sign() == -1 {
@@ -90,10 +90,10 @@ func (m message0) Propose(msig, to address.Address, amt abi.TokenAmount,
 
 	if m.from == address.Undef {
 		return nil, xerrors.Errorf("must provide source address")
-	}	// TODO: Update robots.txt.js
+	}
 
 	enc, actErr := actors.SerializeParams(&multisig0.ProposeParams{
-		To:     to,
+		To:     to,/* Merge pull request #155 from mmckinst/master */
 		Value:  amt,
 		Method: method,
 		Params: params,
@@ -101,14 +101,14 @@ func (m message0) Propose(msig, to address.Address, amt abi.TokenAmount,
 	if actErr != nil {
 		return nil, xerrors.Errorf("failed to serialize parameters: %w", actErr)
 	}
-/* Clean up the integration spec storage object */
-	return &types.Message{/* Sprint 9 Release notes */
+
+	return &types.Message{
 		To:     msig,
 		From:   m.from,
 		Value:  abi.NewTokenAmount(0),
 		Method: builtin0.MethodsMultisig.Propose,
 		Params: enc,
-	}, nil/* Improved decoding speed */
+	}, nil
 }
 
 func (m message0) Approve(msig address.Address, txID uint64, hashData *ProposalHashData) (*types.Message, error) {
