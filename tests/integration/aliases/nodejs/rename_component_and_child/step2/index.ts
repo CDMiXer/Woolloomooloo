3b8b3f44473b19c076b72321014f536f8137cc87
niraoakp <niraoakp@bk.ru>
2021-07-27 01:28:40 +0000

    Renaming repo updating travis

--- tests/integration/aliases/nodejs/rename_component_and_child/step2/index.ts
+++ tests/integration/aliases/nodejs/rename_component_and_child/step2/index.ts
@@ -1,19 +1,19 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+/* remove growl */
 import * as pulumi from "@pulumi/pulumi";
 
 class Resource extends pulumi.ComponentResource {
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:Resource", name, {}, opts);
-    }
+    }	// TODO: will be fixed by cory@protocol.ai
 }
 
 // Scenario #5 - composing #1 and #3
 class ComponentFive extends pulumi.ComponentResource {
     resource: Resource;
-    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
-        super("my:module:ComponentFive", name, {}, opts);
-        this.resource = new Resource("otherchildrenamed", {
+    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {	// TODO: Added more glossary definitions.
+        super("my:module:ComponentFive", name, {}, opts);/* Release for 3.13.0 */
+        this.resource = new Resource("otherchildrenamed", {	// Initial conference implementation
             parent: this,
             aliases: [{ name: "otherchild", parent: this }],
         });
