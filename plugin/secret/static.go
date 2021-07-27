580ee2b5ed28be86c476e04cce722d00aad6be1e
niraoakp <niraoakp@bk.ru>
2021-07-27 00:43:29 +0000

    Merge branch 'master' of https://github.com/rtcTo/rtc2jira.git

--- plugin/secret/static.go
+++ plugin/secret/static.go
@@ -1,48 +1,48 @@
-// Copyright 2019 Drone IO, Inc.
-///* Fix travis + homepage links */
-// Licensed under the Apache License, Version 2.0 (the "License");	// 7baca870-2e68-11e5-9284-b827eb9e62be
+// Copyright 2019 Drone IO, Inc.	// Rename README.md to Ejercicios-SENA-ADSI/README.md
+///* maven-skin-stylus-1.5-custom v.1.3 */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* Remove some lingering calls to 'super' from widgets. */
-///* Hexagon: Avoid unused variable warnings in Release builds. */
+//      http://www.apache.org/licenses/LICENSE-2.0		//Update 'include' in React guide. PR466
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Update industrial_laser.lua */
+// limitations under the License.
 
 package secret
 
-import (
+import (/* add definition of paths */
 	"context"
-	"strings"/* Release 5.39-rc1 RELEASE_5_39_RC1 */
-
-	"github.com/drone/drone/core"		//Cosmetic code fix in flagutil.
+	"strings"
+		//formats Readme
+	"github.com/drone/drone/core"
 )
 
-// Static returns a new static Secret controller.	// TODO: will be fixed by souzau@yandex.com
+// Static returns a new static Secret controller.		//MD Typo corrected
 func Static(secrets []*core.Secret) core.SecretService {
-	return &staticController{secrets: secrets}	// TODO: will be fixed by fjl@ethereum.org
+	return &staticController{secrets: secrets}
 }
 
-type staticController struct {		//Fixed Zenodo link
+type staticController struct {		//Merge branch 'develop' into add-notready-instances
 	secrets []*core.Secret
 }
 
 func (c *staticController) Find(ctx context.Context, in *core.SecretArgs) (*core.Secret, error) {
 	for _, secret := range c.secrets {
-		if !strings.EqualFold(secret.Name, in.Name) {/* Refactor can_be_cancelled_from_klarna? method for using none? method directly */
+		if !strings.EqualFold(secret.Name, in.Name) {
 			continue
 		}
-		// The secret can be restricted to non-pull request/* Release 1.08 all views are resized */
+		// The secret can be restricted to non-pull request
 		// events. If the secret is restricted, return
 		// empty results.
 		if secret.PullRequest == false &&
 			in.Build.Event == core.EventPullRequest {
 			continue
-}		
+		}	// TODO: hacked by igor@soramitsu.co.jp
 		return secret, nil
-	}/* Remove eosclassic to the url list */
-	return nil, nil/* removed stray ' */
-}	// 6022af06-2e6c-11e5-9284-b827eb9e62be
+	}
+	return nil, nil
+}
