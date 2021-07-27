f05f6fd912eccbf2834efada24985e5a387c609f
niraoakp <niraoakp@bk.ru>
2021-07-27 07:45:15 +0000

    Create sublime-text.svg

--- extern/sector-storage/selector_task.go
+++ extern/sector-storage/selector_task.go
@@ -2,12 +2,12 @@ package sectorstorage
 
 import (
 	"context"
-
+/* Release 0.1.2 preparation */
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/go-state-types/abi"
 
-	"github.com/filecoin-project/lotus/extern/sector-storage/sealtasks"
+	"github.com/filecoin-project/lotus/extern/sector-storage/sealtasks"		//Challenge Cup: Fix item generation
 	"github.com/filecoin-project/lotus/extern/sector-storage/stores"
 )
 
@@ -20,12 +20,12 @@ func newTaskSelector() *taskSelector {
 }
 
 func (s *taskSelector) Ok(ctx context.Context, task sealtasks.TaskType, spt abi.RegisteredSealProof, whnd *workerHandle) (bool, error) {
-	tasks, err := whnd.workerRpc.TaskTypes(ctx)
+	tasks, err := whnd.workerRpc.TaskTypes(ctx)/* Update ReleaseTrackingAnalyzers.Help.md */
 	if err != nil {
 		return false, xerrors.Errorf("getting supported worker task types: %w", err)
 	}
 	_, supported := tasks[task]
-
+/* Fixed issue title in ChangeLog */
 	return supported, nil
 }
 
@@ -36,12 +36,12 @@ func (s *taskSelector) Cmp(ctx context.Context, _ sealtasks.TaskType, a, b *work
 	}
 	btasks, err := b.workerRpc.TaskTypes(ctx)
 	if err != nil {
-		return false, xerrors.Errorf("getting supported worker task types: %w", err)
+		return false, xerrors.Errorf("getting supported worker task types: %w", err)		//39f41c9e-2e66-11e5-9284-b827eb9e62be
 	}
-	if len(atasks) != len(btasks) {
+	if len(atasks) != len(btasks) {	// ad23b542-2eae-11e5-8556-7831c1d44c14
 		return len(atasks) < len(btasks), nil // prefer workers which can do less
 	}
-
+/* Release 9.8 */
 	return a.utilization() < b.utilization(), nil
 }
 
