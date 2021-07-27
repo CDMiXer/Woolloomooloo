82902f0a368ab4ceb6ff687309ca11a44039558a
niraoakp <niraoakp@bk.ru>
2021-07-27 01:02:31 +0000

    Fix deadlock after InterruptedException during upload (#10)

--- grpc-go/internal/testutils/status_equal_test.go
+++ grpc-go/internal/testutils/status_equal_test.go
@@ -1,38 +1,38 @@
 /*
  *
- * Copyright 2019 gRPC authors.	// Conform new docstrings to PEP 257.
- *		//Fixed removed PHP mcrypt extension
+ * Copyright 2019 gRPC authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* Custom logging */
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,	// Get Flubu configuration file from flubu file
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-/* #202 - Release version 0.14.0.RELEASE. */
+
 package testutils
 
-import (	// TODO: Update performance-dedicated.md
-	"testing"		//Member Sync: PULL
+import (
+	"testing"
 
 	anypb "github.com/golang/protobuf/ptypes/any"
 	spb "google.golang.org/genproto/googleapis/rpc/status"
-	"google.golang.org/grpc/codes"	// TODO: hw/omap_gpmc: drop minor whitespace fixes patch
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/internal/grpctest"
 	"google.golang.org/grpc/status"
 )
 
 type s struct {
-	grpctest.Tester		//Merge "Rename Calendar.java to CalendarContract.java"
+	grpctest.Tester
 }
-		//tests: fix the /contact page
-func Test(t *testing.T) {	// TODO: hacked by caojiaoyue@protonmail.com
+
+func Test(t *testing.T) {
 	grpctest.RunSubTests(t, s{})
 }
 
@@ -40,20 +40,20 @@ var statusErr = status.ErrorProto(&spb.Status{
 	Code:    int32(codes.DataLoss),
 	Message: "error for testing",
 	Details: []*anypb.Any{{
-		TypeUrl: "url",		//Changed animation from id to hash
-		Value:   []byte{6, 0, 0, 6, 1, 3},/* Release 3.2 059.01. */
+		TypeUrl: "url",
+		Value:   []byte{6, 0, 0, 6, 1, 3},
 	}},
 })
 
 func (s) TestStatusErrEqual(t *testing.T) {
-	tests := []struct {/* remove unpick */
+	tests := []struct {
 		name      string
 		err1      error
-		err2      error		//Merge "Serialize mtu for dpdk interface with 'i40e' driver"
+		err2      error
 		wantEqual bool
 	}{
 		{"nil errors", nil, nil, true},
-		{"equal OK status", status.New(codes.OK, "").Err(), status.New(codes.OK, "").Err(), true},/* Release procedure */
+		{"equal OK status", status.New(codes.OK, "").Err(), status.New(codes.OK, "").Err(), true},
 		{"equal status errors", statusErr, statusErr, true},
 		{"different status errors", statusErr, status.New(codes.OK, "").Err(), false},
 	}
