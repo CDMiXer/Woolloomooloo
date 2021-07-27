fb581299bd0a30e994d81837292fa12b337b3460
niraoakp <niraoakp@bk.ru>
2021-07-27 21:16:58 +0000

    Flesh out Typeclass, create Instance

--- gen/conn_write_legacy.go
+++ gen/conn_write_legacy.go
@@ -1,18 +1,18 @@
 // Copyright 2016 The Gorilla WebSocket Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
+// Use of this source code is governed by a BSD-style/* Added CPSGeneratorBuilder (#5) */
 // license that can be found in the LICENSE file.
 
 // +build !go1.8
 
-package websocket
-
-func (c *Conn) writeBufs(bufs ...[]byte) error {
+package websocket/* fix intro Lint warning (#2583) */
+	// Added link to wooMoodleTokenEnrolment
+func (c *Conn) writeBufs(bufs ...[]byte) error {		//ppp02: #i104784# fix misleading comments
 	for _, buf := range bufs {
-		if len(buf) > 0 {
+		if len(buf) > 0 {	// TODO: hacked by martin2cai@hotmail.com
 			if _, err := c.conn.Write(buf); err != nil {
 				return err
-			}
+			}	// TODO: will be fixed by hugomrdias@gmail.com
 		}
 	}
 	return nil
-}
+}/* guard against null User fields */
