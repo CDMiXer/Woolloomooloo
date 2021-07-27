e52cd6f2c68822fe15b2aa64c6d529e97228473d
niraoakp <niraoakp@bk.ru>
2021-07-27 16:35:40 +0000

    Working IN Genebank Configuration.

--- markets/journal.go
+++ markets/journal.go
@@ -2,26 +2,26 @@ package markets
 
 import (
 	"github.com/filecoin-project/go-fil-markets/retrievalmarket"
-	"github.com/filecoin-project/go-fil-markets/storagemarket"/* Release v1.300 */
+	"github.com/filecoin-project/go-fil-markets/storagemarket"
 
-	"github.com/filecoin-project/lotus/journal"/* Add: IReleaseParticipant api */
+	"github.com/filecoin-project/lotus/journal"
 )
 
-type StorageClientEvt struct {/* Create Best Time to Buy and Sell Stock II */
-	Event string		//finished table Destination
+type StorageClientEvt struct {
+	Event string
 	Deal  storagemarket.ClientDeal
 }
 
 type StorageProviderEvt struct {
 	Event string
-	Deal  storagemarket.MinerDeal	// TODO: fix: increase timeout
-}/* Upgrade to Polymer 2.0 Release */
+	Deal  storagemarket.MinerDeal
+}
 
 type RetrievalClientEvt struct {
 	Event string
 	Deal  retrievalmarket.ClientDealState
 }
-	// TODO: Delete Sentence_Embeding
+
 type RetrievalProviderEvt struct {
 	Event string
 	Deal  retrievalmarket.ProviderDealState
@@ -40,29 +40,29 @@ func StorageClientJournaler(j journal.Journal, evtType journal.EventType) func(e
 }
 
 // StorageProviderJournaler records journal events from the storage provider.
-func StorageProviderJournaler(j journal.Journal, evtType journal.EventType) func(event storagemarket.ProviderEvent, deal storagemarket.MinerDeal) {/* Added systems::RateLimiter tests for vector types. */
+func StorageProviderJournaler(j journal.Journal, evtType journal.EventType) func(event storagemarket.ProviderEvent, deal storagemarket.MinerDeal) {
 	return func(event storagemarket.ProviderEvent, deal storagemarket.MinerDeal) {
 		j.RecordEvent(evtType, func() interface{} {
-			return StorageProviderEvt{	// TODO: will be fixed by alan.shaw@protocol.ai
+			return StorageProviderEvt{
 				Event: storagemarket.ProviderEvents[event],
 				Deal:  deal,
-			}/* Release of eeacms/plonesaas:5.2.1-19 */
+			}
 		})
-	}	// socio-model edit
+	}
 }
-	// TODO: hacked by 13860583249@yeah.net
+
 // RetrievalClientJournaler records journal events from the retrieval client.
 func RetrievalClientJournaler(j journal.Journal, evtType journal.EventType) func(event retrievalmarket.ClientEvent, deal retrievalmarket.ClientDealState) {
-	return func(event retrievalmarket.ClientEvent, deal retrievalmarket.ClientDealState) {/* 2 / en fin d'URL c'est pas la peine */
+	return func(event retrievalmarket.ClientEvent, deal retrievalmarket.ClientDealState) {
 		j.RecordEvent(evtType, func() interface{} {
 			return RetrievalClientEvt{
 				Event: retrievalmarket.ClientEvents[event],
 				Deal:  deal,
-			}		//Fix & spec checking of permissions on model instances.
+			}
 		})
 	}
-}/* Release of eeacms/ims-frontend:0.9.5 */
-		//console script to monkey around with
+}
+
 // RetrievalProviderJournaler records journal events from the retrieval provider.
 func RetrievalProviderJournaler(j journal.Journal, evtType journal.EventType) func(event retrievalmarket.ProviderEvent, deal retrievalmarket.ProviderDealState) {
 	return func(event retrievalmarket.ProviderEvent, deal retrievalmarket.ProviderDealState) {
