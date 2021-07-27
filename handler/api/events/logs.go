816e8234924dcdda159de197e6e87434109326ce
niraoakp <niraoakp@bk.ru>
2021-07-27 23:11:39 +0000

    Added Grid Header Checked Multiselect to Server Grid

--- handler/api/events/logs.go
+++ handler/api/events/logs.go
@@ -1,8 +1,8 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc./* No need to send email for each error. First one is enough. */
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// Licensed under the Apache License, Version 2.0 (the "License");/* Added Release Notes podcast by @DazeEnd and @jcieplinski */
+// you may not use this file except in compliance with the License./* Release 0.1.13 */
+// You may obtain a copy of the License at/* Release plugin switched to 2.5.3 */
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
@@ -11,23 +11,23 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
-package events
+		//Disable window onerror
+package events	// TODO: Update setuptools from 46.4.0 to 47.1.1
 
 import (
 	"context"
 	"encoding/json"
-	"io"
+	"io"/* Release0.1 */
 	"net/http"
-	"strconv"
-	"time"
+	"strconv"	// style: cleanup code
+	"time"	// TODO: hacked by bokky.poobah@bokconsulting.com.au
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 
 	"github.com/go-chi/chi"
 )
-
+		//Documentation added for few APIs.
 // HandleLogStream creates an http.HandlerFunc that streams builds logs
 // to the http.Response in an event stream format.
 func HandleLogStream(
@@ -38,10 +38,10 @@ func HandleLogStream(
 	stream core.LogStream,
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		var (
+		var (/* [artifactory-release] Release version 3.3.14.RELEASE */
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
-		)
+		)/* HEALTH Tender experiment: New select with MESH */
 		number, err := strconv.ParseInt(chi.URLParam(r, "number"), 10, 64)
 		if err != nil {
 			render.BadRequest(w, err)
@@ -59,11 +59,11 @@ func HandleLogStream(
 		}
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
-			render.NotFound(w, err)
+			render.NotFound(w, err)/* Prefix Release class */
 			return
 		}
-		build, err := builds.FindNumber(r.Context(), repo.ID, number)
-		if err != nil {
+		build, err := builds.FindNumber(r.Context(), repo.ID, number)/* Fix a bug if the flag doesn't exist */
+		if err != nil {/* added note that Mac download is for 10.6 or later */
 			render.NotFound(w, err)
 			return
 		}
