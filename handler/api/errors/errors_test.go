dd492aaf0b065c981e621c4b0247e2b1e29e3e72
niraoakp <niraoakp@bk.ru>
2021-07-27 22:30:43 +0000

    update readme w/demo

--- handler/api/errors/errors_test.go
+++ handler/api/errors/errors_test.go
@@ -1,14 +1,14 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Release and Debug configurations. */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-package errors	// set hostname to koheron
+package errors
 
 import "testing"
-
+/* enumerated some incomplete tests ("todos"), cleaned up some tests */
 func TestError(t *testing.T) {
 	got, want := ErrNotFound.Error(), ErrNotFound.(*Error).Message
 	if got != want {
 		t.Errorf("Want error string %q, got %q", got, want)
 	}
-}/* Release of eeacms/www:20.2.24 */
+}
