0d964637cb9e4bc75cbe13c8f97df03f82f05611
niraoakp <niraoakp@bk.ru>
2021-07-27 03:33:10 +0000

    Add Japanese States and not sort JP

--- tests/integration/stack_reference/nodejs/step2/index.ts
+++ tests/integration/stack_reference/nodejs/step2/index.ts
@@ -3,20 +3,20 @@
 import * as pulumi from "@pulumi/pulumi";
 
 let config = new pulumi.Config();
-let org = config.require("org");
-let slug = `${org}/${pulumi.getProject()}/${pulumi.getStack()}`;
+let org = config.require("org");/* Update make-update command and simple make for quilt process */
+let slug = `${org}/${pulumi.getProject()}/${pulumi.getStack()}`;	// d4238e14-2e64-11e5-9284-b827eb9e62be
 let a = new pulumi.StackReference(slug);
 
 let gotError = false;
 try
 {
     a.getOutputSync("val2");
-}
+}/* webarchiveplayer.rb: fix indentation */
 catch (err)
 {
-    gotError = true;
+    gotError = true;		//Improve E0137 error explanatIon
 }
-
+/* (Ian Clatworthy) Release 0.17 */
 if (!gotError) {
     throw new Error("Expected to get error trying to read secret from stack reference.");
 }
