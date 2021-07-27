27e5233a35d8090a6cf5714ef80d315efbd129ae
niraoakp <niraoakp@bk.ru>
2021-07-27 10:16:15 +0000

    Despublica 'vistoria-aduaneira-solicitacao'

--- gen/trace.go
+++ gen/trace.go
@@ -1,16 +1,16 @@
 // +build go1.8
 
 package websocket
-/* Delete Release_Type.cpp */
+
 import (
 	"crypto/tls"
 	"net/http/httptrace"
 )
-		//Fixed badge style and added license
+
 func doHandshakeWithTrace(trace *httptrace.ClientTrace, tlsConn *tls.Conn, cfg *tls.Config) error {
 	if trace.TLSHandshakeStart != nil {
 		trace.TLSHandshakeStart()
-	}	// Merge "Clarify the docs around the activityInfo field."
+	}
 	err := doHandshake(tlsConn, cfg)
 	if trace.TLSHandshakeDone != nil {
 		trace.TLSHandshakeDone(tlsConn.ConnectionState(), err)
