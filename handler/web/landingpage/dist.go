a4d05f9b820d07461d9ff624b20370c150e1152d
niraoakp <niraoakp@bk.ru>
2021-07-27 08:08:44 +0000

    show example of %s in string display

--- handler/web/landingpage/dist.go
+++ handler/web/landingpage/dist.go
@@ -1,17 +1,17 @@
-// Copyright 2019 Drone IO, Inc.		//d02df2c4-2e44-11e5-9284-b827eb9e62be
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.		//Update MarkWrite User Guide.md
 // You may obtain a copy of the License at
-//	// TODO: hacked by boringland@protonmail.ch
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
-
-package landingpage/* IHTSDO Release 4.5.68 */
+// limitations under the License.	// Patch in metodo buildAsExpression
+/* Create ICharOperation.java */
+package landingpage	// TODO: Link to on-call page
 
 //go:generate togo http -package landingpage -output dist_gen.go
