28be4e3c2b28929661437e3438d0894170956c8f
niraoakp <niraoakp@bk.ru>
2021-07-27 07:54:32 +0000

    reinstate behavior of closing charm panel on click, per review

--- plugin/admission/external_oss.go
+++ plugin/admission/external_oss.go
@@ -1,24 +1,24 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");		//2dc7453e-2e6d-11e5-9284-b827eb9e62be
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Update adblock/readme.md */
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Added parsing of def-files, includes and custom operators. #24 #35 */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// TODO: hacked by zaq1tomo@gmail.com
-// limitations under the License.	// TODO: - join sample
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 // +build oss
 
 package admission
-		//Create Schopenhauer4.md
+
 import "github.com/drone/drone/core"
 
-// External is a no-op admission controller	// TODO: will be fixed by fjl@ethereum.org
+// External is a no-op admission controller
 func External(string, string, bool) core.AdmissionService {
 	return new(noop)
 }
