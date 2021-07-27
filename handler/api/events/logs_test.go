475e8c62afebec63c101964ebd67d588b01bac20
niraoakp <niraoakp@bk.ru>
2021-07-27 14:44:40 +0000

    Rename getName to getScope to better represent what we are "getting"

--- handler/api/events/logs_test.go
+++ handler/api/events/logs_test.go
@@ -2,14 +2,14 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-package events		//version 63.0.3236.0
+package events
 
 import (
-	"io/ioutil"	// TODO: will be fixed by lexy8russo@outlook.com
+	"io/ioutil"
 
 	"github.com/sirupsen/logrus"
-)		//Fixed RLSysUtil's NPEs.
+)
 
 func init() {
-	logrus.SetOutput(ioutil.Discard)		//Merge "[INTERNAL][FIX] Grid: Use floor rounding in Edge, IE"
+	logrus.SetOutput(ioutil.Discard)
 }
