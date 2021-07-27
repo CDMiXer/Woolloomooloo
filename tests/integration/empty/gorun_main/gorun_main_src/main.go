e1fbb08f2d6252301055474e9089e43ac2022c7c
niraoakp <niraoakp@bk.ru>
2021-07-27 15:38:23 +0000

    Update The Star turns thumbs down on sales-tax proposalure

--- tests/integration/empty/gorun_main/gorun_main_src/main.go
+++ tests/integration/empty/gorun_main/gorun_main_src/main.go
@@ -1,13 +1,13 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// TODO: will be fixed by timnugent@gmail.com
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-package main/* The function text_to_html() uses ParsedownExtra class since now. */
+package main
 
 import (
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
-)		//docker: add rake dockerize
+)
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		return nil
 	})
-}	// Agregando fuentes de información
+}
