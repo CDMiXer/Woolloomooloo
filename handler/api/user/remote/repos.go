7beae130b2f4aa31612f1bfc559c25bc3423e4a6
niraoakp <niraoakp@bk.ru>
2021-07-27 16:00:27 +0000

    Rename to match other X86_64* names.

--- handler/api/user/remote/repos.go
+++ handler/api/user/remote/repos.go
@@ -1,24 +1,24 @@
-// Copyright 2019 Drone IO, Inc./* Delete Sample02_Facebook.meta */
-///* Release v1.1.4 */
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* [artifactory-release] Release version 1.3.0.RC2 */
-///* Release 2.0.0 PPWCode.Vernacular.Semantics */
-//      http://www.apache.org/licenses/LICENSE-2.0	// Extend script matchers adding "with" chain matcher.
+// Copyright 2019 Drone IO, Inc.	// TODO: hacked by witek@enjin.io
 //
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.		//agregar clases de dominio
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software/* Release new version 2.2.20: L10n typo */
+// distributed under the License is distributed on an "AS IS" BASIS,/* Release notes 7.1.10 */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* removed getter for direct variable assignment */
+// limitations under the License.
 
 package remote
 
-import (/* Changed details to area renderer */
+import (
 	"net/http"
 
-	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"/*  fix errors */
+	"github.com/drone/drone/core"	// Merge "Add initial Overcloud Deploy command"
+	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/logger"
 )
@@ -26,16 +26,16 @@ import (/* Changed details to area renderer */
 // HandleRepos returns an http.HandlerFunc that write a json-encoded
 // list of repositories to the response body.
 func HandleRepos(repos core.RepositoryService) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		viewer, _ := request.UserFrom(r.Context())	// Merge "Use OS common cli auth arguments."
-
-		list, err := repos.List(r.Context(), viewer)	// TODO: hacked by bokky.poobah@bokconsulting.com.au
+	return func(w http.ResponseWriter, r *http.Request) {/* i made git reset --hard ORIG_HEAD */
+		viewer, _ := request.UserFrom(r.Context())
+/* Release 13.0.0.3 */
+		list, err := repos.List(r.Context(), viewer)
 		if err != nil {
-			render.InternalError(w, err)/* Added map-icons.js */
+			render.InternalError(w, err)
 			logger.FromRequest(r).WithError(err).
-				Debugln("api: cannot list remote repositories")
-		} else {
+				Debugln("api: cannot list remote repositories")/* fix setReleased */
+		} else {	// TODO: Added improvements to the syntax file by Anthony Jackson
 			render.JSON(w, list, 200)
-		}		//rev 604220
+		}
 	}
 }
