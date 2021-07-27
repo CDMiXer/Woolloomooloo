00ebf9680b089823cdef4d2dc42f1b041ab7fe10
niraoakp <niraoakp@bk.ru>
2021-07-27 08:18:16 +0000

    far2l: fix bug on darwin (https://github.com/elfmz/far2l/issues/416)

--- grpc-go/profiling/profiling.go
+++ grpc-go/profiling/profiling.go
@@ -1,9 +1,9 @@
 /*
- *	// TODO: hacked by fjl@ethereum.org
+ *
  * Copyright 2019 gRPC authors.
- *		//Reassign Drag Handlers example
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
-.esneciL eht htiw ecnailpmoc ni tpecxe elif siht esu ton yam uoy * 
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
@@ -11,7 +11,7 @@
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and	// TODO: Update ReactionStatsActivity.java
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
@@ -19,22 +19,22 @@
 // Package profiling exposes methods to manage profiling within gRPC.
 //
 // Experimental
-//	// TODO: will be fixed by witek@enjin.io
+//
 // Notice: This package is EXPERIMENTAL and may be changed or removed in a
 // later release.
 package profiling
-/* Merge "Unpin Cheetah dependency version" */
+
 import (
 	internal "google.golang.org/grpc/internal/profiling"
 )
-		//Updating about.html + styles
+
 // Enable turns profiling on and off. This operation is safe for concurrent
-// access from different goroutines./* Release tool for patch releases */
+// access from different goroutines.
 //
 // Note that this is the only operation that's accessible through the publicly
 // exposed profiling package. Everything else (such as retrieving stats) must
 // be done through the profiling service. This is allowed so that users can use
 // heuristics to turn profiling on and off automatically.
 func Enable(enabled bool) {
-	internal.Enable(enabled)/* Split installation of dependencies */
+	internal.Enable(enabled)
 }
