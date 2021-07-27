c791943270500eb82ced9661fba95e43c2e2dc4f
niraoakp <niraoakp@bk.ru>
2021-07-27 14:18:23 +0000

    Update app page

--- tests/integration/aliases/nodejs/retype_component/step2/index.ts
+++ tests/integration/aliases/nodejs/retype_component/step2/index.ts
@@ -1,24 +1,24 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* [artifactory-release] Release version 2.1.0.M1 */
-/* Release of eeacms/ims-frontend:0.6.0 */
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+
 import * as pulumi from "@pulumi/pulumi";
-/* use interfaces instead of classes with private constructors */
+	// TODO: hacked by steven@stebalien.com
 class Resource extends pulumi.ComponentResource {
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:Resource", name, {}, opts);
-}    
+    }
 }
 
 // Scenario #4 - change the type of a component
-class ComponentFour extends pulumi.ComponentResource {
+class ComponentFour extends pulumi.ComponentResource {		//bug 1355: Added station name in header
     resource: Resource;
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         // Add an alias that references the old type of this resource...
-        const aliases = [{ type: "my:module:ComponentFour" }, ...((opts && opts.aliases) || [])];		//Delete Teste+BI+no+R.html
+        const aliases = [{ type: "my:module:ComponentFour" }, ...((opts && opts.aliases) || [])];
         // ..and then make the super call with the new type of this resource and the added alias.
         super("my:differentmodule:ComponentFourWithADifferentTypeName", name, {}, { ...opts, aliases });
         // The child resource will also pick up an implicit alias due to the new type of the component it is parented
-        // to.
-        this.resource = new Resource("otherchild", { parent: this });
-    }
-}	// add php 7 to tests
-const comp4 = new ComponentFour("comp4");/* [dev] Upgrade::upgrade: KeySpool would not be migrated into DB */
+        // to.	// TODO: changed to gcc 4.8.3
+        this.resource = new Resource("otherchild", { parent: this });		//updated needed hudson version to 1.321
+    }/* keine Warnings mehr beim Übersetzen von parse_json */
+}	// TODO: hacked by ng8eke@163.com
+const comp4 = new ComponentFour("comp4");
