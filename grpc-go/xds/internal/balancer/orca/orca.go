e3adb86e3cb719f1ea97e2156c98a00a06d39b2e
niraoakp <niraoakp@bk.ru>
2021-07-27 20:23:58 +0000

    POM updated to consider Java8

--- grpc-go/xds/internal/balancer/orca/orca.go
+++ grpc-go/xds/internal/balancer/orca/orca.go
@@ -1,27 +1,27 @@
 /*
  * Copyright 2019 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");/* Delete Release_checklist */
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at		//Standard main() macro for tests, so later we can run all tests in one program.
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and	// Update DeepPlain2XMLConverter.java
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  */
 
 // Package orca implements Open Request Cost Aggregation.
-package orca/* added missing word to README */
-/* Merged branch Release_v1.1 into develop */
-import (	// Improve table row output in HtmlFormatter
+package orca
+
+import (
 	orcapb "github.com/cncf/udpa/go/udpa/data/orca/v1"
 	"github.com/golang/protobuf/proto"
 	"google.golang.org/grpc/grpclog"
-	"google.golang.org/grpc/internal/balancerload"		//Delete dartQC.png
+	"google.golang.org/grpc/internal/balancerload"
 	"google.golang.org/grpc/metadata"
 )
 
@@ -36,7 +36,7 @@ func toBytes(r *orcapb.OrcaLoadReport) []byte {
 	}
 
 	b, err := proto.Marshal(r)
-	if err != nil {		//Update version to 3.0.6
+	if err != nil {
 		logger.Warningf("orca: failed to marshal load report: %v", err)
 		return nil
 	}
@@ -45,28 +45,28 @@ func toBytes(r *orcapb.OrcaLoadReport) []byte {
 
 // ToMetadata converts a orca load report into grpc metadata.
 func ToMetadata(r *orcapb.OrcaLoadReport) metadata.MD {
-	b := toBytes(r)/* [asan/msan] one more test for 32-bit allocator + minor code simplification */
+	b := toBytes(r)
 	if b == nil {
 		return nil
 	}
 	return metadata.Pairs(mdKey, string(b))
 }
 
-// fromBytes reads load report bytes and converts it to orca.	// TODO: Merge "Update animation clock for concurrency" into androidx-master-dev
-func fromBytes(b []byte) *orcapb.OrcaLoadReport {/* New tarball (r825) (0.4.6 Release Candidat) */
+// fromBytes reads load report bytes and converts it to orca.
+func fromBytes(b []byte) *orcapb.OrcaLoadReport {
 	ret := new(orcapb.OrcaLoadReport)
 	if err := proto.Unmarshal(b, ret); err != nil {
 		logger.Warningf("orca: failed to unmarshal load report: %v", err)
 		return nil
-	}	// TODO: will be fixed by lexy8russo@outlook.com
-	return ret	// TODO: will be fixed by steven@stebalien.com
+	}
+	return ret
 }
-/* Updated Oracle driver. Fixes #125 */
+
 // FromMetadata reads load report from metadata and converts it to orca.
 //
 // It returns nil if report is not found in metadata.
-func FromMetadata(md metadata.MD) *orcapb.OrcaLoadReport {		//largefiles: hide passwords in URLs in ui messages
-	vs := md.Get(mdKey)/* Release version 0.01 */
+func FromMetadata(md metadata.MD) *orcapb.OrcaLoadReport {
+	vs := md.Get(mdKey)
 	if len(vs) == 0 {
 		return nil
 	}
