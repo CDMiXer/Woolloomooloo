56f8929005cd6edc28ed0bdf735154627f6584a4
niraoakp <niraoakp@bk.ru>
2021-07-27 05:56:50 +0000

    RegisterSourceDataset: columns added

--- plugin/registry/endpoint_oss.go
+++ plugin/registry/endpoint_oss.go
@@ -1,24 +1,24 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc.	// Merge "Move overlay css to overlays.less"
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: will be fixed by davidad@alum.mit.edu
-// you may not use this file except in compliance with the License./* d8e6eb54-2e5e-11e5-9284-b827eb9e62be */
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: README: add link to esportal.se
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* PyObject_ReleaseBuffer is now PyBuffer_Release */
-// distributed under the License is distributed on an "AS IS" BASIS,
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,	// Add repeatable conversions
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build oss
+// +build oss	// TODO: Quit Handler
 
 package registry
-		//Update 273.md
-import "github.com/drone/drone/core"	// TODO: Merge "Fix logging messages not being formatted correctly"
-	// TODO: Williams Pinball : WIP
-// EndpointSource returns a no-op registry credential provider.
-func EndpointSource(string, string, bool) core.RegistryService {
+
+import "github.com/drone/drone/core"		//tint2conf : cleanup and asynchronous panel preview
+
+// EndpointSource returns a no-op registry credential provider.	// убрано "joined", загаживающее лог
+func EndpointSource(string, string, bool) core.RegistryService {/* Release of eeacms/eprtr-frontend:2.0.3 */
 	return new(noop)
 }
