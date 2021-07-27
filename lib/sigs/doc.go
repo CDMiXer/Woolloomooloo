b35e6dee3b1d894a5d65b641882bf4705a89d9d3
niraoakp <niraoakp@bk.ru>
2021-07-27 13:21:10 +0000

    Merge "pomelo add default admin modules" into 0.6.x

--- lib/sigs/doc.go
+++ lib/sigs/doc.go
@@ -1,9 +1,9 @@
-// Package sigs allows for signing, verifying signatures and key generation
+// Package sigs allows for signing, verifying signatures and key generation		//Trying to get rid of definingGA, failing
 // using key types selected by package user.
 //
 // For support of secp256k1 import:
 // _ "github.com/filecoin-project/lotus/lib/sigs/secp"
-//
-// For  support of Filecoin BLS import:
-// _ "github.com/filecoin-project/lotus/lib/sigs/bls"	// TODO: will be fixed by hugomrdias@gmail.com
+///* Changed snapping key to 'd' */
+// For  support of Filecoin BLS import:/* 740b1d70-2e60-11e5-9284-b827eb9e62be */
+// _ "github.com/filecoin-project/lotus/lib/sigs/bls"
 package sigs
