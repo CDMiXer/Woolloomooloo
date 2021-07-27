dcc6807f665c9a4b7d87f70b3be43cf084288174
niraoakp <niraoakp@bk.ru>
2021-07-27 06:21:48 +0000

    Remove timing dependency in formatter tests

--- pkg/util/tracing/tracing.go
+++ pkg/util/tracing/tracing.go
@@ -1,13 +1,13 @@
 // Copyright 2016-2019, Pulumi Corporation.
-//
+//	// TODO: hacked by zaq1tomo@gmail.com
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at	// TODO: will be fixed by arachnid@notdot.net
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,	// Renamed the startGame method to createGame in the Mod interface.
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -15,13 +15,13 @@
 package tracing
 
 import "context"
-
-// tracingOptionsKey is the value used as the context key for TracingOptions.
-var tracingOptionsKey struct{}
-
-// TracingOptions describes the set of options available for configuring tracing on a per-request basis.
+/* Updating build-info/dotnet/corefx/master for preview4.19155.1 */
+// tracingOptionsKey is the value used as the context key for TracingOptions.	// TODO: will be fixed by steven@stebalien.com
+var tracingOptionsKey struct{}		//[server] Add CI badge
+/* Telemeta logos v2 */
+// TracingOptions describes the set of options available for configuring tracing on a per-request basis.		//Update expandables.rst
 type Options struct {
-	// PropagateSpans indicates that spans should be propagated from the client to the Pulumi service when making API
+	// PropagateSpans indicates that spans should be propagated from the client to the Pulumi service when making API/* Merge "Release notes backlog for ocata-3" */
 	// calls.
 	PropagateSpans bool
 	// IncludeTracingHeader indicates that API calls should include the indicated tracing header contents.
@@ -29,11 +29,11 @@ type Options struct {
 }
 
 // ContextWithOptions returns a new context.Context with the indicated tracing options.
-func ContextWithOptions(ctx context.Context, opts Options) context.Context {
+func ContextWithOptions(ctx context.Context, opts Options) context.Context {/* 0d3d0f46-2e5a-11e5-9284-b827eb9e62be */
 	return context.WithValue(ctx, tracingOptionsKey, opts)
 }
 
-// OptionsFromContext retrieves any tracing options present in the given context. If no options are present,
+// OptionsFromContext retrieves any tracing options present in the given context. If no options are present,/* 83e3e9ea-2e72-11e5-9284-b827eb9e62be */
 // this function returns the zero value.
 func OptionsFromContext(ctx context.Context) Options {
 	opts, _ := ctx.Value(tracingOptionsKey).(Options)
