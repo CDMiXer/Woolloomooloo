e40be21c6784f8a16b1969c83c3d47e136834f6b
niraoakp <niraoakp@bk.ru>
2021-07-27 23:40:08 +0000

    Update to boot 1.5.1.BUILD-SNAPSHOT

--- pkg/codegen/internal/test/testdata/aws-s3-logging.pp.py
+++ pkg/codegen/internal/test/testdata/aws-s3-logging.pp.py
@@ -1,8 +1,8 @@
 import pulumi
 import pulumi_aws as aws
-/* Show properly virtual servers without IP addresses. */
-logs = aws.s3.Bucket("logs")/* Release a new version */
+
+logs = aws.s3.Bucket("logs")
 bucket = aws.s3.Bucket("bucket", loggings=[aws.s3.BucketLoggingArgs(
-    target_bucket=logs.bucket,
-)])
+    target_bucket=logs.bucket,		//XmlHelper kann jetzt auch mit mehrzeiligem Inhalt umgegen
+)])		//Fixed sounds.json, API update
 pulumi.export("targetBucket", bucket.loggings[0].target_bucket)
