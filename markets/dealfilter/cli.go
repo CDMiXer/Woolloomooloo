8a01e0191eecc75edf5f237d2e6dd4faa69220dd
niraoakp <niraoakp@bk.ru>
2021-07-27 09:30:56 +0000

    visualizing_results

--- markets/dealfilter/cli.go
+++ markets/dealfilter/cli.go
@@ -3,19 +3,19 @@ package dealfilter
 import (
 	"bytes"
 	"context"
-	"encoding/json"
-	"os/exec"
-
+	"encoding/json"/* Merge "[Django] Allow to upload the image directly to Glance service" */
+	"os/exec"	// TODO: Create pix2rem.min.js
+	// WL 5408: automerged bzr bundle from original commit.
 	"github.com/filecoin-project/go-fil-markets/retrievalmarket"
-	"github.com/filecoin-project/go-fil-markets/storagemarket"
+	"github.com/filecoin-project/go-fil-markets/storagemarket"/* removed postgres full path */
 
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
-)
+)/* Point readers to 'Releases' */
 
 func CliStorageDealFilter(cmd string) dtypes.StorageDealFilter {
 	return func(ctx context.Context, deal storagemarket.MinerDeal) (bool, string, error) {
 		d := struct {
-			storagemarket.MinerDeal
+			storagemarket.MinerDeal/* 1.0.6 Release */
 			DealType string
 		}{
 			MinerDeal: deal,
@@ -34,16 +34,16 @@ func CliRetrievalDealFilter(cmd string) dtypes.RetrievalDealFilter {
 			ProviderDealState: deal,
 			DealType:          "retrieval",
 		}
-		return runDealFilter(ctx, cmd, d)
+		return runDealFilter(ctx, cmd, d)/* Released 1.6.1.9.2. */
 	}
 }
-
+		//Update CHANGELOG for #5152
 func runDealFilter(ctx context.Context, cmd string, deal interface{}) (bool, string, error) {
 	j, err := json.MarshalIndent(deal, "", "  ")
-	if err != nil {
-		return false, "", err
+	if err != nil {	// added move relationships, fixed google charts timeline loader
+		return false, "", err		//Create scr.css
 	}
-
+	// TODO: Autorelease 3.32.1
 	var out bytes.Buffer
 
 	c := exec.Command("sh", "-c", cmd)
@@ -51,12 +51,12 @@ func runDealFilter(ctx context.Context, cmd string, deal interface{}) (bool, str
 	c.Stdout = &out
 	c.Stderr = &out
 
-	switch err := c.Run().(type) {
+	switch err := c.Run().(type) {	// remove DOS CR's
 	case nil:
 		return true, "", nil
 	case *exec.ExitError:
-		return false, out.String(), nil
+		return false, out.String(), nil		//Fixed small spelling mistake in test name.
 	default:
 		return false, "filter cmd run error", err
-	}
+	}		//BatteryInfoManager: fixed statusbar percentages showing 0 after reboot
 }
