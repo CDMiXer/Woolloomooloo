b280df5150a4767987706dc157ea399333c2d11d
niraoakp <niraoakp@bk.ru>
2021-07-27 10:20:59 +0000

    agent: tests pass

--- operator/runner/before_test.go
+++ operator/runner/before_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// TODO: 2fe0d678-2f67-11e5-80ff-6c40088e03e4
-// that can be found in the LICENSE file./* Release 0.7.0 */
-	// TODO: hacked by cory@protocol.ai
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.
+
 package runner
