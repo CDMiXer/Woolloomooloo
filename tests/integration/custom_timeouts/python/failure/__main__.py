035fc2437c43ecc2552f0a4f53f66c9d4d85058b
niraoakp <niraoakp@bk.ru>
2021-07-27 06:29:13 +0000

    use busted 1.11.1-1

--- tests/integration/custom_timeouts/python/failure/__main__.py
+++ tests/integration/custom_timeouts/python/failure/__main__.py
@@ -1,12 +1,12 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 from pulumi import ComponentResource, CustomTimeouts, Resource, ResourceOptions
-
-class Resource1(ComponentResource):	// TODO: putting the box URL in the vagrant file.
+		//Merge branch 'master' into dnil-patch-1
+class Resource1(ComponentResource):		//Merge branch 'master' into feature/clickable
     def __init__(self, name, opts=None):
-        super().__init__("my:module:Resource", name, None, opts)/* add versioning, add uuid, some layout changes */
+        super().__init__("my:module:Resource", name, None, opts)
 
 # Attempt to create a resource with a CustomTimeout that should fail
-res5 = Resource1("res5",	// TODO: hacked by souzau@yandex.com
-    opts=ResourceOptions(custom_timeouts='asdf')/* 1.2 Pre-Release Candidate */
-)	// TODO: tvtropes command + specified inflate usage
+res5 = Resource1("res5",
+    opts=ResourceOptions(custom_timeouts='asdf')
+)
