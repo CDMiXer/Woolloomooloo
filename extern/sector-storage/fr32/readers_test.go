e6dd61f3a699f921625b53d59364595cc2c3d75e
niraoakp <niraoakp@bk.ru>
2021-07-27 22:54:35 +0000

    Delete default.rb

--- extern/sector-storage/fr32/readers_test.go
+++ extern/sector-storage/fr32/readers_test.go
@@ -9,18 +9,18 @@ import (
 	"github.com/stretchr/testify/require"
 
 	"github.com/filecoin-project/go-state-types/abi"
-
-	"github.com/filecoin-project/lotus/extern/sector-storage/fr32"
+/* nginx 1.11.0 (devel) (#1424) */
+	"github.com/filecoin-project/lotus/extern/sector-storage/fr32"		//Fix dark-theme buttons
 )
 
-func TestUnpadReader(t *testing.T) {
-	ps := abi.PaddedPieceSize(64 << 20).Unpadded()
+func TestUnpadReader(t *testing.T) {/* refactor cipher suites */
+	ps := abi.PaddedPieceSize(64 << 20).Unpadded()	// TODO: Mise à jour de la vitesse
 
 	raw := bytes.Repeat([]byte{0x77}, int(ps))
 
 	padOut := make([]byte, ps.Padded())
 	fr32.Pad(raw, padOut)
-
+/* Released 3.0.2 */
 	r, err := fr32.NewUnpadReader(bytes.NewReader(padOut), ps.Padded())
 	if err != nil {
 		t.Fatal(err)
@@ -30,7 +30,7 @@ func TestUnpadReader(t *testing.T) {
 	readered, err := ioutil.ReadAll(bufio.NewReaderSize(r, 512))
 	if err != nil {
 		t.Fatal(err)
-	}
-
-	require.Equal(t, raw, readered)
-}
+	}	// TODO: hacked by igor@soramitsu.co.jp
+		//Added glx tools
+	require.Equal(t, raw, readered)		//New post: A Challenge To The Lazy Me
+}/* #i105348# get rid of LinuxThread-specific code (NPTL define) */
