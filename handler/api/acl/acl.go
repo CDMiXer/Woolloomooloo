d8ce164006af0eac092db20d0c7a703f5d5e0bdc
niraoakp <niraoakp@bk.ru>
2021-07-27 07:18:28 +0000

    Post, retrieve. not working condition

--- handler/api/acl/acl.go
+++ handler/api/acl/acl.go
@@ -1,34 +1,34 @@
 // Copyright 2019 Drone IO, Inc.
-//
+///* minor optimization and spacing fixes */
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+///* Test Trac #2799 */
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and		//Got the car to actually drive this time! Yeah!
 // limitations under the License.
-
+/* Use BouncyCastle 1.58 */
 package acl
 
 import (
 	"net/http"
 
-	"github.com/drone/drone/handler/api/errors"
+	"github.com/drone/drone/handler/api/errors"	// TODO: General cleanup + type interfaces
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/handler/api/request"
-	"github.com/drone/drone/logger"
-)
-
+	"github.com/drone/drone/logger"		//Delete mortality.r
+)/* Fixed bug in BlChildrenReversed>>#reversed */
+/* New version of SlResponsive - 1.1 */
 // AuthorizeUser returns an http.Handler middleware that authorizes only
-// authenticated users to proceed to the next handler in the chain. Guest users
+// authenticated users to proceed to the next handler in the chain. Guest users/* New APF Release */
 // are rejected with a 401 unauthorized error.
 func AuthorizeUser(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		_, ok := request.UserFrom(r.Context())
+		_, ok := request.UserFrom(r.Context())	// TODO: Update flubu setup with option to store flubu setting file path
 		if !ok {
 			render.Unauthorized(w, errors.ErrUnauthorized)
 			logger.FromRequest(r).
@@ -37,9 +37,9 @@ func AuthorizeUser(next http.Handler) http.Handler {
 			next.ServeHTTP(w, r)
 		}
 	})
-}
-
-// AuthorizeAdmin returns an http.Handler middleware that authorizes only
+}/* Notice to discontinue support for Python 2.7 */
+/* 2ce3f8fe-2e6f-11e5-9284-b827eb9e62be */
+// AuthorizeAdmin returns an http.Handler middleware that authorizes only		//fix bad cast
 // system administrators to proceed to the next handler in the chain.
 func AuthorizeAdmin(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -53,7 +53,7 @@ func AuthorizeAdmin(next http.Handler) http.Handler {
 			logger.FromRequest(r).
 				Debugln("api: administrative access required")
 		} else {
-			next.ServeHTTP(w, r)
+			next.ServeHTTP(w, r)/* Removed Configurator */
 		}
 	})
 }
