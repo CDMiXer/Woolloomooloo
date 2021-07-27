5be0a02474e897386c1156b3bf9e0a9271298334
niraoakp <niraoakp@bk.ru>
2021-07-27 11:55:13 +0000

    a063a0f6-2e4f-11e5-9284-b827eb9e62be

--- metric/license.go
+++ metric/license.go
@@ -1,16 +1,16 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Copyright 2019 Drone.IO Inc. All rights reserved.		//adae661c-2e61-11e5-9284-b827eb9e62be
+// Use of this source code is governed by the Drone Non-Commercial License		//Merge branch 'master' into assertBodyEquals
 // that can be found in the LICENSE file.
 
 // +build !oss
-
+/* Merge "[FIX] core/StashedControlSupport: unstash with owner component" */
 package metric
-/* Release v0.1.0 */
+
 import "github.com/drone/drone/core"
-	// Add encryption/decryption in CBC mode
-// License registers the license metrics./* rev 560552 */
-func License(license core.LicenseService) {
+
+// License registers the license metrics.
+func License(license core.LicenseService) {		//rev 702385
 	// track days until expires
 	// track user limit
-	// track repo limit	// TODO: Solve deprecations
+	// track repo limit
 }
