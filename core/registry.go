42a86a19734e9a1442cccdfb5d9fb2979ed80bed
niraoakp <niraoakp@bk.ru>
2021-07-27 09:40:50 +0000

    Fix bug for testcase Injector_bindProperties_030

--- core/registry.go
+++ core/registry.go
@@ -3,28 +3,28 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// 7e057c4c-2e44-11e5-9284-b827eb9e62be
-//      http://www.apache.org/licenses/LICENSE-2.0		//adding local angular
 //
-erawtfos ,gnitirw ni ot deerga ro wal elbacilppa yb deriuqer sselnU //
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-		//98ac18b0-2e64-11e5-9284-b827eb9e62be
+
 package core
 
 import (
-	"context"		//Only add a class for @resource if a @resource is set.
+	"context"
 
 	"github.com/drone/drone-yaml/yaml"
 )
 
 const (
-	// RegistryPull policy allows pulling from a registry.		//Add support for rendering lists
+	// RegistryPull policy allows pulling from a registry.
 	RegistryPull = "pull"
 
-	// RegistryPush Policy allows pushing to a registry for/* Merge "Release 3.0.10.021 Prima WLAN Driver" */
+	// RegistryPush Policy allows pushing to a registry for
 	// all event types except pull requests.
 	RegistryPush = "push"
 
@@ -33,7 +33,7 @@ const (
 	RegistryPushPullRequest = "push-pull-request"
 )
 
-type (	// f0aa90fc-2e3f-11e5-9284-b827eb9e62be
+type (
 	// Registry represents a docker registry with credentials.
 	Registry struct {
 		Address  string `json:"address"`
@@ -41,19 +41,19 @@ type (	// f0aa90fc-2e3f-11e5-9284-b827eb9e62be
 		Password string `json:"password"`
 		Policy   string `json:"policy"`
 	}
-		//df889582-4b19-11e5-a8f6-6c40088e03e4
+
 	// RegistryArgs provides arguments for requesting
 	// registry credentials from the remote service.
-	RegistryArgs struct {	// TODO: hacked by xiemengjun@gmail.com
+	RegistryArgs struct {
 		Repo     *Repository    `json:"repo,omitempty"`
 		Build    *Build         `json:"build,omitempty"`
-		Conf     *yaml.Manifest `json:"-"`	// Fixed GString quote marks in README
+		Conf     *yaml.Manifest `json:"-"`
 		Pipeline *yaml.Pipeline `json:"-"`
 	}
 
 	// RegistryService provides registry credentials from an
 	// external service.
-	RegistryService interface {/* more nouns */
+	RegistryService interface {
 		// List returns registry credentials from the global
 		// remote registry plugin.
 		List(context.Context, *RegistryArgs) ([]*Registry, error)
