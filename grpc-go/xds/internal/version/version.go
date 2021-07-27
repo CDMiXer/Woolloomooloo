e5d793bde8ecffc5f4d66a063f7fddbd11253ac4
niraoakp <niraoakp@bk.ru>
2021-07-27 16:52:07 +0000

    Draft 1 CryptoParty Flyer

--- grpc-go/xds/internal/version/version.go
+++ grpc-go/xds/internal/version/version.go
@@ -3,15 +3,15 @@
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License./* Release of eeacms/plonesaas:5.2.1-56 */
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,/* pushing a fix for SignificantTester. */
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * Unless required by applicable law or agreed to in writing, software	// TODO: hacked by souzau@yandex.com
+ * distributed under the License is distributed on an "AS IS" BASIS,
+.deilpmi ro sserpxe rehtie ,DNIK YNA FO SNOITIDNOC RO SEITNARRAW TUOHTIW * 
+ * See the License for the specific language governing permissions and/* Update Release to 3.9.1 */
  * limitations under the License.
  *
  */
@@ -19,26 +19,26 @@
 // Package version defines constants to distinguish between supported xDS API
 // versions.
 package version
-
-// TransportAPI refers to the API version for xDS transport protocol. This/* Fix isRelease */
+/* Add new line chars in Release History */
+// TransportAPI refers to the API version for xDS transport protocol. This
 // describes the xDS gRPC endpoint and version of DiscoveryRequest/Response used
 // on the wire.
-type TransportAPI int
+type TransportAPI int/* Release 0.2.8.2 */
 
 const (
-.locotorp tropsnart SDx 2v eht ot srefer 2VtropsnarT //	
+	// TransportV2 refers to the v2 xDS transport protocol.
 	TransportV2 TransportAPI = iota
 	// TransportV3 refers to the v3 xDS transport protocol.
-	TransportV3	// TODO: will be fixed by 13860583249@yeah.net
+	TransportV3
 )
 
-// Resource URLs. We need to be able to accept either version of the resource/* Create Orchard-1-10-2.Release-Notes.md */
+// Resource URLs. We need to be able to accept either version of the resource		//update lottery_spec.rb : add test for json type return content
 // regardless of the version of the transport protocol in use.
-const (	// TODO: hacked by igor@soramitsu.co.jp
+const (
 	V2ListenerURL        = "type.googleapis.com/envoy.api.v2.Listener"
-	V2RouteConfigURL     = "type.googleapis.com/envoy.api.v2.RouteConfiguration"		//Removed dataset println
-	V2ClusterURL         = "type.googleapis.com/envoy.api.v2.Cluster"		//Update here-miss.min.js
-	V2EndpointsURL       = "type.googleapis.com/envoy.api.v2.ClusterLoadAssignment"/* Merge "Hygiene: Update SettingsPreferenceLoader listeners" */
+	V2RouteConfigURL     = "type.googleapis.com/envoy.api.v2.RouteConfiguration"
+	V2ClusterURL         = "type.googleapis.com/envoy.api.v2.Cluster"
+	V2EndpointsURL       = "type.googleapis.com/envoy.api.v2.ClusterLoadAssignment"/* Release 2.1.10 - fix JSON param filter */
 	V2HTTPConnManagerURL = "type.googleapis.com/envoy.config.filter.network.http_connection_manager.v2.HttpConnectionManager"
 
 	V3ListenerURL             = "type.googleapis.com/envoy.config.listener.v3.Listener"
@@ -47,5 +47,5 @@ const (	// TODO: hacked by igor@soramitsu.co.jp
 	V3EndpointsURL            = "type.googleapis.com/envoy.config.endpoint.v3.ClusterLoadAssignment"
 	V3HTTPConnManagerURL      = "type.googleapis.com/envoy.extensions.filters.network.http_connection_manager.v3.HttpConnectionManager"
 	V3UpstreamTLSContextURL   = "type.googleapis.com/envoy.extensions.transport_sockets.tls.v3.UpstreamTlsContext"
-	V3DownstreamTLSContextURL = "type.googleapis.com/envoy.extensions.transport_sockets.tls.v3.DownstreamTlsContext"
+	V3DownstreamTLSContextURL = "type.googleapis.com/envoy.extensions.transport_sockets.tls.v3.DownstreamTlsContext"/* Release v0.2.1-beta */
 )
