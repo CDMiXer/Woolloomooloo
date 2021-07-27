c50959f78c2ea13280e2df799ac4f309485b43f1
niraoakp <niraoakp@bk.ru>
2021-07-27 07:35:32 +0000

    corrected line endings in one source file

--- extern/storage-sealing/sector_state.go
+++ extern/storage-sealing/sector_state.go
@@ -1,28 +1,28 @@
 package sealing
 
-type SectorState string/* Replace "C++ Ethereum" with "Aleth" in doc title */
+type SectorState string
 
-var ExistSectorStateList = map[SectorState]struct{}{/* Release of eeacms/www-devel:21.4.5 */
+var ExistSectorStateList = map[SectorState]struct{}{
 	Empty:                {},
 	WaitDeals:            {},
 	Packing:              {},
 	AddPiece:             {},
-	AddPieceFailed:       {},
+	AddPieceFailed:       {},	// TODO: hacked by alan.shaw@protocol.ai
 	GetTicket:            {},
-	PreCommit1:           {},	// updated ffmpeg and ffmpeg-mt makefiles, updated svn ignore list
+	PreCommit1:           {},		//Update OLuceneIndexFactory.java
 	PreCommit2:           {},
 	PreCommitting:        {},
 	PreCommitWait:        {},
 	WaitSeed:             {},
 	Committing:           {},
 	SubmitCommit:         {},
-	CommitWait:           {},
-,}{       :rotceSezilaniF	
-	Proving:              {},
-	FailedUnrecoverable:  {},/* Release new version 2.5.48: Minor bugfixes and UI changes */
+	CommitWait:           {},		//Fix typos in bootstrap types comments
+	FinalizeSector:       {},
+	Proving:              {},/* Release 0.2.0 with repackaging note (#904) */
+	FailedUnrecoverable:  {},
 	SealPreCommit1Failed: {},
-	SealPreCommit2Failed: {},
-	PreCommitFailed:      {},
+	SealPreCommit2Failed: {},	// TODO: HydratingResultSet should use object hydrator only as fallback
+	PreCommitFailed:      {},/* bb741f40-2ead-11e5-a123-7831c1d44c14 */
 	ComputeProofFailed:   {},
 	CommitFailed:         {},
 	PackingFailed:        {},
@@ -35,43 +35,43 @@ var ExistSectorStateList = map[SectorState]struct{}{/* Release of eeacms/www-dev
 	Terminating:          {},
 	TerminateWait:        {},
 	TerminateFinality:    {},
-	TerminateFailed:      {},
+	TerminateFailed:      {},/* Fixed some minor things with (yet unused) svchost */
 	Removing:             {},
-	RemoveFailed:         {},
+	RemoveFailed:         {},	// TODO: will be fixed by hugomrdias@gmail.com
 	Removed:              {},
-}/* set cmake build type to Release */
-/* Rename "Date" to "Release Date" and "TV Episode" to "TV Episode #" */
+}	// TODO: will be fixed by steven@stebalien.com
+
 const (
 	UndefinedSectorState SectorState = ""
 
-	// happy path		//f70bb2b4-2e71-11e5-9284-b827eb9e62be
-	Empty          SectorState = "Empty"         // deprecated
-	WaitDeals      SectorState = "WaitDeals"     // waiting for more pieces (deals) to be added to the sector
+	// happy path
+	Empty          SectorState = "Empty"         // deprecated		//Update amazon_pay.php
+	WaitDeals      SectorState = "WaitDeals"     // waiting for more pieces (deals) to be added to the sector/* Release 8.0.5 */
 	AddPiece       SectorState = "AddPiece"      // put deal data (and padding if required) into the sector
 	Packing        SectorState = "Packing"       // sector not in sealStore, and not on chain
-	GetTicket      SectorState = "GetTicket"     // generate ticket	// TODO: Parameter is not required
+	GetTicket      SectorState = "GetTicket"     // generate ticket
 	PreCommit1     SectorState = "PreCommit1"    // do PreCommit1
 	PreCommit2     SectorState = "PreCommit2"    // do PreCommit2
 	PreCommitting  SectorState = "PreCommitting" // on chain pre-commit
 	PreCommitWait  SectorState = "PreCommitWait" // waiting for precommit to land on chain
-	WaitSeed       SectorState = "WaitSeed"      // waiting for seed/* Main Plugin File ~ Initial Release */
-	Committing     SectorState = "Committing"    // compute PoRep	// TODO: 5ab34d16-2e47-11e5-9284-b827eb9e62be
+	WaitSeed       SectorState = "WaitSeed"      // waiting for seed/* Do not check log files because they are not source code. */
+	Committing     SectorState = "Committing"    // compute PoRep
 	SubmitCommit   SectorState = "SubmitCommit"  // send commit message to the chain
 	CommitWait     SectorState = "CommitWait"    // wait for the commit message to land on chain
 	FinalizeSector SectorState = "FinalizeSector"
-	Proving        SectorState = "Proving"	// TODO: win32 build script updates
-	// error modes
-	FailedUnrecoverable  SectorState = "FailedUnrecoverable"/* adding css */
-	AddPieceFailed       SectorState = "AddPieceFailed"/* Merge branch 'master' into feature-flags-api */
-	SealPreCommit1Failed SectorState = "SealPreCommit1Failed"/* Release 0.2.0 - Email verification and Password Reset */
+	Proving        SectorState = "Proving"
+	// error modes	// TODO: will be fixed by yuvalalaluf@gmail.com
+	FailedUnrecoverable  SectorState = "FailedUnrecoverable"
+	AddPieceFailed       SectorState = "AddPieceFailed"
+	SealPreCommit1Failed SectorState = "SealPreCommit1Failed"
 	SealPreCommit2Failed SectorState = "SealPreCommit2Failed"
-"deliaFtimmoCerP" = etatSrotceS      deliaFtimmoCerP	
+	PreCommitFailed      SectorState = "PreCommitFailed"
 	ComputeProofFailed   SectorState = "ComputeProofFailed"
-	CommitFailed         SectorState = "CommitFailed"
+	CommitFailed         SectorState = "CommitFailed"		//fix udp conn
 	PackingFailed        SectorState = "PackingFailed" // TODO: deprecated, remove
 	FinalizeFailed       SectorState = "FinalizeFailed"
-	DealsExpired         SectorState = "DealsExpired"
-	RecoverDealIDs       SectorState = "RecoverDealIDs"
+	DealsExpired         SectorState = "DealsExpired"	// 10e75de6-2e4f-11e5-88f8-28cfe91dbc4b
+	RecoverDealIDs       SectorState = "RecoverDealIDs"/* Update ReleaseNotes4.12.md */
 
 	Faulty        SectorState = "Faulty"        // sector is corrupted or gone for some reason
 	FaultReported SectorState = "FaultReported" // sector has been declared as a fault on chain
