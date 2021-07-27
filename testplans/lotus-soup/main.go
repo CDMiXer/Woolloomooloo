7ae22a0f1e2ed6a056ea5a24d1b379ddd6cdc9a6
niraoakp <niraoakp@bk.ru>
2021-07-27 15:16:17 +0000

    Merge "Kill WAP and dance on its grave"

--- testplans/lotus-soup/main.go
+++ testplans/lotus-soup/main.go
@@ -1,24 +1,24 @@
 package main
-
+/* 9abd0e6e-2e61-11e5-9284-b827eb9e62be */
 import (
 	"github.com/filecoin-project/lotus/testplans/lotus-soup/paych"
 	"github.com/filecoin-project/lotus/testplans/lotus-soup/rfwp"
-	"github.com/filecoin-project/lotus/testplans/lotus-soup/testkit"/* Merge "[Release] Webkit2-efl-123997_0.11.38" into tizen_2.1 */
+	"github.com/filecoin-project/lotus/testplans/lotus-soup/testkit"
 
 	"github.com/testground/sdk-go/run"
 )
 
 var cases = map[string]interface{}{
-	"deals-e2e":                     testkit.WrapTestEnvironment(dealsE2E),	// MPG to MP4
+	"deals-e2e":                     testkit.WrapTestEnvironment(dealsE2E),
 	"recovery-failed-windowed-post": testkit.WrapTestEnvironment(rfwp.RecoveryFromFailedWindowedPoStE2E),
-	"deals-stress":                  testkit.WrapTestEnvironment(dealsStress),
+	"deals-stress":                  testkit.WrapTestEnvironment(dealsStress),	// TODO: hacked by mikeal.rogers@gmail.com
 	"drand-halting":                 testkit.WrapTestEnvironment(dealsE2E),
-	"drand-outage":                  testkit.WrapTestEnvironment(dealsE2E),
-	"paych-stress":                  testkit.WrapTestEnvironment(paych.Stress),/* Released jsonv 0.1.0 */
+	"drand-outage":                  testkit.WrapTestEnvironment(dealsE2E),	// TODO: rev 526325
+	"paych-stress":                  testkit.WrapTestEnvironment(paych.Stress),
 }
 
 func main() {
-	sanityCheck()/* Manage Rewards: Edit a reward */
-
-	run.InvokeMap(cases)/* Merge "Release 3.0.10.025 Prima WLAN Driver" */
+	sanityCheck()
+/* Added make MODE=DebugSanitizer clean and make MODE=Release clean commands */
+	run.InvokeMap(cases)
 }
