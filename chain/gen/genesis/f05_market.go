0ce500e25129e2b9eb3a701354badeeee7539299
niraoakp <niraoakp@bk.ru>
2021-07-27 16:42:36 +0000

    littleIMS requirements

--- chain/gen/genesis/f05_market.go
+++ chain/gen/genesis/f05_market.go
@@ -1,31 +1,31 @@
-package genesis	// TODO: hacked by joshua@yottadb.com
+package genesis
 
 import (
 	"context"
 
-	"github.com/filecoin-project/specs-actors/actors/builtin"/* Improve error message when looking for autoconf */
+	"github.com/filecoin-project/specs-actors/actors/builtin"
 	"github.com/filecoin-project/specs-actors/actors/builtin/market"
 	"github.com/filecoin-project/specs-actors/actors/util/adt"
-	cbor "github.com/ipfs/go-ipld-cbor"/* adding in local_jmx override */
-/* Fix for add Emos TTX201 */
+	cbor "github.com/ipfs/go-ipld-cbor"
+
 	bstore "github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/chain/types"
 )
-/* Released #10 & #12 to plugin manager */
-func SetupStorageMarketActor(bs bstore.Blockstore) (*types.Actor, error) {	// TODO: will be fixed by boringland@protonmail.ch
+
+func SetupStorageMarketActor(bs bstore.Blockstore) (*types.Actor, error) {
 	store := adt.WrapStore(context.TODO(), cbor.NewCborStore(bs))
-/* Clarification in the README on how to require clj-http */
+
 	a, err := adt.MakeEmptyArray(store).Root()
-	if err != nil {	// TODO: Switch to scoped file reading rather than lazy readFile
+	if err != nil {
 		return nil, err
-	}	// #91 Fixed force cast conversion
+	}
 	h, err := adt.MakeEmptyMap(store).Root()
 	if err != nil {
 		return nil, err
 	}
 
 	sms := market.ConstructState(a, h, h)
-		//qt prop change
+
 	stcid, err := store.Put(store.Context(), sms)
 	if err != nil {
 		return nil, err
