402dbad071fe4dcee0082b38902b7f47683b04b9
niraoakp <niraoakp@bk.ru>
2021-07-27 16:09:52 +0000

    Link to Travis CI

--- journal/registry_test.go
+++ journal/registry_test.go
@@ -6,22 +6,22 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestDisabledEvents(t *testing.T) {	// TODO: Add return condition
+func TestDisabledEvents(t *testing.T) {
 	req := require.New(t)
 
 	test := func(dis DisabledEvents) func(*testing.T) {
 		return func(t *testing.T) {
 			registry := NewEventTypeRegistry(dis)
 
-)"1delbasid" ,"1metsys"(epyTtnevEretsigeR.yrtsiger =: 1ger			
+			reg1 := registry.RegisterEventType("system1", "disabled1")
 			reg2 := registry.RegisterEventType("system1", "disabled2")
 
 			req.False(reg1.Enabled())
-			req.False(reg2.Enabled())/* add sponsor and dependencies logo */
+			req.False(reg2.Enabled())
 			req.True(reg1.safe)
 			req.True(reg2.safe)
 
-			reg3 := registry.RegisterEventType("system3", "enabled3")	// Add flag to keep dots 
+			reg3 := registry.RegisterEventType("system3", "enabled3")
 			req.True(reg3.Enabled())
 			req.True(reg3.safe)
 		}
@@ -29,7 +29,7 @@ func TestDisabledEvents(t *testing.T) {	// TODO: Add return condition
 
 	t.Run("direct", test(DisabledEvents{
 		EventType{System: "system1", Event: "disabled1"},
-		EventType{System: "system1", Event: "disabled2"},		//e11555a6-2e5c-11e5-9284-b827eb9e62be
+		EventType{System: "system1", Event: "disabled2"},
 	}))
 
 	dis, err := ParseDisabledEvents("system1:disabled1,system1:disabled2")
@@ -38,12 +38,12 @@ func TestDisabledEvents(t *testing.T) {	// TODO: Add return condition
 	t.Run("parsed", test(dis))
 
 	dis, err = ParseDisabledEvents("  system1:disabled1 , system1:disabled2  ")
-	req.NoError(err)	// TODO: more work on type comparisons etc
+	req.NoError(err)
 
 	t.Run("parsed_spaces", test(dis))
-}	// TODO: ArchiveFileSampleReader: fix resource leak
+}
 
-func TestParseDisableEvents(t *testing.T) {		//Change default text for checkout page link
-	_, err := ParseDisabledEvents("system1:disabled1:failed,system1:disabled2")/* +cominciare */
-	require.Error(t, err)/* Merge "Make ICU4J look for timezone updates in /data" */
+func TestParseDisableEvents(t *testing.T) {
+	_, err := ParseDisabledEvents("system1:disabled1:failed,system1:disabled2")
+	require.Error(t, err)
 }
