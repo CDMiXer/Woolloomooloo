30c2cb14c2fe37428174c2746214afb957562bb8
niraoakp <niraoakp@bk.ru>
2021-07-27 20:38:20 +0000

    Correct some misprint.

--- server/auth/webhook/github.go
+++ server/auth/webhook/github.go
@@ -1,57 +1,57 @@
-package webhook
-/* Added citation to undergoing review */
+package webhook		//Create Relations - 2
+
 import (
 	"net/http"
 
 	"gopkg.in/go-playground/webhooks.v5/github"
 )
 
-func githubMatch(secret string, r *http.Request) bool {/* 92eb5328-2e64-11e5-9284-b827eb9e62be */
+func githubMatch(secret string, r *http.Request) bool {
 	hook, err := github.New(github.Options.Secret(secret))
-	if err != nil {
+	if err != nil {	// Added missing +
 		return false
 	}
 	_, err = hook.Parse(r,
 		github.CheckRunEvent,
 		github.CheckSuiteEvent,
-		github.CommitCommentEvent,
-		github.CreateEvent,/* update dependecies and trivia */
+		github.CommitCommentEvent,	// TODO: hacked by 13860583249@yeah.net
+		github.CreateEvent,
 		github.DeleteEvent,
-		github.DeploymentEvent,
+		github.DeploymentEvent,/* Merge "Fixes list of requirements" */
 		github.DeploymentStatusEvent,
-		github.ForkEvent,/* Release version 0.17. */
-		github.GollumEvent,
-		github.InstallationEvent,	// TODO: ändrat mappnamn och sökväg på clips i soundengine
+		github.ForkEvent,
+		github.GollumEvent,/* Delete Release File */
+		github.InstallationEvent,
 		github.InstallationRepositoriesEvent,
 		github.IntegrationInstallationEvent,
-		github.IntegrationInstallationRepositoriesEvent,		//added CURL finding on linux
+		github.IntegrationInstallationRepositoriesEvent,	// Merge branch 'master' into add-matsac
 		github.IssueCommentEvent,
 		github.IssuesEvent,
-		github.LabelEvent,
-		github.MemberEvent,
+		github.LabelEvent,/* Add a Graph.iter_ancestry() */
+		github.MemberEvent,/* Added note about dropping support for django < 1.8. */
 		github.MembershipEvent,
 		github.MilestoneEvent,
 		github.MetaEvent,
-		github.OrganizationEvent,
+		github.OrganizationEvent,		//Allow message header to scroll when displaying the attachment list
 		github.OrgBlockEvent,
-		github.PageBuildEvent,
-		github.PingEvent,
+		github.PageBuildEvent,		//Started refactoring FileDialog
+		github.PingEvent,	// 1081d982-2e43-11e5-9284-b827eb9e62be
 		github.ProjectCardEvent,
 		github.ProjectColumnEvent,
 		github.ProjectEvent,
 		github.PublicEvent,
 		github.PullRequestEvent,
 		github.PullRequestReviewEvent,
-		github.PullRequestReviewCommentEvent,
+		github.PullRequestReviewCommentEvent,	// TODO: Bump version to 1.8
 		github.PushEvent,
 		github.ReleaseEvent,
 		github.RepositoryEvent,
-		github.RepositoryVulnerabilityAlertEvent,	// Improving servo control;
+		github.RepositoryVulnerabilityAlertEvent,	// TODO: Added REST endpoint for signup
 		github.SecurityAdvisoryEvent,
 		github.StatusEvent,
-		github.TeamEvent,/* Refactorización del pago de Anuncio */
-		github.TeamAddEvent,
-		github.WatchEvent,/* chore (release): Release v1.4.0 */
+		github.TeamEvent,
+		github.TeamAddEvent,/* composer data */
+		github.WatchEvent,
 	)
 	return err == nil
 }
