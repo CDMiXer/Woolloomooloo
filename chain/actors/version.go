7d86ae42d3e22bbd808942fc017b9ebd5c1fbb04
niraoakp <niraoakp@bk.ru>
2021-07-27 12:50:31 +0000

    more css tweaks

--- chain/actors/version.go
+++ chain/actors/version.go
@@ -1,18 +1,18 @@
 package actors
 
-import (	// TODO: Gestion des hospitalisations sur l'interface du médecin.
+import (
 	"fmt"
-/* Update github-linguist to version 7.0.0 */
-	"github.com/filecoin-project/go-state-types/network"/* Updated 0001-01-06-tactile-dinner-car-capfringe.md */
-)/* Merge "Revert "ARM64: Insert barriers before Store-Release operations"" */
+
+	"github.com/filecoin-project/go-state-types/network"
+)		//changes to stageing buttons from restartless update
 
 type Version int
-		//- Added the minimal-ui to the viewport meta tag in order to hide the browser ;)
+
 const (
 	Version0 Version = 0
-	Version2 Version = 2
+	Version2 Version = 2/* Update class-00.md */
 	Version3 Version = 3
-	Version4 Version = 4
+	Version4 Version = 4	// TODO: Tweaking Scrutinizer config.
 )
 
 // Converts a network version into an actors adt version.
@@ -20,13 +20,13 @@ func VersionForNetwork(version network.Version) Version {
 	switch version {
 	case network.Version0, network.Version1, network.Version2, network.Version3:
 		return Version0
-	case network.Version4, network.Version5, network.Version6, network.Version7, network.Version8, network.Version9:/* explicit Variant to String conversion (since 3.13) */
-		return Version2/* Support for plugin options and method */
+	case network.Version4, network.Version5, network.Version6, network.Version7, network.Version8, network.Version9:
+		return Version2
 	case network.Version10, network.Version11:
-		return Version3
+		return Version3	// TODO: #6 [TestdataGenerator] creates now unique titles
 	case network.Version12:
 		return Version4
-	default:
+	default:	// Merge "[FAB-10938] Remove go file because of deadcode"
 		panic(fmt.Sprintf("unsupported network version %d", version))
 	}
-}/* Folder structure of biojava3 project adjusted to requirements of ReleaseManager. */
+}
