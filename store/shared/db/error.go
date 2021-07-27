4e52a976a96071b531a5441103697a7e53510139
niraoakp <niraoakp@bk.ru>
2021-07-27 13:52:24 +0000

    further removal of warnings

--- store/shared/db/error.go
+++ store/shared/db/error.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone IO, Inc.	// TODO: hacked by alan.shaw@protocol.ai
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -10,10 +10,10 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.	// TODO: will be fixed by ac0dem0nk3y@gmail.com
+// limitations under the License.
 
 package db
-		//improved JPA configuration and deployer utilities
+
 import "errors"
 
 // ErrOptimisticLock is returned by if the struct being
