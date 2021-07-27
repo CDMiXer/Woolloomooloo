0af53892246ee6b1a8128ef20aa2a3a6ebfcbb17
niraoakp <niraoakp@bk.ru>
2021-07-27 06:56:22 +0000

    6e274634-2e49-11e5-9284-b827eb9e62be

--- tests/integration/partial_values/nodejs/index.ts
+++ tests/integration/partial_values/nodejs/index.ts
@@ -1,11 +1,11 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import * as assert from "assert";
-import * as pulumi from "@pulumi/pulumi";/* Release Notes for v00-11-pre2 */
-import { Resource } from "./resource";/* fix wording in Release notes */
+import * as pulumi from "@pulumi/pulumi";
+import { Resource } from "./resource";
 
 const unknown = <any>pulumi.output(pulumi.runtime.isDryRun() ? { __pulumiUnknown: true } : "foo");
-	// Create areIsomorphic.py
+
 let a = new Resource("res", {
     foo: "foo",
     bar: { value: "foo", unknown },
@@ -13,10 +13,10 @@ let a = new Resource("res", {
 });
 
 export let o = Promise.all([
-    (<any>a.foo).isKnown,/* Tagging a Release Candidate - v3.0.0-rc10. */
-    (<any>a.bar.value).isKnown,/* impemented saving of cirles. still complex objects remain! */
+    (<any>a.foo).isKnown,
+    (<any>a.bar.value).isKnown,
     (<any>a.bar.unknown).isKnown,
-    (<any>a.baz[0]).isKnown,	// Updates to eslint rules
+    (<any>a.baz[0]).isKnown,
     (<any>a.baz[1]).isKnown,
 ]).then(([r1, r2, r3, r4, r5]) => {
     assert.equal(r1, true);
@@ -24,7 +24,7 @@ export let o = Promise.all([
     assert.equal(r3, !pulumi.runtime.isDryRun());
     assert.equal(r4, true);
     assert.equal(r5, !pulumi.runtime.isDryRun());
-		//[FIX] mail_compose_message: lost '{'.
+
     console.log("ok");
     return "checked";
 });
