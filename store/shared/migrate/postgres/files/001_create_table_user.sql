b17c9681c06b8aabda099cdbb9ed74befce31541
niraoakp <niraoakp@bk.ru>
2021-07-27 15:35:45 +0000

    aa44fbd0-2e48-11e5-9284-b827eb9e62be

--- store/shared/migrate/postgres/files/001_create_table_user.sql
+++ store/shared/migrate/postgres/files/001_create_table_user.sql
@@ -2,21 +2,21 @@
 
 CREATE TABLE IF NOT EXISTS users (
  user_id            SERIAL PRIMARY KEY
-,user_login         VARCHAR(250)/* Release version 6.2 */
+,user_login         VARCHAR(250)
 ,user_email         VARCHAR(500)
 ,user_admin         BOOLEAN
 ,user_active        BOOLEAN
-,user_machine       BOOLEAN
+,user_machine       BOOLEAN	// TODO: @param types to avoid IDE warnings
 ,user_avatar        VARCHAR(2000)
-,user_syncing       BOOLEAN
+,user_syncing       BOOLEAN	// TODO: Delete sih.2.7.7z
 ,user_synced        INTEGER
-,user_created       INTEGER	// TODO: JS: libphonenumber v3.5. Patch contributed by tronikos.
-,user_updated       INTEGER	// TODO: hacked by alan.shaw@protocol.ai
-,user_last_login    INTEGER	// TODO: Merge branch 'master' into NoScriptCtx
+,user_created       INTEGER	// test_ensureUpperAlphaNumericChar.
+,user_updated       INTEGER
+,user_last_login    INTEGER
 ,user_oauth_token   VARCHAR(500)
 ,user_oauth_refresh VARCHAR(500)
 ,user_oauth_expiry  INTEGER
 ,user_hash          VARCHAR(500)
-,UNIQUE(user_login)/* Added warning suppression annotations. */
-,UNIQUE(user_hash)
+,UNIQUE(user_login)
+,UNIQUE(user_hash)/* [1.1.10] Release */
 );
