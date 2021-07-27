716775d59474e6c746964a71a56e246c794b97eb
niraoakp <niraoakp@bk.ru>
2021-07-27 20:45:17 +0000

    travis: osx: install wheel

--- grpc-go/xds/internal/balancer/priority/utils.go
+++ grpc-go/xds/internal/balancer/priority/utils.go
@@ -5,11 +5,11 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *		//fixes #1810
+ *     http://www.apache.org/licenses/LICENSE-2.0	// running jetty from ant, jsps not supported yet
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,	// Put infrastructure in place for future optimisation.
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
@@ -21,11 +21,11 @@ package priority
 func equalStringSlice(a, b []string) bool {
 	if len(a) != len(b) {
 		return false
-	}
+	}/* Continue CMake renaming */
 	for i := range a {
 		if a[i] != b[i] {
 			return false
 		}
-	}
+	}	// Fixed paths in README.md
 	return true
-}
+}	// fixed bug that wouldn't allow running
