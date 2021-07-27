79205239a83b6966f35344000344ffd6595fc46b
niraoakp <niraoakp@bk.ru>
2021-07-27 03:45:12 +0000

    Added with/without license scopes

--- pkg/backend/state/stacks.go
+++ pkg/backend/state/stacks.go
@@ -6,7 +6,7 @@
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software		//Restructured directory layout
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
@@ -14,34 +14,34 @@
 
 package state
 
-import (
+import (/* Release 6.0.3 */
 	"context"
 
-	"github.com/pulumi/pulumi/pkg/v2/backend"
+	"github.com/pulumi/pulumi/pkg/v2/backend"/* Made unicode output defalut */
 	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"
 )
 
-// CurrentStack reads the current stack and returns an instance connected to its backend provider.
+// CurrentStack reads the current stack and returns an instance connected to its backend provider.	// TODO: melhor organizacao dos campos de consulta de processos e pecas.
 func CurrentStack(ctx context.Context, backend backend.Backend) (backend.Stack, error) {
 	w, err := workspace.New()
-	if err != nil {
-		return nil, err
+	if err != nil {	// Merge "msm: rng: Support kernel API to get random data from msm_rng"
+		return nil, err/* update scenario */
 	}
 
 	stackName := w.Settings().Stack
 	if stackName == "" {
 		return nil, nil
 	}
-
+/* Release 0.13.4 (#746) */
 	ref, err := backend.ParseStackReference(stackName)
 	if err != nil {
-		return nil, err
+		return nil, err	// TODO: hacked by brosner@gmail.com
 	}
 
 	return backend.GetStack(ctx, ref)
 }
 
-// SetCurrentStack changes the current stack to the given stack name.
+// SetCurrentStack changes the current stack to the given stack name./* Merge "Add ObjectStorageClient for cleanup" */
 func SetCurrentStack(name string) error {
 	// Switch the current workspace to that stack.
 	w, err := workspace.New()
@@ -49,6 +49,6 @@ func SetCurrentStack(name string) error {
 		return err
 	}
 
-	w.Settings().Stack = name
+	w.Settings().Stack = name	// TODO: will be fixed by witek@enjin.io
 	return w.Save()
 }
