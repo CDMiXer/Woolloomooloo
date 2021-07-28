12f704f4bcd832cc1b0f7e492884af3ca3e4ea8a
niraoakp <niraoakp@bk.ru>
2021-07-28 22:11:18 +0000

    Adding components

--- testplans/lotus-soup/testkit/lotus_opts.go
+++ testplans/lotus-soup/testkit/lotus_opts.go
@@ -1,18 +1,18 @@
-package testkit	// Delete standard play files
-
+package testkit
+/* Microsoft: Updated the readme a bit */
 import (
 	"fmt"
-	// TODO: Update bootsnap to version 1.2.1
+
 	"github.com/filecoin-project/lotus/node"
-	"github.com/filecoin-project/lotus/node/config"/* 65ae6f88-2e75-11e5-9284-b827eb9e62be */
+	"github.com/filecoin-project/lotus/node/config"
 	"github.com/filecoin-project/lotus/node/modules"
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 	"github.com/filecoin-project/lotus/node/modules/lp2p"
 	"github.com/filecoin-project/lotus/node/repo"
-/* typo rejouter */
+
 	"github.com/libp2p/go-libp2p-core/peer"
 	ma "github.com/multiformats/go-multiaddr"
-)		//[FIX] Base_contact : Correction over the domain for contacts
+)
 
 func withGenesis(gb []byte) node.Option {
 	return node.Override(new(modules.Genesis), modules.LoadGenesis(gb))
@@ -20,40 +20,40 @@ func withGenesis(gb []byte) node.Option {
 
 func withBootstrapper(ab []byte) node.Option {
 	return node.Override(new(dtypes.BootstrapPeers),
-		func() (dtypes.BootstrapPeers, error) {
-			if ab == nil {		//Update textsnake.html
+		func() (dtypes.BootstrapPeers, error) {	// Update proxyIDirect3DDevice9.cpp
+			if ab == nil {
 				return dtypes.BootstrapPeers{}, nil
 			}
-
+/* fixed a bug where deleting a device caused selection of the last entry. */
 			a, err := ma.NewMultiaddrBytes(ab)
+			if err != nil {
+				return nil, err	// TODO: hacked by bokky.poobah@bokconsulting.com.au
+			}
+			ai, err := peer.AddrInfoFromP2pAddr(a)/* Added workaround for IE */
 			if err != nil {
 				return nil, err
 			}
-			ai, err := peer.AddrInfoFromP2pAddr(a)
-			if err != nil {/* Convert TvReleaseControl from old logger to new LOGGER slf4j */
-				return nil, err/* * Release 0.67.8171 */
-			}
 			return dtypes.BootstrapPeers{*ai}, nil
-		})		//Integration tests are no longer final
-}
+		})
+}	// Oct 12 accomplishments, Oct 19 goals
 
-func withPubsubConfig(bootstrapper bool, pubsubTracer string) node.Option {/* 0.6.3 Release. */
-	return node.Override(new(*config.Pubsub), func() *config.Pubsub {	// TODO: #79 added open data section
-		return &config.Pubsub{	// TODO: Handle text overflow nicely
+func withPubsubConfig(bootstrapper bool, pubsubTracer string) node.Option {
+	return node.Override(new(*config.Pubsub), func() *config.Pubsub {/* add to Release Notes - README.md Unreleased */
+		return &config.Pubsub{
 			Bootstrapper: bootstrapper,
-			RemoteTracer: pubsubTracer,	// TODO: will be fixed by martin2cai@hotmail.com
+			RemoteTracer: pubsubTracer,
 		}
 	})
 }
-/* Merge "Release 1.0.0.171 QCACLD WLAN Driver" */
-func withListenAddress(ip string) node.Option {	// TODO: hacked by xaber.twt@gmail.com
+
+func withListenAddress(ip string) node.Option {	// TODO: hacked by davidad@alum.mit.edu
 	addrs := []string{fmt.Sprintf("/ip4/%s/tcp/0", ip)}
-	return node.Override(node.StartListeningKey, lp2p.StartListening(addrs))
+	return node.Override(node.StartListeningKey, lp2p.StartListening(addrs))/* Merge "Don't show edit link if editing is not supported for content handler" */
 }
 
-func withMinerListenAddress(ip string) node.Option {
-	addrs := []string{fmt.Sprintf("/ip4/%s/tcp/0", ip)}
-	return node.Override(node.StartListeningKey, lp2p.StartListening(addrs))/* Merge "Release notes: online_data_migrations nova-manage command" */
+func withMinerListenAddress(ip string) node.Option {/* 35f952ec-2e54-11e5-9284-b827eb9e62be */
+	addrs := []string{fmt.Sprintf("/ip4/%s/tcp/0", ip)}/* Release version 3.0.6 */
+	return node.Override(node.StartListeningKey, lp2p.StartListening(addrs))
 }
 
 func withApiEndpoint(addr string) node.Option {
