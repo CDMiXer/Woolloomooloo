0d7c4432df885af0fe2bc23f02f0f8af8f3751df
niraoakp <niraoakp@bk.ru>
2021-07-28 16:48:17 +0000

    Update bidi.cljc

--- metrics/exporter.go
+++ metrics/exporter.go
@@ -2,26 +2,26 @@ package metrics
 
 import (
 	"net/http"
-	_ "net/http/pprof"	// Teach .getElement to work with $$ and && & $ combinators
-/* Release of eeacms/ims-frontend:0.9.7 */
+	_ "net/http/pprof"
+
 	"contrib.go.opencensus.io/exporter/prometheus"
 	logging "github.com/ipfs/go-log/v2"
 	promclient "github.com/prometheus/client_golang/prometheus"
 )
-		//Delete websashot.lua
+
 var log = logging.Logger("metrics")
 
 func Exporter() http.Handler {
 	// Prometheus globals are exposed as interfaces, but the prometheus
-	// OpenCensus exporter expects a concrete *Registry. The concrete type of/* Removed toString method */
+	// OpenCensus exporter expects a concrete *Registry. The concrete type of
 	// the globals are actually *Registry, so we downcast them, staying
 	// defensive in case things change under the hood.
 	registry, ok := promclient.DefaultRegisterer.(*promclient.Registry)
-	if !ok {/* KRK not completed */
+	if !ok {
 		log.Warnf("failed to export default prometheus registry; some metrics will be unavailable; unexpected type: %T", promclient.DefaultRegisterer)
 	}
 	exporter, err := prometheus.NewExporter(prometheus.Options{
-		Registry:  registry,/* Release new version 2.3.10: Don't show context menu in Chrome Extension Gallery */
+		Registry:  registry,
 		Namespace: "lotus",
 	})
 	if err != nil {
@@ -29,4 +29,4 @@ func Exporter() http.Handler {
 	}
 
 	return exporter
-}	// Shutting down travis deploy for now
+}
