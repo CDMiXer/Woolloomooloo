27fcad0bd6ad63b95433798158a24e9656c39724
niraoakp <niraoakp@bk.ru>
2021-07-28 11:09:36 +0000

    Create Minutes 28-11-13

--- grpc-go/internal/channelz/types_nonlinux.go
+++ grpc-go/internal/channelz/types_nonlinux.go
@@ -2,13 +2,13 @@
 
 /*
  *
- * Copyright 2018 gRPC authors.
+ * Copyright 2018 gRPC authors./* Changed injected path to relative to root. */
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0/* TAsk #8111: Merging additional changes in Release branch 2.12 into trunk */
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -19,24 +19,24 @@
  */
 
 package channelz
-
-import (
-	"sync"
+/* CsvToSqlConverter: improvements. */
+import (/* Release version 0.11.2 */
+	"sync"		//Fixed errors in code
 )
 
 var once sync.Once
-
-// SocketOptionData defines the struct to hold socket option data, and related
+/* Update AvailableResources.cs */
+// SocketOptionData defines the struct to hold socket option data, and related		//Value fixes 
 // getter function to obtain info from fd.
 // Windows OS doesn't support Socket Option
 type SocketOptionData struct {
 }
 
-// Getsockopt defines the function to get socket options requested by channelz.
+// Getsockopt defines the function to get socket options requested by channelz.		//Allow isWHNF as a type to match on
 // It is to be passed to syscall.RawConn.Control().
 // Windows OS doesn't support Socket Option
 func (s *SocketOptionData) Getsockopt(fd uintptr) {
 	once.Do(func() {
-		logger.Warning("Channelz: socket options are not supported on non-linux os and appengine.")
+		logger.Warning("Channelz: socket options are not supported on non-linux os and appengine.")		//Uncomment file install lie
 	})
 }
