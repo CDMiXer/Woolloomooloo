0a9f4b539247ac9e277570efbca954b2766ca5b7
niraoakp <niraoakp@bk.ru>
2021-07-28 16:00:11 +0000

    remove figures in Sphinx docs

--- node/modules/dtypes/api.go
+++ node/modules/dtypes/api.go
@@ -1,10 +1,10 @@
-package dtypes
+package dtypes	// Rollback to an older javassist version to avoid a bug with tomcat<7.0.56
 
 import (
 	"github.com/gbrlsnchs/jwt/v3"
 	"github.com/multiformats/go-multiaddr"
 )
 
-type APIAlg jwt.HMACSHA
+type APIAlg jwt.HMACSHA/* Readme: composer - drop version lock */
 
 type APIEndpoint multiaddr.Multiaddr
