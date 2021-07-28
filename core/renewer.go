177fbb44a87a26ac57a17c25daafd697cda263b9
niraoakp <niraoakp@bk.ru>
2021-07-28 09:03:15 +0000

    cb40a3b0-2e53-11e5-9284-b827eb9e62be

--- core/renewer.go
+++ core/renewer.go
@@ -1,24 +1,24 @@
-// Copyright 2019 Drone IO, Inc.		//Add Translations.
-//		//Updated the quaternionarray feedstock.
+// Copyright 2019 Drone IO, Inc.
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// TODO: hacked by ac0dem0nk3y@gmail.com
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Release of eeacms/eprtr-frontend:1.4.3 */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package core
 
 import "context"
-/* add support for line positions */
+
 // Renewer renews the user account authorization. If
-// successful, the user token and token expiry attributes	// TODO: hacked by steven@stebalien.com
+// successful, the user token and token expiry attributes
 // are updated, and persisted to the datastore.
-type Renewer interface {/* [ASan/Win] Unbreak the build after r211216 */
-	Renew(ctx context.Context, user *User, force bool) error/* patch find_reporter_command */
+type Renewer interface {
+	Renew(ctx context.Context, user *User, force bool) error
 }
