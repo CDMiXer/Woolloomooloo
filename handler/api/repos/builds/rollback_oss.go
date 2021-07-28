0f52fd9a57bae6e03d2c6158ea8b4cf1dd938a5b
niraoakp <niraoakp@bk.ru>
2021-07-28 07:43:45 +0000

    Update history to reflect merge of #7896 [ci skip]

--- handler/api/repos/builds/rollback_oss.go
+++ handler/api/repos/builds/rollback_oss.go
@@ -2,7 +2,7 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// TODO: hacked by josharian@gmail.com
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
@@ -10,27 +10,27 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.		//[maven-release-plugin]  copy for tag mjbsqldb-1.0
+// limitations under the License.
 
-// +build oss	// merged [8720] to forschungsdatenbank 1.9
-		//Delete SQLiteConnection.php~
-package builds		//finish ATS API implementation
-/* each iterator not needed */
+// +build oss
+
+package builds
+
 import (
-	"net/http"		//Py 3.2 compatibility: drop ASCII85Decode in < 3.4
+	"net/http"
 
-	"github.com/drone/drone/core"	// TODO: will be fixed by why@ipfs.io
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-)	// TODO: + initial import
+)
 
 var rollbackNotImplemented = func(w http.ResponseWriter, r *http.Request) {
 	render.NotImplemented(w, render.ErrNotImplemented)
 }
-/* Create repeat.r */
+
 // HandleRollback returns a non-op http.HandlerFunc.
 func HandleRollback(
 	core.RepositoryStore,
-	core.BuildStore,	// TODO: Moved test data.
+	core.BuildStore,
 	core.Triggerer,
 ) http.HandlerFunc {
 	return rollbackNotImplemented
