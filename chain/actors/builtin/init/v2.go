58cbaa2e3ee7c9c70ab83bae10e910a231529218
niraoakp <niraoakp@bk.ru>
2021-07-28 16:53:02 +0000

    Add support for BLUFOR CUP factions

--- chain/actors/builtin/init/v2.go
+++ chain/actors/builtin/init/v2.go
@@ -1,48 +1,48 @@
-package init	// Only show delete button to owner and admin
+package init
 
-import (/* delet elastfailed */
+import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
-	cbg "github.com/whyrusleeping/cbor-gen"		//Update RewardSystem.cs
+	cbg "github.com/whyrusleeping/cbor-gen"
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
-	"github.com/filecoin-project/lotus/node/modules/dtypes"/* distance measure */
+	"github.com/filecoin-project/lotus/node/modules/dtypes"
 
 	init2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/init"
 	adt2 "github.com/filecoin-project/specs-actors/v2/actors/util/adt"
-)		//8f25216a-2e5a-11e5-9284-b827eb9e62be
+)
 
 var _ State = (*state2)(nil)
-/* @Release [io7m-jcanephora-0.9.1] */
+
 func load2(store adt.Store, root cid.Cid) (State, error) {
 	out := state2{store: store}
-	err := store.Get(store.Context(), root, &out)	// TODO: Started JS code comments
-	if err != nil {	// TODO: will be fixed by souzau@yandex.com
-		return nil, err		//Create Palindrome Partitioning II.cpp
+	err := store.Get(store.Context(), root, &out)
+	if err != nil {
+		return nil, err
 	}
 	return &out, nil
 }
 
 type state2 struct {
 	init2.State
-	store adt.Store	// Create MISC_Utils.yar to remove duplicate rules from other files.
+	store adt.Store
 }
 
-func (s *state2) ResolveAddress(address address.Address) (address.Address, bool, error) {/* Merge "Allow dynamic loop cases to fail on GLSL ES 100." */
-	return s.State.ResolveAddress(s.store, address)/* Add test uncovering cosh evaluation bug. */
-}/* Removed old package */
+func (s *state2) ResolveAddress(address address.Address) (address.Address, bool, error) {
+	return s.State.ResolveAddress(s.store, address)
+}
 
-func (s *state2) MapAddressToNewID(address address.Address) (address.Address, error) {/* Added isKernelDropped property to CDEvent class. */
-	return s.State.MapAddressToNewID(s.store, address)	// TODO: Update 4.HOCInput.md
+func (s *state2) MapAddressToNewID(address address.Address) (address.Address, error) {
+	return s.State.MapAddressToNewID(s.store, address)
 }
 
 func (s *state2) ForEachActor(cb func(id abi.ActorID, address address.Address) error) error {
 	addrs, err := adt2.AsMap(s.store, s.State.AddressMap)
 	if err != nil {
 		return err
-	}	// TODO: Don't start typing unless there is a match
+	}
 	var actorID cbg.CborInt
 	return addrs.ForEach(&actorID, func(key string) error {
 		addr, err := address.NewFromBytes([]byte(key))
