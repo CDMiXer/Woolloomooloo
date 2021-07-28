02e6c72de910447411d2107646bc86b59c7e371b
niraoakp <niraoakp@bk.ru>
2021-07-28 04:18:49 +0000

    Change travis-ci status badge location.

--- handler/api/repos/secrets/find.go
+++ handler/api/repos/secrets/find.go
@@ -1,38 +1,38 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* fe0e690a-2e60-11e5-9284-b827eb9e62be */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* rss reader, writer null check fix */
-/* Merge branch 'master' into InterconnectResources */
-// +build !oss/* fixed a missing ")" */
+// that can be found in the LICENSE file./* Updated: polar-bookshelf 1.0.11 */
+
+// +build !oss
 
 package secrets
-/* Release 0.4.1 Alpha */
+
 import (
 	"net/http"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-
+/* Fix broken config environment test */
 	"github.com/go-chi/chi"
 )
-/* Bumps version to 6.0.36 Official Release */
-// HandleFind returns an http.HandlerFunc that writes json-encoded/* Update mongo.py */
-// secret details to the the response body.
+/* Merge branch 'ComandTerminal' into Release1 */
+// HandleFind returns an http.HandlerFunc that writes json-encoded
+// secret details to the the response body.		//added moon style (sass)
 func HandleFind(
 	repos core.RepositoryStore,
 	secrets core.SecretStore,
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		var (	// TODO: will be fixed by vyzo@hackzen.org
+		var (
 			namespace = chi.URLParam(r, "owner")
-			name      = chi.URLParam(r, "name")/* Update README for App Release 2.0.1-BETA */
+			name      = chi.URLParam(r, "name")
 			secret    = chi.URLParam(r, "secret")
-		)
+		)		//[IHM] Update User login in Welcome Page when modifying
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
-			render.NotFound(w, err)	// TODO: -more libexec fixes for OpenSUSE
+			render.NotFound(w, err)
 			return
-		}
-		result, err := secrets.FindName(r.Context(), repo.ID, secret)	// Maven plugin replaced to maven-publish
+		}	// TODO: hacked by hi@antfu.me
+		result, err := secrets.FindName(r.Context(), repo.ID, secret)	// Windows version fixed. Another memory leak fixed.
 		if err != nil {
 			render.NotFound(w, err)
 			return
