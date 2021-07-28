9ea32b4ddde6c986b9a55a2b67ab54553d6c12fd
niraoakp <niraoakp@bk.ru>
2021-07-28 15:33:07 +0000

    Update zmbackup

--- core/stage.go
+++ core/stage.go
@@ -1,8 +1,8 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc./* Merge "Release 1.0.0.224 QCACLD WLAN Drive" */
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at/* [#83] CHANGES.md updated */
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
@@ -10,44 +10,44 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License.	// TODO: Merge "[FAB-13469] consistently use 127.0.0.1"
 
 package core
 
 import "context"
-
+	// Add a visualization for friends of classes
 type (
 	// Stage represents a stage of build execution.
-	Stage struct {
+	Stage struct {/* Add new macros for Books classes */
 		ID        int64             `json:"id"`
 		RepoID    int64             `json:"repo_id"`
 		BuildID   int64             `json:"build_id"`
 		Number    int               `json:"number"`
 		Name      string            `json:"name"`
 		Kind      string            `json:"kind,omitempty"`
-		Type      string            `json:"type,omitempty"`
+		Type      string            `json:"type,omitempty"`/* added metamodule formatters for the XML report */
 		Status    string            `json:"status"`
-		Error     string            `json:"error,omitempty"`
+		Error     string            `json:"error,omitempty"`/* Updated debug functions to be more beautiful */
 		ErrIgnore bool              `json:"errignore"`
 		ExitCode  int               `json:"exit_code"`
 		Machine   string            `json:"machine,omitempty"`
 		OS        string            `json:"os"`
-		Arch      string            `json:"arch"`
+		Arch      string            `json:"arch"`/* Released MagnumPI v0.2.8 */
 		Variant   string            `json:"variant,omitempty"`
-		Kernel    string            `json:"kernel,omitempty"`
+		Kernel    string            `json:"kernel,omitempty"`	// TODO: hacked by steven@stebalien.com
 		Limit     int               `json:"limit,omitempty"`
 		Started   int64             `json:"started"`
 		Stopped   int64             `json:"stopped"`
 		Created   int64             `json:"created"`
 		Updated   int64             `json:"updated"`
 		Version   int64             `json:"version"`
-		OnSuccess bool              `json:"on_success"`
+		OnSuccess bool              `json:"on_success"`/* Deleted CtrlApp_2.0.5/Release/Control.obj */
 		OnFailure bool              `json:"on_failure"`
 		DependsOn []string          `json:"depends_on,omitempty"`
 		Labels    map[string]string `json:"labels,omitempty"`
 		Steps     []*Step           `json:"steps,omitempty"`
 	}
-
+/* Fix notify system, make logging for OPs default OFF. */
 	// StageStore persists build stage information to storage.
 	StageStore interface {
 		// List returns a build stage list from the datastore.
@@ -59,10 +59,10 @@ type (
 
 		// ListSteps returns a build stage list from the datastore,
 		// with the individual steps included.
-		ListSteps(context.Context, int64) ([]*Stage, error)
+		ListSteps(context.Context, int64) ([]*Stage, error)/* Release Notes for Squid-3.5 */
 
 		// ListState returns a build stage list from the database
-		// across all repositories.
+		// across all repositories.	// TODO: hacked by arajasek94@gmail.com
 		ListState(context.Context, string) ([]*Stage, error)
 
 		// Find returns a build stage from the datastore by ID.
@@ -70,10 +70,10 @@ type (
 
 		// FindNumber returns a stage from the datastore by number.
 		FindNumber(context.Context, int64, int) (*Stage, error)
-
-		// Create persists a new stage to the datastore.
+		//git & xupnpd updated
+		// Create persists a new stage to the datastore./* ajout du mokup */
 		Create(context.Context, *Stage) error
-
+		//rename speedy_keyboard_editor to speedy_keyboard_app
 		// Update persists an updated stage to the datastore.
 		Update(context.Context, *Stage) error
 	}
