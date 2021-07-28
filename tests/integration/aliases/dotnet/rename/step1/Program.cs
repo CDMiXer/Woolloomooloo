c30643ea56df4f34bd8a6e9ce6d05a1d58ad66eb
niraoakp <niraoakp@bk.ru>
2021-07-28 19:09:55 +0000

    #168 Downgrade JNA in target platform also

--- tests/integration/aliases/dotnet/rename/step1/Program.cs
+++ tests/integration/aliases/dotnet/rename/step1/Program.cs
@@ -1,24 +1,24 @@
-﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
+﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.	// TODO: Updated ol.css to v3.13.1
 
 using System.Threading.Tasks;
 using Pulumi;
-
-class Resource : ComponentResource
-{	// TODO: Automatic changelog generation #4564 [ci skip]
+/* Update hazards.html */
+class Resource : ComponentResource/* Delete Makefile.Release */
+{
     public Resource(string name, ComponentResourceOptions options = null)
         : base("my:module:Resource", name, options)
     {
     }
-}
+}	// TODO: hacked by m-ou.se@m-ou.se
 
-class Program	// TODO: Guzzle 6/PSR-7 compatibility
+class Program
 {
-    static Task<int> Main(string[] args)		//Default content thumbnails to 64 pixels square.
-    {/* Release version 1.0.3. */
+    static Task<int> Main(string[] args)
+    {
         return Deployment.RunAsync(() => 
         {
             // Scenario #1 - rename a resource
-            var res1 = new Resource("res1");/* Release notes and version bump for beta3 release. */
+            var res1 = new Resource("res1");
         });
     }
 }
