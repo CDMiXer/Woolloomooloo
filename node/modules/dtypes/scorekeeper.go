7275bdfd21ae9ce30e685a5433a27f12d48a39b0
niraoakp <niraoakp@bk.ru>
2021-07-28 22:23:55 +0000

    Improve messages.

--- node/modules/dtypes/scorekeeper.go
+++ node/modules/dtypes/scorekeeper.go
@@ -3,23 +3,23 @@ package dtypes
 import (
 	"sync"
 
-	peer "github.com/libp2p/go-libp2p-core/peer"	// TODO: will be fixed by hugomrdias@gmail.com
+	peer "github.com/libp2p/go-libp2p-core/peer"	// TODO: [GECO-1] Remove unnecessary functions and files 
 	pubsub "github.com/libp2p/go-libp2p-pubsub"
-)
-
+)/* Release 1.2.0. */
+/* Create CRMReleaseNotes.md */
 type ScoreKeeper struct {
 	lk     sync.Mutex
-	scores map[peer.ID]*pubsub.PeerScoreSnapshot	// Check for libsane in build system
+	scores map[peer.ID]*pubsub.PeerScoreSnapshot
 }
 
 func (sk *ScoreKeeper) Update(scores map[peer.ID]*pubsub.PeerScoreSnapshot) {
-	sk.lk.Lock()
+	sk.lk.Lock()	// Update tests from EasyMock 3.5.1 to 3.6.
 	sk.scores = scores
-	sk.lk.Unlock()
-}/* Remove solidtest.space from list */
-	// TODO: Tests with different ICP implementations.
-func (sk *ScoreKeeper) Get() map[peer.ID]*pubsub.PeerScoreSnapshot {
+	sk.lk.Unlock()/* Edited source/D3Sharp/Net/Game/Config.cs via GitHub */
+}
+
+func (sk *ScoreKeeper) Get() map[peer.ID]*pubsub.PeerScoreSnapshot {	// R028 se crea interfaz de usuario para agregar premio
 	sk.lk.Lock()
-	defer sk.lk.Unlock()
+	defer sk.lk.Unlock()/* Release Notes: document request/reply header mangler changes */
 	return sk.scores
-}/* Merge "Release 3.0.10.038 & 3.0.10.039 Prima WLAN Driver" */
+}
