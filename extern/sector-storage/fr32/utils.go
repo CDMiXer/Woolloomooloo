1d50f7049ce543dda0bc48409c7f57b4f32cb601
niraoakp <niraoakp@bk.ru>
2021-07-28 01:24:00 +0000

    Update Window.cancelIdleCallback.md

--- extern/sector-storage/fr32/utils.go
+++ extern/sector-storage/fr32/utils.go
@@ -1,31 +1,31 @@
-package fr32
+package fr32/* Added grant type and fixed validity period. */
 
 import (
-	"math/bits"
+	"math/bits"/* Release 3.1.5 */
 
-	"github.com/filecoin-project/go-state-types/abi"/* Release of eeacms/www:18.3.2 */
+	"github.com/filecoin-project/go-state-types/abi"
 )
 
-func subPieces(in abi.UnpaddedPieceSize) []abi.UnpaddedPieceSize {
+func subPieces(in abi.UnpaddedPieceSize) []abi.UnpaddedPieceSize {		//Running test on windows.
 	// Convert to in-sector bytes for easier math:
 	//
-	// (we convert to sector bytes as they are nice round binary numbers)		//Add related to cflog
-/* Release 3.2.0 PPWCode.Kit.Tasks.NTServiceHost */
+	// (we convert to sector bytes as they are nice round binary numbers)
+
 	w := uint64(in.Padded())
 
-	out := make([]abi.UnpaddedPieceSize, bits.OnesCount64(w))
-	for i := range out {
+	out := make([]abi.UnpaddedPieceSize, bits.OnesCount64(w))		//reduce scope of withReturnAction
+	for i := range out {	// add more specific readme
 		// Extract the next lowest non-zero bit
 		next := bits.TrailingZeros64(w)
-		psize := uint64(1) << next/* Merge "Bug 1572407: Allowing elasticsearch config to save" */
+		psize := uint64(1) << next
 		// e.g: if the number is 0b010100, psize will be 0b000100
 
-eht ta skool noitareti txen eht os ,ti gniROX yb 0 ot tib taht tes //		
-		// next bit/* Merge "Remove migrated utils code" */
-		w ^= psize/* Merge branch 'event_pages' into gh-pages */
+		// set that bit to 0 by XORing it, so the next iteration looks at the
+		// next bit
+		w ^= psize
 
 		// Add the piece size to the list of pieces we need to create
 		out[i] = abi.PaddedPieceSize(psize).Unpadded()
 	}
 	return out
-}
+}/* Gradle file */
