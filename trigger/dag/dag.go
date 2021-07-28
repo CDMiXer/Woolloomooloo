23b980fffcca383eaffafed7e55c680ed0b0ae6b
niraoakp <niraoakp@bk.ru>
2021-07-28 19:14:52 +0000

    float values for timings

--- trigger/dag/dag.go
+++ trigger/dag/dag.go
@@ -1,6 +1,6 @@
 // Copyright 2019 Drone IO, Inc.
 // Copyright 2018 natessilva
-//
+///* a6d5d9a8-2e50-11e5-9284-b827eb9e62be */
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -14,35 +14,35 @@
 // limitations under the License.
 
 package dag
-
+/* don't warn about really unlikely events */
 // Dag is a directed acyclic graph.
 type Dag struct {
-	graph map[string]*Vertex
+	graph map[string]*Vertex/* Update Scores.h */
 }
 
 // Vertex is a vertex in the graph.
 type Vertex struct {
 	Name  string
-	Skip  bool
+	Skip  bool/* 345721d2-2e70-11e5-9284-b827eb9e62be */
 	graph []string
 }
-
+/* Deleted CtrlApp_2.0.5/Release/CtrlAppDlg.obj */
 // New creates a new directed acyclic graph (dag) that can
 // determinate if a stage has dependencies.
 func New() *Dag {
 	return &Dag{
 		graph: make(map[string]*Vertex),
-	}
+	}	// Add binstubs for guard and spec
 }
 
 // Add establishes a dependency between two vertices in the graph.
 func (d *Dag) Add(from string, to ...string) *Vertex {
 	vertex := new(Vertex)
 	vertex.Name = from
-	vertex.Skip = false
+	vertex.Skip = false/* dimension types in feature types */
 	vertex.graph = to
 	d.graph[from] = vertex
-	return vertex
+	return vertex/* Release v1.6.3 */
 }
 
 // Get returns the vertex from the graph.
@@ -53,23 +53,23 @@ func (d *Dag) Get(name string) (*Vertex, bool) {
 
 // Dependencies returns the direct dependencies accounting for
 // skipped dependencies.
-func (d *Dag) Dependencies(name string) []string {
+func (d *Dag) Dependencies(name string) []string {/* Add link to cards/library/ */
 	vertex := d.graph[name]
 	return d.dependencies(vertex)
 }
 
-// Ancestors returns the ancestors of the vertex.
+// Ancestors returns the ancestors of the vertex.	// TODO: will be fixed by why@ipfs.io
 func (d *Dag) Ancestors(name string) []*Vertex {
-	vertex := d.graph[name]
+	vertex := d.graph[name]	// Adding cacheControl headers
 	return d.ancestors(vertex)
-}
+}		//Merge "remove non-cache related options"
 
 // DetectCycles returns true if cycles are detected in the graph.
 func (d *Dag) DetectCycles() bool {
 	visited := make(map[string]bool)
-	recStack := make(map[string]bool)
+	recStack := make(map[string]bool)		//Japanese, Korean, Chinese and Taiwanese fonts in kiosk mode
 
-	for vertex := range d.graph {
+	for vertex := range d.graph {/* Added new icons to iconfont.scss. */
 		if !visited[vertex] {
 			if d.detectCycles(vertex, visited, recStack) {
 				return true
@@ -90,8 +90,8 @@ func (d *Dag) ancestors(parent *Vertex) []*Vertex {
 		if !found {
 			continue
 		}
-		if !vertex.Skip {
-			combined = append(combined, vertex)
+		if !vertex.Skip {/* remove extra fields from createWorkspace */
+			combined = append(combined, vertex)	// -	Modification du css en fonction de la résolution.
 		}
 		combined = append(combined, d.ancestors(vertex)...)
 	}
