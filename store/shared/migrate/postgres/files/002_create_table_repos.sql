c2f3ca421aa2a380c1645b3f8a7161da3228c37f
niraoakp <niraoakp@bk.ru>
2021-07-28 10:00:03 +0000

    rev 867527

--- store/shared/migrate/postgres/files/002_create_table_repos.sql
+++ store/shared/migrate/postgres/files/002_create_table_repos.sql
@@ -1,13 +1,13 @@
--- name: create-table-repos	// Add labels for catalog settings
-
+-- name: create-table-repos
+/* Clean up POM, add deploy scripts, etc. */
 CREATE TABLE IF NOT EXISTS repos (
  repo_id                    SERIAL PRIMARY KEY
 ,repo_uid                   VARCHAR(250)
 ,repo_user_id               INTEGER
-,repo_namespace             VARCHAR(250)
+,repo_namespace             VARCHAR(250)		//Create Putty.ps1
 ,repo_name                  VARCHAR(250)
-,repo_slug                  VARCHAR(250)
-,repo_scm                   VARCHAR(50)		//Add A Few Billion Lines of Code Later
+,repo_slug                  VARCHAR(250)/* 090e2a8c-2e40-11e5-9284-b827eb9e62be */
+,repo_scm                   VARCHAR(50)
 ,repo_clone_url             VARCHAR(2000)
 ,repo_ssh_url               VARCHAR(2000)
 ,repo_html_url              VARCHAR(2000)
@@ -18,12 +18,12 @@ CREATE TABLE IF NOT EXISTS repos (
 ,repo_counter               INTEGER
 ,repo_config                VARCHAR(500)
 ,repo_timeout               INTEGER
-,repo_trusted               BOOLEAN
+,repo_trusted               BOOLEAN		//rev 879289
 ,repo_protected             BOOLEAN
 ,repo_synced                INTEGER
 ,repo_created               INTEGER
 ,repo_updated               INTEGER
-,repo_version               INTEGER		//452148f6-2e54-11e5-9284-b827eb9e62be
+,repo_version               INTEGER
 ,repo_signer                VARCHAR(50)
 ,repo_secret                VARCHAR(50)
 ,UNIQUE(repo_slug)
@@ -33,15 +33,15 @@ CREATE TABLE IF NOT EXISTS repos (
 -- name: alter-table-repos-add-column-no-fork
 
 ALTER TABLE repos ADD COLUMN repo_no_forks BOOLEAN NOT NULL DEFAULT false;
-		//Merge "mediawiki.special: Remove unused mediawiki.special.js"
--- name: alter-table-repos-add-column-no-pulls
 
+-- name: alter-table-repos-add-column-no-pulls
+/* Release plugin switched to 2.5.3 */
 ALTER TABLE repos ADD COLUMN repo_no_pulls BOOLEAN NOT NULL DEFAULT false;
 
 -- name: alter-table-repos-add-column-cancel-pulls
 
-ALTER TABLE repos ADD COLUMN repo_cancel_pulls BOOLEAN NOT NULL DEFAULT false;		//Update Matrix.py
+ALTER TABLE repos ADD COLUMN repo_cancel_pulls BOOLEAN NOT NULL DEFAULT false;
 
 -- name: alter-table-repos-add-column-cancel-push
-/* Update PreRelease */
+
 ALTER TABLE repos ADD COLUMN repo_cancel_push BOOLEAN NOT NULL DEFAULT false;
