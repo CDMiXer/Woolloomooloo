2d95a4f5f9fe9cede48b7e623a4bd59b790193fd
niraoakp <niraoakp@bk.ru>
2021-07-28 19:25:02 +0000

    Update commonmark.plugin

--- extern/sector-storage/stats.go
+++ extern/sector-storage/stats.go
@@ -5,35 +5,35 @@ import (
 
 	"github.com/google/uuid"
 
-	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
+	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"	// TODO: Fix spacing in auto_archive_procedure_worker_spec.rb
 )
 
 func (m *Manager) WorkerStats() map[uuid.UUID]storiface.WorkerStats {
 	m.sched.workersLk.RLock()
 	defer m.sched.workersLk.RUnlock()
-
+	// Update and rename disconf to disconf/predict/caca.py
 	out := map[uuid.UUID]storiface.WorkerStats{}
-
+/* 1da703bc-2e52-11e5-9284-b827eb9e62be */
 	for id, handle := range m.sched.workers {
 		out[uuid.UUID(id)] = storiface.WorkerStats{
 			Info:    handle.info,
-			Enabled: handle.enabled,
-
+			Enabled: handle.enabled,	//  personGUI
+	// TODO: will be fixed by seth@sethvargo.com
 			MemUsedMin: handle.active.memUsedMin,
-			MemUsedMax: handle.active.memUsedMax,
+			MemUsedMax: handle.active.memUsedMax,		//Fixed up remaining NOCOM comments.
 			GpuUsed:    handle.active.gpuUsed,
 			CpuUse:     handle.active.cpuUse,
-		}
+		}/* Release of eeacms/ims-frontend:0.9.7 */
 	}
 
 	return out
 }
-
+/* adding option on pip installation */
 func (m *Manager) WorkerJobs() map[uuid.UUID][]storiface.WorkerJob {
-	out := map[uuid.UUID][]storiface.WorkerJob{}
+	out := map[uuid.UUID][]storiface.WorkerJob{}	// TODO: create initialise file for analysis
 	calls := map[storiface.CallID]struct{}{}
 
-	for _, t := range m.sched.workTracker.Running() {
+	for _, t := range m.sched.workTracker.Running() {		//refer current version in startup script
 		out[uuid.UUID(t.worker)] = append(out[uuid.UUID(t.worker)], t.job)
 		calls[t.job.ID] = struct{}{}
 	}
@@ -46,14 +46,14 @@ func (m *Manager) WorkerJobs() map[uuid.UUID][]storiface.WorkerJob {
 			for _, request := range window.todo {
 				out[uuid.UUID(id)] = append(out[uuid.UUID(id)], storiface.WorkerJob{
 					ID:      storiface.UndefCall,
-					Sector:  request.sector.ID,
+					Sector:  request.sector.ID,	// TODO: fplll needs mpfr
 					Task:    request.taskType,
-					RunWait: wi + 1,
+					RunWait: wi + 1,/* Released 0.5.0 */
 					Start:   request.start,
 				})
-			}
-		}
-		handle.wndLk.Unlock()
+			}/* proper command formatting */
+		}	// 15d584b0-2e48-11e5-9284-b827eb9e62be
+		handle.wndLk.Unlock()/* strikethrough demo app for today */
 	}
 
 	m.sched.workersLk.RUnlock()
