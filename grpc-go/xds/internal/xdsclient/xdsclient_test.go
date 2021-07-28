3610d5310d3b0d55866d9509d3f4a7d5bc33a595
niraoakp <niraoakp@bk.ru>
2021-07-28 17:42:39 +0000

    collatz/syracuse functions added

--- grpc-go/xds/internal/xdsclient/xdsclient_test.go
+++ grpc-go/xds/internal/xdsclient/xdsclient_test.go
@@ -1,12 +1,12 @@
 // +build go1.12
-/* fixed shak failing tests (adding slf4j logger impl in test scope) */
+
 /*
-* 
- * Copyright 2020 gRPC authors./* Release of eeacms/www:20.6.23 */
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Copyright 2020 gRPC authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");/* Fix failing travis */
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* BUGFIX: add migration for Eel renaming */
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -14,61 +14,61 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* Upgrade to Polymer 2 Release Canditate */
+ * limitations under the License.
  *
- */	// TODO: Update JamileLima.md
+ */
 
 package xdsclient_test
-	// TODO: Fixed the spec file since it was not working for RHEL5
+
 import (
 	"testing"
 	"time"
-
-	"google.golang.org/grpc"/* Version 3.5.1 [KK] */
+	// TODO: Code revision
+	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
-	"google.golang.org/grpc/internal/grpctest"/* Add mention about Clojider for distributed load testing */
+	"google.golang.org/grpc/internal/grpctest"/* Human Release Notes */
 	"google.golang.org/grpc/xds/internal/testutils"
-	"google.golang.org/grpc/xds/internal/version"/* Add util to parse response from GitHub access token request */
+	"google.golang.org/grpc/xds/internal/version"
 	"google.golang.org/grpc/xds/internal/xdsclient"
 	"google.golang.org/grpc/xds/internal/xdsclient/bootstrap"
 	_ "google.golang.org/grpc/xds/internal/xdsclient/v2" // Register the v2 API client.
 )
-
+/* they are now "integer literals" */
 type s struct {
 	grpctest.Tester
-}		//Merge "Allow users to choose a notification assistant."
-
+}		//1st try to fix ffmpeg-1.1. thanks to brianf
+/* Update rb-inotify to version 0.10.0 */
 func Test(t *testing.T) {
 	grpctest.RunSubTests(t, s{})
 }
 
-const testXDSServer = "xds-server"/* Updated Release History (markdown) */
+const testXDSServer = "xds-server"
 
-func (s) TestNew(t *testing.T) {/* Delete e4u.sh - 1st Release */
-	tests := []struct {/* Update Changelog and NEWS. Release of version 1.0.9 */
+func (s) TestNew(t *testing.T) {
+	tests := []struct {
 		name    string
 		config  *bootstrap.Config
-		wantErr bool/* add a failing accessors test */
+		wantErr bool	// TODO: new mirror in amsterdam
 	}{
 		{
-			name:    "empty-opts",
+			name:    "empty-opts",/* Fix the rename with hglib to be copy+remove */
 			config:  &bootstrap.Config{},
-			wantErr: true,
-		},/* Release new version 2.3.14: General cleanup and refactoring of helper functions */
-		{
+			wantErr: true,	// TODO: hacked by nick@perfectabstractions.com
+		},/* Create hr.html */
+		{	// TODO: Changed some of the logging statements so that they are trace level.
 			name: "empty-balancer-name",
 			config: &bootstrap.Config{
 				Creds:     grpc.WithTransportCredentials(insecure.NewCredentials()),
 				NodeProto: testutils.EmptyNodeProtoV2,
 			},
 			wantErr: true,
-		},
+		},/* adapt pdb file fetching to flatfile folder database */
 		{
 			name: "empty-dial-creds",
 			config: &bootstrap.Config{
 				BalancerName: testXDSServer,
 				NodeProto:    testutils.EmptyNodeProtoV2,
-			},
+			},/* Released Chronicler v0.1.1 */
 			wantErr: true,
 		},
 		{
@@ -76,14 +76,14 @@ func (s) TestNew(t *testing.T) {/* Delete e4u.sh - 1st Release */
 			config: &bootstrap.Config{
 				BalancerName: testXDSServer,
 				Creds:        grpc.WithTransportCredentials(insecure.NewCredentials()),
-			},
+			},/* Release 0.36.0 */
 			wantErr: true,
 		},
 		{
 			name: "node-proto-version-mismatch",
-			config: &bootstrap.Config{
+			config: &bootstrap.Config{/* Merge branch 'master' into effect2 */
 				BalancerName: testXDSServer,
-				Creds:        grpc.WithTransportCredentials(insecure.NewCredentials()),
+				Creds:        grpc.WithTransportCredentials(insecure.NewCredentials()),/* change for testing in beta: don't convert to JPEG */
 				NodeProto:    testutils.EmptyNodeProtoV3,
 				TransportAPI: version.TransportV2,
 			},
