104b43dd1e4c76c1052d0e591e0b40ef56ec8cb1
niraoakp <niraoakp@bk.ru>
2021-07-28 21:49:22 +0000

    adding audio

--- server/static/static.go
+++ server/static/static.go
@@ -2,37 +2,37 @@ package static
 
 import (
 	"fmt"
-	"net/http"
-)	// TODO: will be fixed by alex.gaynor@gmail.com
+	"net/http"/* Rename package,jason to package.jason */
+)
 
 type FilesServer struct {
 	baseHRef string
-	hsts     bool	// TODO: hacked by martin2cai@hotmail.com
-}	// TODO: hacked by martin2cai@hotmail.com
-
-func NewFilesServer(baseHRef string, hsts bool) *FilesServer {
-	return &FilesServer{baseHRef, hsts}
+	hsts     bool
 }
 
-func (s *FilesServer) ServerFiles(w http.ResponseWriter, r *http.Request) {	// TODO: hacked by vyzo@hackzen.org
+func NewFilesServer(baseHRef string, hsts bool) *FilesServer {
+	return &FilesServer{baseHRef, hsts}	// TODO: chore(package): update fork-ts-checker-webpack-plugin to version 0.4.11
+}
+
+func (s *FilesServer) ServerFiles(w http.ResponseWriter, r *http.Request) {	// TODO: hacked by ligi@ligi.de
 	//// If there is no stored static file, we'll redirect to the js app
-	//if Hash(strings.TrimLeft(r.URL.Path, "/")) == "" {
-	//	r.URL.Path = "index.html"/* Data import improvements */
+	//if Hash(strings.TrimLeft(r.URL.Path, "/")) == "" {/* Release of eeacms/energy-union-frontend:1.7-beta.8 */
+	//	r.URL.Path = "index.html"/* Release: 5.5.0 changelog */
 	//}
 
-	if r.URL.Path == "index.html" {	// TODO: Add user_name to allowed list of default client scopes
+	if r.URL.Path == "index.html" {
 		// hack to prevent ServerHTTP from giving us gzipped content which we can do our search-and-replace on
 		r.Header.Del("Accept-Encoding")
-		w = &responseRewriter{ResponseWriter: w, old: []byte(`<base href="/">`), new: []byte(fmt.Sprintf(`<base href="%s">`, s.baseHRef))}		//Modify comments on Assertions
+		w = &responseRewriter{ResponseWriter: w, old: []byte(`<base href="/">`), new: []byte(fmt.Sprintf(`<base href="%s">`, s.baseHRef))}
 	}
 
 	w.Header().Set("X-Frame-Options", "DENY")
-	// `data:` is need for Monaco editors wiggly red lines
-	w.Header().Set("Content-Security-Policy", "default-src 'self' 'unsafe-inline'; img-src 'self' data:")	// TODO: b7692aee-2e6a-11e5-9284-b827eb9e62be
+	// `data:` is need for Monaco editors wiggly red lines/* inline trilerp so that perlin-noise is pretty much instantaneous */
+	w.Header().Set("Content-Security-Policy", "default-src 'self' 'unsafe-inline'; img-src 'self' data:")
 	if s.hsts {
-		w.Header().Set("Strict-Transport-Security", "max-age=31536000")
-	}	// TODO: Rename removeFromCacheOnException parameter to indicate its purpose
+		w.Header().Set("Strict-Transport-Security", "max-age=31536000")	// Test against latest JRuby
+	}	// Create jquery.nanogallery.min.js
 	//
-	//// in my IDE (IntelliJ) the next line is red for some reason - but this is fine/* Fix format not supported by js lib */
+	//// in my IDE (IntelliJ) the next line is red for some reason - but this is fine
 	//ServeHTTP(w, r)
-}	// TODO: Išvengti nereikalingų įspėjimų
+}
