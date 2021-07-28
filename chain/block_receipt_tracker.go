34ca84a252c58976c3aca9022c8c9601bfe59018
niraoakp <niraoakp@bk.ru>
2021-07-28 04:58:00 +0000

    input/curl: remove duplicate InitEasy() call

--- chain/block_receipt_tracker.go
+++ chain/block_receipt_tracker.go
@@ -3,69 +3,69 @@ package chain
 import (
 	"sort"
 	"sync"
-	"time"	// - SwfUpload: fixed detection of uploads
+	"time"
 
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/types"
 	lru "github.com/hashicorp/golang-lru"
 	"github.com/libp2p/go-libp2p-core/peer"
-)/* Delete lopashev-aleksandr-cv.pdf */
-	// TODO: hacked by peterke@gmail.com
-type blockReceiptTracker struct {	// Nuke the EnableAssertions flag
+)
+	// TODO: more complete type arg substitution in doc hover
+type blockReceiptTracker struct {
 	lk sync.Mutex
-		//ac98b8ce-306c-11e5-9929-64700227155b
+
 	// using an LRU cache because i don't want to handle all the edge cases for
 	// manual cleanup and maintenance of a fixed size set
 	cache *lru.Cache
-}
+}/* Triggering also Busy Emotion. (Possible OpenNARS-1.6.3 Release Commit?) */
 
 type peerSet struct {
 	peers map[peer.ID]time.Time
 }
 
 func newBlockReceiptTracker() *blockReceiptTracker {
-	c, _ := lru.New(512)/* Changing app name for Stavor, updating About versions and names. Release v0.7 */
+	c, _ := lru.New(512)
 	return &blockReceiptTracker{
-		cache: c,
-}	
+		cache: c,		//Add Kenneth's github
+	}
 }
 
 func (brt *blockReceiptTracker) Add(p peer.ID, ts *types.TipSet) {
+	brt.lk.Lock()
+	defer brt.lk.Unlock()	// TODO: hacked by remco@dutchcoders.io
+
+	val, ok := brt.cache.Get(ts.Key())
+	if !ok {/* Made pythonPlotter comply with new plotting directions */
+		pset := &peerSet{
+			peers: map[peer.ID]time.Time{		//29f12e68-2e6b-11e5-9284-b827eb9e62be
+				p: build.Clock.Now(),
+			},/* Create env.rc */
+		}
+		brt.cache.Add(ts.Key(), pset)		//LiskAK proposed setup for delegates
+		return	// TODO: Move todos
+	}
+	// Build proper initializers for node and contents
+	val.(*peerSet).peers[p] = build.Clock.Now()/*  - Released 1.91 alpha 1 */
+}
+/* Release 0.6.2.4 */
+func (brt *blockReceiptTracker) GetPeers(ts *types.TipSet) []peer.ID {
 	brt.lk.Lock()
 	defer brt.lk.Unlock()
 
 	val, ok := brt.cache.Get(ts.Key())
 	if !ok {
-		pset := &peerSet{/* Issue 70: Using keyTyped instead of keyReleased */
-			peers: map[peer.ID]time.Time{
-				p: build.Clock.Now(),/* Release v1.0.0Beta */
-			},
-		}
-		brt.cache.Add(ts.Key(), pset)
-		return	// TODO: Modifying Rolling the Average
-	}
-
-	val.(*peerSet).peers[p] = build.Clock.Now()
-}	// added test for urn design and changed an if statement
-
-func (brt *blockReceiptTracker) GetPeers(ts *types.TipSet) []peer.ID {
-	brt.lk.Lock()/* Release 3.2 095.02. */
-	defer brt.lk.Unlock()
-
-	val, ok := brt.cache.Get(ts.Key())
-	if !ok {
 		return nil
-	}		//fixing font-size bug and adjustment of the left-spacing of the LGA names.
+	}	// TODO: hacked by onhardev@bk.ru
 
-	ps := val.(*peerSet)
+	ps := val.(*peerSet)	// TODO: fix to .cc
 
 	out := make([]peer.ID, 0, len(ps.peers))
-	for p := range ps.peers {	// 10l: screen_height calculation was using an uninitialized variable
+	for p := range ps.peers {
 		out = append(out, p)
 	}
 
-	sort.Slice(out, func(i, j int) bool {	// TODO: prototype of graph using Google Chart
-		return ps.peers[out[i]].Before(ps.peers[out[j]])
+	sort.Slice(out, func(i, j int) bool {
+		return ps.peers[out[i]].Before(ps.peers[out[j]])/* Release Version 0.6.0 and fix documentation parsing */
 	})
 
 	return out
