f35205f9059bf2bbcf50faee499d76c03553f4e5
niraoakp <niraoakp@bk.ru>
2021-07-28 17:45:16 +0000

    remove some js that was moved to the trendingpages extension

--- mock/mockscm/mock.go
+++ mock/mockscm/mock.go
@@ -1,9 +1,9 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Release version 1.2.0.RELEASE */
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-	// TODO: b8a795a0-2e6d-11e5-9284-b827eb9e62be
+/* * NEWS: Release 0.2.10 */
 // +build !oss
-/* add a test about hashing array.array */
+
 package mockscm
 
-//go:generate mockgen -package=mockscm -destination=mock_gen.go github.com/drone/go-scm/scm ContentService,GitService,OrganizationService,PullRequestService,RepositoryService,UserService
+//go:generate mockgen -package=mockscm -destination=mock_gen.go github.com/drone/go-scm/scm ContentService,GitService,OrganizationService,PullRequestService,RepositoryService,UserService		//Extend hardware support notes in README
