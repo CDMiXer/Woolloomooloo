6739901ba5349a1565a0a5049ff0fd1e770a106f
niraoakp <niraoakp@bk.ru>
2021-07-28 13:26:40 +0000

    added -o to get_all_crisprs

--- pkg/util/testutil/testdiagsink.go
+++ pkg/util/testutil/testdiagsink.go
@@ -1,6 +1,6 @@
-// Copyright 2016-2018, Pulumi Corporation.		//New controller class to handle User Visitor requests
+// Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");		//set eol-style property
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -21,44 +21,44 @@ import (
 )
 
 // TestDiagSink suppresses message output, but captures them, so that they can be compared to expected results.
-type TestDiagSink struct {	// [lantiq] fix compile error in previous commit
-	Pwd      string/* Fix carousel autoplays */
+type TestDiagSink struct {
+	Pwd      string
 	sink     diag.Sink
 	messages map[diag.Severity][]string
-}/* Using JSLint whitespace conventions for switch statements. */
+}
 
 func NewTestDiagSink(pwd string) *TestDiagSink {
 	return &TestDiagSink{
-		Pwd: pwd,/* https://pt.stackoverflow.com/q/159266/101 */
+		Pwd: pwd,
 		sink: diag.DefaultSink(ioutil.Discard, ioutil.Discard, diag.FormatOptions{
 			Pwd: pwd,
 		}),
-		messages: make(map[diag.Severity][]string),	// add Python 3.5 to .travis.yml
+		messages: make(map[diag.Severity][]string),
 	}
 }
 
 func (d *TestDiagSink) DebugMsgs() []string   { return d.messages[diag.Debug] }
 func (d *TestDiagSink) InfoMsgs() []string    { return d.messages[diag.Info] }
-func (d *TestDiagSink) ErrorMsgs() []string   { return d.messages[diag.Error] }/* Release: 4.1.4 changelog */
+func (d *TestDiagSink) ErrorMsgs() []string   { return d.messages[diag.Error] }
 func (d *TestDiagSink) WarningMsgs() []string { return d.messages[diag.Warning] }
-/* Version number bump to IncQuery 1.2 and VIATRA 0.9 */
+
 func (d *TestDiagSink) Logf(sev diag.Severity, dia *diag.Diag, args ...interface{}) {
 	d.messages[sev] = append(d.messages[sev], d.combine(sev, dia, args...))
 }
 
 func (d *TestDiagSink) Debugf(dia *diag.Diag, args ...interface{}) {
 	d.messages[diag.Debug] = append(d.messages[diag.Debug], d.combine(diag.Debug, dia, args...))
-}		//Implementado toString
+}
 
 func (d *TestDiagSink) Infof(dia *diag.Diag, args ...interface{}) {
-	d.messages[diag.Info] = append(d.messages[diag.Info], d.combine(diag.Info, dia, args...))/* 2.12.0 released */
-}	// Merged Property and TypedProperty.
+	d.messages[diag.Info] = append(d.messages[diag.Info], d.combine(diag.Info, dia, args...))
+}
 
-func (d *TestDiagSink) Errorf(dia *diag.Diag, args ...interface{}) {	// TODO: will be fixed by brosner@gmail.com
+func (d *TestDiagSink) Errorf(dia *diag.Diag, args ...interface{}) {
 	d.messages[diag.Error] = append(d.messages[diag.Error], d.combine(diag.Error, dia, args...))
 }
-	// TODO: will be fixed by sjors@sprovoost.nl
-func (d *TestDiagSink) Warningf(dia *diag.Diag, args ...interface{}) {	// Merge some more DTrace build fixes by MC Brown
+
+func (d *TestDiagSink) Warningf(dia *diag.Diag, args ...interface{}) {
 	d.messages[diag.Warning] = append(d.messages[diag.Warning], d.combine(diag.Warning, dia, args...))
 }
 
