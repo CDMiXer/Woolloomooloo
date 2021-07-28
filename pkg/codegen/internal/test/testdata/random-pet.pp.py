f0290fcbc82771b6d84638a3957e41e44a4c4452
niraoakp <niraoakp@bk.ru>
2021-07-28 17:59:42 +0000

    Fixed incorrect relative paths handling in static/gzip libraries

--- pkg/codegen/internal/test/testdata/random-pet.pp.py
+++ pkg/codegen/internal/test/testdata/random-pet.pp.py
@@ -1,4 +1,4 @@
 import pulumi
 import pulumi_random as random
-/* Merge "Release 1.0.0.174 QCACLD WLAN Driver" */
-random_pet = random.RandomPet("random_pet", prefix="doggo")/* Task #3157: Merging latest changes in LOFAR-Release-0.93 into trunk */
+
+random_pet = random.RandomPet("random_pet", prefix="doggo")
