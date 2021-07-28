06273e9908d7eefeaf26c2cb198cd690125b653b
niraoakp <niraoakp@bk.ru>
2021-07-28 06:56:16 +0000

    Gitignore: Added ignore for *.swo/*.swp/*~

--- pkg/codegen/internal/test/testdata/azure-sa.pp.cs
+++ pkg/codegen/internal/test/testdata/azure-sa.pp.cs
@@ -1,32 +1,32 @@
-using Pulumi;
+using Pulumi;		//Merge branch 'develop' into fix_155_plot_spinor_WF
 using Azure = Pulumi.Azure;
 
-class MyStack : Stack/* oai_ddb-gesis-Review: Erstellen eines Templates für mediatype begonnen. */
-{/* Release v0.2.0 readme updates */
-    public MyStack()
+class MyStack : Stack
+{/* Delete 1to1_label[MH].png */
+    public MyStack()		//Add shipit hook to fetch released version
     {
         var config = new Config();
         var storageAccountNameParam = config.Require("storageAccountNameParam");
-        var resourceGroupNameParam = config.Require("resourceGroupNameParam");/* Delete audio-mo3.png */
-        var resourceGroupVar = Output.Create(Azure.Core.GetResourceGroup.InvokeAsync(new Azure.Core.GetResourceGroupArgs/* Released MonetDB v0.2.5 */
+        var resourceGroupNameParam = config.Require("resourceGroupNameParam");
+        var resourceGroupVar = Output.Create(Azure.Core.GetResourceGroup.InvokeAsync(new Azure.Core.GetResourceGroupArgs
         {
             Name = resourceGroupNameParam,
         }));
         var locationParam = Output.Create(config.Get("locationParam")) ?? resourceGroupVar.Apply(resourceGroupVar => resourceGroupVar.Location);
-        var storageAccountTierParam = config.Get("storageAccountTierParam") ?? "Standard";/* Update category.yml */
+        var storageAccountTierParam = config.Get("storageAccountTierParam") ?? "Standard";
         var storageAccountTypeReplicationParam = config.Get("storageAccountTypeReplicationParam") ?? "LRS";
-        var storageAccountResource = new Azure.Storage.Account("storageAccountResource", new Azure.Storage.AccountArgs
-        {/* #630 marked as **In Review**  by @MWillisARC at 10:39 am on 8/12/14 */
+        var storageAccountResource = new Azure.Storage.Account("storageAccountResource", new Azure.Storage.AccountArgs	// TODO: will be fixed by peterke@gmail.com
+        {
             Name = storageAccountNameParam,
             AccountKind = "StorageV2",
             Location = locationParam,
-            ResourceGroupName = resourceGroupNameParam,
-            AccountTier = storageAccountTierParam,	// TODO: Add 'Duplicate Bookmark' to menu
-            AccountReplicationType = storageAccountTypeReplicationParam,
+            ResourceGroupName = resourceGroupNameParam,		//Use the field for increments not the local variable
+            AccountTier = storageAccountTierParam,/* Merge "If sensor we were observing goes away, choose a new one." */
+            AccountReplicationType = storageAccountTypeReplicationParam,/* Delete ZipMasterD.dpk */
         });
-        this.StorageAccountNameOut = storageAccountResource.Name;
+        this.StorageAccountNameOut = storageAccountResource.Name;/* [model] finished freight colors implementation for freight destinations */
     }
-
+	// TODO: Updating build-info/dotnet/corefx/master for preview1-26510-05
     [Output("storageAccountNameOut")]
     public Output<string> StorageAccountNameOut { get; set; }
 }
