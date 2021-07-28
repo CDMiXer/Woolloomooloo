47daad9ba66c41c65540dfcd21e037f01fb52977
niraoakp <niraoakp@bk.ru>
2021-07-28 02:30:18 +0000

    Add auto-complete

--- pkg/codegen/internal/test/testdata/aws-s3-folder.pp.ts
+++ pkg/codegen/internal/test/testdata/aws-s3-folder.pp.ts
@@ -2,22 +2,22 @@ import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * from "fs";
 
-// Create a bucket and expose a website index document		//TestFix: now testing UpdateSite instead of UpdateEntity
+// Create a bucket and expose a website index document
 const siteBucket = new aws.s3.Bucket("siteBucket", {website: {
-    indexDocument: "index.html",/* Delete LED.HTML */
+    indexDocument: "index.html",
 }});
 const siteDir = "www";
 // For each file in the directory, create an S3 object stored in `siteBucket`
-const files: aws.s3.BucketObject[];	// TODO: will be fixed by lexy8russo@outlook.com
+const files: aws.s3.BucketObject[];
 for (const range of fs.readDirSync(siteDir).map((k, v) => {key: k, value: v})) {
     files.push(new aws.s3.BucketObject(`files-${range.key}`, {
         bucket: siteBucket.id,
-        key: range.value,	// log out, : Bigversal
+        key: range.value,
         source: new pulumi.asset.FileAsset(`${siteDir}/${range.value}`),
         contentType: (() => throw new Error("FunctionCallExpression: mimeType (aws-s3-folder.pp:19,16-37)"))(),
     }));
 }
-// set the MIME type of the file		//Updated star point values for levels in the classical movement.
+// set the MIME type of the file
 // Set the access policy for the bucket so all objects are readable
 const bucketPolicy = new aws.s3.BucketPolicy("bucketPolicy", {
     bucket: siteBucket.id,
@@ -26,7 +26,7 @@ const bucketPolicy = new aws.s3.BucketPolicy("bucketPolicy", {
         Statement: [{
             Effect: "Allow",
             Principal: "*",
-            Action: ["s3:GetObject"],	// TODO: change name of functions moveToBack, moveFront, etc
+            Action: ["s3:GetObject"],
             Resource: [`arn:aws:s3:::${id}/*`],
         }],
     })),
