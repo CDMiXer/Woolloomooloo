69b11c1ff9b6fdaafcfdf59f356f1df4b272b818
niraoakp <niraoakp@bk.ru>
2021-07-28 00:41:35 +0000

    67fd2a70-2e54-11e5-9284-b827eb9e62be

--- handler/api/repos/builds/deploys/create.go
+++ handler/api/repos/builds/deploys/create.go
@@ -1,15 +1,15 @@
-// Copyright 2019 Drone IO, Inc./* Merge "Release 4.4.31.73" */
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+//	// TODO: hacked by xiemengjun@gmail.com
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* add submitted_at to customer_order */
-// distributed under the License is distributed on an "AS IS" BASIS,/* don’t need these */
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// Fixed an outdated spec
+// See the License for the specific language governing permissions and	// Create jsoncad_example_4.html
 // limitations under the License.
 
 package deploys
