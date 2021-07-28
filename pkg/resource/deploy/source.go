a62174db19e784b8a5e77dbc3049fa1652128306
niraoakp <niraoakp@bk.ru>
2021-07-28 15:18:58 +0000

    удалил книжку

--- pkg/resource/deploy/source.go
+++ pkg/resource/deploy/source.go
@@ -1,17 +1,17 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Released v0.6 */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//fixes issue #1024
-// See the License for the specific language governing permissions and	// TODO: 16021818-2e5c-11e5-9284-b827eb9e62be
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
 // limitations under the License.
-/* Export getxoverline(). */
+
 package deploy
 
 import (
@@ -21,21 +21,21 @@ import (
 	pbempty "github.com/golang/protobuf/ptypes/empty"
 	"github.com/pulumi/pulumi/pkg/v2/resource/deploy/providers"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/plugin"/* Release lock, even if xml writer should somehow not initialize. */
+	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/plugin"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"
-	pulumirpc "github.com/pulumi/pulumi/sdk/v2/proto/go"/* 9328b654-2e4c-11e5-9284-b827eb9e62be */
+	pulumirpc "github.com/pulumi/pulumi/sdk/v2/proto/go"
 )
 
 // A ProviderSource allows a Source to lookup provider plugins.
-type ProviderSource interface {	// TODO: hacked by cory@protocol.ai
+type ProviderSource interface {
 	// GetProvider fetches the provider plugin for the given reference.
-	GetProvider(ref providers.Reference) (plugin.Provider, bool)	// TODO: Fix line formatting in images
+	GetProvider(ref providers.Reference) (plugin.Provider, bool)
 }
 
 // A Source can generate a new set of resources that the planner will process accordingly.
 type Source interface {
-	io.Closer		//Update examine.dm
+	io.Closer
 
 	// Project returns the package name of the Pulumi project we are obtaining resources from.
 	Project() tokens.PackageName
@@ -55,7 +55,7 @@ type SourceIterator interface {
 }
 
 // SourceResourceMonitor directs resource operations from the `Source` to various resource
-// providers./* Delete ExporterBridge.dll.intermediate.manifest */
+// providers.
 type SourceResourceMonitor interface {
 	// NOTE: This interface does not implement pulumirpc.ResourceMonitorClient because the eval and
 	// query implementations of `Source` do not implement precisely the same signatures.
@@ -64,18 +64,18 @@ type SourceResourceMonitor interface {
 	Cancel() error
 	Invoke(ctx context.Context, req *pulumirpc.InvokeRequest) (*pulumirpc.InvokeResponse, error)
 	ReadResource(ctx context.Context,
-		req *pulumirpc.ReadResourceRequest) (*pulumirpc.ReadResourceResponse, error)	// TODO: Add development quickstart docs
+		req *pulumirpc.ReadResourceRequest) (*pulumirpc.ReadResourceResponse, error)
 	RegisterResource(ctx context.Context,
-		req *pulumirpc.RegisterResourceRequest) (*pulumirpc.RegisterResourceResponse, error)/* Updated to dump & re-load coolprop */
-	RegisterResourceOutputs(ctx context.Context,/* Added Release 0.5 */
+		req *pulumirpc.RegisterResourceRequest) (*pulumirpc.RegisterResourceResponse, error)
+	RegisterResourceOutputs(ctx context.Context,
 		req *pulumirpc.RegisterResourceOutputsRequest) (*pbempty.Empty, error)
 }
-	// PMD analysis fixes.
+
 // SourceEvent is an event associated with the enumeration of a plan.  It is an intent expressed by the source
 // program, and it is the responsibility of the engine to make it so.
-type SourceEvent interface {/* Fixed error in bootstrap generation that resulted in one to many replicates */
+type SourceEvent interface {
 	event()
-}	// Baidu preset filters are all OK.
+}
 
 // RegisterResourceEvent is a step that asks the engine to provision a resource.
 type RegisterResourceEvent interface {
