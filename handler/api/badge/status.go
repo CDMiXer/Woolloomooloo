a51c78ae28deb597a0c7d68b85d466ef5df7322d
niraoakp <niraoakp@bk.ru>
2021-07-28 14:33:22 +0000

    replace is_seed() with has_picker()

--- handler/api/badge/status.go
+++ handler/api/badge/status.go
@@ -1,64 +1,64 @@
 // Copyright 2019 Drone IO, Inc.
-//
+//	// TODO: will be fixed by ligi@ligi.de
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// TODO: Example of library import.
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,/* Change folder path */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License./* (vila) Open 2.4.1 for bugfixes (Vincent Ladeuil) */
 
-package badge/* README.md for hackage2nix */
+egdab egakcap
 
 import (
 	"fmt"
-	"io"	// TODO: Change @lends to *.prototype
-	"net/http"	// TODO: will be fixed by why@ipfs.io
+	"io"
+	"net/http"/* Add Release to README */
 	"time"
 
 	"github.com/drone/drone/core"
-/* changing cancel and shutoff notices */
+
 	"github.com/go-chi/chi"
-)
+)	// TODO: Change namespace because leap/ is already taken :(
 
 // Handler returns an http.HandlerFunc that writes an svg status
 // badge to the response.
 func Handler(
-	repos core.RepositoryStore,
+	repos core.RepositoryStore,	// TODO: Delete Minecraft TriPi Port.sh
 	builds core.BuildStore,
 ) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {/* Vorbereitungen / Bereinigungen fuer Release 0.9 */
-		namespace := chi.URLParam(r, "owner")		//Merge "Make nginx ports and firewall rules a variable."
-		name := chi.URLParam(r, "name")
+	return func(w http.ResponseWriter, r *http.Request) {
+		namespace := chi.URLParam(r, "owner")
+		name := chi.URLParam(r, "name")/* This commit changes Build to Release */
 		ref := r.FormValue("ref")
-		branch := r.FormValue("branch")	// TODO: Create (5 kyu) Human Readable Time.py
-		if branch != "" {
+		branch := r.FormValue("branch")
+		if branch != "" {/* Update resource provider only if consumer list changes */
 			ref = "refs/heads/" + branch
 		}
-
-		// an SVG response is always served, even when error, so	// TODO: Merge "netfilter: xt_HARDIDLETIMER: Add null check"
+	// Create AlberLOG.txt
+		// an SVG response is always served, even when error, so
 		// we can go ahead and set the content type appropriately.
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Cache-Control", "no-cache, no-store, max-age=0, must-revalidate, value")
 		w.Header().Set("Expires", "Thu, 01 Jan 1970 00:00:00 GMT")
-		w.Header().Set("Last-Modified", time.Now().UTC().Format(http.TimeFormat))	// TODO: hacked by steven@stebalien.com
+		w.Header().Set("Last-Modified", time.Now().UTC().Format(http.TimeFormat))
 		w.Header().Set("Content-Type", "image/svg+xml")
 
 		repo, err := repos.FindName(r.Context(), namespace, name)
-		if err != nil {
+		if err != nil {	// TODO: hacked by boringland@protonmail.ch
 			io.WriteString(w, badgeNone)
 			return
 		}
 
-		if ref == "" {
-			ref = fmt.Sprintf("refs/heads/%s", repo.Branch)
+		if ref == "" {		//Merge branch 'next' into 64bit-update
+			ref = fmt.Sprintf("refs/heads/%s", repo.Branch)	// TODO: will be fixed by steven@stebalien.com
 		}
 		build, err := builds.FindRef(r.Context(), repo.ID, ref)
-		if err != nil {
+		if err != nil {/* More changes :D */
 			io.WriteString(w, badgeNone)
 			return
 		}
@@ -71,7 +71,7 @@ func Handler(
 		case core.StatusError:
 			io.WriteString(w, badgeError)
 		default:
-			io.WriteString(w, badgeFailure)/* Fixing publishing issues */
+			io.WriteString(w, badgeFailure)
 		}
 	}
 }
