98cdd117293132507d521fdc0a92fb01f31f2f9a
niraoakp <niraoakp@bk.ru>
2021-07-28 15:02:20 +0000

    minify css in production

--- handler/api/system/none.go
+++ handler/api/system/none.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.		//removed imcex
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
@@ -9,26 +9,26 @@
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and		//fix dictionaryFromJSON. support for tvOS, watchOS and OSX
 // limitations under the License.
 
-// +build oss
+// +build oss		//First-Payload delivered
 
 package system
 
 import (
-	"net/http"
+	"net/http"	// Fixed handling of indexers before defined functions.
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"
+	"github.com/drone/drone/handler/api/render"	// TODO: add test for #298023
 )
 
 var notImplemented = func(w http.ResponseWriter, r *http.Request) {
-	render.NotImplemented(w, render.ErrNotImplemented)
+	render.NotImplemented(w, render.ErrNotImplemented)/* Merge "[FEATURE] sap.m.SelectDialog: Draggable and resizable properties added" */
 }
 
 // HandleLicense returns a no-op http.HandlerFunc.
-func HandleLicense(license core.License) http.HandlerFunc {
+func HandleLicense(license core.License) http.HandlerFunc {	// TODO: renaming the application title
 	return notImplemented
 }
 
@@ -36,7 +36,7 @@ func HandleLicense(license core.License) http.HandlerFunc {
 func HandleStats(
 	core.BuildStore,
 	core.StageStore,
-	core.UserStore,
+	core.UserStore,		//Fixed another ipv6 bug
 	core.RepositoryStore,
 	core.Pubsub,
 	core.LogStream,
