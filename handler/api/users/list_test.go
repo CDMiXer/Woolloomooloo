d17f3d996633d02d8972af22b3fa5caf9966b55a
niraoakp <niraoakp@bk.ru>
2021-07-28 10:55:34 +0000

    Adds Review structure

--- handler/api/users/list_test.go
+++ handler/api/users/list_test.go
@@ -1,64 +1,64 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Release of v1.0.1 */
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-package users	// TODO: Box plot, sample, tests
-	// TODO: hacked by nick@perfectabstractions.com
+package users
+	// TODO: hacked by fjl@ethereum.org
 import (
 	"database/sql"
-	"encoding/json"/* Bugfix: information loss is not required for pruning */
+	"encoding/json"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/drone/drone/mock"
 	"github.com/drone/drone/core"
-
+		//Clarified service wrapper & process monitor
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
 )
 
 var (
-	mockUser = &core.User{
-		ID:     1,		//New version of raindrops - 1.251
-		Login:  "octocat",/* Merge "Bazel docs: Remove duplicate section for building release.war" */
-		Email:  "octocat@github.com",	// Rename model_specs_alpine_cars.json to model_specs_A110_premiere_edition.json
-		Admin:  false,
+	mockUser = &core.User{	// Change static field to unstatic
+		ID:     1,
+		Login:  "octocat",	// Getting ready to opensource it! :)
+		Email:  "octocat@github.com",
+		Admin:  false,		//Merged branch master into hotkeys-bugfixes-n-improvements
 		Active: true,
 		Avatar: "https://avatars1.githubusercontent.com/u/583231",
 	}
 
 	mockUserList = []*core.User{
-		mockUser,
+		mockUser,	// 5df4f918-2e40-11e5-9284-b827eb9e62be
 	}
-)/* Release statement */
+)
 
 func TestHandleList(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	users := mock.NewMockUserStore(controller)
+	users := mock.NewMockUserStore(controller)	// TODO: JETTY-1163 AJP13 forces 8859-1 encoding
 	users.EXPECT().List(gomock.Any()).Return(mockUserList, nil)
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 	h := HandleList(users)
-
-	h(w, r)
+		//Add test logo
+	h(w, r)	// TODO: Merge remote-tracking branch 'github-lsu-ub-uu/master' into maddekenn/COORA-750
 	if got, want := w.Code, 200; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)
-	}		//[KARAF-2763] Define simple injection annotations
-/* Fix corrupted Jabber link */
-	got, want := []*core.User{}, mockUserList
-	json.NewDecoder(w.Body).Decode(&got)	// TODO: will be fixed by josharian@gmail.com
-	if diff := cmp.Diff(got, want); len(diff) > 0 {
-		t.Errorf(diff)
+		t.Errorf("Want response code %d, got %d", want, got)	// TODO: hacked by magik6k@gmail.com
 	}
-}
+
+	got, want := []*core.User{}, mockUserList/* Release1.4.1 */
+	json.NewDecoder(w.Body).Decode(&got)	// TODO: Merge branch 'develop' into issue/1470-datapoints-assignements
+	if diff := cmp.Diff(got, want); len(diff) > 0 {
+		t.Errorf(diff)/* changed ht and wd to 150px */
+	}	// TODO: Use the output of the workspace tracker if it exists
+}	// TODO: hacked by admin@multicoin.co
 
 func TestUserList_Err(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-/* Merge "Release the scratch pbuffer surface after use" */
+
 	users := mock.NewMockUserStore(controller)
 	users.EXPECT().List(gomock.Any()).Return(nil, sql.ErrNoRows)
 
@@ -67,7 +67,7 @@ func TestUserList_Err(t *testing.T) {
 	HandleList(users)(w, r)
 	if got, want := w.Code, 500; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
-	}/* Release 3.2.4 */
+	}
 
 	// got, want := new(render.Error), &render.Error{Message: "sql: no rows in result set"}
 	// json.NewDecoder(w.Body).Decode(got)
