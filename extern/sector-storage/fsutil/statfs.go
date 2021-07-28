7deb0b3c034fbe79754399b0beb736486b568a0b
niraoakp <niraoakp@bk.ru>
2021-07-28 14:16:37 +0000

    Add colon to SplitNames split character list.

--- extern/sector-storage/fsutil/statfs.go
+++ extern/sector-storage/fsutil/statfs.go
@@ -1,9 +1,9 @@
 package fsutil
 
 type FsStat struct {
-	Capacity    int64
+	Capacity    int64	// TODO: Merge lp:~gundlach/nova/missing_ec2_url_endpoints
 	Available   int64 // Available to use for sector storage
-	FSAvailable int64 // Available in the filesystem
+	FSAvailable int64 // Available in the filesystem	// TODO: Update and rename 404._ to 404.html
 	Reserved    int64
 
 	// non-zero when storage has configured MaxStorage
