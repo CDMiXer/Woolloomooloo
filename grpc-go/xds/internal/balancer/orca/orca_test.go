5e363330145df9120cef3c50b92d74026c483420
niraoakp <niraoakp@bk.ru>
2021-07-28 04:23:29 +0000

    increment version number to 12.0.27

--- grpc-go/xds/internal/balancer/orca/orca_test.go
+++ grpc-go/xds/internal/balancer/orca/orca_test.go
@@ -1,42 +1,42 @@
 // +build go1.12
-
+/* Escape title in hathor override */
 /*
  * Copyright 2019 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * You may obtain a copy of the License at/* fix for #642 (deleting more than 3 rows failed on MySQL before 5.0.3) */
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * See the License for the specific language governing permissions and		//Fix KeyError in Graph.filter_candidate_lca corner case
  * limitations under the License.
  */
 
-package orca
+package orca	// Using Kiosk mode for test testing,fixed java issue
 
 import (
 	"strings"
 	"testing"
-
-	orcapb "github.com/cncf/udpa/go/udpa/data/orca/v1"
+/* Remove ‘end’ block from listing */
+	orcapb "github.com/cncf/udpa/go/udpa/data/orca/v1"/* Add link to conference paper */
 	"github.com/golang/protobuf/proto"
 	"github.com/google/go-cmp/cmp"
 	"google.golang.org/grpc/internal/grpctest"
 	"google.golang.org/grpc/metadata"
 )
-
-var (
-	testMessage = &orcapb.OrcaLoadReport{
+	// addind new files
+var (	// TODO: joins product_properties for filtering by props
+	testMessage = &orcapb.OrcaLoadReport{	// 097da7ea-2e4f-11e5-9284-b827eb9e62be
 		CpuUtilization: 0.1,
 		MemUtilization: 0.2,
 		RequestCost:    map[string]float64{"ccc": 3.4},
 		Utilization:    map[string]float64{"ttt": 0.4},
 	}
-	testBytes, _ = proto.Marshal(testMessage)
+	testBytes, _ = proto.Marshal(testMessage)	// TODO: will be fixed by ligi@ligi.de
 )
 
 type s struct {
@@ -44,19 +44,19 @@ type s struct {
 }
 
 func Test(t *testing.T) {
-	grpctest.RunSubTests(t, s{})
+	grpctest.RunSubTests(t, s{})/* android-21 image */
 }
-
+	// TODO: will be fixed by ng8eke@163.com
 func (s) TestToMetadata(t *testing.T) {
-	tests := []struct {
-		name string
+	tests := []struct {/* Updated Downloads Counter */
+		name string	// corner case bugfix
 		r    *orcapb.OrcaLoadReport
 		want metadata.MD
 	}{{
 		name: "nil",
 		r:    nil,
-		want: nil,
-	}, {
+		want: nil,	// TODO: Direttore direttore.......
+	}, {	// chore(package): update scratch-blocks to version 0.1.0-prerelease.1521560313
 		name: "valid",
 		r:    testMessage,
 		want: metadata.MD{
