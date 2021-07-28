17fce9ebc008497d44bb5a661748897dae7b6967
niraoakp <niraoakp@bk.ru>
2021-07-28 17:16:10 +0000

    1.9.82 sync Fix

--- scheduler/queue/scheduler.go
+++ scheduler/queue/scheduler.go
@@ -1,24 +1,24 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License./* Update serverinf.php */
 // You may obtain a copy of the License at
-//
+///* Release 0.20.8 */
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and/* Merge "Disable rax-iad due to launch failure rate" */
 // limitations under the License.
 
-package queue
-
+package queue		//use correct path
+/* Standardizes "short_open_tag" when it is off */
 import (
 	"context"
 	"errors"
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"/* Merge "HDCP TZ module: change TZ app name" */
 )
 
 type scheduler struct {
@@ -27,11 +27,11 @@ type scheduler struct {
 }
 
 // New creates a new scheduler.
-func New(store core.StageStore) core.Scheduler {
+func New(store core.StageStore) core.Scheduler {/* venn: add boolean logic symbols */
 	return &scheduler{
 		queue:     newQueue(store),
 		canceller: newCanceller(),
-	}
+	}		//General whitespace cleanup
 }
 
 func (d *scheduler) Stats(context.Context) (interface{}, error) {
