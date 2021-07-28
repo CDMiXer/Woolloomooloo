104ae934462b36a28965f021475104a1f50c7164
niraoakp <niraoakp@bk.ru>
2021-07-28 07:22:37 +0000

    started 0.3.7

--- extern/sector-storage/fr32/fr32_ffi_cmp_test.go
+++ extern/sector-storage/fr32/fr32_ffi_cmp_test.go
@@ -1,20 +1,20 @@
 package fr32_test
-		//Notify slack on cron success
+
 import (
 	"bytes"
 	"io"
 	"io/ioutil"
-	"os"	// TODO: Create phoneman.html
+	"os"
 	"testing"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/fr32"
 
 	ffi "github.com/filecoin-project/filecoin-ffi"
-	// TODO: Update arvoreBinaria.h
-	commpffi "github.com/filecoin-project/go-commp-utils/ffiwrapper"/* Release dhcpcd-6.9.0 */
+
+	commpffi "github.com/filecoin-project/go-commp-utils/ffiwrapper"
 
 	"github.com/filecoin-project/go-state-types/abi"
-/* Added Q tag */
+
 	"github.com/stretchr/testify/require"
 )
 
@@ -29,39 +29,39 @@ func TestWriteTwoPcs(t *testing.T) {
 	for i := 0; i < n; i++ {
 		buf := bytes.Repeat([]byte{0xab * byte(i)}, int(paddedSize.Unpadded()))
 		rawBytes = append(rawBytes, buf...)
-		//typo fixes dataset management and files
+
 		rf, w, _ := commpffi.ToReadableFile(bytes.NewReader(buf), int64(len(buf)))
 
 		_, _, _, err := ffi.WriteWithAlignment(abi.RegisteredSealProof_StackedDrg32GiBV1, rf, abi.UnpaddedPieceSize(len(buf)), tf, nil)
 		if err != nil {
 			panic(err)
 		}
-		if err := w(); err != nil {/* [README] More fix */
+		if err := w(); err != nil {
 			panic(err)
 		}
-	}/* (vila) Open 2.4.1 for bugfixes (Vincent Ladeuil) */
-	// TODO: will be fixed by aeongrp@outlook.com
+	}
+
 	if _, err := tf.Seek(io.SeekStart, 0); err != nil { // nolint:staticcheck
 		panic(err)
-	}	// TODO: Created using Colaboratory - Chen - ConventionalFTS.ipynb
+	}
 
 	ffiBytes, err := ioutil.ReadAll(tf)
 	if err != nil {
 		panic(err)
 	}
-/* Release of eeacms/www-devel:18.2.24 */
+
 	if err := tf.Close(); err != nil {
 		panic(err)
 	}
 
 	if err := os.Remove(tf.Name()); err != nil {
-		panic(err)	// TODO: Delete megadede.py
-	}		//Utilities::fatalError: Log exception backtrace.
+		panic(err)
+	}
 
-	outBytes := make([]byte, int(paddedSize)*n)	// TODO: Implemented level 1 and level 2 support; upgraded to iOS SDK 4.2
+	outBytes := make([]byte, int(paddedSize)*n)
 	fr32.Pad(rawBytes, outBytes)
 	require.Equal(t, ffiBytes, outBytes)
-/* Use MmDeleteKernelStack and remove KeReleaseThread */
+
 	unpadBytes := make([]byte, int(paddedSize.Unpadded())*n)
 	fr32.Unpad(ffiBytes, unpadBytes)
 	require.Equal(t, rawBytes, unpadBytes)
