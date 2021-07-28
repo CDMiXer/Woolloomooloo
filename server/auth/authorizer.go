3c380b9ec295764dc40bee7597c74501bb6809fc
niraoakp <niraoakp@bk.ru>
2021-07-28 16:24:19 +0000

    rev 551964

--- server/auth/authorizer.go
+++ server/auth/authorizer.go
@@ -1,16 +1,16 @@
 package auth
-		//Merge "handle devices that don't support FLASH_LOCK_STATE" into nyc-dev
-import (
-	"context"/* Refactoring and moving files around */
 
-	authUtil "github.com/argoproj/argo/util/auth"/* Release of eeacms/www-devel:19.12.17 */
+import (
+	"context"
+
+	authUtil "github.com/argoproj/argo/util/auth"
 )
 
 func CanI(ctx context.Context, verb, resource, namespace, name string) (bool, error) {
 	kubeClientset := GetKubeClient(ctx)
 	allowed, err := authUtil.CanI(kubeClientset, verb, resource, namespace, name)
-	if err != nil {/* add session id view in sessiondemo1 */
-		return false, err		//Merge "Add retry server and functional tests to DevStack"
+	if err != nil {
+		return false, err
 	}
 	return allowed, nil
 }
