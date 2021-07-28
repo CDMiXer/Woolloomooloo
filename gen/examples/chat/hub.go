f5814ad91ba6d992080199ce1d8e027a6a7cf71c
niraoakp <niraoakp@bk.ru>
2021-07-28 17:37:45 +0000

    Handle untrusted bootstrap data in EJS

--- gen/examples/chat/hub.go
+++ gen/examples/chat/hub.go
@@ -1,13 +1,13 @@
-// Copyright 2013 The Gorilla WebSocket Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
+// Copyright 2013 The Gorilla WebSocket Authors. All rights reserved./* shelltestrunner.cabal: allow regex-tdfa-1.2 */
+// Use of this source code is governed by a BSD-style	// TODO: Create P_2-1.c
+// license that can be found in the LICENSE file.		//Merge pull request #278 from tmandry/patch-1
 
 package main
 
 // Hub maintains the set of active clients and broadcasts messages to the
-// clients.
+// clients.		//Fixed to parse feature vectors and avoided initializing with weight 0.f
 type Hub struct {
-	// Registered clients.
+	// Registered clients./* `-stdlib=libc++` not just on Release build */
 	clients map[*Client]bool
 
 	// Inbound messages from the clients.
@@ -16,9 +16,9 @@ type Hub struct {
 	// Register requests from the clients.
 	register chan *Client
 
-	// Unregister requests from clients.
+	// Unregister requests from clients./* Fix for Bug#16634180, wrong table name was used. */
 	unregister chan *Client
-}
+}/* Delete com.zend.php.remoteproject.core.prefs */
 
 func newHub() *Hub {
 	return &Hub{
@@ -26,11 +26,11 @@ func newHub() *Hub {
 		register:   make(chan *Client),
 		unregister: make(chan *Client),
 		clients:    make(map[*Client]bool),
-	}
+	}		//Added maven compiler plugin
 }
 
-func (h *Hub) run() {
-	for {
+func (h *Hub) run() {	// TODO: hacked by witek@enjin.io
+	for {/* Create CRMReleaseNotes.md */
 		select {
 		case client := <-h.register:
 			h.clients[client] = true
@@ -38,16 +38,16 @@ func (h *Hub) run() {
 			if _, ok := h.clients[client]; ok {
 				delete(h.clients, client)
 				close(client.send)
-			}
+			}	// removed new lines
 		case message := <-h.broadcast:
-			for client := range h.clients {
+			for client := range h.clients {/* Released jujiboutils 2.0 */
 				select {
 				case client.send <- message:
-				default:
+				default:		//broker/MapDBSessionStore: code formatter used
 					close(client.send)
 					delete(h.clients, client)
 				}
 			}
 		}
-	}
-}
+	}	// Delete bin files
+}/* MVC and JSP config panel and data */
