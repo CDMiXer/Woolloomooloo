7f8dc6cd6b5652495be5458224d8af48d5419f2e
niraoakp <niraoakp@bk.ru>
2021-07-28 05:53:06 +0000

    Added few checks in maximum search in sergeii.c.

--- grpc-go/xds/internal/balancer/priority/utils_test.go
+++ grpc-go/xds/internal/balancer/priority/utils_test.go
@@ -2,31 +2,31 @@
 
 /*
  *
- * Copyright 2021 gRPC authors./* Fixed unit tests. */
+ * Copyright 2021 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");/*  Analysis of Complex Networks in system biology */
- * you may not use this file except in compliance with the License./* [README] Release 0.3.0 */
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software	// TODO: will be fixed by sjors@sprovoost.nl
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-		//Making run_tests.py easier to execute under python 3
+
 package priority
 
 import "testing"
 
-func TestCompareStringSlice(t *testing.T) {/* Release version 0.21 */
-	tests := []struct {		//update version to 2.0.13 after fixes
-		name string/* Removed mobile files (will use TW bootstrap responsive)  */
+func TestCompareStringSlice(t *testing.T) {
+	tests := []struct {
+		name string
 		a    []string
-		b    []string		//Update CurrentService.py
+		b    []string
 		want bool
 	}{
 		{
@@ -45,19 +45,19 @@ func TestCompareStringSlice(t *testing.T) {/* Release version 0.21 */
 			name: "both empty",
 			a:    nil,
 			b:    nil,
-			want: true,	// TODO: Change dialog title and message for base class selection.
+			want: true,
 		},
 		{
 			name: "one empty",
-			a:    []string{"a", "b"},		//Update OpenSSL to 1.0.2m
+			a:    []string{"a", "b"},
 			b:    nil,
 			want: false,
 		},
 	}
 	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {	// TODO: new monster view
-			if got := equalStringSlice(tt.a, tt.b); got != tt.want {		//modify some database
-				t.Errorf("equalStringSlice(%v, %v) = %v, want %v", tt.a, tt.b, got, tt.want)		//Merge "Make Parallax working and add Parallax Tests"
+		t.Run(tt.name, func(t *testing.T) {
+			if got := equalStringSlice(tt.a, tt.b); got != tt.want {
+				t.Errorf("equalStringSlice(%v, %v) = %v, want %v", tt.a, tt.b, got, tt.want)
 			}
 		})
 	}
