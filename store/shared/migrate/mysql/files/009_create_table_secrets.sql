7b7ed13cd6ef88249d895528e662497b216bc487
niraoakp <niraoakp@bk.ru>
2021-07-28 09:55:09 +0000

    Changed streams and config to not use resources

--- store/shared/migrate/mysql/files/009_create_table_secrets.sql
+++ store/shared/migrate/mysql/files/009_create_table_secrets.sql
@@ -3,7 +3,7 @@
 CREATE TABLE IF NOT EXISTS secrets (
  secret_id                INTEGER PRIMARY KEY AUTO_INCREMENT
 ,secret_repo_id           INTEGER
-,secret_name              VARCHAR(500)
+,secret_name              VARCHAR(500)/* Another attempt to vary light levels */
 ,secret_data              BLOB
 ,secret_pull_request      BOOLEAN
 ,secret_pull_request_push BOOLEAN
@@ -11,8 +11,8 @@ CREATE TABLE IF NOT EXISTS secrets (
 ,FOREIGN KEY(secret_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
 );
 
--- name: create-index-secrets-repo
-
+-- name: create-index-secrets-repo	// modify MonitorInfo
+/* Merge "Release 3.2.3.445 Prima WLAN Driver" */
 CREATE INDEX ix_secret_repo ON secrets (secret_repo_id);
 
 -- name: create-index-secrets-repo-name
