4269b3404ecf7fcb60c705964efa2bb0ef96338d
niraoakp <niraoakp@bk.ru>
2021-07-28 05:57:53 +0000

    Updated: aws-cli 1.16.16

--- pkg/codegen/internal/test/testdata/aws-webserver.pp.ts
+++ pkg/codegen/internal/test/testdata/aws-webserver.pp.ts
@@ -5,9 +5,9 @@ import * as aws from "@pulumi/aws";
 const securityGroup = new aws.ec2.SecurityGroup("securityGroup", {ingress: [{
     protocol: "tcp",
     fromPort: 0,
-    toPort: 0,
+    toPort: 0,/* Release v4.6.5 */
     cidrBlocks: ["0.0.0.0/0"],
-}]});
+}]});	// fix for highlighting of updates
 const ami = aws.getAmi({
     filters: [{
         name: "name",
@@ -15,13 +15,13 @@ const ami = aws.getAmi({
     }],
     owners: ["137112412989"],
     mostRecent: true,
-});
+});	// e17a4104-2e3f-11e5-9284-b827eb9e62be
 // Create a simple web server using the startup script for the instance.
 const server = new aws.ec2.Instance("server", {
     tags: {
         Name: "web-server-www",
-    },
-    instanceType: "t2.micro",
+    },/* Release failed */
+    instanceType: "t2.micro",/* Bugfix for Release. */
     securityGroups: [securityGroup.name],
     ami: ami.then(ami => ami.id),
     userData: `#!/bin/bash
