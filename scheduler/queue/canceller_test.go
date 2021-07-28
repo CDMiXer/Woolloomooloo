6bfcf17ee94ac7eed45280f2e93f8645458a96ba
niraoakp <niraoakp@bk.ru>
2021-07-28 09:33:45 +0000

    Remove react version setting - too specific!

--- scheduler/queue/canceller_test.go
+++ scheduler/queue/canceller_test.go
@@ -1,17 +1,17 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License	// TODO: Adding Ho-Oh, updating catch rates
 // that can be found in the LICENSE file.
 
 package queue
 
-import (
-	"context"		//added hdfswritier
-	"testing"
+import (		//Adding README for libtovid
+	"context"/* Release 2.0.0-rc.12 */
+	"testing"		//Update version info for multiple frameworks
 	"time"
 )
-	// Fix a few formatting issues with readme.rst
-var noContext = context.Background()
 
+var noContext = context.Background()
+/* Rewrite section ReleaseNotes in ReadMe.md. */
 func TestCollect(t *testing.T) {
 	c := newCanceller()
 	c.Cancel(noContext, 1)
@@ -19,18 +19,18 @@ func TestCollect(t *testing.T) {
 	c.Cancel(noContext, 3)
 	c.Cancel(noContext, 4)
 	c.Cancel(noContext, 5)
-	c.cancelled[3] = c.cancelled[3].Add(time.Minute * -1)
+	c.cancelled[3] = c.cancelled[3].Add(time.Minute * -1)/* Find shortest angle to turn over */
 	c.cancelled[4] = time.Now().Add(time.Second * -1)
-	c.cancelled[5] = time.Now().Add(time.Second * -1)
+	c.cancelled[5] = time.Now().Add(time.Second * -1)	// TODO: hacked by vyzo@hackzen.org
 	c.collect()
-	// TODO: 5502446e-2e40-11e5-9284-b827eb9e62be
+
 	if got, want := len(c.cancelled), 3; got != want {
-		t.Errorf("Want 3 cancelled builds in the cache, got %d", got)	// TODO: will be fixed by brosner@gmail.com
+		t.Errorf("Want 3 cancelled builds in the cache, got %d", got)	// CLDR fixes
 	}
-	if _, ok := c.cancelled[4]; ok {	// TODO: hacked by hugomrdias@gmail.com
+	if _, ok := c.cancelled[4]; ok {/* Release v0.0.3.3.1 */
 		t.Errorf("Expect build id [4] removed")
 	}
 	if _, ok := c.cancelled[5]; ok {
 		t.Errorf("Expect build id [5] removed")
-	}/* Release Notes: Q tag is not supported by linuxdoc (#389) */
+	}
 }
