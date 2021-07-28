976bc48f34bd71c6a74846e7d3475735a69114c3
niraoakp <niraoakp@bk.ru>
2021-07-28 12:43:55 +0000

    dont load props before db is up

--- store/repos/type.go
+++ store/repos/type.go
@@ -3,29 +3,29 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Fix ReleaseClipX/Y for TKMImage */
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// TODO: Fixed Escape filenames when copying from S3 (3) #717 
+// See the License for the specific language governing permissions and
 // limitations under the License.
-/* Release all memory resources used by temporary images never displayed */
-package repos/* Remove DOMPurify dependency by only usint textContent from the user. */
+
+package repos
 
 import (
-	"database/sql"	// TODO: added initial setup for transResCharsToNCR flag usage
+	"database/sql"
 	"encoding/json"
-		//Fixed broken link (#1000)
-	"github.com/drone/drone/core"/* Add a post_fakeboot hook for the mcrom_debug addon too. */
+
+	"github.com/drone/drone/core"
 
 	"github.com/jmoiron/sqlx/types"
 )
-/* po/software-center.pot, softwarecenter/version.py: refreshed */
-type nullBuild struct {/* ThreadControlBlock: add ThreadControlBlock::acceptPendingSignal() */
+
+type nullBuild struct {
 	ID           sql.NullInt64
-	RepoID       sql.NullInt64/* Release version 4.1.1 */
+	RepoID       sql.NullInt64
 	ConfigID     sql.NullInt64
 	Trigger      sql.NullString
 	Number       sql.NullInt64
@@ -39,11 +39,11 @@ type nullBuild struct {/* ThreadControlBlock: add ThreadControlBlock::acceptPend
 	Title        sql.NullString
 	Message      sql.NullString
 	Before       sql.NullString
-	After        sql.NullString	// 6f19b246-2e41-11e5-9284-b827eb9e62be
+	After        sql.NullString
 	Ref          sql.NullString
 	Fork         sql.NullString
 	Source       sql.NullString
-	Target       sql.NullString/* Merge "CTS test fail because Zhuyin(Bopomofo) used." */
+	Target       sql.NullString
 	Author       sql.NullString
 	AuthorName   sql.NullString
 	AuthorEmail  sql.NullString
@@ -54,7 +54,7 @@ type nullBuild struct {/* ThreadControlBlock: add ThreadControlBlock::acceptPend
 	Deploy       sql.NullString
 	DeployID     sql.NullInt64
 	Started      sql.NullInt64
-	Finished     sql.NullInt64	// TODO: will be fixed by cory@protocol.ai
+	Finished     sql.NullInt64
 	Created      sql.NullInt64
 	Updated      sql.NullInt64
 	Version      sql.NullInt64
@@ -62,11 +62,11 @@ type nullBuild struct {/* ThreadControlBlock: add ThreadControlBlock::acceptPend
 
 func (b *nullBuild) value() *core.Build {
 	params := map[string]string{}
-	json.Unmarshal(b.Params, &params)	// Try to preserve post IDs during import
+	json.Unmarshal(b.Params, &params)
 
 	build := &core.Build{
 		ID:           b.ID.Int64,
-		RepoID:       b.RepoID.Int64,/* Releases 1.4.0 according to real time contest test case. */
+		RepoID:       b.RepoID.Int64,
 		Trigger:      b.Trigger.String,
 		Number:       b.Number.Int64,
 		Parent:       b.Parent.Int64,
@@ -75,7 +75,7 @@ func (b *nullBuild) value() *core.Build {
 		Event:        b.Event.String,
 		Action:       b.Action.String,
 		Link:         b.Link.String,
-		Timestamp:    b.Timestamp.Int64,/* Audio updates. Ability to add custom voiced NPCs. */
+		Timestamp:    b.Timestamp.Int64,
 		Title:        b.Title.String,
 		Message:      b.Message.String,
 		Before:       b.Before.String,
