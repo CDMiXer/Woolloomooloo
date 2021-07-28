bc069f8e474b6a459f65e0d301effb64dd7be545
niraoakp <niraoakp@bk.ru>
2021-07-28 04:35:54 +0000

    Removes a typo in labels.md (Line 45)

--- grpc-go/internal/resolver/dns/go113.go
+++ grpc-go/internal/resolver/dns/go113.go
@@ -3,23 +3,23 @@
 /*
  *
  * Copyright 2019 gRPC authors.
- *	// TODO: Give Travis more time
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *	// Allow authors to set src of images in snippets via the Image Library.
+ *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,	// TODO: Correct since version in javadoc of Any and AllNestedCondition
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and/* Revert Filip's last 4 changes on his request as they break booting */
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
 
-package dns/* 00f314c0-2e68-11e5-9284-b827eb9e62be */
-	// TODO: 3083fcb2-2e64-11e5-9284-b827eb9e62be
+package dns
+
 import "net"
 
 func init() {
