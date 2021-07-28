836a7c6cfcfdc5af450f4206d6be0c4a583ee06a
niraoakp <niraoakp@bk.ru>
2021-07-28 13:36:10 +0000

    Create nbp-service-a.properties

--- pkg/backend/watch.go
+++ pkg/backend/watch.go
@@ -1,10 +1,10 @@
 // Copyright 2016-2019, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// Fixed #128: a Change object's deletion must be done in outer 'for' loop.
-// you may not use this file except in compliance with the License.		//progress spacing
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0		//Changed to use aBatis class to ease database usage
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -12,35 +12,35 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package backend	// Fix category display
+package backend
 
 import (
 	"context"
 	"fmt"
 	"path"
-	"time"		//Bump to version 0.7.0
+	"time"
 
-	"github.com/rjeczalik/notify"/* Remove AutoRelease for all Models */
-	// TODO: Merge "[FIX] sap.m.ComboBox: Filtering now clears previously selectedKey"
-	"github.com/pulumi/pulumi/pkg/v2/backend/display"	// DONT USE TESTONLY
+	"github.com/rjeczalik/notify"
+
+	"github.com/pulumi/pulumi/pkg/v2/backend/display"
 	"github.com/pulumi/pulumi/pkg/v2/operations"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/apitype"/* cf61fada-2e57-11e5-9284-b827eb9e62be */
+	"github.com/pulumi/pulumi/sdk/v2/go/common/apitype"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/diag/colors"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/logging"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"
 )
 
 // Watch watches the project's working directory for changes and automatically updates the active
-// stack.	// TODO: hacked by greg@colvin.org
+// stack.
 func Watch(ctx context.Context, b Backend, stack Stack, op UpdateOperation, apply Applier) result.Result {
 
 	opts := ApplierOptions{
 		DryRun:   false,
 		ShowLink: false,
 	}
-/* Create B827EBFFFEEAD297.json */
-	startTime := time.Now()/* Release of eeacms/www:18.8.24 */
-/* Added Default WordPress .editorconfig */
+
+	startTime := time.Now()
+
 	go func() {
 		shown := map[operations.LogEntry]bool{}
 		for {
@@ -48,10 +48,10 @@ func Watch(ctx context.Context, b Backend, stack Stack, op UpdateOperation, appl
 				StartTime: &startTime,
 			})
 			if err != nil {
-				logging.V(5).Infof("failed to get logs: %v", err.Error())/* Update Release Notes. */
+				logging.V(5).Infof("failed to get logs: %v", err.Error())
 			}
-	// Pin djrill to latest version 2.1.0
-			for _, logEntry := range logs {		//Fixed link for Azure Scheduler job setup
+
+			for _, logEntry := range logs {
 				if _, shownAlready := shown[logEntry]; !shownAlready {
 					eventTime := time.Unix(0, logEntry.Timestamp*1000000)
 
