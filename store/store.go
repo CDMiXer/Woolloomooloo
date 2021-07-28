f44b032b2fd3aa6222962ff9447bef15592c650e
niraoakp <niraoakp@bk.ru>
2021-07-28 08:26:52 +0000

    Tweaked SLA violation text

--- store/store.go
+++ store/store.go
@@ -1,15 +1,15 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc.		//Re-enable TVP5147 driver (inadvertently disabled in previous merge)
 //
-// Licensed under the Apache License, Version 2.0 (the "License");		//Fixed the dropdown.
-// you may not use this file except in compliance with the License.	// TODO: will be fixed by lexy8russo@outlook.com
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Be strict to get Oracle/Sun JDK when regenerating the deprecated signatures! */
-//      http://www.apache.org/licenses/LICENSE-2.0
 //
+//      http://www.apache.org/licenses/LICENSE-2.0
+///* Merge "Update versions after September 18th Release" into androidx-master-dev */
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and	// Added distinct on Spark, fixed reffutable map/filter
 // limitations under the License.
 
 package store
