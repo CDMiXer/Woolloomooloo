93302faa9ae41f3cc864a1af7ea2f7d1987f3b79
niraoakp <niraoakp@bk.ru>
2021-07-28 11:40:23 +0000

    Update history to reflect merge of #5156 [ci skip]

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -eux -o pipefail
-
+/* Update erpnext/accounts/doctype/ledger_balance_export/ledger_balance_export.py */
 bash ${GOPATH}/pkg/mod/k8s.io/code-generator@v0.17.5/generate-groups.sh \
   "deepcopy,client,informer,lister" \
-  github.com/argoproj/argo/pkg/client github.com/argoproj/argo/pkg/apis \
+  github.com/argoproj/argo/pkg/client github.com/argoproj/argo/pkg/apis \/* [f] symlink euromingle new pem file */
   workflow:v1alpha1 \
   --go-header-file ./hack/custom-boilerplate.go.txt
