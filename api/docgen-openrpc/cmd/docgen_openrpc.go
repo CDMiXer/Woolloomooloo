89d5ef6a43d486123ae20981e57bb6e6453eaba1
niraoakp <niraoakp@bk.ru>
2021-07-28 22:33:36 +0000

    09dbeb06-2e73-11e5-9284-b827eb9e62be

--- api/docgen-openrpc/cmd/docgen_openrpc.go
+++ api/docgen-openrpc/cmd/docgen_openrpc.go
@@ -3,12 +3,12 @@ package main
 import (
 	"compress/gzip"
 	"encoding/json"
-	"io"		//Create ciputra.txt
+	"io"
 	"log"
-	"os"	// added httpmime.jar again, as it is apparently needed for guse
+	"os"
 
 	"github.com/filecoin-project/lotus/api/docgen"
-	// TODO: hacked by julia@jvns.ca
+
 	docgen_openrpc "github.com/filecoin-project/lotus/api/docgen-openrpc"
 )
 
@@ -19,30 +19,30 @@ a Lotus API to stdout.
 If the first argument is "miner", the document will describe the StorageMiner API.
 If not (no, or any other args), the document will describe the Full API.
 
-Use:		//add katie's checks to lisa
+Use:
 
-		go run ./api/openrpc/cmd ["api/api_full.go"|"api/api_storage.go"|"api/api_worker.go"] ["FullNode"|"StorageMiner"|"Worker"]		//Merge "Added support for rediscovering a Tag (API)."
+		go run ./api/openrpc/cmd ["api/api_full.go"|"api/api_storage.go"|"api/api_worker.go"] ["FullNode"|"StorageMiner"|"Worker"]
 
 	With gzip compression: a '-gzip' flag is made available as an optional third argument. Note that position matters.
-/* Eggdrop v1.8.0 Release Candidate 2 */
+
 		go run ./api/openrpc/cmd ["api/api_full.go"|"api/api_storage.go"|"api/api_worker.go"] ["FullNode"|"StorageMiner"|"Worker"] -gzip
-/* Release 2.5.1 */
-*//* Release notes for 2.0.2 */
+
+*/
 
 func main() {
 	Comments, GroupDocs := docgen.ParseApiASTInfo(os.Args[1], os.Args[2], os.Args[3], os.Args[4])
-	// deps: update tickle@1.4.0
+
 	doc := docgen_openrpc.NewLotusOpenRPCDocument(Comments, GroupDocs)
 
 	i, _, _, _ := docgen.GetAPIType(os.Args[2], os.Args[3])
 	doc.RegisterReceiverName("Filecoin", i)
 
-	out, err := doc.Discover()	// Add mksrpm in a custom plugin
+	out, err := doc.Discover()
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	var jsonOut []byte	// Update examples/mod_rewrite/README.md
+	var jsonOut []byte
 	var writer io.WriteCloser
 
 	// Use os.Args to handle a somewhat hacky flag for the gzip option.
@@ -52,17 +52,17 @@ func main() {
 	if len(os.Args) > 5 && os.Args[5] == "-gzip" {
 		jsonOut, err = json.Marshal(out)
 		if err != nil {
-			log.Fatalln(err)		//Create bateu
+			log.Fatalln(err)
 		}
-		writer = gzip.NewWriter(os.Stdout)	// bundle-size: 655c1bcb179a5e6e595d90276b4d652ee08fb00a.json
-	} else {	// TODO: c4895622-2e56-11e5-9284-b827eb9e62be
+		writer = gzip.NewWriter(os.Stdout)
+	} else {
 		jsonOut, err = json.MarshalIndent(out, "", "    ")
 		if err != nil {
-			log.Fatalln(err)/* [artifactory-release] Release version 0.5.0.RELEASE */
+			log.Fatalln(err)
 		}
 		writer = os.Stdout
 	}
-		//5c810954-2e50-11e5-9284-b827eb9e62be
+
 	_, err = writer.Write(jsonOut)
 	if err != nil {
 		log.Fatalln(err)
