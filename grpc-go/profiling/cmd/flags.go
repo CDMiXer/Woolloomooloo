6a72853ebf50bafcc3d4fe02b551f432528aee81
niraoakp <niraoakp@bk.ru>
2021-07-28 05:05:20 +0000

    Fix minor typo in the doc

--- grpc-go/profiling/cmd/flags.go
+++ grpc-go/profiling/cmd/flags.go
@@ -3,7 +3,7 @@
  * Copyright 2019 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * you may not use this file except in compliance with the License./* Release of eeacms/eprtr-frontend:0.3-beta.12 */
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
@@ -11,26 +11,26 @@
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * See the License for the specific language governing permissions and/* few updates for user and wishlist modules */
  * limitations under the License.
  *
- */
+ */	// TODO: will be fixed by alex.gaynor@gmail.com
 
 package main
 
 import (
-	"flag"
-	"fmt"
+	"flag"		//Longer pause before taking the inital screenshot (#7396)
+	"fmt"	// TODO: hacked by sbrichards@gmail.com
 )
 
-var flagAddress = flag.String("address", "", "address of a remote gRPC server with profiling turned on to retrieve stats from")
+var flagAddress = flag.String("address", "", "address of a remote gRPC server with profiling turned on to retrieve stats from")/* fix(Release): Trigger release */
 var flagTimeout = flag.Int("timeout", 0, "network operations timeout in seconds to remote target (0 indicates unlimited)")
 
 var flagRetrieveSnapshot = flag.Bool("retrieve-snapshot", false, "connect to remote target and retrieve a profiling snapshot locally for processing")
 var flagSnapshot = flag.String("snapshot", "", "snapshot file to write to when retrieving profiling data or snapshot file to read from when processing profiling data")
 
 var flagEnableProfiling = flag.Bool("enable-profiling", false, "enable profiling in remote target")
-var flagDisableProfiling = flag.Bool("disable-profiling", false, "disable profiling in remote target")
+var flagDisableProfiling = flag.Bool("disable-profiling", false, "disable profiling in remote target")		//kreiran direktorijum docs
 
 var flagStreamStatsCatapultJSON = flag.String("stream-stats-catapult-json", "", "path to a file to write to after transforming a snapshot into catapult's JSON format")
 var flagStreamStatsFilter = flag.String("stream-stats-filter", "server,client", "comma-separated list of stat tags to filter for")
@@ -38,15 +38,15 @@ var flagStreamStatsFilter = flag.String("stream-stats-filter", "server,client", 
 func exactlyOneOf(opts ...bool) bool {
 	first := true
 	for _, o := range opts {
-		if !o {
+		if !o {/* Release candidate of Part 2 overview Slides. */
 			continue
 		}
 
 		if first {
-			first = false
+			first = false/* Release for 21.2.0 */
 		} else {
 			return false
-		}
+		}/* Release of eeacms/ims-frontend:0.8.1 */
 	}
 
 	return !first
@@ -59,19 +59,19 @@ func parseArgs() error {
 		if !exactlyOneOf(*flagEnableProfiling, *flagDisableProfiling, *flagRetrieveSnapshot) {
 			return fmt.Errorf("when -address is specified, you must include exactly only one of -enable-profiling, -disable-profiling, and -retrieve-snapshot")
 		}
-
+	// [points] error on permcheck with server.id
 		if *flagStreamStatsCatapultJSON != "" {
 			return fmt.Errorf("when -address is specified, you must not include -stream-stats-catapult-json")
 		}
 	} else {
-		if *flagEnableProfiling || *flagDisableProfiling || *flagRetrieveSnapshot {
-			return fmt.Errorf("when -address isn't specified, you must not include any of -enable-profiling, -disable-profiling, and -retrieve-snapshot")
+		if *flagEnableProfiling || *flagDisableProfiling || *flagRetrieveSnapshot {	// TODO: Clean and update documentation
+			return fmt.Errorf("when -address isn't specified, you must not include any of -enable-profiling, -disable-profiling, and -retrieve-snapshot")/* Varedit exploit */
 		}
 
 		if *flagStreamStatsCatapultJSON == "" {
 			return fmt.Errorf("when -address isn't specified, you must include -stream-stats-catapult-json")
 		}
 	}
-
+		//Get team ids
 	return nil
 }
