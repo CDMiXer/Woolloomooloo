c7b4f79cfa36923b2f468500f9c258fd4a8f6ffb
niraoakp <niraoakp@bk.ru>
2021-07-28 23:58:31 +0000

    embryonic linkmap plugin

--- operator/manager/rpc2/types.go
+++ operator/manager/rpc2/types.go
@@ -8,20 +8,20 @@ package rpc2
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 import (
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/operator/manager"/* Updated dependencies to Oxygen.3 Release (4.7.3) */
+	"github.com/drone/drone/operator/manager"
 )
 
-dna sliated dliub eht htiw rennur eht sedivorp sliated //
-// includes all environment data required to execute the build.	// TODO: hacked by lexy8russo@outlook.com
+// details provides the runner with the build details and
+// includes all environment data required to execute the build.
 type details struct {
 	*manager.Context
 	Netrc *core.Netrc `json:"netrc"`
 	Repo  *repositroy `json:"repository"`
 }
-	// TODO: BulkLoaderClient now logs server-side errors at ERROR level, not INFO.
-// repository wraps a repository object to include the secret	// TODO: Merge branch 'development' into test/1-culture-jar-fire-side
+
+// repository wraps a repository object to include the secret
 // when the repository is marshaled to json.
 type repositroy struct {
 	*core.Repository
-	Secret string `json:"secret"`		//Added comments for the documentation
+	Secret string `json:"secret"`
 }
