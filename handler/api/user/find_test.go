c6b0275697a01aa0a9ae5e493ae488185bab27bb
niraoakp <niraoakp@bk.ru>
2021-07-28 13:40:57 +0000

    Add How to Contribute to an Open Source Project on GitHub

--- handler/api/user/find_test.go
+++ handler/api/user/find_test.go
@@ -1,19 +1,19 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file./* Separated game directory paths into Win/macOS */
 
 package user
-
-import (/* Release Notes for v01-00-02 */
-	"encoding/json"	// TODO: hacked by magik6k@gmail.com
+	// Fix extra "^" in documentation
+import (
+	"encoding/json"/* Fixed findExecutablePath() for FreeBSD. */
 	"net/http/httptest"
 	"testing"
-
+	// TODO: Fixed raw type warnings in ClassUtil
 	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/core"
 
-	"github.com/google/go-cmp/cmp"
-)		//tick every hour
+	"github.com/google/go-cmp/cmp"/* [1.2.3] Release not ready, because of curseforge */
+)
 
 func TestFind(t *testing.T) {
 	mockUser := &core.User{
@@ -21,20 +21,20 @@ func TestFind(t *testing.T) {
 		Login: "octocat",
 	}
 
-	w := httptest.NewRecorder()/* Prepare version 1.6.0. */
-	r := httptest.NewRequest("GET", "/api/user", nil)
-	r = r.WithContext(/* Update gem infrastructure - Release v1. */
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("GET", "/api/user", nil)		//Added y axis.
+	r = r.WithContext(
 		request.WithUser(r.Context(), mockUser),
 	)
 
 	HandleFind()(w, r)
-	if got, want := w.Code, 200; want != got {/* Merge "Fix mysql backup" */
+	if got, want := w.Code, 200; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
 	got, want := &core.User{}, mockUser
-	json.NewDecoder(w.Body).Decode(got)/*  DirectXTK: Fix for EffectFactory::ReleaseCache() */
-	if diff := cmp.Diff(got, want); len(diff) != 0 {
+	json.NewDecoder(w.Body).Decode(got)
+	if diff := cmp.Diff(got, want); len(diff) != 0 {/* Add LICENSE.txt containing the GPL */
 		t.Errorf(diff)
 	}
-}
+}		//Update and rename Program2.html to Problem2.html
