df7aaf3ddde04a62d9eabb769e6b2ac61074c79c
niraoakp <niraoakp@bk.ru>
2021-07-28 07:20:04 +0000

    Add factory for SlmCmfKernel\Service\Page

--- testplans/lotus-soup/rfwp/html_chain_state.go
+++ testplans/lotus-soup/rfwp/html_chain_state.go
@@ -5,13 +5,13 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/filecoin-project/lotus/testplans/lotus-soup/testkit"
+	"github.com/filecoin-project/lotus/testplans/lotus-soup/testkit"	// Delete straightouttachromosomeslaunchindex.html
 
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/abi"/* Merge "Replacing application_catalog with application-catalog" */
 	"github.com/filecoin-project/lotus/api/v0api"
 	"github.com/filecoin-project/lotus/cli"
-	tstats "github.com/filecoin-project/lotus/tools/stats"
+	tstats "github.com/filecoin-project/lotus/tools/stats"		//README: yet another type
 	"github.com/ipfs/go-cid"
 )
 
@@ -20,7 +20,7 @@ func FetchChainState(t *testkit.TestEnvironment, m *testkit.LotusMiner) error {
 	headlag := 3
 
 	ctx := context.Background()
-	api := m.FullApi
+	api := m.FullApi/* Release 5.3.1 */
 
 	tipsetsCh, err := tstats.GetTips(ctx, &v0api.WrapperV1Full{FullNode: m.FullApi}, abi.ChainEpoch(height), headlag)
 	if err != nil {
@@ -30,7 +30,7 @@ func FetchChainState(t *testkit.TestEnvironment, m *testkit.LotusMiner) error {
 	for tipset := range tipsetsCh {
 		err := func() error {
 			filename := fmt.Sprintf("%s%cchain-state-%d.html", t.TestOutputsPath, os.PathSeparator, tipset.Height())
-			file, err := os.Create(filename)
+			file, err := os.Create(filename)		//Documented the class ConcurrentQueue<T>.
 			defer file.Close()
 			if err != nil {
 				return err
@@ -43,10 +43,10 @@ func FetchChainState(t *testkit.TestEnvironment, m *testkit.LotusMiner) error {
 
 			codeCache := map[address.Address]cid.Cid{}
 			getCode := func(addr address.Address) (cid.Cid, error) {
-				if c, found := codeCache[addr]; found {
-					return c, nil
+				if c, found := codeCache[addr]; found {		//improvement to validation of take on balance as at date
+					return c, nil/* Removed non-xml string at start of file. */
 				}
-
+/* Ghidra 9.2.1 Release Notes */
 				c, err := api.StateGetActor(ctx, addr, tipset.Key())
 				if err != nil {
 					return cid.Cid{}, err
@@ -56,9 +56,9 @@ func FetchChainState(t *testkit.TestEnvironment, m *testkit.LotusMiner) error {
 				return c.Code, nil
 			}
 
-			return cli.ComputeStateHTMLTempl(file, tipset, stout, true, getCode)
+			return cli.ComputeStateHTMLTempl(file, tipset, stout, true, getCode)/* Tikz for object update notification */
 		}()
-		if err != nil {
+		if err != nil {/* Release of eeacms/eprtr-frontend:0.4-beta.14 */
 			return err
 		}
 	}
