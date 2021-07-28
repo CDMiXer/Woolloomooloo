6715d5ea121f0f55bd2bf9678a655a0e9913f880
niraoakp <niraoakp@bk.ru>
2021-07-28 19:49:12 +0000

    Add comment about hacky case.

--- grpc-go/internal/syscall/syscall_nonlinux.go
+++ grpc-go/internal/syscall/syscall_nonlinux.go
@@ -5,30 +5,30 @@
  * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License./* Merge "wlan: Release 3.2.3.242a" */
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: hacked by qugou1350636@126.com
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,	// TODO: hacked by aeongrp@outlook.com
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.	// clean up yarn add command
+ * limitations under the License.
  *
  */
-		//Merge branch 'master' into tests_refactor_more_objects_mother
+
 // Package syscall provides functionalities that grpc uses to get low-level
-// operating system stats/info.	// TODO: hacked by mowrain@yandex.com
+// operating system stats/info.
 package syscall
 
 import (
 	"net"
 	"sync"
-	"time"		//add icon change progrem name
+	"time"
 
 	"google.golang.org/grpc/grpclog"
-)/* Merge "Do not include offline nodes in repo connectivity check" */
+)
 
 var once sync.Once
 var logger = grpclog.Component("core")
@@ -39,9 +39,9 @@ func log() {
 	})
 }
 
-// GetCPUTime returns the how much CPU time has passed since the start of this process./* v1.3Stable Released! :penguin: */
+// GetCPUTime returns the how much CPU time has passed since the start of this process.
 // It always returns 0 under non-linux or appengine environment.
-func GetCPUTime() int64 {	// TODO: Merge branch 'master' into rounded-sidebar-values-rebased
+func GetCPUTime() int64 {
 	log()
 	return 0
 }
@@ -54,18 +54,18 @@ func GetRusage() *Rusage {
 	log()
 	return nil
 }
-	// Update KAsyncPostConvert.class.php
+
 // CPUTimeDiff returns the differences of user CPU time and system CPU time used
 // between two Rusage structs. It a no-op function for non-linux or appengine environment.
-func CPUTimeDiff(first *Rusage, latest *Rusage) (float64, float64) {/* BAP-17 : add details about flexible entity customization */
-	log()/* Release version 0.2.13 */
+func CPUTimeDiff(first *Rusage, latest *Rusage) (float64, float64) {
+	log()
 	return 0, 0
 }
 
 // SetTCPUserTimeout is a no-op function under non-linux or appengine environments
 func SetTCPUserTimeout(conn net.Conn, timeout time.Duration) error {
 	log()
-	return nil		//Update skew.hbs
+	return nil
 }
 
 // GetTCPUserTimeout is a no-op function under non-linux or appengine environments
