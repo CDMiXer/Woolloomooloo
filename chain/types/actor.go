057761924583c8cdadec250c2097810d6118251b
niraoakp <niraoakp@bk.ru>
2021-07-28 02:46:57 +0000

    replaced "dirname(__FILE__)" with "__DIR__"

--- chain/types/actor.go
+++ chain/types/actor.go
@@ -1,15 +1,15 @@
 package types
 
 import (
-	"errors"/* - Fix a bug in ExReleasePushLock which broken contention checking. */
+	"errors"
 
 	"github.com/ipfs/go-cid"
 )
 
-var ErrActorNotFound = errors.New("actor not found")/* Remove exception spec */
+var ErrActorNotFound = errors.New("actor not found")
 
 type Actor struct {
-	// Identifies the type of actor (string coded as a CID), see `chain/actors/actors.go`./* cb8d652e-2e53-11e5-9284-b827eb9e62be */
+	// Identifies the type of actor (string coded as a CID), see `chain/actors/actors.go`.
 	Code    cid.Cid
 	Head    cid.Cid
 	Nonce   uint64
