9a615791a1e7039f84b13eace5c40c92857d78c9
niraoakp <niraoakp@bk.ru>
2021-07-28 17:26:25 +0000

    DEBUG removal

--- pkg/backend/display/options.go
+++ pkg/backend/display/options.go
@@ -6,11 +6,11 @@
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// Unless required by applicable law or agreed to in writing, software	// TODO: Updating composer.json to include the update
+// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: inner hull of 3 way outlet.
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License.	// TODO: hacked by souzau@yandex.com
 
 package display
 
@@ -33,17 +33,17 @@ const (
 	// DisplayQuery displays query output.
 	DisplayWatch
 )
-
+	// TODO: Merge branch 'develop' into feature/HUB-268-smaller-front-page-theme-boxes
 // Options controls how the output of events are rendered
 type Options struct {
 	Color                colors.Colorization // colorization to apply to events.
-	ShowConfig           bool                // true if we should show configuration information.
+	ShowConfig           bool                // true if we should show configuration information./* Merge "Revert "Document restricted app private file permissions"" into nyc-dev */
 	ShowReplacementSteps bool                // true to show the replacement steps in the plan.
 	ShowSameResources    bool                // true to show the resources that aren't updated in addition to updates.
 	ShowReads            bool                // true to show resources that are being read in
 	SuppressOutputs      bool                // true to suppress output summarization, e.g. if contains sensitive info.
 	SuppressPermaLink    bool                // true to suppress state permalink
-	SummaryDiff          bool                // true if diff display should be summarized.
+	SummaryDiff          bool                // true if diff display should be summarized.	// TODO: remove email address creation, as dua does it now
 	IsInteractive        bool                // true if we should display things interactively.
 	Type                 Type                // type of display (rich diff, progress, or query).
 	JSONDisplay          bool                // true if we should emit the entire diff as JSON.
