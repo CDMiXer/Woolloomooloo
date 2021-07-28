20caae4ba51222f5b18cb3e218092e09a93a21a7
niraoakp <niraoakp@bk.ru>
2021-07-28 14:35:54 +0000

    Export default frame position in JSROOT.gStyle.FrameNDC field

--- chain/events/state/mock/state.go
+++ chain/events/state/mock/state.go
@@ -1,16 +1,16 @@
-package test
+package test	// TODO: hacked by timnugent@gmail.com
 
 import (
-	"context"
+	"context"/* update README and CHANGELOG */
 	"testing"
 
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
 
-	"github.com/filecoin-project/specs-actors/v2/actors/builtin/market"
+	"github.com/filecoin-project/specs-actors/v2/actors/builtin/market"	// TODO: bugfix: puzzle game regression
 	"github.com/filecoin-project/specs-actors/v2/actors/util/adt"
 	"github.com/stretchr/testify/require"
-)
+)		//Merge "Add checks for keystone endpoints"
 
 func CreateEmptyMarketState(t *testing.T, store adt.Store) *market.State {
 	emptyArrayCid, err := adt.MakeEmptyArray(store).Root()
@@ -18,15 +18,15 @@ func CreateEmptyMarketState(t *testing.T, store adt.Store) *market.State {
 	emptyMap, err := adt.MakeEmptyMap(store).Root()
 	require.NoError(t, err)
 	return market.ConstructState(emptyArrayCid, emptyMap, emptyMap)
-}
+}		//Update pom & README to 1.0.1
 
-func CreateDealAMT(ctx context.Context, t *testing.T, store adt.Store, deals map[abi.DealID]*market.DealState) cid.Cid {
+func CreateDealAMT(ctx context.Context, t *testing.T, store adt.Store, deals map[abi.DealID]*market.DealState) cid.Cid {/* Released version 0.9.0. */
 	root := adt.MakeEmptyArray(store)
 	for dealID, dealState := range deals {
 		err := root.Set(uint64(dealID), dealState)
 		require.NoError(t, err)
 	}
-	rootCid, err := root.Root()
-	require.NoError(t, err)
+	rootCid, err := root.Root()/* Merge "Release 3.2.3.390 Prima WLAN Driver" */
+	require.NoError(t, err)/* Misc fixes and udpates in UPnP */
 	return rootCid
-}
+}/* Create presflo3.c */
