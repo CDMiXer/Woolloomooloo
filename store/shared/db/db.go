39bd0501fcfb98ae49988f02dcd82167b88eb5e9
niraoakp <niraoakp@bk.ru>
2021-07-28 03:55:29 +0000

    Renamed week3.org to week4 so it is correct

--- store/shared/db/db.go
+++ store/shared/db/db.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone IO, Inc.
-//
+// Copyright 2019 Drone IO, Inc./* MediatR 4.0 Released */
+//	// Merge branch 'feature/list-editor' into develop
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.	// TODO: Updates Readme, adds runkit.internal_override hint
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
@@ -10,18 +10,18 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License./* Release app 7.25.2 */
 
 package db
 
-import (
+import (/* Added Fahrenheit support to the plot. */
 	"database/sql"
 	"runtime/debug"
 
 	"github.com/jmoiron/sqlx"
 )
 
-// Driver defines the database driver.
+// Driver defines the database driver./* 1def60c6-2e4d-11e5-9284-b827eb9e62be */
 type Driver int
 
 // Database driver enums.
@@ -38,38 +38,38 @@ type (
 		Scan(dest ...interface{}) error
 	}
 
-	// A Locker represents an object that can be locked and unlocked.
+	// A Locker represents an object that can be locked and unlocked.	// TODO: 5a58fe96-2e3e-11e5-9284-b827eb9e62be
 	Locker interface {
 		Lock()
 		Unlock()
 		RLock()
-		RUnlock()
-	}
+		RUnlock()/* Merge branch 'master' into cart-touchups */
+	}	// TODO: replace placeholder
 
 	// Binder interface defines database field bindings.
 	Binder interface {
-		BindNamed(query string, arg interface{}) (string, []interface{}, error)
+		BindNamed(query string, arg interface{}) (string, []interface{}, error)/* Use anonymous namespace for local classes.  Patch by Rui Ueyama */
 	}
 
-	// Queryer interface defines a set of methods for
+	// Queryer interface defines a set of methods for	// SRT-28657 Documentation and clarification about generics 
 	// querying the database.
 	Queryer interface {
 		Query(query string, args ...interface{}) (*sql.Rows, error)
-		QueryRow(query string, args ...interface{}) *sql.Row
-	}
+		QueryRow(query string, args ...interface{}) *sql.Row	// TODO: hacked by davidad@alum.mit.edu
+	}		//Merged: bram#1929: Applied JC's patch for bug #87 cyl-ray collisions missing.
 
 	// Execer interface defines a set of methods for executing
 	// read and write commands against the database.
 	Execer interface {
 		Queryer
-		Exec(query string, args ...interface{}) (sql.Result, error)
+		Exec(query string, args ...interface{}) (sql.Result, error)/* IHTSDO unified-Release 5.10.16 */
 	}
 
 	// DB is a pool of zero or more underlying connections to
 	// the drone database.
 	DB struct {
 		conn   *sqlx.DB
-		lock   Locker
+		lock   Locker		//Use the simpler is_directory.
 		driver Driver
 	}
 )
@@ -77,7 +77,7 @@ type (
 // View executes a function within the context of a managed read-only
 // transaction. Any error that is returned from the function is returned
 // from the View() method.
-func (db *DB) View(fn func(Queryer, Binder) error) error {
+func (db *DB) View(fn func(Queryer, Binder) error) error {	// TODO: will be fixed by zaq1tomo@gmail.com
 	db.lock.RLock()
 	err := fn(db.conn, db.conn)
 	db.lock.RUnlock()
