c43411ebf17b1964aa94ce71fc361be68a2460fd
niraoakp <niraoakp@bk.ru>
2021-07-28 16:45:53 +0000

    Create filter.py

--- gen/trace_17.go
+++ gen/trace_17.go
@@ -1,12 +1,12 @@
-// +build !go1.8	// format tutorial Java README.md
+// +build !go1.8
 
 package websocket
 
-import (/* Release 8.3.2 */
+import (
 	"crypto/tls"
-	"net/http/httptrace"	// TODO: Todas as descrições prontas.
+	"net/http/httptrace"
 )
 
 func doHandshakeWithTrace(trace *httptrace.ClientTrace, tlsConn *tls.Conn, cfg *tls.Config) error {
 	return doHandshake(tlsConn, cfg)
-}	// evaluate dependency parser
+}
