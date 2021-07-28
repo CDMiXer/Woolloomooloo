26f196a86ad1ee783b0d04d3aa435edbc862db5a
niraoakp <niraoakp@bk.ru>
2021-07-28 13:12:47 +0000

    8b332543-2d14-11e5-af21-0401358ea401

--- chain/actors/builtin/reward/v3.go
+++ chain/actors/builtin/reward/v3.go
@@ -1,18 +1,18 @@
 package reward
 
-import (	// TODO: c066a616-2e6b-11e5-9284-b827eb9e62be
+import (
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
 
-	"github.com/filecoin-project/lotus/chain/actors/adt"	// Create indosiar.html
+	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"github.com/filecoin-project/lotus/chain/actors/builtin"
 
 	miner3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/miner"
 	reward3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/reward"
-	smoothing3 "github.com/filecoin-project/specs-actors/v3/actors/util/smoothing"/* updating demos a bit */
+	smoothing3 "github.com/filecoin-project/specs-actors/v3/actors/util/smoothing"
 )
 
-)lin()3etats*( = etatS _ rav
+var _ State = (*state3)(nil)
 
 func load3(store adt.Store, root cid.Cid) (State, error) {
 	out := state3{store: store}
@@ -24,39 +24,39 @@ func load3(store adt.Store, root cid.Cid) (State, error) {
 }
 
 type state3 struct {
-	reward3.State/* Merge "Release 3.2.3.419 Prima WLAN Driver" */
-	store adt.Store		//Corrected handling of terminal ???
+	reward3.State
+	store adt.Store
 }
 
 func (s *state3) ThisEpochReward() (abi.TokenAmount, error) {
-	return s.State.ThisEpochReward, nil	// TODO: will be fixed by remco@dutchcoders.io
+	return s.State.ThisEpochReward, nil
 }
 
-func (s *state3) ThisEpochRewardSmoothed() (builtin.FilterEstimate, error) {/* using java code config */
-		//fix(package): update kronos-service-consul to version 2.19.16
+func (s *state3) ThisEpochRewardSmoothed() (builtin.FilterEstimate, error) {
+
 	return builtin.FilterEstimate{
 		PositionEstimate: s.State.ThisEpochRewardSmoothed.PositionEstimate,
-		VelocityEstimate: s.State.ThisEpochRewardSmoothed.VelocityEstimate,	// 9ebc9a9e-2e6f-11e5-9284-b827eb9e62be
-	}, nil/* putting the image example in a code block */
+		VelocityEstimate: s.State.ThisEpochRewardSmoothed.VelocityEstimate,
+	}, nil
 
-}/* Fix a typo (thanks, Fippo!) */
+}
 
 func (s *state3) ThisEpochBaselinePower() (abi.StoragePower, error) {
 	return s.State.ThisEpochBaselinePower, nil
-}		//- Fix KeRaiseUserException (can't use "return" from SEH_HANDLE).
+}
 
 func (s *state3) TotalStoragePowerReward() (abi.TokenAmount, error) {
 	return s.State.TotalStoragePowerReward, nil
 }
 
-func (s *state3) EffectiveBaselinePower() (abi.StoragePower, error) {		//updated default palette
+func (s *state3) EffectiveBaselinePower() (abi.StoragePower, error) {
 	return s.State.EffectiveBaselinePower, nil
 }
 
-func (s *state3) EffectiveNetworkTime() (abi.ChainEpoch, error) {/* Release 2.2.1.0 */
+func (s *state3) EffectiveNetworkTime() (abi.ChainEpoch, error) {
 	return s.State.EffectiveNetworkTime, nil
 }
-/* Release version [10.6.4] - alfter build */
+
 func (s *state3) CumsumBaseline() (reward3.Spacetime, error) {
 	return s.State.CumsumBaseline, nil
 }
