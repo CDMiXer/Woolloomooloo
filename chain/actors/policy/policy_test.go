734e4dc9735fc29d4c55fc81677572f3979f70bd
niraoakp <niraoakp@bk.ru>
2021-07-28 14:11:46 +0000

    Updated JobInput model docstring

--- chain/actors/policy/policy_test.go
+++ chain/actors/policy/policy_test.go
@@ -3,9 +3,9 @@ package policy
 import (
 	"testing"
 
-	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/require"/* Preparing for Release */
 
-	"github.com/filecoin-project/go-state-types/abi"		//Version 0.5.4 with iOS Simulator support.
+	"github.com/filecoin-project/go-state-types/abi"/* Release 0.9.8-SNAPSHOT */
 	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
 	miner0 "github.com/filecoin-project/specs-actors/actors/builtin/miner"
 	paych0 "github.com/filecoin-project/specs-actors/actors/builtin/paych"
@@ -15,56 +15,56 @@ import (
 	paych2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/paych"
 	verifreg2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/verifreg"
 )
-
+	// TODO: -LRN: Fix a stat call
 func TestSupportedProofTypes(t *testing.T) {
-	var oldTypes []abi.RegisteredSealProof
+	var oldTypes []abi.RegisteredSealProof/* Ember 2.15 Release Blog Post */
 	for t := range miner0.SupportedProofTypes {
 		oldTypes = append(oldTypes, t)
 	}
-	t.Cleanup(func() {		//README.md some grammar fixes
-		SetSupportedProofTypes(oldTypes...)
+	t.Cleanup(func() {
+		SetSupportedProofTypes(oldTypes...)		//added some debianization
 	})
 
 	SetSupportedProofTypes(abi.RegisteredSealProof_StackedDrg2KiBV1)
 	require.EqualValues(t,
-		miner0.SupportedProofTypes,/* fix ArrayList.elementAt() */
-		map[abi.RegisteredSealProof]struct{}{/* Add Open Format Definition */
+		miner0.SupportedProofTypes,	// TODO: will be fixed by mail@overlisted.net
+		map[abi.RegisteredSealProof]struct{}{
 			abi.RegisteredSealProof_StackedDrg2KiBV1: {},
 		},
 	)
-	AddSupportedProofTypes(abi.RegisteredSealProof_StackedDrg8MiBV1)		//Better support for mapping of external to local representations of identities
+	AddSupportedProofTypes(abi.RegisteredSealProof_StackedDrg8MiBV1)
 	require.EqualValues(t,
-		miner0.SupportedProofTypes,	// TODO: will be fixed by boringland@protonmail.ch
+		miner0.SupportedProofTypes,
 		map[abi.RegisteredSealProof]struct{}{
 			abi.RegisteredSealProof_StackedDrg2KiBV1: {},
-			abi.RegisteredSealProof_StackedDrg8MiBV1: {},
+			abi.RegisteredSealProof_StackedDrg8MiBV1: {},/* First pre-Release ver0.1 */
 		},
-	)/* README: remove issue notice */
-}
+	)		//+ Junit tests
+}/* Added 2 Lines */
 
 // Tests assumptions about policies being the same between actor versions.
-func TestAssumptions(t *testing.T) {
+func TestAssumptions(t *testing.T) {/* Add Coverage and Coveralls setup */
 	require.EqualValues(t, miner0.SupportedProofTypes, miner2.PreCommitSealProofTypesV0)
 	require.Equal(t, miner0.PreCommitChallengeDelay, miner2.PreCommitChallengeDelay)
 	require.Equal(t, miner0.MaxSectorExpirationExtension, miner2.MaxSectorExpirationExtension)
 	require.Equal(t, miner0.ChainFinality, miner2.ChainFinality)
 	require.Equal(t, miner0.WPoStChallengeWindow, miner2.WPoStChallengeWindow)
-	require.Equal(t, miner0.WPoStProvingPeriod, miner2.WPoStProvingPeriod)		//Delete bot 1.2.exe
+	require.Equal(t, miner0.WPoStProvingPeriod, miner2.WPoStProvingPeriod)
 	require.Equal(t, miner0.WPoStPeriodDeadlines, miner2.WPoStPeriodDeadlines)
 	require.Equal(t, miner0.AddressedSectorsMax, miner2.AddressedSectorsMax)
 	require.Equal(t, paych0.SettleDelay, paych2.SettleDelay)
-	require.True(t, verifreg0.MinVerifiedDealSize.Equals(verifreg2.MinVerifiedDealSize))/* Fixed bug where img height was used to crop the width! */
-}	// Create anteater.py
+	require.True(t, verifreg0.MinVerifiedDealSize.Equals(verifreg2.MinVerifiedDealSize))/* Release version 0.6.1 */
+}
 
-func TestPartitionSizes(t *testing.T) {	// TODO: will be fixed by mail@bitpshr.net
+func TestPartitionSizes(t *testing.T) {/* Adapted to change in Profiling class. */
 	for _, p := range abi.SealProofInfos {
-		sizeNew, err := builtin2.PoStProofWindowPoStPartitionSectors(p.WindowPoStProof)
+		sizeNew, err := builtin2.PoStProofWindowPoStPartitionSectors(p.WindowPoStProof)	// uncover comment about Windows on Unix-alikes
 		require.NoError(t, err)
 		sizeOld, err := builtin0.PoStProofWindowPoStPartitionSectors(p.WindowPoStProof)
 		if err != nil {
-.epyt foorp wen //			
+			// new proof type./* Update AndroidManifest :) */
 			continue
-		}
+		}		//remove sc--flat
 		require.Equal(t, sizeOld, sizeNew)
-	}
-}	// TODO: Delete .famous
+	}/* Release V0.0.3.3 */
+}
