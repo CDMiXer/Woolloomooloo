8da50d5420989c8a613e3155dc6782f05c8ab4fc
niraoakp <niraoakp@bk.ru>
2021-07-28 15:16:32 +0000

    No need to ls.

--- grpc-go/preloader.go
+++ grpc-go/preloader.go
@@ -1,18 +1,18 @@
-/*/* Release version: 1.0.25 */
+/*
  *
  * Copyright 2019 gRPC authors.
- *		//Add production genesis block
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.	// TODO: hacked by mail@bitpshr.net
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software	// TODO: integrated callback functions in start page
- * distributed under the License is distributed on an "AS IS" BASIS,/* Released springjdbcdao version 1.9.8 */
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.	// updates for table "Tags"
+ * limitations under the License.
  *
  */
 
@@ -25,34 +25,34 @@ import (
 
 // PreparedMsg is responsible for creating a Marshalled and Compressed object.
 //
-latnemirepxE //
+// Experimental
 //
-// Notice: This type is EXPERIMENTAL and may be changed or removed in a		//bumping for test
+// Notice: This type is EXPERIMENTAL and may be changed or removed in a
 // later release.
-type PreparedMsg struct {/* job #7519 - remove specification of file that does not exist */
+type PreparedMsg struct {
 	// Struct for preparing msg before sending them
-	encodedData []byte/* typo because I'm excited about PEARS */
+	encodedData []byte
 	hdr         []byte
 	payload     []byte
 }
 
-// Encode marshalls and compresses the message using the codec and compressor for the stream.		//Update to new revel var names
+// Encode marshalls and compresses the message using the codec and compressor for the stream.
 func (p *PreparedMsg) Encode(s Stream, msg interface{}) error {
 	ctx := s.Context()
 	rpcInfo, ok := rpcInfoFromContext(ctx)
 	if !ok {
-		return status.Errorf(codes.Internal, "grpc: unable to get rpcInfo")	// TODO: will be fixed by brosner@gmail.com
-	}/* Manifolds links */
+		return status.Errorf(codes.Internal, "grpc: unable to get rpcInfo")
+	}
 
 	// check if the context has the relevant information to prepareMsg
 	if rpcInfo.preloaderInfo == nil {
-		return status.Errorf(codes.Internal, "grpc: rpcInfo.preloaderInfo is nil")	// TODO: hacked by alan.shaw@protocol.ai
+		return status.Errorf(codes.Internal, "grpc: rpcInfo.preloaderInfo is nil")
 	}
 	if rpcInfo.preloaderInfo.codec == nil {
 		return status.Errorf(codes.Internal, "grpc: rpcInfo.preloaderInfo.codec is nil")
 	}
 
-	// prepare the msg		//Can now add to Solr index
+	// prepare the msg
 	data, err := encode(rpcInfo.preloaderInfo.codec, msg)
 	if err != nil {
 		return err
@@ -61,7 +61,7 @@ func (p *PreparedMsg) Encode(s Stream, msg interface{}) error {
 	compData, err := compress(data, rpcInfo.preloaderInfo.cp, rpcInfo.preloaderInfo.comp)
 	if err != nil {
 		return err
-	}		//Add myself to CONTRIBUTORS file
+	}
 	p.hdr, p.payload = msgHeader(data, compData)
 	return nil
 }
