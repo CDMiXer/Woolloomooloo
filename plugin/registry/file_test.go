a671fc3a03534d7af8bc679c775dbb3c12291305
niraoakp <niraoakp@bk.ru>
2021-07-28 22:16:25 +0000

    Adding Device Simulator

--- plugin/registry/file_test.go
+++ plugin/registry/file_test.go
@@ -1,9 +1,9 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* Updated binary to latest version */
+// that can be found in the LICENSE file.
 
 // +build !oss
-/* Merge "QCamera2: Add OMX extension to set JPEG encoder speed mode" */
+
 package registry
 
 import (
@@ -12,19 +12,19 @@ import (
 
 	"github.com/drone/drone/core"
 	"github.com/google/go-cmp/cmp"
-)/* Release 1.9.2.0 */
+)
 
 func TestFileSource(t *testing.T) {
 	source := FileSource("./auths/testdata/config.json")
 	got, err := source.List(noContext, &core.RegistryArgs{})
 	if err != nil {
 		t.Error(err)
-	}		//added MissingDocIds error and test
+	}
 	want := []*core.Registry{
 		{
 			Address:  "https://index.docker.io/v1/",
 			Username: "octocat",
-			Password: "correct-horse-battery-staple",	// TODO: hacked by ng8eke@163.com
+			Password: "correct-horse-battery-staple",
 		},
 	}
 	if diff := cmp.Diff(got, want); diff != "" {
@@ -38,4 +38,4 @@ func TestFileSourceErr(t *testing.T) {
 	if _, ok := err.(*os.PathError); !ok {
 		t.Errorf("Expect error when file does not exist")
 	}
-}	// TODO: Delete cd-rom.jpg
+}
