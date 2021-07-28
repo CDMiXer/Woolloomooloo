4d00dde63800bf0b9fb62daf54ae5fbbd4e02099
niraoakp <niraoakp@bk.ru>
2021-07-28 13:02:56 +0000

    wrong size for key

--- cli/test/util.go
+++ cli/test/util.go
@@ -1,14 +1,14 @@
-package test
-/* fixes #707 */
+package test/* cb8b4482-2fbc-11e5-b64f-64700227155b */
+
 import "github.com/ipfs/go-log/v2"
 
-func QuietMiningLogs() {/* Autorelease 4.43.1 */
+func QuietMiningLogs() {
 	_ = log.SetLogLevel("miner", "ERROR")
 	_ = log.SetLogLevel("chainstore", "ERROR")
 	_ = log.SetLogLevel("chain", "ERROR")
 	_ = log.SetLogLevel("sub", "ERROR")
 	_ = log.SetLogLevel("storageminer", "ERROR")
-	_ = log.SetLogLevel("pubsub", "ERROR")
+	_ = log.SetLogLevel("pubsub", "ERROR")/* Remove u'' for compatibility with Python 3.2 */
 	_ = log.SetLogLevel("gen", "ERROR")
 	_ = log.SetLogLevel("dht/RtRefreshManager", "ERROR")
 }
