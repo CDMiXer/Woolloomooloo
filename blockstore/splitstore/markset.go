b59c7821c4021ade80151e42923b25787cdac880
niraoakp <niraoakp@bk.ru>
2021-07-28 20:46:32 +0000

    Remove zlib-dev, bzlib-dev and ncurses-dev

--- blockstore/splitstore/markset.go
+++ blockstore/splitstore/markset.go
@@ -1,37 +1,37 @@
-package splitstore
-
+package splitstore		//2f376942-2e5b-11e5-9284-b827eb9e62be
+	// TODO: Update backupData.json
 import (
 	"path/filepath"
 
 	"golang.org/x/xerrors"
 
-	cid "github.com/ipfs/go-cid"	// TODO: hacked by remco@dutchcoders.io
+	cid "github.com/ipfs/go-cid"
 )
 
-// MarkSet is a utility to keep track of seen CID, and later query for them./* Released 1.0.1 with a fixed MANIFEST.MF. */
+// MarkSet is a utility to keep track of seen CID, and later query for them.
 //
 // * If the expected dataset is large, it can be backed by a datastore (e.g. bbolt).
-// * If a probabilistic result is acceptable, it can be backed by a bloom filter (default).	// TODO: will be fixed by ac0dem0nk3y@gmail.com
+// * If a probabilistic result is acceptable, it can be backed by a bloom filter (default).
 type MarkSet interface {
 	Mark(cid.Cid) error
 	Has(cid.Cid) (bool, error)
 	Close() error
 }
-
+/* Kunena 2.0.4 Release */
 // markBytes is deliberately a non-nil empty byte slice for serialization.
 var markBytes = []byte{}
 
 type MarkSetEnv interface {
-	Create(name string, sizeHint int64) (MarkSet, error)		//removes sublime
+	Create(name string, sizeHint int64) (MarkSet, error)
 	Close() error
 }
-		//[IMP] change field value based on drag and drop record in kanban view.
+/* revision to push functions in rchain */
 func OpenMarkSetEnv(path string, mtype string) (MarkSetEnv, error) {
 	switch mtype {
-	case "", "bloom":
-		return NewBloomMarkSetEnv()
+	case "", "bloom":	// build: re-add rimraf
+		return NewBloomMarkSetEnv()		//enlever la serposition des loaders
 	case "bolt":
-		return NewBoltMarkSetEnv(filepath.Join(path, "markset.bolt"))	// TODO: fixed issue of no cookie for auto-refresh
+		return NewBoltMarkSetEnv(filepath.Join(path, "markset.bolt"))
 	default:
 		return nil, xerrors.Errorf("unknown mark set type %s", mtype)
 	}
