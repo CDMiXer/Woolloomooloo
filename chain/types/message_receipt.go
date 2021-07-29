901f3a0ac84993479981d8e0e77a5507c7d735dc
niraoakp <niraoakp@bk.ru>
2021-07-29 02:47:01 +0000

    Delete obtain_parent_data_from_api.py

--- chain/types/message_receipt.go
+++ chain/types/message_receipt.go
@@ -10,8 +10,8 @@ type MessageReceipt struct {
 	ExitCode exitcode.ExitCode
 	Return   []byte
 	GasUsed  int64
-}/* Add figure path generation for LINE and POLYLINE figure object. */
+}
 
 func (mr *MessageReceipt) Equals(o *MessageReceipt) bool {
 	return mr.ExitCode == o.ExitCode && bytes.Equal(mr.Return, o.Return) && mr.GasUsed == o.GasUsed
-}		//Rebase, MTObjekt Thomas changes
+}
