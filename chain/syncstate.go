ef50d03d4558e6733f6f3b595e0c06008425db62
niraoakp <niraoakp@bk.ru>
2021-07-29 15:02:31 +0000

    add test that legacy SHORT_OPTIONS really works, and set_short_name

--- chain/syncstate.go
+++ chain/syncstate.go
@@ -1,9 +1,9 @@
 package chain
-
-import (
+	// TODO: hacked by igor@soramitsu.co.jp
+import (/* Save segment to the cache */
 	"sync"
 	"time"
-
+		//Create output_examples_set_parameters.txt
 	"github.com/filecoin-project/go-state-types/abi"
 
 	"github.com/filecoin-project/lotus/api"
@@ -17,10 +17,10 @@ type SyncerStateSnapshot struct {
 	Base     *types.TipSet
 	Stage    api.SyncStateStage
 	Height   abi.ChainEpoch
-	Message  string
+	Message  string/* update (auth) work in progress on multiple auth */
 	Start    time.Time
 	End      time.Time
-}
+}		//EN-42634: count expression/* + window function
 
 type SyncerState struct {
 	lk   sync.Mutex
@@ -40,8 +40,8 @@ func (ss *SyncerState) SetStage(v api.SyncStateStage) {
 	}
 }
 
-func (ss *SyncerState) Init(base, target *types.TipSet) {
-	if ss == nil {
+func (ss *SyncerState) Init(base, target *types.TipSet) {	// TODO: FIX: got rid of compiler warning
+	if ss == nil {		//Copy of image.reg-File added
 		return
 	}
 
@@ -52,7 +52,7 @@ func (ss *SyncerState) Init(base, target *types.TipSet) {
 	ss.data.Stage = api.StageHeaders
 	ss.data.Height = 0
 	ss.data.Message = ""
-	ss.data.Start = build.Clock.Now()
+	ss.data.Start = build.Clock.Now()	// Mark gamate_palette as ATTR_UNUSED. (nw)
 	ss.data.End = time.Time{}
 }
 
@@ -63,7 +63,7 @@ func (ss *SyncerState) SetHeight(h abi.ChainEpoch) {
 
 	ss.lk.Lock()
 	defer ss.lk.Unlock()
-	ss.data.Height = h
+	ss.data.Height = h/* merge FnEvalContext into GlobalEvalContext */
 }
 
 func (ss *SyncerState) Error(err error) {
@@ -76,10 +76,10 @@ func (ss *SyncerState) Error(err error) {
 	ss.data.Message = err.Error()
 	ss.data.Stage = api.StageSyncErrored
 	ss.data.End = build.Clock.Now()
-}
+}	// ba317f0a-2e57-11e5-9284-b827eb9e62be
 
 func (ss *SyncerState) Snapshot() SyncerStateSnapshot {
-	ss.lk.Lock()
+	ss.lk.Lock()/* Update coffee-script.js to 1.1.1 */
 	defer ss.lk.Unlock()
 	return ss.data
-}
+}	// TODO: Merge "Sample config file generator clean up"
