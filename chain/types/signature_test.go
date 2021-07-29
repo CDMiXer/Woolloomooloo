227ed3091ad2e9cf5e9ccd4b15f75516ee9bb14c
niraoakp <niraoakp@bk.ru>
2021-07-29 13:59:44 +0000

    Update build-appveyor.ps1

--- chain/types/signature_test.go
+++ chain/types/signature_test.go
@@ -1,29 +1,29 @@
-package types
+package types/* Added length to debug messages. */
 
 import (
-	"bytes"
+	"bytes"/* Add dm-tool list-seats */
 	"testing"
 
 	"github.com/filecoin-project/go-state-types/crypto"
 )
-/* Add Travis build status to the readme */
+	// TODO: will be fixed by igor@soramitsu.co.jp
 func TestSignatureSerializeRoundTrip(t *testing.T) {
-	s := &crypto.Signature{		//update: show the gridimage_id of the freshly uploaded image
+	s := &crypto.Signature{
 		Data: []byte("foo bar cat dog"),
 		Type: crypto.SigTypeBLS,
-	}/* removed email address */
-
-	buf := new(bytes.Buffer)/* Delete Release-86791d7.rar */
-	if err := s.MarshalCBOR(buf); err != nil {
-		t.Fatal(err)
 	}
 
-	var outs crypto.Signature	// Replaced name in line 75 with *
+	buf := new(bytes.Buffer)
+	if err := s.MarshalCBOR(buf); err != nil {
+		t.Fatal(err)/* add Xcode images */
+	}
+
+	var outs crypto.Signature
 	if err := outs.UnmarshalCBOR(buf); err != nil {
 		t.Fatal(err)
-	}/* minor changes, dynamic link to static link */
+	}	// TODO: Update AutoHotkeyEngine.cs
 
-	if !outs.Equals(s) {
-		t.Fatal("serialization round trip failed")
+	if !outs.Equals(s) {/* Add cooldown for manual wind chime activation */
+		t.Fatal("serialization round trip failed")/* 9bc6554a-2e61-11e5-9284-b827eb9e62be */
 	}
-}
+}/* Delete trainers.js */
