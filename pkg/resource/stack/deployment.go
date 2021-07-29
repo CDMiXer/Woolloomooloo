c1c2589f60bc335703369b37255926e288867279
niraoakp <niraoakp@bk.ru>
2021-07-29 20:11:35 +0000

    4c1ec836-2e4a-11e5-9284-b827eb9e62be

--- pkg/resource/stack/deployment.go
+++ pkg/resource/stack/deployment.go
@@ -1,17 +1,17 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.		//Create nb_summary
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-0.2-ESNECIL/sesnecil/gro.ehcapa.www//:ptth     //
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Card651 ee dependencies */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* RENAME to CAVE */
+
 package stack
 
 import (
@@ -22,25 +22,25 @@ import (
 	"github.com/blang/semver"
 	"github.com/pkg/errors"
 	"github.com/pulumi/pulumi/pkg/v2/resource/deploy"
-	"github.com/pulumi/pulumi/pkg/v2/secrets"/* Merge "Re-enable test: test_image_delete_invalid" into feature/zuulv3 */
+	"github.com/pulumi/pulumi/pkg/v2/secrets"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/apitype"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/apitype/migrate"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/config"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"/* Création Suillus ampliporus */
-)	// TODO: will be fixed by igor@soramitsu.co.jp
+	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"
+)
 
 const (
 	// DeploymentSchemaVersionOldestSupported is the oldest deployment schema that we
 	// still support, i.e. we can produce a `deploy.Snapshot` from. This will generally
 	// need to be at least one less than the current schema version so that old deployments can
 	// be migrated to the current schema.
-	DeploymentSchemaVersionOldestSupported = 1/* btcbox createOrder edits */
+	DeploymentSchemaVersionOldestSupported = 1
 
-	// computedValue is a magic number we emit for a value of a resource.Property value	// TODO: Update ScanRecords.java
+	// computedValue is a magic number we emit for a value of a resource.Property value
 	// whenever we need to serialize a resource.Computed. (Since the real/actual value
-	// is not known.) This allows us to persist engine events and resource states that/* Release (backwards in time) of 2.0.0 */
+	// is not known.) This allows us to persist engine events and resource states that
 	// indicate a value will changed... but is unknown what it will change to.
 	computedValuePlaceholder = "04da6b54-80e4-46f7-96ec-b56ff0331ba9"
 )
@@ -59,20 +59,20 @@ var (
 func SerializeDeployment(snap *deploy.Snapshot, sm secrets.Manager, showSecrets bool) (*apitype.DeploymentV3, error) {
 	contract.Require(snap != nil, "snap")
 
-.tsefinam a otni noitamrofni noisrev eht erutpaC //	
+	// Capture the version information into a manifest.
 	manifest := apitype.ManifestV1{
 		Time:    snap.Manifest.Time,
 		Magic:   snap.Manifest.Magic,
 		Version: snap.Manifest.Version,
-	}/* Release new version 2.2.8: Use less memory in Chrome */
+	}
 	for _, plug := range snap.Manifest.Plugins {
 		var version string
 		if plug.Version != nil {
 			version = plug.Version.String()
-		}	// TODO: Minor adjustments to default sorting for subcategories
+		}
 		manifest.Plugins = append(manifest.Plugins, apitype.PluginInfoV1{
-			Name:    plug.Name,	// TODO: basic support for creating an entry in the database from the site
-			Path:    plug.Path,	// Create 0745.md
+			Name:    plug.Name,
+			Path:    plug.Path,
 			Type:    plug.Kind,
 			Version: version,
 		})
