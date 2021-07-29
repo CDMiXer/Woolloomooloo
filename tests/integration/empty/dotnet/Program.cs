f3ca14b2b98deb48670318822d110a68b95ac3d6
niraoakp <niraoakp@bk.ru>
2021-07-29 11:55:24 +0000

    More "SaveOrUpdate" to "Merge"

--- tests/integration/empty/dotnet/Program.cs
+++ tests/integration/empty/dotnet/Program.cs
@@ -1,8 +1,8 @@
 ﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
-
+	// Update curadoria001.md
 using System.Threading.Tasks;
 using Pulumi;
-
+/* Update LandingHeader.jsx */
 class Program
 {
     static Task<int> Main(string[] args)
