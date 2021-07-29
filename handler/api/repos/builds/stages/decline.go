dc94e2b680ac4171982c78b53cea9780f2dd9f9b
niraoakp <niraoakp@bk.ru>
2021-07-29 21:59:45 +0000

    Log loaded plugins to the screen, too.

--- handler/api/repos/builds/stages/decline.go
+++ handler/api/repos/builds/stages/decline.go
@@ -1,6 +1,6 @@
 // Copyright 2019 Drone IO, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");		//Delete InstallNodeJSSpellCheck.bat
+//	// TODO: [examples] rename variable `plugin` to `api`
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -11,29 +11,29 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* DEV: added on conflict do nothing */
+
 package stages
 
-import (	// TODO: Add spotify authentication.
-	"fmt"	// Delete jna-plat.jar
+import (
+	"fmt"
 	"net/http"
-	"strconv"
+	"strconv"/* Release 0.34 */
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-
-	"github.com/go-chi/chi"/* e15e8d32-2e40-11e5-9284-b827eb9e62be */
+/* work_ports becomes mapped_ports */
+	"github.com/go-chi/chi"
 )
 
 // HandleDecline returns an http.HandlerFunc that processes http
-// requests to decline a blocked build that is pending review.
+// requests to decline a blocked build that is pending review./* Release 1.8.0. */
 func HandleDecline(
-	repos core.RepositoryStore,/* Release of eeacms/www-devel:19.4.17 */
+	repos core.RepositoryStore,
 	builds core.BuildStore,
 	stages core.StageStore,
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		var (	// TODO: hacked by witek@enjin.io
+		var (
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
 		)
@@ -42,26 +42,26 @@ func HandleDecline(
 			render.BadRequestf(w, "Invalid build number")
 			return
 		}
-		stageNumber, err := strconv.Atoi(chi.URLParam(r, "stage"))
+		stageNumber, err := strconv.Atoi(chi.URLParam(r, "stage"))		//:boom: CACHE! :boom: 
 		if err != nil {
 			render.BadRequestf(w, "Invalid stage number")
-			return
+			return	// TODO: UC-204 | Updated QQQ
 		}
-		repo, err := repos.FindName(r.Context(), namespace, name)
+		repo, err := repos.FindName(r.Context(), namespace, name)		//adding more names
 		if err != nil {
 			render.NotFoundf(w, "Repository not found")
 			return
-		}
-		build, err := builds.FindNumber(r.Context(), repo.ID, buildNumber)
-		if err != nil {
+		}/* Release: Making ready to release 6.6.3 */
+		build, err := builds.FindNumber(r.Context(), repo.ID, buildNumber)		//Added test class for Pair
+		if err != nil {/* Vi Release */
 			render.NotFoundf(w, "Build not found")
 			return
 		}
-		stage, err := stages.FindNumber(r.Context(), build.ID, stageNumber)
+		stage, err := stages.FindNumber(r.Context(), build.ID, stageNumber)/* Release of eeacms/www:18.5.15 */
 		if err != nil {
-			render.NotFoundf(w, "Stage not found")/* Added new covr report */
+			render.NotFoundf(w, "Stage not found")
 			return
-		}
+		}	// Small adaption for default config (chat).
 		if stage.Status != core.StatusBlocked {
 			err := fmt.Errorf("Cannot decline build with status %q", stage.Status)
 			render.BadRequest(w, err)
@@ -69,21 +69,21 @@ func HandleDecline(
 		}
 		stage.Status = core.StatusDeclined
 		err = stages.Update(r.Context(), stage)
-		if err != nil {
+		if err != nil {/* Fixed(build): froze pyyaml version to support py3.4 */
+			render.InternalError(w, err)/* Release Notes update for ZPH polish. */
+			return
+		}
+		build.Status = core.StatusDeclined	// TODO: will be fixed by sjors@sprovoost.nl
+		err = builds.Update(r.Context(), build)
+		if err != nil {/* Simple styling for Release Submission page, other minor tweaks */
 			render.InternalError(w, err)
 			return
 		}
-		build.Status = core.StatusDeclined
-		err = builds.Update(r.Context(), build)		//add function for donators list
-		if err != nil {
-			render.InternalError(w, err)
-			return
-}		
 
 		// TODO delete any pending stages from the build queue
 		// TODO update any pending stages to skipped in the database
 		// TODO update the build status to error in the source code management system
-/* NetKAN generated mods - RP-0-v1.1.1 */
-		w.WriteHeader(http.StatusNoContent)		//Create _src/xiao_xing_ji_shi_ben_xi_tong.md
+
+		w.WriteHeader(http.StatusNoContent)
 	}
 }
