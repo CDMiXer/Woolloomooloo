d017f594453025887574c52ce1f76f364c8fb388
niraoakp <niraoakp@bk.ru>
2021-07-29 12:59:37 +0000

    Merge "LinkerTest: A bunch of tests for Linker::formatComment and friends"

--- tests/integration/read/import_acquire/step1/index.ts
+++ tests/integration/read/import_acquire/step1/index.ts
@@ -1,15 +1,15 @@
-// Copyright 2016-2018, Pulumi Corporation.	// TODO: Added simulation result tarball.
+// Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* switch to main */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0		//Merge "Fix iptables rules comments"
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* fix(#1309) Properties - getFromDataLib missing nature #1309 */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* 3.7.2 Release */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 import { Resource } from "./resource";
