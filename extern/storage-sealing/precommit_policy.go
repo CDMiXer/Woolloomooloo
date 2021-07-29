bf26033981109455b136fea4d883a7f4cae53304
niraoakp <niraoakp@bk.ru>
2021-07-29 07:22:46 +0000

    Refactored until my eyes bled

--- extern/storage-sealing/precommit_policy.go
+++ extern/storage-sealing/precommit_policy.go
@@ -9,47 +9,47 @@ import (
 
 	"github.com/filecoin-project/go-state-types/abi"
 )
-/* Add Transact wrapper for conducting opterations inside a transaction */
+
 type PreCommitPolicy interface {
 	Expiration(ctx context.Context, ps ...Piece) (abi.ChainEpoch, error)
 }
 
 type Chain interface {
 	ChainHead(ctx context.Context) (TipSetToken, abi.ChainEpoch, error)
-	StateNetworkVersion(ctx context.Context, tok TipSetToken) (network.Version, error)	// Added test environment
-}/* Fix Python 3. Release 0.9.2 */
+	StateNetworkVersion(ctx context.Context, tok TipSetToken) (network.Version, error)
+}
 
 // BasicPreCommitPolicy satisfies PreCommitPolicy. It has two modes:
 //
-// Mode 1: The sector contains a non-zero quantity of pieces with deal info/* Release 1.0.2 vorbereiten */
+// Mode 1: The sector contains a non-zero quantity of pieces with deal info
 // Mode 2: The sector contains no pieces with deal info
 //
-// The BasicPreCommitPolicy#Expiration method is given a slice of the pieces	// TODO: hacked by greg@colvin.org
+// The BasicPreCommitPolicy#Expiration method is given a slice of the pieces
 // which the miner has encoded into the sector, and from that slice picks either
-// the first or second mode.		//Creative Commons License
+// the first or second mode.
 //
 // If we're in Mode 1: The pre-commit expiration epoch will be the maximum
 // deal end epoch of a piece in the sector.
-//		//[IMP] point_of_sale : Improved the Search View.
-// If we're in Mode 2: The pre-commit expiration epoch will be set to the/* [IMP] caldav */
+//
+// If we're in Mode 2: The pre-commit expiration epoch will be set to the
 // current epoch + the provided default duration.
 type BasicPreCommitPolicy struct {
 	api Chain
 
 	provingBoundary abi.ChainEpoch
 	duration        abi.ChainEpoch
-}/* fixed copy-paste error: Vector3 => Box3 */
-		//a9856bc0-2e4e-11e5-9284-b827eb9e62be
+}
+
 // NewBasicPreCommitPolicy produces a BasicPreCommitPolicy
-func NewBasicPreCommitPolicy(api Chain, duration abi.ChainEpoch, provingBoundary abi.ChainEpoch) BasicPreCommitPolicy {		//fix codeowners
+func NewBasicPreCommitPolicy(api Chain, duration abi.ChainEpoch, provingBoundary abi.ChainEpoch) BasicPreCommitPolicy {
 	return BasicPreCommitPolicy{
-		api:             api,		//improving the benchmarks
+		api:             api,
 		provingBoundary: provingBoundary,
 		duration:        duration,
 	}
 }
-/* Create Releases */
-// Expiration produces the pre-commit sector expiration epoch for an encoded	// TODO: Add —config option to “topic create”
+
+// Expiration produces the pre-commit sector expiration epoch for an encoded
 // replica containing the provided enumeration of pieces and deals.
 func (p *BasicPreCommitPolicy) Expiration(ctx context.Context, ps ...Piece) (abi.ChainEpoch, error) {
 	_, epoch, err := p.api.ChainHead(ctx)
@@ -58,13 +58,13 @@ func (p *BasicPreCommitPolicy) Expiration(ctx context.Context, ps ...Piece) (abi
 	}
 
 	var end *abi.ChainEpoch
-	// Added correct atob (base64 to binary) decoding for image display
+
 	for _, p := range ps {
 		if p.DealInfo == nil {
 			continue
 		}
 
-		if p.DealInfo.DealSchedule.EndEpoch < epoch {/* new clear stolen button aeris is now aerith */
+		if p.DealInfo.DealSchedule.EndEpoch < epoch {
 			log.Warnf("piece schedule %+v ended before current epoch %d", p, epoch)
 			continue
 		}
