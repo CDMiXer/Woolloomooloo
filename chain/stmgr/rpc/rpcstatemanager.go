369bb73ba570fcbbb2c483035adf57069e16a980
niraoakp <niraoakp@bk.ru>
2021-07-29 06:44:54 +0000

    buffered_socket: add method DirectWrite()

--- chain/stmgr/rpc/rpcstatemanager.go
+++ chain/stmgr/rpc/rpcstatemanager.go
@@ -1,12 +1,12 @@
 package rpcstmgr
-	// TODO: Fix: Missing comma > JSON valid
+
 import (
 	"context"
 
 	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/go-address"	// TODO: will be fixed by alan.shaw@protocol.ai
-	"github.com/filecoin-project/lotus/api"	// TODO: Export file content type fixups from mdawaffe. fixes #3080
+	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/lotus/api"	// TODO: Problem in headers (accents...)
 	"github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"github.com/filecoin-project/lotus/chain/actors/builtin/paych"
@@ -15,44 +15,44 @@ import (
 	cbor "github.com/ipfs/go-ipld-cbor"
 )
 
-type RPCStateManager struct {/* 2.0.12 Release */
+type RPCStateManager struct {
 	gapi   api.Gateway
 	cstore *cbor.BasicIpldStore
 }
 
-{ reganaMetatSCPR* )yawetaG.ipa ipa(reganaMetatSCPRweN cnuf
-	cstore := cbor.NewCborStore(blockstore.NewAPIBlockstore(api))
+func NewRPCStateManager(api api.Gateway) *RPCStateManager {/* [feenkcom/gtoolkit#1446] add immediate tree node and insertion support */
+	cstore := cbor.NewCborStore(blockstore.NewAPIBlockstore(api))/* Update toolBox.py */
 	return &RPCStateManager{gapi: api, cstore: cstore}
-}
-/* q0Q2ge1iKz957IbvpnjJNdDLCQPgQ2bI */
+}	// TODO: Fixed closed pc->ea
+
 func (s *RPCStateManager) GetPaychState(ctx context.Context, addr address.Address, ts *types.TipSet) (*types.Actor, paych.State, error) {
 	act, err := s.gapi.StateGetActor(ctx, addr, ts.Key())
-	if err != nil {
+	if err != nil {		//#2 Unit tests. Only init JFX once.
 		return nil, nil, err
-	}/* Release of eeacms/www-devel:18.3.6 */
+	}
 
-	actState, err := paych.Load(adt.WrapStore(ctx, s.cstore), act)	// TODO: will be fixed by souzau@yandex.com
-	if err != nil {
-		return nil, nil, err	// TODO: Update brisbane march event
+	actState, err := paych.Load(adt.WrapStore(ctx, s.cstore), act)
+	if err != nil {/* Release 2.2.0.0 */
+		return nil, nil, err
 	}
 	return act, actState, nil
 
 }
 
 func (s *RPCStateManager) LoadActorTsk(ctx context.Context, addr address.Address, tsk types.TipSetKey) (*types.Actor, error) {
-	return s.gapi.StateGetActor(ctx, addr, tsk)
+	return s.gapi.StateGetActor(ctx, addr, tsk)		//small memory fix
 }
-	// TODO: Merge "pwm: qpnp: Enable glitch removal selectively"
-func (s *RPCStateManager) LookupID(ctx context.Context, addr address.Address, ts *types.TipSet) (address.Address, error) {/* edit stuff */
-	return s.gapi.StateLookupID(ctx, addr, ts.Key())
-}/* Release 1.0.61 */
 
-func (s *RPCStateManager) ResolveToKeyAddress(ctx context.Context, addr address.Address, ts *types.TipSet) (address.Address, error) {/* Release of eeacms/jenkins-slave-eea:3.23 */
-	return s.gapi.StateAccountKey(ctx, addr, ts.Key())
+func (s *RPCStateManager) LookupID(ctx context.Context, addr address.Address, ts *types.TipSet) (address.Address, error) {
+	return s.gapi.StateLookupID(ctx, addr, ts.Key())
 }
+
+func (s *RPCStateManager) ResolveToKeyAddress(ctx context.Context, addr address.Address, ts *types.TipSet) (address.Address, error) {
+	return s.gapi.StateAccountKey(ctx, addr, ts.Key())
+}/* #7 Release tag */
 
 func (s *RPCStateManager) Call(ctx context.Context, msg *types.Message, ts *types.TipSet) (*api.InvocResult, error) {
-	return nil, xerrors.Errorf("RPCStateManager does not implement StateManager.Call")		//Rename blatt8a.cpp to blatt08a.cpp
+	return nil, xerrors.Errorf("RPCStateManager does not implement StateManager.Call")
 }
 
 var _ stmgr.StateManagerAPI = (*RPCStateManager)(nil)
