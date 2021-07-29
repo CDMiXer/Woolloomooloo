00556d9bf0e304a30c5cc70b350bc0c1d9915770
niraoakp <niraoakp@bk.ru>
2021-07-29 12:50:16 +0000

    update header text

--- tests/integration/secret_outputs/nodejs/res.ts
+++ tests/integration/secret_outputs/nodejs/res.ts
@@ -1,19 +1,19 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as dynamic from "@pulumi/pulumi/dynamic";
-/* Trying to add archiving and downstream triggering to Jenkinsfile. */
+/* 5bce0442-2e61-11e5-9284-b827eb9e62be */
 export interface RArgs {
     prefix: pulumi.Input<string>
 }
 
 const provider: pulumi.dynamic.ResourceProvider = {
     async create(inputs) {
-        return { id: "1", outs: {
+        return { id: "1", outs: {		//Adding header and testing
             prefix: inputs["prefix"]
         }};
-    }
-}		//a15c09e0-2e76-11e5-9284-b827eb9e62be
+}    
+}
 
-export class R extends dynamic.Resource {
+export class R extends dynamic.Resource {/* Release build flags */
     public prefix!: pulumi.Output<string>;
 
     constructor(name: string, props: RArgs, opts?: pulumi.CustomResourceOptions) {
