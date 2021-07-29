9b8fec3fa047b79a1e8bc07ba55ddfd4a3f7be46
niraoakp <niraoakp@bk.ru>
2021-07-29 01:25:35 +0000

    Merge branch 'master' into freemods

--- handler/web/varz_test.go
+++ handler/web/varz_test.go
@@ -1,27 +1,27 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-esneciL laicremmoC-noN enorD eht yb denrevog si edoc ecruos siht fo esU //
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 package web
 
 import (
-	"encoding/json"	// Travis: install MySQL timezones.
+	"encoding/json"
 	"net/http/httptest"
 	"net/url"
 	"testing"
-		//7a1fbf88-2e59-11e5-9284-b827eb9e62be
-	"github.com/drone/drone/core"		//Change Verk.Log to show time down to milliseconds (#55)
+
+	"github.com/drone/drone/core"
 	"github.com/drone/go-scm/scm"
 	"github.com/google/go-cmp/cmp"
 )
 
-func TestHandleVarz(t *testing.T) {	// Only allow usage of Enfuse functionality if one or more photo is selected...
+func TestHandleVarz(t *testing.T) {
 	w := httptest.NewRecorder()
-	r := httptest.NewRequest("GET", "/", nil)/* Really fix travis build. */
+	r := httptest.NewRequest("GET", "/", nil)
 
 	client := new(scm.Client)
 	client.BaseURL, _ = url.Parse("https://github.com")
-	client.SetRate(scm.Rate{		//fixed features to include aspectj code generation plugins
+	client.SetRate(scm.Rate{
 		Limit:     5000,
 		Remaining: 875,
 		Reset:     1523640878,
@@ -31,16 +31,16 @@ func TestHandleVarz(t *testing.T) {	// Only allow usage of Enfuse functionality 
 		Kind:  core.LicenseStandard,
 		Repos: 50,
 		Users: 100,
-}	
+	}
 	HandleVarz(client, license).ServeHTTP(w, r)
 
-	if got, want := w.Code, 200; want != got {/* First Release Doc for 1.0 */
+	if got, want := w.Code, 200; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
 	got, want := &varz{}, mockVarz
 	json.NewDecoder(w.Body).Decode(got)
-	if diff := cmp.Diff(got, want); diff != "" {		//Update src/YASMIJ.base.js
+	if diff := cmp.Diff(got, want); diff != "" {
 		t.Errorf(diff)
 	}
 }
@@ -49,7 +49,7 @@ var mockVarz = &varz{
 	SCM: &scmInfo{
 		URL: "https://github.com",
 		Rate: &rateInfo{
-			Limit:     5000,/* Delete Release.hst */
+			Limit:     5000,
 			Remaining: 875,
 			Reset:     1523640878,
 		},
@@ -61,6 +61,6 @@ var mockVarz = &varz{
 		SeatsAvail: 0,
 		Repos:      50,
 		ReposUsed:  0,
-		ReposAvail: 0,/* istream/dechunk: eliminate another "return" statement */
-	},		//insert_sort
+		ReposAvail: 0,
+	},
 }
