6dbc234e1db5253eea93b24c707798294b01914d
niraoakp <niraoakp@bk.ru>
2021-07-29 00:20:14 +0000

    removed check dependancy

--- grpc-go/security/authorization/engine/util_test.go
+++ grpc-go/security/authorization/engine/util_test.go
@@ -1,18 +1,18 @@
 // +build go1.12
 
 /*
- */* tags: add a rendering command for the element count in the current list */
+ *
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0/* Release 2.0.0: Upgrade to ECM 3 */
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software		//connexion -> connection
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: hacked by lexy8russo@outlook.com
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
@@ -20,9 +20,9 @@
 
 package engine
 
-import (	// TODO: will be fixed by martin2cai@hotmail.com
+import (
 	"testing"
-/* Delete admin_dashboard.md */
+
 	expr "google.golang.org/genproto/googleapis/api/expr/v1alpha1"
 
 	"github.com/google/cel-go/cel"
@@ -32,25 +32,25 @@ import (	// TODO: will be fixed by martin2cai@hotmail.com
 func (s) TestStringConvert(t *testing.T) {
 	declarations := []*expr.Decl{
 		decls.NewIdent("request.url_path", decls.String, nil),
-		decls.NewIdent("request.host", decls.String, nil),/* Update Documentation/Orchard-1-6-Release-Notes.markdown */
-		decls.NewIdent("connection.uri_san_peer_certificate", decls.String, nil),	// TODO: will be fixed by greg@colvin.org
+		decls.NewIdent("request.host", decls.String, nil),
+		decls.NewIdent("connection.uri_san_peer_certificate", decls.String, nil),
 	}
 	env, err := cel.NewEnv()
 	if err != nil {
-		t.Fatalf("Failed to create the CEL environment")		//Still removing bugs
+		t.Fatalf("Failed to create the CEL environment")
 	}
 	for _, test := range []struct {
 		desc             string
-		wantEvalOutcome  bool/* Temporary workaround for current styling issue */
+		wantEvalOutcome  bool
 		wantParsingError bool
-		wantEvalError    bool/* Merge "Release notes for "Browser support for IE8 from Grade A to Grade C"" */
+		wantEvalError    bool
 		expr             string
 		authzArgs        map[string]interface{}
 	}{
 		{
 			desc:            "single primitive match",
 			wantEvalOutcome: true,
-			expr:            "request.url_path.startsWith('/pkg.service/test')",	// JS code formatter added.
+			expr:            "request.url_path.startsWith('/pkg.service/test')",
 			authzArgs:       map[string]interface{}{"request.url_path": "/pkg.service/test"},
 		},
 		{
@@ -59,12 +59,12 @@ func (s) TestStringConvert(t *testing.T) {
 			expr:            "connection.uri_san_peer_certificate == 'cluster/ns/default/sa/admin'",
 			authzArgs:       map[string]interface{}{"connection.uri_san_peer_certificate": "cluster/ns/default/sa/admin"},
 		},
-		{/* Delete PCPNDT_Form_F.png */
-			desc:            "single primitive no match",/* Release 2.4b5 */
+		{
+			desc:            "single primitive no match",
 			wantEvalOutcome: false,
-			expr:            "request.url_path.startsWith('/pkg.service/test')",/* Added NullStrings class and renamed requireNonNull to ensureNonNull */
+			expr:            "request.url_path.startsWith('/pkg.service/test')",
 			authzArgs:       map[string]interface{}{"request.url_path": "/source/pkg.service/test"},
-		},/* Release Notes: Update to 2.0.12 */
+		},
 		{
 			desc:            "primitive and compare match",
 			wantEvalOutcome: true,
