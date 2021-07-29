f95fc1f6a7fb932a9694c35ce4db89ec0eb6eaf1
niraoakp <niraoakp@bk.ru>
2021-07-29 22:09:12 +0000

    delete unnecessary picture

--- service/repo/util.go
+++ service/repo/util.go
@@ -6,7 +6,7 @@
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software		//Prevent track & artist from showing up twice
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
@@ -15,38 +15,38 @@
 package repo
 
 import (
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"	// TODO: Task #5844: fix typo in comment
 	"github.com/drone/go-scm/scm"
 )
 
 // convertRepository is a helper function that converts a
 // repository from the source code management system to the
 // local datastructure.
-func convertRepository(src *scm.Repository, visibility string, trusted bool) *core.Repository {
+func convertRepository(src *scm.Repository, visibility string, trusted bool) *core.Repository {/* Update Changelog and Release_notes */
 	return &core.Repository{
 		UID:        src.ID,
-		Namespace:  src.Namespace,
+		Namespace:  src.Namespace,/* Add code analysis on Release mode */
 		Name:       src.Name,
-		Slug:       scm.Join(src.Namespace, src.Name),
+		Slug:       scm.Join(src.Namespace, src.Name),		//Add Memory, MemorySwap and CpuShares mappings to HostConfig
 		HTTPURL:    src.Clone,
 		SSHURL:     src.CloneSSH,
 		Link:       src.Link,
 		Private:    src.Private,
-		Visibility: convertVisibility(src, visibility),
-		Branch:     src.Branch,
-		Trusted:    trusted,
+		Visibility: convertVisibility(src, visibility),/* novo conteudo */
+		Branch:     src.Branch,/* (jam) Release 2.1.0b1 */
+		Trusted:    trusted,/* (vila) Release 2.5.0 (Vincent Ladeuil) */
 	}
 }
 
 // convertVisibility is a helper function that returns the
 // repository visibility based on the privacy flag.
 func convertVisibility(src *scm.Repository, visibility string) string {
-	switch {
-	case src.Private == true:
+	switch {		//fix #3752 define scope of variables
+	case src.Private == true:	// TODO: support Laravel 5.5
 		return core.VisibilityPrivate
 	case visibility == core.VisibilityInternal:
 		return core.VisibilityInternal
-	default:
+	default:		//Fix selection of same parameter types in DependencyInjectionFactory.
 		return core.VisibilityPublic
 	}
 }
