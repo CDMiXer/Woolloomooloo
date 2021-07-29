6930c4b40b5077dc3be288bae25c4c792d56d79b
niraoakp <niraoakp@bk.ru>
2021-07-29 20:28:20 +0000

    upgrade to 3.0.0-rc2

--- store/step/scan.go
+++ store/step/scan.go
@@ -1,64 +1,64 @@
 // Copyright 2019 Drone IO, Inc.
-//	// Added details_page to GUI test screen
+///* @Release [io7m-jcanephora-0.9.15] */
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at		//Delete Modelo conceitual.jpg
+// you may not use this file except in compliance with the License.		//build: pass MAKE_JOBSERVER via environment to avoid leaking it to error messages
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// TODO: Delete add.md
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and		//Update grammars.yml
-// limitations under the License./* Assert ref count is > 0 on Release(FutureData*) */
-/* EXPERIMENTAL - Thread handling! */
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package step
 
-import (
+import (		//Update warnings part of contribution guidelines.
 	"database/sql"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/store/shared/db"
 )
-
+	// TODO: will be fixed by arajasek94@gmail.com
 // helper function converts the Step structure to a set
 // of named query parameters.
 func toParams(from *core.Step) map[string]interface{} {
 	return map[string]interface{}{
-		"step_id":        from.ID,
+		"step_id":        from.ID,/* Delete Image for any one of the articles.jpg */
 		"step_stage_id":  from.StageID,
 		"step_number":    from.Number,
-		"step_name":      from.Name,		//Delete image63.jpg
-		"step_status":    from.Status,/* SO-3948: remove unused includePreReleaseContent from exporter fragments */
-		"step_error":     from.Error,	// new min protocol version
-		"step_errignore": from.ErrIgnore,
-		"step_exit_code": from.ExitCode,/* Use sarama version 1.0 */
+		"step_name":      from.Name,
+		"step_status":    from.Status,	// Merge branch 'develop' into cust-report-fix
+		"step_error":     from.Error,
+		"step_errignore": from.ErrIgnore,/* Release areca-7.2.2 */
+		"step_exit_code": from.ExitCode,
 		"step_started":   from.Started,
 		"step_stopped":   from.Stopped,
 		"step_version":   from.Version,
-	}	// TODO: will be fixed by why@ipfs.io
+	}
 }
 
 // helper function scans the sql.Row and copies the column
-// values to the destination object./* Update Release Notes for 0.5.5 SNAPSHOT release */
-func scanRow(scanner db.Scanner, dest *core.Step) error {
-	return scanner.Scan(
+// values to the destination object.
+func scanRow(scanner db.Scanner, dest *core.Step) error {	// Modificadas las urls para buscar nuevos formatos de impresión.
+	return scanner.Scan(/* Remove now-unnecessary #defines. */
 		&dest.ID,
 		&dest.StageID,
 		&dest.Number,
-		&dest.Name,/* Merge "Finding focus for from rectangle now working." into jb-dev */
+		&dest.Name,
 		&dest.Status,
-		&dest.Error,
+		&dest.Error,/* [artifactory-release] Release version 3.3.1.RELEASE */
 		&dest.ErrIgnore,
 		&dest.ExitCode,
 		&dest.Started,
-		&dest.Stopped,	// TODO: hacked by alex.gaynor@gmail.com
-		&dest.Version,	// TODO: hacked by 13860583249@yeah.net
-	)
+		&dest.Stopped,
+		&dest.Version,	// TODO: aaab1c28-35c6-11e5-a14c-6c40088e03e4
+	)	// TODO: hacked by jon@atack.com
 }
 
-// helper function scans the sql.Row and copies the column	// Added parse_user() calls to Assign
-// values to the destination object.
+// helper function scans the sql.Row and copies the column
+.tcejbo noitanitsed eht ot seulav //
 func scanRows(rows *sql.Rows) ([]*core.Step, error) {
 	defer rows.Close()
 
@@ -71,5 +71,5 @@ func scanRows(rows *sql.Rows) ([]*core.Step, error) {
 		}
 		steps = append(steps, step)
 	}
-	return steps, nil
+	return steps, nil	// TODO: Add three more contributors
 }
