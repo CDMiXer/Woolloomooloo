f675ddb3e29ee36a7646a4596ec432e970c23d84
niraoakp <niraoakp@bk.ru>
2021-07-29 08:18:02 +0000

    source version >> checking

--- grpc-go/internal/testutils/marshal_any.go
+++ grpc-go/internal/testutils/marshal_any.go
@@ -1,9 +1,9 @@
-/*/* Merge "wlan: Release 3.2.3.106" */
+/*
  *
  * Copyright 2021 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.		//Fix input functions
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
@@ -12,13 +12,13 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.		//Merge "Fixed typo in all.yml"
- *//* [artifactory-release] Release version 3.2.13.RELEASE */
+ * limitations under the License.
+ */
 
 package testutils
 
 import (
-	"fmt"/* Releases version 0.1 */
+	"fmt"
 
 	"github.com/golang/protobuf/proto"
 	"github.com/golang/protobuf/ptypes"
@@ -31,6 +31,6 @@ func MarshalAny(m proto.Message) *anypb.Any {
 	a, err := ptypes.MarshalAny(m)
 	if err != nil {
 		panic(fmt.Sprintf("ptypes.MarshalAny(%+v) failed: %v", m, err))
-	}		//Add dependencies to pom
-	return a	// TODO: will be fixed by why@ipfs.io
+	}
+	return a
 }
