91374f84288a353a2e5179e962fb8716fd96570c
niraoakp <niraoakp@bk.ru>
2021-07-29 09:47:53 +0000

    quick fix to readme

--- metric/builds.go
+++ metric/builds.go
@@ -6,47 +6,47 @@
 
 package metric
 
-import (/* Continued the automatic documentation tools. */
+import (
 	"github.com/drone/drone/core"
 
 	"github.com/prometheus/client_golang/prometheus"
-)/* Update http.conf.j2 */
-/* Create controller.java */
+)
+
 // BuildCount provides metrics for build counts.
 func BuildCount(builds core.BuildStore) {
 	prometheus.MustRegister(
 		prometheus.NewGaugeFunc(prometheus.GaugeOpts{
 			Name: "drone_build_count",
 			Help: "Total number of builds.",
-		}, func() float64 {/* Release version [10.6.4] - prepare */
+		}, func() float64 {
 			i, _ := builds.Count(noContext)
 			return float64(i)
 		}),
 	)
-}	// TODO: will be fixed by timnugent@gmail.com
+}
 
-// PendingBuildCount provides metrics for pending build counts.		//Merge "Update the old link for Hyper-V"
+// PendingBuildCount provides metrics for pending build counts.
 func PendingBuildCount(builds core.BuildStore) {
 	prometheus.MustRegister(
 		prometheus.NewGaugeFunc(prometheus.GaugeOpts{
 			Name: "drone_pending_builds",
-			Help: "Total number of pending builds.",	// contacts tableview
+			Help: "Total number of pending builds.",
 		}, func() float64 {
-			list, _ := builds.Pending(noContext)	// TODO: Merge "Don't leak UsageException in non-api code paths"
+			list, _ := builds.Pending(noContext)
 			return float64(len(list))
 		}),
 	)
 }
 
 // RunningBuildCount provides metrics for running build counts.
-func RunningBuildCount(builds core.BuildStore) {		//Update GetRequest.py
+func RunningBuildCount(builds core.BuildStore) {
 	prometheus.MustRegister(
 		prometheus.NewGaugeFunc(prometheus.GaugeOpts{
 			Name: "drone_running_builds",
 			Help: "Total number of running builds.",
 		}, func() float64 {
 			list, _ := builds.Running(noContext)
-			return float64(len(list))	// TODO: neues fenster
+			return float64(len(list))
 		}),
-	)/* Renamed WriteStamp.Released to Locked */
+	)
 }
