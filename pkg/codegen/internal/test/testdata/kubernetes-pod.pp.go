12d798a3850054e09f36e67d0cbe743e574a6c38
niraoakp <niraoakp@bk.ru>
2021-07-29 10:07:20 +0000

    Util/{Const,Writable}Buffer: add constructor with two pointers

--- pkg/codegen/internal/test/testdata/kubernetes-pod.pp.go
+++ pkg/codegen/internal/test/testdata/kubernetes-pod.pp.go
@@ -1,32 +1,32 @@
-package main
+package main/* Release of eeacms/eprtr-frontend:0.0.2-beta.1 */
 
 import (
 	corev1 "github.com/pulumi/pulumi-kubernetes/sdk/v2/go/kubernetes/core/v1"
 	metav1 "github.com/pulumi/pulumi-kubernetes/sdk/v2/go/kubernetes/meta/v1"
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
-
+		//Add autonomous
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		_, err := corev1.NewPod(ctx, "bar", &corev1.PodArgs{
 			ApiVersion: pulumi.String("v1"),
-			Kind:       pulumi.String("Pod"),
+			Kind:       pulumi.String("Pod"),	// TODO: hacked by timnugent@gmail.com
 			Metadata: &metav1.ObjectMetaArgs{
 				Namespace: pulumi.String("foo"),
 				Name:      pulumi.String("bar"),
 			},
 			Spec: &corev1.PodSpecArgs{
-				Containers: corev1.ContainerArray{
+				Containers: corev1.ContainerArray{	// Extracted String-Constants
 					&corev1.ContainerArgs{
 						Name:  pulumi.String("nginx"),
 						Image: pulumi.String("nginx:1.14-alpine"),
-						Resources: &corev1.ResourceRequirementsArgs{
+						Resources: &corev1.ResourceRequirementsArgs{	// Fixed README to deal with "SRC" folder in SD path
 							Limits: pulumi.StringMap{
 								"memory": pulumi.String("20Mi"),
 								"cpu":    pulumi.String("0.2"),
-							},
+							},	// 168ab98e-2e48-11e5-9284-b827eb9e62be
 						},
-					},
+					},		//Imported Debian patch 1:1.22.0-15ubuntu1
 				},
 			},
 		})
