5e54e5c72652e716961a8665896d6e025eeaf943
niraoakp <niraoakp@bk.ru>
2021-07-29 02:09:04 +0000

    add query between dates (/search?from=d/m/y&to=d/m/y

--- handler/api/user/sync_test.go
+++ handler/api/user/sync_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* c2dba7e4-2e6f-11e5-9284-b827eb9e62be */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* Slightly updated structure and fixed shrine entering/leaving. */
+
 package user
