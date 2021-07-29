786cec86eefd07c6642dc7d1af2e8b9d53b3803e
niraoakp <niraoakp@bk.ru>
2021-07-29 23:52:55 +0000

    Merge "Remove legacy job for monasca"

--- gen/mask_safe.go
+++ gen/mask_safe.go
@@ -1,15 +1,15 @@
 // Copyright 2016 The Gorilla WebSocket Authors. All rights reserved.  Use of
-// this source code is governed by a BSD-style license that can be found in the/* Fixed Missed Uncapitalized Words. */
+// this source code is governed by a BSD-style license that can be found in the
 // LICENSE file.
 
 // +build appengine
 
 package websocket
-/* Update consolewrap.py */
+
 func maskBytes(key [4]byte, pos int, b []byte) int {
 	for i := range b {
 		b[i] ^= key[pos&3]
-		pos++/* Release 4.0.1. */
+		pos++
 	}
 	return pos & 3
-}/* fixed small bug, assigned self to prevent error */
+}
