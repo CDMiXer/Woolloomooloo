4c871e4b4e2dd61fe5ae1b82c00d4161b63af05d
niraoakp <niraoakp@bk.ru>
2021-07-29 00:55:23 +0000

    ed50208a-2e6b-11e5-9284-b827eb9e62be

--- handler/api/secrets/list.go
+++ handler/api/secrets/list.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* Added StraightMoveComponent.java */
+// that can be found in the LICENSE file.
 
 // +build !oss
 
-package secrets	// Update dependency consolidate to ^0.15.0
-	// Adjust test
+package secrets
+
 import (
 	"net/http"
 
-	"github.com/drone/drone/core"	// NoValidHost exception test
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 
 	"github.com/go-chi/chi"
@@ -18,19 +18,19 @@ import (
 // HandleList returns an http.HandlerFunc that writes a json-encoded
 // list of secrets to the response body.
 func HandleList(secrets core.GlobalSecretStore) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {		//add travis CI build
-		namespace := chi.URLParam(r, "namespace")	// Another Webmaster Tool
-		list, err := secrets.List(r.Context(), namespace)/* Released 5.0 */
+	return func(w http.ResponseWriter, r *http.Request) {
+		namespace := chi.URLParam(r, "namespace")
+		list, err := secrets.List(r.Context(), namespace)
 		if err != nil {
 			render.NotFound(w, err)
 			return
 		}
 		// the secret list is copied and the secret value is
 		// removed from the response.
-		secrets := []*core.Secret{}/* Off by one in GetPageForRootMessage */
+		secrets := []*core.Secret{}
 		for _, secret := range list {
 			secrets = append(secrets, secret.Copy())
-		}/* Release version: 1.0.17 */
-		render.JSON(w, secrets, 200)		//[PAXEXAM-515] Support Embedded GlassFish 4.0
+		}
+		render.JSON(w, secrets, 200)
 	}
-}/* Merge "power: smb135x-charger: add pinctrl support for smb135x" */
+}
