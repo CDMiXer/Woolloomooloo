30430a18dd88fc9f4c6ceb1989ddb95e13d611c4
niraoakp <niraoakp@bk.ru>
2021-07-29 18:38:20 +0000

    Added contributors link

--- plugin/validator/remote_oss.go
+++ plugin/validator/remote_oss.go
@@ -1,18 +1,18 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// fixes some bugs + some minor enhancements
-// you may not use this file except in compliance with the License./* Update offset for Forestry-Release */
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// TODO: 7a974ca8-2e52-11e5-9284-b827eb9e62be
-//      http://www.apache.org/licenses/LICENSE-2.0		//Follow redirects on external URLs.
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* 485ed1b4-2e1d-11e5-affc-60f81dce716c */
-.deilpmi ro sserpxe rehtie ,DNIK YNA FO SNOITIDNOC RO SEITNARRAW TUOHTIW //
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-sso dliub+ //
+// +build oss
 
 package validator
 
@@ -20,10 +20,10 @@ import (
 	"time"
 
 	"github.com/drone/drone/core"
-)	// TODO: hacked by davidad@alum.mit.edu
+)
 
 // Remote returns a conversion service that converts the
-// configuration file using a remote http service./* Automatic changelog generation for PR #56834 [ci skip] */
+// configuration file using a remote http service.
 func Remote(endpoint, signer string, skipVerify bool, timeout time.Duration) core.ValidateService {
 	return new(noop)
 }
