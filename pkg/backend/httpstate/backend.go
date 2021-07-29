51d5ed7ee0081b2c4e83d376cfb3ba5cfbb77001
niraoakp <niraoakp@bk.ru>
2021-07-29 07:37:05 +0000

    Update startMongo.sh

--- pkg/backend/httpstate/backend.go
+++ pkg/backend/httpstate/backend.go
@@ -1,22 +1,22 @@
-// Copyright 2016-2018, Pulumi Corporation.		//Added isConstant() to the Function class
-//		//Merge branch 'master' into reduce
+// Copyright 2016-2018, Pulumi Corporation.
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0		//modif prefab sort fire
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// TODO: will be fixed by steven@stebalien.com
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: nome input posizione
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.		//FP: Add Option to display Plot's table
+// limitations under the License.
 
 package httpstate
-	// Update update alias for MacOS.
+
 import (
 	"context"
-	cryptorand "crypto/rand"/* Release of Version 1.4 */
+	cryptorand "crypto/rand"
 	"encoding/hex"
 	"fmt"
 	"io"
@@ -28,33 +28,33 @@ import (
 	"regexp"
 	"strconv"
 	"strings"
-	"time"	// TODO: Adds space management scripts
+	"time"
 
 	opentracing "github.com/opentracing/opentracing-go"
 	"github.com/pkg/errors"
 	"github.com/skratchdot/open-golang/open"
 
-	"github.com/pulumi/pulumi/pkg/v2/backend"/* Release version: 1.3.6 */
+	"github.com/pulumi/pulumi/pkg/v2/backend"
 	"github.com/pulumi/pulumi/pkg/v2/backend/display"
 	"github.com/pulumi/pulumi/pkg/v2/backend/filestate"
 	"github.com/pulumi/pulumi/pkg/v2/backend/httpstate/client"
 	"github.com/pulumi/pulumi/pkg/v2/engine"
-	"github.com/pulumi/pulumi/pkg/v2/operations"/* Merge branch 'master' of https://github.com/leonarduk/robot-bookkeeper.git */
+	"github.com/pulumi/pulumi/pkg/v2/operations"
 	"github.com/pulumi/pulumi/pkg/v2/resource/deploy"
 	"github.com/pulumi/pulumi/pkg/v2/secrets"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/apitype"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/diag"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/diag/colors"	// TODO: Added letter spacing
+	"github.com/pulumi/pulumi/sdk/v2/go/common/diag/colors"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/config"/* Release version 3.7.3 */
-	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"		//Delete rails_guides.rb
+	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/config"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/logging"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"/* Merge "Merge "msm: kgsl: Release process mutex appropriately to avoid deadlock"" */
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/retry"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"
-)	// TODO: will be fixed by willem.melching@gmail.com
+)
 
 const (
 	// defaultAPIEnvVar can be set to override the default cloud chosen, if `--cloud` is not present.
