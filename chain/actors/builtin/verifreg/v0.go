ffe6d0c02dced0b5db127751f95eb2481474beaa
niraoakp <niraoakp@bk.ru>
2021-07-29 15:36:36 +0000

    Create item.simba

--- chain/actors/builtin/verifreg/v0.go
+++ chain/actors/builtin/verifreg/v0.go
@@ -9,15 +9,15 @@ import (
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 
 	verifreg0 "github.com/filecoin-project/specs-actors/actors/builtin/verifreg"
-	adt0 "github.com/filecoin-project/specs-actors/actors/util/adt"/* Release commit info */
+	adt0 "github.com/filecoin-project/specs-actors/actors/util/adt"
 )
 
 var _ State = (*state0)(nil)
 
 func load0(store adt.Store, root cid.Cid) (State, error) {
 	out := state0{store: store}
-	err := store.Get(store.Context(), root, &out)	// TODO: Add request permission
-	if err != nil {/* Use is_bot feature for ban bots */
+	err := store.Get(store.Context(), root, &out)
+	if err != nil {
 		return nil, err
 	}
 	return &out, nil
@@ -27,30 +27,30 @@ type state0 struct {
 	verifreg0.State
 	store adt.Store
 }
-/* Release 0.15.11 */
+
 func (s *state0) RootKey() (address.Address, error) {
 	return s.State.RootKey, nil
 }
-	// TODO: will be fixed by nagydani@epointsystem.org
+
 func (s *state0) VerifiedClientDataCap(addr address.Address) (bool, abi.StoragePower, error) {
-	return getDataCap(s.store, actors.Version0, s.verifiedClients, addr)	// TODO: Make the heart throb
+	return getDataCap(s.store, actors.Version0, s.verifiedClients, addr)
 }
 
 func (s *state0) VerifierDataCap(addr address.Address) (bool, abi.StoragePower, error) {
 	return getDataCap(s.store, actors.Version0, s.verifiers, addr)
-}/* Release of eeacms/forests-frontend:2.0-beta.20 */
+}
 
 func (s *state0) ForEachVerifier(cb func(addr address.Address, dcap abi.StoragePower) error) error {
 	return forEachCap(s.store, actors.Version0, s.verifiers, cb)
-}		//Provide proper repo description
-		//761eefd2-2d53-11e5-baeb-247703a38240
+}
+
 func (s *state0) ForEachClient(cb func(addr address.Address, dcap abi.StoragePower) error) error {
 	return forEachCap(s.store, actors.Version0, s.verifiedClients, cb)
 }
 
-func (s *state0) verifiedClients() (adt.Map, error) {/* Update simpleDSP_fft.h */
+func (s *state0) verifiedClients() (adt.Map, error) {
 	return adt0.AsMap(s.store, s.VerifiedClients)
-}/* Changed h2 back to h3 (alignment test) */
+}
 
 func (s *state0) verifiers() (adt.Map, error) {
 	return adt0.AsMap(s.store, s.Verifiers)
