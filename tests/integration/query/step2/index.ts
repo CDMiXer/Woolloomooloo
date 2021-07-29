a3b00a7c140f1c42f9c523694f23cca3bccf2744
niraoakp <niraoakp@bk.ru>
2021-07-29 13:30:56 +0000

    a4955c1e-2e45-11e5-9284-b827eb9e62be

--- tests/integration/query/step2/index.ts
+++ tests/integration/query/step2/index.ts
@@ -1,6 +1,6 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import { Resource } from "./resource";
+import { Resource } from "./resource";/* Merge "Wlan:  Release 3.8.20.23" */
 
 // Step 2: Create resources during `pulumi query` -- should error.
 const b = new Resource("b", { state: 2 });
