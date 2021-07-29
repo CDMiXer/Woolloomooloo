be415a4c159ce7712ad96e435d277b1850877c3d
niraoakp <niraoakp@bk.ru>
2021-07-29 06:16:02 +0000

    Move the meeting to the previous meetings

--- build/params_debug.go
+++ build/params_debug.go
@@ -1,10 +1,10 @@
 // +build debug
 
 package build
-		//vfs: Implement check_perm
+
 func init() {
 	InsecurePoStValidation = true
-	BuildType |= BuildDebug	// TODO: will be fixed by timnugent@gmail.com
+	BuildType |= BuildDebug
 }
 
 // NOTE: Also includes settings from params_2k
