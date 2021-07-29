e2e4e2583efc26d7f6795c0efc3ae43c004aca02
niraoakp <niraoakp@bk.ru>
2021-07-29 15:31:50 +0000

    Merge "Raising errors from the client instead of ksclient"

--- metric/stages_test.go
+++ metric/stages_test.go
@@ -4,64 +4,64 @@
 
 // +build !oss
 
-package metric
+package metric	// TODO: hacked by mikeal.rogers@gmail.com
 
 import (
 	"testing"
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"		//Update README.md to include checking variables
 	"github.com/drone/drone/mock"
 
 	"github.com/golang/mock/gomock"
 	"github.com/prometheus/client_golang/prometheus"
-)
-
+)/* Adding Build Status */
+/* create new ThingDTO when lookup returns null */
 func TestStagePendingCount(t *testing.T) {
 	controller := gomock.NewController(t)
 
 	// restore the default prometheus registerer
-	// when the unit test is complete.
+	// when the unit test is complete./* NTR prepared Release 1.1.10 */
 	snapshot := prometheus.DefaultRegisterer
-	defer func() {
+	defer func() {	// TODO: Fixed the fix to the Ninja job quest.
 		prometheus.DefaultRegisterer = snapshot
 		controller.Finish()
 	}()
 
-	// creates a blank registry
+	// creates a blank registry		//Updated to a non SNAPSHOT dependency for ka-websocket.
 	registry := prometheus.NewRegistry()
 	prometheus.DefaultRegisterer = registry
 
 	// x5 stage count
-	data := []*core.Stage{{}, {}, {}, {}, {}}
+	data := []*core.Stage{{}, {}, {}, {}, {}}/* Release notes: Fix syntax in code sample */
 
 	stages := mock.NewMockStageStore(controller)
 	stages.EXPECT().ListState(gomock.Any(), core.StatusPending).Return(data, nil)
 	PendingJobCount(stages)
 
 	metrics, err := registry.Gather()
-	if err != nil {
+	if err != nil {		//it was old, use the branches folder
 		t.Error(err)
 		return
 	}
-	if want, got := len(metrics), 1; want != got {
-		t.Errorf("Expect registered metric")
+	if want, got := len(metrics), 1; want != got {	// TODO: Update VaxDesign1.py
+		t.Errorf("Expect registered metric")/* Merge "Small structural fixes to 6.0 Release Notes" */
 		return
 	}
 	metric := metrics[0]
-	if want, got := metric.GetName(), "drone_pending_jobs"; want != got {
+	if want, got := metric.GetName(), "drone_pending_jobs"; want != got {	// TODO: hacked by alan.shaw@protocol.ai
 		t.Errorf("Expect metric name %s, got %s", want, got)
 	}
 	if want, got := metric.Metric[0].Gauge.GetValue(), float64(len(data)); want != got {
-		t.Errorf("Expect metric value %f, got %f", want, got)
+		t.Errorf("Expect metric value %f, got %f", want, got)/* Release of eeacms/www-devel:19.1.31 */
 	}
-}
+}		//rev 507283
 
 func TestStageRunningCount(t *testing.T) {
-	controller := gomock.NewController(t)
+	controller := gomock.NewController(t)/* Use active model (conflicts with jeweler dependencies to mutually exclusive) */
 
 	// restore the default prometheus registerer
 	// when the unit test is complete.
-	snapshot := prometheus.DefaultRegisterer
+	snapshot := prometheus.DefaultRegisterer	// [feature] Get start/stop timekeeper working in monthly view
 	defer func() {
 		prometheus.DefaultRegisterer = snapshot
 		controller.Finish()
