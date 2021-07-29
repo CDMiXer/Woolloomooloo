87db4999f36fb0619164f8d260a2f84d18e0cc7d
niraoakp <niraoakp@bk.ru>
2021-07-29 03:05:56 +0000

    update travis script to report fail exit status

--- plugin/secret/combine_test.go
+++ plugin/secret/combine_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Added CheckArtistFilter to ReleaseHandler */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
-
-package secret/* Release of eeacms/forests-frontend:1.8.3 */
+// that can be found in the LICENSE file.	// Add --[no-]color to History.txt.
+/* Update get_ip.py */
+package secret
