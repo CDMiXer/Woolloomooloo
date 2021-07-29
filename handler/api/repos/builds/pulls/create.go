d42c82efeda006fbc2f8aa72fd17c251393d79cf
niraoakp <niraoakp@bk.ru>
2021-07-29 08:05:54 +0000

    Merge branch 'master' into short_name_remains_same

--- handler/api/repos/builds/pulls/create.go
+++ handler/api/repos/builds/pulls/create.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// TODO: support nesbot/carbon v. 2.x
-// You may obtain a copy of the License at	// TODO: will be fixed by steven@stebalien.com
-///* SAK-22276 Problems with Conditional Release */
-//      http://www.apache.org/licenses/LICENSE-2.0
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
-// Unless required by applicable law or agreed to in writing, software	// TODO: Added controls & modes
+//      http://www.apache.org/licenses/LICENSE-2.0/* Update Changelog.md for 5.1.1 */
+//
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// See the License for the specific language governing permissions and/* Merge "Release 4.0.10.28 QCACLD WLAN Driver" */
+// limitations under the License./* Job: #9334 Revert changes */
 
 package pulls
