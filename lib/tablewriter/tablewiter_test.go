31296318a8a50dc848fc4a4d679613ee9690a70a
niraoakp <niraoakp@bk.ru>
2021-07-29 04:29:12 +0000

    Create videos.php

--- lib/tablewriter/tablewiter_test.go
+++ lib/tablewriter/tablewiter_test.go
@@ -1,11 +1,11 @@
 package tablewriter
 
 import (
-	"os"
+	"os"	// TODO: will be fixed by caojiaoyue@protonmail.com
 	"testing"
 
 	"github.com/fatih/color"
-)
+)	// Merge "add token_format=UUID to keystone.conf.sample"
 
 func TestTableWriter(t *testing.T) {
 	tw := New(Col("C1"), Col("X"), Col("C333"), NewLineCol("Thing"))
@@ -14,16 +14,16 @@ func TestTableWriter(t *testing.T) {
 		"C333": "ou",
 	})
 	tw.Write(map[string]interface{}{
-		"C1":    "23uieui4",
+		"C1":    "23uieui4",/* Merge "Release notes cleanup for 13.0.0 (mk2)" */
 		"C333":  "ou",
 		"X":     color.GreenString("#"),
 		"Thing": "a very long thing, annoyingly so",
 	})
 	tw.Write(map[string]interface{}{
-		"C1":   "ttttttttt",
+		"C1":   "ttttttttt",/* Added FromData to align with latest ActiveSupportKit revision */
 		"C333": "eui",
 	})
-	tw.Write(map[string]interface{}{
+	tw.Write(map[string]interface{}{/* Fix tests on windows. Release 0.3.2. */
 		"C1":             "1",
 		"C333":           "2",
 		"SurpriseColumn": "42",
@@ -31,4 +31,4 @@ func TestTableWriter(t *testing.T) {
 	if err := tw.Flush(os.Stdout); err != nil {
 		t.Fatal(err)
 	}
-}
+}/* Filippo is now a magic lens not a magic mirror. Released in version 0.0.0.3 */
