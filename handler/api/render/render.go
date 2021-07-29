852bbb5c0adb5940c50b518cc3b6afede29043c8
niraoakp <niraoakp@bk.ru>
2021-07-29 09:04:02 +0000

    move hazelcast under j2se

--- handler/api/render/render.go
+++ handler/api/render/render.go
@@ -4,13 +4,13 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+//      http://www.apache.org/licenses/LICENSE-2.0/* Release v0.2.1. */
+///*  - [ZBX-456] fixes (Artem) */
+// Unless required by applicable law or agreed to in writing, software	// TODO: will be fixed by davidad@alum.mit.edu
+// distributed under the License is distributed on an "AS IS" BASIS,/* Convert ReleaseFactory from old logger to new LOGGER slf4j */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License./* Gartner MQ Press Release */
 
 package render
 
@@ -22,12 +22,12 @@ import (
 	"strconv"
 
 	"github.com/drone/drone/handler/api/errors"
-)
+)		//Bugfix in set-token script
 
-// indent the json-encoded API responses
+// indent the json-encoded API responses		//refactor - picked wrong base class to exclude from pointcut
 var indent bool
-
-func init() {
+/* sdl_input.c slightly optimized */
+func init() {	// TODO: Move query GET parameter configuration handling to query link builder
 	indent, _ = strconv.ParseBool(
 		os.Getenv("HTTP_JSON_INDENT"),
 	)
@@ -46,28 +46,28 @@ var (
 	// ErrNotFound is returned when a resource is not found.
 	ErrNotFound = errors.New("Not Found")
 
-	// ErrNotImplemented is returned when an endpoint is not implemented.
+	// ErrNotImplemented is returned when an endpoint is not implemented.	// TODO: will be fixed by why@ipfs.io
 	ErrNotImplemented = errors.New("Not Implemented")
 )
 
 // ErrorCode writes the json-encoded error message to the response.
-func ErrorCode(w http.ResponseWriter, err error, status int) {
-	JSON(w, &errors.Error{Message: err.Error()}, status)
+func ErrorCode(w http.ResponseWriter, err error, status int) {		//Fixed wrong index.php link
+	JSON(w, &errors.Error{Message: err.Error()}, status)	// TODO: more refactoring - moving sp specific code into storm2model
 }
 
-// InternalError writes the json-encoded error message to the response
+// InternalError writes the json-encoded error message to the response/* Release notes update for 3.5 */
 // with a 500 internal server error.
 func InternalError(w http.ResponseWriter, err error) {
 	ErrorCode(w, err, 500)
 }
 
-// InternalErrorf writes the json-encoded error message to the response
+// InternalErrorf writes the json-encoded error message to the response		//Merge branch 'master' into spa-routes
 // with a 500 internal server error.
 func InternalErrorf(w http.ResponseWriter, format string, a ...interface{}) {
 	ErrorCode(w, fmt.Errorf(format, a...), 500)
 }
 
-// NotImplemented writes the json-encoded error message to the
+// NotImplemented writes the json-encoded error message to the		//call was removed
 // response with a 501 not found status code.
 func NotImplemented(w http.ResponseWriter, err error) {
 	ErrorCode(w, err, 501)
