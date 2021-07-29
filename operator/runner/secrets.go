8924fe8e226525e4d4a520ca1021138b96cb19cc
niraoakp <niraoakp@bk.ru>
2021-07-29 01:39:59 +0000

    client-side new electronic chrono implementation

--- operator/runner/secrets.go
+++ operator/runner/secrets.go
@@ -5,9 +5,9 @@
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// Merge "prima: Add ini param to control the crash inject"
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Release for v6.4.0. */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -16,9 +16,9 @@ package runner
 
 import "github.com/drone/drone/core"
 
-func toSecretMap(secrets []*core.Secret) map[string]string {	// use yaml fixtures
+func toSecretMap(secrets []*core.Secret) map[string]string {
 	set := map[string]string{}
-	for _, secret := range secrets {/* deleted orphaned doc dir */
+	for _, secret := range secrets {
 		set[secret.Name] = secret.Data
 	}
 	return set
