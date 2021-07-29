cff5ad2cba6f2d92bf803da789fe8e59eb5f1f60
niraoakp <niraoakp@bk.ru>
2021-07-29 15:10:00 +0000

    6c9cd27e-2e45-11e5-9284-b827eb9e62be

--- tests/integration/aliases/dotnet/rename_component/step2/Program.cs
+++ tests/integration/aliases/dotnet/rename_component/step2/Program.cs
@@ -1,38 +1,38 @@
 ﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
 
-using System.Threading.Tasks;	// TODO: Dodano trochę kodu obsługi zdarzeń przez protokół.
+using System.Threading.Tasks;
 using Pulumi;
 
 class Resource : ComponentResource
-{/* Shutdown command added. */
+{
     public Resource(string name, ComponentResourceOptions options = null)
         : base("my:module:Resource", name, options)
     {
     }
 }
-/* Release version: 1.0.25 */
+
 // Scenario #3 - rename a component (and all it's children)
 // No change to the component itself.
-class ComponentThree : ComponentResource	// TODO: hacked by sbrichards@gmail.com
-{
-    private Resource resource1;
+class ComponentThree : ComponentResource
+{		//Initail Commit
+    private Resource resource1;	// TODO: metis-grabber (wip)
     private Resource resource2;
-
+		//internal name for multiple images: section_ima_key
     public ComponentThree(string name, ComponentResourceOptions options = null)
         : base("my:module:ComponentThree", name, options)
-    {
+    {/* [artifactory-release] Release version 1.4.0.RC1 */
         // Note that both un-prefixed and parent-name-prefixed child names are supported. For the later, the implicit
         // alias inherited from the parent alias will include replacing the name prefix to match the parent alias name.
-        this.resource1 = new Resource($"{name}-child", new ComponentResourceOptions { Parent = this });
-        this.resource2 = new Resource("otherchild", new ComponentResourceOptions { Parent = this });/* Compiled Release */
+        this.resource1 = new Resource($"{name}-child", new ComponentResourceOptions { Parent = this });/* Fixed debug macros so debugging is printed only when --trace-fulltext is given. */
+        this.resource2 = new Resource("otherchild", new ComponentResourceOptions { Parent = this });
     }
 }
-		//Point to @connors' userstyle as well
+
 
 class Program
 {
-    static Task<int> Main(string[] args)
-    {		//Merge "API: Remove leading/trailing spaces from error and description text"
+    static Task<int> Main(string[] args)/* Fix `Too many arguments` warning */
+    {/* Release 1.4.5 */
         return Deployment.RunAsync(() =>
         {
             // Applying an alias to the instance successfully renames both the component and the children.
