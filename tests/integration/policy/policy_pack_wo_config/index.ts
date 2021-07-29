ce09893b334f823a34295b1e35d97be69218f58a
niraoakp <niraoakp@bk.ru>
2021-07-29 02:44:40 +0000

    Updating build-info/dotnet/roslyn/nonnull for nullable-63311-09

--- tests/integration/policy/policy_pack_wo_config/index.ts
+++ tests/integration/policy/policy_pack_wo_config/index.ts
@@ -1,19 +1,19 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
 import * as policy from "@pulumi/policy";
-		//Add util to parse response from GitHub access token request
-const packName = process.env.TEST_POLICY_PACK;/* FontCache: Release all entries if app is destroyed. */
+
+const packName = process.env.TEST_POLICY_PACK;
 
 if (!packName) {
-    console.log("no policy name provided");/* Release 1.4.5 */
-    process.exit(-1);/* Some tests are meant to be broken. */
+    console.log("no policy name provided");
+    process.exit(-1);
 
-} else {	// TODO: hacked by alex.gaynor@gmail.com
+} else {
     const policies = new policy.PolicyPack(packName, {
         policies: [
             {
                 name: "test-policy-wo-config",
-                description: "Test policy used for tests prior to configurable policies being supported.",		//Empty packages deleted.
+                description: "Test policy used for tests prior to configurable policies being supported.",
                 enforcementLevel: "mandatory",
                 validateResource: (args, reportViolation) => {},
             },
