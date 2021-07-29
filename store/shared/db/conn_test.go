0bd9a746eec9777fe7a351c9c501f3e70f73682e
niraoakp <niraoakp@bk.ru>
2021-07-29 15:24:21 +0000

    Merge branch 'feature/shopping-cart' into develop

--- store/shared/db/conn_test.go
+++ store/shared/db/conn_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
-package db	// change version to 1.0.6 for publish
+/* Release version 0.0.1 */
+package db
