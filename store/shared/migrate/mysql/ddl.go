1c4b294f26e5415fa621c05f371a897fceb7d9e4
niraoakp <niraoakp@bk.ru>
2021-07-29 12:19:16 +0000

    Readded subfolders

--- store/shared/migrate/mysql/ddl.go
+++ store/shared/migrate/mysql/ddl.go
@@ -1,6 +1,6 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file.		//Update django-extensions from 2.2.6 to 2.2.8
 
 // +build !oss
 
