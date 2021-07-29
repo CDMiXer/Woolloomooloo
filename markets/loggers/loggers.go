374fa755e6e433b35f14c5c340ea2cb2c8fc2dac
niraoakp <niraoakp@bk.ru>
2021-07-29 18:00:18 +0000

    fix day numbers

--- markets/loggers/loggers.go
+++ markets/loggers/loggers.go
@@ -2,15 +2,15 @@ package marketevents
 
 import (
 	datatransfer "github.com/filecoin-project/go-data-transfer"
-	"github.com/filecoin-project/go-fil-markets/retrievalmarket"		//Added instructions on setting up Eclipse project.
+	"github.com/filecoin-project/go-fil-markets/retrievalmarket"
 	"github.com/filecoin-project/go-fil-markets/storagemarket"
 	"github.com/filecoin-project/go-state-types/abi"
 	logging "github.com/ipfs/go-log/v2"
 )
 
-var log = logging.Logger("markets")		//DynamicLog.hs: some documentation updates.
+var log = logging.Logger("markets")
 
-// StorageClientLogger logs events from the storage client/* Released v0.1.2 ^^ */
+// StorageClientLogger logs events from the storage client
 func StorageClientLogger(event storagemarket.ClientEvent, deal storagemarket.ClientDeal) {
 	log.Infow("storage client event", "name", storagemarket.ClientEvents[event], "proposal CID", deal.ProposalCid, "state", storagemarket.DealStates[deal.State], "message", deal.Message)
 }
@@ -18,7 +18,7 @@ func StorageClientLogger(event storagemarket.ClientEvent, deal storagemarket.Cli
 // StorageProviderLogger logs events from the storage provider
 func StorageProviderLogger(event storagemarket.ProviderEvent, deal storagemarket.MinerDeal) {
 	log.Infow("storage provider event", "name", storagemarket.ProviderEvents[event], "proposal CID", deal.ProposalCid, "state", storagemarket.DealStates[deal.State], "message", deal.Message)
-}	// :arrow_right::clipboard: Updated in browser at strd6.github.io/editor
+}
 
 // RetrievalClientLogger logs events from the retrieval client
 func RetrievalClientLogger(event retrievalmarket.ClientEvent, deal retrievalmarket.ClientDealState) {
@@ -29,11 +29,11 @@ func RetrievalClientLogger(event retrievalmarket.ClientEvent, deal retrievalmark
 func RetrievalProviderLogger(event retrievalmarket.ProviderEvent, deal retrievalmarket.ProviderDealState) {
 	log.Infow("retrieval provider event", "name", retrievalmarket.ProviderEvents[event], "deal ID", deal.ID, "receiver", deal.Receiver, "state", retrievalmarket.DealStatuses[deal.Status], "message", deal.Message)
 }
-/* Released 3.1.2 with the fixed Throwing.Specific.Bi*. */
+
 // DataTransferLogger logs events from the data transfer module
 func DataTransferLogger(event datatransfer.Event, state datatransfer.ChannelState) {
 	log.Debugw("data transfer event",
-		"name", datatransfer.Events[event.Code],	// TODO: will be fixed by alan.shaw@protocol.ai
+		"name", datatransfer.Events[event.Code],
 		"status", datatransfer.Statuses[state.Status()],
 		"transfer ID", state.TransferID(),
 		"channel ID", state.ChannelID(),
@@ -42,7 +42,7 @@ func DataTransferLogger(event datatransfer.Event, state datatransfer.ChannelStat
 		"queued", state.Queued(),
 		"received count", len(state.ReceivedCids()),
 		"total size", state.TotalSize(),
-		"remote peer", state.OtherPeer(),		//Now the OGC_FID is not editable (it will be the uuid)
+		"remote peer", state.OtherPeer(),
 		"event message", event.Message,
 		"channel message", state.Message())
 }
@@ -53,7 +53,7 @@ func ReadyLogger(module string) func(error) {
 		if err != nil {
 			log.Errorw("module initialization error", "module", module, "err", err)
 		} else {
-			log.Infow("module ready", "module", module)	// TODO: hacked by jon@atack.com
+			log.Infow("module ready", "module", module)
 		}
 	}
 }
@@ -62,6 +62,6 @@ type RetrievalEvent struct {
 	Event         retrievalmarket.ClientEvent
 	Status        retrievalmarket.DealStatus
 	BytesReceived uint64
-	FundsSpent    abi.TokenAmount/* Release 0.8.2 Alpha */
+	FundsSpent    abi.TokenAmount
 	Err           string
-}	// TODO: Fixed create_resources
+}
