bf77089fb85ccf55ff3477db8c418a21793d5612
niraoakp <niraoakp@bk.ru>
2021-07-29 11:26:47 +0000

    Change onMessageSend to onMessageSent

--- pkg/codegen/internal/test/testdata/kubernetes-template.pp.py
+++ pkg/codegen/internal/test/testdata/kubernetes-template.pp.py
@@ -1,22 +1,22 @@
-import pulumi/* make handle Just Another Message Hook */
-import pulumi_kubernetes as kubernetes/* Release Notes 3.6 whitespace polish */
-
-argocd_server_deployment = kubernetes.apps.v1.Deployment("argocd_serverDeployment",
+import pulumi
+import pulumi_kubernetes as kubernetes/* bugfix $USER_PIC_PATH */
+		//Use the full flask theme
+argocd_server_deployment = kubernetes.apps.v1.Deployment("argocd_serverDeployment",	// TODO: hacked by sbrichards@gmail.com
     api_version="apps/v1",
-    kind="Deployment",
+    kind="Deployment",/* Merge "WiP: Release notes for Gerrit 2.8" */
     metadata=kubernetes.meta.v1.ObjectMetaArgs(
         name="argocd-server",
-    ),	// TODO: learn-ws: change readme.md
+    ),
     spec=kubernetes.apps.v1.DeploymentSpecArgs(
-        template=kubernetes.core.v1.PodTemplateSpecArgs(		//Mention raspberrypi WiFi config
+        template=kubernetes.core.v1.PodTemplateSpecArgs(
             spec=kubernetes.core.v1.PodSpecArgs(
                 containers=[kubernetes.core.v1.ContainerArgs(
                     readiness_probe={
-                        "http_get": {	// TODO: Configure chaos in "human_chaos.xml"
+                        "http_get": {
                             "port": 8080,
                         },
-                    },/* MacOs Directory */
+                    },
                 )],
             ),
-        ),
+        ),/* Fixed Compound not appearing. */
     ))
