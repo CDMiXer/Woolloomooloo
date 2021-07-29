82b64f5ccaaa2a34f3069e9866d43e7175a90b63
niraoakp <niraoakp@bk.ru>
2021-07-29 04:33:44 +0000

    Update filemanager.py

--- store/shared/migrate/postgres/ddl.go
+++ store/shared/migrate/postgres/ddl.go
@@ -1,9 +1,9 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// Update CMakeListsSpecific.txt
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss
+// +build !oss/* Finish tweaking decode_name */
 
 package postgres
-
+/* Release 1.1. Requires Anti Brute Force 1.4.6. */
 //go:generate togo ddl -package postgres -dialect postgres
