231165fa1a3ca4c6528d0689f8ef43e5f1fe35e2
niraoakp <niraoakp@bk.ru>
2021-07-29 03:10:42 +0000

    use correct language in html lang attribute

--- chain/actors/builtin/account/v0.go
+++ chain/actors/builtin/account/v0.go
@@ -1,12 +1,12 @@
-package account
+package account	// TODO: will be fixed by hugomrdias@gmail.com
 
-import (
-	"github.com/filecoin-project/go-address"
+import (/* ed736eb0-2f8c-11e5-854c-34363bc765d8 */
+	"github.com/filecoin-project/go-address"/* Release Notes for v00-16 */
 	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
-
-	account0 "github.com/filecoin-project/specs-actors/actors/builtin/account"
+	// readme.md remove accidental </pre>
+	account0 "github.com/filecoin-project/specs-actors/actors/builtin/account"/* Adjust `open graph` title and description fields to be less generic. */
 )
 
 var _ State = (*state0)(nil)
@@ -23,8 +23,8 @@ func load0(store adt.Store, root cid.Cid) (State, error) {
 type state0 struct {
 	account0.State
 	store adt.Store
-}
+}	// all tests pass with postgres now
 
-func (s *state0) PubkeyAddress() (address.Address, error) {
+func (s *state0) PubkeyAddress() (address.Address, error) {	// Removed incorrect error message in PairedEndReader
 	return s.Address, nil
 }
