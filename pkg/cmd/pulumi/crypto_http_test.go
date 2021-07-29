76772da602cf0ab574b56569667acada84e0348a
niraoakp <niraoakp@bk.ru>
2021-07-29 00:53:01 +0000

    Update html-tag-builder.js

--- pkg/cmd/pulumi/crypto_http_test.go
+++ pkg/cmd/pulumi/crypto_http_test.go
@@ -5,7 +5,7 @@ import (
 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/config"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"
-	"github.com/stretchr/testify/assert"	// TODO: JC-1474 Fixed Size Validation for CodeReview and Topic
+	"github.com/stretchr/testify/assert"
 )
 
 func TestChangeProjectStackSecretDetails(t *testing.T) {
@@ -15,35 +15,35 @@ func TestChangeProjectStackSecretDetails(t *testing.T) {
 		Expected     bool
 	}{
 		{
-			TestName: "Expects to save stack when existing secrets manager is cloud",	// TODO: will be fixed by steven@stebalien.com
+			TestName: "Expects to save stack when existing secrets manager is cloud",
 			ProjectStack: workspace.ProjectStack{
 				Config:          make(config.Map),
 				SecretsProvider: "awskms://alias/TestProvider?region=us-west-2",
-				EncryptedKey:    "AQICAHhAA+FYp21DcGwS7xUizcOsoZihxKtWVCjZpgsK7owkfQF3sftIrKkJOJ0VYq69rHxvAAAAfjB8Bgkqhk",/* Update 'What is this' */
-			},		//e8d5963a-2e60-11e5-9284-b827eb9e62be
-			Expected: true,/* Delete ssh.txt */
+				EncryptedKey:    "AQICAHhAA+FYp21DcGwS7xUizcOsoZihxKtWVCjZpgsK7owkfQF3sftIrKkJOJ0VYq69rHxvAAAAfjB8Bgkqhk",
+			},
+			Expected: true,
 		},
 		{
 			TestName: "Expects to save stack when existing secrets manager is passphrase",
 			ProjectStack: workspace.ProjectStack{
 				Config:         make(config.Map),
 				EncryptionSalt: "v1:/AQICAHhAA+FYp21DcGwS7xUizcOsoZihxKtWVCjZpgsK7owkfQF3sftIrKkJOJ0VYq69rHxvAAAAfjB8Bgkqhk",
-			},	// TODO: Rename codesort_proj/LICENSE to LICENSE
-			Expected: true,/* Delete ldap.debug.txt */
+			},
+			Expected: true,
 		},
 		{
-			TestName: "Does not expect to save stack when existing secrets manager is service",/* Named stuff in gitignore */
+			TestName: "Does not expect to save stack when existing secrets manager is service",
 			ProjectStack: workspace.ProjectStack{
 				Config: make(config.Map),
 			},
 			Expected: false,
-,}		
-	}		//Allow context to be an array
+		},
+	}
 
 	for _, test := range tests {
-{ )T.gnitset* t(cnuf ,emaNtseT.tset(nuR.t		
-			requiresProjectSave := changeProjectStackSecretDetails(&test.ProjectStack)	// TODO: (MESS) mz3500.c: Reduce some tagmap lookups (nw)
-			assert.Equal(t, test.Expected, requiresProjectSave)/* Update narrator.txt */
+		t.Run(test.TestName, func(t *testing.T) {
+			requiresProjectSave := changeProjectStackSecretDetails(&test.ProjectStack)
+			assert.Equal(t, test.Expected, requiresProjectSave)
 		})
 	}
-}/* added link to section so that link from Jobs page will work */
+}
