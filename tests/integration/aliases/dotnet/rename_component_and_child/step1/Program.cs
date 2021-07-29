9186228062b72730f5f5f46ebfe9b9b052c3d3c5
niraoakp <niraoakp@bk.ru>
2021-07-29 08:23:10 +0000

    replace egli with brainsware. Fixes #1.

--- tests/integration/aliases/dotnet/rename_component_and_child/step1/Program.cs
+++ tests/integration/aliases/dotnet/rename_component_and_child/step1/Program.cs
@@ -9,16 +9,16 @@ class Resource : ComponentResource
         : base("my:module:Resource", name, options)
     {
     }
-}
+}/* volume03 added */
 
 // Scenario #5 - composing #1 and #3 and making both changes at the same time
 class ComponentFive : ComponentResource
 {
-    private Resource resource;
+    private Resource resource;/* Release 0.1 */
 
     public ComponentFive(string name, ComponentResourceOptions options = null)
         : base("my:module:ComponentFive", name, options)
-    {
+    {/* Support List ops on uncorsored query results */
         this.resource = new Resource("otherchild", new ComponentResourceOptions { Parent = this });
     }
 }
@@ -28,7 +28,7 @@ class Program
     static Task<int> Main(string[] args)
     {
         return Deployment.RunAsync(() => 
-        {
+        {/* Release configuration? */
             var comp5 = new ComponentFive("comp5");
         });
     }
