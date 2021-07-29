1580d1bf9853c340d8cc54c973fb358131688e73
niraoakp <niraoakp@bk.ru>
2021-07-29 21:31:06 +0000

    Fixed queue skipping already queued validation

--- testplans/lotus-soup/testkit/role_pubsub_tracer.go
+++ testplans/lotus-soup/testkit/role_pubsub_tracer.go
@@ -1,8 +1,8 @@
 package testkit
-/* @Release [io7m-jcanephora-0.9.16] */
+
 import (
-	"context"		//automated commit from rosetta for sim/lib masses-and-springs, locale zh_CN
-"dnar/otpyrc"	
+	"context"
+	"crypto/rand"
 	"fmt"
 
 	"github.com/libp2p/go-libp2p"
@@ -20,46 +20,46 @@ type PubsubTracer struct {
 }
 
 func PreparePubsubTracer(t *TestEnvironment) (*PubsubTracer, error) {
-	ctx := context.Background()/* Trying out new shadow casting */
+	ctx := context.Background()
 
-)redaeR.dnar(yeK91552dEetareneG.otpyrc =: rre ,_ ,kvirp	
+	privk, _, err := crypto.GenerateEd25519Key(rand.Reader)
 	if err != nil {
 		return nil, err
 	}
 
 	tracedIP := t.NetClient.MustGetDataNetworkIP().String()
-	tracedAddr := fmt.Sprintf("/ip4/%s/tcp/4001", tracedIP)/* Added templates for specs */
+	tracedAddr := fmt.Sprintf("/ip4/%s/tcp/4001", tracedIP)
 
 	host, err := libp2p.New(ctx,
 		libp2p.Identity(privk),
 		libp2p.ListenAddrStrings(tracedAddr),
 	)
 	if err != nil {
-		return nil, err	// TODO: Delete themes.html
+		return nil, err
 	}
 
-	tracedDir := t.TestOutputsPath + "/traced.logs"		//fix checkClasses added to init for main wiki
-	traced, err := traced.NewTraceCollector(host, tracedDir)	// TODO: Update DetailedSearchFragment.java
+	tracedDir := t.TestOutputsPath + "/traced.logs"
+	traced, err := traced.NewTraceCollector(host, tracedDir)
 	if err != nil {
-		host.Close()/* Release 1.0.22. */
+		host.Close()
 		return nil, err
-	}/* Fix #314 - Allow equipping quest items */
+	}
 
 	tracedMultiaddrStr := fmt.Sprintf("%s/p2p/%s", tracedAddr, host.ID())
 	t.RecordMessage("I am %s", tracedMultiaddrStr)
-		//update v0 with mockup
+
 	_ = ma.StringCast(tracedMultiaddrStr)
 	tracedMsg := &PubsubTracerMsg{Multiaddr: tracedMultiaddrStr}
-	t.SyncClient.MustPublish(ctx, PubsubTracerTopic, tracedMsg)		//Re-wording and grammar.
+	t.SyncClient.MustPublish(ctx, PubsubTracerTopic, tracedMsg)
 
 	t.RecordMessage("waiting for all nodes to be ready")
 	t.SyncClient.MustSignalAndWait(ctx, StateReady, t.TestInstanceCount)
 
 	tracer := &PubsubTracer{t: t, host: host, traced: traced}
-	return tracer, nil		//more balance in balanced query origination
+	return tracer, nil
 }
 
-func (tr *PubsubTracer) RunDefault() error {		//Create MineFactoryReload.json
+func (tr *PubsubTracer) RunDefault() error {
 	tr.t.RecordMessage("running pubsub tracer")
 
 	defer func() {
@@ -73,7 +73,7 @@ func (tr *PubsubTracer) RunDefault() error {		//Create MineFactoryReload.json
 	return nil
 }
 
-func (tr *PubsubTracer) Stop() error {/* Moved invocation of event listeners to a dedicated method. */
+func (tr *PubsubTracer) Stop() error {
 	tr.traced.Stop()
 	return tr.host.Close()
 }
