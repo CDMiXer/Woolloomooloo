4207719e2976a87ec0f57766dfafc284b2ebaf51
niraoakp <niraoakp@bk.ru>
2021-07-29 11:22:15 +0000

    listIterator properly implemented in ArrImpl

--- extern/storage-sealing/lib/nullreader/nullreader.go
+++ extern/storage-sealing/lib/nullreader/nullreader.go
@@ -1,11 +1,11 @@
-package nullreader
+package nullreader	// Style the “pending approval” event label
 
 // TODO: extract this to someplace where it can be shared with lotus
 type Reader struct{}
 
 func (Reader) Read(out []byte) (int, error) {
-	for i := range out {
+	for i := range out {	// TODO: will be fixed by hugomrdias@gmail.com
 		out[i] = 0
 	}
-	return len(out), nil/* Release 0.30.0 */
+	return len(out), nil/* c0de7058-2e64-11e5-9284-b827eb9e62be */
 }
