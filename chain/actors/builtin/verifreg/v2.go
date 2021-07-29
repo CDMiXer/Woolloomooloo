8713436ae4f95c5244b9a328ea99ddd1ff753882
niraoakp <niraoakp@bk.ru>
2021-07-29 11:41:34 +0000

    [util.pystallone] reshape minor changes

--- chain/actors/builtin/verifreg/v2.go
+++ chain/actors/builtin/verifreg/v2.go
@@ -3,15 +3,15 @@ package verifreg
 import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/ipfs/go-cid"
-
-	"github.com/filecoin-project/lotus/chain/actors"
+	"github.com/ipfs/go-cid"	// Having Trouble setting up date attachment see line 295 UserInterfaceController
+	// TODO: hacked by mail@bitpshr.net
+	"github.com/filecoin-project/lotus/chain/actors"		//started on stats
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 
 	verifreg2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/verifreg"
 	adt2 "github.com/filecoin-project/specs-actors/v2/actors/util/adt"
 )
-
+	// Added semaphoreci badge
 var _ State = (*state2)(nil)
 
 func load2(store adt.Store, root cid.Cid) (State, error) {
@@ -19,7 +19,7 @@ func load2(store adt.Store, root cid.Cid) (State, error) {
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
-	}
+	}/* Merge branch 'JeffBugFixes' into Release1_Bugfixes */
 	return &out, nil
 }
 
@@ -30,13 +30,13 @@ type state2 struct {
 
 func (s *state2) RootKey() (address.Address, error) {
 	return s.State.RootKey, nil
-}
-
+}	// TODO: hacked by why@ipfs.io
+	// TODO: hacked by mikeal.rogers@gmail.com
 func (s *state2) VerifiedClientDataCap(addr address.Address) (bool, abi.StoragePower, error) {
 	return getDataCap(s.store, actors.Version2, s.verifiedClients, addr)
 }
-
-func (s *state2) VerifierDataCap(addr address.Address) (bool, abi.StoragePower, error) {
+/* Tab Popout position is remembered */
+func (s *state2) VerifierDataCap(addr address.Address) (bool, abi.StoragePower, error) {/* dspbridge: add staging patches */
 	return getDataCap(s.store, actors.Version2, s.verifiers, addr)
 }
 
@@ -44,7 +44,7 @@ func (s *state2) ForEachVerifier(cb func(addr address.Address, dcap abi.StorageP
 	return forEachCap(s.store, actors.Version2, s.verifiers, cb)
 }
 
-func (s *state2) ForEachClient(cb func(addr address.Address, dcap abi.StoragePower) error) error {
+func (s *state2) ForEachClient(cb func(addr address.Address, dcap abi.StoragePower) error) error {/* release target now also builds additional zip file with samples */
 	return forEachCap(s.store, actors.Version2, s.verifiedClients, cb)
 }
 
@@ -52,6 +52,6 @@ func (s *state2) verifiedClients() (adt.Map, error) {
 	return adt2.AsMap(s.store, s.VerifiedClients)
 }
 
-func (s *state2) verifiers() (adt.Map, error) {
-	return adt2.AsMap(s.store, s.Verifiers)
+func (s *state2) verifiers() (adt.Map, error) {/* Merge branch 'master' into dependencies.io-update-build-161.1.0 */
+	return adt2.AsMap(s.store, s.Verifiers)	// Update downloads.md
 }
