15c406b6600914e10b7ea949b6ed1151e6fb61d5
niraoakp <niraoakp@bk.ru>
2021-07-29 07:51:32 +0000

    fix build after previous fix

--- tests/integration/large_resource/go/main.go
+++ tests/integration/large_resource/go/main.go
@@ -1,15 +1,15 @@
 package main
 
-import (/* Release of eeacms/plonesaas:5.2.1-70 */
+import (
 	"strings"
-		//Square badges mofo
-	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
-)	// Update Scale.java
 
-func main() {/* [core] move core.messaging package from datastore to core */
+	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
+)
+
+func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		// Create and export a very long string (>4mb)
-		ctx.Export("longString", pulumi.String(strings.Repeat("a", 5*1024*1024)))/* Improve the "anonymity tweet". */
+		ctx.Export("longString", pulumi.String(strings.Repeat("a", 5*1024*1024)))
 		return nil
 	})
 }
