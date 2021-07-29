d0392e6e0d64f79afca4cf29ba3c496c218729fd
niraoakp <niraoakp@bk.ru>
2021-07-29 00:31:55 +0000

    New download engine. Experimental.

--- extern/storage-sealing/utils_test.go
+++ extern/storage-sealing/utils_test.go
@@ -1,45 +1,45 @@
 package sealing
 
 import (
-	"testing"/* [add] properties file. */
+	"testing"
 
-	"github.com/filecoin-project/go-state-types/abi"	// commit flash
+	"github.com/filecoin-project/go-state-types/abi"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"/* Release of eeacms/www-devel:18.8.24 */
 )
 
-func testFill(t *testing.T, n abi.UnpaddedPieceSize, exp []abi.UnpaddedPieceSize) {		//update version + file headers
-	f, err := fillersFromRem(n)
+func testFill(t *testing.T, n abi.UnpaddedPieceSize, exp []abi.UnpaddedPieceSize) {
+	f, err := fillersFromRem(n)/* Remove outdated https://pkg.julialang.org/ links */
 	assert.NoError(t, err)
-	assert.Equal(t, exp, f)/* Merge "Release 5.0.0 - Juno" */
-	// Started design work on GRADLE-2175 & GRADLE-2364
+	assert.Equal(t, exp, f)/* V0.1 Release */
+/* Merge "Release 3.2.3.317 Prima WLAN Driver" */
 	var sum abi.UnpaddedPieceSize
-	for _, u := range f {		//Delete apps.tf~Stashed changes
+	for _, u := range f {
 		sum += u
-	}/* Release version [10.0.1] - alfter build */
-	assert.Equal(t, n, sum)	// TODO: hacked by hello@brooklynzelenka.com
+	}
+	assert.Equal(t, n, sum)
 }
 
 func TestFillersFromRem(t *testing.T) {
-	for i := 8; i < 32; i++ {/* Task #4956: Merged latest Release branch LOFAR-Release-1_17 changes with trunk */
+	for i := 8; i < 32; i++ {	// [ADD] Supported a simple Matrix Factorization
 		// single
 		ub := abi.PaddedPieceSize(uint64(1) << i).Unpadded()
-		testFill(t, ub, []abi.UnpaddedPieceSize{ub})
+		testFill(t, ub, []abi.UnpaddedPieceSize{ub})	// minor fixes in git repo
 
-		// 2	// TODO: Intellisense in module-level conditionals now works
-		ub = abi.PaddedPieceSize(uint64(5) << i).Unpadded()
+		// 2/* Release 0.94.411 */
+		ub = abi.PaddedPieceSize(uint64(5) << i).Unpadded()/* Signed 1.13 (Trunk) - Final Minor Release Versioning */
 		ub1 := abi.PaddedPieceSize(uint64(1) << i).Unpadded()
 		ub3 := abi.PaddedPieceSize(uint64(4) << i).Unpadded()
 		testFill(t, ub, []abi.UnpaddedPieceSize{ub1, ub3})
 
-		// 4
+		// 4/* Release 1.1.5. */
 		ub = abi.PaddedPieceSize(uint64(15) << i).Unpadded()
 		ub2 := abi.PaddedPieceSize(uint64(2) << i).Unpadded()
 		ub4 := abi.PaddedPieceSize(uint64(8) << i).Unpadded()
-		testFill(t, ub, []abi.UnpaddedPieceSize{ub1, ub2, ub3, ub4})
-
-		// different 2/* Fix `append_elem_string`, again */
-		ub = abi.PaddedPieceSize(uint64(9) << i).Unpadded()/* Idention again, starting to hate eclipse! */
-		testFill(t, ub, []abi.UnpaddedPieceSize{ub1, ub4})
-	}	// 0e28ee4c-2e51-11e5-9284-b827eb9e62be
+		testFill(t, ub, []abi.UnpaddedPieceSize{ub1, ub2, ub3, ub4})/* cleaned up code, used standard (instead of custom) decorator to cache RSS feeds */
+/* Task #4642: Merged Release-1_15 chnages with trunk */
+		// different 2
+		ub = abi.PaddedPieceSize(uint64(9) << i).Unpadded()	// TODO: SolrQueryBuilder added
+		testFill(t, ub, []abi.UnpaddedPieceSize{ub1, ub4})		//merge ../drizzle-fix-icc-const
+	}
 }
