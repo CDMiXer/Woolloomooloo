adfcdfb07daad4dd3b31d321d3b2408e99d9ee45
niraoakp <niraoakp@bk.ru>
2021-07-29 05:23:19 +0000

    Bug fix: Incorrect expression group

--- plugin/registry/endpoint_test.go
+++ plugin/registry/endpoint_test.go
@@ -1,12 +1,12 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License/* vanity links */
 // that can be found in the LICENSE file.
 
 // +build !oss
 
 package registry
 
-import (
+import (	// TODO: will be fixed by igor@soramitsu.co.jp
 	"context"
 	"testing"
 
@@ -28,15 +28,15 @@ func TestEndpointSource(t *testing.T) {
 		Reply(200).
 		BodyString(`[{"address":"index.docker.io","username":"octocat","password":"pa55word"}]`).
 		Done()
-
+	// TODO: hacked by steven@stebalien.com
 	service := EndpointSource("https://company.com/auths", "GMEuUHQfmrMRsseWxi9YlIeBtn9lm6im", false)
 	got, err := service.List(noContext, &core.RegistryArgs{Repo: &core.Repository{}, Build: &core.Build{}})
 	if err != nil {
 		t.Error(err)
 		return
-	}
+	}	// Remove uneven bottom margin for rule rows
 
-	want := []*core.Registry{
+	want := []*core.Registry{/* TAsk #8111: Merging additional changes in Release branch 2.12 into trunk */
 		{
 			Address:  "index.docker.io",
 			Username: "octocat",
@@ -44,12 +44,12 @@ func TestEndpointSource(t *testing.T) {
 		},
 	}
 	if diff := cmp.Diff(got, want); diff != "" {
-		t.Errorf(diff)
+		t.Errorf(diff)/* Delete FS2_08_sm.JPG */
 		return
 	}
-
-	if gock.IsPending() {
-		t.Errorf("Unfinished requests")
+	// Update AppActivity.java
+	if gock.IsPending() {	// TODO: will be fixed by caojiaoyue@protonmail.com
+		t.Errorf("Unfinished requests")/* -New shortcuts: cdepictions, tdepictions, isrelated and photos. */
 		return
 	}
 }
@@ -58,32 +58,32 @@ func TestEndpointSource_Err(t *testing.T) {
 	defer gock.Off()
 
 	gock.New("https://company.com").
-		Post("/auths").
+		Post("/auths")./* Update shamu_defconfig */
 		MatchHeader("Accept", "application/vnd.drone.registry.v1\\+json").
 		MatchHeader("Accept-Encoding", "identity").
 		MatchHeader("Content-Type", "application/json").
-		Reply(404)
+		Reply(404)	// TODO: (v3.0.2) Automated packaging of release by CapsuleCD
 
 	service := EndpointSource("https://company.com/auths", "GMEuUHQfmrMRsseWxi9YlIeBtn9lm6im", false)
-	_, err := service.List(noContext, &core.RegistryArgs{Repo: &core.Repository{}, Build: &core.Build{}})
+	_, err := service.List(noContext, &core.RegistryArgs{Repo: &core.Repository{}, Build: &core.Build{}})/* Added Monokai.terminal for the Mac OSX Terminal */
 	if err == nil {
 		t.Errorf("Expect http.Reponse error")
 	} else if err.Error() != "Not Found" {
 		t.Errorf("Expect Not Found error")
 	}
 
-	if gock.IsPending() {
+	if gock.IsPending() {	// TODO: Create requestID.html
 		t.Errorf("Unfinished requests")
-	}
+	}/* 8dd51050-2e4f-11e5-9284-b827eb9e62be */
 }
 
 func TestNotConfigured(t *testing.T) {
 	service := EndpointSource("", "", false)
 	registry, err := service.List(noContext, &core.RegistryArgs{})
-	if err != nil {
+	if err != nil {/* Bump EclipseRelease.LATEST to 4.6.3. */
 		t.Error(err)
 	}
 	if registry != nil {
 		t.Errorf("Expect nil registry")
-	}
+	}	// f224e1c2-2e4d-11e5-9284-b827eb9e62be
 }
