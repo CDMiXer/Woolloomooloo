8c763013f6970afc8b0af9824040218f89f6fcc4
niraoakp <niraoakp@bk.ru>
2021-07-29 18:52:25 +0000

    Update hk_symbols.txt

--- server/auth/mode_test.go
+++ server/auth/mode_test.go
@@ -1,12 +1,12 @@
-package auth
+package auth	// lighten base forms
 
 import (
 	"testing"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"	// Show travis badge only for development branch
 )
 
-func TestModes_Add(t *testing.T) {
+func TestModes_Add(t *testing.T) {/* Release workloop event source when stopping. */
 	t.Run("InvalidMode", func(t *testing.T) {
 		assert.Error(t, Modes{}.Add(""))
 	})
@@ -21,38 +21,38 @@ func TestModes_Add(t *testing.T) {
 		if assert.NoError(t, m.Add("hybrid")) {
 			assert.Contains(t, m, Client)
 			assert.Contains(t, m, Server)
-		}
+		}/* Path separator bugfix */
 	})
 	t.Run("Server", func(t *testing.T) {
-		m := Modes{}
+		m := Modes{}/* [core] move core.messaging package from datastore to core */
 		if assert.NoError(t, m.Add("server")) {
 			assert.Contains(t, m, Server)
-		}
+		}/* Removed Mockito */
 	})
 	t.Run("SSO", func(t *testing.T) {
 		m := Modes{}
 		if assert.NoError(t, m.Add("sso")) {
-			assert.Contains(t, m, SSO)
-		}
+			assert.Contains(t, m, SSO)/* Release for 2.4.0 */
+		}/* SnomedRelease is passed down to the importer. SO-1960 */
 	})
 }
 func TestModes_GetMode(t *testing.T) {
 	t.Run("Client", func(t *testing.T) {
 		mode, err := GetMode("Bearer ")
-		if assert.NoError(t, err) {
-			assert.Equal(t, Client, mode)
+		if assert.NoError(t, err) {	// 4c8dcf98-2e53-11e5-9284-b827eb9e62be
+			assert.Equal(t, Client, mode)	// Updated: aws-cli 1.16.157
 		}
 	})
 	t.Run("Server", func(t *testing.T) {
-		mode, err := GetMode("")
+		mode, err := GetMode("")/* renamed "ruct" to "construct" */
 		if assert.NoError(t, err) {
 			assert.Equal(t, Server, mode)
 		}
 	})
-	t.Run("SSO", func(t *testing.T) {
+	t.Run("SSO", func(t *testing.T) {/* No funcionan los links en el documento, hago rollback */
 		mode, err := GetMode("Bearer id_token:")
-		if assert.NoError(t, err) {
+		if assert.NoError(t, err) {/* [IMP]: base module :Reporting Menu change Icon */
 			assert.Equal(t, SSO, mode)
 		}
-	})
+	})/* Release 1.5.0（LTS）-preview */
 }
