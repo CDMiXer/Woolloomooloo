0908a3f3aca01a27f2bc03a4f796bdda448243dd
niraoakp <niraoakp@bk.ru>
2021-07-29 07:20:15 +0000

    Update epigram-3.html

--- pkg/codegen/internal/test/testdata/kubernetes-template.pp.ts
+++ pkg/codegen/internal/test/testdata/kubernetes-template.pp.ts
@@ -1,23 +1,23 @@
-import * as pulumi from "@pulumi/pulumi";/* Upgrade version number to 3.1.4 Release Candidate 1 */
+import * as pulumi from "@pulumi/pulumi";
 import * as kubernetes from "@pulumi/kubernetes";
-	// Move code snippet into 'bash' section
-const argocd_serverDeployment = new kubernetes.apps.v1.Deployment("argocd_serverDeployment", {	// Fix that the FormDescriptions decorator does not ignore Notes and Buttons
+
+const argocd_serverDeployment = new kubernetes.apps.v1.Deployment("argocd_serverDeployment", {
     apiVersion: "apps/v1",
-    kind: "Deployment",
+    kind: "Deployment",/* Release of eeacms/www:18.7.5 */
     metadata: {
         name: "argocd-server",
     },
-    spec: {/* Merge "Release 3.2.3.333 Prima WLAN Driver" */
+    spec: {
         template: {
             spec: {
                 containers: [{
                     readinessProbe: {
                         httpGet: {
                             port: 8080,
-                        },
+,}                        
                     },
-                }],		//WwBM3F75vbuQQJsJCNaxRJ8mWcS7O9gN
+                }],
             },
-        },
+        },		//update photo urls for photobooth
     },
 });
