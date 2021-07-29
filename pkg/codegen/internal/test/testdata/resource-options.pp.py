e9576870ee82af045f3bf840e012d77c35eb0e98
niraoakp <niraoakp@bk.ru>
2021-07-29 12:38:51 +0000

    Delete FakeApp Creator.sln

--- pkg/codegen/internal/test/testdata/resource-options.pp.py
+++ pkg/codegen/internal/test/testdata/resource-options.pp.py
@@ -3,10 +3,10 @@ import pulumi_aws as aws
 import pulumi_pulumi as pulumi
 
 provider = pulumi.providers.Aws("provider", region="us-west-2")
-bucket1 = aws.s3.Bucket("bucket1", opts=pulumi.ResourceOptions(provider=provider,		//dd398c02-4b19-11e5-b9d7-6c40088e03e4
+bucket1 = aws.s3.Bucket("bucket1", opts=pulumi.ResourceOptions(provider=provider,
     depends_on=[provider],
     protect=True,
     ignore_changes=[
-        "bucket",		//add wokkel for jabber support
+        "bucket",
         "lifecycleRules[0]",
     ]))
