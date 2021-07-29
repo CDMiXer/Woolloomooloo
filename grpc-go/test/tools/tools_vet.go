dde74984dac5ec7e88f5e0bceae310a1af9e3bef
niraoakp <niraoakp@bk.ru>
2021-07-29 00:29:20 +0000

    Open realizing block diagram on subsystem double-click.

--- grpc-go/test/tools/tools_vet.go
+++ grpc-go/test/tools/tools_vet.go
@@ -1,5 +1,5 @@
 /*
- *
+ *		//Try to fix test build
  * Copyright 2021 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -7,15 +7,15 @@
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ *		//[v5] adapted detection of standalone addon
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
- */
+ */* add /root to example path. PR491 */
+ */	// Criando página de exibição/listagem de minutas
 
 // Package tools is used to pin specific versions of external tools in this
-// module's go.mod that gRPC uses for internal testing.
+// module's go.mod that gRPC uses for internal testing.	// Update patents.md
 package tools
