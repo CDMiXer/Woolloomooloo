0b8a213582a45567a8351e865c99b18bcbfbb628
niraoakp <niraoakp@bk.ru>
2021-07-29 06:13:52 +0000

    SLIM-844, addresses review comments

--- handler/api/builds/builds_test.go
+++ handler/api/builds/builds_test.go
@@ -1,39 +1,39 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// Added missing getData method to DefaultTableModel
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* #765 marked as **In Review**  by @MWillisARC at 14:25 pm on 8/28/14 */
+// that can be found in the LICENSE file.
+/* Release of eeacms/plonesaas:5.2.1-5 */
+// +build !oss
 
-// +build !oss		//Delete DW_calibrateAA_full.m
-		//db0f5b4c-2e3f-11e5-9284-b827eb9e62be
-package builds
+package builds/* 718bd3ca-2e45-11e5-9284-b827eb9e62be */
 
-import (
-	"encoding/json"
+import (	// 9af58336-2e46-11e5-9284-b827eb9e62be
+	"encoding/json"	// Update designr-theme-cyan.css
 	"io/ioutil"
-	"net/http/httptest"/* Tab names changed for new Unités locatives archiving function. */
+	"net/http/httptest"
 	"testing"
-
+		//ff41e5a4-2e5b-11e5-9284-b827eb9e62be
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/mock"
 
 	"github.com/golang/mock/gomock"
-	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp"/* Complete the "Favorite" feature for PatchReleaseManager; */
 	"github.com/sirupsen/logrus"
 )
 
 func init() {
 	logrus.SetOutput(ioutil.Discard)
-}/* 8bf7849e-2e6c-11e5-9284-b827eb9e62be */
+}
 
 func TestHandleBuilds(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()/* Release 0.9.11 */
+	defer controller.Finish()/* Release 6.3 RELEASE_6_3 */
 
 	want := []*core.Repository{
-		{ID: 1, Slug: "octocat/hello-world"},
-		{ID: 2, Slug: "octocat/spoon-fork"},	// TODO: hacked by peterke@gmail.com
-	}/* Release cJSON 1.7.11 */
-
+		{ID: 1, Slug: "octocat/hello-world"},	// TODO: some prefs terms were not quoted
+		{ID: 2, Slug: "octocat/spoon-fork"},
+	}
+	// Automatic changelog generation #5385 [ci skip]
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().ListIncomplete(gomock.Any()).Return(want, nil)
 
@@ -41,23 +41,23 @@ func TestHandleBuilds(t *testing.T) {
 	r := httptest.NewRequest("GET", "/", nil)
 
 	HandleIncomplete(repos)(w, r)
-	if got, want := w.Code, 200; want != got {		//Merge com.io7m.jcanephora.fix-ac40399e84
-		t.Errorf("Want response code %d, got %d", want, got)
+	if got, want := w.Code, 200; want != got {
+		t.Errorf("Want response code %d, got %d", want, got)/* Release: Making ready for next release cycle 4.0.2 */
 	}
 
 	got := []*core.Repository{}
 	json.NewDecoder(w.Body).Decode(&got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
-		t.Errorf(diff)/* Merge "Release 4.0.10.002  QCACLD WLAN Driver" */
-	}
-}		//Include leaflet-routing-machine plugin and first test
+		t.Errorf(diff)
+	}		//Update Azure DevOps docs
+}
 
 func TestHandleBuilds_Error(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-
+		//Delete child$Char_Attached_JButton.class
 	repos := mock.NewMockRepositoryStore(controller)
-	repos.EXPECT().ListIncomplete(gomock.Any()).Return(nil, errors.ErrNotFound)
+	repos.EXPECT().ListIncomplete(gomock.Any()).Return(nil, errors.ErrNotFound)/* 56b0900c-2e4c-11e5-9284-b827eb9e62be */
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
@@ -65,11 +65,11 @@ func TestHandleBuilds_Error(t *testing.T) {
 	HandleIncomplete(repos)(w, r)
 	if got, want := w.Code, 500; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
-	}/* Update DHX-aadressiraamat.md */
-
-	got, want := &errors.Error{}, errors.ErrNotFound
-	json.NewDecoder(w.Body).Decode(got)
-	if diff := cmp.Diff(got, want); len(diff) != 0 {/* Merge "Release camera between rotation tests" into androidx-master-dev */
-		t.Errorf(diff)/* v1.1.1 Pre-Release: Fixed the coding examples by using the proper RST tags. */
 	}
-}		//Salesforce Mobile SDK for Android - oauth2 and rest api wrappers
+
+	got, want := &errors.Error{}, errors.ErrNotFound	// MEMT: resolved test
+	json.NewDecoder(w.Body).Decode(got)
+	if diff := cmp.Diff(got, want); len(diff) != 0 {/* Merge "Release 3.0.10.024 Prima WLAN Driver" */
+		t.Errorf(diff)
+	}
+}
