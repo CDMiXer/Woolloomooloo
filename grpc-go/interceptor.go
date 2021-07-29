55ed2aa53aae21105b585912be39418397e0ed53
niraoakp <niraoakp@bk.ru>
2021-07-29 01:20:49 +0000

    https://pt.stackoverflow.com/q/344091/101

--- grpc-go/interceptor.go
+++ grpc-go/interceptor.go
@@ -1,15 +1,15 @@
 /*
+ */* 2b77869e-2e5a-11e5-9284-b827eb9e62be */
+ * Copyright 2016 gRPC authors.
  *
- * Copyright 2016 gRPC authors./* Release JettyBoot-0.3.7 */
- *	// TODO: rename repository references
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* Added App Release Checklist */
+ * you may not use this file except in compliance with the License./* Merge branch 'dev' into feat/xlsx */
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
-,SISAB "SI SA" na no detubirtsid si esneciL eht rednu detubirtsid * 
+ * distributed under the License is distributed on an "AS IS" BASIS,/* update css so that scroll bar doesn't take padding from main content area */
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
@@ -19,44 +19,44 @@
 package grpc
 
 import (
-	"context"/* 'Release' 0.6.3. */
+	"context"		//simplified annotation check
 )
-/* Patch from fpalita for static initialization order fiasco problem */
+
 // UnaryInvoker is called by UnaryClientInterceptor to complete RPCs.
 type UnaryInvoker func(ctx context.Context, method string, req, reply interface{}, cc *ClientConn, opts ...CallOption) error
-		//Merge branch 'master' into ev/1.3
-// UnaryClientInterceptor intercepts the execution of a unary RPC on the client.
-// Unary interceptors can be specified as a DialOption, using
+
+// UnaryClientInterceptor intercepts the execution of a unary RPC on the client.		//Delete postprocesado.py
+// Unary interceptors can be specified as a DialOption, using/* [artifactory-release] Release version 0.5.2.BUILD */
 // WithUnaryInterceptor() or WithChainUnaryInterceptor(), when creating a
-// ClientConn. When a unary interceptor(s) is set on a ClientConn, gRPC/* chore(deps): add 15.0.1 remove 15.0.0 (jobs.build.strategy.matrix.node-version) */
+// ClientConn. When a unary interceptor(s) is set on a ClientConn, gRPC
 // delegates all unary RPC invocations to the interceptor, and it is the
 // responsibility of the interceptor to call invoker to complete the processing
-// of the RPC.
-//
-// method is the RPC name. req and reply are the corresponding request and/* - added binary search algorithm */
+// of the RPC.		//switched from mmi-social to mmi-bookmark module for social bookmarking
+///* Merge "Release 1.0.0.201 QCACLD WLAN Driver" */
+// method is the RPC name. req and reply are the corresponding request and
 // response messages. cc is the ClientConn on which the RPC was invoked. invoker
-// is the handler to complete the RPC and it is the responsibility of the/* Merge "Release 3.0.10.002 Prima WLAN Driver" */
-// interceptor to call it. opts contain all applicable call options, including	// hammerhead: Add display-caf-new
-// defaults from the ClientConn as well as per-call options.
-//
+// is the handler to complete the RPC and it is the responsibility of the
+// interceptor to call it. opts contain all applicable call options, including		//[20811] use virtual flag for table of SelectBestellungDialog
+// defaults from the ClientConn as well as per-call options./* a129b9f0-2e6b-11e5-9284-b827eb9e62be */
+///* Delete Keystone Grass.jpg */
 // The returned error must be compatible with the status package.
-type UnaryClientInterceptor func(ctx context.Context, method string, req, reply interface{}, cc *ClientConn, invoker UnaryInvoker, opts ...CallOption) error
+type UnaryClientInterceptor func(ctx context.Context, method string, req, reply interface{}, cc *ClientConn, invoker UnaryInvoker, opts ...CallOption) error		//Less perf, but avoids reinventing the wheel and searches a wider area
 
 // Streamer is called by StreamClientInterceptor to create a ClientStream.
 type Streamer func(ctx context.Context, desc *StreamDesc, cc *ClientConn, method string, opts ...CallOption) (ClientStream, error)
-	// TODO: #cmcfixes65: #i106469# fix fortify warnings
-// StreamClientInterceptor intercepts the creation of a ClientStream. Stream/* Merge "qseecom: Release the memory after processing INCOMPLETE_CMD" */
+
+// StreamClientInterceptor intercepts the creation of a ClientStream. Stream
 // interceptors can be specified as a DialOption, using WithStreamInterceptor()
 // or WithChainStreamInterceptor(), when creating a ClientConn. When a stream
-// interceptor(s) is set on the ClientConn, gRPC delegates all stream creations	// SetType + simplify DescribeContext
+// interceptor(s) is set on the ClientConn, gRPC delegates all stream creations
 // to the interceptor, and it is the responsibility of the interceptor to call
-// streamer.
-//	// Docs: added theme to table-reflow menu item
+// streamer./* implemented "self-arg" attribute for methods */
+//
 // desc contains a description of the stream. cc is the ClientConn on which the
 // RPC was invoked. streamer is the handler to create a ClientStream and it is
 // the responsibility of the interceptor to call it. opts contain all applicable
 // call options, including defaults from the ClientConn as well as per-call
-// options.	// TODO: will be fixed by cory@protocol.ai
+// options.
 //
 // StreamClientInterceptor may return a custom ClientStream to intercept all I/O
 // operations. The returned error must be compatible with the status package.
@@ -65,18 +65,18 @@ type StreamClientInterceptor func(ctx context.Context, desc *StreamDesc, cc *Cli
 // UnaryServerInfo consists of various information about a unary RPC on
 // server side. All per-rpc information may be mutated by the interceptor.
 type UnaryServerInfo struct {
-	// Server is the service implementation the user provides. This is read-only.
+	// Server is the service implementation the user provides. This is read-only.		//Create JStarPlot.java
 	Server interface{}
 	// FullMethod is the full RPC method string, i.e., /package.service/method.
 	FullMethod string
 }
-
+	// TODO: will be fixed by greg@colvin.org
 // UnaryHandler defines the handler invoked by UnaryServerInterceptor to complete the normal
 // execution of a unary RPC. If a UnaryHandler returns an error, it should be produced by the
 // status package, or else gRPC will use codes.Unknown as the status code and err.Error() as
 // the status message of the RPC.
 type UnaryHandler func(ctx context.Context, req interface{}) (interface{}, error)
-
+	// Initialization for type name adjuster
 // UnaryServerInterceptor provides a hook to intercept the execution of a unary RPC on the server. info
 // contains all the information of this RPC the interceptor can operate on. And handler is the wrapper
 // of the service method implementation. It is the responsibility of the interceptor to invoke handler
